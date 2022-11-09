1b18712f4da85a044c19e0598542c2c680f1ba7b
HyebinShin <hyeviin.dev@gmail.com>
2022-11-09 18:55:56 +0900

    Change: 로또 구입 기능
    메소드 이름 변경
    Publish -> Budget

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,26 +8,27 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println(Print.ASK_BUY);
-        String input = inputPublishNum();
+        int budget = getBudget();
+        System.out.println("budget: "+budget);
 
     }
 
     //로또 구입
-    private static String inputPublishNum() {
+    private static String inputBudget() {
         String input = Console.readLine();
 
-        validateInputPublish(input);
+        validateInputBudget(input);
 
         return input;
     }
 
-    private static void validateInputPublish(String input) {
+    private static void validateInputBudget(String input) {
         if(!Pattern.matches("^[0-9]*000$", input)) {
             throw new IllegalArgumentException("[ERROR] 1,000 단위의 숫자만 입력해 주세요.");
         }
     }
 
-    private static int getPublishNum() {
-        return 0;
+    private static int getBudget() {
+        return Integer.parseInt(inputBudget());
     }
 }
