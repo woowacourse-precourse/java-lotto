43e07714c06eef05721c7e56d23ebb1fdccb02e9
ksh-beginner <ohk9134@naver.com>
2022-11-10 02:24:38 +0900

    test(LottoGameServiceTest) : 로또 금액 파라미터 String으로 리팩토링

--- src/test/java/lotto/service/LottoGameServiceTest.java
+++ src/test/java/lotto/service/LottoGameServiceTest.java
@@ -18,16 +18,16 @@ class LottoGameServiceTest {
         lottoGameService = new LottoGameService();
     }
     @ParameterizedTest
-    @ValueSource(ints = {10000, 20000, 35000, 1000})
-    void 로또_발행_개수_반환(int lottoPurchaseAmount) {
+    @ValueSource(strings = {"10000", "20000", "35000", "1000"})
+    void 로또_발행_개수_반환(String lottoPurchaseAmount) {
         int lottoIssueCount = lottoGameService.getLottoIssueCount(lottoPurchaseAmount);
 
-        assertThat(lottoIssueCount).isEqualTo(lottoPurchaseAmount / 1000);
+        assertThat(lottoIssueCount).isEqualTo(Integer.parseInt(lottoPurchaseAmount) / 1000);
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {500, 1500, 1750, 10002})
-    void 로또_구입_금액이_1000으로_나누어_떨어지지_않으면_예외_발생(int lottoPurchaseAmount) {
+    @ValueSource(strings = {"500", "1500", "1750", "10002"})
+    void 로또_구입_금액이_1000으로_나누어_떨어지지_않으면_예외_발생(String lottoPurchaseAmount) {
         assertThatThrownBy(() -> lottoGameService.getLottoIssueCount(lottoPurchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
