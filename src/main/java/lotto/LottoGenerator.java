9d77b2d189f957253b2cbb9080952a21c82a0f06
minhee020 <minhee020@naver.com>
2022-11-10 01:09:53 +0900

    feat : 로또 수량에 맞게 로또를 생성하기 구현

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,14 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class LottoGenerator {
-    private Integer lottoAmount;
+    private final Integer lottoAmount;
     LottoGenerator(Integer purchasedAmount) {
         this.lottoAmount = purchasedAmount / 1000;
     }
 
     public List<List<Integer>> generateLotto() {
-        return null;
+        List<List<Integer>> result = null;
+        for (int i = 0; i < lottoAmount; i++){
+            result.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        }
+        return result;
     }
 }
