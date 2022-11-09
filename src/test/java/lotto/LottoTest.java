d7eeaa35dc92edc949786c266bde7fe5c71ded1a
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-11-09 16:54:10 +0900

    test<LottoTest>: 구입 금액 관련 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,4 +25,12 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 구입 금액이 1,000원 단위로 나누어 떨이지지 않으면 예외가 발생한다.")
+    @Test
+    void wrongOfLottoAmount() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> Lotto.numberOfLottosQuantity(15400L))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 }
