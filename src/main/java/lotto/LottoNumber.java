20bbfa176340a78bac2b95643bfc4c1e21f00a7a
JJ <jeongjae1996@naver.com>
2022-11-10 06:20:44 +0900

    feat: 입력 받은 로또 구매 금액을 통해, 발행 받을 로또 개수를 계산하는 기능 구현

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -4,4 +4,10 @@ public class LottoNumber {
     public LottoNumber() {
         Money money = new Money();
     }
+
+    private int computeLottoNumber(int money) {
+        final int LOTTO_SELLING_PRICE = 1000;
+
+        return money / LOTTO_SELLING_PRICE;
+    }
 }
