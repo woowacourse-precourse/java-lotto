5759de3122e4fb1bc442ccb8985b5ebb347b778f
yg0585 <yg0585@naver.com>
2022-11-09 19:02:55 +0900

    feat: 로또 발행 기능 구현

---
+++ src/main/java/lotto/LottoGenerator.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class LottoGenerator {
+
+    static final int LOTTO_MAX_LEN = 8;
+
+    public List<Integer> createRandomLotto() {
+        List<Integer> numbers = new ArrayList<>();
+
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            addNum(numbers, randomNumber);
+        }
+
+        numbers.sort(Comparator.naturalOrder());
+
+        return numbers;
+    }
+
+    private void addNum(List<Integer> numbers, int randomNumber) {
+        if (!numbers.contains(randomNumber)) {
+            numbers.add(randomNumber);
+        }
+    }
+}
