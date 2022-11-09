96e50ff0418105247e109a5b5fa08823c87c4dd5
ywj9811 <ywj9811@naver.com>
2022-11-10 03:46:34 +0900

    feat: LottoNumberException 클래스
    중복된 값이 있다면 예외를 발생시키도록

--- src/main/java/lotto/exception/LottoNumberException.java
+++ src/main/java/lotto/exception/LottoNumberException.java
@@ -3,13 +3,27 @@ package lotto.exception;
 import java.util.List;
 
 public class LottoNumberException {
-    public void lottoNumberValidate(List<Integer> numbers) {
+    public void lottoNumberSize(List<Integer> numbers) {
         try {
             if (numbers.size() != 6) {
                 throw new IllegalArgumentException("[ERROR] 크기가 6이 아닙니다.");
             }
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public void lottoNumberDuplicated(List<Integer> numbers) {
+        try {
+            for (Integer num : numbers) {
+                if (numbers.indexOf(num) != numbers.lastIndexOf(num)) {
+                    throw new IllegalArgumentException("중복된 값이 있습니다.");
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            throw e;
         }
     }
 }
