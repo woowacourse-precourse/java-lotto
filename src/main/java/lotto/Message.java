363878bbeaf37613429e00d97d6c3f73120099d7
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-09 18:39:11 +0900

    feat : 당첨 내역을 출력하는 메서드 추가 (미완성)

--- src/main/java/lotto/Message.java
+++ src/main/java/lotto/Message.java
@@ -7,11 +7,22 @@ public class Message {
     public static final String BONUS_NUMBER_INPUT = "보너스 번호를 입력해 주세요.";
     public static final String WINNING_STATS = "당첨 통계";
     public static final String LINES = "---";
-    public static final String THREE_MATCHES = "3개 일치 (5,000원) - 개";
-    public static final String FOUR_MATCHES = "4개 일치 (50,000원) - 개";
-    public static final String FIVE_MATCHES = "5개 일치 (1,500,000원) - 개";
-    public static final String FIVE_BONUS_MATCHES = "5개 일치, 보너스 볼 일치 (30,000,000원) - 개";
-    public static final String ALL_MATCHES = "6개 일치 (2,000,000,000원) - 개";
+    public static final String THREE_MATCHES = "3개 일치 (5,000원) - ";
+    public static final String FOUR_MATCHES = "4개 일치 (50,000원) - ";
+    public static final String FIVE_MATCHES = "5개 일치 (1,500,000원) - ";
+    public static final String FIVE_BONUS_MATCHES = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    public static final String ALL_MATCHES = "6개 일치 (2,000,000,000원) - ";
     public static final String YIELDS= "총 수익률은 %입니다.";
     public static final String ERROR = "[ERROR] ";
+
+    static void printWinningHistroy() {
+        System.out.println(WINNING_STATS);
+        System.out.println(LINES);
+        System.out.println(THREE_MATCHES + "" + "개");
+        System.out.println(FOUR_MATCHES + "" + "개");
+        System.out.println(FIVE_MATCHES + "" + "개");
+        System.out.println(FIVE_BONUS_MATCHES + "" + "개");
+        System.out.println(ALL_MATCHES + "" + "개");
+        System.out.println(YIELDS);
+    }
 }
