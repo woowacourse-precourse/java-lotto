dc77f1a2dbbd687c7b721958b8d6fba266734b50
Reznov <reznov03048@gmail.com>
2022-11-09 22:04:52 +0900

    feat: LottoController 생성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,17 @@
+package lotto.controller;
+
+public class LottoController {
+
+    public void start() {
+        // 금액 입력 받음
+        
+        // 로또 n장 생성 (1장 당 6자리 숫자 랜덤 생성) -> Lotto.java
+        
+        // 당첨 번호 입력 받음
+        
+        // 결과 생성
+        
+        // 결과 출력
+    }
+
+}
