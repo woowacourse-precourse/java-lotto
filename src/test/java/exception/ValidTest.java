3e9cae98ba6185d439f11d52b01aeae4b5886c1c
gyuturn <rbtjrrbals@gmail.com>
2022-11-09 23:08:19 +0900

    test($exception): isMoneyOverThousand 테스트
    EXC-A-3
    테스트목록
    - 1000미만인경우: false 리턴
    - 1000이상인경우: true 리턴

--- src/test/java/exception/ValidTest.java
+++ src/test/java/exception/ValidTest.java
@@ -31,8 +31,25 @@ class ValidTest {
         assertThat(result).isTrue();
     }
 
-//    @Test
-//    @DisplayName("[Valid]구매금액이 1000원 이상인경우 true 리턴")
-//    void isMoneyOverThousand() {
-//    }
+    @Test
+    @DisplayName("[Valid]구매금액이 1000원 미만인 경우 false 리턴")
+    void MoneyUnderThousand() {
+        //given
+        int money = 823;
+        //when
+        boolean result = Valid.isMoneyOverThousand(money);
+        //then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("[Valid]구매금액이 1000원 이상인경우 true 리턴")
+    void MoneyOverThousand() {
+        //given
+        int money = 1000;
+        //when
+        boolean result = Valid.isMoneyOverThousand(money);
+        //then
+        assertThat(result).isTrue();
+    }
 }
