bdc11c946cf89d62484adc822b569e9b08ffa6ad
jinny-l <jinny.l.dev@gmail.com>
2022-11-09 18:06:27 +0900

    feat(LottoGenerator): 로또 당첨 번호를 추첨하는 기능 구현

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -6,6 +6,18 @@ import lotto.constant.Constant;
 
 public class LottoGenerator {
 
+    public static List<Integer> getWinningNumbers() {
+        List<Integer> winningNumbers = generateNumbers();
+        winningNumbers.addAll(generateBonusNumber());
+        
+        return winningNumbers;
+    }
+
+    private static List<Integer> generateBonusNumber() {
+        return Randoms.pickUniqueNumbersInRange(Constant.LOTTO_START_NUMBER, Constant.LOTTO_END_NUMBER,
+                Constant.BONUS_NUMBER_SIZE);
+    }
+
     private static List<Integer> generateNumbers() {
         return Randoms.pickUniqueNumbersInRange(Constant.LOTTO_START_NUMBER, Constant.LOTTO_END_NUMBER,
                 Constant.LOTTO_SIZE);
