51ffcf588e5707669a2b6a9d47c85dedd4d93fb2
whdcks420 <whdcks420@gmail.com>
2022-11-10 01:47:47 +0900

    test(validator): 구매금액 유효성 검사

---
+++ src/test/java/lotto/validator/PurchaseAmountValidatorTest.java
@@ -0,0 +1,19 @@
+package lotto.validator;
+
+import lotto.utils.InputUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PurchaseAmountValidatorTest {
+
+    @DisplayName("구매 금액 입력 검사")
+    @ParameterizedTest
+    @ValueSource(strings = {"1001", "-1000", "1234"})
+    void purchaseAmountTest(String money) {
+        assertThrows(IllegalArgumentException.class, () -> PurchaseAmountValidator.validate(money));
+    }
+
+}
