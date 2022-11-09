e47508a158c515d875f3918aac2d00516ba3c4ed
antaeho <an981022@naver.com>
2022-11-09 20:36:28 +0900

    Feat: 기능사항 1 구현
    돈, 당첨 번호, 보너스 번호 입력받기
    돈에 따른 로또 발행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,37 @@
 package lotto;
 
+import java.util.*;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // input money
+        System.out.println("구입금액을 입력해 주세요.");
+        int money = Integer.parseInt(readLine());
+        // exception
+        if(money%1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+
+        // how many lotto
+        int cnt = money/1000;
+        System.out.println(cnt + "개를 구매했습니다.");
+        // print lotto number
+        for(int i = 0; i < cnt; i++) {
+            List<Integer> lottoNumbers;
+            lottoNumbers = pickUniqueNumbersInRange(1, 45 ,6);
+            System.out.println(lottoNumbers);
+        }
+        //당첨 번호, 보너스 번호 입력 받기
+        String winningNumber;
+        String bonusNumber;
+        System.out.println("당첨 번호를 입력해 주세요.");
+        winningNumber = readLine();
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        bonusNumber = readLine();
     }
+
 }
