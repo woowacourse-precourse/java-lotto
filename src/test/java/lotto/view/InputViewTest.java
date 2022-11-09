5adf1f82f7eca7c16b0a62af0aeeb8bfac55d2cb
ksh-beginner <ohk9134@naver.com>
2022-11-10 01:15:52 +0900

    test(InputViewTest) : 로또 구입 금액 입력 받는 기능 테스트

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -1,6 +1,31 @@
 package lotto.view;
 
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class InputViewTest {
 
+    InputView inputView;
+
+    @BeforeEach
+    public void generateInputView() {
+        inputView = new InputView();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"20000"})
+    void 로또_구입_금액_입력_받기(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        String lottoPurchaseAmount = inputView.inputLottoPurchaseAmount();
+
+        assertThat(lottoPurchaseAmount).isEqualTo(input);
+    }
 }
