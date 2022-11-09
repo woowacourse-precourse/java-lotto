0b2c13c577df0976f4db2a5b6c07a8f16a7b83d2
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-09 18:12:21 +0900

    feat(lotto): 로또 구입 금액 입력 파트 기능 구현

--- src/main/java/domain/UserMoney.java
+++ src/main/java/domain/UserMoney.java
@@ -1,2 +1,29 @@
-package domain;public class UserMoney {
+package domain;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserMoney {
+
+
+    public static int inputMoney(){
+        String input_money = Console.readLine();
+        return checkMoney(input_money);
+    }
+
+    public static int checkMoney(String input_money) {
+        int money=0;
+        try {
+            money = Integer.parseInt(input_money);
+        }
+        catch(Exception e){
+            throw new IllegalArgumentException("[ERROR] 사용자의 금액 입력 오류입니다.");
+        }
+
+        if(money % 1000>0){
+            throw new IllegalArgumentException("[ERROR] 올바르지 못한 입력 금액입니다.");
+        }
+        return money;
+    }
+
+
 }
