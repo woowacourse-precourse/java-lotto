5829ba3ff226441ffe4268f69f3ce6abc03e80f9
greeng00se <thegreengoose7@gmail.com>
2022-11-09 22:58:04 +0900

    feat(Lotto): 로또를 표현하는 Lotto 클래스 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Lotto {
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+    private static final String INVALID_SIZE_MESSAGE = "[ERROR] 올바르지 않은 로또 개수입니다.";
+    private static final String DUPLICATED_NUMBER_MESSAGE = "[ERROR] 중복된 로또 번호입니다.";
+    private final List<LottoNumber> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        this.numbers = to(numbers);
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(INVALID_SIZE_MESSAGE);
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        HashSet<Integer> nonDuplicatedNumbers = new HashSet<>(numbers);
+        if (nonDuplicatedNumbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(DUPLICATED_NUMBER_MESSAGE);
+        }
+    }
+
+    private List<LottoNumber> to(List<Integer> numbers) {
+        return numbers.stream()
+                .map(LottoNumber::valueOf)
+                .collect(toList());
+    }
+}
---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Lotto 클래스")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class LottoTest {
+
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호의_개수가_6개_미만인_경우에도_IllegalArgumentException_예외를_던진다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void toString_메서드는_로또_번호를_반환한다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
+    }
+}
