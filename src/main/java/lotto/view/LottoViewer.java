b597c9d4ae6bd59e7a9490433398956bd5db5455
whdcks420 <whdcks420@gmail.com>
2022-11-09 18:54:40 +0900

    feat(view): 로또 구매개수 출력문구 기능

--- src/main/java/lotto/view/LottoViewer.java
+++ src/main/java/lotto/view/LottoViewer.java
@@ -1,7 +1,11 @@
 package lotto.view;
 
 public class LottoViewer {
-    public static void showEnterPurchaseAmountText() {
+    public static void showEnterPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void showLottoPurchaseCount(int count) {
+        System.out.println(count + "개를 구매했습니다.");
     }
 }
