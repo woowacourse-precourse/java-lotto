d99f6b99423e33f434e847c725a7bc8188649177
Eungyu-code <Agyu625@gmail.com>
2022-11-09 20:25:09 +0900

    update(printErrorAboutRange()): 출력하는 문구의 오탈자 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -40,6 +40,6 @@ public class Application {
 
     public static void printErrorAboutRange() {
 
-        System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다");
+        System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
 }
