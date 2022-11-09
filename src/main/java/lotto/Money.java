f16f436aee679558347a7d79f76f3293fabe55df
saewoo1 <wooozooo1123@gmail.com>
2022-11-09 17:02:45 +0900

    feat(Money): 입력된 수가 숫자로만 이루어져 있는지 검사

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -6,4 +6,20 @@ public class Money {
   public String inputMoney() {
     return Console.readLine();
   }
+
+  public boolean checkMoneyIsDigit() {
+    String userMoney = inputMoney();
+
+    for (int i = 0; i < userMoney.length(); i++)
+    {
+      if (!Character.isDigit(userMoney.charAt(i)))
+        throw new IllegalArgumentException();
+    }
+    return true;
+  }
+
+  public boolean checkDividableMoney() {
+
+    return true;
+  }
 }
