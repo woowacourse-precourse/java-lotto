3e4a1c81a176e994540bbe2a3d1837410c209004
mckim123 <minchan06@gmail.com>
2022-11-10 03:23:06 +0900

    feat(lotto): implement getting input of lotto price
    Add inputPrice method to get user input of price
    Need to implement validation method

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -1,10 +1,26 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.LottoAnswer;
 
 public class InputController {
+    private int price;
+    private static String INPUT_STR = "구입금액을 입력해 주세요.";
+
     public int getPrice() {
-        return 0;
+        return inputPrice();
+    }
+
+    private int inputPrice() {
+        System.out.print(INPUT_STR);
+        String userInputPrice = Console.readLine();
+        price = Integer.parseInt(userInputPrice);
+        validatePrice(price);
+        return price;
+    }
+
+    private void validatePrice(int price) {
+
     }
 
     public LottoAnswer getLottoAnswer() {
