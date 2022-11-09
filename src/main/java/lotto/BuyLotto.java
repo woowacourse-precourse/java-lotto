a998bf9ca5a6ad72f7ccc0cb5f49a5a0cc285379
dohyun <ldh19738@gmail.com>
2022-11-09 23:30:24 +0900

    feat(BuyLotto): 가격 입력받는 메서드와 예외 처리

--- src/main/java/lotto/BuyLotto.java
+++ src/main/java/lotto/BuyLotto.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class BuyLotto {
+    private final int INPUT_UNIT = 1000;
+
     public void printMessage() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+    
+    public String inputPrice() {
+        String price = Console.readLine();
+        inputPriceException(price);
+
+        return price;
+    }
+
+    public void inputPriceException(String price) {
+        if((Integer.parseInt(price) % INPUT_UNIT) != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매금액은 1000원 단위여야 합니다.");
+        }
     }
 }
