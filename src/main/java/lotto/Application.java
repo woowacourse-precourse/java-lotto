383df7dd24049cb0479d1949a5c4bc6d401d916b
WonJeongYeon <wonjungyoun@naver.com>
2022-11-09 15:52:12 +0900

    feat: 구입 금액 입력받는 기능 추가
    사용자가 구입할 금액을 입력받는다. 숫자 이외의 값을 입력하거나, 1,000의 배수가 아닐 경우의 예외처리가 포함된다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
+
+        inputPay();
+
         // TODO: 프로그램 구현
+    }
+
+    public static int inputPay() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return checkPay(readLine());
+    }
+
+    public static int checkPay(String str) {
+        int pay;
+        try {
+            pay = Integer.parseInt(str);
+
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 구입 금액은 숫자만 입력하실 수 있습니다.");
+            throw new IllegalArgumentException();
+        }
+        if (pay % 1_000 != 0) {
+            System.out.println("[ERROR] 구입 금액은 1,000원 단위로 이루어져야 합니다.");
+            throw new IllegalArgumentException();
+        }
+        return pay;
     }
 }
