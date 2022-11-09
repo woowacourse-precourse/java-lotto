e06ecebe900f0fc66aa54ef3ce8c655907e555de
yeonsu <jack0810@kookmin.ac.kr>
2022-11-09 20:39:17 +0900

    feat() getMoney (src/main/java/baseball/Application.java)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,47 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
 public class Application {
+    public int getMoney(){
+        String inputMoney = Console.readLine();
+        isInteger(inputMoney);
+
+        int money = Integer.parseInt((inputMoney));
+        isSplit(money);
+
+        int lottoCnt;
+        lottoCnt = money / 1000;
+        return lottoCnt;
+    }
+    public static void isSplit(int money){
+        if (money % 1000 != 0){
+            throw new IllegalArgumentException("다시 입력 해주세요");
+        }
+    }
+    public static void isInteger(String inputMoney){
+        for(int i = 0; i < inputMoney.length(); i++){
+            if (inputMoney.charAt(i) < '0' || inputMoney.charAt(i) > '9' ){
+                throw new IllegalArgumentException("다시 입력 해주세요");
+            }
+        }
+    }
+    public ArrayList<Integer> GenerateLotto(){ // 숫자 생성
+        ArrayList<Integer> lotto = new ArrayList<>();
+        while (lotto.size() < 8) {
+            int randomNumber = Randoms.pickNumberInRange(1, 45);
+            if (!lotto.contains(randomNumber)) {
+                lotto.add(randomNumber);
+            }
+        }
+        return lotto;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
+
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,32 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateCnt(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != 6 ) {
             throw new IllegalArgumentException();
         }
     }
+    private void validateCnt(List<Integer> numbers){
+        int numCnt = new HashSet<>(numbers).size();
+        if (numCnt != 6 ) {
+            throw new IllegalArgumentException();
+        }
 
-    // TODO: 추가 기능 구현
+    }
 }
