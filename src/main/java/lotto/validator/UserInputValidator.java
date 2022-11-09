38f1d7a8f73b211aebefa1a87eab3d465015326d
K-Diger <kitten.diger@gmail.com>
2022-11-09 20:56:35 +0900

    feat : validateLottoNumberRange 메서드 구현 - 입력받은 로또 번호가 1 ~ 45 사이의 수인지 검증하는 메서드

--- src/main/java/lotto/validator/UserInputValidator.java
+++ src/main/java/lotto/validator/UserInputValidator.java
@@ -9,4 +9,11 @@ public class UserInputValidator {
             throw new IllegalArgumentException();
         }
     }
+    public static void validateLottoNumberRange(String winNumber) {
+        for (int index = 0; index < winNumber.length(); index++) {
+            if (Integer.parseInt(winNumber) < 1 || Integer.parseInt(winNumber) > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
 }
