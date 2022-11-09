f04ba843831881a851a9b76d9670e664e910679c
yeonhee <ydol2@naver.com>
2022-11-10 07:22:17 +0900

    refactor(constant) : 메시지,숫자 함수 분리

---
+++ src/main/java/constant/MessageConstant.java
@@ -0,0 +1,19 @@
+package constant;
+
+public class MessageConstant {
+
+    public static final String INPUT_PRICE = "구입금액을 입력해 주세요.";
+    public static final String INPUT_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public static final String OUTPUT_PURCHASE = "%d개를 구매했습니다.";
+    public static final String OUTPUT_EARNING = "총 수익률은 %.1f입니다.";
+
+    public static final String STATISTICS_MESSAGE = "당첨 통계" + System.lineSeparator() + "---";
+    public static final String STATISTICS_THREE_MATCH = "3개 일치 (5,000원) - %d개";
+    public static final String STATISTICS_FOUR_MATCH = "4개 일치 (50,000원) - %d개";
+    public static final String STATISTICS_FIVE_MATCH = "5개 일치 (1,500,000원) - %d개";
+    public static final String STATISTICS_FIVE_MATCH_ONE_BONUS = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+    public static final String STATISTICS_SIX_MATCH = "6개 일치 (2,000,000,000원) - %d개";
+
+}
