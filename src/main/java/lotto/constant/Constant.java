af06a9449a404818d50e05ba6609ed8209dc870f
jinny-l <jinny.l.dev@gmail.com>
2022-11-09 23:43:06 +0900

    feat(InputVerifier): player input의 유효성을 검증하는 기능 구현
    - 구현 목록:
    모든 입력 값에서 정수 입력 여부 확인
    구입 금액 입력 시, 1,000 단위 여부 확인
    당첨 번호 입력 시, 6개 입력 여부 확인
    당첨 번호 입력 값이 1 ~ 45 범위에 있는지 확인
    당첨 번호 입력 시, 중복 값 입력 여부 확인

--- src/main/java/lotto/constant/Constant.java
+++ src/main/java/lotto/constant/Constant.java
@@ -6,5 +6,7 @@ public class Constant {
     public static final int LOTTO_END_NUMBER = 45;
     public static final int LOTTO_SIZE = 6;
     public static final int BONUS_NUMBER_SIZE = 1;
+    public static final int WINNER_NUMBER = 5;
+    public static final int LOTTO_PRICE = 1000;
 
 }
---
+++ src/main/java/lotto/util/InputVerifier.java
@@ -0,0 +1,48 @@
+package lotto.util;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lotto.constant.Constant;
+
+public class InputVerifier {
+
+    public boolean isInteger(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            char needVerify = input.charAt(i);
+
+            if (!Character.isDigit(needVerify)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isMultipleOfThousand(String input) {
+        int number = Integer.parseInt(input);
+        return number / Constant.LOTTO_PRICE == 0;
+    }
+
+    // TODO: 쉼표로 구분했는지 검증하는 기능 구현 필요
+
+    public boolean isValidSize(List<String> input) {
+        return input.size() == 6;
+    }
+
+    public boolean isValidNumber(List<String> input) {
+        for (String i : input) {
+            int number = Integer.parseInt(i);
+
+            if (number < Constant.LOTTO_START_NUMBER || number > Constant.LOTTO_END_NUMBER) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isUniqueNumber(List<String> input) {
+        Set<String> uniqueInput = new HashSet<>(input);
+        return uniqueInput.size() == input.size();
+    }
+
+}
