97dca89c09d52065eb5a1c43a6b2ac13e152f445
gyuturn <rbtjrrbals@gmail.com>
2022-11-09 23:57:50 +0900

    feat($util): 에러메세지 관리
    에러메세지를 상수화하여 관리 위함

---
+++ src/main/java/util/message/ErrorMessage.java
@@ -0,0 +1,8 @@
+package util.message;
+
+public class ErrorMessage {
+    public static final String PREFIX_ERROR = "[ERROR] ";
+
+    public static final String OVER_THOUSAND_MONEY = "로또 구매금액은 1000원이상이어야합니다.";
+    public static final String UNIT_THOUSAND_MONEY = "로또 구입금액은 1000원위의 금액만 입력가능합니다";
+}
