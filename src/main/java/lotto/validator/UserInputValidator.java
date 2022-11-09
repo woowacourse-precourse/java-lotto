b1d8ff4fd268f38691f249910a444ba036b97ecb
K-Diger <kitten.diger@gmail.com>
2022-11-09 21:10:58 +0900

    feat : validateInputLottoNumber 메서드 구현 - 하위 메서드인 validateLottoWinNumberLength, validateLottoNumberRange, validateLottoWinNumberOverlap 를 호출하여 보너스 로또 당첨 번호를 제외한 입력 값을 검증하는 메서드 완성

--- src/main/java/lotto/validator/UserInputValidator.java
+++ src/main/java/lotto/validator/UserInputValidator.java
@@ -9,6 +9,15 @@ public class UserInputValidator {
             throw new IllegalArgumentException();
         }
     }
+
+    public static void validateInputLottoNumber(String[] winNumber) {
+        validateLottoWinNumberLength(winNumber);
+        validateLottoWinNumberOverlap(winNumber);
+        for (String number : winNumber) {
+            validateLottoNumberRange(number);
+        }
+    }
+
     public static void validateLottoNumberRange(String winNumber) {
         for (int index = 0; index < winNumber.length(); index++) {
             if (Integer.parseInt(winNumber) < 1 || Integer.parseInt(winNumber) > 45) {
@@ -16,6 +25,7 @@ public class UserInputValidator {
             }
         }
     }
+
     public static void validateLottoWinNumberOverlap(String[] winNumber) {
         String temp = winNumber[0];
         for (int numberIndex = 1; numberIndex < winNumber.length; numberIndex++) {
@@ -24,11 +34,13 @@ public class UserInputValidator {
             }
         }
     }
+
     public static void validateLottoWinNumberLength(String[] winNumber) {
         if (winNumber.length > 6) {
             throw new IllegalArgumentException("[ERROR] 보너스 숫자를 포함하지 않은 로또 번호 갯수는 최대 6개입니다.");
         }
     }
+
     public static void validateLottoWinNumberLengthContainBonusNumber(String[] winNumber) {
         if (winNumber.length > 7) {
             throw new IllegalArgumentException("[ERROR] 보너스 숫자를 포함한 로또 번호 갯수는 최대 7개입니다.");
