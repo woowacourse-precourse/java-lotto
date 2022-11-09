77383fe595a09c2a32a83a48a9b3fcc4631aeba8
ksh-beginner <ohk9134@naver.com>
2022-11-10 01:16:51 +0900

    feat(InputView) : 로또 구입 금액을 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,13 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String INPUT_LOTTO_PURCHASE_AMOUNT_MESSAGE = "당첨 번호를 입력해 주세요.";
+
+    public String inputLottoPurchaseAmount() {
+        System.out.println(INPUT_LOTTO_PURCHASE_AMOUNT_MESSAGE);
+        return Console.readLine();
+    }
 }
