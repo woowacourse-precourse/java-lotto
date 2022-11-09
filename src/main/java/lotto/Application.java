bbb6838d153bb2e4c786a6afa2c4e89f4043b7b6
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-11-09 21:55:38 +0900

    구입 금액 입력
    숫자 외 다른 문자가 오는 경우 예외 처리
    1000원으로 나누어 떨어지지 않는 경우 예외 처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,42 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static int purchaseAmountInput(){
+        String user_input = Console.readLine();
+        int result;
+
+        purchaseAmount_exceptionHandling(user_input);
+        result=Integer.parseInt(user_input);
+
+        return result;
+    }
+
+    private static void purchaseAmount_exceptionHandling(String user_input){
+        if(!isNumber(user_input)&&!isDevidedThousand(user_input)){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isNumber(String user_input){
+        try{
+            Integer.parseInt(user_input);
+        } catch (NumberFormatException e){
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isDevidedThousand(String user_input_String) {
+        int user_input_int=Integer.parseInt(user_input_String);
+        if(user_input_int%1000!=0){
+            return false;
+        }
+        return true;
     }
 }
