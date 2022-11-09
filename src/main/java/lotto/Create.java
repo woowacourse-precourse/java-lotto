e8626f9a9b038979134ab1aa45b315a69d88ae78
H4R1B0 <qkrguswns25@naver.com>
2022-11-09 23:08:54 +0900

    feat: sort 6 numbers

--- src/main/java/lotto/Create.java
+++ src/main/java/lotto/Create.java
@@ -7,6 +7,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Create {
     public static List<Integer> lotto(){
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return sortLotto(numbers);
+    }
+    
+    public static List<Integer> sortLotto(List<Integer> numbers){
+        Collections.sort(numbers);
         return numbers;
     }
 }
