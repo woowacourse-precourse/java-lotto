1b053b67f92b96f9a90a994fe524d23dfde4fbf3
seokjin8678 <rnrn8678@naver.com>
2022-11-09 23:06:06 +0900

    feat: Game 클래스 추가

---
+++ src/main/java/lotto/game/Game.java
@@ -0,0 +1,15 @@
+package lotto.game;
+
+public class Game {
+    private Game() {
+
+    }
+
+    public static Game create() {
+        return new Game();
+    }
+
+    public void start() {
+
+    }
+}
