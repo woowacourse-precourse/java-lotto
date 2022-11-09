3199e6e3611ad813099287ad5ce53d07d84c152a
eunji53 <duqtj00@naver.com>
2022-11-10 00:14:36 +0900

    <feat>:로또 구입 금액 입력
    구입 금액 입력받고 int형으로 return

--- src/main/java/lotto/domain/PurchaseLotto.java
+++ src/main/java/lotto/domain/PurchaseLotto.java
@@ -1,15 +1,22 @@
 package lotto.domain;
 
-import java.lang.reflect.Array;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 public class PurchaseLotto {
-    public void inputMoney(){
-
+    public int inputMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
+        String moneyInput=readLine();
+        int money =Integer.valueOf(moneyInput);
+        return money;
     }
     public int countLotto(){
         return 0;
     }
-    public Array<Integer> lottoNumber(){
+    public List<Integer> lottoNumber(){
         return null;
     }
+
 }
