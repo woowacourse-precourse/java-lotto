61e2e493e0818978264b754fc5110b641343761e
heesang <dlagmltkd12@naver.com>
2022-11-10 03:58:19 +0900

    feat: 로또 번호가 중복 된 숫자가 있을 경우 예외 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,32 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
 
     public static final int CRITERION_LOTTO_SIZE = 6;
+    public static final String ERROR_NOT_VALID_NUMBER_SIZE = "[ERROR] 로또 번호의 개수는 6개 이어야 합니다.";
+    public static final String ERROR_NOT_VALID_NUMBER_DUPLICATION = "[ERROR] 로또 번호에는 중복 된 숫자가 없어야합니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
+        validateDuplication(numbers);
         this.numbers = numbers;
     }
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != CRITERION_LOTTO_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호의 개수는 6개 이어야 합니다.");
+            throw new IllegalArgumentException(ERROR_NOT_VALID_NUMBER_SIZE);
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        Set<Integer> removeDuplication = new HashSet<>(numbers);
+        if (removeDuplication.size() != CRITERION_LOTTO_SIZE) {
+            throw new IllegalArgumentException(ERROR_NOT_VALID_NUMBER_DUPLICATION);
         }
     }
 
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -17,13 +17,12 @@ class LottoTest {
                 .hasMessage("[ERROR] 로또 번호의 개수는 6개 이어야 합니다.");
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외 발생")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호에는 중복 된 숫자가 없어야합니다.");
     }
 
-    // 아래에 추가 테스트 작성 가능
 }
