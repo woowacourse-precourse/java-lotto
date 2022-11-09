3bb33548e25ccb1165f469fd6e604f8459e9efc1
ym2629 <dydals2629@naver.com>
2022-11-09 12:49:55 +0900

    feat: print lottos

--- src/main/java/lotto/UserInterface.java
+++ src/main/java/lotto/UserInterface.java
@@ -2,9 +2,16 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.*;
 
+import java.util.List;
+
 public class UserInterface {
     int getPurchasePrice(){
         System.out.println("구입금액을 입력해 주세요.");
         return Integer.parseInt(Console.readLine());
+    }
+
+    void printLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        lottos.forEach(System.out::println);
     }
 }
