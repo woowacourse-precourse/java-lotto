1364d6ca1a49327b219f699d24326bd1a9d4c303
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-09 19:16:48 +0900

    feat(LottoController): 컨트롤러생성 및 view를 통해 유저 구입금액 받기

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,24 @@
+package lotto.controller;
+
+import lotto.Utils.InputValid;
+import lotto.model.User;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private User user;
+
+    public LottoController() {
+        user=new User();
+    }
+
+    public void startLotto(){
+        inIt();
+
+    }
+
+    private void inIt(){
+        user.setUserPrice(InputView.getUserPriceNumber());
+
+    }
+}
