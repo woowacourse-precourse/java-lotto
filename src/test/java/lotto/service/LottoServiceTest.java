4ed9172792cb9d4b07ba2d749b51b119a4c740fc
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-10 00:59:39 +0900

    feature(test) : 당첨 결과 테스트 추가

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.IntStream;
 import lotto.Lotto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -50,6 +51,61 @@ class LottoServiceTest {
         IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class,
             () -> service.buy(money));
         assertThat(e.getMessage()).contains("[ERROR]");
+    }
+
+    private static Collection<Arguments> param3() {
+        return Arrays.asList(
+            Arguments.of("1등이 한번 등장",
+                Arrays.asList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6))),
+                Arrays.asList(1, 2, 3, 4, 5, 6), 7, Arrays.asList(1, 0, 0, 0, 0)),
+            Arguments.of("1등이 2번 등장",
+                Arrays.asList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                    new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6))),
+                Arrays.asList(1, 2, 3, 4, 5, 6), 7, Arrays.asList(2, 0, 0, 0, 0)),
+            Arguments.of("1,5등 한번씩 등장",
+                Arrays.asList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                    new Lotto(Arrays.asList(1, 2, 3, 24, 25, 26))),
+                Arrays.asList(1, 2, 3, 4, 5, 6), 7, Arrays.asList(1, 0, 0, 0, 1)),
+            Arguments.of("2,3등 빼고 한번씩 등장",
+                Arrays.asList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                    new Lotto(Arrays.asList(1, 2, 3, 24, 25, 26)),
+                    new Lotto(Arrays.asList(1, 2, 3, 4, 25, 26))),
+                Arrays.asList(1, 2, 3, 4, 5, 6), 7, Arrays.asList(1, 0, 0, 1, 1)),
+            Arguments.of("2등 빼고 한번씩 등장",
+                Arrays.asList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                    new Lotto(Arrays.asList(1, 2, 3, 24, 25, 26)),
+                    new Lotto(Arrays.asList(1, 2, 3, 4, 25, 26)),
+                    new Lotto(Arrays.asList(1, 2, 3, 4, 5, 26))),
+                Arrays.asList(1, 2, 3, 4, 5, 6), 7, Arrays.asList(1, 0, 1, 1, 1)),
+            Arguments.of("1등부터 5등까지 한번씩 등장",
+                Arrays.asList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                    new Lotto(Arrays.asList(1, 2, 3, 24, 25, 26)),
+                    new Lotto(Arrays.asList(1, 2, 3, 4, 25, 26)),
+                    new Lotto(Arrays.asList(1, 2, 3, 4, 5, 26)),
+                    new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7))),
+                Arrays.asList(1, 2, 3, 4, 5, 6), 7, Arrays.asList(1, 1, 1, 1, 1)),
+            Arguments.of("보너스만 맞춘건 노카운팅",
+                Arrays.asList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                    new Lotto(Arrays.asList(21, 22, 23, 24, 25, 7))),
+                Arrays.asList(1, 2, 3, 4, 5, 6), 7, Arrays.asList(1, 0, 0, 0, 0)),
+            Arguments.of("보너스 한개와 당첨 숫자 1개를 맞춘건 노카운팅",
+                Arrays.asList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                    new Lotto(Arrays.asList(21, 22, 23, 24, 5, 7))),
+                Arrays.asList(1, 2, 3, 4, 5, 6), 7, Arrays.asList(1, 0, 0, 0, 0)),
+            Arguments.of("보너스 한개와 당첨 숫자 2개를 맞춘건 노카운팅",
+                Arrays.asList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                    new Lotto(Arrays.asList(21, 22, 23, 4, 5, 7))),
+                Arrays.asList(1, 2, 3, 4, 5, 6), 7, Arrays.asList(1, 0, 0, 0, 0))
+        );
+    }
+
+    @ParameterizedTest(name = "{index}: {0}")
+    @MethodSource("param3")
+    @DisplayName("구매한 로또 목록, 당첨 번호, 보너스 번호를 받아 결과를 리턴하는지 테스트")
+    void test3(String description, List<Lotto> lottos, List<Integer> win, int bonus,
+        List<Integer> result) {
+        List<Integer> a = service.getResult(lottos, win, bonus);
+        IntStream.range(0, 5).forEach(i -> Assertions.assertEquals(result.get(i), a.get(i)));
     }
 
 }
