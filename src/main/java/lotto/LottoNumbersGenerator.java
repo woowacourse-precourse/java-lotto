8344dd5f4583a26c5395c7dd8ad1a61617812102
ksh-beginner <ohk9134@naver.com>
2022-11-09 23:33:14 +0900

    rename : LottoWinningNumbersGenerator -> LottoNumbersGenerator로 네이밍 수정
    로또 당첨 번호를 생성하는 것이 아니라,
    로또 번호를 생성하는 것이므로 네이밍 수정

--- src/main/java/lotto/LottoNumbersGenerator.java
+++ src/main/java/lotto/LottoNumbersGenerator.java
@@ -4,9 +4,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
 
-public class LottoWinningNumbersGenerator {
+public class LottoNumbersGenerator {
 
-    public static List<Integer> generateLottoWinningNumbers() {
+    public static List<Integer> generateLottoNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }
--- src/test/java/lotto/LottoNumbersGeneratorTest.java
+++ src/test/java/lotto/LottoNumbersGeneratorTest.java
@@ -1,18 +1,17 @@
 package lotto;
 
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
-class LottoWinningNumbersGeneratorTest {
+class LottoNumbersGeneratorTest {
 
     @Test
     void 로또_당첨_번호_생성() {
-        List<Integer> lottoWinningNumbers = LottoWinningNumbersGenerator.generateLottoWinningNumbers();
+        List<Integer> lottoWinningNumbers = LottoNumbersGenerator.generateLottoNumbers();
 
         assertThat(lottoWinningNumbers).isNotEmpty();
     }
