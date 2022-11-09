7bd7538aa0bf62aa484555539c0a8582c8085de3
gyuturn <rbtjrrbals@gmail.com>
2022-11-09 23:10:13 +0900

    style($exception): 로또 구매 금액 기준인 1000원 상수화
    하드코딩을 피해 가독성을 높이기위함
    
    1000->PURCHASE_MONEY_UNIT

--- src/main/java/exception/Valid.java
+++ src/main/java/exception/Valid.java
@@ -1,16 +1,17 @@
 package exception;
 
 public class Valid {
+    private static final int PURCHASE_MONEY_UNIT = 1000;
 
     public static boolean isMoneyThousandUnit(int money) {
-        if (money % 1000 != 0) {
+        if (money % PURCHASE_MONEY_UNIT != 0) {
             return false;
         }
         return true;
     }
 
     public static boolean isMoneyOverThousand(int money){
-        if (money < 1000) {
+        if (money < PURCHASE_MONEY_UNIT) {
             return false;
         }
         return true;
