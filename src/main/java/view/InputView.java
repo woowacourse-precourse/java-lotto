d6c8ddd26ea99dfabcbd26dabc867f656f0ffd38
yeonhee <ydol2@naver.com>
2022-11-10 07:33:00 +0900

    feat(InputView) : 입력을 위한 view 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,39 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static constant.MessageConstant.INPUT_LOTTO_NUMBERS;
+import static constant.MessageConstant.INPUT_PRICE;
+import static constant.MessageConstant.INPUT_BONUS_NUMBER;
+
+public enum InputView {
+    INSTANCE;
+
+    public int inputPrice(){
+        System.out.println(INPUT_PRICE);
+        int price = Integer.parseInt(Console.readLine());
+        return price;
+    }
+
+    public List<Integer> inputLottoNumbers(){
+        System.out.println(INPUT_LOTTO_NUMBERS);
+        String lottoNumbers = Console.readLine();
+        return splitNumbers(lottoNumbers);
+    }
+
+    public int inputBonusNumber(){
+        System.out.println(INPUT_BONUS_NUMBER);
+        int bonus = Integer.parseInt(Console.readLine());
+        return bonus;
+    }
+
+    private List<Integer> splitNumbers(final String numbersText) {
+        return Arrays.stream(numbersText.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+}
