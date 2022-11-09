65155297b49eb0aab961fc6d799be0fba9c7d28b
gyuturn <rbtjrrbals@gmail.com>
2022-11-09 22:58:26 +0900

    style($view): 변수명 변경
    purchaseAmount->purchaseMoney

--- src/main/java/view/PrintView.java
+++ src/main/java/view/PrintView.java
@@ -1,9 +1,9 @@
 package view;
 
 public class PrintView {
-    private static final String MESSAGE_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String MESSAGE_PURCHASE_MONEY = "구입금액을 입력해 주세요.";
 
-    public static void purchaseAmount() {
-        System.out.println(MESSAGE_PURCHASE_AMOUNT);
+    public static void purchaseMoney() {
+        System.out.println(MESSAGE_PURCHASE_MONEY);
     }
 }
