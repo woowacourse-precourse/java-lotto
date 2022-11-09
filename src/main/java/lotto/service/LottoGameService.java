11da8d5ee08f8287afd42cbb9df982f4997a5e23
ksh-beginner <ohk9134@naver.com>
2022-11-10 02:22:22 +0900

    refactor(LottoGameService) : 로또 구입 금액 파라미터 String으로 리팩토링
    InputView에서 들어오는 로또 구입 금액은 String이므로
    파라미터는 String으로 받고 LottoGameService에서 int로 변환하도록 리팩토링

--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -6,18 +6,18 @@ public class LottoGameService {
     private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
     private static final String LOTTO_PURCHASE_AMOUNT_DIVIDE_REST_EXCEPTION_MESSAGE = "로또 구입 금액은 1,000원으로 나누어 떨어져야합니다.";
 
-    public int getLottoIssueCount(int lottoPurchaseAmount) {
+    public int getLottoIssueCount(String lottoPurchaseAmount) {
         validateLottoIssueCount(lottoPurchaseAmount);
-        return lottoPurchaseAmount / A_LOTTO_PRICE;
+        return Integer.parseInt(lottoPurchaseAmount) / A_LOTTO_PRICE;
     }
 
-    public void validateLottoIssueCount(int lottoPurchaseAmount) {
+    public void validateLottoIssueCount(String lottoPurchaseAmount) {
         if (isRestOfLottoPurchaseAmountDivideBy1000NotZero(lottoPurchaseAmount)) {
             throw new IllegalArgumentException(ERROR_MESSAGE_PREFIX + LOTTO_PURCHASE_AMOUNT_DIVIDE_REST_EXCEPTION_MESSAGE);
         }
     }
 
-    private boolean isRestOfLottoPurchaseAmountDivideBy1000NotZero(int lottoPurchaseAmount) {
-        return lottoPurchaseAmount % A_LOTTO_PRICE != 0;
+    private boolean isRestOfLottoPurchaseAmountDivideBy1000NotZero(String lottoPurchaseAmount) {
+        return Integer.parseInt(lottoPurchaseAmount) % A_LOTTO_PRICE != 0;
     }
 }
