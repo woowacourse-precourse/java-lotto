029f15d963d78b137aa862174a957cc34e4612d3
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-09 18:17:49 +0900

    feat(lotto):  로또 구입 금액 입력 파트 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import domain.UserMoney;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        int money = UserMoney.inputMoney();
+
+
     }
+
+
 }
