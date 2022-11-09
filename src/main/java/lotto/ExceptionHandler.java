dd9089f907c069c729a22c880bd266b64b3e08fa
yj2980 <gum0945@naver.com>
2022-11-10 00:34:50 +0900

    feat : 예외 처리하는 함수 추가

--- src/main/java/lotto/ExceptionHandler.java
+++ src/main/java/lotto/ExceptionHandler.java
@@ -1,2 +1,9 @@
-package lotto;public class ExceptionHandler {
+package lotto;
+
+public class ExceptionHandler {
+
+	public static void checkMoney(int money) {
+		if ((money % 1000) != 0)
+			throw new IllegalArgumentException("ERROR");
+	}
 }
