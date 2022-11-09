122d77c67cddd81573a34c9babdd07f38c591f30
yeonhee <ydol2@naver.com>
2022-11-09 18:36:19 +0900

    feat(NumberConstant) : 고정 상수 분리하여 구현

---
+++ src/main/java/constant/NumberConstant.java
@@ -0,0 +1,10 @@
+package constant;
+
+public class NumberConstant {
+
+    public static final int LOTTO_INPUT_LENGTH = 6;
+    public static final int LOTTO_BONUS_INPUT_LENGTH = 1;
+    public static final int LOTTO_MIN_VALUE = 1;
+    public static final int LOTTO_MAX_VALUE = 45;
+
+}
