e97b32b90d8f2db025359e3cdbb0082011cba85d
ym2629 <dydals2629@naver.com>
2022-11-09 13:07:29 +0900

    feat: get winnig number by user input

--- src/main/java/lotto/UserInterface.java
+++ src/main/java/lotto/UserInterface.java
@@ -2,7 +2,9 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.*;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserInterface {
     int getPurchasePrice(){
@@ -13,5 +15,19 @@ public class UserInterface {
     void printLottos(List<Lotto> lottos) {
         System.out.println(lottos.size() + "개를 구매했습니다.");
         lottos.forEach(System.out::println);
+    }
+
+    List<Integer> getWinningNumber(){
+        System.out.println("구입금액을 입력해 주세요.");
+        String userInput = Console.readLine();
+        List<Integer> winningNumbers = Arrays.stream(userInput.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        System.out.println("보너스 볼을 입력해 주세요.");
+        int bonus = Integer.parseInt(Console.readLine());
+        winningNumbers.add(bonus);
+
+        return winningNumbers;
     }
 }
