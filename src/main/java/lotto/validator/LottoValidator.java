401f6b91cfbb77ea67a83e6a029e46d19622ae48
whdcks420 <whdcks420@gmail.com>
2022-11-09 19:48:33 +0900

    feat(validate): 숫자 중복확인 기능

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -1,2 +1,25 @@
-package lotto.validator;public class LottoValidator {
+package lotto.validator;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class LottoValidator {
+    private static final int NUMBER_OF_LOTTO_NUMBERS = 6;
+
+    public static void validate(List<Integer> numbers) {
+        if (!isExactSize(numbers)) {
+            throw new IllegalArgumentException();
+        }
+        if (isOverlap(numbers)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isExactSize(List<Integer> numbers) {
+        return numbers.size() == NUMBER_OF_LOTTO_NUMBERS;
+    }
+
+    private static boolean isOverlap(List<Integer> numbers) {
+        return new HashSet<>(numbers).size() != NUMBER_OF_LOTTO_NUMBERS;
+    }
 }
