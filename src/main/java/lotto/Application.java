77cc44cf7a62b528469f68b25f8630561c294a4a
Eungyu-code <Agyu625@gmail.com>
2022-11-09 19:58:57 +0900

    feat(Application class): printLottoNumbers 함수 추가
    발행된 로또 번호를 출력하는 함수
    - "[n1, n2, n3, n4, n5, n6]" 문구 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -8,5 +10,16 @@ public class Application {
     public static void printLottoQuantity(int quantity) {
 
         System.out.println(quantity + "개를 구매했습니다.");
+    }
+
+    public static void printLottoNumbers(List<Integer> lottoNumberList) {
+
+        System.out.print("[");
+
+        for (int i = 0; i < lottoNumberList.size() - 1; i++) {
+            System.out.print(lottoNumberList.get(i) + ", ");
+        }
+
+        System.out.println(lottoNumberList.get(lottoNumberList.size() - 1) + "]");
     }
 }
