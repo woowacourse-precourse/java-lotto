5513b147754a0fe1aeab7a14579fba97a73d848e
ksh-beginner <ohk9134@naver.com>
2022-11-10 02:00:56 +0900

    feat(LottoGameService) : 로또 구입 금액이 1000원으로 나누어 떨어지지 않는 경우 검증 기능 구현

--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -2,7 +2,22 @@ package lotto.service;
 
 public class LottoGameService {
 
+    private static final int A_LOTTO_PRICE = 1000;
+    private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
+    private static final String LOTTO_PURCHASE_AMOUNT_DIVIDE_REST_EXCEPTION_MESSAGE = "로또 구입 금액은 1,000원으로 나누어 떨어져야합니다.";
+
     public int getLottoIssueCount(int lottoPurchaseAmount) {
-        return lottoPurchaseAmount / 1000;
+        validateLottoIssueCount(lottoPurchaseAmount);
+        return lottoPurchaseAmount / A_LOTTO_PRICE;
+    }
+
+    public void validateLottoIssueCount(int lottoPurchaseAmount) {
+        if (isRestOfLottoPurchaseAmountDivideBy1000NotZero(lottoPurchaseAmount)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_PREFIX + LOTTO_PURCHASE_AMOUNT_DIVIDE_REST_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private boolean isRestOfLottoPurchaseAmountDivideBy1000NotZero(int lottoPurchaseAmount) {
+        return lottoPurchaseAmount % A_LOTTO_PRICE != 0;
     }
 }
