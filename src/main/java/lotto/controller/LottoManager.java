2e4cbb540832718815aeca65741625e04947099c
ghchoi <ghchoi0427@gmail.com>
2022-11-09 20:59:03 +0900

    feat: LottoManager 생성

---
+++ src/main/java/lotto/controller/LottoManager.java
@@ -0,0 +1,31 @@
+package lotto.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoManager {
+
+    private final int lottoPrice = 1000;
+
+    public void run() {
+
+    }
+
+    private int buyLotto(int money) {
+        return money / lottoPrice;
+    }
+
+    private List<List<Integer>> pickNumbers(int lottoCount) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        return result;
+    }
+
+    private void calculateWins() {
+
+    }
+
+    private void calculateRatio() {
+
+    }
+}
