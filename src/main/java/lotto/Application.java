8f38199752dbf95a2e2333d769021f3e3bd15dcf
krkr5628 <krkr5628@gmail.com>
2022-11-09 21:29:56 +0900

    v1.0 - 금액 입력 부분 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구매금액을 입력해 주세요.");
+        String input_price = Console.readLine();
+        int price = Integer.parseInt(input_price);
+        // 입력값 검수
+        int buy_payper = price/1000;
+        System.out.println(buy_payper + "개를 구매했습니다.");
+        //Lotto.computer 에서 램덤수 6세트 받기
     }
 }
