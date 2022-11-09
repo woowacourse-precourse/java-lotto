2a7cc354c4605e860f43ef3ed04d975a7dfef7a3
H4R1B0 <qkrguswns25@naver.com>
2022-11-09 23:33:15 +0900

    feat: check that numbers are between 1 and 45

--- src/main/java/lotto/Check.java
+++ src/main/java/lotto/Check.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import java.util.List;
+
 public class Check {
     public static boolean isMultipleOf1000(int number) {
         return number % 1000 == 0;
+    }
+
+    public static boolean isNumbersBetween1And45(List<Integer> numbers) throws IllegalArgumentException {
+        for (int number : numbers) {
+            if (number < 1 || number > 45)
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        return true;
     }
 }
