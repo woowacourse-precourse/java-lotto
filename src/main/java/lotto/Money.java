2225d7cfa0ae986bd33ac4265c12249a177f9e43
소대훈 <theogns0681@naver.com>
2022-11-09 16:21:48 +0900

    [refactor] (Money) 문자열 수정

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -1,11 +1,11 @@
 package lotto;
 
 public enum Money {
-    first("(2,000,000,000원)"),
-    second("(30,000,000원)"),
-    third("(1,500,000원)"),
-    fourth("(50,000원)"),
-    fifth("(5,000원)");
+    first("6개 일치 (2,000,000,000원) - "),
+    second("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    third("5개 일치 (1,500,000원) - "),
+    fourth("4개 일치 (50,000원) - "),
+    fifth("3개 일치 (5,000원) - ");
 
     private final String money;
 
