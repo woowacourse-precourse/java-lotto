166619120bbc9934ae1b046bf033a2c7501ccdee
sangwon <swchoi1997@naver.com>
2022-11-10 08:58:16 +0900

    add: 에러 메세지 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -3,7 +3,10 @@ package lotto;
 public enum ErrorMessage {
 
     COMMON_MESSAGE("[ERROR] "),
-    LOTTE_SIZE_ERROR("Lotto Number Length Is Incorrect. SIZE : ");
+    LOTTE_SIZE_INVALID("Lotto Number Length Is Incorrect. SIZE : "),
+    LOTTE_NUMBER_DUPLICATION("Lotto Number Have Duplicate Number."),
+    LOTTE_NUMBER_OUT_BOUND("Lotto Number Have Out Bound Number.");
+
 
     private final String message;
 
