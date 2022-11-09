60d4325c076175f1d55a1328c99ff99642a1bfd3
cpot5620 <=cpot5620@naver.com>
2022-11-10 00:41:26 +0900

    test(LottoStoreTest): add test case

--- src/test/java/lotto/LottoStoreTest.java
+++ src/test/java/lotto/LottoStoreTest.java
@@ -4,6 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 class LottoStoreTest {
 
     LottoStore lottoStore = new LottoStore();
@@ -20,5 +24,16 @@ class LottoStoreTest {
     void buyLottoByNoMoney() {
         Assertions.assertThatThrownBy(() -> lottoStore.buyLottoNumber(0))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("2000원 지불시, 로또 2장 구매 메세지가 출력된다.")
+    @Test
+    void buyLottoBy2000(){
+        OutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        lottoStore.buyLottoNumber(2000);
+
+        Assertions.assertThat(outputStream.toString()).contains("2개를 구매했습니다.");
     }
 }
