877b22a8dbae05c4934374e4cb6ccdb12e2531a5
5uhwann <5uhwann27@gmail.com>
2022-11-10 01:51:49 +0900

    feat(검증): 로또 구입 금액 최솟값 검증 구현
    로또 구입 금액의 최솟값은 1000 이상이어야 한다.

--- src/main/java/lotto/view/input/InputImpl.java
+++ src/main/java/lotto/view/input/InputImpl.java
@@ -4,9 +4,21 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputImpl implements Input {
 
+    private final int MIN_PAYMENT_AMOUNT = 1000;
+
     @Override
     public int paymentAmountInput() {
         int paymentAmount = Integer.parseInt(Console.readLine());
-        return paymentAmount;
+        if (isAllowedPaymentAmount(paymentAmount)) {
+            return paymentAmount;
+        }
+        return 0;
+    }
+
+    private boolean isAllowedPaymentAmount(int paymentAmount) {
+        if (paymentAmount < MIN_PAYMENT_AMOUNT) {
+            throw new IllegalArgumentException("[ERROR] 최소 로또 구매 금액은 1000원입니다.");
+        }
+        return true;
     }
 }
