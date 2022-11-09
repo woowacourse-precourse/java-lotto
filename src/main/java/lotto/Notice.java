2ab6f0464687d397f9a6ae7785254d176071f4c9
yj2980 <gum0945@naver.com>
2022-11-10 01:05:00 +0900

    feat : 변수 추가

--- src/main/java/lotto/Notice.java
+++ src/main/java/lotto/Notice.java
@@ -5,7 +5,8 @@ public enum Notice {
 	purchase("개를 구매했습니다."),
 	winningNumbers("당첨 번호를 입력해 주세요."),
 	bonusNumber("보너스 번호를 입력해 주세요."),
-	statistics("당첨 통계\n---");
+	statistics("당첨 통계\n---"),
+	profit("총 수익률은 ");
 
 	private String noticeMessage;
 
