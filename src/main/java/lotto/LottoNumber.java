0996e732ca9ff1384beff3548dbaa31cbd754cfe
JJ <jeongjae1996@naver.com>
2022-11-10 06:47:52 +0900

    feat: 발행 받은 로또 개수를 형식에 맞게 출력하도록 구현
    "${number}개를 구매했습니다."이 출력됩니다.

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -6,12 +6,19 @@ public class LottoNumber {
     public LottoNumber() {
         Money money = new Money();
         this.number = computeLottoNumber(money.getMoney());
+        printGuideMessage();
     }
 
     private int computeLottoNumber(int money) {
         final int LOTTO_SELLING_PRICE = 1000;
 
         return money / LOTTO_SELLING_PRICE;
+    }
+
+    private void printGuideMessage() {
+        final String GUIDE_MESSAGE = "개를 구매했습니다.";
+
+        System.out.println(this.number + GUIDE_MESSAGE);
     }
 
     public int getLottoNumber() {
