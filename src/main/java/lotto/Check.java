0b2035b9dca409218e9bab5f97db1d4b6017c622
H4R1B0 <qkrguswns25@naver.com>
2022-11-09 23:51:47 +0900

    chore: rename methods

--- src/main/java/lotto/Check.java
+++ src/main/java/lotto/Check.java
@@ -10,7 +10,7 @@ public class Check {
         return true;
     }
 
-    public static boolean isNumbersBetween1And45(List<Integer> numbers) throws IllegalArgumentException {
+    public static boolean isLottoBetween1And45(List<Integer> numbers) throws IllegalArgumentException {
         for (int number : numbers) {
             if (number < 1 || number > 45)
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
@@ -18,7 +18,7 @@ public class Check {
         return true;
     }
 
-    public static boolean isNumbersDuplicate(List<Integer> numbers) throws IllegalArgumentException {
+    public static boolean isLottoDuplicate(List<Integer> numbers) throws IllegalArgumentException {
         HashSet<Integer> set = new HashSet<>(numbers);
         if (set.size() != 6)
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않은 6개 숫자여야 합니다.");
