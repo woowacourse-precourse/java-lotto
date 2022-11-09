f971a992687223036e57c8663597cd6d4bb07b8c
chlwodyd9829 <chlwodyd9829@naver.com>
2022-11-09 16:54:41 +0900

    refactor: 검증부분 분리로 인한 코드 수정

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -11,13 +11,11 @@ public class User {
     private final int startNumber = 1;
     private final int endNumber = 45;
     private final int count = 6;
-    private int money;
+    private Integer money;
     private List<List<Integer>> myLottoNumbers;
 
-    public User(String money) {
-        validate(money);
-        this.money = Integer.parseInt(money);
-        myLottoNumbers = new ArrayList<>();
+    public User(Integer money) {
+        this.money = money;
         makeMyLottoNumbers();
     }
 
@@ -30,37 +28,12 @@ public class User {
     }
 
     private void makeMyLottoNumbers(){
+        myLottoNumbers = new ArrayList<>();
         int times = this.money / 1000;
         for (int i = 0; i < times; i++){
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(startNumber, endNumber, count);
             Collections.sort(numbers);
             myLottoNumbers.add(numbers);
         }
-    }
-
-    private void validate(String money) {
-        if (!isMoney(money)){
-            throw new IllegalArgumentException();
-        }
-        if (Integer.parseInt(money) % 1000 != 0) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private boolean isMoney(String money){
-        char[] chars = money.toCharArray();
-        for (char c : chars) {
-            if(!isDigit(c)){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean isDigit(char c){
-        if (c < '0' && c > '9'){
-            return false;
-        }
-        return true;
     }
 }
