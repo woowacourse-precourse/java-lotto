57c65738b983a03bb088002c0d011933a9ba2413
JJ <jeongjae1996@naver.com>
2022-11-10 07:43:35 +0900

    feat: 외부 클래스에서 Lotto 클래스의 인스턴스 변수 numbers에 접근할 수 있도록 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,14 @@ package lotto;
 
 import java.util.List;
 
+/*
+- 제공된 Lotto 클래스를 활용해 구현해야 한다.
+- Lotto에 매개 변수가 없는 생성자를 추가할 수 없다.
+- numbers의 접근 제어자인 private을 변경할 수 없다.
+- Lotto에 필드(인스턴스 변수)를 추가할 수 없다.
+- Lotto의 패키지 변경은 가능하다.
+*/
+
 public class Lotto {
     private final List<Integer> numbers;
 
@@ -17,4 +25,7 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
 }
