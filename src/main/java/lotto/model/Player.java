a3572ddebfe0b85b0bb8601cc3e4528a442bac8f
jinny-l <jinny.l.dev@gmail.com>
2022-11-09 18:31:00 +0900

    feat(Player): player의 input을 받는 기능 구현

---
+++ src/main/java/lotto/model/Player.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Player {
+
+    public int getInput() {
+        return Integer.parseInt(input());
+    }
+
+    public List<String> getLottoNumbers() {
+        String numbers = input();
+
+        return new ArrayList<>(Arrays.asList(numbers.split("\\s*,\\s*")));
+    }
+
+    private static String input() {
+        return Console.readLine();
+    }
+}
