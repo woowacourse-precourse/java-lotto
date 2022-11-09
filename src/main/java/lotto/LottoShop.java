bbdfa302637cc83ca914e7acb3a8767c16c4ac4a
cmong0516 <cmong0516@gmail.com>
2022-11-09 18:12:55 +0900

    feat(LottoShopClass): canBuyAmount()
    Customer 가 입력한 구매를 원하는 금액으로 구입할수 있는 로또의 수를 리턴해주는 메서드

--- src/main/java/lotto/LottoShop.java
+++ src/main/java/lotto/LottoShop.java
@@ -4,4 +4,8 @@ public class LottoShop {
     public void requestWantBuyAmountInputMessage() {
         System.out.println("구입금액을 입력해 주세요.");
     }
+
+    public int canBuyAmount(int wantBuyAmountInput) {
+        return wantBuyAmountInput / 1000;
+    }
 }
