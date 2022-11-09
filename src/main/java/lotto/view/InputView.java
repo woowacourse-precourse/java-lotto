87f1418c9e6e72c2e76c7265cca99dce3dd9fac3
ghchoi <ghchoi0427@gmail.com>
2022-11-09 20:56:11 +0900

    feat: InputView 생성

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputView {
+
+    public int inputNumber() {
+        return 0;
+    }
+
+    public List<Integer> inputNumbers() {
+        List<Integer> result = new ArrayList<>();
+        return result;
+    }
+}
