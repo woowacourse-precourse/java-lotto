5bd251e0e65dd7cf89ba06e49e593e71555d4481
saewoo1 <wooozooo1123@gmail.com>
2022-11-09 17:22:06 +0900

    feat(Money): checkMoneyIsDigit, checkDividableMoney를 한번에 검사하는 메소드 작성

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -7,6 +7,8 @@ public class Money {
     return Console.readLine();
   }
 
+  //예외 발생은 어디서 시켜야 할까..
+  // 두 예외를 한번에 검사하는 곳에서 하는게 맞지 않을까
   public boolean checkMoneyIsDigit() {
     String userMoney = inputMoney();
 
@@ -25,5 +27,11 @@ public class Money {
     if (money % 1000 != 0)
         throw new IllegalArgumentException(); // 추후 에러 내용을 출력하는 메세지 추가
     return true;
+  }
+
+  public boolean isValidMoney() {
+    if (checkDividableMoney() && checkMoneyIsDigit())
+      return true;
+    throw new IllegalArgumentException();
   }
 }
---
+++ src/test/java/lotto/MoneyTest.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoneyTest {
+
+  @Test
+  void checkMoneyIsDigit() {
+  }
+
+  @Test
+  void checkDividableMoney() {
+  }
+
+  @Test
+  void isValidMoney() {
+
+  }
+}
