5af09d0535177ab6ad51f16a80459f1a9e2325cf
yg0585 <yg0585@naver.com>
2022-11-09 19:04:52 +0900

    fix: 자릿 수 및 범위 변경

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -12,8 +12,8 @@ public class LottoGenerator {
     public List<Integer> createRandomLotto() {
         List<Integer> numbers = new ArrayList<>();
 
-        while (numbers.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (numbers.size() < LOTTO_MAX_LEN) {
+            int randomNumber = Randoms.pickNumberInRange(1, 45);
             addNum(numbers, randomNumber);
         }
 
