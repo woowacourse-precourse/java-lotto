623b16f45792d85bd059361b6abdc6bc4f3b517b
cmong0516 <cmong0516@gmail.com>
2022-11-09 18:01:44 +0900

    feat(CustomerClass): validWantBuyAmountInput()
    Customer 가 입력한 구매를 원하는 금액을 검증하는 메서드

--- src/main/java/lotto/Customer.java
+++ src/main/java/lotto/Customer.java
@@ -6,5 +6,11 @@ public class Customer {
     public int wantBuyAmountInput() {
         return Integer.parseInt(readLine());
     }
+
+    public void validWnatByAmountInput(int wantBuyAmount) {
+        if (wantBuyAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000 이하의 단위는 입력 불가능 합니다.");
+        }
+    }
 }
 
