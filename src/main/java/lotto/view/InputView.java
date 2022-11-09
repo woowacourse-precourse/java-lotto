5814efbc9f4075b8535bdc51f34f96c9dd270fd1
ksh-beginner <ohk9134@naver.com>
2022-11-10 01:28:27 +0900

    feat(InputView) : 로또 보너스 번호 입력하는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
 
     private static final String INPUT_LOTTO_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해주세요.";
     private static final String INPUT_LOTTO_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_LOTTO_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public String inputLottoPurchaseAmount() {
         System.out.println(INPUT_LOTTO_PURCHASE_AMOUNT_MESSAGE);
@@ -14,6 +15,11 @@ public class InputView {
 
     public String inputLottoWinningNumbers() {
         System.out.println(INPUT_LOTTO_WINNING_NUMBERS_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String inputLottoBonusNumber() {
+        System.out.println(INPUT_LOTTO_BONUS_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }
