2a876dd8d2b6da3bed44dcbcb3d6b24fd9ad9b72
sangwon <swchoi1997@naver.com>
2022-11-10 01:19:31 +0900

    feat: 로또번호 길이 체크
    로또번호 길이가 기준에 맞지 않을 시 예외 처리해주는 메서드 구현

---
+++ src/main/java/lotto/LottoValidator.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import static lotto.ErrorMessage.*;
+import static lotto.LottoGenerator.END_INCLUSIVE;
+import static lotto.LottoGenerator.START_INCLUSIVE;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoValidator {
+
+    private static final List<Integer> STANDARD_LOTTO_NUMBER = IntStream.rangeClosed(START_INCLUSIVE, END_INCLUSIVE)
+            .boxed().collect(Collectors.toList());
+
+    private LottoValidator(){}
+
+    public static void validate(List<Integer> numbers){
+        checkSize(numbers);
+    }
+
+    private static void checkSize(List<Integer> numbers) throws IllegalArgumentException {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(LOTTE_SIZE_ERROR.getMessage() + numbers.size());
+        }
+    }
+}
