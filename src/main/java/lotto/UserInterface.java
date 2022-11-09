f35495c51375be95ec032165f986b0f53395c9e9
ym2629 <dydals2629@naver.com>
2022-11-09 13:14:57 +0900

    feat: print result of rank

--- src/main/java/lotto/UserInterface.java
+++ src/main/java/lotto/UserInterface.java
@@ -30,4 +30,14 @@ public class UserInterface {
 
         return winningNumbers;
     }
+
+    void printResultRank(int first, int second, int thirth, int fourth, int fifth) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - "+fifth+"개");
+        System.out.println("4개 일치 (50,000원) - "+fourth+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+thirth+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+second+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+first+"개");
+    }
 }
