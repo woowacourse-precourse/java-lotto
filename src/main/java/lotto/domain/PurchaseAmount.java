f4c1da8c448e4ef10f94f15fb9f6b6abd6d95b25
Reznov <reznov03048@gmail.com>
2022-11-09 21:01:38 +0900

    feat: 최소 금액 이상을 입력했는지 확인하는 기능 구현

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -8,13 +8,20 @@ public class PurchaseAmount {
     private final int UNIT_PRICE = 1000;
 
     private final String ERROR_INDIVISIBLE = "[ERROR] " + UNIT_PRICE + "원으로 나누어떨어지는 금액만 입력해주십시오.";
+    private final String ERROR_MIN_PRICE = "[ERROR] " + UNIT_PRICE + "원 이상의 금액만 입력해주십시오.";
 
     public PurchaseAmount() {
     }
 
     private void isValidateDivisible(int purchaseAmount) {
-        if(purchaseAmount % UNIT_PRICE != 0){
+        if (purchaseAmount % UNIT_PRICE != 0) {
             throw new IllegalArgumentException(ERROR_INDIVISIBLE);
+        }
+    }
+
+    private void isValidateMinPrice(int purchaseAmount) {
+        if (purchaseAmount < UNIT_PRICE) {
+            throw new IllegalArgumentException(ERROR_MIN_PRICE);
         }
     }
 
