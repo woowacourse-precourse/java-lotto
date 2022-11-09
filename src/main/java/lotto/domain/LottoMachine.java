fba40cc68cbd8814595910d7b1f8b1dc04dcbe24
HwangSeokJin <ghkd3703@gmail.com>
2022-11-10 00:54:36 +0900

    feat(LottoMachine): 로또 발행 개수 구하는 기능 구현

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -6,13 +6,26 @@ public class LottoMachine {
     public void buy() throws IllegalArgumentException {
         String money = Console.readLine().trim();
         validateType(money);
+
+        int count = lottoCount(Integer.parseInt(money));
     }
 
     private void validateType(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
+    }
+
+    private int lottoCount(int money) throws IllegalArgumentException{
+        validateDivisible(money);
+        return money / 2;
+    }
+
+    private void validateDivisible(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("1000원으로 나누어 떨어지지 않습니다.");
         }
     }
 }
