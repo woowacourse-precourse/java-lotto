4904db02e7919cead5d97ca8f9d8a4a4afd5313c
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-10 01:10:59 +0900

    test : LottoRepository test 추가
    로또 숫자들이 중복되지 않는지 검사하는 테스트를 추가하였다.
    Set 에 로또 List 를 활용해 만든 후, set 의 길이가 6자리인지 검사

---
+++ src/test/java/lotto/repository/LottoRepositoryTest.java
@@ -0,0 +1,29 @@
+package lotto.repository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import lotto.domain.Lotto;
+
+public class LottoRepositoryTest {
+
+
+	@DisplayName("로또 숫자들이 중복되지 않는지 검사")
+	@Test
+	void givenNone_whenMakeAutoLotto_thenReturnLottoNumbers(){
+	    //given
+
+	    //when
+		Lotto lotto = LottoRepository.makeAutoLotto();
+		Set<Integer> lottoNumbers = new HashSet<>(lotto.getNumbers());
+
+		//then
+		Assertions.assertThat(lottoNumbers.size())
+			.isEqualTo(6);
+	}
+
+}
