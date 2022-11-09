82b4c79565d301c92a4f69e65332c7f3c30294ef
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-09 21:03:57 +0900

    refactor(ErrorEnum) : 웰컴 메세지 제거

--- src/main/java/lotto/common/Error.java
+++ src/main/java/lotto/common/Error.java
@@ -1,7 +1,6 @@
 package lotto.common;
 
 public enum Error {
-    WELCOME("구입금액을 입력해 주세요."),
     NUMBER("[ERROR] 구매금액은 숫자여야 합니다."),
     THOUSAND("[ERROR] 구매금액은 천원단위여야 합니다.");;
     private String msg;
