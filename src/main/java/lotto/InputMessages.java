7babf835d72bda920390a383c70657c246a16a1a
eyeofsol <kingjihong@naver.com>
2022-11-09 21:46:19 +0900

    feat(InputMessage): 입력을 받는 메세지 enum 구현
    입력을 받는 메세지들을 enum으로 구현했습니다.
    입력받은 action에 따라 메세지를 출력하도록 만들었습니다.
    static final로 상수화 하는 것 보다 더 가독성이 좋아졌습니다.

---
+++ src/main/java/lotto/InputMessages.java
@@ -0,0 +1,10 @@
+package lotto;
+
+public enum InputMessages {
+    MONEY("구입금액을 입력해 주세요."),
+    LOTTO("당첨 번호를 입력해 주세요."),
+    BONUS("보너스 번호를 입력해 주세요.");
+
+    InputMessages(String action) {
+    }
+}
