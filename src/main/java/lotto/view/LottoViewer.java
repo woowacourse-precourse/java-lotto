27ea487369e7cd1ab07b7e9b576b40bf72809309
whdcks420 <whdcks420@gmail.com>
2022-11-09 18:49:44 +0900

    feat(view): 구입금액 입력문 문구출력 기능

--- src/main/java/lotto/view/LottoViewer.java
+++ src/main/java/lotto/view/LottoViewer.java
@@ -1,2 +1,7 @@
-package lotto.view;public class LottoViewer {
+package lotto.view;
+
+public class LottoViewer {
+    public static void showEnterPurchaseAmountText() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
 }
