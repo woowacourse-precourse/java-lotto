14a5f9d613f1b767b657ffec7dde4dace43e2b3f
ouohoon <begreat@kakao.com>
2022-11-09 20:04:01 +0900

    feat(Shop): 로또 구매 금액 검증 기능 구현

--- src/main/java/lotto/Shop.java
+++ src/main/java/lotto/Shop.java
@@ -5,6 +5,11 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Shop {
+
+    private static final String INVALID_MONEY_INPUT =
+            "구매 금액은 1,000 단위로 나누어 떨어져야 합니다.";
+    private static final String INVALID_STRING_INPUT =
+            "숫자만 입력할 수 있습니다.";
     private Integer money;
     private List<Lotto> lottos;
 
@@ -15,6 +20,13 @@ public class Shop {
     }
 
     private void validate(String money) {
-
+        try {
+            Integer nMoney = Integer.parseInt(money);
+            if (nMoney % 1000 != 0) {
+                LottoError.illegalArgumentException(INVALID_MONEY_INPUT);
+            }
+        } catch (NumberFormatException ignored) {
+            LottoError.illegalArgumentException(INVALID_STRING_INPUT);
+        }
     }
 }
