e2c83f2a9ae5a60238706ecf2677575b940bf98c
ksh-beginner <ohk9134@naver.com>
2022-11-09 22:29:47 +0900

    test(LottoWinningNumbersGeneratorTest) : 로또 당첨 번호 생성 테스트

--- src/test/java/lotto/LottoWinningNumbersGeneratorTest.java
+++ src/test/java/lotto/LottoWinningNumbersGeneratorTest.java
@@ -1,6 +1,19 @@
 package lotto;
 
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 class LottoWinningNumbersGeneratorTest {
 
+    @Test
+    void 로또_당첨_번호_생성() {
+        List<Integer> lottoWinningNumbers = LottoWinningNumbersGenerator.generateLottoWinningNumbers();
+
+        assertThat(lottoWinningNumbers).isNotEmpty();
+    }
 }
