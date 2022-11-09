da643ca2a2d9ff140c98e2e34e43d88c15e878b9
yeonhee <ydol2@naver.com>
2022-11-10 07:23:04 +0900

    refactor(OutView) : constant 분리에 의한 변경 적용

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,13 +1,13 @@
 package view;
 
-import static view.MessageConstant.OUTPUT_PURCHASE;
-import static view.MessageConstant.STATISTICS_MESSAGE;
-import static view.MessageConstant.STATISTICS_THREE_MATCH;
-import static view.MessageConstant.STATISTICS_FOUR_MATCH;
-import static view.MessageConstant.STATISTICS_FIVE_MATCH;
-import static view.MessageConstant.STATISTICS_FIVE_MATCH_ONE_BONUS;
-import static view.MessageConstant.STATISTICS_SIX_MATCH;
-import static view.MessageConstant.OUTPUT_EARNING;
+import static constant.MessageConstant.OUTPUT_PURCHASE;
+import static constant.MessageConstant.STATISTICS_MESSAGE;
+import static constant.MessageConstant.STATISTICS_THREE_MATCH;
+import static constant.MessageConstant.STATISTICS_FOUR_MATCH;
+import static constant.MessageConstant.STATISTICS_FIVE_MATCH;
+import static constant.MessageConstant.STATISTICS_FIVE_MATCH_ONE_BONUS;
+import static constant.MessageConstant.STATISTICS_SIX_MATCH;
+import static constant.MessageConstant.OUTPUT_EARNING;
 
 public enum OutputView {
     INSTANCE;
