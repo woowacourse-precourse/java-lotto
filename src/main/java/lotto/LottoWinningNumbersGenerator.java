b5f2a07b56fa21ecc85adca354c0ce34e45a4e37
ksh-beginner <ohk9134@naver.com>
2022-11-09 22:30:41 +0900

    feat(LottoWinningNumbersGenerator) : 로또 당첨 번호 생성 기능 구현

--- src/main/java/lotto/LottoWinningNumbersGenerator.java
+++ src/main/java/lotto/LottoWinningNumbersGenerator.java
@@ -1,4 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class LottoWinningNumbersGenerator {
+
+    public static List<Integer> generateLottoWinningNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
 }
