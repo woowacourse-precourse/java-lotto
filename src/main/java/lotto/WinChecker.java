9ac7b58a63e456f4cdcdb8093e18fa71fc259939
yg0585 <yg0585@naver.com>
2022-11-10 00:24:55 +0900

    feat: 몇 등 로또인지 체크하는 기능 구현

---
+++ src/main/java/lotto/WinChecker.java
@@ -0,0 +1,24 @@
+package lotto;
+
+
+import java.util.List;
+
+public class WinChecker {
+    private final List<Integer> winNumber;
+    private final int bonusNumber;
+
+    public WinChecker(List<Integer> winNumber, int bonusNumber) {
+        this.winNumber = winNumber;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Rank checkLotto(Lotto lottoNumber) {
+        int count = lottoNumber.compare(winNumber);
+
+        return Rank.of(count, isBonus(lottoNumber));
+    }
+
+    private boolean isBonus(Lotto lottoNumber) {
+        return lottoNumber.contains(bonusNumber);
+    }
+}
