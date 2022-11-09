08702c4935b40a7b80532ec4c353739f9b5b74e7
JHS <wnghdtjr129@naver.com>
2022-11-09 20:37:29 +0900

    feat(LottoWinningNumber): 입력값 검증 로직 추가

--- src/main/java/lotto/domain/LottoWinningNumber.java
+++ src/main/java/lotto/domain/LottoWinningNumber.java
@@ -5,9 +5,34 @@ import java.util.List;
 public class LottoWinningNumber extends Lotto {
     private final int bonusNumber;
 
-    public LottoWinningNumber(List<Integer> winningNumber, String bonusNumber) {
+    public LottoWinningNumber(List<Integer> winningNumber, String bonus) {
         super(winningNumber);
-        this.bonusNumber = Integer.parseInt(bonusNumber);
+        int bonusNumber = validateInputIsNumber(bonus);
+        this.bonusNumber = validateDuplicate(bonusNumber);
+    }
+
+    public Result getResult(IssuedLotto issuedLotto) {
+        return issuedLotto.compare(super.getNumbers(), bonusNumber);
+    }
+
+    private int validateInputIsNumber(String bonus) {
+        int bonusNumber;
+        try {
+            bonusNumber = Integer.parseInt(bonus);
+            if (bonusNumber < 1 || bonusNumber > 45) {
+                throw new IllegalArgumentException("1~45 사이 숫자를 입력해 주시기 바랍니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("1~45 사이 숫자를 입력해 주시기 바랍니다.");
+        }
+        return bonusNumber;
+    }
+
+    private int validateDuplicate(int bonusNumber) {
+        if (super.hasBonusNumber(bonusNumber)) {
+            throw new IllegalArgumentException("1~45 사이 숫자를 입력해 주시기 바랍니다.");
+        }
+        return bonusNumber;
     }
 
     @Override
