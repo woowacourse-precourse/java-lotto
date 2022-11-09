a8b421c9b256a02bafb6b5abf719b0518817de10
HyeonjungYun <guswnd9971@gmail.com>
2022-11-09 18:39:14 +0900

    "feat: 예외 사항 추가"

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,36 @@
 package lotto;
 
 import java.util.List;
+import java.util.HashSet;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<Integer> lottoNumbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+    public Lotto(List<Integer> lottoNumbers) {
+        validateLottoNumberCount(lottoNumbers);
+        validateLottoNumberRange(lottoNumbers);
+        validateLottoNumberDuplicated(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateLottoNumberCount(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR]: 로또 당첨 숫자에는 6개를 입력해주세요");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateLottoNumberRange(List<Integer> lottoNumbers) {
+        for (int number: lottoNumbers){
+            if (number < 1 || number > 45) throw new IllegalArgumentException("[ERROR]: 로또 당첨 숫자에는 1이상 45이하의 숫자를 입력해주세요.");
+        }
+    }
+
+    private void validateLottoNumberDuplicated(List<Integer> lottoNumbers) {
+        HashSet<Integer> DuplicatedNumber = new HashSet<>(lottoNumbers);
+
+        if (DuplicatedNumber.size() < 6){
+            throw new IllegalArgumentException("[ERROR]: 로또 당첨 숫자에는 중복된 숫자를 입력하면 안됩니다.");
+        }
+    }
+
 }
