26a7acb2a2fe6f712f8a00c8887c40972ae7acbf
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-09 23:17:26 +0900

    feat : 에러 메시지를 담은 클래스

--- src/main/java/lotto/view/ErrorMessage.java
+++ src/main/java/lotto/view/ErrorMessage.java
@@ -3,4 +3,11 @@ package lotto.view;
 public class ErrorMessage {
     public static final String ERROR_LOTTO_SIZE = "[ERROR] 로또의 번호 개수가 6개가 아닙니다.";
     public static final String ERROR_LOTTO_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String ERROR_DUPLICATE = "[ERROR] 중복된 번호가 있습니다.";
+    public static final String ERROR_PRICE = "[ERROR] 잘못된 금액을 입력하셨습니다. 금액은 1000의 배수이어야 합니다.";
+    public static final String ERROR_NUMBER_RANGE = "[ERROR] 1부터 45 사이의 숫자들을 입력해야 합니다.";
+    public static final String ERROR_LOTTO_LENGTH = "[ERROR] 6개의 당첨번호를 입력해야 합니다.";
+    public static final String ERROR_BONUS_NUMBER = "[ERROR] 1부터 45 사이의 숫자들을 입력해야 합니다.";
+    public static final String ERROR_SEPARATOR = "[ERROR] 당첨 번호는 \",\" 쉼표를 기준으로 입력해야합니다.";
+    public static final String ERROR_BLANK = "[ERROR] 입력한 당첨 번호에 공백이 있습니다. 공백없이 입력해주세요.";
 }
