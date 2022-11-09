81877aed4842be3fb3de0ca7a6c2eed5a09d12dd
whdcks420 <whdcks420@gmail.com>
2022-11-09 20:33:18 +0900

    feat(validator): 로또 구매 금액 validate 기능

--- src/main/java/lotto/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -1,2 +1,12 @@
-package lotto.validator;public class PurchaseAmountValidator {
+package lotto.validator;
+
+public class PurchaseAmountValidator {
+    public static void validate(String money) {
+        if (Integer.parseInt(money) % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+        if (money.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
