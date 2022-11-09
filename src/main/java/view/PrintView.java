69b2902e34325080dfaef02bf590f0d9e9b8978d
gyuturn <rbtjrrbals@gmail.com>
2022-11-09 22:35:39 +0900

    feat($view): "구입금액을 입력해 주세요." 출력 기능
    mvc 패턴적용
    view package에서 class 생성
    
    A-1

---
+++ src/main/java/view/PrintView.java
@@ -0,0 +1,9 @@
+package view;
+
+public class PrintView {
+    private static final String MESSAGE_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+
+    public static void purchaseAmount() {
+        System.out.println(MESSAGE_PURCHASE_AMOUNT);
+    }
+}
