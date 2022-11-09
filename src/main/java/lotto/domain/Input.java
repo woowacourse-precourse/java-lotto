4e4cebb828b1ccea136f6ebc350a4941c28a9da8
panorama713 <yaiba713@gmail.com>
2022-11-09 20:07:06 +0900

    feat(coinError)

--- src/main/java/lotto/domain/Input.java
+++ src/main/java/lotto/domain/Input.java
@@ -20,17 +20,12 @@ public class Input {
     private static List<String> lottoNumber;
     private static String inputCoinString = "구입금액을 입력해 주세요.";
     private static String inputNumberString = "당첨 번호를 입력해 주세요.";
-    private static String coinErrorMessage = "[ERROR] 구입금액은 천원단위만 가능합니다.";
-    private static String numberRangeErrorMessage = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-    private static String numberRepeatErrorMessage = "[ERROR] 반복되는 번호가 있습니다.";
+
     public static void inputCoin() {
         System.out.println(inputCoinString);
         coin = Integer.parseInt(Console.readLine());
         // 입력받은 금액이 1000원 단위가 아닐 때 예외 처리
-        if(coin % 1000 != 0){
-            System.out.println(coinErrorMessage);
-            throw new IllegalArgumentException();
-        }
+
     }
 
     public static void inputNumber() {
