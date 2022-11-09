9bcaed0c7788ad5d2b24391fdddbf722147fc202
HyeonjungYun <guswnd9971@gmail.com>
2022-11-09 21:30:13 +0900

    "feat(Model): 로또 번호를 반복적으로 만들어내는 메서드 repeatGetLottoNumber() 추가"

--- src/main/java/lotto/Model.java
+++ src/main/java/lotto/Model.java
@@ -2,12 +2,23 @@ package lotto;
 
 import java.util.List;
 import java.util.ArrayList;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Model {
     private final int LOTTO_NUMBERS = 6;
 
-    private void getLottoNumber() {
+    public List<List<Integer>> repeatGetLottoNumber(int BONUS_NUMBER) {
+        List<List<Integer>> uncheckedLottos = new ArrayList<>();
+
+        for (int temp = 0; temp < BONUS_NUMBER; temp++) {
+            uncheckedLottos.add(getLottoNumber());
+        }
+
+        return uncheckedLottos;
+    }
+
+    private List<Integer> getLottoNumber() {
         List<Integer> lottoNumbers = new ArrayList<>();
 
         while (lottoNumbers.size() < LOTTO_NUMBERS) {
@@ -17,6 +28,7 @@ public class Model {
                 lottoNumbers.add(randomNumber);
             }
         }
+        return lottoNumbers;
     }
 
 }
