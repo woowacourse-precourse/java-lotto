647b791e011e9879ce614ab4064a7b24378351b6
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-09 18:33:10 +0900

    feat : 출력 메세지들을 상수로 선언

---
+++ src/main/java/lotto/Message.java
@@ -0,0 +1,17 @@
+package lotto;
+
+public class Message {
+    public static final String PURCHASE_PRICE = "구입금액을 입력해 주세요.";
+    public static final String PURCHASE_COUNT = "개를 구매했습니다.";
+    public static final String WINNING_NUMBER_INPUT = "당첨 번호를 입력해주세요.";
+    public static final String BONUS_NUMBER_INPUT = "보너스 번호를 입력해 주세요.";
+    public static final String WINNING_STATS = "당첨 통계";
+    public static final String LINES = "---";
+    public static final String THREE_MATCHES = "3개 일치 (5,000원) - 개";
+    public static final String FOUR_MATCHES = "4개 일치 (50,000원) - 개";
+    public static final String FIVE_MATCHES = "5개 일치 (1,500,000원) - 개";
+    public static final String FIVE_BONUS_MATCHES = "5개 일치, 보너스 볼 일치 (30,000,000원) - 개";
+    public static final String ALL_MATCHES = "6개 일치 (2,000,000,000원) - 개";
+    public static final String YIELDS= "총 수익률은 %입니다.";
+    public static final String ERROR = "[ERROR] ";
+}
