bbf86060c1a229a0f1311993b91f7991b7c8c54d
jeongseungchan <jschan0911@naver.com>
2022-11-09 19:32:03 +0900

    Feat: 구입 금액에 따른 로또 구매 횟수 계산 기능 추가

--- src/main/java/lotto/BuyLotto.java
+++ src/main/java/lotto/BuyLotto.java
@@ -2,10 +2,12 @@ package lotto;
 
 public class BuyLotto {
     private final int paidMoney;
+    public int lottoCount;
 
     public BuyLotto(int paidMoney) {
         validate(paidMoney);
         this.paidMoney = paidMoney;
+        lottoCount = paidMoney / 1000;
     }
 
     private void validate(int paidMoney) {
