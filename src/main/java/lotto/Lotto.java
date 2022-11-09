7d7fa6363a82392e5666181d4f3ef0efb62341a2
ouohoon <begreat@kakao.com>
2022-11-09 20:48:58 +0900

    reafactor(Lotto): 리터럴 값들을 상수로 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 public class Lotto {
 
-    private static final String INVALID_RANGE_INPUT = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-    private static final String INVALID_SIZE_INPUT = "로또 번호는 6개의 숫자여야 합니다.";
-    private static final String DUPLICATED_INPUT = "로또 번호는 중복되지 않습니다.";
-    private static final int SIZE = 6;
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 6;
+    private static final String INVALID_RANGE_INPUT =
+            "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String INVALID_SIZE_INPUT =
+            "로또 번호는 6개의 숫자여야 합니다.";
+    private static final String DUPLICATED_INPUT =
+            "로또 번호는 중복되지 않습니다.";
+    public static final int SIZE = 6;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
 
     private final List<Integer> numbers;
 
@@ -20,13 +23,13 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != SIZE) {
             LottoError.illegalArgumentException(INVALID_SIZE_INPUT);
         }
         Boolean[] isUnique = new Boolean[6];
         Arrays.fill(isUnique, false);
         for (Integer number : numbers) {
-            if (number < 1 || number > 45) {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
                 LottoError.illegalArgumentException(INVALID_RANGE_INPUT);
             }
             if (isUnique[number - 1]) {
