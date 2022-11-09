4c3d29ef9acd2f9f30350cee2ec05a5312077cee
cmong0516 <cmong0516@gmail.com>
2022-11-09 18:36:27 +0900

    feat(LottoMachine): makeUserRandomLottoNumbers
    Randoms.pickUniqueNumbersInRange 메서드를 활용하여 1~45 까지의 중복되지 않은 size 6 의
    List<Integer> 리턴

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class LottoMachine {
     public void requestWantBuyAmountInputMessage() {
         System.out.println("구입금액을 입력해 주세요.");
@@ -7,5 +10,9 @@ public class LottoMachine {
 
     public int canBuyAmount(int wantBuyAmountInput) {
         return wantBuyAmountInput / 1000;
+    }
+
+    public List<Integer> makeUserRandomLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }
