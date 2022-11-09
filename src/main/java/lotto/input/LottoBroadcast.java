d99f4757600a4a383822929606af74a2d950e13b
K-Diger <kitten.diger@gmail.com>
2022-11-09 21:14:28 +0900

    feat : inputBonusNumber 메서드 구현 - 보너스 번호를 입력받는 메서드 구현

--- src/main/java/lotto/input/LottoBroadcast.java
+++ src/main/java/lotto/input/LottoBroadcast.java
@@ -1,6 +1,7 @@
 package lotto.input;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.validator.UserInputValidator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,8 +13,14 @@ public class LottoBroadcast {
     public List<Integer> inputWinNumber() {
         String[] inputValue = Console.readLine().split(",");
         for (String value : inputValue) {
+            UserInputValidator.validateInputLottoNumber(inputValue);
             this.winNumber.add(Integer.valueOf(value));
         }
+        return this.winNumber;
+    }
+
+    public List<Integer> inputBonusNumber() {
+        this.winNumber.add(Integer.valueOf(Console.readLine()));
         return this.winNumber;
     }
 }
