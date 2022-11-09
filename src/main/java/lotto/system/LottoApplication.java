9acb42a6f23c79353269aa69101524d2eaca2ccd
backFox <jorealbusiness@gmail.com>
2022-11-10 00:43:27 +0900

    feat(construct): 애플리케이션을 실행하는 코어 객체 구현

---
+++ src/main/java/lotto/system/LottoApplication.java
@@ -0,0 +1,16 @@
+package lotto.system;
+
+public class LottoApplication {
+	public void run() {
+		doSetting();
+		doAfter();
+	}
+
+	private void doSetting() {
+
+	}
+
+	private void doAfter() {
+
+	}
+}
