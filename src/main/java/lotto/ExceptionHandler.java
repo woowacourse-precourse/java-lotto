c2972a5be97e04492881daf39da95ff3e7f29c30
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-11-09 20:27:06 +0900

    feat<ExceptionHandler>: 로또 번호 관련 예외 처리 추가

--- src/main/java/lotto/ExceptionHandler.java
+++ src/main/java/lotto/ExceptionHandler.java
@@ -2,9 +2,15 @@ package lotto;
 
 public class ExceptionHandler extends IllegalArgumentException{
 
-    public void foolExceaption(String aoptions) {
-        if (aoptions.equals("amount")) {
+    public void foolExceaption(String options) {
+        if (options.equals("amount")) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+        if (options.equals("lottoLength")) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 개수는 6개입니다.");
+        }
+        if (options.equals("lottoDuplicate")) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
         }
     }
 }
