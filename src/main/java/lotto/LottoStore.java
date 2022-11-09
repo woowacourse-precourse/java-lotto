1c3388d7f07eb3ad2d3fe3efc4223d0e140bc005
cpot5620 <=cpot5620@naver.com>
2022-11-10 00:26:11 +0900

    feat(LottoStore): create method to purchase lotto

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -4,15 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoStore {
+    private static final String ERROR_MSG = "[ERROR] 1000원 단위로 입력해주세요";
+    private static final String BUY_MSG = "개를 구매했습니다.";
     private static final int LOTTO_PRICE = 1000;
-    private static final String ERROR_MSG = "1000원 단위로 입력해주세요";
 
-    private List<Lotto> lottoNumbers;
+    private List<Lotto> lottoNumbers = new ArrayList<>();
 
-    public LottoStore(int price) {
+    public void buyLottoNumber(int price) {
         validate(price);
 
-        lottoNumbers = new ArrayList<>();
+        int lottoPurchaseCount = price / LOTTO_PRICE;
+        LottoNumber lottoNumber = new LottoNumber();
+
+        for (int i = 0; i < lottoPurchaseCount; i++) {
+            Lotto lotto = new Lotto(lottoNumber.create());
+
+            lottoNumbers.add(lotto);
+        }
     }
 
     private void validate(int price) {
--- src/test/java/lotto/LottoStoreTest.java
+++ src/test/java/lotto/LottoStoreTest.java
@@ -5,17 +5,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoStoreTest {
+
+    LottoStore lottoStore = new LottoStore();
+
     @DisplayName("1000원으로 나누어떨어지지 않을 경우 예외가 발생한다.")
     @Test
     void buyLottoByInvalidMoney() {
-        Assertions.assertThatThrownBy(() -> new LottoStore(9700))
+        Assertions.assertThatThrownBy(() -> lottoStore.buyLottoNumber(7900))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("금액이 부족하여 구매할 수 없을 경우 예외가 발생한다.")
     @Test
     void buyLottoByNoMoney() {
-        Assertions.assertThatThrownBy(() -> new LottoStore(0))
+        Assertions.assertThatThrownBy(() -> lottoStore.buyLottoNumber(0))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
