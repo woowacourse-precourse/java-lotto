ed01e946430d6e5a6488b41a431f188f237e5932
yg0585 <yg0585@naver.com>
2022-11-10 00:22:52 +0900

    feat: 로또 당첨 등수 enum으로 구현

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,39 @@
+package lotto;
+
+public enum Rank {
+
+    FIRST(6,2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    NOTHING(0, 0);
+
+    private static final int FIRST_PRICE_NUM = 6;
+    private static final int SECOND_PRICE_NUM = 5;
+    private static final int THIRD_PRICE_NUM = 5;
+    private static final int FOURTH_PRICE_NUM = 4;
+    private static final int FIFTH_PRICE_NUM = 3;
+
+    private final int count;
+    private final int prizemoney;
+
+    Rank(int count, int prizemoney) {
+        this.count = count;
+        this.prizemoney = prizemoney;
+    }
+
+    public static Rank of(int count, boolean bonus){
+        if (count == FIRST_PRICE_NUM)
+            return FIRST;
+        if (bonus && count == SECOND_PRICE_NUM)
+            return SECOND;
+        if (count == THIRD_PRICE_NUM)
+            return THIRD;
+        if (count == FOURTH_PRICE_NUM)
+            return FOURTH;
+        if (count == FIFTH_PRICE_NUM)
+            return FIFTH;
+        return NOTHING;
+    }
+}
