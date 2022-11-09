b5033d31af5fbf0fa10b5c4d6e4a1982af7035ac
Eungyu-code <Agyu625@gmail.com>
2022-11-09 19:45:22 +0900

    update(Lotto class): validate 함수의 이름 변경
    validate(List<Integer>) -> validateNumbersSize(List<Integer>)
    - 어떤 것에 대한 검사가 이루어지는지 명확하게 하기 위해 함수 이름 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,11 +6,11 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumbersSize(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateNumbersSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
