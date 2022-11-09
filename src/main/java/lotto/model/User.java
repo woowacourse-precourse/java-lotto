22b6ab84cbcc7a757e67629226d596e9a89a09ec
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-09 19:05:52 +0900

    feat(User): user 객체 생성 - userPrice 값 가짐

---
+++ src/main/java/lotto/model/User.java
@@ -0,0 +1,14 @@
+package lotto.model;
+
+public class User {
+    private int userPrice;
+
+    public User() {
+        userPrice=0;
+    }
+
+    public void setUserPrice(int userPrice) {
+        this.userPrice = userPrice;
+    }
+
+}
