5b26feb61dd406f97bfb9df7c7741e0e19d96c1b
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-10 00:00:05 +0900

    feature(controller) : 당첨번호와 보너스 번호를 받는 기능 구현

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -2,6 +2,7 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import lotto.Lotto;
@@ -13,6 +14,8 @@ public class GameController {
     private final LottoService service = new LottoService();
     private List<Lotto> lottos = new ArrayList<>();
     private String input = "";
+    private List<Integer> winns = new ArrayList<>();
+    private int bonus;
 
     public void start() {
         welcome();
@@ -21,8 +24,15 @@ public class GameController {
         if (isEmptyLottos()) {
             return;
         }
+
         printNumberOfLotto();
         printLottos();
+
+        inputWinnings();
+
+        inputBonus();
+
+        printResult();
     }
 
     private void welcome() {
@@ -45,7 +55,12 @@ public class GameController {
         return lottos.isEmpty();
     }
 
+    private void printLn() {
+        System.out.println();
+    }
+
     private void printNumberOfLotto() {
+        printLn();
         System.out.println(lottos.size() + "개를 구매했습니다.");
     }
 
@@ -59,5 +74,25 @@ public class GameController {
             System.out.print(numbers.get(numbers.size() - 1));
             System.out.println("]");
         });
+    }
+
+    private void inputWinnings() {
+        printLn();
+        System.out.println(Msg.WINNING.getMsg());
+        String s = Console.readLine();
+        String[] split = s.split(",");
+        winns = Arrays.stream(split).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    private void inputBonus() {
+        printLn();
+        System.out.println(Msg.BONUS.getMsg());
+        String s = Console.readLine();
+        bonus = Integer.parseInt(s);
+    }
+
+    private void printResult() {
+        printLn();
+        System.out.println(Msg.RESULT.getMsg());
     }
 }
