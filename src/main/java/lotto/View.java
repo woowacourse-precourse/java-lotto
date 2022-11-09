d5f7781c008d12d7f40bb5686e911e7bd580870d
HyeonjungYun <guswnd9971@gmail.com>
2022-11-09 19:40:26 +0900

    "feat(View): 입력된 문자열이 숫자로만 이루어져있는지 확인하는 메서드 isRealNumber() 추가"

---
+++ src/main/java/lotto/View.java
@@ -0,0 +1,10 @@
+package lotto;
+
+public class View {
+
+    private void isRealNumber (String inputStatement) {
+        for (char element: inputStatement.toCharArray()) {
+            if (element < 49 || element > 57) throw new IllegalArgumentException();
+        }
+    }
+}
