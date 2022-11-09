26240fe1e461908e898eb22e9596030b77c8ec9b
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-09 19:47:31 +0900

    feat : 1~45까지의 랜덤한 6개의 번호를 만드는 메서드 추가

--- src/main/java/lotto/Computer.java
+++ src/main/java/lotto/Computer.java
@@ -1,2 +1,28 @@
-package lotto;public class Computer {
+package lotto;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+import static lotto.view.ErrorMessage.ERROR_DUPLICATE;
+import static lotto.view.ErrorMessage.ERROR_LOTTO_RANGE;
+import static lotto.view.ErrorMessage.ERROR_LOTTO_SIZE;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Computer {
+    private static final int START_NUM = 1;
+    private static final int END_NUM = 45;
+    private static final int LOTTO_SIZE = 6;
+
+    static List<Integer> autoNumbers;
+
+    static List<Integer> createAutoNumbers() {
+        autoNumbers = pickUniqueNumbersInRange(START_NUM, END_NUM, LOTTO_SIZE);
+        Set<Integer> duplicates = new HashSet<>(autoNumbers);
+        autoNumbers = new ArrayList<>(duplicates);
+        Collections.sort(autoNumbers);
+        return autoNumbers;
+    }
 }
