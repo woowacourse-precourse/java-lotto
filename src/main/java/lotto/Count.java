02c4d303631105be8326f54acece1944eca74f8b
EUNJEONGMUN <munej26@gmail.com>
2022-11-10 01:40:37 +0900

    feat: 사용자에게 입력 받은 금액 천원 단위가 아닐 경우 예외 처리

--- src/main/java/lotto/Count.java
+++ src/main/java/lotto/Count.java
@@ -1,11 +1,14 @@
 package lotto;
 
 public class Count {
+    static final int MONEY_UNIT = 1000;
+    static final int REMAINDER_ZERO = 0;
+
     private final int numOfLotto;
 
     public Count(String price) {
         validPriceOfLotto(price);
-        numOfLotto = Integer.parseInt(price);
+        numOfLotto = calculateNumOfLotto(Integer.parseInt(price));
     }
 
     private int validPriceOfLotto(String priceOfLotto) {
@@ -14,5 +17,12 @@ public class Count {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
         }
+    }
+
+    private int calculateNumOfLotto(int priceOfLotto) {
+        if (priceOfLotto%MONEY_UNIT==REMAINDER_ZERO) {
+            return priceOfLotto/MONEY_UNIT;
+        }
+        throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해주세요.");
     }
 }
