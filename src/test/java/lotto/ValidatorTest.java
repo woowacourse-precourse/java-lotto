eaa0f4ec1054adc9363f6fb4730202d0c93db29b
chlwodyd9829 <chlwodyd9829@naver.com>
2022-11-09 17:39:16 +0900

    test: 사용자에게 입력 받은 로또 값 검증 테스트

--- src/test/java/lotto/ValidatorTest.java
+++ src/test/java/lotto/ValidatorTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,6 +25,21 @@ class ValidatorTest {
     void moneyCheck(String s){
         assertThatThrownBy(()->{
             validator.moneyValidate(s);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void winningNumberTest(){
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        assertThat(winningNumber).isEqualTo(
+                validator.winningNumberValidate("1,2,3,4,5,6")
+        );
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4 ,6,3","1,1,1,1,1","1,2,3,4,4,4"})
+    void notWinningNumberTest(String s){
+        assertThatThrownBy(()->{
+            validator.winningNumberValidate(s);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 }
