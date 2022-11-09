fe3536038b21e05a992b83c2c8461277919b5fda
dev_kong <ahdjd5@gmail.com>
2022-11-09 19:27:16 +0900

    test(ValidatorTest): add validate multiple of thousand test code

--- src/test/java/lotto/ValidatorTest.java
+++ src/test/java/lotto/ValidatorTest.java
@@ -13,9 +13,17 @@ public class ValidatorTest {
 
     @DisplayName("입력 받은 값이 숫자가 아닐경우 예외가 발생한다")
     @ParameterizedTest
-    @ValueSource(strings = {"123k","k123","adsdf","12_3","123 4"})
+    @ValueSource(strings = {"123k", "k123", "adsdf", "12_3", "123 4"})
     void validateNumber(String input) {
         assertThatThrownBy(() -> validator.isNumberOrNot(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력 받은 값이 1000의 배수가 아닐 경우 예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1001", "1002", "800", "2100"})
+    void validateMultipleOfThousand(String input) {
+        assertThatThrownBy(()-> validator.isMultiplesOfThousand(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
