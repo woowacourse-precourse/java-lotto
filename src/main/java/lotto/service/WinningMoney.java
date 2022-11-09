34797715a2b2e632f29677cecf5596c14c07d02c
ywj9811 <ywj9811@naver.com>
2022-11-10 02:43:21 +0900

    feat: WinningMoney 클래스
    각 금액들 상수로 생성, 금액을 가지고 있는 리스트 생성

---
+++ src/main/java/lotto/service/WinningMoney.java
@@ -0,0 +1,22 @@
+package lotto.service;
+
+import lotto.domain.MyLottoNumbers;
+import lotto.domain.WinningLottoNumbers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WinningMoney {
+    private final static int NONE = 0;
+    private final static int FIRST = 2000000000;
+    private final static int SECOND = 30000000;
+    private final static int THIRD = 1500000;
+    private final static int FOURTH = 50000;
+    private final static int FIFTH = 5000;
+    private final static List<Integer> MONEYS =
+            Arrays.asList(NONE, NONE, NONE, FIFTH, FOURTH, THIRD, SECOND, FIRST);
+
+
+
+}
