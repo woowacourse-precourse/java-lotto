39d7e2b3ce4e6f284824d66b96ac0af03e94d1c0
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-10 00:59:23 +0900

    feature(service) : 당첨 결과 리턴하는 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,6 +2,7 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import lotto.Lotto;
 import lotto.common.Error;
@@ -42,5 +43,30 @@ public class LottoService {
         if (i % 1000 != 0) {
             throw new IllegalArgumentException(Error.THOUSAND.getMsg());
         }
+    }
+
+    public List<Integer> getResult(List<Lotto> lottos, List<Integer> winns, int bonus) {
+        int w1 = 0;
+        int w2 = 0;
+        int w3 = 0;
+        int w4 = 0;
+        int w5 = 0;
+        for (Lotto lotto : lottos) {
+            List<Integer> numbers = lotto.getNumbers();
+            int cnt = 0;
+            int b = 0;
+            for (int i = 0; i < 6; i++) {
+                if (numbers.get(i) == bonus) b++;
+                for (int j = 0; j < 6; j++) {
+                    if (numbers.get(i) == winns.get(j)) cnt++;
+                }
+            }
+            if (cnt == 6) w1++;
+            else if (cnt == 5 && b == 1) w2++;
+            else if (cnt == 5) w3++;
+            else if (cnt == 4) w4++;
+            else if (cnt == 3) w5++;
+        }
+        return Arrays.asList(w1, w2, w3, w4, w5);
     }
 }
