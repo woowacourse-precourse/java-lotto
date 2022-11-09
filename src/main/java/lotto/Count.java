370b16635b242ef20eec493ee2bac9e0a57a6db3
EUNJEONGMUN <munej26@gmail.com>
2022-11-10 01:31:40 +0900

    feat: 사용자에게 입력 받은 금액 숫자가 아닐 경우 예외처리

--- src/main/java/lotto/Count.java
+++ src/main/java/lotto/Count.java
@@ -4,6 +4,15 @@ public class Count {
     private final int numOfLotto;
 
     public Count(String price) {
+        validPriceOfLotto(price);
         numOfLotto = Integer.parseInt(price);
+    }
+
+    private int validPriceOfLotto(String priceOfLotto) {
+        try {
+            return Integer.parseInt(priceOfLotto);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
+        }
     }
 }
