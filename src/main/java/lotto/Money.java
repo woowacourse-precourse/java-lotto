ba237ba30ec626af4c270e505fa464b62b2b0922
saewoo1 <wooozooo1123@gmail.com>
2022-11-09 17:14:15 +0900

    feat(Money): 1000원 단위로만 수가 입력되었는지 검사

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -13,13 +13,17 @@ public class Money {
     for (int i = 0; i < userMoney.length(); i++)
     {
       if (!Character.isDigit(userMoney.charAt(i)))
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException(); // 추후 에러 내용을 출력하는 메세지 추가
     }
     return true;
   }
 
   public boolean checkDividableMoney() {
+    String userMoney = inputMoney();
+    int money = Integer.parseInt(userMoney);
 
+    if (money % 1000 != 0)
+        throw new IllegalArgumentException(); // 추후 에러 내용을 출력하는 메세지 추가
     return true;
   }
 }
