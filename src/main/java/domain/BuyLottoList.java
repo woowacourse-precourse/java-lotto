36c088b33594509d99b813ed93f7af6e44e63ed3
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-10 01:30:42 +0900

    refactor(lotto): 출력부 클래스 명 변화에 따른 코드 수정

--- src/main/java/domain/BuyLottoList.java
+++ src/main/java/domain/BuyLottoList.java
@@ -4,11 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import view.PrintLotto;
+import view.PrintBuyLottoList;
 
 public class BuyLottoList {
 
-    PrintLotto printLotto = new PrintLotto();
+    PrintBuyLottoList printBuyLottoList = new PrintBuyLottoList();
 
 
     public void putLottoNumberList(int money){
@@ -17,7 +17,7 @@ public class BuyLottoList {
         for(int number=0; number<pick_count; number++){
             setLottoNumberList(pickList, number);
           }
-        printLotto.printLottoList(pick_count, pickList);
+        printBuyLottoList.printLottoList(pick_count, pickList);
     }
 
     private void setLottoNumberList(List<List<Integer>> pickList, int number) {
