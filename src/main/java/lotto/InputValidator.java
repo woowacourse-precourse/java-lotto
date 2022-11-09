fc97a5c15e0bc5b233c1b37a5cd670c47e6426da
이한솔 <hansol8701@naver.com>
2022-11-09 20:31:08 +0900

    feat(InputValidator): 스켈레톤 코드 작성

---
+++ src/main/java/lotto/InputValidator.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class InputValidator {
+    public boolean validatePurchaseAmount(int cash){
+        return false;
+    }
+
+    public boolean validateLottoNumber(int number){
+        return false;
+    }
+}
