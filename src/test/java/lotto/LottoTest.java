80dcbaf9a7f8c067c58735ee6fdf4bfcc0694ee1
junhei Lee <jun2ee22@naver.com>
2022-11-09 23:36:55 +0900

    test: 같은 번호의 개수 반환 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,8 +4,11 @@ import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,5 +27,13 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("포함하고 있는 개수를 반환하는지 확인한다.")
+    @Test
+    void checkReturnCorrectNumber() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> drawnNumbers = new ArrayList<>(Arrays.asList(7,8,9,10,11,12));
+
+        assertThat(lotto.checkNumbers(drawnNumbers)).isEqualTo(0);
+    }
     // 아래에 추가 테스트 작성 가능
 }
