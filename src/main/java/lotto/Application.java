6e7b19bc6a834f34bddb37f2fa4e6adf753f938d
noah.lee <rhkd2612@naver.com>
2022-11-10 05:53:37 +0900

    feat: create sorted random lotto sequenced

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,7 +2,7 @@ package lotto;
 
 /*
  * [필요 기능 명세]
- * [X] 랜덤 로또 번호 생성
+ * [O] 랜덤 로또 번호 생성
  * [X] 당첨 로또 번호 입력
  * [X] 당첨 통계 계산
  * [X] 총 수익률 계산
@@ -11,8 +11,22 @@ package lotto;
  * [X] 테스트 확인
  */
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+
+        createRandomLottoList();
+    }
+
+    private static List<Integer> createRandomLottoList() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).stream().sorted().collect(Collectors.toList());
+        System.out.println(numbers.toString());
+        return numbers;
     }
 }
