93b2523af98017a953da7d75497fe1e95b9695ba
Jeongthingamajig <netpetet@naver.com>
2022-11-09 23:15:10 +0900

    Feat : 출력 메세지 클래스 구현
    출력하는 메세지들을 모아 클래스한 곳에서 관리한다.

---
+++ src/main/java/lotto/OutputMessage.java
@@ -0,0 +1,19 @@
+package lotto;
+
+public class OutputMessage {
+    public static String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    public static String BUY_LOTTE_MESSAGE = "개를 구매했습니다.";
+    public static String INPUT_LOTTO_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    public static String WIN_A_LOTTERY_STATUS = "당첨 통계";
+    public static String CATCH_THREE_NUMBER = "3개 일치 (5,000원) - ";
+    public static String CATCH_FOUR_NUMBER = "4개 일치 (50,000원) - ";
+    public static String CATCH_FIVE_NUMBER = "5개 일치 (1,500,000원) - ";
+    public static String CATCH_FIVE_AND_BONUS_NUMBER = "5개 일치, 보너스 볼 일치 (300,000,000원) - ";
+    public static String CONGRATULATIONS = "6개 일치 (2,000,000,000원) - ";
+    public static String SEPARATION = "---";
+    public static String CATCH_NUMBER_BACK = "개";
+    public static String USER_RETURN_FRONT = "총 수익률은";
+    public static String USER_RETURN_BACK = "입니다.";
+
+}
