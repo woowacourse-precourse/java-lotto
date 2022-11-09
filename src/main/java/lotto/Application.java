8f961ab2a2cc098bdb19c4568550bddc3c6862c3
noah.lee <rhkd2612@naver.com>
2022-11-10 06:01:07 +0900

    feat: winnings enum class

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,7 @@ package lotto;
 /*
  * [필요 기능 명세]
  * [O] 랜덤 로또 번호 생성
+ * [O] 당첨금 enum class 생성
  * [X] 당첨 로또 번호 입력
  * [X] 당첨 통계 계산
  * [X] 총 수익률 계산
@@ -17,6 +18,24 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Application {
+    public enum WINNINGS{
+        FIRST(2000000000),
+        SECOND(30000000),
+        THIRD(1500000),
+        FOURTH(50000),
+        FIFTH(5000);
+
+        int value;
+
+        private WINNINGS(int value){
+            this.value = value;
+        }
+
+        public int getValue(){
+            return value;
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
@@ -26,7 +45,9 @@ public class Application {
 
     private static List<Integer> createRandomLottoList() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).stream().sorted().collect(Collectors.toList());
-        System.out.println(numbers.toString());
+
+        // System.out.println(numbers.toString());
+
         return numbers;
     }
 }
