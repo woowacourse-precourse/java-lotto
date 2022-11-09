c599a41e86969a833128e03dca804096253c145e
yj2980 <gum0945@naver.com>
2022-11-10 00:32:42 +0900

    feat : enum 클래스 생성

--- src/main/java/lotto/Notice.java
+++ src/main/java/lotto/Notice.java
@@ -1,2 +1,19 @@
-package lotto;public enum notice {
+package lotto;
+
+public enum Notice {
+	start("구입금액을 입력해 주세요."),
+	purchase("개를 구매했습니다."),
+	winningNumbers("당첨 번호를 입력해 주세요."),
+	bonusNumber("보너스 번호를 입력해 주세요."),
+	statistics("당첨 통계\n---");
+
+	private String noticeMessage;
+
+	Notice(String noticeMessage) {
+		this.noticeMessage = noticeMessage;
+	}
+
+	public String getNoticeMessage() {
+		return noticeMessage;
+	}
 }
