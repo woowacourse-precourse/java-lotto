25b0e90d76999cf86f69675289ad96a00cc61774
KSB-skyBlueLover <gogotkdqja@yonsei.ac.kr>
2022-11-10 01:01:28 +0900

    feat: add a feature that validates money

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,6 +9,8 @@ public class Application {
     final static int lottoPrice = 1000;
     final static String lottoPriceString = addComma(lottoPrice);
     final static String errorMsg1 = "[ERROR] 숫자를 입력해 주세요.";
+    final static String errorMsg2 =
+            "[ERROR] 구입 금액은 " + lottoPriceString + "원으로 나누어 떨어지는 자연수여야 합니다.";
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -28,4 +30,9 @@ public class Application {
         }
     }
 
+    public static void validateMoney(int money) throws IllegalArgumentException{
+        if (money <= 0 || money % lottoPrice != 0) {
+            throw new IllegalArgumentException(errorMsg2);
+        }
+    }
 }
