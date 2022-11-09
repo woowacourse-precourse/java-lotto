d47d2c2a5125ad06106394fbf43dcc5a476bf556
whdcks420 <whdcks420@gmail.com>
2022-11-09 22:43:01 +0900

    feat(valid): 로또 당첨번호 입력 validate

--- src/main/java/lotto/validator/LottoNumberValidator.java
+++ src/main/java/lotto/validator/LottoNumberValidator.java
@@ -1,2 +1,36 @@
-package lotto.validator;public class LottoNumberValidator {
+package lotto.validator;
+
+import java.util.stream.Collectors;
+
+public class LottoNumberValidator {
+
+    private static final String REGEX = "^([0-9]*,){5}[0-9]*$";
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int MAX_NUMBER = 46;
+    private static final int MIN_NUMBER = 0;
+
+    public static void validate(String lottoNumber) {
+        if (lottoNumber.matches(REGEX)) {
+            throw new IllegalArgumentException();
+        }
+        if (isisOverlap(lottoNumber)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (!isCorrectRange(lottoNumber)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isCorrectRange(String lottoNumber) {
+        return lottoNumber.chars().mapToObj(i -> (char) i)
+                .collect(Collectors.toList())
+                .stream().allMatch(lottoNumbers -> lottoNumbers < MAX_NUMBER && lottoNumbers > MIN_NUMBER);
+    }
+
+    private static boolean isisOverlap(String lottoNumber) {
+        return lottoNumber.chars().filter(i -> (char) i == ',')
+                .boxed()
+                .collect(Collectors.toSet()).size() != LOTTO_NUMBER_SIZE;
+    }
 }
