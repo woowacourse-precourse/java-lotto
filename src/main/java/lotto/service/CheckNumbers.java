c0d25754b3d0c4bc282edb2e16aa56cfb9299784
ywj9811 <ywj9811@naver.com>
2022-11-10 02:27:44 +0900

    feat: CheckNumbers 클래스
    당첨 번호에 구매한 번호가 몇개 포함되는지 카운트하여 리턴

---
+++ src/main/java/lotto/service/CheckNumbers.java
@@ -0,0 +1,32 @@
+package lotto.service;
+
+import java.util.List;
+
+public class CheckNumbers {
+    private static int count = 0;
+    /**
+     * 1등: 6개 번호 일치 / 2,000,000,000원
+     * 2등: 5개 번호 + 보너스 번호 일치 / 30,000,000원
+     * 3등: 5개 번호 일치 / 1,500,000원
+     * 4등: 4개 번호 일치 / 50,000원
+     * 5등: 3개 번호 일치 / 5,000원
+     * 메소드 15줄 이내로
+     */
+    public int getCorrect(List<Integer> winningNumbers, List<Integer> myNumbers, int bonusNumber) {
+        for (int idx = 0; idx < winningNumbers.size(); idx++) {
+            if (winningNumbers.contains(myNumbers.get(idx))) {
+                count++;
+            }
+        }
+        if (count == 5)
+            secondGrade(myNumbers, bonusNumber);
+
+        return count;
+    }
+
+    public void secondGrade(List<Integer> myNumbers, int bonusNumber) {
+        if (myNumbers.contains(bonusNumber)) {
+            count++;
+        }
+    }
+}
