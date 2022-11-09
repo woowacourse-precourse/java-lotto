af036dfd2e6f59d4810b890896030f2937d1093b
HyebinShin <hyeviin.dev@gmail.com>
2022-11-09 18:52:38 +0900

    Feat: 로또 구입 기능: 로또 구입 금액을 입력
    Console.readLine()으로 로또 구입 금액 입력
    입력 받은 값 유효성 검사: 1000 단위의 숫자

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(Print.ASK_BUY);
+        String input = inputPublishNum();
+
+    }
+
+    //로또 구입
+    private static String inputPublishNum() {
+        String input = Console.readLine();
+
+        validateInputPublish(input);
+
+        return input;
+    }
+
+    private static void validateInputPublish(String input) {
+        if(!Pattern.matches("^[0-9]*000$", input)) {
+            throw new IllegalArgumentException("[ERROR] 1,000 단위의 숫자만 입력해 주세요.");
+        }
+    }
+
+    private static int getPublishNum() {
+        return 0;
     }
 }
