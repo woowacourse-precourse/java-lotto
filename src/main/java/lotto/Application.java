ac44d90a6ac91de51ff8d2fda10d04d64822f016
YIMHYEONGSUP <hs.yim@illb.co.kr>
2022-11-10 08:58:03 +0900

    fix - 구입 금액 검정 예외 메세지 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,25 +1,32 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Application {
 
     public static String ERROR_MESSAGE = "[ERROR]";
+    public static int LOTTO_PRICE = 1000;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         // 구입 금액 입력 받기
         int account = setAccount();
-        System.out.println("account = " + account);
+
 
     }
+
 
     private static int setAccount() {
         int account = Integer.parseInt(Console.readLine());
         // 입력 받은 금액 검증
         if(validAccount(account)){
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 구입 금액은 1,000원 단위로 입력해주세요.");
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_INPUT_VALUE.message());
         }
         return account;
     }
