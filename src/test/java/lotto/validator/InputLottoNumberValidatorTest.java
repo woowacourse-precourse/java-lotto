08bd81ea09dc821336082ea4f2b8f821aba2d94d
whdcks420 <whdcks420@gmail.com>
2022-11-10 01:38:56 +0900

    test(validator): 당첨 번호 입력 포맷 테스트

--- src/test/java/lotto/validator/InputLottoNumberValidatorTest.java
+++ src/test/java/lotto/validator/InputLottoNumberValidatorTest.java
@@ -1,4 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.validator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class InputLottoNumberValidatorTest {
-  
+
+    @DisplayName("당첨 번호 입력 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6,","1,2,3,3,6,7"})
+    void inputLottoNumber(String input) {
+        assertThrows(IllegalArgumentException.class,
+                () -> InputLottoNumberValidator.validate(input));
+    }
 }
