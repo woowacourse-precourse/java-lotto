14e013e79a2904410d66dc84c6d592b5cb164707
iamminho <minho100227@kakao.com>
2022-11-10 08:20:46 +0900

    test: 입력한 로또구입금액 예외처리 테스트

---
+++ src/test/java/lotto/MoneyTest.java
@@ -0,0 +1,71 @@
+package lotto;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+
+	@Test
+	@DisplayName("숫자 외 다른 문자가 포함된 경우")
+	void validateNumberRangeTest1() {
+		String inputMoney = "7000a";
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+			Money money = new Money(inputMoney);
+		});
+
+		String expectedMessage = "[ERROR] 숫자만 입력이 가능합니다.";
+		assertThat(expectedMessage).isEqualTo(exception.getMessage());
+	}
+
+	@Test
+	@DisplayName("공백이 포함된 경우")
+	void validateNumberRangeTest2() {
+		String inputMoney = " 7000";
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+			Money money = new Money(inputMoney);
+		});
+
+		String expectedMessage = "[ERROR] 숫자만 입력이 가능합니다.";
+		assertThat(expectedMessage).isEqualTo(exception.getMessage());
+	}
+
+	@Test
+	@DisplayName("첫째자리 숫자가 0인 경우")
+	void validateFirstDigitZeroTest() {
+		String inputMoney = "09000";
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+			Money money = new Money(inputMoney);
+		});
+
+		String expectedMessage = "[ERROR] 숫자의 첫째자리로 0은 불가능합니다.";
+		assertThat(expectedMessage).isEqualTo(exception.getMessage());
+	}
+
+	@Test
+	@DisplayName("1000보다 작은 경우")
+	void validateMultipleOfThousandTest1() {
+		String inputMoney = "900";
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+			Money money = new Money(inputMoney);
+		});
+
+		String expectedMessage = "[ERROR] 숫자는 1000이상부터 가능합니다.";
+		assertThat(expectedMessage).isEqualTo(exception.getMessage());
+	}
+
+	@Test
+	@DisplayName("1000의 배수가 아닌 경우")
+	void validateMultipleOfThousandTest2() {
+		String inputMoney = "1900";
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+			Money money = new Money(inputMoney);
+		});
+
+		String expectedMessage = "[ERROR] 숫자는 1000이상부터 가능합니다.";
+		assertThat(expectedMessage).isEqualTo(exception.getMessage());
+	}
+
+}
