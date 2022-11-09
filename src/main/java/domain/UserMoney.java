698a1f4051d5d28e31792c452f54bd530ec20327
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-10 01:09:27 +0900

    feat(lotto): 사용자 로또 구입 금액 입력 기능, 예외 처리  구현

--- src/main/java/domain/UserMoney.java
+++ src/main/java/domain/UserMoney.java
@@ -1,29 +1,65 @@
 package domain;
 
+import static view.ExceptionInfo.EXCEPTION_DETECT;
+
 import camp.nextstep.edu.missionutils.Console;
+import java.util.stream.IntStream;
+import view.ExceptionInfo;
 
 public class UserMoney {
 
-
     public static int inputMoney(){
         String input_money = Console.readLine();
-        return checkMoney(input_money);
+        if(!validateMoney(input_money)){
+            return EXCEPTION_DETECT;
+        }
+
+        return Integer.parseInt(input_money);
     }
 
-    public static int checkMoney(String input_money) {
-        int money=0;
+    public static boolean validateMoney(String input_money){
+        if (checkProcessIsNumber(input_money)) {
+            return false;
+        }
+        if (checkProcessIsCorrectMoney(input_money)) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean checkProcessIsCorrectMoney(String input_money) {
+        try{
+            isCorrectMoney(Integer.parseInt(input_money));
+        }
+        catch (IllegalArgumentException e){
+            ExceptionInfo.printInCorrectMoney();
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean checkProcessIsNumber(String input_money) {
         try {
-            money = Integer.parseInt(input_money);
+           isNumber(input_money);
         }
-        catch(Exception e){
-            throw new IllegalArgumentException("[ERROR] 사용자의 금액 입력 오류입니다.");
+        catch(IllegalArgumentException e){
+            ExceptionInfo.printNotNumber();
+            return true;
         }
-
-        if(money % 1000>0){
-            throw new IllegalArgumentException("[ERROR] 올바르지 못한 입력 금액입니다.");
-        }
-        return money;
+        return false;
     }
 
+    private static void isCorrectMoney(int money) {
+        if(money %1000>0){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void isNumber(String input_money){
+        IntStream chars = input_money.chars();
+        if(chars.anyMatch(c -> c<'0' && c>'9')){
+            throw new IllegalArgumentException();
+        }
+    }
 
 }
