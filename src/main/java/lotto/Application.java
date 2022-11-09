866d88f043e4f086a0546a93c84fc3af42a3bcc4
sumin-Heo <102477741+sumin-Heo@users.noreply.github.com>
2022-11-10 00:27:47 +0900

    feat : 정답 6개를 입력 받는 함수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,25 +1,48 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Application {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
     }
 
-    public static Integer takeCost(){
+    public static Integer takeCost() {
         int cost = 0;
-
-        Scanner sc = new Scanner(System.in);
 
         cost = sc.nextInt();
         CostRight(cost);
 
         return cost;
     }
-    public static void CostRight(int cost){
-        if(cost%1000 != 0)
+
+    public static void CostRight(int cost) {
+        if (cost % 1000 != 0)
             throw new IllegalArgumentException("[ERROR] 입력 금액 오류");
+    }
+
+    public static List<String> createAnswer(){
+        String answerN;
+        answerN = sc.nextLine();
+        int flag = 0;
+
+        List<String> answer = List.of(answerN.split(","));
+        for (int i = 0; i<6; i++){
+            for(int j = 1; j<6;j++) {
+                if (answer.get(i) == answer.get(j)) flag = 1;
+                if(flag == 1) break;
+            }
+        }
+
+        if(flag == 1) createAnswer();
+
+        return answer;
     }
 
 
