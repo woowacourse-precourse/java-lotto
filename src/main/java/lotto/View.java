7a817bc2139b881f8f078e8eb55b813ea026d811
HyeonjungYun <guswnd9971@gmail.com>
2022-11-09 19:41:15 +0900

    "feat(View): 로또를 구매할 금액을 입력하는 메서드 inputMoney() 추가"

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -1,6 +1,21 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class View {
+    public void ui () {
+        inputMoney();
+    }
+
+    private int inputMoney () {
+        String money;
+
+        System.out.println("구입 금액을 입력해주세요.");
+        money = Console.readLine();
+        isRealNumber(money);
+
+        return Integer.parseInt(money);
+    }
 
     private void isRealNumber (String inputStatement) {
         for (char element: inputStatement.toCharArray()) {
