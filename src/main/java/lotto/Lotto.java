1cf969c3ef1e1324eb7ad7bf5dbb6bc83aef8ed1
cpot5620 <=cpot5620@naver.com>
2022-11-09 23:01:30 +0900

    feat(Lotto): create method to check for duplicate numbers

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,13 @@
 package lotto;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
+    private static final String ERROR_MSG = "로또 번호는 1~45 사이의 중복되지 않은 6개의 숫자입니다";
+    private static final int LOTTO_SIZE = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +16,25 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_SIZE) {
+            System.out.println(ERROR_MSG);
+            throw new IllegalArgumentException();
+        }
+
+        if (hasDuplicateNumber(numbers)) {
+            System.out.println(ERROR_MSG);
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean hasDuplicateNumber(List<Integer> numbers) {
+        long size = numbers.stream().distinct().count();
+
+        if (size != numbers.size())
+            return true;
+
+        return false;
+    }
+
+
 }
