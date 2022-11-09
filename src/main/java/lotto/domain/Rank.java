3362d52140a48d7611f49166df870209db5579a6
mckim123 <minchan06@gmail.com>
2022-11-09 23:35:34 +0900

    feat(lotto): Implement enum class for rankings and rename Winnings.java
    Implement enum class for rankings
    - each of them contains winning price and constraints to get that rank
    - isCurrentRank function could judge rank
    Rename Winnings.java to Rank.java

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+public enum Rank {
+    _1등(2000000000, 6, null, null),
+    _2등(30000000, 5, 5, true),
+    _3등(1500000, 5, 5, false),
+    _4등(50000, 4, 4, null),
+    _5등(5000, 3, 3, null),
+    _6등(0, null, null, null);
+
+    final private Integer winning;
+    final private Integer minMatchNum;
+    final private Integer maxMatchNum;
+    final private Boolean isBonusRequired;
+
+    Rank(Integer winning, Integer minMatchNum, Integer maxMatchNum, Boolean isBonusRequired) {
+        this.winning = winning;
+        this.minMatchNum = minMatchNum;
+        this.maxMatchNum = maxMatchNum;
+        this.isBonusRequired = isBonusRequired;
+    }
+
+    public Integer getWinning() {
+        return winning;
+    }
+
+    public boolean isCurrentRank(int correctNumberCount, boolean isBonusCorrect) {
+        if (minMatchNum != null && correctNumberCount < minMatchNum) {
+            return false;
+        }
+        if (maxMatchNum != null && correctNumberCount > maxMatchNum) {
+            return false;
+        }
+        if (isBonusRequired != null && isBonusCorrect != isBonusRequired) {
+            return false;
+        }
+        return true;
+    }
+
+}
