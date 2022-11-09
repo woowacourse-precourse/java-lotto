f194fb243008e0c3b2bfda6365b7eca1c2d6019a
YIMHYEONGSUP <hs.yim@illb.co.kr>
2022-11-09 18:54:23 +0900

    feat - 구입 금액 입력 받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    public static String ERROR_MESSAGE = "[ERROR]";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 구입 금액 입력 받기
+        int account = setAccount();
+        System.out.println("account = " + account);
+
     }
+
+    private static int setAccount() {
+        int account = Integer.parseInt(Console.readLine());
+        return account;
+    }
+
+
+
+
 }
