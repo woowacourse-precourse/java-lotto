d1378ca68cbde092c575c23166628e2b18b143e7
Reznov <reznov03048@gmail.com>
2022-11-09 19:42:23 +0900

    feat: 당첨 번호를 입력받는 기능 구현

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -8,6 +8,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
 
     private final String INPUT_LOTTO_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
 
     public Input() {
     }
@@ -17,4 +18,11 @@ public class Input {
 
         return Integer.parseInt(Console.readLine());
     }
+
+    public String getInputWinningNumber(){
+        System.out.println(INPUT_WINNING_NUMBER);
+
+        return Console.readLine();
+    }
+
 }
