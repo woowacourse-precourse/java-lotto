e3fee183c1a7014b7cacd95c4ac4c0993c7b69a4
whdcks420 <whdcks420@gmail.com>
2022-11-10 01:41:58 +0900

    feat(validator): 구매 금액 유효성 검사

--- src/main/java/lotto/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -2,11 +2,19 @@ package lotto.validator;
 
 public class PurchaseAmountValidator {
     public static void validate(String money) {
-        if (Integer.parseInt(money) % 1000 != 0) {
-            throw new IllegalArgumentException();
+        if (!isDigit(money)) {
+            throw new IllegalArgumentException("[ERROR] 올바른 입력이 아닙니다.");
         }
-        if (money.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException();
+        if (!isCorrectUnit(money)) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해야합니다.");
         }
+    }
+
+    private static boolean isDigit(String money) {
+        return money.chars().allMatch(Character::isDigit);
+    }
+
+    private static boolean isCorrectUnit(String money) {
+        return Integer.parseInt(money) % 1000 == 0;
     }
 }
