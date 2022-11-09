24037af594b9b99a84fcb84b48b60df4be673ae9
seokjin8678 <rnrn8678@naver.com>
2022-11-10 00:38:32 +0900

    feat: Money 클래스 추가

---
+++ src/main/java/lotto/game/Money.java
@@ -0,0 +1,44 @@
+package lotto.game;
+
+public class Money {
+    private final long value;
+
+    private Money(long value) {
+        validate(value);
+        this.value = value;
+    }
+
+    public static Money of(long value) {
+        return new Money(value);
+    }
+
+    private void validate(long value) {
+        if (value % 1000 == 0) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 1000으로 나눠 떨어지는 금액만 입력해주세요.");
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Money money = (Money) o;
+
+        return value == money.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (value ^ (value >>> 32));
+    }
+}
