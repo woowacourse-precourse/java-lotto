b7197567f0091d0e3a1f8c97ad6d797ba61246c4
yg0585 <yg0585@naver.com>
2022-11-09 19:28:51 +0900

    feat: 발행해야 하는 로또 만큼 로또 발행 후 리스트에 저장

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGame {
+
+    private final List<List<Integer>> userLottos = new ArrayList<>();
+
     private int countLotto(int price) {
         return price / 1000;
+    }
+
+    private void issueLotto(int price) {
+        int lottoNum;
+        LottoGenerator lottoGenerator = new LottoGenerator();
+
+        lottoNum = countLotto(price);
+        for (int count = 0; count < lottoNum; count++){
+            List<Integer> lotto = lottoGenerator.createRandomLotto();
+            userLottos.add(lotto);
+        }
     }
 }
