0705f3011e4d1c7578613a36802932088b26a4ce
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-10 01:30:06 +0900

    refactor(lotto): 클래스 이름 수정

--- src/main/java/view/PrintBuyLottoList.java
+++ src/main/java/view/PrintBuyLottoList.java
@@ -2,10 +2,8 @@ package view;
 
 import java.util.List;
 
-public class PrintLotto {
+public class PrintBuyLottoList {
 
-    public PrintLotto() {
-    }
 
     public void printLottoList(int count, List<List<Integer>> printList){
         System.out.println(count + "개 구매하셨습니다.");
