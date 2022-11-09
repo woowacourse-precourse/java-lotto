3cc2d98ff6722a2090b10a57422db34423bcf173
shin5774 <babo5774@naver.com>
2022-11-10 01:08:56 +0900

    feat: add printInputGuide
    -add class Problem
     -add method printInputGuide
     -add Constructor

---
+++ src/main/java/lotto/Problem.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class Problem {
+
+    Problem(){
+        printInputGuide();
+    }
+
+    void printInputGuide(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+}
