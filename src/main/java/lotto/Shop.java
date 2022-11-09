258b170d312c3a7056d1e679e5b0fb56d2183765
ouohoon <begreat@kakao.com>
2022-11-09 20:20:59 +0900

    feat(Shop): 로또 판매 기능 구현

--- src/main/java/lotto/Shop.java
+++ src/main/java/lotto/Shop.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Shop {
@@ -11,8 +12,9 @@ public class Shop {
             "구매 금액은 1,000 단위로 나누어 떨어져야 합니다.";
     private static final String INVALID_STRING_INPUT =
             "숫자만 입력할 수 있습니다.";
+    private static final Integer LOTTO_PRICE = 1000;
     private Integer money;
-    private List<Lotto> lottos;
+    private List<Lotto> lotto;
 
     public void inputMoney() {
         String money = Console.readLine();
@@ -23,7 +25,7 @@ public class Shop {
     private void validate(String money) {
         try {
             Integer nMoney = Integer.parseInt(money);
-            if (nMoney % 1000 != 0) {
+            if (nMoney % LOTTO_PRICE != 0) {
                 LottoError.illegalArgumentException(INVALID_MONEY_INPUT);
             }
         } catch (NumberFormatException ignored) {
@@ -33,6 +35,18 @@ public class Shop {
 
     private List<Integer> createLottoNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    private Lotto createLotto() {
+        return new Lotto(createLottoNumbers());
+    }
+
+    public List<Lotto> sellLotto() {
+        List<Lotto> lotto = new ArrayList<>();
+        for (int i = 0; i < money; i++) {
+            lotto.add(createLotto());
+        }
+        return lotto;
     }
 
 }
