9df0e5d241813285ee55e29ccbdbae90383bb671
HyebinShin <hyeviin.dev@gmail.com>
2022-11-09 19:32:08 +0900

    Feat: 로또 발행 기능: 로또 발행 횟수만큼 반복 시행
    Randoms.pickUniqueNumberInRange()를 이용하여 List<Integer> 를 생성
    오름차순으로 정렬
    해당 리스트를 Lotto 객체 생성자의 매개 변수로 이용
    List<Lotto> 에 해당 Lotto 객체 저장
    유효성 검사 : List<Lotto> 의 size()가 로또 발행 횟수와 같은지

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class Application {
@@ -9,8 +14,14 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println(Print.ASK_BUY);
         int publishNum = getPublishNum();
-        System.out.println("publishNum: "+publishNum);
+        System.out.println(publishNum+Print.ANSWER_BUY);
+        
+        List<Lotto> lotto = getLotto(publishNum);
 
+        System.out.println("lotto 목록");
+        for (Lotto value : lotto) {
+            System.out.println(Arrays.toString(value.getNumbers().toArray(new Integer[0])));
+        }
     }
 
     //로또 구입
@@ -36,5 +47,36 @@ public class Application {
 
     private static int getPublishNum() {
         return getBudget()/1000;
+    }
+    
+    //로또 발행
+    private static List<Lotto> getLotto(int publishNum) {
+        List<Lotto> lotto = new ArrayList<>();
+
+        while(lotto.size() < publishNum) {
+            lotto.add(new Lotto(getLottoNum()));
+        }
+
+        validateLotto(lotto, publishNum);
+
+        return lotto;
+    }
+
+    private static void validateLotto(List<Lotto> lotto, int publishNum) {
+        if(lotto.size()!=publishNum) {
+            throw new IllegalArgumentException("[ERROR] 로또의 발행 횟수와 로또 발행 리스트의 크기는 같아야 합니다.");
+        }
+    }
+
+    private static List<Integer> getLottoNum() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+        sortNumbers(numbers);
+
+        return numbers;
+    }
+
+    private static void sortNumbers(List<Integer> numbers) {
+        numbers.sort(Comparator.naturalOrder());
     }
 }
