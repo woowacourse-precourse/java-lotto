3861dfe154c5b72a2ff834fc6c13f8e9257af56b
HyeonjungYun <guswnd9971@gmail.com>
2022-11-09 20:18:06 +0900

    "feat(View): 로또 당첨 번호를 입력하는 메서드 inputMoney(), 진짜 숫자인지 확인하는 메서드 isRealLottoNumber() 추가"

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -1,10 +1,14 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class View {
     public void ui () {
         inputMoney();
+        Lotto lotto = new Lotto(inputLottoNumber());
     }
 
     private int inputMoney () {
@@ -17,8 +21,38 @@ public class View {
         return Integer.parseInt(money);
     }
 
+    private List<Integer> inputLottoNumber () {
+        String tempLottoNumber;
+        List<Integer> lottoNumber;
+
+        System.out.println("당첨 번호를 입력해주세요.");
+        tempLottoNumber = Console.readLine();
+        isRealLottoNumber(tempLottoNumber);
+        lottoNumber = splitLottoNumber(tempLottoNumber);
+
+        return lottoNumber;
+    }
+
+    private List<Integer> splitLottoNumber (String tempLottoNumber) {
+        List<Integer> lottoNumber = new ArrayList<>();
+
+        for(String element: tempLottoNumber.split(",")) {
+            lottoNumber.add(Integer.parseInt(element));
+        }
+
+        return lottoNumber;
+    }
+
     private void isRealNumber (String inputStatement) {
         for (char element: inputStatement.toCharArray()) {
+            if (element < 49 || element > 57) throw new IllegalArgumentException();
+        }
+    }
+
+    private void isRealLottoNumber (String lottoNumber) {
+        String mergedNumber = String.join(lottoNumber);
+
+        for (char element: mergedNumber.toCharArray()) {
             if (element < 49 || element > 57) throw new IllegalArgumentException();
         }
     }
