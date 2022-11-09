94706f705a7fbd928cb530525ce4af14a1c6bb90
ouohoon <begreat@kakao.com>
2022-11-09 17:05:16 +0900

    feat(Lotto): 당첨 번호 검증 기능 구현
    - 당첨 번호의 숫자 범위는 1~45 사이다.
    - 당첨 번호의 숫자는 중복되지 않는다.
    - 당첨 번호는 숫자 6개다.
    올바르지 않은 입력이라면 에러 메시지를 출력하고 IllegalArugmentException 예외를 발생시킴

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,41 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Lotto {
+
+    private static final String INVALID_RANGE_INPUT = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String INVALID_SIZE_INPUT = "로또 번호는 6개의 숫자여야 합니다.";
+    private static final String DUPLICATED_INPUT = "로또 번호는 중복되지 않습니다.";
+    private static final int SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            LottoError.illegalArgumentException(INVALID_SIZE_INPUT);
+        }
+        Boolean[] isUnique = new Boolean[6];
+        Arrays.fill(isUnique, false);
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                LottoError.illegalArgumentException(INVALID_RANGE_INPUT);
+            }
+            if (isUnique[number - 1]) {
+                LottoError.illegalArgumentException(DUPLICATED_INPUT);
+            }
+            isUnique[number - 1] = true;
         }
     }
 
-    // TODO: 추가 기능 구현
+
 }
