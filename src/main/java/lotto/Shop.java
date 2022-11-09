ff61f28ca81bb24a966de6fee8e6d5069d6f1d04
devmizz <garamminchan@gmail.com>
2022-11-10 01:08:00 +0900

    refactor: Shop 클래스 싱글톤으로 변경
    static method를 지우고 싱글톤으로 변경
    static method를 사용하는 것보다 싱글톤으로 사용하는 게 메모리 상 이익이 있기 때문

--- src/main/java/lotto/Shop.java
+++ src/main/java/lotto/Shop.java
@@ -7,50 +7,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Shop {
-    
-    private static int money;
+
     private static final int LOTTO_PRICE = 1000;
+    private static Shop shop;
 
-    public static List<Lotto> purchase() {
-        List<Lotto> lotteryTickets = new ArrayList<>();
-        
-        System.out.println("구입금액을 입력해 주세요.");
-        getMoney();
+    private Shop() {
+    }
 
-        System.out.println();
-
-        while(money > 0) {
-            lotteryTickets.add(publishLotteryTicket());
-            money -= LOTTO_PRICE;
+    public static Shop getInstance() {
+        if(shop == null) {
+            shop = new Shop();
         }
+
+        return shop;
+    }
+
+    public List<Lotto> purchaseLottery() {
+        List<Lotto> lotteryTickets = publishLotteryTicket(getMoney());
 
         printLotteryTickets(lotteryTickets);
 
         return lotteryTickets;
     }
 
-    private static void getMoney() {
+    private int getMoney() {
+        int money;
+
+        System.out.println("구입금액을 입력해 주세요.");
+
         try {
             money = Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자가 아닙니다");
         }
 
-        validate();
+        validate(money);
+
+        return money;
     }
 
-    private static void validate() {
+    private void validate(int money) {
         if (money % 1000 > 0) {
             throw new IllegalArgumentException("[ERROR] 1,000원 단위로 나뉘지 않습니다");
         }
     }
 
-    private static Lotto publishLotteryTicket() {
-        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    private List<Lotto> publishLotteryTicket(int money) {
+        List<Lotto> lotteryTickets = new ArrayList<>();
+
+        while(money > 0) {
+            lotteryTickets.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+            money -= 1000;
+        }
+
+        return lotteryTickets;
     }
 
-    private static void printLotteryTickets(List<Lotto> lotteryTickets) {
+    private void printLotteryTickets(List<Lotto> lotteryTickets) {
+        System.out.println();
         System.out.println(lotteryTickets.size() + "개를 구매했습니다.");
+
         for(Lotto lotto : lotteryTickets) {
             System.out.println(lotto.getNumbers());
         }
