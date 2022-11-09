8f0d554df225e52af636198e558d72222e4c2f4e
greeng00se <thegreengoose7@gmail.com>
2022-11-10 01:05:02 +0900

    fix(Lotto): LOTTO_NUMBERS_SIZE 상수 public으로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,9 +6,10 @@ import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
-    private static final int LOTTO_NUMBERS_SIZE = 6;
+    public static final int LOTTO_NUMBERS_SIZE = 6;
     private static final String INVALID_SIZE_MESSAGE = "[ERROR] 올바르지 않은 로또 개수입니다.";
     private static final String DUPLICATED_NUMBER_MESSAGE = "[ERROR] 중복된 로또 번호입니다.";
+
     private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
