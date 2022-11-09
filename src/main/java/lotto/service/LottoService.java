3bc181319986a41118886278db354dbd204bc2ed
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-09 21:33:27 +0900

    feature(service) : 로또구매 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,14 +1,34 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
+import lotto.Lotto;
 import lotto.common.Error;
 
 public class LottoService {
 
-    public void buy(String in) {
-        int money = isValid(in);
+    public List<Lotto> buy(String in) {
+        isValid(in);
+        int money = getMoney(in);
+        return getLottos(money);
     }
 
-    private int isValid(String in) {
+    private int getMoney(String in) {
+        return Integer.parseInt(in);
+    }
+
+    private List<Lotto> getLottos(int money) {
+        List<Lotto> lottos = new ArrayList<>();
+        int numberOfLotto = money /1000;
+        for (int i = 0; i < numberOfLotto; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
+
+    private void isValid(String in) {
         int i = 0;
         try {
             i = Integer.parseInt(in);
@@ -18,6 +38,5 @@ public class LottoService {
         if (i % 1000 != 0) {
             throw new IllegalArgumentException(Error.THOUSAND.getMsg());
         }
-        return i;
     }
 }
