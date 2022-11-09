267957853e74c4cdd12bd19bf153234edfa2144a
ghchoi <ghchoi0427@gmail.com>
2022-11-09 20:58:04 +0900

    feat: Validation 생성

---
+++ src/main/java/lotto/validation/Validation.java
@@ -0,0 +1,16 @@
+package lotto.validation;
+
+public class Validation {
+
+    public void validateWinNumberInput() {
+
+    }
+
+    public void validateNumberInput() {
+
+    }
+
+    public void validateBonusNumberInput() {
+
+    }
+}
