ff0e03a6347d027e68dacb15c2a56c4d36327dfb
cmong0516 <cmong0516@gmail.com>
2022-11-09 18:10:35 +0900

    feat(CustomerClass): validWantBuyAmountInput()
    Customer 가 입력한 구매를 원하는 금액을 검증하는 메서드

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,15 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoShop lottoShop = new LottoShop();
+        Customer customer = new Customer();
+
+        lottoShop.requestWantBuyAmountInputMessage();
+        try {
+            customer.wantBuyAmountInput();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 1000 단위 이상의 숫자를 입력해주세요.");
+        }
+
     }
 }
