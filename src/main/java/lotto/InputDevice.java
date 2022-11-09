4eb6787bed049a302165262b5a4d9f61249025df
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-09 20:23:02 +0900

    feat: 로또 당첨번호 입력 기능
    InputDevice - sendWinningNumbers method 생성

--- src/main/java/lotto/InputDevice.java
+++ src/main/java/lotto/InputDevice.java
@@ -1,11 +1,21 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputDevice {
-    
+
     public int sendMoneyToBuyLotto() {
         String money = Console.readLine();
         return Integer.parseInt(money);
+    }
+
+    public List<Integer> sendWinningNumbers() {
+        String input = Console.readLine();
+        return Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
