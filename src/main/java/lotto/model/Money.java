2160a5756f7d4f33e04c18e9d81b4873e6df5918
jiwon1217 <koagjw@gmail.com>
2022-11-10 00:43:11 +0900

    feat: 양수만 입력 가능하도록 예외처리

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -1,19 +1,27 @@
 package lotto.model;
 
 public class Money {
-    public static final String INVALID_MONEY_TYPE = "[ERROR] 금액은 숫자여야 합니다.";
+    private static final String INVALID_MONEY_TYPE = "[ERROR] 금액은 숫자여야 합니다.";
+    private static final String MONEY_MUST_NOT_NEGATIVE = "[ERROR] 금액은 음수일 수 없습니다.";
     private final int money;
 
     public Money(String money) {
-        validate(money);
+        isValidMoneyType(money);
+        isPositive(money);
         this.money = Integer.parseInt(money);
     }
 
-    private void validate(String money) {
+    private void isValidMoneyType(String money) {
         try {
             Integer.parseInt(money);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_MONEY_TYPE);
+        }
+    }
+
+    private void isPositive(String money) {
+        if (Integer.parseInt(money) < 0) {
+            throw new IllegalArgumentException(MONEY_MUST_NOT_NEGATIVE);
         }
     }
 }
