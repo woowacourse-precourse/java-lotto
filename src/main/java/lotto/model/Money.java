0c016e937522505c1e3a8c64ffc0d187cdc67be5
jiwon1217 <koagjw@gmail.com>
2022-11-10 00:54:50 +0900

    feat: 1,000원 단위로 나누어 떨어지는 금액만 입력 가능하도록 예외처리

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -3,11 +3,13 @@ package lotto.model;
 public class Money {
     private static final String INVALID_MONEY_TYPE = "[ERROR] 금액은 숫자여야 합니다.";
     private static final String MONEY_MUST_NOT_NEGATIVE = "[ERROR] 금액은 음수일 수 없습니다.";
+    private static final String MONEY_MUST_MULTIPLE_OF_THOUSAND = "[ERROR] 금액은 1,000원 단위로 나누어 떨어져야 합니다.";
     private final int money;
 
     public Money(String money) {
         isValidMoneyType(money);
         isPositive(money);
+        isDivideByThousand(money);
         this.money = Integer.parseInt(money);
     }
 
@@ -22,6 +24,12 @@ public class Money {
     private void isPositive(String money) {
         if (Integer.parseInt(money) < 0) {
             throw new IllegalArgumentException(MONEY_MUST_NOT_NEGATIVE);
+        }
+    }
+
+    private void isDivideByThousand(String money) {
+        if (Integer.parseInt(money) % 1000 != 0) {
+            throw new IllegalArgumentException(MONEY_MUST_MULTIPLE_OF_THOUSAND);
         }
     }
 }
