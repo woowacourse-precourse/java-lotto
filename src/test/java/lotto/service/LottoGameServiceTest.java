c923e1c59550bd7415c48302a0925175f5b011b5
ksh-beginner <ohk9134@naver.com>
2022-11-10 01:40:25 +0900

    test(LottoGameServiceTest) : 로또 발행 개수를 반환하는 기능 테스트

--- src/test/java/lotto/service/LottoGameServiceTest.java
+++ src/test/java/lotto/service/LottoGameServiceTest.java
@@ -1,6 +1,27 @@
 package lotto.service;
 
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
 class LottoGameServiceTest {
 
+    LottoGameService lottoGameService;
+
+    @BeforeEach
+    void generateLottoGameService() {
+        lottoGameService = new LottoGameService();
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {10000, 20000, 35000, 1000})
+    void 로또_발행_개수_반환(int lottoPurchaseAmount) {
+        int lottoIssueCount = lottoGameService.getLottoIssueCount(lottoPurchaseAmount);
+
+        assertThat(lottoIssueCount).isEqualTo(lottoPurchaseAmount / 1000);
+    }
 }
