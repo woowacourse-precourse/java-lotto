07ed6cb5f7405cf58b36dbd97593526645508a46
JJ <jeongjae1996@naver.com>
2022-11-10 05:29:46 +0900

    feat: 로또 구입 금액 형식이 맞지 않는 경우, IllegalArgumentException이 발생하도록 구현
    - 숫자가 아닌 문자가 있는 경우 예외 처리
    - 숫자가 1,000원 단위로 입력되지 않은 경우 예외 처리

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -2,8 +2,21 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Pattern;
+
 public class Money {
     private String getMoney() {
         return Console.readLine();
+    }
+
+    private void validate(String money) {
+        final String MONEY_PATTERN = "^[1-9][0-9]*0{3}$";
+        final String EXCEPTION_MESSAGE = "[ERROR] 숫자가 잘못된 형식입니다.";
+
+        boolean isMatch = Pattern.matches(MONEY_PATTERN, money);
+
+        if (!isMatch) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
+        }
     }
 }
