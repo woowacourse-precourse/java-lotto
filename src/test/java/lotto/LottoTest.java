1d283b5f6199079c471b3bfa8810b505fed0de50
sangwon <swchoi1997@naver.com>
2022-11-10 08:57:39 +0900

    test: 로또 번호 유효범위 내 생성 여부 확인 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,14 @@
 package lotto;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,4 +27,12 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @DisplayName("로또 번호에 범위에 벗어나는 숫자가 있으면 예외가 발생한다. : 1보다 작은 수")
+    @Test
+    void createLottoByOutBoundNumber() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new Lotto(List.of(0, 1, 2, 3, 4, 5)));
+        assertThat(exception.getMessage()).isEqualTo("[ERROR] Lotto Number Have Out Bound Number.");
+    }
 }
