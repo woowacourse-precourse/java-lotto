5e8418df49aac372d70b54a12b8b44b5f0bb59e8
Reznov <reznov03048@gmail.com>
2022-11-09 21:38:21 +0900

    feat: 입력받는 기능에 static 추가

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -7,26 +7,26 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class Input {
 
-    private final String INPUT_LOTTO_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    private final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
-    private final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String INPUT_LOTTO_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public Input() {
     }
 
-    public int getInputPurchaseAmount() {
+    public static int getInputPurchaseAmount() {
         System.out.println(INPUT_LOTTO_PURCHASE_AMOUNT);
 
         return Integer.parseInt(Console.readLine());
     }
 
-    public String getInputWinningNumber() {
+    public static String getInputWinningNumber() {
         System.out.println(INPUT_WINNING_NUMBER);
 
         return Console.readLine();
     }
 
-    public int getInputBonusNumber() {
+    public static int getInputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER);
 
         return Integer.parseInt(Console.readLine());
