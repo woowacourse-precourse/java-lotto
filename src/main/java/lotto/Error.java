12e3eb00894a1eb1f1bc0bd8f29206c7bedbcce5
ym2629 <dydals2629@naver.com>
2022-11-09 13:52:35 +0900

    feat: check duplication in winng number

--- src/main/java/lotto/Error.java
+++ src/main/java/lotto/Error.java
@@ -1,9 +1,25 @@
 package lotto;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Error {
     static void purchasePrice(int purchasePrice) {
         if (purchasePrice % LottoConst.LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구매 금액을 정확히 입력 해 주세요. 금액 : " + purchasePrice);
+        }
+    }
+
+    static void duplicationWinnigNumber(List<Integer> winnigNumber) {
+        List<Integer> removedDuplicationNumber =
+                winnigNumber.stream()
+                        .distinct()
+                        .collect(Collectors.toList());
+
+        if (removedDuplicationNumber.size() != 6) {
+            throw new IllegalArgumentException("당첨번호에 중복된 숫자가 있습니다.");
         }
     }
 }
