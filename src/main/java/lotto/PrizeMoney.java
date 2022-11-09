9d03e0bf087e73b8f8e6f24fee9617dba2c21516
ouohoon <begreat@kakao.com>
2022-11-09 16:31:11 +0900

    feat(PrizeMoney): 등수에 맞는 당첨 상금 리턴
    Enum 클래스로 등수에 맞는 당첨 상금을 리턴한다

--- src/main/java/lotto/PrizeMoney.java
+++ src/main/java/lotto/PrizeMoney.java
@@ -1,2 +1,19 @@
-package lotto;public enum PrizeMoney {
+package lotto;
+
+public enum PrizeMoney {
+    ONE(2000000000),
+    TWO(30000000),
+    THREE(1500000),
+    FOUR(50000),
+    FIVE(5000);
+
+    private final Integer prizeMoney;
+
+    PrizeMoney(Integer prizeMoney) {
+        this.prizeMoney = prizeMoney;
+    }
+
+    public Integer getPrizeMoney() {
+        return prizeMoney;
+    }
 }
