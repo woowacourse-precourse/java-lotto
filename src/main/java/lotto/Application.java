c9a84918d6f303607f537a80da2c14e5ab72292b
Eungyu-code <Agyu625@gmail.com>
2022-11-09 19:47:48 +0900

    feat(Application class): printLottoQuantity 함수 추가
    발행한 로또 수량을 출력하는 함수
    - "n개를 구매했습니다." 문구 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,4 +4,9 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public static void printLottoQuantity(int quantity) {
+
+        System.out.println(quantity + "개를 구매했습니다.");
+    }
 }
