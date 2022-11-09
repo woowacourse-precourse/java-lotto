7b56d24e76d86e2642fa46aa008e85074bb3ed65
ksh-beginner <ohk9134@naver.com>
2022-11-10 02:38:10 +0900

    feat(LottoGameService) : 로또 구입 금액이 숫자가 아닌 경우 검증 구현

--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -1,10 +1,14 @@
 package lotto.service;
 
+import java.util.regex.Pattern;
+
 public class LottoGameService {
 
     private static final int A_LOTTO_PRICE = 1000;
     private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
     private static final String LOTTO_PURCHASE_AMOUNT_DIVIDE_REST_EXCEPTION_MESSAGE = "로또 구입 금액은 1,000원으로 나누어 떨어져야합니다.";
+    private static final String LOTTO_PURCHASE_AMOUNT_NOT_DIGIT_EXCEPTION_MESSAGE = "로또 구입 금액은 숫자여야합니다.";
+    private static final String DIGIT_REGEX = "^[0-9]+$";
 
     public int getLottoIssueCount(String lottoPurchaseAmount) {
         validateLottoIssueCount(lottoPurchaseAmount);
@@ -12,6 +16,9 @@ public class LottoGameService {
     }
 
     public void validateLottoIssueCount(String lottoPurchaseAmount) {
+        if (isLottoPurchaseAmountNotDigit(lottoPurchaseAmount)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_PREFIX + LOTTO_PURCHASE_AMOUNT_NOT_DIGIT_EXCEPTION_MESSAGE);
+        }
         if (isRestOfLottoPurchaseAmountDivideBy1000NotZero(lottoPurchaseAmount)) {
             throw new IllegalArgumentException(ERROR_MESSAGE_PREFIX + LOTTO_PURCHASE_AMOUNT_DIVIDE_REST_EXCEPTION_MESSAGE);
         }
@@ -19,5 +26,9 @@ public class LottoGameService {
 
     private boolean isRestOfLottoPurchaseAmountDivideBy1000NotZero(String lottoPurchaseAmount) {
         return Integer.parseInt(lottoPurchaseAmount) % A_LOTTO_PRICE != 0;
+    }
+
+    private boolean isLottoPurchaseAmountNotDigit(String lottoPurchaseAmount) {
+        return !Pattern.compile(DIGIT_REGEX).matcher(String.valueOf(lottoPurchaseAmount)).matches();
     }
 }
