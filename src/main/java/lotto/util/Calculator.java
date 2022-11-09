34d4e2232fd706c3a10aa0144103c68a4eb3af3a
jinny-l <jinny.l.dev@gmail.com>
2022-11-09 22:17:41 +0900

    feat(Calculator): 로또 추첨 번호와 일치하는 로또 번호 개수를 반환하는 기능 구현

--- src/main/java/lotto/util/Calculator.java
+++ src/main/java/lotto/util/Calculator.java
@@ -1,6 +1,20 @@
 package lotto.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
+
+    public static int countMatchingNumbers(List<Integer> winningNumbers, List<Integer> playerNumbers) {
+        List<Integer> intersection = new ArrayList<>(winningNumbers);
+        intersection.removeAll(playerNumbers);
+
+        if (intersection.isEmpty()) {
+            return 0;
+        }
+
+        return intersection.size();
+    }
 
     public static long getProfitRates(int purchaseAmount, int winningAmount) {
         return winningAmount / purchaseAmount;
