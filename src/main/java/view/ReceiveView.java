15efd1fc1e3e7d90681c464d54ae4e3f2bb07010
gyuturn <rbtjrrbals@gmail.com>
2022-11-10 00:31:05 +0900

    fix($view): return값 String으로 변경
    isDigit 예외체크 함수를 사용하기 위함

--- src/main/java/view/ReceiveView.java
+++ src/main/java/view/ReceiveView.java
@@ -1,12 +1,13 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import util.exception.Valid;
 
 public class ReceiveView {
 
     public static int purchaseAmount() {
         String money = Console.readLine();
-        int purchaseMoney = Integer.parseInt(money);
-        return purchaseMoney;
+        Valid.purchaseMoney(money);
+        return Integer.parseInt(money);
     }
 }
