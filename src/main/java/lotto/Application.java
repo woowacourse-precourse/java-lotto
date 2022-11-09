6fd397f5965553be523f8c6db27ca56f70e81c3c
sumin-Heo <102477741+sumin-Heo@users.noreply.github.com>
2022-11-09 22:47:48 +0900

    feat : 1,000원으로 나누어 떨어지지 않는 경우 예외 처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int cost = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        cost = sc.nextInt();
+        CostRight(cost);
     }
+    public static void CostRight(int cost){
+        if(cost%1000 != 0)
+            throw new IllegalArgumentException("[ERROR] 입력 금액 오류");
+    }
+
+
 }
