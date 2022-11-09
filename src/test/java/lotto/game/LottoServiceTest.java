7c03162363e09179900e7edb761f8037e4c4ad31
seokjin8678 <rnrn8678@naver.com>
2022-11-10 00:40:57 +0900

    test: 테스트 코드 수정 및 추가

--- src/test/java/lotto/game/LottoServiceTest.java
+++ src/test/java/lotto/game/LottoServiceTest.java
@@ -26,12 +26,24 @@ class LottoServiceTest {
     }
 
     @Test
+    @DisplayName("입력된 금액이 1000으로 나눠떨어지지 않으면 예외가 발생한다")
+    void createMoneyByNotDivisible1000() {
+        // given
+        String input = "12345";
+        assertThatThrownBy(() -> lottoService.convertInputToMoney(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 1000으로 나눠 떨어지는 금액만 입력해주세요.");
+    }
+
+    @Test
     @DisplayName("입력된 금액이 정상적이면 돈을 반환한다.")
     void createMoney() {
         // given
         String input = "10000";
+        Money money = Money.of(10000);
+
         // expect
         assertThat(lottoService.convertInputToMoney(input))
-                .isEqualTo(10000);
+                .isEqualTo(money);
     }
 }
