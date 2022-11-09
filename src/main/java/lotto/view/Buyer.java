fd45bcdbb223147e4d36133dee63fe6e48dd73d8
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-09 22:27:05 +0900

    feat : 구입할 로또 금액을 입력하는 메서드

--- src/main/java/lotto/view/Buyer.java
+++ src/main/java/lotto/view/Buyer.java
@@ -1,6 +1,23 @@
 package lotto.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.view.ErrorMessage.ERROR_BLANK;
+import static lotto.view.ErrorMessage.ERROR_LOTTO_LENGTH;
+import static lotto.view.ErrorMessage.ERROR_PRICE;
+import static lotto.view.Message.BONUS_NUMBER_INPUT;
+import static lotto.view.Message.PURCHASE_COUNT;
+import static lotto.view.Message.PURCHASE_PRICE;
+import static lotto.view.Message.WINNING_NUMBER_INPUT;
+
 public class Buyer {
+    static int purchaseLottoCount;
+    static String winningNumber;
+    static String bonusNumber;
 
-
+    void purchaseLotto() {
+        System.out.println(PURCHASE_PRICE);
+        String purchasePrice = readLine();
+        purchaseLottoCount = Integer.parseInt(purchasePrice);
+        System.out.println(purchaseLottoCount/1000 + PURCHASE_COUNT);
+    }
 }
