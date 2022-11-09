86d4ab5979760da7c0e581813c0a69400d2526f8
noah.lee <rhkd2612@naver.com>
2022-11-10 04:49:53 +0900

    docs: lotto game methods

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,16 @@
 package lotto;
 
+/*
+ * [필요 기능 명세]
+ * [X] 랜덤 로또 번호 생성
+ * [X] 당첨 로또 번호 입력
+ * [X] 당첨 통계 계산
+ * [X] 총 수익률 계산
+ * [X] 입력 예외 처리
+ * [X] 단위 테스트 생성
+ * [X] 테스트 확인
+ */
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
