d8fce54df1afdd1ba8ade842cc89ad2cd2858770
ksh-beginner <ohk9134@naver.com>
2022-11-10 00:09:54 +0900

    feat(Lotto) : 발행한 로또 번호 개수가 6개가 아닌 경우 검증 기능에 예외 메세지 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,8 +1,14 @@
 package lotto.model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
+
+    private static final int CORRECT_NUMBERS_SIZE = 6;
+    private static final String ERROR_MESSAGE_PREFIX = "[ERROR] : ";
+    private static final String NUMBERS_SIZE_NOT_SIX_EXCEPTION_MESSAGE = "발행 로또 번호의 개수는 6개여야합니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,11 +18,11 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (isNumbersSizeNotSix(numbers)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MESSAGE_PREFIX + NUMBERS_SIZE_NOT_SIX_EXCEPTION_MESSAGE);
         }
     }
 
     private boolean isNumbersSizeNotSix(List<Integer> numbers) {
-        return numbers.size() != 6;
+        return numbers.size() != CORRECT_NUMBERS_SIZE;
     }
 }
