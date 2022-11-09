306f0649029fb3aab6f570395db835f7358bc94e
jiwon1217 <koagjw@gmail.com>
2022-11-10 00:31:59 +0900

    feat: 숫자만 입력 가능하도록 예외처리

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -1,9 +1,19 @@
 package lotto.model;
 
 public class Money {
+    public static final String INVALID_MONEY_TYPE = "[ERROR] 금액은 숫자여야 합니다.";
     private final int money;
 
-    public Money(int money) {
-        this.money = money;
+    public Money(String money) {
+        validate(money);
+        this.money = Integer.parseInt(money);
+    }
+
+    private void validate(String money) {
+        try {
+            Integer.parseInt(money);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_MONEY_TYPE);
+        }
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,10 +3,10 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    public static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
 
-    public static int inputMoney() {
+    public static String inputMoney() {
         System.out.println(INPUT_MONEY);
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 }
