c23de75b7bb4406ee5aa4666cfba32a135a1330b
jinny-l <jinny.l.dev@gmail.com>
2022-11-10 00:29:50 +0900

    feat(Controller): player에게 당첨 번호를 입력받고 유효성 검증을 하는 기능 구현

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.List;
 import lotto.model.LottoGenerator;
 import lotto.model.Player;
 import lotto.model.Service;
@@ -35,5 +36,33 @@ public class Controller {
         }
     }
 
+    // TODO: 구매 정보를 출력하는 기능 구현
+
+    private void guessLottoNumbers() {
+        View.printInputLottoNumber();
+        List<String> lottoNumbers = player.guessLottoNumbers();
+
+        for (String lottoNumber : lottoNumbers) {
+            if (!InputVerifier.isInteger(lottoNumber)) {
+                View.printNotInteger();
+                throw new IllegalArgumentException();
+            }
+        }
+
+        if (!InputVerifier.isValidSize(lottoNumbers)) {
+            View.printNotLottoSize();
+            throw new IllegalArgumentException();
+        }
+
+        if (!InputVerifier.isUniqueNumber(lottoNumbers)) {
+            View.printNotUniqueNumber();
+            throw new IllegalArgumentException();
+        }
+
+        if (!InputVerifier.isValidNumber(lottoNumbers)) {
+            View.printNotLottoPrice();
+            throw new IllegalArgumentException();
+        }
+    }
 
 }
