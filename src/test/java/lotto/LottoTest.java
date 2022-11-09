729a0b5baa053d44c32a95d4d6aaba6a39720be1
K-Diger <kitten.diger@gmail.com>
2022-11-09 23:16:42 +0900

    test : 로또 번호 중복 검증 테스트케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,13 @@
 package lotto;
 
 import lotto.lotteryshop.Lotto;
-import lotto.validator.UserInputValidator;
+import lotto.lotteryshop.Store;
+import lotto.validator.ValueValidator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -28,24 +31,48 @@ class LottoTest {
     @DisplayName("로또 당첨번호 예외 1 ~ 45 사이의 수가 아닌 경우")
     @Test
     void 로또당첨번호입력받기_실패1() {
-        String[] inputValue = {"1", "2", "0", "4", "5", "6"};
-        assertThatThrownBy(() -> UserInputValidator.validateInputLottoNumber(inputValue))
+        List<Integer> inputValue = List.of(1, 2, 0, 4, 5, 6);
+        assertThatThrownBy(() -> ValueValidator.validateInputLottoNumber(inputValue))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 당첨번호 예외 1 ~ 45 사이의 수가 아닌 경우")
     @Test
     void 로또당첨번호입력받기_실패1_2() {
-        String[] inputValue = {"1", "2", "3", "46", "5", "6"};
-        assertThatThrownBy(() -> UserInputValidator.validateInputLottoNumber(inputValue))
+        List<Integer> inputValue = List.of(1, 2, 3, 4, 46, 6);
+        assertThatThrownBy(() -> ValueValidator.validateInputLottoNumber(inputValue))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 당첨번호 예외 중복된 수가 입력된 경우")
     @Test
     void 로또당첨번호입력받기_실패2() {
-        String[] inputValue = {"1", "2", "3", "4", "1", "6"};
-        assertThatThrownBy(() -> UserInputValidator.validateInputLottoNumber(inputValue))
+        List<Integer> inputValue = List.of(1, 2, 3, 4, 1, 6);
+        assertThatThrownBy(() -> ValueValidator.validateInputLottoNumber(inputValue))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 당첨번호 예외 중복된 수가 입력된 경우")
+    @Test
+    void 로또당첨번호입력받기_실패2_2() {
+        List<Integer> inputValue = List.of(1, 2, 3, 4, 6, 6);
+        assertThatThrownBy(() -> ValueValidator.validateInputLottoNumber(inputValue))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 당첨번호 예외 중복된 수가 입력된 경우")
+    @Test
+    void 로또당첨번호입력받기_실패2_3() {
+        List<Integer> inputValue = List.of(6, 2, 3, 4, 5, 6);
+        assertThatThrownBy(() -> ValueValidator.validateInputLottoNumber(inputValue))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 당첨번호 예외 중복된 수가 입력된 경우")
+    @Test
+    void 로또당첨번호입력받기_실패2_4() {
+        List<Integer> inputValue = List.of(6, 6, 3, 4, 5, 1);
+        assertThatThrownBy(() -> ValueValidator.validateInputLottoNumber(inputValue))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -53,7 +80,7 @@ class LottoTest {
     @Test
     void 로또구매금액_실패_1() {
         String lottoPurchaseMoney = "0";
-        assertThatThrownBy(() -> UserInputValidator.validateLottoPurchaseMoney(lottoPurchaseMoney))
+        assertThatThrownBy(() -> ValueValidator.validateLottoPurchaseMoney(lottoPurchaseMoney))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -61,7 +88,7 @@ class LottoTest {
     @Test
     void 로또구매금액_실패_1_2() {
         String lottoPurchaseMoney = "1001";
-        assertThatThrownBy(() -> UserInputValidator.validateLottoPurchaseMoney(lottoPurchaseMoney))
+        assertThatThrownBy(() -> ValueValidator.validateLottoPurchaseMoney(lottoPurchaseMoney))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -69,7 +96,36 @@ class LottoTest {
     @Test
     void 로또구매금액_실패_2() {
         String lottoPurchaseMoney = "2147483648";
-        assertThatThrownBy(() -> UserInputValidator.validateLottoPurchaseMoney(lottoPurchaseMoney))
+        assertThatThrownBy(() -> ValueValidator.validateLottoPurchaseMoney(lottoPurchaseMoney))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구매한 로또 갯수 출력")
+    @Test
+    void 구매한로또갯수출력() {
+        System.out.println("구매금액을 입력해 주세요.");
+        int inputValue = 110000;
+        System.out.printf("%d개를 구매했습니다.", inputValue / 1000);
+    }
+
+    @DisplayName("로또 번호 생성")
+    @Test
+    void 로또번호생성() {
+        Store store = new Store();
+        int purchaseNumber = 9;
+        List<List<Integer>> generatedLottoPaper = store.generateLottoNumber(purchaseNumber);
+        Assertions.assertThat(generatedLottoPaper.size()).isEqualTo(purchaseNumber);
+    }
+
+    @DisplayName("로또 번호 출력")
+    @Test
+    void 로또번호출력() {
+        Store store = new Store();
+        int purchaseNumber = 9;
+        List<List<Integer>> generatedLottoPaper = store.generateLottoNumber(purchaseNumber);
+        // store.getPublishedLottoNumber();
+        for (List<Integer> eachLottoPaper : generatedLottoPaper) {
+            System.out.println(Arrays.toString(eachLottoPaper.toArray()));
+        }
     }
 }
