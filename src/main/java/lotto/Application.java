b421e5a4a644251e9db12060efca7f7837bf8a1b
이한솔 <hansol8701@naver.com>
2022-11-09 20:22:28 +0900

    refactor: 어플리케이션 메인 함수 InputHandler를 통해 입력받도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,5 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -12,13 +8,10 @@ public class Application {
     public static void main(String[] args) {
         User user = new User();
         NumberGenerator numberGenerator = new NumberGenerator();
-        List<Integer> winNumbers = new ArrayList<>();
+
         InputHandler inputHandler = new InputHandler();
-        int bonusNumber;
 
-        System.out.println("구입금액을 입력해 주세요.");
-        int cash = Integer.parseInt(Console.readLine());
-
+        int cash = inputHandler.inputPurchaseAmount();
         int numOfLotto = user.getNumberOfAvailableLotto(cash);
         for (int i = 0; i < numOfLotto; i++) {
             List<Integer> randomNumbers = numberGenerator.generateSixRandomNumbers();
@@ -28,13 +21,8 @@ public class Application {
         System.out.printf("\n%d개를 구매했습니다.", numOfLotto);
         user.showWholeLotto();
 
-        System.out.println("당첨 번호를 입력해 주세요.");
-        for (int i = 0; i < 6; i++) {
-            int number = Integer.parseInt(Console.readLine());
-            winNumbers.add(number);
-        }
-        System.out.println("\n보너스 번호를 입력해 주세요.");
-        bonusNumber = Integer.parseInt(Console.readLine());
+        List<Integer> winNumbers = inputHandler.inputWinNumbers();
+        int bonusNumber = inputHandler.inputBonusNumber();
 
         StatisticsAnalyzer statisticsAnalyzer = new StatisticsAnalyzer(winNumbers, bonusNumber);
         System.out.println("당첨 통계\n---\n");
