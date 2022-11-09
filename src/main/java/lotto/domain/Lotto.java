db2b57da9a1a8374a6aea3b0b1e989db2fa0a799
HyeonJun0530 <ksc6203@gmail.com>
2022-11-10 00:13:58 +0900

    feat(Lotto): 중복된 숫자, 6개가 아닌 값 예외처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,17 +1,28 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicateValidate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void duplicateValidate(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if(set.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
