e6864006050268c2a07a37e20053e31c3caa39e1
sangwon <swchoi1997@naver.com>
2022-11-10 08:47:52 +0900

    feat: 로또 번호 중복확인 기능 추가

--- src/main/java/lotto/LottoValidator.java
+++ src/main/java/lotto/LottoValidator.java
@@ -5,6 +5,7 @@ import static lotto.LottoGenerator.COUNT;
 import static lotto.LottoGenerator.END_INCLUSIVE;
 import static lotto.LottoGenerator.START_INCLUSIVE;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -18,11 +19,20 @@ public class LottoValidator {
 
     public static void validate(List<Integer> numbers){
         checkSize(numbers);
+        checkDuplication(numbers);
     }
 
     private static void checkSize(List<Integer> numbers) throws IllegalArgumentException {
         if (numbers.size() != COUNT) {
-            throw new IllegalArgumentException(COMMON_MESSAGE + LOTTE_SIZE_ERROR.getMessage() + numbers.size());
+            throw new IllegalArgumentException(COMMON_MESSAGE + LOTTE_SIZE_INVALID.getMessage() + numbers.size());
         }
     }
+
+    private static void checkDuplication(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != numbers.size()) {
+            throw new IllegalArgumentException(COMMON_MESSAGE + LOTTE_NUMBER_DUPLICATION.getMessage());
+        }
+    }
+
+
 }
