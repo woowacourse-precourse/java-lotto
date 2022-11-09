788ad0204b405ed5a1c4632c4d0882953295c9b0
sangwon <swchoi1997@naver.com>
2022-11-10 01:12:06 +0900

    test: 로또번호 여섯자리 확인
    로또번호 여섯자리 인지 확인하는 테스트케이스

---
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import static lotto.LottoGenerator.COUNT;
+import static lotto.LottoGenerator.END_INCLUSIVE;
+import static lotto.LottoGenerator.START_INCLUSIVE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LottoGeneratorTest {
+
+    private static final List<Integer> STANDARD_LOTTO_NUMBER = IntStream.rangeClosed(START_INCLUSIVE, END_INCLUSIVE)
+            .boxed().collect(Collectors.toList());
+    LottoGenerator lottoGenerator;
+    List<Integer> lottoNumber;
+
+
+    @BeforeEach
+    void setUp() {
+        lottoGenerator = LottoGenerator.getInstance();
+        lottoNumber = lottoGenerator.makeLotto();
+    }
+
+    @Test
+    void 랜덤_번호_여섯자리_생성_확인() {
+        assertThat(lottoNumber.size()).isEqualTo(COUNT);
+    }
+
+
+}
