134f9103d2accedeb89c930cec29a8ba4fe0a0b9
cmong0516 <cmong0516@gmail.com>
2022-11-09 18:28:53 +0900

    refactor(LottoShopClass): Rename LottoShop to LottoMachine

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,15 +3,20 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        LottoShop lottoShop = new LottoShop();
+        LottoMachine lottoMachine = new LottoMachine();
         Customer customer = new Customer();
 
-        lottoShop.requestWantBuyAmountInputMessage();
+        lottoMachine.requestWantBuyAmountInputMessage();
+
+        int wantBuyAmountInput = 0;
+
         try {
-            customer.wantBuyAmountInput();
+            wantBuyAmountInput = customer.wantBuyAmountInput();
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 1000 단위 이상의 숫자를 입력해주세요.");
         }
+
+        int canBuyAmount = lottoMachine.canBuyAmount(wantBuyAmountInput);
 
     }
 }
--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,6 +1,6 @@
 package lotto;
 
-public class LottoShop {
+public class LottoMachine {
     public void requestWantBuyAmountInputMessage() {
         System.out.println("구입금액을 입력해 주세요.");
     }
