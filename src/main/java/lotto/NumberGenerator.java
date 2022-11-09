8662a049c31f4c1f28eb9acbe9f16629f1b621ab
Hayoon <hy97@sju.ac.kr>
2022-11-09 21:01:33 +0900

    feat(NumberGenerator): 구매금액 만큼 로또발행수량 계산 기능 메서드 생성

--- src/main/java/lotto/NumberGenerator.java
+++ src/main/java/lotto/NumberGenerator.java
@@ -7,9 +7,12 @@ public class NumberGenerator {
     public NumberGenerator() {
     }
 
+    public int calculatorLottoNumber(int purchasePrice) {
+        return purchasePrice / Constant.PRICE_UNIT;
+    }
+
     public int enterPurchasePrice() {
         String purchasePrice = Console.readLine();
-
         return Integer.parseInt(purchasePrice);
     }
 }
