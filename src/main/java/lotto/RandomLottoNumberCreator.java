818779b246e406d1227ba611dbaf54cb96695dc9
heesang <dlagmltkd12@naver.com>
2022-11-11 02:53:32 +0900

    feat: 주어진 개수 만큼 6개의 로또 번호들을 만드는 기능

---
+++ src/main/java/lotto/RandomLottoNumberCreator.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.LottoValidationUtils.*;
+
+public class RandomLottoNumberCreator {
+
+    public static final int CRITERION_ZERO = 0;
+
+    public static List<Integer> createLottoNumber() {
+        return Randoms.pickUniqueNumbersInRange(MINIMUM_LOTTO_NUMBER, MAXIMUM_LOTTO_NUMBER, CRITERION_LOTTO_SIZE);
+    }
+
+    public static List<List<Integer>> createLottoNumber(int count) {
+        List<List<Integer>> lottoNumbers = new ArrayList<>();
+        while (count --> CRITERION_ZERO) {
+            lottoNumbers.add(createLottoNumber());
+        }
+        return lottoNumbers;
+    }
+}
--- src/test/java/lotto/RandomNumbersCreatorTest.java
+++ src/test/java/lotto/RandomNumbersCreatorTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
@@ -12,11 +13,17 @@ public class RandomNumbersCreatorTest {
     @DisplayName("주어진 범위 안에 6개의 랜덤 로또 번호를 만드는 기능")
     @RepeatedTest(50)
     void createLottoNumberInRange() {
-        List<Integer> lottoNumbers = RandomNLottoNumberCreator.createLottoNumber();
+        List<Integer> lottoNumbers = RandomLottoNumberCreator.createLottoNumber();
         assertThat(lottoNumbers.size()).isEqualTo(6);
         for (int number : lottoNumbers) {
             assertThat(number >= 1 && number <= 45).isTrue();
         }
     }
 
+    @DisplayName("주어진 범위 안에서 주어진 개수 만큼 6개의 랜덤 로또 번호들을 만드는 기능")
+    @Test
+    void createLottoNumbersInRange() {
+        List<List<Integer>> lottoNumbers = RandomLottoNumberCreator.createLottoNumber(4);
+        assertThat(lottoNumbers.size()).isEqualTo(4);
+    }
 }
