3a0416d0aa7ba2397545863dd1a444d575b11470
Jin409 <sualng123@gmail.com>
2022-11-10 19:05:31 +0800

    feat(NumberValidator, ErrorMessages): 로또 번호 관련 유효성 검사 기능 구현

---
+++ src/main/java/lotto/validator/ErrorMessages.java
@@ -0,0 +1,8 @@
+package lotto.validator;
+
+public class ErrorMessages {
+    public static final String INVALID_LENGTH_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개의 숫자로 이루어져야 합니다.";
+    public static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복되지 않아야 합니다.";
+    public static final String INVALID_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다";
+    public static final String INVALID_TYPE_ERROR_MESSAGE = "[ERROR] 로또 번호는 정수여야 합니다.";
+}
