9c8a0110e09a59163ca1889d0c6daa189a64224d
hodomaroo <enghemdwkd@gmail.com>
2022-11-10 23:08:12 +0900

    [Feat] : Lotto 클래스의 각 기능별 메서드 Frame 선언 완료
    Lotto 클래스의 각 기능을 구현하는 메서드의 Frame을 선언하였습니다.
    
    유효성 검증을 담당하는 validate 메서드부터 추후 구현 예정입니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 한장의 로또 티켓 정보를 관리하는 클래스
@@ -12,12 +16,42 @@ public class Lotto {
         validate(numbers);
         this.numbers = numbers;
     }
-
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if(!validateConsistOfSixNumber(numbers)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자로 이루어져야 합니다.");
+        }
+
+        if(!validateEachNumberInRange(numbers)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 각 숫자는 1~45 사이의 값을 가져야 합니다.");
+        }
+
+        if(!validateEachNumberIsDistinct(numbers)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 숫자들은 중복되어서는 안됩니다.");
         }
     }
 
+    private Boolean validateEachNumberInRange(List<Integer> numbers){
+        return null;
+    }
+
+    private Boolean validateConsistOfSixNumber(List<Integer> numbers){
+        return null;
+    }
+
+    private Boolean validateEachNumberIsDistinct(List<Integer> numbers){
+        return null;
+    }
     // TODO: 추가 기능 구현
+
+    public Integer getCountOfMatchNumber(Lotto otherLotto){
+        return null;
+    }
+
+    public Boolean hasNumber(Integer number){
+        return null;
+    }
+
+    public String toString(){
+        return null;
+    }
 }
