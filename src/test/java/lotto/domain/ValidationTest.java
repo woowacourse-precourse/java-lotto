1c062dc76abafa77ee006380c0b97e2e96175a1f
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-10 16:31:19 +0900

    test : 로또 번호 조건 불일치 판별 메서드 검증 테스트 구현

--- src/test/java/lotto/domain/ValidationTest.java
+++ src/test/java/lotto/domain/ValidationTest.java
@@ -14,14 +14,14 @@ public class ValidationTest {
 	@ParameterizedTest
 	@ValueSource(strings={"1,000", "10l", "ABab"})
 	void VerifyNotANumberReturningTrue(String input) {
-		assertThat(Validation.isNotANumber(input)).isFalse();
+		assertThat(Validation.isNotANumber(input)).isTrue();
 	}
 
 	@DisplayName("유저가 입력한 구입금액이 숫자로만 이루어져 있는지 검증한다")
 	@ParameterizedTest
 	@ValueSource(strings={"123", "10000000"})
 	void VerifyNotANumberReturningFalse(String input) {
-		assertThat(Validation.isNotANumber(input)).isTrue();
+		assertThat(Validation.isNotANumber(input)).isFalse();
 	}
 
 	@DisplayName("유저가 입력한 구입금액이 1000원 단위인지 검증한다")
@@ -44,7 +44,34 @@ public class ValidationTest {
 		assertTrue(Validation.isLessThan1000("500"));
 		assertFalse(Validation.isLessThan1000("1000"));
 		assertFalse(Validation.isLessThan1000("22000"));
+	}
 
+	@DisplayName("유저가 입력한 당첨번호가 형식에 맞게 입력되었는지를 검증한다")
+	@Test
+	void VerifyIsCorrectlyFormatted(){
+		assertTrue(Validation.isCorrectlyFormatted("1,2"));
+		assertTrue(Validation.isCorrectlyFormatted("1,2,30,a,b,c"));
+	}
+
+	@DisplayName("유저가 입력한 당첨번호가 6개인지를 검증한다")
+	@Test
+	void VerifyIsCountCorrectlyProvided(){
+		assertTrue(Validation.isCountCorrectlyProvided("1,2,5,10,20,30"));
+	}
+
+	@DisplayName("유저가 입력한 당첨번호가 1이상 45이하의 자연수인지를 검증한다")
+	@Test
+	void VerifyIsCorrectlyRanged(){
+		assertTrue(Validation.isCorrectlyRanged("1,2,3,4,5,45"));
+		assertFalse(Validation.isCorrectlyRanged("1,2,3,4,5,46"));
+		assertFalse(Validation.isCorrectlyRanged("0,2,3,4,5,45"));
+	}
+
+	@DisplayName("유저가 입력한 당첨번호에 중복이 존재하는지를 검증한다")
+	@Test
+	void VerifyIsDuplicateExists(){
+		assertTrue(Validation.isDuplicateExists("1,1,3,4,5,45"));
+		assertFalse(Validation.isDuplicateExists("1,2,3,4,5,45"));
 	}
 
 }
