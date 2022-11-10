62f9bfce890042e91fda2bf4a325ec60a15282a2
Eundms <dms011221@gmail.com>
2022-11-10 15:07:33 +0900

    feat(LottoIssueMachine): 자동,수동 예측 번호 생성

---
+++ src/main/java/lotto/LottoIssueMachine.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoIssueMachine {//로또 번호 발행 기계
+    public Lotto autoGenerate(){
+        List<Integer> numbers=new ArrayList<>();
+        while(numbers.size()<6){
+            int randomNumber = Randoms.pickNumberInRange(1, 45);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return new Lotto(numbers);
+    }
+    public Lotto manualGenerate(List<Integer> numbers){
+        return new Lotto(numbers);
+    }
+}
