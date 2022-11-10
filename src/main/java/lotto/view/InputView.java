e53d84877d238aa547f85837fe75e5b43709d18a
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-11-10 22:53:49 +0900

    fix(lotto): 에러 메세지 출력 결과 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String INPUT_PRICE_AMOUNT = "구입금액을 입력해 주세요.";
-    private static final String DATA_TYPE_ERR_MESSAGE = "숫자가 아닙니다.";
+    private static final String DATA_TYPE_ERR_MESSAGE = "[ERROR] 숫자가 아닙니다.";
 
     public InputView() {
     }
