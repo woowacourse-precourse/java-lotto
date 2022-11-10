9baa986216283b095e497a1c05000736f23c7371
youngkyunIm <yee0230@naver.com>
2022-11-11 00:50:58 +0900

    test(Money): print error message

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,17 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import lotto.constants.ErrorMessage;
+import lotto.model.Money;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            new Money(readLine());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
