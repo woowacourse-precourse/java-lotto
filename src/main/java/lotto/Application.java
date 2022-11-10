c43b3298f63c71fbadec88f0017e7f07ad2fba91
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-10 21:33:18 +0900

    feat: 에러를 받기 위한 try-catch 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,9 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         startMessage();
-        Money money = new Money(Console.readLine());
+        try {
+            Money money = new Money(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] "+e.getMessage());
+        }
     }
 
     public static void startMessage() {
