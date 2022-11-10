a78f8d141cdc843abfa8f4d4c81f46e8fe8813ba
juhwang <juhong4930@naver.com>
2022-11-10 23:13:40 +0900

    feat(Lotto): find out duplication between win numbers and lotto numbers

---
+++ src/main/java/store/Lotto.java
@@ -0,0 +1,33 @@
+package store;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        List<Integer> lottoNumbers = new ArrayList<>(numbers);
+        Collections.sort(lottoNumbers);
+        this.numbers = lottoNumbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // TODO: 추가 기능 구현
+    public String toString() {
+        return String.valueOf(numbers);
+    }
+
+    public int compareWinNumbers(List<Integer> winNumbers) {
+        List<Integer> copyNumbers = new ArrayList<>(numbers);
+        copyNumbers.retainAll(winNumbers);
+        return copyNumbers.size();
+    }
+}
