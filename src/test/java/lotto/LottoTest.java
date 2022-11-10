1d1faeaedd09b5d8ec20e78044017b79dd0def36
rt3310 <btac3310@gmail.com>
2022-11-10 19:04:47 +0900

    test: 숫자가 아닌 값이 입력됐을 시 예외처리하는 기능 테스트 코드 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,9 +3,11 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +25,28 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("숫자가 아닌 다른 글자가 들어가면 예외가 발생한다")
+    @Test
+    void createMoneyByNotNumber() throws NoSuchMethodException {
+        LottoGame lottoGame = new LottoGame();
+
+        Method method = lottoGame.getClass().getDeclaredMethod("validateMoneyType", String.class);
+        method.setAccessible(true);
+
+        assertThatThrownBy(() -> method.invoke(lottoGame, "a1000"))
+                .getCause()
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자가 들어가면 예외가 발생하지 않는다")
+    @Test
+    void createMoneyByNumber() throws NoSuchMethodException {
+        LottoGame lottoGame = new LottoGame();
+
+        Method method = lottoGame.getClass().getDeclaredMethod("validateMoneyType", String.class);
+        method.setAccessible(true);
+
+        assertThatNoException()
+                .isThrownBy(() -> method.invoke(lottoGame, "1000"));
+    }
 }
