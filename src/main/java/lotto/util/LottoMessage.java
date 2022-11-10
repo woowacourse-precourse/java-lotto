d7593f232dca15a3e59fba5f36075fee19ed73d0
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-10 22:36:52 +0900

    feat<InputView>: 유저 구입금액 입력 받는 함수 구현
    - LottoMessage 구입금액 입력 메세지 추가
    - 메세지 출력
    - 유저 입력 받기

--- src/main/java/lotto/util/LottoMessage.java
+++ src/main/java/lotto/util/LottoMessage.java
@@ -1,4 +1,15 @@
 package lotto.util;
 
 public enum LottoMessage {
+    PURCHASE_PRICE("구입금액을 입력해 주세요.");
+
+    private final String message;
+
+    LottoMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,15 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.util.LottoMessage;
+
 public class InputView {
+    public String inputPurchasePrice() {
+        System.out.println(LottoMessage.PURCHASE_PRICE.getMessage());
+        return getUserInput();
+    }
+
+    private String getUserInput() {
+        return Console.readLine();
+    }
 }
