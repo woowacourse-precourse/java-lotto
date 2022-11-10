a6922fa707067803748938af7bad0e49e8f91495
minju412 <ln8847@ajou.ac.kr>
2022-11-11 04:17:04 +0900

    feat(Application): update code to work
    - enter purchase cost
    
    - draw lottery numbers
      depending on the purchase cost entered
    
    - enter winning numbers
    
    - enter bonus numbers
    
    - print winning statistics
    
    - print earnings rate

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,58 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.Calculator;
 import lotto.domain.LotteryDrawMachine;
+import lotto.domain.Lotto;
+import lotto.util.InputValidator;
+import lotto.view.Output;
 
 public class Application {
+
     public static void main(String[] args) {
         LotteryDrawMachine lotteryDrawMachine = new LotteryDrawMachine();
-        System.out.println(lotteryDrawMachine.drawLotteryNumbers().toString());
+
+        Output.enterPurchaseCost();
+        int purchaseCost = Integer.parseInt(Console.readLine());
+        if (!InputValidator.checkPurchaseCost(purchaseCost)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 구매 금액입니다.");
+        }
+
+        int purchaseCount = purchaseCost / 1000;
+        Output.purchaseCountNotification(purchaseCount);
+
+        List<List<Integer>> groupOfUserLotteryNumbers = new ArrayList<>();
+
+        for (int count = 0; count < purchaseCount; count++) {
+            List<Integer> lotteryNumbers = lotteryDrawMachine.drawLotteryNumbers();
+            Lotto lotto = new Lotto(lotteryNumbers);
+            groupOfUserLotteryNumbers.add(lotteryNumbers);
+        }
+
+        Output.enterWinningNumbers();
+        List<String> list = Arrays.asList(Console.readLine().split(","));
+        List<Integer> winningNumbers = list.stream()
+                .map(winningNumber -> Integer.parseInt(winningNumber))
+                .collect(Collectors.toList());
+        if (!InputValidator.checkWinningNumbers(winningNumbers)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 당첨 번호입니다.");
+        }
+
+        Output.enterBonusNumber();
+        int bonusNumber = Integer.parseInt(Console.readLine());
+
+        Output.winningStatistics();
+        Calculator calculator = new Calculator();
+        List<Integer> matchResults = calculator.calculateMatchResults(groupOfUserLotteryNumbers, winningNumbers,
+                bonusNumber);
+        Output.printWinningStatistics(matchResults);
+
+        int profit = calculator.calculateProfit(matchResults);
+        double earningsRate = calculator.calculateEarningsRate(purchaseCost, profit);
+        Output.earningsRateNotification(earningsRate);
     }
 }
