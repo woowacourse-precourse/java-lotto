d5ef9a9035ae1d8482b3340bfe8518127ec84ada
sunhokim1 <srrsr14@gmail.com>
2022-11-11 05:44:27 +0900

    feat : input구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,8 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputUtil inputUtil = new InputUtil();
+        Lotto lotto = new Lotto(inputUtil.listNumber());
+        System.out.println(lotto);
     }
 }
---
+++ src/main/java/lotto/InputUtil.java
@@ -0,0 +1,13 @@
+package  lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputUtil {
+    
+    public String inputNumber() {
+        String inputNumbers = readLine();
+        return inputNumbers;
+    }
+}
