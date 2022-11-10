4c49cbd5e19921cb7e096e5ee1c16a0e9c14c342
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-10 14:07:47 +0900

    style(Lotto) : 요구사항 작성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,14 @@
 package lotto.domain;
 
+/*
+ * 1. 제공된 Lotto 클래스를 활용해 구현해야 한다.
+ * 2. Lotto에 매개 변수가 없는 생성자를 추가할 수 없다.
+ * 3. numbers의 접근 제어자인 private을 변경할 수 없다.
+ * 4. Lotto에 필드(인스턴스 변수)를 추가할 수 없다.
+ * 5. Lotto의 패키지 변경은 가능하다.
+ */
+
+
 import java.util.List;
 
 public class Lotto {
@@ -15,6 +24,5 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
-
     // TODO: 추가 기능 구현
 }
