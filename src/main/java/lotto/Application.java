eeef043c8fb0aafc64a4a2206f276a91db58c405
kimdahee0815 <diana815@naver.com>
2022-11-10 18:19:31 +0900

    feat(lotto) : 로또 개수만큼 매번 6개의 랜덤 넘버 추출하여 리스트에 담아 전체 리스트 하나에 추가하는 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,19 +1,22 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        
+        NumberGenerator numbergenerator = new NumberGenerator();
+        List<ArrayList<Integer>> lottonumbersall = new ArrayList<ArrayList<Integer>>();
+        lottonumbersall = numbergenerator.createRandomLotto(lottocount);
     }
     
     public static void askTotalPrice() {
-    	
+    
     }
     
-    public static void askNumbers() {
-    	
+    public static int askNumbers() {
+    	int lottocount = 0;
+    	return lottocount;
     }
     
     public void lottoValidateNumber() {
--- src/main/java/lotto/NumberGenerator.java
+++ src/main/java/lotto/NumberGenerator.java
@@ -1,8 +1,18 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class NumberGenerator {
-	public void createRandomLotto() {
-		
+	public List<List<Integer>> createRandomLotto(int lottocount) {
+		List<List<Integer>> lottonumbersall = new ArrayList<List<Integer>>();
+		for(int i = 0; i < lottocount; i++) {
+			List<Integer> lottonumbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+			lottonumbersall.add(lottonumbers);
+		}
+		return lottonumbersall;
 	}
 	
 	public void ascendingOrder() {
