ba9efb3433b1d3d15d8bace5aedf7496b82c0974
rt3310 <btac3310@gmail.com>
2022-11-10 19:16:37 +0900

    feat: 1000원 단위가 아닐 때 나오는 에러 메세지 Enum 생성

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -1,7 +1,8 @@
 package lotto;
 
 public enum ErrorMessage {
-    INVALID_MONEY_TYPE("숫자만 입력 가능합니다");
+    INVALID_MONEY_TYPE("숫자만 입력 가능합니다"),
+    INVALID_UNIT_OF_MONEY("1000원 단위로만 입력 가능합니다");
 
     private String message;
 
