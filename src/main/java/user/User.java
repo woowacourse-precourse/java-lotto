fe3fb2f06f314c4941f4e40984362843fdab0e1c
ParkJoungWoo <jug78@naver.com>
2022-11-11 03:13:38 +0900

    feat(user/User.java) : User 클래스 작성
    사용자 클래스의 기능을 추가했습니다.
    
    - 수익률을 계산하는 기능
    - 사용자의 상금을 받는 기능
    - 상금의 유효성을 검증하는 기능
    - 사용자의 로또들을 받는 기능

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,38 +1,69 @@
 package user;
 
 import lotto.Lotto;
+
 import java.util.List;
 
 public class User {
     private final boolean NOT_VALID = false;
+    private final int BASE_MONEY = 1000;
     private List<Lotto> lottos = null;
     private int seedMoney = 0;
     private int prizeMoney = 0;
-    public User(int money){
-        if(isMoneyValid() == NOT_VALID){
+
+    public User(int money) {
+        if (isMoneyNumeric(money) == NOT_VALID) {
+            throw new IllegalArgumentException();
+        }
+        if (isMoneyThousandMultiple(money) == NOT_VALID) {
             throw new IllegalArgumentException();
         }
         this.seedMoney = money;
     }
-    private boolean isMoneyValid(){
-        if(isMoneyNotNull(seedMoney)==NOT_VALID){
+
+    private boolean isMoneyNumeric(int money) {
+        if (isMoneyNotNull(money) == NOT_VALID) {
             throw new IllegalArgumentException();
         }
-        if(isMoneyPositive(seedMoney)==NOT_VALID){
-            throw new IllegalArgumentException();
-        }
-        if(isMoneyThousandMultiple(seedMoney)==NOT_VALID){
+        if (isMoneyPositive(money) == NOT_VALID) {
             throw new IllegalArgumentException();
         }
         return true;
     }
-    private boolean isMoneyNotNull(Integer money){
+
+    private boolean isMoneyNotNull(Integer money) {
         return !money.equals(null);
     }
-    private boolean isMoneyPositive(int money){
+
+    private boolean isMoneyPositive(int money) {
         return money > 0;
     }
-    private boolean isMoneyThousandMultiple(int money){
-        return (money % 1000 == 0) && (money / 1000 > 0);
+
+    private boolean isMoneyThousandMultiple(int money) {
+        return (money % BASE_MONEY == 0) && (money / BASE_MONEY > 0);
+    }
+
+    public void setLottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public void setPrizeMoney(int prizeMoney) {
+        if (isMoneyNumeric(prizeMoney) == NOT_VALID) {
+            throw new IllegalArgumentException();
+        }
+        this.prizeMoney = prizeMoney;
+    }
+    public String getRatio(){
+        isMoneyNumeric(seedMoney);
+        isMoneyNumeric(prizeMoney);
+        return String.format("%.2f",(double)prizeMoney/(double)seedMoney);
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
     }
 }
