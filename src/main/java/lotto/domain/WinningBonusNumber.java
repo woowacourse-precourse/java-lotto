bfeba048af3926edd3557615cf8b32f770321604
Reznov <reznov03048@gmail.com>
2022-11-10 22:46:14 +0900

    feat: 당첨 번호의 숫자들이 유효한 범위 안에 있는지 확인하는 기능 구현

--- src/main/java/lotto/domain/WinningBonusNumber.java
+++ src/main/java/lotto/domain/WinningBonusNumber.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -28,6 +29,15 @@ public class WinningBonusNumber {
 
     private void validateNumbersLength(List<Integer> winningNumbers) {
         if (winningNumbers.size() != Number.LOTTO_NUMBER_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumbersRange(List<Integer> winningNumbers) {
+        int min = winningNumbers.stream().sorted().findFirst().get();
+        int max = winningNumbers.stream().sorted(Comparator.reverseOrder()).findFirst().get();
+
+        if(min < Number.LOTTO_NUMBER_MIN || max > Number.LOTTO_NUMBER_MAX) {
             throw new IllegalArgumentException();
         }
     }
