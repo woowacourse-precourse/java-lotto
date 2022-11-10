46cb56ece5fac3a3af88c2625eb4aefa9407ebd5
jungle <minuet1215@jbnu.ac.kr>
2022-11-10 14:54:25 +0900

    Docs: 구현 기능 목록 작성

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -7,13 +7,24 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public List<Integer> getLotto() {
+        return this.numbers;
     }
 
     // TODO: 추가 기능 구현
