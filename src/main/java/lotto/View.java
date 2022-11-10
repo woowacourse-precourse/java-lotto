ef8c9d0fd2ed680a730993ba3933e7d0cd548adc
KimSangHyeok <tkdgur0906@naver.com>
2022-11-11 00:05:27 +0900

    Feat(lotto): 당첨번호를 입력받는 기능

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -2,21 +2,32 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class View {
-    public int askPrice(){
+    public int askPrice() {
         System.out.println("구입금액을 입력해 주세요.");
         String price = Console.readLine();
         return Integer.parseInt(price);
     }
 
-    public void buyLottoList(int price, List<Lotto> lottoBundle){
+    public void buyLottoList(int price, List<Lotto> lottoBundle) {
         System.out.println(price / 1000 + "개를 구매했습니다.");
         for (Lotto lotto : lottoBundle) {
             lotto.getNumbers().sort(Comparator.naturalOrder());
             System.out.println(lotto.getNumbers().toString());
         }
+    }
+
+    public List<Integer> askWinningNumber() {
+        List<Integer> winningLotto = new ArrayList<>();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        for (int count = 0; count < 6; count++) {
+            String number = Console.readLine();
+            winningLotto.add(Integer.parseInt(number));
+        }
+        return winningLotto;
     }
 }
