5a8a50a8bd08c2ec715063d2536da91b1f96376f
lee <whddlsk123@naver.com>
2022-11-11 01:30:33 +0900

    refactor(InputData) : 메소드 접근자를 public -> private 변경

--- src/main/java/lotto/ui/InputData.java
+++ src/main/java/lotto/ui/InputData.java
@@ -7,13 +7,13 @@ import java.util.Scanner;
 public class InputData {
     private static Scanner scanner = new Scanner(System.in);
 
-    public String readInput() {
+    private String readInput() {
         String userInput = scanner.nextLine();
 
         return userInput;
     }
 
-    public String readPurchaseAmount() {
+    private String readPurchaseAmount() {
         System.out.println(Messages.INPUT_MONEY.getMessage());
         String amount = scanner.nextLine();
 
@@ -21,7 +21,7 @@ public class InputData {
     }
 
     //예외사항 검사
-    public List<String> readWinningNumber() {
+    private List<String> readWinningNumber() {
         System.out.println(Messages.INPUT_LOTTE_NUMBER.getMessage());
         String userInput = readInput();
 
@@ -31,7 +31,7 @@ public class InputData {
     }
 
     //예외사항 검사
-    public String readBonusNumber() {
+    private String readBonusNumber() {
         System.out.println(Messages.INPUT_BONUS_NUMBER);
         String userInput = readInput();
 
