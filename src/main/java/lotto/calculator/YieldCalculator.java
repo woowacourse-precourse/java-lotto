3286b0a5f5fbcc1af212d2e81fec2ea52713cac3
K-Diger <kitten.diger@gmail.com>
2022-11-10 17:34:36 +0900

    feat : getStatistics 메서드 구현 - 당첨 통계를 출력하는 메서드 구현 완료

---
+++ src/main/java/lotto/calculator/YieldCalculator.java
@@ -0,0 +1,14 @@
+package lotto.calculator;
+
+import java.util.List;
+
+public class YieldCalculator {
+
+    public void getStatistics(List<Integer> winCount) {
+        System.out.println("당첨 통계\n---");
+        System.out.printf("3개 일치 (5,000원) - %d개\n", winCount.get(0));
+        System.out.printf("4개 일치 (50,000원) - %d개\n", winCount.get(1));
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", winCount.get(2));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", winCount.get(3));
+    }
+}
