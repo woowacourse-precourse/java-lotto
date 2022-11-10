95940c69a77303b2b7aab242b4b2245749f4e6a5
jjongwa <troas96@naver.com>
2022-11-11 01:10:59 +0900

    refactor: 금액 입력
    test에 용이하도록 input함수를 MyMoney 클래스에서 분리해 Application.java로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    static int inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String myMoney = Console.readLine();
+        MyMoney money = new MyMoney(myMoney);
+        System.out.println();
+        return money.getAmount();
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int purchase = inputMoney();
+
     }
 }
+
--- src/main/java/lotto/MyMoney.java
+++ src/main/java/lotto/MyMoney.java
@@ -1,43 +1,33 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class MyMoney {
     private final int amount;
 
-    public MyMoney() {
-        this.amount = inputMoney();
+    public MyMoney(String myMoney) {
+        checkInputMoney(myMoney);
+        this.amount = Integer.parseInt(myMoney);
     }
 
-    private int checkIsNumber(String myMoney){
-        try{
+    private int checkIsNumber(String myMoney) {
+        try {
             return Integer.parseInt(myMoney);
-        }
-        catch (NumberFormatException exception){
+        } catch (NumberFormatException exception) {
             throw new IllegalArgumentException();
         }
     }
 
-    private void checkIsDivide(int moneyNum){
-        if(moneyNum % 1000 != 0){
+    private void checkIsDivide(int moneyNum) {
+        if (moneyNum % 1000 != 0) {
             throw new IllegalArgumentException();
         }
     }
 
-    private void checkInputMoney(String myMoney){
+    private void checkInputMoney(String myMoney) {
         int moneyNum = checkIsNumber(myMoney);
         checkIsDivide(moneyNum);
     }
 
-    private int inputMoney(){
-        System.out.println("구입금액을 입력해 주세요.");
-        String myMoney = Console.readLine();
-        checkInputMoney(myMoney);
-        System.out.println();
-        return Integer.parseInt(myMoney);
-    }
-
-    public int getAmount(){
+    public int getAmount() {
         return this.amount;
     }
 }
