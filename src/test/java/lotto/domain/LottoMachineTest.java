259ecc6f062e1e0cd68d50d28f31f79d2f9c7e1d
HwangSeokJin <ghkd3703@gmail.com>
2022-11-10 22:19:00 +0900

    test(LottoMachine): 구입 개수가 정확한지 테스트하는 코드 작성

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Test;
 import lotto.domain.LottoMachine;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoMachineTest {
+    LottoMachine machine = new LottoMachine();
     @DisplayName("구매 금액 입력 시 숫자가 아닌 값을 입력하면 예외 발생")
     @Test
     void InputNonNumericValue() {
-        LottoMachine machine = new LottoMachine();
         assertThatThrownBy(() -> machine.printLottoBundle("1000k"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -18,8 +19,28 @@ class LottoMachineTest {
     @DisplayName("구입 금액이 1000원으로 나누어 떨어지지 않으면 예외 발생")
     @Test
     void InputNonDivisibleNumber() {
-        LottoMachine machine = new LottoMachine();
         assertThatThrownBy(() -> machine.printLottoBundle("1001"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구입 개수가 정확한지 테스트")
+    @Test
+    void TestNumberOfPurchases() {
+        String[] input = {
+                "1000",
+                "3000",
+                "7000",
+                "12000"
+        };
+        int[] results = {
+                1,
+                3,
+                7,
+                12
+        };
+        for (int i = 0; i < input.length; i++) {
+            int count = machine.printLottoBundle(input[i]).size();
+            assertThat(count).isEqualTo(results[i]);
+        }
     }
 }
