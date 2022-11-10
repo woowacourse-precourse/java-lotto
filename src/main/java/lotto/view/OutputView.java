988dd923752ce542a44af3a2490dd9a0993e2a97
mckim123 <minchan06@gmail.com>
2022-11-10 16:21:29 +0900

    feat(lotto): implement lotto printing method
    Create printLottos method
    : print lottoCount and lottos as required format

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import java.text.DecimalFormat;
+import java.util.List;
+import lotto.domain.Lotto;
 import lotto.domain.LottoResult;
 import lotto.domain.Rank;
 
@@ -18,5 +20,15 @@ public class OutputView {
         df = new DecimalFormat(YIELD_PATTERN);
         sb.append(String.format("총 수익률은 %s%%입니다.", df.format(yield)));
         System.out.println(sb.toString());
+    }
+
+
+    public static void printLottos(int count, List<Lotto> lottos) {
+        System.out.println((String.format("%d개를 구매했습니다.", count)));
+        sb = new StringBuilder();
+        for (Lotto lotto : lottos) {
+            sb.append(lotto.getLotto());
+            sb.append("\n");
+        }
     }
 }
