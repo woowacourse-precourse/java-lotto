e74919d6f24ad3d6a6fe8afc4e242e5a1d126934
jungle <minuet1215@jbnu.ac.kr>
2022-11-10 15:50:04 +0900

    Test(BuyAmountValidatorTest): 구입 금액에 대한 테스트 추가

--- src/test/java/lotto/Validator/BuyAmountValidatorTest.java
+++ src/test/java/lotto/Validator/BuyAmountValidatorTest.java
@@ -1,30 +1,30 @@
 package lotto.Validator;
 
-import lotto.validator.InputValidator;
+import lotto.validator.BuyAmountValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class ValidatorTest {
+public class BuyAmountValidatorTest {
     @DisplayName("유효하지 않은 구입 금액 입력시 예외가 발생한다.")
     @Test
     void createByInvalidInput() {
-        assertThatThrownBy(() -> new InputValidator().validateInvalidNumber("1000j"))
+        assertThatThrownBy(() -> new BuyAmountValidator().validateInvalidNumber("1000j"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("1000으로 나누어떨어지지 않을 경우 예외가 발생한다.")
     @Test
     void validateIsDivideBy1000() {
-        assertThatThrownBy(() -> new InputValidator().validateDivideBy1000(999))
+        assertThatThrownBy(() -> new BuyAmountValidator().validateDivideBy1000(999))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("유효하지 않은 구입 금액 입력시 예외가 발생한다.")
+    @DisplayName("구입 금액이 음수일 경우 예외가 발생한다.")
     @Test
     void createByNegativeNumber() {
-        assertThatThrownBy(() -> new InputValidator().validatePositiveNumber(-1))
+        assertThatThrownBy(() -> new BuyAmountValidator().validatePositiveNumber(-1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
