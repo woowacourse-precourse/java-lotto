da95d775f4a0572bacf6d54c1fc93bd9d045e1eb
eora21 <eora21@naver.com>
2022-11-10 13:01:29 +0900

    test: 💍 추가 정수 확인 테스트들

--- src/test/java/lotto/ValidateTest.java
+++ src/test/java/lotto/ValidateTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidateTest {
@@ -14,5 +15,22 @@ class ValidateTest {
     public void NULL_테스트() {
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> validate.validNull(null));
         assertThat(exception.getMessage()).contains(Invalid.NULL.toString());
+    }
+
+    @Test
+    public void 정수_테스트_빈문자() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> validate.validInt(""));
+        assertThat(exception.getMessage()).contains(Invalid.NOT_INT.toString());
+    }
+
+    @Test
+    public void 정수_테스트_문자() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> validate.validInt("12L"));
+        assertThat(exception.getMessage()).contains(Invalid.NOT_INT.toString());
+    }
+
+    @Test
+    public void 정수_테스트_숫자() {
+        assertThatCode(() -> validate.validInt("123")).doesNotThrowAnyException();
     }
 }
