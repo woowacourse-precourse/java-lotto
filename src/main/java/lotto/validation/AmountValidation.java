73722fd73ebca3ba28cf0480975cab14fc75a77c
ykmxxi <ykmxxi97@gmail.com>
2022-11-10 18:10:14 +0900

    feat: 로또 구입 금액 입력 예외처리 기능 메소드 skeleton code 구현

--- src/main/java/lotto/validation/AmountValidation.java
+++ src/main/java/lotto/validation/AmountValidation.java
@@ -1,4 +1,22 @@
 package lotto.validation;
 
 public class AmountValidation {
+    public AmountValidation() {
+    }
+
+    public void isNull(String amount) {
+
+    }
+
+    public void isNumber(String amount) {
+
+    }
+
+    public void isZero(String amount) {
+
+    }
+
+    public void hasChanges(String amount) {
+
+    }
 }
