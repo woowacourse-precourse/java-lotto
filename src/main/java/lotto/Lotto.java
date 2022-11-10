74acc1e2c2956142130f9322008fbf5b38ed0d62
penrose15 <seongju99@naver.com>
2022-11-10 18:25:55 +0900

    feat : add Lotto domain for numbers that player choose

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,12 @@
 package lotto;
 
+import lotto.constant.LottoNumbers;
+import lotto.exception.ExceptionPhrase;
+
 import java.util.List;
+
+import static lotto.constant.LottoNumbers.LOTTO_LENGTH;
+import static lotto.exception.ExceptionPhrase.EXCEPTION_DUPLICATE_NUMBER;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,8 +17,15 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_LENGTH.getNumber()) {
             throw new IllegalArgumentException();
+        }
+        duplicate(numbers);
+    }
+    private void duplicate(List<Integer> numbers) {
+        int numbersLength = (int) numbers.stream().distinct().count();
+        if(numbers.size() != numbersLength) {
+            throw new IllegalArgumentException(EXCEPTION_DUPLICATE_NUMBER.getMessage());
         }
     }
 
