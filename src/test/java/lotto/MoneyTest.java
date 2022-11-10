c760975afe3bac13b2826448a3480259bd7382d5
KimJaeSung <behappy0916@naver.com>
2022-11-11 02:03:38 +0900

    docs: LottoTest에 있던 Money Test를 MoneyTest 테스트클래스로 옮김

---
+++ src/test/java/lotto/MoneyTest.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import lotto.domain.Money;
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+    @Test
+    void 숫자가_아닌_입력_체크(){
+        assertThatThrownBy(() -> new Money("ㅁㄴㅇ"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void 나누어_지지_않는_금액_체크(){
+        assertThatThrownBy(() -> new Money("999"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 금액_음수_체크(){
+        assertThatThrownBy(() -> new Money("-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
