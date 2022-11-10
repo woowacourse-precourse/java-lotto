107b670b33b3ccdc0bcd8d7e7020b89146292847
minju412 <ln8847@ajou.ac.kr>
2022-11-11 00:32:38 +0900

    feat: calculate match results
    - It is divided into the case where the match count is 5
      and the case where it is not.
      - because if the match count is 5, bonus number is also considered

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -1,12 +1,61 @@
 package lotto.domain;
 
+import static lotto.constant.SystemValue.FIVE_MATCHES_WITHOUT_BONUS_INDEX;
+import static lotto.constant.SystemValue.FIVE_MATCHES_WITH_BONUS_INDEX;
+import static lotto.constant.SystemValue.FOUR_MATCHES_INDEX;
+import static lotto.constant.SystemValue.SIX_MATCHES_INDEX;
+import static lotto.constant.SystemValue.THREE_MATCHES_INDEX;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Calculator {
 
-    public List<Integer> calculateMatchResults() {
-        // TODO: 당첨 결과 반환 (3개부터 6개까지 일치하는 개수)
-        return null;
+    public List<Integer> calculateMatchResults(List<List<Integer>> groupOfUserLotteryNumbers,
+            List<Integer> winningNumbers, int bonusNumber) {
+        Comparator comparator = new Comparator();
+        List<Integer> matchResults = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0));
+        int matchCount = 0;
+        for (List<Integer> userLotteryNumbers : groupOfUserLotteryNumbers) {
+            matchCount = comparator.getMatchCount(userLotteryNumbers, winningNumbers);
+            if (matchCount == 5) {
+                setMatchResultsWithBonusNumber(bonusNumber, comparator, matchResults, userLotteryNumbers);
+                continue;
+            }
+            if (matchCount >= 3) {
+                setMatchResults(matchCount, matchResults);
+            }
+        }
+        return matchResults;
+    }
+
+    private void setMatchResults(int matchCount, List<Integer> matchResults) {
+        int index = getIndex(matchCount);
+        matchResults.set(index, matchResults.get(index) + 1);
+    }
+
+    private int getIndex(int matchCount) {
+        if (matchCount == 3) {
+            return THREE_MATCHES_INDEX;
+        }
+        if (matchCount == 4) {
+            return FOUR_MATCHES_INDEX;
+        }
+        return SIX_MATCHES_INDEX;
+    }
+
+    private void setMatchResultsWithBonusNumber(int bonusNumber, Comparator comparator, List<Integer> matchResults,
+            List<Integer> userLotteryNumbers) {
+        int index = getIndexWithBonusNumber(bonusNumber, userLotteryNumbers, comparator);
+        matchResults.set(index, matchResults.get(index) + 1);
+    }
+
+    private int getIndexWithBonusNumber(int bonusNumber, List<Integer> userLotteryNumbers, Comparator comparator) {
+        if (!comparator.hasBonusNumber(userLotteryNumbers, bonusNumber)) {
+            return FIVE_MATCHES_WITHOUT_BONUS_INDEX;
+        }
+        return FIVE_MATCHES_WITH_BONUS_INDEX;
     }
 
     public int calculateProfit(List<Integer> matchResults) {
