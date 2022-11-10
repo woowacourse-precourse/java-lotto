38466608ea8aa6259a4bfcea48f2fcef5e1dd70f
SEONMl <sm000117@naver.com>
2022-11-10 14:04:55 +0900

    feat: String으로 입력한 값을 Set으로 반환하는 mapper 클래스 구현

---
+++ src/main/java/lotto/mapper/InputMapper.java
@@ -0,0 +1,12 @@
+package lotto.mapper;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class InputMapper {
+
+    public Set<Integer> stringToSetSplitBy(String input, String regex) throws NumberFormatException {
+        return Arrays.stream(input.split(regex)).map(String::trim).map(Integer::parseInt).collect(Collectors.toSet());
+    }
+}
