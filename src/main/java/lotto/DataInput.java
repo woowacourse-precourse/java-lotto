c431d5ffb7a97b25e85737af3e2fcaafeea53f6f
SimSimEEE <rebe123@naver.com>
2022-11-11 03:45:14 +0900

    feat(DataInput.class): add function to confirm input is validate unit

--- src/main/java/lotto/DataInput.java
+++ src/main/java/lotto/DataInput.java
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class DataInput {
     private static final String VALID_INPUT_PATTERN = "[^0-9]";
+    private static final String VALID_UNIT_PATTERN = "[0]{3}$";
     private static final String ERROR_MESSAGE_NUMBER = "[ERROR] 숫자를 입력해주세요";
+    private static final String ERROR_MESSAGE_UNIT = "[ERROR] 1000원 단위로 입력해주세요";
     public static String UserInput(String Message) {
         System.out.println(Message);
         String input = Console.readLine();
@@ -15,6 +17,13 @@ public class DataInput {
     private static void ValidateNumber(String input) {
         if (input.matches(VALID_INPUT_PATTERN)) {
             System.out.println(ERROR_MESSAGE_NUMBER);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void ValidateAmountUnit(String input) {
+        if (input.matches(VALID_UNIT_PATTERN)) {
+            System.out.println(ERROR_MESSAGE_UNIT);
             throw new IllegalArgumentException();
         }
     }
