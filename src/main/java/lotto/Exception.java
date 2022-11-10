7524aef3271c2af04de70492873a95118a9d2285
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-10 15:59:53 +0900

    fix(vailMoney) : boolean 함수로 변경

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -1,11 +1,12 @@
 package lotto;
 
 public class Exception {
-    public void validMoney(int money) {
+    public boolean validMoney(int money) {
         if(money < 1000)
             throw new IllegalArgumentException("[ERROR] 1000원 이상의 금액을 입력하세요.");
         if(money % 1000 != 0)
             throw new IllegalArgumentException("[ERROR] 유효한 값을 입력하세요.");
+        return true;
     }
     public void
 }
