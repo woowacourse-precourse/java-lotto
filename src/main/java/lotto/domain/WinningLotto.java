35a1e5040f1719a29f8a179750bb4aa16b78335c
kokodak <kokodakadokok@gmail.com>
2022-11-10 22:18:34 +0900

    feat(winningLotto): 당첨 번호의 범위가 1~45 사이인지 검증하는 기능 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -4,12 +4,30 @@ import java.util.List;
 
 public class WinningLotto {
 
+    private static final String WINNING_NUMBERS_ARE_BETWEEN_ONE_AND_FORTY_FIVE = "[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.";
+
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
 
     public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        validateWinningNumbers(winningNumbers);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateWinningNumbers(List<Integer> winningNumbers) {
+        if (!isBetweenOneAndFortyFive(winningNumbers)) {
+            throw new IllegalArgumentException(WINNING_NUMBERS_ARE_BETWEEN_ONE_AND_FORTY_FIVE);
+        }
+    }
+
+    private boolean isBetweenOneAndFortyFive(List<Integer> winningNumbers) {
+        for (int winningNumber : winningNumbers) {
+            if (winningNumber < 1 || winningNumber > 45) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public List<Integer> getWinningNumbers() {
