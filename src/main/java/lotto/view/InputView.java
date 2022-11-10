12507e14c7bc7ceb52dc6c779a14dee924ad75d6
hectick <chyeon1223@cau.ac.kr>
2022-11-11 03:17:34 +0900

    feat: 입력된 당첨번호를 파싱하는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,9 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
@@ -13,10 +16,20 @@ public class InputView {
         return input;
     }
 
-    public String getWinningNumbers(){
+    public List<String> getWinningNumbers(){
         System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
-        String input = Console.readLine();
-        return input;
+        String line = Console.readLine();
+        List<String> inputs = parse(line);
+        return inputs;
+    }
+
+    private List<String> parse(String line){
+        String values[] = line.split(",");
+        List<String> inputs = new ArrayList<>();
+        for(int i = 0; i < values.length; i++){
+            inputs.add(values[i]);
+        }
+        return inputs;
     }
 
     public String getBonusNumber(){
