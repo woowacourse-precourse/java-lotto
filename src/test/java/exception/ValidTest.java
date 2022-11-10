fd98ffa611ab20bb44ee9e39c6cb115855dc7483
gyuturn <rbtjrrbals@gmail.com>
2022-11-10 22:40:17 +0900

    test($util): List<Integer> input이 중복을 가지는지 유효성 검사
    추후 로또번호 입력받을때를 위하여 Valid 클래스에 구현
    
    B-1
    중복을 가질때, 가지지 않을때 테스트 완료

--- src/test/java/exception/ValidTest.java
+++ src/test/java/exception/ValidTest.java
@@ -3,6 +3,8 @@ package exception;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import util.exception.Valid;
@@ -32,5 +34,25 @@ class ValidTest {
         assertThat(result).isTrue();
     }
 
+    @Test
+    @DisplayName("[Valid-hasDuplicateNum]Input list가 중복가지는지 유효성 테스트")
+    public void hasDuplicate() {
+        //given
+        List<Integer> numbers= Arrays.asList(1,2,3,4,5,6,5);
+        //when
+        boolean result = Valid.hasDuplicateNum(numbers);
+        //then
+        assertThat(result).isTrue();
+    }
 
+    @Test
+    @DisplayName("[Valid-hasDuplicateNum]Input list가 중복가지지 않는지 유효성 테스트")
+    public void hasNotDuplicate() {
+        //given
+        List<Integer> numbers= Arrays.asList(1,2,3,4,5,6,7,8);
+        //when
+        boolean result = Valid.hasDuplicateNum(numbers);
+        //then
+        assertThat(result).isFalse();
+    }
 }
