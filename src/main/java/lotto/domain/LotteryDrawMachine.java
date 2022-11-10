ec439c5a7b373211d09e85d9fd9fa86e8dcb556d
minju412 <ln8847@ajou.ac.kr>
2022-11-10 17:58:37 +0900

    refactor: change system values to constant

--- src/main/java/lotto/domain/LotteryDrawMachine.java
+++ src/main/java/lotto/domain/LotteryDrawMachine.java
@@ -1,11 +1,15 @@
 package lotto.domain;
 
+import static lotto.constant.SystemValue.LOTTERY_NUMBERS_SIZE;
+import static lotto.constant.SystemValue.MAXIMUM_LOTTERY_NUMBER;
+import static lotto.constant.SystemValue.MINIMUM_LOTTERY_NUMBER;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class LotteryDrawMachine {
 
     public List<Integer> drawLotteryNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(MINIMUM_LOTTERY_NUMBER, MAXIMUM_LOTTERY_NUMBER, LOTTERY_NUMBERS_SIZE);
     }
 }
