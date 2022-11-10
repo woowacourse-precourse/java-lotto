3b655a7fde89bad260936430e04ae08c720c372b
swonny <wonny921@gmail.com>
2022-11-11 00:38:04 +0900

    test: LottoGenerator 기능 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,8 +3,10 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -30,4 +32,25 @@ class LottoTest {
         assertThatThrownBy(() -> LottoGenerator.validatePayment(10200))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("지불한 금액에 따라 로또 발행 개수가 달라진다.")
+    @Test
+    void countNumberOfLotto() {
+        assertThat(LottoGenerator.countNumberOfLotto(12000)).isEqualTo(12);
+    }
+
+    @DisplayName("로또 번호가 오름차순으로 정렬된다.")
+    @Test
+    void validateOrderOfLottoNumbers() {
+        assertThat(LottoGenerator.orderNumbers(Arrays.asList(6, 5, 4, 3, 10, 1)))
+                .isEqualTo(Arrays.asList(1, 3, 4, 5, 6, 10));
+    }
+
+    @DisplayName("지불한 금액 만큼의 로또가 생성된다.")
+    @Test
+    void generateLotto() {
+        assertThat(LottoGenerator.generate("10000").size())
+                .isEqualTo(10);
+    }
+    
 }
