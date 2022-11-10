d6e349c990e451898b793f192dfd4482b51bba1e
KIMSEI1124 <workju1124@gmail.com>
2022-11-10 15:18:18 +0900

    feat: 구매 금액이 1,000원 단위 확인하는 기능 구현

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -1,7 +1,8 @@
 package lotto.exception;
 
 public enum ErrorMessage {
-    NOT_DIGIT("[ERROR] 구매 금액과 로또 번호는 숫자만 입력이 가능합니다.");
+    NOT_DIGIT("[ERROR] 구매 금액과 로또 번호는 숫자만 입력이 가능합니다."),
+    NOT_THOUSAND_MONEY("[ERROR] 구매 금액은 1,000원 단위로 입력이 가능합니다.");
 
     private final String message;
 
--- src/main/java/lotto/exception/InputException.java
+++ src/main/java/lotto/exception/InputException.java
@@ -8,4 +8,11 @@ public class InputException extends IllegalArgumentException {
         }
         throw new IllegalArgumentException(ErrorMessage.NOT_DIGIT.getMessage());
     }
+
+    public boolean notThousandMoneys(int money) {
+        if (money % 1000 != 0) {
+            return true;
+        }
+        throw new IllegalArgumentException(ErrorMessage.NOT_THOUSAND_MONEY.getMessage());
+    }
 }
