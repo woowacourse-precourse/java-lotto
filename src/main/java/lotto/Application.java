495bd6c2134cd83fd9afd09ba9c5c10f65926335
EUNJEONGMUN <munej26@gmail.com>
2022-11-10 11:30:48 +0900

    feat: 중복하지 않는 6개의 숫자를 가진 로또 발행 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    private static Lotto createLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
     }
 }
