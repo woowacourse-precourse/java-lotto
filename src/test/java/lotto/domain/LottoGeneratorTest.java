99dfa92df247d4d0fe9eb7674e6c97ff4889e9bd
i960107 <i960107@naver.com>
2022-11-11 00:53:21 +0900

    test(LottoGenerator): 로또 발행 기능에 대한 단위 테스트 추가

---
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoGeneratorTest {
+    @DisplayName("금액이 유효하지 않으면 예외 발생")
+    @ParameterizedTest()
+    @ValueSource(ints = {-1000, 1200, 10100})
+    void test_when_money_is_not_valid(int money) {
+        LottoGenerator generator = LottoGenerator.getInstance();
+        assertThatThrownBy(() -> generator.generateLottos(money))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 금액이 1000으로 나누어 떨어지지 않습니다");
+    }
+
+    @DisplayName("올바른 금액이면 구입 가능한 개수 만큼 로또 발행")
+    @ParameterizedTest()
+    @CsvSource({"1000,1", "5000,5", "10000,10"})
+    void test_when_money_is_not_valid(int money, int expectedSize) {
+        LottoGenerator generator = LottoGenerator.getInstance();
+        assertThat(generator.generateLottos(money).size())
+                .isEqualTo(expectedSize);
+    }
+
+}
