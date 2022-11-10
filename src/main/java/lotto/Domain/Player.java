f5ab71acc2ad05f963a6868445244c05466cced1
Park shinyeong <syngh503@gmail.com>
2022-11-11 00:02:28 +0900

    Feat(Player): 메소드 및 변수 생성
    테스트 전 기능에 필요한 메소드와 변수를 생성하였습니다.
    
    Relate to: #1

--- src/main/java/lotto/Domain/Player.java
+++ src/main/java/lotto/Domain/Player.java
@@ -1,4 +1,11 @@
 package lotto.Domain;
 
 public class Player {
+    private int money;
+    private int lottoCount;
+    private int playerLotto;
+
+    public int getLottoCount(){
+        return 0;
+    }
 }
