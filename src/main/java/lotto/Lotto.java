b74937189608069ff58aa60cf6e282f9be20f198
anneu <b.imnuj@gmail.com>
2022-11-11 03:33:57 +0900

    refactor: 공통으로 사용되는 상수를 ConstantUtil 클래스로 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,13 @@
 package lotto;
 
+import static lotto.utils.ConstantUtil.LOTTO_NUMBER_SIZE;
+import static lotto.utils.ConstantUtil.MAX_LOTTO_NUMBER;
+import static lotto.utils.ConstantUtil.MIN_LOTTO_NUMBER;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Lotto {
-    private static final int LOTTO_NUMBER_SIZE = 6;
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
     private static final String SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호의 개수는 6개이어야 합니다.";
     private static final String DUPLICATES_ERROR_MESSAGE = "[ERROR] 로또 번호에 중복된 숫자가 있습니다.";
     private static final String RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호에 1 ~ 45 범위를 벗어난 숫자가 있습니다.";
---
+++ src/main/java/lotto/utils/ConstantUtil.java
@@ -0,0 +1,10 @@
+package lotto.utils;
+
+public final class ConstantUtil {
+    public static final int LOTTO_NUMBER_SIZE = 6;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+
+    private ConstantUtil() {
+    }
+}
