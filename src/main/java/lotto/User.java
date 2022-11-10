399862e7e145d2831febecf2120dc001f5de1f6e
swandevson <shai4956@gmail.com>
2022-11-10 18:00:34 +0900

    Feat(User): User 생성 및 로또 할당

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,24 +1,37 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class User {
     private List<Lotto> lottos;
     private int lottoNum;
     private int money;
-    private int earn;
+    private double earn;
 
 
     public User(int money) {
         validateMoney(money);
 
         this.money = money;
+        this.lottoNum = money / 1000;
+        generateLottos();
+        this.earn = 0.0;
     }
 
 
     private void validateMoney(int money) {
         if (money % 1000 != 0) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void generateLottos() {
+        for(int i = 0; i < this.lottoNum; i++) {
+            Lotto lotto = Lotto.generateLotto();
+
+            lottos.add(lotto);
         }
     }
 }
