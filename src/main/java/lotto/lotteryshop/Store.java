08274462d7ff05bfc0b3dc87e9c4c79e3fba21da
K-Diger <kitten.diger@gmail.com>
2022-11-10 15:12:17 +0900

    refactor : getPurchaseNumberOfLotto, getPublishedLottoNumber 입력 안내에 대한 출력 문구를, 출력 예시와 일치하도록 출력 형태 조정

--- src/main/java/lotto/lotteryshop/Store.java
+++ src/main/java/lotto/lotteryshop/Store.java
@@ -9,25 +9,27 @@ import java.util.List;
 
 public class Store {
 
-    List<List<Integer>> generatedLottoPaper = new ArrayList<>();
     public int getPurchaseNumberOfLotto() {
         System.out.println("구매금액을 입력해 주세요.");
         InputAgent inputAgent = new InputAgent();
         int inputValue = inputAgent.inputUserMoney();
-        System.out.printf("%d개를 구매했습니다.", inputValue / 1000);
-        return inputValue;
+        System.out.printf("\n%d개를 구매했습니다.\n", inputValue / 1000);
+        return inputValue / 1000;
     }
 
     public List<List<Integer>> generateLottoNumber(int purchaseNumber) {
+        List<List<Integer>> generatedLottoPaper = new ArrayList<>();
+
         for (int number = 0; number < purchaseNumber; number++) {
-            this.generatedLottoPaper.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            generatedLottoPaper.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         }
-        return this.generatedLottoPaper;
+        return generatedLottoPaper;
     }
 
-    public void getPublishedLottoNumber() {
-        for (List<Integer> eachLottoPaper : this.generatedLottoPaper) {
+    public void getPublishedLottoNumber(List<List<Integer>> generatedLottoPaper) {
+        for (List<Integer> eachLottoPaper : generatedLottoPaper) {
             System.out.println(Arrays.toString(eachLottoPaper.toArray()));
         }
+        System.out.println("");
     }
 }
