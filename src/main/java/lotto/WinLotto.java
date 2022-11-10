ec0c1d54707452d2d5ea82eecc8fb937ede5ad23
eora21 <eora21@naver.com>
2022-11-10 23:14:29 +0900

    feat: 🎸 추가 당첨 로또 클래스 및 검증 메서드
    보너스 번호가 당첨 번호와 중복된다면 예외를 발생시킨다.

---
+++ src/main/java/lotto/WinLotto.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import constant.LottoText;
+
+import java.util.List;
+
+public class WinLotto extends Lotto {
+    private final int bonusNumber;
+
+    public WinLotto(List<Integer> numbers, int bonusNumber) {
+        super(numbers);
+        this.bonusNumber = bonusNumber;
+        validate(numbers, bonusNumber);
+    }
+
+    private void validate(List<Integer> numbers, int bonusNumber) {
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(
+                    LottoText.ERROR + "보너스 번호가 당첨 번호와 중복되었습니다.");
+        }
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
