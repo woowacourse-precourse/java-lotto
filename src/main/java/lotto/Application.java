2334d99a98950efdd18f35f9ad3179b4787c59d7
Minjune <minjune8506@gmail.com>
2022-11-10 18:39:09 +0900

    feat(Lotto): 구입 금액을 검사하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,5 +6,6 @@ public class Application {
     public static void main(String[] args) {
         int amount = InputView.askAmount();
         System.out.println(amount);
+        AmountValidator.validate(amount);
     }
 }
