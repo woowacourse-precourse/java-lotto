dc78435f7de80e3d497a56c1c67ec06c5a86e9f4
dev_kong <ahdjd5@gmail.com>
2022-11-10 17:23:43 +0900

    feat(Converter): add method to convert String to Integer list

--- src/main/java/lotto/utils/Converter.java
+++ src/main/java/lotto/utils/Converter.java
@@ -1,11 +1,27 @@
 package lotto.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Converter {
     public static int toIntFromString(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+        }
+    }
+
+    public static List<Integer> toIntList(String winningNumber) {
+        try {
+            return Arrays.stream(winningNumber.split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException exception) {
+            System.out.println("[ERROR] 숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException();
         }
     }
 }
