5066abc987bef3596f1f35fc86d8c6cbca2704fc
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-10 14:52:20 +0900

    feat(User): 게임 가능한 횟수 변수 추가과 getter 추가

--- src/main/java/lotto/model/User.java
+++ src/main/java/lotto/model/User.java
@@ -2,13 +2,17 @@ package lotto.model;
 
 public class User {
     private int userPrice;
-
+    private int userGameCnt;
     public User() {
         userPrice=0;
     }
 
     public void setUserPrice(int userPrice) {
+        this.userGameCnt=userPrice/1000;
         this.userPrice = userPrice;
     }
 
+    public int getUserGameCnt() {
+        return userGameCnt;
+    }
 }
