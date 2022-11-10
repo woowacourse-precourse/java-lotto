15b53b4a9fa1587a6d82804caf0695dda27b88aa
Donghyun <flaehan@gmail.com>
2022-11-10 11:37:31 +0900

    type : feat 예외사항 추가
    body : 로또 구입 금액이 1000원 단위가 아닐 경우
    
    footer :

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,15 +4,20 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private int inputMoney;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<Integer> numbers) { // 생성자
         validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate(List<Integer> numbers) { // 예외사항
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 숫자는 여섯자리여야 합니다.");
+        }
+
+        if(inputMoney % 1000 != 0){
+            throw new IllegalArgumentException("로또 구입 금액은 1,000원 단위입니다.");
         }
     }
 
