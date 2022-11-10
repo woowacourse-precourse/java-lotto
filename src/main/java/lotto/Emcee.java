6fb498ea8d11bc6da433d8f0acc39bc8b2d7e356
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-10 10:38:05 +0900

    feat(Emcee): build skeleton code
    Emcee guides what to do and show results

---
+++ src/main/java/lotto/Emcee.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import java.util.List;
+
+public class Emcee {
+
+    public void guideGame() {
+
+    }
+
+    public void guideAmountOfPurchaseMoney() {
+
+    }
+
+    public void showPublishedLottos(List<Lotto> lottos) {
+
+    }
+
+    public void guideWinningNumber() {
+
+    }
+
+    public void guideBonusNumber() {
+
+    }
+
+    public void showResultOfLottoGame() {
+
+    }
+
+    private void showStatistics() {
+
+    }
+
+    private void showYield() {
+
+    }
+}
