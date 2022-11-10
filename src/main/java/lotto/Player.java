6201c36e00f75fc20e3452916f92378f3df89116
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-10 10:50:34 +0900

    feat(Player): build skeleton code
    Player enters values which game request.

---
+++ src/main/java/lotto/Player.java
@@ -0,0 +1,16 @@
+package lotto;
+
+public class Player {
+
+    public String payAmountOfPurchaseMoney() {
+        return "";
+    }
+
+    public String selectWinningNumbers() {
+        return "";
+    }
+
+    public String selectBonusNumber() {
+        return "";
+    }
+}
