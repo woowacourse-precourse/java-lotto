41409ecf3282044b07861281c357b6cc9f0d6547
Reznov <reznov03048@gmail.com>
2022-11-10 22:44:28 +0900

    feat: 당첨 번호에 중복된 숫자가 있는지 확인하는 기능 구현

--- src/main/java/lotto/domain/WinningBonusNumber.java
+++ src/main/java/lotto/domain/WinningBonusNumber.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningBonusNumber {
     private List<Integer> winningNumbers;
@@ -12,5 +14,15 @@ public class WinningBonusNumber {
         this.bonusNumber = bonusNumber;
     }
 
+    private void validateDuplicated(List<Integer> winningNumbers) {
+        Set<Integer> checkDuplicated = new HashSet<>();
 
+        for (int number : winningNumbers) {
+            checkDuplicated.add(number);
+        }
+
+        if (checkDuplicated.size() != Number.LOTTO_NUMBER_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
