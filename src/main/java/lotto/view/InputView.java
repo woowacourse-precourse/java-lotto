f15945541994813c299031d5663ec0117ddc30ad
CMHyuk <alsgur9710@naver.com>
2022-11-11 00:01:36 +0900

    feat(InputView): 구입금액 입력 출력문

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class InputView {
+    private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+
+    public static void setInputMoney() {
+        System.out.println(INPUT_MONEY);
+    }
+}
