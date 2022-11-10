e4bfe7f22d1e94d8b5790143259f706774d5a79c
i960107 <i960107@naver.com>
2022-11-10 21:01:43 +0900

    refactor(Lotto): 유효성 검사 기능을 별도의 클래스로 분리
    로또 번호와 당첨 번호는 유효성 검사 로직이 같음. 두 클래스에서 공유할 수 있도록 별도의 클래스로 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,34 +7,8 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        Validator.validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        isProperSize(numbers);
-        isAllNumberUnique(numbers);
-        for (Integer number : numbers) {
-            isInRange(number);
-        }
-    }
-
-    private void isProperSize(List<Integer> numbers) {
-        if (numbers.size() != LottoNumber.TOTAL_COUNT) {
-            throw new IllegalArgumentException("[ERROR]count of numbers in lotto should be" + LottoNumber.TOTAL_COUNT);
-        }
-    }
-
-    private void isAllNumberUnique(List<Integer> numbers) {
-        if (new HashSet<>(numbers).size() != LottoNumber.TOTAL_COUNT) {
-            throw new IllegalArgumentException("[ERROR]all numbers in lotto should be unique");
-        }
-    }
-
-    private void isInRange(int number) {
-        if (number < LottoNumber.MIN || number > LottoNumber.MAX) {
-            throw new IllegalArgumentException
-                    ("[ERROR]number should be between " + LottoNumber.MIN + "and" + LottoNumber.MAX);
-        }
-    }
 }
---
+++ src/main/java/lotto/Validator.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Validator {
+
+    public static void validate(List<Integer> numbers) {
+        isProperSize(numbers);
+        isAllNumberUnique(numbers);
+        for (Integer number : numbers) {
+            isInRange(number);
+        }
+    }
+
+    private static void isProperSize(List<Integer> numbers) {
+        if (numbers.size() != LottoRule.TOTAL_COUNT) {
+            throw new IllegalArgumentException("[ERROR]count of numbers in lotto should be" + LottoRule.TOTAL_COUNT);
+        }
+    }
+
+    private static void isAllNumberUnique(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != LottoRule.TOTAL_COUNT) {
+            throw new IllegalArgumentException("[ERROR]all numbers in lotto should be unique");
+        }
+    }
+
+    private static void isInRange(int number) {
+        if (number < LottoRule.MIN_NUMBER || number > LottoRule.MAX_NUMBER) {
+            throw new IllegalArgumentException
+                    ("[ERROR]number should be between " + LottoRule.MIN_NUMBER + "and" + LottoRule.MAX_NUMBER);
+        }
+    }
+
+}
