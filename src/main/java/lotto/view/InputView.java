07b14a17c28c8d1836ec7f844fcf46a61e485131
wlsh44 <k87913j@gmail.com>
2022-11-11 00:12:38 +0900

    feat: 구입 금액 입력 예외처리 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String PURCHASE_ERROR_MESSAGE = "구입 금액은 문자일 수 없습니다.";
 
     public static int inputPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
@@ -12,6 +13,10 @@ public class InputView {
     }
 
     private static int toInteger(String input) {
-        return Integer.parseInt(input);
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(PURCHASE_ERROR_MESSAGE);
+        }
     }
 }
