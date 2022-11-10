6c8f3535540bed082061105fe53a69099f8abac3
kki4504 <rhkddlf9876@gmail.com>
2022-11-10 17:17:09 +0900

    feat(BuyLotto) : 구매 가능한 만큼 로또 구매
    availablePurchasesNumber 메서드 이용 구매 가능한 로또 개수 확인 후,
    lottos에 Lotto객체 주입

--- src/main/java/lotto/util/BuyLotto.java
+++ src/main/java/lotto/util/BuyLotto.java
@@ -1,15 +1,30 @@
 package lotto.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Lotto;
 import lotto.util.input.Money;
 
 import java.util.List;
 
 public class BuyLotto {
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public Money getMoney() {
+        return money;
+    }
+
     List<Lotto> lottos;
-
+    Money money;
     public BuyLotto(Money money) {
-
+        this.money = money;
+        for (int i = 0; i < availablePurchasesNumber(money.getMoney()); i++) {
+            lottos.add(i, new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+    }
+    private int availablePurchasesNumber (int money) {
+        return money / 1000;
     }
 
 }
