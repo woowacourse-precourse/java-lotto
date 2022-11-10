b47109e08d4134b2e77ed77242f66c317bee472c
drunkenhw <drunkenhw@gmail.com>
2022-11-11 01:25:48 +0900

    feat(domain): 당첨 번호를 입력받는 기능 추가

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningLotto {
+    private List<Integer> numbers;
+
+    public WinningLotto(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,20 +1,35 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
 
     public static int getMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
+        return convertToNumber(input);
+    }
+
+    private static int convertToNumber(String input) {
         validateNumber(input);
         return Integer.parseInt(input);
     }
 
+    public static List<Integer> getWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return Arrays.stream(input.split(","))
+                .map(InputView::convertToNumber)
+                .collect(Collectors.toList());
+    }
+
     private static void validateNumber(String input) {
-        if (!input.matches("^[0-9]*$")) {
+        if (!input.matches("^[0-9]*$") || input.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요");
         }
-
     }
+
 }
