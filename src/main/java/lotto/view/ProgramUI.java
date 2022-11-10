eb7b8e9a142f0e9fa87f4a1d1aabfa07f1c5c5a7
ddubidubap <park62000@gmail.com>
2022-11-11 00:52:05 +0900

    feat: 구매 내역 출력하는 함수

--- src/main/java/lotto/view/ProgramUI.java
+++ src/main/java/lotto/view/ProgramUI.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static lotto.view.Constants.*;
 
@@ -9,5 +11,14 @@ public class ProgramUI {
     public void inputMoney() {
         System.out.println(INPUT_MONEY);
         this.moneyInput = readLine();
+    }
+
+    public void printTickets(int numOfTickets, List<List<Integer>> purchasedTickets) {
+        String purchased = String.format(BOUGHT_NOTICE, numOfTickets);
+        System.out.println();
+        System.out.println(purchased);
+        for (List<Integer> ticket: purchasedTickets) {
+            System.out.println(ticket);
+        }
     }
 }
