51749fecf8dc3af30b71ffb7993471b61cc25ab7
Kwak-JunYoung <klop9090@g.skku.edu>
2022-11-11 01:35:40 +0900

    fix(lotto/Lotto.java) Added error messages to be printed when an exception is thrown

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,14 +12,14 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리 수입니다.");
         }
 
         for (int i = 1; i <= 45; i++)
-            if (Collections.frequency(numbers, i) >= 2) throw new IllegalArgumentException();
+            if (Collections.frequency(numbers, i) >= 2) throw new IllegalArgumentException("[ERROR] 중복되는 숫자가 없어야 합니다.");
 
         for (int i = 0; i < 6; i++)
-            if (numbers.get(i) < 1 || numbers.get(i) > 45) throw new IllegalArgumentException();
+            if (numbers.get(i) < 1 || numbers.get(i) > 45) throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
 
     public List<Integer> getNumbers() {
