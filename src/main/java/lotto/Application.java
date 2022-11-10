419420d80fbf78ffb3ce7b7cdb1243cde25a9e65
whibaek <jibaek0513@gmail.com>
2022-11-11 03:10:23 +0900

    feat(Application): 프로그램 흐름 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,20 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        User user = new User();
+        NumberGenerator numberGenerator = new NumberGenerator();
+        user.readMoney();
+        user.setNumOfLotto();
+
+        System.out.println(user.getNumOfLotto() + " 개를 구매했습니다.");
+        for(int i=0; i<user.getNumOfLotto(); i++){
+            user.addLotto(numberGenerator.makeNumber());
+        }
+        user.showLottos();
+
+        numberGenerator.readLotto();
+        numberGenerator.readBonus();
+
+        System.out.println("당첨 통계\n---");
     }
 }
