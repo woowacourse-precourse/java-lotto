ce5a3ea663022651417be95cc55b6c06f07de7e1
raycho <raycho521@yonsei.ac.kr>
2022-11-10 12:16:45 +0900

    feat(lotto): update Lotto.java
    2022.11.10 by Ray
    LottoTest.java 의 테스트코드를 충족시키기 위한 validation 코드 작성
    validateDuplicate,validateNumberRange 메소드 추가,
    생성자 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,38 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
+        validateNumberRange(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자를 입력해야 합니다.");
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> numSet = new HashSet<>(numbers);
+        if (numbers.size() != numSet.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) > 45 || numbers.get(i) < 1)
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
 }
