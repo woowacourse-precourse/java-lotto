7a8632e029045be6f89da882162de1e378b43d95
K-Diger <kitten.diger@gmail.com>
2022-11-10 19:38:40 +0900

    refactor : getLottoPaper 메서드 제거 - 입력을 받음과 동시에 출력이 이루어지는 메서드가 이미 존재하므로 제거

--- src/main/java/lotto/client/Client.java
+++ src/main/java/lotto/client/Client.java
@@ -1,23 +1,16 @@
 package lotto.client;
 
-import lotto.host.LottoHost;
 import lotto.lotteryshop.Store;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Client {
 
-    public void buyLotto() {
+    public List<List<Integer>> buyLotto() {
         Store store = new Store();
         int purchaseNumberOfLotto = store.getPurchaseMoney();
         List<List<Integer>> generatedLottoNumber = store.generateLottoNumber(purchaseNumberOfLotto);
         store.getPublishedLottoNumber(generatedLottoNumber);
-    }
-
-    public void getLottoPaper() {
-        LottoHost lottoHost = new LottoHost();
-        List<Integer> lottoNumbers = lottoHost.publishWinningLottoNumbers(new ArrayList<>());
-        lottoHost.publishWinningBonusNumber(lottoNumbers);
+        return generatedLottoNumber;
     }
 }
