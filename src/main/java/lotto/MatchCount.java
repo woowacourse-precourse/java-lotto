baf5edbde8ab664cebac7c421860c0ab6e952224
EUNJEONGMUN <munej26@gmail.com>
2022-11-10 22:55:02 +0900

    feat: 사용자의 로또 번호 비교 숫자를 담을 MatchCount 클래스 생성

---
+++ src/main/java/lotto/MatchCount.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class MatchCount {
+    private int winningCount;
+    private int bonusCount;
+
+    public MatchCount(int winningCount, int bonusCount) {
+        this.winningCount = winningCount;
+        this.bonusCount = bonusCount;
+    }
+}
