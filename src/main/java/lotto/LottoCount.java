d7169f6845f1c319a231d4e736c1b720a134c63c
kimdahee0815 <diana815@naver.com>
2022-11-10 19:19:32 +0900

    feat(LottoCount) : validateNumber 함수 작성. 입력시 숫자가 아니거나 1000미만의 수 입력시 IllegalArgumentException 발생, 에러메시지([ERROR] 1000원 이상의 금액만 입력하세요.)

--- src/main/java/lotto/LottoCount.java
+++ src/main/java/lotto/LottoCount.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.regex.Pattern;
+
 public class LottoCount {
 	private int price = 0;
 	public int lottocount = 0;
@@ -7,11 +9,15 @@ public class LottoCount {
 	public LottoCount(int price) {
 		this.price = price;
 		this.lottocount = price / 1000;
-		validateNumber();
+		validateNumber(price);
 	}
 	
-	public boolean validateNumber() {
-		
+	public boolean validateNumber(int price) {
+		String pattern = "^[0-9]+$";
+		if (Pattern.matches(pattern, String.valueOf(price)) && price >= 1000) {
+			return true;
+		} 
+		throw new IllegalArgumentException("[ERROR] 1000원 이상의 금액만 입력하세요.");
 	}
 	
 	public boolean validateDivideRight() {
