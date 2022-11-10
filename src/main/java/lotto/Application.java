a0ae35a2539ae1378b15f6a96fb6cffac2b024b5
devmizz <garamminchan@gmail.com>
2022-11-10 17:50:09 +0900

    feat: 당첨 번호 입력 받기
    입력 받은 당첨 번호로 로또를 만들기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,9 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Shop shop = Shop.getInstance();
+        LotteryMachine lotteryMachine = LotteryMachine.getInstance();
+
         shop.purchaseLottery();
+        lotteryMachine.setWinningLottery();
     }
 }
---
+++ src/main/java/lotto/LotteryMachine.java
@@ -0,0 +1,61 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LotteryMachine {
+
+    private static LotteryMachine lotteryMachine;
+    private static Lotto winningLottery;
+    private static int bonus;
+
+    private LotteryMachine() {}
+
+    public static LotteryMachine getInstance() {
+        if(lotteryMachine == null) {
+            lotteryMachine = new LotteryMachine();
+        }
+
+        return lotteryMachine;
+    }
+
+    public Lotto getWinningLottery() {
+        return winningLottery;
+    }
+
+    public void setWinningLottery() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+
+        setWinningLottery(Console.readLine().trim());
+    }
+
+    private void setWinningLottery(String winningStr) {
+        winningLottery = new Lotto(convert(winningStr));
+
+        validate();
+    }
+
+    private List<Integer> convert(String winningStr) {
+        return Arrays.stream(winningStr.split(","))
+                .map(Integer::parseInt)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    private void validate() {
+        List<Integer> winningNumbers = winningLottery.getNumbers();
+
+        if(winningNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 6개가 입력되지 않았습니다");
+        }
+
+        if(new HashSet<Integer>(winningNumbers).size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복된 값이 존재합니다");
+        }
+    }
+}
