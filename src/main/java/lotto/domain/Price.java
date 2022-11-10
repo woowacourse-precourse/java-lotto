7aa68eff2199eb1d1fb63e6890aaa69b30f1665d
csj <koreaxman@naver.com>
2022-11-10 17:40:47 +0900

    feat: create made up of numbers exception

--- src/main/java/lotto/domain/Price.java
+++ src/main/java/lotto/domain/Price.java
@@ -3,17 +3,25 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Price {
-    int input;
 
     public int purchaseAmount() {
-        input = Integer.parseInt(Console.readLine());
-        validateDivide1000Won(input);
-        return input;
+        String input = Console.readLine();
+        validateNumbers(input);
+        validateDivide1000Won(Integer.parseInt(input));
+        return Integer.parseInt(input);
     }
 
     private void validateDivide1000Won(int price) {
         if (price % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 금액이 1000원으로 나눠떨어지지 않습니다.");
+        }
+    }
+
+    private void validateNumbers(String price) {
+        for (char c : price.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("[ERROR] 금액이 숫자가 아닙니다.");
+            }
         }
     }
 }
