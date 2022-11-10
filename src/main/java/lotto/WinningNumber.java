1118aa45745e6d6da77ea9a30c06e3489a9aadd1
ohhyungsuh <ohhyungsuh@gmail.com>
2022-11-11 02:35:05 +0900

    feat: 당첨번호 및 보너스번호 분리 기능 추가

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -2,46 +2,39 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class WinningNumber {
 
-    private static final String inputWinningNumber = "당첨 번호를 입력해 주세요";
-    private static final String inputBonusNumber = "보너스 번호를 입력해 주세요";
+    private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요";
 
-    public List<Integer> initWinningNumber(){
+    public List<String> initWinningNumber(){
         List<String> winningNumber = getWinningNumber();
-        winningNumber.add(getBonusNumber());
-        List<Integer> winningNumbers = stringToIntegerList(winningNumber);
-        return winningNumbers;
+        return winningNumber;
     }
 
-    private List<String> getWinningNumber(){
-        System.out.println(inputWinningNumber);
+    public String initBonusNumber(){
+        return getBonusNumber();
+    }
+
+    private static List<String> getWinningNumber(){
+        System.out.println(INPUT_WINNING_NUMBER);
         String winningNumber = Console.readLine();
         return parsingWinningNumber(winningNumber);
     }
 
-    private List<String> parsingWinningNumber(String winningNumber){
+    private static List<String> parsingWinningNumber(String winningNumber){
         String parsedWinningNumber = winningNumber.replaceAll(" ", "");
         List<String> winningNumberList = Arrays.asList(parsedWinningNumber.split(","));
         return winningNumberList;
     }
 
-    private String getBonusNumber(){
-        System.out.println(inputBonusNumber);
+    private static String getBonusNumber(){
+        System.out.println(INPUT_BONUS_NUMBER);
         String bonusNumber = Console.readLine();
         return bonusNumber;
     }
 
-    private List<Integer> stringToIntegerList(List<String> winningNumbers){
-        List<Integer> list = winningNumbers.stream()
-                .map(s -> Integer.parseInt(s))
-                .collect(Collectors.toList());
-
-        return list;
-    }
 }
