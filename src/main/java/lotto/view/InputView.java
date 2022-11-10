1343fec49e450958fba79a440494ba6a80253fb5
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-11-10 22:47:11 +0900

    feat(lotto): 로또 시작 문구 출력, 금액 입력 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,25 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String INPUT_PRICE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String DATA_TYPE_ERR_MESSAGE = "숫자가 아닙니다.";
+
+    public InputView() {
+    }
+
+    public static int getPriceAmount() {
+        System.out.println(INPUT_PRICE_AMOUNT);
+        return convertStringtoInt(Console.readLine());
+    }
+
+    private static int convertStringtoInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(DATA_TYPE_ERR_MESSAGE);
+        }
+    }
+}
