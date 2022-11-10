b0a68d6c3e4787b3e9d9b21e340b439971b1203c
JunH <tjdtls690@gmail.com>
2022-11-10 20:14:33 +0900

    :art: refactor(Domain : NumberValidator) : 생성자의 접근제어자를 디폴트로 개선

--- src/main/java/lotto/domain/validator/NumberValidator.java
+++ src/main/java/lotto/domain/validator/NumberValidator.java
@@ -4,6 +4,9 @@ public class NumberValidator {
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
     
+    NumberValidator() {
+    }
+    
     public static boolean isOutOfRange(final Integer lottoNumber) {
         return lottoNumber < MIN_LOTTO_NUMBER || lottoNumber > MAX_LOTTO_NUMBER;
     }
