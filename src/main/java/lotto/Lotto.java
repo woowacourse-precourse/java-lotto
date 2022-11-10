141de6c4e2803c3a429e9d6a95f220ef6044c07c
김태용 <amico741@naver.com>
2022-11-11 03:06:57 +0900

    feat: 로또 번호 중복 검사 메서드 추가
    중복 발생 시 예외가 발생해야한다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,18 +1,28 @@
 package lotto;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int MAX_SIZE = 6;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateOverlap(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != MAX_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateOverlap(List<Integer> numbers) {
+        HashSet<Integer> set = new HashSet<>(numbers);
+        if (set.size() != MAX_SIZE) {
             throw new IllegalArgumentException();
         }
     }
