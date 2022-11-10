3fe19872ae58a9f0a41b93f6ce8b0d780d71dfd9
c4fiber <qudcjf153@gmail.com>
2022-11-11 00:51:21 +0900

    feat(LottoStore): 입력된 금액에서 구매할 수 있는 수 만큼 로또를 판매하는 기능
    입력된 금액을 바탕으로 구매할 수 있는 로또갯수에 따라 로또를 생성하여 반환한다

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,5 +13,16 @@ public class LottoStore {
         Collections.sort(numbers);
 
         return new Lotto(numbers);
+    }
+
+    public List<Lotto> sellLottos(int money) {
+        int affordLottos = money / 1000;
+
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i=0; i<affordLottos; i++) {
+            lottos.add(getNewLotto());
+        }
+
+        return lottos;
     }
 }
