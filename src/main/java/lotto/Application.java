a90986da485b2104431074ee80e0b3eb6a3ae107
jmj005 <jmj00005@naver.com>
2022-11-10 15:34:46 +0900

    feat(Application): 당첨 번호 유효성 확인하기 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,7 +2,10 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
@@ -31,10 +34,50 @@ public class Application {
 
     public static List<Integer> inputWinningNumbers() {
         String numbers = Console.readLine();
+        validateWinningNumbers(numbers);
         List<Integer> winningNumbers = new ArrayList<>();
         for (String number : numbers.split(",")) {
             winningNumbers.add(Integer.parseInt(number));
         }
         return winningNumbers;
+    }
+
+    public static void validateWinningNumbers(String numbers) {
+        if (!isDigit(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+        }
+        if (!hasDuplication(numbers)) {
+            throw new IllegalStateException("[ERROR] 6개의 다른 숫자만 입력 가능합니다.");
+        }
+        if (!hassValidScope(numbers)) {
+            throw new IllegalStateException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    public static boolean isDigit(String numbers) {
+        for (char element : numbers.toCharArray()) {
+            if (element == ',') {
+                continue;
+            }
+            if (!Character.isDigit(element)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean hasDuplication(String numbers) {
+        Set<String> elements = new HashSet<>(Arrays.asList(numbers.split(",")));
+        return elements.size() == 6;
+    }
+
+    public static boolean hassValidScope(String numbers) {
+        for (String number : numbers.split(",")) {
+            int element = Integer.parseInt(number);
+            if (!(0 < element && element < 46)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
