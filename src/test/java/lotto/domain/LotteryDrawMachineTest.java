ffb7fb1b5e640dbcbaaf13b2b2a2126030a895c8
minju412 <ln8847@ajou.ac.kr>
2022-11-10 16:46:43 +0900

    test: add LotteryDrawMachineTest

---
+++ src/test/java/lotto/domain/LotteryDrawMachineTest.java
@@ -0,0 +1,41 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LotteryDrawMachineTest {
+
+    private LotteryDrawMachine lotteryDrawMachine;
+
+    @BeforeEach
+    void initialize() {
+        lotteryDrawMachine = new LotteryDrawMachine();
+    }
+
+    @Test
+    void 로또_번호는_6개여야_합니다() {
+        List<Integer> lotteryNumbers = lotteryDrawMachine.drawLotteryNumbers();
+
+        assertThat(lotteryNumbers.size())
+                .isEqualTo(6);
+    }
+
+    @Test
+    void 로또_번호는_1부터_45까지_범위여야_합니다() {
+        List<Integer> lotteryNumbers = lotteryDrawMachine.drawLotteryNumbers();
+
+        assertThat(lotteryNumbers.stream().allMatch(number -> number >= 1 && number <= 45))
+                .isTrue();
+    }
+
+    @Test
+    void 로또_번호는_중복이_없어야_합니다() {
+        List<Integer> lotteryNumbers = lotteryDrawMachine.drawLotteryNumbers();
+
+        assertThat(lotteryNumbers)
+                .doesNotHaveDuplicates();
+    }
+}
