2f39244884048014684f6674f2ab99dd6c9739c6
MuSeung <peter0757@nvaer.com>
2022-11-10 16:52:07 +0900

    test: Payment의 0원 입력 상황을 제거(입력에서 관리)하고 예외 메세지 출력 확인 테스트 구현
    createPaymentByWrongNumberForPrint
        - Payment에서 예외가 발생했을 경우 예외 메세지가 출력되는 지 확인

--- src/test/java/lotto/PaymentTest.java
+++ src/test/java/lotto/PaymentTest.java
@@ -3,6 +3,9 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,12 +15,6 @@ public class PaymentTest {
     @DisplayName("1,000원으로 나누어 떨어지지 않는 경우 예외가 발생한다.")
     void createPaymentByWrongNumber() {
         assertThatThrownBy(() -> new Payment(1500))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-    @Test
-    @DisplayName("0원 입력시 구매할 수 없기에 예외가 발생한다.")
-    void createPaymentByZero() {
-        assertThatThrownBy(() -> new Payment(0))
                 .isInstanceOf(IllegalArgumentException.class);
     }
     @Test
@@ -33,5 +30,14 @@ public class PaymentTest {
     void lottoPurchasePrice() {
         Payment payment = new Payment(8000);
         assertThat(payment.getPay()).isEqualTo(8000);
+    }
+    @Test
+    @DisplayName("1,000원으로 나누어 떨어지지 않는 경우 예외메세지가 출력된다.")
+    void createPaymentByWrongNumberForPrint() {
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        assertThatThrownBy(() -> new Payment(1500))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThat(out.toString()).isEqualTo("[ERROR] 1,000원으로 나누어 떨어지는 값을 입력해야합니다.");
     }
 }
