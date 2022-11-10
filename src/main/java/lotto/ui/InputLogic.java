91adca17f743f574d7ee7d5056d44985b25e3120
khsrla9806 <kimhunsope@naver.com>
2022-11-10 19:19:04 +0900

    feat(InputLogic): 입력받은 금액으로 발행할 수 있는 로또의 수를 저장할 변수 구현 lottoIssueCount: 발행할 수 있는 로또의 수를 저장하는 변수

--- src/main/java/lotto/ui/InputLogic.java
+++ src/main/java/lotto/ui/InputLogic.java
@@ -3,11 +3,18 @@ package lotto.ui;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputLogic {
+    private static int lottoIssueCount = 0;
     public static int enterPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         int purchaseAmount = Integer.valueOf(Console.readLine());
         validatePurchaseAmount(purchaseAmount);
+        lottoIssueCount = purchaseAmount / 1000;
+        System.out.println();
         return purchaseAmount;
+    }
+
+    public static int getLottoIssueCount() {
+        return lottoIssueCount;
     }
 
     public static void validatePurchaseAmount(int purchaseAmount) {
