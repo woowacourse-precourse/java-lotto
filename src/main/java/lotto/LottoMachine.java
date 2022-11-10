9b83ad0d406f0e8a337e6944fdd98fb8c4387636
rt3310 <btac3310@gmail.com>
2022-11-10 23:59:16 +0900

    feat: 입력된 금액을 통해 로또 생성 횟수를 계산하는 기능 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -4,6 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class LottoMachine {
+    private static final int LOTTO_PRICE = 1000;
+
+    public int getLottoCount(int money) {
+        return money / LOTTO_PRICE;
+    }
 
     private List<Integer> generateLottoNumber() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
