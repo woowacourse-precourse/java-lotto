315bbc0dd3bd811e21acd34a26e5aba3ca0b0f73
LHS-11 <ghktjq1119@naver.com>
2022-11-11 00:19:32 +0900

    [Feat] : 4. 로또 수량만큼 로또 번호 뽑기 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.exception.LottoException;
+import lotto.model.Lotto;
 import lotto.model.LottoCount;
+import lotto.model.LottoGenerator;
 import lotto.view.InputView;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         InputView inputView = new InputView();
         LottoException exception = new LottoException();
+//        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+//        for (Integer number : numbers) {
+//            System.out.println("number = " + number);
+//        }
+//        LottoGenerator lottoGenerator = new LottoGenerator();
+//        List<Lotto> lottos = lottoGenerator.createLottoNumbers(14);
+//        for (Lotto lotto : lottos) {
+//            List<Integer> numbers = lotto.getNumbers();
+//            for (Integer number : numbers) {
+//                System.out.print(number+", ");
+//            }
+//            System.out.println();
+//        }
     }
 }
--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -1,4 +1,25 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+import java.util.ArrayList;
+
 public class LottoGenerator {
+
+    public List<Lotto> createLottoNumbers(int lottoAmount){
+         List<Lotto> lottos = new ArrayList<>();
+
+         while(lottoAmount>0) {
+             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+             Lotto lotto = new Lotto(numbers);
+             lottos.add(lotto);
+             lottoAmount--;
+         }
+
+        return lottos;
+
+    }
+
+
 }
