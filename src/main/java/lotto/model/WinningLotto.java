de90f3769a026484c384f833c7b3be02c5280a13
김현재 <41482946+galug@users.noreply.github.com>
2022-11-11 01:55:56 +0900

    feat(winninglotto): 당첨 로또 및 보너스 번호에 대한 검증 기능 추가

---
+++ src/main/java/lotto/model/WinningLotto.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+import java.util.List;
+
+public class WinningLotto extends Lotto {
+    private static final String BONUS_NUMBER_RANGE_ERROR = "[ERROR] 보너스 숫자의 범위가 1~45사이가 아닙니다.";
+    private static final String EXIST_BONUS_NUMBER_ERROR = "[ERROR] 보너스 숫자가 이미 당첨번호에 속해있습니다.";
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        super(winningNumbers);
+        validateBonusNumber(bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(int bonusNumber) {
+        if (bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(BONUS_NUMBER_RANGE_ERROR);
+        }
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(EXIST_BONUS_NUMBER_ERROR);
+        }
+    }
+}
