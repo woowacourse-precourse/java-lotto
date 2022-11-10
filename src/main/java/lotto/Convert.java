182fa3c7323fabd3bb8bb18591f27f407c372ad2
eora21 <eora21@naver.com>
2022-11-10 22:42:13 +0900

    feat: 🎸 추가 로또 당첨 번호 변환 메서드
    쉼표를 기준으로 입력된 로또번호들을 로또 당첨 번호로 만드는 메서드

--- src/main/java/lotto/Convert.java
+++ src/main/java/lotto/Convert.java
@@ -3,6 +3,9 @@ package lotto;
 import constant.Amount;
 import constant.LottoText;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Convert {
     private final Validate validate = new Validate();
 
@@ -17,5 +20,21 @@ public class Convert {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(LottoText.ERROR + "입력값을 정수로 변환할 수 없습니다.");
         }
+    }
+
+    public List<Integer> toWinLottoNumbers(String input) {
+        String[] strNumbers = input.split(",");
+        validate.validWinLottoForm(strNumbers);
+
+        List<Integer> winLottoNumbers = new ArrayList<>();
+
+        for (String strNumber : strNumbers) {
+            int number = toInt(strNumber);
+            validate.validLottoNumber(number);
+            winLottoNumbers.add(number);
+        }
+
+        validate.validDuplicate(winLottoNumbers);
+        return winLottoNumbers;
     }
 }
