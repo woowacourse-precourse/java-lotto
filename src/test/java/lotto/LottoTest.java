3805e599865e8949a873ad8bd808ce41ea67c8f5
MuSeung <peter0757@nvaer.com>
2022-11-10 14:15:39 +0900

    test: Lotto의 번호가 6개 초과해 입력되었을때 에러 메세지 출력을 확인하는 테스트 구현
    createLottoByOverSizeForPrint
        - assertThatThrownBy로 예외 상황을 발생시킨 후 버퍼에 저장된 출력값을 확인한다.
        - "[ERROR] 로또 번호는 6개입니다."가 출력이 되는지 확인

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,18 @@
 package lotto;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -24,10 +29,19 @@ class LottoTest {
 
     @DisplayName("로또 번호에 1~45이외의 번호가 있으면 예외가 발생한다.")
     @Test
-    void createLottoByOutOfBound(){
+    void createLottoByOutOfBound() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 45)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외 메세지가 출력된다.")
+    @Test
+    void createLottoByOverSizeForPrint() {
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThat(out.toString()).isEqualTo("[ERROR] 로또 번호는 6개입니다.");
     }
 }
