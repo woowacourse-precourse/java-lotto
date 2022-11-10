7da1e674ea7ac23c9ab1add5151f657924d7399b
sangwon <swchoi1997@naver.com>
2022-11-10 11:15:18 +0900

    test: 사용자 구매 금액 1000원 단위인지 확인하는 로직 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -6,7 +6,9 @@ public enum ErrorMessage {
     LOTTE_SIZE_INVALID("Lotto Number Length Is Incorrect. SIZE : "),
     LOTTE_NUMBER_DUPLICATION("Lotto Number Have Duplicate Number."),
     LOTTE_NUMBER_OUT_BOUND("Lotto Number Have Out Bound Number."),
-    LOTTE_NUMBER_NOT_ASC("Lotto Number Not Sorted by ASC.");
+    LOTTE_NUMBER_NOT_ASC("Lotto Number Not Sorted by ASC."),
+    PURCHASE_QTY_CONSISTENCE("Purchase Quantity Isn't Consist Of Number."),
+    PURCHASE_QTY_REMINDER("Purchase Quantity Reminder Not 0.");
 
 
     private final String message;
--- src/main/java/lotto/InputValidator.java
+++ src/main/java/lotto/InputValidator.java
@@ -6,17 +6,26 @@ import static lotto.ErrorMessage.PURCHASE_QTY_REMINDER;
 
 public class InputValidator {
 
+    public static final int MINIMUM_ORDER = 1000;
+    public static final int STANDARD_REMINDER = 0;
     public static final String NUMBER_REG_EXP = "^[0-9]+$";
 
     private InputValidator(){}
 
     public static void validateQuantity(String quantity) {
         checkConsistNumber(quantity);
+        checkReminder(quantity);
     }
 
     private static void checkConsistNumber(String quantity) {
         if (!quantity.matches(NUMBER_REG_EXP)) {
             throw new IllegalArgumentException(COMMON_MESSAGE.getMessage() + PURCHASE_QTY_CONSISTENCE.getMessage());
+        }
+    }
+
+    private static void checkReminder(String quantity) {
+        if (STANDARD_REMINDER != Integer.parseInt(quantity) % MINIMUM_ORDER) {
+            throw new IllegalArgumentException(COMMON_MESSAGE.getMessage() + PURCHASE_QTY_REMINDER.getMessage());
         }
     }
 }
