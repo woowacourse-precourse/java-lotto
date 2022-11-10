7508bffd570cd5e7cbfe92acb779e4a2e1e5e2c7
TaeMin <xoals3094@naver.com>
2022-11-10 19:30:46 +0900

    feat(Calculator): 일치하는 개수를 당첨금으로 바꿔주눈 기능 구현

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -1,2 +1,22 @@
-package lotto.domain;public class Calculator {
+package lotto.domain;
+
+import lotto.domain.Prize;
+
+public class Calculator {
+    public Prize givePrize(int matchNumber, boolean bonus) {
+        if (matchNumber == 6) {
+            return Prize.FIRST;
+        } else if (matchNumber == 5) {
+            if (bonus) {
+                return Prize.SECOND;
+            }
+            return Prize.THIRD;
+        } else if (matchNumber == 4) {
+            return Prize.FOURTH;
+        } else if (matchNumber == 3){
+            return Prize.FIFTH;
+        }
+
+        return Prize.LAST;
+    }
 }
