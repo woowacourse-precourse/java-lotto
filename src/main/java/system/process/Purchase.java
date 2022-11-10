bf0380f02bbc6aacff59af4d45b40ab22995bf9b
myungsikyoon <yms8795@gmail.com>
2022-11-10 13:11:38 +0900

    feat : 구매량을 산정하고 이에 맞게 로또를 구매하는 기능 구현.

--- src/main/java/system/process/Purchase.java
+++ src/main/java/system/process/Purchase.java
@@ -8,12 +8,13 @@ public class Purchase {
 
     //TODO : 헷갈리는 이름 수정할 것. ex) purchasedLottos 와 purchaseLottos
     private PurchasedLottosData purchasedLottos;
-    public Purchase(){
+
+    public Purchase() {
         purchasedLottos = proceedPurchase();
         printPurchasedLottoList();
     }
 
-    private PurchasedLottosData proceedPurchase(){
+    private PurchasedLottosData proceedPurchase() {
 
         printAmountPaidText();
 
@@ -21,18 +22,31 @@ public class Purchase {
         return purchaseLottos(amount);
     }
 
-    void printAmountPaidText(){
+    void printAmountPaidText() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    int inputAmountPaid() throws IllegalArgumentException{
-        String moneyInput =  Input.input();
+    int inputAmountPaid() throws IllegalArgumentException {
+        String moneyInput = Input.input();
 
-        if(moneyInput.contains(",")){
-            throw new IllegalArgumentException("[ERROR] 금액을 입력해주세요.");
+        if (moneyInput.contains(",")) {
+            throw new IllegalArgumentException("[ERROR] 올바른 금액을 입력해주세요.");
         }
 
         return Integer.parseInt(moneyInput);
+    }
+
+    int calculateAvailableAmount (int amountPaid) throws IllegalArgumentException {
+
+        if (amountPaid % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 금액에 맞춰 로또를 구매할 수 없습니다.");
+        }
+
+        return amountPaid / 1000;
+    }
+
+    private PurchasedLottosData purchaseLottos(int amount){
+        return new PurchasedLottosData(amount);
     }
 
 
