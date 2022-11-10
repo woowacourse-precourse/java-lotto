1956cca40b90a86d1c4602474c8182c0f50dbcb8
Soseung Soo <sss9073@naver.com>
2022-11-11 01:33:45 +0900

    feat(WinningController): 당첨된 복권 등수 증가

--- src/main/java/lotto/domain/WinningController.java
+++ src/main/java/lotto/domain/WinningController.java
@@ -25,5 +25,44 @@ public class WinningController {
                 correctNumbers++;
             }
         }
+        calculateWinningLotto(correctNumbers,isCorrectBonusNumber);
+    }
+
+    public void calculateWinningLotto(int correctNumbers, boolean isCorrectBonusNumber){
+        checkFirst(correctNumbers);
+        checkSecond(correctNumbers,isCorrectBonusNumber);
+        checkThird(correctNumbers,isCorrectBonusNumber);
+        checkFourth(correctNumbers);
+        checkFifth(correctNumbers);
+    }
+
+    private void checkFirst(int correctNumbers) {
+        if(correctNumbers == WinningStandard.FIRST.getNumber()){
+            lottoWinningResultMap.put(WinningStandard.FIRST, lottoWinningResultMap.getOrDefault(WinningStandard.FIRST,0)+ 1);
+        }
+    }
+
+    private void checkSecond(int correctNumbers, boolean isCorrectBonusNumber){
+        if(correctNumbers == WinningStandard.SECOND.getNumber() && isCorrectBonusNumber == true){
+            lottoWinningResultMap.put(WinningStandard.SECOND, lottoWinningResultMap.getOrDefault(WinningStandard.SECOND,0)+ 1);
+        }
+    }
+
+    private void checkThird(int correctNumbers, boolean isCorrectBonusNumber){
+        if(correctNumbers == WinningStandard.THIRD.getNumber() && isCorrectBonusNumber == false){
+            lottoWinningResultMap.put(WinningStandard.THIRD, lottoWinningResultMap.getOrDefault(WinningStandard.THIRD,0)+ 1);
+        }
+    }
+
+    private void checkFourth(int correctNumbers) {
+        if(correctNumbers == WinningStandard.FOURTH.getNumber()){
+            lottoWinningResultMap.put(WinningStandard.FOURTH, lottoWinningResultMap.getOrDefault(WinningStandard.FOURTH,0)+ 1);
+        }
+    }
+
+    private void checkFifth(int correctNumbers) {
+        if(correctNumbers == WinningStandard.FIFTH.getNumber()){
+            lottoWinningResultMap.put(WinningStandard.FIFTH, lottoWinningResultMap.getOrDefault(WinningStandard.FIFTH,0)+ 1);
+        }
     }
 }
