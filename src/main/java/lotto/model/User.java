59eef5fbba988cf8feb27033711dbd7d1581f0cb
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-10 15:10:16 +0900

    refactor(User): 유저 가격 단위 변수 상수화

--- src/main/java/lotto/model/User.java
+++ src/main/java/lotto/model/User.java
@@ -1,14 +1,19 @@
 package lotto.model;
 
+import lotto.Utils.InputValid;
+
 public class User {
+    private static final int USER_PRICE_UNIT=1000;
+
     private int userPrice;
     private int userGameCnt;
+
     public User() {
         userPrice=0;
     }
 
     public void setUserPrice(int userPrice) {
-        this.userGameCnt=userPrice/1000;
+        this.userGameCnt=userPrice/ USER_PRICE_UNIT;
         this.userPrice = userPrice;
     }
 
