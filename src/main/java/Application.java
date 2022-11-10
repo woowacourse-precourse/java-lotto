50100ee6c4d094a4859a3f18a5e649e42c713dbe
yeonhee <ydol2@naver.com>
2022-11-10 18:46:07 +0900

    feat(Application) : 애플리케이션에 controller 적용

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,8 @@
+import controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = LottoController.create();
+        lottoController.run();
     }
 }
