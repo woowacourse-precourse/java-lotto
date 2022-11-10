8c0727d1ca1b7df0be721b9453aafff0546123bd
Reznov <reznov03048@gmail.com>
2022-11-10 22:35:01 +0900

    feat: 당첨 번호와 보너스 번호를 관리하는 class 생성

---
+++ src/main/java/lotto/domain/WinningBonusNumber.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+public class WinningBonusNumber {
+    private Lotto winningNumbers;
+    private int bonusNumber;
+
+    public WinningBonusNumber(Lotto winningNumbers, int bonusNumber) {
+
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+
+
+}
