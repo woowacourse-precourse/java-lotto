d154de9052084667608d985184e00f269213c2c3
K-Diger <kitten.diger@gmail.com>
2022-11-10 17:50:13 +0900

    refactor : LottoHost 클래스 내 메서드명 변경 - inputLottoNumbers -> publishWinningLottoNumbers - inputBonusNumber -> inputWinningBonusNumber

--- src/main/java/lotto/host/LottoHost.java
+++ src/main/java/lotto/host/LottoHost.java
@@ -7,14 +7,14 @@ import java.util.List;
 public class LottoHost {
     InputAgent inputAgent = new InputAgent();
 
-    public List<Integer> inputLottoNumbers(List<Integer> emptyLottoNumbers) {
+    public List<Integer> publishWinningLottoNumbers(List<Integer> emptyLottoNumbers) {
         System.out.println("당첨 번호를 입력해주세요.");
-        emptyLottoNumbers.addAll(inputAgent.inputLottoNumbers());
+        emptyLottoNumbers.addAll(inputAgent.inputWinningNumbers());
         return emptyLottoNumbers;
     }
 
-    public void inputBonusNumber(List<Integer> lottoNumbers) {
+    public void publishWinningBonusNumber(List<Integer> lottoNumbers) {
         System.out.println("\n보너스 번호를 입력해주세요.");
-        lottoNumbers.add(inputAgent.inputBonusNumber());
+        lottoNumbers.add(inputAgent.inputWinningBonusNumber());
     }
 }
