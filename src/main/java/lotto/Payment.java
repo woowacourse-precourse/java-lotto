d9d70cf1ce0cb5bec96376b458b17e086612b8a0
MuSeung <peter0757@nvaer.com>
2022-11-10 17:18:36 +0900

    refactor: Payment에서 LOTTO_PRICE = 1000으로 정의하여 정리

--- src/main/java/lotto/Payment.java
+++ src/main/java/lotto/Payment.java
@@ -1,18 +1,18 @@
 package lotto;
 
 public class Payment {
-
+    public static int LOTTO_PRICE = 1000;
     private final int pay;
 
     public Payment(int money) {
-        if (money % 1000 != 0) {
+        if (money % LOTTO_PRICE != 0) {
             System.out.print("[ERROR] 1,000원으로 나누어 떨어지는 값을 입력해야합니다.");
             throw new IllegalArgumentException();
         }
         pay = money;
     }
     public int purchase() {
-        return pay / 1000;
+        return pay / LOTTO_PRICE;
     }
     public int getPay() {
         return pay;
