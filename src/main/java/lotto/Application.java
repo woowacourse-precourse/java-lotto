5e077bf095fd6cfd4dcb21038af362e16acfa1b3
swandevson <shai4956@gmail.com>
2022-11-11 02:58:06 +0900

    Feat(Application): 번호 비교를 통한 당첨 내역 반환
    - 번호를 비교해서 몇 등인지 enum 클래스를 반환
    - 3개 미만 일치할 시 당첨이 안 됐으므로 null 반환

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,28 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
+    public static Winning getWinning(Lotto userLotto, Lotto winningLotto) {
+        List<Integer> userNumbers = userLotto.getNumbers();
+        List<Integer> winningNumbers = winningLotto.getNumbers();
+        boolean bonusHit = false;
+        int hitCount = 0;
+
+        winningNumbers.retainAll(userNumbers);
+        hitCount = winningNumbers.size();
+
+        if (hitCount < 3) {
+            return null;
+        }
+
+        if (userNumbers.contains(Lotto.getBonusNumber())) {
+            bonusHit = true;
+        }
+
+        return Winning.getWinning(hitCount, bonusHit);
+    }
+
+
+
     public static void main(String[] args) {
         int money;
         List<Integer> winningNumbers;
