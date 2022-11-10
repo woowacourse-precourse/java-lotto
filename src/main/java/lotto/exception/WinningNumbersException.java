45e6740a7c586961140b2f873dffc0beb6b657fb
YuHayeon <youha0930@gmail.com>
2022-11-11 00:48:31 +0900

    feat(WinningNumbersException): 당첨 번호 숫자가 1~45 사이의 수가 아닐 경우 예외처리
    당첨 번호의 숫자가 1~45 사이의 숫자여야 하므로
    아닐 경우 1~45 사이의 숫자를 입력해달라는 메시지 출력

--- src/main/java/lotto/exception/WinningNumbersException.java
+++ src/main/java/lotto/exception/WinningNumbersException.java
@@ -6,6 +6,8 @@ public class WinningNumbersException {
 
     private static final int WINNING_NUMBERS_SIZE = 6;
     private static final String NUMBER_REGEX = "[0-9]+";
+    private static final int MAX_WINNING_NUMBER = 45;
+    private static final int MIN_WINNING_NUMBER = 1;
 
     public static void checkWinningNumbers(String winningNumbersInput) {
         CommonInputException.checkCommonInput(winningNumbersInput, "당첨 번호를");
@@ -24,12 +26,19 @@ public class WinningNumbersException {
         for (String winningNumber : winningNumbers) {
             CommonInputException.checkHasBlankInInput(winningNumber, "당첨 번호를");
             checkWinningNumberDigit(winningNumber);
+            checkWinningNumberRange(Integer.parseInt(winningNumber));
         }
     }
 
     private static void checkWinningNumberDigit(String winningNumber) {
         if (!winningNumber.matches(NUMBER_REGEX)) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자를 입력해주세요.");
+        }
+    }
+
+    private static void checkWinningNumberRange(Integer winningNumber) {
+        if (winningNumber < MIN_WINNING_NUMBER || winningNumber > MAX_WINNING_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1~45 사이의 숫자를 입력해주세요.");
         }
     }
 }
