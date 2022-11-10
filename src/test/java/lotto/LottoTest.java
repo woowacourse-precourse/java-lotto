6998ec343101d1663b477c26650c1a14355c4d00
sangwon <swchoi1997@naver.com>
2022-11-10 09:13:07 +0900

    test: 로또 번호가 정렬되지 않을 시 예외 기능 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -30,9 +30,25 @@ class LottoTest {
 
     @DisplayName("로또 번호에 범위에 벗어나는 숫자가 있으면 예외가 발생한다. : 1보다 작은 수")
     @Test
-    void createLottoByOutBoundNumber() {
+    void createLottoByOutBoundNumberUnderZero() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> new Lotto(List.of(0, 1, 2, 3, 4, 5)));
         assertThat(exception.getMessage()).isEqualTo("[ERROR] Lotto Number Have Out Bound Number.");
+    }
+
+    @DisplayName("로또 번호에 범위에 벗어나는 숫자가 있으면 예외가 발생한다. : 1보다 큰 수")
+    @Test
+    void createLottoByOutBoundNumberUpper45() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new Lotto(List.of(0, 1, 2, 3, 4, 50)));
+        assertThat(exception.getMessage()).isEqualTo("[ERROR] Lotto Number Have Out Bound Number.");
+    }
+
+    @DisplayName("로또가 오름차순으로 정렬되지 않으면 예외가 발생한다.")
+    @Test
+    void createLottoByNotSortedByAsc(){
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new Lotto(List.of(9, 1, 8, 3, 5, 4)));
+        assertThat(exception.getMessage()).isEqualTo("[ERROR] Lotto Number Not Sorted by ASC.");
     }
 }
