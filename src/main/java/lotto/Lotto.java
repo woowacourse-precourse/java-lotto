c6134c10982fa46bb288c8b89e0dae840f658e6e
HyebinShin <hyeviin.dev@gmail.com>
2022-11-10 19:21:06 +0900

    Feat: 로또 발행 클래스 Lotto 분리
    로또 구입 금액을 매개 변수로 로또 발행 횟수 결정
    로또 발행 횟수만큼 반복 시행하여 로또 List 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Lotto {
@@ -18,9 +22,52 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    //로또 발행
+    public static List<Lotto> getLotto(String budget) {
+        List<Lotto> lottos = new ArrayList<>();
+        int publishNum = getPublishNum(budget);
+
+        while(lottos.size() < publishNum) {
+            lottos.add(new Lotto(getLottoNum()));
+        }
+
+        validateLotto(lottos, publishNum);
+
+        return lottos;
+    }
+
+    private static int parseBudget(String budget) {
+        return Integer.parseInt(budget);
+    }
+
+    public static int getPublishNum(String budget) {
+        return parseBudget(budget)/1000;
+    }
+
+    private static List<Integer> getLottoNum() {
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+
+        return sortNumbers(numbers);
+    }
+
+    public static List<Integer> sortNumbers(List<Integer> numbers) {
+        numbers.sort(Comparator.naturalOrder());
+
+        return numbers;
+    }
+
+    private static void validateLotto(List<Lotto> lotto, int publishNum) {
+        if(lotto.size()!=publishNum) {
+            throw new IllegalArgumentException(ErrMsg.VALIDATE_LOTTO_SIZE);
+        }
+    }
 
     @Override
     public String toString() {
         return Arrays.toString(numbers.toArray());
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 }
