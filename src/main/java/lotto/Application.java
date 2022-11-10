fbf31cde270254940a2eaff86d9090b6dffe1540
eugene1204 <s1061001@naver.com>
2022-11-10 17:28:07 +0900

    feat : Lotto 클래스, toString Override

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,11 @@
 package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.stream.*;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
