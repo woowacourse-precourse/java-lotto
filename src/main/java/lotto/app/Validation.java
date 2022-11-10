0465301d56a98d8f45c352510c68f197965ba839
Joonsoo Kim <kjsoo1901@gmail.com>
2022-11-10 18:51:43 +0900

    feat: Add isInRange in Validation.java

--- src/main/java/lotto/app/Validation.java
+++ src/main/java/lotto/app/Validation.java
@@ -8,4 +8,13 @@ public class Validation {
             return false;
         return true;
     }
+
+    public boolean isInRange(List<Integer> numbers){
+        for(int i = 0; i < 6; i++){
+            if( 1 > numbers.get(i) || numbers.get(i) > 45){
+                return false;
+            }
+        }
+        return true;
+    }
 }
