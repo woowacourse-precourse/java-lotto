68e8d1c10b161d9adedaee9b33b29c2b63de76ed
anneu <b.imnuj@gmail.com>
2022-11-11 04:49:32 +0900

    feat: 로또를 발행하는 기능 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,5 +1,13 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+import static lotto.utils.ConstantUtil.LOTTO_NUMBER_SIZE;
+import static lotto.utils.ConstantUtil.MAX_LOTTO_NUMBER;
+import static lotto.utils.ConstantUtil.MIN_LOTTO_NUMBER;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoMachine {
     private static final int MIN_MONEY = 1000;
     private static final String MIN_MONEY_ERROR_MESSAGE = "[ERROR] 구입금액은 1000원 이상부터 가능합니다.";
@@ -13,6 +21,25 @@ public class LottoMachine {
     public void inputMoney(int money) {
         validate(money);
         this.money = money;
+    }
+
+    public List<Lotto> publishLotto() {
+        List<Lotto> allLotto = new ArrayList<>();
+
+        while (money != 0) {
+            allLotto.add(new Lotto(createRandomNumbers()));
+            spendMoney();
+        }
+
+        return allLotto;
+    }
+
+    private void spendMoney() {
+        money -= MIN_MONEY;
+    }
+
+    private List<Integer> createRandomNumbers() {
+        return pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_SIZE);
     }
 
     private void validate(int money) {
