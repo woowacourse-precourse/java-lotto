c212228dd02f9bb01753efe4ae8e451854769216
tjwjdgus12 <01048733438a@gmail.com>
2022-11-10 20:02:34 +0900

    refactor: description에 reward 중복 정보 제거

--- src/main/java/lotto/LottoRanking.java
+++ src/main/java/lotto/LottoRanking.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import java.text.DecimalFormat;
+
 public enum LottoRanking {
-    RANK_5("3개 일치 (5,000원)", 5000),
-    RANK_4("4개 일치 (50,000원)", 50000),
-    RANK_3("5개 일치 (1,500,000원)", 1500000),
-    RANK_2("5개 일치, 보너스 볼 일치 (30,000,000원)", 30000000),
-    RANK_1("6개 일치 (2,000,000,000원)", 2000000000);
+    RANK_5("3개 일치", 5000),
+    RANK_4("4개 일치", 50000),
+    RANK_3("5개 일치", 1500000),
+    RANK_2("5개 일치, 보너스 볼 일치", 30000000),
+    RANK_1("6개 일치", 2000000000);
 
     private final String description;
     private final int reward;
@@ -15,11 +17,13 @@ public enum LottoRanking {
         this.reward = reward;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     public int getReward() {
         return reward;
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat formatter = new DecimalFormat("###,###");
+        return description + " (" + formatter.format(reward) + ")";
     }
 }
