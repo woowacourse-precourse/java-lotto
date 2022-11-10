cde2cbdd2918dd9a415c6266c8d25cfa13874706
YIMHYEONGSUP <hs.yim@illb.co.kr>
2022-11-11 08:33:29 +0900

    feat - 로또 당첨 여부 확인

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -15,6 +17,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
 
         System.out.println("구입 금액을 입력 해주세요.");
         // 구입 금액 입력 받기
@@ -31,10 +34,39 @@ public class Application {
         // 당첨 번호 , 보너스 번호 입력 받기
         Set<Integer> winnerNumber = setWinnerNumber();
         System.out.println("winnerNumber = " + winnerNumber);
+
         int bonusNumber = setBonusNumber();
         System.out.println("bonusNumber = " + bonusNumber);
 
+        System.out.println("보너스 넘버 완료 ================");
 
+        // 로또 당첨 여부 확인
+        List<List<Integer>> result = checkLotto(lottos, winnerNumber, bonusNumber);
+        System.out.println("result = " + result);
+
+
+    }
+
+    private static List<List<Integer>> checkLotto(List<List<Integer>> lottos, Set<Integer> winnerNumber, int bonusNumber) {
+        List<List<Integer>> resultLotto = new ArrayList<>();
+        lottos.forEach(lotto -> {
+            resultLotto.add(checkNumber(lotto, winnerNumber, bonusNumber));
+        });
+        return resultLotto;
+    }
+
+    private static List<Integer> checkNumber(List<Integer> lotto, Set<Integer> winnerNumber, int bonusNumber) {
+        AtomicInteger rightNumbers = new AtomicInteger();
+        AtomicInteger rightBonusNumber = new AtomicInteger();
+        lotto.forEach(number -> {
+            if(winnerNumber.contains(number)){
+                rightNumbers.getAndIncrement();
+            }
+            if (bonusNumber == number) {
+                rightBonusNumber.getAndIncrement();
+            }
+        });
+      return List.of(rightNumbers.intValue(), rightBonusNumber.intValue());
     }
 
     private static int setBonusNumber() {
@@ -67,7 +99,8 @@ public class Application {
     }
 
     private static boolean validWinnerNumber(String input) {
-        if(Pattern.matches("(\\d,){5}\\d" , input)){
+        // 정규표현식 1 ~ 45 의 수 확인하기 -> 숫자 검증 로직을 별도 생성
+        if(Pattern.matches("(\\d,){5}\\d", input)){
             return true;
         }
         throw new IllegalArgumentException(ExceptionMessage.INVALID_INPUT_VALUE.message());
