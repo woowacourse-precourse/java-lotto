6751640d4b74a3d3e760201293cccc4d25720169
WhalesBob <ndw8200@naver.com>
2022-11-10 16:21:01 +0900

    feat(Lotto) : add codes for sorting List

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -12,6 +13,7 @@ public class Lotto {
         validateDuplicateNumberExist(numbers);
         validateNumbersInRange(numbers);
         this.numbers = numbers;
+        numbers.sort(Comparator.naturalOrder());
     }
 
     private void validateLottoSize(List<Integer> numbers) {
