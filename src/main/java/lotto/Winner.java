5c9cdc1b0a9d0b9085c1b74a291aa07a6ded018b
areyouhun <areyouhun@gmail.com>
2022-11-10 22:21:16 +0900

    feat(winner): 보너스 번호가 당첨 번호에 이미 있으면 예외 발생

--- src/main/java/lotto/Winner.java
+++ src/main/java/lotto/Winner.java
@@ -3,17 +3,22 @@ package lotto;
 import java.util.List;
 
 public class Winner {
-//    private final int bonusNumber;
+//    private final integer bonusNumber;
 //    private final List<Integer> winningNumbers;
 
-    public Winner(String winningNumbers, int bonusNumber) {
-        validateFormat(winningNumbers);
+    public Winner(String winningNumbers, String bonusNumber) {
     }
 
     private void validateFormat(String winningNumbers) {
         String[] winningNumbersSplited = winningNumbers.split(",");
         if (winningNumbersSplited.length != 6) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 쉼표(,)를 기준으로 구분되는 6자리의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateBonusNumber(List<Integer> winningNumbers, Integer bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호가 이미 당첨 번호에 포함되어 있습니다.");
         }
     }
 }
