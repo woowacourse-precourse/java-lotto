50f10ffcbd98ac56ca7d659b15e6506e6672b2bf
lunchRamen <bloom6561@naver.com>
2022-11-11 04:40:21 +0900

    feat(Print): 출력 관련 로직 전용 클래스 생성 후 구입금액, 로또 구입갯수 출력 로직 생성 후 리팩토링

---
+++ src/main/java/lotto/Print.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class Print {
+
+    public static void PrintUserPurchaseAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void PrintUserPurchaseLottoAmount(Integer lottoMoney){
+        System.out.println(lottoMoney+"개를 구매했습니다.");
+    }
+}
