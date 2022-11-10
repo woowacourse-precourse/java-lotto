9b427feee1f6449bc870a0e2d74e30166f9dc378
LHS-11 <ghktjq1119@naver.com>
2022-11-11 01:39:08 +0900

    [Feat] : 4-1 로또 번호 생성 예외처리 ( 중복 번호 및 범위 체크)

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Lotto {
@@ -18,6 +20,31 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void checkLottoNumberRange(List<Integer> numbers){
+        for (Integer number : numbers) {
+            if(number<1 || number>45){
+                throw new IllegalArgumentException("[ERROR] : 로또 번호의 범위를 벗어났습니다.");
+            }
+        }
+    }
+
+    private void checkDuplicatedLottoNumber(List<Integer> numbers){
+        Set<Integer> notDuplicatedNumber = getNotDuplicatedNumber(numbers);
+        if(numbers.size()==notDuplicatedNumber.size()){
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] : 로또 번호에 중복 숫자가 없어야 합니다.");
+    }
+
+    private static Set<Integer> getNotDuplicatedNumber(List<Integer> numbers) {
+        Set<Integer> notDuplicatedNumber = new HashSet<>();
+        for (Integer number : numbers) {
+            notDuplicatedNumber.add(number);
+        }
+        return notDuplicatedNumber;
+    }
+
+
     public List<Integer> getNumbers(){
         return this.numbers;
     }
