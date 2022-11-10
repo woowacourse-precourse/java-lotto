f719a554a863451b6f50a6a2b1f49f901044832d
YuHayeon <youha0930@gmail.com>
2022-11-11 01:08:03 +0900

    test(WinningNumbersException): 당첨 번호의 숫자가 6개인지 테스트
    당첨 번호의 숫자가 6개가 입력되었는지 테스트

--- src/test/java/lotto/exception/WinningNumbersExceptionTest.java
+++ src/test/java/lotto/exception/WinningNumbersExceptionTest.java
@@ -2,9 +2,27 @@ package lotto.exception;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class WinningNumbersExceptionTest {
+
+    @Test
+    void 당첨번호_숫자_6개_확인() {
+
+        assertThatThrownBy(() -> WinningNumbersException.checkRightWinningNumbersSize(List.of("23")))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> WinningNumbersException.checkRightWinningNumbersSize(List.of("1", "3")))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> WinningNumbersException.checkRightWinningNumbersSize(List.of("1", "45", "22")))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() ->
+                WinningNumbersException.checkRightWinningNumbersSize(List.of("1", "45", "22", "1", "21", "38", "34")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @Test
     void 당첨번호_숫자인지_확인() {
