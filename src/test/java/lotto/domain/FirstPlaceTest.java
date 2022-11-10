b276faa7421fbb8f40d8b3bf7ea5a53ef8318ec6
sangwon <swchoi1997@naver.com>
2022-11-10 17:43:36 +0900

    test: 경계값 테스트 추가, 예외 테스트 케이스 추가
    0,1,45,46에서 경계값 테스트 추가
    유요숫자 범위 테스트 추가
    중복값 입력 테스트 추가

--- src/test/java/lotto/domain/FirstPlaceTest.java
+++ src/test/java/lotto/domain/FirstPlaceTest.java
@@ -9,16 +9,33 @@ import org.junit.jupiter.api.Test;
 
 class FirstPlaceTest {
 
-    @DisplayName("객체 생성 테스트")
+    @DisplayName("객체 생성 테스트1")
     @Test
     void createFirstPlace() {
         String firstPlace = "1,2,3,4,5,6";
         assertThat(new FirstPlace(firstPlace)).isEqualTo(new FirstPlace("1,2,3,4,5,6"));
     }
 
+    @DisplayName("객체 생성 테스트2")
+    @Test
+    void createFirstPlace2() {
+        String firstPlace = "10,21,31,45,15,36";
+        assertThat(new FirstPlace(firstPlace)).isEqualTo(new FirstPlace("10,21,31,45,15,36"));
+    }
+
+
+    @DisplayName("쉼표로 숫자를 구분하지 않으면 예외가 발생한다.")
+    @Test
+    void createFirstPlaceByNotDivideCommas1(){
+        String firstPlace = "1,2,3,4,5,6,,,";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new FirstPlace(firstPlace));
+        assertThat(exception.getMessage()).isEqualTo("[ERROR] Please DIVIDE Number With Commas");
+    }
+
     @DisplayName("쉼표로 숫자를 구분하지 않으면 예외가 발생한다. : 쉼표랑 다른 구분자 있을 때")
     @Test
-    void createFirstPlaceByNotDivideCommas() {
+    void createFirstPlaceByNotDivideCommas2() {
         String firstPlace = "1,2,3,4/5/6";
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> new FirstPlace(firstPlace));
@@ -32,5 +49,32 @@ class FirstPlaceTest {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> new FirstPlace(firstPlace));
         assertThat(exception.getMessage()).isEqualTo("[ERROR] Please DIVIDE Number With Commas");
+    }
+
+    @DisplayName("유요 숫자 범위 내 숫자를 입력하지 않으면 예외가 발생한다. 0 포함")
+    @Test
+    void createFirstPlaceByOutBoundNumber1() {
+        String firstPlace = "0,1,2,3,4,5";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new FirstPlace(firstPlace));
+        assertThat(exception.getMessage()).isEqualTo("[ERROR] Lotto Number Have Out Bound Number.");
+    }
+
+    @DisplayName("유요 숫자 범위 내 숫자를 입력하지 않으면 예외가 발생한다. 46이상 포함")
+    @Test
+    void createFirstPlaceByOutBoundNumber2() {
+        String firstPlace = "3,1,46,7,4,5";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new FirstPlace(firstPlace));
+        assertThat(exception.getMessage()).isEqualTo("[ERROR] Lotto Number Have Out Bound Number.");
+    }
+
+    @DisplayName("중복된 숫자를 입력하면 예외가 발생한다.")
+    @Test
+    void createFirstPlaceByDuplicateNumber() {
+        String firstPlace = "1,2,3,4,4,5";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new FirstPlace(firstPlace));
+        assertThat(exception.getMessage()).isEqualTo("[ERROR] Lotto Number Have Duplicate Number.");
     }
 }
