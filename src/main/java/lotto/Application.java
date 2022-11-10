2ed452a5803afb2f6b53a23cb6f99dcefdf5f214
sunhokim1 <srrsr14@gmail.com>
2022-11-11 06:07:04 +0900

    feat : 로또 랜덤숫자 오름차순으로 정렬하는 메서드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,6 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         DataProcessing dataProcessing = new DataProcessing();
-        dataProcessing.lottoNumbers();
+        dataProcessing.sortLottoNumbers();
     }
 }
--- src/main/java/lotto/DataProcessing.java
+++ src/main/java/lotto/DataProcessing.java
@@ -1,12 +1,20 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
 import java.util.List;
 
 public class DataProcessing {
     public List<Integer> lottoNumbers() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        System.out.println(numbers);
         return numbers;
+    }
+
+    public List<Integer> sortLottoNumbers() {
+        List<Integer> sortNumber = lottoNumbers();
+        Collections.sort(sortNumber);
+        System.out.println(sortNumber);
+        return sortNumber;
     }
 }
