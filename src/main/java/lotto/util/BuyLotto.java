b74b2a2d72c9b7b54413c0df72d098097070b9c8
kki4504 <rhkddlf9876@gmail.com>
2022-11-10 16:58:05 +0900

    feat(BuyLotto) : 로또 구매 기능 클래스 생성

--- src/main/java/lotto/util/BuyLotto.java
+++ src/main/java/lotto/util/BuyLotto.java
@@ -1,2 +1,15 @@
-package lotto.util;public class BuyLotto {
+package lotto.util;
+
+import lotto.Lotto;
+import lotto.util.input.Money;
+
+import java.util.List;
+
+public class BuyLotto {
+    List<Lotto> lottos;
+
+    public BuyLotto(Money money) {
+
+    }
+
 }
