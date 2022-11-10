7cf4b49161e9763d2c9e313782b9583c8ba4b9f6
sunhokim1 <srrsr14@gmail.com>
2022-11-11 05:46:35 +0900

    feat : split사용해 , 를 구분자로 나누어주는 함수 추가

--- src/main/java/lotto/InputUtil.java
+++ src/main/java/lotto/InputUtil.java
@@ -5,9 +5,13 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputUtil {
-    
     public String inputNumber() {
         String inputNumbers = readLine();
         return inputNumbers;
+    }
+    public String[] splitNumber() {
+        String numbers = inputNumber();
+        String[] arrayNumbers = numbers.split(",");
+        return arrayNumbers;
     }
 }
