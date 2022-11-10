1289ba0ec84314690f36e57a1814bef43d206c04
TaeMin <xoals3094@naver.com>
2022-11-11 02:53:03 +0900

    fix(Lotto): 1~45 사이의 숫자 검증 코드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,11 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        for (int number: numbers){
+            if(!(number >= 1 && number <= 45)){
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
     public List<Integer> getNumbers() {
