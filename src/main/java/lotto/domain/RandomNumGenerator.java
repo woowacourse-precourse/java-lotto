4c9406f3486285e235df4972c2a3cac473e4a134
woosung1223 <dntjd991223@naver.com>
2022-11-10 21:51:22 +0900

    feat(RandomNumGenerator): 클래스 및 makeUniqueRandomList 메소드 생성

---
+++ src/main/java/lotto/domain/RandomNumGenerator.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.util.Constant;
+
+import java.util.List;
+
+public final class RandomNumGenerator {
+    private RandomNumGenerator() {}
+
+    public static List<Integer> makeUniqueRandomList() {
+        return Randoms.pickUniqueNumbersInRange(
+                Constant.START_INCLUSIVE,
+                Constant.END_INCLUSIVE,
+                Constant.LOTTO_SIZE
+        );
+    }
+}
