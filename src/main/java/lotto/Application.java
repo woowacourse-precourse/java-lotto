5827b9b596ee79b09c0669c6806047fdbf1604d5
jwp345 <jwp135@naver.com>
2022-11-10 21:30:35 +0900

    feat: 로또 구입 금액 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int price = askPrice();
+        List<Lotto> lottos = makeLotto(price);
     }
+
+    public static List<Lotto> makeLotto(int price) {
+        if(price % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] price must divided by 1000");
+        }
+        int amount = price / 1000;
+        List<Lotto> lottos = new ArrayList<>();
+
+        return lottos;
+    }
+
+    public static int askPrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = readLine();
+
+        return Integer.parseInt(input);
+    }
+
 }
