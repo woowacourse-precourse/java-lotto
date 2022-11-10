4f27d006c0374d3d450ade544890efbb1a9d9465
jinny-l <jinny.l.dev@gmail.com>
2022-11-10 13:43:08 +0900

    fix(LottoGenerator): UnsupportedOperationException 에러 수정

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import lotto.constant.Constant;
@@ -8,20 +9,22 @@ import lotto.constant.Constant;
 public class LottoGenerator {
 
     public static List<Integer> getWinningNumbers() {
-        List<Integer> winningNumbers = generateNumbers();
+        List<Integer> winningNumbers = new ArrayList<>(generateNumbers());
         winningNumbers.addAll(generateBonusNumber());
 
         return winningNumbers;
     }
 
     private static List<Integer> generateBonusNumber() {
-        return Randoms.pickUniqueNumbersInRange(Constant.LOTTO_START_NUMBER, Constant.LOTTO_END_NUMBER,
-                Constant.BONUS_NUMBER_SIZE);
+        return Randoms.pickUniqueNumbersInRange(
+                Constant.LOTTO_START_NUMBER, Constant.LOTTO_END_NUMBER, Constant.BONUS_NUMBER_SIZE);
     }
 
     private static List<Integer> generateNumbers() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(Constant.LOTTO_START_NUMBER, Constant.LOTTO_END_NUMBER,
-                Constant.LOTTO_SIZE);
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(
+                Constant.LOTTO_START_NUMBER, Constant.LOTTO_END_NUMBER, Constant.LOTTO_SIZE)
+        );
+
         Collections.sort(numbers);
 
         return numbers;
