121b110d094149c58a86b2cdb939a521a58fca26
소대훈 <theogns0681@naver.com>
2022-11-10 10:34:09 +0900

    [refactor] (Domain) 조건문 조건 변경

--- src/main/java/lotto/Domain.java
+++ src/main/java/lotto/Domain.java
@@ -13,11 +13,11 @@ public class Domain {
 
     int checkWinningCount(List<Integer> computers, int[] numbers) {
         int count = 0;
-        for (int i = 0; i < numbers.length - 1; i++) {
-            if (computers.contains(numbers[i])) count++;
+        for (int number : numbers) {
+            if (computers.contains(number)) count++;
         }
-        if (Objects.equals(count,6)) return 7;
-        if (Objects.equals(count,5) || computers.contains(numbers[6])) return 6;
+        if (Objects.equals(count, 6)) return 7;
+        if (Objects.equals(count, 5) && computers.contains(numbers[6])) return 6;
         return count;
     }
 
