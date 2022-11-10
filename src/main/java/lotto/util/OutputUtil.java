e1a69728c0e96f2d339cb4ed8193a357f372a61f
KimDongho <hkim4410@naver.com>
2022-11-10 22:19:47 +0900

    feat(OutputUtil): 에러 메시지를 출력하는 기능 추가

--- src/main/java/lotto/util/OutputUtil.java
+++ src/main/java/lotto/util/OutputUtil.java
@@ -15,6 +15,8 @@ public class OutputUtil {
     private final String RESULT_SECOND = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
     private final String RESULT_FIRST = "6개 일치 (2,000,000,000원) - ";
 
+    private final String ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+
     public void printUserLottoCount(int lottoCount) {
         System.out.println("" + lottoCount + "개를 구매했습니다.");
     }
@@ -31,6 +33,10 @@ public class OutputUtil {
         System.out.println(RESULT_THIRD + ranks.get(Number.THREE) + "개");
         System.out.println(RESULT_SECOND + ranks.get(Number.TWO) + "개");
         System.out.println(RESULT_FIRST + ranks.get(Number.ONE) + "개");
+    }
+
+    public void printErrorMessage() {
+        System.out.println(ERROR_MESSAGE);
     }
 
     public void printYield(double yield) {
