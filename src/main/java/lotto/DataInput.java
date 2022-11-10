792774a0a8e4b60124b61ea03c3bbc9faae5db3f
SimSimEEE <rebe123@naver.com>
2022-11-11 04:30:31 +0900

    feat(DataInput.class): add function to get purchase amount

--- src/main/java/lotto/DataInput.java
+++ src/main/java/lotto/DataInput.java
@@ -4,12 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class DataInput {
     private static final String VALID_INPUT_PATTERN = "[^0-9]";
-    private static final String VALID_UNIT_PATTERN = "[0-9]$";
+    private static final String VALID_UNIT_PATTERN = "0{3}$";
     private static final String ERROR_MESSAGE_NUMBER = "[ERROR] 숫자를 입력해주세요";
     private static final String ERROR_MESSAGE_UNIT = "[ERROR] 1000원 단위로 입력해주세요";
     private static final String ERROR_MESSAGE_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자를 입력해주세요";
     private static final int MAXIMUM_NUMBER_OF_LOTTO = 45;
     private static final int MINIMUM_NUMBER_OF_LOTTO = 1;
+    private static final String MESSAGE_TO_INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+
+    public static int PurchaseAmount() {
+        String input = UserInput(MESSAGE_TO_INPUT_PURCHASE_AMOUNT);
+        ValidateNumber(input);
+        ValidateAmountUnit(input);
+
+        return Integer.parseInt(input);
+    }
+
     public static String UserInput(String Message) {
         System.out.println(Message);
         String input = Console.readLine();
