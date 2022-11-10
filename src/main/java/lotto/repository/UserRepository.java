68498b105c34c34e29c1451081c813c676a1d1d9
jjmin9797 <jjmin9797@naver.com>
2022-11-10 22:02:51 +0900

    feat(UserRepository.java): setUserOwnLotto 기능 구현 입력 받은 로또를 사용자가 가지고 있는 로또 리스트에 저장하는 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

---
+++ src/main/java/lotto/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package lotto.repository;
+
+import lotto.domain.Lotto;
+import lotto.domain.User;
+
+import java.util.ArrayList;
+
+public class UserRepository {
+
+    private User user = new User();
+
+
+
+    public void setUserOwnLotto(Lotto lotto) {
+        ArrayList<Lotto> OriginalLotties = new ArrayList<>();
+        OriginalLotties.add(lotto);
+        this.user.setLotties(OriginalLotties);
+    }
+
+}
