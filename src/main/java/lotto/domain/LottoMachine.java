8c60b7a15ec1f1361d7c955d4fcc538e64ebb777
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-10 21:33:38 +0900

    feat(LottoMachine): LottoMachine 객체 생성
    LottoMachine:
    - 입력된 값만큼 LottoTicket 발행 및 저장
    - 저장중인 LottoTickets 값 리턴

---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class LottoMachine {
+	private static final int MIN_NUMBER = 1;
+	private static final int MAX_NUMBER = 45;
+	private static final int LOTTO_NUMBERS_SIZE = 6;
+
+	private final List<Lotto> lottoTickets;
+
+	public LottoMachine(int getNumberOfTickets) {
+		List<Lotto> newLottoTickets = publishLottoTickets(getNumberOfTickets);
+		this.lottoTickets = new ArrayList<>(newLottoTickets);
+	}
+
+	public List<Lotto> getLottoTickets() {
+		return Collections.unmodifiableList(lottoTickets);
+	}
+
+	private List<Lotto> publishLottoTickets(int getNumberOfTickets) {
+		List<Lotto> list = new ArrayList<>();
+		for (int i = 0; i < getNumberOfTickets; i++) {
+			list.add(new Lotto(Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_NUMBERS_SIZE)));
+		}
+		return list;
+	}
+
+}
