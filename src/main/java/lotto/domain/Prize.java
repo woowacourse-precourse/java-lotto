025fee70e37cdd7b10aab16163a78adfac327c94
TaeMin <xoals3094@naver.com>
2022-11-10 19:29:17 +0900

    feat(Prize): 상금 관련 enum형 상수 추가

---
+++ src/main/java/lotto/domain/Prize.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+enum Prize{
+    FIRST(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FOURTH(50_000),
+    FIFTH(5_000),
+    LAST(0);
+
+    private final int prize;
+    private Prize(int prize){
+        this.prize = prize;
+    }
+
+    public int getPrize(){
+        return this.prize;
+    }
+}
