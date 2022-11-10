ffcc1c084633922cb7407569b1f1b3d52f6d1660
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-11 01:51:52 +0900

    Feat(CheckInputException) : 1000으로 나누어떨어지지 않으면 예외 처리 기능 추가 #17
    checkBuyerInputIsNotDivided(final String input) : 입력한 input이 1000으로 나누어 나머지가 0이 아니라면 IllegalArgumentException예외가 발생하는 기능 추가

--- src/main/java/lotto/CheckInputException.java
+++ src/main/java/lotto/CheckInputException.java
@@ -2,18 +2,26 @@ package lotto;
 
 class CheckInputException {
 
+    private static final String ERROR_MESSAGE = "[ERROR]";
     public static void checkBuyerInputIsMinus(final String input) throws IllegalArgumentException{
 
         final Long money = Long.valueOf(input);
 
         if (money < 0) {
-            throw new IllegalArgumentException("[ERROR] 입력하신 돈이 음수입니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 입력하신 돈이 음수입니다.");
         }
     }
 
     public static void checkBuyerInputIsNotNumber(final String input) throws IllegalArgumentException{
-        if (Util.isNumberic(input) == false)
-            throw new IllegalArgumentException("[ERROR] 입력하신 문자열은 숫자열이 아닙니다.");
+        if (Util.isNumberic(input) == false) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 입력하신 문자열은 숫자열이 아닙니다.");
+        }
+    }
+
+    public static void checkBuyerInputIsNotDivided(final String input) throws IllegalArgumentException{
+        if (Util.isDividedBuyThousand(input) == false){
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 1000으로 나누어 떨어지지 않습니다.");
+        }
     }
 
 
