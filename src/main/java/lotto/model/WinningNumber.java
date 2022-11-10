6cd5399ea02e338cf67f3ba0e040b1706a511065
Eunkeee <eunkeeeee@gmail.com>
2022-11-10 15:21:40 +0900

    refactor: 메소드 분리
    separateStringByComma, convertStringListToIntList, inputWinningNumbers

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -1,31 +1,35 @@
 package lotto.model;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import lotto.view.InputView;
 
 public class WinningNumber {
 
     private List<Integer> winningNumbers;
 
     public WinningNumber() {
-        String input = removeSpace(inputWinningNumbers());
-        winningNumbers = Arrays.asList(input.split(",")).stream().map(s -> Integer.parseInt(s))
-                .collect(Collectors.toList());
-        if (winningNumbers.size() != 6) {
+        String input = removeSpace(InputView.inputWinningNumbers());
+        List<Integer> numbers = convertStringListToIntList(separateStringByComma(input));
+        if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
-        for (Integer winningNumber : winningNumbers) {
+        for (Integer winningNumber : numbers) {
             if (winningNumber < 1 || winningNumber > 45) {
                 throw new IllegalArgumentException();
             }
         }
-        System.out.println(winningNumbers);
+        this.winningNumbers = numbers;
     }
 
-    private static String inputWinningNumbers() {
-        return Console.readLine();
+    private static List<String> separateStringByComma(String input) {
+        return Arrays.asList(input.split(","));
+    }
+
+    private static List<Integer> convertStringListToIntList(List<String> list) {
+        return list.stream().map(s -> Integer.parseInt(s))
+                .collect(Collectors.toList());
     }
 
     private String removeSpace(String input) {
