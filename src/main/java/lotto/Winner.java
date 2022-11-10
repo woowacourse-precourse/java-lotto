f64a6356d120a6a3fba61ade2bacefac5a2786a9
areyouhun <areyouhun@gmail.com>
2022-11-11 02:16:59 +0900

    refactor(winner): winning numbers를 클래스 lotto를 통해 생성

--- src/main/java/lotto/Winner.java
+++ src/main/java/lotto/Winner.java
@@ -2,67 +2,63 @@ package lotto;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Winner {
     private final static String ONLY_NUMBER = "^\\d+$";
     private final Integer bonusNumber;
-    private final List<Integer> winningNumbers;
+    private final Lotto winningNumbers;
 
-    public Winner(String winningNumbers, String bonusNumber) {
-        String[] winningNumbersSplited = winningNumbers.split(",");
-        validateFormat(winningNumbersSplited);
-        for (String number : winningNumbersSplited) {
-            validateIfNumeric(number);
-        }
-        validateIfNumeric(bonusNumber);
+    public Winner(String numbersWithCommas, String number) {
+        String[] numbersSplited = numbersWithCommas.split(",");
+        validateFormat(numbersSplited);
+        winningNumbers = new Lotto(createWinningNumbersByNumbers(numbersSplited));
 
-        this.winningNumbers = new ArrayList<>();
-        updateWinningNumbers(winningNumbersSplited);
-        validateUniqueness(this.winningNumbers);
-        this.bonusNumber = Integer.valueOf(bonusNumber);
-        validateRange(this.bonusNumber);
+        validateIfNumeric(number);
+        bonusNumber = Integer.valueOf(number);
+        validateRange(bonusNumber);
         validateIfBonusNumberIncludedInWinningNumbers();
+    }
+
+    private List<Integer> createWinningNumbersByNumbers(String[] numbers) {
+        List<Integer> validNumbers = new ArrayList<>();
+        for (String number : numbers) {
+            validateIfNumeric(number);
+            Integer numericValue = Integer.valueOf(number);
+            validNumbers.add(numericValue);
+        }
+        return validNumbers;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers.getLottoNumbers();
+    }
+
+    public Integer getBonusNumber() {
+        return bonusNumber;
     }
 
     private void validateFormat(String[] numbers) {
         if (numbers.length != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 쉼표(,)를 기준으로 구분되어야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 쉼표(,)를 기준으로 구분됩니다.");
         }
     }
 
     private void validateIfNumeric(String input) {
         boolean isNumber = input.matches(ONLY_NUMBER);
         if (!isNumber) {
-            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값이 포함되어 있습니다. 공백이나 문자가 들어갔는지 확인해주세요.");
+            throw new IllegalArgumentException("[ERROR] 숫자 이외의 값이 입력되었습니다. 문자나 띄어쓰기 등이 들어갔는지 확인해주세요.");
         }
     }
 
     private void validateRange(Integer number) {
         if (number < 1 || number > 45) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-    }
-
-    private void updateWinningNumbers(String[] numbers) {
-        for (String number : numbers) {
-            Integer numericValue = Integer.valueOf(number);
-            validateRange(numericValue);
-            winningNumbers.add(numericValue);
-        }
-    }
-
-    private void validateUniqueness(List<Integer> numbers) {
-        List<Integer> numbersToBeValidated = numbers.stream()
-                .distinct()
-                .collect(Collectors.toList());
-        if (numbersToBeValidated.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 겹치는 번호가 존재합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자들로 이루어져야 합니다.");
         }
     }
 
     private void validateIfBonusNumberIncludedInWinningNumbers() {
-        if (winningNumbers.contains(bonusNumber)) {
+        List<Integer> numbers = winningNumbers.getLottoNumbers();
+        if (numbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 겹치는 번호가 존재합니다.");
         }
     }
