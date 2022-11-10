30d28c533a57aadbc168c978a4d10e7dd2a002b4
jjmin9797 <jjmin9797@naver.com>
2022-11-10 22:59:00 +0900

    test(UserServiceTest.java): calculateTheRateOfReturnTest 테스트 작성 상금과 구입금액을 입력하면 수익률을 반환하는 기능 테스트
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

---
+++ src/test/java/lotto/service/UserServiceTest.java
@@ -0,0 +1,21 @@
+package lotto.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+@DisplayName("[TEST] UserServiceTest")
+class UserServiceTest {
+    @Test
+    @DisplayName("상금과 구입금액을 입력하면 수익률을 반환한다.")
+    void calculateTheRateOfReturnTest() {
+        int prizeMoney = 5000;
+        int purchaseMoney = 8000;
+        UserService userService = new UserService();
+        assertThat(userService.calculateTheRateOfReturn(prizeMoney,purchaseMoney))
+                .isNotNaN()
+                .isPositive()
+                .isEqualTo(62.5);
+    }
+
+}
