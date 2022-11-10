e2c55ea7da3f2dcbfc38892c3b3349135aad4f18
소대훈 <theogns0681@naver.com>
2022-11-10 10:16:10 +0900

    [feat] (Domain) 당첨 개수 반환과 금액 계산

---
+++ src/main/java/lotto/Domain.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Domain {
+    void checkWinning(Computer[] computers, int[] list, int[] numbers) {
+        for (Computer computer : computers) {
+            int index = checkWinningCount(computer.getNumbers(), numbers);
+            list[index]++;
+        }
+    }
+
+    int checkWinningCount(List<Integer> computers, int[] numbers) {
+        int count = 0;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (computers.contains(numbers[i])) count++;
+        }
+        if (Objects.equals(count,6)) return 7;
+        if (Objects.equals(count,5) || computers.contains(numbers[6])) return 6;
+        return count;
+    }
+
+    Long checkWinningMoney(int[] list) {
+        long result = 0;
+        result += list[7] * 2000000000L;
+        result += list[6] * 30000000L;
+        result += list[5] * 1500000L;
+        result += list[4] * 50000L;
+        result += list[3] * 5000L;
+        return result;
+    }
+}
