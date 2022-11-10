754c19ee031480db592887967cb4f7e9ec3642d6
kevstevie <jianna442@gmail.com>
2022-11-10 22:50:28 +0900

    feat: Money 입력 함수 추가

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -1,6 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class UserInput {
-    private UserInput() {
+
+    private UserInput() {}
+
+    static public long inputMoney() {
+        String input = Console.readLine();
+        validateMoney(input);
+        return Long.parseLong(input);
     }
 }
