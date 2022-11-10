c4a526393369802cb28d8fe9bffed2c55183a831
swandevson <shai4956@gmail.com>
2022-11-11 07:29:10 +0900

    Test(ApplicationTest): 당첨 내역 출력
    - 당첨된 로또 개수 출력 테스트

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -98,6 +98,29 @@ class ApplicationTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("올바른 당첨 내역을 출력하는지 확인한다.")
+    @Test
+    void 출력_당첨_내역() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    User user = new User(1000);
+                    WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+
+                    Application.addWinnings(user, winningLotto);
+                    user.printWinnings();
+
+                    assertThat(output()).contains(
+                            "3개 일치 (5,000원) - 0개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 1개"
+                    );
+                },
+                List.of(1, 2, 3, 4, 5, 6)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
