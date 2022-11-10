d7e0870d31bb2da1aba3774adbd37b0b5868e1d1
Kwon-sang <kwonsg514@gmail.com>
2022-11-11 04:38:47 +0900

    [구입금액 입력 기능 구현]

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현c
     }
 
+    static String inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    static int transformStringNumberToInteger(String purchaseAmount) {
+        return Integer.parseInt(purchaseAmount);
+    }
 
 }
