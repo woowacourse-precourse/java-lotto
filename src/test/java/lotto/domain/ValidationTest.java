5254b7676cd30da6fc6d7e8c34d83d6b8d787631
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-10 11:07:16 +0900

    test : isAssignedCurrencyUnit 기능에 대한 테스트 메서드 구현

--- src/test/java/lotto/domain/ValidationTest.java
+++ src/test/java/lotto/domain/ValidationTest.java
@@ -8,19 +8,33 @@ import static org.assertj.core.api.Assertions.*;
 
 public class ValidationTest {
 
-	@DisplayName("스트링의 내용이 숫자로만 이루어져 있는지 검증한다")
-	@ParameterizedTest
-	@ValueSource(strings={"1,000", "10l", "ABab"})
-	void canVerifyNumberOnlyReturningFalse(String input) {
-		assertThat(Validation.isNumberOnly(input)).isFalse();
-	}
 
-	@DisplayName("스트링의 내용이 숫자로만 이루어져 있는지 검증한다")
+	@DisplayName("유저가 입력한 구입금액이 숫자로만 이루어져 있는지 검증한다")
 	@ParameterizedTest
 	@ValueSource(strings={"123", "10000000"})
 	void canVerifyNumberOnlyReturningTrue(String input) {
 		assertThat(Validation.isNumberOnly(input)).isTrue();
 	}
 
+	@DisplayName("유저가 입력한 구입금액이 숫자로만 이루어져 있는지 검증한다")
+	@ParameterizedTest
+	@ValueSource(strings={"1,000", "10l", "ABab"})
+	void canVerifyNumberOnlyReturningFalse(String input) {
+		assertThat(Validation.isNumberOnly(input)).isFalse();
+	}
+
+	@DisplayName("유저가 입력한 구입금액이 1000원보다 크고 1000원 단위인지 검증한다")
+	@ParameterizedTest
+	@ValueSource(strings={"1000", "880000"})
+	void canVerifyAssignedCurrencyUnitReturningTrue(String input) {
+		assertThat(Validation.isAssignedCurrencyUnit(input)).isTrue();
+	}
+
+	@DisplayName("유저가 입력한 구입금액이 1000원보다 크고 1000원 단위인지 검증한다")
+	@ParameterizedTest
+	@ValueSource(strings={"500", "3700", "885200"})
+	void canVerifyAssignedCurrencyUnitReturningFalse(String input) {
+		assertThat(Validation.isAssignedCurrencyUnit(input)).isFalse();
+	}
 
 }
