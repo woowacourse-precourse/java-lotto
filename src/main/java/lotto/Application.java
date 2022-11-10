4d01ae7a8eb8d57fef1ab1c21b121d119c676d20
Dan-HeeYoung9182 <ldan9182@gmail.com>
2022-11-10 21:36:37 +0900

    로또 리스트를 반복하여 당첨번호 숫자를 리턴하는 메서드를 콜하고 리턴되는 숫자를 리스트에 저장

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,7 +11,7 @@ public class Application {
     static int BONUS_NUMBER;
     static int numberOfPurchasedLotto;
     static List<Lotto> purchasedLotto =  new ArrayList<>();
-    static List<Integer> trackEachPlace = List.of(0,0,0,0,0);
+    static List<Integer> trackEachPlace = new ArrayList<>();
     static Lotto winning_number_lotto;
     static final String ENTER_THE_AMOUNT = "구입금액을 입력해 주세요.";
     static final String ENTER_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
@@ -19,18 +19,19 @@ public class Application {
     static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     static final String LOTTO_PURCHASE_MESSAGE = "개를 구매했습니다.";
     enum places{
-        FIRST_PLACE(2000000000, "6개 일치"),
-        SECOND_PLACE(30000000, "5개 일치, 보너스 볼 일치"),
-        THIRD_PLACE(1500000, "5개 일치"),
-        FOURTH_PLACE(50000, "4개 일치"),
-        FIFTH_PLACE(5000, "3개 일치");
+        FIRST_PLACE(2000000000, 6),
+        SECOND_PLACE(30000000, 5),
+        THIRD_PLACE(1500000, 5),
+        FOURTH_PLACE(50000, 4),
+        FIFTH_PLACE(5000, 3);
 
         final long price;
-        final String message;
+        final int numberMatch;
 
-        places(long price, String numberMatch){
+
+        places(long price, int numberMatch){
             this.price = price;
-            this.message = numberMatch;
+            this.numberMatch = numberMatch;
         }
     }
     public static void main(String[] args) {
@@ -38,6 +39,7 @@ public class Application {
         validateGivenNumbers();
         initializeBonusNumber();
         countMatchingNumber();
+        for (int i: trackEachPlace) System.out.println(i);
     }
 
     public static void enterTheAmount(){
@@ -94,7 +96,9 @@ public class Application {
     public static void countMatchingNumber(){
         for(Lotto lotto: purchasedLotto){
             int matchedNumber = returnMatchedNumber(lotto);
-            System.out.println("Matching number--> " + matchedNumber);
+            boolean containsBonusNumber = containsBonusNumber(lotto);
+            if(containsBonusNumber && matchedNumber==places.SECOND_PLACE.numberMatch) trackEachPlace.add(Integer.MAX_VALUE);
+            if(!containsBonusNumber) trackEachPlace.add(matchedNumber);
         }
     }
 
@@ -103,7 +107,6 @@ public class Application {
         for(int number: winning_number_lotto.getNumbers()){
             if(lotto.containsTheNumber(number)) count++;
         }
-
         return count;
     }
 
