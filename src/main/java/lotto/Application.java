a4cb87af3873b7d0dd8fbb2083f092e22c546826
Kwon-sang <kwonsg514@gmail.com>
2022-11-11 05:30:13 +0900

    [구입금액 입력 기능] 개발완료 및 입력 테스트 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,13 +7,42 @@ public class Application {
         // TODO: 프로그램 구현c
     }
 
-    static String inputPurchaseAmount() {
+    /* 구매금액 입력 관련 */
+    static Long purchase() {
+        String purchaseAmountInputStr = purchaseAmountInput();
+        validatePurchaseAmountInput(purchaseAmountInputStr);
+
+        Long purchaseAmountInput = transformStringNumberToLong(purchaseAmountInputStr);
+        return calculatePurchaseQuantity(purchaseAmountInput);
+    }
+
+    static String purchaseAmountInput() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
-    static int transformStringNumberToInteger(String purchaseAmount) {
-        return Integer.parseInt(purchaseAmount);
+    static Long transformStringNumberToLong(String purchaseAmount) {
+        return Long.parseLong(purchaseAmount);
+    }
+
+    static void validatePurchaseAmountInput(String purchaseAmountInput) {
+        if (!purchaseAmountInput.replaceAll("[0-9]", "").equals("")) {
+            throw new IllegalArgumentException("[ERROR] 구입금액은 숫자만 입력 가능합니다.");
+        }
+
+        if (purchaseAmountInput.length() == 0) {
+            throw new IllegalArgumentException("[ERROR] 구입금액을 입력해 주세요.");
+        }
+
+        Long intPurchaseAmountInput = transformStringNumberToLong(purchaseAmountInput);
+        if(intPurchaseAmountInput % 1000 != 0 || intPurchaseAmountInput == 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위로 입력해 주세요.");
+        }
+
+    }
+
+    static Long calculatePurchaseQuantity(Long purchaseAmount) {
+        return purchaseAmount / 1000;
     }
 
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.Application.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,4 +27,50 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("입력금액에 문자 포함시 예외 발생")
+    @Test
+    void 구매금액_문자포함() {
+        String input1 = "ds1000";
+        String input2 = "12d34";
+
+        assertThatThrownBy(()->validatePurchaseAmountInput(input1)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->validatePurchaseAmountInput(input2)).isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(()->validatePurchaseAmountInput(input1)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입금액은 숫자만 입력 가능합니다.");
+        assertThatThrownBy(()->validatePurchaseAmountInput(input2)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입금액은 숫자만 입력 가능합니다.");
+    }
+
+    @DisplayName("입력금액이 1000원 단위가 아닐 경우 예외 발생")
+    @Test
+    void 구매금액_1000원단위_아닌입력() {
+        String input1 = "1234";
+        String input2 = "12432422211534";
+        String input3 = "0";
+        String input4 = "999";
+
+        assertThatThrownBy(()->validatePurchaseAmountInput(input1)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->validatePurchaseAmountInput(input2)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->validatePurchaseAmountInput(input3)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->validatePurchaseAmountInput(input4)).isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(()->validatePurchaseAmountInput(input1)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 1000원 단위로 입력해 주세요.");
+        assertThatThrownBy(()->validatePurchaseAmountInput(input2)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 1000원 단위로 입력해 주세요.");
+        assertThatThrownBy(()->validatePurchaseAmountInput(input3)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 1000원 단위로 입력해 주세요.");
+        assertThatThrownBy(()->validatePurchaseAmountInput(input4)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 1000원 단위로 입력해 주세요.");
+    }
+
+    @DisplayName("입력안할 경우 예외 발생")
+    @Test
+    void 미입력() {
+        String input = "";
+        assertThatThrownBy(()->validatePurchaseAmountInput(input)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->validatePurchaseAmountInput(input)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입금액을 입력해 주세요.");
+    }
 }
