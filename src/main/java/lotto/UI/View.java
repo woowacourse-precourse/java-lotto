cad762520eb7f21e764d0328c349dc60167723d8
TaeMin <xoals3094@naver.com>
2022-11-10 20:39:16 +0900

    feat(View): 당첨 번호를 입력받는 기능 구현

--- src/main/java/lotto/UI/View.java
+++ src/main/java/lotto/UI/View.java
@@ -2,10 +2,25 @@ package lotto.UI;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class View {
     public int getInvestmentCash(){
         String input = Console.readLine();
 
         return Integer.parseInt(input);
+    }
+
+    public List<Integer> getWinningNumber(){
+        String input = Console.readLine();
+        String[] numbers = input.split(",");
+
+        List<Integer> winningNumber = new ArrayList<>();
+        for(String number: numbers){
+            winningNumber.add(Integer.parseInt(number));
+        }
+
+        return winningNumber;
     }
 }
