4b36a45345d19db2745279245bf8c268899ebab2
KIMSEI1124 <workju1124@gmail.com>
2022-11-10 15:12:12 +0900

    feat: InputService Update
    입력받은 금액에 예외 사항을 체크하고 없으면 숫자로 변환

--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -1,5 +1,18 @@
 package lotto.service;
 
-public class InputService {
+import lotto.exception.InputException;
 
+public class InputService {
+    public int inputMoneyToInteger(String inputMoney) {
+        moneyException(inputMoney);
+        return Integer.parseInt(inputMoney);
+    }
+
+    public boolean moneyException(String inputMoney) {
+        InputException inputException = new InputException();
+        if (inputException.notDigitException(inputMoney)) {
+            return true;
+        }
+        return false;
+    }
 }
