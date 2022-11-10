0c36c41ff4b3c2171554bda92c1f2d2557946963
whibaek <jibaek0513@gmail.com>
2022-11-10 23:09:08 +0900

    feat(Lotto): 보너스 번호 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private int bonusNumber;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -18,6 +19,14 @@ public class Lotto {
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
     }
 
     // TODO: 추가 기능 구현
