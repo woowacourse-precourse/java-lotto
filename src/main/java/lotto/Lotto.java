b337254ac4e382e61bb6798aa61beaf02730c7e6
chosunghyun18 <krismon@naver.com>
2022-11-10 18:55:26 +0900

    feat(lotto): add the generate the lotto number
    관련 목록 서로 다른 6자리의 숫자를 만든다
    Randoms 라이브러리 에서 제공하는 generateuniquenumbers 를 이용

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
@@ -25,9 +27,21 @@ public class Lotto {
         numbers.stream()
                 .filter(n -> !items.add(n))
                 .collect(Collectors.toSet());
-        if (items.size() != 3) {
+        if (items.size() != numbers.size()) {
             throw new IllegalArgumentException();
         }
     }
 
+    public List<Integer> generateLotto() {
+        return Randoms.pickUniqueNumbersInRange(1,
+                45,
+                6);
+    }
+
+    public void setNumbers(List<Integer> numbers){
+        this.numbers = numbers;
+    }
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
