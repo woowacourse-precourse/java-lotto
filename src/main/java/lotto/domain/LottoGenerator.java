dcad8a570c56e83fdbe1740e23bab5e035975aef
TaeMin <xoals3094@naver.com>
2022-11-10 18:50:42 +0900

    feat(Lotto Generator): 로또 번호 생성 기능 구현

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,9 @@
+package lotto.domain;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class LottoGenerator {
+    public List<Integer> createLottoNumber(){
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}
