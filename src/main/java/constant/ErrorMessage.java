bf0b052f9d4eb317665d1b9765ed1e3b3d2bf70d
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-10 13:40:35 +0900

    refactor(lotto): Enum클래스 public으로 수정

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -6,9 +6,9 @@ public enum ErrorMessage {
     ERROR_NOT_NUMBER("[ERROR] 숫자가 아닌 것이 입력되었습니다"),
     ERROR_INCORRECT_MONEY("[ERROR] 올바르지 못한 입력 금액입니다.");
 
-   private final String message;
+   private String message;
 
-    ErrorMessage(String message) {
+    private ErrorMessage(String message) {
         this.message=message;
     }
 
