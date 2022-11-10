2d20401e62081b12d8b6b805bfe57dbad25af5e1
penrose15 <seongju99@naver.com>
2022-11-10 18:35:50 +0900

    feat : add input

---
+++ src/main/java/lotto/view/View.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class View {
+
+    public String input() {
+        String value = readLine();
+        return value;
+    }
+}
+
