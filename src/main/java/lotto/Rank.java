20316007c51833b49544f5bd865368917bfdd327
jmj005 <jmj00005@naver.com>
2022-11-11 04:32:18 +0900

    refactor(Rank): enum 생성

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -6,6 +6,36 @@ import java.util.TreeMap;
 
 public class Rank {
 
+    enum Grade {
+        THREE(3, "5,000", false),
+        FOUR(4, "50,000", false),
+        FIVE(5, "1,500,000", false),
+        FIVE_BONUS(5, "30,000,000", true),
+        SIX(6, "2,000,000,000", false);
+
+        private final int matchCount;
+        private final String prize;
+        private final boolean check;
+
+        Grade(int matchCount, String prize, boolean check) {
+            this.matchCount = matchCount;
+            this.prize = prize;
+            this.check = check;
+        }
+
+        public int getMatchCount() {
+            return matchCount;
+        }
+
+        public String getPrize() {
+            return prize;
+        }
+
+        public boolean isCheck() {
+            return check;
+        }
+    }
+
     public static Map<Integer, Integer> getResult (List<Integer> winningNumbers, List<Lotto> lottoList, int bonusNumber) {
         Map<Integer, Integer> result = new TreeMap<>();
         for (Lotto lotto : lottoList) {
