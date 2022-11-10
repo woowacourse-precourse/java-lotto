9a6bf07b82a406f04c199970a4e6ef496b92d633
bsh1603 <bsh1603@github.com>
2022-11-10 17:25:50 +0900

    Feat: 사용자 요청 view 작성

---
+++ src/main/java/view/RequestUser.java
@@ -0,0 +1,16 @@
+package view;
+
+public class RequestUser {
+
+    public void requestMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void requestLottoNum(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void requestBonusNum(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}
