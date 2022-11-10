98b2ee4712622249f2f1dbfff3e974b6b3519ec4
Laeng <Laeng@users.noreply.github.com>
2022-11-10 23:32:04 +0900

    feat(MoneyInputView): view 구현을 위한 controller 와 view 추가
    다음 사항을 추가했습니다:
    ### LottoController
    showMoneyInput 메서드
    - 입력값에 대한 검증을 위한 메서드 호출
    validateInput 메서드
    - 간단한 입력 검증을 수행
    parseInt 메서드
    - 문자열을 숫자형으로 변환
    
    ### MoneyInputView
    show 메서드
    - 메세지 출력 및 사용자 입력 대기
    getResponse 메서드
    - 사용자 입력 데이터를 가져오기

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,9 +3,35 @@ package lotto.controller;
 import lotto.view.IntroduceView;
 import lotto.view.View;
 
+import java.util.regex.Pattern;
+
 public class LottoController {
     public void showIntroduce() {
         View view = new IntroduceView();
         view.Show();
+    }
+
+    public void showMoneyInput() {
+        View view = new MoneyInputView();
+        view.Show();
+
+        String response = view.getResponse();
+        validateInput("^(\\d+[0][0][0])$", response);
+
+        //TODO - 로또 번호 생성 service 호출
+    }
+
+    public static void validateInput(String regex, String input) {
+        if (!Pattern.matches(regex, input)) {
+            throw new IllegalArgumentException("안내에 따라 값을 입력해주세요.");
+        }
+    }
+
+    public static int parseInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
     }
 }
---
+++ src/main/java/lotto/controller/MoneyInputView.java
@@ -0,0 +1,20 @@
+package lotto.controller;
+
+import lotto.view.View;
+
+public class MoneyInputView extends View {
+    private String money;
+
+    @Override
+    public void Show() {
+        printLine("구입 금액을 입력해 주세요.");
+        printLine("구입 금액은 숫자만 입력할 수 있으며 1000원 단위로 구매할 수 있어요.");
+
+        this.money = readLine("ex) 8000");
+    }
+
+    @Override
+    public String getResponse() {
+        return this.money;
+    }
+}
