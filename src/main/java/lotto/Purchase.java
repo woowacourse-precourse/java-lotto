4e1500a765655b629c672c5cb4b098e64694a070
Eunkeee <eunkeeeee@gmail.com>
2022-11-10 12:24:25 +0900

    feat: 구입 금액 입력 받기

---
+++ src/main/java/lotto/Purchase.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Purchase {
+    public Purchase() {
+        int money = Integer.parseInt(Console.readLine());
+        System.out.println(money);
+    }
+}
