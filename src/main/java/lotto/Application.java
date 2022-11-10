1093873e4206f2d5ed8bf2d53ea36fcd7cc2d51e
gusu9 <suhan.ku9@gmail.com>
2022-11-10 10:29:31 +0900

    feat: change Lotto number creater with get function

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
-        ArrayList<Lotto> lotto = new Lotto();
-        lotto.getNumbers();
-        System.out.println();
+        Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        System.out.println(lotto.getNumbers());
+        System.out.println("test");
     }
 }
