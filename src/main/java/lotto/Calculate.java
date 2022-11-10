e9cbd0e86b66dc014ae2e11a4015c5ad7c0ec7b5
JangHyojoon <fdasfaj9@gmail.com>
2022-11-11 06:43:12 +0900

    기능 구현 및 테스트: 게임 기능 구현 수정 사항: 범위 오류 수정

--- src/main/java/lotto/Calculate.java
+++ src/main/java/lotto/Calculate.java
@@ -1,6 +1,45 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Calculate {
+
+  public void drawLotto(){
+    try {
+      LotteryMachine lotteryMachine = messageLottoPurchase();
+      InputNumbers inputNumbers = inputLottoNumberAndBonusNumber();
+      Lotto lotto = new Lotto(inputNumbers.lottoNumbers);
+      messageStatistics(lotteryMachine.lottoRepository, lotto.numbers, inputNumbers.bonusNumber);
+
+    } catch (IllegalArgumentException e){
+      System.out.println(e.getMessage());
+    }
+  }
+
+  private LotteryMachine messageLottoPurchase(){
+    InputMessage.messageAbleBuyMoney();
+    InputMoney inputMoney = new InputMoney(Console.readLine());
+
+    LotteryMachine lotteryMachine = new LotteryMachine(inputMoney.getAbleCount());
+    OutputMessage.messageGenerateLottoRepository(inputMoney.getAbleCount(), lotteryMachine.lottoRepository);
+
+    return lotteryMachine;
+  }
+
+  private InputNumbers inputLottoNumberAndBonusNumber(){
+    String numbers = InputMessage.messageLottoInput();
+    String num = InputMessage.messageBonusInput();
+    return new InputNumbers(numbers, num);
+  }
+
+  private void messageStatistics(List<List<Integer>> lottoRepository, List<Integer> numbers, Integer num){
+    Score calculateLotto = new Score(lottoRepository, numbers, num);
+    Float earning = calculateEarning(calculateLotto.ranking, lottoRepository.size());
+    OutputMessage.messageStatisticsMessage(calculateLotto.ranking, earning);
+  }
+
 
   private Float calculateEarning(int[] ranking, int lottoTicket){
     return  100*((Grade.FIRST.getReward()*ranking[1])+
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,7 +7,7 @@ public class Lotto {
     private static final String LENGTH_ERROR_MESSAGE = "[ERROR] 6개의 숫자를 입력해 주세요.";
     private static final String ONE_BETWEEN_NINE_ERROR_MESSAGE = "[ERROR] 1부터 45 사이의 숫자를 입력해 주세요.";
     private static final String OVERLAP_ERROR_MESSAGE = "[ERROR] 중복되지 않는 숫자를 입력해 주세요.";
-    private final List<Integer> numbers;
+    public final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -18,26 +18,21 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(LENGTH_ERROR_MESSAGE);
         }
-        if (range(numbers)){
-            throw new IllegalArgumentException(ONE_BETWEEN_NINE_ERROR_MESSAGE);
+        check1Between45(numbers);
+        checkOverlap(numbers);
+    }
+
+    private void check1Between45(List<Integer> numbers){
+        for (Integer num : numbers){
+            if (num < 1 || num > 45){
+                throw new IllegalArgumentException(ONE_BETWEEN_NINE_ERROR_MESSAGE);
+            }
         }
-        if (overlap(numbers)){
+    }
+
+    private void checkOverlap(List<Integer> numbers){
+        if (new HashSet<Integer>(numbers).size() != numbers.size()){
             throw new IllegalArgumentException(OVERLAP_ERROR_MESSAGE);
         }
     }
-
-    private Boolean range(List<Integer> numbers){
-        for (Integer num : numbers){
-            if (num < 1 || num > 45){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private Boolean overlap(List<Integer> numbers){
-        return new HashSet<Integer>(numbers).size() == numbers.size();
-    }
-
-
 }
