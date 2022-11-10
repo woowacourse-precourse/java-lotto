46864ae434f86bea89640fc02d5cf0eae5aff59a
anneu <b.imnuj@gmail.com>
2022-11-11 02:28:01 +0900

    refactor: 메소드 분리에 따른 테스트 변경

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -2,21 +2,29 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoMachineTest {
+    private LottoMachine lottoMachine;
+
+    @BeforeEach
+    void setUp() {
+        lottoMachine = new LottoMachine();
+    }
+
     @DisplayName("로또 구입금액이 1000원 미만일 경우 예외가 발생한다.")
     @Test
     void createLessThanMinMoney() {
-        assertThatThrownBy(() -> new LottoMachine(100))
+        assertThatThrownBy(() -> lottoMachine.inputMoney(100))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 구입금액이 1000원 단위가 아닐 경우 예외가 발생한다.")
     @Test
     void createWrongMoneyUnit() {
-        assertThatThrownBy(() -> new LottoMachine(1100))
+        assertThatThrownBy(() -> lottoMachine.inputMoney(1100))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
