ca68f2a6388050e4e34924679aa4fcda58688e56
Eundms <dms011221@gmail.com>
2022-11-10 17:31:09 +0900

    feat(Lotto): sort된 로또 번호

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -9,35 +10,35 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
-    public List<Integer> getNumbers(){
+    public List<Integer> getNumbers() {
         return numbers;
     }
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자여야 합니다.");
         }
-        if (!validateRange(numbers)) {
+        if(!validateRange(numbers)){
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
-        if (!validateDuplicates(numbers)) {
+        if(!validateDuplicates(numbers)){
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
         }
     }
-
-    private boolean validateRange(List<Integer> numbers) {
-        for (int number : numbers) {
-            if (number < 1 || number > 45) {
+    private boolean validateRange(List<Integer> numbers){
+        for(int number : numbers){
+            if(number<1 || number>45){
                 return false;
             }
         }
         return true;
     }
-
-    private boolean validateDuplicates(List<Integer> numbers) {
-        Set<Integer> notDuplicated = new HashSet<>(numbers);
-        return notDuplicated.size() == 6;
+    private boolean validateDuplicates(List<Integer> numbers){
+        Set<Integer> notDuplicated=new HashSet<>(numbers);
+        return notDuplicated.size()==6;
     }
+
 
 }
