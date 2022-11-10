11b8b9a041632a80c65e0b86b94b3f0a9c63dd6c
ysng_ysng <yeasung67@gmail.com>
2022-11-11 03:07:27 +0900

    feat(LotteryStoreUi): Implement the methods : takeWinningLotteryNumbers
    로또 1등 번호를 입력 받는 takeWinningLotteryNumbers 메서드를 구현했습니다.

--- src/main/java/lotto/domain/lotterystore/LotteryStoreUi.java
+++ src/main/java/lotto/domain/lotterystore/LotteryStoreUi.java
@@ -1,33 +1,53 @@
 package lotto.domain.lotterystore;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.dto.lotterystore.NumberForPurchase;
+import lotto.controller.LotteryRequestController;
+import lotto.dto.lotterystore.MoneyForPurchase;
+import lotto.dto.lotterystore.NumbersForWinningPrize;
 
 public class LotteryStoreUi {
 	private static final ErrorChecker errorChecker = new ErrorChecker();
+	private final LotteryRequestController requestController;
+
+	public LotteryStoreUi(LotteryRequestController requestController) {
+		this.requestController = requestController;
+	}
 
 	public void takeMoneyFromBuyer() {
-		int moneyForPurchase;
 		printPurchaseMessage();
 
 		String money = Console.readLine();
-		errorChecker.ValidateMoneyFromBuyer(money);
+		errorChecker.validateMoneyFromBuyer(money);
 
-		moneyForPurchase = Integer.parseInt(money);
-		transferNumbersForPurchase(moneyForPurchase);
+		requestForTransferingMoneyForPurchase(money);
+	}
+
+	public void takeWinningLotteryNumbers() {
+		printMessageForWinningPrizeNumbers();
+
+		String winningNumbers = Console.readLine();
+		errorChecker.validateWinningNumbers(winningNumbers);
+
+		requestFortransferingWinningPrizeNumbers(winningNumbers);
 	}
 
 	private void printPurchaseMessage() {
 		System.out.println("구매금액을 입력해 주세요.");
 	}
 
-	private int setNumbersForPurchase(int money) {
-		return money / 1000;
+	private void printMessageForWinningPrizeNumbers() {
+		System.out.println("당첨 번호를 입력해 주세요.");
 	}
 
-	private void transferNumbersForPurchase(int money) {
-		// API를 호출해서 데이터 전송하는 것과 비슷하게?? 구현
-		int numberOfPurchase = setNumbersForPurchase(money);
-		NumberForPurchase numberDto = new NumberForPurchase(numberOfPurchase);
+	private void requestForTransferingMoneyForPurchase(String money) {
+		// FrontEnd에서 API를 호출해서 데이터 전송하는 것과 비슷하게(??) 구현
+		MoneyForPurchase moneyDto = new MoneyForPurchase(money);
+		requestController.receiveMoneyForPurchase(moneyDto);
+	}
+
+	private void requestFortransferingWinningPrizeNumbers(String winningNumbers) {
+		// FrontEnd에서 API를 호출해서 데이터 전송하는 것과 비슷하게(??) 구현
+		NumbersForWinningPrize winningNumbersDto = new NumbersForWinningPrize(winningNumbers);
+		requestController.receiveNumbersForWinningPrize(winningNumbersDto);
 	}
 }
