1ede9ef9f1a5446aa7416e08e47cdb32672fcc84
HyebinShin <hyeviin.dev@gmail.com>
2022-11-10 09:06:39 +0900

    Feat: 로또 당첨: 로또 당첨 번호 입력 기능
    Console.readLine()
    유효성 검사 : 입력의 형태가 '숫자, 숫자, 숫자, 숫자, 숫자, 숫자'가 맞는지?
    이 때 숫자의 범위는 [1-45]이고 개수는 6개
    split()을 이용하여 숫자 분리하여 List<Integer>에 저장
    유효성 검사 : List<Integer>의 size()가 6이 맞는지? 각 숫자들이 서로 다른지?

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -21,6 +21,9 @@ public class Application {
         for (Lotto lotto : lottos) {
             System.out.println(lotto);
         }
+
+        System.out.println(Print.ASK_PRIZE);
+        List<Integer> prize = getPrize();
     }
 
     //로또 구입
@@ -75,5 +78,67 @@ public class Application {
 
     private static void sortNumbers(List<Integer> numbers) {
         numbers.sort(Comparator.naturalOrder());
+    }
+
+    //로또 당첨
+    private static List<Integer> getPrize() {
+        List<Integer> prize = setPrizeNum();
+
+        return prize;
+    }
+
+    private static String inputPrizeNum() {
+        String prizeNum = Console.readLine();
+
+        validateInputPrizeNum(prizeNum);
+
+        return prizeNum;
+    }
+
+    private static void validateInputPrizeNum(String prizeNum) {
+        String regexGroup = "([1-9]|[1-3][0-9]|4[0-5])";
+        String pattern = "^("+regexGroup+",){5}"+regexGroup;
+        //String pattern = "^(([1-9]|[1-3][0-9]|4[0-5]),){5}([1-9]|[1-3][0-9]|4[0-5])";
+        System.out.println("pattern: "+pattern);
+
+        if(!Pattern.matches(pattern, prizeNum)) {
+            throw new IllegalArgumentException("[ERROR] 1부터 45까지의 숫자를 콤마(,)를 이용해서 6개 적어주세요.");
+        }
+    }
+
+    private static String[] splitInputPrizeNum() {
+        return inputPrizeNum().split(",");
+    }
+
+    private static List<Integer> setPrizeNum() {
+        String[] splitInput = splitInputPrizeNum();
+        List<Integer> prizeNum = new ArrayList<>();
+
+        for(String input : splitInput) {
+            prizeNum.add(Integer.parseInt(input));
+        }
+
+        //System.out.println("prizeNum: "+Arrays.toString(prizeNum.toArray()));
+        validatePrizeNum(prizeNum);
+        //System.out.println("prizeNum: "+Arrays.toString(prizeNum.toArray()));
+
+        return prizeNum;
+    }
+
+    private static void validatePrizeNum(List<Integer> prizeNum) {
+        if(prizeNum.size()!=6) {
+            throw new IllegalArgumentException("[ERROR] 로또 당첨 번호는 6개여야 합니다.");
+        }
+        for(int i=0; i<prizeNum.size(); i++) {
+            int num = prizeNum.get(0);
+            prizeNum.remove(0);
+            //System.out.println("removed prizeNum: "+prizeNum);
+
+            if(prizeNum.contains(num)) {
+                throw new IllegalArgumentException("[ERROR] 로또 당첨 번호는 서로 달라야 합니다.");
+            }
+
+            prizeNum.add(num);
+        }
     }
 }
