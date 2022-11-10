25a49e9b79c5c0084c3928f109260a5555161dd5
김태용 <amico741@naver.com>
2022-11-11 01:48:03 +0900

    test: 구입 금액에 따른 예외 발생 유닛 테스트 추가
    0이 입력 되었을 때 에러를 던지지 않는 문제를 발견

--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -1,20 +1,27 @@
 package lotto;
 
 import lotto.domain.NumberGenerator;
+import lotto.service.LottoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoServiceTest {
     NumberGenerator generator;
+    LottoService lottoService;
+
     @BeforeEach
     void setUp() {
         generator = new NumberGenerator();
+        lottoService = new LottoService();
     }
 
     @Test
@@ -28,5 +35,14 @@ public class LottoServiceTest {
     void 랜덤으로_중복되지않는_숫자리스트_생성_테스트() {
         Set<Integer> set = new HashSet<>(generator.createDuplicateNumbers());
         assertThat(set.size()).isEqualTo(6);
+    }
+
+    @ParameterizedTest
+    @DisplayName("구입 금액이 1000원으로 나누어 떨어지지 않기 때문에 IllegalArgumentException테스트")
+    @ValueSource(ints = {1005, 20001, 500, 2, 200, 0})
+    void 로또구입금액으로인해_예외발생_테스트(int input) {
+        assertThatIllegalArgumentException()
+                .as("[ERROR] 실패하는 입력값이 존재합니다.")
+                .isThrownBy(() -> lottoService.validateMoney(input));
     }
 }
