7a3175291fabc2b88959ba6d5c701a635ce2e018
areyouhun <areyouhun@gmail.com>
2022-11-11 04:19:14 +0900

    feat(prize): 맞힌 볼의 개수로 당첨금 가져오기

--- src/test/java/lotto/Prize.java
+++ src/test/java/lotto/Prize.java
@@ -1,25 +1,57 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Prize {
-    public enum Rank {
+    private Ranks myRank;
+    private int reward;
+
+    Prize(int winningNumberCount, int bonusNumberCount) {
+        myRank = getRankByMatchingNumberCount(winningNumberCount, bonusNumberCount);
+        reward = myRank.getPrizeMoney();
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public Ranks getRankByMatchingNumberCount(int winningNumberCount, int bonusNumberCount) {
+        return Arrays.stream(myRank.values())
+                .filter(rank -> rank.matchingWinningNumberCount == winningNumberCount
+                        && rank.matchingBonusNumberCount == bonusNumberCount)
+                .findFirst()
+                .orElse(Ranks.NONE);
+    }
+
+    public enum Ranks {
         FIRST(6, 0, 2_000_000_000),
         SECOND(5, 1, 30_000_000),
         THIRD(5, 0, 1_500_000),
         FOURTH(4, 0, 50_000),
-        FIFTH(3, 0, 5_000);
+        FIFTH(3, 0, 5_000),
+        NONE(0, 0, 0);
 
         private final int matchingWinningNumberCount;
         private final int matchingBonusNumberCount;
-        private final int money;
+        private final int prizeMoney;
 
-        Rank(int matchingWinningNumberCount, int matchingBonusNumberCount, int money) {
+        Ranks(int matchingWinningNumberCount, int matchingBonusNumberCount, int prizeMoney) {
             this.matchingWinningNumberCount = matchingWinningNumberCount;
             this.matchingBonusNumberCount = matchingBonusNumberCount;
-            this.money = money;
+            this.prizeMoney = prizeMoney;
         }
 
-        public int getMoney() {
-            return this.money;
+        public int getPrizeMoney() {
+            return prizeMoney;
+        }
+
+        public Ranks getRankByMatchingNumberCount(int winningNumberCount, int bonusNumberCount) {
+            return Arrays.stream(values())
+                    .filter(rank -> rank.matchingWinningNumberCount == winningNumberCount
+                            && rank.matchingBonusNumberCount == bonusNumberCount)
+                    .findFirst()
+                    .orElse(Ranks.NONE);
         }
     }
 }
