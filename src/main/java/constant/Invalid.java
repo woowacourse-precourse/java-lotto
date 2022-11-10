ea4f67b0e999edd1ab2a22c80abab0d7bab6162c
eora21 <eora21@naver.com>
2022-11-10 11:03:01 +0900

    feat: 🎸 추가 유효값이 아닐 경우 예외처리를 맡을 Invalid enum 클래스
    값을 검사하였을 때 유효하지 않다면, 예외처리와 유효하지 않다는 텍스트를 출력할 Invalid enum 클래스 작성

---
+++ src/main/java/constant/Invalid.java
@@ -0,0 +1,22 @@
+package constant;
+
+public enum Invalid {
+    NULL("입력값은 null일 수 없습니다."),
+    NOT_INT("입력값이 정수가 아닙니다."),
+    CAN_NOT_BE_DIVIDED(Amount.UNIT + "로 나눌 수 없습니다."),
+    OVER_MATCH_ALL_AMOUNT("입력 금액은 1등 당첨금보다 높게 설정할 수 없습니다."),
+    NOT_LOTTO_FORM("입력값을 로또 형태로 전환할 수 없습니다."),
+    DUPLICATE_LOTTO("중복된 값이 있습니다."),
+    DUPLICATE_BONUS("보너스 번호가 당첨 번호와 중복되었습니다."),
+    ;
+
+    final String content;
+
+    Invalid(String content) {
+        this.content = content;
+    }
+
+    public void throwException() {
+        throw new IllegalArgumentException(this.content);
+    }
+}
