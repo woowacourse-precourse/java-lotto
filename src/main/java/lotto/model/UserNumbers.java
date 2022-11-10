a94d4464b5772674860c2b77de58d78059bcad96
sinsu <sinsu19012@gmail.com>
2022-11-10 22:35:20 +0900

    ✨feat :보너스 번호 입력 받기 기능 구현,Utiliy 클래스 분리

--- src/main/java/lotto/model/UserNumbers.java
+++ src/main/java/lotto/model/UserNumbers.java
@@ -9,7 +9,7 @@ public class UserNumbers {
 
     public UserNumbers(String userinput) {
         for (String s : stringSplit(userinput)) {
-            intValidate(s);
+            Utility.intValidate(s);
             NumbersAdd(stringConvertInteger(s));
         }
     }
@@ -22,11 +22,7 @@ public class UserNumbers {
         NUMBERS.add(userinput);
     }
 
-    private void intValidate(String s) {
-        if (!s.matches("-?\\d+")) {
-            throw new IllegalArgumentException("[ERROR] 잘못된 입력 값입니다..");
-        }
-    }
+
 
     private List<String> stringSplit(String userinput) {
         List<String> stringlist = new ArrayList<>();
