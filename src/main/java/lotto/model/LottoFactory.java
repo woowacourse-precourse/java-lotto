9358b7fd34963fe00d5bbb4ec76521ef81f2092d
김현재 <41482946+galug@users.noreply.github.com>
2022-11-10 20:53:08 +0900

    feat(lottofactory): 구입한 매수만큼 로또를 생성해주는 기능 추가

--- src/main/java/lotto/model/LottoFactory.java
+++ src/main/java/lotto/model/LottoFactory.java
@@ -1,13 +1,23 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoFactory {
     private static final LottoNumbersGenerator lottoNumbersGenerator = LottoNumbersGenerator.getInstance();
 
+    public List<Lotto> createLottos(int numberOfLotto) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < numberOfLotto; i++) {
+            Lotto lotto = createLotto();
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
+
     private Lotto createLotto() {
         List<Integer> lottoNumbers = lottoNumbersGenerator.create();
-        Lotto lotto = new Lotto(lottoNumbers);
-        return lotto;
+        return new Lotto(lottoNumbers);
     }
 }
