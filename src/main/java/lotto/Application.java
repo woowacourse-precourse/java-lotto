9d8818d947d7daf4122ef63126941e71f64fec41
saturnone1 <saturnone1@naver.com>
2022-11-10 10:28:11 +0900

    feat(Application): Consumer 클래스의 user 인스턴스 생성 - 해당 인스턴스는 로또 구매자의 입력을 받는 클래용스로 사

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,12 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Consumer user = new Consumer();
+
+        try {
+            Lotto lotto = new Lotto(user.winningNumber());
+        } catch (IllegalArgumentException e) {
+            System.out.printf("[ERROR] %s\n",e.getMessage());
+        }
     }
 }
