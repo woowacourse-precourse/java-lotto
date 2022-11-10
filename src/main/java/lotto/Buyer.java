d2ad55b9575a0bb806bb48129b0ce163de9ef2a6
dydckd <toofriend09@naver.com>
2022-11-10 21:39:10 +0900

    feat 기능추가 1.로또 구입금액 , 2. 로또 갯수 구하기 3.로또 구매메시지
    - 1. 로또 구입금액 입력
            * 1000원단위로 입력받고 1000원 단위가 아닌경우 [ERROR]발생
    - 2. 로또 갯수 구하기
            * 로또 구입금액으로 로또 구매 갯수 구하는 매서드 추가 테스트 케이스 추가
                들어온 금액이 1000원단위가 아닌경우 [ERROR]발생하는 테스트
    - 3. 로또 구매 메시지
            * "n개를 구매했습니다" 메시지 출력 메서드 추가

--- src/main/java/lotto/Buyer.java
+++ src/main/java/lotto/Buyer.java
@@ -1,6 +1,26 @@
 package lotto;
 
 public class Buyer {
+    final int purchaseAmount;
+    int lottoAmount;
+
+    public Buyer(int purchaseAmount) {
+        amountRange(purchaseAmount);
+        this.purchaseAmount = purchaseAmount;
+    }
+    private void amountRange(int purchaseAmount){
+        if(purchaseAmount%1000 !=0){
+            throw new IllegalArgumentException("[ERROR] 입력금액의 단위는 1000입니다.");
+        }
+    }
+    public int lottoAmount(int purchaseAmount){
+       return this.lottoAmount = purchaseAmount/1000;
+    }
+    public void lottoAmountMessage(){
+        System.out.println(lottoAmount + "개를 구매하셨습니다.");
+    }
+
+
     public void buyMessage(){
         System.out.println("구입금액을 입력해 주세요.");
     }
