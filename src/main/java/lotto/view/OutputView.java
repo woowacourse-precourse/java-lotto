269bfc8683bdfd7d02458f9a253cc1d3e168cf58
Yohan <sprae114@naver.com>
2022-11-10 20:18:32 +0900

    feat : 통계 출력 메시지 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,6 +6,17 @@ import java.util.List;
 
 public class OutputView {
     private static final String BUY_MESSAGE = "%d개를 구매했습니다.\n";
+    private static final String STATISTICS_MESSAGE = "당첨 통계\n---";
+    private static final String LOTTO_RESULT_MESSAGE= "%s - %d개\n";
+    private static final String EARNING_MESSAGE = "총 수익률은 %.1f%%입니다.\n";
+
+
+    public static void printStatisticsMessage(int[] ranking, Float earning){
+        System.out.println();
+        System.out.println(STATISTICS_MESSAGE);
+        printLottoResult(ranking);
+        System.out.printf(EARNING_MESSAGE, earning);
+    }
 
     public static void printGenerateLottoRepository(Integer ableCount, List<List<Integer>> lottoRepository){
         System.out.println();
@@ -16,4 +27,11 @@ public class OutputView {
         }
     }
 
+    public static void printLottoResult(int[] ranking){
+        System.out.printf(LOTTO_RESULT_MESSAGE, Grade.FIFTH.getOutput(), ranking[5]);
+        System.out.printf(LOTTO_RESULT_MESSAGE, Grade.FOURTH.getOutput(), ranking[4]);
+        System.out.printf(LOTTO_RESULT_MESSAGE, Grade.THIRD.getOutput(), ranking[3]);
+        System.out.printf(LOTTO_RESULT_MESSAGE, Grade.SECOND.getOutput(), ranking[2]);
+        System.out.printf(LOTTO_RESULT_MESSAGE, Grade.FIRST.getOutput(), ranking[1]);
+    }
 }
