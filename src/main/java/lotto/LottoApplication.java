357500c59c195a5d4dacad6992dc8c4293dab7c9
wlsh44 <k87913j@gmail.com>
2022-11-11 00:06:15 +0900

    feat: Money 도메인 및 구입 금액 검증 기능 추가

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,10 +1,11 @@
 package lotto;
 
+import lotto.domain.Money;
 import lotto.view.InputView;
 
 public class LottoApplication {
 
     public static void run() {
-        int money = InputView.inputPurchaseAmount();
+        Money money = new Money(InputView.inputPurchaseAmount());
     }
 }
---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+public class Money {
+
+    private static final int LOTTO_PRICE = 1000;
+    private static final String MONEY_ERROR_MESSAGE = "구입 금액은 1000원으로 나누어 떨어져야 합니다.";
+
+    private final int money;
+
+    public Money(int money) {
+        validateMoney(money);
+        this.money = money;
+    }
+
+    private void validateMoney(int money) {
+        if (money % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(MONEY_ERROR_MESSAGE);
+        }
+    }
+}
