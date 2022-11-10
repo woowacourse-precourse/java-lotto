40e1056d524994ca38ce7269aa5d7444106fae7e
sangwon <swchoi1997@naver.com>
2022-11-10 22:42:24 +0900

    feat: 당첨 결과를 담을 EnumMap 초기화 기능 추가

--- src/main/java/lotto/domain/model/LottoResult.java
+++ src/main/java/lotto/domain/model/LottoResult.java
@@ -1,11 +1,37 @@
 package lotto.domain.model;
 
+import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class LottoResult {
     private final Map<ResultCase, Integer> lottoResult;
 
     public LottoResult() {
-        this.lottoResult = null;
+        this.lottoResult = Arrays.stream(ResultCase.values())
+                .collect(Collectors.toMap(value -> value, count -> 0, (a, b) -> b,
+                        () -> new EnumMap<>(ResultCase.class)));
+    }
+    public LottoResult(final Map<ResultCase, Integer> lottoResult) {
+        this.lottoResult = lottoResult;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoResult that = (LottoResult) o;
+        return Objects.equals(lottoResult, that.lottoResult);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoResult);
     }
 }
