a05162fca5c5e5cb80ea3508a7b95b30b75173ee
jeongwle <dldl4568@naver.com>
2022-11-10 17:45:05 +0900

    test(enum): enum이 예상대로 잘 적용되는지 테스트

---
+++ src/test/java/lotto/enums/EnumTest.java
@@ -0,0 +1,57 @@
+package lotto.enums;
+
+import static lotto.enums.Messages.ERROR_MESSAGE_PREFIX;
+import static lotto.enums.Messages.PRICE_ERROR_MESSAGE;
+import static lotto.enums.Messages.RATE_OF_RETURN_MESSAGE;
+import static lotto.enums.Messages.RESULT_OF_PURCHASE_MESSAGE;
+import static lotto.enums.Messages.WINNING_STATISTICS_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class EnumTest {
+
+    @Test
+    void 구매_메세지_Enum이_잘_적용되는지_확인합니다() {
+        String format = MessageFormat.format(RESULT_OF_PURCHASE_MESSAGE.toString(), 5);
+
+        assertThat(format).isEqualTo("5개를 구매했습니다.");
+    }
+
+    @Test
+    void 통계_메세지_Enum이_잘_적용되는지_확인합니다() {
+        List<Integer> arguments = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            arguments.add(i);
+        }
+        String format = MessageFormat.format(WINNING_STATISTICS_MESSAGE.toString(), arguments.toArray());
+
+        assertThat(format).isEqualTo("당첨 통계\n"
+                + "---\n"
+                + "3개 일치 (5,000원) - 0개\n"
+                + "4개 일치 (50,000원) - 1개\n"
+                + "5개 일치 (1,500,000원) - 2개\n"
+                + "5개 일치, 보너스 볼 일치 (30,000,000원) - 3개\n"
+                + "6개 일치 (2,000,000,000원) - 4개");
+    }
+
+    @Test
+    void 수익률_메세지_Enum이_잘_적용되는지_확인합니다() {
+        double number = 0.6255;
+        String rate = String.format("%.1f", number * 100);
+        String result = MessageFormat.format(RATE_OF_RETURN_MESSAGE.toString(), rate);
+
+        assertThat(result).isEqualTo("총 수익률은 62.6%입니다.");
+    }
+
+    @Test
+    void 에러메세지_Enum이_잘_적용되는지_확인합니다() {
+        String errorMessage = ERROR_MESSAGE_PREFIX.toString() + PRICE_ERROR_MESSAGE;
+
+        assertThat(errorMessage).isEqualTo("[ERROR] 금액은 1000원 단위여야 합니다.");
+    }
+
+}
