bfb0cbebeef7e85315c56c350de8f1bcc474dcfc
imsongj <imsongj@gmail.com>
2022-11-10 13:55:40 +0900

    style(lotto) : 로또 생성 기능 메서드명 변경

--- src/main/java/lotto/domain/Machine.java
+++ src/main/java/lotto/domain/Machine.java
@@ -6,10 +6,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 import lotto.constant.LottoNumberStatistic;
 
 public class Machine {
-    public Machine(){
+    public Machine() {
     }
 
-    public Lotto generateLotto(){
+    public Lotto generateSingleLotto() {
         List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(LottoNumberStatistic.MIN_NUMBER.getValue(),
                 LottoNumberStatistic.MAX_NUMBER.getValue(), LottoNumberStatistic.NUMBER_OF_LOTTERY_NUMBERS.getValue());
         return new Lotto(randomNumbers);
--- src/test/java/lotto/domain/MachineTest.java
+++ src/test/java/lotto/domain/MachineTest.java
@@ -8,9 +8,9 @@ import static org.assertj.core.api.Assertions.*;
 public class MachineTest {
     @Test
     @DisplayName("랜덤 숫자 6개를 가진 로또를 생성한다.")
-    void generateLotto_generate_lotto(){
+    void generateSingleLotto_generate_lotto() {
         Machine machine = new Machine();
-        assertThatCode(() -> machine.generateLotto())
+        assertThatCode(() -> machine.generateSingleLotto())
                 .doesNotThrowAnyException();
     }
 }
