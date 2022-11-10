95714a9bee64ade0f4c8fc59fae9a2de2b252361
eastsage <dpwk8@naver.com>
2022-11-10 18:45:57 +0900

    test: 생성된 로또들이 오름차순으로 정렬되어 있는지 확인하는 테스트 추가

--- src/test/java/lotto/domain/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -1,9 +1,11 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -12,11 +14,29 @@ import org.junit.jupiter.api.Test;
  * 잘못된 입력 exception
  */
 class LottoGeneratorTest {
+    LottoGenerator lottoGenerator;
+    List<Lotto> lottos;
+    int size = 8;
+
+    @BeforeEach
+    void Init_Before_Test() {
+        lottoGenerator = new LottoGenerator(size);
+        lottos = lottoGenerator.generateLottos();
+    }
 
     @Test
-    void lottoGenerator_SizeOfLottosIsEqualsToQuantity() {
-        int size = 5;
-        assertThat(new LottoGenerator(size).generateLottos().size()).isEqualTo(size);
+    void lottoGenerator_SizeOfLottosIsEqualsToQuantity_True() {
+        Assertions.assertThat(lottos.size()).isEqualTo(size);
+    }
+
+    @Test
+    void lottoGenerator_LottosIsAscendingOrder_True() {
+        for (Lotto lotto : lottos) {
+            List<Integer> list = new ArrayList<>(lotto.get());
+            for (int i = 0; i < 5; i++) {
+                assertThat(list.get(i)).isLessThan(list.get(i + 1));
+            }
+        }
     }
 
 }
