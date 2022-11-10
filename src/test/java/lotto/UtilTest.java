31be23f108af8d59cb9afbd61a5b2bb92ca35f15
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-11 00:55:51 +0900

    Test(UtilTest) : isNumberic() 테스트 코드 리펙토링 #11
    숫자열_판별_테스트()의 DisplayName을 추가했습니다.
    
    Ref : #10

--- src/test/java/lotto/UtilTest.java
+++ src/test/java/lotto/UtilTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import java.util.List;
 
@@ -12,11 +13,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 class UtilTest {
 
     @Test
+    @DisplayName("숫자열인지 아닌지 판별한다.")
     void 숫자열_판별_테스트(){
-        String numberic = "123";
+        final String numberic = "123";
         assertThat(Util.isNumberic(numberic)).isTrue();
 
-        String string = "123a";
+        final String minusNumberic = "-123";
+        assertThat(Util.isNumberic(numberic)).isTrue();
+
+        final String string = "123a";
         assertThat(Util.isNumberic(string)).isFalse();
     }
 
