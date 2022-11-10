17d47367fd0e6005b6e85ff8cec1f5ceb0fb4e0a
java-saeng <gusdn85554@naver.com>
2022-11-10 15:47:08 +0900

    feat : 로또 구입 금액 입력 시 0원 예외 처리

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -5,4 +5,8 @@ public class InputValidator {
     public boolean restEqualZero(long dividend, long divisor) {
         return dividend % divisor == 0;
     }
+
+    public static boolean isZero(long number) {
+        return number == 0;
+    }
 }
