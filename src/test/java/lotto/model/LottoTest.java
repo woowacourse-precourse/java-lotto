0187f96602d5547fc748fd60e0f22197913fcdfd
ksh-beginner <ohk9134@naver.com>
2022-11-10 10:30:13 +0900

    test(LottoTest) : 발행한 로또 번호 오름차순인지 테스트

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import lotto.model.Lotto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,6 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -32,5 +34,17 @@ class LottoTest {
 
         assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 발행한_로또_번호_오름차순() {
+        List<Integer> notSortedNumbers = List.of(4, 6, 3, 2, 1, 5);
+        Lotto lotto = new Lotto(notSortedNumbers);
+        List<Integer> sortedNumbers = lotto.getNumbers();
+
+        assertThat(sortedNumbers.get(0)).isNotEqualTo(4);
+        assertThat(sortedNumbers.get(0)).isEqualTo(1);
+        assertThat(sortedNumbers.get(notSortedNumbers.size() - 1)).isNotEqualTo(5);
+        assertThat(sortedNumbers.get(notSortedNumbers.size() - 1)).isEqualTo(6);
     }
 }
