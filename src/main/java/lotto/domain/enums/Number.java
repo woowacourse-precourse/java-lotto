c382a5b7b8dedb36b5addc71a28db63260b36fc0
KimDongho <hkim4410@naver.com>
2022-11-10 21:22:53 +0900

    refactor(Number): Number enum 클래스, 인스턴스 변수와 생성자 및 getter 추가

--- src/main/java/lotto/domain/enums/Number.java
+++ src/main/java/lotto/domain/enums/Number.java
@@ -1,5 +1,15 @@
 package lotto.domain.enums;
 
 public enum Number {
-    ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX;
+    ZERO(0), ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6);
+
+    private final int value;
+
+    Number(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
