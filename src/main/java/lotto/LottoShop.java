16fe047a124b4466bc080ebabf87eccd039499b2
이경범 <tkdwk567@naver.com>
2022-11-10 22:14:13 +0900

    feat : 금액에 대한 수량 계산 and 당첨 로또 번호 생성 and 보너스 번호 생성 기능 LottoShop class 에 역할 부여

---
+++ src/main/java/lotto/LottoShop.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import lotto.exception.PayMoneyException;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LottoShop {
+
+    private final MemberWriteService memberWriteService = new MemberWriteService();
+
+    public List<Lotto> createLottoForPayment(String pay) {
+        int payMoney = memberWriteService.writePay(pay);
+        try {
+            int quantity = Lotto.moneyOfQuantity(payMoney);
+
+            return Lotto.createLottoNumbers(quantity);
+        } catch (PayMoneyException e) {
+
+            return Collections.emptyList();
+        }
+    }
+
+    public WinningLottoNumber createWinningLottoNumber(String writeLottoNumber, String writeBonusNumber) {
+        return memberWriteService.writeWinningLottoNumbers(writeLottoNumber, writeBonusNumber);
+    }
+}
