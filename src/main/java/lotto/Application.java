2608b088d78d889e8042dc1eff7673d4991c827a
Sunghyun-0 <zkdldk95@naver.com>
2022-11-10 23:41:12 +0900

    기능 목록 수정 / 금액 유효성 검사 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,10 @@
 package lotto;
 
 public class Application {
+    public static boolean validationCheck(int money){
+        return money % 1000 == 0;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
