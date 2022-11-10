456fab0b162771540fd1e41e56ca44e404d77e36
java-saeng <gusdn85554@naver.com>
2022-11-10 16:32:00 +0900

    feat : 당첨 번호 입력하는 기능 추가

--- src/main/java/lotto/utils/InputUtil.java
+++ src/main/java/lotto/utils/InputUtil.java
@@ -4,22 +4,51 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.exception.ExceptionConstants;
 import lotto.validator.InputValidator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static lotto.exception.ExceptionConstants.*;
+
 public class InputUtil {
 
     public static final int LOTTO_PRICE = 1000;
+    public static final String WINNING_NUMBER_DELIM = ",";
 
     public static long inputLottoPurchaseAmount() {
 
         long purchasePrice = Long.parseLong(Console.readLine());
 
         if (InputValidator.isZero(purchasePrice)) {
-            throw new IllegalArgumentException(ExceptionConstants.CANNOT_INPUT_ZERO);
+            throw new IllegalArgumentException(CANNOT_INPUT_ZERO);
         }
 
         if (InputValidator.restEqualZero(purchasePrice, LOTTO_PRICE)) {
-            throw new IllegalArgumentException(ExceptionConstants.CAN_ONLY_MULTIPLY_1000);
+            throw new IllegalArgumentException(CAN_ONLY_MULTIPLY_1000);
         }
 
         return purchasePrice;
+    }
+
+    public static List<Integer> inputWinningLottoNumber() {
+
+        List<Integer> winningNumbers = Stream.of(Console.readLine().split(WINNING_NUMBER_DELIM))
+                                             .map(Integer::parseInt)
+                                             .collect(Collectors.toList());
+
+        if (!InputValidator.hasSixNumbers(winningNumbers)) {
+            throw new IllegalArgumentException(CAN_ONLY_SIX_COUNT);
+        }
+
+        if (!InputValidator.hasDuplicatedNumber(winningNumbers)) {
+            throw new IllegalArgumentException(CANNOT_DUPLICATED_NUMBER);
+        }
+
+        if (!InputValidator.satisfyLottoNumberRange(winningNumbers)) {
+            throw new IllegalArgumentException(CANNOT_SATISFY_NUMBER_RANGE);
+        }
+
+        return winningNumbers;
     }
 }
