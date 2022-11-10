cc789e1b684cfd6d5ede92004b66eb8bed81ee31
myungsikyoon <yms8795@gmail.com>
2022-11-10 16:55:51 +0900

    feat : 통계에 사용될 상수값을 위한 enum 클래스 구현.

---
+++ src/main/java/constants/PrizeDivision.java
@@ -0,0 +1,27 @@
+package constants;
+
+public enum PrizeDivision {
+
+    FIRST("6개 일치","2,000,000,000"),
+    SECOND("5개 일치, 보너스 볼 일치","30,000,000"),
+    THIRD("5개 일치","1,500,000"),
+    FOURTH("4개 일치","50,000"),
+    FIFTH("3개 일치","5,000")
+    ;
+
+    private final String sameCount;
+    private final String money;
+
+    PrizeDivision(String sameCount, String money) {
+        this.sameCount = sameCount;
+        this.money = money;
+    }
+
+    public String getSameCount() {
+        return sameCount;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+}
