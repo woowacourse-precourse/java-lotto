18a0f01ffd0d9c76568f83de2c84a8f5eb6fa9d4
ParkRootSeok <g_e_un@naver.com>
2022-11-10 22:51:14 +0900

    feat : Customer 클래스에 로또 가격에 대한 상수 변수 추가

--- src/main/java/lotto/domain/Store.java
+++ src/main/java/lotto/domain/Store.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Store {
 
     private static final String BUY_LOTTO_NUMBER = "개를 구매했습니다.";
+    private static final int LOTTO_PRICE = 1000;
 
     private List<List<Integer>> lottos = new ArrayList<>();
     private int lottoNumber;
