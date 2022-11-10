bb39fcc3cc29dda6b1d87d669363f9cd1594b381
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-11-10 10:40:05 +0900

    feat : 로또 번호를 출력하는 함수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
+
+        List<Integer> numbers = new ArrayList<>();
+        getLottoNumber(numbers);
+
+        Lotto lotto = new Lotto(numbers);
+
+        System.out.println(numbers);
+        System.out.println(lotto);
+
         // TODO: 프로그램 구현
     }
+
+    public static void getLottoNumber(List<Integer> numbers){
+        while(numbers.size() < 6){
+            int b = Randoms.pickNumberInRange(1,45);
+            if(!numbers.contains(b))
+                numbers.add(b);
+        }
+        Collections.sort(numbers);
+    }
+
+
 }
