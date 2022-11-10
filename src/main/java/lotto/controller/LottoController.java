91ceec04f94f2b5ab86ca26959c917ff83a911ef
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-11 01:34:16 +0900

    feat: 구매 금액에 맞춰 로또 생성
    - LottoController - 구매 금액에 맞춰 로또 생성 method추가: generateLottoAuto
    - Lotto - 필요 상수들 생성
     - 가장 작은 수 : 1
     - 가장 큰 수 : 45
     - 로또 숫자 수 : 6

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,25 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoController {
+
+    public List<Lotto> generateLottoAuto(int purchaseAmount) {
+
+        int generatedLottoNum = purchaseAmount / Lotto.PRICE;
+        List<Lotto> generatedLottos = new ArrayList<>();
+
+        while (generatedLottos.size() < generatedLottoNum) {
+            List<Integer> numbers =
+                    Randoms.pickUniqueNumbersInRange(Lotto.MIN_NUMBER, Lotto.MAX_NUMBER, Lotto.NUMBER_COUNT);
+            generatedLottos.add(new Lotto(numbers));
+        }
+
+        return generatedLottos;
+    }
+
 }
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class Lotto {
 
     public static final int PRICE = 1000;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int NUMBER_COUNT = 6;
 
     private final List<Integer> numbers;
 
