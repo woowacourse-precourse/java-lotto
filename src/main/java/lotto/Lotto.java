c29603e2e11880dc6909cc5073ae9c4e162f1db4
JunDonghyuk <84345881+jundonghyuk@users.noreply.github.com>
2022-11-10 22:16:24 +0900

    feat(Lotto): 로또 번호 맞힌 개수 구하는 기능 완성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -20,4 +20,26 @@ public class Lotto {
         return numbers;
     }
 
+    public Integer countMatchNumbers(WinningNumber winningNumber) {
+        int matchNumber = 0;
+        for (int number = 0; number < numbers.size(); number++) {
+            matchNumber += isMatched(numbers.get(number), winningNumber.getWinningNumbers());
+        }
+        return matchNumber;
+    }
+
+    public Integer isMatched(int myNumber, List<Integer> winningNumbers) {
+        if (winningNumbers.contains(myNumber)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public boolean haveBonusNumber(WinningNumber winningNumber) {
+        if (numbers.contains(winningNumber.getBonusNumber())) {
+            return true;
+        }
+        return false;
+    }
+
 }
---
+++ src/main/java/lotto/LottoPlace.java
@@ -0,0 +1,17 @@
+package lotto;
+
+public enum LottoPlace {
+    FIFTH(3, 5_000),
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    FIRST(6, 2_000_000_000);
+
+    private final int matchNumbers;
+    private final int winMoney;
+
+    LottoPlace(int matchNumbers, int winMoney) {
+        this.matchNumbers = matchNumbers;
+        this.winMoney = winMoney;
+    }
+}
