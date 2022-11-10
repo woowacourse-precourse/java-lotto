659aab26514761aecb2c51cbd0a571e8d9bb5e12
swandevson <shai4956@gmail.com>
2022-11-10 15:11:47 +0900

    Feat(LottoNumberValidator): 중복된 로또 번호 존재 여부 판별
    - 중복된 로또 번호가 있는지 판별하는 메소드 작성

---
+++ src/main/java/lotto/LottoNumberValidator.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoNumberValidator {
+    private static final int NUMBER_EA = 6;
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
+
+
+    public static boolean existDuplicate(List<Integer> numbers) {
+        long numbersTypeCount = numbers
+                .stream()
+                .distinct()
+                .count();
+
+        if (numbersTypeCount != numbers.size()) {
+            return true;
+        }
+
+        return false;
+    }
+}
