d946ab3357830f9e159ead4c083f6fe89f5fe3c8
imsongj <imsongj@gmail.com>
2022-11-10 17:57:28 +0900

    refactor(lotto) : 오름차순으로 정렬된 랜덤 숫자 생성 기능 리팩토링

--- src/main/java/lotto/domain/Machine.java
+++ src/main/java/lotto/domain/Machine.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
@@ -11,9 +12,15 @@ public class Machine {
     //public List<Lotto> buyLotto(long money){
 
     //}
-    public Lotto generateSingleLotto() {
-        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(LottoStatistic.MIN_NUMBER.getValue(),
+    public Lotto createSingleLottoTicket() {
+        List<Integer> sortedRandomNumbers = createSortedRandomNumbers(LottoStatistic.MIN_NUMBER.getValue(),
                 LottoStatistic.MAX_NUMBER.getValue(), LottoStatistic.NUMBER_OF_LOTTERY_NUMBERS.getValue());
-        return new Lotto(randomNumbers);
+        return new Lotto(sortedRandomNumbers);
+    }
+
+    public List<Integer> createSortedRandomNumbers(int start, int end, int count) {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(start, end, count);
+        Collections.sort(randomNumbers);
+        return randomNumbers;
     }
 }
--- src/test/java/lotto/domain/MachineTest.java
+++ src/test/java/lotto/domain/MachineTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,9 +9,17 @@ import static org.assertj.core.api.Assertions.*;
 public class MachineTest {
     @Test
     @DisplayName("랜덤 숫자 6개를 가진 로또를 생성한다.")
-    void generateOneLottoWithoutException() {
+    void createOneLottoWithoutException() {
         Machine machine = new Machine();
-        assertThatCode(() -> machine.generateSingleLotto())
+        assertThatCode(() -> machine.createSingleLottoTicket())
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("오름차순으로 정렬된 랜덤 숫자를 생성한다")
+    void createSortedRandomNumbers() {
+        Machine machine = new Machine();
+        List<Integer> randomNumbers = machine.createSortedRandomNumbers(1, 6, 6);
+        assertThat(randomNumbers).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
     }
 }
