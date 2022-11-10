4c638539ccf62691e555b51bad1cddf41bd0ab24
swandevson <shai4956@gmail.com>
2022-11-10 14:15:00 +0900

    Feat(InputValidator): money 입력값 예외 판별
    - 숫자가 아닌 문자가 있는지 판단

--- src/main/java/lotto/InputValidator.java
+++ src/main/java/lotto/InputValidator.java
@@ -8,6 +8,14 @@ public class InputValidator {
     private static final String NUMBER_REGEX = "^[0-9]";
 
 
+    public static boolean isInvalidMoneyForm(String input) {
+        if (!input.matches(NUMBER_REGEX)) {
+            return true;
+        }
+
+        return false;
+    }
+
     public static boolean isInvalidSeparator(List<String> splitedInput) {
         for (String eachInput : splitedInput) {
             if (!eachInput.matches(NUMBER_REGEX)) {
