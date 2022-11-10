15a57787e962213543d4e8abd9ca4304aba62a03
eastsage <dpwk8@naver.com>
2022-11-10 20:15:58 +0900

    refactor: Lotto 클래스 오류 출력문 수정
    요구사항에 맞춰 예외와 함께 [Error] 로 시작하는 메세지 출력하도록 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -13,13 +13,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자만 허용됩니다.");
         }
     }
 
     private List<Integer> deDuplicationAndSort(List<Integer> numbers) {
         if (deDuplicationAndSort(numbers).size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[Error] 로또 번호는 중복이 허용되지 않습니다.");
         }
         return List.copyOf(new TreeSet<Integer>(numbers));
     }
