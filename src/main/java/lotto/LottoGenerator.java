cbf98315ad98147e6388f60966c3ea6c0ceb8cc1
swonny <wonny921@gmail.com>
2022-11-10 23:33:43 +0900

    feat(LottoGenerator): 구매자가 지불한 금액만큼의 로또를 발행한다.

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,12 +1,20 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoGenerator {
     static final int AVAILABLE_MONEY_UNIT = 1000;
 
     public static void generate(String payment) {
         validatePayment(Integer.valueOf(payment));
+        List<Lotto> lottos = new ArrayList<>();
+        for (int lottoNumber = 0; lottoNumber < countNumberOfLotto(Integer.valueOf(payment)); lottoNumber++) {
+            lottos.add(new Lotto(pickLottoNumbers()));
+        }
     }
 
     public static void validatePayment(int amount) {
@@ -15,8 +23,12 @@ public class LottoGenerator {
         }
     }
 
-    public static int countNumberOfLotto(String amount) {
-        return 0;
+    public static int countNumberOfLotto(int amount) {
+        return amount / 1000;
+    }
+
+    public static List<Integer> pickLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 
     public static void orderNumbers(Lotto lotto) {
