a74ab6ea63c053239a1e7bdc8079ed86a0d001d2
MuSeung <peter0757@nvaer.com>
2022-11-10 11:07:21 +0900

    test: QuickPick.create()의 번호 등장 비율이 일정하게 나오는지 확인하는 테스트 구현
    numberRatio()
        - 50만회 시행 후 예상치보다 10%의 오차를 허용하는 선에서 각 숫자가 해당 비율로 등장헀는지 확인
    QuickPickTest
        - 일부 상수를 LottoState에 담아 매직넘버 제거

---
+++ src/main/java/lotto/LottoState.java
@@ -0,0 +1,15 @@
+package lotto;
+
+public enum LottoState {
+    START(1),
+    END(45),
+    RANGE(END.value - START.value + 1),
+    LENGTH(6);
+    private final int value;
+    LottoState(int value) {
+        this.value = value;
+    }
+    public int number() {
+        return this.value;
+    }
+}
--- src/test/java/lotto/QuickPickTest.java
+++ src/test/java/lotto/QuickPickTest.java
@@ -9,23 +9,42 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class QuickPickTest {
+
     private List<Integer> quickPickNumbers = QuickPick.create();
     @DisplayName("로또 번호의 개수는 6개이다.")
     @Test
-    void isNumberLengthSix(){
-        assertThat(quickPickNumbers.size()).isEqualTo(6);
+    void isNumberLengthSix() {
+        assertThat(quickPickNumbers.size()).isEqualTo(LottoState.LENGTH.number());
     }
     @DisplayName("번호가 1~45 범위 안이다.")
     @Test
-    void inBoundary(){
-        for(int number : quickPickNumbers){
-            assertThat(number).isBetween(1,45);
+    void inBoundary() {
+        for (int number : quickPickNumbers) {
+            assertThat(number).isBetween(LottoState.START.number(), LottoState.END.number());
         }
     }
     @DisplayName("번호에 중복이 없다.")
     @Test
-    void nonDuplicated(){
+    void nonDuplicated() {
         Set<Integer> quickPickNumberSet = new HashSet<>(quickPickNumbers);
         assertThat(quickPickNumbers.size()).isEqualTo(quickPickNumberSet.size());
+    }
+    @DisplayName("반복 시행시 번호들이 등장하는 비율이 1/45이다.")
+    @Test
+    void numberRatio() {
+        final double ERROR_RANGE = 0.01;
+        int numberTrials = 5000000;
+        double[] numberCounts = new double[LottoState.RANGE.number()];
+        for (int trial = 0; trial < numberTrials; trial++) {
+            List<Integer> quickPickNumbers = QuickPick.create();
+            for (int number : quickPickNumbers) {
+                numberCounts[number - LottoState.START.number()]++;
+            }
+        }
+        for (double numberCount : numberCounts) {
+            assertThat(numberCount / numberTrials / LottoState.LENGTH.number())
+                    .isBetween((1 - ERROR_RANGE) / LottoState.RANGE.number(),
+                            (1 + ERROR_RANGE) / LottoState.RANGE.number());
+        }
     }
 }
