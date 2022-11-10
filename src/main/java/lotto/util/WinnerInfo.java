5381f3660ff0944c63a2bd1c208aa56686b3d9b3
jinny-l <jinny.l.dev@gmail.com>
2022-11-10 20:49:42 +0900

    feat(WinnerInfo): 당첨 정보에 랭크 순위를 확인할 수 있는 필드 추가
    - 기타 수정 항목:
    상금 정보를 String 타입에서 int 타입으로 수정

--- src/main/java/lotto/util/WinnerInfo.java
+++ src/main/java/lotto/util/WinnerInfo.java
@@ -2,25 +2,31 @@ package lotto.util;
 
 public enum WinnerInfo {
 
-    RANK1("6개 일치", "2,000,000,000원"),
-    RANK2("5개 일치, 보너스 볼 일치", "30,000,000원"),
-    RANK3("5개 일치", "1,500,000원"),
-    RANK4("4개 일치", "50,000원"),
-    RANK5("3개 일치", "5, 000원");
+    RANK1(1, "6개 일치", 2_000_000_000),
+    RANK2(2, "5개 일치, 보너스 볼 일치", 30_000_000),
+    RANK3(3, "5개 일치", 1_500_000),
+    RANK4(4, "4개 일치", 50_000),
+    RANK5(5, "3개 일치", 5_000);
 
+    private final int rank;
     private final String winningCondition;
-    private final String prize;
+    private final int prize;
 
-    WinnerInfo(String winningCondition, String prize) {
+    WinnerInfo(int rank, String winningCondition, int prize) {
+        this.rank = rank;
         this.winningCondition = winningCondition;
         this.prize = prize;
+    }
+
+    public int getRank() {
+        return this.rank;
     }
 
     public String getWinningCondition() {
         return this.winningCondition;
     }
 
-    public String getPrizeInformation() {
+    public int getPrizeInformation() {
         return this.prize;
     }
 
