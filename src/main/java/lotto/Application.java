662ec3b4d0b0c23b62787d1a69e8ffa10aa22b42
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-10 18:37:48 +0900

    feat: 입력 금액 객체 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,6 +6,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         startMessage();
+        Money money = new Money(Console.readLine());
     }
 
     public static void startMessage() {
