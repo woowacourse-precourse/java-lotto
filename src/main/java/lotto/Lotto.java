a271b61a1ecccfd4511108984fbee45524d526c6
seokjin8678 <rnrn8678@naver.com>
2022-11-10 23:12:00 +0900

    feat: Lotto 클래스 validate 메서드 에외 메시지 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -13,7 +13,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.");
         }
     }
 
