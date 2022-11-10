62db286e971f3a247b83da76540feb13987cf987
Eungyu-code <Agyu625@gmail.com>
2022-11-10 21:41:04 +0900

    feat(Application class): inputWinningNumbers 함수 추가
    당첨 번호를 입력받는 함수

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -125,4 +125,9 @@ public class Application {
 
         return changeCashStringToInteger(input);
     }
+
+    public static String inputWinningNumbers() {
+
+        return Console.readLine();
+    }
 }
