ef9126973b51158bf942dbc01e7acce21575b65c
yumyeonghan <mgtmh991013@gmail.com>
2022-11-10 18:33:01 +0900

    feat: Implementation of purchaseAmount()
    Enter purchase amount

---
+++ src/main/java/lotto/User.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+
+public class User {
+
+    public int purchaseAmount() {
+
+        return Integer.parseInt(readLine());
+
+    }
+}
