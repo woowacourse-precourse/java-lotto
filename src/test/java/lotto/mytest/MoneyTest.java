bda2aa3b73f3ad9dd1e821b7e43766a61d8f973c
KyungKyu Park <rudrb0505@gmail.com>
2022-11-10 17:44:48 +0900

    refactor(MoneyTest): 중복되는 성격의 테스트를 묶어서 진행

--- src/test/java/lotto/mytest/MoneyTest.java
+++ src/test/java/lotto/mytest/MoneyTest.java
@@ -1,43 +1,31 @@
 package lotto.mytest;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import lotto.Money;
 
 public class MoneyTest {
-	Money money = new Money();
 
-	@DisplayName("금액 입력 시 숫자가 아니라면 예외 발생")
-	@Test
-	void notNumber() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			money.checkMoneyInput("abc");
-		});
-	}
-
-	@DisplayName("금액 입력 시 정수가 아니라면 예외 발생")
-	@Test
-	void notInteger() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			money.checkMoneyInput("1.5");
-		});
-	}
-
-	@DisplayName("금액 입력 시 음수라면 예외 발생")
-	@Test
-	void notPositive() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			money.checkMoneyInput("-1");
-		});
+	@DisplayName("금액 입력 예외처리 모음")
+	@ParameterizedTest
+	@ValueSource(strings = {"abc", "1.5", "-1", "1q2w3e4r", " ", "-", "!@#"})
+	void exceptionTest(String money) {
+		assertThatThrownBy(() -> {
+			Money.checkMoneyInput(money);
+		}).isInstanceOf(IllegalArgumentException.class);
 	}
 
 	@DisplayName("금액 입력 시 1000원 단위가 아니라면 예외 발생")
 	@Test
 	void notUnitOfThousand() {
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			money.checkMoneyInput("1500");
+			Money.checkMoneyInput("1500");
 		});
 	}
 }
