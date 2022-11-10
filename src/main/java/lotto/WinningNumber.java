16905d7070109944d39a55fda60ff3148307d27e
dohyun <ldh19738@gmail.com>
2022-11-10 15:14:38 +0900

    feat(WinningNumber): 당첨 번호 입력받는 기능 구현

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -1,5 +1,10 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class WinningNumber {
     public void printInputWinningNumberMessage() {
         System.out.println("당첨 번호를 입력해 주세요");
@@ -7,5 +12,17 @@ public class WinningNumber {
 
     public void printInputBonusNumberMessage() {
         System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public List<Integer> input() {
+        String inputNumber = Console.readLine();
+        List<Integer> inputNumbers = new ArrayList<>();
+        String[] inputNumberArr = inputNumber.split(",");
+
+        for(int i = 0; i < inputNumberArr.length; i++) {
+            inputNumbers.add(Integer.valueOf(inputNumberArr[i]));
+        }
+
+        return inputNumbers;
     }
 }
