26154a7f8c8dd3b90ea3c81d9c520c515f098db0
김현재 <41482946+galug@users.noreply.github.com>
2022-11-10 11:55:35 +0900

    feat(purchase): 구매 가격에 대한 검증 기능 추가

--- src/main/java/lotto/model/Purchase.java
+++ src/main/java/lotto/model/Purchase.java
@@ -2,14 +2,19 @@ package lotto.model;
 
 public class Purchase {
     private static final int LOTTO_PRICE = 1000;
+    private static final String INPUT_PRICE_ERROR = "[ERROR] 잘못된 가격을 입력하셨습니다.";
 
     public int changeToLotto(String moneyInput) {
         validate(moneyInput);
+
         int money = Integer.parseInt(moneyInput);
         return money / LOTTO_PRICE;
     }
 
     private void validate(String input) {
-        // TODO: 1000원 단위로 나누어 떨어지는지는 가격인지에 대한 검증이 필요
+        String regex = "^[0-9]{1,}000$";
+        if (!input.matches(regex)) {
+            throw new IllegalArgumentException(INPUT_PRICE_ERROR);
+        }
     }
 }
