2927e605450f9ac44f9c154bdded88a787816f84
HubCreator <chocomilk8604@gmail.com>
2022-11-10 15:14:18 +0900

    refactor(LottoTest) : 관련된 테스트끼리 @Nested로 묶음

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -12,58 +12,72 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
 
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
-    @Test
-    void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_COUNT.getValue());
-    }
+    @DisplayName("예외 테스트")
+    @Nested
+    class ExceptionTest {
+        @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+        @Test
+        void createLottoByOverSize() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_COUNT.getValue());
+        }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void createLottoByDuplicatedNumber() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.HAVE_DUPLICATED_NUMBER.getValue());
-    }
+        @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+        @Test
+        void createLottoByDuplicatedNumber() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.HAVE_DUPLICATED_NUMBER.getValue());
+        }
 
-    @DisplayName("로또 번호가 1~45 이내의 숫자가 아니면 예외가 발생한다.")
-    @Test
-    void createLottoByOverNumber() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 30, 40, 42, 46)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_NUMBER.getValue());
-    }
+        @DisplayName("로또 번호가 1~45 이내의 숫자가 아니면 예외가 발생한다.")
+        @Test
+        void createLottoByOverNumber() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 30, 40, 42, 46)))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_NUMBER.getValue());
+        }
 
-    @DisplayName("보너스 번호가 로또 번호와 중복되면 예외가 발생한다.")
-    @Test
-    void createBonusNumber() {
-        assertThatThrownBy(() -> new WinLotto(List.of(1, 2, 3, 4, 5, 6), 3))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_BONUS_NUMBER.getValue());
-    }
-
-    @DisplayName("자동으로 로또 번호를 생성한다.")
-    @RepeatedTest(10)
-    void createLotto() {
-        Lotto generatedLotto = Lotto.create();
-        List<Integer> numbers = generatedLotto.getNumbers();
-        assertThat(numbers.size()).isEqualTo(6);
-        for (Integer number : numbers) {
-            assertThat(number).isGreaterThan(0).isLessThan(46);
+        @DisplayName("보너스 번호가 로또 번호와 중복되면 예외가 발생한다.")
+        @Test
+        void createBonusNumber() {
+            assertThatThrownBy(() -> new WinLotto(List.of(1, 2, 3, 4, 5, 6), 3))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_BONUS_NUMBER.getValue());
         }
     }
 
-    @DisplayName("자동으로 생성된 로또 번호를 형식에 맞게 출력한다.")
-    @RepeatedTest(10)
-    void createLottoAndPrintNumbers() {
-        Lotto generatedLotto = Lotto.create();
-        System.out.println(generatedLotto.printNumbers());
+
+    @DisplayName("로또 생성 테스트")
+    @Nested
+    class CreateLottoTest {
+        @DisplayName("자동으로 로또 번호를 생성한다.")
+        @RepeatedTest(10)
+        void createLotto() {
+            Lotto generatedLotto = Lotto.create();
+            List<Integer> numbers = generatedLotto.getNumbers();
+            assertThat(numbers.size()).isEqualTo(6);
+            for (Integer number : numbers) {
+                assertThat(number).isGreaterThan(0).isLessThan(46);
+            }
+        }
+
+        @DisplayName("자동으로 생성된 로또 번호를 형식에 맞게 출력한다.")
+        @RepeatedTest(10)
+        void createLottoAndPrintNumbers() {
+            Lotto generatedLotto = Lotto.create();
+            String printResult = generatedLotto.printNumbers();
+            assertThat(printResult.startsWith("[")).isTrue();
+            assertThat(printResult.endsWith("]")).isTrue();
+
+            String[] split = printResult.split(", ");
+            assertThat(split.length).isEqualTo(6);
+        }
     }
 
 
-    @DisplayName("하나의 로또 번호에 대한 당첨 내역을 출력한다.")
+    @DisplayName("하나의 로또 번호에 대한 당첨 내역 테스트")
     @Nested
     class getSingleResult {
 
