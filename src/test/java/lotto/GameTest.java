6348d44f0b51582e088d75bd2d2b1624d5eecf8f
sunwng <sunwgna@gmail.com>
2022-11-10 16:29:11 +0900

    test: feature_number_5 / test completed

--- src/test/java/lotto/GameTest.java
+++ src/test/java/lotto/GameTest.java
@@ -6,4 +6,12 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GameTest {
+    @Test
+    void generateGameTest() {
+        Game testGame = new Game(10000);
+        testGame.generateGame();
+        for (Lotto lottoI : testGame.games) {
+            assertThat(lottoI.getNumbers()).isEqualTo(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        }
+    }
 }
