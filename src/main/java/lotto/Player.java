49b2b081f048cd30444efe7736920a9d686e2004
heesang <dlagmltkd12@naver.com>
2022-11-11 01:26:27 +0900

    feat: 주어진 금액으로 살 수 있는 로또 개수와 입력한 로또 넘버의 개수가 같지 않는 경우 예외 처리

--- src/main/java/lotto/Player.java
+++ src/main/java/lotto/Player.java
@@ -8,6 +8,7 @@ public class Player {
     public static final int LOTTO_PRICE = 1000;
     public static final String ERROR_NOT_VALID_PRICE = "[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다.";
     public static final int CRITERION_ZERO = 0;
+    public static final String ERROR_PRICE_COUNT_NOT_EQUAL_LOTTO_SIZE = "[ERROR] 주어진 금액과 주어진 로또 번호의 개수가 일치하지 않습니다.";
 
     public int canBuyLottoCount(int price) {
         validate(price);
@@ -25,10 +26,22 @@ public class Player {
     }
 
     public List<Lotto> buyLottos(int price, List<List<Integer>> lottoNumbers) {
-        canBuyLottoCount(price);
+        int buyLottoCount = canBuyLottoCount(price);
+        validateCount(buyLottoCount, lottoNumbers);
+
         return lottoNumbers.stream()
                 .map(Lotto::new)
                 .collect(Collectors.toList());
+    }
+
+    private void validateCount(int buyLottoCount, List<List<Integer>> lottoNumbers) {
+        if (!isSame(buyLottoCount, lottoNumbers)) {
+            throw new IllegalArgumentException(ERROR_PRICE_COUNT_NOT_EQUAL_LOTTO_SIZE);
+        }
+    }
+
+    private boolean isSame(int count, List<List<Integer>> lottoNumbers) {
+        return count == lottoNumbers.size();
     }
 
 }
--- src/test/java/lotto/PlayerTest.java
+++ src/test/java/lotto/PlayerTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +11,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PlayerTest {
 
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player();
+    }
+
     @DisplayName("10000원이 주어졌을 때 살수있는 로또의 개수는 10개")
     @Test
     void canByLottoCount() {
-        Player player = new Player();
         int count = player.canBuyLottoCount(10000);
         assertThat(count).isEqualTo(10);
     }
@@ -21,23 +28,31 @@ public class PlayerTest {
     @DisplayName("구임 금액이 1000으로 나누어 떨어지지 않으면 예외 발생")
     @Test
     void validatePrice() {
-        assertThatThrownBy(() -> {
-            Player player = new Player();
-            player.canBuyLottoCount(1350);
-        }).isInstanceOf(IllegalArgumentException.class)
+        assertThatThrownBy(() -> player.canBuyLottoCount(1350))
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다.");
     }
 
     @DisplayName("주어진 금액 많큼 로또를 발행하는 기능")
     @Test
     void buyLottos() {
-        Player player = new Player();
-        List<List<Integer>> lottoNumbers = List.of(
+        List<Lotto> lottos = player.buyLottos(3000, createLottoNumbers());
+        assertThat(lottos.size()).isEqualTo(3);
+    }
+
+    private List<List<Integer>> createLottoNumbers() {
+        return List.of(
                 List.of(1, 2, 3, 4, 5, 6),
                 List.of(1, 2, 3, 4, 5, 7),
                 List.of(1, 2, 3, 4, 5, 8)
         );
-        List<Lotto> lottos = player.buyLottos(3000, lottoNumbers);
-        assertThat(lottos.size()).isEqualTo(3);
+    }
+
+    @DisplayName("주어진 금액으로 살 수 있는 로또 개수와 로또 넘버의 개수가 같이 않는 경우 예외 발생")
+    @Test
+    void buyLottosDifferentSize() {
+        assertThatThrownBy(() -> player.buyLottos(4000, createLottoNumbers()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 주어진 금액과 주어진 로또 번호의 개수가 일치하지 않습니다.");
     }
 }
