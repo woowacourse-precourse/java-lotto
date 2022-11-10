a5204974686695e12da37020038124fec048de23
박원종 <wonjong1030@naver.com>
2022-11-11 02:55:44 +0900

    test: 구매한 로또 출력 기능 테스트

--- src/test/java/lotto/ConsoleTest.java
+++ src/test/java/lotto/ConsoleTest.java
@@ -1,4 +1,17 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ConsoleTest {
-  
+    @Test
+    void 구매한_로또_출력_테스트() {
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        List<Integer>[] randomNumbers = lottoGenerator.createRandomNumbers(8);
+        Console console = new Console();
+        console.outputMyLottos(randomNumbers);
+    }
 }
