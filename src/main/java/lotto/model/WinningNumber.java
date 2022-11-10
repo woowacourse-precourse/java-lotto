6879510c7bb5881fea5973322ce0418160af019e
Eunkeee <eunkeeeee@gmail.com>
2022-11-10 14:40:54 +0900

    feat: 당첨 번호의 개수가 6개가 아니면 예외 처리

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -9,9 +9,16 @@ public class WinningNumber {
     public WinningNumber() {
         List<String> winningNumbers;
         // 당첨번호 입력
-        String input = removeSpace(Console.readLine());
+        String input = removeSpace(inputWinningNumbers());
         winningNumbers = Arrays.asList(input.split(","));
+        if(winningNumbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
         System.out.println(winningNumbers);
+    }
+
+    private static String inputWinningNumbers() {
+        return Console.readLine();
     }
 
     private String removeSpace(String input) {
