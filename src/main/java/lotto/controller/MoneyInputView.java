7b39183ab9ff23fbf7982c1ebbdb151a79d7c7be
Laeng <Laeng@users.noreply.github.com>
2022-11-10 23:52:10 +0900

    fix(MoneyInputView): show 메서드명 변경
    ### MoneyInputView
    show 메서드
    - 메서드 이름 변경

--- src/main/java/lotto/controller/MoneyInputView.java
+++ src/main/java/lotto/controller/MoneyInputView.java
@@ -6,7 +6,7 @@ public class MoneyInputView extends View {
     private String money;
 
     @Override
-    public void Show() {
+    public void show() {
         printLine("구입 금액을 입력해 주세요.");
         printLine("구입 금액은 숫자만 입력할 수 있으며 1000원 단위로 구매할 수 있어요.");
 
