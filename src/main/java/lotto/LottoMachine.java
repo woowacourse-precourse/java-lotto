56b1b973d58c388e74f1d699b703f53740f6bd0a
anneu <b.imnuj@gmail.com>
2022-11-11 01:11:50 +0900

    feat: 로또 구입금액이 1000원 단위인지 검증하는 기능 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -3,6 +3,7 @@ package lotto;
 public class LottoMachine {
     private static final int MIN_MONEY = 1000;
     private static final String MIN_MONEY_ERROR_MESSAGE = "[ERROR] 구입금액은 1000원 이상부터 가능합니다.";
+    private static final String MONEY_UNIT_ERROR_MESSAGE = "[ERROR] 구입금액은 1000원 단위만 가능합니다.";
 
     private int money;
 
@@ -13,11 +14,22 @@ public class LottoMachine {
 
     private void validate(int money) {
         validateMinMoney(money);
+        validateMoneyUnit(money);
     }
 
     private void validateMinMoney(int money) {
         if (money < MIN_MONEY) {
             throw new IllegalArgumentException(MIN_MONEY_ERROR_MESSAGE);
         }
+    }
+
+    private void validateMoneyUnit(int money) {
+        if (isNotValidMoneyUnit(money)) {
+            throw new IllegalArgumentException(MONEY_UNIT_ERROR_MESSAGE);
+        }
+    }
+
+    private boolean isNotValidMoneyUnit(int money) {
+        return money % MIN_MONEY != 0;
     }
 }
