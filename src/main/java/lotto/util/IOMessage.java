5af93818af5d46efa2d1d954b8f4b7137dec3f51
woosung1223 <dntjd991223@naver.com>
2022-11-10 23:05:15 +0900

    feat(IOMessage): 입출력 관련 메세지를 담당하는 IOMessage enum 클래스 생성

---
+++ src/main/java/lotto/util/IOMessage.java
@@ -0,0 +1,17 @@
+package lotto.util;
+
+public enum IOMessage {
+    READ_AMOUNT_MESSAGE("구입금액을 입력해 주세요."),
+    READ_WINNING_NUM_MESSAGE("당첨 번호를 입력해 주세요."),
+    READ_BONUS_NUM_MESSAGE("보너스 번호를 입력해 주세요.");
+
+    private final String message;
+
+    IOMessage(String message) {
+        this.message = message;
+    }
+
+    public void printMessage() {
+        System.out.println(this.message);
+    }
+}
