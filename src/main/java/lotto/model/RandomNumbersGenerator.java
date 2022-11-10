1f0db2126c76d9c11f19d3b02574f8c5c4e5a5f4
Albert <guang7226961@naver.com>
2022-11-10 10:00:27 +0900

    feat($model) 중복 된 숫자 있으면 예외처리

--- src/main/java/lotto/model/RandomNumbersGenerator.java
+++ src/main/java/lotto/model/RandomNumbersGenerator.java
@@ -6,8 +6,10 @@ import java.util.List;
 
 class RandomNumbersGenerator {
 
+    public static final String REPEAT_NUMBER_ERROR = "중복된 숫자가 있습니다.";
     public static final String NUMBERS_SIZE_ERROR = "[ERROR] 숫자가 6개이여야 합니다.";
     public static final int REQUIRED_SIZE = 6;
+
 
     public static List<Integer> creatSixDifferentNumbers() {
         List<Integer> result = pickUniqueNumbersInRange(1, 46, 6);
@@ -18,6 +20,13 @@ class RandomNumbersGenerator {
     public static void isValidSize(List<Integer> numbers) {
         if (numbers.size() != REQUIRED_SIZE) {
             throw new IllegalArgumentException(NUMBERS_SIZE_ERROR);
+        }
+    }
+
+    public static void isValidRepeat(List<Integer> numbers) {
+        long numbersSize = numbers.stream().distinct().count();
+        if (numbersSize != REQUIRED_SIZE) {
+            throw new IllegalArgumentException(REPEAT_NUMBER_ERROR);
         }
     }
 }
--- src/test/java/lotto/model/RandomNumbersGeneratorTest.java
+++ src/test/java/lotto/model/RandomNumbersGeneratorTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 class RandomNumbersGeneratorTest {
 
     @Test
-    void 번호가_6개_이여야_한다() {
+    void 번호가_6개아니면_예외() {
         List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5));
         assertThatThrownBy(() -> isValidSize(numbers)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(NUMBERS_SIZE_ERROR);
@@ -23,5 +23,16 @@ class RandomNumbersGeneratorTest {
         numbers.add(9);
         assertThatThrownBy(() -> isValidSize(numbers)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(NUMBERS_SIZE_ERROR);
+    }
+
+    @Test
+    void 중복_수가_있으면_예외(){
+        List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 5));
+        assertThatThrownBy(() -> RandomNumbersGenerator.isValidRepeat(numbers)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 숫자가 있습니다.");
+
+        numbers.remove(numbers.size() - 1);
+        numbers.add(6);
+        assertThatCode(() -> RandomNumbersGenerator.isValidRepeat(numbers)).doesNotThrowAnyException();
     }
 }
