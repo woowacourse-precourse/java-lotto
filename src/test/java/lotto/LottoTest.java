61d9e75e092706c782fd0ef71745524c445e0d44
HyebinShin <hyeviin.dev@gmail.com>
2022-11-10 22:08:48 +0900

    Test: 발행된 로또 수량과 로또 발행 횟수가 다를 때 예외 테스트 추가
    createDiffLottoSizeFromPublishNum()

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -38,4 +38,16 @@ class LottoTest {
         List<Integer> numbers = new ArrayList<>(List.of(6, 5, 4, 3, 2, 1));
         assertThat(Lotto.sortNumbers(numbers)).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
     }
+
+    @Test
+    @DisplayName("발행된 로또의 수량과 로또 발행 횟수가 다르면 예외가 발생한다.")
+    void createDiffLottoSizeFromPublishNum() {
+        List<Lotto> lotto = new ArrayList<>(List.of(
+                new Lotto(List.of(1,2,3,4,5,6)),
+                new Lotto(List.of(11,12,13,14,15,16)),
+                new Lotto(List.of(21,22,23,24,25,26))
+        ));
+        assertThatThrownBy(() -> Lotto.validateLotto(lotto, 2))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
