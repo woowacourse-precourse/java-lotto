013d7acfac50ec0c59084dcfd4d1c36abb915e36
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-10 15:00:57 +0900

    feat(PrintMessenger) : volumeBuying

--- src/main/java/lotto/PrintMessenger.java
+++ src/main/java/lotto/PrintMessenger.java
@@ -4,4 +4,7 @@ public class PrintMessenger {
     public static void askPriceToBuy(){
         System.out.println("구입금액을 입력해 주세요.");
     }
+    public static void volumeBuying(int volume){
+        System.out.println(volume + "개를 구매했습니다.");
+    }
 }
