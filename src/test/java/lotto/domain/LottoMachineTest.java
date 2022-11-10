b009ed1c499fd9caa43e181042054dbb2fdff639
khsrla9806 <kimhunsope@naver.com>
2022-11-10 18:47:57 +0900

    test(LottoMachineTest): 발행된 로또에 중복된 숫자가  있는지 테스트 구현

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -4,16 +4,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoMachineTest {
     @DisplayName("6개의 번호를 갖는 로또가 발행됐는지 테스트")
     @Test
-    void issueLottoTest() {
+    void issueLottoSizeTest() {
         LottoMachine lottoMachine = new LottoMachine();
         List<Integer> numbers = lottoMachine.issueLotto();
+        assertThat(numbers.size()).isEqualTo(6);
+    }
+
+    @DisplayName("발행된 로또에 중복이 있는지 테스트")
+    @Test
+    void issueLottoDuplicateTest() {
+        LottoMachine lottoMachine = new LottoMachine();
+        Set<Integer> numbers = new HashSet<>(lottoMachine.issueLotto());
         assertThat(numbers.size()).isEqualTo(6);
     }
 }
