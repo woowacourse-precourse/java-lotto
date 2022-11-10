43704974edc41a615fd4062d9446c05e69e35ee5
young2866 <62212610+young2866@users.noreply.github.com>
2022-11-11 03:10:37 +0900

    feat(Valid): 사용자가 입력한 구입금액이 숫자인지 검증하는 기능 추가

--- src/main/java/lotto/Valid.java
+++ src/main/java/lotto/Valid.java
@@ -6,7 +6,7 @@ public class Valid {
 	private static final String INPUT_PURCHASE_AMOUNT_ERROR = "[ERROR]구입금액 입력이 잘못 되었습니다.";
 
 	public static int validatePurchaseAmount(String purchaseAmount) {
-		int verification = Integer.parseInt(purchaseAmount);
+		int verification = isNumber(purchaseAmount);
 		if(isDividedBy1000(verification))
 			return verification;
 		throw new IllegalArgumentException(INPUT_PURCHASE_AMOUNT_ERROR);
@@ -16,5 +16,16 @@ public class Valid {
 		if(verification % LOTTO_PRICE == ZERO)
 			return true;
 		return false;
+	}
+
+	private static int isNumber(String purchaseAmount) {
+		if (isNumeric(purchaseAmount)) {
+			return Integer.parseInt(purchaseAmount);
+		}
+		throw new IllegalArgumentException(INPUT_PURCHASE_AMOUNT_ERROR);
+	}
+
+	private static boolean isNumeric(String str) {
+		return str.chars().allMatch(Character::isDigit);
 	}
 }
