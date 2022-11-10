e56109cd0313423884b8b9436e43de60927c0378
Park shinyeong <syngh503@gmail.com>
2022-11-10 22:39:53 +0900

    Feat(InputUtils): InputUtils 메소드 생성
    InputUtils 테스트를 생성하기 전 미리 각 기능에 대한 메소드를 생성하였습니다.
    메소드의 각 기능을 구현하지는 않았습니다.
    
    Relate to: #6

---
+++ src/main/java/Utils/InputUtils.java
@@ -0,0 +1,30 @@
+package Utils;
+
+public class InputUtils {
+    public static boolean isMultipleOfOneThousand() {
+        // 1000원 단위로 떨어지는지 검증
+        return false;
+    }
+
+    public static boolean isNumberLengthSix() {
+        // 당첨 번호가 6자리인지 확인
+        return false;
+    }
+
+    public static boolean isNumberSplitWithComma() {
+        // 당첨 번호가 쉼표로 구분되어 있는지
+        return false;
+    }
+
+    public static boolean isDigit() {
+        // 정수 이외의 다른 문자 값을 입력하였는지 확인
+        return false;
+    }
+
+    public static boolean isNumberInLottoRange() {
+        // 숫자가 1~45 사이의 범위에 존재하는지 확인
+        return false;
+    }
+    }
+
+}
