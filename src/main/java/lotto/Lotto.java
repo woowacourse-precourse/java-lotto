9cb8271a41a94a1afd189163025df341c53b4fdc
Hayoon <hy97@sju.ac.kr>
2022-11-10 15:46:14 +0900

    feat(Lotto): 총 수익률(%) 데이터 변환 후 출력기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,8 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class Lotto{
+public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -20,13 +21,13 @@ public class Lotto{
     }
 
     // TODO: 추가 기능 구현
-    public void calculatorMatchingNumbers(List<List<Integer>> userLottos, int bonusNumber) {
-        HashMap<Integer, Integer> matchingPair = new HashMap<>(5){{
-                put(3, 0);
-                put(4, 0);
-                put(5, 0);
-                put(7, 0); // 5개 일치 + 보너스 번호
-                put(6, 0);
+    public void calculatorMatchingNumbers(List<List<Integer>> userLottos, int bonusNumber, int purchasePrice) {
+        HashMap<Integer, Integer> matchingPair = new HashMap<>(5) {{
+            put(3, 0);
+            put(4, 0);
+            put(5, 0);
+            put(7, 0); // 5개 일치 + 보너스 번호
+            put(6, 0);
         }};
 
         for (List<Integer> userLotto : userLottos) {
@@ -35,7 +36,8 @@ public class Lotto{
                 if (userLotto.contains(number)) {
                     matchingCount++;
                 }
-            } if (matchingCount >= 3 && matchingCount <= 6) {
+            }
+            if (matchingCount >= 3 && matchingCount <= 6) {
                 if (matchingCount == 5 && userLotto.contains(bonusNumber)) {
                     matchingPair.replace(7, matchingPair.get(7) + 1);
                 } else {
@@ -49,9 +51,20 @@ public class Lotto{
         System.out.println(Constant.FIVE_MATCHING_WITH_BONUS_NUM + " - " + matchingPair.get(7) + "개");
         System.out.println(Constant.SIX_MATCHING + " - " + matchingPair.get(6) + "개");
 
-        //System.out.println(Constant.TOTAL_YIELD + " " + + "%입니다.");
+        //총 수익률
+        int sum = 0;
+        sum += matchingPair.get(3) * 5000;
+        sum += matchingPair.get(4) * 50000;
+        sum += matchingPair.get(5) * 1500000;
+        sum += matchingPair.get(7) * 30000000;
+        sum += matchingPair.get(6) * 2000000000;
+
+        double yield = (double)(sum * 100) / (double)purchasePrice;
+
+        System.out.println(Constant.TOTAL_YIELD + String.format("%.1f", yield) + "%입니다.");
     }
 }
+
 /**
  * 3개 일치 (5,000원) - 1개
  * 4개 일치 (50,000원) - 0개
