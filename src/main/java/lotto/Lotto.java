3add1dd8b2013ca1ba67e474e1b6774842278c4a
anneu <b.imnuj@gmail.com>
2022-11-10 23:14:29 +0900

    feat: 로또 번호에 중복된 숫자가 없는지 검증하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,12 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private static final int LOTTO_NUMBER_SIZE = 6;
     private static final String SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호의 개수는 6개이어야 합니다.";
+    private static final String DUPLICATES_ERROR_MESSAGE = "[ERROR] 로또 번호에 중복된 숫자가 있습니다.";
 
     private final List<Integer> numbers;
 
@@ -15,11 +17,22 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateNumberSize(numbers);
+        validateDuplicateNumber(numbers);
     }
 
     private void validateNumberSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        List<Integer> distinctNumbers = numbers.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        if (numbers.size() != distinctNumbers.size()) {
+            throw new IllegalArgumentException(DUPLICATES_ERROR_MESSAGE);
         }
     }
 }
