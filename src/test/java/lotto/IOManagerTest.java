3cfb815df4c4af734a09e604fb5fa496c194268b
hodomaroo <enghemdwkd@gmail.com>
2022-11-10 19:36:57 +0900

    [Test] : IoManager 숫자 입력 기능 테스트 코드 작성
    IoManager 클래스의 사용자로부터 숫자 하나를 입력받는 기능의 테스트 코드
    작성을 완료하였습니다.

--- src/test/java/lotto/IOManagerTest.java
+++ src/test/java/lotto/IOManagerTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -61,6 +62,55 @@ public class IOManagerTest {
                 Assertions.assertThatThrownBy(() -> {
                     command("1,,2");
                     IoManager.getNumbersFromUser(",");
+                }).isInstanceOf(IllegalArgumentException.class);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("getNumberFromUser 메서드는")
+    class describe_getNumberFromUser {
+
+        @Nested
+        @DisplayName("사용자로부터 숫자를 입력받는데")
+        class describe_its_parameter {
+
+            @Test
+            @DisplayName("알맞은 입력인 경우 이를 Integer 형태로 반환한다.")
+            void return_number_when_receive_right_input() {
+                command("1");
+                Assertions.assertThat(IoManager.getNumberFromUser().equals(1));
+
+                command("52");
+                Assertions.assertThat(IoManager.getNumberFromUser().equals(52));
+            }
+
+            @Test
+            @DisplayName("잘못된 입력인 경우 예외를 발생시킨다")
+            void throw_Exception_when_input_is_wrong() {
+                Assertions.assertThatThrownBy(() -> {
+                    command(",");
+                    IoManager.getNumberFromUser();
+                }).isInstanceOf(IllegalArgumentException.class);
+
+                Assertions.assertThatThrownBy(() -> {
+                    command("123,,55");
+                    IoManager.getNumberFromUser();
+                }).isInstanceOf(IllegalArgumentException.class);
+
+                Assertions.assertThatThrownBy(() -> {
+                    command("");
+                    IoManager.getNumberFromUser();
+                }).isInstanceOf(IllegalArgumentException.class);
+
+                Assertions.assertThatThrownBy(() -> {
+                    command("a");
+                    IoManager.getNumberFromUser();
+                }).isInstanceOf(IllegalArgumentException.class);
+
+                Assertions.assertThatThrownBy(() -> {
+                    command("1,,2");
+                    IoManager.getNumberFromUser();
                 }).isInstanceOf(IllegalArgumentException.class);
             }
         }
