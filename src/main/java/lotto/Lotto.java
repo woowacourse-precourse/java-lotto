c41e7d2eacff205ff102c7f92860f2134045b624
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-10 23:58:46 +0900

    feat : 중복된 숫자가 입력 됬을 때 오류 출력
    중복된 숫자가 들어오면 오류가 출력이 되도록 구현 하였습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,14 +8,23 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        check_duplicate_numbers(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void check_duplicate_numbers(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if (Collections.frequency(numbers, numbers.get(i)) != 1)
+                throw new IllegalArgumentException();
+        }
+
+        // TODO: 추가 기능 구현
+    }
 }
