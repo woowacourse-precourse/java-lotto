e8f2f752672f19c27f06472d348510ae6d1b1b17
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-10 19:37:52 +0900

    <feat>(Lotto.java): Lotto 클래스 생성자에 정렬 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,7 +8,12 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
     private void validate(List<Integer> numbers) {
