eb7930fa33ce8cfbb95ca8a11a53bcaf6167907e
eora21 <eora21@naver.com>
2022-11-10 21:53:43 +0900

    refactor: 💡 변경 당첨 번호 입력 네이밍 변경
    그냥 로또 번호와 당첨 로또 번호를 따로 구분하여 명확히 이해하기 위해 이름 변경

--- src/main/java/constant/LottoText.java
+++ src/main/java/constant/LottoText.java
@@ -3,7 +3,7 @@ package constant;
 public enum LottoText {
     REQUEST_PURCHASE_PRICE("구입금액을 입력해 주세요."),
     PURCHASE_COUNT("%d개를 구매했습니다."),
-    REQUEST_LOTTO_NUMBER("당첨 번호를 입력해 주세요."),
+    REQUEST_WIN_LOTTO_NUMBER("당첨 번호를 입력해 주세요."),
     REQUEST_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     WINNING_STATISTICS("당첨 통계\n---\n"),
     MATCHES("%d개 일치 (%s원) - %d개"),
