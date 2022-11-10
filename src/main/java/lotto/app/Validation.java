cfb0f58c9de69d2c9a920c8cbc5b22d773d52adc
Joonsoo Kim <kjsoo1901@gmail.com>
2022-11-10 19:59:58 +0900

    fix: Update Validation.java
    Fix all method that returns boolean to void
    
    Add IllegalArgumentException and printing message

--- src/main/java/lotto/app/Validation.java
+++ src/main/java/lotto/app/Validation.java
@@ -4,35 +4,40 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Validation {
-    public boolean hasSixItems(List<Integer> numbers){
-        if(numbers.size() != 6)
-            return false;
-        return true;
+    public void hasSixItems(List<Integer> numbers){
+        if(numbers.size() != 6){
+            System.out.println("[ERROR] 로또 번호는 총 6개여야 합니다.");
+            throw new IllegalArgumentException();
+        }
+
     }
 
-    public boolean isInRange(List<Integer> numbers){
+    public void isInRange(List<Integer> numbers){
         for(int i = 0; i < 6; i++){
             if( 1 > numbers.get(i) || numbers.get(i) > 45){
-                return false;
+                System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                throw new IllegalArgumentException();
             }
         }
-        return true;
+
     }
 
-    public boolean isNotDuplicate(List<Integer> numbers){
+    public void isNotDuplicate(List<Integer> numbers){
         for(int i = 0; i < 6; i++){
             List<Integer> number = numbers.stream()
                     .filter(numbers.get(i)::equals)
                     .collect(Collectors.toList());
-            if(number.size() != 1)
-                return false;
+            if(number.size() != 1){
+                System.out.println("[ERROR] 로또 번호는 중복될 수 없습니다.");
+                throw new IllegalArgumentException();
+            }
         }
-        return true;
     }
 
-    public boolean isThousandUnit(int money){
-        if(money % 1000 != 0)
-            return false;
-        return true;
+    public void isThousandUnit(int money){
+        if(money % 1000 != 0){
+            System.out.println("[ERROR] 로또 구입 금액은 1000 단위여야 합니다.");
+            throw new IllegalArgumentException();
+        }
     }
 }
