d99e64c9eda9ada3f4f44f5aabbcd36b804a53a1
kkm5291 <kkm23125291@gmail.com>
2022-11-11 01:08:23 +0900

    [FEAT] Check : 구매할 로또티켓 수 구하는 메서드 추가

--- src/main/java/validate/Check.java
+++ src/main/java/validate/Check.java
@@ -3,9 +3,12 @@ package validate;
 import java.util.List;
 
 public class Check {
-    public static void numberSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+
+    public static int countLottoTicket(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 올바른 금액이 아닙니다. (지폐만 가능)");
         }
+
+        return money / 1000;
     }
 }
