56cf6834492bca14cca5220dde4bf9c66c3c81eb
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2022-11-10 20:50:02 +0900

    TODO LIST 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,11 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 입력값을 확인 후 몇장인지 반환
+        // TODO: 랜덤으로 구매한 로또를 보여줌
+        // TODO: 당첨 번호 입력
+        // TODO: 보너스 번호 입력
+        // TODO: 1등부터 5등까지 몇개나 당첨되었는지 출력
+        // TODO: 수익률을 소수점 둘째 자리에서 반올림하여 출력
     }
 }
