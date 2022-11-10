ec68c531d4999568a4087a9c3046ad5ff6990305
wonju-dev <wonjulee.dev@gmail.com>
2022-11-10 21:37:08 +0900

    feat(Validator): 조건 검증 기능 구현
    Validator는 검증할 조건 목록을 효율적으로 처리하는
    기능을 수행하는 클래스입니다.
    
    getNotPassConditionIndex메소드는 파라미터로 전달받은 조건 목록과
    입력을 검증한 뒤, false를 반환하는 검증의 인덱스를 반환합니다.
    만약 모든 조건이 참인 경우, -1을 반환합니다.

---
+++ src/main/java/lotto/userinterface/validator/Validator.java
@@ -0,0 +1,27 @@
+package lotto.userinterface.validator;
+
+import lotto.userinterface.condition.Condition;
+
+import java.util.List;
+
+public class Validator {
+    private static final Validator instance = new Validator();
+
+    private Validator() {
+    }
+
+    public static Validator getInstance() {
+        return instance;
+    }
+
+    public Integer getNotPassConditionIndex(List<Condition> conditions, String input) {
+        for (int index = 0; index < conditions.size(); index++) {
+            Condition condition = conditions.get(index);
+            if (condition.isSatisfied(input)) {
+                return index;
+            }
+        }
+
+        return -1;
+    }
+}
