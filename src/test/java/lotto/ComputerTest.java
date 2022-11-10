1a0e29c8837f278894d2dda991c57b70dd37c47c
23Yong <1239824@naver.com>
2022-11-10 22:15:55 +0900

    test(Computer): 로또 번호 발행 테스트 코드 작성
    로또 번호가 6개의 중복되지 않는 무작위 수인지 검사하는 테스트 코드를 작성한다

---
+++ src/test/java/lotto/ComputerTest.java
@@ -0,0 +1,34 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ComputerTest {
+
+    private final Computer computer;
+
+    public ComputerTest() {
+        this.computer = new Computer();
+    }
+
+    @DisplayName("범위내의 중복없는 6개의 숫자를 발급하는데 성공한다.")
+    @Test
+    void 범위내의_중복없는_6개의_숫자를_발급한다() {
+        // given
+
+        // when
+        computer.generateLotteryNumbers();
+
+        // then
+        List<Integer> lotteryNumbers = computer.getLotteryNumbers();
+
+        assertThat(lotteryNumbers).hasSize(6);
+        assertThat(lotteryNumbers.stream().collect(Collectors.toSet())).hasSize(6);
+    }
+
+}
