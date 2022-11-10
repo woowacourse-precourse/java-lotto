7c4a482bd9a33fc7bfb6251236eb96a0099b40aa
Sungjaeyeong <wodud2587@gmail.com>
2022-11-10 19:41:22 +0900

    feat(lotto): 로또 머신 클래스 생성

---
+++ src/main/java/lotto/Machine.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Machine {
+    public void buy(int amount) {
+
+    }
+}
