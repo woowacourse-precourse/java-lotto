b080405b78cc08ad0b517f2f93210372fb99d8a3
CMHyuk <alsgur9710@naver.com>
2022-11-11 00:19:25 +0900

    test(LottoNumberGenerator): 로또 구입 수만큼 번호 생성되는지 테스트

---
+++ src/test/java/lotto/domain/LottoNumberGeneratorTest.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class LottoNumberGeneratorTest {
+
+    LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+
+    @DisplayName("로또 구입 수만큼 번호 생성되는지 테스트")
+    @Test
+    void checkSize() {
+        List<List<Integer>> lottoNumbers = lottoNumberGenerator.generateLottoNumber(5);
+        Assertions.assertThat(lottoNumbers.size()).isSameAs(5);
+    }
+}
