fdc73c0b5d72e389a875e899fc435f890f8cbbd0
MuSeung <peter0757@nvaer.com>
2022-11-10 13:32:54 +0900

    refactor: Lotto의 validate 메서드의 if문의 판별 기준을 메서드로 따로 정리
    isWrongSize
        - 사이즈가 6이 아니라면 true를 반환
    isDuplicate
        - 중복이 있다면 true를 반환
    isOutOfBoud
        - 범위에서 벗어난 수가 있다면 true를 반환

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,19 +14,29 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (isWrongSize(numbers)) {
             throw new IllegalArgumentException();
         }
-        Set<Integer> numbersSet = new HashSet<>(numbers);
-        if (numbers.size() != numbersSet.size()) {
+        if (isDuplicate(numbers)) {
             throw new IllegalArgumentException();
         }
-        for (int number : numbers) {
-            if (number < LottoState.START.number() || number > LottoState.END.number()) {
-                throw new IllegalArgumentException();
-            }
+        if (isOutOfBound(numbers)) {
+            throw new IllegalArgumentException();
         }
     }
-
-
+    private boolean isWrongSize(List<Integer> numbers) {
+        return numbers.size() != 6;
+    }
+    private boolean isDuplicate(List<Integer> numbers) {
+        Set<Integer> numbersSet = new HashSet<>(numbers);
+        return numbers.size() != numbersSet.size();
+    }
+    private boolean isOutOfBound(List<Integer> numbers) {
+        boolean hasWrongBoundaryNumber = false;
+        for (int number : numbers) {
+            hasWrongBoundaryNumber |= number < LottoState.START.number();
+            hasWrongBoundaryNumber |= number > LottoState.END.number();
+        }
+        return hasWrongBoundaryNumber;
+    }
 }
