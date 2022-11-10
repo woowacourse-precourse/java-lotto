48b56ac55c184e3dbc0f5bd825ff8cadcc1eaeab
jmj005 <jmj00005@naver.com>
2022-11-10 14:09:04 +0900

    feat(Application): 로또 구입 금액 입력받기 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static int inputMoney() {
+        String inputMoney = Console.readLine();
+        validateMoney(inputMoney);
+        return Integer.parseInt(inputMoney);
+    }
+
+    public static void validateMoney(String inputMoney) {
+        if (Integer.parseInt(inputMoney) % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해야 합니다.");
+        }
     }
 }
