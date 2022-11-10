9178bf7eb2c2ddbfd27d6e063771b4d766d301a3
yj2980 <gum0945@naver.com>
2022-11-10 22:28:55 +0900

    feat : Company 객체 생성

--- src/main/java/lotto/domain/Company.java
+++ src/main/java/lotto/domain/Company.java
@@ -1,2 +1,22 @@
-package lotto.domain;public class Company {
+package lotto.domain;
+
+import java.util.List;
+
+public class Company {
+
+	private List<Integer> winningLotto;
+	private int bonus;
+
+	public Company(List<Integer> winningLotto, int bonus) {
+		this.winningLotto = winningLotto;
+		this.bonus = bonus;
+	}
+
+	public int getBonus() {
+		return bonus;
+	}
+
+	public List<Integer> getWinningLotto() {
+		return winningLotto;
+	}
 }
