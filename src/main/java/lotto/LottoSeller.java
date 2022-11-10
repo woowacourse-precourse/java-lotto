4e9332d647d18a91b90d89428d942938f502888a
cpot5620 <=cpot5620@naver.com>
2022-11-11 01:20:25 +0900

    feat(LottoSeller): create method to check winning number

--- src/main/java/lotto/LottoSeller.java
+++ src/main/java/lotto/LottoSeller.java
@@ -6,8 +6,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static lotto.LottoMachine.*;
+
 public class LottoSeller {
     private static final String NUM_ERROR_MSG = "[ERROR] 숫자만 입력해주세요";
+    private static final String RANGE_ERROR_MSG = "[ERROR] 1~45 사이의 숫자를 입력해주세요";
     private static final String PURCHASE_PRICE_MSG = "구매금액을 입력해 주세요.";
     private static final String WINNING_NUM_MSG = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUM_MSG = "보너스 번호를 입력해 주세요.";
@@ -22,10 +25,7 @@ public class LottoSeller {
     }
 
     private void validatePrice(String input) {
-        if (!input.matches("^[0-9]*$")) {
-            System.out.println(NUM_ERROR_MSG);
-            throw new IllegalArgumentException();
-        }
+        validateNumber(input);
     }
 
     public List<Integer> receiveWinningNumbers() {
@@ -42,24 +42,24 @@ public class LottoSeller {
     }
 
     private void validateWinningNumbers(String input) {
-        String str = input.split(SEPARATOR).toString();
+        String[] numbers = input.split(SEPARATOR, 0);
 
-        if (!str.matches("^[1-45]*$")) {
-            System.out.println(NUM_ERROR_MSG);
+        for (int i = 0; i < numbers.length; i++) {
+            validateNumber(numbers[i]);
+            int number = Integer.parseInt(numbers[i]);
+            validateNumberRange(number);
+        }
+    }
+
+    private void validateNumberRange(int number) {
+        if (number < MIN_LOTTO_NUM || number > MAX_LOTTO_NUM) {
+            System.out.println(RANGE_ERROR_MSG);
             throw new IllegalArgumentException();
         }
     }
 
-    public int receiveBonusNumber() {
-        System.out.println(BONUS_NUM_MSG);
-        String userInput = receiveUserInput();
-        validateBonusNumber(userInput);
-
-        return Integer.parseInt(userInput);
-    }
-
-    private void validateBonusNumber(String input) {
-        if (!input.matches("^[1-45]*$")) {
+    private void validateNumber(String input){
+        if (!input.matches("^[0-9]*$")) {
             System.out.println(NUM_ERROR_MSG);
             throw new IllegalArgumentException();
         }
--- src/test/java/lotto/LottoSellerTest.java
+++ src/test/java/lotto/LottoSellerTest.java
@@ -6,24 +6,26 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
 class LottoSellerTest {
     final LottoSeller lottoSeller = new LottoSeller();
 
     @DisplayName("숫자가 아닌 금액을 입력할 경우 예외가 발생한다.")
     @Test
-    void inputStringForPrice(){
+    void inputStringForPrice() {
         String input = "200a";
         InputStream inputStream = new ByteArrayInputStream(input.getBytes());
         System.setIn(inputStream);
 
-        Assertions.assertThatThrownBy(()-> lottoSeller.receivePurchasePrice())
+        Assertions.assertThatThrownBy(() -> lottoSeller.receivePurchasePrice())
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("올바른 금액을 입력하면 정상적으로 동작한다.")
     @Test
-    void inputValidPrice(){
+    void inputValidPrice() {
         String input = "20000";
         InputStream inputStream = new ByteArrayInputStream(input.getBytes());
         System.setIn(inputStream);
@@ -31,5 +33,51 @@ class LottoSellerTest {
         int price = lottoSeller.receivePurchasePrice();
 
         Assertions.assertThat(price).isEqualTo(20000);
+    }
+
+    @DisplayName("중복된 당첨번호를 입력하면 예외가 발생한다.")
+    @Test
+    void inputWinningNumberByDuplicatedNumber() {
+        String input = "1,1,2,3,4,5";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        Assertions.assertThatThrownBy(() -> new Lotto(lottoSeller.receiveWinningNumbers()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자가 아닌 당첨번호를 입력하면 예외가 발생한다.")
+    @Test
+    void inputStringForWinningNumber() {
+        String input = "1,1,2,3,a,5";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        Assertions.assertThatThrownBy(() -> lottoSeller.receiveWinningNumbers())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("6개가 아닌 당첨번호를 입력하면 예외가 발생한다.")
+    @Test
+    void inputWinningNumberBySizeOver() {
+        String input = "1,9,8,7,6,5,4";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        Assertions.assertThatThrownBy(() -> new Lotto(lottoSeller.receiveWinningNumbers()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("올바른 당첨번호를 입력하면 정상적으로 동작한다.")
+    @Test
+    void inputValidWinningNumber() {
+        String input = "15,16,20,45,11,9";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        List<Integer> winningNumbers = lottoSeller.receiveWinningNumbers();
+
+        Assertions.assertThat(winningNumbers)
+                .isEqualTo(Arrays.asList(15, 16, 20, 45, 11, 9));
     }
 }
