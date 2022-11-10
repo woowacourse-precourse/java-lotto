672d548637848d209d1d0c632d24439e09f45e5d
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-10 17:14:38 +0900

    feat: 1~45사이의 숫자를 얻을 수 있는 기능 생성

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,2 +1,25 @@
-package lotto.domain;public class LottoNumber {
+package lotto.domain;
+
+public class LottoNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final String ERROR_MESSAGE = "[ERROR]: 로또 숫자는 %d에서 %d사이의 값이여야 합니다.";
+
+    private final int value;
+
+
+    public LottoNumber(int value) {
+        validateValue(value);
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    private void validateValue(int value) {
+        if (value < MIN_NUMBER || value > MAX_NUMBER) {
+            throw new IllegalArgumentException(String.format(ERROR_MESSAGE,MIN_NUMBER,MAX_NUMBER));
+        }
+    }
 }
