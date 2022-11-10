8d3b722fc7ffb113b3a21dc41ed638603fc6a832
juhwang <juhong4930@naver.com>
2022-11-10 16:26:45 +0900

    test(CustomerTest): validate readline is integer or not

--- src/test/java/lotto/CustomerTest.java
+++ src/test/java/lotto/CustomerTest.java
@@ -1,25 +1,25 @@
 package lotto;
 
-import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import user.Customer;
 
-import java.lang.reflect.Field;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CustomerTest {
 
-    @DisplayName("구입 금액이 1,000원 단위인 경우 객체 생성")
+    @DisplayName("입력값이 숫자인 경우 Customer 객체 생성")
     @Test
     void case1_validate_unit() {
-        assertThat(new Customer(1000))
+        assertThat(new Customer("1000"))
                 .isInstanceOf(Customer.class);
     }
 
-    @DisplayName("구입 금액이 1,000원 단위가 아닌 경우 예외 발생")
+    @DisplayName("입력값이 숫자가 아닌 아닌 경우 예외 발생")
     @Test
     void case2_validate_unit() {
-        assertThatThrownBy(() -> new Customer(1001))
+        assertThatThrownBy(() -> new Customer("1000j"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
