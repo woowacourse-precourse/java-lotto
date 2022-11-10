a2d8b7299193d479eb36017022aeea3d17b0f984
EUNJEONGMUN <munej26@gmail.com>
2022-11-11 00:10:13 +0900

    fix: MatchCount 클래스에 hashCode와 equals 오버라이드
    totalMathResult에 삽입할 때 같은 당첨 개수를 가진 객체가
    같은 객체로 인식하지 못해 hashCode와 equals를 오버라이드해
    수정함.

--- src/main/java/lotto/MatchCount.java
+++ src/main/java/lotto/MatchCount.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Objects;
+
 public class MatchCount {
     private int winningCount;
     private int bonusCount;
@@ -7,5 +9,20 @@ public class MatchCount {
     public MatchCount(int winningCount, int bonusCount) {
         this.winningCount = winningCount;
         this.bonusCount = bonusCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(winningCount, bonusCount);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof MatchCount)) {
+            return false;
+        }
+        MatchCount matchCount = (MatchCount) obj;
+        return matchCount.winningCount == winningCount &&
+                matchCount.bonusCount == bonusCount;
     }
 }
