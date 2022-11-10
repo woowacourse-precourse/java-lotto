de93ad2b51426b73d837b4e2fe6954f1e74f5be2
wonju-dev <wonjulee.dev@gmail.com>
2022-11-10 19:31:39 +0900

    refactor(Lotto): magic number 상수 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,9 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+
+    private static final Integer SIZE = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,7 +14,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != SIZE) {
             throw new IllegalArgumentException();
         }
     }
