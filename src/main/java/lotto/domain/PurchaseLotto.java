c32294e8c290da77fe6f225de50c3695bbbf992e
eunji53 <duqtj00@naver.com>
2022-11-10 15:00:29 +0900

    <Style>:메서드 이름 변경

--- src/main/java/lotto/domain/PurchaseLotto.java
+++ src/main/java/lotto/domain/PurchaseLotto.java
@@ -8,19 +8,21 @@ import java.util.Comparator;
 import java.util.List;
 
 public class PurchaseLotto {
-    public int inputMoney(){
+    public int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        String moneyInput=readLine();
-        int money =Integer.valueOf(moneyInput);
+        String moneyInput = readLine();
+        int money = Integer.valueOf(moneyInput);
         return money;
     }
-    public static int countLotto(int money){
-        if (money%1000!=0){
+
+    public static int countLotto(int money) {
+        if (money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 금액을 1000원 단위로 입력해 주세요.");
         }
-        return money/1000;
+        return money / 1000;
     }
-    public List<Integer> lottoNumber(){
+
+    public List<Integer> myLottoNumber() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         numbers.sort(Comparator.naturalOrder());
         return numbers;
