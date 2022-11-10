2b5194552e021913929f50f6ad35a5e2d8391c23
devmizz <garamminchan@gmail.com>
2022-11-10 22:12:23 +0900

    test: 보너스 번호 입력 받기 테스트 작성
    숫자 범위, 중복 확인, 개수 확인

--- src/test/java/lotto/LotteryMachineTest.java
+++ src/test/java/lotto/LotteryMachineTest.java
@@ -83,4 +83,68 @@ class LotteryMachineTest {
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
+
+    @DisplayName("정답 로또 입력에 숫자 범위를 벗어난 값이 입력될 때 예외가 발생하는지")
+    @Test
+    void createLottoOutOfRange() {
+        String money = "12000";
+        String numbers = "1,8,4,2,5,77";
+        String input = money + System.getProperty("line.separator") + numbers;
+
+        assertThatThrownBy(() -> {
+            System.setIn(new ByteArrayInputStream(input.getBytes()));
+            Application.main(new String[]{});
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("보너스 번호 생성이 제대로 되는지")
+    @Test
+    void createBonus() {
+        String money = "12000";
+        String numbers = "1,8,4,2,5,3";
+        String bonus = "15";
+        String input = money + System.getProperty("line.separator")
+                + numbers + System.getProperty("line.separator")
+                + bonus;
+
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        Application.main(new String[]{});
+
+        assertThat(LotteryMachine.getInstance().getBonus()).isEqualTo(Integer.parseInt(bonus));
+    }
+
+    @DisplayName("보너스 번호 생성시 중복 숫자에 대한 예외가 발생이 되는지")
+    @Test
+    void createBonusDuplicate() {
+        String money = "12000";
+        String numbers = "1,8,4,2,5,3";
+        String bonus = "3";
+        String input = money + System.getProperty("line.separator")
+                + numbers + System.getProperty("line.separator")
+                + bonus;
+
+        assertThatThrownBy(() -> {
+            System.setIn(new ByteArrayInputStream(input.getBytes()));
+            Application.main(new String[]{});
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("보너스 번호 생성시 숫자 범위를 벗어나는 입력에 대한 예외가 발생이 되는지")
+    @Test
+    void createBonusOutOfRange() {
+        String money = "12000";
+        String numbers = "1,8,4,2,5,3";
+        String bonus = "48";
+        String input = money + System.getProperty("line.separator")
+                + numbers + System.getProperty("line.separator")
+                + bonus;
+
+        assertThatThrownBy(() -> {
+            System.setIn(new ByteArrayInputStream(input.getBytes()));
+            Application.main(new String[]{});
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
 }
