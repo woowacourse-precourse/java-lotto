e53d9a9c0614a95ebf5755bdef40f2f4d3091838
jiwon1217 <koagjw@gmail.com>
2022-11-11 04:26:24 +0900

    feat: 발행한 로또 번호 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,13 +1,31 @@
 package lotto.view;
 
+import java.util.stream.Collectors;
+import lotto.model.Lotto;
 import lotto.model.LottoCount;
+import lotto.model.Lottos;
 
 public class OutputView {
     private static final String NEW_LINE = "\n";
+    private static final String OPEN_BRACKET = "[";
+    private static final String CLOSE_BRACKET = "]";
+    private static final String COMMA = ", ";
     private static final String NUMBER_OF_PURCHASED_LOTTOS = "%d개를 구매했습니다.";
 
     public static void printLottoCount(LottoCount lottoCount) {
         System.out.print(NEW_LINE);
         System.out.printf(NUMBER_OF_PURCHASED_LOTTOS, lottoCount.getLottoCount());
+    }
+
+    public static void printPurchasedLottoNumbers(Lottos lottos) {
+        for (Lotto lotto : lottos.getLottos()) {
+            String lottoNumbers = OPEN_BRACKET
+                    + lotto.get()
+                    .stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(COMMA))
+                    + CLOSE_BRACKET;
+            System.out.println(lottoNumbers);
+        }
     }
 }
