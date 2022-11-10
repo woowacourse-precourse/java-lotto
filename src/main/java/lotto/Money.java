fadad18745c152b9fc808c9ef0a0114adbc419da
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-10 17:47:57 +0900

    feat: 입력 금액이 숫자인지 판단하는 함수 작성

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -1,10 +1,26 @@
 package lotto;
 
+import java.lang.IllegalArgumentException;
+
 public class Money {
 
     private final Integer money;
 
     public Money(String money) {
+        validate(money);
         this.money = Integer.parseInt(money);
+    }
+
+    public void validate(String money) {
+        if (!validateIsDigit(money))
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    public boolean validateIsDigit(String money) {
+        for (char c : money.toCharArray()) {
+            if (!Character.isDigit(c))
+                return false;
+        }
+        return true;
     }
 }
