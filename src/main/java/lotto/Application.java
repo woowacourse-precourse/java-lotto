809bb72b0fe3430cfc1ff5948a5323122378efd2
49ehyeon42 <49ehyeon42@gmail.com>
2022-11-10 19:33:10 +0900

    fix: 문자열이 숫자인지 검증하는 메서드 static 구문 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,7 +6,7 @@ public class Application {
         // TODO: 프로그램 구현
     }
 
-    private boolean isNumber(String string) {
+    private static boolean isNumber(String string) {
         try {
             Integer.parseInt(string);
             return true;
