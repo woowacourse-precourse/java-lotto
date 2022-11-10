7b578a609164014cb8b5f6e3942cb45cbc03c5e0
Limjongho <whdgh9873@icloud.com>
2022-11-10 18:56:21 +0900

    test: 중복되지 않는 6개의 숫자 뽑기 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import domain.NumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,4 +27,17 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @DisplayName("중복되지 않는 숫자 6개를 추출하는 기능 테스트")
+    @Test
+    void createUniqueNumbers() {
+        NumberGenerator numberGenerator = new NumberGenerator();
+        for (int i = 0; i < 100; i++) {
+            List<Integer> randomNumbers = numberGenerator.createUniqueRandomNumbers();
+            List<Integer> uniqueRandomNumbers = randomNumbers.stream()
+                    .distinct()
+                    .collect(Collectors.toList());
+            assertThat(uniqueRandomNumbers.size()).isEqualTo(6);
+        }
+    }
 }
