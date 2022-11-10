9b3dd55db2af1ad99d3f6a50b4b56dcd94030d75
sangil <gsl0505@naver.com>
2022-11-10 22:33:37 +0900

    feat(Buyer): 로또 번호를 오름차순으로 정렬해주는 기능 구현

--- src/main/java/lotto/domain/Buyer.java
+++ src/main/java/lotto/domain/Buyer.java
@@ -2,24 +2,42 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Buyer {
 	private static final int ZERO_COUNT = 0;
 
-	private final Integer amount;
+	private final int amount;
 	private final List<Lotto> lottos;
 
 	public Buyer(String inputAmount) {
 		amount = Integer.parseInt(inputAmount);
 		lottos = new ArrayList<>();
-
+		buyLotto();
 	}
 
-	private void generateLottoNumber() {
-		List<Integer> numbers = Randoms.pickUniqueNumbersInRange(ConstValue.MIN_LOTTO_NUMBER,
+	private List<Integer> generateLottoNumber() {
+		return Randoms.pickUniqueNumbersInRange(ConstValue.MIN_LOTTO_NUMBER,
 			ConstValue.MAX_LOTTO_NUMBER, ConstValue.LOTTO_NUMBERS);
 	}
 
+	private List<Integer> sortNumbers(List<Integer> lottoNumbers) {
+		return lottoNumbers.stream()
+			.sorted()
+			.collect(Collectors.toList());
+	}
+
+	private void buyLotto() {
+		for (int i = ZERO_COUNT; i < lottoCount(); i++) {
+			List<Integer> lottoNumbers = generateLottoNumber();
+			lottos.add(new Lotto(sortNumbers(lottoNumbers)));
+		}
+
+	}
+
+	private int lottoCount() {
+		return amount / ConstValue.LOTTO_PRICE;
+	}
 }
