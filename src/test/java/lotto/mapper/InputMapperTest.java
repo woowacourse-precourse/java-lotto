0693fe84a9992aa29cbca19938348fd854f96693
SEONMl <sm000117@naver.com>
2022-11-10 14:05:16 +0900

    test: String으로 입력한 값을 Set으로 반환하는 mapper 클래스 테스트 코드 작성

---
+++ src/test/java/lotto/mapper/InputMapperTest.java
@@ -0,0 +1,34 @@
+package lotto.mapper;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+class InputMapperTest {
+
+    private InputMapper inputMapper = new InputMapper();
+
+    @Test
+    @DisplayName("split 메서드 기능 테스트")
+    void split_with_blank() {
+        Set<Integer> set = inputMapper.stringToSetSplitBy("1 , 2 ,   3  , 4, 5", ",");
+        assertThat(set).isEqualTo(Set.of(1, 2, 3, 4, 5));
+    }
+
+    @Test
+    @DisplayName("split 메서드 기능 테스트")
+    void split_without_blank() {
+        Set<Integer> set = inputMapper.stringToSetSplitBy("1,2,3,4,5", ",");
+        assertThat(set).isEqualTo(Set.of(1, 2, 3, 4, 5));
+    }
+
+    @Test
+    @DisplayName("split 메서드 기능 테스트")
+    void split_regex_error() {
+        assertThatThrownBy(() -> inputMapper.stringToSetSplitBy("1,2  ,3.  4.5", ","))
+                .isInstanceOf(NumberFormatException.class);
+    }
+}
