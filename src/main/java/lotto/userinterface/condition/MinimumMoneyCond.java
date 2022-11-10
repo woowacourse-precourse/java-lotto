f8ecd287ef434806325f1a662a0efb00bcda5868
wonju-dev <wonjulee.dev@gmail.com>
2022-11-10 21:32:52 +0900

    feat(MinimumMoneyCond): '최소 금액 검증' 로직 구현
    MinimumMoneyCond는 사용자가 최소 금액(1000원) 이상을 입력했는지
    검증하는 기능을 수행합니다.

---
+++ src/main/java/lotto/userinterface/condition/MinimumMoneyCond.java
@@ -0,0 +1,20 @@
+package lotto.userinterface.condition;
+
+public class MinimumMoneyCond implements Condition {
+
+    private static final Integer UNIT = 1000;
+    private static final Condition instance = new MinimumMoneyCond();
+
+    private MinimumMoneyCond() {
+    }
+
+    public static Condition getInstance() {
+        return instance;
+    }
+
+
+    @Override
+    public Boolean isSatisfied(String input) {
+        return Integer.parseInt(input) >= UNIT;
+    }
+}
