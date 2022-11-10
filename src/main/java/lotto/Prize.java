852694d9876cff38e72457fbc0f71aa2b51edce0
EUNJEONGMUN <munej26@gmail.com>
2022-11-10 23:28:13 +0900

    feat: 당첨 기준 저장 enum Prize 추가

---
+++ src/main/java/lotto/Prize.java
@@ -0,0 +1,44 @@
+package lotto;
+
+import java.util.List;
+
+public enum Prize {
+    THREE_MATCH(3, 0, 5000, "3개 일치 (5,000원)"),
+    FOUR_MATCH(4, 0, 50000, "4개 일치 (50,000원)"),
+    FIVE_MATCH(5, 0, 1500000, "5개 일치 (1,500,000원)"),
+    FIVE_BONUS_MATCH(5, 1, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    SIX_MATCH(6, 0, 2000000000, "6개 일치 (2,000,000,000원)");
+
+    private final int matchCount;
+    private final int bonusCount;
+    private final int prizeMoney;
+    private final String message;
+
+    Prize(int matchCount, int bonusCount, int prizeMoney, String message) {
+        this.matchCount = matchCount;
+        this.bonusCount = bonusCount;
+        this.prizeMoney = prizeMoney;
+        this.message = message;
+    }
+
+    public static List<Prize> getAllPrize() {
+        return List.of(Prize.values());
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getBonusCount() {
+        return bonusCount;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
