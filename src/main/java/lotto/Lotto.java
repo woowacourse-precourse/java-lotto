cc772e8c7f83ee25cdadd7abf173cb29e6bf669e
chosunghyun18 <krismon@naver.com>
2022-11-10 17:27:46 +0900

    feat(lottonumber): add the validate duplicate the number check

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,33 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> items = new HashSet<>();
+        numbers.stream()
+                .filter(n -> !items.add(n))
+                .collect(Collectors.toSet());
+        if (items.size() != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
