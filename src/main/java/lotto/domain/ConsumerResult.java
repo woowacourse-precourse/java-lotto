e2a6d494512f673489f7a8517757b4ec5b59db9d
eunji53 <duqtj00@naver.com>
2022-11-10 15:01:25 +0900

    <feat>:로또 번호 출력
    금액 입력 후 로또 번호 출력

--- src/main/java/lotto/domain/ConsumerResult.java
+++ src/main/java/lotto/domain/ConsumerResult.java
@@ -3,6 +3,13 @@ package lotto.domain;
 import java.util.List;
 
 public class ConsumerResult {
+    public void myLotto(){
+        PurchaseLotto purchaseLotto = new PurchaseLotto();
+        int money = purchaseLotto.inputMoney();
+        for (int count=0; count < purchaseLotto.countLotto(money); count++){
+            System.out.println(purchaseLotto.myLottoNumber());
+        }
+    }
     public float profit(){
         return 0;
     }
