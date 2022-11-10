ddc166514a1422a45388d7136835eb19c4d595c2
swonny <wonny921@gmail.com>
2022-11-11 00:50:03 +0900

    feat(Lotto): 로또 번호 오름차순 정렬 검사

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -13,6 +13,7 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         checkLength(numbers);
         checkDuplicate(numbers);
+        checkOrder(numbers);
     }
 
     // TODO: 추가 기능 구현
@@ -27,6 +28,16 @@ public class Lotto {
     private void checkLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkOrder(List<Integer> numbers) {
+        int previousNumber = 0;
+        for (Integer number : numbers) {
+            if (previousNumber > number) {
+                throw new IllegalArgumentException();
+            }
+            previousNumber = number;
         }
     }
 }
