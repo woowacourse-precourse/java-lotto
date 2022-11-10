dcb789d5f0ba6b7eaf5f92c23eb5b21e826463f5
Kwak-JunYoung <klop9090@g.skku.edu>
2022-11-10 20:31:44 +0900

    feat(lotto/Lotto.java) Added exception handling of numbers list
    Consideration of exception occurred by having the following condition made:
    - when there are duplicate numbers in one list
    - when at least one of the numbers is not between that from 1 to 45.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -14,6 +15,14 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+        for (int i = 1; i <= 45; i++)
+            if(Collections.frequency(numbers, i) >= 2)
+                throw new IllegalArgumentException();
+
+        for (int i = 0; i < 6; i++)
+            if(numbers.get(i) < 1 || numbers.get(i) > 45)
+                throw new IllegalArgumentException();
     }
 
     // TODO: 추가 기능 구현
