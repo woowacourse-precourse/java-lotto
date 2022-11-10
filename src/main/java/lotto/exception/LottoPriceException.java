3ca42629575beaa0df8fd1478cb6fa8e85c43ce4
YuHayeon <youha0930@gmail.com>
2022-11-10 21:56:29 +0900

    feat(LottoPriceException): 로또 금액을 숫자로 변환
    로또 구입 금액이 숫자가 아닌 다른 문자일 경우 예외처리를 해준 후 숫자를 입력해달라는 메시지를 출력

--- src/main/java/lotto/exception/LottoPriceException.java
+++ src/main/java/lotto/exception/LottoPriceException.java
@@ -5,6 +5,7 @@ public class LottoPriceException {
     public static void checkLottoPrice(String priceInput) {
         checkEmptyLottoPrice(priceInput);
         checkHasBlankInLottoPrice(priceInput);
+        Integer price = changePriceToInteger(priceInput);
     }
 
     public static void checkEmptyLottoPrice(String price) {
@@ -17,5 +18,12 @@ public class LottoPriceException {
         if (!price.equals(price.trim())) {
             throw new IllegalArgumentException("[ERROR] 로또 금액을 공백없이 입력해주세요.");
         }
+    }
+
+    public static Integer changePriceToInteger(String price) {
+        if (!price.matches("[0-9]+")) {
+            throw new IllegalArgumentException("[ERROR] 로또 금액을 숫자로 입력해주세요.");
+        }
+        return Integer.parseInt(price);
     }
 }
