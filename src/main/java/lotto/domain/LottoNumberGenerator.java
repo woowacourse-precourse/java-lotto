05b4cabec244674618ec2cbdbe83a03f166c4f23
jeujeon <1132jjw@naver.com>
2022-11-10 17:41:40 +0900

    feat(LottoNumberGenerator): 중복되지 않는 범위 내의 수 6개 생성하는 기능 추가

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -1,11 +1,16 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoNumberGenerator {
 
 	public static List<Integer> generate() {
-		return null;
+		return Randoms.pickUniqueNumbersInRange(1, 45, 6);
 	}
 
 }
