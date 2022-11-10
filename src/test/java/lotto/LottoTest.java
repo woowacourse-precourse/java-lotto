e548ca6136478353fef48ca68071168175971c21
juhwang <juhong4930@naver.com>
2022-11-10 23:20:06 +0900

    test(LottoTest): find out duplication between win numbers and lotto numbers

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,9 +2,11 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import store.Lotto;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,4 +26,11 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호와 당첨 번호를 비교하여 중복된 개수를 출력한다.")
+    @Test
+    void compareLottoAndWinNumbers() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int result = 5;
+        assertThat(lotto.compareWinNumbers(List.of(2,3,4,5,6,7))).isEqualTo(result);
+    }
 }
