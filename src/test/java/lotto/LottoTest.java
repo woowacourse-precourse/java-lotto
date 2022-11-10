743c466d31c3ab189b8fc58c42fbd6272214f733
wonju-dev <wonjulee.dev@gmail.com>
2022-11-10 20:03:53 +0900

    test(numberOf): 테스트 코드 작성
    올바른 입력이 들어온 경우에는 정상적으로 Lotto 객체를 생성한다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,6 +22,17 @@ class LottoTest {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> Lotto.numberOf(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("올바른 입력이 들어오면, 정상적으로 객체를 생성한다.")
+    @Test
+    void 로또_생성_성공() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        // when
+        Lotto lotto = Lotto.numberOf(numbers);
+        // then
+        assertThat(lotto).isInstanceOf(Lotto.class);
     }
 
     // 아래에 추가 테스트 작성 가능
