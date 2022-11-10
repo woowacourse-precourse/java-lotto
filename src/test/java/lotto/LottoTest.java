f22d6a4bf2352bef995fca85a56334973dce997a
kmularise <kmula222@gmail.com>
2022-11-10 14:54:31 +0900

    test(lotto): add lotto test checking vaild range of numbers

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -18,10 +18,15 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호가 1과 45사이에 없으면 예외가 발생한다.")
+    @Test
+    void createLottoByInValidNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 47, 13, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
     // 아래에 추가 테스트 작성 가능
 }
