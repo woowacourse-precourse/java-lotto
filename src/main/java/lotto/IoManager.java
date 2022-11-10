7995f19028ccacf138e34eca2da023f108877588
hodomaroo <enghemdwkd@gmail.com>
2022-11-10 19:38:26 +0900

    [Feat] : IoManager 클래스 숫자 목록 입력 기능 구현 완료
    IoManager 클래스에서 사용자로부터 숫자 목록을 입력받아, 리스트 형태로
    반환하는 getNumbersFromUser 메서드의 작성을 완료하였습니다.
    
    테스트 코드를 통한 검증을 완료 후, 커밋하였습니다.

--- src/main/java/lotto/IoManager.java
+++ src/main/java/lotto/IoManager.java
@@ -1,8 +1,42 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 /**
  * Input / Output 기능을 담당하는 클래스
  */
 public class IoManager {
 
+    public static List<Integer> getNumbersFromUser(String parser) {
+        try {
+
+            String line = Console.readLine();
+            String pattern = "[0-9]+(" + parser + "[0-9]+)*";
+
+            if (!validateFitInPattern(line, pattern)) {
+                throw new Exception();
+            }
+
+            return Arrays.stream(line.split(parser)).map((string) -> Integer.parseInt(string))
+                    .collect(Collectors.toList());
+
+        } catch (Exception e) {
+            System.out.println("[ERROR] 형식에 알맞지 않은 입력입니다. 입력은 숫자[,숫자] 형태로 주어져야 합니다.");
+            throw new IllegalArgumentException("입력은 숫자[,숫자]형태여야 합니다.");
+        }
+    }
+
+    public static Integer getNumberFromUser() {
+        return null;
+    }
+
+    private static boolean validateFitInPattern(String string, String patternString) {
+        return string.matches(patternString);
+    }
 }
