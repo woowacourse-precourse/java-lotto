0a4dfe762710293a47e80a7054c3dba107d2173f
Jin409 <sualng123@gmail.com>
2022-11-10 19:04:39 +0800

    feat(LottoConstant): 로또 관련 전역 상수 정의

---
+++ src/main/java/lotto/constants/LottoConstant.java
@@ -0,0 +1,9 @@
+package lotto.constants;
+
+public class LottoConstant {
+    public static final int VALID_MIN_RANGE = 1;
+    public static final int VALID_MAX_RANGE = 45;
+    public static final int VALID_LOTTERY_NUMBER_LENGTH = 6;
+    public static final int VALID_BONUS_NUMBER_LENGTH = 1;
+    public static final String LOTTERY_NUMBER_SEPARATOR = ",";
+}
