d65cc807647d9a7cf4d83d647072b2f99bb490c6
2jun0 <soo28819@gmail.com>
2022-11-11 04:06:07 +0900

    Feat(LottoGame): 게임 로직을 수행하는 LottoGame 구현

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,66 @@
+package lotto;
+
+import lotto.console.Input;
+import lotto.console.Output;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoSeller;
+import lotto.domain.match.MatchAndReward;
+import lotto.domain.match.Results;
+import lotto.domain.match.WinningNumbers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGame {
+
+    private final Input input;
+    private final Output output;
+
+    public LottoGame(Input input, Output output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public void play() {
+        output.writeEnterMoney();
+        int money = input.readInt();
+        output.writeEmptyLine();
+
+        LottoSeller seller = new LottoSeller();
+        List<Lotto> lottos = seller.buyLottos(money);
+
+        output.writeBuyCount(lottos.size());
+        for (Lotto lotto : lottos) {
+            output.writeLine(lotto.toString());
+        }
+        output.writeEmptyLine();
+
+        output.writeEnterWinningNumbers();
+        List<Integer> numbers = input.readIntList();
+        output.writeEmptyLine();
+
+        output.writeEnterBonusNumber();
+        int bonusNumber = input.readInt();
+        output.writeEmptyLine();
+
+        WinningNumbers winningNumbers = new WinningNumbers(numbers, bonusNumber);
+        List<MatchAndReward> matchAndRewards = new ArrayList<>();
+
+        for (Lotto lotto : lottos) {
+            MatchAndReward matchAndReward = winningNumbers.match(lotto);
+            if (matchAndReward != null)
+                matchAndRewards.add(matchAndReward);
+        }
+
+        Results results = new Results(matchAndRewards);
+
+        output.writePrefixMatchStatistics();
+        for (MatchAndReward matchAndReward : MatchAndReward.values()) {
+            output.writeMatchStatistic(
+                    matchAndReward.getMatch(),
+                    matchAndReward.getReward(),
+                    results.getCount(matchAndReward));
+        }
+        output.writeYield(results.getYield(money));
+    }
+}
