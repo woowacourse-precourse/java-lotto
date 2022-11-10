012a638a0df60d945bc62ef22b6d7932504cd331
YunByungil <dbsquddlfz@naver.com>
2022-11-10 19:08:33 +0900

    feat(lotto): 로또와 당첨번호 비교할 수 있는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,30 +3,43 @@ package lotto;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        List<Integer> test1 = List.of(1,2,3,4,5,6);
-        List<Integer> test2 = new ArrayList<>();
-
-
         int count = Integer.parseInt(Console.readLine());
+
+
         System.out.println((count/1000)+"개를 구매했습니다.");
         LottoNumbers lottoNumbers = new LottoNumbers(count);
-
-        System.out.println("당첨 번호를 입력해주세요.");
-        String num = Console.readLine();
-//        Lotto lotto = new Lotto(num);
-        System.out.println("보너스 번호를 입력해주세요.");
-        String num2 = Console.readLine();
 
         List<List<Integer>> test = lottoNumbers.getNumbers();
         for (List<Integer> integers : test) {
             Collections.sort(integers);
             System.out.println("integers = " + integers);
         }
+
+
+        System.out.println("당첨 번호를 입력해주세요.");
+        String[] str = new String[6];
+        str = Console.readLine().split(",");
+        List<Integer> basket = new ArrayList<>();
+        for (int i = 0; i < str.length; i++) {
+            basket.add(Integer.parseInt(str[i]));
+        }
+        Lotto lotto = new Lotto(basket);
+        System.out.println("보너스 번호를 입력해주세요.");
+        int num2 = Integer.parseInt(Console.readLine());
+
+
+        Manager manager = new Manager();
+        for (List<Integer> integers : test) {
+            Collections.sort(integers);
+//            System.out.println("integers = " + integers);
+        }
+        manager.inputWinNumbers(test, basket);
     }
 }
--- src/main/java/lotto/Manager.java
+++ src/main/java/lotto/Manager.java
@@ -1,9 +1,29 @@
 package lotto;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Manager {
 
-    public void inputWinNumbers() {
+    public void inputWinNumbers(List<List<Integer>> numbers, List<Integer> winNumber) {
+        int count = 0;
+        Map<String, Integer> winner = new HashMap<>();
+        for (int i = 0; i < numbers.size(); i++) {
+            for (int j = 0; j < numbers.get(i).size(); j++) {
+                if (numbers.get(i).contains(winNumber.get(j))) {
+                    System.out.println("numbers.get(i) = " + numbers.get(i));
+                    winner.put(numbers.get(i).toString(),winner.getOrDefault(numbers.get(i).toString(), count) + 1);
+                }
+            }
+        }
+        System.out.println("count = " + count);
 
+        for (String s : winner.keySet()) {
+            System.out.print("s = " + s + " ");
+            System.out.print("/ winner = " + winner.get(s) +"개 일치");
+            System.out.println();
+        }
     }
 
     public void inputBonusNumber() {
