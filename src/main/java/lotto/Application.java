6f11a66fc6299fd9fb9e5b6458c3807765737e16
KimJaeSung <behappy0916@naver.com>
2022-11-10 13:27:19 +0900

    feat: 당첨 번호와 임의의 수 6개를 비교할 수 있는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,14 +2,18 @@ package lotto;
 
 import java.util.ArrayList;
 import java.util.List;
+import lotto.domain.Judgement;
 import lotto.domain.NumberGenerator;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         NumberGenerator numberGenerator = new NumberGenerator();
-        List<Integer> numbers = new ArrayList<>();
-        numbers = numberGenerator.createRandomNumbers();
-        System.out.println(numbers);
+        Judgement judgement = new Judgement();
+//        List<Integer> numbers = new ArrayList<>();
+//        numbers = numberGenerator.createRandomNumbers();
+//        System.out.println(numbers);
+        int count = judgement.countCorrectNumbers(List.of(1,2,3,4,5,6),List.of(1,2,3,4,5,6));
+        System.out.println(count);
     }
 }
