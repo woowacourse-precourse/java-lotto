a4a07b786e48ec2aaf44f2f378b969cbc1d0f2ae
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-10 21:56:08 +0900

    Fix(CheckInputException) : 사용자가 양수 입력시 예외 발생 에러 수정 #4
    checkBuyerInputIsMinus()메소드가 양수를 받으면 예외 처리를 해버리는 이슈가 발견되어 음수를 받으면 예외 처리를 하도록 수정하였습니다.
    
    Resolves : #2

--- src/main/java/lotto/CheckInputException.java
+++ src/main/java/lotto/CheckInputException.java
@@ -6,7 +6,7 @@ class CheckInputException {
 
         Long money = Long.valueOf(inputMoney);
 
-        if (money > 0) {
+        if (money < 0) {
             throw new IllegalArgumentException("[ERROR] 입력하신 돈이 음수입니다.");
         }
     }
