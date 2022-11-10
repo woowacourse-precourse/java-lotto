4e5385b3543e519b1cfbe1c1d4a002d256e20248
areyouhun <areyouhun@gmail.com>
2022-11-10 21:12:55 +0900

    feat(generator): pick unique numbers in range 메서드로 로또 번호 생성

---
+++ src/main/java/lotto/Generator.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
+import java.util.List;
+
+public class Generator {
+    private static final int START_NUMBER_INCLUSIVE = 1;
+    private static final int END_NUMBER_INCLUSIVE = 45;
+    private static final int COUNT = 6;
+
+    public static List<Integer> LottoNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_NUMBER_INCLUSIVE, END_NUMBER_INCLUSIVE, COUNT);
+        Collections.sort(numbers);
+        return numbers;
+    }
+}
