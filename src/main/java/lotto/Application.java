a93ce07ca1670bc79f232ee6d0f7fbe40551ba9d
lee2963 <sodel2963@naver.com>
2022-11-10 18:59:04 +0900

    Feat: 로또 구입 비용 입력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -9,6 +10,21 @@ public class Application {
     public static void main(String[] args) {
 
 
+    }
+
+    public static int inputBuyLottoMoney() {
+        System.out.println("구입금액을 입력해 주세요");
+        String inputBuyMoney = Console.readLine();
+
+        if(!inputBuyMoney.matches("^[0-9]*$"))
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+
+        int buyMoney = Integer.parseInt(inputBuyMoney);
+
+        if (buyMoney % 1000 != 0 || buyMoney == 0)
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위입니다.");
+
+        return buyMoney;
     }
 
     public static List<Integer> makeLottoNum() {
