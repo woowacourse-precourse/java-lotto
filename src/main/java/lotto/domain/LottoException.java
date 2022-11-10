c3f46e0cfc5279503583f2100995e789f0035d20
greeng00se <thegreengoose7@gmail.com>
2022-11-10 12:09:07 +0900

    remove(LottoException): 사용하지 않는 CustomException 제거

--- src/main/java/lotto/domain/LottoException.java
+++
@@ -1,9 +0,0 @@
-package lotto.domain;
-
-public class LottoException extends RuntimeException {
-    private static final String MESSAGE = "[ERROR] 로또 오류 발생";
-
-    public LottoException() {
-        super(MESSAGE);
-    }
-}
