3f917eabdaf5fc496b523aa9f9253d9562466f03
kokodak <kokodakadokok@gmail.com>
2022-11-10 16:31:20 +0900

    feat(lottoGenerator): 로또를 생성하는 기능 구현

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class LottoGenerator {
+
+    private static final int LOTTO_NUMBER_LOWER_LIMIT = 1;
+    private static final int LOTTO_NUMBER_UPPER_LIMIT = 45;
+    private static final int LOTTO_NUMBER_QUANTITY = 6;
+
+    private List<Lotto> lottos = new ArrayList<>();
+    private int lottoQuantity;
+
+    public LottoGenerator(int money) {
+        lottoQuantity = money / 1000;
+    }
+
+    private Lotto generateLotto() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_LOWER_LIMIT, LOTTO_NUMBER_UPPER_LIMIT, LOTTO_NUMBER_QUANTITY);
+        randomNumbers.sort(Comparator.naturalOrder());
+        return new Lotto(randomNumbers);
+    }
+}
