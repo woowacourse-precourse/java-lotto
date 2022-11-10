6a318782d54578b7b1c14d2fe45f9e21b47d89dd
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-11-10 16:49:14 +0900

    [ADD] 로또 - 상수 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,19 @@
 package lotto;
 
 public class Application {
+    private final static String MESSAGE_BUDGET = "구입금액을 입력해 주세요.";
+    private final static String MESSAGE_BOUGHT = "개를 구매했습니다.";
+    private final static String MESSAGE_JACKPOT = "당첨 번호를 입력해 주세요.";
+    private final static String MESSAGE_BONUS = "보너스 번호를 입력해 주세요.";
+    private final static String MESSAGE_RESULT = "당첨 통계%n---";
+
+    private final static String RESULT_FIRST = "개 일치 (2,000,000,000원) - %f개";
+    private final static String RESULT_SECOND = "개 일치 (30,000,000원) - %f개";
+    private final static String RESULT_THIRD = "개 일치 (1,500,000원) - %f개";
+    private final static String RESULT_FOURTH = "개 일치 (50,000원) - %f개";
+    private final static String RESULT_FIFTH = "개 일치 (5,000원) - %f개";
+    private final static String RESULT_EARNING_RATE = "총 수익률은 %.1f%입니다.";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
