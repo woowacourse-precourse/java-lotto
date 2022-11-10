763de2a820986456fedb42aef50b175efb0f5257
CMHyuk <alsgur9710@naver.com>
2022-11-10 23:02:15 +0900

    feat(LottoCount): 로또 구입 금액 예외 처리

--- src/main/java/lotto/domain/LottoCount.java
+++ src/main/java/lotto/domain/LottoCount.java
@@ -3,6 +3,7 @@ package lotto.domain;
 public class LottoCount {
 
     public int getLottoCount(int money) {
+        isCorrectMoney(money);
         return getMoney(money);
     }
 
@@ -11,5 +12,11 @@ public class LottoCount {
             money /= 1000;
         }
         return money;
+    }
+
+    private void isCorrectMoney(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("1000원 단위로 입력해주세요.");
+        }
     }
 }
