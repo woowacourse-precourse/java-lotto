676ea024ab30b2bf8ad64b91ff7a650fd0788d7c
imsongj <imsongj@gmail.com>
2022-11-10 21:56:44 +0900

    feat(lotto) : 구매한 로또 수량 및 번호를 출력 기능 구현

--- src/main/java/lotto/constant/GameMessage.java
+++ src/main/java/lotto/constant/GameMessage.java
@@ -1,8 +1,11 @@
 package lotto.constant;
 
 public enum GameMessage {
-    ASK_FOR_INITIAL_MONEY("구입금액을 입력해 주세요.");
+    ASK_FOR_INITIAL_MONEY("구입금액을 입력해 주세요."),
+    LOTTO_PURCHASE("%d개를 구매했습니다.");
+
     private final String message;
+
     GameMessage(String message) {
         this.message = message;
     }
