445d94d9494feeedcc1cc50af53d56f6dcbc5fcb
ksh-beginner <ohk9134@naver.com>
2022-11-11 00:59:43 +0900

    feat(LottoGameService) : 입력받은 로또 당첨 번호를 리스트로 변환하는 메소드 추가

--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -6,6 +6,8 @@ import lotto.model.Lotto;
 
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottoGameService {
 
@@ -16,7 +18,7 @@ public class LottoGameService {
     private static final String DIGIT_REGEX = "^[0-9]+$";
 
     private Lotto lotto;
-
+           
     public void generateLotto() {
         this.lotto = new Lotto(LottoNumbersGenerator.generateLottoNumbers());
     }
@@ -51,6 +53,11 @@ public class LottoGameService {
 
     public boolean isPurchaseNumbersMatchBonusNumber(List<Integer> purchaseLottoNumbers, int bonusNumber) {
         return purchaseLottoNumbers.contains(bonusNumber);
+    }
+
+    public List<String> inputWinningNumberConvertToCollection(String lottoWinningNumber) {
+        return Stream.of(lottoWinningNumber.split(","))
+                .collect(Collectors.toList());
     }
 
     public LottoWinningRank decideWinningRank(List<Integer> purchaseLottoNumbers,
