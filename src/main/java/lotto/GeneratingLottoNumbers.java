7105270578cbcdcaebf5c73785c106a7babd859f
WhalesBob <ndw8200@naver.com>
2022-11-10 16:21:48 +0900

    feat(GeneratingLottoNumbers) : add methods of generating Lotto Papers

---
+++ src/main/java/lotto/GeneratingLottoNumbers.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class GeneratingLottoNumbers {
+
+    public Lotto[] generatingAllLottoPapers(int amountOfLotto){
+        Lotto[] lottoPapers = new Lotto[amountOfLotto];
+        for(int i = 0; i < amountOfLotto; i++){
+            lottoPapers[i] = generateOneLottoPaper();
+        }
+        return lottoPapers;
+    }
+
+    public Lotto generateOneLottoPaper(){
+        List<Integer> generateRandomNumbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+        return new Lotto(generateRandomNumbers);
+    }
+}
