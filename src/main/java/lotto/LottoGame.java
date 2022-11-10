1a5755e2420f475729c93207f2e6a4227e878bff
wugawuga <dnr3655@naver.com>
2022-11-10 23:53:37 +0900

    feat: 로또 오름차순 정렬

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoGame {
@@ -13,6 +14,7 @@ public class LottoGame {
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < lottoTickets; i++) {
             List<Integer> random = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(random);
             lottos.add(new Lotto(random));
         }
     }
