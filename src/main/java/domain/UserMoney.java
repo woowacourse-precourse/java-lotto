158157f5646462fe8aead7b475b09bb295dfe5b2
bsh1603 <bsh1603@github.com>
2022-11-10 18:02:11 +0900

    Feat: user 입력 돈 예외 처리

--- src/main/java/domain/UserMoney.java
+++ src/main/java/domain/UserMoney.java
@@ -1,8 +1,11 @@
 package domain;
 
+import exception.MoneyExceptionType;
+
 public class UserMoney {
 
     private final int THOUSAND = 1000;
+    private String input;
     private int money;
 
     public int getMoney() {
@@ -11,8 +14,22 @@ public class UserMoney {
 
     public void isDivideThousand(int money){
         if(money % THOUSAND != 0){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(MoneyExceptionType.NOT_THOUSAND_MONEY.getErrorMessage());
         }
     }
 
+    public void notNumber(String input){
+        for (int i = 0; i < input.length(); i++) {
+            int num = input.length();
+            if(num < 48 || num > 57){
+                throw new IllegalArgumentException(MoneyExceptionType.NO_NUMBER_MONEY.getErrorMessage());
+            }
+        }
+    }
+
+    public void negativeNumber(int money){
+        if(money < 0){
+            throw new IllegalArgumentException(MoneyExceptionType.NOT_MONEY_NEGATIVE.getErrorMessage());
+        }
+    }
 }
