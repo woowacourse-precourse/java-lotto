0dbc6e1343a8288d2256f4332b280e74cdda5943
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-11 00:01:52 +0900

    feat : 로또 당첨 번호와 보너스 번호가 중복될 시 예외발생

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,2 +1,35 @@
-package lotto.controller;public class LottoGame {
+package lotto.controller;
+
+import static lotto.domain.LottoBuyer.purchaseLotto;
+import static lotto.domain.WinningLotto.bonusNumber;
+import static lotto.domain.WinningLotto.bonusNumberInput;
+import static lotto.domain.WinningLotto.winningNumber;
+import static lotto.domain.WinningLotto.winningNumberInput;
+
+import lotto.domain.Lotto;
+import lotto.view.ErrorMessage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LottoGame {
+
+    public void start() {
+        List<Lotto> lottoList;
+        lottoList = purchaseLotto();
+        System.out.println(lottoList);
+        winningNumberInput();
+        bonusNumberInput();
+        System.out.println(Arrays.toString(winningNumber.split(",")));
+        validateDuplicate(winningNumber, bonusNumber);
+    }
+
+    private static void validateDuplicate(String winningNumber, String bonusNumber) {
+        String[] dupliateCheck = winningNumber.split(",");
+        for (String str : dupliateCheck) {
+            if (str.equals(bonusNumber)) {
+                throw new IllegalArgumentException(ErrorMessage.ERROR_DUPLICATE.getErrorMessage());
+            }
+        }
+    }
 }
