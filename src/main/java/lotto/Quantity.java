543a701c41e518100f4db2046687da7f28eb2abe
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-10 17:36:41 +0900

    docs(README): 기능 목록 구현 우선 순위 변경

--- src/main/java/lotto/Quantity.java
+++ src/main/java/lotto/Quantity.java
@@ -2,6 +2,7 @@ package lotto;
 
 public class Quantity {
 
+    // TODO: 2022-11-10 Quantity가 존재해야하는지?, != 0을 어떻게 정의할지 고민하기
     private static final int UNIT_STANDARD = 1_000;
     private static final String UNIT_STANDARD_ERROR = "[ERROR] 구입금액은 1,000원 단위입니다.";
 
