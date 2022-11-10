ec40a82e655a85cf8a9d8135c1703fc19f24852f
wonju-dev <wonjulee.dev@gmail.com>
2022-11-10 21:50:45 +0900

    refactor(UserInterface): getMoney 메소드 리펙토링
    기존에는 검증할 조건들을 일일이 조건문으로 검증했는데,
    validator를 사용하여, 모든 조건을 반복문을 통해 검증하도록 개선했다.

--- src/main/java/lotto/userinterface/UserInterface.java
+++ src/main/java/lotto/userinterface/UserInterface.java
@@ -1,10 +1,16 @@
 package lotto.userinterface;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.userinterface.condition.Condition;
+import lotto.userinterface.condition.MinimumMoneyCond;
+import lotto.userinterface.condition.MoneyDivisionCond;
+import lotto.userinterface.validator.Validator;
+
+import java.util.List;
 
 public class UserInterface {
 
-    private static final Integer UNIT = 1000;
+    private static final Validator validator = Validator.getInstance();
 
     private UserInterface() {
     }
@@ -12,15 +18,19 @@ public class UserInterface {
     public static Integer getMoney() {
         String input = getInput();
 
-        if (isNotDividedWithUnit(input)) {
+        List<Condition> conditions = List.of(
+                MinimumMoneyCond.getInstance(),
+                MoneyDivisionCond.getInstance()
+        );
+
+        Integer notPassConditionIndex = validator.getNotPassConditionIndex(conditions, input);
+
+        if (notPassConditionIndex != -1) {
+            Condition notPassCondition = conditions.get(notPassConditionIndex);
             throw new IllegalArgumentException();
         }
 
         return Integer.parseInt(input);
-    }
-
-    private static boolean isNotDividedWithUnit(String input) {
-        return Integer.parseInt(input) % UNIT != 0;
     }
 
     private static String getInput() {
