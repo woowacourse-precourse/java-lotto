bba727906b330cb2c28aa9aa37b878db3c461371
gyuturn <rbtjrrbals@gmail.com>
2022-11-11 00:03:05 +0900

    feat($util): error message
    EXC-B-1, EXC-B-2
    예외 메세지 목록
    - 1~45사이 숫자 목록이 아니경우
    - 중복된 번호가 있는경우

--- src/main/java/util/message/ErrorMessage.java
+++ src/main/java/util/message/ErrorMessage.java
@@ -6,4 +6,7 @@ public class ErrorMessage {
     public static final String OVER_THOUSAND_MONEY = "로또 구매금액은 1000원이상이어야합니다.";
     public static final String UNIT_THOUSAND_MONEY = "로또 구입금액은 1000원 단위의 금액만 입력가능합니다.";
     public static final String NOT_STRING_MONEY = "로또 구입금액은 숫자만 가능합니다.(ex - 1000)";
+
+    public static final String HAS_NOT_DUPLICATE_NUM = "로또는 중복된 숫자를 가질 수 없습니다.";
+    public static final String IS_NOT_LOTTO_RANGE_NUM = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 }
