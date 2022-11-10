f96aa9818493fd7ab58f328cb67e6e72308fb5a3
woo-chang <clllickme@naver.com>
2022-11-10 22:11:26 +0900

    feat(InputService): 보너스 번호를 입력하는 기능 구현

--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -6,6 +6,7 @@ public class InputService {
 
     public static String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     public static String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public String readPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
@@ -14,6 +15,11 @@ public class InputService {
 
     public String readWinningNumber() {
         System.out.println(WINNING_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String readBonusNumber() {
+        System.out.println(BONUS_NUMBER_MESSAGE);
         return Console.readLine();
     }
 
