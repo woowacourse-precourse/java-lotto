734b1e820506178c60d76042936d5c4d76a7824c
Joonsoo Kim <kjsoo1901@gmail.com>
2022-11-10 18:16:35 +0900

    feat: Add hasSixItems in Validation.java

--- src/main/java/lotto/app/Validation.java
+++ src/main/java/lotto/app/Validation.java
@@ -1,4 +1,11 @@
 package lotto.app;
 
+import java.util.List;
+
 public class Validation {
+    public boolean hasSixItems(List<Integer> numbers){
+        if(numbers.size() != 6)
+            return false;
+        return true;
+    }
 }
