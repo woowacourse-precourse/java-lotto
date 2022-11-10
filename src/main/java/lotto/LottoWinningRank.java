0a45cffd0564ba467a03cb02861afb6fe1c531a1
ksh-beginner <ohk9134@naver.com>
2022-11-11 00:00:04 +0900

    refactor(LottoWinningRank) : 보너스 번호 일치/불일치 필드 삭제 리팩토링
    보너스 번호 일치하는지 불일치하는지 판단하는 필드
    불필요해서 삭제하는 리팩토링 진행

--- src/main/java/lotto/LottoWinningRank.java
+++ src/main/java/lotto/LottoWinningRank.java
@@ -4,23 +4,20 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum LottoWinningRank {
-    NO_RANK(0, List.of(0L, 1L, 2L), false, "0"),
-    FIRST(1, List.of(6L), false, "2,000,000,000"),
-    SECOND(2, List.of(5L), true, "30,000,000"),
-    THIRD(3, List.of(5L), false, "1,500,000"),
-    FOURTH(4, List.of(4L), false, "50,000"),
-    FIFTH(5, List.of(3L), false, "5,000");
+    NO_RANK(0, List.of(0L, 1L, 2L), "0"),
+    FIRST(1, List.of(6L), "2,000,000,000"),
+    SECOND(2, List.of(5L), "30,000,000"),
+    THIRD(3, List.of(5L), "1,500,000"),
+    FOURTH(4, List.of(4L), "50,000"),
+    FIFTH(5, List.of(3L),  "5,000");
 
     private final int rank;
     private final List<Long> purchaseNumbersMatchWinningNumbersCounts;
-    private final boolean purchaseNumbersMatchBonusNumber;
     private final String winningMoney;
 
-    LottoWinningRank(int rank, List<Long> purchaseNumbersMatchWinningNumbersCount,
-                     boolean purchaseNumbersMatchBonusNumber, String winningMoney) {
+    LottoWinningRank(int rank, List<Long> purchaseNumbersMatchWinningNumbersCount, String winningMoney) {
         this.rank = rank;
         this.purchaseNumbersMatchWinningNumbersCounts = purchaseNumbersMatchWinningNumbersCount;
-        this.purchaseNumbersMatchBonusNumber = purchaseNumbersMatchBonusNumber;
         this.winningMoney = winningMoney;
     }
 
@@ -32,9 +29,6 @@ public enum LottoWinningRank {
         return purchaseNumbersMatchWinningNumbersCounts;
     }
 
-    public boolean isPurchaseNumbersMatchBonusNumber() {
-        return purchaseNumbersMatchBonusNumber;
-    }
 
     public String getWinningMoney() {
         return winningMoney;
