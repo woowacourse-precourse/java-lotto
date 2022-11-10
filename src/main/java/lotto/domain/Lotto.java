01dcbdc60f5978f9388fa1b48101231f61f0ff0b
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-11-10 18:19:02 +0900

    feat: 로또 예외 검증 기능 구현

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validateNumberSize(numbers);
+        validateDuplicateNumber(numbers);
+        validateNumberRange(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validateNumberSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> notDuplicateNumbers = new HashSet<>(numbers);
+        if (notDuplicateNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호끼리는 중복 될 수 없습니다.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers){
+        int rightRangeNumberCount = (int)numbers.stream().filter(num -> 1 <= num && num <= 45).count();
+        if (rightRangeNumberCount != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 1부터 45까지 입니다.");
+        }
+    }
+
+}
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import lotto.domain.Lotto;
+
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
@@ -18,9 +20,15 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호 중 1~45사이의 값이 아닌 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByWrongRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(11,23,35,86,1,90)))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
     // 아래에 추가 테스트 작성 가능
