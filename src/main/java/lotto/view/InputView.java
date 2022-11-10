357b7d0f7ca6e2745f23ca4ef4c17dd97692edb7
SH <gram753@naver.com>
2022-11-10 12:57:08 +0900

    feat: 로또 번호를 입력받는 기능, 문자열을 정수 리스트로 변환하는 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,24 @@
+package lotto.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+	static final String NUMBER_DELIMITER = ",";
+
+	public static String[] getLottoNumbers(){
+		String[] pickNumbers = Console.readLine().split(NUMBER_DELIMITER);
+		return pickNumbers;
+	}
+
+	public static List<Integer> convertIntoIntegerList(String[] strings){
+		return Arrays.stream(strings).
+			map(string -> Integer.parseInt(string)).
+			collect(Collectors.toList());
+	}
+
+
+}
