182aad960122d9696dab1cd52ec39091f01432f0
saewoo1 <wooozooo1123@gmail.com>
2022-11-10 18:24:56 +0900

    feat(Lotto): 당첨 번호 검사 코드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -13,23 +13,18 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자만 입력해주세요.");
         }
-    }
-    // 외부에 난수가 generate 되는 메소드를 만들고 Lotto에 넣고 검사를 돌린다.
-    // 그 난수를 어딘가에 저장해야 당첨 번호랑 match 시켜볼텐데...
-    // 당첨 번호를 new Lotto(번호) 이런 식으로 검사 해보는것도 좋을듯
-    // TODO: 추가 기능 구현
-
-    public void luckyNumberIsNotDuplicate(String userNumber) {
-        Set<Character> set = new HashSet<>();
-        for (int i = 0; i < userNumber.length(); i++) {
-            set.add(userNumber.charAt(i));
-        }
-        if (userNumber.length() != set.size()) {
+        if (numbers.size() != set.size()) {
             throw new IllegalArgumentException("[ERROR] 중복된 숫자를 입력하였습니다.");
         }
+    }
+    // TODO: 추가 기능 구현
+
+    public List<Integer> properNumber() {
+        return this.numbers;
     }
 
 }
--- src/main/java/lotto/LuckyNumber.java
+++ src/main/java/lotto/LuckyNumber.java
@@ -1,8 +1,6 @@
 package lotto;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class LuckyNumber {
