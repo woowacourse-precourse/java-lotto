05204e1e2c6a5822868a0731962cd2345b1767bf
rt3310 <btac3310@gmail.com>
2022-11-10 19:04:18 +0900

    feat: 숫자가 아닌 값이 입력됐을 시 예외처리하는 기능 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -2,10 +2,20 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Pattern;
+
 public class LottoGame {
+    private static final String INVALID_MONEY_TYPE_REGEX = "\\D";
 
     public String inputMoney() {
         String money = Console.readLine();
+        validateMoneyType(money);
         return money;
+    }
+
+    private void validateMoneyType(String money) {
+        if (Pattern.compile(INVALID_MONEY_TYPE_REGEX).matcher(money).find()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_MONEY_TYPE.getMessage());
+        }
     }
 }
