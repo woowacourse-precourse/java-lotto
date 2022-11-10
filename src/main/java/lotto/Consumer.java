ac18a83b5b0f6f65b290ecd72e69b1b710025954
saturnone1 <saturnone1@naver.com>
2022-11-10 10:37:49 +0900

    feat(Consumer): purchaseAmount, 구입 금액 입가력 메소드 추가

--- src/main/java/lotto/Consumer.java
+++ src/main/java/lotto/Consumer.java
@@ -7,7 +7,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Consumer {
-    private List<Integer> winning() {
+    public int purchaseAmount() throws IllegalArgumentException{
+        System.out.println("구입 금액을 입력해 주세요.");
+        try {
+            int amount = Integer.parseInt(Console.readLine());
+            return amount;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력하지 않으셨습니다.");
+        }
+    }
+
+    public List<Integer> winningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
         List<Integer> lottonumbers = Arrays.asList(Console.readLine().split(","))
                 .stream()
                 .map((s -> Integer.parseInt(s.trim())))
