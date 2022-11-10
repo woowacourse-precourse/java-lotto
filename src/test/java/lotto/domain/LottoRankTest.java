8119e48b45fbfcc5c1181bdd6c54dff21d9346de
JunH <tjdtls690@gmail.com>
2022-11-10 17:59:47 +0900

    :white_check_mark: test(Domain : LottoRankTest) : 꽝 구하는 기능 테스트

--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -40,5 +41,12 @@ class LottoRankTest {
     @ValueSource(booleans = {true, false})
     void fifthRank(final boolean existBonusLottoNumber) {
         assertThat(LottoRank.parseRank(3, existBonusLottoNumber)).isEqualTo(LottoRank.FIFTH);
+    }
+    
+    @DisplayName("꽝인지 확인")
+    @ParameterizedTest(name = "{displayName} : 매칭 번호 개수 => {0}, 보너스 번호 매칭 여부 => {1}")
+    @CsvSource(value = {"2, true", "2, false", "1, true", "1, false", "0, true", "0, false"})
+    void fifthRank(final int countOfSameLottoNumber, final boolean existBonusLottoNumber) {
+        assertThat(LottoRank.parseRank(countOfSameLottoNumber, existBonusLottoNumber)).isEqualTo(LottoRank.MISS);
     }
 }
