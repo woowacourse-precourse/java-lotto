24e8c44371c5d7dcb83ec821999abfb0ae424586
JHS <wnghdtjr129@naver.com>
2022-11-10 22:35:23 +0900

    style(Application): ProjectView static import 적용

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,32 +4,32 @@ import lotto.domain.IssuedLotto;
 import lotto.domain.LottoWinningNumber;
 import lotto.domain.WinningResult;
 import lotto.domain.Seller;
-import lotto.view.ProjectView;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
+import static lotto.view.ProjectView.*;
 
 public class Application {
     public static void main(String[] args) {
-        String money = ProjectView.askPurchaseMoney();
+        String money = askPurchaseMoney();
 
         Seller seller = Seller.getInstance();
         try {
             IssuedLotto issuedLotto = seller.sellLotto(money);
-            ProjectView.sell(issuedLotto);
+            sell(issuedLotto);
 
-            String winningNumber = ProjectView.getWinningNumber();
+            String winningNumber = getWinningNumber();
             List<Integer> winningNumbers = convertStringToList(winningNumber);
-            String bonusNumber = ProjectView.getBonusNumber();
+            String bonusNumber = getBonusNumber();
 
             LottoWinningNumber lottoWinningNumber = new LottoWinningNumber(winningNumbers, bonusNumber);
 
             WinningResult result = lottoWinningNumber.getResult(issuedLotto);
-            ProjectView.printResult(result, Integer.parseInt(money));
+            printResult(result, Integer.parseInt(money));
         } catch (IllegalArgumentException e) {
-            ProjectView.printErrorMessage(e.getMessage());
+            printErrorMessage(e.getMessage());
         }
     }
 
