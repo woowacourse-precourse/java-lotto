89251edfe812cbee80e5bc51285aaefc556d87f0
java-saeng <gusdn85554@naver.com>
2022-11-10 16:13:43 +0900

    feat : 당첨 번호가 1~45 범위를 만족하는지 확인하는 기능 추가

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class InputValidator {
 
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+
     public static boolean restEqualZero(long dividend, long divisor) {
         return dividend % divisor == 0;
     }
@@ -14,5 +17,17 @@ public class InputValidator {
 
     public static boolean hasSixNumbers(List<Integer> numbers) {
         return numbers.size() == 6;
+    }
+
+    public static boolean satisfyLottoNumberRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if(!isRangeTrue(number)) return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isRangeTrue(int number) {
+        return number >= MIN_LOTTO_NUMBER && number <= MAX_LOTTO_NUMBER;
     }
 }
