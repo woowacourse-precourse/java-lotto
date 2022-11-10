65d3578ef586b200a4a74af762fd4e4564c0c658
TaemHam <tk4003950@gmail.com>
2022-11-11 01:20:12 +0900

    Feat: lotto.data.type.MessageType 구현

---
+++ src/main/java/lotto/data/type/MessageType.java
@@ -0,0 +1,17 @@
+package lotto.data.type;
+
+public enum MessageType {
+    BUY("구입금액을 입력해 주세요."),
+    WIN("당첨 번호를 입력해 주세요."),
+    BONUS("보너스 번호를 입력해 주세요.");
+
+    private final String message;
+
+    MessageType(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(String... args) {
+        return String.format(this.message, (Object[]) args);
+    }
+}
