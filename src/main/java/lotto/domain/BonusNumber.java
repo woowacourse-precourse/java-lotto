38d5f0ef69b03706aa0193f91f4e3bcc79ecb9d6
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-10 12:49:54 +0900

    feat: BonusNumber클래스와 테스트코드 구현

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+public class BonusNumber {
+    private final int bonusNumber;
+
+    public BonusNumber(int bonusNumber){
+
+    }
+
+    public int getBonusNumber(){
+        return 0;
+    }
+}
---
+++ src/test/java/lotto/BonusNumberTest.java
@@ -0,0 +1,18 @@
+package lotto;
+import lotto.domain.BonusNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BonusNumberTest {
+
+    BonusNumber bonusNumber;
+
+    @Test
+    @DisplayName("보너스 번호 생성자에 정수형 입력변수는 저장된다.")
+    void checkVariableInConstructor(){
+        bonusNumber = new BonusNumber(31);
+        assertThat(bonusNumber.getBonusNumber()).isEqualTo(31);
+    }
+}
