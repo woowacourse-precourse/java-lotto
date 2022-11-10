c21c997cb15c45de469038a77a6d8b83d79befe6
HubCreator <chocomilk8604@gmail.com>
2022-11-10 10:06:52 +0900

    feat(ValidationUtil) : 1~45 사이의 숫자인지 확인

---
+++ src/main/java/lotto/ValidationUtil.java
@@ -0,0 +1,7 @@
+package lotto;
+
+public class ValidationUtil {
+    public static boolean isInRange(int number) {
+        return number >= 1 && number <= 45;
+    }
+}
--- src/test/java/lotto/Validation.java
+++ src/test/java/lotto/Validation.java
@@ -3,12 +3,19 @@ package lotto;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class Validation {
 
     @Nested
     class ValidateOneDigit {
         @Test
         void isInRange() {
+            assertThat(ValidationUtil.isInRange(1)).isTrue();
+            assertThat(ValidationUtil.isInRange(45)).isTrue();
+            assertThat(ValidationUtil.isInRange(0)).isFalse();
+            assertThat(ValidationUtil.isInRange(46)).isFalse();
+            assertThat(ValidationUtil.isInRange(-1)).isFalse();
         }
     }
 }
