c19ac7a9f7bb0c0f820d2354fd5e133ce845ec1f
myungsikyoon <yms8795@gmail.com>
2022-11-10 13:01:45 +0900

    feat : 금액 입출력 기능 구현.

--- src/main/java/system/process/Purchase.java
+++ src/main/java/system/process/Purchase.java
@@ -1,22 +1,39 @@
 package system.process;
 
 import models.PurchasedLottosData;
+import net.bytebuddy.implementation.bytecode.Throw;
+import utils.Input;
 
 public class Purchase {
 
+    //TODO : 헷갈리는 이름 수정할 것. ex) purchasedLottos 와 purchaseLottos
     private PurchasedLottosData purchasedLottos;
     public Purchase(){
         purchasedLottos = proceedPurchase();
         printPurchasedLottoList();
     }
 
-    private void proceedPurchase(){
+    private PurchasedLottosData proceedPurchase(){
 
         printAmountPaidText();
 
         int amount = calculateAvailableAmount(inputAmountPaid());
         return purchaseLottos(amount);
-
     }
+
+    void printAmountPaidText(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    int inputAmountPaid() throws IllegalArgumentException{
+        String moneyInput =  Input.input();
+
+        if(moneyInput.contains(",")){
+            throw new IllegalArgumentException("[ERROR] 금액을 입력해주세요.");
+        }
+
+        return Integer.parseInt(moneyInput);
+    }
+
 
 }
