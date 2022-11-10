08933c005fc93e4a592a57038f5521509ee2e58a
Jzakka <mouse4786@gmail.com>
2022-11-10 23:54:19 +0900

    feat: User 클래스 생성
    예수금 검증을 위한 메서드 validate 생성
    로또 장수를 반환할 메서드 getLottoCount 생성
    수익률을 반환할 메서드 getYield 생성

---
+++ src/main/java/lotto/domain/User.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private final Integer MONEY;
+
+    List<Lotto> lottos = new ArrayList<>();
+
+    private Integer benefit;
+
+    public User(Integer money) {
+        validate(money);
+        this.MONEY = money;
+        this.benefit = 0;
+    }
+
+    private void validate(Integer money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("예수금은 1000원 단위여야 합니다.");
+        }
+    }
+
+    public Integer getLottoCount() {
+        return MONEY / 1000;
+    }
+
+    public void addLotto(Lotto lotto) {
+        lottos.add(lotto);
+    }
+
+    public double getYield() {
+        return benefit / MONEY * 100;
+    }
+}
