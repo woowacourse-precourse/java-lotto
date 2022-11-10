bc66655590e436941f2713e72a31cd64f6b5aaa7
woo-chang <clllickme@naver.com>
2022-11-10 22:10:10 +0900

    feat(InputService): 당첨 번호를 입력하는 기능 구현

--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -5,9 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputService {
 
     public static String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    public static String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public String readPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String readWinningNumber() {
+        System.out.println(WINNING_NUMBER_MESSAGE);
         return Console.readLine();
     }
 
