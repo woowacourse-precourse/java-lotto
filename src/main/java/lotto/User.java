36170a9eacdf3a8de2f50a45747fcac0e8d2355c
swandevson <shai4956@gmail.com>
2022-11-10 17:09:03 +0900

    Refactor(User): Validator 삭제에 따른 리팩토링
    - 돈이 1000원 단위로 떨어지는지 예외 체크

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -10,10 +10,15 @@ public class User {
 
 
     public User(int money) {
-        if (InputValidator.isInvalidMoneyUnit(money)) {
-            throw new IllegalArgumentException();
-        }
+        validateMoney(money);
 
         this.money = money;
+    }
+
+
+    private void validateMoney(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }
