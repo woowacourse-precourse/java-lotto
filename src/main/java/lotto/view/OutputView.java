d2d92b285d7c725e98a8d9a2ee8e9ead8afb1918
kokodak <kokodakadokok@gmail.com>
2022-11-10 18:21:03 +0900

    feat(outputView): 구매한 로또들의 번호를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,19 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+
 public class OutputView {
 
     private static final String HOW_MANY_LOTTO_USER_PURCHASED_MESSAGE = "개를 구매했습니다.";
 
     public static void printHowManyLottoUserPurchased(int lottoQuantity) {
         System.out.println(lottoQuantity + HOW_MANY_LOTTO_USER_PURCHASED_MESSAGE);
+    }
+
+    public static void printLottos(Lottos lottos) {
+        for (Lotto lotto : lottos.getLottos()) {
+            System.out.println(lotto.toString());
+        }
     }
 }
