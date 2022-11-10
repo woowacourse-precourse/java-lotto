8df0e77da8a5d32fcc26792bb91c083459a2b280
H4R1B0 <qkrguswns25@naver.com>
2022-11-10 22:29:36 +0900

    feat: get rank with result of lotto comparison

--- src/main/java/lotto/Calculation.java
+++ src/main/java/lotto/Calculation.java
@@ -9,16 +9,29 @@ public class Calculation {
         return String.format("%.1f%%", rate);
     }
 
-    public static List<Integer> compareLotto(List<Integer> winNumbers, int bonus, List<Integer> lotto) {
+    public static int compareLotto(List<Integer> winNumbers, int bonus, List<Integer> lotto) {
         int[] result = new int[2];
         for (int number : lotto) {
             if (winNumbers.contains(number)) {
                 result[0]++;
-            }
-            else if(number == bonus){
+            } else if (number == bonus) {
                 result[1]++;
             }
         }
-        return List.of(result[0], result[1]);
+        return getRank(result[0], result[1]);
+    }
+
+    private static int getRank(int winCount, int bonusCount) {
+        if (winCount == 6)
+            return 5;
+        if (winCount == 5 && bonusCount == 1)
+            return 4;
+        if (winCount == 5)
+            return 3;
+        if (winCount == 4)
+            return 2;
+        if (winCount == 3)
+            return 1;
+        return 0;
     }
 }
