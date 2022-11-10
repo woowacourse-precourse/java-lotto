84e7fbab64b5f0ef182c558b3ace18f7b15cf287
Sungjaeyeong <wodud2587@gmail.com>
2022-11-10 19:48:47 +0900

    feat(lotto): 금액에 맞게 로또 개수 구하기

--- src/main/java/lotto/Machine.java
+++ src/main/java/lotto/Machine.java
@@ -2,12 +2,15 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Machine {
     public void buy(int amount) {
+        int lottoCount = getCount(amount);
+    }
 
+    public int getCount(int amount) {
+        return amount / 1000;
     }
 
     private List<Integer> createLottoNumbers() {
