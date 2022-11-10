5f6b592fd8d1fc4170bf2f72d2d6054a13a6c57d
panorama713 <yaiba713@gmail.com>
2022-11-10 17:27:38 +0900

    feat(test.lotto): class 별 테스트기능  뼈대 추가

---
+++ src/test/java/lotto/CalculatorTest.java
@@ -0,0 +1,15 @@
+package lotto;
+
+/**
+ * packageName : lotto
+ * fileName : CalculatorTest
+ * author : gim-yeong-geun
+ * date : 2022/11/10
+ * description :
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/11/10         gim-yeong-geun          최초 생성
+ */
+public class CalculatorTest {
+}
