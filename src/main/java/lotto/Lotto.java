3823221a668cde259f650a841c6e8f9ead91d13e
이경범 <tkdwk567@naver.com>
2022-11-10 21:29:55 +0900

    feat : 수량 계산 기능 and 수량에 대한 로또 생성 기능 add
    - [x] 금액 입력 시 로또 발행 수량 계산
    - [x] 로또 발행 수량만큼 1 ~ 45 번까지의 중복 되지 않는 6개의 숫자를 뽑는다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.exception.PayMoneyException;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int PRICE = 1000;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,5 +22,25 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static int moneyOfQuantity(int pay) {
+        if (pay % PRICE != 0) {
+            throw new PayMoneyException("지불한 돈이 0으로 나누어 떨어지지 않습니다.");
+        }
+
+        return getQuantity(pay);
+    }
+
+    private static int getQuantity(int pay) {
+        return pay / PRICE;
+    }
+
+    public static List<Lotto> createLottoNumbers(int quantity) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int count = 0; count < quantity; count++) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            lottos.add(lotto);
+        }
+
+        return lottos;
+    }
 }
