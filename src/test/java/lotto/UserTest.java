622bc00b37aa3b517ff146d0432f5d52b6556add
whipbaek <jibaek0513@gmail.com>
2022-11-10 19:57:21 +0900

    test(UserTest): String으로 입력받은 로또번호가 List<Integer>로 변환되는지 검사 - void convertLottoNumberTest() 추가

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -28,4 +28,12 @@ public class UserTest {
         assertThatThrownBy(() -> user.validateInputNumber(1001))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("String이 List<Integer>로 잘 변환되는지 검사")
+    void convertLottoNumberTest(){
+        List<Integer> testNumber = List.of(1, 2, 3, 4, 5, 6);
+        String testValue = "1, 2, 3, 4, 5, 6";
+        assertThat(testNumber).isEqualTo(user.convertLottoNumber(testValue));
+    }
 }
