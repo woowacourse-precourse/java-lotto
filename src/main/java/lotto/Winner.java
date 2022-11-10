c69f4974a8057813393c2f08de7d3f0ef414f30c
areyouhun <areyouhun@gmail.com>
2022-11-11 00:06:23 +0900

    test(winner): 당첨 번호와 보너스 번호 테스트 세부화

--- src/main/java/lotto/Winner.java
+++ src/main/java/lotto/Winner.java
@@ -19,25 +19,33 @@ public class Winner {
         this.winningNumbers = new ArrayList<>();
         updateWinningNumbers(winningNumbersSplited);
         this.bonusNumber = Integer.valueOf(bonusNumber);
+        validateRange(this.bonusNumber);
         validateIfBonusNumberIncludedInWinningNumbers();
     }
 
     private void validateFormat(String[] numbers) {
         if (numbers.length != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 쉼표(,)를 기준으로 구분되는 6자리의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 쉼표(,)를 기준으로 구분되어야 합니다.");
         }
     }
 
     private void validateIfNumeric(String input) {
         boolean isNumber = input.matches(ONLY_NUMBER);
         if (!isNumber) {
-            throw new IllegalArgumentException("[ERROR] 띄어쓰기 없이 숫자를 입력해주세요.");
+            throw new IllegalArgumentException("[ERROR] [ERROR] 숫자가 아닌 값이 포함되어 있습니다. 공백이나 문자가 들어갔는지 확인해주세요.");
+        }
+    }
+
+    private void validateRange(Integer number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
     private void updateWinningNumbers(String[] numbers) {
         for (String number : numbers) {
             Integer numericValue = Integer.valueOf(number);
+            validateRange(numericValue);
             winningNumbers.add(numericValue);
         }
     }
--- src/test/java/lotto/WinnerTest.java
+++ src/test/java/lotto/WinnerTest.java
@@ -2,24 +2,46 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class WinnerTest {
-    private static final String ERROR_MESSAGE = "[ERROR]";
+    private static final String ERROR_OF_INVALID_FORMAT = "[ERROR] 당첨 번호는 쉼표(,)를 기준으로 구분되어야 합니다.";
+    private static final String ERROR_OF_INVALID_INPUT = "[ERROR] [ERROR] 숫자가 아닌 값이 포함되어 있습니다. 공백이나 문자가 들어갔는지 확인해주세요.";
+    private static final String ERROR_OF_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String ERROR_OF_DUPLICATE = "[ERROR] 보너스 번호가 이미 당첨 번호에 포함되어 있습니다.";
 
-    @DisplayName("당첨 번호 입력 시 쉼표를 올바르게 사용하지 않으면 예외가 발생한다.")
+    @DisplayName("당첨 번호 입력 시 쉼표를 사용하지 않으면 예외가 발생한다.")
     @ParameterizedTest
-    @CsvSource(value = {"1.2.3.4.5.6:45", "123456:45", "1&2&3&4&5&6:45", "1,2,3,,,4,5,6:45", "1,2,3,4,5and6:45"},
+    @CsvSource(value = {"1.2.3.4.5.6:45", "123456:45", "1&2&3&4&5&6:45", "1+2+3+4+5+6:45", "12345and6:45"},
             delimiter = ':')
-    void winningNumbersWithoutProperComma(String winningNumbers, String bonusNumber) {
+    void winningNumbersWithoutComma(String winningNumbers, String bonusNumber) {
         assertThatThrownBy(() -> new Winner(winningNumbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+                .hasMessageContaining(ERROR_OF_INVALID_FORMAT);
+    }
+
+    @DisplayName("당첨 번호 입력 시 숫자를 올바르게 쓰지 않으면 예외가 발생한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,4,5, 6:45", "1,!!,3,4,5,6:45", "하나,2,3,4,5,6:45", "one,2,3,4,5,6:45"},
+            delimiter = ':')
+    void winningNumbersWithoutPropervalue(String winningNumbers, String bonusNumber) {
+        assertThatThrownBy(() -> new Winner(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_OF_INVALID_INPUT);
+    }
+
+    @DisplayName("당첨 번호 입력 시 1에서 45 사이의 숫자를 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void winningNumbersOutOfRange() {
+        String winningNumbers = "1,2,3,4,5,46";
+        String bonusNumber = "45";
+
+        assertThatThrownBy(() -> new Winner(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_OF_RANGE);
     }
 
     @DisplayName("보너스 번호 입력 시 1에서 45 사이의 숫자를 입력하지 않으면 예외가 발생한다.")
@@ -30,7 +52,7 @@ class WinnerTest {
 
         assertThatThrownBy(() -> new Winner(winningNumbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+                .hasMessageContaining(ERROR_OF_RANGE);
     }
 
     @DisplayName("보너스 번호 입력 시 당첨 번호와 겹치면 예외가 발생한다.")
@@ -41,6 +63,6 @@ class WinnerTest {
 
         assertThatThrownBy(() -> new Winner(winningNumbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+                .hasMessageContaining(ERROR_OF_DUPLICATE);
     }
 }
