1cdcce6c4185521b2ab0d31410b85c2b522e5a52
YuHayeon <youha0930@gmail.com>
2022-11-11 01:04:09 +0900

    test(WinningNumbersException): 당첨 번호 입력이 숫자인지 테스트
    당첨 번호의 입력 문자열이 숫자가 아닐 경우 예외처리 테스트

--- src/test/java/lotto/exception/WinningNumbersExceptionTest.java
+++ src/test/java/lotto/exception/WinningNumbersExceptionTest.java
@@ -7,6 +7,22 @@ import org.junit.jupiter.api.Test;
 class WinningNumbersExceptionTest {
 
     @Test
+    void 당첨번호_숫자인지_확인() {
+
+        assertThatThrownBy(() -> WinningNumbersException.checkWinningNumberDigit("test"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> WinningNumbersException.checkWinningNumberDigit("1abc"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> WinningNumbersException.checkWinningNumberDigit("한글"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> WinningNumbersException.checkWinningNumberDigit("test1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     void 당첨번호_1과_45_사이인지_확인() {
 
         assertThatThrownBy(() -> WinningNumbersException.checkWinningNumberRange(-1))
