9e7e23c26a5288261c1ed71662e4c259c14f6aff
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-10 16:25:00 +0900

    feat : 로또 정렬 기능 추가
    랜덤 숫자로 만든 후 오름차순으로 정렬해 로또 반환

--- src/main/java/lotto/repository/LottoRepository.java
+++ src/main/java/lotto/repository/LottoRepository.java
@@ -1,5 +1,8 @@
 package lotto.repository;
 
+import java.util.Collections;
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
 
@@ -15,7 +18,9 @@ public class LottoRepository {
 	}
 
 	public Lotto makeAutoLotto() {
-		return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+		List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+		Collections.sort(lotto);
+		return new Lotto(lotto);
 	}
 
 }
