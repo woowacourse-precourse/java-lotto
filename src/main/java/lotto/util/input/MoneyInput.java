2d779a7623122c5e2e99c8fac41fb6e3a3f38257
kki4504 <rhkddlf9876@gmail.com>
2022-11-10 16:22:24 +0900

    feat(validateOnlyNumber) : 구입 금액 validate 기능
    정규식 사용 숫자만 입력됐는지 판별

--- src/main/java/lotto/util/input/MoneyInput.java
+++ src/main/java/lotto/util/input/MoneyInput.java
@@ -1,5 +1,23 @@
 package lotto.util.input;
 
-public class MoneyInput {
+import java.util.regex.Pattern;
 
+public class MoneyInput {
+    private int money;
+
+    public MoneyInput(String money) {
+        validate(money);
+        this.money = Integer.parseInt(money);
+    }
+
+    public int getMoney() {
+        return money;
+    }
+    private void validate(String money) {
+
+    }
+    private boolean validateOnlyNumber (String money) {
+        String REGEX = "^[0-9]$";
+        return Pattern.matches(money, REGEX);
+    }
 }
