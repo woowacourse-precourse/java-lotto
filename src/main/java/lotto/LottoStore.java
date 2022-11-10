627286e75a4e93c99763557a361c02f1717e0d6d
cpot5620 <=cpot5620@naver.com>
2022-11-10 22:31:17 +0900

    refactor(LottoStore): rename constant

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoStore {
-    private static final String ERROR_MSG = "[ERROR] 1000원 단위로 입력해주세요";
+    private static final String PRICE_ERROR_MSG = "[ERROR] 1000원 단위로 입력해주세요";
     private static final String BUY_MSG = "개를 구매했습니다.";
     private static final int LOTTO_PRICE = 1000;
 
@@ -27,12 +27,12 @@ public class LottoStore {
 
     private void validate(int price) {
         if (price < LOTTO_PRICE) {
-            System.out.println(ERROR_MSG);
+            System.out.println(PRICE_ERROR_MSG);
             throw new IllegalArgumentException();
         }
 
         if (price % LOTTO_PRICE != 0) {
-            System.out.println(ERROR_MSG);
+            System.out.println(PRICE_ERROR_MSG);
             throw new IllegalArgumentException();
         }
     }
