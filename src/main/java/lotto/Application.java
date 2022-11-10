a71e35a28a299690b86a47d460cc1de31a5585ae
saturnone1 <saturnone1@naver.com>
2022-11-10 19:24:24 +0900

    fix(Application) : README.md에 기재된 출력 순서에 맞추어 메소드 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,16 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Application {
-    public static List<Integer> winningNumber() {
+    private List<Integer> winningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
         List<Integer> lottonumbers = Arrays.asList(Console.readLine().split(","))
                 .stream()
@@ -17,13 +20,39 @@ public class Application {
         return lottonumbers;
     }
 
+    private int purchaseAmount() {
+        System.out.println("구입 금액을 입력해 주세요.");
+        int amount = Integer.parseInt(Console.readLine());
+        if (amount <= 0) {
+            throw new IllegalArgumentException("0보다 같거나 작은 수는 허용되지 않습니다.");
+        } else if (amount%1000 != 0) {
+            throw new IllegalArgumentException("구매 금액은 1000 배수여야 합니다.");
+        }
+
+        return amount/1000;
+    }
+
+    private List<List<Integer>> makeUserLottoNumbers(int numOfLotto) {
+        System.out.printf("%d개를 구매했습니다.\n",numOfLotto);
+        List<List<Integer>> userLottoNumbers = new ArrayList<>();
+        for (int n=0; n<numOfLotto; n++) {
+            List<Integer> userNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(userNumber);
+            System.out.println(userNumber);
+            userLottoNumbers.add(userNumber);
+        }
+        return userLottoNumbers;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application app =  new Application();
         try {
-            Lotto lotto = new Lotto(Application.winningNumber());
-            int amount = lotto.purchaseAmount();
+            int amount = app.purchaseAmount();
+            List<List<Integer>> userLottoNumbers = app.makeUserLottoNumbers(amount);
+            Lotto lotto = new Lotto(app.winningNumber());
+            int bonus = lotto.bonus();
         } catch (IllegalArgumentException e) {
-            System.out.printf("[ERROR] %s\n",e.getMessage());
+            System.out.printf("[ERROR] %s\n", e.getMessage());
         }
     }
 }
