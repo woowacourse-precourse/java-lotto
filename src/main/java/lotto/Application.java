04b397dfc6d1d9c3b8691f4f9459624761b1d5e6
이현진 <ehj1107@naver.com>
2022-11-10 17:36:26 +0900

    feat: 사용자의 입력 값을 받는 함수 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static String GetUserInputData(){
+        String input = Console.readLine();
+        return input;
     }
 }
