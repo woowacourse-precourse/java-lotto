6a309b6a4676a15a21b313f203046333fccfa309
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-10 16:33:28 +0900

    feat: 로또 수익률 출력
    LottoView - printYield 메소드 작성

--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -10,6 +10,8 @@ public class LottoView {
 
     private static final String LOTTO_NUMBER_MESSAGE = "개를 구매했습니다.";
     private static final int SKIP_RESULT_ELSE_CASE = 1;
+    private static final String PREFIX_YIELD_MESSAGE = "총 수익률은 ";
+    private static final String SUFFIX_YIELD_MESSAGE = "%입니다.";
 
     public void printLottoInfo(List<Lotto> lottoBundle) {
         System.out.println(lottoBundle.size() + LOTTO_NUMBER_MESSAGE);
@@ -25,5 +27,10 @@ public class LottoView {
         Arrays.stream(LottoResult.values())
                 .skip(SKIP_RESULT_ELSE_CASE)
                 .forEach(lottoResult -> lottoResult.printMessage(lottoResultToCount));
+    }
+
+    public void printYield(double yield) {
+        String yieldFormat = String.format("%.1f", yield);
+        System.out.println(PREFIX_YIELD_MESSAGE + yieldFormat + SUFFIX_YIELD_MESSAGE);
     }
 }
