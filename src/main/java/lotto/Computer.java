2f8d4b20262a53ff0631f7790c0121cb19f6906b
hingjae <ljh23270@naver.com>
2022-11-10 16:21:22 +0900

    로또 번호 발급

--- src/main/java/lotto/Computer.java
+++ src/main/java/lotto/Computer.java
@@ -1,33 +1,43 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Computer {
-    public int inputMoneyMsg() {
-        System.out.println("구입금액을 입력해 주세요.");
-        String answer = Console.readLine();
-        isValid(answer);
-        int amount = Integer.parseInt(answer);
-        return amount;
-    }
 
-    public void isValid(String answer) {
-        for(char c : answer.toCharArray()){
-            if(!Character.isDigit(c)) {
-                throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다.");
-            }
-        }
-    }
-
-    public List generateLotto(int amount) {
+    public List<List<Integer>> issuedLotto(int amount) {
+        List<List<Integer>> lottoList = new ArrayList<>();
         printAmount(amount);
-
+        generateLotto(amount, lottoList);
+        sortLotto(lottoList);
+        printLottoList(lottoList);
+        return lottoList;
     }
 
     public void printAmount(int amount) {
-        System.out.println(amount+ "개를 구매했습니다.");
+        System.out.println(amount + "개를 구매했습니다.");
     }
 
+    public void generateLotto(int amount, List<List<Integer>> lottoList) {
+        List lotto = new ArrayList();
+        for (int idx = 0; idx < amount; idx++) {
+            lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoList.add(lotto);
+        }
+    }
+
+    public void sortLotto(List<List<Integer>> lottoList) {
+        for (List lotto : lottoList) {
+            Collections.sort(lotto);
+        }
+    }
+
+    public void printLottoList(List<List<Integer>> lottoList) {
+        for (List lotto : lottoList) {
+            System.out.println(lotto);
+        }
+    }
 }
