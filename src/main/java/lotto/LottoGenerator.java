6ac3f4ec1219e0a734cd13ee30034fa8251833bf
swonny <wonny921@gmail.com>
2022-11-10 22:14:45 +0900

    feat(LottoGenerator): 로또 발행 기능 뼈대 구현

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -3,7 +3,19 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class LottoGenerator {
-    public static void generate() {
+    public static void generate(String amount) {
+
+    }
+
+    public static void validateAmount(String amount) {
+
+    }
+
+    public static int countNumberOfLotto(String amount) {
+        return 0;
+    }
+
+    public static void orderNumbers(Lotto lotto) {
 
     }
 }
