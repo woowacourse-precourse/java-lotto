4e0beee20c97596ed0551ea14c90b87ed882262f
YuHayeon <youha0930@gmail.com>
2022-11-10 22:12:48 +0900

    refactor(LottoPriceException): 공통된 예외처리 CommonInputException으로 처리
    모든 입력에 공통된 에외처리는 따로 CommonInputException으로 사용함

--- src/main/java/lotto/exception/LottoPriceException.java
+++ src/main/java/lotto/exception/LottoPriceException.java
@@ -3,23 +3,10 @@ package lotto.exception;
 public class LottoPriceException {
 
     public static void checkLottoPrice(String priceInput) {
-        checkEmptyLottoPrice(priceInput);
-        checkHasBlankInLottoPrice(priceInput);
+        CommonInputException.checkCommonInput(priceInput, "로또 금액을");
         Integer price = changePriceToInteger(priceInput);
         checkLessThanOneThousand(price);
         checkThousandUnits(price);
-    }
-
-    public static void checkEmptyLottoPrice(String price) {
-        if (price.isEmpty()) {
-            throw new IllegalArgumentException("[ERROR] 로또 금액을 입력해주세요.");
-        }
-    }
-
-    public static void checkHasBlankInLottoPrice(String price) {
-        if (!price.equals(price.trim())) {
-            throw new IllegalArgumentException("[ERROR] 로또 금액을 공백없이 입력해주세요.");
-        }
     }
 
     public static Integer changePriceToInteger(String price) {
