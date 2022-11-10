ba084a067127dfb722fe2c528f8648e554889a03
java-saeng <gusdn85554@naver.com>
2022-11-10 16:15:24 +0900

    feat : 중복된 숫자가 있는지 확인하는 기능 추가

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,11 +1,14 @@
 package lotto.validator;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class InputValidator {
 
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int MAX_LOTTO_NUMBER_COUNT = 6;
 
     public static boolean restEqualZero(long dividend, long divisor) {
         return dividend % divisor == 0;
@@ -29,5 +32,11 @@ public class InputValidator {
 
     private static boolean isRangeTrue(int number) {
         return number >= MIN_LOTTO_NUMBER && number <= MAX_LOTTO_NUMBER;
+    }
+
+    public static boolean hasDuplicatedNumber(List<Integer> numbers) {
+        Set<Integer> duplicatedCheck = new HashSet<>(numbers);
+
+        return duplicatedCheck.size() == MAX_LOTTO_NUMBER_COUNT;
     }
 }
