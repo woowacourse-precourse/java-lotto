594d120b7d0e44418656b512a1f44fbefe60f55e
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-10 20:20:34 +0900

    feat: add buying tickets by payment amount

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,13 +1,23 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class User {
 
-    List<Lotto> tickets;
+    Lotto[] tickets;
 
     User(int payment){
         validatePayment(payment);
+
+        int ticketNum = payment/1000;
+        tickets = new Lotto[ticketNum];
+
+        List<Integer> random6Digit;
+        for(int i = 0; i < ticketNum; i++){
+            random6Digit = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            tickets[i] = new Lotto(random6Digit);
+        }
     }
 
     public static void validatePayment(int payment){
