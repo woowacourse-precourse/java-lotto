af371b347aa5e2a129a7071124987ceb8859ec75
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-11 01:27:25 +0900

    feat($insert) : 당첨번호를 저장하는 기능

--- src/main/java/lotto/insert/WinningNumber.java
+++ src/main/java/lotto/insert/WinningNumber.java
@@ -1,2 +1,34 @@
-package lotto.insert;public class WinningNumber {
+package lotto.insert;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Integer.valueOf;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WinningNumber {
+
+    private String winningNumber;
+    private List<Integer> integerList = new ArrayList<>();
+
+    public WinningNumber(String insertLine) {
+        this.winningNumber = Console.readLine();
+    }
+
+    void iterator() {
+
+        String replace = winningNumber.replace(",", "");
+        char[] separate = replace.toCharArray();
+
+        for (char character : separate) {
+            integerList.add((int) character);
+        }
+
+    }
+
+    public List<Integer> getWinningNumber() {
+        return integerList;
+    }
 }
