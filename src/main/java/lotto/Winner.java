1f62cff96016aa17fd8c6437d2e5f0b341af249b
areyouhun <areyouhun@gmail.com>
2022-11-10 22:36:52 +0900

    refactor(winner): validate bonus number 메서드 검사 내용 변경

--- src/main/java/lotto/Winner.java
+++ src/main/java/lotto/Winner.java
@@ -1,24 +1,28 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Winner {
-//    private final integer bonusNumber;
-//    private final List<Integer> winningNumbers;
+    private final static String VALID_BONUS_NUMBER = "^\\d+$";
+    private final Integer bonusNumber = null;
+    private final List<Integer> winningNumbers = new ArrayList<>();
 
     public Winner(String winningNumbers, String bonusNumber) {
+        String[] winningNumbersSplited = winningNumbers.split(",");
+        validateFormat(winningNumbersSplited);
+        validateBonusNumber(bonusNumber);
     }
 
-    private void validateFormat(String winningNumbers) {
-        String[] winningNumbersSplited = winningNumbers.split(",");
-        if (winningNumbersSplited.length != 6) {
+    private void validateFormat(String[] numbers) {
+        if (numbers.length != 6) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 쉼표(,)를 기준으로 구분되는 6자리의 숫자여야 합니다.");
         }
     }
 
-    private void validateBonusNumber(List<Integer> winningNumbers, Integer bonusNumber) {
-        if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호가 이미 당첨 번호에 포함되어 있습니다.");
+    private void validateBonusNumber(String bonusNumber) {
+        if (!bonusNumber.matches(VALID_BONUS_NUMBER)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자만 입력할 수 있습니다.");
         }
     }
 }
