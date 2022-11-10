6b0f9e91ed49bc57fce39fd7bb0abdad2405d9af
ostar11 <cjw5877@gmail.com>
2022-11-10 17:10:14 +0900

    test(JudgementTest): 보너스_숫자_포함_테스트 구현

--- src/test/java/lotto/JudgementTest.java
+++ src/test/java/lotto/JudgementTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class JudgementTest {
     @Test
@@ -13,5 +15,15 @@ class JudgementTest {
         int input = judgement.correctCount(Arrays.asList(1, 2, 3, 4, 5, 6),Arrays.asList(7, 2, 3, 4, 5, 6));
 
         assertThat(input).isEqualTo(5);
+    }
+
+    @Test
+    void 보너스_숫자_포함_테스트() {
+        Judgement judgement = new Judgement();
+        boolean falseInput = judgement.hasBonusNumbers(13, Arrays.asList(1, 14, 15, 22, 33, 34));
+        boolean trueInput = judgement.hasBonusNumbers(14, Arrays.asList(1, 14, 15, 22, 33, 34));
+
+        assertThat(falseInput).isEqualTo(false);
+        assertThat(trueInput).isEqualTo(true);
     }
 }
