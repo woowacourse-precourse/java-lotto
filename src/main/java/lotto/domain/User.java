f0490030b3c3dcfc32189e8594767cdd9954abd0
KIMSEI1124 <workju1124@gmail.com>
2022-11-10 15:38:44 +0900

    feat: User의 구매 금액 저장 기능 구현

---
+++ src/main/java/lotto/domain/User.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+public class User {
+    private final int money;
+
+    public User(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}
--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -1,13 +1,19 @@
 package lotto.service;
 
+import lotto.domain.User;
 import lotto.exception.InputException;
 
 public class InputService {
+    User user;
 
     public int inputMoneyToInteger(String inputMoney) {
         InputException inputException = new InputException();
         inputException.notDigitException(inputMoney);
         inputException.notThousandMoneys(Integer.parseInt(inputMoney));
         return Integer.parseInt(inputMoney);
+    }
+
+    public void setMoneyToUser(String inputMoney) {
+        user = new User(inputMoneyToInteger(inputMoney));
     }
 }
