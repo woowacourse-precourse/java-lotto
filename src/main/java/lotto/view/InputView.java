c4928167bb8c83f9b445b44c8e016544d49c547a
hectick <chyeon1223@cau.ac.kr>
2022-11-11 00:20:08 +0900

    refactor: 클래스 내부에서만 쓰는 상수에 private 설정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,8 +3,9 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
-    static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public String getPurchaseAmount(){
         System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
@@ -14,6 +15,12 @@ public class InputView {
 
     public String getWinningNumbers(){
         System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
+        String input = Console.readLine();
+        return input;
+    }
+
+    public String getBonusNumber(){
+        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
         String input = Console.readLine();
         return input;
     }
