d6a08ba796619354b99c2e0f95278f90bf6a7729
김태용 <amico741@naver.com>
2022-11-11 02:09:57 +0900

    feat: 구입 금액 입력이 숫자인지 검사 메서드 추가
    숫자가 아닐시 예외를 발생시킴

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,11 +2,14 @@ package lotto.service;
 
 import lotto.domain.Buyer;
 
+import java.util.regex.Pattern;
+
 public class LottoService {
     public static final int STANDARD_WON = 1000;
     public static final int ZERO_WON = 0;
     public static final String NOT_MOD_ZERO_MESSAGE = "[ERROR] 1000으로 나누어지는 금액이 아닙니다.";
     public static final String REJECT_ZERO = "[ERROR] 구입 금액에 0을 입력할 수 없습니다.";
+    public static final String REJECT_NOT_NUMBERS = "[ERROR] 구입 금액에 숫자를 제외한 다른 입력이 올 수 없습니다.";
     
     public LottoService() { }
 
@@ -14,5 +17,10 @@ public class LottoService {
         if(money == ZERO_WON) { throw new IllegalArgumentException(REJECT_ZERO); }
 
         if(Math.floorMod(Math.round(money), STANDARD_WON) != 0) { throw new IllegalArgumentException(NOT_MOD_ZERO_MESSAGE); }
+    }
+
+    public void validateConsistOfNumbers(String input) {
+        boolean result = Pattern.matches("^[0-9]*$", input);
+        if(!result) { throw new IllegalArgumentException(REJECT_NOT_NUMBERS); }
     }
 }
