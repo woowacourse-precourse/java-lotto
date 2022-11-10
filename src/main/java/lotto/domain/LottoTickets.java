0f204c1ef33c229d1adab302569e35dd6b2102c6
jeujeon <1132jjw@naver.com>
2022-11-10 23:34:00 +0900

    feat(LottoTickets): 구입 금액을 받으면 그 만큼의 로또 발행하는 기능 추가

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -1,4 +1,29 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LottoTickets {
+	private List<LottoTicket> lottoTickets;
+	public LottoTickets(String purchasingAmount) {
+		if (!isValidPurchasingAmount(purchasingAmount)) {
+			throw new IllegalArgumentException();
+		}
+		purchaseLottoTickets(toInt(purchasingAmount));
+
+	}
+
+	private void purchaseLottoTickets(int purchasingAmount) {
+		while (purchasingAmount != 0) {
+			lottoTickets.add(new LottoTicket(LottoNumberGenerator.generate()));
+			purchasingAmount -= 1000;
+		}
+	}
+
+	private boolean isValidPurchasingAmount(String purchasingAmount) {
+		return true;
+	}
+
+	private int toInt(String str) {
+		return Integer.valueOf(str);
+	}
 }
