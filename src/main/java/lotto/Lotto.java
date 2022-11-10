28ba3cdaac4f1955880053397970fe3f44308a62
swonny <wonny921@gmail.com>
2022-11-11 00:34:44 +0900

    feat(Lotto): 로또 번호 중복 검사

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,10 +11,22 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        checkLength(numbers);
+        checkDuplicate(numbers);
+    }
+
+    // TODO: 추가 기능 구현
+    private void checkDuplicate(List<Integer> numbers) {
+        for (int numberIndex = 0; numberIndex < numbers.size(); numberIndex++) {
+            if (numbers.lastIndexOf(numbers.get(numberIndex)) != numberIndex) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void checkLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
-
-    // TODO: 추가 기능 구현
 }
