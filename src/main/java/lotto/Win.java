8cc4b55688cfe09fa7f3a6b8426793d02dbc9cd0
dohyun <ldh19738@gmail.com>
2022-11-10 20:10:56 +0900

    feat(Win): 당첨 메세지 출력하는 기능 추가

---
+++ src/main/java/lotto/Win.java
@@ -0,0 +1,23 @@
+package lotto;
+
+public class Win {
+    public void threeMatchMessage() {
+        System.out.println("3개 일치 (5,000원) - ");
+    }
+
+    public void fourMatchMessage() {
+        System.out.println("4개 일치 (50,000원) - ");
+    }
+
+    public void fiveMatchMessage() {
+        System.out.println("5개 일치 (1,500,000원) - ");
+    }
+
+    public void fiveAndBonusMatchMessage() {
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - ");
+    }
+
+    public void sixMatchMessage() {
+        System.out.println("6개 일치 (2,000,000,000원) - ");
+    }
+}
