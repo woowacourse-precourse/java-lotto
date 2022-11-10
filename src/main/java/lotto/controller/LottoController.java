d572f34993e3297ed0bb04b4027bb2414c66b615
yj2980 <gum0945@naver.com>
2022-11-10 23:16:56 +0900

    refactor : controller 기능 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,13 +1,14 @@
 package lotto.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import static lotto.service.LottoService.*;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 import lotto.domain.Lotto;
+import lotto.service.LottoService;
 import lotto.view.Input;
+import lotto.view.Output;
 
 public class LottoController {
 	public static List<List<Integer>> publishLotteries(int quantity) {
@@ -15,38 +16,19 @@ public class LottoController {
 
 		for (int i = 0; i < quantity; i++) {
 			Lotto lotto = new Lotto(pickLottoNumbers());
+
 			publishLotto(candidate, lotto.getNumbers());
-			System.out.println(candidate.get(i));
+
+			Output.printPublishLotteries(candidate.get(i));
 		}
 
 		return candidate;
 	}
-	public static List<List<Integer>> publishLotto(List<List<Integer>> candidateLotto, List<Integer> candidate) {
-		candidateLotto.add(candidate);
-
-		return candidateLotto;
-	}
-
-	public static List<Integer> pickLottoNumbers() {
-		List<Integer> lottoNumbers = new ArrayList<>();
-
-		lottoNumbers = Randoms.pickUniqueNumbersInRange(1,45,6);
-
-		lottoNumbers.stream()
-				.sorted();
-
-		return lottoNumbers;
-	}
 
 	public static List<Integer> pickWinningNumbers() {
 		String winningNumbers = Input.pickWinningNumbers();
-		List<String> lotto = Arrays.asList(winningNumbers.split(","));
 
-		List<Integer> lottoNumbers = lotto.stream()
-				.map(s -> Integer.parseInt(s))
-				.collect(Collectors.toList());
-
-		return lottoNumbers;
+		return LottoService.convertStringToList(winningNumbers);
 	}
 
 	public static int pickBonusNumbers() {
@@ -55,58 +37,28 @@ public class LottoController {
 		return Integer.parseInt(bonusNumber);
 	}
 
-	public static int compareNumbers(List<Integer> candidateNumbers, List<Integer> winningNumbers) {
-		int count = 0;
+	public static List<Integer> getWinningRanking(List<List<Integer>> candidate, List<Integer> winningNumbers, int bonusNumber) {
+		/*
+		List<Integer> ranking = Arrays.asList(0,0,0,0,0);
 
-		for (int i = 0; i < candidateNumbers.size(); i++) {
-			if (candidateNumbers.contains(winningNumbers.get(i))) {
-				count++;
-			}
-		}
-
-		return count;
-	}
-
-	public static List<Integer> countWinningLotto(List<List<Integer>> lotto, List<Integer> winningNumbers, int bonusNumbers, int quantity) {
-		List<Integer> winningCount = Arrays.asList(0,0,0,0,0);
-
-		for (int i = 0; i < lotto.size(); i++) {
-			int count = compareNumbers(lotto.get(i), winningNumbers);
+		for (int i = 0; i < candidate.size(); i++) {
+			int count = compareNumbers(candidate.get(i), winningNumbers);
 			if (count == 3)
-				winningCount.set(0, winningCount.get(0) + 1);
+				ranking.set(0, ranking.get(0) + 1);
 			if (count == 4)
-				winningCount.set(1, winningCount.get(1) + 1);
+				ranking.set(1, ranking.get(1) + 1);
 			if (count == 5)
-				winningCount.set(2, winningCount.get(2) + 1);
-			if (count == 5 && lotto.get(i).contains(bonusNumbers))
-				winningCount.set(3, winningCount.get(3) + 1);
+				ranking.set(2, ranking.get(2) + 1);
+			if (count == 5 && candidate.get(i).contains(bonusNumbers))
+				ranking.set(3, ranking.get(3) + 1);
 			if (count == 6)
-				winningCount.set(4, winningCount.get(4) + 1);
+				ranking.set(4, ranking.get(4) + 1);
 		}
-
-		return winningCount;
+		 */
+		return LottoService.countWinningNumber(candidate, winningNumbers, bonusNumber);
 	}
 
 	public static double calculateProfit(List<Integer> winningCount, double money) {
-		double prizeMoney = sumPrizeMoney(winningCount);
-
-		double profit = (prizeMoney/money) * 100;
-
-
-		//반올림
-		//profit = Math.round(profit*100)/100.0;
-
-		return profit;
-	}
-
-	private static int sumPrizeMoney(List<Integer> winningCount) {
-		int sum = 0;
-		List<Integer> prizeMoney = Arrays.asList(5_000, 50_000, 1_500_000, 30_000_000, 2_000_000_000);
-
-		for (int i = 0; i < winningCount.size(); i++) {
-			sum += winningCount.get(i) * prizeMoney.get(i);
-		}
-
-		return sum;
+		return LottoService.getRateOfReturn(winningCount, money);
 	}
 }
---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,100 @@
+package lotto.service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class LottoService {
+	public static List<Integer> pickLottoNumbers() {
+		List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+
+		lottoNumbers.stream()
+				.sorted();
+
+		return lottoNumbers;
+	}
+	public static List<List<Integer>> publishLotto(List<List<Integer>> candidateLotto, List<Integer> candidate) {
+		candidateLotto.add(candidate);
+
+		return candidateLotto;
+	}
+
+	public static List<Integer> convertStringToList(String numbers) {
+		List<String> lotto = Arrays.asList(numbers.split(","));
+
+		List<Integer> lottoNumbers = lotto.stream()
+				.map(s -> Integer.parseInt(s))
+				.collect(Collectors.toList());
+
+		return lottoNumbers;
+	}
+	public static List<Integer> countWinningNumber(List<List<Integer>> candidate, List<Integer> winningNumbers, int bonusNumber) {
+		List<Integer> ranking = Arrays.asList(0,0,0,0,0);
+
+		for (int i = 0; i < candidate.size(); i++) {
+			int count = compareNumbers(candidate.get(i), winningNumbers);
+
+			if (count == 4 && checkBonus(candidate.get(i), bonusNumber)) {
+				count = -1;
+			}
+			judgementRanking(count, ranking);
+		}
+
+		return ranking;
+	}
+	private static void judgementRanking(int count, List<Integer> ranking) {
+		if (count == -1) {
+			ranking.set(3, ranking.get(3) + 1);
+		}
+		if (count == 3) {
+			ranking.set(0, ranking.get(0) + 1);
+		}
+		if (count == 4) {
+			ranking.set(1, ranking.get(1) + 1);
+		}
+		if (count == 5) {
+			ranking.set(2, ranking.get(2) + 1);
+		}
+		if (count == 6)
+			ranking.set(4, ranking.get(4) + 1);
+	}
+	private static boolean checkBonus(List<Integer> candidate, int bonusNumber) {
+		if (candidate.contains(bonusNumber))
+			return true;
+		return false;
+	}
+
+	public static int compareNumbers(List<Integer> candidateNumbers, List<Integer> winningNumbers) {
+		int count = 0;
+
+		for (int i = 0; i < candidateNumbers.size(); i++) {
+			if (candidateNumbers.contains(winningNumbers.get(i))) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	public static double getRateOfReturn(List<Integer> winningCount, double money) {
+		int revenue = sumRevenue(winningCount);
+
+		double rateOfReturn = (revenue / money) * 100;
+		//반올림
+		//rateOfReturn = Math.round(profit*100)/100.0;
+		return rateOfReturn;
+	}
+
+	private static int sumRevenue(List<Integer> winningCount) {
+		int sum = 0;
+		List<Integer> prizeMoney = Arrays.asList(5_000, 50_000, 1_500_000, 30_000_000, 2_000_000_000);
+
+		for (int i = 0; i < winningCount.size(); i++) {
+			sum += winningCount.get(i) * prizeMoney.get(i);
+		}
+
+		return sum;
+	}
+}
