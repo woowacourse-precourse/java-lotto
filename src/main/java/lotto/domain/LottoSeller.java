18a586eab0ced22c717f57c69f5f166a88318d2e
yj2980 <gum0945@naver.com>
2022-11-10 21:45:48 +0900

    feat : LottoSeller 객체 생성

--- src/main/java/lotto/domain/LottoSeller.java
+++ src/main/java/lotto/domain/LottoSeller.java
@@ -1,2 +1,29 @@
-package lotto.domain;public class lottoSeller {
+package lotto.domain;
+
+import java.util.List;
+
+import lotto.controller.LottoController;
+
+public class LottoSeller {
+	private static int quantity;
+	private static List<List<Integer>> lotto;
+
+	public LottoSeller(int money) {
+		this.quantity = checkQuantityOfLotto(money);
+		setLotto();
+	}
+	private int checkQuantityOfLotto(int money) {
+		return money / 1000;
+	}
+
+	private void setLotto() {
+		//controller에게 요청한다. 발행하라고
+		this.lotto = LottoController.publishLotteries(quantity);
+	}
+	public static int getQuantity() {
+		return quantity;
+	}
+	public static List<List<Integer>> getLotto() {
+		return lotto;
+	}
 }
