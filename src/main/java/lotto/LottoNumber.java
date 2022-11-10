f8c831c363ba58bd6e5fbf4227723aa0b999c0cb
EUNJEONGMUN <munej26@gmail.com>
2022-11-10 10:12:42 +0900

    feat: 로또 번호 숫자 범위 1~45 사이 검사 메서드 구현

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoNumber {
+    static final int LOTTO_NUMBER_MIN = 1;
+    static final int LOTTO_NUMBER_MAX = 45;
+
+    private List<Integer> lottoNumbers = new ArrayList<>();
+    private int bonusNumber;
+
+    public LottoNumber(String lottoNumbers, String bonusNumber) {
+
+    }
+
+    private int validNumberRange(int num) {
+        if (num < LOTTO_NUMBER_MIN || num > LOTTO_NUMBER_MAX) {
+            throw new IllegalArgumentException("[ERROR] 1에서 45사이의 숫자를 입력해주세요.");
+        }
+        return num;
+    }
+}
