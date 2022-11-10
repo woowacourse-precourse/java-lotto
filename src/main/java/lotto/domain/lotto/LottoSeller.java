c61964d294af3b93a0e9f46afaf2f5e3f31ce3c1
2jun0 <soo28819@gmail.com>
2022-11-11 03:42:00 +0900

    Feat(Lotto): Lotto를 발급하는 셀러 구현

---
+++ src/main/java/lotto/domain/lotto/LottoSeller.java
@@ -0,0 +1,29 @@
+package lotto.domain.lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.domain.lotto.Lotto.*;
+
+public class LottoSeller {
+
+    static final int PRICE = 1000;
+
+    private Lotto createRandomLotto() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBER_COUNT);
+        return new Lotto(randomNumbers);
+    }
+
+    public List<Lotto> buyLottos(int money) {
+        int lottoCount = money / PRICE;
+        List<Lotto> lottoList = new ArrayList<>();
+
+        for (int i = 0; i < lottoCount; i++) {
+            lottoList.add(createRandomLotto());
+        }
+
+        return lottoList;
+    }
+}
