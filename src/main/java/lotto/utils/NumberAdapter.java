b6058f6b8d8f5f8e2cd51a123abd500505825f0c
Jin409 <sualng123@gmail.com>
2022-11-11 01:18:10 +0800

    feat(NumberAdapter): 문자열인 입력값을 적절한 형태로 바꾸는 기능 구현

---
+++ src/main/java/lotto/utils/NumberAdapter.java
@@ -0,0 +1,23 @@
+package lotto.utils;
+
+import static lotto.constants.LottoConstant.LOTTERY_NUMBER_SEPARATOR;
+import static lotto.validator.NumberValidator.hasValidType;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NumberAdapter {
+    public static List<Integer> getLotteryNumbers(String beforeLotteryNumbers) {
+        hasValidType(beforeLotteryNumbers);
+        String[] tempLotteryNumbers = beforeLotteryNumbers.split(LOTTERY_NUMBER_SEPARATOR);
+        int[] afterLotteryNumbers = Arrays.stream(tempLotteryNumbers).mapToInt(Integer::parseInt).toArray();
+        return Arrays.stream(afterLotteryNumbers)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    public static int getBonusNumber(String beforeBonusNumber) {
+        return Integer.parseInt(beforeBonusNumber);
+    }
+}
