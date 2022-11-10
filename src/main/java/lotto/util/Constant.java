40a5f8fb54379a5a5f67ca4fc563324c0090c055
woosung1223 <dntjd991223@naver.com>
2022-11-10 21:48:37 +0900

    feat(Constant): 시스템 관련 상수를 저장하는 클래스 생성

---
+++ src/main/java/lotto/util/Constant.java
@@ -0,0 +1,9 @@
+package lotto.util;
+
+public final class Constant {
+    private Constant() {}
+
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 45;
+    public static final int LOTTO_SIZE = 6;
+}
