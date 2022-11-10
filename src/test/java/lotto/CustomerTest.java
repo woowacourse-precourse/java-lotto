949d03b2f4112f79421a88b993b0ab1d330dcf08
ddubidubap <park62000@gmail.com>
2022-11-11 00:22:18 +0900

    test: 구입 금액이 1000으로 나누어지지 않을 경우 예외 발생 테스트

---
+++ src/test/java/lotto/CustomerTest.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import lotto.domain.Customer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CustomerTest {
+    @DisplayName("구입 금액이 1000으로 나누어지지 않으면 예외가 발생한다.")
+    @Test
+    void createCustomerByWrongMoney() {
+        assertThatThrownBy(() -> new Customer(1001))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
