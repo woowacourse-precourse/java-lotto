741dd102a3e9f57fc23d430a83c924caf99ef315
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-10 17:52:33 +0900

    test: Lotto에 유효하지 않은 값이 입력됬을 경우 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -3,8 +3,13 @@ package lotto.domain;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -21,6 +26,14 @@ class LottoTest {
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 1~45 이외의 숫자를 입력하면 예외를 발생시킨다")
+    @ParameterizedTest
+    @CsvSource({"-1,2,3,4,5,6","1,2,3,4,5,46"})
+    void 유효하지_않은_로또값_입력은_예외를_발생시킨다(int num1, int num2, int num3, int num4, int num5, int num6) {
+        assertThatThrownBy(() -> new Lotto(List.of(num1, num2, num3, num4, num5, num6)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
