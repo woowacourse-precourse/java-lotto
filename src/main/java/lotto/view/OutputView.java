b11d04f84959d2c58d6422ed136964adf440e1d2
woosung1223 <dntjd991223@naver.com>
2022-11-10 23:47:05 +0900

    feat(OutputView): printWinningRecord 메소드 생성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
 public final class OutputView {
     private OutputView() {
     }
@@ -11,5 +13,18 @@ public final class OutputView {
         // 따라서 상태에 접근해도 됨.
         lottos.getLottos().stream()
                 .forEach(lotto -> System.out.println(lotto));
+    }
+
+    public static void printWinningRecord(Lotto userLotto) {
+        // 당첨 Enum은 몇개 일치, 몇원에 대한 정보를 가지고 있고
+        // 메소드를 통해 당첨 개수를 알아낼 수 있다. 메소드의 인자는 Lotto 객체.
+        // 리턴 값은 List로 줄 수도 있는데, 일급 컬렉션을 사용할까 고민임.
+
+        WinningLotto.makeNumberOfWins(userLotto).stream()
+                .forEach(prizeCount -> {
+                        WinningLotto.printPrizeInfo();
+                        System.out.println(prizeCount);
+                    }
+                );
     }
 }
