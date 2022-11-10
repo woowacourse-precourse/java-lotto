fc08e9061f8141b5f129de582f38a8db451c91d0
Donghyun <flaehan@gmail.com>
2022-11-10 12:18:57 +0900

    type : 로또 구입금액
    body : 로또 구입 금액 입력 기능 구현
    
    footer :

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,28 +1,39 @@
 package lotto;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
-    private int inputMoney;
 
-    public Lotto(List<Integer> numbers) { // 생성자
+    private final List<Integer> numbers; // 기존 코드
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public Lotto(List<Integer> numbers) { // 생성자, 기존 코드
         validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) { // 예외사항
+    public int inputMoney() throws IOException {
+        System.out.println("구입금액을 입력해 주세요.");
+        int inputMoney = Integer.parseInt(br.readLine());
+        return inputMoney;
+    }
+
+    private void validate(List<Integer> numbers) throws IOException { // 예외사항 기존 코드
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("로또 숫자는 여섯자리여야 합니다.");
         }
 
-        if(inputMoney % 1000 != 0){
+        if (inputMoney() % 1000 != 0) {
             throw new IllegalArgumentException("로또 구입 금액은 1,000원 단위입니다.");
         }
 
-        if(Lotto 중복) {
-            throw new IllegalArgumentException("로또는 서로 다른 여섯자리 숫자로 구입해야 합니다.");
-        }
+//        if(Lotto 중복) {
+//            throw new IllegalArgumentException("로또는 서로 다른 여섯자리 숫자로 구입해야 합니다.");
+//        }
     }
 
     // TODO: 추가 기능 구현
