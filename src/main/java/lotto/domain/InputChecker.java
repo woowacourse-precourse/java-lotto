8d5a02af762083a5696224ebc42ca1e0d2a8d646
ddubidubap <park62000@gmail.com>
2022-11-11 01:12:09 +0900

    feat: 당첨 번호가 모두 숫자인지 확인하는 함수

--- src/main/java/lotto/domain/InputChecker.java
+++ src/main/java/lotto/domain/InputChecker.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static lotto.view.Constants.*;
 
 public class InputChecker {
@@ -9,5 +13,17 @@ public class InputChecker {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NOT_NUMERIC);
         }
+    }
+
+    public void validateWinningNumbers(String input) {
+        List<String> splitForm = splitInput(input);
+        for (String piece: splitForm) {
+            validateNumeric(piece);
+        }
+    }
+
+    private List<String> splitInput(String input) {
+        String[] splitForm = input.split(",");
+        return Arrays.asList(splitForm);
     }
 }
