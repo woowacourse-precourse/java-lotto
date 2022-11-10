1e9cdc55fd0f1a8aab3b9ebf0055fed891dcc4f4
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-10 15:19:03 +0900

    move : BuyerLotto의 당첨번호 예외코드 WinningLotto로 이동

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -11,5 +11,43 @@ public class WinningLotto {
     public void winningNumberInput() {
         System.out.println(WINNING_NUMBER_INPUT.getMessage());
         String winningNumber = readLine();
+        validateWinningNumber(winningNumber);
+        validateWinningNumberSize(winningNumber);
+        validateInputBlank(winningNumber);
+        validateInputSeparator(winningNumber);
+    }
+
+    void validateWinningNumber(String winningNumber) {
+        Integer[] winningNumberArr = Stream.of(winningNumber.split(","))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .toArray(Integer[]::new);
+        for (Integer number : winningNumberArr) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(ErrorMessage.ERROR_NUMBER_RANGE.getErrorMessage());
+            }
+        }
+    }
+
+    void validateWinningNumberSize(String winningNumber) {
+        String[] numberSizeCheck = winningNumber.split(",");
+        if (numberSizeCheck.length != 6) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_LOTTO_LENGTH.getErrorMessage());
+        }
+    }
+
+    void validateInputBlank(String winningNumber) {
+        for (int i = 0; i < winningNumber.length(); i++) {
+            if (winningNumber.charAt(i) == ' ') {
+                throw new IllegalArgumentException(ErrorMessage.ERROR_BLANK.getErrorMessage());
+            }
+        }
+    }
+
+    void validateInputSeparator(String winningNumber) {
+        String[] separatorCheck = winningNumber.split(",");
+        if (separatorCheck.length != 6) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_SEPARATOR.getErrorMessage());
+        }
     }
 }
