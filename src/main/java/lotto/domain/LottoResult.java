537f1201a4700805a88cea184e6fda8f5fd190d3
eunji53 <duqtj00@naver.com>
2022-11-10 13:36:40 +0900

    <feat>:당첨번호 입력

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,9 +1,23 @@
 package lotto.domain;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottoResult {
-    public List<Integer> inputResult(){
-        return null;
+    public List<Integer> inputLottoNumber(){
+        System.out.println("당첨번호를 입력해 주세요.");
+        String lottoNumber=readLine();
+        List<String> numbers = Stream.of(lottoNumber.split(",")).collect(Collectors.toList());
+        List<Integer> lotto=new ArrayList<>();
+        for (String s : numbers) {
+            lotto.add(Integer.parseInt(s));
+        }
+        return lotto;
     }
 }
