ab2249e96f53fd673498cd4669fc6d50ed8fc02a
asas6978 <asas6978@naver.com>
2022-11-11 01:20:50 +0900

    feat: 에러 메시지 구현

---
+++ src/main/java/lotto/ExceptionMessage.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class ExceptionMessage {
+    public static final String ERROR = "[ERROR] ";
+    public static final String NUMBER_OUT_OF_RANGE = "로또 번호는 6개여야 합니다.";
+    public static final String LOTTO_OUT_OF_RANGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String CANNOT_CONVERT_NUMBER = "입력 값을 숫자로 변환할 수 없습니다.";
+    public static final String INVALID_AMOUNT_UNIT = "금액은 1,000원 단위로 입력해주세요.";
+}
