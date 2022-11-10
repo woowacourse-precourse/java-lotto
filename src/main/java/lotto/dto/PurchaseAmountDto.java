70009ec318e9c62f06c60daa925814fdb6f2b39c
greeng00se <thegreengoose7@gmail.com>
2022-11-10 20:48:05 +0900

    feat(PurchaseAmountDto): 구입 금액에 대한 값을 가진 PurchaseAmountDto 추가

---
+++ src/main/java/lotto/dto/PurchaseAmountDto.java
@@ -0,0 +1,14 @@
+package lotto.dto;
+
+public class PurchaseAmountDto {
+
+    private final Integer purchaseAmount;
+
+    public PurchaseAmountDto(Integer purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    public Integer getPurchaseAmount() {
+        return purchaseAmount;
+    }
+}
