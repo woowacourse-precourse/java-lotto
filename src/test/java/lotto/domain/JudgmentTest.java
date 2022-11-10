caa9ece8805d643bc984ce6c99bf5ee047ee2c0b
im-shung <seoyoung1113@gmail.com>
2022-11-10 23:02:48 +0900

    test: :white_check_mark: 컴퓨터의 수와 플레이어의 수를 비교해서 몇 개의 숫자가 일치하는 지 알 수 있는 기능 테스트

--- src/test/java/lotto/domain/JudgmentTest.java
+++ src/test/java/lotto/domain/JudgmentTest.java
@@ -6,11 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class JudgmentTest {
@@ -22,6 +24,19 @@ class JudgmentTest {
     @CsvSource({"0, 8, true", "0, 4, false", "1, 21, true"})
     void isEqualPlace(int index, int number, boolean expected) {
         boolean actual = Judgment.hasPlace(ANSWER, index, number);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("컴퓨터의 수와 플레이어의 수를 비교해서 몇 개의 숫자가 일치하는 지 알 수 있다")
+    @ParameterizedTest
+    @CsvSource({"4, 21, 23, 41, 42, 43, 5", "4, 5, 23, 41, 42, 43, 4", "4, 5, 1, 41, 42, 43, 3",
+            "4, 5, 1, 27, 42, 43, 2", "4, 5, 1, 27, 34, 43, 1", "4, 5, 1, 27, 34, 22, 0"})
+    void isEqualCorrectCount(int number1, int number2, int number3, int number4, int number5,
+            int number6, int expected) {
+        int actual = (int) IntStream.range(0, 6)
+                .filter(index -> Judgment.hasPlace(ANSWER, index,
+                        Arrays.asList(number1, number2, number3, number4, number5, number6)
+                                .get(index))).count();
         assertThat(actual).isEqualTo(expected);
     }
 
