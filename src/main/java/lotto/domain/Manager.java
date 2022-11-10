e8e3c081380431beb87a7ce47bb5fdb29407bbd6
kdkdhoho <hkim4410@naver.com>
2022-11-10 17:00:37 +0900

    feat(Manager): 당첨 번호와 유저의 로또 번호를 비교하여 맞은 개수 구하는 함수 추가

--- src/main/java/lotto/domain/Manager.java
+++ src/main/java/lotto/domain/Manager.java
@@ -1,9 +1,31 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Manager {
+
+    private List<Integer> winningNumbers = new ArrayList<>();
+    private int bonusNumber;
 
     public int changeLottoCount(int money) {
         // TODO: money가 1천원 단위인지 확인
         return (money / 1000);
+    }
+
+    public int getCorrectCount(List<Integer> userLotto) {
+        int correctCount = 0;
+
+        Collections.sort(winningNumbers);
+        Collections.sort(userLotto);
+
+        for(int i = 0; i < Constant.LOTTO_SIZE; i++) {
+            if(winningNumbers.get(i) == userLotto.get(i)) {
+                correctCount++;
+            }
+        }
+
+        return correctCount;
     }
 }
