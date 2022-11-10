5769951522c401e183320f407e7d3507aacda5ff
KimSangHyeok <tkdgur0906@naver.com>
2022-11-10 23:56:41 +0900

    Feat(lotto): 구매 로또 번호 목록 출력 기능

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -2,10 +2,21 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class View {
     public int askPrice(){
         System.out.println("구입금액을 입력해 주세요.");
         String price = Console.readLine();
         return Integer.parseInt(price);
+    }
+
+    public void buyLottoList(int price, List<Lotto> lottoBundle){
+        System.out.println(price / 1000 + "개를 구매했습니다.");
+        for (Lotto lotto : lottoBundle) {
+            lotto.getNumbers().sort(Comparator.naturalOrder());
+            System.out.println(lotto.toString());
+        }
     }
 }
