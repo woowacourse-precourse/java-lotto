3924c0f1af36a800241841fbb73890befebcbdd0
ddubidubap <park62000@gmail.com>
2022-11-11 00:32:41 +0900

    feat: 구입한 개수만큼 로또 번호 랜덤 생성하는 함수

--- src/main/java/lotto/domain/Customer.java
+++ src/main/java/lotto/domain/Customer.java
@@ -1,22 +1,35 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import static lotto.view.Constants.NOT_DIVISIBLE_BY_THOUSAND;
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+import static lotto.view.Constants.*;
 
 public class Customer {
     private final int paidMoney;
     private final int numOfTicket;
+    private final List<List<Integer>> purchasedTickets = new ArrayList<>();
 
     public Customer(int paidMoney) {
         validateProperMoney(paidMoney);
         this.paidMoney = paidMoney;
         this.numOfTicket = this.paidMoney / 1000;
+        purchaseLottery();
     }
 
     private void validateProperMoney(int paidMoney) {
         if (paidMoney % 1000 != 0) {
             throw new IllegalArgumentException(NOT_DIVISIBLE_BY_THOUSAND);
+        }
+    }
+
+    private void purchaseLottery() {
+        for (int i = 0; i < this.numOfTicket; i++) {
+            List<Integer> ticket = pickUniqueNumbersInRange(MIN_NUM, MAX_NUM, COUNT);
+            Collections.sort(ticket);
+            this.purchasedTickets.add(ticket);
         }
     }
 }
