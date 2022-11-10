b9b23d591bb0f79de061c88994154891a82e8734
Choi-JJunho <junho5336@gmail.com>
2022-11-10 23:37:21 +0900

    test(Publisher): add getLottoCount

--- src/test/java/lotto/domain/PublisherTest.java
+++ src/test/java/lotto/domain/PublisherTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("로또 발행에 대한 테스트")
 class PublisherTest {
@@ -16,5 +19,12 @@ class PublisherTest {
     void createLottoByInvalidMoney(int money) {
         assertThatThrownBy(() -> publisher.getLottoGroup(money))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "{0}원이면 {1}장의 로또가 나온다")
+    @CsvSource(value = {"1000:1", "2000:2", "10000:10", "4000:4"}, delimiter = ':')
+    void getLottoCount(int money, int count) {
+        List<List<Integer>> lottoGroup = publisher.getLottoGroup(money);
+        assertEquals(lottoGroup.size(), count);
     }
 }
