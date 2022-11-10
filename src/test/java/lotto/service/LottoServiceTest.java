682ce8db8f279c40e96ef03608e7cf8baf2202c5
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-10 15:24:58 +0900

    test : 구매 테스트 추가

---
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -0,0 +1,27 @@
+package lotto.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import lotto.domain.LottoGroup;
+
+public class LottoServiceTest {
+
+	private final LottoService lottoService = new LottoService();
+
+	@DisplayName("로또 구매 수량 입력시 원하는만큼 구매가 되었는지 확인하는 테스트")
+	@Test
+	void givenPurchaseQuantity_whenMakeLottoGroup_thenLottosSizeEqualPurchaseQuantity(){
+	    //given
+		int purchaseQuantity = 8;
+
+	    //when
+		LottoGroup lottoGroup = lottoService.makeLottoGroup(purchaseQuantity);
+
+		//then
+		Assertions.assertThat(lottoGroup.getLottosSize())
+			.isEqualTo(purchaseQuantity);
+	}
+
+}
