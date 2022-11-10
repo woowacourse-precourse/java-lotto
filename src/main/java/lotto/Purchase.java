0d0a64f9bd330fd6420842e1af3b17bed9ea308d
Eunkeee <eunkeeeee@gmail.com>
2022-11-10 13:13:05 +0900

    feat: 구입 금액 검증

--- src/main/java/lotto/Purchase.java
+++ src/main/java/lotto/Purchase.java
@@ -1,11 +1,24 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class Purchase {
+    private static final Pattern MONEY_REGEX = Pattern.compile("/^[0-9]*$/");
     private final int issueCount;
+
     public Purchase() {
-        int money = Integer.parseInt(Console.readLine());
+        String input = Console.readLine();
+        if (!MONEY_REGEX.matcher(input).matches()) {
+            throw new IllegalArgumentException("[ERROR] 올바른 로또 구입 금액을 입력해 주세요.");
+        }
+        int money = Integer.parseInt(input);
+        if (money < 0) {
+            throw new IllegalArgumentException("[ERROR] 올바른 로또 구입 금액을 입력해 주세요.");
+        }
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 가능합니다.");
+        }
         this.issueCount = money / 1000;
         System.out.println(issueCount);
     }
