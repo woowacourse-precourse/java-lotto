e3ab14d53d1a853a10a8d108f334883f22757a90
eora21 <eora21@naver.com>
2022-11-10 22:08:39 +0900

    feat: 🎸 추가 로또 형태 확인 메서드
    쉼표를 기준으로 6개의 값이 나오지 않으면 로또 숫자 형태가 아님을 판단하는 메서드

--- src/main/java/lotto/Validate.java
+++ src/main/java/lotto/Validate.java
@@ -1,17 +1,23 @@
 package lotto;
 
 import constant.Amount;
+import constant.LottoRule;
 import constant.LottoText;
 
 public class Validate {
-
-//    NOT_LOTTO_FORM("입력값을 로또 형태로 전환할 수 없습니다."),
 //    DUPLICATE_LOTTO("중복된 값이 있습니다."),
 //    DUPLICATE_BONUS("보너스 번호가 당첨 번호와 중복되었습니다."),
     public void validDivisible(int purchasePrice) {
         if (purchasePrice % Amount.UNIT.getPrice() != 0) {
             throw new IllegalArgumentException(
                     LottoText.ERROR + "입력값이 " + Amount.UNIT + "으로 나누어떨어지지 않습니다.");
+        }
+    }
+
+    public void validWinLottoForm(String[] splitInput) {
+        if (splitInput.length != LottoRule.COUNT.getValue()) {
+            throw new IllegalArgumentException(
+                    LottoText.ERROR + "입력값을 로또 형태로 전환할 수 없습니다.");
         }
     }
 }
