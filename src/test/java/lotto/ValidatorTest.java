9c229a91a181864e8c12500e63b115cf8dc7856e
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-10 12:49:52 +0900

    test(ValidatorTest): build skeleton test code
    Throw exception when value is:
        - not integer
        - zero
        - is negative
        - starting with zero
        - wrong unit
    Success when no exception thrown.

---
+++ src/test/java/lotto/ValidatorTest.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+class ValidatorTest {
+
+    @Test
+    void validateAmountOfPurchaseMoney_isNotInteger() {
+
+    }
+
+    @Test
+    void validateAmountOfPurchaseMoney_isZero() {
+
+    }
+
+    @Test
+    void validateAmountOfPurchaseMoney_isNegative() {
+
+    }
+
+    @Test
+    void validateAmountOfPurchaseMoney_startingWithZero() {
+
+    }
+
+    @Test
+    void validateAmountOfPurchaseMoney_wrongUnit() {
+
+    }
+
+    @Test
+    void validateAmountOfPurchaseMoney_success() {
+
+    }
+}
