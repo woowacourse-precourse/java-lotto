08b322887a019037f658ce031b69ba9078d3aa28
Donghyun <flaehan@gmail.com>
2022-11-10 16:38:06 +0900

    type : feat 로또 번호 정렬
    body : 생성된 로또 번호 정렬하는 기능 추가
    
    footer :

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,9 @@ package lotto;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -16,7 +19,8 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    public int inputMoney() throws IOException { // 로또 구입 금액
+    // 로또 구입 금액
+    public static int inputMoney() throws IOException {
         System.out.println("구입금액을 입력해 주세요.");
         int inputMoney = Integer.parseInt(br.readLine());
         if(inputMoney % 1000 != 0){
@@ -25,18 +29,27 @@ public class Lotto {
         return inputMoney;
     }
 
-    public int theNumberOfLotto() throws IOException { // 로또 개수
+    // 로또 개수
+    public static int theNumberOfLotto() throws IOException {
         int inputMoney = inputMoney() / 1000;
         return inputMoney;
     }
 
+    public static ArrayList<Integer> myLottoNumber(int num) throws IOException {
+        num = theNumberOfLotto();
+
+        Collections.sort(myLottoNumber(num)); // 로또 번호 정렬
+        return null;
+    }
+
+    // 예외사항
     private void validate(List<Integer> numbers) throws IOException { // 예외사항 기존 코드
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("로또 숫자는 여섯자리여야 합니다.");
         }
 
         if (inputMoney() % 1000 != 0) {
-            throw new IllegalArgumentException("로또 구입 금액은 1,000원 단위입니다.");
+            throw new IllegalArgumentException("로또 구입 금액은 1,000원 단위입니다."); // 추후에 삭제 예정
         }
 
 //        if(Lotto 중복) {
@@ -44,5 +57,4 @@ public class Lotto {
 //        }
     }
 
-    // TODO: 추가 기능 구현
 }
