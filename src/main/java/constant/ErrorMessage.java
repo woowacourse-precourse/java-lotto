f31c1e948287ebc77ed8d4002b44f99a69cd2d96
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-10 13:21:16 +0900

    feat(lotto): 예외 출력을 위한 Enum 생성

---
+++ src/main/java/constant/ErrorMessage.java
@@ -0,0 +1,19 @@
+package constant;
+
+public enum ErrorMessage {
+
+
+    ERROR_NOT_NUMBER("[ERROR] 숫자가 아닌 것이 입력되었습니다"),
+    ERROR_INCORRECT_MONEY("[ERROR] 올바르지 못한 입력 금액입니다.");
+
+   private final String message;
+
+    ErrorMessage(String message) {
+        this.message=message;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+}
