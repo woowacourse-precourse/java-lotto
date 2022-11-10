37c8730008497098e8ec221b2f409a63c9e991d3
김경훈 <54941130+ouvir@users.noreply.github.com>
2022-11-10 22:01:19 +0900

    feat: add method of Lotto
    validate method:
    validateSize()
    validateOverlap()
    validateOutOfRange()
    check the numbers of Lotto
    
    get number of Lotto method:
    getLottoNumbers()
    get numbers of Lotto

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,40 @@
 package lotto;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateOverlap(numbers);
+        validateOutOfRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateOverlap(List<Integer> numbers) {
+        Set<Integer> removeOverlap = new HashSet<>(numbers);
+        if (numbers.size() != removeOverlap.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateOutOfRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number > 45 || number < 1) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return this.numbers;
+    }
+
 }
