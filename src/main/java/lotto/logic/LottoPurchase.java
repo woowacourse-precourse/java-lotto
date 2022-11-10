2dfa548c3c04fa7b3560ae209c466fb30b0736dd
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-10 20:04:29 +0900

    feat: 구현기능 목록 1.1번 로또 구입을 위한 금액입력 기능을 추가함

---
+++ src/main/java/lotto/logic/LottoPurchase.java
@@ -0,0 +1,14 @@
+package lotto.logic;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoPurchase {
+
+    public String inputAmount () {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        return input;
+        }
+    }
+
+
