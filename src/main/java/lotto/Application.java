eeba79f6f345cd49b902c4a392432dc6b30d205e
Sunghyun-0 <zkdldk95@naver.com>
2022-11-10 23:50:15 +0900

    로또 생성 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static boolean validationCheck(int money){
         return money % 1000 == 0;
+    }
+
+    public static List<Lotto> createLottos(int money){
+        int numberOfGames = money / 1000;
+        List<Lotto> Lottos = new ArrayList<>();
+
+        for(int i=0; i<numberOfGames; i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            Lottos.add(lotto);
+        }
+        return Lottos;
     }
 
     public static void main(String[] args) {
