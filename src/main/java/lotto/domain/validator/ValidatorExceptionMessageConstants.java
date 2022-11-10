33ecb17231ae68eff4ceecfe64800f6782e7c63b
JunH <tjdtls690@gmail.com>
2022-11-11 00:04:57 +0900

    :art: refactor(Domain : ValidatorExceptionMessageConstants) : 유틸 클래스 생성을 막기위해 private 생성자 구현

--- src/main/java/lotto/domain/validator/ValidatorExceptionMessageConstants.java
+++ src/main/java/lotto/domain/validator/ValidatorExceptionMessageConstants.java
@@ -2,4 +2,8 @@ package lotto.domain.validator;
 
 public class ValidatorExceptionMessageConstants {
     public static final String UTILITY_CLASS_CREATE_EXCEPTION_MESSAGE = "생성할 수 없는 유틸 클래스입니다.";
+    
+    private ValidatorExceptionMessageConstants() {
+        throw new IllegalStateException(UTILITY_CLASS_CREATE_EXCEPTION_MESSAGE);
+    }
 }
