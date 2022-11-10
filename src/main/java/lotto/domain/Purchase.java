362cde448a7ed50b2bf8c05c93c7312b7cc1cb4b
jeomxon <jeomxon@gmail.com>
2022-11-11 02:53:08 +0900

    feat: 구입금액을 입력받는 기능 구현

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -1,32 +1,25 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import camp.nextstep.edu.missionutils.Console;
 
 public class Purchase {
     private int money = 0;
-//    private List<List<Integer>> totalLotto = new ArrayList<>();
-    private static final String PURCHASE_AMOUNT_ERROR = "[ERROR] 구입 금액은 1,000원 단위여야 합니다.";
     private static final String INPUT_PURCHASE_AMOUNT = "구매금액을 입력해 주세요.";
+    private static final String PURCHASE_AMOUNT_ERROR = "[ERROR] 구입 금액은 1,000원 단위여야 합니다.";
 
-    public Purchase(int money) {
-        validate(money);
-        this.money = money;
+    public int inputMoney() {
+        System.out.println(INPUT_PURCHASE_AMOUNT);
+        int input = Integer.parseInt(Console.readLine());
+
+        validate(input);
+
+        return numOfLotto(input);
     }
 
     private void validate(int money) {
         if (money % 1000 != 0) {
             throw new IllegalArgumentException(PURCHASE_AMOUNT_ERROR);
         }
-    }
-
-    public void inputMoney() {
-        System.out.println(INPUT_PURCHASE_AMOUNT);
-        int input = Integer.parseInt(Console.readLine());
-
-        numOfLotto(input);
     }
 
     private int numOfLotto(int money) {
