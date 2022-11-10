b82b8a1aad671f15b8a83f29f0dfb5cdf5e7b987
whibaek <jibaek0513@gmail.com>
2022-11-11 02:13:41 +0900

    feat(Result): 총 당첨결과를 출력하는 기능 추가 - public void showResult() 추가

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -42,4 +42,13 @@ public class Result {
     public int getFifth() {
         return fifth;
     }
+
+    public void showResult(){
+        System.out.println("3개 일치 (5,000원) - " + fifth + "개");
+        System.out.println("4개 일치 (50,000원) - " + fourth + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + third + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + second + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + first + "개");
+        System.out.println("총 수익률은 " + earningsRate + "입니다.");
+    }
 }
