ba371f009e0f0617d73a86c4ac4058700dbe8f05
JHS <wnghdtjr129@naver.com>
2022-11-10 13:38:50 +0900

    test(LottoWinningNumberTest): 당첨 결과 확인 테스트 코드 작성

--- src/test/java/lotto/domain/LottoWinningNumberTest.java
+++ src/test/java/lotto/domain/LottoWinningNumberTest.java
@@ -5,13 +5,14 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.domain.LottoRank.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoWinningNumberTest {
     @Test
     @DisplayName("당첨 번호, 보너스 번호 입력 성공")
-    void input_WinningNumber_And_BonusNumber_Success(){
+    void input_WinningNumber_And_BonusNumber_Success() {
         List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
         String bonus = "7";
 
@@ -21,7 +22,7 @@ class LottoWinningNumberTest {
 
     @Test
     @DisplayName("당첨 번호 중복 입력 실패")
-    void input_WinningNumber_Duplicated_Fail(){
+    void input_WinningNumber_Duplicated_Fail() {
         List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 5);
         String bonus = "7";
 
@@ -31,7 +32,7 @@ class LottoWinningNumberTest {
 
     @Test
     @DisplayName("당첨 번호 개수 부족 실패")
-    void input_WinningNumber_CountLack_Fail(){
+    void input_WinningNumber_CountLack_Fail() {
         List<Integer> winningNumber = List.of(1, 2, 3, 4, 5);
         String bonus = "5";
 
@@ -41,7 +42,7 @@ class LottoWinningNumberTest {
 
     @Test
     @DisplayName("보너스 번호 중복 입력 실패")
-    void input_BonusNumber_Duplicated_Fail(){
+    void input_BonusNumber_Duplicated_Fail() {
         List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
         String bonus = "5";
 
@@ -51,11 +52,37 @@ class LottoWinningNumberTest {
 
     @Test
     @DisplayName("보너스 번호 문자 입력 실패")
-    void input_BonusNumber_NoNumber_Fail(){
+    void input_BonusNumber_NoNumber_Fail() {
         List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
         String bonus = "a";
 
         assertThatThrownBy(() -> new LottoWinningNumber(winningNumber, bonus))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("당첨 결과 확인")
+    void get_Lotto_Result() {
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        String bonus = "7";
+
+        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber(winningNumber, bonus);
+        IssuedLotto issuedLotto = new IssuedLotto(List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 7)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 8)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 11)),
+                new Lotto(List.of(1, 2, 3, 4, 8, 9)),
+                new Lotto(List.of(1, 2, 3, 7, 8, 9)),
+                new Lotto(List.of(1, 2, 8, 9, 10, 11))
+
+        ));
+        WinningResult result = lottoWinningNumber.getResult(issuedLotto);
+        assertThat(result.getWinningCount(FIRST)).isEqualTo(2);
+        assertThat(result.getWinningCount(SECOND)).isEqualTo(1);
+        assertThat(result.getWinningCount(THIRD)).isEqualTo(2);
+        assertThat(result.getWinningCount(FOURTH)).isEqualTo(1);
+        assertThat(result.getWinningCount(FIFTH)).isEqualTo(1);
     }
 }
