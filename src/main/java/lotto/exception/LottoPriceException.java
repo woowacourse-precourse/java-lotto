1082de62d42fe3024472efc2fe52c15da9cc6221
YuHayeon <youha0930@gmail.com>
2022-11-10 21:54:37 +0900

    feat(LottoPriceException): 로또 금액에 공백이 있을 경우 예외처리
    로또 구입 금액의 공백이 있다면 예외처리를 해준 후 공백없이 입력해달라는 메시지를 출력

--- src/main/java/lotto/exception/LottoPriceException.java
+++ src/main/java/lotto/exception/LottoPriceException.java
@@ -4,11 +4,18 @@ public class LottoPriceException {
 
     public static void checkLottoPrice(String priceInput) {
         checkEmptyLottoPrice(priceInput);
+        checkHasBlankInLottoPrice(priceInput);
     }
 
     public static void checkEmptyLottoPrice(String price) {
         if (price.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 로또 금액을 입력해주세요.");
+        }
+    }
+
+    public static void checkHasBlankInLottoPrice(String price) {
+        if (!price.equals(price.trim())) {
+            throw new IllegalArgumentException("[ERROR] 로또 금액을 공백없이 입력해주세요.");
         }
     }
 }
