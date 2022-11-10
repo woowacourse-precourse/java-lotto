bc0ad6ae2a44ab721a06e590b8d042b47dbfe4d2
Reznov <reznov03048@gmail.com>
2022-11-10 20:15:36 +0900

    feat: 로또 티켓들을 생성하는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.domain.LottoTicket;
 import lotto.domain.PurchaseAmount;
 import lotto.view.Input;
 
@@ -9,7 +10,7 @@ public class LottoController {
         // 금액 입력 받음
         PurchaseAmount purchaseAmount = inputPurchaseAmount();
         // 로또 n장 생성 (1장 당 6자리 숫자 랜덤 생성) -> Lotto.java
-
+        LottoTicket lottoTicket = getTickets(purchaseAmount);
         // 당첨 번호 입력 받음
 
         // 결과 생성
@@ -23,5 +24,13 @@ public class LottoController {
 
     private int calculateTicketNumber(PurchaseAmount purchaseAmount) {
         return purchaseAmount.getPurchaseAmount() / purchaseAmount.getUNIT_PRICE();
+    }
+
+    private LottoTicket getTickets(PurchaseAmount purchaseAmount) {
+        int ticketNumber = calculateTicketNumber(purchaseAmount);
+
+        LottoTicket lottoTicket = new LottoTicket(ticketNumber);
+
+        return lottoTicket;
     }
 }
