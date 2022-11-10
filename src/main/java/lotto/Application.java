5b394451d062f87b0c7db74d119fc36afc4150eb
49ehyeon42 <49ehyeon42@gmail.com>
2022-11-10 23:38:39 +0900

    refactor: 구입 금액 문자열을 검증 후 로또 수량을 반환하는 메서드 리팩터링

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,41 +7,27 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
 
-        int lottoCount = getLottoCount(Console.readLine());
+        try {
+            int lottoCount = getLottoCount(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
     }
 
-    public static int getLottoCount(String string) {
-        if (!isNumber(string)) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액이 숫자 형식이 아닙니다.");
+    public static int getLottoCount(String string) throws IllegalArgumentException {
+        int number;
+        try {
+            number = Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액이 숫자 형식 또는 정수가 아닙니다.");
         }
-
-        int number = Integer.parseInt(string);
-
-        if (!isPositiveNumber(number)) {
+        if (number <= 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액이 양수가 아닙니다.");
         }
-
-        if (!isDividedBy1000(number)) {
+        if (number % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액이 1,000원으로 나누어떨어지지 않습니다.");
         }
-
         return number / 1000;
-    }
-
-    private static boolean isNumber(String string) {
-        try {
-            Integer.parseInt(string);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
-    private static boolean isPositiveNumber(int number) {
-        return number > 0;
-    }
-
-    private static boolean isDividedBy1000(int number) {
-        return number % 1000 == 0;
     }
 }
