5fa7558ac6e75a294fcfe3ea2f26b64f45d1d6e4
KimJaeSung <behappy0916@naver.com>
2022-11-10 22:50:21 +0900

    feat: 각 숫자 중복 예외 기능 구현
    중복이 있는 지 판별하는 기능 구현
    로또 형식이 일치하는 지 판별하는 기능 구현
    로또 사이즈가 맞는 지 판별하는 기능 구현

---
+++ src/main/java/lotto/exception/Exceptions.java
@@ -0,0 +1,43 @@
+package lotto.exception;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Exceptions {
+    private static final int COUNT_NUMBER = 1;
+    private static final int LOTTO_SIZE = 6;
+
+    static List<String> numbers;
+
+    public static boolean isOverLapNumber(String request){
+        numbers = new ArrayList<String>(Arrays.asList(request.split(",")));
+        for(int i=0; i<numbers.size(); i++){
+            int numberCount = Collections.frequency(numbers, numbers.get(i));
+            if(numberCount > COUNT_NUMBER){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isRightLottoNumbersType(String request){
+        for(int i=0; i<request.length(); i++){
+            if((i%2==0 && !Character.isDigit(request.charAt(i))) ||
+                    (i%2!=0 && request.charAt(i) != 44 )){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isCorrectSize(String request){
+        numbers = new ArrayList<String>(Arrays.asList(request.split(",")));
+        if(numbers.size() != LOTTO_SIZE){
+            return false;
+        }
+        return true;
+    }
+}
