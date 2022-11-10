1598314b6d25d0cccaa4d95a5010e72d561782f9
kimminsu <xjfcnfw3@naver.com>
2022-11-10 09:06:56 +0900

    test: 로또 번호 입력 형식 예외 테스트

--- src/test/java/lotto/user/UserTest.java
+++ src/test/java/lotto/user/UserTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.List;
+import lotto.user.validation.ExceptionMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -54,5 +55,33 @@ public class UserTest {
         assertThatThrownBy(User::inputLottoNumbers)
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining(ERROR);
+    }
+
+    @DisplayName("로또 번호를 ,로 구별하지 않으면 예외가 발생한다.")
+    @Test
+    void inputLottoNumbersByWrongFormat() {
+        //given
+        String userInput = "1,2,3,4|5,6";
+        InputStream in = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(in);
+
+        // when, then
+        assertThatThrownBy(User::inputLottoNumbers)
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ExceptionMessage.WRONG_FORMAT.toString());
+    }
+
+    @DisplayName("로또 번호는 숫자로 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void inputLottoNumbersByNotNumber() {
+        //given
+        String userInput = "1,2,3,4,5,six";
+        InputStream in = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(in);
+
+        // when, then
+        assertThatThrownBy(User::inputLottoNumbers)
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ExceptionMessage.WRONG_FORMAT.toString());
     }
 }
