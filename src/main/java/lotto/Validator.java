350e4f8c2d73d593a22cd6847833ceeddf81613e
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-10 11:03:14 +0900

    feat(Validator): build skeleton code
    Validator validates player's input.

---
+++ src/main/java/lotto/Validator.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import java.util.List;
+
+public class Validator {
+
+    public void validateAmountOfPurchaseMoney(int amountOfPurchaseMoney) {
+
+    }
+
+    public void validateWinningNumbers(List<Integer> winningNumbers) {
+
+    }
+
+    public void validateBonusNumber(int bonusNumber) {
+
+    }
+}
