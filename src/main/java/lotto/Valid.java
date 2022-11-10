c89263139c22644b886068f920b2769a9ef63efa
young2866 <62212610+young2866@users.noreply.github.com>
2022-11-11 03:24:00 +0900

    feat(Valid): 사용자가 입력한 구입금액이 0원인지 검증하는 기능 추가 및 오류 메세지 수정

--- src/main/java/lotto/Valid.java
+++ src/main/java/lotto/Valid.java
@@ -3,13 +3,19 @@ package lotto;
 public class Valid {
 	private static final int LOTTO_PRICE = 1000;
 	private static final int ZERO = 0;
-	private static final String INPUT_PURCHASE_AMOUNT_ERROR = "[ERROR]구입금액 입력이 잘못 되었습니다.";
+	private static final String NOT_DIVIDED_BY_1000_ERROR = "[ERROR]구입금액이 1000원 단위가 아닙니다.";
+	private static final String PURCHASE_AMOUNT_IS_ZERO_ERROR = "[ERROR]구입 금액이 0원입니다.";
+	private static final String INPUT_IS_NOT_NUMBER_ERROR = "[ERROR]구입금액이 숫자가 아닙니다.";
 
 	public static int validatePurchaseAmount(String purchaseAmount) {
 		int verification = isNumber(purchaseAmount);
+
+		if (isZero(verification)) {
+			throw new IllegalArgumentException(PURCHASE_AMOUNT_IS_ZERO_ERROR);
+		}
 		if(isDividedBy1000(verification))
 			return verification;
-		throw new IllegalArgumentException(INPUT_PURCHASE_AMOUNT_ERROR);
+		throw new IllegalArgumentException(NOT_DIVIDED_BY_1000_ERROR);
 	}
 
 	private static boolean isDividedBy1000(int verification) {
@@ -22,7 +28,13 @@ public class Valid {
 		if (isNumeric(purchaseAmount)) {
 			return Integer.parseInt(purchaseAmount);
 		}
-		throw new IllegalArgumentException(INPUT_PURCHASE_AMOUNT_ERROR);
+		throw new IllegalArgumentException(INPUT_IS_NOT_NUMBER_ERROR);
+	}
+	private static boolean isZero(int purchaseAmount) {
+		if (purchaseAmount == ZERO) {
+			return true;
+		}
+		return false;
 	}
 
 	private static boolean isNumeric(String str) {
