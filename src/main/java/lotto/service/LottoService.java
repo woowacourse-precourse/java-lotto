1d5fa4774173d7d8ca8fb6a681b4fca5d0b88d2b
김태용 <amico741@naver.com>
2022-11-11 02:00:00 +0900

    fix: 0이 입력되어도 예외가 발생하지 않는 버그 수정
    0으로 입력된 금액 또한 예외 처리가 발생해야 한다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -4,13 +4,15 @@ import lotto.domain.Buyer;
 
 public class LottoService {
     public static final int STANDARD_WON = 1000;
+    public static final int ZERO_WON = 0;
     public static final String NOT_MOD_ZERO_MESSAGE = "[ERROR] 1000으로 나누어지는 금액이 아닙니다.";
-
+    public static final String REJECT_ZERO = "[ERROR] 구입 금액에 0을 입력할 수 없습니다.";
+    
     public LottoService() { }
 
     public void validateMoney(int money) {
-        if(Math.floorMod(Math.round(money), STANDARD_WON) != 0) {
-            throw new IllegalArgumentException(NOT_MOD_ZERO_MESSAGE);
-        }
+        if(money == ZERO_WON) { throw new IllegalArgumentException(REJECT_ZERO); }
+
+        if(Math.floorMod(Math.round(money), STANDARD_WON) != 0) { throw new IllegalArgumentException(NOT_MOD_ZERO_MESSAGE); }
     }
 }
