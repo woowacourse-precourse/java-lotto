3e7fa46e53eea6489a8e3e67382780f278ccbeb3
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-10 19:51:23 +0900

    feat: add validate payment input

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import java.util.Scanner;
+
 public class Application {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int payment = sc.nextInt();
+        validatePayment(payment);
+    }
+    public static void validatePayment(int payment){
+        if(payment <= 0 || payment%1000 != 0)
+            throw new IllegalArgumentException();
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -26,9 +26,8 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != 6)
             throw new IllegalArgumentException();
-        }
     }
 
     // TODO: 추가 기능 구현
