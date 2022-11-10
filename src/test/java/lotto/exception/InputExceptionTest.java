17c4acd30c6eabb630f21e7705d134cacc1f3a77
KIMSEI1124 <workju1124@gmail.com>
2022-11-10 14:31:18 +0900

    test: InputExceptionTest Create
    숫자가 아닌 문자열 입력시 예외 발생 테스트 생성하여 오류 확인
    
    @1 : 입력 문자에 공백과 특수 문자 포함시 다른 예외 발생
    Fixed : @1

---
+++ src/test/java/lotto/exception/InputExceptionTest.java
@@ -0,0 +1,41 @@
+package lotto.exception;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class InputExceptionTest {
+
+    private final inputException inputException = new inputException();
+
+    @Nested
+    @DisplayName("숫자가 아니여서 예외 처리")
+    class notDigit {
+        @ParameterizedTest
+        @CsvSource({
+                "looo", "ㅇㅇㅇㅇ",
+                "일공공공", "100o",
+                "100공", "100ㅇ",
+                "100 ", "10,0"
+        })
+        void case1(String inputData) {
+            Assertions.assertThatThrownBy(() -> inputException.notDigitExcept(inputData))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("숫자여서 정상 실행")
+    class digit {
+        @ParameterizedTest
+        @CsvSource({
+                "1000", "2000", "10000"
+        })
+        void case1(String inputData) {
+            Assertions.assertThatCode(() -> inputException.notDigitExcept(inputData))
+                    .doesNotThrowAnyException();
+        }
+    }
+}
