c9ba1d1c64077e96ee97c7eae20e4d4824156129
JunH <tjdtls690@gmail.com>
2022-11-10 21:33:07 +0900

    :white_check_mark: test(Domain : PaymentTest) : 구매 금액 예외처리 테스트 추가

--- src/test/java/lotto/domain/PaymentTest.java
+++ src/test/java/lotto/domain/PaymentTest.java
@@ -1,9 +1,12 @@
 package lotto.domain;
 
+import lotto.domain.validator.ErrorMessageConstant;
+import lotto.domain.validator.PaymentValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class PaymentTest {
     @Test
@@ -11,5 +14,21 @@ class PaymentTest {
     void countOfPurchasedLotto() {
         final Payment payment = new Payment(14000);
         assertThat(payment.countOfPurchasedLotto()).isEqualTo(14);
+    }
+    
+    @Test
+    @DisplayName("예외 처리 : 1000 단위가 아닌 경우")
+    void erroneousPaymentException() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Payment(14500))
+                .withMessageStartingWith(ErrorMessageConstant.ERROR_MESSAGE);
+    }
+    
+    @Test
+    @DisplayName("예외 처리 : 0인 경우")
+    void isPaymentZero() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Payment(0))
+                .withMessageStartingWith(ErrorMessageConstant.ERROR_MESSAGE);
     }
 }
