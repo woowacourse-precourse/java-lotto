b3a278e68103be7c9ab332dcc6fe59fc7cbbe010
eora21 <eora21@naver.com>
2022-11-10 14:39:10 +0900

    refactor: 💡 변경 나누어떨어지지 않는다는 의미의 NOT_DIVISIBLE로 이름 변경

--- src/main/java/constant/Invalid.java
+++ src/main/java/constant/Invalid.java
@@ -3,7 +3,7 @@ package constant;
 public enum Invalid {
     NULL("입력값은 null일 수 없습니다."),
     NOT_INT("입력값이 정수가 아닙니다."),
-    CAN_NOT_BE_DIVIDED(Amount.UNIT + "로 나눌 수 없습니다."),
+    NOT_DIVISIBLE(Amount.UNIT + "로 나눌 수 없습니다."),
     OVER_MATCH_ALL_AMOUNT("입력 금액은 1등 당첨금보다 높게 설정할 수 없습니다."),
     NOT_LOTTO_FORM("입력값을 로또 형태로 전환할 수 없습니다."),
     DUPLICATE_LOTTO("중복된 값이 있습니다."),
