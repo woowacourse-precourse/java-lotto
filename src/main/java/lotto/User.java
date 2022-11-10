afc2ba0462b3347bade54388549dc772b67639b8
whipbaek <jibaek0513@gmail.com>
2022-11-10 19:35:02 +0900

    refactor(User): 하드코딩된 숫자들 상수 변수로 변경

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -7,6 +7,11 @@ import java.util.stream.Collectors;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class User {
+    static final int UNIT = 1000;
+    static final int LOTTO_LEN = 6;
+    static final int LOTTO_MAX_SIZE = 45;
+    static final int LOTTO_MIN_SIZE = 1;
+
     private int userMoney = 0;
     private int resultMoney = 0;
     private int numOfLotto = 0;
@@ -19,7 +24,7 @@ public class User {
     }
 
     private void validateInputNumber(int inputMoney) {
-        if(inputMoney%1000 != 0){
+        if(inputMoney%UNIT != 0){
             System.out.println("[ERROR] 금액은 1000원 단위로 나뉘어 떨어져야 합니다.");
             throw new IllegalArgumentException();
         }
@@ -40,14 +45,14 @@ public class User {
 
     private void validateInputLottoNumber(String inputLottoNumber) {
         ArrayList<String> split = new ArrayList<>(List.of(inputLottoNumber.split(", ")));
-        if(split.size() != 6){
+        if(split.size() != LOTTO_LEN){
             System.out.println("[ERROR] 숫자 입력이 잘못 되었습니다.");
             throw new IllegalArgumentException();
         }
         List<Integer> lottoNumber = convertLottoNumber(inputLottoNumber);
 
         for (Integer integer : lottoNumber) {
-            if(integer > 45 || integer < 1){
+            if(integer > LOTTO_MAX_SIZE || integer < LOTTO_MIN_SIZE){
                 System.out.println("[ERROR] 숫자는 1이상 45 이하의 숫자로 구성되어야 합니다.");
                 throw new IllegalArgumentException();
             }
@@ -62,7 +67,7 @@ public class User {
 
     public void setNumOfLotto(){
         if(userMoney != 0){
-            numOfLotto = userMoney/1000;
+            numOfLotto = userMoney/UNIT;
         }
     }
 }
