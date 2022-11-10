74d4cf87fb3beacb2e3293952d603816ab270ed4
Kwak-JunYoung <klop9090@g.skku.edu>
2022-11-10 23:15:55 +0900

    feat(lotto/Lotto.java) Rank-calculating method implemented
    Method that calculates the matches between the user's ticket number, the winning number, and the bonus number to calculate the user's winning rank is implemented

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,6 @@
 package lotto;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -18,12 +16,31 @@ public class Lotto {
         }
 
         for (int i = 1; i <= 45; i++)
-            if(Collections.frequency(numbers, i) >= 2)
-                throw new IllegalArgumentException();
+            if (Collections.frequency(numbers, i) >= 2) throw new IllegalArgumentException();
 
         for (int i = 0; i < 6; i++)
-            if(numbers.get(i) < 1 || numbers.get(i) > 45)
-                throw new IllegalArgumentException();
+            if (numbers.get(i) < 1 || numbers.get(i) > 45) throw new IllegalArgumentException();
     }
+
+    private int get_rank(Lotto win_number, int bonus) {
+        int count = 0;
+        boolean bonus_match = false;
+        for (int i = 0; i < 6; i++) {
+            if (Objects.equals(this.numbers.get(i), win_number.numbers.get(i))) {
+                count += 1;
+            }
+            if (bonus == this.numbers.get(i)) bonus_match = true;
+        }
+
+        if (count == 5) {
+            if (bonus_match) return 2;
+            return 3;
+        }
+        if (count == 6) {
+            return 1;
+        }
+        return 8 - count;
+    }
+
     // TODO: 추가 기능 구현
 }
