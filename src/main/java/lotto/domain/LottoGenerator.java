dc8e63e4a220e38489cb83235d0fc7db17b2e6f3
jeomxon <jeomxon@gmail.com>
2022-11-11 02:45:56 +0900

    feat: 금액에 맞는 로또 생성기 구현

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class LottoGenerator {
+    private static final int STARTING_INDEX = 0;
+    private final List<List<Integer>> totalLottos = new ArrayList<>();
+
+    public LottoGenerator(int numOfLotto) {
+
+        for (int i = STARTING_INDEX; i < numOfLotto; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            totalLottos.add(lotto.getNumbers());
+        }
+    }
+}
