00e8fbbe90afbc2b8839dfb364d0216b74bb5242
2jun0 <soo28819@gmail.com>
2022-11-11 04:06:33 +0900

    Feat(LottoGame): Lotto의 상수 public으로 수정 (WinningNumbers에서 사용해야 함)

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -6,8 +6,8 @@ import java.util.StringJoiner;
 
 public class Lotto {
     public static final int NUMBER_COUNT = 6;
-    static final int MIN_NUMBER = 1;
-    static final int MAX_NUMBER = 45;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
 
     private final List<Integer> numbers;
 
