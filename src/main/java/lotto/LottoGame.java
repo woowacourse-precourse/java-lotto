a86d63b4821f8cabe6ce4f36ab8a4675640d4d48
rt3310 <btac3310@gmail.com>
2022-11-10 18:24:15 +0900

    feat: 돈을 입력하는 기능 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,4 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class LottoGame {
+
+    public String inputMoney() {
+        String money = Console.readLine();
+        return money;
+    }
 }
