815917b235f9124251b1cc12a9bf35f1ba264d9e
HubCreator <chocomilk8604@gmail.com>
2022-11-10 11:29:08 +0900

    refactor(Lotto) : ValidationUtil의 검증로직을 Lotto 클래스로 이동

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,12 @@
 package lotto;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int LOTTO_COUNT = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +15,31 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        isValidCount(numbers);
+        isValidNumber(numbers);
+        hasDistinctNumbers(numbers);
+    }
+
+    private static void isValidNumber(List<Integer> numbers) {
+        Optional<Integer> result = numbers.stream()
+                .filter(number -> number < MIN_NUMBER || number > MAX_NUMBER)
+                .findFirst();
+
+        if (result.isPresent()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER.getValue());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private static void isValidCount(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_COUNT.getValue());
+        }
+    }
+
+    private static void hasDistinctNumbers(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if (set.size() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.HAVE_DUPLICATED_NUMBER.getValue());
+        }
+    }
 }
--- src/main/java/lotto/ValidationUtil.java
+++ src/main/java/lotto/ValidationUtil.java
@@ -1,50 +1,10 @@
 package lotto;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 public class ValidationUtil {
-
-    public static final int MIN_NUMBER = 1;
-    public static final int MAX_NUMBER = 45;
-    public static final int LOTTO_COUNT = 6;
 
     private static void isValidInput(String str) {
         if (!str.matches("[0-9]|,")) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getValue());
         }
-    }
-
-    public static void isValidNumber(String str) {
-        int number = Integer.parseInt(str);
-        if (number < MIN_NUMBER || number > MAX_NUMBER) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER.getValue());
-        }
-    }
-
-    public static void isValidCount(String[] array) {
-        if (array.length != LOTTO_COUNT) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_COUNT.getValue());
-        }
-    }
-
-    private static void hasDistinctNumbers(String[] array) {
-        Set<String> set = new HashSet<>(Arrays.asList(array));
-        if (set.size() != array.length) {
-            throw new IllegalArgumentException(ErrorMessage.HAVE_DUPLICATED_NUMBER.getValue());
-        }
-    }
-
-    public static boolean isValid(String str) {
-        isValidInput(str);
-
-        String[] split = str.split(",");
-        isValidCount(split);
-        for (String s : split) {
-            isValidNumber(s);
-        }
-        hasDistinctNumbers(split);
-        return true;
     }
 }
