f61252985af0b8145dd6f18087b532fed6a6bcee
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-11 02:03:03 +0900

    feat(Lotto): 로또 생성 validation 추가
    - 중복안되는 6개의 숫자인지 확인
    - 범위가 1이상 45이하인지 확인

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
 
@@ -9,6 +11,12 @@ public class Lotto {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
     public static final int NUMBER_COUNT = 6;
+
+    public static final String NUMBER_INPUT_COUNT_ERROR_MESSAGE =
+            "[ERROR] 로또 번호는 중복되지 않는 " + NUMBER_COUNT + "개의 숫자여야 합니다.";
+    public static final String NUMBER_INPUT_RANGE_ERROR_MESSAGE =
+            "[ERROR] 로또 번호는 " + MIN_NUMBER + "부터 " + MAX_NUMBER + " 사이의 숫자여야 합니다.";
+
 
     private final List<Integer> numbers;
 
@@ -19,8 +27,12 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        Set<Integer> nonDuplicatedNumbers = new HashSet<>(numbers);
+        if (nonDuplicatedNumbers.size() != 6) {
+            throw new IllegalArgumentException(NUMBER_INPUT_COUNT_ERROR_MESSAGE);
+        }
+        if (numbers.get(1) < MIN_NUMBER || numbers.get(NUMBER_COUNT - 1) > MAX_NUMBER) {
+            throw new IllegalArgumentException(NUMBER_INPUT_RANGE_ERROR_MESSAGE);
         }
     }
 
