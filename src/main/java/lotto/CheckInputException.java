d72e88ee528cbf73b58be7614a86ef740d562625
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-10 23:13:51 +0900

    Docs(README.md) : 기능 목록 추가 #8
    Util클래스를 추가했습니다.
    문자열이 숫자열인지 아닌지 판별하는 isNumberic()메소드를 추가했습니다.

--- src/main/java/lotto/CheckInputException.java
+++ src/main/java/lotto/CheckInputException.java
@@ -2,13 +2,17 @@ package lotto;
 
 class CheckInputException {
 
-    public static void checkBuyerInputIsMinus(String inputMoney) throws IllegalArgumentException{
+    public static void checkBuyerInputIsMinus(String input) throws IllegalArgumentException{
 
-        Long money = Long.valueOf(inputMoney);
+        Long money = Long.valueOf(input);
 
         if (money < 0) {
             throw new IllegalArgumentException("[ERROR] 입력하신 돈이 음수입니다.");
         }
+    }
+
+    public static void checkBuyerInputIsNotNumber(String input) throws IllegalArgumentException{
+        input.chars().allMatch(Character::isDigit);
     }
 
 
