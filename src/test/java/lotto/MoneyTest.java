f1f670815b28abfbdbf618701c0177c4c4b736e7
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-10 18:15:38 +0900

    test: 입력 금액에 맞는 구매가능한 로또의 갯수 반환 함수 작성

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MoneyTest {
@@ -22,5 +23,10 @@ class MoneyTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("옳바른 금액이 입력되어 구매 가능한 로또의 갯수가 반환되는지 확인한다.")
+    @Test
+    void createQuotientByMoney() {
+        Money money = new Money("13000");
+        assertThat(money.getLottoCount()).isEqualTo(13);
+    }
 }
