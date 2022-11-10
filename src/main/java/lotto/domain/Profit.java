6dd098cee2d5c3b5895af807b094b2afe7feeb1e
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-10 12:59:24 +0900

    feat: 도메인 Profit과 테스트코드 클래스 생성

---
+++ src/main/java/lotto/domain/Profit.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+public class Profit {
+
+    private long payment;
+    private long price;
+
+    public Profit(){}
+
+    public void checkPayment(long payment){}
+
+    public void addPrice(long price){}
+
+    public float getProfit(){return 0;}
+
+}
---
+++ src/test/java/lotto/domain_test/ProfitTest.java
@@ -0,0 +1,26 @@
+package lotto.domain_test;
+
+import lotto.domain.Profit;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ProfitTest {
+
+    Profit profit;
+
+    @BeforeEach
+    void initializeClass(){
+        profit = new Profit();
+    }
+
+    @Test
+    @DisplayName("로또 구매비용과 당첨금을 입력하면 수익률을 퍼센트로 출력한다. ")
+    void getProfit(){
+        profit.checkPayment(10000);
+        profit.addPrice(5000);
+        assertThat(profit.getProfit()).isEqualTo(50.0);
+    }
+}
