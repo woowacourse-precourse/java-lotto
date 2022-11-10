17d01e48567cd501114326e53698630c24003edf
ywj9811 <ywj9811@naver.com>
2022-11-10 23:25:23 +0900

    test: LottoTest : 최대 최소값 예외 확인

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -18,8 +18,21 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 최대값이 맞지 않다")
+    @Test
+    void createLottoByOverMax() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 70)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 최대값이 맞지 않다")
+    @Test
+    void createLottoByLowerMin() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
