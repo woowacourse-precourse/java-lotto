09604d70e2a7211321ac3120df9c0a32c4671511
JunDonghyuk <84345881+jundonghyuk@users.noreply.github.com>
2022-11-10 21:52:33 +0900

    fix(Message): 에러메시지 포맷 수정
    변경 전
    - [ERROR]...
    변경 후
    - [ERROR] ...
    - 띄어쓰기 추가

--- src/main/java/lotto/Message/ErrorMessage.java
+++ src/main/java/lotto/Message/ErrorMessage.java
@@ -1,7 +1,7 @@
 package lotto.Message;
 
 public class ErrorMessage {
-    public static final String BASE_ERROR_MESSAGE = "[ERROR]";
+    public static final String BASE_ERROR_MESSAGE = "[ERROR] ";
     public static final String NUMBER_RANGE_ERROR_MESSAGE = BASE_ERROR_MESSAGE + "로또 번호는 1부터 45사이의 숫자여야 합니다.";
     public static final String MONEY_INPUT_ERROR_MESSAGE = BASE_ERROR_MESSAGE + "1000원 단위로 입력해야 합니다.";
     public static final String NON_NUMERIC_INPUT_ERROR_MESSAGE = BASE_ERROR_MESSAGE + "숫자를 입력해야 합니다.";
