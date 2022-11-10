477df8f140fa25ec0ca5bf81352617fb836ae6fb
wlsh44 <k87913j@gmail.com>
2022-11-11 02:44:04 +0900

    feat: 당첨 번호 입력 예외 상황 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -11,7 +11,8 @@ public class InputView {
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String PURCHASE_ERROR_MESSAGE = "구입 금액은 문자일 수 없습니다.";
     private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String WINNING_NUMBERS_ERROR_MESSAGE = "당첨 번호는 숫자여야 합니다.";
+    private static final String WINNING_NUMBERS_ERROR_MESSAGE = "올바른 당첨 번호 입력이 아닙니다.";
+    private static final String DELIMITER = ",";
 
     public static int inputPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
@@ -35,10 +36,17 @@ public class InputView {
 
     private static List<Integer> toIntegerList(String input) {
         try {
-            return Arrays.stream(input.split(","))
+            validateWinningNumbersInput(input);
+            return Arrays.stream(input.split(DELIMITER))
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
         } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(WINNING_NUMBERS_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateWinningNumbersInput(String input) {
+        if (input.endsWith(DELIMITER)) {
             throw new IllegalArgumentException(WINNING_NUMBERS_ERROR_MESSAGE);
         }
     }
