16b7a6cf8c3fea7a659b4dad379110df6aa819a6
이경범 <tkdwk567@naver.com>
2022-11-10 19:56:59 +0900

    refactor : 금액 입력 기능 예외 발생 add
    입력한 데이터가 숫자가 아닌 경우 IllegalArgumentException 발생

--- src/main/java/lotto/MemberWriteService.java
+++ src/main/java/lotto/MemberWriteService.java
@@ -2,19 +2,25 @@ package lotto;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class MemberWriteService {
 
     // 금액 입력 기능
     public int writePay(String writePay) {
-        try {
-            return Integer.parseInt(writePay);
-        } catch (NumberFormatException e) {
-            System.err.println(e.getMessage());
-            return 0;
+        // 사용자가 잘못 입력한 시 IllegalArgumentException 발생
+        validWritePay(writePay);
+        return Integer.parseInt(writePay);
+    }
+
+    private void validWritePay(String writePay) {
+        if (!isDigit(writePay)) {
+            throw new IllegalArgumentException("This Lotto Number is Exception !!");
         }
+    }
+
+    private boolean isDigit(String writePay) {
+        return writePay.chars().allMatch(Character::isDigit);
     }
 
     // 당첨 로또 입력 기능
