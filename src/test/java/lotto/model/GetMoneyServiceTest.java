c3d18d71eb98e8f728e273098dafa9f4ec9baddb
siwookim97 <tldn12gh@gmail.com>
2022-11-11 03:56:58 +0900

    test: 로또 구입 금액 서비스 관련 통합 기능 테스트
    로또 구입 금액 서비스 관련 통합 기능 테스트
     - GetMoneyService 기능의 예외처리를 테스트
     - IllegalArgumentException 발생시키는지 확인
     - [ERROR] 메시지를 포함하는지 확인

---
+++ src/test/java/lotto/model/GetMoneyServiceTest.java
@@ -0,0 +1,22 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class GetMoneyServiceTest {
+    @DisplayName("구입금액의 예외처리 테스트")
+    @Test
+    void getMoneyTest() {
+        assertThatThrownBy(() -> new GetMoneyService("-1000"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+        assertThatThrownBy(() -> new GetMoneyService("8001"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+        assertThatThrownBy(() -> new GetMoneyService("0"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+}
