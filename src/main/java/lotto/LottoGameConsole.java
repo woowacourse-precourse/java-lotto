59e3bab19fbb6ea0b205ed1d66266f1aeebf90bb
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-10 10:07:44 +0900

    feat(LottoGameConsole): build skeleton code

---
+++ src/main/java/lotto/LottoGameConsole.java
@@ -0,0 +1,57 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoGameConsole {
+
+    public void startLottoGame() {
+        // 게임 시작 안내
+        // 구입 금액 안내, 입력, 검증
+        // Lotto 발행
+        // 당첨 lotto 생성
+        // 당첨 결과 처리, 출력
+    }
+
+    private void startingGuide() {
+        // 안내문 출력
+    }
+
+    private int handleAmountOfPurchaseMoney() {
+        // 구입 금액 입력 안내
+        // 구입 금액 입력
+        // 구입 금액 입력값 검증
+        return 0;
+    }
+
+    private List<Lotto> publishLottos(int amountOfPurchaseMoney) {
+        // 발행 개수 계산
+        // 발행
+        // 발행 lotto 출력
+        return null;
+    }
+
+    private void createWinningLotto() {
+        // 당첨 번호 생성
+        // 보너스 번호 생성
+        return; // 당첨 번호 + 보너스 번호
+    }
+
+    private List<Integer> createWinningNumbers() {
+        // 당첨 번호 안내
+        // 당첨 번호 입력
+        // 당첨 번호 검증
+        return null;
+    }
+
+    private int createBonusNumber() {
+        // 보너스 번호 안내
+        // 보너스 번호 입력
+        // 보너스 번호 검증
+        return 0;
+    }
+
+    private void handlePrizeOfLottoGame() {
+        // 당첨 결과 처리
+        // 당첨 결과 출력
+    }
+}
