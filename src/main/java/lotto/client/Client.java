6fdefb7df6bf9983a3a22a7220e43f431eee8da4
K-Diger <kitten.diger@gmail.com>
2022-11-10 17:33:54 +0900

    feat : getLottoPaper 메서드 구현 - 로또 구매 갯수만큼 발급받은 로또 내용을 출력한다.

--- src/main/java/lotto/client/Client.java
+++ src/main/java/lotto/client/Client.java
@@ -1,7 +1,9 @@
 package lotto.client;
 
+import lotto.host.LottoHost;
 import lotto.lotteryshop.Store;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Client {
@@ -11,5 +13,11 @@ public class Client {
         int purchaseNumberOfLotto = store.getPurchaseMoney();
         List<List<Integer>> generatedLottoNumber = store.generateLottoNumber(purchaseNumberOfLotto);
         store.getPublishedLottoNumber(generatedLottoNumber);
+    }
+
+    public void getLottoPaper() {
+        LottoHost lottoHost = new LottoHost();
+        List<Integer> lottoNumbers = lottoHost.inputLottoNumbers(new ArrayList<>());
+        lottoHost.inputBonusNumber(lottoNumbers);
     }
 }
