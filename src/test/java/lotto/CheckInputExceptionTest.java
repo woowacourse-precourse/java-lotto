0daa3bbad4282cd32dbe5f7139616b8c58826688
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-11 01:56:26 +0900

    Test(CheckInputExceptionTest) : 나머지가 0이 아니면 예외 발생메소드 테스트 코드 추가 #18
    CheckInputExceptionTest 클래스의 checkBuyerInputIsNotDivided() 메소드의 테스트 코드 추가
    
    Ref : #17

--- src/test/java/lotto/CheckInputExceptionTest.java
+++ src/test/java/lotto/CheckInputExceptionTest.java
@@ -16,17 +16,30 @@ class CheckInputExceptionTest {
     @DisplayName("사용자의 입력이 음수이면 예외처리 되는지 확인")
     void 구매자_음수_입력시_예외_테스트(){
 
-        String number = "-123";
+        final String number = "-123";
         assertThatThrownBy(() -> CheckInputException.checkBuyerInputIsMinus(number))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @Test
     @DisplayName("사용자의 입력이 숫자열이 아니면 예외 처리 되는지 확인")
     void 구매자_문자열_입력시_예외_테스트(){
 
-        String string = "123a";
+        final String string = "123a";
         assertThatThrownBy(() -> CheckInputException.checkBuyerInputIsNotNumber(string))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
     }
+
+    @Test
+    @DisplayName("1000으로 나누어 떨어지지않는지 확인")
+    void 구매자_입력시_1000으로_나누어_떨어지는지_예외_테스트(){
+
+        final String numberic = "4321";
+        assertThatThrownBy(() -> CheckInputException.checkBuyerInputIsNotDivided(numberic))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
 }
