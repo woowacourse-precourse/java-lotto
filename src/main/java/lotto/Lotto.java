0a382e030f0ac34faf39ce3b6c8369cbb5b991ad
WhalesBob <ndw8200@naver.com>
2022-11-10 15:38:45 +0900

    feat(Lotto) : add method about validating duplicate number exists

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,6 +15,16 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicateNumberExist(List<Integer> numbers){
+        Set<Integer> afterRemoveDuplicateNumbers = new HashSet<>(numbers);
+
+        boolean isDuplicateNumberExist = (numbers.size() != afterRemoveDuplicateNumbers.size());
+
+        if(isDuplicateNumberExist){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 서로 중복되어서는 안됩니다.\n");
         }
     }
 
