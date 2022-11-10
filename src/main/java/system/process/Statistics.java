e5832d32eaa52ad6038777ddfa682e4bb77d0fac
myungsikyoon <yms8795@gmail.com>
2022-11-10 22:48:29 +0900

    feat : 산출한 통계 내역을 출력해주는 기능 구현.

--- src/main/java/system/process/Statistics.java
+++ src/main/java/system/process/Statistics.java
@@ -1,8 +1,12 @@
 package system.process;
 
+import constants.PrizeDivision;
 import models.PurchasedLottosData;
 import models.StatisticsData;
 import models.WinningLottoData;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Statistics {
 
@@ -14,7 +18,8 @@ public class Statistics {
         this.purchasedLottosData = purchasedLottos.getPurchasedLottosData();
         this.winningLottoData = winningLotto.getWinningLottoData();
         statisticsData = new StatisticsData();
-
+        calculateStatistics();
+        printStatistics();
     }
 
     public void calculateStatistics() {
@@ -28,6 +33,29 @@ public class Statistics {
     }
 
     public void printStatistics(){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        List<PrizeDivision> prizeDivision = Arrays.asList(PrizeDivision.values());
+
+        int sum = 0;
+
+        for(int index=5; index>=1; index--){
+            String sameCount = prizeDivision.get(index).getSameCount();
+            String money = prizeDivision.get(index).getMoney();
+            int count = statisticsData.getDivisionCount().get(index);
+
+            System.out.println(sameCount + " (" + money +") - " + count + "개");
+
+            if(count > 0){
+                sum += Integer.parseInt(money.replaceAll("," ,""))*count;
+            }
+
+        }
+
+        double percent = (double)sum/(purchasedLottosData.getAmount()*1000);
+
+        System.out.println("총 수익률은 "+(double)Math.round(percent*1000)/10 +"%입니다.");
 
     }
 
