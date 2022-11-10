2ede6e2958c4dad6a3b0d7a84acc33bf07b35318
saturnone1 <saturnone1@naver.com>
2022-11-10 15:16:22 +0900

    feat(validate): 몇 가지 예외사항 추가 - 중복되는 숫자 - 1~45 숫자인 가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -13,6 +13,10 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("로또 번호의 길이는 6으로 맞추어야 합니다.");
+        } else if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("중복되는 숫자가 존재합니다.");
+        } else if (numbers.stream().anyMatch(s -> s>45 || s<1)) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
