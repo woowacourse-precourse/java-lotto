1a527ce13f4190bdef76c67720536899a54d71c2
Lee Min Woo <lmw7414@naver.com>
2022-11-11 05:00:53 +0900

    feat : Lotto 검증 기능 추가 - 중복된 번호
    로또 번호가 6개가 아니면 에러 메시지와 함께 에러를 반환하고, 중복된 숫자가 있을 경우 에러 메시지와 함께 에러를 반환한다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,11 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static lotto.constant.Constants.DUPLICATED_NUMBER_ERROR_MESSAGE;
+import static lotto.constant.Constants.INVALID_LENGTH_ERROR_MESSAGE;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,9 +17,14 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_LENGTH_ERROR_MESSAGE);
+        }
+
+        Set<Integer> lottoSet = new HashSet<>(numbers);
+        if (lottoSet.size() != 6) {
+            throw new IllegalArgumentException(DUPLICATED_NUMBER_ERROR_MESSAGE);
         }
     }
-
     // TODO: 추가 기능 구현
+
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,8 +3,12 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import static lotto.constant.Constants.DUPLICATED_NUMBER_ERROR_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -18,10 +22,31 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외메시지와 함께 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumberThenThrowException() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(DUPLICATED_NUMBER_ERROR_MESSAGE);
+    }
+
+    @DisplayName("중복된 숫자가 있을 시에 오류 반환 - validatedForDuplicatedNumber 메서드 테스트")
+    @Test
+    void testForValidateForDuplicatedNumber() {
+        List<Integer> lotto = List.of(1, 20, 32, 14, 25, 25);
+        assertThatIllegalArgumentException().isThrownBy(() -> validateForDuplicatedNumber(lotto))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private void validateForDuplicatedNumber(List<Integer> lotto) {
+        Set<Integer> lottoSet = new HashSet<>(lotto);
+        if (lottoSet.size() != 6) {
+            throw new IllegalArgumentException(DUPLICATED_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
 }
