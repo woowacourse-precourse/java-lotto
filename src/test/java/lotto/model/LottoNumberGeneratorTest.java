35ea8be23d9499660ee62124fa64386ec785bea1
jiwon1217 <koagjw@gmail.com>
2022-11-11 03:32:19 +0900

    test: Lotto 발행 테스트

---
+++ src/test/java/lotto/model/LottoNumberGeneratorTest.java
@@ -0,0 +1,21 @@
+package lotto.model;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+class LottoNumberGeneratorTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"1000", "2000", "3000"})
+    void 로또는_구입_금액에_해당하는_만큼_발행된다(String inputMoney) {
+        Money money = new Money(inputMoney);
+        Money unit = new Money("1000");
+        int lottoCount = money.calculateLottoCount(unit);
+
+        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+        Lottos lottos = lottoNumberGenerator.generateLottos(money);
+
+        assertThat(lottos.getLottos()).hasSize(lottoCount);
+    }
+}
