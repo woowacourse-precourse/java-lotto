7c806363ae4a91139a4de7c71db727e613737566
whipbaek <jibaek0513@gmail.com>
2022-11-10 19:15:41 +0900

    feat(User): 사용자에게 입력받은 로또번호 검증기능 추가 - private void validateInputLottoNumber(String inputLottoNumber) 추가 - 입력형태대로 입력이 안되었거나, 1미만 45초과 값이 들어오면 예외처리한다.

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -2,6 +2,7 @@ package lotto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -28,5 +29,24 @@ public class User {
         String inputLottoNumber = readLine();
         validateInputLottoNumber(inputLottoNumber);
         lottos.add(new Lotto(convertLottoNumber(inputLottoNumber)));
+    }
+
+    private void validateInputLottoNumber(String inputLottoNumber) {
+        ArrayList<String> split = new ArrayList<>(List.of(inputLottoNumber.split(", ")));
+        if(split.size() != 6){
+            System.out.println("[ERROR] 숫자 입력이 잘못 되었습니다.");
+            throw new IllegalArgumentException();
+        }
+        List<Integer> lottoNumber = split.stream()
+                .map(s -> Integer.parseInt(s))
+                .collect(Collectors.toList());
+
+        for (Integer integer : lottoNumber) {
+            if(integer > 45 || integer < 1){
+                System.out.println("[ERROR] 숫자는 1이상 45 이하의 숫자로 구성되어야 합니다.");
+                throw new IllegalArgumentException();
+            }
+        }
+
     }
 }
