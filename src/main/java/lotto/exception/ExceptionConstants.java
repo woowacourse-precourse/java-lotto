ae224fe00df819a3e215914fdba70c166681b32e
java-saeng <gusdn85554@naver.com>
2022-11-10 16:04:07 +0900

    feat : 예외 상수를 저장하는 추상 클래스 추가

---
+++ src/main/java/lotto/exception/ExceptionConstants.java
@@ -0,0 +1,7 @@
+package lotto.exception;
+
+public abstract class ExceptionConstants {
+
+    public static final String CANNOT_INPUT_ZERO = "[ERROR] 로또 구입 금액은 0원이 될 수 없습니다.";
+    public static final String CAN_ONLY_MULTIPLY_1000 = "[ERROR] 로또 구입 금액은 1000의 배수여야합니다.";
+}
