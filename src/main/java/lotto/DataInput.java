1d3c21f36206442a2fe5a58ea8d0ae7b55550af1
SimSimEEE <rebe123@naver.com>
2022-11-11 04:20:49 +0900

    feat(DataInput.class): add function to confirm input range

--- src/main/java/lotto/DataInput.java
+++ src/main/java/lotto/DataInput.java
@@ -4,9 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class DataInput {
     private static final String VALID_INPUT_PATTERN = "[^0-9]";
-    private static final String VALID_UNIT_PATTERN = "[0]{3}$";
+    private static final String VALID_UNIT_PATTERN = "[0-9]$";
     private static final String ERROR_MESSAGE_NUMBER = "[ERROR] 숫자를 입력해주세요";
     private static final String ERROR_MESSAGE_UNIT = "[ERROR] 1000원 단위로 입력해주세요";
+    private static final String ERROR_MESSAGE_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자를 입력해주세요";
+    private static final int MAXIMUM_NUMBER_OF_LOTTO = 45;
+    private static final int MINIMUM_NUMBER_OF_LOTTO = 1;
     public static String UserInput(String Message) {
         System.out.println(Message);
         String input = Console.readLine();
@@ -24,6 +27,14 @@ public class DataInput {
     private static void ValidateAmountUnit(String input) {
         if (input.matches(VALID_UNIT_PATTERN)) {
             System.out.println(ERROR_MESSAGE_UNIT);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void ValidateRange(String input) {
+        int bonusNumber = Integer.parseInt(input);
+        if (bonusNumber >= MINIMUM_NUMBER_OF_LOTTO && bonusNumber <= MAXIMUM_NUMBER_OF_LOTTO) {
+            System.out.println(ERROR_MESSAGE_RANGE);
             throw new IllegalArgumentException();
         }
     }
