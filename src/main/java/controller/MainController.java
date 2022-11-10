9c408ade287387ba1e953ec18640c3e229455234
gyuturn <rbtjrrbals@gmail.com>
2022-11-11 00:15:36 +0900

    feat($controller): C-1, C-2 기능 구현
    C-1, C-2
    purchase 객체를 사용하여 user를 만드는함수와 구매정보를 알려주는 함수 분리

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,12 +1,22 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.Lotto;
 import model.Purchase;
+import model.User;
 import view.PrintView;
 import view.ReceiveView;
 
 public class MainController {
 
-    public Purchase purchaseLotto() {
+    public void run(){
+        Purchase usersPurchase = purchaseLotto();
+        User user = purchaseInfo(usersPurchase);
+
+    }
+
+    private Purchase purchaseLotto() {
         PrintView.purchaseLotto();
 
         int purchaseLotto = 0;
@@ -16,8 +26,27 @@ public class MainController {
             PrintView.error(e.getMessage());
             throw new IllegalArgumentException(e.getMessage());
         }
-
         Purchase purchase = new Purchase(purchaseLotto);
         return purchase;
+    }
+
+    private User purchaseInfo(Purchase purchase){
+        User user = makeUserToPurchase(purchase);
+        PrintView.infoBoughtLotto(user);
+
+        return user;
+    }
+
+    private User makeUserToPurchase(Purchase purchase) {
+        int purchaseMoney = purchase.getMoney();
+        int purchaseCount = purchaseMoney / Lotto.PER_PRICE_LOTTO;
+
+        List<Lotto> boughtLotto = new ArrayList<>();
+        for (int count = 0; count < purchaseCount; count += 1) {
+            List<Integer> generateLotto = Lotto.generateLotto();
+            boughtLotto.add(new Lotto(generateLotto));
+        }
+
+        return new User(boughtLotto,purchase);
     }
 }
