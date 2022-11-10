a80566cf66fb4ba79c18649791af1f8de32a99d9
YuHayeon <youha0930@gmail.com>
2022-11-11 01:30:02 +0900

    feat(BonusNumberException): 보너스 번호가 1~45 사이의 숫자가 아니면 예외처리
    보너스 번호가 1~45 사이의 숫자이어야 하므로
    1~45 사이의 숫자가 아닐 경우 예외처리 후
    1~45 사이의 숫자로 입력해달라는 메시지 출력

--- src/main/java/lotto/exception/BonusNumberException.java
+++ src/main/java/lotto/exception/BonusNumberException.java
@@ -3,15 +3,24 @@ package lotto.exception;
 public class BonusNumberException {
 
     private static final String NUMBER_REGEX = "[0-9]+";
+    private static final int MAX_BONUS_NUMBER = 45;
+    private static final int MIN_BONUS_NUMBER = 1;
 
     public static void checkBonusNumber(String bonusNumberInput) {
         CommonInputException.checkCommonInput(bonusNumberInput, "보너스 번호를");
         checkBonusNumberDigit(bonusNumberInput);
+        checkBonusNumberRange(Integer.parseInt(bonusNumberInput));
     }
 
     public static void checkBonusNumberDigit(String bonusNumber) {
         if (!bonusNumber.matches(NUMBER_REGEX)) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자를 입력해주세요.");
+        }
+    }
+
+    public static void checkBonusNumberRange(Integer bonusNumber) {
+        if (bonusNumber < MIN_BONUS_NUMBER || bonusNumber > MAX_BONUS_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이의 숫자로 입력해주세요.");
         }
     }
 }
