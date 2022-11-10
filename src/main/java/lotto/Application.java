249ccd66c8bbc34e47bfbcd8f19fa677d654be72
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-10 18:37:21 +0900

    feat: 시작 메시지 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        startMessage();
+    }
+
+    public static void startMessage() {
+        System.out.println("구입 금액을 입력해 주세요.");
     }
 }
