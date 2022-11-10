f74fdc57c3b897e95405286bf323e47eeb0849e4
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-11-10 23:53:28 +0900

    refactor: 로또 티켓 발행 로직 수정

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
@@ -17,9 +16,7 @@ public class LottoTicket {
 		List<List<Integer>> lottoTickets = new ArrayList<>();
 		for (int i = 0; i < ticketNumber; i++) {
 			Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-			List<Integer> lottoTicket = lotto.getNumbers();
-			Collections.sort(lottoTicket);
-			lottoTickets.add(lottoTicket);
+			lottoTickets.add(lotto.getNumbers());
 		}
 		return lottoTickets;
 	}
