bc2ea82bc531b96c5040cddbb8c9aa8dca9303bd
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-11 01:28:27 +0900

    test($ShellTest) : 쉼표 제거 테스트

--- src/test/java/lotto/insert/ShellTest.java
+++ src/test/java/lotto/insert/ShellTest.java
@@ -1,4 +1,37 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.insert;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class ShellTest {
-  
+
+
+    @Test
+    public void 쉼표_제거(){
+        //given
+        String insertLine = "1,2,3,4,5,6";
+        String replace = insertLine.replace(",", "");
+        List<Integer> result = new ArrayList<>();
+        List<Integer> assertList = new ArrayList<>();
+
+        //when
+        char[] separate = replace.toCharArray();
+
+        for (int index = 1; index <= 6; index++) {
+            assertList.add(index);
+        }
+        for (char character : separate) {
+            result.add((int) character);
+        }
+
+        //then
+        Assertions.assertThat(result).isEqualTo(assertList);
+    }
 }
