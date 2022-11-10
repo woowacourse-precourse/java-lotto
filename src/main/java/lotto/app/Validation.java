6e9e08e05c040d71ea3058bcbd0ac12e58060e96
Joonsoo Kim <kjsoo1901@gmail.com>
2022-11-10 19:15:13 +0900

    feat: Add isNotDuplicate in Validation.java

--- src/main/java/lotto/app/Validation.java
+++ src/main/java/lotto/app/Validation.java
@@ -1,6 +1,7 @@
 package lotto.app;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Validation {
     public boolean hasSixItems(List<Integer> numbers){
@@ -14,6 +15,17 @@ public class Validation {
             if( 1 > numbers.get(i) || numbers.get(i) > 45){
                 return false;
             }
+        }
+        return true;
+    }
+
+    public boolean isNotDuplicate(List<Integer> numbers){
+        for(int i = 0; i < 6; i++){
+            List<Integer> number = numbers.stream()
+                    .filter(numbers.get(i)::equals)
+                    .collect(Collectors.toList());
+            if(number.size() != 1)
+                return false;
         }
         return true;
     }
