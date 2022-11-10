e500a355f1505cb1fe2264bb56697f80631cf042
YuHayeon <youha0930@gmail.com>
2022-11-11 01:23:57 +0900

    feat(BonusNumberException): 보너스 번호 입력이 숫자가 아닐 경우 예외처리
    보너스 번호의 입력이 숫자가 아닐 경우 예외처리 후 숫자를 입력해달라는 메시지 출력

--- src/main/java/lotto/exception/BonusNumberException.java
+++ src/main/java/lotto/exception/BonusNumberException.java
@@ -2,7 +2,16 @@ package lotto.exception;
 
 public class BonusNumberException {
 
+    private static final String NUMBER_REGEX = "[0-9]+";
+
     public static void checkBonusNumber(String bonusNumberInput) {
         CommonInputException.checkCommonInput(bonusNumberInput, "보너스 번호를");
+        checkBonusNumberDigit(bonusNumberInput);
+    }
+
+    public static void checkBonusNumberDigit(String bonusNumber) {
+        if (!bonusNumber.matches(NUMBER_REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자를 입력해주세요.");
+        }
     }
 }
