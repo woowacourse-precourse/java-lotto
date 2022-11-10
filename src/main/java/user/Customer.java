0aa93371f6415c5a239e01802f674221d136efe0
juhwang <juhong4930@naver.com>
2022-11-10 16:58:55 +0900

    feat(Customer): print lotto list

--- src/main/java/user/Customer.java
+++ src/main/java/user/Customer.java
@@ -1,12 +1,14 @@
 package user;
 
 import store.Lotto;
+import store.LottoMachine;
 
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class Customer {
     private static final String NUMERIC_ERROR = "[ERROR] 숫자를 입력해 주시기 바랍니다.";
+    private static final String NUMBER_OF_PURCHASES_PHRASE = "개를 구매했습니다.\n";
 
     private List<Lotto> lotteries;
     private int pay;
@@ -14,7 +16,12 @@ public class Customer {
     public Customer(String readline) {
         validateNumeric(readline);
         int pay = Integer.parseInt(readline);
+        lotteries = buyLotteries(pay);
         this.pay = pay;
+    }
+
+    private List<Lotto> buyLotteries(int pay) {
+        return LottoMachine.getInstance().pickLotteries(pay);
     }
 
     private void validateNumeric(String readline) {
@@ -23,4 +30,13 @@ public class Customer {
         }
     }
 
+    public String toLottoString() {
+        int numberOfPurchases = lotteries.size();
+        StringBuilder currentLotteries = new StringBuilder(numberOfPurchases + NUMBER_OF_PURCHASES_PHRASE);
+        while (numberOfPurchases > 0) {
+            currentLotteries.append(lotteries.get(numberOfPurchases - 1)).append("\n");
+            numberOfPurchases--;
+        }
+        return currentLotteries.toString();
+    }
 }
