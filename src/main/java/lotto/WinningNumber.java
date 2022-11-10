4df9aa70418d3b6d5f5e8fa15c47925b7493e568
minhee020 <minhee020@naver.com>
2022-11-10 23:35:41 +0900

    docs : WinningNumber 구현 목록 작성

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -5,17 +5,30 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 기능 구현 목록 - 로또 번호와 보너스 번호를 받아 하나의 객체로 만들어주는 클래스!
+ * 1. 로또 6자리 받기
+ * 2. 보너스 번호 받기
+ * 3. 번호 받은 것들은 필드로 저장하기
+ * 4. 로또 + 보너스가 유효한지 검사
+ * 5. 보너스가 범위 안의 숫자인지 검사
+ */
+
 public class WinningNumber {
+
+    private Lotto lotto;
+    private int bonus;
     public List<Integer> getWinningNumber() {
         List<Integer> result = new ArrayList<>();
 
-        // 당첨번호 입력
-        String[] input = Console.readLine().split(",");
-        for (String number : input) {
+        String input = Console.readLine();
+        String[] split = input.split(",");
+        for (String number : split) {
             result.add(Integer.parseInt(number));
         }
-        // 보너스 번호 입력
-        result.add(Integer.parseInt(Console.readLine()));
+
+        lotto = new Lotto(result);
+        bonus = Integer.parseInt(Console.readLine();
 
         return result;
     }
