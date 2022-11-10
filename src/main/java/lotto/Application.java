5ab5904e88b09861f287278118d8b34ac3fc4322
ddingmin <ddingminyt@gmail.com>
2022-11-11 05:20:33 +0900

    feat(generateLottos): count만큼 로또를 발행하는 기능 구현.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
+import lotto.domain.NumberGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int count = 8;
+        List<Lotto> lottos = generateLottos(count);
+    }
+
+    public static List<Lotto> generateLottos(int count) {
+        NumberGenerator generator = new NumberGenerator();
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = generator.createRandomUniqueNumbers();
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+        }
+        return lottos;
     }
 }
--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -1,10 +1,11 @@
 package lotto.domain;
 
 import java.util.List;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberGenerator {
-    public List<Integer> createRandomUniqueNumbers(){
+    public List<Integer> createRandomUniqueNumbers() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return numbers;
     }
