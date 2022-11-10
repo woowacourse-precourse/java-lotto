3fa3f2807df4a69a26ff8892298a88122d7a9f0f
tjwjdgus12 <01048733438a@gmail.com>
2022-11-10 18:00:36 +0900

    feat: Lotto에서 숫자를 정렬해서 저장, toString() Override

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        numbers.sort(Comparator.naturalOrder());
         this.numbers = numbers;
     }
 
@@ -17,5 +19,10 @@ public class Lotto {
         if (numbers.size() != numbers.stream().distinct().count()) {
             throw new IllegalArgumentException();
         }
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
     }
 }
