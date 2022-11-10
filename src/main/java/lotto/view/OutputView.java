b86845c7a9765b9068230a4c2df0622a65842688
Dongmin Kim <kimdongmin68@naver.com>
2022-11-11 01:10:23 +0900

    fix(lotto): 구입한 로또 개수 출력 메소드의 파라미터와 출력 문자열 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,14 +6,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputView {
-
-    private static final String PURCHASE_LOTTO = "%d개를 구매했습니다.";
+    private static final int LOTTO_PRICE = 1000;
+    private static final String PURCHASE_LOTTO = "\n%d개를 구매했습니다.\n";
     private static final String OPEN_BRACKET = "[";
     private static final String CLOSE_BRACKET = "]";
     private static final String SEPARATOR = ", ";
 
-    public void printPurchaseLottoCount(int count) {
-        System.out.printf(PURCHASE_LOTTO, count);
+    public void printPurchaseLottoCount(int money) {
+        System.out.printf(PURCHASE_LOTTO, money / LOTTO_PRICE);
     }
 
     public void printLottos(List<Lotto> lottos) {
