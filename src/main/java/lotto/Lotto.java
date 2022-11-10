7364be5598c93c421eeee7c778512d9cce69b0c2
kmularise <kmula222@gmail.com>
2022-11-10 14:33:15 +0900

    feat(lotto): add the lotto class

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,7 +14,29 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if (!isValidRange(numbers)){
+            throw new IllegalArgumentException();
+        }
+        if (hasSameNumber(numbers)){
+            throw new IllegalArgumentException();
+        }
+    }
+    // TODO: 추가 기능 구현
+    // TODO: 예외 사항 구현
+
+    private Boolean isValidRange(List<Integer> numbers){
+        for (int number: numbers){
+            if (number < 1 || number > 45){
+                return false;
+            }
+        }
+        return true;
     }
 
-    // TODO: 추가 기능 구현
+    private Boolean hasSameNumber(List<Integer> numbers){
+        if (numbers.size() != numbers.stream().distinct().count()){
+            return true;
+        }
+        return false;
+    }
 }
