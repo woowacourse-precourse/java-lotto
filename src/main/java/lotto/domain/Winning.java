23d15ab6a933a0299a25c90fad21651d058fcd7c
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-10 14:59:17 +0900

    feat: Winning 클래스와 테스트코드 생성

---
+++ src/main/java/lotto/domain/Winning.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import lotto.Ranking;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Winning {
+    private List<Ranking> score;
+
+    public Winning(){}
+
+    public void addRankingOfPlayer(Ranking lottoRanking){}
+
+    public HashMap<Ranking, Integer> figureSameRanking(){
+        return new HashMap<>();
+    }
+}
---
+++ src/test/java/lotto/domain_test/WinningTest.java
@@ -0,0 +1,42 @@
+package lotto.domain_test;
+import lotto.Ranking;
+import lotto.domain.Winning;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static lotto.Ranking.*;
+import static org.assertj.core.api.Assertions.*;
+public class WinningTest {
+
+    Winning winning;
+
+    @BeforeEach
+    void initializeClass(){
+        this.winning = new Winning();
+    }
+
+    @Test
+    @DisplayName("리스트에 넣은 순위들은 해시맵으로 해당 등수를 몇번 달성했는지 리턴해준다. ")
+    void createMapWithRankings(){
+    }
+
+    private void addCases(){
+        winning.addRankingOfPlayer(THREE_MATCH);
+        winning.addRankingOfPlayer(THREE_MATCH);
+        winning.addRankingOfPlayer(THREE_MATCH);
+        winning.addRankingOfPlayer(Ranking.FIVE_MATCH_WITH_BONUS);
+        winning.addRankingOfPlayer(Ranking.FIVE_MATCH);
+        assertThat(winning.figureSameRanking()).isEqualTo(makeExampleMap());
+    }
+
+    private HashMap<Ranking, Integer> makeExampleMap(){
+        HashMap<Ranking, Integer> exampleMap = new HashMap<>();
+        exampleMap.put(THREE_MATCH, 3);
+        exampleMap.put(FIVE_MATCH, 1);
+        exampleMap.put(FIVE_MATCH_WITH_BONUS, 1);
+        return exampleMap;
+    }
+}
