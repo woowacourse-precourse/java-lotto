ff947481c0183fcb4868706981114b3e9236d53e
박원종 <wonjong1030@naver.com>
2022-11-11 02:46:14 +0900

    test: 금액에 따른 로또 개수 테스트

--- src/test/java/lotto/CalculatorTest.java
+++ src/test/java/lotto/CalculatorTest.java
@@ -1,4 +1,16 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+    @Test
+    void 로또_개수_테스트() {
+        Calculator calculator = new Calculator();
+        int count = calculator.getLottoCount(14000);
+
+        assertThat(count).isEqualTo(14);
+    }
 }
