60ad2cf0c66c359848c29f118b03f66b3c20802f
박원종 <wonjong1030@naver.com>
2022-11-11 02:26:01 +0900

    test: 개수에 따른 로또 발행 테스트

--- src/test/java/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -1,4 +1,29 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class LottoGeneratorTest {
-  
+    @Test
+    void 로또_1개_발행() {
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        List<Integer>[] list = lottoGenerator.createRandomNumbers(1);
+
+        System.out.println(list[0]);
+        assertThat(list.length).isEqualTo(1);
+    }
+    @Test
+    void 로또_8개_발행() {
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        List<Integer>[] list = lottoGenerator.createRandomNumbers(8);
+
+        for (int i = 0; i < 8; i++) {
+            System.out.println(list[i]);
+        }
+        assertThat(list.length).isEqualTo(8);
+    }
 }
