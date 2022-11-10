6eb671ccc8204a04ab4e829921723498494a4445
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-10 15:02:49 +0900

    feat(PrintMessenger) : askBonusNumber

--- src/main/java/lotto/PrintMessenger.java
+++ src/main/java/lotto/PrintMessenger.java
@@ -11,4 +11,7 @@ public class PrintMessenger {
     {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
+    public static void askBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
 }
