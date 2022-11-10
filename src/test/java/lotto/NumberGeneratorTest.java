3b9eff48edc26b4e5a3c06e1e5e62eb8cf8735cb
panorama713 <yaiba713@gmail.com>
2022-11-10 20:20:00 +0900

    test(lotto.NumberGeneratorTest): 랜덤넘버 부여 동작 테스트

--- src/test/java/lotto/NumberGeneratorTest.java
+++ src/test/java/lotto/NumberGeneratorTest.java
@@ -1,5 +1,14 @@
 package lotto;
+import lotto.domain.NumberGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 /**
  * packageName : lotto
  * fileName : NumberGeneratorTest
@@ -12,5 +21,15 @@ package lotto;
  * 2022/11/10         gim-yeong-geun          최초 생성
  */
 public class NumberGeneratorTest {
-
+    @DisplayName("랜덤 번호를 부여하고 그 번호를 출력")
+    @Test
+    void createRandomNumberTest(){
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    assertThat(NumberGenerator.createRandomNumber())
+                            .contains(8, 21, 23, 41, 42, 43);
+            },
+            List.of(8, 21, 23, 41, 42, 43)
+        );
+    }
 }
