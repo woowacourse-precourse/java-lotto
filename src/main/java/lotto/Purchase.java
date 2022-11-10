377010d90384fc82e790c5bb44442ea7c36369b8
areyouhun <areyouhun@gmail.com>
2022-11-10 20:09:44 +0900

    feat(purchase): 입력 받은 구입 금액이 천 원 단위가 아니면 예외 발생

--- src/main/java/lotto/Purchase.java
+++ src/main/java/lotto/Purchase.java
@@ -11,7 +11,10 @@ public class Purchase {
         System.out.println(ENTER_PURCHASE_PRICE_MESSAGE);
         String input = Console.readLine();
         validateIfNumeric(input);
+
         Integer price = Integer.valueOf(input);
+        validateIfDividedByThousand(price);
+
         this.price = price;
     }
 
@@ -22,7 +25,15 @@ public class Purchase {
     private void validateIfNumeric(String input) {
         boolean isNumber = input.matches(ONLY_NUMBER);
         if (!isNumber) {
-            throw new IllegalArgumentException("[ERROR] 금액은 숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException("[ERROR] 금액은 숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    private void validateIfDividedByThousand(Integer price) {
+        final int expectedRemainder = 0;
+        final int currencyUnit = 1000;
+        if (price % currencyUnit != expectedRemainder) {
+            throw new IllegalArgumentException("[ERROR] 금액은 천 원 단위만 입력할 수 있습니다.");
         }
     }
 }
