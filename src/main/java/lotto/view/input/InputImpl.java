78851c0d3f25d74387b979faf494835bb39fd174
5uhwann <5uhwann27@gmail.com>
2022-11-11 00:00:41 +0900

    feat(검증): 로또 당첨 번호 입력 검증 - 1~45의 숫자를 입력했는지 검증 구현

--- src/main/java/lotto/view/input/InputImpl.java
+++ src/main/java/lotto/view/input/InputImpl.java
@@ -7,6 +7,8 @@ import java.util.List;
 public class InputImpl implements Input {
 
     private static final int MIN_PAYMENT_AMOUNT = 1000;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
 
     @Override
     public int inputPaymentAmount() {
@@ -34,7 +36,7 @@ public class InputImpl implements Input {
     }
 
     private boolean isAllowedWinningNumbers(List<String> winningNumbers) {
-        if (winningNumbers.size() != 6 || isDuplicatedNumber(winningNumbers)) {
+        if (winningNumbers.size() != 6 || isDuplicatedNumber(winningNumbers) || !isLottoNumber(winningNumbers)) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 서로다른 6개 숫자를 입력해야합니다.");
         }
         return true;
@@ -45,6 +47,16 @@ public class InputImpl implements Input {
                 .distinct().count();
         if (winningNumberSize == 6) {
             return false;
+        }
+        return true;
+    }
+
+    private boolean isLottoNumber(List<String> winningNumbers) {
+        for (String winningNumber : winningNumbers) {
+            int number = Integer.parseInt(winningNumber);
+            if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+                return false;
+            }
         }
         return true;
     }
