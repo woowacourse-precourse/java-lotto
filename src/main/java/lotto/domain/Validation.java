91bc4756efa0830bb9b251962e542a32b082dc4f
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-10 11:40:58 +0900

    refactor : 메서드명 변경 isGreaterThan1000 -> isLessThan1000 및 형변환 방식 변경

---
+++ src/main/java/lotto/domain/Validation.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import static java.lang.Integer.*;
+
+public class Validation {
+	public static boolean isNumberOnly(String input){
+		return input.matches("^\\d+$");
+	}
+
+	public static boolean isAssignedCurrencyUnit(String input){
+		long amount = parseInt(input);
+		return amount%1000 == 0;
+	}
+
+	public static boolean isLessThan1000(String input){
+		long amount = parseInt(input);
+		return amount >= 1000;
+	}
+}
+
+
