a61dc6ab87b391d3bf7632bac72c62e5e51a373f
dclxxi <dclxxi@kakao.com>
2022-11-10 20:09:53 +0900

    feat: 로또 구입 금액을 입력받는 기능 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,13 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+
+    public static String inputPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT);
+
+        return Console.readLine();
+    }
+}
