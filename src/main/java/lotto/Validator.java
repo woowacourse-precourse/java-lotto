b1dedb3f1fdef629a4676545a7514f12bf86f08f
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-10 23:36:48 +0900

    feat: 로또 구매 금액 예외처리 메소드 추가

---
+++ src/main/java/lotto/Validator.java
@@ -0,0 +1,10 @@
+package lotto;
+
+public class Validator {
+
+    public void validateMoneyInput(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
+    }
+}
