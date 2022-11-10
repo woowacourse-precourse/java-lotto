fc9f8da3acc82f4329a5e0f0e989947a3a8507bb
jmj005 <jmj00005@naver.com>
2022-11-10 14:28:31 +0900

    feat(Lotto): 로또 번호 발생시키기 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -16,5 +18,21 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static List<Lotto> getLottoList(int money) {
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int index = 0; index < money / 1000; index++) {
+            lottoList.add(generateLottoList());
+        }
+        return lottoList;
+    }
+
+    public static Lotto generateLottoList() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
+
+    @Override
+    public  String toString() {
+        return numbers.toString();
+    }
 }
