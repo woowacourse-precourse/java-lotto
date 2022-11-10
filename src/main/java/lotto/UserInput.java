82d5e5106de35763f2b466ebee47054c511418a0
kevstevie <jianna442@gmail.com>
2022-11-10 23:25:29 +0900

    feat: 입력받은 money 유효성 검사 함수 추가

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -11,4 +11,17 @@ public class UserInput {
         validateMoney(input);
         return Long.parseLong(input);
     }
+
+    static private void validateMoney(String input) {
+        try {
+            long money = Long.parseLong(input);
+            if (money % 1000 != 0) {
+                Message.ERROR_INPUT_1000.printError();
+                throw new IllegalArgumentException();
+            }
+        } catch (NumberFormatException e) {
+            Message.ERROR_INPUT_NUMBER.printError();
+            throw new IllegalArgumentException();
+        }
+    }
 }
