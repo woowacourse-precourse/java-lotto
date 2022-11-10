1a09d1aa3155c5499238d114762b40480f0e695a
JunH <tjdtls690@gmail.com>
2022-11-10 16:24:58 +0900

    :sparkles: feat(Domain : LottoNumbersValidator) : 6개의 숫자 중, 중복되는 숫자가 있는 경우 예외 처리 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,45 +1,18 @@
 package lotto.domain;
 
+import lotto.domain.validator.LottoNumbersValidator;
+
 import java.util.List;
 
 public class Lotto {
-    private static final int LENGTH_OF_LOTTO_NUMBERS = 6;
-    
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
-
+    
     private void validate(List<Integer> numbers) {
-        validateOutOfLength(numbers);
-        validateExistDuplicateNumber(numbers);
-    }
-    
-    private void validateOutOfLength(final List<Integer> numbers) {
-        if (isOutOfLength(numbers)) {
-            throw new IllegalArgumentException();
-        }
-    }
-    
-    private boolean isOutOfLength(final List<Integer> numbers) {
-        return isSameLength(numbers.size(), LENGTH_OF_LOTTO_NUMBERS);
-    }
-    
-    private void validateExistDuplicateNumber(final List<Integer> numbers) {
-        if (isSameLength(numbers.size(), lengthAfterDeduplication(numbers))) {
-            throw new IllegalArgumentException();
-        }
-    }
-    
-    private boolean isSameLength(final int lottoNumbersLength, final int lengthToCompare) {
-        return lottoNumbersLength != lengthToCompare;
-    }
-    
-    private int lengthAfterDeduplication(final List<Integer> numbers) {
-        return (int) numbers.stream()
-                .distinct()
-                .count();
+        LottoNumbersValidator.validate(numbers);
     }
 }
---
+++ src/main/java/lotto/domain/validator/LottoNumbersValidator.java
@@ -0,0 +1,43 @@
+package lotto.domain.validator;
+
+import java.util.List;
+
+public class LottoNumbersValidator {
+    private static final String UTILITY_CLASS_CREATE_EXCEPTION_MESSAGE = "생성할 수 없는 유틸 클래스입니다.";
+    private static final int LENGTH_OF_LOTTO_NUMBERS = 6;
+    
+    private LottoNumbersValidator() {
+        throw new IllegalStateException(UTILITY_CLASS_CREATE_EXCEPTION_MESSAGE);
+    }
+    
+    public static void validate(List<Integer> numbers) {
+        validateOutOfLength(numbers);
+        validateExistDuplicateNumber(numbers);
+    }
+    
+    private static void validateOutOfLength(final List<Integer> numbers) {
+        if (isOutOfLength(numbers)) {
+            throw new IllegalArgumentException();
+        }
+    }
+    
+    private static boolean isOutOfLength(final List<Integer> numbers) {
+        return isSameLength(numbers.size(), LENGTH_OF_LOTTO_NUMBERS);
+    }
+    
+    private static void validateExistDuplicateNumber(final List<Integer> numbers) {
+        if (isSameLength(numbers.size(), lengthAfterDeduplication(numbers))) {
+            throw new IllegalArgumentException();
+        }
+    }
+    
+    private static boolean isSameLength(final int lottoNumbersLength, final int lengthToCompare) {
+        return lottoNumbersLength != lengthToCompare;
+    }
+    
+    private static int lengthAfterDeduplication(final List<Integer> numbers) {
+        return (int) numbers.stream()
+                .distinct()
+                .count();
+    }
+}
