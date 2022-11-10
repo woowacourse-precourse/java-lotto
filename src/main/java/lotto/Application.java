87e88bf8047c4c31c599048ac27e0eb3552423da
goldcollar95 <yjs7298@naver.com>
2022-11-10 23:46:40 +0900

    feat(User): 1부터~45까지 랜덤수 발생 lottoRandomNumber메서드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,9 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        User user = new User();
+        user.lottoRandomNumber(3);
+
     }
 }
--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,4 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    public List<List<Integer>> lottoRandomNumber(Integer number){   //사용자 입력값을 받는 것
+            List<List<Integer>> numberResult = new ArrayList<>();
+
+
+        for(int i = 0; i < number; i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            numberResult.add(numbers);
+
+//            for(int j = 0; j < numbers.size(); j++) {
+
+
+                System.out.println(numbers);
+
+        }
+        return numberResult;
+    }
 }
