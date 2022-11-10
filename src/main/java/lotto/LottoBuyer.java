a9ca2da43c57733e47bc9c3980c9da40238c0399
ghkdgus29 <ghkdgus29@naver.com>
2022-11-11 08:58:05 +0900

    feat: 지불한 돈이 1000원 단위로 떨어지지 않을 경우 예외처리

--- src/main/java/lotto/LottoBuyer.java
+++ src/main/java/lotto/LottoBuyer.java
@@ -5,11 +5,18 @@ public class LottoBuyer {
     private final int cost;
 
     public LottoBuyer(int cost) {
+        costValidate(cost);
         this.cost = cost;
     }
 
     public int getCost() {
         return cost;
+    }
+
+    public void costValidate(int cost) {
+        if (cost % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위로 입력 가능합니다.");
+        }
     }
 
 }
--- src/test/java/lotto/LottoBuyerTest.java
+++ src/test/java/lotto/LottoBuyerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoBuyerTest {
@@ -22,4 +23,15 @@ class LottoBuyerTest {
         assertThat(cost).isEqualTo(12000);
     }
 
+    @Test
+    void costValidation() {
+        ByteArrayInputStream in = new ByteArrayInputStream("12100".getBytes());
+        System.setIn(in);
+
+        UI.setCost();
+        assertThatThrownBy(() -> new LottoBuyer(UI.getCost()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 구입 금액은 1,000원 단위로 입력 가능합니다.");
+
+    }
 }
