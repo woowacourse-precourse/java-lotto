59649f2a15f993773ad6370e0011968b2eaccbdd
WhalesBob <ndw8200@naver.com>
2022-11-10 15:02:20 +0900

    feat(UserInput) : add feature about input getWinningNumbers from users

--- src/main/java/lotto/UserInputHandler.java
+++ src/main/java/lotto/UserInputHandler.java
@@ -2,6 +2,9 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserInputHandler {
 
     static final int moneyOfOneTicket = 1000;
@@ -13,9 +16,33 @@ public class UserInputHandler {
         return money / moneyOfOneTicket;
     }
 
+    public List<Integer> getWinningNumbers(){
+        String input = Console.readLine();
+        return convertToLottoNumbers(input);
+    }
+
+    private List<Integer> convertToLottoNumbers(String input){
+        List<Integer> lottoNumbers = new ArrayList<>();
+        String[] inputList = input.split(",");
+
+        for(int i = 0; i < numbersOfLotto; i++){
+            lottoNumbers.add(validateLottoNumbers(inputList[i]));
+        }
+        return lottoNumbers;
+    }
+
     private void validateMoney(int money){
         if(money % moneyOfOneTicket != 0){
             throw new IllegalArgumentException("[ERROR] 로또는 1,000원 단위로 구입할 수 있습니다.\n");
         }
     }
+
+    private int validateLottoNumbers(String lottoNumberInput){
+        try{
+            return Integer.parseInt(lottoNumberInput);
+        }catch(NumberFormatException ex){
+            throw new IllegalArgumentException("[ERROR] 콤마(,) 와 숫자 이외의 다른 값을 당첨 값 입력할 수 없습니다.\n");
+        }
+    }
+
 }
