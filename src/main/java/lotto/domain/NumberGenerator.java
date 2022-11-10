ccf4a3d1161f2c05637e62544847bb53717192d1
csj <koreaxman@naver.com>
2022-11-10 23:45:02 +0900

    feat: sort lotto number lists in ascending order

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class NumberGenerator {
@@ -26,9 +27,16 @@ public class NumberGenerator {
         return numbers;
     }
 
+    public List<List<Integer>> sortLottoNumbers(List<List<Integer>> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            Collections.sort(numbers.get(i));
+        }
+        return numbers;
+    }
+
     public void showPurchaseLottery(int count) {
         List<List<Integer>> numbers = createNumbersBasedPrice(count);
-
+        numbers = sortLottoNumbers(numbers);
         for (int i = 0; i < count; i++) {
             System.out.println(numbers.get(i));
         }
