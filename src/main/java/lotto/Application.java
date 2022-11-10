be8ac1335f56b8af6319c6de9261480d39d1efba
HwangSeokJin <ghkd3703@gmail.com>
2022-11-10 18:27:33 +0900

    feat(LottoMachine): 잘못된 구입금액이 입력되면 [ERROR]로 시작되는 에러 메시지 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,8 +3,16 @@ package lotto;
 import java.util.List;
 import lotto.domain.LottoSystem;
 import lotto.domain.Lotto;
+import lotto.domain.Message;
 
 public class Application {
     public static void main(String[] args) {
+        LottoSystem lottoSystem = new LottoSystem();
+
+        try {
+            List<Lotto> lottoBundle = lottoSystem.buy();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(Message.ERROR_MESSAGE + exception.getMessage());
+        }
     }
 }
--- src/main/java/lotto/domain/Message.java
+++ src/main/java/lotto/domain/Message.java
@@ -2,4 +2,5 @@ package lotto.domain;
 
 public class Message {
     public static final String INPUT_PURCHASE_AMOUNT = "구입 금액을 입력하세요.";
+    public static final String ERROR_MESSAGE = "[ERROR]";
 }
