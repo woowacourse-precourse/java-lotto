8e184971b76860a45642402e98920d4b53437830
Hayoon <hy97@sju.ac.kr>
2022-11-10 15:19:24 +0900

    feat(Lotto): 당첨 통계 계산 기능 메서드 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.HashMap;
 import java.util.List;
 
-public class Lotto {
+public class Lotto{
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -17,4 +20,28 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public void calculatorMatchingNumbers(List<List<Integer>> userLottos, int bonusNumber) {
+        HashMap<Integer, Integer> matchingPair = new HashMap<>(5){{
+                put(3, 0);
+                put(4, 0);
+                put(5, 0);
+                put(7, 0); // 5개 일치 + 보너스 번호
+                put(6, 0);
+        }};
+
+        for (List<Integer> userLotto : userLottos) {
+            int matchingCount = 0;
+            for (Integer number : numbers) {
+                if (userLotto.contains(number)) {
+                    matchingCount++;
+                }
+            } if (matchingCount >= 3 && matchingCount <= 6) {
+                if (matchingCount == 5 && userLotto.contains(bonusNumber)) {
+                    matchingPair.replace(7, matchingPair.get(7) + 1);
+                } else {
+                    matchingPair.replace(matchingCount, matchingPair.get(matchingCount) + 1);
+                }
+            }
+        }
+    }
 }
