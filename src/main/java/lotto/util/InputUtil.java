d0e8c352cdd2b3123a46bb8d2877b3cfe6381c93
KimDongho <hkim4410@naver.com>
2022-11-10 21:57:48 +0900

    feat(InputUtil): 사용자의 입력을 받는 함수 추가

---
+++ src/main/java/lotto/util/InputUtil.java
@@ -0,0 +1,11 @@
+package lotto.util;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputUtil {
+
+    public String getUserInput() {
+        String input = readLine();
+        return input;
+    }
+}
