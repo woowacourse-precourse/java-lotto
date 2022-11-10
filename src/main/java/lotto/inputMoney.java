c76642daa3c5f9911f75c45f1a79bb9cf0bf28db
eugene1204 <s1061001@naver.com>
2022-11-10 17:08:29 +0900

    feat : inputMoney 클래스, buyLotto, buyLottoprint메소드 작성

--- src/main/java/lotto/inputMoney.java
+++ src/main/java/lotto/inputMoney.java
@@ -7,6 +7,13 @@ public class inputMoney {
         validate(money);
         this.money = money;
     }
+    private int buyLotto(){
+        return Integer.valueOf(money) / 1000;
+    }
+    public int buyLottoprint(){
+        System.out.println(buyLotto()+"개를 구매했습니다.");
+        return buyLotto();
+    }
     private void validate(String money){
         if(Integer.valueOf(money) % 1000 != 0) throw new IllegalArgumentException();
         if(money.replaceAll("[^0-9]]","").length() != money.length()) throw new IllegalArgumentException();
