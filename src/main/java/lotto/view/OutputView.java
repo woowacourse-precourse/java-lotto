4c8dfd67a59087967750ee142e1d0763d2ce2a4c
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-10 15:29:27 +0900

    feat(OutputView): GameCount를 출력과 동시에 RandomNumbers도 출력하게 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,21 @@
 package lotto.view;
 
+import lotto.Utils.Random;
+
+import java.util.List;
+
 public class OutputView {
     public static final String NUMBER_OF_USER_GAME_COUNT="개를 구매했습니다.";
 
     public static void printGameCount(int userGameCnt){
         System.out.println(userGameCnt+NUMBER_OF_USER_GAME_COUNT);
-    }
 
+        printRandomNumbers(userGameCnt);
+    }
+    private static void printRandomNumbers(int userGamCnt){
+        for(int i=0;i<userGamCnt;i++) {
+            List<Integer> randomNumbers = Random.makeRandomNumbers();
+            System.out.println(randomNumbers);
+        }
+    }
 }
