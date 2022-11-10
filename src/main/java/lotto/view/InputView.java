32ea8761ffa840f42e863af51018cc055fc758ea
parkjungyoon <tbr06057@naver.com>
2022-11-10 21:28:36 +0900

    feat(View): 로또 구입 금액 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,11 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static int getLottoPurchaseAmount() {
+        OutputView.printLottoPurchaseAmount();
+        int input = Integer.parseInt(Console.readLine());
+        return input;
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,9 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+public class OutputView {
+    private final static String LOTTO_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static void printLottoPurchaseAmount() {
+        System.out.println(LOTTO_PURCHASE_MESSAGE);
+    }
 }
