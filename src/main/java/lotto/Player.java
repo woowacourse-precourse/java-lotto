3282b1ab80f0a1afe772b1f67248767f2b717a97
heesang <dlagmltkd12@naver.com>
2022-11-11 00:43:55 +0900

    feat: 구입 급액은 1000원 단위로 입력, 1000으로 나누어 떨어지지 않는 경우 예외 처리

--- src/main/java/lotto/Player.java
+++ src/main/java/lotto/Player.java
@@ -3,8 +3,22 @@ package lotto;
 public class Player {
 
     public static final int LOTTO_PRICE = 1000;
+    public static final String ERROR_NOT_VALID_PRICE = "[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다.";
+    public static final int CRITERION_ZERO = 0;
 
     public int canBuyLottoCount(int price) {
+        validate(price);
         return price / LOTTO_PRICE;
     }
+
+    private void validate(int price) {
+        if (!isDivided(price)) {
+            throw new IllegalArgumentException(ERROR_NOT_VALID_PRICE);
+        }
+    }
+
+    private boolean isDivided(int price) {
+        return ((price % LOTTO_PRICE) == CRITERION_ZERO);
+    }
+
 }
--- src/test/java/lotto/PlayerTest.java
+++ src/test/java/lotto/PlayerTest.java
@@ -1,8 +1,10 @@
 package lotto;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PlayerTest {
 
@@ -11,6 +13,16 @@ public class PlayerTest {
     void canByLottoCount() {
         Player player = new Player();
         int count = player.canBuyLottoCount(10000);
-        Assertions.assertThat(count).isEqualTo(10);
+        assertThat(count).isEqualTo(10);
+    }
+
+    @DisplayName("구임 금액이 1000으로 나누어 떨어지지 않으면 예외 발생")
+    @Test
+    void validatePrice() {
+        assertThatThrownBy(() -> {
+            Player player = new Player();
+            player.canBuyLottoCount(1350);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다.");
     }
 }
