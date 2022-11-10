fbc7af7ba401510b22ef446846767a7d766f48ce
hgleeee <94894328+hgleeee@users.noreply.github.com>
2022-11-10 15:28:50 +0900

    당첨금 열거형 타입 생성

--- src/main/java/domain/Winnings.java
+++ src/main/java/domain/Winnings.java
@@ -1,4 +1,19 @@
 package domain;
 
 public enum Winnings {
+    FIRST(2000000000),
+    SECOND(30000000),
+    THIRD(1500000),
+    FOURTH(50000),
+    FIFTH(5000);
+
+    private final int value;
+
+    Winnings(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
