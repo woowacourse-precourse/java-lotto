9a0a309df8dd2d692554e20082ad80fb08cc8b0f
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-10 13:55:34 +0900

    refactor(lotto): 클래스 이름 수정

--- src/main/java/view/InputUserMoney.java
+++ src/main/java/view/InputUserMoney.java
@@ -1,12 +1,11 @@
-package domain;
+package view;
+
+import static util.ValidateUserMoney.validateMoney;
 
 import camp.nextstep.edu.missionutils.Console;
-import constant.ErrorMessage;
 import constant.ExceptionNumber;
-import java.util.stream.IntStream;
-import view.ExceptionInfo;
 
-public class UserMoney {
+public class InputUserMoney {
 
     public static int inputMoney(){
         String input_money = Console.readLine();
@@ -14,53 +13,8 @@ public class UserMoney {
         if(!validateMoney(input_money)){
             return ExceptionNumber.EXCEPTION_CODE.getCode();
         }
-
         return Integer.parseInt(input_money);
     }
 
-    public static boolean validateMoney(String input_money){
-        if (checkProcessIsNumber(input_money)) {
-            return false;
-        }
-        if (checkProcessIsCorrectMoney(input_money)) {
-            return false;
-        }
-        return true;
-    }
-
-    private static boolean checkProcessIsCorrectMoney(String input_money){
-        try{
-            isCorrectMoney(Integer.parseInt(input_money));
-        }
-        catch (IllegalArgumentException e){
-            ExceptionInfo.printException(e);
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean checkProcessIsNumber(String input_money){
-        try {
-           isNumber(input_money);
-        }
-        catch(IllegalArgumentException e){
-            ExceptionInfo.printException(e);
-            return true;
-        }
-        return false;
-    }
-
-    private static void isCorrectMoney(int money){
-        if(money %1000>0){
-            throw new IllegalArgumentException(ErrorMessage.ERROR_INCORRECT_MONEY.getMessage());
-        }
-    }
-
-    private static void isNumber(String input_money){
-        IntStream chars = input_money.chars();
-        if(chars.anyMatch(c ->  (c<'0' || c>'9'))){
-            throw new IllegalArgumentException(ErrorMessage.ERROR_NOT_NUMBER.getMessage());
-        }
-    }
 
 }
