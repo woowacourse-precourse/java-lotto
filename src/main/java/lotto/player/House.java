93d5735b87ae0c07536b5129407b113eb012396c
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-10 21:03:58 +0900

    feat: add player architecture

--- src/main/java/lotto/player/House.java
+++ src/main/java/lotto/player/House.java
@@ -1,4 +1,28 @@
 package lotto.player;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import lotto.data.Lotto;
+
 public class House {
+    private final Lotto win;
+    public House(String winnerNumbers, int bonus) {
+        List<Integer> numbersInput = convertWinnerNumber(winnerNumbers);
+        win = new Lotto(numbersInput, bonus);
+    }
+
+    private List<Integer> convertWinnerNumber(String winnerNumbers){
+        try{
+            String[] splitByComma = winnerNumbers.split(",");
+            Stream<String> streamOfStr = Arrays.stream(splitByComma);
+            Stream<Integer> streamOfInt = streamOfStr.map(Integer::parseInt);
+            return streamOfInt.collect(Collectors.toList());
+        }catch (Error e){
+            throw new IllegalArgumentException();
+        }
+    }
+
+
 }
