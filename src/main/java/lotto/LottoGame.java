a50ed61d598532e3a9c80be38492e73e85ea67a1
rt3310 <btac3310@gmail.com>
2022-11-10 19:17:06 +0900

    feat: 1000원 단위가 아닐 시 예외처리하는 기능 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -6,16 +6,24 @@ import java.util.regex.Pattern;
 
 public class LottoGame {
     private static final String INVALID_MONEY_TYPE_REGEX = "\\D";
+    private static final int UNIT_OF_MONEY = 1000;
 
     public String inputMoney() {
         String money = Console.readLine();
         validateMoneyType(money);
+        validateUnitOf1000(money);
         return money;
     }
 
     private void validateMoneyType(String money) {
         if (Pattern.compile(INVALID_MONEY_TYPE_REGEX).matcher(money).find()) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_MONEY_TYPE.getMessage());
+        }
+    }
+
+    private void validateUnitOf1000(String money) {
+        if (Integer.parseInt(money) % UNIT_OF_MONEY != 0) {
+            throw new IllegalArgumentException();
         }
     }
 }
