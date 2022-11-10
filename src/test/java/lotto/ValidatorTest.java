df1d94497b525998cf1c35c302520b88da213a0e
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-10 13:58:35 +0900

    refactor(ValidatorTest): refactor imports

--- src/test/java/lotto/ValidatorTest.java
+++ src/test/java/lotto/ValidatorTest.java
@@ -1,10 +1,10 @@
 package lotto;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ValidatorTest {
 
