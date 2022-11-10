c259e34db834cc3fc73caa67cd4cf413c69a2644
juhwang <juhong4930@naver.com>
2022-11-10 21:44:35 +0900

    feat(LottoDraw): validate range numbers one by one

--- src/main/java/user/LottoDraw.java
+++ src/main/java/user/LottoDraw.java
@@ -3,12 +3,15 @@ package user;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class LottoDraw {
-    private static final String COMMA_ERROR = "쉼표로 구분된 6개 숫자를 입력해 주시기 바랍니다.";
-    private static final String DUPLICATION_ERROR = "입력된 숫자에 중복이 있습니다.";
+    private static final String COMMA_ERROR = "[ERROR] 쉼표로 구분된 6개 숫자를 입력해 주시기 바랍니다.";
+    private static final String DUPLICATION_ERROR = "[ERROR] 입력된 숫자에 중복이 있습니다.";
+    private static final String RANGE_ERROR = "[ERROR] 1~45 사이의 수를 입력해 주시기 바랍니다.";
+    private static final String NUMERIC_ERROR = "[ERROR] 숫자를 입력해 주시기 바랍니다.";
+
 
     private static LottoDraw lottoDraw;
 
@@ -24,13 +27,15 @@ public class LottoDraw {
 
     public List<Integer> pickWinNumbers(String readline) {
         validateComma(readline);
+        validateNumeric(readline.split(","));
         List<Integer> winNumbers = changeStringToInt(readline);
         validateDuplication(winNumbers);
+        validateRange(winNumbers);
         return winNumbers;
     }
 
     private List<Integer> changeStringToInt(String readline) {
-        int[] changeInteger= Arrays.stream(readline.split(",")).mapToInt(Integer::parseInt).toArray();
+        int[] changeInteger = Arrays.stream(readline.split(",")).mapToInt(Integer::parseInt).toArray();
         return Arrays.stream(changeInteger).boxed().collect(Collectors.toList());
     }
 
@@ -40,13 +45,29 @@ public class LottoDraw {
         }
     }
 
+    private void validateNumeric(String[] readline) {
+        if (!Pattern.matches("^\\d*$", String.join("", readline))) {
+            throw new IllegalArgumentException(NUMERIC_ERROR);
+        }
+    }
+
     private void validateDuplication(List<Integer> winNumbers) {
-        if (duplicateNumbers(winNumbers)){
+        if (duplicateNumbers(winNumbers)) {
             throw new IllegalArgumentException(DUPLICATION_ERROR);
         }
     }
 
     private boolean duplicateNumbers(List<Integer> winNumbers) {
         return new HashSet<>(winNumbers).size() != winNumbers.size();
+    }
+
+    private void validateRange(List<Integer> winNumbers) {
+        if (overRange(winNumbers)) {
+            throw new IllegalArgumentException(RANGE_ERROR);
+        }
+    }
+
+    private boolean overRange(List<Integer> winNumbers) {
+        return winNumbers.stream().anyMatch(winNumber -> winNumber < 1 || winNumber > 45);
     }
 }
