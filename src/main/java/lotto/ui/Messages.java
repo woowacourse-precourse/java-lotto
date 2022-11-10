58bca78967ff77dfec4a286ce0c3b9888731bfe8
lee <whddlsk123@naver.com>
2022-11-11 00:31:22 +0900

    feat(Messages) : 입출력 메세지를 모아놓은 class 생성

---
+++ src/main/java/lotto/ui/Messages.java
@@ -0,0 +1,19 @@
+package lotto.ui;
+
+public enum Messages {
+    INPUT_MONEY("구입금액을 입력해 주세요"),
+    INPUT_LOTTE_NUMBER("당첨 번호를 입력해 주세요"),
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요");
+
+
+    private final String message;
+
+    Messages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
