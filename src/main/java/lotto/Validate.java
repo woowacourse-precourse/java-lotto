543e55f3227d8ab26ee05b9fe7816633b7748ed1
eora21 <eora21@naver.com>
2022-11-10 12:56:45 +0900

    feat: 🎸 추가 숫자가 아닌 문자를 검사하는 메서드
    숫자가 아니라면 예외를 발생시킨다. 정규식에서 matches는 대상 문자열 전체와 패턴을 비교하고, find는 하나라도 일치하는
    값이 있는지 찾는다. 이 때, 빈 문자열이 들어왔을 때에도 검증하려면 matches가 낫다고 판단했다.

--- src/main/java/lotto/Validate.java
+++ src/main/java/lotto/Validate.java
@@ -2,10 +2,19 @@ package lotto;
 
 import constant.Invalid;
 
+import java.util.regex.Pattern;
+
 public class Validate {
+    private static final Pattern intPattern = Pattern.compile("[0-9]+");
     public void validNull(String input) {
         if (input == null) {
             Invalid.NULL.throwException();
+        }
+    }
+
+    public void validInt(String input) {
+        if (!intPattern.matcher(input).matches()) {
+            Invalid.NOT_INT.throwException();
         }
     }
 }
