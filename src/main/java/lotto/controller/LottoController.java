576b7492a9cec90ad19df669101f5c391b2f9ddb
Reznov <reznov03048@gmail.com>
2022-11-10 19:22:12 +0900

    feat: 입력 받은 금액을 가져오는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,17 +1,23 @@
 package lotto.controller;
 
+import lotto.domain.PurchaseAmount;
+import lotto.view.Input;
+
 public class LottoController {
 
     public void start() {
         // 금액 입력 받음
-        
+        PurchaseAmount purchaseAmount = inputPurchaseAmount();
         // 로또 n장 생성 (1장 당 6자리 숫자 랜덤 생성) -> Lotto.java
-        
+
         // 당첨 번호 입력 받음
-        
+
         // 결과 생성
-        
+
         // 결과 출력
     }
 
+    private PurchaseAmount inputPurchaseAmount() {
+        return new PurchaseAmount(Input.getInputPurchaseAmount());
+    }
 }
