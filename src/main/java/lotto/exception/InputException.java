b82aa30bb145a0bfa8216e8de008f1f889bc0a5f
KIMSEI1124 <workju1124@gmail.com>
2022-11-10 15:24:40 +0900

    refactor: inputException 반환 타입 변경
    반환 값이 사용되지 않아서 void로 수정함

--- src/main/java/lotto/exception/InputException.java
+++ src/main/java/lotto/exception/InputException.java
@@ -2,16 +2,16 @@ package lotto.exception;
 
 public class InputException extends IllegalArgumentException {
 
-    public boolean notDigitException(String inputString) {
+    public void notDigitException(String inputString) {
         if (inputString.chars().allMatch(Character::isDigit)) {
-            return true;
+            return;
         }
         throw new IllegalArgumentException(ErrorMessage.NOT_DIGIT.getMessage());
     }
 
-    public boolean notThousandMoneys(int money) {
-        if (money % 1000 != 0) {
-            return true;
+    public void notThousandMoneys(int money) {
+        if (money % 1000 == 0) {
+            return;
         }
         throw new IllegalArgumentException(ErrorMessage.NOT_THOUSAND_MONEY.getMessage());
     }
--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -3,16 +3,11 @@ package lotto.service;
 import lotto.exception.InputException;
 
 public class InputService {
-    public int inputMoneyToInteger(String inputMoney) {
-        moneyException(inputMoney);
-        return Integer.parseInt(inputMoney);
-    }
 
-    public boolean moneyException(String inputMoney) {
+    public int inputMoneyToInteger(String inputMoney) {
         InputException inputException = new InputException();
-        if (inputException.notDigitException(inputMoney)) {
-            return true;
-        }
-        return false;
+        inputException.notDigitException(inputMoney);
+        inputException.notThousandMoneys(Integer.parseInt(inputMoney));
+        return Integer.parseInt(inputMoney);
     }
 }
