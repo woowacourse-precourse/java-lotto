0c1c972e781e657376ede27ae8ab8b7e4b2c5489
anneu <b.imnuj@gmail.com>
2022-11-11 00:56:47 +0900

    test: 로또 구입금액이 1000원 이상인지 검증하는 기능 테스트

---
+++ src/test/java/lotto/LottoMachineTest.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoMachineTest {
+    @DisplayName("로또 구입금액이 1000원 미만일 경우 예외가 발생한다.")
+    @Test
+    void createLessThanMinMoney() {
+        assertThatThrownBy(() -> new LottoMachine(100))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
