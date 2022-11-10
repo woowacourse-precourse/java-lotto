3aa902bd252a3f78c077e73e58a8c5acfc0aef77
dohyun <ldh19738@gmail.com>
2022-11-10 13:20:56 +0900

    feat(WinningNumber): 당첨 번호와 보너스 번호 입력 받는 메세지 출력

---
+++ src/main/java/lotto/WinningNumber.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class WinningNumber {
+    public void printInputWinningNumberMessage() {
+        System.out.println("당첨 번호를 입력해 주세요");
+    }
+
+    public void printInputBonusNumberMessage() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}
