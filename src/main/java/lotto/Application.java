4e7a678677ad044fdb3a211461502ee441922f30
hojunking96 <junsong96@naver.com>
2022-11-10 15:54:35 +0900

    feat(java-lotto) : 구입 금액이 1000원으로 나누어 떨어지지 않는 예외 처리 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int money = Integer.parseInt(Console.readLine());
+        moneyInputError(money);
+    }
+
+    public static void moneyInputError(int money) {
+        if (money % 1000 != 0) {
+            System.out.println("금액 입력 오류입니다.");
+            throw new IllegalArgumentException();
+        }
     }
 }
