0295adbaf87dd8583863835f5f3e98624aef219c
minju412 <ln8847@ajou.ac.kr>
2022-11-11 04:10:39 +0900

    feat: define print methods

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -5,6 +5,12 @@ import static lotto.constant.MatchType.FIVE_MATCHES_WITH_BONUS;
 import static lotto.constant.MatchType.FOUR_MATCHES;
 import static lotto.constant.MatchType.SIX_MATCHES;
 import static lotto.constant.MatchType.THREE_MATCHES;
+import static lotto.constant.SystemMessage.EARNINGS_RATE_NOTIFICATION;
+import static lotto.constant.SystemMessage.ENTER_BONUS_NUMBER;
+import static lotto.constant.SystemMessage.ENTER_PURCHASE_COST;
+import static lotto.constant.SystemMessage.ENTER_WINNING_NUMBERS;
+import static lotto.constant.SystemMessage.PURCHASE_COUNT_NOTIFICATION;
+import static lotto.constant.SystemMessage.WINNING_STATISTICS;
 import static lotto.constant.SystemValue.FIVE_MATCHES_WITHOUT_BONUS_INDEX;
 import static lotto.constant.SystemValue.FIVE_MATCHES_WITH_BONUS_INDEX;
 import static lotto.constant.SystemValue.FOUR_MATCHES_INDEX;
@@ -25,5 +31,29 @@ public class Output {
 
     public static void printLotteryNumbers(List<Integer> lotteryNumbers) {
         System.out.println(lotteryNumbers.toString());
+    }
+
+    public static void enterPurchaseCost() {
+        System.out.println(ENTER_PURCHASE_COST);
+    }
+
+    public static void purchaseCountNotification(int purchaseCount) {
+        System.out.printf(PURCHASE_COUNT_NOTIFICATION, purchaseCount);
+    }
+
+    public static void enterWinningNumbers() {
+        System.out.println(ENTER_WINNING_NUMBERS);
+    }
+
+    public static void enterBonusNumber() {
+        System.out.println(ENTER_BONUS_NUMBER);
+    }
+
+    public static void winningStatistics() {
+        System.out.println(WINNING_STATISTICS);
+    }
+
+    public static void earningsRateNotification(double earningsRate) {
+        System.out.printf(EARNINGS_RATE_NOTIFICATION, earningsRate);
     }
 }
