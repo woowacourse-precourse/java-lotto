255f36d1d142c9282b9d029ebaaec9131b8b62a0
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-10 19:32:54 +0900

    refactor(WinningNumber): 불필요 생성자 삭제

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -4,13 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class WinningNumbers {
 	private static final int MIN_NUMBER = 1;
 	private static final int MAX_NUMBER = 45;
 	private static final String REGEX = "^[1-9]{1}$|^[1-3]{1}[0-9]{1}|^4{1}[0-5]{1}$";
-
 	private static final Pattern numberPattern = Pattern.compile(REGEX);
 	private static final String REGEX_ERROR_MESSAGE = "[ERROR] %d ~ %d 사이의 자연수만 입력해주세요.";
 	private static final String BONUS_ERROR_MESSAGE = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
@@ -18,20 +15,11 @@ public class WinningNumbers {
 	private final Lotto lottoNumbers;
 	private final int bonusNumber;
 
-	public WinningNumbers() {
-		String inputWinningNumbers = Console.readLine();
+	public WinningNumbers(String inputWinningNumbers, String inputBonusNumber) {
 		List<Integer> numbers = stringToList(inputWinningNumbers);
 		this.lottoNumbers = new Lotto(numbers);
 
-		String inputBonus = Console.readLine();
-		int bonusNumber = validateREGEX(inputBonus);
-		this.bonusNumber = validateBonus(bonusNumber);
-	}
-
-	public WinningNumbers(String testInput1, String testInput2) {
-		List<Integer> numbers = stringToList(testInput1);
-		this.lottoNumbers = new Lotto(numbers);
-		int bonusNumber = validateREGEX(testInput2);
+		int bonusNumber = validateREGEX(inputBonusNumber);
 		this.bonusNumber = validateBonus(bonusNumber);
 	}
 
