66925e00f6a4956b7446c1d860cf79f6baeed1f8
jinny-l <jinny.l.dev@gmail.com>
2022-11-10 21:01:30 +0900

    feat(Service): 당첨 기준에 부합하는지 확인하는 기능 구현

--- src/main/java/lotto/model/Service.java
+++ src/main/java/lotto/model/Service.java
@@ -1,8 +1,11 @@
 package lotto.model;
 
-public class Service {
+import java.util.ArrayList;
+import java.util.List;
+import lotto.constant.Constant;
+import lotto.util.WinnerInfo;
 
-    Player player = new Player();
+public class Service {
 
     public static int countLottoAmount(String purchaseAmount) {
         return Integer.parseInt(purchaseAmount) / 1000;
@@ -10,6 +13,33 @@ public class Service {
 
     public static long getProfitRates(int purchaseAmount, int winningAmount) {
         return winningAmount / purchaseAmount;
+    }
+
+    // TODO: 리팩토링 필요
+    public static int getLottoResult(
+            List<Integer> lottoNumbers, List<Integer> playerLottoNumbers, int playerBonusNumber) {
+
+        int count = 0;
+        List<Integer> intersection = new ArrayList<>(playerLottoNumbers);
+        intersection.retainAll(lottoNumbers);
+
+        if (intersection.size() == Constant.CHECK_BONUS_COUNT) {
+            playerLottoNumbers.removeAll(lottoNumbers);
+
+            if (playerLottoNumbers.get(0) == playerBonusNumber) {
+                return WinnerInfo.RANK2.getRank();
+            }
+            return WinnerInfo.RANK3.getRank();
+        }
+
+        if (intersection.size() == WinnerInfo.RANK1.getWinningCondition()) {
+            return WinnerInfo.RANK1.getRank();
+        }
+
+        if (intersection.size() == WinnerInfo.RANK4.getWinningCondition()) {
+            return WinnerInfo.RANK4.getRank();
+        }
+        return WinnerInfo.RANK5.getRank();
     }
 
 }
