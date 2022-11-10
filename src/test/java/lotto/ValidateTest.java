ee8d775a2a4415b6a1845eba10b0520b592265d5
eora21 <eora21@naver.com>
2022-11-10 22:04:34 +0900

    test: 💍 추가 로또 형태에 대한 테스트코드
    쉼표를 기준으로 6개로 나눠지지 않으면 로또 번호 형태가 아니다.

--- src/test/java/lotto/ValidateTest.java
+++ src/test/java/lotto/ValidateTest.java
@@ -20,4 +20,18 @@ class ValidateTest {
                 assertThrows(IllegalArgumentException.class, () -> validate.validDivisible(51_001));
         assertThat(exception.getMessage()).contains("나누어떨어지지");
     }
+
+    @Test
+    public void 로또_형태() {
+        assertThatCode(() -> validate.validWinLottoForm(new String[] {"1", "4", "7", "14", "28", "37"}))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    public void 로또_형태_아님() {
+        Throwable exception =
+                assertThrows(IllegalArgumentException.class, () ->
+                        validate.validWinLottoForm(new String[] {"1", "4", "7", "14", "28"}));
+        assertThat(exception.getMessage()).contains("입력값을 로또 형태로 전환할 수 없습니다.");
+    }
 }
