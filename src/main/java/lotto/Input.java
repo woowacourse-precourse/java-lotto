6adcad5c0b2a6205cc96a6dfb72e1b5ea32ab2bc
HyebinShin <hyeviin.dev@gmail.com>
2022-11-10 20:23:47 +0900

    Feat: 클래스 분리 - 로또 보너스 번호 입력
    로또 보너스 번호 입력

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.util.regex.Pattern;
 
 public class Input {
+    private static final String regexGroup = "([1-9]|[1-3][0-9]|4[0-5])";
     private Input() {
 
     }
@@ -39,11 +40,24 @@ public class Input {
     }
 
     private static void validateInputPrize(String prize) throws IllegalArgumentException {
-        String regexGroup = "([1-9]|[1-3][0-9]|4[0-5])";
         String pattern = "^("+regexGroup+",){5}"+regexGroup;
 
         if(!Pattern.matches(pattern, prize)) {
             throw new IllegalArgumentException(ErrMsg.VALIDATE_INPUT_PRIZE_NUM);
+        }
+    }
+
+    public static String inputBonus() throws IllegalArgumentException {
+        String bonus = input();
+
+        validateInputBonus(bonus);
+
+        return bonus;
+    }
+
+    private static void validateInputBonus(String bonus) throws IllegalArgumentException {
+        if(!Pattern.matches("^"+regexGroup, bonus)) {
+            throw new IllegalArgumentException(ErrMsg.VALIDATE_BONUS_NUM_ONE);
         }
     }
 }
