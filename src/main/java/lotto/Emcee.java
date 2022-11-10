c045d91dfd9f949ee69ea697ecbf616795527bec
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-10 10:40:27 +0900

    feat(guideGame): implement method
    Guide of the game.

--- src/main/java/lotto/Emcee.java
+++ src/main/java/lotto/Emcee.java
@@ -5,7 +5,12 @@ import java.util.List;
 public class Emcee {
 
     public void guideGame() {
-
+        String startingGuide = "Lotto game 에 오신 것을 환영합니다.\n" +
+                "원하시는 만큼의 lotto 를 구매할 수 있으며 lotto 는 자동 발행됩니다.\n" +
+                "당첨 번호와 보너스 번호를 생성하면 발행된 lotto 들의 당첨 여부를 계산합니다.\n" +
+                "최종적으로 당첨 통계와 수익률을 알려드립니다.\n" +
+                "게임을 시작하겠습니다.";
+        System.out.println(startingGuide);
     }
 
     public void guideAmountOfPurchaseMoney() {
