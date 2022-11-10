928bf4e5513023e5451600b6e5a2a5ad3f1b41ca
minhee020 <minhee020@naver.com>
2022-11-11 01:02:56 +0900

    feat : 등수를 카운트하는 함수 구현

--- src/main/java/lotto/WinningResult.java
+++ src/main/java/lotto/WinningResult.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WinningResult {
      WinningNumber winningNumber;
@@ -9,12 +10,12 @@ public class WinningResult {
           this.winningNumber = winningNumber;
      }
      public void winningHistory(List<Lotto> lottos) {
-          for (Lotto lotto : lottos) {
-               rankCount(lotto);
-          }
+          List<Integer> rank = rankCount(lottos);
      }
 
-     private Integer rankCount(Lotto lotto) {
-
+     private List<Integer> rankCount(List<Lotto> lottos) {
+          return lottos.stream()
+                  .map(winningNumber::match)
+                  .collect(Collectors.toList());
      }
 }
