fac0f982b62baf2c4f7e06615d68e4a6e8fc5b38
TaeMin <xoals3094@naver.com>
2022-11-10 18:53:02 +0900

    feat(Judgment): 로또 번호와 당첨 번호를 비교해 일치하는 번호 개수를 확인하는 기능 구현

---
+++ src/main/java/lotto/domain/Judgment.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Judgment {
+    public int compare(List<Integer> winningNumber, List<Integer> playerNumber){
+        int matchNumber = 0;
+        for(int number: playerNumber){
+            if(winningNumber.contains(number)){
+                matchNumber++;
+            }
+        }
+
+        return matchNumber;
+    }
+}
