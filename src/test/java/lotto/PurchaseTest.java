dcb9916bbc25ca98fad51322d4405242a6d4ee88
areyouhun <areyouhun@gmail.com>
2022-11-10 20:51:37 +0900

    test(purchase): 입력값이 천 원 단위가 아니면 예외 발생

--- src/test/java/lotto/PurchaseTest.java
+++ src/test/java/lotto/PurchaseTest.java
@@ -11,8 +11,17 @@ class PurchaseTest {
 
     @DisplayName("입력값에 숫자가 아닌 문자가 있으면 예외 발생")
     @ParameterizedTest
-    @ValueSource(strings = {"1300k", "이만원", "1ooo", "twentythousand"})
-    void nonNumericInput(String input) {
+    @ValueSource(strings = {"1300k", "이만원", "1ooo", "twentythousand", "!7000"})
+    void nonNumeric(String input) {
+        assertThatThrownBy(() -> new Purchase(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @DisplayName("입력값이 천 원 단위가 아니면 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"10005", "50010", "2080332900", "1300"})
+    void nonDividedByThousand(String input) {
         assertThatThrownBy(() -> new Purchase(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE);
