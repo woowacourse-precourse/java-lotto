7e1264d5828604a40f4d21f28b77006f7e1c97e9
jinny-l <jinny.l.dev@gmail.com>
2022-11-10 23:52:04 +0900

    feat(WinnerInfo): 메시지 출력을 위한 요소 추가
    - 이유:
    View에서 처리하려고 했으나 불가능하여 요소 추가

--- src/main/java/lotto/util/WinnerInfo.java
+++ src/main/java/lotto/util/WinnerInfo.java
@@ -2,19 +2,21 @@ package lotto.util;
 
 public enum WinnerInfo {
 
-    RANK1(1, 6, 2_000_000_000),
-    RANK2(2, 5, 30_000_000),
-    RANK3(3, 5, 1_500_000),
-    RANK4(4, 4, 50_000),
-    RANK5(5, 3, 5_000);
+    RANK1(1, 6, "6개 일치 (2,000,000,000원)", 2_000_000_000),
+    RANK2(2, 5, "5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000),
+    RANK3(3, 5, "5개 일치 (1,500,000원)", 1_500_000),
+    RANK4(4, 4, "4개 일치 (50,000원)", 50_000),
+    RANK5(5, 3, "3개 일치 (5,000원)", 5_000);
 
     private final int rank;
     private final int winningCondition;
+    private final String winningMessage; // TODO: 출력 때문에 추가한 부분인데, View에서 자체적으로 해결할 수 있는 방법이 있을지?
     private final int prize;
 
-    WinnerInfo(int rank, int winningCondition, int prize) {
+    WinnerInfo(int rank, int winningCondition, String winningMessage, int prize) {
         this.rank = rank;
         this.winningCondition = winningCondition;
+        this.winningMessage = winningMessage;
         this.prize = prize;
     }
 
@@ -24,6 +26,10 @@ public enum WinnerInfo {
 
     public int getWinningCondition() {
         return this.winningCondition;
+    }
+
+    public String getWinningMessage() {
+        return this.winningMessage;
     }
 
     public int getPrizeInformation() {
