811be255f3d85b3894f0c88e0ac24e05090be0c3
HyebinShin <hyeviin.dev@gmail.com>
2022-11-10 09:26:50 +0900

    Feat: 에러 메시지 클래스 분리
    [ERROR] 1,000 단위의 숫자만 입력해 주세요.
    [ERROR] 로또의 발행 횟수와 로또 발행 리스트의 크기는 같아야 합니다.
    [ERROR] 1부터 45까지의 숫자를 콤마(,)를 이용해서 6개 적어주세요.
    [ERROR] 로또 당첨 번호는 6개여야 합니다.
    [ERROR] 로또 당첨 번호는 서로 달라야 합니다.
    [ERROR] 1부터 45까지의 숫자를 1개만 적어주세요.
    [ERROR] 보너스 번호와 당첨 번호는 서로 달라야 합니다.

---
+++ src/main/java/lotto/ErrMsg.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class ErrMsg {
+    public static final String VALIDATE_INPUT_BUDGET = "[ERROR] 1,000 단위의 숫자만 입력해 주세요.";
+    public static final String VALIDATE_LOTTO_SIZE = "[ERROR] 로또의 발행 횟수와 로또 발행 리스트의 크기는 같아야 합니다.";
+    public static final String VALIDATE_INPUT_PRIZE_NUM = "[ERROR] 1부터 45까지의 숫자를 콤마(,)를 이용해서 6개 적어주세요.";
+    public static final String VALIDATE_PRIZE_NUM_SIX = "[ERROR] 로또 당첨 번호는 6개여야 합니다.";
+    public static final String VALIDATE_PRIZE_NUM_UNIQUE = "[ERROR] 로또 당첨 번호는 서로 달라야 합니다.";
+    public static final String VALIDATE_BONUS_NUM_ONE = "[ERROR] 1부터 45까지의 숫자를 1개만 적어주세요.";
+    public static final String VALIDATE_BONUS_NUM_UNIQUE = "[ERROR] 보너스 번호와 당첨 번호는 서로 달라야 합니다.";
+}
