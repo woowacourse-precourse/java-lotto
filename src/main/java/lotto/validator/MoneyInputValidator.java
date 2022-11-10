eec7836bdbc8bdbae1e16370183c90f74d6240e3
baexxbin <soo3886@gmail.com>
2022-11-11 01:03:19 +0900

    test(validator): 로또 구입 금액 입력값 유효성 검사 테스트 작성

--- src/main/java/lotto/validator/MoneyInputValidator.java
+++ src/main/java/lotto/validator/MoneyInputValidator.java
@@ -5,17 +5,15 @@ import static lotto.validator.ErrorMessage.*;
 import java.util.regex.Pattern;
 
 public class MoneyInputValidator {
-	private static final Pattern pattern = Pattern.compile("^[1-9]*?");
+	private static final Pattern pattern = Pattern.compile("[0-9]+");
 
 	public void runMoneyInputValidator(String inputMoney) {
-		int money = Integer.parseInt(inputMoney);
-
+		int money;
 		if (isNotConsistOneData(inputMoney)) {
 			throw new IllegalArgumentException(ONE_INPUT.getErrorMessage());
 		}
-		if (isNegativeNumber(money)) {
-			throw new IllegalArgumentException(NOT_POSITIVE_NUMBER.getErrorMessage());
-		}
+
+		money = Integer.parseInt(inputMoney);
 		if (isNotDivided(money)) {
 			throw new IllegalArgumentException(NOT_DIVIDED.getErrorMessage());
 		}
---
+++ src/test/java/lotto/validator/MoneyInputValidatorTest.java
@@ -0,0 +1,32 @@
+package lotto.validator;
+
+import static lotto.validator.ErrorMessage.*;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MoneyInputValidatorTest {
+	@DisplayName("구입금액 입력값이 하나의 숫자입력인지 확인")
+	@Test
+	void isNotConsistOneData() {
+		//	given
+		MoneyInputValidator moneyInputValidator = new MoneyInputValidator();
+
+		assertThatThrownBy(() -> moneyInputValidator.runMoneyInputValidator("1,2,3,4,5,6"))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(ONE_INPUT.getErrorMessage());
+	}
+
+	@DisplayName("구입금액 입력값이 1000으로 나누어 떨어지는지 확인")
+	@Test
+	void isNotDivided() {
+		// given
+		MoneyInputValidator moneyInputValidator = new MoneyInputValidator();
+
+		assertThatThrownBy(() -> moneyInputValidator.runMoneyInputValidator("12500"))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(NOT_DIVIDED.getErrorMessage());
+	}
+
+}
