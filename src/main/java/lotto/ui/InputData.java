2393f63e99e702748879f7c89d72cd10d9973606
lee <whddlsk123@naver.com>
2022-11-11 01:09:58 +0900

    feat(readPurhchaseAmount) : 로또 구매금액을 입력받은뒤 반환

--- src/main/java/lotto/ui/InputData.java
+++ src/main/java/lotto/ui/InputData.java
@@ -1,16 +1,26 @@
 package lotto.ui;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputData {
-    private static Scanner scanner;
+    private static Scanner scanner = new Scanner(System.in);
 
     public String readInput() {
-        scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
 
         return userInput;
     }
 
+    public int readPurchaseAmount() {
+        System.out.println(Messages.INPUT_MONEY.getMessage());
+        int amount = Integer.parseInt(scanner.nextLine());
+
+        return amount;
+    }
+
+
+    
 }
---
+++ src/main/java/lotto/ui/Test.java
@@ -0,0 +1,9 @@
+package lotto.ui;
+
+public class Test {
+    public static void main(String[] args) {
+        InputData inputData = new InputData();
+
+        System.out.println(inputData.readWinningNumber());
+    }
+}
