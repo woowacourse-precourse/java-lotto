9a566258acc24e6310b1ec637bc43e5aacdb80ea
KimJaeSung <behappy0916@naver.com>
2022-11-11 00:52:35 +0900

    feat: 입력 받은 Money 생성 및 기능 구현
    입력받은 money를 int로 변환해주는 기능 구현
    money가 1000원으로 나누어 떨어지지 않으면 예외 처리 구현
    money가 음수이거나 1000원 이하일 때 예외 처리 기능 구현

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+
+public class Money {
+    private static final int LOTTO_PRICE = 1000;
+    private static final String AGAIN_INPUT_NUMBER = "[ERROR] 숫자만 입력해주세요.";
+    private static final String NOT_VALID_MONEY = "[ERROR] 1000원 단위로 입력해주세요.";
+    private final int money;
+
+    public Money(String money) {
+        this.money = convertToInt(money);
+        validateMoney(this.money);
+    }
+
+    public void validateMoney(int money) {
+        if(calculator(money)){
+            throw new IllegalArgumentException(NOT_VALID_MONEY);
+        }
+    }
+
+    private boolean calculator(int money) {
+        return money % LOTTO_PRICE != 0 || money < 0;
+    }
+
+    private int convertToInt(String money) {
+        try {
+            return Integer.parseInt(money);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(AGAIN_INPUT_NUMBER);
+        }
+    }
+
+}
