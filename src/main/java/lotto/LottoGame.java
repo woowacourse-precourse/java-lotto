de522be123d5ce13a852aac99a0c43ccaad2ed2d
wugawuga <dnr3655@naver.com>
2022-11-11 01:31:29 +0900

    refactor: 로또 생성 클래스 분리

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,22 +1,16 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class LottoGame {
+    private final LottoGenerator generator = new LottoGenerator();
+
     public void start() {
         Print.money();
         int lottoTickets = Input.purchaseAmount();
         Print.purchaseLotto(lottoTickets);
 
-        List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < lottoTickets; i++) {
-            List<Integer> random = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            Collections.sort(random);
-            lottos.add(new Lotto(random));
-        }
+        List<Lotto> lottos = generator.makeLottos(lottoTickets);
         Print.lottos(lottos);
     }
 }
---
+++ src/main/java/lotto/LottoGenerator.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoGenerator {
+    public List<Lotto> makeLottos(int lottoTickets) {
+        checkPositive(lottoTickets);
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoTickets; i++) {
+            List<Integer> random = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(random);
+            lottos.add(new Lotto(random));
+        }
+        return lottos;
+    }
+
+    private void checkPositive(int lottoTickets) {
+        if (lottoTickets < 1) {
+            throw new IllegalArgumentException("[ERROR] 로또 개수가 1미만일 수는 없습니다.");
+        }
+    }
+}
