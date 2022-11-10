ec093305fee12355f4201e23302a01ae9525b913
chlwodyd9829 <chlwodyd9829@naver.com>
2022-11-10 17:49:06 +0900

    feat: 맞춘 갯수 식별하기 위한 멤버 변수 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,11 +2,17 @@ package lotto.domain;
 
 public enum Money {
 
-    FIRST(2_000_000_000),SECOND(30_000_000),THIRD(1_500_000),FOURTH(50_000),FIFTH(5_000);
+    FIRST(2_000_000_000,6),SECOND(30_000_000,7),THIRD(1_500_000,5),FOURTH(50_000,4),FIFTH(5_000,3);
     private final Integer description;
+    private final Integer count;
 
-    Money(Integer description) {
+    Money(Integer description,Integer count) {
+        this.count = count;
         this.description = description;
+    }
+
+    public Integer getCount() {
+        return count;
     }
 
     public Integer getDescription() {
