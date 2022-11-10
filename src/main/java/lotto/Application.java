88eba695899de664c4b6c58fa8ef4b1738e96aba
Sungjaeyeong <wodud2587@gmail.com>
2022-11-10 18:16:28 +0900

    feat(lotto): 올바른 금액을 입력하지 않을 경우 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
 public class Application {
+    public static final String INVALID_AMOUNT_ERROR_MESSAGE = "[ERROR] 올바른 금액을 입력해주세요.";
+    public static final String NOT_NUMBER_ERROR_MESSAGE = "[ERROR] 숫자만 입력해주세요.";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
     }
+
+    public static void isValidAmount(String enterValue) {
+        for (int i = 0; i < enterValue.length(); i++) {
+            if (!('0' <= enterValue.charAt(i) && enterValue.charAt(i) <= '9')) {
+                throw new IllegalArgumentException(NOT_NUMBER_ERROR_MESSAGE);
+            }
+        }
+        if (Integer.parseInt(enterValue) % 1000 != 0) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_ERROR_MESSAGE);
+        }
+    }
+
 }
