decef19b3ca34624d60186286433be9ff7dd29f6
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-11-10 11:44:49 +0900

    feat : 사용자가 값을 입력하면 해당 개수의 로또 구매 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,33 +1,44 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
 
-        List<Integer> numbers = new ArrayList<>();
-        getLottoNumber(numbers);
 
-        Lotto lotto = new Lotto(numbers);
 
-        System.out.println(numbers);
-        System.out.println(lotto);
+        List<List<Integer>> lotto_list = new ArrayList<>();
+
+
+        getPurchaseAmount(lotto_list);
+
+
 
         // TODO: 프로그램 구현
     }
 
-    public static void getLottoNumber(List<Integer> numbers){
-        while(numbers.size() < 6){
-            int b = Randoms.pickNumberInRange(1,45);
-            if(!numbers.contains(b))
-                numbers.add(b);
-        }
-        Collections.sort(numbers);
+    public static void getPurchaseAmount(List<List<Integer>> lotto_list){
+        int purchase_price = 0;
+        int lotto_amount = 0;
+        System.out.println("구입금액을 입력해 주세요.");
+        purchase_price = Integer.parseInt(Console.readLine());
+        lotto_amount = purchase_price/1000;
+        getLottoNumber(lotto_list, lotto_amount);
     }
 
 
+    public static void getLottoNumber(List<List<Integer>> lotto_list, Integer lotto_amount){
+        List<Integer> numbers;
+        while(lotto_list.size() < lotto_amount) {
+            numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers);
+            lotto_list.add(numbers);
+        }
+    }
 }
