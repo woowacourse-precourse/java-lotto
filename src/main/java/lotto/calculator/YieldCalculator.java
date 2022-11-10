a5daf69304a1c422baabd76f6b75d4fc22787834
K-Diger <kitten.diger@gmail.com>
2022-11-10 17:36:15 +0900

    feat : getYield 메서드 구현 - 총 수익률을 출력하는 메서드 구현

--- src/main/java/lotto/calculator/YieldCalculator.java
+++ src/main/java/lotto/calculator/YieldCalculator.java
@@ -1,5 +1,6 @@
 package lotto.calculator;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class YieldCalculator {
@@ -10,5 +11,9 @@ public class YieldCalculator {
         System.out.printf("4개 일치 (50,000원) - %d개\n", winCount.get(1));
         System.out.printf("5개 일치 (1,500,000원) - %d개\n", winCount.get(2));
         System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", winCount.get(3));
+    }
+
+    public void getYield(BigDecimal yield) {
+        System.out.printf("총 수익률은 %s" + "%입니다.", yield);
     }
 }
