948f0b13fa9ff4c97d739d0fb9c0fb56e294a388
bsh1603 <bsh1603@github.com>
2022-11-10 19:39:49 +0900

    Refactor: errorcode 삭제

--- src/main/java/exception/MoneyExceptionType.java
+++ src/main/java/exception/MoneyExceptionType.java
@@ -1,20 +1,14 @@
 package exception;
 
 public enum MoneyExceptionType{
-    NOT_THOUSAND_MONEY(600, "[Error] 구입 금액이 1000원 단위가 아닙니다."),
-    NO_NUMBER_MONEY(601, "[ERROR] 구입 금액은 숫자여야 합니다."),
-    NOT_MONEY_NEGATIVE(602, "[ERROR] 구입 금액은 항상 양수여야 합니다.");
+    NOT_THOUSAND_MONEY("[Error] 구입 금액이 1000원 단위가 아닙니다."),
+    NO_NUMBER_MONEY("[ERROR] 구입 금액은 숫자여야 합니다."),
+    NOT_MONEY_NEGATIVE("[ERROR] 구입 금액은 항상 양수여야 합니다.");
 
-    private int errorCode;
     private String errorMessage;
 
-    MoneyExceptionType(int errorCode, String errorMessage) {
-        this.errorCode = errorCode;
+    MoneyExceptionType(String errorMessage) {
         this.errorMessage = errorMessage;
-    }
-
-    public int getErrorCode() {
-        return this.errorCode;
     }
 
     public String getErrorMessage() {
