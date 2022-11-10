d06b2912f62e22701dcbfcb140b50584f5052a64
areyouhun <areyouhun@gmail.com>
2022-11-11 01:25:52 +0900

    refactor(purchase): 원시값 포장

--- src/main/java/lotto/Purchase.java
+++ src/main/java/lotto/Purchase.java
@@ -3,13 +3,20 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Purchase {
-    private final Integer price;
+    private final static String ERROR_TITLE = "[ERROR] ";
+    private final static String NON_NUMERIC = "숫자 이외의 값이 입력되었습니다. 문자나 띄어쓰기 등이 들어갔는지 확인해주세요.";
+    private final static String NOT_IN_THOUSANDS = "금액은 천 원 단위로만 입력할 수 있습니다.";
     private final static String ONLY_NUMBER = "^\\d+$";
+
+    private final static int PRICE_UNIT = 1000;
+    private final static int EXPECTED_REMAINDER = 0;
+
+    private final Integer price;
 
     public Purchase(String input) {
         validateIfNumeric(input);
         Integer price = Integer.valueOf(input);
-        validateIfDividedByThousand(price);
+        validateIfInThousands(price);
 
         this.price = price;
     }
@@ -21,15 +28,13 @@ public class Purchase {
     private void validateIfNumeric(String input) {
         boolean isNumber = input.matches(ONLY_NUMBER);
         if (!isNumber) {
-            throw new IllegalArgumentException("[ERROR] 금액은 숫자만 입력할 수 있습니다.");
+            throw new IllegalArgumentException(ERROR_TITLE + NON_NUMERIC);
         }
     }
 
-    private void validateIfDividedByThousand(Integer price) {
-        final int expectedRemainder = 0;
-        final int currencyUnit = 1000;
-        if (price % currencyUnit != expectedRemainder) {
-            throw new IllegalArgumentException("[ERROR] 금액은 천 원 단위만 입력할 수 있습니다.");
+    private void validateIfInThousands(Integer price) {
+        if (price % PRICE_UNIT != EXPECTED_REMAINDER) {
+            throw new IllegalArgumentException(ERROR_TITLE + NOT_IN_THOUSANDS);
         }
     }
 }
