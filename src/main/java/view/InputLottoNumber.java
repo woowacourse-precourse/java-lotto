bd7968e49fa57d15ccf204055f4e13e2295826ee
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-10 14:16:21 +0900

    feat(lotto): 당첨 번호 입력 기능 구현

--- src/main/java/view/InputLottoNumber.java
+++ src/main/java/view/InputLottoNumber.java
@@ -1,2 +1,21 @@
-package view;public class InputLottoNumber {
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+import domain.Lotto;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputLottoNumber {
+
+    public static void inputLottoNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String Lotto = Console.readLine();
+        String[] split = Lotto.split(",");
+        List<Integer> lottoList = new ArrayList<>();
+        for (String s : split) {
+            lottoList.add(Integer.parseInt(s));
+        }
+        Lotto lotto = new Lotto(lottoList);
+    }
+
 }
