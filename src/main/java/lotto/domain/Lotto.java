e8f1fba22c5cb8a2e67b81fb49860ff7eddfcca6
CMHyuk <alsgur9710@naver.com>
2022-11-11 00:04:53 +0900

    feat(Lotto): 로또 번호 검증, 정렬

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+        this.numbers.sort(Comparator.naturalOrder());
+    }
+
+    private void validate(List<Integer> numbers) {
+        isCorrectSize(numbers);
+        isDuplication(numbers);
+        isCorrectNumber(numbers);
+    }
+
+    private void isCorrectNumber(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (number > 45 || number < 1) {
+                throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        });
+    }
+
+    private void isDuplication(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("중복된 숫자는 입력할 수 없습니다.");
+        }
+    }
+
+    private void isCorrectSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("숫자 6개를 입력해주세요.");
+        }
+    }
+}
