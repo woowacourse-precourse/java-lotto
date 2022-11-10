d5172216c5c82879c9dde7b2acccfc30676d80ba
sangwon <swchoi1997@naver.com>
2022-11-10 21:43:43 +0900

    add: 테스트를 위한 생성자 추가

--- src/main/java/lotto/domain/model/Bonus.java
+++ src/main/java/lotto/domain/model/Bonus.java
@@ -3,10 +3,16 @@ package lotto.domain.model;
 import static lotto.utils.Advice.BonusValidator.checkRange;
 import static lotto.utils.Advice.BonusValidator.checkSizeAndNumber;
 
+import java.util.Objects;
+
 
 public class Bonus {
 
     private final Integer bonus;
+
+    public Bonus(Integer bonus) {
+        this.bonus = bonus;
+    }
 
     public Bonus(FirstPlace firstPlace, String bonus) {
         validate(bonus);
@@ -17,5 +23,22 @@ public class Bonus {
     private static void validate(String bonus) {
         checkSizeAndNumber(bonus);
         checkRange(bonus);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Bonus bonus1 = (Bonus) o;
+        return Objects.equals(bonus, bonus1.bonus);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bonus);
     }
 }
