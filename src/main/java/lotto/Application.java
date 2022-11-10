18a09511d2d6f9956874784d18e7345a52fbd133
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-10 15:20:48 +0900

    refactor(lotto): 빼먹었던 금액 입력시 출력문 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,6 +9,7 @@ public class Application {
     public static void main(String[] args) throws IllegalArgumentException{
         // TODO: 프로그램 구현
         BuyLottoList buyLottoList = new BuyLottoList();
+        System.out.println("구입 금액을 입력해주세요.");
         int money = InputUserMoney.inputMoney();
         if(money== ExceptionNumber.EXCEPTION_CODE.getCode()){
             return;
