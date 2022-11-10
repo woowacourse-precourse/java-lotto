3112eab7a13a30dd54d7b1afe1252c09c3cfc8d7
im-shung <seoyoung1113@gmail.com>
2022-11-10 23:42:39 +0900

    feat: :sparkles: 컴퓨터의 수(1자리)와 플레이어의 수(1자리)를 비교하는 기능 구현

--- src/main/java/lotto/domain/Referee.java
+++ src/main/java/lotto/domain/Referee.java
@@ -5,7 +5,11 @@ import java.util.List;
 public class Referee {
 
     public static void compareLotto(List<Integer> computer, List<Integer> player) {
-        int correctCount =  Judgment.getCorrectCount(computer, player);
+        int correctCount = Judgment.getCorrectCount(computer, player);
+    }
+
+    public static boolean compareBonusNumber(int computer, int player) {
+        return computer == player;
     }
 
 }
