fdbbb3b0da347f6bd23195781f5093f6f4c95ef9
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-10 15:59:37 +0900

    feat: 로또 당첨 결과를 출력하는 기능
    LottoView
     - printLottoResult 메소드 추가
    
    LottoResult
     - status 변수 추가
    - printMessage 메소드 추가

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,17 +1,30 @@
 package lotto;
 
+import java.text.DecimalFormat;
+import java.util.Map;
+
 public enum LottoResult {
 
-    ELSE(0),
-    THREE( 5_000),
-    FOUR(50_000),
-    FIVE(1_500_000),
-    SIX(30_000_000),
-    FIVE_WITH_BONUS(2_000_000_000);
+    ELSE(0, "3개 미만"),
+    THREE( 5_000, "3개 일치"),
+    FOUR(50_000, "4개 일치"),
+    FIVE(1_500_000, "5개 일치"),
+    FIVE_WITH_BONUS(2_000_000_000, "5개 일치, 보너스 볼 일치"),
+    SIX(30_000_000, "6개 일치");
 
     private int payout;
+    private String status;
 
-    LottoResult(int payout) {
+    LottoResult(int payout, String status) {
         this.payout = payout;
+        this.status = status;
+    }
+
+    public void printMessage(Map<LottoResult, Long> lottoResultToCount) {
+        DecimalFormat df = new DecimalFormat("###,###");
+        String message = status + " " +
+                "(" + df.format(payout) + "원)" +
+                " - " + lottoResultToCount.getOrDefault(this, 0L) + "개";
+        System.out.println(message);
     }
 }
--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -1,15 +1,29 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class LottoView {
 
     private static final String LOTTO_NUMBER_MESSAGE = "개를 구매했습니다.";
+    private static final int SKIP_RESULT_ELSE_CASE = 1;
 
     public void printLottoInfo(List<Lotto> lottoBundle) {
         System.out.println(lottoBundle.size() + LOTTO_NUMBER_MESSAGE);
         for (Lotto lotto : lottoBundle) {
             lotto.printNumbers();
         }
+    }
+
+    public void printLottoResult(List<LottoResult> lottoResults) {
+        Map<LottoResult, Long> lottoResultToCount = lottoResults.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        Arrays.stream(LottoResult.values())
+                .skip(SKIP_RESULT_ELSE_CASE)
+                .forEach(lottoResult -> lottoResult.printMessage(lottoResultToCount));
     }
 }
