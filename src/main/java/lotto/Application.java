9f8c795bf11d8065d341eba5f1a3542817c3764d
im-shung <seoyoung1113@gmail.com>
2022-11-10 20:02:39 +0900

    feat: :sparkles: 1부터 45까지의 서로 다른 임의의 수 6개를 생성하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.NumberGenerator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Lotto computer = new Lotto(NumberGenerator.createDifferentRandomNumbers());
     }
 }
