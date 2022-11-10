1b11ff77fdffb315d9c56c4ce45d56c78f9be9b0
hodomaroo <enghemdwkd@gmail.com>
2022-11-10 21:44:43 +0900

    [Feat] : LottoGrade Enum 구현 완료
    LottoGrade의 1등 ~ 5등 / 꽝을 나타내는 상수 객체 (FIRST ~ FIFTH /
    FAIL)의 선언을 완료하였습니다.
    
    일치하는 숫자의 개수 / 보너스 숫자와의 일치여부를 입력받아 알맞은 등수
    객체를 반환하는 getMatchGrade 메서드의 구현을 완료하였습니다

--- src/main/java/lotto/LottoGrade.java
+++ src/main/java/lotto/LottoGrade.java
@@ -1,7 +1,37 @@
 package lotto;
 
+import java.text.DecimalFormat;
+import java.text.Format;
+import java.util.Arrays;
+import java.util.Formatter;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 /**
  * 가능한 로또 등수 정보를 관리하는 Enum
  */
 public enum LottoGrade {
+    FIRST(1, 6, 2_000_000_000, false),
+    SECOND(2, 5, 30_000_000, true),
+    THIRD(3, 5, 1_500_000, false),
+    FOURTH(4, 4, 50_000, false),
+    FIFTH(5, 3, 5_000, false),
+    FAIL(6, 0, 0, false);
+
+    private final int grade;
+    private final int matchNumberCount;
+    private final int prize;
+    private final boolean matchBonus;
+
+    LottoGrade(int grade, int matchNumberCount, int prize, boolean matchBonus) {
+        this.grade = grade;
+        this.matchNumberCount = matchNumberCount;
+        this.prize = prize;
+        this.matchBonus = matchBonus;
+    }
+
+    public static LottoGrade getMatchGrade(int matchNumberCount, boolean matchBonus) {
+        return Arrays.stream(LottoGrade.values()).filter((grade) -> grade.matchBonus == matchBonus)
+                .filter((grade) -> grade.matchNumberCount == matchNumberCount).findFirst().orElse(FAIL);
+    }
 }
