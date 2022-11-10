b55ac99fa2700cb54caa479e2ec3a0d57a23c322
wonju-dev <wonjulee.dev@gmail.com>
2022-11-10 21:18:58 +0900

    feat(MoneyDivisionCond): 사용자 입력 금액 검증 로직 구현
    MoneyDivisionCond는 Condition인터페이스의 구현체입니다.
    해당 클래스에 구현된 isSatisfied 메소드를 통해, 사용자가 입력한 금액이
    1000원으로 나누어 떨어지는지 검증할 수 있습니다.

---
+++ src/main/java/lotto/userinterface/condition/MoneyDivisionCond.java
@@ -0,0 +1,19 @@
+package lotto.userinterface.condition;
+
+public class MoneyDivisionCond implements Condition {
+
+    private static final Integer UNIT = 1000;
+    private static final Condition instance = new MoneyDivisionCond();
+
+    private MoneyDivisionCond() {
+    }
+
+    public static Condition getInstance() {
+        return instance;
+    }
+
+    @Override
+    public Boolean isSatisfied(String input) {
+        return Integer.parseInt(input) % UNIT == 0;
+    }
+}
