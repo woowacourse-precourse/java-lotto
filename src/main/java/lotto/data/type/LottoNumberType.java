3c732a6afe6c2d9f0df42fd16bb0ad8d5f40c820
TaemHam <tk4003950@gmail.com>
2022-11-11 00:53:02 +0900

    Feat: lotto.data.type.LottoNumberType 구현

---
+++ src/main/java/lotto/data/type/LottoNumberType.java
@@ -0,0 +1,17 @@
+package lotto.data.type;
+
+public enum LottoNumberType {
+    COUNT(6),
+    MIN(1),
+    MAX(45);
+
+    private final int value;
+
+    LottoNumberType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
