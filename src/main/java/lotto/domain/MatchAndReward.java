b8917a203c2118787c1d10ec275b02e5db8fa561
2jun0 <soo28819@gmail.com>
2022-11-11 02:40:06 +0900

    Refactor(MatchAndReward): 보너스 점수로도 검색할 수 있게 수정

--- src/main/java/lotto/domain/MatchAndReward.java
+++ src/main/java/lotto/domain/MatchAndReward.java
@@ -1,28 +1,39 @@
 package lotto.domain;
 
 public enum MatchAndReward {
-    FIRST(6, 2_000_000_000, 0),
-    SECOND(5, 30_000_000, 1),
-    THIRD(5, 1_500_000, 0),
-    FOURTH(4, 50_000, 0),
-    FIFTH(4, 5_000, 0);
+    FIRST(6, 2_000_000_000, false),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(4, 5_000, false);
 
-    public final int reward;
-    public final int match;
-    public final int bonusMatch;
+    private final int reward;
+    private final int match;
+    private final boolean requireBonus;
 
-    MatchAndReward(int match, int reward, int bonusMatch) {
+    MatchAndReward(int match, int reward, boolean requireBonus) {
         this.reward = reward;
         this.match = match;
-        this.bonusMatch = bonusMatch;
+        this.requireBonus = requireBonus;
     }
 
-    public static MatchAndReward getByMatch(int match) {
+    public static MatchAndReward find(int match, boolean isBonusMatched) {
+        if (isBonusMatched) {
+            for (MatchAndReward matchAndReward : MatchAndReward.values()) {
+                if (matchAndReward.requireBonus && matchAndReward.match == match)
+                    return matchAndReward;
+            }
+        }
+
         for (MatchAndReward matchAndReward : MatchAndReward.values()) {
-            if (matchAndReward.match == match)
+            if (!matchAndReward.requireBonus && matchAndReward.match == match)
                 return matchAndReward;
         }
 
         return null;
+    }
+
+    public int getReward() {
+        return reward;
     }
 }
