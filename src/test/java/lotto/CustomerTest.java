5c342fa7938484498efc7d7710d3dc2c759641e3
juhwang <juhong4930@naver.com>
2022-11-11 00:29:05 +0900

    test(CustomerTest): count the duplicate lotto numbers

--- src/test/java/lotto/CustomerTest.java
+++ src/test/java/lotto/CustomerTest.java
@@ -2,8 +2,9 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import user.Customer;
+import lotto.user.Customer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
@@ -12,28 +13,40 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CustomerTest {
 
-    @DisplayName("입력값이 숫자인 경우 Customer 객체 생성")
+    @DisplayName("입력값이 숫자인 경우 Customer 객체를 생성한다.")
     @Test
     void case1_validate_unit() {
         assertThat(new Customer("1000"))
                 .isInstanceOf(Customer.class);
     }
 
-    @DisplayName("입력값이 숫자가 아닌 아닌 경우 예외 발생")
+    @DisplayName("입력값이 숫자가 아닌 아닌 경우 예외가 발생한다.")
     @Test
     void case2_validate_unit() {
         assertThatThrownBy(() -> new Customer("1000j"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 구매내역 출력")
+    @DisplayName("로또 구매내역을 출력한다.")
     @Test
     void case3_print_out_lotto_list() {
         assertRandomUniqueNumbersInRangeTest(() -> {
             Customer input = new Customer("1000");
-            String result = "1개를 구매했습니다.\n" + List.of(1, 2, 3, 4, 5, 6) + "\n";
+            String result = "1개를 구매했습니다.\n" + List.of(1, 2, 3, 4, 5, 6);
 
             assertThat(input.toLottoString()).isEqualTo(result);
         }, List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @DisplayName("당첨번호와 비교 후 맞는 번호 개수를 출력한다.")
+    @Test
+    void case4_count_the_duplicate_lotto_numbers() {
+        assertRandomUniqueNumbersInRangeTest(() -> {
+            Customer input = new Customer("2000");
+            List<Integer> winNumber = List.of(1,2,3,4,5,6);
+            List<Integer> result = List.of(6,4);
+
+            assertThat(input.determineRanking(winNumber)).isEqualTo(result);
+        }, List.of(1, 2, 3, 4, 5, 6),List.of(3,4,5,6,7,8));
     }
 }
