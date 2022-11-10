b2e682af23b126e4ea3b5a0649b0cfc2b3b670f3
2jun0 <soo28819@gmail.com>
2022-11-11 04:14:23 +0900

    Fix(Console): 이상하게 포매팅하는 오류 수정

--- src/main/java/lotto/console/OutputFormatter.java
+++ src/main/java/lotto/console/OutputFormatter.java
@@ -3,10 +3,10 @@ package lotto.console;
 class OutputFormatter {
 
     static String formatMoney(int money) {
-        return String.format(String.valueOf(money), "%,.2f", money);
+        return String.format("%,d", money);
     }
 
     static String formatPercent(float number) {
-        return String.format(String.valueOf(number), ".1f%%", number*100f);
+        return String.format("%.1f%%", number*100f);
     }
 }
