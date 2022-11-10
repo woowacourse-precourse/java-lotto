2f1fc68afbe91e7c59ee9e2bccd8fe4578ae5673
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-11-10 17:05:57 +0900

    feat(lotto) : 스켈레톤 코드 생성

---
+++ src/main/java/lotto/domain/User.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class User {
+    public List<Integer> randomLotto(int price) {
+
+        return null;
+    }
+}
---
+++ src/main/java/lotto/domain/Winning.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+public class Winning {
+
+    public void score() {
+
+    }
+
+    public double rate() {
+        return 0;
+    }
+}
