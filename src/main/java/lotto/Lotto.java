8bd832f6125346d1b28749f8b40ab771da1165c5
ma-an-jong <alswhd1113@gmail.com>
2022-11-10 15:59:25 +0900

    feat(Lotto): 로또 번호의 유효성을 검사하는 기능 추가
    BREAKING CHANGE: 로또 번호가 길이가 6이 아니거나 중복된 숫자가 나타날 경우 예외를 생성하는
    메소드를 추가
    
    Before:
    
        Lotto
        {
            validate
        }
    
    After:
    
        Lotto
        {
            validate
            lottoSizeValidation
            duplicationValidation
        }
    
        로또 번호의 유효성을 검사하기 위해 길이 검사, 중복 검사 메소드를 추가함

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,44 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<Integer> lottoNumbers;
+    private static final Integer LOTTO_SIZE = 6;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.lottoNumbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        lottoSizeValidation(numbers);
+        duplicationValidation(numbers);
+    }
+
+    private void lottoSizeValidation(List<Integer> numbers)
+    {
+        if (numbers.size() != LOTTO_SIZE)
+        {
+            throw new IllegalArgumentException(Message.getLottoSizeErrorMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void duplicationValidation(List<Integer> numbers)
+    {
+        Set<Integer> lottoNumberGroup = new HashSet<>();
+
+        for(int lottoNumber : numbers)
+        {
+            if(lottoNumberGroup.contains(lottoNumber))
+            {
+                throw new IllegalArgumentException(Message.getLottoNumberDuplicationErrorMessage());
+            }
+
+            lottoNumberGroup.add(lottoNumber);
+        }
+    }
+
 }
