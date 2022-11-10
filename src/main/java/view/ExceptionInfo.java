49d5b284d34a6c06e13f24dba2281632970a9e9e
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-10 13:23:51 +0900

    refactor(lotto): Enum생성에 따라 에러 메시지 출력문 하나로 통합

--- src/main/java/view/ExceptionInfo.java
+++ src/main/java/view/ExceptionInfo.java
@@ -1,22 +1,14 @@
 package view;
 
+import constant.ErrorMessage;
+
 public class ExceptionInfo {
-
-    public static String ERROR_MESSAGE = "[ERROR]";
-    public static String ERROR_NOT_NUMBER = ERROR_MESSAGE + "숫자가 아닌 것이 입력되었습니다.";
-    public static String ERROR_INCORRECT_MONEY= ERROR_MESSAGE + "올바르지 못한 입력 금액입니다.";
-
     public static final int EXCEPTION_DETECT=-1;
 
 
-    public static void printInCorrectMoney(){
-        System.out.println(ERROR_INCORRECT_MONEY);
+    public static void printException(Exception e){
+        System.out.println(e.getMessage());
     }
-
-    public static void printNotNumber(){
-        System.out.println(ERROR_NOT_NUMBER);
-    }
-
 
 
 }
