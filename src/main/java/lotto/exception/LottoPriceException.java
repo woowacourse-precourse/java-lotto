cbe8651a9530bad074f8caa0bf8bc9678c2c5c2a
YuHayeon <youha0930@gmail.com>
2022-11-10 22:09:07 +0900

    feat(LottoPriceException): 로또 금액이 1000 단위가 아닐 경우 예외처리
    로또 1개의 금액은 1000원이므로 입력된 금액이 1000단위가 아닐 경우 예외처리 후
    1000단위의 금액을 입력해달라고 출력

--- src/main/java/lotto/exception/LottoPriceException.java
+++ src/main/java/lotto/exception/LottoPriceException.java
@@ -7,6 +7,7 @@ public class LottoPriceException {
         checkHasBlankInLottoPrice(priceInput);
         Integer price = changePriceToInteger(priceInput);
         checkLessThanOneThousand(price);
+        checkThousandUnits(price);
     }
 
     public static void checkEmptyLottoPrice(String price) {
@@ -31,6 +32,12 @@ public class LottoPriceException {
     public static void checkLessThanOneThousand(Integer price) {
         if (price < 1000) {
             throw new IllegalArgumentException("[ERROR] 로또 1장은 1000원입니다. 1000원 보다 더 높은 금액을 입력해주세요.");
+        }
+    }
+
+    public static void checkThousandUnits(Integer price) {
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 1장은 1000원입니다. 1000단위의 금액을 입력해주세요.");
         }
     }
 }
