b90d4791da920ffcd4087e53a2b205c177662115
ma-an-jong <alswhd1113@gmail.com>
2022-11-10 18:02:40 +0900

    test(LottoTest): 생성한 로또가 유효한지 검증하는 테스트를 작성
    BREAKING CHANGE: 로또 생성과정에서 로또 번호의 개수나 중복된 숫자, 범위를 벗어난 숫자가 있는지 유효성 검사를 하는
    테스트를 진행하는 코드를 작성
    
    Before:
    
        LottoTest
        {
            createLottoByOverSize
            createLottoByDuplicatedNumber
        }
    
    After:
    
        LottoTest
        {
            createLottoByOverSize
            createLottoByDuplicatedNumber
            createLottoByOutOfBoundNumber : 값을 벗어났을때 예외를 확인하는 테스트
            createLotto : 정상 케이스를 테스트
            createWinningLotto : 정상 케이스의 당첨 로또를 생성하는 테스트
            createDuplicationWinningLotto : 중복된 보너스 숫자가 존재하는 당첨 로또를 테스트
            createOutOfBoundWinningLotto : 보너스 숫자가 값을 벗어나는 경우를 테스트
        }
    
        로또와 당첨 로또가 상황에 맞게 생성되거나 예외를 반환하는지 테스트하는 코드를 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,14 @@
 package lotto;
 
+
+import lotto.Lotto.Lotto;
+import lotto.Lotto.WinningLotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -20,6 +24,51 @@ class LottoTest {
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 범위에서 벗어나면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfBoundNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("정상적인 로또 번호가 들어오면 테스트는 통과한다.")
+    @Test
+    void createLotto() {
+        List<Integer> validLottoNumbers = List.of(1, 12, 13, 31, 32, 45);
+        Lotto lotto = new Lotto(validLottoNumbers);
+    }
+
+    @DisplayName("당첨 로또는 기존 로또에서 보너스 번호를 추가로 가진다.")
+    @Test
+    void createWinningLotto() {
+        List<Integer> validLottoNumbers = List.of(	8,9,20,25,29,33);
+        Lotto lotto = new WinningLotto(validLottoNumbers,7);
+    }
+
+    @DisplayName("당첨 로또는 보너스 번호가 중첩되도 예외가 발생한다.")
+    @Test
+    void createDuplicationWinningLotto() {
+        List<Integer> validLottoNumbers = List.of(9,18,20,22,38,44);
+        Integer duplicatedSpecialNumber = 20;
+        assertThatThrownBy(()-> new WinningLotto(validLottoNumbers,duplicatedSpecialNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 갖는 값의 범위가 넘어도 예외가 발생한다.")
+    @Test
+    void createOutOfBoundWinningLotto() {
+        List<Integer> validLottoNumbers = List.of(10,14,16,18,29,35);
+        Integer outOfBoundSpecialNumber1 = 46;
+        Integer outOfBoundSpecialNumber2 = 0;
+
+        assertThatThrownBy(()-> new WinningLotto(validLottoNumbers,outOfBoundSpecialNumber1))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()-> new WinningLotto(validLottoNumbers,outOfBoundSpecialNumber2))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
