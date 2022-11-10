d50c3038453814bf4e7d42ee14dfe9f038ad92c5
이경범 <tkdwk567@naver.com>
2022-11-10 20:11:36 +0900

    refactor : 입력기능 유효성 검사 refactor

--- src/main/java/lotto/MemberWriteService.java
+++ src/main/java/lotto/MemberWriteService.java
@@ -30,15 +30,13 @@ public class MemberWriteService {
                 .collect(Collectors.toList());
 
         validWinningNumbers(winningNumbers);
-
-        int bonusNumber = 0;
-        bonusNumber = getValidBonusNumber(writeBonusNumber, bonusNumber);
+        int bonusNumber = getValidBonusNumber(writeBonusNumber);
 
         return WinningLottoNumber.createWinningLottoNumber(winningNumbers, bonusNumber);
     }
 
     private void validWinningNumbers(List<Integer> winningNumbers) {
-        if (!(isNumberCount(winningNumbers) && isNumberRange(winningNumbers))) {
+        if (!isNumberCount(winningNumbers) && !isNumberRange(winningNumbers)) {
             throw new IllegalArgumentException("This Lotto Number is Exception !!");
         }
     }
@@ -51,7 +49,8 @@ public class MemberWriteService {
         return winningNumbers.stream().allMatch(number -> number >= 1 && number <= 45);
     }
 
-    private int getValidBonusNumber(String writeBonusNumber, int bonusNumber) {
+    private int getValidBonusNumber(String writeBonusNumber) {
+        int bonusNumber;
         try {
             bonusNumber = Integer.parseInt(writeBonusNumber);
             rangeValidBonusNumber(bonusNumber);
@@ -65,7 +64,7 @@ public class MemberWriteService {
     }
 
     private void rangeValidBonusNumber(int bonusNumber) {
-        if (bonusNumber >=1 && bonusNumber <= 45) {
+        if (!(bonusNumber >=1 && bonusNumber <= 45)) {
             throw new IllegalArgumentException("This Lotto Number is Exception !!");
         }
     }
