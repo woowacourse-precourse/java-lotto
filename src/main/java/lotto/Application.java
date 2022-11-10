830216552c47d4d1c007b3f0892221fbf9f0cc17
KimSangHyeok <tkdgur0906@naver.com>
2022-11-11 00:00:41 +0900

    Fix(lotto): 구매한 로또 번호 목록 출력 기능 오류 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,15 @@
 package lotto;
 
+import lotto.domain.LottoGenerator;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         View view = new View();
         int price = view.askPrice();
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        view.buyLottoList(price, lottoGenerator.generateLottoBundle(price));
+
+        
     }
 }
--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -16,7 +16,7 @@ public class View {
         System.out.println(price / 1000 + "개를 구매했습니다.");
         for (Lotto lotto : lottoBundle) {
             lotto.getNumbers().sort(Comparator.naturalOrder());
-            System.out.println(lotto.toString());
+            System.out.println(lotto.getNumbers().toString());
         }
     }
 }
