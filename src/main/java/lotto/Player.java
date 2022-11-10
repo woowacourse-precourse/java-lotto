0a9303683c7912564801ad6baa25e39cc7427aba
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-10 10:53:44 +0900

    feat(payAmountOfPurchaseMoney): implement method
    Enter amount of purchase money form console and return value.

--- src/main/java/lotto/Player.java
+++ src/main/java/lotto/Player.java
@@ -1,9 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Player {
 
     public String payAmountOfPurchaseMoney() {
-        return "";
+        return Console.readLine();
     }
 
     public String selectWinningNumbers() {
