aec95bae01959c21a44375e863c43b5764a033e2
KimSangHyeok <tkdgur0906@naver.com>
2022-11-10 13:21:23 +0900

    Feat(lotto): 구입금액에 해당하는 만큼 로또 발행 기능 추가

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -2,8 +2,24 @@ package lotto.domain;
 
 import lotto.Lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class LottoGenerator {
-    public Lotto generateLotto(){
-        return null;
+    private static final int LOTTO_PRICE = 1000;
+    private static final int START_LOTTO_NUMBER = 1;
+    private static final int END_LOTTO_NUMBER = 45;
+    private static final int LOTTO_COUNT = 6;
+    public List<Lotto> generateLottoBundle(int money) {
+        List<Lotto> lottoBundle = new ArrayList<>();
+        int lottoCounts = money / LOTTO_PRICE;
+
+        for (int count = 0; count < lottoCounts; count++) {
+            Lotto newLotto = new Lotto(Randoms.pickUniqueNumbersInRange(START_LOTTO_NUMBER, END_LOTTO_NUMBER,LOTTO_COUNT));
+            lottoBundle.add(newLotto);
+        }
+        return lottoBundle;
     }
 }
