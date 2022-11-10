a532748bfad9bb536e64e10c3554bbae4124ede3
JJ <jeongjae1996@naver.com>
2022-11-10 18:01:28 +0900

    feat: 당첨 번호를 입력 받기 위한 안내 메시지의 유형을 Enum으로 구성

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -3,6 +3,17 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class WinningNumbers {
+    private enum GuideMessageType {
+        WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+        BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+
+        private final String message;
+
+        GuideMessageType(String message) {
+            this.message = message;
+        }
+    }
+    
     private String setNumber() {
         return Console.readLine();
     }
