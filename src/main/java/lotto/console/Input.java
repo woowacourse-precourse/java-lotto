24fe6eb1b94ee85874d7f9413348c4ba81579c3d
CMHyuk <alsgur9710@naver.com>
2022-11-10 23:57:12 +0900

    feat(Input): 로또 당첨 번호 입력

--- src/main/java/lotto/console/Input.java
+++ src/main/java/lotto/console/Input.java
@@ -2,9 +2,13 @@ package lotto.console;
 
 import camp.nextstep.edu.missionutils.Console;
 
+
 public class Input {
 
     public static int inputMoney() {
         return Integer.parseInt(Console.readLine());
+    }
+    public static String[] inputLottoNumbers() {
+        return Console.readLine().split(",");
     }
 }
