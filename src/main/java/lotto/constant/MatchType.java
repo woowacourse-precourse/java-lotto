659eafcee19b7fa406e979fe85ea3bf10da03517
minju412 <ln8847@ajou.ac.kr>
2022-11-11 02:39:22 +0900

    feat: define system messages as constants

--- src/main/java/lotto/constant/MatchType.java
+++ src/main/java/lotto/constant/MatchType.java
@@ -1,10 +1,10 @@
 package lotto.constant;
 
 public enum MatchType {
-    THREE_MATCHES("3개 일치 (5,000원) - %d개"),
-    FOUR_MATCHES("4개 일치 (50,000원) - %d개"),
-    FIVE_MATCHES_WITHOUT_BONUS("5개 일치 (1,500,000원) - %d개"),
-    FIVE_MATCHES_WITH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    THREE_MATCHES("3개 일치 (5,000원) - %d개\n"),
+    FOUR_MATCHES("4개 일치 (50,000원) - %d개\n"),
+    FIVE_MATCHES_WITHOUT_BONUS("5개 일치 (1,500,000원) - %d개\n"),
+    FIVE_MATCHES_WITH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
     SIX_MATCHES("6개 일치 (2,000,000,000원) - %d개");
 
     private final String message;
---
+++ src/main/java/lotto/constant/SystemMessage.java
@@ -0,0 +1,10 @@
+package lotto.constant;
+
+public final class SystemMessage {
+    public static final String ENTER_PURCHASE_COST = "구입금액을 입력해 주세요.";
+    public static final String PURCHASE_COUNT_NOTIFICATION = "\n%d개를 구매했습니다.\n";
+    public static final String ENTER_WINNING_NUMBERS = "\n당첨 번호를 입력해 주세요.";
+    public static final String ENTER_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+    public static final String WINNING_STATISTICS = "당첨 통계\n---";
+    public static final String EARNINGS_RATE_NOTIFICATION = "총 수익률은 %.1f%%입니다.";
+}
