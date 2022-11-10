64f8d7ec38bdff5e834a27da1ce1a776d8b75513
chosunghyun18 <krismon@naver.com>
2022-11-10 18:56:02 +0900

    test(lotto): add check the domain logic of generate random number

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +25,13 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호생성 도메인 로직 확인")
+    @Test
+    void createLottoByRandomNumber() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> targetNumber = lotto.generateLotto();
+        lotto.setNumbers(targetNumber);
+
+        assertThat(targetNumber).isEqualTo(lotto.getNumbers());
+    }
 }
