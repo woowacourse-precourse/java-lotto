d20f513d7773dbd3202300af39e2323f5e67bd3a
keon0711 <fnc6488@gmail.com>
2022-11-11 04:33:48 +0900

    refactor(lotto): change magic number to constant

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
+    private static final int LENGTH_OF_LOTTO_NUM = 6;
+    private static final int LOWER_LIMIT_OF_LOTTO_NUM = 1;
+    private static final int UPPER_LIMIT_OF_LOTTO_NUM = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -13,17 +16,17 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LENGTH_OF_LOTTO_NUM) {
             throw new IllegalArgumentException("[ERROR] 6개의 로또 번호가 필요합니다.");
         }
 
         Set<Integer> removedNumbers = new HashSet<>(numbers);
-        if (removedNumbers.size() != 6) {
+        if (removedNumbers.size() != LENGTH_OF_LOTTO_NUM) {
             throw new IllegalArgumentException("[ERROR] 중복된 숫자가 존재하면 안됩니다.");
         }
 
-        for (int i = 0; i < numbers.size(); i++) {
-            if (numbers.get(i) < 1 || numbers.get(i) > 45) {
+        for (int number : numbers) {
+            if (number < LOWER_LIMIT_OF_LOTTO_NUM || number > UPPER_LIMIT_OF_LOTTO_NUM) {
                 throw new IllegalArgumentException("[ERROR] 1에서 45 사이의 숫자를 입력해야 합니다.");
             }
         }
