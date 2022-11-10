9ab0c7bf9559a9bdde097f18e9f590d5766ad997
sugamypapa <sk000801@naver.com>
2022-11-10 22:26:25 +0900

    feat: 당첨 숫자(6개) 예외 처리 및 저장 기능 구현

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -2,23 +2,30 @@ package view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 public class Input {
     private int number;
 
-    private final String answer;
+    private List<Integer> answer;
+
+    private Valid valid;
 
     private final static int unitPrice = 1000;
 
     public Input() {
         number = 0;
-        answer = "";
+        answer = new ArrayList<>();
     }
 
     public int getNumber() {
         return this.number;
     }
 
-    public String getAnswer() {
+    public List<Integer> getAnswer() {
         return this.answer;
     }
 
@@ -28,17 +35,37 @@ public class Input {
         return Integer.parseInt(price);
     }
 
-    public boolean isNotMultiple(int price) {
-        return (price % unitPrice != 0);
-    }
-
     public void returnNumber(int price) {
-        if(!isNotMultiple(price)) {
+        if (!valid.isNotMultiple(price)) {
             number = price / unitPrice;
         }
-        if(isNotMultiple(price)) {
+        if (valid.isNotMultiple(price)) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액을 1000으로 나누어 떨어져야 합니다.");
         }
     }
 
+    public String[] enterAnswer() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine().split(",");
+    }
+
+    public void checkValid(String[] enterAnswer) {
+        if (valid.hasRepeatNumber(enterAnswer) || valid.enterNumberOverLength(enterAnswer)
+            || valid.isOverRange(enterAnswer)) {
+            throw new IllegalArgumentException("1-45까지의 수 중 6개의 수를 골라 입력하세요.");
+        }
+    }
+
+    public void returnAnswer(String[] enterAnswer) {
+        if (!valid.hasRepeatNumber(enterAnswer) && !valid.enterNumberOverLength(enterAnswer)
+                && !valid.isOverRange(enterAnswer)) {
+            int[] answerNumber= Arrays.stream(enterAnswer).mapToInt(Integer::parseInt).toArray();
+            for (int i : answerNumber) {
+                answer.add(i);
+            }
+        }
+    }
+
+
 }
+
