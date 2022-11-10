59bdac62c2ee83f25f27acb54d2b022d03421d52
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-10 15:03:11 +0900

    feat : 로또 번호를 생성하고 중복을 제거하는 메서드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,18 @@
-package lotto;
+package lotto.domain;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.view.ErrorMessage;
 
 public class Lotto {
+    private static final int START_NUM = 1;
+    private static final int END_NUM = 45;
+    private static final int LOTTO_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -10,11 +20,27 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.ERROR_LOTTO_SIZE.getErrorMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    List<Integer> createRandomNumbers() {
+        return pickUniqueNumbersInRange(START_NUM, END_NUM, LOTTO_SIZE);
+    }
+
+    public void createLottoNumber() {
+        Set<Integer> duplicate = new HashSet<>();
+        while (duplicate.size() != LOTTO_SIZE) {
+            duplicate.addAll(createRandomNumbers());
+        }
+        numbers.addAll(duplicate);
+        Collections.sort(numbers);
+        System.out.println(numbers);
+    }
 }
