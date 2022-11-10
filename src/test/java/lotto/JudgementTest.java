da640b12d203068a81ac788acd3b41685af03989
ostar11 <cjw5877@gmail.com>
2022-11-10 16:57:48 +0900

    test(JudgementTest): 같은_숫자_세기_테스트 구현

--- src/test/java/lotto/JudgementTest.java
+++ src/test/java/lotto/JudgementTest.java
@@ -1,10 +1,17 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class JudgementTest {
+    @Test
+    void 같은_숫자_세기_테스트() {
+        Judgement judgement = new Judgement();
+        int input = judgement.correctCount(Arrays.asList(1, 2, 3, 4, 5, 6),Arrays.asList(7, 2, 3, 4, 5, 6));
+
+        assertThat(input).isEqualTo(5);
+    }
 }
