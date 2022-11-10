13d96145205b16bd4a99cfad090d65b3f3999736
seokjin8678 <rnrn8678@naver.com>
2022-11-10 23:15:44 +0900

    test: WinningLotto 관련 테스트 코드 추가

--- src/test/java/lotto/game/LottoServiceTest.java
+++ src/test/java/lotto/game/LottoServiceTest.java
@@ -2,6 +2,9 @@ package lotto.game;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+import lotto.Lotto;
+import lotto.WinningLotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -46,5 +49,79 @@ class LottoServiceTest {
         // expect
         assertThat(lottoService.convertInputToMoney(input))
                 .isEqualTo(money);
+    }
+
+    @Test
+    @DisplayName("올바른 번호를 입력하면 당첨 번호가 정상적으로 생성된다.")
+    void createWinningLotto() {
+        // given
+        String numbers = "1,2,3,4,5,6";
+        String bonusNumber = "7";
+        WinningLotto lotto = new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)), 7);
+
+        // expect
+        assertThat(lottoService.convertInputToWinningLotto(numbers, bonusNumber))
+                .isEqualTo(lotto);
+    }
+
+    @Test
+    @DisplayName("당첨 번호를 만들때 잘못된 숫자를 입력하면 예외가 발생한다.")
+    void createWinningLottoByWrongNumber1() {
+        // given
+        String numbers = "1,2,3,4,5,46";
+        String bonusNumber = "7";
+        // expect
+        assertThatThrownBy(() -> lottoService.convertInputToWinningLotto(numbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 올바른 로또 번호가 아닙니다.");
+    }
+
+    @Test
+    @DisplayName("당첨 번호를 만들때 잘못된 숫자를 입력하면 예외가 발생한다.")
+    void createWinningLottoByWrongNumber2() {
+        // given
+        String numbers = "0,1,2,3,4,5";
+        String bonusNumber = "6";
+        // expect
+        assertThatThrownBy(() -> lottoService.convertInputToWinningLotto(numbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 올바른 로또 번호가 아닙니다.");
+    }
+
+    @Test
+    @DisplayName("당첨 번호를 만들때 잘못된 숫자를 입력하면 예외가 발생한다.")
+    void createWinningLottoByWrongNumber3() {
+        // given
+        String numbers = "0,1,2,3,4,5,";
+        String bonusNumber = "6";
+        // expect
+        assertThatThrownBy(() -> lottoService.convertInputToWinningLotto(numbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 올바른 로또 번호가 아닙니다.");
+    }
+
+    @Test
+    @DisplayName("당첨 번호를 만들때 중복된 숫자가 있으면 예외가 발생한다.")
+    void createWinningLottoByDuplicate() {
+        // given
+        String input = "1,2,3,4,5,5";
+        String bonusNumber = "6";
+        // expect
+        assertThatThrownBy(() -> lottoService.convertInputToWinningLotto(input, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 중복된 숫자가 있습니다.");
+    }
+
+    @Test
+    @DisplayName("당첨 번호를 만들때 보너스 번호와 중복되면 예외가 발생한다.")
+    void createWinningLottoByDuplicateBonusNumber() {
+        // given
+        String input = "1,2,3,4,5,6";
+        String bonusNumber = "6";
+
+        // expect
+        assertThatThrownBy(() -> lottoService.convertInputToWinningLotto(input, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호와 중복이 있습니다.");
     }
 }
