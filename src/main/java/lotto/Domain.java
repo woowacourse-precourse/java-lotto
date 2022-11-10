d6c8d53560aec32450d35abe3aef0290e97860ef
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-10 19:15:01 +0900

    <feat>(Domain.java): isValidPriceInput 메서드 작성
     - 로또 구입 금액 input 입력 받아 유효성 검증하는 메서드 생성
     - int로만 이루어졌는지, 1000단위 입력인지 확인

--- src/main/java/lotto/Domain.java
+++ src/main/java/lotto/Domain.java
@@ -1,2 +1,17 @@
-package lotto;public class Domain {
+package lotto;
+
+public class Domain {
+    public boolean isValidPriceInput(String input) {
+        if (!input.matches("^[0-9]+$")) {
+            return false;
+        }
+        int inputPrice = Integer.parseInt(input);
+        if (inputPrice % 1000 != 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    
 }
