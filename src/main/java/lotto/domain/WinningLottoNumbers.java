1c95a5f9924baf21e7ac7e297b75311b119a9f36
ywj9811 <ywj9811@naver.com>
2022-11-10 23:22:32 +0900

    fix: WinningLottoNumbers 클래스
    로또 번호의 경우 수동으로 입력하는 방식으로 수정
    그리고 보너스 번호 또한 getter생성

--- src/main/java/lotto/domain/WinningLottoNumbers.java
+++ src/main/java/lotto/domain/WinningLottoNumbers.java
@@ -2,26 +2,37 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Lotto;
+import lotto.exception.LottoNumberException;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.*;
+
 public class WinningLottoNumbers {
-    private static final int MIN_NUM = 1;
-    private static final int MAX_NUM = 45;
-    private static final int SIZE = 6;
+    LottoNumberException lottoNumberException = new LottoNumberException();
 
-    private List<Integer> winningNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUM, MAX_NUM, SIZE);
+    /**
+     * 당첨 번호를 입력 받으며 쉼표(,)를 기준으로 구분
+     */
+    public List<Integer> getWinningLottoNumbers() {
+        String inputNumbers = readLine();
+        String[] splitInputNumbers = (inputNumbers.split(","));
+        List<Integer> winningNumbers = new ArrayList<>();
 
+        for (int idx = 0; idx < splitInputNumbers.length; idx++) {
+            winningNumbers.add
+                    (Integer.parseInt(splitInputNumbers[idx]));
+        }
 
-    Lotto lotto = new Lotto(winningNumbers);
+        Lotto lotto = new Lotto(winningNumbers);
 
-    public List<Integer> getWinningNumbers() {
-        Collections.sort(winningNumbers);
         return winningNumbers;
     }
 
-    public int getBonusNumber() {
-        return lotto.getBonusNumber();
+    public int getBonusNumber(List<Integer> winningNumbers) {
+        int bonusNumber = Integer.parseInt(readLine());
+        lottoNumberException.bonusNumber(winningNumbers, bonusNumber);
+        return bonusNumber;
     }
 }
