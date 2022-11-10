4c8615ade014be4fd3f614d3edc8d6db36703141
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-10 16:16:59 +0900

    feat: 입력 받은 돈을 가격으로 나누는 기능 생성

---
+++ src/main/java/lotto/domain/TicketMachine.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+public class TicketMachine {
+    private static final String ERROR_MESSAGE = "[ERROR] 유효하지 않은 금액입니다. ";
+    private static final int TICKET_PRICE = 1000;
+
+    public Integer getTicket(int payment) {
+        validatePayment(payment);
+        return payment / TICKET_PRICE;
+    }
+
+    private void validatePayment(int payment) {
+        if (payment % TICKET_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] : ");
+        }
+    }
+}
