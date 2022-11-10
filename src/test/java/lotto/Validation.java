4ce0e06db41e5d3bac850fdb2322676a1fb0a67b
HubCreator <chocomilk8604@gmail.com>
2022-11-10 10:04:01 +0900

    docs : 기능 목록 수정

---
+++ src/test/java/lotto/Validation.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class Validation {
+
+    @Nested
+    class ValidateOneDigit {
+        @Test
+        void isInRange() {
+        }
+    }
+}
