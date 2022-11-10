5a46016d0dd1b067d741a30f5a81b07ba87b3b0a
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-10 15:08:15 +0900

    refactor : 번호 구현 방식 변경
    직접 구현 -> 우테코에서 제공해주는 함수로 변경

--- src/main/java/lotto/repository/LottoRepository.java
+++ src/main/java/lotto/repository/LottoRepository.java
@@ -1,22 +1,12 @@
 package lotto.repository;
 
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.TreeSet;
-
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
 
 public class LottoRepository {
 
-	public static final int QUANTITY_OF_NUMBERS = 6;
-	public static final int NUMBER_MAX_RANGE = 46;
-
 	public static Lotto makeAutoLotto() {
-		Set<Integer> numbers = new TreeSet<>();
-		while (numbers.size() < QUANTITY_OF_NUMBERS) {
-			numbers.add((int)(Math.random() * NUMBER_MAX_RANGE));
-		}
-		return new Lotto(new ArrayList<>(numbers));
+		return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
 	}
 
 }
