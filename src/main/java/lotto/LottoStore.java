3886a3210a598fd37e021819ddcb9692b3b3ce6e
c4fiber <qudcjf153@gmail.com>
2022-11-11 00:48:49 +0900

    feat(LottoStore): 로또를 생성하여 반환하는 기능
    1~45 범위에서 고유한 번호 6개를 뽑아 구성된 Lotto 객체를 반환한다

---
+++ src/main/java/lotto/LottoStore.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LottoStore {
+
+    private Lotto getNewLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+
+        return new Lotto(numbers);
+    }
+}
