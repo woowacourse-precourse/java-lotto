4394c5486749a1e84d295d0503eff8bc8933acc9
jihoon-gh <jihoon806@gmail.com>
2022-11-10 23:59:59 +0900

    DOCS - README.md 필요 기능 목록 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        System.out.println("구매금액을 입력해 주세요");
+        Integer inputMoney = Integer.parseInt(sc.nextLine());
+        validateMoney(inputMoney);
+        Integer nums = inputMoney/1000;
+
+
+    }
+
+    static void validateMoney(Integer money){
+        if(money%1000!=0){
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위여야 합니다.");
+        }
     }
 }
