b8f305ddf5b2b2eb8920ce4fbf68d1aa32c7d9cb
yangjiyoung <0112jyoung@naver.com>
2022-11-10 16:02:57 +0900

    feat: add input message view

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+public class InputView {
+    public static void requestPurchaseMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+    public static void requestLottoNumbers(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+    public static void requestBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}
