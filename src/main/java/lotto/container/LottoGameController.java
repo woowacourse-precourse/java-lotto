2238807a812366d1c85ef22fd53d1b3082bdd908
ywj9811 <ywj9811@naver.com>
2022-11-11 05:53:53 +0900

    feat: LottoGameController 클래스
    당첨 통계 출력문 추가

--- src/main/java/lotto/container/LottoGameController.java
+++ src/main/java/lotto/container/LottoGameController.java
@@ -3,7 +3,6 @@ package lotto.container;
 import lotto.service.*;
 import lotto.view.InputOutputView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGameController {
@@ -38,27 +37,39 @@ public class LottoGameController {
      */
     private InputOutputView inputOutputView = new InputOutputView();
     private MyTotalMoney myTotalMoney = new MyTotalMoney();
+    private BuyLotto buyLotto = new BuyLotto();
     private ProduceWinningNumbers produceWinningNumbers = new ProduceWinningNumbers();
+    private WinningMoney winningMoney;
     private TotalYield totalYield;
 
     private int money;
     private int gain;
     private double myYield;
-    private List<Integer> winningNumbers;
     private int bonusNumber;
+    private int quantity;
+    private List<List<Integer>> myLottoNumbers;
+    private List<Integer> winningNumbers;
+    private List<Integer> grades;
 
     public void gameStart() {
         money = inputOutputView.buyMoney();
+        quantity = buyLotto.buyLotto(money);
+        myLottoNumbers = buyLotto.getMyLottoNumbers(quantity);
         winningNumbers = produceWinningNumbers.getWinningNumbers();
         bonusNumber = produceWinningNumbers.getBonusNumber(winningNumbers);
-        gain = myTotalMoney.totalMoney(money, winningNumbers, bonusNumber);
+        gain = myTotalMoney.totalMoney(myLottoNumbers, winningNumbers, bonusNumber);
 
         totalYield = new TotalYield(money, gain);
 
         myYield = totalYield.getYield();
 
+        winningMoney = new WinningMoney();
+
         System.out.println("당첨 통계");
-        System.out.println("총 수익률은 " + myYield);
+        System.out.println("--");
+        grades = winningMoney.getGrade(winningNumbers, myLottoNumbers, bonusNumber);
+        inputOutputView.saveResult(grades);
+        inputOutputView.totalYield(myYield);
     }
 
 }
