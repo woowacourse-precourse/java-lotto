e0ce60228f67f59146a922db6627ad4749b9036e
KimDongho <hkim4410@naver.com>
2022-11-10 22:05:45 +0900

    feat: 유저가 발행한 로또 번호를 모두 출력하는 기능 추가

--- src/main/java/lotto/util/OutputUtil.java
+++ src/main/java/lotto/util/OutputUtil.java
@@ -1,8 +1,18 @@
 package lotto.util;
 
+import lotto.domain.Lotto;
+
+import java.util.List;
+
 public class OutputUtil {
 
     public void printUserLottoCount(int lottoCount) {
         System.out.println("" + lottoCount + "개를 구매했습니다.");
+    }
+
+    public void printUserLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
     }
 }
