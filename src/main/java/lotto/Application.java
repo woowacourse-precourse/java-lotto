6fb6263bbf42426b57408dbdf2d961ec48b69a0a
49ehyeon42 <49ehyeon42@gmail.com>
2022-11-10 21:35:37 +0900

    feat: 구입 금액 문자열을 검증 후 로또 수량을 반환하는 메서드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+
+        int lottoCount = getLottoCount(Console.readLine());
+    }
+
+    public static int getLottoCount(String string) {
+        if (!isNumber(string)) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액이 숫자 형식이 아닙니다.");
+        }
+
+        int number = Integer.parseInt(string);
+
+        if (!isPositiveNumber(number)) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액이 양수가 아닙니다.");
+        }
+
+        if (!isDividedBy1000(number)) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액이 1,000원으로 나누어떨어지지 않습니다.");
+        }
+
+        return number / 1000;
     }
 
     private static boolean isNumber(String string) {
