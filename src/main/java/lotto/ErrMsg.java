17f0e8910fdb1c2eda900f5459ff5f0b390f2431
HyebinShin <hyeviin.dev@gmail.com>
2022-11-10 19:38:45 +0900

    Change: 에러 메시지 출력문 추가
    발행될 로또 번호 관련 에러 메시지 출력문 추가

--- src/main/java/lotto/ErrMsg.java
+++ src/main/java/lotto/ErrMsg.java
@@ -8,4 +8,6 @@ public class ErrMsg {
     public static final String VALIDATE_PRIZE_NUM_UNIQUE = "[ERROR] 로또 당첨 번호는 서로 달라야 합니다.";
     public static final String VALIDATE_BONUS_NUM_ONE = "[ERROR] 1부터 45까지의 숫자를 1개만 적어주세요.";
     public static final String VALIDATE_BONUS_NUM_UNIQUE = "[ERROR] 보너스 번호와 당첨 번호는 서로 달라야 합니다.";
+
+    public static final String VALIDATE_LOTTO_NUM = "[ERROR] 발행될 로또 번호는 서로 다른 6자리의 숫자여야 합니다.";
 }
