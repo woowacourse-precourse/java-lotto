d57eef2913aa6bcee23cb1b5da77da047b5aea7c
Reznov <reznov03048@gmail.com>
2022-11-10 18:53:23 +0900

    feat: 중복된 값이 있는지 확인하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 로또 번호의 유효성을 확인하는 클래스
@@ -19,5 +21,15 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void isValidateDuplicated(List<Integer> numbers) {
+        Set<Integer> checkDuplicated = new HashSet<>();
+
+        for(int number : numbers) {
+            checkDuplicated.add(number);
+        }
+
+        if(checkDuplicated.size() != Number.LOTTO_NUMBER_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
