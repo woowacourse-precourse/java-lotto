7372943cbdbb93719a3dc37244bbb467e0a6d343
siwookim97 <tldn12gh@gmail.com>
2022-11-11 03:25:28 +0900

    refactor: 상수들의 위치 변경
    상수들의 위치 변경
     - InputView 클래스 내부의 상수를 Constants 클래스로 이동
     - Constants 클래스에서 앞으로 상수를 다룰 예정

---
+++ src/main/java/lotto/util/Constants.java
@@ -0,0 +1,9 @@
+package lotto.util;
+
+public class Constants {
+    // 입력요청 메시지 관련 상수
+    public static final String PRINT_INPUT_MONEY = "구입금액을 입력해 주세요.";
+
+    // 에러 메시지 관련 상수
+    public static final String ERROR_INPUT_MONEY = "[ERROR] 구입금액은 1000단위 양수여야 합니다.";
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,12 +1,11 @@
 package lotto.view;
 
+import lotto.util.Constants;
 import lotto.util.Utils;
 
 public class InputView {
-    private static final String PRINT_INPUT_MONEY = "구입금액을 입력해 주세요.";
-
     public static String inputMoney() {
-        System.out.println(PRINT_INPUT_MONEY);
+        System.out.println(Constants.PRINT_INPUT_MONEY);
         return Utils.readConsole();
     }
 }
