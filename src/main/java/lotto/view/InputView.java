38fed563ebc747730993c72e70c44bcb85583346
woosung1223 <dntjd991223@naver.com>
2022-11-10 22:47:49 +0900

    feat(InputView): readWinningNumber 메소드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,12 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public final class InputView {
     private InputView() {}
 
@@ -9,5 +15,12 @@ public final class InputView {
         String line = Console.readLine();
         // verify(line);
         return Integer.parseInt(line);
+    }
+
+    public static List<Integer> readWinningNumber() {
+        String line = Console.readLine();
+        return Arrays.stream(line.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
