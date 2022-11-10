84714e1edb1a3074deea8e530cb5cdf842a9dfdb
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-10 18:31:26 +0900

    style : 예외처리 메서드 Private로 변경

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -3,14 +3,18 @@ package lotto.domain;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import java.util.stream.Stream;
+
 import lotto.view.ErrorMessage;
 import lotto.view.Message;
 
 public class WinningLotto {
 
+    private String winningNumber;
+    private String bonusNumber;
+
     public void winningNumberInput() {
         System.out.println(Message.WINNING_NUMBER_INPUT.getMessage());
-        String winningNumber = readLine();
+        winningNumber = readLine();
         validateWinningNumber(winningNumber);
         validateWinningNumberSize(winningNumber);
         validateInputBlank(winningNumber);
@@ -19,11 +23,11 @@ public class WinningLotto {
 
     public void bonusNumberInput() {
         System.out.println(Message.BONUS_NUMBER_INPUT.getMessage());
-        String bonusNumber = readLine();
+        bonusNumber = readLine();
         validateInputBonusNumber(bonusNumber);
     }
 
-    void validateWinningNumber(String winningNumber) {
+    private static void validateWinningNumber(String winningNumber) {
         Integer[] winningNumberArr = Stream.of(winningNumber.split(","))
                 .mapToInt(Integer::parseInt)
                 .boxed()
@@ -35,14 +39,14 @@ public class WinningLotto {
         }
     }
 
-    void validateWinningNumberSize(String winningNumber) {
+    private static void validateWinningNumberSize(String winningNumber) {
         String[] numberSizeCheck = winningNumber.split(",");
         if (numberSizeCheck.length != 6) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_LOTTO_LENGTH.getErrorMessage());
         }
     }
 
-    void validateInputBlank(String winningNumber) {
+    private static void validateInputBlank(String winningNumber) {
         for (int i = 0; i < winningNumber.length(); i++) {
             if (winningNumber.charAt(i) == ' ') {
                 throw new IllegalArgumentException(ErrorMessage.ERROR_BLANK.getErrorMessage());
@@ -50,14 +54,14 @@ public class WinningLotto {
         }
     }
 
-    void validateInputSeparator(String winningNumber) {
+    private static void validateInputSeparator(String winningNumber) {
         String[] separatorCheck = winningNumber.split(",");
         if (separatorCheck.length != 6) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_SEPARATOR.getErrorMessage());
         }
     }
 
-    void validateInputBonusNumber(String bonusNumber) {
+    private static void validateInputBonusNumber(String bonusNumber) {
         Integer[] bonusNumberArr = Stream.of(bonusNumber)
                 .mapToInt(Integer::parseInt)
                 .boxed()
