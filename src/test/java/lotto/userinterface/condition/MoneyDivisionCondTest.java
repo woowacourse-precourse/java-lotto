65fe8edd9f0beacf0c7dd3f0319b03812d779079
wonju-dev <wonjulee.dev@gmail.com>
2022-11-10 21:40:45 +0900

    refactor(MoneyDivisionCondTest): 중복 사용된 변수 분리

--- src/test/java/lotto/userinterface/condition/MoneyDivisionCondTest.java
+++ src/test/java/lotto/userinterface/condition/MoneyDivisionCondTest.java
@@ -2,17 +2,16 @@ package lotto.userinterface.condition;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.*;
 
 class MoneyDivisionCondTest {
+
+    private static final Condition condition = MoneyDivisionCond.getInstance();
 
     @Test
     void 천원으로_나누어_떨어지지_않는_금액() {
         // given
         String money = "1001";
-        Condition condition = MoneyDivisionCond.getInstance();
 
         // when
         Boolean result = condition.isSatisfied(money);
@@ -25,7 +24,6 @@ class MoneyDivisionCondTest {
     void 천원으로_나누어_떨어지는_금액() {
         // given
         String money = "3000";
-        Condition condition = MoneyDivisionCond.getInstance();
 
         // when
         Boolean result = condition.isSatisfied(money);
