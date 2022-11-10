0a95eb1174f032924da46fffd3663d54024c5f9b
jeomxon <jeomxon@gmail.com>
2022-11-11 02:01:47 +0900

    feat: 구입금액에 따른 로또의 개수를 구하는 기능구현

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -9,6 +9,7 @@ public class Purchase {
     private int money = 0;
 //    private List<List<Integer>> totalLotto = new ArrayList<>();
     private static final String PURCHASE_AMOUNT_ERROR = "[ERROR] 구입 금액은 1,000원 단위여야 합니다.";
+    private static final String INPUT_PURCHASE_AMOUNT = "구매금액을 입력해 주세요.";
 
     public Purchase(int money) {
         validate(money);
@@ -19,5 +20,17 @@ public class Purchase {
         if (money % 1000 != 0) {
             throw new IllegalArgumentException(PURCHASE_AMOUNT_ERROR);
         }
+    }
+
+    public void inputMoney() {
+        System.out.println(INPUT_PURCHASE_AMOUNT);
+        int input = Integer.parseInt(Console.readLine());
+
+        numOfLotto(input);
+    }
+
+    private int numOfLotto(int money) {
+        int num = money / 1000;
+        return num;
     }
 }
