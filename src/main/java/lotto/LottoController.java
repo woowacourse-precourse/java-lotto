81f70f96ce6106240d0e0426860e29abff728b3b
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-10 16:37:18 +0900

    feat: LottoController 생성
    로직을 실행하는 클래스

---
+++ src/main/java/lotto/LottoController.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoController {
+
+    private LottoService lottoService = new LottoService();
+    private LottoView lottoView = new LottoView();
+    private InputDevice inputDevice = new InputDevice();
+
+    public void run() {
+        List<Lotto> lottoTickets =  lottoService.buy(inputDevice.sendMoneyToBuyLotto());
+        lottoView.printLottoInfo(lottoTickets);
+        List<Integer> winningNumbers = inputDevice.sendWinningNumbers();
+        int bonusNumber = inputDevice.sendBonusNumber(winningNumbers);
+        List<LottoResult> lottoResults = lottoService.compareLottoNumbers(lottoTickets, winningNumbers, bonusNumber);
+        lottoView.printLottoResult(lottoResults);
+        double yield = lottoService.calculateYield(lottoResults);
+        lottoView.printYield(yield);
+    }
+}
