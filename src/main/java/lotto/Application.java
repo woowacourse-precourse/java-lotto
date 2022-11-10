979ec90e8b423e04b07f3c6faa9082202f569148
alstjr706 <alstjr706@naver.com>
2022-11-11 01:54:37 +0900

    feat : Application 클래스 코드 비워두고, LottoGame 클래스로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,8 @@
 package lotto;
 
-import lotto.domain.RandomLotto;
-import lotto.input.LottoTickets;
-
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
-
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.run();
     }
 }
