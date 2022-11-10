02bef0278f1d7f4ac239a89182206512a1889ed0
CMHyuk <alsgur9710@naver.com>
2022-11-10 22:38:16 +0900

    feat(Input): 로또 구입 금액 입력

---
+++ src/main/java/lotto/console/Input.java
@@ -0,0 +1,10 @@
+package lotto.console;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public static int inputMoney() {
+        return Integer.parseInt(Console.readLine());
+    }
+}
