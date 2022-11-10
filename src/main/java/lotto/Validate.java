ee17b21b27981512f52eeb51ba83c8ac4e976947
eora21 <eora21@naver.com>
2022-11-10 20:47:53 +0900

    refactor: 💡 변경 1000으로 나누어떨어지지 않을 시의 출력문구 자연스럽게 수정

--- src/main/java/lotto/Validate.java
+++ src/main/java/lotto/Validate.java
@@ -4,15 +4,14 @@ import constant.Amount;
 import constant.LottoText;
 
 public class Validate {
-//    NULL("입력값은 null일 수 없습니다."),
-//    NOT_INT("입력값이 정수가 아닙니다."),
-//    NOT_DIVISIBLE(),
+
 //    NOT_LOTTO_FORM("입력값을 로또 형태로 전환할 수 없습니다."),
 //    DUPLICATE_LOTTO("중복된 값이 있습니다."),
 //    DUPLICATE_BONUS("보너스 번호가 당첨 번호와 중복되었습니다."),
     public void validDivisible(int purchasePrice) {
         if (purchasePrice % Amount.UNIT.getPrice() != 0) {
-            throw new IllegalArgumentException(LottoText.ERROR + "입력값을" + Amount.UNIT + "로 나눌 수 없습니다.");
+            throw new IllegalArgumentException(
+                    LottoText.ERROR + "입력값이 " + Amount.UNIT + "으로 나누어떨어지지 않습니다.");
         }
     }
 }
