9bc78f7c81881cfc9b5cb39535afbf0f5e5bac34
소대훈 <theogns0681@naver.com>
2022-11-10 10:39:19 +0900

    [refactor] (Money) 숫자도 저장하게 수정

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -1,19 +1,21 @@
 package lotto;
 
 public enum Money {
-    first("6개 일치 (2,000,000,000원) - "),
-    second("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    third("5개 일치 (1,500,000원) - "),
-    fourth("4개 일치 (50,000원) - "),
-    fifth("3개 일치 (5,000원) - ");
+    first("6개 일치 (2,000,000,000원) - ",2000000000),
+    second("5개 일치, 보너스 볼 일치 (30,000,000원) - ",30000000),
+    third("5개 일치 (1,500,000원) - ",1500000),
+    fourth("4개 일치 (50,000원) - ",50000),
+    fifth("3개 일치 (5,000원) - ",5000);
 
-    private final String money;
-
-    Money(String money) {
+    private final String moneyString;
+    private final int money;
+    Money(String moneyString, int money) {
+        this.moneyString = moneyString;
         this.money = money;
     }
 
-    public String getMoney() {
-        return money;
+    public String getMoneyString() {
+        return moneyString;
     }
+    public int getMoney() { return money;}
 }
