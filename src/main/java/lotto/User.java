97fddbbdadcf09fd621af702b15c3b594c725b6f
whipbaek <jibaek0513@gmail.com>
2022-11-10 19:10:36 +0900

    feat(User): 사용자에게 로또 번호를 입력받고 추가하는 기능 추가 - public void readLotto() 추가 - 입력받고, 문제 없으면 로또목록에 추가함.

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,10 +1,15 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class User {
     private int userMoney;
     private int resultMoney;
+    private int numOfLotto;
+    private List<Lotto> lottos = new ArrayList<>();
 
     public void readMoney(){
         int inputMoney = Integer.parseInt(readLine());
@@ -17,5 +22,11 @@ public class User {
             System.out.println("[ERROR] 금액은 1000원 단위로 나뉘어 떨어져야 합니다.");
             throw new IllegalArgumentException();
         }
+    }
+
+    public void readLotto(){
+        String inputLottoNumber = readLine();
+        validateInputLottoNumber(inputLottoNumber);
+        lottos.add(new Lotto(convertLottoNumber(inputLottoNumber)));
     }
 }
