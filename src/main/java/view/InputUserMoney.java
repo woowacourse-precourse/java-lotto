c1ebf43e4914a5b33ad4c1c707489db6b6dea86f
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-10 15:10:08 +0900

    refactor(lotto): 패키지 삭제에 따른 리팩토링

--- src/main/java/view/InputUserMoney.java
+++ src/main/java/view/InputUserMoney.java
@@ -1,9 +1,10 @@
 package view;
 
-import static util.ValidateUserMoney.validateMoney;
 
 import camp.nextstep.edu.missionutils.Console;
+import constant.ErrorMessage;
 import constant.ExceptionNumber;
+import java.util.stream.IntStream;
 
 public class InputUserMoney {
 
@@ -14,6 +15,50 @@ public class InputUserMoney {
             return ExceptionNumber.EXCEPTION_CODE.getCode();
         }
         return Integer.parseInt(input_money);
+    }
+
+    public static boolean validateMoney(String input_money){
+        if (checkProcessIsNumber(input_money)) {
+            return false;
+        }
+        if (checkProcessIsCorrectMoney(input_money)) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean checkProcessIsCorrectMoney(String input_money){
+        try{
+            isCorrectMoney(Integer.parseInt(input_money));
+        }
+        catch (IllegalArgumentException e){
+            ExceptionInfo.printException(e);
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean checkProcessIsNumber(String input_money){
+        try {
+            isNumber(input_money);
+        }
+        catch(IllegalArgumentException e){
+            ExceptionInfo.printException(e);
+            return true;
+        }
+        return false;
+    }
+    private static void isCorrectMoney(int money){
+        if(money %1000>0){
+            throw new IllegalArgumentException(ErrorMessage.ERROR_INCORRECT_MONEY.getMessage());
+        }
+    }
+
+    private static void isNumber(String input_money){
+        IntStream chars = input_money.chars();
+        if(chars.anyMatch(c ->  (c<'0' || c>'9'))){
+            throw new IllegalArgumentException(ErrorMessage.ERROR_NOT_NUMBER.getMessage());
+        }
     }
 
 
