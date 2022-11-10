5ad371fbc29e9d002ab08ecd6de34fe91e4912e3
YunByungil <dbsquddlfz@naver.com>
2022-11-10 16:46:48 +0900

    feat(lotto): 로또 숫자에 중복 숫자가 있는지 알 수 있는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,8 @@ package lotto;
 
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Lotto {
     private final List<Integer> numbers;
 
@@ -14,7 +16,23 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if (hasNumber(i, numbers.get(i), numbers)) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
+
+    private boolean hasNumber(int indexNumber, int number, List<Integer> numbers) {
+        for (int j = indexNumber + 1; j < numbers.size(); j++) {
+            if (numbers.get(j) == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     // TODO: 추가 기능 구현
 }
