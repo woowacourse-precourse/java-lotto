305db988bfcf778a375e6d0ceb44aaaf5e537b6e
youhobin <ghdb132@naver.com>
2022-11-10 11:33:24 +0900

    feat(validate) : 범위 밖의 숫자 유효성 검사 기능 추가

--- src/main/java/lotto/utils/Validation.java
+++ src/main/java/lotto/utils/Validation.java
@@ -3,20 +3,31 @@ package lotto.utils;
 import java.util.List;
 
 public class Validation {
-
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+    private static final int LOTTO_FIRST_NUMBER = 1;
+    private static final int LOTTO_LAST_NUMBER = 45;
     public static void validate(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplication(numbers);
+        validateNumber(numbers);
+    }
+
+    private static void validateNumber(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < LOTTO_FIRST_NUMBER || number > LOTTO_LAST_NUMBER) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
     private static void validateDuplication(List<Integer> numbers) {
-        if (numbers.stream().distinct().count() != 6) {
+        if (numbers.stream().distinct().count() != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException();
         }
     }
 
     private static void validateSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException();
         }
     }
