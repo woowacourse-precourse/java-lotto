1ac35ef7a0b8a728c2fcf0a63f634ecf9f40b50b
lee2963 <sodel2963@naver.com>
2022-11-10 18:22:15 +0900

    Feat: 로또 번호 생성 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+
+    }
+
+    public static List<Integer> makeLottoNum() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }
