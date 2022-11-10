314079314ef057717b47cf8e139201018122ce7f
eunji53 <duqtj00@naver.com>
2022-11-10 14:08:07 +0900

    <feat>:1~45이외의 숫자 입력 시 오류 발생

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -8,15 +8,24 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LottoResult {
-    public List<Integer> inputLottoNumber(){
+    public List<Integer> inputLottoNumber() {
         System.out.println("당첨번호를 입력해 주세요.");
-        String lottoNumberInput=readLine();
+        String lottoNumberInput = readLine();
         List<String> numbers = Stream.of(lottoNumberInput.split(",")).collect(Collectors.toList());
-        List<Integer> lottoNumber=new ArrayList<>();
+        List<Integer> lottoNumber = new ArrayList<>();
         for (String s : numbers) {
             lottoNumber.add(Integer.parseInt(s));
         }
         Lotto lotto = new Lotto(lottoNumber);
+        numberCheck(lottoNumber);
         return lottoNumber;
+    }
+
+    public void numberCheck(List<Integer> numbers) {
+        for (int index = 0; index < numbers.size(); index++) {
+            if (numbers.get(index) < 1 || numbers.get(index) > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 숫자를 확인 하세요.");
+            }
+        }
     }
 }
