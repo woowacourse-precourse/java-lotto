5f7753664ba85c03528ba557ca08504dac12e688
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-10 11:30:11 +0900

    test : isAssignedCurrencyUnit 기능을 분화하고 isGreaterThan1000 기능에 대한 테스트 메서드 구현

--- src/test/java/lotto/domain/ValidationTest.java
+++ src/test/java/lotto/domain/ValidationTest.java
@@ -1,13 +1,14 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidationTest {
-
 
 	@DisplayName("유저가 입력한 구입금액이 숫자로만 이루어져 있는지 검증한다")
 	@ParameterizedTest
@@ -23,18 +24,27 @@ public class ValidationTest {
 		assertThat(Validation.isNumberOnly(input)).isFalse();
 	}
 
-	@DisplayName("유저가 입력한 구입금액이 1000원보다 크고 1000원 단위인지 검증한다")
+	@DisplayName("유저가 입력한 구입금액이 1000원 단위인지 검증한다")
 	@ParameterizedTest
-	@ValueSource(strings={"1000", "880000"})
+	@ValueSource(strings={"5000", "880000"})
 	void canVerifyAssignedCurrencyUnitReturningTrue(String input) {
 		assertThat(Validation.isAssignedCurrencyUnit(input)).isTrue();
 	}
 
-	@DisplayName("유저가 입력한 구입금액이 1000원보다 크고 1000원 단위인지 검증한다")
+	@DisplayName("유저가 입력한 구입금액이 1000원 단위인지 검증한다")
 	@ParameterizedTest
-	@ValueSource(strings={"500", "3700", "885200"})
+	@ValueSource(strings={"3700", "885200"})
 	void canVerifyAssignedCurrencyUnitReturningFalse(String input) {
 		assertThat(Validation.isAssignedCurrencyUnit(input)).isFalse();
+	}
+
+	@DisplayName("유저가 입력한 구입금액이 1000원보다 큰지 검증한다")
+	@Test
+	void canVerifyAmountIsGreaterThan1000(){
+		assertTrue(Validation.isGreaterThan1000("1000"));
+		assertTrue(Validation.isGreaterThan1000("22000"));
+		assertFalse(Validation.isGreaterThan1000("500"));
+
 	}
 
 }
