2e448136412fc2f28cfa5694381e49559de4e40b
hectick <chyeon1223@cau.ac.kr>
2022-11-11 01:39:36 +0900

    feat: 구매 금액에 해당하는 개수만큼 로또를 발행하는 기능 추가

--- src/main/java/lotto/domain/LottoPurchaseInformation.java
+++ src/main/java/lotto/domain/LottoPurchaseInformation.java
@@ -4,14 +4,24 @@ import java.util.ArrayList;
 
 public class LottoPurchaseInformation {
     private static final int THOUSAND = 1000;
+    private final int NUMBER_OF_TICKETS;
     private ValueValidation valueValidator = new ValueValidation();
     private ArrayList<Lotto> lottoTickets = new ArrayList<>();
-    private int numberOfTickets;
+    private LottoGenerator lottoGenerator = new LottoGenerator();
+
 
     public LottoPurchaseInformation(int purchaseAmount){
         valueValidator.checkDividedByThousand(purchaseAmount);
-        numberOfTickets = purchaseAmount/THOUSAND;
+        NUMBER_OF_TICKETS = purchaseAmount/THOUSAND;
     }
+
+    public void generateLottoTickets(){
+        for(int i = 0; i < NUMBER_OF_TICKETS; i++){
+            Lotto lotto = new Lotto(lottoGenerator.generateLottoNumbers());
+            lottoTickets.add(lotto);
+        }
+    }
+
 
 
 }
