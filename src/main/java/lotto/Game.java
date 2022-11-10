03aeedb2743e0293a978631efced4a010460d7db
sunwng <sunwgna@gmail.com>
2022-11-10 16:28:47 +0900

    feat: feature_number_5 / add generating numbers feature

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,11 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Game {
-    private int gameNumber;
-    private Lotto[] games;
+    int gameNumber; // TODO : test 이후 private로 다시 변경
+    Lotto[] games; // TODO : test 이후 private로 다시 변경
 
     public Game(int money) {
         this.gameNumber = money % 1000;
         this.games = new Lotto[gameNumber];
+    }
+
+    public void generateGame() {
+        for (int i = 0; i < gameNumber; i++) {
+            games[i] = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        }
     }
 }
---
+++ src/test/java/lotto/GameTest.java
@@ -0,0 +1,9 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameTest {
+}
