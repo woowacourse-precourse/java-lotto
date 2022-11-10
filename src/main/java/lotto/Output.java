2530670cc940c748a077f03dcc81eb12dbfce4c4
HyebinShin <hyeviin.dev@gmail.com>
2022-11-10 19:40:34 +0900

    Feat: 출력 클래스 분리 - 로또 구입 및 발행
    로또 구입 묻기
    발행된 로또 개수 출력
    발행된 로또 목록 출력

---
+++ src/main/java/lotto/Output.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import java.util.List;
+
+public class Output {
+    public static String budget;
+    public static List<Lotto> lottos;
+    public static void askBuy() {
+        System.out.println(Print.ASK_BUY);
+        budget = Input.inputBudget();
+    }
+
+    public static void answerBuy() {
+        lottos = Lotto.getLotto(budget);
+        System.out.println(lottos.size() + Print.ANSWER_BUY);
+    }
+
+    public static void printLotto() {
+        for(Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
+}
