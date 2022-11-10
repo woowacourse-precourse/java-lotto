998d364d0a7808df7bbc539090f76f89f6c8fd35
whipbaek <jibaek0513@gmail.com>
2022-11-10 20:24:59 +0900

    refactor(User): 로또 넘버 입력 검사 기능을 세분화 함 - unValidShape, duplicatedNumber, checkNumberArea 3개의 메소드 추가 - 입력형태, 중복검사, 범위체크를 구성하는 메소드들로 구분

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -23,7 +23,7 @@ public class User {
         userMoney = inputMoney;
     }
 
-    private void validateInputNumber(int inputMoney) {
+    public void validateInputNumber(int inputMoney) {
         if(inputMoney%UNIT != 0){
             System.out.println("[ERROR] 금액은 1000원 단위로 나뉘어 떨어져야 합니다.");
             throw new IllegalArgumentException();
@@ -36,21 +36,39 @@ public class User {
         lottos.add(new Lotto(convertLottoNumber(inputLottoNumber)));
     }
 
-    private List<Integer> convertLottoNumber(String inputLottoNumber) {
+    public List<Integer> convertLottoNumber(String inputLottoNumber) {
         ArrayList<String> split = new ArrayList<>(List.of(inputLottoNumber.split(", ")));
         return split.stream()
                 .map(s -> Integer.parseInt(s))
                 .collect(Collectors.toList());
     }
 
-    private void validateInputLottoNumber(String inputLottoNumber) {
+    public void validateInputLottoNumber(String inputLottoNumber) {
         ArrayList<String> split = new ArrayList<>(List.of(inputLottoNumber.split(", ")));
-        if(split.size() != LOTTO_LEN){
+        unValidShape(split);
+
+        List<Integer> lottoNumber = convertLottoNumber(inputLottoNumber);
+        duplicatedNumber(lottoNumber);
+        checkNumberArea(lottoNumber);
+    }
+
+    public void unValidShape(ArrayList<String> lottoNumber){
+        if(lottoNumber.size() != LOTTO_LEN){
             System.out.println("[ERROR] 숫자 입력이 잘못 되었습니다.");
             throw new IllegalArgumentException();
         }
-        List<Integer> lottoNumber = convertLottoNumber(inputLottoNumber);
+    }
 
+    public void duplicatedNumber(List<Integer> lottoNumber) {
+        List<Integer> collect = lottoNumber.stream().distinct().collect(Collectors.toList());
+        if(collect.size() != LOTTO_LEN){
+            System.out.println("[ERROR] 중복된 숫자가 있습니다.");
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+    public void checkNumberArea(List<Integer> lottoNumber){
         for (Integer integer : lottoNumber) {
             if(integer > LOTTO_MAX_SIZE || integer < LOTTO_MIN_SIZE){
                 System.out.println("[ERROR] 숫자는 1이상 45 이하의 숫자로 구성되어야 합니다.");
