fb403b240e165f4eca50f4f0f0759f5571cff4f3
java-saeng <gusdn85554@naver.com>
2022-11-11 00:23:00 +0900

    fix : 로또 등수에 들지 않을 경우 예외 던지지 않음
    예외를 던지게 되면 애플리케이션이 종료되어 다른 Enum 생성하여 분기 처리

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -2,15 +2,14 @@ package lotto.model;
 
 import java.util.Arrays;
 
-import static lotto.global.ExceptionConstants.*;
-
 public enum LottoRank {
 
     FIRST(6, false, "6개 일치", 2_000_000_000),
     SECOND(5, true, "5개 일치, 보너스 볼 일치", 30_000_000),
     THIRD(5, false, "5개 일치", 1_500_000),
     FOURTH(4, false, "4개 일치", 50_000),
-    FIFTH(3, false, "3개 일치", 5_000);
+    FIFTH(3, false, "3개 일치", 5_000),
+    NONE(1, false, "실패", 0);
 
     private final long count;
     private final boolean bounsFlag;
@@ -29,7 +28,7 @@ public enum LottoRank {
                      .filter(lottoRank -> lottoRank.count == matchingCount)
                      .filter(lottoRank -> lottoRank.bounsFlag == bounsFlag)
                      .findFirst()
-                     .orElseThrow(() -> new IllegalArgumentException(LESS_THAN_THREE_MATCHING_NUMBER));
+                     .orElse(NONE);
     }
 
     public String getDescription() {
