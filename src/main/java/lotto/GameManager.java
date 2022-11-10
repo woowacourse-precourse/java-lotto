f051e7797f9fcbf0b098568e598b5f866cd3487f
hum02 <ehdgma02@gmail.com>
2022-11-10 21:46:56 +0900

    feat(lotto): 구매금액,당첨번호,보너스번호를 입력받아 로또게임을 생성해 길행 구현
    GameManager의 run() 메소드에서 실행

--- src/main/java/lotto/GameManager.java
+++ src/main/java/lotto/GameManager.java
@@ -1,9 +1,11 @@
 package lotto;
 
+import lotto.utils.View;
 import java.util.ArrayList;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static lotto.utils.Input.*;
 
 public class GameManager {
 
@@ -11,6 +13,34 @@ public class GameManager {
     }
 
     public void run() {
+        View.printPriceMessage();
+        Integer price = inputPrice();
+        List<Lotto> publishedLottos = publishLottos(getCountFromPrice(price));
+        View.printPublishedLottos(publishedLottos);
+
+        View.printWinningLottoMessage();
+        Lotto winningLotto = new Lotto(inputWinningLotto());
+
+        View.printBonusNumberMessage();
+        Integer bonusNumber = inputBonusNumber();
+
+        LottoGame game = new LottoGame(price, bonusNumber, winningLotto, publishedLottos);
+        game.play();
+    }
+
+    private Integer getCountFromPrice(Integer Price) {
+        Integer lottoCount = Price / 1000;
+        return lottoCount;
+    }
+
+    private List<Lotto> publishLottos(Integer lottoCount) {
+        List<Lotto> Lottos = new ArrayList<>();
+        while (Lottos.size() < lottoCount) {
+            Lotto lotto = publishRandomLotto();
+            Lottos.add(lotto);
+        }
+
+        return Lottos;
     }
 
     private Lotto publishRandomLotto() {
