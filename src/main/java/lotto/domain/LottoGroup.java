c19b904693c255da9230daa0cde2fe400572958e
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-10 16:27:24 +0900

    feat : 로또 그룹 생성자에 출력 기능 추가
    구매한 로또 번호를 모두 출력한다

--- src/main/java/lotto/domain/LottoGroup.java
+++ src/main/java/lotto/domain/LottoGroup.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
+
+import lotto.view.MainView;
 
 public class LottoGroup {
 
@@ -8,10 +11,16 @@ public class LottoGroup {
 
 	public LottoGroup(List<Lotto> lottos) {
 		this.lottos = lottos;
+		MainView.purchaseLottoGroup(getLottosSize());
+		MainView.printAllLotto(getLottos());
 	}
 
 	public int getLottosSize() {
 		return lottos.size();
+	}
+
+	private List<Lotto> getLottos() {
+		return Collections.unmodifiableList(this.lottos);
 	}
 
 }
