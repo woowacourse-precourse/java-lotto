ced42f433c7d9309b8883fab4db51d643b390d21
juhwang <juhong4930@naver.com>
2022-11-10 20:56:55 +0900

    feat(LottoDraw): validate duplication in numbers

--- src/main/java/user/LottoDraw.java
+++ src/main/java/user/LottoDraw.java
@@ -1,11 +1,15 @@
 package user;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class LottoDraw {
     private static final String COMMA_ERROR = "쉼표로 구분된 6개 숫자를 입력해 주시기 바랍니다.";
+    private static final String DUPLICATION_ERROR = "입력된 숫자에 중복이 있습니다.";
+
     private static LottoDraw lottoDraw;
 
     private LottoDraw() {
@@ -20,13 +24,29 @@ public class LottoDraw {
 
     public List<Integer> pickWinNumbers(String readline) {
         validateComma(readline);
-        int[] a = Arrays.stream(readline.split(",")).mapToInt(Integer::parseInt).toArray();
-        return Arrays.stream(a).boxed().collect(Collectors.toList());
+        List<Integer> winNumbers = changeStringToInt(readline);
+        validateDuplication(winNumbers);
+        return winNumbers;
+    }
+
+    private List<Integer> changeStringToInt(String readline) {
+        int[] changeInteger= Arrays.stream(readline.split(",")).mapToInt(Integer::parseInt).toArray();
+        return Arrays.stream(changeInteger).boxed().collect(Collectors.toList());
     }
 
     private void validateComma(String readline) {
         if (readline.split(",").length != 6) {
             throw new IllegalArgumentException(COMMA_ERROR);
         }
+    }
+
+    private void validateDuplication(List<Integer> winNumbers) {
+        if (duplicateNumbers(winNumbers)){
+            throw new IllegalArgumentException(DUPLICATION_ERROR);
+        }
+    }
+
+    private boolean duplicateNumbers(List<Integer> winNumbers) {
+        return new HashSet<>(winNumbers).size() != winNumbers.size();
     }
 }
