547ea4ed531245c4f73548eb6152ea4c40175903
SEONMl <sm000117@naver.com>
2022-11-10 17:10:00 +0900

    feat: 맞힌 개수와 당첨 금액 포맷팅해서 반환하는 메서드 구현

--- src/main/java/lotto/domain/Winner.java
+++ src/main/java/lotto/domain/Winner.java
@@ -1,21 +1,31 @@
 package lotto.domain;
 
+import lotto.config.InputConfig;
+
 public enum Winner {
-    FIRST(1, false, 2000000000),
-    SECOND(2, true, 30000000),
-    THIRD(3, false, 1500000),
-    FOURTH(4, false, 50000),
-    FIFTH(5, false, 5000),
+    FIRST(InputConfig.LOTTO_NUMBER_COUNT, false, 2000000000),
+    SECOND(InputConfig.LOTTO_NUMBER_COUNT-1, true, 30000000),
+    THIRD(InputConfig.LOTTO_NUMBER_COUNT-1, false, 1500000),
+    FOURTH(InputConfig.LOTTO_NUMBER_COUNT-2, false, 50000),
+    FIFTH(InputConfig.LOTTO_NUMBER_COUNT-3, false, 5000),
     NONE(0, false, 0);
 
-    private int rank;
+    private int correctness;
     private boolean value;
     private int prizeMoney;
 
-    Winner(int rank, boolean value, int prizeMoney) {
-        this.rank = rank;
+    Winner(int correctness, boolean value, int prizeMoney) {
+        this.correctness = correctness;
         this.value = value;
         this.prizeMoney = prizeMoney;
     }
 
+    public int getCorrectnessCount() {
+        return correctness;
+    }
+
+    public String getPrizeMoney() {
+        String formatted = String.format("%,d",prizeMoney);
+        return formatted;
+    }
 }
