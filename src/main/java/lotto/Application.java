a1dd4ca0355ffd7fa30dff3dcbaf2d7f773b2d29
jwp345 <jwp135@naver.com>
2022-11-11 01:37:36 +0900

    feat: 당첨 번호와 보너스 번호를 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,13 +9,50 @@ public class Application {
     public static void main(String[] args) {
         int price = askPrice();
         List<Lotto> lottos = makeLotto(price);
+        Lotto winningLotto = askWinning();
+        int bonusNumber = askBonus();
+    }
+
+    public static int askBonus() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = readLine();
+        int num = validateNumber(input);
+        return num;
+    }
+
+    public static Lotto askWinning() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = readLine();
+        List<Integer> nums = new ArrayList<>();
+        for (String str : input.split(",")) {
+            int num = validateNumber(str);
+            nums.add(Integer.parseInt(str));
+        }
+
+        Lotto winning = new Lotto(nums);
+        return winning;
+    }
+
+    public static int validateNumber(String str) {
+        int num;
+        try {
+            num = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자여야 합니다.");
+        }
+        if(num < 1 || num > 45) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1 ~ 45여야 합니다.");
+        }
+        return num;
     }
 
     public static List<Lotto> makeLotto(int price) {
         if(price % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] price must divided by 1000");
+            throw new IllegalArgumentException("[ERROR] 가격은 1000원 단위여야 합니다.");
         }
         int amount = price / 1000;
+        System.out.println(amount + "개를 구매했습니다.");
+
         List<Lotto> lottos = new ArrayList<>();
         while(lottos.size() < amount) {
             List<Integer> nums = pickUniqueNumbersInRange(1, 45, 6);
@@ -26,6 +63,10 @@ public class Application {
                 continue;
             }
             lottos.add(lotto);
+        }
+
+        for(Lotto lotto : lottos) {
+            System.out.println(lotto);
         }
         return lottos;
     }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,27 +8,26 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
         int[] nums = new int[46];
         for(int num : numbers) {
             nums[num]++;
             if(nums[num] > 1) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("[ERROR] 중복된 로또 번호가 존재합니다.");
             }
         }
     }
 
     @Override
     public String toString() {
-        return "Lotto{" +
-                "numbers=" + numbers +
-                '}';
+        return numbers.toString();
     }
 
     // TODO: 추가 기능 구현
