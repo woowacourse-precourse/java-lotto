7236520903a75fcaf9264b799dea475a971f960f
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-10 15:01:07 +0900

    feat : 일급 컬렉션 추가

---
+++ src/main/java/lotto/domain/LottoGroup.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoGroup {
+
+	private final List<Lotto> lottos;
+
+	public LottoGroup(List<Lotto> lottos) {
+		this.lottos = lottos;
+	}
+
+}
