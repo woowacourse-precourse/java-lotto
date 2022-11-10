043ae90add9156914c0c77d6d403fda3b5502e63
Albert <guang7226961@naver.com>
2022-11-10 16:00:38 +0900

    test($model) 숫자를 받고 상태 반환 테스트 추가

--- src/test/java/lotto/model/LottoStatusTest.java
+++ src/test/java/lotto/model/LottoStatusTest.java
@@ -6,6 +6,7 @@ import static lotto.model.Constants.LottoStatus.MATCH_FOUR_PROFIT;
 import static lotto.model.Constants.LottoStatus.MATCH_THREE_PROFIT;
 import static lotto.model.Constants.LottoStatus.MATH_SIX_PROFIT;
 import static lotto.model.Constants.LottoStatus.MATH_UNSATISFIED_PROFIT;
+import static lotto.model.LottoStatus.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
@@ -16,22 +17,34 @@ class LottoStatusTest {
     @DisplayName("각 상태에서 수익을 반환")
     @Test
     void return_profit() {
-        LottoStatus lottoStatus = LottoStatus.MATCH_UNSATISFIED;
+        LottoStatus lottoStatus = MATCH_UNSATISFIED;
         assertThat(lottoStatus.getProfit()).isEqualTo(MATH_UNSATISFIED_PROFIT);
 
-        lottoStatus = LottoStatus.MATCH_THREE;
+        lottoStatus = MATCH_THREE;
         assertThat(lottoStatus.getProfit()).isEqualTo(MATCH_THREE_PROFIT);
 
-        lottoStatus = LottoStatus.MATCH_FOUR;
+        lottoStatus = MATCH_FOUR;
         assertThat(lottoStatus.getProfit()).isEqualTo(MATCH_FOUR_PROFIT);
 
-        lottoStatus = LottoStatus.MATCH_FIVE;
+        lottoStatus = MATCH_FIVE;
         assertThat(lottoStatus.getProfit()).isEqualTo(MATCH_FIVE_PROFIT);
 
-        lottoStatus = LottoStatus.MATCH_FIVE_ADD_BONUS;
+        lottoStatus = MATCH_FIVE_ADD_BONUS;
         assertThat(lottoStatus.getProfit()).isEqualTo(MATCH_FIVE_ADD_BONUS_PROFIT);
 
-        lottoStatus = LottoStatus.MATCH_SIX;
+        lottoStatus = MATCH_SIX;
         assertThat(lottoStatus.getProfit()).isEqualTo(MATH_SIX_PROFIT);
+    }
+
+    @DisplayName("숫자를 받고 상태 반환")
+    @Test
+    void get_count_return_status(){
+        assertThat(checkValue(0)).isEqualTo(MATCH_UNSATISFIED);
+        assertThat(checkValue(1)).isEqualTo(MATCH_UNSATISFIED);
+        assertThat(checkValue(2)).isEqualTo(MATCH_UNSATISFIED);
+        assertThat(checkValue(3)).isEqualTo(MATCH_THREE);
+        assertThat(checkValue(4)).isEqualTo(MATCH_FOUR);
+        assertThat(checkValue(5)).isEqualTo(MATCH_FIVE);
+        assertThat(checkValue(6)).isEqualTo(MATCH_SIX);
     }
 }
