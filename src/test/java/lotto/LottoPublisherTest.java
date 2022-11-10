e556c1f52d17628b22b463a7841ad330abc2fa42
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-10 16:34:41 +0900

    test(publishLottosAccordingToPayment): implement test method and found error #1
    Error #1 is about number of lottos published.
    Should publish payment / 1000 but published 0 lottos.

--- src/test/java/lotto/LottoPublisherTest.java
+++ src/test/java/lotto/LottoPublisherTest.java
@@ -2,11 +2,23 @@ package lotto;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LottoPublisherTest {
 
     @Test
     void publishLottosAccordingToPayment() {
+        // given
+        LottoPublisher publisher = new LottoPublisher();
 
+        // when
+        int payment = 5000;
+        List<Lotto> lottos = publisher.publishLottos(payment);
+
+        // then
+        assertThat(lottos.size()).isEqualTo(payment / 1000);
     }
 
     @Test
