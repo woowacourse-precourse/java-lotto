d2c5e19dbc1a602d7d0643e45d5ef9992d101d58
khsrla9806 <kimhunsope@naver.com>
2022-11-10 20:01:29 +0900

    feat(InputLogic): 당첨 번호를 입력받는 메서드 구현 enterWinningNumber: 당첨 번호를 입력받아 List형태로 반환

--- src/main/java/lotto/ui/InputLogic.java
+++ src/main/java/lotto/ui/InputLogic.java
@@ -2,8 +2,16 @@ package lotto.ui;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputLogic {
     private static int lottoIssueCount = 0;
+    public static int getLottoIssueCount() {
+        return lottoIssueCount;
+    }
     public static int enterPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         int purchaseAmount = Integer.valueOf(Console.readLine());
@@ -12,11 +20,6 @@ public class InputLogic {
         System.out.println();
         return purchaseAmount;
     }
-
-    public static int getLottoIssueCount() {
-        return lottoIssueCount;
-    }
-
     public static void validatePurchaseAmount(int purchaseAmount) {
         if (purchaseAmount == 0) {
             throw new IllegalArgumentException("[ERROR] 입력된 금액이 0원보다는 커야 합니다.");
@@ -24,5 +27,15 @@ public class InputLogic {
         if (purchaseAmount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 입력된 금액이 1000원 단위로 나눠 떨어져야 합니다.");
         }
+    }
+
+    public static List<Integer> enterWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] numbers = Console.readLine().split(",");
+        List<Integer> winningNumber = Arrays.stream(numbers)
+                .map(number -> Integer.valueOf(number))
+                .collect(Collectors.toList());
+        Collections.sort(winningNumber);
+        return winningNumber;
     }
 }
