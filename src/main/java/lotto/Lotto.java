8ef159fb188d30c0fb191f406fd8e35ea3583b94
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-11-10 14:35:34 +0900

    <feat>Lotto : validate 메서드 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -18,7 +19,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 입력한 로또 번호의 개수가 6개가 아닙니다.");
+        }
+        if (Collections.min(numbers) < 1 || Collections.max(numbers) > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
         }
     }
 
