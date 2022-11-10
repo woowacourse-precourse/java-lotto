ccb9a86396b90192f646a12bd07b4d347ca4f1c1
H4R1B0 <qkrguswns25@naver.com>
2022-11-10 22:10:30 +0900

    feat: compare between win numbers and lotto numbers

--- src/main/java/lotto/Calculation.java
+++ src/main/java/lotto/Calculation.java
@@ -1,8 +1,24 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Calculation {
     public static String profitRate(int cost, int earn) {
         double rate = (double) earn / (double) cost * 100;
         return String.format("%.1f%%", rate);
+    }
+
+    public static List<Integer> compareLotto(List<Integer> winNumbers, int bonus, List<Integer> lotto) {
+        int[] result = new int[2];
+        for (int number : lotto) {
+            if (winNumbers.contains(number)) {
+                result[0]++;
+            }
+            else if(number == bonus){
+                result[1]++;
+            }
+        }
+        return List.of(result[0], result[1]);
     }
 }
