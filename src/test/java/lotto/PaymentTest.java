77984297270e2e37268e929d57c79a7e483b2d42
MuSeung <peter0757@nvaer.com>
2022-11-10 16:30:32 +0900

    test: Payment가 올바른 구매수를 반환하는지 테스트
    lottoPurchase
        - 8000이 입력 되었을 때 8이 나오는지 확인
        - 19000이 입력 되었을 때 19가 나오는지 확인

--- src/test/java/lotto/PaymentTest.java
+++ src/test/java/lotto/PaymentTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,5 +13,13 @@ public class PaymentTest {
     void createPaymentByWrongNumber() {
         assertThatThrownBy(() -> new Payment(1500))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("로또 구매수")
+    void lottoPurchase(){
+        Payment payment = new Payment(8000);
+        assertThat(payment.purchase()).isEqualTo(8);
+        payment = new Payment(19000);
+        assertThat(payment.purchase()).isEqualTo(19);
     }
 }
