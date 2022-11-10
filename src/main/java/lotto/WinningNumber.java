c6e1b11b8926436cedfd18dd9dd574a787c34018
dohyun <ldh19738@gmail.com>
2022-11-10 20:00:13 +0900

    feat(WinningNumber): 보너스 번호 입력받는 기능 추가

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -27,6 +27,14 @@ public class WinningNumber {
         return inputNumbers;
     }
 
+    public int inputBonusNumber() {
+       int bonusNumber = Integer.valueOf(Console.readLine());
+
+       inputException(bonusNumber);
+
+       return bonusNumber;
+    }
+
     private void inputException(int number) {
         if(number < 1 || number > 45) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
