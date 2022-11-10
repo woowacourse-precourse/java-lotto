0e2d0837fe1f24cd925c566f57b70ef5cbc91721
junhei Lee <jun2ee22@naver.com>
2022-11-10 15:46:29 +0900

    refactor(domain): 금액 클래스 리팩토링
    생성 시 투입 금액 초기화로 변경

--- src/main/java/lotto/domain/Amount.java
+++ src/main/java/lotto/domain/Amount.java
@@ -3,14 +3,26 @@ package lotto.domain;
 
 public class Amount {
     private final static int lottoPrice = 1000;
+    private final int inputAmount;
 
-    public int calculatePurchasesQuantity(int inputAmount) {
+    private int purchasesQuantity;
+
+    Amount(int inputAmount) {
+        this.inputAmount = inputAmount;
+    }
+
+    public int getPurchasesQuantity() {
+        calculatePurchasesQuantity();
+        return this.purchasesQuantity;
+    }
+    private void calculatePurchasesQuantity() {
         int purchasesQuantity = inputAmount / lottoPrice;
         if (inputAmount % lottoPrice != 0) {
             throw new IllegalArgumentException();
         }
-        return purchasesQuantity;
+        this.purchasesQuantity = purchasesQuantity;
     }
+
 
     public double calculateEarningRate() {
         return 0;
--- src/test/java/lotto/domain/AmountTest.java
+++ src/test/java/lotto/domain/AmountTest.java
@@ -11,21 +11,20 @@ public class AmountTest {
 
     private Amount amount;
 
-    @BeforeEach
-    void setUp() {
-        amount = new Amount();
-    }
-
     @DisplayName("1000원으로 나누어 떨어지지 않으면 예외가 발생한다")
     @Test
     void inputNumberDividedBy1000RemainingNumber() {
-        assertThatThrownBy(() -> amount.calculatePurchasesQuantity(12344))
+        amount = new Amount(12344);
+
+        assertThatThrownBy(() -> amount.getPurchasesQuantity())
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("만 원 입력 시 10개를 반환한다")
     @Test
     void returnPurchasesNumber() {
-        assertThat(amount.calculatePurchasesQuantity(10000)).isEqualTo(10);
+        amount = new Amount(10000);
+
+        assertThat(amount.getPurchasesQuantity()).isEqualTo(10);
     }
 }
