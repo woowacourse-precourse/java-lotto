483ce20de660eaad4f4319d7f5313e057908d73e
jinny-l <jinny.l.dev@gmail.com>
2022-11-10 20:52:41 +0900

    refactor(WinnerInfo): 필드 변수 반환 타입을 String에서 int로 수정
    - 목적: 다른 클래스에서 연산 시 활용하기 위함

--- src/main/java/lotto/util/WinnerInfo.java
+++ src/main/java/lotto/util/WinnerInfo.java
@@ -2,17 +2,17 @@ package lotto.util;
 
 public enum WinnerInfo {
 
-    RANK1(1, "6개 일치", 2_000_000_000),
-    RANK2(2, "5개 일치, 보너스 볼 일치", 30_000_000),
-    RANK3(3, "5개 일치", 1_500_000),
-    RANK4(4, "4개 일치", 50_000),
-    RANK5(5, "3개 일치", 5_000);
+    RANK1(1, 6, 2_000_000_000),
+    RANK2(2, 5, 30_000_000),
+    RANK3(3, 5, 1_500_000),
+    RANK4(4, 4, 50_000),
+    RANK5(5, 3, 5_000);
 
     private final int rank;
-    private final String winningCondition;
+    private final int winningCondition;
     private final int prize;
 
-    WinnerInfo(int rank, String winningCondition, int prize) {
+    WinnerInfo(int rank, int winningCondition, int prize) {
         this.rank = rank;
         this.winningCondition = winningCondition;
         this.prize = prize;
@@ -22,7 +22,7 @@ public enum WinnerInfo {
         return this.rank;
     }
 
-    public String getWinningCondition() {
+    public int getWinningCondition() {
         return this.winningCondition;
     }
 
