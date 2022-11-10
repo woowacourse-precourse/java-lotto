acac74642c2388038b7c0431c6ce222acfc55356
juhwang <juhong4930@naver.com>
2022-11-10 16:25:44 +0900

    refactor(Customer): move validatePayUnit method to LottoMachine and add validateNumeric method

--- src/main/java/user/Customer.java
+++ src/main/java/user/Customer.java
@@ -3,21 +3,23 @@ package user;
 import store.Lotto;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Customer {
-    private static final String UNIT_ERROR = "[ERROR] 올바른 단위가 아닙니다.";
+    private static final String NUMERIC_ERROR = "[ERROR] 숫자를 입력해 주시기 바랍니다.";
 
     private List<Lotto> lotteries;
     private int pay;
 
-    public Customer(int pay){
-        validatePayUnit(pay);
+    public Customer(String readline) {
+        validateNumeric(readline);
+        int pay = Integer.parseInt(readline);
         this.pay = pay;
     }
 
-    private void validatePayUnit(int pay) {
-        if(pay%1000 != 0){
-            throw new IllegalArgumentException(UNIT_ERROR);
+    private void validateNumeric(String readline) {
+        if (Pattern.matches("^\\d", readline)) {
+            throw new IllegalArgumentException(NUMERIC_ERROR);
         }
     }
 
