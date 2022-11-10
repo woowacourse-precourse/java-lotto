a1fa7bc2452289a5c033eef96da48b3a9d2ff002
Albert <guang7226961@naver.com>
2022-11-10 17:55:12 +0900

    refactor($model) 보너스 로또 번호 예외처리

--- src/main/java/lotto/model/InputValidator.java
+++ src/main/java/lotto/model/InputValidator.java
@@ -5,14 +5,19 @@ import static lotto.model.Changer.StringToIntegers;
 import java.util.List;
 
 class InputValidator {
+
+    public static final String BONUS_NUMBER_ERROR = "정확한 보너스 번호가 아닙니다.";
+
     public static void isValid(String input) {
         List<Integer> list = StringToIntegers(input);
         NumbersValidator.isValid(list);
     }
 
-    public static boolean isValidBonusNumber(String targetLotto, String bonusNumber) {
+    public static void isValidBonusNumber(String targetLotto, String bonusNumber) {
         List<Integer> lottoNumbers = StringToIntegers(targetLotto);
         Integer bonusNum = Integer.valueOf(bonusNumber);
-        return lottoNumbers.contains(bonusNum);
+        if (lottoNumbers.contains(bonusNum)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_ERROR);
+        }
     }
 }
--- src/test/java/lotto/model/InputValidatorTest.java
+++ src/test/java/lotto/model/InputValidatorTest.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static lotto.model.InputValidator.BONUS_NUMBER_ERROR;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +12,8 @@ class InputValidatorTest {
     @DisplayName("보너스 로또 번호 예외처리")
     @Test
     void is_valid_bonus_number() {
-        assertThat(InputValidator.isValidBonusNumber("17,2,3,4,5,6", "7")).isFalse();
-        assertThat(InputValidator.isValidBonusNumber("1,2,3,4,5,6", "1")).isTrue();
+        assertThatThrownBy(() -> InputValidator.isValidBonusNumber("17,2,3,4,5,6", "17")).isInstanceOf(
+                IllegalArgumentException.class).hasMessage(BONUS_NUMBER_ERROR);
+        assertThatCode(() -> InputValidator.isValidBonusNumber("17,2,3,4,5,6", "7")).doesNotThrowAnyException();
     }
 }
