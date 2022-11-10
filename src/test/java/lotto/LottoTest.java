9031ee45c4baa957d28d7b18b262822bcbd81545
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-11-10 15:13:44 +0900

    <test>LottoTest : 새로운 생성자 테스트 코드 작성
    생성자 안에 validate메서드가 있기 때문에 유효성을 확인하지 않고 인스턴스 생성 유무만을 확인했습니다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -34,5 +35,12 @@ class LottoTest {
     void createLottoByUnderMinNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void checkValidityNewConstructor() {
+        Lotto test = new Lotto(6);
+
+        assertThat(test).isInstanceOf(Lotto.class);
     }
 }
