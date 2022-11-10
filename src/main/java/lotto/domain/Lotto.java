58d1983c32f1c4d0c20de1fbc7f80b07307ee439
Jzakka <mouse4786@gmail.com>
2022-11-11 00:10:33 +0900

    feat: 에러메시지 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -24,13 +24,13 @@ public class Lotto {
     private void uniqueValidate(List<Integer> numbers) {
         List<Integer> uniqueNumbers = numbers.stream().distinct().collect(Collectors.toList());
         if (uniqueNumbers.size() != numbers.size()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호는 중복되어선 안됩니다.");
         }
     }
 
     private void sizeValidate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자는 6개여야 합니다.");
         }
     }
 }
