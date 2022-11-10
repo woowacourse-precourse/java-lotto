4f540629ea7f319b3281562020693097ff2b6224
jhon3242 <rldotnr2@gmail.com>
2022-11-10 12:42:01 +0900

    feat(UnitTest) : 로또 발행 테스트 구현

--- src/test/java/lotto/UnitTest.java
+++ src/test/java/lotto/UnitTest.java
@@ -1,17 +1,31 @@
 package lotto;
 
 import jdk.jshell.execution.Util;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UnitTest {
 
 	@Test
 	void 랜덤_숫자_추출_테스트(){
 		int randomNum = Util.getRandomNum(1, 45);
-		Assertions.assertThat(randomNum)
+		assertThat(randomNum)
 				.isGreaterThanOrEqualTo(1)
 				.isLessThanOrEqualTo(45);
 	}
+
+	@Test
+	void 로또_발행_테스트(){
+		List<Integer> randomNums = Util.getDistinctRandomNums(6);
+		List<Integer> filtered = randomNums.stream()
+				.distinct()
+				.collect(Collectors.toList());
+		assertThat(filtered.size()).isEqualTo(6);
+	}
+
 
 }
