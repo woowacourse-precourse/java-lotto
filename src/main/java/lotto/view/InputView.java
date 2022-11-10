6b881d34b3387fac6bdb5826feeb631a4e20614c
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-10 16:35:14 +0900

    feat(InputView): LottoNumber을 Input하는 View 작성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,8 +3,13 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.Utils.InputValid;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
     private static final String NUMBER_OF_USER_PRICE_INPUT_MESSAGE="구입금액을 입력해 주세요.";
+    private static final String LIST_OF_LOTTO_INPUT_MESSAGE="당첨 번호를 입력해 주세요.";
 
     public static int getUserPriceNumber(){
         System.out.println(NUMBER_OF_USER_PRICE_INPUT_MESSAGE);
@@ -13,5 +18,16 @@ public class InputView {
 
         InputValid.checkPriceUnit(userPrice);
         return userPrice;
+    }
+
+    public static List<Integer> getLottoNumber(){
+        System.out.println(LIST_OF_LOTTO_INPUT_MESSAGE);
+        String strLottoNumber=Console.readLine();
+        List<String> listLottoNumber= Arrays.asList(strLottoNumber.split(","));
+
+        return listLottoNumber
+                .stream()
+                .map(s->Integer.parseInt(s))
+                .collect(Collectors.toList());
     }
 }
