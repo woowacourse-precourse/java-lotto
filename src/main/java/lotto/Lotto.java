0d3d7ac35f844383fd9b89c45ca5aabe44937ba9
gusu9 <suhan.ku9@gmail.com>
2022-11-10 11:22:16 +0900

    comment: Add comments for random creation function in Lotto.java

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    // final의 의미는 그 list format이 final? 아니면 그곳에 들어가는 값이 final?
+    // [질문] Lotto class 에서 random 생성 명령까지 내리고 Application에서는 random 함수를 안쓸수는 없나? - 그냥 출력만 받아오는 방식은?
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
