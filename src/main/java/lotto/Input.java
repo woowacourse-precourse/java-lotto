6e385c927599b94d421c8337021bfaa4220da870
areyouhun <areyouhun@gmail.com>
2022-11-11 01:16:45 +0900

    refactor(input): 중복 제거

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -3,29 +3,15 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
+    // 상수는 추후 application으로 이동
     private final static String ENTER_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
     private final static String ENTER_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private final static String ENTER_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-    private final static String COMMA = ",";
 
-    public static final String price() {
-        System.out.println(ENTER_PURCHASE_PRICE_MESSAGE);
-        String input = Console.readLine();
+    public static final String submit(String message) {
+        System.out.println(message);
+        String inputValue = Console.readLine();
         System.out.println();
-        return input;
-    }
-
-    public static final String winningNumbers() {
-        System.out.println(ENTER_WINNING_NUMBERS_MESSAGE);
-        String input = Console.readLine();
-        System.out.println();
-        return input;
-    }
-
-    public static final String bonusNumber() {
-        System.out.println(ENTER_BONUS_NUMBER_MESSAGE);
-        String input = Console.readLine();
-        System.out.println();
-        return input;
+        return inputValue;
     }
 }
