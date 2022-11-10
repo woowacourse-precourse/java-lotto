ca8b75960e98e62e135d1696694453c4c2c75045
CMHyuk <alsgur9710@naver.com>
2022-11-11 00:02:13 +0900

    feat(InputView): 로또 구입 수 출력문

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,8 +2,13 @@ package lotto.view;
 
 public class InputView {
     private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private static final String PURCHASE_LOTTO = "개를 구매했습니다.";
 
     public static void setInputMoney() {
         System.out.println(INPUT_MONEY);
+    }
+
+    public static void printLottoCount(int count) {
+        System.out.println(count + PURCHASE_LOTTO);
     }
 }
