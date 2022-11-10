17dfda3dfdf634772c46f61ac93427c56736e58c
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-10 12:16:16 +0900

    test : 가독성을 위해 표현 변경(NotANumber, less than)

--- src/test/java/lotto/domain/ValidationTest.java
+++ src/test/java/lotto/domain/ValidationTest.java
@@ -12,38 +12,38 @@ public class ValidationTest {
 
 	@DisplayName("유저가 입력한 구입금액이 숫자로만 이루어져 있는지 검증한다")
 	@ParameterizedTest
-	@ValueSource(strings={"123", "10000000"})
-	void canVerifyNumberOnlyReturningTrue(String input) {
-		assertThat(Validation.isNumberOnly(input)).isTrue();
+	@ValueSource(strings={"1,000", "10l", "ABab"})
+	void VerifyNotANumberReturningTrue(String input) {
+		assertThat(Validation.isNotANumber(input)).isFalse();
 	}
 
 	@DisplayName("유저가 입력한 구입금액이 숫자로만 이루어져 있는지 검증한다")
 	@ParameterizedTest
-	@ValueSource(strings={"1,000", "10l", "ABab"})
-	void canVerifyNumberOnlyReturningFalse(String input) {
-		assertThat(Validation.isNumberOnly(input)).isFalse();
+	@ValueSource(strings={"123", "10000000"})
+	void VerifyNotANumberReturningFalse(String input) {
+		assertThat(Validation.isNotANumber(input)).isTrue();
 	}
 
 	@DisplayName("유저가 입력한 구입금액이 1000원 단위인지 검증한다")
 	@ParameterizedTest
 	@ValueSource(strings={"5000", "880000"})
-	void canVerifyAssignedCurrencyUnitReturningTrue(String input) {
+	void VerifyAssignedCurrencyUnitReturningTrue(String input) {
 		assertThat(Validation.isAssignedCurrencyUnit(input)).isTrue();
 	}
 
 	@DisplayName("유저가 입력한 구입금액이 1000원 단위인지 검증한다")
 	@ParameterizedTest
 	@ValueSource(strings={"3700", "885200"})
-	void canVerifyAssignedCurrencyUnitReturningFalse(String input) {
+	void VerifyAssignedCurrencyUnitReturningFalse(String input) {
 		assertThat(Validation.isAssignedCurrencyUnit(input)).isFalse();
 	}
 
 	@DisplayName("유저가 입력한 구입금액이 1000원보다 큰지 검증한다")
 	@Test
-	void canVerifyAmountIsGreaterThan1000(){
-		assertTrue(Validation.isGreaterThan1000("1000"));
-		assertTrue(Validation.isGreaterThan1000("22000"));
-		assertFalse(Validation.isGreaterThan1000("500"));
+	void VerifyAmountIsLessThan1000(){
+		assertTrue(Validation.isLessThan1000("500"));
+		assertFalse(Validation.isLessThan1000("1000"));
+		assertFalse(Validation.isLessThan1000("22000"));
 
 	}
 
