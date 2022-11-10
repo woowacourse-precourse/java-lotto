c1a39e37419eb67b0fa060c96a02c1bc66b2a5b7
eora21 <eora21@naver.com>
2022-11-10 15:23:04 +0900

    refactor: 💡 변경 제한 금액을 1등 당첨금인 20억이 아닌 정수형의 최대값으로 수정
    20억보다 정수형의 최대값으로 제한하여 받는 게 좋다고 생각되었다.

--- src/main/java/constant/Invalid.java
+++ src/main/java/constant/Invalid.java
@@ -3,8 +3,8 @@ package constant;
 public enum Invalid {
     NULL("입력값은 null일 수 없습니다."),
     NOT_INT("입력값이 정수가 아닙니다."),
+    OVER_INT_MAX("입력 금액이 INT로 표현할 수 있는 최대값을 넘었습니다."),
     NOT_DIVISIBLE(Amount.UNIT + "로 나눌 수 없습니다."),
-    OVER_MATCH_ALL_AMOUNT("입력 금액은 1등 당첨금보다 높게 설정할 수 없습니다."),
     NOT_LOTTO_FORM("입력값을 로또 형태로 전환할 수 없습니다."),
     DUPLICATE_LOTTO("중복된 값이 있습니다."),
     DUPLICATE_BONUS("보너스 번호가 당첨 번호와 중복되었습니다."),
