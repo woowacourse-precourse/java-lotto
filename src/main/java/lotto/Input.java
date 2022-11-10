0dc3eb84369d46be7334739cb9c0c675752fcdc2
HyebinShin <hyeviin.dev@gmail.com>
2022-11-10 20:03:00 +0900

    Feat: 입력 클래스 분리 - 로또 당첨 번호
    로또 당첨 번호 입력

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -14,6 +14,7 @@ public class Input {
         return Console.readLine();
     }
 
+    //로또 구입 금액 입력
     public static String inputBudget() throws IllegalArgumentException {
         String budget = input();
 
@@ -25,6 +26,24 @@ public class Input {
     private static void validateInputBudget(String input) throws IllegalArgumentException {
         if(!Pattern.matches("^[0-9]*000$", input)) {
             throw new IllegalArgumentException(ErrMsg.VALIDATE_INPUT_BUDGET);
+        }
+    }
+
+    //로또 당첨
+    public static String inputPrize() throws IllegalArgumentException {
+        String prize = input();
+
+        validateInputPrize(prize);
+
+        return prize;
+    }
+
+    private static void validateInputPrize(String prize) throws IllegalArgumentException {
+        String regexGroup = "([1-9]|[1-3][0-9]|4[0-5])";
+        String pattern = "^("+regexGroup+",){5}"+regexGroup;
+
+        if(!Pattern.matches(pattern, prize)) {
+            throw new IllegalArgumentException(ErrMsg.VALIDATE_INPUT_PRIZE_NUM);
         }
     }
 }
