710a77e56e179b06e7ac8a9bfc8a310984b16946
wlsh44 <k87913j@gmail.com>
2022-11-11 03:57:20 +0900

    feat: 보너스 숫자 검증 기능 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -8,6 +8,13 @@ public class WinningLotto extends Lotto {
 
     public WinningLotto(List<Integer> numbers, int bonusNumber) {
         super(numbers);
+        validateBonusNumber(numbers, bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(List<Integer> numbers, int bonusNumber) {
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("중복된 당첨 번호가 존재합니다.");
+        }
     }
 }
