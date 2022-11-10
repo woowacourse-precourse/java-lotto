dcd740dbed7d027a0b25b66e25068ac0a57dce3a
Limjongho <whdgh9873@icloud.com>
2022-11-10 18:54:13 +0900

    feat: 중복되지 않는 6개의 숫자 뽑기 기능 추가

---
+++ src/main/java/domain/NumberGenerator.java
@@ -0,0 +1,21 @@
+package domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import util.Constant;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class NumberGenerator {
+
+    public List<Integer> createUniqueRandomNumbers() {
+        Set<Integer> LottoNumbers = new HashSet<>();
+        while (LottoNumbers.size() < 6) {
+            int number = Randoms.pickNumberInRange(Constant.MINIMUM_NUMBER, Constant.MAXIMUM_NUMBER);
+            LottoNumbers.add(number);
+        }
+        return new ArrayList<>(LottoNumbers);
+    }
+}
---
+++ src/main/java/util/Constant.java
@@ -0,0 +1,6 @@
+package util;
+
+public class Constant {
+    public static final int MINIMUM_NUMBER = 1;
+    public static final int MAXIMUM_NUMBER = 45;
+}
