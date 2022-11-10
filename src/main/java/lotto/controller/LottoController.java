aa3bed3c320db841cb2cd31bbd8e1c2c5628f302
Reznov <reznov03048@gmail.com>
2022-11-10 22:42:58 +0900

    feat: 당첨 번호와 보너스 번호를 가져오는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,13 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
 import lotto.domain.LottoTicket;
 import lotto.domain.PurchaseAmount;
+import lotto.domain.WinningBonusNumber;
 import lotto.view.Input;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoController {
 
@@ -12,7 +17,7 @@ public class LottoController {
         // 로또 n장 생성 (1장 당 6자리 숫자 랜덤 생성) -> Lotto.java
         LottoTicket lottoTicket = getTickets(purchaseAmount);
         // 당첨 번호 입력 받음
-
+        WinningBonusNumber winningBonusNumber = inputWinningBonusNumber();
         // 결과 생성
 
         // 결과 출력
@@ -33,4 +38,28 @@ public class LottoController {
 
         return lottoTicket;
     }
+
+    private WinningBonusNumber inputWinningBonusNumber() {
+        String inputWinningNumbers = Input.getInputWinningNumber();
+        List<Integer> winningNumbers = makeStringToList(inputWinningNumbers);
+
+        int inputBonusNumber = Input.getInputBonusNumber();
+
+        WinningBonusNumber winningBonusNumber = new WinningBonusNumber(winningNumbers, inputBonusNumber);
+
+        return winningBonusNumber;
+    }
+
+    private List<Integer> makeStringToList(String inputWinningNumbers) {
+        List<Integer> numbers = new ArrayList<>();
+        String[] inputNumbers = inputWinningNumbers.split(",");
+
+        for(int index=0; index<inputNumbers.length; index++){
+            int number = Integer.parseInt(inputNumbers[index]);
+            numbers.add(number);
+        }
+
+        return numbers;
+    }
+
 }
