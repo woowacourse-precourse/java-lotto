9fa5e249e5f30f0ff414f46ac9693e203b1212e1
seoyoung <sy9723@korea.ac.kr>
2022-11-10 13:16:19 +0900

    feat: 예외 처리 추가 ([당첨 번호] 입력된 번호에 중복이 있는 경우)

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,10 +11,23 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateLengthOfNumbers(numbers);
+        validateDuplicationOfNumbers(numbers);
+    }
+
+    private void validateLengthOfNumbers(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private  void validateDuplicationOfNumbers(List<Integer> numbers) {
+        int[] checkDuplication = new int[46];
+        for(Integer number : numbers){
+            if (checkDuplication[number] == 1) {
+                throw new IllegalArgumentException();
+            }
+            checkDuplication[number] = 1;
+        }
+    }
 }
