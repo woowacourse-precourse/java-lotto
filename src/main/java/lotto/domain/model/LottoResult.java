e08f484242ef0be0122650776db617cf7ec8b1e4
sangwon <swchoi1997@naver.com>
2022-11-10 22:04:37 +0900

    feat: 결과를 계산할 객체 생성

---
+++ src/main/java/lotto/domain/model/LottoResult.java
@@ -0,0 +1,11 @@
+package lotto.domain.model;
+
+import java.util.Map;
+
+public class LottoResult {
+    private final Map<ResultCase, Integer> lottoResult;
+
+    public LottoResult() {
+        this.lottoResult = null;
+    }
+}
