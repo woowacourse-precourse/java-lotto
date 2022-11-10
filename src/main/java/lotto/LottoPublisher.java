aa0ebfb26e1216d4fadf730674c8893b5bf76925
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-10 15:59:31 +0900

    feat(LottoPublisher): build skeleton code
    Lotto publisher publishes list of lottos.

---
+++ src/main/java/lotto/LottoPublisher.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoPublisher {
+
+    public List<Lotto> publishLottos(int payment) {
+        // 구입 금액에 따른 로또 개수 계산
+        // 개수 만큼 로또 발행
+        return null;
+    }
+
+    private int numberOfLottos(int payment) {
+        return 0;
+    }
+
+    private Lotto publishLotto() {
+        // 서로 다른 6개의 숫자 리스트 생성
+        // Lotto 생성
+        return null;
+    }
+
+    private List<Integer> publishLottoNumbers() {
+        // 서로 다른 6개의 숫자 set 생성
+        return null;
+    }
+}
