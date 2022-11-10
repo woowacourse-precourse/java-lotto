e01242df128a29f140bb0e294dbdf5d232de4583
JHS <wnghdtjr129@naver.com>
2022-11-10 22:33:37 +0900

    refactor(LottoWinningNumber): 검증 로직 메서드 분리 및 에러 메세지 상수화

--- src/main/java/lotto/domain/LottoWinningNumber.java
+++ src/main/java/lotto/domain/LottoWinningNumber.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import java.util.List;
 
 public class LottoWinningNumber extends Lotto {
+    private static final String EXCEPTION_MESSAGE = "1~45 사이 숫자를 입력해 주시기 바랍니다.";
     private final int bonusNumber;
 
     public LottoWinningNumber(List<Integer> winningNumber, String bonus) {
@@ -19,18 +20,22 @@ public class LottoWinningNumber extends Lotto {
         int bonusNumber;
         try {
             bonusNumber = Integer.parseInt(bonus);
-            if (bonusNumber < 1 || bonusNumber > 45) {
-                throw new IllegalArgumentException("1~45 사이 숫자를 입력해 주시기 바랍니다.");
-            }
+            validateRange(bonusNumber);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("1~45 사이 숫자를 입력해 주시기 바랍니다.");
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
         }
         return bonusNumber;
     }
 
+    private void validateRange(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
+        }
+    }
+
     private int validateDuplicate(int bonusNumber) {
         if (super.hasBonusNumber(bonusNumber)) {
-            throw new IllegalArgumentException("1~45 사이 숫자를 입력해 주시기 바랍니다.");
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
         }
         return bonusNumber;
     }
