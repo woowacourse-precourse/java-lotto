a8e907c7b3d449f6925f2923f3378a05e2da48ec
hectick <chyeon1223@cau.ac.kr>
2022-11-10 23:42:33 +0900

    feat: 구입 금액을 입력받는 메세지 내용 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,7 +3,7 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    static final String PURCHASE_AMOUNT_INPUT_MESSAGE = " ";
+    static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public String getPurchaseAmount(){
