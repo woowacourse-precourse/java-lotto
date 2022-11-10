4b4ba5bfedb7fc280f930cf40cc6d0904f2aef8d
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-10 23:19:54 +0900

    feat(Lotto): 입력되는 로또번호 예외처리
    validate(): 로또의 번호가 6개가 넘어가면 IllegalArgumentException 예외를 발생시키고 [ERROR]로 시작하는 에러메시지를 출력합니다.
    checkLottoByDuplicatedNumber(): 로또의 번호에 중복값이 존재하면 IllegalArgumentException 예외를 발생시키고 [ERROR]로 시작하는 에러메시지를 출력합니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Lotto {
@@ -7,12 +9,19 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkLottoByDuplicatedNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 개수는 6개가 넘으면 안됩니다.");
+        }
+    }
+
+    private void checkLottoByDuplicatedNumber(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 
