8e5579bc3adb88a26da407fbeba8c6a47ed3cfd7
김태용 <amico741@naver.com>
2022-11-11 01:36:01 +0900

    refactor: 불필요한 생성자 내용 제거
    테스트 코드 작성 시, 문제가 되는 요소로 예기되어 제거함

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -6,10 +6,7 @@ public class LottoService {
     public static final int STANDARD_WON = 1000;
     public static final String NOT_MOD_ZERO_MESSAGE = "[ERROR] 1000으로 나누어지는 금액이 아닙니다.";
 
-    public LottoService(Buyer buyer) {
-        int money = Integer.parseInt(buyer.inputMoney());
-        validateMoney(money);
-    }
+    public LottoService() { }
 
     public void validateMoney(int money) {
         if(Math.floorMod(Math.round(money), STANDARD_WON) != 0) {
