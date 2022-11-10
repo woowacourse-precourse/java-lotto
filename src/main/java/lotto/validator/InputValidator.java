04b8c14dbacd9691b28e8533ac6dfeda3b8799f9
java-saeng <gusdn85554@naver.com>
2022-11-10 15:47:32 +0900

    refactor : 로또 구입 금액 입력 시 나누어 떨어지지 않는 메서드 유틸리티 메서드로 변경

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -2,7 +2,7 @@ package lotto.validator;
 
 public class InputValidator {
 
-    public boolean restEqualZero(long dividend, long divisor) {
+    public static boolean restEqualZero(long dividend, long divisor) {
         return dividend % divisor == 0;
     }
 
