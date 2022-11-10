c902daf316c3db9462a537115e349d5b29197060
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-10 15:15:37 +0900

    feat(PrintMessenger) : winningResult

--- src/main/java/lotto/PrintMessenger.java
+++ src/main/java/lotto/PrintMessenger.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Map;
+
 public class PrintMessenger {
     public static void askPriceToBuy(){
         System.out.println("구입금액을 입력해 주세요.");
@@ -13,5 +15,14 @@ public class PrintMessenger {
     }
     public static void askBonusNumber(){
         System.out.println("보너스 번호를 입력해 주세요.");
+    }
+    public static void winningResult(Map<Integer, Integer> lottoResult)// list를 받을지 int를 받을지 고민;;
+    {
+        System.out.println("당첨 통계\n" + "---");
+        System.out.println("3개 일치 (5,000원) -" + lottoResult.get(0) + "개");
+        System.out.println("4개 일치 (50,000원) -"+ lottoResult.get(1) +"개");
+        System.out.println("5개 일치 (1,500,000원) -"+ lottoResult.get(2) +"0개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) -"+ lottoResult.get(3) +"0개");
+        System.out.println("6개 일치 (2,000,000,000원) -"+ lottoResult.get(4) +"0개");
     }
 }
