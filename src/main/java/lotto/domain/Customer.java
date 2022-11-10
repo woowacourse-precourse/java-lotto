db36399e3bd02e1860e33a4b9a37b72fa24137ba
ddubidubap <park62000@gmail.com>
2022-11-11 00:12:52 +0900

    feat: 구입 금액이 1000으로 나누어지지 않을 경우의 예외처리 함수

--- src/main/java/lotto/domain/Customer.java
+++ src/main/java/lotto/domain/Customer.java
@@ -2,12 +2,21 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.view.Constants.NOT_DIVISIBLE_BY_THOUSAND;
+
 public class Customer {
     private final int paidMoney;
     private final int numOfTicket;
 
     public Customer(int paidMoney) {
+        validateProperMoney(paidMoney);
         this.paidMoney = paidMoney;
         this.numOfTicket = this.paidMoney / 1000;
+    }
+
+    private void validateProperMoney(int paidMoney) {
+        if (paidMoney % 1000 != 0) {
+            throw new IllegalArgumentException(NOT_DIVISIBLE_BY_THOUSAND);
+        }
     }
 }
