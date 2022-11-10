ed4c73ba09c80d12d55bfb5b6cc1a504e0e3df40
jsyeo97 <jsyeo97@naver.com>
2022-11-10 11:37:13 +0900

    feat: 생성된 로또가 조건에 맞는지 확인 및 예외 처리 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,11 @@
 package lotto;
 
+import org.mockito.internal.matchers.Null;
+
 import java.util.List;
 
 public class Lotto {
+    private static final int MAX_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +14,19 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != MAX_SIZE) {
+            throw new IllegalArgumentException();
+        }
+        if(numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException();
+        }
+
+        if(numbers.stream().filter(n -> n < 1 || n > 45).findFirst() != null) {
             throw new IllegalArgumentException();
         }
     }
+
+
 
     // TODO: 추가 기능 구현
 }
