332aea44d8eec043082ba6d8289e72b4c8978bab
Kwak-JunYoung <klop9090@g.skku.edu>
2022-11-10 20:48:02 +0900

    feat(lotto/Application.java) get price that the user used for lottery
    Get input from the user by using readLine of the Console API given.
    - Consideration of the input not being a multiple of 1000 made.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    static int get_comsumption() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int consumption = Integer.parseInt(Console.readLine());
+        if (consumption % 1000 != 0)
+            throw new IllegalArgumentException();
+        return consumption;
+    }
+
+    static List<Lotto> get_user_tickets(int count) {
+        List<Lotto> ticket_list = new ArrayList<Lotto>();
+        return ticket_list;
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+
     }
 }
