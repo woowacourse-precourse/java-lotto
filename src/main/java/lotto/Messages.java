6baef1be290205248f68a6eb7256adc84fd5c2d2
jmj005 <jmj00005@naver.com>
2022-11-11 02:35:40 +0900

    refactor(Messages): 메세지 클래스 생성

---
+++ src/main/java/lotto/Messages.java
@@ -0,0 +1,10 @@
+package lotto;
+
+public class Messages {
+    public static final String OUTPUT_START_GAME_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String OUTPUT_BUY_MESSAGE = System.lineSeparator() + "%d개를 구매했습니다." +System.lineSeparator();
+    public static final String IMPUT_WINNING_NUMBERS_MESSAGE = System.lineSeparator() + "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER_MESSAGE =  System.lineSeparator() +"보너스 번호를 입력해 주세요.";
+    public static final String OUTPUT_WINNING_MESSAGE = System.lineSeparator() + "당첨 통계" + System.lineSeparator() + "---";
+    public static final String OUTPUT_RESULT_MESSAGE = System.lineSeparator() + "%d개 일치 (%s원) - %d개" + System.lineSeparator();
+}
