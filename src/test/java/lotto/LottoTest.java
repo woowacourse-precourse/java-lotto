0d6923e571375245e992a7a449616a6dc05bb239
wonju-dev <wonjulee.dev@gmail.com>
2022-11-10 20:06:12 +0900

    refactor(LottoTest): 상수 처리

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -9,10 +9,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
+    private static final List<Integer> rightNumbers = List.of(1, 2, 3, 4, 5, 6);
+    private static final List<Integer> sizeOverNumbers = List.of(1, 2, 3, 4, 5, 6, 7);
+    private static final List<Integer> duplicatedNumbers = List.of(1, 2, 3, 4, 5, 5);
+
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> Lotto.numberOf(List.of(1, 2, 3, 4, 5, 6, 7)))
+        assertThatThrownBy(() -> Lotto.numberOf(sizeOverNumbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -20,7 +26,7 @@ class LottoTest {
     @Test
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> Lotto.numberOf(List.of(1, 2, 3, 4, 5, 5)))
+        assertThatThrownBy(() -> Lotto.numberOf(duplicatedNumbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -28,7 +34,7 @@ class LottoTest {
     @Test
     void 로또_생성_성공() {
         // given
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> numbers = rightNumbers;
         // when
         Lotto lotto = Lotto.numberOf(numbers);
         // then
