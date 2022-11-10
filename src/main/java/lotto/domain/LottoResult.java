2540cc3a7b51a0bafe0b2eb797db358a2a806e1b
eunji53 <duqtj00@naver.com>
2022-11-10 14:00:53 +0900

    <feat>:중복, 로또 개수 잘못 입력 시 오류

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -3,8 +3,6 @@ package lotto.domain;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,12 +10,13 @@ import java.util.stream.Stream;
 public class LottoResult {
     public List<Integer> inputLottoNumber(){
         System.out.println("당첨번호를 입력해 주세요.");
-        String lottoNumber=readLine();
-        List<String> numbers = Stream.of(lottoNumber.split(",")).collect(Collectors.toList());
-        List<Integer> lotto=new ArrayList<>();
+        String lottoNumberInput=readLine();
+        List<String> numbers = Stream.of(lottoNumberInput.split(",")).collect(Collectors.toList());
+        List<Integer> lottoNumber=new ArrayList<>();
         for (String s : numbers) {
-            lotto.add(Integer.parseInt(s));
+            lottoNumber.add(Integer.parseInt(s));
         }
-        return lotto;
+        Lotto lotto = new Lotto(lottoNumber);
+        return lottoNumber;
     }
 }
