63615efb716845300dcf3a74622aa6f0d0b2fe9b
csj <koreaxman@naver.com>
2022-11-10 17:26:40 +0900

    feat: create divided into 1000won exception

--- src/main/java/lotto/domain/Price.java
+++ src/main/java/lotto/domain/Price.java
@@ -2,13 +2,18 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.List;
-
 public class Price {
     int input;
 
     public int purchaseAmount() {
         input = Integer.parseInt(Console.readLine());
+        validateDivide1000Won(input);
         return input;
+    }
+
+    private void validateDivide1000Won(int price) {
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액이 1000원으로 나눠떨어지지 않습니다.");
+        }
     }
 }
