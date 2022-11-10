3d9f17c34e198e7169fdc1ed913a59298333717c
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-10 15:23:25 +0900

    feat(Exception) : vaildMoney

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -1,5 +1,11 @@
 package lotto;
 
 public class Exception {
+    public void validMoney(int money) {
+        if(money < 1000)
+            throw new IllegalArgumentException("[ERROR] 1000원 이상의 금액을 입력하세요.");
+        if(money % 1000 != 0)
+            throw new IllegalArgumentException("[ERROR] 유효한 값을 입력하세요.");
+    }
 
 }
