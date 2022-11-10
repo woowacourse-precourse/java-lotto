49d07cd39ffa7c26e92166273f1b3e5121b83bb4
Soseung Soo <sss9073@naver.com>
2022-11-10 15:41:32 +0900

    refactor(LottoController): 금액을 토대로 구입한 로또의 갯수 계산

--- src/main/java/lotto/domain/LottoController.java
+++ src/main/java/lotto/domain/LottoController.java
@@ -6,12 +6,20 @@ import java.util.List;
 
 public class LottoController {
     private int price;
+    private int lottoNumber;
+    private static final int oneLottoPrice = 1000;
     private static final int start_Range = 1;
     private static final int end_Range = 45;
     private static final int count = 6;
-
+    private List<Integer> winningLotto;
     public List<Integer> generateLotto(){
         return Randoms.pickUniqueNumbersInRange(start_Range,end_Range, count);
     }
 
+    public int calculateLottoNumber(int price){
+        if(price % oneLottoPrice != 0){
+            throw new IllegalArgumentException("[ERROR] 가격은 천원 단위로 입력해주세요.");
+        }
+        return price / oneLottoPrice;
+    }
 }
