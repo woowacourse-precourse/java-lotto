4641ae5142bc3d7ac6fc3961f5d9562ca76662a7
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-10 17:33:48 +0900

    feat: Lotto 객체 생성 완료

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,9 +1,13 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final String SIZE_ERROR_MESSAGE = "[ERROR] 로또는 6자리의 숫자로 입력되야 합니다.";
+    private static final String DUPLICATION_ERROR_MESSAGE = "[ERROR] 로또는 중복되지 않은 숫자로 이루어져야 합니다.";
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -11,10 +15,26 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplication(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            new LottoNumber(number);
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(DUPLICATION_ERROR_MESSAGE);
+        }
+    }
 }
