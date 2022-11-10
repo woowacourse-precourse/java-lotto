8a0ba67da75525b89b202fdadb103b73674077e9
junhei Lee <jun2ee22@naver.com>
2022-11-10 17:38:22 +0900

    feat(ui): 입력 안내 문자열 선언

--- src/main/java/lotto/entity/InputGuide.java
+++ src/main/java/lotto/entity/InputGuide.java
@@ -1,4 +1,13 @@
 package lotto.entity;
 
 public enum InputGuide {
+    PURCHASING_AMOUNT("구입금액을 입력해 주세요."),
+    WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+
+    private final String message;
+
+    private InputGuide(String message) {
+        this.message = message;
+    }
 }
