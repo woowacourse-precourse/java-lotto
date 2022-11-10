33f05fe332a55790bc832cfa7e90398ad3ac79d9
Soseung Soo <sss9073@naver.com>
2022-11-11 03:21:55 +0900

    refactor: EnumMap으로 변경 및 Map 초기화 메서드 생성

--- src/main/java/lotto/domain/WinningController.java
+++ src/main/java/lotto/domain/WinningController.java
@@ -3,25 +3,35 @@ package lotto.domain;
 import lotto.Lotto;
 import lotto.WinningLotto;
 
+경import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class WinningController {
     static final int BonusNumberIndex = 7;
-    private Map<WinningStandard, Integer> lottoWinningResultMap = new HashMap<>();
+    static final int ZERO = 0;
+    private Map<WinningStandard, Integer> lottoWinningResultMap = new EnumMap<>(WinningStandard.class);
 
+    public void init(){
+        WinningStandard[] standards = WinningStandard.values();
+        for(WinningStandard standard : standards){
+            lottoWinningResultMap.put(standard, ZERO);
+        }
+    }
     public int getWinMoney(Map<WinningStandard, Integer> winningResultMap){
         int winMoney = 0;
         for(WinningStandard winningStandard : winningResultMap.keySet()){
             Integer number = winningResultMap.get(winningStandard);
-            int money = winningStandard.getWon() * number;
+            int won = Integer.parseInt(winningStandard.getWon().replaceAll(",", ""));
+            int money = won * number;
             winMoney += money;
         }
         return winMoney;
     }
 
     public Map<WinningStandard, Integer> calculateWinningLottos(List<Lotto> purchasedLottos, WinningLotto winningLotto){
+        init();
         for(Lotto lotto : purchasedLottos){
             compare(lotto, winningLotto);
         }
@@ -30,7 +40,7 @@ public class WinningController {
 
     public void compare(Lotto lotto, WinningLotto winningLotto){
         List<Integer> winningLottoNumbers = winningLotto.getNumbers();
-        final int bonusNumber = winningLottoNumbers.get(BonusNumberIndex);
+        final int bonusNumber = winningLottoNumbers.get(BonusNumberIndex-1);
         int correctNumbers = 0;
         boolean isCorrectBonusNumber = false;
         for(int number : lotto.getNumbers()){
@@ -55,13 +65,13 @@ public class WinningController {
 
     private void checkFirst(int correctNumbers) {
         if(correctNumbers == WinningStandard.FIRST.getNumber()){
-            lottoWinningResultMap.put(WinningStandard.FIRST, lottoWinningResultMap.getOrDefault(WinningStandard.FIRST,0)+ 1);
+            lottoWinningResultMap.put(WinningStandard.FIRST, lottoWinningResultMap.getOrDefault(WinningStandard.FIRST, ZERO)+ 1);
         }
     }
 
     private void checkSecond(int correctNumbers, boolean isCorrectBonusNumber){
         if(correctNumbers == WinningStandard.SECOND.getNumber() && isCorrectBonusNumber == true){
-            lottoWinningResultMap.put(WinningStandard.SECOND, lottoWinningResultMap.getOrDefault(WinningStandard.SECOND,0)+ 1);
+            lottoWinningResultMap.put(WinningStandard.SECOND, lottoWinningResultMap.getOrDefault(WinningStandard.SECOND, ZERO)+ 1);
         }
     }
 
