9ba41c8b5e0707883b840731e30bf2c0be007fe6
EUNJEONGMUN <munej26@gmail.com>
2022-11-10 11:33:08 +0900

    feat: 사용자의 구입 금액에 해당하는 만큼 로또를 발행 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,26 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static final int INITIAL_NUMBER = 0;
+    static List<Lotto> lottoTickets = new ArrayList<>();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Count count = UserInput.inputPrice();
+        createLottoTickets(count.getNumOfLotto());
+    }
+
+    private static List<Lotto> createLottoTickets(int numOfLotto) {
+        for (int i=INITIAL_NUMBER; i<numOfLotto; i++) {
+            lottoTickets.add(createLotto());
+        }
+        return lottoTickets;
+    }
+
     private static Lotto createLotto() {
         return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
     }
