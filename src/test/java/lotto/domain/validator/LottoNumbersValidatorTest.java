2079af9b273559eac05f842b33dc9e26d4cd23c2
JunH <tjdtls690@gmail.com>
2022-11-10 16:39:18 +0900

    :white_check_mark: test(Domain : LottoNumbersValidatorTest) : 예외처리 클래스의 단위 테스트 추가

--- src/test/java/lotto/domain/validator/LottoNumbersValidatorTest.java
+++ src/test/java/lotto/domain/validator/LottoNumbersValidatorTest.java
@@ -1,4 +1,4 @@
-package lotto.domain;
+package lotto.domain.validator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,19 +10,19 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class LottoTest {
+class LottoNumbersValidatorTest {
     @Test
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoNumbersValidator.validate(List.of(1, 2, 3, 4, 5, 6, 7)));
     }
-
+    
     @Test
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     void createLottoByDuplicatedNumber() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoNumbersValidator.validate(List.of(1, 2, 3, 4, 5, 5)));
     }
     
     @DisplayName("예외 처리 : 6개의 숫자 중, 1~45 범위를 벗어난 숫자가 존재하는 경우")
@@ -30,6 +30,6 @@ class LottoTest {
     @ValueSource(ints = {0, 46})
     void outOfRangeException(int lottoNumber) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, lottoNumber)));
+                .isThrownBy(() -> LottoNumbersValidator.validate(List.of(1, 2, 3, 4, 5, lottoNumber)));
     }
 }
