17cd3279c025b55ffe221ee3d9d3b0119360ec17
minju412 <ln8847@ajou.ac.kr>
2022-11-10 23:23:19 +0900

    refactor: constant system values

--- src/main/java/lotto/constant/MatchType.java
+++ src/main/java/lotto/constant/MatchType.java
@@ -3,9 +3,9 @@ package lotto.constant;
 public enum MatchType {
     THREE_MATCHES("3개 일치 (5,000원) - %d개"),
     FOUR_MATCHES("4개 일치 (50,000원) - %d개"),
-    FIVE_MATCHES_WITHOUT_BONUS_BALL("5개 일치 (1,500,000원) - %d개"),
-    FIVE_MATCHES_WITH_BONUS_BALL("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
-    SIX_MATCHES("6개 일치 (2,000,000,000원) - 0개");
+    FIVE_MATCHES_WITHOUT_BONUS("5개 일치 (1,500,000원) - %d개"),
+    FIVE_MATCHES_WITH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    SIX_MATCHES("6개 일치 (2,000,000,000원) - %d개");
 
     private final String message;
 
--- src/main/java/lotto/constant/SystemValue.java
+++ src/main/java/lotto/constant/SystemValue.java
@@ -6,4 +6,10 @@ public final class SystemValue {
     public static final int MAXIMUM_LOTTERY_NUMBER = 45;
 
     public static final int LOTTERY_NUMBERS_SIZE = 6;
+
+    public static final int THREE_MATCHES_INDEX = 0;
+    public static final int FOUR_MATCHES_INDEX = 1;
+    public static final int FIVE_MATCHES_WITHOUT_BONUS_INDEX = 2;
+    public static final int FIVE_MATCHES_WITH_BONUS_INDEX = 3;
+    public static final int SIX_MATCHES_INDEX = 4;
 }
