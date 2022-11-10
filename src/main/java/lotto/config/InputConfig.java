2711ba7737b938d489f038fb2f76803061df7e67
SEONMl <sm000117@naver.com>
2022-11-10 14:04:18 +0900

    feat: 구입 금액 입력값의 유효성 검사하는 클래스 구현

---
+++ src/main/java/lotto/config/InputConfig.java
@@ -0,0 +1,63 @@
+package lotto.config;
+
+import java.util.Set;
+
+public class InputConfig {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+
+    //
+//    private void checkCorrectInput() {
+//
+//    }
+//
+    void isBonusNumberInLotto(Set<Integer> lotto, int bonus) {
+        if(lotto.contains(bonus)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void hasCorrectSize(Set<Integer> set) {
+        if(set.size()!=6) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
+    private static void isInCorrectRange(int value) {
+        if (!(1 <= value && value <= 45)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
+    // 구입 금액 입력
+    public static void checkPurchaseInput(String input) {
+        int value = Integer.valueOf(input);
+        isMoreThanZero(value);
+        isDividedByThousand(value);
+    }
+
+    private static void isMoreThanZero(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
+    private static void isDividedByThousand(int value) {
+        if (value % 1000 != 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
+}
+
+
+/*
+[x] 1000원으로 나누어 떨어지지 않았을 때
+- [x] **0원일 경우는?** (나누어 떨어지니 허용?)
+- 당첨 번호 관련
+- [x] 6개 미만 또는 초과하는 번호를 입력했을 때
+- [x] 1 ~ 45 범위를 벗어나는 번호를 입력했을 때
+- [ ] 쉼표(,) 구분되지 않는 입력이 주어졌을 때
+- [x] 당첨 번호에 중복되는 숫자가 존재할 때  set --> set의 size가 6이 안됨
+- [x] 보너스 번호가 당첨 번호와 중복될 때 set
+ */
