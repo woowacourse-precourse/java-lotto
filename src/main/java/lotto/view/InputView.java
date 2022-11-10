077613afbd6a94c5e5fb8ab9cfbfec2f046c5887
khsrla9806 <kimhunsope@naver.com>
2022-11-11 02:09:31 +0900

    feat(InputView): 보너스 번호를 입력받는 메서드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,6 +3,7 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.exception.InputException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -31,6 +32,11 @@ public class InputView {
         InputException.validateWinningNumber(winningNumber);
         Collections.sort(winningNumber);
         return winningNumber;
+    }
+
+    public static int enterBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Integer.valueOf(Console.readLine());
     }
 
 }
