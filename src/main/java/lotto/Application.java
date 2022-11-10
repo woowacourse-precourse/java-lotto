1f5ea8208d7bc97e03dba96b011bda5d5729efc1
alstjr706 <alstjr706@naver.com>
2022-11-11 01:43:50 +0900

    feat : 랜덤 로또 숫자 생성기 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,12 @@
 package lotto;
 
+import lotto.domain.RandomLotto;
 import lotto.input.LottoTickets;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        LottoTickets lottoTickets = new LottoTickets();
-        int paid = lottoTickets.getPaid();
-        System.out.println(paid);
+
     }
 }
---
+++ src/main/java/lotto/domain/RandomLotto.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomLotto {
+
+    public List<List<Integer>> randomGenerator(int tickets) {
+        List<List<Integer>> randomLotto = new ArrayList<>();
+        List<Integer> oneTicket = new ArrayList<>();
+
+        for (int j = 0; j < tickets; j++) {
+            for (int i = 0; i < 1; i++) {
+                oneTicket = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            }
+            randomLotto.add(oneTicket);
+        }
+        return randomLotto;
+    }
+}
