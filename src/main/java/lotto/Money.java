c79fdd7606de0da469a1e16c7e3b14e45dd64e9b
KyungKyu Park <rudrb0505@gmail.com>
2022-11-10 17:10:48 +0900

    feat(Money): 사용자가 구입금액을 입력하는 기능 구현

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Money {
+	public Money() {
+	}
+
+	public static void inputMoney() {
+		System.out.println("구입금액을 입력해 주세요.");
+		String userInput = Console.readLine();
+	}
+}
