6db662f08399ae9f475b925150556dacd8e1c4b7
Soseung Soo <sss9073@naver.com>
2022-11-10 14:53:50 +0900

    refactor(LottoController): 이름 변경 range -> count

--- src/main/java/lotto/domain/LottoController.java
+++ src/main/java/lotto/domain/LottoController.java
@@ -2,17 +2,16 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LottoController {
     private int price;
     private static final int start_Range = 1;
     private static final int end_Range = 45;
-    private static final int range = 6;
+    private static final int count = 6;
 
     public List<Integer> generateLotto(){
-        return Randoms.pickUniqueNumbersInRange(start_Range,end_Range,range);
+        return Randoms.pickUniqueNumbersInRange(start_Range,end_Range, count);
     }
 
 }
