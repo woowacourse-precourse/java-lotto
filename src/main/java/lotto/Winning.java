e557bebbf0fb92711851bb37fe7735c9eb369487
swandevson <shai4956@gmail.com>
2022-11-11 04:10:01 +0900

    Style(Winning): 열거형 이름 오타 수정
    - THRID -> THIRD

--- src/main/java/lotto/Winning.java
+++ src/main/java/lotto/Winning.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 public enum Winning {
     FIRST(6, false, 2_000_000_000),
     SECOND(5, true, 30_000_000),
-    THRID(5, false, 1_500_000),
+    THIRD(5, false, 1_500_000),
     FOURTH(4, false, 50_000),
     FIFTH(3, false, 5_000);
 
