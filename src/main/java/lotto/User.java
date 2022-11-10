ec3b0025090f34c495febfc102dcb3e8de016297
whipbaek <jibaek0513@gmail.com>
2022-11-10 17:16:10 +0900

    feat(User): 사용자 금액 잘못된 값 입력 검사 - private void validateInputNumber(int inputMoney) 추가 - 1000원 단위로 입력하는지 검사

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -11,4 +11,11 @@ public class User {
         validateInputNumber(inputMoney);
         userMoney = inputMoney;
     }
+
+    private void validateInputNumber(int inputMoney) {
+        if(inputMoney%1000 != 0){
+            System.out.println("[ERROR] 금액은 1000원 단위로 나뉘어 떨어져야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
 }
