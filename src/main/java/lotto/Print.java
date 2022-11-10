39d2b59eb42608160dfc93a3634ef24c4ef31dde
HyebinShin <hyeviin.dev@gmail.com>
2022-11-10 09:27:22 +0900

    Feat: 출력문 클래스 분리
    구입금액을 입력해 주세요.
    개를 구매했습니다.
    당첨 번호를 입력해 주세요.
    보너스 번호를 입력해 주세요.
    당첨 통계
    총 수익률은
    %입니다.

---
+++ src/main/java/lotto/Print.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class Print {
+    public static final String ASK_BUY = "구입금액을 입력해 주세요.";
+    public static final String ANSWER_BUY = "개를 구매했습니다.";
+    public static final String ASK_PRIZE = "당첨 번호를 입력해 주세요.";
+    public static final String ASK_BONUS = "보너스 번호를 입력해 주세요.";
+    public static final String ANALYSE_PRIZE = "당첨 통계";
+    public static final String RATE_BEFORE = "총 수익률은";
+    public static final String RATE_AFTER = "%입니다.";
+}
