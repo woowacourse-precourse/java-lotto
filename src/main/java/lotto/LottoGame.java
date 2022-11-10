adb6b6097c370142f32787fbfda8516d778f0fb0
wugawuga <dnr3655@naver.com>
2022-11-10 23:53:18 +0900

    feat: 발행한 수량만큼 로또 생성

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,9 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGame {
     public void start() {
         Print.money();
         int lottoTickets = Input.purchaseAmount();
         Print.purchaseLotto(lottoTickets);
+
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoTickets; i++) {
+            List<Integer> random = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(random));
+        }
     }
 }
