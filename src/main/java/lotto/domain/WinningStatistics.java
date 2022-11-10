8391bbdfeb853b15b9fc9b66d835ef907a7c50ed
kokodak <kokodakadokok@gmail.com>
2022-11-10 21:06:52 +0900

    feat(winningStatistics): 로또 번호와 당첨 번호를 비교하여 당첨 내역을 계산하는 기능 구현

--- src/main/java/lotto/domain/WinningStatistics.java
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -1,2 +1,20 @@
-package lotto.domain;public class WinningStatistics {
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningStatistics {
+
+    public static int compareNumbersWithWinningNumbers(Lotto lotto, WinningLotto winningLotto) {
+        List<Integer> numbers = lotto.getNumbers();
+        List<Integer> winningNumbers = winningLotto.getWinningNumbers();
+        return (int) numbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+    }
+
+    public static boolean compareNumbersWithBonusNumber(Lotto lotto, WinningLotto winningLotto) {
+        List<Integer> numbers = lotto.getNumbers();
+        int bonusNumber = winningLotto.getBonusNumber();
+        return numbers.contains(bonusNumber);
+    }
 }
