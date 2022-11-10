2fd093aff2550004c297594484165b372ce0c84f
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-10 20:24:29 +0900

    feat: add return the number of user tickets

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -5,23 +5,28 @@ import java.util.List;
 
 public class User {
 
-    Lotto[] tickets;
+    private final Lotto[] tickets;
 
     User(int payment){
         validatePayment(payment);
 
         int ticketNum = payment/1000;
-        tickets = new Lotto[ticketNum];
-
+        Lotto[] tickets = new Lotto[ticketNum];
         List<Integer> random6Digit;
         for(int i = 0; i < ticketNum; i++){
             random6Digit = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             tickets[i] = new Lotto(random6Digit);
         }
+
+        this.tickets = tickets;
     }
 
-    public static void validatePayment(int payment){
+    private void validatePayment(int payment){
         if(payment <= 0 || payment%1000 != 0)
             throw new IllegalArgumentException();
+    }
+
+    public int getTicketNum(){
+        return this.tickets.length;
     }
 }
