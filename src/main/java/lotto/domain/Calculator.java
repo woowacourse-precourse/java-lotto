af68200b91c3cdb44cfc75c85c99f1ff326d88da
khsrla9806 <kimhunsope@naver.com>
2022-11-11 19:19:28 +0900

    feat(Calculator): 총 수입금(당첨금액 총합)을 계산하는 메서드 구현 prizeMoney: 각 등수에 해당하는 당첨금액을 모아둔 리스트 calculateTotalEarnings: 총 수익금을 계산하는 메서드

---
+++ src/main/java/lotto/domain/Calculator.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Calculator {
+    private int totalEarnings;
+    private double earningsRate;
+    private List<Integer> prizeMoney = List.of(0, 2000000000, 30000000, 1500000, 50000, 5000);
+
+    private void calculateTotalEarnings(List<Integer> result) {
+        for (int i = 1; i < result.size(); i++) {
+            totalEarnings += (prizeMoney.get(i) * result.get(i));
+        }
+    }
+    public int getTotalEarnings() {
+        return totalEarnings;
+    }
+}
