e5301b82501cfbc32e3e8587ece6da7382371833
sysliar <sysliar@gmail.com>
2022-11-11 20:51:01 +0900

    feat(Drawer): add lotto drawer

--- src/main/java/lotto/Drawer.java
+++ src/main/java/lotto/Drawer.java
@@ -1,13 +1,22 @@
 package lotto;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import camp.nextstep.edu.missionutils.Console;
 
 public class Drawer {
-    public List<Integer> draw() {
-        return new ArrayList<Integer>();
+    private static final String DRAW_LOTTO_QUESTION = "당첨 번호를 입력해 주세요.";
+    private static final String DRAW_BONUS_QUESTION = "보너스 번호를 입력해 주세요.";
+
+    public Lotto drawLotto() {
+        List<Integer> numbers = new ArrayList<>();
+        System.out.println(DRAW_LOTTO_QUESTION);
+        String answer = Console.readLine();
+        for (String number : answer.split(",")) {
+            numbers.add(Integer.valueOf(number));
+        }
+        return new Lotto(numbers);
     }
 
     public void compare(List<Lotto> lottoTickets, List<Integer> lotto) {
