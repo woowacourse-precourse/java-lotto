5711a3f50c0fe108ad9d4a35c2ff914a44ca0af5
lee <whddlsk123@naver.com>
2022-11-11 15:24:06 +0900

    reafactor(UserLotto) : 메서드들의 접근 권한을 private -> public으로 수정

--- src/main/java/lotto/domain/UserLotto.java
+++ src/main/java/lotto/domain/UserLotto.java
@@ -8,21 +8,21 @@ import java.util.Map;
 
 public class UserLotto {
     private final Map<Integer,List<Integer>> userLotto = new HashMap<>();
-    
+
     //예외사항(입력단위가 1000원이 아니면)
-    private int getLottoAmount(int money) {
+    public int getLottoAmount(int money) {
         int amount = money/1000;
 
         return amount;
     }
     
-    private List<Integer> createLotto() {
+    public List<Integer> createLotto() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
 
         return lottoNumbers;
     }
     
-    private void createUserLotto(int amount) {
+    public void createUserLotto(int amount) {
         for (int key = 0; key < amount; key++) {
             userLotto.put(key, createLotto());
         }
