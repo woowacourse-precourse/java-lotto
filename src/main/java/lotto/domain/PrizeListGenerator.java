021dd12ab98bdfddfe5f41b6025a1d94b39bc808
YSEO <1park4170@gmail.com>
2022-11-11 15:16:37 +0900

    refactor<compute lottery result, convert it into Map> : winningNumbers become instance variable, all instance variable into private
    it is better to bonus and winningNumbers as instance variable for safety, also save memory, make variables into private and use getter only if needed
    
    - before
    winningNumbers was local variable
    - after
    winningNumbers into instance variable,
    all variables are private

--- src/main/java/lotto/domain/PrizeListGenerator.java
+++ src/main/java/lotto/domain/PrizeListGenerator.java
@@ -9,20 +9,16 @@ import java.util.List;
 import java.util.Map;
 
 public class PrizeListGenerator {
-    public Map<Enum, Integer> winnerResult = new HashMap<>();
-    private IssuingMachine issuingMachine = new IssuingMachine();
+    private Map<Enum, Integer> winnerResult = new HashMap<>();
+    private Map<Integer, Integer> winningNumbers;
     private int bonus;
 
-    private WinnerNumberGetter winnerNumberGetter = new WinnerNumberGetter();
-
-    public Map<Integer, Integer> getWinningNumbers() {
-        Map<Integer,Integer> winningNumbers = winnerNumberGetter.getLotteryWinningNumbersInput();
-        bonus = winnerNumberGetter.getBonusInput();
-
-        return winningNumbers;
+    public void getWinningNumbers() {
+        winningNumbers = WinnerNumberGetter.getLotteryWinningNumbersInput();
+        bonus = WinnerNumberGetter.getBonusInput();
     }
 
-    public void iterateLotteriesForStatistic(Map<Integer, Integer> winningNumbers, List<Lotto> lotteries) {
+    public void iterateLotteriesForStatistic(List<Lotto> lotteries) {
         int score = 0;
 
         for (Lotto lottery : lotteries) { // 당첨 계산하는 제일 핵심로직  시간복잡도 개선방법 생각해보기
@@ -39,7 +35,7 @@ public class PrizeListGenerator {
             }
 
             Enum<PrizeStandards> prizeStandardsEnum = getCorrespondingPrizeStandard(score);
-            winnerResult.computeIfPresent(prizeStandardsEnum, (k,v) -> v+1);
+            winnerResult.computeIfPresent(prizeStandardsEnum, (k, v) -> v + 1);
             winnerResult.computeIfAbsent(prizeStandardsEnum, v -> 1);
 
             score = 0;
