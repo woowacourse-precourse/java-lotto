5cdd857f95745033c5c61ccba2ef9141eb56f45a
saewoo1 <wooozooo1123@gmail.com>
2022-11-11 15:29:30 +0900

    feat(PrintOut): 당첨번호, 보너스번호 입력 메세지 작성

--- src/main/java/lotto/PrintOut.java
+++ src/main/java/lotto/PrintOut.java
@@ -11,8 +11,11 @@ public class PrintOut {
     System.out.println(count + "개를 구매했습니다.");
   }
 
-  public void lottoPrint(List<Integer> numbers){
-//    Lotto lotto = new Lotto();
-//    System.out.println(lotto.generateLottoNumber());
+  public static void requestWinningNumberMessage() {
+    System.out.println("당첨 번호를 입력해 주세요.");
+  }
+
+  public static void requesBonusNumberMessage() {
+    System.out.println("보너스 번호를 입력해 주세요.");
   }
 }
