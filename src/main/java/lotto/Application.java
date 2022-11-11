a84c8e46a7f1e12302c50e28a2fcccc3e9a80886
Kwon-sang <kwonsg514@gmail.com>
2022-11-11 22:30:21 +0900

    [구입 UI를 위한 controller 구성]

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,22 @@
 package lotto;
 
-import lotto.domain.Winning;
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.*;
 
 public class Application {
     public static void main(String[] args) {
-        Winning winning = new Winning();
+        purchaseInputController();
+    }
 
+    static void purchaseInputController () {
+        System.out.println("구입 금액을 입력해 주세요.");
+        String purchaseAmount = Console.readLine();
+
+        Purchase purchase = new Purchase(purchaseAmount);
+
+        System.out.printf("\n%d개를 구매했습니다.\n", purchase.getPurchaseQuantity());
+        for (Lotto lotto : purchase.getLottos()) {
+            lotto.printLotto();
+        }
     }
 }
