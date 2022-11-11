ca94d4ae14bc6b2e8e41aee34e087b8c420a1954
CMHyuk <alsgur9710@naver.com>
2022-11-11 20:09:14 +0900

    feat(LottoScore): 로또 번호 맞은 개수 저장

--- src/main/java/lotto/domain/LottoScore.java
+++ src/main/java/lotto/domain/LottoScore.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import lotto.constant.Score;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static lotto.constant.Score.*;
@@ -18,5 +19,48 @@ public class LottoScore {
         scoreStore.put(FIVE_BONUS, 0);
         scoreStore.put(SIX, 0);
         return scoreStore;
+    }
+
+    public static void update(List<Integer> lottoNumbers, int score, int bonusNumber) {
+        isThree(score);
+        isFour(score);
+        isFive(lottoNumbers, score, bonusNumber);
+        isFiveBonus(lottoNumbers, score, bonusNumber);
+        isSix(score);
+    }
+
+    private static void isSix(int score) {
+        if (score == 6) {
+            Integer count = scoreStore.get(SIX);
+            scoreStore.put(SIX, ++count);
+        }
+    }
+
+    private static void isFiveBonus(List<Integer> lottoNumbers, int score, int bonusNumber) {
+        if (score == 5 && lottoNumbers.contains(bonusNumber)) {
+            Integer count = scoreStore.get(FIVE_BONUS);
+            scoreStore.put(FIVE_BONUS, ++count);
+        }
+    }
+
+    private static void isFive(List<Integer> lottoNumbers, int score, int bonusNumber) {
+        if (score == 5 && !lottoNumbers.contains(bonusNumber)) {
+            Integer count = scoreStore.get(FIVE);
+            scoreStore.put(FIVE, ++count);
+        }
+    }
+
+    private static void isFour(int score) {
+        if (score == 4) {
+            Integer count = scoreStore.get(FOUR);
+            scoreStore.put(FOUR, ++count);
+        }
+    }
+
+    private static void isThree(int score) {
+        if (score == 3) {
+            Integer count = scoreStore.get(THREE);
+            scoreStore.put(THREE, ++count);
+        }
     }
 }
