bf93b22656af73e84e42c6822c1149ad6f391376
HyebinShin <hyeviin.dev@gmail.com>
2022-11-11 17:41:54 +0900

    Test: 로또 구입 금액 입력 유효성 검사
    Change: 공통 부분 메소드 getCountErr()

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,14 +14,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class InputTest {
     public static final String ERROR_MSG = "[ERROR]";
 
-    private Input input = new Input();
+    private final Input input = new Input();
+    private int countErr;
     private final List<String> prize = List.of(
-            "1, 2, 3, 4, 5, 47",
-            "가나다",
-            "abc",
-            "123abc",
-            "1, 2, 3, 4, 5"
+            "1, 2, 3, 4, 5, 47", "가나다", "abc", "123abc", "1, 2, 3, 4, 5"
     );
+    private final List<String> budget = List.of("999","abc","가","10990","1~947");
 
     private Method invokeMethod(String declaredMethod) throws NoSuchMethodException {
         Method method = input.getClass().getDeclaredMethod(declaredMethod, String.class);
@@ -29,24 +28,46 @@ public class InputTest {
         return method;
     }
 
-    @Test
-    @DisplayName("로또 당첨번호 입력 유효성 검사")
-    void validateInputPrizeNum() throws Exception {
-        int countErr = 0;
-        Method method = invokeMethod("validateInputPrize");
-
-        for(String inputPrize : prize) {
-            System.out.println("inputPrize: "+inputPrize);
+    private void getCountErr(Method method, List<String> whichCase) {
+        for(String inputCase : whichCase) {
+            System.out.println("input: "+inputCase);
             try {
-                method.invoke(input, inputPrize);
+                method.invoke(input, inputCase);
             } catch (InvocationTargetException e) {
                 if(e.getTargetException().getMessage().contains(ERROR_MSG)) {
                     countErr++;
                 }
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
             }
         }
+    }
 
+    @BeforeEach
+    void setCountErr() {
+        countErr = 0;
+    }
+
+    @Test
+    @DisplayName("로또 당첨번호 입력 유효성 검사")
+    void validateInputPrizeNum() throws Exception {
+        Method method = invokeMethod("validateInputPrize");
+
+        getCountErr(method, prize);
+
+        System.out.println("countErr: "+countErr);
         assertEquals(countErr, prize.size());
+    }
+
+    @Test
+    @DisplayName("로또 구입 금액 입력 유효성 검사")
+    void validateInputBudget() throws Exception {
+        Method method = invokeMethod("validateInputBudget");
+
+        getCountErr(method, budget);
+
+        System.out.println("countErr: "+countErr);
+        assertEquals(countErr, budget.size());
     }
 
 }
