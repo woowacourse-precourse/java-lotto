0bdccd6a716b415123bb7e53d824fe269d0d68a3
khj <bbb6254177@gmail.com>
2022-11-11 18:29:41 +0900

    feat(lotto): add LottoGenerator class

---
+++ src/main/java/lotto/LottoGenerator.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGenerator {
+    public List<Lotto> generateLotto() {
+        List<Lotto> lotto = new ArrayList<>();
+        return lotto;
+    }
+}
