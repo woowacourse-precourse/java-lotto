301faf2f6cc24099b93e4f6d1b94faa59cc2d92f
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-12 01:40:46 +0900

    feat(PurchaseAmount): 입력 값이 0이 아닌지 확인하는 기능 추가
    입력된 값이 0이 아닌지 확인한다.
    만약, 0이라면 0을 초과한 금액을 투입해야 한다는 메시지와 함께 예외가 발생한다.
    docs: 0 초과인지 확인 & 0을 입력한 경우 예외 처리 완료 체크

--- src/main/java/lotto/PurchaseAmount.java
+++ src/main/java/lotto/PurchaseAmount.java
@@ -1,6 +1,7 @@
 package lotto;
 
 public class PurchaseAmount {
+    private static final int ZERO = 0;
     private final int amount;
 
     public PurchaseAmount(String amount) {
@@ -15,6 +16,8 @@ public class PurchaseAmount {
     private void validate(String amount) {
         if (isStringEmpty(amount) || !isStringDigit(amount)) {
             throw new IllegalArgumentException(ExceptionMessage.PURCHASE_AMOUNT_IS_NOT_NUMBER.getMessage());
+        } else if (!isExceedZero(amount)) {
+            throw new IllegalArgumentException(ExceptionMessage.PURCHASE_AMOUNT_IS_NOT_EXCEED_ZERO.getMessage());
         }
     }
 
@@ -24,6 +27,10 @@ public class PurchaseAmount {
 
     private boolean isStringDigit(String amount) {
         return amount.chars().allMatch(Character::isDigit);
+    }
+
+    private boolean isExceedZero(String amount) {
+        return convertStringToInt(amount) > ZERO;
     }
 
     private int convertStringToInt(String amount) {
