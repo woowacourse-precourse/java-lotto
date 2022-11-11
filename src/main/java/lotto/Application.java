9de0fee1dd5dd3c0411b3fbf4c30903619f88f4b
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-11 22:44:34 +0900

    feat : "추가 예외 상황 시 종료"
    메인 함수에 while 문을 추가하여 예외 사항이 나오면 거짓이 되게 해서 프로그램을 종료하도록 하였습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,8 +2,14 @@ package lotto;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+        I_O_System IO = new I_O_System();
         // TODO: 프로그램 구현
-
+        while (IO.Check_True())
+        {
+            IO.Enter_Price();
+            System.out.println("Hello");
+        }
     }
+
 }
--- src/main/java/lotto/I_O_System.java
+++ src/main/java/lotto/I_O_System.java
@@ -2,28 +2,45 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.IllformedLocaleException;
 import java.util.List;
 
 public class I_O_System {
     static final int lottey_price = 1000;
     public static final String BAGIC_ERROR_MESSAGE = "[ERROR]";
+    private String Enter_Price;
 
     public void Enter_Price()
     {
-        String Enter_Price = Console.readLine();
-        Differnet_Error(Enter_Price);
+        Enter_Price = Console.readLine();
+       if(Differnet_Error()){
+           int Number= Integer.parseInt(Enter_Price);
+       }
+
     }
 
-    private void Differnet_Error(String Number) {
+    private boolean Differnet_Error()
+    {
         try {
-            if(!(Number != null && Number.matches("[0-9.]+"))) {
+            if(!(Enter_Price != null && Enter_Price.matches("[0-9.]+"))) {
                 throw new IllegalArgumentException(BAGIC_ERROR_MESSAGE + " 오류 발생");
             }
         } catch (IllegalArgumentException ex) {
             System.out.println(ex.getMessage());
+            return false;
 
         }
-
+        return true;
     }
+
+    public boolean Check_True()
+    {
+        if(!Differnet_Error()) {
+            return false;
+        }
+
+        return true;
+    }
+
 
 }
