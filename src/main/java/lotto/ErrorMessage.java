f35bb831ca0758aa85d8251fd089fcb29a81c4e9
rt3310 <btac3310@gmail.com>
2022-11-12 03:19:29 +0900

    refactor: 같은 성격의 메시지 통합

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -1,9 +1,8 @@
 package lotto;
 
 public enum ErrorMessage {
-    INVALID_MONEY_TYPE("숫자만 입력 가능합니다"),
+    INVALID_NUMBER_TYPE("숫자만 입력 가능합니다"),
     INVALID_UNIT_OF_MONEY("1000원 단위로만 입력 가능합니다"),
-    INVALID_WINNING_NUMBER_TYPE("로또 번호는 숫자만 입력 가능합니다"),
     INVALID_LOTTO_NUMBER_COUNT("로또 번호는 6개까지 입력 가능합니다"),
     INVALID_LOTTO_NUMBER_RANGE("로또 번호는 1~45사이의 숫자만 입력 가능합니다"),
     DUPLICATION_BONUS_NUMBER("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
