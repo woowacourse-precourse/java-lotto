81a7038fe3c749e41418256c668220c4b345c452
yg0585 <yg0585@naver.com>
2022-11-11 17:33:10 +0900

    feat: 여러 로또 확인하는 기능 추가

--- src/main/java/lotto/domain/WinChecker.java
+++ src/main/java/lotto/domain/WinChecker.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class WinChecker {
     private final List<Integer> winNumber;
@@ -12,10 +14,20 @@ public class WinChecker {
         this.bonusNumber = bonusNumber;
     }
 
-    public Rank checkLotto(Lotto lottoNumber) {
+    private Rank getRank(Lotto lottoNumber) {
         int count = lottoNumber.compare(winNumber);
 
         return Rank.of(count, isBonus(lottoNumber));
+    }
+
+    public Map<Rank, Integer> checkLotto(List<Lotto> lottos){
+        Map<Rank, Integer> result = new HashMap<>();
+        for (Lotto lotto: lottos){
+            Rank rank = getRank(lotto);
+            result.put(rank, result.getOrDefault(rank, 0) + 1);
+        }
+
+        return result;
     }
 
     private boolean isBonus(Lotto lottoNumber) {
