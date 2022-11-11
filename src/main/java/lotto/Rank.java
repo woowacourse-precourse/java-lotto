22746aada0d82091ed2aa6899e5e3457e97d2011
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-12 00:33:31 +0900

    feat : 순위 정보를 담을 enum클래스 생성
    Rank 클래스 생성

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,20 @@
+package lotto;
+
+public enum Rank {
+    FIRST("6개 일치 "),
+    SECOND("5개 일치, 보너스 볼 일치 "),
+    THIRD("5개 일치 "),
+    FOURTH("4개 일치 "),
+    FIFTH("3개 일치 ");
+
+    private final String result;
+
+    Rank(String result) {
+        this.result = result;
+
+    }
+
+    public String getValue() {
+        return result;
+    }
+}
