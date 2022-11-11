583be3b7d1227b78056544f8e18587377396ab59
JunH <tjdtls690@gmail.com>
2022-11-11 17:22:34 +0900

    :sparkles: feat(View : OutputView) : 구입 금액 입력 안내 메시지 출력 기능 구현
    Signed-off-by: JunH <tjdtls690@gmail.com>

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,9 +3,11 @@ package lotto.controller;
 import lotto.domain.Payment;
 import lotto.domain.WinningLottoNumbers;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
     public void run() {
+        OutputView.printPaymentInputMessage();
         Payment payment = InputView.inputPayment();
         WinningLottoNumbers winningLottoNumbers = InputView.inputWinningLottoNumbers();
         System.out.println(winningLottoNumbers);
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class OutputView {
+    private static final String PAYMENT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    
+    public static void printPaymentInputMessage() {
+        System.out.println(PAYMENT_INPUT_MESSAGE);
+    }
+}
