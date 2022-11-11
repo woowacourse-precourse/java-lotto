d252678862e5b1b2629e9052a8de29368d4ce704
K-Diger <kitten.diger@gmail.com>
2022-11-11 19:00:38 +0900

    fix : ValueValidator 클래스 내의 예외처리 방식 변경 - 기존 예외가 발생한 상황에 대해서 throw 만 해주던 방식을 - System.out.println 메서드로 예외 메세지를 출력하여 예외 로그를 남길 수 있도록 조정

--- src/main/java/lotto/validator/ValueValidator.java
+++ src/main/java/lotto/validator/ValueValidator.java
@@ -8,25 +8,13 @@ public class ValueValidator {
     public static void validateLottoPurchaseMoney(String lottoPurchaseMoney) {
         try {
             Integer.parseInt(lottoPurchaseMoney);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("[ERROR] 구매 금액은 최대 2,147,483,647 까지 입니다.");
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 구매 금액은 정수로만 입력이 가능하며, 범위는 최대 2,147,483,647 까지 입니다.");
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 정수로만 입력이 가능하며, 범위는 최대 2,147,483,647 까지 입니다.");
         }
         if (Integer.parseInt(lottoPurchaseMoney) % 1000 != 0 || Integer.parseInt(lottoPurchaseMoney) == 0) {
+            System.out.println("[ERROR] 구매 금액은 1000으로 나누어 떨어지는 단위만 가능합니다.");
             throw new IllegalArgumentException("[ERROR] 구매 금액은 1000으로 나누어 떨어지는 단위만 가능합니다.");
-        }
-    }
-
-    public static void validateInputLottoNumber(List<Integer> numbers) {
-        validateLottoNumbersLength(numbers);
-        validateLottoNumbersOverlap(numbers);
-        for (Integer number : numbers) {
-            validateLottoNumbersRange(number);
-        }
-    }
-
-    public static void validateLottoNumbersRange(int number) {
-        if (number < 1 || number > 45) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
@@ -35,20 +23,31 @@ public class ValueValidator {
         for (int numberIndex = 0; numberIndex < numbers.size() - 1; numberIndex++) {
             stack.push(numbers.get(numberIndex));
             if (stack.contains(numbers.get(numberIndex + 1))) {
+                System.out.println("[ERROR] 로또 번호는 중복될 수 없습니다.");
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
             }
         }
     }
 
     public static void validateLottoNumbersLength(List<Integer> numbers) {
-        if (numbers.size() > 6) {
-            throw new IllegalArgumentException("[ERROR] 보너스 숫자를 포함하지 않은 로또 번호 갯수는 최대 6개입니다.");
+        if (numbers.size() != 6) {
+            System.out.println("[ERROR] 보너스 숫자를 포함하지 않은 로또 번호 갯수는 6개입니다.");
+            throw new IllegalArgumentException("[ERROR] 보너스 숫자를 포함하지 않은 로또 번호 갯수는 6개입니다.");
         }
     }
 
-    public static void validateLottoNumbersLengthContainBonusNumber(List<Integer> numbers) {
-        if (numbers.size() > 7) {
-            throw new IllegalArgumentException("[ERROR] 보너스 숫자를 포함한 로또 번호 갯수는 최대 7개입니다.");
+    public static void validateLottoNumbersRange(int number) {
+        if (number < 1 || number > 45) {
+            System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
+
+    public static void validateBonusNumberOverlap(List<Integer> numbers, int bonusNumber) {
+        if (numbers.contains(bonusNumber)) {
+            System.out.println("[ERROR] 중복된 보너스 번호입니다.");
+            throw new IllegalArgumentException("[ERROR] 중복된 보너스 번호입니다.");
+        }
+    }
+
 }
