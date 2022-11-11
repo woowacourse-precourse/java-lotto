0237e0b0e8f0ab6d9faf5e6fb729e3c2ee05e353
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-12 00:42:40 +0900

    refactor (BonusNumber,WinningNumbers) : 클래스 분리

---
+++ src/main/java/domain/BonusNumber.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class BonusNumber {
+    private final String bonusNumber;
+
+    public BonusNumber(String bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public String getBonusNumber() {
+        return bonusNumber;
+    }
+}
---
+++ src/main/java/domain/WinningNumbers.java
@@ -0,0 +1,17 @@
+package domain;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private final List<Integer> winningNumbers;
+
+    public WinningNumbers(List<Integer> winningNumbers) {
+        this.winningNumbers = winningNumbers;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+
+}
