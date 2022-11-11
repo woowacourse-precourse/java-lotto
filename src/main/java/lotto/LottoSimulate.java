8b51a6c11fef0ec763414ddf9a3226b8d2ab2109
eora21 <eora21@naver.com>
2022-11-11 21:39:03 +0900

    feat: 🎸 추가 로또 통계 획득 메서드 및 예외 출력
    로또 통계를 얻어오는 메서드, 그리고 예외사항을 출력하는 메서드를 작성했다.

--- src/main/java/lotto/LottoSimulate.java
+++ src/main/java/lotto/LottoSimulate.java
@@ -1,9 +1,11 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import constant.Prize;
 import constant.LottoText;
 
 import java.util.List;
+import java.util.Map;
 
 public class LottoSimulate {
     private final Print print = new Print();
@@ -12,14 +14,20 @@ public class LottoSimulate {
 
     private final LottoShop lottoShop = new LottoShop();
 
+    private Statistics statistics = new Statistics();
+
     public void start() {
-        int purchasePrice = inputPurchasePrice();
-        List<Lotto> lottos = buyLottos(purchasePrice);
-        List<Integer> winLottoNumbers = inputWinLottoNumbers();
-        int winLottoBonusNumber = inputWinLottoBonusNumber();
+        try {
+            int purchasePrice = inputPurchasePrice();
+            List<Lotto> lottos = buyLottos(purchasePrice);
+            WinLotto winLotto = new WinLotto(inputWinLottoNumbers(), inputWinLottoBonusNumber());
+            Map<Prize, Integer> statistic = getLottoStatics(winLotto, lottos);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    private int inputPurchasePrice() {
+    private int inputPurchasePrice() throws IllegalArgumentException {
         print.out(LottoText.REQUEST_PURCHASE_PRICE);
         String input = Console.readLine();
         return convert.toInt(input);
@@ -45,5 +53,11 @@ public class LottoSimulate {
         print.out(LottoText.REQUEST_BONUS_NUMBER);
         String input = Console.readLine();
         return convert.toWinLottoBonusNumber(input);
+    }
+
+    private Map<Prize,Integer> getLottoStatics(WinLotto winLotto, List<Lotto> lottos) {
+        Map<Prize,Integer> lottoStatics = statistics.produce(winLotto, lottos);
+        print.out(LottoText.WINNING_STATISTICS);
+        return lottoStatics;
     }
 }
