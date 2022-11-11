6b1718a3a2f473abad44a0999d92e5aa7ec5f7ce
sysliar <sysliar@gmail.com>
2022-11-11 20:49:52 +0900

    feat(Lotto): add getter

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,6 +11,9 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getLottoNumbers() {
+        return numbers;
+    }
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
