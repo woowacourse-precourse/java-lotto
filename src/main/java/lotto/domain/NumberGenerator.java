f6fa8e4a99babde36ff91b88508ebb02ac53e7dc
qdeu22 <qdeu22@gmail.com>
2022-11-12 02:52:55 +0900

    feat(lotto): 1~45까지의 로또 번호 생성하는 기능 구현

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -1,6 +1,12 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class NumberGenerator {
+    public List<Integer> createNumbers(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
 }
