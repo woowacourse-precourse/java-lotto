42ff3d81a0753eb15b3169199141e85601a66748
Jin409 <sualng123@gmail.com>
2022-11-11 22:35:10 +0800

    feat(MoneyValidator): 지불 금액 관련 예외처리 기능 구현

--- src/main/java/lotto/constants/LottoConstant.java
+++ src/main/java/lotto/constants/LottoConstant.java
@@ -6,4 +6,5 @@ public class LottoConstant {
     public static final int VALID_LOTTERY_NUMBER_LENGTH = 6;
     public static final int VALID_BONUS_NUMBER_LENGTH = 1;
     public static final String LOTTERY_NUMBER_SEPARATOR = ",";
+    public static final int LOTTO_PRICE = 1000;
 }
---
+++ src/main/java/lotto/validator/MoneyValidator.java
@@ -0,0 +1,12 @@
+package lotto.validator;
+
+import static lotto.constants.LottoConstant.LOTTO_PRICE;
+import static lotto.validator.ErrorMessages.INVALID_PAID_MONEY_MESSAGE;
+
+public class MoneyValidator {
+    public static void hasValidPaidMoney(int paidMoney) {
+        if (!(paidMoney % LOTTO_PRICE == 0)) {
+            throw new IllegalArgumentException(INVALID_PAID_MONEY_MESSAGE);
+        }
+    }
+}
