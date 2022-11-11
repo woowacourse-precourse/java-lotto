5d18677b8484ef6982e2b1f41ad73b7250763979
yg0585 <yg0585@naver.com>
2022-11-11 16:25:35 +0900

    feat: output 기능 구현

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -3,17 +3,49 @@ package lotto;
 import java.util.List;
 
 public class OutputView {
-    public static void printStartMesseage(){
+    public static void printStartMesseage() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public static void printLottoNumber(int count){
+    public static void printLottoNumber(int count) {
         System.out.println(count + "개를 구매했습니다.");
     }
 
-    public static void printLotto(List<Lotto> lottos){
-        for (Lotto lotto: lottos){
+    public static void printLotto(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
             System.out.println(lotto);
         }
+    }
+
+    public static void printRequestWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void printRequestBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public static void printFifth(int count) {
+        System.out.println("3개 일치 (5,000원) - " + count + "개");
+    }
+
+    public static void printFourth(int count){
+        System.out.println("4개 일치 (50,000원) - " + count + "개");
+    }
+
+    public static void printThird(int count){
+        System.out.println("5개 일치 (1,500,000원) - " + count + "개");
+    }
+
+    public static void printSecond(int count){
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + count + "개");
+    }
+
+    public static void printFirst(int count){
+        System.out.println("6개 일치 (2,000,000,000원) - " + count + "개");
+    }
+
+    public static void printYield(float yield){
+        System.out.printf("총 수익률은 %.1f입니다.\n", yield);
     }
 }
