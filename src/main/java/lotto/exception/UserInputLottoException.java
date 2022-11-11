6be034e3a0832285ccbcd477d058111eba89f836
sosow0212 <sosow0212@naver.com>
2022-11-11 11:54:02 +0900

    feat : 로또 번호가 적절한 범위인지 확인해주는 예외 작성

--- src/main/java/lotto/exception/UserInputLottoException.java
+++ src/main/java/lotto/exception/UserInputLottoException.java
@@ -6,6 +6,8 @@ public class UserInputLottoException {
 
     private final static int PERMITTED_LOTTO_LENGTH = 6;
     private final static int PERMITTED_BONUS_LENGTH = 1;
+    private final static int MIN_LOTTO_NUMBER = 1;
+    private final static int MAX_LOTTO_NUMBER = 45;
 
     public void validateLotto(String numbers, String bonusNumber) {
         validateLottoNumberLength(numbers);
@@ -14,6 +16,14 @@ public class UserInputLottoException {
     public void validateLottoNumberLength(String inputNumbers) {
         if (inputNumbers.replace(",", "").length() != PERMITTED_LOTTO_LENGTH) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateNumbersPermittedRange(List<Integer> inputNumbers) {
+        for(int input : inputNumbers) {
+            if(input < MIN_LOTTO_NUMBER || input > MAX_LOTTO_NUMBER) {
+                throw new IllegalArgumentException();
+            }
         }
     }
 
