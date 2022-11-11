ef4a0864b41478308f571eabfd7be55a386bca84
ddubidubap <park62000@gmail.com>
2022-11-11 15:58:36 +0900

    refactor: 당첨 금액 해시맵 필드 추가

--- src/main/java/lotto/domain/Comparator.java
+++ src/main/java/lotto/domain/Comparator.java
@@ -1,13 +1,13 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class Comparator {
     private final List<List<Integer>> tickets;
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
+    private final LinkedHashMap<Prize, Integer> winningTickets = new LinkedHashMap<>();
 
     public Comparator(
             List<List<Integer>> tickets,
@@ -17,6 +17,10 @@ public class Comparator {
         this.tickets = tickets;
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+
+        for (Prize prize: Prize.values()) {
+            this.winningTickets.put(prize, 0);
+        }
     }
 
 
