e92b63324325218d766dc575ba3d293f87764278
wugawuga <dnr3655@naver.com>
2022-11-11 14:47:16 +0900

    refactor: 테스트 예외메세지 확인, 상수 분리

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class InputTest {
+    private static final String NUMBER_FORMAT_ERROR = "[ERROR] 숫자를 입력해 주세요.";
+    private static final String SEPARATOR_ERROR = "[ERROR] 구분자 ',' 를 입력해 주세요.";
+    private static final String AMOUNT_UNIT_ERROR = "[ERROR] 1000원 단위로 입력하세요.";
 
     @ParameterizedTest
     @DisplayName("1000원으로 나누어 떨어지지 않을 때 예외")
@@ -22,7 +25,8 @@ class InputTest {
         System.setIn(in);
 
         assertThatThrownBy(Input::purchaseAmount)
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(AMOUNT_UNIT_ERROR);
     }
 
     @ParameterizedTest
@@ -36,7 +40,8 @@ class InputTest {
         System.setIn(in);
 
         assertThatThrownBy(Input::purchaseAmount)
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NUMBER_FORMAT_ERROR);
     }
 
     @ParameterizedTest
@@ -51,7 +56,8 @@ class InputTest {
         System.setIn(in);
 
         assertThatThrownBy(Input::winningNumber)
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NUMBER_FORMAT_ERROR);
     }
 
     @Test
@@ -61,7 +67,8 @@ class InputTest {
         System.setIn(in);
 
         assertThatThrownBy(Input::winningNumber)
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(SEPARATOR_ERROR);
     }
 
     private InputStream generateInput(String input) {
