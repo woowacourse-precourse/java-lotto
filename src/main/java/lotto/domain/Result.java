50bf280c7b483ccdf228c04ab12e7eb9d419d819
dev_kong <ahdjd5@gmail.com>
2022-11-11 15:13:53 +0900

    feat(Result): add method countWinningCase

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -20,13 +20,42 @@ public class Result {
     };
 
     public Result(List<List<Integer>> purchasedLotteries,
-                       WinningNumbers winningNumbers,
-                       BonusNumber bonusNumber) {
+                  WinningNumbers winningNumbers,
+                  BonusNumber bonusNumber) {
         this.purchasedLotteries = purchasedLotteries;
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
 
+    public Map<String, Integer> getResultMap() {
+        return resultMap;
+    }
+
+    // TODO : 리팩토링 인덴트 1 유지 & 하드코딩 삭제
     public void countWinningCase() {
+        for (List<Integer> purchasedLotto : purchasedLotteries) {
+            int winningCount = winningNumbers.countWinning(purchasedLotto);
+
+            if (winningCount == 6) {
+                resultMap.put("first", resultMap.get("first") + 1);
+            }
+            if (winningCount == 5) {
+                if (bonusNumber.checkContainBonusNumber(purchasedLotto)) {
+                    resultMap.put("second", resultMap.get("second") + 1);
+                }
+
+                if (bonusNumber.checkContainBonusNumber(purchasedLotto)) {
+                    resultMap.put("third", resultMap.get("third") + 1);
+                }
+            }
+            if (winningCount == 4) {
+                resultMap.put("fourth", resultMap.get("fourth") + 1);
+            }
+            if (winningCount == 3) {
+                resultMap.put("fifth", resultMap.get("fifth") + 1);
+            }
+
+
+        }
     }
 }
