1b7db1a8e6fc8b91e5f1a6787504d9198cc4c4df
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-11 18:38:44 +0900

    test(InputValidTest): 숫자가 아닌지 확인하는 테스트 메서드 추가

--- src/test/java/lotto/Utils/InputValidTest.java
+++ src/test/java/lotto/Utils/InputValidTest.java
@@ -1,23 +1,30 @@
 package lotto.Utils;
 
-import lotto.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class InputValidTest {
 
     @DisplayName("구입 금액이 1,000원 단위가 아니면 예외가 발생한다")
     @Test
     void CreateUserPriceBy1000Unit(){
-        assertThatThrownBy(()->InputValid.checkPriceUnit(1400))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThat(InputValid.checkPriceUnit("1400"))
+                .isEqualTo(false);
 
-        assertThatThrownBy(()->InputValid.checkPriceUnit(400))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThat(InputValid.checkPriceUnit("400"))
+                .isEqualTo(false);
+    }
+
+    @DisplayName("구입 금액이 숫자만 있는게 아니라면 예외가 발생한다")
+    @Test
+    void CreateUserPriceByNumber(){
+        assertThat(InputValid.checkPriceUnit("1000j"))
+                .isEqualTo(false);
+
+        assertThat(InputValid.checkPriceUnit("non"))
+                .isEqualTo(false);
     }
 }
