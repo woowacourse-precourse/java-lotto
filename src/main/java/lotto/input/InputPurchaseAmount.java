98757b8152069e172635877e8783dc308f64aaa6
chanwonlee <clw8679@naver.com>
2022-11-12 00:12:22 +0900

    feat: InputPurchaseAmount 구현

---
+++ src/main/java/lotto/input/InputPurchaseAmount.java
@@ -0,0 +1,20 @@
+package lotto.input;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.model.PurchaseAmount;
+
+public class InputPurchaseAmount {
+    public PurchaseAmount input() {
+        String input = Console.readLine();
+        validateInteger(input);
+        return new PurchaseAmount(Integer.parseInt(input));
+    }
+
+    private void validateInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
+        }
+    }
+}
