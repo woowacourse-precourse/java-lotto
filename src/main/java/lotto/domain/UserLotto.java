7d8de25d42775e67c95e2871d6fb993eeea9f669
woosung1223 <dntjd991223@naver.com>
2022-11-11 18:21:44 +0900

    fix(UserLotto): buyLotto 메소드 %가 아닌 /로 변경

--- src/main/java/lotto/domain/UserLotto.java
+++ src/main/java/lotto/domain/UserLotto.java
@@ -9,11 +9,12 @@ import java.util.List;
 public class UserLotto {
     private LottoWrapper lottoSet = new LottoWrapper();
 
-    public void buyLottos() {
-        long count = readMoney() % Constant.MONEY_UNIT;
+    public void buyLotto() {
+        long count = readMoney() / Constant.MONEY_UNIT;
         while (count-- > 0) {
             lottoSet.add(makeLotto());
         }
+        System.out.println(lottoSet);
     }
 
     private long readMoney() {
