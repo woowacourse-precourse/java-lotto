8e088fd6ac2940b3296aa9b5078e72166b830cea
박준용 <jo101044@naver.com>
2022-11-11 20:53:38 +0900

    feat: "Application 실행 기능 추가"
    LottoLauncher 인스턴스를 생성하고 lottoLauncher.startLotto() 이용해 프로그램을 실행시킨다.
    예외 발생시 예외 메세지를 출력한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,13 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoLauncher lottoLauncher = new LottoLauncher();
+
+        try {
+            lottoLauncher.startLotto();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
