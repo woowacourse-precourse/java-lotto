4158b0d647bcd87f1ebd4ac42a5dd7857aeb7ce1
sosow0212 <sosow0212@naver.com>
2022-11-11 14:24:04 +0900

    fix : 로또 중복 번호 구하는 예외에서 set에 번호 저장 안되는 문제 해결

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -6,33 +6,36 @@ import static lotto.util.LottoUtil.lottoNumbersParser;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class UserInputLottoException {
+public class LottoException {
 
     private final static int PERMITTED_LOTTO_LENGTH = 6;
     private final static int MIN_LOTTO_NUMBER = 1;
     private final static int MAX_LOTTO_NUMBER = 45;
     private static final String ONLY_NUMBER_REGEX = "[1-45]+";
 
-    public void validateLotto(String inputNumbers, String inputBonusNumber) {
+    public void validateLottoNumbers(String inputNumbers) {
         validateLottoNumbersLength(inputNumbers);
         validateOverlapNumbers(lottoNumbersParser(inputNumbers));
         validateNumbersPermittedRange(lottoNumbersParser(inputNumbers));
+    }
+
+    public void validateBonusNumbers(String inputNumbers, String inputBonusNumber) {
         validateBonusNumberIsNumber(inputBonusNumber);
         validateBonusNumberPermittedRange(bonusNumberParser(inputBonusNumber));
         validateBonusNumberOverlapNumbers(lottoNumbersParser(inputNumbers), bonusNumberParser(inputBonusNumber));
-
     }
 
     public void validateLottoNumbersLength(String numbers) {
         if (numbers.replace(",", "").length() != PERMITTED_LOTTO_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호는 6개 입력해주세요.");
         }
     }
 
     public void validateOverlapNumbers(List<Integer> inputNumbers) {
         Set<Integer> numbers = new HashSet<>();
-        inputNumbers.stream().map(number -> numbers.add(number));
+        inputNumbers.stream().map(number -> numbers.add(number)).collect(Collectors.toList());
         if (numbers.size() != PERMITTED_LOTTO_LENGTH) {
             throw new IllegalArgumentException("번호 중에 중복 번호가 있습니다.");
         }
