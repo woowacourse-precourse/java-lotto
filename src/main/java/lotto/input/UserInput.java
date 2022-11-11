c1fa9932fce25480635dd5ba0a25e59c9c9538d6
unknown <ymecca12@gmail.com>
2022-11-12 03:21:33 +0900

    feat : 당첨 번호 입력

--- src/main/java/lotto/input/UserInput.java
+++ src/main/java/lotto/input/UserInput.java
@@ -3,18 +3,33 @@ package lotto.input;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.validate.Validator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserInput implements Input {
     @Override
     public void readInput() {
-        System.out.println("구입금액을 입력해 주세요.");
         //    구매 금액 입력
+        System.out.println("구입금액을 입력해 주세요.");
         String purchasePrice = Console.readLine();
-        int purchaseAmount = Validator.validateModIsZero(Integer.parseInt(purchasePrice));
-        System.out.println(purchaseAmount + "개를 구매했습니다.");
+        Validator.validateModIsZero(Integer.parseInt(purchasePrice));
+        Integer purchaseAmount = Integer.parseInt(purchasePrice) / 1000;
+        System.out.println(purchaseAmount + "개를 구매했습니다." + "\n");
+        //    구매 로또 번호 출력
 
         //    당첨 번호 입력
-        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String prizeInput = Console.readLine();
+        String[] prizeString = prizeInput.split(",");
+        List<Integer> prizeNumbers = new ArrayList<>();
+        for (String str : prizeString) {
+            Integer i = Integer.parseInt(str);
+            prizeNumbers.add(i);
+        }
+        Validator.validateOutOfBound(prizeNumbers);
         //    보너스 번호 입력
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        String bonusNumber = Console.readLine();
     }
 
 }
--- src/main/java/lotto/validate/Validator.java
+++ src/main/java/lotto/validate/Validator.java
@@ -1,16 +1,26 @@
 package lotto.validate;
 
+import java.util.List;
+
 public class Validator {
     private static final String ERROR_MESSAGE = "[ERROR] : ";
     private static final String MOD_IS_ZERO = "1,000원 단위로 ";
+    private static final String OUT_OF_BOUND = "1~45 사이의 숫자를 입력해주세요";
 
     public static String generateErrorMessage(String input){
         return ERROR_MESSAGE + input;
     }
-    public static int validateModIsZero(Integer purchaseAmount) {
+    public static void validateModIsZero(Integer purchaseAmount) {
         if((purchaseAmount % 1000) > 0){
             throw new IllegalArgumentException(generateErrorMessage(MOD_IS_ZERO));
         }
-        return purchaseAmount / 1000;
+    }
+
+    public static void validateOutOfBound(List<Integer> prizeNumbers) {
+        for (Integer prizeNumber : prizeNumbers) {
+            if(prizeNumber < 1 && prizeNumber > 45){
+                throw new IllegalArgumentException(generateErrorMessage(OUT_OF_BOUND));
+            }
+        }
     }
 }
