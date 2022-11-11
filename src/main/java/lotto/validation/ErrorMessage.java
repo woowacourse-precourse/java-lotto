db53a91c849dfc14939c7f715aa76a4b8b81dadd
ykmxxi <ykmxxi97@gmail.com>
2022-11-11 17:30:24 +0900

    feat: 로또 당첨 번호 6개 입력 예외처리 구현

--- src/main/java/lotto/validation/ErrorMessage.java
+++ src/main/java/lotto/validation/ErrorMessage.java
@@ -4,7 +4,12 @@ public enum ErrorMessage {
     BLANK_EXCEPTION("[ERROR] 금액을 입력해 주세요."),
     NUMBER_EXCEPTION("[ERROR] 숫자만 입력해 주세요."),
     ZERO_EXCEPTION("[ERROR] 0원을 입력할 수 없습니다. 1,000원 이상 입력해 주세요."),
-    CHANGES_EXCEPTION("[ERROR] 잔돈이 있습니다. 1,000원 단위로 입력해 주세요.");
+    CHANGES_EXCEPTION("[ERROR] 잔돈이 있습니다. 1,000원 단위로 입력해 주세요."),
+    BLANK_WINNING_EXCEPTION("[ERROR] 당첨 번호를 입력해 주세요."),
+    NUMBER_COMMA_WINNING_EXCEPTION("[ERROR] 당첨 번호 6개를 쉼표(,)를 기준으로 구분해 입력해 주세요."),
+    CORRECT_RANGE_WINNING_EXCEPTION("[ERROR] 당첨 번호는 1 부터 45 사이의 숫자로 입력해 주세요."),
+    CORRECT_SIZE_WINNING_EXCEPTION("[ERROR] 당첨 번호는 총 6개 입니다. 6개의 당첨 번호를 입력해 주세요."),
+    DUPLICATE_WINNING_EXCEPTION("[ERROR] 중복된 당첨 번호가 존재합니다. 당첨 번호를 다시 입력해 주세요.");
 
     private final String message;
 
--- src/main/java/lotto/validation/WinningValidation.java
+++ src/main/java/lotto/validation/WinningValidation.java
@@ -1,6 +1,18 @@
 package lotto.validation;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class WinningValidation extends IllegalArgumentException {
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 45;
+    private static final int WINNING_SIZE = 6;
+
+    private ErrorMessage message;
+    private RegEx regEx;
+    private List<String> winning;
 
     public WinningValidation() {
     }
@@ -15,27 +27,53 @@ public class WinningValidation extends IllegalArgumentException {
     public void isValidBonus() {
     }
 
-    public void isBlank() {
+    public void isBlank(String input) {
+        if (input.isBlank()) {
+            message = ErrorMessage.valueOf("BLANK_WINNING_EXCEPTION");
+            throwError(message.getMessage());
+        }
     }
 
-    public void isNumberAndComma() {
+    public void isNumberAndComma(String input) {
+        regEx = RegEx.valueOf("WINNING_INPUT_REG_EX");
+        if (!Pattern.matches(regEx.getRegEx(), input)) {
+            message = ErrorMessage.valueOf(("NUMBER_COMMA_WINNING_EXCEPTION"));
+            throwError(message.getMessage());
+        }
     }
 
-    public void isCorrectRange() {
+    public void isCorrectRange(String input) {
+        winning = Arrays.asList(input.split(","));
+        for (String number : winning) {
+            if (Integer.parseInt(number) < START_INCLUSIVE || Integer.parseInt(number) > END_INCLUSIVE) {
+                message = ErrorMessage.valueOf("CORRECT_RANGE_WINNING_EXCEPTION");
+                throwError(message.getMessage());
+            }
+        }
     }
 
-    public void isCorrectWinningSize() {
+    public void isCorrectWinningSize(String input) {
+        winning = Arrays.asList(input.split(","));
+        if (winning.size() != WINNING_SIZE) {
+            message = ErrorMessage.valueOf("CORRECT_SIZE_WINNING_EXCEPTION");
+            throwError(message.getMessage());
+        }
     }
 
-    public void isDuplicate() {
+    public void isDuplicate(String input) {
+        winning = Arrays.asList(input.split(","));
+        if (new HashSet<>(winning).size() != WINNING_SIZE) {
+            message = ErrorMessage.valueOf("DUPLICATE_WINNING_EXCEPTION");
+            throwError(message.getMessage());
+        }
     }
 
-    public void isNumber() {
+    public void isNumber(String input) {
     }
 
-    public void isCorrectBonusSize() {
+    public void isCorrectBonusSize(String input) {
     }
 
-    public void isContainWinning() {
+    public void isContainWinning(String input, List<Integer> winnings) {
     }
 }
