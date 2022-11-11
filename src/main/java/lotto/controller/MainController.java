21d908840c5278fe9c7df247d902f42becd9a94d
chanwonlee <clw8679@naver.com>
2022-11-12 00:30:36 +0900

    feat: MainController 구현

---
+++ src/main/java/lotto/controller/MainController.java
@@ -0,0 +1,28 @@
+package lotto.controller;
+
+import lotto.input.InputBonusNumber;
+import lotto.input.InputPurchaseAmount;
+import lotto.input.InputWinningNumbers;
+import lotto.model.*;
+import lotto.view.LottoResultView;
+import lotto.view.ProfitRateView;
+import lotto.view.TicketView;
+
+import java.util.List;
+
+public class MainController {
+    public static void run() {
+        PurchaseAmount purchaseAmount = InputPurchaseAmount.input();
+
+        Ticket ticket = new Ticket(purchaseAmount.getMoney());
+        TicketView.outPut(ticket);
+
+        List<Integer> winningNumbers = InputWinningNumbers.input();
+        BonusNumber bonusNumber = InputBonusNumber.input();
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+
+        LottoResult lottoResult = new LottoResult(ticket, winningLotto);
+        LottoResultView.outPut(lottoResult);
+        ProfitRateView.outPut(purchaseAmount, lottoResult);
+    }
+}
