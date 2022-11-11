bd9cfb72dff11690ac56861f98df6ce86dba4f11
Donghyun <flaehan@gmail.com>
2022-11-11 10:45:10 +0900

    type : feat 구입 금액 입력
    body : Console.readLine 활용하여 구입금액 입력 기능 구현
    
    footer :

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,19 +2,49 @@ package lotto;
 
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers ;
+    private List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45 , 6);
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
+    // 구입 금액 입력
+    public int inputMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
+        int inputMoney = Integer.parseInt(Console.readLine());
+
+        return inputMoney;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또는 6자리의 숫자여야 합니다.");
+        }
+        for (int i =0; i < numbers.size(); i++){
+            if (numbers.get(i) <= 0 || numbers.get(i) > 45){
+                throw new IllegalArgumentException("[ERRO] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
         }
     }
 
