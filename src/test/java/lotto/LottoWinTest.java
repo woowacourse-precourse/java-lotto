8f44b4cd6cbdc9a21136a15689b50705fe9b90e6
MuSeung <peter0757@nvaer.com>
2022-11-11 14:24:07 +0900

    test: LottoWin에 1등이 2개인 경우 테스트 추가
    twoFirstPrize
        - 1등이 2개인 상황에서 map의 1등의 값이 2인지 확인

--- src/test/java/lotto/LottoWinTest.java
+++ src/test/java/lotto/LottoWinTest.java
@@ -14,24 +14,32 @@ public class LottoWinTest {
 
     @DisplayName("중복된 보너스 번호가 들어오면 에러가 발생한다")
     @Test
-    void duplicatedBonusNumber(){
-        assertThatThrownBy(() -> new LottoWin(List.of(List.of(1,2,3,4,5,6)),List.of(1,2,3,4,5,6), 6))
+    void duplicatedBonusNumber() {
+        assertThatThrownBy(() -> new LottoWin(List.of(List.of(1, 2, 3, 4, 5, 6)), List.of(1, 2, 3, 4, 5, 6), 6))
                 .isInstanceOf(IllegalArgumentException.class);
     }
     @DisplayName("1~45 범위에서 벗어난 보너스 번호가 들어오면 에러가 발생한다")
     @Test
-    void outOfBoundBonusNumber(){
-        assertThatThrownBy(() -> new LottoWin(List.of(List.of(1,2,3,4,5,6)),List.of(1,2,3,4,5,6), 0))
+    void outOfBoundBonusNumber() {
+        assertThatThrownBy(() -> new LottoWin(List.of(List.of(1, 2, 3, 4, 5, 6)), List.of(1, 2, 3, 4, 5, 6), 0))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new LottoWin(List.of(List.of(1,2,3,4,5,6)),List.of(1,2,3,4,5,6), 46))
+        assertThatThrownBy(() -> new LottoWin(List.of(List.of(1, 2, 3, 4, 5, 6)), List.of(1, 2, 3, 4, 5, 6), 46))
                 .isInstanceOf(IllegalArgumentException.class);
     }
     @DisplayName("1등 출력 확인")
     @Test
-    void firstPrize(){
-        LottoWin lottoPrizes = new LottoWin(List.of(List.of(1,2,3,4,5,6)),List.of(1,2,3,4,5,6),7);
-        assertThat(lottoPrizes.getPrizesMap()).contains(entry("1st",1));
+    void firstPrize() {
+        LottoWin lottoPrizes = new LottoWin(List.of(List.of(1, 2, 3, 4, 5, 6)), List.of(1, 2, 3, 4, 5, 6), 7);
+        assertThat(lottoPrizes.getPrizesMap()).contains(entry("1st", 1));
 
+    }
+    @DisplayName("1등 2명 출력 확인")
+    @Test
+    void twoFirstPrize() {
+        LottoWin lottoPrizes = new LottoWin(
+                List.of(List.of(1, 2, 3, 4, 5, 6), List.of(1, 2, 3, 4, 5, 6)),
+                List.of(1, 2, 3, 4, 5, 6), 7);
+        assertThat(lottoPrizes.getPrizesMap()).contains(entry("1st", 2));
     }
 
 }
