97ed5404f4bd55f5afee47836c54be954b66dfa6
sosow0212 <sosow0212@naver.com>
2022-11-11 12:25:39 +0900

    feat : 보너스 번호가 1~45 범위 내에 있는지 확인해주는 예외 작성

--- src/main/java/lotto/exception/UserInputLottoException.java
+++ src/main/java/lotto/exception/UserInputLottoException.java
@@ -1,5 +1,8 @@
 package lotto.exception;
 
+import static lotto.util.LottoUtil.bonusNumberParser;
+import static lotto.util.LottoUtil.lottoNumbersParser;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -7,22 +10,22 @@ import java.util.Set;
 public class UserInputLottoException {
 
     private final static int PERMITTED_LOTTO_LENGTH = 6;
-    private final static int PERMITTED_BONUS_LENGTH = 1;
     private final static int MIN_LOTTO_NUMBER = 1;
     private final static int MAX_LOTTO_NUMBER = 45;
     private static final String ONLY_NUMBER_REGEX = "[1-45]+";
 
     public void validateLotto(String inputNumbers, String inputBonusNumber) {
         validateLottoNumbersLength(inputNumbers);
-        validateOverlapNumbers()
-        validateNumbersPermittedRange();
+        validateOverlapNumbers(lottoNumbersParser(inputNumbers));
+        validateNumbersPermittedRange(lottoNumbersParser(inputNumbers));
         validateBonusNumberIsNumber(inputBonusNumber);
-        validateBonusNumberOverlapNumbers();
+        validateBonusNumberPermittedRange(bonusNumberParser(inputBonusNumber));
+        validateBonusNumberOverlapNumbers(lottoNumbersParser(inputNumbers), bonusNumberParser(inputBonusNumber));
 
     }
 
-    public void validateLottoNumbersLength(String inputNumbers) {
-        if (inputNumbers.replace(",", "").length() != PERMITTED_LOTTO_LENGTH) {
+    public void validateLottoNumbersLength(String numbers) {
+        if (numbers.replace(",", "").length() != PERMITTED_LOTTO_LENGTH) {
             throw new IllegalArgumentException();
         }
     }
@@ -30,27 +33,33 @@ public class UserInputLottoException {
     public void validateOverlapNumbers(List<Integer> inputNumbers) {
         Set<Integer> numbers = new HashSet<>();
         inputNumbers.stream().map(number -> numbers.add(number));
-        if(numbers.size() != PERMITTED_LOTTO_LENGTH) {
+        if (numbers.size() != PERMITTED_LOTTO_LENGTH) {
             throw new IllegalArgumentException("번호 중에 중복 번호가 있습니다.");
         }
     }
 
-    public void validateNumbersPermittedRange(List<Integer> inputNumbers) {
-        for(int input : inputNumbers) {
-            if(input < MIN_LOTTO_NUMBER || input > MAX_LOTTO_NUMBER) {
+    public void validateNumbersPermittedRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
                 throw new IllegalArgumentException("로또의 범위는 1~45입니다.");
             }
         }
     }
-    // 밑에 예외가 있음
-    public void validateBonusNumberIsNumber(String inputBonusNumber) {
-        if(!inputBonusNumber.matches(ONLY_NUMBER_REGEX)) {
+
+    public void validateBonusNumberIsNumber(String bonusNumber) {
+        if (!bonusNumber.matches(ONLY_NUMBER_REGEX)) {
             throw new IllegalArgumentException("보너스 번호는 한 개의 숫자만 입력 가능합니다.");
         }
     }
 
+    public void validateBonusNumberPermittedRange(int bonusNumber) {
+        if (bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("로또의 범위는 1~45입니다.");
+        }
+    }
+
     public void validateBonusNumberOverlapNumbers(List<Integer> numbers, int bonusNumber) {
-        if(numbers.contains(bonusNumber)) {
+        if (numbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("보너스 번호가 중복됩니다.");
         }
     }
