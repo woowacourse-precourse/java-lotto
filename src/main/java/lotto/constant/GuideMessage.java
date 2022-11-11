b3790191b2bb97513973ef656be2985eaf15048d
yubin <dbqls2821@gmail.com>
2022-11-11 15:15:06 +0900

    refactor(GuideMessage): 이름을 InGameMessage -> GuideMessage로 변경

--- src/main/java/lotto/constant/GuideMessage.java
+++ src/main/java/lotto/constant/GuideMessage.java
@@ -1,6 +1,6 @@
 package lotto.constant;
 
-public enum InGameMessage {
+public enum GuideMessage {
   INPUT_MONEY("구입금액을 입력해 주세요."),
   OUTPUT_NUM_OF_LOTTO("개를 구매했습니다."),
   INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
@@ -8,7 +8,7 @@ public enum InGameMessage {
 
   private String message;
 
-  InGameMessage(String message) {
+  GuideMessage(String message) {
     this.message = message;
   }
 
