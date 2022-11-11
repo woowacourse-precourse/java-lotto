bcc9c2bd026faebeacaf58a67104d8811a18e6d2
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-11 19:07:19 +0900

    feat(WinningLotto): 보너스 번호 validation
    - 보너스 번호 로또 번호 범위 내인가 확인
    - 보너스 번호가 기존 당첨 번호와 안겹치는가 확인

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -4,10 +4,22 @@ import java.util.List;
 
 public class WinningLotto extends Lotto {
 
+    public static final String BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE = "[ERROR] 보너스 번호는 당첨 번호와 중복되면 안됩니다.";
+
     private final int bonusNumber;
 
     public WinningLotto(List<Integer> numbers, int bonusNumber) {
         super(numbers);
+        validateBonusNumber(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(int bonusNumberInput) {
+        if (bonusNumberInput < MIN_NUMBER || bonusNumberInput > MIN_NUMBER) {
+            throw new IllegalArgumentException(NUMBER_INPUT_RANGE_ERROR_MESSAGE);
+        }
+        if (super.isContainNumber(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE);
+        }
     }
 }
