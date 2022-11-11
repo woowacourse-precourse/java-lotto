16bdb469e48772993f6443a8759851b426dd59d4
YSEO <1park4170@gmail.com>
2022-11-11 10:12:18 +0900

    feat<Lotto> : add validator that check if numbers are duplicated
    - validateDuplicate : turn list into Hashset, and check if its size is not same with list's size

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,15 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -17,6 +20,15 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
     public List<Integer> getNumbers() {
         return numbers;
     }
