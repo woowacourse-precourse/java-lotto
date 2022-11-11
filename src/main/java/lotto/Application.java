29003081d4b3ab35dc4c78bb43f70973f8256a14
Minjune <minjune8506@gmail.com>
2022-11-11 20:16:06 +0900

    refactor(Lotto): Lottos 출력 기능 리팩터링
    - toString 메서드를 이용하여 출력하게 기능 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,13 @@
 package lotto;
 
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
         Money money = new Money(InputView.askAmount());
         Lottos lottos = new Lottos(money.getMoney());
+        OutputView.printLottos(lottos);
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,19 +1,12 @@
 package lotto.view;
 
-import java.util.List;
-import lotto.Lotto;
-import lotto.Tickets;
+import lotto.Lottos;
 
 public class OutputView {
 
-    public static void printTickets(Tickets tickets) {
-        List<Lotto> lottoTickets = tickets.getLottoTickets();
-        int count = lottoTickets.size();
-
-        System.out.printf("%d개를 구매했습니다.\n", count);
-        for (Lotto lottoTicket : lottoTickets) {
-            System.out.println(lottoTicket);
-        }
+    public static void printLottos(Lottos lottos) {
+        System.out.printf("%d개를 구매했습니다.\n", lottos.getSize());
+        System.out.println(lottos);
     }
 
 }
