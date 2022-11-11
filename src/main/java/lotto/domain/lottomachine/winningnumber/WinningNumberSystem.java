209b1f85523d4cd111a6da22057cafa0236eae5b
chanminmaxscaler <jcm04@naver.com>
2022-11-12 01:08:23 +0900

    feat: WinningNumberSystem 클래스에 사용자에게 당첨번호를 입력받는 기능을 구현한다
    사용자에게 당첨번호를 입력받는 기능을 createWinningNumberList, receiveWinningNumber 메소드로 구현한다.
    createWinningNumberList 메소드는 당첨번호 숫자가 담긴 문자열을 받아 ","를 기준으로 문자열 배열을 만든다. 그후 스트림 연산으로 WinningNumber 객체를 생성해 리스트로 반환한다.
    receiveWinningNumber 메소드는 readUserInput 메소드로 사용자의 입력을 받고 createWinningNumberList 메소드로 사용자의 입력으로 List<WinningNumber>를 생성한다. 그후 이를 WinningNumbers 객체로 생성해 반환한다.

--- src/main/java/lotto/domain/lottomachine/winningnumber/WinningNumberSystem.java
+++ src/main/java/lotto/domain/lottomachine/winningnumber/WinningNumberSystem.java
@@ -2,9 +2,26 @@ package lotto.domain.lottomachine.winningnumber;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class WinningNumberSystem {
     public static final String REGULAR_EXPRESSION_FOR_WINNING_NUMBERS_INPUT = "\\d{1,2}\\,\\d{1,2}\\,\\d{1,2}\\,\\d{1,2}\\,\\d{1,2}\\,\\d{1,2}";
     public static final String REGULAR_EXPRESSION_FOR_BONUS_NUMBER_INPUT = "\\d";
+
+    public WinningNumbers receiveWinningNumber() {
+        String userInput = readUserInput(REGULAR_EXPRESSION_FOR_WINNING_NUMBERS_INPUT);
+        List<WinningNumber> numbers = createWinningNumberList(userInput);
+        return new WinningNumbers(numbers);
+    }
+
+    private List<WinningNumber> createWinningNumberList(String userInput) {
+        return Arrays.stream(userInput.split(","))
+                .mapToInt(Integer::parseInt)
+                .mapToObj(WinningNumber::new)
+                .collect(Collectors.toList());
+    }
 
     private String readUserInput(String regularExpression) {
         String userInput = Console.readLine();
