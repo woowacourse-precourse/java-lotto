4220b653b41808e09e66aa86f5b314b87731d318
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-11 22:34:14 +0900

    refactor(WinningRank): 당첨 안된경우 추가
    - 당첨되지 않은 경우 추가
    - rank field 삭제

--- src/main/java/lotto/domain/WinningRank.java
+++ src/main/java/lotto/domain/WinningRank.java
@@ -1,20 +1,19 @@
 package lotto.domain;
 
 public enum WinningRank {
-    FIFTH_RANK(5_000, "3개 일치", 5),
-    FOURTH_RANK(50_000, "4개 일치", 4),
-    THIRD_RANK(1_500_000, "5개 일치", 3),
-    SECOND_RANK(30_000_000, "5개 일치, 보너스 볼 일치", 2),
-    FIRST_RANK(2_000_000_000, "6개 일치", 1);
+    NONE_RANKED(0, ""),
+    FIFTH_RANK(5_000, "3개 일치"),
+    FOURTH_RANK(50_000, "4개 일치"),
+    THIRD_RANK(1_500_000, "5개 일치"),
+    SECOND_RANK(30_000_000, "5개 일치, 보너스 볼 일치"),
+    FIRST_RANK(2_000_000_000, "6개 일치");
 
     private final int prizeMoney;
     private final String winningRule;
-    private final int rank;
 
-    WinningRank(int prizeMoney, String winningRule, int rank) {
+    WinningRank(int prizeMoney, String winningRule) {
         this.prizeMoney = prizeMoney;
         this.winningRule = winningRule;
-        this.rank = rank;
     }
 
 
