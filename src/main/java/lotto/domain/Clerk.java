ffb496fb92ac54d337d9023d690aa2c1bf94c58b
keon0711 <fnc6488@gmail.com>
2022-11-12 04:13:47 +0900

    feat(lotto): validate amount of money

--- src/main/java/lotto/domain/Clerk.java
+++ src/main/java/lotto/domain/Clerk.java
@@ -2,6 +2,17 @@ package lotto.domain;
 
 public class Clerk {
     public int calculateNumberOfLottos(int money) {
+        validate(money);
         return money / 1000;
+    }
+
+    private void validate(int money) {
+        if (money <= 0) {
+            throw new IllegalArgumentException("[ERROR] 양수를 입력해야 합니다.");
+        }
+
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해야 합니다.");
+        }
     }
 }
