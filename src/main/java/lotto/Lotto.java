6b48657f80d8e55b410f1e813bd87361bc5cb7a8
Amyhds <dasomhan524@gmail.com>
2022-11-11 13:18:41 +0900

    feat(lotto): 로또 번호 숫자 범위 검증 메서드 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,17 +4,26 @@ import java.util.List;
 
 public class Lotto {
     private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateLength(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
     private void validateLength(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < MIN_NUMBER || number > MAX_NUMBER)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 }
