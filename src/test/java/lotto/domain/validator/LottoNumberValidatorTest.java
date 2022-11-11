eccfa01cc183c3fd96a61b98086eb987688e0cf2
yummygyudon <bang2brew@gmail.com>
2022-11-11 12:28:08 +0900

    TEST(domain): 로또 당첨 번호 입력값 숫자 여부 검사 테스트
        - LottoNumberValidator`checkingNotNumber` 메서드 Test
          - 이름 : notNumberExceptionTest()
          - 항목
            1. IllegalArgumentException 발생 여부
            2. ErrorMessages.NOT_NUMBER_EXIST 메세지 동일 여부

--- src/test/java/lotto/domain/validator/LottoNumberValidatorTest.java
+++ src/test/java/lotto/domain/validator/LottoNumberValidatorTest.java
@@ -8,9 +8,15 @@ import static org.junit.jupiter.api.Assertions.*;
 import lotto.util.ErrorMessages;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@DisplayName("Test about Validation of LottoNumber Input")
+@TestMethodOrder(OrderAnnotation.class)
 class LottoNumberValidatorTest {
 
     private LottoNumberValidator validator;
@@ -21,7 +27,7 @@ class LottoNumberValidatorTest {
     }
 
     @Order(1)
-    @DisplayName("Separator Check")
+    @DisplayName("when wrong separator")
     @Test
     void separatorExceptionTest(){
         String inputValue = "1, 2, 3, 4. 5. 6";
@@ -31,5 +37,18 @@ class LottoNumberValidatorTest {
         );
         Exception exception = assertThrows(IllegalArgumentException.class, () ->validator.checkingSeparator(inputValue));
         assertThat(exception.getMessage()).isEqualTo(ErrorMessages.WRONG_SEPARATOR.getMessage()) ;
+    }
+
+    @Order(2)
+    @DisplayName("when contains not number")
+    @Test
+    void notNumberExceptionTest(){
+        String inputValue = "1, 2, 3, a, b, 5";
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> validator.checkingNotNumber(inputValue))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->validator.checkingNotNumber(inputValue));
+        assertThat(exception.getMessage()).isEqualTo(ErrorMessages.NOT_NUMBER_EXIST.getMessage()) ;
     }
 }
