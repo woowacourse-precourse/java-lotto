9dc4a7a77446e18a67d6924f539eafcac2980f3b
swandevson <shai4956@gmail.com>
2022-11-11 15:42:13 +0900

    Feat(LottoValue): 로또와 관련된 상수 클래스 생성

---
+++ src/main/java/lotto/Constant/LottoValue.java
@@ -0,0 +1,7 @@
+package lotto.Constant;
+
+public class LottoValue {
+    public static final int MIN_RANGE = 1;
+    public static final int MAX_RANGE = 45;
+    public static final int NUMBER_EA = 6;
+}
