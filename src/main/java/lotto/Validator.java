aa02e70f1e5ac911f4f2be0cb71d3ebfcb2b0e17
anneu <b.imnuj@gmail.com>
2022-11-11 20:03:15 +0900

    feat: 입력 값 검증 기능 구현

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -11,7 +11,8 @@ public class Validator {
     private static final String SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호의 개수는 6개이어야 합니다.";
     private static final String DUPLICATES_ERROR_MESSAGE = "[ERROR] 로또 번호에 중복된 숫자가 있습니다.";
     private static final String RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호에 1 ~ 45 범위를 벗어난 숫자가 있습니다.";
-
+    private static final String DIGIT_ERROR_MESSAGE = "[ERROR] 숫자만 입력 가능합니다.";
+    private static final String DIGIT_COMMA_ERROR_MESSAGE = "[ERROR] 숫자와 콤마(,)만 입력 가능합니다.";
 
     public void validateNumberSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBER_SIZE) {
@@ -19,7 +20,7 @@ public class Validator {
         }
     }
 
-     public void validateDuplicateNumber(List<Integer> numbers) {
+    public void validateDuplicateNumber(List<Integer> numbers) {
         List<Integer> distinctNumbers = numbers.stream()
                 .distinct()
                 .collect(Collectors.toList());
@@ -34,6 +35,22 @@ public class Validator {
             if (isNotValidRange(number)) {
                 throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
             }
+        }
+    }
+
+    public void validateDigit(String input) {
+        String replacedResult = input.replaceAll("[0-9,]", "");
+
+        if (replacedResult.length() != 0) {
+            throw new IllegalArgumentException(DIGIT_ERROR_MESSAGE);
+        }
+    }
+
+    public void validateDigitAndComma(String input) {
+        String replacedResult = input.replaceAll("[0-9,]", "");
+
+        if (replacedResult.length() != 0) {
+            throw new IllegalArgumentException(DIGIT_COMMA_ERROR_MESSAGE);
         }
     }
 
