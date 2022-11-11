e4a5e4c0d384b2bf55bfcd4d7bbd4a14f19317ba
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-11 21:12:32 +0900

    feat(InputManager): implement winning number input

--- src/main/java/lotto/InputManager.java
+++ src/main/java/lotto/InputManager.java
@@ -2,6 +2,10 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputManager {
     private final InputValidator inputValidator = new InputValidator();
     public static final int LOTTO_PRICE = 1_000;
@@ -10,11 +14,19 @@ public class InputManager {
         System.out.println(ConsoleMessage.REQUEST_PURCHASE);
         String input = Console.readLine();
         inputValidator.validateInputPurchaseAmount(input);
+
         return Integer.parseInt(input) / LOTTO_PRICE;
     }
 
     public Lotto inputWinningNumbers() {
-        throw new UnsupportedOperationException();
+        System.out.println(ConsoleMessage.REQUEST_WINNING_NUMBERS);
+        String input = Console.readLine();
+        inputValidator.validateInputWinningNumbers(input);
+        List<Integer> inputToList = Arrays.stream(input.split(","))
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+
+        return new Lotto(inputToList);
     }
 
     public int inputBonusNumber() {
