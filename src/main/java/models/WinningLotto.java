020d4de67c9b21b001cd7139669e06079cb2e406
scyllacore <yms8795@gmail.com>
2022-11-12 08:48:39 +0900

    feat : 보너스 번호 유효성 검사 기능 업데이트.

--- src/main/java/models/WinningLotto.java
+++ src/main/java/models/WinningLotto.java
@@ -1,6 +1,7 @@
 package models;
 
 import constants.LottoConstant;
+import system.process.exception.IllegalArgument;
 
 import java.util.List;
 
@@ -8,9 +9,16 @@ public class WinningLotto {
     private Lotto lotto;
     private int bonusNumber;
 
-    public WinningLotto(List<Integer> lottoNumbers, int bonusNumber){
+    public WinningLotto(List<Integer> lottoNumbers, int bonusNumber) {
         this.lotto = new Lotto(lottoNumbers);
+        validateBonusNumber(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    void validateBonusNumber(int bonusNumber) {
+        if (bonusNumber < LottoConstant.NUMBER_START || bonusNumber > LottoConstant.NUMBER_END) {
+            IllegalArgument.handleException(IllegalArgument.NOT_IN_RANGE.getMessage());
+        }
     }
 
     public Lotto getLotto() {
--- src/main/java/system/process/exception/IllegalArgument.java
+++ src/main/java/system/process/exception/IllegalArgument.java
@@ -7,17 +7,15 @@ public enum IllegalArgument {
     // TODO : 구역 나누는 방법을 알아내야겠다..
 
     // Number
-    NOT_SET_SIZE(String.format("%d개의 숫자가 아닙니다.", LottoConstant.NUMBERS_SIZE)),
-    NOT_IN_RANGE(String.format("숫자 중, %d~%d의 범위가 아닌 숫자가 있습니다.",LottoConstant.NUMBER_START,LottoConstant.NUMBER_END)),
+    NOT_SET_SIZE(String.format("숫자의 개수가 %d개가 아닙니다.", LottoConstant.NUMBERS_SIZE)),
+    NOT_IN_RANGE(String.format("숫자 중, %d~%d의 범위를 벗어난 숫자가 있습니다.", LottoConstant.NUMBER_START, LottoConstant.NUMBER_END)),
     DUPLICATION("중복된 값이 존재합니다."),
 
     // Input
     NOT_DIGIT_OR_COMMA_TEXT("숫자나 쉼표로 이루어진 입력이 아닙니다."),
     NOT_DIGIT_TEXT("올바른 금액을 입력해주세요."),
-    NOT_DIVISION("금액에 맞춰 로또를 구매할 수 없습니다."),
-    WRONG_COMMA("잘못된 위치에 쉼표가 존재합니다.");
-
-
+    NOT_DIVISION("입력한 금액에 맞춰 로또를 구매할 수 없습니다."),
+    WRONG_NUMBERS_INPUT("올바르게 숫자를 입력해주세요.");
 
     private final String message;
 
@@ -29,11 +27,7 @@ public enum IllegalArgument {
         return message;
     }
 
-    public static void handleException(String message) {
+    public static void handleException(String message) throws IllegalArgumentException {
         throw new IllegalArgumentException(message);
     }
-
-
-
-
 }
