63d93910d2d78c2f5b9d0bb711164468764fb94a
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-11 19:30:27 +0900

    feat: Lotto 에서 LottoException.validate을 통해 예외처리하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.exception.LottoException;
+
 import java.util.List;
 
 public class Lotto {
@@ -11,11 +13,12 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-
+        LottoException lottoException = new LottoException();
+        lottoException.validate(numbers);
     }
 
     public List<Integer> getNumbers(){
-        return numbers;
+
     }
 
     // TODO: 추가 기능 구현
