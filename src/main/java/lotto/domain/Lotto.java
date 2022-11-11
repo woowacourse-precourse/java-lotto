a9d9e800483a0884c62af21416775f9eedff5c1e
Soomin Lim <augustth511@gmail.com>
2022-11-11 23:33:38 +0900

    feat(Lotto): 로또 번호 생성 시 예외 처리 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,13 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,6 +16,26 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateRange(numbers);
+        validateDuplicate(numbers);
+        validateSize(numbers);
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        if (numbers.stream()
+                .anyMatch(number -> number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> numberStorage = new HashSet<>(numbers);
+        if (numberStorage.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
