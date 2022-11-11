8932273da681586fd57ca0bff0908c85d8c065ca
KangHyeon3315 <son2786w@gmail.com>
2022-11-11 20:15:49 +0900

    feat: 로또의 유효성을 검사하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -31,10 +32,27 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    /**
+     * 로또번호의 길이와 중복을 체크해 유효한지 검사하는 함수 <br/>
+     * 유효하지 않은경우 IllegalArgumentException 발생
+     *
+     * @param numbers 로또번호
+     */
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != LOTTERY_NUMBER_LENGTH) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != LOTTERY_NUMBER_LENGTH || hasDuplicated(numbers)) {
+            throw new IllegalArgumentException(String.format("[ERROR] 로또 번호는 중복되지 않는 %d ~ %d의 숫자 %d개로 구성되어야 합니다.", LOTTERY_START_NUMBER, LOTTERY_END_NUMBER, LOTTERY_NUMBER_LENGTH));
         }
+
+    }
+
+
+    /**
+     * numbers가 중복되는 값을 가지고 있는지 판단하는 함수
+     *
+     * @return 중복된 값이 있으면 true, 없으면 false
+     */
+    public boolean hasDuplicated(List<Integer> numbers) {
+        return new HashSet<>(numbers).size() != numbers.size();
     }
 
     // TODO: 추가 기능 구현
