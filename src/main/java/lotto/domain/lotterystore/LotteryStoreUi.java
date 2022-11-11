c1370daa42c1bad06c14eeb4487e2534b5facbe1
ysng_ysng <yeasung67@gmail.com>
2022-11-11 23:18:23 +0900

    feat(LotteryStoreUi): Implement the method : takeBonusNumber
    보너스번호를 입력받는 takeBonusNumber 메서드를 구현했습니다.

--- src/main/java/lotto/domain/lotterystore/LotteryStoreUi.java
+++ src/main/java/lotto/domain/lotterystore/LotteryStoreUi.java
@@ -9,6 +9,8 @@ public class LotteryStoreUi {
 	private static final LotteryStoreUi storeUi = new LotteryStoreUi();
 	private static final ErrorChecker errorChecker = new ErrorChecker();
 	private final LotteryRequestController requestController;
+	private String winningNumbers;
+	private String bonusNumber;
 
 	private LotteryStoreUi() {
 		this.requestController = LotteryRequestController.getController();
@@ -33,7 +35,16 @@ public class LotteryStoreUi {
 		String winningNumbers = Console.readLine();
 		errorChecker.validateWinningNumbers(winningNumbers);
 
-		requestForTransferingWinningPrizeNumbers(winningNumbers);
+		this.winningNumbers = winningNumbers;
+	}
+
+	public void takeBonusNumber() {
+		printMessageForBonusNumber();
+
+		String bonusNumber = Console.readLine();
+		errorChecker.validateBonusNumbers(bonusNumber);
+
+		requestForTransferingWinningPrizeNumbers(winningNumbers, bonusNumber);
 	}
 
 	private void printPurchaseMessage() {
@@ -44,15 +55,18 @@ public class LotteryStoreUi {
 		System.out.println("당첨 번호를 입력해 주세요.");
 	}
 
+	private void printMessageForBonusNumber() {
+		System.out.println("보너스 번호를 입력해 주세요.");
+	}
 	private void requestForTransferingMoneyForPurchase(String money) {
 		// FrontEnd에서 API를 호출해서 데이터 전송하는 것과 비슷하게(??) 구현
 		MoneyForPurchase moneyDto = new MoneyForPurchase(money);
 		requestController.receiveMoneyForPurchase(moneyDto);
 	}
 
-	private void requestForTransferingWinningPrizeNumbers(String winningNumbers) {
+	private void requestForTransferingWinningPrizeNumbers(String winningNumbers, String bonusNumber) {
 		// FrontEnd에서 API를 호출해서 데이터 전송하는 것과 비슷하게(??) 구현
-		NumbersForWinningPrize winningNumbersDto = new NumbersForWinningPrize(winningNumbers);
+		NumbersForWinningPrize winningNumbersDto = new NumbersForWinningPrize(winningNumbers, bonusNumber);
 		requestController.receiveNumbersForWinningPrize(winningNumbersDto);
 	}
 }
