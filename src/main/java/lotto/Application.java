a8c9dc5ff2f73eebc995b69fa593094ae87f0275
making-a-scene <4936joo@naver.com>
2022-11-11 15:09:56 +0900

    [UI+Core] seperate method: get amount of money + convert String to int

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,13 +4,26 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
+    private static final int LOTTO_PRICE = 1000;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
 
-    public static int getPriceToBuy() {
+    public static void getPriceToBuy() {
         System.out.println("구입 금액을 입력해주세요.");
         String response = Console.readLine();
-        return Integer.parseInt(response);
+        stringToInt(response);
     }
+
+    public static int stringToInt(String response) {
+        try {
+            return Integer.parseInt(response);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 정수로 입력해야 합니다.");
+        }
+    }
+
+
 }
