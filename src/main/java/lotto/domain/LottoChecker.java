4f5b615fa8e2b97c451a05c8a5b42e10626c0898
KimSangHyeok <tkdgur0906@naver.com>
2022-11-11 23:09:39 +0900

    Feat(lotto): 수익률 구하는 기능 추가

--- src/main/java/lotto/domain/LottoChecker.java
+++ src/main/java/lotto/domain/LottoChecker.java
@@ -6,20 +6,24 @@ import lotto.WinningLotto;
 import java.util.List;
 
 public class LottoChecker {
-    public int lottoCompare(Lotto userLotto, WinningLotto winningLotto){
+    public int lottoCompare(Lotto userLotto, WinningLotto winningLotto) {
         List<Integer> lotto = userLotto.getNumbers();
         Lotto winningNumbers = winningLotto.getWinningNumbers();
         int sameCount = 0;
 
         for (Integer number : lotto) {
-            if(winningNumbers.getNumbers().contains(number)){
+            if (winningNumbers.getNumbers().contains(number)) {
                 sameCount++;
             }
         }
         return sameCount;
     }
 
-    public boolean bonusCompare(Lotto userLotto, WinningLotto winningLotto){
+    public boolean bonusCompare(Lotto userLotto, WinningLotto winningLotto) {
         return userLotto.getNumbers().contains(winningLotto.getBonusNumber());
+    }
+
+    public double profitRate(int principal, int profit) {
+        return Math.round((double) profit / (double) principal * 1000) / 10.0;
     }
 }
