f30163000f34e0eaa065ae2dfa8c4db155f42cec
kkm5291 <kkm23125291@gmail.com>
2022-11-11 14:08:46 +0900

    [FEAT] Check : 당첨번호 중복 여부 확인 메서드 추가

--- src/main/java/validate/Check.java
+++ src/main/java/validate/Check.java
@@ -5,10 +5,19 @@ import java.util.List;
 public class Check {
 
     public static int countLottoTicket(int money) {
+        int tickets = money / 1000;
+
         if (money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 올바른 금액이 아닙니다. (지폐만 가능)");
         }
 
-        return money / 1000;
+        return tickets;
+    }
+
+    public static void winningNumberDuplicate(List<String> numbers, String number) {
+
+        if (numbers.contains(number)) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자는 잘못된 입력값 입니다.");
+        }
     }
 }
