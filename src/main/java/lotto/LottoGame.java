f95dc74f8b5825bab0d83e9d87a3d745d741b3cd
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-11 23:14:22 +0900

    feat: 로또 구매로직 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -10,6 +10,11 @@ public class LottoGame {
     private static final String TYPE_ERROR_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
     private static final String INPUT_MONEY_ERROR_MESSAGE = "[ERROR] 금액을 1000원 단위로 입력해주세요.";
 
+    public List<Lotto> buyLotto() {
+        int money = inputMoney();
+        int lottoNum = countLotto(money);
+        return pickLottoList(lottoNum);
+    }
 
     public List<Lotto> pickLottoList(int count) {
         List<Lotto> lottoList = new ArrayList<>();
