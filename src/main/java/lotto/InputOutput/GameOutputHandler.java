b7a7897690d4aac5d505f68a87b1df6c6ccfcac4
WhalesBob <ndw8200@naver.com>
2022-11-11 14:08:18 +0900

    feat(GameOutput) : make printProfitRate Method and put last two print together

--- src/main/java/lotto/InputOutput/GameOutputHandler.java
+++ src/main/java/lotto/InputOutput/GameOutputHandler.java
@@ -7,12 +7,13 @@ import java.util.Arrays;
 import java.util.Map;
 
 public class GameOutputHandler {
+    static int totalEarnMoney;
+
     public void requestHowMuchToBuy(){
         System.out.println("구입 금액을 입력해 주세요.");
     }
     public void checkHowMuchToBuy(int amountOfLotto){
         System.out.printf("\n%d개를 구매했습니다.\n",amountOfLotto);
-
     }
     public void printAllLottoPapers(Lotto[] allLottoPapers){
         for (Lotto lottoPaper : allLottoPapers) {
@@ -31,8 +32,13 @@ public class GameOutputHandler {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
+    public void printAllRecord(Map<Integer, Integer> winningCounts, int buyingMoney){
+        printWinningRecord(winningCounts);
+        printProfitRate(buyingMoney);
+    }
+
     public void printWinningRecord(Map<Integer, Integer> winningCounts){
-        // Integer : 등수, Integer : 갯수
+        totalEarnMoney = 0;
         System.out.print("당첨 통계\n---\n");
         for(int ranking = 5; ranking >= 1; ranking--){
             int howManyNumbersCorrect = WinningPrize.findHowManyNumbers(ranking).getHowManyNumbersIncluded();
@@ -45,9 +51,16 @@ public class GameOutputHandler {
                 amountOfThatRanking = 0;
             }
 
+            totalEarnMoney += (winningMoney * amountOfThatRanking);
             boolean isSecondRank = (ranking == 2);
             printWinningRecordOneByOne(howManyNumbersCorrect,winningMoney,amountOfThatRanking,isSecondRank);
         }
+    }
+
+    private void printProfitRate(int buyingMoney){
+        double rate = ((double)totalEarnMoney / (double)buyingMoney) * 100;
+        double roundOff = Math.round(rate * 100.0) / 100.0;
+        System.out.print("총 수익률은 " + roundOff + "% 입니다.");
     }
     private void printWinningRecordOneByOne(int numbersCorrect, int winningMoney, int amountOfThatRanking, boolean isSecondRank){
         StringBuilder printMake = new StringBuilder();
