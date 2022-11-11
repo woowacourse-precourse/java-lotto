f9797dc38754f3f2becd6951a16b956df7b43426
Donghyun <flaehan@gmail.com>
2022-11-12 08:30:25 +0900

    type : docs 📚 기능목록 작성
    body : 예외사항 추가
    
    footer :

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,22 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers);
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != Set.copyOf(numbers).size()) {
+            throw new IllegalArgumentException(
+            );
         }
     }
 
-    // TODO: 추가 기능 구현
 }
