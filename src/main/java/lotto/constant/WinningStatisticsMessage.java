aa16e64556d3b57a470addd8e641aef78a4b0a75
yubin <dbqls2821@gmail.com>
2022-11-11 14:21:35 +0900

    feat(WinningStatisticsMessage): 당첨 통계를 enum에서 출력

--- src/main/java/lotto/constant/WinningStatisticsMessage.java
+++ src/main/java/lotto/constant/WinningStatisticsMessage.java
@@ -2,17 +2,28 @@ package lotto.constant;
 
 public enum WinningStatisticsMessage {
 
-  NUMBER("개"),
+  WINNING_STATISTICS("당첨 통계\n---"),
   SAME_THREE_NUMBER("3개 일치 (5,000원) - "),
   SAME_FOUR_NUMBER("4개 일치 (50,000원) - "),
   SAME_FIVE_NUMBER("5개 일치 (1,500,000원) - "),
   SAME_FIVE_AND_BONUS_NUMBER("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-  SAME_SIX_NUMBER("6개 일치 (2,000,000,000원) - ");
+  SAME_SIX_NUMBER("6개 일치 (2,000,000,000원) - "),
+  NUMBER("개");
 
   private String message;
 
   WinningStatisticsMessage(String message) {
     this.message = message;
+  }
+
+  //TODO 인자의 갯수를 줄일 수 없을까
+  public void print(int same3Num, int same4Num, int same5Num, int same5NumAndBonus, int same6Num) {
+    System.out.println(WINNING_STATISTICS);
+    System.out.println(SAME_THREE_NUMBER + Integer.toString(same3Num) + NUMBER);
+    System.out.println(SAME_FOUR_NUMBER + Integer.toString(same4Num) + NUMBER);
+    System.out.println(SAME_FIVE_NUMBER + Integer.toString(same5Num) + NUMBER);
+    System.out.println(SAME_FIVE_AND_BONUS_NUMBER + Integer.toString(same5NumAndBonus) + NUMBER);
+    System.out.println(SAME_SIX_NUMBER + Integer.toString(same6Num) + NUMBER);
   }
 
 }
