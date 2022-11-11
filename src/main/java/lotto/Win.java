20be4088d65df44f444e836537a237edd3d00c6e
dohyun <ldh19738@gmail.com>
2022-11-11 19:11:06 +0900

    feat(Win): 당첨 번호와 보너스 번호의 일치 개수 세는 기능 구현

--- src/main/java/lotto/Win.java
+++ src/main/java/lotto/Win.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 public class Win {
     public void threeMatchMessage() {
         System.out.println("3개 일치 (5,000원) - ");
@@ -19,5 +21,27 @@ public class Win {
 
     public void sixMatchMessage() {
         System.out.println("6개 일치 (2,000,000,000원) - ");
+    }
+
+    public int numberThatMatchesWinNumber(List<Integer> lottoNumbers, List<Integer> winNumbers) {
+        int matchCount = 0;
+
+        for (int winNumber : winNumbers) {
+            if (lottoNumbers.contains(winNumber)) {
+                matchCount++;
+            }
+        }
+
+        return matchCount;
+    }
+
+    public int numberThatMatchesBonusNumber(List<Integer> lottoNumbers, int bonusNumber) {
+        int bonusMatchCount = 0;
+
+        if (lottoNumbers.contains(bonusNumber)) {
+            bonusMatchCount++;
+        }
+
+        return bonusMatchCount;
     }
 }
