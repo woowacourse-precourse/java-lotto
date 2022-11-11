8a125c5fc720f65f53500af41d123b9a7c8a389a
kdkdhoho <hkim4410@naver.com>
2022-11-11 16:32:16 +0900

    feat(OutputUtil): printInputPurchaseAmount() 추가

--- src/main/java/lotto/util/OutputUtil.java
+++ src/main/java/lotto/util/OutputUtil.java
@@ -9,6 +9,10 @@ import java.util.Map;
 
 public class OutputUtil {
 
+    public void printInputPurchaseAmount() {
+        System.out.println(Message.INPUT_PURCHASE_AMOUNT_MESSAGE.getMessage());
+    }
+
     public void printUserLottoCount(int lottoCount) {
         System.out.println("" + lottoCount + "개를 구매했습니다.");
     }
