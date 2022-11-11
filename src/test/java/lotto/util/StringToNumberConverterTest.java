9f34fa14c85da18a28cc3a43528fa3979472ed06
woo-chang <clllickme@naver.com>
2022-11-11 16:24:07 +0900

    test(StringToNumberConverter): 문자열을 숫자로 변환하는 기능 테스트

---
+++ src/test/java/lotto/util/StringToNumberConverterTest.java
@@ -0,0 +1,38 @@
+package lotto.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StringToNumberConverterTest {
+
+    @DisplayName("문자열을 숫자로 변환한다.")
+    @Test
+    void convertStringToNumber() {
+        //given
+        Converter<String, Integer> converter = new StringToNumberConverter();
+        String target = "123";
+        Integer number = 123;
+
+        //when
+        Integer result = converter.convert(target);
+
+        //then
+        assertThat(result).isEqualTo(number);
+    }
+
+    @DisplayName("문자열이 숫자로 변환할 수 없으면 IllegalArgumentException이 발생한다.")
+    @Test
+    void convertFail() {
+        //given
+        Converter<String, Integer> converter = new StringToNumberConverter();
+        String target = "12a";
+
+        //then
+        assertThatThrownBy(() -> converter.convert(target)).isInstanceOf(
+            IllegalArgumentException.class);
+    }
+
+}
