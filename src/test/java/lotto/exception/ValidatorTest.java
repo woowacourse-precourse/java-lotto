90b0d24081c1c870daa740c8abc1d8ceb3835b7f
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-11 13:43:43 +0900

    fix: Validator - validateValueRange 변경으로 테스트코드 깨지던 문제 수정
    상수 변경으로 해결

--- src/test/java/lotto/exception/ValidatorTest.java
+++ src/test/java/lotto/exception/ValidatorTest.java
@@ -20,7 +20,7 @@ public class ValidatorTest {
     public static final String NUMBER_NOT_DIVISIBLE_BY_ONE_THOUSAND = "2500";
 
     public static final String VALUE_IS_NOT_USE_COMMA = "123";
-    public static final String VALUE_IS_NOT_NUMBER_USE_COMMA = "가,나,다";
+    public static final String VALUE_IS_NOT_NUMBER_USE_COMMA = "가,나,다,1,4,5";
     public static final String VALUE_SIZE_IS_NOT_SIX = "1,2,3";
     public static final String VALUE_RANGE_IS_NOT_RIGHT_USE_COMMA = "1,2,3,4,5,46";
 
@@ -75,20 +75,21 @@ public class ValidatorTest {
                     .hasMessage(NEED_TO_USE_COMMA);
         }
 
-        @DisplayName("숫자가 아니면 예외를 반환한다")
-        @Test
-        void test2() {
-            assertThatThrownBy(() -> validator.validateWinningNumbers(VALUE_IS_NOT_NUMBER_USE_COMMA))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessage(NEED_TO_INPUT_NUMBER);
-        }
-
         @DisplayName("숫자가 6자리가 아니면 예외를 반환한다")
         @Test
-        void test3() {
+        void test2() {
             assertThatThrownBy(() -> validator.validateWinningNumbers(VALUE_SIZE_IS_NOT_SIX))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage(NEED_TO_INPUT_SIX_NUMBER);
+        }
+
+
+        @DisplayName("숫자가 아니면 예외를 반환한다")
+        @Test
+        void test3() {
+            assertThatThrownBy(() -> validator.validateWinningNumbers(VALUE_IS_NOT_NUMBER_USE_COMMA))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(NEED_TO_INPUT_NUMBER);
         }
 
         @DisplayName("범위가 벗어나는 숫자가 있다면 예외를 반환한다.")
