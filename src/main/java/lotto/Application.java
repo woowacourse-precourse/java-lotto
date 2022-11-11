2b81dd0429647006537f63a03f51635c5d139eac
jin-chang-ho <ckdgh8204@naver.com>
2022-11-12 02:06:00 +0900

    feat: 사용자로부터 로또 구입 금액을 입력 받음

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import java.util.*;
+
 public class Application {
+    static String how_much_money() {
+        Scanner scanner = new Scanner(System.in);
+
+        String user_money;
+        user_money = scanner.nextLine();
+
+        return user_money;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String user_money;
+        user_money = how_much_money();
     }
 }
