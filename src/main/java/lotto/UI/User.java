bdef57adda837e0c9008d06f53bb58a05a417093
JunTaeINC <juntea3523@naver.com>
2022-11-11 23:14:17 +0900

    feat : (UI.User) 유저에게 보너스 번호 입력 받는기능 구현

--- src/main/java/lotto/UI/User.java
+++ src/main/java/lotto/UI/User.java
@@ -6,6 +6,7 @@ import lotto.domain.Lotto;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.SimpleTimeZone;
 
 public class User {
 
@@ -27,21 +28,29 @@ public class User {
 
     public static List<Integer> lottoNumberReceiveInput() {
         System.out.println("당첨 번호를 입력해 주세요.");
-
         String[] lineTest = Console.readLine().split(",");
         List<Integer> lottoNumbers = new ArrayList<>();
-
         for (int length = 0; length < lineTest.length; length++) {
             try {
                 lottoNumbers.add(Integer.parseInt(lineTest[length]));
             } catch (NumberFormatException numberFormatException) {
-                throw new IllegalArgumentException("[ERROR] 문자 또는 공백은 입력이 불가능합니다. 1~45 사이의 숫자를 입력해 주세요.");
+                throw new IllegalArgumentException("[ERROR] 문자 또는 공백은 입력이 불가능합니다. 1~45 사이의 숫자 6개를 입력해 주세요.");
             }
         }
         new Lotto(lottoNumbers);
         Collections.sort(lottoNumbers);
         return lottoNumbers;
+    }
 
+    public static Integer inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        try {
+            String inputNumber = Console.readLine();
+            Integer bonusNumber = Integer.parseInt(inputNumber);
+            return bonusNumber;
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException("[ERROR] 문자 또는 공백은 입력이 불가능합니다. 1~45 사이의 숫자 한개를 입력해 주세요.");
+        }
     }
 
 }
