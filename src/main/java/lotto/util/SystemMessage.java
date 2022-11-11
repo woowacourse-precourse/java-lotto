d441b7a522a2859ae0f2161735cb6d212c0ac0fe
kki4504 <rhkddlf9876@gmail.com>
2022-11-11 15:53:19 +0900

    feat(SystemMessage) : HORIZON 추가

--- src/main/java/lotto/util/SystemMessage.java
+++ src/main/java/lotto/util/SystemMessage.java
@@ -2,15 +2,16 @@ package lotto.util;
 
 public enum SystemMessage {
     MONEY_INPUT("구입금액을 입력해 주세요."),
-    PURCHASES_RESULT("개를 구매했습니다."),
+    PURCHASES_RESULT("%d개를 구매했습니다."),
     WIN_NUMBER_INPUT("당첨 번호를 입력해 주세요."),
     BONUS_NUMBER_INPUT("보너스 번호를 입력해 주세요"),
     WIN_STATS("당첨 통계"),
-    FIRST_PRIZE("6개 일치 (2,000,000,000원) - "),
-    SECOND_PRIZE("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    THIRD_PRIZE("5개 일치 (1,500,000원) - "),
-    FOURTH_PRIZE("4개 일치 (50,000원) - "),
-    FIFTH_PRIZE("3개 일치 (5,000원) - "),
+    HORIZON("---"),
+    FIRST_PRIZE("6개 일치 (2,000,000,000원) - %d"),
+    SECOND_PRIZE("5개 일치, 보너스 볼 일치 (30,000,000원) - %d"),
+    THIRD_PRIZE("5개 일치 (1,500,000원) - %d"),
+    FOURTH_PRIZE("4개 일치 (50,000원) - %d"),
+    FIFTH_PRIZE("3개 일치 (5,000원) - %d"),
     _BLANK("");
 
     private String content;
