2f98e91358bbdc93e9ed051e1270fbdccf3c9128
Eungyu-code <Agyu625@gmail.com>
2022-11-11 18:02:57 +0900

    fix(printGradeStats()): 출력 문구 수정
    잘못된 출력 문구로 인한 에러를 수정을 통해 해결
    
    - 수정 전
    System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) = " + second + "개");
    
    - 수정 후
    System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + second + "개");

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -64,7 +64,7 @@ public class Application {
         System.out.println("3개 일치 (5,000원) - " + fifth + "개");
         System.out.println("4개 일치 (50,000원) - " + forth + "개");
         System.out.println("5개 일치 (1,500,000원) - " + third + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) = " + second + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + second + "개");
         System.out.println("6개 일치 (2,000,000,000원) - " + first + "개");
         System.out.println("총 수익률은 " + surplusRate + "%입니다.");
     }
