de26fad6d5793f1b1f9c61393e0d5e54340e1546
박승민 <tmddudals369@naver.com>
2022-11-12 03:53:06 +0900

    test: 당첨 결과 구하기 테스트

---
+++ src/test/java/lotto/domain/ResultTest.java
@@ -0,0 +1,97 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.List;
+import lotto.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ResultTest {
+    @Test
+    @DisplayName("당첨 번호와 로또 번호가 모두 일치할 경우 1등")
+    void testName() {
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+
+        Result result = new Result();
+        result.initRank(winningNumber, bonusNumber, lottos);
+        HashMap<Integer, Integer> rank = result.getRank();
+
+        assertThat(rank.get(1)).isEqualTo(1);
+        assertThat(rank.get(2)).isEqualTo(0);
+        assertThat(rank.get(3)).isEqualTo(0);
+        assertThat(rank.get(4)).isEqualTo(0);
+        assertThat(rank.get(5)).isEqualTo(0);
+    }
+    @Test
+    @DisplayName("당첨 번호와 로또 번호가 5개 그리고 보너스 번호가 일치할 경우 2등")
+    void testName2() {
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 7)));
+
+        Result result = new Result();
+        result.initRank(winningNumber, bonusNumber, lottos);
+        HashMap<Integer, Integer> rank = result.getRank();
+
+        assertThat(rank.get(1)).isEqualTo(0);
+        assertThat(rank.get(2)).isEqualTo(1);
+        assertThat(rank.get(3)).isEqualTo(0);
+        assertThat(rank.get(4)).isEqualTo(0);
+        assertThat(rank.get(5)).isEqualTo(0);
+    }
+    @Test
+    @DisplayName("당첨 번호와 로또 번호가 5개가 일치할 경우 3등")
+    void testName3() {
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 8)));
+
+        Result result = new Result();
+        result.initRank(winningNumber, bonusNumber, lottos);
+        HashMap<Integer, Integer> rank = result.getRank();
+
+        assertThat(rank.get(1)).isEqualTo(0);
+        assertThat(rank.get(2)).isEqualTo(0);
+        assertThat(rank.get(3)).isEqualTo(1);
+        assertThat(rank.get(4)).isEqualTo(0);
+        assertThat(rank.get(5)).isEqualTo(0);
+    }
+    @Test
+    @DisplayName("당첨 번호와 로또 번호가 4개가 일치할 경우 4등")
+    void testName4() {
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 9, 10)));
+
+        Result result = new Result();
+        result.initRank(winningNumber, bonusNumber, lottos);
+        HashMap<Integer, Integer> rank = result.getRank();
+
+        assertThat(rank.get(1)).isEqualTo(0);
+        assertThat(rank.get(2)).isEqualTo(0);
+        assertThat(rank.get(3)).isEqualTo(0);
+        assertThat(rank.get(4)).isEqualTo(1);
+        assertThat(rank.get(5)).isEqualTo(0);
+    }
+    @Test
+    @DisplayName("당첨 번호와 로또 번호가 3개가 일치할 경우 5등")
+    void testName5() {
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 11, 15, 16)));
+
+        Result result = new Result();
+        result.initRank(winningNumber, bonusNumber, lottos);
+        HashMap<Integer, Integer> rank = result.getRank();
+
+        assertThat(rank.get(1)).isEqualTo(0);
+        assertThat(rank.get(2)).isEqualTo(0);
+        assertThat(rank.get(3)).isEqualTo(0);
+        assertThat(rank.get(4)).isEqualTo(0);
+        assertThat(rank.get(5)).isEqualTo(1);
+    }
+}
