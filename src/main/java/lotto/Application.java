560aaff26b166db32d494b71ad63f74f77c86452
MIN-04 <kimji_0424@naver.com>
2022-11-11 14:56:04 +0900

    refactor, feat(Application.java): modify 사용자 입력 변수명, add 기능 4
    Modified userInput -> inputMoney:
    - 구입 금액을 받는 의미를 더 명확하게 알 수 있게 수정
    - 입력 금액 유효성 체크 메서드명 수정
    
    Added 기능 목록 4 (4. 당첨 번호를 입력 받는다.):
    - 당첨번호 입력
    - 유효성 체크 method validateInputWinningNums()
    - 보너스번호 입력
    - 유효성 체크 method validateInputBonusNum()

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,12 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
 
     static String NUMBER_REG = "/^[0-9]+$/";
+    static String LOTTO_NUMBER_REG = "^[1-9]{1}$|^[1-3]{1}[0-9]{1}$|^4{1}[0-5]{1}$";
     public static void main(String[] args) {
         // TODO: Java Enum 만들기
         /*- 1. 로또 구입 금액을 입력 받는다.
@@ -22,15 +25,17 @@ public class Application {
             - 로또 번호는 오름차순으로 정렬하여 보여준다.
           - 4. 당첨 번호를 입력 받는다.
             - 중복되지 않는 숫자 6개 (번호는 (,) 기준으로 구분한다.)
+            - 사용자가 잘못된 당첨 번호를 입력할 경우 IllegalArgumentException를 발생시키고, "[ERROR]"로 시작하는 에러 메시지를 출력 후 종료한다.
             - 보너스 숫자 1개
+            - 사용자가 잘못된 보너스 숫자를 입력할 경우 IllegalArgumentException를 발생시키고, "[ERROR]"로 시작하는 에러 메시지를 출력 후 종료한다.
           - 5. 당첨 내역을 출력한다.
             - n개 일치 (당첨 금액) - 해당 금액 총 당첨 개수
             - 총 수익률 (소수점 둘째 자리에서 반올림)*/
 
         // 1.
         System.out.println("구입금액을 입력해 주세요.");
-        String userInput = Console.readLine();
-        int money = validateUserInput(userInput);
+        String inputMoney = Console.readLine();
+        int money = validateInputMoney(inputMoney);
 
         // 2.
         List<Lotto> lottos = createLottos(money);
@@ -38,17 +43,49 @@ public class Application {
         // 3.
         printLottos(money, lottos);
 
+        // 4.
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String inputWinningNums = Console.readLine();
+        validateInputWinningNums(inputWinningNums);
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String inputBonusNum = Console.readLine();
+        validateInputBonusNum(inputBonusNum);
+
+        List<Integer> winningNums = parseInputWinningNumsIntoInteger(inputWinningNums);
+        int bonusNum = Integer.parseInt(inputBonusNum);
 
 
     }
 
-    private static int validateUserInput(String userInput) {
-        if (!userInput.matches(NUMBER_REG)) {
+    private static int validateInputMoney(String inputMoney) {
+        if (!inputMoney.matches(NUMBER_REG)) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
-        } else if (Integer.parseInt(userInput) % 1000 != 0) {
+        } else if (Integer.parseInt(inputMoney) % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위만 입력할 수 있습니다.");
         }
-        return Integer.parseInt(userInput);
+        return Integer.parseInt(inputMoney);
+    }
+
+    private static void validateInputWinningNums(String inputWinningNums) {
+        if (inputWinningNums.contains(",")) {
+            throw new IllegalArgumentException("[ERROR] 구분자 \",\"를 포함해야 합니다.");
+        }
+        String[] inputWinningNumsArr = inputWinningNums.split(",");
+        if (inputWinningNumsArr.length < 6) {
+            throw new IllegalArgumentException("[ERROR] 숫자 6개를 입력해야 합니다.");
+        }
+        for (String s : inputWinningNumsArr) {
+            if (!s.matches(LOTTO_NUMBER_REG)) {
+                throw new IllegalArgumentException("[ERROR] 숫자는 1~45까지만 입력할 수 있습니다.");
+            }
+        }
+    }
+
+    private static void validateInputBonusNum(String inputBonusNum) {
+        if (!inputBonusNum.matches(NUMBER_REG)) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+        }
     }
 
     private static List<Lotto> createLottos(int money) {
@@ -67,5 +104,12 @@ public class Application {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    private static List<Integer> parseInputWinningNumsIntoInteger(String inputWinningNums) {
+        List<String> inputWinningNumsList = Arrays.asList(inputWinningNums.split(","));
+        return inputWinningNumsList.stream()
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
     }
 }
