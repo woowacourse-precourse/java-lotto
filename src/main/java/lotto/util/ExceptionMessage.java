36ce6e7a043f6fb75fc54689cb77f702ab61fea1
woosung1223 <dntjd991223@naver.com>
2022-11-11 16:01:10 +0900

    fix(ExceptionMessage): NUMBER_EACH_OUT_OF_RANGE가 정상 값을 참조하도록 수정

--- src/main/java/lotto/util/ExceptionMessage.java
+++ src/main/java/lotto/util/ExceptionMessage.java
@@ -14,7 +14,7 @@ public final class ExceptionMessage {
             Constant.LOTTO_SIZE + "개의 숫자가 아닙니다.";
     public static final String EACH_NOT_NUMERIC = "[ERROR] 입력된 당첨번호는 각각이 숫자여야 합니다.";
     public static final String NUMBER_EACH_OUT_OF_RANGE = "[ERROR] 입력된 당첨번호 각각은 " +
-            Constant.LOTTO_SIZE + "부터 " + Constant.END_INCLUSIVE + "까지의 값이어야 합니다";
+            Constant.START_INCLUSIVE + "부터 " + Constant.END_INCLUSIVE + "까지의 값이어야 합니다";
     public static final String NUMBER_NOT_DISTINCT = "[ERROR] 당첨번호는 중복되서는 안됩니다.";
     public static final String NUMBER_OUT_OF_TYPE_RANGE = "[ERROR] 입력된 당첨번호는 정상적인 범위의 값이 아닙니다.";
 }
