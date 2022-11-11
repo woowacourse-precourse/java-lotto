1d28e4d8c274a56d840c677096e5a0757af3368b
Joon <orijoon98@gmail.com>
2022-11-11 13:54:27 +0900

    feat: add print buy lotto method

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -5,6 +5,7 @@ public class Print {
     private final static String INPUT_PRICE = "구입금액을 입력해 주세요.";
     private final static String INPUT_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
     private final static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private final static String BUY_LOTTO = "개를 구매했습니다.";
 
     public static void inputPrice() {
         System.out.println(INPUT_PRICE);
@@ -20,5 +21,10 @@ public class Print {
 
     public static void newLine() {
         System.out.println();
+    }
+
+    public static void buyLotto(int count) {
+        System.out.print(count);
+        System.out.println(BUY_LOTTO);
     }
 }
