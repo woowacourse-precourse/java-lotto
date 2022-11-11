20c27491eda98b2281e5b8b2827cdad94121ea19
woosung1223 <dntjd991223@naver.com>
2022-11-11 20:55:17 +0900

    feat(LottoSystem): private 변수 선언부 추가

--- src/main/java/lotto/controller/LottoSystem.java
+++ src/main/java/lotto/controller/LottoSystem.java
@@ -1,4 +1,15 @@
 package lotto.controller;
 
-public class LottoSystem {
+import lotto.domain.BonusNumber;
+import lotto.domain.UserLotto;
+import lotto.domain.WinningLotto;
+
+public final class LottoSystem {
+    private final UserLotto userLotto = new UserLotto();
+    private final WinningLotto winningLotto = new WinningLotto();
+    private final BonusNumber bonusNumber = new BonusNumber();
+
+    public void run() {
+        userLotto.printMyLotto();
+    }
 }
