71a906254ced5dfc4de945030cd8082d323cd725
JunTaeINC <juntea3523@naver.com>
2022-11-11 22:14:38 +0900

    feat : (domain.Lotto) 로또 규정에 대한 예외 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,12 +9,31 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         sizeCheck(numbers);
+        duplicationCheck(numbers);
+        numberRangeCheck(numbers);
         this.numbers = numbers;
     }
 
     private static List<Integer> sizeCheck(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 수로 이루어져 있습니다.");
+        }
+        return numbers;
+    }
+
+    private static List<Integer> numberRangeCheck(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (1 > number || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호의 숫자 범위는 1~45까지 입니다.");
+            }
+        }
+        return numbers;
+    }
+
+    private static List<Integer> duplicationCheck(List<Integer> numbers) {
+        Set<Integer> duplicationTestSet = new HashSet<>(numbers);
+        if (duplicationTestSet.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 입력 하신 번호중 중복이 있습니다.");
         }
         return numbers;
     }
