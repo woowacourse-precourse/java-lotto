702e5947f622cf3b1da7c35a2f65f5a0029bd113
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-11 15:51:37 +0900

    fix(Statistics): Fix field type, constructor access type
    Feature fixed:
    - Fix price field type to Long
    - Set getter method to access price field

--- src/main/java/lotto/service/Statistics.java
+++ src/main/java/lotto/service/Statistics.java
@@ -4,20 +4,24 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum Statistics {
-    FIRST(6, false, 2_000_000_000),
-    SEC(5, true,30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
-    FIFTH(3, false, 5_000),;
+    FIRST(6, false, 2_000_000_000L),
+    SEC(5, true,30_000_000L),
+    THIRD(5, false, 1_500_000L),
+    FOURTH(4, false, 50_000L),
+    FIFTH(3, false, 5_000L),;
 
     private final int matchNums;
     private final boolean bonus;
-    private final int price;
+    private final Long price;
 
-    private Statistics(int matchNums, boolean bonus, int price) {
+    Statistics(int matchNums, boolean bonus, Long price) {
         this.matchNums = matchNums;
         this.bonus = bonus;
         this.price = price;
+    }
+
+    public Long getPrice() {
+        return price;
     }
 
     public static Optional<Statistics> getRank(int num, boolean bonus) {
