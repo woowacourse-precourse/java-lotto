36ec3e6c594b8ee17e9c6f82f9bc2fa712f098c4
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-12 00:22:06 +0900

    feat : 사용자가 당첨 번호를 입력하는 기능 구현
    LottoCompany 클래스 생성
    winningNumbers()구현
    makeWinningNumbers()구현

---
+++ src/main/java/lotto/LottoCompany.java
@@ -0,0 +1,43 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class LottoCompany {
+
+    private final int LENGTH=6;
+
+    Validator validator = new Validator();
+    Result result = new Result();
+    Map<Integer, LottoNumber> lottoNumberMap = new LottoNumberGenerater().makeLottoNumberMap();
+    List<Integer>forValidate = new ArrayList<>();
+    public Lottos lottos;
+    public Lotto lotto;
+    public LottoNumber bonus;
+
+    public LottoCompany(){
+
+    }
+
+    public void winningNumbers(Lottos lottos){
+        this.lottos = lottos;
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningNumbers = Console.readLine();
+        makeWinningNumbers(winningNumbers);
+    }
+
+    public void makeWinningNumbers(String numbers) {
+        String [] winningNumbers = numbers.split(",");
+        Arrays.sort(winningNumbers);
+        validate(winningNumbers);
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        for(int order=0; order<LENGTH;order++){
+            lottoNumbers.add(lottoNumberMap.get(Integer.parseInt(winningNumbers[order])));
+        }
+        this.lotto = new Lotto(lottoNumbers);
+        bonusNumber();
+    }
+}
