60cf7446cdd7b3edf5d8c370addd1066a74c237d
Joon <orijoon98@gmail.com>
2022-11-11 13:39:38 +0900

    feat: add Print class

---
+++ src/main/java/lotto/Print.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class Print {
+
+    private final static String INPUT_PRICE = "구입금액을 입력해 주세요.";
+    private final static String INPUT_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private final static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+}
