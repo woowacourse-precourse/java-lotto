03c96d6fc1561b33b54b21d55ce53769e0b10b64
anneu <b.imnuj@gmail.com>
2022-11-11 19:31:50 +0900

    refactor: 검증 관련 로직을 Validator 클래스로 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,17 +1,9 @@
 package lotto;
 
-import static lotto.utils.ConstantUtil.LOTTO_NUMBER_SIZE;
-import static lotto.utils.ConstantUtil.MAX_LOTTO_NUMBER;
-import static lotto.utils.ConstantUtil.MIN_LOTTO_NUMBER;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Lotto {
-    private static final String SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호의 개수는 6개이어야 합니다.";
-    private static final String DUPLICATES_ERROR_MESSAGE = "[ERROR] 로또 번호에 중복된 숫자가 있습니다.";
-    private static final String RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호에 1 ~ 45 범위를 벗어난 숫자가 있습니다.";
-
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -26,36 +18,10 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        validateNumberSize(numbers);
-        validateDuplicateNumber(numbers);
-        validateNumberRange(numbers);
-    }
+        Validator validator = new Validator();
 
-    private void validateNumberSize(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_NUMBER_SIZE) {
-            throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
-        }
-    }
-
-    private void validateDuplicateNumber(List<Integer> numbers) {
-        List<Integer> distinctNumbers = numbers.stream()
-                .distinct()
-                .collect(Collectors.toList());
-
-        if (numbers.size() != distinctNumbers.size()) {
-            throw new IllegalArgumentException(DUPLICATES_ERROR_MESSAGE);
-        }
-    }
-
-    private void validateNumberRange(List<Integer> numbers) {
-        for (Integer number : numbers) {
-            if (isNotValidRange(number)) {
-                throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
-            }
-        }
-    }
-
-    private boolean isNotValidRange(int number) {
-        return (!(MIN_LOTTO_NUMBER <= number && number <= MAX_LOTTO_NUMBER));
+        validator.validateNumberSize(numbers);
+        validator.validateDuplicateNumber(numbers);
+        validator.validateNumberRange(numbers);
     }
 }
