6beb71f6c641e80ba2b35e737992a44c51c8c042
ehs2803 <hr2803@naver.com>
2022-11-11 12:18:04 +0900

    feat : 당첨 번호 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    private static List<Integer> lottoNumbers = new ArrayList<>();
+    private static int lottoBonusNumber;
+
     // 구입금액 입력
     private static int inputBuyMoney(){
         System.out.println("구입금액을 입력해 주세요.");
@@ -41,6 +44,23 @@ public class Application {
         System.out.println();
     }
 
+    // 당첨 번호 입력
+    private static void inputLottoNumbers(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        try {
+            String[] inputLottoNumbers = Console.readLine().split(",");
+            for(String number : inputLottoNumbers){
+                lottoNumbers.add(Integer.parseInt(number));
+            }
+        } catch (IllegalArgumentException exception){
+            throw new IllegalArgumentException("[ERROR] 잘못된 형식의 입력입니다.");
+        }
+
+        if(lottoNumbers.size()!=6){
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개를 입력해야 합니다.");
+        }
+    }
+
     public static void main(String[] args) {
         // 구입금액 입력
         int lottoCnt = inputBuyMoney()/1000;
@@ -50,5 +70,8 @@ public class Application {
 
         // 로또 번호 출력
         printLottoNumbers(userLottoNum);
+
+        // 당첨 번호 입력
+        inputLottoNumbers();
     }
 }
