6673f6eb821b36f4af64c9fa0f73c72ae2eada2e
HamSeungChan <head0618@naver.com>
2022-11-11 18:29:20 +0900

    "feat : "로또 금액을 입력 받고 발행할 로또 갯수 구하는 기능 구현"

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+  static final int LOTTO_AMOUNT = 1000;
+
+  public static void main(String[] args) {
+    // TODO: 프로그램 구현
+  }
+
+  private static int enterPurchaseAmount() {
+    System.out.println("구매금액을 입력해 주세요.");
+    String purchaseAmount = Console.readLine();
+    return Integer.parseInt(purchaseAmount);
+  }
+
+  private static int getNumberOfBuyLotto(int purchaseAmount) {
+    return purchaseAmount / LOTTO_AMOUNT;
+  }
 }
