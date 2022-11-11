cd2067406153c2e10b1199aa39db978e49fd3ff1
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-11 17:24:36 +0900

    test(LottoPaper) : testcase

--- src/main/java/lotto/LottoPaper.java
+++ src/main/java/lotto/LottoPaper.java
@@ -5,13 +5,14 @@ import java.util.List;
 public class LottoPaper {
     private final List<Integer> number;
     public LottoPaper(List<Integer> number){
+
         this.number = number;
     }
     public int getResult(List<Integer> winningNumber, int bonusNumber){
         if(this.number.contains(bonusNumber)){
             this.number.retainAll(winningNumber);
             if(this.number.size() == 5){
-                return 6;
+                return 7;
             }
         }
         this.number.retainAll(winningNumber);
--- src/test/java/lotto/LottoPaperTest.java
+++ src/test/java/lotto/LottoPaperTest.java
@@ -2,12 +2,18 @@ package lotto;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 class LottoPaperTest {
 
     @Test
     void getResult() {
-
+        LottoPaper lottoPaper = new LottoPaper(new ArrayList<>(List.of(1, 2, 45, 40, 50, 10)));
+        List<Integer> testLotto = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+        int bonus = 10;
+        assertThat(lottoPaper.getResult(testLotto, bonus)).isEqualTo(2);
     }
 }
