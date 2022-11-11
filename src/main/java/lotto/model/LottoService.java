2fc658cd3c35138d3a69b54067f405d0edb6c4e5
siwookim97 <tldn12gh@gmail.com>
2022-11-12 02:38:54 +0900

    feat: 발급한 로또 번호를 오름차순으로 정리하는 기능 추가
    발급한 로또 번호를 오름차순으로 정리하는 기능 추가
     - List<Integer> 형태의 인스턴스를 lotto 인스턴스 생성전에 정렬

--- src/main/java/lotto/model/LottoService.java
+++ src/main/java/lotto/model/LottoService.java
@@ -2,38 +2,41 @@ package lotto.model;
 
 import lotto.util.Utils;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoService {
-    private List<Lotto> lottoList;
+    private List<Lotto> lottoList = new ArrayList<>();
 
     public LottoService(int money) {
         int count = getLottoCount(money);
         for (int cnt = 0; cnt < count; cnt++) {
-            // 추 후 SORT 기능 추가
             addLotto(createLotto());
         }
     }
 
-    private int getLottoCount(int money) {
+    public int getLottoCount(int money) {
         int count = money / 1000;
 
         return count;
     }
 
-    private Lotto createLotto() {
+    public Lotto createLotto() {
         List<Integer> numbers = Utils.getIntegerList(1, 45, 6);
+        numbers = sortLotto(numbers);
         Lotto createdLotto = new Lotto(numbers);
 
         return createdLotto;
     }
 
-    private void addLotto(Lotto lotto) {
+    public void addLotto(Lotto lotto) {
         lottoList.add(lotto);
     }
 
-//    private Lotto sortLotto(Lotto lotto) {
-//
-//        return null;
-//    }
+    public List<Integer> sortLotto(List<Integer> numbers) {
+        Collections.sort(numbers);
+
+        return numbers;
+    }
 }
