e5251bb74374abe07bcd4288439effd6b12ad494
Laeng <Laeng@users.noreply.github.com>
2022-11-11 12:32:25 +0900

    feat(Application): 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.View;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            LottoController controller = new LottoController();
+
+            controller.showIntroduce();
+            controller.showMoneyInput();
+            controller.showLottoPurchaseNumbers();
+            controller.showLottoNumberInput();
+            controller.showBonusNumberInput();
+            controller.showLottoStatistic();
+
+        } catch (IllegalArgumentException exception) {
+            View.printLine("[ERROR ]" + exception.getMessage());
+
+            throw exception;
+        }
     }
 }
