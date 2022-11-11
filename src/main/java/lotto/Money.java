e474860557ed5f5656b6538705055f20503e8a48
01192mg <01192mg@gmail.com>
2022-11-12 00:28:04 +0900

    fix(Money): 인스턴스 변수 누락 수정

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -3,9 +3,11 @@ package lotto;
 public class Money {
     private static final int THOUSAND = 1_000;
     private static final int ZERO = 0;
+    private int amount;
 
     public Money(int amount) {
         validate(amount);
+        this.amount = amount;
     }
 
     private void validate(int amount) {
