4e4e7d11c1c4f4ae3c23d0c041d48f0e4c93d2f4
jkde7721 <jkde7721@naver.com>
2022-11-11 13:41:51 +0900

    feat(LottoDrawingMachine): bonusNumber 예외처리 로직 추가

--- src/main/java/lotto/model/LottoDrawingMachine.java
+++ src/main/java/lotto/model/LottoDrawingMachine.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class LottoDrawingMachine {
 
+    public static final int LOTTO_RANGE_MIN = 1;
+    public static final int LOTTO_RANGE_MAX = 45;
     private Lotto winningLotto;
     private Integer bonusNumber;
 
@@ -14,8 +16,11 @@ public class LottoDrawingMachine {
     }
 
     private void validateBonusNumber(List<Integer> winningNumbers, Integer bonusNumber) {
+        if (bonusNumber < LOTTO_RANGE_MIN || bonusNumber > LOTTO_RANGE_MAX) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 숫자 범위는 1~45까지여야 합니다.");
+        }
         if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
         }
     }
 }
