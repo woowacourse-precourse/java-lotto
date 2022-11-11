4d5cf6e25440aa18440278d62a9341800cdbc7d1
miffyKing <whqudghk28@gmail.com>
2022-11-11 22:09:56 +0900

    feat: feat(Answer): invert String type input to Lotto type.(#8)
    Input Lotto answer is String type, so change into List<Integer> and wrap
    it by Lotto Class.

--- src/main/java/lotto/Answer.java
+++ src/main/java/lotto/Answer.java
@@ -1,18 +1,39 @@
 package lotto;
 
+import io.Input;
+import io.Output;
+import io.Sentence;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Answer {
-    ArrayList<Integer> answers = new ArrayList<>();
+    Lotto answer; //= new Lotto();
     // 여기에 여섯자리짜리 로또 정답이 들어가고 일급 컬렉션으로 래핑됨.
-
     Answer(ArrayList<Integer> userInputToList) {
         validity(userInputToList);
-        this.answers = userInputToList;
+        this.answer = new Lotto(userInputToList);
     }
 
-    private void validity(ArrayList<Integer> userInput) {
-        //userinput이 유효한지 확인. 이 전에 스트링을 잘라내는 과정이 필요. -> 잘라내기만 하고 가져오자.
+    public static void InputAnswer(Input input, Output output) {
+        String answerInput = input.getInput(Sentence.INPUTANSWER.getValue(), output);
+        //List<Integer> splitedNumbers = splitByComma(answerInput);
+        validity(splitByComma(answerInput));
+    }
+
+    private static List<Integer> splitByComma(String answerInput) {
+        List<Integer> splited;
+        splited = Arrays.asList(answerInput.split(",")).stream()
+            .map(s -> Integer.parseInt(s))
+            .collect(Collectors.toList());
+        //System.out.println(splited);
+        return splited;
+    }
+
+    private static void validity(List<Integer> userInput) {
+
+        //splited가 유효한지 확인. 이 전에 스트링을 잘라내는 과정이 필요. -> 잘라내기만 하고 가져오자.
     }
 }
 // ******   로또 정답 6자리 리스트를 감싸고 있는 일급 컬렉션   ******//
