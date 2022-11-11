d3902353e3a00e8274e89379f09e0d4493dacaf3
woo-chang <clllickme@naver.com>
2022-11-11 17:31:07 +0900

    refactor(OutputService): 접근 제어자 및 static 키워드 수정, 에러 메시지를 출력하는 기능 구현
    - 외부에 노출될 가능성이 없는 상수는 private으로 수정
    - 상속을 막기 위한 private 생성자
    - 인스턴스 생성없이 메서드 사용을 위한 static 키워드 사용
    - 에러 prefix 유연한 변경을 위해 에러 메시지 출력 기능 구현

--- src/main/java/lotto/service/OutputService.java
+++ src/main/java/lotto/service/OutputService.java
@@ -9,14 +9,22 @@ public class OutputService {
     private static final String WIN_STATUS_MESSAGE = "당첨 통계";
     private static final String LINE = "---";
     private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f입니다.\n";
+    private static final String ERROR_PREFIX = "[ERROR] ";
 
-    public void printGeneratedLottos(List<Lotto> lottos) {
+    private OutputService() {
+    }
+
+    public static void printGeneratedLottos(List<Lotto> lottos) {
         System.out.println(lottos.size() + PURCHASE_MESSAGE);
         lottos.forEach(System.out::println);
     }
 
-    public void printRateOfReturn(Double rate) {
+    public static void printRateOfReturn(Double rate) {
         System.out.printf(RATE_OF_RETURN_MESSAGE, rate);
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.println(ERROR_PREFIX + message);
     }
 
 }
