7fb2dc72a7cdd334257330b992d719cfc81db419
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-12 04:13:02 +0900

    feat(Lotto): 당첨 번호 입력 값을 List로 형 변환, 숫자인지 확인하는 기능 추가
    당첨 번호로 입력받은 값을 쉼표를 기준으로 분리하여 List<String>으로 반환한다.
    그리고 해당 값을 stream()을 통해 List의 모든 값들이 숫자인지 확인한다.
    만약 숫자가 아닌 값이 존재한다면, 1 ~ 45 사이의 값만 입력해 달라는 메시지와 함께 예외를 발생시킨다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,8 +2,10 @@ package lotto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
+    private final static String COMMA = ",";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -17,18 +19,26 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+
     }
 
     private void validateString(String numbers) {
         if (isStringEmpty(numbers)) {
             throw new IllegalArgumentException(ExceptionMessage.LOTTO_WRONG_NUMBER_OF_VALUE.getMessage());
+        } else if (!isDigit(convertStringToList(numbers))) {
+            throw new IllegalArgumentException(ExceptionMessage.LOTTO_OUT_OF_RANGE.getMessage());
         }
     }
 
     private boolean isStringEmpty(String numbers) {
         return numbers == null || numbers.isBlank();
+    }
+
+    private List<String> convertStringToList(String numbers) {
+        return List.of(numbers.split(COMMA));
+    }
+
+    private boolean isDigit(List<String> numbers) {
+        return numbers.stream().allMatch(number -> number.chars().allMatch(Character::isDigit));
     }
 }
