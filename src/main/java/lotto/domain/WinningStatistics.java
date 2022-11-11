5c3b4b5901da6973c0650b990dbab9b4f54868b2
kokodak <kokodakadokok@gmail.com>
2022-11-11 12:09:41 +0900

    feat(winningStatistics): 당첨 내역을 계산하는 기능 구현

--- src/main/java/lotto/domain/WinningStatistics.java
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -1,8 +1,23 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Map;
 
 public class WinningStatistics {
+
+    public static Map<WinningRank, Integer> getWinningDetails(Lottos lottos, WinningLotto winningLotto) {
+        Map<WinningRank, Integer> winningDetails = WinningRank.generateWinningDetails();
+        for (Lotto lotto : lottos.getLottos()) {
+            int matchingCount = compareNumbersWithWinningNumbers(lotto, winningLotto);
+            boolean containsBonusNumber = false;
+            if (matchingCount == 5) {
+                containsBonusNumber = compareNumbersWithBonusNumber(lotto, winningLotto);
+            }
+            WinningRank winningRank = WinningRank.findWinningRank(matchingCount, containsBonusNumber);
+            winningDetails.replace(winningRank, winningDetails.get(winningRank) + 1);
+        }
+        return winningDetails;
+    }
 
     public static int compareNumbersWithWinningNumbers(Lotto lotto, WinningLotto winningLotto) {
         List<Integer> numbers = lotto.getNumbers();
