97550117d90a490f646bcc7d9015b3d42f314048
anneu <b.imnuj@gmail.com>
2022-11-11 11:24:07 +0900

    feat: 보너스 번호 입력 요청 메세지 출력 기능 구현

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -6,6 +6,7 @@ public final class Print {
     private static final String PURCHASE_MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String PURCHASE_QUANTITY_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static void printPurchaseMoneyInput() {
         System.out.println(PURCHASE_MONEY_INPUT_MESSAGE);
@@ -24,6 +25,10 @@ public final class Print {
 
     public static void printWinningLottoNumbersInput() {
         System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
+    }
+
+    public static void printBonusLottoNumberInput() {
+        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
     }
 
     private Print() {
