2e8f49e448306807d526f40808e44f2a29d28646
khj <bbb6254177@gmail.com>
2022-11-11 18:51:40 +0900

    feat(lotto): add function to LottoSeller class
    add function to sell lotto to user
    add function to calculate the number of lotto

--- src/main/java/lotto/LottoSeller.java
+++ src/main/java/lotto/LottoSeller.java
@@ -1,11 +1,16 @@
 package lotto;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LottoSeller {
     public List<Lotto> sellTo(int amount) {
-        List<Lotto> lotto = new ArrayList<>();
-        return lotto;
+        int numOfLotto = numberOfLotto(amount);
+        LottoGenerator lottogenerator = new LottoGenerator();
+
+        return lottogenerator.generateLotto(numOfLotto);
+    }
+
+    private int numberOfLotto(int amount) {
+        return amount/1000;
     }
 }
