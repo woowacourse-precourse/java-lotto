5949c731f97c16f011c10e58002a527dbe1b6473
kimdahee0815 <diana815@naver.com>
2022-11-11 13:27:19 +0900

    feat(numbersNoRepeat) : 입력 숫자 6개 중 하나라도 중복되는 경우 IllegalArgumentException 발생, 에러메시지([ERROR] 서로 다른 숫자 6개를 입력하세요.)
    style(validate) : error메시지 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {  
@@ -7,12 +8,13 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) { // 사용자 입력 숫자 6개를 받아서 numbers 인스턴스 final 변수에 저장
         validate(numbers);
+        numbersNoRepeat(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 숫자의 개수가 6개가 아닙니다.");
         }
     }
 
@@ -20,7 +22,12 @@ public class Lotto {
     	return numbers;
     }
     
-    public void numbersNoRepeat() {
+    public void numbersNoRepeat(List<Integer> numbers) {
+    	HashSet numbersset = new HashSet();
+    	numbersset.addAll(numbers);
     	
+    	if (numbers.size() != 6) {
+    		throw new IllegalArgumentException("[ERROR] 서로 다른 숫자 6개를 입력하세요.");
+    	}
     }
 }
