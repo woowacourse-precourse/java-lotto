8790de38361257f96a7ec83e16a8e53ea15e3a72
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-11 19:55:33 +0900

    fix(ProfitRateCalculate): getRanks 타입 변화로 인한 일부 코드 수정

--- src/main/java/lotto/domain/ProfitRateCalculator.java
+++ src/main/java/lotto/domain/ProfitRateCalculator.java
@@ -1,11 +1,11 @@
 package lotto.domain;
 
-import java.util.List;
+import java.util.Map;
 
 public class ProfitRateCalculator {
 	private final double profitRate;
 
-	public ProfitRateCalculator(List<Integer> getRanks, int purchaseAmount) {
+	public ProfitRateCalculator(Map<Rank, Integer> getRanks, int purchaseAmount) {
 		int prizeMoney = calculatePrizeMoney(getRanks);
 		this.profitRate = calculateProfitRate(prizeMoney, purchaseAmount);
 	}
@@ -19,13 +19,13 @@ public class ProfitRateCalculator {
 		return Math.round(profitRate * 10) / 10.0;
 	}
 
-	private int calculatePrizeMoney(List<Integer> getRanks) {
-		int[] prizesByRank = {5000, 50000, 1500000, 30000000, 2000000000};
-
+	private int calculatePrizeMoney(Map<Rank, Integer> getRanks) {
 		int prizeMoney = 0;
-		for (int i = 0; i < getRanks.size(); i++) {
-			prizeMoney += getRanks.get(i) * prizesByRank[i];
+
+		for (Rank rank : getRanks.keySet()) {
+			prizeMoney += rank.getPrizeMoney() * getRanks.get(rank);
 		}
+
 		return prizeMoney;
 	}
 }
