666296d01e5a10c4852d17e5d230fcf58ffaab9d
TaeMin <xoals3094@naver.com>
2022-11-12 06:53:38 +0900

    feat(Cash) : Cash 자료형(클래스) 추가

---
+++ src/main/java/lotto/Cash.java
@@ -0,0 +1,21 @@
+package lotto;
+
+public class Cash {
+    private int cash;
+
+    public Cash(int cash){
+        isValidData(cash);
+        this.cash = cash;
+    }
+
+    private void isValidData(int cash){
+        final int UNIT = 1000;
+        if((cash % UNIT) != 0){
+            throw new IllegalArgumentException("1000원 단위 금액을 입력해주세요");
+        }
+    }
+
+    public int getCash(){
+        return cash;
+    }
+}
