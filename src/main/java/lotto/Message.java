d5937ada8c67a5f9a7a341e086254b08c0b7f167
young2866 <62212610+young2866@users.noreply.github.com>
2022-11-12 02:37:41 +0900

    refactor(Valid): Valid 클래스의 Message String을 Message클래스로 이동

--- src/main/java/lotto/Message.java
+++ src/main/java/lotto/Message.java
@@ -2,5 +2,7 @@ package lotto;
 
 public class Message {
 	public static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-
+	public static final String NOT_DIVIDED_BY_1000_ERROR = "[ERROR]구입금액이 1000원 단위가 아닙니다.";
+	public static final String PURCHASE_AMOUNT_IS_ZERO_ERROR = "[ERROR]구입 금액이 0원입니다.";
+	public static final String INPUT_IS_NOT_NUMBER_ERROR = "[ERROR]구입금액이 숫자가 아닙니다.";
 }
--- src/main/java/lotto/Valid.java
+++ src/main/java/lotto/Valid.java
@@ -3,21 +3,18 @@ package lotto;
 public class Valid {
 	private static final int LOTTO_PRICE = 1000;
 	private static final int ZERO = 0;
-	private static final String NOT_DIVIDED_BY_1000_ERROR = "[ERROR]구입금액이 1000원 단위가 아닙니다.";
-	private static final String PURCHASE_AMOUNT_IS_ZERO_ERROR = "[ERROR]구입 금액이 0원입니다.";
-	private static final String INPUT_IS_NOT_NUMBER_ERROR = "[ERROR]구입금액이 숫자가 아닙니다.";
 
 	public static int validatePurchaseAmount(String purchaseAmount) {
 		int verification = isNumber(purchaseAmount);
 
 		if (isZero(verification)) {
-			System.out.println(PURCHASE_AMOUNT_IS_ZERO_ERROR);
-			throw new IllegalArgumentException(PURCHASE_AMOUNT_IS_ZERO_ERROR);
+			System.out.println(Message.PURCHASE_AMOUNT_IS_ZERO_ERROR);
+			throw new IllegalArgumentException(Message.PURCHASE_AMOUNT_IS_ZERO_ERROR);
 		}
 		if(isDividedBy1000(verification))
 			return verification;
-		System.out.println(NOT_DIVIDED_BY_1000_ERROR);
-		throw new IllegalArgumentException(NOT_DIVIDED_BY_1000_ERROR);
+		System.out.println(Message.NOT_DIVIDED_BY_1000_ERROR);
+		throw new IllegalArgumentException(Message.NOT_DIVIDED_BY_1000_ERROR);
 	}
 
 	private static boolean isDividedBy1000(int verification) {
@@ -30,8 +27,8 @@ public class Valid {
 		if (isNumeric(purchaseAmount)) {
 			return Integer.parseInt(purchaseAmount);
 		}
-		System.out.println(INPUT_IS_NOT_NUMBER_ERROR);
-		throw new IllegalArgumentException(INPUT_IS_NOT_NUMBER_ERROR);
+		System.out.println(Message.INPUT_IS_NOT_NUMBER_ERROR);
+		throw new IllegalArgumentException(Message.INPUT_IS_NOT_NUMBER_ERROR);
 	}
 	private static boolean isZero(int purchaseAmount) {
 		if (purchaseAmount == ZERO) {
