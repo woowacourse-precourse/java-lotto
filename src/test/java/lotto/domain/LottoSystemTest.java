810b174303681dff67c12289652bac3fcf2cab1c
HwangSeokJin <ghkd3703@gmail.com>
2022-11-11 15:32:31 +0900

    test(LottoSystem): 당첨 번호 입력 시 모두 다른 숫자인지 확인하는 테스트 코드 작성

--- src/test/java/lotto/domain/LottoSystemTest.java
+++ src/test/java/lotto/domain/LottoSystemTest.java
@@ -14,6 +14,7 @@ class LottoSystemTest {
         String[] invalidInput = {
                 "1, 2, 3, 4, 6, w",
                 "1, e, r, @, 1, 2",
+                "wofjweoifjweio124"
         };
         for (String input : invalidInput) {
             assertThatThrownBy(() -> system.setWinningNumbers(input))
@@ -40,6 +41,19 @@ class LottoSystemTest {
         String[] invalidInput = {
                 "1, 46, 2, 3, 4, 5",
                 "-1, 45, 4, 24, 14, 5",
+        };
+        for (String input : invalidInput) {
+            assertThatThrownBy(() -> system.setWinningNumbers(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @DisplayName("당첨 번호 입력 시 모두 다른 숫자인지 확인한다")
+    @Test
+    void InputDuplicateNumbers() {
+        String[] invalidInput = {
+                "1, 2, 3, 5, 5, 6",
+                "7, 8, 11, 10, 11, 12",
         };
         for (String input : invalidInput) {
             assertThatThrownBy(() -> system.setWinningNumbers(input))
