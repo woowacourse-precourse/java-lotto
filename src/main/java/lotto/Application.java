b7683e82fe13344e04458018b06469d12994ee54
Ji-Un-Gil <wldns2577@gmail.com>
2022-11-11 15:46:01 +0900

    feat(Application.java): main function Updated function: - 로또 게임에 필요한 상수 선언 및 주어진 입력들을 받아오는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,19 +1,32 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import org.assertj.core.util.Arrays;
 
 public class Application {
+    private static final int ticket_price = 1000;
 
     public static void main(String[] args) {
+        final List<Integer> lotto_win;
+        final int purchase, bonus;
         Scanner sc = new Scanner(System.in);
-        int purchase = sc.nextInt();
+        purchase = sc.nextInt();
+
         validate(purchase);
-        
+        sc.nextLine();
+        lotto_win = Arrays.asList(sc.nextLine().split(",")).stream().map(s -> Integer.parseInt(((String) s).trim())).collect(Collectors.toList());
+        Lotto winLotto = new Lotto(lotto_win);
+        bonus = sc.nextInt();
         sc.close();
     }
 
     private static void validate(Integer purchase){
-        if (purchase % 1000 != 0 || purchase <= 0){
+        if (purchase % ticket_price != 0 || purchase <= 0){
             System.out.println("[ERROR] 금액은 반드시 1,000원 이상, 1,000원 단위로 입력하세요.");
             throw new IllegalArgumentException();
         }
