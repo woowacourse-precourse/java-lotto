d0061cec91fdadfb97de1798adac1411e01fce9b
49ehyeon42 <49ehyeon42@gmail.com>
2022-11-11 15:14:39 +0900

    feat: 당첨 번호 문자열 관련 입출력 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -27,6 +27,11 @@ public class Application {
                 System.out.println(lottery.getLottoNumbers().toString());
             }
             System.out.println();
+
+            System.out.println("당첨 번호를 입력해 주세요.");
+
+            Lotto winningLotto = getWinningLotto(Console.readLine());
+            System.out.println();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
