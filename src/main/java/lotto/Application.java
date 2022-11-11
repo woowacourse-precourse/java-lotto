0de42bb52e06416a7360f165a5254cf92bebb19f
김경훈 <54941130+ouvir@users.noreply.github.com>
2022-11-11 13:27:48 +0900

    feat: add method(askForMoney) and variable(moneyToBuy)
    ask for money:
    readLine()
    validate exception
    save to moneyToBuy
    
    validate money method:
    checkOnlyNumber()
    checkDivisibleNumber()

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,35 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+    private int moneyToBuy;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Application newRound = new Application();
+        newRound.askForMoney();
     }
+
+    public void askForMoney() {
+        String answer = readLine().replace(" ", "");
+        validateMoney(answer);
+        this.moneyToBuy = Integer.parseInt(answer);
+    }
+
+    private void validateMoney(String answer) {
+        if (checkOnlyNumber(answer) || checkDivisibleNumber(answer)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean checkOnlyNumber(String answer) {
+        return (answer.matches("[^0-9]"));
+    }
+
+    private boolean checkDivisibleNumber(String answer) {
+        return (Integer.parseInt(answer) % 1000 != 0);
+    }
+
+
 }
