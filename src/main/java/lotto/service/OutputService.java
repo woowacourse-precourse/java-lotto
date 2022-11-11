a4e8297e68f5d82f05f7370d7351429a3431f714
woo-chang <clllickme@naver.com>
2022-11-11 15:19:58 +0900

    feat(OutputService): 수익률을 출력하는 기능 구현

--- src/main/java/lotto/service/OutputService.java
+++ src/main/java/lotto/service/OutputService.java
@@ -1,15 +1,22 @@
 package lotto.service;
 
 import java.util.List;
-import lotto.Lotto;
+import lotto.entity.Lotto;
 
 public class OutputService {
 
-    public static String PURCHASE_MESSAGE = "개를 구매했습니다.";
+    private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
+    private static final String WIN_STATUS_MESSAGE = "당첨 통계";
+    private static final String LINE = "---";
+    private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f입니다.\n";
 
     public void printGeneratedLottos(List<Lotto> lottos) {
         System.out.println(lottos.size() + PURCHASE_MESSAGE);
         lottos.forEach(System.out::println);
+    }
+
+    public void printRateOfReturn(Double rate) {
+        System.out.printf(RATE_OF_RETURN_MESSAGE, rate);
     }
 
 }
