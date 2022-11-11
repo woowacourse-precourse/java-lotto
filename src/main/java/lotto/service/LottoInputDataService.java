885187ff54668b24ab5a3082d69b550e90728dc8
sosow0212 <sosow0212@naver.com>
2022-11-11 14:38:38 +0900

    feat : 유저가 구매한 로또 번호를 출력해주는 기능 구현

---
+++ src/main/java/lotto/service/LottoInputDataService.java
@@ -0,0 +1,39 @@
+package lotto.service;
+
+import lotto.domain.Lotto;
+import lotto.exception.LottoException;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoInputDataService {
+
+    public static void main(String[] args) {
+        LottoInputDataService in = new LottoInputDataService();
+        in.inputLottoData();
+    }
+
+    private final LottoException lottoException = new LottoException();
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public Lotto inputLottoData() {
+        String lottoNumbers = inputLottoNumbers();
+        String bonusNumber = inputBonusNumber(lottoNumbers);
+        Lotto lotto = new Lotto(lottoNumbers, bonusNumber); // 로또 번호 + 보너스 번호 받기
+        return lotto;
+    }
+
+    public String inputLottoNumbers() {
+        outputView.askLottoNumbers();
+        String lottoNumbers = inputView.inputLottoNumbersView();
+        lottoException.validateLottoNumbers(lottoNumbers);
+        return lottoNumbers;
+    }
+
+    public String inputBonusNumber(String lottoNumbers) {
+        outputView.askBonusNumber();
+        String bonusNumber = inputView.inputBonusNumberView();
+        lottoException.validateBonusNumbers(lottoNumbers, bonusNumber);
+        return bonusNumber;
+    }
+}
