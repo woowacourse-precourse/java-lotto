ba67248d8133c4dfedf9defce01188956f5c2e67
정승구 <jeongseung-gu@jeongseung-guui-MacBookAir.local>
2022-11-12 06:51:38 +0900

    feat: runGame클래스 생성

--- src/main/java/lotto/RunGame.java
+++ src/main/java/lotto/RunGame.java
@@ -1,2 +1,14 @@
-package lotto;public class RunGame {
+package lotto;
+
+import lotto.model.UserDTO;
+import lotto.view.InputPurchase;
+
+public class RunGame {
+    InputPurchase input = new InputPurchase();
+
+    public void runGame() {
+        UserDTO userDTO;
+
+        userDTO = input.inputPurchase();
+    }
 }
