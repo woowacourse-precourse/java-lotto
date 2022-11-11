c89181fec30dcb2c30e889bef05d9dcbabc9bdbb
Cyma-s <ssol0319@naver.com>
2022-11-11 17:55:56 +0900

    feat(Constant): add lotto number count constant

--- src/main/java/lotto/model/Constant.java
+++ src/main/java/lotto/model/Constant.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 public class Constant {
-    public static int NUMBER_RANGE_START = 1;
-    public static int NUMBER_RANGE_END = 45;
+    public static final int NUMBER_RANGE_START = 1;
+    public static final int NUMBER_RANGE_END = 45;
+    public static final int LOTTO_NUMBER_COUNT = 6;
 }
