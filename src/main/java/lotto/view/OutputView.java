1a77260b8c0220781d309424939aa0eeb6dc2daf
ksh-beginner <ohk9134@naver.com>
2022-11-11 18:39:21 +0900

    feat(OutputView) : 로또 발행 번호를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,17 @@
 package lotto.view;
 
 
+import java.util.List;
+
 public class OutputView {
 
     private static final String PURCHASE_LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
 
     public void printPurchaseLottoCount(int purchaseLottoCount) {
         System.out.println(purchaseLottoCount + PURCHASE_LOTTO_COUNT_MESSAGE);
+    }
+
+    public void printIssuedLottoNumbers(List<Integer> issuedLottoNumbers) {
+        System.out.println(issuedLottoNumbers.toString());
     }
 }
