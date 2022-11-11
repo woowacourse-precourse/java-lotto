4ccded411320b76c7f71a89355d9b68610dbb468
K-Diger <kitten.diger@gmail.com>
2022-11-11 17:36:06 +0900

    refactor : 구매금액을 입력받는 기능 구현 - Client 클래스 내 getPurchaseMoney() 메서드를 구현하여 사용자에게 구매금액을 입력받는다.

--- src/main/java/lotto/client/Client.java
+++ src/main/java/lotto/client/Client.java
@@ -1,16 +1,14 @@
 package lotto.client;
 
-import lotto.lotteryshop.Store;
-
-import java.util.List;
+import lotto.input.InputAgent;
 
 public class Client {
 
-    public List<List<Integer>> buyLotto() {
-        Store store = new Store();
-        int purchaseNumberOfLotto = store.getPurchaseMoney();
-        List<List<Integer>> generatedLottoNumber = store.generateLottoNumber(purchaseNumberOfLotto);
-        store.getPublishedLottoNumber(generatedLottoNumber);
-        return generatedLottoNumber;
+    public int getPurchaseMoney() {
+        System.out.println("구매금액을 입력해 주세요.");
+        InputAgent inputAgent = new InputAgent();
+        int inputValue = inputAgent.inputUserMoney();
+        System.out.printf("\n%d개를 구매했습니다.\n", inputValue / 1000);
+        return inputValue / 1000;
     }
 }
