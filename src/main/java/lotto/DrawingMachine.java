c0b2a6f72cf6aada791bacf043f38596a9f0ef71
rt3310 <btac3310@gmail.com>
2022-11-11 13:35:36 +0900

    feat: 맞춘 로또 번호 개수를 계산하여 당첨 결과를 가져오는 기능 구현

--- src/main/java/lotto/DrawingMachine.java
+++ src/main/java/lotto/DrawingMachine.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class DrawingMachine {
@@ -11,13 +12,19 @@ public class DrawingMachine {
         this.bonusNumber = bonusNumber;
     }
 
-    public long getHitCount(Lotto lotto) {
-        return winningNumbers.stream()
+    public int getHitCount(Lotto lotto) {
+        return (int) winningNumbers.stream()
                 .filter(lotto::isContain)
                 .count();
     }
 
     public boolean isHitBonusNumber(Lotto lotto) {
         return lotto.isContain(bonusNumber);
+    }
+
+    public LottoRank getRank(Lotto lotto) {
+        return Arrays.stream(LottoRank.values())
+                .filter(lottoRank -> lottoRank.equal(getHitCount(lotto), isHitBonusNumber(lotto)))
+                .findAny().orElse(LottoRank.NOTHING);
     }
 }
--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -1,2 +1,26 @@
-package lotto;public enum LottoRank {
+package lotto;
+
+public enum LottoRank {
+    FIRST_PLACE(6, false),
+    SECOND_PLACE(5, true),
+    THIRD_PLACE(5, false),
+    FOURTH_PLACE(4, false),
+    FIFTH_PLACE(3, false),
+    NOTHING(0, false);
+
+    private int hitCount;
+    private boolean hitBonus;
+
+    LottoRank(int hitCount, boolean hitBonus) {
+        this.hitCount = hitCount;
+        this.hitBonus = hitBonus;
+    }
+
+    boolean equal(int hitCount, boolean hitBonus) {
+        if (this.hitCount == hitCount && hitCount != 5) {
+            return true;
+        }
+
+        return this.hitCount == hitCount && this.hitBonus == hitBonus;
+    }
 }
