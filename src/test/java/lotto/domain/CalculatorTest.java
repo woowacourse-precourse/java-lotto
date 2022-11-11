d49fe570cf337911961ce9d9d3bba9af5f308fa2
khsrla9806 <kimhunsope@naver.com>
2022-11-11 19:20:07 +0900

    test(CalculateTest): 총 수익금을 계산하는 기능 테스트 구현

---
+++ src/test/java/lotto/domain/CalculatorTest.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+    @DisplayName("총 수익금을 계산하는 기능 테스트")
+    @ParameterizedTest
+    @MethodSource("calculateTotalEarningsTestSource")
+    void calculateTotalEarningsTest(int totalEarnings, List<Integer> result) throws Exception {
+        Calculator calculator = new Calculator();
+        Method privateMethod = calculator.getClass().getDeclaredMethod("calculateTotalEarnings", List.class);
+        privateMethod.setAccessible(true);
+
+        privateMethod.invoke(calculator, result);
+
+        assertThat(calculator.getTotalEarnings()).isEqualTo(totalEarnings);
+    }
+    private static Stream<Arguments> calculateTotalEarningsTestSource() {
+        return Stream.of(
+                Arguments.of(2033150000, List.of(0, 1, 1, 2, 3, 0)),
+                Arguments.of(2031555000, List.of(0, 1, 1, 1, 1, 1)),
+                Arguments.of(0, List.of(0, 0, 0, 0, 0, 0))
+        );
+    }
+}
