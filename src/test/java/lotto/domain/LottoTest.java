3d98e43a9ddf117b7e47adce0dbddefb12449d98
yeonhee <ydol2@naver.com>
2022-11-12 03:34:06 +0900

    test(LottoTest) : lotto가 범위에 벗어나면 예외가 발생하는지 검증

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -24,4 +24,17 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 45를 초과하는 수가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByOverRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 1미만인 수가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
