e7c26bf121c72d3d44fc5c862b060c14759d7bd1
doongu <must1080@naver.com>
2022-11-11 19:54:31 +0900

    feat(LottoSystem): 1000원 단위로 나누어 떨어지는지 확인하며 예외처리하는 기능  구현

--- src/main/java/lotto/domain/LottoSystem.java
+++ src/main/java/lotto/domain/LottoSystem.java
@@ -1,2 +1,20 @@
-package lotto.domain;public class LottoSystem {
+package lotto.domain;
+
+public class LottoSystem {
+    private int userLottoNumber;
+
+    public LottoSystem(User user) {
+        this.userLottoNumber = user.getUserPayAmount();
+        validate();
+    }
+
+    private void validate() {
+        final int STANDARD = 1000;
+        int remainder;
+
+        remainder = userLottoNumber % STANDARD;
+        if (remainder != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000원으로 나누어 떨어지지 않습니다.");
+        }
+    }
 }
