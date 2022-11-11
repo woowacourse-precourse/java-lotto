d5ad02f1d9c0566d53b60ff34f943eede7847c36
jihun-24k <gi020477@gmail.com>
2022-11-11 12:43:30 +0900

    Feat(MessageList): 출력문구를 관리하는 Enum 요소 추가

--- src/main/java/lotto/message/MessageList.java
+++ src/main/java/lotto/message/MessageList.java
@@ -1,5 +1,20 @@
 package lotto.message;
 
 public enum MessageList {
+    PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
+    NUMBER_OF_PURCHASES("개를 구매했습니다."),
+    WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    BONNUS_NUMBER("보너스 번호를 입력해 주세요."),
+    WINNIG_STATISTICS("당첨 통계"),
+    LINE("---");
 
+    private final String message;
+
+    MessageList(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
 }
