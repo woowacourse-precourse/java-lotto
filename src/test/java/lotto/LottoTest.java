67c8dcb9ee46c60f08d13c567ae6e0e5d1ed7310
shinD <huipulci1@naver.com>
2022-11-11 11:21:47 +0900

    test: 로또 숫자 유효성 검증 테스트코드 작성
    - 숫자는 1 ~ 45 사이의 범위
    - 중복되지 않은 6개의 숫자여야 함

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.domain.lotto.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -18,10 +21,17 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @ParameterizedTest(
+            name = "로또 번호에 1 ~ 45 범위를 벗어난 숫자 " +
+                    "EX({argumentsWithNames})가 있으면 예외가 발생한다."
+    )
+    @ValueSource(ints = {-12, 0, 46, 102})
+    void createLottoByOutOfRange(final int number) {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 45, number, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
