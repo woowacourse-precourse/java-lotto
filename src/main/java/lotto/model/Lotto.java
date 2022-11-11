79a0e159f433dd9da3a3f48ef0f4a0c67523eb30
Cyma-s <ssol0319@naver.com>
2022-11-11 17:11:27 +0900

    feat(Lotto): add exception statement

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.model;
 
 import java.util.List;
 
@@ -6,15 +6,13 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumberCount(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateNumberCount(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorConstant.INVALID_DIGITS_COUNT);
         }
     }
-
-    // TODO: 추가 기능 구현
 }
