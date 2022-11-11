fbb2d4556d2a82e46230982ffcad9c5366740f00
Ukj0ng <xb253q@gmail.com>
2022-11-11 18:14:34 +0900

    feat(inputException): 양수가 아닌 구입금액이면 예외처리

--- src/main/java/lotto/exception/InputException.java
+++ src/main/java/lotto/exception/InputException.java
@@ -2,7 +2,8 @@ package lotto.exception;
 
 public class InputException {
     private static final String PRICE_INTEGER_ERROR = "[ERROR] 구입금액은 숫자여야 합니다.";
-
+    private static final String PRICE_IS_POSITIVE_NUMBER = "[ERROR] 시도 횟수는 1이상의 양수여야 합니다.";
+    private static final int MIN_PRICE_NUMBER = 1;
 
 
 
@@ -12,6 +13,13 @@ public class InputException {
         } catch (NumberFormatException e){
             throw new IllegalArgumentException(PRICE_INTEGER_ERROR);
         }
+    }
+
+    private static boolean isNaturalNumber(int price){
+        if (price < MIN_PRICE_NUMBER){
+            throw new IllegalArgumentException(PRICE_IS_POSITIVE_NUMBER);
+        }
+        return true;
     }
 
 }
