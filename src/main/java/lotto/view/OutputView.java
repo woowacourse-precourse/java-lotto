e35fb3b9ba3d48b6afb724071ae8a4c4271cf48a
heesang <dlagmltkd12@naver.com>
2022-11-12 06:25:48 +0900

    feat: 당첨 통계 메시지를 출력하는 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,6 +5,7 @@ public class OutputView {
     public static final String INPUT_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String INPUT_ANSWER_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    public static final String WINNING_STATISTICS_MESSAGE = "당첨 통계\n" + "---";
 
     public static void printInputPurchasePrice() {
         System.out.println(INPUT_PURCHASE_PRICE_MESSAGE);
@@ -16,5 +17,9 @@ public class OutputView {
 
     public static void printInputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+    }
+
+    public static void printWinningStatistics() {
+        System.out.println(WINNING_STATISTICS_MESSAGE);
     }
 }
