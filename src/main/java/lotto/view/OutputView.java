2d4140083d96c0f654e3c711ba7b8d2c041b8423
sosow0212 <sosow0212@naver.com>
2022-11-11 14:36:45 +0900

    refactor : service와 유기적인 관계를 위해 OutputView의 메서드 매개변수 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,24 +1,23 @@
 package lotto.view;
 
-import java.util.ArrayList;
-import java.util.List;
 import lotto.domain.User;
-import lotto.domain.UserLotto;
 
 public class OutputView {
     public void askBuyingPriceView() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public void responseBuyingQuantity(int quantity, User user) {
-        System.out.println(quantity + "개를 구매했습니다.");
+    public void responseBuyingQuantity(User user) {
+        System.out.println(getQuantity(user) + "개를 구매했습니다.");
+    }
 
+    public void responseUserLottoNumber(User user) {
         user.getLottos().stream().forEach(userLotto -> {
             System.out.println(userLotto.getNumbers());
         });
     }
 
-    public void askNumbers() {
+    public void askLottoNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
@@ -26,8 +25,7 @@ public class OutputView {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
-//    public void responseResult() {
-//        System.out.println("당첨 통계");
-//        System.out.println("---");
-//    }
+    public int getQuantity(User user) {
+        return user.getBuyingPrice() / 1000;
+    }
 }
