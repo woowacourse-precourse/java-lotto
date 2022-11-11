db15d7c59808de439325cd39767228b066557893
ykmxxi <ykmxxi97@gmail.com>
2022-11-11 18:46:37 +0900

    feat: 당첨 번호와 보너스 번호를 입력받는 기능 구현

--- src/main/java/lotto/view/input/BonusInput.java
+++ src/main/java/lotto/view/input/BonusInput.java
@@ -2,16 +2,22 @@ package lotto.view.input;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import lotto.validation.WinningValidation;
 
-public class WinningInput extends Input {
+public class BonusInput extends Input {
     private final WinningValidation validation = new WinningValidation();
     private String input;
+    // 당첨 번호 입력을 List<Integer> 형태로 가져오기 필요
+    private List<Integer> winnings;
 
     @Override
     public String getInput() {
         input = readLine();
-        validation.isValidWinning(input);
+        winnings = new ArrayList<>();
+        validation.isValidBonus(input, winnings);
         return input;
     }
 }
