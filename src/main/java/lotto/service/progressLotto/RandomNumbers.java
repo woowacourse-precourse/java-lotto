6ffdfceba64aa08f2138597e45c47ef7e0c3f88d
baksohyeon <bori12370@gmail.com>
2022-11-11 15:14:46 +0900

    feat: make file structor which is fitted MVC pattern

---
+++ src/main/java/lotto/service/progressLotto/RandomNumbers.java
@@ -0,0 +1,5 @@
+package lotto.service.progressLotto;
+
+public class RandomNumbers {
+    // 당첨 번호 6자리 뽑아줌
+}
---
+++ src/main/java/lotto/service/user/UserInput.java
@@ -0,0 +1,7 @@
+package lotto.service.user;
+
+public class UserInput {
+    // , 처리
+    // validation 처리해서 에러 핸들링
+    // bonus 값 받아오는 로직
+}
