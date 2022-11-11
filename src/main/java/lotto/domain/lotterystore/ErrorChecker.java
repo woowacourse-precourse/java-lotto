3801af3f95e046b42ba6bdbcd9916bfbaaf1c61e
ysng_ysng <yeasung67@gmail.com>
2022-11-11 22:57:06 +0900

    feat(ErrorChecker): Implement the methods about validateWinningNumbers
    validateWinningNumbers에 대한 예외처리 메서드를 구현했습니다.
    구현목록으로는 isNotValidLength,
    isNotAllPositiveNumber, isNotUniqueNumbers, isNotRange입니다.

--- src/main/java/lotto/domain/lotterystore/ErrorChecker.java
+++ src/main/java/lotto/domain/lotterystore/ErrorChecker.java
@@ -1,11 +1,53 @@
 package lotto.domain.lotterystore;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class ErrorChecker {
 	public void validateMoneyFromBuyer(String money) {
 		if (isNotNumber(money) || isNotPositiveNumber(money) || isNotZeroRemainder(money)) {
 			throw new IllegalArgumentException("[ERROR] 올바른 금액을 입력해주세요!\n");
 		}
 	}
+
+	public void validateWinningNumbers(String winningNumbers) {
+		String[] separation = winningNumbers.split(",");
+
+		if (isNotValidLength(separation) || isNotAllPositiveNumber(separation) || isNotUniqueNumbers(
+			separation)) {
+			throw new IllegalArgumentException("[ERROR] 올바른 당첨번호를 입력해주세요!\n");
+		}
+	}
+
+	private boolean isNotValidLength(String[] separation) {
+		return separation.length != 6;
+	}
+
+	private boolean isNotAllPositiveNumber(String[] separation) {
+		String[] numbers;
+		Stream<String> eachNumber = Arrays.stream(separation);
+		try {
+			numbers =
+				eachNumber.filter(number -> Integer.parseInt(number) > 0).toArray(String[]::new);
+		} catch (IllegalArgumentException e) {
+			return true;
+		}
+
+		return isNotValidLength(numbers) || isNotRange(numbers);
+	}
+
+	private boolean isNotUniqueNumbers(String[] separation) {
+		Stream<String> eachNumber = Arrays.stream(separation);
+		String[] numbers = eachNumber.distinct().toArray(String[]::new);
+		return numbers.length != 6;
+	}
+
+	private boolean isNotRange(String[] numbers) {
+		return Arrays.stream(numbers)
+			.anyMatch(elem -> Integer.parseInt(elem) < 1 || Integer.parseInt(elem) > 45);
+	}
+
+
 
 	private boolean isNotNumber(String money) {
 		// 숫자가 아닌 경우 참값 반환
