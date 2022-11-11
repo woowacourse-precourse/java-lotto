f53310ac3ffe9cd970fb4e781c84fd8fbec4d5f2
anneu <b.imnuj@gmail.com>
2022-11-11 10:07:59 +0900

    feat: 로또 구입금액 입력 요청 메세지 출력 기능 구현

---
+++ src/main/java/lotto/Print.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public final class Print {
+    private static final String PURCHASE_MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static void printPurchaseMoneyInput() {
+        System.out.println(PURCHASE_MONEY_INPUT_MESSAGE);
+    }
+
+    private Print() {
+    }
+}
