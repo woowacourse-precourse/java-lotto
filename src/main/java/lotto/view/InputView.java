be8c1aa07470bc6b4366ce290b505a5d194f2d7d
Soomin Lim <augustth511@gmail.com>
2022-11-12 00:09:52 +0900

    feat(InputView): 사용자 입력 시 숫자를 입력하지 않으면 예외 발생 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,11 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
 
@@ -8,24 +13,45 @@ public class InputView {
     private final static String WINNIG_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private final static String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public static String playerMoney() {
+    private final static String NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 숫자를 입력해야 합니다.";
+
+    public static int playerMoney() {
         System.out.println(PLAYER_MONEY_MESSAGE);
 
-        String playerMoney = Console.readLine();
-        return playerMoney;
+        return convertStringToInteger(Console.readLine());
     }
 
-    public static String winningNumbers() {
+    public static Lotto winningNumbers() {
         System.out.println(WINNIG_NUMBER_MESSAGE);
 
-        String winnigNumbers = Console.readLine();
-        return winnigNumbers;
+        return convertStringToLotto(Console.readLine());
     }
 
-    public static String bonusNumber() {
+    public static int bonusNumber() {
         System.out.println(BONUS_NUMBER_MESSAGE);
 
-        String bonusNumber = Console.readLine();
-        return bonusNumber;
+        return convertStringToInteger(Console.readLine());
+    }
+
+    private static int convertStringToInteger(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            System.out.println(NUMBER_FORMAT_ERROR_MESSAGE);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static Lotto convertStringToLotto(String winningNumbers) {
+        try {
+            String[] lottoNumbers = winningNumbers.split(",");
+            List<Integer> numbers = Arrays.stream(lottoNumbers)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            return new Lotto(numbers);
+        } catch (NumberFormatException e) {
+            System.out.println(NUMBER_FORMAT_ERROR_MESSAGE);
+            throw new IllegalArgumentException();
+        }
     }
 }
