914440dcc088332eed981d3f76b25cd13d8f329e
jthugg <jthug@naver.com>
2022-11-12 06:14:45 +0900

    Style: 불필요한 구문 삭제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,10 +2,9 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
         try {
             Request request = new Request();
+            
             request.requestBettingMoney();
             request.validateBettingMoney();
             UserBet userBet = new UserBet(request.getBettingMoney());
