34fdca77d4f487eb170edac379a07ad271270a41
gyuturn <rbtjrrbals@gmail.com>
2022-11-12 00:08:43 +0900

    feat($view): 당첨번호입력을 받기위한 문장 상수화
    D-1
    "당첨 번호를 입력해 주세요." 상수화

--- src/main/java/util/message/ConsoleMessage.java
+++ src/main/java/util/message/ConsoleMessage.java
@@ -2,5 +2,6 @@ package util.message;
 
 public class ConsoleMessage {
     public static final String MESSAGE_PURCHASE_LOTTO = "구입금액을 입력해 주세요.";
+    public static final String WIN_LOTTO_NUMBER="당첨 번호를 입력해 주세요.";
 
 }
