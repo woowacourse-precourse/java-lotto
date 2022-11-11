dd37f01f486963ac7746bd3cf2696576b28ceed5
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-11 20:24:47 +0900

    style(Validator): change method location

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -12,6 +12,14 @@ public class Validator {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위의 수이어야 합니다.");
     }
 
+    private boolean isPositiveInteger(String string) {
+        return string.matches("[1-9][0-9]+");
+    }
+
+    private boolean isAvailablePayment(String payment) {
+        return Integer.parseInt(payment) % 1000 == 0;
+    }
+
     public void validateWinningNumbers(String winningNumbers) {
         validateFormOfWinningNumbers(winningNumbers);
         validateValuesOfWinningNumbers(Arrays.stream(winningNumbers.split(","))
@@ -36,13 +44,5 @@ public class Validator {
 
     public void validateBonusNumber(int bonusNumber) {
 
-    }
-
-    private boolean isPositiveInteger(String string) {
-        return string.matches("[1-9][0-9]+");
-    }
-
-    private boolean isAvailablePayment(String payment) {
-        return Integer.parseInt(payment) % 1000 == 0;
     }
 }
