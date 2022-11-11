31fcc9b58292dad4a025553318d66e903b4d9e86
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-11 21:20:32 +0900

    style: 구매금액 입력 코드스타일  변경

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -3,14 +3,13 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class LottoGame {
+    private static final String TYPE_ERROR_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
 
     public int inputMoney() {
-        String input = Console.readLine();
         try {
-            return Integer.parseInt(input);
+            return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException numberFormatException) {
-            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
+            throw new IllegalArgumentException(TYPE_ERROR_MESSAGE);
         }
     }
-
 }
