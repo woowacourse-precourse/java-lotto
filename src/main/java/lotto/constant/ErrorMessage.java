907f83d4af6263fdd4a40a9572bb450417dcc1cd
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-11 19:40:35 +0900

    feat: 예외 발생시 에러 메시지 접두사를 추가

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -1,6 +1,7 @@
 package lotto.constant;
 
 public enum ErrorMessage {
+    ERROR_MESSAGE_PREFIX("[ERROR] "),
     UNIT_OF_LOTTO_PURCHASE_DOES_NOT_COMPLY_WITH_RULE("구매 단위에 맞지 않게 입력하셨습니다"),
     CAN_NOT_EXIST_DUPLICATE_NUMBER_IN_LOTTO("로또에 중복된 숫자는 존재할 수 없습니다"),
     LOTTO_NUMBER_IS_OUT_OF_RANGE("범위 밖의 로또 번호입니다"),
@@ -15,6 +16,6 @@ public enum ErrorMessage {
     }
 
     public String getMessage() {
-        return message;
+        return ERROR_MESSAGE_PREFIX.message + message;
     }
 }
