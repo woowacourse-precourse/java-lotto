ac3c112b5d8ef9c49b708fbfdd0632b5a40a9166
sosow0212 <sosow0212@naver.com>
2022-11-11 14:39:05 +0900

    feat : 유저의 로또 번호를 설정해주는 기능 구현

--- src/main/java/lotto/service/UserInputDataService.java
+++ src/main/java/lotto/service/UserInputDataService.java
@@ -8,6 +8,15 @@ public class UserInputDataService {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
 
+    public void setUserLotto(User user) {
+        outputView.responseBuyingQuantity(user);
+        user.setLottos(getBuyingPrice(user));
+    }
+
+    public void printLottoNumbers(User user) {
+        outputView.responseUserLottoNumber(user);
+    }
+
     public int getBuyingPrice(User user) {
         return user.getBuyingPrice();
     }
