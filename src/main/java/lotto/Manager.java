124d0daf465522da00c3645a8ddd7abdc40b16ed
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-11 23:30:12 +0900

    feat: 구매한 로또번호와 보너스번호 비교기능 추가

--- src/main/java/lotto/Manager.java
+++ src/main/java/lotto/Manager.java
@@ -34,12 +34,15 @@ public class Manager {
     public int compareLottoNumbers(Lotto purchasedLotto, Lotto winningLotto) {
         int numberOfMatches = 0;
         for (int i = 0; i < winningLotto.getNumbers().size(); i++) {
-            if(purchasedLotto.getNumbers().contains(winningLotto.getNumbers().get(i))){
-                numberOfMatches ++;
+            if (purchasedLotto.getNumbers().contains(winningLotto.getNumbers().get(i))) {
+                numberOfMatches++;
             }
         }
         return numberOfMatches;
     }
 
+    public boolean hasBonusNumber(Lotto purchasedLotto, int bonunsNumber) {
+        return purchasedLotto.getNumbers().contains(bonunsNumber);
+    }
 }
 
