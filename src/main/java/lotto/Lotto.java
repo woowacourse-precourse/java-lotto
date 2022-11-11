f50c3036a862a86c4def2ed77504d94f6fe09361
MIN-04 <kimji_0424@naver.com>
2022-11-11 15:52:31 +0900

    feat(Lotto.java): add 등수 확인 메서드
    Added method checkWinningNums():
    - 당첨 금액 list와 보너스 볼을 받아 당첨 등수를 확인하는 메서드

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,8 @@ package lotto;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -22,5 +24,24 @@ public class Lotto {
     public List<Integer> getNumbers() {
         numbers.sort(Comparator.naturalOrder());
         return numbers;
+    }
+
+    public String checkWinningNums(List<Integer> winningNums, int bonusNum) {
+        List<Integer> differentNums = numbers.stream().
+            filter(number -> winningNums.stream().noneMatch(Predicate.isEqual(number)))
+            .collect(Collectors.toList());
+
+        if (differentNums.isEmpty()) {
+            return "1등";
+        } else if (differentNums.size() == 1 && differentNums.get(0).equals(bonusNum)) {
+            return "2등";
+        } else if (differentNums.size() == 1 && !differentNums.get(0).equals(bonusNum)) {
+            return "3등";
+        } else if (differentNums.size() == 2) {
+            return "4등";
+        } else if (differentNums.size() == 3) {
+            return "5등";
+        }
+        return "당첨 X";
     }
 }
