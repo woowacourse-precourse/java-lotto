ae4ce82b62d7809cefd163fa9a5436dc45ee4395
dev_kong <ahdjd5@gmail.com>
2022-11-11 13:12:01 +0900

    feat(WinningNumber): add countWinning method

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -7,14 +7,27 @@ import lotto.utils.Validator;
 public class WinningNumbers {
     private final List<Integer> numbers;
 
-    public WinningNumbers (List<Integer> numbers) {
+    public WinningNumbers(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
+    //TODO : refactoring indent -1
+    public int countWinning(List<Integer> lottoNumbers) {
+        int count = 0;
+
+        for (int lottoNumber : lottoNumbers) {
+            if (numbers.contains(lottoNumber)) {
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+
     private void validate(List<Integer> numbers) {
         Validator.containDuplicate(numbers);
-        Validator.listLengthCheck(numbers,6);
+        Validator.listLengthCheck(numbers, 6);
         for (int number : numbers) {
             Validator.isInRange(number);
         }
