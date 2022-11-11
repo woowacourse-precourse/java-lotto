5068ed4bc9483972c2407feae5aee2bb7dc81b12
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-11 17:19:33 +0900

    feat(OutputUi): 당첨번호 입력 메시지 출력
    - 당첨번호 입력 메시지 출력
    - 보너스번호 입력 메시지 출력

--- src/main/java/lotto/ui/OutputUi.java
+++ src/main/java/lotto/ui/OutputUi.java
@@ -8,6 +8,9 @@ public class OutputUi {
 
     private static final String INSERT_MONEY_INFO_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String PURCHASED_LOTTO_COUNT_INFO_MESSAGE_FORMAT = "\n%d개를 구매했습니다.";
+    private static final String INPUT_WINNING_NUMBERS_INFO_MESSAGE = "\n당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_INFO_MESSAGE = "\n보너스 번호를 입력해 주세요.";
+
 
     public static void printInsertMoneyInfoMessage() {
         System.out.println(INSERT_MONEY_INFO_MESSAGE);
@@ -21,4 +24,13 @@ public class OutputUi {
             System.out.println(lotto.toString());
         }
     }
+
+    public static void printInputWinningNumbersInfoMessage() {
+        System.out.println(INPUT_WINNING_NUMBERS_INFO_MESSAGE);
+    }
+
+    public static void printInputBonusNumberInfoMessage() {
+        System.out.println(INPUT_BONUS_NUMBER_INFO_MESSAGE);
+    }
+
 }
