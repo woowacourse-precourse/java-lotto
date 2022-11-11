cb1857de27bf6cb40e49f74789b64a5999105d2a
NotableDeveloper <castlereal@naver.com>
2022-11-10 22:11:36 -0800

    <feat> : 구매 금액만큼 추첨 번호를 생성하는 기능 사용자가 구매한 금액만큼 추첨 번호를 생성하는 기능 구현. 관련 테스트 코드 작성 및 통과하는 것까지 확인.

--- src/main/java/lotto/LotteryNumber.java
+++ src/main/java/lotto/LotteryNumber.java
@@ -1,4 +1,27 @@
 package lotto;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+
 public class LotteryNumber {
+    private List<List<Integer>> numbers = new ArrayList<>();
+
+    public LotteryNumber(int money) {
+        int iterationNumber = money / 1000;
+        generateNumbers(iterationNumber);
+    }
+
+    private void generateNumbers(int iterationNumber) {
+        for(int i = 0; i < iterationNumber; i++) {
+            numbers.add(pickUniqueNumbersInRange(1, 45, 6));
+        }
+    }
+
+    public List<List<Integer>> getNumbers() {
+        return this.numbers;
+    }
 }
--- src/test/java/lotto/LotteryNumberTest.java
+++ src/test/java/lotto/LotteryNumberTest.java
@@ -3,10 +3,13 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class LotteryNumberTest {
     @DisplayName("추첨 번호는 사용자가 구매한 수량만큼 생성되어야 한다.")
     @Test
     void createLotteryNumberByPurchaseQuantity() {
-        LotteryNumber lotteryNumber = new LotteryNumber();
+        LotteryNumber lotteryNumber = new LotteryNumber(3000);
+        assertThat(lotteryNumber.getNumbers().size() == 3);
     }
 }
