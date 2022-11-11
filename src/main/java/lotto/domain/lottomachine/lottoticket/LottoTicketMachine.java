12cdeb7108dd2b2dc5649026fb24b017abe5f9d8
chanminmaxscaler <jcm04@naver.com>
2022-11-11 18:03:56 +0900

    feat: LottoTicketMachine 클래스에 난수 6개를 담은 리스트를 반환하는 기능을 구현한다
    난수 6개를 담은 리스트를 반환하는 기능을 generateSixRandomNumbers 메소드로 구현한다.
    generateSixRandomNumbers 메소드는 Randoms.pickUniqueNumbersInRange 메소드를 이용해 중복이 없는, 1부터 45까지의 범위의, 6개의 숫자를 리스트에 담아 반환한다.

--- src/main/java/lotto/domain/lottomachine/lottoticket/LottoTicketMachine.java
+++ src/main/java/lotto/domain/lottomachine/lottoticket/LottoTicketMachine.java
@@ -1,8 +1,17 @@
 package lotto.domain.lottomachine.lottoticket;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class LottoTicketMachine {
 
     private int convertMoneyIntoTickets(int money) {
         return money / 1000;
     }
+
+    private List<Integer> generateSixRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
 }
