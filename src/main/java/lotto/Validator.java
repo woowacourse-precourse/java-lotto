da30d3243b32fb5d7c3da2635791a79b55fc0f40
은지 <midzy0228@gmail.com>
2022-11-11 22:44:22 +0900

    feat(구입금액): 100000원 미만인지 확인 기능 구현

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -7,6 +7,7 @@ public class Validator {
     public void validDatePurchaseAmount(String purchaseAmount) {
         validDateType(purchaseAmount);
         validDateThousandUnitNumber(Integer.parseInt(purchaseAmount));
+        validRangeNumber(Integer.parseInt(purchaseAmount));
     }
 
     private void validDateType(String input) {
@@ -19,6 +20,11 @@ public class Validator {
     private void validDateThousandUnitNumber(Integer purchase) {
         if (purchase % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위의 숫자를 입력하셔야 합니다.");
+        }
+    }
+    private void validRangeNumber(Integer purchase) {
+        if (! (1000 <= purchase && purchase <= 100000)) {
+            throw new IllegalArgumentException("[ERROR] 로또는 최대 100,000원까지 구매하실 수 있습니다.");
         }
     }
 }
