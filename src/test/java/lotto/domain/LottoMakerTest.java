ce297df76ba1b5f53f7dd8ad4c565c078f9d3ddf
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-11 13:54:04 +0900

    test: 랜덤 로또를 여러개 만드는 기능이 작동하는지 테스트로 검증

--- src/test/java/lotto/domain/LottoMakerTest.java
+++ src/test/java/lotto/domain/LottoMakerTest.java
@@ -1,2 +1,23 @@
-package lotto.domain;public class LottoMakerTest {
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoMakerTest {
+
+    LottoMaker lottoMaker = new LottoMaker();
+
+    @DisplayName("랜덤 로또 리스트가 잘 생성되는지 확인")
+    @Test
+    void 여러개의_랜덤_로또_출력_확인() {
+        List<Lotto> lottos = lottoMaker.getRandomLottos(5);
+        assertThat(lottos)
+                .hasSize(5)
+                .allMatch((lotto) -> lotto instanceof Lotto);
+    }
 }
