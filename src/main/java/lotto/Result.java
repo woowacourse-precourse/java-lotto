ea2f80998c0f84b8b0331988d935d356b3aa49f3
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-12 00:36:53 +0900

    feat : 당첨 번호와 무작위 생성 번호를 대조하여 결과를 만드는 기능 구현
    Result 클래스 생성
    compareEachLotto() 구현
    compareNumbers() 구현
    compareResult() 구현
    winFirst()~winFifth() 구현

---
+++ src/main/java/lotto/Result.java
@@ -0,0 +1,84 @@
+package lotto;
+
+public class Result {
+
+    public final int FIRST_PRIZE = 2_000_000_000;
+    public final int SECOND_PRIZE = 30_000_000;
+    public final int THIRD_PRIZE = 1_500_000;
+    public final int FOURTH_PRIZE = 50_000;
+    public final int FIFTH_PRIZE = 5000;
+
+    int count=0;
+    int bonus_index=0;
+    double total_prize=0;
+    int [] winning = new int[5];
+
+    public Result() {
+
+    }
+
+    public void compareNumbers(Lottos userLottos, Lotto winningLotto, LottoNumber bonus) {
+        for(int sheet=0;sheet<userLottos.size();sheet++){
+            Lotto lotto = userLottos.get(sheet);
+            compareEachLotto(winningLotto, bonus, lotto);
+            compareResult();
+            count=0;
+            bonus_index=0;
+        }
+        calculateEarningRate(total_prize, Buyer.money);
+    }
+
+    private void compareResult() {
+        winFirst();
+        winSecond();
+        winThird();
+        winFourth();
+        winFifth();
+    }
+
+    private void winFifth() {
+        if(count==3){
+            winning[0]++;
+            total_prize+=FIFTH_PRIZE;
+        }
+    }
+
+    private void winFourth() {
+        if(count==4){
+            winning[1]++;
+            total_prize+=FOURTH_PRIZE;
+        }
+    }
+
+    private void winThird() {
+        if(count==5&&bonus_index==0){
+            winning[2]++;
+            total_prize+=THIRD_PRIZE;
+        }
+    }
+
+    private void winSecond() {
+        if(count==5&&bonus_index==1){
+            winning[3]++;
+            total_prize+=SECOND_PRIZE;
+        }
+    }
+
+    private void winFirst() {
+        if(count==6){
+            winning[4]++;
+            total_prize+=FIRST_PRIZE;
+        }
+    }
+
+    private void compareEachLotto(Lotto winningLotto, LottoNumber bonus, Lotto lotto) {
+        for(int order = 0; order< lotto.size(); order++){
+            if(winningLotto.contains(lotto.get(order))){
+                count++;
+            }
+            if(lotto.get(order)== bonus){
+                bonus_index=1;
+            }
+        }
+    }
+}
