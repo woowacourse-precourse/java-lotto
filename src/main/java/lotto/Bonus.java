0ba57286031483219f2621d5852f0302f8ec6cad
HyeonjungYun <guswnd9971@gmail.com>
2022-11-11 23:11:34 +0900

    "style(Bonus): 변수명 변경"

--- src/main/java/lotto/Bonus.java
+++ src/main/java/lotto/Bonus.java
@@ -26,20 +26,20 @@ public class Bonus {
         return number;
     }
 
-    public void checkBonusNumberRange(int lottoBonusNumber){
-        if (lottoBonusNumber < 1 || lottoBonusNumber > 45){
+    public void checkBonusNumberRange(int bonusNumber){
+        if (bonusNumber < 1 || bonusNumber > 45){
             throw new IllegalArgumentException("[ERROR]: 보너스 번호는 반드시 1~45 이내에 숫자여야 합니다.");
         }
     }
 
-    public void checkBonusDuplicatedNumber(int lottoBonusNumber, List<Integer> lotto){
-        if (lotto.contains(lottoBonusNumber)){
+    public void checkBonusDuplicatedNumber(int bonusNumber, List<Integer> lotto){
+        if (lotto.contains(bonusNumber)){
             throw new IllegalArgumentException("[ERROR]: 보너스 번호는 입력한 당첨번호와 중복되어선 안됩니다.");
         }
     }
 
-    private void isRealNumber (String inputStatement) {
-        for (char element: inputStatement.toCharArray()) {
+    private void isRealNumber (String inputBonusNumber) {
+        for (char element: inputBonusNumber.toCharArray()) {
             if (element < 48 || element > 57) {
                 throw new IllegalArgumentException("[ERROR]: 숫자만 입력할 수 있습니다.");
             }
