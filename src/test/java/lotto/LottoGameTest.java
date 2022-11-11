74d50c67aafb1697df81d6379edbb3ecf653e98d
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-12 01:11:56 +0900

    test: 로또 당첨번호 입력 로직 테스트

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -2,10 +2,36 @@ package lotto;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 class LottoGameTest {
+
+    @Test
+    void translateNumbers() {
+
+    }
+
+    @Test
+    void 당첨번호_입력받아_세팅하기() {
+        LottoGame lottoGame = new LottoGame();
+        List<Integer> integers = lottoGame.translateNumbers("1");
+        List<Integer> result = Arrays.asList(1);
+
+        assertThat(integers).isEqualTo(result);
+    }
+
+    @Test
+    void 당첨번호_입력받아_세팅하기2() {
+        LottoGame lottoGame = new LottoGame();
+        List<Integer> integers = lottoGame.translateNumbers("1,2,3,4,5");
+        List<Integer> result = Arrays.asList(1, 2, 3, 4, 5);
+
+        assertThat(integers).isEqualTo(result);
+    }
 
     @Test
     void 구매금액에_따른_로또개수_반환1(){
