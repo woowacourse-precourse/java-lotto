2984b3b9771d84ffd0035f838acf5b0eb8a138d1
Cho SangHyun <98shcho@naver.com>
2022-11-12 00:45:26 +0900

    feat(LottoGameSimulator): 뼈대 코드 추가

--- src/main/java/lotto/LottoGameSimulator.java
+++ src/main/java/lotto/LottoGameSimulator.java
@@ -1,4 +1,35 @@
 package lotto;
 
 public class LottoGameSimulator {
+    public LottoGameSimulator() {
+
+    }
+
+    public void run() {
+        getInputOfMoney();
+        printGeneratedLotto();
+        getInputOfWinningNumbers();
+        getInputOfBonusNumber();
+        printResult();
+    }
+
+    public void getInputOfMoney() {
+
+    }
+
+    public void printGeneratedLotto() {
+
+    }
+
+    public void getInputOfWinningNumbers() {
+
+    }
+
+    public void getInputOfBonusNumber() {
+
+    }
+
+    public void printResult() {
+
+    }
 }
