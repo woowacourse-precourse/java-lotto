50efd634354513dab71956207349902eee78b7df
chanminmaxscaler <jcm04@naver.com>
2022-11-11 18:00:10 +0900

    test: LottoTicketMachine 클래스의 난수 6개를 담은 리스트를 반환하는 기능의 테스트 코드를 작성한다
    generateSixRandomNumbers 메소드가 여섯 개의 숫자를 담은 리스트를 반환하는지 확인한다.

--- src/test/java/lotto/domain/lottomachine/lottoticket/LottoTicketMachineTest.java
+++ src/test/java/lotto/domain/lottomachine/lottoticket/LottoTicketMachineTest.java
@@ -2,12 +2,14 @@ package lotto.domain.lottomachine.lottoticket;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,5 +31,16 @@ class LottoTicketMachineTest {
         int tickets = (int) method.invoke(lottoTicketMachine, money);
 
         assertThat(tickets).isEqualTo(numberOfTickets);
+    }
+
+    @DisplayName("generateSixRandomNumbers 메소드가 여섯 개의 숫자를 담은 리스트를 반환하는지 확인")
+    @Test
+    void generateSixRandomNumbers_test() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Method method = lottoTicketMachine.getClass().getDeclaredMethod("generateSixRandomNumbers");
+        method.setAccessible(true);
+
+        List<Integer> numbers = (List<Integer>) method.invoke(lottoTicketMachine);
+
+        assertThat(numbers.size()).isEqualTo(6);
     }
 }
