3d304f27ca00c2c6dc60bd56744e87d50eecccdb
yg0585 <yg0585@naver.com>
2022-11-11 16:04:18 +0900

    feat: 로또 번호 출력

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import java.util.List;
+
 public class OutputView {
     public static void printStartMesseage(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void printLottoNumber(int count){
+        System.out.println(count + "개를 구매했습니다.");
+    }
+
+    public static void printLotto(List<Lotto> lottos){
+        for (Lotto lotto: lottos){
+            System.out.println(lotto);
+        }
     }
 }
