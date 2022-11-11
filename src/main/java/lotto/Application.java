f88ce6a99d53968ae5ef6e76fa8146efcd708953
jin-chang-ho <ckdgh8204@naver.com>
2022-11-12 02:17:11 +0900

    Feat: 로또 구입 금액 입력 문구 출력
    Ref: #3

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,10 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 import java.util.*;
 
 public class Application {
+    static void ask_how_much_money() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
     static String how_much_money() {
         Scanner scanner = new Scanner(System.in);
 
@@ -15,6 +19,8 @@ public class Application {
     }
 
     public static void main(String[] args) {
+        ask_how_much_money();
+
         String user_money;
         user_money = how_much_money();
     }
