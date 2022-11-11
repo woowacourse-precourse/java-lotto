4b729d34f34850aa159e277f7b41ca24768f2f20
leesky <leehaneul0623@gmail.com>
2022-11-11 13:12:49 +0900

    feat(lotto) : 로또 추첨 구현
    로또의 당첨 등수를 알기 위해서 로또 추첨을 구현
    
    테스트 성공 목록 :
     - 맞은 번호 갯수 별로 예상되는 등수 반환 테스트

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,7 +17,21 @@ public class Lotto {
     }
 
     public int draw(List<Integer> answer, int bonus) {
+        int correct = getCorrectCount(answer);
+        if (correct == 6) return 1;
+        if (correct == 5 && getNumbers().contains(bonus)) return 2;
+        if (correct == 5) return 3;
+        if (correct == 4) return 4;
+        if (correct == 3) return 5;
         return 0;
+    }
+
+    private int getCorrectCount(List<Integer> answer) {
+        int count = 0;
+        for (Integer number : getNumbers()) {
+            if (answer.contains(number)) count += 1;
+        }
+        return count;
     }
 
     private void validate(List<Integer> numbers) {
@@ -26,10 +40,9 @@ public class Lotto {
         }
 
         Set<Integer> set = new HashSet<>(numbers);
-        if(set.size() != 6) {
+        if (set.size() != 6) {
             throw new IllegalArgumentException();
-        };
+        }
     }
-
 
 }
