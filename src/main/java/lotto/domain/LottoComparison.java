f2ce0d3ce7f29cedb4da1153ff63de5171c53464
alstjr706 <alstjr706@naver.com>
2022-11-11 15:07:41 +0900

    refactor : (로또 게임) 랜덤숫자와 당첨번호를 비교 로직 수정

--- src/main/java/lotto/domain/LottoComparison.java
+++ src/main/java/lotto/domain/LottoComparison.java
@@ -16,16 +16,21 @@ public class LottoComparison {
     public List<Integer> findNumber(List<List<Integer>> randomLotto) {
         Lotto answer = winningNumber.getAnswer();
         List<Integer> winning = answer.getNumbers();
-
-        System.out.println(winning);
+        System.out.println("당첨 번호: " + winning);
+        System.out.println("------------");
 
         List<Integer> result = new ArrayList<>();
 
         for (List<Integer> random : randomLotto) {
             int cont = 0;
+
             for (int i = 0; i < winning.size(); i++) {
-                if (random.get(i).equals(winning.get(i))) {
-                    cont++;
+                int crrentNumber = winning.get(i);
+
+                for (int j = 0; j < random.size(); j++) {
+                    if (crrentNumber == random.get(j)) {
+                        cont++;
+                    }
                 }
             }
             result.add(cont);
