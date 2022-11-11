ee06404c4c84963c5ded2483a736dad80a47023b
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-11 10:08:30 +0900

    style : 코드 포맷팅 수정

--- src/main/java/lotto/LottoIssuer.java
+++ src/main/java/lotto/LottoIssuer.java
@@ -10,7 +10,7 @@ import static lotto.ui.ConsoleOutput.*;
 public class LottoIssuer {
 
 	private static int purchasedLotto;
-	public static List<List<Integer>> issuedLottoes = new ArrayList<List<Integer>>();
+	public static List<List<Integer>> issuedLottoes = new ArrayList<>();
 
 	public LottoIssuer(long totalAmount) {
 		countLotto(totalAmount);
@@ -20,7 +20,7 @@ public class LottoIssuer {
 	private void countLotto(long totalAmount) {
 		int purchasedLotto = Math.toIntExact(totalAmount) / Setup.LOTTO_PRICE.getValue();
 		PrintNotificationMessage(purchasedLotto);
-		this.purchasedLotto = purchasedLotto;
+		LottoIssuer.purchasedLotto = purchasedLotto;
 	}
 
 	private static void issueLotto() {
@@ -31,7 +31,6 @@ public class LottoIssuer {
 	}
 
 	private static List<Integer> generateLottoNumbers() {
-		List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-		return numbers;
+		return Randoms.pickUniqueNumbersInRange(1, 45, 6);
 	}
 }
