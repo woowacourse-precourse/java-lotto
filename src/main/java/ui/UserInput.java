09a390f4c4dfabb1a104b8cb2fc64fc31283bbb4
Limjongho <whdgh9873@icloud.com>
2022-11-11 13:05:42 +0900

    feat(UserInput): 사용자의 구매 금액 입력 기능 추가

---
+++ src/main/java/ui/UserInput.java
@@ -0,0 +1,11 @@
+package ui;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInput {
+
+    public String buyLotto() {
+        String money = Console.readLine();
+        return money;
+    }
+}
