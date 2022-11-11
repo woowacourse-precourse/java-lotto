a3e68b997e01c18e43b97af9552e75553b9a8b1f
kokodak <kokodakadokok@gmail.com>
2022-11-11 15:15:37 +0900

    feat(lottoController): 로또 프로그램을 시작하는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,29 @@
 package lotto.controller;
 
+import java.util.Map;
+import lotto.domain.LottoGenerator;
+import lotto.domain.Lottos;
+import lotto.domain.WinningLotto;
+import lotto.domain.WinningRank;
+import lotto.domain.WinningStatistics;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class LottoController {
 
     public void lottoStart() {
+        int purchaseAmount = InputView.getPurchaseAmount();
+        LottoGenerator lottoGenerator = new LottoGenerator(purchaseAmount);
+        Lottos lottos = new Lottos(lottoGenerator.generateLottos());
 
+        OutputView.printHowManyLottoUserPurchased(lottoGenerator.getLottoQuantity());
+        OutputView.printLottos(lottos);
+
+        WinningLotto winningLotto = new WinningLotto(InputView.getLottoNumber(), InputView.getLottoBonusNumber());
+
+        Map<WinningRank, Integer> winningDetails = WinningStatistics.getWinningDetails(lottos, winningLotto);
+        OutputView.printWinningStatistics();
+        OutputView.printWinningDetails(winningDetails);
+        OutputView.printLottoYield(WinningStatistics.getLottoYield(winningDetails, purchaseAmount));
     }
 }
