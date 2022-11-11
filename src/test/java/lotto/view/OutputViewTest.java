7a7f878c9fe0b0bf55afb09c5c2acaca2f2254f9
ksh-beginner <ohk9134@naver.com>
2022-11-11 20:56:22 +0900

    test(OutputViewTest) : 로또 당첨 내역을 출력하는 기능 테스트

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -1,12 +1,15 @@
 package lotto.view;
 
+import lotto.LottoWinningRank;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -41,5 +44,23 @@ class OutputViewTest {
         String purchaseLottoCountMessage = "[1, 2, 3, 4, 5, 6]\n";
 
         assertThat(output.toString()).isEqualTo(purchaseLottoCountMessage);
+    }
+
+    @Test
+    void 로또_당첨_내역_출력() {
+        Map<LottoWinningRank, Integer> lottoWinningRankMap = new HashMap<>();
+        lottoWinningRankMap.put(LottoWinningRank.FIRST, 0);
+        lottoWinningRankMap.put(LottoWinningRank.SECOND, 0);
+        lottoWinningRankMap.put(LottoWinningRank.THIRD, 0);
+        lottoWinningRankMap.put(LottoWinningRank.FOURTH, 0);
+        lottoWinningRankMap.put(LottoWinningRank.FIFTH, 1);
+        outputView.printLottoWinningStatistics(lottoWinningRankMap);
+        String lottoWinningStatisticsMessage = "3개 일치 (5,000원) - 1개\n" +
+                "4개 일치 (50,000원) - 0개\n" +
+                "5개 일치 (1,500,000원) - 0개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n" +
+                "6개 일치 (2,000,000,000원) - 0개\n";
+
+        assertThat(output.toString()).isEqualTo(lottoWinningStatisticsMessage);
     }
 }
