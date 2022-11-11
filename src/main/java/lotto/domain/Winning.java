70a6db938b58382775964478852641160c671925
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-11 20:59:17 +0900

    feat: Winning 생성자를 이용해 해시맵을 초기화하는 기능 구현

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -6,13 +6,23 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Winning {
-    private List<Ranking> score;
+    private HashMap<Ranking, Integer> score;
 
-    public Winning(){}
+    public Winning(){
+        initializeScore();
+    }
 
     public void addRankingOfPlayer(Ranking lottoRanking){}
 
     public HashMap<Ranking, Integer> figureSameRanking(){
         return new HashMap<>();
+    }
+
+    private void initializeScore(){
+        score.put(Ranking.THREE_MATCH, 0);
+        score.put(Ranking.FOUR_MATCH, 0);
+        score.put(Ranking.FIVE_MATCH, 0);
+        score.put(Ranking.FIVE_MATCH_WITH_BONUS, 0);
+        score.put(Ranking.SIX_MATCH, 0);
     }
 }
