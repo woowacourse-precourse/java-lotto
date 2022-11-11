e392ac2b0577d29e07997696151942b2eb963a34
K-Diger <kitten.diger@gmail.com>
2022-11-11 17:44:12 +0900

    feat : 클라이언트 코드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,29 @@
 package lotto;
 
+import lotto.calculator.YieldCalculator;
+import lotto.client.Client;
+import lotto.lotteryhost.LottoHost;
+import lotto.lotteryshop.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Client client = new Client();
+        int purchaseNumber = client.getPurchaseMoney();
+
+        List<Integer> winningLotto = new ArrayList<>();
+        Lotto lotto = new Lotto(winningLotto);
+        List<List<Integer>> generatedLottoNumber = lotto.generateLottoNumber(purchaseNumber);
+        lotto.getGeneratedLottoNumber(generatedLottoNumber);
+        winningLotto = lotto.publishWinningLottoNumbers();
+        lotto.publishWinningBonusNumber(winningLotto);
+
+        LottoHost lottoHost = new LottoHost();
+        List<Integer> matchResult = lottoHost.match(generatedLottoNumber, winningLotto);
+
+        YieldCalculator yieldCalculator = new YieldCalculator();
+        yieldCalculator.getYield(yieldCalculator.getStatistics(matchResult), purchaseNumber);
     }
 }
