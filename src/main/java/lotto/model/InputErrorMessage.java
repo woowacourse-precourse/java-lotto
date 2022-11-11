bc6dfce5d92e2db47f2f0536074bc5c52dbe7244
Cyma-s <ssol0319@naver.com>
2022-11-12 02:03:04 +0900

    feat(InputErrorMessage): add error code that verifies invalid format of money

--- src/main/java/lotto/model/InputErrorMessage.java
+++ src/main/java/lotto/model/InputErrorMessage.java
@@ -3,7 +3,8 @@ package lotto.model;
 public enum InputErrorMessage {
     INVALID_DIGITS_COUNT("로또 번호는 6자리여야 합니다."),
     DUPLICATE_NUMBERS("당첨 번호의 숫자가 중복됩니다."),
-    OUT_OF_RANGE_NUMBER("로또 번호는 1부터 45 사이의 숫자여야 합니다.");;
+    OUT_OF_RANGE_NUMBER("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INVALID_FORMAT_MONEY("구입 금액은 숫자여야 합니다.");
     private static final String ERROR_HEADER = "[ERROR] ";
     private final String errorMessage;
 
