919a742f08b544a194007b6f18bdc86818690ca0
chanminmaxscaler <jcm04@naver.com>
2022-11-12 01:11:21 +0900

    feat: WinningNumber 클래스를 구현한다
    WinningNumber 클래스 int형의 당첨번호 혹은 보너스 번호를 포장하는 클래스이다. 당첨번호인 것을 검증하는 기능을 갖는다.
    validateDuplication 메소드는 당첨번호을 검증한다. 숫자가 1부터 45의 범위 안에 있는지 확인한다. 밖에 있을 경우 IllegalArgumentException을 발생시킨다.

---
+++ src/main/java/lotto/domain/lottomachine/winningnumber/WinningNumber.java
@@ -0,0 +1,35 @@
+package lotto.domain.lottomachine.winningnumber;
+
+import java.util.Objects;
+
+public class WinningNumber {
+    private final int number;
+
+    public WinningNumber(int number) {
+        validateRange(number);
+        this.number = number;
+    }
+
+    private void validateRange(int number) {
+        if (isOutOfRange(number)) {
+            throw new IllegalArgumentException("[ERROR] 숫자는 1부터 45에 속해야 합니다.");
+        }
+    }
+
+    private boolean isOutOfRange(int number) {
+        return number < 1 || number > 45;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WinningNumber that = (WinningNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
