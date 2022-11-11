5bce04065125b63895e19eb232d8b1514a31fdfc
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-11-11 18:41:08 +0900

    add : 예외를 잡아 IllegalArgumentException 발생 클래스

---
+++ src/main/java/lotto/util/Validator.java
@@ -0,0 +1,19 @@
+package lotto.util;
+
+import static lotto.util.Message.WRONG_INPUT;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class Validator {
+    private static final String MONEY_PATTERN = "^[0-9]*$";
+    private static final int MONEY_UNIT = 1000;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int TOTAL_NUMBER = 6;
+    private static final String STRING_TOKEN = ",";
+
+    private List<Integer> list = new LinkedList<>();
+
+
+}
