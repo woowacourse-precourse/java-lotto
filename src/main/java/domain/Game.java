3b67846c60cdbeb28b8ce9efe5826b1d59bc8bb2
glass <glass7312@gmail.com>
2022-11-11 23:18:06 +0900

    feat,Refactor:기능 추가,변수명 변경(Game class) 로또 갯수 반환하는 기능 추가 buyingLotto -> paymentAmount

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -7,28 +7,37 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Game {
+    public static final int LottoPrice = 1000;
+
     public static String winningNumbers_Input;
     public static int bonusNumber;
-    public static int buyingLotto;
+    public static int paymentAmount;
     private static List<Integer> winning_numbers;
 
-    public static void changeToInt(String numbers){
-        String[] str=numbers.split(",");
-        List<String> number=List.of(str);
-        List<Integer> newforms=number.stream().map(s->Integer.parseInt(s)).collect(Collectors.toList());
-        Game.winning_numbers=newforms;
+    public static void changeToInt(String numbers) {
+        String[] str = numbers.split(",");
+        List<String> number = List.of(str);
+        List<Integer> newforms = number.stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+        Game.winning_numbers = newforms;
     }
+
     public static String read_WinningNumbers() {
         System.out.print("당첨번호를 입력해주세요.");
-        winningNumbers_Input=Console.readLine();
+        winningNumbers_Input = Console.readLine();
         return winningNumbers_Input;
     }
-    public static void read_buyingLottoAmount(){
+
+    public static void read_buyingLottoAmount() {
         System.out.print("구입 금액을 입력해주세요." + "\n");
-        buyingLotto=Integer.parseInt(Console.readLine());
+        paymentAmount = Integer.parseInt(Console.readLine());
     }
-    public static void read_bonusNumber(){
+
+    public static void read_bonusNumber() {
         System.out.print("보너스 번호를 입력해주세요." + "\n");
-        bonusNumber=Integer.parseInt(Console.readLine());
+        bonusNumber = Integer.parseInt(Console.readLine());
+    }
+
+    public static int getLottoCount() {
+        return paymentAmount / LottoPrice;
     }
 }
