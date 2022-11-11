131634f52ec43d624a26cf75ce86fe3284e61ff6
woo-chang <clllickme@naver.com>
2022-11-11 20:33:12 +0900

    test(WinningLotto): 보너스 숫자가 정해진 범위에 속하지 않으면 예외 발생시키는 기능 테스트

---
+++ src/test/java/lotto/entity/WinningLottoTest.java
@@ -0,0 +1,35 @@
+package lotto.entity;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningLottoTest {
+
+    @DisplayName("보너스 번호가 정해진 시작 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByOutOfStartRangeBonus() {
+        //given
+        int bonus = 0;
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        //then
+        assertThatThrownBy(() -> new WinningLotto(lotto, bonus)).isInstanceOf(
+            IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 정해진 끝 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByOutOfEndRangeBonus() {
+        //given
+        int bonus = 46;
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        //then
+        assertThatThrownBy(() -> new WinningLotto(lotto, bonus)).isInstanceOf(
+            IllegalArgumentException.class);
+    }
+
+}
