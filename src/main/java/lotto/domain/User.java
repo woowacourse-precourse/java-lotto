995c362088577dc36bf47b8a4eee717f5cbc6b8b
nuyh <hjaehyun25@gmail.com>
2022-11-11 19:19:13 +0900

    feat(User): 로또 발행하기 기능 추가

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,10 +1,20 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.exception.Input;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class User {
     private int purchaseAmount;
     private int earnedAmouont;
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
 
     public int getEarnedAmouont() {
         return earnedAmouont;
@@ -17,11 +27,21 @@ public class User {
     /**
      * 도메인 로직
      */
-    public void inputPurchaseAmouont(String input) {
+    public void inputPurchaseAmount(String input) {
         validateNumeric(input);
         validateUnitOfWon(input);
 
         this.purchaseAmount = Integer.parseInt(input);
+    }
+
+    public void createLottos() {
+        int count = purchaseAmount / 1000;
+
+        while (count-- > 0) {
+            List<Integer> randoms = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            randoms.sort(Comparator.naturalOrder());
+            lottos.add(new Lotto(randoms));
+        }
     }
 
     private void validateNumeric(String input) {
