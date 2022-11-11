618264867e64eaa5c53fb70f41e9f44bc047eaa7
dydtmd6801 <dydtmd6801@naver.com>
2022-11-12 00:19:07 +0900

    feat(Client): 로또 구입 금액 입력받는 기능

---
+++ src/main/java/view/Client.java
@@ -0,0 +1,10 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+public class Client {
+    public int getLottoMoney(){
+        return Integer.parseInt(Console.readLine());
+    }
+}
