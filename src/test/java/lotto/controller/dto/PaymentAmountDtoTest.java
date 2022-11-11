5a3cfeb33083f274532a07a82f5a200a204c9aba
KTH-96 <taehyun960@gmail.com>
2022-11-12 04:02:53 +0900

    test(PaymentAmountDtoTest): 금액이 1000으로 나누어 떨어지지 않는 예외테스트 작성

--- src/test/java/lotto/controller/dto/PaymentAmountDtoTest.java
+++ src/test/java/lotto/controller/dto/PaymentAmountDtoTest.java
@@ -1,9 +1,7 @@
 package lotto.controller.dto;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -17,5 +15,14 @@ class PaymentAmountDtoTest {
         assertThatThrownBy(() -> new PaymentAmountDto(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 지불금액에 정수값이 아닌 값이 포함되어 있습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1100", "100", "234124", "10001"})
+    @DisplayName("지불금액이 1000으로 나누어 떨어지지않는 예외테스트")
+    void isDivisionTest(String input) {
+        assertThatThrownBy(() -> new PaymentAmountDto(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 지불금액이 1000으로 나누어 떨어지지 않습니다.");
     }
 }
