9508c2142eae45f6d99abc35b308d64343601c06
chanminmaxscaler <jcm04@naver.com>
2022-11-11 21:10:09 +0900

    feat: LottoTicketMachine 클래스에 로또 티켓을 발행하는 기능을 구현한다
    로또 티켓을 발행하는 기능을 createLotto, issueLottoTicket, issueLottoTickets 메소드로 구현한다.
    createLotto 메소드는 숫자 리스트를 입력받아 Lotto 객체를 생성해 반환한다.
    issueLottoTicket 메소드는 6자리의 난수를 리스트로 생성해 정렬한 후, Lotto 객체를 생성해 반환한다.
    issueLottoTickets 메소드는 금액을 입력하면 해당 금액을 convertMoneyIntoTickets 메소드로 티켓 장수로 바꾼다. 그 후 로또 리스트를 생성해 티켓 장수만큼 로또 티켓을 발행한다. 발행한 티켓을 리스트에 저장하고, 일급컬렉션을 생성해 반환한다.

--- src/main/java/lotto/domain/lottomachine/lottoticket/LottoTicketMachine.java
+++ src/main/java/lotto/domain/lottomachine/lottoticket/LottoTicketMachine.java
@@ -2,10 +2,31 @@ package lotto.domain.lottomachine.lottoticket;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoTicketMachine {
+
+    public LottoTickets issueLottoTickets(int money) {
+        int numberOfTickets = convertMoneyIntoTickets(money);
+        List<Lotto> tickets = new ArrayList<>();
+
+        for (int i = 0; i < numberOfTickets; i++) {
+            tickets.add(issueLottoTicket());
+        }
+        return new LottoTickets(tickets);
+    }
+
+    private Lotto issueLottoTicket() {
+        List<Integer> numbers = generateSixRandomNumbers();
+        sortInAscendingOrder(numbers);
+        return createLotto(numbers);
+    }
+
+    private Lotto createLotto(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
 
     private int convertMoneyIntoTickets(int money) {
         return money / 1000;
