1f2d4e34f1b994ab9925e67b621e9498c7b12fe6
baksohyeon <bori12370@gmail.com>
2022-11-11 16:45:52 +0900

    test: amount validation method test code
    [테스트 목록]
    1. 금액이 음수 값인 경우
    2. 금액이 단위 값보다 적은 경우
    3. 올바른 값인 경우

---
+++ src/test/java/lotto/service/validation/isValidAmountTest.java
@@ -0,0 +1,46 @@
+package lotto.service.validation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class isValidAmountTest {
+    @DisplayName("금액이 음수 값일 경우 false 반환")
+    @Test
+    void amount_is_minus_Test() {
+        int amount = -100;
+        boolean EXPECTED_RESULT = false;
+        boolean result = AmountValidation.isValidAmount(amount);
+        assertThat(result).isEqualTo(EXPECTED_RESULT);
+    }
+
+    @DisplayName("금액이 1000원 이하일 경우 false 반환")
+    @Test
+    void amount_is_lower_than_unit_of_amount_Test() {
+        int amount = -100;
+        boolean EXPECTED_RESULT = false;
+        boolean result = AmountValidation.isValidAmount(amount);
+        assertThat(result).isEqualTo(EXPECTED_RESULT);
+    }
+
+    @DisplayName("금액이 1000원 단위가 아닌 경우 false 반환")
+    @Test
+    void amount_does_not_match_the_unit() {
+        int amount = 1001;
+        boolean EXPECTED_RESULT = false;
+        boolean result = AmountValidation.isValidAmount(amount);
+        assertThat(result).isEqualTo(EXPECTED_RESULT);
+    }
+
+    @DisplayName("금액이 1000원 단위인 올바른 값인 경우 true 반환")
+    @Test
+    void amount_conditions_are_satisfied() {
+        int amount = 2345000;
+        boolean EXPECTED_RESULT = true;
+        boolean result = AmountValidation.isValidAmount(amount);
+        assertThat(result).isEqualTo(EXPECTED_RESULT);
+    }
+}
