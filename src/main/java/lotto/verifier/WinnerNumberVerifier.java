b0467e0a53fc5cb9e3d38622638cf44e8c7b4ade
woosung1223 <dntjd991223@naver.com>
2022-11-11 11:13:01 +0900

    feat(WinnerNumberVerifier): checkEachNumeric 메소드 생성

--- src/main/java/lotto/verifier/WinnerNumberVerifier.java
+++ src/main/java/lotto/verifier/WinnerNumberVerifier.java
@@ -3,6 +3,8 @@ package lotto.verifier;
 import lotto.util.Constant;
 import lotto.util.ExceptionMessage;
 
+import java.util.Arrays;
+
 public class WinnerNumberVerifier implements Verifier {
     @Override
     public void check(String input) {
@@ -11,6 +13,15 @@ public class WinnerNumberVerifier implements Verifier {
     private void checkNumberCount(String input) {
         if (input.split(",").length != Constant.LOTTO_SIZE) {
             throw new IllegalArgumentException(ExceptionMessage.NUMBER_COUNT_NOT_EQUAL);
+        }
+    }
+
+    private void checkEachNumeric(String input) {
+        String[] divided = input.split(",");
+        try {
+            Arrays.stream(divided).forEach(Long::parseLong);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(ExceptionMessage.NUMBER_EACH_NOT_NUMBER);
         }
     }
 }
