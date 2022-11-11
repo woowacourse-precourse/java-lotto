b79b05d7aa56fcda576f44b7a0d4170263691f84
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-11 15:54:27 +0900

    feat(OutputUi): 구매한 로또 정보 출력
    - 구매한 로또 수 출력
    - 구매한 로또 번호들 출력
    - 로또 번호 출력을 위한 Lotto.toString() 구현

--- src/main/java/lotto/ui/OutputUi.java
+++ src/main/java/lotto/ui/OutputUi.java
@@ -1,10 +1,24 @@
 package lotto.ui;
 
+import lotto.domain.Lotto;
+
+import java.util.List;
+
 public class OutputUi {
 
     private static final String INSERT_MONEY_INFO_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String PURCHASED_LOTTO_COUNT_INFO_MESSAGE_FORMAT = "\n%d개를 구매했습니다.";
 
     public static void printInsertMoneyInfoMessage(){
         System.out.println(INSERT_MONEY_INFO_MESSAGE);
+    }
+
+    public static void printPurchasedLottos(List<Lotto> lottos){
+
+        System.out.println(String.format(PURCHASED_LOTTO_COUNT_INFO_MESSAGE_FORMAT, lottos.size()));
+
+        for(Lotto lotto : lottos){
+            System.out.println(lotto.toString());
+        }
     }
 }
