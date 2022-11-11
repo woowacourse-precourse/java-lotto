b7d638c027a9ccf259fe681105b2709b050aa899
박원종 <wonjong1030@naver.com>
2022-11-11 15:04:31 +0900

    test: 등수 찾기 기능 테스트

--- src/test/java/lotto/RankTest.java
+++ src/test/java/lotto/RankTest.java
@@ -1,4 +1,29 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class RankTest {
-  
+    @Test
+    void 삼등찾기테스트() {
+        Rank  rank = Rank.findByCorrectCountAndBonus(5,false);
+        assertThat(rank).isEqualTo(Rank.THIRD);
+    }
+    @Test
+    void 이등찾기테스트() {
+        Rank  rank = Rank.findByCorrectCountAndBonus(5,true);
+        assertThat(rank).isEqualTo(Rank.SECOND);
+    }
+    @Test
+    void 일등찾기테스트() {
+        Rank  rank = Rank.findByCorrectCountAndBonus(6, false);
+        assertThat(rank).isEqualTo(Rank.FIRST);
+    }
+    @Test
+    void 당첨실패테스트() {
+        Rank  rank = Rank.findByCorrectCountAndBonus(2,false);
+        assertThat(rank).isEqualTo(null);
+    }
 }
