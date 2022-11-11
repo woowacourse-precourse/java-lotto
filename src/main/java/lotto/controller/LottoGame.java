47c5b3239de60d12bcde633efbd55a1aff9b49e1
parkjungyoon <tbr06057@naver.com>
2022-11-11 14:24:02 +0900

    feat(Controller): 로또 구입 기능 구현

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,10 +1,27 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
+import lotto.domain.RandomNumbers;
 import lotto.view.InputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoGame {
     private int getLottoPurchaseAmount() {
         int quantity = InputView.getLottoPurchaseAmount() / 1000;
         return quantity;
+    }
+
+    private List<Lotto> buyLotto(int quantity) {
+        List<Lotto> purchaseLotto = new ArrayList<>();
+        RandomNumbers randomNumbers = new RandomNumbers();
+
+        for (int i = 0; i < quantity; i++) {
+            List<Integer> numbers = randomNumbers.generateRandomNumbers();
+            Lotto lotto = new Lotto(numbers);
+            purchaseLotto.add(lotto);
+        }
+        return purchaseLotto;
     }
 }
