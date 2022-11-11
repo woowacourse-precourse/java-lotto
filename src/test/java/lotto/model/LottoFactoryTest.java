e0de5916e5a482e26174df80cd7114fef44c37ef
dhkim <akdlzmf1123@gmail.com>
2022-11-11 15:34:11 +0900

    test(LottoFactoryTest): 사용자가 구매한 로또 n개를 만드는 테스트 구현

--- src/test/java/lotto/model/LottoFactoryTest.java
+++ src/test/java/lotto/model/LottoFactoryTest.java
@@ -1,15 +1,23 @@
 package lotto.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoFactoryTest {
-    @Test
-    void 사용자_로또를_1개_생성하는_기능() {
-        LottoFactory lottoFactory = new LottoFactory();
-        Lottos lottos = lottoFactory.generate(1);
-        assertThat(lottos.getSize()).isEqualTo(1);
+    LottoFactory lottoFactory;
+    @BeforeEach
+    void 전처리() {
+        lottoFactory = new LottoFactory();
+    }
+    @ParameterizedTest
+    @CsvSource({"1,1", "10,10"})
+    void 사용자_로또를_n개_생성하는_기능(int expected, int actual) {
+        Lottos lottos = lottoFactory.generate(actual);
+        assertThat(lottos.getSize()).isEqualTo(expected);
     }
 }
