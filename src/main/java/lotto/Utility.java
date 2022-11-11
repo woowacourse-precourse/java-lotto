f3c485f48d430606b2874eb9b364866fd42c509e
황민주 <minjoo@hwangminjooui-MacBookAir.local>
2022-11-11 19:34:47 +0900

    feat(Utility): printMessage (src/main/java/lotto/Utility.java)
    printMessage 메소드 추가:
    -메세지를 출력하기 위해 추가함

---
+++ src/main/java/lotto/Utility.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public abstract class Utility {
+
+    public void printMessage(String message){
+        System.out.println(message);
+    }
+
+}
