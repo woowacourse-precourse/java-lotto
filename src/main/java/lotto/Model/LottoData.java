2cb0a9165b2b65c00d95345beb2722ad86bbbdaa
raonrabbit <raonrabbit@gmail.com>
2022-11-12 01:57:49 +0900

    feat: 구매한 복권 수 setter 구현

---
+++ src/main/java/lotto/Model/LottoData.java
@@ -0,0 +1,8 @@
+package lotto.Model;
+
+public class LottoData{
+    private int countOfLotto;
+    public void setCountOfLotto(int numberOfLotto){
+        this.countOfLotto = numberOfLotto;
+    }
+}
