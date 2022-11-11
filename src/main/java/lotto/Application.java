cb299a7245a703df955f6e64e2ef8d2b8968a2f6
Kwak-JunYoung <klop9090@g.skku.edu>
2022-11-11 20:26:31 +0900

    fix(lotto/Application.java) Fixed matches-related functions and profit-rate computing function
    Matches-related functions
    - Refactored match-counting feature out of rank-getting method
    
    profit-rate computing function
    - Profit-calculating formula fixed

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,10 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class Application {
 
@@ -28,18 +25,18 @@ public class Application {
         List<Integer> sort_result = new ArrayList<Integer>();
         for (int i = 0; i < numbers.size(); i++)
             sort_vehicle[numbers.get(i)] += 1;
-        for (int i = 0; i < numbers.size(); i++)
+        for (int i = 0; i < 46; i++)
             if (sort_vehicle[i] != 0)
                 sort_result.add(i);
         return sort_result;
     }
 
     static List<Lotto> get_user_tickets(int count) {
+        System.out.println(count + "개를 구매했습니다.");
         List<Lotto> ticket_list = new ArrayList<Lotto>();
         for (int i = 0; i < count; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            numbers = sort_ticket_num(numbers);
-            System.out.println(numbers.toString());
+            List<Integer> numbers = sort_ticket_num(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            System.out.println(numbers);
             ticket_list.add(new Lotto(numbers));
         }
         return ticket_list;
@@ -49,11 +46,10 @@ public class Application {
         System.out.println("당첨 번호를 입력해 주세요.");
         String[] win_num_input = Console.readLine().split(",");
         List<Integer> inputs = new ArrayList<Integer>();
-        for (int i = 0; i < win_num_input.length; i++) {
+        for (int i = 0; i < 6; i++) {
             inputs.add(Integer.parseInt(win_num_input[i]));
         }
         Lotto win_numbers = new Lotto(inputs);
-
         return win_numbers;
     }
 
@@ -66,11 +62,22 @@ public class Application {
         return bonus_num;
     }
 
-    static int get_rank(List<Integer> user_numbers, List<Integer> win_numbers, int bonus) {
+    static int get_match_cnt(List<Integer> user_numbers, List<Integer> win_numbers) {
+        int[] dummy = new int[46];
+        for (int i = 0; i < 6; i++) {
+            dummy[user_numbers.get(i)] += 1;
+        }
         int count = 0;
+        for (int i = 0; i < 6; i++) {
+            count += dummy[win_numbers.get(i)];
+        }
+        return count;
+    }
+
+    static int get_rank(List<Integer> user_numbers, List<Integer> win_numbers, int bonus) {
+        int count = get_match_cnt(user_numbers,win_numbers);
         boolean bonus_match = false;
         for (int i = 0; i < 6; i++) {
-            if (Objects.equals(user_numbers.get(i), win_numbers.get(i))) count += 1;
             if (bonus == user_numbers.get(i)) bonus_match = true;
         }
         if (count == 5) {
@@ -95,7 +102,7 @@ public class Application {
         System.out.println("5개 일치 (1,500,000원) - " + ranks[3] + "개");
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + ranks[2] + "개");
         System.out.println("6개 일치 (2,000,000,000원) - " + ranks[1] + "개");
-        Double profit_rate = (Math.round(total_win_price / consumption * 10) / 10.0);
+        Double profit_rate = (Math.round((total_win_price * 1000 / consumption)) / 10.0);
         System.out.println("총 수익률은 " + profit_rate + "%입니다.");
     }
 
@@ -111,11 +118,10 @@ public class Application {
             int bonus_number = get_bonus_number();
 
 //         check result of get_winning_number and get_bonus_number
-            int[] ranks = new int[6];
+            int[] ranks = new int[10];
             for (int i = 0; i < user_tickets.size(); i++) {
                 ranks[get_rank(user_tickets.get(i).getNumbers(), winning_number.getNumbers(), bonus_number)] += 1;
             }
-
             print_statistics(ranks, consumption);
         } catch (IllegalArgumentException e) {
             System.out.println(e);
