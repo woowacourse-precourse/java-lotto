50f3805463a1b4a7e34299ca7f2bfdb72a08a86d
박승민 <tmddudals369@naver.com>
2022-11-12 00:57:23 +0900

    feat: 입력 기능 구현

---
+++ src/main/java/lotto/GameConsole.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameConsole {
+
+    public void inputPurchase(){
+        String purchase = Console.readLine();
+    }
+
+    public void inputWinningNumber(){
+        String winningNumber = Console.readLine();
+    }
+
+    public void inputBonusNumber(){
+        String bonusNumber = Console.readLine();
+    }
+}
