1850a03eaec54347af5bbee155fb074aba8f2e8b
YuHayeon <youha0930@gmail.com>
2022-11-12 02:08:42 +0900

    feat(InputView): 사용자가 당첨 번호를 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,9 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String LOTTO_PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_INPUT_MESSAGE = "\n당첨 번호를 입력해 주세요.";
 
     public static String lottoPriceInput() {
         System.out.println(LOTTO_PRICE_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String winningNumbersInput() {
+        System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
         return Console.readLine();
     }
 }
