96f9b95517b2655eda9bfc15c74d414a05661486
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-12 01:39:39 +0900

    feat(ExceptionMessage): 입력 값이 0인 경우에 대한 예외 처리 메시지 추가

--- src/main/java/lotto/ExceptionMessage.java
+++ src/main/java/lotto/ExceptionMessage.java
@@ -3,6 +3,7 @@ package lotto;
 public enum ExceptionMessage {
 
     PURCHASE_AMOUNT_IS_NOT_NUMBER("[ERROR] 금액을 투입해주셔야 합니다."),
+    PURCHASE_AMOUNT_IS_NOT_EXCEED_ZERO("[ERROR] 0을 초과한 금액을 투입해 주셔야 합니다."),
     PURCHASE_AMOUNT_IS_NOT_THOUSAND_UNITS("[ERROR] 입력 금액은 1,000원 단위여야 합니다."),
     LOTTO_WRONG_NUMBER_OF_VALUE("[ERROR] 6개의 값을 쉼표로 구분해 작성해 주셔야 합니다."),
     LOTTO_OUT_OF_RANGE("[ERROR] 1 ~ 45 사이의 값만 입력해 주셔야 합니다."),
