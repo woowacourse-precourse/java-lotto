5018d18b3a1503f3b24924452156d089300d3f5c
woosung1223 <dntjd991223@naver.com>
2022-11-11 20:36:53 +0900

    refactor(UserLotto): 생성자에서 buyLotto 메소드를 호출하도록 변경

--- src/main/java/lotto/domain/UserLotto.java
+++ src/main/java/lotto/domain/UserLotto.java
@@ -9,17 +9,21 @@ import java.util.List;
 public class UserLotto {
     private final LottoWrapper lottoSet = new LottoWrapper();
 
-    public void buyLotto() {
-        long count = readMoney() / Constant.MONEY_UNIT;
-        while (count-- > 0) {
-            lottoSet.add(makeLotto());
-        }
+    public UserLotto() {
+        buyLotto();
     }
 
     public void printMyLotto() {
         lottoSet.printAllLotto();
     }
 
+    private void buyLotto() {
+        long count = readMoney() / Constant.MONEY_UNIT;
+        while (count-- > 0) {
+            lottoSet.add(makeLotto());
+        }
+    }
+    
     private long readMoney() {
         return InputView.readAmount();
     }
