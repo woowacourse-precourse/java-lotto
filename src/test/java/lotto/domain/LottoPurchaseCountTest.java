40e5c4171e13099eaf66437e4d56cfe1af0093f8
CMHyuk <alsgur9710@naver.com>
2022-11-11 19:48:02 +0900

    test(LottoPurchaseCountTest): static import로 코드 간결화, 테스트 메서드 이름 변경

--- src/test/java/lotto/domain/LottoPurchaseCountTest.java
+++ src/test/java/lotto/domain/LottoPurchaseCountTest.java
@@ -1,10 +1,10 @@
 package lotto.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoPurchaseCountTest {
@@ -18,14 +18,14 @@ class LottoPurchaseCountTest {
 
     @DisplayName("로또 개수 확인 테스트")
     @Test
-    void lottoCountComparison() {
+    void checkLottoCount() {
         int count = lottoPurchaseCount.getLottoCount(5000);
-        Assertions.assertThat(count).isSameAs(5);
+        assertThat(count).isSameAs(5);
     }
 
     @DisplayName("로또 금액 입력 예외 테스트")
     @Test
-    void inputLottoMoneyException() {
+    void checkInputLottoMoneyException() {
         assertThatThrownBy(() -> lottoPurchaseCount.getLottoCount(5500))
                 .isInstanceOf(IllegalArgumentException.class);
     }
