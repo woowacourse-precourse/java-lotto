03963bb5a7b49ce59b0335661d0f7a8c1c30ae65
khj1 <dhepd8849@gmail.com>
2022-11-11 19:52:42 +0900

    feat: 로또 구매 갯수를 문자열로 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,10 @@
 package lotto.view;
 
+import lotto.domain.Lottos;
+
 public class OutputView {
+    public static void informPurchaseCount(Lottos lottos) {
+        int count = lottos.countTotal();
+        System.out.println(String.format("%d개를 구매했습니다.", count));
+    }
 }
