ae3c1c2a772747b68091c8cb6116945cb078585a
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-12 01:34:22 +0900

    style(ExceptionMessage): 오탈자 수정
    PURCHASE_AMOUNT_IS_NOT_THOUSAND_UNIT -> PURCHASE_AMOUNT_IS_NOT_THOUSAND_UNITS

--- src/main/java/lotto/ExceptionMessage.java
+++ src/main/java/lotto/ExceptionMessage.java
@@ -3,7 +3,7 @@ package lotto;
 public enum ExceptionMessage {
 
     PURCHASE_AMOUNT_IS_NOT_NUMBER("[ERROR] 금액을 투입해주셔야 합니다."),
-    PURCHASE_AMOUNT_IS_NOT_THOUSAND_UNIT("[ERROR] 입력 금액은 1,000원 단위여야 합니다."),
+    PURCHASE_AMOUNT_IS_NOT_THOUSAND_UNITS("[ERROR] 입력 금액은 1,000원 단위여야 합니다."),
     LOTTO_WRONG_NUMBER_OF_VALUE("[ERROR] 6개의 값을 쉼표로 구분해 작성해 주셔야 합니다."),
     LOTTO_OUT_OF_RANGE("[ERROR] 1 ~ 45 사이의 값만 입력해 주셔야 합니다."),
     LOTTO_DUPLICATE_VALUE("[ERROR] 서로 다른 숫자들을 입력해 주셔야 합니다."),
