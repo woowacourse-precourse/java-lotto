31b6de0289e7c67f9b89a4a0b8ef344dde337c99
박승민 <tmddudals369@naver.com>
2022-11-12 02:12:18 +0900

    feat: 로또 발행하기 구현

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,14 +1,29 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.Lotto;
+
 public class User {
 
     private int lottoCount;
+    private List<Lotto> lottos;
 
-    public User(int purchase){
+    public User(int purchase) {
         this.lottoCount = purchase / 1000;
+    }
+
+    public void createLottos() {
+        for (int repeat = 0; repeat < lottoCount; repeat++) {
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
     }
 
     public int getLottoCount() {
         return lottoCount;
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
     }
 }
