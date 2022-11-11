19a31f1e365722c1caaf4b0e67963f14b31918e2
bjm7701 <bjm7701@naver.com>
2022-11-11 19:30:54 +0900

    feat : declare and make Lotto member functions

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -9,11 +9,49 @@ public class Lotto {
         validate(numbers);
         this.numbers = numbers;
     }
-
+    private boolean valduplicate(List<Integer> numbers){
+        for(int i = 0; i < numbers.size(); i++){
+            for(int j = 0; j < numbers.size(); j++){
+                if(i == j) continue;
+                if(numbers.get(i) == numbers.get(j)) return false;
+            }
+        }
+        return true;
+    }
+    private boolean valrange(List<Integer> numbers){
+        for(int i = 0; i < numbers.size(); i++)
+            if(numbers.get(i) < 0 || numbers.get(i) > 45) return false;
+        return true;
+    }
     private void validate(List<Integer> numbers) {
+        //길이검사
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        //중복검사
+        if(valduplicate(numbers) == false) throw new IllegalArgumentException();
+        //범위검사
+        if(valrange(numbers) == false) throw new IllegalArgumentException();
+    }
+    private int findidx(int n){
+        for(int i = 0; i < numbers.size(); i++){
+            if(numbers.get(i) == n) return i;
+        }
+        return -1;
+    }
+    public int getRtn(List<Integer> truenum,int bonusnum){
+        int cnt = 0;
+        int bonus = 0;
+        if(findidx(bonusnum) == 1) bonus = 1;
+        for(int i = 0; i < truenum.size(); i++){
+            if(truenum.get(i) != -1) cnt++;
+        }
+        if(cnt == 6) return 1;
+        if(cnt == 5 && bonus == 1) return 2;
+        if(cnt == 5) return 3;
+        if(cnt == 4 || (cnt == 3 && bonus == 1)) return 4;
+        if(cnt == 3 || (cnt == 2 && bonus == 1)) return 5;
+        return 6;
     }
 
     // TODO: 추가 기능 구현
