7b6d534611071d21bca414d26e16b8010b4cef76
kki4504 <rhkddlf9876@gmail.com>
2022-11-11 09:18:54 +0900

    fix(validate) : 정규식 오류 수정
    정규식에서 00을 받아도 에러를 내지 않아서, 범위 안의 값을 확인하는
    메서드를 생성

--- src/main/java/lotto/util/input/WinNumbers.java
+++ src/main/java/lotto/util/input/WinNumbers.java
@@ -2,12 +2,15 @@ package lotto.util.input;
 
 import lotto.Lotto;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class WinNumbers {
-    private final Lotto winNumbers;
+    private final int MAX_VALUE = 45;
+    private final int MIN_VALUE = 1;
+    private Lotto winNumbers;
     private Set<Integer> winNumbersSet = new HashSet<>();
 
     public WinNumbers(String winNumbers) {
@@ -18,12 +21,20 @@ public class WinNumbers {
     private void validate(String winNumbers) {
         for (String winNumber : winNumbers.split(",")) {
             validateOnlyNumber(winNumber);
+            validateNumberInRange(winNumber);
+
             winNumbersSet.add(Integer.parseInt(winNumber));
         }
     }
     private void validateOnlyNumber (String winNumber) {
-        String REGEX = "^[0-9]*[1-9]+$";
+        final String REGEX = "^[0-9]*$";
         if (!Pattern.matches(REGEX, winNumber)) {
+            throw new IllegalArgumentException();
+        }
+    }
+    private void validateNumberInRange (String winNumber) {
+        int number = Integer.parseInt(winNumber);
+        if (!(number >= MIN_VALUE && number <= MAX_VALUE)) {
             throw new IllegalArgumentException();
         }
     }
