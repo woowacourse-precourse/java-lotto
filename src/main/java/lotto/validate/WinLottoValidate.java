ecc639722163544476e5ffa743c52bbaa4707236
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-11 14:22:59 +0900

    feat: create lottoBonusDuplicateValidate Method and Test

--- src/main/java/lotto/validate/WinLottoValidate.java
+++ src/main/java/lotto/validate/WinLottoValidate.java
@@ -5,12 +5,20 @@ import lotto.domain.Lotto;
 public class WinLottoValidate {
     private static final int START_NUMBER = 1;
     private static final int LAST_NUMBER = 45;
-    public void validate(Lotto winLotto,int bonus){
+
+    public void validate(Lotto winLotto, int bonus) {
         bonusRangeValidate(bonus);
+        lottoBonusDuplicateValidate(winLotto, bonus);
+    }
+
+    private void lottoBonusDuplicateValidate(Lotto winLotto, int bonus) {
+        if (winLotto.getNumbers().contains(bonus)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void bonusRangeValidate(int bonus) {
-        if(bonus < START_NUMBER || bonus > LAST_NUMBER){
+        if (bonus < START_NUMBER || bonus > LAST_NUMBER) {
             throw new IllegalArgumentException();
         }
     }
--- src/test/java/lotto/validate/WinLottoValidateTest.java
+++ src/test/java/lotto/validate/WinLottoValidateTest.java
@@ -1,5 +1,6 @@
 package lotto.validate;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,16 +11,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WinLottoValidateTest {
     private static WinLottoValidate winLottoValidate;
+
     @BeforeEach
-    public void beforeEach(){
+    public void beforeEach() {
         winLottoValidate = new WinLottoValidate();
     }
 
     @Test
-    public void bonusValidateTest() throws Exception{
+    public void bonusValidateTest() throws Exception {
         assertThatThrownBy(() -> winLottoValidate.bonusRangeValidate(0))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> winLottoValidate.bonusRangeValidate(46))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void lottoBonusDuplicateTest() throws Exception {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThatThrownBy(() -> winLottoValidate.validate(lotto, 6))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
