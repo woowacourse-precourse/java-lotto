965ff86c3a43ec26d04a860b4484418220d4c731
Eungyu-code <Agyu625@gmail.com>
2022-11-11 17:40:45 +0900

    update(printStats()): 함수 이름 변경
    15라인을 넘지 않는 요구사항을 만족하기 위해, 기존의 printStats 함수를 둘로 나누고, 당첨 통계가 출력됨을 알리는 함수임을 명확히하기 위해 printStatsPhrase로 함수 이름 변경 및 인수 삭제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -47,24 +47,11 @@ public class Application {
         System.out.println("구입 금액을 입력해 주세요.");
     }
 
-    public static void printStats(List<Lotto> lottoList, List<Integer> winningNumberList, int bonusNumber, int cash) {
-
-        int first = calculateFirst(lottoList, winningNumberList);
-        int second = calculateSecond(lottoList, winningNumberList, bonusNumber);
-        int third = calculateThird(lottoList, winningNumberList, bonusNumber);
-        int forth = calculateForth(lottoList, winningNumberList);
-        int fifth = calculateFifth(lottoList, winningNumberList);
-        double surplusRate = calculateSurplus(first, second, third, forth, fifth, cash);
+    public static void printStatsPhrase() {
 
         System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - " + first + "개");
-        System.out.println("4개 일치 (50,000원) - " + second + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + third + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + forth + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + fifth + "개");
-        System.out.println("총 수익률은 " + surplusRate + "%입니다.");
     }
 
     public static void printErrorAboutRange() {
