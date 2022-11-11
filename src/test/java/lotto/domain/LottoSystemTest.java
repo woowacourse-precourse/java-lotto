1fc72894152edf95afb7564da9a5cba8cc839aeb
HwangSeokJin <ghkd3703@gmail.com>
2022-11-11 15:17:30 +0900

    test(LottoSystem): 당첨 번호 입력 시 1부터 45 사이의 숫자인지 확인하는 테스트 코드 작성

--- src/test/java/lotto/domain/LottoSystemTest.java
+++ src/test/java/lotto/domain/LottoSystemTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class LottoSystemTest {
     LottoSystem system = new LottoSystem();
 
-    @DisplayName("당첨 번호 임력 시 숫자 이외에 값이 있는지 확인한다.")
+    @DisplayName("당첨 번호 입력 시 숫자 이외에 값이 있는지 확인한다.")
     @Test
     void InputInvalidType() {
         String[] invalidInput = {
@@ -21,12 +21,25 @@ class LottoSystemTest {
         }
     }
 
-    @DisplayName("당첨 번호 임력 시 숫자가 6개 입력되었는지 확인한다")
+    @DisplayName("당첨 번호 입력 시 숫자가 6개 입력되었는지 확인한다")
     @Test
     void InputInvalidCount() {
         String[] invalidInput = {
                 "1, 2, 3, 4",
                 "1, 2, 3, 4, 5, 6, 7",
+        };
+        for (String input : invalidInput) {
+            assertThatThrownBy(() -> system.setWinningNumbers(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @DisplayName("당첨 번호 입력 시 1부터 45 사이의 숫자인지 확인한다")
+    @Test
+    void InputInvalidRange() {
+        String[] invalidInput = {
+                "1, 46, 2, 3, 4, 5",
+                "-1, 45, 4, 24, 14, 5",
         };
         for (String input : invalidInput) {
             assertThatThrownBy(() -> system.setWinningNumbers(input))
