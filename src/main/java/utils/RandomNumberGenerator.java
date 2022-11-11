079d81e01abb54e349178fb5c45e71e90f98550b
TaemHam <tk4003950@gmail.com>
2022-11-12 00:27:26 +0900

    Docs: RandomNumberGenerator 주석 삭제

--- src/main/java/utils/RandomNumberGenerator.java
+++ src/main/java/utils/RandomNumberGenerator.java
@@ -4,10 +4,6 @@ package utils;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
-/**
- * '만약 라이브러리가 더이상 지원을 안하면 어떻게 해야할까?'
- * 원래 코드는 수정하지 않고 부품처럼 갈아 끼울 수 있도록 모듈화 했습니다.
- */
 public class RandomNumberGenerator {
 
     public static List<Integer> generateMultipleNumbers(int leftBound, int rightBound, int count) {
