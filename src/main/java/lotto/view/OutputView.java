0606e25c3924cd2c986529b51f53369285ca7131
sosow0212 <sosow0212@naver.com>
2022-11-11 19:12:38 +0900

    refactor : 출력 값을 위해 공백 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,6 +10,7 @@ public class OutputView {
     }
 
     public void responseBuyingQuantity(User user) {
+        System.out.println();
         System.out.println(getQuantity(user) + "개를 구매했습니다.");
     }
 
@@ -19,26 +20,31 @@ public class OutputView {
         });
     }
 
+    public void askLottoNumbers() {
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void askBonusNumber() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
     public void responseWinningHistory(Map<Winning, Integer> countOfWinning) {
-        System.out.println("3개 일치 (5,000원) - " + countOfWinning.getOrDefault(Winning.THIRD, 0));
-        System.out.println("4개 일치 (50,000원) - " + countOfWinning.getOrDefault(Winning.FOURTH, 0));
-        System.out.println("5개 일치 (1,500,000원) - " + countOfWinning.getOrDefault(Winning.FIFTH, 0));
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + countOfWinning.getOrDefault(Winning.FIFTH_WITH_BONUS, 0));
-        System.out.println("6개 일치 (2,000,000,000원) - " + countOfWinning.getOrDefault(Winning.SIXTH, 0));
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + countOfWinning.getOrDefault(Winning.THIRD, 0) + "개");
+        System.out.println("4개 일치 (50,000원) - " + countOfWinning.getOrDefault(Winning.FOURTH, 0) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + countOfWinning.getOrDefault(Winning.FIFTH, 0) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + countOfWinning.getOrDefault(Winning.FIFTH_WITH_BONUS, 0) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + countOfWinning.getOrDefault(Winning.SIXTH, 0) + "개");
     }
 
     public void responseYieldOfLotto(double yieldOfLotto) {
         System.out.println("총 수익률은 " + yieldOfLotto + "%입니다.");
     }
 
-
-    public void askLottoNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
-    }
-
-    public void askBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
-    }
 
     public int getQuantity(User user) {
         return user.getBuyingPrice() / 1000;
