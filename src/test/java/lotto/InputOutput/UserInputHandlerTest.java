5a7de3ac9a767f7757a6264e393b82f5e5df1f06
WhalesBob <ndw8200@naver.com>
2022-11-11 22:57:53 +0900

    refactor(InputTest) : put Functional Interface(Supplier) to nested GetHowMuchTicketsTest class and delete that in method

--- src/test/java/lotto/InputOutput/UserInputHandlerTest.java
+++ src/test/java/lotto/InputOutput/UserInputHandlerTest.java
@@ -18,7 +18,7 @@ class UserInputHandlerTest{
     OutputStream out;
     InputStream in;
     UserInputHandler inputHandler = new UserInputHandler();
-    Supplier<Integer> functionSupply;
+
 
     void beforeSetting(String input){
         out = new ByteArrayOutputStream();
@@ -35,6 +35,8 @@ class UserInputHandlerTest{
 
     @Nested
     class GetHowMuchTicketsTest{
+
+        Supplier<Integer> functionSupply = () -> inputHandler.getHowMuchTickets();
         @Test
         void getHowMuchTickets_case1() {
             String input = "8000";
@@ -55,7 +57,6 @@ class UserInputHandlerTest{
         void getHowMuchTickets_exception1(){
             String input = "Hello World";
             beforeSetting(input);
-            functionSupply = () -> inputHandler.getHowMuchTickets();
             assertSimpleTest(() ->{
                 exceptionRunning(functionSupply);
                 assertThat(out.toString().trim()).isEqualTo("[ERROR] 구입 금액에는 정수만 입력할 수 있습니다.");
@@ -65,7 +66,6 @@ class UserInputHandlerTest{
         void getHowMuchTickets_exception2() {
             String input = "-1000";
             beforeSetting(input);
-            functionSupply = () -> inputHandler.getHowMuchTickets();
             assertSimpleTest(() -> {
                 exceptionRunning(functionSupply);
                 assertThat(out.toString().trim()).isEqualTo("[ERROR] 지불하는 돈은 양수여야 합니다.");
@@ -76,7 +76,6 @@ class UserInputHandlerTest{
         void getHowMuchTickets_exception3() {
             String input = "11500";
             beforeSetting(input);
-            functionSupply = () -> inputHandler.getHowMuchTickets();
             assertSimpleTest(() -> {
                 exceptionRunning(functionSupply);
                 assertThat(out.toString().trim()).contains("[ERROR] 로또는 1,000원 단위로 구입할 수 있습니다.");
@@ -87,7 +86,6 @@ class UserInputHandlerTest{
         void getHowMuchTickets_exception4() {
             String input = "!!@@##$%^";
             beforeSetting(input);
-            functionSupply = () -> inputHandler.getHowMuchTickets();
             assertSimpleTest(() -> {
                 exceptionRunning(functionSupply);
                 assertThat(out.toString().trim()).contains("[ERROR] 구입 금액에는 정수만 입력할 수 있습니다.");
