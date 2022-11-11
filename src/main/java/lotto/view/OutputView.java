2b26657cef82e88435db7082f09b2565e87a0911
SH <gram753@naver.com>
2022-11-11 22:53:35 +0900

    feat: 생성된 로또를 출력하는 기능, 당첨통계를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,17 +1,37 @@
 package lotto.view;
 
+import static lotto.constant.Constants.*;
+
 import java.util.List;
 
+import lotto.constant.PrizeMoney;
+
 public class OutputView {
-	public static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
-	public static final String PURCHASE_NOTIFICATION = "개를 구매했습니다.";
-	public static final String WIN_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
-	public static final String BONUS_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
-	public static final String WIN_COUNT = "개";
 
 	public static void printLotto(List<List<Integer>> lottoGames) {
 		for (List<Integer> lottoGame : lottoGames) {
 			System.out.println(lottoGame);
 		}
+	}
+
+	public static void printStatistics(List<Integer> places, int money) {
+		int totalPrize = getTotalPrize(places);
+		double earningRate = (double)totalPrize / money * 100;
+
+		System.out.println(STATISTICS_START);
+		System.out.printf(FIFTH_MESSAGE, places.get(4));
+		System.out.printf(FOURTH_MESSAGE, places.get(3));
+		System.out.printf(THIRD_MESSAGE, places.get(2));
+		System.out.printf(SECOND_MESSAGE, places.get(1));
+		System.out.printf(FIRST_MESSAGE, places.get(0));
+		System.out.printf(EARNING_MESSAGE, (double)Math.round(earningRate * 10) / 10);
+	}
+
+	private static int getTotalPrize(List<Integer> places) {
+		return PrizeMoney.FIRST.getPrize() * places.get(0)
+			+ PrizeMoney.SECOND.getPrize() * places.get(1)
+			+ PrizeMoney.THIRD.getPrize() * places.get(2)
+			+ PrizeMoney.FOURTH.getPrize() * places.get(3)
+			+ PrizeMoney.FIFTH.getPrize() * places.get(4);
 	}
 }
