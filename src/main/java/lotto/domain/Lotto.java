18c3fc48d6e42777e69a567e534d070e6bb260b4
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-11 18:26:52 +0900

    refactor: static final 상수값 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+    public static final int PRICE = 1000;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
