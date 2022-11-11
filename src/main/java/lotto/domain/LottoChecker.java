00d8ef2bf0b8510a02a01babc5d5923c3afdf693
KimSangHyeok <tkdgur0906@naver.com>
2022-11-11 20:46:44 +0900

    Refactor(lotto): 보너스번호 비교기능 분리

--- src/main/java/lotto/domain/LottoChecker.java
+++ src/main/java/lotto/domain/LottoChecker.java
@@ -6,10 +6,9 @@ import lotto.WinningLotto;
 import java.util.List;
 
 public class LottoChecker {
-    public String compare(Lotto userLotto, WinningLotto winningLotto){
+    public int lottoCompare(Lotto userLotto, WinningLotto winningLotto){
         List<Integer> lotto = userLotto.getNumbers();
         Lotto winningNumbers = winningLotto.getWinningNumbers();
-        int bonusNumber = winningLotto.getBonusNumber();
         int sameCount = 0;
 
         for (Integer number : lotto) {
@@ -17,14 +16,10 @@ public class LottoChecker {
                 sameCount++;
             }
         }
-
-        if(isCorrectBonusNumber(lotto, bonusNumber, sameCount)){
-            return "5개 일치, 보너스 볼 일치 (30,000,000원)";
-        }
-        return Integer.toString(sameCount) + "개 일치";
+        return sameCount;
     }
 
-    private boolean isCorrectBonusNumber(List<Integer> lotto, int bonusNumber, int sameCount) {
-        return sameCount == 5 && lotto.contains(bonusNumber);
+    public boolean bonusCompare(Lotto userLotto, WinningLotto winningLotto){
+        return userLotto.getNumbers().contains(winningLotto.getBonusNumber());
     }
 }
