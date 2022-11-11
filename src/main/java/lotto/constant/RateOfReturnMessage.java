fe9e1b0e48eb96d3270123220c0dc5dabacff800
yubin <dbqls2821@gmail.com>
2022-11-11 15:07:27 +0900

    feat(RateOfReturnMessage): 수익률 메시지를 인자를 받는 enum 형식으로 상수화

---
+++ src/main/java/lotto/constant/RateOfReturnMessage.java
@@ -0,0 +1,17 @@
+package lotto.constant;
+
+import java.util.function.Function;
+
+public enum RateOfReturnMessage {
+  RATE_OF_RETURN_MESSAGE(x -> "총 수익률은 " + x + "입니다.");
+
+  private Function<String, String> template;
+
+  RateOfReturnMessage(Function<String, String> template) {
+    this.template = template;
+  }
+
+  public String print(String rateOfReturn) {
+    return this.template.apply(rateOfReturn);
+  }
+}
