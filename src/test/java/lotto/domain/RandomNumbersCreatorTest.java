001abaa07ab7ec41933f42fb36e5451a994e3180
heesang <dlagmltkd12@naver.com>
2022-11-11 19:03:13 +0900

    test: RandomNumbersCreatorTest import문 정리

--- src/test/java/lotto/domain/RandomNumbersCreatorTest.java
+++ src/test/java/lotto/domain/RandomNumbersCreatorTest.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import lotto.domain.RandomLottoNumberCreator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
