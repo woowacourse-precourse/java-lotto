c3adaa437ec874d3d9227bd8a20dc4ce72c27539
juhwang <juhong4930@naver.com>
2022-11-12 02:02:21 +0900

    test(CustomerTest): make rankings by the win number and the bonus number and validate duplication between win numbers and bonus number

--- src/test/java/lotto/CustomerTest.java
+++ src/test/java/lotto/CustomerTest.java
@@ -1,11 +1,11 @@
 package lotto;
 
+import lotto.user.Customer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import lotto.user.Customer;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -43,22 +43,43 @@ public class CustomerTest {
     void case4_count_the_duplicate_lotto_numbers() {
         assertRandomUniqueNumbersInRangeTest(() -> {
             Customer input = new Customer("2000");
-            List<Integer> winNumber = List.of(1,2,3,4,5,6);
-            List<Integer> result = List.of(6,4);
+            List<Integer> winNumber = List.of(1, 2, 3, 4, 5, 6);
+            List<Integer> result = List.of(6, 4);
 
             assertThat(input.matchWinNumbers(winNumber)).isEqualTo(result);
-        }, List.of(1, 2, 3, 4, 5, 6),List.of(3,4,5,6,7,8));
+        }, List.of(1, 2, 3, 4, 5, 6), List.of(3, 4, 5, 6, 7, 8));
     }
 
-    @DisplayName("당첨번호와 비교 후 맞는 번호 개수를 출력한다.")
+    @DisplayName("보너스 번호가 있는지 출력한다.")
     @Test
     void case5_count_the_duplicate_bonus_number() {
         assertRandomUniqueNumbersInRangeTest(() -> {
-            Customer input = new Customer("2000");
+            Customer input = new Customer("3000");
             int bonusNumber = 1;
-            List<Integer> result = List.of(1,0);
+            List<Boolean> result = List.of(true, false, false);
 
-            assertThat(input.matchBonusNumber(bonusNumber)).isEqualTo(result);
-        }, List.of(1, 2, 3, 4, 5, 6),List.of(3,4,5,6,7,8));
+            assertThat(input.matchBonusNumber(bonusNumber, List.of(5, 5, 4))).isEqualTo(result);
+        }, List.of(1, 2, 3, 4, 5, 6), List.of(3, 4, 5, 6, 7, 8), List.of(1, 2, 3, 4, 5, 9));
+    }
+
+    @DisplayName("당첨 번호와 보너스 번호를 기준으로 등수를 매긴다.")
+    @Test
+    void case6_create_rankings() {
+        assertRandomUniqueNumbersInRangeTest(() -> {
+            Map<LottoInformation, Integer> input = new Customer("2000")
+                    .createWinnings("1,2,3,4,5,7", "6");
+            List<Integer> result = List.of(1, 1);
+
+            assertThat(List.of(input.get(LottoInformation.SECOND), input.get(LottoInformation.FOURTH)))
+                    .isEqualTo(result);
+        }, List.of(1, 2, 3, 4, 5, 6), List.of(3, 4, 5, 6, 7, 8));
+    }
+
+    @DisplayName("당첨 번호와 보너스 번호가 중복되면 예외를 출력한다.")
+    @Test
+    void case7_validate_duplication_between_win_numbers_and_bonus_number() {
+        Customer input = new Customer("3000");
+        assertThatThrownBy(() -> input.matchBonusNumber(1, List.of(1, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
