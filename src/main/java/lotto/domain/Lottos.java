0dd6dbff54450fe7574ebcadfb353de0fbb58be5
yangjiyoung <0112jyoung@naver.com>
2022-11-11 20:44:14 +0900

    feat: define Lottos

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private List<Lotto> lottos=new ArrayList<>();
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+}
