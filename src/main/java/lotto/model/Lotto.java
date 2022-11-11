16c79697c07f35b17277b407cc25e94e8e7f4d0b
dhkim <akdlzmf1123@gmail.com>
2022-11-11 15:30:01 +0900

    feat(LottoFactory): 사용자가 구매한 로또 n개를 만드는 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class Lotto {
     public static final int PRICE = 1000;
+    public static final int NUMBER_LOWER_BOUNDS = 1;
+    public static final int NUMBER_UPPER_BOUNDS = 45;
+    public static final int NUMBERS_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
--- src/main/java/lotto/model/LottoFactory.java
+++ src/main/java/lotto/model/LottoFactory.java
@@ -1,8 +1,26 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class LottoFactory {
 
-    public Lottos generate(int size) {
-        return null;
+    public static Lottos generate(int size) {
+        List<Lotto> lottos = Stream.generate(LottoFactory::generate)
+                .limit(size)
+                .collect(Collectors.toList());
+        return new Lottos(lottos);
+    }
+
+    private static Lotto generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(Lotto.NUMBER_LOWER_BOUNDS
+                                                                , Lotto.NUMBER_UPPER_BOUNDS, Lotto.NUMBERS_SIZE);
+        Collections.sort(numbers);
+
+        return new Lotto(numbers);
     }
 }
