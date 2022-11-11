cbb71b894db2dd2147ad3e1284e0707e1a159f5f
jeomxon <jeomxon@gmail.com>
2022-11-12 01:36:13 +0900

    feat: 로또 번호를 생성할 때 오름차순 정렬을 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,25 @@
-package lotto;
+package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+    private static final String NOT_SIX_NUMBERS = "[ERROR] 로또 번호는 6개의 수로만 이루어져야 합니다.";
     private final List<Integer> numbers;
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NOT_SIX_NUMBERS);
         }
     }
-
-    private
 }
