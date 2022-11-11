ee0fb9a327b4cf220126f20f17199af189498f26
ddingmin <ddingminyt@gmail.com>
2022-11-11 15:09:10 +0900

    test(Money): 입력 받은 돈 Test

---
+++ src/test/java/lotto/MoneyTest.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import lotto.domain.Money;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MoneyTest {
+
+
+}
