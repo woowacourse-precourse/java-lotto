c86a6f3b2778c1a23cf06767c27d838087dc0d59
hongsikcho <jjdd9758@naver.com>
2022-11-12 01:17:37 +0900

    feat :1 LottoController ,View 생성 및 구입금액 입력 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,13 @@
+package lotto.controller;
+
+import lotto.view.View;
+
+public class LottoController {
+    public LottoController() {
+
+    }
+
+    public void startGame() {
+        String userInputMoney = View.printUserInputMoney();
+    }
+}
