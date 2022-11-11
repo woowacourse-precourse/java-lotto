5375b27e91ca1b99736ebd8dd19f22ddc4d07fea
JunH <tjdtls690@gmail.com>
2022-11-12 00:45:38 +0900

    :white_check_mark: test(Study : SetTest) : Set의 각 요소가 존재하는지 확인하는 학습 테스트 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,5 +27,12 @@ class SetTest {
     @DisplayName("Set의 크기 확인")
     void size() {
         assertThat(numbers).hasSize(3);
+    }
+    
+    @DisplayName("Set의 각 요소가 존재하는지 확인")
+    @ParameterizedTest(name = "{displayName} => {0}")
+    @ValueSource(ints = {1, 2, 3})
+    void contains(final int value) {
+        assertThat(numbers).contains(value);
     }
 }
