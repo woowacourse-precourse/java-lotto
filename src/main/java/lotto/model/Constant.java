ccc1fade1e40a299c4c35cbb17be0ff9cf3d6711
Cyma-s <ssol0319@naver.com>
2022-11-12 02:08:52 +0900

    feat(Constant): add money unit constant

--- src/main/java/lotto/model/Constant.java
+++ src/main/java/lotto/model/Constant.java
@@ -4,4 +4,5 @@ public class Constant {
     public static final int NUMBER_RANGE_START = 1;
     public static final int NUMBER_RANGE_END = 45;
     public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int MONEY_UNIT = 1000;
 }
