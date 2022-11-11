fdf58ca60f0c326ce376d81f23794213f21be9e8
Joon <orijoon98@gmail.com>
2022-11-11 13:44:12 +0900

    feat: add print input price method

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -5,4 +5,8 @@ public class Print {
     private final static String INPUT_PRICE = "구입금액을 입력해 주세요.";
     private final static String INPUT_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
     private final static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public static void inputPrice() {
+        System.out.println(INPUT_PRICE);
+    }
 }
