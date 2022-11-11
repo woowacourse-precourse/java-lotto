b3144c568df48cd4ea2ee6ff28a630047862d5ad
dclxxi <dclxxi@kakao.com>
2022-11-11 22:22:08 +0900

    feat: 계산한 개수만큼 로또 번호를 생성하는 기능 추가

--- src/main/java/model/Lottos.java
+++ src/main/java/model/Lottos.java
@@ -1,15 +1,44 @@
 package model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import constant.Config;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Lottos {
-    private final int count;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int NUMBER_COUNT = 6;
+    private final List<Lotto> lottos;
 
     public Lottos(Money money) {
-        this.count = money.getMoney() / Config.PRICE;
+        this.lottos = new ArrayList<>();
+
+        setLottos(money);
     }
 
-    public int getCount() {
-        return count;
+    public List<Lotto> getLottos() {
+        return Collections.unmodifiableList(lottos);
+    }
+
+    private void setLottos(Money money) {
+        int count = getCount(money);
+
+        for (int i = 0; i < count; i++) {
+            Lotto lotto = new Lotto(getNumbers());
+            lottos.add(lotto);
+        }
+    }
+
+    private int getCount(Money money) {
+        return money.getMoney() / Config.PRICE;
+    }
+
+    private List<Integer> getNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBER_COUNT);
+        Collections.sort(numbers);
+        return numbers;
     }
 }
--- src/test/java/model/LottosTest.java
+++ src/test/java/model/LottosTest.java
@@ -3,14 +3,25 @@ package model;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottosTest {
 
-    @ParameterizedTest(name = "{index}  money = {0} {1}")
+    @ParameterizedTest(name = "{index}  {0}원 입력 시 {1}개 생성")
     @CsvSource(value = {"1000:1", "20000:20", "345000:345"}, delimiter = ':')
-    void 구입_금액에_해당하는_로또_개수를_계산한다(int money, int count) {
-        model.Lottos lottos = new model.Lottos(new Money(money));
-        assertEquals(lottos.getCount(), count);
+    void 구입_금액에_해당하는_로또_번호_생성(int money, int count) {
+        Lottos lottos = new Lottos(new Money(money));
+        assertEquals(lottos.getLottos().size(), count);
+    }
+
+    @ParameterizedTest(name = "{index}  {0}원 입력 시 오름차순으로 생성")
+    @CsvSource(value = {"1000", "20000", "345000"})
+    void 로또_번호를_오름차순으로_생성한다(int money) {
+        Lottos lottos = new Lottos(new Money(money));
+
+        for (Lotto lotto : lottos.getLottos()) {
+            assertThat(lotto.getNumbers()).isSorted();
+        }
     }
 }
