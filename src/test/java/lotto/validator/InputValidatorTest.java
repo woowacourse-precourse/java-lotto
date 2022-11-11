3379f32af76de6a7eacd67a853801ef795a91ed0
java-saeng <gusdn85554@naver.com>
2022-11-11 11:54:42 +0900

    test : 로또 구입 시 1000의 배수를 입력하지 않으면 예외 발생 테스트 추가

--- src/test/java/lotto/validator/InputValidatorTest.java
+++ src/test/java/lotto/validator/InputValidatorTest.java
@@ -15,4 +15,18 @@ class InputValidatorTest {
         assertThrows(IllegalArgumentException.class,
                      () -> InputValidator.validatePurchaseInput("0", 1000));
     }
+
+    @Test
+    @DisplayName("로또 구입 입력 시 1000의 배수가 아니면 IllegalArgumentException을 발생시킵니다.")
+    void testValidatePurchaseInput_Multiply_1000_IllegalArgumentException() throws Exception {
+        //when & then
+        assertThrows(IllegalArgumentException.class,
+                     () -> InputValidator.validatePurchaseInput("0", 1000));
+
+        assertThrows(IllegalArgumentException.class,
+                     () -> InputValidator.validatePurchaseInput("4232", 1000));
+
+        assertThrows(IllegalArgumentException.class,
+                     () -> InputValidator.validatePurchaseInput("4001", 1000));
+    }
 }
