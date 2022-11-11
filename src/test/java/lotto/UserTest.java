fc0a0c02f963ae2c0f5b11504da1819ed25985e7
khj <bbb6254177@gmail.com>
2022-11-11 19:57:09 +0900

    fix: fix typo

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class UserTest {
     @DisplayName("구입 금액이 1000으로 나눠 떨어지지 않으면 예외가 발생한다.")
     @Test
-    void createAmountNotdivided1000() {
+    void createAmountNotDivided1000() {
         assertThatThrownBy(() -> new User(14500))
                 .isInstanceOf(IllegalArgumentException.class);
     }
