76c297ccd7eceba409fe55121c43b1852903d280
dohyun <ldh19738@gmail.com>
2022-11-11 20:02:51 +0900

    feat(Win): 생성자 선언하여 당첨 횟수 변수 초기화하는 기능 구현

--- src/main/java/lotto/Win.java
+++ src/main/java/lotto/Win.java
@@ -3,6 +3,20 @@ package lotto;
 import java.util.List;
 
 public class Win {
+    private int firstPlace;
+    private int secondPlace;
+    private int thirdPlace;
+    private int fourthPlace;
+    private int fifthPlace;
+
+    public Win() {
+        this.firstPlace = 0;
+        this.secondPlace = 0;
+        this.thirdPlace = 0;
+        this.fourthPlace = 0;
+        this.fifthPlace = 0;
+    }
+
     public void threeMatchMessage() {
         System.out.println("3개 일치 (5,000원) - ");
     }
