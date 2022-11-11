3766c05f5a2af41e695097fb7970fc2c6e939b2f
Joon <orijoon98@gmail.com>
2022-11-11 12:56:18 +0900

    feat: add input lotto numbers method and related validation methods

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,8 +1,14 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Input {
+
+    private final static int LOTTO_NUMBERS_INPUT_SIZE = 6;
 
     public static void validateNumber(String input) {
         try {
@@ -37,5 +43,32 @@ public class Input {
         price = Integer.parseInt(input);
         validatePrice(price);
         return price;
+    }
+
+    public static List<Integer> lottoNumbers() {
+        Set<Integer> lottoNumbers = new HashSet<>();
+        String input = Console.readLine();
+        String[] splitByCommaInput = input.split(",");
+        validateSplitByCommaInputSize(splitByCommaInput);
+        for (String lottoNumber : splitByCommaInput) {
+            validateLottoNumber(lottoNumber);
+            lottoNumbers.add(Integer.parseInt(lottoNumber));
+        }
+        validateNonDuplicatedLottoNumbers(lottoNumbers);
+        return new ArrayList<>(lottoNumbers);
+    }
+
+    public static void validateSplitByCommaInputSize(String[] splitByCommaInput) {
+        if (splitByCommaInput.length != LOTTO_NUMBERS_INPUT_SIZE) {
+            // TODO: 2022/11/11 에러 메시지 출력 추가
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateNonDuplicatedLottoNumbers(Set<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBERS_INPUT_SIZE) {
+            // TODO: 2022/11/11 에러 메시지 출력 추가
+            throw new IllegalArgumentException();
+        }
     }
 }
