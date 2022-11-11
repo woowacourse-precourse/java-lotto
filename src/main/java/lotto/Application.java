8dd37ceb4d364ddf98e79741aec468dbcc8d564e
SH <gram753@naver.com>
2022-11-11 23:00:57 +0900

    feat: 실행 클래스 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.system.LottoSystem;
+import lotto.system.NewLottoSystem;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		// LottoSystem lottoSystem = new LottoSystem();
+		// lottoSystem.lottoLogic();
+
+		NewLottoSystem lottoSystem = new NewLottoSystem();
+		lottoSystem.lottoLogic();
+	}
 }
