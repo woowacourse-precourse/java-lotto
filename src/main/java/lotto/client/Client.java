d7b3eb673628377f97002c225db2698195a49c4f
K-Diger <kitten.diger@gmail.com>
2022-11-11 18:55:23 +0900

    fix : Client 클래스 에서 구매금액을 입력받는 메서드를 예외 catch 하도록 수정

--- src/main/java/lotto/client/Client.java
+++ src/main/java/lotto/client/Client.java
@@ -7,8 +7,14 @@ public class Client {
     public int getPurchaseMoney() {
         System.out.println("구매금액을 입력해 주세요.");
         InputAgent inputAgent = new InputAgent();
-        int inputValue = inputAgent.inputUserMoney();
+        int inputValue = 0;
+        try {
+            inputValue = inputAgent.inputUserMoney();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.printf("\n%d개를 구매했습니다.\n", inputValue / 1000);
         return inputValue / 1000;
+
     }
 }
