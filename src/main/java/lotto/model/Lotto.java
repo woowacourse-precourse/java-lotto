cb48eb53cbd6986c8b33ed7334dc48faa8fd5b6d
jkde7721 <jkde7721@naver.com>
2022-11-11 11:30:52 +0900

    feat(Lotto): 로또 번호 validate 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,6 +3,10 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
+
+    public static final int LOTTO_LENGTH = 6;
+    public static final int LOTTO_RANGE_MIN = 1;
+    public static final int LOTTO_RANGE_MAX = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,8 +15,32 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validateLottoLength(numbers);
+        validateLottoRange(numbers);
+        validateDuplication(numbers);
+    }
+
+    private void validateLottoLength(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자 6개이어야 합니다.");
+        }
+    }
+
+    private void validateLottoRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < LOTTO_RANGE_MIN || number > LOTTO_RANGE_MAX) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호의 숫자 범위는 1~45까지여야 합니다.");
+            }
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            List<Integer> comparedNumbers = numbers.subList(i + 1, numbers.size());
+            Integer number = numbers.get(i);
+            if (comparedNumbers.contains(number)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호 각각은 서로 중복될 수 없습니다.");
+            }
         }
     }
 
