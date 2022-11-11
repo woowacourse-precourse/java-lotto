57273e887470e940ee1439b9d484e44c60015470
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-11 23:53:15 +0900

    fix(Application): Try-Catch 구문 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,9 +3,13 @@ package lotto;
 import lotto.controller.LottoController;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        LottoController lottoController = new LottoController();
-        lottoController.play();
-    }
+	public static void main(String[] args) {
+		try {
+			LottoController lottoController = new LottoController();
+			lottoController.play();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
 }
