8698ce76acf862cdd33ca9ed64f85fff4a9639c8
woo-chang <clllickme@naver.com>
2022-11-11 17:29:39 +0900

    refactor(InputService): 접근 제어자 및 static 키워드 수정
    - 외부에 노출될 가능성이 없는 상수는 private으로 수정
    - 상속을 막기 위한 private 생성자
    - 인스턴스 생성없이 메서드 사용을 위한 static 키워드 사용

--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -4,21 +4,24 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputService {
 
-    public static String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    public static String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-    public static String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public String readPurchaseAmount() {
+    private InputService() {
+    }
+
+    public static String readPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
         return Console.readLine();
     }
 
-    public String readWinningNumber() {
+    public static String readWinningNumber() {
         System.out.println(WINNING_NUMBER_MESSAGE);
         return Console.readLine();
     }
 
-    public String readBonusNumber() {
+    public static String readBonusNumber() {
         System.out.println(BONUS_NUMBER_MESSAGE);
         return Console.readLine();
     }
