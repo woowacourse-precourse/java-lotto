5dbae56c83b0d9c882fc9434372516675d0c26ae
yubin <dbqls2821@gmail.com>
2022-11-12 01:17:12 +0900

    fix: 입력값의 길이가 11이 아닐때 예외사항을 던지는 건 잘못되었으므로 구현 목록에서 삭제함

--- src/main/java/lotto/domain/constant/ExceptionMessage.java
+++ src/main/java/lotto/domain/constant/ExceptionMessage.java
@@ -2,7 +2,6 @@ package lotto.domain.constant;
 
 public enum ExceptionMessage {
   INPUT_IS_NOT_SEPARATED_BY_COMMAS("[ERROR] 입력값은 단일 콤마로 구분되지 않았습니다."),
-  INPUT_LENGTH_IS_NOT_11("[ERROR] 입력값의 전체 길이는 11이 아닙니다."),
   INPUT_IS_NOT_SIX_NUMBERS("[ERROR] 입력값의 숫자는 6개가 아닙니다."),
   INPUT_IS_NOT_NUMBER("[ERROR] 입력값은 숫자가 아닙니다."),
   INPUT_NUMBER_RANGE_IS_NOT_CORRECT("[ERROR] 입력값은 1이상 45이하의 숫자가 아닙니다."),
