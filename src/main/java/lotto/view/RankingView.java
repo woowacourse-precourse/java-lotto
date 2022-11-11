131fda1bb7dcf0ee463f9070246efafcb8bae10c
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-11 21:25:39 +0900

    feat: RankingView 당첨 금액당 당첨된 로또 개수를 출력해주는 기능 구현

--- src/main/java/lotto/view/RankingView.java
+++ src/main/java/lotto/view/RankingView.java
@@ -1,12 +1,17 @@
 package lotto.view;
 
+import lotto.Ranking;
+import lotto.domain.Winning;
+
+import java.util.HashMap;
+
 public class RankingView {
     private static final String WINNING_STATISTICS = "당첨 통계\n---";
-    private static final String THREE_MATCH = "3개 일치 (5,000원) - ";
-    private static final String FOUR_MATCH = "4개 일치 (50,000원) - ";
-    private static final String FIVE_MATCH = "5개 일치 (1,500,000원) - ";
-    private static final String FIVE_MATCH_WITH_BONUS = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
-    private static final String SIX_MATCH = "6개 일치 (2,000,000,000원) - ";
+    private static final String THREE_MATCH_COMMENT = "3개 일치 (5,000원) - ";
+    private static final String FOUR_MATCH_COMMENT = "4개 일치 (50,000원) - ";
+    private static final String FIVE_MATCH_COMMENT = "5개 일치 (1,500,000원) - ";
+    private static final String FIVE_MATCH_WITH_BONUS_COMMENT = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    private static final String SIX_MATCH_COMMENT = "6개 일치 (2,000,000,000원) - ";
     private static final String TELL_COUNT = "개";
     private static final String TOTAL_PROFIT_START = "총 수익률은 ";
     private static final String TOTAL_PROFIT_END = "%입니다.";
@@ -19,8 +24,12 @@ public class RankingView {
         System.out.println(WINNING_STATISTICS);
     }
 
-    private void showPriceToPlayer(){
-
+    private void showPriceToPlayer(HashMap<Ranking, Integer> score){
+        System.out.println(THREE_MATCH_COMMENT + score.get(Ranking.THREE_MATCH) + TELL_COUNT);
+        System.out.println(FOUR_MATCH_COMMENT + score.get(Ranking.FOUR_MATCH) + TELL_COUNT);
+        System.out.println(FIVE_MATCH_COMMENT + score.get(Ranking.FIVE_MATCH) + TELL_COUNT);
+        System.out.println(FIVE_MATCH_WITH_BONUS_COMMENT + score.get(Ranking.FIVE_MATCH_WITH_BONUS) + TELL_COUNT);
+        System.out.println(SIX_MATCH_COMMENT + score.get(Ranking.SIX_MATCH) + TELL_COUNT);
     }
 
     private void showProfit(){
