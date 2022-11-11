09e721decdd088accc5a912ed3adf1dd2045565c
bjm7701 <bjm7701@naver.com>
2022-11-11 20:17:08 +0900

    refactor : assign idx to enum

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -1,5 +1,10 @@
 package lotto;
 
 public enum LottoRank {
-    First,Second,Third,Fourth,Fifth,Sixth
+    First(0),Second(1),Third(2),Fourth(3),Fifth(4),Sixth(5);
+    private int value;
+    LottoRank(int i) {
+        this.value = i;
+    }
+    public int getValue(){ return this.value; }
 }
