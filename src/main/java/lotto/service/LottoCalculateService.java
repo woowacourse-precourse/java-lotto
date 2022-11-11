24803d5d86e891d830961ee7975d94bd6cf04648
sosow0212 <sosow0212@naver.com>
2022-11-11 15:20:23 +0900

    feat : HashMap을 이용해서 로또 번호 6개를 맞추는 경우 카운팅 해주는 기능 추가

--- src/main/java/lotto/service/LottoCalculateService.java
+++ src/main/java/lotto/service/LottoCalculateService.java
@@ -1,4 +1,41 @@
 package lotto.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.User;
+import lotto.domain.Winning;
+import lotto.view.OutputView;
+
 public class LottoCalculateService {
+    private final OutputView outputView = new OutputView();
+    private int winningPrice = 0;
+    private Map<Winning, Integer> countOfWinning = new HashMap<>();
+
+    public void calculateWinning(User user, Lotto lotto) {
+
+    }
+
+    public void countEqualsSix(List<Integer> numbers, Lotto lotto) {
+        if (numbers.equals(lotto.getNumbers())) {
+            countOfWinning.put(Winning.SIXTH, countOfWinning.getOrDefault(Winning.SIXTH, 0) + 1);
+        }
+    }
+
+    public void countEqualsFiveWithBonus() {
+
+    }
+
+    public void countEqualsFive() {
+
+    }
+
+    public void countEqualsFour() {
+
+    }
+
+    public void countEqualsThree() {
+
+    }
 }
