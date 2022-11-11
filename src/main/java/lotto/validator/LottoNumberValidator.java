591a30a11ad41d45477bc96852503716b8883e5c
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-11 15:31:03 +0900

    feat: 보너스 번호가 이미 로또 번호에 존재하는지 테스트 추가

--- src/main/java/lotto/validator/LottoNumberValidator.java
+++ src/main/java/lotto/validator/LottoNumberValidator.java
@@ -24,6 +24,11 @@ public class LottoNumberValidator {
     }
 
     public static void validateBonusNumberAndLottoAreDuplicate(Lotto lotto, int bonusNumber) {
+        if (lotto.isContains(bonusNumber)) {
+            throw new IllegalArgumentException(
+                    ErrorMessage.BONUS_NUMBER_AND_WINNING_LOTTO_NUMBERS_ARE_DUPLICATE.getMessage()
+            );
+        }
     }
 
     private static boolean isInRange(int number) {
