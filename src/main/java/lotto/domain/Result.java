ae9aa7bdde3a85c8a5a5f962c39354e651c6b833
dev_kong <ahdjd5@gmail.com>
2022-11-11 13:29:19 +0900

    feat(Result): add Result class

---
+++ src/main/java/lotto/domain/Result.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Result {
+    List<List<Integer>> purchasedLotteries;
+    WinningNumbers winningNumbers;
+    BonusNumber bonusNumber;
+    Map<String, Integer> resultMap = new HashMap<String, Integer>() {
+        {
+            put("first", 0);
+            put("second", 0);
+            put("third", 0);
+            put("fourth", 0);
+            put("fifth", 0);
+            put("sixth", 0);
+        }
+    };
+
+    public void Result(List<List<Integer>> purchasedLotteries,
+                       WinningNumbers winningNumbers,
+                       BonusNumber bonusNumber) {
+        this.purchasedLotteries = purchasedLotteries;
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public void countWinningCase() {
+    }
+}
