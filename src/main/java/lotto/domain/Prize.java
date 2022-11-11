87a74309a2ddc21f262f1aabeef5b3cabd1a0bd6
ddubidubap <park62000@gmail.com>
2022-11-11 18:42:19 +0900

    refactor: Enum에 문자열 추가

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,19 +1,27 @@
 package lotto.domain;
 
+import static lotto.view.Constants.*;
+
 public enum Prize {
-    FIFTH(5_000),
-    FOURTH(50_000),
-    THIRD(1_500_000),
-    SECOND(30_000_000),
-    FIRST(2_000_000_000);
+    FIFTH(5_000, SAME_THREE),
+    FOURTH(50_000, SAME_FOUR),
+    THIRD(1_500_000, SAME_FIVE),
+    SECOND(30_000_000, SAME_FIVE_BONUS),
+    FIRST(2_000_000_000, SAME_SIX);
 
     private final int prizeMoney;
+    private final String prizeNotice;
 
-    Prize(int prizeMoney) {
+    Prize(int prizeMoney, String prizeNotice) {
         this.prizeMoney = prizeMoney;
+        this.prizeNotice = prizeNotice;
     }
 
     public int getPrizeMoney() {
         return prizeMoney;
+    }
+
+    public String getPrizeNotice() {
+        return prizeNotice;
     }
 }
--- src/main/java/lotto/view/Constants.java
+++ src/main/java/lotto/view/Constants.java
@@ -10,6 +10,11 @@ public class Constants {
     public static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
     public static final String WIN_STAT = "당첨 통계\n---";
+    public static final String SAME_THREE = "3개 일치 (5,000원) - %d개";
+    public static final String SAME_FOUR = "4개 일치 (50,000원) - %d개";
+    public static final String SAME_FIVE = "5개 일치 (1,500,000원) - %d개";
+    public static final String SAME_FIVE_BONUS = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+    public static final String SAME_SIX = "6개 일치 (2,000,000,000원) - %d개";
     public static final String EARNED_RATE = "총 수익률은 %.f입니다.";
 
     public static final String ERROR_ALERT = "[ERROR] ";
