c73b71116175b36ca061cc831512866d62dcce38
Sieun Park <psieun0606@gmail.com>
2022-11-12 02:57:23 +0900

    refactor: 출력 문구 상수로 변경

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -4,22 +4,26 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String INPUT_PURCHASING_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
     public String inputPurchasing() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(INPUT_PURCHASING_AMOUNT);
         String purchasingAmount = Console.readLine();
 
         return purchasingAmount;
     }
 
     public String inputWinningNumber() {
-        System.out.println("당첨 번호를 입력해주세요.");
+        System.out.println(INPUT_WINNING_NUMBER);
         String winningNumber = Console.readLine();
 
         return winningNumber;
     }
 
     public String inputBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(INPUT_BONUS_NUMBER);
         String bonusNumber = Console.readLine();
 
         return bonusNumber;
