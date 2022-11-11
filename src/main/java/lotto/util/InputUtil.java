2ef8f54e7cfbe0bb8358a8a672ba2b797166e8e2
kdkdhoho <hkim4410@naver.com>
2022-11-11 11:51:59 +0900

    feat(InputUtil): 입력받은 금액이 유효한 입력인지 판단하는 기능 추가

--- src/main/java/lotto/util/InputUtil.java
+++ src/main/java/lotto/util/InputUtil.java
@@ -1,5 +1,8 @@
 package lotto.util;
 
+import lotto.domain.enums.Message;
+import lotto.domain.enums.Number;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputUtil {
@@ -8,4 +11,14 @@ public class InputUtil {
         String input = readLine();
         return input;
     }
+
+    public void checkValidationMoney(String input) {
+        int inputNumber = Integer.parseInt(input);
+
+        if(inputNumber % Number.THOUSAND.getValue() != Number.ZERO.getValue()) {
+            IllegalArgumentException illegalArgumentException = new IllegalArgumentException(Message.NOT_THOUSAND_UNIT_INPUT_ERROR.getMessage());
+            throw illegalArgumentException;
+        }
+    }
+
 }
