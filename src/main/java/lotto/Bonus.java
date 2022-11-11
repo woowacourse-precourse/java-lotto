56a22e81b21d001ede64ac6790fe075accdf43c9
TaeMin <xoals3094@naver.com>
2022-11-12 08:48:10 +0900

    refactor : 에러메시지를 수정합니다

--- src/main/java/lotto/Bonus.java
+++ src/main/java/lotto/Bonus.java
@@ -10,7 +10,7 @@ public class Bonus {
 
     private void isValidData(int bonus) {
         if (!(bonus >= 1 && bonus <= 45)) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -13,7 +13,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 숫자는 6개만 허용합니다");
         }
         List<Integer> duplication = new ArrayList<>();
         for (int number : numbers) {
