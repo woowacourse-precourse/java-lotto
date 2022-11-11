bb3f4566ba38846a3f6d1455a9d9112d02ca2387
woosung1223 <dntjd991223@naver.com>
2022-11-11 20:30:13 +0900

    refactor(InputView): 입력 메소드 각각에 verfier 적용

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,26 +2,35 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.util.IOMessage;
+import lotto.verifier.BonusNumberVerifier;
+import lotto.verifier.MoneyVerifier;
+import lotto.verifier.WinnerNumberVerifier;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public final class InputView {
+    private static MoneyVerifier moneyVerifier = new MoneyVerifier();
+    private static BonusNumberVerifier bonusNumberVerifier = new BonusNumberVerifier();
+    private static WinnerNumberVerifier winnerNumberVerifier = new WinnerNumberVerifier();
+
     private InputView() {
     }
 
     public static int readAmount() {
         printMessage(IOMessage.READ_AMOUNT_MESSAGE);
         String line = Console.readLine();
-        // verify(line);
+        moneyVerifier.check(line);
+
         return Integer.parseInt(line);
     }
 
     public static List<Integer> readWinningNumber() {
         printMessage(IOMessage.READ_WINNING_NUM_MESSAGE);
         String line = Console.readLine();
-        // verify(line);
+        winnerNumberVerifier.check(line);
+
         return Arrays.stream(line.split(","))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
@@ -30,7 +39,8 @@ public final class InputView {
     public static int readBonusNumber() {
         printMessage(IOMessage.READ_BONUS_NUM_MESSAGE);
         String line = Console.readLine();
-        // verify(line);
+        bonusNumberVerifier.check(line);
+        
         return Integer.parseInt(line);
     }
 
