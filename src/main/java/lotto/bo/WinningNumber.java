7b60d0545fea9e3dd8843f751696bebf2928eff7
bjk1649 <qhwns1649@naver.com>
2022-11-11 22:23:19 +0900

    feat: ,로 구분된 문자열을 List<Integer> 형태로 바꿔주는 메소드 추가
    추후 검증을 편하게 하기 위해 만든 기능

--- src/main/java/lotto/bo/WinningNumber.java
+++ src/main/java/lotto/bo/WinningNumber.java
@@ -2,6 +2,8 @@ package lotto.bo;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WinningNumber {
     private final List<Integer> numbers;
@@ -9,9 +11,20 @@ public class WinningNumber {
     public WinningNumber(String winningLottoNumbers, String BonusNumber) {
         validateInteger(winningLottoNumbers);
         validateInteger(BonusNumber);
-        validateRange(winningLottoNumbers);
-        validateRange(BonusNumber);
+
+        List<Integer> tmpNumbers = convertStrToNumberList(winningLottoNumbers);
+        tmpNumbers.add(Integer.valueOf(BonusNumber));
+
+        validateRange(tmpNumbers);
+        validateRange(tmpNumbers);
         this.numbers = Collections.emptyList();
+    }
+
+    private List<Integer> convertStrToNumberList(String numbers) {
+        List<Integer> result;
+        result = Stream.of(numbers.split(","))
+                .map(Integer::valueOf).collect(Collectors.toList());
+        return result;
     }
 
     private void validateInteger(String numbers) {
@@ -24,9 +37,9 @@ public class WinningNumber {
         }
     }
 
-    private void validateRange(String numbers) {
-        for (String number : numbers.split(",")) {
-            if (Integer.valueOf(number) < 1 || Integer.valueOf(number) > 45) {
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
                 throw new IllegalArgumentException("당첨 번호와 보너스 번호는 1~ 45 사이의 숫자이어야합니다.");
             }
         }
