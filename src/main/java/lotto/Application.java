2a35afb0065317ffcb6f88dbebf4155b5350eabf
neppiness <0414kjh@naver.com>
2022-11-11 21:19:49 +0900

    feat: implement function 1 in Application (src/main/java/lotto)
    기능 1. 당첨 번호 입력
    - 당첨 번호 입력 양식 안내 문구 출력: 당첨 번호 6개를 쉼표로 구분해 입력해주세요:
    - 중복되지 않는 1이상 45이하의 자연수 6개가 쉼표(,)로 구분된 문자열을 입력 받음
    - 정상 입력이 아닌 경우 IllegalArgumentException을 발생시킨 뒤 프로그램 종료, 각 로그는 기능 목록을 따름

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,81 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static void guideWinningNumberFormat() {
+        System.out.print("당첨 번호 6개를 쉼표로 구분해 입력해주세요: ");
+    }
+
+    public static void isVoidInput(String winningNumberInput) {
+        if (winningNumberInput.length() == 0)
+            throw new IllegalArgumentException("문자를 입력하십시오.");
+    }
+
+    public static void isCommaAndNumber(String winningNumberInput) {
+        int inputLength = winningNumberInput.length();
+        for (int index = 0; index < inputLength; index++) {
+            char charAtIndex = winningNumberInput.charAt(index);
+            if (charAtIndex >= '0' && charAtIndex <= '9') continue;
+            if (charAtIndex == ',') continue;
+            throw new IllegalArgumentException("쉼표와 숫자 외 다른 문자는 입력이 불가합니다.");
+        }
+    }
+
+    public static void hasSixNumbers(String[] winningNumbers) {
+        if (winningNumbers.length != 6)
+            throw new IllegalArgumentException("당첨 번호는 쉼표로 구분되는 6개의 수로 구성돼야 합니다.");
+    }
+
+    public static List<Integer> parseNumbers(String[] splitNumbers) {
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String numberInString : splitNumbers) {
+            int numberInInteger = Integer.parseInt(numberInString);
+            winningNumbers.add(numberInInteger);
+        }
+        return winningNumbers;
+    }
+
+    public static void checkRange(List<Integer> winningNumbers) {
+        for (int number : winningNumbers)
+            if (number > 45 || number < 1)
+                throw new IllegalArgumentException("당첨 번호는 1이상 45이하의 자연수로 구성해야 합니다.");
+    }
+
+    public static void isDuplicated(List<Integer> winningNumbers) {
+        boolean[] isWinningNumber = new boolean[46];
+        for (int number : winningNumbers) {
+            if (isWinningNumber[number])
+                throw new IllegalArgumentException("당첨 번호엔 중복된 숫자가 없어야 합니다.");
+            isWinningNumber[number] = true;
+        }
+    }
+    public static boolean[] checkWinningNumberByIndex(List<Integer> winningNumbers) {
+        boolean[] isWinningNumber = new boolean[46];
+        for (int number : winningNumbers)
+            isWinningNumber[number] = true;
+        return isWinningNumber;
+    }
+    public static boolean[] getWinningNumbers() {
+        guideWinningNumberFormat();
+        String winningNumberInput = Console.readLine();
+
+        isVoidInput(winningNumberInput);
+        isCommaAndNumber(winningNumberInput);
+
+        String[] splitNumbers = winningNumberInput.split(",");
+        hasSixNumbers(splitNumbers);
+
+        List<Integer> winningNumbers = parseNumbers(splitNumbers);
+        checkRange(winningNumbers);
+        isDuplicated(winningNumbers);
+        return checkWinningNumberByIndex(winningNumbers);
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        boolean[] isWinningNumber = getWinningNumbers();
     }
 }
