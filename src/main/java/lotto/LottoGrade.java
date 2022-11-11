ff1359e9c155bacbc90fa0767e07ced0186fea56
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-11 15:37:14 +0900

    feat(LottoGrade) : create enum
    Create an enum class for lottery winnings
    - FIRST
    - SECOND
    - THIRD
    - FOURTH
    - FIFTH

--- src/main/java/lotto/LottoGrade.java
+++ src/main/java/lotto/LottoGrade.java
@@ -1,4 +1,32 @@
 package lotto;
 
 public enum LottoGrade {
+    FIRST(6, 2_000_000_000, false),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(3, 5_000, false);
+
+    private final int matchCount;
+    private final int price;
+    private final boolean isMatchBonus;
+
+    LottoGrade(int matchCount, int price, boolean isMatchBonus) {
+        this.matchCount = matchCount;
+        this.price = price;
+        this.isMatchBonus = isMatchBonus;
+    }
+
+    //3개 일치 (5,000원) - 1개
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(price).append("개 일치");
+        if (isMatchBonus) {
+            stringBuilder.append(", 보너스 볼 일치");
+        }
+        stringBuilder.append("(").append(price).append("원)");
+
+        return stringBuilder.toString();
+    }
 }
