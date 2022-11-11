143c9535af9b54cc62da97a01435aab4370c74da
sinsu <sinsu19012@gmail.com>
2022-11-11 15:48:14 +0900

    ✅test: Lotto Test 번호 정렬 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTest {
 
@@ -24,6 +26,17 @@ class LottoTest {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
             .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 정렬 되는지 테스트")
+    @Test
+    void orderbyLottoNumber() {
+        List<Integer> testdata = new ArrayList<>();
+        testdata.addAll(List.of(1, 2, 3, 4, 5, 6));
+
+        List<Integer> testLottodata = new ArrayList<>();
+        testLottodata.addAll(List.of(1, 3, 6, 4, 5, 2));
+        assertThat(new Lotto(testLottodata).getNumbers()).isEqualTo(testdata);
     }
 
     // 아래에 추가 테스트 작성 가능
