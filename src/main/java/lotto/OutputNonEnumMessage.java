bc4d4cc7c3439deff9c457ccd12e6b7890b01e7f
Jeongthingamajig <netpetet@naver.com>
2022-11-12 02:23:12 +0900

    Refactor : Message 분리
    OutputEnumMessage와 OutputNonEnumMessage로 분리

---
+++ src/main/java/lotto/OutputNonEnumMessage.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class OutputNonEnumMessage {
+    public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String BUY_LOTTE_MESSAGE = "개를 구매했습니다.";
+    public static final String INPUT_LOTTO_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    public static final String WIN_A_LOTTERY_STATUS = "당첨 통계";
+    public static final String SEPARATION = "---";
+    public static final String USER_RETURN_FRONT = "총 수익률은 %.2f입니다.";
+
+}
