cf80c53e017a2c2136defcb07666379488445bf8
Ji-Un-Gil <wldns2577@gmail.com>
2022-11-11 18:42:48 +0900

    feat(Application.java): get_prize_num function Added function: - 구입한 로또들을 하나씩 확인해 당첨 번호와 3개 이상 일치하는 로또의 갯수를 조건에 맞게 리스트에 더하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,11 +11,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     private static final int ticket_price = 1000;
+    private static final List<Integer> prize = List.of(5000, 50000, 1500000, 30000000, 2000000000);
+    private static List<Integer> prize_num = List.of(0, 0, 0, 0, 0);
 
     public static void main(String[] args) {
         final List<Integer> lotto_win;
         final List<Lotto> lotto_issuance;
         final Integer purchase, bonus;
+        Lotto winLotto;
         purchase = input_validate();
 
         validate(purchase);
@@ -23,7 +26,7 @@ public class Application {
         display_lottos(lotto_issuance);
 
         lotto_win = Arrays.asList(Console.readLine().split(",")).stream().map(s -> Integer.parseInt(((String) s).trim())).collect(Collectors.toList());
-        Lotto winLotto = new Lotto(lotto_win);
+        winLotto = new Lotto(lotto_win);
         bonus = input_validate();
     }
 
@@ -48,17 +51,27 @@ public class Application {
 
     private static List<Lotto> lotto_create(int purchase){
         final int tickets = (int) purchase / ticket_price;
-        final List<Lotto> lotto_issuance = new ArrayList<>();
+        final List<Lotto> issuance = new ArrayList<>();
         for (int i = 0; i < tickets; i++){
-            lotto_issuance.add(new Lotto());
+            issuance.add(new Lotto());
         }
-        return lotto_issuance;
+        return issuance;
     }
 
-    private static void display_lottos(List<Lotto> lotto_issuance){
-        System.out.printf("%d개를 구매했습니다.\n", lotto_issuance.size());
-        for (int i = 0; i < lotto_issuance.size(); i++){
-            lotto_issuance.get(i).display_num();
+    private static void display_lottos(List<Lotto> issuance){
+        System.out.printf("%d개를 구매했습니다.\n", issuance.size());
+        for (int i = 0; i < issuance.size(); i++){
+            issuance.get(i).display_num();
+        }
+    }
+
+    private static void get_prize_num(Lotto winLotto, List<Lotto> issuance, int bonus){
+        int same;
+        for (int i = 0; i < issuance.size(); i++){
+            same = issuance.get(i).compare_win(winLotto, bonus);
+            if (same >= 3){
+                prize_num.set(prize_num.get(same - 3), prize_num.get(same - 3) + 1);
+            }
         }
     }
 }
