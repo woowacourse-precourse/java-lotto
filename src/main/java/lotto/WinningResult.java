67f85b78162cfdde898cc9fda41a55d368652e32
minhee020 <minhee020@naver.com>
2022-11-11 10:13:34 +0900

    feat : enum Rank 적용

--- src/main/java/lotto/WinningResult.java
+++ src/main/java/lotto/WinningResult.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+enum Rank{
+     NONE(0), FIRST(2_000_000_000), SECOND(30_000_000), THIRD(1_500_000), FOURTH(50_000), FIFTH(5_000);
+     int value;
+     Rank(int value){
+          this.value = value;
+     }
+     public int getValue(){
+          return value;
+     }
+}
 public class WinningResult {
      WinningNumber winningNumber;
      private List<Integer> rank;
