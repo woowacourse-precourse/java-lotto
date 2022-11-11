7acbe5a653a8a396c144614f294d948b0821d38d
sangwon <swchoi1997@naver.com>
2022-11-11 16:33:47 +0900

    add: 출력 메세지 추가

--- src/main/java/lotto/domain/view/IOMessage.java
+++ src/main/java/lotto/domain/view/IOMessage.java
@@ -3,7 +3,12 @@ package lotto.domain.view;
 public enum IOMessage {
     INPUT_PURCHASE_LOTTO("구입금액을 입력해 주세요."),
     INPUT_DESIGNATE_FIRSTPLCAE("당첨 번호를 입력해 주세요."),
-    INPUT_DESIGNATE_BONUS("보너스 번호를 입력해 주세요.");
+    INPUT_DESIGNATE_BONUS("보너스 번호를 입력해 주세요."),
+
+    OUTPUT_PURCHASE_LOTTO("개를 구매했습니다."),
+    OUTPUT_LOTTO_RESULT_HEAD("당첨 통계\n---"),
+    STANDARD_STATISTICS_MESSAGE("%d개 일치 (%d원) - %d개"),
+    BONUS_STATISTICS_MESSAGE("%d개 일치, 보너스 볼 일치 (%d원) - %d개");
 
     private String message;
 
