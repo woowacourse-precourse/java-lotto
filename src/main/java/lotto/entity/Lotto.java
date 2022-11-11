d31e25948820a04d0df8616236ab5d004f417a47
woo-chang <clllickme@naver.com>
2022-11-11 20:15:09 +0900

    feat(Lotto): 로또 숫자가 정해진 범위에 속하지 않으면 예외 발생시키는 기능 구현

--- src/main/java/lotto/entity/Lotto.java
+++ src/main/java/lotto/entity/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.entity;
 
 import static lotto.service.LottoService.COUNT;
+import static lotto.service.LottoService.RANGE_END;
+import static lotto.service.LottoService.RANGE_START;
 
 import java.util.List;
 
@@ -18,6 +20,19 @@ public class Lotto {
             throw new IllegalArgumentException(
                 "로또는 " + COUNT + "개를 가져야 합니다. 입력 : " + numbers.size());
         }
+        if (hasOutOfRangeNumber(numbers)) {
+            throw new IllegalArgumentException(
+                "로또 번호는 " + RANGE_START + "부터 " + RANGE_END + " 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private boolean hasOutOfRangeNumber(List<Integer> numbers) {
+        return numbers.stream()
+            .anyMatch(this::outOfRange);
+    }
+
+    private boolean outOfRange(int number) {
+        return RANGE_START > number || RANGE_END < number;
     }
 
     // TODO: 추가 기능 구현
