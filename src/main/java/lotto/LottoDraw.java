4068ddacba4888db24852fde6db409705a1d228b
ghkdgus29 <ghkdgus29@naver.com>
2022-11-11 11:05:50 +0900

    feat: UI 클래스를 통해 로또 당첨 번호 입력

--- src/main/java/lotto/LottoDraw.java
+++ src/main/java/lotto/LottoDraw.java
@@ -1,20 +1,30 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class LottoDraw {
 
     private int totalPrizeMoney;
     private Map<String, Integer> numberOfWins = new HashMap<>();
+    private final List<Integer> winningNumber;
 
     public LottoDraw() {
         this.totalPrizeMoney = 0;
         for (int rank = 1; rank <= 5; rank++) {
             numberOfWins.put(String.valueOf(rank) + "등", 0);
         }
+        this.winningNumber = enterWinningNumber();
+    }
+
+    public List<Integer> enterWinningNumber() {
+        String input = Console.readLine();
+        String[] split = input.split(",");
+        Integer[] numbers = Arrays.stream(split).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
+        return Arrays.asList(numbers);
     }
 
     public int getTotalPrizeMoney() {
@@ -23,5 +33,9 @@ public class LottoDraw {
 
     public Map<String, Integer> getNumberOfWins() {
         return numberOfWins;
+    }
+
+    public List<Integer> getWinningNumber() {
+        return winningNumber;
     }
 }
--- src/test/java/lotto/LottoDrawTest.java
+++ src/test/java/lotto/LottoDrawTest.java
@@ -3,6 +3,9 @@ package lotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,4 +22,13 @@ class LottoDrawTest {
                 .contains(entry("1등", 0), entry("2등", 0), entry("3등", 0), entry("4등", 0), entry("5등", 0));
     }
 
+    @Test
+    void enterWinningNumber() {
+        ByteArrayInputStream in = new ByteArrayInputStream("1,3,5,7,9,13".getBytes());
+        System.setIn(in);
+
+
+        LottoDraw lottoDraw = new LottoDraw();
+        assertThat(lottoDraw.getWinningNumber()).isEqualTo(List.of(1, 3, 5, 7, 9, 13));
+    }
 }
