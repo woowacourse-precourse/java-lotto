cbe73ed1d92e5ec979d7a79c65dd8c9be90ffc7f
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-11 23:11:54 +0900

    feat: 로또 개수에 따른 로또리스트 추출 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,10 +1,25 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoGame {
     private static final String TYPE_ERROR_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
     private static final String INPUT_MONEY_ERROR_MESSAGE = "[ERROR] 금액을 1000원 단위로 입력해주세요.";
+
+
+    public List<Lotto> pickLottoList(int count) {
+        List<Lotto> lottoList = new ArrayList<>();
+        while (count > 0) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoList.add(new Lotto(numbers));
+            count -= 1;
+        }
+        return lottoList;
+    }
 
     public int countLotto(int money) {
         if (money % 1000 != 0) {
