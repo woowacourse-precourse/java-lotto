5f09d39baf934a3cac98076bed9b744044e50ec5
khj <bbb6254177@gmail.com>
2022-11-11 19:08:31 +0900

    feat(lotto): add function to LottoGenerator class
    add function to generate list of lotto
    add function to generate a lotto

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,11 +1,21 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
-    public List<Lotto> generateLotto() {
+    public List<Lotto> generateLottoList(int numOfLotto) {
         List<Lotto> lotto = new ArrayList<>();
+
+        for (int i = 0; i<numOfLotto; i++){
+            lotto.add(generateLotto());
+        }
+
         return lotto;
+    }
+
+    private Lotto generateLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
     }
 }
