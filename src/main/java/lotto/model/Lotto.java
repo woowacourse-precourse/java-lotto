f6cdd2352da03d5bdaba7f314e964506afa3e3c1
Cyma-s <ssol0319@naver.com>
2022-11-11 17:41:47 +0900

    feat(Lotto): add duplicate numbers confirmation function

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -7,12 +7,29 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validateNumberCount(numbers);
+        validateDuplicateNumbers(numbers);
         this.numbers = numbers;
     }
 
     private void validateNumberCount(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException(ErrorConstant.INVALID_DIGITS_COUNT);
+            throw new IllegalArgumentException(InputErrorMessage.INVALID_DIGITS_COUNT.getErrorMessage());
+        }
+    }
+
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        for(int i = 0; i<numbers.size(); i++) {
+            checkDuplicateNumber(numbers, i);
+        }
+    }
+
+    private void checkDuplicateNumber(List<Integer> numbers, int index) {
+        Integer uncheckedNumber = numbers.get(index);
+        for(int j = index; j<numbers.size(); j++) {
+            Integer nextNumber = numbers.get(j);
+            if(uncheckedNumber.equals(nextNumber)) {
+                throw new IllegalArgumentException(InputErrorMessage.DUPLICATE_NUMBERS.getErrorMessage());
+            }
         }
     }
 }
