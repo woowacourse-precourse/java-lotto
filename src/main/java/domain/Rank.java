f883b63742d84812a9feee6a424c758248cb681f
Limjongho <whdgh9873@icloud.com>
2022-11-11 20:04:51 +0900

    feat(Rank): nothing 열거형 상수 추가

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -5,7 +5,8 @@ public enum Rank {
     second(5,true,2),
     third(5,false,3),
     fourth(4,false,4),
-    fifth(3, false, 5);
+    fifth(3, false, 5),
+    nothing(0,false,0);
 
     public final int matchCount;
     public final boolean checkBonusNumber;
