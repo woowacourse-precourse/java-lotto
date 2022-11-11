f171faf3e276beb17708a6d6f43f5cf8e5f20e07
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-11 15:33:22 +0900

    feat(InputLottoNumber) : 사용자로 부터 값을 입력받아 List에 저장하는 기능구현

--- src/main/java/lotto/InputLottoNumber.java
+++ src/main/java/lotto/InputLottoNumber.java
@@ -1,11 +1,31 @@
 package lotto;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
-
+import camp.nextstep.edu.missionutils.Console;
+import exceptioncase.InputLottoNumberException;
 public class InputLottoNumber {
     public static List<Integer> inputLottoAnswerNumber(){
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer;
+        String input = Console.readLine();
+        answer = makeAnswerNumbers(input);
+        String bonusNumber = Console.readLine();
+        addBonusNumber(answer,bonusNumber);
         return answer;
     }
+    public static List<Integer> makeAnswerNumbers(String input){
+        InputLottoNumberException.exceptionsTest(input);
+        List<Integer> answerNumbers = new LinkedList<>();
+        for(String nowNumber : input.split(",")){
+            answerNumbers.add(Integer.parseInt(nowNumber));
+        }
+        Collections.sort(answerNumbers);
+        return answerNumbers;
+    }
+    public static void addBonusNumber(List<Integer> answerNumbers, String bonusNumber){
+        InputLottoNumberException.bonusNumberTest(bonusNumber,answerNumbers);
+        answerNumbers.add(Integer.parseInt(bonusNumber));
+    }
+
 }
