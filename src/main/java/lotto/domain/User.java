70c68f629d17c657dd938dfd2b791f9281eee627
juhee park <jouhi3322@gmail.com>
2022-11-12 00:44:14 +0900

    feat : 로또 구입 금액에 맞춰 로또 발행
    로또클래스 패키지 수정

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.service.LottoGenerator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static constants.Constants.START_MSG;
@@ -13,7 +15,18 @@ public class User {
 
     public void buyLotto(){
         purchaseMoney = scanPayMoney();
-        purchaseLottoNum = purchaseMoney%1000;
+        purchaseLottoNum = purchaseMoney/1000;
+        purchaseLotto = issueLotto(purchaseLottoNum);
+    }
+
+    private List<Lotto> issueLotto(int purchaseLottoNum) {
+        LottoGenerator generator = new LottoGenerator();
+        List<Lotto> tempLotto = new ArrayList<>();
+        for(int buy=0;buy<purchaseLottoNum;buy++){
+            List<Integer> generatedNums =  generator.generateRandomNums();
+            tempLotto.add(new Lotto(generatedNums));
+        }
+        return tempLotto;
     }
 
     private int scanPayMoney() {
@@ -27,7 +40,5 @@ public class User {
         if(money%1000 ==0 )return;
         throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위로 끝나야 합니다. ");
     }
-
-
 
 }
---
+++ src/main/java/lotto/service/LottoGenerator.java
@@ -0,0 +1,13 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoGenerator {
+
+    public List<Integer> generateRandomNums() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+}
