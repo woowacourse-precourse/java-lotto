b914ce55c53f9f8e70c2c8deff1be81a0daa99f5
chlwodyd9829 <chlwodyd9829@naver.com>
2022-11-11 13:25:08 +0900

    style: enum 클래스 변경

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,7 +2,11 @@ package lotto.domain;
 
 public enum Money {
 
-    FIRST(2_000_000_000,6),SECOND(30_000_000,7),THIRD(1_500_000,5),FOURTH(50_000,4),FIFTH(5_000,3);
+    FIRST(2_000_000_000,6),
+    SECOND(30_000_000,7),
+    THIRD(1_500_000,5),
+    FOURTH(50_000,4),
+    FIFTH(5_000,3);
     private final Integer description;
     private final Integer count;
 
