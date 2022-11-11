0bc0a4e6fe43101f0a70935827745181dc08207d
Eundms <dms011221@gmail.com>
2022-11-11 14:39:08 +0900

    fix(Buyer): return unmodifiableList

--- src/main/java/lotto/domain/Buyer.java
+++ src/main/java/lotto/domain/Buyer.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Buyer {
@@ -9,12 +10,11 @@ public class Buyer {
         if (purchaseMoney % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위여야 합니다.");
         }
-
         int lottoTicketAmount=purchaseMoney/1000;
         holdingLottoTickets=lottoVendor.sale(lottoTicketAmount);
     }
     public List<Lotto> getHoldingLottoTickets() {
-        return holdingLottoTickets;
+        return Collections.unmodifiableList(holdingLottoTickets);
     }
     public int countHoldingLotto(){
         return holdingLottoTickets.size();
