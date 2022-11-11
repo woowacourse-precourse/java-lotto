9d75a2dcf57e0dfe98d426104c954b54316b0c8a
kdkdhoho <hkim4410@naver.com>
2022-11-11 14:23:58 +0900

    refactor(Message): NUMBER_RANGE_ERROR 메시지 수정

--- src/main/java/lotto/domain/enums/Message.java
+++ src/main/java/lotto/domain/enums/Message.java
@@ -7,7 +7,7 @@ public enum Message {
     RESULT_THIRD("5개 일치 (1,500,000원) - "),
     RESULT_SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
     RESULT_FIRST("6개 일치 (2,000,000,000원) - "),
-    NUMBER_RANGE_ERROR("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    NUMBER_RANGE_ERROR("[ERROR] 로또 번호는 1부터 45 사이의 중복되지 않는 숫자여야 합니다."),
     NOT_THOUSAND_UNIT_INPUT_ERROR("[ERROR] 입력 금액은 1,000원 단위입니다."),
     BONUS_NUMBER_INPUT_ERROR("[ERROR] 보너스 번호는 로또 번호와 중복되지 않는 1~9 사이의 하나의 숫자입니다."),
     WINNING_NUMBER_INPUT_ERROR("1부터 45 사이의 수를 중복되지 않게 쉼표(,)를 이용하여 입력해주세요.\n예) 1,2,3,4,5,6"),
