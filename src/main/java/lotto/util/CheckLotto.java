58c7d0a9a8ea0e6514c53dace339e37fedc6e989
kki4504 <rhkddlf9876@gmail.com>
2022-11-11 09:47:45 +0900

    feat(CheckLotto) : 생성자 구현

--- src/main/java/lotto/util/CheckLotto.java
+++ src/main/java/lotto/util/CheckLotto.java
@@ -1,4 +1,16 @@
 package lotto.util;
 
+import lotto.util.input.BonusNumber;
+import lotto.util.input.WinNumbers;
+
 public class CheckLotto {
+    private WinNumbers winNumbers;
+    private BonusNumber bonusNumber;
+    private BuyLotto buyLotto;
+
+    public CheckLotto(WinNumbers winNumbers, BonusNumber bonusNumber, BuyLotto buyLotto) {
+        this.winNumbers = winNumbers;
+        this.bonusNumber = bonusNumber;
+        this.buyLotto = buyLotto;
+    }
 }
