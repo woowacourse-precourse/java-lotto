924d5282f189ae3f24eb74719208e0e57c9476c6
은지 <midzy0228@gmail.com>
2022-11-12 01:02:42 +0900

    feat(당첨 번호): 1부터 45 까지의 중복되지 않는 6개 숫자인지 확인 기능 구현

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -1,18 +1,36 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Validator {
 
-    public void validDatePurchaseAmount(String purchaseAmount) {
-        validDateType(purchaseAmount);
-        validDateThousandUnitNumber(Integer.parseInt(purchaseAmount));
-        validRangeNumber(Integer.parseInt(purchaseAmount));
+    private int purchaseAmount;
+    private int bonusNumber;
+    private List<Integer> lottoNumbers;
+
+    public void validDatePurchaseAmount(String input) {
+        validDateType(input);
+        int purchaseAmount = Integer.parseInt(input);
+        validDateThousandUnitNumber(purchaseAmount);
+        validRangeNumber(purchaseAmount);
     }
 
-    public void validDateBonusNumber(String bonusNumber) {
-        validDateType(bonusNumber);
-        validRangeLottoNumber(Integer.parseInt(bonusNumber));
+    public void validDateBonusNumber(String input) {
+        validDateType(input);
+        int bonusNumber = Integer.parseInt(input);
+        validRangeLottoNumber(bonusNumber);
+    }
+
+    public void validDateWinningNumbers(String input) {
+        List<String> lottoNumbers = List.of(input.split(","));
+        validDateLottoNumberSize(lottoNumbers);
+        validDateLottoNumbers(lottoNumbers);
+        List<Integer> winningLottoNumbers = lottoNumbers.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        nonOverlap(winningLottoNumbers);
+        System.out.println(input);
     }
 
     private void validDateType(String input) {
@@ -35,6 +53,24 @@ public class Validator {
     private void validRangeLottoNumber(Integer number) {
         if (!(1 <= number && number <= 45)) {
             throw new IllegalArgumentException("[ERROR] 로또 숫자는 1이상 45 이하이 숫자를 입력하셔야 합니다.");
+        }
+    }
+    private void validDateLottoNumberSize(List<String> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 총 6개의 숫자를 입력해주세요.");
+        }
+    }
+    private void validDateLottoNumbers(List<String> numbers) {
+        for (String number : numbers) {
+            validDateType(number);
+            validRangeLottoNumber(Integer.parseInt(number));
+        }
+    }
+
+    private void nonOverlap(List<Integer> numbers) {
+        List<Integer> tmp = numbers.stream().distinct().collect(Collectors.toList());
+        if (tmp.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복되지 않은 숫자를 입력해주세요.");
         }
     }
 }
