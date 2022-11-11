46d5008bfd0b19e3558a7e9847d6b3a7f5a219c8
abbd2 <ajkety00@gmail.com>
2022-11-11 15:03:37 +0900

    Feat(ShowMessage): "당첨 번호를 입력해 주세요."를 출력하는 메서드 구현

--- src/main/java/lotto/ShowMessage.java
+++ src/main/java/lotto/ShowMessage.java
@@ -2,15 +2,20 @@ package lotto;
 
 public class ShowMessage {
 
-    private final static String AMOUNT_BUY_MESSAGE = "구입 금액을 입력해 주세요.";
+    private final static String INPUT_AMOUNT_BUY_MESSAGE = "구입 금액을 입력해 주세요.";
     private final static String HOW_MANY_BUY_MESSAGE = "개를 구매했습니다.";
+    private final static String INPUT_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
 
     public void showInputAmountBuy() {
-        System.out.println(AMOUNT_BUY_MESSAGE);
+        System.out.println(INPUT_AMOUNT_BUY_MESSAGE);
     }
 
     public void showHowManyBuy(int number) {
         System.out.println(number + HOW_MANY_BUY_MESSAGE);
+    }
+
+    public void showInputLottoNumber() {
+        System.out.println(INPUT_LOTTO_NUMBER);
     }
 
 }
