dddc67c4d7f3b763c16776ca74b35400851bdc1c
Your Name <you@example.com>
2022-11-11 15:09:51 +0900

    feat(LottoInputOutput): 로또 정답 번호 입력, 보너스 번호 입력 구현
    1~45의 숫자인지 확인
    중복 확인

--- src/main/java/lotto/LottoInputOutput.java
+++ src/main/java/lotto/LottoInputOutput.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 import camp.nextstep.edu.missionutils.Console;
@@ -41,6 +42,52 @@ public class LottoInputOutput {
     public void printLotto(List<Lotto> buyerLotto) {
         Stream<Lotto> stream = buyerLotto.stream();
         stream.forEach(lotto -> lotto.printLotto());
+    }
+
+    public List<Integer> getWinnigLotto() {
+        String winnigLottoString = Console.readLine();
+
+        String[] split = winnigLottoString.split(",");
+
+        List<Integer> winningNumbers = arrayStringToIntList(split);
+
+        return winningNumbers;
+    }
+
+    private List<Integer> arrayStringToIntList(String[] split) {
+        List<Integer> winningNumbers = new ArrayList<>();
+        for(int index = 0; index < split.length;index++)
+        {
+            String singleNumString = split[index];
+            int num = stringToIntAndCheckError(winningNumbers, singleNumString);
+            winningNumbers.add(num);
+        }
+        return winningNumbers;
+    }
+
+    private int stringToIntAndCheckError(List<Integer> winningNumbers, String string) {
+        int num = stringToInt(string);
+
+        checkNumInRange(num);
+
+        checkDuplication(winningNumbers, num);
+        return num;
+    }
+
+    private void checkNumInRange(int num) {
+        if(num <1 || num > 45)
+            throw new IllegalArgumentException("[ERROR] 1~45의 범위에 있는 숫자를 입력해주세요.");
+    }
+
+    private void checkDuplication(List<Integer> winningNumbers, int num) {
+        if(winningNumbers.contains(num))
+            throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자를 입력해주세요.");
+    }
+
+    public int getBonusNumber(List<Integer> winningNumber) {
+        String bonusNumString = Console.readLine();
+        int bonusNum = stringToIntAndCheckError(winningNumber, bonusNumString);
+        return bonusNum;
     }
 
 }
