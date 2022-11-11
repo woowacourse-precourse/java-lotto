c61741dbc76bff9e50bf22fcba34803e05dedf99
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-11 18:27:54 +0900

    feat: 로또 수량 확인 기능 추가

--- src/main/java/lotto/Manager.java
+++ src/main/java/lotto/Manager.java
@@ -3,9 +3,18 @@ package lotto;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
 
 public class Manager {
+
     public List<Integer> getRandomNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    public int getAmount(int won) {
+        if (won % Lotto.PRICE != 0) {
+            throw new IllegalArgumentException();
+        }
+        return won / Lotto.PRICE;
     }
 }
