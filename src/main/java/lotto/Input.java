5eef0718181487131d6d5be996f0a0caa4edfa8d
wugawuga <dnr3655@naver.com>
2022-11-11 14:44:57 +0900

    refactor: 상수 분리

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -6,16 +6,23 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Input {
+    private static final int ZERO = 0;
+    private static final String SEPARATOR = ",";
+    private static final int LOTTO_MINIMUM_AMOUNT = 1000;
+    private static final String NUMBER_FORMAT_ERROR = "[ERROR] 숫자를 입력해 주세요.";
+    private static final String SEPARATOR_ERROR = "[ERROR] 구분자 '"+ SEPARATOR +"' 를 입력해 주세요.";
+    private static final String AMOUNT_UNIT_ERROR = "[ERROR] " + LOTTO_MINIMUM_AMOUNT + "원 단위로 입력하세요.";
+
     public static int purchaseAmount() {
         int parseInput = changeNumber(input());
         checkLottoAmount(parseInput);
-        return parseInput / 1000;
+        return parseInput / LOTTO_MINIMUM_AMOUNT;
     }
 
     public static List<Integer> winningNumber() {
         String input = input();
         checkContainSeparator(input);
-        return Stream.of(input.split(","))
+        return Stream.of(input.split(SEPARATOR))
                 .map(Input::changeNumber)
                 .collect(Collectors.toList());
     }
@@ -24,19 +31,19 @@ public class Input {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+            throw new IllegalArgumentException(NUMBER_FORMAT_ERROR);
         }
     }
 
     private static void checkContainSeparator(String input) {
-        if (!input.contains(",")) {
-            throw new IllegalArgumentException("[ERROR] 구분자 ',' 를 입력해 주세요.");
+        if (!input.contains(SEPARATOR)) {
+            throw new IllegalArgumentException(SEPARATOR_ERROR);
         }
     }
 
     private static void checkLottoAmount(int input) {
-        if (input % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력하세요.");
+        if (input % LOTTO_MINIMUM_AMOUNT != ZERO) {
+            throw new IllegalArgumentException(AMOUNT_UNIT_ERROR);
         }
     }
 
