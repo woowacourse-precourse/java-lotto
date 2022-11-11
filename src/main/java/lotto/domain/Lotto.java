922564241535be7e4c1603153e53d91e4336c521
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-11 21:56:18 +0900

    feat: 금액을 입력 받으면 그에 맞는 개수만큼 로또를 발행하는 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,17 +1,38 @@
 package lotto.domain;
 
+import static lotto.validator.LottoNumberValidator.validateExistDuplicateNumber;
+import static lotto.validator.LottoNumberValidator.validateRangeOfNumber;
+import static lotto.validator.UserInputValidator.validateWinningLottoNumberComplyWithRule;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import lotto.constant.Constants;
+import lotto.constant.LottoNumber;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        validateRangeOfNumber(numbers);
+        validateExistDuplicateNumber(numbers);
+
+        this.numbers = numbers.stream().sorted().collect(Collectors.toList());
+    }
+
+    public Lotto(String userInput) {
+        validateWinningLottoNumberComplyWithRule(userInput);
+        List<Integer> numbers = convertToNumbers(userInput);
+        validate(numbers);
+        validateRangeOfNumber(numbers);
+        validateExistDuplicateNumber(numbers);
+
+        this.numbers = numbers.stream().sorted().collect(Collectors.toList());
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LottoNumber.NUMBER_OF_LOTTO_NUMBERS.getNumber()) {
             throw new IllegalArgumentException();
         }
     }
@@ -19,5 +40,11 @@ public class Lotto {
     // TODO: 추가 기능 구현
     public boolean isContains(int number) {
         return numbers.contains(number);
+    }
+
+    private List<Integer> convertToNumbers(String str) {
+        return Arrays.stream(str.split(Constants.WINNING_LOTTO_NUMBER_INPUT_SEPARATOR))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/lotto/generator/LottoGenerator.java
+++ src/main/java/lotto/generator/LottoGenerator.java
@@ -1,11 +1,30 @@
 package lotto.generator;
 
+import static lotto.constant.Constants.UNIT_OF_LOTTO_PURCHASE;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
+import lotto.constant.LottoNumber;
 import lotto.domain.Lotto;
 
 public class LottoGenerator {
 
     public static List<Lotto> generateByMoney(int money) {
-        return null;
+        int availableQuantity = calculateAvailableQuantity(money);
+        List<Lotto> purchasedLottos = new ArrayList<>();
+        for (int purchasingQuantity = 0; purchasingQuantity < availableQuantity; purchasingQuantity++) {
+            purchasedLottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(
+                    LottoNumber.START_NUMBER_OF_LOTTO.getNumber(),
+                    LottoNumber.END_NUMBER_OF_LOTTO.getNumber(),
+                    availableQuantity
+            )));
+        }
+
+        return purchasedLottos;
+    }
+
+    private static int calculateAvailableQuantity(int money) {
+        return money / UNIT_OF_LOTTO_PURCHASE;
     }
 }
