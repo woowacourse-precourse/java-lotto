3aaeb68df011f4d0092b5152856e6eb566de9f4b
alstjr706 <alstjr706@naver.com>
2022-11-11 15:10:08 +0900

    fix : (로또 게임) 보너스 번호가 해당 되는 로또 찾기 구현

---
+++ src/main/java/lotto/domain/BonusLottoBall.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BonusLottoBall {
+    public List<Integer> findNumber(List<List<Integer>> random, List<Integer> resultNumber, int bonus) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < resultNumber.size(); i++) {
+            int cont = 0;
+
+            if (resultNumber.get(i) == 5) { // 5개까지 맞는 티켓인가 확인
+
+                // 여기까지 온 상태면 로또 맞은 갯수가 5개라는 것이다.
+                for (List<Integer> randomNumber : random) {
+                    if (randomNumber.contains(bonus)) { // 보너스랑 같은 숫자가 있는지 확인다.
+                        cont++; // 있다면 카운트
+                        break;
+                    }
+                }
+            }
+            result.add(cont);
+        }
+
+        return result;
+    }
+
+}
--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -14,18 +14,27 @@ public class LottoGame {
         System.out.println(paid);
 
         RandomLotto randomLotto = new RandomLotto();
-        List<List<Integer>> lists = randomLotto.randomGenerator(paid);
+        List<List<Integer>> lists = randomLotto.randomGenerator(paid); // 랜덤 리스트
         for (List<Integer> b : lists) {
             System.out.println(b);
         }
 
         LottoComparison lottoComparison = new LottoComparison(new WinningNumber());
-        List<Integer> number = lottoComparison.findNumber(lists);
+        List<Integer> number = lottoComparison.findNumber(lists); // 결과 리스트
 
         for (int a : number) {
             System.out.println(a);
         }
 
+        AdditionalNumber additionalNumber = new AdditionalNumber();
+        int bonus = additionalNumber.getBonus();
+
+        BonusLottoBall bonusLottoBall = new BonusLottoBall();
+        List<Integer> bonusNumbers = bonusLottoBall.findNumber(lists, number, bonus);
+
+        for (int c : bonusNumbers) {
+            System.out.println(c);
+        }
 
     }
 }
