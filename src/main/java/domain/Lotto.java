9d2f47f57a624f632123faf5cd09675d4ffd10e3
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-11 21:54:37 +0900

    feat (Lotto) : 검증 메서드 구현

---
+++ src/main/java/domain/Lotto.java
@@ -0,0 +1,34 @@
+package domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validateSize(numbers);
+        validateReference(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // TODO: 추가 기능 구현
+    private void validateReference(List<Integer> numbers) {
+        int referenceSize = new HashSet<>(numbers).size();
+        if (numbers.size() != referenceSize) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+}
