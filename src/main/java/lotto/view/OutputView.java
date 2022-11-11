89187b888144231450f94be4516268263d85712f
heesang <dlagmltkd12@naver.com>
2022-11-12 06:16:38 +0900

    feat: 구입 금액 입력 메시지 출력 기능

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class OutputView {
+
+    public static final String INPUT_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static void printPurchasePrice() {
+        System.out.println(INPUT_PURCHASE_PRICE_MESSAGE);
+    }
+}
