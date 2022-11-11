a5e779aa2bb6566e146b74533051ec293c88b66a
panorama713 <yaiba713@gmail.com>
2022-11-11 19:13:06 +0900

    test(Input): 보너스 당첨 번호와 중복 에러 테스트 기능 구현

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -62,6 +63,14 @@ public class InputTest {
     @Test
     void bonusRangeErrorTest(){
         assertThatThrownBy(() -> Input.bonusRangeError(46))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 당첨 번호와 중복 에러가 생긴다")
+    @Test
+    void bonusDupleErrorTest(){
+        List<Integer> testList = Arrays.asList(1,2,3,4,5,6);
+        assertThatThrownBy(() -> Input.bonusDupleError(testList,1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
