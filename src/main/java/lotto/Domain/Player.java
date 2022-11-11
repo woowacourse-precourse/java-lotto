2b35f301632fd3cdc07d0ceb4e527ba867be933b
Park shinyeong <syngh503@gmail.com>
2022-11-11 22:55:47 +0900

    Feat(Player): 로또 목록 생성 메소드 구현
    로또 구입 개수를 매개변수로 받고, 개수만큼 로또 목록을 세팅하는 setLottos 메소드를 구현하였습니다.
    
    Relate to: #1

--- src/main/java/lotto/Domain/Player.java
+++ src/main/java/lotto/Domain/Player.java
@@ -1,16 +1,31 @@
 package lotto.Domain;
 
+import Utils.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
     private int money;
     private int lottoCount;
-    private int Lottos;
+    private Lotto playerLotto;
+    private int playerBonusNumber;
+    private List<Lotto> Lottos;
 
-    public void setLottoCount(int money){
+    public int getLottoCount() {
+        return this.lottoCount;
+    }
+
+    public void setLottoCount(int money) {
         final int THOUSAND_WON = 1000;
         this.lottoCount = money / THOUSAND_WON;
     }
 
-    public int getLottoCount() {
-        return this.lottoCount;
+    public void setLottos(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(new Lotto(RandomUtils.getRandomLottoNumber()));
+        }
+        this.Lottos = lottos; 
     }
 }
