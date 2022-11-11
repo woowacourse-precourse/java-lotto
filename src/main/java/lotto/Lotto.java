5be2cdd7e46c9602f895bd85452e98e0ac1cb853
Polite-Jehu <werber112@kyonggi.ac.kr>
2022-11-12 00:04:47 +0900

    feat(Lotto): add array input as string is also added to be processed

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,6 +5,20 @@ import java.util.*;
 public class Lotto {
     private final List<Integer> numbers;
 
+    public Lotto(String[] numbers) {
+        try {
+            ArrayList<Integer> changeNumbers = new ArrayList<>();
+            for (int i = 0; i < numbers.length; i++) {
+                changeNumbers.add(Integer.parseInt(numbers[i]));
+            }
+            validate(changeNumbers);
+            this.numbers = changeNumbers;
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+
+    }
+
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
@@ -16,10 +30,14 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        for (int i = 0; i < numbers.size(); i++) {
+            int number = numbers.get(i);
+            if (!(number >= 1 && number <= 45))
+                throw new IllegalArgumentException();
+        }
     }
 
-    public String getLottoText()
-    {
+    public String getLottoText() {
         Collections.sort(numbers);
         return Arrays.toString(numbers.toArray(new Integer[numbers.size()]));
     }
