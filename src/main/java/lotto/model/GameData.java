dffa19c6b3e9604e55e8d5d75c139bb47d58b52e
정승구 <jeongseung-gu@jeongseung-guui-MacBookAir.local>
2022-11-11 22:53:56 +0900

    feat: GameData클래스 및 내부 변수 구현

---
+++ src/main/java/lotto/model/GameData.java
@@ -0,0 +1,11 @@
+package lotto.model;
+
+import java.util.List;
+
+public class GameData {
+    private List<Integer> winningNumber;
+    private int bonusNumber;
+    private int[] winningCount;
+    private int winningAmount;
+    private int returnOfRate;
+}
