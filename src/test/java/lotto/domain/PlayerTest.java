318614911a8ed5251152fbb9df573fc586e04ece
heesang <dlagmltkd12@naver.com>
2022-11-11 19:02:57 +0900

    test: PlayerTest import문 정리

--- src/test/java/lotto/domain/PlayerTest.java
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -1,7 +1,5 @@
 package lotto.domain;
 
-import lotto.domain.Lotto;
-import lotto.domain.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
