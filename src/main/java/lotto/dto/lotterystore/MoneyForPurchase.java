ec64265952427c29229fb35d4b885a4df9631fee
ysng_ysng <yeasung67@gmail.com>
2022-11-11 23:11:46 +0900

    feat(MoneyForPurchase): Implement the DTO to transfer moneyForPurchase
    복권구매금액을 전송하는 DTO를 구현했습니다.

---
+++ src/main/java/lotto/dto/lotterystore/MoneyForPurchase.java
@@ -0,0 +1,13 @@
+package lotto.dto.lotterystore;
+
+public class MoneyForPurchase {
+	private final String money;
+
+	public MoneyForPurchase(String money) {
+		this.money = money;
+	}
+
+	public String getMoney() {
+		return money;
+	}
+}
