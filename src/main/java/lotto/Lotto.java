de1595c6eff0f45e0d23f9add955734ed3fcf59d
suheonjoo <suheonj95@naver.com>
2022-11-11 13:43:31 +0900

    feat(Lotto): 생성된 사용자 로또 번호 에러 체크 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,10 +11,31 @@ public class Lotto {
 	}
 
 	private void validate(List<Integer> numbers) {
+		validNumbersSize(numbers);
+		validRange(numbers);
+		validDuplicated(numbers);
+	}
+
+	private void validNumbersSize(List<Integer> numbers) {
 		if (numbers.size() != 6) {
 			throw new IllegalArgumentException();
 		}
 	}
 
-	// TODO: 추가 기능 구현
+	private void validDuplicated(List<Integer> numbers) {
+		//중복되지 않는 숫자
+		int count = (int)numbers.stream().filter(n -> numbers.contains(n)).count();
+		if (count != 6) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void validRange(List<Integer> numbers) {
+		//범위 1부터 45 까지
+		int count = (int)numbers.stream().filter(i -> 1 <= i && i <= 45).count();
+		if (count != 6) {
+			throw new IllegalArgumentException();
+		}
+	}
+
 }
