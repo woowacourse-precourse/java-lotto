6d7c6f1a2fc69264688878c8ddd1e09feebf8e97
HubCreator <chocomilk8604@gmail.com>
2022-11-11 12:30:54 +0900

    refactor(InputView) : 입력값을 받아와야 하는 것들만 로직들을 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,9 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Lotto;
 import lotto.domain.Result;
 import lotto.domain.ResultStatistics;
 import lotto.domain.WinLotto;
-import lotto.util.Util;
 import lotto.view.InputView;
 
 import java.util.ArrayList;
@@ -14,25 +12,13 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         try {
-            int count = InputView.getView();
+            int count = InputView.showPurchaseView();
+            List<Lotto> generatedLottos = generateLottos(count);
 
-            List<Lotto> generatedLottos = new ArrayList<>();
+            List<Integer> validatedWinNumbers = InputView.showWinLottoNumberView();
+            int bonusNumber = InputView.showWinBonusNumberView();
 
-            for (int i = 0; i < count; i++) {
-                Lotto lotto = Lotto.create();
-                generatedLottos.add(lotto);
-                System.out.println(lotto.printNumbers());
-            }
-
-            System.out.println("당첨 번호를 입력해 주세요.");
-            String winNumbers = Console.readLine();
-            List<Integer> validatedWinNumbers = Util.isValidWinNumbers(winNumbers);
-
-            System.out.println("보너스 번호를 입력해 주세요.");
-            String bonusNumber = Console.readLine();
-
-            WinLotto winLotto = new WinLotto(validatedWinNumbers, Integer.parseInt(bonusNumber));
-
+            WinLotto winLotto = new WinLotto(validatedWinNumbers, bonusNumber);
 
             System.out.println("당첨 통계");
             System.out.println("---");
@@ -54,5 +40,15 @@ public class Application {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public static List<Lotto> generateLottos(int count) {
+        List<Lotto> generatedLottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            Lotto lotto = Lotto.create();
+            generatedLottos.add(lotto);
+            System.out.println(lotto.printNumbers());
+        }
+        return generatedLottos;
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,14 +3,27 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.util.Util;
 
+import java.util.List;
+
 public class InputView {
 
-    public static int getView() {
+    public static int showPurchaseView() {
         System.out.println("구입금액을 입력해 주세요.");
         String amount = Console.readLine();
         Util.isValidAmount(amount);
         int count = Integer.parseInt(amount) / 1000;
         System.out.println(count + "개를 구매했습니다.");
         return count;
+    }
+
+    public static List<Integer> showWinLottoNumberView() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winNumbers = Console.readLine();
+        return Util.isValidWinNumbers(winNumbers);
+    }
+
+    public static int showWinBonusNumberView() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
     }
 }
