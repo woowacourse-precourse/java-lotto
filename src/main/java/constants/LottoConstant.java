d30db5a687b63e4003da4a50ea207d5ad9cff829
scyllacore <yms8795@gmail.com>
2022-11-11 22:15:13 +0900

    refactor : 로또 생성에 필요한 상수들을 클래스에 분리.

--- src/main/java/constants/LottoConstant.java
+++ src/main/java/constants/LottoConstant.java
@@ -1,4 +1,11 @@
 package constants;
 
 public class LottoConstant {
+    public static final int NUMBERS_SIZE = 6;
+    public static final int NUMBER_START = 1;
+    public static final int NUMBER_END = 45;
+
+    public static final int ONE_LOTTO_PRICE = 1000;
+
+
 }
