8f3ca53bbe1d95310d84862cf8db863579549b7a
yangjiyoung <0112jyoung@naver.com>
2022-11-11 21:36:48 +0900

    feat: revise input logic

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.domain.LottoQuantity;
 import lotto.view.InputView;
 
 import java.util.List;
@@ -8,15 +9,16 @@ public class LottoController {
     private List<Integer> userLottoNumbers;
     private int userLottoQuantity;
     private int userLottoMoney;
+    private LottoQuantity lottoQuantity = new LottoQuantity();
 
     public void startLotto() {
         getUserMoney();
     }
 
     public void getUserMoney() {
-        InputView.requestPurchaseMoney();
-        //userLottoMoney= Integer.parseInt(Console.readLine());
+        userLottoMoney=InputView.requestPurchaseMoney();
     }
+
 
     public void printLottoNumbers() {
         //service에서 구현하기
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,8 +1,12 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
-    public static void requestPurchaseMoney(){
+    public static Integer requestPurchaseMoney(){
         System.out.println("구입금액을 입력해 주세요.");
+        String inputMoney=Console.readLine();
+        return Integer.parseInt(inputMoney);
     }
     public static void requestLottoNumbers(){
         System.out.println("당첨 번호를 입력해 주세요.");
