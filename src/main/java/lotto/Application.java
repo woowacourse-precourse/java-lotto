9475a96a1ce364c4a4139afdcdc4e4292d4f9bdb
Sungjaeyeong <wodud2587@gmail.com>
2022-11-11 14:36:09 +0900

    feat(lotto): 당첨 번호, 보너스 번호 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,19 +2,38 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
     public static final String INVALID_AMOUNT_ERROR_MESSAGE = "[ERROR] 올바른 금액을 입력해주세요.";
     public static final String NOT_NUMBER_ERROR_MESSAGE = "[ERROR] 숫자만 입력해주세요.";
+    public static final String AMOUNT_ENTER_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String ANSWER_NUMBER_ENTER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_ENTER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static void main(String[] args) {
         Machine machine = new Machine();
         List<Lotto> lottoList = machine.buy(enterAmount());
+        getAnswerNumber();
+        getBonusNumber();
+    }
+
+    private static int getBonusNumber() {
+        System.out.println(BONUS_NUMBER_ENTER_MESSAGE);
+        return Integer.parseInt(Console.readLine());
+    }
+
+    private static List<Integer> getAnswerNumber() {
+        System.out.println(ANSWER_NUMBER_ENTER_MESSAGE);
+        return Arrays.stream(Console.readLine().split(","))
+                .map(el -> Integer.parseInt(el))
+                .collect(Collectors.toList());
     }
 
     private static int enterAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(AMOUNT_ENTER_MESSAGE);
         String money = Console.readLine();
         isValidAmount(money);
         return Integer.parseInt(money);
