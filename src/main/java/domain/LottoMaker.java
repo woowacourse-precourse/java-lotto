f7fa3c5bb37a24218d676f9cc2ee9e298782fc72
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-11 22:53:41 +0900

    feat (LottoMaker) : 오름차순 정렬 기능 추가

--- src/main/java/domain/LottoMaker.java
+++ src/main/java/domain/LottoMaker.java
@@ -2,12 +2,17 @@ package domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Collections;
 import java.util.List;
 
 public class LottoMaker {
 
     public Lotto makeLotto() {
-        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+
+        return (Lotto) numbers;
     }
 
 }
