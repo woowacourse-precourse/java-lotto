92af1bae09ea05fe6e59951a55cbca7fba71f1f2
saewoo1 <wooozooo1123@gmail.com>
2022-11-11 16:46:21 +0900

    test(BonusNumberTest): 보너스 숫자 테스트 코드 작성

--- src/main/java/lotto/BonusNumber.java
+++ src/main/java/lotto/BonusNumber.java
@@ -2,7 +2,6 @@ package lotto;
 
 public class BonusNumber {
   public void isValidBonusNumber(String number) {
-    int num = Integer.parseInt(number);
     if (number.length() > 2) {
       throw new IllegalArgumentException("[ERROR] 하나의 숫자만 입력해 주세요.");
     }
@@ -11,8 +10,14 @@ public class BonusNumber {
         throw new IllegalArgumentException(Constant.NOT_ONLY_DIGIT);
       }
     }
+    int num = Integer.parseInt(number);
     if (num < 1 || num > 45) {
       throw new IllegalArgumentException(Constant.NOT_RANGE_MESSAGE);
     }
+  }
+
+  public static void main(String[] args) {
+    BonusNumber bonusNumber = new BonusNumber();
+    bonusNumber.isValidBonusNumber("242"); // 이럴 경우에 길이 검사가 먼저 일어남.. 고쳐야대~
   }
 }
