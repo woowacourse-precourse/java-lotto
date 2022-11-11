43057c07efa73fbd94edf343419515d9c9ac658c
Amyhds <dasomhan524@gmail.com>
2022-11-11 16:02:35 +0900

    feat(lottomessage): 문자열 추가 구매 예외 처리

--- src/main/java/lotto/LottoMessage.java
+++ src/main/java/lotto/LottoMessage.java
@@ -5,6 +5,7 @@ public enum LottoMessage {
     RANGE_ERROR_MESSAGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     DUPLICATE_ERROR_MESSAGE("[ERROR] 로또 번호는 중복되어선 안됩니다."),
     INPUT_ERROR_MESSAGE("[ERROR] 로또 번호 입력 시, 숫자와 쉼표만 입력해야 합니다."),
+    PRICE_ERROR_MESSAGE("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다."),
     START_MESSAGE("구입금액을 입력해 주세요."),
     PURCHASE_MESSAGE("개를 구매했습니다."),
     LUCKY_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
