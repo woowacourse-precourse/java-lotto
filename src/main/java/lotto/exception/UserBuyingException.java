b69d912b12dc5a4e82d6f55e80e93f399b9f5cdc
sosow0212 <sosow0212@naver.com>
2022-11-11 18:32:47 +0900

    feat : 구매 수량 예외처리 추가

--- src/main/java/lotto/exception/UserBuyingException.java
+++ src/main/java/lotto/exception/UserBuyingException.java
@@ -3,9 +3,30 @@ package lotto.exception;
 public class UserBuyingException {
     private final static int LOTTO_PRICE = 1000;
 
+    public void validateBuyingException(String buyingPrice) {
+        validIsNumber(buyingPrice);
+        validPrice(buyingPrice);
+    }
+
     public void validBuyingException(int buyingMoney) {
         if (buyingMoney % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException("구매는 1,000원 단위로 가능합니다.");
+            throw new IllegalArgumentException("[ERROR] 구매는 1,000원 단위로 가능합니다.");
+        }
+    }
+
+    public void validPrice(String pay) {
+        int buyingPrice = Integer.parseInt(pay);
+        if (buyingPrice % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매는 1,000원 단위로 가능합니다.");
+        }
+    }
+
+    static boolean validIsNumber(String s) { //정수 판별 함수
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
         }
     }
 }
