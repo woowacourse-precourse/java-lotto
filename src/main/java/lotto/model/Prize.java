f45411c1364d053f2dfd82a6bcb5fda2dee041ac
dhkim <akdlzmf1123@gmail.com>
2022-11-11 20:24:32 +0900

    feat(Prize): 당첨 번호와 일치하는 번호 갯수와 보너스 번호 포함 여부로 몇 등 당첨인지 판별하는 기능 추가

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -1,4 +1,28 @@
 package lotto.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Prize {
+    FIRST,
+    SECOND,
+    THIRD,
+    FOURTH,
+    FIFTH,
+    NO_PRIZE;
+
+    private static final Map<CompareResult, Prize> compareResultToPrize = new HashMap<>();
+
+    static {
+        compareResultToPrize.put(new CompareResult(6, false), FIRST);
+        compareResultToPrize.put(new CompareResult(5, true), SECOND);
+        compareResultToPrize.put(new CompareResult(5, false), THIRD);
+        compareResultToPrize.put(new CompareResult(4, true), FOURTH);
+        compareResultToPrize.put(new CompareResult(4, false), FOURTH);
+        compareResultToPrize.put(new CompareResult(3, true), FIFTH);
+        compareResultToPrize.put(new CompareResult(3, false), FIFTH);
+    }
+    public static Prize toPrize(CompareResult lottoResult) {
+        return compareResultToPrize.getOrDefault(lottoResult, NO_PRIZE);
+    }
 }
--- src/test/java/lotto/model/PrizeTest.java
+++ src/test/java/lotto/model/PrizeTest.java
@@ -1,10 +1,30 @@
 package lotto.model;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PrizeTest {
+    @ParameterizedTest(name = "로또 {1} Place 당첨")
+    @MethodSource("lottoResultAndPrize")
+    void CompareResult가_몇등_당첨인지_판단하는_기능(CompareResult lottoResult, Prize prize) {
+        assertThat(Prize.toPrize(lottoResult)).isEqualTo(prize);
+    }
 
+    static Stream<Arguments> lottoResultAndPrize() {
+        return Stream.of(
+            Arguments.of(new CompareResult(6, false), Prize.FIRST)
+                , Arguments.of(new CompareResult(5, true), Prize.SECOND)
+                , Arguments.of(new CompareResult(5, false), Prize.THIRD)
+                , Arguments.of(new CompareResult(4, true), Prize.FOURTH)
+                , Arguments.of(new CompareResult(4, false), Prize.FOURTH)
+                , Arguments.of(new CompareResult(3, true), Prize.FIFTH)
+                , Arguments.of(new CompareResult(3, false), Prize.FIFTH)
+                , Arguments.of(new CompareResult(2, true), Prize.NO_PRIZE)
+        );
+    }
 }
