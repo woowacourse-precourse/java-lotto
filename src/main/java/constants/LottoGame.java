537625cf38f2732bfba15a6610c0e5d60fdf0447
jindaram-stu <jinsb1999@naver.com>
2022-11-11 23:49:47 +0900

    feat: 로또 생성 시 예외처리 추가
    입력받은 구입금액이 1000원 단위로 떨어지지 않으면 예외 발생

---
+++ src/main/java/constants/LottoGame.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class LottoGame {
+
+    public static final int LOTTO_PRICE = 1000;
+
+}
--- src/main/java/lotto/domain/LottoAmount.java
+++ src/main/java/lotto/domain/LottoAmount.java
@@ -7,7 +7,14 @@ public class LottoAmount {
     private final int amount;
 
     public LottoAmount(int amount) {
+        validate(amount);
         this.amount = amount;
+    }
+
+    public void validate(int amount) {
+        if (amount % LottoGame.LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public int getLottoCount() {
