a40d7f5e988050f457a32328559fe076de6ee8a3
yeonhee <ydol2@naver.com>
2022-11-12 02:20:08 +0900

    chore(LottoController) : 당첨 번호와 보너스 번호 input 묶기

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,9 +3,11 @@ package lotto.controller;
 import lotto.domain.Lotto;
 import lotto.domain.LottoPrice;
 import lotto.domain.LottoTicket;
+import lotto.domain.WinningNumbers;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static lotto.constant.UtilConstant.LOTTO_OPEN;
@@ -15,6 +17,8 @@ import static lotto.constant.UtilConstant.COMMA_AND_SPACE;
 public class LottoController {
     private static final InputView inputView = new InputView();
     private static final OutputView outputView = new OutputView();
+    private List<Lotto> lottoTicketList = new ArrayList<>();
+    private WinningNumbers winningNumbers;
 
     public static LottoController create(){
         return new LottoController();
@@ -22,11 +26,12 @@ public class LottoController {
     public void run(){
         LottoPrice lottoPrice = buyLottoTicket();
         printTicketCount(lottoPrice);
-        for(int i=0 ; i<lottoPrice.lottoTicketCount() ;i++)
-            printLottoNumbers(LottoTicket.generate());
-        inputLottoNumbers();
-        inputBonusNumber();
-        printStatistics();
+        for(int i=0 ; i<lottoPrice.lottoTicketCount() ;i++){
+            lottoTicketList.add(LottoTicket.generate());
+            printLottoNumbers(lottoTicketList.get(i));
+        }
+        winningNumbers = inputLottoNumbers();
+        printStatistics(lottoTicketList,winningNumbers);
         printEarning();
     }
     private LottoPrice buyLottoTicket(){
@@ -46,15 +51,11 @@ public class LottoController {
         System.out.println(result);
     }
 
-    private List<Integer> inputLottoNumbers(){
-        return inputView.inputLottoNumbers();
+    private WinningNumbers inputLottoNumbers(){
+        return new WinningNumbers(inputView.inputLottoNumbers(),inputView.inputBonusNumber());
     }
 
-    private int inputBonusNumber(){
-        return inputView.inputBonusNumber();
-    }
-
-    private void printStatistics(){
+    private void printStatistics(List<Lotto> lottoTicketList,WinningNumbers winningNumbers){
         outputView.printStatistics(1,0,0,0,0);
     }
 
