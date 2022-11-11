b70297aaab1abdcf77981ad6f2542a321de739dd
ehs2803 <hr2803@naver.com>
2022-11-11 11:07:49 +0900

    feat : 구입금액 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 구입금액 입력
+        System.out.println("구입금액을 입력해 주세요.");
+        int money = Integer.parseInt(Console.readLine());
+
+        if(money%1000!=0){
+            throw new IllegalArgumentException("[ERROR] 로또 구매 금액은 1000원 단위로 입력해야 합니다.");
+        }
+
+        int lottoCnt = money/1000;
+
+
     }
 }
