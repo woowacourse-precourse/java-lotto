26c1843d5ce20f916256e17e2c2533592e2a9aa8
chanwonlee <clw8679@naver.com>
2022-11-12 00:12:44 +0900

    feat: InputWinningNumbers 구현

---
+++ src/main/java/lotto/input/InputWinningNumbers.java
@@ -0,0 +1,35 @@
+package lotto.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputWinningNumbers {
+    public List<Integer> input() {
+        String input = Console.readLine();
+        validate(input);
+
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String number : input.split(",")) {
+            winningNumbers.add(Integer.parseInt(number));
+        }
+
+        return winningNumbers;
+    }
+
+    private void validate(String input) {
+        String[] numbers = input.split(",");
+        for (String number : numbers) {
+            validateInteger(number);
+        }
+    }
+
+    private void validateInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 쉼표(,)를 기준으로 숫자를 입력해주세요. (예:1,2,3,4,5,6)");
+        }
+    }
+}
