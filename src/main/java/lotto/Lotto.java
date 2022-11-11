680b9a5af07fabff6680c390299491c52f8b3358
Donghyun <flaehan@gmail.com>
2022-11-11 10:52:31 +0900

    type : feat ✨로또 구입 한 만큼 생성하기
    body :
    
    footer :

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,10 +4,14 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
-    private List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45 , 6);
+    private static List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45 , 6);
+    private static List<List> myLottoNum = new ArrayList<>();
+    private static int howManyLotto;
+
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
@@ -17,18 +21,27 @@ public class Lotto {
     public int inputMoney(){
         System.out.println("구입금액을 입력해 주세요.");
         int inputMoney = Integer.parseInt(Console.readLine());
-
+        if(inputMoney % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 로또 구이 금액은 1,000원 단위입니다.");
+        }
         return inputMoney;
     }
 
     // 로또 구입 갯수
     public int theNumberOfLotto(){
-        int howManyLotto = inputMoney() / 1000;
+        howManyLotto = inputMoney() / 1000;
 
         return howManyLotto;
     }
 
+    // 로또 생성
+    public List<List> myLottoNumber(){
+        for(int i = 0; i < howManyLotto; i++){
+        myLottoNum.add(numbers);
+        }
 
+        return myLottoNum;
+    }
 
 
 
