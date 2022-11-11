16fe939d81d1d02485ed434ec0205e76817d3592
EUNJEONGMUN <munej26@gmail.com>
2022-11-11 15:03:26 +0900

    refactor: 계산 관련 상수 Constant 클래스로 이동

--- src/main/java/constant/Constant.java
+++ src/main/java/constant/Constant.java
@@ -5,5 +5,7 @@ public class Constant {
     public static final int LOTTO_SIZE = 6;
     public static final int LOTTO_NUMBER_MIN = 1;
     public static final int LOTTO_NUMBER_MAX = 45;
+    public static final int MONEY_UNIT = 1000;
+    public static final int REMAINDER_ZERO = 0;
     public static final String EMPTY = "";
 }
