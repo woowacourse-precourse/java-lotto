322b653731e54cd4996a41bac25f2017680b4281
JunH <tjdtls690@gmail.com>
2022-11-12 00:30:46 +0900

    :white_check_mark: test(Study : StringTest) : 학습 테스트의 로직을 containsExactly 메서드를 이용해서 로직 개선

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -12,14 +12,12 @@ class StringTest {
     @Test
     @DisplayName("\"1,2\"을 , 로 split 했을 때 1과 2로 잘 분리되는지 확인")
     void split() {
-        final List<String> strings = Arrays.asList("1,2".split(","));
-        assertThat(strings).isEqualTo(List.of("1", "2"));
+        assertThat("1,2".split(",")).containsExactly("1", "2");
     }
     
     @Test
     @DisplayName("\"1\"을 , 로 split 했을 때 1만을 포함하는 배열이 반환되는지 확인")
     void one_split() {
-        final List<String> strings = Arrays.asList("1".split(","));
-        assertThat(strings).isEqualTo(List.of("1"));
+        assertThat("1".split(",")).containsExactly("1");
     }
 }
