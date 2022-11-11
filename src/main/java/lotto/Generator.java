ceee91e4f8c4282533f622b0a47afed46a2615fc
wxxhyeong <wxxhyeong@gmail.com>
2022-11-11 22:11:24 +0900

    test: 로또 개수 구하기

--- src/main/java/lotto/Generator.java
+++ src/main/java/lotto/Generator.java
@@ -7,32 +7,27 @@ import java.util.regex.Pattern;
 
 public class Generator {
 
-    public static Integer purchasePrice() {
+    public Integer purchasePrice() {
         String userMoney;
         System.out.println("구입 금액을 입력하세요 : ");
         userMoney = Console.readLine();
 
-        return Integer.parseInt(userMoney);
-    }
-
-    public static int calculateLottoNum(int userMoney) {
-
-        if (userMoney % 1000 == 0) {
-            return userMoney / 1000;
+        if (checkValidation(userMoney)) {
+            return Integer.parseInt(userMoney);
         }
         return 0;
     }
 
-    public static boolean checkValidation(String userMoney) {
+    public int calculateLottoNum(int userMoney) {
+        return userMoney / 1000;
+    }
+
+    public boolean checkValidation(String userMoney) {
         String regExp = "^[1-9]*0{3}$";
         if (Pattern.matches(regExp, userMoney)) {
             return true;
         }
         throw new IllegalArgumentException(Message.ERROR + Message.ERROR_MESSAGE_WON);
     }
-
-
-
-
 
 }
--- src/test/java/lotto/GeneratorTest.java
+++ src/test/java/lotto/GeneratorTest.java
@@ -33,19 +33,31 @@ class GeneratorTest {
         InputStream in = getPlayerInput(input);
         System.setIn(in);
 
-        assertThat(Generator.purchasePrice()).isEqualTo(6000);
+        assertThat(generator.purchasePrice()).isEqualTo(6000);
     }
 
     @Test
     @DisplayName("1,000원 단위 체크")
     void correctInput() {
-        assertThat(Generator.checkValidation("22000")).isEqualTo(true);
+        assertThat(generator.checkValidation("22000")).isEqualTo(true);
     }
 
     @ParameterizedTest
     @DisplayName("1,000원 단위가 아닐 시 에러 발생")
     @ValueSource(strings = {"12345", "가나다", "", "1200"})
     void incorrectInput(String input) {
-        assertThrows(IllegalArgumentException.class, () -> Generator.checkValidation(input));
+        assertThrows(IllegalArgumentException.class, () -> generator.checkValidation(input));
+    }
+
+    @ParameterizedTest
+    @DisplayName("구입 금액에 따른 로또 개수 구하기")
+    @ValueSource(strings = {"2000", "19000"})
+    void countLottoNum(String input) {
+        int n;
+        InputStream in = getPlayerInput(input);
+        System.setIn(in);
+
+        n = generator.calculateLottoNum(generator.purchasePrice());
+        Assertions.assertThat(n).isEqualTo(Integer.parseInt(input)/1000);
     }
 }
