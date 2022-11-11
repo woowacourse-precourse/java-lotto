fca90174f56cf8461f2dccb8294825d7efa806aa
minju412 <ln8847@ajou.ac.kr>
2022-11-11 22:17:48 +0900

    fix: bonus number exception handling
    - exception occurs
      - if the number is not in the range of 1 to 45
      - if the number contains characters

--- src/main/java/lotto/constant/SystemMessage.java
+++ src/main/java/lotto/constant/SystemMessage.java
@@ -21,4 +21,6 @@ public final class SystemMessage {
     public static final String WINNING_NUMBERS_CONFLICT_ERROR = "당첨 번호에 중복이 포함될 수 없습니다";
 
     public static final String BONUS_NUMBER_CONFLICT_ERROR = "보너스 번호는 당첨 번호와 중복될 수 없습니다";
+    public static final String BONUS_NUMBER_RANGE_ERROR = "보너스 번호는 1부터 45까지의 수여야 합니다";
+    public static final String BONUS_NUMBER_TYPE_ERROR = "보너스 번호에 문자가 포함될 수 없습니다";
 }
--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,8 +1,15 @@
 package lotto.domain;
 
 import static lotto.constant.SystemMessage.BONUS_NUMBER_CONFLICT_ERROR;
+import static lotto.constant.SystemMessage.BONUS_NUMBER_RANGE_ERROR;
+import static lotto.constant.SystemMessage.BONUS_NUMBER_TYPE_ERROR;
+import static lotto.constant.SystemMessage.WINNING_NUMBERS_RANGE_ERROR;
+import static lotto.constant.SystemValue.LOTTERY_NUMBERS_SIZE;
+import static lotto.constant.SystemValue.MAXIMUM_LOTTERY_NUMBER;
+import static lotto.constant.SystemValue.MINIMUM_LOTTERY_NUMBER;
 
 import java.util.List;
+import lotto.util.TypeConverter;
 import lotto.view.Output;
 
 public class BonusNumber {
@@ -17,13 +24,30 @@ public class BonusNumber {
     }
 
     private void validate(String bonusNumber) {
-        if (!isConflictWithWinningsValid(bonusNumber)) {
+        if (!isTypeValid(bonusNumber) || !isRangeValid(bonusNumber) || !isConflictWithWinningsValid(bonusNumber)) {
             throw new IllegalArgumentException();
         }
     }
 
     public int getNumber() {
         return bonusNumber;
+    }
+
+    public static boolean isTypeValid(String bonusNumber) {
+        boolean isType = bonusNumber.chars().allMatch(Character::isDigit);
+        if (!isType) {
+            Output.printError(BONUS_NUMBER_TYPE_ERROR);
+        }
+        return isType;
+    }
+
+    public static boolean isRangeValid(String bonusNumber) {
+        int number = Integer.parseInt(bonusNumber);
+        boolean isRange = (number >= MINIMUM_LOTTERY_NUMBER && number <= MAXIMUM_LOTTERY_NUMBER);
+        if (!isRange) {
+            Output.printError(BONUS_NUMBER_RANGE_ERROR);
+        }
+        return isRange;
     }
 
     public boolean isConflictWithWinningsValid(String bonusNumber) {
