37d590f3e8e810143f9fe174f35145fd603e58e5
saewoo1 <wooozooo1123@gmail.com>
2022-11-11 21:46:43 +0900

    refactor(RandomNumberCreator): Map -> List<Lotto> 변환 Lotto class 사용을 위해 map을 만들어 하나씩 넣고 반환하는 방식에서 integer List에 수를 넣는 방식 사용

--- src/main/java/lotto/RandomNumberCreater.java
+++ src/main/java/lotto/RandomNumberCreater.java
@@ -2,23 +2,21 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class RandomNumberCreater {
-  Money money = new Money();
-  UserNumberScan scanner = new UserNumberScan();
-  public Map<Integer, List<Integer>> generateLottoNumber() {
-    int count = money.countLotto(scanner.requestValue());
+  public static List<Lotto> generateLottoNumber(int count) {
 
-    Map<Integer, List<Integer>> lottoMap = new HashMap<>();
+    List<Lotto> lottoList = new ArrayList<>();
     for (int i = 0; i < count; i++)
     {
       List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-      new Lotto(lottoNumbers); // validate 검사
-      lottoMap.put(i, lottoNumbers);
+      Lotto lotto = new Lotto(lottoNumbers); // validate 검사
+      lottoList.add(lotto);
     }
-    return lottoMap;
+    return lottoList;
   }
 }
