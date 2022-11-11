ce09981870a909f33994ef3eb16cf2452b749318
wugawuga <dnr3655@naver.com>
2022-11-11 14:46:39 +0900

    test: 당첨 번호 입력값 구분자 확인, 문자 확인

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -35,6 +36,31 @@ class InputTest {
         System.setIn(in);
 
         assertThatThrownBy(Input::purchaseAmount)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("당첨 번호 문자 입력했을 때 예외")
+    @ValueSource(strings = {
+            "1,2,a,3,4,5",
+            "a,b,c,d,e,f",
+            "1,2.3,4,5,6"
+    })
+    void validateNumberOfWinningNumber(String input) {
+        InputStream in = generateInput(input);
+        System.setIn(in);
+
+        assertThatThrownBy(Input::winningNumber)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("당첨 번호 문자 입력값에 구분자 ',' 가 없을 때 예외")
+    void validateSeparatorOfWinningNumber() {
+        InputStream in = generateInput("1.2.3.4.5.6");
+        System.setIn(in);
+
+        assertThatThrownBy(Input::winningNumber)
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
