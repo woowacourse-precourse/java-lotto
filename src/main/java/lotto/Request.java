f8ab0cb3e5eac2ed2159beef6390f2927df90f5a
jthugg <jthug@naver.com>
2022-11-12 01:27:51 +0900

    Feat: 구매 할 로또 금액에 대한 유효성 검사 기능 추가

--- src/main/java/lotto/Request.java
+++ src/main/java/lotto/Request.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class Request {
 
@@ -12,4 +13,29 @@ public class Request {
 		userInput = Console.readLine();
 	}
 
+	public void validateByStatus(String status) {
+		if(status.equals("request money")) {
+			validateMoney();
+		}
+
+		if(status.equals("request winning numbers")) {
+			validateWinningNumbers();
+		}
+
+		if(status.equals("request bonus number")) {
+			validateBonusNumber();
+		}
+	}
+
+	public void validateMoney() {
+		if(!Pattern.matches("^1?[0-9]?[0-9]+000$", userInput)) {
+			throw new IllegalArgumentException();
+		}
+
+		int input = Integer.parseInt(userInput);
+
+		if(input > 100000 || input == 0) {
+			throw new IllegalArgumentException();
+		}
+	}
 }
