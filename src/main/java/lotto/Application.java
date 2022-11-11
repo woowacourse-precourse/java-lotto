a8a4e2d9322a41184c5a6ddb6bd5deff5770a437
gyumin159 <gyumin159@naver.com>
2022-11-11 17:25:02 +0900

    feat : 당첨번호 입력시 6개의 숫자가 중복없이 왔는지 확인하는 메소드

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -41,7 +41,6 @@ public class Application {
      * 숫자와 콤마로만 이루어져 있는지 확인한다.
       * @param winningNumbers 로또 1등 당첨 번호(보너스 번호 제외)
      */
-
     private static void checkDigitAndComma(String winningNumbers) {
         boolean isRightLottoNumbers = Pattern.matches("^[0-9,]*$", winningNumbers);
         if (!isRightLottoNumbers){
@@ -49,7 +48,19 @@ public class Application {
         }
     }
 
-
+    /**
+     * 6개의 숫자가 중복없이 들어왔는지 확인한다.
+     * @param winningNumbers 로또 1등 당첨 번호(보너스 번호 제외)
+     */
+    private static void checkOverlapAndSize(String winningNumbers) {
+        List<String> temp = Arrays.asList(winningNumbers.split(","));
+        boolean isSizeSix = temp.size() == 6;
+        Set<String> delOverlap = new HashSet<>(temp);
+        boolean isNoOverlap = delOverlap.size() == 6;
+        if (!(isSizeSix && isNoOverlap)){
+            throw new IllegalArgumentException();
+        }
+    }
 
     public static void main(String[] args) {
         String a = Console.readLine();
