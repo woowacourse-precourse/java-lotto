86ec4c8fab3a14b45441633fe34aef55edd8ffd6
MuSeung <peter0757@nvaer.com>
2022-11-11 17:53:43 +0900

    feat: LottoWin에 3,4,5등을 처리할 수 있는 기능 추가
    LottoPrizes
        - 3,4,5등을 저장한 객체 추가

--- src/main/java/lotto/LottoPrizes.java
+++ src/main/java/lotto/LottoPrizes.java
@@ -3,6 +3,9 @@ package lotto;
 public enum LottoPrizes {
     FIRST(1, "1st"),
     SECOND(2, "2nd"),
+    THIRD(3, "3rd"),
+    FOURTH(4,"4th"),
+    FIFTH(5,"5th"),
     NOTHING(0, "nothing");
     private final int indexNumber;
     private final String keyName;
@@ -16,6 +19,15 @@ public enum LottoPrizes {
         }
         if (condition == 5 && hasBonus) {
             return SECOND;
+        }
+        if (condition == 5) {
+            return THIRD;
+        }
+        if (condition == 4) {
+            return FOURTH;
+        }
+        if (condition == 3) {
+            return FIFTH;
         }
         return NOTHING;
     }
