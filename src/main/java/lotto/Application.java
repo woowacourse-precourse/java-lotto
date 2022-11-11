81293096aa841c822c48206ac7302a6e49165e59
TaeMin <xoals3094@naver.com>
2022-11-12 08:33:26 +0900

    refactor(Application) : MVC 패턴으로 수정하며 main 메소드의 코드를 대폭 수정합니다
    main 메소드를 사용자라고 생각하고 코드를 작성합니다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,60 +1,21 @@
 package lotto;
 
-import lotto.UI.View;
-import lotto.domain.Calculator;
-import lotto.domain.Judgment;
-import lotto.domain.LottoGenerator;
-import lotto.domain.Prize;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import lotto.Controller.MainController;
 
 public class Application {
     public static void main(String[] args) {
-        View view = new View();
-        Calculator calculator = new Calculator();
+        MainController mainController = new MainController();
+        try {
+            mainController.inputCash();
+            mainController.createMyLottos();
 
-        // 입력 파트
-        int investmentCash = view.getInvestmentCash();
-        int myLottosNumber = calculator.countPublicationLotto(investmentCash);
-        List<Lotto> myLottos = new ArrayList<>();
-        LottoGenerator lottoGenerator = new LottoGenerator();
-        for (int i = 0; i < myLottosNumber; i++) {
-            List<Integer> lottoNumber = lottoGenerator.createLottoNumber();
-            myLottos.add(new Lotto(lottoNumber));
+            mainController.inputWinningNumber();
+            mainController.inputBonusNumber();
+
+            mainController.calculateWinning();
+        }catch (IllegalArgumentException e){
+            System.out.println("[ERROR] " + e.getMessage());
         }
-
-        view.printMyLottoInfo(myLottos);
-
-        Lotto winningLotto = new Lotto(view.getWinningNumber());
-        int bonusNumber = view.getBonusNumber();
-
-        // 계산 파트
-        HashMap<Prize, Integer> state = new HashMap<>();
-        state.put(Prize.FIRST, 0);
-        state.put(Prize.SECOND, 0);
-        state.put(Prize.THIRD, 0);
-        state.put(Prize.FOURTH, 0);
-        state.put(Prize.FIFTH, 0);
-
-        int totalPrize = 0;
-        for (Lotto lotto : myLottos) {
-            Judgment judgment = new Judgment();
-            int matchNumber = judgment.compare(winningLotto.getNumbers(), lotto.getNumbers());
-            boolean bonus = judgment.hasBonusNumber(winningLotto.getNumbers(), bonusNumber);
-            Prize prize = calculator.givePrize(matchNumber, bonus);
-
-            if (prize != Prize.LAST) {
-                int point = state.get(prize);
-                state.replace(prize, point + 1);
-
-                totalPrize += prize.getPrize();
-            }
-        }
-
-        double profit = calculator.getProfit(totalPrize, investmentCash);
-        //결과
-        view.printResult(state, profit);
     }
 }
