d33ff3fa6a4a7ad360f13a3de21054a50b6749f8
Seokhyun Lee <lseokhyun99@gmail.com>
2022-11-11 17:39:53 +0900

    feat: 로또 발행 메소드 구현
    로또 발행 메소드 issueLotto() 를 구현하였다.
    
    앞에서 입력된 금액을 토대로
    계산한 lottoCount 만큼
    로또를 발행한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -8,12 +14,18 @@ public class Application {
         runLottoApp();
     }
     public static void runLottoApp(){
+
         int lottoCount = howMuchLottoBuy();
+
+        List<Lotto> lotto = issueLotto(lottoCount);
+
     }
     public static int howMuchLottoBuy(){
         System.out.println("구입금액을 입력해 주세요.");
         String money = readLine();
+
         moneyErrorCheck(money);
+
         return Integer.parseInt(money)/1000;
     }
     public static void moneyErrorCheck(String money){
@@ -24,5 +36,18 @@ public class Application {
         }
         if((Integer.parseInt(money) % 1000) != 0)
             throw new IllegalArgumentException("[ERROR] 구입금액은 1000으로 나누어 떨어져야합니다.");
+    }
+    public static List<Lotto> issueLotto(int lottoCount){
+        System.out.println("\n"+lottoCount+"개를 구매했습니다.");
+        List<Lotto> lotto = new ArrayList<>();
+
+        for(int i = 0 ; i < lottoCount ; i++){
+            List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1,45,6);
+            lottoNumber.sort(Comparator.naturalOrder());
+            System.out.println(lottoNumber);
+            lotto.add(new Lotto(lottoNumber));
+        }
+
+        return lotto;
     }
 }
