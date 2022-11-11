0eb6cf95635cc5f20eab005484cc7fd07e14aef8
Soomin Lim <augustth511@gmail.com>
2022-11-11 15:03:11 +0900

    feat(Player): 구입 금액만큼 로또 발행 기능 구현

---
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoNumberGenerator {
+
+    public static List<Integer> generate() {
+        List<Integer> lotto = new ArrayList<>();
+
+        return lotto;
+    }
+}
---
+++ src/main/java/lotto/domain/Player.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import lotto.Lotto;
+
+import java.util.List;
+
+public class Player {
+    private List<Lotto> lottos;
+
+    public void issueLottos(int money) {
+        int number = money /1000;
+
+        for (int i = 0; i < number; i++) {
+            List<Integer> lottoNumbers = LottoNumberGenerator.generate();
+            lottos.add(new Lotto(lottoNumbers));
+        }
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+}
