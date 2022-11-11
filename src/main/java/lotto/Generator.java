a832d1024a5dfbbb97b1bd628adf50107c4bc692
sysliar <sysliar@gmail.com>
2022-11-11 19:08:32 +0900

    feat(Generator): add lotto generator

--- src/main/java/lotto/Generator.java
+++ src/main/java/lotto/Generator.java
@@ -1,11 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Generator {
-    public List<Lotto> generate() {
-        return new ArrayList<Lotto>();
+    public List<Lotto> generate(Integer ticketCount) {
+        List<Lotto> lottoTickets = new ArrayList<Lotto>();
+        for (int i = 0; i < ticketCount; i++) {
+            lottoTickets.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+            return lottoTickets;
     }
 
     private void printGeneratedLotto(List<Lotto> generatedLotto) {
