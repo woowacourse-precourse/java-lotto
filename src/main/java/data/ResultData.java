2786f637065376327376566f8c12be3a2fb68dc6
cuttlefish55 <funade7@gmail.com>
2022-11-11 18:18:58 +0900

    feat: Add constant data for result output

---
+++ src/main/java/data/ResultData.java
@@ -0,0 +1,17 @@
+package data;
+
+public enum ResultData {
+    FIFTH("3개 일치 ("),
+    FOURTH("4개 일치 ("),
+    THIRD("5개 일치 ("),
+    SECOND("5개 일치, 보너스 볼 일치 ("),
+    FIRST("6개 일치 ("),
+    CLOSE("원 ) - "),
+    PROFIT_RATE("총 수익률은 "),
+    PROFIT_RATE_CLOSE("%입니다.");
+
+    private String resultData;
+    ResultData(String resultData){
+        this.resultData = resultData;
+    }
+}
