3f0937950ed2283fe51328e07e76ecfc41eead7a
ym2629 <dydals2629@naver.com>
2022-11-11 12:18:50 +0900

    fix: print result of rank method

--- src/main/java/lotto/UserInterface.java
+++ src/main/java/lotto/UserInterface.java
@@ -3,7 +3,9 @@ package lotto;
 import camp.nextstep.edu.missionutils.*;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class UserInterface {
@@ -31,14 +33,14 @@ public class UserInterface {
         return winningNumbers;
     }
 
-    void printResultRank(int first, int second, int thirth, int fourth, int fifth) {
+    void printResultRank(Map<Rank, Integer> rankCount) {
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - "+fifth+"개");
-        System.out.println("4개 일치 (50,000원) - "+fourth+"개");
-        System.out.println("5개 일치 (1,500,000원) - "+thirth+"개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+second+"개");
-        System.out.println("6개 일치 (2,000,000,000원) - "+first+"개");
+        Arrays.stream(Rank.values())
+                .sorted(Comparator.reverseOrder())
+                .filter(rank -> rank != Rank.NOTHING)
+                .map(rank -> rank.getNumberOfRight() + "개 일치" + (rank == Rank.SECOND ? ", 보너스 볼 일치" : "") + " (" + rank.getFormatPrizeMoney() + "원) - " + rankCount.get(rank) + "개")
+                .forEach(System.out::println);
     }
 
     void printRateReturn(double rateOfReturn){
