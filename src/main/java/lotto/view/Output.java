b902bc91f48d626015f204d71d6d821b2be79823
hyun-seung <ardimento22@naver.com>
2022-11-12 04:48:16 +0900

    [FEAT] (VIEW) 구매 금액에 해당하는 만큼 로또를 발행한다.

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,16 +1,29 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.domain.Lotto;
+
 public class Output {
 
     public void printGetMoney() {
         System.out.println("구매금액을 입력해주세요.");
     }
 
+    public void printLottos(List<Lotto> lottos) {
+        System.out.println();
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.toString());
+        }
+    }
+
     public void printGetWinningNumbers() {
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
     public void pringGetWinningBonusNumber() {
+        System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
