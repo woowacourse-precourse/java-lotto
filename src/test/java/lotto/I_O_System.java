c954a2d983496eb1ec6316665273186ddbf2453f
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-11 15:41:24 +0900

    Feat : "추가 Enter_Price 함수 및 로또 금액 변수"
    추가 I_O_System.java 를 만들어서 복권 사는 금액을 입력 받는 함수를 추가하였다. 로또 금액인 1000원을 정적 변수로 만들었다.

---
+++ src/test/java/lotto/I_O_System.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class I_O_System {
+    static final int lottey_price = 1000;
+
+    private void Enter_Price()
+    {
+        String Enter_Price = Console.readLine();
+
+    }
+}
