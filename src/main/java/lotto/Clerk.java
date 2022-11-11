7992c27c6bf5ae413434ca814dffb9cca06b3bd4
skck0226 <skck0226@ajou.ac.kr>
2022-11-11 14:56:36 +0900

    [feat] 구입가격만큼 로또 발행

--- src/main/java/lotto/Clerk.java
+++ src/main/java/lotto/Clerk.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static lotto.Enum.ConstantNumber.*;
@@ -9,8 +10,25 @@ import static lotto.Enum.Error.*;
 public class Clerk {
 
     public static List<Lotto> sellLotto(String purchaseAmount) {
+
         validatePurchaseAmount(purchaseAmount);
-        return List.of(new Lotto());
+
+        int lotto_amount = getLottoAmount(purchaseAmount);
+
+        return makeLottoList(lotto_amount);
+    }
+
+    private static List<Lotto> makeLottoList(int lotto_amount) {
+        List<Lotto> lottoList = new ArrayList<>();
+
+        for (int i = 0; i < lotto_amount; i++) {
+            lottoList.add(new Lotto());
+        }
+        return lottoList;
+    }
+
+    private static int getLottoAmount(String purchaseAmount) {
+        return Integer.parseInt(purchaseAmount) % THOUSAND.getValue();
     }
 
     private static void validatePurchaseAmount(String purchaseAmount) {
@@ -24,7 +42,7 @@ public class Clerk {
     }
 
     private static void validatePurchaseOnlyNumber(String purchaseAmount) {
-        if (!purchaseAmount.matches(REGEX_ONLY_NUMBER.getValue())) {
+        if(!purchaseAmount.matches(REGEX_ONLY_NUMBER.getValue())){
             throw new IllegalArgumentException(PURCHASE_ONLY_NUMBER_ERROR.getMessage());
         }
     }
