f4aa7239e01da0d5487a250e7cce4039bc079c3e
KimSangHyeok <tkdgur0906@naver.com>
2022-11-11 20:47:44 +0900

    Feat(lotto): 당첨내역 관리 기능 추가

--- src/main/java/lotto/WinningCount.java
+++ src/main/java/lotto/WinningCount.java
@@ -1,2 +1,33 @@
-package lotto;public class WinningCount {
+package lotto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WinningCount {
+    private Map<String, Integer> count;
+
+    public WinningCount() {
+        count = new HashMap<>();
+        count.put("3", 0);
+        count.put("4", 0);
+        count.put("5", 0);
+        count.put("6", 0);
+        count.put("bonus", 0);
+    }
+
+    public Map<String, Integer> getCount() {
+        return count;
+    }
+
+    public void addWinningResult(String str, boolean bonusCheck) {
+        if (bonusCheck && str == "5") {
+            count.put("bonus", count.get("bonus"));
+            return;
+        }
+        if (count.containsKey(str)) {
+            count.put(str, count.get(str) + 1);
+        }
+    }
+
+
 }
