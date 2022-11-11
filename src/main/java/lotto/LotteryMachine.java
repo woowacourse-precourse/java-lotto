8fb8f5de78d3415d5003e84b92e278e003faa508
lee2963 <sodel2963@naver.com>
2022-11-11 20:12:23 +0900

    Feat: 보너스 번호 뽑는 기능 추가

--- src/main/java/lotto/LotteryMachine.java
+++ src/main/java/lotto/LotteryMachine.java
@@ -6,7 +6,22 @@ import java.util.List;
 
 public class LotteryMachine {
 
-    public static List<Integer> makeLottoNum() {
+    public List<Integer> makeLottoNum() {
+
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
+
+    public int makeBonusNum(List<Integer> lottoNum) {
+
+
+        List<Integer> bonusNum = Randoms.pickUniqueNumbersInRange(1, 45, 1);
+        if (lottoNum.contains(bonusNum.get(0))) {
+            throw new IllegalArgumentException(Error.BONUS_DUPLICATE_LOTTO.getErrorMsg());
+        }
+
+        return bonusNum.get(0);
+    }
+
+
+
 }
