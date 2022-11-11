c814498ae651179106fb9496b16ebe8c645f84ab
sangwon <swchoi1997@naver.com>
2022-11-11 10:35:47 +0900

    feat: 로또 결과 종합하여 결과 리턴하는 로직 추가, 예외추가
    로또 결과를 List에 종합하여 결과 리턴, 구매한 로또개수보다 결과가 많을 때 예외처리

--- src/main/java/lotto/domain/model/UserLotto.java
+++ src/main/java/lotto/domain/model/UserLotto.java
@@ -1,8 +1,12 @@
 package lotto.domain.model;
 
-import static lotto.utils.Advice.PurchaseValidator.*;
+import static lotto.utils.Advice.PurchaseValidator.MINIMUM_ORDER;
+import static lotto.utils.Advice.PurchaseValidator.checkConsistNumber;
+import static lotto.utils.Advice.PurchaseValidator.checkReminder;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lotto.utils.LottoGenerator;
@@ -20,13 +24,25 @@ public class UserLotto {
         this.userLotto = IntStream.rangeClosed(1, Integer.parseInt(pay) / MINIMUM_ORDER)
                 .mapToObj(count -> new Lotto(LottoGenerator.makeLotto())).collect(Collectors.toList());
     }
+    public List<Lotto> getUserLotto() {
+        return userLotto;
+    }
 
     private static void validate(String pay) {
         checkConsistNumber(pay);
         checkReminder(pay);
     }
 
-    public List<Lotto> getUserLotto() {
-        return userLotto;
+    public List<LottoRank> compareLottoNumber(LottoNumber lottoNumber) {
+        List<LottoRank> lottoRanks = userLotto.stream().map(lotto -> lotto.compareLottoNumber(lottoNumber))
+                .filter(lottoRank -> lottoRank != LottoRank.NONE).collect(Collectors.toList());
+        validateLottoRankSize(lottoRanks);
+        return lottoRanks;
+    }
+
+    private void validateLottoRankSize(List<LottoRank> lottoRanks) {
+        if (this.userLotto.size() < lottoRanks.size()) {
+            throw new IllegalArgumentException();
+        }
     }
 }
