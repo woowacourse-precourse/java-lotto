eaa104e042a5be327c0a233d8f9bb25533caa7b5
Limjongho <whdgh9873@icloud.com>
2022-11-11 20:17:01 +0900

    feat(Result): 상금 조회 기능 추가

--- src/main/java/domain/Result.java
+++ src/main/java/domain/Result.java
@@ -1,8 +1,9 @@
 package domain;
 
+import java.nio.channels.Pipe;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 
 public class Result {
 
@@ -12,6 +13,14 @@ public class Result {
                 .filter(rank -> rank.checkBonusNumber == isMatchingBonusNumber)
                 .findAny()
                 .orElse(Rank.nothing);
+    }
+
+    public int getPrize(Rank rank) {
+        return Arrays.stream(Prize.values())
+                .filter(prize -> prize.ranking == rank.ranking)
+                .findAny()
+                .get()
+                .rankingPrize;
     }
 
     public int countMatchingNumber(List<Integer> winningLottoNumbers, List<Integer> userNumbers) {
