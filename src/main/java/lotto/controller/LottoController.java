7e1b6964396b7bfd91046d2c5e5909ecd2a7b516
yangjiyoung <0112jyoung@naver.com>
2022-11-11 21:51:59 +0900

    feat: add lotto quantity

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -6,13 +6,13 @@ import lotto.view.InputView;
 import java.util.List;
 
 public class LottoController {
-    private List<Integer> userLottoNumbers;
-    private int userLottoQuantity;
-    private int userLottoMoney;
-    private LottoQuantity lottoQuantity = new LottoQuantity();
-
+    public List<Integer> userLottoNumbers;
+    public int userLottoQuantity;
+    public int userLottoMoney;
+    public LottoQuantity lottoQuantity=new LottoQuantity();
     public void startLotto() {
         getUserMoney();
+        printLottoQuantity();
     }
 
     public void getUserMoney() {
@@ -20,7 +20,9 @@ public class LottoController {
     }
 
 
-    public void printLottoNumbers() {
-        //service에서 구현하기
+    public void printLottoQuantity() {
+        LottoQuantity lottoQuantity=new LottoQuantity();
+        lottoQuantity.setLottoQuantityNumber(userLottoMoney/10);
+        userLottoQuantity=lottoQuantity.getLottoQuantityNumber();
     }
 }
--- src/main/java/lotto/domain/LottoQuantity.java
+++ src/main/java/lotto/domain/LottoQuantity.java
@@ -3,6 +3,10 @@ package lotto.domain;
 public class LottoQuantity {
     private int lottoQuantityNumber;
 
+    public void setLottoQuantityNumber(int lottoQuantityNumber) {
+        this.lottoQuantityNumber = lottoQuantityNumber;
+    }
+
     public int getLottoQuantityNumber() {
         return lottoQuantityNumber;
     }
