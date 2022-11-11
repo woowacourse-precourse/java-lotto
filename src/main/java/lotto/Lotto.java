21d744954eba7b104d266bbac119172e3b3b9e07
tseop <77163842+tseop@users.noreply.github.com>
2022-11-11 16:20:18 +0900

    feat(lotto): 로또 클래스 추가 기능 구현
    로또 번호를 뽑는 기능 구현
    요청한 수량만큼 로또 번호를 뽑는 기능 구현
    로또 번호 반환 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -17,4 +20,28 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private static List<Integer> createLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    public static List<Lotto> buyLotto(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int buyCount = 0; buyCount < count; buyCount++) {
+            Lotto lotto =
+                    new Lotto(Lotto.createLottoNumbers()
+                    .stream()
+                    .sorted()
+                    .collect(Collectors.toList()));
+            lottos.add(lotto);
+        }
+
+        return lottos;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+
 }
