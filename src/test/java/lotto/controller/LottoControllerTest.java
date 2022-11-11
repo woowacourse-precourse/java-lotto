24c7baed6b0e9d8caa9f4e2ec0c36d007cf9ca28
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-11 23:24:47 +0900

    test: LottoController test 수정, 추가
    - generateLottoAuto 바뀐 parameter에 맞게 테스트 수정
    - judgeRank 테스트 추가

--- src/test/java/lotto/controller/LottoControllerTest.java
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -1,6 +1,8 @@
 package lotto.controller;
 
 import lotto.domain.Lotto;
+import lotto.domain.WinningLotto;
+import lotto.domain.WinningRank;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -12,16 +14,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LottoControllerTest {
 
     private LottoController lottoController;
+    private Lotto baseLotto;
 
     @BeforeEach
     void setUp() {
         lottoController = new LottoController();
+        baseLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
     }
 
-    @ParameterizedTest(name = "로또_자동생성_테스트 {index} : {0} 원 - {1}개")
-    @CsvSource(value = {"5000,5", "42000,42"})
-    void 로또_생성_테스트(int purchaseAmount, int expectedLottoCount) {
-        List<Lotto> lottos = lottoController.generateLottoAuto(purchaseAmount);
+    @ParameterizedTest(name = "로또_자동생성_테스트 {index} : {0}")
+    @CsvSource(value = {"5,5", "42,42"})
+    void 로또_생성_테스트(int generatingNumber, int expectedLottoCount) {
+        List<Lotto> lottos = lottoController.generateLottoAuto(generatingNumber);
         assertThat(lottos.size()).isEqualTo(expectedLottoCount);
     }
+
+    @ParameterizedTest(name = "등수 확인 테스트 {index}: {7}등")
+    @CsvSource(value = {
+            "1,2,3,4,5,6,7,1",
+            "1,2,3,4,5,8,6,2",
+            "1,2,3,4,5,8,7,3",
+            "1,2,3,4,7,8,40,4",
+            "1,2,3,7,8,9,40,5",
+            "1,2,7,8,9,10,4,6"
+    })
+    void 등수_확인_테스트(int num1, int num2, int num3, int num4, int num5, int num6, int bonusNum, int rankNum) {
+        List<Integer> numbers = List.of(num1, num2, num3, num4, num5, num6);
+        WinningLotto winningLotto = new WinningLotto(numbers, bonusNum);
+
+        WinningRank actualRank = lottoController.judgeRank(winningLotto, baseLotto);
+        WinningRank expectedRank = WinningRank.findByRank(rankNum);
+
+        assertThat(actualRank).isEqualTo(expectedRank);
+    }
+
 }
