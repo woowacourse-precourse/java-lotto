207cff8aafac350226bfb79e7266e75758ccacc1
MoonJeWoong <munjin0201@naver.com>
2022-11-12 01:20:34 +0900

    feat(view): 구매한 로또 번호들을 출력하는 기능

--- src/main/java/lotto/view/OutputMessage.java
+++ src/main/java/lotto/view/OutputMessage.java
@@ -6,4 +6,8 @@ public final class OutputMessage {
     public static void purchaseComplete(int count){
         System.out.printf("%d개를 구매했습니다.\n", count);
     }
+
+    public static void lottoNumbers(List<Integer> numbers){
+        System.out.println(numbers);
+    }
 }
