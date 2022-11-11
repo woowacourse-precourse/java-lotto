e156c2468a7dbbadf7b6282cce9d37f45026aea5
MoonJeWoong <munjin0201@naver.com>
2022-11-12 02:12:43 +0900

    feat(controller): 로또게임 시작 기능

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,4 +1,17 @@
 package lotto.controller;
 
+import lotto.service.Service;
+
 public class Controller {
+    Service service = new Service();
+
+    public void run(){
+        startLottoGame();
+    }
+
+    public void startLottoGame(){
+        service.initBuyer();
+        service.buyAllLotto();
+        service.showBuyingResult();
+    }
 }
