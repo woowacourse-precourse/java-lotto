4b4238f675f3b5e7aeb658b0fb9d0a2601e9c7aa
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-11 10:23:56 +0900

    feat : 생성된 로또 번호 중복 검증 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,7 +2,9 @@ package lotto;
 
 import lotto.ui.ConsoleOutput;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
 	private final List<Integer> numbers;
@@ -17,6 +19,16 @@ public class Lotto {
 		if (numbers.size() != 6) {
 			throw new IllegalArgumentException();
 		}
+
+		if (isDuplicateExists(numbers)) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private boolean isDuplicateExists(List<Integer> numbers) {
+		Set<Integer> checkDuplicate = new HashSet<>(numbers);
+		checkDuplicate.retainAll(numbers);
+		return checkDuplicate.size() != 6;
 	}
 
 	public List<Integer> getNumbers() {
