24731d1f5e05ae86d8b336355eb803034a3ea404
Hongsugyeong <olivia258@naver.com>
2022-11-11 17:33:15 +0900

    (feat) : 로또 구입 금액 입력 받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.view.InputView;
+
 public class Application {
+
+    private static InputView inputView;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        inputView = new InputView();
+        inputView.buyLotto();
     }
 }
