25285bf07ab9d5f1553c76e2bcb653d8dcfc6e6b
wath1457 <wath1457@gmail.com>
2022-11-12 02:36:50 +0900

    feat(Guide): 안내 문구 출력 추가

--- src/main/java/lotto/domain/Guide.java
+++ src/main/java/lotto/domain/Guide.java
@@ -12,8 +12,16 @@ public class Guide {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public static void printLottoCount(int LottoCount) {
+    public static void printGetLottoCount(int LottoCount) {
         System.out.println("\n" + Integer.toString(LottoCount) + "개를 구매했습니다.");
+    }
+
+    public static void printGetWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void printGetBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
     }
 
     public static int convertStringToInt(String Number) {
