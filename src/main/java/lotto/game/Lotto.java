f444aa9d75483dfaceb97696cd2807f85ca18fbc
euncheol-kim <klmeuncheol@kakao.com>
2022-11-11 22:43:05 +0900

    feat(main/java/lotto/Lotto.java) : 기존 Lotto 클래스의 위치변경
    - 기능 목록으로 명세된 내용을 토대로 Lotto 클래스의 위치를 lotto.game 패키지 하위로 이동했습니다.

--- src/main/java/lotto/game/Lotto.java
+++ src/main/java/lotto/game/Lotto.java
@@ -1,10 +1,12 @@
-package lotto;
+package lotto.game;
 
 import java.util.List;
 
 public class Lotto {
+    // 인스턴스 변수 생성 불가능하다. (객체 내에 선언되는 변수, 공유되지 않는 변수)
     private final List<Integer> numbers;
 
+    // 매개 변수가 없는 생성자를 추가할 수 없다.
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
