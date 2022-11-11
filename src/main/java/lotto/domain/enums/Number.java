29099bf50bb06ae9452808416fc3a2353b3929c4
kdkdhoho <hkim4410@naver.com>
2022-11-11 11:45:17 +0900

    feat(Number): 1천을 나타내는 THOUSAND 열거형 상수 추가

--- src/main/java/lotto/domain/enums/Number.java
+++ src/main/java/lotto/domain/enums/Number.java
@@ -1,13 +1,13 @@
 package lotto.domain.enums;
 
 public enum Number {
-    ZERO(0), ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6),
+    ZERO(0), ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), THOUSAND(1000),
     LOTTO_SIZE(3),
     FIFTH_WINNING_AMOUNT(5_000),
     FOURTH_WINNING_AMOUNT(50_000),
     THIRD_WINNING_AMOUNT(1_500_000),
     SECOND_WINNING_AMOUNT(30_000_000),
-    FIRST_WINNING_AMOUNT(2_000_000_000);;
+    FIRST_WINNING_AMOUNT(2_000_000_000);
 
     private final int value;
 
