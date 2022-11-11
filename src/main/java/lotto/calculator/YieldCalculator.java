779953379e92321b5f1747f1ea16d454cef394f7
K-Diger <kitten.diger@gmail.com>
2022-11-11 17:40:46 +0900

    refactor : YieldCalculator 클래스 내 당첨 통계 출력 방식 변경

--- src/main/java/lotto/calculator/YieldCalculator.java
+++ src/main/java/lotto/calculator/YieldCalculator.java
@@ -1,19 +1,34 @@
 package lotto.calculator;
 
-import java.math.BigDecimal;
-import java.util.List;
+import java.util.*;
 
 public class YieldCalculator {
 
-    public void getStatistics(List<Integer> winCount) {
-        System.out.println("당첨 통계\n---");
-        System.out.printf("3개 일치 (5,000원) - %d개\n", winCount.get(0));
-        System.out.printf("4개 일치 (50,000원) - %d개\n", winCount.get(1));
-        System.out.printf("5개 일치 (1,500,000원) - %d개\n", winCount.get(2));
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", winCount.get(3));
-    }
+    public Map<Integer, Integer> getStatistics(List<Integer> matchResult) {
+        Map<Integer, Integer> winInformation = new HashMap<>() {{
+            put(3, 0);
+            put(4, 0);
+            put(5, 0);
+            put(6, 0);
+        }};
 
-    public void getYield(BigDecimal yield) {
-        System.out.printf("총 수익률은 %s" + "%입니다.", yield);
+        for (Integer integer : matchResult) {
+            if (integer == 3) {
+                winInformation.put(3, (winInformation.get(3) + 1));
+            } else if (integer == 4) {
+                winInformation.put(4, (winInformation.get(4) + 1));
+            } else if (integer == 5) {
+                winInformation.put(5, (winInformation.get(5) + 1));
+            } else if (integer == 6) {
+                winInformation.put(6, (winInformation.get(6) + 1));
+            }
+        }
+        System.out.println("\n당첨 통계\n---");
+        System.out.printf("3개 일치 (5,000원) - %d개\n", winInformation.get(3));
+        System.out.printf("4개 일치 (50,000원) - %d개\n", winInformation.get(4));
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", winInformation.get(5));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", winInformation.get(6));
+
+        return winInformation;
     }
 }
