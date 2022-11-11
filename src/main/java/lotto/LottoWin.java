1de2593e0ae86076ef4e8797ff77778d2855483d
MuSeung <peter0757@nvaer.com>
2022-11-11 13:41:24 +0900

    feat: LottoWin에서 bonus 입력에 대하여 에러가 발생하는 기능 구현
    LottoWin
        - LottoWin : 인자를 구현하고 항상 에러를 발생시켜 테스트를 통과하도록 함

---
+++ src/main/java/lotto/LottoWin.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoWin {
+
+    public LottoWin(List<List<Integer>> quickPicks, List<Integer> lotto, int bonus) {
+        throw new IllegalArgumentException();
+    }
+}
