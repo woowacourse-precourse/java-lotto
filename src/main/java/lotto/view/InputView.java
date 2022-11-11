de17d2a6287ea08f3cc8b6e7c4050a31c5ec4aeb
akskflwn <akskflwn@gmail.com>
2022-11-12 01:19:05 +0900

    feat (InputView) : 당첨 번호를 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,14 +1,21 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class InputView {
-    private final static String MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+
+    private final static String GET_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String NUMBER_REGEX = "^[0-9]*$";
+    private static final String GET_WIN_LOTTO_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String WIN_LOTTO_REGEX = "^[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+$";
+    private static final String SPLIT_REGEX = ",";
 
     public static int getMoneyInput() {
-        System.out.println(MONEY_MESSAGE);
+        System.out.println(GET_MONEY_MESSAGE);
         String input = Console.readLine();
         validate(input);
         return toNumbers(input);
@@ -22,6 +29,20 @@ public class InputView {
 
     private static int toNumbers(String input) {
         return Integer.parseInt(input);
+    }
+
+    public static List<Integer> getWinLotto() {
+        System.out.println(GET_WIN_LOTTO_MESSAGE);
+        String input = Console.readLine();
+        validateWinLotto(input);
+        return Arrays.stream(input.split(SPLIT_REGEX)).map(InputView::toNumbers)
+            .collect(Collectors.toList());
+    }
+
+    private static void validateWinLotto(String input) {
+        if (!Pattern.matches(WIN_LOTTO_REGEX, input)) {
+            throw new IllegalArgumentException("[ERROR] 입력 형식이 올바르지 않습니다.");
+        }
     }
 
 }
