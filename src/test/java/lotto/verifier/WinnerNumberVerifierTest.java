be01e35b1fb76d59238ed59f3bfebf0c97c7d8d7
woosung1223 <dntjd991223@naver.com>
2022-11-11 15:11:46 +0900

    test(WinnerNumberVerifierTest): 유효한 개수가 입력되었는지 테스트

---
+++ src/test/java/lotto/verifier/WinnerNumberVerifierTest.java
@@ -0,0 +1,31 @@
+package lotto.verifier;
+
+import lotto.util.ExceptionMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class WinnerNumberVerifierTest {
+    WinnerNumberVerifier winnerNumberVerifier = new WinnerNumberVerifier();
+    @Nested
+    @DisplayName("유효한 개수가 입력되지 않았을 때")
+    class CheckNumberCount {
+        @ParameterizedTest(name = "{0}가 입력되었을 때")
+        @ValueSource(strings = {
+                " ",
+                "1",
+                "1,2",
+                "1,2,3",
+                "1,2,3, 4,5",
+                "1,2,3,4,5,6,7"
+        })
+        void 유효한_개수가_입력되었는지_테스트한다(String target) {
+            assertThatThrownBy(() -> winnerNumberVerifier.check(target))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ExceptionMessage.COUNT_NOT_EQUAL);
+        }
+    }
+}
