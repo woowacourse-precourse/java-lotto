0707e3470a6b163b4678d708c040a0658f6caba0
yummygyudon <bang2brew@gmail.com>
2022-11-11 11:44:01 +0900

    FEAT(util): 상황별 에러메시지 출력을 위한 ErrorMessages Enum 구현
    - README.md에 작성한 입력값에 대한 예외 발생 케이스별 에러메시지 작성
      1. HAS_OVERLAP_NUMBER : 중복값 입력
      2. LESS_THAN_SIX : 6개 미만 입력
      3. MORE_THAN_SIX : 6개를 초과하여 입력
      4. WRONG_SEPARATOR : 잘못된 구분자 입력
      5. NOT_NUMBER_EXIST : 숫자가 아닌 값 입력
      6. OUT_OF_BOUND : 1~45 범위에 포함되지 않는 번호 입력
      7. MONEY_UNDIVIDED : 1,000 단위가 아닌 구매액 입력
      8. BELOW_THE_PRICE_THRESHOLD : 1,000원 이하의 값을 구매액으로 입력

---
+++ src/main/java/lotto/util/ErrorMessages.java
@@ -0,0 +1,22 @@
+package lotto.util;
+
+public enum ErrorMessages {
+    HAS_OVERLAP_NUMBER("[ERROR] 중복된 값이 있습니다."),
+    LESS_THAN_SIX("[ERROR] 6개의 숫자를 입력해야 합니다."),
+    MORE_THAN_SIX("[ERROR] 6개의 숫자를 입력해야 합니다."),
+    WRONG_SEPARATOR("[ERROR] 쉼표를 통해 구분해야합니다."),
+    NOT_NUMBER_EXIST("[ERROR] 숫자만 입력해야 합니다."),
+    OUT_OF_BOUND("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    MONEY_UNDIVIDED("[ERROR] 1,000원 단위로만 구매할 수 있습니다."),
+    BELOW_THE_PRICE_THRESHOLD("[ERROR] 최소 구매 가능액은 1,000원 입니다.");
+
+
+    private final String errorMessage;
+
+    ErrorMessages(String errorMessage){
+        this.errorMessage = errorMessage ;
+    }
+    public String getMessage(){
+        return this.errorMessage;
+    }
+}
