48c0eb0fb3936713d1cf0390c403ddb063da6fdc
kdkdhoho <hkim4410@naver.com>
2022-11-11 16:00:40 +0900

    refactor(Nubmer): LOTTO_SIZE 값 변경

--- src/main/java/lotto/domain/enums/Number.java
+++ src/main/java/lotto/domain/enums/Number.java
@@ -2,7 +2,7 @@ package lotto.domain.enums;
 
 public enum Number {
     ZERO(0), ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), THOUSAND(1000),
-    LOTTO_SIZE(3),
+    LOTTO_SIZE(6),
     FIFTH_WINNING_AMOUNT(5_000),
     FOURTH_WINNING_AMOUNT(50_000),
     THIRD_WINNING_AMOUNT(1_500_000),
