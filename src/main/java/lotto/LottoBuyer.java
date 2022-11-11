d76cd1ae2a319ad8fb6a6fe0a1b1fbd77f63aa7c
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-12 02:24:03 +0900

    Feat(LottoBuyer) : 사용자가 입력하는  #48
    inputLottoNumbers() 메소드를 추가했습니다.
    이 메소드는 사용자로부터 문자열을 입력받아 ','을 기준으로 List를 Integer 형으로 반환합니다.

--- src/main/java/lotto/LottoBuyer.java
+++ src/main/java/lotto/LottoBuyer.java
@@ -3,13 +3,13 @@ package lotto;
 import lotto.CheckInputException;
 import lotto.Util;
 
+import java.util.List;
 import java.math.BigInteger;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class LottoBuyer {
 
-    String input = Console.readLine();
     public static BigInteger inputMoney() throws IllegalArgumentException{
         final String input = Console.readLine();
 
@@ -21,5 +21,19 @@ public class LottoBuyer {
             throw iae;
         }
         return new BigInteger(input);
+    }
+
+    public static List<Integer> inputLottoNumbers() throws IllegalArgumentException{
+        final String input = Console.readLine();
+        List<Integer> inputNumbers;
+
+        try{
+            CheckInputException.checkBuyerInputIsNotSixNumbers(input);
+            inputNumbers = Util.splitInteger(input, ",");
+        }catch (IllegalArgumentException iae){
+            throw iae;
+        }
+        System.out.println(inputNumbers);
+        return inputNumbers;
     }
 }
