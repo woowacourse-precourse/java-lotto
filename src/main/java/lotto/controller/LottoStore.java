d923d6602a62471204f1286bb50b357238e7117a
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-11 20:48:28 +0900

    feat: 로또 구매하는 기능 구현

---
+++ src/main/java/lotto/controller/LottoStore.java
@@ -0,0 +1,29 @@
+package lotto.controller;
+
+import lotto.domain.Lotto;
+import lotto.ui.InputUi;
+import lotto.ui.OutputUi;
+
+import java.util.List;
+
+public class LottoStore {
+
+    private LottoController lottoController;
+
+    public LottoStore() {
+        this.lottoController = new LottoController();
+    }
+
+    public void startLottoService() {
+        List<Lotto> myLottos = buyLottos();
+    }
+
+    private List<Lotto> buyLottos() {
+        OutputUi.printInsertMoneyInfoMessage();
+        int purchasedAmount = InputUi.readPurchaseAmount();
+
+        List<Lotto> generatedLottos = lottoController.generateLottoAuto(purchasedAmount);
+        OutputUi.printPurchasedLottos(generatedLottos);
+    }
+
+}
