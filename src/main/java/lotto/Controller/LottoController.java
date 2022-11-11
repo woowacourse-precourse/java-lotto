5314297ac0dad7a79a7e87fa9ca9376b44504d34
raonrabbit <raonrabbit@gmail.com>
2022-11-12 02:30:32 +0900

    feat: 구매한 복권 수 만큼 랜덤 번호 생성 구현

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -1,7 +1,12 @@
 package lotto.Controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Model.Lotto;
 import lotto.Model.LottoData;
 import lotto.View.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -12,5 +17,15 @@ public class LottoController {
     private void setLottoCount(){
         output.askPurchasePrice();
         lottoData.setCountOfLotto(Integer.parseInt(readLine()));
+    }
+
+    private void makeLottoNumber(){
+        List<Lotto> lottos = new ArrayList<Lotto>();
+        for(int i = 0; i < lottoData.getCountOfLotto(); i++){
+            List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(lottoNumber);
+            lottos.add(lotto);
+        }
+        lottoData.setLottos(lottos);
     }
 }
--- src/main/java/lotto/Model/LottoData.java
+++ src/main/java/lotto/Model/LottoData.java
@@ -1,8 +1,18 @@
 package lotto.Model;
 
+import java.util.List;
+
 public class LottoData{
     private int countOfLotto;
+    private List<Lotto> lottos;
     public void setCountOfLotto(int numberOfLotto){
         this.countOfLotto = numberOfLotto;
+    }
+
+    public int getCountOfLotto(){
+        return countOfLotto;
+    }
+    public void setLottos(List<Lotto> lottos){
+        this.lottos = lottos;
     }
 }
