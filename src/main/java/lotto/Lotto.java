cac470a665de20641d86fbef3b26070379a0ad3c
oyoungsun <plusmate@naver.com>
2022-11-12 00:58:20 +0900

    feat : validate fuction in Lotto Class

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,9 +11,16 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        isSize_6(numbers);
+        isNotDuplicated(numbers);
+    }
+    private void isSize_6(List<Integer> numbers){
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개 여야 합니다.");
         }
+    }
+    private void isNotDuplicated(List<Integer> numbers){
+        int notDuplicatedSize = (int) numbers.stream().distinct().count();
     }
 
     // TODO: 추가 기능 구현
