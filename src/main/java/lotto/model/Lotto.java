8da164e1c9ab6ee87a27774e616b262f926be1f5
Cyma-s <ssol0319@naver.com>
2022-11-11 18:09:29 +0900

    fix(Lotto): delete numbers sort function

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,5 @@
 package lotto.model;
 
-import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -11,7 +10,6 @@ public class Lotto {
         validateNumberRange(numbers);
         validateDuplicateNumbers(numbers);
         this.numbers = numbers;
-        sortNumbers();
     }
 
     private void validateNumberCount(List<Integer> numbers) {
@@ -19,31 +17,28 @@ public class Lotto {
             throw new IllegalArgumentException(InputErrorMessage.INVALID_DIGITS_COUNT.getErrorMessage());
         }
     }
+
     private void validateNumberRange(List<Integer> numbers) {
-        for(Integer number : numbers) {
-            if(number < Constant.NUMBER_RANGE_START || number > Constant.NUMBER_RANGE_END) {
+        for (Integer number : numbers) {
+            if (number < Constant.NUMBER_RANGE_START || number > Constant.NUMBER_RANGE_END) {
                 throw new IllegalArgumentException(InputErrorMessage.OUT_OF_RANGE_NUMBER.getErrorMessage());
             }
         }
     }
 
     private void validateDuplicateNumbers(List<Integer> numbers) {
-        for(int i = 0; i<numbers.size(); i++) {
+        for (int i = 0; i < numbers.size(); i++) {
             checkDuplicateNumber(numbers, i);
         }
     }
 
     private void checkDuplicateNumber(List<Integer> numbers, int index) {
         Integer uncheckedNumber = numbers.get(index);
-        for(int j = index; j<numbers.size(); j++) {
+        for (int j = index; j < numbers.size(); j++) {
             Integer nextNumber = numbers.get(j);
-            if(uncheckedNumber.equals(nextNumber)) {
+            if (uncheckedNumber.equals(nextNumber)) {
                 throw new IllegalArgumentException(InputErrorMessage.DUPLICATE_NUMBERS.getErrorMessage());
             }
         }
-    }
-
-    public void sortNumbers() {
-        Collections.sort(numbers);
     }
 }
