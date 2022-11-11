0169af698422f523da27929e85fd52cc5b2f523d
sangil <gsl0505@naver.com>
2022-11-11 18:15:53 +0900

    feat(Exception): 구입 금액이 숫자로 구성되어 있는지 확인하는 기능 추가

---
+++ src/main/java/lotto/exception/InputExceptionHandler.java
@@ -0,0 +1,19 @@
+package lotto.exception;
+
+public class InputExceptionHandler {
+	private static final String NUMBER_EXCEPTION_MESSAGE = "[ERROR] 구입 금액은 숫자로 입력받아야 합니다.";
+	private static final String DIVISION_EXCEPTION_MESSAGE = "[ERROR] 구입 금액은 1000원 단위여야 합니다.";
+	private static final String DUPLICATED_WINNING_NUMBER_EXCEPTION = "[ERROR] 당첨 번호는 중복이 안됩니다.";
+	private static final String ONE_TO_FORTY_FIVE_EXCEPTION = "[ERROR] 1 ~ 45 사이의 숫자여야합니다.";
+	private static final String WINNING_NUMBER_COUNT_EXCEPTION = "[ERROR] 당첨 번호는 6개를 입력해야 합니다.";
+	private static final String WINNING_NUMBER_DIVISION_EXCEPTION = "[ERROR] 당첨 번호는 쉼표로 구분지어야 합니다.";
+	private static final String DUPLICATED_BONUS_NUMBER_MESSAGE = "[ERROR] 보너스 번호는 당첨 번호와 중복되면 안됩니다.";
+	private static final String NUMBER_REGEX = "^[0-9]*$";
+
+	private void isNumber(String input) {
+		if (!input.matches(input)) {
+			throw new IllegalArgumentException(NUMBER_EXCEPTION_MESSAGE);
+		}
+	}
+
+}
