a4f59d11002389bfa07dd08278a51cad51544572
lee <whddlsk123@naver.com>
2022-11-11 15:11:39 +0900

    feat(createUserLotto) : 로또 구매개수를 입력받아 갯수만큼 로또를 생성

--- src/main/java/lotto/domain/UserLotto.java
+++ src/main/java/lotto/domain/UserLotto.java
@@ -2,20 +2,29 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserLotto {
-
+    private final Map<Integer,List<Integer>> userLotto = new HashMap<>();
+    
     //예외사항(입력단위가 1000원이 아니면)
     private int getLottoAmount(int money) {
         int amount = money/1000;
 
         return amount;
     }
-
+    
     private List<Integer> createLotto() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
 
         return lottoNumbers;
+    }
+    
+    private void createUserLotto(int amount) {
+        for (int key = 0; key < amount; key++) {
+            userLotto.put(key, createLotto());
+        }
     }
 }
---
+++ src/test/java/lotto/domainTest/UserLottoTest.java
@@ -0,0 +1,12 @@
+package lotto.domainTest;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserLottoTest {
+
+    @DisplayName("")
+    @Test
+    void
+}
