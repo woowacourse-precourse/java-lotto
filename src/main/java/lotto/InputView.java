27602d29b7c863675ecbaaa9f8172a62fb47f2ee
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-12 02:18:01 +0900

    feat(inputView): 사용자 로또 당첨 번호 입력 기능 추가
    docs: 로또 당첨 번호 입력 완료 체크

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public String enterAmount() {
         System.out.println(INPUT_AMOUNT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String enterWinningNumber() {
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }
