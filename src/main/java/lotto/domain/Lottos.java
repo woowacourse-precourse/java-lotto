a2ccde97bd94cf4a9a8df069b2845bcd1f1b9e89
i960107 <i960107@naver.com>
2022-11-11 15:20:35 +0900

    feat(Lottos): iterator추가 및 순위 정보, 당첨 결과 저장하는 기능 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,12 +1,23 @@
 package lotto.domain;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
-public class Lottos {
-    private List<Lotto> lottos;
+public class Lottos implements Iterable<Lotto> {
+    private final int size;
+    private final List<Lotto> lottos;
+    private Map<LottoRank, Integer> ranks;
+    private LottoResult result;
 
     public Lottos(List<Lotto> lottos) {
+        this.size = lottos.size();
         this.lottos = lottos;
+    }
+
+
+    public Iterator<Lotto> iterator() {
+        return lottos.iterator();
     }
 
     public Lotto get(int index) {
@@ -15,6 +26,14 @@ public class Lottos {
 
     public int size() {
         return lottos.size();
+    }
+
+    public void setRanks(Map<LottoRank, Integer> ranks) {
+        this.ranks = ranks;
+    }
+
+    public void setResult(LottoResult result) {
+        this.result = result;
     }
 
 }
