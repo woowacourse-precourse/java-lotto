d813a7bfbb01fc5d68db53098f87bd5d91bb7938
heesang <dlagmltkd12@naver.com>
2022-11-12 06:30:45 +0900

    feat: 발행한 로또 수량 및 번호를 출력하는 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,14 +1,34 @@
 package lotto.view;
 
+import lotto.dto.LottosResponseDto;
+
+import java.util.List;
+
+import static lotto.dto.LottosResponseDto.*;
+
 public class OutputView {
 
     public static final String INPUT_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String INPUT_ANSWER_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     public static final String WINNING_STATISTICS_MESSAGE = "당첨 통계\n" + "---";
+    public static final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
 
     public static void printInputPurchasePrice() {
         System.out.println(INPUT_PURCHASE_PRICE_MESSAGE);
+    }
+
+    public static void printPurchaseLottos(LottosResponseDto responseDto) {
+        List<LottoResponseDto> lottoResponseDtos = responseDto.getLottoResponseDtos();
+        printLottoCount(lottoResponseDtos.size());
+
+        for (LottoResponseDto lottosResponseDto : lottoResponseDtos) {
+            System.out.println(lottosResponseDto.getLottoNumber());
+        }
+    }
+
+    private static void printLottoCount(int lottoCount) {
+        System.out.printf(LOTTO_COUNT_MESSAGE, lottoCount);
     }
 
     public static void printInputAnswerNumbers() {
