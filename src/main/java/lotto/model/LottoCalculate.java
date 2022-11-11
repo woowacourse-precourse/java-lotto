f9b01031c5a2ed0ba438e7151f94e952d6cd5b96
newh08 <newh08@gmail.com>
2022-11-11 11:30:49 +0900

    refactor: change variable name
    Change variable name at checkMatches method.
    - correctNumber -> matchCount
    Because matchCount is more proper name.

--- src/main/java/lotto/model/LottoCalculate.java
+++ src/main/java/lotto/model/LottoCalculate.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class LottoCalculate {
 
     int checkMatches(Collection<Integer> lottoNumbers, List<Integer> winNumbers) {
-        int correctNumber = lottoNumbers.size();
+        int matchCount = lottoNumbers.size();
         lottoNumbers.removeAll(winNumbers);
-        correctNumber -= lottoNumbers.size();
-        return correctNumber;
+        matchCount -= lottoNumbers.size();
+        return matchCount;
     }
 
     boolean checkBonusNumber(List<Integer> lottoNumbers, int bonusNumber) {
