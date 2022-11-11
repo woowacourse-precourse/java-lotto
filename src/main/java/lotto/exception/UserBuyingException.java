6a32ada3b4714593a796b4d7e999a648993eecea
sosow0212 <sosow0212@naver.com>
2022-11-11 12:35:39 +0900

    feat : 유저가 로또를 1,000원 단위로 구매했는지 확인해주는 예외 작성

---
+++ src/main/java/lotto/exception/UserBuyingException.java
@@ -0,0 +1,11 @@
+package lotto.exception;
+
+public class UserBuyingException {
+    private final static int LOTTO_PRICE = 1000;
+
+    public void validBuyingException(int buyingMoney) {
+        if (buyingMoney % LOTTO_PRICE == 0) {
+            throw new IllegalArgumentException("구매는 1,000원 단위로 가능합니다.");
+        }
+    }
+}
