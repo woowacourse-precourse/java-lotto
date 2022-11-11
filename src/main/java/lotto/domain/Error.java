a9b57a314ae9b9768152f5be14f6c454adfac150
opuopuo <pureuml.dev@gmail.com>
2022-11-11 18:58:26 +0900

    feat(money): 구입금액 단위 예외처리 구현

--- src/main/java/lotto/domain/Error.java
+++ src/main/java/lotto/domain/Error.java
@@ -1,7 +1,8 @@
 package lotto.domain;
 
 public enum Error {
-    NOT_NUMBER_ERROR("구매 금액은 숫자여야 합니다.");
+    NOT_NUMBER_ERROR("구입금액은 숫자여야 합니다."),
+    MONEY_UNIT_ERROR("구입금액은 1000원 단위여야 합니다.");
 
     private final String message;
 
--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,11 +1,20 @@
 package lotto.domain;
 
+import static lotto.domain.Error.MONEY_UNIT_ERROR;
+
 public class Money {
     private static final int UNIT_MONEY = 1_000;
 
     private int money;
 
     public Money(int money) {
+        isValidMoney(money);
         this.money = money;
+    }
+
+    private void isValidMoney(int money) {
+        if (money  % UNIT_MONEY != 0) {
+            throw new IllegalArgumentException(MONEY_UNIT_ERROR.toString());
+        }
     }
 }
