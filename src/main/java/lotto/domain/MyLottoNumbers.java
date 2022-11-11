c1311b5098005165b9a77e3af65a3b80623717c9
ywj9811 <ywj9811@naver.com>
2022-11-12 01:50:18 +0900

    style: MyLottoNumbers 주석 제거

--- src/main/java/lotto/domain/MyLottoNumbers.java
+++ src/main/java/lotto/domain/MyLottoNumbers.java
@@ -23,20 +23,4 @@ public class MyLottoNumbers {
         Collections.sort(myLottoNumbers);
         return myLottoNumbers;
     }
-    /**
-     * Collections.sort를 사용하니 java.lang.UnsupportedOperationException 발생
-     * new로 객체가 완전히 구현되지 않은 상태에서 사용했기 때문에 발생한다고 한다.
-     * ArrayList 객체로 생성을 한 후 진행해도 똑같다.
-     * 애초에 Randoms.pick... 여기서 ArrayList로 반환이 되는 것 같은데...
-     * Test에서만 문제가 발생하니 Test를 더 확인해보도록 해야겠다.
-     *
-     * 테스트에서 List.of 를 사용하고 있기 때문에 예외가 발생하는 것이 아닌지 생각이 든다
-     * 어떻게 해결할까...
-     *
-     * List.of로 생성한 것들을 ArrayList<>()에 넣어주면 해결할 수 있다고 한다.
-     * 어떻게 넣어줄 것인가.
-     * pickUniqueNumbersInRange 로 생성할 것들에
-     * List.of 의 것들이 차례대로 들어간다는 것을 생각하자.
-     * 해결!!!
-     */
 }
