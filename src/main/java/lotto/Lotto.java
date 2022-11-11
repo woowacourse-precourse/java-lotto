c55b2b895c9f032d65889673c05f57dbb6a44442
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-11 13:19:19 +0900

    fix(Lotto) : List의 중복 체크 기능과 List를 문자열로 표현하는 기능 구현.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,34 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        overlapsFind(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 숫자 부족!");
         }
     }
-
+    private void overlapsFind(List<Integer> numbers){
+        Set<Integer> overlapTest = new HashSet<>();
+        for(int nowNumber : numbers){
+            overlapTest.add(nowNumber);
+        }
+        if(overlapTest.size()!=6)
+            throw new IllegalArgumentException("[ERROR] 중복 숫자발견");
+    }
+    @Override
+    public String toString(){
+        return numbers.toString();
+    }
     // TODO: 추가 기능 구현
 }
