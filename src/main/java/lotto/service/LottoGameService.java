341138bf4dea4e88c88299bb8243ea436f1fabe5
ksh-beginner <ohk9134@naver.com>
2022-11-11 10:04:52 +0900

    feat(LottoGameService) : 보너스 번호의 범위가 1~45가 아닌 경우 검증 구현

--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -23,6 +23,7 @@ public class LottoGameService {
     private static final String LOTTO_WINNING_NUMBERS_REGEX = "^[0-9,]+$";
     private static final String LOTTO_WINNING_NUMBERS_TYPE_EXCEPTION_MESSAGE = "로또 당첨 번호는 공백 없이 쉼표로 구분된 숫자여야합니다.";
     private static final String BONUS_NUMBER_NOT_DIGIT_EXCEPTION_MESSAGE = "보너스 번호는 숫자여야합니다.";
+    private static final String BONUS_NUMBER_RANGE_EXCEPTION_MESSAGE = "보너스 번호의 범위는 1~45여야합니다.";
 
     private Lotto lotto;
 
@@ -64,6 +65,9 @@ public class LottoGameService {
     public void validateBonusNumber(String bonusNumber) {
         if (isBonusNumberNotDigit(bonusNumber)) {
             throw new IllegalArgumentException(ERROR_MESSAGE_PREFIX + BONUS_NUMBER_NOT_DIGIT_EXCEPTION_MESSAGE);
+        }
+        if (isBonusNumberWrongRange(bonusNumber)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_PREFIX + BONUS_NUMBER_RANGE_EXCEPTION_MESSAGE);
         }
     }
 
@@ -108,6 +112,10 @@ public class LottoGameService {
 
     private boolean isBonusNumberNotDigit(String bonusNumber) {
         return !Pattern.compile(DIGIT_REGEX).matcher(bonusNumber).matches();
+    }
+
+    private boolean isBonusNumberWrongRange(String bonusNumber) {
+        return Integer.parseInt(bonusNumber) < 1 || Integer.parseInt(bonusNumber) > 45;
     }
 
     public List<Integer> inputWinningNumberConvertToCollection(String lottoWinningNumber) {
