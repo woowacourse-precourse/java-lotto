8493fcbb1d9e6e7b302e11571bffd2fcd77dd7e8
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2022-11-11 18:14:06 +0900

    refactor : insert feature of sorting lottery numbers as natural order in LotteryMachine#generateLotteries

--- src/main/java/lotto/domain/LotteryMachine.java
+++ src/main/java/lotto/domain/LotteryMachine.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class LotteryMachine {
@@ -12,10 +13,10 @@ public class LotteryMachine {
         List<Lotto> lotteries = new ArrayList<>();
         for (int i = 0; i < numberOfLotteries; ++i) {
             List<Integer> lotteryNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lotteryNumbers.sort(Comparator.naturalOrder());
             Lotto lotto = new Lotto(lotteryNumbers);
             lotteries.add(lotto);
         }
-
         user.setLotteries(lotteries);
     }
 }
