34d62b7b1a0b2f8896ed56443c5fd4ebe444d135
CMHyuk <alsgur9710@naver.com>
2022-11-11 22:57:07 +0900

    refactor(Input): 구입금액 입력 검증 추가

--- src/main/java/lotto/console/Input.java
+++ src/main/java/lotto/console/Input.java
@@ -6,8 +6,24 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
 
     public static int inputMoney() {
-        return Integer.parseInt(Console.readLine());
+        String money = Console.readLine();
+        isCorrectPattern(money);
+        isFirstNumberZero(money);
+        return Integer.parseInt(money);
     }
+
+    private static void isFirstNumberZero(String money) {
+        if (money.charAt(0) == '0') {
+            throw new IllegalArgumentException("첫 자리는 0을 입력할 수 없습니다.");
+        }
+    }
+
+    private static void isCorrectPattern(String money) {
+        if (!money.matches("^[0-9]+$")) {
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+    }
+
     public static String[] inputLottoNumbers() {
         return Console.readLine().split(",");
     }
