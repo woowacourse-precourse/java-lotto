c4de28279734966135095836dfcd2215de967401
rhqudco <rhqudco1204@naver.com>
2022-11-11 22:19:24 +0900

    STYLE: 인터페이스 public 제거

--- src/main/java/lotto/LottoExceptionCase.java
+++ src/main/java/lotto/LottoExceptionCase.java
@@ -3,15 +3,15 @@ package lotto;
 import java.util.List;
 
 public interface LottoExceptionCase {
-    public void validateInputEmpty(String number);
-    public void validateRangeBonusNumber(int bonusNumber);
-    public void validateContainsBonusNumber(int bonusNumber, List<Integer> numbers);
-    public void validateCharacter(char inputSequence);
-    public void validateDuplicate(List<Integer> numbers);
-    public void validateNumberRange(List<Integer> numbers);
-    public void validateInputValue(String winningNumber);
-    public void validateCommaCount(String winningNumber);
-    public void validateDuplicateComma(char sequence1, char sequence2);
-    public void validateLastIndexIsComma(String winningNumber);
-    public void validateFirstIndexIsComma(String winningNumber);
+    void validateInputEmpty(String number);
+    void validateRangeBonusNumber(int bonusNumber);
+    void validateContainsBonusNumber(int bonusNumber, List<Integer> numbers);
+    void validateCharacter(char inputSequence);
+    void validateDuplicate(List<Integer> numbers);
+    void validateNumberRange(List<Integer> numbers);
+    void validateInputValue(String winningNumber);
+    void validateCommaCount(String winningNumber);
+    void validateDuplicateComma(char sequence1, char sequence2);
+    void validateLastIndexIsComma(String winningNumber);
+    void validateFirstIndexIsComma(String winningNumber);
 }
