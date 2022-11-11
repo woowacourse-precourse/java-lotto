a255ad162415e83c1ef5c51f9f586162167a1d7a
Joon <orijoon98@gmail.com>
2022-11-11 12:29:59 +0900

    fix: add validate number method and fix validateLottoNumber method

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -2,14 +2,19 @@ package lotto;
 
 public class Input {
 
-    public static void validateLottoNumber(String input) {
-        int lottoNumber;
+    public static void validateNumber(String input) {
         try {
-            lottoNumber = Integer.parseInt(input);
+            Integer.parseInt(input);
         } catch (NumberFormatException exception) {
             // TODO: 2022/11/11 에러 메시지 출력 추가
             throw new IllegalArgumentException();
         }
+    }
+
+    public static void validateLottoNumber(String input) {
+        int lottoNumber;
+        validateNumber(input);
+        lottoNumber = Integer.parseInt(input);
         if (lottoNumber < 1 || lottoNumber > 45) {
             // TODO: 2022/11/11 에러 메시지 출력 추가
             throw new IllegalArgumentException();
