1dc0be93c2377488edbcd5fa2774cf5f21c4aef0
EUNJEONGMUN <munej26@gmail.com>
2022-11-11 11:16:53 +0900

    refactor: 에러 메시지 상수 클래스 생성하여 한 곳에 모음

---
+++ src/main/java/constant/ErrorMessage.java
@@ -0,0 +1,7 @@
+package constant;
+
+public class ErrorMessage {
+    public static final String NOT_NUMBER = "[ERROR] 숫자를 입력해주세요.";
+    public static final String NOT_RADIX_1000 = "[ERROR] 1,000원 단위로 입력해주세요.";
+    public static final String NOT_LOTTO_NUMBER_RANGE = "[ERROR] 1에서 45사이의 숫자를 입력해주세요.";
+}
