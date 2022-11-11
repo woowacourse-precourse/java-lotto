92b538a5844ade717f1498f69d3a3d754c60df1c
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-11 19:38:37 +0900

    feat(Rank): Rank enum 생성
    Rank Enum 주요 기능 목록:
    - 맞춘 번호 갯수와 보너스 번호 포함여부를 판단 하여 등수를 리턴한다.

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+	FIRST(6, 2_000_000_000),
+	SECOND(5, 30_000_000),
+	THIRD(5, 1_500_000),
+	FOURTH(4, 50_000),
+	FIFTH(3, 5_000),
+	MISS(0, 0);
+
+	private final int hitNumberCount;
+	private final int prizeMoney;
+
+	Rank(int hitNumberCount, int prizeMoney) {
+		this.hitNumberCount = hitNumberCount;
+		this.prizeMoney = prizeMoney;
+	}
+
+	public static Rank of(int hitNumberCount, boolean isContainsBonusNumber) {
+		if (isContainsBonusNumber && SECOND.hitNumberCount == hitNumberCount) {
+			return SECOND;
+		}
+
+		return Arrays.stream(values())
+			.filter(Rank -> ((Rank != SECOND) && Rank.hitNumberCount == hitNumberCount))
+			.findFirst()
+			.orElse(MISS);
+	}
+
+	public int getPrizeMoney() {
+		return prizeMoney;
+	}
+
+}
