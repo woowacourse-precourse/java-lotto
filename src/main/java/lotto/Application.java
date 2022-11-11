c2fa980c804df7c398db31ae31ab58b44b4d4141
making-a-scene <4936joo@naver.com>
2022-11-11 14:30:45 +0900

    [UI] Get amount of money to buy Lotto

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static int getPriceToBuy() {
+        System.out.println("구입 금액을 입력해주세요.");
+        String response = Console.readLine();
+        return Integer.parseInt(response);
     }
 }
