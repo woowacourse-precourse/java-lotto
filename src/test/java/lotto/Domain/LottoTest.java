2644c97418e54bd80b07d4586073a23357390f7c
Park shinyeong <syngh503@gmail.com>
2022-11-12 01:09:32 +0900

    Test(LottoTest): LottoTest 중복 제거
    test의 lotto 패키지에 LottoTest, Domain 패키지에 LottoTest가 생성되어 같은 클래스에 대해 두 개의 테스트가 생성되었습니다.
    중복을 제거해 Domain에 LottoTest 클래스를 생성하였습니다.
    
    Relate to: #14

--- src/test/java/lotto/Domain/LottoTest.java
+++ src/test/java/lotto/Domain/LottoTest.java
@@ -1,35 +1,28 @@
 package lotto.Domain;
 
-
+import lotto.Domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
-    Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-
-    @Nested
-    @DisplayName("SetLottos 메소드 테스트")
-    class TestSetLottos {
-
-        @Test
-        @DisplayName("구입 횟수가 1번일 때")
-        void case1() {
-            int lottoCount = 1;
-            List<List<Integer>> result = lotto.setLottos(lottoCount);
-            assertThat(result.size()).isEqualTo(1);
-        }
-
-        @Test
-        @DisplayName("구입 횟수가 여러번일 때")
-        void case2() {
-            int lottoCount = 9;
-            List<List<Integer>> result = lotto.setLottos(lottoCount);
-            assertThat(result.size()).isEqualTo(9);
-        }
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    // 아래에 추가 테스트 작성 가능
 }
