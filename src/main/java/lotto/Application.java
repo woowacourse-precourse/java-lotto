f489f573f66aee4ceb190e189654d402013c41c2
jkde7721 <jkde7721@naver.com>
2022-11-11 10:51:09 +0900

    feat: 로또 게임 시작 main 메소드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import lotto.controller.LottoGame;
+import lotto.model.LottoDrawingMachine;
+import lotto.model.LottoIssuingMachine;
+import lotto.model.LottoWinningStatistics;
+import lotto.view.LottoView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoGame lottoGame = getLottoGame();
+        lottoGame.play();
+    }
+
+    private static LottoGame getLottoGame() {
+        LottoIssuingMachine issuingMachine = new LottoIssuingMachine();
+        LottoDrawingMachine drawingMachine = new LottoDrawingMachine();
+        LottoWinningStatistics winningStatistics = new LottoWinningStatistics();
+        LottoView lottoView = LottoView.getInstance();
+        return new LottoGame(issuingMachine, drawingMachine, winningStatistics, lottoView);
     }
 }
