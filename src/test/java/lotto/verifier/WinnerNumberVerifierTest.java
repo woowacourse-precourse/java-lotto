c4ab64f7f71d156f2bcfa6a4420f794559c359d4
woosung1223 <dntjd991223@naver.com>
2022-11-11 15:32:31 +0900

    test(WinnerNumberVerifierTest): 각 원소가 숫자인지 테스트

--- src/test/java/lotto/verifier/WinnerNumberVerifierTest.java
+++ src/test/java/lotto/verifier/WinnerNumberVerifierTest.java
@@ -28,4 +28,21 @@ class WinnerNumberVerifierTest {
                     .hasMessageContaining(ExceptionMessage.COUNT_NOT_EQUAL);
         }
     }
+
+    @Nested
+    @DisplayName("각 원소가 숫자가 아닐 때")
+    class CheckEachNumeric {
+        @ParameterizedTest(name = "{0}가 입력되었을 때")
+        @ValueSource(strings = {
+                "1,2,3,4,a,6",
+                "@,2,3,4,5,6",
+                "\\,2,3,4,5,6",
+                "아,1,2,3,4,5"
+        })
+        void 각_원소가_숫자인지_테스트한다(String target) {
+            assertThatThrownBy(() -> winnerNumberVerifier.check(target))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ExceptionMessage.EACH_NOT_NUMERIC);
+        }
+    }
 }
