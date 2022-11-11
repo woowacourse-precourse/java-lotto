1234db967dc3edd2049d2598e2bbce920a972fc8
DongwookKim0823 <dwkim0823@naver.com>
2022-11-12 01:22:03 +0900

    feat(lotto): 사용자에게 입력받은 로또 구매 금액을 검증하는 기능

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -7,6 +7,13 @@ public class LottoView {
     public int getMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         int money = Integer.parseInt(Console.readLine());
+        validateMoney(money);
         return money;
+    }
+
+    private void validateMoney(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
     }
 }
