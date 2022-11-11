6d39820473dfe0f9a75c35b881ac9331377a35da
JJongmen <pjy90123@gmail.com>
2022-11-12 00:54:58 +0900

    refactor: 로또 클래스의 매직 넘버 상수화 및 에러 메시지 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
+    private static final int SIZE = 6;
+    private static final int RANGE_BEGIN = 1;
+    private static final int RANGE_END = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -15,23 +18,23 @@ public class Lotto {
     }
 
     private static void validateSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private static void validateRange(List<Integer> numbers) {
-        for (Integer number : numbers) {
-            if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("[ERROR] 1~45 범위 밖의 숫자가 포함되어 있습니다.");
-            }
+        if (numbers.size() != SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또는 숫자가 6개이어야 합니다.");
         }
     }
 
     private static void validateDuplicates(List<Integer> numbers) {
         Set<Integer> nonDuplicatesNumbers = new HashSet<>(numbers);
-        if (nonDuplicatesNumbers.size() != 6) {
+        if (nonDuplicatesNumbers.size() != SIZE) {
             throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.");
+        }
+    }
+
+    private static void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < RANGE_BEGIN || number > RANGE_END) {
+                throw new IllegalArgumentException("[ERROR] 1~45 범위 밖의 숫자가 포함되어 있습니다.");
+            }
         }
     }
 }
