fa541e750bc937a20cc1c008666015b7336f410b
asas6978 <asas6978@naver.com>
2022-11-11 22:07:59 +0900

    feat: 출력용 안내 문구 추가

---
+++ src/main/java/lotto/GuideMessage.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class GuideMessage {
+    public static final String ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String CONFIRM_PURCHASE_AMOUNT = "개를 구매했습니다.";
+    public static final String ENTER_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    public static final String ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public static final String WINNING_STATISTICS = "당첨 통계";
+    public static final String MATCH = "개 일치";
+    public static final String MATCH_NUMBER = " - %d개";
+}
