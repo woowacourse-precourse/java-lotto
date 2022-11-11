37a2a366156354bdb292118ff1709f71a6c49b84
yejin130 <jinis2023@gmail.com>
2022-11-11 13:17:45 +0900

    docs: 기능목록 및 함수명 작성

---
+++ src/main/java/lotto/Constant.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class Constant {
+    public static final String MESSAGE_INPUT_PAY_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String MESSAGE_INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    public static final String MESSAGE_INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+
+}
