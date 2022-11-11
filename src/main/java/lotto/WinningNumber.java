9e1e85fc2678c03c7f1de0c07a8daeafb12ee125
KyungKyu Park <rudrb0505@gmail.com>
2022-11-11 14:11:25 +0900

    feat(WinningNumber): 구분자가 쉼표가 아닌 경우 예외 발생

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import java.util.StringTokenizer;
+
 public class WinningNumber {
 	public static void checkWinningNumber(String winningNumber) {
-		throw new IllegalArgumentException();
+		StringTokenizer splitter = new StringTokenizer(winningNumber, ",");
+		if (splitter.countTokens() != 6) {
+			throw new IllegalArgumentException();
+		}
 	}
 }
