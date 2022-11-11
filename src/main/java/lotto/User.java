a8bc725f888c477e59e30d9eca4bfedf7e5b00ab
khj <bbb6254177@gmail.com>
2022-11-11 17:37:33 +0900

    feat(lotto): add User class

---
+++ src/main/java/lotto/User.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private int amount;
+    private List<Lotto> lotto = new ArrayList<>();
+
+    public User(int amount) {
+        this.amount = amount;
+    }
+
+    public void buyLotto() {
+
+    }
+}
