ee3585dde55384296e759729133ae2b8488d7443
GwansuShin <happysun7080@naver.com>
2022-11-11 15:42:41 +0900

    feat(Constant): 상수 정의
    프로젝트에서 사용되는 모든 상수 정의

---
+++ src/main/java/lotto/util/Constant.java
@@ -0,0 +1,9 @@
+package lotto.util;
+
+public interface Constant {
+    int THREE_MATCHES = 5_000;
+    int FOUR_MATCHES = 50_000;
+    int FIVE_MATCHES = 1_500_000;
+    int FIVE_WITH_BONUS = 30_000_000;
+    int SIX_MATCHES = 2_000_000_000;
+}
