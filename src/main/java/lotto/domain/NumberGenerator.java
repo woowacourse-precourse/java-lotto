f2bba4c78b9c4a7622f49863771b7150b17806ae
ddingmin <ddingminyt@gmail.com>
2022-11-11 15:50:40 +0900

    feat(NumberGenerator): 로또 번호를 오름차순 하도록 변경

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
@@ -7,6 +8,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class NumberGenerator {
     public List<Integer> createRandomUniqueNumbers() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
         return numbers;
     }
 }
