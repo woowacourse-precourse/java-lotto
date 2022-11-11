86876ff2ac0d3652f0bd2cdaa0994fcc701a4e52
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-11 23:24:26 +0900

    test: 구매한 로또번호와 당첨번호 비교기능 테스트 추가

--- src/test/java/lotto/ManagerTest.java
+++ src/test/java/lotto/ManagerTest.java
@@ -1,8 +1,14 @@
 package lotto;
 
+import java.util.List;
+import java.util.stream.Stream;
+
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -33,6 +39,25 @@ public class ManagerTest {
         assertThat(manager.getLottoTable().size()).isEqualTo(5);
     }
 
+    @DisplayName("로또 번호 비교하기")
+    @ParameterizedTest(name = "{displayName} {0} {1}개 일치")
+    @MethodSource("generateData")
+    void compareLottoNumbers(List<Integer> numbers, int numberOfMatches) {
+        Manager manager = new Manager();
+        Lotto purchasedLotto = new Lotto(numbers);
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThat(manager.compareLottoNumbers(purchasedLotto, winningLotto)).isEqualTo(numberOfMatches);
+    }
+
+    static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 6),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 8), 5),
+                Arguments.of(List.of(11, 12, 13, 14, 15, 16), 0)
+
+        );
+    }
 }
 
 
