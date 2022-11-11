c816c8875abbd6177aec9ac19f495a1b346ddbf1
kevstevie <jianna442@gmail.com>
2022-11-11 12:25:30 +0900

    refactor: inputMoney에 readLine 추가
    input 함수에 readLine이 있는게 직관적으로 맞다고 생각함

--- src/test/java/lotto/UserInputTest.java
+++ src/test/java/lotto/UserInputTest.java
@@ -2,26 +2,38 @@ package lotto;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.io.*;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.*;
 
 class UserInputTest {
-
+    final private String ERROR = "[ERROR]";
     @Test
     void 돈_입력_예외_1() {
-        assertThatThrownBy(() -> UserInput.inputMoney("1100"))
+        String input = "1100";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> UserInput.inputMoney())
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 돈_입력_예외_2() {
-        assertThatThrownBy(() -> UserInput.inputMoney("rrr"))
+        String input = "NotaNumber";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> UserInput.inputMoney())
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 돈_입력_테스트() {
-        long a = UserInput.inputMoney("2000");
+        String input = "2000";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        long a = UserInput.inputMoney();
         assertThat(a).isEqualTo(2000);
     }
 }
