01855ffd1224df475c1fd144307e813ae8155787
cpot5620 <=cpot5620@naver.com>
2022-11-11 22:04:32 +0900

    feat(LottoConstant): add constants

--- src/main/java/lotto/LottoConstant.java
+++ src/main/java/lotto/LottoConstant.java
@@ -1,10 +1,15 @@
 package lotto;
 
 public class LottoConstant {
-    public final static int MIN_LOTTO_NUM = 1;
-    public final static int MAX_LOTTO_NUM = 45;
-    public final static int LOTTO_SIZE = 6;
+    public static final int MIN_LOTTO_NUM = 1;
+    public static final int MAX_LOTTO_NUM = 45;
+    public static final int LOTTO_SIZE = 6;
     public static final int LOTTO_PRICE = 1000;
+    public static final int WINNING_THREE_NUM = 3;
+    public static final int WINNING_FOUR_NUM = 4;
+    public static final int WINNING_FIVE_NUM = 5;
+    public static final int WINNING_SIX_NUM = 6;
+
 
     public static final String PURCHASE_PRICE_MSG = "구매금액을 입력해 주세요.";
     public static final String WINNING_NUM_MSG = "당첨 번호를 입력해 주세요.";
