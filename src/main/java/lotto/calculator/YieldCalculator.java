995892f2963c83d54bf1dc70ce01d583d8a760e1
K-Diger <kitten.diger@gmail.com>
2022-11-11 19:19:53 +0900

    fix : 보너스 번호가 일치하고, 5개가 일치하는 상황에 대한 기능 수정 - getStatistics 메서드에서 보너스 번호가 일치하고 5개가 일치하는 상황에 대한 로직 추가 및 출력문 수정 - getStatistics 메서드의 반환 타입 변경으로 getYield에 반영

--- src/main/java/lotto/calculator/YieldCalculator.java
+++ src/main/java/lotto/calculator/YieldCalculator.java
@@ -6,48 +6,59 @@ import java.util.Map;
 
 public class YieldCalculator {
 
-    public Map<Integer, Integer> getStatistics(List<Integer> matchResult) {
-        Map<Integer, Integer> winInformation = new HashMap<>() {{
-            put(3, 0);
-            put(4, 0);
-            put(5, 0);
-            put(6, 0);
-            put(7, 0);
+    public Map<String, Integer> getStatistics(
+            List<Integer> matchResultNotContainBonusNumber, List<Integer> matchResultContainBonusNumber) {
+        Map<String, Integer> winInformation = new HashMap<>() {{
+            put("three", 0);
+            put("four", 0);
+            put("five", 0);
+            put("six", 0);
+            put("FiveContainingBonusNumber", 0);
         }};
 
-        for (Integer integer : matchResult) {
+        for (Integer integer : matchResultNotContainBonusNumber) {
             if (integer == 3) {
-                winInformation.put(3, (winInformation.get(3) + 1));
+                winInformation.put("three", (winInformation.get("three") + 1));
             } else if (integer == 4) {
-                winInformation.put(4, (winInformation.get(4) + 1));
+                winInformation.put("four", (winInformation.get("four") + 1));
             } else if (integer == 5) {
-                winInformation.put(5, (winInformation.get(5) + 1));
+                winInformation.put("five", (winInformation.get("five") + 1));
             } else if (integer == 6) {
-                winInformation.put(6, (winInformation.get(6) + 1));
+                winInformation.put("six", (winInformation.get("six") + 1));
             }
         }
+
+        for (Integer integer : matchResultContainBonusNumber) {
+            if (integer == 5) {
+                winInformation.put("FiveContainingBonusNumber", (winInformation.get("FiveContainingBonusNumber") + 1));
+            }
+        }
+
+        System.out.println("winInformation = " + winInformation);
+
         System.out.println("\n당첨 통계\n---");
-        System.out.printf("3개 일치 (5,000원) - %d개\n", winInformation.get(3));
-        System.out.printf("4개 일치 (50,000원) - %d개\n", winInformation.get(4));
-        System.out.printf("5개 일치 (1,500,000원) - %d개\n", winInformation.get(5));
-        System.out.printf("5개 일치 보너스 볼 일치 (30,000,000원) - %d개\n", winInformation.get(6));
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", winInformation.get(7));
+        System.out.printf("3개 일치 (5,000원) - %d개\n", winInformation.get("three"));
+        System.out.printf("4개 일치 (50,000원) - %d개\n", winInformation.get("four"));
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", winInformation.get("five"));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", winInformation.get("FiveContainingBonusNumber"));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", winInformation.get("six"));
 
         return winInformation;
     }
 
-    public double countMatchResult(Map<Integer, Integer> winInformation, int purchaseMoney) {
+    public double countMatchResult(Map<String, Integer> winInformation, int purchaseMoney) {
         int result = 0;
 
-        result += winInformation.get(3) * 5000;
-        result += winInformation.get(4) * 50000;
-        result += winInformation.get(5) * 1500000;
-        result += winInformation.get(6) * 2000000000;
+        result += winInformation.get("three") * 5000;
+        result += winInformation.get("four") * 50000;
+        result += winInformation.get("five") * 1500000;
+        result += winInformation.get("FiveContainingBonusNumber") * 30000000;
+        result += winInformation.get("six") * 2000000000;
 
         return (double) result / (double) purchaseMoney * 100;
     }
 
-    public void getYield(Map<Integer, Integer> winInformation, int purchaseNumber) {
+    public void getYield(Map<String, Integer> winInformation, int purchaseNumber) {
         double yield = countMatchResult(winInformation, (purchaseNumber * 1000));
         System.out.printf("총 수익률은 %.1f%%입니다.", yield);
     }
