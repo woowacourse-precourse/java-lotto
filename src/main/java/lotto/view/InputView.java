cc065b32843cf1ac2a66ea296aa2da1da834cb9a
parkjungyoon <tbr06057@naver.com>
2022-11-11 20:21:44 +0900

    feat(View): 로또 당첨번호, 보너스 번호 입출력 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,8 +2,7 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import static lotto.validator.NumberValidator.validateNonNumeric;
-import static lotto.validator.NumberValidator.validateUnit;
+import static lotto.validator.NumberValidator.*;
 
 public class InputView {
     public static int inputLottoPurchaseAmount() {
@@ -12,6 +11,21 @@ public class InputView {
 
         validateNonNumeric(input);
         validateUnit(input);
+        return Integer.parseInt(input);
+    }
+
+    public static String inputLottoNumbers() {
+        OutputView.printLottoNumbers();
+        String input = Console.readLine();
+        return input;
+    }
+
+    public static int inputBonusNumber() {
+        OutputView.printLottoBonusNumber();
+        String input = Console.readLine();
+
+        validateNonNumeric(input);
+        validateRange(Integer.parseInt(input));
         return Integer.parseInt(input);
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,6 +7,8 @@ import java.util.List;
 public class OutputView {
     private final static String LOTTO_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
     private final static String LOTTO_PURCHASE_COMPLETE_MESSAGE = "%d개를 구매했습니다.";
+    private final static String LOTTO_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private final static String LOTTO_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static void printLottoPurchaseAmount() {
         System.out.println(LOTTO_PURCHASE_MESSAGE);
@@ -20,5 +22,13 @@ public class OutputView {
         for (Lotto lotto : purchaseLotto) {
             System.out.println(lotto.toString());
         }
+    }
+
+    public static void printLottoNumbers() {
+        System.out.println(LOTTO_NUMBERS_MESSAGE);
+    }
+
+    public static void printLottoBonusNumber() {
+        System.out.println(LOTTO_BONUS_NUMBER_MESSAGE);
     }
 }
