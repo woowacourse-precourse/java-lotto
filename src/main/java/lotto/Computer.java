c28feff4ec3b3a3fad3428d24e1b9078431b8779
hingjae <ljh23270@naver.com>
2022-11-11 15:49:17 +0900

    당첨 번호 입력

--- src/main/java/lotto/Computer.java
+++ src/main/java/lotto/Computer.java
@@ -1,10 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Computer {
 
@@ -39,5 +38,73 @@ public class Computer {
         for (List lotto : lottoList) {
             System.out.println(lotto);
         }
+    }
+
+    public List inputWinningNumber () {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<Integer> winnigNumberList = new ArrayList<>();
+        String number = inputNumber();
+        winnigNumberList = convertToList(splitNumber(number));
+        return winnigNumberList;
+    }
+
+    public String inputNumber() {
+        String number = Console.readLine();
+        return number;
+    }
+
+    public int[] splitNumber(String number) {
+        String[] numberArray = number.split(",");
+        int[] intArray = isValid(numberArray);
+        return intArray;
+    }
+
+    public int[] isValid(String[] numberArray) {
+        isNum(numberArray);
+        int[] intArray = isCorrectRange(numberArray);
+        isCorrectSize(intArray);
+        return intArray;
+    }
+
+    public void isNum(String[] numberArray) {
+        for(String number : numberArray){
+            findText(number);
+        }
+    }
+
+    public void findText(String number) {
+        for(char item : number.toCharArray()) {
+            if(!Character.isDigit(item)) {
+                throw new IllegalArgumentException("[ERROR] 숫자만 입력할 수 있습니다.");
+            }
+        }
+    }
+
+    public int[] isCorrectRange(String[] numberArray){
+        int[] intArray = Arrays.stream(numberArray).mapToInt(Integer::parseInt).toArray();
+        for(int number : intArray){
+            if(!(number >= 1 && number <= 45)){
+                throw new IllegalArgumentException("[ERROR] 1부터 45까지의 숫자만 입력할 수 있습니다.");
+            }
+        }
+        return intArray;
+    }
+
+    public void isCorrectSize(int[] intArray) {
+        Set sizeAndDupCheck = new HashSet<>();
+        for(int number : intArray){
+            sizeAndDupCheck.add(number);
+        }
+        if(sizeAndDupCheck.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복되는 숫자를 입력할 수 없고, 6개의 숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    public List convertToList(int[] numberArray) {
+        List<Integer> numberList = new ArrayList<>();
+        for(int number : numberArray){
+            numberList.add(number);
+        }
+        return numberList;
     }
 }
