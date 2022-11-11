3dc9e06a89bd383ff3754c78521d5c5f1018236e
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-11 17:39:51 +0900

    feat(Statistics): 구입 금액이 1000원으로 나누어 떨어지지 않는 예외 처리

--- src/main/java/lotto/domain/Statistics.java
+++ src/main/java/lotto/domain/Statistics.java
@@ -8,6 +8,7 @@ public class Statistics {
 
     public Statistics(int purchaseAmount) {
         hasDigitsOnly(purchaseAmount);
+        isDividedByLottoAmount(purchaseAmount);
         this.purchaseAmount = purchaseAmount;
     }
 
@@ -15,6 +16,13 @@ public class Statistics {
         String money = String.valueOf(purchaseAmount);
         if (!money.matches(REG_XP_DIGITS)) {
             System.out.println("[ERROR] 금액은 0-9까지의 숫자로만 이루어저야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isDividedByLottoAmount(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            System.out.println("[ERROR] 금액은 로또 한장의 가격인 1000원 단위로만 입력 가능합니다.");
             throw new IllegalArgumentException();
         }
     }
