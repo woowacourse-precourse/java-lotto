ed51302fd409bce6608e222eb0ec73c4b227cab1
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-11 22:18:29 +0900

    feat: 보너스 번호 소유 여부 확인 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -22,4 +22,8 @@ public class WinningLotto extends Lotto {
             throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE);
         }
     }
+
+    public boolean containBonusNumber(Lotto lotto) {
+        return lotto.isContainNumber(bonusNumber);
+    }
 }
