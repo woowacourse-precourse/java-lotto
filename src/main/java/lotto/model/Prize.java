980a0e9a1ffedc1f4cbb24315f7f648f1d63dfe3
dhkim <akdlzmf1123@gmail.com>
2022-11-11 20:37:12 +0900

    feat(Prize): 당첨 등수에서 상금 구하는 기능 추가

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -4,14 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum Prize {
-    FIRST,
-    SECOND,
-    THIRD,
-    FOURTH,
-    FIFTH,
-    NO_PRIZE;
+    FIRST(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FOURTH(50_000),
+    FIFTH(5_000),
+    NO_PRIZE(0);
 
     private static final Map<CompareResult, Prize> compareResultToPrize = new HashMap<>();
+    private final int money;
 
     static {
         compareResultToPrize.put(new CompareResult(6, false), FIRST);
@@ -22,7 +23,15 @@ public enum Prize {
         compareResultToPrize.put(new CompareResult(3, true), FIFTH);
         compareResultToPrize.put(new CompareResult(3, false), FIFTH);
     }
+
+    private Prize(int money) {
+        this.money = money;
+    }
     public static Prize toPrize(CompareResult lottoResult) {
         return compareResultToPrize.getOrDefault(lottoResult, NO_PRIZE);
+    }
+
+    public int getMoney() {
+        return money;
     }
 }
--- src/test/java/lotto/model/PrizeTest.java
+++ src/test/java/lotto/model/PrizeTest.java
@@ -1,7 +1,9 @@
 package lotto.model;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -15,9 +17,16 @@ class PrizeTest {
         assertThat(Prize.toPrize(lottoResult)).isEqualTo(prize);
     }
 
+    @ParameterizedTest(name = "{0} 당첨 상금 {1}원")
+    @CsvSource({"FIRST,2_000_000_000", "SECOND,30_000_000", "THIRD,1_500_000", "FOURTH,50_000", "FIFTH,5_000"})
+    void 당첨_상금_반환_기능(String prize, int moneyAmount) {
+        assertThat(Prize.valueOf(prize).getMoney()).isEqualTo(moneyAmount);
+    }
+
     static Stream<Arguments> lottoResultAndPrize() {
+
         return Stream.of(
-            Arguments.of(new CompareResult(6, false), Prize.FIRST)
+                Arguments.of(new CompareResult(6, false), Prize.FIRST)
                 , Arguments.of(new CompareResult(5, true), Prize.SECOND)
                 , Arguments.of(new CompareResult(5, false), Prize.THIRD)
                 , Arguments.of(new CompareResult(4, true), Prize.FOURTH)
