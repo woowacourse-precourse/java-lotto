62e845369dd5116c681c913f4a8e6da546db75cc
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-11 19:05:31 +0900

    test(Validatetest) : 예외처리 적용 후 테스트 케이스 수정

--- src/test/java/lotto/ValidateTest.java
+++ src/test/java/lotto/ValidateTest.java
@@ -3,6 +3,9 @@ package lotto;
 import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class ValidateTest {
 
     @Test
@@ -22,9 +25,10 @@ class ValidateTest {
     }
     @Test
     void validateBonusNumber() {
-        String testcase = "123";
+        String testcase = "1";
+        List<Integer> testcase1 = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
         Validate test = new Validate();
-        assertThatThrownBy(() -> test.validateBonusNumber(testcase))
+        assertThatThrownBy(() -> test.validateBonusNumber(testcase1, testcase))
                 .isInstanceOf(IllegalArgumentException.class);
     }
     @Test
