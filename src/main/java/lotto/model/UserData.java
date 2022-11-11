ec8f86ef2602fb81e15532add911f1f279063b0b
정승구 <jeongseung-gu@jeongseung-guui-MacBookAir.local>
2022-11-11 22:25:02 +0900

    feat: UserData클래스 생성 및 내부 변수 선언

--- src/main/java/lotto/model/UserData.java
+++ src/main/java/lotto/model/UserData.java
@@ -1,2 +1,9 @@
-package lotto.model;public class UserData {
+package lotto.model;
+
+import java.util.List;
+
+public class UserData {
+    private int purchaseAmount;
+    private int gameCount;
+    private List<Lotto> games;
 }
