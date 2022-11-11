9cb01faaaf6b85dcc77d982735d4b0f4ad34a667
Kim woo jong <skekdnwhd@gmail.com>
2022-11-12 02:16:23 +0900

    Test : 당첨번호 등록 서비스 테스트케이스 추가

--- src/test/java/lotto/service/RegisterWinningNumberServiceTest.java
+++ src/test/java/lotto/service/RegisterWinningNumberServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class RegisterWinningNumberServiceTest extends NsTest {
     RegisterWinningNumberService registerWinningNumberService;
@@ -27,6 +29,17 @@ class RegisterWinningNumberServiceTest extends NsTest {
         assertThat(winningLotto)
                 .usingRecursiveComparison()
                 .isEqualTo(new WinningLotto("1,2,3,4,5,6", "7"));
+    }
+
+    @Test
+    @DisplayName("당첨번호에 보너스번호가 포함될 때 exception과 에러메세지가 출력 되야 한다.")
+    void test2() {
+        // given
+        run("1,2,3,4,5,6", "6");
+
+        // expected
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> registerWinningNumberService.register());
+        assertEquals("[ERROR] 보너스 번호에 중복 된 숫자가 포함되어있습니다.", exception.getMessage());
     }
 
     @Override
