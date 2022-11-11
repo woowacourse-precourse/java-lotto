908141bf06af3986b075196a19d2b3066dabbb20
unknown <ymecca12@gmail.com>
2022-11-12 04:08:42 +0900

    feat : 중복 검사, 범위 검사

--- src/main/java/lotto/input/UserInput.java
+++ src/main/java/lotto/input/UserInput.java
@@ -6,30 +6,41 @@ import lotto.validate.Validator;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserInput implements Input {
-    @Override
-    public void readInput() {
-        //    구매 금액 입력
-        System.out.println("구입금액을 입력해 주세요.");
-        String purchasePrice = Console.readLine();
-        Validator.validateModIsZero(Integer.parseInt(purchasePrice));
-        Integer purchaseAmount = Integer.parseInt(purchasePrice) / 1000;
-        System.out.println(purchaseAmount + "개를 구매했습니다." + "\n");
-        //    구매 로또 번호 출력
+public class UserInput {
+    private List<Integer> getBounsNumber(List<Integer> prizeNumbers) {
+        //    보너스 번호 입력
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        String bonusValue = Console.readLine();
+        Integer bounusNumber = Integer.parseInt(bonusValue);
+        Validator.validateOutOfBound(bounusNumber);
+        prizeNumbers.add(bounusNumber);
+        return prizeNumbers;
+    }
 
+    private List<Integer> getPrizeNumber() {
         //    당첨 번호 입력
         System.out.println("당첨 번호를 입력해 주세요.");
         String prizeInput = Console.readLine();
         String[] prizeString = prizeInput.split(",");
         List<Integer> prizeNumbers = new ArrayList<>();
         for (String str : prizeString) {
-            Integer i = Integer.parseInt(str);
-            prizeNumbers.add(i);
+            Integer value = Integer.parseInt(str);
+            prizeNumbers.add(value);
         }
-        Validator.validateOutOfBound(prizeNumbers);
-        //    보너스 번호 입력
-        System.out.println("\n보너스 번호를 입력해 주세요.");
-        String bonusNumber = Console.readLine();
+        Validator.validateOutOfSize(prizeNumbers);
+        Validator.validateOutOfBoundList(prizeNumbers);
+        Validator.validateDuplicateData(prizeNumbers);
+        return prizeNumbers;
+    }
+
+    private Integer getPurchasePrice() {
+        //    구매 금액 입력
+        System.out.println("구입금액을 입력해 주세요.");
+        String purchasePrice = Console.readLine();
+        Validator.validateModIsZero(Integer.parseInt(purchasePrice));
+        Integer purchaseAmount = Integer.parseInt(purchasePrice) / 1000;
+        System.out.println(purchaseAmount + "개를 구매했습니다." + "\n");
+        return purchaseAmount;
     }
 
 }
--- src/main/java/lotto/validate/Validator.java
+++ src/main/java/lotto/validate/Validator.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 public class Validator {
     private static final String ERROR_MESSAGE = "[ERROR] : ";
-    private static final String MOD_IS_ZERO = "1,000원 단위로 ";
+    private static final String MOD_IS_ZERO = "1,000원 단위로 입력해주세요";
     private static final String OUT_OF_BOUND = "1~45 사이의 숫자를 입력해주세요";
+    private static final String OUT_OF_SIZE = "6개의 데이터만 입력해주세요";
+    private static final String DUPLICATED_MESSAGE = "중복된 데이터가 입력됐습니다";
+
 
     public static String generateErrorMessage(String input){
         return ERROR_MESSAGE + input;
@@ -16,11 +19,28 @@ public class Validator {
         }
     }
 
-    public static void validateOutOfBound(List<Integer> prizeNumbers) {
+    public static void validateOutOfBoundList(List<Integer> prizeNumbers) {
         for (Integer prizeNumber : prizeNumbers) {
             if(prizeNumber < 1 && prizeNumber > 45){
                 throw new IllegalArgumentException(generateErrorMessage(OUT_OF_BOUND));
             }
+        }
+    }
+    public static void validateOutOfBound(Integer number){
+        if(number < 1 && number > 45){
+            throw new IllegalArgumentException(generateErrorMessage(OUT_OF_BOUND));
+        }
+    }
+
+    public static void validateOutOfSize(List<Integer> prizeNumbers) {
+        if(prizeNumbers.size()>6){
+            throw new IllegalArgumentException(generateErrorMessage(OUT_OF_SIZE));
+        }
+    }
+
+    public static void validateDuplicateData(List<Integer> prizeNumbers) {
+        if(prizeNumbers.size() != prizeNumbers.stream().distinct().count()){
+            throw new IllegalArgumentException(generateErrorMessage(DUPLICATED_MESSAGE))
         }
     }
 }
