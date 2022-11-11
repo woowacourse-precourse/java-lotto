fbebed7be1f701786afef612baf5610a1531a71d
cpot5620 <=cpot5620@naver.com>
2022-11-12 04:29:54 +0900

    refactor(WinningType): change type's message

--- src/main/java/lotto/WinningType.java
+++ src/main/java/lotto/WinningType.java
@@ -3,11 +3,11 @@ package lotto;
 import static lotto.LottoConstant.*;
 
 public enum WinningType {
-    THREE("3개 일치 (5,000원) - ", WINNING_THREE_MONEY),
-    FOUR("4개 일치 (50,000원) - ", WINNING_FOUR_MONEY),
-    FIVE("5개 일치 (1,500,000원) - ", WINNING_FIVE_MONEY),
-    FIVE_AND_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - ", WINNING_FIVE_AND_BONUS_MONEY),
-    SIX("6개 일치 (2,000,000,000원) - ", WINNING_SIX_MONEY);
+    THREE("3개 일치 (5,000원) - %d개", WINNING_THREE_MONEY),
+    FOUR("4개 일치 (50,000원) - %d개", WINNING_FOUR_MONEY),
+    FIVE("5개 일치 (1,500,000원) - %d개", WINNING_FIVE_MONEY),
+    FIVE_AND_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개", WINNING_FIVE_AND_BONUS_MONEY),
+    SIX("6개 일치 (2,000,000,000원) - %d개", WINNING_SIX_MONEY);
 
     private final String message;
     private final int money;
