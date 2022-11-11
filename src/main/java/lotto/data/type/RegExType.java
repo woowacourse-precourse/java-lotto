8154e2d321a3612f5cd555c10622e487902ae1f8
TaemHam <tk4003950@gmail.com>
2022-11-11 11:45:25 +0900

    Feat: lotto.data.type.RegExType 구현

--- src/main/java/lotto/data/type/RegExType.java
+++ src/main/java/lotto/data/type/RegExType.java
@@ -1,2 +1,15 @@
-package lotto.data.type;public enum RegExType {
+package lotto.data.type;
+
+public enum RegExType {
+    LOTTO_ANSWER("(\\s*\\d\\s*,){5}\\s*\\d\\s*");
+
+    private final String pattern;
+
+    RegExType(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getPattern() {
+        return this.pattern;
+    }
 }
