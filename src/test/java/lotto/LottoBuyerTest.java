565672f03fc0f995a3f4547a2907b875c9e28f41
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-12 00:45:11 +0900

    Feat(LottoBuyerTest) : 구매 가격 입력 시 테스트 코드 추가 #45
    LottoBuyerTest클래스의 inputMoney() 메소드의 테스트 코드가 추가되었습니다.
    
     Ref : #44

---
+++ src/test/java/lotto/LottoBuyerTest.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import org.junit.jupiter.api.*;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import camp.nextstep.edu.missionutils.test.NsTest;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.math.BigInteger;
+
+class LottoBuyerTest extends NsTest{
+
+    public static String ERROR_MESSAGE = "[ERROR]";
+
+    //성공했을때 확인 필요
+    @Test
+    @DisplayName("로또를 구매할 돈을 입력 확인")
+    void 로또를_구매할_돈을_입력_테스트(){
+
+        assertThatThrownBy(() ->run("8000j"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+
+        assertThatThrownBy(() ->run("-8000"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+
+        assertThatThrownBy(() ->run("1200"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+    public void runMain() {
+        LottoBuyer.inputMoney();
+    }
+}
