2f39750f56076c8691803d4a45bc9d55fcda57f2
kdkdhoho <hkim4410@naver.com>
2022-11-11 11:50:53 +0900

    feat(Message): 일부 상수명 변경 및 NOT_THOUSAND_UNIT_INPUT_ERROR 추가

--- src/main/java/lotto/domain/enums/Message.java
+++ src/main/java/lotto/domain/enums/Message.java
@@ -7,7 +7,8 @@ public enum Message {
     RESULT_THIRD("5개 일치 (1,500,000원) - "),
     RESULT_SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
     RESULT_FIRST("6개 일치 (2,000,000,000원) - "),
-    ERROR_MESSAGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    NUMBER_RANGE_ERROR("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    NOT_THOUSAND_UNIT_INPUT_ERROR("[ERROR] 입력 금액은 1,000원 단위입니다."),
     YIELD_MESSAGE("총 수익률은 %.1d%입니다.\n");
 
     private final String message;
