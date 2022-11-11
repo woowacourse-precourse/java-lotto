dfbc145b45069a57ad7721e1de324d93ee5a022a
Your Name <you@example.com>
2022-11-11 14:13:36 +0900

    feat(Lotto): 로또 출력 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -7,14 +8,30 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkDuplication(numbers);
         this.numbers = numbers;
+    }
+
+    private void checkDuplication(List<Integer> numbers) {
+        List<Integer> tempNum = new ArrayList<>();
+        
+        numbers.stream().forEach(num -> {
+            if(tempNum.contains(num))
+                throw new IllegalArgumentException("[ERROR] 중복된 숫자를 입력하였습니다.");
+            tempNum.add(num);
+        });
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해주세요.");
         }
     }
 
+    public void printLotto() {
+        System.out.println(numbers);
+    }
+
     // TODO: 추가 기능 구현
+    
 }
