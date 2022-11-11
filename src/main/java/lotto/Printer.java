4b1e9e80a1546a7c54b1e0c4b9badb07f6001981
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-12 00:40:28 +0900

    feat : 전체 당첨 정보를 출력하는 기능 구현
    printResult()구현

--- src/main/java/lotto/Printer.java
+++ src/main/java/lotto/Printer.java
@@ -24,4 +24,14 @@ public class Printer {
         System.out.println(list);
     }
 
+    public void printResult(int[] winning, double yield) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.printf(Rank.FIFTH.getValue()+"(%d원) - "+winning[0]+"개%n", new Result().FIFTH_PRIZE);
+        System.out.printf(Rank.FOURTH.getValue()+"(%d원) - "+winning[1]+"개%n", new Result().FOURTH_PRIZE);
+        System.out.printf(Rank.THIRD.getValue()+"(%d원) - "+winning[2]+"개%n", new Result().THIRD_PRIZE);
+        System.out.printf(Rank.SECOND.getValue()+"(%d원) - "+winning[3]+"개%n", new Result().SECOND_PRIZE);
+        System.out.printf(Rank.FIRST.getValue()+"(%d원) - "+winning[4]+"개%n", new Result().FIRST_PRIZE);
+        System.out.print("총 수익률은 "+String.format("%,.1f",yield)+ "%입니다.");
+    }
 }
