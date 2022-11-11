1c6a41aebe0bfde883ea176159ce04a2e7b8016e
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-11 13:52:02 +0900

    feat: 랜덤 로또를 여러개 만드는 기능 구현

--- src/main/java/lotto/domain/LottoMaker.java
+++ src/main/java/lotto/domain/LottoMaker.java
@@ -1,4 +1,22 @@
 package lotto.domain;
 
-public class LottoMachine {
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMaker {
+
+    RandomLotto randomLotto = new RandomLotto();
+
+    public List<Lotto> getRandomLottos(int lottoTicket) {
+        List<Lotto> resultLottos = new ArrayList<>();
+        for (int i = 0; i < lottoTicket; i++) {
+            resultLottos.add(getSingleRandomLotto());
+        }
+        return resultLottos;
+    }
+
+    public Lotto getSingleRandomLotto() {
+        return randomLotto.getRandomLotto();
+    }
+
 }
