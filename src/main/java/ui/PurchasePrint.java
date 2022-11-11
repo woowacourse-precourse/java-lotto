6ab6ddfb616b4670558a19d8cb1d5387cdbc1888
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-11 14:04:09 +0900

    feat(PurchasePrint) : 출력 기능 추가.

--- src/main/java/ui/PurchasePrint.java
+++ src/main/java/ui/PurchasePrint.java
@@ -1,7 +1,22 @@
 package ui;
+import lotto.Lotto;
+import java.util.List;
+import java.util.SimpleTimeZone;
 
 public class PurchasePrint {
     public static void intputMoney(){
         System.out.println("구입금액을 입력해 주세요");
+    }
+    public static void checkPurchase(int count){
+        System.out.println(count +"개를 구매했습니다.");
+    }
+    public static String printLottoNumber(List<Lotto> numbers){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<numbers.size(); i++) {
+            sb.append(numbers.get(i).toString());
+            if(i!=numbers.size()-1)
+            sb.append("\n");
+        }
+       return sb.toString();
     }
 }
