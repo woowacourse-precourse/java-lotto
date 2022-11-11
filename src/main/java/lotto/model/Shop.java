c72da54ec9111079abefe8eabd4e23b60848f55d
minaver <psmjoshua@gmail.com>
2022-11-11 21:34:00 +0900

    feat(Shop): Shop Class 생성

---
+++ src/main/java/lotto/model/Shop.java
@@ -0,0 +1,31 @@
+package lotto.model;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+public class Shop {
+    private int price;
+    private int lottoSize;
+    private List<Lotto> lottoPayed;
+
+    public Shop(int price){
+        this.price = price;
+    }
+
+    public List<Lotto> getlottoPayed() {
+        return null;
+    }
+
+    private void calcLottoSize(int price) {
+
+    }
+
+    private void setLottoPayed(int lottoSize) {
+
+    }
+
+    private void priceValidate(int price) {
+        
+    }
+
+}
