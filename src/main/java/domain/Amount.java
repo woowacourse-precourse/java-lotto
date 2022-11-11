44fe717a5f83abd35404d5c705bd9364ed7b68a6
glass <glass7312@gmail.com>
2022-11-11 21:14:23 +0900

    feat: getGrade 기능추가(Amount class) 맞은 번호 횟수별 등수를 반환하는 기능 추가

--- src/main/java/domain/Amount.java
+++ src/main/java/domain/Amount.java
@@ -5,8 +5,22 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Amount {
-    enum grade {first, second, third, fourth, fifth}
+    public static final int THREE_HIT = 3;
+    public static final int FOUR_HIT = 4;
+    public static final int FIVE_HIT = 5;
+    public static final int SIX_HIT = 6;
+
+    enum grade {first, second, third, fourth, fifth,lose}
 
     private final List<grade> winning_amount = Arrays.asList(grade.first, grade.second, grade.third, grade.fourth, grade.fifth);
 
+    public static grade getGrade(int hitCount, boolean bonus_hit) {
+        if (hitCount == THREE_HIT) return grade.fifth;
+        if (hitCount == FOUR_HIT) return grade.fourth;
+        if (hitCount == FIVE_HIT && !bonus_hit) return grade.third;
+        if (hitCount == FIVE_HIT && bonus_hit) return grade.second;
+        if (hitCount == SIX_HIT) return grade.first;
+        else return grade.lose;
+
+    }
 }
