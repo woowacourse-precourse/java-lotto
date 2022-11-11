5ccc504ef7fa28f3ac4daee59f149fd9e2565883
yeonsu <jack0810@kookmin.ac.kr>
2022-11-11 18:51:17 +0900

    fix($Application) functions
    fix functions output type

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static int getMoney(){
@@ -13,9 +14,7 @@ public class Application {
         int money = Integer.parseInt((inputMoney));
         isSplit(money);
 
-        int lottoCnt;
-        lottoCnt = money / 1000;
-        return lottoCnt;
+        return money;
     }
     public static void isSplit(int money){
         if (money % 1000 != 0){
@@ -25,31 +24,27 @@ public class Application {
     public static void isInteger(String inputMoney){
         for(int i = 0; i < inputMoney.length(); i++){
             if (inputMoney.charAt(i) < '0' || inputMoney.charAt(i) > '9' ){
+                System.err.println("[ERROR]");
                 throw new IllegalArgumentException("다시 입력 해주세요");
             }
         }
     }
-    public static ArrayList<Integer> generateLotto(){ // 숫자 생성
-        ArrayList<Integer> lotto = new ArrayList<>();
-        while (lotto.size() < 8) {
-            int randomNumber = Randoms.pickNumberInRange(1, 45);
-            if (!lotto.contains(randomNumber)) {
-                lotto.add(randomNumber);
-            }
-        }
+    public static List<Integer> generateLotto(){ // 숫자 생성
+        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1,45,6);
         return lotto;
     }
 
-    public static void printLotto(ArrayList<Integer> lotto){
+    public static void printLotto(List<Integer> lotto){
         System.out.println(lotto);
     }
 
-    public static ArrayList<Integer> getWinningNum() {
+    public static List<Integer> getWinningNum() {
         String inputNum= Console.readLine();
-        validateNum(inputNum);
 
+        validateNum(inputNum);
         String[] inputNums = inputNum.split(",");
-        ArrayList<Integer> winningNums = new ArrayList<>();
+
+        List<Integer> winningNums = new ArrayList<>();
         for(String num:inputNums){
             winningNums.add(Integer.parseInt(num.trim()));
         }
@@ -68,7 +63,7 @@ public class Application {
         }
     }
 
-    public static void validateNumCnt(ArrayList<Integer> winningNums){
+    public static void validateNumCnt(List<Integer> winningNums){
         if (winningNums.size() != 6){
             throw new IllegalArgumentException();
         }
@@ -84,16 +79,32 @@ public class Application {
 
     public static void validateBonusNum(String bonusNum){
         if(bonusNum.length() != 1){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자 하나만 입력해 주세요");
         }
         if(bonusNum.charAt(0) < '0' || bonusNum.charAt(0) > '9'){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자를 입력해 주세요");
         }
+    }
+
+    public static void execute(){
+        int money = getMoney();
+        int lottoCnt = money / 1000;
+        String output = "";
+        output += lottoCnt + "개를 구매했습니다.";
+        System.out.println(output);
+        List<Integer> lotto;
+        List<List<Integer>> generatedLottos = new ArrayList<>();
+        for (int i = 0; i < lottoCnt; i++){
+            lotto = generateLotto();
+            printLotto(lotto);
+            generatedLottos.add(lotto);
+        }
+
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        execute();
     }
 
 }
