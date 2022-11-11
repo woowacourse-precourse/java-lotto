a4978bf7fdadbafeecfb4e049709c67f1d4d2271
jthugg <jthug@naver.com>
2022-11-12 05:33:26 +0900

    Feat: 전체 시퀀스 구성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,39 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        try {
+            Status status = new Status();
+            Request request = new Request();
+
+            status.setStatus("request money");
+            request.printRequestMessageByStatus(status.getStatus());
+            request.requestUserInput();
+            request.validateByStatus(status.getStatus());
+
+            UserBet userBet = new UserBet(request.getUserInput());
+
+            status.setStatus("request winning numbers");
+            request.printRequestMessageByStatus(status.getStatus());
+            request.requestUserInput();
+            request.validateByStatus(status.getStatus());
+
+            Lotto lotto = new Lotto(request.getWinningNumbers());
+
+            status.setStatus("request bonus number");
+            request.printRequestMessageByStatus(status.getStatus());
+            request.requestUserInput();
+            request.validateByStatus(status.getStatus());
+
+            BonusNumber bonusNumber = new BonusNumber(request.getUserInput());
+
+            Compare compare = new Compare();
+            compare.compareNumbers(userBet.getMyLottos(), lotto.getWinningNumbers(), bonusNumber.getBonusNumber());
+            compare.sumWinnings();
+            compare.figureYield(userBet.getBettingMoney());
+            compare.printResult();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
