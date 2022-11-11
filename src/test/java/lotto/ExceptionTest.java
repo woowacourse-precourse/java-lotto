b89073ad1e7fcc954e5b821d69eec159f3e17cea
소대훈 <theogns0681@naver.com>
2022-11-11 10:13:36 +0900

    [test] 테스트케이스 추가

---
+++ src/test/java/lotto/ExceptionTest.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ExceptionTest {
+    @DisplayName("1000원 단위가 아닌 숫자를 입력하면 예외가 발생한다.")
+    @Test
+    void inputIncorrectMoney(){
+        Exception ex = new Exception();
+        assertThatThrownBy(() -> ex.initialInput("1000001"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("숫자를 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void inputOnlyNumber(){
+        Exception ex = new Exception();
+        assertThatThrownBy(() -> ex.initialInput("111111j"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("보너스로 숫자를 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void inputBonusOnlyNumber(){
+        Exception ex = new Exception();
+        assertThatThrownBy(() -> ex.inputWinningBonus("10jj"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("보너스로 1~45범위 숫자를 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void inputBonusOnlyRange(){
+        Exception ex = new Exception();
+        assertThatThrownBy(()->ex.inputWinningBonus("46"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
