635c4e05b4218b614882739c08cc281aab278645
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-11 17:49:05 +0900

    feat(InputUi): 당첨 번호 입력받기
    - 당첨번호 string입력받기
    - 입력받은 string을 integer list로 변환하기

--- src/main/java/lotto/ui/InputUi.java
+++ src/main/java/lotto/ui/InputUi.java
@@ -3,7 +3,13 @@ package lotto.ui;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.InputValidator;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class InputUi {
+
+    private static String NUMBER_INPUT_DIVIDER = ",";
 
     public static int readPurchaseAmount() throws IllegalArgumentException {
         String amountInput = Console.readLine();
@@ -14,4 +20,23 @@ public class InputUi {
 
         return purchaseAmount;
     }
+
+    public static List<Integer> readWinningLottoNumbers() {
+        String winningNumbersInput = Console.readLine();
+
+        List<Integer> winningNumbers = splitNumbersStringToList(winningNumbersInput);
+        return Collections.unmodifiableList(winningNumbers);
+    }
+
+    private static List<Integer> splitNumbersStringToList(String numberInput){
+        String[] dividedNumbersInput = numberInput.split(NUMBER_INPUT_DIVIDER);
+
+        List<Integer> numbers = new ArrayList<>();
+        for(String number : dividedNumbersInput){
+            numbers.add(Integer.valueOf(number));
+        }
+
+        return Collections.unmodifiableList(numbers);
+    }
+
 }
