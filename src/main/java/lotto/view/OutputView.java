8f50f9d5cd9fd6d07a6716a94c980985d898e771
CMHyuk <alsgur9710@naver.com>
2022-11-11 20:47:52 +0900

    feat(OutputView): 로또 당첨 통계 출력문

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,32 @@
 package lotto.view;
 
+import lotto.constant.Score;
+
 import java.util.List;
+import java.util.Map;
+
+import static lotto.constant.Score.*;
 
 public class OutputView {
 
+    private static final String THREE_CORRECT = "3개 일치 (5,000원) - ";
+    private static final String FOUR_CORRECT = "4개 일치 (50,000원) - ";
+    private static final String FIVE_CORRECT = "5개 일치 (1,500,000원) - ";
+    private static final String FIVE_BONUS_CORRECT = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    private static final String SIX_CORRECT = "6개 일치 (2,000,000,000원) - ";
+    private static final String COUNT = "개";
+    private static final String STATISTICS = "당첨 통계\n" + "---";
+
     public static void printRandomLottoNumbers(List<List<Integer>> randomLottoNumbers) {
         randomLottoNumbers.forEach(System.out::println);
+    }
+
+    public static void printStatistics(Map<Score, Integer> scoreStore) {
+        System.out.println(STATISTICS);
+        System.out.println(THREE_CORRECT + scoreStore.get(THREE) + COUNT);
+        System.out.println(FOUR_CORRECT + scoreStore.get(FOUR) + COUNT);
+        System.out.println(FIVE_CORRECT + scoreStore.get(FIVE) + COUNT);
+        System.out.println(FIVE_BONUS_CORRECT + scoreStore.get(FIVE_BONUS) + COUNT);
+        System.out.println(SIX_CORRECT + scoreStore.get(SIX) + COUNT);
     }
 }
