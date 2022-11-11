13e7c0da49229a65fc15314f87c67c483aa47c41
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-11 12:46:25 +0900

    feat(PurchaseLotto) : 사용자의 입력을 받아 랜덤한 Lotto번호들을 만드는 기능 구현.
    Lotto 구매에 관련한 모든 과정을 구현.
    접근제어자 변경.

--- src/main/java/lotto/PurchaseLotto.java
+++ src/main/java/lotto/PurchaseLotto.java
@@ -1,18 +1,38 @@
 package lotto;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 import ui.PurchasePrint;
 import exceptioncase.PurchaseException;
+import camp.nextstep.edu.missionutils.Randoms;
 public class PurchaseLotto {
-    public static int inputPurchaseMeony(){
+    public static List<Lotto> purchaseLotto(){
+        int lottocount = inputPurchaseMoney();
+        List<Lotto> purchaselottos = drawLotto(lottocount);
+        return purchaselottos;
+    }
+    private static int inputPurchaseMoney(){
         PurchasePrint.intputMoney();
         String money = Console.readLine();
         PurchaseException.inputMoneyException(money);
         return Integer.valueOf(money)/1000;
     }
-    public static List<List<Integer>> drawLotto(){
-        List<List<Integer>> answer = Collections.emptyList();
+    private static List<Lotto> drawLotto(int lottocount){
+        List<Lotto> answer = Collections.emptyList();
+        for(int i=0 ;i<lottocount ; i++){
+            answer.add(new Lotto(makeRandomNumbers()));
+        }
         return answer;
+    }
+    private static List<Integer> makeRandomNumbers(){
+        List<Integer> numbers = new LinkedList<>();
+        while(numbers.size()<6){
+            int randomnumber = Randoms.pickNumberInRange(1,45);
+            if(numbers.contains(randomnumber))
+                continue;
+            numbers.add(randomnumber);
+        }
+        return numbers;
     }
 }
