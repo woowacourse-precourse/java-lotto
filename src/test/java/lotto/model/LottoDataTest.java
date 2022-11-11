3928a44f74871fd2087d4f34f2891a4a18269d04
newh08 <newh08@gmail.com>
2022-11-12 00:32:31 +0900

    test: implement tests for function that put prize and prize money data to Lotto object

--- src/test/java/lotto/model/LottoDataTest.java
+++ src/test/java/lotto/model/LottoDataTest.java
@@ -2,15 +2,18 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.BeforeAll;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoDataTest {
-    static LottoData lottoData;
+    LottoData lottoData;
 
-    @BeforeAll
-    static void initAll() {
+    @BeforeEach
+    void initAll() {
         lottoData = new LottoData(12000);
     }
 
@@ -25,5 +28,35 @@ class LottoDataTest {
     void makeLottoTest() {
         lottoData.makeLotto();
         assertThat(lottoData.allLotto.size()).isEqualTo(12);
+    }
+
+    @DisplayName("로또가 절대 당첨될 수 없는 상황일 때 모든 로또의 등수, 상금이 0이다.")
+    @Test
+    void putDataToLottoTest1() {
+        List<Integer> winNumbers = new ArrayList<>(Arrays.asList(46, 47, 48, 49, 50, 51));
+        int bonusNumber = 52;
+
+        lottoData.makeLotto();
+        lottoData.putDataToLotto(winNumbers, bonusNumber);
+
+        for (Lotto lotto : lottoData.allLotto) {
+            assertThat(lotto.getPrize()).isEqualTo(0);
+            assertThat(lotto.getPrizeMoney()).isEqualTo(0);
+        }
+    }
+
+    @DisplayName("로또가 무조건 1등 당첨되는 상황일 때 모든 로또의 등수, 상금이 1등 이다.")
+    @Test
+    void putDataToLottoTest2() {
+        List<Integer> winNumbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45));
+        int bonusNumber = 52;
+
+        lottoData.makeLotto();
+        lottoData.putDataToLotto(winNumbers, bonusNumber);
+
+        for (Lotto lotto : lottoData.allLotto) {
+            assertThat(lotto.getPrize()).isEqualTo(1);
+            assertThat(lotto.getPrizeMoney()).isEqualTo(2000000000);
+        }
     }
 }
