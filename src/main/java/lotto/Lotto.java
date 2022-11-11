63dc68e7f0850d1cfb7554a053bb65c51f97ec62
jthugg <jthug@naver.com>
2022-11-12 00:39:53 +0900

    Docs: 기능 목록 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,17 +3,10 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
     }
 
     // TODO: 추가 기능 구현
