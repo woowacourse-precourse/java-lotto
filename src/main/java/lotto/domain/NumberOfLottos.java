2877b0ea14147109fec8b3be4cf153401f9ebecb
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-11-11 18:31:06 +0900

    feat(lotto): 금액 입력 기능 예외처리 구현

--- src/main/java/lotto/domain/NumberOfLottos.java
+++ src/main/java/lotto/domain/NumberOfLottos.java
@@ -3,11 +3,31 @@ package lotto.domain;
 public class NumberOfLottos {
 
     private static final int MIN_PRICE_AMOUNT = 1000;
+    private static final String MONETARY_UNIT_ERR_MESSAGE = "[ERROR] 화폐 단위가 옳지 않습니다.";
+    private static final String MIN_PRICE_ERR_MESSAGE = "[ERROR] 금액은 최소 1,000원이어야 합니다.";
 
     public NumberOfLottos() {
     }
 
     public int calculateNumberOfLottos(int price) {
+        validatePriceAmount(price);
         return price / MIN_PRICE_AMOUNT;
+    }
+
+    private void validatePriceAmount(int price) {
+        validateDivisible(price);
+        validateMinPrice(price);
+    }
+
+    private void validateDivisible(int price) {
+        if (price % MIN_PRICE_AMOUNT != 0) {
+            throw new IllegalArgumentException(MONETARY_UNIT_ERR_MESSAGE);
+        }
+    }
+
+    private void validateMinPrice(int price) {
+        if (price < MIN_PRICE_AMOUNT) {
+            throw new IllegalArgumentException(MIN_PRICE_ERR_MESSAGE);
+        }
     }
 }
--- src/test/java/lotto/domain/NumberOfLottosTest.java
+++ src/test/java/lotto/domain/NumberOfLottosTest.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -22,5 +24,14 @@ class NumberOfLottosTest {
     void calculateNumberOfLottos(int price, int expected) {
         int actual = numberOfLottos.calculateNumberOfLottos(price);
         Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"8100,[ERROR] 화폐 단위가 옳지 않습니다.", "0,[ERROR] 금액은 최소 1,000원이어야 합니다."})
+    @DisplayName("입력 금액 예외처리 기능")
+    void validatePriceAmount(int price, String errorMessage) {
+        assertThatThrownBy(() -> numberOfLottos.calculateNumberOfLottos(price))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(errorMessage);
     }
 }
