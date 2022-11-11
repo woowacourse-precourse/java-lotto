2d9874fac0a456ce4793a36a7052e81a35d4439a
Laeng <Laeng@users.noreply.github.com>
2022-11-11 12:17:51 +0900

    test(LottoControllerTest): Static 메서드에 대한 테스트 추가
    validateInput 메서드와 parseIntByWrongInput 메서드에 대한 예외 테스트 케이스를 추가했습니다.

---
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -0,0 +1,32 @@
+package lotto.controller;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoControllerTest {
+    @ParameterizedTest(name = "잘못된 입력 예외 테스트")
+    @CsvSource(value = {
+            "^([1-9,])$;가나,다라.마바",
+            "^([1-9]{1-2})$;123",
+            "^([1-9]{1-2})$;가나다"
+    }, delimiter = ';')
+    void validateByWrongInput(String regex, String input) {
+        assertThatThrownBy(() -> LottoController.validateInput(regex, input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "잘못된 문자 변환 예외 테스트")
+    @ValueSource(strings = {
+            "가", "a", "1w"
+    })
+    void parseIntByWrongInput(String input) {
+        assertThatThrownBy(() -> LottoController.parseInt(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
