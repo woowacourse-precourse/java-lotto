e64e33589a8d1b5d62128e75aa90bedc9e29ad93
akskflwn <akskflwn@gmail.com>
2022-11-12 01:21:58 +0900

    feat (InputView) : 보너스 번호를 입력 받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -13,6 +13,7 @@ public class InputView {
     private static final String GET_WIN_LOTTO_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String WIN_LOTTO_REGEX = "^[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+$";
     private static final String SPLIT_REGEX = ",";
+    private static final String GET_BONUS_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static int getMoneyInput() {
         System.out.println(GET_MONEY_MESSAGE);
@@ -43,6 +44,13 @@ public class InputView {
         if (!Pattern.matches(WIN_LOTTO_REGEX, input)) {
             throw new IllegalArgumentException("[ERROR] 입력 형식이 올바르지 않습니다.");
         }
+    }
+
+    public static int getBonus() {
+        System.out.println(GET_BONUS_MESSAGE);
+        String input = Console.readLine();
+        validate(input);
+        return toNumbers(input);
     }
 
 }
