9127f63332dd00cf83eb9d705b36d0cf89710b7c
anneu <b.imnuj@gmail.com>
2022-11-11 10:31:05 +0900

    feat: 발행 된 로또 번호 출력 기능 구현

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 public final class Print {
     private static final String PURCHASE_MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String PURCHASE_QUANTITY_MESSAGE = "개를 구매했습니다.";
@@ -10,6 +12,12 @@ public final class Print {
 
     public static void printPurchaseQuantity(int quantity) {
         System.out.println(quantity + PURCHASE_QUANTITY_MESSAGE);
+    }
+
+    public static void printPublishedLottoNumbers(List<Lotto> publishedLotto) {
+        for (Lotto lotto : publishedLotto) {
+            System.out.println(lotto.getSortedLottoNumbers());
+        }
     }
 
     private Print() {
