7cd22c6f6e9d52c39dec5ad37a6ec07de17dae37
Polite-Jehu <werber112@kyonggi.ac.kr>
2022-11-11 23:13:28 +0900

    feat(Lotto): add UI

--- src/main/java/lotto/LottoClerk.java
+++ src/main/java/lotto/LottoClerk.java
@@ -1,4 +1,17 @@
 package lotto;
 
+import java.util.ArrayList;
+
 public class LottoClerk {
+    public void sayBuyLotto(int lottoCount)
+    {
+        System.out.println(lottoCount+"개를 구매했습니다.");
+    }
+    public void sayLottoReceipt(ArrayList<Lotto> lottoReceipt)
+    {
+        for(Lotto l:lottoReceipt)
+        {
+            System.out.println(l.getLottoText());
+        }
+    }
 }
