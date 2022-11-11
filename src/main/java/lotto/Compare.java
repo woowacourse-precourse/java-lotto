b4095ca762ea15cb102a9f13208e6de249da8757
jthugg <jthug@naver.com>
2022-11-12 04:43:03 +0900

    Feat: 당첨번호 + 보너스번호를 구매한 로또 번호와 비교해 1~5등 체크하는 기능 추가

--- src/main/java/lotto/Compare.java
+++ src/main/java/lotto/Compare.java
@@ -1,5 +1,67 @@
 package lotto;
 
-public class Compare {
+import java.util.List;
 
+public class Compare {
+	private int[] compareResult = {0, 0, 0, 0, 0};
+
+	public Compare() {}
+
+	public void compareNumbers(List<List<Integer>> myLottos, List<Integer> winningNumbers, int bonusNumber) {
+		for(List<Integer> myLotto : myLottos) {
+			int hitCount = 0;
+
+			for(int winningNumber : winningNumbers) {
+				hitCount += isContains(myLotto, winningNumber);
+			}
+
+			setResult(hitCount, myLotto, bonusNumber);
+		}
+	}
+
+	public int isContains(List<Integer> myLotto, int winningNumber) {
+		if(myLotto.contains(winningNumber)) {
+			return 1;
+		}
+
+		return 0;
+	}
+
+	public void setResult(int hitCount, List<Integer> myLotto, int bonusNumber) {
+		setFirst(hitCount);
+		setSecond(hitCount, myLotto, bonusNumber);
+		setThird(hitCount, myLotto, bonusNumber);
+		setFourth(hitCount);
+		setFifth(hitCount);
+	}
+
+	public void setFirst(int hitCount) {
+		if(hitCount == 6) {
+			compareResult[0]++;
+		}
+	}
+
+	public void setSecond(int htiCount, List<Integer> myLotto, int bonusNumber) {
+		if(htiCount == 5 && myLotto.contains(bonusNumber)) {
+			compareResult[1]++;
+		}
+	}
+
+	public void setThird(int hitCount, List<Integer> myLotto, int bonusNumber) {
+		if(hitCount == 5 && !myLotto.contains(bonusNumber)) {
+			compareResult[2]++;
+		}
+	}
+
+	public void setFourth(int hitCount) {
+		if(hitCount == 4) {
+			compareResult[3]++;
+		}
+	}
+
+	public void setFifth(int hitCount) {
+		if(hitCount == 3) {
+			compareResult[4]++;
+		}
+	}
 }
