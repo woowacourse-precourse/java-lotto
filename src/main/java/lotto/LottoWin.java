6e623218663aa0d4874e40983a0434a8aa601eb6
MuSeung <peter0757@nvaer.com>
2022-11-11 14:25:38 +0900

    feat: LottoWin에 1등을 구분하는 기능 구현
    LottoWin
        - count int 인스턴스 추가
        - LottoWin : 1등을 구분하는 기능을 구현해 1등 값을 count에 저장
        - getPrizesMap : 1등의 값을 count로 설정

--- src/main/java/lotto/LottoWin.java
+++ src/main/java/lotto/LottoWin.java
@@ -1,19 +1,29 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class LottoWin {
+    int count;
 
     public LottoWin(List<List<Integer>> quickPicks, List<Integer> lotto, int bonus) {
+        count = 0;
         if(lotto.contains(bonus)) {
             throw new IllegalArgumentException();
         }
         if(bonus < LottoState.START.number() || bonus > LottoState.END.number()){
             throw new IllegalArgumentException();
         }
+        for(List<Integer> quickPick: quickPicks) {
+            ArrayList<Integer> tmpArr = new ArrayList<>(quickPick);
+            tmpArr.retainAll(lotto);
+            if (tmpArr.size() == LottoState.LENGTH.number()) {
+                count++;
+            }
+        }
     }
     public Map<String,Integer> getPrizesMap() {
-        return Map.of("1st", 1);
+        return Map.of("1st", count);
     }
 }
