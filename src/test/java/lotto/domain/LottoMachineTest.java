bbc20aaad142304280dea40e09c908bf4572c1dd
kdkdhoho <hkim4410@naver.com>
2022-11-11 15:11:35 +0900

    test(LottoMachineTest): generateLottoNumbers 함수 테스트 작성

---
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class LottoMachineTest {
+
+    LottoMachine lottoMachine = new LottoMachine();
+
+    @Test
+    void 생성한_로또번호_검증_테스트() {
+        List<Integer> numbers = lottoMachine.generateLottoNumbers();
+        Assertions.assertThatNoException()
+                .isThrownBy(() -> new Lotto(numbers));
+    }
+}
