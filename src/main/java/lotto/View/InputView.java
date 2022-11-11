d8965d919f58be03a7a03cf285906b8c80db704e
Sieun Park <psieun0606@gmail.com>
2022-11-12 02:49:32 +0900

    refactor: 메서드 이름 변경

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -4,21 +4,21 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String InputPurchasing() {
+    public String inputPurchasing() {
         System.out.println("구입금액을 입력해 주세요.");
         String purchasingAmount = Console.readLine();
 
         return purchasingAmount;
     }
 
-    public String InputWinningNumber() {
+    public String inputWinningNumber() {
         System.out.println("당첨 번호를 입력해주세요.");
         String winningNumber = Console.readLine();
 
         return winningNumber;
     }
 
-    public String InputBonusNumber() {
+    public String inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String bonusNumber = Console.readLine();
 
