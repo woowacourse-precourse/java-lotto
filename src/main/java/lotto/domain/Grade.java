ef739fd87df0906081696869f7f751bbe43f92c7
K-jun98 <alap_u@naver.com>
2022-11-11 22:12:55 +0900

    refactor - Money 객체와 PrizeMoney 분리

--- src/main/java/lotto/domain/Grade.java
+++ src/main/java/lotto/domain/Grade.java
@@ -3,19 +3,19 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Grade {
-    FIRST(new Money(2_000_000_000), 6),
-    SECOND(new Money(30_000_000), 5),
-    THIRD(new Money(1_500_000), 5),
-    FORTH(new Money(50_000), 4),
-    FIFTH(new Money(5_000), 3),
-    PASS(new Money(0),0);
+    FIRST(new PrizeMoney(2_000_000_000), 6),
+    SECOND(new PrizeMoney(30_000_000), 5),
+    THIRD(new PrizeMoney(1_500_000), 5),
+    FORTH(new PrizeMoney(50_000), 4),
+    FIFTH(new PrizeMoney(5_000), 3),
+    PASS(new PrizeMoney(0),0);
 
-    private final Money winningMoney;
+    private final PrizeMoney prizeMoney;
     private final int count;
 
 
-    Grade(Money winningMoney, int count) {
-        this.winningMoney = winningMoney;
+    Grade(PrizeMoney prizeMoney, int count) {
+        this.prizeMoney = prizeMoney;
         this.count = count;
     }
 
---
+++ src/main/java/lotto/domain/PrizeMoney.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+public class PrizeMoney {
+
+    private final int prizeMoney;
+
+    public PrizeMoney(int prizeMoney) {
+        this.prizeMoney = prizeMoney;
+    }
+}
