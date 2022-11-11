ee7368a9072a4a65c972024e6af7934e8354b51b
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-11 17:32:33 +0900

    feat<LottoNumber>: 로또가 가지는 숫자 클래스 구현
    - 1~45 범위 숫자 검증 함수 추가
    - equals, hashCode, toString 오버라이딩
    - Comparable 구현
    - Exception Message 추가

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -1,4 +1,55 @@
 package lotto.model;
 
-public class LottoNumber {
+import java.util.Objects;
+
+public class LottoNumber implements Comparable<LottoNumber> {
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    private final int number;
+
+    public LottoNumber(int number) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(int number) {
+        if (hasLottoNumberRange(number)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean hasLottoNumberRange(int number) {
+        return number < MIN_NUMBER || number > MAX_NUMBER;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) o;
+        return getNumber() == that.getNumber();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNumber());
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
+    }
+
+    @Override
+    public int compareTo(LottoNumber lottoNumber) {
+        return Integer.compare(this.number, lottoNumber.getNumber());
+    }
 }
--- src/main/java/lotto/util/LottoExceptionMessage.java
+++ src/main/java/lotto/util/LottoExceptionMessage.java
@@ -2,7 +2,9 @@ package lotto.util;
 
 public enum LottoExceptionMessage {
     PURCHASE_PRICE_UNIT_OF_MONEY("[ERROR] 구입 금액은 1,000원 단위여야 합니다."),
-    PURCHASE_PRICE_IS_NUMBER("[ERROR] 구입 금액은 숫자여야 합니다.");
+    PURCHASE_PRICE_IS_NUMBER("[ERROR] 구입 금액은 숫자여야 합니다."),
+    DUPLICATE_LOTTO_NUMBER("[ERROR] 중복되지 않는 숫자 6개여야 합니다."),
+    LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
 
     private final String message;
 
