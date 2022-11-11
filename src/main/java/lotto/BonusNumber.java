2196ee2fc3cf99197f117bf3cea25793a57b7b87
kimdahee0815 <diana815@naver.com>
2022-11-11 14:01:17 +0900

    feat(BonusNumber 생성자) : 사용자 입력 보너스 번호를 int로 바꿔서 bonnusnumber 변수에 저장하는 기능

--- src/main/java/lotto/BonusNumber.java
+++ src/main/java/lotto/BonusNumber.java
@@ -3,8 +3,11 @@ package lotto;
 import java.util.regex.Pattern;
 
 public class BonusNumber {
+	private int bonusnumber = 0;
 	public BonusNumber(String stringbonusnumber) {
 		validateBonusNumber(stringbonusnumber);
+		
+		this.bonusnumber = Integer.parseInt(stringbonusnumber);
 	}
 	
 	public void validateBonusNumber(String stringbonusnumber) {
@@ -18,4 +21,7 @@ public class BonusNumber {
 		
 	}
 	
+	public int getBonusNumber() {
+		return this.bonusnumber;
+	}
 }
