880a2d69a811c450fff34c50f12c957404162ede
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-11 15:34:54 +0900

    feat: create WinLotto generate Method

--- src/main/java/lotto/generate/WinLottoGenerate.java
+++ src/main/java/lotto/generate/WinLottoGenerate.java
@@ -1,36 +1,42 @@
 package lotto.generate;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
+import lotto.domain.WinLotto;
 import lotto.output.Output;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class WinLottoGenerate {
     private final int LOTTO_SIZE = 6;
+    private final String[] winNumber;
+    private final String bonus;
 
     public WinLottoGenerate() {
         Output.askWinNumber();
-        String[] winNumber = Console.readLine().split(",");
-        winNumberValidate(winNumber);
+        winNumber = Console.readLine().split(",");
+        winNumberValidate();
         Output.askBonusNumber();
-        String bonus = Console.readLine();
-        bonusValidate(bonus);
+        bonus = Console.readLine();
+        bonusValidate();
     }
 
-    private void bonusValidate(String bonus) {
-        if(!bonus.chars().allMatch(Character::isDigit)){
+    private void bonusValidate() {
+        if (!bonus.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException();
         }
     }
 
-    private void winNumberValidate(String[] winNumber) {
+    private void winNumberValidate() {
         lottoSizeValidate(winNumber);
         isDigit(winNumber);
     }
 
     private void isDigit(String[] winNumber) {
-        for(String number : winNumber){
-            if(!number.chars().allMatch(Character::isDigit)){
+        for (String number : winNumber) {
+            if (!number.chars().allMatch(Character::isDigit)) {
                 throw new IllegalArgumentException();
             }
         }
@@ -41,4 +47,10 @@ public class WinLottoGenerate {
             throw new IllegalArgumentException();
         }
     }
+    public WinLotto generate(){
+        List<Integer> numbers = Arrays.stream(winNumber).map(Integer::parseInt).collect(Collectors.toList());
+        Integer bonusInteger = Integer.parseInt(bonus);
+        return new WinLotto(new Lotto(numbers), bonusInteger);
+    }
+
 }
