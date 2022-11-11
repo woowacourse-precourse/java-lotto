4740bb31dafc3ce9728424611261e0481d0b44d0
bjk1649 <qhwns1649@naver.com>
2022-11-11 22:21:48 +0900

    feat: 당첨 번호가 1 ~ 45 사이의 숫자인지 검증하는 기능 추가

--- src/test/java/lotto/bo/WinningNumberTest.java
+++ src/test/java/lotto/bo/WinningNumberTest.java
@@ -11,7 +11,14 @@ class WinningNumberTest {
     @DisplayName("당첨 번호에 문자가 들어갈 시 예외가 발생한다.")
     @Test
     void createWinningNumberByNotInteger() {
-        assertThatThrownBy(() -> new WinningNumber("1,ad,324,12,3","2"))
+        assertThatThrownBy(() -> new WinningNumber("1,ad,324,12,3,7","2"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호가 1 ~ 45의 숫자가 아닐 시 예외가 발생한다.")
+    @Test
+    void createWinningNumberByOutOfRangeNumber() {
+        assertThatThrownBy(() -> new WinningNumber("1,2,324,12,3,6","2"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
