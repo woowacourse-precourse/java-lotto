fe69c663586a160267fe0ca10fb30b7b76102bac
49ehyeon42 <49ehyeon42@gmail.com>
2022-11-11 13:27:48 +0900

    feat: 로또 번호를 검증하는 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,7 +12,10 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 개수가 6개가 아닙니다.");
+        }
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호 중 중복이 있습니다.");
         }
     }
 
