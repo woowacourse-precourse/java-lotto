ea8a0dfc801d150f90b47ee6c5d8f64b22ce8087
bjm7701 <bjm7701@naver.com>
2022-11-11 20:23:16 +0900

    feat : declare main function members

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
 
 public class Application {
+    private static int bonusnum;
+    private static List<Integer> truenum;
+    private static List<Lotto> lottos;
+    private static List<Integer> res;
+    private static void generateLotto(){
+
+    }
+    private static void allocateInitial(){
+
+    }
+    private static List<Integer> calcFinal(){
+
+    }
+    private static void printFinal(){
+
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
