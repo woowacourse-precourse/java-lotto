8cdd5e7bdd83174ef1ceaa851715768cf682fbfd
박원종 <wonjong1030@naver.com>
2022-11-11 15:47:27 +0900

    fix: 하나의 랭크만 수익률 계산가능 했던 것을 전체 결과 랭크에 대해서 계산하도록 수정

--- src/main/java/lotto/Calculator.java
+++ src/main/java/lotto/Calculator.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Map;
+
 public class Calculator {
     public int getLottoCount(int money) {
         return money / 1000;
@@ -12,8 +14,11 @@ public class Calculator {
         return Rank.findByCorrectCountAndBonus(correctCount, bonus);
     }
 
-    public double calculateYield(Rank rank, double money) {
-        double earnMoney = rank.money;
+    public double calculateYield(Map<Rank, Integer> result, double money) {
+        double earnMoney = 0;
+        for (Rank rank : result.keySet()) {
+            earnMoney += rank.money * result.get(rank);
+        }
         return Math.round(earnMoney/money * 100 * 100) / 100.0;
     }
 }
