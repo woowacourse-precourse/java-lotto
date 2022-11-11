4951197dfb76b9be1da47d458872f74a625331cc
Seokhyun Lee <lseokhyun99@gmail.com>
2022-11-11 16:28:56 +0900

    feat: 로또앱 실행/구매금액 입력/금액 오류체크 메소드 추가
    로또앱 실행 메소드 runLottoApp()
    로또 구매할 금액 입력 메소드 howMuchLottoBuy()
    로또 구매할 금액 오류 체크 메소드 moneyErrorCheck()
    3가지 메소드를 구현하였다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        runLottoApp();
+    }
+    public static void runLottoApp(){
+        int lottoCount = howMuchLottoBuy();
+    }
+    public static int howMuchLottoBuy(){
+        System.out.println("구입금액을 입력해 주세요.");
+        String money = readLine();
+        moneyErrorCheck(money);
+        return Integer.parseInt(money)/1000;
+    }
+    public static void moneyErrorCheck(String money){
+        for(int i = 0 ; i < money.length() ;i++){
+            if(!Character.isDigit(money.charAt(i))){
+                throw new IllegalArgumentException("[ERROR] 숫자를 입력해야합니다.");
+            }
+        }
+        if((Integer.parseInt(money) % 1000) != 0)
+            throw new IllegalArgumentException("[ERROR] 구입금액은 1000으로 나누어 떨어져야합니다.");
     }
 }
