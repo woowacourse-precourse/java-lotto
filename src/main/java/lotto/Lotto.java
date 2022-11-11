b6a8e8c9c409917e687f835881b3c51b6022602c
jh868 <2weeks9947@naver.com>
2022-11-12 00:51:07 +0900

    feat: 로또 번호 관련 예외처리 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,7 +12,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 개수가 6개가 아닙니다.");
+        }
+        if (numbers.size() != numbers.stream().distinct().count()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+        }
+        if (numbers.stream().anyMatch(number -> number < 1|| number > 45)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
