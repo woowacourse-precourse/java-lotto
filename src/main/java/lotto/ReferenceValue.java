a103ebe2650bbe2fae70786dc89257b1f5dae2c1
JaeUpSu <hyeongs2323@gmail.com>
2022-11-11 18:18:05 +0900

    ReferenceValue | 변수 네이밍 구분하기 쉽게 수정

--- src/main/java/lotto/ReferenceValue.java
+++ src/main/java/lotto/ReferenceValue.java
@@ -4,18 +4,28 @@ public class ReferenceValue {
 
     public static final int LOTTO_SIZE = 6;
     public static final int LOTTO_PRICE = 1_000;
-    public static final int THREE_MATCHES_MONEY = 5_000;
-    public static final int FOUR_MATCHES_MONEY = 50_000;
-    public static final int FIVE_MATCHES_MONEY = 1_500_000;
-    public static final int FIVE_BONUS_MATCHES_MONEY = 30_000_000;
-    public static final int SIX_MATCHES_MONEY = 2_000_000_000;
+    public static final int LOTTO_START_RANGE = 1;
+    public static final int LOTTO_END_RANGE = 45;
+    public static final int MONEY_THREE_MATCHES = 5_000;
+    public static final int MONEY_FOUR_MATCHES = 50_000;
+    public static final int MONEY_FIVE_MATCHES = 1_500_000;
+    public static final int MONEY_FIVE_BONUS_MATCHES = 30_000_000;
+    public static final int MONEY_SIX_MATCHES = 2_000_000_000;
 
-    public static final int THREE_MATCHES_INDEX = 0;
-    public static final int FOUR_MATCHES_INDEX = 1;
-    public static final int FIVE_MATCHES_INDEX = 2;
-    public static final int FIVE_BONUS_MATCHES_INDEX = 3;
-    public static final int SIX_MATCHES_INDEX = 4;
+    public static final int COUNT_THREE_MATCHES = 3;
+    public static final int COUNT_FOUR_MATCHES = 4;
+    public static final int COUNT_FIVE_MATCHES = 5;
+    public static final int COUNT_SIX_MATCHES = 6;
+    public static final int INDEX_OTHER_MATCHES = 0;
+    public static final int INDEX_THREE_MATCHES = 1;
+    public static final int INDEX_FOUR_MATCHES = 2;
+    public static final int INDEX_FIVE_MATCHES = 3;
+    public static final int INDEX_FIVE_BONUS_MATCHES = 4;
+    public static final int INDEX_SIX_MATCHES = 5;
+    public static final Integer IS_LOTTO_BONUS_NUMBER = 777;
 
+    public static final int NOTHING = 0;
+    public static final int MULTIPLE_HUNDRED = 100;
     public static final int MULTIPLE_FIRST_DECIMAL = 10;
     public static final double DIVIDE_FIRST_DECIMAL = 10.0;
 
