321442ee2ba8d21ae6c18a48d9ccddceae9e68f8
ParkJoungWoo <jug78@naver.com>
2022-11-11 12:46:56 +0900

    perf(user/User.java) : User 클래스 개선
    기존의 int 타입의 구입 금액과 상금 금액을 Money 객체로 수정했습니다.
    
    - 금액에 대한 검증 책임을 Money로 이전했습니다.

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -5,62 +5,32 @@ import lotto.Lotto;
 import java.util.List;
 
 public class User {
-    private final boolean NOT_VALID = false;
-    private final int BASE_MONEY = 1000;
-    private List<Lotto> lottos = null;
-    private int seedMoney = 0;
-    private int prizeMoney = 0;
+    private List<Lotto> lottos;
+    private Money seedMoney;
+    private Money prizeMoney;
 
-    public User(int money) {
-        if (isMoneyNumeric(money) == NOT_VALID) {
-            throw new IllegalArgumentException();
-        }
-        if (isMoneyThousandMultiple(money) == NOT_VALID) {
-            throw new IllegalArgumentException();
-        }
-        this.seedMoney = money;
-    }
-
-    private boolean isMoneyNumeric(int money) {
-        if (isMoneyNotNull(money) == NOT_VALID) {
-            throw new IllegalArgumentException();
-        }
-        if (isMoneyPositive(money) == NOT_VALID) {
-            throw new IllegalArgumentException();
-        }
-        return true;
-    }
-
-    private boolean isMoneyNotNull(Integer money) {
-        return !money.equals(null);
-    }
-
-    private boolean isMoneyPositive(int money) {
-        return money > 0;
-    }
-
-    private boolean isMoneyThousandMultiple(int money) {
-        return (money % BASE_MONEY == 0) && (money / BASE_MONEY > 0);
+    public User() {
+        int inputMoney = 0;
+        seedMoney = new Money(inputMoney);
+        prizeMoney = new Money();
     }
 
     public void setLottos(List<Lotto> lottos) {
         this.lottos = lottos;
     }
 
-    public void setPrizeMoney(int prizeMoney) {
-        if (isMoneyNumeric(prizeMoney) == NOT_VALID) {
-            throw new IllegalArgumentException();
-        }
-        this.prizeMoney = prizeMoney;
-    }
-    public String getRatio(){
-        isMoneyNumeric(seedMoney);
-        isMoneyNumeric(prizeMoney);
-        return String.format("%.2f",(double)prizeMoney/(double)seedMoney);
+    public void setPrizeMoney(Money prize) {
+        prizeMoney = prize;
     }
 
-    public int getPrizeMoney() {
-        return prizeMoney;
+    public String getRatio() {
+        double prize = (double) prizeMoney.getMoney();
+        double seed = (double) seedMoney.getMoney();
+        return String.format("%.1f", prize / seed);
+    }
+
+    public Money getSeedMoney() {
+        return seedMoney;
     }
 
     public List<Lotto> getLottos() {
