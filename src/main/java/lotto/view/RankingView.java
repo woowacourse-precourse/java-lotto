efe023e12fd14be418ae30334be8b66e484869f1
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-11 12:49:04 +0900

    feat: RankingView 클래스 생성

---
+++ src/main/java/lotto/view/RankingView.java
@@ -0,0 +1,30 @@
+package lotto.view;
+
+public class RankingView {
+    private static final String WINNING_STATISTICS = "당첨 통계\n---\n";
+    private static final String THREE_MATCH = "3개 일치 (5,000원) - ";
+    private static final String FOUR_MATCH = "4개 일치 (50,000원) - ";
+    private static final String FIVE_MATCH = "5개 일치 (1,500,000원) - ";
+    private static final String FIVE_MATCH_WITH_BONUS = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    private static final String SIX_MATCH = "6개 일치 (2,000,000,000원) - ";
+    private static final String TELL_COUNT = "개";
+    private static final String TOTAL_PROFIT_START = "총 수익률은 ";
+    private static final String TOTAL_PROFIT_END = "%입니다.";
+
+    public void showWinningResult(){
+
+    }
+
+    private void startWinningStatistics(){
+
+    }
+
+    private void showPriceToPlayer(){
+
+    }
+
+    private void showProfit(){
+
+    }
+
+}
