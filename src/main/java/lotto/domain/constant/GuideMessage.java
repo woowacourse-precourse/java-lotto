99d253c3361e1ea881cbbc456ab799a047023a46
yubin <dbqls2821@gmail.com>
2022-11-11 15:48:25 +0900

    refactor(GuideMessage): 구매한 갯수에 대한 enum 상수의 이름을 수정

--- src/main/java/lotto/domain/constant/GuideMessage.java
+++ src/main/java/lotto/domain/constant/GuideMessage.java
@@ -2,7 +2,7 @@ package lotto.domain.constant;
 
 public enum GuideMessage {
   INPUT_MONEY("구입금액을 입력해 주세요."),
-  OUTPUT_NUM_OF_LOTTO("개를 구매했습니다."),
+  OUTPUT_PURCHASE_NUM_OF_LOTTO("개를 구매했습니다."),
   INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
   INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
 
