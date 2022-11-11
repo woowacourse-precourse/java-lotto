203d7ad810285e8950fea9dbef3843811d5e1619
ywj9811 <ywj9811@naver.com>
2022-11-12 00:09:28 +0900

    feat: RelativeLottoService클래스
    당첨 번호, 나의 번호, 보너스 번호 등등 로또와 관련된 내용을 통합적으로 관리할 수 있는 통합 서비스

---
+++ src/main/java/lotto/service/RelativeLottoService.java
@@ -0,0 +1,41 @@
+package lotto.service;
+
+import lotto.view.InputOutputView;
+
+import java.util.List;
+
+public class RelativeLottoService {
+    private BuyLotto buyLotto = new BuyLotto();
+    private InputOutputView inputOutputView = new InputOutputView();
+    private ProduceWinningNumbers produceWinningNumbers = new ProduceWinningNumbers();
+
+    public int getMoney() {
+        return money;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public List<List<Integer>> getMyLottoNumbers() {
+        return myLottoNumbers;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    private int money;
+    private int bonusNumber;
+    private int quantity;
+    private List<List<Integer>> myLottoNumbers;
+    private List<Integer> winningNumbers;
+
+    public void relativeLottoNumber() {
+        money = inputOutputView.buyMoney();
+        quantity = buyLotto.buyLotto(money);
+        myLottoNumbers = buyLotto.getMyLottoNumbers(quantity);
+        winningNumbers = produceWinningNumbers.getWinningNumbers();
+        bonusNumber = produceWinningNumbers.getBonusNumber(winningNumbers);
+    }
+}
