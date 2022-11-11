5149fab24c80bda339dbb8ffbd31351af5bde5ac
jihun-24k <gi020477@gmail.com>
2022-11-11 12:49:48 +0900

    Feat: 로또 게임 view 부분 추가

---
+++ src/main/java/lotto/view/LottoGame.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+import lotto.message.MessageList;
+
+public class LottoGame {
+    public void start() {
+        System.out.println(MessageList.PURCHASE_AMOUNT.getMessage());
+    }
+}
