fefb90a2eb9bf33b38d7c25fe8be2dfaa3b64cdd
kevstevie <jianna442@gmail.com>
2022-11-11 17:50:40 +0900

    feat: validate 함수에 케이스 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,9 +12,26 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            Message.ERROR_INPUT_SIX.printError();
+            throw new IllegalArgumentException();
+        }
+        validateLottoRange(numbers);
+        validateLottoDuplicate(numbers);
+    }
+
+    private void validateLottoRange(List<Integer> numbers) {
+        long count = numbers.stream().filter(s -> s > 45 || s < 1).count();
+        if (count != 0) {
+            Message.ERROR_INPUT_RANGE.printError();
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateLottoDuplicate(List<Integer> result) {
+        long count = result.stream().distinct().count();
+        if (count != result.size()) {
+            Message.ERROR_INPUT_DUPLICATE.printError();
+            throw new IllegalArgumentException();
+        }
+    }
 }
