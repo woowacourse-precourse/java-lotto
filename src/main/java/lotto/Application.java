dc78408f3c66ba5582e675288f8aba47da8c8803
Gaby <gaby@Gabyui-MacBookPro.local>
2022-11-11 22:54:15 +0900

    comment: reset Prize comment
    Since Prize ENUM class are reused, make sure the count for
    each prize is reset befere reuse

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,7 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         try {
+            // reset for prize status is required for multiple gradlew test
             Prize.prizeReset();
             InOutput ui = new InOutput();
             ui.printAll();
