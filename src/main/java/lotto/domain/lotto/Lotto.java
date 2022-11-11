c54cbfbd63fd64307eaa4f13891ad246ab203e95
shinD <huipulci1@naver.com>
2022-11-11 11:21:33 +0900

    feat: 로또 숫자 유효성 검증 기능 추가
    - 숫자는 1 ~ 45 사이의 범위
    - 중복되지 않은 6개의 숫자여야 함

---
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -0,0 +1,38 @@
+package lotto.domain.lotto;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+
+        long correctNumberCount = numbers.stream()
+                .distinct()
+                .filter(num -> isInRange(num, 1, 45))
+                .count();
+
+        if (correctNumberCount != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isInRange(
+            final int number,
+            final int startInclusive,
+            final int endInclusive
+    ) {
+        return (number >= startInclusive) && (number <= endInclusive);
+    }
+
+    // TODO: 추가 기능 구현
+
+}
