e79847f3f35daca716c0b6dbbeaf3b6fd519174d
은지 <midzy0228@gmail.com>
2022-11-11 22:41:39 +0900

    feat(구입금액): 1000으로 나누어 떨어지는지 확인 기능 구현

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -6,6 +6,7 @@ public class Validator {
 
     public void validDatePurchaseAmount(String purchaseAmount) {
         validDateType(purchaseAmount);
+        validDateThousandUnitNumber(Integer.parseInt(purchaseAmount));
     }
 
     private void validDateType(String input) {
@@ -13,6 +14,11 @@ public class Validator {
             int number = Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해주셔야 합니다.");
+        }
+    }
+    private void validDateThousandUnitNumber(Integer purchase) {
+        if (purchase % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위의 숫자를 입력하셔야 합니다.");
         }
     }
 }
