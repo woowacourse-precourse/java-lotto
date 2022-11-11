5624e7fb1dcaf8d419733021e04e7b8f3f5fa29c
yumyeonghan <mgtmh991013@gmail.com>
2022-11-11 16:41:10 +0900

    feat: Implementation of enterWinningNumber(), enterBonusNumber()
    - Enter the winning number with exception handling
    - Enter the bonus number so that it does not overlap with the winning number

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,9 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -9,19 +12,54 @@ public class Application {
         System.out.println("구입금액을 입력해 주세요.");
         User user = new User();
         int amount = user.purchaseAmount();
+        System.out.println();
 
-        if (amountValidation(amount)) {
-            lotteryNumberOutput(amount);
+        List<Integer> winningNumber = new ArrayList<Integer>();
+
+        int bonusNumber = 0;
+
+        try {
+            if (amountValidation(amount)) {
+                lotteryNumberOutput(amount);
+
+                winningNumber = getWinningNumber(user);
+
+                bonusNumber = getBonusNumber(user, winningNumber);
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("e.getMessage() = " + e.getMessage());
         }
+    }
+
+    private static int getBonusNumber(User user, List<Integer> winningNumber) {
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonusNumber = user.enterBonusNumber(winningNumber);
+        System.out.println();
+
+        return bonusNumber;
+    }
+
+    private static List<Integer> getWinningNumber(User user) {
+        List<Integer> winningNumber;
+        System.out.println("당첨 번호를 입력해 주세요.");
+        winningNumber = user.enterWinningNumber();
+        System.out.println();
+        return winningNumber;
     }
 
     private static void lotteryNumberOutput(int amount) {
         Lotto[] lottos = new Lotto[amount / 1000];
 
+        System.out.println(amount / 1000 + "개를 구매했습니다.");
+
         for (Lotto lotto : lottos) {
             lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             System.out.println(lotto.getNumbers());
         }
+
+        System.out.println();
+
     }
 
     private static boolean amountValidation(int amount) {
--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,5 +1,10 @@
 package lotto;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class User {
@@ -11,5 +16,45 @@ public class User {
         } catch (NumberFormatException e) {
             return -1;
         }
+    }
+
+    public List enterWinningNumber() {
+
+        String str = readLine();
+
+        List<String> winningNumber = Stream.of(str.split("\\s*,\\s*"))
+                .collect(Collectors.toList());
+
+        HashSet set = new HashSet(winningNumber);
+
+        if (set.size() != winningNumber.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복되는 숫자가 있습니다.");
+        }
+
+        if (winningNumber.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 6자리 숫자를 입력해주세요");
+        }
+
+        for (String s : winningNumber) {
+            if (1 > Integer.parseInt(s) || 45 < Integer.parseInt(s)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+
+        return winningNumber.stream().map(value -> Integer.parseInt(value)).collect(Collectors.toList());
+
+    }
+
+    public int enterBonusNumber(List<Integer> winningNumber) {
+
+        int bonusNumber = Integer.parseInt(readLine());
+
+        for (Integer number : winningNumber) {
+            if (bonusNumber == number) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호와 보너스 번호가 중복됩니다.");
+            }
+        }
+
+        return bonusNumber;
     }
 }
