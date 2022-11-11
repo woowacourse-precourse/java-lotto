d5c73a146d9f72077a15b9a43b93e6bc52c8c887
seoyoung <sy9723@korea.ac.kr>
2022-11-11 12:58:54 +0900

    feat: 기능 추가 (당첨 번호와 생성된 로또 번호를 비교한다)

---
+++ src/main/java/lotto/domain/Judgment.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Judgment {
+    public final int firstCount;
+
+    public Judgment(List<List<Integer>> collectionOfLottoNumbers, List<Integer>numbers, int bonusNumber) {
+        this.firstCount = calculateFirst(collectionOfLottoNumbers, numbers);
+    }
+
+    private int calculateFirst(List<List<Integer>> collectionOfLottoNumbers, List<Integer>numbers) {
+        int count = 0;
+        return count;
+    }
+
+    public int correctCount(List<Integer> actualNumbers, List<Integer> expectedNumbers) {
+        int count = 0;
+        int[] checkCorrect = new int[46];
+        for (Integer actualNumber : actualNumbers) {
+            checkCorrect[actualNumber] = 1;
+        }
+        for (Integer expectedNumber : expectedNumbers) {
+            if (checkCorrect[expectedNumber] == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
