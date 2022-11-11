a7af9b2f22048916cb6098ff41952e25e6fbb9a6
khj <bbb6254177@gmail.com>
2022-11-11 18:07:55 +0900

    feat(lotto): add LottoSeller class

---
+++ src/main/java/lotto/LottoSeller.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoSeller {
+    public List<Lotto> sellTo(int amount) {
+        List<Lotto> lotto = new ArrayList<>();
+        return lotto;
+    }
+}
