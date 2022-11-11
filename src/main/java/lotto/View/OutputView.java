657da61b7854f5f9e4dfd1c9acec78cd9967319f
raonrabbit <raonrabbit@gmail.com>
2022-11-12 01:53:23 +0900

    style: 메소드 static 속성 제거

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -3,7 +3,7 @@ package lotto.View;
 import java.util.List;
 
 public class OutputView {
-    public static void askPurchasePrice(){
+    public void askPurchasePrice(){
         System.out.print("구매 금액을 입력해 주세요.");
     }
 }
