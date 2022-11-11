24553967eb36206bebad0b06f05013dcc920b488
Eunkeee <eunkeeeee@gmail.com>
2022-11-11 10:16:36 +0900

    fix: 보너스 번호가 당첨 번호와 중복되면 예외 처리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -8,6 +8,10 @@ public class LottoController {
     public static void run() {
         Lotto lotto = new Lotto(InputView.inputWinningNumbers());
         BonusNumber bonusNumber = new BonusNumber(InputView.inputBonusNumber());
+        System.out.println("Winning Numbers: " + lotto.get());
+        System.out.println("Bonus Number: " + bonusNumber.get());
+        if (lotto.get().contains(bonusNumber.get())) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.");
+        }
     }
-
 }
--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -6,24 +6,18 @@ import static lotto.Util.verifyRangeOfLottoNumber;
 
 public class BonusNumber {
     private static int bonusNumber;
-
     public BonusNumber(String input) {
         this.bonusNumber = validateBonusNumber(formatInput(input));
     }
-
     private static int validateBonusNumber(int number) {
         verifyRangeOfLottoNumber(number);
         /*verifyDuplicateNumber(number);*/
         return number;
     }
-
-  /*  private static void verifyDuplicateNumber(int number) {
-        if (Lotto.getNumbers().contains(number)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.");
-        }
-    }*/
-
     private static int formatInput(String input) {
         return Integer.parseInt(removeSpace(input));
+    }
+    public int get() {
+        return bonusNumber;
     }
 }
