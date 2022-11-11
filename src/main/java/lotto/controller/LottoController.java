c2aef1c273dcdbda1920b263c8b0cfec371401bd
hossang <zkzk104@naver.com>
2022-11-11 19:57:19 +0900

    feat: 로또 구입 금액 입력받기

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,16 +1,21 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.LottoMoney;
 import lotto.view.InputView;
 
 public class LottoController {
     private InputView inputView;
+    private LottoMoney money;
 
-    public LottoController(InputView inputView) {
-        this.inputView = inputView;
+    public LottoController() {
+        this.inputView = new InputView();
+        this.money = new LottoMoney();
     }
 
     public void run() {
         inputView.printRequest();
+        money.inputMoney(Console.readLine());
 
     }
 }
---
+++ src/main/java/lotto/domain/LottoMoney.java
@@ -0,0 +1,9 @@
+package lotto.domain;
+
+public class LottoMoney {
+    private int money;
+
+    public void inputMoney(String input) {
+        money = Integer.parseInt(input);
+    }
+}
