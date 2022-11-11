6b4d7af5ce67dfdf353697444b17a0f26b33b823
limzzum <chfhddl1234@naver.com>
2022-11-12 01:07:57 +0900

    feat : 로또 번호와 보너스 번호 검증 함수 생성

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -5,7 +5,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import lotto.model.Lotto;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class User {
     private List<Lotto> lottos = new ArrayList<>();
@@ -18,12 +20,44 @@ public class User {
 
     public void inputWinningNumber(){
         System.out.println("당첨 번호를 입력해 주세요.");
-        String[] numbers = Console.readLine().split(",");
+        String input = Console.readLine();
+        List<String> numbers = List.of(input.split(","));
+        //validateLottoPlusBonusNum(numbers);
         for(String num: numbers){
             winningNumber.add(Integer.parseInt(num));
         }
         System.out.println("보너스 번호를 입력해 주세요.");
-        bonusNumber = Integer.parseInt(Console.readLine());
+        String bonusInput = Console.readLine();
+
+    }
+
+    private void validateLottoNum(List<String> numbers){
+        if(numbers.size()!=6){
+            throw  new IllegalArgumentException("[ERROR] 로또의 번호가 6개여야 합니다");
+        }
+
+        if(Set.copyOf(numbers).size() != 6 ){
+            throw  new IllegalArgumentException("[ERROR] 로또 번호와 보너스 번호는 중복이 없어야 합니다");
+        }
+        for(int i=0; i<numbers.size(); i++){
+            String num = numbers.get(i);
+            if(num.charAt(i) <1 || num.charAt(i)>45){
+                throw new IllegalArgumentException("[ERROR] 1 ~ 45 숫자만 입력 가능합니다");
+            }
+        }
+
+    }
+    private void validateBonusNum(String num){
+        if(num.length() != 1){
+            throw  new IllegalArgumentException("[ERROR] 보너스 번호는 한 개입니다");
+        }
+
+        if(winningNumber.contains(bonusNumber)){
+            throw  new IllegalArgumentException("[ERROR] 로또 번호와 보너스 번호는 중복이 없어야 합니다");
+        }
+        if( num.charAt(0)<1 || num.charAt(0)>45){
+            throw new IllegalArgumentException("[ERROR] 1 ~ 45 숫자만 입력 가능합니다");
+        }
 
     }
 
