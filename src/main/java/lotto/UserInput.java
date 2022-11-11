25ec5539fea5415d0f15ebb217acb3a5185b5508
kevstevie <jianna442@gmail.com>
2022-11-11 12:26:29 +0900

    refactor: inputMoney에 readLine 추가
    input 함수에 readLine이 있는게 직관적으로 맞다고 생각함

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class UserInput {
 
     private UserInput() {}
 
-    static public long inputMoney(String input) {
+    static public long inputMoney() {
+        String input = Console.readLine();
         validateMoney(input);
         return Long.parseLong(input);
     }
