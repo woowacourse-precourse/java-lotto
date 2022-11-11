3c504c5fa05c92cfc325e3430bee97810b5897b1
qudtjs0753 <qudtjs0753@naver.com>
2022-11-11 10:42:54 +0900

    refactor: InputParser의 입력값 최대 최소범위 안내 에러 문구로 변경

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -4,7 +4,13 @@ public enum ErrorMessage {
     NOTICE("[ERROR] "),
     UNEXPECTED("예기치 않은 에러가 발생하였습니다."),
     NOT_DIVISIBLE("금액을 맞춰서 주시기 바랍니다."),
-    NOT_NUMBER("숫자를 입력하셔야 합니다.");
+    NOT_NUMBER(
+            String.format(
+                    "%d이상, %d이하의 정수를 입력하셔야 합니다.",
+                    Integer.MIN_VALUE,
+                    Integer.MAX_VALUE
+            )
+    );
 
     private final String message;
     private final StringBuilder sb = new StringBuilder();
