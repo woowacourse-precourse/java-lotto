d39c6b79b49fe1d9f14329ed51cf4aac849012ab
SEONMl <sm000117@naver.com>
2022-11-11 16:35:26 +0900

    feat: 수익률을 계산하는 메서드 구현

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,15 +1,32 @@
 package lotto.domain;
 
-import lotto.service.LottoGenerator;
+import lotto.config.InputConfig;
 
 import java.util.List;
+import java.util.Map;
 
 public class User {
     private final List<Lotto> purchasedLotto;
     private Statistics statistics;
 
-    public User() {
-        this.lottoGenerator = new LottoGenerator();
+    public User(List<Lotto> lotto) {
+        this.purchasedLotto = lotto;
+    }
+
+    public void setStatistics(Statistics statistics) {
+        this.statistics = statistics;
+    }
+
+    public float calculateProfit() {
+        long purchaseMoney = purchasedLotto.size() * InputConfig.LOTTO_PRICE;
+        long totalProfit = statistics.getTotalProfit();
+
+        float profitPercent = (float)totalProfit / (float) purchaseMoney * 100;
+        return profitPercent;
+    }
+
+    public Map<Winner, Integer> getResult() {
+        return statistics.getResult(purchasedLotto);
     }
 
 }
