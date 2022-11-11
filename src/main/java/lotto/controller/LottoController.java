d527bbd53e30b89868a41147a66df295ebe71d8f
kjb95 <topmedia44@gmail.com>
2022-11-11 21:15:41 +0900

    refactor: 로또를 구매하는 부분을 분리하여 buyLotto() 메소드 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -8,9 +8,15 @@ import lotto.view.OutputView;
 public class LottoController {
 
     public void run() {
+        LottoCollection lottoCollection = buyLotto();
+    }
+
+    private LottoCollection buyLotto() {
         PayingMoney payingMoney = new PayingMoney(InputView.requestMoney());
         LottoCollection lottoCollection = payingMoney.createLottoCollection();
 
         OutputView.printLottoCollection(lottoCollection);
+        return lottoCollection;
     }
+
 }
