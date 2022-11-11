4d67bec60044d6732e93b0d99a4dca39f5de248a
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-11 22:22:08 +0900

    test (Money) : 로또 구매 금액 검증 테스트 추가

--- src/test/java/domain/MoneyTest.java
+++ src/test/java/domain/MoneyTest.java
@@ -9,10 +9,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MoneyTest {
 
+    @DisplayName("사용자 입력값이 숫자가 아니라면 예외가 발생한다.")
+    @Test
+    public void createUserAmountByNotNumber() throws Exception{
+        assertThatThrownBy(()->new Money("123456j")).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
     @DisplayName("사용자 입력값이 1,000의 배수가 아니라면 예외가 발생한다.")
     @Test
-    public void userAmount_test(){
-        assertThatThrownBy(()->new Money(123456)).isInstanceOf(IllegalArgumentException.class);
+    public void createUserAmountByWrongNumber(){
+        assertThatThrownBy(()->new Money("123456")).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
