98c9879b02a9e2b507ece1295720159c15fee395
김태용 <amico741@naver.com>
2022-11-11 18:36:13 +0900

    feat: 로또 구매시, 로또 개수와 번호 출력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,21 @@
 package lotto;
 
-import lotto.domain.NumberGenerator;
+import lotto.domain.Buyer;
+import lotto.domain.Money;
+import lotto.service.LottoService;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        NumberGenerator numbers = new NumberGenerator();
-        System.out.println(numbers.createDuplicateNumbers());
+        Buyer buyer = new Buyer();
+        LottoService lottoService = new LottoService();
+        System.out.print("구입금액을 입력해 주세요.");
+        String input = buyer.inputMoney();
+        List<Lotto> lottos = lottoService.publishLotto(new Money(input).getLottoCount());
+        System.out.println(lottos.size() + "개를 구매하였습니다.");
+        for(Lotto lotto : lottos){
+            System.out.println(lotto.getNumbers());
+        }
     }
 }
