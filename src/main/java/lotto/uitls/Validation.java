f2d392bf47f6cd29449c631ad053c9ace73a76bb
yujaemin <woawoa0513@gmail.com>
2022-11-11 20:35:45 +0900

    feat: 로또 가격으로 나눠 떨어지는지 확인 함수 구현
    로또 구매 금액을 입력받았을 때, 해당 금액이 로또의 가격인 1000원으로 나누어지는지 검사한다.

--- src/main/java/lotto/uitls/Validation.java
+++ src/main/java/lotto/uitls/Validation.java
@@ -1,8 +1,13 @@
 package lotto.uitls;
 
 public class Validation {
+    private static final int LOTTO_PRICE = 1_000;
 
     public boolean hasNonDigitCharacter(String input) {
         return input.chars().allMatch(Character::isDigit);
+    }
+
+    public boolean isDividedByLottoPrice(int purchaseAmount) {
+        return purchaseAmount % LOTTO_PRICE == 0;
     }
 }
