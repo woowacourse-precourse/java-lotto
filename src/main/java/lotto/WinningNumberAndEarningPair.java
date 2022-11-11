6ff0f60a0a017482a7752aef88667426f7c1fa7e
2021110806 <2021110806@dgu.ac.kr>
2022-11-12 00:57:26 +0900

    feat($WinningNumberAndEarningPair):add WinningNumberAndEarningPair enum

---
+++ src/main/java/lotto/WinningNumberAndEarningPair.java
@@ -0,0 +1,16 @@
+package lotto;
+
+public enum WinningNumberAndEarningPair {
+
+    CORRECT_3(5000),
+    CORRECT_4(50000),
+    CORRECT_5(1500000),
+    CORRECT_5_BONUS(30000000),
+    CORRECT_6(2000000000);
+
+    private final int earning;
+
+    WinningNumberAndEarningPair(int earning) {
+        this.earning = earning;
+    }
+}
