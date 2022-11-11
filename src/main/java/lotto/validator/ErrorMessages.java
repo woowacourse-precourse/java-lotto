1ba59625a5ba37d198b37442f17ff8a910d13edd
Jin409 <sualng123@gmail.com>
2022-11-12 00:04:22 +0800

    refactor(ErrorMessages): 에러 메시지가 상수를 사용하도록 수정
    - 하드코딩을 지양할 수 있도록 LottoConstants 의 상수를 사용할 수 있도록 수정

--- src/main/java/lotto/validator/ErrorMessages.java
+++ src/main/java/lotto/validator/ErrorMessages.java
@@ -1,10 +1,17 @@
 package lotto.validator;
 
+import static lotto.constants.LottoConstant.LOTTO_PRICE;
+import static lotto.constants.LottoConstant.VALID_LOTTERY_NUMBER_LENGTH;
+import static lotto.constants.LottoConstant.VALID_MAX_RANGE;
+import static lotto.constants.LottoConstant.VALID_MIN_RANGE;
+
 public class ErrorMessages {
-    public static final String INVALID_LENGTH_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개의 숫자로 이루어져야 합니다.";
+    public static final String INVALID_LENGTH_ERROR_MESSAGE =
+            "[ERROR] 로또 번호는 " + VALID_LOTTERY_NUMBER_LENGTH + "개의 숫자로 이루어져야 합니다.";
     public static final String DUPLICATE_LOTTO_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복되지 않아야 합니다.";
-    public static final String INVALID_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다";
+    public static final String INVALID_RANGE_ERROR_MESSAGE =
+            "[ERROR] 로또 번호는 " + VALID_MIN_RANGE + "부터 " + VALID_MAX_RANGE + "사이의 숫자여야 합니다";
     public static final String INVALID_TYPE_ERROR_MESSAGE = "[ERROR] 로또 번호는 정수여야 합니다.";
-    public static final String INVALID_PAID_MONEY_MESSAGE = "[ERROR] 지불된 금액은 1000원 단위여야 합니다.";
+    public static final String INVALID_PAID_MONEY_MESSAGE = "[ERROR] 지불된 금액은 " + LOTTO_PRICE + "원 단위여야 합니다.";
     public static final String DUPLICATE_BONUS_NUMBER_ERROR_MESSAGE = "[ERROR] 해당 보너스 번호는 이미 당첨 번호에 포함되어 있습니다.";
 }
