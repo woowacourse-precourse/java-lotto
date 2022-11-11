09da72941dad25be91a3c3e14649ddf18935129c
gyuturn <rbtjrrbals@gmail.com>
2022-11-12 00:40:06 +0900

    test($view): 정규표현식을 이용한 당첨번호 숫자 유효성검사 테스트
    EXC-B-3
    1,2,3,4,5,6과 같은 형식인것과 아닌경우 테스트

--- src/test/java/view/ReceiveViewTest.java
+++ src/test/java/view/ReceiveViewTest.java
@@ -1,7 +1,9 @@
 package view;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import model.Purchase;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import util.message.ErrorMessage;
@@ -17,5 +19,27 @@ class ReceiveViewTest {
         //then
         assertThatThrownBy(() -> ReceiveView.purchaseLottoValid(invalidMoney))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorMessage.NOT_STRING_MONEY);
+    }
+
+    @Test
+    @DisplayName("[ReceiveView] 당첨번호입력시 1,2,3,4,,,5 와 같이 유효하지 않은 인풋값 테스트")
+    void winLottoNotValid(){
+        //given
+        String winLottoInput = "1,2,3,4,,,5";
+        //when
+        ;
+        //then
+        assertThatThrownBy(() -> ReceiveView.winLottoValid(winLottoInput))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorMessage.INVALID_INPUT_WIN_LOTTO);
+    }
+
+    @Test
+    @DisplayName("[ReceiveView] 당첨번호입력시 1,2,3,4,5,6 와 같이 유효하지 않은 인풋값 테스트")
+    void winLottoValid(){
+        //given
+        String winLottoInput = "1,2,3,4,5,6";
+        //when
+        //then
+        assertDoesNotThrow(()-> ReceiveView.winLottoValid(winLottoInput));
     }
 }
