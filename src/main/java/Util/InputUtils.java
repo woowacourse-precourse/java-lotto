f7eb20b3f136cac88f593e7669690d94fc0bbb0c
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-11-12 01:40:29 +0900

    feat(inputUtils): 보너스 번호 입력
    보너스 번호 입력 및 예외사항 추가
    
    변경사항:
    보너스 입력 메세지 상수 텍스트 수정

--- src/main/java/Util/InputUtils.java
+++ src/main/java/Util/InputUtils.java
@@ -5,6 +5,7 @@ import domain.ConstantNumber;
 import lotto.Lotto;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -21,9 +22,41 @@ public class InputUtils {
         return Integer.parseInt(money);
     }
 
+    public Lotto StringToWinningLottoNumber(){
+        System.out.println(INPUT_LOTTO_NUMBER_MESSAGE.getMessage());
+        String WinningLottoNumber = Console.readLine();
+        validateWinningLottoNumber(WinningLottoNumber);
+
+        return new Lotto(
+                Arrays.stream(WinningLottoNumber.split(","))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    public int StringToBonus(List<Integer> winningNumber) {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE.getMessage());
+        String bonus = Console.readLine();
+        validateBonus(bonus, winningNumber);
+
+        return Integer.parseInt(bonus);
+    }
+
     public void validateMoney(String input) {
         isNumber(input);
         isEnoughMoney(input);
+    }
+
+    public void validateWinningLottoNumber(String input){
+        isLottoNumberStyle(input);
+    }
+
+    public void validateBonus(String input, List<Integer> lottoNumbers) {
+        isNumber(input);
+        int bonus = Integer.parseInt(input);
+
+        isLottoRange(bonus);
+        isContainWinningLotto(bonus, lottoNumbers);
     }
 
     private void isEnoughMoney(String input) {
@@ -39,26 +72,22 @@ public class InputUtils {
             throw new IllegalArgumentException(NOT_NUMBER_ERROR.getMessage());
         }
     }
-    public Lotto StringToWinningLottoNumber(){
-        System.out.println(INPUT_LOTTO_NUMBER_MESSAGE.getMessage());
-        String WinningLottoNumber = Console.readLine();
-        validateWinningLottoNumber(WinningLottoNumber);
-
-        return new Lotto(
-            Arrays.stream(WinningLottoNumber.split(","))
-                    .map(Integer::parseInt)
-                    .collect(Collectors.toList())
-        );
-    }
-
-    public void validateWinningLottoNumber(String input){
-        isLottoNumberStyle(input);
-    }
 
     private void isLottoNumberStyle(String input){
         String regex = "^\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2}";
         if(!Pattern.matches(regex, input)){
             throw new IllegalArgumentException(NOT_WINNING_LOTTO_INPUT_STYLE_ERROR.getMessage());
+        }
+    }
+    private void isContainWinningLotto(int bonus, List<Integer> lottoNumbers) {
+        if (lottoNumbers.contains(bonus)){
+            throw new IllegalArgumentException(NOT_BONUS_SAME_WINNING_LOTTO_ERROR.getMessage());
+        }
+    }
+    private void isLottoRange(int number){
+        if(number < ConstantNumber.MIN_LOTTO_NUMBER.getNumber()
+                || number > ConstantNumber.MAX_LOTTO_NUMBER.getNumber()) {
+            throw new IllegalArgumentException(NOT_LOTTO_RANGE_ERROR.getMessage());
         }
     }
 }
--- src/main/java/domain/InputMessage.java
+++ src/main/java/domain/InputMessage.java
@@ -3,7 +3,7 @@ package domain;
 public enum InputMessage {
     INPUT_BUY_MESSAGE("구입금액을 입력해 주세요."),
     INPUT_LOTTO_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
-    INPUT_SPECIAL_NUMBER_MESSAGE("보너스 번호를 입력해 주세요.");
+    INPUT_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요.");
 
 
     final String message;
