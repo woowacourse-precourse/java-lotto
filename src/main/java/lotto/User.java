8d4d7bdec9a8af49acec9811999bbcbc36a135b8
2021110806 <2021110806@dgu.ac.kr>
2022-11-12 00:06:00 +0900

    feat($User):add inputWinningNumber method

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,11 +1,25 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
     private int money;
 
     public void inputMoney(int money) {
         this.money = money;
+    }
+
+    public List<Integer> inputWinningNumber(String inputtedWinningNumber) {
+        int convertedWinningNumber;
+        List<Integer> winningNumber = new ArrayList<>();
+        String[] unconvertedWinningNumber = inputtedWinningNumber.split(",");
+        for (String unconvertedWinningNumberContent : unconvertedWinningNumber) {
+            convertedWinningNumber = Integer.parseInt(unconvertedWinningNumberContent);
+            winningNumber.add(convertedWinningNumber);
+        }
+        return winningNumber;
     }
 
 
