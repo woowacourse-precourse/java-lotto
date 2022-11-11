0935b6acba8f76fb61e6a2f8155bd3ce75585725
glass <glass7312@gmail.com>
2022-11-11 21:35:30 +0900

    feat: get_hitCount 기능추가(Lotto class) 당첨번호와 사용자가 구매한 로또의 번호를 비교하여 맞은 번호의 갯수를 반환하는 기능 추가.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Lotto {
@@ -18,6 +20,15 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
+    public List<Integer> getNumbers(){
+        return this.numbers;
+    }
+    public int get_hitCount(List<Integer> winning_numbers,int bonusNumber){
+        int hit_Count=0;
+        for(int checkNumber :numbers) {
+            if(winning_numbers.contains(checkNumber)) hit_Count++;
+        }
+        return hit_Count;
+    }
 
-    // TODO: 추가 기능 구현
 }
