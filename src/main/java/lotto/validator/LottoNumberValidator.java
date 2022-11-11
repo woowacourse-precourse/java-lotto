835d0eb213abf18479c997d14abe69055f81e1b9
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-11 13:16:24 +0900

    feat: 로또에 넣을 숫자들에 중복이 있는지 검증 기능 추가

--- src/main/java/lotto/validator/LottoNumberValidator.java
+++ src/main/java/lotto/validator/LottoNumberValidator.java
@@ -1,10 +1,14 @@
 package lotto.validator;
 
 import java.util.List;
+import java.util.Set;
+import lotto.constant.ErrorMessage;
 
 public class LottoNumberValidator {
 
     public static void validateExistDuplicateNumber(List<Integer> numbers) {
-
+        if (numbers.size() != Set.copyOf(numbers).size()) {
+            throw new IllegalArgumentException(ErrorMessage.CAN_NOT_EXIST_DUPLICATE_NUMBER_IN_LOTTO.message());
+        }
     }
 }
