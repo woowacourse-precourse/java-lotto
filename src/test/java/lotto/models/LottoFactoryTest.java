645cca42b72e631baec68c587a425e842bc3c72e
xi_jjun <rlawowns97@naver.com>
2022-11-11 19:18:16 +0900

    test: 사용자가 구매한 만큼의 로또 번호들을 생성하는 기능 테스트

--- src/test/java/lotto/models/LottoFactoryTest.java
+++ src/test/java/lotto/models/LottoFactoryTest.java
@@ -2,12 +2,14 @@ package lotto.models;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoFactoryTest {
 
@@ -24,5 +26,15 @@ class LottoFactoryTest {
 		// then : 중복없이 6개의 숫자를 생성했기에 정상적으로 Lotto 번호가 만들어진다.
 		final int NON_DUPLICATED_LOTTO_LENGTH = 6;
 		assertThat(result.size()).isEqualTo(NON_DUPLICATED_LOTTO_LENGTH);
+	}
+
+	@ParameterizedTest(name = "로또 구입 금액:{0} ==> 발급할 수 있는 최대 로또 개수: {1}개. (로또 1개 1000원)")
+	@CsvSource(value = {"1000:1", "2000:2", "999:0", "0:0", "3000:3", "5500:5", "91283:91", "1001:1", "2999:2"}, delimiter = ':')
+	void issueValidLotteriesTest(int purchaseAmount, int expectedSize) {
+		LottoFactory factory = new LottoFactory();
+
+		List<Lotto> resultLotteries = factory.issueLottoNumbersByPurchaseAmount(purchaseAmount);
+
+		assertThat(resultLotteries.size()).isEqualTo(expectedSize);
 	}
 }
