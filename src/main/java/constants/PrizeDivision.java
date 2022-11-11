87b2e6375869a70cc9c6a23fd3ddd18db59281c7
scyllacore <yms8795@gmail.com>
2022-11-11 22:18:40 +0900

    refactor : 등수 산정에 필요한 상수 추가.

--- src/main/java/constants/PrizeDivision.java
+++ src/main/java/constants/PrizeDivision.java
@@ -1,27 +1,32 @@
 package constants;
 
 public enum PrizeDivision {
-    EMPTY("invalid","invalid"),
-    FIRST("6개 일치","2,000,000,000"),
-    SECOND("5개 일치, 보너스 볼 일치","30,000,000"),
-    THIRD("5개 일치","1,500,000"),
-    FOURTH("4개 일치","50,000"),
-    FIFTH("3개 일치","5,000")
-    ;
+    NO_DIVISION("invalid", "invalid", -1),
+    FIRST("6개 일치", "2,000,000,000", 1),
+    SECOND("5개 일치, 보너스 볼 일치", "30,000,000", 2),
+    THIRD("5개 일치", "1,500,000", 3),
+    FOURTH("4개 일치", "50,000", 4),
+    FIFTH("3개 일치", "5,000", 5);
 
-    private final String sameCount;
-    private final String money;
+    private final String matchingCount;
+    private final String prizeAmount;
+    private final int division;
 
-    PrizeDivision(String sameCount, String money) {
-        this.sameCount = sameCount;
-        this.money = money;
+    PrizeDivision(String matchingCount, String prizeAmount, int division) {
+        this.matchingCount = matchingCount;
+        this.prizeAmount = prizeAmount;
+        this.division = division;
     }
 
-    public String getSameCount() {
-        return sameCount;
+    public String getMatchingCount() {
+        return matchingCount;
     }
 
-    public String getMoney() {
-        return money;
+    public String getPrizeAmount() {
+        return prizeAmount;
+    }
+
+    public int getDivision() {
+        return division;
     }
 }
