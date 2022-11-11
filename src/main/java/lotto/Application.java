b68bfae53e461d5518238abb8767db74ad08dd2e
Ji-Un-Gil <wldns2577@gmail.com>
2022-11-11 14:39:38 +0900

    feat(Application.java): validate function Added function: - 로또 구입 금액이 조건과 다를 경우의 예외 처리 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import java.util.Scanner;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        int purchase = sc.nextInt();
+        validate(purchase);
+        
+        sc.close();
+    }
+
+    private static void validate(Integer purchase){
+        if (purchase % 1000 != 0 || purchase <= 0){
+            System.out.println("[ERROR] 금액은 반드시 1,000원 이상, 1,000원 단위로 입력하세요.");
+            throw new IllegalArgumentException();
+        }
     }
 }
