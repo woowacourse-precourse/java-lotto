035b5283fa5d2b6f1afdb9d04a2cda9cb5232642
abbd2 <ajkety00@gmail.com>
2022-11-11 20:19:04 +0900

    Feat(InputLotto): 문자열을 ,(쉼표)를 기준으로 나누어 저장하는 메서드 구현

--- src/main/java/lotto/InputLotto.java
+++ src/main/java/lotto/InputLotto.java
@@ -2,6 +2,8 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class InputLotto {
 
     public long inputValueToints(){
@@ -17,6 +19,11 @@ public class InputLotto {
         ExceptionHandling exceptionHandling = new ExceptionHandling();
         exceptionHandling.isNumeric(value); // 숫자인지 검사
         return Long.parseLong(value);
+    }
+
+    public List<String> splitCommas(String value){
+        List<String> valueList = List.of(value.split(","));
+        return valueList;
     }
 
 }
