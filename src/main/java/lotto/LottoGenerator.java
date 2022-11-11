3ade59b92f64e85a1b031e88f05976e0b2ef321b
박원종 <wonjong1030@naver.com>
2022-11-11 18:24:34 +0900

    fix: Arrays.asList로 만들어진 list는 수정 불가해서 ArrayList로 변환하여 오름차순 정렬하도록 수정

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,7 +10,7 @@ public class LottoGenerator {
     public List<Integer>[] createRandomNumbers(int count) {
         List<Integer>[] numbers = new List[count];
         for (int i = 0; i < count; i++) {
-            List<Integer> number = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> number = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             Collections.sort(number);
             numbers[i] = number;
         }
