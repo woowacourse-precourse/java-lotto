6ae5888730516bb7832bafa7c336d4881744bc42
khsrla9806 <kimhunsope@naver.com>
2022-11-11 19:43:26 +0900

    test(CalculatorTest): 수익률 계산 기능 테스트 구현

--- src/test/java/lotto/domain/CalculatorTest.java
+++ src/test/java/lotto/domain/CalculatorTest.java
@@ -1,9 +1,12 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.lang.reflect.Method;
 import java.util.List;
@@ -29,6 +32,28 @@ public class CalculatorTest {
                 Arguments.of(2033150000, List.of(0, 1, 1, 2, 3, 0)),
                 Arguments.of(2031555000, List.of(0, 1, 1, 1, 1, 1)),
                 Arguments.of(0, List.of(0, 0, 0, 0, 0, 0))
+        );
+    }
+    @DisplayName("수익률 계산 테스트")
+    @ParameterizedTest
+    @MethodSource("calculateEarningsRateTestSource")
+    void calculateEarningsRateTest(List<Integer> result, int purchaseAmount, double earningsRate) throws Exception{
+        Calculator calculator = new Calculator();
+        Method privateMethod = calculator.getClass().getDeclaredMethod("calculateEarningsRate", int.class);
+        privateMethod.setAccessible(true);
+        Method setTotalEarnings = calculator.getClass().getDeclaredMethod("calculateTotalEarnings", List.class);
+        setTotalEarnings.setAccessible(true);
+
+        setTotalEarnings.invoke(calculator, result);
+        privateMethod.invoke(calculator, purchaseAmount);
+
+        assertThat(calculator.getEarningsRate()).isEqualTo(earningsRate);
+    }
+    private static Stream<Arguments> calculateEarningsRateTestSource() {
+        return Stream.of(
+                Arguments.of(List.of(0, 1, 1, 2, 3, 0), 9000, 22590555.6),
+                Arguments.of(List.of(0, 1, 1, 1, 1, 1), 8000, 25394437.5),
+                Arguments.of(List.of(0, 0, 0, 0, 0, 0), 10000, 0)
         );
     }
 }
