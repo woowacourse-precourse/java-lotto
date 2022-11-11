8131e31c1e56f75c424ae63ed0c61318544a30c0
cpot5620 <=cpot5620@naver.com>
2022-11-12 02:46:31 +0900

    feat(WinningType): add value to check money by winning

--- src/main/java/lotto/LottoConstant.java
+++ src/main/java/lotto/LottoConstant.java
@@ -9,11 +9,19 @@ public class LottoConstant {
     public static final int WINNING_FOUR_NUM = 4;
     public static final int WINNING_FIVE_NUM = 5;
     public static final int WINNING_SIX_NUM = 6;
-
+    public static final int INIT_WINNING_COUNT = 0;
+    public static final int PLUS_ONE_COUNT = 1;
+    public static final int WINNING_THREE_MONEY = 5000;
+    public static final int WINNING_FOUR_MONEY = 50000;
+    public static final int WINNING_FIVE_MONEY = 1500000;
+    public static final int WINNING_FIVE_AND_BONUS_MONEY = 30000000;
+    public static final int WINNING_SIX_MONEY = 2000000000;
+    public static final int ROUND_POINT = 2;
 
     public static final String PURCHASE_PRICE_MSG = "구매금액을 입력해 주세요.";
     public static final String WINNING_NUM_MSG = "당첨 번호를 입력해 주세요.";
     public static final String BONUS_NUM_MSG = "보너스 번호를 입력해 주세요.";
+    public static final String BUY_MSG = "%d개를 구매했습니다.\n";
+    public static final String WINNING_RESULT_MSG = "당첨 통계\n---\n총 수익률은 %.1f%%입니다.";
     public static final String SEPARATOR = ",";
-    public static final String BUY_MSG = "개를 구매했습니다.";
 }
--- src/main/java/lotto/WinningType.java
+++ src/main/java/lotto/WinningType.java
@@ -1,19 +1,27 @@
 package lotto;
 
+import static lotto.LottoConstant.*;
+
 public enum WinningType {
-    THREE("3개 일치 (5,000원) - "),
-    FOUR("4개 일치 (50,000원) - "),
-    FIVE("5개 일치 (1,500,000원) - "),
-    FIVE_WITH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    SIX("6개 일치 (2,000,000,000원) - 0");
+    THREE("3개 일치 (5,000원) - ", WINNING_THREE_MONEY),
+    FOUR("4개 일치 (50,000원) - ", WINNING_FOUR_MONEY),
+    FIVE("5개 일치 (1,500,000원) - ", WINNING_FIVE_MONEY),
+    FIVE_AND_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - ", WINNING_FIVE_AND_BONUS_MONEY),
+    SIX("6개 일치 (2,000,000,000원) - ", WINNING_SIX_MONEY);
 
-    private final String value;
+    private final String message;
+    private final int money;
 
-    WinningType(String value) {
-        this.value = value;
+    WinningType(String message, int money) {
+        this.message = message;
+        this.money = money;
     }
 
-    public String getValue() {
-        return value;
+    public String getMessage() {
+        return message;
+    }
+
+    public int getMoney() {
+        return money;
     }
 }
