df6dadde4ab9b8295ac407416fa6ad1656772dea
sosow0212 <sosow0212@naver.com>
2022-11-11 14:32:39 +0900

    refactor : 예외 처리를 View에서 진행하려고 리팩토링

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,15 +4,12 @@ import static lotto.util.LottoUtil.bonusNumberParser;
 import static lotto.util.LottoUtil.lottoNumbersParser;
 
 import java.util.List;
-import lotto.exception.UserInputLottoException;
 
 public class Lotto {
     private final List<Integer> numbers;
     private final int bonusNumber;
-    private final UserInputLottoException userInputLottoException = new UserInputLottoException();
 
     public Lotto(String inputNumbers, String inputBonusNumber) {
-        userInputLottoException.validateLotto(inputNumbers, inputBonusNumber);
         this.numbers = lottoNumbersParser(inputNumbers);
         this.bonusNumber = bonusNumberParser(inputBonusNumber);
     }
