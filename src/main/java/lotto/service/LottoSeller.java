1294e9e6cd0c4e79481dacfcf794fad9298e82bc
simba <shim9597@gmail.com>
2022-11-11 20:50:19 +0900

    feat(lotto): 생성된 모든 로또를 출력하는 기능 구현

--- src/main/java/lotto/service/LottoSeller.java
+++ src/main/java/lotto/service/LottoSeller.java
@@ -2,35 +2,52 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Lotto;
+import lotto.domain.LottoPurchaser;
 import lotto.utils.LottoMaker;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoSeller {
 
     public static final int LOTTO_PRICE = 1000;
     LottoMaker lottoMaker = new LottoMaker();
-    public List<Lotto> allLottoOfPurchaser = new ArrayList<>(); // Purchaser 클래스에 있어야 할 변수
+    LottoPurchaser purchaser = new LottoPurchaser();
 
-
-    List<Lotto> takeMoneyAndGiveLotto() {
+    public void sellLottoToPurchaser() {
         int numberOfLottoToMake = getNumberOfLottoToMake();
+        List<Lotto> newLottoList = new ArrayList<>();
 
         for (int i = 0; i < numberOfLottoToMake; i++) {
             List<Integer> numbersInOneLotto = lottoMaker.newLotto();
             Lotto newLotto = new Lotto(numbersInOneLotto);
-            allLottoOfPurchaser.add(newLotto);
+            newLottoList.add(newLotto);
+            purchaser.setAllLottoOfPurchaser(newLottoList);
         }
-        return allLottoOfPurchaser;
+        printAllLotto();
+    }
+
+
+    public void printAllLotto() {
+        for (Lotto lotto : purchaser.getAllLottoOfPurchaser()) {
+            List<Integer> numbers = lotto.getNumbers();
+            Collections.sort(numbers);
+            System.out.println(numbers);
+        }
     }
 
     private int getNumberOfLottoToMake() {
+
+        System.out.println("구입금액을 입력해 주세요.");
+
         String moneyAsString = Console.readLine();
 
         int money = Integer.parseInt(moneyAsString);
 
         int howManyLotto = money / LOTTO_PRICE;
+
+        System.out.println(howManyLotto + "개를 구매했습니다.");
 
         return howManyLotto;
     }
