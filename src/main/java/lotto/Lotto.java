bbfd2388c82605b7e7932726173965444b0a08ee
Donghyun <flaehan@gmail.com>
2022-11-11 11:03:23 +0900

    type : feat ✨로또 구입 한 만큼 생성하기
    body :
    
    footer :

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,5 @@
 package lotto;
 
-package lotto;
-
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
@@ -11,35 +9,36 @@ public class Lotto {
     private static List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45 , 6);
     private static List<List> myLottoNum = new ArrayList<>();
     private static int howManyLotto;
-
+    private static int inputMoney;
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
     // 구입 금액 입력
-    public int inputMoney(){
+    public static int inputMoney(){
         System.out.println("구입금액을 입력해 주세요.");
-        int inputMoney = Integer.parseInt(Console.readLine());
+        int typeInputMoney = Integer.parseInt(Console.readLine());
+        inputMoney = typeInputMoney;
         if(inputMoney % 1000 != 0){
-            throw new IllegalArgumentException("[ERROR] 로또 구이 금액은 1,000원 단위입니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위입니다.");
         }
         return inputMoney;
     }
 
     // 로또 구입 갯수
-    public int theNumberOfLotto(){
-        howManyLotto = inputMoney() / 1000;
-
+    public static int theNumberOfLotto(){
+        howManyLotto = inputMoney / 1000;
+        System.out.println(howManyLotto + "개를 구매했습니다.");
         return howManyLotto;
     }
 
-    // 로또 생성
-    public List<List> myLottoNumber(){
+    // 구입한 만큼 로또 생성
+    public static List<List> myLottoNumber(){
         for(int i = 0; i < howManyLotto; i++){
-        myLottoNum.add(numbers);
+            List<Integer> ranLotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        myLottoNum.add(ranLotto);
         }
-
         return myLottoNum;
     }
 
