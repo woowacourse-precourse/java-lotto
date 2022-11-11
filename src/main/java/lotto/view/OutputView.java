1bd03e57a7cfc5f3060a5f0602734b01b7f49966
ksh-beginner <ohk9134@naver.com>
2022-11-11 20:56:52 +0900

    feat(OuputView) : 로또 당첨 내역 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,21 @@
 package lotto.view;
 
 
+import lotto.LottoWinningRank;
+import lotto.service.LottoGameService;
+
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
     private static final String PURCHASE_LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
+    private static final String LOTTO_RANK_FIFTH_MESSAGE = "3개 일치 (5,000원) - ";
+    private static final String LOTTO_RANK_FOURTH_MESSAGE = "4개 일치 (50,000원) - ";
+    private static final String LOTTO_RANK_THIRD_MESSAGE = "5개 일치 (1,500,000원) - ";
+    private static final String LOTTO_RANK_SECOND_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    private static final String LOTTO_RANK_FIRST_MESSAGE = "6개 일치 (2,000,000,000원) - ";
+    private static final String COUNT_UNIT = "개";
 
     public void printPurchaseLottoCount(int purchaseLottoCount) {
         System.out.println(purchaseLottoCount + PURCHASE_LOTTO_COUNT_MESSAGE);
@@ -13,5 +23,13 @@ public class OutputView {
 
     public void printIssuedLottoNumbers(List<Integer> issuedLottoNumbers) {
         System.out.println(issuedLottoNumbers.toString());
+    }
+
+    public void printLottoWinningStatistics(Map<LottoWinningRank, Integer> lottoWinningRankMap) {
+        System.out.println(LOTTO_RANK_FIFTH_MESSAGE + lottoWinningRankMap.get(LottoWinningRank.FIFTH) + COUNT_UNIT);
+        System.out.println(LOTTO_RANK_FOURTH_MESSAGE + lottoWinningRankMap.get(LottoWinningRank.FOURTH) + COUNT_UNIT);
+        System.out.println(LOTTO_RANK_THIRD_MESSAGE + lottoWinningRankMap.get(LottoWinningRank.THIRD) + COUNT_UNIT);
+        System.out.println(LOTTO_RANK_SECOND_MESSAGE + lottoWinningRankMap.get(LottoWinningRank.SECOND) + COUNT_UNIT);
+        System.out.println(LOTTO_RANK_FIRST_MESSAGE + lottoWinningRankMap.get(LottoWinningRank.FIRST) + COUNT_UNIT);
     }
 }
