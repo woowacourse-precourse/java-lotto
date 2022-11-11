66e9288f4925b7aacfcc8448c6c0f40d38bb7fce
cpot5620 <=cpot5620@naver.com>
2022-11-12 02:51:10 +0900

    refactor(LottoMachine): refactor variable initialization

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,8 +11,8 @@ import static lotto.LottoConstant.*;
 public class LottoMachine {
 
     public List<Integer> createNumbers() {
-        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUM,
-                MAX_LOTTO_NUM, LOTTO_SIZE);
+        List<Integer> lottoNumbers = new ArrayList<>(
+                Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUM, MAX_LOTTO_NUM, LOTTO_SIZE));
 
         Collections.sort(lottoNumbers);
 
