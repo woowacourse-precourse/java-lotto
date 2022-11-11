ec58fba4d6b0e772e30a696c3b00f77f4823b0c1
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-11 17:44:18 +0900

    fix<LottoNumberGenerator>: 로또 생성 클래스 구현
    - 중복되지 않는 1~45 범위의 6개 숫자를 뽑는다.
    - 숫자를 LottoNumber로 인스턴스화한다.
    - LottoNumber들을 TreeSet에 저장하여 Lotto를 생성한다.

--- src/main/java/lotto/model/LottoNumberGenerator.java
+++ src/main/java/lotto/model/LottoNumberGenerator.java
@@ -1,4 +1,17 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
 public class LottoNumberGenerator {
+    public Lotto generate() {
+        return new Lotto(createLottoNumbers());
+    }
+
+    private Set<LottoNumber> createLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6).stream()
+                .map(LottoNumber::new).collect(Collectors.toCollection(TreeSet::new));
+    }
 }
