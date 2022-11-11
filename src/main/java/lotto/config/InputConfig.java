2d8881fe3b6da8335fabf15077f7e4a9b66f13a8
SEONMl <sm000117@naver.com>
2022-11-12 00:01:50 +0900

    feat: IllegalArgumentException 에러 메시지에 구체 설명 추가

--- src/main/java/lotto/config/InputConfig.java
+++ src/main/java/lotto/config/InputConfig.java
@@ -6,6 +6,13 @@ import java.util.Set;
 
 public class InputConfig {
     public static final String ERROR_MESSAGE = "[ERROR]";
+
+    private static final String ERROR_INVALID_PRICE= ERROR_MESSAGE+ "%d 단위로 입력해 주세요.";
+    private static final String ERROR_NOT_POSITIVE_NUMBER = ERROR_MESSAGE+" 양수인 값만 입력해 주세요.";
+    private static final String ERROR_RANGE_OVERFLOW= ERROR_MESSAGE+ "입력 가능한 범위를 초과했습니다. %d~%d 사이의 값을 입력해 주세요.";
+    private static final String ERROR_NOT_LOTTO_LENGTH= ERROR_MESSAGE + " %d 개의 숫자를 입력해 주세요.";
+    private static final String ERROR_OVERLAPPED_BONUS_NUMBER= ERROR_MESSAGE+ " 당첨 번호와 보너스 번호가 중복됩니다.";
+
     public static final int START_INCLUSIVE = 1;
     public static final int END_INCLUSIVE = 45;
     public static final int LOTTO_NUMBER_COUNT = 6;
@@ -26,19 +33,19 @@ public class InputConfig {
     public static void isBonusNumberInLotto(Set<Integer> lotto, int bonus) {
         isInRange(bonus);
         if (lotto.contains(bonus)) {
-            throw new IllegalArgumentException(ERROR_MESSAGE);
+            throw new IllegalArgumentException(ERROR_OVERLAPPED_BONUS_NUMBER);
         }
     }
 
     private static void hasCorrectSize(Set<Integer> set) {
         if (set.size() != LOTTO_NUMBER_COUNT) {
-            throw new IllegalArgumentException(ERROR_MESSAGE);
+            throw new IllegalArgumentException(String.format(ERROR_NOT_LOTTO_LENGTH, LOTTO_NUMBER_COUNT));
         }
     }
 
     private static void isInRange(int value) {
         if (!(START_INCLUSIVE <= value && value <= END_INCLUSIVE)) {
-            throw new IllegalArgumentException(ERROR_MESSAGE);
+            throw new IllegalArgumentException(String.format(ERROR_RANGE_OVERFLOW, START_INCLUSIVE, END_INCLUSIVE));
         }
     }
 
@@ -53,19 +60,19 @@ public class InputConfig {
         try {
             return Long.parseLong(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ERROR_MESSAGE);
+            throw new IllegalArgumentException(ERROR_NOT_POSITIVE_NUMBER);
         }
     }
 
     private static void isMoreThanZero(long value) {
         if (value < 0) {
-            throw new IllegalArgumentException(ERROR_MESSAGE);
+            throw new IllegalArgumentException(ERROR_NOT_POSITIVE_NUMBER);
         }
     }
 
     private static void isDividedByPrice(long value) {
         if (value % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException(ERROR_MESSAGE);
+            throw new IllegalArgumentException(String.format(ERROR_INVALID_PRICE, LOTTO_PRICE));
         }
     }
 
