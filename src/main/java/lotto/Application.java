33a5c9011c867259be5f331f9d926e617d53055f
SH <gram753@naver.com>
2022-11-11 23:30:21 +0900

    refactor: 실행 클래스 리팩토링

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,9 @@
 package lotto;
 
-import lotto.system.LottoSystem;
 import lotto.system.NewLottoSystem;
 
 public class Application {
 	public static void main(String[] args) {
-		// LottoSystem lottoSystem = new LottoSystem();
-		// lottoSystem.lottoLogic();
-
 		NewLottoSystem lottoSystem = new NewLottoSystem();
 		lottoSystem.lottoLogic();
 	}
