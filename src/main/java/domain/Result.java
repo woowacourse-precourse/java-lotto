51c8d5b34dd5209a9593778f6ab1ebaa7efcc86c
Limjongho <whdgh9873@icloud.com>
2022-11-11 20:05:12 +0900

    feat(Result): 랭킹 조회 기능 추가

--- src/main/java/domain/Result.java
+++ src/main/java/domain/Result.java
@@ -6,6 +6,14 @@ import java.util.Objects;
 
 public class Result {
 
+    public Rank getRank(int matchingCount, boolean isMatchingBonusNumber) {
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank.matchCount == matchingCount)
+                .filter(rank -> rank.checkBonusNumber == isMatchingBonusNumber)
+                .findAny()
+                .orElse(Rank.nothing);
+    }
+
     public int countMatchingNumber(List<Integer> winningLottoNumbers, List<Integer> userNumbers) {
         int matchingCount = 0;
         for (int number : winningLottoNumbers) {
