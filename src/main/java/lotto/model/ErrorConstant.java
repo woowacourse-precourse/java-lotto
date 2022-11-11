baf9e2056fba8afa943356816f5afbc745f1d022
Cyma-s <ssol0319@naver.com>
2022-11-11 17:10:37 +0900

    feat(ErrorConstant): add ErrorConstant class

---
+++ src/main/java/lotto/model/ErrorConstant.java
@@ -0,0 +1,7 @@
+package lotto.model;
+
+public class ErrorConstant {
+    public static String ERROR_HEADER = "[ERROR] ";
+    public static String INVALID_DIGITS_COUNT = ERROR_HEADER + "로또 번호는 6자리여야 합니다.";
+
+}
