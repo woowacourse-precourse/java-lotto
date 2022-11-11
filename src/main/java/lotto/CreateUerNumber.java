1007dded88c16374c3919899622be2aea74566c8
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-11 14:51:24 +0900

    fix(CreateUserNumber) : makeWinningNumber 수정

--- src/main/java/lotto/CreateUerNumber.java
+++ src/main/java/lotto/CreateUerNumber.java
@@ -5,6 +5,13 @@ import java.util.List;
 
 public class CreateUerNumber {
       public List<Integer> makeWinningNumber(String userInput){
-
+        Exception checkLottoNumber = new Exception();
+        List<Integer> WinningNumber = new ArrayList<>();
+        checkLottoNumber.validateUserInput(userInput);
+        String [] splitNumber = userInput.split(",");
+        for (String num : splitNumber) {
+            WinningNumber.add(Integer.parseInt(num));
+        }
+        return WinningNumber;
     }
 }
