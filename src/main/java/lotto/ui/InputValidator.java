8c62f57c07a150944c61a07a9b5093ca655a70f4
lee <whddlsk123@naver.com>
2022-11-11 19:37:16 +0900

    feat(validateInputRange) : 입력 숫자가 1 ~ 45 사이인지 검사 범위를 벗어날시 예외 반환

--- src/main/java/lotto/ui/InputValidator.java
+++ src/main/java/lotto/ui/InputValidator.java
@@ -1,11 +1,20 @@
 package lotto.ui;
 
+import java.util.List;
 
 public class InputValidator {
 
     public void validateInputDigit(String userInput) {
         for(char input : userInput.toCharArray()) {
             if(!Character.isDigit(input)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public void validateInputRange(List<Integer> userInput) {
+        for(int input : userInput) {
+            if(input < 1 && input > 45) {
                 throw new IllegalArgumentException();
             }
         }
