2cedf01a11b28c4ddaa2d5432a4479dcf784ffac
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-11 09:19:42 +0900

    feat: 구입 금액이 규칙에서 지정한 단위인지 검증하는 기능 추가

--- src/main/java/lotto/validator/LottoPurchaseValidator.java
+++ src/main/java/lotto/validator/LottoPurchaseValidator.java
@@ -1,7 +1,12 @@
 package lotto.validator;
 
+import static lotto.constant.Constants.UNIT_OF_LOTTO_PURCHASE;
+
 public class LottoPurchaseValidator {
 
     public static void validatePurchaseUnit(int money) {
+        if (money % UNIT_OF_LOTTO_PURCHASE != 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }
