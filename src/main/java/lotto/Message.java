d7ee8380fe424bf588a86a9f929b29aebb9273be
kevstevie <jianna442@gmail.com>
2022-11-11 15:01:30 +0900

    feat: error 메시지 추가

--- src/main/java/lotto/Message.java
+++ src/main/java/lotto/Message.java
@@ -15,7 +15,8 @@ public enum Message {
     ERROR_INPUT_NUMBER("숫자를 입력해야 합니다."),
     ERROR_INPUT_1000("1000원 단위로 입력해야 합니다."),
     ERROR_INPUT_SIX("숫자를 여섯개 입력해야 합니다."),
-    ERROR_INPUT_DUPLICATE("중복되지 않은 숫자를 입력해야 합니다.");
+    ERROR_INPUT_DUPLICATE("중복되지 않은 숫자를 입력해야 합니다."),
+    ERROR_INPUT_RANGE("1부터 45사이의 숫자를 입력해야 합니다.");
 
 
     final String msg;
