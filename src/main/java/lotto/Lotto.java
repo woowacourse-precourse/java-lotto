e99cd25057b50bb7a45bec7198becf39a2839e82
Ji-Un-Gil <wldns2577@gmail.com>
2022-11-11 18:03:41 +0900

    feat(Lotto.java): get_numbers, compare_win function Added function: - Lotto class Getter 기능과, 로또 당첨번호와 일치하는 갯수를 구하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,7 +2,6 @@ package lotto;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,6 +25,10 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> get_numbers(){
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6 || numbers.size() != numbers.stream().distinct().count()) {
             System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 정수여야 하며 중복은 불가합니다.");
@@ -38,4 +41,13 @@ public class Lotto {
         System.out.println(Arrays.deepToString(numbers.toArray()));
     }
     
+    public int compare_win(Lotto win){
+        int count = 0;
+        for (int i = 0; i < numbers.size(); i++){
+            if (win.get_numbers().contains(numbers.get(i))){
+                count++;
+            }
+        }
+        return count;
+    }
 }
