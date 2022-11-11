c33aeab1564748126aca7d797a3c3d60eb6db921
ddubidubap <park62000@gmail.com>
2022-11-11 13:37:04 +0900

    test: 입력값이 숫자가 아닐 경우와 1~45 사이의 값이 아닐 경우 예외 발생 테스트

---
+++ src/test/java/lotto/InputValidatorTest.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import lotto.domain.InputValidator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputValidatorTest {
+    InputValidator inputValidator = new InputValidator();
+
+    @DisplayName("구입 금액이 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void createMoneyByString() {
+        assertThatThrownBy(() -> inputValidator.validateNumeric("fff"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 숫자 외의 문자가 존재하면 예외가 발생한다.")
+    @Test
+    void createWinningNumbersByString() {
+        assertThatThrownBy(() -> inputValidator.validateWinningNumbers("1,2,3,a,b,c"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호 중 0 이하 또는 46 이상의 번호가 존재하면 예외가 발생한다.")
+    @Test
+    void createWinningNumbersByWrongNumber() {
+        assertThatThrownBy(() -> inputValidator.validateWinningNumbers("1,2,3,46,4,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
