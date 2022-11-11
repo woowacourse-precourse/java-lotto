f264383b5db2e33045f99732b9567bb6b38d2f17
Limjongho <whdgh9873@icloud.com>
2022-11-11 19:06:03 +0900

    test(InputConverter): 구입 금액 변환 기능 테스트 추가

--- src/test/java/domain/InputConverterTest.java
+++ src/test/java/domain/InputConverterTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static util.Constant.*;
 
 class InputConverterTest {
 
@@ -38,5 +39,40 @@ class InputConverterTest {
         int bonusNumber = InputConverter.getNumber(bonusNumberInput);
 
         Assertions.assertThat(bonusNumber).isEqualTo(7);
+    }
+
+    @DisplayName("구매 금액에 숫자가 아닌 값이 포함되어 있으면 예외가 발생한다.")
+    @Test
+    void enterNotNumeralMoney() {
+        String money = "1000a";
+        Assertions.assertThatThrownBy(() -> InputConverter.getMoney(money))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액에 숫자가 아닌 값이 포함되어 있습니다.");
+    }
+
+    @DisplayName("구매 금액의 단위가 1000원 단위가 아니면 예외가 발생한다.")
+    @Test
+    void enterNotValidMoneyUnit() {
+        String money = "1001";
+        Assertions.assertThatThrownBy(() -> InputConverter.getMoney(money))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 1000원 단위로 입력되어야 합니다.");
+    }
+
+    @DisplayName("보너스 숫자에 숫자가 아닌 값이 입력되면 에외가 발생한다.")
+    @Test
+    void enterNotNumeralBonusNumber() {
+        String bonusNumber = "a";
+        Assertions.assertThatThrownBy(() -> InputConverter.getNumber(bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 숫자가 숫자가 아닌 값으로 입력되었습니다.");
+    }
+
+    @DisplayName("보너스 숫자가 범위 외의 숫자인 경우 예외가 발생한다.")@Test
+    void enterOutBoundBonusNumber() {
+        String bonusNumber = "46";
+        Assertions.assertThatThrownBy(() -> InputConverter.getNumber(bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1~45 범위의 숫자여야 합니다.");
     }
 }
