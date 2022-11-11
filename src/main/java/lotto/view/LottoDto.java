7be5f5761a4d3aa8113f855f0cfb3116f1ba59db
kimJaeHyeon <kmr2644@gmail.com>
2022-11-11 16:01:06 +0900

    feat: 랜덤으로 생성된 로또번호를 가지는 LottoDto 구현

--- src/main/java/lotto/view/LottoDto.java
+++ src/main/java/lotto/view/LottoDto.java
@@ -1,2 +1,16 @@
-package lotto.view;public class LottoDto {
+package lotto.view;
+
+import java.util.List;
+
+public class LottoDto {
+    private List<List<Integer>> lotteries;
+
+    public LottoDto(List<List<Integer>> lotteries) {
+        this.lotteries = lotteries;
+    }
+
+    public List<List<Integer>> getLotteries() {
+        return lotteries;
+    }
+
 }
