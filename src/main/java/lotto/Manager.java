fb901a62efbf655152c33ea9656e0daf6b633762
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-11 20:26:10 +0900

    feat: 로또 구매 기능 추가

--- src/main/java/lotto/Manager.java
+++ src/main/java/lotto/Manager.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
 
 public class Manager {
+    private List<Lotto> lottoTable = new ArrayList<>();
 
     public List<Integer> getRandomNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
@@ -16,5 +18,12 @@ public class Manager {
             throw new IllegalArgumentException();
         }
         return won / Lotto.PRICE;
+    }
+
+    public void purchaseLotto(int amount){
+        for(int i = 0; i < amount; i++){
+            Lotto lotto = new Lotto(getRandomNumbers());
+            lottoTable.add(lotto);
+        }
     }
 }
