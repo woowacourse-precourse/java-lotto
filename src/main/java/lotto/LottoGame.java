766fa9dd9463a3a685c09c985ce4345b18824488
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-11 21:23:09 +0900

    feat: 구입 금액에 따른 로또 개수 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -4,6 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class LottoGame {
     private static final String TYPE_ERROR_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
+    private static final String INPUT_MONEY_ERROR_MESSAGE = "[ERROR] 금액을 1000원 단위로 입력해주세요.";
+
+    public int countLotto(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException(INPUT_MONEY_ERROR_MESSAGE);
+        }
+        return money / 1000;
+    }
 
     public int inputMoney() {
         try {
