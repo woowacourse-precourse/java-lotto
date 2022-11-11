6c3b39743ea1e991697d16270784554b2babf6a5
rohsik2 <rohsik2@gmail.com>
2022-11-11 12:58:56 +0900

    feat: validation code for user input split method

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -19,7 +19,17 @@ public class Application {
                 throw new IllegalArgumentException("[ERROR] 숫자 사이는 ,로 구분되어야 합니다.");
             }
         }
+        lotteryValidationCheck(winnings);
 
         return winnings;
+    }
+
+    public static void lotteryValidationCheck(List<Integer> lotteryNumbers){
+        for(Integer i : lotteryNumbers){
+            if(i < 1 || i > 45)
+                throw new IllegalArgumentException("[ERROR] 숫자는 1 ~ 45 중 하나의 숫자여야 합니다.");
+        }
+        if(lotteryNumbers.size() != 6)
+            throw new IllegalArgumentException("[ERROR] 숫자는 6개 이어야 합니다.");
     }
 }
