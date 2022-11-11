ef21ca147f91b4c4b1ee668bfc1883ae10530737
HwangSeokJin <ghkd3703@gmail.com>
2022-11-11 15:10:34 +0900

    test(LottoSystem): 당첨 번호 입력 시 숫자가 6개 입력되었는지 확인하는 테스트 코드 작성

--- src/test/java/lotto/domain/LottoSystemTest.java
+++ src/test/java/lotto/domain/LottoSystemTest.java
@@ -11,12 +11,25 @@ class LottoSystemTest {
     @DisplayName("당첨 번호 임력 시 숫자 이외에 값이 있는지 확인한다.")
     @Test
     void InputInvalidType() {
-        String[] invalidTypes = {
+        String[] invalidInput = {
                 "1, 2, 3, 4, 6, w",
                 "1, e, r, @, 1, 2",
         };
-        for (String invalidInput : invalidTypes) {
-            assertThatThrownBy(() -> system.setWinningNumbers(invalidInput))
+        for (String input : invalidInput) {
+            assertThatThrownBy(() -> system.setWinningNumbers(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @DisplayName("당첨 번호 임력 시 숫자가 6개 입력되었는지 확인한다")
+    @Test
+    void InputInvalidCount() {
+        String[] invalidInput = {
+                "1, 2, 3, 4",
+                "1, 2, 3, 4, 5, 6, 7",
+        };
+        for (String input : invalidInput) {
+            assertThatThrownBy(() -> system.setWinningNumbers(input))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
