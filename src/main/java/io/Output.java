239edfca88f4230211ba7bf5892b17c99fda9d6a
miffyKing <whqudghk28@gmail.com>
2022-11-11 22:07:51 +0900

    refactor: feat(io): Change return value (#7)
    Output class methods returned String but changed to void. no need to
    return anything in output class.

--- src/main/java/io/Output.java
+++ src/main/java/io/Output.java
@@ -1,5 +1,6 @@
 package io;
 
 public interface Output {
-    String printOutput(String sentence);
+    void printOutput(String sentence);
+
 }
--- src/main/java/io/OutputScreen.java
+++ src/main/java/io/OutputScreen.java
@@ -3,9 +3,7 @@ package io;
 public class OutputScreen implements Output{
 
     @Override
-    public String printOutput(String sentence) {
-
+    public void printOutput(String sentence) {
         System.out.println(sentence);
-        return null;
     }
 }
