4ad5358ff20e3406c166b2d3350f6a8cd09ac4d5
yeonhee <ydol2@naver.com>
2022-11-12 03:33:22 +0900

    chore(Lotto) : 로또 수 검증 로직 호출 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,17 +3,19 @@ package lotto.domain;
 import java.util.List;
 
 import static lotto.view.InputValidator.validateDifferentLottoNumbers;
+import static lotto.view.InputValidator.validateRangeLottoNumbers;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        validateDifferentLottoNumbers(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
+        validateDifferentLottoNumbers(numbers);
+        validateRangeLottoNumbers(numbers);
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
