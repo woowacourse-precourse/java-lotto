b8789442aeee746f95f6664cc447dc2bc3c89417
hyun-seung <ardimento22@naver.com>
2022-11-11 17:48:57 +0900

    [FEAT] 당첨 번호를 입력 받는다.

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
 import lotto.domain.Money;
 import lotto.utils.ExceptionType;
 
@@ -12,12 +15,32 @@ public class Input {
         return new Money(convertedInput);
     }
 
-    public int stringToInt(String target) {
+    public Lotto getWinningLotto() {
+        Lotto winningLotto = getWinningLottoNumbers();
+        return winningLotto;
+    }
+
+    public Lotto getWinningLottoNumbers() {
+        String input = Console.readLine();
+        List<Integer> numbers = stringToIngerListByComma(input);
+        return new Lotto(numbers);
+    }
+
+    private int stringToInt(String target) {
         try {
             return Integer.parseInt(target);
         } catch (NumberFormatException e) {
             Output.printErrorAndExit(ExceptionType.IS_NOT_NUMBER.getMessage());
             throw new IllegalArgumentException(ExceptionType.IS_NOT_NUMBER.getMessage());
         }
+    }
+
+    private List<Integer> stringToIngerListByComma(String input) {
+        List<Integer> returns = new ArrayList<>();
+        String[] split = input.split(",");
+        for(String number : split) {
+            returns.add(stringToInt(number));
+        }
+        return returns;
     }
 }
