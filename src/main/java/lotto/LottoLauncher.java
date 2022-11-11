d7296e1c980f458972795dcde3387427e79ee91b
박준용 <jo101044@naver.com>
2022-11-11 20:50:29 +0900

    feat: "LottoLauncher 생성"
    로또 프로그램을 실행 시켜주는 객체
    startLotto() : 프로그램을 실행 시켜줌,
    inputMoney() : 구입금액을 입력받고 LottoList 를 리턴,
    create1stLotto() : 당첨 번호를 입력받고 Lotto 를 리턴,
    createBonusNumber() : 보너스 번호를 입력받고 int 를 리턴,
    castInt() : 숫자가 아닌 값을 입력시 예외 발생

---
+++ src/main/java/lotto/LottoLauncher.java
@@ -0,0 +1,47 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoLauncher {
+    public void startLotto(){
+        LottoList lottoList = inputMoney();
+
+        System.out.println(lottoList);
+
+        Lotto lotto_1st = create1stLotto();
+        int bonusNumbers = createBonusNumber();
+        CompareLotto compareLotto = new CompareLotto(lottoList, lotto_1st, bonusNumbers);
+
+        System.out.println(compareLotto);
+    }
+
+    private LottoList inputMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
+
+        int money = castInt(Console.readLine());
+
+        return new LottoList(money);
+    }
+
+    private Lotto create1stLotto(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+
+        String numbers = Console.readLine();
+
+        return new Lotto(numbers);
+    }
+
+    private int createBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+
+        return castInt(Console.readLine());
+    }
+
+    private int castInt(String readLine) {
+        try {
+            return Integer.parseInt(readLine);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR]");
+        }
+    }
+}
