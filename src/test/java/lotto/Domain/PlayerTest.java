5324cf2402d6bffb4509346f7102a4359a8e91c2
Park shinyeong <syngh503@gmail.com>
2022-11-12 01:04:44 +0900

    Test(PlayerTest): setLottos 테스트 케이스 생성
    LottoTest에서 생성한 setLottos 테스트 케이스를 PlayerTest로 옮겼습니다.
    
    Relate to: #15

--- src/test/java/lotto/Domain/PlayerTest.java
+++ src/test/java/lotto/Domain/PlayerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PlayerTest {
@@ -30,6 +32,28 @@ class PlayerTest {
             int expect = 2000;
 
             assertThat(result).isEqualTo(expect);
+        }
+    }
+
+
+    @Nested
+    @DisplayName("SetLottos 메소드 테스트")
+    class TestSetLottos {
+
+        @Test
+        @DisplayName("구입 횟수가 1번일 때")
+        void case1() {
+            int lottoCount = 1;
+            player.setLottos(lottoCount);
+            assertThat(player.getLottos().size()).isEqualTo(1);
+        }
+
+        @Test
+        @DisplayName("구입 횟수가 여러번일 때")
+        void case2() {
+            int lottoCount = 9;
+            player.setLottos(lottoCount);
+            assertThat(player.getLottos().size()).isEqualTo(9);
         }
     }
 }
