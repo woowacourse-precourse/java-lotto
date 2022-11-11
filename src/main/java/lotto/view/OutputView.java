a87f730550f8a8ac07c04770ab05806e13abb4b6
khsrla9806 <kimhunsope@naver.com>
2022-11-11 20:36:48 +0900

    feat(OutputView): 당첨 통계와 수익률을 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import lotto.domain.Lotto;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class OutputView {
@@ -13,5 +14,16 @@ public class OutputView {
             System.out.println(myLotto.get(count).getLottoNumbers().toString());
         }
         System.out.println();
+    }
+    public static void printLottoResult(List<Integer> result, double earningsRate) {
+        BigDecimal rate = new BigDecimal(earningsRate);
+        System.out.println("당첨통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + result.get(5) + "개");
+        System.out.println("4개 일치 (50,000원) - " + result.get(4) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + result.get(3) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + result.get(2) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + result.get(1) + "개");
+        System.out.println("총 수익률은 " + String.format("%.1f", rate) + "%입니다.");
     }
 }
