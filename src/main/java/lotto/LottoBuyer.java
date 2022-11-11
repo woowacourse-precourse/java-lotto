aba74fd875826294e8d14ffb6f6a6f0e83aef6bc
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-12 05:37:47 +0900

    Fix(LottoBuyer) : 클래스에 인스턴스 변수를 추가및 메소드가 변경됐습니다. #52
    private Lotto 변수를 추가했습니다.
    private BigInteger 변수를 추가했습니다.
    
    public static BigInteger inputLottoNumber()를
    public void setMoney()로 변경했습니다.
    이 메소드는 인스턴스 변수인 money변수의 값을 대입합니다.
    
    public static List<Integer> inputLottoNumbers()를
    public void setLottoNumbers()로 변경했습니다.
    이 메소드는 인스턴스 변수인 lotto변수의 값을 대입합니다.
    
    Resolves : #46, #48
    Ref : #46, #48

--- src/main/java/lotto/LottoBuyer.java
+++ src/main/java/lotto/LottoBuyer.java
@@ -2,6 +2,7 @@ package lotto;
 
 import lotto.CheckInputException;
 import lotto.Util;
+import lotto.Lotto;
 
 import java.util.List;
 import java.math.BigInteger;
@@ -10,7 +11,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class LottoBuyer {
 
-    public static BigInteger inputMoney() throws IllegalArgumentException{
+    private Lotto lotto;
+    private BigInteger money;
+
+    public LottoBuyer(){
+        lotto = null;
+        money = null;
+    }
+    public void setMoney() throws IllegalArgumentException{
+        System.out.println("구입금액을 입력해주세요.");
         final String input = Console.readLine();
 
         try {
@@ -20,20 +29,20 @@ public class LottoBuyer {
         } catch (IllegalArgumentException iae) {
             throw iae;
         }
-        return new BigInteger(input);
+        money =  new BigInteger(input);
     }
 
-    public static List<Integer> inputLottoNumbers() throws IllegalArgumentException{
+    public void setLottoNumbers() throws IllegalArgumentException{
+        System.out.println("당첨 번호를 입력해주세요.");
         final String input = Console.readLine();
         List<Integer> inputNumbers;
 
         try{
             CheckInputException.checkBuyerInputIsNotSixNumbers(input);
             inputNumbers = Util.splitInteger(input, ",");
+            lotto = new Lotto(inputNumbers);
         }catch (IllegalArgumentException iae){
             throw iae;
         }
-        System.out.println(inputNumbers);
-        return inputNumbers;
     }
 }
