bd91bce82a875d3982673adcee421ed30ed42566
Your Name <you@example.com>
2022-11-11 14:14:07 +0900

    feat(LottoTest): 로또 출력 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,14 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -22,6 +25,19 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
+    
     // 아래에 추가 테스트 작성 가능
+    
+    @DisplayName("로또 출력 확인")
+    @Test
+    void testPrintLotto() {
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        lotto.printLotto();
+
+        assertEquals("[1, 2, 3, 4, 5, 6]", out.toString().trim());
+    }
+
 }
