605a3facad81a90a99c64f0a5941e60587080e6a
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-11 17:37:56 +0900

    fix<Lotto>: 예외 메세지 추가
    - 6개의 숫자가 아닐 경우 예외 발생시 메세지 추가
    - 로또 가격 상수 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,31 @@
 package lotto.model;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+import lotto.util.LottoExceptionMessage;
 
 public class Lotto {
-    private final Set<Integer> numbers;
+    public static final int PRICE = 1000;
+    private final Set<LottoNumber> numbers;
 
-    public Lotto(Set<Integer> numbers) {
+    public Lotto(Set<LottoNumber> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(Set<Integer> numbers) {
+    public Set<LottoNumber> getNumbers() {
+        return numbers.stream().collect(Collectors.toSet());
+    }
+
+    private void validate(Set<LottoNumber> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LottoExceptionMessage.DUPLICATE_LOTTO_NUMBER.getMessage());
         }
     }
 
     // TODO: 추가 기능 구현
+    @Override
+    public String toString() {
+        return String.valueOf(numbers);
+    }
 }
