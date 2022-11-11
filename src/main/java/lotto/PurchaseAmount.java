37de02b530aa0942bd6a8ac6e96b330630b0d4b5
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-12 01:40:12 +0900

    feat(PurchaseAmount): 입력 값이 존재하고 숫자인지 확인하는 기능 추가
    입력된 값이 존재하고 숫자인지 확인한다.
    만약, 두 조건에 만족하지 않을 시 금액을 투입해야 한다는 메시지와 함께 예외가 발생한다.
    docs: 숫자인지 확인 & 입력 값이 없거나 숫자가 아닌 경우 예외 처리 완료 체크

--- src/main/java/lotto/PurchaseAmount.java
+++ src/main/java/lotto/PurchaseAmount.java
@@ -4,11 +4,26 @@ public class PurchaseAmount {
     private final int amount;
 
     public PurchaseAmount(String amount) {
+        validate(amount);
         this.amount = convertStringToInt(amount);
     }
 
     public int getAmount() {
         return amount;
+    }
+
+    private void validate(String amount) {
+        if (isStringEmpty(amount) || !isStringDigit(amount)) {
+            throw new IllegalArgumentException(ExceptionMessage.PURCHASE_AMOUNT_IS_NOT_NUMBER.getMessage());
+        }
+    }
+
+    private boolean isStringEmpty(String amount) {
+        return amount == null || amount.isBlank();
+    }
+
+    private boolean isStringDigit(String amount) {
+        return amount.chars().allMatch(Character::isDigit);
     }
 
     private int convertStringToInt(String amount) {
