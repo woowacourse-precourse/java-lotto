a6f8d59f722da4f8875a32b616438e72beae6f2c
박원종 <wonjong1030@naver.com>
2022-11-11 15:03:08 +0900

    feat: 등수 계산/찾기 기능 구현

--- src/main/java/lotto/Calculator.java
+++ src/main/java/lotto/Calculator.java
@@ -4,4 +4,11 @@ public class Calculator {
     public int getLottoCount(int money) {
         return money / 1000;
     }
+
+    public Rank calculateRank(int correctCount, boolean bonus) {
+        if (correctCount != 5) {
+            bonus = false;
+        }
+        return Rank.findByCorrectCountAndBonus(correctCount, bonus);
+    }
 }
--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,7 +1,35 @@
 package lotto;
 
-public enum RankEnum {
+public enum Rank {
+    FIRST(6, 2000000000, false),
+    SECOND(5, 30000000, true),
+    THIRD(5, 1500000, false),
+    FOURTH(4, 50000, false),
+    FIFTH(3, 5000, false);
 
-    int rank;
-    
+
+    int correctCount;
+    int money;
+    boolean bonus;
+
+    Rank (int correctCount, int money, boolean bonus) {
+        this.correctCount = correctCount;
+        this.money = money;
+        this.bonus = bonus;
+    }
+    public static Rank findByCorrectCountAndBonus(int correctCount, boolean bonus) {
+        if (correctCount == 5) {
+            if (bonus) {
+                return SECOND;
+            }
+            return THIRD;
+        }
+
+        for (Rank value : values()) {
+            if (value.correctCount == correctCount) {
+                return value;
+            }
+        }
+        return null;
+    }
 }
