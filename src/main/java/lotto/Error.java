a944f09798b0c06c846c5a2b1a8efc364435e9c5
dooboocookie <bkh3030@gmail.com>
2022-11-11 19:19:18 +0900

    feat: 문자열 입력과 숫자 입력을 받는 기능 추가

--- src/main/java/lotto/Error.java
+++ src/main/java/lotto/Error.java
@@ -1,7 +1,8 @@
 package lotto;
 
 public enum Error {
-    ERROR_GAME_MONEY_AVAILABLE("[ERROR] 로또 1매 가격은 1000원입니다. 1000원 단위를 맞춰주세요.");
+    ERROR_GAME_MONEY_AVAILABLE("[ERROR] 로또 1매 가격은 1000원입니다. 1000원 단위를 맞춰주세요."),
+    ERROR_INPUT_PARSING_INT("[ERROR] 해당 입력 값은 숫자형이 아닙니다.");
 
     private String errorMessage;
 
--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,4 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
+
+    public static String readString() {
+        return Console.readLine();
+    }
+
+    public static int readInteger() {
+        try {
+            return Integer.parseInt(readString());
+        } catch (Exception e) {
+            throw new IllegalArgumentException(Error.ERROR_INPUT_PARSING_INT.getValue());
+        }
+    }
 }
