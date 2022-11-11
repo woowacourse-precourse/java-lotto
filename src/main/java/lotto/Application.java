b43371f34ca03721cfe09d82972a5c853337c619
khj1 <dhepd8849@gmail.com>
2022-11-11 20:33:23 +0900

    feat: 예외 상황 시 에러 문구를 출력하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,15 @@
 package lotto;
 
 import lotto.controller.LottoMachine;
+import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        LottoMachine lottoMachine = new LottoMachine();
-        lottoMachine.purchaseLotto();
+        try {
+            LottoMachine lottoMachine = new LottoMachine();
+            lottoMachine.purchaseLotto();
+        } catch (IllegalArgumentException error) {
+            OutputView.printErrorMessage(error);
+        }
     }
 }
