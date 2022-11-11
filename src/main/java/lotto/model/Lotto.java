eb6c3fa97d50de5911b776e9e2c9654ce7b43738
chanwonlee <clw8679@naver.com>
2022-11-11 21:58:54 +0900

    feat: Lotto 모델 검증 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -8,21 +8,34 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        validateLotto(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validateLotto(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplication(numbers);
-        this.numbers = numbers;
+        validateRange(numbers);
     }
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또의 개수는 6개 이어야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호 개수는 6개 이어야 합니다.");
         }
     }
 
     private void validateDuplication(List<Integer> numbers) {
         Set<Integer> removeDuplication = new HashSet<>(numbers);
         if (numbers.size() != removeDuplication.size()) {
-            throw new IllegalArgumentException("[ERROR] 로또의 번호들은 중복되면 안됩니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호들은 중복되면 안됩니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
         }
     }
 }
