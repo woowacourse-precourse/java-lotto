da6606176b2e484d79495425260c16a9f662dd51
khj <bbb6254177@gmail.com>
2022-11-11 18:21:04 +0900

    test: add test for validate function

---
+++ src/test/java/lotto/UserTest.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class UserTest {
+    @DisplayName("구입 금액이 1000으로 나눠 떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void createAmountNotdivided1000() {
+        assertThatThrownBy(() -> new User(14500))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
