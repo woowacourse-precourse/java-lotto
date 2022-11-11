03f4b22d06f89df33b43919eb45fe090fafbd664
xi_jjun <rlawowns97@naver.com>
2022-11-11 19:17:32 +0900

    feat: 사용자가 구매한 만큼의 로또 번호들을 생성하는 기능 추가

--- src/main/java/lotto/constants/Constant.java
+++ src/main/java/lotto/constants/Constant.java
@@ -6,4 +6,5 @@ public class Constant {
 	public static final int LOTTO_START_NUMBER = 1;
 	public static final int LOTTO_END_NUMBER = 45;
 	public static final int LOTTO_NUMBER_LENGTH = 6;
+	public static final int LOTTO_PRICE = 1_000;
 }
--- src/main/java/lotto/models/LottoFactory.java
+++ src/main/java/lotto/models/LottoFactory.java
@@ -2,7 +2,10 @@ package lotto.models;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static lotto.constants.Constant.*;
 
@@ -16,5 +19,15 @@ public class LottoFactory {
 		List<Integer> lottoNumber = createUniqueLottoNumber();
 
 		return new Lotto(lottoNumber);
+	}
+
+	public List<Lotto> issueLottoNumbersByPurchaseAmount(int purchaseAmount) {
+		return IntStream.iterate(purchaseAmount, this::hasEnoughMoney, remainedMoney -> remainedMoney - LOTTO_PRICE)
+				.mapToObj(seq -> issueLottoNumber())
+				.collect(Collectors.toList());
+	}
+
+	private boolean hasEnoughMoney(int remainedMoney) {
+		return remainedMoney >= LOTTO_PRICE;
 	}
 }
