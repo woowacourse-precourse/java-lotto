85e38462b72c77877e7a61d2e11a66eba64cc6f2
goldcollar95 <yjs7298@naver.com>
2022-11-11 11:29:59 +0900

    feat(User): 중복된 숫자가 오지않도록 repeatDestroy()메서드 구현(Set을 이용)

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -3,7 +3,9 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -27,15 +29,17 @@ public class User {
         lottoList = readLine();
     }
 
-    public List<Integer> lottoToList(String UserIn){
+    public List<Integer> lottoToList(String UserIn, String User, List<Integer> numberResult){
 
         String lottoList = new String();
         List<Integer> lottoToList = new ArrayList<>();
+        Set<Integer> repeatDestroy = new HashSet<Integer>(lottoToList);
 
         if(!lottoList.equals(UserIn)){
             lottoToList.contains(lottoList);
         }
-        return lottoToList;
+
+        return (List<Integer>) repeatDestroy;
     }
 
 }
