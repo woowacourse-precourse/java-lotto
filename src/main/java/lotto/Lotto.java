1ace147c44410483ee29015d99902dcb0ad432a7
ykmxxi <ykmxxi97@gmail.com>
2022-11-11 11:08:51 +0900

    feat: 로또를 자동 발행하는 기능 skeleton code 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,5 +16,6 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void ascending() {
+    }
 }
---
+++ src/main/java/lotto/LottoAutomaton.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoAutomaton extends Lotto {
+    public LottoAutomaton(List<Integer> numbers) {
+        super(numbers);
+    }
+
+    public void getCount() {
+    }
+
+    public void createNumbers() {
+    }
+}
