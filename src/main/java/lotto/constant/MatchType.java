3470085fa0bb6c64b9bc0b35aa3cf8b85f7937e2
minju412 <ln8847@ajou.ac.kr>
2022-11-11 15:53:13 +0900

    refactor: formatting strings

--- src/main/java/lotto/constant/MatchType.java
+++ src/main/java/lotto/constant/MatchType.java
@@ -1,19 +1,22 @@
 package lotto.constant;
 
+import java.util.function.Function;
+
 public enum MatchType {
-    THREE_MATCHES("3개 일치 (5,000원) - %d개\n"),
-    FOUR_MATCHES("4개 일치 (50,000원) - %d개\n"),
-    FIVE_MATCHES_WITHOUT_BONUS("5개 일치 (1,500,000원) - %d개\n"),
-    FIVE_MATCHES_WITH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
-    SIX_MATCHES("6개 일치 (2,000,000,000원) - %d개\n");
 
-    private final String message;
+    THREE_MATCHES(count -> "3개 일치 (%,d원) - " + count + "개\n"),
+    FOUR_MATCHES(count -> "4개 일치 (%,d원) - " + count + "개\n"),
+    FIVE_MATCHES_WITHOUT_BONUS(count -> "5개 일치 (%,d원) - " + count + "개\n"),
+    FIVE_MATCHES_WITH_BONUS(count -> "5개 일치, 보너스 볼 일치 (%,d원) - " + count + "개\n"),
+    SIX_MATCHES(count -> "6개 일치 (%,d원) - " + count + "개\n");
 
-    MatchType(String message) {
-        this.message = message;
+    private final Function<Integer, String> template;
+
+    MatchType(Function<Integer, String> template) {
+        this.template = template;
     }
 
-    public String getMessage() {
-        return this.message;
+    public String printMessage(Integer input) {
+        return this.template.apply(input);
     }
 }
