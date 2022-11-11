b792fa22d766bb6fc0d34e5cca9a68baad9372e8
alstjr706 <alstjr706@naver.com>
2022-11-12 03:24:37 +0900

    feat : (로또 게임) 당첨 통계의 로그를 위한 enum 클래스 구현

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000);
+
+    private final int match;
+    private final int money;
+
+    Rank(int match, int money) {
+        this.match = match;
+        this.money = money;
+    }
+
+    public int getMatch() {
+        return match;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}
