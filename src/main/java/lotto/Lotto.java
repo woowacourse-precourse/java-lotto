ab846d210b26d1997fe32955add98ae46174b087
abbd2 <ajkety00@gmail.com>
2022-11-11 18:18:52 +0900

    Feat(Lotto): 1 ~ 45까지의 숫자 중 6개의 숫자를 랜덤하게 생성하는 메서드 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,11 @@
 package lotto;
 
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
     private final List<Integer> numbers;
+    ExceptionHandling exceptionHandling = new ExceptionHandling();
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -14,6 +16,11 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public List<Integer> createLottoNumber(){
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return lottoNumbers;
     }
 
     // TODO: 추가 기능 구현
