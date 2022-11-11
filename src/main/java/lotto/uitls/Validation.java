41bf3660fa75e26ad2926cc1f8d96ffcfd16bc1f
yujaemin <woawoa0513@gmail.com>
2022-11-11 21:14:08 +0900

    feat: 로또 구매 금액 유효성 검사 함수 구현
    사용자가 입력한 로또 구매 금액이 유효한 금액인지 검사한다.

--- src/main/java/lotto/uitls/Validation.java
+++ src/main/java/lotto/uitls/Validation.java
@@ -3,15 +3,27 @@ package lotto.uitls;
 public class Validation {
     private static final int LOTTO_PRICE = 1_000;
 
+    public void validateLottoPurchaseAmount(String purchaseAmount) {
+        if (hasNonDigitCharacter(purchaseAmount)) {
+            throw new IllegalArgumentException("[ERROR] 로또 구매 금액에는 숫자 외의 문자가 포함될 수 없습니다.");
+        }
+        if (!isDividedByLottoPrice(purchaseAmount)) {
+            throw new IllegalArgumentException("[ERROR] 로또는 1,000원 단위로만 구매할 수 있습니다.");
+        }
+        if (!isPossiblePrice(purchaseAmount)) {
+            throw new IllegalArgumentException("[ERROR] 0원 이하의 금액은 입력할 수 없습니다.");
+        }
+    }
+
     public boolean hasNonDigitCharacter(String input) {
         return input.chars().allMatch(Character::isDigit);
     }
 
-    public boolean isDividedByLottoPrice(int purchaseAmount) {
-        return purchaseAmount % LOTTO_PRICE == 0;
+    public boolean isDividedByLottoPrice(String purchaseAmount) {
+        return Integer.parseInt(purchaseAmount) % LOTTO_PRICE == 0;
     }
 
-    public boolean isPossiblePrice(int purchaseAmount) {
-        return purchaseAmount > 0;
+    public boolean isPossiblePrice(String purchaseAmount) {
+        return Integer.parseInt(purchaseAmount) > 0;
     }
 }
