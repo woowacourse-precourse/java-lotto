c130381ca5bf9b15487c38fd035d5ada819af0c5
CMHyuk <alsgur9710@naver.com>
2022-11-11 13:15:25 +0900

    test(LottoPurchaseCountTest): 전체 테스트 통과를 위해 @BeforeEach로 초기화

--- src/test/java/lotto/domain/LottoPurchaseCountTest.java
+++ src/test/java/lotto/domain/LottoPurchaseCountTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +9,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoPurchaseCountTest {
 
-    LottoPurchaseCount lottoPurchaseCount = new LottoPurchaseCount();
+    LottoPurchaseCount lottoPurchaseCount;
+
+    @BeforeEach
+    void init() {
+        lottoPurchaseCount = new LottoPurchaseCount();
+    }
 
     @DisplayName("로또 개수 확인 테스트")
     @Test
