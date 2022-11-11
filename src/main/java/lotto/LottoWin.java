fc7fbfc260171048be1a50e08aab82b4e7f9c72d
MuSeung <peter0757@nvaer.com>
2022-11-11 14:13:53 +0900

    feat: LottoWin이 예외가 아닌 경우 예외를 발생시키지 않게하고 1등이 포함된 map을 반환하도록 구현
    LottoWin
        - getPrizesMap : 1등 포함 맵이 리턴 되도록 구현
        - LottoWin : 예외 상황을 구분하는 if 구현

--- src/main/java/lotto/LottoWin.java
+++ src/main/java/lotto/LottoWin.java
@@ -1,10 +1,19 @@
 package lotto;
 
 import java.util.List;
+import java.util.Map;
 
 public class LottoWin {
 
     public LottoWin(List<List<Integer>> quickPicks, List<Integer> lotto, int bonus) {
-        throw new IllegalArgumentException();
+        if(lotto.contains(bonus)) {
+            throw new IllegalArgumentException();
+        }
+        if(bonus < LottoState.START.number() || bonus > LottoState.END.number()){
+            throw new IllegalArgumentException();
+        }
+    }
+    public Map<String,Integer> getPrizesMap() {
+        return Map.of("1st", 1);
     }
 }
