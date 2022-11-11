2099961198408b5d82893d460e783f4d2354f2ec
sosow0212 <sosow0212@naver.com>
2022-11-11 12:36:15 +0900

    refactor : 유저가 로또를 1,000원 단위로 구매했는지 확인해주는 예외 작성

--- src/main/java/lotto/exception/UserBuyingException.java
+++ src/main/java/lotto/exception/UserBuyingException.java
@@ -4,7 +4,7 @@ public class UserBuyingException {
     private final static int LOTTO_PRICE = 1000;
 
     public void validBuyingException(int buyingMoney) {
-        if (buyingMoney % LOTTO_PRICE == 0) {
+        if (buyingMoney % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("구매는 1,000원 단위로 가능합니다.");
         }
     }
