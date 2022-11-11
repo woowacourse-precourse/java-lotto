9cf82c1e0d91e2846156be77431c8ab2b0d528b4
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-11 21:17:10 +0900

    feat: ComputerView 앞뒤 구분을 위해 공백 문자열 출력하는 기능 구현

--- src/main/java/lotto/view/ComputerView.java
+++ src/main/java/lotto/view/ComputerView.java
@@ -22,4 +22,8 @@ public class ComputerView {
     private String scanInput(){
         return Console.readLine();
     }
+
+    private void giveSpaceOnView(){
+        System.out.println();
+    }
 }
