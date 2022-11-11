3178b568e2c65e00f077287cddd2e6242aa473bf
Limjongho <whdgh9873@icloud.com>
2022-11-11 11:16:46 +0900

    feat(Lotto): 로또 번호의 예외사항 처리 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,9 @@ package lotto;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static util.Constant.NUMBER_OF_NUMBERS;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,8 +15,32 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        isValidNumberOfNumbers(numbers);
+        isUniqueNumbers(numbers);
+        isInRangeNumbers(numbers);
+    }
+
+    private void isValidNumberOfNumbers(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != NUMBER_OF_NUMBERS) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자로 이루어져야 합니다.");
+        }
+    }
+
+    private void isUniqueNumbers(List<Integer> winningNumbers) {
+        List<Integer> uniqueNumbers = winningNumbers.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        if (uniqueNumbers.size() != NUMBER_OF_NUMBERS) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+        }
+    }
+
+    private static void isInRangeNumbers(List<Integer> winningNumbers) {
+        for (int number : winningNumbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 범위의 숫자여야 합니다.");
+            }
         }
     }
 
