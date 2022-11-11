3d8eb29393852656b7b68155f85c84140dd3fe90
khsrla9806 <kimhunsope@naver.com>
2022-11-11 20:39:17 +0900

    feat(Application): 로또 프로그램 실행 코드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.domain.LottoManager;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoManager lottoManager = new LottoManager();
+        lottoManager.playLottoProgram();
     }
 }
