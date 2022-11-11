3324cc18a1e6a8e3717463ef0fc1cf454f57fcb6
abbd2 <ajkety00@gmail.com>
2022-11-11 14:52:20 +0900

    Feat(ShowMessage): ShowMessage 클래스 생성, "구입 금액을 입력해 주세요."를 출력하는 showInputAmountBuy() 메소드 생성

---
+++ src/main/java/lotto/ShowMessage.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class ShowMessage {
+
+    private final static String AMOUNT_BUY_MESSAGE = "구입 금액을 입력해 주세요.";
+
+    public void showInputAmountBuy(){
+        System.out.println(AMOUNT_BUY_MESSAGE);
+    }
+
+}
