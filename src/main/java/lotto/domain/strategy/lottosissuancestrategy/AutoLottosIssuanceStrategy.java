64276a1da5b3419f842770d0a64dc6e07f14ba21
JunH <tjdtls690@gmail.com>
2022-11-11 14:54:31 +0900

    :art: refactor(Domain : AutoLottosIssuanceStrategy) : 인라인 식의 로직을, 변경 대응에 대한 유연성을 위해 매개변수를 통해 역할을 전달하는 로직으로 개선
    Signed-off-by: JunH <tjdtls690@gmail.com>

--- src/main/java/lotto/domain/strategy/lottosissuancestrategy/AutoLottosIssuanceStrategy.java
+++ src/main/java/lotto/domain/strategy/lottosissuancestrategy/AutoLottosIssuanceStrategy.java
@@ -2,6 +2,7 @@ package lotto.domain.strategy.lottosissuancestrategy;
 
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
+import lotto.domain.strategy.lottoissuancestrategy.LottoIssuanceStrategy;
 import lotto.domain.strategy.lottoissuancestrategy.RandomLottoIssuanceStrategy;
 
 import java.util.List;
@@ -18,11 +19,11 @@ public class AutoLottosIssuanceStrategy implements LottosIssuanceStrategy {
     
     private List<Lotto> createLottos(final int numberOfLottoPurchased) {
         return IntStream.rangeClosed(MIN_NUMBER_OF_LOTTO_PURCHASED, numberOfLottoPurchased)
-                .mapToObj(count -> issueLotto())
+                .mapToObj(count -> issueLotto(new RandomLottoIssuanceStrategy()))
                 .collect(Collectors.toList());
     }
     
-    private Lotto issueLotto() {
-        return new RandomLottoIssuanceStrategy().issueLotto();
+    private Lotto issueLotto(LottoIssuanceStrategy lottoIssuanceStrategy) {
+        return lottoIssuanceStrategy.issueLotto();
     }
 }
