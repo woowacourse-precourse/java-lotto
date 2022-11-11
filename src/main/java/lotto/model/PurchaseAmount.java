d5488ad64e5aee855098c6d892540762afe038ec
chanwonlee <clw8679@naver.com>
2022-11-12 01:38:52 +0900

    fix: 구입 금액 에러 메시지 수정

--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -14,10 +14,10 @@ public class PurchaseAmount {
 
     private void validateMoney(int money) {
         if (money < 1000) {
-            throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 이상이어야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 이상이어야 합니다.");
         }
         if (money % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위로 입력해야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.");
         }
     }
 }
