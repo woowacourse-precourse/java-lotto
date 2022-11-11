4b8b5afdea513b4d25eecdaea160c9b3bdc7c02b
jinyeong-afk <xoeh7002@naver.com>
2022-11-12 00:30:16 +0900

    fix: 로또 번호 오름차순 정렬 수정

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
@@ -15,6 +16,7 @@ public class LottoGenerator {
 
         for (int i = 0; i < lottoCount; i++) {
             lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(lottoNumber);
             lotteries.add(lottoNumber);
         }
         this.lotteries = lotteries;
