39516acad217249d5de4cb60ef0520199c61fd36
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-11 18:36:56 +0900

    fix(inputValid): 에러 메시지를 출력 후 예외 처리 && View 에서 출력을 하도록 수정

--- src/main/java/lotto/Utils/InputValid.java
+++ src/main/java/lotto/Utils/InputValid.java
@@ -2,17 +2,22 @@ package lotto.Utils;
 
 public class InputValid {
     private final static int EXCEPTED_UNIT_OF_PRICE=1000;
-    private final static String EXCEPTED_UNIT_OF_PRICE_MESSAGE="[ERROR] 1000 단위만 입력 가능합니다.";
 
     public InputValid() {
     }
 
-    public static void checkPriceUnit(int userPrice){
-        int isValidUserGameChance=userPrice%EXCEPTED_UNIT_OF_PRICE;
+    public static boolean checkPriceUnit(String userPrice){
+        int isValidUserGameChance;
 
-        if(isValidUserGameChance!=0){
-            throw new IllegalArgumentException(EXCEPTED_UNIT_OF_PRICE_MESSAGE);
+        try{
+            isValidUserGameChance=Integer.parseInt(userPrice)%EXCEPTED_UNIT_OF_PRICE;
+        }catch (Exception e){
+            return false;
         }
 
+        if(isValidUserGameChance!=0){
+            return false;
+        }
+        return true;
     }
 }
