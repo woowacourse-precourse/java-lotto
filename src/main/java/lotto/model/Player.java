d1d0451b94dcfb786f50821c2b19d8fb124a5c5f
jinny-l <jinny.l.dev@gmail.com>
2022-11-11 15:26:14 +0900

    refactor(Player): player input의 리턴타입을 String -> int로 변경
    - 목적:
    Player 객체에서 input 타입이 정수 여부를 책임지기 위함

--- src/main/java/lotto/model/Player.java
+++ src/main/java/lotto/model/Player.java
@@ -4,17 +4,47 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import lotto.util.Convertor;
+import lotto.view.View;
+
+/*
+ * player의 입력을 담당하는 객체
+ */
 
 public class Player {
 
-    public String getInput() {
-        return input();
+    public int inputSingleNumber() {
+        String input = input();
+        int number;
+
+        try {
+            number = Integer.parseInt(input);
+        } catch (IllegalArgumentException ie) {
+            View.printNotInteger();
+            throw ie;
+        }
+        return number;
     }
 
-    public List<String> guessLottoNumbers() {
-        String numbers = input();
+    public List<Integer> inputLottoNumbers() {
+        String input = input();
+        List<String> checkComma;
+        List<Integer> numbers;
 
-        return new ArrayList<>(Arrays.asList(numbers.split("\\s*,\\s*")));
+        try {
+            checkComma = new ArrayList<>(Arrays.asList(input.split("\\s*,\\s*")));
+        } catch (IllegalArgumentException ie) {
+            View.printNotComma();
+            throw ie;
+        }
+
+        try {
+            numbers = Convertor.StringToInteger(checkComma);
+        } catch (IllegalArgumentException ie) {
+            View.printNotInteger();
+            throw ie;
+        }
+        return numbers;
     }
 
     private static String input() {
--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -6,23 +6,24 @@ import lotto.constant.Constant;
 import lotto.util.WinnerInfo;
 
 public class View {
-    private static final String GET_PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해 주세요.";
+    private static final String GET_MONEY_MESSAGE = "구입 금액을 입력해 주세요.";
     private static final String PURCHASE_INFORMATION_MESSAGE = "%d개를 구매했습니다.%n";
-    private static final String GET_LOTTO_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String GET_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String GET_LOTTO_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String GET_BONUS_NUMBERS_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String RESULT_TITLE = "당첨 통계" + "\n" + "---";
     private static final String GET_RESULT_MESSAGE = "%s - %d개%n";
     private static final String GET_PROFIT_RATES_MESSAGE = "총 수익률은 %.1f%%입니다.";
 
     private static final String ERROR_MESSAGE = "[ERROR]";
     private static final String NOT_INTEGER_MESSAGE = ERROR_MESSAGE + " 정수만 입력 가능합니다. 게임이 종료됩니다.";
-    private static final String NOT_LOTTO_PRICE_MESSAGE = ERROR_MESSAGE + "%d단위로만 입력 가능합니다. 게임이 종료됩니다.%n";
-    private static final String NOT_LOTTO_SIZE_MESSAGE = ERROR_MESSAGE + "%d개의 숫자만 입력 가능합니다. 게임이 종료됩니다.%n";
-    private static final String NOT_LOTTO_NUMBER_MESSAGE = ERROR_MESSAGE + "%d ~ %d 사이의 숫자만 입력 가능합니다. 게임이 종료됩니다.%n";
-    private static final String NOT_UNIQUE_NUMBER_MESSAGE = ERROR_MESSAGE + "중복된 숫자는 입력할 수 없습니다. 게임이 종료됩니다.";
+    private static final String NOT_LOTTO_PRICE_MESSAGE = ERROR_MESSAGE + " %d단위로만 입력 가능합니다. 게임이 종료됩니다.%n";
+    private static final String NOT_SEPARATED_BY_COMMA_MESSAGE = ERROR_MESSAGE + "숫자는 콤마로 구분해 주세요. 게임이 종료됩니다.";
+    private static final String NOT_LOTTO_SIZE_MESSAGE = ERROR_MESSAGE + " %d개의 숫자만 입력 가능합니다. 게임이 종료됩니다.%n";
+    private static final String NOT_LOTTO_NUMBER_MESSAGE = ERROR_MESSAGE + " %d ~ %d 사이의 숫자만 입력 가능합니다. 게임이 종료됩니다.%n";
+    private static final String NOT_UNIQUE_NUMBER_MESSAGE = ERROR_MESSAGE + " 중복된 숫자는 입력할 수 없습니다. 게임이 종료됩니다.";
 
     public static void printInputPurchaseAmount() {
-        System.out.println(GET_PURCHASE_AMOUNT_MESSAGE);
+        System.out.println(GET_MONEY_MESSAGE);
     }
 
     public static void printPurchaseInformation(int lottoAmount, List<List<Integer>> lottoNumbers) {
@@ -42,11 +43,11 @@ public class View {
     }
 
     public static void printInputLottoNumber() {
-        System.out.println(GET_LOTTO_NUMBER_MESSAGE);
+        System.out.println(GET_LOTTO_NUMBERS_MESSAGE);
     }
 
     public static void printInputBonusNumber() {
-        System.out.println(GET_BONUS_NUMBER_MESSAGE);
+        System.out.println(GET_BONUS_NUMBERS_MESSAGE);
     }
 
     public static void printResult(int[] lottoResult) {
@@ -69,6 +70,10 @@ public class View {
 
     public static void printNotLottoPrice() {
         System.out.printf(NOT_LOTTO_PRICE_MESSAGE, Constant.LOTTO_PRICE);
+    }
+
+    public static void printNotComma() {
+        System.out.println(NOT_SEPARATED_BY_COMMA_MESSAGE);
     }
 
     public static void printNotLottoSize() {
