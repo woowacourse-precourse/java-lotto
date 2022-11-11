31efa01aa113248c9ded9ae3f10d246b3febed40
2jun0 <soo28819@gmail.com>
2022-11-12 01:17:45 +0900

    Test(Lotto): 1~45 이외의 값이 주어졌을 때 Lotto 생성 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import lotto.domain.lotto.Lotto;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -24,5 +26,22 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 1~45가 아닌 숫자가 있으면 예외가 발생한다.")
+    @Nested
+    class createLottoByOutRangeNumber {
+
+        @Test
+        void includeMinOut() {
+            assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+
+        @Test
+        void includeMaxOut() {
+            assertThatThrownBy(() -> new Lotto(List.of(46, 2, 3, 4, 5, 6)))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
 }
