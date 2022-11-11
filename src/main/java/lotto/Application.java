733ab4a8a81f8ea4215396d3117857d9d0e812bc
CMHyuk <alsgur9710@naver.com>
2022-11-11 22:52:45 +0900

    feat(Application): 로또 프로그램 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import static lotto.config.Config.lottoMachine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            lottoMachine().run();
+        } catch (Exception e) {
+            System.out.println("[ERROR]" + e.getMessage());
+        }
     }
 }
