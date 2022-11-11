428a735f457b40072fe7a2d5894ea58d2fa387e5
woosung1223 <dntjd991223@naver.com>
2022-11-11 10:58:41 +0900

    feat(WinnerNumberVerifier): checkNumberCount 메소드 생성

--- src/main/java/lotto/verifier/WinnerNumberVerifier.java
+++ src/main/java/lotto/verifier/WinnerNumberVerifier.java
@@ -1,7 +1,16 @@
 package lotto.verifier;
 
+import lotto.util.Constant;
+import lotto.util.ExceptionMessage;
+
 public class WinnerNumberVerifier implements Verifier {
     @Override
     public void check(String input) {
+    }
+
+    private void checkNumberCount(String input) {
+        if (input.split(",").length != Constant.LOTTO_SIZE) {
+            throw new IllegalArgumentException(ExceptionMessage.NUMBER_COUNT_NOT_EQUAL);
+        }
     }
 }
