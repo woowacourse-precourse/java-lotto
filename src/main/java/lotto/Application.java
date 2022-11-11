546cecc501e6ebc9a770f248bc804a17d5be9ee1
woosung1223 <dntjd991223@naver.com>
2022-11-11 20:30:52 +0900

    feat(LottoSystem): 컨트롤러 클래스 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.controller.LottoSystem;
+import lotto.domain.UserLotto;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new LottoSystem().run();
     }
 }
