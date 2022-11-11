ccbbd606623929bdd03dcc86310cd443ae767749
ywj9811 <ywj9811@naver.com>
2022-11-12 00:08:00 +0900

    refactor: LottoGameController 클래스 간략화
    메소드 내부에 너무 복잡한 내용이 들어가 가독성이 떨어져 분리함

--- src/main/java/lotto/container/LottoGameController.java
+++ src/main/java/lotto/container/LottoGameController.java
@@ -6,41 +6,17 @@ import lotto.view.InputOutputView;
 import java.util.List;
 
 public class LottoGameController {
-    private InputOutputView inputOutputView = new InputOutputView();
-    private MyTotalMoney myTotalMoney = new MyTotalMoney();
-    private BuyLotto buyLotto = new BuyLotto();
-    private ProduceWinningNumbers produceWinningNumbers = new ProduceWinningNumbers();
-    private WinningMoney winningMoney;
-    private TotalYield totalYield;
-
-    private int money;
-    private int gain;
-    private double myYield;
-    private int bonusNumber;
-    private int quantity;
-    private List<List<Integer>> myLottoNumbers;
-    private List<Integer> winningNumbers;
-    private List<Integer> grades;
+    private RelativeLottoService relativeLottoService = new RelativeLottoService();
+    private RelativeResultService relativeResultService;
 
     public void gameStart() {
-        money = inputOutputView.buyMoney();
-        quantity = buyLotto.buyLotto(money);
-        myLottoNumbers = buyLotto.getMyLottoNumbers(quantity);
-        winningNumbers = produceWinningNumbers.getWinningNumbers();
-        bonusNumber = produceWinningNumbers.getBonusNumber(winningNumbers);
-        gain = myTotalMoney.totalMoney(myLottoNumbers, winningNumbers, bonusNumber);
-
-        totalYield = new TotalYield(money, gain);
-
-        myYield = totalYield.getYield();
-
-        winningMoney = new WinningMoney();
-
-        System.out.println("당첨 통계");
-        System.out.println("--");
-        grades = winningMoney.getGrade(winningNumbers, myLottoNumbers, bonusNumber);
-        inputOutputView.saveResult(grades);
-        inputOutputView.totalYield(myYield);
+        relativeLottoService.relativeLottoNumber();
+        relativeResultService = new RelativeResultService(
+                relativeLottoService.getMyLottoNumbers(),
+                relativeLottoService.getWinningNumbers(),
+                relativeLottoService.getBonusNumber(),
+                relativeLottoService.getMoney());
+        relativeResultService.relativeResult();
     }
 
 }
