33c2808dcbd850ad17d1452224f18e45a6a7fe69
doongu <must1080@naver.com>
2022-11-11 19:32:59 +0900

    feat(User): 구매한 랜덤 로또 번호를 기억하는 기능 구현

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,9 +1,17 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class User {
     private final int payAmount;
+    private List<List<Integer>> userLottoNumber;
+
 
     public User(int payAmount){
         this.payAmount = payAmount;
+    }
+
+    public void setUserLottoNumber(List<List<Integer>> userLottoNumber){
+        this.userLottoNumber = userLottoNumber;
     }
 }
