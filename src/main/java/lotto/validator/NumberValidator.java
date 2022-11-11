057b566f534c5eade1e492658ef9bce8bdefb5fc
parkjungyoon <tbr06057@naver.com>
2022-11-11 16:34:29 +0900

    feat(Validator): 여러 숫자에 대한 예외처리 구현

--- src/main/java/lotto/validator/NumberValidator.java
+++ src/main/java/lotto/validator/NumberValidator.java
@@ -2,22 +2,31 @@ package lotto.validator;
 
 public class NumberValidator {
     private final static int UNIT = 1000;
-    private final static String RANGE_ERROR_MESSAGE = "[ERROR] 1000으로 나누어 떨어지는 숫자여야 합니다.";
+    private final static int MIN_RANGE_NUM = 1;
+    private final static int MAX_RANGE_NUM = 45;
+    private final static String UNIT_ERROR_MESSAGE = "[ERROR] 1000으로 나누어 떨어지는 숫자여야 합니다.";
+    private final static String RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private final static String NON_NUMERIC_ERROR_MESSAGE = "[ERROR] 숫자로만 이루어진 값을 입력해주세요.";
 
     public static void validateUnit(String input) {
         int amount = Integer.parseInt(input);
         if (amount % UNIT != 0) {
-            throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
+            throw new IllegalArgumentException(UNIT_ERROR_MESSAGE);
         }
     }
 
     public static void validateNonNumeric(String input) {
         for (char charString : input.toCharArray()) {
-            if (charString >= 48 && charString <= 57) {
+            if ((int)charString >= 48 && (int)charString <= 57) {
                 continue;
             }
             throw new IllegalArgumentException(NON_NUMERIC_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateRange(int input) {
+        if (input < MIN_RANGE_NUM || input > MAX_RANGE_NUM) {
+            throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
         }
     }
 }
---
+++ src/main/java/lotto/validator/NumbersValidator.java
@@ -0,0 +1,37 @@
+package lotto.validator;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class NumbersValidator extends NumberValidator {
+    private final static int INPUT_SIZE = 6;
+    private final static String SIZE_ERROR_MESSAGE = "[ERROR] 당첨 로또는 6개입니다.";
+    private final static String DUPLICATE_ERROR_MESSAGE = "[ERROR] 중복된 숫자가 포함되어 있습니다.";
+
+    public static void validateNonNumericElements(List<String> input) {
+        for (String element : input) {
+            validateNonNumeric(element);
+        }
+    }
+
+    public static void validateSize(List<String> input) {
+        int inputSize = input.size();
+        if (inputSize != INPUT_SIZE) {
+            throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> numSet = new HashSet<>(numbers);
+        if (numSet.size() != numbers.size()) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateRangeNumbers(List<Integer> numbers) {
+        for (int element : numbers) {
+            validateRange(element);
+        }
+    }
+}
