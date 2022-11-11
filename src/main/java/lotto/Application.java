8c38d13142413b1d817cc75bc2e65617986703fd
rohsik2 <rohsik2@gmail.com>
2022-11-11 12:57:41 +0900

    feat: user input split method

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static List<Integer> getWinningNumbers(String userInput) throws IllegalArgumentException {
+        String[] splitedInputs = userInput.trim().split(",");
+        List<Integer> winnings = new ArrayList<>();
+        for (String s : splitedInputs) {
+            try {
+                winnings.add(Integer.parseInt(s));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("[ERROR] 숫자 사이는 ,로 구분되어야 합니다.");
+            }
+        }
+
+        return winnings;
     }
 }
