e9e9ea7d52c4cfa9a05a5ccd04584a87656c3f7f
ddingmin <ddingminyt@gmail.com>
2022-11-11 15:36:49 +0900

    test(Money): 거스름돈이 생기는 경우, 로또를 살 수 없는 경우 (1000원 이하의 수)

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -9,6 +9,18 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MoneyTest {
+    @DisplayName("1000 단위로 나누어 떨어지지 않는 경우 (거스름돈이 생기는 경우)")
+    @Test
+    void createMoneyByHavingMoney() {
+        assertThatThrownBy(() -> new Money(Integer.parseInt("123456")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @DisplayName("로또를 살 수 없는 경우 (1000 이하의 수가 입력된 경우)")
+    @Test
+    void createMoneyByCannotBuyLotto() {
+        assertThatThrownBy(() -> new Money(Integer.parseInt("0")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
 }
