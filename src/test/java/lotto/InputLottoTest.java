6967f3db134454c7b92e2a75fa2683a10d9a19b5
abbd2 <ajkety00@gmail.com>
2022-11-11 23:51:28 +0900

    Test(InputLottoTest): ,(쉼표)를 나누어 반환하는 splitCommas 메서드 테스트 추가

--- src/test/java/lotto/InputLottoTest.java
+++ src/test/java/lotto/InputLottoTest.java
@@ -3,7 +3,10 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class InputLottoTest {
 
@@ -11,6 +14,13 @@ class InputLottoTest {
     @Test
     void toLongs() {
         InputLotto inputLotto = new InputLotto();
-        assertEquals(3000000000L,inputLotto.toLongs("3000000000"));
+        assertEquals(3000000000L, inputLotto.toLongs("3000000000"));
+    }
+
+    @DisplayName(",(쉼표)를 기준으로 나누어 반환하는 메서드")
+    @Test
+    void splitCommas() {
+        InputLotto inputLotto = new InputLotto();
+        assertEquals(List.of("23", "42", "12", "3", "24", "26"), inputLotto.splitCommas("23,42,12,3,24,26"));
     }
 }
