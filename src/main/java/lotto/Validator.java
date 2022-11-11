1d0819f287d32acd74f1f013b4e8bccd0b9982c6
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-11 19:25:34 +0900

    feat(Validator): build more skeleton code

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 public class Validator {
 
     public void validatePayment(String payment) {
@@ -8,7 +10,17 @@ public class Validator {
     }
 
     public void validateWinningNumbers(String winningNumbers) {
+        // 형태를 검증한다.
+        // 숫자들을 검증한다.
+    }
 
+    private void validateFormOfWinningNumbers(String winningNumbers) {
+        // 숫자와 쉼표를 제외한 문자가 있어서는 안된다.
+        // 처음과 끝은 쉼표가 아니다.
+    }
+
+    private void validateValuesOfWinningNumbers(List<Integer> winningNumbers) {
+        // 숫자들은 1과 45 사이의 중복되지 않는 6개의 수이다.
     }
 
     public void validateBonusNumber(int bonusNumber) {
