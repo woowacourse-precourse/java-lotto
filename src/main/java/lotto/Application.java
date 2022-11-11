e45a4075fa3fd93d1c4365f2c7d2122424d51e32
jjongwa <troas96@naver.com>
2022-11-11 11:58:32 +0900

    feat: 로또 구매
    로또 구매 및 출력 함수 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,10 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     static int inputMoney() {
@@ -11,10 +15,40 @@ public class Application {
         return money.getAmount();
     }
 
+    static List<Integer> pickLottoNum() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 6) {
+            int randomNumber = Randoms.pickNumberInRange(1, 45);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
+    }
+
+    static List<Lotto> buyLotto(int count) {
+        List<Lotto> myLotto = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            Lotto pickLotto = new Lotto(pickLottoNum());
+            myLotto.add(pickLotto);
+        }
+        return myLotto;
+    }
+
+    static void printMyLotto(List<Lotto> myLotto) {
+        System.out.println(myLotto.size() + "개를 구매했습니다.");
+        for (Lotto lotto : myLotto) {
+            System.out.println(lotto.getLottoNumbers());
+        }
+        System.out.println();
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int purchase = inputMoney();
-
+        int purchase = inputMoney() / 1000;
+        List<Lotto> myLottos = buyLotto(purchase);
+        printMyLotto(myLottos);
     }
 }
 
