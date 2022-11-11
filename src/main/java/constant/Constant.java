9f6d43a0206d1fd4d9860ec0e74bf6404d0148b0
EUNJEONGMUN <munej26@gmail.com>
2022-11-11 13:35:19 +0900

    refactor: costant 클래스에 EMPTY 추가

--- src/main/java/constant/Constant.java
+++ src/main/java/constant/Constant.java
@@ -5,4 +5,5 @@ public class Constant {
     public static final int LOTTO_SIZE = 6;
     public static final int LOTTO_NUMBER_MIN = 1;
     public static final int LOTTO_NUMBER_MAX = 45;
+    public static final String EMPTY = "";
 }
