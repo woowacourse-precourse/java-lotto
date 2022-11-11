47e1341d06ab2453576aa8739f0ae1577d2ba7d7
ksh-beginner <ohk9134@naver.com>
2022-11-11 23:41:37 +0900

    feat(LottoGameController) : 로또 구입 단계 진행 기능 구현

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,26 @@
+package lotto.controller;
+
+import lotto.service.LottoGameService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGameController {
+
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    LottoGameService lottoGameService = new LottoGameService();
+
+    public void lottoPurchase() {
+        String lottoPurchaseAmount = inputView.inputLottoPurchaseAmount();
+        int lottoIssueCount = lottoGameService.getLottoIssueCount(lottoPurchaseAmount);
+        outputView.printPurchaseLottoCount(lottoIssueCount);
+        generateLottoNumbers(lottoIssueCount);
+        outputView.printIssuedLottoNumbers(lottoGameService.getLottoNumbers());
+    }
+
+    public void generateLottoNumbers(int lottoIssueCount) {
+        for (int generateIndex = 0; generateIndex < lottoIssueCount; generateIndex++) {
+            lottoGameService.addLottoNumberToLottoNumbers();
+        }
+    }
+}
