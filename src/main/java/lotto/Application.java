da8816e52beeb0bab83371c7ef55984984097b5c
K-Diger <kitten.diger@gmail.com>
2022-11-11 21:38:32 +0900

    fix : 예외처리 테스트가 통과하지 않던 문제 클라이언트 코드의 try-catch를 추가하여 해결

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,8 @@ import lotto.infrascturcture.adapter.in.LottoStarter;
 public class Application {
     public static void main(String[] args) {
         LottoStarter lottoStarter = new LottoStarter();
-        lottoStarter.start();
+        try {
+            lottoStarter.start();
+        } catch (IllegalArgumentException exception) {}
     }
 }
