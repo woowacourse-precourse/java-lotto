88f025db564390280a757cd09cffe878ad75ff8d
oyoungsun <plusmate@naver.com>
2022-11-12 01:09:27 +0900

    feat : isInRange() in Lotto

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -21,7 +21,15 @@ public class Lotto {
     }
     private void isNotDuplicated(List<Integer> numbers){
         int notDuplicatedSize = (int) numbers.stream().distinct().count();
+        if(notDuplicatedSize != 6){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+        }
     }
-
-    // TODO: 추가 기능 구현
+    private void isInRange(List<Integer> numbers){
+        for(int n : numbers){
+            if(n<1 && n>45){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
 }
