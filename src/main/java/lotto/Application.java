cd521ae3ce9d9268d5176b358e9750696139b1f6
Polite-Jehu <werber112@kyonggi.ac.kr>
2022-11-11 23:18:08 +0900

    feat(Lotto): add print lotto

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,49 +5,50 @@ import lotto.Domain.ErrorMessage;
 
 public class Application {
     private boolean systemError = false;
-    private int money=0;
-    private LottoClerk lottoClerk;
-    private LottoMachine lottoMachine;
-    public Application()
-    {
-        lottoClerk=new LottoClerk();
-        lottoMachine=new LottoMachine();
+    private int money = 0;
+    private static LottoClerk lottoClerk;
+    private static LottoMachine lottoMachine;
+
+    public Application() {
+        lottoClerk = new LottoClerk();
+        lottoMachine = new LottoMachine();
         this.inputMoney();
     }
+
     public static void main(String[] args) {
         Application application = new Application();
-        if(application.systemError)
+        if (application.systemError)
             return;
-
+        lottoClerk.sayBuyLotto(lottoMachine.makeLottoCount(application.getMoney()));
+        lottoMachine.makeLottoReceipt();
+        lottoClerk.sayLottoReceipt(lottoMachine.getLottoReceipt());
     }
 
-    public boolean getSystemError(){
+    public int getMoney() {
+        return money;
+    }
+
+    public boolean getSystemError() {
         return systemError;
     }
-    public void inputMoney()
-    {
-        try{
+
+    public void inputMoney() {
+        try {
             System.out.println("구입금액을 입력해 주세요.");
             money = Integer.parseInt(Console.readLine());
-            if(money%1000!=0)
+            if (money % 1000 != 0)
                 throw new IllegalArgumentException();
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             errorCatcher(ErrorMessage.NOTTHOUSANDSNUMBER.getErrorMessage());
         }
     }
 
-    public boolean errorCatcher(String errorMessage)
-    {
-        try
-        {
+    public boolean errorCatcher(String errorMessage) {
+        try {
             throw new IllegalArgumentException();
-        }
-        catch(IllegalArgumentException e)
-        {
-            System.out.println("[ERROR]"+errorMessage);
-            return systemError=true;
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR]" + errorMessage);
+            return systemError = true;
         }
     }
 
--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,4 +1,29 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoMachine {
+    ArrayList<Lotto> lottoReceipt = new ArrayList<>();
+
+    int count =0;
+    //makeProcess
+    public int makeLottoCount(int money) {
+        count = money / 1000;
+        return count;
+    }
+
+    public void makeLottoReceipt() {
+        for (int i = 0; i < count; i++) {
+            List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(lottoNumber);
+            lottoReceipt.add(lotto);
+        }
+    }
+
+    public ArrayList<Lotto> getLottoReceipt() {
+        return lottoReceipt;
+    }
 }
