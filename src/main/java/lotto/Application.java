6568bf9d4756b9df3569992a97a19c0491616955
HaODay <ycis5013@naver.com>
2022-11-11 13:52:00 +0900

    feat(lotto): 구입 금액 검증 기능 구현
    - 입력값이 숫자인지
    - 양수인지
    - int의 범위를 벗어나지 않는지
    - 1,000원으로 나누어 떨어지는지 검증

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,19 +5,37 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        Chatbot chatbot = new Chatbot();
-        int price = validatePrice(chatbot.askPrice());
-        List<List<Integer>> userNumber = setUserNumber(price / 1000);
-        chatbot.printUserNumber(userNumber);
-        Lotto lotto=new Lotto(chatbot.askLottoNumber());
-        int bonus = validateLottoBonus(chatbot.askLottoBonus());
-        List<Integer> result=compareNumbers(userNumber,lotto,bonus);
-        float rate=calculateRate(price,result);
-        chatbot.printResult(rate,result);
+        final String ERROR_MESSAGE = "[ERROR]";
+
+        try {
+            Chatbot chatbot = new Chatbot();
+            int lottoPrice = validatePrice(chatbot.askPrice());
+            List<List<Integer>> userNumber = setUserNumber(lottoPrice / 1000);
+            chatbot.printUserNumber(userNumber);
+            Lotto lotto = new Lotto(chatbot.askLottoNumber());
+            int bonus = validateLottoBonus(chatbot.askLottoBonus());
+            List<Integer> result = compareNumbers(userNumber, lotto, bonus);
+            float rate = calculateRate(lottoPrice, result);
+            chatbot.printResult(rate, result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_MESSAGE + e.getMessage());
+        }
     }
 
     public static int validatePrice(String price) {
-        return 0;
+        for (char c : price.toCharArray()) {
+            if (!('0' <= c && c <= '9')) {
+                throw new IllegalArgumentException("입력값이 숫자가 아닙니다.");
+            }
+        }
+        int lottoPrice = Integer.parseInt(price);
+        if (lottoPrice <= 0) {
+            throw new IllegalArgumentException("입력값이 범위를 벗어났습니다.(1,000 이상 2,147,483,000 이하)");
+        }
+        if (lottoPrice % 1000 != 0) {
+            throw new IllegalArgumentException("입력값이 1,000원으로 나누어 떨어지지 않습니다.");
+        }
+        return lottoPrice;
     }
 
     public static List<List<Integer>> setUserNumber(int amount) {
@@ -30,13 +48,13 @@ public class Application {
         return 0;
     }
 
-    public static List<Integer> compareNumbers(List<List<Integer>> userNumber, Lotto lotto, int bonus){
-        List<Integer> result=new ArrayList();
+    public static List<Integer> compareNumbers(List<List<Integer>> userNumber, Lotto lotto, int bonus) {
+        List<Integer> result = new ArrayList();
 
         return result;
     }
 
-    public static float calculateRate(int price, List<Integer> result){
+    public static float calculateRate(int price, List<Integer> result) {
         return 0f;
     }
 
