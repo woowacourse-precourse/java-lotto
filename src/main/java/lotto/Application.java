7eea0bffef5387a4a00622c75aa93a0ec8860ff3
WhalesBob <ndw8200@naver.com>
2022-11-11 12:35:15 +0900

    feat(Application) : add instances for I/O Handler and Generating, Counting Features

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,19 +1,32 @@
 package lotto;
 
+import lotto.InputOutput.GameOutputHandler;
 import lotto.InputOutput.UserInputHandler;
+import lotto.controller.GeneratingLottoNumbers;
+import lotto.controller.Lotto;
+import lotto.controller.LottoCounting;
+
+import java.util.Map;
 
 public class Application {
+
     static UserInputHandler inputHandler;
+    static GameOutputHandler outputHandler;
+    static GeneratingLottoNumbers lottoNumbers;
+    static LottoCounting lottoCounter;
 
     public static void main(String[] args) {
         Application lottoApplication = new Application();
 
         inputHandler = new UserInputHandler();
+        outputHandler = new GameOutputHandler();
+        lottoNumbers = new GeneratingLottoNumbers();
+        lottoCounter = new LottoCounting();
+
         lottoApplication.start();
     }
 
     void start(){
 
-        inputHandler.getHowMuchTickets();
     }
 }
