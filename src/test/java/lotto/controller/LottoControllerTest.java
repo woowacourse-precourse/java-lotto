fb091d44bd1088d6e5c498faaf7e607cdb81ee68
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-12 00:16:29 +0900

    test: 종류별 당첨 로또 수 세기 테스트

--- src/test/java/lotto/controller/LottoControllerTest.java
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -4,12 +4,15 @@ import lotto.domain.Lotto;
 import lotto.domain.WinningLotto;
 import lotto.domain.WinningRank;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LottoControllerTest {
 
@@ -46,6 +49,24 @@ class LottoControllerTest {
         WinningRank expectedRank = WinningRank.findByRank(rankNum);
 
         assertThat(actualRank).isEqualTo(expectedRank);
+    }
+
+    @Test
+    void 당첨_로또_세기_테스트() {
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+        List<Lotto> lottos = List.of(
+                new Lotto(List.of(1,2,3,4,5,6)),
+                new Lotto(List.of(1,2,3,4,5,7)),
+                new Lotto(List.of(1,2,7,8,9,10)),
+                new Lotto(List.of(5,6,7,8,9,10))
+        );
+
+        Map<WinningRank, Integer> winLottoCount = lottoController.countWinLotto(winningLotto, lottos);
+        assertAll(
+                () -> assertThat(winLottoCount.get(WinningRank.FIRST_RANK)).isEqualTo(1),
+                () -> assertThat(winLottoCount.get(WinningRank.SECOND_RANK)).isEqualTo(1),
+                () -> assertThat(winLottoCount.get(WinningRank.NONE_RANKED)).isEqualTo(2)
+        );
     }
 
 }
