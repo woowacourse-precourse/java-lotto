739f80b112a9d0fa20e394eadddea1915fefd4a4
박승민 <tmddudals369@naver.com>
2022-11-12 02:26:23 +0900

    fix: lottos에 접근할 경우 NullPointException 문제 해결

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,13 +1,14 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 import lotto.Lotto;
 
 public class User {
 
     private int lottoCount;
-    private List<Lotto> lottos;
+    private List<Lotto> lottos = new ArrayList<>();
 
     public User(int purchase) {
         this.lottoCount = purchase / 1000;
