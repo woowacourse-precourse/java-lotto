67996c2b44b76e49af2f74f467dbc6a604af42f9
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-11 15:34:19 +0900

    feat(OutputUi): 구입금액 입력 메시지 출력

---
+++ src/main/java/lotto/ui/OutputUi.java
@@ -0,0 +1,10 @@
+package lotto.ui;
+
+public class OutputUi {
+
+    private static final String INSERT_MONEY_INFO_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static void printInsertMoneyInfoMessage(){
+        System.out.println(INSERT_MONEY_INFO_MESSAGE);
+    }
+}
