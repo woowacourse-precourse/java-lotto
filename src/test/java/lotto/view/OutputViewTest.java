d59e5305af73afd12ffc8747459ea13da65cc02a
ksh-beginner <ohk9134@naver.com>
2022-11-11 18:39:51 +0900

    test(OutputViewTest) : 로또 발행 번호 출력 기능 테스트

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,7 +30,16 @@ class OutputViewTest {
     void 로또_구매_개수_출력() {
         String purchaseLottoCountMessage = "8개를 구매했습니다.\n";
         outputView.printPurchaseLottoCount(8);
+
         assertThat(output.toString()).isEqualTo(purchaseLottoCountMessage);
     }
 
+    @Test
+    void 로또_발행_번호_출력() {
+        List<Integer> issuedLottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        outputView.printIssuedLottoNumbers(issuedLottoNumbers);
+        String purchaseLottoCountMessage = "[1, 2, 3, 4, 5, 6]\n";
+
+        assertThat(output.toString()).isEqualTo(purchaseLottoCountMessage);
+    }
 }
