148f44a6fab839bac5d7d8f0358a373a6c45b469
seoyoung <sy9723@korea.ac.kr>
2022-11-11 12:38:37 +0900

    refactor: 인스턴스 변수 final로 변경

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class NumberGenerator {
-    public List<List<Integer>> collectionOfLottoNumbers;
+    public final List<List<Integer>> collectionOfLottoNumbers;
 
     public NumberGenerator(int money) {
         validate(money);
