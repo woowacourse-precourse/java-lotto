ffafd09d6f9ebbb4ce8c37402c225be3a5f76be0
Joon <orijoon98@gmail.com>
2022-11-11 14:37:15 +0900

    feat: add print winning lottery result method

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -12,6 +12,14 @@ public class Print {
     private final static String LOTTO_NUMBER_DELIMITER = ", ";
     private final static String LOTTO_NUMBER_START = "[";
     private final static String LOTTO_NUMBER_END = "]";
+    private final static String LOTTERY_RESULT_TITLE = "당첨 통계";
+    private final static String LOTTERY_RESULT_SUBDIVISION = "---";
+    private final static String LOTTERY_RESULT_FIFTH = "3개 일치 (5,000원) - ";
+    private final static String LOTTERY_RESULT_FOURTH = "4개 일치 (50,000원) - ";
+    private final static String LOTTERY_RESULT_THIRD = "5개 일치 (1,500,000원) - ";
+    private final static String LOTTERY_RESULT_SECOND = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    private final static String LOTTERY_RESULT_FIRST = "6개 일치 (2,000,000,000원) - ";
+    private final static String LOTTERY_RESULT_COUNT = "개";
 
     public static void inputPrice() {
         System.out.println(INPUT_PRICE);
@@ -39,5 +47,15 @@ public class Print {
                 .map(String::valueOf)
                 .collect(Collectors.joining(LOTTO_NUMBER_DELIMITER));
         System.out.println(LOTTO_NUMBER_START + lottoNumbers + LOTTO_NUMBER_END);
+    }
+
+    public static void winningLotteryResult(int fifth, int fourth, int third, int second, int first) {
+        System.out.println(LOTTERY_RESULT_TITLE);
+        System.out.println(LOTTERY_RESULT_SUBDIVISION);
+        System.out.println(LOTTERY_RESULT_FIFTH + fifth + LOTTERY_RESULT_COUNT);
+        System.out.println(LOTTERY_RESULT_FOURTH + fourth + LOTTERY_RESULT_COUNT);
+        System.out.println(LOTTERY_RESULT_THIRD + third + LOTTERY_RESULT_COUNT);
+        System.out.println(LOTTERY_RESULT_SECOND + second + LOTTERY_RESULT_COUNT);
+        System.out.println(LOTTERY_RESULT_FIRST + first + LOTTERY_RESULT_COUNT);
     }
 }
