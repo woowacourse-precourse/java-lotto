45aa0a72988ae4251c721b646bc74846d3022bb9
saewoo1 <wooozooo1123@gmail.com>
2022-11-11 15:25:59 +0900

    feat(WinningNumberTest): validateNumber 메소드에 대한 테스트 케이스 추가

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -1,33 +1,29 @@
 package lotto;
 
-import java.util.Arrays;
-
 public class WinningNumber {
-
+  // ','를 기준으로 split 시행
   public void splitNumber(String userNumber) {
     String[] number = userNumber.split(",");
     for (String num : number) {
-      validateLine(num);
+      validateNumber(num);
     }
   }
-
-  public void validateLine(String num) {
+// 유효성 검사
+  public void validateNumber(String num) {
     for (int i = 0; i < num.length(); i++) {
       if (!Character.isDigit(num.charAt(i))) {
         throw new IllegalArgumentException("[ERROR] 숫자가 아닙니다");
       }
     }
     int result = Integer.parseInt(num);
-
     if (result > 45 || result < 1) {
-      throw new IllegalArgumentException("[ERROR] 범위 내의 숫자가 아닙니다.");
+      throw new IllegalArgumentException("[ERROR] 1 ~ 45 내의 숫자가 아닙니다.");
     }
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args){
     WinningNumber win = new WinningNumber();
-
-    win.splitNumber("12a,3,45,23");
+    win.splitNumber("133,2,4");
   }
 }
 
---
+++ src/test/java/lotto/WinningNumberTest.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class WinningNumberTest {
+
+  @DisplayName("들어온 String이 범위 내에 포함되어있지 않을 경")
+  @Test
+  void notRangeNumber() {
+    assertThatThrownBy(() -> new WinningNumber().splitNumber("152,3,4"))
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @DisplayName("들어온 String이 숫자 외의 것이 포함되어 있을 경우")
+  @Test
+  void notOnlyDigitNumber() {
+    assertThatThrownBy(() -> new WinningNumber().splitNumber("a,25,24"))
+            .isInstanceOf(IllegalArgumentException.class);
+    assertThatThrownBy(() -> new WinningNumber().splitNumber("24 2 26"))
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+}
