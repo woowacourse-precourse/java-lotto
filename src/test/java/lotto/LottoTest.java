4e47c2995cb370937ae9a2e06f414f7eff69b5d7
glass <glass7312@gmail.com>
2022-11-11 16:28:06 +0900

    feat:Lotto클래스 중복 체크 작성 및 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,16 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import domain.Game;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -18,9 +23,14 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void overLapTest(){
+        List<Integer> test = Randoms.pickUniqueNumbersInRange(1,45,6);
+        assertThat(test.size()).isEqualTo(test.stream().distinct().count());
     }
 
     // 아래에 추가 테스트 작성 가능
