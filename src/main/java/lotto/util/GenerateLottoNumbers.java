a1b77ce2d60ac33533661a66b50ae38eaa9877fe
sosow0212 <sosow0212@naver.com>
2022-11-11 12:50:38 +0900

    refactor : 로또 번호 생성 util의 메서드 분리

--- src/main/java/lotto/util/GenerateLottoNumbers.java
+++ src/main/java/lotto/util/GenerateLottoNumbers.java
@@ -8,8 +8,16 @@ import lotto.domain.UserLotto;
 
 public class GenerateLottoNumbers {
     public static List<UserLotto> generateLottoNumbers(int buyingPrice) {
+        List<UserLotto> lottos = getBuyingLottos(getBuyingQuantity(buyingPrice));
+        return lottos;
+    }
+
+    public static int getBuyingQuantity(int buyingPrice) {
+        return buyingPrice / 1000;
+    }
+
+    public static List<UserLotto> getBuyingLottos(int quantity) {
         List<UserLotto> lottos = new ArrayList<>();
-        int quantity = buyingPrice / 1000;
         for (int i = 0; i < quantity; i++) {
             List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Collections.sort(lottoNumbers);
