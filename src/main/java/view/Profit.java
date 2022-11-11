4d5359b393eb7d2bc57b127ae8d5a187a40492dc
sugamypapa <sk000801@naver.com>
2022-11-11 22:17:24 +0900

    feat: 당첨 개수와 금액이 담긴 enum 생성

--- src/main/java/view/Profit.java
+++ src/main/java/view/Profit.java
@@ -1,4 +1,16 @@
 package view;
 
 public enum Profit {
+    Three(5000), Four(50000), Five(1500000),
+    FiveBonus(30000000), Six(2000000000);
+
+    private int prize = 0;
+
+    Profit(int prize) {
+        this.prize = prize;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
 }
