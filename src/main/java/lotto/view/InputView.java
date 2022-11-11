42169b4e5db5a8407cffc044f1c6c895592d3849
yg0585 <yg0585@naver.com>
2022-11-11 23:08:46 +0900

    feat: 입력 숫자 범위와 숫자 갯수 예외 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,22 +5,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
-    public static int getPurchase() throws NumberFormatException{
+    public static int getPurchase() throws NumberFormatException {
         return Integer.parseInt(Console.readLine());
     }
 
-    public static List<Integer> getWinningNumber() throws NumberFormatException{
+    public static List<Integer> getWinningNumber() throws NumberFormatException {
         String input = Console.readLine();
         String[] splitInput = input.split(",");
         List<Integer> winningNumber = new ArrayList<>();
 
         for (String s : splitInput) {
-            winningNumber.add(Integer.parseInt(s));
+            int number = Integer.parseInt(s);
+            if (number > 45 || number < 1)
+                throw new NumberFormatException();
+            winningNumber.add(number);
         }
-        return  winningNumber;
+        if (winningNumber.size() != 6) {
+            throw new NumberFormatException();
+        }
+        return winningNumber;
     }
 
-    public static int getBonusNumber() throws NumberFormatException{
+    public static int getBonusNumber() throws NumberFormatException {
         String input = Console.readLine();
 
         return Integer.parseInt(input);
