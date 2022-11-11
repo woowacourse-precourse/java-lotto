beb4d46352554120a26520934926af9dd32e55ad
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-11 16:22:58 +0900

    feat(lotto): 총 당첨금액 계산 작업

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import view.InputBonusNumber;
 import view.InputWinningLottoNumber;
@@ -11,6 +12,7 @@ public class LottoGame {
     List<Integer> winningLottoList = new ArrayList<>();
     List<Integer> matchCountList = new ArrayList<>();
     List<Integer> rankedList = new ArrayList<>();
+    List<Integer> moneyRankedList = new ArrayList<>();
     int bonusNumber;
 
 
@@ -22,8 +24,37 @@ public class LottoGame {
             matchCountList.add(matchCount(winningLottoList, nowLottoList, bonusNumber));
         }
 
+        calculate(matchCountList);
+    }
+
+    private  void calculate(List<Integer> matchCountList) {
+
+        countingRank(matchCountList);
+        int sumMoney = calculateSumMoney(rankedList);
+        System.out.println("sumMoney = " + sumMoney);
 
     }
+
+    private int calculateSumMoney(List<Integer> rankedList) {
+        List<Integer> money = Arrays.asList(5000, 50000, 1500000, 2000000000, 30000000);
+        int sumGetMoney=0;
+        for(int i=0; i<rankedList.size(); i++){
+            Integer nowRankMoney = money.get(i);
+            Integer howManyRanked = rankedList.get(i);
+            sumGetMoney += nowRankMoney * howManyRanked;
+        }
+
+        return sumGetMoney;
+    }
+
+    private void countingRank(List<Integer> matchCountList) {
+        for(int j=3; j<=7; j++){
+            int finalJ = j;
+            rankedList.add((int) matchCountList.stream().filter(n -> n == finalJ).count());
+        }
+        System.out.println("rankedList = " + rankedList);
+    }
+
     public void setting() {
         this.buyLottoList = BuyLottoList.getBuyLottoList();
         this.winningLottoList = InputWinningLottoNumber.getWinningLottoList();
@@ -35,6 +66,7 @@ public class LottoGame {
         long count = winningLottoList.stream().filter(n -> nowBuyLottoList.contains(n)).count();
         if(count==5){
             if(isInBonusNumber(nowBuyLottoList, bonusNumber)){
+                System.out.println("count = " + 7);
                 return 7;
             }
         }
@@ -42,14 +74,9 @@ public class LottoGame {
         return Math.toIntExact(count);
     }
 
-
-
-
     public static boolean isInBonusNumber(List<Integer> nowBuyLottoList, int bonusNumber){
         return nowBuyLottoList.contains(bonusNumber);
     }
-
-
 
 
 }
