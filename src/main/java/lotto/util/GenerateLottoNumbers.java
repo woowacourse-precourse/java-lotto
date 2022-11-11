fe3b4a3422b3abcc47a534549a542e946d63bf7d
sosow0212 <sosow0212@naver.com>
2022-11-11 12:45:45 +0900

    feat : 유저의 금액을 바탕으로 로또들을 생성해주는 util 구현

--- src/main/java/lotto/util/GenerateLottoNumbers.java
+++ src/main/java/lotto/util/GenerateLottoNumbers.java
@@ -1,13 +1,20 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import lotto.domain.UserLotto;
 
 public class GenerateLottoNumbers {
-    public static List<Integer> generateLottoNumbers() {
-        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        Collections.sort(lottoNumbers);
-        return lottoNumbers;
+    public static List<UserLotto> generateLottoNumbers(int buyingPrice) {
+        List<UserLotto> lottos = new ArrayList<>();
+        int quantity = buyingPrice / 1000;
+        for (int i = 0; i < quantity; i++) {
+            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(lottoNumbers);
+            lottos.add(new UserLotto(lottoNumbers));
+        }
+        return lottos;
     }
 }
