2fbd8c5ec3a635f611e727cbf674037b17148d11
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-12 00:28:27 +0900

    feat : Lotto를 담을 Lottos 클래스 생성

---
+++ src/main/java/lotto/Lottos.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import java.util.List;
+
+public class Lottos {
+
+    public List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+}
