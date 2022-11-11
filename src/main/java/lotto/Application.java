44e1a904c21bc262822e9450c1cc84af19dce6f2
Ji-Un-Gil <wldns2577@gmail.com>
2022-11-11 16:28:19 +0900

    feat(Application.java): display_lottos function Added function: - 발행한 로또 수량 및 번호를 출력하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -13,6 +13,7 @@ public class Application {
 
     public static void main(String[] args) {
         final List<Integer> lotto_win;
+        final List<Lotto> lotto_issuance;
         final int purchase, bonus;
         Scanner sc = new Scanner(System.in);
         purchase = sc.nextInt();
@@ -23,7 +24,8 @@ public class Application {
         Lotto winLotto = new Lotto(lotto_win);
         bonus = sc.nextInt();
 
-        
+        lotto_issuance = lotto_create(purchase);
+        display_lottos(lotto_issuance);
         sc.close();
     }
 
@@ -31,6 +33,22 @@ public class Application {
         if (purchase % ticket_price != 0 || purchase <= 0){
             System.out.println("[ERROR] 금액은 반드시 1,000원 이상, 1,000원 단위로 입력하세요.");
             throw new IllegalArgumentException();
+        }
+    }
+
+    private static List<Lotto> lotto_create(int purchase){
+        final int tickets = (int) purchase / ticket_price;
+        final List<Lotto> lotto_issuance = new ArrayList<>();
+        for (int i = 0; i < tickets; i++){
+            lotto_issuance.add(new Lotto());
+        }
+        return lotto_issuance;
+    }
+
+    private static void display_lottos(List<Lotto> lotto_issuance){
+        System.out.printf("%d개를 구매했습니다.\n", lotto_issuance.size());
+        for (int i = 0; i < lotto_issuance.size(); i++){
+            lotto_issuance.get(i).display_num();
         }
     }
 }
