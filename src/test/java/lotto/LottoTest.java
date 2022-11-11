91d483a4b7b481e86998f525721cf7d3dab79a6b
yj2980 <gum0945@naver.com>
2022-11-11 22:49:59 +0900

    test : 예외 사항과 관련된 테스트 케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -7,6 +7,10 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import lotto.domain.Company;
+import lotto.domain.Lotto;
+import lotto.service.LottoService;
+
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
@@ -24,4 +28,34 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("숫자를 쉼표로 구분하지 않았을 경우 예외가 발생한다.")
+    @Test
+    void createLottoByNotSeparatedComma() {
+        assertThatThrownBy(() -> ExceptionHandler.checkSpilt("123456"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("로또 번호가 범위 안에 있지 않을 경우 예외가 발생한다. ")
+    @Test
+    void createLottoOutOfLange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1,13,67,34,2,7)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("로또 번호가 숫자가 아닌 경우 예외가 발생한다.")
+    @Test
+    void createLottoNotNumbers() {
+        assertThatThrownBy(() -> LottoService.convertStringToList("a,b,c,d,e,f"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+    @DisplayName("로또 번호와 보너스 번호가 일치할 경우 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedBonusNumber() {
+        assertThatThrownBy(() -> new Company(List.of(1,2,3,4,5,6), 4))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
 }
