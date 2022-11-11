e76e489ec2ce5e8718f878d64be38fa06b44b8c4
heesang <dlagmltkd12@naver.com>
2022-11-12 06:22:21 +0900

    feat: 당첨 번호 입력 메시지 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,8 +3,13 @@ package lotto.view;
 public class OutputView {
 
     public static final String INPUT_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String INPUT_ANSWER_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
 
-    public static void printPurchasePrice() {
+    public static void printInputPurchasePrice() {
         System.out.println(INPUT_PURCHASE_PRICE_MESSAGE);
+    }
+
+    public static void printInputAnswerNumber() {
+        System.out.println(INPUT_ANSWER_NUMBERS_MESSAGE);
     }
 }
