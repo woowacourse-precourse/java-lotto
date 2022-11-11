58518649bf7847127093a3f2e98b0fc56d09601a
yangjiyoung <0112jyoung@naver.com>
2022-11-11 23:00:59 +0900

    style: delete useless import

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -4,22 +4,22 @@ import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Lotto;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Lottos {
-    private List<Lotto> lottos=new ArrayList<>();
+    private List<Lotto> lottos = new ArrayList<>();
 
-    public Lottos(int set){
-       initLottos(set);
+    public Lottos(int set) {
+        initLottos(set);
     }
 
     public List<Lotto> getLottos() {
         return lottos;
     }
-    public void initLottos(int set){
-        for(int i=0;i<set;i++){
-            Lotto lotto=new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6));
+
+    public void initLottos(int set) {
+        for (int i = 0; i < set; i++) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             lotto.rangeLottoNumbers();
             lottos.add(lotto);
         }
