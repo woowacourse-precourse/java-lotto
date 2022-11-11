54cd766d8db27b9bb7328e6265ec1b2022ebb85e
kimJaeHyeon <kmr2644@gmail.com>
2022-11-11 15:55:25 +0900

    printBuyLotto 메서드 구현

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class View {
 
@@ -14,6 +15,14 @@ public class View {
     public void validateMoney(int money) {
         if (money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 입력금액은 1000원 단위여야 합니다.");
+        }
+    }
+
+    public void printBuyLotto(LottoDto lottoDto) {
+        List<List<Integer>> lotteries = lottoDto.getLotteries();
+        System.out.println(lotteries.size() + "개를 구매했습니다.");
+        for (List<Integer> lottery : lotteries) {
+            System.out.println(lottery);
         }
     }
 
