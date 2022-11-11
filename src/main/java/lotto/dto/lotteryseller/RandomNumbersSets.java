9d64bedf45727e2a82bd7831991975617afba2d2
ysng_ysng <yeasung67@gmail.com>
2022-11-12 02:27:49 +0900

    feat(RandomNumbersSets): Implement the dto : RandomNumbersSets
    Dto인 RandomNumbersSets를 구현했습니다.

--- src/main/java/lotto/dto/lotteryseller/RandomNumbersSets.java
+++ src/main/java/lotto/dto/lotteryseller/RandomNumbersSets.java
@@ -1,4 +1,14 @@
 package lotto.dto.lotteryseller;
 
+import java.util.List;
+
 public class RandomNumbersSets {
+	private final List<List<Integer>> randomNumbersSets;
+	public RandomNumbersSets(List<List<Integer>> randomNumbersSets) {
+		this.randomNumbersSets =randomNumbersSets;
+	}
+
+	public List<List<Integer>> getRandomNumbersSets() {
+		return randomNumbersSets;
+	}
 }
