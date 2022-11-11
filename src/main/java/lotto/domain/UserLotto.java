0a7ac0f89fa11590ccbefcb552eafbc0c53aaebe
woosung1223 <dntjd991223@naver.com>
2022-11-11 18:07:25 +0900

    feat(UserLotto): 클래스 생성 및 buyLottos 메소드 생성

---
+++ src/main/java/lotto/domain/UserLotto.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import lotto.util.Constant;
+import lotto.view.InputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserLotto {
+    private LottoWrapper lottoSet = new LottoWrapper();
+
+    public void buyLottos() {
+        long count = readMoney() % Constant.MONEY_UNIT;
+        while (count-- > 0) {
+            lottoSet.add(makeLotto());
+        }
+    }
+
+}
