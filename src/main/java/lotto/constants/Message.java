78dd8db233cedc7fbe87431e52b55b22d46d4159
xi_jjun <rlawowns97@naver.com>
2022-11-12 01:21:22 +0900

    refactor: 에러 메세지 상수 이름을 좀 더 일반화 시킴

--- src/main/java/lotto/constants/Message.java
+++ src/main/java/lotto/constants/Message.java
@@ -9,9 +9,10 @@ public class Message {
 	public static final String ERROR_INVALID_LOTTO_LENGTH = ERROR_PREFIX + "유효하지 않는 로또번호 개수 입니다.";
 	public static final String ERROR_HAS_INVALID_LOTTO_NUMBER_VALUE = ERROR_PREFIX + "로또 번호는 1~45 사이어야 합니다.";
 
-	public static final String ERROR_EMPTY_PURCHASE_AMOUNT_INPUT = ERROR_PREFIX + "빈 문자열을 입력하면 안됩니다.";
-	public static final String ERROR_NOT_NUMERIC_PURCHASE_AMOUNT_INPUT = ERROR_PREFIX + "숫자만 입력해야 합니다.";
-	public static final String ERROR_FIRST_ZERO_PURCHASE_AMOUNT_INPUT = ERROR_PREFIX + "맨 첫 숫자는 0이면 안됩니다.";
+	public static final String ERROR_EMPTY_INPUT = ERROR_PREFIX + "빈 문자열을 입력하면 안됩니다.";
+	public static final String ERROR_IS_NOT_NUMERIC_VALUE = ERROR_PREFIX + "숫자만 입력해야 합니다.";
+	public static final String ERROR_FIRST_CHAR_IS_ZERO = ERROR_PREFIX + "맨 첫 숫자는 0이면 안됩니다.";
+
 	public static final String ERROR_NOT_ENOUGH_PURCHASE_AMOUNT_INPUT =
 			ERROR_PREFIX + "적어도 1개의 로또는 살 수 있는 금액이어야 합니다.";
 	public static final String ERROR_DONT_DIVIDED_BY_LOTTO_PRICE =
