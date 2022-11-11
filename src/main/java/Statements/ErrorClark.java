cd154ea107461d4ea1c204978c3bfb5fccc7e50c
comet7001 <comet7001@gmail.com>
2022-11-11 14:49:17 +0900

    feat(statements): add two enum classes printing (command/error) messages to user

--- src/main/java/Statements/ErrorClark.java
+++ src/main/java/Statements/ErrorClark.java
@@ -1,4 +1,18 @@
 package statements;
 
-public class ErrorClark {
+public enum ErrorClark {
+    SHOUT_NOT_NUMBER_ERROR("[ERROR] 올바르지 않은 입력입니다. 1~45사이의 숫자를 입력해주세요."),
+    SHOUT_NUMBER_RANGE_ERROR("[ERROR] 범위를 넘어선 숫자입니다. 1~45사이의 숫자를 입력해주세요."),
+    SHOUT_INVALID_FORMAT_ERROR("[ERROR] 유효하지 않은 형식입니다. 값을 입력해주세요."),
+    SHOUT_PAYMENT_UNIT_ERROR("[ERROR] 가격 단위가 부적절합니다. 1000원 단위로 입력해주세요.");
+
+    private final String warning;
+    private ErrorClark(String warning){
+        this.warning=warning;
+    }
+
+    public String alarmed(){
+        return warning;
+    }
+
 }
--- src/main/java/Statements/RobotClerk.java
+++ src/main/java/Statements/RobotClerk.java
@@ -1,4 +1,28 @@
 package statements;
 
-public class RobotClerk {
+public enum RobotClerk {
+    ASK_MONEY_TO_BUY_LOTTO("구입금액을 입력해 주세요."),
+    LET_USER_KNOW_THE_NUMBER_OF_TICKETS("개를 구매했습니다."),
+    ASK_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+    ASK_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    THREE_NUMBERS_MATCHED("3개 일치 (5,000원) - "),
+    FOUR_NUMBERS_MATCHED("4개 일치 (50,000원) - "),
+    FIVE_NUMBERS_MATCHED("5개 일치 (1,500,000원) - "),
+    FIVE_NUMBERS_AND_BONUS_MATCHED("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    SIX_NUMBERS_MATCHED("6개 일치 (2,000,000,000원) - "),
+    COUNTING_UNIT("개");
+
+    private final String saying;
+
+    RobotClerk(String saying){
+        this.saying = saying;
+    }
+
+    public String done(){
+        return saying;
+    }
+
+
+
+
 }
