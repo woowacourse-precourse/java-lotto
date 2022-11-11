ec082d94a59602280f2afc7796b384b4d7326668
MoonJeWoong <munjin0201@naver.com>
2022-11-12 03:38:43 +0900

    feat(utils): 입력 문자열을 숫자 리스트로 변환하는 기능

--- src/main/java/lotto/utils/Utils.java
+++ src/main/java/lotto/utils/Utils.java
@@ -2,8 +2,10 @@ package lotto.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class Utils {
     public static  List<Integer> randomUniqueNumberGenerate(int start, int end, int count){
@@ -12,5 +14,12 @@ public final class Utils {
 
     public static void sortListNaturalOrder(List<Integer> list){
         Collections.sort(list);
+    }
+
+    public static int[] stringToIntArray(String string){
+        return Arrays.stream(string.split(",")).mapToInt(Integer::parseInt).toArray();
+    }
+    public static List<Integer> stringToIntegerList(String string){
+        return Arrays.stream(stringToIntArray(string)).boxed().collect(Collectors.toList());
     }
 }
