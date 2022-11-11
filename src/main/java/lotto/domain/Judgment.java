d8c080af57abc202d1314c72fdb2fe3f94d2def3
seoyoung <sy9723@korea.ac.kr>
2022-11-11 18:02:42 +0900

    feat: 기능 추가 (4등으로 당첨된 로또의 개수를 계산한다)

--- src/main/java/lotto/domain/Judgment.java
+++ src/main/java/lotto/domain/Judgment.java
@@ -47,6 +47,17 @@ public class Judgment {
         return count;
     }
 
+    private int calculateFourth(List<Integer>numbers) {
+        int count = 0;
+        for (List<Integer> lottoNumbers : collectionOfLottoNumbers) {
+            if (correctCount(lottoNumbers, numbers) == 4) {
+                collectionOfLottoNumbers.remove(lottoNumbers);
+                count++;
+            }
+        }
+        return count;
+    }
+
     public int correctCount(List<Integer> actualNumbers, List<Integer> expectedNumbers) {
         int count = 0;
         int[] checkCorrect = new int[46];
