7f8b21102917d057a3170ac2635a39738176c99d
KimSangHyeok <tkdgur0906@naver.com>
2022-11-11 23:10:24 +0900

    Test(lotto): 수익률 테스트

--- src/test/java/lotto/domain/LottoCheckerTest.java
+++ src/test/java/lotto/domain/LottoCheckerTest.java
@@ -1,4 +1,16 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 class LottoCheckerTest {
-  
+
+    @Test
+    void 수익률_테스트(){
+        LottoChecker lottoChecker = new LottoChecker();
+        assertThat(lottoChecker.profitRate(8000,5000)).isEqualTo(62.5);
+    }
 }
