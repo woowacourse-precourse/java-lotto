949ac1a98ddb42211627ec9cd5be121e6773bfb1
yj2980 <gum0945@naver.com>
2022-11-11 21:52:32 +0900

    rename : 변수명 대문자로 변경

--- src/main/java/lotto/Notice.java
+++ src/main/java/lotto/Notice.java
@@ -1,12 +1,13 @@
 package lotto;
 
 public enum Notice {
-	start("구입금액을 입력해 주세요."),
-	purchase("개를 구매했습니다."),
-	winningNumbers("당첨 번호를 입력해 주세요."),
-	bonusNumber("보너스 번호를 입력해 주세요."),
-	statistics("당첨 통계\n---"),
-	profit("총 수익률은 ");
+	ERROR("[ERROR]"),
+	START("구입금액을 입력해 주세요."),
+	PURCHASE("개를 구매했습니다."),
+	WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+	BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+	STATISTICS("당첨 통계\n---"),
+	RATE_OF_RETURN("총 수익률은 ");
 
 	private String noticeMessage;
 
