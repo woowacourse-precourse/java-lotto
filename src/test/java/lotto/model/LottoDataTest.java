239a222712b16b1f2eacac3844ee31c0c13e361e
newh08 <newh08@gmail.com>
2022-11-11 21:44:38 +0900

    test: implement a test for function that making Lotto List

--- src/test/java/lotto/model/LottoDataTest.java
+++ src/test/java/lotto/model/LottoDataTest.java
@@ -2,16 +2,28 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoDataTest {
+    static LottoData lottoData;
+
+    @BeforeAll
+    static void initAll() {
+        lottoData = new LottoData(12000);
+    }
 
     @DisplayName("12000으로 LottoData 객체를 생성하면 lottoAmount 에 12가 저장된다.")
     @Test
     void constructorTest() {
-        LottoData lottoData = new LottoData(12000);
         assertThat(lottoData.lottoAmount).isEqualTo(12);
     }
 
+    @DisplayName("12000으로 LottoData 객체 생성 후 makeLotto 호출하면 allLotto 에 12개의 Lotto 객체가 담긴다.")
+    @Test
+    void makeLottoTest() {
+        lottoData.makeLotto();
+        assertThat(lottoData.allLotto.size()).isEqualTo(12);
+    }
 }
