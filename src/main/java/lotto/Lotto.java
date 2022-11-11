0277549ab0f7613bccab301f407dd25a040c11a1
HyeonjungYun <guswnd9971@gmail.com>
2022-11-11 21:46:46 +0900

    "refactor(Lotto): 로또 번호 생성 시 받는 매개 변수 String으로 변경, 변수명 변경"

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,20 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.HashSet;
 
 public class Lotto {
-    private final List<Integer> lottoNumbers;
+    private final List<Integer> numbers;
+
+    public Lotto(String inputLottoNumber) {
+        List<Integer> numbers = convertNumbers(inputLottoNumber);
+
+        validateLottoNumberCount(numbers);
+        validateLottoNumberRange(numbers);
+        validateLottoNumberDuplicated(numbers);
+        this.numbers = numbers;
+    }
 
     public Lotto(List<Integer> lottoNumbers) {
         validateLottoNumberCount(lottoNumbers);
@@ -19,14 +29,14 @@ public class Lotto {
         }
     }
 
-    private void validateLottoNumberRange(List<Integer> lottoNumbers) {
-        for (int number: lottoNumbers){
+    private void validateLottoNumberRange(List<Integer> numbers) {
+        for (int number: numbers){
             if (number < 1 || number > 45) throw new IllegalArgumentException("[ERROR]: 로또 당첨 숫자에는 1이상 45이하의 숫자를 입력해주세요.");
         }
     }
 
-    private void validateLottoNumberDuplicated(List<Integer> lottoNumbers) {
-        HashSet<Integer> DuplicatedNumber = new HashSet<>(lottoNumbers);
+    private void validateLottoNumberDuplicated(List<Integer> numbers) {
+        HashSet<Integer> DuplicatedNumber = new HashSet<>(numbers);
 
         if (DuplicatedNumber.size() < 6){
             throw new IllegalArgumentException("[ERROR]: 로또 당첨 숫자에는 중복된 숫자를 입력하면 안됩니다.");
