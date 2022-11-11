7fa3333380ce265313cbe359095d43127c8406ca
SEONMl <sm000117@naver.com>
2022-11-11 16:26:45 +0900

    test: 메서드 명 변경에 따른 테스트 코드 수정

--- src/test/java/lotto/util/InputMapperTest.java
+++ src/test/java/lotto/util/InputMapperTest.java
@@ -3,6 +3,7 @@ package lotto.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.*;
@@ -14,21 +15,21 @@ class InputMapperTest {
     @Test
     @DisplayName("split 메서드 기능 테스트")
     void split_with_blank() {
-        Set<Integer> set = inputMapper.stringToSetSplitBy("1 , 2 ,   3  , 4, 5", ",");
+        List<Integer> set = inputMapper.splitBy("1 , 2 ,   3  , 4, 5", ",");
         assertThat(set).isEqualTo(Set.of(1, 2, 3, 4, 5));
     }
 
     @Test
     @DisplayName("split 메서드 기능 테스트")
     void split_without_blank() {
-        Set<Integer> set = inputMapper.stringToSetSplitBy("1,2,3,4,5", ",");
+        List<Integer> set = inputMapper.splitBy("1,2,3,4,5", ",");
         assertThat(set).isEqualTo(Set.of(1, 2, 3, 4, 5));
     }
 
     @Test
     @DisplayName("split 메서드 기능 테스트")
     void split_regex_error() {
-        assertThatThrownBy(() -> inputMapper.stringToSetSplitBy("1,2  ,3.  4.5", ","))
+        assertThatThrownBy(() -> inputMapper.splitBy("1,2  ,3.  4.5", ","))
                 .isInstanceOf(NumberFormatException.class);
     }
 }
