89a04bcf011bdc843e388b9b66690e369660aec3
HaODay <ycis5013@naver.com>
2022-11-11 17:07:13 +0900

    feat(lotto): 당첨 번호를 검증하는 기능 구현
    - 숫자가 6개인지,
    - 숫자 범위는 1~45까지인지
    - 중복되지 않는지 검증

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,9 +14,16 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호는 6개입니다.");
+        }
+        for(int i=0;i<6;i++){
+            if (!(0<numbers.get(i) && numbers.get(i)<= 45)) {
+                throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+        Set<Integer> noDuplicate = new HashSet<>(numbers);
+        if(noDuplicate.size()!= numbers.size()){
+            throw new IllegalArgumentException("로또 번호는 중복되지 않아야 합니다.");
         }
     }
-
-    // TODO: 추가 기능 구현
 }
