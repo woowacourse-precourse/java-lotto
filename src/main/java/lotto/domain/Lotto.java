e147b69c3e5e1e00e4c9d2d1a8a1d8660477dc87
hyun-seung <ardimento22@naver.com>
2022-11-11 17:59:49 +0900

    [FEAT] 입력된 당첨 번호가 정상적인 번호인지 확인한다. - 입력된 숫자가 올바른 로또 숫자 범위인지 확인한다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,11 @@
 package lotto.domain;
 
+import static lotto.utils.Constant.NUMBER_OF_LOTTO;
+
+import java.util.HashSet;
 import java.util.List;
+import lotto.utils.ExceptionType;
+import lotto.view.Output;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,9 +16,31 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateBySize(numbers);
+        validateByRange(numbers);
+    }
+
+    private void validateBySize(List<Integer> numbers) {
+        if (numbers.size() != NUMBER_OF_LOTTO) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private void validateByRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            inLottoRange(number);
+        }
+    }
+
+    private void inLottoRange(int number) {
+        if (number < 1 || number > 45) {
+            Output.printErrorAndExit(ExceptionType.NOT_WITHIN_THE_RANGE.getMessage());
+            throw new IllegalArgumentException(ExceptionType.NOT_WITHIN_THE_RANGE.getMessage());
+        }
+    }
+
+    public void addNumber(int number) {
+        this.numbers.add(number);
     }
 
     // TODO: 추가 기능 구현
--- src/main/java/lotto/utils/Constant.java
+++ src/main/java/lotto/utils/Constant.java
@@ -2,5 +2,5 @@ package lotto.utils;
 
 public class Constant {
     public static final int LOTTO_PRICE = 1000;
-
+    public static final int NUMBER_OF_LOTTO = 6;
 }
