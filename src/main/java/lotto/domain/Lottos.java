626fd45aa15690d280991c390248268a9dbe22b3
YuHayeon <youha0930@gmail.com>
2022-11-12 01:36:23 +0900

    feat(Lottos): 원하는 수량만큼 로또를 발행

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import lotto.exception.LottoPriceException;
 import lotto.view.InputView;
 
@@ -9,13 +11,24 @@ public class Lottos {
 
     private Integer quantity;
 
+    private List<Lotto> lottoNumbers;
+
     public Lottos() {
         this.quantity = countLottoQuantityByInputPrice();
+        this.lottoNumbers = generateLottoNumbersByQuantity();
     }
 
     public Integer countLottoQuantityByInputPrice() {
         String price = InputView.lottoPriceInput();
         LottoPriceException.checkLottoPrice(price);
         return Integer.parseInt(price) / LOTTO_PRICE;
+    }
+
+    public List<Lotto> generateLottoNumbersByQuantity() {
+        List<Lotto> lottoNumbers = new ArrayList<>();
+        for (int i = 0; i < this.quantity; i++) {
+            lottoNumbers.add(new Lotto(LottoGenerator.generateLottoNumbers()));
+        }
+        return lottoNumbers;
     }
 }
