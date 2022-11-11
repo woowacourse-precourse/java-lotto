95479eaeb44845d27c07365c8e20215d6359cb02
HubCreator <chocomilk8604@gmail.com>
2022-11-11 13:09:20 +0900

    refactor(Application) : generateLottos 메서드를 Util 클래스로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,16 +2,16 @@ package lotto;
 
 import lotto.domain.Lotto;
 import lotto.domain.WinLotto;
+import lotto.util.Util;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         try {
-            List<Lotto> generatedLottos = generateLottos(InputView.getGeneratedLottoCount());
+            List<Lotto> generatedLottos = Util.generateLottos(InputView.getGeneratedLottoCount());
 
             WinLotto winLotto = new WinLotto(InputView.getWinLottoNumbers(), InputView.getWinBonusNumber());
 
@@ -21,13 +21,5 @@ public class Application {
         }
     }
 
-    public static List<Lotto> generateLottos(int count) {
-        List<Lotto> generatedLottos = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            Lotto lotto = Lotto.create();
-            System.out.println(lotto.printNumbers());
-            generatedLottos.add(lotto);
-        }
-        return generatedLottos;
-    }
+
 }
--- src/main/java/lotto/util/Util.java
+++ src/main/java/lotto/util/Util.java
@@ -1,6 +1,7 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
 import lotto.enums.ConstVariable;
 import lotto.enums.ErrorMessage;
 
@@ -69,5 +70,15 @@ public class Util {
         return Randoms.pickUniqueNumbersInRange(ConstVariable.MIN_NUMBER.getValue(),
                 ConstVariable.MAX_NUMBER.getValue(),
                 ConstVariable.LOTTO_COUNT.getValue());
+    }
+
+    public static List<Lotto> generateLottos(int count) {
+        List<Lotto> generatedLottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            Lotto lotto = Lotto.create();
+            System.out.println(lotto.printNumbers());
+            generatedLottos.add(lotto);
+        }
+        return generatedLottos;
     }
 }
