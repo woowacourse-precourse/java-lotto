b353001e7b793cf9aecb243233df11fbb9f554d4
bjk1649 <qhwns1649@naver.com>
2022-11-11 21:20:05 +0900

    feat: 로또의 번호가 1 ~ 45 사이의 숫자인지 검증하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,10 +3,13 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    static final int LOTTO_MIN_NUMBER = 1;
+    static final int LOTTO_MAX_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
     private void validateSize(List<Integer> numbers) {
@@ -15,5 +18,11 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1 ~ 45 사이의 숫자이어야합니다.");
+            }
+        }
+    }
 }
