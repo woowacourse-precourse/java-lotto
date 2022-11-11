baafeedb301cc7b345a4003dca936b52103605d9
win9 <krrr8@naver.com>
2022-11-11 14:07:18 +0900

    Feat(print): UI 입,출력부 분리
    요구사항에 따라서 UI를 관장하는 부분을
    분리하여 따로 관리를 하도록 한다.

---
+++ src/main/java/lotto/Util/InputPrint.java
@@ -0,0 +1,9 @@
+package lotto.Util;
+
+import lotto.type.TextType;
+
+public class InputPrint {
+    public static void startInput(){
+        System.out.println(TextType.START);
+    }
+}
---
+++ src/main/java/lotto/Util/OutputPrint.java
@@ -0,0 +1,9 @@
+package lotto.Util;
+
+import lotto.type.TextType;
+
+public class OutputPrint {
+    public static void moneyToNumberPrint(int number){
+        System.out.println(number + "" + TextType.PRICE_TO_NUMBER);
+    }
+}
