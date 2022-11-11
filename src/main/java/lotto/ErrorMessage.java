d917afde7bb6d3b1eb16a83429a27c1b7b94ca7a
swandevson <shai4956@gmail.com>
2022-11-11 15:29:36 +0900

    Feat(ErrorMessage): 예외 메세지 추가
    - 잘못된 단위의 돈에 대한 예외 메세지 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -2,8 +2,9 @@ package lotto;
 
 public class ErrorMessage {
     public static final String ERROR = "[ERROR]";
-    public static final String WRONG_INPUT = "올바르지 않은 입력입니다.";
-    public static final String WRONG_RANGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-    public static final String WRONG_SIZE = "로또 번호는 6개로 이루어져야 합니다.";
-    public static final String DUPLICATED_INPUT = "중복된 숫자가 포함되면 안됩니다.";
+    public static final String WRONG_INPUT = ERROR + " 올바르지 않은 입력입니다.";
+    public static final String WRONG_RANGE = ERROR +" 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String WRONG_SIZE = ERROR + " 로또 번호는 6개로 이루어져야 합니다.";
+    public static final String DUPLICATED_INPUT = ERROR + " 중복된 숫자가 포함되면 안됩니다.";
+    public static final String WRONG_MONEY_UNIT = ERROR + "올바르지 않은 단위입니다.";
 }
