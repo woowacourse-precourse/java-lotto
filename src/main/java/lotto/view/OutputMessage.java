dfccb4cbb6a8a286e55c49d3c9c67d51a5f1df80
MoonJeWoong <munjin0201@naver.com>
2022-11-12 01:18:43 +0900

    feat(view): 로또를 몇 개 구입했는지 출력하는 기능

--- src/main/java/lotto/view/OutputMessage.java
+++ src/main/java/lotto/view/OutputMessage.java
@@ -1,4 +1,9 @@
 package lotto.view;
 
+import java.util.List;
+
 public final class OutputMessage {
+    public static void purchaseComplete(int count){
+        System.out.printf("%d개를 구매했습니다.\n", count);
+    }
 }
