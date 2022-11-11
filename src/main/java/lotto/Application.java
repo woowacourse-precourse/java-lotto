bdd54367c6a99ae612dae17e9b5c6ac1dcd155cf
yimo22 <yimo22@naver.com>
2022-11-12 01:36:25 +0900

    feat: Lotto 시스템 (tmp Saved)
    featured:
    - enum class
    - UI
    
    breakPoint:
    - Lotto.matchResult

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,80 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Application {
+    static int amount, bonusNumber;
+    static List<Lotto> lottos;
+    static List<Integer> answerNumbers;
+    static final int _LOTTO_LAST_NUM = 45;
+    static final int _LOTTO_START_NUM = 1;
+    static final int _LOTTO_COUNT_NUM = 6;
+    static int[] result;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        init();
+        checkingLotto();
+        getLottoNumbers();
+        calc();
+        resultOut();
+    }
+
+    private static void resultOut() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for(LottoPrice cur : LottoPrice.values()){
+            System.out.println(cur.getLabel() + " " + "(" + cur.getPrice() + ")" + " - " + result[cur.ordinal()] + "개");
+        }
+    }
+
+    private static void calc() {
+        // 해당 lottos 의 당첨 여부를 판단하는 함수
+        for (Lotto lotto : lottos){
+            lotto.matchResult(answerNumbers, bonusNumber, result);
+        }
+    }
+
+    private static void getLottoNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        // 당첨 번호는 쉼표(,) 를 기준으로 구분한다.
+        // 토큰의 갯수는 총 6개 && 1~45 사이의 수 이어야 한다.
+        StringTokenizer st = new StringTokenizer(Console.readLine(), ",");
+
+        // Exception 처리
+
+        while (st.hasMoreTokens()) {
+            answerNumbers.add(Integer.parseInt(st.nextToken()));
+        }
+        System.out.println("보너스 번호를 입력해 주세요");
+        bonusNumber = Integer.parseInt(Console.readLine());
+    }
+
+    private static void checkingLotto() {
+        for (Lotto lotto : lottos) {
+            lotto.sortNumbers();
+            System.out.println(lotto);
+        }
+    }
+
+    static void init() {
+        // amount 가 음수가 나올 경우 예외처리
+        System.out.println("구입금액을 입력해 주세요.");
+        amount = Integer.parseInt(Console.readLine());
+        answerNumbers = new ArrayList<>();
+        lottos = new ArrayList<>();
+        result = new int[5];
+        Arrays.fill(result, 0);
+        // making lottos
+        for (int i = 0; i < amount / 1000; i++) {
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(_LOTTO_START_NUM, _LOTTO_LAST_NUM, _LOTTO_COUNT_NUM)));
+        }
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -17,4 +18,36 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+    // sorting numbers
+    public void sortNumbers(){
+        Collections.sort(numbers);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + numbers +"]";
+    }
+
+    // match checking
+    public LottoPrice matchResult(List<Integer> list, int wildCard, int[] result){
+        // Exception 처리
+        // 1. 서로 크기가 다른 경우
+        LottoPrice res;
+        int _cnt = 0;
+        for(int here : numbers){
+            for(int there : list){
+                if(here == there) _cnt++;
+            }
+        }
+        res = LottoPrice.valueOfMatchCounts(_cnt);
+        if(_cnt == 5 && numbers.contains(wildCard)) res = LottoPrice.SECOND;
+        result[res.ordinal()]++;
+        return res;
+    }
+
+    // for bonus card checking
+    public boolean matchBonusNum(int num){
+        return numbers.contains(num);
+    }
 }
