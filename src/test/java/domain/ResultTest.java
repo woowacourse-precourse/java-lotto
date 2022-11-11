a1d04f025ea0dc9dfd6ef0c3fe68d14d66092c0c
Limjongho <whdgh9873@icloud.com>
2022-11-11 19:47:31 +0900

    test(Result): 사용자의 번호가 보너스번호와 일치하는지 확인 테스트 추가 및 중복 리펙토링

--- src/test/java/domain/ResultTest.java
+++ src/test/java/domain/ResultTest.java
@@ -12,16 +12,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class ResultTest {
 
     Result result;
+    List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
 
     @BeforeEach
     void setUp() {
-        result = new Result();
+        this.result = new Result();
     }
 
     @DisplayName("로또 당첨 번호와 구매자의 번호가 일치하는 개수를 반환한다.")
     @Test
     void countMatchingNumberTest1() {
-        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
         List<Integer> userNumbers = List.of(3, 4, 5, 1, 2, 6);
 
         int matchingCount = result.countMatchingNumber(winningNumbers, userNumbers);
@@ -32,7 +32,6 @@ class ResultTest {
     @DisplayName("로또 당첨 번호와 구매자의 번호가 일치하는 개수를 반환한다.")
     @Test
     void countMatchingNumberTest2() {
-        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
         List<Integer> userNumbers = List.of(5, 10, 15, 20, 25, 30);
 
         int matchingCount = result.countMatchingNumber(winningNumbers, userNumbers);
@@ -43,11 +42,32 @@ class ResultTest {
     @DisplayName("로또 당첨 번호와 구매자의 번호가 일치하는 개수를 반환한다.")
     @Test
     void countMatchingNumberTest3() {
-        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
         List<Integer> userNumbers = List.of(1, 3, 7, 9, 11, 13);
 
         int matchingCount = result.countMatchingNumber(winningNumbers, userNumbers);
 
         Assertions.assertThat(matchingCount).isEqualTo(2);
+    }
+
+    @DisplayName("구매자의 번호가 보너스번호와 일치하는지 확인한다.")
+    @Test
+    void isMatchingBonusNumberTest1() {
+        List<Integer> userNumbers = List.of(1, 3, 4, 5, 8, 9);
+        int bonusNumber = 1;
+
+        boolean matchingBonusNumber = result.isMatchingBonusNumber(userNumbers, bonusNumber);
+
+        Assertions.assertThat(matchingBonusNumber).isEqualTo(true);
+    }
+
+    @DisplayName("구매자의 번호가 보너스번호와 일치하는지 확인한다.")
+    @Test
+    void isMatchingBonusNumberTest2() {
+        List<Integer> userNumbers = List.of(1, 3, 4, 5, 8, 9);
+        int bonusNumber = 2;
+
+        boolean matchingBonusNumber = result.isMatchingBonusNumber(userNumbers, bonusNumber);
+
+        Assertions.assertThat(matchingBonusNumber).isEqualTo(false);
     }
 }
