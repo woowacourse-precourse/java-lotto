55879d028d4b63321e3c6e1b89e9006e39b5c7b8
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-11-11 18:38:12 +0900

    fix  : LottoController 로 run() 실행시 IllegalArgumentExcetion에러 발생시 로그를 남기고 종료.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,13 @@
 package lotto;
 
 public class Application {
+    public static final String WRONG_INPUT = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
-        lottoController.run();
+        try {
+            lottoController.run();
+        }catch (IllegalArgumentException e){
+            System.out.println(WRONG_INPUT);
+        }
     }
 }
