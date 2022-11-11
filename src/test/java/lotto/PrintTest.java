c40d514c814726b292a8eae7f82819a85f5d57e1
eora21 <eora21@naver.com>
2022-11-11 22:17:42 +0900

    test: 💍 추가 EnumMap 출력 확인

---
+++ src/test/java/lotto/PrintTest.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import constant.Prize;
+import org.junit.jupiter.api.Test;
+
+import java.util.EnumMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrintTest {
+    private Print print = new Print();
+
+    @Test
+    public void 출력_확인() {
+        EnumMap<Prize, Integer> prizeIntegerEnumMap = new EnumMap<>(Prize.class);
+        prizeIntegerEnumMap.put(Prize.FIRST, 3);
+        prizeIntegerEnumMap.put(Prize.SECOND, 1);
+        prizeIntegerEnumMap.put(Prize.THIRD, 2);
+        prizeIntegerEnumMap.put(Prize.FOURTH, 0);
+        prizeIntegerEnumMap.put(Prize.FIFTH, 22);
+        print.out(prizeIntegerEnumMap);
+    }
+}
