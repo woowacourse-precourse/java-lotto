7d981b9c8e51e1acfdafe830d9835627b4d175bf
khj1 <dhepd8849@gmail.com>
2022-11-11 09:58:19 +0900

    feat: 로또 번호를 발행하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Lotto {
+    public static final int LOTTO_SIZE = 6;
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +17,14 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    public Lotto creatRandom() {
+        List<Integer> uniqueNumbers = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, LOTTO_SIZE);
+
+        return new Lotto(uniqueNumbers);
+    }
 }
