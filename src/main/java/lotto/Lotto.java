c9f4b539b9d5ee371d4c135f423b29875a466d99
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-12 05:14:55 +0900

    feat(Lotto): 입력된 값 중 중복되는 값 확인 기능 추가
    입력된 값들 중 중복되는 값이 존재하는지 확인한다.
    만약 중복되는 값이 존재한다면, 서로 다른 숫자를 입력해 달라는 메시지와 함께 예외를 발생시킨다.
    docs: 중복되는 값이 없는지 확인, 중복되는 값이 있는 경우 예외 발생 완료 체크

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -26,8 +26,9 @@ public class Lotto {
             throw new IllegalArgumentException(ExceptionMessage.LOTTO_WRONG_NUMBER_OF_VALUE.getMessage());
         } else if (!isBetweenLottoRange(numbers)) {
             throw new IllegalArgumentException(ExceptionMessage.LOTTO_OUT_OF_RANGE.getMessage());
+        } else if (isDuplicateNumber(numbers)) {
+            throw new IllegalArgumentException(ExceptionMessage.LOTTO_DUPLICATE_VALUE.getMessage());
         }
-
     }
 
     private void validateString(String numbers) {
@@ -62,5 +63,9 @@ public class Lotto {
 
     private boolean isBetweenLottoRange(List<Integer> numbers) {
         return numbers.stream().allMatch(number -> number >= LOTTO_GAME_START_NUMBER && number <= LOTTO_GAME_END_NUMBER);
+    }
+
+    private boolean isDuplicateNumber(List<Integer> numbers) {
+        return numbers.size() != numbers.stream().distinct().count();
     }
 }
