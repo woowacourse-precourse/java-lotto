11f5314be79722492928e1dbc2779e34e7574a17
khsrla9806 <kimhunsope@naver.com>
2022-11-11 17:13:44 +0900

    feat(LottoManager): 입력된 금액의 수만큼 로또를 발행하는 메서드 구현 issueLottoSeveralTimes: 로또 발행 가능 갯수만큼 로또를 발행하는 메서드

--- src/main/java/lotto/domain/LottoManager.java
+++ src/main/java/lotto/domain/LottoManager.java
@@ -2,15 +2,26 @@ package lotto.domain;
 
 import lotto.view.InputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoManager {
     private int purchaseAmount;
     private int lottoIssueCount;
+    private List<Lotto> myLotto = new ArrayList<>();
 
     private void savePurchaseAmount() {
         this.purchaseAmount = InputView.enterPurchaseAmount();
     }
     private void saveLottoIssueCount() {
         this.lottoIssueCount = purchaseAmount / 1000;
+    }
+    private void issueLottoSeveralTimes(int lottoIssueCount, List<Lotto> myLotto) {
+        for (int count = 0; count < lottoIssueCount; count++) {
+            LottoMachine lottoMachine = new LottoMachine();
+            Lotto lotto = new Lotto(lottoMachine.issueLotto());
+            myLotto.add(lotto);
+        }
     }
 
 }
