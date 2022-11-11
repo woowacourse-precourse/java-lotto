69bc32e14746cfe96a372cf5b8c82cacd478e6e2
YuHayeon <youha0930@gmail.com>
2022-11-12 01:32:00 +0900

    feat(Lotto): 로또 랜덤값에 중복된 값이 없는지 확인 후 있을 경우 예외처리
    로또 랜덤값에 중복된 값이 없는지 확인
    있을 경우 로또 랜덤값에 중복된 값이 있는 메시지를 출력

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+
+    public static final int LOTTO_NUMBERS_SIZE = 6;
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        checkRightSize(numbers);
+        checkUniqueNumbers(numbers);
+    }
+
+    private void checkRightSize(List<Integer> numbers) {
+        if (LOTTO_NUMBERS_SIZE != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void checkUniqueNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (LOTTO_NUMBERS_SIZE != uniqueNumbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 값이 있습니다.");
+        }
+    }
+}
