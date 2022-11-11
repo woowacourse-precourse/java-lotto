fc9bfb53673f4ef450261d0e056712d85275b03b
TaeMin <xoals3094@naver.com>
2022-11-12 06:54:32 +0900

    feat(Bonus) : bonus getter 추가

--- src/main/java/lotto/Bonus.java
+++ src/main/java/lotto/Bonus.java
@@ -13,4 +13,8 @@ public class Bonus {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
+
+    public int getBonus(){
+        return this.bonus;
+    }
 }
