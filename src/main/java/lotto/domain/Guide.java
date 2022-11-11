539f34dbf44f5667b71b4beb287b87185a973c5a
wath1457 <wath1457@gmail.com>
2022-11-11 21:31:46 +0900

    feat(Guide): 로또 구매 개수 출력 추가

--- src/main/java/lotto/domain/Guide.java
+++ src/main/java/lotto/domain/Guide.java
@@ -4,4 +4,8 @@ public class Guide {
     public static void printGetMoney() {
         System.out.println("구입금액을 입력해 주세요.");
     }
+
+    public static void printLottoCount(int LottoCount) {
+        System.out.println("\n" + Integer.toString(LottoCount) + "개를 구매했습니다.");
+    }
 }
