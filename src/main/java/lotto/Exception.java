ea87e0c8978d73c9b7d39936e70bdf29b2177523
소대훈 <theogns0681@naver.com>
2022-11-11 09:27:29 +0900

    [refactor] (Exception) 예외 발생시 return true 반환

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -1,54 +1,75 @@
 package lotto;
 
 public class Exception {
-    public void initialInput(String input) {
-        onlyNumber(input);
+    public boolean initialInput(String input) throws IllegalArgumentException {
+        if (onlyNumber(input)) return true;
         int inMoney = Integer.parseInt(input);
-        onlyOneThousand(inMoney);
+        if (onlyOneThousand(inMoney)) return true;
         System.out.println();
+        return false;
     }
 
-    private void onlyOneThousand(int inMoney) {
-        if (inMoney % 1000 != 0) {
-            System.out.println("[ERROR] 1000원 단위로 입력해주세요.");
-            throw new IllegalArgumentException();
+    private boolean onlyOneThousand(int inMoney) {
+        try {
+            if (inMoney % 1000 != 0) getTh();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return true;
         }
+        return false;
     }
 
-    private void onlyNumber(String temp) {
+    private boolean onlyNumber(String temp) {
         for (int i = 0; i < temp.length(); i++) {
             try {
-                if (temp.charAt(i) < '0' || temp.charAt(i) > '9') getMS();
-            }catch (IllegalArgumentException e){
+                if (temp.charAt(i) < '0' || temp.charAt(i) > '9') getNum();
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
+                return true;
             }
         }
+        return false;
     }
 
-    public void inputWinningBonus(String input) {
-        onlyNumberBonus(input);
+    public boolean inputWinningBonus(String input) {
+        if (onlyNumberBonus(input)) return true;
         int bonus = Integer.parseInt(input);
-        onlyRangeBonus(bonus);
+        if (onlyRangeBonus(bonus)) return true;
         System.out.println();
+        return false;
     }
 
-    private void onlyRangeBonus(int bonus) {
-        if (bonus < 1 || bonus > 45) {
-            System.out.println("[ERROR] 1 ~ 45 사이의 숫자를 입력해주세요.");
-            throw new IllegalArgumentException();
+    private boolean onlyRangeBonus(int bonus) {
+        try {
+            if (bonus < 1 || bonus > 45) getRan();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return true;
         }
+        return false;
     }
 
-    private void onlyNumberBonus(String input) {
+    private boolean onlyNumberBonus(String input) {
         for (int i = 0; i < input.length(); i++) {
             try {
-                if (input.charAt(i) < '0' || input.charAt(i) > '9') getMS();
-            }catch (IllegalArgumentException e){
+                if (input.charAt(i) < '0' || input.charAt(i) > '9') getNum();
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
+                return true;
             }
         }
+        return false;
     }
-    private void getMS(){
+
+    private void getNum() {
         throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
+    }
+
+    private void getTh() {
+        throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
+    }
+
+    private void getRan() {
+        throw new IllegalArgumentException("[ERROR] 1에서 45 사이의 숫자를 입력해주세요.");
     }
 }
