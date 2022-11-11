106f7420d3e51e871150b6b5d356d7c2f45dd167
TaeMin <xoals3094@naver.com>
2022-11-12 07:27:13 +0900

    fix(LottoGenerator) : 로또 번호 생성기가 생성할 로또 개수를 입력받고, 입력받은 로또 개수만큼 로또를 생성합니다

--- src/main/java/lotto/Model/LottoGenerator.java
+++ src/main/java/lotto/Model/LottoGenerator.java
@@ -1,11 +1,19 @@
-package lotto.domain;
+package lotto.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
 
 public class LottoGenerator {
-    public List<Integer> createLottoNumber() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    public List<Lotto> createMyLottos(int lottoNumber) {
+        List<Lotto> myLottos = new ArrayList<>();
+        for(int i = 0; i < lottoNumber; i++){
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            myLottos.add(lotto);
+        }
+
+        return myLottos;
     }
 }
