9120d14a37dea9f31845697e98ec90321cc61397
newh08 <newh08@gmail.com>
2022-11-11 19:46:28 +0900

    test: implement tests for function that inputting win numbers
    Test the error that wrong input values occurring.
    Test with correct input value.

--- src/test/java/lotto/model/InputLottoTest.java
+++ src/test/java/lotto/model/InputLottoTest.java
@@ -5,6 +5,9 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,7 +20,7 @@ class InputLottoTest {
         inputLotto = new InputLotto();
     }
 
-    @DisplayName("숫자가 아닌 값을 넣으면 IllegalArgumentException 이 발생합니다.")
+    @DisplayName("구매 금액 입력 시 숫자가 아닌 값을 넣으면 IllegalArgumentException 이 발생합니다.")
     @Test
     void inputMoneyTest1() {
         String input = "wrong";
@@ -26,7 +29,7 @@ class InputLottoTest {
         assertThatIllegalArgumentException().isThrownBy(() -> inputLotto.inputMoney());
     }
 
-    @DisplayName("1000단위 숫자가 아닌 값을 넣으면 IllegalArgumentException 이 발생합니다.")
+    @DisplayName("구매 금액 입력 시 1000단위 숫자가 아닌 값을 넣으면 IllegalArgumentException 이 발생합니다.")
     @Test
     void inputMoneyTest2() {
         String input = "12450";
@@ -35,12 +38,72 @@ class InputLottoTest {
         assertThatIllegalArgumentException().isThrownBy(() -> inputLotto.inputMoney());
     }
 
-    @DisplayName("정확한 값을 넣으면 그 값을 Integer 로 반환합니다.")
+    @DisplayName("구매 금액 입력 시 정확한 값을 넣으면 그 값을 Integer 로 반환합니다.")
     @Test
     void inputMoneyTest3() {
         String input = "12000";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
         assertThat(inputLotto.inputMoney()).isEqualTo(12000);
+    }
+
+    @DisplayName("당첨 번호 입력 시 정확한 입력값은 List 로 변환해 반환합니다.")
+    @Test
+    void inputWinNumberTest1() {
+        String input = "1,2,3,4,5,6";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        List<Integer> expect = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        assertThat(inputLotto.inputWinNumber()).isEqualTo(expect);
+    }
+
+    @DisplayName("당첨 번호 입력시 숫자 구분시 ',' 이 아닌 다른것을 사용하면 IllegalArgumentException 이 발생합니다.")
+    @Test
+    void inputWinNumberTest2() {
+        String input = "1.2,3,4,5,6";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatIllegalArgumentException().isThrownBy(() -> inputLotto.inputWinNumber());
+    }
+
+    @DisplayName("당첨 번호 입력시 1~45 사이 이외의 숫자를 넣으면 IllegalArgumentException 이 발생합니다.")
+    @Test
+    void inputWinNumberTest3() {
+        String input = "123,2,3,4,5,6";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatIllegalArgumentException().isThrownBy(() -> inputLotto.inputWinNumber());
+    }
+
+    @DisplayName("당첨 번호 입력시 숫자 이외 다른것을 넣으면 IllegalArgumentException 이 발생합니다.")
+    @Test
+    void inputWinNumberTest4() {
+        String input = "s,2,3,4,5,6";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatIllegalArgumentException().isThrownBy(() -> inputLotto.inputWinNumber());
+    }
+
+    @DisplayName("당첨 번호 입력시 6개 초과의 숫자를 넣으면 IllegalArgumentException 이 발생합니다.")
+    @Test
+    void inputWinNumberTest5() {
+        String input = "1,2,3,4,5,6,7";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatIllegalArgumentException().isThrownBy(() -> inputLotto.inputWinNumber());
+    }
+
+    @DisplayName("당첨 번호 입력시 중복된 숫자를 넣으면 IllegalArgumentException 이 발생합니다.")
+    @Test
+    void inputWinNumberTest6() {
+        String input = "1,1,3,4,5,6";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatIllegalArgumentException().isThrownBy(() -> inputLotto.inputWinNumber());
     }
 }
