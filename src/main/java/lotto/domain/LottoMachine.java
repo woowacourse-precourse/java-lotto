49ee90dae98c70be8f0b6c56b9b9badcdafbda57
limzzum <chfhddl1234@naver.com>
2022-11-11 20:18:57 +0900

    feat : 구매한 로또 수 만큼 로또 생성

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,10 +1,17 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class LottoMachine {
     private final int LOTTO_PRICE = 1000;
     private int paid;
+    private ArrayList<Lotto> lottos = new ArrayList<>();
 
     public LottoMachine() {}
 
@@ -19,6 +26,17 @@ public class LottoMachine {
         if(money % LOTTO_PRICE != 0){
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해 주세요");
         }
+    }
+
+    public List<Lotto> createLottoNumber(){
+        int cnt = paid / LOTTO_PRICE;
+        for(int i=0; i<cnt; i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+        }
+        paid = 0;
+        return lottos;
     }
 
 
