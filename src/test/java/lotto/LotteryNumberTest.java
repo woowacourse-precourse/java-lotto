b96f7880ee3f429596e6786ecebdf6ab46017d33
NotableDeveloper <castlereal@naver.com>
2022-11-10 21:56:14 -0800

    <test> : 추첨 번호 생성 관련 테스트 코드 최초 작성 추첨 번호가 구매 수량만큼 생성되었는 지에 대한 테스트 코드를 최초로 작성.

--- src/test/java/lotto/LotteryNumberTest.java
+++ src/test/java/lotto/LotteryNumberTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LotteryNumberTest {
+    @DisplayName("추첨 번호는 사용자가 구매한 수량만큼 생성되어야 한다.")
     @Test
-    void createLotteryNumberToSuccessful() {
-
+    void createLotteryNumberByPurchaseQuantity() {
+        LotteryNumber lotteryNumber = new LotteryNumber();
     }
 }
