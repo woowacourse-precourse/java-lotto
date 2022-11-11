b39108943815c81d6fef4f7e4cc7af5e93dcf8d2
Kwon-sang <kwonsg514@gmail.com>
2022-11-11 23:25:20 +0900

    [당첨및 보너스번호 입력 UI를 위한 controller 구성]

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,9 +6,10 @@ import lotto.domain.*;
 public class Application {
     public static void main(String[] args) {
         purchaseInputController();
+        winningInputController();
     }
 
-    static void purchaseInputController () {
+    static void purchaseInputController() {
         System.out.println("구입 금액을 입력해 주세요.");
         String purchaseAmount = Console.readLine();
 
@@ -19,4 +20,17 @@ public class Application {
             lotto.printLotto();
         }
     }
+
+    static void winningInputController() {
+        Winning winning = new Winning();
+
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        String winningNumberInput = Console.readLine();
+        winning.initWinningNumbers(winningNumberInput);
+
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        String bonusNumberInput = Console.readLine();
+        winning.initBonusNumber(bonusNumberInput);
+    }
+
 }
--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -4,53 +4,31 @@ import java.util.*;
 
 public class Winning {
     private final String ERROR_MSG = "[ERROR] 잘못된 입력 입니다.";
-    private final List<Integer> winning_numbers;
-    private final int bonusNumber;
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
 
-    public Winning(String winningNumberInput, String bonusNumberInput) {
-        this.winning_numbers = transformWinningInput(winningNumberInput);
-        this.bonusNumber = transformBonusInput(bonusNumberInput);
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
     }
 
-    public List<Integer> getWinning_numbers() {
-        return winning_numbers;
+    public void initWinningNumbers(String winningNumberInput) {
+        this.winningNumbers = new ArrayList<>();
+        String[] inputs = winningNumberInput.split(",");
+        for (String input : inputs) {
+            validate(input);
+            this.winningNumbers.add(Integer.parseInt(input));
+        }
+        validateInputLength(this.winningNumbers);
     }
 
     public int getBonusNumber() {
         return bonusNumber;
     }
 
-
-    public List<Integer> transformWinningInput(String winningNumberInput) {
-        validateWinningNumber(winningNumberInput);
-        String[] inputs = winningNumberInput.split(",");
-
-        List<Integer> winning = new ArrayList<>();
-        Arrays.stream(inputs).mapToInt(Integer::parseInt).forEach(winning::add);
-        return winning;
-    }
-
-    public int transformBonusInput(String bonusNumberInput) {
-        validateBonusNumber(bonusNumberInput);
-        return Integer.parseInt(bonusNumberInput);
-    }
-
-
-    public void validateWinningNumber(String winningNumberInput) {
-        String[] inputs = winningNumberInput.split(",");
-        for (String number : inputs) {
-            validate(number);
-        }
-
-        if (inputs.length != 6) {
-            throw new IllegalArgumentException(ERROR_MSG);
-        }
-    }
-
-    public void validateBonusNumber(String bonusNumberInput) {
+    public void initBonusNumber(String bonusNumberInput) {
         validate(bonusNumberInput);
+        this.bonusNumber = Integer.parseInt(bonusNumberInput);
     }
-
 
     public void validate(String input) {
         validateStringToInteger(input);
@@ -75,7 +53,13 @@ public class Winning {
 
     public void validateUniqueNumber(String input) {
         int intInput = Integer.parseInt(input);
-        if (this.winning_numbers.contains(intInput)) {
+        if (this.winningNumbers.contains(intInput)) {
+            throw new IllegalArgumentException(ERROR_MSG);
+        }
+    }
+
+    public void validateInputLength(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != 6) {
             throw new IllegalArgumentException(ERROR_MSG);
         }
     }
