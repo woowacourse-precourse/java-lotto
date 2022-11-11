13293594e06c1347ac3bbfb7fc78aec780a1683e
박준용 <jo101044@naver.com>
2022-11-11 20:32:25 +0900

    feat: "LottoList 생성"
    LottoList 생성자 : 실행시 LottoList 를 생성,
    createLottoList : 입력 받은 값 만큼 LottoList 에 Lotto 를 추가,
    validate() : 유효성 검사,
    getLottoList() : LottoList 의 getter,
    toString() : 발행한 lottoList 의 값들을 문자열로 반환

---
+++ src/main/java/lotto/LottoList.java
@@ -0,0 +1,52 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoList {
+    private final List<Lotto> lottoList;
+
+    public LottoList(int money) {
+        validate(money);
+
+        this.lottoList = createLottoList(money / 1000);
+    }
+
+    private List<Lotto> createLottoList(int size) {
+        List<Lotto> lottoList = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            List<Integer> list = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+            Lotto lotto = new Lotto(list);
+
+            lottoList.add(lotto);
+        }
+
+        return lottoList;
+    }
+
+    private void validate(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
+    }
+
+    public List<Lotto> getLottoList() {
+        return lottoList;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(lottoList.size()).append("개를 구매했습니다.");
+
+        lottoList.stream().map(Lotto::getNumbers)
+                .forEach(integers -> sb.append("\n").append(integers));
+
+        return sb.toString();
+    }
+}
