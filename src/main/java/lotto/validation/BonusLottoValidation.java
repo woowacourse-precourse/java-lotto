21b39ae4cd8815723928be2c873cafd7921a7b4e
math0917 <math0917@naver.com>
2022-11-11 19:18:34 +0900

    feat(BonusLottoValidation): 작성 test를 통과하는 코드

--- src/main/java/lotto/validation/BonusLottoValidation.java
+++ src/main/java/lotto/validation/BonusLottoValidation.java
@@ -16,6 +16,7 @@ public class BonusLottoValidation extends Validation{
         isBonusLottoInputLength(splitString);
         isBonusLottoInputNumeric(splitString);
         isBonusLottoInputBetween(splitString);
+        isContainsWinningLotto(splitString[0], compareToUserInput);
     }
 
     private void isBonusLottoInputLength(String[] splitString) {
@@ -34,6 +35,13 @@ public class BonusLottoValidation extends Validation{
         if (!(LottoValidation.isLottoInputBetween(splitString, IntConstant.LOTTO_NUMBER_BIGGER_THAN,
                 IntConstant.LOTTO_NUMBER_SMALLER_THAN))) {
             createError(StringConstant.BONUS_LOTTO_INPUT_NUMBER_BETWEEN_ERROR_MESSAGE.getMessage());
+        }
+    }
+
+    private void isContainsWinningLotto(String bonusLotto, List<Integer> compareToUserInput) {
+        int bonus = Integer.parseInt(bonusLotto);
+        if (compareToUserInput.contains(bonus)) {
+            createError(StringConstant.BONUS_LOTTO_OVERLAPPING_WITH_WINNING_LOTTO_ERROR_MESSAGE.getMessage());
         }
     }
 
