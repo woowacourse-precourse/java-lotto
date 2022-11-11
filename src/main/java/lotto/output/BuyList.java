eda7fe9789c227abd6c9bc46d792980e90aaa364
Ridealist <cjsn92@gmail.com>
2022-11-12 00:47:41 +0900

    feat(BuyList): 총 구입한 로또 번호 리스트 콘솔에 프린트해 보여주는 기능 구현

--- src/main/java/lotto/output/BuyList.java
+++ src/main/java/lotto/output/BuyList.java
@@ -1,13 +1,17 @@
 package lotto.output;
 
-import java.util.Collection;
+import lotto.domain.Lotto;
 import java.util.Collections;
 import java.util.List;
 
 public class BuyList {
 
-    public void show(List<Integer> lottoNumbers) {
-        Collections.sort(lottoNumbers);
-        System.out.println(lottoNumbers);
+    public static void show(List<Lotto> lotteryNumbers, int purchaseAmount) {
+        System.out.println(purchaseAmount + "개를 구매했습니다.");
+        for (Lotto lotto : lotteryNumbers) {
+            List<Integer> lottoNumbers = lotto.getLottoNumbers();
+            Collections.sort(lottoNumbers);
+            System.out.println(lotteryNumbers);
+        }
     }
 }
