d05acb6b8426fa889d9f74669745f870c5df35ae
2jun0 <soo28819@gmail.com>
2022-11-11 23:14:59 +0900

    Refactor(LottoGame): play함수의 분리
    구입금액 입력
    구입한 로또 출력
    당첨번호 입력
    보너스 번호 입력
    구입한 로또와 당첨번호의 일치 확인
    당첨 통계 출력
    수익률 출력

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -21,58 +21,92 @@ public class LottoGame {
         this.output = output;
     }
 
+    private int inputMoney() {
+        output.writeEnterMoney();
+        int money = input.readInt();
+        output.writeEmptyLine();
+
+        return money;
+    }
+
+    private void printLottos(List<Lotto> lottos) {
+        output.writeBuyCount(lottos.size());
+        for (Lotto lotto : lottos) {
+            output.writeLine(lotto.toString());
+        }
+        output.writeEmptyLine();
+    }
+
+    private List<Integer> inputWinningNumbers() {
+        output.writeEnterWinningNumbers();
+        List<Integer> numbers = input.readIntList();
+        output.writeEmptyLine();
+
+        return numbers;
+    }
+
+    private int inputBonusNumber() {
+        output.writeEnterBonusNumber();
+        int bonusNumber = input.readInt();
+        output.writeEmptyLine();
+
+        return bonusNumber;
+    }
+
+    private Results matchLottoWithWinningNumbers(List<Lotto> lottos,
+            WinningNumbers winningNumbers) {
+        List<Reward> rewards = new ArrayList<>();
+
+        for (Lotto lotto : lottos) {
+            Reward reward = winningNumbers.match(lotto);
+            if (reward != null) {
+                rewards.add(reward);
+            }
+        }
+
+        return new Results(rewards);
+    }
+
+    private void printStatistics(Results results) {
+        output.writePrefixMatchStatistics();
+        for (Reward reward : Reward.values()) {
+            if (!reward.isRequireBonus()) {
+                output.writeMatchStatistic(
+                        reward.getMatch(),
+                        reward.getReward(),
+                        results.getCount(reward));
+            }
+
+            if (reward.isRequireBonus()) {
+                output.writeMatchStatisticWithBonus(
+                        reward.getMatch(),
+                        reward.getReward(),
+                        results.getCount(reward));
+            }
+        }
+    }
+
+    private void printYield(Results results, int money) {
+        output.writeYield(results.getYield(money));
+    }
+
     public void play() {
         try {
-            output.writeEnterMoney();
-            int money = input.readInt();
-            output.writeEmptyLine();
+            int money = inputMoney();
 
             LottoSeller seller = new LottoSeller();
             List<Lotto> lottos = seller.buyLottos(money);
 
-            output.writeBuyCount(lottos.size());
-            for (Lotto lotto : lottos) {
-                output.writeLine(lotto.toString());
-            }
-            output.writeEmptyLine();
+            printLottos(lottos);
 
-            output.writeEnterWinningNumbers();
-            List<Integer> numbers = input.readIntList();
-            output.writeEmptyLine();
-
-            output.writeEnterBonusNumber();
-            int bonusNumber = input.readInt();
-            output.writeEmptyLine();
-
+            List<Integer> numbers = inputWinningNumbers();
+            int bonusNumber = inputBonusNumber();
             WinningNumbers winningNumbers = new WinningNumbers(numbers, bonusNumber);
-            List<Reward> rewards = new ArrayList<>();
 
-            for (Lotto lotto : lottos) {
-                Reward reward = winningNumbers.match(lotto);
-                if (reward != null) {
-                    rewards.add(reward);
-                }
-            }
+            Results results = matchLottoWithWinningNumbers(lottos, winningNumbers);
 
-            Results results = new Results(rewards);
-
-            output.writePrefixMatchStatistics();
-            for (Reward reward : Reward.values()) {
-                if (!reward.isRequireBonus()) {
-                    output.writeMatchStatistic(
-                            reward.getMatch(),
-                            reward.getReward(),
-                            results.getCount(reward));
-                }
-
-                if (reward.isRequireBonus()) {
-                    output.writeMatchStatisticWithBonus(
-                            reward.getMatch(),
-                            reward.getReward(),
-                            results.getCount(reward));
-                }
-            }
-            output.writeYield(results.getYield(money));
+            printStatistics(results);
+            printYield(results, money);
         } catch (IllegalArgumentException exception) {
             output.writeErrorMessage(exception.getMessage());
         }
