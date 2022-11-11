c3687cff7e02ffe595bda04861c73fc41556553c
정승구 <jeongseung-gu@jeongseung-guui-MacBookAir.local>
2022-11-11 22:45:12 +0900

    feat: UserData 생성자 구현 및 간단한 데이터 처리 메서드 구현

--- src/main/java/lotto/model/UserData.java
+++ src/main/java/lotto/model/UserData.java
@@ -6,4 +6,24 @@ public class UserData {
     private int purchaseAmount;
     private int gameCount;
     private List<Lotto> games;
+
+    UserData(int purchaseAmount) {
+        checkMoney(purchaseAmount);
+        this.purchaseAmount = purchaseAmount;
+        this.gameCount = calculateGameCount(this.purchaseAmount);
+        this.games = makeLottoGame(this.gameCount);
+    }
+
+    private int calculateGameCount(int purchaseAmount) {
+        return purchaseAmount / 1000;
+    }
+
+    private void checkMoney(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위로만 구입 가능합니다.");
+        }
+    }
+    private List<Lotto> makeLottoGame(int gameCount) {
+        return null;
+    }
 }
