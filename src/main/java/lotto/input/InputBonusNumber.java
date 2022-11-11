1a4771742165098d4cb83f35973b98c33c0d301a
chanwonlee <clw8679@naver.com>
2022-11-12 00:16:55 +0900

    feat: InputBonusNumber 구현

---
+++ src/main/java/lotto/input/InputBonusNumber.java
@@ -0,0 +1,20 @@
+package lotto.input;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.model.BonusNumber;
+
+public class InputBonusNumber {
+    public BonusNumber input() {
+        String input = Console.readLine();
+        validateInteger(input);
+        return new BonusNumber(Integer.parseInt(input));
+    }
+
+    private void validateInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자를 입력해야 합니다.");
+        }
+    }
+}
