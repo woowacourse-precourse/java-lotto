c49e7ecb83520cf85df8ad37b78dfa6978a6a01a
EUNJEONGMUN <munej26@gmail.com>
2022-11-11 13:09:10 +0900

    test: LottoNumber 클래스의 당첨번호 입력 관련 테스트코드 작성
    당첨번호 정상입력, 비정상입력 - 숫자인지 여부, 범위 충족여부

---
+++ src/test/java/lotto/LottoNumberTest.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class LottoNumberTest {
+    @Test
+    void 당첨번호_정상입력() {
+        new LottoNumber("1,2,3,4,5,6", "7");
+    }
+
+    @Test
+    void 당첨번호_비정상입력_숫자x() {
+        assertThatThrownBy(() -> new LottoNumber("1*,2,3,4,5,6", "7"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 숫자를 입력해주세요.");
+    }
+
+    @Test
+    void 당첨번호_비정상입력_숫자범위초과() {
+        assertThatThrownBy(() -> new LottoNumber("1,2,3,4,5,46", "7"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 1에서 45사이의 숫자를 입력해주세요.");
+    }
+
+    @Test
+    void 당첨번호_비정상입력_숫자범위미달() {
+        assertThatThrownBy(() -> new LottoNumber("1,2,3,4,5,0", "7"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 1에서 45사이의 숫자를 입력해주세요.");
+    }
+}
