6afa1d78d93724ef58cfe9255b6e5ddf210f8023
kokodak <kokodakadokok@gmail.com>
2022-11-11 11:19:49 +0900

    feat(winningRank): Map 형태의 초기 당첨 내역을 생성하는 기능 구현

--- src/main/java/lotto/domain/WinningRank.java
+++ src/main/java/lotto/domain/WinningRank.java
@@ -1,11 +1,17 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+
 public enum WinningRank {
     FIRST_PLACE(6, false, 2_000_000_000),
     SECOND_PLACE(5, true, 30_000_000),
     THIRD_PLACE(5, false, 1_500_000),
     FOURTH_PLACE(4, false, 50_000),
     FIFTH_PLACE(3, false, 5_000);
+
+    private static final int INITIAL_VALUE = 0;
 
     private final int matchingCount;
     private final boolean containsBonusNumber;
@@ -15,6 +21,12 @@ public enum WinningRank {
         this.matchingCount = matchingCount;
         this.containsBonusNumber = containsBonusNumber;
         this.winningPrice = winningPrice;
+    }
+
+    public static Map<WinningRank, Integer> getWinningDetails() {
+        Map<WinningRank, Integer> winningDetails = new EnumMap<>(WinningRank.class);
+        Arrays.stream(values()).forEach(winningRank -> winningDetails.put(winningRank, INITIAL_VALUE));
+        return winningDetails;
     }
 
     public int getMatchingCount() {
