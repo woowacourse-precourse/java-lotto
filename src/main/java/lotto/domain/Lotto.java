b8b4a4ef5ce718907ec8ebd62760696dc17df192
khsrla9806 <kimhunsope@naver.com>
2022-11-11 20:41:53 +0900

    fix(Lotto): 로또 번호에 중복된 숫자가 있을 때 예외 발생을 위한 코드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,6 +14,9 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+        if (Set.copyOf(numbers).size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 존재합니다.");
         }
     }
 
