f8ad8b27c84905120122470399a6185c04f9c951
임원정 <oddish0513@gmail.com>
2022-11-11 22:22:14 +0900

    docs(lotto): 기능 목록 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+/*
+객체지향 프로그래밍
+1. 기능을 가지고 있는 객체를 인스턴스(객체)화 한다.
+2. 필요한 기능을 인스턴스가 수행하게 한다.
+3. 각 결과를 종합한다.
+ */
+
 public class Application {
     public static void main(String[] args) {
+
         // TODO: 프로그램 구현
     }
 }
