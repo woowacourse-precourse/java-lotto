4e3a7c0a8d1c288c74843bed06d7072baa3d9e0d
hectick <chyeon1223@cau.ac.kr>
2022-11-11 16:06:28 +0900

    feat: 수익률을 계산하는 기능 추가

--- src/main/java/lotto/domain/LottoPurchaseInformation.java
+++ src/main/java/lotto/domain/LottoPurchaseInformation.java
@@ -5,25 +5,38 @@ import java.util.List;
 
 public class LottoPurchaseInformation {
     private static final int THOUSAND = 1000;
-    private final int NUMBER_OF_TICKETS;
     private ValidationInDomain validator = new ValidationInDomain();
     private List<Lotto> lottoTickets = new ArrayList<>();
     private LottoGenerator lottoGenerator = new LottoGenerator();
+    private int numberOfTickets;
+    private int purchaseAmount;
+    private int totalWinningAmount = 10000;
+    private double rateOfReturn;
 
 
     public LottoPurchaseInformation(int purchaseAmount){
         validator.checkDividedByThousand(purchaseAmount);
-        NUMBER_OF_TICKETS = purchaseAmount/THOUSAND;
+        this.purchaseAmount = purchaseAmount;
+        this.numberOfTickets = this.purchaseAmount/THOUSAND;
     }
     public List<Lotto> getLottoTickets(){
         return lottoTickets;
     }
 
     public void generateLottoTickets(){
-        for(int i = 0; i < NUMBER_OF_TICKETS; i++){
+        for(int i = 0; i < numberOfTickets; i++){
             Lotto lotto = new Lotto(lottoGenerator.generateLottoNumbers());
             lottoTickets.add(lotto);
         }
+    }
+
+    public void calculateRateOfReturn(){
+        double tmp = ((double)totalWinningAmount/purchaseAmount)*1000;
+        rateOfReturn = Math.round(tmp)/10.0;
+    }
+
+    public double getRateOfReturn(){
+        return rateOfReturn;
     }
 
 
