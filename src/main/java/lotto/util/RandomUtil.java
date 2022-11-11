28264437c3245943aaf8a0f1217c5ae5d963d6f4
the9kim <96895686+the9kim@users.noreply.github.com>
2022-11-11 13:48:21 +0900

    feat: 로또 번호 랜덤 생성 기능 구현 및 테스트

---
+++ src/main/java/lotto/util/RandomUtil.java
@@ -0,0 +1,59 @@
+package lotto.util;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomUtil {
+
+    static final int LENGTH_OF_LOTTO_NUMBERS = 6;
+
+    public static List<List<Integer>> getLottoNumbers(String money){
+        int countFromMoney = getCountFromMoney(money);
+        return createLottoNumberList(countFromMoney);
+    }
+
+    private static int getCountFromMoney(String money) {
+        int MoneyOfInteger = Integer.parseInt(money);
+        int countFromMoney = MoneyOfInteger / 1000;
+        return countFromMoney;
+    }
+
+    private static List<List<Integer>> createLottoNumberList(int countFromMoney) {
+        List<List<Integer>> lottoNumberList = new ArrayList<>();
+        for (int i=0; i<countFromMoney; i++) {
+            List<Integer> lottoNumbers = createLottoNumbers();
+            lottoNumberList.add(lottoNumbers);
+        }
+        return lottoNumberList;
+    }
+
+    private static List<Integer> createLottoNumbers() {
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        while (!validateLottoNumbers(lottoNumbers)) {
+            lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        }
+        return lottoNumbers;
+    }
+
+    private static boolean validateLottoNumbers(List<Integer> lottoNumbers) {
+        Collections.sort(lottoNumbers);
+        for (int i=0; i<LENGTH_OF_LOTTO_NUMBERS; i++) {
+            if (isDuplicate(lottoNumbers, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isDuplicate(List<Integer> lottoNumbers, int index) {
+        Integer eachNumber = lottoNumbers.get(index);
+        List<Integer> restOfListValues = lottoNumbers.subList(index, LENGTH_OF_LOTTO_NUMBERS);
+        if (restOfListValues.contains(eachNumber)) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/lotto/RandomUtilTest.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lotto.util.RandomUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+class RandomUtilTest {
+
+    @Test
+    @DisplayName("로또번호 개수가 금액에 부합하는지 테스트")
+    void countOfLottoNumbers(){
+        String money = "5000";
+
+        List<List<Integer>> lottoNumbers = RandomUtil.getLottoNumbers(money);
+
+        assertThat(5).isEqualTo(lottoNumbers.size());
+    }
+
+    @Test
+    @DisplayName("로또 번호 하나가 6자리 숫자를 포함하는 지 테스트")
+    void sizeOfLottoNumbers(){
+        String money = "1000";
+
+        List<List<Integer>> lottoNumbers = RandomUtil.getLottoNumbers(money);
+
+        assertThat(6).isEqualTo(lottoNumbers.get(0).size());
+    }
+}
