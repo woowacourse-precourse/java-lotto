ade33edbc6146d28d39e62ede72075ba68f99f63
ywj9811 <ywj9811@naver.com>
2022-11-12 02:56:14 +0900

    feat: Lotto 클래스
    변수 제거하며 메소드로 변환

--- src/main/java/lotto/utils/Lotto.java
+++ src/main/java/lotto/utils/Lotto.java
@@ -7,17 +7,21 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
 
-    LottoNumberException lottoNumberException = new LottoNumberException();
-
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
+        exception(numbers);
+    }
+    // TODO: 추가 기능 구현
+
+    private void exception(List<Integer> numbers) {
+        LottoNumberException lottoNumberException = new LottoNumberException();
+
         lottoNumberException.lottoNumberSize(numbers);
         lottoNumberException.lottoNumberDuplicated(numbers);
         lottoNumberException.lottoNumberMaxMin(numbers);
     }
-    // TODO: 추가 기능 구현
 }
