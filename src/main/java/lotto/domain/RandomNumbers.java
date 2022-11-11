9b79959f7fac54baca74e4320d73d46e0673c301
parkjungyoon <tbr06057@naver.com>
2022-11-11 16:48:23 +0900

    refactor(Domain): 랜덤 숫자 리스트 정렬 코드 추가

--- src/main/java/lotto/domain/RandomNumbers.java
+++ src/main/java/lotto/domain/RandomNumbers.java
@@ -2,11 +2,13 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Collections;
 import java.util.List;
 
 public class RandomNumbers {
     public List<Integer> generateRandomNumbers() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(lottoNumbers);
         return lottoNumbers;
     }
 }
