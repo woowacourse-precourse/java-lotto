0aeb31338e753eec38e4c7a2b5370674e694c0c9
YuHayeon <youha0930@gmail.com>
2022-11-13 01:34:23 +0900

    feat(Ranking): 당첨 기준, 상금, 보너스 번호 여부를 저장하는 enum
    당첨 기준에 필요한 값을 enum으로 관리

---
+++ src/main/java/lotto/domain/Ranking.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+public enum Ranking {
+
+    FIFTH(3, 5_000, false),
+    FOURTH(4, 50_000, false),
+    THIRD(5, 1_500_000, false),
+    SECOND(5, 30_000_000, true),
+    FIRST(6, 2_000_000_000, false),
+    NOTHING(0, 0, false);
+
+    private final int rightCount;
+    private final int prize;
+    private final boolean hasBonusNumber;
+
+    Ranking(int rightCount, int prize, boolean hasBonusNumber) {
+        this.rightCount = rightCount;
+        this.prize = prize;
+        this.hasBonusNumber = hasBonusNumber;
+    }
+}
