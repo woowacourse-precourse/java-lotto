d95c26813c273aa4fc8677584e84c6c1711ca793
dohyun <ldh19738@gmail.com>
2022-11-12 23:54:09 +0900

    feat(Grade): 불필요한 메서드 삭제하고 enum요소로 등수 추가

--- src/main/java/lotto/Grade.java
+++ src/main/java/lotto/Grade.java
@@ -3,18 +3,20 @@ package lotto;
 import java.util.Arrays;
 
 public enum Grade {
-    FIVE(3, 5_000),
-    FOUR(4, 50_000),
-    THREE(5, 1_500_000),
-    SECOND(5, 30_000_000),
-    FIRST(6, 2_000_000_000);
+    FIVE(3, 5_000, 5),
+    FOUR(4, 50_000, 4),
+    THREE(5, 1_500_000, 3),
+    SECOND(5, 30_000_000, 2),
+    FIRST(6, 2_000_000_000, 1);
 
     private final int matchCount;
     private final int prizeMoney;
+    private final int ranking;
 
-    Grade(int matchCount, int prizeMoney) {
+    Grade(int matchCount, int prizeMoney, int ranking) {
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
+        this.ranking = ranking;
     }
 
     public static Grade findingRank(int matchCount, int bonusCount) {
@@ -28,11 +30,7 @@ public enum Grade {
         return Arrays.stream(values()).filter(grade -> grade.matchCount == matchCount)
                 .findAny().orElseThrow(() -> new IllegalArgumentException("[ERROR] 미당첨입니다."));
     }
-
-    public int getMatchCount() {
-        return matchCount;
-    }
-
+    
     public int getPrizeMoney() {
         return prizeMoney;
     }
