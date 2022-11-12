68ce2382c2b4253718fd459c67fdd487215bf114
iamjooon2 <iamjooon2@gmail.com>
2022-11-13 04:01:35 +0900

    feat: 사용자가 구매한 복권 정보를 출력하는 기능

--- src/main/java/lotto/domain/Shop.java
+++ src/main/java/lotto/domain/Shop.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import lotto.view.OutputView;
 
 public class Shop {
     private static final int PRICE = 1_000;
@@ -10,6 +11,7 @@ public class Shop {
 
     public Shop(Money money) {
         int count = money.getMoney() / PRICE;
+        OutputView.printLotteryCount(count);
     }
 
     public List<Lotto> buyLotto() {
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,8 +4,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputView {
+    private static final String UNIT = "개";
+    private static final String MESSAGE_BUY_SUFFIX = "를 구매했습니다.";
 
-    public static void printString(String input){
+    public static void printLotteryCount(Integer count) {
+        System.out.println(count.toString() + UNIT + MESSAGE_BUY_SUFFIX);
+    }
+
+    public static void printString(String input) {
         System.out.println(input);
     }
 
