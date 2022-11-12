5365fce1e1a912c37efefa44bb2ad6801ef8f3f2
woo-chang <clllickme@naver.com>
2022-11-12 23:22:24 +0900

    test(Consumer): 가지고 있는 로또의 결과를 확인하는 기능 테스트

--- src/test/java/lotto/entity/ConsumerTest.java
+++ src/test/java/lotto/entity/ConsumerTest.java
@@ -1,22 +1,40 @@
 package lotto.entity;
 
-import static lotto.LottoApplication.PRICE;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ConsumerTest {
 
-    @DisplayName("구매 금액이 정해진 금액으로 나누어떨어지지 않으면 예외가 발생한다.")
+    @DisplayName("로또 결과를 확인한다.")
     @Test
-    void invalidPurchaseAmount() {
+    void confirmResult() {
         //given
-        int purchaseAmount = PRICE / 2;
+        List<Lotto> lottos = new ArrayList<>(List.of(
+            new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+            new Lotto(List.of(2, 3, 4, 5, 6, 7)),
+            new Lotto(List.of(3, 4, 5, 6, 7, 8)),
+            new Lotto(List.of(4, 5, 6, 7, 8, 9))
+        ));
+        Consumer consumer = new Consumer(lottos);
+        Lotto winningNumberLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonus = 7;
+
+        //when
+        Map<Rank, Integer> result = consumer.confirmResult(
+            new WinningLotto(winningNumberLotto, bonus));
 
         //then
-        assertThatThrownBy(() -> new Consumer(purchaseAmount)).isInstanceOf(
-            IllegalArgumentException.class);
+        assertThat(result.get(Rank.FIRST)).isEqualTo(1);
+        assertThat(result.get(Rank.SECOND)).isEqualTo(1);
+        assertThat(result.get(Rank.THIRD)).isEqualTo(0);
+        assertThat(result.get(Rank.FOURTH)).isEqualTo(1);
+        assertThat(result.get(Rank.FIFTH)).isEqualTo(1);
+        assertThat(result.get(Rank.NONE)).isEqualTo(0);
     }
 
 }
