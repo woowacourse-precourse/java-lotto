2a748f6d6e572aefbdb3a92086a9b6de942e80a9
kanghyki <kanghyki@gmail.com>
2022-11-13 03:26:39 +0900

    feat(LottoGame): 로또 구매 메서드 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGame {
     public enum eLottoPlace {
         FIRST_PLACE,
@@ -8,6 +12,24 @@ public class LottoGame {
         FOURTH_PLACE,
         FIFTH_PLACE,
         NOTHING
+    }
+    private static final int _lotto_price = 1000;
+    private static final String _buy_msg = "개를 구매했습니다.\n";
+    private static final String _error_default_msg = "[ERROR]";
+
+    public static List<Lotto> buyLotto(int money) {
+        List<Lotto> lottoes = new ArrayList<>();
+        int lottoCount = money / _lotto_price;
+
+        if (money % _lotto_price != 0) {
+            throw new IllegalArgumentException(_error_default_msg + " 로또는 천원 단위로 구매할 수 있습니다.");
+        }
+
+        for (int i = 0; i < lottoCount; ++i) {
+            lottoes.add(new Lotto(makeUniqueLottoNumber()));
+        }
+        System.out.print(lottoCount + _buy_msg);
+        return lottoes;
     }
 
     public static eLottoPlace matchLotto(Lotto l1, Lotto l2, int bonus) {
@@ -33,5 +55,9 @@ public class LottoGame {
         if (matches == 3)
             return eLottoPlace.FIFTH_PLACE;
         return eLottoPlace.NOTHING;
+    }
+
+    private static List<Integer> makeUniqueLottoNumber() {
+        return (Randoms.pickUniqueNumbersInRange(1, 45, 6));
     }
 }
