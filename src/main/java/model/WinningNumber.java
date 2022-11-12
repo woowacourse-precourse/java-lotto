0c4e78f135b59f36b17e8f7eeeb297a65ea75145
parkmuhyeun <pjhg410@gmail.com>
2022-11-12 16:05:22 +0900

    feat: 당첨번호 클래스 생성

---
+++ src/main/java/model/WinningNumber.java
@@ -0,0 +1,12 @@
+package model;
+
+public class WinningNumber {
+    private final Lotto luckyNumber;
+    private final int bonusNumber;
+
+    public WinningNumber(Lotto luckyNumber, int bonusNumber) {
+        this.luckyNumber = luckyNumber;
+        this.bonusNumber = bonusNumber;
+    }
+
+}
