824e24a714a375e69b27ad8f72c7ce3a4de1effb
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-12 09:16:46 +0900

    feat: ChangeLottoPriceService 당첨여부에 따른 금액 리턴해주는 기능 구현

--- src/main/java/lotto/service/ChangeLottoPriceService.java
+++ src/main/java/lotto/service/ChangeLottoPriceService.java
@@ -4,10 +4,22 @@ import lotto.Ranking;
 
 public class ChangeLottoPriceService {
 
+    private static final long PRICE_FOR_THREE_MATCH = 5000L;
+    private static final long PRICE_FOR_FOUR_MATCH = 50000L;
+    private static final long PRICE_FOR_FIVE_MATCH = 1500000L;
+    private static final long PRICE_FOR_FIVE_MATCH_WITH_BONUS = 30000000L;
+    private static final long PRICE_FOR_SIX_MATCH = 2000000000L;
+    private static final long NOTHING = 0L;
+
     public ChangeLottoPriceService(){}
 
     public long changeRankingToPrice(Ranking ranking){
-        return 1L;
+        if(isThreeMatch(ranking)) return PRICE_FOR_THREE_MATCH;
+        if(isFourMatch(ranking)) return PRICE_FOR_FOUR_MATCH;
+        if(isFiveMatch(ranking)) return PRICE_FOR_FIVE_MATCH;
+        if(isFiveMatchWithBonusNumber(ranking)) return PRICE_FOR_FIVE_MATCH_WITH_BONUS;
+        if(isSixMatch(ranking)) return PRICE_FOR_SIX_MATCH;
+        return NOTHING;
     }
 
     private boolean isThreeMatch(Ranking ranking){
