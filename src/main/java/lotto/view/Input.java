36db1e9205dcbf08770d40700f3dbf19bfed21eb
Isu Shin <isu010614@naver.com>
2022-11-12 23:07:44 +0900

    feat: 서로 다른 임의의 당첨번호 6개를 입력받는 기능 추가
    예외처리
    - 숫자가 아닌 문자기 입력된 경우 예외 발생

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -2,21 +2,45 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class Input {
-    private static final String ENTER_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    public static int enterAmount(){
-        System.out.println(ENTER_PURCHASE_AMOUNT_MESSAGE);
+import static java.util.stream.Collectors.toList;
+
+public class Input {
+    private static final int LOTTO_SIZE = 6;
+    private static final int BONUS_NUMBER = 1;
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 45;
+    private static final String ENTER_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String ENTER_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+
+    public static int getAmount() {
         try {
-            return getNumber();
-        }catch(Exception e){
+            String amount = Console.readLine();
+            return Integer.parseInt(amount);
+        } catch (Exception e) {
             throw new IllegalArgumentException("[ERROR] Only enter numbers.");
         }
     }
 
-    private static int getNumber(){
-        String number = Console.readLine();
-        return Integer.parseInt(number);
+    public static List<Integer> getWinningNumber() {
+        System.out.println(ENTER_WINNING_NUMBER_MESSAGE);
+        try {
+            List<Integer> winningNumber = stringToList(Console.readLine());
+
+            return winningNumber;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] Only enter numbers.");
+        }
     }
+
+    private static List<Integer> stringToList(String numbers) {
+        return Arrays.stream(numbers.split(", "))
+                .map(Integer::parseInt)
+                .collect(toList());
+    }
+
 
 }
