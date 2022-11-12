e65510ba75527107c72533319fa5a1fb7c6ea998
oyoungsun <plusmate@naver.com>
2022-11-12 23:37:25 +0900

    feat : countMatchNumber in LottoCalculator

--- src/main/java/lotto/LottoCalculator.java
+++ src/main/java/lotto/LottoCalculator.java
@@ -1,2 +1,19 @@
-package lotto;public class LottoCalculator {
+package lotto;
+
+import java.util.List;
+
+public class LottoCalculator {
+
+    public int countMatchNumber(Lotto lotto, Lotto winningLotto) {
+        int matchCount = 0;
+        List<Integer> lottoNum = lotto.getNumbers();
+        List<Integer> winningNum = winningLotto.getNumbers();
+        for (int i = 0; i < 6; i++) {
+            if (lottoNum.get(i) == winningNum.get(i)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
+
 }
