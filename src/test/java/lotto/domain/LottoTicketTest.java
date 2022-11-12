917367b32cad96b4f0e0c7452a1951c3e8a76dde
yeonhee <ydol2@naver.com>
2022-11-13 02:59:37 +0900

    test(LottoTicketTest) : 랜덤 로또 정렬 test

--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -3,8 +3,11 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.Collectors;
+
 import static lotto.common.exception.Validator.validateDifferentLottoNumbers;
 import static lotto.common.exception.Validator.validateRangeLottoNumbers;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoTicketTest {
@@ -27,5 +30,12 @@ class LottoTicketTest {
         assertDoesNotThrow(()->{
             validateRangeLottoNumbers(lotto.get());
         });
+    }
+
+    @DisplayName("로또 번호 랜덤 생성 테스트 - 정렬")
+    @Test
+    void generateSortTest() {
+        Lotto lotto = lottoTicket.generate();
+        assertThat(lotto.get()).isEqualTo(lotto.get().stream().sorted().collect(Collectors.toList()));
     }
 }
