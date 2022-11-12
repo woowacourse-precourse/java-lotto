be5af6b8d428a7dd426aef94eafd43a978d147c6
comet7001 <comet7001@gmail.com>
2022-11-12 22:56:05 +0900

    feat(output): implement tellNumberOfLottosUserBought(int theNumberOfLottos){}

--- src/main/java/utils/ConsoleUserOutput.java
+++ src/main/java/utils/ConsoleUserOutput.java
@@ -12,5 +12,9 @@ public class ConsoleUserOutput {
         System.out.println(NormalStatements.ASK_MONEY_TO_BUY_LOTTO.done());
     }
 
+    public static void tellNumberOfLottosUserBought(int theNumberOfLottos){
+        System.out.println(theNumberOfLottos+NormalStatements.LET_USER_KNOW_THE_NUMBER_OF_TICKETS.done());
+    }
+
 
 }
