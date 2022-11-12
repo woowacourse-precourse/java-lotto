45ca0b3f1ff5f4b91f8117c8af39e01401de2667
Donguk <95donguk.dev@gmail.com>
2022-11-13 02:19:44 +0900

    style(ExceptionMessage): 1000원 단위가 아닌 가격 메시지 부분 오타 수정

--- src/main/java/lotto/view/message/ExceptionMessage.java
+++ src/main/java/lotto/view/message/ExceptionMessage.java
@@ -2,7 +2,7 @@ package lotto.view.message;
 
 public enum ExceptionMessage {
     NON_NUMERIC_VALUE("숫자가 아닌 값을 입력하셨습니다."),
-    NON_UNITS_OF_ONE_THOUSAND_WON("1000원 단위가 아닌 가격입니다..");
+    NON_UNITS_OF_ONE_THOUSAND_WON("1000원 단위가 아닌 가격입니다.");
 
     private static final String ERROR_MESSAGE = "[ERROR]";
 
