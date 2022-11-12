e7146dfe64355f26848be3b1dd517cd9de8e3537
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-11-12 22:36:26 +0900

    Feat : 3.-2) 기능 추가
    입력 번호가 중복이 있으면 예외처리 후 종료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -13,78 +13,82 @@ public class Application {
     private static final int STOP_VALUE = -1;
     private static final int AMOUNT_UNIT = 1000;
     private static final int LOTTERY_NUMBER_LIMIT = 6;
+
     public static void main(String[] args) {
         startLottoGame();
     }
 
-    private static void startLottoGame(){
+    private static void startLottoGame() {
         System.out.println("구입금액을 입력해 주세요.");
         int buyingAmount = inputBuyingAmount();
-        if(buyingAmount != STOP_VALUE){
+        if (buyingAmount != STOP_VALUE) {
             final int countOfLottery = buyingAmount / AMOUNT_UNIT;
             System.out.println("\n" + countOfLottery + "개를 구매했습니다.");
             checkWinning(countOfLottery);
         }
     }
-    private static int inputBuyingAmount(){
+
+    private static int inputBuyingAmount() {
         int buyingAmount = checkIfInteger(Console.readLine());
-        if(buyingAmount == STOP_VALUE) return STOP_VALUE;
+        if (buyingAmount == STOP_VALUE) return STOP_VALUE;
         return checkIf1000Unit(buyingAmount);
     }
 
-    private static int checkIfInteger(String inputAmountString){
+    private static int checkIfInteger(String inputAmountString) {
         int buyingAmount = 0;
-        try{
+        try {
             buyingAmount = Integer.parseInt(inputAmountString);
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             System.out.println("[ERROR] 구입 금액은 항상 정수로만 입력해야 합니다.");
             return STOP_VALUE;
         }
         return buyingAmount;
     }
 
-    private static int checkIf1000Unit(int buyingAmount){
-        try{
+    private static int checkIf1000Unit(int buyingAmount) {
+        try {
             if (buyingAmount % AMOUNT_UNIT != 0) throw new IllegalArgumentException();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println("[ERROR] 구입 금액은 항상 1000워 단위로 입력하셔야 합니다.");
             return STOP_VALUE;
         }
         return buyingAmount;
     }
 
-    private static void checkWinning(int countOfLottery){
+    private static void checkWinning(int countOfLottery) {
         ArrayList<int[]> lotteryBundleArray = new ArrayList<>();
         int cntForCreateLottery = 0;
         while (cntForCreateLottery < countOfLottery) {
             lotteryBundleArray.add(createAndPrintLottery());
             cntForCreateLottery++;
         }
-        Lotto lotto = new Lotto(inputWinningNumbers());
+        checkLottoWinnings(inputWinningNumbers(),lotteryBundleArray);
+
     }
-    private static int[] createAndPrintLottery(){
-        List<Integer> tempLotteryArray = Randoms.pickUniqueNumbersInRange(1,45,LOTTERY_NUMBER_LIMIT);
+
+    private static int[] createAndPrintLottery() {
+        List<Integer> tempLotteryArray = Randoms.pickUniqueNumbersInRange(1, 45, LOTTERY_NUMBER_LIMIT);
         int[] lotteryNumberArray = new int[LOTTERY_NUMBER_LIMIT];
         Collections.sort(tempLotteryArray);
         System.out.println(tempLotteryArray);
         Iterator lotteryNumberItr = tempLotteryArray.iterator();
         int index = 0;
-        while (lotteryNumberItr.hasNext()){
-            lotteryNumberArray[index++] = (int)lotteryNumberItr.next();
+        while (lotteryNumberItr.hasNext()) {
+            lotteryNumberArray[index++] = (int) lotteryNumberItr.next();
         }
         return lotteryNumberArray;
     }
 
-    private static List<Integer> inputWinningNumbers(){
+    private static List<Integer> inputWinningNumbers() {
         System.out.println("\n당첨 번호를 입력해 주세요.");
         String tempWinningNumbersString = Console.readLine();
         ArrayList<Integer> winningNumbersArray = new ArrayList<>();
-        for (String winningNumber : tempWinningNumbersString.split(",")){
+        for (String winningNumber : tempWinningNumbersString.split(",")) {
             winningNumbersArray.add(Integer.parseInt(winningNumber));
         }
         return winningNumbersArray;
     }
-
+    private static void checkLottoWinnings(List<Integer> winningNumbers , ArrayList<int[]> lotteryBundleArray){
+        Lotto lotto = new Lotto(winningNumbers);
+    }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,35 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-
 public class Lotto {
+    private final int LOTTERY_NUMBER_LIMIT = 6;
     private final List<Integer> numbers;
-
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateOverLap(numbers);
         this.numbers = numbers;
     }
-
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTERY_NUMBER_LIMIT) {
+            System.out.println("[ERROR] 당첨 번호는 정확히 6개를 입력하셔야 합니다.");
             throw new IllegalArgumentException();
         }
     }
+    private void validateOverLap(List<Integer> numbers) {
+        Iterator itrForNumbers = numbers.iterator();
+        int[] arrayForCheckOverLap = new int[46];
+        while (itrForNumbers.hasNext()){
+            int checkIndex = (int)itrForNumbers.next();
+            if (arrayForCheckOverLap[checkIndex] == 1){
+                System.out.println("[ERROR] 당첨 번호는 중복이 없어야 합니다.");
+                throw new IllegalArgumentException();
+            }
+            arrayForCheckOverLap[checkIndex] = 1;
+        }
+    }
 
-    // TODO: 추가 기능 구현
+
+
 }
