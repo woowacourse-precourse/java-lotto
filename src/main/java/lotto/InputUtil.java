15efe6d49cd26629d0e78f7734949d6af78b40af
jingyu9804 <swc05161@gmail.com>
2022-11-12 15:30:34 +0900

    feat(lotto) : 당첨 번호 입력 기능

--- src/main/java/lotto/InputUtil.java
+++ src/main/java/lotto/InputUtil.java
@@ -2,9 +2,30 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputUtil {
     public int inputPurchasePrice(){
         String input = Console.readLine();
         return Integer.valueOf(input);
+    }
+
+    public List<Integer> inputWinningNumber(){
+        String input = Console.readLine();
+        return stringArrToIntList(commaSeparation(input));
+    }
+
+    private String[] commaSeparation(String str){
+        String[] arr = str.split(",");
+        return arr;
+    }
+
+    private List<Integer> stringArrToIntList(String[] input){
+        List<Integer> list = new ArrayList<>();
+        for(String s:input){
+            list.add(Integer.valueOf(s));
+        }
+        return list;
     }
 }
