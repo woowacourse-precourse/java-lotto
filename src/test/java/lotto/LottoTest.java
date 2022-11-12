5e9728a1a4ebf6a17e712f6b55b81d7f42ac9700
syr9707 <yrshin9771@gmail.com>
2022-11-12 12:56:42 +0900

    test(inputTest): 입력 금액 예외 테스트
    1000의 배수가 아닐 시

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,8 +3,10 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
