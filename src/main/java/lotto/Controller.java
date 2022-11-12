1d8fef4f45b1080a70a66e5be521ae57c8a4f695
yoon <beatus96@naver.com>
2022-11-12 23:37:53 +0900

    feat : 등수별 당첨된 로또개수 맵 만들기 기능 추가

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -2,13 +2,17 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Controller {
     private final int MONEY_UNIT = 1000;
+
+    public void generate() {
+        //1. 금액에 해당하는 로또 생성
+        //2. 당첨번호, 보너스 번호 입력
+        //3. 당첨된 로또 개수 맵 만들기 {등수:개수}
+        //4.
+    }
 
     public Set<Lotto> createLottos(int money) {
         int count = money/MONEY_UNIT;
@@ -22,28 +26,23 @@ public class Controller {
         return lottoSet;
     }
 
-    // 로또 클래스에 넣기 :
-    public int calculateMatchLotto(List<Integer> winningLotto, List<Integer> myLotto, int bonusNumber) {
-        int matchCount = 0;
-        for(Integer lottoNumber :myLotto){
-            if(winningLotto.contains(myLotto)){
-                matchCount++;
-            }
+
+    // 당첨된 로또 개수 맵 만들기 {등수:개수}
+    public void createResultMap(Set<Lotto> lottoSet, List<Integer> winningLotto, int bonusNumber) {
+        Map<String, Integer> resultMap = new HashMap<>();
+
+        for(Lotto eachLotto : lottoSet){
+            eachLotto.setRank(winningLotto, bonusNumber);
+            String rank = eachLotto.getRank();
         }
-        return matchCount;
+
     }
 
-    //당첨 로또 저장공간 만들기
-    public void addToStatistics(Map<Integer, Integer> statistics, int matchCount) {
+    //당첨 기준별 로또 개수 세기 기능.
+    public void addToResultMap(Map<Integer, Integer> statistics, int matchCount) {
         //putIfAbsent나 다른 것 사용해보기
         int winningCount = statistics.getOrDefault(matchCount, 0);
         statistics.put(matchCount, winningCount);
     }
-
-
-
-//        if(matchCount == 5){
-//        myLotto.contains(bonusNumber)
-//
 
 }
