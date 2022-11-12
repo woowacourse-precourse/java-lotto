2ce1ad04c792d334cfcbdbcc8c9a995567fde82a
iamjooon2 <iamjooon2@gmail.com>
2022-11-13 04:20:38 +0900

    feat: 중복된 숫자가 있는지 확인하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,10 +1,13 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private static final int LOTTO_NUMBER_COUNT = 6;
     private static final String ERROR_NUMBERS_COUNT = "[ERROR] 로또 숫자는 6개여야 합니다.";
+    private static final String ERROR_NUMBER_DUPLICATION = "[ERROR] 중복된 번호가 존재합니다.";
 
     private final List<Integer> numbers;
 
@@ -16,6 +19,10 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException(ERROR_NUMBERS_COUNT);
+        }
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != numbers.size()) {
+            throw new IllegalArgumentException(ERROR_NUMBER_DUPLICATION);
         }
     }
 
