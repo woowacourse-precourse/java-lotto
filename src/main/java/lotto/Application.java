0e08569d381e9ed446d12d6e6c22a1761c204a7a
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-11-12 13:29:52 +0900

    refactor: 예외처리 테스트 통과를 위한 try catch구문 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,11 @@ import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
-        lottoController.run();
+        try {
+            LottoController lottoController = new LottoController();
+            lottoController.run();
+        } catch (IllegalArgumentException exception) {
+            System.out.println("[ERROR]" + exception.getMessage());
+        }
     }
 }
