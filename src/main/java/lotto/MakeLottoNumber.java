6920f72ead595e3a37d96d9ecf126d38ade1b092
JuneYub <ajsjdlwj0123@gmail.com>
2022-11-12 19:04:36 +0900

    <feat> 로또번호 생성 기능 구현

---
+++ src/main/java/lotto/MakeLottoNumber.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MakeLottoNumber {
+    private final int maxNumber = 6;
+    private final int rangeStartValue = 1;
+    private final int rangeEndValue = 45;
+    List<Integer> lottoNumber = new ArrayList<>();
+
+    public List<Integer> returnLottoNumber() {
+        int initNum = Randoms.pickNumberInRange(rangeStartValue, rangeEndValue);
+        lottoNumber.add(initNum);
+        for(int i = 1; i<maxNumber; i++){
+            lottoNumber = checkAndPutNumber(lottoNumber);
+        }
+        Collections.sort(lottoNumber);
+        return lottoNumber;
+    }
+
+    public List<Integer> checkAndPutNumber(List<Integer> lotto) {
+        int tmp = Randoms.pickNumberInRange(rangeStartValue, rangeEndValue);
+
+        for(int i = 0; i<lotto.size(); i++) {
+            if(tmp == lotto.get(i)){
+                tmp = Randoms.pickNumberInRange(rangeStartValue, rangeEndValue);
+                i = -1;
+            }
+        }
+
+        lotto.add(tmp);
+        return lotto;
+    }
+
+}
