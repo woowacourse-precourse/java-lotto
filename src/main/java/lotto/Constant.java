14859562a6bc3c5fe0b4b17749cd4c25dbce7634
Hayoon <hy97@sju.ac.kr>
2022-11-12 16:09:48 +0900

    style(Constant): 문자열 수정

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -11,7 +11,7 @@ public class Constant {
     public static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
     public static final String INPUT_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    public static final String WINNING_STATISTICS = "당첨통계" + "\n" + "---";
+    public static final String WINNING_STATISTICS = "당첨 통계" + "\n" + "---";
 
     public static final String THREE_MATCHING = "3개 일치 (5,000원)";
     public static final String FOUR_MATCHING = "4개 일치 (50,000원)";
