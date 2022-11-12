ae459b3b9b4ea8194d4b5f9e81b3a0a175981070
Donguk <95donguk.dev@gmail.com>
2022-11-13 01:54:05 +0900

    refactor(InputViewTest): 숫자가 아닌 값 입력 테스트 수정 및 테스트 케이스 추가

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -2,16 +2,24 @@ package lotto.view;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class InputViewTest {
-    @Test
-    @DisplayName("숫자가 아닌 값 입력한 경우 예외가 발생한다.")
-    void inputValueByNonNumericValue() {
-        InputView inputView = new InputView();
+    private InputView inputView;
 
-        assertThatThrownBy(() -> inputView.validateValue("가나다"))
+    @BeforeEach
+    void setUp() {
+        inputView = new InputView();
+    }
+
+    @DisplayName("숫자가 아닌 값 입력한 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @CsvSource({"가나다", "1000j", "-1000"})
+    void inputValueByNonNumericValue(String inputValue) {
+        assertThatThrownBy(() -> inputView.validateValue(inputValue))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
