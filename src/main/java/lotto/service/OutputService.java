b60f7f8ff82c00ef91e74a815116aad8d570dc90
woo-chang <clllickme@naver.com>
2022-11-12 22:13:51 +0900

    chore(OutputService): 당첨 통계에 대한 상수명 수정

--- src/main/java/lotto/service/OutputService.java
+++ src/main/java/lotto/service/OutputService.java
@@ -7,7 +7,7 @@ import lotto.entity.Result;
 public class OutputService {
 
     private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
-    private static final String WIN_STATUS_MESSAGE = "당첨 통계";
+    private static final String RESULT_STATUS_MESSAGE = "당첨 통계";
     private static final String LINE = "---";
     private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다.\n";
     private static final String ERROR_PREFIX = "[ERROR] ";
@@ -21,7 +21,7 @@ public class OutputService {
     }
 
     public static void printResult(Result result) {
-        System.out.println(WIN_STATUS_MESSAGE);
+        System.out.println(RESULT_STATUS_MESSAGE);
         System.out.println(LINE);
         System.out.println(result);
     }
