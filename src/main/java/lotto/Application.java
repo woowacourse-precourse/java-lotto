10da0996395774ff9d8a913e44a4a54271da6473
bfly_notebook <kkyxw@soongsil.ac.kr>
2022-11-12 23:32:28 +0900

    feat: implement the function to receive the input about winning numbers and validate that the input obeys the rule

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -16,12 +16,20 @@ public class Application {
         List<Lotto> lotteries = QuickPickGenerator.quickPick(purchaseCount);
         printLotteries(lotteries);
 
+        System.out.println("당첨 번호를 입력해 주세요.");
+        Lotto winningNumbers = new Lotto(getWinningNumbers());
+
+        System.out.println();
+
+//        System.out.println("보너스 번호를 입력해 주세요.");
+//        int bonusNumber = getBonusNumber();
+
+//        printWinningStatistics(lotteries, winningNumbers);
     }
 
     public static int getPurchasePrice() {
         String input = Console.readLine();
         validateIsNumber(input);
-
 
         int purchasePrice = Integer.parseInt(input);
         validateIsMultipleOfThousand(purchasePrice);
@@ -37,18 +45,51 @@ public class Application {
         System.out.println();
     }
 
+    public static List<Integer> getWinningNumbers() {
+        String input = Console.readLine();
+        String[] splitInput = input.split(",");
+        List<Integer> winningNumbers = new ArrayList<>();
+
+        for (String str : splitInput) {
+            validateIsNumber(str);
+            int number = Integer.parseInt(str);
+
+            validateIsInLottoRange(number);
+            winningNumbers.add(number);
+        }
+
+        validateHasNoDuplication(winningNumbers);
+
+        return winningNumbers;
+    }
+
     private static void validateIsNumber(String str) {
         try {
             Integer.parseInt(str);
         }
         catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 입력받은 값이 숫자가 아닙니다.");
+            throw new IllegalArgumentException("[ERROR] 입력한 값이 숫자가 아닙니다.");
         }
     }
 
     private static void validateIsMultipleOfThousand(int num) {
         if (num % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 입력받은 금액이 1000원으로 나누어 떨어지지 않습니다.");
+            throw new IllegalArgumentException("[ERROR] 입력한 금액이 1,000원으로 나누어 떨어지지 않습니다.");
+        }
+    }
+
+    private static void validateIsInLottoRange(int num) {
+        if (num < 1 || num > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private static void validateHasNoDuplication(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            int num = numbers.get(i);
+            if (numbers.lastIndexOf(num) != i) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않는 수로 이루어져야 합니다.");
+            }
         }
     }
 }
