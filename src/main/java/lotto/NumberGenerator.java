328c2f909ddeea65bfa95aa1f6dc206c016d89ad
Hayoon <hy97@sju.ac.kr>
2022-11-12 15:24:07 +0900

    feat(NumberGenerator): 로또 번호 오름차순 기능 구현

--- src/main/java/lotto/NumberGenerator.java
+++ src/main/java/lotto/NumberGenerator.java
@@ -3,15 +3,13 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class NumberGenerator {
-
-    List<List<Integer>> userLottos;
+    List<List<Integer>> autoLotto;
     List<Integer> computerLotto;
+    int bonusNumber;
 
     public NumberGenerator() {
         computerLotto = new ArrayList<>();
@@ -22,16 +20,17 @@ public class NumberGenerator {
     }
 
     public int enterPurchasePrice() {
-        String purchasePrice = Console.readLine();
-        return Integer.parseInt(purchasePrice);
+        int purchasePrice = exceptionInvalidPurchasePrice();
+        exceptionDividedIntoThousands(purchasePrice);
+        return purchasePrice;
     }
 
     public void enterLottoNumbers() {
         exceptionInvalidLottoNumber();
     }
 
-    public int enterBonusNumber() {
-        return exceptionInvalidBonusNumber();
+    public void enterBonusNumber() {
+        bonusNumber = exceptionInvalidBonusNumber();
     }
 
     private List<Integer> addComputerNumber(List<String> computerLottoString) {
@@ -42,22 +41,36 @@ public class NumberGenerator {
         return computerLottoString.stream().map(Integer::parseInt).collect(Collectors.toList());
     }
 
-    public void operationLotto(int purchaseQuantity) {
-        userLottos = new ArrayList<>(purchaseQuantity);
-        createLottoNumbers(purchaseQuantity);
-        printLottoNumbers();
-    }
-
-    private void createLottoNumbers(int purchaseQuantity) {
+    public void createLottos(int purchaseQuantity) {
+        autoLotto = new ArrayList<>(purchaseQuantity);
         for (int i = 0; i < purchaseQuantity; i++) {
             List<Integer> integers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            userLottos.add(integers);
+            autoLotto.add(integers);
         }
     }
 
-    private void printLottoNumbers() {
-        for (List<Integer> lotto : userLottos) {
-            System.out.println(lotto);
+    public void printLottos(List<List<Integer>> autoLotto) {
+        for (List<Integer> lotto : autoLotto) {
+            Collections.sort(lotto);
+            System.out.println(Arrays.toString(lotto.toArray()));
+        }
+    }
+
+    private int exceptionInvalidPurchasePrice() {
+        String purchasePrice = Console.readLine();
+        try {
+            return Integer.parseInt(purchasePrice);
+        } catch (NumberFormatException exception) {
+            NoSuchElementException e = new NoSuchElementException();
+            System.out.println(Constant.ERROR_MESSAGE + "구매금액은 숫자여야합니다.");
+            throw e;
+        }
+    }
+    private void exceptionDividedIntoThousands(int purchasePrice) {
+        if (purchasePrice % 1000 != 0) {
+            IllegalArgumentException e = new IllegalArgumentException();
+            System.out.println(Constant.ERROR_MESSAGE + "구매금액은 1,000원으로 나누어 떨어져야 합니다.");
+            throw e;
         }
     }
 
@@ -65,43 +78,20 @@ public class NumberGenerator {
         try {
             String[] lottoNumbers = Console.readLine().split(",");
             computerLotto = addComputerNumber(new ArrayList<>(Arrays.asList(lottoNumbers)));
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        } catch (NumberFormatException exception) {
+            IllegalArgumentException e = new IllegalArgumentException();
+            System.out.println(Constant.ERROR_MESSAGE + "로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw e;
         }
     }
 
     private int exceptionInvalidBonusNumber() {
         try {
             return Integer.parseInt(Console.readLine());
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        } catch (NumberFormatException exception) {
+            IllegalArgumentException e = new IllegalArgumentException();
+            System.out.println(Constant.ERROR_MESSAGE + "보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw e;
         }
     }
 }
-
-/**
- * 8개를 구매했습니다.
- * [8, 21, 23, 41, 42, 43]
- * [3, 5, 11, 16, 32, 38]
- * [7, 11, 16, 35, 36, 44]
- * [1, 8, 11, 31, 41, 42]
- * [13, 14, 16, 38, 42, 45]
- * [7, 11, 30, 40, 42, 43]
- * [2, 13, 22, 32, 38, 45]
- * [1, 3, 5, 14, 22, 45]
- *
- * 당첨 번호를 입력해 주세요.
- * 1,2,3,4,5,6
- *
- * 보너스 번호를 입력해 주세요.
- * 7
- *
- * 당첨 통계
- * ---
- * 3개 일치 (5,000원) - 1개
- * 4개 일치 (50,000원) - 0개
- * 5개 일치 (1,500,000원) - 0개
- * 5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
- * 6개 일치 (2,000,000,000원) - 0개
- * 총 수익률은 62.5%입니다.
- */
