b8f7cb7645750fd8b1dfd585ff1a015d4c6d5189
Heebeom Yang <takeny1998@gmail.com>
2022-11-12 12:53:28 +0900

    feat: add testGetSameNumCount()

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -19,7 +20,6 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -27,8 +27,17 @@ class LottoTest {
     @DisplayName("로또 번호는 1부터 45 사이의 숫자여야 한다.")
     @Test
     void createLottoByOutOfRangeNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(-45, -1, 0, 1, 45, 46)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 getSameNumCount() 기능 테스트")
+    @Test
+    void testGetSameNumCount() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto otherLotto = new Lotto(List.of(2, 3, 6, 9, 12, 15));
+        int result = 3;
+
+        assertThat(lotto.getSameNumCount(otherLotto.getNumbers())).isEqualTo(result);
     }
 }
