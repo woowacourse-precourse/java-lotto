6ffa2ebf8d14d9e1343778a0715c131d7cf5d1f0
oyoungsun <plusmate@naver.com>
2022-11-12 23:04:23 +0900

    test : test code for  setBonusNumber

--- src/test/java/lotto/PlayerTest.java
+++ src/test/java/lotto/PlayerTest.java
@@ -20,6 +20,16 @@ public class PlayerTest {
     @DisplayName("로또 구입 입력이 정수가 아니면 에러가 발생한다. ")
     @Test
     void createLottoCashByInteger(){
-
+        String money = "123j";
+        Player player = new Player();
+        assertThatThrownBy(()-> player.putMoneyforLotto(money))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("보너스 정수가 1~45가 아니면 에러가 발생한다. ")
+    @Test
+    void createBonusByRange(){
+        Player player = new Player();
+        assertThatThrownBy(()-> player.setBonusNumber(55))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
