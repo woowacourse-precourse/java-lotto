eecd6b41d6c2231fb5bef5a5a83de709e1283c41
ChanghyunKim <variety82p@gamil.com>
2022-11-12 20:23:38 +0900

    feat(Validator) : 보너스 넘버를 검증하는 메서드 구현

--- src/main/java/lotto/domain/Validator.java
+++ src/main/java/lotto/domain/Validator.java
@@ -5,17 +5,23 @@ import java.util.regex.Pattern;
 import static lotto.utils.Messages.ERROR_MESSAGE;
 
 public class Validator {
-
+    static final int LOTTO_LOWER_BOUND = 1;
+    static final int LOTTO_UPPER_BOUND = 45;
     public void validateInputAmount(String input){
         validateNumeric(input);
         validateGraterThousand(input);
         validateMultipleOfThousands(input);
     }
 
+    public void validateBonusNumber(String input){
+        validateNumeric(input);
+        validateLottoNumberBound(input);
+    }
+
     public void validateNumeric(String input){
         String numericPattern = "^[0-9]*$";
         if(!Pattern.matches(numericPattern, input)){
-            throw new IllegalArgumentException(ERROR_MESSAGE + "숫자형식이 아닙니다");
+            throw new IllegalArgumentException(ERROR_MESSAGE + "숫자 형태가 아닙니다");
         }
     }
     public void validateGraterThousand(String input){
@@ -29,6 +35,13 @@ public class Validator {
         int amount = Integer.parseInt(input);
         if(amount % 1000 != 0){
             throw new IllegalArgumentException(ERROR_MESSAGE + "1,000의 단위가 아닙니다.");
+        }
+    }
+
+    public void validateLottoNumberBound(String input){
+        int bonusNumber = Integer.parseInt(input);
+        if(bonusNumber < LOTTO_LOWER_BOUND || bonusNumber > LOTTO_UPPER_BOUND){
+            throw new IllegalArgumentException(ERROR_MESSAGE + "1-45 사이의 범위가 아닙니다");
         }
     }
 
