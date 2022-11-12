8e2970a06159b17f5af35b77e3a18c308ca654e7
Yunsik-Choi <choiys0311@gmail.com>
2022-11-13 01:53:18 +0900

    ♻️ refactor : Rank 순위 필드 제거

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -5,25 +5,22 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum Rank {
-    FIRST(1, 6, false, 2_000_000_000),
-    SECOND(2, 5, true, 30_000_000),
-    THIRD(3, 5, false, 1_500_000),
-    FOURTH(4, 4, false, 50_000),
-    FIFTH(5, 3, false, 5_000),
-    MISS(0, 0, false, 0);
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    MISS(0, false, 0);
 
-    private final int rank;
     private final int matchCount;
     private final boolean matchBonusNumber;
     private final long reward;
 
-    Rank(int rank, int matchCount, boolean matchBonusNumber, int reward) {
-        this.rank = rank;
+    Rank(int matchCount, boolean matchBonusNumber, int reward) {
         this.matchCount = matchCount;
         this.matchBonusNumber = matchBonusNumber;
         this.reward = reward;
     }
-
 
     public static Rank result(int matchCount, boolean matchBonusNumber) {
         return matchBonusNumberRank(matchBonusNumber, matchCountRanks(matchCount));
