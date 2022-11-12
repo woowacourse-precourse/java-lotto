a1f581d8bf4574f20d31ba197fb9a0f47b17640c
peterysh <52229539+peterysh@users.noreply.github.com>
2022-11-12 21:32:50 +0900

    add: add ControllerOut messages and WinningStatus

--- src/main/java/lotto/WinningStatus.java
+++ src/main/java/lotto/WinningStatus.java
@@ -1,4 +1,15 @@
 package lotto;
 
 public enum WinningStatus {
+    MATCH_THREE("3개 일치 (5,000원)"),
+    MATCH_FOUR("4개 일치 (50,000원)"),
+    MATCH_FIVE("5개 일치 (1,500,000원)"),
+    MATCH_FIVE_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    MATCH_SIX("6개 일치 (2,000,000,000원)");
+
+    private final String message;
+
+    WinningStatus(String message){
+        this.message = message;
+    }
 }
--- src/main/java/lotto/controller/LottoControllerString.java
+++ src/main/java/lotto/controller/LottoControllerString.java
@@ -1,4 +1,28 @@
 package lotto.controller;
 
-public interface LottoControllerString {
+import lotto.WinningStatus;
+
+public class LottoControllerString {
+    public static String INPUT_PURCHASE_PRICE =  "구입금액을 입력해 주세요.";
+    public static String INPUT_LOTTO_NUMBRES = "당첨 번호를 입력해 주세요.";
+    public static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public static String WINNING_STATISTICS = "당첨 통계\n ---";
+    public static String purchaserMessage(int price){
+        return String.format("%d개를 구매했습니다.", price);
+    }
+
+    public static String winningStatistic(int threeMatch, int fourMatch, int fiveMatch,
+                                            int fiveAndBonusMatch, int sixMatch){
+        return String.format(
+                "%s - %d개\n", WinningStatus.MATCH_THREE, threeMatch,
+                "%s - %d개\n", WinningStatus.MATCH_FOUR, fourMatch,
+                "%s - %d개\n", WinningStatus.MATCH_FIVE, fiveMatch,
+                "%s - %d개\n", WinningStatus.MATCH_FIVE_BONUS, fiveAndBonusMatch,
+                "%s - %d개\n", WinningStatus.MATCH_SIX, sixMatch
+        );
+    }
+    public static String winningRateMessage(double winningRate){
+        return String.format("총 수익률은 %.1f% 입니다.",winningRate);
+    }
+
 }
