65672012f6559bffa13aeb30f165ba7f63c5d71e
ChanghyunKim <variety82p@gamil.com>
2022-11-12 16:58:08 +0900

    feat(Messages) : 로또 게임 메시지 생성

---
+++ src/main/java/lotto/utils/Messages.java
@@ -0,0 +1,9 @@
+package lotto.utils;
+
+public class Messages {
+    public static final String LOTTO_BUY_MESSAGE = "구매금액을 입력해주세요." ;
+    public static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+    public static final String LOTTO_RESULT_MESSAGE ="당첨 통계\n___";
+}
+
