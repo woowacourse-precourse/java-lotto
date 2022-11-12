f2c30385c864e281842625c84a94de61bb9fabb1
임원정 <oddish0513@gmail.com>
2022-11-12 20:28:50 +0900

    feat(lotto): 중복되지 않는 숫자 6개를 생성하는 기능 구현

---
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class NumberGenerator {
+
+	List<Integer> createRandomNumbers(){
+		return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+	}
+
+}
