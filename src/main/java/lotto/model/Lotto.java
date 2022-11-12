e6a342f012c3fd37f63927efb8b0455c9d812769
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-13 00:27:24 +0900

    type:feat 로또 번호가 중복되지 않는지 확인하는 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Lotto {
@@ -7,12 +9,23 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        hasSameNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void hasSameNumber(List<Integer> numbers) {
+        List<Integer> tmpNumbers = new ArrayList<>(numbers);
+        tmpNumbers.sort(Comparator.naturalOrder());
+        for (int index = 1; index < tmpNumbers.size(); index++) {
+            if (tmpNumbers.get(index) == tmpNumbers.get(index-1)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않는 6개의 숫자여야 합니다.");
+            }
         }
     }
 
