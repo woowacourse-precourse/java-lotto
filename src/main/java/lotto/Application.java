f651cc169c8ebcdac5bdb11336962d1b018e28b7
김형기 <khk211113@naver.com>
2022-11-13 02:40:43 +0900

    feat: 전체 프로그램 로직 완성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,52 @@
 package lotto;
 
+import config.PrintConstants;
+import ui.UserInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            int payment = UserInterface.readPayment();
+            List<Lotto> boughtLotteries = buyLotteries(payment / 1000);
+            LottoRound lottoRound = makeLottoRound();
+            LottoStatistics statistics = makeStatistics(boughtLotteries, lottoRound);
+            UserInterface.printStatistics(statistics);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<Lotto> buyLotteries(int num) {
+        List<Lotto> lotteries = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            lotteries.add(Lotto.createRandomLotto());
+        }
+        showBuyResult(lotteries);
+        return lotteries;
+    }
+
+    private static void showBuyResult(List<Lotto> lotteries) {
+        System.out.println("\n" + lotteries.size() + PrintConstants.BUY_COMPLETE);
+        for (Lotto lotto : lotteries) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    private static LottoRound makeLottoRound() throws IllegalArgumentException {
+        Lotto luckyNumbers = UserInterface.readLuckyNumbers();
+        int bonusNumber = UserInterface.readBonusNumber();
+        return new LottoRound(luckyNumbers, bonusNumber);
+    }
+
+    private static LottoStatistics makeStatistics(List<Lotto> boughtLotteries, LottoRound lottoRound) throws IllegalArgumentException {
+        LottoStatistics statistics = new LottoStatistics();
+        for (Lotto lotto : boughtLotteries) {
+            statistics.add(lottoRound.getRank(lotto));
+        }
+        return statistics;
     }
 }
