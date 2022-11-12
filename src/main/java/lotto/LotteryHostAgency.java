a837dcf79e847f751b886d1c5885dae6b9d92eea
kimdeokyoung <dudejrss@naver.com>
2022-11-12 20:17:30 +0900

    feat (src.main.java.lotto.LotteryHostAgency) : 로또 당첨번호 검증 기능

--- src/main/java/lotto/LotteryHostAgency.java
+++ src/main/java/lotto/LotteryHostAgency.java
@@ -1,27 +1,46 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LotteryHostAgency {
 
     private List<Integer> winningNumbers;
-    private int bonusNumber;
+    private int bonusNumber = 1;
 
     protected void setWinningNumbers(List<Integer> numbers) {
-        // TODO: 메소드 구현
+        Lotto.isValidList(numbers);
+        this.winningNumbers = new ArrayList<>((numbers));
     }
 
-    protected void setBonusNumber() {
-        // TODO: 메소드 구현
+    protected void isValidNumber(int number) {
+        if (!((0 < number) && (number < 46)))
+            throw new IllegalArgumentException("[ERROR] Number must between 1 and 45");
+        if (this.winningNumbers.contains(number))
+            throw new IllegalArgumentException("[ERROR] Bonus number must not be in the Winning numbers");
+
     }
+
+    protected void setBonusNumber(int number) {
+        isValidNumber(number);
+        this.bonusNumber = number;
+    }
+
 
     public void draw(List<Integer> winningNumbers, int bonusNumber) {
-        // TODO: 메소드 구현
+        setWinningNumbers(winningNumbers);
+        setBonusNumber(bonusNumber);
     }
 
-    public WinningCriteria getResult(List<Integer> challengeNumbers) {
-        // TODO: 메소드 구현
+    public WinningCriteria getResult(Lotto challenge) {
+        List<Integer> challengeNumbers = challenge.getNumbers();
+        int matchCount = (int) winningNumbers.parallelStream().filter(challengeNumbers::contains).count();
+        if (matchCount == 6) return WinningCriteria.FIRST;
+        if (matchCount == 5 && challengeNumbers.contains(bonusNumber)) return WinningCriteria.SECOND;
+        if (matchCount == 5) return WinningCriteria.THIRD;
+        if (matchCount == 4) return WinningCriteria.FORTH;
+        if (matchCount == 3) return WinningCriteria.FIFTH;
         return WinningCriteria.NOTHING;
     }
 
