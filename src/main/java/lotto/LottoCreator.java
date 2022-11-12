80e00ded3e40f3b050187b5a574c644ed2531d36
oyoungsun <plusmate@naver.com>
2022-11-12 23:09:16 +0900

    fix : delete print part

--- src/main/java/lotto/LottoCreator.java
+++ src/main/java/lotto/LottoCreator.java
@@ -4,11 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class LottoCreator {
-    public Lotto createLotto(){
+    public Lotto createRandomLotto(){
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        for (int i : numbers){
-            System.out.println(i);
-        }
         return new Lotto(numbers);
     }
 }
