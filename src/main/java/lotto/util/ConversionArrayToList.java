a404f9e04776a652905e4e9cd98e8dc3eeabf493
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-12 19:27:34 +0900

    style: 들여쓰기 적용

--- src/main/java/lotto/util/ConversionArrayToList.java
+++ src/main/java/lotto/util/ConversionArrayToList.java
@@ -5,9 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ConversionArrayToList {
-    public List<Integer> stringArrayToIntegerList(String[] stringArray){
+    public List<Integer> stringArrayToIntegerList(String[] stringArray) {
         List<Integer> list = Arrays.stream(stringArray)
-                .map(i->Integer.parseInt(i))
+                .map(i -> Integer.parseInt(i))
                 .collect(Collectors.toList());
         return list;
     }
