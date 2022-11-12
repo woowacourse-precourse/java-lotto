e4d63999ef6b79683a5b8ef659db343b100f4ff0
sumin-Heo <102477741+sumin-Heo@users.noreply.github.com>
2022-11-12 15:43:18 +0900

    fix : enum class를 사용하도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -22,9 +22,9 @@ public class Application {
             }
             Lotto answer = createAnswer();
             int bonus = createBonus();
-            List<Integer> prize = Winnig(winALottery(answer, Lottos, bonus));
-            printCount(prize);
-            System.out.println("총 수익률은 "+rate(many,prize)/10+"."+rate(many,prize)%10+"%입니다.");
+            Winnig(winALottery(answer, Lottos, bonus));
+            printCount();
+            System.out.println("총 수익률은 "+rate(many)/10+"."+rate(many)%10+"%입니다.");
         }catch (IOException e){
             throw new IllegalArgumentException("[ERROR]");
         }
@@ -42,7 +42,7 @@ public class Application {
         try {
             temp = Integer.parseInt(cost);
         }catch (NumberFormatException e){
-            System.out.println("[ERROR]1");
+            System.out.println("[ERROR]");
         }
 
         if(temp%1000 != 0)
@@ -54,13 +54,10 @@ public class Application {
         return temp;
     }
 
-    public static void CostRight(int cost) {
-        if (cost % 1000 != 0)
-            throw new IllegalArgumentException("[ERROR] 입력 금액 오류");
-    }
-
     public static Lotto createAnswer() throws IOException {
         List<String> answer = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+
         System.out.println("당첨 번호를 입력해 주세요.");
 
         String answerN = Console.readLine();
@@ -68,18 +65,14 @@ public class Application {
         if(answerN != null)
             answer = List.of(answerN.split(","));
 
-        System.out.println(answer);
-        List<Integer> temp = new ArrayList<>();
-
-
         if(answer.stream().distinct().count() != answer.size())
             throw new IllegalArgumentException("[ERROR] 겹치는 번호 입력");
+
+        System.out.println(answer);
 
         for(int i =0; i<6; i++){
             temp.add(Integer.valueOf(answer.get(i)));
         }
-
-        System.out.println(temp);
 
         return new Lotto(temp);
     }
@@ -108,36 +101,46 @@ public class Application {
         return winnig;
     }
 
-    public static List<Integer> Winnig(List winning) throws IOException {
-        List<Integer> count = new ArrayList<>();
+    public enum Rank{
+        first("6개 일치 (2,000,000,000원) - ",0),
+        second("5개 일치, 보너스 볼 일치 (30,000,000원) - ",0),
+        third("5개 일치 (1,500,000원) - ",0),
+        fourth("4개 일치 (50,000원) - ",0),
+        fifth("3개 일치 (5,000원) - ",0);
 
-        int first = 0,second=0,third=0,fourth=0,fifth = 0;
+        private final String ment;
+        private Integer count;
+        Rank(String ment, Integer count){
+            this.ment = ment;
+            this.count = count;
+        }
+    }
+
+    public static void Winnig(List winning) throws IOException {
+        //List<Integer> count = new ArrayList<>();
 
         for(int i = 0; i<winning.size();i++){
-            if(winning.get(i).equals(6)) first++;
-            else if(winning.get(i).equals(7)) second++;
-            else if (winning.get(i).equals(5)) third++;
-            else if (winning.get(i).equals(4)) fourth++;
-            else if (winning.get(i).equals(3)) fifth++;
+            if(winning.get(i).equals(6)) Rank.first.count++;
+            else if(winning.get(i).equals(7)) Rank.second.count++;
+            else if (winning.get(i).equals(5)) Rank.third.count++;
+            else if (winning.get(i).equals(4)) Rank.fourth.count++;
+            else if (winning.get(i).equals(3)) Rank.fifth.count++;
         }
-        count.add(fifth); count.add(fourth); count.add(third); count.add(second);count.add(first);
-
-        return count;
+        //count.add(fifth); count.add(fourth); count.add(third); count.add(second);count.add(first);
     }
 
-
-    public static void printCount(List Winning) throws IOException {
+    public static void printCount() throws IOException {
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - "+Winning.get(0)+"개");
-        System.out.println("4개 일치 (50,000원) - "+Winning.get(1)+"개");
-        System.out.println("5개 일치 (1,500,000원) - "+Winning.get(2)+"개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+Winning.get(3)+"개");
-        System.out.println("6개 일치 (2,000,000,000원) - "+Winning.get(4)+"개");
+        System.out.println(Rank.fifth.ment+Rank.fifth.count+"개");
+        System.out.println(Rank.fourth.ment+Rank.fourth.count+"개");
+        System.out.println(Rank.third.ment+Rank.third.count+"개");
+        System.out.println(Rank.second.ment+Rank.second.count+"개");
+        System.out.println(Rank.first.ment+Rank.first.count+"개");
     }
 
-    public static Integer rate(Integer takeCost,List Winnng){
-       int allAdd = 2000000000 * (int)Winnng.get(4) + 30000000*(int)Winnng.get(3) + 1500000*(int)Winnng.get(2) + 50000*(int) Winnng.get(1)+ 5000*(int) Winnng.get(0);
+    public static Integer rate(Integer takeCost){
+       int allAdd = 2000000000 * Rank.first.count + 30000000*Rank.second.count + 1500000*Rank.third.count + 50000*Rank.fourth.count+ 5000*Rank.fifth.count;
        int rate = allAdd*10 / takeCost;
 
        if(rate%10 >= 5)
