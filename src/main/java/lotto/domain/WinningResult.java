fea6772589cddc905cefb4ed9011b50e733666fc
JJongmen <pjy90123@gmail.com>
2022-11-13 01:35:44 +0900

    refactor: 보너스 번호를 검증하는 기능의 메서드명 변경

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -5,12 +5,12 @@ public class WinningResult {
     private final LottoNumber bonusNumber;
 
     public WinningResult(Lotto winningNumbers, LottoNumber bonusNumber) {
-        validateBonusNumber(winningNumbers, bonusNumber);
+        validateHasBonusNumber(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
 
-    private void validateBonusNumber(Lotto winningNumbers, LottoNumber bonusNumber) {
+    private void validateHasBonusNumber(Lotto winningNumbers, LottoNumber bonusNumber) {
         if (winningNumbers.hasNumber(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호에 포함되어 있습니다.");
         }
