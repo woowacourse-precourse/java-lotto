8b61a2305b26dfebedfdc11a2c656b18d8a69d8b
sen <seeunchoi99@gmail.com>
2022-11-13 00:10:33 +0900

    ✨ 입력받은 금액에 따라 로또를 구매하는 로직 구현
    LottoGameController.buyLottos 를 통해 로또 구매하고, 발행된 로또를 받는다.

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,38 @@
+package lotto.controller;
+
+import java.util.Collections;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.view.UserInput;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class LottoGameController {
+    public void run() {
+        buyLottos();
+    }
+
+    private Lottos buyLottos() {
+        final Integer money = UserInput.inputMoney();
+        final Integer numberOfLottos = money / 1000;
+
+        return publishLottos(numberOfLottos);
+    }
+
+    private Lottos publishLottos(Integer numberOfLottos) {
+        Lottos lottos = new Lottos();
+        for (int i = 0; i < numberOfLottos; i++) {
+            lottos.addLotto(publishLotto());
+        }
+
+        return lottos;
+    }
+
+    private Lotto publishLotto() {
+        final List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+
+        return new Lotto(numbers);
+    }
+}
--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -6,12 +6,18 @@ import java.util.List;
 
 public class Lottos {
     private final List<Lotto> lottos = new ArrayList<>();
+    private Integer numberOfLottos = 0;
 
     public List<Lotto> getLottos() {
         return Collections.unmodifiableList(lottos);
     }
 
+    public Integer getNumberOfLottos() {
+        return numberOfLottos;
+    }
+
     public void addLotto(Lotto lotto) {
         this.lottos.add(lotto);
+        this.numberOfLottos += 1;
     }
 }
