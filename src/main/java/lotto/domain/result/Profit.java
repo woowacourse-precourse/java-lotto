47dc3318ea01f78b05013f69adcf5aa5fc060b0f
minju412 <ln8847@ajou.ac.kr>
2022-11-13 04:26:27 +0900

    feat: add Profit
    - 총 수익을 계산한다
    
    - 수익률을 계산한다

---
+++ src/main/java/lotto/domain/result/Profit.java
@@ -0,0 +1,40 @@
+package lotto.domain.result;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Profit {
+
+    private final int profit;
+    private final double earningsRate;
+
+    public Profit(int purchaseCost, Result result) {
+        this.profit = calculateProfit(result);
+        this.earningsRate = calculateEarningsRate(purchaseCost);
+    }
+
+    public double getEarningsRate() {
+        return earningsRate;
+    }
+
+    public int calculateProfit(Result matchResults) {
+        int profit = 0;
+        Map<Integer, Integer> result = matchResults.getResult();
+        List<Integer> prizeMoneys = Stream.of(Rank.values())
+                .map(Rank::getPrize)
+                .collect(Collectors.toList());
+        for (int index = 0; index < result.size(); index++) {
+            if (result.get(index) > 0) {
+                profit += (result.get(index) * prizeMoneys.get(index));
+            }
+        }
+        return profit;
+    }
+
+    public double calculateEarningsRate(int purchaseCost) {
+        double earningsRate = ((double) profit / purchaseCost) * 100;
+        return (double) Math.round(earningsRate * 10) / 10;
+    }
+}
