95c41c6f2bbabd05064a48415cf42b76ad702980
5nam <ohnam90@gmail.com>
2022-11-12 14:40:44 +0900

    feat: 구매 금액이 숫자가 아닐 경우 예외 발생하는 기능 추가

--- src/main/java/lotto/domain/LottoBuyer.java
+++ src/main/java/lotto/domain/LottoBuyer.java
@@ -1,9 +1,16 @@
 package lotto.domain;
 
 public class LottoBuyer {
+    private static final String INT_REGEX = "^[0-9]*$";
     public void checkPayUnit(int money) {
         if(money%1_000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구매 금액은 1,000 단위로만 입력 가능합니다.");
+        }
+    }
+
+    public void checkOnlyNumber(String input) {
+        if(!input.matches(INT_REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
         }
     }
 }
