002b95fc219b66f871bed3fdb6f41f25e6924b8f
wjh2012 <wjh2012@naver.com>
2022-11-12 12:32:34 +0900

    feat(Lotto): 범위를 벗어나면 IllegalArgumentException 발생

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,16 +4,19 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
-import static lotto.Constants.LOTTO_SIZE;
+import static lotto.config.Constants.*;
 
 public class Lotto {
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+
         validateSize(numbers);
         validateDuplicated(numbers);
+        validateRange(numbers);
         sortNumbers(numbers);
+
         this.numbers = numbers;
     }
 
@@ -29,7 +32,14 @@ public class Lotto {
         }
     }
 
+    private void validateRange(List<Integer> numbers) {
+        if (!numbers.stream().allMatch(n -> n >= LOTTO_NUMBER_START_INCLUSIVE && n <= LOTTO_NUMBER_END_INCLUSIVE)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public void sortNumbers(List<Integer> numbers) {
         numbers.sort(Comparator.naturalOrder());
     }
+
 }
