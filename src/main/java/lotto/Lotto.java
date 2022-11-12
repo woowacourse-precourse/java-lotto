1366e519dc11d7d0ad74e9fa2caa3a9db67176d3
SeokHwan An <asok369258@naver.com>
2022-11-12 15:12:32 +0900

    feat : 로또 번호가 서로 다른 숫자로 구성되어 있는지 검증하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,16 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateOverlap(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +20,11 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    // 서로 다른 숫자로 구성되었는지 검증하는 기능
+    private void validateOverlap(List<Integer> numbers) {
+        Set<Integer> lotto = new HashSet<>(numbers);
+        if (numbers.size() != lotto.size()) {
+            throw new IllegalArgumentException("[ERROR] 서로 다른 숫자로 구성되어 있지 않습니다.");
+        }
+    }
 }
