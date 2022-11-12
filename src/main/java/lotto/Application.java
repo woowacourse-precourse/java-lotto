c20b683f5dab48eb60202cea4e7f69d3692a90db
Lee-SungMin <saevers26@gmail.com>
2022-11-13 02:24:13 +0900

    feat(lotto): 당첨 번호와 보너스 번호 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Lotto;
+import lotto.domain.Winning;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,17 @@ public class Application {
             List <Integer> numbers = new ArrayList<>();
             Lotto lotto = new Lotto(numbers);
         }
+
+        disableWarning();
+
+        numbersPrint();
+        String inputNumbers = Console.readLine();
+        Winning.addWinning(inputNumbers);
+
+        bonusPrint();
+        String inputBonus = Console.readLine();
+        Winning.addBonus(inputBonus);
+
     }
 
     public static void inputPrint(){
@@ -25,6 +37,19 @@ public class Application {
 
     public static void countPrint(int lottoCount){
         System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+
+    public static void numbersPrint(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void bonusPrint(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public static void disableWarning() {
+        System.err.close();
+        System.setErr(System.out);
     }
 
 }
---
+++ src/main/java/lotto/domain/Winning.java
@@ -0,0 +1,52 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Winning {
+    public static List<Integer> numbers = new ArrayList<>();
+
+    public Winning(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    private static void validateWinning(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR]: 당첨 번호의 개수는 6개를 입력해야 합니다.");
+        }
+    }
+
+    private static void checkInt(String input) {
+        for(int i = 0; i < input.length(); i++){
+            if(!Character.isDigit(input.charAt(i))){
+                throw new IllegalArgumentException("[ERROR]: 숫자만 입력해야 합니다.");
+            }
+        }
+    }
+
+    public static void addWinning(String input){
+        List<String> inputNumbers = Arrays.asList(input.split(","));
+        for(int i = 0; i < inputNumbers.size(); i++){
+            checkInt(inputNumbers.get(i));
+            int inputNumber = Integer.valueOf(inputNumbers.get(i));
+            if(!numbers.contains(inputNumber)){
+                numbers.add(inputNumber);
+            }
+        }
+        validateWinning(numbers);
+    }
+
+    public static void addBonus(String input){
+        checkInt(input);
+        int inputNumber = Integer.valueOf(input);
+        if(inputNumber < 1 || inputNumber > 45){
+            throw new IllegalArgumentException("[ERROR]: 잘못된 값 입력입니다.");
+        }
+        if(!numbers.contains(inputNumber)) {
+            numbers.add(inputNumber);
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR]: 당첨 번호와 보너스 번호는 중복될 수 없습니다.");
+    }
+}
