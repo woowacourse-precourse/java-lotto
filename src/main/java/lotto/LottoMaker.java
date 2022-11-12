6363d7db576f9a1c8e8396b8a43efda7b43f2fe0
metorg <metrog950@gmail.com>
2022-11-12 17:28:31 +0900

    feat: 스트링 인트로 변환 메서드 생성

--- src/main/java/lotto/LottoMaker.java
+++ src/main/java/lotto/LottoMaker.java
@@ -1,13 +1,27 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMaker {
+
+    private Exception exception = new Exception();
 
     private List<Integer> splitNumber(String number){
 
         String [] numbers =number.split(",");
         return stringToInteger(numbers);
+    }
+
+    private List<Integer> stringToInteger(String [] numbers){
+        List<Integer> LottoNumber = new ArrayList<>();
+        for(int i=0;i<numbers.length;i++){
+            if(exception.lottoValidator(Integer.parseInt(numbers[i]))){
+                LottoNumber.add(Integer.parseInt(numbers[i]));
+            }
+            throw new IllegalArgumentException("ERROR");
+        }
+        return LottoNumber;
     }
 
 }
