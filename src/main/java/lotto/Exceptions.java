e888a67833af9f018b166d64a069385315767b17
minje <alswp0910@naver.com>
2022-11-12 18:38:21 +0900

    feat: 입력받은 당첨번호를 쉽표(,)로 나누는 기능 추가
    쉼표로 나눌 수 없는 경우 예외 처리

--- src/main/java/lotto/Exceptions.java
+++ src/main/java/lotto/Exceptions.java
@@ -5,6 +5,9 @@ public class Exceptions {
         throw new IllegalArgumentException("[ERROR] 숫자를 입력하지 않으셨습니다.");
     }
     public void notDivisionPrice() throws IllegalArgumentException {
-        throw new IllegalArgumentException("[ERROR] 입력 받은 금액은 1,000원으로 나눌 수 없습니다..");
+        throw new IllegalArgumentException("[ERROR] 입력 받은 금액은 1,000원으로 나눌 수 없습니다.");
+    }
+    public void notSplitNumbers() throws IllegalArgumentException {
+        throw new IllegalArgumentException("[ERROR] 쉼표를 제대로 입력하지 않으셨습니다.");
     }
 }
--- src/main/java/lotto/domain/Computer.java
+++ src/main/java/lotto/domain/Computer.java
@@ -1,11 +1,27 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.Exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Computer {
+    Exceptions exceptions = new Exceptions();
+
     public String inputNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String numbers = Console.readLine();
         return numbers;
+    }
+
+    public String[] splitNumbers(String numbers) {
+        String[] splitNum = new String[6];
+        try {
+            splitNum = numbers.split(",");
+        } catch (Exception e) {
+            exceptions.notSplitNumbers();
+        }
+        return splitNum;
     }
 }
