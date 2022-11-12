67a2dcdfb853c81e2a9ded25946126f40e71a737
jagajang <kosb7544@gmail.com>
2022-11-12 09:44:21 +0900

    dev: create lotto number generating method

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -17,4 +20,19 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public static List<Integer> generateRandomLottoNumbers() {
+        List<Integer> lottoNum = new ArrayList<>();
+
+        while(lottoNum.size() < 6) {
+            int generatedNumber = Randoms.pickNumberInRange(1, 45);
+
+            if(!lottoNum.contains(generatedNumber)) {
+                lottoNum.add(generatedNumber);
+            }
+        }
+
+        lottoNum.sort(Comparator.naturalOrder());
+
+        return lottoNum;
+    }
 }
