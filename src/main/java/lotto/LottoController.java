b1a8707d294a12f056b60337a8d4c4bcaaed0341
은지 <midzy0228@gmail.com>
2022-11-12 22:54:41 +0900

    feat: 컨트롤러 생성 및 예외 상황 시 에러 발생후 메시지 출력 기능 구현

---
+++ src/main/java/lotto/LottoController.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoController {
+
+    public void start() {
+        try {
+            InputView inputView = new InputView();
+
+            LottoAmount lottoAmount = inputAmount(inputView.getPurchaseAmount());
+            List<List<Integer>> lotto = lottoAmount.buyLotto();
+            Lotto winningLotto = inputLotto(inputView.getWinningNumbers());
+            BonusNumber bonusNumber = inputBonusNumber(inputView.getBonusNumber());
+
+            Statistics statistics = new Statistics(winningLotto.getWinningLotto(), bonusNumber.getBonusNumber());
+            statistics.compare(lotto, lottoAmount.getPurchaseAmount());
+        } catch (IllegalArgumentException errorMessage) {
+            System.out.println(errorMessage);
+        }
+    }
+    private LottoAmount inputAmount(int input) {
+        return new LottoAmount(input);
+    }
+
+    private Lotto inputLotto(List<Integer> input) {
+        return new Lotto(input);
+    }
+
+    private BonusNumber inputBonusNumber(String input) {
+        return new BonusNumber(input);
+    }
+}
