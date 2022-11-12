bb7db25c39ac2f5c2df067b5beaaccbbddbcdc6a
JaehongDev <println@kakao.com>
2022-11-12 22:33:09 +0900

    test (MoneyTest) : Money클래스의 ROI를 검증하는 테스트코드 작성

--- src/test/java/lotto/domain/money/MoneyTest.java
+++ src/test/java/lotto/domain/money/MoneyTest.java
@@ -2,9 +2,11 @@ package lotto.domain.money;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -29,5 +31,20 @@ class MoneyTest {
     void 금액은_최소금액으로_나누어_떨어져야합니다(final String input) {
         assertThatIllegalArgumentException().isThrownBy(() -> new Money(input))
                 .withMessageContaining(Money.ERROR_MONEY_DIVIDE_MIN_NUMBER);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "8000:5000:62.5",
+            "10000:1000:10.0",
+            "10000:4000:40.0",
+            "20000:3000:15.0"
+    }, delimiterString = ":")
+    void 금액의_수익률을_구할_수_있습니다(final String principleInput, final String profitInput, final Double percentInput) {
+        var principle = new Money(principleInput);
+        var profit = new Money(profitInput);
+
+        var actual = principle.calculateROI(profit);
+        Assertions.assertThat(actual).isEqualTo(percentInput);
     }
 }
