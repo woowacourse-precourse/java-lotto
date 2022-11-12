1c84dab43300fc28adb4594d96d1b06f038a798a
Brian <bwook9908@gmail.com>
2022-11-12 21:33:44 +0900

    feat(Print): 로또 번호를 출력하는 기능 구현
    - 로또 수량은 기존에 구현함
    - 로또 번호를 출력하는 기능 구현

--- src/main/java/InputOutput/Print.java
+++ src/main/java/InputOutput/Print.java
@@ -1,16 +1,18 @@
 package InputOutput;
 
+import java.util.List;
+
 public class Print {
 
-    public static void BuyLotto() {
+    public static void buyLotto() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public static void LottoCount(int count) {
+    public static void lottoCount(int count) {
         System.out.println(String.format("%d개를 구입했습니다.", count));
     }
 
-    public static void WinningNumber(){
+    public static void winningNumber(){
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
@@ -22,4 +24,7 @@ public class Print {
         System.out.println(String.format("[ERROR] %s", message));
     }
 
+    public static void lottoNumber(List<Integer> lotto){
+        System.out.println(lotto);
+    }
 }
--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,16 @@
 package lotto;
 
 import InputOutput.Input;
+import InputOutput.Print;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+//        List<Integer> lotto = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6));
+//        Print.lottoNumber(lotto);
     }
 }
