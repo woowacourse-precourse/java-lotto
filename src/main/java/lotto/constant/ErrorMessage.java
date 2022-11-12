5141ccb8e120a77375f74c2bdab45b1223fb8661
JunginPark4364 <wjddlsee@gmail.com>
2022-11-12 18:35:35 +0900

    feat: 구입금액 에러 메시지 상수 처리

---
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -0,0 +1,8 @@
+package lotto.constant;
+
+public class ErrorMessage {
+    public static final String ERROR = "[ERROR] ";
+    public static final String ERROR_MONEY = "구입금액은 0이상의 1000단위 숫자여야 합니다.";
+
+    private ErrorMessage() {}
+}
