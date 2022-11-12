3f4484aa9e04b251c941d238a5bf6b0e5c8c9008
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-12 18:19:07 +0900

    feat(ErrorMessage): ErrorMessage Enum 생성

---
+++ src/main/java/lotto/domain/ErrorMessage.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+public enum ErrorMessage {
+	SIZE("로또 번호는 %d개만 가능합니다."),
+	DUPLICATE("로또 번호들은 중복될 수 없습니다."),
+	UNIT("%d 단위 숫자만 입력해주세요."),
+	ZERO("최소 %d원부터 구매 가능합니다."),
+	NUMBER("%d ~ %d 사이의 자연수만 입력해주세요."),
+	BONUS("보너스 번호는 당첨 번호와 중복될 수 없습니다.")
+	;
+
+	private static final String error = "[ERROR] ";
+
+	private final String message;
+
+	ErrorMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return error + message;
+	}
+}
