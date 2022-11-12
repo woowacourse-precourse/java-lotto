ca93e77c11b4ca7ae4418cb598a86cd990810720
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-11-12 15:49:21 +0900

    feat: add print lotto amount
    - 구매한 로또 갯수 출력 메소드 추가

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -11,4 +11,8 @@ public class View {
     public static void printDepositView() {
         System.out.println("구입금액을 입력해 주세요.");
     }
+
+    public static void printLottoAmount(Integer amount) {
+        System.out.println(String.format("%d개를 구매했습니다.", amount));
+    }
 }
