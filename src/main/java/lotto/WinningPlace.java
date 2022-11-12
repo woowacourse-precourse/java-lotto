f3c31037fe78bcff91723b351df4e04bcfe01b69
KangSungBum <peter9706@naver.com>
2022-11-12 14:43:40 +0900

    feat(WinningPlace.java) : getPlace 메소드 추가
    getPlace() 메소드 추가
    - 입력받은 인자 correspondingNumber, correspondingBonusNumber 기준으로 WinningPlace의 일치하는 순위 반환

--- src/main/java/lotto/WinningPlace.java
+++ src/main/java/lotto/WinningPlace.java
@@ -1,12 +1,16 @@
 package lotto;
 
-public enum WinningPlace {
-    FIRST_PLACE(2000000000,"6개 번호 일치"),
-    SECOND_PLACE(30000000,"5개 번호 + 보너스 번호 일치"),
-    THIRD_PLACE(1500000,"5개 번호 일치"),
-    FOURTH_PLACE(50000,"4개 번호 일치"),
-    FIFTH_PLACE(5000,"3개 번호 일치");
+import java.util.Arrays;
 
+public enum WinningPlace {
+    FIRST_PLACE(6,0,2000000000,"6개 번호 일치"),
+    SECOND_PLACE(5,1,30000000,"5개 번호 + 보너스 번호 일치"),
+    THIRD_PLACE(5,0,1500000,"5개 번호 일치"),
+    FOURTH_PLACE(4,0,50000,"4개 번호 일치"),
+    FIFTH_PLACE(3,0,5000,"3개 번호 일치");
+
+    private Integer correspondingNumber;
+    private Integer correspondingBonusNumber;
     private Integer winnings;
     private String message;
 
@@ -18,7 +22,18 @@ public enum WinningPlace {
         return this.message;
     }
 
-    WinningPlace(Integer winnings, String message) {
+    public WinningPlace getPlace(int correspondingNumber, int correspondingBonusNumber) {
+        return Arrays.stream(values())
+                .filter(winningPlace -> winningPlace.correspondingNumber == correspondingNumber)
+                .filter(winningPlace -> winningPlace.correspondingBonusNumber == correspondingBonusNumber)
+                .findAny()
+                .orElseThrow();
+//                .orElseThrow(new IllegalArgumentException("Wrong"));
+    }
+
+    WinningPlace(Integer correspondingNumber, Integer correspondingBonusNumber, Integer winnings, String message) {
+        this.correspondingNumber = correspondingNumber;
+        this.correspondingBonusNumber = correspondingBonusNumber;
         this.winnings = winnings;
         this.message = message;
     }
