ebc7ad1abf808bcd5c2889febaee8d152d42631d
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-12 20:46:33 +0900

    feat: 기능구현 목록 2번 2.2 1.2에서 반환받은 로또의 개수만큼 출력

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,16 +1,30 @@
 package lotto.controller;
 
+import lotto.logic.Lotto;
+import lotto.logic.LottoGenerator;
 import lotto.logic.LottoPurchase;
+
+import java.util.List;
 
 public class LottoGame {
 
     private LottoPurchase purchase;
+    private LottoGenerator generator;
 
     public LottoGame() {
         purchase = new LottoPurchase();
+        generator = new LottoGenerator();
     }
 
-    public void play(){
-        int lotto = purchase.puchaseLotto();
+    public void play() {
+        int lottocnt = purchase.puchaseLotto();
+        showPurchaseLotto(lottocnt);
+    }
+
+    protected void showPurchaseLotto(int lottocnt) {
+        for (int i = 0; i < lottocnt; i++) {
+            Lotto lotto = new Lotto(generator.lottoGenerate());
+            System.out.println(lotto.getNumbers());
+        }
     }
 }
