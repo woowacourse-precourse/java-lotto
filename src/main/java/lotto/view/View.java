68388a7fe3b9aef26a49a17455c19021830cd88e
minaver <psmjoshua@gmail.com>
2022-11-12 15:26:26 +0900

    feat(View): printWinningStats method 추가

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -1,20 +1,32 @@
 package lotto.view;
 
+import lotto.Constant;
+import lotto.enums.Rank;
 import lotto.model.Lotto;
 
 import java.util.List;
+import java.util.Map;
 
 public class View {
 
     public void printLottoPayed(List<Lotto> lottoPayed) {
-        System.out.println(lottoPayed.size()+"개를 구매했습니다.");
+        System.out.printf(Constant.buyingFormat,lottoPayed.size());
         for (Lotto lotto : lottoPayed) {
             System.out.println(lotto.getNumbers());
         }
     }
 
-    public void printWinningStats() {
-
+    public void printWinningStats(Map<Rank, Integer> winningStats) {
+        System.out.printf(Constant.winningFormat,Constant.fifthRankNum
+                ,Constant.fifthRankPriceString,winningStats.get(Rank.fifth));
+        System.out.printf(Constant.winningFormat,Constant.fourthRankNum
+                ,Constant.fourthRankPriceString,winningStats.get(Rank.fourth));
+        System.out.printf(Constant.winningFormat,Constant.thirdRankNum
+                ,Constant.thirdRankPriceString,winningStats.get(Rank.third));
+        System.out.printf(Constant.winningFormatBonus,Constant.secondRankNum
+                ,Constant.secondRankPriceString,winningStats.get(Rank.second));
+        System.out.printf(Constant.winningFormat,Constant.firstRankNum
+                ,Constant.firstRankPriceString,winningStats.get(Rank.first));
     }
 
     public void printEarningRate() {
