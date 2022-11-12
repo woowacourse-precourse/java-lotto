e1a3edf7228485c889195c3ceb34e2cf42a01f11
kimtaehoonDev <taehoon_dev@naver.com>
2022-11-12 12:48:27 +0900

    feat(): 입력받은 금액을 검증한다

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -11,6 +11,16 @@ public class LottoGame {
     }
 
     public void start() {
-        Input.inputMoney();
+        Integer moneyEntered = Input.inputMoney();
+        validateInputMoney(moneyEntered);
+    }
+
+    protected void validateInputMoney(Integer moneyEntered) {
+        if(moneyEntered % 1000 != 0) {
+            throw new IllegalArgumentException("1000으로 나누어 떨어지는 숫자를 입력하세요");
+        }
+        if (moneyEntered < 1000) {
+            throw new IllegalArgumentException("1000 이상의 숫자를 입력하세요");
+        }
     }
 }
