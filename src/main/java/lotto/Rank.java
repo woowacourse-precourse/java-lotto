d2a84dc702fe775e350fb5594460813d6223ed0f
ddogga <hayeonlee60327@gmail.com>
2022-11-12 22:18:39 +0900

    등수 Rank enum 생성

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,4 +1,19 @@
 package lotto;
 
 public enum Rank {
+
+    FIRST(6, 2_000_000_000),
+    SECOND(5,3_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5000);
+
+
+    private final int prize;
+    private final int winNum;
+
+    Rank(int winNum, int prize) {
+        this.prize = prize;
+        this.winNum = winNum;
+    }
 }
