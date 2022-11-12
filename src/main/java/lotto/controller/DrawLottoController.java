c9734db0ba496fbe55c189e69c0248ba6f3e45ba
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-12 20:16:55 +0900

    feat: 로또를 구매하는 메서드 추가

--- src/main/java/lotto/controller/DrawLottoController.java
+++ src/main/java/lotto/controller/DrawLottoController.java
@@ -1,19 +1,31 @@
 package lotto.controller;
 
+import lotto.domain.User;
 import lotto.service.DrawLottoService;
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class DrawLottoController {
 
     DrawLottoService drawLottoService = new DrawLottoService();
     OutputView outputView = new OutputView();
+    InputView inputView = new InputView();
+    User user = new User();
 
-    public void startDrawLotto(){
+    public void startDrawLotto() {
         buyLottoFromMyAmount();
         setWiningLottoNumver();
         drawLotto();
         setWiningCount();
         printWiningCount(drawLottoService.getWiningCount());
         outputView.printRevenue(getRevenue());
+    }
+
+    public void buyLottoFromMyAmount() {
+        outputView.printBuyAmountMessage();
+        user.setBuyAmount(inputView.InputBuyAmount());
+        int count = drawLottoService.countBuyingLotto(user.getBuyAmount());
+        buyLotto(count);
+        outputView.printCountBuyingLotto(count, user.getMyLotto());
     }
 }
