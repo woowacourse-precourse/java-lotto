53a9833948c84e57c00d224f32c5888606f3137c
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-12 16:34:00 +0900

    feat: 당첨통계 출력 기능 추가
    - 당첨내역 출력하기
    - 수익률 출력하기

--- src/main/java/lotto/ui/Ouput.java
+++ src/main/java/lotto/ui/Ouput.java
@@ -15,16 +15,30 @@ public class Ouput {
         }
     }
 
-    public void notifyHowMuchInput(){
+    public void notifyHowMuchInput() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public void notifyWinningLottoNumbersInput(){
+    public void notifyWinningLottoNumbersInput() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
-    public void notifyBonusNumberInput(){
+    public void notifyBonusNumberInput() {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
+    public void notifyRateOfReturn(float rateOfReturn) {
+        System.out.printf("총 수익률은 %.1f%%입니다\n", rateOfReturn);
+    }
+
+    public void notifySumOfResult(List<Integer> sumOfResult) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.printf("3개 일치 (5,000원) - %d개\n", sumOfResult.get(0));
+        System.out.printf("4개 일치 (50,000원) -%d개\n", sumOfResult.get(1));
+        System.out.printf("5개 일치 (1,500,000원) - 0개\n", sumOfResult.get(2));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n", sumOfResult.get(3));
+        System.out.printf("6개 일치 (2,000,000,000원) - 0개\n", sumOfResult.get(4));
+
+    }
 }
