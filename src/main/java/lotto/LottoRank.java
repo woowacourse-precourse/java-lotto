ae2b5b707113c25e71e33176b433478fa94e7d11
김형기 <khk211113@naver.com>
2022-11-12 22:12:15 +0900

    feat: 순위와 그에 맞는 상금, 설명을 enum으로 관리

---
+++ src/main/java/lotto/LottoRank.java
@@ -0,0 +1,25 @@
+package lotto;
+
+public enum LottoRank {
+    FIRST("6개 일치", 2000000000),
+    SECOND("5개 일치, 보너스 볼 일치", 30000000),
+    THIRD("5개 일치", 1500000),
+    FOURTH("4개 일치", 50000),
+    FIFTH("3개 일치", 5000);
+
+    private String description;
+    private int prize;
+
+    LottoRank(String description, int prize) {
+        this.description = description;
+        this.prize = prize;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getPrize() {
+        return this.prize;
+    }
+}
