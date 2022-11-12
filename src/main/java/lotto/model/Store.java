0d29a6bf4292ea2e4d4a5e1f22f283d492f779bf
Jin409 <sualng123@gmail.com>
2022-11-12 11:50:01 +0800

    feat(Store): 가게에서 당첨 번호와 보너스 번호를 입력받는 기능 구현

--- src/main/java/lotto/model/Store.java
+++ src/main/java/lotto/model/Store.java
@@ -1,7 +1,14 @@
 package lotto.model;
 
+import static lotto.utils.NumberAdapter.fitBonusNumber;
+import static lotto.utils.NumberAdapter.fitWinningNumber;
+import static lotto.utils.NumberAdapter.fitWinningNumberWithBonusNumber;
 import static lotto.utils.NumberAdapter.getPaidMoney;
+import static lotto.validator.BonusNumberValidator.hasValidBonusNumber;
+import static lotto.validator.LottoNumberValidator.hasValidLotteryNumber;
+import static lotto.view.InputView.readBonusNumber;
 import static lotto.view.InputView.readUserPaidMoney;
+import static lotto.view.InputView.readWinningNumber;
 
 import java.util.List;
 
@@ -15,5 +22,23 @@ public class Store {
     public void sellLottoToCustomer() {
         int paidMoney = getPaidMoney(readUserPaidMoney());
         this.customer = new Customer(paidMoney);
+    }
+
+    public void pickWinningNumberWithBonusNumber() {
+        List<Integer> winningNumber = pickWinningNumber();
+        int bonusNumber = pickBonusNumber(winningNumber);
+        this.winningNumberWithBonusNumber = fitWinningNumberWithBonusNumber(winningNumber, bonusNumber);
+    }
+
+    private List<Integer> pickWinningNumber() {
+        List<Integer> winningNumber = fitWinningNumber(readWinningNumber());
+        hasValidLotteryNumber(winningNumber);
+        return winningNumber;
+    }
+
+    private int pickBonusNumber(List<Integer> winningNumber) {
+        int bonusNumber = fitBonusNumber(readBonusNumber());
+        hasValidBonusNumber(winningNumber, bonusNumber);
+        return bonusNumber;
     }
 }
