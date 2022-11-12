987af239ecc08b0f338da3a76e898ed986c8b985
5nam <ohnam90@gmail.com>
2022-11-12 15:05:37 +0900

    feat: 입력한 로또 번호가 숫자가 아닐 경우 예외처리 기능 추가

---
+++ src/main/java/lotto/domain/LottoDesignator.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class LottoDesignator {
+    private static final String INT_REGEX = "^[0-9]*$";
+
+    public void checkOnlyNumber(String input) {
+        String lotto = Arrays
+                .stream(input.split(","))
+                .collect(Collectors.joining());
+        if(!lotto.matches(INT_REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자만 입력 가능합니다.(구분자 쉼표만 가능)");
+        }
+    }
+}
