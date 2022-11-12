82f0ae73b4aceb52efe0c22bde1ae30df0bb5693
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-11-12 15:51:34 +0900

    refactor: 당첨 번호 예외 로직 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -16,6 +17,7 @@ public class InputView {
 	private static final String VALID_MONEY_ERROR_MESSAGE = "구입 금액은 1000원 단위로 입력하셔야 합니다.";
 	private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
 	private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해주세요";
+	private static final String INPUT_WIN_NUMBER_MESSAGE = "당첨 번호는 ,로 구분된 숫자(1,2,3,4,5,6)여야 합니다.";
 	private static final String VALID_WIN_NUMBER_MESSAGE = "당첨 번호는 1~45의 범위를 가지는 6개의 숫자여야 합니다.";
 	private static final String VALID_BONUS_NUMBER_MESSAGE = "보너스 번호는 1~45의 범위를 가지는 1개의 숫자여야 합니다.";
 	private static final String INPUT_WIN_NUMBER_DIVIDE_STRING = ",";
@@ -46,10 +48,24 @@ public class InputView {
 		return money % DIVIDE_UNIT != ZERO_NUMBER;
 	}
 
-	public String inputWinningNumber() {
+	public List<Integer> inputWinningNumber() {
+		List<Integer> winNumbers = new ArrayList<>();
 		System.out.println();
 		System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
-		return Console.readLine();
+		try {
+			winNumbers = Arrays.stream(Console.readLine().split(INPUT_WIN_NUMBER_DIVIDE_STRING))
+				.map(Integer::parseInt).collect(Collectors.toList());
+		} catch (IllegalArgumentException e) {
+			System.out.println(DEFAULT_ERROR_MESSAGE + INPUT_WIN_NUMBER_MESSAGE);
+		}
+		return winNumbers;
+	}
+
+	public void validWinningNumber(String userInput) {
+		if (isWrongSize(userInput) || isDuplicateNumber(userInput)
+			|| isWrongRangeWinningNumber(userInput)) {
+			throw new IllegalArgumentException(DEFAULT_ERROR_MESSAGE + VALID_WIN_NUMBER_MESSAGE);
+		}
 	}
 
 	public String inputBonusNumber() {
@@ -67,13 +83,6 @@ public class InputView {
 	private boolean isWrongRangeBonusNumber(String str) {
 		int bonusNumber = Integer.parseInt(str);
 		return !(MIN_VALUE <= bonusNumber && bonusNumber <= MAX_VALUE);
-	}
-
-	public void validWinningNumber(String userInput) {
-		if (isWrongSize(userInput) || isDuplicateNumber(userInput)
-			|| isWrongRangeWinningNumber(userInput)) {
-			throw new IllegalArgumentException(DEFAULT_ERROR_MESSAGE + VALID_WIN_NUMBER_MESSAGE);
-		}
 	}
 
 	private boolean isDuplicateNumber(String str) {
