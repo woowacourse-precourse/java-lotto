f9669266368f7778db81afbb6e745d84d5ac0801
shinD <huipulci1@naver.com>
2022-11-12 22:27:23 +0900

    feat: 통계 기능 추가

---
+++ src/main/java/lotto/domain/statistics/Statistics.java
@@ -0,0 +1,45 @@
+package lotto.domain.statistics;
+
+import lotto.domain.rank.Rank;
+import lotto.domain.yield.Yield;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.counting;
+
+public class Statistics {
+
+    private final Map<Rank, Long> statistics;
+    private final Yield yield;
+
+    private Statistics(Map<Rank, Long> statistics, Yield yield) {
+        this.statistics = new EnumMap<>(statistics);
+        this.yield = yield;
+    }
+
+    public static Statistics of(int principal, List<Rank> ranks) {
+
+        Map<Rank, Long> result = ranks.stream()
+                .collect(Collectors.groupingBy(r -> r, counting()));
+
+        int totalReword = ranks.stream()
+                .map(Rank::getReward)
+                .reduce(0, Integer::sum);
+
+        return new Statistics(
+                result,
+                Yield.calculate(principal, totalReword)
+        );
+    }
+
+    public long count(Rank rank) {
+        return statistics.get(rank);
+    }
+
+    public double yield() {
+        return yield.getPercent();
+    }
+}
