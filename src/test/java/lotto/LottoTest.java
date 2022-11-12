23b7e35b499d8fe36748b1ba0955c8910df5a66c
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-12 18:08:36 +0900

    Test(LottoTest) : 마킹역할하는 리스트 반환 기능 테스트 코드 추가 #61
    Lotto클래스의 getLottoLottoInfo()메소드의 테스트 코드 추가
    
    Ref : #60

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,4 +25,10 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또의 마킹역할 하는 리스트를 제대로 봔한하는지 확인")
+    @Test
+    void 로또_마킹_반환_테스트(){
+        assertThat(new Lotto(List.of(1, 2, 3, 4, 5, 6)).getLottoInfo())
+                .isEqualTo(List.of(1,2,3,4,5,6));
+    }
 }
