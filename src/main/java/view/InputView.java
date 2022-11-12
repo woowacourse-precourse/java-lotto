6dd92076ebeb44a0b58c89555044daa1e94468e8
tenta <eunoia3802@gmail.com>
2022-11-12 19:45:56 +0900

    correct: 입력 메서드 입력 값 받기 readLine()으로 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,29 +1,27 @@
 package view;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    Scanner scanner;
     private final String INPUT_MONEY = "구입금액을 입력해 주세요.";
     private final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public int inputMoney() {
-        scanner = new Scanner(System.in);
         System.out.println(INPUT_MONEY);
-        int money = scanner.nextInt();
+        String stringMoney = Console.readLine();
+        int money = Integer.parseInt(stringMoney);
         return money;
     }
 
     public String inputWinningNumber() {
-        scanner = new Scanner(System.in);
-        String winningNumber = scanner.nextLine();
+        String winningNumber = Console.readLine();
         return winningNumber;
     }
 
     public int inputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER);
-        scanner = new Scanner(System.in);
-        int bonusNumber = scanner.nextInt();
+        String StringBonusNumber = Console.readLine();
+        int bonusNumber = Integer.parseInt(StringBonusNumber);
         return bonusNumber;
     }
 }
