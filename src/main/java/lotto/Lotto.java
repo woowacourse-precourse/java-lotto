7a534501f03e61114e7a07b5f696e969f378cacf
yoon <beatus96@naver.com>
2022-11-12 23:27:07 +0900

    feat : 로또 등수 정하기 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Lotto {
@@ -9,11 +11,43 @@ public class Lotto {
     private final int LOTTO_MAX = 45;
     private final List<Integer> numbers;
 
+    private String rank;
+
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         sort(numbers);
         this.numbers = numbers;
     }
+
+    public int calculateMatchCount(List<Integer> winningLotto) {
+        int matchCount = 0;
+        for(Integer lottoNumber : numbers){
+            if(winningLotto.contains(numbers)){
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
+
+    public void setRank(List<Integer> winningLotto, int bonusNumber){
+        int matchCount = calculateMatchCount(winningLotto);
+        if(matchCount ==5 && numbers.contains(bonusNumber)){
+            rank = "SECOND";
+            return;
+        }
+        rank = rankMap.getOrDefault(matchCount,"");
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public Map<Integer,String> rankMap = Map.of(
+        6, "FIRST",
+        5,"THIRD",
+        4,"FOURTH",
+        3,"FIFTH"
+    );
 
     private void validate(List<Integer> numbers) {
         if (!isValidLotto(numbers)) {
