601a585e42ef15eafc6f793a662533f84bc1163e
wlsh44 <k87913j@gmail.com>
2022-11-12 16:47:48 +0900

    refactor: 로또 티켓 생성 방식 변경

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -4,20 +4,26 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class LottoGenerator {
 
     public static final int LOTTO_NUMBER_COUNT = 6;
-    public static final int MAX_LOTTO_NUMBER = 45;
-    public static final int MINIMUM_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int MINIMUM_LOTTO_NUMBER = 1;
 
 
     public static LottoTickets generateTickets(Money money) {
         List<Lotto> lottoTickets = new ArrayList<>();
-        for (int i = 0; i < money.getTicketCount(); i++) {
-            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MINIMUM_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT);
-            lottoTickets.add(new Lotto(lottoNumbers));
-        }
+        Stream.generate(LottoGenerator::makeNewLotto)
+                .limit(money.getTicketCount())
+                .forEach(lottoTickets::add);
         return new LottoTickets(lottoTickets);
+    }
+
+    private static Lotto makeNewLotto() {
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MINIMUM_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT);
+        return new Lotto(lottoNumbers);
     }
 }
