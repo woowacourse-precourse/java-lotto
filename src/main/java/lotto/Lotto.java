2e27e2ea714a1d8f2a663339a8335c466b431beb
grand7070 <grand7070@naver.com>
2022-11-12 16:22:06 +0900

    feat: 로또 번호를 오름차순으로 정렬시킨다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
@@ -10,6 +11,7 @@ public class Lotto {
         validate(numbers);
         duplicateValidate(numbers);
         this.numbers = numbers;
+        sort(numbers);
     }
 
     private void validate(List<Integer> numbers) {
@@ -20,5 +22,14 @@ public class Lotto {
     private void duplicateValidate(List<Integer> numbers) {
         // 2-1. 로또 번호는 중복되지 않게 한다.
         if(new HashSet<>(numbers).size() != numbers.size()) throw new IllegalArgumentException();
+    }
+
+    private void sort(List<Integer> numbers) {
+        // 2-2. 로또 번호를 오름차순으로 정렬시킨다.
+        Collections.sort(numbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
     }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,9 +3,11 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -22,5 +24,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호는 오름차순으로 정렬된다.")
+    @Test
+    void createLottoIsSort() {
+        assertThat(new Lotto(new ArrayList<>(List.of(3, 5, 1, 21, 19, 6))).getNumbers())
+                .isEqualTo(List.of(1, 3, 5, 6, 19, 21));
+    }
 }
