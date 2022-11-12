ad091c7abe250b84158897f1fc5b0ffefafd7f79
ddogga <hayeonlee60327@gmail.com>
2022-11-12 22:27:53 +0900

    사용자에게 금액을 입력받고 변수에 저장

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,10 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        //구입금액 입력 받기
+        int price = InputNum.askPrice();
+
+
     }
 }
--- src/main/java/lotto/InputNum.java
+++ src/main/java/lotto/InputNum.java
@@ -1,4 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputNum {
+
+
+    public static int askPrice() {
+
+        System.out.println("구입금액을 입력해 주세요.");
+        String price = Console.readLine();
+        return Integer.parseInt(price);
+    }
 }
