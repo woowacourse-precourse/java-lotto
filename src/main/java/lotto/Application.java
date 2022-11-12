14c9c979ebc24188e05597db14c04fbf2a76c33c
jh868 <2weeks9947@naver.com>
2022-11-12 11:09:24 +0900

    feat: 구입할 금액 입력처리 구현 및 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,29 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int purchasedLottoNumber;
+
+        purchasedLottoNumber = purchaseLotto();
+        System.out.println();
+        System.out.println(purchasedLottoNumber+"개를 구매했습니다.");
+
+    }
+    public static int purchaseLotto(){
+        int cost;
+
+        System.out.println("구입금액을 입력해 주세요.");
+        try{
+            cost = Integer.parseInt(Console.readLine());
+        }
+        catch (Exception e){
+            throw new IllegalArgumentException("[ERROR] 금액은 숫자로 입력하세요.");
+        }
+        if (cost % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 1000원 단위에 금액을 입력하세요.");
+        }
+        return cost/1000;
     }
 }
