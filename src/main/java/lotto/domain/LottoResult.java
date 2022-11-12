d5b0e157da2322e21bf945e1f5ad97b73d7fc29f
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-12 17:47:33 +0900

    feat: 로또의 총합 상금 계산 기능 완성

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -5,6 +5,14 @@ import java.util.Optional;
 
 public class LottoResult {
 
+    public int getTotalReward(Map<LottoRanking, Integer> rankingData) {
+        Optional<Integer> result = rankingData.entrySet()
+                .stream()
+                .map((entry) -> entry.getKey().getReward() * entry.getValue())
+                .reduce((x, y) -> x + y);
+        return result.get();
+    }
+
     public int getTotalTicket(Map<LottoRanking, Integer> rankingData) {
         Optional<Integer> totalTicket = rankingData.values()
                 .stream()
