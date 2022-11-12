62a21574c62614ce71038ad14c0faf4969f629b1
sen <seeunchoi99@gmail.com>
2022-11-13 00:49:04 +0900

    ✨ 로또 당첨 번호 입력 로직 구현

--- src/main/java/lotto/view/UserInput.java
+++ src/main/java/lotto/view/UserInput.java
@@ -1,10 +1,21 @@
 package lotto.view;
 
+import static lotto.view.util.InputUtil.*;
+
+import java.util.List;
+import lotto.domain.Lotto;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserInput {
-    public Integer inputMoney() {
+    public static Integer inputMoney() {
         return validateInputMoney(getInput());
+    }
+
+    public static Lotto inputLottoWinNumber() {
+        final List<String> lottoWinNumber = stringToList(getInput());
+
+        return validateLottoWinNumber(lottoWinNumber);
     }
 
     private static Integer validateInputMoney(String inputMoney) {
@@ -15,8 +26,12 @@ public class UserInput {
         return stringToInteger(inputMoney);
     }
 
-    private static Integer stringToInteger(String inputMoney) {
-        return Integer.parseInt(inputMoney);
+    private static Lotto validateLottoWinNumber(List<String> inputLottoWinNumber) {
+        if (!inputLottoWinNumber.stream().allMatch(n -> n.matches("^[0-9]+$"))) {
+            throw new IllegalArgumentException();
+        }
+
+        return new Lotto(stringListToIntegerList(inputLottoWinNumber));
     }
 
     private static String getInput() {
