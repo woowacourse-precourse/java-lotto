459a3ce2d937d2eedcdb78bd1433fef22da63eb9
hongil <sonhl0723@naver.com>
2022-11-12 22:27:41 +0900

    feat: 보너스 번호 중복 검증

--- src/main/java/lotto/BonusNumber.java
+++ src/main/java/lotto/BonusNumber.java
@@ -10,9 +10,10 @@ public class BonusNumber {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
-    public BonusNumber(String bonusNumber) {
+    public BonusNumber(String bonusNumber, List<Integer> winNumber) {
         int tmpBonusNumber = validateBonusNumberType(bonusNumber);
         validateBonusNumberRange(tmpBonusNumber);
+        validateBonusNumberReplicate(tmpBonusNumber, winNumber);
         this.bonusNumber = tmpBonusNumber;
     }
 
@@ -30,6 +31,13 @@ public class BonusNumber {
     public void validateBonusNumberRange(int bonusNumber) {
         if (bonusNumber < 1 || bonusNumber > 45) {
             System.out.println("[ERROR] 보너스 번호는 1~45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateBonusNumberReplicate(int bonusNumber, List<Integer> numbers) {
+        if (numbers.contains(bonusNumber)) {
+            System.out.println("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
             throw new IllegalArgumentException();
         }
     }
--- src/test/java/lotto/BonusNumberTest.java
+++ src/test/java/lotto/BonusNumberTest.java
@@ -1,27 +1,46 @@
 package lotto;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BonusNumberTest {
 
+    public List<Integer> numbers;
+
+    @BeforeEach
+    void createNumbers() {
+        numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+    }
+
     @DisplayName("보너스 번호에 숫자 이외의 문자가 포함되어 있으면 예외가 발생한다.")
     @Test
     void createBonusNumberByCharacter() {
-        assertThatThrownBy(() -> new BonusNumber("ab-"))
+        assertThatThrownBy(() -> new BonusNumber("ab-", numbers))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new BonusNumber("."))
+        assertThatThrownBy(() -> new BonusNumber(".", numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("보너스 번호에 숫자 이외의 문자가 포함되어 있으면 예외가 발생한다.")
     @Test
     void createBonusNumberWithInvalidNumber() {
-        assertThatThrownBy(() -> new BonusNumber("0"))
+        assertThatThrownBy(() -> new BonusNumber("0", numbers))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new BonusNumber("46"))
+        assertThatThrownBy(() -> new BonusNumber("46", numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호와 중복된다면 예외가 발생한다.")
+    @Test
+    void createBonusNumberByReplicate() {
+        assertThatThrownBy(() -> new BonusNumber("1", numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
