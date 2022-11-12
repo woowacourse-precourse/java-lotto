889b1ffba3c3cb57409263505a2622f853ffb2f6
3juhwan <13selfesteem91@naver.com>
2022-11-12 23:27:23 +0900

    feat(lotto): 로또 생성 시 예외처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -11,10 +12,38 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (!isSizeValid(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 생성된 숫자의 갯수가 일치하지 않습니다. ");
+        }
+        if (!isRangeValid(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 생성된 숫자가 1보다 작거나 45보다 큽니다. ");
+        }
+        if (!noOverlap(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 생성된 숫자 중 중복된 숫자가 있습니다. ");
         }
     }
 
     // TODO: 추가 기능 구현
+    private boolean isSizeValid(List<Integer> numbers) {
+        return numbers.size() == 6;
+    }
+
+    private boolean isRangeValid(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean noOverlap(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (Collections.frequency(numbers, number) != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
