2c8db3b6c72883a1a608a54f0f59324bc8c64860
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-12 19:46:35 +0900

    feat (LottoController) : 실행 메서드 생성

---
+++ src/main/java/controller/LottoController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import camp.nextstep.edu.missionutils.Console;
+import domain.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoController {
+    Validator validator = new Validator();
+
+    /**
+     * 로또 구입
+     * - 구입 금액 입력
+     * <p>
+     * 로또 발행
+     * - 구매 갯수 출력
+     * - 발행 번호 출력
+     * <p>
+     * 당첨 번호 입력
+     * - 당첨 번호 6자리 입력
+     * - 보너스 번호 입력
+     * <p>
+     * 당첨 통계, 총 수익률 출력
+     */
+
+    public void run() {
+
+
+    }
+
+
+
+    
+
+
+}
