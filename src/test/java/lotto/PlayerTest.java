a686d0702fd530b935d58b2349adb0b1384df974
oyoungsun <plusmate@naver.com>
2022-11-12 22:39:24 +0900

    Test code for checkDivideBy1000 in Player

--- src/test/java/lotto/PlayerTest.java
+++ src/test/java/lotto/PlayerTest.java
@@ -5,20 +5,21 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class UserTest {
+public class PlayerTest {
     @DisplayName("로또 구입 금액이 1000원 단위가 아니면 에러가 발생한다. ")
     @Test
     void createLottoCashBy1000(){
-        User.putMoney()
-        assertThatThrownBy(()-> User.)
+        int money = 1255;
+        Player player = new Player();
+        assertThatThrownBy(()-> player.checkDivideBy1000(money))
                 .isInstanceOf(IllegalArgumentException.class);
     }
     @DisplayName("로또 구입 입력이 정수가 아니면 에러가 발생한다. ")
     @Test
     void createLottoCashByInteger(){
-        assertThatThrownBy(()-> new Lotto(List.of(1, 2, 3 ,4, 55, 6)))
-                .isInstanceOf(IllegalArgumentException.class);
+
     }
 }
