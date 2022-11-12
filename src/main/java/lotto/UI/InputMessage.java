0629c52634ed0a7a34adb80badc8c9a0e4e280ac
cgk95 <kjkj5868@gmail.com>
2022-11-12 17:19:03 +0900

    refactor(UI) : 입력 요구 메시지에 출력 문자열 추가

--- src/main/java/lotto/UI/InputMessage.java
+++ src/main/java/lotto/UI/InputMessage.java
@@ -4,7 +4,9 @@ public enum InputMessage {
     PLEASE_INPUT_BUDGET("구입금액을 입력해 주세요."),
     PLEASE_INPUT_WIN_NUMBER("당첨번호를 입력해 주세요."),
 
-    PLEASE_INPUT_EXTRA_NUMBER("보너스 번호를 입력해 주세요.");
+    PLEASE_INPUT_EXTRA_NUMBER("보너스 번호를 입력해 주세요."),
+
+    PURCHASE_NUMBER_OF_GAME("개를 구매했습니다.");
 
     private final String value;
 
