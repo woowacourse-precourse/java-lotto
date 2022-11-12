8efb35ca1d4077f6f92e21090e59975c8d1cfab7
은지 <midzy0228@gmail.com>
2022-11-12 22:51:40 +0900

    fix: unsupportedoperationexception 오류 수정

--- src/main/java/lotto/NumberGenerator.java
+++ src/main/java/lotto/NumberGenerator.java
@@ -9,7 +9,7 @@ public class NumberGenerator {
     public List<List<Integer>> createRandomNumbers(Integer lottoTickets) {
         List<List<Integer>> lotto = new ArrayList<>();
         for (int count = 0; count < lottoTickets; count++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             numbers.sort(Integer::compareTo);
             lotto.add(numbers);
             System.out.println(numbers);
