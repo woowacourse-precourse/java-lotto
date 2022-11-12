d3519a3efd50ac30b031d46235c8bd07315f3365
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-12 16:22:04 +0900

    feat: 수익률 계산기능 추가

--- src/main/java/lotto/Manager.java
+++ src/main/java/lotto/Manager.java
@@ -72,6 +72,15 @@ public class Manager {
         }
         return sumOfResult;
     }
+
+    public float getRateOfReturn(List<Integer> sumOfResult) {
+        float sum = 0;
+        int[] prizeMoney = {5_000, 50_000, 1_500_000, 30_000_000, 2_000_000_000};
+        for (int i = 0; i < sumOfResult.size(); i++) {
+            sum += sumOfResult.get(i) * prizeMoney[i];
+        }
+        return sum / (getLottoTable().size() * Lotto.PRICE) * 100;
+    }
 }
 
 
