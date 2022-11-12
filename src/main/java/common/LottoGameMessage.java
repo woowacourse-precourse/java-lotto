b5c5f3ed476247f34dcd04464a95a320eff53c7b
박시균\psk98 <psg980331@naver.com>
2022-11-12 18:37:25 +0900

    Feat: 사용자의 로또 당첨 통계 출력

--- src/main/java/common/LottoGameMessage.java
+++ src/main/java/common/LottoGameMessage.java
@@ -32,4 +32,13 @@ public class LottoGameMessage {
             System.out.println(playerRandomLottoNumbers.getNumbers());
         }
     }
+    public void printLottoWinningStatistics(Player player){
+        System.out.println(WINNING_STATISTICS_MESSAGE);
+        System.out.println(UNDER_SCORE_MESSAGE);
+        System.out.println(WINNING_LOTTO_THREE_MESSAGE + player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_3,0));
+        System.out.println(WINNING_LOTTO_FOUR_MESSAGE + player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_4,0));
+        System.out.println(WINNING_LOTTO_FIVE_MESSAGE + player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_5,0));
+        System.out.println(WINNING_LOTTO_FIVE_BONUS_MESSAGE + player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_5_BONUS,0));
+        System.out.println(WINNING_LOTTO_SIX_MESSAGE + player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_6,0));
+    }
 }
