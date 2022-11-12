5a7eb0710cb4d4b05f8d95b4743144943a771905
shinD <huipulci1@naver.com>
2022-11-12 22:46:34 +0900

    feat: 기본 i/o 기능 구현

---
+++ src/main/java/lotto/infra/port/outbound/StandardOutput.java
@@ -0,0 +1,11 @@
+package lotto.infra.port.outbound;
+
+public class StandardOutput {
+
+    public static void print(Object message) {
+        System.out.print(message);
+    }
+    public static void println(Object message) {
+        System.out.println(message);
+    }
+}
