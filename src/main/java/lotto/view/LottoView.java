33e9dfe2f4393357ecf64990e5969e249298ae60
JunginPark4364 <wjddlsee@gmail.com>
2022-11-12 17:01:53 +0900

    feat: 구입금액 입력 기능

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import static camp.nextstep.edu.missionutils.Console.*;
+
 public class LottoView {
     private LottoView() {
 
@@ -7,5 +9,10 @@ public class LottoView {
 
     public static String printMessage(String message) {
         return message;
+    }
+
+    public static int inputMoney() {
+        String input = readLine();
+        return Integer.parseInt(input);
     }
 }
