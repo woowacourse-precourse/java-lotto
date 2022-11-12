98dcaa5283d15ee86a479b1fb4fc4d46c9c873ad
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-12 20:19:23 +0900

    feat: 로또를 구매하여 User에 저장하는 메서드 추가

--- src/main/java/lotto/controller/DrawLottoController.java
+++ src/main/java/lotto/controller/DrawLottoController.java
@@ -1,7 +1,9 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
 import lotto.domain.User;
 import lotto.service.DrawLottoService;
+import lotto.util.CreateLottoNumber;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -10,8 +12,9 @@ public class DrawLottoController {
     DrawLottoService drawLottoService = new DrawLottoService();
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
+    CreateLottoNumber createLottoNumber = new CreateLottoNumber();
     User user = new User();
-
+    Lotto lotto;
     public void startDrawLotto() {
         buyLottoFromMyAmount();
         setWiningLottoNumver();
@@ -29,4 +32,10 @@ public class DrawLottoController {
         outputView.printCountBuyingLotto(count, user.getMyLotto());
     }
 
+    private void buyLotto(int count){
+        for (int i =0;i<count;i++){
+            lotto = new Lotto(createLottoNumber.createLottoNumber());
+            user.setMyLotto(lotto);
+        }
+    }
 }
