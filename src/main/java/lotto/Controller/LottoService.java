4887e9306ae8843e4f5f7c29afd2023edbee8149
Park shinyeong <syngh503@gmail.com>
2022-11-12 22:27:45 +0900

    Feat(LottoService): 로또 서비스 구현
    로또 구입과 관련된 로또 서비스를 구현하였습니다.
    
    로또를 구입해 구입한 횟수와 로또 목록을 출력하는 메소드입니다.
    
    Closes: #8

--- src/main/java/lotto/Controller/LottoService.java
+++ src/main/java/lotto/Controller/LottoService.java
@@ -1,4 +1,30 @@
 package lotto.Controller;
 
+import Utils.InputUtils;
+import lotto.Domain.Player;
+import lotto.View.InputView;
+import lotto.View.OutputView;
+
 public class LottoService {
+    private Player player;
+
+    public LottoService(Player player) {
+        this.player = player;
+    }
+
+    public void purchaseLotto() throws IllegalArgumentException {
+        String lottoMoney = InputView.requestLottoMoney();
+        isValidateLottoMoney(lottoMoney);
+        player.setLottoCount(Integer.parseInt(lottoMoney));
+        int lottoCount = player.getLottoCount();
+        player.setLottos(lottoCount);
+        OutputView.printPurchaseLotto(lottoCount, player.getLottos());
+    }
+
+    public void isValidateLottoMoney(String money) {
+        final String ERROR_MESSAGE = "[ERROR] 1000원의 배수인 금액을 입력하여야 합니다.";
+        if(!InputUtils.isDigit(money) || !InputUtils.isMultipleOfOneThousand(money)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
 }
