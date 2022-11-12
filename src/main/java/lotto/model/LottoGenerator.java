e17677df8ad54385ad5ccefaea35b7e6914a1b46
gomljo <gomljo0307@naver.com>
2022-11-13 02:37:21 +0900

    feat(LottoGenerator.java): 구매한 로또의 수만큼 로또를 발행하는 기능 구현

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -9,8 +9,18 @@ public class LottoGenerator {
     private static final int LOTTO_START_NUMBER=1;
     private static final int LOTTO_END_NUMBER=45;
     private static final int NUMBER_OF_LOTTO=6;
+    private int GENERATED_LOTTO=0;
 
     public List<Integer> createLotto(){
         return Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, NUMBER_OF_LOTTO);
+    }
+    public List<List<Integer>> generateLottoOfBuyer(int numberOfTickets){
+        List<List<Integer>> lottoOfBuyer = new ArrayList<>();
+        while(GENERATED_LOTTO<numberOfTickets){
+            List<Integer> lotto=createLotto();
+            lottoOfBuyer.add(lotto);
+            GENERATED_LOTTO++;
+        }
+        return lottoOfBuyer;
     }
 }
