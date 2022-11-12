2cf74e6d7e5fcc8028cae3b2110f39cab78e43c5
KangSungBum <peter9706@naver.com>
2022-11-12 16:14:49 +0900

    feat(Application.java) : 구입가격 개수 변환 함수 추가
    convertBuyingPriceIntoLotteries() 메소드 추가
    - param1: buyintPrice: 사용자가 입력한 구입가격
    - 1000단위의 가격인지 확인
    - 정상 입력시 로또 개수로 변환해서 반환
    - 비정상 입력시 IllegalArgumentException 반환

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,4 +4,12 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public static int convertBuyingPriceIntoLotteries(int buyingPrice) {
+        if (buyingPrice % 1000 != 0) {
+            throw new IllegalArgumentException(ErrorResponse.INPUT_BUYING_RANGE_ERROR.getErrorMessage());
+        }
+
+        return buyingPrice / 1000;
+    }
 }
