522d982ec8dcf88c6caeaa9facebd5d21bee0381
leewoojin <zmfpdl64@naver.com>
2022-11-12 23:49:21 +0900

    Refactoring: 리펙토링 때문에 import 파일 변경 및 삭제

--- src/test/java/lotto/object/UserTest.java
+++ src/test/java/lotto/object/UserTest.java
@@ -1,17 +1,14 @@
 package lotto.object;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.test.NsTest;
-import lotto.Application;
+import lotto.playlotto.object.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest extends NsTest {
 
