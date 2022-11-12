18079ecbf630ca53031f4d0cb0c9cd094bd75b96
woo-chang <clllickme@naver.com>
2022-11-12 21:20:56 +0900

    fix(OutputService): 수익률 출력 메시지를 요구사항에 알맞게 수정

--- src/main/java/lotto/service/OutputService.java
+++ src/main/java/lotto/service/OutputService.java
@@ -9,7 +9,7 @@ public class OutputService {
     private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
     private static final String WIN_STATUS_MESSAGE = "당첨 통계";
     private static final String LINE = "---";
-    private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f입니다.\n";
+    private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다.\n";
     private static final String ERROR_PREFIX = "[ERROR] ";
 
     private OutputService() {
