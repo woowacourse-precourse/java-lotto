8eb06a6e7b9aa7ba35646b8b1086de000d725995
tenta <eunoia3802@gmail.com>
2022-11-12 17:55:50 +0900

    feat: 로또 당첨번호 입력 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,12 +3,20 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
+    Scanner scanner;
     private final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
 
     public int inputMoney() {
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
         System.out.println(INPUT_MONEY);
         int money = scanner.nextInt();
         return money;
+    }
+
+    public void inputWinningNumber() {
+        scanner = new Scanner(System.in);
+        System.out.println(INPUT_WINNING_NUMBER);
+        int winningNumber = scanner.nextInt();
     }
 }
