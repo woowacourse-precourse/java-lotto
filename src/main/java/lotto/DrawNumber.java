60eee8069bdc07723a9de82a43031958b9867d66
Amyhds <dasomhan524@gmail.com>
2022-11-12 19:50:21 +0900

    refactor(drawnumber): 리팩토링

--- src/main/java/lotto/DrawNumber.java
+++ src/main/java/lotto/DrawNumber.java
@@ -5,20 +5,21 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import java.util.Collections;
 
-public class DrawNumberGenerator {
+public class DrawNumber {
     public static List<Lotto> Lottos;
 
-    public void purchaseOutput() {
-        int lottoCount = Purchase.calculate;
-        System.out.println(Integer.toString(lottoCount) + LottoMessage.PURCHASE_MESSAGE.getMessage());
-        for (int count = 0; count < lottoCount; count++) {
-            List<Integer> oneLottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            Collections.sort(oneLottoNumbers);
-            Lotto oneLotto = new Lotto(oneLottoNumbers);
+    public static void generateNumber(int ticketAmount) {
+        System.out.println(Integer.toString(ticketAmount) + GameMessage.PURCHASE_MESSAGE.getMessage());
+        for (int count = 0; count < ticketAmount; count++) {
+            List<Integer> oneTicketNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(oneTicketNumber);
+            Lotto oneLotto = new Lotto(oneTicketNumber);
             Lottos.add(oneLotto);
         }
+    }
 
-        for (int count = 0; count < lottoCount; count++) {
+    public static void printNumber(int ticketAmount) {
+        for (int count = 0; count < ticketAmount; count++) {
             System.out.println(Lottos.get(count));
         }
     }
