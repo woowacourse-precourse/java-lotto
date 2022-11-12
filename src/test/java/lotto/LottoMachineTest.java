0d8d72aa1ea4fbfdad9503f4088a0b2073c964eb
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-12 23:37:40 +0900

    Feat(LottoMachine) : 다수의 로또를 구매하는 기능의 테스트 코드를 추가했습니다. #74
    public static void buyLotto(List<Lotto> lottos, BigInteger money)메소드의 테스트 코드를 추가했습니다.
    
    Ref : #73

---
+++ src/test/java/lotto/LottoMachineTest.java
@@ -0,0 +1,34 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import lotto.Lotto;
+import lotto.LottoMachine;
+
+import java.util.Iterator;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.ArrayList;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoMachineTest {
+
+    @DisplayName("복권을 구매합니다.")
+    @Test
+    void 복권_구매_테스트(){
+        List<Lotto> lottos = new ArrayList<>();
+
+        LottoMachine.buyLotto(lottos, BigInteger.valueOf(8000));
+
+        Iterator<Lotto>iter = lottos.iterator();
+        while (iter.hasNext()){
+            Lotto lotto = iter.next();
+            System.out.println(lotto);
+        }
+    }
+}
