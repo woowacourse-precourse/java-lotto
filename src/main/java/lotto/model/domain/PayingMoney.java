0ba183f4a7694a9c5013800373a43d762c171797
kjb95 <topmedia44@gmail.com>
2022-11-12 18:13:24 +0900

    test: 로또 구입 금액 비정상적인 입력값 처리하기 테스트 코드 추가
    - 숫자가 아닌경우
    - 1000원 단위로 나누어 떨어지지 않는 경우
    - 1000원 미만인 경우

--- src/main/java/lotto/model/domain/PayingMoney.java
+++ src/main/java/lotto/model/domain/PayingMoney.java
@@ -7,10 +7,10 @@ public class PayingMoney {
 
     private static final int LOTTO_PRICE = 1000;
     private static final int NOTHING = 0;
-    private static final String MONEY_REGEX = "^[0-9]$";
+    private static final String MONEY_REGEX = "^[0-9]*$";
     private static final String MONEY_IS_ONLY_NUMBER = "로또 구입 금액은 숫자 이어야 합니다.";
     private static final String MONEY_IS_MORE_THAN_1000 = "로또 구입 금액은 최소 1000원 이상 이어야 합니다.";
-    private static final String MONEY_IS_DIVISOR_OF_1000 = "로또 구입 금액은 1000원으로 나누어 떨어져야 합니다.";
+    private static final String MONEY_IS_FACTOR_OF_1000 = "로또 구입 금액은 1000원으로 나누어 떨어져야 합니다.";
 
     private final int money;
 
@@ -29,7 +29,7 @@ public class PayingMoney {
             throw new IllegalArgumentException(MONEY_IS_MORE_THAN_1000);
         }
         if (parsedMoney % LOTTO_PRICE != NOTHING) {
-            throw new IllegalArgumentException(MONEY_IS_DIVISOR_OF_1000);
+            throw new IllegalArgumentException(MONEY_IS_FACTOR_OF_1000);
         }
         return parsedMoney;
     }
--- src/test/java/lotto/model/domain/PayingMoneyTest.java
+++ src/test/java/lotto/model/domain/PayingMoneyTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.model.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class PayingMoneyTest {
-  
+    
+    @Test
+    void 로또_구입_금액_정상적인_입력값_처리하기() {
+        Assertions.assertThatCode(() -> new PayingMoney(" 1 0 0 0 "))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 로또_구입_금액_비정상적인_입력값_처리하기() {
+        Assertions.assertThatThrownBy(() -> new PayingMoney("1000a"))
+                .isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> new PayingMoney("1001"))
+                .isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> new PayingMoney("999"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
