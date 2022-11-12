62c4a2282eb3bec83168e4def3290772dd98441f
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-11-12 14:58:04 +0900

    fix : enum함수 수정

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,12 +1,13 @@
 package lotto;
 
 public enum Prize {
-    first(0,"1등", 2000000000),
-    second(0,"2등", 30000000),
-    third(0,"3등", 1500000),
-    fourth(0,"4등", 50000),
-    fifth(0,"5등", 5000),
-    nocount(0,"아무것도아님",0);
+    prize_0(0,"1등", 2000000000),
+    prize_1(0,"2등", 30000000),
+    prize_2(0,"3등", 1500000),
+    prize_3(0,"4등", 50000),
+    prize_4(0,"5등", 5000),
+    prize_5(0,"아무것도아님",0),
+    prize_6(0,"아무것도아님2",0);
 
     private int count;
     private final String name;
