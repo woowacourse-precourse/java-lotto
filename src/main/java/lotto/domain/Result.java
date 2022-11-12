b49fa9089cc3a0be3635fa39ea7292145338f710
Reznov <reznov03048@gmail.com>
2022-11-13 01:01:54 +0900

    style: 줄 정리

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -8,14 +8,13 @@ import java.util.Map;
 public class Result {
     private Map<Ranking, Integer> result = new EnumMap<Ranking, Integer>(Ranking.class);
 
-    public Result(List<Ranking> rankings){
+    public Result(List<Ranking> rankings) {
         makeWinningResult(rankings);
     }
 
     private void makeWinningResult(List<Ranking> rankings) {
-        for(Ranking ranking : rankings) {
+        for (Ranking ranking : rankings) {
             result.put(ranking, result.getOrDefault(ranking, 0) + 1);
         }
     }
-
 }
