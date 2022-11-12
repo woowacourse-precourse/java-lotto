ab56cec8bdfc0cf10623c30d9b7466aaa42fbf09
JuHyeokParkk <hyeok5596@naver.com>
2022-11-13 01:29:40 +0900

    feat: 수익률을 계산하는 메서드 구현

--- src/main/java/lotto/LottoPrizeRecordingManager.java
+++ src/main/java/lotto/LottoPrizeRecordingManager.java
@@ -1,10 +1,12 @@
 package lotto;
 
 import java.util.EnumMap;
+import java.util.stream.Collectors;
 
 public class LottoPrizeRecordingManager {
 
     private EnumMap<WinPrize, Long> prizeRecording = new EnumMap<>(WinPrize.class);
+    private long totalPrizeMoney;
 
     public void setPrizeRecording(WinPrize winPrize) {
         if (!winPrize.equals(WinPrize.NOTHING)) {
@@ -15,4 +17,24 @@ public class LottoPrizeRecordingManager {
     public EnumMap<WinPrize, Long> getPrizeRecording() {
         return prizeRecording;
     }
+
+    public long getTotalPrizeMoney() {
+        return totalPrizeMoney;
+    }
+
+    public void setTotalPrizeMoney() {
+        totalPrizeMoney = prizeRecording.keySet().stream().map(
+                (key) -> key.price * prizeRecording.get(key))
+                .collect(Collectors.toList())
+                .stream()
+                .reduce(0L, Long::sum);
+    }
+
+    public double getPrizeRate(long purchaseMoney) {
+        setTotalPrizeMoney();
+
+        return totalPrizeMoney / purchaseMoney;
+    }
+
+
 }
