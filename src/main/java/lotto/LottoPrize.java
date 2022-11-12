63f5184abb36944d7ea8cab85eac88c11fa2cdf2
jiwon <ljwon77@daum.net>
2022-11-12 22:54:22 +0900

    feat: compute matched count through comparing winning / user number
    당첨 번호와 사용자의 번호를 비교하여 일치하는 개수를 리턴하는 기능을 추가하였습니다.

---
+++ src/main/java/lotto/LottoPrize.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class LottoPrize {
+
+    public long getMatchCount(List<Integer> winningNums, List<Integer> userNums) {
+        return winningNums.stream()
+                .filter(lottoNum -> userNums.stream()
+                        .noneMatch(Predicate.isEqual(lottoNum)))
+                .count();
+    }
+
+}
