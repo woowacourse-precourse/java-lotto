c88fa9b954e41acc48ad9ec27bf2059620d8787a
HamSeungChan <head0618@naver.com>
2022-11-12 12:38:32 +0900

    "refactor : "로또 구매를 관리 하는 클래스 생성 후 로또 구매 기능 클래스로 이동 & 변수명 변경"

---
+++ src/main/java/lotto/controller/LottoPurchaseManagement.java
@@ -0,0 +1,27 @@
+package lotto.controller;
+
+public class LottoPurchaseManagement {
+
+  static final int LOTTO_PRICE = 1000;
+
+  final int LOTTO_TICKETS ;
+
+  public LottoPurchaseManagement(int purchaseMoney) {
+    valid(purchaseMoney);
+    this.LOTTO_TICKETS = changeMoneyToLottoTickets(purchaseMoney);
+  }
+
+  private int changeMoneyToLottoTickets(int purchaseAmount) {
+    return purchaseAmount / LOTTO_PRICE;
+  }
+
+  private void valid(int purchaseMoney) {
+    if (purchaseMoney % LOTTO_PRICE > 0) {
+      throw new IllegalArgumentException("[ERROR] 구매 금액이 1000원 단위여야 합니다.");
+    }
+  }
+
+  public int getNumberOfLottoTickets(){
+    return LOTTO_TICKETS;
+  }
+}
