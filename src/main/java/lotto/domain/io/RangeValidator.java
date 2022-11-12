7e37dd012a1585fe56049edf92890cbb5a7b6a6c
yubin <dbqls2821@gmail.com>
2022-11-12 15:57:01 +0900

    refactor(RangeValidator): 로또 번호의 범위를 검증할 때 boundaryValue의 enum 상수값 사용하도록 수정

--- src/main/java/lotto/domain/io/RangeValidator.java
+++ src/main/java/lotto/domain/io/RangeValidator.java
@@ -1,5 +1,6 @@
 package lotto.domain.io;
 
+import lotto.domain.constant.BoundaryValue;
 import lotto.domain.constant.ExceptionMessage;
 
 import java.util.List;
@@ -7,13 +8,13 @@ import java.util.List;
 public class RangeValidator {
 
   void validate(int input) {
-    if (input < 1 || input > 45) {
+    if (input < BoundaryValue.LOTTO_MIN_NUMBER.getBoundaryValue() || input > BoundaryValue.LOTTO_MAX_NUMBER.getBoundaryValue()) {
       throw new IllegalArgumentException(ExceptionMessage.INPUT_NUMBER_RANGE_IS_NOT_CORRECT.getMessage());
     }
   }
 
   void validate(List<Integer> input) {
-    boolean isWithinRange = input.stream().allMatch(it -> it < 46 && it > 0);
+    boolean isWithinRange = input.stream().allMatch(it -> it < BoundaryValue.LOTTO_EXCEEDED_NUMBER.getBoundaryValue() && it > BoundaryValue.LOTTO_LESS_THAN_NUMBER.getBoundaryValue());
     if (!isWithinRange) {
       throw new IllegalArgumentException(ExceptionMessage.INPUT_NUMBER_RANGE_IS_NOT_CORRECT.getMessage());
     }
