1ed42d569117ac79e40970990034309e1d0743fb
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-12 18:15:36 +0900

    feat(lotto) : 로또 번호의 중복을 체크하는 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,12 +1,15 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
@@ -14,6 +17,17 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR]로또 번호의 개수가 6개가 아닙니다");
         }
+
+        if (checkDuplicate(numbers)) {
+            throw new IllegalArgumentException("[ERROR]중복입니다.");
+        }
+    }
+
+    private boolean checkDuplicate(List<Integer> numbers) {
+        long distinctSize = numbers.stream()
+                .distinct()
+                .count();
+        return numbers.size() != distinctSize;
     }
 
     public List<Integer> getNumbers() {
