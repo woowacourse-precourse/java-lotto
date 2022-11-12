920661b45e5dbe3def8f0e83ef9313f1e583fb95
hyowonhan <wain1719@gmail.com>
2022-11-13 00:53:30 +0900

    fix: 예외처리 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -10,6 +10,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Lotto {
     private final List<Integer> numbers;
     private static final String BONUS_INPUT_SENTENCE  = "보너스 번호를 입력해 주세요.\n";
+    private static final String ERROR_MESSAGE = "[ERROR]";
 
     public Lotto(List<Integer> numbers) {
         validateCount(numbers);
@@ -20,14 +21,14 @@ public class Lotto {
 
     private void validateCount(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 당첨 번호는 6개여야 합니다.");
         }
     }
 
     private void validateOverlap(List<Integer> numbers) {
         for(int i=0; i<numbers.size(); i++){
             if (Collections.frequency(numbers,numbers.get(i)) != 1) {
-                throw new IllegalArgumentException("[ERROR] 숫자가 중복되지 않아야 합니다.");
+                throw new IllegalArgumentException(ERROR_MESSAGE + " 숫자가 중복되지 않아야 합니다.");
             }
         }
     }
@@ -35,7 +36,7 @@ public class Lotto {
     private void validateRange(List<Integer> numbers) {
         for(int i=0; i<numbers.size(); i++) {
             if(numbers.get(i)< 1 || numbers.get(i)> 45) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                throw new IllegalArgumentException(ERROR_MESSAGE + " 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
         }
     }
@@ -44,7 +45,7 @@ public class Lotto {
         System.out.print(BONUS_INPUT_SENTENCE);
         int bonusNum = Integer.parseInt(Console.readLine());
         if(bonusNum< 1 || bonusNum> 45) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
         return bonusNum;
     }
