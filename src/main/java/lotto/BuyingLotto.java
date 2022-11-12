53ac090edd4fdb76862fd99f7b7fb515639cc0e7
yejin <kwonyj1022@naver.com>
2022-11-13 04:08:46 +0900

    feat: BuyingLotto 클래스: 로또 구입 금액을 입력받고 로또 발행

---
+++ src/main/java/lotto/BuyingLotto.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuyingLotto {
+    final int count;
+    List<List<Integer>> lottos = new ArrayList<>();
+    BuyingLotto() {
+        System.out.println("구매금액을 입력해 주세요.");
+        count = new Customer().count;
+        System.out.println(count + "개를 구매했습니다.");
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = new LottoGenerator().numbers;
+            lottos.add(numbers);
+            System.out.println(numbers);
+        }
+    }
+
+    public static void main(String[] args) {
+        new BuyingLotto();
+    }
+}
