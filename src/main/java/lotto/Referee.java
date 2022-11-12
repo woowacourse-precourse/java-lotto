3936076aa43e424cebddf8559fd7e332c84ea3fa
jeongjihun <wlgns2234@naver.com>
2022-11-12 17:43:58 +0900

    feat(lotto): 사용자 로또들과 당첨번호(보너스) 비교하는 기능 구현

--- src/main/java/lotto/Referee.java
+++ src/main/java/lotto/Referee.java
@@ -1,9 +1,31 @@
 package lotto;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Referee {
-    public int compare(List<Integer> winningLotto, List<Integer> userLotto) {
-        return 3;
+    public static Map<Integer, Integer> compare(int bonusNumber, List<Integer> winningLotto, List<Lotto> userLotto) {
+        Map<Integer, Integer> result = new HashMap<>();
+        for (int i = 0; i < 8; i++) {
+            result.put(i, 0);
+        }
+
+        for (int i = 0; i < userLotto.size(); i++) {
+            Lotto lottoClass = userLotto.get(i);
+            List<Integer> user = lottoClass.get();
+
+            int correctNumber = Judgment.correctCount(winningLotto, user);
+            if (Judgment.hasBonusNumber(bonusNumber, user)){
+                correctNumber += 1;
+            }
+
+            System.out.println(user);
+            result.replace(correctNumber, result.get(correctNumber) + 1);
+        }
+
+        return result;
     }
 }
