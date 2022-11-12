e857da30b3a5b087c1bae3e643b743f422a5b35a
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-12 20:31:34 +0900

    feat: 당첨 순위를 가독성을 위해 올바른 이름으로 선언

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,4 +1,17 @@
 package lotto.domain;
 
 public enum Rank {
+    fifth(3, 5000),
+    fourth(4, 50000),
+    third(5, 1500000),
+    second(6, 30000000),
+    first(7, 2000000000),
+    ;
+    private int correctCount;
+    private int price;
+
+    Rank(int correctCount, int price) {
+        this.correctCount = correctCount;
+        this.price = price;
+    }
 }
