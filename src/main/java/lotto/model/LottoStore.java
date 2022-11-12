ae2f263f0d03fee15c34dc8813dc8846b3bae094
marinesnow34 <m4rinesnow34@gmail.com>
2022-11-13 00:04:47 +0900

    feat: 생성된 모든 로또 출력

--- src/main/java/lotto/model/LottoStore.java
+++ src/main/java/lotto/model/LottoStore.java
@@ -8,6 +8,7 @@ public class LottoStore {
     private static final int LOTTO_SIZE = 6;
 
     private List<Lotto> lottos;
+    private int lottoCount;
 
     public LottoStore(int count) {
         List<Lotto> lottos = new ArrayList<>();
@@ -15,10 +16,17 @@ public class LottoStore {
             lottos.add(makeLotto());
         }
         this.lottos = lottos;
+        this.lottoCount = count;
     }
 
     private Lotto makeLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return new Lotto(numbers);
+    }
+
+    private void printLottos() {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
     }
 }
