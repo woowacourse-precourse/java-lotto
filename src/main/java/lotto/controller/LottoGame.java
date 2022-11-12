93e05d39244b1de1c24000f1e76c072fd530f74a
JunTaeINC <juntea3523@naver.com>
2022-11-13 03:32:11 +0900

    feat : (controller.LottoGame) 로또 당첨 통계 출력 기능 구현

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -2,6 +2,10 @@ package lotto.controller;
 
 import java.util.List;
 
+import lotto.enums.LottoRanking;
+
+import static lotto.enums.LottoRanking.*;
+
 public class LottoGame {
 
     public static Integer compareLottoNumber(List<Integer> user, List<Integer> computer, Integer bonusNumber) {
@@ -18,5 +22,32 @@ public class LottoGame {
         return sameNumber;
     }
 
+    public static void statisticsInput(Integer sameNumber) {
+        switch (sameNumber) {
+            case 3:
+                FIFTH.count++;
+                break;
+            case 4:
+                FOURTH.count++;
+                break;
+            case 5:
+                THIRD.count++;
+                break;
+            case 7:
+                SECOND.count++;
+                break;
+            case 6:
+                FIRST.count++;
+                break;
+        }
+
+    }
+
+    public static void statisticalOutput() {
+        LottoRanking[] grades = LottoRanking.values();
+        for (LottoRanking grade : grades) {
+            System.out.println(grade.getValue());
+        }
+    }
 
 }
