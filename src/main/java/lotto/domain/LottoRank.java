57898416f989606be563fd57832f97c376545295
김형기 <khk211113@naver.com>
2022-11-13 00:36:34 +0900

    chore: LottoRank enum에 등수도 함께 기록

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,18 +1,20 @@
 package lotto.domain;
 
 public enum LottoRank {
-    FIRST("6개 일치", 2000000000),
-    SECOND("5개 일치, 보너스 볼 일치", 30000000),
-    THIRD("5개 일치", 1500000),
-    FOURTH("4개 일치", 50000),
-    FIFTH("3개 일치", 5000),
-    LOSE("꽝", 0);
+    FIRST("6개 일치", 1, 2000000000),
+    SECOND("5개 일치, 보너스 볼 일치", 2, 30000000),
+    THIRD("5개 일치", 3, 1500000),
+    FOURTH("4개 일치", 4, 50000),
+    FIFTH("3개 일치", 5, 5000),
+    LOSE("꽝", 0, 0);
 
-    private String description;
-    private int prize;
+    private final String description;
+    private final int rank;
+    private final int prize;
 
-    LottoRank(String description, int prize) {
+    LottoRank(String description, int rank, int prize) {
         this.description = description;
+        this.rank = rank;
         this.prize = prize;
     }
 
@@ -22,5 +24,9 @@ public enum LottoRank {
 
     public int getPrize() {
         return this.prize;
+    }
+
+    public int getRank() {
+        return this.rank;
     }
 }
