8537d9000cfa931c706989ff483e5c5ae3221714
JUHA <khabh@naver.com>
2022-11-12 21:52:05 +0900

    feat(LotteryWheel): 오름차순으로 정렬된 로또 번호 생성

--- src/main/java/lotto/domain/LotteryWheel.java
+++ src/main/java/lotto/domain/LotteryWheel.java
@@ -1,25 +1,19 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import lotto.Lotto;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class LotteryWheel {
     private final static int MIN_LOTTO_NUMBER = 1;
     private final static int MAX_LOTTO_NUMBER = 45;
-    private final static int NUMBERS_COUNT = 7;
+    private final static int NUMBERS_COUNT = 6;
 
-    private final Lotto winningLotto;
-    private final int bonusNumber;
+    public List<Integer> generateLotteryNumbers() {
+        List<Integer> lotteryNumbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, NUMBERS_COUNT);
+        lotteryNumbers.sort(Comparator.naturalOrder());
 
-    public LotteryWheel() {
-        List<Integer> lotteryNumbers = pickWinningLotteryNumbers();
-        this.bonusNumber = lotteryNumbers.remove(NUMBERS_COUNT);
-        this.winningLotto = new Lotto(lotteryNumbers);
-    }
-
-    private List<Integer> pickWinningLotteryNumbers() {
-        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, NUMBERS_COUNT);
+        return lotteryNumbers;
     }
 }
