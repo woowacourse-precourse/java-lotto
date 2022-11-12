3922a5ab93c589f135d62d132df449b913cab284
yj2980 <gum0945@naver.com>
2022-11-12 23:34:29 +0900

    fix : 예외 처리 수정

--- src/main/java/lotto/ExceptionHandler.java
+++ src/main/java/lotto/ExceptionHandler.java
@@ -5,16 +5,19 @@ import java.util.List;
 public class ExceptionHandler {
 
 	public static void checkInput(List<String> money) {
-		for (int i = 0; i < money.size(); i++) {
+		if (money.size() != 6) {
+			throw new IllegalArgumentException(Notice.ERROR.getNoticeMessage() + "쉼표로 구분해 주세요");
+		}
+		for (String s : money) {
 
-			if (!isNumeric(money.get(i))) {
+			if (!isNumeric(s)) {
 				throw new IllegalArgumentException(Notice.ERROR.getNoticeMessage() + "숫자만 입력해주세요");
 			}
 
-			int num = Integer.parseInt(money.get(i));
+			int num = Integer.parseInt(s);
 
 			if (num < 0 || num > 45) {
-				throw new IllegalArgumentException(Notice.ERROR.getNoticeMessage() +" 1 ~ 45 사이의 숫자만 입력해 주세요");
+				throw new IllegalArgumentException(Notice.ERROR.getNoticeMessage() + " 1 ~ 45 사이의 숫자만 입력해 주세요");
 			}
 		}
 
@@ -24,21 +27,13 @@ public class ExceptionHandler {
 		return str.chars().allMatch(Character :: isDigit);
 	}
 
-	public static void checkSpilt(String numbers) {
-		for (int i = 1; i < numbers.length(); i += 2) {
-			if (numbers.charAt(i) != ',') {
-				throw new IllegalArgumentException("[ERROR] 쉼표로 숫자를 구분해 주세요.");
-			}
-		}
-	}
-
 	public static void checkBonus(String bonus) {
-		if (bonus.length() < 0 || bonus.length() > 2) {
-			throw new IllegalArgumentException("[ERROR] 1개만 입력해 주세요.");
+		if (bonus.length() > 2) {
+			throw new IllegalArgumentException(Notice.ERROR.getNoticeMessage() +"1개만 입력해 주세요.");
 		}
 
 		if (Integer.parseInt(bonus) < 1 || Integer.parseInt(bonus) > 45) {
-			throw new IllegalArgumentException("[ERROR] 1 ~ 45 사이의 숫자를 입력해 주세요.");
+			throw new IllegalArgumentException(Notice.ERROR.getNoticeMessage() + "1 ~ 45 사이의 숫자를 입력해 주세요.");
 		}
 	}
 }
