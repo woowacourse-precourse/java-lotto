05667241ee2ec12fbeb1cec31eb489184d7e61af
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-12 16:05:49 +0900

    feat: MatchLotteryService 같은 개수를 enum타입의 랭킹으로 전환해주는 기능 구현

--- src/main/java/lotto/service/MatchLotteryService.java
+++ src/main/java/lotto/service/MatchLotteryService.java
@@ -7,7 +7,10 @@ import lotto.model.Player;
 
 public class MatchLotteryService {
 
+    private static final int THREE_MATCH = 3;
+    private static final int FOUR_MATCH = 4;
     private static final int FIVE_MATCH = 5;
+    private static final int SIX_MATCH = 6;
 
     private final Player player;
     private final Computer computer;
@@ -23,32 +26,16 @@ public class MatchLotteryService {
         return (int)lottoEach.getNumbers().stream().map( s ->computer.getComputerNumber().getNumbers().contains(s)).count();
     }
 
-    private void convertCountToRanking(){
-
-    }
-
-    private boolean isFiveMatch(int equalNumber){
-        return equalNumber == FIVE_MATCH;
+    private Ranking convertCountToRanking(Lotto lottoEach, int equalCount){
+        if(equalCount == THREE_MATCH) return Ranking.THREE_MATCH;
+        if(equalCount == FOUR_MATCH) return Ranking.FOUR_MATCH;
+        if(equalCount == FIVE_MATCH) return separateByBonusNumber(lottoEach);
+        if(equalCount == SIX_MATCH) return Ranking.SIX_MATCH;
+        return Ranking.NOTHING;
     }
 
     private Ranking separateByBonusNumber(Lotto lottoEach){
         if(lottoEach.getNumbers().contains(computer.getBonusNumber())) return Ranking.FIVE_MATCH_WITH_BONUS;
         return Ranking.FIVE_MATCH;
-    }
-
-    public void makePlayerLottoByPayment(String payment){
-
-    }
-
-    public void saveComputerNumber(String lottoNumber, String bonusNumber){
-
-    }
-
-    public Player getPlayer(){
-        return player;
-    }
-
-    public Computer getComputer(){
-        return computer;
     }
 }
