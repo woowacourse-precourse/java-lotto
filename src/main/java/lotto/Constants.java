1d0a363519dd40a33bb7e3a67a020b302f628e04
wjh2012 <wjh2012@naver.com>
2022-11-12 12:31:21 +0900

    feat(Constants): 로또 숫자 범위 상수 추가

--- src/main/java/lotto/Constants.java
+++
@@ -1,9 +0,0 @@
-package lotto;
-
-public final class Constants {
-    private Constants() {
-    }
-
-    public static final int LOTTO_SIZE = 6;
-
-}
---
+++ src/main/java/lotto/config/Constants.java
@@ -0,0 +1,11 @@
+package lotto.config;
+
+public final class Constants {
+    private Constants() {
+    }
+
+    public static final int LOTTO_SIZE = 6;
+
+    public static final int LOTTO_NUMBER_START_INCLUSIVE = 1;
+    public static final int LOTTO_NUMBER_END_INCLUSIVE = 45;
+}
