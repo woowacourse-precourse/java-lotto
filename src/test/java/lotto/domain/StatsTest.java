a24930353c7db3f5f8ef101223c0a473f22a29b8
sangil <gsl0505@naver.com>
2022-11-12 21:23:06 +0900

    feat(StatsTest): 수익률 계산이 일치하는지 확인해주는 테스트 기능 추가

--- src/test/java/lotto/domain/StatsTest.java
+++ src/test/java/lotto/domain/StatsTest.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import lotto.service.MachineSystem;
 
 class StatsTest {
 
@@ -16,5 +18,20 @@ class StatsTest {
 		stats.add(Rank.RANK_2ND);
 		int sum = Rank.RANK_1ST.getReward() + Rank.RANK_2ND.getReward();
 		Assertions.assertThat(stats.totalReward()).isEqualTo(sum);
+	}
+
+	@Test
+	@DisplayName("수익률 계산해주는 테스트")
+	void calculateYieldTest() {
+		Stats stats = Stats.initStats();
+		stats.add(Rank.RANK_1ST);
+		stats.add(Rank.RANK_2ND);
+		String amount = "10000";
+		MachineSystem machineSystem = new MachineSystem(new Buyer(amount),
+			new WinningLotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+
+		double totalReward = Rank.RANK_1ST.getReward() + Rank.RANK_2ND.getReward();
+		double yield = totalReward / Double.parseDouble(amount) * 100;
+		Assertions.assertThat(machineSystem.yield(stats.totalReward())).isEqualTo(yield);
 	}
 }
