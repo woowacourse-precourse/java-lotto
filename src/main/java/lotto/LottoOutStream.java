d11a3ac6c025ea5f7e0270281c18e1a718e034c4
kanghyki <kanghyki@gmail.com>
2022-11-13 04:17:46 +0900

    feat(LottoOutStream): 당첨 통계 출력 메서드 구현

--- src/main/java/lotto/LottoOutStream.java
+++ src/main/java/lotto/LottoOutStream.java
@@ -1,4 +1,15 @@
 package lotto;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class LottoOutStream {
+    public static void outStreamLottoResult (int[] matches) {
+        System.out.print("당첨 통계\n---\n");
+        System.out.print("3개 일치 (5,000원) - " + matches[LottoGame._fifth_place] + "개\n");
+        System.out.print("4개 일치 (50,000원) - " + matches[LottoGame._fourth_place] + "개\n");
+        System.out.print("5개 일치 (1,500,000원) - " + matches[LottoGame._third_place] + "개\n");
+        System.out.print("5개 일치, 보너스 볼 일치 (30,000,000원) - " + matches[LottoGame._second_place] + "개\n");
+        System.out.print("6개 일치 (2,000,000,000원) - " + matches[LottoGame._first_place] + "개\n");
+    }
 }
