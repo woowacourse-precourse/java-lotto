745201981422a311ea2e7fa63a022a951405e074
HyunggiPark <phg9587@naver.com>
2022-11-12 15:39:54 +0900

    refact(Application): 로또 구입 금액 입력 메서드명 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,7 +7,7 @@ public class Application {
         // TODO: 프로그램 구현
     }
 
-    public static int askPurchaseAmount() {
+    public static int askPurchaseMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         String userInput = readLine();
 
