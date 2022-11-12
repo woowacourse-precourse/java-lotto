ff90043f16b4a1746a50fd72b12b3070d869dade
sieun <sieun960513@naver.com>
2022-11-12 20:36:37 +0900

    [Validation] 로또 번호 범위 확인 로직

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -4,4 +4,8 @@ public class Validation {
     public static boolean thousandUnit(int num) {
         return num%1000 == 0;
     }
+
+    public static boolean lottoNumRange(int num) {
+        return 1 <= num && num <= 45;
+    }
 }
