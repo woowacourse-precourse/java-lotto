1de7d594c5c2d1815a8932a52df27aff1070c957
juhwang <juhong4930@naver.com>
2022-11-12 16:50:51 +0900

    feat(LottoInformation): return result string

--- src/main/java/lotto/LottoInformation.java
+++ src/main/java/lotto/LottoInformation.java
@@ -3,12 +3,12 @@ package lotto;
 import java.util.Arrays;
 
 public enum LottoInformation {
-    FIRST(6, false, 2_000_000_000L),
-    SECOND(5, true, 30_000_000L),
-    THIRD(5, false, 1_500_000L),
-    FOURTH(4, false, 50_000L),
+    OTHER(0, false, 0L),
     FIFTH(3, false, 5_000L),
-    OTHER(0, false, 0L);
+    FOURTH(4, false, 50_000L),
+    THIRD(5, false, 1_500_000L),
+    SECOND(5, true, 30_000_000L),
+    FIRST(6, false, 2_000_000_000L);
 
     private final int matchNumber;
     private final boolean matchBonus;
@@ -21,7 +21,24 @@ public enum LottoInformation {
     }
 
     public long getWinnings(int count) {
-        return this.prizeMoney*count;
+        return this.prizeMoney * count;
+    }
+
+    public String getInformation(int count) {
+        if (validateSecond()) {
+            String RESULT_RANK_SECOND_ALERT = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+            return String.format(RESULT_RANK_SECOND_ALERT, count);
+        }
+        String RESULT_RANK_ALERT = "%d개 일치 (%,d원) - %d개";
+        return String.format(RESULT_RANK_ALERT, this.matchNumber, this.prizeMoney, count);
+    }
+
+    private boolean validateSecond() {
+        return this.matchNumber == 5 && this.matchBonus;
+    }
+
+    public int getMatchNumber() {
+        return this.matchNumber;
     }
 
     public static LottoInformation makeLottoInformation(int matchNumber, boolean matchBonus) {
