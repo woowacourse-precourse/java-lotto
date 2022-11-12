2cafaa0107e71928a15b55f66384a62d6f484cdc
yubin <dbqls2821@gmail.com>
2022-11-12 16:00:12 +0900

    feat(DuplicationValidator): 당첨 번호와 보너스 번호에 중복이 있을 때 예외를 던지는 메소드 추가

--- src/main/java/lotto/domain/io/DuplicationValidator.java
+++ src/main/java/lotto/domain/io/DuplicationValidator.java
@@ -5,8 +5,15 @@ import lotto.domain.constant.ExceptionMessage;
 import java.util.List;
 
 public class DuplicationValidator {
+
   void validate(List<Integer> input){
     if (input.size() != input.stream().distinct().count()) {
+      throw new IllegalArgumentException(ExceptionMessage.INPUT_IS_DUPLICATED.getMessage());
+    }
+  }
+
+  void validate(List<Integer> input, int bonusNumber) {
+    if (input.contains(bonusNumber)) {
       throw new IllegalArgumentException(ExceptionMessage.INPUT_IS_DUPLICATED.getMessage());
     }
   }
