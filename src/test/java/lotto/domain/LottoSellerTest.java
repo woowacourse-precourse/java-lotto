d6fd1e162776a651b3d393fbdd302de5b5bcd5ee
5nam <ohnam90@gmail.com>
2022-11-12 14:26:34 +0900

    test(refactor): LottoSeller 기능 이용을 위해 생성자 코드 추가

--- src/test/java/lotto/domain/LottoSellerTest.java
+++ src/test/java/lotto/domain/LottoSellerTest.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LottoSellerTest {
+    private static final LottoSeller lottoSeller = new LottoSeller();
     @DisplayName("구매 금액만큼 발급 개수 반환한다.")
     @Test
     void 구매_금액만큼_로또_개수() {
         int money = 15_000;
-        int pages = lottoSeller.getSellPages();
+        int pages = lottoSeller.getSellPages(money);
         assertThat(pages).isEqualTo(15);
     }
 }
