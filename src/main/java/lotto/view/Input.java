74c61b9b9abd1308b63dd918d94af59120fae644
Kid-Chang <pch3310@gmail.com>
2022-11-12 23:17:54 +0900

    fix(Input): 유저의 정보를 받아내는 유틸을 개선
    1. static으로 변경
    2. List<Integer> -> List<String>으로 변경

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,21 +1,28 @@
 package lotto.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
-    public int getYourMoney() {
+    public static int getYourMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         return Integer.parseInt(Console.readLine());
     }
 
-    public String getAnswerNumbers() {
+    public static List<String> getAnswerNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        return Console.readLine();
+        String value = Console.readLine();
+
+        return Stream.of(value.split(","))
+            .collect(Collectors.toList());
     }
 
-    public int getBonusNumber() {
+    public static String getBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 }
