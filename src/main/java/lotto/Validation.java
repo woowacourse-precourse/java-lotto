e7f876762973c40a4bc364fc5d447834f0407682
lacram <chemin9898@gmail.com>
2022-11-12 21:32:03 +0900

    feat: 1000원 단위로 입력했는지 검증

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -6,8 +6,15 @@ public class Validation {
     public static void isDigit(String numbers) {
         for (char number : numbers.toCharArray()) {
             if (!Character.isDigit(number)) {
-                throw new IllegalArgumentException("숫자만 입력해주세요.");
+                throw new IllegalArgumentException("숫자만 입력해 주세요.");
             }
+        }
+    }
+    // 1000원 단위로 입력했는지 검증
+    public static void isThousandUnit(String purchaseAmount) {
+        long money = Long.parseLong(purchaseAmount);
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("1000원 단위로 입력해 주세요.");
         }
     }
 
