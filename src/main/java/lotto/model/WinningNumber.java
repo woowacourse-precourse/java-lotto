42104d41b273027430ba1f00bd1ab3018e208645
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-12 17:40:37 +0900

    type:feat 당첨 번호를 관리하는 클래스 추가

---
+++ src/main/java/lotto/model/WinningNumber.java
@@ -0,0 +1,10 @@
+package lotto.model;
+
+import java.util.List;
+
+public class WinningNumber {
+    private final List<Integer> numbers = List.of(0,0,0,0,0,0);
+
+    public WinningNumber(String numbers) {
+    }
+}
