0c1a014ab93a1a490379297dfee74518e08fabe7
hossang <zkzk104@naver.com>
2022-11-12 11:09:14 +0900

    feat: 발행한 로또 수량 및 번호 출력하기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,19 @@
 package lotto.view;
 
+import java.util.List;
+
 public class InputView {
     private static final String PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
 
     public void printRequest() {
         System.out.println(PURCHASE_AMOUNT);
     }
+
+    public void printLottos(List<List<Integer>> lottos) {
+        System.out.println();
+        for (List<Integer> lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
+
 }
