242c00b183865f2ddb57ea16003cc46f8548bbc5
ykmxxi <ykmxxi97@gmail.com>
2022-11-12 10:33:58 +0900

    feat: 로또 번호와 당첨 번호를 비교하는 기능 skeleton code 구현

---
+++ src/main/java/lotto/LottoCheck.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoCheck {
+
+    public void checkWinning(List<Integer> winnings, List<Integer> numbers) {
+    }
+
+    public void checkBonus(List<Integer> winnings, List<Integer> numbers) {
+    }
+
+    public void checkRanking(int winningCount, int bonusCount) {
+    }
+}
