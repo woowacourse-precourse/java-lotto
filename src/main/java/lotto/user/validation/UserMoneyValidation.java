95ff71672333cf2497d786e5c13450f9d7ddd9c7
kimminsu <xjfcnfw3@naver.com>
2022-11-12 21:38:19 +0900

    feat: [예외처리 추가] 입력 금액은 반드시 숫자로 입력받아야 한다.

--- src/main/java/lotto/user/validation/UserMoneyValidation.java
+++ src/main/java/lotto/user/validation/UserMoneyValidation.java
@@ -1,17 +1,26 @@
 package lotto.user.validation;
 
 import lotto.domain.LottoEnum;
+import lotto.domain.validation.LottoBonusValidation;
 
 public class UserMoneyValidation {
     private static final int REMAINDER_ZERO = 0;
+    private static final String NUMBER_REGEXP = "^[0-9]+$";
 
     public static void validate(String userAmount) {
+        validateOnlyNumber(userAmount);
         validateUnits(userAmount);
     }
 
     private static void validateUnits(String userAmount) {
         if ((Integer.parseInt(userAmount) % LottoEnum.PRICE.getValue()) != REMAINDER_ZERO) {
             throw new IllegalArgumentException(ExceptionMessage.UNITS.toString());
+        }
+    }
+
+    private static void validateOnlyNumber(String userAmount) {
+        if (!userAmount.matches(NUMBER_REGEXP)) {
+            throw new IllegalArgumentException(ExceptionMessage.ONLY_NUMBER.toString());
         }
     }
 }
