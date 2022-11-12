16c75ecb5646ec5c4eabcc8b98e9c00f60c11ae6
Park shinyeong <syngh503@gmail.com>
2022-11-12 21:12:56 +0900

    Feat(RankType): setRankType 메소드 추가
    맞춘 개수와 보너스 번호가 포함되어 있는지 여부를 매개변수로 받아 그에 맞는 RankType을 리턴해주는 setRankType 메소드를 생성하였습니다.
    
    Relate to: #3

--- src/main/java/lotto/type/RankType.java
+++ src/main/java/lotto/type/RankType.java
@@ -2,17 +2,18 @@ package lotto.type;
 
 public enum RankType {
 
-    FIRST(200000000, "1등", "6개 일치"),
-    SECOND(30000000, "2등", "5개 일치, 보너스 볼 일치"),
-    THIRD(1500000, "3등", "5개 일치"),
-    FOURTH(50000, "4등", "4개 일치"),
-    FIFTH(5000, "5등", "3개 일치");
+    FIRST(200000000, 1, "6개 일치"),
+    SECOND(30000000, 2, "5개 일치, 보너스 볼 일치"),
+    THIRD(1500000, 3, "5개 일치"),
+    FOURTH(50000, 4, "4개 일치"),
+    FIFTH(5000, 5, "3개 일치"),
+    NOTHING(0, 0, "일치하는 수가 없음");
 
     private int prizeMoney;
-    private String rank;
+    private int rank;
     private String result;
 
-    RankType(int prizeMoney, String rank, String result) {
+    RankType(int prizeMoney, int rank, String result) {
         this.prizeMoney = prizeMoney;
         this.rank = rank;
         this.result = result;
@@ -22,11 +23,30 @@ public enum RankType {
         return prizeMoney;
     }
 
-    public String getRank() {
+    public Integer getRank() {
         return rank;
     }
 
     public String getResult() {
         return result;
+    }
+
+    public RankType setRankType(int count, boolean isBonusBallContain) {
+        if(count == 6) {
+            return RankType.FIRST;
+        }
+        if (count == 5 && isBonusBallContain) {
+            return RankType.SECOND;
+        }
+        if (count == 5) {
+            return RankType.THIRD;
+        }
+        if(count == 4) {
+            return RankType.FOURTH;
+        }
+        if(count == 3) {
+            return RankType.FIFTH;
+        }
+        return RankType.NOTHING;
     }
 }
