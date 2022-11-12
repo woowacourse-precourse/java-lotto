56419d32a0c312675e7c88f7d2107c521c38d1cb
5nam <ohnam90@gmail.com>
2022-11-12 15:08:34 +0900

    refactor: 로또 번호 입력 받은 후 ',' 제거하는 기능 분리

--- src/main/java/lotto/domain/LottoDesignator.java
+++ src/main/java/lotto/domain/LottoDesignator.java
@@ -8,10 +8,14 @@ public class LottoDesignator {
 
     public void checkOnlyNumber(String input) {
         String lotto = Arrays
-                .stream(input.split(","))
+                .stream(removeComma(input))
                 .collect(Collectors.joining());
         if(!lotto.matches(INT_REGEX)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자만 입력 가능합니다.(구분자 쉼표만 가능)");
         }
+    }
+
+    private String[] removeComma(String input) {
+        return input.split(",");
     }
 }
