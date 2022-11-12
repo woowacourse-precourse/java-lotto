1227b512a47a49aa479f12348a01dbf0a81667c6
June <68291395+june-777@users.noreply.github.com>
2022-11-12 13:48:30 +0900

    refactor(로또 구입금액 입력): 전체 유효성 검증 메소드 콜 하는 validate 메소드 생성, 내부용 메소드 private으로 변경
    코드 가독성을 위해 validate 메소드에서 전체 유효성 검증 메소드 콜

--- src/main/java/utils/InputLottoPurchaseAmount.java
+++ src/main/java/utils/InputLottoPurchaseAmount.java
@@ -7,27 +7,34 @@ public class InputLottoPurchaseAmount {
 	public static int readLottoPurchaseAmount() {
 		System.out.println("구입금액을 입력해 주세요.");
 		String purchaseAmount = Console.readLine();
+		int purchaseAmountResult = validate(purchaseAmount);
 
-		validateNumeric(purchaseAmount);
-
-		int purchaseAmountResult = Integer.parseInt(purchaseAmount);
 		return purchaseAmountResult;
 	}
 
-	public static void validateNumeric(String purchaseAmount) {
+	private static int validate(String purchaseAmount) {
+		validateNumeric(purchaseAmount);
+		int purchaseAmountResult = Integer.parseInt(purchaseAmount);
+		validatePurchaseAmountRange(purchaseAmountResult);
+		validatePurchaseAmountUnit(purchaseAmountResult);
+
+		return purchaseAmountResult;
+	}
+
+	private static void validateNumeric(String purchaseAmount) {
 		String regex = "[+-]?\\d*(\\.\\d+)?";
 		if (!purchaseAmount.matches(regex)) {
 			throw new IllegalArgumentException("[ERROR] 로또 구입금액은 숫자만 입력해야 합니다.");
 		}
 	}
 
-	public static void validatePurchaseAmountRange(int purchaseAmoutResult) {
+	private static void validatePurchaseAmountRange(int purchaseAmoutResult) {
 		if (purchaseAmoutResult < 1000) {
 			throw new IllegalArgumentException("[ERROR] 로또 최소 구입금액은 1000원 입니다.");
 		}
 	}
 
-	public static void validatePurchaseAmountUnit(int purchaseAmoutResult) {
+	private static void validatePurchaseAmountUnit(int purchaseAmoutResult) {
 		if (purchaseAmoutResult % 1000 != 0) {
 			throw new IllegalArgumentException("[ERROR] 로또 구입금액은 1000원 단위여야 합니다.");
 		}
