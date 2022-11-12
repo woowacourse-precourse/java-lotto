569f63cd65d0312030f0dc41d52089b0fa25cacd
23Yong <1239824@naver.com>
2022-11-12 16:46:49 +0900

    test(Validator): 당첨 번호 검증 테스트 코드 작성
    - 올바른 값이 입력될 경우의 테스트 코드를 작성한다
    - 올바르지 않은 값이 입력될 경우 `IllegalArgumentException` 이 발생하는지 검증한다
    - 예외 메시지가 `[ERROR]` 로 시작하는지 검증한다

--- src/test/java/utils/ValidatorTest.java
+++ src/test/java/utils/ValidatorTest.java
@@ -59,4 +59,52 @@ class ValidatorTest {
             );
         }
     }
+
+    @DisplayName("사용자로부터 당첨 번호를 입력받으면 ")
+    @Nested
+    class WinNumbersTest {
+
+        @DisplayName("올바른 값으로 인해 검증에 통과한다.")
+        @Test
+        void 올바른_당첨_번호를_입력받아_검증에_통과한다() {
+            // given
+
+            // when & then
+            assertAll(
+                    () -> assertThatCode(() -> Validator.checkWinsNumberIsValid("1,2,3,4,5,6"))
+                            .doesNotThrowAnyException(),
+                    () -> assertThatCode(() -> Validator.checkWinsNumberIsValid("13,24,36,45,1,5"))
+                            .doesNotThrowAnyException()
+            );
+        }
+
+        @DisplayName("잘못된 값으로 인해 예외가 던져진다.")
+        @Test
+        void 잘못된_당첨_번호를_입력받아_검증에_통과하지_못한다() {
+            // given
+
+            // when & then
+            IllegalArgumentException underRangeEx = assertThrows(
+                    IllegalArgumentException.class,
+                    () -> Validator.checkWinsNumberIsValid("0,1,2,3,4,5")
+            );
+            IllegalArgumentException overRangeEx = assertThrows(
+                    IllegalArgumentException.class,
+                    () -> Validator.checkWinsNumberIsValid("1,2,3,4,5,46")
+            );
+            IllegalArgumentException includeNotNumberEx = assertThrows(
+                    IllegalArgumentException.class,
+                    () -> Validator.checkWinsNumberIsValid("0,1,2,3,4,5")
+            );
+            IllegalArgumentException duplicatedEx = assertThrows(
+                    IllegalArgumentException.class,
+                    () -> Validator.checkWinsNumberIsValid("1,2,3,4,5,5")
+            );
+
+            assertThat(underRangeEx.getMessage()).startsWith("[ERROR]");
+            assertThat(overRangeEx.getMessage()).startsWith("[ERROR]");
+            assertThat(includeNotNumberEx.getMessage()).startsWith("[ERROR]");
+            assertThat(duplicatedEx.getMessage()).startsWith("[ERROR]");
+        }
+    }
 }
