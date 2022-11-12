aa57377df6f01dda5b2948b712806a2cfc5b77f0
ChanghyunKim <variety82p@gamil.com>
2022-11-12 20:05:27 +0900

    feat(LottoController) : lottoListView 연동

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,8 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoTickets;
 import lotto.domain.Validator;
 import lotto.view.View;
 
@@ -8,12 +11,18 @@ public class LottoController {
     private static final Validator  validator= new Validator();
     public void lottoGame(){
         String input = view.startLottoView();
+
         try{
         validator.validateInputAmount(input);
         }catch(IllegalArgumentException e){
             System.out.println(e.getMessage());
             return;
         }
+
+        int lottoCount  = Integer.parseInt(input) / 1000;
+        LottoTickets lottoTickets = new LottoTickets(lottoCount);
+
+        view.lottoListView(lottoCount, lottoTickets);
     }
 
 }
