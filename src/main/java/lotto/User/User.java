d96df087f0c0cee1f0c5967adb2db0cb02b56514
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-13 02:48:02 +0900

    feat(User/buyLottos): buyLottos 함수 작성

--- src/main/java/lotto/User/User.java
+++ src/main/java/lotto/User/User.java
@@ -1,5 +1,6 @@
 package lotto.User;
 
+import lotto.BoughtLotto;
 import lotto.Lotto;
 
 import java.util.ArrayList;
@@ -13,10 +14,18 @@ public class User {
     }
 
     public void buyLottos(int money) {
-
+        if (!validateMoney(money)) {
+            System.out.println(money);
+           throw new IllegalArgumentException("금액을 잘못 입력하셨습니다.");
+        }
+        int countOfBuyLotto = money / 1000;
+        for (int i = 0; i < countOfBuyLotto; i++) {
+            lottos.add(new BoughtLotto());
+        }
     }
 
     private boolean validateMoney(int money) {
+        System.out.println(money % 1000 == 0);
         return money % 1000 == 0;
     }
 
