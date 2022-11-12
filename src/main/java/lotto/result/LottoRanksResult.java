c191fbdfdea8798bf05a88b17e645a9ba0936bac
kimminsu <xjfcnfw3@naver.com>
2022-11-12 19:47:50 +0900

    refactor: 로또 결과 객체에서 수익률, 등수 결과 객체 분리

---
+++ src/main/java/lotto/result/LottoRanksResult.java
@@ -0,0 +1,19 @@
+package lotto.result;
+
+import java.util.List;
+import lotto.GameMessage;
+import lotto.domain.Rank;
+
+public class LottoRanksResult {
+    public static void showRanksStats(List<Integer> lottoRanks) {
+        for (Rank rank : Rank.values()) {
+            showRank(lottoRanks, rank);
+        }
+    }
+
+    private static void showRank(List<Integer> lottoRanks, Rank rank) {
+        if (rank != Rank.ZERO) {
+            System.out.println(rank.toString() + lottoRanks.get(rank.getRank()) + GameMessage.RESULT_RANK_UNITS);
+        }
+    }
+}
---
+++ src/main/java/lotto/result/LottoYieldResult.java
@@ -0,0 +1,28 @@
+package lotto.result;
+
+import java.util.List;
+import lotto.GameMessage;
+import lotto.domain.Rank;
+
+public class LottoYieldResult {
+    private static final String YIELD_FORMAT = "%.1f";
+    private static final int PERCENTAGE = 100;
+
+    public static void showYield(List<Integer> lottoRanks, int lottoPrice) {
+        System.out.print(GameMessage.FRONT_YIELD_MESSAGE);
+
+        System.out.print(calculateYield(lottoRanks, lottoPrice));
+
+        System.out.println(GameMessage.BACK_YIELD_MESSAGE);
+    }
+
+    private static String calculateYield(List<Integer> lottoRanks, int lottoPrice) {
+        float prizeAmount = 0;
+        String resultYield;
+        for (Rank rank : Rank.values()) {
+            prizeAmount += lottoRanks.get(rank.getRank()) * rank.getMoney();
+        }
+        resultYield = String.format(YIELD_FORMAT, (prizeAmount / lottoPrice) * PERCENTAGE);
+        return resultYield;
+    }
+}
