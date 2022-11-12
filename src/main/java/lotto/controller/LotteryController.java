a59451869c209557dba83788d277fddbc5d11e35
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-12 16:26:24 +0900

    feat: LotteryController 클래스 생성

---
+++ src/main/java/lotto/controller/LotteryController.java
@@ -0,0 +1,52 @@
+package lotto.controller;
+
+import lotto.model.Computer;
+import lotto.model.Player;
+
+public class LotteryController {
+
+    Player player = new Player();
+    Computer computer = new Computer();
+
+    public void createPlayer(){}
+
+    private String takePlayersMoney(){
+        return "";
+    }
+
+    private void makeLottoForPlayer(){
+
+    }
+
+    private void giveLottoToPlayer(){
+
+    }
+
+    public void insertWinningNumbers(){
+
+    }
+
+    private String typeWinningLottery(){
+        return "";
+    }
+
+    private void insertWinningLottery(){
+
+    }
+
+    private String typeWinningBonusNumber(){
+        return "";
+    }
+
+    private void insertWinningBonusNumber(){
+
+    }
+
+    private void matchPlayerLottoWithWinningNumbers(){
+
+    }
+
+    private void showPlayersResult(){
+
+    }
+}
