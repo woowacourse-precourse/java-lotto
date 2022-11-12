94befbba3f391733e39e096dd2cc9ec71aadbba6
sxxgxxnz <tmddms2292@gmail.com>
2022-11-12 18:15:11 +0900

    feat : 구입할 로또 금액을 입력한다.

---
+++ src/main/java/lotto/Input.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class Input {
+    public static String input(){
+        String input = readLine();
+        return input;
+    }
+}
