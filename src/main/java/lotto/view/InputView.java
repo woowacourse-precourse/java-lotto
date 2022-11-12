957b6257493f1b3a8401eb7ffb02736151046325
Longstick0 <789456jang@naver.com>
2022-11-12 14:41:49 +0900

    feat: 사용자의 Input 값을 받을 view 메서드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,4 +6,22 @@ import static java.lang.Integer.parseInt;
 
 public class InputView {
 
+    private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUM = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUM = "보너스 번호를 입력해 주세요.";
+
+    private int inputAmount() {
+        System.out.println(INPUT_PURCHASE_AMOUNT);
+        return parseInt(Console.readLine());
+    }
+
+    private String inputWinningNum() {
+        System.out.println(INPUT_WINNING_NUM);
+        return Console.readLine();
+    }
+
+    private int inputBonusNum() {
+        System.out.println(INPUT_BONUS_NUM);
+        return parseInt(Console.readLine());
+    }
 }
