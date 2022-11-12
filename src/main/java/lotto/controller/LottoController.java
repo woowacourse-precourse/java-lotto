53c2198648cf2116eb74b816457ceee26c5fc284
peterysh <52229539+peterysh@users.noreply.github.com>
2022-11-12 21:59:16 +0900

    feat: add milestone in controller

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -10,5 +10,33 @@ public class LottoController {
         lottoService = new LottoService();
     }
 
+    public void run(){
+        //input price
 
+        //print guess number
+
+        //input Lotto number
+
+        //input bonus number
+
+        //print statistics
+    }
+
+    private String inputPrice(){
+        System.out.println(LottoControllerString.INPUT_PURCHASE_PRICE);
+        return "ok";
+    }
+
+    private void printGuessNumber(){
+
+    }
+    private String inputLottoNumber(){
+        return "ok";
+    }
+    private String inputBonusNumber(){
+        return "ok";
+    }
+    private void statistics(){
+
+    }
 }
