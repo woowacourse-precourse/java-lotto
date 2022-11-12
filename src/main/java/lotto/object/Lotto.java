6ffd6a06c883b6bfc0086a8da2ca460fdff2c578
leewoojin <zmfpdl64@naver.com>
2022-11-12 15:10:19 +0900

    Test: Lotto 객체 테스트를 위한 main 메소드 추가
    테스트 목적:
    1. 당첨 번호를 입력받아 정상적으로 동작하여 6자리가 생성되었는지 확인

--- src/main/java/lotto/object/Lotto.java
+++ src/main/java/lotto/object/Lotto.java
@@ -1,14 +1,16 @@
 package lotto.object;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.input_output.Input;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Lotto {
+public class Lotto extends Input {
     private final List<Integer> numbers;    //로또를 시작할 때 사용자에게 입력받는 값
 
     public Lotto(List<Integer> numbers) {
@@ -24,7 +26,14 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    /*
+    - 당첨 번호 입력 받아 numbers에 저장
+     */
 
+    public static void main(String[] args) {
+        Lotto lotto = new Lotto(Lotto.returnList());
+        System.out.println("numbers 사이즈: "+ lotto.numbers.size());
+    }
 
 
 }
