cca26cdf5c686fa343eb9fc2c85bca4a58694c55
정핸모 <gusah009@naver.com>
2022-11-12 10:07:21 +0900

    refactor: enum functional interface를 활용하여 더 간단하게 구현

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,28 +1,63 @@
 package lotto.domain;
 
+import static lotto.domain.Rank.Bonus.ALL;
+import static lotto.domain.Rank.Bonus.MATCH;
+import static lotto.domain.Rank.Bonus.MISMATCH;
+
+import java.util.function.Function;
+
 /**
  * 출력하고자 하는 순서대로 상수들을 선언해주셔야 올바른 순서로 출력됩니다.
  */
 public enum Rank {
-    FIFTH(5_000, "3개 일치"),
-    FOURTH(50_000, "4개 일치"),
-    THIRD(1_500_000, "5개 일치"),
-    SECOND(30_000_000, "5개 일치, 보너스 볼 일치"),
-    FIRST(2_000_000_000, "6개 일치");
+    FIFTH(3, ALL, 5_000, "3개 일치"),
+    FOURTH(4, ALL, 50_000, "4개 일치"),
+    THIRD(5, MISMATCH, 1_500_000, "5개 일치"),
+    SECOND(5, MATCH, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    FIRST(6, ALL, 2_000_000_000, "6개 일치");
 
-    final int winningPrice;
-    final String descriptionMessage;
+    private final int matchCount;
+    private final Bonus bonusMatch;
+    private final int winningPrice;
+    private final String descriptionMessage;
 
-    Rank(int winningPrice, String descriptionMessage) {
+    Rank(int matchCount, Bonus bonusMatch, int winningPrice, String descriptionMessage) {
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
         this.winningPrice = winningPrice;
         this.descriptionMessage = descriptionMessage;
     }
 
-    public int getWinningPrice() {
-        return winningPrice;
+    public static Rank getRank(int matchCount, boolean bonusMatch) {
+        for (Rank rank : Rank.values()) {
+            if (rank.matchCount == matchCount && rank.bonusMatch.isMatch(bonusMatch)) {
+                return rank;
+            }
+        }
+        return null;
     }
 
     public String getDescriptionMessage() {
-        return descriptionMessage;
+        return this.descriptionMessage;
+    }
+
+    public int getWinningPrice() {
+        return this.winningPrice;
+    }
+
+    enum Bonus {
+        MATCH(bonusMatch -> bonusMatch == true),
+        MISMATCH(bonusMatch -> bonusMatch == false),
+        ALL(bonusMatch -> true);
+
+        private final Function<Boolean, Boolean> matchFunction;
+
+        Bonus(Function<Boolean, Boolean> matchFunction) {
+            this.matchFunction = matchFunction;
+        }
+
+        boolean isMatch(boolean bonusMatch) {
+            return this.matchFunction.apply(bonusMatch);
+        }
     }
 }
--- src/main/java/lotto/repository/RankRepository.java
+++ src/main/java/lotto/repository/RankRepository.java
@@ -1,15 +1,6 @@
 package lotto.repository;
 
-import static lotto.domain.Rank.FIFTH;
-import static lotto.domain.Rank.FIRST;
-import static lotto.domain.Rank.FOURTH;
-import static lotto.domain.Rank.SECOND;
-import static lotto.domain.Rank.THIRD;
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import lotto.domain.Lotto;
 import lotto.domain.Rank;
@@ -17,28 +8,11 @@ import lotto.domain.WinningLotto;
 
 public class RankRepository {
 
-    static final Map<Score, Rank> ranks = new HashMap<>();
-
-    static {
-        insertRankData();
-    }
-
-    private static void insertRankData() {
-        ranks.put(Score.of(6, false), FIRST);
-        ranks.put(Score.of(6, true), FIRST);
-        ranks.put(Score.of(5, true), SECOND);
-        ranks.put(Score.of(5, false), THIRD);
-        ranks.put(Score.of(4, false), FOURTH);
-        ranks.put(Score.of(4, true), FOURTH);
-        ranks.put(Score.of(3, false), FIFTH);
-        ranks.put(Score.of(3, true), FIFTH);
-    }
-
     public static Optional<Rank> getRank(Lotto playerLotto, WinningLotto winningLotto) {
         List<Integer> playerNumbers = playerLotto.getNumbers();
         int matchedCount = getMatchedCount(winningLotto, playerNumbers);
         boolean bonusMatch = isBonusMatch(winningLotto, playerNumbers);
-        return Optional.ofNullable(ranks.get(Score.of(matchedCount, bonusMatch)));
+        return Optional.ofNullable(Rank.getRank(matchedCount, bonusMatch));
     }
 
     private static int getMatchedCount(WinningLotto winningLotto, List<Integer> playerNumbers) {
@@ -54,37 +28,5 @@ public class RankRepository {
 
     private static boolean isBonusMatch(WinningLotto winningLotto, List<Integer> playerNumbers) {
         return playerNumbers.contains(winningLotto.getBonusNumber());
-    }
-
-    private static class Score {
-
-        final int matchedCount;
-        final boolean bonusMatch;
-
-        private Score(int matchedCount, boolean bonusMatch) {
-            this.matchedCount = matchedCount;
-            this.bonusMatch = bonusMatch;
-        }
-
-        public static Score of(int matchedCount, boolean bonusMatch) {
-            return new Score(matchedCount, bonusMatch);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (!(o instanceof Score)) {
-                return false;
-            }
-            Score score = (Score) o;
-            return matchedCount == score.matchedCount && bonusMatch == score.bonusMatch;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(matchedCount, bonusMatch);
-        }
     }
 }
