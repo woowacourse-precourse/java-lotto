896b556458dd5b33e19950a95087422686cc9035
marinesnow34 <m4rinesnow34@gmail.com>
2022-11-12 19:38:30 +0900

    feat: 구매자 번호 입력

--- src/main/java/lotto/Buyer.java
+++ src/main/java/lotto/Buyer.java
@@ -1,19 +1,29 @@
 package lotto;
 
 import static lotto.view.BuyerView.inputMoney;
+import static lotto.view.BuyerView.inputNumbers;
+
+import java.util.List;
 
 public class Buyer {
     public int money;
+    public List<Integer> numbers;
 
     public Buyer() {
         this.money = getMoney();
+        this.numbers = getNumbers();
     }
 
-    public Buyer(int money) {
+    public Buyer(int money, List<Integer> numbers) {
         this.money = money;
+        this.numbers = numbers;
     }
 
-    public int getMoney() {
+    private int getMoney() {
         return inputMoney();
+    }
+
+    private List<Integer> getNumbers() {
+        return inputNumbers();
     }
 }
--- src/main/java/lotto/view/BuyerView.java
+++ src/main/java/lotto/view/BuyerView.java
@@ -2,11 +2,16 @@ package lotto.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class BuyerView {
     private static final String MESSAGE_INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private static final String MESSAGE_INPUT_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String ERROR_NOT_INT = "[ERROR] 숫자만 입력 가능합니다.";
+    private static final String ERROR_NUMBERS = "[ERROR] 숫자,숫자 형식을 맞춰주세요.";
     private static final String REGEX_INT = "^[0-9]+$";
     private static final int MIN_BUY_UNIT = 1_000;
 
@@ -15,6 +20,15 @@ public class BuyerView {
         String input = readLine();
         isValidMoneyInput(input);
         return Integer.parseInt(input);
+    }
+
+    public static List<Integer> inputNumbers() {
+        System.out.println(MESSAGE_INPUT_NUMBERS);
+        String input = readLine();
+        isValidNumbers(input);
+        return Arrays.stream(input.split(","))
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
     }
 
     private static void isValidMoneyInput(String input) {
@@ -29,5 +43,11 @@ public class BuyerView {
 
     private static boolean isBuyUnit(String input) {
         return Integer.parseInt(input) % MIN_BUY_UNIT == 0;
+    }
+
+    private static void isValidNumbers(String input) {
+        if (!Arrays.stream(input.split(",")).allMatch(BuyerView::isDigit)) {
+            throw new IllegalArgumentException(ERROR_NUMBERS);
+        }
     }
 }
