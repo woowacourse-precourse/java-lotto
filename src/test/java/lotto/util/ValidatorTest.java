6d09ea5b8f99208b19768027871eda0a76423aae
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-12 23:55:04 +0900

    test(ValidatorTest): 당첨 번호의 개수를 확인하는 기능 테스트

--- src/test/java/lotto/util/ValidatorTest.java
+++ src/test/java/lotto/util/ValidatorTest.java
@@ -2,6 +2,7 @@ package lotto.util;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -26,5 +27,26 @@ class ValidatorTest {
 
         // expected
         Validator.validateUnitStandard(purchaseAmount);
+    }
+
+    @DisplayName("당첨 번호 개수를 6개 입력했는지 확인 - 따르지 않음")
+    @Test
+    void validatePlayNumbersCountNotFollow() {
+        // given
+        List<Integer> playerNumbers = List.of(1, 2, 3, 4, 5);
+
+        // expected
+        assertThatThrownBy(() -> Validator.validatePlayNumbersCount(playerNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호 개수를 6개 입력했는지 확인 - 따름")
+    @Test
+    void validatePlayNumbersCountFollow() {
+        // given
+        List<Integer> playerNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        // expected
+        Validator.validatePlayNumbersCount(playerNumbers);
     }
 }
