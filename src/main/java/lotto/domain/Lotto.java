93c49686ea5e5498f48b5a8fe3953cef7c95dd57
bunsung92 <bunsung92@naver.com>
2022-11-12 11:57:12 +0900

    feat(lotto): 스켈레톤 코드 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import java.util.List;
+
+/**
+ * 제공된 Lotto 클래스를 활용해 구현해야 한다.
+ * Lotto에 매개 변수가 없는 생성자를 추가할 수 없다.
+ * numbers의 접근 제어자인 private을 변경할 수 없다.
+ * Lotto에 필드(인스턴스 변수)를 추가할 수 없다.
+ * Lotto의 패키지 변경은 가능하다.
+ */
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // TODO: 추가 기능 구현
+}
