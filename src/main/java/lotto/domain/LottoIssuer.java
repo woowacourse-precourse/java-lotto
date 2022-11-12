4b8f0e08c71678f91f679151393fa3bf7f9a73e3
3juhwan <13selfesteem91@naver.com>
2022-11-12 21:18:15 +0900

    feat(lotto): 로또 발행기 구현

--- src/main/java/lotto/domain/LottoIssuer.java
+++ src/main/java/lotto/domain/LottoIssuer.java
@@ -1,7 +1,23 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoIssuer {
-    public static Lotto issueLotto () {
-        return null;
+    public Lotto issueLotto() {
+        return new Lotto(randomNumberGenerator());
+    }
+
+    private List<Integer> randomNumberGenerator() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 6) {
+            int randomNumber = Randoms.pickNumberInRange(1, 45);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
     }
 }
