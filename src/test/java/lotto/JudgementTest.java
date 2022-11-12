3fb305216a29b5c04c45f3aa765910f936256786
NotableDeveloper <castlereal@naver.com>
2022-11-11 23:43:13 -0800

    <test> : 추첨 번호, 당첨 번호 비교에 대한 테스트 코드 생성 추첨 번호와 당첨 번호를 비교하는 테스트 코드 생성. 또, 두 번호를 비교하는 Judgement 클래스 생성.

---
+++ src/test/java/lotto/JudgementTest.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JudgementTest {
+    @DisplayName("추첨 번호와 당첨 번호가 정확하게 일치하는 경우")
+    @Test
+    void compareLotteryNumberAndLottoToSuccessful() {
+    }
+}
