b8240b8f157918ed49306a2d2ad003e227ed840f
ykmxxi <ykmxxi97@gmail.com>
2022-11-12 11:56:00 +0900

    feat: 로또 번호와 당첨 번호를 비교하는 기능 구현

--- src/main/java/lotto/LottoCheck.java
+++ src/main/java/lotto/LottoCheck.java
@@ -1,15 +1,42 @@
 package lotto;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class LottoCheck {
+    private static final int LOTTO_SIZE = 6;
 
-    public void checkWinning(List<Integer> winnings, List<Integer> numbers) {
+    public LottoCheck() {
     }
 
-    public void checkBonus(List<Integer> winnings, List<Integer> numbers) {
+    public int checkWinning(List<Integer> winnings, List<Integer> numbers) {
+        List<Integer> duplicate = numbers.stream()
+                .filter(number -> winnings.stream().noneMatch(Predicate.isEqual(number)))
+                .collect(Collectors.toList());
+        return LOTTO_SIZE - duplicate.size();
     }
 
-    public void checkRanking(int winningCount, int bonusCount) {
+    public boolean checkBonus(int bonus, List<Integer> numbers) {
+        return numbers.contains(bonus);
+    }
+
+    public String checkRanking(List<Integer> winnings, List<Integer> numbers, int bonus) {
+        if (checkWinning(winnings, numbers) == LOTTO_SIZE) {
+            return "FIRST";
+        }
+        if (checkWinning(winnings, numbers) == LOTTO_SIZE - 1 && checkBonus(bonus, numbers)) {
+            return "SECOND";
+        }
+        if (checkWinning(winnings, numbers) == LOTTO_SIZE - 1 && !checkBonus(bonus, numbers)) {
+            return "THIRD";
+        }
+        if (checkWinning(winnings, numbers) == LOTTO_SIZE - 2) {
+            return "FOURTH";
+        }
+        if (checkWinning(winnings, numbers) == LOTTO_SIZE - 3) {
+            return "FIFTH";
+        }
+        return null;
     }
 }
