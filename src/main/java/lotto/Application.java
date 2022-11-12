54f532cdfc3ec7ae2a1b4eafbe6df7a7b18fbb13
syr9707 <yrshin9771@gmail.com>
2022-11-12 12:48:42 +0900

    feat(lotto): 상수 지정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,25 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    public static String[] PLACE = {"3개 일치 (5,000원) - ",
+                                    "4개 일치 (50,000원) - ",
+                                    "5개 일치 (1,500,000원) - ",
+                                    "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+                                    "6개 일치 (2,000,000,000원) - "};
+
+    public static int[] SAME_STAT = new int[5];
+    public static int QUANTITY = 0;
+
+    public static final int[] PAY = {5000, 50000, 1500000, 30000000, 2000000000};
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
