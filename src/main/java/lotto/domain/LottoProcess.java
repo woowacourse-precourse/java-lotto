6a83d25e128c2b8e8190bb44aeac6a62f231d057
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-12 23:39:39 +0900

    feat(LottoProcess) : 1등부터 5등까지 몇명이 당첨되었는지 반환하는 기능 추가 decideRank() 메소드로 몇등인지 결정 decideRanks() 메소드로 모두 몇등인지 결정 countRanks() 메소드로 1등부터 5등까지 몇명인지 세서 반환

--- src/main/java/lotto/domain/LottoProcess.java
+++ src/main/java/lotto/domain/LottoProcess.java
@@ -1,6 +1,6 @@
 package lotto.domain;
 
-import java.util.List;
+import java.util.*;
 
 public class LottoProcess {
     private final List<List<Integer>> lottos;
@@ -21,6 +21,36 @@ public class LottoProcess {
         }
 
         return countMachingNumber;
+    }
+
+    public List<Integer> decideRanks(List<List<Integer>> lottos, List<Integer> winningNumbers){
+        List<Integer> ranks = new ArrayList<>();
+
+        for(List<Integer> lotto : lottos){
+            ranks.add(decideRank(compareWinningNumbers(lotto,winningNumbers),lotto));
+        }
+
+        return ranks;
+    }
+
+    public int decideRank(int countMachingNumber, List<Integer> lotto){
+        if(countMachingNumber == 6) return 1;
+        if(countMachingNumber == 5 && lotto.contains(bonusNumber)) return 2;
+        if(countMachingNumber == 5) return 3;
+        if(countMachingNumber == 4) return 4;
+        if(countMachingNumber == 3) return 5;
+        return 6;
+    }
+
+    public Map<String, Integer> countRanks(List<Integer> ranks){
+        Map<String, Integer> rankInfomation = new HashMap<>();
+        rankInfomation.put("FIRST", Collections.frequency(ranks,1));
+        rankInfomation.put("SECOND", Collections.frequency(ranks,2));
+        rankInfomation.put("THIRD", Collections.frequency(ranks,3));
+        rankInfomation.put("FOURTH", Collections.frequency(ranks,4));
+        rankInfomation.put("FIFTH", Collections.frequency(ranks,5));
+
+        return rankInfomation;
     }
 
 }
