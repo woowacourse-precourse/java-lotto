17e8682a2c627b4c95dd46c396b4dd912efebef4
lacram <chemin9898@gmail.com>
2022-11-12 21:35:06 +0900

    refactor: 숫자만 입력했는지 검증하는 기능, 1000원 단위로 입력했는지 검증하는 기능 에러메세지 수정

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -6,7 +6,7 @@ public class Validation {
     public static void isDigit(String numbers) {
         for (char number : numbers.toCharArray()) {
             if (!Character.isDigit(number)) {
-                throw new IllegalArgumentException("숫자만 입력해 주세요.");
+                throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.");
             }
         }
     }
@@ -14,10 +14,10 @@ public class Validation {
     public static void isThousandUnit(String purchaseAmount) {
         long money = Long.parseLong(purchaseAmount);
         if (money % 1000 != 0) {
-            throw new IllegalArgumentException("1000원 단위로 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해 주세요.");
         }
     }
-    // 로또 구입 금액을 검증하는 기능
+    // 로또 구입 금액을 검증
     public static void validateLottoPurchaseAmount(String purchaseAmount) {
         isDigit(purchaseAmount);
         isThousandUnit(purchaseAmount);
@@ -25,5 +25,7 @@ public class Validation {
 
 
 
-
 }
+
+
+
