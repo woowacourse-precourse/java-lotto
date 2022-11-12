0d7171ffa17c455be85f007ff81e3ca69da748c0
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-12 16:12:06 +0900

    refactor: LOTTO_PRICE chagne Enum

--- src/main/java/lotto/domain/Buyer.java
+++ src/main/java/lotto/domain/Buyer.java
@@ -1,22 +1,29 @@
 package lotto.domain;
 
+import lotto.enums.ConstantInteger;
 import lotto.validate.BuyerValidate;
+
+import static lotto.enums.ConstantInteger.*;
 
 public class Buyer {
     private final int buyPrice;
-    private static final int LOTTO_PRICE = 1000;
+
 
     public Buyer(int buyPrice) {
         validate(buyPrice);
         this.buyPrice = buyPrice;
     }
-    public int getBuyPrice(){return buyPrice;}
+
+    public int getBuyPrice() {
+        return buyPrice;
+    }
+
     public int getBuyLottoCount() {
-        return buyPrice / LOTTO_PRICE;
+        return buyPrice / ConstantInteger.LOTTO_PRICE.getValue();
     }
 
     private void validate(int buyPrice) {
         BuyerValidate buyerValidate = new BuyerValidate();
-        buyerValidate.validate(buyPrice, LOTTO_PRICE);
+        buyerValidate.validate(buyPrice, LOTTO_PRICE.getValue());
     }
 }
