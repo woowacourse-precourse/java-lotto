aacac54ae711ef25f0af914e193c953978a1ba4d
gudals113 <gudals113@gmail.com>
2022-11-12 22:24:21 +0900

    feat: 구매한 로또 장수 계산
    feat: 구매한 로또 장수 계산
    
    1000원 단위 아니면 예외 처리하기

--- src/main/java/lotto/Console.java
+++ src/main/java/lotto/Console.java
@@ -2,9 +2,9 @@ package lotto;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Console {
 
-    public double inputMoney() {
+    public int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        double money = Double.valueOf(readLine());
+        int money = Integer.valueOf(readLine());
         return money;
     }
 }
--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -1,8 +1,15 @@
 package lotto;
 
 public class Controller {
-    public int getCount(double money) {
+    public int getCount(int money) {
         int count = 0;
+        if (money%1000 !=0) {
+            System.out.println("[ERROR] 1,000원 단위가 아닙니다.");
+            throw new IllegalArgumentException();
+        }
+        count = money/1000;
         return count;
     }
+
+
 }
