23059410e8fcd1bf0f65bca5e91f134988be8015
Park shinyeong <syngh503@gmail.com>
2022-11-12 21:49:25 +0900

    Feat(OutputView): 출력 뷰 메소드 생성
    로또 구매 개수 및 로또 목록 출력 메소드 printPurchaseLotto와
    당첨 통계를 출력하는 메소드 printWinningStatistics를 구현하였습니다.
    
    Closes: #5

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -1,4 +1,30 @@
 package lotto.View;
 
+import lotto.type.RankType;
+
+import java.util.List;
+import java.util.Map;
+
 public class OutputView {
+
+    public static void printPurchaseLotto(int lottoCount, List<String> lottos) {
+        System.out.printf("%d개를 구매했습니다.", lottoCount);
+        for (String lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
+
+    public static void printWinningStatistics(Map<RankType, Integer> rank, int earningRate) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        for (RankType rankType : rank.keySet()) {
+            if (rankType == RankType.NOTHING) {
+                continue;
+            }
+            String format = String.format("%,d", rankType.getPrizeMoney());
+            System.out.printf("%s (%s원) - %d개", rankType.getResult(), format, rank.get(rankType));
+        }
+        System.out.printf("총 수익률은 %d입니다.", earningRate);
+    }
 }
