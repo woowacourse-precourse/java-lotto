070c1555116977da1ffaa21cfcf30d40037c3806
최용태 <smc2315@naver.com>
2022-11-12 20:53:48 +0900

    feat: 메시지 추가

--- src/main/java/lotto/utils/Message.java
+++ src/main/java/lotto/utils/Message.java
@@ -2,4 +2,5 @@ package lotto.utils;
 
 public class Message {
     public static final String REQUEST_USER_AMOUNT = "구입 금액을 입력해 주세요.";
+    public static final String REQUEST_WIN_NUMBERS = "당첨 번호를 입력해 주세요.";
 }
