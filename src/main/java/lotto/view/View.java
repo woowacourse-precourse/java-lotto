44584d3d4210a1d3132b501fe4b83e1df61fc97f
minaver <psmjoshua@gmail.com>
2022-11-12 15:04:15 +0900

    feat(View): printLottoPayed method 추가

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -1,4 +1,24 @@
 package lotto.view;
 
+import lotto.model.Lotto;
+
+import java.util.List;
+
 public class View {
+
+    public void printLottoPayed(List<Lotto> lottoPayed) {
+        System.out.println(lottoPayed.size()+"개를 구매했습니다.");
+        for (Lotto lotto : lottoPayed) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public void printWinningStats() {
+
+    }
+
+    public void printEarningRate() {
+
+    }
+
 }
