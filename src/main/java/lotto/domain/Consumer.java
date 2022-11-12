1af3643a7e9333b934b7bcf9d7981c5e3c4f7e27
JUHA <khabh@naver.com>
2022-11-12 22:02:10 +0900

    feat(LotteryWheel): 오름차순으로 정렬된 로또 번호 생성

--- src/main/java/lotto/domain/Consumer.java
+++ src/main/java/lotto/domain/Consumer.java
@@ -1,14 +1,21 @@
 package lotto.domain;
 
+import lotto.Lotto;
 import lotto.view.InputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Consumer {
     private final static int PURCHASE_UNIT = 1000;
+
+    private final List<Lotto> lotteries;
 
     public Consumer() {
         InputView input = new InputView();
         int purchasePrice = input.readPurchasePrice();
         validatePrice(purchasePrice);
+        this.lotteries = getLotteries(purchasePrice);
     }
 
     private void validatePrice(int price) {
@@ -16,5 +23,18 @@ public class Consumer {
             System.out.println("[ERROR] 금액은 1,000원 단위로 입력해야 합니다. (최소 금액 1,000원)");
             throw new IllegalArgumentException();
         }
+    }
+
+    private List<Lotto> getLotteries(int purchasePrice) {
+        List<Lotto> lotteries = new ArrayList<>();
+        LotteryWheel lotteryWheel = new LotteryWheel();
+
+        while (purchasePrice > 0) {
+            Lotto lotto = new Lotto(lotteryWheel.generateLotteryNumbers());
+            lotteries.add(lotto);
+            purchasePrice -= PURCHASE_UNIT;
+        }
+
+        return lotteries;
     }
 }
