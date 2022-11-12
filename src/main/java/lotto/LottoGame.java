22fa2bc141135ca44ab751490708e0b81c9cc6f7
kanghyki <kanghyki@gmail.com>
2022-11-13 03:11:43 +0900

    feat(LottoGame): 로또 번호 비교 후 enum 값을 리턴하는 메서드 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -9,4 +9,29 @@ public class LottoGame {
         FIFTH_PLACE,
         NOTHING
     }
+
+    public static eLottoPlace matchLotto(Lotto l1, Lotto l2, int bonus) {
+        int matches = 0;
+
+        for (int num : l1.getNumbers()) {
+            if (l2.getNumbers().contains(num)) {
+                matches += 1;
+            }
+        }
+        return checkPlace(matches, l2.getNumbers().contains(bonus));
+    }
+
+    private static eLottoPlace checkPlace(int matches, boolean isBonusMatch) {
+        if (matches == 6)
+            return eLottoPlace.FIRST_PLACE;
+        if (matches == 5 && isBonusMatch)
+            return eLottoPlace.SECOND_PLACE;
+        if (matches == 5)
+            return eLottoPlace.THIRD_PLACE;
+        if (matches == 4)
+            return eLottoPlace.FOURTH_PLACE;
+        if (matches == 3)
+            return eLottoPlace.FIFTH_PLACE;
+        return eLottoPlace.NOTHING;
+    }
 }
