5c96191aa63fdfc0f51f543bbc41e3e731379e35
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-12 18:45:22 +0900

    feat: 구매금액과 로또를 저장할 변수, 상수를 선언

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,4 +1,10 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private static int buyAmount;
+
+    private List<Lotto> myLotto = new ArrayList<>();
 }
