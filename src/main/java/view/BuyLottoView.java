28b7e259f46b6ba9db2b82777a1dfab3b8411127
JIWEON-JEONG <dlektl6@naver.com>
2022-11-13 01:10:21 +0900

    feat : 구매한 로또 정보를 보여주는 View

--- src/main/java/view/BuyLottoView.java
+++ src/main/java/view/BuyLottoView.java
@@ -1,9 +1,17 @@
 package view;
 
+import java.util.List;
+
 public class BuyLottoView {
     public static void ask() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
+    public static void printNumberOfLotto(int count) {
+        System.out.println(count + "개를 구매했습니다.");
+    }
 
+    public static void printLotteryNumbers(List<Integer> lotteryNumbers) {
+        System.out.println(lotteryNumbers);
+    }
 }
