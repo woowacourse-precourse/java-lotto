0fad7bf95a3692c250b6801660661cf43314386f
Ukj0ng <xb253q@gmail.com>
2022-11-12 16:03:18 +0900

    feat(inputView): 당첨번호와 보너스번호 입력기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,14 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
+import lotto.domain.WinningLotto;
 import lotto.exception.InputException;
 
 public class InputView {
     private final static String NUMBER_OF_PRICE = "구입금액을 입력해 주세요.";
+    private final static String NUMBER_OF_LOTTO = "당첨 번호를 입력해 주세요.";
+    private final static String NUMBER_OF_BONUSNUMBER = "보너스 번호를 입력해 주세요";
 
     public static int getInputPrice(){
         while(true){
@@ -15,6 +19,24 @@ public class InputView {
                 System.out.println(e.getMessage());
             }
         }
-
     }
+
+    public static WinningLotto getInputWinningLotto(){
+        while(true){
+            try{
+                System.out.println(NUMBER_OF_LOTTO);
+                Lotto winningNumber = InputException.isValidLottoNumber(Console.readLine());
+
+                System.out.println(NUMBER_OF_BONUSNUMBER);
+                int bonusNumber = InputException.isValidBonusNumber(Console.readLine());
+
+                return InputException.isValidWinningLotto(winningNumber, bonusNumber);
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+
+
 }
