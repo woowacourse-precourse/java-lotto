5b9130dfd8bad927aad3bb4952fcaf1a5e2e4b13
임원정 <oddish0513@gmail.com>
2022-11-12 21:18:29 +0900

    feat(lotto): 금액만큼 로또를 발행하는 기능

---
+++ src/main/java/lotto/domain/Store.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+	int count;
+	List<List<Integer>> lottoTicket;
+
+
+	public Store(int pay) {
+		new Pay(pay);
+		count = pay / 1000;
+	}
+
+	void issueLottoTicket(){
+		NumberGenerator numberGenerator = new NumberGenerator();
+		lottoTicket= new ArrayList<>();
+
+		for (int index = 0 ; index < count ; index++){
+			Lotto lotto = new Lotto(numberGenerator.createRandomNumbers());
+			lottoTicket.add(lotto.get());
+		}
+
+		for (List<Integer> list : lottoTicket)
+			System.out.println(list);
+	}
+}
