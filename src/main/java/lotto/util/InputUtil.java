3c3e15c11de93644b985b27597bf7eab9dd07a05
DongHo Kim <hkim4410@naver.com>
2022-11-12 14:59:15 +0900

    chore(InputUtil): 사용하지 않는 import 문 제거

--- src/main/java/lotto/util/InputUtil.java
+++ src/main/java/lotto/util/InputUtil.java
@@ -1,13 +1,5 @@
 package lotto.util;
 
-import lotto.domain.enums.Message;
-import lotto.domain.enums.Number;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputUtil {
