8f2e316d32bd1f8f06ac146e55fb8c61b4640715
asas6978 <asas6978@naver.com>
2022-11-12 10:33:25 +0900

    fix: 당첨 번호 일치 숫자 수정

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -6,7 +6,7 @@ public enum WinningNumber {
     THREE_MATCHES(3, 0, 5000),
     FOUR_MATCHES(4, 0, 50000),
     FIVE_MATCHES(5, 0, 1500000),
-    FIVE_AND_BONUS_BALL_MATCHES(0, 1, 30000000),
+    FIVE_AND_BONUS_BALL_MATCHES(5, 1, 30000000),
     SIX_MATCHES(6, 0, 2000000000);
 
     private final int numberOfWinning;
