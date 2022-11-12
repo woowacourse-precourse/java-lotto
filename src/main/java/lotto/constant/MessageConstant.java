ebb2c7ff2f91163ae3fd30c7624ebe4d1d49305f
yeonhee <ydol2@naver.com>
2022-11-12 12:52:07 +0900

    refactor(OutputView) : OutputView에만 쓰이는 메시지 상수 MessageConstant에서 가져오기

--- src/main/java/lotto/constant/MessageConstant.java
+++ src/main/java/lotto/constant/MessageConstant.java
@@ -9,14 +9,4 @@ public class MessageConstant {
     public static final String INPUT_LOTTO_NUMBERS = LINE + "당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER = LINE + "보너스 번호를 입력해 주세요.";
 
-    public static final String OUTPUT_PURCHASE = LINE + "%d개를 구매했습니다." + LINE;
-    public static final String OUTPUT_EARNING = LINE + "총 수익률은 %.1f%%입니다.";
-
-    public static final String STATISTICS_MESSAGE = LINE + "당첨 통계" + LINE + "---";
-    public static final String STATISTICS_THREE_MATCH = "3개 일치 (5,000원) - %d개";
-    public static final String STATISTICS_FOUR_MATCH = LINE + "4개 일치 (50,000원) - %d개";
-    public static final String STATISTICS_FIVE_MATCH = LINE + "5개 일치 (1,500,000원) - %d개";
-    public static final String STATISTICS_FIVE_MATCH_ONE_BONUS = LINE + "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
-    public static final String STATISTICS_SIX_MATCH = LINE + "6개 일치 (2,000,000,000원) - %d개";
-
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,20 +4,19 @@ import lotto.domain.Lotto;
 
 import java.util.List;
 
-import static lotto.constant.MessageConstant.OUTPUT_PURCHASE;
-import static lotto.constant.MessageConstant.STATISTICS_MESSAGE;
-import static lotto.constant.MessageConstant.STATISTICS_THREE_MATCH;
-import static lotto.constant.MessageConstant.STATISTICS_FOUR_MATCH;
-import static lotto.constant.MessageConstant.STATISTICS_FIVE_MATCH;
-import static lotto.constant.MessageConstant.STATISTICS_FIVE_MATCH_ONE_BONUS;
-import static lotto.constant.MessageConstant.STATISTICS_SIX_MATCH;
-import static lotto.constant.MessageConstant.OUTPUT_EARNING;
-
-import static lotto.constant.UtilConstant.LOTTO_OPEN;
-import static lotto.constant.UtilConstant.LOTTO_CLOSE;
-import static lotto.constant.UtilConstant.COMMA_AND_SPACE;
-
 public class OutputView {
+    private static final String LINE = System.lineSeparator();
+    private static final String STATISTICS_MESSAGE = LINE + "당첨 통계" + LINE + "---";
+    private static final String STATISTICS_THREE_MATCH = "3개 일치 (5,000원) - %d개";
+    private static final String STATISTICS_FOUR_MATCH = LINE + "4개 일치 (50,000원) - %d개";
+    private static final String STATISTICS_FIVE_MATCH = LINE + "5개 일치 (1,500,000원) - %d개";
+    private static final String STATISTICS_FIVE_MATCH_ONE_BONUS = LINE + "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+    private static final String STATISTICS_SIX_MATCH = LINE + "6개 일치 (2,000,000,000원) - %d개";
+    private static final String OUTPUT_PURCHASE = LINE + "%d개를 구매했습니다." + LINE;
+    private static final String OUTPUT_EARNING = LINE + "총 수익률은 %.1f%%입니다.";
+    private static final String LOTTO_OPEN = "[";
+    private static final String LOTTO_CLOSE = "]";
+    private static final String COMMA_AND_SPACE = ", ";
 
     public void printPurchaseCount(int lottoCount){
         System.out.printf(OUTPUT_PURCHASE,lottoCount);
