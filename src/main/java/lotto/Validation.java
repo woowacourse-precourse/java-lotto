1152966ea969f011d60d47c27bce67c3bddbbf7f
sieun <sieun960513@naver.com>
2022-11-12 22:45:21 +0900

    [validation] 입력받은 당첨 숫자 중복 검사

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validation {
 
@@ -19,5 +21,11 @@ public class Validation {
 
     public static boolean lottoLength(List<Integer> arr) {
         return arr.size() == LOTTO_SIZE;
+    }
+
+    public static boolean lottoNumDuplicated(List<Integer> arr) {
+        int originalSize = arr.size();
+        Set<Integer> set = new HashSet<>(arr);
+        return originalSize == set.size();
     }
 }
