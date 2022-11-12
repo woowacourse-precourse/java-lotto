fe0c05adcd32edc6e0b5cd459c77d9bf35e569c1
Amyhds <dasomhan524@gmail.com>
2022-11-12 14:14:25 +0900

    feat(consoleinput): 당첨번호와 보너스 번호를 luckyandbonus 클래스로 넘겨주기

--- src/main/java/lotto/ConsoleInput.java
+++ src/main/java/lotto/ConsoleInput.java
@@ -7,13 +7,14 @@ import java.util.List;
 public class ConsoleInput {
     public static int purchaseCount;
     public static List<Integer> luckySix;
+    public static int bonusNumber;
 
     public void priceInput() {
         System.out.println(LottoMessage.START_MESSAGE.getMessage());
         String purchaseAmount = Console.readLine();
-        for(int index=0;index<purchaseAmount.length();index++) {
+        for (int index = 0; index < purchaseAmount.length(); index++) {
             if (!Character.isDigit(purchaseAmount.charAt(index))) {
-                throw new IllegalArgumentException(LottoMessage.INTEGER_INPUT_ERROR_MESSAGE.getMessage());
+                throw new IllegalArgumentException(LottoMessage.INTEGER_ERROR_MESSAGE.getMessage());
             }
             Purchase userPurchase = new Purchase(Integer.parseInt(purchaseAmount));
             purchaseCount = userPurchase.calculateCount();
@@ -23,16 +24,30 @@ public class ConsoleInput {
     public void luckySixInput() {
         System.out.println(LottoMessage.LUCKY_NUMBER_MESSAGE.getMessage());
         String inputNumbers = Console.readLine();
-        for(int index=0;index<inputNumbers.length();index++){
+        for (int index = 0; index < inputNumbers.length(); index++) {
             char oneNumber = inputNumbers.charAt(index);
-            if(oneNumber == ','){
+            if (oneNumber == ',') {
                 continue;
             }
             if (!Character.isDigit(oneNumber)) {
-                throw new IllegalArgumentException(LottoMessage.INTEGER_INPUT_ERROR_MESSAGE.getMessage());
+                throw new IllegalArgumentException(LottoMessage.INTEGER_ERROR_MESSAGE.getMessage());
             }
             luckySix.add(Character.getNumericValue(oneNumber));
-            Lotto luckyNumber = new Lotto(luckySix);
         }
+    }
+
+    public void bonusInput() {
+        System.out.println(LottoMessage.BONUS_NUMBER_MESSAGE.getMessage());
+        String inputBonus = Console.readLine();
+        for (int index = 0; index < inputBonus.length(); index++) {
+            if (!Character.isDigit(inputBonus.charAt(index))) {
+                throw new IllegalArgumentException(LottoMessage.INTEGER_ERROR_MESSAGE.getMessage());
+            }
+            if (index == 1) {
+                throw new IllegalArgumentException(LottoMessage.BONUS_ERROR_MESSAGE.getMessage());
+            }
+            bonusNumber = Character.getNumericValue(inputBonus.charAt(index));
+        }
+        LuckyAndBonus finalLuckyNumbers = new LuckyAndBonus(luckySix, bonusNumber);
     }
 }
