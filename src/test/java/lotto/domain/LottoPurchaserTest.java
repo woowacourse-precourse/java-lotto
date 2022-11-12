9d0243248a6cb24b017ea9a0bfc67cf7d7b3c77d
YE-EUN <musiclove529@naver.com>
2022-11-13 06:15:08 +0900

    test(LottoPuchaserTest) : 테스트 추가
    - 입력이 정상인 경우 purchaseAmount에 입력한 값이 저장되어 있는지 확인
    - 입력이 숫자가 아닐 경우 예외 처리 확인
    - 입력이 로또 금액(1000원)으로 안나뉘는 경우 예외 처리 확인

--- src/test/java/lotto/domain/LottoPurchaserTest.java
+++ src/test/java/lotto/domain/LottoPurchaserTest.java
@@ -2,8 +2,54 @@ package lotto.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoPurchaserTest {
+
+    @Test
+    void 로또_정상_구입(){
+        String input = "8000";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        LottoPurchaser lottoPurchaser = new LottoPurchaser();
+
+        int expected = 8000;
+
+        assertThat(lottoPurchaser.getPurchaseAmount()).isEqualTo(expected);
+    }
+
+    @Test
+    void 숫자가_아닌_입력_예외처리(){
+        String input = "abcd";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        String expected = "[ERROR] 구입금액은 숫자여야 합니다.";
+
+        try{
+            LottoPurchaser lottoPurchaser = new LottoPurchaser();
+        }catch (IllegalArgumentException e){
+            assertThat(e.getMessage()).isEqualTo(expected);
+        }
+    }
+
+    @Test
+    void 로또금액으로_안나뉘는_경우_예외처리(){
+        String input = "1500";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        String expected = "[ERROR] 구입금액은 1000원 단위여야 합니다.";
+
+        try{
+            LottoPurchaser lottoPurchaser = new LottoPurchaser();
+        }catch (IllegalArgumentException e){
+            assertThat(e.getMessage()).isEqualTo(expected);
+        }
+    }
 
 }
