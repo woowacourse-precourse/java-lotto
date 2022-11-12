10e97948a24c5af437bd9c7a032ce92b8e19f616
kimminsu <xjfcnfw3@naver.com>
2022-11-12 17:56:05 +0900

    feat: 로또 번호들의 등수 결과를 구하는 기능 구현

--- src/main/java/lotto/lottopaper/LottoPaper.java
+++ src/main/java/lotto/lottopaper/LottoPaper.java
@@ -1,13 +1,31 @@
 package lotto.lottopaper;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.Rank;
 
 public class LottoPaper {
+    private static final int ZERO = 0;
     private final List<Lotto> lottos = new ArrayList<>();
 
     public void addLotto(Lotto lotto) {
         lottos.add(lotto);
+    }
+
+    private List<Integer> initRanks() {
+        return new ArrayList<>(Collections.nCopies(Rank.LENGTH, ZERO));
+    }
+
+    public List<Integer> checkLottos(List<Integer> userNumbers, Integer bonusNumber) {
+        List<Integer> resultRanks = initRanks();
+
+        for (Lotto lotto : lottos) {
+            int rank = lotto.getRank(userNumbers, bonusNumber);
+            resultRanks.set(rank, resultRanks.get(rank)+1);
+        }
+
+        return resultRanks;
     }
 }
