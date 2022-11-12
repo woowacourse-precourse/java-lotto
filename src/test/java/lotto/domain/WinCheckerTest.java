9de4dfa6fbbf108fe71d00fd384bbedac46e50ef
yg0585 <yg0585@naver.com>
2022-11-12 17:25:28 +0900

    style: 안쓰는 import 제거

--- src/test/java/lotto/domain/WinCheckerTest.java
+++ src/test/java/lotto/domain/WinCheckerTest.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
