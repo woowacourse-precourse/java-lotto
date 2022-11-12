e40d617d7d041690067e0d6f0904844fc55c2c98
hodomaroo <enghemdwkd@gmail.com>
2022-11-12 16:44:00 +0900

    [Feat] : LottoPublisher 클래스 구현 완료
    주어진 금액만큼 로또를 생성해 반환하는 publishLottos,
    입력으로 들어온 금액의 유효성을 검증하는 validateMultipleOfLotto,
    로또 1장을 생성해 반환하는 publishLotto 구현을 모두 완료하였습니다.
    
    테스트 코드를 통한 각 기능 메서드의 정상 동작을 확인하였습니다.

--- src/main/java/lotto/LottoPublisher.java
+++ src/main/java/lotto/LottoPublisher.java
@@ -1,8 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * 로또 발행을 담당하는 클래스
  */
 public class LottoPublisher {
 
+    public static List<Lotto> publishLottos(int money){
+        validateMultipleOfLottoCost(money);
+
+        int lottoCount = money / Lotto.LOTTO_COST;
+        return new ArrayList<>(Stream.generate(() -> publishLotto()).limit(lottoCount).collect(Collectors.toList()));
+    }
+    public static Lotto publishLotto(){
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6));
+    }
+
+    private static void validateMultipleOfLottoCost(int money){
+        if(money % Lotto.LOTTO_COST != 0 || money / Lotto.LOTTO_COST == 0){
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 " + Lotto.LOTTO_COST +"의 배수여야 합니다");
+        }
+    }
 }
