9d92a70fa3f4401902bf1b504bf0b73cffe462d3
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-13 00:03:46 +0900

    feat(BoughtLotto): 구입한 로또 클래스 메서드 선언

---
+++ src/main/java/lotto/BoughtLotto.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoughtLotto extends Lotto{
+    public BoughtLotto() {
+        super(new ArrayList<>());
+    }
+
+    private List<Integer> createLottoNumber() {
+        return new ArrayList<>();
+    }
+
+    public void printLottoNumber() {
+
+    }
+}
