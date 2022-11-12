17f91c3c7d30663abf2cae1d73d3495630102cca
Soomin Lim <augustth511@gmail.com>
2022-11-12 17:11:59 +0900

    feat(PlayerMoney): 로또 구입 금액 입력 시 1,000으로 나누어 떨어지지 않으면 예외처리 기능 및 에러문구 출력 기능 구현

--- src/main/java/lotto/domain/PlayerMoney.java
+++ src/main/java/lotto/domain/PlayerMoney.java
@@ -1,12 +1,22 @@
 package lotto.domain;
 
+import static lotto.domain.ErrorMessage.PLAYER_MONEY_ERROR_MESSAGE;
+
 public class PlayerMoney {
     static final int lottoPrice = 1000;
 
     private final int money;
 
     public PlayerMoney(int money) {
+        validate(money);
         this.money = money;
+    }
+
+    private void validate(int money) {
+        if (money % lottoPrice != 0) {
+            System.out.println(PLAYER_MONEY_ERROR_MESSAGE);
+            throw new IllegalArgumentException();
+        }
     }
 
     public int getMoney() {
