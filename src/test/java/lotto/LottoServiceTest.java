0289db782c9a28d3195c6c14ee1f42ad969b11ff
jybin96 <jybin96@changwon.ac.kr>
2022-11-13 01:11:57 +0900

    test: add validate for number testcode add test code list
    - Exceptions occur if the user input value is not number

--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -3,6 +3,7 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.service.LottoService;
+import lotto.system.SystemValid;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,15 +12,22 @@ public class LottoServiceTest {
 
     @DisplayName("사용자 금액이 로또금액으로 나누어지지 떨어지지 않는 다면 예외가 발생한다.")
     @Test
-    void createdLottoServiceByMoneyDivideLottoPrice() throws Exception {
+    void createdLottoServiceByMoneyDivideLottoPrice() {
         assertThatThrownBy(() -> lottoService.buy(2300))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("사용자 금액 0원이라면 예외가 발생한다.")
     @Test
-    void createdLottoServiceByMoneyZero() throws Exception {
+    void createdLottoServiceByMoneyZero() {
         assertThatThrownBy(() -> lottoService.buy(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사용자 입력 금액이 숫자가 아니라면 예외가 발생한다.")
+    @Test
+    void createdSystemValidByValidateNumber(){
+        assertThatThrownBy(() -> SystemValid.validateForNumber("1000asw"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
