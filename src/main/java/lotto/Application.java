39e71444ab7ac6442d19eed8ad404fb003dffac6
kanghyki <kanghyki@gmail.com>
2022-11-13 04:26:18 +0900

    feat(Application): 메인 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int money = LottoInStream.readToBuyLotto();
+        List<Lotto> lottoes = LottoGame.buyLotto(money);
+        LottoOutStream.outStreamLottoesNumber(lottoes);
+
+        Lotto winningLotto = new Lotto(LottoInStream.readToGetLottoNumbers());
+        int bonusNumber = LottoInStream.readToGetLottoBonus();
+
+        int[] matches = LottoGame.getResultLotto(lottoes, winningLotto, bonusNumber);
+
+        LottoOutStream.outStreamLottoResult(matches);
+        LottoOutStream.outStreamRateOfReturn(LottoGame.calcRateOfReturn(matches, money));
     }
 }
