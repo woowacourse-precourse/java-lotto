f52d26b118ab5d6c9c5673c0da712798d7249b40
ghltjd369 <ghltjd369@gmail.com>
2022-11-12 16:47:28 +0900

    feat : 입력받은 당첨 번호를 ","를 기준으로 분리하는 기능 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -2,9 +2,16 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class LottoMachine {
     public void inputWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        String number_str = Console.readLine();
+        String inputNumber = Console.readLine();
+        String[] winningNumber = splitNumber(inputNumber);
+    }
+
+    public String[] splitNumber(String number) {
+        return number.split(",");
     }
 }
