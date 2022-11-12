823178cbf9b8e73a7afb47e8b85031e6a6dd8f35
Jimin <thswlals0219@gmail.com>
2022-11-12 16:29:15 +0900

    feat(User): 입력값이 숫자인지 검사하는 기능 구현

--- src/main/java/lotto/model/User.java
+++ src/main/java/lotto/model/User.java
@@ -1,9 +1,19 @@
 package lotto.model;
 
+
 public class User {
     private int purchaseAmount;
 
     public User(String purchaseAmount) {
+        validate(purchaseAmount);
         this.purchaseAmount = Integer.parseInt(purchaseAmount);
+    }
+
+    private void validate(String purchaseAmount) {
+
+    }
+
+    private boolean isNumber(String purchaseAmount) {
+        return purchaseAmount.chars().allMatch(Character::isDigit);
     }
 }
