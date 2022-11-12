13e54f167f78041d90ea14751f5216e0cb3aca67
KSH <tkdgns543@gmail.com>
2022-11-13 04:25:28 +0900

    feat(domain): 사용자 입력 당첨번호 예외 확인

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,11 +12,44 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    public void validate(List<Integer> numbers) throws IllegalArgumentException {
+        checkNumberLength(numbers);
+
+        for (Integer number : numbers) {
+            checkNumberRange(number);
+        }
+
+        makeSureUniqueNumbers(numbers);
+    }
+
+    public void validateBonus(int bonusNumer) throws IllegalArgumentException {
+        checkNumberRange(bonusNumer);
+
+        makeSureUniqueNumber(bonusNumer, numbers);
+    }
+
+    private void checkNumberLength(List<Integer> numbers) throws IllegalArgumentException {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 숫자의 갯수는 6개만 가능합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void checkNumberRange(int number) throws IllegalArgumentException {
+        if (!(number>=1 && number <=45)) {
+            throw new IllegalArgumentException("[ERROR] 숫자의 범위는 1~45까지만 가능합니다.");
+        }
+    }
+
+    private void makeSureUniqueNumbers(List<Integer> numbers) throws IllegalArgumentException{
+        Set uniqueNumbersCounter = new HashSet(numbers);
+        if (uniqueNumbersCounter.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자 입력은 불가능 합니다.");
+        }
+    }
+
+    private void makeSureUniqueNumber(int number, List<Integer> numbers) throws IllegalArgumentException{
+        if (numbers.contains(number)) {
+            throw new IllegalArgumentException("[ERROR] 이미 있던 숫자와 중복 할 수 없습니다.");
+        }
+    }
 }
