b762c7c9b17a5a246da953a64d873dcbb3e767ca
limjae <limjae96@gmail.com>
2022-11-12 22:24:56 +0900

    test: Lotto 결과 비교 기능 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import lotto.result.RewardEnum;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -38,5 +40,22 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호 결과 매칭 기능1")
+    @Test
+    void matchLottoAndWinningNumber1(){
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        WinningNumber winningNumber = new WinningNumber(List.of(1, 2, 3, 4, 5, 6), 7);
+
+        assertThat(lotto.match(winningNumber)).isEqualTo(RewardEnum.WIN_6);
+    }
+
+    @DisplayName("로또 번호 결과 매칭 기능2")
+    @Test
+    void matchLottoAndWinningNumber2(){
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        WinningNumber winningNumber = new WinningNumber(List.of(1, 2, 3, 4, 5, 6), 7);
+
+        assertThat(lotto.match(winningNumber)).isEqualTo(RewardEnum.WIN_5_BONUS);
+    }
 
 }
