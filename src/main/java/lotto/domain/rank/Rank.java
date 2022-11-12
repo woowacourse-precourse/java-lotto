94999296490dfb5d98298385209f9778137e9be0
shinD <huipulci1@naver.com>
2022-11-13 02:53:10 +0900

    feat: Rank에 등수에 해당하는 일치숫자 개수 필드 추가

--- src/main/java/lotto/domain/rank/Rank.java
+++ src/main/java/lotto/domain/rank/Rank.java
@@ -7,24 +7,22 @@ import java.util.function.BiPredicate;
 
 public enum Rank {
 
-    FIRST(2_000_000_000, (matchCount, matchBonus) -> matchCount == 6),
-    SECOND(30_000_000, (matchCount, matchBonus) -> matchCount == 5 && matchBonus),
-    THIRD(1_500_000, (matchCount, matchBonus) -> matchCount == 5),
-    FOURTH(50_000, (matchCount, matchBonus) -> matchCount == 4),
-    FIFTH(5_000, (matchCount, matchBonus) -> matchCount == 3),
-    UNWINNABLE(0, (matchCount, matchBonus) -> matchCount < 3),
+    FIRST(2_000_000_000, 6, (matchCount, matchBonus) -> matchCount == 6),
+    SECOND(30_000_000, 5, (matchCount, matchBonus) -> matchCount == 5 && matchBonus),
+    THIRD(1_500_000, 5, (matchCount, matchBonus) -> matchCount == 5),
+    FOURTH(50_000, 4, (matchCount, matchBonus) -> matchCount == 4),
+    FIFTH(5_000, 3, (matchCount, matchBonus) -> matchCount == 3),
+    UNWINNABLE(0, 0, (matchCount, matchBonus) -> matchCount < 3),
     ;
 
     private final Money reward;
+    private final int matchCount;
     private final BiPredicate<Integer, Boolean> match;
 
-    Rank(int reward, BiPredicate<Integer, Boolean> match) {
+    Rank(int reward, int matchCount, BiPredicate<Integer, Boolean> match) {
         this.reward = Money.valueOf(reward);
+        this.matchCount = matchCount;
         this.match = match;
-    }
-
-    public Money getReward() {
-        return reward;
     }
 
     public static Rank matchRank(int matchCount, boolean matchBonus) {
@@ -32,5 +30,13 @@ public enum Rank {
                 .filter(r -> r.match.test(matchCount, matchBonus))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException("matchCount는 6 이하의 숫자여야 합니다."));
+    }
+
+    public Money getReward() {
+        return reward;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
     }
 }
