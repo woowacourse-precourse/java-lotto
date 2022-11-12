530314bef893712a8dd1d958b89735f05b47b551
ChanghyunKim <variety82p@gamil.com>
2022-11-12 17:40:58 +0900

    feat(LottoController) : 입력 금액 검증 파트 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,11 +1,14 @@
 package lotto.controller;
 
+import lotto.domain.Validator;
 import lotto.view.View;
 
 public class LottoController {
     private static final View view = new View();
+    private static final Validator  validator= new Validator();
     public void lottoGame(){
-        view.startLottoView();
+        String input = view.startLottoView();
+        validator.validateInputAmount(input);
     }
 
 }
