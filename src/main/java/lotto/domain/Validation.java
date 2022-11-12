983d7015c8aa16d748911487992881d6ff1ad25a
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-12 10:32:41 +0900

    refactor : regex 설명 추가 업데이트

--- src/main/java/lotto/domain/Validation.java
+++ src/main/java/lotto/domain/Validation.java
@@ -7,11 +7,11 @@ import static lotto.Setup.*;
 import static lotto.domain.Player.*;
 
 public class Validation {
-	private static final String NUMBER_ONLY_REGEX = "^\\d+$";
+	private static final String NUMBER_ONLY_REGEX = "^\\d+$"; // 숫자로만 구성된 형식을 표현한다
 	private static final String NUMBER_INPUT_FORMAT_REGEX = "^(.+,)+.+$"; // "<시작문자><쉼표><종료문자>" 형식을 표현한다
 	private static final String NUMBER_INPUT_COUNT_REGEX = "^(.+,){5}.+$"; // "(<시작문자><쉼표>)X5<종료문자>" 형식을 표현한다
 	private static final String NUMBER_IN_BETWEEN_1_AND_45 = "(^[1-9]$|^[1-3][0-9]$|^4[0-5]$)"; // "1~45" 숫자 형식을 표현한다
-	private static final String BONUS_NUMBER_INPUT_COUNT_REGEX = "^(.+)$"; //
+	private static final String BONUS_NUMBER_INPUT_COUNT_REGEX = "^(.+)$"; // 1개의 문자 형식을 표현한다
 
 	public static boolean isNotANumber(String input) {
 		return !input.matches(NUMBER_ONLY_REGEX);
