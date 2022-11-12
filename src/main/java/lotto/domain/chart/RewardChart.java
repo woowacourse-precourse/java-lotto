4a8e111f612c30ac086a989c3225792d92c1bae4
jaehyeon <jhjh0022@naver.com>
2022-11-13 01:04:04 +0900

    feat: 등수에 따른 보상금 차트

--- src/main/java/lotto/domain/chart/RewardChart.java
+++ src/main/java/lotto/domain/chart/RewardChart.java
@@ -1,2 +1,15 @@
-package lotto.domain.chart;public class RewardChart {
+package lotto.domain.chart;
+
+public enum RewardChart {
+    First(2_000_000_000), Second(30_000_000), Third(1_500_000), Fourth(50_000), Fifth(5_000);
+
+    private final int value;
+
+    RewardChart(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
