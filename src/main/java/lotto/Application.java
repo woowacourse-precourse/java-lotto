4e89292d993ff4ccce8d61629c267411d558ec54
CMHyuk <alsgur9710@naver.com>
2022-11-12 12:15:26 +0900

    refactor(Application): [ERROR] 상수로 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,12 +3,15 @@ package lotto;
 import static lotto.config.Config.lottoMachine;
 
 public class Application {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         try {
             lottoMachine().run();
         } catch (Exception e) {
-            System.out.println("[ERROR]" + e.getMessage());
+            System.out.println(ERROR_MESSAGE + e.getMessage());
         }
     }
 }
