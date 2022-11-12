163545122fbbfc7ced2839cd3e9b3572a6781d89
leeyongjun <leeyongjun0310@naver.com>
2022-11-13 03:57:07 +0900

    feat(lotto): ','이 2개 이상 입력이 되었는지 확인하는 기능 구현

--- src/main/java/lotto/domain/Change.java
+++ src/main/java/lotto/domain/Change.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Change {
+
     public static List<Integer> lottoNumber(String winningNumber) {
+        checkManyDot(winningNumber);
         List<String> newWinningNumber = Arrays.asList(winningNumber.split(","));
         List<Integer> result = new ArrayList<>();
         for (String number : newWinningNumber) {
@@ -13,5 +15,21 @@ public class Change {
         }
 
         return result;
+    }
+
+    private static void checkManyDot(String winningNumber) {
+        int dotCount = 0;
+        for (int i = 0; i < winningNumber.length(); i++) {
+            if (!(winningNumber.charAt(i) == ',')) {
+                dotCount = 0;
+                continue;
+            }
+            dotCount++;
+            if (dotCount >= 2) {
+                System.out.println("[ERROR] 당첨번호를 다시 입력해주세요. - ,2개");
+                throw new IllegalArgumentException();
+            }
+
+        }
     }
 }
