20a9f4dce8a1d9ab6ed94472b7ccb122df1fdaae
Amyhds <dasomhan524@gmail.com>
2022-11-12 13:31:09 +0900

    feat(consoleinput): 당첨번호 6자리 입력받는 메서드 구현 숫자가 아니면 예외 발생 lotto 클래스에 새 인스턴스로 추가

--- src/main/java/lotto/ConsoleInput.java
+++ src/main/java/lotto/ConsoleInput.java
@@ -2,8 +2,11 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class ConsoleInput {
     public static int purchaseCount;
+    public static List<Integer> luckySix;
 
     public void priceInput() {
         System.out.println(LottoMessage.START_MESSAGE.getMessage());
@@ -14,6 +17,22 @@ public class ConsoleInput {
             }
             Purchase userPurchase = new Purchase(Integer.parseInt(purchaseAmount));
             purchaseCount = userPurchase.calculateCount();
+        }
+    }
+
+    public void luckySixInput() {
+        System.out.println(LottoMessage.LUCKY_NUMBER_MESSAGE.getMessage());
+        String inputNumbers = Console.readLine();
+        for(int index=0;index<inputNumbers.length();index++){
+            char oneNumber = inputNumbers.charAt(index);
+            if(oneNumber == ','){
+                continue;
+            }
+            if (!Character.isDigit(oneNumber)) {
+                throw new IllegalArgumentException(LottoMessage.INTEGER_INPUT_ERROR_MESSAGE.getMessage());
+            }
+            luckySix.add(Character.getNumericValue(oneNumber));
+            Lotto luckyNumber = new Lotto(luckySix);
         }
     }
 }
