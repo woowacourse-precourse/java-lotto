819653c6148899b6d4c968909fde48875d6d38d5
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-12 13:12:48 +0900

    feat: 두 개의 로또에서 숫자가 몇개 같은지 알아내는 기능 완성

--- src/main/java/lotto/domain/LottoReader.java
+++ src/main/java/lotto/domain/LottoReader.java
@@ -3,6 +3,14 @@ package lotto.domain;
 import java.util.List;
 
 public class LottoReader {
+
+    public int countMatchingNumber(Lotto lotto, Lotto compareLotto) {
+        List<Integer> lottoNumbers = lotto.getNumbers();
+        return (int)lottoNumbers.stream()
+                .filter((lottoNumber) -> isIncludedNumber(compareLotto, lottoNumber))
+                .count();
+    }
+
     public boolean isIncludedNumber(Lotto lotto, int number) {
         List<Integer> lottoNumbers = lotto.getNumbers();
         return lottoNumbers.contains(number);
