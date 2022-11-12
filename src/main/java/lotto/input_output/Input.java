c793275dedc18848cc0688ab9d4c7cbc82edb213
leewoojin <zmfpdl64@naver.com>
2022-11-12 13:24:35 +0900

    Feat: Lotto 객체 당첨 번호 List<Integer> 형식 반환 함수
    input 클래스를 상속받은 클래스에서 사용할 수 있게 protected 접급 제한자를 사용했고
    로또 당첨번호를 입력받으면 List<Integer> 형식의 값이 필요해 만들었다.

--- src/main/java/lotto/input_output/Input.java
+++ src/main/java/lotto/input_output/Input.java
@@ -1,4 +1,24 @@
 package lotto.input_output;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Input {
+
+    /*
+### Input
+
+- [ ] Lotto 당첨 번호 입력받기 List<Integer>로 반환
+- [ ] 보너스 번호 입력받기
+- [ ] User 구매 금액 입력받기
+
+     */
+    protected static List<Integer> returnList(){ //Lotto 당첨 번호 입력받기 List<Integer>로 반환
+        String[] input  = Console.readLine().split(",");
+        return Arrays.stream(input).mapToInt(Integer::valueOf).boxed().collect(Collectors.toList());
+    }
+
 }
