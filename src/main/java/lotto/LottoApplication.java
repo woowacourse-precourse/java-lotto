257f9a9569ba8b80daa756261e38a2de012622ab
JuHyeokParkk <hyeok5596@naver.com>
2022-11-13 03:29:33 +0900

    feat: LottoApplication 클래스 구현

---
+++ src/main/java/lotto/LottoApplication.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class LottoApplication {
+
+    private LottoBuyer lottoBuyer;
+
+    public LottoApplication() {
+        this.lottoBuyer = new LottoBuyer();
+    }
+
+
+}
