8bec21237a826da0b373bc79e62a68300e8f0d0d
ma-an-jong <alswhd1113@gmail.com>
2022-11-12 23:40:25 +0900

    feat(Application): 로또 게임 메인 로직 구현
    BREAKING CHANGE: 로또 게임을 구현하기 위해서 추가한 기능을 Application 클래스에 모아 메인 로직을 구현함
    
    Before:
    
        Application
        {
    
        }
    
    After:
    
        Application
        {
            purchaseLotties : LottoGenerator를 이용한 로또 구매 기능
            inputWinningLotto : 당첨 로또 생성 기능
            inputWinningLottoNumbers : 당첨 로또 번호 입력 기능
            inputSpecialNumber : 보너스 숫자 입력 기능
            getMyLottoPrize : 구매 로또와 당첨 로또를 비교하여 당첨 내역 조회 기능
            displayStatisticsPrizes : 당첨 내역을 통계 내는 기능
            run : 로또 게임을 시작하는 메소드
        }
    
        로또 게임을 진행하는 메인 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,135 @@
 package lotto;
 
+import lotto.Lotto.Lotto;
+import lotto.Lotto.LottoInfo;
+import lotto.Lotto.WinningLotto;
+import lotto.input.ConsoleInputManager;
+import lotto.input.InputManager;
+import lotto.output.ConsoleOutputManager;
+import lotto.output.OutputManager;
+
+import java.util.*;
+
 public class Application {
+    private EarningRateCalculator calculator;
+    private LottoComparator lottoComparator;
+    private LottoGenerator lottoGenerator;
+    private OutputManager consoleOutputManager;
+    private InputManager consoleInputManager;
+
+    public Application()
+    {
+        calculator = EarningRateCalculator.getInstance();
+        lottoComparator = LottoComparator.getInstance();
+        lottoGenerator = LottoGenerator.getInstance();
+
+        consoleInputManager = ConsoleInputManager.getInstance();
+        consoleOutputManager = ConsoleOutputManager.getInstance();
+    }
+
+    private List<Lotto> purchaseLotties()
+    {
+        consoleOutputManager.write(Message.LOTTO_PURCHASE_REQUEST_MESSAGE);
+        String userInputForPrice = consoleInputManager.readLine();
+
+        while(true)
+        {
+            try
+            {
+                int price = Integer.parseInt(userInputForPrice);
+                return lottoGenerator.generateLotties(price);
+            }
+            catch (NumberFormatException e) {
+                consoleOutputManager.write(Message.LOTTO_PURCHASE_RETRY_MESSAGE);
+            }
+        }
+    }
+
+    private WinningLotto inputWinningLotto()
+    {
+        while(true)
+        {
+            try
+            {
+                List<Integer>winningNumbers = inputWinningLottoNumbers();
+                consoleOutputManager.write(Message.BONUS_NUMBER_REQUEST_MESSAGE);
+                int specialNumber = inputSpecialNumber();
+                return new WinningLotto(winningNumbers,specialNumber);
+            }
+            catch (NumberFormatException e)
+            {
+                consoleOutputManager.write(Message.WINNING_LOTTO_NUMBERS_INPUT_RETRY_MESSAGE);
+            }
+        }
+    }
+
+    private List<Integer> inputWinningLottoNumbers()
+    {
+        consoleOutputManager.write(Message.WINNING_LOTTO_NUMBERS_INPUT_REQUEST_MESSAGE);
+        String userInputForWinningNumbers = consoleInputManager.readLine();
+        List<Integer> winningNumbers = new ArrayList<Integer>();
+
+        for (String userInputForWinningNumber: userInputForWinningNumbers.split(","))
+        {
+            winningNumbers.add(Integer.parseInt(userInputForWinningNumber));
+        }
+
+        return winningNumbers;
+    }
+
+    private Integer inputSpecialNumber()
+    {
+        String userInputForSpecialNumber = consoleInputManager.readLine();
+        return Integer.parseInt(userInputForSpecialNumber);
+    }
+
+    private List<Prize> getMyLottoPrize(List<Lotto> myLotties, WinningLotto winningLotto)
+    {
+        List<Prize> lottoPrizes = new ArrayList<>();
+
+        for (Lotto lotto: myLotties)
+        {
+            Prize prize = lottoComparator.compare(lotto, winningLotto);
+            if(prize == null) continue;
+            lottoPrizes.add(prize);
+        }
+
+        return lottoPrizes;
+    }
+
+    private void displayStatisticsPrizes(List<Prize> prizes,int price)
+    {
+        consoleOutputManager.write(Message.STATISTICS_MESSAGE);
+        consoleOutputManager.write(Message.getPrizeInfoMessage(prizes));
+
+        double earnPercentage = calculator.calculate(prizes,price);
+        consoleOutputManager.write(Message.getEarningMessage(earnPercentage));
+
+    }
+
+    public void run() {
+        consoleOutputManager.write(Message.GAME_START_MESSAGE);
+        try
+        {
+            List<Lotto> lotties = purchaseLotties();
+            int price = lotties.size()*LottoInfo.LOTTO_PRICE;
+            consoleOutputManager.write(Message.getLottiesNumberInfoMessage(lotties));
+
+            WinningLotto winningLotto = inputWinningLotto();
+            List<Prize> myLottoPrizes = getMyLottoPrize(lotties,winningLotto);
+
+            displayStatisticsPrizes(myLottoPrizes,price);
+        }
+        catch (IllegalArgumentException e)
+        {
+            consoleOutputManager.write(e.getMessage());
+        }
+        consoleOutputManager.write(Message.GAME_END_MESSAGE);
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        new Application().run();
+
     }
 }
