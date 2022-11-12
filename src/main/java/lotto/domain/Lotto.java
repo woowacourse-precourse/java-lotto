80e8cd273599966035563623d85538c9a1b2cfc7
ChanghyunKim <variety82p@gamil.com>
2022-11-12 19:17:30 +0900

    feat(Lotto) : Lotto인스턴스의 로또번호를 얻는 메서드 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
-package lotto;
+package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -17,4 +19,8 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public List<Integer> getLottoNumber(){
+        return Collections.unmodifiableList(numbers);
+    }
+
 }
