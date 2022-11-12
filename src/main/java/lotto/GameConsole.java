81de00f8968db9d495c45bb61de93fab2eba6a74
박승민 <tmddudals369@naver.com>
2022-11-12 22:26:03 +0900

    feat: 안내 문구 출력 기능 구현

--- src/main/java/lotto/GameConsole.java
+++ src/main/java/lotto/GameConsole.java
@@ -4,12 +4,21 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class GameConsole {
 
-    public void inputPurchase(){
+    private static final String INPUT_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public int inputPurchase(){
+        notice(INPUT_PURCHASE_MESSAGE);
         String purchase = Console.readLine();
     }
 
     public void inputWinningNumber(){
         String winningNumber = Console.readLine();
+    }
+
+    private static void notice(String inputMessage) {
+        System.out.println(inputMessage);
     }
 
     public void inputBonusNumber(){
