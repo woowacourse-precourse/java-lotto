111e9cf81c4d36e1d7a361337e6a32da19abcc8c
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-12 23:52:02 +0900

    feat(service): generateLotto method

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -16,4 +16,16 @@ public class LottoService {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
+
+    /**
+     * 1-45 범위의 6자리 랜덤 숫자를 만들어 Lotto 객체를 반환
+     * @return 랜덤 숫자가 부여된 Lotto 객체
+     */
+    public Lotto generateLotto(){
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+        randomNumbers.sort(null);
+        Lotto lotto = new Lotto(randomNumbers);
+
+        return lotto;
+    }
 }
