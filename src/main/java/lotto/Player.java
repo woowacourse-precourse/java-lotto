831ad29c75c4a35955269931bdc0cb8f64b4d227
oyoungsun <plusmate@naver.com>
2022-11-12 22:41:35 +0900

    feat : seperate console input from Player

--- src/main/java/lotto/Player.java
+++ src/main/java/lotto/Player.java
@@ -3,17 +3,20 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Player {
-    static int totalPayment;
 
-    public void putMoneyforLotto(){
+
+    public int putMoneyforLotto(String money){
+        int totalPayment;
         try {
-            totalPayment = Integer.parseInt(Console.readLine());
+            totalPayment = Integer.parseInt(money);
         }catch (NumberFormatException e){
             throw new IllegalArgumentException("[ERROR] 정수를 입력해야 합니다.");
         }
+        checkDivideBy1000(totalPayment);
 
+        return (int)totalPayment/1000;
     }
-    public void checkDivideBy1000() {
+    public void checkDivideBy1000(int totalPayment) {
         if(totalPayment % 1000 !=0){
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해야합니다.");
         }
