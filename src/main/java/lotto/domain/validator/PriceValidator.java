53aa080ee10b7110751ff1789f7c4527c0b9d95a
yummygyudon <bang2brew@gmail.com>
2022-11-12 13:58:17 +0900

    FEAT(domain): 구매 금액 입력값 1000의 배수 검사
    - 1000으로 나누어 떨어지는 검사하는 `checkingDividable` 메서드 구현
      1. int 타입의 inputMoney 매개변수
      2. static 변수 값인 REF_MONEY_AMOUNT 값로 나눈 결과 나머지가 0이 아닐 경우 예외 발생
      2-1. ErrorMessages.MONEY_UNDIVIDED의 메세지 출력

--- src/main/java/lotto/domain/validator/PriceValidator.java
+++ src/main/java/lotto/domain/validator/PriceValidator.java
@@ -3,15 +3,21 @@ package lotto.domain.validator;
 import lotto.util.ErrorMessages;
 
 public class PriceValidator {
-    private static final int MINIMUM_MONEY = 1_000 ;
+    private static final int REF_MONEY_AMOUNT = 1_000 ;
 
 //    public void validateMoneyInput(String inputValue){
 //        final int INPUT_MONEY = Integer.parseInt(inputValue.replace(",",""));
 //    }
 
     public void checkingPurchasable(int inputMoney){
-        if (inputMoney < MINIMUM_MONEY){
+        if (inputMoney < REF_MONEY_AMOUNT){
             throw new IllegalArgumentException(ErrorMessages.BELOW_THE_PRICE_THRESHOLD.getMessage());
+        }
+    }
+
+    public void checkingDividable(int inputMoney){
+        if ((inputMoney % REF_MONEY_AMOUNT) != 0){
+            throw new IllegalArgumentException(ErrorMessages.MONEY_UNDIVIDED.getMessage());
         }
     }
 
