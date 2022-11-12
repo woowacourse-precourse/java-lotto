8bf3f1b1ed7c0a1e143d6129efc2ae6fb562d303
박시균\psk98 <psg980331@naver.com>
2022-11-12 15:19:38 +0900

    Feat: 로또 구입 금액 메시지 출력

---
+++ src/main/java/common/LottoGameMessage.java
@@ -0,0 +1,17 @@
+package common;
+
+import domain.Lotto;
+import domain.LottoRank;
+import domain.Player;
+import java.util.Iterator;
+
+import static common.Constant.*;
+
+public class LottoGameMessage {
+
+    public void printPurchasePriceMessage(){
+        System.out.println(PURCHASE_PRICE_MESSAGE);
+    }
+    
+
+}
