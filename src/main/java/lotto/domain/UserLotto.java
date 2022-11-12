b7351d2dfb381438250de11756c6fc50ed0d8fb8
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-12 17:44:28 +0900

    feat(UserLotto): 당첨 번호의 숫자 범위가 벗어나거나 형식에 어긋나는 예외 처리
    - 쉼표로 구성되어 있는지 확인
    - 특수문자, 영/한문자, 음수가 아닌지 확인
    - 1-45 사이의 숫자로 구성되어 있는지 확인

--- src/main/java/lotto/domain/UserLotto.java
+++ src/main/java/lotto/domain/UserLotto.java
@@ -1,23 +1,34 @@
 package lotto.domain;
 
+import lotto.exception.InputException;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public final class UserLotto {
 
+    private final String REG_XP_LOTTO = "^[1-45,]+$";
+    private final String SPLIT_STANDARD = ",";
+
     private final List<Integer> winNumbers;
     private final int bonusNumber;
 
     public UserLotto(String winNumbers, String bonusNumber) {
+        isValidRangeAndSplitByStandard(winNumbers);
         this.winNumbers = List.copyOf(mapToList(winNumbers));
         this.bonusNumber = Integer.parseInt(bonusNumber);
     }
 
+    private void isValidRangeAndSplitByStandard(String winNumbers) {
+        if (!winNumbers.matches(REG_XP_LOTTO)) {
+            throw new IllegalArgumentException(InputException.INPUT_INVALID_DIGIT_RANGE.message());
+        }
+    }
+
     private List<Integer> mapToList(String winNumbers) {
-        return Arrays.stream(winNumbers.split(""))
+        return Arrays.stream(winNumbers.split(SPLIT_STANDARD))
                 .map(Integer::valueOf)
                 .collect(Collectors.toList());
     }
-
 }
