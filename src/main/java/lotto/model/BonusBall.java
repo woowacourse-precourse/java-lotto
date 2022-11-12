7a8e7f1f6c11422de801b6800be098f8541d292c
situodys <situodys@gmail.com>
2022-11-12 15:32:30 +0900

    feat(BonusBall): 보너스볼 생성을 위한 값이 정수 범위를 넘거나 범위 외 값이 포함된 경우 검증 구현

--- src/main/java/lotto/model/BonusBall.java
+++ src/main/java/lotto/model/BonusBall.java
@@ -2,6 +2,8 @@ package lotto.model;
 
 import static lotto.constant.BonusBallConstants.EMPTY_BONUSBALL_MSG;
 import static lotto.constant.BonusBallConstants.INVALID_BONUSBALL_INPUT_MSG;
+import static lotto.constant.LottoConstants.MAX_VALID_LOTTO_VALUE;
+import static lotto.constant.LottoConstants.MIN_VALID_LOTTO_VALUE;
 
 public class BonusBall {
     private int value;
@@ -18,6 +20,8 @@ public class BonusBall {
     private void validate(String userInput) {
         isBlank(userInput);
         isConsistsWithOnlyDigits(userInput);
+        isIntegerRange(userInput);
+        isInProperRange(userInput);
     }
 
     private void isBlank(String userInput) {
@@ -34,6 +38,21 @@ public class BonusBall {
 
     private void isDigit(char piece) {
         if (!Character.isDigit(piece)) {
+            throw new IllegalArgumentException(INVALID_BONUSBALL_INPUT_MSG);
+        }
+    }
+
+    private void isIntegerRange(String userInput) {
+        try {
+            Integer.parseInt(userInput);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(INVALID_BONUSBALL_INPUT_MSG);
+        }
+    }
+
+    private void isInProperRange(String userInput) {
+        int pieceToInt = Integer.parseInt(userInput);
+        if (pieceToInt < MIN_VALID_LOTTO_VALUE || pieceToInt >MAX_VALID_LOTTO_VALUE ) {
             throw new IllegalArgumentException(INVALID_BONUSBALL_INPUT_MSG);
         }
     }
--- src/test/java/lotto/model/BonusBallTest.java
+++ src/test/java/lotto/model/BonusBallTest.java
@@ -3,7 +3,6 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.constant.BonusBallConstants;
-import lotto.constant.MoneyConstants;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -27,6 +26,18 @@ public class BonusBallTest {
     @ParameterizedTest
     @ValueSource(strings = {"1 "," 1"," 1 ","1 2","1a","1a2","a1b","1가나","가2나","가나1","abc","가나다","a가b나c"})
     void createBonusBallWithNotOnlyDigitsButAlsoChars(String userInput) {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> BonusBall.from(userInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(BonusBallConstants.INVALID_BONUSBALL_INPUT_MSG);
+    }
+
+    @DisplayName("보너스 볼 값이 1에서 45까지의 범위 내 숫자가 아닌 경우 이유를 나타내는 메세지를 포함한 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"0","46","111","111111111112","1234567891011"})
+    void createBonusBallByInvalidRangeValue(String userInput) {
         //given
         //when
         //then
