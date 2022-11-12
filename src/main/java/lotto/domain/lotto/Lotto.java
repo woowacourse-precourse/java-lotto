cf4b4310ef21ddf5bff3d91425c8abbcad84671d
JaehongDev <println@kakao.com>
2022-11-12 14:29:49 +0900

    feature (Lotto) : 로또 클래스의 요구사항 구현
    - 로또 클래스의 값은 6자리 고정
    - 로또 클래스의 값은 중복될 수 없음
    - 로또 클래스의 값은 오름차순으로 정렬되어야 함

---
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -0,0 +1,54 @@
+package lotto.domain.lotto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lotto {
+
+    // 인스턴스 변수는 추가하면 안되지만 클래스 변수는 추가 가능함
+    public final static String ERROR_LOTTO_NUMBER_SIZE = "[ERROR] 입력 받을 수 있는 로또의 크기는 6입니다.";
+    public static final String ERROR_LOTTO_NUMBER_DUPLICATED = "[ERROR] 로또 번호는 중복될 수 없습니다.";
+    private final static Integer FIX_SIZE = 6;
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validateNumbersSizeIsFixSize(numbers);
+
+        var generatedLotto = generateLotto(numbers);
+        validateLottoNumbersDuplicated(generatedLotto);
+
+        this.numbers = generatedLotto.stream()
+                .sorted()
+                .map(LottoNumber::number)
+                .collect(Collectors.toList());
+    }
+
+    private void validateLottoNumbersDuplicated(List<LottoNumber> generatedLotto) {
+        var size = generatedLotto.size();
+        var removeDuplicatedSize = (int) generatedLotto.stream().distinct().count();
+
+        if (size != removeDuplicatedSize) {
+            throw new IllegalArgumentException(ERROR_LOTTO_NUMBER_DUPLICATED);
+        }
+
+    }
+
+    private List<LottoNumber> generateLotto(List<Integer> numbers) {
+        return numbers.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+    }
+
+    private void validateNumbersSizeIsFixSize(List<Integer> numbers) {
+        if (numbers.size() != FIX_SIZE) {
+            throw new IllegalArgumentException(ERROR_LOTTO_NUMBER_SIZE);
+        }
+    }
+
+    public List<Integer> numbers() {
+        return this.numbers;
+    }
+
+    // TODO: 추가 기능 구현
+}
