9cf6c87d1f132bbac7fef69a2c060f3a4d4d18bf
lecocococo <chemwang13@naver.com>
2022-11-12 23:23:51 +0900

    refactor(inputView): Scanner 대신 Console을 사용하도록 수정
    - Scanner 대신 camp.nextstep.edu.missionutils의 Console.readline 을 사용하여 사용자의 입력을 읽도록 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,15 +1,19 @@
 package lotto.view;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String ASK_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String TYPE_WRONG = "[ERROR] 숫자만 입력해주세요";
-    private static final Scanner SCANNER = new Scanner(System.in);
+
 
     public static int inputAmount() {
         System.out.println(ASK_PURCHASE_AMOUNT);
-        return convertToInt(SCANNER.nextLine());
+        return convertToInt(Console.readLine());
     }
 
     private static int convertToInt(String input) {
