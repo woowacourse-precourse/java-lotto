436f4c8015eb2b15f1e5212fb6bada73006f21bd
leewoojin <zmfpdl64@naver.com>
2022-11-12 23:19:21 +0900

    Refactoring: Output 클래스 내부에 있던 Enum을 추출했다.

--- src/main/java/lotto/enums/Out.java
+++ src/main/java/lotto/enums/Out.java
@@ -1,2 +1,18 @@
-package lotto.enums;public enum Out {
+package lotto.enums;
+
+public enum Out {
+        BUY_LOTTERY("구입금액을 입력해 주세요"),
+        PRINT_BUY_LOTTERY("%d개를 구매했습니다."),
+        WIN_LOTTERY_NUMBER("당첨 번호를 입력해 주세요."),
+        BONUS_LOTTERY_NUMBER("보너스 번호를 입력해 주세요."),
+        PRINT_RESULT("당첨 통계%n---%n"),
+        YIELD("총 수익률은 %.1f%%입니다.%n");
+
+        private String output;
+        public String getOutput(){
+            return output;
+        }
+        private Out(String output){
+            this.output = output;
+        }
 }
