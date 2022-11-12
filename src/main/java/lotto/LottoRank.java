3af60f7683cb2b99d9ac756e9e697c2d5e63237a
김형기 <khk211113@naver.com>
2022-11-12 23:04:04 +0900

    chore: LottoRank에 꽝 추가

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -5,7 +5,8 @@ public enum LottoRank {
     SECOND("5개 일치, 보너스 볼 일치", 30000000),
     THIRD("5개 일치", 1500000),
     FOURTH("4개 일치", 50000),
-    FIFTH("3개 일치", 5000);
+    FIFTH("3개 일치", 5000),
+    LOSE("꽝", 0);
 
     private String description;
     private int prize;
