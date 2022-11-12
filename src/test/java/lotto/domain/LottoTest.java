6cbee40288da0a7298557463a5e28a1b42440cbd
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-12 21:17:13 +0900

    test(lotto) : 맞춘 번호의 개수 테스트 케이스 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -5,8 +5,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,5 +27,13 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("맞춘 번호의 개수를 올바르게 리턴")
+    @Test
+    void compareNumbers_case1() {
+        List<Integer> winningNumber = Stream.of(1, 2, 3, 4, 5, 6).collect(Collectors.toList());
+        WinningLotto winningLotto = new WinningLotto(winningNumber, 7);
+        List<Integer> myNumber = Stream.of(1, 2, 3, 4, 5, 6).collect(Collectors.toList());
+
+        assertThat(winningLotto.compareNumbers(myNumber)).isEqualTo(6);
+    }
 }
