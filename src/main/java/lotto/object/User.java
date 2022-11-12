f2839a65e0ee4d9636487861550cdc7b66c4c9e8
leewoojin <zmfpdl64@naver.com>
2022-11-12 14:38:06 +0900

    Feat: User 클래스 돈을 입력 받아 복권 갯수 발행 메서드
    Input 클래스를 상속받아 사용자에게 금액을 받고 복권 갯수를 저장한다.

--- src/main/java/lotto/object/User.java
+++ src/main/java/lotto/object/User.java
@@ -1,4 +1,26 @@
 package lotto.object;
 
-public class User {
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.input_output.Input;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User extends Input {
+    /*
+    ### User
+
+- [ ] 구입 금액 입력
+- [ ] 1000원 당 6자리 로또 발행
+- [ ] 발행한 로또 추가하는 함수
+
+     */
+    private List<List<Integer>> lottery_nums = new ArrayList<>();
+    private Integer lottery_count;
+
+    public void pay_Money(){// 구입 금액 입력
+        int money = returnInt();
+        this.lottery_count = money / 1000;
+    }
+
 }
