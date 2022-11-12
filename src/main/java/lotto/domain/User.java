9767ffd715dd19c5d81d973be2cdb5ca8a3b12be
MinjeongKong <kong1301@ajou.ac.kr>
2022-11-12 18:14:07 +0900

    feat: 로또 구입 금액 입력

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,21 +1,39 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class User {
+    public static final int lottoPrice = 1000;
     private int money;
     private List<Lotto> lottos;
 
     public User() {
     }
 
-    public void buy() {
-
+    public void buy() throws IllegalArgumentException {
+        String amount = Console.readLine();
+        validate(amount);
     }
 
-    private void validate() {
-
+    private void validate(String amount) throws IllegalArgumentException {
+        isNumeric(amount);
+        isCorrectPrice(amount);
     }
+
+    private void isNumeric(String amount) throws IllegalArgumentException {
+        if (!amount.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isCorrectPrice(String amount) throws IllegalArgumentException {
+        if (Integer.parseInt(amount) % lottoPrice != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 
     private void setMoney(int money) {
         this.money = money;
