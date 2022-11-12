928a0932453fa22455afe133a3c0d3402a1cd3c6
kanghyki <kanghyki@gmail.com>
2022-11-13 05:59:56 +0900

    style: 출력 메서드 네이밍 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,14 +6,14 @@ public class Application {
     public static void main(String[] args) {
         int money = LottoInStream.readAmount();
         List<Lotto> lottoes = LottoGame.buyLotto(money);
-        LottoOutStream.outStreamLottoesNumber(lottoes);
+        LottoOutStream.showLottoList(lottoes);
 
         Lotto winningLotto = new Lotto(LottoInStream.readLottoNumbers());
         int bonusNumber = LottoInStream.readBonusNumber(winningLotto);
 
         int[] matches = LottoGame.getResultLotto(lottoes, winningLotto, bonusNumber);
 
-        LottoOutStream.outStreamLottoResult(matches);
-        LottoOutStream.outStreamRateOfReturn(LottoGame.calcRateOfReturn(matches, money));
+        LottoOutStream.showLottoResult(matches);
+        LottoOutStream.showRateOfReturn(LottoGame.calcRateOfReturn(matches, money));
     }
 }
--- src/main/java/lotto/LottoOutStream.java
+++ src/main/java/lotto/LottoOutStream.java
@@ -7,7 +7,7 @@ public class LottoOutStream {
     private static final String _input_buy_str = "구입금액을 입력해 주세요.\n";
     private static final String _input_numbers_str = "당첨 번호를 입력해 주세요.\n";
     private static final String _input_bonus_number_str = "보너스 번호를 입력해 주세요.\n";
-    public static void outStreamLottoResult (int[] matches) {
+    public static void showLottoResult (int[] matches) {
         System.out.print("당첨 통계\n---\n");
         System.out.print("3개 일치 (5,000원) - " + matches[LottoGame._fifth_place] + "개\n");
         System.out.print("4개 일치 (50,000원) - " + matches[LottoGame._fourth_place] + "개\n");
@@ -16,11 +16,11 @@ public class LottoOutStream {
         System.out.print("6개 일치 (2,000,000,000원) - " + matches[LottoGame._first_place] + "개\n");
     }
 
-    public static void outStreamRateOfReturn(BigDecimal rateOfReturn) {
+    public static void showRateOfReturn(BigDecimal rateOfReturn) {
         System.out.print("총 수익률은 " + rateOfReturn + "%입니다.");
     }
 
-    public static void outStreamLottoesNumber(List<Lotto> lottoes) {
+    public static void showLottoList(List<Lotto> lottoes) {
         for (Lotto lotto: lottoes) {
             lotto.showNumbers();
         }
