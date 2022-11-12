6b6ad45af9b2eda4761034689c8251b1c26b5267
황민주 <minjoo@hwangminjooui-MacBookAir.local>
2022-11-12 13:54:02 +0900

    feat(Utility): createLotto (src/main/java/lotto/Utility.java)
    createLotto 메소드 추가:
    -1부터 45까지의 숫자 중 랜덤으로 6개의 숫자를 중복 없이 추출해 1개의 로또를 생성하기 위해 추가함

--- src/main/java/lotto/Utility.java
+++ src/main/java/lotto/Utility.java
@@ -1,6 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
 
 public abstract class Utility {
 
@@ -15,5 +18,11 @@ public abstract class Utility {
 
     public long calculateQuotient(long dividend, long divisor){
         return dividend/divisor;
+    }
+
+    public Lotto createLotto(){
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Lotto lotto = new Lotto(lottoNumbers);
+        return lotto;
     }
 }
