b190f3a5865342b27082868d76a9c648fdcf104f
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-12 19:43:15 +0900

    refactor : 입력 기능 리팩토링에 따른 로또 가격 형 변환

--- src/main/java/lotto/Setup.java
+++ src/main/java/lotto/Setup.java
@@ -2,18 +2,27 @@ package lotto;
 
 public enum Setup {
 
-	LOTTO_PRICE(1000),
+	LOTTO_PRICE(1000L),
 	LOTTO_FIRST_NUMBER_INCLUSIVE(1),
 	LOTTO_LAST_NUMBER_INCLUSIVE(45),
 	LOTTO_NUMBER_DIGITS(6);
 
-	private final int value;
+	private int value;
+	private long money;
 
 	Setup(int value) {
 		this.value = value;
 	}
 
+	Setup(long money) {
+		this.money = money;
+	}
+
 	public int getValue() {
 		return value;
+	}
+
+	public long getMoney() {
+		return money;
 	}
 }
