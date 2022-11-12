a2f69bbd634d4bb3b9cc27773c896e1fbfff85c6
김태용 <amico741@naver.com>
2022-11-13 02:47:46 +0900

    feat: 당첨번호 숫자 중복 검사 기능 추가

--- src/main/java/lotto/domain/WinningTicket.java
+++ src/main/java/lotto/domain/WinningTicket.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class WinningTicket {
     private static final String RANGE_EXCEED_MESSAGE = "[ERROR] 숫자는 1 ~ 45로 입력할 수 있습니다.";
     private static final String LENGTH_MESSAGE = "[ERROR] 6개의 숫자를 입력해야 합니다.";
+    private static final String OVERLAP_MESSAGE = "[ERROR] 중복되는 숫자가 존재합니다.";
     private static final int MAX_SIZE = 6;
     private List<Integer> winnings;
     private int bonusNumber;
@@ -13,6 +15,7 @@ public class WinningTicket {
         try {
             validateNumberRange(winnings);
             validateLength(winnings);
+            validateOverlap(winnings);
             this.winnings = winnings;
             this.bonusNumber = bonusNumber;
         } catch(IllegalArgumentException e) {
@@ -27,6 +30,12 @@ public class WinningTicket {
     public void validateLength(List<Integer> winnings) throws IllegalArgumentException {
         if(winnings.size() != MAX_SIZE) {
             throw new IllegalArgumentException(LENGTH_MESSAGE);
+        }
+    }
+
+    public void validateOverlap(List<Integer> winnings) throws IllegalArgumentException {
+        if(new HashSet<>(winnings).size() != MAX_SIZE) {
+            throw new IllegalArgumentException(OVERLAP_MESSAGE);
         }
     }
 
