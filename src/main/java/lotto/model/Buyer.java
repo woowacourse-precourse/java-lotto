5cb471681ca2201bf8ff69c8fc646fc01653a8c0
gomljo <gomljo0307@naver.com>
2022-11-12 23:08:20 +0900

    feat: 구매 금액이 최소 구매 금액 단위인지 확인하는 기능 구현

--- src/main/java/lotto/model/Buyer.java
+++ src/main/java/lotto/model/Buyer.java
@@ -1,15 +1,25 @@
 package lotto.model;
 
 public class Buyer {
+
     private static final int AMOUNT_UNIT=1000;
+    private static final String ENTER_WRONG_AMOUNT_MESSAGE="[ERROR] 금액의 최소 단위(1,000원)가 맞지 않습니다. 프로그램을 종료합니다.";
+    private static final int PROPER_AMOUNT_CONDITION=0;
     private int purchaseAmount;
     private int ticketAmount;
 
     public Buyer(int purchaseAmount){
+
         this.purchaseAmount=purchaseAmount;
+        validateProperUnitAmount(this.purchaseAmount);
         calculateAmountOfTickets();
     }
     private void calculateAmountOfTickets(){
         ticketAmount=purchaseAmount/AMOUNT_UNIT;
+    }
+    private void validateProperUnitAmount(int purchaseAmount){
+        if(purchaseAmount%AMOUNT_UNIT!=PROPER_AMOUNT_CONDITION){
+            throw new IllegalArgumentException(ENTER_WRONG_AMOUNT_MESSAGE);
+        }
     }
 }
