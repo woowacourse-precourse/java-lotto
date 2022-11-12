031b792d34916f301d1eb2a790269f5800c7c472
JuHyeokParkk <hyeok5596@naver.com>
2022-11-13 00:14:26 +0900

    feat: 주어진 로또에 대해 당첨 번호와 일치하는 번호의 개수를 계산하는 메서드 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,6 +13,18 @@ public class LottoMachine {
     public static int bonusNumber;
     public InputController winNumberInputController = new WinNumberInputController();
     public BonusNumberInputController bonusNumberInputController = new BonusNumberInputController();
+    public HashMap<Long, WinPrize> winPrizeRecording;
+
+    public LottoMachine() {
+
+        winPrizeRecording = new HashMap<>();
+
+        winPrizeRecording.put(WinPrize.ONE_GRADE.matchCount, WinPrize.ONE_GRADE);
+        winPrizeRecording.put(WinPrize.THREE_GRADE.matchCount, WinPrize.THREE_GRADE);
+        winPrizeRecording.put(WinPrize.FOUR_GRADE.matchCount, WinPrize.FOUR_GRADE);
+        winPrizeRecording.put(WinPrize.FIVE_GRADE.matchCount, WinPrize.FIVE_GRADE);
+
+    }
 
     public List<Lotto> issueLottoNumbers(long count) {
 
@@ -54,4 +67,39 @@ public class LottoMachine {
     public int getBonusNumber() {
         return bonusNumber;
     }
+
+    public WinPrize getWinPrize(Lotto buyerLotto) {
+
+        long matchingCount = getMatchingCount(buyerLotto);
+
+        boolean isBonusNumberMatching = checkBonusNumberMatching(buyerLotto);
+
+        WinPrize buyerWinPrize = winPrizeRecording.get(matchingCount);
+
+        if(matchingCount == WinPrize.THREE_GRADE.matchCount && isBonusNumberMatching) {
+            return WinPrize.TWO_GRADE;
+        }
+
+        if(matchingCount >= 3) {
+            return buyerWinPrize;
+        }
+
+        return WinPrize.NOTHING;
+    }
+
+    public long getMatchingCount(Lotto buyerLotto) {
+
+        return buyerLotto.getLotto().stream()
+                .filter(
+                number -> winNumber.getLotto().contains(number))
+                .count();
+    }
+
+    public boolean checkBonusNumberMatching(Lotto buyerLotto) {
+
+        return buyerLotto.getLotto().stream()
+                .anyMatch(number -> number == getBonusNumber());
+    }
+
+
 }
