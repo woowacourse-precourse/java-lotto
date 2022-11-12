de4d5161b4ece6272b9b4ec1cbba51bdb9e034a0
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-12 19:09:32 +0900

    test: 로또번호와 당첨번호 비교하여 등수 산출 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,9 +5,42 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
+    @Test
+    void checkWinning3(){
+        List<Integer> winningNUmbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
+        int rank = lotto.checkWinning(winningNUmbers, bonusNumber);
+        int result = 3;
+        assertThat(rank).isEqualTo(result);
+    }
+
+    @Test
+    void checkWinning2(){
+        List<Integer> winningNUmbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        int rank = lotto.checkWinning(winningNUmbers, bonusNumber);
+        int result = 2;
+        assertThat(rank).isEqualTo(result);
+    }
+
+    @Test
+    void checkWinning1(){
+        List<Integer> winningNUmbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int rank = lotto.checkWinning(winningNUmbers, bonusNumber);
+        int result = 1;
+        assertThat(rank).isEqualTo(result);
+    }
+
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
