1f49c6524606874f07c33707e01cce44bed69bca
yejin <kwonyj1022@naver.com>
2022-11-13 00:13:01 +0900

    feat: Lotto 클래스 - 로또 번호 유효성 검사 구현 완료

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -12,9 +13,29 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 당첨번호는 6개여야 합니다");
+        }
+        if (!numbersInRange(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (!numbersAllDifferent(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 없어야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean numbersInRange(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++)
+            if ((numbers.get(i) < 1) || (numbers.get(i) > 45))
+                return false;
+        return true;
+    }
+
+    private boolean numbersAllDifferent(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            int tmp = numbers.get(i);
+            if (Collections.frequency(numbers, tmp) > 1)
+                return false;
+        }
+        return true;
+    }
 }
