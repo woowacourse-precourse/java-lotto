7ab7c1f295711af8d8f7ea072d7fb1aba8a2b2f6
5nam <ohnam90@gmail.com>
2022-11-12 15:13:40 +0900

    feat: 입력한 로또 번호가 6자리가 아닐때 예외처리 기능 추가

--- src/main/java/lotto/domain/LottoDesignator.java
+++ src/main/java/lotto/domain/LottoDesignator.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 public class LottoDesignator {
     private static final String INT_REGEX = "^[0-9]*$";
+    private static final int CIPHER = 6;
 
     public void checkOnlyNumber(String input) {
         String lotto = Arrays
@@ -12,6 +13,13 @@ public class LottoDesignator {
                 .collect(Collectors.joining());
         if(!lotto.matches(INT_REGEX)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자만 입력 가능합니다.(구분자 쉼표만 가능)");
+        }
+    }
+
+    public void checkSixCipher(String input) {
+        String[] lotto = removeComma(input);
+        if(lotto.length != CIPHER) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개를 입력해야 합니다.");
         }
     }
 
