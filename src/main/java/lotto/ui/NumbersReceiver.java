91c02b9ba8e2424606b1ae9aec9ad9ed5ca3550a
KSH <tkdgns543@gmail.com>
2022-11-12 14:32:52 +0900

    feat(ui): 구매 금액 입력 구현

---
+++ src/main/java/lotto/ui/NumbersReceiver.java
@@ -0,0 +1,11 @@
+package lotto.ui;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class NumbersReceiver {
+    NumbersReceiver() {}
+
+    public int insertMoney() {
+        return Integer.parseInt(Console.readLine());
+    }
+}
