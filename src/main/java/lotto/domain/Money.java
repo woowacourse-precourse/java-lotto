bbacbdb682c6f8ee37995fcee20ac17272f3079c
Heebeom Yang <takeny1998@gmail.com>
2022-11-12 13:50:37 +0900

    feat: add money
    Money 객체 구현
    Money 객체를 검사하는 Test Code 구현

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+public class Money {
+    private int money;
+
+    public Money(String input) {
+        validate(input);
+        this.money = Integer.parseInt(input);
+    }
+
+    private void validate(String input) {
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException(ErrorMessages.MONEY_NOT_INTEGER.get());
+            }
+        }
+
+        int money = Integer.parseInt(input);
+        if (money < 1000) {
+            throw new IllegalArgumentException(ErrorMessages.MONEY_UNDER_1000.get());
+        }
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException(ErrorMessages.MONEY_NOT_DIVIDED_BY_1000.get());
+        }
+    }
+}
