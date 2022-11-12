f3e1b72a303b0983c3816822a84a55e0e103cfd1
leewoojin <zmfpdl64@naver.com>
2022-11-12 23:46:29 +0900

    Test: PlayLottoTest 로또 번호와 보너스 번호 확인 테스트 구현

--- src/test/java/lotto/playlotto/PlayLottoTest.java
+++ src/test/java/lotto/playlotto/PlayLottoTest.java
@@ -1,4 +1,82 @@
+package lotto.playlotto;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-class PlayLottoTest {
-  
+
+class PlayLottoTest extends NsTest {
+
+    @Test
+    void play() {
+    }
+
+    @Test
+    void buy_lottery() {
+    }
+
+    @Test
+    void print_lottery_nums() {
+    }
+
+    @Test
+    void create_lotto() {
+    }
+
+    @Test
+    void create_bonus() {
+    }
+
+    @Test
+    void compare_lottery() {
+    }
+
+    @Test
+    void equal_count() {
+
+    }
+
+    @Test
+    void check_num() {
+        PlayLotto lotto = new PlayLotto();
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
+        Integer num = 5;
+        Boolean condition = lotto.check_num(list, 5);
+        assertThat(condition).isTrue();
+    }
+
+    @Test
+    void check_bonus() {
+        PlayLotto lotto = new PlayLotto();
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
+        Integer bonus = 5;
+        Boolean condition = lotto.check_bonus(list, 5);
+        assertThat(condition).isTrue();
+
+    }
+
+    @Test
+    void check_result() {
+    }
+
+    @Test
+    void print_result() {
+
+    }
+
+    @Test
+    void print_yeild() {
+    }
+
+    @Test
+    void cal_revenue() {
+    }
+
+    @Override
+    protected void runMain() {
+        PlayLotto.main(new String[]{});
+    }
 }
