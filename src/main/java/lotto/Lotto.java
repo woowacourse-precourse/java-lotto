ef372adcfe553c063a7c252b5964fb1a41a3ea09
dsaf2007 <dsaf2007@naver.com>
2022-11-12 20:25:59 +0900

    feat : 입력 숫자가 범위 내에 존제하는지 검증 하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,4 +17,17 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+    //입력한 숫자가 (1, 45) 인 수인지 검증 
+    protected void isNumberInRange() {
+        for (Integer numbers_ : this.numbers) {
+            if (numbers_ > 45) {
+                throw new IllegalArgumentException("[ERROR] input bigger than 45");
+            }
+            if (numbers_ < 1) {
+                throw new IllegalArgumentException("[ERROR] input smaller than 1");
+            }
+        }
+    }
+    
 }
