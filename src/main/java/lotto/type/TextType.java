19378ac5acb1040a2421f72a860002e8d3ebb3b1
win9 <krrr8@naver.com>
2022-11-12 21:34:05 +0900

    Refactor(Type): Enum 이름 수정
    명확한 역할에 맞추어 좀더 명시적인 네이밍으로 수정

--- src/main/java/lotto/type/TextType.java
+++ src/main/java/lotto/type/TextType.java
@@ -2,7 +2,7 @@ package lotto.type;
 
 public enum TextType {
     START("구입금액을 입력해 주세요."),
-    LOTTO_WIN_NUMBER("당첨 번호를 입력해 주세요"),
+    LOTTO_PRIZE_NUMBER("당첨 번호를 입력해 주세요"),
     PRICE_TO_NUMBER("개를 구매했습니다."),
     LOTTO_BONUS("보너스 번호를 입력해 주세요."),
     WINNING_STATISTICS("당첨 통계.");
