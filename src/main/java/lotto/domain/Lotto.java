cea05d2aa54d282fb36e598927d2b2f891d89ee0
sangeun-b <baek0007@algonquinlive.com>
2022-11-13 07:28:22 +0900

    feat: 로또 번호 중복 체크 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,13 +2,16 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplication(numbers);
         this.numbers = numbers;
     }
 
@@ -18,7 +21,12 @@ public class Lotto {
         }
     }
 
-    // todo: 중복 확인
+    private void validateDuplication(List<Integer> numbers){
+        Set<Integer> copyNumbers = new HashSet<>(numbers);
+        if(copyNumbers.size()!=numbers.size()){
+            throw new IllegalArgumentException("[ERROR] 숫자는 중복되지 않아야합니다.");
+        }
+    }
 
     public List<Integer> getLotto(){
         return numbers;
