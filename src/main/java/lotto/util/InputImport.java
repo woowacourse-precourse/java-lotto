d537e4900e5349d39908bb1a279105680ebbc408
seonghoo1217 <seonghoo1217@naver.com>
2022-11-12 21:49:19 +0900

    feat : 값 입력(ReadLine)을 클래스화 시킴
    - 입력메서드의 재사용성을 높여 효율성을 높이기위해 클래스화시킨다.

---
+++ src/main/java/lotto/util/InputImport.java
@@ -0,0 +1,9 @@
+package lotto.util;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputImport {
+	public static String ReadLine(){
+		String user_input_value = Console.readLine();
+		return user_input_value;
+	}
+}
