e346032166cfa14659dbe855260b6523aefee90e
박승민 <tmddudals369@naver.com>
2022-11-12 16:51:37 +0900

    test: 당첨 결과 별도의 enum타입 분리로 인한 테스트 코드 변경

--- src/test/java/lotto/domain/ResultTest.java
+++ src/test/java/lotto/domain/ResultTest.java
@@ -2,7 +2,6 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.HashMap;
 import java.util.List;
 import lotto.Lotto;
 import org.junit.jupiter.api.DisplayName;
@@ -15,16 +14,18 @@ class ResultTest {
         List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumber = 7;
         List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
-
-        Result result = new Result();
-        result.initRank(winningNumber, bonusNumber, lottos);
-        HashMap<Integer, Integer> rank = result.getRank();
-
-        assertThat(rank.get(1)).isEqualTo(1);
-        assertThat(rank.get(2)).isEqualTo(0);
-        assertThat(rank.get(3)).isEqualTo(0);
-        assertThat(rank.get(4)).isEqualTo(0);
-        assertThat(rank.get(5)).isEqualTo(0);
+        int count = (int) lottos.get(0).getNumbers().stream()
+                .filter(winningNumber::contains)
+                .count();
+        boolean contains = lottos.get(0).getNumbers().contains(bonusNumber);
+        Rank rank = Rank.valueOfRank(count, contains);
+        rank.count++;
+        Rank[] values = Rank.values();
+        assertThat(values[0].getCount()).isEqualTo(1);
+        assertThat(values[1].getCount()).isEqualTo(0);
+        assertThat(values[2].getCount()).isEqualTo(0);
+        assertThat(values[3].getCount()).isEqualTo(0);
+        assertThat(values[4].getCount()).isEqualTo(0);
     }
     @Test
     @DisplayName("당첨 번호와 로또 번호가 5개 그리고 보너스 번호가 일치할 경우 2등")
@@ -32,16 +33,18 @@ class ResultTest {
         List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumber = 7;
         List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 7)));
-
-        Result result = new Result();
-        result.initRank(winningNumber, bonusNumber, lottos);
-        HashMap<Integer, Integer> rank = result.getRank();
-
-        assertThat(rank.get(1)).isEqualTo(0);
-        assertThat(rank.get(2)).isEqualTo(1);
-        assertThat(rank.get(3)).isEqualTo(0);
-        assertThat(rank.get(4)).isEqualTo(0);
-        assertThat(rank.get(5)).isEqualTo(0);
+        int count = (int) lottos.get(0).getNumbers().stream()
+                .filter(winningNumber::contains)
+                .count();
+        boolean contains = lottos.get(0).getNumbers().contains(bonusNumber);
+        Rank rank = Rank.valueOfRank(count, contains);
+        rank.count++;
+        Rank[] values = Rank.values();
+        assertThat(values[0].getCount()).isEqualTo(0);
+        assertThat(values[1].getCount()).isEqualTo(1);
+        assertThat(values[2].getCount()).isEqualTo(0);
+        assertThat(values[3].getCount()).isEqualTo(0);
+        assertThat(values[4].getCount()).isEqualTo(0);
     }
     @Test
     @DisplayName("당첨 번호와 로또 번호가 5개가 일치할 경우 3등")
@@ -49,16 +52,18 @@ class ResultTest {
         List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumber = 7;
         List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 8)));
-
-        Result result = new Result();
-        result.initRank(winningNumber, bonusNumber, lottos);
-        HashMap<Integer, Integer> rank = result.getRank();
-
-        assertThat(rank.get(1)).isEqualTo(0);
-        assertThat(rank.get(2)).isEqualTo(0);
-        assertThat(rank.get(3)).isEqualTo(1);
-        assertThat(rank.get(4)).isEqualTo(0);
-        assertThat(rank.get(5)).isEqualTo(0);
+        int count = (int) lottos.get(0).getNumbers().stream()
+                .filter(winningNumber::contains)
+                .count();
+        boolean contains = lottos.get(0).getNumbers().contains(bonusNumber);
+        Rank rank = Rank.valueOfRank(count, contains);
+        rank.count++;
+        Rank[] values = Rank.values();
+        assertThat(values[0].getCount()).isEqualTo(0);
+        assertThat(values[1].getCount()).isEqualTo(0);
+        assertThat(values[2].getCount()).isEqualTo(1);
+        assertThat(values[3].getCount()).isEqualTo(0);
+        assertThat(values[4].getCount()).isEqualTo(0);
     }
     @Test
     @DisplayName("당첨 번호와 로또 번호가 4개가 일치할 경우 4등")
@@ -66,16 +71,18 @@ class ResultTest {
         List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumber = 7;
         List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 9, 10)));
-
-        Result result = new Result();
-        result.initRank(winningNumber, bonusNumber, lottos);
-        HashMap<Integer, Integer> rank = result.getRank();
-
-        assertThat(rank.get(1)).isEqualTo(0);
-        assertThat(rank.get(2)).isEqualTo(0);
-        assertThat(rank.get(3)).isEqualTo(0);
-        assertThat(rank.get(4)).isEqualTo(1);
-        assertThat(rank.get(5)).isEqualTo(0);
+        int count = (int) lottos.get(0).getNumbers().stream()
+                .filter(winningNumber::contains)
+                .count();
+        boolean contains = lottos.get(0).getNumbers().contains(bonusNumber);
+        Rank rank = Rank.valueOfRank(count, contains);
+        rank.count++;
+        Rank[] values = Rank.values();
+        assertThat(values[0].getCount()).isEqualTo(0);
+        assertThat(values[1].getCount()).isEqualTo(0);
+        assertThat(values[2].getCount()).isEqualTo(0);
+        assertThat(values[3].getCount()).isEqualTo(1);
+        assertThat(values[4].getCount()).isEqualTo(0);
     }
     @Test
     @DisplayName("당첨 번호와 로또 번호가 3개가 일치할 경우 5등")
@@ -83,16 +90,18 @@ class ResultTest {
         List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumber = 7;
         List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 11, 15, 16)));
-
-        Result result = new Result();
-        result.initRank(winningNumber, bonusNumber, lottos);
-        HashMap<Integer, Integer> rank = result.getRank();
-
-        assertThat(rank.get(1)).isEqualTo(0);
-        assertThat(rank.get(2)).isEqualTo(0);
-        assertThat(rank.get(3)).isEqualTo(0);
-        assertThat(rank.get(4)).isEqualTo(0);
-        assertThat(rank.get(5)).isEqualTo(1);
+        int count = (int) lottos.get(0).getNumbers().stream()
+                .filter(winningNumber::contains)
+                .count();
+        boolean contains = lottos.get(0).getNumbers().contains(bonusNumber);
+        Rank rank = Rank.valueOfRank(count, contains);
+        rank.count++;
+        Rank[] values = Rank.values();
+        assertThat(values[0].getCount()).isEqualTo(0);
+        assertThat(values[1].getCount()).isEqualTo(0);
+        assertThat(values[2].getCount()).isEqualTo(0);
+        assertThat(values[3].getCount()).isEqualTo(0);
+        assertThat(values[4].getCount()).isEqualTo(1);
     }
 
     @Test
@@ -101,9 +110,14 @@ class ResultTest {
         List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumber = 7;
         List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 11, 15, 16)));
+        int count = (int) lottos.get(0).getNumbers().stream()
+                .filter(winningNumber::contains)
+                .count();
+        boolean contains = lottos.get(0).getNumbers().contains(bonusNumber);
+        Rank rank = Rank.valueOfRank(count, contains);
+        rank.count++;
 
-        Result result = new Result();
-        result.initRank(winningNumber, bonusNumber, lottos);
+        Result result = new Result(Rank.values());
         result.initWinningAmount(8000);
 
         assertThat(String.format("%.1f", result.getWinningAmount())).isEqualTo("62.5");
