abbd4b2a30ab95ba58d00918f77d7b573b34a9d1
greeng00se <thegreengoose7@gmail.com>
2022-11-12 17:12:18 +0900

    feat(LottoInformationDto): 로또 구매 정보를 담는 LottoInformationDto 클래스 추가

---
+++ src/main/java/lotto/dto/LottoInformationDto.java
@@ -0,0 +1,22 @@
+package lotto.dto;
+
+import java.util.List;
+
+public class LottoInformationDto {
+
+    private List<List<Integer>> lottoTicket;
+    private Integer size;
+
+    public LottoInformationDto(List<List<Integer>> lottoTicket) {
+        this.lottoTicket = lottoTicket;
+        this.size = lottoTicket.size();
+    }
+
+    public List<List<Integer>> getLottoTicket() {
+        return lottoTicket;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+}
