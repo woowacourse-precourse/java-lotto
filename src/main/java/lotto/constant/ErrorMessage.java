bad2c10daa95bc05160a7bfa7b715577f3defafc
JunginPark4364 <wjddlsee@gmail.com>
2022-11-12 19:40:59 +0900

    feat: 입력한 당첨번호 검증하는 기능

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -3,6 +3,9 @@ package lotto.constant;
 public class ErrorMessage {
     public static final String ERROR = "[ERROR] ";
     public static final String ERROR_MONEY = "구입금액은 0이상의 1000단위 숫자여야 합니다.";
+    public static final String ERROR_COMMA_DELIMITER = "쉼표로 구분해야 합니다.";
+    public static final String ERROR_DUPLICATE_SIZE = "중복없는 6개 숫자여야 합니다.";
+    public static final String ERROR_RANGE = "1 ~ 45 숫자여야 합니다.";
 
     private ErrorMessage() {}
 }
--- src/main/java/lotto/validator/Validator.java
+++ src/main/java/lotto/validator/Validator.java
@@ -2,11 +2,65 @@ package lotto.validator;
 
 import lotto.constant.ErrorMessage;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static lotto.constant.ErrorMessage.*;
+
 public class Validator {
+    public static final int LOTTO_COUNT = 6;
 
     public void validateMoney(int money) {
         if (money < 0 || money % 1000 != 0) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_MONEY);
+            throw new IllegalArgumentException(ERROR_MONEY);
         }
+    }
+
+    public void validateWinningNumbers(String numbers) {
+        Set<String> uniqueNumbers;
+
+        // 쉼표 개수 != 로또 번호 개수
+        if (!hasCommaDelimiter(numbers)) {
+            throw new IllegalArgumentException(ERROR_COMMA_DELIMITER);
+        }
+
+        uniqueNumbers = new HashSet<>(Arrays.asList(numbers.split(",")));
+
+        // Set 을 통한 중복 체크
+        if (!isLottoCount(uniqueNumbers)) {
+            throw new IllegalArgumentException(ERROR_DUPLICATE_SIZE);
+        }
+
+        // 1 ~ 45 인지 체크
+        if (!isRange(uniqueNumbers)) {
+            throw new IllegalArgumentException(ERROR_RANGE);
+        }
+    }
+
+    private boolean hasCommaDelimiter(String numbers) {
+        return numbers.contains(",");
+    }
+
+    private boolean isLottoCount(Set<String> uniqueNumbers) {
+        return uniqueNumbers.size() == LOTTO_COUNT;
+    }
+
+    private boolean isRange(Set<String> uniqueNumbers) {
+        int number = 0;
+
+        for (String uniqueNumber : uniqueNumbers) {
+            try {
+                number = Integer.parseInt(uniqueNumber);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+
+            if (number < 1 || number > 45) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
