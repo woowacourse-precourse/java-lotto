0d313089497a8e8d6e48687b9be0720554026dfc
nswon <swnam0729@gmail.com>
2022-11-12 18:57:10 +0900

    feat: 로또발표자 객체 구현

---
+++ src/main/java/lotto/domain/Presenter.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Presenter {
+
+    public String inputWinLottoNumbers() {
+        return Console.readLine();
+    }
+
+    public String inputBonusNumber() {
+        return Console.readLine();
+    }
+}
