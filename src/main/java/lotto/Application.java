053e0e991f4409dcac12f65a56c1321d98c2ba5a
Jenn Kim <grdd0419@gmail.com>
2022-11-13 00:32:28 +0900

    feat: 당첨 번호를 입력받는 메서드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,7 +2,8 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -21,5 +22,27 @@ public class Application {
         if (money % 1000 != 0) throw new IllegalArgumentException("[ERROR]Input error");
 
         return money;
+    }
+
+    public static Lotto getWinningNumber() {
+        String winningNumberInput = Console.readLine();
+
+        if(!winningNumberInput.contains(","))
+            throw new IllegalArgumentException("[ERROR]Winning number error");
+
+        String[] winningNumberSplit = ",".split(winningNumberInput);
+        if(winningNumberSplit.length != 6)
+            throw new IllegalArgumentException("[ERROR]Winning number error");
+
+        List<Integer> winningNunmbers = new ArrayList<>();
+        for(String num : winningNumberSplit) {
+            try {
+                winningNunmbers.add(Integer.parseInt(num));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("[ERROR]Winning number error");
+            }
+        }
+
+        return new Lotto(winningNunmbers);
     }
 }
