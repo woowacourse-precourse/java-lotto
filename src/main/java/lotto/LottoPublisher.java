d90cf365be3d8ad3479aaa1735f972117c7161d8
wjh2012 <wjh2012@naver.com>
2022-11-12 16:09:26 +0900

    feat(LottoPublisher): 금액에 맞는 로또 발급

--- src/main/java/lotto/LottoPublisher.java
+++ src/main/java/lotto/LottoPublisher.java
@@ -2,12 +2,28 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static lotto.config.Constants.LottoNumber.*;
 import static lotto.config.Constants.LottoPrice.LOTTO_PRICE;
 
 public class LottoPublisher {
+
+    private List<Lotto> lottos;
+
+    public LottoPublisher() {
+        this.lottos = new ArrayList<>();
+    }
+
+    public void publish(int money) {
+
+        validate(money);
+
+        for (int i = 0; i < affordableLotto(money); i++) {
+            lottos.add(new Lotto(getRandomLottoNumber()));
+        }
+    }
 
     private void validate(int money) {
         if (money % LOTTO_PRICE != 0) {
@@ -19,7 +35,11 @@ public class LottoPublisher {
         return money / LOTTO_PRICE;
     }
 
-    private List<Integer> publishLottoNumber() {
+    private List<Integer> getRandomLottoNumber() {
         return Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_START_INCLUSIVE, LOTTO_NUMBER_END_INCLUSIVE, LOTTO_SIZE);
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
     }
 }
