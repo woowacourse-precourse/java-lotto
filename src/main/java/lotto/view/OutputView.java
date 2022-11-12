931c2cd04298e33a2b3b33f737410d8132fd0d5d
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-13 00:03:49 +0900

    feat: startInput_Output()-> 개를 구매했습니다 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,5 +4,7 @@ public class OutputView{
     private static final String MoneyOutput = "개를 구매했습니다.";
     private static final String NumberOutput = "당첨 통계";
 
-
+    public void startInput_Output(){
+        System.out.println(MoneyOutput);
+    }
 }
