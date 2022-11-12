88b69d958e8bd5c189c113d0d4617413e00a1f06
cmong0516 <cmong0516@gmail.com>
2022-11-12 19:04:20 +0900

    feat(Application Class): 구입금액 Input Try Catch
    요구사항에 따라 구입희망 금액 입력시 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,23 +1,57 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         LottoMachine lottoMachine = new LottoMachine();
-        User customer = new User();
+        User user = new User();
 
         lottoMachine.requestWantBuyAmountInputMessage();
 
         int wantBuyAmountInput = 0;
 
         try {
-            wantBuyAmountInput = customer.wantBuyAmountInput();
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 1000 단위 이상의 숫자를 입력해주세요.");
+            wantBuyAmountInput = user.wantBuyAmountInput();
+        } catch (Exception e) {
+            wantBuyAmountInput = user.wantBuyAmountInput();
         }
+
 
         int canBuyAmount = lottoMachine.canBuyAmount(wantBuyAmountInput);
 
         lottoMachine.canBuyAmountMessage(canBuyAmount);
+
+        List<Lotto> lottoList = lottoMachine.lottoList(canBuyAmount);
+
+        for (Lotto lotto : lottoList) {
+            System.out.println(lotto.getNumbers());
+        }
+
+        lottoMachine.requestWinningNumbersMessage();
+        String winningNumbersInput = user.winningNumbersInput();
+        List<Integer> winningNumberList = user.winningNumbersToList(winningNumbersInput);
+
+        lottoMachine.requestBonusNumbersMessage();
+
+        int bonusNumberInput = user.bonusNumberInput();
+        user.validBonusNumberInputContainWinningNumbers(bonusNumberInput, winningNumberList);
+
+        lottoMachine.scoreBoardMessage();
+
+        List<Integer> correctCntList = lottoMachine.countCorrect(lottoList, winningNumberList);
+
+        List<Boolean> containBonusNumberList = lottoMachine.isContainBonusNumberList(lottoList, bonusNumberInput);
+
+        int[] scoreBoard = lottoMachine.makeScoreBoard(correctCntList, containBonusNumberList);
+
+        lottoMachine.scoreBoardMessage(scoreBoard);
+
+        int totalIncome = lottoMachine.totalIncome(correctCntList, containBonusNumberList);
+
+        lottoMachine.makeYield(totalIncome, wantBuyAmountInput);
+
+
     }
 }
