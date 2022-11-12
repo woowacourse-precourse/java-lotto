c77725e15aee31ecb3d5a6ede44f75e457092d2b
RumbleKAT <reki318@naver.com>
2022-11-12 17:29:40 +0900

    (feat) 1-1. 로또 구입시 중복 로직 추가

--- src/main/java/lotto/Model/Lotto.java
+++ src/main/java/lotto/Model/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.Model;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Lotto {
@@ -11,14 +12,14 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 숫자는 6개여야 합니다.");
-        }
-
-        if(numbers.stream().anyMatch(num -> num < 1 || num > 45)){
+        if (numbers.size() != 6) throw new IllegalArgumentException("[ERROR] 로또 숫자는 6개여야 합니다.");
+        if(numbers.stream().anyMatch(num -> num < 1 || num > 45))
             throw new IllegalArgumentException("[ERROR] 로또 숫자는 1부터 45사이 숫자이어야 합니다.");
-        }
+        HashMap<Integer,Integer> duplicated = new HashMap<>();
+        numbers.forEach(number->{
+            int cnt = duplicated.getOrDefault(number,0);
+            if(cnt >= 1) throw new IllegalArgumentException("[ERROR] 중복된 숫자는 입력할 수 없습니다.");
+            duplicated.put(number, cnt+1);
+        });
     }
-
-    // TODO: 추가 기능 구현
 }
