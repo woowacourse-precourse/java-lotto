0bbce9eb2faff285771e0e5806d6210c64c92539
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-12 17:50:12 +0900

    feat(WinningHistory): 당첨 기준과 금액 상수에 대한 enum 클래스 추가

---
+++ src/main/java/lotto/WinningHistory.java
@@ -0,0 +1,25 @@
+package lotto;
+
+public enum WinningHistory {
+    FIRST_PRIZE(6, 2_000_000_000),
+    SECOND_PRIZE(5, 30000000),
+    THIRD_PRIZE(5, 1_500_000),
+    FOURTH_PRIZE(4, 50_000),
+    FIFTH_PRIZE(3, 5_000);
+
+    private final int matchNumbers;
+    private final int prizeMoney;
+
+    WinningHistory(int matchNumbers, int prizeMoney) {
+        this.matchNumbers = matchNumbers;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public int getMatchNumbers() {
+        return matchNumbers;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+}
