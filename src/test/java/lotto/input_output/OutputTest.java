f2aa4b3466fdbe06adb1b904eae5a90a64ebefa5
leewoojin <zmfpdl64@naver.com>
2022-11-12 23:47:13 +0900

    Test: Output 클래스 테스트 출력 잘 되는지 확인

---
+++ src/test/java/lotto/input_output/OutputTest.java
@@ -0,0 +1,43 @@
+package lotto.input_output;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OutputTest extends NsTest {
+
+    @Test
+    void print_lottery() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("6000");
+                    assertThat(output()).contains(
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[13, 14, 16, 38, 42, 45]",
+                            "[7, 11, 30, 40, 42, 43]"
+                    );
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(7, 11, 30, 40, 42, 43)
+
+        );
+
+    }
+
+    @Override
+    protected void runMain() {
+        Output.main(new String[]{});
+    }
+}
