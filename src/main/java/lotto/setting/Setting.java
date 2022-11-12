68107c34c5c3dc1909f3578ca36d74ce6643c09d
kang jihyeong <her4386@gmail.com>
2022-11-12 23:15:39 +0900

    Feat / 로또 게임 환경 세팅

---
+++ src/main/java/lotto/setting/Setting.java
@@ -0,0 +1,11 @@
+package lotto.setting;
+
+public class Setting {
+
+    public static int LOTTO_MIN_NUMBER = 1;
+    public static int LOTTO_MAX_NUMBER = 45;
+
+    public static int LOTTO_PRICE_PER_ONE = 1000;
+
+
+}
