2a1c28937c7f62a5b2304b2dd33a12a3e588f4d7
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-12 21:50:37 +0900

    test<LottoTest>: Lotto 클래스 테스트 추가
    - 기존 테스트 케이스 통과
    - 로또 번호 개수가 6개 미만이면 예외 발생 테스트 케이스 추가
    - 성공 테스트 케이스 추가

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,16 +1,25 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
+    Set<LottoNumber> makeLotto(int... numbers) {
+        return Arrays.stream(numbers)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toSet());
+    }
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(Set.of(1, 2, 3, 4, 5, 6, 7)))
+        assertThatThrownBy(() -> new Lotto(makeLotto(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -18,9 +27,23 @@ class LottoTest {
     @Test
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new Lotto(Set.of(1, 2, 3, 4, 5, 5)))
+        assertThatThrownBy(() -> new Lotto(makeLotto(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호의 개수가 5개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderSize() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(makeLotto(0, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("서로 다른 숫자 6개면 성공한다.")
+    @Test
+    void createLottoByNumberRange() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertDoesNotThrow(() -> new Lotto(makeLotto(1, 2, 3, 4, 5, 6)));
+    }
 }
