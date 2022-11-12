e9fcac9ec489175ffca56fef5c3ee514b99d459c
dev_kong <ahdjd5@gmail.com>
2022-11-12 16:09:31 +0900

    feat(LottoInfo): add Lotto Info enum

---
+++ src/main/java/lotto/utils/LottoInfo.java
@@ -0,0 +1,17 @@
+package lotto.utils;
+
+public enum LottoInfo {
+    LENGTH(6),
+    START_NUMBER(1),
+    END_NUMBER(45);
+
+    private final int number;
+
+    LottoInfo(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
