e06df758cd46ef2b1ac61ecfdd39ceb61adcd22c
asas6978 <asas6978@naver.com>
2022-11-12 10:29:47 +0900

    refactor: 출력 형식 변경

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -1,8 +1,13 @@
 package lotto;
 
+import java.text.DecimalFormat;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Print {
+    private static final String contour = "---";
+
     public static void purchaseMessage() {
         System.out.println(GuideMessage.ENTER_PURCHASE_AMOUNT);
     }
@@ -13,15 +18,34 @@ public class Print {
         for (Lotto lotto : lottery) {
             lotto.print();
         }
-
-        System.out.println();
     }
 
     public static void enterWinningNumberMessage() {
-        System.out.println(GuideMessage.ENTER_WINNING_NUMBER);
+        System.out.printf(GuideMessage.ENTER_WINNING_NUMBER);
     }
 
     public static void enterBonusNumberMessage() {
-        System.out.println(GuideMessage.ENTER_BONUS_NUMBER);
+        System.out.printf(GuideMessage.ENTER_BONUS_NUMBER);
+    }
+
+    public static void matchResult(Map<WinningNumber, Integer> matchCount) {
+        System.out.printf(GuideMessage.WINNING_STATISTICS);
+        System.out.println(contour);
+
+        for (WinningNumber winningNumber : matchCount.keySet()
+                .stream()
+                .sorted()
+                .collect(Collectors.toList())) {
+            String amountMessage = getAmountMessage(winningNumber);
+            System.out.printf(amountMessage + GuideMessage.MATCH_NUMBER,
+                    winningNumber.getNumberOfWinning(), winningNumber.getMoney(), matchCount.get(winningNumber));
+        }
+    }
+
+    private static String getAmountMessage(WinningNumber winningNumber) {
+        if (winningNumber.getNumberOfBonus() == WinningNumber.FIVE_AND_BONUS_BALL_MATCHES.getNumberOfBonus()) {
+            return GuideMessage.MATCH + ", " + GuideMessage.BONUS_BALL_MATCH + GuideMessage.AMOUNT;
+        }
+        return GuideMessage.MATCH + GuideMessage.AMOUNT;
     }
 }
