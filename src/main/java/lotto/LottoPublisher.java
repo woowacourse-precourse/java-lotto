0ace929a410c436a9848a14490f74b31a8409eaa
wjh2012 <wjh2012@naver.com>
2022-11-12 14:36:47 +0900

    feat(LottoPublisher): 1~45의 서로 다른 임의의 숫자 6개 생성

--- src/main/java/lotto/LottoPublisher.java
+++ src/main/java/lotto/LottoPublisher.java
@@ -1,16 +1,25 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+import static lotto.config.Constants.LottoNumber.*;
 import static lotto.config.Constants.LottoPrice.LOTTO_PRICE;
 
 public class LottoPublisher {
 
     private void validate(int money) {
-        if (money % LOTTO_PRICE != 0){
+        if (money % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException();
         }
     }
 
     private int affordableLotto(int money) {
         return money / LOTTO_PRICE;
+    }
+
+    private List<Integer> publishLottoNumber() {
+        return Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_START_INCLUSIVE, LOTTO_NUMBER_END_INCLUSIVE, LOTTO_SIZE);
     }
 }
