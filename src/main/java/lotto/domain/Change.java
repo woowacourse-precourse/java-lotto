fdf87f8b5242bbfa2022b31f7b42693d72dcea4a
leeyongjun <leeyongjun0310@naver.com>
2022-11-13 03:57:46 +0900

    feat(lotto): 숫자와 ','을 제외한 문자가 입력이 되었는지 확인하는 기능 구현

--- src/main/java/lotto/domain/Change.java
+++ src/main/java/lotto/domain/Change.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Change {
+    private static final String REGEX = "[0-9]+";
 
     public static List<Integer> lottoNumber(String winningNumber) {
+        checkAnyWord(winningNumber);
         checkManyDot(winningNumber);
         List<String> newWinningNumber = Arrays.asList(winningNumber.split(","));
         List<Integer> result = new ArrayList<>();
@@ -30,6 +32,13 @@ public class Change {
                 throw new IllegalArgumentException();
             }
 
+        }
+    }
+
+    private static void checkAnyWord(String winningNumber) {
+        if (!(winningNumber.matches(REGEX) || winningNumber.contains(","))) {
+            System.out.println("[ERROR] 당첨번호를 다시 입력해주세요. - 다른 문자 입력");
+            throw new IllegalArgumentException();
         }
     }
 }
