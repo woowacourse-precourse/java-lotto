cf5043c632d7536967dbd1799c002baafa1ec7d1
bunsung92 <bunsung92@naver.com>
2022-11-12 16:16:06 +0900

    feat(lotto):  로또 번호 검증을 위한 메서드 추가
    valid() 메서드의 추가 기능:
    - validatedRangeNumbers() 만들어진 로또의 숫자 범위를 검증하는 메서드 추가
    - validateOverlapNumbers() 만들어진 로또의 숫자에 중복이 있는지 검증하는 메서드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,15 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
- * 제공된 Lotto 클래스를 활용해 구현해야 한다.
- * Lotto에 매개 변수가 없는 생성자를 추가할 수 없다.
- * numbers의 접근 제어자인 private을 변경할 수 없다.
- * Lotto에 필드(인스턴스 변수)를 추가할 수 없다.
- * Lotto의 패키지 변경은 가능하다.
+ * 제공된 Lotto 클래스를 활용해 구현해야 한다. O
+ * Lotto에 매개 변수가 없는 생성자를 추가할 수 없다. O
+ * numbers의 접근 제어자인 private을 변경할 수 없다. O
+ * Lotto에 필드(인스턴스 변수)를 추가할 수 없다. O
+ * Lotto의 패키지 변경은 가능하다. O
  */
 public class Lotto {
     private final List<Integer> numbers;
@@ -18,10 +20,32 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateRangeNumbers(numbers);
+        validateOverlapNumbers(numbers);
+
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리 수가 입력 되어야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRangeNumbers(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
+    private void validateOverlapNumbers(List<Integer> numbers) {
+        Set<Integer> pickedUniqueNumbersInRange = new HashSet<>(numbers);
+
+        if (pickedUniqueNumbersInRange.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복된 수는 로또 번호로 사용할 수 없습니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
 }
