23fd39f8e5a52f887cd66b45369ff186abb729af
minaver <psmjoshua@gmail.com>
2022-11-12 15:32:35 +0900

    feat(Controller): getPrice method 추가

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,4 +1,19 @@
 package lotto.controller;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Controller {
+
+    public int getPrice() {
+        String price = readLine();
+        return Integer.parseInt(price);
+    }
+
+    public void getWinningLotto() {
+
+    }
+
+    public void getBonusNum() {
+
+    }
 }
