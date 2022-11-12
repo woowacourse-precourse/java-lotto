40dfc522dd7ea680ade1005766ab2b8577b67e7e
devyourown <hyojoon56@gmail.com>
2022-11-12 22:10:55 +0900

    test : modify testcase and pass the case

--- src/main/java/lotto/domain/LottoMaker.java
+++ src/main/java/lotto/domain/LottoMaker.java
@@ -1,4 +1,17 @@
 package lotto.domain;
 
+import java.util.Collections;
+import java.util.List;
+
 public class LottoMaker {
+    private static final int ONE_LOTTO_PRICE = 1000;
+    public static List<Lotto> makeLotto(int price) {
+        validate(price);
+        return Collections.emptyList();
+    }
+
+    private static void validate(int price) {
+        if (price % ONE_LOTTO_PRICE != 0)
+            throw new IllegalArgumentException("로또는 1000원 단위로 구매할 수 있습니다.");
+    }
 }
--- src/test/java/lotto/domain/LottoMakerTest.java
+++ src/test/java/lotto/domain/LottoMakerTest.java
@@ -7,7 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class LottoMakerTest {
     @Test
     void testMakeExceptionGettingWrongPrice() {
-        LottoMaker.makeLotto(1101);
+        assertThrows(IllegalArgumentException.class, () -> {
+            LottoMaker.makeLotto(1101);
+        });
     }
 
 }
