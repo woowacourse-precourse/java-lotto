fc40b605a34cfd569905907222176fd670496704
yubin <dbqls2821@gmail.com>
2022-11-12 11:27:23 +0900

    refactor(ExceptionMessage): 예외사항 출력 메시지 수정

--- src/main/java/lotto/domain/constant/ExceptionMessage.java
+++ src/main/java/lotto/domain/constant/ExceptionMessage.java
@@ -4,7 +4,7 @@ public enum ExceptionMessage {
   INPUT_IS_NOT_SEPARATED_BY_COMMAS("[ERROR] 입력값은 단일 콤마로 구분되지 않았습니다."),
   INPUT_IS_NOT_SIX_NUMBERS("[ERROR] 입력값의 숫자는 6개가 아닙니다."),
   INPUT_IS_NOT_NUMBER("[ERROR] 입력값은 숫자가 아닙니다."),
-  INPUT_NUMBER_RANGE_IS_NOT_CORRECT("[ERROR] 입력값은 1이상 45이하의 숫자가 아닙니다."),
+  INPUT_NUMBER_RANGE_IS_NOT_CORRECT("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
   INPUT_IS_DUPLICATED("[ERROR] 입력값에 중복된 숫자가 있습니다.");
 
   private String message;
