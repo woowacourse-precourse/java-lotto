44d60a6559b7d3cf185c9a38a4b6d965ef232139
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-12 15:21:59 +0900

    feat: 정답 로또와 보너스 번호를 기반으로 구입한 로또들의 결과를 반환하는 기능 완성

--- src/main/java/lotto/domain/LottoReader.java
+++ src/main/java/lotto/domain/LottoReader.java
@@ -1,8 +1,20 @@
 package lotto.domain;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoReader {
+
+    public Map<LottoResult, Integer> getLottoResults(List<Lotto> lottos, Lotto compareLotto, int bonusNumber) {
+        Map<LottoResult, Integer> result = new HashMap<>();
+        for (Lotto lotto : lottos) {
+            LottoResult lottoResult = getLottoResult(lotto, compareLotto, bonusNumber);
+            result.computeIfPresent(lottoResult,(key, value) ->value + 1);
+            result.computeIfAbsent(lottoResult, key -> 1);
+        }
+        return result;
+    }
 
     public LottoResult getLottoResult(Lotto lotto, Lotto compareLotto, int bonusNumber) {
         double matchingNumber = countMatchingNumber(lotto, compareLotto);
