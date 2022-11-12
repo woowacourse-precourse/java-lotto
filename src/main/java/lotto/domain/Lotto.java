296034dc711eb825e8fcd6faf699c70cafdb2791
hoonn9 <hun9935@gmail.com>
2022-11-12 22:57:07 +0900

    feat(Lotto): 오름차순 정렬, 중복 검증, 출력 문자열 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,43 @@
 package lotto.domain;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
+    
     private final List<Integer> numbers;
-
+    
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
+        
         this.numbers = numbers;
     }
-
+    
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+    
+    public String getStringByNumbers() {
+        return "[" + numbers.stream().map(String::valueOf).collect(Collectors.joining(", ")) + "]";
+    }
+    
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+    }
+    
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
-
-    // TODO: 추가 기능 구현
+    
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.size() != new HashSet<>(numbers).size()) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
