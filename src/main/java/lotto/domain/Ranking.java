8ba7dd33f92336ed86d08b9532386f52f2e6bd38
YuHayeon <youha0930@gmail.com>
2022-11-13 02:10:17 +0900

    feat(Ranking): 출력 형식 지정 (toString 재정의)
    보너스가 있을 때와 없을 때로 구분하여 출력형식 지정

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 public enum Ranking {
@@ -11,6 +12,9 @@ public enum Ranking {
     FIRST(6, 2_000_000_000, false),
     NOTHING(0, 0, false);
 
+    private static final String HAS_BONUS_NUMBER_MESSAGE = "%d개 일치, 보너스 볼 일치 (%s원)";
+    private static final String NO_BONUS_NUMBER_MESSAGE = "%d개 일치 (%s원)";
+    
     private final int rightCount;
     private final int prize;
     private final boolean hasBonusNumber;
@@ -26,5 +30,15 @@ public enum Ranking {
                 .filter(ranking -> ranking.rightCount == rightCount && ranking.hasBonusNumber == hasBonusNumber)
                 .findAny()
                 .orElse(NOTHING);
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat decimalFormat = new DecimalFormat("###,###");
+        if (hasBonusNumber) {
+            return String.format(HAS_BONUS_NUMBER_MESSAGE, rightCount, decimalFormat.format(prize));
+
+        }
+        return String.format(NO_BONUS_NUMBER_MESSAGE, rightCount, decimalFormat.format(prize));
     }
 }
