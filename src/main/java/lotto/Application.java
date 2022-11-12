b3172ceddaa9951c04a6328911f9d6f060761733
LeeJaeHong <0915jhlee@daum.net>
2022-11-12 18:03:09 +0900

    2. 로또 발행 2.1 로또 번호 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,20 +1,45 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.*;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("구입금액을 입력해 주세요.");
         int lottoNum=userInputMoney();
-        makeLotto(lottoNum);
+
+        List<Lotto> lottos=makeLotto(lottoNum);
     }
 
-    private static void makeLotto(int lottoNum) {
-
+    private static List<Lotto> makeLotto(int lottoNum) {
+        ArrayList<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoNum; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers);
+            Lotto lotto = new Lotto(numbers);
+            printLotto(numbers);
+            lottos.add(lotto);
+        }
+        return lottos;
     }
-
+    private static void printLotto(List<Integer> lottonumbers){
+        System.out.print("[");
+        for (int i = 0; i < lottonumbers.size(); i++) {
+            if(i==lottonumbers.size()-1){
+                System.out.print(lottonumbers.get(i)+"]");
+            }
+            System.out.print(lottonumbers.get(i)+", ");
+        }
+    }
     //사용자 금액 입력
     private static int userInputMoney() {
         UserMoney userMoney = new UserMoney(Integer.parseInt(readLine()));
+        System.out.println(userMoney.getLottoNumber()+"개를 구매했습니다.");
         return userMoney.getLottoNumber();
     }
 
