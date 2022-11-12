09f103eddfd2f47613741f7ff1b5324df75c2741
yoonjuho <dbswn990@naver.com>
2022-11-13 07:44:07 +0900

    test(LottoTest) : 로또 구입 금액 예외 발생 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,14 +1,18 @@
 package lotto;
 
+import lotto.domain.Issue;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -35,6 +39,13 @@ class LottoTest {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         int result = 3;
         assertThat(result).isEqualTo(lotto.compare(List.of(1, 3, 4, 8, 10, 32)));
+    }
+    @DisplayName("로또 구입 금액이 1000단위가 아니라면 예외 처리 한다")
+    @ParameterizedTest
+    @CsvSource({"900", "1100", "3", "2001"})
+    void convertMoneyByWrongUnit(String input) {
+        assertThatThrownBy(() -> new Issue().getAmount(Integer.parseInt(input)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 
