7f741b694be548814e5dd503a7ff5440cc1c5d1c
ostar11 <cjw5877@gmail.com>
2022-11-12 16:13:10 +0900

    feat(LottoManager): inputLottoCount 구현

--- src/main/java/lotto/view/LottoManager.java
+++ src/main/java/lotto/view/LottoManager.java
@@ -1,8 +1,13 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.Lotto;
 
 public class LottoManager {
+    public int inputLottoCount() {
+        return Integer.parseInt(Console.readLine());
+    }
+
     public void printLottoNumbers(Lotto[] lottos) {
         for (int i = 0; i < lottos.length; i++) {
             System.out.println(lottos[i].getNumbers());
