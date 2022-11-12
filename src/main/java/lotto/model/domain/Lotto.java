b1fade56ad74381e05955a2884f20418d871ee62
kjb95 <topmedia44@gmail.com>
2022-11-12 21:44:35 +0900

    refactor: Lotto 클래스
    - 변수명, 메소드명 변경
    - 멤버변수 numbers 유효성검사 추가

--- src/main/java/lotto/model/domain/Lotto.java
+++ src/main/java/lotto/model/domain/Lotto.java
@@ -5,22 +5,50 @@ import java.util.stream.Collectors;
 
 public class Lotto {
 
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
     private static final String LEFT_BRACKETS = "[";
     private static final String RIGHT_BRACKETS = "]";
     private static final String NUMBER_SEPARATOR = ", ";
     private static final String NEW_LINE = "\n";
+    private static final String WRONG_NUMBER_SIZE = "로또 번호 숫자는 6개 입니다.";
+    private static final String DUPLICATED_NUMBER = "로또 번호는 중복되지 않아야 합니다.";
+    private static final String INVALID_NUMBER_RANGE = "로또 번호는 1이상 45 이어야 합니다.";
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateNumbers(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException(WRONG_NUMBER_SIZE);
         }
+        if (isInvalidAllLottoNumberRange(numbers)) {
+            throw new IllegalArgumentException(INVALID_NUMBER_RANGE);
+        }
+        if (hasDuplicatedLottoNumber(numbers)) {
+            throw new IllegalArgumentException(DUPLICATED_NUMBER);
+        }
+    }
+
+    private boolean isInvalidAllLottoNumberRange(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(this::isValidLottoNumberRange)
+                .count() != LOTTO_NUMBER_SIZE;
+    }
+
+    private boolean isValidLottoNumberRange(int num) {
+        return num >= LOTTO_NUMBER_MIN && num <= LOTTO_NUMBER_MAX;
+    }
+
+    private boolean hasDuplicatedLottoNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .count() != LOTTO_NUMBER_SIZE;
     }
 
     @Override
