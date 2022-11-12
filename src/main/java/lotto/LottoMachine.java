c45e0b1e368425981806d474e92acd693f5d54e0
ghltjd369 <ghltjd369@gmail.com>
2022-11-12 17:03:39 +0900

    refactor : 배열에서 리스트 사용으로 변경

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -2,21 +2,27 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoMachine {
     public void inputWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String inputNumber = Console.readLine();
-        int[] winningNumber = splitNumber(inputNumber);
+        List<Integer> winningNumber = splitNumber(inputNumber);
+
     }
 
-    public int[] splitNumber(String number) {
-        String[] winningNumber = number.split(",");
+    public List<Integer> splitNumber(String number) {
+        List<String> winningNumber = Arrays.asList(number.split(","));
 
-        //String형 배열을 int형 배열로 변환하여 return
-        return Arrays.stream(winningNumber).mapToInt(Integer::parseInt).toArray();
+        //String형 리스트를 int형 리스트로 변경
+        return winningNumber.stream()
+                .map(s -> Integer.parseInt(s))
+                .collect(Collectors.toList());
     }
 
     public void inputBonusNumber() {
