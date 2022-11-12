d29d8601d1d349a42aeff063aafba29d02d803dc
marinesnow34 <m4rinesnow34@gmail.com>
2022-11-12 17:16:42 +0900

    feat: 구매자 돈 입력 예외 처리

--- src/main/java/lotto/Buyer.java
+++ src/main/java/lotto/Buyer.java
@@ -2,10 +2,22 @@ package lotto;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.regex.Pattern;
+
 public class Buyer {
+    private final String ERROR_NOT_INT = "[ERROR] 숫자만 입력 가능합니다.";
+    private final String REGEX_INT = "^[0-9]+$";
     public int money;
 
     public void getMoney() {
-        this.money = Integer.parseInt(readLine());
+        String input = readLine();
+        isValidMoneyInput(input);
+        this.money = Integer.parseInt(input);
+    }
+
+    private void isValidMoneyInput(String input) {
+        if (!Pattern.matches(REGEX_INT, input)) {
+            throw new IllegalArgumentException(ERROR_NOT_INT);
+        }
     }
 }
