88e98d04f83f391760a620f693fb2c9435901f7f
Amyhds <dasomhan524@gmail.com>
2022-11-13 00:54:46 +0900

    fix: 버그 수정 new arraylist로 리스트 선언

--- src/main/java/lotto/DrawNumber.java
+++ src/main/java/lotto/DrawNumber.java
@@ -2,18 +2,18 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
 
 public class DrawNumber {
-    public static List<List<Integer>> lottos;
+    public static List<List<Integer>> lottos = new ArrayList<>();
 
     public static void generateNumber(int ticketAmount) {
-        System.out.println(Integer.toString(ticketAmount) + GameMessage.PURCHASE_MESSAGE.getMessage());
+        System.out.println(ticketAmount + GameMessage.PURCHASE_MESSAGE.getMessage());
         for (int count = 0; count < ticketAmount; count++) {
-            List<Integer> oneTicketNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> oneTicketNumber = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             Collections.sort(oneTicketNumber);
-            Lotto oneLotto = new Lotto(oneTicketNumber);
             lottos.add(oneTicketNumber);
         }
     }
--- src/main/java/lotto/LuckyNumber.java
+++ src/main/java/lotto/LuckyNumber.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LuckyNumber {
-    public static List<Integer> luckySix;
+    public static List<Integer> luckySix = new ArrayList<>();
     public static int luckyBonus;
 
     public static void pickSixNumber() {
