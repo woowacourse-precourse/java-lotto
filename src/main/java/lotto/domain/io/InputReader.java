c55f97e9a52c6a1544611c176cb29ceb587364d9
yubin <dbqls2821@gmail.com>
2022-11-12 16:39:19 +0900

    refactor(InputReader): 세부 validation은 각 클래스에 맡기고, input만 담당하도록 수정

--- src/main/java/lotto/domain/io/InputReader.java
+++ src/main/java/lotto/domain/io/InputReader.java
@@ -1,41 +1,28 @@
 package lotto.domain.io;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.BonusNumber;
 import lotto.domain.Lotto;
+import lotto.domain.PurchaseAmount;
 
 import java.util.List;
 
 public class InputReader {
 
-  private CommaAndNumberValidator commaAndNumberValidator;
-  private RangeValidator rangeValidator;
-  private NumberValidator numberValidator;
-  private Multiple1000Validator multiple1000Validator;
-
-  public InputReader() {
-    this.commaAndNumberValidator = new CommaAndNumberValidator();
-    this.rangeValidator = new RangeValidator();
-    this.numberValidator = new NumberValidator();
-    this.multiple1000Validator = new Multiple1000Validator();
-  }
-
-  public int readLottoPurchaseAmount() {
+  public PurchaseAmount readLottoPurchaseAmount() {
     String input = Console.readLine();
-    int inputToInt = numberValidator.validate(input);
-    multiple1000Validator.validate(inputToInt);
-    return inputToInt;
+    return new PurchaseAmount(input);
   }
 
   public Lotto readWinningNumber() {
     String input = Console.readLine();
+    CommaAndNumberValidator commaAndNumberValidator = new CommaAndNumberValidator();
     List<Integer> inputs = commaAndNumberValidator.validate(input);
     return new Lotto(inputs);
   }
 
-  public int readBonusNumber() {
+  public BonusNumber readBonusNumber() {
     String input = Console.readLine();
-    int inputToInt = numberValidator.validate(input);
-    rangeValidator.validate(inputToInt);
-    return inputToInt;
+    return new BonusNumber(input);
   }
 }
