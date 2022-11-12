a57e8d74f538874401757a13e5ae6832a36b1242
TaeMin <xoals3094@naver.com>
2022-11-13 04:37:08 +0900

    style : 멤버 변수 위치 최상단으로 변경

--- src/main/java/lotto/Controller/MainController.java
+++ src/main/java/lotto/Controller/MainController.java
@@ -14,6 +14,14 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MainController {
+    private Cash cash;
+    private int lottoNumber;
+    private List<Lotto> myLottos;
+    private Lotto winningLotto;
+    private Bonus bonus;
+    private HashMap<Rank, Integer> point;
+    private double profit;
+
     public MainController() {
         point = new HashMap<Rank, Integer>();
         point.put(Rank.FIRST, 0);
@@ -23,8 +31,6 @@ public class MainController {
         point.put(Rank.FIFTH, 0);
         point.put(Rank.LAST, 0);
     }
-
-    private Cash cash;
 
     public void inputCash() throws IllegalArgumentException {
         System.out.println("구입 금액을 입력해주세요.");
@@ -39,26 +45,20 @@ public class MainController {
         lottoNumber = countLottoNumber(cash);
     }
 
-    private int lottoNumber;
-
     private int countLottoNumber(Cash cash) throws IllegalArgumentException {
         CalculatorModel calculator = new CalculatorModel();
         return calculator.countLottoNumber(cash);
     }
-
-    private List<Lotto> myLottos;
 
     public void createMyLottos() {
         LottoGeneratorModel lottoGenerator = new LottoGeneratorModel();
         myLottos = lottoGenerator.createMyLottos(lottoNumber);
     }
 
-    public void printMyLottos(){
+    public void printMyLottos() {
         OutputView outputView = new OutputView();
         outputView.printMyLottoInfo(myLottos);
     }
-
-    private Lotto winningLotto;
 
     public void inputWinningNumber() throws IllegalArgumentException {
         System.out.println("당첨 번호를 입력해 주세요.");
@@ -77,8 +77,6 @@ public class MainController {
         winningLotto = new Lotto(winningNumber);
     }
 
-    private Bonus bonus;
-
     public void inputBonusNumber() throws IllegalArgumentException {
         System.out.println("보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
@@ -93,8 +91,6 @@ public class MainController {
         bonus = new Bonus(bonusNumber, winningLotto);
     }
 
-    HashMap<Rank, Integer> point;
-    double profit;
     public void calculateWinning() {
         CalculatorModel calculator = new CalculatorModel();
         int total = 0;
@@ -106,11 +102,11 @@ public class MainController {
             int p = point.get(rank);
             point.replace(rank, p + 1);
         }
-        
+
         profit = calculator.getProfit(total, cash.getCash());
     }
 
-    public void printResult(){
+    public void printResult() {
         OutputView outputView = new OutputView();
         outputView.printResult(point, profit);
     }
