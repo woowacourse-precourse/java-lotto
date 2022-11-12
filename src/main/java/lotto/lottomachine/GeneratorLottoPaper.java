22023ba4772e705548a41fe4cc102753424e254a
kimminsu <xjfcnfw3@naver.com>
2022-11-12 21:10:35 +0900

    feat: 입력 금액에 맞는 로또를 생성하여 종이에 넣는 기능 구현

---
+++ src/main/java/lotto/lottomachine/GeneratorLottoPaper.java
@@ -0,0 +1,42 @@
+package lotto.lottomachine;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.GameMessage;
+import lotto.domain.Lotto;
+import lotto.domain.LottoEnum;
+import lotto.lottopaper.LottoPaper;
+
+public class GeneratorLottoPaper {
+    public static LottoPaper generateLottoPaper(int lottoPrice) {
+        int lottoCount = lottoPrice / LottoEnum.PRICE.getValue();
+        showLottoCount(lottoCount);
+
+        LottoPaper lottoPaper = new LottoPaper();
+
+        while (lottoCount != 0) {
+            lottoPaper.addLotto(generateLotto());
+            lottoCount--;
+        }
+
+        return lottoPaper;
+    }
+
+    private static void showLottoCount(int lottoCount) {
+        System.out.println(String.valueOf(lottoCount) + GameMessage.RESULT_RANK_UNITS + GameMessage.BUY_RESULT);
+    }
+
+    private static Lotto generateLotto() {
+        List<Integer> lottoNumbers = generateLottoNumbers();
+        Lotto lotto = new Lotto(lottoNumbers);
+        System.out.println(lottoNumbers);
+        return lotto;
+    }
+
+    private static List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(
+            LottoEnum.MIN_NUMBER.getValue(),
+            LottoEnum.MAX_NUMBER.getValue(),
+            LottoEnum.LENGTH.getValue());
+    }
+}
