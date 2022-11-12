d61756570401886bb7e9ae3ef812354e579f8780
DongwookKim0823 <dwkim0823@naver.com>
2022-11-12 14:49:26 +0900

    feat(lotto): 생성된 로또 번호 검증 기능

--- src/main/java/lotto/domian/lotto/Lotto.java
+++ src/main/java/lotto/domian/lotto/Lotto.java
@@ -6,13 +6,17 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateLottoNumbers(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 생성된 번호가 6개가 아닙니다.");
+        }
+
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 생성된 번호들 사이에 중복이 존재합니다.");
         }
     }
 
