2a40bb1338022ed461b100e62c1d098e3d41c23f
JIWEON-JEONG <dlektl6@naver.com>
2022-11-13 01:15:41 +0900

    feat : 당첨 로또에 대한 view

---
+++ src/main/java/view/LottoAnswerView.java
@@ -0,0 +1,11 @@
+package view;
+
+public class LottoAnswerView {
+    public static void askAnswer() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void askBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}
