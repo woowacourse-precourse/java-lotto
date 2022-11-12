2715416967378d7bca9fb0c618516f7f332129b8
skydreamer21 <skydreamer210@gmail.com>
2022-11-13 01:27:32 +0900

    feat(InputView): 구매 문구 출력 기능 구현
    [세부사항]
    - 구현 메서드 : printPurchaseMoneyRequest

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+import lotto.view.constant.InputGuidePhrase;
+
+public class InputView {
+    public static void printPurchaseMoneyRequest() {
+        System.out.println(InputGuidePhrase.PURCHASE_MONEY_REQUEST);
+    }
+}
---
+++ src/main/java/lotto/view/constant/InputGuidePhrase.java
@@ -0,0 +1,11 @@
+package lotto.view.constant;
+
+public enum InputGuidePhrase {
+    PURCHASE_MONEY_REQUEST("구입금액을 입력해 주세요.");
+
+    private final String text;
+
+    private InputGuidePhrase(String text) {
+        this.text = text;
+    }
+}
