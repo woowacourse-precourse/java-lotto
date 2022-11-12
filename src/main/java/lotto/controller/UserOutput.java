2a1cebe3697535889e9087aaf846eaba9e970553
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-13 00:28:15 +0900

    type:feat 사용자에게 구입한 로또 갯수와 번호를 보여주는 기능 추가

--- src/main/java/lotto/controller/UserOutput.java
+++ src/main/java/lotto/controller/UserOutput.java
@@ -1,2 +1,20 @@
-package lotto.controller;public class UserOutput {
+package lotto.controller;
+
+import lotto.model.Lotto;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class UserOutput {
+    public static void viewLottos(ManageLotto manageLotto) {
+        System.out.println(String.format("%d개를 구매했습니다.", manageLotto.lottoNum));
+        manageLotto.makeMyLotto(manageLotto.lottoNum);
+        for (Lotto newLotto: manageLotto.myLotto) {
+            List<Integer> tmpNumbers = new ArrayList<>(newLotto.getLottoNumbers());
+            tmpNumbers.sort(Comparator.naturalOrder());
+            System.out.println(tmpNumbers);
+        }
+        System.out.println();
+    }
 }
