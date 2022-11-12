79e8ecb9ff44d53fa7494f757ac707cfafd83235
Ilchul Kim <huj0159@gmail.com>
2022-11-12 19:09:13 +0900

    feat: check input number is digit

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -7,7 +7,15 @@ import java.util.regex.Pattern;
 public class LottoController {
 
     public void readUserMoney(String userMoney) {
+        isDigit(userMoney);
         validate(userMoney);
+    }
+
+    private void isDigit(String userInput){
+        String pattern = "^[0-9]*$";
+        if (!Pattern.matches(pattern, userInput)) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주셔야 합니다");
+        }
     }
 
     private void validate(String userInput) {
