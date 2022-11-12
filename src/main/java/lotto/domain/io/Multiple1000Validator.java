756330ed9d3a06a1460d8126d162b93d12085136
yubin <dbqls2821@gmail.com>
2022-11-12 15:40:42 +0900

    chore(Multiple1000Validator): 매개변수의 타입 변경

--- src/main/java/lotto/domain/io/Multiple1000Validator.java
+++ src/main/java/lotto/domain/io/Multiple1000Validator.java
@@ -3,9 +3,8 @@ package lotto.domain.io;
 import lotto.domain.constant.ExceptionMessage;
 
 public class Multiple1000Validator {
-  void validate(String input) {
-    int inputToInt = Integer.parseInt(input);
-    if (inputToInt % 1000 != 0) {
+  void validate(int input) {
+    if (input % 1000 != 0) {
       throw new IllegalArgumentException(ExceptionMessage.INPUT_IS_NOT_MULTIPLE_OF_1000.getMessage());
     }
   }
