38174ec5fdee04118f02e0bb2d2400132cd6450e
wugawuga <dnr3655@naver.com>
2022-11-12 14:18:32 +0900

    feat: 비교 결과로 찾은 로또 등수가 몇 개인지 확인

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoGame {
     private final LottoGenerator generator = new LottoGenerator();
@@ -18,5 +20,15 @@ public class LottoGame {
 
         BonusLotto bonusLotto = new BonusLotto(winningLotto, bonus);
         List<Integer> matchCount = lottos.result(bonusLotto);
+        Map<LottoOperator, Integer> winningResult = findWinningResult(matchCount);
+    }
+
+    private Map<LottoOperator, Integer> findWinningResult(List<Integer> matchCount) {
+        Map<LottoOperator, Integer> winningResult = new HashMap<>();
+        for (Integer count : matchCount) {
+            LottoOperator rank = LottoOperator.find(count);
+            winningResult.put(rank, winningResult.getOrDefault(rank, 0) + 1);
+        }
+        return winningResult;
     }
 }
