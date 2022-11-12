72ea2582d0b73b4fdbfb8d8ee59823e71691c4e7
jyj1143 <jyj1143@gmail.com>
2022-11-12 11:39:06 +0900

    feat(Lotto): 로또 번호 생성 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,16 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 45;
+    public static final int SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,5 +24,14 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private Lotto createRandomNumbers() {
+        Set<Integer> randomNumberSet = new HashSet<>();
+        while (randomNumberSet.size() != SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE);
+            randomNumberSet.add(randomNumber);
+        }
+        List<Integer> randomNumberList = new ArrayList<>(randomNumberSet);
+        return new Lotto(randomNumberList);
+    }
+
 }
