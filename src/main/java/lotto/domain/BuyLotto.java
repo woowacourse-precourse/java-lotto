ccb91813e8704984a55e7b9b45873be48f514457
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-12 13:50:41 +0900

    refactor(lotto) : 로또를 여러장 뽑는 메서드 추가

--- src/main/java/lotto/domain/BuyLotto.java
+++ src/main/java/lotto/domain/BuyLotto.java
@@ -1,17 +1,37 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BuyLotto {
     public static final int LOTTO_PRICE = 1000;
     int price;
+    List<Lotto> lottos;
 
     public BuyLotto(int price) {
         this.price = price;
-
+        int count = numberOfLotto();
+        lottos = drawLottos(count);
     }
 
     private int numberOfLotto() {
         return price / LOTTO_PRICE;
     }
 
+    private List<Integer> drawLotto() {
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return lottoNumbers;
+    }
 
+    private List<Lotto> drawLottos(int count) {
+        List<Lotto> list = new ArrayList<>();
+        for(int i = 0; i < count; i++) {
+            Lotto lotto = new Lotto(drawLotto());
+            list.add(lotto);
+        }
+        return list;
+    }
 }
