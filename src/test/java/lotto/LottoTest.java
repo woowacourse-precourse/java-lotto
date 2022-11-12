9ca306ab2344c2d93226651c428aca3ae3060888
eyeofsol <kingjihong@naver.com>
2022-11-12 14:19:06 +0900

    Test(Lotto): 당첨을 판별해 등수를 알려주는 기능 테스트 작성
    맞춘 갯수에 따라 등수를 알려주능 기능의 테스트를 작성했습니다. 가장 주목해야 할 부분은 5개를 맞춘 후, 보너스의 당첨 여부입니다. 그것을 중점으로 테스트 코드를 작성했고, 성공적으로 통과했습니다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,4 +26,30 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("5개를 맞추고 보너스는 못맞췄으면 3를 반환한다.")
+    @Test
+    void getRankByFiveSameNumberWithBonus() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int rank = lotto.draw(List.of(1, 2, 3, 4, 5, 9), 7);
+
+        assertThat(rank).isEqualTo(3);
+    }
+
+    @DisplayName("5개를 맞추고 보너스를 맞췄으면 2를 반환한다")
+    @Test
+    void getRankByFiveSameNumberWithoutBouus() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        int rank = lotto.draw(List.of(1, 2, 3, 4, 5, 9), 7);
+
+        assertThat(rank).isEqualTo(2);
+    }
+
+    @DisplayName("2개를 맞추고 보너스를 맞췄으면 0을 반환한다")
+    @Test
+    void getNonRankByTwoSameNumberWithBonus() {
+        Lotto lotto = new Lotto(List.of(1, 2, 13, 14, 15, 7));
+        int rank = lotto.draw(List.of(1, 2, 3, 4, 5, 9), 7);
+
+        assertThat(rank).isEqualTo(0);
+    }
 }
