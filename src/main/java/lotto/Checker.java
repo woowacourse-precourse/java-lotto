b72d40bc8037d375017e793982edd38ebf29795f
swonny <wonny921@gmail.com>
2022-11-12 17:56:42 +0900

    feat(Checker): 2등 판별을 위한 hasBonusNUmber() 메소드 구현
    1등과 2등의 일치하는 번호의 개수는 중복된다.
    따라서 일치하는 번호 중 보너스 번호가 포함되어 있다면 2등으로 처리해주어야 한다.
    해당 기능을 구현하기 위해 hasBonusNumber()메소드를 구현하였다.

--- src/main/java/lotto/Checker.java
+++ src/main/java/lotto/Checker.java
@@ -8,19 +8,28 @@ public class Checker extends LottoKiosk {
     static final int THIRD_PRIZE_COUNT = 5;
     static final int FOURTH_PRIZE_COUNT = 4;
     static final int FIFTH_PRIZE_COUNT = 3;
-    static HashMap<Integer, Enum> countMatchingPrizes = new HashMap<>() {{
-        countMatchingPrizes.put(FIRST_PRIZE_COUNT, PrizeRank.first);
-        countMatchingPrizes.put(THIRD_PRIZE_COUNT, PrizeRank.third);
-        countMatchingPrizes.put(FOURTH_PRIZE_COUNT, PrizeRank.fourth);
-        countMatchingPrizes.put(FIFTH_PRIZE_COUNT, PrizeRank.fifth);
-    }};
 
-    public static void compare(List<Lotto> buyerLottos, int buyerBonusNumber) {
-        for (Lotto lotto : buyerLottos) {
-            int countSameNumbers = countSameNumbers(lotto);
-            resultStatistics.add(getRank().ordinal(), resultStatistics.get(getRank().ordinal()) + 1);
+    static HashMap<Integer, PrizeRank> countMatchingPrizes = new HashMap<>() {
+        {
+            put(FIRST_PRIZE_COUNT, PrizeRank.FIRST);
+            put(THIRD_PRIZE_COUNT, PrizeRank.THIRD);
+            put(FOURTH_PRIZE_COUNT, PrizeRank.FOURTH);
+            put(FIFTH_PRIZE_COUNT, PrizeRank.FIFTH);
         }
+    };
 
+    public static void compare(List<Lotto> buyerLottos) {
+        for (Lotto lotto : buyerLottos) {
+            Enum lottoRank = getRank(countSameNumbers(lotto));
+            if (lottoRank.equals(PrizeRank.THIRD) && hasBonusNumber(lotto)) {
+                lottoRank = PrizeRank.SECOND;
+            }
+            resultStatistics[lottoRank.ordinal()]++;
+        }
+    }
+
+    public static boolean hasBonusNumber(Lotto lotto) {
+        return lotto.contains(winningBonusNumber);
     }
 
     public static int countSameNumbers(Lotto lotto) {
@@ -36,31 +45,26 @@ public class Checker extends LottoKiosk {
     public static Enum getRank(int countSameNumbers) {
         return countMatchingPrizes.get(countSameNumbers);
     }
-
-    public static int drawFirstPrize(List<Integer> buyerNumbers) {
-        if (buyerNumbers.equals(LottoKiosk.winningNumbers)) {
-            return 1;
-        }
-        return 0;
-    }
-
-    public static int drawSecondPrize(List<Integer> buyerNumbers, List<Integer> winningNumbers,) {
-        return 0;
-    }
-
-    public static int drawThirdPrize(List<Integer> buyerNumbers, List<Integer> winningNumbers) {
-        return 0;
-    }
-
-    public static int drawFourthPrize(List<Integer> buyerNumbers, List<Integer> winningNumbers) {
-        return 0;
-    }
-
-    public static int drawFifthPrize(List<Integer> buyerNumbers, List<Integer> winningNumbers) {
-        return 0;
-    }
 }
 
 enum PrizeRank {
-    first, second, third, fourth, fifth
+    FIRST(0),
+    SECOND(1),
+    THIRD(2),
+    FOURTH(3),
+    FIFTH(4);
+
+    private final int ranks;
+
+    PrizeRank(int ranks) {
+        this.ranks = ranks;
+    }
+
+    public static PrizeRank getPrizeIndex(String prizeRank) {
+        return PrizeRank.valueOf(prizeRank);
+    }
+
+    public static int getPrizeIndex(PrizeRank prizeRank) {
+        return prizeRank.ordinal();
+    }
 }
