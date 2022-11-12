86ebcb5b8705291e0a4f156b4fa0d50dea9ffac0
임원정 <oddish0513@gmail.com>
2022-11-12 18:55:53 +0900

    test(lotto): 중복되지 않는 숫자 검사

---
+++ src/test/java/lotto/domain/NumberGeneratorTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberGeneratorTest {
+
+	@Test
+	void 중복_수_확인(){
+		Set<Integer> numberSet = new HashSet<>();
+		NumberGenerator numberGenerator = new NumberGenerator();
+		List<Integer> numberList = numberGenerator.createRandomNumbers();
+//		List<Integer> numberList = Arrays.asList(1,2,3);
+		for (int number : numberList)
+			numberSet.add(number);
+		boolean equals = numberSet.size() == numberList.size();
+		assertEquals(equals, true);
+
+	}
+}
