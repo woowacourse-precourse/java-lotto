2b65c478a557eb9853f3546f599706384ce545ef
iamjooon2 <iamjooon2@gmail.com>
2022-11-13 03:48:10 +0900

    feat: 복권 번호를 발급하는 기능 구현

---
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.view.OutputView;
+
+public class NumberGenerator {
+    private static final Integer MIN_RANGE = 1;
+    private static final Integer MAX_RANGE = 45;
+    private static final Integer COUNT = 6;
+
+    public static List<Integer> getNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_RANGE, MAX_RANGE, COUNT);
+        return numbers;
+    }
+
+}
