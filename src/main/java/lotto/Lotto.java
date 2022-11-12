b415fddee8c3c88fd26ff2bc86196c12951dc06c
hyejungg <kimhyejung12@naver.com>
2022-11-12 22:47:46 +0900

    feat: 로또 정답과 내가 산 로또 매치되는 개수에 따라 WinType return 하는 함수 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import static lotto.LottoWinType.matchGameScoreAndLottoWinType;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import lotto.utils.Constant;
@@ -21,4 +23,20 @@ public class Lotto {
     public List<Integer> getNumbers() {
         return numbers.stream().sorted().collect(Collectors.toList());
     }
+
+    public LottoWinType findMatchedLotto(List<Integer> myLotto, List<Integer> winLotto, int bonusLotto) {
+        int answerCount = 0;
+        int bonusCount = 0;
+        for (Integer number : myLotto) {
+            if (winLotto.contains(number)) {
+                answerCount++;
+            }
+
+            if (number == bonusLotto) {
+                bonusCount++;
+            }
+        }
+        return matchGameScoreAndLottoWinType(answerCount, bonusCount);
+    }
+
 }
