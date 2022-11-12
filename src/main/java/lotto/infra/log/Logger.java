b77729fb052c499dfef8a3b35f5367649d5698fd
shinD <huipulci1@naver.com>
2022-11-13 00:01:28 +0900

    feat: 에러 로그 기능 추가

---
+++ src/main/java/lotto/infra/log/Logger.java
@@ -0,0 +1,29 @@
+package lotto.infra.log;
+
+import lotto.infra.port.outbound.StandardOutput;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class Logger {
+
+    public static final String ERROR_PREFIX = "[ERROR]";
+
+    public void error(String message, Class<? extends RuntimeException> exceptionClass) {
+        try {
+            Constructor<? extends RuntimeException> constructor = exceptionClass.getDeclaredConstructor(String.class);
+
+            error(message);
+
+            throw constructor.newInstance(message);
+        } catch (InvocationTargetException | InstantiationException
+                 | IllegalAccessException | NoSuchMethodException e) {
+            // ignore
+        }
+    }
+
+    private void error(final String message) {
+        String log = String.format("%s %s", ERROR_PREFIX, message);
+        StandardOutput.println(log);
+    }
+}
