4e3bd9a201e1b695c3f8dfbe1bb36c518cacc9a7
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-11-13 00:02:46 +0900

    Feat : 1.-2) 기능 추가
    입력 번호가 중복이 있으면 예외처리 후 종료

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,7 +15,16 @@ public class Lotto {
             System.out.println("[ERROR] 당첨 번호는 정확히 6개를 입력하셔야 합니다.");
             throw new IllegalArgumentException();
         }
-
+        Iterator itrForNumbers = numbers.iterator();
+        int[] arrayForCheckOverLap = new int[46];
+        while (itrForNumbers.hasNext()){
+            int checkIndex = (int)itrForNumbers.next();
+            if (arrayForCheckOverLap[checkIndex] == 1){
+                System.out.println("[ERROR] 당첨 번호는 중복이 없어야 합니다.");
+                throw new IllegalArgumentException();
+            }
+            arrayForCheckOverLap[checkIndex] = 1;
+        }
     }
     public void checkLottoWinnings(ArrayList<int[]> lotteryBundleArray, int bonusWinningNumber) {
 
