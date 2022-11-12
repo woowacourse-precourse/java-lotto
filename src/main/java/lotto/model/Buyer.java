0b6b2a13a0fc4c9db8d0e59ae7b1060da66fb150
gomljo <gomljo0307@naver.com>
2022-11-12 23:33:16 +0900

    feat: 숫자가 아닌 문자를 입력했는지 확인하는 기능 구현

--- src/main/java/lotto/model/Buyer.java
+++ src/main/java/lotto/model/Buyer.java
@@ -1,6 +1,10 @@
 package lotto.model;
 
 public class Buyer {
+    private static final String ENTER_WRONG_LETTERS_MESSAGE = "[ERROR] 금액을 잘못 입력하셨습니다. 프로그램을 종료합니다.";
+
+    private static final String NUMERIC = "\\d+";
+    private static final String BLANK = "";
     private static final int AMOUNT_UNIT=1000;
     private static final String ENTER_WRONG_AMOUNT_MESSAGE="[ERROR] 금액의 최소 단위(1,000원)가 맞지 않습니다. 프로그램을 종료합니다.";
     private static final int PROPER_AMOUNT_CONDITION=0;
@@ -21,4 +25,10 @@ public class Buyer {
             throw new IllegalArgumentException(ENTER_WRONG_AMOUNT_MESSAGE);
         }
     }
+    private void validateProperLetters(String purchaseAmount) {
+        if (!purchaseAmount.replaceAll(NUMERIC, BLANK).equals(BLANK)) {
+            throw new IllegalArgumentException(ENTER_WRONG_LETTERS_MESSAGE);
+        }
+    }
+
 }
