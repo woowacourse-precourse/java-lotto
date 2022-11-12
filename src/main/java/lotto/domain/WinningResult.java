0b5327c4954ab0beae1acbfb652d7180c23ee4dd
devyourown <hyojoon56@gmail.com>
2022-11-12 23:32:20 +0900

    refactor : make the logic real and pass the test

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -6,13 +6,26 @@ public class WinningResult {
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
     private int accordedNumbers;
+    private boolean isHitBonus;
+
     public WinningResult(List<Integer> winningNumbers, int bonusNumber) {
         this.winningNumbers= winningNumbers;
         this.bonusNumber = bonusNumber;
     }
 
     public void checkTheResult(List<Integer> lottoNumbers) {
-        accordedNumbers = 3;
+        accordedNumbers = (int)winningNumbers.stream()
+                .filter(num -> lottoNumbers.contains(num))
+                .count();
+        checkTheBonus(lottoNumbers);
+    }
+
+    private void checkTheBonus(List<Integer> lottoNumbers) {
+        if (accordedNumbers != 5)
+            return ;
+        isHitBonus = lottoNumbers.contains(bonusNumber);
+        if (isHitBonus)
+            accordedNumbers += 1;
     }
 
     public int getAccordedNumbers() {
