16da9633a1ba783ad19caa75a6152f6a530ecb56
somsom13 <somin1113@naver.com>
2022-11-13 00:17:41 +0900

    test(WinningLotto): test code for checking WinningLotto creation rule
    콘솔로 입력받은 보너스 번호는
    1. 당첨 번호와 중복이 되면 안된다.
    2. 1~45의 범위 내에 있는 숫자여야 한다.
    당첨 번호 및 보너스 번호를 생성할 때 위의 규칙을 검증하기 위한 테스트 코드를 작성한다.
    
    변경사항:
    - WinningLottoTest : 중복 여부, 범위를 벗어나는지를 검증하기 위한 테스트 코드 추가
    - InputValidatorTest : 입력 자체의 예외를 검증하기 위한 테스트 코드 추가

---
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import lotto.domain.errorenum.Error;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningLottoTest {
+
+    @DisplayName("보너스 번호가 당첨 로또 번호와 중복되는 경우 오류가 발생한다")
+    @Test
+    void createBonusNumberWithDuplication() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 6;
+
+        assertThatThrownBy(() -> new WinningLotto(lotto, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Error.BONUS_NUMBER_DUPLICATE.getCode());
+    }
+
+    @DisplayName("보너스 번호가 로또 숫자 범위에서 벗어나는 경우 오류가 발생한다")
+    @Test
+    void createBonusNumberOutOfRange() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 0;
+
+        assertThatThrownBy(() -> new WinningLotto(lotto, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Error.NOT_IN_RANGE.getCode());
+    }
+
+}
