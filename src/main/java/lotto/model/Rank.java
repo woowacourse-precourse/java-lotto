f2430e30edd20c38e5261dfe508c4643752ccef9
김현재 <41482946+galug@users.noreply.github.com>
2022-11-12 11:37:52 +0900

    feat(rank): 순위에 따른 문구를 반환해주는 기능 추가

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,15 +1,24 @@
 package lotto.model;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 public enum Rank {
     FIRST(6,2000000000),
-    SECOND(5,30000000),
+    SECOND(5,30000000){
+        @Override
+        public String toString() {
+            return getMatchingState() + BONUS_MATCHING_STATE + getPriceState();
+        }
+    },
     THIRD(5,1500000),
     FOURTH(4,50000),
     FIFTH(3,5000),
     NOTHING(0,0);
 
+    private static final String MATCHING_STATE = "%d개 일치";
+    private static final String BONUS_MATCHING_STATE = ", 보너스 볼 일치";
+    private static final String PRICE_STATE = " (%s원)";
     private final int matchingNumber;
     private final int price;
 
@@ -30,5 +39,19 @@ public enum Rank {
                 .filter(value -> value.matchingNumber == matchingNumber)
                 .findFirst()
                 .orElse(Rank.NOTHING);
+    }
+
+    @Override
+    public String toString() {
+        return getMatchingState() + getPriceState();
+    }
+
+    protected String getMatchingState() {
+        return String.format(MATCHING_STATE, matchingNumber);
+    }
+
+    protected String getPriceState() {
+        DecimalFormat df = new DecimalFormat("###,###");
+        return String.format(PRICE_STATE, df.format(price));
     }
 }
