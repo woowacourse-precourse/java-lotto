f9ae8f82fe51d9ff193424aa1e9700f965e0e802
YE-EUN <musiclove529@naver.com>
2022-11-13 04:53:05 +0900

    feat(Application) : run() 구현
    - 로또 프로그램을 실행하는 함수

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.domain.LottoGenerator;
+import lotto.domain.LottoPurchaser;
+import lotto.domain.Statistics;
+import lotto.domain.WinningNumbers;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        run();
+    }
+
+    public static void run(){
+        LottoPurchaser lottoPurchaser = new LottoPurchaser();
+        LottoGenerator lottoGenerator = new LottoGenerator(lottoPurchaser.getPurchaseAmount());
+        WinningNumbers winningNumbers = new WinningNumbers();
+        Statistics statistics = new Statistics(winningNumbers.getNumbers(), lottoGenerator.getLottos());
+        System.out.println(statistics.toString());
     }
 }
