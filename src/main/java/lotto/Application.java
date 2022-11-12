a33c03ed7ff3404a6550b5952307dc3d7ae58722
coldbucks <met.hsw@gmail.com>
2022-11-12 17:24:05 +0900

    feat(Lotto): ''개를 구매했습니다. 출력 및 연산 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,16 +1,24 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
 
 public class Application {
+
+    static int lottoPrice = 1000;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Application application = new Application();
+
 
         System.out.println("구매금액을 입력해 주세요.");
         String buyMoney = Console.readLine();
 
         int buyLottoMoney = Integer.parseInt(buyMoney);
+        //로또 한장은 1000원이다
+
+        int lottoAmount = buyLottoMoney / lottoPrice ;
+        System.out.println(lottoAmount + "개를 구매했습니다.");
 
     }
 }
