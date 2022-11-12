a017e5754dbfa3d94db5aec1699a11835506ce27
grand7070 <grand7070@naver.com>
2022-11-12 16:20:17 +0900

    feat: 로또 번호를 n장 만큼 구한다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,10 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -9,5 +13,12 @@ public class Application {
         Price price = new Price(Console.readLine());
         // 1-3. 로또 구입 갯수 n(로또 구입 금액에서 1000원으로 나누어 떨어지는 몫)을 구한다.
         Integer n = price.getLottoNum();
+
+        // 2. 로또 번호를 n장 만큼 구한다.
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            lottos.add(lotto);
+        }
     }
 }
