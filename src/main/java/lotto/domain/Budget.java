704c066db18ad4b09dd2560a451611d189bcfe80
cgk95 <kjkj5868@gmail.com>
2022-11-12 17:02:35 +0900

    feat(domain) : 로또 구입 금액과 그 유효성을 검증하는 Budget 클래스

---
+++ src/main/java/lotto/domain/Budget.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import lotto.UI.ErrorMessage;
+
+import java.util.function.Predicate;
+
+public class Budget {
+    private final int budgetNumber; // 구입한 로또 갯수
+
+    public Budget(String money) {
+        isMoneyValid(money);
+        int budget = Integer.parseInt(money);
+        isMoneyDividable(budget);
+        this.budgetNumber = budget / 1000;
+    }
+
+    private void isMoneyValid(String money) throws IllegalArgumentException {
+        Boolean notDigit = money.chars().mapToObj(obj -> (char) obj).anyMatch(Predicate.not(Character::isDigit));
+        if (notDigit) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_NUMBER.getValue());
+        }
+    }
+
+    private void isMoneyDividable(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_MULTIPLE_OF_1000.getValue());
+        }
+    }
+    public int getBudgetNumber(){ // getter
+        return this.budgetNumber;
+    }
+}
