e777ef1ba41137c68c950cea47c3a9186de35108
SeongchanCho <wordnumperiod@gmail.com>
2022-11-12 17:14:43 +0900

    feat(Lotto.java): 하나의 로또 답안지 채점하는 코드 작성
    produceWinningStatistic 메소드 정의 작성
    로또 정답과 비교해 당첨 통계를 계산하는 코드이다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -21,6 +23,23 @@ public class Lotto {
             }
         }
     }
+
+    private WinningStatistic produceWinningStatistic(Lotto answer, int bonusNum){
+        Set<Integer> answerNumSet=new HashSet<>(answer.numbers);
+        int numOfCorrectLottoNum=0;
+        boolean bonusNumCorrect=false;
+        for (int number :numbers){
+            if(answerNumSet.contains(number)){
+                numOfCorrectLottoNum+=1;
+                answerNumSet.remove(number);
+            }
+            if(number==bonusNum){
+                bonusNumCorrect=true;
+            }
+        }
+        return WinningStatistic.getInstance(numOfCorrectLottoNum,bonusNumCorrect);
+    }
+
 
     // TODO: 추가 기능 구현
 }
