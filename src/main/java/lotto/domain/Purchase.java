06f0b95c9d61bacc211469404dd052aa2f06ef41
sangeun-b <baek0007@algonquinlive.com>
2022-11-13 05:24:40 +0900

    feat: 로또 구매 금액 입력 기능 추가

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -2,4 +2,14 @@ package lotto.domain;
 
 public class Purchase {
 
+    public int calculateAmount(String amount){
+        validate(Integer.parseInt(amount));
+        return Integer.parseInt(amount)/1000;
+    }
+
+    private void validate(int amount){
+        if(amount%1000!=0){
+            throw new IllegalArgumentException("[ERROR] 구입 금액이 1000원 단위로 나누어떨어지지 않습니다.");
+        }
+    }
 }
--- src/main/java/lotto/view/Play.java
+++ src/main/java/lotto/view/Play.java
@@ -1,16 +1,30 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Purchase;
+
 public class Play {
-    public static final String START_MESSAGE = "구입금액을 입력해 주세요.";
-    public static final String CHECK_MESSAGE = "개를 구매했습니다.";
-    public static final String INPUT_WINNING_MESSAGE = "당첨 번호를 입력해 주세요.";
-    public static final String INPUT_BONUS_MESSAGE = "보너스 번호를 입력해 주세요.";
-    public static final String WINNING_MESSAGE = "당첨 통계\n" +"---";
-    public static final String RATE_MESSAGE = "총 수익률은 %f%입니다.";
+    private static final String START_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String CHECK_MESSAGE = "개를 구매했습니다.";
+    private static final String INPUT_WINNING_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String WINNING_MESSAGE = "당첨 통계\n" +"---";
+    private static final String RATE_MESSAGE = "총 수익률은 %f%입니다.";
+
+    private Purchase purchase = new Purchase();
 
     public void startPlay(){
         System.out.println(START_MESSAGE);
 
+        String amount = Console.readLine();
+        int total = purchase.calculateAmount(amount);
+
+
+
+
+
+
     }
+
 
 }
