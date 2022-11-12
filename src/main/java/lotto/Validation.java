e60ae84b9743d74e0c93a5233fea6a26261193f2
sieun <sieun960513@naver.com>
2022-11-12 21:09:28 +0900

    [validation] 당첨번호 길이 확인 로직

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import java.util.List;
+
 public class Validation {
 
     static final int MONEY_UNIT = 1000;
     static final int LOTTO_MIN_NUM = 1;
     static final int LOTTO_MAX_NUM = 45;
+    static final int LOTTO_SIZE = 6;
 
     public static boolean thousandUnit(int num) {
         return num % MONEY_UNIT == 0;
@@ -12,5 +15,9 @@ public class Validation {
 
     public static boolean lottoNumRange(int num) {
         return LOTTO_MIN_NUM <= num && num <= LOTTO_MAX_NUM;
+    }
+
+    public static boolean lottoLength(List<Integer> arr) {
+        return arr.size() == LOTTO_SIZE;
     }
 }
