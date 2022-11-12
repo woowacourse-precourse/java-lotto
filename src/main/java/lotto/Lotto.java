026c62c50da472f34da3b09fa95a58df632ff90c
dong yeol <ehfql6363@naver.com>
2022-11-13 01:51:02 +0900

    FEAT(Lotto.java) : Update method(validate), add method(getNumbers)_
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,14 +8,29 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 총 6개의 숫자로 구성되어 있습니다.");
+        }
+
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않습니다.");
+        }
+
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45사이의 수 입니다.");
+            }
         }
     }
 
     // TODO: 추가 기능 구현
+    /* 리스트 불러오기 */
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
