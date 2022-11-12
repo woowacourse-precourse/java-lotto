2f08dabe2c0aa2873c7e60b30c13fba9d3193a96
lacram <chemin9898@gmail.com>
2022-11-12 21:04:38 +0900

    feat: 보너스 번호를 입력 받는 기능

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -3,19 +3,20 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
-    /*
-    1. 로또 구입 금액을 입력받는 기능
-    2. 당첨 번호를 입력 받는 기능
-    3. 보너스 번호를 입력 받는 기능
-    * */
 
+    // 로또 구입 금액을 입력받는 기능
     public static String enterLottoPurchaseAmout() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
-
+    // 당첨 번호를 입력 받는 기능
     public static String enterLottoWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+    // 보너스 번호를 입력 받는 기능
+    public static String enterLottoBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
 }
