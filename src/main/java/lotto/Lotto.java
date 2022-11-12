5bb33851fd93995bec4ebd544fc53da0d552f296
Eungyu-code <Agyu625@gmail.com>
2022-11-12 17:21:02 +0900

    update(validateNumbersAreDuplicate()): 접근 제어자 변경
    private -> public
    - Application class에서 함수에 접근하기 위해 변

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -32,7 +32,7 @@ public class Lotto {
         }
     }
 
-    private void validateNumbersAreDuplicate(List<Integer> numbers) {
+    public void validateNumbersAreDuplicate(List<Integer> numbers) {
         for (int i = 0; i < numbers.size() - 1; i++) {
             for (int j = i + 1; j < numbers.size(); j++) {
                 checkNumbersAreDuplicate(numbers.get(i), numbers.get(j));
