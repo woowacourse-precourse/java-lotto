185918519a11ebf9fe0245b17e7363889bdaa4d4
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-12 15:26:31 +0900

    style: 불필요한 주석 제거

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,15 +1,9 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-
-    Validator validator = new Validator();
-    User user = new User();
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -21,10 +15,4 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
-
-
-    /**
-     * 당첨 번호를 입력하는 메소드
-     * 게임 횟수만큼 랜덤한 로또 번호를 출력하는 메소드
-     */
 }
