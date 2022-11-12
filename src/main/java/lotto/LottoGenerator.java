5348ba0dc6197a5bdad2ea23ad973a74276a1d0a
Cho SangHyun <98shcho@naver.com>
2022-11-12 22:40:30 +0900

    feat(로또발행하기): 정렬기능 추가

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class LottoGenerator {
@@ -9,9 +10,14 @@ public class LottoGenerator {
         List<Lotto> lottos = new ArrayList<>();
         for (int index = 0; index < lottoQuantity; index++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            sortNumbers(numbers);
             Lotto lotto = new Lotto(numbers);
             lottos.add(lotto);
         }
         return lottos;
+    }
+
+    private static void sortNumbers(List<Integer> numbers) {
+        numbers.sort(Comparator.naturalOrder());
     }
 }
