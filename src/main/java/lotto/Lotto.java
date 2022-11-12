32ab1f1543bbaea1a24d3ce7ab1cf70a13d40503
sieun <sieun960513@naver.com>
2022-11-12 22:35:53 +0900

    [lotto] 로또 점수를 Score class로 반환

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -22,7 +23,33 @@ public class Lotto {
         return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
+    public Score getScore(List<Integer> winningNumbers, int bonusNumber) {
+        int[] output = new int[2];
+        Set<Integer> nums = Converter.listToSet(numbers);
+        Set<Integer> winNums = Converter.listToSet(winningNumbers);
+        nums.retainAll(winNums);
+        output[0] = nums.size();
+        if(numbers.contains(bonusNumber))
+            output[1] = 1;
+        return getScoreEnum(output);
+    }
+
+    public Score getScoreEnum(int[] score) {
+        if (score[0] == 3) {
+            return Score.FIFTH;
+        }
+        if (score[0] == 4) {
+            return Score.FOURTH;
+        }
+        if (score[0] == 5) {
+            if (score[1] == 1) {
+                return Score.SECOND;
+            }
+            return Score.THIRD;
+        }
+        if (score[0] == 6) {
+            return Score.FIRST;
+        }
+        return Score.NOTHING;
     }
 }
