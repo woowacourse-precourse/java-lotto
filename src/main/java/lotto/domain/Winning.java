7f8119370539b4307cffe42aba6c43998b1608ba
MinjeongKong <kong1301@ajou.ac.kr>
2022-11-12 17:47:01 +0900

    feat: Winning 도메인 생성

---
+++ src/main/java/lotto/domain/Winning.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Winning {
+    private List<Integer> numbers;
+    private int bonus;
+
+    public Winning() {
+    }
+
+    public void pickNum() {
+
+    }
+
+    private void validate() {
+
+    }
+
+    public void compare(User user) {
+
+    }
+}
