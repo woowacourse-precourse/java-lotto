7539c64c076e75851f5978ae737c7c9edfcfc65e
Sehbeom <j3beom@gmail.com>
2022-11-12 19:08:29 +0900

    feat(view): 발행된 로또 안내 기능 추가
    발행된 로또의 수량과 로또 번호를 출력하는 기능 추가

--- src/main/java/lotto/domain/view/LottoGuide.java
+++ src/main/java/lotto/domain/view/LottoGuide.java
@@ -1,7 +1,9 @@
 package lotto.domain.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 import lotto.constants.GuideSentences;
+import lotto.domain.model.Lotto;
 
 public class LottoGuide {
 
@@ -11,5 +13,11 @@ public class LottoGuide {
         int purchaseAmount = Integer.parseInt(Console.readLine());
 
         return purchaseAmount;
+    }
+
+    public void informIssuedLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + GuideSentences.OUTPUT_NUMBER_OF_PURCHASES.getSentence());
+
+        lottos.forEach(oneLotto -> System.out.println(oneLotto.getNumbers()));
     }
 }
