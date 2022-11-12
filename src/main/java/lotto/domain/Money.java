4b383ee5024f135b7311402931d30a53658a321d
이정우 <acg6138@naver.com>
2022-11-12 18:02:55 +0900

    feat(Money): 구매 금액을 입력받고 검증하는 돈 객체 구현

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+public class Money {
+    public static final int MONEY_DIVIDE_UNIT = 1000;
+    private final int money;
+    private Money(String input) {
+        this.money = validate(input);
+    }
+
+    public int processInput(String input) {
+        try{
+            return Integer.parseInt(input);
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액에는 숫자만 입력해야 합니다.");
+        }
+    }
+
+    public int validate(String input) {
+        int money = processInput(input);
+        if (money < MONEY_DIVIDE_UNIT) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원보다 커야합니다.");
+        }
+        if (money % MONEY_DIVIDE_UNIT != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위만 입력해야 합니다.");
+        }
+        return money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}
