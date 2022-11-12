550b2723b530413626c31ac49fdcae799721b8ce
JUN <chltmdwns96@naver.com>
2022-11-12 23:26:07 +0900

    refactor : rename Message to Constants

--- src/main/java/values/Constants.java
+++ src/main/java/values/Constants.java
@@ -1,6 +1,6 @@
 package values;
 
-public class Message {
+public class Constants {
     public static class Console {
         public static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
         public static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
