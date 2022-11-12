69036bb83f59011ac266b721f87e658059154d9b
grand7070 <grand7070@naver.com>
2022-11-12 15:53:20 +0900

    feat: 로또 구입 금액을 입력 받는다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 1. 로또 구입 금액을 입력 받는다.
+        System.out.println("구입금액을 입력해 주세요.");
+        Price price = new Price(Console.readLine());
     }
 }
---
+++ src/main/java/lotto/Price.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class Price {
+    private final String price;
+
+    public Price(String price) {
+        this.price = price;
+    }
+}
