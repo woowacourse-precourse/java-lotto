93cb077bcbea8bd8ce5d8851b191c41f4cd734ad
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-12 23:58:32 +0900

    feat(User): User 클래스 메서드 선언

---
+++ src/main/java/lotto/User/User.java
@@ -0,0 +1,30 @@
+package lotto.User;
+
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    List<Lotto> lottos;
+
+    public User() {
+        this.lottos = new ArrayList<>();
+    }
+
+    public void buyLotto(int money) {
+
+    }
+
+    private boolean validateMoney(int money) {
+        return true;
+    }
+
+    public void printLottos() {
+
+    }
+
+    public void printOneLotto() {
+
+    }
+}
