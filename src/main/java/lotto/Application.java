618bfb38d7971b4fa56f4d83e6dc9a86d0dd5e79
limjae <limjae96@gmail.com>
2022-11-12 23:33:55 +0900

    feat: 예외 발생 시 텍스트 출력 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import java.util.Arrays;
+import lotto.generator.NumberGenerator;
+import lotto.generator.RandomsNumberGenerator;
+import lotto.manager.LottoManager;
+import lotto.ui.Input.ConsoleParameterInput;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            LottoManager manager = new LottoManager(new RandomsNumberGenerator(), new ConsoleParameterInput());
+            manager.tryLotto();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            Arrays.stream(e.getStackTrace()).forEach(System.out::println);
+        }
     }
 }
