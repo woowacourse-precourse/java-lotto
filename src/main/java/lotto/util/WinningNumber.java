ffdd097fc9d1d9be442148c207a1d3c1702dc778
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-12 23:57:24 +0900

    feat: WinningNumber class

---
+++ src/main/java/lotto/util/WinningNumber.java
@@ -0,0 +1,11 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class WinningNumber {
+
+}
