16cd9976b13116352a9a1633662750bb8303ef98
goldcollar95 <yjs7298@naver.com>
2022-11-12 16:23:24 +0900

    feat(Validator): User.lottoListSix()에 해당하는 입력값에 대한 제한조건을 구현한 메서드

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -1,4 +1,33 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Validator {
+
+    User user = new User();
+
+    public void ValidatorSix(List<Integer> ValidatorNumber){
+        if(ValidatorNumber.size() != 6){
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해야 합니다!");
+        }
+    }
+
+    public void ValidatorRepeatBan(List<Integer> ValidatorNumber){
+        for(Integer integer : ValidatorNumber){
+            if(Collections.frequency(Collections.singleton(integer), ValidatorNumber) > 1){
+                throw new IllegalArgumentException("[ERROR] 중복된 수는 들어올수 없습니다!");
+            }
+        }
+    }
+
+    public void ValidatorRange(List<Integer> ValidatorNumber){
+        for(Integer integer : ValidatorNumber){
+            if(integer > 45 || integer < 0){
+                throw new IllegalArgumentException("[ERROR] 1부터~45의 수까지 입력가능합니다!")
+            }
+        }
+    }
 }
