b571e7e668fb3b1878d0673560fa8ed76e9e4987
yummygyudon <bang2brew@gmail.com>
2022-11-12 15:38:08 +0900

    FEAT(domain): 구매 금액 입력값 통합 유효성 검사 메서드
    - 메서드 명 : `validateMoneyInput`
    - 순서
      1. String 타입의 inputValue 매개변수
      2. `replace`메서드를 통해서 "," 제거 및 Integer 형 변환
      3-1. 최소 구매 금액 충족 여부 검사 : checkingPurchasable
      3-2. 나눗셈 가능 금액 여부 검사 : checkingDividable
    - 추가 예외 처리
      : 쉼표를 제거한 input값에 숫자가 아닌 값이 존재하여
      Integer.parseInt() 메서드 수행 중 NumberFormatException이 발생할 경우,
       ErrorMessages.NOT_NUMBER_EXIST의 에러 메세지를 출력하는 IllegalArgumentException을 발생 시킨다.

--- src/main/java/lotto/domain/validator/PriceValidator.java
+++ src/main/java/lotto/domain/validator/PriceValidator.java
@@ -5,9 +5,16 @@ import lotto.util.ErrorMessages;
 public class PriceValidator {
     private static final int REF_MONEY_AMOUNT = 1_000 ;
 
-//    public void validateMoneyInput(String inputValue){
-//        final int INPUT_MONEY = Integer.parseInt(inputValue.replace(",",""));
-//    }
+    public void validateMoneyInput(String inputValue){
+        try{
+            final int inputMoney = Integer.parseInt(inputValue.replace(",",""));
+            checkingPurchasable(inputMoney);
+            checkingDividable(inputMoney);
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException(ErrorMessages.NOT_NUMBER_EXIST.getMessage());
+        }
+
+    }
 
     public void checkingPurchasable(int inputMoney){
         if (inputMoney < REF_MONEY_AMOUNT){
