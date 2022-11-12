e062c700e1e7e5080c8d6f89b62b1f1f4ba5d620
ParkRootSeok <g_e_un@naver.com>
2022-11-13 02:36:01 +0900

    test(Lotto) : 로또 발행, 용량 초과, 중복 번호에 대한 테스트 코드 작성

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,54 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.config.Constant.LOTTO_NUMBER_CAPACITY;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoTest {
+
+    private static Lotto lotto;
+
+    @BeforeAll
+    static void 로또_발행() {
+        Store store = new Store();
+        List<Integer> numbers = store.drawNumber();
+        lotto = new Lotto(numbers);
+    }
+
+    @Test
+    void 로또_발행_확인() {
+
+        for (int number : lotto.getLotto()) {
+            System.out.printf(" %d", number);
+        }
+        System.out.println();
+
+        assertThat(lotto.getLotto().size()).isEqualTo(LOTTO_NUMBER_CAPACITY);
+
+    }
+
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    // 아래에 추가 테스트 작성 가능
+}
