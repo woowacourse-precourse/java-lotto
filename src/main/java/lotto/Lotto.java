18c0688dac0cc609a4abfd8c53139d6b7ad7912b
linda2927 <linda2927@naver.com>
2022-11-12 20:28:14 +0900

    fix: change access modifier for validateBonusNumber

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -37,7 +37,7 @@ public class Lotto {
             throw new IllegalArgumentException("[Error] 당첨번호는 총 6개여야 합니다.");
         }
         for (int i = 0; i < winningNumber.size() - 1; i++) {
-            if (winningNumber.subList(i, winningNumber.size()).contains(winningNumber.get(i))) {
+            if (winningNumber.subList(i + 1, winningNumber.size()).contains(winningNumber.get(i))) {
                 throw new IllegalArgumentException("[Error] 당첨번호에는 중복되는 숫자가 있어선 안됩니다.");
             }
             if (winningNumber.get(i) < 1 || winningNumber.get(i) > 45) {
@@ -62,7 +62,7 @@ public class Lotto {
      * @param bonusNumInput String format of user input
      * @throws IllegalArgumentException if bonus number is not a number between 1 and 45 or is not a number
      */
-    private void validateBonusNumber(String bonusNumInput) throws IllegalArgumentException {
+    public void validateBonusNumber(String bonusNumInput) throws IllegalArgumentException {
         int bonusNum = Integer.parseInt(bonusNumInput);
         if (bonusNum < 1 || bonusNum > 45) {
             throw new IllegalArgumentException("[Error] 보너스 번호는 1 에서 45 사이의 정수여야 합니다.");
