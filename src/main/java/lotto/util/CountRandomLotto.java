a7a89b4f25c9e08f534aec0fb64729446859905d
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-12 23:56:14 +0900

    feat: calculate()-> int 형 변수를 입력받아, WinningNumber 클래스의 RandomLotto()를 int 형 변수 만큼 생성

--- src/main/java/lotto/util/CountRandomLotto.java
+++ src/main/java/lotto/util/CountRandomLotto.java
@@ -7,4 +7,11 @@ public class CountRandomLotto {
     private static final WinningNumber winningNumber = new WinningNumber();
     private static final List<List<Integer>> myLotto=new ArrayList<>();
 
+    public List<List<Integer>> calculate(int count){
+        for(int i=0; i<count; i++){
+            myLotto.add(winningNumber.RandomLotto());
+            System.out.println(myLotto.get(i));
+        }
+        return myLotto;
+    }
 }
