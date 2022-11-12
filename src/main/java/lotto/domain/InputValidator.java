a04c44811567daceb12f18459e6fb38cbb4a57f0
ddubidubap <park62000@gmail.com>
2022-11-12 18:39:13 +0900

    refactor: 구입 금액, 당첨 번호, 보너스 번호의 예외처리 함수를 public으로 변경
    숫자인지 확인, 1~45 사이의 숫자인지 확인하는 함수를 private으로 변경

--- src/main/java/lotto/domain/InputValidator.java
+++ src/main/java/lotto/domain/InputValidator.java
@@ -3,34 +3,42 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import static lotto.view.Constants.*;
 
 public class InputValidator {
-    public Integer validateNumeric(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            System.out.println(NOT_NUMERIC);
-            throw new IllegalArgumentException();
-        }
+    public Integer validateMoney(String input) {
+        return validateNumeric(input);
     }
 
     public List<Integer> validateWinningNumbers(String input) {
         List<Integer> numbers = new ArrayList<>();
         List<String> splitForm = splitInput(input);
         for (String piece: splitForm) {
-            int number = validateProperNumber(piece);
+            int number = validateLotteryNumber(piece);
             numbers.add(number);
         }
         return numbers;
     }
 
-    public int validateProperNumber(String input) {
+    public int validateBonusNumber(String input) {
+        return validateLotteryNumber(input);
+    }
+
+    private int validateLotteryNumber(String input) {
         int number = validateNumeric(input);
         validateRange(number);
         return number;
+    }
+
+
+    private Integer validateNumeric(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println(NOT_NUMERIC);
+            throw new IllegalArgumentException();
+        }
     }
 
     private void validateRange(int number) {
