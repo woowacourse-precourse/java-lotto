950f5c87f02eef71c119b034b7a57358c9df16ee
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-13 02:51:00 +0900

    feat(BoughtLotto): UserTest를 위한 임시 생성자 수정

--- src/main/java/lotto/BoughtLotto.java
+++ src/main/java/lotto/BoughtLotto.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class BoughtLotto extends Lotto{
     public BoughtLotto() {
-        super(new ArrayList<>());
+        super(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
     }
 
     private List<Integer> createLottoNumber() {
