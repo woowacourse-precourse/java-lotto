d70827dc6b8b57c91ddecde4b88543d5a78defae
ChanghyunKim <variety82p@gamil.com>
2022-11-12 18:44:51 +0900

    test(AmountTest) : 구매금액이 숫자 형식이 아닌케이스 테스트

---
+++ src/test/java/lotto/AmountTest.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static lotto.utils.Messages.ERROR_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AmountTest extends NsTest {
+
+    @DisplayName("구매 금액에 숫자가 아니면 예외가 발생한다")
+    @Test
+    void createNonNumericInput(){
+        assertSimpleTest(() -> {
+            runException("1000j");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
