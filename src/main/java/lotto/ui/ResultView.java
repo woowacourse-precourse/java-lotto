eaae5070df5a72c617c091f4a2e21132ab1691b7
SHEOMM <esh0821@snu.ac.kr>
2022-11-12 20:53:27 +0900

    feat: 로또번호 3개 일치 메시지 출력

--- src/main/java/lotto/ui/ResultView.java
+++ src/main/java/lotto/ui/ResultView.java
@@ -1,6 +1,14 @@
 package lotto.ui;
 
+import java.util.List;
+
 public class ResultView implements View{
+    private List<Integer> result;
+
+    ResultView(List<Integer> result){
+        this.result = result;
+    }
+
     @Override
     public void makeView() {
 
@@ -9,5 +17,9 @@ public class ResultView implements View{
     private void printDefaultMessage(){
         System.out.println("당첨 통계");
         System.out.println("---");
+    }
+
+    private void print3matches(){
+        System.out.println("3개 일치 (5,000원) - "+ result.get(0) +"개");
     }
 }
