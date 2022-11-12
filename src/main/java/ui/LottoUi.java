553da29e154142cc867b30e36dbb52939d304fca
JUN <chltmdwns96@naver.com>
2022-11-12 17:27:30 +0900

    feat : get lotto count with money - validateMoney method - getMoney method - getLottoCnt method

--- src/main/java/ui/LottoUi.java
+++ src/main/java/ui/LottoUi.java
@@ -1,17 +1,21 @@
 package ui;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static values.Message.Error.*;
 
 public class LottoUi {
+    private static final int LOTTO_PRICE = 1000;
 
-    public static void validate(List<Integer> numbers){
+    public static void validate(List<Integer> numbers) {
         validateNumbersSize(numbers);
         validateDuplication(numbers);
         validateNumbersRange(numbers);
     }
+
 
     private static void validateNumbersSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
@@ -19,19 +23,37 @@ public class LottoUi {
         }
     }
 
-    private static void validateDuplication(List<Integer> numbers){
+    private static void validateDuplication(List<Integer> numbers) {
         List<Integer> duplicationRemoved = numbers.stream().distinct().collect(Collectors.toList());
 
-        if(duplicationRemoved.size() != 6){
+        if (duplicationRemoved.size() != 6) {
             throw new IllegalArgumentException(DUPLICATION_ERROR);
         }
     }
 
-    private static void validateNumbersRange(List<Integer> numbers){
+    private static void validateNumbersRange(List<Integer> numbers) {
         numbers.forEach(num -> {
-            if(!(num >= 1 && num <=45)){
+            if (!(num >= 1 && num <= 45)) {
                 throw new IllegalArgumentException(RANGE_ERROR);
             }
         });
+    }
+
+    public static int getLottoCnt(){
+        int lottoCnt = getMoney()/ LOTTO_PRICE;
+        return lottoCnt;
+    }
+
+    private static int getMoney(){
+        int money = Integer.parseInt(Console.readLine());
+        validateMoney(money);
+
+        return money;
+    }
+
+    private static void validateMoney(int money){
+        if(money % LOTTO_PRICE != 0){
+            throw new IllegalArgumentException();
+        }
     }
 }
