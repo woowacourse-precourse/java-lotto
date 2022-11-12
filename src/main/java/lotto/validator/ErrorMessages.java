dea2676aac4a541ef501bf9aaedc566c3fa8c1ab
Jin409 <sualng123@gmail.com>
2022-11-12 11:02:16 +0800

    refactor(ErrorMessages): [ERROR] 상수로 수정 및 일부 에러 메시지 수정
    - 하드코딩을 지양하기 위해 겹치는 [ERROR] 문구를 상수로 바꿈
    - 일부 에러 메시지의 변수정을
    에러명_ERROR_MESSAGE 라는 일관성을 주기 위하여 수정

--- src/main/java/lotto/validator/ErrorMessages.java
+++ src/main/java/lotto/validator/ErrorMessages.java
@@ -6,12 +6,15 @@ import static lotto.constants.LottoConstant.VALID_MAX_RANGE;
 import static lotto.constants.LottoConstant.VALID_MIN_RANGE;
 
 public class ErrorMessages {
+    public static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
     public static final String INVALID_LENGTH_ERROR_MESSAGE =
-            "[ERROR] 로또 번호는 " + VALID_LOTTERY_NUMBER_LENGTH + "개의 숫자로 이루어져야 합니다.";
-    public static final String DUPLICATE_LOTTO_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복되지 않아야 합니다.";
+            ERROR_MESSAGE_PREFIX + "로또 번호는 " + VALID_LOTTERY_NUMBER_LENGTH + "개의 숫자로 이루어져야 합니다.";
+    public static final String DUPLICATE_LOTTO_NUMBER_ERROR_MESSAGE = ERROR_MESSAGE_PREFIX + "로또 번호는 중복되지 않아야 합니다.";
     public static final String INVALID_RANGE_ERROR_MESSAGE =
-            "[ERROR] 로또 번호는 " + VALID_MIN_RANGE + "부터 " + VALID_MAX_RANGE + "사이의 숫자여야 합니다";
-    public static final String INVALID_TYPE_ERROR_MESSAGE = "[ERROR] 로또 번호는 정수여야 합니다.";
-    public static final String INVALID_PAID_MONEY_MESSAGE = "[ERROR] 지불된 금액은 " + LOTTO_PRICE + "원 단위여야 합니다.";
-    public static final String DUPLICATE_BONUS_NUMBER_ERROR_MESSAGE = "[ERROR] 해당 보너스 번호는 이미 당첨 번호에 포함되어 있습니다.";
+            ERROR_MESSAGE_PREFIX + "로또 번호는 " + VALID_MIN_RANGE + "부터 " + VALID_MAX_RANGE + "사이의 숫자여야 합니다";
+    public static final String INVALID_TYPE_ERROR_MESSAGE = ERROR_MESSAGE_PREFIX + "로또 번호는 정수여야 합니다.";
+    public static final String INVALID_PAID_MONEY_ERROR_MESSAGE =
+            ERROR_MESSAGE_PREFIX + "지불된 금액은 " + LOTTO_PRICE + "원 단위여야 합니다.";
+    public static final String DUPLICATE_BONUS_NUMBER_ERROR_MESSAGE =
+            ERROR_MESSAGE_PREFIX + "해당 보너스 번호는 이미 당첨 번호에 포함되어 있습니다.";
 }
