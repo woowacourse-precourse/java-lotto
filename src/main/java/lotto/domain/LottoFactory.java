f58dafb901df176bd02fa7a030aa4e76bfec4efa
dev_kong <ahdjd5@gmail.com>
2022-11-12 23:11:13 +0900

    refactor(LottoFactory): split randomNumbers from new Lotto's parameter

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -1,19 +1,18 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.utils.LottoInfo;
 
 public class LottoFactory {
     public static Lotto generateLotto() {
-        return new Lotto(
-                Randoms.pickUniqueNumbersInRange(LottoInfo.START_NUMBER.getNumber(),
-                        LottoInfo.END_NUMBER.getNumber(),
-                        LottoInfo.LENGTH.getNumber()));
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(
+                LottoInfo.START_NUMBER.getNumber(),
+                LottoInfo.END_NUMBER.getNumber(),
+                LottoInfo.LENGTH.getNumber()
+        );
+
+        return new Lotto(randomNumbers);
     }
 }
