cc4019e9fd996c76d3c66e8c1dddabd4a1053a63
HamSeungChan <head0618@naver.com>
2022-11-12 14:41:47 +0900

    "feat : 1 ~ 45까지의 서로 다른 임이의 수 6개 생성 기능 구현"

---
+++ src/main/java/lotto/controller/LottoNumberManagement.java
@@ -0,0 +1,23 @@
+package lotto.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoNumberManagement {
+  private final int LOTTO_START_NUMBER = 1;
+  private final int LOTTO_END_NUMBER = 45;
+  private final int LOTTO_NUMBERS_SIZE = 6;
+
+  private List<Integer> numbers;
+
+  public LottoNumberManagement() {
+    numbers = new ArrayList<>();
+  }
+
+  public List<Integer> generateRandomNumbers() {
+    numbers = Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, LOTTO_NUMBERS_SIZE);
+    return numbers;
+  }
+}
