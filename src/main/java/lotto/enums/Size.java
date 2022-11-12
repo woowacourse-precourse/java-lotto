e38ea22256d1659489521b2377633b560a4b3a4b
leewoojin <zmfpdl64@naver.com>
2022-11-12 23:21:03 +0900

    Feat: 복권의 크기를 나타낼 Enum 형 생성

--- src/main/java/lotto/enums/Size.java
+++ src/main/java/lotto/enums/Size.java
@@ -1,2 +1,12 @@
-package lotto.enums;public enum Size {
+package lotto.enums;
+
+public enum Size {
+    LOTTERY_SIZE(6);
+    private  Integer size;
+    private Size(Integer size){
+        this.size = size;
+    }
+    public Integer getSize(){
+        return size;
+    }
 }
