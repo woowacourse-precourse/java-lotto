722cb9e334c92bb85be6eb434d31d578618e625f
TaeMin <xoals3094@naver.com>
2022-11-12 09:37:26 +0900

    refactor(MainController) : calculateWinning에서 초기화되는 point 해시를 생성자 코드로 옮깁니다

--- src/main/java/lotto/Controller/MainController.java
+++ src/main/java/lotto/Controller/MainController.java
@@ -15,9 +15,17 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MainController {
-    private Cash cash;
-    private List<Lotto> myLottos;
+    public MainController(){
+        point = new HashMap<Prize, Integer>();
+        point.put(Prize.FIRST, 0);
+        point.put(Prize.SECOND, 0);
+        point.put(Prize.THIRD, 0);
+        point.put(Prize.FOURTH, 0);
+        point.put(Prize.FIFTH, 0);
+        point.put(Prize.LAST, 0);
+    }
 
+    private Cash cash;
     public void inputCash() throws IllegalArgumentException {
         System.out.println("구입 금액을 입력해주세요.");
         String input = Console.readLine();
@@ -32,12 +40,12 @@ public class MainController {
     }
 
     private int lottoNumber;
-
     private int countLottoNumber(Cash cash) throws IllegalArgumentException {
         Calculator calculator = new Calculator();
         return calculator.countLottoNumber(cash);
     }
 
+    private List<Lotto> myLottos;
     public void createMyLottos() {
         LottoGenerator lottoGenerator = new LottoGenerator();
         myLottos = lottoGenerator.createMyLottos(lottoNumber);
@@ -46,7 +54,6 @@ public class MainController {
     }
 
     private Lotto winningLotto;
-
     public void inputWinningNumber() throws IllegalArgumentException {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
@@ -65,7 +72,6 @@ public class MainController {
     }
 
     private Bonus bonus;
-
     public void inputBonusNumber() throws IllegalArgumentException {
         System.out.println("보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
@@ -80,21 +86,14 @@ public class MainController {
         bonus = new Bonus(bonusNumber, winningLotto);
     }
 
+    HashMap<Prize, Integer> point;
     public void calculateWinning() {
         Judgment judgment = new Judgment();
         Calculator calculator = new Calculator();
         int total = 0;
-        HashMap<Prize, Integer> point = new HashMap<>();
-        point.put(Prize.FIRST, 0);
-        point.put(Prize.SECOND, 0);
-        point.put(Prize.THIRD, 0);
-        point.put(Prize.FOURTH, 0);
-        point.put(Prize.FIFTH, 0);
-        point.put(Prize.LAST, 0);
         for (Lotto myLotto : myLottos) {
             int match = judgment.compare(winningLotto, myLotto);
             boolean hasBonus = judgment.hasBonusNumber(myLotto, bonus);
-
             Prize prize = calculator.givePrize(match, hasBonus);
             total += prize.getPrize();
             int p = point.get(prize);
