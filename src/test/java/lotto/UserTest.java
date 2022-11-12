7d2be316ad289d2781910886514d2ff4021b83cb
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-13 02:31:33 +0900

    test(User/buyLotto): buyLotto 테스트 코드 작성

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class UserTest {
 
@@ -29,5 +31,21 @@ public class UserTest {
         validateMoneyMethod.setAccessible(true);
         boolean isValidate = (boolean) validateMoneyMethod.invoke(user, 12500);
         assertThat(isValidate).isEqualTo(false);
+    }
+
+    @DisplayName("입력된 숫자가 1000 단위인 경우에는 금액을 1000으로 나눈 몫만큼 로또를 구매한다.")
+    @Test
+    void buyLotto_정상() {
+        int money = 12000;
+        user.buyLotto(money);
+        assertThat(user.getLotto().length).isEqualTo(money / 1000);
+    }
+
+    @DisplayName("입력된 숫자가 1000 단위가 아닌 경우에는 IllegalArgumentException을 반환한다.")
+    @Test
+    void buyLotto_예외() {
+        int money = 12500;
+        assertThatThrownBy(() -> user.buyLotto(money))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
