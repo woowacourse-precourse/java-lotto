ede4c2715a61c2b9b8b894c43c3ae2f64f75e230
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-12 23:58:56 +0900

    feat: SortRandomLotto()-> 리스트를 받아 sort 시키고 다른 리스트에 넣어 반환

--- src/main/java/lotto/util/WinningNumber.java
+++ src/main/java/lotto/util/WinningNumber.java
@@ -11,4 +11,11 @@ public class WinningNumber {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return SortRandomLotto(numbers);
     }
+
+    private List<Integer> SortRandomLotto(List<Integer> numbers) {
+        List<Integer> sortNumber = new ArrayList<>();
+        sortNumber.addAll(numbers);
+        Collections.sort(sortNumber);
+        return sortNumber;
+    }
 }
