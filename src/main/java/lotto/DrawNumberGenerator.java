e5985792beb206a1e3ed6d3c485bec96548a5f59
Amyhds <dasomhan524@gmail.com>
2022-11-12 15:50:12 +0900

    feat(drawnumbergenerator): 정렬된 로또 번호 출력 기능 추가

--- src/main/java/lotto/DrawNumberGenerator.java
+++ src/main/java/lotto/DrawNumberGenerator.java
@@ -3,19 +3,23 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
+import java.util.Collections;
 
 public class DrawNumberGenerator {
     public static List<Lotto> Lottos;
 
     public void purchaseOutput() {
         int lottoCount = ConsoleInput.purchaseCount;
-        System.out.println(Integer.toString(lottoCount)+LottoMessage.PURCHASE_MESSAGE);
-        for (int count = 0;count<lottoCount;count++){
+        System.out.println(Integer.toString(lottoCount) + LottoMessage.PURCHASE_MESSAGE);
+        for (int count = 0; count < lottoCount; count++) {
             List<Integer> oneLottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(oneLottoNumbers);
             Lotto oneLotto = new Lotto(oneLottoNumbers);
             Lottos.add(oneLotto);
         }
+
+        for (int count = 0; count < lottoCount; count++) {
+            System.out.println(Lottos.get(count));
+        }
     }
-
-
 }
