b73d0a2211c74f6eb2be12be22a2b8b072a61460
5nam <ohnam90@gmail.com>
2022-11-12 14:36:00 +0900

    test(refactor): LottoBuyer 기능 사용하기 위한 생성자 코드 static final 로 변경

--- src/test/java/lotto/domain/LottoBuyerTest.java
+++ src/test/java/lotto/domain/LottoBuyerTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoBuyerTest {
-    LottoBuyer lottoBuyer = new LottoBuyer();
+    private static final LottoBuyer lottoBuyer = new LottoBuyer();
 
     @DisplayName("구매 금액 지불 시 1,000원 단위로 지불하지 않은 경우 예외가 발생한다.")
     @Test
