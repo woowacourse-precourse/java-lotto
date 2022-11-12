166393d99dec6d36fce7cf78fd7cb1bb5331bbec
ykmxxi <ykmxxi97@gmail.com>
2022-11-12 10:17:23 +0900

    fix: 당첨 번호와 보너스 번호 입력 정규식 수정

--- src/main/java/lotto/validation/RegEx.java
+++ src/main/java/lotto/validation/RegEx.java
@@ -3,8 +3,9 @@ package lotto.validation;
 public enum RegEx {
     NUMBER_REG_EX("^[0-9]*$"),
     ZERO_REG_EX("^0$"),
-    WINNING_INPUT_REG_EX("^([1-9],{1}|[1-4]{1}[0-5]{1},){5}([1-9]{1}|[1-4]{1}[1-5]{1}){1}$"),
-    BONUS_INPUT_REG_EX("^([1-9]{1}|[1-4]{1}[1-5]{1}){1}$");
+    WINNING_INPUT_REG_EX(
+            "^([1-9],{1}|[1-3]{1}[0-9]{1}|[4]{1}[0-5]{1},){5}([1-9]{1}|[1-3]{1}[0-9]{1}|[4]{1}[0-5]{1}){1}$"),
+    BONUS_INPUT_REG_EX("^([1-9]{1}|[1-3]{1}[0-9]{1}|[4]{1}[0-5]{1}){1}$");
 
     private final String regEx;
 
