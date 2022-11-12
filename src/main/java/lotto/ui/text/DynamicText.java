bda821063176f598308e911eed1b64ade61894ec
limjae <limjae96@gmail.com>
2022-11-12 23:19:05 +0900

    fix: 모든 로또 통계 출력 오류 수정

--- src/main/java/lotto/ui/text/DynamicText.java
+++ src/main/java/lotto/ui/text/DynamicText.java
@@ -22,7 +22,7 @@ public class DynamicText {
         return String.format("당첨 통계\n" + "---\n" + "3개 일치 (5,000원) - %d개\n"
                         + "4개 일치 (50,000원) - %d개\n" + "5개 일치 (1,500,000원) - %d개\n"
                         + "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n" + "6개 일치 (2,000,000,000원) - %d개\n"
-                        + "총 수익률은 %.2f%%입니다.",
+                        + "총 수익률은 %.1f%%입니다.",
                 statistics.get(RewardEnum.WIN_3), statistics.get(RewardEnum.WIN_4),
                 statistics.get(RewardEnum.WIN_5), statistics.get(RewardEnum.WIN_5_BONUS),
                 statistics.get(RewardEnum.WIN_6), 100.00 * earn / money
