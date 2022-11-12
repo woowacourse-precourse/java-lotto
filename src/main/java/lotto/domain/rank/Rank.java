30c4df935458e4cafeff39260dd80cc24eb3ca64
shinD <huipulci1@naver.com>
2022-11-12 19:55:04 +0900

    feat: 일치 번호에 따른 등수 계산과 등수에 따른 당첨 금액 구현
    - 6개의 번호가 모두 일치하면 1등이다.
    - 5개의 번호가 일치하고 보너스 번호가 일치하면 2등이다.
    - 5개의 번호가 일치하면 3등이다.
    - 4개의 번호가 일치하면 4등이다.
    - 3개의 번호가 일치하면 5등이다.
    
    - 1등은 2,000,000,000 원을 받는다.
    - 2등은 30,000,000 원을 받는다.
    - 3등은 1,500,000 원을 받는다.
    - 4등은 50,000 원을 받는다.
    - 5등은 5,000 원을 받는다.

---
+++ src/main/java/lotto/domain/rank/Rank.java
@@ -0,0 +1,34 @@
+package lotto.domain.rank;
+
+import java.util.Arrays;
+import java.util.function.BiPredicate;
+
+public enum Rank {
+
+    FIRST(2_000_000_000, (matchCount, matchBonus) -> matchCount == 6),
+    SECOND(30_000_000, (matchCount, matchBonus) -> matchCount == 5 && matchBonus),
+    THIRD(1_500_000, (matchCount, matchBonus) -> matchCount == 5),
+    FOURTH(50_000, (matchCount, matchBonus) -> matchCount == 4),
+    FIFTH(5_000, (matchCount, matchBonus) -> matchCount == 3),
+    UNWINNABLE(0, (matchCount, matchBonus) -> matchCount < 3),
+    ;
+
+    private final int reward;
+    private final BiPredicate<Integer, Boolean> match;
+
+    Rank(int reward, BiPredicate<Integer, Boolean> match) {
+        this.reward = reward;
+        this.match = match;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public static Rank matchRank(int matchCount, boolean matchBonus) {
+        return Arrays.stream(Rank.values())
+                .filter(r -> r.match.test(matchCount, matchBonus))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("matchCount는 6 이하의 숫자여야 합니다."));
+    }
+}
