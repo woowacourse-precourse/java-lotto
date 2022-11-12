052adda5a7416c0c51746d754b2e175881ccf24d
kanghyki <kanghyki@gmail.com>
2022-11-13 01:28:52 +0900

    fix(LottoInStream): 불필요한 매개변수 삭제

--- src/main/java/lotto/LottoInStream.java
+++ src/main/java/lotto/LottoInStream.java
@@ -13,7 +13,7 @@ public class LottoInStream {
     private static final String _input_numbers_str = "당첨 번호를 입력해 주세요.\n";
     private static final String _input_bonus_number_str = "보너스 번호를 입력해 주세요.\n";
 
-    public static Integer readToBuyLotto(Integer money) {
+    public static Integer readToBuyLotto() {
         System.out.print(_input_buy_str);
         String line = readline();
 
