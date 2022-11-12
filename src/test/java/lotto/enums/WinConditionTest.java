8e0f32e436b63f45e4b1e4f63e0dd4255b89e05b
leewoojin <zmfpdl64@naver.com>
2022-11-12 23:48:10 +0900

    Test: WinCondition 로또 맞은 갯수와 보너스 갯수에 따라 결과 확인

--- src/test/java/lotto/enums/WinConditionTest.java
+++ src/test/java/lotto/enums/WinConditionTest.java
@@ -1,4 +1,20 @@
+package lotto.enums;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class WinConditionTest {
-  
+
+    @Test
+    void getIndex() {
+        int index = WinCondition.getIndex(6, 0);
+        assertThat(index).isEqualTo(4);
+    }
+    @Test
+    void getIndex2() {
+        int index = WinCondition.getIndex(2, 0);
+        assertThat(index).isEqualTo(-1);
+    }
 }
