1c166baa2f5b8c37724b9ce639af8838154012bf
nswon <swnam0729@gmail.com>
2022-11-12 18:50:03 +0900

    feat: 구매자 객체 구현

---
+++ src/main/java/lotto/domain/Buyer.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Buyer {
+    private final List<Lotto> lottos;
+
+    public Buyer() {
+        lottos = new ArrayList<>();
+    }
+
+    public String inputLottoAmount() {
+        return Console.readLine();
+    }
+
+    public void buyLottos() {
+
+    }
+}
