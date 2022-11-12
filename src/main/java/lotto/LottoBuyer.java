d20d3161176cc760597cc7ed19d629c09d3db13a
JuHyeokParkk <hyeok5596@naver.com>
2022-11-13 03:15:06 +0900

    feat: 로또 구매자와 관련된 LottoBuyer 클래스 구현

---
+++ src/main/java/lotto/LottoBuyer.java
@@ -0,0 +1,16 @@
+package lotto;
+
+public class LottoBuyer {
+
+    private LottoPurchaseInfo lottoPurchaseInfo;
+    private LottoPrizeRecordingManager lottoPrizeRecordingManager;
+    private LottoSeller lottoSeller;
+    private PrintController printController;
+
+    public LottoBuyer() {
+        lottoSeller = new LottoSeller();
+        lottoPrizeRecordingManager = new LottoPrizeRecordingManager();
+        printController = new PrintController();
+    }
+
+}
