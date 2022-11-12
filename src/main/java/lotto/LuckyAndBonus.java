f2e7a47e786b21d244916c52f99df213f7c8c408
Amyhds <dasomhan524@gmail.com>
2022-11-12 14:13:05 +0900

    feat(luckyandbonus): 당첨번호와 보너스 번호 클래스 생성

---
+++ src/main/java/lotto/LuckyAndBonus.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import java.util.List;
+
+public class LuckyAndBonus {
+    public List<Integer> luckySix;
+    public int bonusNumber;
+
+    public LuckyAndBonus(List<Integer> luckySix, int bonusNumber) {
+
+    }
+}
