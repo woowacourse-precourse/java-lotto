62c0ca19c50b68172aa5436e54bff6df4404068a
minje <alswp0910@naver.com>
2022-11-12 17:38:28 +0900

    feat: 입력 받은 금액으로 발행해야 할 로또의 개수를구하는 기능 추가

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.Exceptions;
 
 public class User {
-    public int inputPrice(){
+    public int inputPrice() {
         System.out.println("구입금액을 입력해 주세요.");
         String price = Console.readLine();
         boolean chkPrice = price.matches("-?\\d+");
@@ -12,5 +12,14 @@ public class User {
             new Exceptions();
         }
         return Integer.parseInt(price);
+    }
+
+    public int countLotto(int price) {
+        int countLotto = 0;
+        if (price % 1000 > 0) {
+            new Exceptions();
+        }
+        countLotto = price / 1000;
+        return countLotto;
     }
 }
---
+++ src/test/java/lotto/domain/UserTest.java
@@ -0,0 +1,6 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+}
