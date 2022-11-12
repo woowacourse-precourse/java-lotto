ee6313dc5555ee5fd57040b0cdca3e235935c64d
KangSungBum <peter9706@naver.com>
2022-11-12 18:01:55 +0900

    feat(ErrorResponse.java): 에러 메세지 추가
    bonus nuber 범위 검증 에러 enum value 추가
    - INPUT_BOUNS_NUMBER_RANGE_ERROR 에러 메세지 추가

--- src/main/java/lotto/ErrorResponse.java
+++ src/main/java/lotto/ErrorResponse.java
@@ -2,6 +2,7 @@ package lotto;
 
 public enum ErrorResponse {
     INPUT_LOTTO_RANGE_ERROR("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INPUT_BONUS_NUMBER_RANGE_ERROR("[Error] 보너스 번호는 1부터 45 사이의 숫자여야 합니다."),
     INPUT_WRONG_SIZE_ERROR("[ERROR] 로또 당첨 번호느 6개의 수여야 합니다."),
     INPUT_BUYING_RANGE_ERROR("[ERROR] 1000으로 나누어지는 단위의 구입 금액을 입력해야 합니다."),
     INPUT_WITHOUT_COMMA_ERROR("[ERROR] 당첨번호는 쉽표(,)로 구분하여 입력해야 합니다."),
