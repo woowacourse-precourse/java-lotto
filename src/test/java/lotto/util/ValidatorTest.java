dc152310f0f5c804aa05977553aae55443b67945
Devtist <yt930310@snu.ac.kr>
2022-11-13 02:45:23 +0900

    test: 구입 금액이 로또 한장의 가격으로 나누어 떨어지는지를 검증하는 테스트코드 작성

--- src/test/java/lotto/util/ValidatorTest.java
+++ src/test/java/lotto/util/ValidatorTest.java
@@ -15,7 +15,19 @@ class ValidatorTest {
 
 	@Test
 	@DisplayName("정수가 아닌 입력이 들어오면 예외가 발생한다.")
-	void validate() {
+	void validateNotIntergerInput() {
 		assertThatThrownBy(() -> Validator.validateIntegerOrNot("abc")).isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("1000원 단위가 아닌 구입 금액이 입력되면 예외가 발생한다.")
+	void validateNotDivisibleInput() {
+		assertThatThrownBy(() -> Validator.validateDivisibility(242221)).isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("1000원 단위의 입력인지 검증한다.")
+	void validateDivisibleInput() {
+		assertThatNoException().isThrownBy(() -> Validator.validateDivisibility(4214000));
 	}
 }
