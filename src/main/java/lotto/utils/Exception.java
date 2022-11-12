3c4840dcea358026aaf667a3e725472e390b94b9
alivejuicy <kkddhh1998@gmail.com>
2022-11-12 23:45:47 +0900

    Feat : Exception - 에러코드로 분리하여 관리

---
+++ src/main/java/lotto/utils/Exception.java
@@ -0,0 +1,10 @@
+package lotto.utils;
+
+public class Exception {
+    public static final String errorCode = "[ERROR] 잘못된 금액 입력입니다.";
+
+    public static String catchException(){
+        return errorCode;
+    }
+
+}
