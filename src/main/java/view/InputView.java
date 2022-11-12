800e75ebbb15d89356a12d96eaa67ef5d445b9d5
tenta <eunoia3802@gmail.com>
2022-11-12 19:11:07 +0900

    feat: 보너스 번호 입력 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class InputView {
     Scanner scanner;
     private final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public int inputMoney() {
         scanner = new Scanner(System.in);
@@ -17,5 +18,12 @@ public class InputView {
         scanner = new Scanner(System.in);
         String winningNumber = scanner.nextLine();
         return winningNumber;
+    }
+
+    public int inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER);
+        scanner = new Scanner(System.in);
+        int bonusNumber = scanner.nextInt();
+        return bonusNumber;
     }
 }
