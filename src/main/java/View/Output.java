a1de5fd9a04bfe1f6d88720ccbd733ad81831847
swonny <wonny921@gmail.com>
2022-11-13 08:59:06 +0900

    feat(Output): 구매 금액 입력 메시지 출력 기능 구현

--- src/main/java/View/Output.java
+++ src/main/java/View/Output.java
@@ -1,7 +1,25 @@
 package View;
 
-public class Messages {
-    public void getPurchaseAmount() {
-        System.out.println();
+import enumCollections.GuideMessage;
+
+public class Output {
+    public static void getPurchaseAmount() {
+        getGuideMessage(GuideMessage.PURCHASE_INPUT);
+    }
+
+    public static void getNumberOfPurchase(int number) {
+        getGuideMessage(GuideMessage.NUMBER_OF_PURCHASE, number);
+    }
+
+    public static void getWinningNumbers() {
+        getGuideMessage(GuideMessage.WINNING_NUMBERS_INPUT);
+    }
+
+    public static void getGuideMessage(GuideMessage message) {
+        System.out.println(message);
+    }
+
+    public static void getGuideMessage(GuideMessage message, int value) {
+        System.out.println(Integer.toString(value) + message);
     }
 }
