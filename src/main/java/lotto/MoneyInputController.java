ee5bb89326e3f7820e3b0391f381dd87c71faf22
JuHyeokParkk <hyeok5596@naver.com>
2022-11-13 02:43:37 +0900

    feat: 입력한 구입 금액을 검증하는 클래스 구현

---
+++ src/main/java/lotto/MoneyInputController.java
@@ -0,0 +1,19 @@
+package lotto;
+
+public class MoneyInputController extends InputController {
+
+    public void validate(Object input) {
+
+        long price;
+
+        try {
+            price = Long.parseLong((String) input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 올바른 숫자 입력이 아니거나, 너무 큰 수입니다.");
+        }
+
+        if (price == 0 || price % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위만 입력 가능합니다.");
+        }
+    }
+}
