e1c6a707915b8c6416e61ded2c95829234e8fc9f
kanghyki <kanghyki@gmail.com>
2022-11-12 20:45:27 +0900

    feat(LottoInStream): public 메서드 프로토 타입 선언

--- src/main/java/lotto/LottoInStream.java
+++ src/main/java/lotto/LottoInStream.java
@@ -1,8 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class LottoInStream {
     private static final String _input_buy_str = "구입금액을 입력해 주세요.\n";
     private static final String _input_numbers_str = "당첨 번호를 입력해 주세요.\n";
     private static final String _input_bonus_number_str = "보너스 번호를 입력해 주세요.\n";
+
+    public static Integer readToBuyLotto(Integer money) {
+
+    }
+    public static List<Integer> readToGetLottoNumbers() {
+
+    }
 
 }
