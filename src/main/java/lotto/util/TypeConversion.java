eb4970d9ca7cf4a71105af386ca5b9def037c49e
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-12 23:46:16 +0900

    feat(TypeConversion): 당첨 번호가 숫자인지 확인하는 기능 구현

--- src/main/java/lotto/util/TypeConversion.java
+++ src/main/java/lotto/util/TypeConversion.java
@@ -1,8 +1,13 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TypeConversion {
 
     private static final String PURCHASE_AMOUNT_TYPE_ERROR_MESSAGE = "[ERROR] 구입금액은 숫자여야 합니다.";
+    private static final String SPLIT_EXPRESSION = ",";
 
     public static int stringToInt(String input) {
 
@@ -14,5 +19,20 @@ public class TypeConversion {
         }
 
         return purchaseAmount;
+    }
+
+    public static List<String> splitInput(String input) {
+        String[] separatedInput = input.split(SPLIT_EXPRESSION);
+
+        return Arrays.asList(separatedInput);
+    }
+
+    public static List<Integer> stringListToIntList(String input) {
+
+        List<String> playerNumbers = splitInput(input);
+
+        return playerNumbers.stream()
+                .map(TypeConversion::stringToInt)
+                .collect(Collectors.toList());
     }
 }
