6804b060672c2f4f2c9bf4e90a33d86969c73c49
kimJaeHyeon <kmr2644@gmail.com>
2022-11-12 15:01:39 +0900

    Prize list 와 수익률을 가지는 PrizeDto 구현

--- src/main/java/lotto/view/PrizeDto.java
+++ src/main/java/lotto/view/PrizeDto.java
@@ -1,2 +1,27 @@
-package lotto.view;public class PrizeDto {
+package lotto.view;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+import java.util.List;
+import java.util.Map;
+import lotto.type.Prize;
+
+public class PrizeDto {
+    private List<Prize> prizes;
+    private Double yield;
+
+    public PrizeDto(List<Prize> prizes, Double yield) {
+        this.prizes = prizes;
+        this.yield = yield;
+    }
+
+    public Map<Prize, Long> groupByPrizes() {
+        return prizes.stream()
+                .collect(groupingBy(prize -> prize, counting()));
+    }
+
+    public Double getYield() {
+        return yield;
+    }
 }
