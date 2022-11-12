62e72fd8d52848c4b0b62152295e5d378c7dffbc
yoonjuho <dbswn990@naver.com>
2022-11-13 06:12:16 +0900

    test(LottoTest) : 당첨번호와 입력번호 중복 개수 비교

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -27,6 +27,14 @@ class LottoTest {
     void createLottoByOutofRange() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 55, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨번호와 입력번호를 비교한다")
+    @Test
+    void compareLottoNumber() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int result = 3;
+        assertThat(result).isEqualTo(lotto.compare(List.of(1, 3, 4, 8, 10, 32)));
     }
 
 
