55ca8103e7c64404d3c9f1b5aca8e4d11b356bf3
Ilchul Kim <huj0159@gmail.com>
2022-11-12 18:59:47 +0900

    feat: check user input fits condition

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,2 +1,18 @@
-package lotto;public class LottoController {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Pattern;
+
+public class LottoController {
+
+    public void readUserMoney(String userMoney) {
+        validate(userMoney);
+    }
+
+    private void validate(String userInput) {
+        if (Integer.parseInt(userInput) % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000 단위의 금액을 입력해야합니다");
+        }
+    }
 }
