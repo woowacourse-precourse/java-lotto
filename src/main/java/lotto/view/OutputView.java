77497a6d73a91eaf7b032ef16f46936bfcbd8829
YuHayeon <youha0930@gmail.com>
2022-11-13 02:19:59 +0900

    feat(OutputView): 당첨 내역 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,15 @@
 package lotto.view;
 
 import lotto.domain.Lottos;
+import lotto.domain.WinningResult;
 
 public class OutputView {
 
     public static void lottoPriceOutput(Lottos lottos) {
         System.out.println("\n" + lottos);
+    }
+
+    public static void winningNumberOutput(WinningResult winning) {
+        System.out.println("\n" + winning);
     }
 }
