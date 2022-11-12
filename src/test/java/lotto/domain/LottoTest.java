ddbde0e40b2c74aacae07c1e775196c065ac330f
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-12 21:35:34 +0900

    test: 로또 번호가 1~45를 벗어난 값 예외처리

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,4 +24,11 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호의 범위가 1~45가 아니면 예외가 발생한다.")
+    @Test
+    void createLottoByOverLength() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 50)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
