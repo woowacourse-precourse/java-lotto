511cd05322473022e1b08a6354a842aebdbfec29
sosow0212 <sosow0212@naver.com>
2022-11-12 12:36:52 +0900

    style : LottoCalculateService 줄 맞춤 수정

--- src/main/java/lotto/service/LottoCalculateService.java
+++ src/main/java/lotto/service/LottoCalculateService.java
@@ -15,21 +15,19 @@ public class LottoCalculateService {
     private static final int PRICE_OF_FIFTH = 1500000;
     private static final int PRICE_OF_FOURTH = 50000;
     private static final int PRICE_OF_THIRD = 5000;
-    private final OutputView outputView = new OutputView();
+    private static final OutputView outputView = new OutputView();
     private Map<Winning, Integer> countOfWinning = new HashMap<>();
 
     public void calculateWinning(User user, Lotto lotto) {
         countLottoWinningCount(user, lotto);
         outputView.responseWinningHistory(countOfWinning);
-        double yieldOfLotto = getYieldOfLotto(user, user.getWinningPrice());
-        outputView.responseYieldOfLotto(yieldOfLotto);
+        outputView.responseYieldOfLotto(getYieldOfLotto(user, user.getWinningPrice()));
     }
 
     public double getYieldOfLotto(User user, int totalPrice) {
         double yield = ((double) totalPrice / user.getBuyingPrice() * 100.0);
         return yield;
     }
-
 
     public void countLottoWinningCount(User user, Lotto lotto) {
         List<UserLotto> userLottos = user.getLottos();
@@ -44,39 +42,39 @@ public class LottoCalculateService {
 
     public void countEqualsSix(User user, List<Integer> numbers, Lotto lotto) {
         if (numbers.equals(lotto.getNumbers())) {
-            inputWinning(Winning.SIXTH);
+            inputWinningLotto(Winning.SIXTH);
             user.addWinningPrice(PRICE_OF_SIXTH);
         }
     }
 
     public void countEqualsFive(User user, List<Integer> numbers, Lotto lotto) {
-        if (containsNumCount(numbers, lotto.getNumbers()) == 5 && numbers.contains(lotto.getBonusNumber())) {
-            inputWinning(Winning.FIFTH_WITH_BONUS);
+        if (countUserNumbersContainLotto(numbers, lotto.getNumbers()) == 5 && numbers.contains(lotto.getBonusNumber())) {
+            inputWinningLotto(Winning.FIFTH_WITH_BONUS);
             user.addWinningPrice(PRICE_OF_FIFTH_WITH_BONUS);
             return;
         }
 
-        if (containsNumCount(numbers, lotto.getNumbers()) == 5) {
-            inputWinning(Winning.FIFTH);
+        if (countUserNumbersContainLotto(numbers, lotto.getNumbers()) == 5) {
+            inputWinningLotto(Winning.FIFTH);
             user.addWinningPrice(PRICE_OF_FIFTH);
         }
     }
 
     public void countEqualsFour(User user, List<Integer> numbers, Lotto lotto) {
-        if (containsNumCount(numbers, lotto.getNumbers()) == 4) {
-            inputWinning(Winning.FOURTH);
+        if (countUserNumbersContainLotto(numbers, lotto.getNumbers()) == 4) {
+            inputWinningLotto(Winning.FOURTH);
             user.addWinningPrice(PRICE_OF_FOURTH);
         }
     }
 
     public void countEqualsThird(User user, List<Integer> numbers, Lotto lotto) {
-        if (containsNumCount(numbers, lotto.getNumbers()) == 3) {
-            inputWinning(Winning.THIRD);
+        if (countUserNumbersContainLotto(numbers, lotto.getNumbers()) == 3) {
+            inputWinningLotto(Winning.THIRD);
             user.addWinningPrice(PRICE_OF_THIRD);
         }
     }
 
-    public int containsNumCount(List<Integer> userNumbers, List<Integer> lottoNumbers) {
+    public int countUserNumbersContainLotto(List<Integer> userNumbers, List<Integer> lottoNumbers) {
         int count = 0;
         for (int number : userNumbers) {
             if (lottoNumbers.contains(number)) {
@@ -86,7 +84,7 @@ public class LottoCalculateService {
         return count;
     }
 
-    public void inputWinning(Winning winning) {
+    public void inputWinningLotto(Winning winning) {
         countOfWinning.put(winning, countOfWinning.getOrDefault(winning, 0) + 1);
     }
 }
