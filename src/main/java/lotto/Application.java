2711aa84665865f1c90a4b101be5d76cb2ab28fd
hectick <chyeon1223@cau.ac.kr>
2022-11-12 17:05:20 +0900

    feat: 예외메세지를 출력하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,10 @@ import lotto.controller.Controller;
 public class Application {
     public static void main(String[] args) {
         Controller lottoController = new Controller();
-        lottoController.execute();
+        try{
+            lottoController.execute();
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
