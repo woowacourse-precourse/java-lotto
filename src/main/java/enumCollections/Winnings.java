718f5ca1b1f0ad12a7809474f354baf86c49617f
swonny <wonny921@gmail.com>
2022-11-13 00:08:52 +0900

    refactor(Checker): 당첨 금액을 저장하는 winnings 해시맵 초기화 메소드 제거
    Enum을 사용하여 당첨 금액을 저장할 수 있기 때문에 winnings 해시맵은 불필요하여 제거하였다.

---
+++ src/main/java/enumCollections/Winnings.java
@@ -0,0 +1,2 @@
+package enumCollections;public enum Winnings {
+}
--- src/main/java/lotto/Checker.java
+++ src/main/java/lotto/Checker.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import enumCollections.PrizeRank;
+import enumCollections.RankNumber;
+import enumCollections.Winnings;
+
 import java.util.HashMap;
 import java.util.List;
 
@@ -50,60 +54,16 @@ public class Checker extends Kiosk {
         return (getTotalWinnings() / payment) * 100;
     }
 
+    public static float getRoundedYield(float yield) {
+        return Math.round(yield * 100) / 100.0f;
+    }
+
     public static int getTotalWinnings() {
         int totalWinnings = 0;
-        HashMap<PrizeRank, Integer> winnings = initializeWinnings();
         for (PrizeRank rank : PrizeRank.values()) {
             totalWinnings += resultStatistics[rank.ordinal()] * Winnings.getAmount(rank.name());
         }
         return totalWinnings;
     }
 
-    public static HashMap<PrizeRank, Integer> initializeWinnings() {
-        HashMap<PrizeRank, Integer> winnings = new HashMap<>();
-        for (PrizeRank rank : PrizeRank.values()) {
-            winnings.put(rank, Winnings.getAmount(rank.name()));
-        }
-        return winnings;
-    }
-}
-
-enum Winnings {
-    FIRST(2000000000),
-    SECOND(30000000),
-    THIRD(1500000),
-    FOURTH(50000),
-    FIFTH(5000);
-
-    private final int amount;
-
-    Winnings(int amount) {
-        this.amount = amount;
-    }
-
-    static public int getAmount(String rank) {
-        return Winnings.valueOf(rank).amount;
-    }
-}
-
-enum PrizeRank {
-    FIRST(0),
-    SECOND(1),
-    THIRD(2),
-    FOURTH(3),
-    FIFTH(4);
-
-    private final int ranks;
-
-    PrizeRank(int ranks) {
-        this.ranks = ranks;
-    }
-
-    public static PrizeRank getPrizeIndex(String prizeRank) {
-        return PrizeRank.valueOf(prizeRank);
-    }
-
-    public static int getPrizeIndex(PrizeRank prizeRank) {
-        return prizeRank.ordinal();
-    }
 }
