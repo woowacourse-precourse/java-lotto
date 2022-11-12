6dd209621b990b255e3ec6b42a653e376faf5f2e
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-12 22:04:49 +0900

    feat(view): 당첨 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,10 +4,18 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
     private static final String AMOUNT_PHRASE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_PHRASE = "당첨 번호를 입력해 주세요.";
 
     public static String amount() {
         System.out.println(AMOUNT_PHRASE);
         String input = readLine();
         return input;
     }
+
+    public static String winningNumber() {
+        System.out.println(WINNING_NUMBER_PHRASE);
+        String input = readLine();
+        return input;
+    }
+
 }
