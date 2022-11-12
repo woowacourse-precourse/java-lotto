33985f80555be091b7aca21452b570542f62e3e5
fakerdeft <whakswp123@naver.com>
2022-11-13 03:13:03 +0900

    feat(inputCash): add 구매 수량 출력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,14 +2,19 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Cash;
+import lotto.domain.LottoMachine;
+
 
 public class Application {
     public static void main(String[] args) {
-        inputCash();
+        LottoMachine.pickLottoNumbers(inputCash());
     }
-    public static void inputCash(){
+    public static int inputCash(){
+        int countLotto;
         System.out.println("구입금액을 입력해 주세요.");
-        System.out.println(Cash.countPurchaseQuantity(Console.readLine()));
+        countLotto = Cash.countPurchaseQuantity(Console.readLine());
+        System.out.println(countLotto+"개를 구매했습니다.");
+        return countLotto;
     }
 
 }
