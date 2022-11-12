8507aff8182d0bbbaef86708d100df029591620c
woosung1223 <dntjd991223@naver.com>
2022-11-13 01:17:20 +0900

    refactor(OutputView): 결과를 출력하도록 함수 내부 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,14 +1,34 @@
 package lotto.view;
 
+import lotto.domain.Award;
+
+import java.text.DecimalFormat;
+import java.util.Collections;
+import java.util.List;
+
 public final class OutputView {
     private OutputView() {
+    }
+
+    public static void printReward(List<Award> awards) {
+        printIOMessage("3개 일치 (5,000원) - " + Collections.frequency(awards, Award.THREE) + "개");
+        printIOMessage("4개 일치 (50,000원) - " + Collections.frequency(awards, Award.FOUR) + "개");
+        printIOMessage("5개 일치 (1,500,000원) - " + Collections.frequency(awards, Award.FIVE) + "개");
+        printIOMessage("5개 일치, 보너스 볼 일치 (30,000,000원) - " + Collections.frequency(awards, Award.FIVE_BONUS) + "개");
+        printIOMessage("6개 일치 (2,000,000,000원) - " + Collections.frequency(awards, Award.SIX) + "개");
+    }
+
+    public static void printRate(float rate) {
+        DecimalFormat formatter = new DecimalFormat("###,###.#");
+
+        String roundedRate = String.format("%.1f", rate);
+        String formattedRate = formatter.format(Float.parseFloat(roundedRate));
+
+        printIOMessage("총 수익률은 " + formattedRate + "%입니다.");
     }
 
     public static void printIOMessage(String message) {
         System.out.println(message);
     }
 
-    public static void printRate(float rate) {
-//        String forOutput = "총 수익률은 " + Float.
-    }
 }
