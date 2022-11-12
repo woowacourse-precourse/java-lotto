d666ed3fd90827ad9c62cf81e60ea8c8a4bcc756
5nam <ohnam90@gmail.com>
2022-11-12 15:21:27 +0900

    feat: 입력한 로또 번호에 중복된 숫자가 있을 경우 예외 처리하는 기능 추가

--- src/main/java/lotto/domain/LottoDesignator.java
+++ src/main/java/lotto/domain/LottoDesignator.java
@@ -1,7 +1,10 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottoDesignator {
     private static final String INT_REGEX = "^[0-9]*$";
@@ -20,6 +23,18 @@ public class LottoDesignator {
         String[] lotto = removeComma(input);
         if(lotto.length != CIPHER) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개를 입력해야 합니다.");
+        }
+    }
+
+    public void checkDuplication(String input) {
+        Integer[] numbers = Stream.of(removeComma(input))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .toArray(Integer[]::new);
+        List<Integer> lotto = Arrays.asList(numbers);
+        HashSet<Integer> removeDuplication = new HashSet<>(lotto);
+        if(numbers.length != removeDuplication.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 
