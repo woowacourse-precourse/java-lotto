99583a95e1007d22bbf163141c6bb738655e04e5
gomljo <gomljo0307@naver.com>
2022-11-13 03:56:49 +0900

    feat(RaffleNumber.java): 생성된 추첨 번호가 6개가 아닌지 확인 하는 기능 구현

--- src/main/java/lotto/model/RaffleNumber.java
+++ src/main/java/lotto/model/RaffleNumber.java
@@ -4,8 +4,15 @@ import java.util.List;
 
 public class RaffleNumber {
     private final List<Integer> raffleNumbers;
-
-    public RaffleNumber(List<Integer> numbers) {
-        this.raffleNumbers = numbers;
+    private static final int PROPER_QUANTITY=6;
+    private static final String WRONG_QUANTITY_ERROR_MESSAGE="[ERROR] 추첨 번호가 6개가 아닙니다. 프로그램을 종료합니다.";
+    public RaffleNumber(List<Integer> raffleNumbers) {
+        validateProperQuantity(raffleNumbers);
+        this.raffleNumbers = raffleNumbers;
+    }
+    private void validateProperQuantity(List<Integer> raffleNumbers) {
+        if (raffleNumbers.size() != PROPER_QUANTITY) {
+            throw new IllegalArgumentException(WRONG_QUANTITY_ERROR_MESSAGE);
+        }
     }
 }
