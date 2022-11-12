bddb3d8dd1fa4dfdf303ef5ad63fe9cb3f2eaabf
Amyhds <dasomhan524@gmail.com>
2022-11-12 14:28:08 +0900

    feat(luckyandbonus): 당첨번호와 보너스 번호 중복 확인 메서드 구현

--- src/main/java/lotto/LuckyAndBonus.java
+++ src/main/java/lotto/LuckyAndBonus.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LuckyAndBonus {
@@ -7,6 +8,12 @@ public class LuckyAndBonus {
     public int bonusNumber;
 
     public LuckyAndBonus(List<Integer> luckySix, int bonusNumber) {
-
+        List<Integer> tempNumbers = new ArrayList<>(luckySix);
+        tempNumbers.add(bonusNumber);
+        if (tempNumbers.size() != tempNumbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(LottoMessage.LUCKY_DUPLICATE_ERROR_MESSAGE.getMessage());
+        }
+        this.luckySix = luckySix;
+        this.bonusNumber = bonusNumber;
     }
 }
