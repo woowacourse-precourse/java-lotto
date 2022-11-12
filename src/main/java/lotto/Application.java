d5f453ed3e2bdc74bfb3a974ca4fb365d788bdd3
HyunggiPark <phg9587@naver.com>
2022-11-12 15:35:27 +0900

    refact(Application): 로또 구입 금액 입력 메서드의 내용을 추가적으로 메서드 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -22,6 +22,21 @@ public class Application {
     }
 
     private static void validateUserInput(String userInput) {
+        String verifiedUserInput = verifyUserInputIsDigit(userInput);
+        verifyUserInputIsMultipleOf1000(verifiedUserInput);
+    }
+
+    private static void verifyUserInputIsMultipleOf1000(String verifiedUserInput) {
+        try {
+            if (Integer.parseInt(verifiedUserInput) % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력을 해주세요.");
+            }
+        }catch (NumberFormatException e) {
+            throw new NumberFormatException("[ERROR] 2^31-1 까지의 범위만 입력이 가능합니다.");
+        }
+    }
+
+    private static String verifyUserInputIsDigit(String userInput) {
         for (int idx = 0; idx < userInput.length(); idx++) {
             int eachNum = userInput.charAt(idx);
 
@@ -30,12 +45,6 @@ public class Application {
             }
         }
 
-        try {
-            if (Integer.parseInt(userInput) % 1000 != 0) {
-                throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력을 해주세요.");
-            }
-        }catch (NumberFormatException e) {
-            throw new NumberFormatException("[ERROR] 2^31-1 까지의 범위만 입력이 가능합니다.");
-        }
+        return userInput;
     }
 }
