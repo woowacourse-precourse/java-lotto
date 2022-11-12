7c20d15138c946866f0fbba2b2676596ef77535d
kimminsu <xjfcnfw3@naver.com>
2022-11-12 19:40:39 +0900

    test: 로또 수익률 출력 테스트

--- src/test/java/lotto/result/LottoResultTest.java
+++ src/test/java/lotto/result/LottoResultTest.java
@@ -25,13 +25,28 @@ public class LottoResultTest {
     @Test
     void showRankStats() {
         // given
-        List<Integer> ranks = List.of(5,1,0,0,0,1);
-        List<String> answerMessage = List.of(Rank.ONE+"1", Rank.FIVE+"1");
+        List<Integer> ranks = List.of(5, 1, 0, 0, 0, 1);
+        List<String> answerMessage = List.of(Rank.ONE + "1", Rank.FIVE + "1");
 
         // when
         LottoResult.showResult(ranks, 0);
 
         // then
         assertThat(out.toString()).contains(answerMessage);
+    }
+
+    @DisplayName("수익률 출력 테스트")
+    @Test
+    void showResultYield() {
+        // given
+        List<Integer> ranks = List.of(7, 0, 0, 0, 0, 1);
+        int lottoPrice = 8000;
+        String answer = "총 수익률은 62.5%입니다.";
+
+        // when
+        LottoResult.showResult(ranks, lottoPrice);
+
+        // then
+        assertThat(out.toString()).contains(answer);
     }
 }
