1e9110992a3191c12c0eccbb7c949cd8d6538d27
juhwang <juhong4930@naver.com>
2022-11-12 17:35:57 +0900

    feat(Application): do lotto process

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import lotto.user.LottoConsole;
+
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            LottoConsole.doLotto();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
+
 }
