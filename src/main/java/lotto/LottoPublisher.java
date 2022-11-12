f357cc78cd38cffb0fdf7802a98d592474e83212
wjh2012 <wjh2012@naver.com>
2022-11-12 14:03:28 +0900

    feat(LottoPublisher): 입력받은 금액이 1,000으로 나누어 떨어지는지 계산

---
+++ src/main/java/lotto/LottoPublisher.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import static lotto.config.Constants.LottoPrice.LOTTO_PRICE;
+
+public class LottoPublisher {
+
+    private void validate(int money) {
+        if (money % LOTTO_PRICE != 0){
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
