00d342ebbfca5798b9720fba052c5d22cfeb0fb2
ddubidubap <park62000@gmail.com>
2022-11-13 02:03:43 +0900

    feat: 사용자가 구입한 로또 용지를 받기까지의 과정을 나타낸 함수
    구입 금액 입력, 구입 금액 상당의 로또 용지 발급 받기, 사용자의 로또 번호 출력

---
+++ src/main/java/lotto/controller/LottoService.java
@@ -0,0 +1,29 @@
+package lotto.controller;
+
+import lotto.domain.Bonus;
+import lotto.domain.Lotto;
+import lotto.domain.PaidMoney;
+import lotto.domain.Ticket;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class LottoService {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final Validator validator = new Validator();
+
+    private List<List<Integer>> userTicket() {
+        String moneyInput = inputView.inputMoney();
+        int money = validator.validateMoney(moneyInput);
+        PaidMoney paidMoney = new PaidMoney(money);
+
+        int numOfTicket = paidMoney.buyNTicket();
+        Ticket ticket = new Ticket(numOfTicket);
+        List<List<Integer>> lottoTickets = ticket.purchaseTickets();
+        outputView.printTickets(numOfTicket, lottoTickets);
+
+        return lottoTickets;
+    }
+}
