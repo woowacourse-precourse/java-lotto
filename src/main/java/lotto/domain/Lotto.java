9cda78059aae2da413eb29b387035ad55119e6fa
iamjooon2 <iamjooon2@gmail.com>
2022-11-13 04:23:22 +0900

    feat: 숫자가 1부터 45 사이의 숫자인지 검증하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,8 +6,11 @@ import java.util.Set;
 
 public class Lotto {
     private static final int LOTTO_NUMBER_COUNT = 6;
+    private static final int LOTTO_RANGE_MAX = 45;
+    private static final int LOTTO_RANGE_MIN = 1;
     private static final String ERROR_NUMBERS_COUNT = "[ERROR] 로또 숫자는 6개여야 합니다.";
     private static final String ERROR_NUMBER_DUPLICATION = "[ERROR] 중복된 번호가 존재합니다.";
+    private static final String ERROR_NUMBER_RANGE = "[ERROR] 1부터 45 사이의 숫자만 입력가능합니다.";
 
     private final List<Integer> numbers;
 
@@ -23,6 +26,11 @@ public class Lotto {
         Set<Integer> numberSet = new HashSet<>(numbers);
         if (numberSet.size() != numbers.size()) {
             throw new IllegalArgumentException(ERROR_NUMBER_DUPLICATION);
+        }
+        for (Integer number : numbers) {
+            if (number > LOTTO_RANGE_MAX || number < LOTTO_RANGE_MIN) {
+                throw new IllegalArgumentException(ERROR_NUMBER_RANGE);
+            }
         }
     }
 
