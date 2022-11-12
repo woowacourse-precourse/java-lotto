6b7ca141b8beb058552da155a1b60ee2d2498515
great-park <cksgh1735@naver.com>
2022-11-13 01:08:03 +0900

    refactor: 문자열 wrapping

--- src/main/java/lotto/domain/PurchaseMoney.java
+++ src/main/java/lotto/domain/PurchaseMoney.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import lotto.view.Input;
 
 public class PurchaseMoney {
+    private final String PURCHASE_MONEY_MUST_OVER_1000 = "구매 금액은 1000원 이상을 입력해주세요.";
+    private final String PURCHASE_MONEY_UNIT_IS_1000 = "구매 금액은 1000원 단위로 입력해주세요.";
     private final int LOTTO_PRICE_UNIT = 1000;
     private final int price;
 
@@ -13,10 +15,10 @@ public class PurchaseMoney {
 
     private void validatePrice(int price) {
         if (price < LOTTO_PRICE_UNIT){
-            throw new IllegalArgumentException("구매 금액은 1000원 이상을 입력해주세요.");
+            throw new IllegalArgumentException(PURCHASE_MONEY_MUST_OVER_1000);
         }
         if (price % LOTTO_PRICE_UNIT != 0) {
-            throw new IllegalArgumentException("구매 금액은 1000원 단위로 입력해주세요.");
+            throw new IllegalArgumentException(PURCHASE_MONEY_UNIT_IS_1000);
         }
     }
 }
--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -5,8 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 import static java.lang.Integer.parseInt;
 
 public class Input {
+
+    private static final String PURCHASEMONEY_REQUEST = "구입금액을 입력해 주세요.";
+    private static final String INPUT_IS_NOT_NUMBER = "문자열이 아닌 숫자(정수)를 입력해주세요.";
+
     public static int insertPurchaseMoney() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(PURCHASEMONEY_REQUEST);
 
         String purchaseMoney = Console.readLine();
         checkNumber(purchaseMoney);
@@ -15,7 +19,7 @@ public class Input {
 
     private static void checkNumber(String input) {
         if (!input.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException("문자열이 아닌 숫자(정수)를 입력해주세요.");
+            throw new IllegalArgumentException(INPUT_IS_NOT_NUMBER);
         }
     }
 }
