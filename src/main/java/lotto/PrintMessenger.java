227c3abb9ba0d17b57e2a566da753b8471c04011
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-12 17:40:38 +0900

    fix(PrintMessenger) : winningResult ' '추가

--- src/main/java/lotto/PrintMessenger.java
+++ src/main/java/lotto/PrintMessenger.java
@@ -19,11 +19,11 @@ public class PrintMessenger {
     public static void winningResult(long [] countResult)// list를 받을지 int를 받을지 고민;;
     {
         System.out.println("당첨 통계\n" + "---");
-        System.out.println("3개 일치 (5,000원) -" + countResult[3]+ "개");
-        System.out.println("4개 일치 (50,000원) -"+ countResult[4]+"개");
-        System.out.println("5개 일치 (1,500,000원) -"+ countResult[5] +"개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) -"+ countResult[7] +"개");
-        System.out.println("6개 일치 (2,000,000,000원) -"+ countResult[6] +"개");
+        System.out.println("3개 일치 (5,000원) - " + countResult[3]+ "개");
+        System.out.println("4개 일치 (50,000원) - "+ countResult[4]+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+ countResult[5] +"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+ countResult[7] +"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+ countResult[6] +"개");
     }
     public static void yieldOfLotto(double value){
         System.out.println("총 수익률은 "+ value +"%입니다.");
