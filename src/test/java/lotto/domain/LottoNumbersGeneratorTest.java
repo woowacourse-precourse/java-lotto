9d50d1b25c13eca68c4a7429caa6d9be8e8823ed
jeujeon <1132jjw@naver.com>
2022-11-12 15:56:56 +0900

    test(LottoNumbersGenerator): 사용자의 입력값을 List<Integer>형으로 잘 바꾸어주는지 확인하는 테스트 작성

---
+++ src/test/java/lotto/domain/LottoNumbersGeneratorTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoNumbersGeneratorTest {
+	@DisplayName("사용자가 정상적으로 로또 숫자를 입력했을 때.")
+	@Test
+	void createLottoRightWay() {
+		assertThat(LottoNumbersGenerator.generate("1,2,3,4,5,6")).isEqualTo(
+			List.of(1, 2, 3, 4, 5, 6));
+	}
+
+	@DisplayName("사용자가 비정상적인 문자를 입력했을 때.")
+	@Test
+	void createLottoByInvalidCharater() {
+		assertThatThrownBy(() -> LottoNumbersGenerator.generate("1,2,3,4,5,6"))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+}
--- src/test/java/lotto/domain/LottoTicketNumberGeneratorTest.java
+++ src/test/java/lotto/domain/LottoTicketNumberGeneratorTest.java
@@ -1,25 +1,24 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
-class LottoNumberGeneratorTest {
+class LottoTicketNumberGeneratorTest {
 
 	@Test
 	void 중복된_숫자_테스트() {
-		Set<Integer> lottoNums = new HashSet<>(LottoNumberGenerator.generate());
+		Set<Integer> lottoNums = new HashSet<>(LottoTicketNumberGenerator.generate());
 		assertThat(lottoNums).hasSize(6);
 
 	}
 
 	@Test
 	void 숫자_범위_테스트() {
-		assertThat(LottoNumberGenerator.generate())
+		assertThat(LottoTicketNumberGenerator.generate())
 			.filteredOn(num -> num >= 1 && num <= 45)
 			.hasSize(6);
 	}
