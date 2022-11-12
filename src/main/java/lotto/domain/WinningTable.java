378d633bbf792e995f3157780b88d24bea1d3373
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-12 15:59:00 +0900

    feat(lotto) : 로또 당첨 테이블 추가
    등수별 당첨금액을 포함하고 있는 로또 당첨 테이블 추가

--- src/main/java/lotto/domain/WinningTable.java
+++ src/main/java/lotto/domain/WinningTable.java
@@ -1,15 +1,16 @@
 package lotto.domain;
 
-public enum Winning {
+public enum WinningTable {
     FIRST(2000000000),
     SECOND(30000000),
     THIRD(1500000),
     FORTH(50000),
-    FIFTH(5000);
+    FIFTH(5000),
+    LOSE(0);
 
     int price;
 
-    Winning(int price) {
+    WinningTable(int price) {
         this.price = price;
     }
 
