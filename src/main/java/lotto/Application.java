88ef4322023f96b23e3b6d996c553b24b2671738
leewoojin <zmfpdl64@naver.com>
2022-11-12 23:45:43 +0900

    Feat: Application 실행 클래스에 Lotto 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.playlotto.PlayLotto;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        PlayLotto lotto = new PlayLotto();
+        lotto.play();
     }
 }
