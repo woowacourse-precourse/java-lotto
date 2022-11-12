68ba50a2659c2df9b786d295611b6e349ec83969
newh08 <newh08@gmail.com>
2022-11-12 20:22:12 +0900

    feat: add function to terminate the program when an error occurs

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,7 +5,11 @@ import lotto.controller.LottoController;
 public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
-        lottoController.buyLotto();
-        lottoController.checkLottoResult();
+        try {
+            lottoController.buyLotto();
+            lottoController.checkLottoResult();
+        } catch(IllegalArgumentException e){
+            System.out.println("프로그램을 종료합니다.");
+        }
     }
 }
