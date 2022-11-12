8c4d33200e6ea2ec72117d0d5275c33eb3559629
noyes5 <noyes5@naver.com>
2022-11-12 23:37:36 +0900

    refactor: 사용자가 로또 구입하는 기능 변경

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,23 +1,31 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class User {
-    int money = 0;
     public static final int ONEGAMECOST = 1000;
 
     public int inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
         String inputMoney = Console.readLine();
-        int money = validate(inputMoney);
-        validate(money);
-        return money;
+        int gameMoney = validate(inputMoney);
+        validate(gameMoney);
+        return gameMoney;
     }
 
-    public void validate(int money) {
-        if (money % ONEGAMECOST != 0) {
+
+
+    public void validate(int gameMoney) {
+        if (gameMoney % ONEGAMECOST != 0) {
             throw new IllegalArgumentException("[ERROR]금액이 잘못되었습니다. 1000 단위로 입력해주세요.");
         }
-        if (money == 0) {
+        if (gameMoney == 0) {
             throw new IllegalArgumentException("[ERROR]금액이 0 입니다.");
         }
     }
@@ -26,6 +34,18 @@ public class User {
             return Integer.parseInt(inputMoney);
         } catch (NumberFormatException e) {
             throw new RuntimeException("[ERROR]");
+        }
+    }
+
+    public void buyLotto() {
+        List<List<Integer>> totalBuyLotto = new ArrayList<>();
+        int lottoAmount = inputMoney() / 1000;
+        for (int i = 0; i < lottoAmount; i++) {
+            totalBuyLotto.add(Lotto.create());
+        }
+        System.out.println(totalBuyLotto.size() + "개를 구매했습니다.");
+        for (List<Integer> val: totalBuyLotto) {
+            System.out.println(val);
         }
     }
 }
