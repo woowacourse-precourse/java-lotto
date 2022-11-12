87934e862d6be97074461beda40d7318e82298be
ChanghyunKim <variety82p@gamil.com>
2022-11-12 20:04:28 +0900

    feat(View) : 로또번호 출력 view 구현

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -1,13 +1,21 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.LottoTickets;
 
 import static lotto.utils.Messages.*;
 
 public class View {
     public String startLottoView(){
         System.out.println(LOTTO_BUY_MESSAGE);
-        String amount = Console.readLine();
-        return amount;
+        String input = Console.readLine();
+        return input;
+    }
+
+    public void lottoListView(int lottoCount, LottoTickets lottoTickets){
+        System.out.printf(LOTTO_COUNT_MESSAGE, lottoCount);
+        for(int i = 0; i < lottoCount; i++){
+            System.out.println(lottoTickets.getLottoTickets().get(i).getLottoNumber());
+        }
     }
 }
