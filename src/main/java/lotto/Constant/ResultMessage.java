b097e626c9bf1642b8f5bf9547df6e575345c81e
swandevson <shai4956@gmail.com>
2022-11-12 15:30:30 +0900

    Fix(ResultMessage): 수익률 단위 및 띄어쓰기

--- src/main/java/lotto/Constant/ResultMessage.java
+++ src/main/java/lotto/Constant/ResultMessage.java
@@ -5,5 +5,5 @@ public class ResultMessage {
     public static final String RESULT_START = "당첨 통계" + System.lineSeparator() + "---";
     public static final String RESULT_RANK = "%d개 일치%s(%s원) - %d개" + System.lineSeparator();
     public static final String HIT_BONUS_NUMBER = ", 보너스 볼 일치 ";
-    public static final String PROFIT = "총 수익률은 %.2f% 입니다." + System.lineSeparator();
+    public static final String PROFIT = "총 수익률은 %.1f%%입니다." + System.lineSeparator();
 }
