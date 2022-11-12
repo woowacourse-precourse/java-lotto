8e904305cbd62391ebc5016ba7d859b72867618c
kanghyki <kanghyki@gmail.com>
2022-11-13 03:11:05 +0900

    feat(LottoGame): 로또 등수 enum 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,4 +1,12 @@
 package lotto;
 
 public class LottoGame {
+    public enum eLottoPlace {
+        FIRST_PLACE,
+        SECOND_PLACE,
+        THIRD_PLACE,
+        FOURTH_PLACE,
+        FIFTH_PLACE,
+        NOTHING
+    }
 }
