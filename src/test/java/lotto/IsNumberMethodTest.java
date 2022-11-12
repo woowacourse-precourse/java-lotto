64f2082697f9b5547600b1cd68840c87ee393312
neppiness <0414kjh@naver.com>
2022-11-12 11:26:06 +0900

    test: isNumber method test (src/test/java/lotto)
    Return true if the character input is number and return false if not

---
+++ src/test/java/lotto/IsNumberMethodTest.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import static lotto.Application.isNumber;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class IsNumberMethodTest {
+    @Test
+    void isComma_메소드_정상_입력_테스트() {
+        for(char number = '0'; number <= '9'; number++)
+            assertThat(isNumber(number)).isEqualTo(true);
+    }
+
+    @Test
+    void isComma_메소드_예외_테스트1() {
+        for(char asciicode = 0; asciicode < 256; asciicode++) {
+            if(asciicode >= '0' && asciicode <= '9') continue;
+            assertThat(isNumber(asciicode)).isEqualTo(false);
+        }
+    }
+}
