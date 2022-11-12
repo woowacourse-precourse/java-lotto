76a023f084bbded1354047f516a789aacf789e4c
CHO Min HO <chominho96@naver.com>
2022-11-12 13:33:58 +0900

    feat(randomnumber): add random number generator
    - Add random number generator to generate 7 random numbers

---
+++ src/main/java/lotto/randomnumber/RandomGenerator.java
@@ -0,0 +1,18 @@
+package lotto.randomnumber;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+/**
+ * 난수 생성과 관련된 클래스입니다.
+ */
+public class RandomGenerator {
+
+    /**
+     * 랜덤 로또 번호를 생성하는 기능을 담당합니다.
+     * @return 1~45의 범위를 갖는 중복되지 않은 일반 로또 번호 6개와, 보너스 로또 번호 1개
+     */
+    public static List<Integer> generateRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 7);
+    }
+}
---
+++ src/test/java/lotto/randomnumber/RandomGeneratorTest.java
@@ -0,0 +1,28 @@
+package lotto.randomnumber;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static lotto.randomnumber.RandomGenerator.generateRandomNumbers;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * RandomGenerator 클래스에 관한 테스트 클래스입니다.
+ */
+class RandomGeneratorTest {
+
+    /**
+     * 정상적으로 7개의 중복되지 않는 난수를 생성하는지 테스트합니다.
+     */
+    @Test
+    void generateRandomNumbersTest() {
+        for (int i = 0; i < 100; i++) {
+            List<Integer> randomNumbers = generateRandomNumbers();
+            Set<Integer> uniqueRandomNumbers = new HashSet<>(randomNumbers);
+            assertThat(uniqueRandomNumbers.size()).isEqualTo(7);
+        }
+    }
+}
