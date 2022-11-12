fcb5e265925627ccd4a8404ab589e8040f373a75
ParkJoungWoo <jug78@naver.com>
2022-11-12 16:28:25 +0900

    feat(lotto/Lotto.java) : Lotto 클래스 기능 추가
    로또 객체의 유효성 검증을 로또객체 내부에서 할 수 있도록 검증 메소드를
    추가했습니다.
    
    - 로또 숫자가 중복되는지 확인하는 기능
    - 로또 숫자에 1~45 이외의 숫자가 있는지 확인하는 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,32 +2,53 @@ package lotto;
 
 import ui.View;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
-
-    private void validate(List<Integer> numbers) {
+    private void validate(List<Integer> numbers){
+        validateDuplicate(numbers);
+        validateLength(numbers);
+        validateOutRange(numbers);
+    }
+    private void validateLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
-    private String numberForm(){
+    private void validateOutRange(List<Integer> numbers){
+        for(int i = 0; i < numbers.size(); i++){
+            if(numbers.get(i) < 1 || numbers.get(i) > 45){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+    private void validateDuplicate(List<Integer> numbers){
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if(numberSet.size() != 6){
+            throw new IllegalArgumentException();
+        }
+    }
+    public String toNumberForm(){
         String message ="[";
         for(int number : numbers){
             message += number + ", ";
         }
-        message = message.substring(0, message.length() - 1) + "]";
+        message = message.substring(0, message.length() - 2) + "]";
         return message;
     }
 
     public void printLotto(){
-        View.printLottoNumber(numberForm());
+        View.printLottoNumber(toNumberForm());
     }
 
 }
--- src/main/java/user/Money.java
+++ src/main/java/user/Money.java
@@ -1,46 +1,59 @@
 package user;
 
+import camp.nextstep.edu.missionutils.Console;
+import ui.View;
+
 public class Money {
-    private final boolean NOT_VALID = false;
     private final boolean VALID = true;
     private final int BASE_MONEY = 1000;
     private int money;
 
-    public Money(){
-        money = 0;
-    }
-    public Money(int money) {
-        if(isMoneyNumeric(money) && isMoneyThousandMultiple(money)) {
+    public Money() {
+        View.promptSeedMoney();
+        int money = inputToInteger();
+        if (isMoneyPositive(money) && isMoneyThousandMultiple(money)) {
             this.money = money;
         }
     }
 
-    private boolean isMoneyNumeric(int money) {
-        if (isMoneyNotNull() == NOT_VALID) {
-            throw new IllegalArgumentException();
+    public Money(int money) {
+        if (isMoneyPositive(money) && isMoneyThousandMultiple(money)) {
+            this.money = money;
         }
-        if (isMoneyPositive() == NOT_VALID) {
-            throw new IllegalArgumentException();
-        }
-        return true;
     }
 
-    private boolean isMoneyNotNull() {
-        Integer moneyObj = this.money;
-        return moneyObj.equals(null);
+    private int inputToInteger() {
+        String input = Console.readLine();
+        checkInputNull(input);
+        checkInputNumber(input);
+        return Integer.parseInt(input);
     }
 
-    private boolean isMoneyPositive() {
+    private void checkInputNull(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkInputNumber(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) < '0' || input.charAt(i) > '9') {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private boolean isMoneyPositive(int money) {
         return money >= 0;
     }
 
     public boolean isMoneyThousandMultiple(int money) {
-        return (money % BASE_MONEY == 0) && (money / BASE_MONEY > 0);
+        return money % BASE_MONEY == 0;
     }
 
-    public void setPrizeMoney(int money) {
-        if(isMoneyNumeric(money) == VALID){
-            this.money = money;
+    public void addMoney(int money) {
+        if (isMoneyPositive(money) == VALID) {
+            this.money += money;
         }
     }
 
