d01580b903f77381082e61db205c3176fe2bdfb3
Soomin Lim <augustth511@gmail.com>
2022-11-12 18:24:59 +0900

    feat(LottoGameController): 로또 구입 금액 입력, 로또 발행 및 출력, 당첨번호와 보너스 번호 입력 과정 컨트롤러 구현

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,41 @@
+package lotto.controller;
+
+import lotto.domain.LottoPurchaseMoney;
+import lotto.domain.PlayerLotto;
+import lotto.domain.WinningLotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class LottoGameController {
+
+    public void playGame() {
+        LottoPurchaseMoney lottoPurchaseMoney = createLottoPurchaseMoney();
+
+        PlayerLotto playerLotto = createPlayerLotto(lottoPurchaseMoney);
+        printPlayerLotto(playerLotto);
+
+        WinningLotto winningLotto = createWinningLotto();
+    }
+
+    private LottoPurchaseMoney createLottoPurchaseMoney() {
+        int money = InputView.lottoPurchaseMoney();
+        return new LottoPurchaseMoney(money);
+    }
+
+    private PlayerLotto createPlayerLotto(LottoPurchaseMoney lottoPurchaseMoney) {
+        return new PlayerLotto();
+    }
+
+    private void printPlayerLotto(PlayerLotto playerLotto) {
+        OutputView.printLottos(playerLotto.getLottos());
+    }
+
+    private WinningLotto createWinningLotto() {
+        List<Integer> winningNumbers = InputView.winningNumbers();
+        int bonusNumber = InputView.bonusNumber();
+
+        return new WinningLotto(winningNumbers, bonusNumber);
+    }
+}
