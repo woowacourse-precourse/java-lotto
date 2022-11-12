e457b6ced106649d35cbbd4441489664a0ee3218
gomljo <gomljo0307@naver.com>
2022-11-13 00:28:09 +0900

    feat(Lotto.java): 생성된 로또 번호가 6 개가 아닌지 확인하는 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,17 +1,16 @@
-package lotto;
+package lotto.model;
 
 import java.util.List;
 import java.util.stream.Stream;
 
 public class Lotto {
     private static final int PROPER_QUANTITY=6;
-    private static final String DUPLICATE_ERROR_MESSAGE="[ERROR] 숫자가 중복되었습니다. 프로그램을 종료합니다.";
     private static final String WRONG_QUANTITY_ERROR_MESSAGE="[ERROR] 로또 번호의 수가 6개가 아닙니다. 프로그램을 종료합니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateProperQuantity(numbers);
-        validateDuplicatedNumbers(numbers);
+
         this.numbers = numbers;
     }
 
@@ -20,13 +19,4 @@ public class Lotto {
             throw new IllegalArgumentException(WRONG_QUANTITY_ERROR_MESSAGE);
         }
     }
-    private void validateDuplicatedNumbers(List<Integer> numbers) {
-        int numberOfUniqueNumbers;
-        Stream<Integer> lottoNumbers = numbers.stream();
-        numberOfUniqueNumbers= (int)lottoNumbers.distinct().count();
-        if (numberOfUniqueNumbers != PROPER_QUANTITY) {
-            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
-        }
-    }
-
 }
