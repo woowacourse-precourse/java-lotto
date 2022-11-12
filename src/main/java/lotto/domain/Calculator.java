e5369c2e4ccf0660b07bd4002dd84b96b1497887
jaehyeon <jhjh0022@naver.com>
2022-11-13 02:59:03 +0900

    style: 줄간격 맞추기

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -4,16 +4,16 @@ import java.util.List;
 
 public class Calculator {
 
-    public static double df(List<Integer> rewardList, int money){
+    public static double df(List<Integer> rewardList, int money) {
         double moneys = money;
         double total = totalRewardmoney(rewardList);
 
-        return (double)Math.round((total/moneys)*10000)/1000);
+        return (double) Math.round((total / moneys) * 10000) / 1000;
     }
 
-    private static int totalRewardmoney(List<Integer> rewardList){
+    private static int totalRewardmoney(List<Integer> rewardList) {
         int sum = 0;
-        for(int money: rewardList){
+        for (int money : rewardList) {
             sum += money;
         }
         return sum;
