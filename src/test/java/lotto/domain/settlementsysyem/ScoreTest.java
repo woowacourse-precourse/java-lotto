29122607430e3e962cc491504283101e89d9768a
JaehongDev <println@kakao.com>
2022-11-12 15:38:18 +0900

    test (ScoreTest) : 점수 기능요구사항에 대한 테스트코드 작성
    - 스코어가 0점 0점이면 낙첨입니다
    - 스코어가 3점 1점 혹은 3점 0점이면 4등입니다 (Integer, Integer)
    - 스코어가 5점 0점 혹은 4점 1점이면 3등입니다 (Integer, Integer)
    - 스코어가 5점 1점이면 2등입니다 (Integer, Integer)
    - 스코어는 일반점수와 보너스점수가 다르면 다른 스코어입니다 (Integer, Integer)
    - 스코어가 2점 1점이면 5등입니다 (Integer, Integer)
    - 그 외의 경우는 전부 낙첨입니다 (Integer, Integer)
    - 스코어가 6점 0점이면 1등입니다 (Integer, Integer)
    스코어는 일반점수와 보너스점수가 같으면 같은 스코어입니다 (Integer, Integer)

--- src/test/java/lotto/domain/settlementsysyem/ScoreTest.java
+++ src/test/java/lotto/domain/settlementsysyem/ScoreTest.java
@@ -2,6 +2,7 @@ package lotto.domain.settlementsysyem;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -10,16 +11,64 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class ScoreTest {
-
+    private final ScoreGenerator scoreGenerator = new ScoreGenerator();
 
     @Test
-    void 비어있는_스코어를_추가할_수_있습니다() {
+    void _스코어가_0점_0점이면_낙첨입니다() {
         var score = Score.generateEmptyScore();
-
-        assertThat(score.getBonus()).isEqualTo(0);
-        assertThat(score.getNormal()).isEqualTo(0);
+        var actual = score.generateRank();
+        Assertions.assertThat(actual).isEqualTo(Ranking.NOT_WINNING);
     }
 
+
+    @ParameterizedTest
+    @CsvSource(value = {"6:0"}, delimiterString = ":")
+    void _스코어가_6점_0점이면_1등입니다(final Integer normal, final Integer bonus) {
+        var score = this.scoreGenerator.generate(normal, bonus);
+        var actual = score.generateRank();
+        Assertions.assertThat(actual).isEqualTo(Ranking.FIRST_WINNING);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"5:1"}, delimiterString = ":")
+    void 스코어가_5점_1점이면_2등입니다(final Integer normal, final Integer bonus) {
+        var score = this.scoreGenerator.generate(normal, bonus);
+        var actual = score.generateRank();
+        Assertions.assertThat(actual).isEqualTo(Ranking.SECOND_WINNING);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"4:1", "5:0"}, delimiterString = ":")
+    void 스코어가_5점_0점_혹은_4점_1점이면_3등입니다(final Integer normal, final Integer bonus) {
+        var score = this.scoreGenerator.generate(normal, bonus);
+        var actual = score.generateRank();
+        Assertions.assertThat(actual).isEqualTo(Ranking.THIRD_WINNING);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"3:1", "4:0"}, delimiterString = ":")
+    void 스코어가_3점_1점_혹은_3점_0점이면_4등입니다(final Integer normal, final Integer bonus) {
+        var score = this.scoreGenerator.generate(normal, bonus);
+        var actual = score.generateRank();
+        Assertions.assertThat(actual).isEqualTo(Ranking.FOURTH_WINNING);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2:1"}, delimiterString = ":")
+    void 스코어가_2점_1점이면_5등입니다(final Integer normal, final Integer bonus) {
+        var score = this.scoreGenerator.generate(normal, bonus);
+        var actual = score.generateRank();
+        Assertions.assertThat(actual).isEqualTo(Ranking.FIFTH_WINNING);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:1", "0:0", "1:0", "1:1", "2:0"}, delimiterString = ":")
+    void 그_외의_경우는_전부_낙첨입니다(final Integer normal, final Integer bonus) {
+        var score = this.scoreGenerator.generate(normal, bonus);
+        var actual = score.generateRank();
+        Assertions.assertThat(actual).isEqualTo(Ranking.NOT_WINNING);
+    }
 
     @ParameterizedTest
     @CsvSource(value = {"6:1", "5:0", "0:0", "1:1", "2:1", "3:0"}, delimiterString = ":")
@@ -38,5 +87,20 @@ class ScoreTest {
         var original = new Score(bonus, normal);
 
         assertThat(target).isNotEqualTo(original);
+    }
+
+
+    class ScoreGenerator {
+        public Score generate(final Integer normalCount, final Integer bonusCount) {
+            var score = Score.generateEmptyScore();
+            for (int i = 0; i < normalCount; i++) {
+                score = score.plusNormal();
+            }
+
+            for (int i = 0; i < bonusCount; i++) {
+                score = score.plusBonus();
+            }
+            return score;
+        }
     }
 }
