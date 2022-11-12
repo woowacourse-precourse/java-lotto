c04844fdf8baf7a589454fb0559df66d29e84b93
hong-sile <gurwns9325@naver.com>
2022-11-12 15:19:05 +0900

    feat(Rank): 번호의 일치 개수와, 보너스 번호 포함 여부를 입력받아 일치하는 Rank를 반환하는 메소드추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,11 +1,13 @@
 package lotto.domain;
 
+import java.util.Optional;
+
 public enum Rank {
-    FIFTH_PLACE(3, false, 5_000),
-    FOURTH_PLACE(4, false, 50_000),
-    THIRD_PLACE(5, false, 1_500_000),
+    FIRST_PLACE(6, false, 2_000_000_000),
     SECOND_PLACE(5, true, 30_000_000),
-    FIRST_PLACE(6, false, 2_000_000_000);
+    THIRD_PLACE(5, false, 1_500_000),
+    FOURTH_PLACE(4, false, 50_000),
+    FIFTH_PLACE(3, false, 5_000);
 
     private final int matchCount;
     private final boolean bonusMatch;
@@ -16,6 +18,24 @@ public enum Rank {
         this.matchCount = matchCount;
         this.bonusMatch = bonusMatch;
         this.prize = prize;
+    }
+
+    public static Optional<Rank> matchRank(int matchCount, boolean bonusMatch) {
+        switch (matchCount) {
+            case 6:
+                return Optional.of(FIRST_PLACE);
+            case 5:
+                if (bonusMatch) {
+                    return Optional.of(SECOND_PLACE);
+                }
+                return Optional.of(THIRD_PLACE);
+            case 4:
+                return Optional.of(FOURTH_PLACE);
+            case 3:
+                return Optional.of(FIFTH_PLACE);
+            default:
+                return Optional.empty();
+        }
     }
 
     public int getPrize() {
