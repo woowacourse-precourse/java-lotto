190052a7284a05ff3763df792e60173c039d34c1
slbin-park <smpts00@naver.com>
2022-11-12 22:09:33 +0900

    refactor(Validator) : 매직넘버 상수로 변경 및 에러 메세지 수정
    - changeNumber 함수 INVALID_NUMBER로 변경

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -6,6 +6,7 @@ public class Validator {
     private final String INVALID_NUMBER = "[ERROR] 숫자만 입력 가능합니다.";
     private final String INVALID_MONEY = "[ERROR] 1000원 단위로 입력이 가능합니다.";
     private final int MONEY_UNIT = 1000;
+    private final int NOTHING_CHANGES = 0;
 
     public int moneyInputValidator(String money) {
         int moneyNumber = changeNumber(money);
@@ -17,12 +18,12 @@ public class Validator {
         try {
             return Integer.parseInt(money);
         } catch (Exception e) {
-            throw new IllegalArgumentException(INVALID_MONEY);
+            throw new IllegalArgumentException(INVALID_NUMBER);
         }
     }
 
     public void checkChanges(int money) {
-        if (money % MONEY_UNIT == 0) {
+        if (money % MONEY_UNIT == NOTHING_CHANGES) {
             return;
         }
         throw new IllegalArgumentException(INVALID_MONEY);
