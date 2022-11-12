d62ab3688081bf91f5538186c23e2aab5c750757
H4R1B0 <qkrguswns25@naver.com>
2022-11-12 15:35:22 +0900

    chore: use enum message of notification

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -6,26 +6,26 @@ import java.util.List;
 public class Validation {
     public static boolean isMultipleOf1000(int number) throws IllegalArgumentException {
         if (number % 1000 != 0)
-            throw new IllegalArgumentException("[ERROR] 로또 구매 금액은 1000원 단위여야 합니다.");
+            throw new IllegalArgumentException(Notification.ERROR_MESSAGE_MONEY_IS_NOT_MULTIPLE_1000.getMessage());
         return true;
     }
 
     public static void isLottoBetween1And45(List<Integer> numbers) throws IllegalArgumentException {
         for (int number : numbers) {
             if (number < 1 || number > 45)
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                throw new IllegalArgumentException(Notification.ERROR_MESSAGE_NUMBER_IS_NOT_BETWEEN_1_AND_45.getMessage());
         }
     }
 
     public static void isLottoDuplicate(List<Integer> numbers) throws IllegalArgumentException {
         HashSet<Integer> set = new HashSet<>(numbers);
         if (set.size() != 6)
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않은 6개 숫자여야 합니다.");
+            throw new IllegalArgumentException(Notification.ERROR_MESSAGE_NUMBER_IS_DUPLICATE.getMessage());
     }
 
     public static boolean isBonusInLotto(List<Integer> numbers, int bonus) throws IllegalArgumentException {
         if (numbers.contains(bonus))
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 로또 번호에 없는 번호여야 합니다.");
+            throw new IllegalArgumentException(Notification.ERROR_MESSAGE_BONUS_IN_LOTTO.getMessage());
         return true;
     }
 }
