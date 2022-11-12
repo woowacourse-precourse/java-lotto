065ed1d52bb200cce633b9d4177c55f802328dfb
김현재 <41482946+galug@users.noreply.github.com>
2022-11-12 12:10:50 +0900

    test(ranktest): 순위에 따른 문구를 반환하는 기능 테스트

--- src/test/java/lotto/model/RankTest.java
+++ src/test/java/lotto/model/RankTest.java
@@ -5,12 +5,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RankTest {
+    private static final String FIRST_STATE = "6개 일치 (2,000,000,000원)";
+    private static final String SECOND_STATE = "5개 일치, 보너스 볼 일치 (30,000,000원)";
+    private static final String THIRD_STATE = "5개 일치 (1,500,000원)";
+    private static final String FOURTH_STATE = "4개 일치 (50,000원)";
+    private static final String FIFTH_STATE = "3개 일치 (5,000원)";
+
     @DisplayName("맞춘 개수가 6개일 경우 1등을 반환하는지 확인한다.")
     @Test
     void createFirstRankWithMatchingNumber() {
         Rank firstRank = Rank.of(6, false);
 
         Assertions.assertThat(firstRank).isEqualTo(Rank.FIRST);
+        Assertions.assertThat(firstRank.toString()).isEqualTo(FIRST_STATE);
     }
 
     @DisplayName("맞춘 개수가 5개 + 보너스 1개일 경우 1등을 반환하는지 확인한다.")
@@ -19,6 +26,7 @@ class RankTest {
         Rank firstRank = Rank.of(5, true);
 
         Assertions.assertThat(firstRank).isEqualTo(Rank.SECOND);
+        Assertions.assertThat(firstRank.toString()).isEqualTo(SECOND_STATE);
     }
 
     @DisplayName("맞춘 개수가 5개일 경우 3등을 반환하는지 확인한다.")
@@ -27,6 +35,7 @@ class RankTest {
         Rank firstRank = Rank.of(5, false);
 
         Assertions.assertThat(firstRank).isEqualTo(Rank.THIRD);
+        Assertions.assertThat(firstRank.toString()).isEqualTo(THIRD_STATE);
     }
 
     @DisplayName("맞춘 개수가 4개일 경우 4등을 반환하는지 확인한다.")
@@ -35,6 +44,7 @@ class RankTest {
         Rank firstRank = Rank.of(4, false);
 
         Assertions.assertThat(firstRank).isEqualTo(Rank.FOURTH);
+        Assertions.assertThat(firstRank.toString()).isEqualTo(FOURTH_STATE);
     }
 
     @DisplayName("맞춘 개수가 3개일 경우 5등을 반환하는지 확인한다.")
@@ -43,6 +53,7 @@ class RankTest {
         Rank firstRank = Rank.of(3, false);
 
         Assertions.assertThat(firstRank).isEqualTo(Rank.FIFTH);
+        Assertions.assertThat(firstRank.toString()).isEqualTo(FIFTH_STATE);
     }
 
     @DisplayName("맞춘 개수가 2개일 경우 꼴등임을 반환하는지 확인한다.")
