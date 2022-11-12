64b64c78e81b4fe763aad41bf0da5dcbfce038ec
qdeu22 <qdeu22@gmail.com>
2022-11-12 10:51:44 +0900

    feat(lotto): 사용자가 입력한 로또 번호와 보너스 번호를 당첨 번호와 같은지 알수있는 기능

--- src/main/java/lotto/domain/Judgement.java
+++ src/main/java/lotto/domain/Judgement.java
@@ -1,4 +1,23 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Judgement {
+    public int compareNumbers(List<Integer> computer, List<Integer> player) {
+        int result = 0;
+        for (int i = 0; i < computer.size(); i++) {
+            if (player.contains(computer.get(i))) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public int compareBonus(List<Integer> computer, int number) {
+        int bonus = 0;
+        if(computer.contains(number)){
+            bonus++;
+        }
+        return bonus;
+    }
 }
---
+++ src/test/java/lotto/domain/JudgementTest.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JudgementTest {
+
+    @DisplayName("로또 번호가 몇개 당첨됬는지 확인한다.")
+    @Test
+    void correctNumbers() {
+        Judgement judgement = new Judgement();
+        int result = judgement.compareNumbers(List.of(1, 2, 3, 4, 5, 6), List.of(1, 2, 3, 4, 5, 6));
+        assertThat(result).isEqualTo(6);
+    }
+
+    @DisplayName("보너스번호가 당첨되는지 확인한다.")
+    @Test
+    void correctBonus() {
+        Judgement judgement = new Judgement();
+        int result = judgement.compareBonus(List.of(1, 2, 3, 4, 5, 6), 7);
+        assertThat(result).isEqualTo(0);
+    }
+}
