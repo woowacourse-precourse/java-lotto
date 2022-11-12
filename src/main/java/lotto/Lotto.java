a505bd3b92ce44f121f837ea15b809ef472ecb04
hyowonhan <wain1719@gmail.com>
2022-11-12 18:12:37 +0900

    feat: 당첨 번호와 사용자 로또 번호 비교 메서드 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,14 +1,16 @@
 package lotto;
 
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final String BONUS_INPUT_SENTENCE  = "보너스 번호를 입력해 주세요.\n";
 
     public Lotto(List<Integer> numbers) {
         validateCount(numbers);
@@ -37,5 +39,30 @@ public class Lotto {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
         }
+    }
+
+    private int bonus() {
+        System.out.print(BONUS_INPUT_SENTENCE);
+        int bonusNum = Integer.parseInt(Console.readLine());
+        if(bonusNum< 1 || bonusNum> 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        return bonusNum;
+    }
+
+    public List<Integer> compareList() {
+        BuyLotto buy = new BuyLotto();
+        List<List<Integer>> userLottoNumber = buy.lottoNumberMaker();
+        List<Integer> winLottoNumber = this.numbers;
+        List<Integer> sameCount = new ArrayList<>();
+        System.out.println("당첨 번호: " + winLottoNumber);
+        for(int i=0; i<userLottoNumber.size(); i++) {
+            List<Integer> result = userLottoNumber.get(i).stream()
+                    .filter(old -> winLottoNumber.stream()
+                            .anyMatch(Predicate.isEqual(old)))
+                    .collect(Collectors.toList());
+            sameCount.add(result.size());
+        }
+        return sameCount;
     }
 }
