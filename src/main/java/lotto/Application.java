95f2ce3a2d786a2454ee160da4ebba3c4ff25ecd
YeaChan05 <qkenrdl05@gmail.com>
2022-11-13 02:57:24 +0900

    범위 내의 보너스 번호 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,18 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
-    static List<Lotto> Lottos = new ArrayList<>();
+    static List<Lotto> lottos = new ArrayList<>();
+    static List<Integer> winningNumber = new ArrayList<>();
+    static int bonusNumber;
 
     public static void main(String[] args) {
         try {
             purchaseLotto(Integer.parseInt(Console.readLine()));
             printLotto();
-
-            // TODO: 당첨 번호 입력
-            // TODO: 보너스 번호 입력
+            inputWinningNumber(Console.readLine());
+            inputBonusNumber(Console.readLine());
             // TODO: 1등부터 5등까지 몇개나 당첨되었는지 출력
             // TODO: 수익률을 소수점 둘째 자리에서 반올림하여 출력
         } catch (IllegalArgumentException e) {
@@ -23,16 +25,40 @@ public class Application {
         }
     }
 
+    private static void inputBonusNumber(String input) {
+        bonusNumber = Integer.parseInt(input);
+        checkInputRange(input);
+    }
+
+    private static void inputWinningNumber(String input) {
+        String str[] = input.split(",");
+        checkInputCount(str);
+        for (int i = 0; i < str.length; i++) {
+            checkInputRange(str[i]);
+            winningNumber.add(i, Integer.parseInt(str[i]));
+        }
+    }
+
+    private static void checkInputCount(String[] str) {
+        if (str.length != 6)
+            throw new IllegalArgumentException("[ERROR]invalid input: 6개의 값을 입력해주세요.");
+    }
+
+    private static void checkInputRange(String str) {
+        if ((Integer.parseInt(str) < 1 || Integer.parseInt(str) > 45))
+            throw new IllegalArgumentException("[ERROR]invalid input: 1과 45 사이의 값을 입력해주세요.");
+    }
+
     private static void printLotto() {
-        System.out.println(Lottos.size()+"개를 구매했습니다.");
-        for (int i = 0; i < Lottos.size(); i++)
-            System.out.println(Lottos.get(i).getNumbers());
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (int i = 0; i < lottos.size(); i++)
+            System.out.println(lottos.get(i).getNumbers());
     }
 
     private static void purchaseLotto(int money) {
         int lottoCount = money / 1000;
         for (int i = 0; i < lottoCount; i++)
-            Lottos.add(quickPicks());
+            lottos.add(quickPicks());
     }
 
     private static Lotto quickPicks() {
