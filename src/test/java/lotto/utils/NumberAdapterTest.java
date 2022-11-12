1fc8a988c814fad658ef12b435efddf6cef3af3a
Jin409 <sualng123@gmail.com>
2022-11-12 10:27:55 +0800

    test(NumberAdapterTest): 당첨번호와 보너스 번호를 합쳐서 하나의 List 로 반환하는 기능의 테스트 코드 추가

--- src/test/java/lotto/utils/NumberAdapterTest.java
+++ src/test/java/lotto/utils/NumberAdapterTest.java
@@ -2,8 +2,10 @@ package lotto.utils;
 
 import static lotto.utils.NumberAdapter.getBonusNumber;
 import static lotto.utils.NumberAdapter.getLotteryNumbers;
+import static lotto.utils.NumberAdapter.getWinningNumber;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,7 +26,7 @@ public class NumberAdapterTest {
 
     @Test
     @DisplayName("조건에 맞는 입력값을 입력했을 때 문자열에서 정수형으로 형을 변환하여 반환한다.")
-    void checkGetBonusNumber(){
+    void checkGetBonusNumber() {
         //given
         String validInput = "3";
 
@@ -33,5 +35,19 @@ public class NumberAdapterTest {
 
         //then
         assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("조건에 맞는 입력값을 입력했을 때 List형과 정수를 하나의 정수 List 로 형을 변환하여 반환한다.")
+    void checkGetWinningNumber() {
+        //given
+        List<Integer> winningNumber = new ArrayList<>(List.of(1,2,3,4,5,6));
+        int bonusNumber = 7;
+
+        //when
+        List<Integer> result = getWinningNumber(winningNumber, bonusNumber);
+
+        //then
+        assertThat(result).isEqualTo(List.of(1,2,3,4,5,6,7));
     }
 }
