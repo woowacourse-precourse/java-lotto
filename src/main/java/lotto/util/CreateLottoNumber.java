d3c1ac68377ed71fba63d1606f7baef9c2a27ff9
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-12 19:19:48 +0900

    feat: 로또 번호를 정렬시켜주는 메서드 추가

--- src/main/java/lotto/util/CreateLottoNumber.java
+++ src/main/java/lotto/util/CreateLottoNumber.java
@@ -1,12 +1,20 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class CreateLottoNumber {
-    public List<Integer> createLottoNumber(){
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+    public List<Integer> createLottoNumber() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return numbers;
+    }
+
+    private List<Integer> sortLottoNumber(List<Integer> numbers) {
+        List<Integer> sortNumbers = new ArrayList<>();
+        sortNumbers.addAll(numbers);
+        Collections.sort(sortNumbers);
+        return sortNumbers;
     }
 }
