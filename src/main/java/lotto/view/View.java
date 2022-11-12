c92ddb9eeaa83d55d5d772184576ca0f4fbba496
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-11-12 15:39:59 +0900

    feat: add print deposit view
    - 입금 요구 화면 출력 메소드 추가

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -7,4 +7,8 @@ public class View {
     public static String getInput() {
         return Console.readLine();
     }
+
+    public static void printDepositView() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
 }
