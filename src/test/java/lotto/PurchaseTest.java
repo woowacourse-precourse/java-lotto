1da3dce4debc1484bf783bd846305e47363db858
SHEOMM <esh0821@snu.ac.kr>
2022-11-13 01:29:03 +0900

    test: 구매 금액이 숫자가 아닌 경우 에러 테스트
    구매 금액이 숫자가 아닌 형태로 들어올 경우 IllegalArgumentException을 올바르게 출력하는지 테스트

---
+++ src/test/java/lotto/PurchaseTest.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.domain.PurchaseService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PurchaseTest extends NsTest {
+    @DisplayName("구매 금액이 숫자가 아닌 경우 에러 출력 테스트")
+    @Test
+    void 구매금액_숫자_아닌_경우_에러_테스트(){
+        String input="숫자아님";
+        PurchaseService purchaseService = new PurchaseService();
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> purchaseService.parsePurchaseInput(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
