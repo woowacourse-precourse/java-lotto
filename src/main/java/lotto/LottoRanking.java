dd0a194f537ed54c4fceebd074eb13ecce7c89c9
김형기 <khk211113@naver.com>
2022-11-12 23:14:23 +0900

    feat: 당첨번호와 비교하는 기능

---
+++ src/main/java/lotto/LottoRanking.java
@@ -0,0 +1,79 @@
+package lotto;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LottoRanking {
+
+    private final Lotto winningLotto;
+    private final Set<Integer> luckyNumbers;
+    private final int bonusNumber;
+
+    private LottoRanking(Lotto winningLotto, int bonusNumber) {
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+        this.luckyNumbers = new HashSet<>(winningLotto.getNumbers());
+        validateBonusNumber();
+    }
+
+    public LottoRank compare(Lotto selectedNumber) {
+        int sameNumbers = getSameNumbers(selectedNumber);
+        if (sameNumbers == 6) {
+            return LottoRank.FIRST;
+        }
+        if (sameNumbers == 5) {
+            if (haveBonusNumber(selectedNumber)) {
+                return LottoRank.SECOND;
+            }
+            return LottoRank.THIRD;
+        }
+        if (sameNumbers == 4) {
+            return LottoRank.FOURTH;
+        }
+        if (sameNumbers == 3) {
+            return LottoRank.FIFTH;
+        }
+        return LottoRank.LOSE;
+    }
+
+    private void validateBonusNumber() {
+        if (bonusNumber < 0 || bonusNumber > 45) {
+            throw new IllegalArgumentException(LottoRankingErrorMessages.INVALID_BONUS_NUMBER_RANGE.message);
+        }
+
+        if (luckyNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(LottoRankingErrorMessages.BONUS_NUMBER_DUPLICATED.message);
+        }
+    }
+    
+    private int getSameNumbers(Lotto selectedNumber) {
+        int result = 0;
+        for (int num : selectedNumber.getNumbers()) {
+            if (luckyNumbers.contains(num)) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    private boolean haveBonusNumber(Lotto selectedNumber) {
+        for (int num : selectedNumber.getNumbers()) {
+            if (num == this.bonusNumber) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    enum LottoRankingErrorMessages {
+        INVALID_BONUS_NUMBER_RANGE("보너스 숫자는 1 ~ 45 범위여야 합니다."),
+        BONUS_NUMBER_DUPLICATED("보너스 숫자와 중복되는 당첨번호가 존재합니다.");
+
+        private final String message;
+
+        LottoRankingErrorMessages(String message) {
+            this.message = message;
+        }
+    }
+
+}
