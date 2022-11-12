c7adc02ecbda767970f4d88d8fc6d788b88aea24
KangSungBum <peter9706@naver.com>
2022-11-12 16:33:37 +0900

    refactor(ErrorResponse.java) : 출력 형식 변경
    ErrorResponse 출력 형식 변경
    - Error -> ERROR 수정

--- src/main/java/lotto/ErrorResponse.java
+++ src/main/java/lotto/ErrorResponse.java
@@ -1,10 +1,11 @@
 package lotto;
 
 public enum ErrorResponse {
-    INPUT_LOTTO_RANGE_ERROR("[Error] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    INPUT_BUYING_RANGE_ERROR("[Error] 1000으로 나누어지는 단위의 구입 금액을 입력해야 합니다."),
-    INPUT_WITHOUT_COMMA_ERROR("[Error] 당첨번호는 쉽표(,)로 구분하여 입력해야 합니다."),
-    NOT_IN_WINNING_PLACE("[Error] 일치하는 등수에 존재하지 않습니다.")
+    INPUT_LOTTO_RANGE_ERROR("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INPUT_BUYING_RANGE_ERROR("[ERROR] 1000으로 나누어지는 단위의 구입 금액을 입력해야 합니다."),
+    INPUT_WITHOUT_COMMA_ERROR("[ERROR] 당첨번호는 쉽표(,)로 구분하여 입력해야 합니다."),
+    INPUT_CONTAINS_CHAR_ERROR("[ERROR] 입력은 문자를 포함할 수 없습니다."),
+    NOT_IN_WINNING_PLACE("[ERROR] 일치하는 등수에 존재하지 않습니다.")
     ;
 
     private String errorMessage;
