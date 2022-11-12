a6bf7f6007557d702fc40217211a780575d398a4
NoseSSG <dmlcks1998@naver.com>
2022-11-12 23:32:17 +0900

    [FEAT] 구입금액을 받아오고 숫자가 아닐경우 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
+    static int check_money(){
+        String input_money;
+        int money;
+        System.out.println("구입금액을 입력해 주세요");
+        input_money = Console.readLine();
+        if (input_money.isBlank()) throw new IllegalArgumentException();
+        try {
+            money = Integer.parseInt(input_money);
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException();
+        }
+        return money;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        check_money();
 
     }
 }
