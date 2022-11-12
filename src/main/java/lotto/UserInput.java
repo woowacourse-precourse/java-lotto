3b469aa6bc5134736a0c7bf24352bbff37adcd49
kevstevie <jianna442@gmail.com>
2022-11-12 14:38:41 +0900

    refactor: UserInput 클래스 static 제거하고 객체 생성해서 쓰도록 수정

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -10,16 +10,15 @@ import java.util.List;
 
 public class UserInput {
 
-    static List<List<Integer>> lotteries = new ArrayList<>();
-    private UserInput() {}
+    List<List<Integer>> lotteries = new ArrayList<>();
 
-    static public long inputMoney() {
+    public long inputMoney() {
         String input = Console.readLine();
         validateMoney(input);
         return Long.parseLong(input);
     }
 
-    static private void validateMoney(String input) {
+    private void validateMoney(String input) {
         try {
             long money = Long.parseLong(input);
             if (money % 1000 != 0 || money == 0) {
@@ -30,28 +29,28 @@ public class UserInput {
         }
     }
 
-    static void generateRandomLotto(long quantity) {
+    void generateRandomLotto(long quantity) {
         for (int i = 0; i < quantity; i++) {
-            List tmpLotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> tmpLotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             tmpLotto.sort(Comparator.naturalOrder());
             lotteries.add(tmpLotto);
         }
     }
 
-    static void printLotto() {
+    void printLotto() {
         Iterator it = lotteries.iterator();
         while (it.hasNext()) {
             System.out.println(it.next());
         }
     }
 
-    static Lotto inputWinningNumber() {
+    Lotto inputWinningNumber() {
         String winningNumber = Console.readLine();
         List numbers = validateLotto(winningNumber);
         return new Lotto(numbers);
     }
 
-    static private List validateLotto(String winningNumber) {
+    private List validateLotto(String winningNumber) {
         String[] splitInput = winningNumber.split(",");
         List<Integer> result = new ArrayList();
         validateLottoType(splitInput, result);
@@ -59,7 +58,7 @@ public class UserInput {
         return result;
     }
 
-    static private void validateLottoType(String[] splitInput, List result) {
+    private void validateLottoType(String[] splitInput, List result) {
         try {
             for (int i = 0; i < splitInput.length; i++) {
                 result.add(Integer.parseInt(splitInput[i]));
@@ -69,13 +68,13 @@ public class UserInput {
         }
     }
 
-    static int inputBonus() {
+    int inputBonus() {
         String bonus = Console.readLine();
         int result = validateBonus(bonus);
         return result;
     }
 
-    static private int validateBonus(String bonus) {
+    private int validateBonus(String bonus) {
         int result;
         try {
             result = Integer.parseInt(bonus);
