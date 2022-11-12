519bf905bb19a6a91371a8e228c603abad166724
sosow0212 <sosow0212@naver.com>
2022-11-12 13:21:55 +0900

    feat : Enum 클래스 안에 생성자와 레이블 추가

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -1,5 +1,32 @@
 package lotto.domain;
 
 public enum Winning {
-    THIRD, FOURTH, FIFTH, FIFTH_WITH_BONUS, SIXTH
+    FIND(1, 0),
+    THIRD(3, 5000),
+    FOURTH(4, 50000),
+    FIFTH(5, 1500000),
+    FIFTH_WITH_BONUS(7, 30000000),
+    SIXTH(6, 2000000000);
+
+    private final int label;
+    private final int price;
+
+    Winning(int label, int price) {
+        this.label = label;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Winning valueOf(int label) {
+        for (Winning winning : Winning.values()) {
+            if (label == winning.label) {
+                return winning;
+            }
+        }
+
+        return null;
+    }
 }
