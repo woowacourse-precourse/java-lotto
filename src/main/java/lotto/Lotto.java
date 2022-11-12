ed279f1639bd011b3d393f0c1ca000e9f62c7bfd
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-13 01:27:51 +0900

    feat : Lotto 객체를 생성한다. - Lotto 객체를 생성할 때, Lotto의 원소 개수가 6이고 중복된 원소가 없는지 검증한다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,36 @@
 package lotto;
 
+import org.assertj.core.util.Sets;
+
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private static final int LOTTO_SIZE = 6;
+    private static final String LOTTO_SIZE_IS_NOT_6 = "[ERROR] 로또 번호 개수가 6이 아닙니다.";
+    private static final String IS_DUPLICATED_LOTTO_NUMBER = "중복되는 로또 번호가 있습니다.";
+    private final List<LottoNumber> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
+    public Lotto(List<LottoNumber> numbers) {
+        validateLottoValue(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateLottoValue(List<LottoNumber> numbers) {
+        validateLottoSize(numbers);
+        validateIsNotDuplicatedNumberInLotto(numbers);
+    }
+
+    private void validateIsNotDuplicatedNumberInLotto(List<LottoNumber> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(LOTTO_SIZE_IS_NOT_6);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateLottoSize(List<LottoNumber> numbers) {
+        Set<LottoNumber> lottoNumbers = Sets.newHashSet(numbers);
+        if (lottoNumbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(IS_DUPLICATED_LOTTO_NUMBER);
+        }
+    }
 }
