f897fa20e953ad1faae5313aaaa0012721be98ef
jinny-l <jinny.l.dev@gmail.com>
2022-11-12 15:46:06 +0900

    feat(BonusNumber): 보너스 숫자 1개를 의미하는 객체 구현

---
+++ src/main/java/lotto/model/BonusNumber.java
@@ -0,0 +1,30 @@
+package lotto.model;
+
+import lotto.util.InputVerifier;
+import lotto.view.View;
+
+/*
+ * 보너스 숫자 1개를 의미하는 객체
+ */
+
+public class BonusNumber {
+
+    private final int number;
+
+    public BonusNumber(int number) {
+        validateNumberRange(number);
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    private void validateNumberRange(int number) {
+        if (!InputVerifier.isValidNumber(number)) {
+            View.printNotLottoNumber();
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
