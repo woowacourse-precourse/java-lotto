ff491ef92d1b35b38e66fe43ab8356fa1bda8607
lseok <lseokhyun99@gmail.com>
2022-11-12 22:47:43 +0900

    feat: 보너스 번호 입력 메소드 추가
    보너스 번호 입력 메소드
    bonusNumberInput() 기능 구현, 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -21,6 +21,7 @@ public class Application {
         List<Lotto> lotto = issueLotto(lottoCount);
 
         List<Integer> winningNumber = winningNumberInput();
+        Integer bonusNumber = bonusNumberInput();
     }
     public static int howMuchLottoBuy(){
         System.out.println("구입금액을 입력해 주세요.");
@@ -66,4 +67,13 @@ public class Application {
         return winningNumber;
     }
 
+    public static Integer bonusNumberInput(){
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+
+        String number = readLine();
+
+        Integer bonusNumber = Integer.valueOf(number);
+
+        return bonusNumber;
+    }
 }
