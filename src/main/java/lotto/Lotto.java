c8ece4c5cd0ab0b0ff2fb61ce986aa021451e0ee
rabbit0216 <rabbit1999k@gmail.com>
2022-11-12 16:30:29 +0900

    feat(lotto): 로또 번호 범위 체크 함수 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,12 +12,20 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
        sizeValidate(numbers);
-
+       inputValidate(numbers);
     }
 
     private void sizeValidate(List<Integer> numbers){
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[Error] 로또 번호 개수는 6개여야 합니다.");
+        }
+    }
+
+    private void inputValidate(List<Integer> numbers){
+        for(Integer number : numbers){
+            if(number < 1 || number > 45){
+                throw new IllegalArgumentException("[Error] 로또 번호는 1에서 45여야 합니다.");
+            }
         }
     }
 
