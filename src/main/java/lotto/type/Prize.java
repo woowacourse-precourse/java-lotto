561a1dbe99ae141117068b7f47cafaa7e2210c36
kimJaeHyeon <kmr2644@gmail.com>
2022-11-12 15:00:05 +0900

    feat: 당첨종류를 나타내는 Prize 구현

--- src/main/java/lotto/type/Prize.java
+++ src/main/java/lotto/type/Prize.java
@@ -1,2 +1,13 @@
-package lotto.type;public class Prize {
+package lotto.type;
+
+public enum Prize {
+    THREE("3개 일치"),
+    FOUR("4개 일치"),
+    FIVE("5개 일치"),
+    FIVE_AND_BONUS("5개 일치, 보너스 볼 일치"),
+    SIX("6개 일치");
+
+    Prize(String description) {
+
+    }
 }
