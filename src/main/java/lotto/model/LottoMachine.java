993dc5ebe18faf3e9f41b71620b4e6c21d9b0afc
Jimin <thswlals0219@gmail.com>
2022-11-12 23:08:00 +0900

    feat(LottoMachine): 로또 티켓을 생성하는 기능 구현

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -1,9 +1,16 @@
 package lotto.model;
 
+import lotto.Lotto;
 import lotto.util.constants.LottoConstants;
 
 public class LottoMachine {
     public int computeLottoTicketsCount(int purchaseAmount) {
         return purchaseAmount / LottoConstants.LOTTO_TICKET_PRICE;
+    }
+
+    public Lotto getLottoTicket() {
+        LottoRandom lottoRandom = new LottoRandom();
+        Lotto lotto = new Lotto(lottoRandom.pickNumbers());
+        return lotto;
     }
 }
