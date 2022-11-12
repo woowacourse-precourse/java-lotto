5fb2acded5473dbe5764c3edac7cfc84f8008d8d
LeeJaeHong <0915jhlee@daum.net>
2022-11-12 17:00:50 +0900

    1. 사용자 금액 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int lottoNum=userInputMoney();
+    }
+
+    private static int userInputMoney() {
+        String userMoney= readLine();
+        int lottoNum=Integer.parseInt(userMoney)/1000;
+        return lottoNum;
     }
 }
