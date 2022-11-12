ed20095cfac8a2146c140460366926c54e27d094
JUHA <khabh@naver.com>
2022-11-12 20:41:22 +0900

    feat(LotteryWheel): 보너스 번호 1개 뽑기
    LotteryWheel에서 Lotto 객체를 저장하도록 변경

--- src/main/java/lotto/domain/LotteryWheel.java
+++ src/main/java/lotto/domain/LotteryWheel.java
@@ -1,17 +1,25 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
 
 import java.util.List;
 
 public class LotteryWheel {
-    private final List<Integer> winningLotto;
+    private final static int MIN_LOTTO_NUMBER = 1;
+    private final static int MAX_LOTTO_NUMBER = 45;
+    private final static int NUMBERS_COUNT = 7;
 
-    public  LotteryWheel() {
-        winningLotto = createWinningLotteryNumbers();
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public LotteryWheel() {
+        List<Integer> lotteryNumbers = pickWinningLotteryNumbers();
+        this.bonusNumber = lotteryNumbers.remove(NUMBERS_COUNT);
+        this.winningLotto = new Lotto(lotteryNumbers);
     }
 
-    private List<Integer> createWinningLotteryNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    private List<Integer> pickWinningLotteryNumbers() {
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, NUMBERS_COUNT);
     }
 }
