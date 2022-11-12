35eb1fae704439181df9b3a651981382c40496bc
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-12 14:57:35 +0900

    feat: 보너스 번호의 유무에 따른 번호 필터 메소드 구현
    한 게임의 번호, 당첨 번호, 보너스 번호를 입력받아서 중복된 숫자를 제거

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,39 @@
 package lotto;
 
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+
+        System.out.println("금액 입력");
+        int lottoMoney = inputView.inputLottoPurchaseMoney();
+        int lottoChance = inputView.getLottoChance(lottoMoney);
+
+        List<Integer> winningNumbers = inputView.createWinningNumber();
+        int bonusNumber = inputView.inputBonusNumber();
+
+        inputView.allLottoNumber = inputView.createAllGameLottoNumber(lottoChance);
+        outputView.printLottoPurchaseCompleteMessage(lottoChance);
+        outputView.printLottoNumber(inputView.allLottoNumber);
+
+        for (int i = 0; i < inputView.allLottoNumber.size(); i++) {
+            inputView.compareWinningNumber(inputView.allLottoNumber.get(i),winningNumbers,bonusNumber);
+        }
+
+
+        System.out.println("당첨 번호 : " + winningNumbers);
     }
+
+
+
+
+
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,25 +2,33 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-import lotto.Validator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class InputView {
     public static int lottoMoney = 0;
     public final int PRICE_PER_LOTTO = 1000;
+    public final int LOTTO_NUMBER_SIZE = 6;
 
-    public List<Integer> result = new ArrayList<>();
+    List<Integer> totalRank = Arrays.asList(0, 0, 0, 0, 0);
+
     public List<List<Integer>> allLottoNumber = new ArrayList<>();
-    public int bonusNumber = 0;
 
 
-    Validator validator = new Validator();
+    public void validateMoneyInput(int money) {
+        if (money % 1000 != 0) {
+
+            throw new IllegalArgumentException("[ERROR]");
+        }
+    }
 
     public int inputLottoPurchaseMoney() {
         lottoMoney = Integer.parseInt(Console.readLine());
-        validator.validateMoneyInput(lottoMoney);
+        validateMoneyInput(lottoMoney);
         return lottoMoney;
     }
 
@@ -28,6 +36,21 @@ public class InputView {
         return lottoMoney / PRICE_PER_LOTTO;
     }
 
+    public List<Integer> createWinningNumber() {
+        String inputNumbers = Console.readLine();
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String value : inputNumbers.split(",")) {
+            winningNumbers.add(Integer.parseInt(value));
+        }
+        return winningNumbers;
+    }
+
+    // 보너스 숫자를 입력해 int 형으로 반환함
+    public int inputBonusNumber() {
+        return Integer.parseInt(Console.readLine());
+    }
+
+    // 한 게임의 로또 번호를 자동으로 생성해서 Integer List를 반환
     public List<Integer> createLottoOneGameNumber() {
         List<Integer> oneGameNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         oneGameNumbers.sort(Integer::compareTo);
@@ -42,34 +65,29 @@ public class InputView {
         return allLottoNumber;
     }
 
-    public void compareWinningNumber(List<Integer> lottoNumbers, List<Integer> winningNumbers) {
-        lottoNumbers.retainAll(winningNumbers);
-        lottoNumbers.size();// 몇개 같은 지 출력
-        if (lottoNumbers.size() == 3) {
-            // 5등
+
+    public void compareWinningNumber(List<Integer> lottoNumbers, List<Integer> winningNumbers, int bonusNumber) {
+
+        if(lottoNumbers.contains(bonusNumber)){ // 일단 여기에 걸리면 1등은 불가능
+            List <Integer> result = lottoNumbers.stream()
+                    .filter(lottoNumber -> winningNumbers.stream().noneMatch(Predicate.isEqual(lottoNumber)))
+                    .collect(Collectors.toList());
+
+            System.out.println("여기 보너스 넘버 걸린 리스트임" +result);
         }
-        if (lottoNumbers.size() == 4) {
-            // 4등
+        // 여기에 걸리면 5,4,3,1등을 할 수 있다
+        if(!lottoNumbers.contains(bonusNumber)){
+            List <Integer> result = lottoNumbers.stream()
+                    .filter(lottoNumber -> winningNumbers.stream().noneMatch(Predicate.isEqual(lottoNumber)))
+                    .collect(Collectors.toList());
+
+            System.out.println("여기 보너스 넘버 안 걸린 리스트임" +result);
         }
-        if (lottoNumbers.size() == 5) {
-            // 3등
-        }
-        if (lottoNumbers.size() == 5 && ) {
-            // 2등
-        }
+
+
+
     }
 
     // 당첨 번호를 입력하는 메소드
-    public List<Integer> createWinningNumber() {
-        String inputNumbers = Console.readLine();
-        List<Integer> winningNumbers = new ArrayList<>();
-        for (String value: inputNumbers.split(",")) {
-            winningNumbers.add(Integer.parseInt(value));
-        }
-        return winningNumbers;
-    }
 
-    public int inputBonusNumber() {
-        return Integer.parseInt(Console.readLine());
-    }
 }
