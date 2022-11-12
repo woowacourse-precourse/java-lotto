57bfe279d0e256a42161dfe4499a5211ebe6f7c5
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-12 23:55:34 +0900

    feat: statc value

---
+++ src/main/java/lotto/util/CountRandomLotto.java
@@ -0,0 +1,10 @@
+package lotto.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountRandomLotto {
+    private static final WinningNumber winningNumber = new WinningNumber();
+    private static final List<List<Integer>> myLotto=new ArrayList<>();
+
+}
