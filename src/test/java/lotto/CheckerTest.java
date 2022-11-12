9990c22dfe7b9e26a9651328d11cb868e8fb0b2c
swonny <wonny921@gmail.com>
2022-11-12 16:43:17 +0900

    test(EnumTest, CheckerTest): enum 사용 연습 테스트 구현, Checker 테스트 구현

--- src/test/java/lotto/CheckerTest.java
+++ src/test/java/lotto/CheckerTest.java
@@ -1,4 +1,14 @@
 package lotto;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class CheckerTest {
+    @DisplayName("로또번호와 당첨번호를 비교하여 당첨된 등수를 알 수 있다.")
+    @Test
+    void compareLottoNumbers() {
+        assertThat(Checker.getRank(5)).isEqualTo(PrizeRank.THIRD);
+    }
 }
--- src/test/java/study/EnumTest.java
+++ src/test/java/study/EnumTest.java
@@ -2,12 +2,14 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class EnumTest {
     @Test
     void enumGetValueIndexByStringTest() {
-        assertThat(PrizeRankIndex.getPrizeIndex("FIRST"))
+        assertThat(PrizeRankIndex.getPrizeIndex("FIRST").ordinal())
                 .isEqualTo(0);
     }
 
@@ -15,6 +17,38 @@ public class EnumTest {
     void enumGetValueIndexByValueTest() {
         assertThat(PrizeRankIndex.getPrizeIndex(PrizeRankIndex.FIRST))
                 .isEqualTo(0);
+    }
+
+    @Test
+    void enumWithHashMapTest() {
+        int FIRST_PRIZE_COUNT = 6;
+        int THIRD_PRIZE_COUNT = 5;
+        int FOURTH_PRIZE_COUNT = 4;
+        int FIFTH_PRIZE_COUNT = 3;
+
+        HashMap<Integer, PrizeRankIndex> countMatchingPrizes = new HashMap<>();
+        countMatchingPrizes.put(FIRST_PRIZE_COUNT, PrizeRankIndex.FIRST);
+        countMatchingPrizes.put(THIRD_PRIZE_COUNT, PrizeRankIndex.SECOND);
+        countMatchingPrizes.put(FOURTH_PRIZE_COUNT, PrizeRankIndex.THIRD);
+        countMatchingPrizes.put(FIFTH_PRIZE_COUNT, PrizeRankIndex.FOURTH);
+
+        assertThat(countMatchingPrizes.get(6)).isEqualTo(PrizeRankIndex.FIRST);
+    }
+
+    @Test
+    void enumWithHashMapIndexTest() {
+        int FIRST_PRIZE_COUNT = 6;
+        int THIRD_PRIZE_COUNT = 5;
+        int FOURTH_PRIZE_COUNT = 4;
+        int FIFTH_PRIZE_COUNT = 3;
+
+        HashMap<Integer, PrizeRankIndex> countMatchingPrizes = new HashMap<>();
+        countMatchingPrizes.put(FIRST_PRIZE_COUNT, PrizeRankIndex.FIRST);
+        countMatchingPrizes.put(THIRD_PRIZE_COUNT, PrizeRankIndex.SECOND);
+        countMatchingPrizes.put(FOURTH_PRIZE_COUNT, PrizeRankIndex.THIRD);
+        countMatchingPrizes.put(FIFTH_PRIZE_COUNT, PrizeRankIndex.FOURTH);
+
+        assertThat(countMatchingPrizes.get(6).ordinal()).isEqualTo(0);
     }
 }
 
@@ -31,11 +65,11 @@ enum PrizeRankIndex {
         this.ranks = ranks;
     }
 
-    public static int getPrizeIndex(String prizeRank) {
-        return PrizeRankIndex.FIRST.ordinal();
+    public static PrizeRankIndex getPrizeIndex(String prizeRank) {
+        return PrizeRankIndex.valueOf(prizeRank);
     }
 
     public static int getPrizeIndex(PrizeRankIndex prizeRank) {
-        return PrizeRankIndex.FIRST.ordinal();
+        return prizeRank.ordinal();
     }
 }
