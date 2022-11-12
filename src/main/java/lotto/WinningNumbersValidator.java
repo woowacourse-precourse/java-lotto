6da19a3aae7e5da9d749afe31a6c25c0fd376cd9
Cho SangHyun <98shcho@naver.com>
2022-11-13 01:13:07 +0900

    feat(당첨번호입력 예외처리): 각 번호가 유효범위가 아니면 오류를 내는 기능 추가

--- src/main/java/lotto/WinningNumbersValidator.java
+++ src/main/java/lotto/WinningNumbersValidator.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public class WinningNumbersValidator {
     private static int WINNING_NUMBERS_SIZE = 6;
+    private static int MIN_WINNING_NUMBER = 1;
+    private static int MAX_WINNING_NUMBER = 45;
     private static String SIX_NUMBERS_ERROR_MESSAGE = "입력된 값들이 6개가 아닙니다";
     private static String ALL_NUMERIC_ERROR_MESSAGE = "숫자들을 띄어쓰기없이 쉼표로 구분해서 입력해야 합니다";
+    private static String RANGE_ERROR_MESSAGE = String.format("%d ~ %d 사이의 숫자들로 입력해야 합니다",
+            MIN_WINNING_NUMBER, MAX_WINNING_NUMBER);
 
     private static boolean isSixNumbers(List<String> winningNumbers) {
         return winningNumbers.size() == WINNING_NUMBERS_SIZE;
@@ -31,10 +35,23 @@ public class WinningNumbersValidator {
         return true;
     }
 
+    private static boolean isInValidRange(List<String> winningNumbers) {
+        for (String number : winningNumbers) {
+            int num = Integer.parseInt(number);
+            if (num < MIN_WINNING_NUMBER || MAX_WINNING_NUMBER < num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void validate(String winningNumberInput) {
         List<String> winningNumbers = Arrays.asList(winningNumberInput.split(","));
         if (!isAllNumeric(winningNumbers)) {
             throw new IllegalArgumentException(ALL_NUMERIC_ERROR_MESSAGE);
+        }
+        if (!isInValidRange(winningNumbers)) {
+            throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
         }
         if (!isSixNumbers(winningNumbers)) {
             throw new IllegalArgumentException(SIX_NUMBERS_ERROR_MESSAGE);
