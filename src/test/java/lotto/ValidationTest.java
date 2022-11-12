314226373fdbedc2ad5593a7218acf09475823ce
KYR <freekyr7529@gmail.com>
2022-11-13 02:22:44 +0900

    teset : 테스트 케이스 추가
    validInputLength 메소드가 정상적으로 작동하는지 확인하는 테스트 케이스 추가
    validIsInputInRange 메소드가 정상적으로 작동하는지 확인하는 테스트 케이스 추가

--- src/test/java/lotto/ValidationTest.java
+++ src/test/java/lotto/ValidationTest.java
@@ -1,7 +1,10 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +20,23 @@ public class ValidationTest {
     @DisplayName("사용자가 입력한 수가 6자리로 이뤄져 있다.")
     @Test
     void inputAmountIsSix() {
+        List<Integer> input = new ArrayList<>();
+        input.add(1);
+        input.add(2);
+        input.add(3);
+        input.add(4);
+        input.add(5);
+        input.add(6);
 
+        assertThatCode(() -> validInput.validInputLength(input)).doesNotThrowAnyException();
+        input.remove(5);
+        assertThatThrownBy(() -> validInput.validInputLength(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사용자가 입력한 수가 범위 내에 있다.")
+    @Test
+    void isNumberInRange() {
+        assertThatCode(() -> validInput.validIsInputInRange(10)).doesNotThrowAnyException();
+        assertThatThrownBy(() -> validInput.validIsInputInRange(50)).isInstanceOf(IllegalArgumentException.class);
     }
 }
