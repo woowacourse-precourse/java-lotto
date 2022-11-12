79fefcfe020efb6aa32745b6fc45b353eb5b3e9d
phobyjun <phobyjun@gmail.com>
2022-11-12 19:50:48 +0900

    fix(User): fix division to moderation in validatePurchaseMoney

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -22,7 +22,7 @@ public class User {
             System.out.println("[ERROR] 로또 구입 금액은 0보다 크거나 같아야 합니다.");
             throw new IllegalArgumentException();
         }
-        if (purchaseMoney / 1000 != 0) {
+        if (purchaseMoney % 1000 != 0) {
             System.out.println("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");
             throw new IllegalArgumentException();
         }
