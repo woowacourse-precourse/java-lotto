9f1fa157fdb120f5d97b59c6a8ee82272047cdcc
hyejungg <kimhyejung12@naver.com>
2022-11-12 22:30:35 +0900

    feat: 입력값 데이터 타입 확인하는 함수 추가

--- src/main/java/lotto/ValidateUtils.java
+++ src/main/java/lotto/ValidateUtils.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class ValidateUtils {
     private static final String ERROR_MEESAGE_HEADER = "[ERROR] ";
 
+    private static final String VALIDATE_MONEY_TYPE = "금액은 숫자만 입력 가능합니다. ";
     private static final String VALIDATE_MONEY_UNIT = "금액은 1,000원 단위로만 입력 가능합니다. ";
     private static final String VALIDATE_DELIMITER = "당첨 번호는 구분자(',')를 이용하여 입력합니다.";
     private static final String VALIDATE_RANGE_ANSWER_NUMBER = "당첨 번호는 1 ~ 45 범위의 값만 입력합니다.";
@@ -12,6 +13,15 @@ public class ValidateUtils {
     private static final String VALIDATE_DUPLICATE_NUMBER = "중복된 당첨 번호는 입력할 수 없습니다.";
 
     private static final int MONEY_UNIT = 1000;
+
+    public static void checkTypeNumber(String money) {
+        for (int i = 0; i < money.length(); i++) {
+            if (!Character.isDigit(money.charAt(i))) {
+                System.out.println(ERROR_MEESAGE_HEADER + VALIDATE_MONEY_TYPE);
+                throw new IllegalArgumentException(String.format(ERROR_MEESAGE_HEADER + VALIDATE_MONEY_TYPE));
+            }
+        }
+    }
 
     public static void checkMoneyUnit(int money) {
         if (money % MONEY_UNIT != 0) {
