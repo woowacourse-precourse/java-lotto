db9a626d67eaeb8b6a224ca61506640c31a5de12
hodomaroo <enghemdwkd@gmail.com>
2022-11-13 01:45:35 +0900

    [Feat] : WinningLotto 클래스 구현(당첨 로또 정보 저장 클래스)
    입력으로 주어지는 보너스 숫자에 대한 검증 및
    
    로또 객체를 입력받아 그 등수를 Grade 객체로 반환하는 기능의 구현을
    완료하였습니다

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -1,9 +1,42 @@
 package lotto;
 
+import java.util.List;
+
 /**
  * 당첨 로또 정보를 관리하는 클래스
  */
 public class WinningLotto {
-    private Lotto lottoNumber;
+
+    private Lotto winningLotto;
     private Integer bonusNumber;
+
+    public WinningLotto(List<Integer> winningNumbers, Integer bonusNumber) {
+        Lotto winningLotto = new Lotto(winningNumbers);
+
+        validate(winningLotto, bonusNumber);
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(Lotto lotto, Integer bonusNumber) {
+        if (validateBonusNumberNotInLotto(lotto, bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호는 로또 번호와 중복되어서는 안됩니다.");
+        }
+
+        if (!validateBonusNumberInRange(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호는 [1,45] 사이의 값을 가져야 합니다.");
+        }
+    }
+
+    private boolean validateBonusNumberNotInLotto(Lotto lotto, Integer bonusNumber) {
+        return lotto.hasNumber(bonusNumber);
+    }
+
+    private boolean validateBonusNumberInRange(Integer bonusNumber) {
+        return 1 <= bonusNumber && bonusNumber <= 45;
+    }
+
+    public LottoGrade getCompareResult(Lotto lotto) {
+        return LottoGrade.getMatchGrade(winningLotto.getCountOfMatchNumber(lotto), lotto.hasNumber(bonusNumber));
+    }
 }
