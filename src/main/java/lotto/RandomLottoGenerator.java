1881c71ec8b788b1249454290bd5d121b9def81f
KimTaeKang57 <zizoen123@gmail.com>
2022-11-12 16:55:31 +0900

    feat: RandomLottoGenerator 클래스 추가

---
+++ src/main/java/lotto/RandomLottoGenerator.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RandomLottoGenerator {
+    private List<Integer> numbers;
+
+    public RandomLottoGenerator() {
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(String.format("[ERROR] 로또 번호 숫자가 6개가 아닙니다."));
+        }
+
+        Integer max = numbers.stream().max(Integer::compareTo).orElseThrow();
+        Integer min = numbers.stream().min(Integer::compareTo).orElseThrow();
+        if (min < 1 || max > 45) {
+            throw new IllegalArgumentException(String.format("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."));
+        }
+
+        Set<Integer> set = new HashSet<>(numbers);
+        if (set.size() != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
