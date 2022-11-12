6b0b87a78be1cae650ca1a4fd2309c0ef5dc24d3
csj <koreaxman@naver.com>
2022-11-12 17:12:49 +0900

    feat: create enter winning numbers and make exception

--- src/main/java/lotto/domain/Game.java
+++ src/main/java/lotto/domain/Game.java
@@ -3,15 +3,21 @@ package lotto.domain;
 public class Game {
     private static final String ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String BUY_SOMETHING = "개를 구매했습니다.";
+    private static final String ENTER_WINNING_LOTTO_NUMBER = "\n당첨 번호를 입력해 주세요.";
 
     Price price = new Price();
     NumberGenerator numberGenerator = new NumberGenerator();
+    WinningNumber winningNumber = new WinningNumber();
+    Lotto lotto;
 
     public void start() {
         askPrice();
         price.inputPurchaseAmount();
         showNumberOfPurchase();
         numberGenerator.showPurchaseLottery(getCount());
+        askWinningNumber();
+        String winningNumbers = winningNumber.inputWinningNumbers();
+        lotto = new Lotto(winningNumber.seperatedByComma(winningNumbers));
     }
 
     public void askPrice() {
@@ -22,6 +28,10 @@ public class Game {
         int count = getCount();
         System.out.println();
         System.out.println(count + BUY_SOMETHING);
+    }
+
+    public void askWinningNumber() {
+        System.out.println(ENTER_WINNING_LOTTO_NUMBER);
     }
 
     public int getCount() {
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,46 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
 public class Lotto {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateNumbers(numbers);
+        validateDuplicateNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 입력된 로또 번호가 6자리가 아닙니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateNumbers(List<Integer> winningNumbers) {
+        for (int i = 0; i < winningNumbers.size(); i++) {
+            if ((winningNumbers.get(i) < MIN_NUMBER)
+                    || (winningNumbers.get(i) > MAX_NUMBER)) {
+                throw new IllegalArgumentException("[ERROR] 입력된 로또 번호가 1 ~ 45 사이가 아닙니다.");
+            }
+        }
+    }
+
+    private void validateDuplicateNumbers(List<Integer> winningNumbers) {
+        int count=0;
+        for (int i = 0; i < winningNumbers.size(); i++) {
+            validateDuplicateNumberFor(winningNumbers, i);
+        }
+    }
+
+    private void validateDuplicateNumberFor(List<Integer> winningNumbers, int i) {
+        for (int j = i + 1; j < winningNumbers.size(); j++) {
+            if (winningNumbers.get(i).equals(winningNumbers.get(j))) {
+                throw new IllegalArgumentException("[ERROR] 입력된 로또 번호 중 중복되는 숫자가 있습니다.");
+            }
+        }
+    }
 }
