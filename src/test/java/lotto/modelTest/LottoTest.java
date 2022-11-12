bd28f528430cd1010a96426dbc22f44736a4bb80
gomljo <gomljo0307@naver.com>
2022-11-13 02:09:56 +0900

    test(LottoTest.java): 로또 번호 중복이 존재할 때 확인하는 기능 검증

--- src/test/java/lotto/modelTest/LottoTest.java
+++ src/test/java/lotto/modelTest/LottoTest.java
@@ -14,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTest {
     private static final String WRONG_QUANTITY_ERROR_MESSAGE="[ERROR] 로또 번호의 수가 6개가 아닙니다. 프로그램을 종료합니다.";
+    private static final String DUPLICATE_ERROR_MESSAGE="[ERROR] 숫자가 중복되었습니다. 프로그램을 종료합니다.";
 
     @Test
     public void 로또_번호가_5개인_경우_예외_확인하기(){
@@ -24,5 +25,10 @@ public class LottoTest {
     public void 로또_번호가_7개인_경우_예외_확인하기(){
         List<Integer> lotto = List.of(1,2,3,4,5,6,7);
         assertThatThrownBy(()->new Lotto(lotto)).isInstanceOf(IllegalArgumentException.class).hasMessageContaining(WRONG_QUANTITY_ERROR_MESSAGE);
+    }
+    @Test
+    public void 로또_번호가_중복된_경우_예외_확인하기(){
+        List<Integer> lotto = List.of(1,2,3,4,6,6);
+        assertThatThrownBy(()->new Lotto(lotto)).isInstanceOf(IllegalArgumentException.class).hasMessageContaining(DUPLICATE_ERROR_MESSAGE);
     }
 }
