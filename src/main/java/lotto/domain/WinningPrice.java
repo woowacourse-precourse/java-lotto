803cbc55ad20f3e4d64d2fad917257c0d3348156
devyourown <hyojoon56@gmail.com>
2022-11-13 01:26:30 +0900

    feature : complete making output

--- src/main/java/lotto/domain/WinningPrice.java
+++ src/main/java/lotto/domain/WinningPrice.java
@@ -1,19 +1,38 @@
 package lotto.domain;
 
+import java.util.List;
+
 public enum WinningPrice {
-   FIRST(2000000000),
-    SECOND(30000000),
-    THIRD(1500000),
-    FOURTH(50000),
-    FIFTH(5000);
+   FIRST("2,000,000,000원", 6, false),
+    SECOND("30,000,000원", 5, true),
+    THIRD("1,500,000원", 5, false),
+    FOURTH("50,000원", 4, false),
+    FIFTH("5,000원", 3, false);
 
-    private final int price;
+    private final String price;
+    private final int accordedNumber;
+    private final boolean shouldHitBonus;
 
-    WinningPrice(final int price) {
+    WinningPrice(final String price, int accordedNumber, boolean shouldHitBonus) {
        this.price = price;
+       this.accordedNumber = accordedNumber;
+       this.shouldHitBonus = shouldHitBonus;
     }
 
-    public int getPrice() {
+    public String getPrice() {
         return this.price;
+    }
+    public int getAccordedNumber() { return this.accordedNumber; }
+    public boolean shouldHitBonus() { return this.shouldHitBonus; }
+
+    public int countMatchResult(List<Result> results) {
+        return (int) results.stream()
+                .filter(result -> isSameAsThis(result))
+                .count();
+    }
+
+    private boolean isSameAsThis(Result result) {
+        return result.getAccordedNumbers() == accordedNumber &&
+                result.isHitBonus() == shouldHitBonus;
     }
 }
---
+++ src/main/java/lotto/io/ConsoleOutput.java
@@ -0,0 +1,55 @@
+package lotto.io;
+
+import lotto.domain.Result;
+import lotto.domain.WinningPrice;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class ConsoleOutput {
+    public static final int PURCHASE_AMOUNT = 1;
+    public static final int WINNING_NUMBER = 2;
+    public static final int BONUS_NUMBER = 3;
+
+    public static void printGuide(int guideNumber) {
+        if (guideNumber == PURCHASE_AMOUNT)
+            System.out.println("구입금액을 입력해 주세요.");
+        else if (guideNumber == WINNING_NUMBER)
+            System.out.println("당첨 번호를 입력해 주세요.");
+        else if (guideNumber == BONUS_NUMBER)
+            System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public static void printNumOfLotto(int numOfLotto) {
+        System.out.println(numOfLotto + "개를 구매했습니다.");
+    }
+
+    public static void printLottoNumber(List<Integer> numbers) {
+        System.out.print("[");
+        IntStream.range(0, numbers.size())
+                .forEach(index -> {
+                    System.out.print(numbers.get(index));
+                    if (index != numbers.size() - 1)
+                        System.out.print(", ");
+                });
+        System.out.println("]");
+    }
+
+    public static void printWinningStatistics(List<Result> results, double profit) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (WinningPrice winningPrice : WinningPrice.values()) {
+            printWinningPrice(winningPrice);
+            System.out.println(winningPrice.countMatchResult(results) + "개");
+        }
+        System.out.printf("총 수익률은 %.1f%입니다.\n", profit);
+    }
+
+    private static void printWinningPrice(WinningPrice winningPrice) {
+        System.out.print(winningPrice.getAccordedNumber() + "개 일치 ");
+        if (winningPrice.shouldHitBonus()) {
+            System.out.println(", 보너스 볼 일치 ");
+        }
+        System.out.print("(" + winningPrice.getPrice() + ") - ");
+    }
+}
