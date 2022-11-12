3ef78550b0034f66620826e2a7c58733aae20e7a
SHEOMM <esh0821@snu.ac.kr>
2022-11-12 20:42:07 +0900

    refactor: purchaseView의 메소드 이름 가독성 향상
    인스턴스의 변수 이름과 함께 메소드 이름이 출력되는 특성 상 직관성을 위해 불필요한 네이밍 제거

--- src/main/java/lotto/ui/PurchaseView.java
+++ src/main/java/lotto/ui/PurchaseView.java
@@ -3,19 +3,23 @@ package lotto.ui;
 import camp.nextstep.edu.missionutils.Console;
 
 public class PurchaseView implements View{
-    String purchaseInput;
+    private String purchaseNumbers;
 
+    @Override
     public void makeView(){
-        purchasePrint();
-        getPurchaseInput();
+        print();
+        getInput();
     }
 
-    private void purchasePrint(){
+    private void print(){
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    private void getPurchaseInput(){
-        purchaseInput = Console.readLine();
+    private void getInput(){
+        purchaseNumbers = Console.readLine();
     }
 
+    public String getPurchaseNumbers() {
+        return purchaseNumbers;
+    }
 }
