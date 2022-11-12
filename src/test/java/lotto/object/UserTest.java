5e2093652afce9a4191a7be1116aa1eff2c0384d
leewoojin <zmfpdl64@naver.com>
2022-11-12 14:43:42 +0900

    Test: User객체 복권 발행 갯수 확인
    1. 내가 구매한 복권 갯수와 일치하는가

--- src/test/java/lotto/object/UserTest.java
+++ src/test/java/lotto/object/UserTest.java
@@ -1,4 +1,10 @@
 import static org.junit.jupiter.api.Assertions.*;
 class UserTest {
   
+    @Test
+    void create_Lottery_nums() {
+        User user = new User();
+        assertThat(user.TestReturnSize()).isEqualTo(6);
+    }
+
 }
