4e32b7fe6cdff5de0f950995d66dc0d873600f31
kevstevie <jianna442@gmail.com>
2022-11-12 14:40:40 +0900

    refactor: UserInput 클래스 수정에 맞춰서 수정

--- src/test/java/lotto/UserInputTest.java
+++ src/test/java/lotto/UserInputTest.java
@@ -1,10 +1,10 @@
 package lotto;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -12,67 +12,70 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class UserInputTest {
     final private String ERROR = "[ERROR]";
 
-    @AfterEach
-    void 구매_로또_초기화() {
-        UserInput.lotteries.clear();
-    }
     @Test
     void 돈_입력_예외_1() {
+        UserInput user = new UserInput();
         String input = "1100";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-        assertThatThrownBy(() -> UserInput.inputMoney())
+        assertThatThrownBy(() -> user.inputMoney())
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 돈_입력_예외_2() {
+        UserInput user = new UserInput();
         String input = "NotaNumber";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-        assertThatThrownBy(() -> UserInput.inputMoney())
+        assertThatThrownBy(() -> user.inputMoney())
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 돈_입력_테스트() {
+        UserInput user = new UserInput();
         String input = "2000";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-        long a = UserInput.inputMoney();
+        long a = user.inputMoney();
         assertThat(a).isEqualTo(2000);
     }
 
     @Test
     void 로또_구매_생성() {
-        UserInput.generateRandomLotto(3);
+        UserInput user = new UserInput();
+        user.generateRandomLotto(3);
         int lottoSize = 3;
-        assertThat(UserInput.lotteries.size()).isEqualTo(lottoSize);
+        assertThat(user.lotteries.size()).isEqualTo(lottoSize);
     }
 
     @Test
     void 로또_구매_생성_2() {
-        UserInput.generateRandomLotto(1);
+        UserInput user = new UserInput();
+        user.generateRandomLotto(1);
         int lottoSize = 6;
-        assertThat(UserInput.lotteries.get(0).size()).isEqualTo(lottoSize);
+        assertThat(user.lotteries.get(0).size()).isEqualTo(lottoSize);
     }
 
     @Test
     void 당첨_번호_입력_예외_1() {
+        UserInput user = new UserInput();
         String input = "나, 5, 6, 7, 가";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-        assertThatThrownBy(() -> UserInput.inputWinningNumber())
+        assertThatThrownBy(() -> user.inputWinningNumber())
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 예외_메시지() {
+        UserInput user = new UserInput();
         String input = "1000j";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
-        assertThatThrownBy(() -> UserInput.inputMoney())
+        assertThatThrownBy(() -> user.inputMoney())
                 .hasMessageContaining("[ERROR]")
                 .isInstanceOf(IllegalArgumentException.class);
     }
