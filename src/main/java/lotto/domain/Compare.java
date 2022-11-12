c117d20de7626eb609a73eff9537c729e1503bf0
nswon <swnam0729@gmail.com>
2022-11-12 19:04:50 +0900

    feat: 로또기계 행위 메서드 추가

--- src/main/java/lotto/domain/Compare.java
+++
@@ -1,8 +0,0 @@
-package lotto.domain;
-
-public class Compare {
-
-    public void lottoNumbers() {
-
-    }
-}
---
+++ src/main/java/lotto/domain/Machine.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+public class Machine {
+
+    public void compareLottoNumbers() {
+
+    }
+
+    public void calculateRateOfReturn() {
+
+    }
+}
