175f39718bfdfc9a73d2b0ada67ac9529d0a6766
woo-chang <clllickme@naver.com>
2022-11-12 21:56:26 +0900

    refactor(LottoApplication): 메서드를 적절한 단위로 분리

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -6,41 +6,55 @@ import lotto.entity.Result;
 import lotto.entity.WinningLotto;
 import lotto.service.InputService;
 import lotto.service.OutputService;
-import lotto.util.StringToNumberConverter;
-import lotto.util.StringToNumbersConverter;
+import lotto.util.NumericConverter;
+import lotto.util.MultipleNumericConverter;
 
 public class LottoApplication {
 
     public final static int COUNT = 6;
+    public final static int PRICE = 1000;
     public final static int RANGE_START = 1;
     public final static int RANGE_END = 45;
-    public final static int PRICE = 1000;
 
-    private final StringToNumberConverter stringToNumberConverter;
-    private final StringToNumbersConverter stringToNumbersConverter;
+    private final NumericConverter numericConverter;
+    private final MultipleNumericConverter multipleNumericConverter;
 
     public LottoApplication() {
-        stringToNumberConverter = new StringToNumberConverter();
-        stringToNumbersConverter = new StringToNumbersConverter();
+        numericConverter = new NumericConverter();
+        multipleNumericConverter = new MultipleNumericConverter();
     }
 
     public void play() {
         try {
-            String purchaseInput = InputService.readPurchaseAmount();
-            Integer purchase = stringToNumberConverter.convert(purchaseInput);
+            int purchase = getPurchase();
             Consumer consumer = new Consumer(purchase);
-            String winningNumberInput = InputService.readWinningNumber();
-            Lotto winningNumberLotto = new Lotto(
-                stringToNumbersConverter.convert(winningNumberInput));
-            String bonusInput = InputService.readBonusNumber();
-            Integer bonus = stringToNumberConverter.convert(bonusInput);
+            Lotto winningNumberLotto = getWinningNumberLotto();
+            int bonus = getBonus();
             WinningLotto winningLotto = new WinningLotto(winningNumberLotto, bonus);
-            Result result = consumer.confirmResultOfLottos(winningLotto);
-            OutputService.printResult(result);
-            OutputService.printRateOfReturn(result.calculateRateOfReturn());
+            printResult(consumer.confirmResultOfLottos(winningLotto));
         } catch (IllegalArgumentException e) {
             OutputService.printErrorMessage(e.getMessage());
         }
+    }
+
+    private int getPurchase() {
+        String purchaseInput = InputService.readPurchaseAmount();
+        return numericConverter.convert(purchaseInput);
+    }
+
+    private Lotto getWinningNumberLotto() {
+        String winningNumberInput = InputService.readWinningNumber();
+        return new Lotto(multipleNumericConverter.convert(winningNumberInput));
+    }
+
+    private int getBonus() {
+        String bonusInput = InputService.readBonusNumber();
+        return numericConverter.convert(bonusInput);
+    }
+
+    private void printResult(Result result) {
+        OutputService.printResult(result);
+        OutputService.printRateOfReturn(result.calculateRateOfReturn());
     }
 
 }
