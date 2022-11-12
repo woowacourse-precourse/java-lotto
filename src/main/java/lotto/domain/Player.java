af2d7937d4bef1fe9533abd5221483ad92908f3b
heesang <dlagmltkd12@naver.com>
2022-11-13 02:57:20 +0900

    refactor: Player 내 검증 로직 PlayerValidationUtils로 분리

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -8,16 +8,13 @@ import java.util.stream.Collectors;
 public class Player {
 
     public static final int LOTTO_PRICE = 1000;
-    public static final String ERROR_NOT_VALID_PRICE = "[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다.";
-    public static final int CRITERION_ZERO = 0;
-    public static final String ERROR_PRICE_COUNT_NOT_EQUAL_LOTTO_SIZE = "[ERROR] 주어진 금액과 주어진 로또 번호의 개수가 일치하지 않습니다.";
 
     private Lottos lottos;
     private int purchasePrice;
 
     public Lottos purchaseLottos(int purchasePrice, List<List<Integer>> lottoNumbers) {
         int purchaseLottoCount = calculateLottoCount(purchasePrice);
-        validateSize(purchaseLottoCount, lottoNumbers.size());
+        PlayerValidationUtils.validateSize(purchaseLottoCount, lottoNumbers.size());
 
         lottos = new Lottos(createLottos(lottoNumbers));
         this.purchasePrice = purchasePrice;
@@ -25,28 +22,8 @@ public class Player {
     }
 
     public int calculateLottoCount(int purchasePrice) {
-        validateCanPurchase(purchasePrice);
+        PlayerValidationUtils.validateCanPurchase(purchasePrice);
         return purchasePrice / LOTTO_PRICE;
-    }
-
-    private void validateCanPurchase(int purchasePrice) {
-        if (!isDivided(purchasePrice)) {
-            throw new IllegalArgumentException(ERROR_NOT_VALID_PRICE);
-        }
-    }
-
-    private boolean isDivided(int purchasePrice) {
-        return ((purchasePrice % LOTTO_PRICE) == CRITERION_ZERO);
-    }
-
-    private void validateSize(int buyLottoCount, int lottoNumbersSize) {
-        if (!isSame(buyLottoCount, lottoNumbersSize)) {
-            throw new IllegalArgumentException(ERROR_PRICE_COUNT_NOT_EQUAL_LOTTO_SIZE);
-        }
-    }
-
-    private boolean isSame(int count, int lottoNumbersSize) {
-        return count == lottoNumbersSize;
     }
 
     private List<Lotto> createLottos(List<List<Integer>> lottoNumbers) {
---
+++ src/main/java/lotto/domain/PlayerValidationUtils.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+public class PlayerValidationUtils {
+
+    public static final String ERROR_NOT_VALID_PRICE = "[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다.";
+    public static final String ERROR_PRICE_COUNT_NOT_EQUAL_LOTTO_SIZE = "[ERROR] 주어진 금액과 주어진 로또 번호의 개수가 일치하지 않습니다.";
+    public static final int CRITERION_ZERO = 0;
+
+    public static void validateCanPurchase(int purchasePrice) {
+        if (!isDivided(purchasePrice)) {
+            throw new IllegalArgumentException(ERROR_NOT_VALID_PRICE);
+        }
+    }
+
+    private static boolean isDivided(int purchasePrice) {
+        return ((purchasePrice % Player.LOTTO_PRICE) == CRITERION_ZERO);
+    }
+
+    public static void validateSize(int buyLottoCount, int lottoNumbersSize) {
+        if (!isSame(buyLottoCount, lottoNumbersSize)) {
+            throw new IllegalArgumentException(ERROR_PRICE_COUNT_NOT_EQUAL_LOTTO_SIZE);
+        }
+    }
+
+    private static boolean isSame(int count, int lottoNumbersSize) {
+        return count == lottoNumbersSize;
+    }
+}
