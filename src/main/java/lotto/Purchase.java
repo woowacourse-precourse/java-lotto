9fd82c8e48cebe9aa65f873589d3e2467b8de607
Amyhds <dasomhan524@gmail.com>
2022-11-12 11:35:32 +0900

    feat(purchase): 로또 구매 장수 반환 메서드 구현

--- src/main/java/lotto/Purchase.java
+++ src/main/java/lotto/Purchase.java
@@ -3,7 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Purchase {
-    private final int price;
+    public final int price;
 
     public Purchase(int price) {
         validateDivision(price);
@@ -14,5 +14,9 @@ public class Purchase {
         if (price % 1000 != 0) {
             throw new IllegalArgumentException(LottoMessage.PRICE_ERROR_MESSAGE.getMessage());
         }
+    }
+
+    public int calculateCount() {
+        return (price/1000);
     }
 }
