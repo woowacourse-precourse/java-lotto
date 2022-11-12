298777d0d86aa5e417f7f965dbb35c9f518d454f
jeongjihun <wlgns2234@naver.com>
2022-11-13 00:00:46 +0900

    feat(lotto): 중복된 숫자가 들어올 경우 예외 던지기 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,10 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+        if(numbers.size() != numbers.stream().distinct().count()){
+            throw new IllegalArgumentException();
+        }
     }
 
     public List<Integer> get() {
