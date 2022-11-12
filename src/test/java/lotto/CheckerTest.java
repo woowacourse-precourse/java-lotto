9fdb653b5ec5b53a19aa9ee843ddecf311bf3884
swonny <wonny921@gmail.com>
2022-11-12 20:38:07 +0900

    test(CheckerTest): 당첨 금액 저장 해시맵을 초기화하는 메소드 테스트 추가

--- src/test/java/lotto/CheckerTest.java
+++ src/test/java/lotto/CheckerTest.java
@@ -3,7 +3,6 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -18,7 +17,7 @@ public class CheckerTest {
     @DisplayName("당첨번호와 일치하는 번호의 개수를 알 수 있다.")
     @Test
     void countSameNumbersTest() {
-        LottoKiosk.winningNumbers = List.of(1, 2, 3, 7, 8, 9);
+        Kiosk.winningNumbers = List.of(1, 2, 3, 7, 8, 9);
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         assertThat(Checker.countSameNumbers(lotto)).isEqualTo(3);
     }
@@ -26,13 +25,35 @@ public class CheckerTest {
     @DisplayName("당첨번호와 로또 번호를 비교한다.")
     @Test
     void compareLottoTest() {
-        LottoKiosk.winningNumbers = List.of(1, 2, 3, 7, 8, 9);
+        Kiosk.winningNumbers = List.of(1, 2, 3, 7, 8, 9);
         List<Lotto> lottos = List.of(
                 new Lotto(List.of(1, 2, 3, 4, 5, 6)),
                 new Lotto(List.of(3, 4, 5, 6, 7, 8)),
                 new Lotto(List.of(1, 2, 3, 7, 8, 9))
         );
         Checker.compare(lottos);
-        assertThat(LottoKiosk.resultStatistics).isEqualTo(new Integer[]{1, 0, 0, 0, 2, 0});
+        assertThat(Kiosk.resultStatistics).isEqualTo(new Integer[]{1, 0, 0, 0, 2});
+    }
+
+    @DisplayName("2등 당첨 확인")
+    @Test
+    void winSecondPrizeTest() {
+        Kiosk.winningNumbers = List.of(1, 2, 3, 7, 8, 9);
+        Kiosk.bonusNumber = 10;
+        List<Lotto> lottos = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(3, 4, 5, 6, 7, 8)),
+                new Lotto(List.of(1, 2, 3, 7, 8, 9)),
+                new Lotto(List.of(1, 2, 3, 7, 8, 10))
+        );
+        Checker.compare(lottos);
+        assertThat(Kiosk.resultStatistics).isEqualTo(new Integer[]{1, 1, 0, 0, 2});
+    }
+
+    @DisplayName("당첨 등수에 따른 당첨금 저장 해시맵 검사")
+    @Test
+    void winningsHashMapTest() {
+        Checker checker = new Checker();
+        assertThat(checker.initializeWinnings()).isEqualTo("");
     }
 }
