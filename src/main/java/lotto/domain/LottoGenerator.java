47717aab75871c499e8037489f425ad4ced2e515
YE-EUN <musiclove529@naver.com>
2022-11-13 06:42:58 +0900

    feat(LottoGenerator) : generate() 구현
    - 사용자가 구매한 금액만큼 로또 생성하는 기능 구현

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,11 +1,16 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
     static final int LOTTO_SIZE = 6;
     static final int LOTTO_PRICE = 1000;
+    static final int LOTTO_NUMBER_RANGE_START = 1;
+    static final int LOTTO_NUMBER_RANGE_END = 45;
 
     private final int numberOfLotto;
     private final List<Lotto> lottos;
@@ -16,7 +21,15 @@ public class LottoGenerator {
     }
 
     private List<Lotto> generate(int numberOfLotto){
-        return Collections.EMPTY_LIST;
+        List<Lotto> lottos = new ArrayList<>();
+
+        for(int i = 0;i<numberOfLotto;i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_RANGE_START, LOTTO_NUMBER_RANGE_END, LOTTO_SIZE);
+            Collections.sort(numbers);
+            lottos.add(new Lotto(numbers));
+        }
+
+        return lottos;
     }
 
     public int getNumberOfLotto() {
