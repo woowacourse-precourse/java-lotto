fca48f1ee32ed34630c11ff5f872de2b682a0645
jinny-l <jinny.l.dev@gmail.com>
2022-11-12 16:03:41 +0900

    refactor(Application): Controller에서 예외 처리함에 따라 어플리케이션 구현 코드 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,10 @@
 package lotto;
 
 public class Application {
-    public static void main(String[] args) throws IllegalArgumentException {
+    public static void main(String[] args) {
 
-        try {
-            Controller controller = new Controller();
-            controller.run();
-        } catch (IllegalArgumentException ie) {
-            return;
-        }
-        
+        Controller controller = new Controller();
+        controller.run();
+
     }
 }
