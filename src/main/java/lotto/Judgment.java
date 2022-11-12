adffe2949cf36b5d171a5b814daf80e26ca82be6
jeongjihun <wlgns2234@naver.com>
2022-11-12 16:35:14 +0900

    feat(lotto): 당첨번호와 일치하는 숫자 개수찾기 기능 구현

--- src/main/java/lotto/Judgment.java
+++ src/main/java/lotto/Judgment.java
@@ -3,7 +3,15 @@ package lotto;
 import java.util.List;
 
 public class Judgment {
+    private final int LOTTO_NUM = 6;
+
     public int correctCount(List<Integer> winningLotto, List<Integer> userLotto) {
-        return 3;
+        int count = 0;
+        for (int i = 0; i < LOTTO_NUM; i++) {
+            if (winningLotto.contains(userLotto.get(i))) {
+                count++;
+            }
+        }
+        return count;
     }
 }
