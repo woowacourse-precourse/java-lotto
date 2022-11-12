fd441a4305ac942215ad21c868c910547f796559
Isu Shin <isu010614@naver.com>
2022-11-12 23:39:56 +0900

    feat: 보너스 번호 입력받는 기능 구현
    예외처리
    - 당첨번호와 중복된 숫자인 경우

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,7 +10,7 @@ import static java.util.stream.Collectors.toList;
 
 public class Input {
     private static final int LOTTO_SIZE = 6;
-    private static final int BONUS_NUMBER = 1;
+    private static final int BONUS_NUMBER_SIZE = 1;
     private static final int START_NUMBER = 1;
     private static final int END_NUMBER = 45;
     private static final String ENTER_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
@@ -25,7 +26,7 @@ public class Input {
         System.out.println(ENTER_WINNING_NUMBER_MESSAGE);
         try {
             List<Integer> winningNumber = stringToList(Console.readLine());
-            validateSize(winningNumber);
+            validateSize(winningNumber, LOTTO_SIZE);
             validateDuplicate(winningNumber);
             validateNumberRange(winningNumber);
             return winningNumber;
@@ -34,7 +35,21 @@ public class Input {
         }
     }
 
-    public static int getNumber(){
+    public static List<Integer> getBonusNumber(List<Integer> winningNumber) {
+        System.out.println(ENTER_BONUS_NUMBER_MESSAGE);
+        try {
+            int number = getNumber();
+            List<Integer> bonusNumber = new ArrayList<>(number);
+            validateDuplicateWinning(winningNumber, number);
+            validateSize(bonusNumber, BONUS_NUMBER_SIZE);
+            validateNumberRange(bonusNumber);
+            return bonusNumber;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] Only enter numbers.");
+        }
+    }
+
+    public static int getNumber() {
         try {
             String number = Console.readLine();
             return Integer.parseInt(number);
@@ -49,8 +64,8 @@ public class Input {
                 .collect(toList());
     }
 
-    private static void validateSize(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_SIZE) {
+    private static void validateSize(List<Integer> numbers, int size) {
+        if (numbers.size() != size) {
             throw new IllegalArgumentException("[ERROR] Must enter 6 lottery winning numbers.");
         }
     }
@@ -74,4 +89,9 @@ public class Input {
         return number < START_NUMBER || number > END_NUMBER;
     }
 
+    private static void validateDuplicateWinning(List<Integer> winningNumber, int bonusNumber) {
+        if (winningNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] Must enter a number that does not overlap with the winning number.");
+        }
+    }
 }
