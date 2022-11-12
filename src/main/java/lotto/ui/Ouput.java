b334196c4aa7106943c1cc19209441c7633ec622
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-12 21:56:42 +0900

    refactor: 문자열 상수화 및 enum 추가

--- src/main/java/lotto/ui/Ouput.java
+++ src/main/java/lotto/ui/Ouput.java
@@ -1,43 +1,60 @@
 package lotto.ui;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import lotto.domain.Lotto;
 
 public class Ouput {
+    static final String PURCAHSED_N_LOTTOS_NOTICE = "%d개를 구매했습니다.\n";
+    static final String ENTER_AMOUNT_OF_MONEY_NOTICE = "구입금액을 입력해 주세요.";
+    static final String ENTER_WINNNG_LOTTO_NUMBERS_NOTICE = "당첨 번호를 입력해 주세요.";
+    static final String ENTER_BOUNUS_NUMBER_NOTICE = "보너스 번호를 입력해 주세요.";
+    static final String RATE_OF_RETURN_IS_N_NOTICE = "총 수익률은 %.1f%%입니다.\n";
+    static final String WIN_STATISTICS_NOTICE = "당첨 통계";
+    static final String LINE_NOTICE = "---";
+
+    enum PlaceNotice {
+        FIRST("6개 일치 (2,000,000,000원) - 0개\n"),
+        SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n"),
+        THIRD("5개 일치 (1,500,000원) - 0개\n"),
+        FOURTH("4개 일치 (50,000원) - %d개\n"),
+        FIFTH("3개 일치 (5,000원) - %d개\n");
+        private final String notice;
+
+        PlaceNotice(String string) {
+            notice = string;
+        }
+    }
+
     public void notifyPurchasedLottoTable(List<Lotto> lottoTable) {
-        System.out.printf("%d개를 구매했습니다.\n", lottoTable.size());
-        for (int i = 0; i < lottoTable.size(); i++) {
-            System.out.println(lottoTable.get(i).getNumbers());
+        System.out.printf(PURCAHSED_N_LOTTOS_NOTICE, lottoTable.size());
+        for (int order = 0; order < lottoTable.size(); order++) {
+            System.out.println(lottoTable.get(order).getNumbers());
         }
     }
 
     public void notifyHowMuchInput() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(ENTER_AMOUNT_OF_MONEY_NOTICE);
     }
 
     public void notifyWinningLottoNumbersInput() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(ENTER_WINNNG_LOTTO_NUMBERS_NOTICE);
     }
 
     public void notifyBonusNumberInput() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(ENTER_BOUNUS_NUMBER_NOTICE);
     }
 
     public void notifyRateOfReturn(float rateOfReturn) {
-        System.out.printf("총 수익률은 %.1f%%입니다.\n", rateOfReturn);
+        System.out.printf(RATE_OF_RETURN_IS_N_NOTICE, rateOfReturn);
     }
 
     public void notifySumOfResult(List<Integer> sumOfResult) {
-        System.out.println("당첨 통계");
-        System.out.println("---");
-        System.out.printf("3개 일치 (5,000원) - %d개\n", sumOfResult.get(0));
-        System.out.printf("4개 일치 (50,000원) - %d개\n", sumOfResult.get(1));
-        System.out.printf("5개 일치 (1,500,000원) - 0개\n", sumOfResult.get(2));
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n", sumOfResult.get(3));
-        System.out.printf("6개 일치 (2,000,000,000원) - 0개\n", sumOfResult.get(4));
-
+        System.out.println(WIN_STATISTICS_NOTICE);
+        System.out.println(LINE_NOTICE);
+        PlaceNotice[] place = PlaceNotice.values();
+        for (int order = 0; order < 5; order++) {
+            System.out.printf(place[order].notice, sumOfResult.get(order));
+        }
     }
 }
