4de690a3f8a1af5fde46284c08532f1e66b0e86e
KSH <tkdgns543@gmail.com>
2022-11-12 14:37:02 +0900

    feat(ui): 당첨번호 두종류 입력 구현
    기본 6자리 당첨번호 입력
    보너스 번호 입력

--- src/main/java/lotto/ui/NumbersReceiver.java
+++ src/main/java/lotto/ui/NumbersReceiver.java
@@ -2,10 +2,28 @@ package lotto.ui;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumbersReceiver {
     NumbersReceiver() {}
 
     public int insertMoney() {
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public List<Integer> setBasicNumbers() {
+        String[] correctNumbers = Console.readLine().split(",");
+        List<Integer> result = new ArrayList<>();
+
+        for (String singleNumber : correctNumbers) {
+            result.add(Integer.parseInt(singleNumber));
+        }
+
+        return result;
+    }
+
+    public int setBonusNumber() {
         return Integer.parseInt(Console.readLine());
     }
 }
