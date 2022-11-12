643385bf20fb6e2aabbeba381c3685cf0867ee59
hoonn9 <hun9935@gmail.com>
2022-11-12 22:57:33 +0900

    test(Lotto): 로또 테스트 코드 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,28 +1,45 @@
 package lotto.domain;
 
-import lotto.domain.Lotto;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
+    
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
-    // 아래에 추가 테스트 작성 가능
+    
+    @DisplayName("로또 번호를 배열 형태의 문자열로 반환한다.")
+    @Test
+    void getPrintByNumbers() {
+        List<Integer> lottoNumbers = new ArrayList<>(List.of(1, 2, 4, 3, 5, 6));
+        
+        assertThat(new Lotto(lottoNumbers).getStringByNumbers())
+                .isEqualTo("[1, 2, 3, 4, 5, 6]");
+    }
+    
+    @DisplayName("로또 번호는 오름차순으로 정렬되어야 한다.")
+    @Test
+    void getNumbers() {
+        List<Integer> lottoNumbers = new ArrayList<>(List.of(6, 2, 3, 1, 4, 5));
+        
+        assertThat(new Lotto(lottoNumbers).getNumbers())
+                .isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+    }
 }
