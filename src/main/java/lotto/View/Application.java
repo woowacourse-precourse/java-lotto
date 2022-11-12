2ee6c99ce006062f0122c2545bb5d1e39df60225
whibaek <jibaek0513@gmail.com>
2022-11-12 16:52:51 +0900

    refactor(Application) : 전반적인 흐름 재구성 - 리팩토링 된 다른 클래스를 활용하여 전반적인 프로그램 흐름 구성

---
+++ src/main/java/lotto/View/Application.java
@@ -0,0 +1,33 @@
+package lotto.View;
+
+import lotto.domain.*;
+
+public class Application {
+    public static void main(String[] args) {
+        // TODO: 프로그램 구현
+        Screen screen = new Screen();
+
+        // User Part
+        User user= new User(screen.readMoney());
+        user.makeLottos();
+
+        // Computer Part
+        LottoAnswer lottoAnswer = new LottoAnswer(screen.readLotto(), screen.readBonus());
+
+        screen.printLottoCount(user.getNumOfLotto());
+        screen.printLottos(user.getLottos());
+
+        // Calculate Part
+        Result result = new Result(user.getUserMoney());
+        Calculator calculator = new Calculator();
+        for (Lotto lotto : user.getLottos()) {
+            calculator.clearCount();
+            calculator.checkCount(lotto, lottoAnswer);
+            result.checkRanks(calculator);
+        }
+        result.calcAllResult();
+
+        // Result Part ( print )
+        screen.printResult(result);
+    }
+}
