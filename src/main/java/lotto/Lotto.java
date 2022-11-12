2c99550a784551f87bae4c83ca27d5f42b246367
fakerdeft <whakswp123@naver.com>
2022-11-13 03:11:35 +0900

    feat(validate): add 예외처리 시 출력 구문 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,9 +12,8 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또는 6자리 입니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
 }
