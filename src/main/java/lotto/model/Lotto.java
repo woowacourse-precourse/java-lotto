421079541253ab24e194bed85c7393b4e0e243bb
Kid-Chang <pch3310@gmail.com>
2022-11-12 22:17:16 +0900

    feat(Lotto): 로또 클래스의 패키지 변경

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.model;
 
 import java.util.List;
 
@@ -17,4 +17,11 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+    // 제공된 Lotto 클래스를 활용해 구현해야 한다.
+    // Lotto에 매개 변수가 없는 생성자를 추가할 수 없다.
+    // numbers의 접근 제어자인 private을 변경할 수 없다.
+    // Lotto에 필드(인스턴스 변수)를 추가할 수 없다.
+    // Lotto의 패키지 변경은 가능하다.
+
 }
