c93ba6c6c2cc03876e1d12ec3d07ec141fda958f
Yunsik-Choi <choiys0311@gmail.com>
2022-11-13 01:29:55 +0900

    ⛑️ test : LottoNumberTest 로또 숫자 값을 반환하는 테스트 추가

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,10 +1,13 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoNumberTest {
 
@@ -17,5 +20,11 @@ public class LottoNumberTest {
                 () -> Assertions.assertDoesNotThrow(() -> LottoNumber.of(45)),
                 () -> assertThatThrownBy(() -> LottoNumber.of(46)).isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @ParameterizedTest(name = "로또 숫자 값을 반환한다.")
+    @ValueSource(ints = {1, 4, 16, 25, 35, 43, 45})
+    void name(int number) {
+        assertThat(LottoNumber.of(number).number()).isEqualTo(number);
     }
 }
