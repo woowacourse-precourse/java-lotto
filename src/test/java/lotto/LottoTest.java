08ab7f7be82858296dcfad3b7831cc56e8edcd10
SeokHwan An <asok369258@naver.com>
2022-11-12 15:43:16 +0900

    test : 로또 번호가 1부터 45사이에 수로 구성되어 있는지 검증 기능 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -23,5 +23,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1과 45 사이의 수로 구성되어 있지 않으면 예외가 발생한다.")
+    @Test
+    void createLottoByOver45() {
+        assertThatThrownBy(() -> new Lotto(List.of(1,2,3,35,45,46)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 }
