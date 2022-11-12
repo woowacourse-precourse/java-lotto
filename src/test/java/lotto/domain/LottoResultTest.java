7de65da0a54f1130ee1d456ef48167a557697dc9
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-12 17:47:05 +0900

    test: 로또 결과의 상금 총합 계산 기능 테스트

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -16,20 +17,11 @@ import static org.assertj.core.api.Assertions.*;
 class LottoResultTest {
 
     private LottoResult lottoResult = new LottoResult();
-
-    @DisplayName("로또 장수 계산 기능 테스트")
-    @ParameterizedTest
-    @MethodSource("lottoRankingDataSource")
-    void 로또_장수_계산_테스트(Map<LottoRanking, Integer> lottoData, int expected) {
-
-        assertThat(lottoResult.getTotalTicket(lottoData)).isEqualTo(expected);
-    }
-
-    private static Stream<Arguments> lottoRankingDataSource() {
-        Map<LottoRanking, Integer> firstData = new HashMap<>();
-        Map<LottoRanking, Integer> secondData = new HashMap<>();
-        Map<LottoRanking, Integer> thirdData = new HashMap<>();
-
+    static Map<LottoRanking, Integer> firstData = new HashMap<>();
+    static Map<LottoRanking, Integer> secondData = new HashMap<>();
+    static Map<LottoRanking, Integer> thirdData = new HashMap<>();
+    @BeforeEach
+    void valueSetting(){
         firstData.put(LottoRanking.FIRST, 1);
         firstData.put(LottoRanking.SECOND, 2);
         firstData.put(LottoRanking.THIRD, 3);
@@ -50,11 +42,36 @@ class LottoResultTest {
         thirdData.put(LottoRanking.FOURTH, 0);
         thirdData.put(LottoRanking.FIFTH, 1);
         thirdData.put(LottoRanking.MISS, 7);
+    }
 
+    @DisplayName("로또 장수 계산 기능 테스트")
+    @ParameterizedTest
+    @MethodSource("lottoRankingTicketDataSource")
+    void 로또_장수_계산_테스트(Map<LottoRanking, Integer> lottoData, int expected) {
+
+        assertThat(lottoResult.getTotalTicket(lottoData)).isEqualTo(expected);
+    }
+
+    @DisplayName("로또 상금 계산 기능 테스트")
+    @ParameterizedTest
+    @MethodSource("lottoRankingRewardDataSource")
+    void 로또_상금_계산_테스트(Map<LottoRanking, Integer> lottoData, int expected) {
+        assertThat(lottoResult.getTotalReward(lottoData)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> lottoRankingTicketDataSource() {
         return Stream.of(
                 Arguments.of(firstData, 21),
                 Arguments.of(secondData, 21),
                 Arguments.of(thirdData, 8)
+        );
+    }
+
+    private static Stream<Arguments> lottoRankingRewardDataSource() {
+        return Stream.of(
+                Arguments.of(firstData, 2064725000),
+                Arguments.of(secondData, 3280000),
+                Arguments.of(thirdData, 5000)
         );
     }
 }
