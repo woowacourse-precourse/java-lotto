01eee8dd6749012ed887449f346a7c054b047202
gomljo <gomljo0307@naver.com>
2022-11-13 04:14:40 +0900

    feat(RaffleNumber): 추첨 번호 6개 중 중복이 발생했는지 확인하는 기능 구현

--- src/main/java/lotto/model/RaffleNumber.java
+++ src/main/java/lotto/model/RaffleNumber.java
@@ -1,18 +1,29 @@
 package lotto.model;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class RaffleNumber {
     private final List<Integer> raffleNumbers;
     private static final int PROPER_QUANTITY=6;
     private static final String WRONG_QUANTITY_ERROR_MESSAGE="[ERROR] 추첨 번호가 6개가 아닙니다. 프로그램을 종료합니다.";
+    private static final String DUPLICATE_ERROR_MESSAGE="[ERROR] 숫자가 중복되었습니다. 프로그램을 종료합니다.";
     public RaffleNumber(List<Integer> raffleNumbers) {
         validateProperQuantity(raffleNumbers);
+        validateDuplicatedNumbers(raffleNumbers);
         this.raffleNumbers = raffleNumbers;
     }
     private void validateProperQuantity(List<Integer> raffleNumbers) {
         if (raffleNumbers.size() != PROPER_QUANTITY) {
             throw new IllegalArgumentException(WRONG_QUANTITY_ERROR_MESSAGE);
+        }
+    }
+    private void validateDuplicatedNumbers(List<Integer> numbers) {
+        int numberOfUniqueNumbers;
+        Stream<Integer> lottoNumbers = numbers.stream();
+        numberOfUniqueNumbers= (int)lottoNumbers.distinct().count();
+        if (numberOfUniqueNumbers != PROPER_QUANTITY) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
         }
     }
 }
