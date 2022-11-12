2d4a32ad35e09e7241574b16428e3e53e2e06083
김태용 <amico741@naver.com>
2022-11-13 02:12:27 +0900

    feat: 숫자 범위에 맞는 입력값 검증 기능 추가
    범위 벗어나면 예외 발생

--- src/main/java/lotto/domain/WinningTicket.java
+++ src/main/java/lotto/domain/WinningTicket.java
@@ -3,11 +3,35 @@ package lotto.domain;
 import java.util.List;
 
 public class WinningTicket {
+    private static final String RANGE_EXCEED_MESSAGE = "[ERROR] 숫자는 1 ~ 45로 입력할 수 있습니다.";
     private List<Integer> winnings;
     private int bonusNumber;
 
     public WinningTicket(List<Integer> winnings, int bonusNumber) {
-        this.winnings = winnings;
-        this.bonusNumber = bonusNumber;
+        try {
+            validateNumberRange(winnings);
+            this.winnings = winnings;
+            this.bonusNumber = bonusNumber;
+        } catch(IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void validateNumberRange(List<Integer> winnings) throws IllegalArgumentException {
+        try {
+            winnings.forEach(this::checkRange);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
+    public void checkRange(int num) throws IllegalArgumentException {
+        try {
+            if(num < 1 || num > 45) {
+                throw new IllegalArgumentException(RANGE_EXCEED_MESSAGE);
+            }
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 }
