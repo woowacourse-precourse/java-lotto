ed4132ea726cb086492d61d2688fff484db5b3f7
ghltjd369 <ghltjd369@gmail.com>
2022-11-12 21:50:43 +0900

    feat : 당첨 번호 입력에 대한 예외 처리 기능 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -24,7 +24,7 @@ public class LottoMachine {
         System.out.println("보너스 번호를 입력해 주세요.");
         try {
             String bonusNumber = Console.readLine();
-        } catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자를 입력해야 합니다.");
         }
     }
@@ -32,7 +32,15 @@ public class LottoMachine {
     public void checkThatWinningNumberIsRightInput(String inputNumber) {
         List<String> winningNumberTemp = splitNumber(inputNumber);
         List<Integer> winningNumber = Exception.changeListStringToInteger(winningNumberTemp);
-
+        if (Exception.isOverlap(winningNumber)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호에 중복된 숫자가 있습니다.");
+        }
+        if (Exception.isNotSixSize(winningNumber)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호가 6개가 아닙니다.");
+        }
+        if (Exception.isNotInRange(winningNumber)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호가 범위를 벗어났습니다.");
+        }
     }
 
 
