2b8b5adc14a43302a31d16d131c48293cc3d830c
devyourown <hyojoon56@gmail.com>
2022-11-12 22:40:39 +0900

    refactor : split function to functions

--- src/main/java/lotto/domain/LottoMaker.java
+++ src/main/java/lotto/domain/LottoMaker.java
@@ -10,15 +10,27 @@ public class LottoMaker {
     private static final int ONE_LOTTO_PRICE = 1000;
     public static List<Lotto> makeLotto(int price) {
         validate(price);
-        List<Lotto> result = new ArrayList<>();
-        for (int i=0; i<price/ONE_LOTTO_PRICE; i++) {
-            result.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
-        }
-        return result;
+        return getLottos(getNumOfLotto(price));
     }
 
     private static void validate(int price) {
         if (price % ONE_LOTTO_PRICE != 0 || price < 1000)
             throw new IllegalArgumentException("로또는 1000원 단위로 구매할 수 있습니다.");
+    }
+
+    private static int getNumOfLotto(int price) {
+        return price/ONE_LOTTO_PRICE;
+    }
+
+    private static List<Lotto> getLottos(int numOfLotto) {
+        List<Lotto> result = new ArrayList<>();
+        for (int i=0; i<numOfLotto; i++) {
+            result.add(getLotto());
+        }
+        return result;
+    }
+
+    private static Lotto getLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
     }
 }
