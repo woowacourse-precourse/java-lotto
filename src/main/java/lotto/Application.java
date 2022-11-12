8af9edd3fa950e0d5a7c36d3e047d62653aa6b4d
lullulu__ <75472116+earthyoung@users.noreply.github.com>
2022-11-13 01:26:27 +0900

    feat: 보너스 번호 입력받고 유효성 판단 후 저장 - 숫자이며 당첨 번호와 중복되지 않는지 확인

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,7 +2,6 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +11,8 @@ public class Application {
         System.out.println("구입금액을 입력해 주세요.");
         int lottoCount = validateBudget(Console.readLine());
         List<Lotto> lottoArray = generateLottoNumbers(lottoCount);
-        Lotto lottoAnswer = generateLottoAnswers(Console.readLine());
+        Lotto lottoAnswer = validateLottoAnswers(Console.readLine());
+        int bonusNumber = validateBonusNumber(lottoAnswer.getNumbers(), Console.readLine());
 
     }
 
@@ -20,7 +20,7 @@ public class Application {
         int budget = 0;
         try{
             budget = Integer.parseInt(input);
-        }catch(Exception e){throwException("구입 금액은 숫자이어야 합니다.");}
+        }catch(NumberFormatException e){throwException("구입 금액은 숫자이어야 합니다.");}
         if (budget % 1000 != 0) throwException("구입 금액은 1000의 배수여야 합니다.");
         System.out.println("\n" + (budget/1000) + "개를 구매했습니다.");
         return (budget / 1000);
@@ -37,14 +37,14 @@ public class Application {
         return lottoArray;
     }
 
-    public static Lotto generateLottoAnswers(String input){
+    public static Lotto validateLottoAnswers(String input){
         List<Integer> numberList = new ArrayList<>();
         String[] strings = input.split(",");
         for(int i=0; i<strings.length; i++){
             try{
                 int elem = Integer.parseInt(strings[i]);
                 numberList.add(elem);
-            }catch(Exception e){throwException("당첨 번호는 모두 숫자이어야 합니다.");}
+            }catch(NumberFormatException e){throwException("당첨 번호는 모두 숫자이어야 합니다.");}
         }
         if(!isUniqueList(numberList)) throwException("당첨 번호는 중복되지 않아야 합니다.");
         return new Lotto(numberList);
@@ -56,6 +56,15 @@ public class Application {
             if(numberList.indexOf(elem) != numberList.lastIndexOf(elem)) return false;
         }
         return true;
+    }
+
+    public static int validateBonusNumber(List<Integer> numberList, String input){
+        int bonusNumber = 0;
+        try{
+            bonusNumber = Integer.parseInt(input);
+        }catch(NumberFormatException e){throwException("보너스 번호는 숫자이어야 합니다.");}
+        if(numberList.contains(bonusNumber)) throwException("보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
+        return bonusNumber;
     }
 
     public static void throwException(String message){
