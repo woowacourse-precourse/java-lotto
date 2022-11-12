b62a76c568c7420b880f9900781f9583ca5cd3ad
yubin <dbqls2821@gmail.com>
2022-11-12 15:41:13 +0900

    feat(ExceptionMessage): 입력값이 1000의 배수가 아닐 경우 출력될 메시지 추가

--- src/main/java/lotto/domain/constant/ExceptionMessage.java
+++ src/main/java/lotto/domain/constant/ExceptionMessage.java
@@ -5,7 +5,8 @@ public enum ExceptionMessage {
   INPUT_IS_NOT_SIX_NUMBERS("[ERROR] 입력값의 숫자는 6개가 아닙니다."),
   INPUT_IS_NOT_NUMBER("[ERROR] 입력값은 숫자가 아닙니다."),
   INPUT_NUMBER_RANGE_IS_NOT_CORRECT("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-  INPUT_IS_DUPLICATED("[ERROR] 입력값에 중복된 숫자가 있습니다.");
+  INPUT_IS_DUPLICATED("[ERROR] 입력값에 중복된 숫자가 있습니다."),
+  INPUT_IS_NOT_MULTIPLE_OF_1000("[ERROR] 입력값은 1000의 배수가 아닙니다.");
 
   private String message;
 
