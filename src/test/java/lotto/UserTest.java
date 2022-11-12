4af3abd04c50c5afbc82551649ed98fc974db4e4
ghltjd369 <ghltjd369@gmail.com>
2022-11-12 16:00:13 +0900

    refactor : paraterizedtest를 사용하여 더 간편한 테스트 코드 작성

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -11,20 +13,12 @@ import static org.assertj.core.api.Assertions.*;
 
 class UserTest {
     User user = new User();
-    @DisplayName("입력된 구입 금액이 0보다 작거나 같으면 예외가 발생한다.")
-    @Test
-    void createMoneyNotPositive() {
+    @DisplayName("입력된 구입 금액이 양수가 아니거나 1,000원으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"-12", "0", "-12345", "12345", "123"})
+    void createMoneyNotPositive(String number) {
         assertThatThrownBy(
-                () -> user.checkThatPurchaseMoneyIsRightInput("-12"))
+                () -> user.checkThatPurchaseMoneyIsRightInput(number))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
-    @DisplayName("입력된 구입 금액이 1,000원으로 나누어 떨어지지 않으면 예외가 발생한다.")
-    @Test
-    void createMoneyNotMultipleOfThousand() {
-        assertThatThrownBy(
-                () -> user.checkThatPurchaseMoneyIsRightInput("1234"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
 }
