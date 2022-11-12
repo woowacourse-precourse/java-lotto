1a7878a5ad5edecbf8e04ebdbe722a0a28adebc4
Soomin Lim <augustth511@gmail.com>
2022-11-12 21:28:19 +0900

    test(LottoPurchaseMoneyTest): 로또 구입 금액 입력 시 예외 처리 테스트 코드 작성

---
+++ src/test/java/lotto/domain/LottoPurchaseMoneyTest.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoPurchaseMoneyTest {
+    @DisplayName("로또 구입 금액이 1,000으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void createLottoPurchaseMoneyByInvalidNumber() {
+        assertThatThrownBy(() -> new LottoPurchaseMoney(1500))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
