acc5b5d97759350578aa386c4702016e01159436
leeyongjun <leeyongjun0310@naver.com>
2022-11-13 01:41:29 +0900

    feat(lotto): 로또 구입 금액을 입력받는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int purchaseAmount = askPurchaseAmount();
+        System.out.println(purchaseAmount);
+    }
 
+    private static int askPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
     }
 }
