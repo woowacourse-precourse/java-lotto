0e4e0f14d2fcacacbe35875c120c9c9dd6570e91
kwYoohae <gocksdl7979@gmail.com>
2022-11-13 00:18:15 +0900

    test: 각 번호들이 오름차순으로 정렬되서 저장되는지 확인하는 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,8 +4,11 @@ import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,5 +27,20 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호는 오름차순으로 정렬이 되어 있어야한다.")
+    @Test
+    void createLottoByOrdered() throws Exception {
+        List question = new ArrayList<>();
+        for (int i = 0 ; i < 6; i++) {
+            question.add(i+1);
+        }
+        Lotto lotto = new Lotto(question);
+
+        List<Integer> result = List.of(1, 2, 3, 4, 5, 6);
+        Field privateField = lotto.getClass().getDeclaredField("numbers");
+        privateField.setAccessible(true);
+        List<Integer> numbers = (List<Integer>) privateField.get(lotto);
+
+        assertThat(numbers).isEqualTo(result);
+    }
 }
