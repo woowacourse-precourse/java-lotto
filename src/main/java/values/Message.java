f15e4c621a79abcaacb70980101553c35ae9cad5
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-11-12 14:01:29 +0900

    fix : add Error Message

--- src/main/java/values/Message.java
+++ src/main/java/values/Message.java
@@ -1,19 +1,26 @@
 package values;
 
 public class Message {
-    static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
-    static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    static final String OUTPUT_CNT = "개를 구매했습니다.";
-    static final String STATISTICS = "당첨 통계\n---";
+    public static class Console {
+        static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+        static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+        static final String OUTPUT_CNT = "개를 구매했습니다.";
+        static final String STATISTICS = "당첨 통계\n---";
 
-    public String getTotalRevenueMessage(Double percent){
-        StringBuffer message = new StringBuffer();
-        String roundedPercent = String.format("%.1f", percent);
+        public String getTotalRevenueMessage(Double percent) {
+            StringBuffer message = new StringBuffer();
+            String roundedPercent = String.format("%.1f", percent);
 
-        message.append("총 수익률은 ");
-        message.append(roundedPercent);
-        message.append("%입니다.");
+            message.append("총 수익률은 ");
+            message.append(roundedPercent);
+            message.append("%입니다.");
 
-        return message.toString();
+            return message.toString();
+        }
+    }
+
+    public static class Error {
+        static final String BOUND_ERROR = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+        static final String DUPLICATION_ERROR = "[ERROR] 로또 번호에는 중복이 없어야 합니다.";
     }
 }
