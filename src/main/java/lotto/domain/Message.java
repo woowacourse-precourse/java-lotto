92407b96ec25fb54b50e11399c6900dfc8828018
YE-EUN <musiclove529@naver.com>
2022-11-13 04:28:07 +0900

    feat(Message) : 클래스 생성

---
+++ src/main/java/lotto/domain/Message.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+public class Message {
+    static final String PURCHASE_AMOUNT_REQUEST  = "구입금액을 입력해 주세요.";
+    static final String LOTTO_GENERATE_SUCCESS = "개를 구매했습니다.";
+    static final String WINNING_NUMBERS_REQUEST = "당첨 번호를 입력해 주세요.";
+    static final String BONUS_NUMBER_REQUEST = "보너스 번호를 입력해 주세요.";
+    static final String STATISTICS_TITLE = "당첨 통계";
+    static final String START_OF_EARNING_RATE = "총 수익률은 ";
+    static final String END_OF_EARNING_RATE = "%입니다.";
+}
