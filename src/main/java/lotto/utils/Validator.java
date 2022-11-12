aa02b57e2e1b3c9bb3b5710a83e00eb87424631b
myeongju00 <j51677767@gmail.com>
2022-11-12 19:15:33 +0900

    feat : 입력 금액 에러 처리

---
+++ src/main/java/lotto/utils/Validator.java
@@ -0,0 +1,39 @@
+package lotto.utils;
+
+import java.util.regex.Pattern;
+import lotto.domain.ErrorMessage;
+
+public class Validator {
+    private static final String INPUT_NUMBER_REGEX = "^[0-9]*$";
+    public void isValidMoney(String input) {
+        if(!isNumber(input)) {
+            throw new IllegalArgumentException(String.valueOf(ErrorMessage.MONEY_IS_NOT_NUMBER));
+        }
+        if(isZero(input)) {
+            throw new IllegalArgumentException(String.valueOf(ErrorMessage.MONEY_IS_ZERO));
+        }
+        if(!is1000Units(Integer.parseInt(input))) {
+            throw new IllegalArgumentException(String.valueOf(ErrorMessage.MONEY_IS_NOT_1000_UNIT));
+        }
+        if(isEnteredSpace(input)){
+            throw new IllegalArgumentException(String.valueOf(ErrorMessage.MONEY_IS_NOT_ENTERED));
+        }
+    }
+
+    private boolean isNumber(String input) {
+        Pattern numberPattern = Pattern.compile(INPUT_NUMBER_REGEX);
+        return numberPattern.matcher(input).matches();
+    }
+
+    private boolean isZero(String input) {
+        return input.equals("0");
+    }
+
+    private boolean is1000Units(int money) {
+        return money % 1000 == 0;
+    }
+
+    private boolean isEnteredSpace(String input) {
+        return input.equals("");
+    }
+}
