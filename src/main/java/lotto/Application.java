573031f1da55036fbf4b88e87137920bcc0bd61e
minaver <psmjoshua@gmail.com>
2022-11-12 17:36:11 +0900

    feat(main): 로또 스퀀스 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import lotto.controller.Controller;
+import lotto.enums.Rank;
+import lotto.model.Lotto;
+import lotto.view.View;
+
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller();
+        View view = new View();
+
+        List<Lotto> lottoPayed = controller.buyLotto();
+        view.printLottoPayed(lottoPayed);
+
+        controller.setWinningNums();
+
+        Map<Rank, Integer> winningStat = controller.getWinningStat(lottoPayed);
+        double earningRate = controller.getEarningRate(winningStat);
+
+        view.printWinningStats(winningStat);
+        view.printEarningRate(earningRate);
     }
 }
