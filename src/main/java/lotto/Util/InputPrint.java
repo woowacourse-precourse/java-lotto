034c2473fde40d47172f8cb3e41f9e3ddb97553d
win9 <krrr8@naver.com>
2022-11-12 21:36:28 +0900

    Feat(Player): 당첨번호 입력부 구현
    요구사항에 맞추어서 당첨번호를 입력할때, 1~45의 6자리 숫자를
    쉼표를 이용하여 구분하여 입력하도록 한다. 이때 이 규칙을 지키지
    않으면 예외를 발생시키도록 한다.

--- src/main/java/lotto/Util/InputPrint.java
+++ src/main/java/lotto/Util/InputPrint.java
@@ -1,9 +1,16 @@
 package lotto.Util;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.type.TextType;
 
 public class InputPrint {
-    public static void startInput(){
+    public static String startInput(){
         System.out.println(TextType.START);
+        return Console.readLine();
+    }
+
+    public static String prizeNumberInput(){
+        System.out.println(TextType.LOTTO_PRIZE_NUMBER);
+        return Console.readLine();
     }
 }
--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -1,16 +1,34 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Console;
+import lotto.Util.InputPrint;
 import lotto.valid.Validation;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Player {
+    private List<Integer> prizeNumbers;
     private int purchaseNumber;
-    public void inputString(){
-        purchaseNumber = Integer.parseInt(Console.readLine());
+
+    public void inputMoney(){
+        String startInput = InputPrint.startInput();
+        purchaseNumber = Integer.parseInt(startInput);
         Validation.purchaseNumberValid(purchaseNumber);
+    }
+
+    public void inputPrizeNumbers(){
+        prizeNumbers = Arrays.stream(InputPrint.prizeNumberInput().split(","))
+                .map(num -> Integer.parseInt(num))
+                .collect(Collectors.toList());
+        Validation.prizeNumbersValid(prizeNumbers);
     }
 
     public int getPurchaseNumber() {
         return purchaseNumber;
+    }
+
+    public List<Integer> getPrizeNumbers() {
+        return prizeNumbers;
     }
 }
