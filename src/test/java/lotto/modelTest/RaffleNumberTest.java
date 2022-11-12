9fd0ea87deb706f18eba60e859350acd7ed25c8d
gomljo <gomljo0307@naver.com>
2022-11-13 04:12:16 +0900

    test(RaffleNumberTest.java): 생성된 추첨 번호가 6개가 아닌지 확인 하는 기능 검증

--- src/test/java/lotto/modelTest/RaffleNumberTest.java
+++ src/test/java/lotto/modelTest/RaffleNumberTest.java
@@ -12,10 +12,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RaffleNumberTest {
     private static final String WRONG_QUANTITY_ERROR_MESSAGE="[ERROR] 추첨 번호가 6개가 아닙니다. 프로그램을 종료합니다.";
+    private static final String DUPLICATE_ERROR_MESSAGE="[ERROR] 숫자가 중복되었습니다. 프로그램을 종료합니다.";
     @Test
     @DisplayName("추첨 번호 6개가 아니라면 에러를 출력한다.")
     public void createRaffleNumberTest(){
         List<Integer> raffleNumber = List.of(1,2,3,4,5,6,7);
         assertThatThrownBy(()->new RaffleNumber(raffleNumber)).isInstanceOf(IllegalArgumentException.class).hasMessageContaining(WRONG_QUANTITY_ERROR_MESSAGE);
     }
+
 }
