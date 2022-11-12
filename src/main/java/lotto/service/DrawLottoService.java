bc25a63f502c3b5376ea6aa106e01d2b7100bc2d
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-12 19:30:23 +0900

    feat: 입력으로 들어온 로또 번호를 리스트로 변환하여 리턴해주는 메서드 추가

--- src/main/java/lotto/service/DrawLottoService.java
+++ src/main/java/lotto/service/DrawLottoService.java
@@ -1,8 +1,19 @@
 package lotto.service;
 
+import java.util.List;
+import lotto.util.ConversionArrayToList;
+
 public class DrawLottoService {
 
-    public int countBuyingLotto(int amount){
-        return amount/1000;
+    ConversionArrayToList conversionArrayToList = new ConversionArrayToList();
+
+    public int countBuyingLotto(int amount) {
+        return amount / 1000;
+    }
+
+    public List<Integer> getListToWiningNumber(String winingLottoNUmber) {
+        String[] winingNUmber = winingLottoNUmber.split(",");
+        List<Integer> winingNumberList = conversionArrayToList.stringArrayToIntegerList(winingNUmber);
+        return winingNumberList;
     }
 }
