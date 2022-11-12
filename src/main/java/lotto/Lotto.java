637ca67bec3f9a8a6a39b4f8b2361f864e78b489
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-12 15:28:58 +0900

    refactor: 불필요한 함수 제거

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,9 +13,15 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("6개의 숫자만 입력 가능합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getLottoNumber () {
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            nums.add(numbers.get(i));
+        }
+        return nums;
+    }
 }
