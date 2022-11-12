362039243b907b9d4bf8243739207fd01662ba68
devyourown <hyojoon56@gmail.com>
2022-11-13 00:05:44 +0900

    test : add code to pass the lottoTest

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -12,8 +12,10 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개이어야 합니다.");
         }
+        if (numbers.stream().distinct().count() != numbers.size())
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안 됩니다.");
     }
 
     // TODO: 추가 기능 구현
