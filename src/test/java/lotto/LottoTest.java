4d463c5c014243cac803d7f1ce3801f2a9c463a9
jybin96 <jybin96@changwon.ac.kr>
2022-11-12 22:48:05 +0900

    test: add lotto issuance validate for between values testcode
    Test code for if an exception occurs if it does not exist in the interval value

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -18,10 +18,14 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 지정된 숫자 범위안에 존재하지 않는다면 예외가 발생한다.")
+    @Test
+    void createLottoByBetweenValue(){
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 4, 5, 6, 49)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
