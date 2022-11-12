68da8d94876bb0db531ff1ce08e91ba3c7eef472
최용태 <smc2315@naver.com>
2022-11-12 20:53:06 +0900

    feat: input 요청 메시지 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,14 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 import static lotto.utils.ExceptionMessage.*;
+import static lotto.utils.Message.*;
 
 public class InputView {
     public static int requestUserAmount(){
-
+        System.out.println(REQUEST_USER_AMOUNT);
         return StringToInt(Console.readLine());
     }
 
     public static String requestWinNumbers(){
+        System.out.println(REQUEST_WIN_NUMBERS);
         return(Console.readLine());
     }
 
