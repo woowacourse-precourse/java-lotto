749d91158362ccda8638f0596fb8ff54260959dc
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-12 18:25:54 +0900

    refactor(LottoStore): 상수 제거 및 String - 정수 형 변환 분리

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -5,19 +5,17 @@ import java.util.regex.Pattern;
 public class LottoStore {
 	private static final String REGEX = "^[0-9]*000$";
 	private static final Pattern numberPattern = Pattern.compile(REGEX);
-	private static final int MIN_PURCHASE_UNIT = 1000;
-	private static final String REGEX_ERROR_MESSAGE = "[ERROR] %d 단위 숫자만 입력해주세요.";
-	private static final String ZERO_ERROR_MESSAGE = "[ERROR] 최소 %d원부터 구매 가능합니다.";
 
 	private final int purchaseAmount;
 	private final int numberOfTickets;
 
 	public LottoStore(String scanPurchaseAmount) {
-		int purchaseAmount = validateREGEX(scanPurchaseAmount);
+		validateREGEX(scanPurchaseAmount);
+		int purchaseAmount = Integer.parseInt(scanPurchaseAmount);
 		validateZero(purchaseAmount);
 
 		this.purchaseAmount = purchaseAmount;
-		this.numberOfTickets = purchaseAmount / MIN_PURCHASE_UNIT;
+		this.numberOfTickets = purchaseAmount / Number.UNIT.getValue();
 	}
 
 	public int getPurchaseAmount() {
@@ -28,16 +26,15 @@ public class LottoStore {
 		return numberOfTickets;
 	}
 
-	private int validateREGEX(String userInput) {
+	private void validateREGEX(String userInput) {
 		if (!numberPattern.matcher(userInput).matches()) {
-			throw new IllegalArgumentException(String.format(REGEX_ERROR_MESSAGE, MIN_PURCHASE_UNIT));
+			throw new IllegalArgumentException(String.format(ErrorMessage.UNIT.getMessage(), Number.UNIT.getValue()));
 		}
-		return Integer.parseInt(userInput);
 	}
 
 	private void validateZero(int purchaseAmount) {
 		if (purchaseAmount == 0) {
-			throw new IllegalArgumentException(String.format(ZERO_ERROR_MESSAGE, MIN_PURCHASE_UNIT));
+			throw new IllegalArgumentException(String.format(ErrorMessage.ZERO.getMessage(), Number.UNIT.getValue()));
 		}
 	}
 }
