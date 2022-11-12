f52327af323ab2c5105fa03881814fcb87315933
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-12 17:08:37 +0900

    test: Lotto - isWrongRange 테스트
    테스트 코드 계층화 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -12,19 +12,40 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
-    @Test
-    void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+
+    @Nested
+    @DisplayName("로또 번호 검증")
+    class LottoNumberValidate {
+
+        @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+        @Test
+        void createLottoByOverSize() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+        @Test
+        void createLottoByDuplicatedNumber() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("로또 번호가 범위를 벗어나면 예외를 반환한다. - 초과하는 경우")
+        @Test
+        void test() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("로또 번호가 범위를 벗어나면 예외를 반환한다. - 미만인 경우")
+        @Test
+        void test2() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void createLottoByDuplicatedNumber() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 
     @Nested
     @DisplayName("Lotto - numbers의 불변 여부 테스트")
@@ -59,49 +80,49 @@ class LottoTest {
         @Test
         void test1() {
             Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-            assertThat(lotto.compareWinningNumbers(WINNING_NUMBERS)).isEqualTo(6);
+            assertThat(lotto.getNumberOfMatchesByWinningNumbers(WINNING_NUMBERS)).isEqualTo(6);
         }
 
         @DisplayName("당첨번호가 5개인 경우")
         @Test
         void test2() {
             Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 45));
-            assertThat(lotto.compareWinningNumbers(WINNING_NUMBERS)).isEqualTo(5);
+            assertThat(lotto.getNumberOfMatchesByWinningNumbers(WINNING_NUMBERS)).isEqualTo(5);
         }
 
         @DisplayName("당첨번호가 4개인 경우")
         @Test
         void test3() {
             Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 44, 45));
-            assertThat(lotto.compareWinningNumbers(WINNING_NUMBERS)).isEqualTo(4);
+            assertThat(lotto.getNumberOfMatchesByWinningNumbers(WINNING_NUMBERS)).isEqualTo(4);
         }
 
         @DisplayName("당첨번호가 3개인 경우.")
         @Test
         void test4() {
             Lotto lotto = new Lotto(List.of(1, 2, 3, 43, 44, 45));
-            assertThat(lotto.compareWinningNumbers(WINNING_NUMBERS)).isEqualTo(3);
+            assertThat(lotto.getNumberOfMatchesByWinningNumbers(WINNING_NUMBERS)).isEqualTo(3);
         }
 
         @DisplayName("당첨번호가 2개인 경우")
         @Test
         void test5() {
             Lotto lotto = new Lotto(List.of(1, 2, 42, 43, 44, 45));
-            assertThat(lotto.compareWinningNumbers(WINNING_NUMBERS)).isEqualTo(2);
+            assertThat(lotto.getNumberOfMatchesByWinningNumbers(WINNING_NUMBERS)).isEqualTo(2);
         }
 
         @DisplayName("당첨번호가 1개인 경우")
         @Test
         void test6() {
             Lotto lotto = new Lotto(List.of(1, 41, 42, 43, 44, 45));
-            assertThat(lotto.compareWinningNumbers(WINNING_NUMBERS)).isEqualTo(1);
+            assertThat(lotto.getNumberOfMatchesByWinningNumbers(WINNING_NUMBERS)).isEqualTo(1);
         }
 
         @DisplayName("당첨번호가 0개인 경우")
         @Test
         void test7() {
             Lotto lotto = new Lotto(List.of(40, 41, 42, 43, 44, 45));
-            assertThat(lotto.compareWinningNumbers(WINNING_NUMBERS)).isEqualTo(0);
+            assertThat(lotto.getNumberOfMatchesByWinningNumbers(WINNING_NUMBERS)).isEqualTo(0);
         }
     }
 
