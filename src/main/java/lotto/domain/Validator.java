937c6a3c646d6ef657a3dc152acb047290231924
ChanghyunKim <variety82p@gamil.com>
2022-11-12 17:37:15 +0900

    feat(Validator) : 1,000단위 검증 메서드 구현

--- src/main/java/lotto/domain/Validator.java
+++ src/main/java/lotto/domain/Validator.java
@@ -2,12 +2,21 @@ package lotto.domain;
 
 import java.util.regex.Pattern;
 
+import static lotto.utils.Messages.ERROR_MESSAGE;
+
 public class Validator {
 
-    public void validateNumeric(String amount){
+    public void validateNumeric(String input){
         String numericPattern = "^[0-9]*$";
-        if(!Pattern.matches(numericPattern, amount)){
-            throw new IllegalArgumentException("[ERROR] 숫자형식이 아닙니다");
+        if(!Pattern.matches(numericPattern, input)){
+            throw new IllegalArgumentException(ERROR_MESSAGE + "숫자형식이 아닙니다");
+        }
+    }
+
+    public void validateMultipleOfThousands(String input){
+        int amount = Integer.parseInt(input);
+        if(amount % 1000 != 0){
+            throw new IllegalArgumentException(ERROR_MESSAGE + "1,000의 단위가 아닙니다.");
         }
     }
 }
