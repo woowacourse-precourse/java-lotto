0a3b9abbf26161a18bfb000a10517bed586c4a45
박원종 <wonjong1030@naver.com>
2022-11-12 15:43:40 +0900

    feat: 기능 완성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,57 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 구입 금액 입력
+        Console console = new Console();
+        Calculator calculator = new Calculator();
+        double money=0;
+        try {
+            money = console.inputMoney();
+        } catch (NumberFormatException e) {
+            console.outputErrorMessage();
+            return;
+        }
+        int count = calculator.getLottoCount(money);
+
+        // 로또 구매
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        List<Integer>[] issuedLottos = lottoGenerator.createRandomNumbers(count);
+        console.outputMyLottos(issuedLottos);
+
+        // 당첨 번호 입력
+        Lotto lotto = null;
+        try {
+             lotto = console.inputWinningNumbers();
+        } catch (IllegalArgumentException e) {
+            console.outputErrorMessage();
+            return;
+        }
+
+        // 보너스 번호 입력
+        // TODO: 보너스 입력 예외 추가해야함
+        int bonusNumber = console.inputBonusNumber();
+
+        LottoResult lottoResult = new LottoResult();
+        for (List<Integer> issuedLotto : issuedLottos) {
+            // 로또 번호 비교
+            int correctCount = lotto.compareNumbers(issuedLotto);
+            // 보너스 번호 비교
+            boolean bonus = lotto.compareBonus(bonusNumber);
+            // 당첨 순위 계산
+            Rank rank = calculator.calculateRank(correctCount, bonus);
+            if (rank == null) {
+                continue;
+            }// 순위 저장
+            lottoResult.insertResult(rank);
+        }
+
+        Map<Rank, Integer> result = lottoResult.getCopyResult();
+        double yield = calculator.calculateYield(result, money);
+        console.outputWinningResult(result, yield);
     }
 }
