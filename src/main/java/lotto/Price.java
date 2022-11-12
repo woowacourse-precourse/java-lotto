851afaa2b593a98f167c6e032fe84a737a9b6e5a
grand7070 <grand7070@naver.com>
2022-11-12 16:04:56 +0900

    refactor: 숫자 예외와 나누어 떨어지는 예외 분리 및 이름 변경

--- src/main/java/lotto/Price.java
+++ src/main/java/lotto/Price.java
@@ -4,15 +4,19 @@ public class Price {
     private final String price;
 
     public Price(String price) {
-        validate(price);
+        numberValidate(price);
+        divideValidate(price);
         this.price = price;
     }
 
-    private void validate(String price) {
+    private void numberValidate(String price) {
         // 1-1. 숫자가 아니면 예외처리를 한다.
         for (int idx = 0; idx < price.length(); idx++) {
             if(!('0' <= price.charAt(idx) && price.charAt(idx) <= '9')) throw new IllegalArgumentException();
         }
+    }
+
+    private void divideValidate(String price) {
         // 1-2. 1000원 단위로 나누어 떨어지지 않으면 예외처리를 한다.
         if(Integer.parseInt(price) % 1000 != 0) throw new IllegalArgumentException();
     }
