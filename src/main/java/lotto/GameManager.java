a38b91dd84045af78bf0bbb461ed55d84b7acd7a
kwYoohae <gocksdl7979@gmail.com>
2022-11-13 00:02:44 +0900

    feat: 로또를 몇개를 구입하는지 변환하는 기능 작성

--- src/main/java/lotto/GameManager.java
+++ src/main/java/lotto/GameManager.java
@@ -1,16 +1,24 @@
 package lotto;
 
+import lotto.domain.Wallet;
+import lotto.utils.Constant;
 import lotto.views.Input;
 
 public class GameManager {
 
     private final Input input;
+    private Wallet wallet;
 
     public GameManager(Input input) {
         this.input = input;
     }
 
     public void run() {
-        input.getLottoPrice();
+        int lottoPrice = input.getUserMoney();
+        int numberOfPurchase = changeNumberOfLottoToBuy(lottoPrice);
+    }
+
+    public int changeNumberOfLottoToBuy(int lottoPrice) {
+        return lottoPrice % Constant.LOTTO_PRICE;
     }
 }
