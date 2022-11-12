2a20e5115ee483ca9d86e342bb83631f021dca9b
은지 <midzy0228@gmail.com>
2022-11-12 18:23:48 +0900

    feat(로또 구매): 오름차순으로 정렬

--- src/main/java/lotto/NumberGenerator.java
+++ src/main/java/lotto/NumberGenerator.java
@@ -10,6 +10,7 @@ public class NumberGenerator {
         List<List<Integer>> lotto = new ArrayList<>();
         for (int count = 0; count < lottoTickets; count++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            numbers.sort(Integer::compareTo);
             lotto.add(numbers);
             System.out.println(numbers);
         }
