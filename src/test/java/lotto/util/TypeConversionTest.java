b72c0a3c038a6690a782d4677a542c72bdf5c030
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-12 23:46:38 +0900

    test(TypeConversionTest): 당첨 번호가 숫자인지 확인하는 기능 테스트

--- src/test/java/lotto/util/TypeConversionTest.java
+++ src/test/java/lotto/util/TypeConversionTest.java
@@ -1,7 +1,9 @@
 package lotto.util;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -26,5 +28,29 @@ class TypeConversionTest {
 
         // expected
         TypeConversion.stringToInt(input);
+    }
+
+    @DisplayName("문자열을 쉼표로 구분할 수 있는지 확인 - 구분 가능")
+    @Test
+    void stringListToIntList() {
+        // given
+        String input = "1,2,3,4,5,6";
+
+        // when
+        List<Integer> result = TypeConversion.stringListToIntList(input);
+
+        // then
+        assertThat(result).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @DisplayName("문자열을 쉼표로 구분할 수 있는지 확인 - 구분 불가능")
+    @Test
+    void stringListToIntList2() {
+        // given
+        String input = "1,2,3,4,5, 6";
+
+        // expected
+        assertThatThrownBy(() -> TypeConversion.stringListToIntList(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
