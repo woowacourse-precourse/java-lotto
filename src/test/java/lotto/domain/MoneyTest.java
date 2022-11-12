21d3952a2db734ded5dd0f84dbc98921cd8da6f6
drunkenhw <drunkenhw@gmail.com>
2022-11-12 16:53:44 +0900

    refactor(Money): 테스트 이름 추가

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -7,16 +7,16 @@ import org.junit.jupiter.api.Test;
 
 class MoneyTest {
 
-    @Test
     @DisplayName("로또 구입 금액 단위가 아니면 예외를 발생한다.")
+    @Test
     void buyLottoEx() {
         Money money = new Money(12500);
 
         assertThatThrownBy(money::exchangeLotto).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
     @DisplayName("로또 구입 금액만큼 로또 갯수를 반환한다.")
+    @Test
     void buyLotto() {
         Money money = new Money(12000);
 
