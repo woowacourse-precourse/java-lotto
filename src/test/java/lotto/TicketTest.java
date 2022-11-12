86b9b3c8ca4731bdd08ff5982062fdfc36d81cc7
JunginPark4364 <wjddlsee@gmail.com>
2022-11-13 02:49:11 +0900

    test: TicketTest 중복제거, 메서드 이름 변경
    Ticket 생성하는 부분을 공통으로 추출하고
    테스트 메서드 이름을 수정했습니다.

--- src/test/java/lotto/TicketTest.java
+++ src/test/java/lotto/TicketTest.java
@@ -1,16 +1,23 @@
 package lotto;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 class TicketTest {
+    private Ticket ticket;
+
+    @BeforeEach
+    public void setup() {
+        int money = 1000;
+        ticket = Ticket.purchase(money);
+    }
+
     @DisplayName("로또 구매하는 기능 테스트입니다.")
     @Test
-    public void test() throws Exception {
-        int money = 1000;
-
-        assertThat(Ticket.purchase(money)).isInstanceOf(Ticket.class);
+    public void purchaseTicket() {
+        assertThat(ticket).isInstanceOf(Ticket.class);
     }
 }
