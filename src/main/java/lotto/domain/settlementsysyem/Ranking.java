76bc399564baf4825dce5f186d33d560949123ad
JaehongDev <println@kakao.com>
2022-11-12 18:09:16 +0900

    feature  (Ranking) : 등수가 결과 값을 가지도록 기능 구현

--- src/main/java/lotto/domain/settlementsysyem/Ranking.java
+++ src/main/java/lotto/domain/settlementsysyem/Ranking.java
@@ -1,22 +1,26 @@
 package lotto.domain.settlementsysyem;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public enum Ranking {
-    NOT_WINNING(0, List.of()),
-    FIFTH_WINNING(5, List.of(new Score(2, 1), new Score(3, 0))),
-    FOURTH_WINNING(4, List.of(new Score(3, 1), new Score(4, 0))),
-    THIRD_WINNING(3, List.of(new Score(4, 1), new Score(5, 0))),
-    SECOND_WINNING(2, List.of(new Score(5, 1))),
-    FIRST_WINNING(1, List.of(new Score(6, 0)));
+    NOT_WINNING(0, List.of(), ""),
+    FIFTH_WINNING(5000, List.of(new Score(2, 1), new Score(3, 0)), "3개 일치"),
+    FOURTH_WINNING(50000, List.of(new Score(3, 1), new Score(4, 0)), "4개 일치"),
+    THIRD_WINNING(15000000, List.of(new Score(4, 1), new Score(5, 0)), "5개 일치"),
+    SECOND_WINNING(300_000_000, List.of(new Score(5, 1)), "5개 일치, 보너스 볼 일치"),
+    FIRST_WINNING(20_000_000, List.of(new Score(6, 0)), "6개 일치");
 
-    private final Integer rank;
+    private final Integer prize;
     private final List<Score> score;
+    private final String result;
 
-    Ranking(final Integer rank, final List<Score> score) {
-        this.rank = rank;
+    Ranking(final Integer rank, final List<Score> score, final String result) {
+        this.prize = rank;
         this.score = score;
+        this.result = result;
     }
 
     public static Ranking calculateScore(Score score) {
@@ -24,5 +28,20 @@ public enum Ranking {
                 .filter(ranking -> ranking.score.contains(score))
                 .findAny()
                 .orElse(NOT_WINNING);
+    }
+
+    public static Map<Ranking, Integer> generateList() {
+        Map<Ranking, Integer> list = new LinkedHashMap<>();
+        for (var ranking : Ranking.values()) {
+            if (ranking.prize != 0) {
+                list.put(ranking, 0);
+            }
+        }
+        return list;
+    }
+
+    public String result() {
+        String prize = String.valueOf(this.prize).replaceAll("\\B(?=(\\d{3})+(?!\\d))", ",");
+        return this.result + " (" + prize + ") ";
     }
 }
