46313f8d0f2f5db4add7813cc2a0540be0ba2cf9
yeonhee <ydol2@naver.com>
2022-11-13 02:59:00 +0900

    fix(LottoTicket) : test로 정렬 오류 발견 및 해결

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Collections;
 import java.util.List;
 
 import static lotto.common.constant.NumberConstant.LOTTO_LENGTH;
@@ -12,6 +13,7 @@ public class LottoTicket {
 
     public static Lotto generate(){
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_VALUE, LOTTO_MAX_VALUE, LOTTO_LENGTH);
+        Collections.sort(lottoNumbers);
         return new Lotto(lottoNumbers);
     }
 
