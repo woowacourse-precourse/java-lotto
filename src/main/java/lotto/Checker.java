e22b4b976a08ef6be5e346702734b9c59b044c20
swonny <wonny921@gmail.com>
2022-11-12 14:59:45 +0900

    feat(Checker, Kiosk): 등수를 확인 메소드 구현, 메소드 구현에 필요한 해시맵 추가

--- src/main/java/lotto/Checker.java
+++ src/main/java/lotto/Checker.java
@@ -1,18 +1,26 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Checker extends LottoKiosk {
+    static final int FIRST_PRIZE_COUNT = 6;
+    static final int THIRD_PRIZE_COUNT = 5;
+    static final int FOURTH_PRIZE_COUNT = 4;
+    static final int FIFTH_PRIZE_COUNT = 3;
+    static HashMap<Integer, Enum> countMatchingPrizes = new HashMap<>() {{
+        countMatchingPrizes.put(FIRST_PRIZE_COUNT, PrizeRank.first);
+        countMatchingPrizes.put(THIRD_PRIZE_COUNT, PrizeRank.third);
+        countMatchingPrizes.put(FOURTH_PRIZE_COUNT, PrizeRank.fourth);
+        countMatchingPrizes.put(FIFTH_PRIZE_COUNT, PrizeRank.fifth);
+    }};
+
     public static void compare(List<Lotto> buyerLottos, int buyerBonusNumber) {
         for (Lotto lotto : buyerLottos) {
             int countSameNumbers = countSameNumbers(lotto);
+            resultStatistics.add(getRank().ordinal(), resultStatistics.get(getRank().ordinal()) + 1);
         }
 
-        int numberOfFirstPrizes = drawFirstPrize(buyerLottos);
-        int numberOfSecondPrizes = drawSecondPrize();
-        int numberOfThirdPrizes = drawThirdPrize(buyerNumbers);
-        int numberOfFourthPrizes = drawFourthPrize(buyerNumbers);
-        int numberOfFifthPrizes = drawFifthPrize(buyerNumbers);
     }
 
     public static int countSameNumbers(Lotto lotto) {
@@ -23,6 +31,10 @@ public class Checker extends LottoKiosk {
             }
         }
         return count;
+    }
+
+    public static Enum getRank(int countSameNumbers) {
+        return countMatchingPrizes.get(countSameNumbers);
     }
 
     public static int drawFirstPrize(List<Integer> buyerNumbers) {
@@ -47,4 +59,8 @@ public class Checker extends LottoKiosk {
     public static int drawFifthPrize(List<Integer> buyerNumbers, List<Integer> winningNumbers) {
         return 0;
     }
+}
+
+enum PrizeRank {
+    first, second, third, fourth, fifth
 }
--- src/main/java/lotto/LottoKiosk.java
+++ src/main/java/lotto/LottoKiosk.java
@@ -1,8 +1,11 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoKiosk {
     static protected List<Integer> winningNumbers;
     static protected int winningBonusNumber;
+    static protected List<Integer> resultStatistics =
+            new ArrayList<Integer>(PrizeRank.values().length);
 }
