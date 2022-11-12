95e226f00f1ca891176905f0839b2a6591d9e0d4
codingvegeta <hong090992@gmail.com>
2022-11-12 22:24:41 +0900

    feat : "로또를 생성하는 기능을 구현했습니다. 테스트 조건을 통과하게 중복이 있을시 오류를 발생하게 작성했습니다."

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        if (numbers.size() == numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또번호는 중복일 수 없습니다.");
+        }
+            validate(numbers);
         this.numbers = numbers;
     }
 
@@ -15,6 +21,7 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
+
 
     // TODO: 추가 기능 구현
 }
