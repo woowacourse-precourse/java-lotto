1c95117f7be49d5bbf40b9534c8fb8abd2a5f767
bfly_notebook <kkyxw@soongsil.ac.kr>
2022-11-12 20:55:40 +0900

    feat: implement the function to make the lotto number list according to purchase price

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,17 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("구입금액을 입력해 주세요.");
         int purchasePrice = getPurchasePrice();
+        int purchaseCount = purchasePrice / 1000;
 
+        List<Lotto> lotteries = QuickPickGenerator.quickPick(purchaseCount);
 
     }
 
---
+++ src/main/java/lotto/QuickPickGenerator.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuickPickGenerator {
+	public static List<Lotto> quickPick(int count) {
+		List<Lotto> lotteries = new ArrayList<>();
+		for (int i = 0; i < count; i++) {
+			lotteries.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+		}
+
+		return lotteries;
+	}
+}
