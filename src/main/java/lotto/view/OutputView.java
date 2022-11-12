1ef770d007018faee427edb933579f22477c98e4
situodys <situodys@gmail.com>
2022-11-13 01:14:54 +0900

    refactor(OutputView): 상수 클래스 분리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,16 +3,9 @@ package lotto.view;
 import lotto.model.Lotto;
 import lotto.model.Lottos;
 
+import static lotto.constant.OutputViewConstants.*;
+
 public class OutputView {
-    private static final String PAY_NOTICE = "구입금액을 입력해 주세요.";
-    private static final String LOTTO_PREFIX = "[";
-    private static final String LOTTO_SUFFIX = "]";
-    private static final String PAY_COUNT_NOTICE = "개를 구매했습니다.";
-    private static final String ERR_PREFIX = "[ERROR] ";
-    private static final String WINNING_LOTTO_INPUT_NOTICE = "당첨 번호를 입력해 주세요.";
-    private static final String BONUSBALL_INPUT_NOTICE = "보너스 번호를 입력해 주세요.";
-
-
 
     public static void announcePayNotice() {
         System.out.println(PAY_NOTICE);
