92c9c25d09ac0bdd990631a85c5a5a9d12b6b6fd
NotableDeveloper <castlereal@naver.com>
2022-11-12 09:09:45 -0800

    <feat> : 당첨 통지 기능 구현 당첨 결과 테이블을 탐색하여 그 결과에 맞게 통지하는 기능을 구현. 관련 테스트 코드 작성 예정.

--- src/main/java/lotto/Judgement.java
+++ src/main/java/lotto/Judgement.java
@@ -3,6 +3,7 @@ package lotto;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Judgement {
     private List<HashMap<Integer, Boolean>> resultTable;
@@ -27,10 +28,7 @@ public class Judgement {
             if (this.lotto.contains(number)) count++;
         }
 
-        if(lotteryNumbers.contains(bonusNumber)) {
-            count++;
-            bonusFlag = true;
-        }
+        if(lotteryNumbers.contains(bonusNumber)) bonusFlag = true;
 
         tableValue.put(count, bonusFlag);
         resultTable.add(tableValue);
@@ -41,5 +39,36 @@ public class Judgement {
             compare(lotteryNumber);
         }
 
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + search(3, false) + "개");
+        System.out.println("4개 일치 (50,000원) - " + search(4, false) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + search(5, false) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + search(5, true) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + search(6, false) + "개");
+        System.out.println("총 수익률은" + "%입니다.");
+    }
+
+    private int search(int count, boolean flag){
+
+        // 5개 일치, 보너스 볼 일치인 경우 조회
+        if(flag){
+            return (int) resultTable.stream()
+                    .filter(map -> map.containsKey(count))
+                    .filter(map -> map.containsValue(flag))
+                    .count();
+        }
+
+        // 5개 일치, 보너스 볼 불일치인 경우 조회
+        if(count == 5) {
+            return (int) resultTable.stream()
+                    .filter(map -> map.containsKey(count))
+                    .filter(map -> map.containsValue(false))
+                    .count();
+        }
+
+        return (int) resultTable.stream()
+                .filter(map -> map.containsKey(count))
+                .count();
     }
 }
