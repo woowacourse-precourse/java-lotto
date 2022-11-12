889d6e593a782f3ddff6df93e3a935f7220ed8ba
NoseSSG <dmlcks1998@naver.com>
2022-11-13 00:30:58 +0900

    [FEAT] 당첨번호가 1 ~ 45가 아닐경우 예외처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -21,7 +21,7 @@ public class Lotto {
 
     // TODO: 추가 기능 구현
     // 당첨번호중 중복이 있을경우 예외처리
-    private void overlap(List<Integer>numbers){
+    private void overlap(List<Integer> numbers){
         Set<Integer> number = new HashSet<Integer>();
         int temp;
         for(int i =0; i < numbers.size();i++){
@@ -30,6 +30,14 @@ public class Lotto {
         }
         if (number.size() !=0 ){
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void overnum(List<Integer> numbers){
+        int temp;
+        for(int i =0;i < numbers.size();i++){
+            temp = numbers.get(i);
+            if(temp>45 || temp <1) throw new IllegalArgumentException();
         }
     }
 }
