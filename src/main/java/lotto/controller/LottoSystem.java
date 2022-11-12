ef64e92189d57b8fe55f1f7ebcae481a935e36e8
myeongju00 <j51677767@gmail.com>
2022-11-12 21:56:52 +0900

    feat : 구매 수량만큼 로또 생성

--- src/main/java/lotto/controller/LottoSystem.java
+++ src/main/java/lotto/controller/LottoSystem.java
@@ -1,10 +1,18 @@
 package lotto.controller;
 
+import lotto.domain.UserLotto;
 import lotto.view.InputView;
 
 public class LottoSystem {
     private static final InputView inputview = new InputView();
     private static final int LOTTO_PRICE = 1000;
+    private UserLotto userLotto;
+
+    private void makeLotto() {
+        userLotto = new UserLotto();
+        int quantity = LottoQuantityCalculation();
+        userLotto.setLottos(quantity);
+    }
 
     private int LottoQuantityCalculation() {
         int userMoney = Integer.parseInt(inputview.inputUserMoney());
---
+++ src/main/java/lotto/domain/UserLotto.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class UserLotto {
+    private List<Lotto> lottos;
+
+    public void setLottos(int lottoQuantity) {
+        this.lottos = new ArrayList<>();
+        for (int i = 0; i < lottoQuantity; i++) {
+            lottos.add(makeLotto());
+        }
+    }
+
+    private Lotto makeLotto() {
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(lottoNumbers);
+        return new Lotto(lottoNumbers);
+    }
+}
