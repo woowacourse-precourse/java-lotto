169678b41734095faedc8b233f094e1661bc7bc1
ym2629 <dydals2629@naver.com>
2022-11-12 13:41:58 +0900

    test: size of winning number

--- src/test/java/lotto/ErrorTest.java
+++ src/test/java/lotto/ErrorTest.java
@@ -40,4 +40,16 @@ public class ErrorTest {
                     .hasMessageContaining("confirmed wrong price");
         }
     }
+
+    @DisplayName("당첨 번호의 크기 테스트")
+    @Test
+    void checkSizeWinningNumber(){
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6, 7);
+
+        if (winningNumber.size() != LottoConst.LOTTO_NUMBER_COUNT) {
+            assertThatThrownBy(() -> { throw new IllegalArgumentException("confirmed wrong size of input"); })
+                    .isInstanceOf(Exception.class)
+                    .hasMessageContaining("confirmed wrong size of input");
+        }
+    }
 }
