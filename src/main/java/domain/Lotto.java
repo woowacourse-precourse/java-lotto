ac9103181fbcba4fdce8bfdcc68eb50553a238fb
박시균\psk98 <psg980331@naver.com>
2022-11-12 18:41:47 +0900

    Feat: 로또 클래스

---
+++ src/main/java/domain/Lotto.java
@@ -0,0 +1,25 @@
+package domain;
+
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+}
