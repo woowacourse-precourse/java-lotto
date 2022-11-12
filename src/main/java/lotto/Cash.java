b3ac0b77b0d11e37b9f0c26384dee332c3195f2d
TaeMin <xoals3094@naver.com>
2022-11-12 09:53:02 +0900

    feat(Cash) : 음수인 경우에 대한 예외처리 추가

--- src/main/java/lotto/Cash.java
+++ src/main/java/lotto/Cash.java
@@ -8,8 +8,12 @@ public class Cash {
         this.cash = cash;
     }
 
-    private void isValidData(int cash) {
+    private void isValidData(int cash) throws IllegalArgumentException {
         final int UNIT = 1000;
+
+        if(cash <= 0){
+            throw new IllegalArgumentException("양수만 허용합니다");
+        }
         if ((cash % UNIT) != 0) {
             throw new IllegalArgumentException("1000원 단위 금액을 입력해주세요");
         }
