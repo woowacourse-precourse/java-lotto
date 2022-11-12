7d5ab8dc9c6e359e06ebc0b63ec8b77e5d92ac81
Jin409 <sualng123@gmail.com>
2022-11-12 10:36:02 +0800

    feat(Customer): 고객의 로또 번호 생성 기능 구현

--- src/main/java/lotto/model/Customer.java
+++ src/main/java/lotto/model/Customer.java
@@ -1,7 +1,10 @@
 package lotto.model;
 
+import static lotto.constants.LottoConstant.LOTTO_PRICE;
+import static lotto.utils.LottoNumberGenerator.generateLottoNumber;
 import static lotto.validator.MoneyValidator.hasValidPaidMoney;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Customer {
@@ -9,11 +12,18 @@ public class Customer {
     private final List<Lotto> lottos;
 
     public Customer(
-            int paidMoney,
-            List<Lotto> lottos
+            int paidMoney
     ) {
         hasValidPaidMoney(paidMoney);
         this.paidMoney = paidMoney;
-        this.lottos = lottos;
+        this.lottos = generateLottosForCustomer(paidMoney);
+    }
+
+    private List<Lotto> generateLottosForCustomer(int paidMoney) {
+        List<Lotto> result = new ArrayList<>();
+        while (result.size() < (paidMoney / LOTTO_PRICE)) {
+            result.add(new Lotto(generateLottoNumber()));
+        }
+        return result;
     }
 }
