81cac7a3699f5d0c6046a73b47c360b57e7770fc
iamjooon2 <iamjooon2@gmail.com>
2022-11-13 03:41:12 +0900

    feat: 사용자가 숫자를 입력했는지 확인하는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,10 +3,16 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String NUMBER_REGEX = "^[0-9]+$";
+    private static final String ERROR_NUMBER_ONLY = "[ERROR] 투입금액은 숫자만 입력가능합니다.";
     private static final String MESSAGE_MONEY_INPUT = "구입금액을 입력해주세요.";
 
-    public static String getMoneyInput() {
+    public static int getMoneyInput() {
         OutputView.printString(MESSAGE_MONEY_INPUT);
-        return Console.readLine();
+        String moneyInput = Console.readLine();
+        if (!moneyInput.matches(NUMBER_REGEX)) {
+            throw new IllegalArgumentException(ERROR_NUMBER_ONLY);
+        }
+        return Integer.parseInt(moneyInput);
     }
 }
