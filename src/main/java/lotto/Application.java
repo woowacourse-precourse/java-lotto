d1f5cc5246fce2f39e6a0db24b302fba47bbb9eb
masiljangajji <hjk172262@gmail.com>
2022-11-13 00:08:42 +0900

    feat: 구입금액 입력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
+
+    public static final int MINIMUM_NUMBER = 1;
+    public static final int MAXIMUM_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println(Message.START.getMessage());
+        String purchaseAmount = Console.readLine();
+        String inputLottoNumber;
+
+        int number=Integer.valueOf(purchaseAmount)/1000;
+
+
+        System.out.printf("%d"+Message.TOTAL.getMessage(),number);
+
+
+
     }
 }
