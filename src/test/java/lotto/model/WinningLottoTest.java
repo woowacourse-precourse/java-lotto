1a36e462998f9d82c96c14d0607930c3cf3fbc93
김현재 <41482946+galug@users.noreply.github.com>
2022-11-12 13:43:44 +0900

    test(winninglottotest): 당첨 로또 번호와 로또 번호를 맞춰보는 테스트 추가

--- src/test/java/lotto/model/WinningLottoTest.java
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -1,12 +1,12 @@
 package lotto.model;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class WinningLottoTest {
 
@@ -36,5 +36,38 @@ class WinningLottoTest {
     void createWinningLottoNumbersContainsBonusNumber() {
         assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 1))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 로또 번호를 기준으로 당첨된 로또 개수를 올바르게 센다..")
+    @Test
+    void countMatchingLottoWithWinningLotto() {
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        int matchingCount = winningLotto.countMatchingLottoNumbers(lotto);
+
+        Assertions.assertThat(matchingCount).isEqualTo(6);
+    }
+
+    @DisplayName("보너스 번호를 가지고 있는 경우 true 가 된다.")
+    @Test
+    void haveBonusNumber() {
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+
+        boolean hasBonusNumber = winningLotto.matchBonusNumber(lotto);
+
+        Assertions.assertThat(hasBonusNumber).isTrue();
+    }
+
+    @DisplayName("보너스 번호를 가지고 있지 않은 경우 false 가 된다.")
+    @Test
+    void haveNoBonusNumber() {
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
+
+        boolean hasBonusNumber = winningLotto.matchBonusNumber(lotto);
+
+        Assertions.assertThat(hasBonusNumber).isFalse();
     }
 }
