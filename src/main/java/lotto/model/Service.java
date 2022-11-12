c97107e4d53a3670b50754a5afe2da7ea15da74b
jinny-l <jinny.l.dev@gmail.com>
2022-11-12 15:56:01 +0900

    feat(Service): player input 및 로또 발행 기능 구현
    - 이유:
    새로운 객체 생성(Money, BonusNumber)으로 인한 새로운 기능 구현

--- src/main/java/lotto/model/Service.java
+++ src/main/java/lotto/model/Service.java
@@ -5,12 +5,57 @@ import java.util.Arrays;
 import java.util.List;
 import lotto.constant.Constant;
 import lotto.util.Convertor;
+import lotto.util.LottoGenerator;
+import lotto.util.PlayerInput;
 import lotto.util.WinnerInfo;
+
+/*
+ * 로또 게임 로직을 담당하는 클래스
+ * 1. 구입 금액에 따른 로또 구매 개수 리턴
+ * 2. 구매 개수만큼 로또(보너스 번호 포함) 발행
+ * 3. 로또 당첨 결과 리턴
+ * 4. 로또 수익률 리턴
+ */
 
 public class Service {
 
+    public static int getMoney() {
+        Money money = new Money(PlayerInput.getInteger());
+        return money.getMoney();
+    }
+
     public static int getLottoAmount(int money) {
-        return money / 1000;
+        return money / Constant.LOTTO_PRICE;
+    }
+
+    public static List<List<Integer>> getLottoNumbers(int lottoAmount) {
+        List<List<Integer>> lottoNumbers = new ArrayList<>();
+
+        for (int i = 0; i < lottoAmount; i++) {
+            Lotto lotto = new Lotto(LottoGenerator.generateLottoNumbers());
+            lottoNumbers.add(lotto.getNumbers());
+        }
+        return lottoNumbers;
+    }
+
+    public static List<Integer> getBonusNumber(int lottoAmount) {
+        List<Integer> bonusNumbers = new ArrayList<>();
+
+        for (int i = 0; i < lottoAmount; i++) {
+            BonusNumber bonusNumber = new BonusNumber(LottoGenerator.generateBonusNumber());
+            bonusNumbers.add(bonusNumber.getNumber());
+        }
+        return bonusNumbers;
+    }
+
+    public static List<Integer> getWinningLottoNumber() {
+        Lotto lotto = new Lotto(PlayerInput.getLottoNumbers());
+        return lotto.getNumbers();
+    }
+
+    public static int getWinningBonusNumber() {
+        BonusNumber bonusNumber = new BonusNumber(PlayerInput.getInteger());
+        return bonusNumber.getNumber();
     }
 
     public static double getProfitRates(int money, int[] lottoResult) {
