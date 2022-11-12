01aa4e1886a03cdb5e498b53232364be0908f306
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-13 01:08:17 +0900

    <feat>(Application.java): application main 메서드 작성
     - 단계별 모듈화 필요

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,34 @@
 package lotto;
 
-public class Application {
-    public static void main(String[] args) {
-        // 1. 구입 금액 입력받기
+import java.util.List;
 
-        // 2. 자동생성된 로또번호 리스트 출력하기
-        // 3. 당첨 번호 입력받기
-        // 4. 보너스 번호 입력받기
-        // 5. 당첨 통계 출력하기
-        // 6. 수익률 출력하기
+public class Application {
+    static Domain domain = new Domain();
+    static UI ui = new UI();
+    private static final String ERROR_MESSAGE = "[ERROR] ";
+
+
+    public static void main(String[] args) {
+        try {
+            // 1. 구입 금액 입력받기
+            String moneyInput = ui.getInputMoney();
+            domain.validatePriceInput(moneyInput);
+            int numberOfLottos = Integer.parseInt(moneyInput) / 1000;
+            // 2. 자동생성된 로또번호 리스트 저장 후 출력하기
+            List<Lotto> lottos = domain.createRandomLottoNumbers(numberOfLottos);
+            ui.printLottoNumber(lottos);
+            // 3. 당첨 번호 입력받기
+            String winningNumberInput = ui.getInputWinningNumber();
+            domain.validateWinningNumberInput(winningNumberInput);
+            List<Integer> winningNumbers = domain.trimLottoNumberInput(winningNumberInput);
+            // 4. 보너스 번호 입력받기
+            String bonusNumberInput = ui.getInputBonusNumber();
+            domain.validateNumberRange(bonusNumberInput);
+            int bonusNumber = Integer.parseInt(bonusNumberInput);
+            // 5. 당첨 통계, 수익률 출력하기
+            ui.printWinningStats(domain.getRankList(lottos, winningNumbers, bonusNumber), numberOfLottos);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_MESSAGE + e.getMessage());
+        }
     }
 }
