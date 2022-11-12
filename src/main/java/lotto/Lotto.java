12143f99e60858641e06e9b69a8199d26b1c46e7
JH-TT <leejongho9803@gmail.com>
2022-11-13 02:20:02 +0900

    feat(Lotto) : 중복 숫자가 있는지 확인하는 함수 구현. 올바른 범위에 있는지 확인하는 함수 구현
    Refactor : Lotto에 validate함수에 중복 숫자 체크하는 함수 호출 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,6 +16,28 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        checkDuplicatedNumber(numbers);
+    }
+
+    private void isCorrectRange(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            int num = numbers.get(i);
+            if(num < 0 || num > 45) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private void checkDuplicatedNumber(List<Integer> numbers) {
+        Set<Integer> targetset = new HashSet<>(numbers);
+        if(targetset.size() != 6) {
+            // 중복되는 숫자가 존재. -> 예외처리
+            throw new IllegalArgumentException();
+        }
     }
 
     public void printSortedNumbers() {
