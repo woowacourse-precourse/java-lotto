ac4d1beeff83e329db7d83d52ae59bfa9ae7284d
jkde7721 <jkde7721@naver.com>
2022-11-12 17:49:39 +0900

    feat(LottoWinningStatus): 인자로 넘겨준 numberOfMatch 값에 따라 enum 값 반환하는 valueOf 메소드 정의

--- src/main/java/lotto/model/LottoWinningStatus.java
+++ src/main/java/lotto/model/LottoWinningStatus.java
@@ -6,7 +6,7 @@ public enum LottoWinningStatus {
 
     MATCH_THREE_NUMBERS(3, 5000L), MATCH_FOUR_NUMBERS(4, 50000L),
     MATCH_FIVE_NUMBERS(5, 1500000L), MATCH_FIVE_NUMBERS_WITH_BONUS_NUMBERS(5, 30000000L),
-    MATCH_SIX_NUMBERS(6, 2000000000L);
+    MATCH_SIX_NUMBERS(6, 2000000000L), NOT_WINNING_STATUS(0, 0);
 
     private final int numberOfMatch;
     private final long winningMoney;
@@ -14,6 +14,15 @@ public enum LottoWinningStatus {
     LottoWinningStatus(int numberOfMatch, long winningMoney) {
         this.numberOfMatch = numberOfMatch;
         this.winningMoney = winningMoney;
+    }
+
+    public static LottoWinningStatus valueOf(int numberOfMatch) {
+        for (LottoWinningStatus winningStatus : LottoWinningStatus.values()) {
+            if (winningStatus.getNumberOfMatch() == numberOfMatch) {
+                return winningStatus;
+            }
+        }
+        return NOT_WINNING_STATUS;
     }
 
     @Override
