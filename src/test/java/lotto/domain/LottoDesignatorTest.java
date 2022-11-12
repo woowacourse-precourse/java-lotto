d8e10100c5d612fc86ae2234b843ef171b01db16
5nam <ohnam90@gmail.com>
2022-11-12 15:11:08 +0900

    test: 입력받은 로또 번호가 6 자리가 아닐 경우 예외가 발생하는 테스트 코드 추가

--- src/test/java/lotto/domain/LottoDesignatorTest.java
+++ src/test/java/lotto/domain/LottoDesignatorTest.java
@@ -14,4 +14,12 @@ public class LottoDesignatorTest {
         assertThatThrownBy(() -> lottoDesignator.checkOnlyNumber(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("입력받은 로또번호가 6 자리가 아닐 경우 예외가 발생한다.")
+    @Test
+    void 로또_번호_자릿수_예외() {
+        String input = "1,2,3,4,5";
+        assertThatThrownBy(() -> lottoDesignator.checkSixCipher(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
