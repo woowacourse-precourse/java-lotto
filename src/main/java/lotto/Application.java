7cc87b6d8e79ba3583c5a634ec0e3a0ce8a8b625
Joon <orijoon98@gmail.com>
2022-11-12 14:23:06 +0900

    feat: complete main method

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import java.util.List;
+import lotto.game.Game;
+import lotto.ui.Input;
+import lotto.ui.Print;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Print.inputPrice();
+        int price = Input.price();
+        Print.newLine();
+        Game game = new Game(price);
+        Print.newLine();
+        Print.inputLottoNumbers();
+        List<Integer> winningLottoNumbers = Input.lottoNumbers();
+        game.setWinningLottoNumbers(winningLottoNumbers);
+        Print.newLine();
+        Print.inputBonusNumber();
+        int bonusNumber = Input.bonusNumber();
+        game.setBonusNumber(bonusNumber);
+        Print.newLine();
+        game.getGameResult();
     }
 }
