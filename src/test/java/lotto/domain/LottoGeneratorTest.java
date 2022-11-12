99193899c1ab3e552b01fb224d23b825f0429f05
5nam <ohnam90@gmail.com>
2022-11-12 13:53:41 +0900

    test: 자동으로 뽑은 번호를 오름차순으로 정렬한다.

--- src/test/java/lotto/domain/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -1,8 +1,10 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -15,5 +17,13 @@ public class LottoGeneratorTest {
         List<Integer> numbers = lottoGenerator.createLotto();
         Lotto lotto = new Lotto(numbers);
         assertThat(lotto.getLotto()).isEqualTo(numbers);
+    }
+
+    @DisplayName("뽑은 번호를 오름차순으로 정렬한다.")
+    @Test
+    void 로또_번호_오름차순() {
+        List<Integer> numbers = List.of(6,5,4,3,2,1);
+        List<Integer> sortNumbers = lottoGenerator.sortLotto(numbers);
+        assertThat(List.of(1,2,3,4,5,6)).isEqualTo(sortNumbers);
     }
 }
