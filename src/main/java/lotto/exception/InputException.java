23e2fbd546d9d808fff50a7c3346f808359d6773
khsrla9806 <kimhunsope@naver.com>
2022-11-12 18:14:26 +0900

    refactor(InputException): ErrorMessage enum을 사용하여 가독성을 높이는 작업 진행

--- src/main/java/lotto/exception/InputException.java
+++ src/main/java/lotto/exception/InputException.java
@@ -1,5 +1,7 @@
 package lotto.exception;
 
+import lotto.resources.ErrorMessage;
+
 import java.util.List;
 import java.util.Set;
 
@@ -8,31 +10,31 @@ public class InputException {
         try {
             Integer.valueOf(purchaseAmount);
         } catch (Exception exception) {
-            System.out.println("[ERROR] 입력된 금액이 숫자가 아닙니다.");
+            System.out.println(ErrorMessage.NOT_NUMBER_ERROR.getErrorMessage());
             throw new IllegalArgumentException();
         }
         if (Integer.valueOf(purchaseAmount) == 0) {
-            System.out.println("[ERROR] 입력된 금액이 0원보다는 커야 합니다.");
+            System.out.println(ErrorMessage.ZERO_WON_ERROR.getErrorMessage());
             throw new IllegalArgumentException();
         }
         if (Integer.valueOf(purchaseAmount) % 1000 != 0) {
-            System.out.println("[ERROR] 입력된 금액이 1000원 단위로 나눠 떨어져야 합니다.");
+            System.out.println(ErrorMessage.UNIT_ERROR.getErrorMessage());
             throw new IllegalArgumentException();
         }
     }
 
     public static void validateWinningNumber(List<Integer> winningNumber) {
         if (winningNumber.size() != 6) {
-            System.out.println("[ERROR] 입력된 숫자의 개수가 6개가 아닙니다.");
+            System.out.println(ErrorMessage.SIZE_ERROR.getErrorMessage());
             throw new IllegalArgumentException();
         }
         if (Set.copyOf(winningNumber).size() != 6) {
-            System.out.println("[ERROR] 중복된 숫자가 입력되었습니다.");
+            System.out.println(ErrorMessage.DUPLICATE_ERROR.getErrorMessage());
             throw new IllegalArgumentException();
         }
         for (Integer number : winningNumber) {
             if (number < 1 || number > 45) {
-                System.out.println("[ERROR] 입력 숫자 범위를 벗어났습니다.");
+                System.out.println(ErrorMessage.RANGE_ERROR.getErrorMessage());
                 throw new IllegalArgumentException();
             }
         }
@@ -40,10 +42,10 @@ public class InputException {
 
     public static void validateBonusNumber(int bonusNumber, List<Integer> winningNumber) {
         if (bonusNumber < 1 || bonusNumber > 45) {
-            throw new IllegalArgumentException("[ERROR] 입력 숫자 범위를 벗어났습니다.");
+            throw new IllegalArgumentException(ErrorMessage.RANGE_ERROR.getErrorMessage());
         }
         if (winningNumber.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 이미 당첨 번호에 있는 숫자 입니다.");
+            throw new IllegalArgumentException(ErrorMessage.ALREADY_EXIST_ERROR.getErrorMessage());
         }
     }
 }
