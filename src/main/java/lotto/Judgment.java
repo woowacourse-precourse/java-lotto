6125b23ec40d63b2d8892432e247b0cecc830527
jeongjihun <wlgns2234@naver.com>
2022-11-12 16:20:31 +0900

    docs: 중복을 체크해주는 기능목록제거
    이미 Randoms.pickUniqueNumbersInRange()에서 중복을 제거하고 입력받도록 되어있음

--- src/main/java/lotto/Judgment.java
+++ src/main/java/lotto/Judgment.java
@@ -3,10 +3,6 @@ package lotto;
 import java.util.List;
 
 public class Judgment {
-    public boolean hasDuplicateNumber(List<Integer> lotto) {
-        return true;
-    }
-
     public int correctCount(List<Integer> winningLotto, List<Integer> userLotto) {
         return 3;
     }
