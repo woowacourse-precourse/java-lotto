87ff5815d69735948f9d554402f62d0a63fbcb82
swonny <wonny921@gmail.com>
2022-11-12 13:26:51 +0900

    feat(Checker): 로또 체커 클래스 뼈대 구현

--- src/main/java/lotto/Checker.java
+++ src/main/java/lotto/Checker.java
@@ -1,4 +1,33 @@
 package lotto;
 
+import java.util.List;
+
 public class Checker extends LottoKiosk {
+    public static void compare(List<Integer> buyerNumbers, List<Integer> winningNumbers) {
+        int numberOfFirstPrizes = drawFirstPrize(buyerNumbers, winningNumbers);
+        int numberOfSecondPrizes = drawSecondPrize(buyerNumbers, winningNumbers);
+        int numberOfThirdPrizes = drawThirdPrize(buyerNumbers, winningNumbers);
+        int numberOfFourthPrizes = drawFourthPrize(buyerNumbers, winningNumbers);
+        int numberOfFifthPrizes = drawFifthPrize(buyerNumbers, winningNumbers);
+    }
+
+    public static int drawFirstPrize(List<Integer> buyerNumbers, List<Integer> winningNumbers) {
+        return 0;
+    }
+
+    public static int drawSecondPrize(List<Integer> buyerNumbers, List<Integer> winningNumbers) {
+        return 0;
+    }
+
+    public static int drawThirdPrize(List<Integer> buyerNumbers, List<Integer> winningNumbers) {
+        return 0;
+    }
+
+    public static int drawFourthPrize(List<Integer> buyerNumbers, List<Integer> winningNumbers) {
+        return 0;
+    }
+
+    public static int drawFifthPrize(List<Integer> buyerNumbers, List<Integer> winningNumbers) {
+        return 0;
+    }
 }
