941a36e0e5d70a1827ebb69cd03af491e8a3ba65
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-11-13 01:55:50 +0900

    feat: 로또 당첨 결과 판정

--- src/main/java/lotto/domain/winningresult/WinningResultType.java
+++ src/main/java/lotto/domain/winningresult/WinningResultType.java
@@ -1,17 +1,33 @@
 package lotto.domain.winningresult;
 
-public enum WinningResultType {
-    FIFTH_PLACE("3개 일치", 5_000),
-    FOURTH_PLACE("4개 일치", 50_000),
-    THIRD_PLACE("5개 일치", 1_500_000),
-    SECOND_PLACE("5개 일치, 보너스 볼 일치", 30_000_000),
-    FIRST_PLACE("6개 일치", 2_000_000_000);
+import java.util.Arrays;
+import java.util.NoSuchElementException;
 
+public enum WinningResultType {
+    FIFTH_PLACE(3,"3개 일치", 5_000),
+    FOURTH_PLACE(4,"4개 일치", 50_000),
+    THIRD_PLACE(5,"5개 일치", 1_500_000),
+    SECOND_PLACE(5,"5개 일치, 보너스 볼 일치", 30_000_000),
+    FIRST_PLACE(6,"6개 일치", 2_000_000_000);
+
+    private int matchedCount;
     private String content;
     private int reward;
 
-    WinningResultType(String content, int reward) {
+    WinningResultType(int matchedCount, String content, int reward) {
+        this.matchedCount = matchedCount;
         this.content = content;
         this.reward = reward;
+    }
+
+    public static WinningResultType getWinningResult(int matchedCount, boolean isBonusMatched) {
+        if (isBonusMatched && matchedCount == 5) {
+            return SECOND_PLACE;
+        }
+
+        return Arrays.stream(values())
+                .filter(result -> matchedCount == result.matchedCount)
+                .findFirst()
+                .orElseThrow();
     }
 }
--- src/test/java/lotto/domain/winningresult/WinningResultTypeTest.java
+++ src/test/java/lotto/domain/winningresult/WinningResultTypeTest.java
@@ -5,10 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningResultTypeTest {
-
     @Test
-    public void 일치하는_숫자에_따른_당첨_결과_생성_테스트() {
-        //given
+    public void 당첨_결과_생성_테스트() {
         int matchedCount = 4;
         boolean isBonusMatched = false;
 
@@ -17,16 +15,38 @@ class WinningResultTypeTest {
         Assertions.assertThat(winningResultType).isEqualTo(WinningResultType.FOURTH_PLACE);
     }
 
-    @DisplayName("당첨_번호_5개와_보너스_번호가_일치하면_5등")
+    @DisplayName("당첨 번호 5개만 일치하면 3등")
     @Test
-    public void 일치하는_숫자에_따른_당첨_결과_생성_테스트_2() {
+    public void 당첨_결과_생성_테스트_3등() {
         //given
+        int matchedCount = 5;
+        boolean isBonusMatched = false;
+
+        WinningResultType winningResultType = WinningResultType.getWinningResult(matchedCount, isBonusMatched);
+
+        Assertions.assertThat(winningResultType).isEqualTo(WinningResultType.THIRD_PLACE);
+    }
+
+    @DisplayName("당첨 번호 5개와 보너스 번호가 일치하면 2등")
+    @Test
+    public void 당첨_결과_생성_테스트_2등() {
         int matchedCount = 5;
         boolean isBonusMatched = true;
 
         WinningResultType winningResultType = WinningResultType.getWinningResult(matchedCount, isBonusMatched);
 
-        Assertions.assertThat(winningResultType).isEqualTo(WinningResultType.FIFTH_PLACE);
+        Assertions.assertThat(winningResultType).isEqualTo(WinningResultType.SECOND_PLACE);
+    }
+
+    @DisplayName("일치하는 당첨 번호가 없으면 반환하지 않음")
+    @Test
+    public void 당첨_결과_생성_테스트_해당_없는_경우() {
+        int matchedCount = 1;
+        boolean isBonusMatched = false;
+
+        Assertions.assertThatThrownBy(() ->
+                WinningResultType.getWinningResult(matchedCount, isBonusMatched))
+                .isInstanceOf(Exception.class);
     }
 
 
