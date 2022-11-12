f65c35cf9f38f5770cb08f7866bd72afc77b7157
JunH <tjdtls690@gmail.com>
2022-11-12 17:07:38 +0900

    :art: refactor(Validator : InputWinningLottoValidator) : 로또 당첨 번호 입력 시, 예외 메시지의 종류를 나누기 위한 정규 표현식 개선

--- src/main/java/lotto/validator/view/InputWinningLottoValidator.java
+++ src/main/java/lotto/validator/view/InputWinningLottoValidator.java
@@ -6,8 +6,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class InputWinningLottoValidator {
-    private static final String WINNING_LOTTO_INPUT_FORM = "((4[0-5]|[1-3]\\d|[1-9]),){5}(4[0-5]|[1-3]\\d|[1-9])";
     private static final String WINNING_LOTTO_INPUT_FORM_EXCEPTION_MESSAGE = "[ERROR] 로또 번호 입력 형식이 잘못 되었습니다.";
+    private static final String EXIST_ZERO_AT_FIRST_PLACE_EXCEPTION_MESSAGE = "[ERROR] 숫자의 1번째 자리는 0일 수 없습니다.";
+    private static final String WINNING_LOTTO_INPUT_FORM = "(\\d+,){5}\\d+";
+    private static final String EXIST_ZERO_AT_FIRST_PLACE_FORM = "0\\d+";
     
     private InputWinningLottoValidator() {
         throw new UtilClassCreateException();
@@ -16,6 +18,7 @@ public class InputWinningLottoValidator {
     public static void validate(final String inputWinningLottoNumbers) {
         validateNullOrEmpty(inputWinningLottoNumbers);
         validateInputWinningLottoFormatMatching(inputWinningLottoNumbers);
+        validateZeroAtFirstPlaceExist(inputWinningLottoNumbers);
     }
     
     private static void validateNullOrEmpty(final String inputPayment) {
@@ -23,20 +26,30 @@ public class InputWinningLottoValidator {
     }
     
     private static void validateInputWinningLottoFormatMatching(final String inputWinningLottoNumbers) {
-        if (isNotMatchInputFormat(matcher(inputWinningLottoNumbers))) {
+        if (isNotMatchInputFormat(matcher(inputWinningLottoNumbers, WINNING_LOTTO_INPUT_FORM))) {
             throw new IllegalArgumentException(WINNING_LOTTO_INPUT_FORM_EXCEPTION_MESSAGE);
         }
     }
     
-    private static Matcher matcher(final String inputWinningLottoNumbers) {
-        return compiler().matcher(inputWinningLottoNumbers);
-    }
-    
-    private static Pattern compiler() {
-        return Pattern.compile(WINNING_LOTTO_INPUT_FORM);
-    }
-    
     private static boolean isNotMatchInputFormat(final Matcher matcher) {
         return !matcher.matches();
+    }
+    
+    private static void validateZeroAtFirstPlaceExist(final String inputWinningLottoNumbers) {
+        if (isExistZeroAtFirstPlace(inputWinningLottoNumbers)) {
+            throw new IllegalArgumentException(EXIST_ZERO_AT_FIRST_PLACE_EXCEPTION_MESSAGE);
+        }
+    }
+    
+    private static boolean isExistZeroAtFirstPlace(final String inputPayment) {
+        return matcher(inputPayment, EXIST_ZERO_AT_FIRST_PLACE_FORM).find();
+    }
+    
+    private static Matcher matcher(final String inputWinningLottoNumbers, final String correctLottoNumbersForm) {
+        return compiler(correctLottoNumbersForm).matcher(inputWinningLottoNumbers);
+    }
+    
+    private static Pattern compiler(final String correctInputForm) {
+        return Pattern.compile(correctInputForm);
     }
 }
