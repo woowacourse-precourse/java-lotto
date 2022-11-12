59d12b0c8641418289efbfe714c3c59c48c06326
woo-chang <clllickme@naver.com>
2022-11-12 21:22:06 +0900

    fix(Rank): Rank가 알아야 하는 개수, 보너스 정보 추가
    - Rank 정보에 알맞은 출력을 위해 toString 구현

--- src/main/java/lotto/entity/Rank.java
+++ src/main/java/lotto/entity/Rank.java
@@ -1,21 +1,47 @@
 package lotto.entity;
 
+import java.util.function.BiFunction;
+
 public enum Rank {
 
-    FIRST(2_000_000_000),
-    SECOND(30_000_000),
-    THIRD(1_500_000),
-    FOURTH(50_000),
-    FIFTH(5_000);
+    FIRST(6, false,2_000_000_000, (count, bonus) -> count == 6),
+    SECOND(5, true, 30_000_000, (count, bonus) -> count == 5 && bonus),
+    THIRD(5, false, 1_500_000, (count, bonus) -> count == 5 && !bonus),
+    FOURTH(4, false, 50_000, (count, bonus) -> count == 4),
+    FIFTH(3, false, 5_000, (count, bonus) -> count == 3),
+    NONE(0, false, 0, (count, bonus) -> count <= 2);
 
+    private final int count;
+    private final boolean bonus;
     private final int prize;
+    private final BiFunction<Integer, Boolean, Boolean> expression;
 
-    Rank(int prize) {
+    Rank(int count, boolean bonus, int prize, BiFunction<Integer, Boolean, Boolean> expression) {
+        this.count = count;
+        this.bonus = bonus;
         this.prize = prize;
+        this.expression = expression;
     }
+
+    public int getCount() { return count; }
 
     public int getPrize() {
         return prize;
+    }
+
+    public boolean win(int count, boolean bonus) {
+        return expression.apply(count, bonus);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        output.append(count).append("개 일치");
+        if (bonus) {
+            output.append(", 보너스 볼 일치");
+        }
+        output.append(" (").append(String.format("%,d", prize)).append("원)");
+        return output.toString();
     }
 
 }
