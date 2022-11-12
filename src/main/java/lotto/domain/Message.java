3221333e410d377eed7bac6fdcdfefbebd28db79
임원정 <oddish0513@gmail.com>
2022-11-12 18:33:35 +0900

    feat(lotto): ui 메세지

---
+++ src/main/java/lotto/domain/Message.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+public enum Message {
+	START("구입금액을 입력해 주세요."),
+	BUY("8개를 구매했습니다."),
+	WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+	BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+	RESULT("당첨 통계\n---")
+	;
+
+	private final String message;
+
+	private Message(String message){
+		this.message = message;
+	}
+	public String get() {
+		return this.message;
+	}
+}
