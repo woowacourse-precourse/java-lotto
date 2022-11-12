b71817cac675d26a1a1eb52da587959f7a52bde4
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-12 20:23:07 +0900

    feat(WinningHistory): 당첨 번호 개수와 보너스 당첨 여부에 따른 등수 정보 반환 기능 추가

--- src/main/java/lotto/WinningHistory.java
+++ src/main/java/lotto/WinningHistory.java
@@ -6,7 +6,7 @@ public enum WinningHistory {
     THIRD_PRIZE(5, 1_500_000),
     FOURTH_PRIZE(4, 50_000),
     FIFTH_PRIZE(3, 5_000),
-    BLANK(0, 0);
+    BLANK(2, 0);
 
     private final int matchNumbers;
     private final int prizeMoney;
@@ -22,5 +22,21 @@ public enum WinningHistory {
 
     public int getPrizeMoney() {
         return prizeMoney;
+    }
+
+    public WinningHistory getWinningHistoryType(int matchNumbers, boolean matchBonus) {
+        if (matchNumbers == FIRST_PRIZE.getMatchNumbers()) {
+            return FIRST_PRIZE;
+        } else if (matchNumbers == SECOND_PRIZE.getMatchNumbers() && matchBonus) {
+            return SECOND_PRIZE;
+        } else if (matchNumbers == THIRD_PRIZE.getMatchNumbers() && !matchBonus || matchNumbers == FOURTH_PRIZE.getMatchNumbers() && matchBonus) {
+            return THIRD_PRIZE;
+        } else if (matchNumbers == FOURTH_PRIZE.getMatchNumbers() && !matchBonus || matchNumbers == FIFTH_PRIZE.getMatchNumbers() && matchBonus) {
+            return FOURTH_PRIZE;
+        } else if (matchNumbers == FIFTH_PRIZE.getMatchNumbers() && !matchBonus || matchNumbers == BLANK.getMatchNumbers() && matchBonus) {
+            return FIFTH_PRIZE;
+        }
+
+        return BLANK;
     }
 }
