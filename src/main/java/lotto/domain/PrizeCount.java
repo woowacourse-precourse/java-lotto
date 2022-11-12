dc51abb250c52e4abc4cac4113d3cb4c4a595125
im-shung <seoyoung1113@gmail.com>
2022-11-12 22:30:19 +0900

    feat: :sparkles: [1~5]등의 개수를 관리하는 기능 생성

---
+++ src/main/java/lotto/domain/PrizeCount.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import java.util.Map;
+
+public class PrizeCount {
+
+    private Map<Prize, Integer> items = Map.of(
+            Prize.FIRST, 0,
+            Prize.SECOND, 0,
+            Prize.THIRD, 0,
+            Prize.FOURTH, 0,
+            Prize.FIFTH, 0
+    );
+
+    public void addPrizeCount(Prize prize) {
+        items.put(prize, items.get(prize) + 1);
+    }
+
+}
