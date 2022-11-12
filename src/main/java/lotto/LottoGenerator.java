3d3102fdcc2cee15977248dbde8efedeab2d8dce
Donghyun <flaehan@gmail.com>
2022-11-12 09:47:54 +0900

    type : feat ✨ 구입 금액 검열작업 추가
    body : 음수인지, 1000원 단위인지, 10만원 한도 내인지 확인하는 기능 추가
    
    footer :

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,6 +1,10 @@
 package lotto;
 
 import static lotto.FinalString.REQUIRE_INPUT_MONEY;
+import static lotto.Validator.validateInputMoney;
+import static lotto.Validator.validateInteger;
+import static lotto.Validator.validateMaxInputMoney;
+import static lotto.Validator.validateNegativePrice;
 
 import camp.nextstep.edu.missionutils.Console;
 
@@ -10,14 +14,22 @@ public class LottoGenerator {
     private static final int MAX_LOTTO_NUM = 45;
     private static final int LOTTO_NUM_LENGTH = 6;
 
+    //금액 검사
+    private void validateMoney(int price) {
+        validateNegativePrice(price);        // 음수 확인
+        validateInputMoney(price);           // 1000원 단위인지 확인
+        validateMaxInputMoney(price);        // 10만원 한도 넘겼는지 확인
+    }
+
     private int inputMoney() {
         System.out.println(REQUIRE_INPUT_MONEY);
-        int inputMoney = Integer.parseInt(Console.readLine());
+        String input = Console.readLine();
+        validateInteger(input);
+        int inputMoney = Integer.parseInt(input);
+        validateMoney(inputMoney);
 
         return inputMoney;
     }
 
-    public int getInputMoney(){
-        return inputMoney();
-    }
 }
+
