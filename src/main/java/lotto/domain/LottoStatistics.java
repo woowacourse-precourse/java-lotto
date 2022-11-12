1979f2b18f45b26031ba8825775db9e9e8ea0b6e
김형기 <khk211113@naver.com>
2022-11-13 00:44:17 +0900

    fix: 로또 결과가 없을 때 수익률이 NaN 발생하지 않도록 수정

--- src/main/java/lotto/domain/LottoStatistics.java
+++ src/main/java/lotto/domain/LottoStatistics.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LottoStatistics {
 
     private int payment = 0;
@@ -17,10 +19,23 @@ public class LottoStatistics {
     }
 
     /**
+     * 여러 번의 로또 결과를 추가합니다.
+     * @param ranks 로또 결과 목록
+     */
+    public void addAll(List<LottoRank> ranks) {
+        for (LottoRank rank : ranks) {
+            add(rank);
+        }
+    }
+
+    /**
      * 수익률을 반환합니다.
      * @return 수익률
      */
     public double getYield() {
+        if (payment == 0) {
+            throw new IllegalArgumentException("최소 한 번의 결과가 존재해야 합니다.");
+        }
         return (double) totalPrize / payment;
     }
 
