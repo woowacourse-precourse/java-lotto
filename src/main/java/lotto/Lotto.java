e0a88c9be86d735ae3b6d7c4597af9fb30ec9f69
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-11-13 04:34:53 +0900

    Feat : 2.-3) ,2.-4) 기능 추가
    경우에 따른 당첨 금액 합산, 합산한 금액 구입 금액 나눠서 수익률 계산
    통계 배열에 저장된 결과와 수익률을 양식에 따라 출력

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -27,15 +27,23 @@ public class Lotto {
             arrayForCheckOverLap[checkIndex] = 1;
         }
     }
-    public int[] checkLottoWinnings(ArrayList<ArrayList<Integer>> lotteryBundleArray, int bonusWinningNumber) {
-        int [] countWinningNumbersCaseArray = new int[6];
+    public ArrayList<BigInteger> checkLottoWinnings(ArrayList<ArrayList<Integer>> lotteryBundleArray, int bonusWinningNumber) {
+        ArrayList<BigInteger> countWinningNumbersCaseArray = new ArrayList<>();
+        BigInteger add1 = new BigInteger("1");
         final int SET_TO_INDEX_VALUE = 3;
+        for (int index = 0; index < 5; index++){
+            countWinningNumbersCaseArray.add(new BigInteger("0"));
+        }
         for (ArrayList<Integer> lottery : lotteryBundleArray){
             int settedIndex = countWinningNumbers(lottery,bonusWinningNumber)-SET_TO_INDEX_VALUE;
             if (settedIndex >= 0) {
-                countWinningNumbersCaseArray[settedIndex]++;
+                countWinningNumbersCaseArray.set(settedIndex, countWinningNumbersCaseArray.get(settedIndex).add(add1));
+                System.out.println(countWinningNumbersCaseArray.get(settedIndex));
             }
         }
+//        for (int index = 0; index < countWinningNumbersCaseArray.size(); index++){
+//            System.out.println(countWinningNumbersCaseArray.get(index));
+//        }
         return countWinningNumbersCaseArray;
     }
 
@@ -52,7 +60,48 @@ public class Lotto {
         return countOfWinningNumbers;
     }
 
+    public void printLottoWinningsResult(ArrayList<BigInteger> resultArray, int buyingAmount){
+        BigInteger sumOfWinAmount = new BigInteger("0");
+        System.out.println("\n당첨 통계\n---");
+        sumOfWinAmount = sumOfWinAmount.add(printCase3Correct(resultArray.get(0)));
+        sumOfWinAmount = sumOfWinAmount.add(printCase4Correct(resultArray.get(1)));
+        sumOfWinAmount = sumOfWinAmount.add(printCase5Correct(resultArray.get(2)));
+        sumOfWinAmount = sumOfWinAmount.add(printCase5BonusCorrect(resultArray.get(4)));
+        sumOfWinAmount = sumOfWinAmount.add(printCase6Correct(resultArray.get(3)));
+        printPercentOfProfit(sumOfWinAmount, Integer.toString(buyingAmount));
+    }
 
+    private BigInteger printCase3Correct(BigInteger countForCase) {
+        BigInteger tempInteger = new BigInteger("5000");
+        System.out.println("3개 일치 (5,000원) - " + countForCase + "개");
+        return tempInteger.multiply(countForCase);
+    }
+    private BigInteger printCase4Correct(BigInteger countForCase) {
+        BigInteger tempInteger = new BigInteger("50000");
+        System.out.println("4개 일치 (50,000원) - " + countForCase + "개");
+        return tempInteger.multiply(countForCase);
+    }
+    private BigInteger printCase5Correct(BigInteger countForCase) {
+        BigInteger tempInteger = new BigInteger("1500000");
+        System.out.println("5개 일치 (1,500,000원) - " + countForCase + "개");
+        return tempInteger.multiply(countForCase);
+    }
+    private BigInteger printCase5BonusCorrect(BigInteger countForCase) {
+        BigInteger tempInteger = new BigInteger("30000000");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + countForCase + "개");
+        return tempInteger.multiply(countForCase);
+    }
+    private BigInteger printCase6Correct(BigInteger countForCase) {
+        BigInteger tempInteger = new BigInteger("2000000000");
+        System.out.println("6개 일치 (2,000,000,000원) - " + countForCase + "개");
+        return tempInteger.multiply(countForCase);
+    }
+    private void printPercentOfProfit(BigInteger sumOfWinAmount, String buyingAmountString) {
+        BigInteger buyingAmount = new BigInteger(buyingAmountString);
 
+        double percentOfProfit = sumOfWinAmount.doubleValue()/buyingAmount.doubleValue()*100;
+        percentOfProfit = Math.round(percentOfProfit * 100)/100.0;
 
+        System.out.println("총 수익률은 "+percentOfProfit+"%입니다.");
+    }
 }
