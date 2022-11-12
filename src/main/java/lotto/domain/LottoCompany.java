ccba6cbe3eda5469e83389d7aabf478ae189f5ed
shpark30 <seohyeonpark30.gmail.com>
2022-11-12 23:42:03 +0900

    feat(LottoCompany): 보너스 번호 입력 및 검증 기능 추가

--- src/main/java/lotto/domain/LottoCompany.java
+++ src/main/java/lotto/domain/LottoCompany.java
@@ -10,21 +10,67 @@ import java.util.stream.Collectors;
  * 로또 당첨 번호 발표
  */
 public class LottoCompany {
-    public Lotto drawWinningNumbers() {
+    private Lotto winningNumbers;
+    private int bonusNumber;
+
+    public void drawWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         List<String> winningNumbers = Arrays.asList(readLine().split(","));
-        validateNumeric(winningNumbers);
-        return new Lotto(winningNumbers.stream()
-                .map(n -> Integer.parseInt(n))
+        validateNumericWinningNumbers(winningNumbers);
+        this.winningNumbers = new Lotto(winningNumbers.stream()
+                .map(Integer::parseInt)
                 .collect(Collectors.toList()));
     }
 
-    private void validateNumeric(List<String> numbers) {
+    public void drawBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String number = readLine();
+        validateNumericBonusNumber(number);
+        int bonusNumber = Integer.parseInt(number);
+        validateRange(bonusNumber);
+        validateDuplicate(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Lotto getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private void validateNumericWinningNumbers(List<String> numbers) {
         for (String number : numbers) {
-            if (!number.matches("^[0-9]+$")) {
+            if (!isNumeric(number)) {
                 System.out.println("[ERROR] 로또 번호는 숫자여야 합니다.");
                 throw new IllegalArgumentException();
             }
         }
+    }
+
+    private void validateNumericBonusNumber(String number) {
+        if (!isNumeric(number)) {
+            System.out.println("[ERROR] 보너스 번호는 숫자여야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRange(int number) {
+        if (number < 1 || number > 45) {
+            System.out.println("[ERROR] 당첨 번호는 1과 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(int number) {
+        if (this.winningNumbers.getNumbers().contains(number)) {
+            System.out.println("[ERROR] 당첨 번호는 로또 번호와 다른 번호를 입력해야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isNumeric(String number) {
+        return number.matches("^[0-9]+$");
     }
 }
--- src/test/java/lotto/domain/LottoCompanyTest.java
+++ src/test/java/lotto/domain/LottoCompanyTest.java
@@ -5,34 +5,69 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-class LottoCompanyTest extends NsTest {
-    @DisplayName("로또 번호를 5개만 뽑으면 예외가 발생한다.")
-    @Test
-    void draw5Numbers() {
-        assertThatThrownBy(() -> run("1,2,3,4,5"))
-                .isInstanceOf(IllegalArgumentException.class);
+class LottoCompanyTest {
+    @Nested
+    class DrawWinningNumbersTest extends NsTest {
+        @DisplayName("로또 번호를 5개만 뽑으면 예외가 발생한다.")
+        @Test
+        void draw5Numbers() {
+            assertThatThrownBy(() -> run("1,2,3,4,5"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("로또 번호를 7개 뽑으면 예외가 발생한다.")
+        @Test
+        void draw7Numbers() {
+            assertThatThrownBy(() -> run("1,2,3,4,5,6,7"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("로또 번호를 정상적으로 6개 뽑는 경우")
+        @Test
+        void draw6Numbers() {
+            run("1,2,3,4,5,6");
+            assertThat(output()).contains("당첨 번호를 입력해 주세요.", "[1, 2, 3, 4, 5, 6]");
+        }
+
+        @Override
+        protected void runMain() {
+            LottoCompany lottoCompany = new LottoCompany();
+            lottoCompany.drawWinningNumbers();
+            System.out.print(lottoCompany.getWinningNumbers().getNumbers());
+        }
     }
 
-    @DisplayName("로또 번호를 7개 뽑으면 예외가 발생한다.")
-    @Test
-    void draw7Numbers() {
-        assertThatThrownBy(() -> run("1,2,3,4,5,6,7"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+    @Nested
+    class DrawBonusNumber extends NsTest {
+        @DisplayName("보너스 번호가 1보다 작으면 예외가 발생한다.")
+        @Test
+        void draw0() {
+            assertThatThrownBy(() -> run("1,2,3,4,5,6", "0"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-    @DisplayName("로또 번호를 정상적으로 6개 뽑는 경우")
-    @Test
-    void draw6Numbers() {
-        run("1,2,3,4,5,6");
-        assertThat(output()).contains("당첨 번호를 입력해 주세요.", "[1, 2, 3, 4, 5, 6]");
-    }
+        @DisplayName("보너스 번호가 45보다 크면 예외가 발생한다.")
+        @Test
+        void draw46() {
+            assertThatThrownBy(() -> run("1,2,3,4,5,6", "46"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-    @Override
-    protected void runMain() {
-        LottoCompany lottoCompany = new LottoCompany();
-        Lotto winningLotto = lottoCompany.drawWinningNumbers();
-        System.out.print(winningLotto.getNumbers());
+        @DisplayName("보너스 번호가 당첨 번호와 중복된 숫자면 예외가 발생한다.")
+        @Test
+        void drawBonusNumberInWinningNumbers() {
+            assertThatThrownBy(() -> run("1,2,3,4,5,6", "1"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Override
+        protected void runMain() {
+            LottoCompany lottoCompany = new LottoCompany();
+            lottoCompany.drawWinningNumbers();
+            lottoCompany.drawBonusNumber();
+        }
     }
 }
