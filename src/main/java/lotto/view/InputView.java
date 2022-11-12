dc502a548aede472f45be9412e937981b4a327d5
watterricecake <fruturum@nate.com>
2022-11-12 20:23:57 +0900

    feat: input money
    input money:
    - get money as string (inputMoney)

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,8 +1,11 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
-    public static void inputMoney(){
+    public static String inputMoney(){
         System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
     }
 
     public static void inputWinningView(){
