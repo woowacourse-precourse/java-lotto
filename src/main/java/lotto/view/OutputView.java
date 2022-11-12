afe208b72f1083c40b111b708f987feeda2d3b55
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-12 20:08:45 +0900

    feat: 보너스 번호 입력 메시지를 출력하는 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -19,4 +19,8 @@ public class OutputView {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
+    public void printBonusNumberMessage() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
 }
