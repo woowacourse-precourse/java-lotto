4c76454cd69df5850136946c5481a43ad7d98e23
Jeongthingamajig <netpetet@naver.com>
2022-11-12 17:28:49 +0900

    Refactor : ValidUserInput 삭제
    유저가 입력한 값의 모든 검증 메소드를 모은것이 아닌 각 값의 개인적인 검증 메소드는 분리하고 공통적인 검증 메소드만을 한 클래스에 모음

--- src/main/java/lotto/domain/ValidUserInput.java
+++ src/main/java/lotto/domain/ValidUserInput.java
@@ -1,7 +1,5 @@
 package lotto.domain;
 
-import java.util.List;
-
 public class ValidUserInput {
     public static void isInputNumberInteger(String inputMoney){
         char check;
@@ -12,26 +10,10 @@ public class ValidUserInput {
             }
         }
     }
-
-    public static void isInputMoneyDivideThousand(String inputMoney){
-        int money = Integer.parseInt(inputMoney);
-        if((money % 1000) != 0){
-            throw new IllegalArgumentException("[ERROR] 입력한 금액이 1000으로 나누어 지지 않습니다.");
-        }
-    }
-
-    public static void isBonusNumberNotSameRandomNumber(int bonusNumber, List<Integer> userInputNumber){
-        if(userInputNumber.contains(bonusNumber)){
-            throw new IllegalArgumentException("[ERROR] 보너스 볼이 자동 생성된 로또 번호와 같습니다.");
-        }
-    }
-
     public static void isInputNumberOverZero(String input){
         int money = Integer.parseInt(input);
         if(money < 0){
             throw new IllegalArgumentException("[ERROR] 음수를 입력 할 수 없습니다.");
         }
     }
-
-
 }
