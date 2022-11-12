718a795000bb84382eb8215605ca0915a7d7356d
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-12 20:14:46 +0900

    feat: 로또 추첨을 진행하는 메서드 추가

--- src/main/java/lotto/controller/DrawLottoController.java
+++ src/main/java/lotto/controller/DrawLottoController.java
@@ -1,4 +1,19 @@
 package lotto.controller;
 
+import lotto.service.DrawLottoService;
+import lotto.view.OutputView;
+
 public class DrawLottoController {
+
+    DrawLottoService drawLottoService = new DrawLottoService();
+    OutputView outputView = new OutputView();
+
+    public void startDrawLotto(){
+        buyLottoFromMyAmount();
+        setWiningLottoNumver();
+        drawLotto();
+        setWiningCount();
+        printWiningCount(drawLottoService.getWiningCount());
+        outputView.printRevenue(getRevenue());
+    }
 }
