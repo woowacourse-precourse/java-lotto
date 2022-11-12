7dbebd83051327bde77755b61d93f06046c6946d
jeomxon <jeomxon@gmail.com>
2022-11-12 22:21:07 +0900

    feat: 당첨 통계에 대한 포맷 추가

--- src/main/java/lotto/domain/PrizeType.java
+++ src/main/java/lotto/domain/PrizeType.java
@@ -1,15 +1,15 @@
 package lotto.domain;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public enum PrizeType {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 30000000),
-    FOURTH(4, 1500000),
     FIFTH(3, 5000),
-    NONE(0, 0);
+    FOURTH(4, 1500000),
+    THIRD(5, 30000000),
+    SECOND(5, 30000000),
+    FIRST(6, 2000000000);
 
     private final int numOfMatched;
     private final int prizeMoney;
@@ -24,9 +24,6 @@ public enum PrizeType {
         if (numOfMatched == 5 && containBonusNumber) {
             return SECOND;
         }
-        if (numOfMatched < 3) {
-            return NONE;
-        }
 
         return Arrays.stream(values())
                 .filter(prizeType -> prizeType.numOfMatched == numOfMatched)
@@ -34,11 +31,13 @@ public enum PrizeType {
                 .orElseThrow(NoSuchElementException::new);
     }
 
-    public int getNumOfMatched() {
-        return numOfMatched;
+    public String getNumOfMatched() {
+        return numOfMatched + "개 일치 ";
     }
 
-    public int getPrizeMoney() {
-        return prizeMoney;
+    public String getPrizeMoney() {
+        DecimalFormat decimalFormat = new DecimalFormat("###,###");
+
+        return "(" + decimalFormat.format(prizeMoney) + "원)";
     }
 }
