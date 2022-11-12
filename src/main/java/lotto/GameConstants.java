c6c7a5600e2b5bc02e9581a7c340e724abe5d7d6
Jimin <thswlals0219@gmail.com>
2022-11-12 15:46:31 +0900

    feat(Constatns): 로또 게임의 상수 클래스 생성

---
+++ src/main/java/lotto/GameConstants.java
@@ -0,0 +1,14 @@
+package lotto;
+
+public final class GameConstants {
+    public static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public static final String OUTPUT_PURCHASE_COUNT = "개를 구매했습니다.";
+    public static final String OUTPUT_WINNING_STATS = "당첨 통계\n---";
+
+    private GameConstants() {
+
+    }
+}
