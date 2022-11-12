74fd792d0d2fc8e67d4a3a9259ff0cf9c302e59b
opuopuo <pureuml.dev@gmail.com>
2022-11-12 21:36:25 +0900

    feat(lotto): 로또 번호 예외처리 기능 추가

--- src/main/java/lotto/domain/Error.java
+++ src/main/java/lotto/domain/Error.java
@@ -1,8 +1,15 @@
 package lotto.domain;
 
 public enum Error {
-    NOT_NUMBER_ERROR("구입금액은 숫자여야 합니다."),
-    MONEY_UNIT_ERROR("구입금액은 1000원 단위여야 합니다.");
+    MONEY_NOT_NUMBER_ERROR("구입금액은 숫자여야 합니다."),
+    MONEY_UNIT_ERROR("구입금액은 1000원 단위여야 합니다."),
+    NUMBER_SIZE_ERROR("로또 번호 숫자는 6개여야 합니다."),
+    NOT_NUMBER_ERROR("로또 번호는 숫자여야 합니다."),
+    DUPLICATE_NUMBER_ERROR("로또 번호는 중복되지 않은 숫자여야 합니다."),
+    OUT_OF_RANGE_NUMBER_ERROR("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    SEPARATOR_ERROR("당첨 번호는 쉼표(,)를 구분자로 넣어주셔야 합니다."),
+    BONUS_NUMBER_SIZE_ERROR("보너스 번호 숫자는 1개여야 합니다."),
+    BONUS_NOT_NUMBER_ERROR("보너스 번호는 숫자여야 합니다.");
 
     private final String message;
 
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,7 +1,13 @@
 package lotto.domain;
 
+import static lotto.domain.Error.DUPLICATE_NUMBER_ERROR;
+import static lotto.domain.Error.NUMBER_SIZE_ERROR;
+import static lotto.domain.Error.OUT_OF_RANGE_NUMBER_ERROR;
+
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private static final int LOTTO_MIN_NUMBER = 1;
@@ -17,19 +23,33 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        isSizeSix(numbers);
+        isInRange(numbers);
+        isDuplicate(numbers);
+    }
+
+    private void isSizeSix(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBER_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NUMBER_SIZE_ERROR.toString());
         }
+    }
+
+    private void isInRange(List<Integer> numbers) {
         for (int number : numbers) {
             if (!(number >= LOTTO_MIN_NUMBER && number <= LOTTO_MAX_NUMBER)) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(OUT_OF_RANGE_NUMBER_ERROR.toString());
             }
+        }
+    }
+
+    private void isDuplicate(List<Integer> numbers) {
+        Set<Integer> lottoNumber = new HashSet<>(numbers);
+        if (numbers.size() != lottoNumber.size()) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR.toString());
         }
     }
 
     public List<Integer> getNumbers() {
         return this.numbers;
     }
-
-    // TODO: 추가 기능 구현
 }
