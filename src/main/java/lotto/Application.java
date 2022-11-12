13dbd2b2f021fcf40977336fe233ebd02b97dd89
KangSungBum <peter9706@naver.com>
2022-11-12 16:36:52 +0900

    feat(Application.java) : 입력 조건 확인 메소드 추가
    checkInputCondition 메소드 추가
    - 사용자로부터 입력 받은 값은 숫자만으로 구성되어야 한다
    - Character.isDigit()으로 숫자인지 체크
    - 숫자가 아닌 다른 문자 포함시 IllegalArgumentException 반환

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,16 @@ public class Application {
         // TODO: 프로그램 구현
     }
 
+    public static int checkInputCondition(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                throw new IllegalArgumentException(ErrorResponse.INPUT_CONTAINS_CHAR_ERROR.getErrorMessage());
+            }
+        }
+
+        return Integer.parseInt(input);
+    }
+
     public static int convertBuyingPriceIntoLotteries(int buyingPrice) {
         if (buyingPrice % 1000 != 0) {
             throw new IllegalArgumentException(ErrorResponse.INPUT_BUYING_RANGE_ERROR.getErrorMessage());
