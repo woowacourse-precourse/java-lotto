325d664f082d9f72a72a8da70f64d0581a6f7934
Laeng <Laeng@users.noreply.github.com>
2022-11-12 21:25:17 +0900

    feat(LottoService): issueLotteryTicketsAs 인스턴스 메서드 추가
    금액에 맞추어 복권을 구매(발행)하고 DB에 저장합니다.

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,28 @@
+package lotto.service;
+
+import lotto.dao.PurchaseNumber;
+import lotto.vo.Lotto;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LottoService {
+    private final PurchaseNumber purchaseNumberDao;
+
+    public LottoService() {
+        this.purchaseNumberDao = PurchaseNumber.getInstance();
+    }
+
+    public void issueLotteryTicketsAs(int money) {
+        if (money <= 0 || (money % 1000) > 0) {
+            throw new IllegalArgumentException("올바른 금액을 입력해 주세요.");
+        }
+
+        int numberOfLotteries = (money / 1000);
+
+        for (int count = 0; count < numberOfLotteries; count++) {
+            Lotto lottery = new Lotto(Lotto.createNumbers());
+            this.purchaseNumberDao.insert(lottery);
+        }
+    }
+}
