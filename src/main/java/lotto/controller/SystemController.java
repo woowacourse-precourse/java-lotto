3f7ad2d9f2ef66d0fc740412c650e654aabeed35
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-12 23:25:05 +0900

    feat: static value and default constructor

---
+++ src/main/java/lotto/controller/SystemController.java
@@ -0,0 +1,40 @@
+package lotto.controller;
+
+import lotto.model.Bonus;
+import lotto.model.Lotto;
+import lotto.model.MatchScore;
+import lotto.model.Money;
+import lotto.model.ScoreResult;
+
+import lotto.util.CountRandomLotto;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static lotto.model.MatchScore.Three;
+import static lotto.model.MatchScore.Four;
+import static lotto.model.MatchScore.Five;
+import static lotto.model.MatchScore.FiveAndBonus;
+import static lotto.model.MatchScore.Six;
+
+public class SystemController {
+    private static int BonusNumber =0;
+    private static String purchaseMoney=new String();
+    private static ScoreResult scoreResult=new ScoreResult();
+    private static List<Integer> numbers=new ArrayList<>();
+    private static List<MatchScore> matchScores=new ArrayList<>();
+    private static List<List<Integer>> myLotto = new ArrayList<>();
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+    private static final CountRandomLotto countRandomLotto = new CountRandomLotto();
+    private static final List<MatchScore> saveScores=List.of(Three, Four, Five, FiveAndBonus, Six);
+
+    public SystemController(){
+    }
+
+
+}
