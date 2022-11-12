8540e74e920618b4427350723e79d5ba4d12526e
hojunking96 <junsong96@naver.com>
2022-11-13 00:45:14 +0900

    feats(java-lotto) : 구입 금액 입력 시 숫자가 아닌 입력 예외 처리 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,25 +4,52 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        int money = Integer.parseInt(Console.readLine());
-        moneyInputError(money);
+        int money = moneyInput();
         List<Lotto> lotto = new ArrayList<>();
         buyLotto(money, lotto);
         printLotto(lotto);
         List<Integer> winNums = winNumInput();
         int bonusNum = bonusNumInput();
         List<Result> results = new ArrayList<>();
+/*
+        int first = 0, second = 0, third = 0, fourth = 0, fifth = 0;
+        for (Result e : results) {
+            if (e.getMatchCnt() == 6)
+                first++;
+            if (e.getMatchCnt() == 5)
+                if (e.isBonusMatch())
+                    second++;
+                else third++;
+            if (e.getMatchCnt() == 4)
+                fourth++;
+            if (e.getMatchCnt() == 3)
+                fifth++;
+            PrintEnd(first, second, third, fourth, fifth, money);
+        }
+        */
+    }
 
+    public static int moneyInput() {
+        String input = Console.readLine();
+        for (int i = 0; i < input.length(); i++) {
+            char tmp = input.charAt(i);
+            if (!Character.isDigit(tmp)) {
+                System.out.println("[ERROR] 로또 구입 금액은 숫자여야 합니다.");
+                throw new IllegalArgumentException();
+            }
+        }
+        int money = Integer.parseInt(input);
+        moneyInputError(money);
+        return money;
     }
 
     public static void moneyInputError(int money) {
         if (money % 1000 != 0) {
-            System.out.println("금액 입력 오류입니다.");
+            System.out.println("[ERROR] 로또 구입 금액은 1000 단위여야 합니다.");
             throw new IllegalArgumentException();
         }
     }
@@ -55,16 +82,18 @@ public class Application {
         for (Lotto e : lotto)
             e.printNumbers();
     }
-
-    public static void PrintEnd(Result result){
+/*
+    public static void PrintEnd(int first, int second, int third, int fourth, int fifth, int money) {
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.println("3개 일치 (5,000)원 - ~개");
-        System.out.println("4개 일치 (50,000)원 - ~개");
-        System.out.println("5개 일치 (1,500,000)원 - ~개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000)원 - ~개");
-        System.out.println("6개 일치 (2,000,000,000원) - ~개");
-        System.out.println("총 수익률은 ~~입니다.");
+        System.out.println("3개 일치 (5,000)원 - " + fifth + "개");
+        System.out.println("4개 일치 (50,000)원 - " + fourth + "개");
+        System.out.println("5개 일치 (1,500,000)원 - " + third + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000)원 - " + second + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + first + "개");
+        int total = fifth * 5000 + fourth * 50000 + third * 1500000 + second * 30000000 + first * 2000000000;
+        float rev = total / money;
+        System.out.println("총 수익률은 " + Math.round(rev * 10) / 10.0 + "입니다.");
     }
-
+*/
 }
