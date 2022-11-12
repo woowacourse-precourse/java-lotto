a50ee0326e67481a99376ce63cdd75c3b77acec4
splitube <voicesplit@daum.net>
2022-11-12 16:13:53 +0900

    test(LottoKiosk): 단위 테스트 작성
    입력값이 Long 으로 잘 변환 저장되는지, 판매할 로또 갯수 계산, 조회 테스트 작성

--- src/test/java/lotto/LottoKioskTest.java
+++ src/test/java/lotto/LottoKioskTest.java
@@ -8,13 +8,14 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class LottoKioskTest {
-    private static final String ERROR_MESSAGE = "[ERROR]";
+    private final int LOTTO_PRICE = 1000;
+    private final String ERROR_MESSAGE = "[ERROR]";
 
     @DisplayName("[오류 테스트] 입력에 숫자가 아닌 것이 존재")
     @Test
     void inputNotNum() {
         //입력값 정의
-        String input = "10000c";
+        String input = "10c000";
         ByteArrayInputStream byteInput = new ByteArrayInputStream(input.getBytes());
         System.setIn(byteInput);
         //given
@@ -36,11 +37,28 @@ class LottoKioskTest {
         //given
         LottoKiosk lottoKiosk = new LottoKiosk();
         lottoKiosk.moneyInserted();
-        lottoKiosk.moneyCharged();
+        lottoKiosk.chargeMoney();
         //when then
         assertThatThrownBy(() -> lottoKiosk.validateMoney())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @DisplayName("문자열로 받은 금액 Long 으로 저장")
+    @Test
+    void chargeMoney() {
+        //입력값 정의
+        String input = "100000";
+        ByteArrayInputStream byteInput = new ByteArrayInputStream(input.getBytes());
+        System.setIn(byteInput);
+        //given
+        LottoKiosk lottoKiosk = new LottoKiosk();
+        lottoKiosk.moneyInserted();
+        //when
+        lottoKiosk.chargeMoney();
+        //then
+        long moneyExpected = Long.parseLong(input);
+        assertThat(lottoKiosk.showInsertedMoney()).isEqualTo(moneyExpected);
     }
 
     @DisplayName("올바른 입력시 금액 조회")
@@ -52,10 +70,28 @@ class LottoKioskTest {
         System.setIn(byteInput);
         //given
         LottoKiosk lottoKiosk = new LottoKiosk();
-        //when
         lottoKiosk.insertMoney();
+        //when
         long chargedMoney = lottoKiosk.showInsertedMoney();
         //then
-        assertThat(chargedMoney).isEqualTo(Long.parseLong(input));
+        long moneyExpected = Long.parseLong(input);
+        assertThat(chargedMoney).isEqualTo(moneyExpected);
+    }
+
+    @DisplayName("몇개의 로또를 파는지 계산")
+    @Test
+    void calculateLottoAmount() {
+        //입력값 정의
+        String input = "20000";
+        ByteArrayInputStream byteInput = new ByteArrayInputStream(input.getBytes());
+        System.setIn(byteInput);
+        //given
+        LottoKiosk lottoKiosk = new LottoKiosk();
+        lottoKiosk.insertMoney();
+        //when
+        lottoKiosk.calculateLottoAmount();
+        //then
+        long howManyLotto = lottoKiosk.showInsertedMoney() / LOTTO_PRICE;
+        assertThat(lottoKiosk.showHowMany()).isEqualTo(howManyLotto);
     }
 }
