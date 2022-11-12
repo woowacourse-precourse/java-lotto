bf836db04fe5aabdf9071158d6aa0a11ff17085a
metorg <metrog950@gmail.com>
2022-11-12 17:29:22 +0900

    feat: 로또번호 유효성 검사 메서드 생성 1~45 번호에 속해있는지, 문자가 포함되어있는지를 파악하는 메서드

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -4,13 +4,9 @@ public class Exception {
 
     //숫자가 아닌 문자 입력시 예외처리
     public boolean isNumeric(int money){
-        try {
-            return true;
-        }catch(NumberFormatException e){
-            return false;
-        }
+
     }
-    
+
     public boolean divThousandOfMoney(int money){
         if(money%1000==0)return true;
         return false;
@@ -18,6 +14,12 @@ public class Exception {
 
     public boolean lessThanThousand(int money){
         if(money<1000)return true;
+        return false;
+    }
+
+    //숫자가 45에 들어가는지
+    public boolean lottoValidator(int number){
+        if(1<=number&&number<=45)return true;
         return false;
     }
 }
