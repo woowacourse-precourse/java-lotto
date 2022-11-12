611c620f2a49b9e3ee5e62891f8fbaaef18dfb06
jihoon-gh <jihoon806@gmail.com>
2022-11-12 23:08:40 +0900

    FEAT - 로또 리스트 생성 및 로또 인스턴스 생성 후 리스트에 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,18 +12,38 @@ public class Application {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("구매금액을 입력해 주세요");
-        Integer inputMoney = Integer.parseInt(sc.nextLine());
+        String inputMoney = sc.nextLine();
+        validateInput(inputMoney);
 
-        validateMoney(inputMoney);
-        Integer nums = inputMoney/lottoPrice;
+        Integer money = Integer.parseInt(sc.nextLine());
+        validateMoney(money);
+        Integer nums = money/lottoPrice;
+
+        List<Lotto> lottos = new ArrayList<>();
+        for(Integer num = 0; num<nums; num++) {
+            List<Integer> lottoNums = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto =new Lotto(lottoNums);
+            lottos.add(lotto);
+        }
+
 
 
 
     }
 
+    static void validateInput(String inputMoney){
+        try{
+            Integer money = Integer.parseInt(inputMoney);
+        }catch(Throwable e){
+            System.out.println("[ERROR] 금액을 정확히 입력해 주시기 바랍니다.");
+            throw new IllegalArgumentException("[ERROR] 금액을 정확히 입력해 주시기 바랍니다.");
+        }
+    }
+
     static void validateMoney(Integer money){
-        if(money%1000!=0){
-            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위여야 합니다.");
+        if(money%1000 != 0){
+            System.out.println("[ERROR] 1000원 단위로 금액을 입력해 주시기 바랍니다.");
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 금액을 입력해 주시기 바랍니다.");
         }
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,13 +3,16 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private  List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicatedNumbers(numbers);
         this.numbers = numbers;
     }
 
@@ -21,6 +24,13 @@ public class Lotto {
 
     private void pickNumberForLotto(){
         numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+    }
+
+    private void validateDuplicatedNumbers(List<Integer> numbers){
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if(numberSet.size()!=numbers.size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
     }
 
     private void printNums() {
