70185620b62b967a536f6de16b9132819095bbea
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-12 21:59:36 +0900

    fix<LottoTest>: DisplayName 수정

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -32,7 +32,7 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
-    @DisplayName("로또 번호의 개수가 5개가 넘어가면 예외가 발생한다.")
+    @DisplayName("로또 번호의 개수가 6개 미만이면 예외가 발생한다.")
     @Test
     void createLottoByUnderSize() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
