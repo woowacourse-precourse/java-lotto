44e34f61fbdacf179d1c3f8f2d9385a02108ecf6
ChanghyunKim <variety82p@gamil.com>
2022-11-12 17:09:11 +0900

    feat(LottoController) : 로또게임을 위한 구매금액 화면 출력 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,11 @@
+package lotto.controller;
+
+import lotto.view.View;
+
+public class LottoController {
+    private static final View view = new View();
+    public void lottoGame(){
+        view.startLottoView();
+    }
+
+}
