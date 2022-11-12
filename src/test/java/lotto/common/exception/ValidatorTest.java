ba6edd67a94787a9c21bcfb703595168dc228502
yeonhee <ydol2@naver.com>
2022-11-13 03:01:25 +0900

    refactor(Validator) : 불필요한 코드 제거

--- src/test/java/lotto/common/exception/ValidatorTest.java
+++ src/test/java/lotto/common/exception/ValidatorTest.java
@@ -1,6 +1,5 @@
 package lotto.common.exception;
 
-import lotto.domain.Lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
