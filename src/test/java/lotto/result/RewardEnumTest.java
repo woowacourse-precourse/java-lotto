6739eb1051c75f55b3377e1899a75cbba3d5e2f4
limjae <limjae96@gmail.com>
2022-11-12 21:59:09 +0900

    style: 안쓰는 import 제거

--- src/test/java/lotto/result/RewardEnumTest.java
+++ src/test/java/lotto/result/RewardEnumTest.java
@@ -1,9 +1,7 @@
 package lotto.result;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class RewardEnumTest {
