4ab75231fc7e648ca8a9968278fd3f0e5d338702
jiwon <ljwon77@daum.net>
2022-11-12 21:20:45 +0900

    fix: remove error output message
    예외 메시지 출력의 경우 IllegalArugmentException을 던질 때 메시지를 처리하기 때문에 출력 메서드를 삭제하였습니다.

--- src/main/java/lotto/util/MessageUtil.java
+++ src/main/java/lotto/util/MessageUtil.java
@@ -1,53 +1,36 @@
 package lotto.util;
 
-import lotto.constant.ErrorOutputMessage;
-import lotto.constant.InputMessage;
-import lotto.constant.OutputMessage;
+import static lotto.constant.InputMessage.*;
+import static lotto.constant.OutputMessage.*;
 
 
 public class MessageUtil {
 
     public void printPurchaseInput() {
-        System.out.println(InputMessage.PURCHASE.getMessage());
+        System.out.println(PURCHASE.getMessage());
     }
 
     public void printWinningInput() {
-        System.out.println(InputMessage.WINNING.getMessage());
+        System.out.println(WINNING.getMessage());
     }
 
     public void printBonusInput() {
-        System.out.println(InputMessage.BONUS.getMessage());
-    }
-
-    public void printPurchaseError() {
-        System.out.println(ErrorOutputMessage.PURCHASE);
-    }
-
-    public void printWinningTypeError() {
-        System.out.println(ErrorOutputMessage.WINNING_TYPE);
-    }
-
-    public void printWinningDuplicateError() {
-        System.out.println(ErrorOutputMessage.WINNING_DUPLICATE);
-    }
-
-    public void printLottoNumError() {
-        System.out.println(ErrorOutputMessage.LOTTO_NUM);
+        System.out.println(BONUS.getMessage());
     }
 
     public void printPurchaseCount() {
-        System.out.println(OutputMessage.PURCHASE_COUNT.getMessage());
+        System.out.println(PURCHASE_COUNT.getMessage());
     }
 
     public void printWinningStats() {
-        System.out.println(OutputMessage.WINNING_STATS.getMessage());
+        System.out.println(WINNING_STATS.getMessage());
     }
 
     public void printWinningStatsResult(int matchCount, int amount, int lottoCount) {
-        System.out.printf(OutputMessage.WINNING_STATS_RESULT.getMessage(), matchCount, amount, lottoCount);
+        System.out.printf(WINNING_STATS_RESULT.getMessage(), matchCount, amount, lottoCount);
     }
 
     public void printEarningRate(Double rate) {
-        System.out.printf(OutputMessage.EARNING_RATE.getMessage(), rate);
+        System.out.printf(EARNING_RATE.getMessage(), rate);
     }
 }
