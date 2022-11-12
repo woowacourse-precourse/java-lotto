49188230caf3575cf79a216c3f8272adfb8cbda0
SHEOMM <esh0821@snu.ac.kr>
2022-11-12 20:50:44 +0900

    feat: 당첨 통계 최상위 메시지 출력

---
+++ src/main/java/lotto/ui/ResultView.java
@@ -0,0 +1,13 @@
+package lotto.ui;
+
+public class ResultView implements View{
+    @Override
+    public void makeView() {
+
+    }
+
+    private void printDefaultMessage(){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+}
