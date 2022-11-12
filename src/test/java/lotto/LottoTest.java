ea5ba01d1c0c056bca6c136241411214fb09c2d2
KangSungBum <peter9706@naver.com>
2022-11-12 15:48:10 +0900

    feat(LottoTest.java) : countCorrespondingNumberTest 추가
    LottoTest : Lotto 번호 일치 순위 반환 메소드 테스트 케이스 추가
    - 주어진 순위에 대한 경우에만 순위(WinningPlace)를 반환
    - 주어진 순위에 존재하지 않는 경우에는 Exception(NOT_IN_LIST) 예외 발생

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,9 +3,11 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +25,130 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 당첨 번호와 사용자의 로또 번호의 일치 여부에 따른 1위 순위 반환 확인")
+    @Test
+    public void countCorrespondingNumbersTest_FirstPlace() throws Exception {
+        //given
+        List<Integer> winnerNumber = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> checkTargetNumber = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        WinningPlace expectedValue = WinningPlace.FIRST_PLACE;
+
+        //when
+        Lotto lotto = new Lotto(winnerNumber);
+        WinningPlace actualValue = lotto.countCorrespondingNumbers(winnerNumber, checkTargetNumber, bonusNumber);
+
+        //then
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @DisplayName("로또 당첨 번호와 사용자의 로또 번호의 일치 여부에 따른 2위 순위 반환 확인")
+    @Test
+    public void countCorrespondingNumbersTest_SecondPlace() throws Exception {
+        //given
+        List<Integer> winnerNumber = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> checkTargetNumber = Arrays.asList(1, 2, 3, 4, 5, 7);
+        int bonusNumber = 7;
+        WinningPlace expectedValue = WinningPlace.SECOND_PLACE;
+
+        //when
+        Lotto lotto = new Lotto(winnerNumber);
+        WinningPlace actualValue = lotto.countCorrespondingNumbers(winnerNumber, checkTargetNumber, bonusNumber);
+
+        //then
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @DisplayName("로또 당첨 번호와 사용자의 로또 번호의 일치 여부에 따른 3위 순위 반환 확인")
+    @Test
+    public void countCorrespondingNumbersTest_ThirdPlace() throws Exception {
+        //given
+        List<Integer> winnerNumber = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> checkTargetNumber = Arrays.asList(1, 2, 3, 4, 5, 7);
+        int bonusNumber = 8;
+        WinningPlace expectedValue = WinningPlace.THIRD_PLACE;
+
+        //when
+        Lotto lotto = new Lotto(winnerNumber);
+        WinningPlace actualValue = lotto.countCorrespondingNumbers(winnerNumber, checkTargetNumber, bonusNumber);
+
+        //then
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @DisplayName("보너스 번호 일치로 인한 3위 인정은 예외가 발생한다.")
+    @Test
+    public void countCorrespondingNumbersTest_ThirdPlace_BonusNumberException() throws Exception {
+        //given
+        List<Integer> winnerNumber = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> checkTargetNumber = Arrays.asList(1, 2, 3, 4, 7, 8);
+        int bonusNumber = 8;
+        //when
+
+        //then
+        assertThatThrownBy(() -> new Lotto(winnerNumber).countCorrespondingNumbers(winnerNumber, checkTargetNumber, bonusNumber))
+                .isInstanceOf(Exception.class);
+    }
+
+    @DisplayName("로또 당첨 번호와 사용자의 로또 번호의 일치 여부에 따른 4위 순위 반환 확인")
+    @Test
+    public void countCorrespondingNumbersTest_FourthPlace() throws Exception {
+        //given
+        List<Integer> winnerNumber = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> checkTargetNumber = Arrays.asList(1, 2, 3, 4, 7, 8);
+        int bonusNumber = 9;
+        WinningPlace expectedValue = WinningPlace.FOURTH_PLACE;
+
+        //when
+        Lotto lotto = new Lotto(winnerNumber);
+        WinningPlace actualValue = lotto.countCorrespondingNumbers(winnerNumber, checkTargetNumber, bonusNumber);
+
+        //then
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @DisplayName("보너스 번호 일치로 인한 4위 인정은 예외가 발생한다.")
+    @Test
+    public void countCorrespondingNumbersTest_FourthPlace_BonusNumberException() throws Exception {
+        //given
+        List<Integer> winnerNumber = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> checkTargetNumber = Arrays.asList(1, 2, 3, 7, 8, 9);
+        int bonusNumber = 9;
+        //when
+
+        //then
+        assertThatThrownBy(() -> new Lotto(winnerNumber).countCorrespondingNumbers(winnerNumber, checkTargetNumber, bonusNumber))
+                .isInstanceOf(Exception.class);
+    }
+
+    @DisplayName("로또 당첨 번호와 사용자의 로또 번호의 일치 여부에 따른 5위 순위 반환 확인")
+    @Test
+    public void countCorrespondingNumbersTest_FifthPlace() throws Exception {
+        //given
+        List<Integer> winnerNumber = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> checkTargetNumber = Arrays.asList(1, 2, 3, 7, 8, 9);
+        int bonusNumber = 10;
+        WinningPlace expectedValue = WinningPlace.FIFTH_PLACE;
+
+        //when
+        Lotto lotto = new Lotto(winnerNumber);
+        WinningPlace actualValue = lotto.countCorrespondingNumbers(winnerNumber, checkTargetNumber, bonusNumber);
+
+        //then
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @DisplayName("당첨되지 않은 경우에는 NOT_IN_LIST 예외가 발생한다.")
+    @Test
+    public void countCorrespondingNumbersTest_NotInList() throws Exception{
+        //given
+        List<Integer> winnerNumber = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> checkTargetNumber = Arrays.asList(1, 2, 10, 11, 12, 13);
+        int bonusNumber = 14;
+        //when
+
+        //then
+        assertThatThrownBy(() -> new Lotto(winnerNumber).countCorrespondingNumbers(winnerNumber, checkTargetNumber, bonusNumber))
+                .isInstanceOf(Exception.class);
+    }
 }
