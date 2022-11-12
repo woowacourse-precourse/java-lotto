0bc7ec2d4bd5f8a06b9f7cbe19670b4f84416258
jaehyeon <jhjh0022@naver.com>
2022-11-13 02:31:34 +0900

    feat: 수익률 계산 해주는 기능 추가

---
+++ src/main/java/lotto/domain/Calculator.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Calculator {
+
+    public static double df(List<Integer> rewardList, int money){
+        double moneys = money;
+        double total = totalRewardmoney(rewardList);
+
+        return (double)Math.round((total/moneys)*10000)/1000);
+    }
+
+    private static int totalRewardmoney(List<Integer> rewardList){
+        int sum = 0;
+        for(int money: rewardList){
+            sum += money;
+        }
+        return sum;
+    }
+
+}
