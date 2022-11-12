a58735aba12c205f75ba8a4b111ce3e866ec8a24
leeyongjun <leeyongjun0310@naver.com>
2022-11-13 02:23:25 +0900

    feat(lotto): 1 ~ 45 사이의 임의의 6자리 로또 번호를 발행 수 만큼 생성하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,19 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Generator;
 import lotto.domain.Purchase;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int purchaseAmount = askPurchaseAmount();
         int lottoCount = Purchase.getLottoCount(purchaseAmount);
-        System.out.println(lottoCount);
+        Generator generator = new Generator(lottoCount);
+        List<List<Integer>> lottos = generator.createLotto();
+        System.out.println(lottos);
     }
 
     private static int askPurchaseAmount() {
--- src/main/java/lotto/domain/Generator.java
+++ src/main/java/lotto/domain/Generator.java
@@ -1,4 +1,23 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Generator {
+    private final int lottoCount;
+
+    public Generator(int lottoCount) {
+        this.lottoCount = lottoCount;
+    }
+
+    public List<List<Integer>> createLotto() {
+        List<List<Integer>> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
 }
