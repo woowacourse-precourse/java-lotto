7961c4629823d39bf295919eebed4f4bd0a0118c
watterricecake <fruturum@nate.com>
2022-11-12 20:18:24 +0900

    feat: inputView (inputMoney/inputWinningView/inputBonusView)
    print text about input event:
    - text about money input (inputMoney)
    - text about winning numbers input (inputWinningView)
    - text about bonus number input (inputBonusView)

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+public class InputView {
+    public static void inputMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void inputWinningView(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void inputBonusView(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}
