221a58c216b0ccb0d1de3a660fe39f2d7510aa96
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-12 21:43:33 +0900

    refactor : 당첨번호, 보너스번호 저장 및 전달 방식 변경

--- src/main/java/lotto/domain/player/Player.java
+++ src/main/java/lotto/domain/player/Player.java
@@ -1,21 +1,10 @@
 package lotto.domain.player;
 
-import lotto.domain.Validator;
 import lotto.ui.ConsoleInput;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static java.lang.Integer.*;
 import static lotto.ui.ConsoleOutput.*;
 
 public class Player {
-
-	public static List<Integer> winningNumber;
-	public static List<Integer> bonusNumber;
 
 	public String receivePurchaseAmount() {
 		PrintRequestMessage(REQUEST_TOTAL_AMOUNT);
@@ -30,6 +19,5 @@ public class Player {
 	public String receiveBonusNumber() {
 		PrintRequestMessage(REQUEST_BONUS_NUMBER);
 		return ConsoleInput.readLine();
-
 	}
 }
