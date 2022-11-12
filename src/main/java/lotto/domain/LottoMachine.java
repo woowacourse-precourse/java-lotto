ec4c3b651adc655ff5b04ca666963eb5a2ac1df6
jini_11 <jieun21124@naver.com>
2022-11-12 18:00:28 +0900

    feat: 사용자 입력 예외 처리 조건 추가

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -3,7 +3,7 @@ package lotto.domain;
 public class LottoMachine {
     private static final String TYPE_ERROR_MESSAGE = "[ERROR] 숫자만 입력 가능합니다.";
     private static final String UNIT_ERROR_MESSAGE = "[ERROR] 1,000 단위의 수만 입력 가능합니다.";
-
+    private static final String RANGE_ERROR_MESSAGE = "[ERROR] 1,000 이상의 수만 입력 가능합니다.";
     private static final int UNIT_PRICE = 1000;
 
     private final int purchasePrice;
@@ -11,13 +11,19 @@ public class LottoMachine {
     public LottoMachine(String inputPrice) {
         validateType(inputPrice);
         this.purchasePrice = Integer.parseInt(inputPrice);
+        validateRange();
         validateUnit();
-
     }
 
     public void validateType(String inputPrice) {
         if (!inputPrice.matches("^[0-9]*$")) {
             throw new IllegalArgumentException(TYPE_ERROR_MESSAGE);
+        }
+    }
+
+    public void validateRange() {
+        if (purchasePrice < UNIT_PRICE) {
+            throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
         }
     }
 
