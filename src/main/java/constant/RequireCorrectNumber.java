c8b2ec4a34d818866c05e9510e8067bbd2020344
김동규 <2016025196@hanyang.ac.kr>
2022-11-12 21:39:01 +0900

    로또번호 개수 enum클래스 하나로 통합

--- src/main/java/constant/RequireCorrectNumber.java
+++ src/main/java/constant/RequireCorrectNumber.java
@@ -1,15 +1,22 @@
 package constant;
 
 public enum RequireCorrectNumber {
-    FIRST_CORRECT(6), SECOND_CORRECT(5), THIRD_CORRECT(5), FOURTH_CORRECT(4), FIFTH_CORRECT(3);
+    FIRST_CORRECT(6,0), SECOND_CORRECT(5,1),
+    THIRD_CORRECT(5,0), FOURTH_CORRECT(4,0),
+    FIFTH_CORRECT(3,0);
 
 
-    private final int value;
-        RequireCorrectNumber(int value){
-        this.value = value;
+    private final int normalNumberCount;
+    private final int bonusNumberCount;
+        RequireCorrectNumber(int normalNumber, int bonusNumber){
+        this.normalNumberCount = normalNumber;
+        this.bonusNumberCount = bonusNumber;
     }
 
-    public int getValue(){
-        return value;
+    public int getNormalCount(){
+        return normalNumberCount;
+    }
+    public int getBonusNumberCount(){
+        return bonusNumberCount;
     }
 }
