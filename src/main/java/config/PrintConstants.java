01224e2747410f9f043d49190c5b9c4e387df625
김형기 <khk211113@naver.com>
2022-11-13 01:44:14 +0900

    chore: 출력되는 문자열을 상수 클래스에서 분리하여 관리

--- src/main/java/config/PrintConstants.java
+++ src/main/java/config/PrintConstants.java
@@ -1,4 +1,11 @@
 package config;
 
 public final class PrintConstants {
+    
+    public static final String INPUT_PAYMENT = "구입금액을 입력해주세요.";
+    public static final String INPUT_LUCKY_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    
+    public static final String BUY_COMPLETE = "개를 구매했습니다.";
+
 }
