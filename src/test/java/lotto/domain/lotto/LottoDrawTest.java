411ff14ee9667fe5bfadfe66d3546c719b0a89c4
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-11-13 02:07:04 +0900

    test(LottoDrawTest): 추첨 번호와 로또 번호를 대조하여 당첨 결과 확인 테스트

--- src/test/java/lotto/domain/lotto/LottoDrawTest.java
+++ src/test/java/lotto/domain/lotto/LottoDrawTest.java
@@ -2,6 +2,7 @@ package lotto.domain.lotto;
 
 import java.util.List;
 import lotto.common.InputErrors;
+import lotto.domain.winningresult.WinningResultType;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +20,46 @@ class LottoDrawTest {
                 .hasMessageContaining(ERROR_PREFIX)
                 .hasMessageContaining(InputErrors.BONUS_NUMBER_DUPLICATED_WIN_NUMBER.getMessage());
     }
+
+    @Test
+    public void 추첨_결과와_로또_번호_결과_확인_테스트_5등() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoDraw lottoDraw = LottoDraw.of(List.of(1, 3, 5, 7, 9, 11), 4);
+
+        //when
+        WinningResultType winningResultType = lottoDraw.match(lotto);
+
+        //then
+        Assertions.assertThat(winningResultType).isEqualTo(WinningResultType.FIFTH_PLACE);
+    }
+
+    @Test
+    public void 추첨_결과와_로또_번호_결과_확인_테스트_3등() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoDraw lottoDraw = LottoDraw.of(List.of(1, 2, 3, 4, 5, 7), 8);
+
+        //when
+        WinningResultType winningResultType = lottoDraw.match(lotto);
+
+        //then
+        Assertions.assertThat(winningResultType).isEqualTo(WinningResultType.THIRD_PLACE);
+    }
+
+    @Test
+    public void 추첨_결과와_로또_번호_결과_확인_테스트_2등() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoDraw lottoDraw = LottoDraw.of(List.of(1, 2, 3, 4, 5, 7), 6);
+
+        //when
+        WinningResultType winningResultType = lottoDraw.match(lotto);
+
+        //then
+        Assertions.assertThat(winningResultType).isEqualTo(WinningResultType.SECOND_PLACE);
+    }
+
 
 
 }
