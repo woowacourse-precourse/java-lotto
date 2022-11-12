0e338f8098c132cf6b9988e25001dd47638340af
yg0585 <yg0585@naver.com>
2022-11-12 16:45:16 +0900

    style: 사용하지 않는 import 제거

--- src/test/java/lotto/domain/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -6,7 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 
 public class LottoGeneratorTest {
 
