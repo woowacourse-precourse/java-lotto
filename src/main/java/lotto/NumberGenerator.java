f79c650c0318bb3b9121e1f74136045d3aafa416
은지 <midzy0228@gmail.com>
2022-11-12 18:23:21 +0900

    feat(로또 구매): 구입금액의 1000으로 나눈 몫만큼 로또 구매

--- src/main/java/lotto/NumberGenerator.java
+++ src/main/java/lotto/NumberGenerator.java
@@ -2,10 +2,17 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NumberGenerator {
-    public List<Integer> createRandomNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    public List<List<Integer>> createRandomNumbers(Integer lottoTickets) {
+        List<List<Integer>> lotto = new ArrayList<>();
+        for (int count = 0; count < lottoTickets; count++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lotto.add(numbers);
+            System.out.println(numbers);
+        }
+        return lotto;
     }
 }
