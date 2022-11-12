dcda308c3004af54b4f94124dd88f1de11582edb
jaehyeon <jhjh0022@naver.com>
2022-11-12 20:57:03 +0900

    feat: 당첨번호 입력받고 검증하는 기능 구현

---
+++ src/main/java/lotto/console/WinningNumberInput/InputWinningNumber.java
@@ -0,0 +1,52 @@
+package lotto.console.WinningNumberInput;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class InputWinningNumber {
+
+    public List<Integer> getAndVerifyNumber() {
+        List<Integer> numbers = getWinningNumber();
+        verifyBetween1and45(numbers);
+        verifyDuplicateNumber(numbers);
+        verifySixNumber(numbers);
+
+        return numbers;
+    }
+
+
+    public List<Integer> getWinningNumber() {
+        return parseWinningNumber(Console.readLine());
+    }
+
+    public static List<Integer> parseWinningNumber(String winningNumber) {
+        return Stream.of(winningNumber.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public void verifyBetween1and45(List<Integer> winningNumbers) {
+        for (int number : winningNumbers) {
+            if (!(1 <= number && number <= 45)) {
+                throw new RuntimeException("[ERROR] 1 ~ 45 사이의 숫자만 입력해 주세요");
+            }
+        }
+    }
+
+    public void verifyDuplicateNumber(List<Integer> winningNumbers) {
+        if (!(winningNumbers.size() == winningNumbers.stream().distinct().count())) {
+            throw new RuntimeException("[ERROR] 중복인 숫자가 있습니다.");
+        }
+    }
+
+    public void verifySixNumber(List<Integer> winningNumbers) {
+        if (winningNumbers.size() == 6) {
+            throw new RuntimeException("[ERROR] 6개의 숫자를 입력하세여");
+        }
+    }
+
+
+}
