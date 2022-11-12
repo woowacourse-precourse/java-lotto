03b2047cdd26ef7e8504bd54d788290b9bdbe01f
im-shung <seoyoung1113@gmail.com>
2022-11-12 23:18:28 +0900

    test: :white_check_mark: 등수의 합산 금액이 맞는지 확인하는 테스트 구현

--- src/test/java/lotto/domain/PrizeCountTest.java
+++ src/test/java/lotto/domain/PrizeCountTest.java
@@ -22,4 +22,9 @@ class PrizeCountTest {
         assertThat(prizeCount.getPrizeCount(Prize.FIFTH)).isEqualTo(2);
     }
 
+    @DisplayName("$등수의 합산 금액이 맞는지 확인하기")
+    @Test
+    void getTotalPrizeMoney() {
+        assertThat(prizeCount.getTotalPrizeMoney(Prize.FIFTH)).isEqualTo(Prize.FIFTH.getMoney() * 2);
+    }
 }
