797c40e4e80f4716c336e41e8af8670bb48d4ef1
iamjooon2 <iamjooon2@gmail.com>
2022-11-13 05:18:40 +0900

    feat: 일치 개수, 해당 등수, 나온 횟수를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,10 +2,19 @@ package lotto.view;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import lotto.domain.Calculator;
+import lotto.domain.Rank;
 
 public class OutputView {
     private static final String UNIT = "개";
     private static final String MESSAGE_BUY_SUFFIX = "를 구매했습니다.";
+    private static final String MESSAGE_OUTPUT_INIT = "당첨 통계\n---";
+    private static final String MESSAGE_FIFTH_GRADE = "3개 일치 (5,000원) - ";
+    private static final String MESSAGE_FOURTH_GRADE = "4개 일치 (50,000원) - ";
+    private static final String MESSAGE_THIRD_GRADE = "5개 일치 (1,500,000원) - ";
+    private static final String MESSAGE_SECOND_GRADE = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    private static final String MESSAGE_FIRST_GRADE = "6개 일치 (2,000,000,000원) - ";
+
 
     public static void printLotteryCount(Integer count) {
         System.out.println(count.toString() + UNIT + MESSAGE_BUY_SUFFIX);
@@ -18,6 +27,15 @@ public class OutputView {
     public static void printCreatedNumbers(List<Integer> numbers) {
         List<Integer> sortedList = numbers.stream().sorted().collect(Collectors.toList());
         System.out.println(sortedList);
+    }
+
+    public static void printWinningInfo(Calculator rankCountMap) {
+        System.out.println(MESSAGE_OUTPUT_INIT);
+        System.out.println(MESSAGE_FIFTH_GRADE + rankCountMap.getCount(Rank.FIFTH) + UNIT);
+        System.out.println(MESSAGE_FOURTH_GRADE + rankCountMap.getCount(Rank.FOURTH) + UNIT);
+        System.out.println(MESSAGE_THIRD_GRADE + rankCountMap.getCount(Rank.THIRD) + UNIT);
+        System.out.println(MESSAGE_SECOND_GRADE + rankCountMap.getCount(Rank.SECOND) + UNIT);
+        System.out.println(MESSAGE_FIRST_GRADE + rankCountMap.getCount(Rank.FIRST) + UNIT);
     }
 
 }
