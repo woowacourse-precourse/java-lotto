25a1a3224ec3df4b50ac96133c40095e77270f52
H4R1B0 <qkrguswns25@naver.com>
2022-11-12 16:24:53 +0900

    chore: change access modifier and method order

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -4,23 +4,28 @@ import java.util.HashSet;
 import java.util.List;
 
 public class Validation {
-    public static boolean isMultipleOf1000(int number) throws IllegalArgumentException {
-        if (number % 1000 != 0)
-            throw new IllegalArgumentException(Notification.ERROR_MESSAGE_MONEY_IS_NOT_MULTIPLE_1000.getMessage());
-        return true;
+    public Validation(List<Integer> numbers){
+        isLottoDuplicate(numbers);
+        isLottoBetween1And45(numbers);
     }
 
-    public static void isLottoBetween1And45(List<Integer> numbers) throws IllegalArgumentException {
+    private void isLottoBetween1And45(List<Integer> numbers) throws IllegalArgumentException {
         for (int number : numbers) {
             if (number < 1 || number > 45)
                 throw new IllegalArgumentException(Notification.ERROR_MESSAGE_NUMBER_IS_NOT_BETWEEN_1_AND_45.getMessage());
         }
     }
 
-    public static void isLottoDuplicate(List<Integer> numbers) throws IllegalArgumentException {
+    private void isLottoDuplicate(List<Integer> numbers) throws IllegalArgumentException {
         HashSet<Integer> set = new HashSet<>(numbers);
         if (set.size() != 6)
             throw new IllegalArgumentException(Notification.ERROR_MESSAGE_NUMBER_IS_DUPLICATE.getMessage());
+    }
+
+    public static boolean isMultipleOf1000(long number) throws IllegalArgumentException {
+        if (number % 1000 != 0)
+            throw new IllegalArgumentException(Notification.ERROR_MESSAGE_MONEY_IS_NOT_MULTIPLE_1000.getMessage());
+        return true;
     }
 
     public static boolean isBonusInLotto(List<Integer> numbers, int bonus) throws IllegalArgumentException {
