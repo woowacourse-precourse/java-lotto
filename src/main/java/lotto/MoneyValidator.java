75e62cf76b1ea2f892e78dc76e7ef2f698f5a6d9
Cho SangHyun <98shcho@naver.com>
2022-11-12 17:37:01 +0900

    feat(금액예외처리기능): 로또가격으로 나누어떨어지지 않으면 오류내는 기능 추가

--- src/main/java/lotto/MoneyValidator.java
+++ src/main/java/lotto/MoneyValidator.java
@@ -1,6 +1,8 @@
 package lotto;
 
 public class MoneyValidator {
+    private static int COST_OF_LOTTO = 1000;
+
     private static boolean isNumeric(String moneyInput) {
         for (int index = 0; index < moneyInput.length(); index++) {
             if (!Character.isDigit(moneyInput.charAt(index))) {
@@ -10,8 +12,19 @@ public class MoneyValidator {
         return true;
     }
 
+    private static boolean isDividedByConstOfLotto(String moneyInput) {
+        int money = Integer.parseInt(moneyInput);
+        if (money == 0 || money % COST_OF_LOTTO != 0) {
+            return false;
+        }
+        return true;
+    }
+
     public static void validateMoney(String moneyInput) {
         if (!isNumeric((moneyInput))) {
+            throw new IllegalArgumentException();
+        }
+        if (!isDividedByConstOfLotto(moneyInput)) {
             throw new IllegalArgumentException();
         }
     }
