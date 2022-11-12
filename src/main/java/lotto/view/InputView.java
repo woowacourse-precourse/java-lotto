4af40f86adb108f9299a837311a74da3d3d3b97d
shinD <huipulci1@naver.com>
2022-11-13 04:30:25 +0900

    feat: 화면에 보여질 기능등을 담당하는 View 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,57 @@
+package lotto.view;
+
+import lotto.domain.lotto.Lotto;
+import lotto.domain.money.Money;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static java.lang.Integer.parseInt;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
+import static lotto.infra.port.inbound.StandardInput.readLine;
+import static lotto.infra.port.outbound.StandardOutput.println;
+
+public class InputView {
+
+    public static Money inputPurchaseMoney() {
+        println("구입금액을 입력해 주세요.");
+
+        int input = inputNumber();
+        println("");
+
+        return Money.valueOf(input);
+    }
+
+    public static Lotto inputWinningLottoNumber() {
+        println("당첨 번호를 입력해 주세요.");
+
+        List<Integer> inputNumbers = toIntegers(readLine());
+        println("");
+
+        return new Lotto(inputNumbers);
+    }
+
+    public static int inputBonusNumber() {
+        println("보너스 번호를 입력해 주세요.");
+
+        int bonusNumber = inputNumber();
+        println("");
+
+        return bonusNumber;
+    }
+
+    private static int inputNumber() {
+        try {
+            return parseInt(readLine());
+        } catch (RuntimeException e) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    private static List<Integer> toIntegers(String input) {
+        return stream(input.split(","))
+                .map(Integer::parseInt)
+                .collect(toList());
+    }
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,68 @@
+package lotto.view;
+
+import lotto.controller.result.LottoPurchaseResult;
+import lotto.controller.result.WinningResult;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.rank.Rank;
+import lotto.domain.statistics.Statistics;
+
+import java.util.List;
+
+import static java.lang.String.format;
+import static lotto.domain.rank.Rank.*;
+import static lotto.infra.port.outbound.StandardOutput.println;
+
+public class OutputView {
+
+    private static final List<Rank> RANKS = List.of(FIFTH, FOURTH, THIRD, SECOND, FIRST);
+
+    public static void printPurchaseLottoCount(LottoPurchaseResult result) {
+        List<Lotto> lottos = result.getLottos();
+
+        println(format("%d개를 구매했습니다.", lottos.size()));
+
+        lottos.forEach(lotto -> println(lotto.toString()));
+
+        println("");
+    }
+
+    public static void printStatistic(WinningResult result) {
+        Statistics statistics = result.getStatistics();
+
+        println("당첨 통계");
+        println("---");
+
+        RANKS.forEach(rank -> printRank(rank, statistics));
+
+        printYield(statistics);
+    }
+
+
+    private static void printRank(Rank rank, Statistics statistics) {
+        if (rank == SECOND) {
+            printSecondRank(rank, statistics);
+            return;
+        }
+        printDefaultRank(rank, statistics);
+    }
+
+    private static void printDefaultRank(Rank rank, Statistics statistics) {
+        println(format(
+                "%d개 일치 (%s원) - %d개",
+                rank.getMatchCount(),
+                rank.getReward(),
+                statistics.count(rank)));
+    }
+
+    private static void printSecondRank(Rank rank, Statistics statistics) {
+        println(format(
+                "%d개 일치, 보너스 볼 일치 (%s원) - %d개",
+                rank.getMatchCount(),
+                rank.getReward(),
+                statistics.count(rank)));
+    }
+
+    private static void printYield(Statistics statistics) {
+        println(format("총 수익률은 %.1f%%입니다.", statistics.yield()));
+    }
+}
