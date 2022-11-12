b2543a96fd0f7ea64585a08e73c32b0a17455536
ttaehee <ttaehee1105@gmail.com>
2022-11-12 18:40:17 +0900

    feat : 구입금액 입력값 유효성 체크 기능 구현

--- src/main/java/lotto/io/View.java
+++ src/main/java/lotto/io/View.java
@@ -2,6 +2,7 @@ package lotto.io;
 
 public class View {
     private static final String REQUEST_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String WRONG_PURCHASE_AMOUNT = "[ERROR] 1000 단위만 입력할 수 있습니다.";
     private final Input input;
     private final Output output;
 
@@ -14,7 +15,15 @@ public class View {
         output.printOutput(REQUEST_PURCHASE_AMOUNT);
     }
 
-    public String getPurchaseAmount() {
-        return input.input();
+    public int getPurchaseAmount() {
+        int inputAmount = Integer.parseInt(input.input());
+        checkThousand(inputAmount);
+        return inputAmount;
+    }
+
+    private void checkThousand(int inputAmount) {
+        if (inputAmount % 1000 != 0) {
+            throw new IllegalArgumentException(WRONG_PURCHASE_AMOUNT);
+        }
     }
 }
