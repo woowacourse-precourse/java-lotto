0a2e0cb644d3cc0234a6d1dee2d09aee48e67174
woosung1223 <dntjd991223@naver.com>
2022-11-12 18:25:52 +0900

    test(WinningLottoTest): grade 메소드에 대한 테스트 생성

---
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -0,0 +1,49 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WinningLottoTest {
+    OutputStream out = new ByteArrayOutputStream();
+    InputStream in = System.in;
+
+    @BeforeEach
+    private final void init() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @Nested
+    @DisplayName("정답 로또로 채점할 때")
+    class Grade {
+        @ParameterizedTest
+        @MethodSource("parameterProvider")
+        void 정답_로또로_다른_로또를_채점할_때를_테스트한다(String source, Lotto target, int expected) {
+            ByteArrayInputStream in = new ByteArrayInputStream(source.getBytes());
+            System.setIn(in);
+            WinningLotto winningLotto = new WinningLotto();
+
+            assertThat(winningLotto.grade(target)).isEqualTo(expected);
+        }
+
+        private Stream<Arguments> parameterProvider() {
+            return Stream.of(
+                    Arguments.of("1,2,3,4,5,6", new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)), 6),
+                    Arguments.of("1,2,3,4,5,6", new Lotto(Arrays.asList(1, 2, 3, 7, 8, 9)), 3),
+                    Arguments.of("1,2,3,4,5,6", new Lotto(Arrays.asList(7, 8, 9, 10, 11, 12)), 0)
+                    );
+        }
+    }
+
+}
