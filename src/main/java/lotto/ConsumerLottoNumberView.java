1f9cb5e1df3571b136ad13bfa0553081b19b74f3
이경범 <tkdwk567@naver.com>
2022-11-13 00:11:58 +0900

    feat : 구매한 로또 번호 출력 기능 add

---
+++ src/main/java/lotto/ConsumerLottoNumberView.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import java.util.List;
+
+public class ConsumerLottoNumberView {
+
+    public static void show(List<Lotto> lottos) {
+        System.out.printf("%d개를 구매했습니다.\n", lottos.size());
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+}
