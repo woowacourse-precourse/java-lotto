a3bef5932c1d5ac26885707666a84aea888db075
slbin-park <smpts00@naver.com>
2022-11-12 22:05:28 +0900

    feat(Validator) : 로또 구입 금액 유효성 검사 기능 추가
    - 1000원 단위로 입력하지 않을시 에러 발생 기능 추가
    - 숫자가 아닌 문자가 들어올 경우 에러 발생 기능 추가

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -3,34 +3,40 @@ package lotto.util;
 import java.util.List;
 
 public class Validator {
+    private final String INVALID_NUMBER = "[ERROR] 숫자만 입력 가능합니다.";
     private final String INVALID_MONEY = "[ERROR] 1000원 단위로 입력이 가능합니다.";
+    private final int MONEY_UNIT = 1000;
 
     public int moneyInputValidator(String money) {
         int moneyNumber = changeNumber(money);
-        return 1;
+        checkChanges(moneyNumber);
+        return moneyNumber;
     }
 
     public int changeNumber(String money) {
-        try{
+        try {
             return Integer.parseInt(money);
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new IllegalArgumentException(INVALID_MONEY);
         }
     }
 
-    public void checkChanges(String money) {
-
+    public void checkChanges(int money) {
+        if (money % MONEY_UNIT == 0) {
+            return;
+        }
+        throw new IllegalArgumentException(INVALID_MONEY);
     }
 
     public static void winningNumberInputValidator(String money) {
 
     }
 
-    public static void checkValidNumber(int number){
+    public static void checkValidNumber(int number) {
 
     }
 
-    public static void checkDuplication(List<Integer> numbers, int number){
+    public static void checkDuplication(List<Integer> numbers, int number) {
 
     }
 
