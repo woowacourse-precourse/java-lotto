4b57968ca99d767bcf6e5df1a654f65f8b30695f
mydreamis-18 <hitoryok@naver.com>
2022-11-12 19:04:04 +0900

    [feat] 구매 수량 만큼의 로또 번호 생성 기능 구현
    - after -
    
    Application.java:
    구매 수량 만큼의 로또 번호 생성 기능 구현
    getMyLottos() 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -8,7 +11,19 @@ public class Application {
     	int payment = User.getPayment();
     	int purchaseQuantity = payment / Constant.PAYMENT_UNIT.value;
 
-    	Lotto lotto = Lotto.newLotto();
-		System.out.println(lotto.getNumbers());
+    	List<List<Integer>> myLottos = getMyLottos(purchaseQuantity);
+    	System.out.println(myLottos);
+    }
+    
+    public static List<List<Integer>> getMyLottos(int purchaseQuantity) {
+    	
+    	List<List<Integer>> myLottos = new ArrayList<List<Integer>>(purchaseQuantity);
+    	
+    	for (int count = 0; count < purchaseQuantity; count++) {
+    		
+    		myLottos.add(Lotto.newLotto().getNumbers());
+    	}
+    	
+    	return myLottos;
     }
 }
