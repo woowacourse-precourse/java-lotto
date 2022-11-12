64e1c0b005d08f55a09cb9daf0d80d8fe2318ec5
asas6978 <asas6978@naver.com>
2022-11-12 12:57:55 +0900

    feat: 메인 실행 메서드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            Print.purchaseMessage();
+
+            List<Lotto> lottery = Purchase.lotto();
+            Print.lottery(lottery);
+
+            CheckLotto winningLottery = Winning.getWinningLottery();
+            Map<WinningNumber, Integer> matchCount = winningLottery.match(lottery);
+
+            Print.matchResult(matchCount, lottery.size());
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
