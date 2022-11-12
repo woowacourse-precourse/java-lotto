556bb9aab2eab7a5417fa8d1dd99b2bd60c68866
김형기 <khk211113@naver.com>
2022-11-13 00:56:40 +0900

    test: 당첨 통계 테스트 케이스 추가

--- src/test/java/lotto/LottoStatisticsTest.java
+++ src/test/java/lotto/LottoStatisticsTest.java
@@ -1,17 +1,66 @@
 package lotto;
 
+import lotto.domain.LottoRank;
 import lotto.domain.LottoStatistics;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoStatisticsTest {
 
-    @DisplayName("결과를 추가하지 않았을 때 모두 0 출력.")
+    @DisplayName("결과를 추가하지 않았을 때 수익률 조회 시 예외 처리.")
     @Test
     void resultByNeverAdded() {
         LottoStatistics statistics = new LottoStatistics();
+
+        assertThatThrownBy(() -> statistics.getYield())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("1건의 로또결과에 대한 수익률 비교.")
+    @Test
+    void resultByOneRankAdded() {
+        LottoStatistics statistics = new LottoStatistics();
+        statistics.add(LottoRank.SECOND);
+
+        assertThat(statistics.getYield()).isEqualTo(3000000);
+    }
+
+    @DisplayName("2건의 로또결과에 대한 수익률 비교.")
+    @Test
+    void resultByTwoRankAdded() {
+        LottoStatistics statistics = new LottoStatistics();
+        statistics.add(LottoRank.FOURTH);
+        statistics.add(LottoRank.LOSE);
+
+        assertThat(statistics.getYield()).isEqualTo(2500);
+    }
+
+    @DisplayName("4건의 낙첨과 1건의 3등에 대한 수익률 비교.")
+    @Test
+    void resultByManyRankAdded() {
+        LottoStatistics statistics = new LottoStatistics();
+        List<LottoRank> ranks = List.of(LottoRank.LOSE, LottoRank.LOSE, LottoRank.LOSE,
+                LottoRank.THIRD, LottoRank.LOSE);
+        for (LottoRank rank : ranks) {
+            statistics.add(rank);
+        }
+
+        assertThat(statistics.getYield()).isEqualTo(30000);
+    }
+
+    @DisplayName("모두 낙첨되었을 때에 대한 수익률 비교.")
+    @Test
+    void resultByOnlyLose() {
+        LottoStatistics statistics = new LottoStatistics();
+        List<LottoRank> ranks = List.of(LottoRank.LOSE, LottoRank.LOSE, LottoRank.LOSE, LottoRank.LOSE);
+        for (LottoRank rank : ranks) {
+            statistics.add(rank);
+        }
 
         assertThat(statistics.getYield()).isEqualTo(0);
     }
