01abbcdafdd4487ef1c449a0abc63a60ed2584b2
dong young <dong5854@gmail.com>
2022-11-12 18:13:54 +0900

    [로또 발행 관련] feat: 로또 구입 금액 입력

---
+++ src/main/java/lotto/LottoUI.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoUI {
+    public static Integer purchase() {
+        System.out.println("구입금액을 입력해 주세요.");
+        final String input = Console.readLine();
+        if (!isNumeric(input)) {
+            throw new IllegalArgumentException("[ERROR] 숫자로 변환할 수 없는 입력입니다.");
+        }
+        return Integer.parseInt(input);
+    }
+
+    public static boolean isNumeric(String str){
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e){
+            return false;
+        }
+    }
+}
