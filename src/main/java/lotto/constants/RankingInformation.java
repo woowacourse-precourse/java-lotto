81b3933a7cdd83d3f82a18a84fd97b65c7c73106
Sehbeom <j3beom@gmail.com>
2022-11-12 20:00:59 +0900

    fix(constants): 당첨 순위 열거 상수 수정
    반대로 설정된 순위 정보 수정

--- src/main/java/lotto/constants/RankingInformation.java
+++ src/main/java/lotto/constants/RankingInformation.java
@@ -1,11 +1,11 @@
 package lotto.constants;
 
 public enum RankingInformation {
-    FIRST_PLACE("3개 일치 (5,000원) - "),
-    SECOND_PLACE("4개 일치 (50,000원) - "),
+    FIFTH_PLACE("3개 일치 (5,000원) - "),
+    FOURTH_PLACE("4개 일치 (50,000원) - "),
     THIRD_PLACE("5개 일치 (1,500,000원) - "),
-    FOURTH_PLACE("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    FIFTH_PLACE("6개 일치 (2,000,000,000원) - ");
+    SECOND_PLACE("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    FIRST_PLACE("6개 일치 (2,000,000,000원) - ");
 
     private final String statisticSentence;
 
