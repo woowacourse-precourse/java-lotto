143131272b3e5f8779be077c4ab157f7a25d2db4
Soomin Lim <augustth511@gmail.com>
2022-11-12 20:14:57 +0900

    test(WinningLottoTest): WinningLotto 생성 시 예외 처리 테스트 코드 작성

---
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -0,0 +1,46 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class WinningLottoTest {
+
+    @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByOverSize() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6, 7), 45))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 5), 45))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 1~45 범위 이외의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByInvalidRangeNumber() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 50), 45))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호에 1~45 범위 이외의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByInvalidRangeBonusNumber() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 100))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByDuplicatedBonusNumber() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
