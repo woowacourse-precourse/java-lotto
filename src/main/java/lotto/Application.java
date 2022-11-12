6152cb938d7e713c4ab867e22d23f51a488de954
ghltjd369 <ghltjd369@gmail.com>
2022-11-12 16:41:06 +0900

    feat : 당첨 번호 입력하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,7 +5,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
     public static void main(String[] args) {
         User user = new User();
+        LottoMachine lottoMachine = new LottoMachine();
+
         user.inputPurchaseMoney();
         user.purchaseLotto();
+        lottoMachine.inputWinningNumber();
     }
 }
---
+++ src/main/java/lotto/LottoMachine.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoMachine {
+    public void inputWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String number_str = Console.readLine();
+    }
+}
