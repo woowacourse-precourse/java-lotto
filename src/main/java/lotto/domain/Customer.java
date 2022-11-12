ef1e3a9eebb4c3fde8676d4264fe24fb5b54b368
ParkRootSeok <g_e_un@naver.com>
2022-11-12 23:58:34 +0900

    feat(Customer) : 금액 입력 예외 처리 추가

--- src/main/java/lotto/domain/Customer.java
+++ src/main/java/lotto/domain/Customer.java
@@ -6,13 +6,22 @@ public class Customer {
 
     private static final String INPUT_MONEY = "구입 금액을 입력해주세요.";
     private static final int LOTTO_PRICE = 1000;
+    private static final int REMAINDER = 0;
 
     private int money;
     private int hasLotto;
 
     public void payMoney() {
-        System.out.println(INPUT_MONEY);
-        this.money = Integer.parseInt(Console.readLine());
+        try {
+            System.out.println(INPUT_MONEY);
+            this.money = Integer.parseInt(Console.readLine());
+
+            if ((getMoney() % LOTTO_PRICE) != REMAINDER) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] : 급액은 1,000원 단위로 입력하시오.");
+        }
     }
 
     public int getMoney() {
@@ -23,8 +32,9 @@ public class Customer {
         hasLotto = getMoney() / LOTTO_PRICE;
     }
 
-    public int getLotto() {
+    public int getHasLotto() {
         return hasLotto;
     }
+
 
 }
