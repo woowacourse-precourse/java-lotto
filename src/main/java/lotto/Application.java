1a2aa30a033920017a4b189adf366930f8da2b51
heesang <dlagmltkd12@naver.com>
2022-11-12 23:16:18 +0900

    feat: Application 메인 메소드 예외 핸들링 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,8 +4,13 @@ import lotto.controller.LottoGameController;
 import lotto.service.LottoGameService;
 
 public class Application {
+
     public static void main(String[] args) {
         LottoGameController lottoGameController = new LottoGameController(new LottoGameService());
-        lottoGameController.run();
+        try {
+            lottoGameController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
