09532b0bcb7cfff62a129c2e4dac64f82570373b
marinesnow34 <m4rinesnow34@gmail.com>
2022-11-12 23:42:59 +0900

    feat: 구매 수량에 맞는 로또 생성

--- src/main/java/lotto/model/LottoStore.java
+++ src/main/java/lotto/model/LottoStore.java
@@ -1,4 +1,24 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoStore {
+    private static final int LOTTO_SIZE = 6;
+
+    private List<Lotto> lottos;
+
+    public LottoStore(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(makeLotto());
+        }
+        this.lottos = lottos;
+    }
+
+    private Lotto makeLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
 }
