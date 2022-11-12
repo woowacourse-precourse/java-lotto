5f36f0fbce720954846cd27e76ee79bade3477d6
asas6978 <asas6978@naver.com>
2022-11-12 11:13:28 +0900

    refactor: 출력용 안내 문구 수정

--- src/main/java/lotto/GuideMessage.java
+++ src/main/java/lotto/GuideMessage.java
@@ -3,12 +3,12 @@ package lotto;
 public class GuideMessage {
     public static final String ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     public static final String CONFIRM_PURCHASE_AMOUNT = "개를 구매했습니다.";
-    public static final String ENTER_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
-    public static final String ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    public static final String WINNING_STATISTICS = "당첨 통계";
-    public static final String MATCH = "개 일치";
+    public static final String ENTER_WINNING_NUMBER = "%n당첨 번호를 입력해 주세요.%n";
+    public static final String ENTER_BONUS_NUMBER = "%n보너스 번호를 입력해 주세요.%n";
+    public static final String WINNING_STATISTICS = "%n당첨 통계%n";
+    public static final String MATCH = "%d개 일치";
     public static final String BONUS_BALL_MATCH = "보너스 볼 일치";
     public static final String AMOUNT = " (%s원)";
-    public static final String MATCH_NUMBER = " - %d개";
+    public static final String MATCH_NUMBER = " - %d개%n";
     public static final String FINAL_REVENUE = "총 수익률은 %.1f%%입니다.";
 }
