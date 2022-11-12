77f395cdc9fdcc2bb5100a90420503299e3af7e4
Soomin Lim <augustth511@gmail.com>
2022-11-12 13:16:34 +0900

    refactor(ErrorMessage): InputView의 에러 메시지 상수 관리를 ErrorMessage 클래스로 위임

---
+++ src/main/java/lotto/domain/ErrorMessage.java
@@ -0,0 +1,7 @@
+package lotto.domain;
+
+public abstract class ErrorMessage {
+    public static final String NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 숫자를 입력해야 합니다.";
+
+
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,13 +6,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static lotto.domain.ErrorMessage.NUMBER_FORMAT_ERROR_MESSAGE;
+
 public class InputView {
 
-    private final static String PLAYER_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
-    private final static String WINNIG_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private final static String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-
-    private final static String NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 숫자를 입력해야 합니다.";
+    private static final String PLAYER_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static int playerMoney() {
         System.out.println(PLAYER_MONEY_MESSAGE);
@@ -21,7 +21,7 @@ public class InputView {
     }
 
     public static List<Integer> winningNumbers() {
-        System.out.println(WINNIG_NUMBER_MESSAGE);
+        System.out.println(WINNING_NUMBER_MESSAGE);
 
         return convertStringToLotto(Console.readLine());
     }
@@ -36,7 +36,7 @@ public class InputView {
         try {
             return Integer.parseInt(number);
         } catch (NumberFormatException e) {
-            System.out.println(NUMBER_FORMAT_ERROR_MESSAGE);
+            System.out.println();
             throw new IllegalArgumentException();
         }
     }
