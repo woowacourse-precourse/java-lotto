1c89be9ab477d9db12ad746ba3201813b23f01f6
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-13 01:51:55 +0900

    refactor: 접근제한자 수정

--- src/main/java/lotto/ui/Error.java
+++ src/main/java/lotto/ui/Error.java
@@ -1,12 +1,12 @@
 package lotto.ui;
 
 public class Error {
-    static final String ERROR = "[ERROR] ";
-    static final String INVAILD_INPUT_VALUE = ERROR + "올바른 입력값이 아닙니다.";
-    static final String MUST_BE_NUMBERS_BETWEEN_1_TO_45 = ERROR + "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-    static final String HAD_DUPLICATE_NUMBER = ERROR + "중복된 로또 번호 입니다.";
-    static final String MUST_BE_SIX_DIGIT = ERROR + "로또 번호는 6자리 이어야합니다.";
-    static final String ENTER_IN_UNITS_OF_1000_WON = ERROR + "1000원 단위로 입력해주세요";
+    public static final String ERROR = "[ERROR] ";
+    public static final String INVAILD_INPUT_VALUE = ERROR + "올바른 입력값이 아닙니다.";
+    public static final String MUST_BE_NUMBERS_BETWEEN_1_TO_45 = ERROR + "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String HAD_DUPLICATE_NUMBER = ERROR + "중복된 로또 번호 입니다.";
+    public static final String MUST_BE_SIX_DIGIT = ERROR + "로또 번호는 6자리 이어야합니다.";
+    public static final String ENTER_IN_UNITS_OF_1000_WON = ERROR + "1000원 단위로 입력해주세요";
 }
 
 
