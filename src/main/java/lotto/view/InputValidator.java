e6b549c61f7c8d1e4f52d2aad5c8fb7064b21fdf
greeng00se <thegreengoose7@gmail.com>
2022-11-12 20:47:49 +0900

    feat(InputValidator): 로또 당첨 번호에 대한 입력값 검증 메서드 추가

--- src/main/java/lotto/view/InputValidator.java
+++ src/main/java/lotto/view/InputValidator.java
@@ -1,9 +1,21 @@
 package lotto.view;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.dto.LottoDto;
+
 class InputValidator {
     private static final int VALID_LOTTO_PRICE = 1000;
     private static final String INVALID_NUMBER_FORMAT_MESSAGE = "[ERROR] 올바르지 않은 숫자 형식입니다.";
     private static final String INVALID_AMOUNT_MESSAGE = "[ERROR] 구입 금액은 " + VALID_LOTTO_PRICE + "원 단위여야 합니다.";
+    private static final String WINNING_NUMBERS_DELIMITER = ",";
+    private static final int VALID_LOTTO_NUMBER_LOWER_BOUND = 1;
+    private static final int VALID_LOTTO_NUMBER_UPPER_BOUND = 45;
+    private static final int VALID_LOTTO_NUMBERS_SIZE = 6;
+    private static final String INVALID_LOTTO_NUMBERS_MESSAGE = "[ERROR] 올바른 로또 번호가 아닙니다.";
+    private static final String DISTINCT_BONUS_NUMBERS_MESSAGE = "[ERROR] 보너스 번호는 로또 번호와 다른 번호를 입력해야 합니다.";
 
     private InputValidator() {
     }
@@ -13,9 +25,9 @@ class InputValidator {
         validateRange(amount);
     }
 
-    private static Integer validateNumeric(String purchaseAmount) {
+    private static Integer validateNumeric(String number) {
         try {
-            return Integer.valueOf(purchaseAmount);
+            return Integer.valueOf(number);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_NUMBER_FORMAT_MESSAGE);
         }
@@ -24,6 +36,38 @@ class InputValidator {
     private static void validateRange(Integer amount) {
         if (amount < VALID_LOTTO_PRICE || amount % VALID_LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(INVALID_AMOUNT_MESSAGE);
+        }
+    }
+
+    public static LottoDto validateLottoNumber(String winningInput, String bonusInput) {
+        List<Integer> winningNumbers = getDistinctWinningNumbers(winningInput);
+        validateWinningNumbers(winningNumbers);
+        Integer bonusNumber = validateNumeric(bonusInput);
+        validateBonusNumber(winningNumbers, bonusNumber);
+        return new LottoDto(winningNumbers, bonusNumber);
+    }
+
+    private static List<Integer> getDistinctWinningNumbers(String winningNumbers) {
+        return Arrays.stream(winningNumbers.split(WINNING_NUMBERS_DELIMITER))
+                .map(InputValidator::validateNumeric)
+                .filter(InputValidator::isValidLottoNumberRange)
+                .distinct()
+                .collect(toList());
+    }
+
+    private static boolean isValidLottoNumberRange(Integer number) {
+        return VALID_LOTTO_NUMBER_LOWER_BOUND <= number && number <= VALID_LOTTO_NUMBER_UPPER_BOUND;
+    }
+
+    private static void validateWinningNumbers(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != VALID_LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBERS_MESSAGE);
+        }
+    }
+
+    private static void validateBonusNumber(List<Integer> winningNumbers, Integer bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(DISTINCT_BONUS_NUMBERS_MESSAGE);
         }
     }
 }
--- src/test/java/lotto/view/InputValidatorTest.java
+++ src/test/java/lotto/view/InputValidatorTest.java
@@ -1,11 +1,17 @@
 package lotto.view;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.List;
+import lotto.dto.LottoDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("InputValidator 클래스")
@@ -31,4 +37,45 @@ class InputValidatorTest {
     void validatePurchaseAmount_메서드는_허용된_숫자형식의_문자열을_입력하면_예외가_발생하지_않는다(String amount) {
         InputValidator.validatePurchaseAmount(amount);
     }
+
+    @ParameterizedTest
+    @MethodSource("InvalidLottoNumbers")
+    void validateLottoNumber_메서드는_올바른_당첨번호와_보너스번호_형식의_문자열을_입력하지_않으면_IllegalArgumentException을_던진다(
+            String winningNumbers,
+            String bonusNumber) {
+        assertThatThrownBy(() -> InputValidator.validateLottoNumber(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static List<Arguments> InvalidLottoNumbers() {
+        return Arrays.asList(
+                Arguments.of("1,2,3,4,5", "6"),
+                Arguments.of("1,2,3,4,5,7", "f"),
+                Arguments.of("1,2,3,4,53,4", "6"),
+                Arguments.of("1d,2,3,4,5,6", "7"),
+                Arguments.of("1,2,3,4,5,6", ""),
+                Arguments.of("", "3")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("validLottoNumbers")
+    void validateLottoNumber_메서드는_올바른_당첨번호와_보너스_번호를_입력하면_LottoDto를_반환한다(
+            String winningNumbers,
+            String bonusNumber) {
+        LottoDto lottoDto = InputValidator.validateLottoNumber(winningNumbers, bonusNumber);
+
+        assertThat(lottoDto.getWinningNumbers().size()).isEqualTo(6);
+        assertThat(lottoDto.getBonusNumber()).isEqualTo(Integer.valueOf(bonusNumber));
+    }
+
+    static List<Arguments> validLottoNumbers() {
+        return Arrays.asList(
+                Arguments.of("1,2,3,4,5,6", "7"),
+                Arguments.of("5,12,22,25,35,42", "8"),
+                Arguments.of("11,12,13,14,15,16", "17"),
+                Arguments.of("1,23,34,42,17,44", "45")
+        );
+    }
+
 }
