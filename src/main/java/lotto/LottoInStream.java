bd515276583c0f07a13f103d40edfd13908df5fe
kanghyki <kanghyki@gmail.com>
2022-11-12 20:44:17 +0900

    feat(LottoInStream): 입력 시 출력 메세지 상수 선언

--- src/main/java/lotto/LottoInStream.java
+++ src/main/java/lotto/LottoInStream.java
@@ -1,4 +1,8 @@
 package lotto;
 
 public class LottoInStream {
+    private static final String _input_buy_str = "구입금액을 입력해 주세요.\n";
+    private static final String _input_numbers_str = "당첨 번호를 입력해 주세요.\n";
+    private static final String _input_bonus_number_str = "보너스 번호를 입력해 주세요.\n";
+
 }
