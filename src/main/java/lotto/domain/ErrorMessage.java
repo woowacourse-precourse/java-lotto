f8345c6556d4aa35a4ce55e867cb57c90f56ae31
Soomin Lim <augustth511@gmail.com>
2022-11-12 13:55:22 +0900

    feat(Lotto): 로또 번호 관련 예외 상황 시 에러 문구 출력 기능 구현

--- src/main/java/lotto/domain/ErrorMessage.java
+++ src/main/java/lotto/domain/ErrorMessage.java
@@ -3,5 +3,9 @@ package lotto.domain;
 public abstract class ErrorMessage {
     public static final String NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 숫자를 입력해야 합니다.";
 
+    public static final String PLAYER_MONEY_ERROR_MESSAGE = "[ERROR] 티켓은 1000원 입니다. 1000원 단위로 입력해야 합니다.";
 
+    public static final String LOTTO_NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE = "[ERROR] 로또 번호는 서로 중복되지 않아야 합니다.";
+    public static final String LOTTO_NUMBER_SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호의 개수는 6개여야 합니다.";
 }
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static lotto.domain.ErrorMessage.*;
+
 public class Lotto {
     private static final int MINIMUM_NUMBER = 1;
     private static final int MAXIMUM_NUMBER = 45;
@@ -24,6 +26,7 @@ public class Lotto {
     private void validateRange(List<Integer> numbers) {
         if (numbers.stream()
                 .anyMatch(number -> number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER)) {
+            System.out.println(LOTTO_NUMBER_RANGE_ERROR_MESSAGE);
             throw new IllegalArgumentException();
         }
     }
@@ -31,12 +34,14 @@ public class Lotto {
     private void validateDuplicate(List<Integer> numbers) {
         Set<Integer> numberStorage = new HashSet<>(numbers);
         if (numberStorage.size() != 6) {
+            System.out.println(LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE);
             throw new IllegalArgumentException();
         }
     }
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            System.out.println(LOTTO_NUMBER_SIZE_ERROR_MESSAGE);
             throw new IllegalArgumentException();
         }
     }
