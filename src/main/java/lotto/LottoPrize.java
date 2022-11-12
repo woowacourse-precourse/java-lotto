963972a4258c45d077864b0106387b3183ce30ed
Your Name <you@example.com>
2022-11-12 16:19:55 +0900

    feat(getLottoPrizeMoney): 로또 등수 가격 getter 구현

--- src/main/java/lotto/LottoPrize.java
+++ src/main/java/lotto/LottoPrize.java
@@ -1,11 +1,10 @@
 package lotto;
 
-import java.lang.reflect.Array;
 import java.text.DecimalFormat;
 
 public enum LottoPrize {
     FIRST(6, "6개 일치", 2000000000), 
-    SECOND(5, "5개 일치, 보너스 볼 일치", 300000000), 
+    SECOND(5, "5개 일치, 보너스 볼 일치", 30000000), 
     THIRD(5, "5개 일치", 1500000), 
     FOURTH(4, "4개 일치", 50000), 
     FIFTH(3, "3개 일치", 5000), 
@@ -15,12 +14,17 @@ public enum LottoPrize {
     private int lottoPrizeMoney;
     private String description;
 
+    
     LottoPrize(int matches, String description, int prizeMoney) {
         this.matches = matches;
         this.description = description;
         this.lottoPrizeMoney = prizeMoney;
     }
-
+    
+    public int getLottoPrizeMoney() {
+        return lottoPrizeMoney;
+    }
+    
     public LottoPrize getPrize(int matches) {
         LottoPrize[] val = values();
         for (int index = 0; index < val.length; index++) {
