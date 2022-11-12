c391f317b9a8c1f9f7328712faac6d01f299d013
SeongchanCho <wordnumperiod@gmail.com>
2022-11-13 08:54:34 +0900

    feat(Application.java): 로또 구입 금액 입력 받는 기능 구현 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int money=getMoneyInput();
+
     }
+
+    private static int getMoneyInput(){
+        int moneyInput=getIntegerInput();
+        if(moneyInput<0){
+            throw new IllegalArgumentException();
+        }
+        return moneyInput;
+    }
+
+    private static int getIntegerInput(){
+        try{
+            return Integer.parseInt(Console.readLine());
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
