2fd6ed7ad40bddbe05cb3780d3d5ac50b0e2e37a
Sungjaeyeong <wodud2587@gmail.com>
2022-11-12 23:01:59 +0900

    feat(lotto): 총 수입을 구함

--- src/main/java/lotto/Calculator.java
+++ src/main/java/lotto/Calculator.java
@@ -4,4 +4,12 @@ public class Calculator {
     static public float calculate(int buyPrice) {
         return 0;
     }
+
+    public static float getTotalPrize() {
+        float result = 0;
+        for (Rank rank : Rank.values()) {
+            result += rank.getPrize() * rank.getCount();
+        }
+        return result;
+    }
 }
