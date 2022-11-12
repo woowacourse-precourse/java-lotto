7d2a060d97487ae915f5dbf1669132cfc22e6060
jiwon <ljwon77@daum.net>
2022-11-12 23:57:15 +0900

    feat: get winning / bonus number and validate
    사용자로부터 당첨 번호 리스트와 보너스 번호를 입력받고 검증하는 기능을 추가하였습니다.

--- src/main/java/lotto/LottoPlay.java
+++ src/main/java/lotto/LottoPlay.java
@@ -1,58 +1,22 @@
 package lotto;
 
-import lotto.constant.LottoNumber;
-import lotto.util.InputUtil;
 import lotto.util.MessageUtil;
-import lotto.util.ValidationUtil;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LottoPlay {
 
     private final MessageUtil messageUtil = new MessageUtil();
 
-    private final InputUtil inputUtil = new InputUtil();
+    private final LottoPurchase lottoPurchase = new LottoPurchase();
 
-    private final ValidationUtil validationUtil = new ValidationUtil();
-
-    private final NumberGenerator numberGenerator = new NumberGenerator();
-
-    private final List<Lotto> userLottos = new ArrayList<>();
+    private final LottoWinningNumber lottoWinningNumber = new LottoWinningNumber();
 
     public void play() {
-        messageUtil.printBonusInput();
-        int purchaseAmount = getUserPurchaseAmount();
-        getPurchaseInfo(purchaseAmount);
+        messageUtil.printPurchaseInput();
+        List<Lotto> userLottos = lottoPurchase.getPurchaseInfo();
+        lottoWinningNumber.getWinningNumbersInfo();
+
     }
-
-    private int getUserPurchaseAmount() {
-        int purchaseAmount = Integer.parseInt(inputUtil.getUserInput());
-        validationUtil.validatePurchase(purchaseAmount);
-        return purchaseAmount;
-    }
-
-    private void getPurchaseInfo(int purchaseAmount) {
-        int purchaseCount = getPurchaseCount(purchaseAmount);
-
-        for (int i = 0; i < purchaseCount; i++) {
-            Lotto lotto = generateLottoNumbers();
-            userLottos.add(lotto);
-        }
-    }
-
-    private Lotto generateLottoNumbers() {
-        List<Integer> lottoNumbers = numberGenerator.getLottoNumbers();
-        Lotto lotto = new Lotto(lottoNumbers);
-        messageUtil.printPurchaseInfo(lotto.getLottoNumbers());
-        return lotto;
-    }
-
-    private int getPurchaseCount(int purchaseAmount) {
-        int purchaseCount = purchaseAmount / LottoNumber.PURCHASE_AMOUNT_COND.getNumber();
-        messageUtil.printPurchaseCount(purchaseCount);
-        return purchaseCount;
-    }
-
 }
 
---
+++ src/main/java/lotto/LottoWinningNumber.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import lotto.util.InputUtil;
+import lotto.util.MessageUtil;
+import lotto.util.ValidationUtil;
+
+public class LottoWinningNumber {
+
+    private final InputUtil inputUtil = new InputUtil();
+
+    private final MessageUtil messageUtil = new MessageUtil();
+
+    private final ValidationUtil validationUtil = new ValidationUtil();
+
+    public void getWinningNumbersInfo() {
+        getWinningNumbers();
+        getBonusNumbers();
+    }
+
+    private void getWinningNumbers() {
+        messageUtil.printWinningInput();
+        String winningNums = inputUtil.getUserInput();
+        validationUtil.validateWinningAmount(winningNums);
+    }
+
+    private void getBonusNumbers() {
+        messageUtil.printBonusInput();
+        int bonusNum = Integer.parseInt(inputUtil.getUserInput());
+        validationUtil.validateBonusRange(bonusNum);
+    }
+}
