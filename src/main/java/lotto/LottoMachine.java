2a196a92a8a23186296b9c83d6be6016efab38f4
tenta <eunoia3802@gmail.com>
2022-11-12 16:11:06 +0900

    feat: 로또 랜덤 번호 생성 기능 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import view.InputView;
+
+import java.util.List;
 
 public class LottoMachine {
 
@@ -9,5 +12,11 @@ public class LottoMachine {
         int money = inputView.inputMoney();
         int lottoCount = money / 1000;
         return lottoCount;
+    }
+
+    public List<Integer> createLottoNumber() {
+        List<Integer> numbers =
+                Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
     }
 }
