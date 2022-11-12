2e463ddddebf7b91839fae159614a77758715c0c
lacram <chemin9898@gmail.com>
2022-11-12 22:56:24 +0900

    feat: 당첨번호와 발행된 로또를 비교하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,6 +15,15 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
-
-    // TODO: 추가 기능 구현
+    
+    // 당첨번호와 발행된 로또를 비교하는 기능
+    public int compareWithWinningNumbers(List<Integer> winningNumbers) {
+        int count = 0;
+        for (Integer winningNumber : winningNumbers) {
+            if (numbers.contains(winningNumber)) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
