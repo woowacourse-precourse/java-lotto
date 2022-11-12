db9e3b6045237c3af4d23d301340a09873501076
yoon <beatus96@naver.com>
2022-11-12 18:40:41 +0900

    feat : 구매한 로또 출력 기능 추가

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -1,9 +1,15 @@
 package lotto;
 
+import java.util.Set;
+
 public class Output {
 
     public void printMoney(int money) {
         System.out.println(money);
     }
-}
 
+    public void pringLottos(int count, Set<Lotto> lottos) {
+        System.out.println(count+"개를 구매했습니다.");
+        lottos.forEach(System.out::println);
+    }
+}
