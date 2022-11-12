28aefb88a4a81840def45039effe8da1f57018bf
HyeonjungYun <guswnd9971@gmail.com>
2022-11-12 16:54:26 +0900

    "style(Money): 가독성 향상을 위해 줄 바꿈 추가"

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -24,17 +24,20 @@ public class Money {
 
     public void validateMoney(int money) {
         if (money % 1000 != 0){
-            throw new IllegalArgumentException("[ERROR]: 로또를 구매할 돈의 최소 단위는 반드시 1000원이어야 합니다.");
+            throw new IllegalArgumentException
+                    ("[ERROR]: 로또를 구매할 돈의 최소 단위는 반드시 1000원이어야 합니다.");
         }
         if (money < 1000) {
-            throw new IllegalArgumentException("[ERROR]: 로또를 구매할 돈은 반드시 1000원 이상이어야 합니다.");
+            throw new IllegalArgumentException
+                    ("[ERROR]: 로또를 구매할 돈은 반드시 1000원 이상이어야 합니다.");
         }
     }
 
     private void isRealNumber (String inputStatement) {
         for (char element: inputStatement.toCharArray()) {
             if (element < 48 || element > 57) {
-                throw new IllegalArgumentException("[ERROR]: 숫자만 입력할 수 있습니다.");
+                throw new IllegalArgumentException
+                        ("[ERROR]: 숫자만 입력할 수 있습니다.");
             }
         }
     }
