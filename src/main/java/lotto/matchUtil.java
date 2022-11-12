1f67c6f531b975e0af1c5fbcebb4acb746588fef
jingyu9804 <swc05161@gmail.com>
2022-11-12 15:58:13 +0900

    feat(lotto) : 로또 당첨 번호 및 보너스 번호 매칭 기능

---
+++ src/main/java/lotto/matchUtil.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import java.util.List;
+
+public class matchUtil {
+    private int cnt,length=Constant.LOTTO_SIZE;
+    public int matchWinningNumber(List<Integer> lotto, List<Integer> userNum){
+        cnt = 0;
+        for(int i=0;i<length;i++){
+            if(lotto.get(i).equals(userNum.get(i))){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public int matchBonusNumber(List<Integer> lotto, int bonusNum){
+        cnt = 0;
+        for(int i=0;i<length;i++){
+            if(lotto.get(i).equals(bonusNum)){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
