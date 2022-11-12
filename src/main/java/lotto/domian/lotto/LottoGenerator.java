2e1cdf5b3686b4910e9360b68df1a98c9b06331b
DongwookKim0823 <dwkim0823@naver.com>
2022-11-12 16:46:38 +0900

    feat(lotto): 로또의 개수를 검증하는 기능

--- src/main/java/lotto/domian/lotto/LottoGenerator.java
+++ src/main/java/lotto/domian/lotto/LottoGenerator.java
@@ -13,6 +13,13 @@ public class LottoGenerator {
         while (lottos.size() < count) {
             lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
         }
+        validateLottos(count, lottos);
         return lottos;
+    }
+
+    private void validateLottos(int count, List<Lotto> lottos) {
+        if (count != lottos.size()) {
+            throw new IllegalArgumentException("[ERROR] 올바른 개수만큼의 로또가 생성되지 않았습니다.");
+        }
     }
 }
