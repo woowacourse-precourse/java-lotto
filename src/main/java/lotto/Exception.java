6901146436509ec63d46fc74faa4d823f27b2d52
metorg <metrog950@gmail.com>
2022-11-12 15:29:21 +0900

    feat: 숫자가 아닌 문자 예외처리 메서드 생성

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -1,4 +1,13 @@
 package lotto;
 
 public class Exception {
+
+    //숫자가 아닌 문자 입력시 예외처리
+    public boolean isNumeric(int money){
+        try {
+            return true;
+        }catch(NumberFormatException e){
+            return false;
+        }
+    }
 }
--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -6,15 +6,18 @@ public class View {
 
     private final Scanner scanner;
 
+    public Exception exception=new Exception();
 
     public View(Scanner scanner) {
-        this.scanner = scanner;
+        this.scanner = scanner;;
     }
 
     public int requestMoney(){
         int inputMoney = scanner.nextInt();
-
-        return inputMoney;
+        if(exception.isNumeric(inputMoney)){
+            return inputMoney;
+        }
+        throw new IllegalArgumentException("ERROR");
     }
 
 
