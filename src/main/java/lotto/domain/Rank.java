d2301a366fdd084f9b81503d204c205896ad2304
MinjeongKong <kong1301@ajou.ac.kr>
2022-11-12 20:09:20 +0900

    feat: 랭크 정보 업데이트

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,5 +1,19 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST, SECOND, THIRD, FOURTH, FIFTH, NONE
+    FIRST(6, false, 2000000000),
+    SECOND(5,true,30000000),
+    THIRD(5,false,1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3,false,5000);
+
+    private final int cnt;
+    private final boolean bonus;
+    private final int reward;
+
+    Rank(int cnt, boolean bonus, int reward) {
+        this.cnt = cnt;
+        this.bonus = bonus;
+        this.reward = reward;
+    }
 }
