b0b68670de806ed6695de3a4ef604043e47c1ff4
ChanghyunKim <variety82p@gamil.com>
2022-11-12 19:59:08 +0900

    feat(LottoTickets) : 생성된 로또를 담는 LottoTickets 클래스 구현

---
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoTickets {
+    private final List<Lotto> lottoTickets = new ArrayList<>();
+    public LottoTickets(int count){
+        addLotto(count);
+    }
+
+    public void addLotto(int count){
+        for(int i = 0; i < count; i++){
+            Lotto lotto = new Lotto(LottoNumber.createLotto());
+            lottoTickets.add(lotto);
+        }
+    }
+
+    public List<Lotto> getLottoTickets(){
+        return Collections.unmodifiableList(lottoTickets);
+    }
+}
