e56a82c0b293961360982d14bbd2df46fb689e67
Jenn Kim <grdd0419@gmail.com>
2022-11-12 17:08:14 +0900

    feat: 로또 구입 금액을 입력받는 메서드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static int getInputMoney() {
+        int money;
+        try {
+            money = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR]Input error");
+        }
+
+        if (money < 1000) throw new IllegalArgumentException("[ERROR]Input error");
+        if (money % 1000 != 0) throw new IllegalArgumentException("[ERROR]Input error");
+
+        return money;
     }
 }
