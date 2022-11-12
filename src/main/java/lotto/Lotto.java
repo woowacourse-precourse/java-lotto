e875c8adc7ac39e8903ab60e9865316f62e6b754
yoon <beatus96@naver.com>
2022-11-12 20:15:41 +0900

    feat : 로또 번호 유효성 체크 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,12 @@
 package lotto;
 
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    private final int LOTTO_NUMBER = 6;
+    private final int LOTTO_MIN = 1;
+    private final int LOTTO_MAX = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,7 +16,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (!isValidLotto(numbers)) {
             throw new IllegalArgumentException();
         }
     }
@@ -21,5 +25,20 @@ public class Lotto {
         numbers.sort(Integer::compare);
     }
 
+    private boolean isCorrectSize(List<Integer> numbers) {
+        return numbers.size() == LOTTO_NUMBER;
+    }
+
+    private boolean isNotDuplicated(List<Integer> numbers) {
+        return Set.of(numbers).size() == LOTTO_NUMBER;
+    }
+
+    private boolean isInRange(List<Integer> numbers) {
+        return numbers.stream().allMatch(v -> LOTTO_MIN <= v && v <= LOTTO_MAX);
+    }
+
+    private boolean isValidLotto(List<Integer> numbers) {
+        return isCorrectSize(numbers) && isInRange(numbers) && isNotDuplicated(numbers);
+    }
     // TODO: 추가 기능 구현
 }
