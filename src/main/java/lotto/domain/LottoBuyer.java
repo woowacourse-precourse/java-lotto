593cd6b4116e8097938e4ba8f1788e942515412d
5nam <ohnam90@gmail.com>
2022-11-12 14:44:11 +0900

    feat: 구매 금액 입력받고, 반환하는 기능 추가

--- src/main/java/lotto/domain/LottoBuyer.java
+++ src/main/java/lotto/domain/LottoBuyer.java
@@ -1,7 +1,22 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class LottoBuyer {
     private static final String INT_REGEX = "^[0-9]*$";
+    public int getPay() {
+        String input = Console.readLine();
+        int money = changeInt(input);
+
+        try {
+            checkOnlyNumber(input);
+            checkPayUnit(money);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return money;
+    }
     public void checkPayUnit(int money) {
         if(money%1_000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구매 금액은 1,000 단위로만 입력 가능합니다.");
