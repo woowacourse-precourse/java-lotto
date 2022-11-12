6be32d5b85cc1b94d272734c3632c76ea2bdde6b
jingyu9804 <swc05161@gmail.com>
2022-11-12 14:59:12 +0900

    refactor(lotto) : 상수 클래스 변수명 변경

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -1,21 +1,21 @@
 package lotto;
 
 public class Constant {
-    public static final String inputPurchasePrice = "구입금액을 입력해 주세요.";
-    public static final String outputPurchaseQuantity = "개를 구매했습니다.";
+    public static final String INPUT_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
+    public static final String OUTPUT_PURCHASE_QUANTITY = "개를 구매했습니다.";
 
-    public static final String inputWinningNumber = "당첨 번호를 입력해 주세요.";
-    public static final String inputBonusNumber = "보너스 번호를 입력해 주세요.";
+    public static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public static final String winningStatics = "당첨 통계";
-    public static final String dash = "---";
-    public static final String threeCorrect = "3개 일치 (5,000원) - ";
-    public static final String fourCorrect = "4개 일치 (50,000원) - ";
-    public static final String fiveCorrect = "5개 일치 (1,500,000원) - ";
-    public static final String fiveAndBonusCorrect = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
-    public static final String sixCorrect = "6개 일치 (2,000,000,000원) - 0";
-    public static final String count = "개";
+    public static final String WINNING_STATICS = "당첨 통계";
+    public static final String DASH = "---";
+    public static final String THREE_CORRECT = "3개 일치 (5,000원) - ";
+    public static final String FOUR_CORRECT = "4개 일치 (50,000원) - ";
+    public static final String FIVE_CORRECT = "5개 일치 (1,500,000원) - ";
+    public static final String FIVE_AND_BONUS_CORRECT = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    public static final String SIX_CORRECT = "6개 일치 (2,000,000,000원) - 0";
+    public static final String COUNT = "개";
 
-    public static final String totalStart = "총 수익률은 ";
-    public static final String totalEnd = "%입니다.";
+    public static final String TOTAL_START = "총 수익률은 ";
+    public static final String TOTAL_END = "%입니다.";
 }
