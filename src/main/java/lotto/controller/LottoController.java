08f9e5e46cf320ca9836e187b7f1ec4415cec2b0
kjb95 <topmedia44@gmail.com>
2022-11-12 16:50:35 +0900

    refactor: 로또 당첨 결과를 계산하고 출력하는 부분을 분리하여 computeWinningStatistics() 메소드 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,7 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.model.domain.Lotto;
 import lotto.model.domain.LottoCollection;
 import lotto.model.domain.PayingMoney;
 import lotto.model.domain.WinningNumber;
@@ -11,27 +13,32 @@ public class LottoController {
 
     public void run() {
         LottoCollection lottoCollection = buyLotto();
-        WinningNumber winningNumber = WinnerAnnouncement();
+        WinningNumber winningNumber = winnerAnnouncement();
 
-        WinningStatistics winningStatistics = new WinningStatistics(lottoCollection.get(), winningNumber);
-        OutputView.printWinningStatistics(winningStatistics.get());
-        OutputView.printTotalYield(winningStatistics.computeTotalYield());
-
+        computeWinningStatistics(lottoCollection.get(), winningNumber);
     }
 
     private LottoCollection buyLotto() {
-        PayingMoney payingMoney = new PayingMoney(InputView.requestMoney());
+        String money = InputView.requestMoney();
+        PayingMoney payingMoney = new PayingMoney(money);
         LottoCollection lottoCollection = payingMoney.createLottoCollection();
 
         OutputView.printLottoCollection(lottoCollection);
         return lottoCollection;
     }
 
-    private WinningNumber WinnerAnnouncement() {
+    private WinningNumber winnerAnnouncement() {
         String winningNumber = InputView.requestWinningNumber();
         String bonusNumber = InputView.requestBonusNumber();
 
         return new WinningNumber(winningNumber, bonusNumber);
+    }
+
+    private void computeWinningStatistics(List<Lotto> lottoCollection, WinningNumber winningNumber) {
+        WinningStatistics winningStatistics = new WinningStatistics(lottoCollection, winningNumber);
+
+        OutputView.printWinningStatistics(winningStatistics.get());
+        OutputView.printTotalYield(winningStatistics.computeTotalYield());
     }
 
 }
