8ccbc98cd1b468a2d519a01d2e56741dd5c37cd8
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-13 01:57:46 +0900

    refactor: 에러 메세지 추가 및 에러 잡기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,16 @@
 package lotto;
 
+import lotto.domain.Manager;
+import lotto.ui.Input;
+
 public class Application {
     public static void main(String[] args) {
-        System system = new System();
-        system.purchaseLottoTable();
-        system.notifySumOfResult(system.compareWinningLottoNumbers());
+        try {
+            Operator operator = new Operator();
+            operator.purchaseLottoTable();
+            operator.notifySumOfResult(operator.compareWinningLottoNumbers());
+        }catch (IllegalArgumentException e){
+            System.out.println(e.toString());
+        }
     }
 }
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.ui.Error;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -15,12 +17,15 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6 || numbers.size() != numbers.stream().distinct().count()) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(Error.MUST_BE_SIX_DIGIT);
+        }
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(Error.HAD_DUPLICATE_NUMBER);
         }
         for (int i = 0; i < numbers.size(); i++) {
-            if (numbers.get(i) < 1 || numbers.get(i) > 45){
-                throw new IllegalArgumentException();
+            if (numbers.get(i) < 1 || numbers.get(i) > 45) {
+                throw new IllegalArgumentException(Error.MUST_BE_NUMBERS_BETWEEN_1_TO_45);
             }
         }
     }
