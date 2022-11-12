184d4b3cc6a1e9d6c8e9273ed8872fc6fc9dea1e
Heebeom Yang <takeny1998@gmail.com>
2022-11-12 14:33:21 +0900

    style: change message format

--- src/main/java/lotto/domain/Messages.java
+++ src/main/java/lotto/domain/Messages.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
 public enum Messages {
-    INPUT_MONEY_MESSAGE("구입금액을 입력해 주세요.");
+    INPUT_MONEY("구입금액을 입력해 주세요.\n"),
+    INPUT_WIN_LOTTO_NUM("당첨 번호를 입력해 주세요.\n"),
+    OUTPUT_LOTTO_COUNT("%d개를 구매했습니다.\n");
 
     final private String message;
 
