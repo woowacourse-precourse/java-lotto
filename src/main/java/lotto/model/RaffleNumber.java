7cfadd6f07ea105c61b87ad5fecf0d4212b852c0
gomljo <gomljo0307@naver.com>
2022-11-13 03:50:52 +0900

    feat(RaffleNumber.java): 추첨 번호 도메인 생성

---
+++ src/main/java/lotto/model/RaffleNumber.java
@@ -0,0 +1,11 @@
+package lotto.model;
+
+import java.util.List;
+
+public class RaffleNumber {
+    private final List<Integer> raffleNumbers;
+
+    public RaffleNumber(List<Integer> numbers) {
+        this.raffleNumbers = numbers;
+    }
+}
