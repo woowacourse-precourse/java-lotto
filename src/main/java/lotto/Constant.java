5f58ea4e58b48b0a7c766ed9620f7bad80a2188f
mydreamis-18 <hitoryok@naver.com>
2022-11-13 03:59:30 +0900

    [feat] 입력 받은 당첨 번호에 대한 예외 처리 구현
    - after -
    
    Constant.java:
    LOTTO_STARTING_NUMBER 상수 생성
    LOTTO_ENDING_NUMBER 상수 생성
    
    User.java:
    입력 받은 당첨 번호에 대한 예외 처리 구현
    (숫자인지, 형식이 맞는지, 6개인지, 범위가 맞는지, 중복이 없는지 확인)
    
    converToNumber() 구현
    verifyNumbers() 구현
    verifyLength() 구현
    verifyRange() 구현
    verifyOverlap() 구현

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -3,6 +3,8 @@ package lotto;
 enum Constant {
 
 	PAYMENT_UNIT(1000),
+	LOTTO_STARTING_NUMBER(1),
+	LOTTO_ENDING_NUMBER(45),
 	LOTTO_LENGTH(6);
 
 	int value;
--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -3,8 +3,6 @@ package lotto;
 import java.util.List;
 import java.util.ArrayList;
 
-import org.assertj.core.util.Arrays;
-
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
@@ -32,23 +30,86 @@ public class User {
 		boolean isThousandUnits = payment % Constant.PAYMENT_UNIT.value == 0;
 
 		if (!isThousandUnits) {
-			throw new IllegalArgumentException("[ERROR] " + Constant.PAYMENT_UNIT.value + "원 단위로 입력해야 합니다.");
+
+			String message = "[ERROR] " + Constant.PAYMENT_UNIT.value + "원 단위로 입력해야 합니다.";
+			throw new IllegalArgumentException(message);
 		}
 	}
-	
+
 	public static List<Integer> getWinningNumbers() {
-		
+
 		System.out.println("\n당첨 번호를 입력해 주세요.");
 		String[] numbers = Console.readLine().split(",");
-		
-		List<Integer> winningNumbers = new ArrayList<Integer>(Constant.LOTTO_LENGTH.value);
+
+		List<Integer> winningNumbers = converToNumber(numbers);
+
+		verifyNumbers(winningNumbers);
+		return winningNumbers;
+	}
+
+	public static List<Integer> converToNumber(String[] numbers) {
+
+		List<Integer> result = new ArrayList<Integer>(Constant.LOTTO_LENGTH.value);
 		
 		for (String number : numbers) {
 			
-			Integer _number = Integer.valueOf(number);
-			winningNumbers.add(_number);
+			try {
+				Integer _number = Integer.valueOf(number);
+				result.add(_number);
+			}
+			
+			catch (NumberFormatException e) {
+				throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다.");
+			}
 		}
 		
-		return winningNumbers;
+		return result;
+	}
+
+	public static void verifyNumbers(List<Integer> numbers) {
+
+		verifyLength(numbers);
+		verifyRange(numbers);
+		verifyOverlap(numbers);
+	}
+
+	public static void verifyLength(List<Integer> numbers) {
+
+		boolean isValid = numbers.size() == Constant.LOTTO_LENGTH.value;
+		if (!isValid) {
+
+			String message = "[ERROR] '1,2,3,4,5,6' 형식으로 번호 6개를 입력해야 합니다.";
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void verifyRange(List<Integer> numbers) {
+
+		for (Integer number : numbers) {
+
+			boolean isValidStartingNumber = number >= Constant.LOTTO_STARTING_NUMBER.value;
+			boolean isValidEndingNumber = number <= Constant.LOTTO_ENDING_NUMBER.value;
+
+			if (!isValidStartingNumber || !isValidEndingNumber) {
+
+				String range = Constant.LOTTO_STARTING_NUMBER.value + "부터 " + Constant.LOTTO_ENDING_NUMBER.value;
+				String message = "[ERROR] 로또 번호는 " + range + " 사이의 숫자여야 합니다.";
+				throw new IllegalArgumentException(message);
+			}
+		}
+	}
+
+	public static void verifyOverlap(List<Integer> numbers) {
+
+		List<Integer> uniqueNumbers = new ArrayList<Integer>(Constant.LOTTO_LENGTH.value);
+
+		for (Integer number : numbers) {
+
+			if (uniqueNumbers.contains(number)) {
+				throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자들로 입력해야 합니다.");
+			}
+
+			uniqueNumbers.add(number);
+		}
 	}
 }
