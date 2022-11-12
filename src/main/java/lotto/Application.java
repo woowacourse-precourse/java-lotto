0ed71c66ce1f7a13782fd2d769a7c11585c0620f
Your Name <you@example.com>
2022-11-12 16:26:21 +0900

    feat: 구현 요소들을 application main에 배치

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Buyer buyer = new Buyer();
+        LottoCore lottoCore = new LottoCore();
+        LottoInputOutput lottoIO = new LottoInputOutput();
+
+        buyer.setMoney(lottoIO.getMoneyInput());
+        buyer.setLottoCount();
+
+        List<Lotto> buyerLottos = lottoCore.createManyLotto(buyer.getLottoCount());
+        lottoIO.printLottoCount(buyer.getLottoCount());
+        lottoIO.printLotto(buyerLottos);
+        buyer.setLottos(buyerLottos);
+
+        List<Integer> winnigLotto = lottoIO.getWinnigLottoAndCheck();
+        int bonus = lottoIO.getBonusNumberAndCheck(winnigLotto);
+
+        lottoCore.setWinnigLotto(winnigLotto);
+        lottoCore.setBonus(bonus);
+
+        Map<LottoPrize, Integer> prizeList = lottoCore.compareLotto(buyer.getLottos());
+        lottoIO.printWinningList(prizeList);
+
+        buyer.setWinmoney(prizeList);
+        lottoIO.printWinningRate(buyer.getWinMoney(), buyer.getMoney());
+
     }
 }
