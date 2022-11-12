c6db280bc7d67b88c585dd20a42a2342ebbb0bc1
jingyu9804 <swc05161@gmail.com>
2022-11-12 14:46:33 +0900

    feat(lotto) : 상수 클래스 선언

---
+++ src/main/java/lotto/Constant.java
@@ -0,0 +1,21 @@
+package lotto;
+
+public class Constant {
+    public static final String inputPurchasePrice = "구입금액을 입력해 주세요.";
+    public static final String outputPurchaseQuantity = "개를 구매했습니다.";
+
+    public static final String inputWinningNumber = "당첨 번호를 입력해 주세요.";
+    public static final String inputBonusNumber = "보너스 번호를 입력해 주세요.";
+
+    public static final String winningStatics = "당첨 통계";
+    public static final String dash = "---";
+    public static final String threeCorrect = "3개 일치 (5,000원) - ";
+    public static final String fourCorrect = "4개 일치 (50,000원) - ";
+    public static final String fiveCorrect = "5개 일치 (1,500,000원) - ";
+    public static final String fiveAndBonusCorrect = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    public static final String sixCorrect = "6개 일치 (2,000,000,000원) - 0";
+    public static final String count = "개";
+
+    public static final String totalStart = "총 수익률은 ";
+    public static final String totalEnd = "%입니다.";
+}
