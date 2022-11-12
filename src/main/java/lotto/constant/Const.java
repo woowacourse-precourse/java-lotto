978d98229ec47cf8ddfb4deef654e4dbc86cd960
GilHun <suleemon1@naver.com>
2022-11-12 23:36:30 +0900

    feat(constant): 정적 변수 및 출력문 상수화

---
+++ src/main/java/lotto/constant/Const.java
@@ -0,0 +1,18 @@
+package lotto.constant;
+
+public class Const {
+    public static final String BUY_LOTTO_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String PRINT_PURCHASED_LOTTO = "N개를 구매했습니다.";
+    public static final String ENTER_LOTTO_NUM = "당첨 번호를 입력해 주세요.";
+    public static final String ENTER_BONNUS_NUM = "보너스 번호를 입력해 주세요.";
+    public static final String PRINT_STATISTICS = "당첨 통계";
+
+    public static final int LOTTO_COST = 1000;
+    public static final int LOTTO_NUM_COUNT = 6;
+    public static final int LOTTO_BONUS_NUM_COUNT = 1;
+    public static final long FIRST_WINNINGS = 2000000000;
+    public static final long SECOND_WINNINGS = 30000000;
+    public static final int THIRD_WINNINGS = 1500000;
+    public static final int FOURTH_WINNINGS = 50000;
+    public static final int FIFTH_WINNINGS = 5000;
+}
