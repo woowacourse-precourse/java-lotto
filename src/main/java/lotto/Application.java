e8a597e673e10b1ec949e093b0a1127a2080bc31
heesang <dlagmltkd12@naver.com>
2022-11-12 19:11:38 +0900

    feat: Application main 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.controller.LottoGameController;
+import lotto.service.LottoGameService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoGameController lottoGameController = new LottoGameController(new LottoGameService());
+        lottoGameController.run();
     }
 }
