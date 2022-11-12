332914dd26d4f92532a943890deeade06f89c6a7
황민주 <minjoo@hwangminjooui-MacBookAir.local>
2022-11-12 14:06:19 +0900

    feat(Lotto): sortNumbers (src/main/java/lotto/Lotto.java)
    sortNumbers 메소드 추가:
    -숫자 리스트를 받으면 오름차순으로 정렬해서 반환하기 위해 추가함

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -16,5 +17,8 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private List <Integer> sortNumbers(List<Integer> numbers){
+        Collections.sort(numbers);
+        return numbers;
+    }
 }
