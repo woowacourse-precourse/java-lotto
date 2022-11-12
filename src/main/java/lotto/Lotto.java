f49aac17aa122226c38f4a0ecb5843ff6c983393
hyowonhan <wain1719@gmail.com>
2022-11-12 17:20:17 +0900

    feat: 당첨 번호 범위 예외 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -27,6 +27,7 @@ public class Lotto {
         numbers = numbers();
         validateCount(numbers);
         validateOverlap(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
@@ -51,6 +52,14 @@ public class Lotto {
         for(int i=0; i<numbers.size(); i++){
             if (Collections.frequency(numbers,numbers.get(i)) != 1) {
                 throw new IllegalArgumentException("[ERROR] 숫자가 중복되지 않아야 합니다.");
+            }
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for(int i=0; i<numbers.size(); i++) {
+            if(numbers.get(i)< 1 || numbers.get(i)> 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
         }
     }
