290f9bba13d78528314f814af7e4d2fde93b2ac1
Soomin Lim <augustth511@gmail.com>
2022-11-12 16:45:11 +0900

    feat(WinningLotto): 보너스 번호 입력 시 예외 처리 기능 및 에러문구 출력 기능 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -2,17 +2,43 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.domain.ErrorMessage.BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE;
+import static lotto.domain.ErrorMessage.LOTTO_NUMBER_RANGE_ERROR_MESSAGE;
+
 public class WinningLotto {
-    private final Lotto winningLotto;
+    private final Lotto winningNumbers;
     private final int bonusNumber;
 
     public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
-        this.winningLotto = new Lotto(winningNumbers);
+        this.winningNumbers = new Lotto(winningNumbers);
+
+        validateBonusNumber(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
-    public Lotto getWinningLotto() {
-        return winningLotto;
+    private void validateBonusNumber(int bonusNumber) {
+        validateRange(bonusNumber);
+        validateDuplicateWithWiningNumbers(bonusNumber);
+    }
+
+    private void validateRange(int bonusNumber) {
+        if (bonusNumber < Lotto.MINIMUM_NUMBER || bonusNumber > Lotto.MAXIMUM_NUMBER) {
+            System.out.println(LOTTO_NUMBER_RANGE_ERROR_MESSAGE);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicateWithWiningNumbers(int bonusNumber) {
+        if (winningNumbers.getNumbers()
+                .stream()
+                .anyMatch(number -> number == bonusNumber)) {
+            System.out.println(BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Lotto getWinningNumbers() {
+        return winningNumbers;
     }
 
     public int getBonusNumber() {
