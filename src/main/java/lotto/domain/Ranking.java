83a7010bcfba97c72f1117af678f1ab92644169b
Reznov <reznov03048@gmail.com>
2022-11-13 00:06:19 +0900

    feat: 중복 숫자 개수와 보너스 번호 유무를 확인해 몇 등 인지 알아내는 기능 구현

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -1,11 +1,14 @@
 package lotto.domain;
 
+import java.util.Arrays;
+
 public enum Ranking {
     FIRST(6, 2_000_000_000, false),
     SECOND(5, 30_000_000, true),
     THIRD(5, 1_500_000, false),
     FOURTH(4, 50_000, false),
-    FIFTH(3, 5_000, false);
+    FIFTH(3, 5_000, false),
+    NOTHING(0, 0, false);
 
     private int count;
     private int money;
@@ -15,5 +18,12 @@ public enum Ranking {
         this.count = count;
         this.money = money;
         this.hasBonusNumber = hasBonusNumber;
+    }
+
+    public static Ranking matchRanking(int sameNumberCount, boolean hasBonusNumber) {
+        return Arrays.stream(Ranking.values())
+                .filter(ranking -> ranking.count == sameNumberCount && ranking.hasBonusNumber == hasBonusNumber)
+                .findAny()
+                .orElse(NOTHING);
     }
 }
