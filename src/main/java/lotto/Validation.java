0b97a1f17636db171f272a4b2a3b1fab1d5c1d09
sieun <sieun960513@naver.com>
2022-11-12 20:42:56 +0900

    [validation] 하드 코딩 지양

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -1,11 +1,16 @@
 package lotto;
 
 public class Validation {
+
+    static final int MONEY_UNIT = 1000;
+    static final int LOTTO_MIN_NUM = 1;
+    static final int LOTTO_MAX_NUM = 45;
+
     public static boolean thousandUnit(int num) {
-        return num%1000 == 0;
+        return num % MONEY_UNIT == 0;
     }
 
     public static boolean lottoNumRange(int num) {
-        return 1 <= num && num <= 45;
+        return LOTTO_MIN_NUM <= num && num <= LOTTO_MAX_NUM;
     }
 }
