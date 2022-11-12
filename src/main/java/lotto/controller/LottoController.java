4de8d603fb2f8f4e91466d1b5d4c5546dfcd3c14
minju412 <ln8847@ajou.ac.kr>
2022-11-12 23:26:53 +0900

    refactor: separate methods
    - separate methods by function
    
    - use try-catch only once

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,5 @@
 package lotto.controller;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.BonusNumber;
@@ -10,68 +9,56 @@ import lotto.domain.LotteryDrawMachine;
 import lotto.domain.Lotto;
 import lotto.domain.LottoGroup;
 import lotto.domain.WinningLotto;
+import lotto.view.Input;
 import lotto.view.Output;
 
 public class LottoController {
 
     public void run() {
+        try {
+            Cost cost = createCost();
+
+            int purchaseCount = cost.getCost() / 1000;
+            Output.purchaseCountNotification(purchaseCount);
+
+            LottoGroup lottoGroup = createLottoGroup(purchaseCount);
+            WinningLotto winningLotto = createWinningLotto();
+            BonusNumber bonusNumber = createBonusNumber(winningLotto);
+
+            Calculator calculator = new Calculator();
+            List<Integer> matchResults = calculator.calculateMatchResults(lottoGroup, winningLotto,
+                    bonusNumber);
+            Output.printWinningStatistics(matchResults);
+
+            int profit = calculator.calculateProfit(matchResults);
+            double earningsRate = calculator.calculateEarningsRate(cost.getCost(), profit);
+            Output.earningsRateNotification(earningsRate);
+
+        } catch (IllegalArgumentException ex) {
+            Output.printError(ex.getMessage());
+        }
+    }
+
+    private Cost createCost() {
+        return new Cost(Input.inputCost());
+    }
+
+    private LottoGroup createLottoGroup(int purchaseCount) {
         LotteryDrawMachine lotteryDrawMachine = new LotteryDrawMachine();
-
-        Output.enterPurchaseCost();
-        Cost cost;
-        try {
-            String purchaseCost = Console.readLine();
-            cost = new Cost(purchaseCost);
-        } catch (IllegalArgumentException ex) {
-            Output.printError(ex.getMessage());
-            return;
-        }
-
-        int purchaseCount = cost.getCost() / 1000;
-        Output.purchaseCountNotification(purchaseCount);
-
         List<Lotto> lottos = new ArrayList<>();
-        try {
-            for (int count = 0; count < purchaseCount; count++) {
-                Lotto lotto = lotteryDrawMachine.createLotto();
-                System.out.println(lotto.getNumbers());
-                lottos.add(lotto);
-            }
-        } catch (IllegalArgumentException ex) {
-            Output.printError(ex.getMessage());
-            return;
+        for (int count = 0; count < purchaseCount; count++) {
+            Lotto lotto = lotteryDrawMachine.createLotto();
+            Output.printLotteryNumbers(lotto.getNumbers());
+            lottos.add(lotto);
         }
-        LottoGroup lottoGroup = new LottoGroup(lottos);
+        return new LottoGroup(lottos);
+    }
 
+    private WinningLotto createWinningLotto() {
+        return new WinningLotto(Input.inputWinningNumbers());
+    }
 
-        Output.enterWinningNumbers();
-        String inputWinningNumbers = Console.readLine();
-        WinningLotto winningLotto;
-        try {
-            winningLotto = new WinningLotto(inputWinningNumbers);
-        } catch (IllegalArgumentException ex) {
-            Output.printError(ex.getMessage());
-            return;
-        }
-
-        Output.enterBonusNumber();
-        String inputBonusNumber = Console.readLine();
-        BonusNumber bonusNumber;
-        try {
-            bonusNumber = new BonusNumber(inputBonusNumber, winningLotto);
-        } catch (IllegalArgumentException ex) {
-            Output.printError(ex.getMessage());
-            return;
-        }
-
-        Output.winningStatistics();
-        Calculator calculator = new Calculator();
-        List<Integer> matchResults = calculator.calculateMatchResults(lottoGroup, winningLotto,
-                bonusNumber);
-        Output.printWinningStatistics(matchResults);
-
-        int profit = calculator.calculateProfit(matchResults);
-        double earningsRate = calculator.calculateEarningsRate(cost.getCost(), profit);
-        Output.earningsRateNotification(earningsRate);
+    private BonusNumber createBonusNumber(WinningLotto winningLotto) {
+        return new BonusNumber(Input.inputBonusNumbers(), winningLotto);
     }
 }
