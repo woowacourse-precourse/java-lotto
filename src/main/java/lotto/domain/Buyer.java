255ad91ebcd6dc8500a971c1319ddeac654a3413
KSH <tkdgns543@gmail.com>
2022-11-12 13:41:57 +0900

    feat(domain): 로또 장수 확인 기능 구현
    1000원 단위 금액 예외 확인
    로또 장수 반환

--- src/main/java/lotto/domain/Buyer.java
+++ src/main/java/lotto/domain/Buyer.java
@@ -1,4 +1,13 @@
 package lotto.domain;
 
 public class Buyer {
+    public Buyer() {}
+
+    public long countLottos (long money) throws IllegalArgumentException {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위만 가능합니다");
+        }
+
+        return money / 1000;
+    }
 }
