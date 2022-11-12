7b0c39e99038b2016db30517b318a51ba446ae31
coldbucks <met.hsw@gmail.com>
2022-11-12 17:47:41 +0900

    feat(Lotto):보너스 번호 출력 입력 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -26,6 +27,12 @@ public class Application {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             System.out.println(numbers);
         }
+
+        System.out.println("당첨 번호를 입력해 주세요");
+        String winnerNumber = Console.readLine();
+
+        System.out.println("보너스 번호를 입력해 주세요");
+        String bonnusNumber = Console.readLine();
     }
 }
 
