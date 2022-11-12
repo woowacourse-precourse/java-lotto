63c9036ff416e7cd5806dd0077288a1febc6834f
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-12 21:02:37 +0900

    refactor(lotto): 하드 코딩 된 부분 수정 및 의미없는 코드 수정

--- src/main/java/domain/BuyLottoTickets.java
+++ src/main/java/domain/BuyLottoTickets.java
@@ -8,19 +8,24 @@ import view.OutputView;
 
 public class BuyLottoTickets {
 
+    private static final int CURRENCY_UNIT=1000;
+    private static final int MAX_NUMBER = 45;
+    private static final int MIN_NUMBER =1;
+    private static final int TICKET_SIZE=6;
     static List<List<Integer>> buyLottoTickets = new ArrayList<>();
 
+
     public void putLottoNumberTickets(int money){
-        int pick_count= money/1000;
+        int pick_count= money/CURRENCY_UNIT;
         for(int number=0; number<pick_count; number++){
-            setLottoNumberTicket(buyLottoTickets, number);
+            setLottoNumberTicket(buyLottoTickets);
           }
 
         OutputView.printLottoTickets(pick_count, buyLottoTickets);
     }
 
-    private void setLottoNumberTicket(List<List<Integer>> buyLottoTickets, int number) {
-        List<Integer> LottoTicket = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    private void setLottoNumberTicket(List<List<Integer>> buyLottoTickets) {
+        List<Integer> LottoTicket = new ArrayList<>(Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, TICKET_SIZE));
         Collections.sort(LottoTicket);
         buyLottoTickets.add(LottoTicket);
     }
