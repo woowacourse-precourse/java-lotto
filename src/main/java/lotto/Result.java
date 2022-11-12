79cdd503245ded9e10611c9dc8a700eb3595a223
EUNJEONGMUN <munej26@gmail.com>
2022-11-12 12:10:37 +0900

    refactor: saveMatchResult 메서드 접근제어자 public 에서 private으로 수정

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -18,7 +18,7 @@ public class Result {
         showResult();
     }
 
-    public void saveMatchResult(List<Lotto> lottoTickets) {
+    private void saveMatchResult(List<Lotto> lottoTickets) {
         for (Lotto lotto : lottoTickets) {
             MatchCount matchResult = LottoNumber.getMatchResult(lotto);
             totalMatchResult.put(matchResult, totalMatchResult.getOrDefault(matchResult, 0)+1);
