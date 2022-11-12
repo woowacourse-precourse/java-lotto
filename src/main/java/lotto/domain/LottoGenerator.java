4869f7d151710f5dd03ee631cf5732ea71e26d27
5nam <ohnam90@gmail.com>
2022-11-12 14:07:33 +0900

    feat: 뽑은 숫자를 오른차순으로 정렬한다.

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -2,11 +2,17 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
     public List<Integer> createLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
+
+    public List<Integer> sortLotto(List<Integer> numbers) {
+        Collections.sort(numbers);
         return numbers;
     }
 }
