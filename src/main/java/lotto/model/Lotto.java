fccb20794d3d95712f15244d65c10040633e7390
SHEOMM <esh0821@snu.ac.kr>
2022-11-13 02:12:59 +0900

    refactor(Lotto): 로또 사이즈 체크 함수를 validate 함수에서 분리
    기능 분리를 위해 로또 사이즈 체크 기능을 별도로 구현

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,25 @@
+package lotto.model;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        checkSize(numbers);
+    }
+
+    private void checkSize(List<Integer> numbers){
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+
+}
