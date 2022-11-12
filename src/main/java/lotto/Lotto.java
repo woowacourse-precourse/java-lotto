f4f81318bc8c6b495718d105096db4a857787ef8
eora21 <eora21@naver.com>
2022-11-12 20:03:15 +0900

    feat: 🎸 추가 로또 생성 시 중복된 값 확인 및 예외 메시지 작성
    로또 생성 시 값의 길이가 6이 아닐 때, 그리고 중복값을 제거한 길이가 6이 아닐 때 예외처리를 해주었다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import constant.LottoRule;
+
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -11,8 +14,12 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != LottoRule.SIZE.getValue()) {
+            throw new IllegalArgumentException("값의 길이가 " + LottoRule.SIZE + "를 넘습니다.");
+        }
+
+        if (new HashSet<>(numbers).size() != LottoRule.SIZE.getValue()) {
+            throw new IllegalArgumentException("중복된 값이 있습니다.");
         }
     }
 
