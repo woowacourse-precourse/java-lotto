2a48b7d4d1c86b4c8b632d4975e3e4ea6adb1945
Devtist <yt930310@snu.ac.kr>
2022-11-12 23:52:01 +0900

    feat: 구입 금액 입력 안내를 출력하고 사용자의 입력을 받는 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.constant.Message;
+
+public class InputView {
+	public String getPurchasingAmount() {
+		printAskingPurchasingAmountMessage();
+		return Console.readLine();
+	}
+
+	private void printAskingPurchasingAmountMessage() {
+		System.out.println(Message.ASKING_PURCHASING_AMOUNT.message());
+	}
+
+
+}
