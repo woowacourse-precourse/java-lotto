b8c7454b5d832c5f828da4572476569da99d4c11
김형기 <khk211113@naver.com>
2022-11-13 03:46:48 +0900

    docs: 메인 애플리케이션 주석 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -22,6 +22,11 @@ public class Application {
         }
     }
 
+    /**
+     * 입력된 수만큼 로또를 자동으로 구매합니다.
+     * @param num 구매할 로또 개수
+     * @return 생성된 로또번호 목록
+     */
     private static List<Lotto> buyLotteries(int num) {
         List<Lotto> lotteries = new ArrayList<>();
         for (int i = 0; i < num; i++) {
@@ -30,13 +35,26 @@ public class Application {
         return lotteries;
     }
 
+    /**
+     * 사용자에게 입력받은 값으로 로또 회차를 생성합니다.
+     * UserInterface를 통해 당첨번호, 보너스 번호를 입력받습니다.
+     * @return 로또 회차
+     * @throws IllegalArgumentException 사용자 입력에 의한 오류
+     */
     private static LottoRound makeLottoRound() throws IllegalArgumentException {
         Lotto luckyNumbers = UserInterface.readLuckyNumbers();
         int bonusNumber = UserInterface.readBonusNumber();
         return new LottoRound(luckyNumbers, bonusNumber);
     }
 
-    private static LottoStatistics makeStatistics(List<Lotto> boughtLotteries, LottoRound lottoRound) throws IllegalArgumentException {
+    /**
+     * 당첨 통계 결과를 생성합니다.
+     * @param boughtLotteries 구매한 로또번호 목록
+     * @param lottoRound 로또 회차 정보
+     * @return 당첨 통계 정보
+     * @throws IllegalArgumentException
+     */
+    private static LottoStatistics makeStatistics(List<Lotto> boughtLotteries, LottoRound lottoRound) {
         LottoStatistics statistics = new LottoStatistics();
         for (Lotto lotto : boughtLotteries) {
             statistics.add(lottoRound.getRank(lotto));
