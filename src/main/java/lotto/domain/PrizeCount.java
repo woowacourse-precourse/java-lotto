3a58cc98afc6960aeb90ed5c1fb2e19854cfe324
im-shung <seoyoung1113@gmail.com>
2022-11-12 23:11:05 +0900

    fix: :bug: Map 초기화 방식 변경 Map.of()는 Immutable 객체를 반환한다. Map.of()는 Map 초기화 후 값 조회만 하는 경우에만 사용하도록 한다.

--- src/main/java/lotto/domain/PrizeCount.java
+++ src/main/java/lotto/domain/PrizeCount.java
@@ -1,19 +1,26 @@
 package lotto.domain;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class PrizeCount {
 
-    private Map<Prize, Integer> items = Map.of(
-            Prize.FIRST, 0,
-            Prize.SECOND, 0,
-            Prize.THIRD, 0,
-            Prize.FOURTH, 0,
-            Prize.FIFTH, 0
-    );
+    private final Map<Prize, Integer> items = new HashMap<>() {
+        {
+            put(Prize.FIRST, 0);
+            put(Prize.SECOND, 0);
+            put(Prize.THIRD, 0);
+            put(Prize.FOURTH, 0);
+            put(Prize.FIFTH, 0);
+        }
+    };
 
     public void addPrizeCount(Prize prize) {
         items.put(prize, items.get(prize) + 1);
+    }
+
+    public int getPrizeCount(Prize prize) {
+        return items.get(prize);
     }
 
     public int getTotalPrizeMoney(Prize prize) {
