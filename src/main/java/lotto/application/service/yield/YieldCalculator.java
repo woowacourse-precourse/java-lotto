ca6592269d262716092cff493dbbd2d27e55c41d
K-Diger <kitten.diger@gmail.com>
2022-11-12 17:39:04 +0900

    style : YieldCalculator 클래스 내 줄바꿈 추가 - showStatistic() 메서드에 줄바꿈을 추가한다.

--- src/main/java/lotto/application/service/yield/YieldCalculator.java
+++ src/main/java/lotto/application/service/yield/YieldCalculator.java
@@ -48,11 +48,16 @@ public class YieldCalculator {
 
     public void showStatistic(Map<String, Integer> winInformation) {
         System.out.println("\n당첨 통계\n---");
-        System.out.printf("3개 일치 (5,000원) - %d개\n", winInformation.get("three"));
-        System.out.printf("4개 일치 (50,000원) - %d개\n", winInformation.get("four"));
-        System.out.printf("5개 일치 (1,500,000원) - %d개\n", winInformation.get("five"));
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", winInformation.get("fiveContainingBonusNumber"));
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", winInformation.get("six"));
+        System.out.printf("3개 일치 (5,000원) - %d개\n",
+                winInformation.get("three"));
+        System.out.printf("4개 일치 (50,000원) - %d개\n",
+                winInformation.get("four"));
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n",
+                winInformation.get("five"));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n",
+                winInformation.get("fiveContainingBonusNumber"));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n",
+                winInformation.get("six"));
     }
 
     public double extractYield(Map<String, Integer> winInformation, int purchaseMoney) {
