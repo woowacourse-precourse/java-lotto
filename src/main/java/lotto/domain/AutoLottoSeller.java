fc57c4352483d23af7f62395814638fb2d668404
jaehyeon <jhjh0022@naver.com>
2022-11-12 23:33:24 +0900

    feat: 금액에 맞게 로또 갯수를 파악하고 갯수만큼 로또를 만들어주는 기능

--- src/main/java/lotto/domain/AutoLottoSeller.java
+++ src/main/java/lotto/domain/AutoLottoSeller.java
@@ -1,2 +1,31 @@
-package lotto.domain;public class AutoLottoSeller {
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutoLottoSeller {
+
+    private Lotto selectRandomNumbers() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+        return new Lotto(randomNumbers);
+    }
+
+    private int buyaFewLotto(int money) {
+        return money / 1000;
+    }
+
+    public List<Lotto> buyLotto(int money) {
+        int cnt = buyaFewLotto(money);
+        List<Lotto> lottoNumbers = new ArrayList<>();
+        for (int i = 0; i < cnt; i++) {
+            lottoNumbers.add(selectRandomNumbers());
+        }
+        return lottoNumbers;
+    }
+
+
 }
