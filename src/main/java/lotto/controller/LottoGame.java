e0feefcb916e56969edf541927904e60f5a037e0
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-13 01:11:19 +0900

    feat : 당첨 내역을 출력하는 메서드

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,27 +1,57 @@
 package lotto.controller;
 
-import static lotto.domain.LottoBuyer.purchaseLotto;
-import static lotto.domain.WinningLotto.bonusNumber;
-import static lotto.domain.WinningLotto.bonusNumberInput;
-import static lotto.domain.WinningLotto.winningNumber;
-import static lotto.domain.WinningLotto.winningNumberInput;
+import static lotto.view.LottoBuyer.purchaseLotto;
+import static lotto.view.LottoBuyer.purchaseLottoCount;
+import static lotto.view.BonusInput.*;
+import static lotto.view.WinningInput.*;
 
-import lotto.domain.Lotto;
-import lotto.view.ErrorMessage;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import lotto.domain.LottoRank;
+import lotto.domain.ErrorMessage;
 
-import java.util.Arrays;
 import java.util.List;
+import lotto.view.Message;
 
 public class LottoGame {
 
     public void start() {
-        List<Lotto> lottos;
+        List<List<Integer>> lottos;
         lottos = purchaseLotto();
-        System.out.println(lottos);
         winningNumberInput();
         bonusNumberInput();
-        System.out.println(Arrays.toString(winningNumber.split(",")));
-        validateDuplicate(winningNumber, bonusNumber);
+        validateDuplicate(winningInput, bonusInput);
+        getWinningResult(lottos, winningNumbers);
+        getBonusResult(lottos);
+        getTotalResult();
+    }
+
+    private static void getTotalResult() {
+        Map<LottoRank, Integer> result = setToMap();
+        LottoRank rank;
+        System.out.println(Message.WINNING_STATS.getMessage());
+        for (int i = 0; i < winningCount; i++) {
+            rank = LottoRank.getRankResult(winningCount, bonus);
+            result.put(rank, result.get(rank) + 1);
+        }
+        printWinningList(result);
+    }
+
+    private static void printWinningList(Map<LottoRank, Integer> result) {
+        for (LottoRank rank : LottoRank.values()) {
+            if (rank.getMoney() == 0) {
+                continue;
+            }
+            System.out.println(rank.getMessage() + result.get(rank) + "개");
+        }
+    }
+
+    private static Map<LottoRank, Integer> setToMap() {
+        Map<LottoRank, Integer> result = new LinkedHashMap<>();
+        for (LottoRank rank : LottoRank.values()) {
+            result.put(rank, 0);
+        }
+        return result;
     }
 
     private static void validateDuplicate(String winningNumber, String bonusNumber) {
