d1c48af47a36295e8ebb346bb12f114fbe4429f2
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-12 20:41:42 +0900

    Feat(Lotto) : 로또 번호 출력 기능 추가 #69
    toString()을 Override했습니다.
    이 메소드는 numbers를 문자열로 리턴합니다.
    
    Ref : #66

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,13 +2,20 @@ package lotto;
 
 import lotto.CheckInputException;
 
+import lotto.Util;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<Integer> numbers) throws IllegalArgumentException{
         validate(numbers);
+        try {
+            CheckInputException.checkDuplicateNumbers(numbers);
+        } catch(IllegalArgumentException iae){
+            throw iae;
+        }
         this.numbers = numbers;
     }
 
@@ -22,4 +29,10 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
+
+
 }
