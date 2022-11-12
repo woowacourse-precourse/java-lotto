d6d31e1134dd84f8bc52f9bded4ea6d7a54ba405
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-12 15:03:06 +0900

    refactor(LottoController): buyLottoTickets 메서드 및 calculateResult 메서드 분리, try-catch문 적용

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,8 @@
 package lotto.controller;
 
+import java.util.List;
+
+import lotto.domain.Lotto;
 import lotto.domain.LottoMachine;
 import lotto.domain.LottoStore;
 import lotto.domain.ProfitRateCalculator;
@@ -11,15 +14,27 @@ import lotto.view.OutputView;
 public class LottoController {
 
 	public void play() {
-		LottoStore lottoStore = new LottoStore(InputView.scanPurchaseAmount());
-		int numberOfTickets = lottoStore.getNumberOfTickets();
+		try {
+			LottoStore lottoStore = new LottoStore(InputView.scanPurchaseAmount());
+			List<Lotto> lottoTickets = buyLottoTickets(lottoStore.getNumberOfTickets());
+			WinningNumbers winningNumbers = new WinningNumbers(InputView.scanWinningNumbers(),
+				InputView.scanBonusNumber());
+			calculateResult(lottoStore.getPurchaseAmount(), lottoTickets, winningNumbers);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	private List<Lotto> buyLottoTickets(int numberOfTickets) {
 		LottoMachine lottoMachine = new LottoMachine(numberOfTickets);
 		OutputView.printNumberOfTickets(numberOfTickets);
 		OutputView.printLottoTickets(lottoMachine);
-		WinningNumbers winningNumbers = new WinningNumbers(InputView.scanWinningNumbers(), InputView.scanBonusNumber());
-		RankCounter rankCounter = new RankCounter(lottoMachine.getLottoTickets(), winningNumbers);
-		ProfitRateCalculator profitRateCalculator = new ProfitRateCalculator(rankCounter.getRanks(),
-			lottoStore.getPurchaseAmount());
+		return lottoMachine.getLottoTickets();
+	}
+
+	private void calculateResult(int purchaseAmount, List<Lotto> lottoTickets, WinningNumbers winningNumbers) {
+		RankCounter rankCounter = new RankCounter(lottoTickets, winningNumbers);
+		ProfitRateCalculator profitRateCalculator = new ProfitRateCalculator(rankCounter.getRanks(), purchaseAmount);
 		OutputView.printWinningResult(rankCounter.getRanks());
 		OutputView.printProfitRate(profitRateCalculator.getProfitRate());
 	}
