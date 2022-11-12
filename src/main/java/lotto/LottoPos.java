bb5b1eaa83a8d7e6386450095685d8aa3a4c52c7
JJongmen <pjy90123@gmail.com>
2022-11-12 18:13:09 +0900

    feat: 로또를 발행하는 기능 구현

---
+++ src/main/java/lotto/LottoPos.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoPos {
+    private static final int SIZE = 6;
+    private static final int RANGE_BEGIN = 1;
+    private static final int RANGE_END = 45;
+
+    public Lotto issue() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(RANGE_BEGIN, RANGE_END, SIZE);
+        Collections.sort(numbers);
+        return new Lotto(numbers);
+    }
+}
