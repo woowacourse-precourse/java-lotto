f8680d001f4c401b932fb353d436cd87b887f934
coldbucks <met.hsw@gmail.com>
2022-11-12 17:37:52 +0900

    feat(Lotto): ''개의 로또 번호 연산 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
+
+import java.util.List;
 
 public class Application {
 
@@ -9,7 +12,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Application application = new Application();
-
 
         System.out.println("구매금액을 입력해 주세요.");
         String buyMoney = Console.readLine();
@@ -20,6 +22,10 @@ public class Application {
         int lottoAmount = buyLottoMoney / lottoPrice ;
         System.out.println(lottoAmount + "개를 구매했습니다.");
 
+        for(int ticket = 1; ticket <= lottoAmount; ticket++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            System.out.println(numbers);
+        }
     }
 }
 
