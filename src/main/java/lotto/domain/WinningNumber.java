61dac31b4da4858ebbc8a7deb1311e7e110b04ee
HwangSeokJin <ghkd3703@gmail.com>
2022-11-13 04:03:52 +0900

    feat: 보너스 번호가 당첨 번호와 겹치면 예외 처리한다

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -2,7 +2,7 @@ package lotto.domain;
 
 import java.util.List;
 
-public class WinningNumber extends Lotto{
+public class WinningNumber extends Lotto {
     private int bonusNumber;
 
     public WinningNumber(List<Integer> numbers) {
@@ -11,7 +11,14 @@ public class WinningNumber extends Lotto{
 
     public void setBonusNumber(int bonusNumber) {
         validateRange(bonusNumber);
+        validateBonusNumber(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    void validateBonusNumber(int number) {
+        if (getNumbers().contains(number)) {
+            throw new IllegalArgumentException("보너스 번호가 당첨 번호와 겹칩니다.");
+        }
     }
 
     public int getBonusNumber() {
