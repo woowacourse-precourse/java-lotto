91bb0ce198bf54ebe7bd5d40e0f3be26756ea07e
codingvegeta <hong090992@gmail.com>
2022-11-13 01:37:47 +0900

    refact : "enum 클래스를 변경하였습니다."

--- src/main/java/domain/Victory.java
+++ src/main/java/domain/Victory.java
@@ -1,11 +1,19 @@
 package domain;
 
 public enum Victory {
-    MATCH3("3개 일치 (5,000원)"), MATCH4("4개 일치 (50,000원)"),  MATCH5("5개 일치 (1,500,000원)"),
-    MATCH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원)"), MATCH_ALL("6개 일치 (2,000,000,000원)");
+    MATCH3(5000,3,false),
+    MATCH4(50000,4,false),
+    MATCH5(1500000,5,false),
+    MATCH_BONUS(30000000,6,true),
+    MATCH_ALL(2000000000,6,false);
 
-    String matchType = "";
-    Victory(String matchType) {
-        this.matchType = matchType;
+    private final int winningMoney;
+    private final int matchCount;
+    private final boolean isBonus;
+
+    Victory(int winningMoney, int matchCount, boolean isBonus) {
+        this.winningMoney = winningMoney;
+        this.matchCount = matchCount;
+        this.isBonus = isBonus;
     }
 }
