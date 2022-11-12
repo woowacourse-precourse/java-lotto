dbe9c9087fde1d886a9d2eafbcb1f0e716bb2b66
syr9707 <yrshin9771@gmail.com>
2022-11-12 12:36:39 +0900

    feat(createLottoNumbers): 랜덤 로또 숫자 생성

--- src/main/java/lotto/CreateLottoNumbers.java
+++ src/main/java/lotto/CreateLottoNumbers.java
@@ -1,2 +1,13 @@
-package lotto;public class createLottoNumbers {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+// 랜덤 로또 숫자 생성
+public class CreateLottoNumbers {
+
+    List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    Lotto lotto = new Lotto(numbers);
+
 }
