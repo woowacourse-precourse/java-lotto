72b825573b10cf7bc9a5f3aa71b86429a8e44e47
grand7070 <grand7070@naver.com>
2022-11-12 19:08:06 +0900

    refactor: 전체 상금 로직 분리

--- src/main/java/lotto/Yield.java
+++ src/main/java/lotto/Yield.java
@@ -11,12 +11,16 @@ public class Yield {
 
     public Double getYield() {
         // 5-3. 소수점 둘째 자리에서 반올림한 수익률을 구한다.
+        return Math.round(((double) reward() / Integer.parseInt(this.price.getPrice())) * 100) / 100.0;
+    }
+
+    private Integer reward() {
         Integer reward = 0;
-        reward += match.getMatches().get(Lanking.rank1) * 2000000000;
-        reward += match.getMatches().get(Lanking.rank2) * 30000000;
-        reward += match.getMatches().get(Lanking.rank3) * 1500000;
-        reward += match.getMatches().get(Lanking.rank4) * 50000;
-        reward += match.getMatches().get(Lanking.rank5) * 5000;
-        return Math.round(((double) reward / Integer.parseInt(this.price.getPrice()))*100) / 100.0;
+        reward += this.match.getMatches().get(Lanking.rank1) * 2000000000;
+        reward += this.match.getMatches().get(Lanking.rank2) * 30000000;
+        reward += this.match.getMatches().get(Lanking.rank3) * 1500000;
+        reward += this.match.getMatches().get(Lanking.rank4) * 50000;
+        reward += this.match.getMatches().get(Lanking.rank5) * 5000;
+        return reward;
     }
 }
