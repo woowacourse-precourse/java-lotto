6cb27a8a65933741a4c421e9d9ece2692cd41307
ddubidubap <park62000@gmail.com>
2022-11-12 16:36:13 +0900

    feat: Enum에 보너스 번호 인정 여부를 나타내는 필드 추가

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -7,31 +7,43 @@ import java.util.stream.Stream;
 import static lotto.view.Constants.*;
 
 public enum Prize {
-    FIFTH(5_000, SAME_THREE),
-    FOURTH(50_000, SAME_FOUR),
-    THIRD(1_500_000, SAME_FIVE),
-    SECOND(30_000_000, SAME_FIVE_BONUS),
-    FIRST(2_000_000_000, SAME_SIX);
+    FIFTH(5_000, false, SAME_THREE),
+    FOURTH(50_000, false, SAME_FOUR),
+    THIRD(1_500_000, false, SAME_FIVE),
+    SECOND(30_000_000, true, SAME_FIVE_BONUS),
+    FIRST(2_000_000_000, false, SAME_SIX);
 
     private final int prizeMoney;
+    private final boolean acceptBonus;
     private final String prizeNotice;
 
-    Prize(int prizeMoney, String prizeNotice) {
+    Prize(int prizeMoney, boolean acceptBonus, String prizeNotice) {
         this.prizeMoney = prizeMoney;
+        this.acceptBonus = acceptBonus;
         this.prizeNotice = prizeNotice;
     }
 
-    public int getPrizeMoney() {
+    private int getPrizeMoney() {
         return prizeMoney;
     }
 
-    public String getPrizeNotice() {
+    private boolean getAcceptBonus() {
+        return acceptBonus;
+    }
+
+    private String getPrizeNotice() {
         return prizeNotice;
     }
 
     public static List<Integer> getMoneyValues() {
         return Stream.of(Prize.values())
                 .map(Prize::getPrizeMoney)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Boolean> getBonusValues() {
+        return Stream.of(Prize.values())
+                .map(Prize::getAcceptBonus)
                 .collect(Collectors.toList());
     }
 
