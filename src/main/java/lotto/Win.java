5fc97f6b86890234c6a7988bdcd463b76f78da5c
JunginPark4364 <wjddlsee@gmail.com>
2022-11-13 03:28:23 +0900

    feat: 당첨관련 Win 클래스 생성

---
+++ src/main/java/lotto/Win.java
@@ -0,0 +1,20 @@
+package lotto;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Win {
+    private static final int BEGIN_RANKING = 1;
+    private static final int END_RANKING = 5;
+
+    private Map<Integer, Integer> ranking;
+
+    public Win(Map<Integer, Integer> ranking) {
+        this.ranking = ranking;
+    }
+
+    // TODO 당첨 내역 출력
+
+    // TODO 총 당첨금액 반환
+}
