876544c6701c4dc19c8fc6f00361ca08f65bc1e7
eastsage <dpwk8@naver.com>
2022-11-13 02:21:45 +0900

    feat: 전체 동작을 제어하는 LottoGame 클래스 추가
    UI 로직과 핵심 로직 사이에서 흐름을 제어해주는 역할을 하는 클래스 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.UI.ConsoleView;
+import lotto.UI.InputMoney;
+import lotto.UI.InputWinningAndBonusNumbers;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            LottoGame lottoGame = new LottoGame();
+            lottoGame.start();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 잘못된 사용자 입력입니다.");
+        }
     }
 }
---
+++ src/main/java/lotto/domain/LottoGame.java
@@ -0,0 +1,56 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Map;
+import lotto.UI.ConsoleView;
+import lotto.UI.InputMoney;
+import lotto.UI.InputWinningAndBonusNumbers;
+
+public class LottoGame {
+    private List<Lotto> lottos;
+    private int money;
+    private Lotto winningNumbers;
+    private Integer bonusNumber;
+    private double profitRate;
+
+    public LottoGame() {
+    }
+
+    public void start() {
+        InputMoney inputMoney = new InputMoney();
+        InputWinningAndBonusNumbers inputAnswers = new InputWinningAndBonusNumbers();
+        ConsoleView view = new ConsoleView();
+
+        buyLotto(inputMoney, view);
+        drawLotto(inputAnswers, view);
+        checkResult(view);
+    }
+
+    private void checkResult(ConsoleView view) {
+        CheckLottosPrize checkPrize = new CheckLottosPrize(lottos, winningNumbers, bonusNumber);
+
+        Map<String, Integer> totalPrize = checkPrize.getTotalPrize();
+        view.printTotalPrize(totalPrize);
+        CalculateProfitRate calculateProfitRate = new CalculateProfitRate(money, totalPrize);
+        profitRate = calculateProfitRate.getProfitRate();
+        view.printProfitRate(profitRate);
+    }
+
+    private void drawLotto(InputWinningAndBonusNumbers inputAnswers, ConsoleView view) {
+        view.printRequestWinningNumbers();
+        winningNumbers = inputAnswers.inputWinningNumbers();
+
+        view.printRequestBonusNumber();
+        bonusNumber = inputAnswers.inputBonusNumber();
+    }
+
+    private void buyLotto(InputMoney inputMoney, ConsoleView view) {
+        view.printRequestMoney();
+        money = inputMoney.input();
+
+        view.printNumberOfLotto(money);
+        LottoGenerator generator = new LottoGenerator(money);
+        lottos = generator.generateLottos();
+        view.printLottos(lottos);
+    }
+}
