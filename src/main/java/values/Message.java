06eb8dcc9a97c664975d9e9e7bfca7ef6ad3d630
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-11-12 14:24:05 +0900

    fix : add SIZE_ERROR Message

--- src/main/java/values/Message.java
+++ src/main/java/values/Message.java
@@ -2,10 +2,10 @@ package values;
 
 public class Message {
     public static class Console {
-        static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
-        static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-        static final String OUTPUT_CNT = "개를 구매했습니다.";
-        static final String STATISTICS = "당첨 통계\n---";
+        public static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+        public static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+        public static final String OUTPUT_CNT = "개를 구매했습니다.";
+        public static final String STATISTICS = "당첨 통계\n---";
 
         public String getTotalRevenueMessage(Double percent) {
             StringBuffer message = new StringBuffer();
@@ -20,7 +20,8 @@ public class Message {
     }
 
     public static class Error {
-        static final String BOUND_ERROR = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-        static final String DUPLICATION_ERROR = "[ERROR] 로또 번호에는 중복이 없어야 합니다.";
+        public static final String RANGE_ERROR = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+        public static final String DUPLICATION_ERROR = "[ERROR] 로또 번호에는 중복이 없어야 합니다.";
+        public static final String SIZE_ERROR = "[ERROR] 로또 번호는 6개여야 합니다.";
     }
 }
