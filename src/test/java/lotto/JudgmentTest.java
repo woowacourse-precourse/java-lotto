a596262448fb8c2364abfb493c3568fa451879d0
jeongjihun <wlgns2234@naver.com>
2022-11-12 17:08:46 +0900

    test(lotto): 보너지 번호 가지는지 확인 테스트

--- src/test/java/lotto/JudgmentTest.java
+++ src/test/java/lotto/JudgmentTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 
@@ -25,6 +26,16 @@ class JudgmentTest {
     void 같은_숫자_개수_찾기2() {
         int result = judgment.correctCount(Arrays.asList(1,2,3,4,5,6), Arrays.asList(1, 3, 5, 14, 22, 45));
         assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void 보너스번호_있는지() {
+        assertTrue(judgment.hasBonusNumber(3, Arrays.asList(1,2,3,4,5,6)));
+    }
+
+    @Test
+    void 보너스번호_없는지() {
+        assertFalse(judgment.hasBonusNumber(39, Arrays.asList(1,2,3,4,5,6)));
     }
 
 }
