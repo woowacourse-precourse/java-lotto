e597de6094e7e38ff93691097478331e922260d1
Isu Shin <isu010614@naver.com>
2022-11-12 18:31:52 +0900

    feat: 구매한 로또의 수량을 계산하는 기능 구현
    예외처리
    - 구매 금액이 1000원 미만인 경우
    - 1000원으로 나누어 떨어지지 않는 경우

---
+++ src/main/java/lotto/domain/BuyLotto.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+public class BuyLotto {
+    private static final int LOTTO_PRICE = 1000;
+    private final int price;
+
+    public BuyLotto(final int price) {
+        validatePrice(price);
+        this.price = price;
+    }
+
+    public int lottoQuantity() {
+        return price / LOTTO_PRICE;
+    }
+
+    private void validatePrice(final int price) {
+        if (price < LOTTO_PRICE) {
+            throw new IllegalArgumentException("[ERROR]Lotto purchase price must be more than 1000 won.");
+        }
+        if (price % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR]Buy the lotto by 1000 won.");
+        }
+    }
+
+}
