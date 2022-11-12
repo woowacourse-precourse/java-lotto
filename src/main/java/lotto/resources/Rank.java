beacf406d78b2863ba2789feaf2708487dc3a45d
khsrla9806 <kimhunsope@naver.com>
2022-11-12 18:43:55 +0900

    feat(Rank): 로또 순위별 당첨금액을 저장하는 enum을 구현

---
+++ src/main/java/lotto/resources/Rank.java
@@ -0,0 +1,20 @@
+package lotto.resources;
+
+public enum Rank {
+    FIRST_PLACE(2000000000),
+    SECOND_PLACE(30000000),
+    THIRD_PLACE(1500000),
+    FOURTH_PLACE(50000),
+    FIFTH_PLACE(5000),
+    NOTHING(0);
+
+    private int prizeMoney;
+
+    Rank(int prizeMoney) {
+        this.prizeMoney = prizeMoney;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+}
