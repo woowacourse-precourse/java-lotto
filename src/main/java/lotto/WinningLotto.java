bdfc549385221a14fd0113fde5e7eea05192aff4
tenta <eunoia3802@gmail.com>
2022-11-12 20:25:23 +0900

    refactor: 당첨번호 입력문 입력 클래스로 변경

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -6,11 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WinningLotto {
-    private final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
-
     public void creteWinningNumbers() {
         InputView inputView = new InputView();
-        System.out.println(INPUT_WINNING_NUMBER);
         String winningNumber = inputView.inputWinningNumber();
         List<Integer> winningNumbers = new ArrayList<>();
         List<String> winningStringNumbers = List.of(winningNumber.split(","));
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public int inputMoney() {
@@ -14,6 +15,7 @@ public class InputView {
     }
 
     public String inputWinningNumber() {
+        System.out.println(INPUT_WINNING_NUMBER);
         String winningNumber = Console.readLine();
         return winningNumber;
     }
