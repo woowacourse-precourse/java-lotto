9e2471d1483fb0f53df86d8265f0dcee19d2232e
greeng00se <thegreengoose7@gmail.com>
2022-11-12 22:20:33 +0900

    feat(LottoPrize): 맞춘개수를 입력받아 LottoPrize를 반환하는 메서드 추가

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -1,11 +1,18 @@
 package lotto.domain;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public enum LottoPrize {
     _1ST_PRIZE(6, 2_000_000_000),
     _2ND_PRIZE(5, 30_000_000),
     _3RD_PRIZE(5, 1_500_000),
     _4TH_PRIZE(4, 50_000),
-    _5TH_PRIZE(3, 5_000);
+    _5TH_PRIZE(3, 5_000),
+    _NOTHING(0, 0);
 
     private final Integer match;
     private final Integer prize;
@@ -13,5 +20,31 @@ public enum LottoPrize {
     LottoPrize(Integer match, Integer prize) {
         this.match = match;
         this.prize = prize;
+    }
+
+    public static LottoPrize getEnum(int matchedCount, boolean bonusNumberMatched) {
+        LottoPrize lottoPrize = getLottoPrize(matchedCount);
+        return checkSecondPrize(lottoPrize, bonusNumberMatched);
+    }
+
+    private static LottoPrize getLottoPrize(int matchedCount) {
+        return lowestPrizeOrder().stream()
+                .filter(prize -> prize.match == matchedCount)
+                .findAny()
+                .orElse(_NOTHING);
+    }
+
+    private static List<LottoPrize> lowestPrizeOrder() {
+        return Arrays.stream(values())
+                .filter(lottoPrize -> !lottoPrize.equals(_NOTHING))
+                .sorted(Comparator.comparingInt(o -> o.prize))
+                .collect(toList());
+    }
+
+    private static LottoPrize checkSecondPrize(LottoPrize lottoPrize, boolean bonusNumberMatched) {
+        if (lottoPrize == _3RD_PRIZE && bonusNumberMatched) {
+            return _2ND_PRIZE;
+        }
+        return lottoPrize;
     }
 }
--- src/test/java/lotto/domain/LottoPrizeTest.java
+++ src/test/java/lotto/domain/LottoPrizeTest.java
@@ -1,10 +1,44 @@
 package lotto.domain;
 
+import static lotto.domain.LottoPrize._1ST_PRIZE;
+import static lotto.domain.LottoPrize._2ND_PRIZE;
+import static lotto.domain.LottoPrize._3RD_PRIZE;
+import static lotto.domain.LottoPrize._4TH_PRIZE;
+import static lotto.domain.LottoPrize._5TH_PRIZE;
+import static lotto.domain.LottoPrize._NOTHING;
+import static lotto.domain.LottoPrize.getEnum;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayName("LottoPrize 이늄")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class LottoPrizeTest {
+
+    @ParameterizedTest(name = "{0}개를 맞추고 보너스 맞춤 여부가 {1}일 경우 {2}를 반환한다")
+    @MethodSource("matchedInformation")
+    void getEnum_메서드는_맞춘개수를_입력받아_LottoPrize를_반환한다(int matched, boolean bonusMatched, LottoPrize result) {
+        assertThat(getEnum(matched, bonusMatched)).isEqualTo(result);
+    }
+
+    static List<Arguments> matchedInformation() {
+        return Arrays.asList(
+                Arguments.of(6, true, _1ST_PRIZE),
+                Arguments.of(6, false, _1ST_PRIZE),
+                Arguments.of(5, true, _2ND_PRIZE),
+                Arguments.of(5, false, _3RD_PRIZE),
+                Arguments.of(4, true, _4TH_PRIZE),
+                Arguments.of(4, false, _4TH_PRIZE),
+                Arguments.of(3, true, _5TH_PRIZE),
+                Arguments.of(3, false, _5TH_PRIZE),
+                Arguments.of(2, false, _NOTHING)
+        );
+    }
 }
