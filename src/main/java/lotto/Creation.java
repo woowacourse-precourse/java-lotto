2f8e9464788145eb2a7ef51c6e0aa932dac64755
H4R1B0 <qkrguswns25@naver.com>
2022-11-12 15:45:41 +0900

    chore: rename class

--- src/main/java/lotto/Creation.java
+++ src/main/java/lotto/Creation.java
@@ -2,15 +2,16 @@ package lotto;
 
 import java.util.Collections;
 import java.util.List;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class Create {
-    public static List<Integer> lotto(){
+public class Creation {
+    public static List<Integer> lotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return sortLotto(numbers);
     }
-    
-    public static List<Integer> sortLotto(List<Integer> numbers){
+
+    public static List<Integer> sortLotto(List<Integer> numbers) {
         Collections.sort(numbers);
         return numbers;
     }
