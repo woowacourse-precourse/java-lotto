1a80ef73f5f2e49f2e93120524a5a77ee4ec1df3
leewoojin <zmfpdl64@naver.com>
2022-11-12 23:17:01 +0900

    Refactoring: Output 안에 있던 Enum 형을 분리했다.

--- src/main/java/lotto/input_output/Output.java
+++ src/main/java/lotto/input_output/Output.java
@@ -1,5 +1,11 @@
 package lotto.input_output;
 
+import lotto.enums.Out;
+import lotto.playlotto.object.User;
+import lotto.enums.Reward;
+
+import java.util.List;
+
 /*
 - [x] 구입 금액 메시지 출력<br>
   ex) 구입금액을 입력해 주세요.
@@ -27,24 +33,42 @@ ex) [ERROR] 로또 번호는 1부터 45 사이의 숫자여야 한다.
 
  */
 public class Output {
+    public void buy_lottery(){
 
-    private enum Print{
-        BUY_LOTTERY("구입금액을 입력해 주세요"),
-        PRINT_BUY_LOTTERY("%d개를 구매했습니다."),
-        PRINT_LOTTERY(""),  //TODO: 추후 수정
-        WIN_LOTTERY_NUMBER("당첨 번호를 입력해 주세요."),
-        BOUNNS_LOTTERY_NUMBER("보너스 번호를 입력해 주세요."),
-        PRINT_RESULT("당첨 통계%n"); //TODO: 추후 수정
-
-        private String output;
-        public String getOutput(){
-            return output;
-        }
-        private Print(String output){
-            this.output = output;
-        }
-
+        System.out.println(Out.BUY_LOTTERY.getOutput());
     }
+    public void print_buy_lottery(int buy){
+        System.out.printf(Out.PRINT_BUY_LOTTERY.getOutput()+"%n", buy);
+    }
+    public void print_lottery(User user){ //TODO: 복권 갯수 마다 출력
+        for(List<Integer> lottery : user.return_lottery_nums()){
+            System.out.println(lottery);
+        }
+    }
+    public void win_lottery_number(){
+        System.out.println(Out.WIN_LOTTERY_NUMBER.getOutput());
+    }
+    public void bonus_lottery_number(){
+        System.out.println(Out.BONUS_LOTTERY_NUMBER.getOutput());
+    }
+    public void print_result(List<Integer> result) {
+        for(Reward reward : Reward.values()){
+            System.out.printf(reward.getReward(), result.get(reward.ordinal()));
+        }
+    }
+
+    public void print_revenue(Float revenue){
+        System.out.printf(Out.YIELD.getOutput(), revenue);
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        Output out1 = new Output();
+        user.pay_Money();
+        user.create_Lottery_nums();
+        out1.print_lottery(user);
+    }
+
 
 
 }
