ee2f079796a6bee5e9c5b1d7e0b1961e7c77420d
NoseSSG <dmlcks1998@naver.com>
2022-11-12 23:24:20 +0900

    [FEAT] 당첨번호가 중복될 경우 예외처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -17,4 +19,16 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    // 당첨번호중 중복이 있을경우 예외처리
+    private void overlap(List<Integer>numbers){
+        Set<Integer> number = new HashSet<Integer>();
+        int temp;
+        for(int i =0; i < numbers.size();i++){
+            temp = numbers.get(0);
+            number.add(temp);
+        }
+        if (number.size() !=0 ){
+            throw new IllegalArgumentException();
+        }
+    }
 }
