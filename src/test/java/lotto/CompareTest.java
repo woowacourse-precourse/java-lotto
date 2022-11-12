a896737042d19341dedc7b1fb234c11435b191b8
syr9707 <yrshin9771@gmail.com>
2022-11-12 12:41:47 +0900

    test(Compare): 숫자 비교 테스트
    compareNumbers 메서드 테스트

--- src/test/java/lotto/CompareTest.java
+++ src/test/java/lotto/CompareTest.java
@@ -1,2 +1,18 @@
-package lotto;public class CompareTest {
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CompareTest {
+
+    @Test
+    void 숫자_비교_테스트() {
+        Compare compare = new Compare();
+        int sameNumber = compare.compareNumbers(Arrays.asList(1, 2, 3, 4, 5, 6), Arrays.asList(2, 3, 4, 9, 10, 13));
+
+        assertThat(sameNumber).isEqualTo(3);
+    }
 }
