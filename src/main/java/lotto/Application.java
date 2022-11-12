385640be68bc53ecb57312b044a21996ab16c65e
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-12 15:28:17 +0900

    feat: create Program play and try catch

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.service.LottoService;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try{
+            new LottoService().play();
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
