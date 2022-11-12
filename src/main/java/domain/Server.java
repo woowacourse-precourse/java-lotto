23cd7d2ab859fa293a5c9c3ad95a5735aac5e9fa
dydtmd6801 <dydtmd6801@naver.com>
2022-11-12 15:55:27 +0900

    feat(Server): 로또 구입 매수 결정 기능
    사용자가 입력한 금액에 따라 로또 구입 매수를 결정하는 기능

--- src/main/java/domain/Server.java
+++ src/main/java/domain/Server.java
@@ -8,8 +8,13 @@ public class Server {
     private static final int START_INCLUSIVE = 1;
     private static final int END_INCLUSIVE = 45;
     private static final int NUMBER_COUNT = 6;
+    private static final int LOTTO_PRICE = 1000;
 
     public List<Integer> publishLotto(){
         return Randoms.pickUniqueNumbersInRange(START_INCLUSIVE,END_INCLUSIVE,NUMBER_COUNT);
+    }
+
+    public int decideToBuyLotto(int money){
+        return money / LOTTO_PRICE;
     }
 }
