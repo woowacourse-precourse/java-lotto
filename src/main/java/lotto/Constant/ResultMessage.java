0e32fcaeb5c1483865f908283e67501740686fc4
swandevson <shai4956@gmail.com>
2022-11-12 15:12:56 +0900

    Fix(ResultMessage): 오타, 포맷팅 변경
    - 줄 바꿈
    - 3,000원 처럼 반점이 있어야하므로 돈을 문자열로 받음
    - "보너스 볼 일치" 뒤에 공백 있어야함

--- src/main/java/lotto/Constant/ResultMessage.java
+++ src/main/java/lotto/Constant/ResultMessage.java
@@ -1,9 +1,9 @@
 package lotto.Constant;
 
 public class ResultMessage {
-    public static final String LOTTO_NUM = "%d개 구매했습니다.";
+    public static final String LOTTO_NUM = "%d개를 구매했습니다." + System.lineSeparator();
     public static final String RESULT_START = "당첨 통계" + System.lineSeparator() + "---";
-    public static final String RESULT_RANK = "%d개 일치%s(%d)원 - %d개" + System.lineSeparator();
-    public static final String HIT_BONUS_NUMBER = ", 보너스 볼 일치";
+    public static final String RESULT_RANK = "%d개 일치%s(%s원) - %d개" + System.lineSeparator();
+    public static final String HIT_BONUS_NUMBER = ", 보너스 볼 일치 ";
     public static final String PROFIT = "총 수익률은 %.2f% 입니다." + System.lineSeparator();
 }
