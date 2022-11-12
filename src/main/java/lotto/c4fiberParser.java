1b07ea19baa5c5d23fcd4775b7d1b33cc5d277ba
c4fiber <qudcjf153@gmail.com>
2022-11-12 09:31:14 +0900

    feat(c4fiberParser): 문자열에서 단일 상수값을 추출하는 기능 추가

--- src/main/java/lotto/c4fiberParser.java
+++ src/main/java/lotto/c4fiberParser.java
@@ -1,6 +1,14 @@
 package lotto;
 
 public class c4fiberParser {
+    public static Integer getIntegerFromString(String input) {
+        if (!isParsable(input)) {
+            throw new IllegalArgumentException();
+        }
+
+        return Integer.parseInt(input);
+    }
+
     private static boolean isParsable(String input) {
         try {
             for (String s : input.split(",")){
