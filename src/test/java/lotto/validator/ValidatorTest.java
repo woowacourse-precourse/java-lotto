f30f347b045cf59065bd265765ed35624401e5d0
JunginPark4364 <wjddlsee@gmail.com>
2022-11-12 23:32:08 +0900

    test: 테스트 DisplayName 내용 정리

--- src/test/java/lotto/validator/ValidatorTest.java
+++ src/test/java/lotto/validator/ValidatorTest.java
@@ -16,7 +16,7 @@ class ValidatorTest {
         validator = new Validator();
     }
 
-    @DisplayName("0이상의 정수가 아니면 예외가 발생한다.")
+    @DisplayName("구매 금액이 0이상의 정수가 아니면 예외가 발생한다.")
     @Test
     public void validateMoneyLessThen0() {
         int money = -1;
@@ -25,7 +25,7 @@ class ValidatorTest {
                 .withMessage(ErrorMessage.ERROR_MONEY);
     }
 
-    @DisplayName("1000단위 숫자가 아니면 예외가 발생한다.")
+    @DisplayName("구매 금액이 1000단위 숫자가 아니면 예외가 발생한다.")
     @Test
     public void validateMoneyByNot1000Multiples() {
         int money = 1100;
@@ -34,7 +34,7 @@ class ValidatorTest {
                 .withMessage(ErrorMessage.ERROR_MONEY);
     }
 
-    @DisplayName("당첨번호 입력에 콤마 구분자가 없으면 예외가 발생한다.")
+    @DisplayName("당첨 번호에 콤마 구분자가 없으면 예외가 발생한다.")
     @Test
     public void validateWinningNumbersByCommaDelimiter() {
         String numbers = "1234";
@@ -43,7 +43,7 @@ class ValidatorTest {
                 .withMessage(ERROR_COMMA_DELIMITER);
     }
 
-    @DisplayName("당첨번호 입력에 중복된 값이 있으면 예외가 발생한다.")
+    @DisplayName("당첨 번호에 중복된 값이 있으면 예외가 발생한다.")
     @Test
     public void validateWinningNumberByDuplicatedValues() {
         String numbers = "1,2,3,3,4,5";
@@ -52,7 +52,7 @@ class ValidatorTest {
                 .withMessage(ERROR_DUPLICATE_SIZE);
     }
 
-    @DisplayName("당첨번호 입력에 LOTTO_COUNT 만큼의 개수가 없으면 예외가 발생한다.")
+    @DisplayName("당첨 번호에 LOTTO_COUNT 만큼의 개수가 없으면 예외가 발생한다.")
     @Test
     public void validateWinningNumberByNotEqualsSize() {
         String greatThanSizeNumbers = "1,2,3,4,5,6,7,8";
@@ -64,7 +64,7 @@ class ValidatorTest {
                 .withMessage(ERROR_DUPLICATE_SIZE);
     }
 
-    @DisplayName("당첨번호 입력에 1 ~ 45 사이 숫자가 아닌 값이 있으면 예외가 발생한다.")
+    @DisplayName("당첨 번호에 1 ~ 45 사이 숫자가 아닌 값이 있으면 예외가 발생한다.")
     @Test
     public void validateWinningNumberByRange() {
         String numbers = "1,2,3,4,46,7";
