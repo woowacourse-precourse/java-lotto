e4ee2fd7eaa51713ad945d4dc9ea11b00ba1cc5d
NoseSSG <dmlcks1998@naver.com>
2022-11-13 00:28:18 +0900

    [FEAT] 당첨번호를 받아 List로 변환하는 함수

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -26,6 +26,7 @@ public class Application {
         int chance;
         if(money % 1000 !=0) throw new IllegalArgumentException();
         chance = money/1000;
+        System.out.println(chance + "개를 구매했습니다.");
         return chance;
     }
     static List<Integer> make_lotto_num(){
@@ -39,9 +40,33 @@ public class Application {
         for(int i = 1 ; i <= chance ; i++){
             lotto = make_lotto_num();
             lotto_nums.add(lotto);
+            System.out.println(lotto);
         }
 
         return lotto_nums;
+    }
+
+    static List<Integer> check_winning(String[] nums){
+        List<Integer> winning_num = new ArrayList<Integer>();
+        int num;
+        for (int i = 0;i < nums.length;i++){
+            num = Integer.parseInt(nums[i]);
+            winning_num.add(num);
+        }
+        return winning_num;
+    }
+    static List<Integer> read_winning(){
+        List<Integer> winning_num;
+        String num;
+        String[] nums;
+        int temp;
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+
+        num = Console.readLine();
+        nums = num.split(",");
+
+        return winning_num;
     }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -51,5 +76,6 @@ public class Application {
         money = check_money();
         chance = money2chance(money);
         lotto_nums = buy_lotto(chance);
+        List<Integer> winning_num;
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -9,6 +9,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        overlap(numbers);
         this.numbers = numbers;
     }
 
