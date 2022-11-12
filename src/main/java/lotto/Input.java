c97fe1fd20508c9599d198462ad2eb1e8f3af5e3
yoon <beatus96@naver.com>
2022-11-12 18:36:21 +0900

    feat : 금액만큼 로또 만들기 기능 추가

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,11 +1,17 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Input {
     private final int MONEY_UNIT = 1000;
+    private final int LOTTO_NUMBER = 6;
     private final String INPUT_MESSAGE = "구입금액을 입력해 주세요.";
-
 
     public int getMoney() {
         System.out.println(INPUT_MESSAGE);
@@ -16,6 +22,16 @@ public class Input {
         }
 
         return Integer.parseInt(input);
+    }
+
+    public void createLottos(int money) {
+        int count = money/MONEY_UNIT;
+        Set<Lotto> lottoSet = new HashSet<>();
+
+        while(lottoSet.size() == count) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6));
+            lottoSet.add(lotto);
+        }
     }
 
 
