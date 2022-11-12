5591f118d1f14d39700c7f44557a9273ebbde4f2
fakerdeft <whakswp123@naver.com>
2022-11-13 02:10:04 +0900

    feat(inputCash): add 구입금액 입력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Cash;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        inputCash();
     }
+    public static void inputCash(){
+        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(Cash.countPurchaseQuantity(Console.readLine()));
+    }
+
 }
