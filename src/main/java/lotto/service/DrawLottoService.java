5b988ed869ca81db2b4264d9a1b5c06f1cd39a94
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-12 19:31:38 +0900

    feat: 일치하는 번호의 갯수를 저장하는 Map 선언

--- src/main/java/lotto/service/DrawLottoService.java
+++ src/main/java/lotto/service/DrawLottoService.java
@@ -1,11 +1,14 @@
 package lotto.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lotto.util.ConversionArrayToList;
 
 public class DrawLottoService {
-
+    Map<Integer, Integer> correctNumberCountMap = new HashMap<>();
     ConversionArrayToList conversionArrayToList = new ConversionArrayToList();
+
 
     public int countBuyingLotto(int amount) {
         return amount / 1000;
