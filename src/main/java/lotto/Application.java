d6677db321d71f067eecafa549b3c14144f73af7
Amyhds <dasomhan524@gmail.com>
2022-11-12 23:17:03 +0900

    feat(application): 당첨 확인 클래스와 수익률 계산 클래스 호출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         int ticketAmount = Purchase.buyLotto();
@@ -7,5 +9,8 @@ public class Application {
         DrawNumber.printNumber(ticketAmount);
         LuckyNumber.pickSixNumber();
         LuckyNumber.pickBonusNumber();
+        Winning.checkWinning(ticketAmount);
+        Winning.printWinningResult();
+        Profit.calculateProfit(ticketAmount*1000);
     }
 }
