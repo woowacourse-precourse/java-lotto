e04ca6832f1b07a39bb6a4fdb0ac823c40aac3f9
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-12 19:45:42 +0900

    type:feat 사용자의 입력한 정답 번호와 보너스번호를 저장하는 기능 추가

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -1,10 +1,32 @@
 package lotto.model;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class WinningNumber {
-    private final List<Integer> numbers = List.of(0,0,0,0,0,0);
+    private List<Integer> numbers = new ArrayList<>();
+    private int bonusNumber;
 
     public WinningNumber(String numbers) {
+        stringToNumbers(numbers);
+    }
+
+    private void stringToNumbers(String numbers) {
+        String[] stringNumbers = numbers.split(",");
+        for (String number: stringNumbers) {
+            this.numbers.add(Integer.valueOf(number));
+        }
+    }
+
+    public void setBonusNumber(int number) {
+        this.bonusNumber = number;
+    }
+
+    public List<Integer> getWinningNumber() {
+        return numbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
     }
 }
