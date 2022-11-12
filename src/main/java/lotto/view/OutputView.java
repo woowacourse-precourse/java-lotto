de3bf669207e4978d93806203e11ef65194186cb
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-12 20:12:02 +0900

    refactor(OutputView): 매직 리터럴 상수화

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -11,7 +11,9 @@ import lotto.domain.Rank;
 public class OutputView {
 	private static final String NUMBER_OF_TICKETS_MESSAGE = "%d개를 구매했습니다.%n";
 	private static final String WINNING_RESULT_MESSAGE = "%d개 일치 (%s원) - %d개%n";
+	private static final String SECOND_RESULT_MESSAGE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개\n";
 	private static final String PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.%n";
+	private static final String RESULT_HEADER_MESSAGE = "당첨 통계\n---";
 
 	private OutputView() {
 	}
@@ -27,7 +29,7 @@ public class OutputView {
 	}
 
 	public static void printWinningResult(Map<Rank, Integer> getRanks) {
-		System.out.println("당첨 통계\n---");
+		System.out.println(RESULT_HEADER_MESSAGE);
 
 		List<Rank> rankValue = List.of(Rank.FIFTH, Rank.FOURTH, Rank.THIRD, Rank.SECOND, Rank.FIRST);
 		for (Rank ranking : rankValue) {
@@ -45,7 +47,7 @@ public class OutputView {
 
 	private static String createWinningResultMessage(Rank ranking) {
 		if (ranking == Rank.SECOND) {
-			return "%d개 일치, 보너스 볼 일치 (%s원) - %d개\n";
+			return SECOND_RESULT_MESSAGE;
 		}
 		return WINNING_RESULT_MESSAGE;
 	}
