1dbcd33f994a4aad1b5a0c3277d8007c20e6c658
Jimin <thswlals0219@gmail.com>
2022-11-12 23:15:02 +0900

    feat(LottoMachine): 수량 만큼 로또 티켓을 발행하는 기능 구현

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -1,11 +1,23 @@
 package lotto.model;
 
-import lotto.Lotto;
+import java.util.ArrayList;
+import java.util.List;
 import lotto.util.constants.LottoConstants;
 
 public class LottoMachine {
     public int computeLottoTicketsCount(int purchaseAmount) {
         return purchaseAmount / LottoConstants.LOTTO_TICKET_PRICE;
+    }
+
+    public List<Lotto> publishLottoTickets(int countLottoTickets) {
+        List<Lotto> lottoTickets = new ArrayList<>();
+
+        for (int ticketCount = 0; ticketCount < countLottoTickets; ticketCount++) {
+            Lotto lottoTicket = getLottoTicket();
+            lottoTickets.add(lottoTicket);
+        }
+
+        return lottoTickets;
     }
 
     public Lotto getLottoTicket() {
