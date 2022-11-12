b4b223959a1d0c1993647c84c9d31e6e8af5a4e9
heesang <dlagmltkd12@naver.com>
2022-11-12 17:23:23 +0900

    feat: 당첨 번호를 입력하는 기능

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,9 +2,27 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
+
+    public static final String CRITERION_SPLIT = ",";
 
     public static int inputPurchasePrice() {
         return Integer.parseInt(Console.readLine());
+    }
+
+    public static List<Integer> inputAnswerNumbers() {
+        String answerNumbers = Console.readLine();
+        String[] splitAnswerNumbers = answerNumbers.split(CRITERION_SPLIT);
+        return mapToInteger(splitAnswerNumbers);
+    }
+
+    private static List<Integer> mapToInteger(String[] split) {
+        return Arrays.stream(split)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
