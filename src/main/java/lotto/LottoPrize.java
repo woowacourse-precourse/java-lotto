8bc9373ff7158c98a17446f13974c71307bebd5b
jiwon <ljwon77@daum.net>
2022-11-13 00:53:28 +0900

    feat: check user numbers contain bonus number and fix return int type
    생성자에서 당첨 번호, 보너스 번호에 대한 정보를 전달하여 필드에 저장해두었습니다.
    또한, 당첨 번호가 5개 일치할 경우 보너스 번호가 존재하는지에 대한 체크 기능을 추가하였습니다.
    존재한다면 추후 구분을 위해서 일치하는 개수에 10을 곱해주어 큰 수를 리턴하도록 하였습니다.
    기존에 stream의 count 사용 시 long 타입을 리턴하는 문제를 해결하기 위해 wrapper class로 변경 후 intValue()를 통해 Int로 반환하게 만들었습니다.

--- src/main/java/lotto/LottoPrize.java
+++ src/main/java/lotto/LottoPrize.java
@@ -1,15 +1,44 @@
 package lotto;
 
+import lotto.util.MessageUtil;
+
 import java.util.List;
 import java.util.function.Predicate;
 
 public class LottoPrize {
 
-    public long getMatchCount(List<Integer> winningNums, List<Integer> userNums) {
-        return winningNums.stream()
+    private final MessageUtil messageUtil = new MessageUtil();
+
+    private final LottoWinningNumber winningNumInfo;
+
+    private final Integer BONUS_AVAILABLE_NUM = 5;
+
+    public LottoPrize(LottoWinningNumber lottoWinningNumber) {
+        this.winningNumInfo = lottoWinningNumber;
+    }
+    public void getLottoStats(List<Lotto> userLottos) {
+        messageUtil.printWinningStats();
+
+        for(Lotto userLotto : userLottos) {
+            List<Integer> userLottoNums = userLotto.getLottoNumbers();
+            int matchCount = getMatchCount(userLottoNums);
+            matchCount  = checkBonusNumber(userLottoNums, matchCount);
+        }
+    }
+
+    private int getMatchCount(List<Integer> userNums) {
+        return Long.valueOf(winningNumInfo.getWinningNums()
+                .stream()
                 .filter(lottoNum -> userNums.stream()
-                        .noneMatch(Predicate.isEqual(lottoNum)))
-                .count();
+                        .anyMatch(Predicate.isEqual(lottoNum)))
+                .count()).intValue();
+    }
+
+    private int checkBonusNumber(List<Integer> userNums, int matchCount) {
+        if(matchCount == BONUS_AVAILABLE_NUM && userNums.contains(matchCount)) {
+            return matchCount * 10;
+        }
+        return matchCount;
     }
 
     public String computeEarningRate(int purchaseAmount, int winningAmount) {
