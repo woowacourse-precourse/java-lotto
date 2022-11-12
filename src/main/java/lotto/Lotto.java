7c68bb6ac774e6452b146f30698a7646d2de2a4d
49ehyeon42 <49ehyeon42@gmail.com>
2022-11-12 14:31:01 +0900

    fix: 테스트를 통과하기 위해 만든 메서드 추가
    ApplicationTest 중 기능_테스트에서 List.of() 데이터로 리스트를 받는다. 수정 전 소스코드는 로또 클래스에 리스트를 받고 정렬하기 때문에 에러가 발생한다. 에러를 해결하기 위해 받은 리스트를 수정 가능한 새로운 리스트로 바꾸고 정렬한 데이터를 반환하는 메서드를 만듬

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,6 @@
 package lotto;
 
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -9,8 +9,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        numbers.sort(Comparator.comparingInt(o -> o));
-        this.numbers = numbers;
+        this.numbers = getSortNumbers(numbers);
     }
 
     private void validate(List<Integer> numbers) {
@@ -20,6 +19,13 @@ public class Lotto {
         if (numbers.size() != numbers.stream().distinct().count()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호 중 중복이 있습니다.");
         }
+    }
+
+    // List.of() 리스트를 정렬하기 위해 리스트를 새로 만들고 정렬한 리스트를 반환하는 메스드
+    private List<Integer> getSortNumbers(List<Integer> numbers) {
+        List<Integer> list = new ArrayList<>(numbers);
+        list.sort(Integer::compareTo);
+        return list;
     }
 
     public List<Integer> getLottoNumbers() {
