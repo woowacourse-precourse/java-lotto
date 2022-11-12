605854d22c017a4107da2799166157f872cf4ba4
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-12 21:08:50 +0900

    feat : 로또 당첨 내역의 값들을 갖는 LottoRank 클래스

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+public enum LottoRank {
+    NONE_MATCHE(0, "", 0),
+    THREE_MATCHES(3, "3개 일치 (5,000원) - ", 5_000),
+    FOUR_MATCHES(4, "4개 일치 (50,000원) - ", 50_000),
+    FIVE_MATCHES(5, "5개 일치 (1,500,000원) - ", 1_500_000),
+    FIVE_BONUS_MATCHES(5, "5개 일치, 보너스 볼 일치 (30,000,000원) - ", 30_000_000),
+    ALL_MATCHES(6, "6개 일치 (2,000,000,000원) - ", 2_000_000_000);
+
+    public final int ranking;
+    private final String message;
+    private final int money;
+
+    LottoRank(int ranking, String message, int money) {
+        this.ranking = ranking;
+        this.message = message;
+        this.money = money;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public static LottoRank getRankResult(int winningCount, boolean bonus) {
+        if (FIVE_BONUS_MATCHES.ranking == winningCount && bonus) {
+            return FIVE_BONUS_MATCHES;
+        }
+        for (LottoRank rank : values()) {
+            if (rank.ranking == winningCount && rank != FIVE_BONUS_MATCHES) {
+                return rank;
+            }
+        }
+        return NONE_MATCHE;
+    }
+}
