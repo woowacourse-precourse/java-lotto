c507431a945f349aaf386b329661333ab3cec75e
eunji53 <duqtj00@naver.com>
2022-11-13 12:48:12 +0900

    <feat>:메인 함수에 로또 구매 후 출력 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.ConsumerResult;
+import lotto.domain.LottoResult;
+import lotto.domain.PurchaseLotto;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PurchaseLotto purchaseLotto = new PurchaseLotto();
+        LottoResult lottoResult = new LottoResult();
+        ConsumerResult consumerResult = new ConsumerResult();
+        List<List<Integer>> totalLotto = purchaseLotto.myLotto(purchaseLotto.inputMoney());
+        purchaseLotto.printMyLotto(totalLotto);
+
     }
 }
--- src/main/java/lotto/domain/PurchaseLotto.java
+++ src/main/java/lotto/domain/PurchaseLotto.java
@@ -3,36 +3,63 @@ package lotto.domain;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import org.mockito.internal.util.collections.ListUtil;
 
 public class PurchaseLotto {
+    //1
     public int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         String moneyInput = readLine();
         int money = Integer.valueOf(moneyInput);
-        countLotto(money);
-        return money;
+        return countLotto(money);
+
     }
 
-    private static int countLotto(int money) {
+    public static int countLotto(int money) {
         if (money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 금액을 1000원 단위로 입력해 주세요.");
         }
         return money / 1000;
     }
 
-    public List<Integer> myLottoNumber() {
+    private List<Integer> myLottoNumber() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         numbers.sort(Comparator.naturalOrder());
         return numbers;
     }
-    public void myLotto(){
-        int money = inputMoney();
-        for (int count=0; count < countLotto(money); count++){
-            System.out.println(myLottoNumber());
+    //2
+    public List<List<Integer>> myLotto(int count){
+        List<List<Integer>> totalLotto = new ArrayList<>();
+        System.out.printf("%d개를 구매했습니다.\n",count);
+        for (int index=0;index<count;index++){
+            totalLotto.add(myLottoNumber());
+        }
+    return totalLotto;
+    }
+    public void printMyLotto(List<List<Integer>> totalLotto){
+        for (int index=0;index<totalLotto.size();index++){
+            System.out.println(totalLotto.get(index));
         }
     }
-
+    public List<Integer> myLottoResult(List<Integer> lottoNumber, int bonusNumber) {
+        int money = inputMoney();
+        ConsumerResult consumerResult =new ConsumerResult();
+        List<Integer> totalCount=new ArrayList<>();
+        List<Integer> bonusCount=new ArrayList<>();
+        List<Integer> jionCount=new ArrayList<>();
+        for (int count=0; count < countLotto(money); count++){
+            List<Integer> numbers=myLottoNumber();
+            System.out.println(numbers);
+            totalCount.add(consumerResult.compareLotto(numbers,lottoNumber));
+            bonusCount.add(consumerResult.compareBonus(numbers, bonusNumber));
+        }
+        jionCount.addAll(totalCount);
+        jionCount.addAll(bonusCount);
+        return jionCount;
+    }
 }
