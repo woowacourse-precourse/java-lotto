0254da9504f939a35e15dd5a02b0f51f6ae7e13f
yejin <kwonyj1022@naver.com>
2022-11-13 11:30:37 +0900

    fix: LottoGenerator 클래스 수정 - 로또 번호 list 복사 후 정렬해서 numbers에 저장

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -2,15 +2,20 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
-    List<Integer> numbers;
+    List<Integer> numbers = new ArrayList<>();
 
     LottoGenerator() {
-        numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> tmp = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        for (int i = 0; i < tmp.size(); i++) {
+            numbers.add(tmp.get(i));
+        }
         Collections.sort(numbers);
+
     }
 
 }
