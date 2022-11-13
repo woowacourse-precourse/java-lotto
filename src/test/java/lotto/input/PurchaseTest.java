c0c760f7007dbfdf29c1f9a0f40b80ff24b2040c
Ridealist <cjsn92@gmail.com>
2022-11-13 19:31:04 +0900

    test(Purchase): 빈 값 입력시 예외 발생 테스트 추가

--- src/test/java/lotto/input/PurchaseTest.java
+++ src/test/java/lotto/input/PurchaseTest.java
@@ -7,14 +7,20 @@ import org.junit.jupiter.api.Test;
 class PurchaseTest {
 
     @Test
-    void validatedAmount_notNumberInput_exceptionThrown() {
+    void validate_blankInput_exceptionThrown() {
+        assertThatThrownBy(() -> new Purchase(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 구입금액을 입력해주세요.");
+    }
+    @Test
+    void validate_notNumberInput_exceptionThrown() {
         assertThatThrownBy(() -> new Purchase("100a"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 구입금액은 숫자여야 합니다.");
     }
 
     @Test
-    void validatedAmount_notMultipleOfThousandInput_exceptionThrown() {
+    void validate_notMultipleOfThousandInput_exceptionThrown() {
         assertThatThrownBy(() -> new Purchase("1500"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 1,000원 단위로 구입해야 합니다.");
