561b3c1b7ae31256edd277d4542c972a5f7e2cb0
sonsoowon <yuwoo1018@gmail.com>
2022-11-14 01:16:57 +0900

    feat(InputView): 당첨 번호 입력
    LottoValidator의 메서드를 이용하므로 InputValidator에 당첨 번호 유효성 검증에 대한 메서드를 추가하지 않는다

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -1,12 +1,19 @@
 package lotto.ui;
 
 import camp.nextstep.edu.missionutils.Console;
+
 import lotto.valid.InputValidator;
+import lotto.valid.LottoValidator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public enum InputView {
     INSTANCE;
     private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
-
+    private static final String INPUT_JACKPOT_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String JACKPOT_NUMBER_SEPARATOR = ",";
     public static int inputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
         String money = Console.readLine();
@@ -14,5 +21,20 @@ public enum InputView {
         InputValidator.validateMoney(money);
 
         return Integer.parseInt(money);
+    }
+    public static List<Integer> inputJackpotNumbers() {
+        System.out.println(INPUT_JACKPOT_NUMBERS_MESSAGE);
+        String jackpotNumberInput = Console.readLine();
+
+        InputValidator.validateJackpotNumberFormat(jackpotNumberInput);
+
+        List<Integer> jackpotNumbers =
+                Arrays.stream(jackpotNumberInput.split(JACKPOT_NUMBER_SEPARATOR))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+
+        LottoValidator.validate(jackpotNumbers);
+
+        return jackpotNumbers;
     }
 }
