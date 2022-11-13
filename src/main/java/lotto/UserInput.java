275f8918aacd4edcc1635f9b1cb614673c50a680
UngHeung <t01047377477@gmail.com>
2022-11-13 22:40:29 +0900

    refactor : (Lotto) 메시지 출력 추가

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -13,17 +13,23 @@ public class UserInput {
 	
 	UserInput() {
 		money = new Money(money());
-		lotto = new Lotto(lottoNumbers());
+		lotto = new Lotto(userNumbers());
 		bonus = new Bonus(bonusNumber());
 	}
 	
 	
 	private int money() {
+		String message = "구입금액을 입력해 주세요.";
+		System.out.println(message);
+		
 		int money = Integer.parseInt(Console.readLine());
 		return money;
 	}
 	
-	private List<Integer> lottoNumbers() {
+	private List<Integer> userNumbers() {
+		String message = "당첨 번호를 입력해 주세요";
+		System.out.println(message);
+		
 		StringTokenizer token = new StringTokenizer(Console.readLine(), ",");
 		
 		List<Integer> numbers = new ArrayList<>();
@@ -36,8 +42,10 @@ public class UserInput {
 	}
 	
 	private int bonusNumber() {
+		String message = "보너스 번호를 입력해 주세요";
+		System.out.println(message);
+		
 		int bonus = Integer.parseInt(Console.readLine());
 		return bonus;
 	}
-	
 }
