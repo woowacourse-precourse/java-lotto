d6a601f56e1b5a465bfe1cf6e018dbfee599e048
hodomaroo <enghemdwkd@gmail.com>
2022-11-13 16:56:04 +0900

    [Feat] : LottoGameMachine 클래스 구현 -> 전체 구현 완료
    로또 게임 전체를 실행하고, 예외를 처리하는 LottoGameMachine클래스의
    구현을 완료하였습니다.
    
    프로그램 전체 구현을 완료, ApplicationTest를 통한 검증을 완료하였습니다

--- src/main/java/lotto/LottoGameMachine.java
+++ src/main/java/lotto/LottoGameMachine.java
@@ -1,8 +1,34 @@
 package lotto;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * 로또 게임을 진행하는 클래스
  */
 public class LottoGameMachine {
+
+    private LottoPocket lottoPocket;
+
+    public void run() {
+        try {
+            int bill = IoManager.getBillFromUser();
+
+            List<Lotto> lottos = Collections.unmodifiableList(LottoPublisher.publishLottos(bill));
+            IoManager.printLottoPublishInfo(lottos);
+
+            LottoPocket pocket = new LottoPocket(lottos);
+
+            List<Integer> winningNumber = IoManager.getWinningNumbersFromUser();
+            int bonusNumber = IoManager.getBonusNumberFromUser();
+            WinningLotto winningLotto = new WinningLotto(winningNumber, bonusNumber);
+
+            ScratchResult scratchResult = pocket.getScratchResult(winningLotto);
+            IoManager.printScratchResult(scratchResult);
+
+        } catch (IllegalArgumentException e) {
+            IoManager.printException(e);
+        }
+    }
 
 }
