c8b018ee22b0e797072d101e8f8a5a025391da85
kimminsu <xjfcnfw3@naver.com>
2022-11-13 13:28:09 +0900

    feat: 유저 입력 전 메시지 출력 기능 구현

--- src/main/java/lotto/user/User.java
+++ src/main/java/lotto/user/User.java
@@ -2,22 +2,24 @@ package lotto.user;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import lotto.GameMessage;
 import lotto.domain.validation.LottoNumbersValidation;
 
 public class User {
     public static String buyLotto() {
-        String userAmount = Console.readLine();
-        return userAmount;
+        System.out.println(GameMessage.BUY);
+        return Console.readLine();
     }
 
     public static List<Integer> inputLottoNumbers() {
+        System.out.println(GameMessage.INPUT_NUMBERS);
         String userNumbers = Console.readLine();
         LottoNumbersValidation.validateInputLottoNumbers(userNumbers);
         return UserUtil.convertUserInputToNumbers(userNumbers);
     }
 
     public static String inputBonusNumber() {
-        String bonusNumber = Console.readLine();
-        return bonusNumber;
+        System.out.println(GameMessage.INPUT_BONUS);
+        return Console.readLine();
     }
 }
