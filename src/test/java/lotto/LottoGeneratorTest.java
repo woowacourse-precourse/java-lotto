31f4dc87c279cf131b34edf7ed9094f7829810d4
Hyunseok Lee <hyun104467@naver.com>
2022-11-14 00:50:01 +0900

    feat(lotto): checkMoneyUnitTest

---
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import lotto.domain.LottoGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoGeneratorTest {
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void inputLottoGeneratorNotCorrectUnit() {
+        assertThatThrownBy(() -> new LottoGenerator(11111))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    // 아래에 추가 테스트 작성 가능
+}
