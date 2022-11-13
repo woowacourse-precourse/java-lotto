20ed947aaeb120f70f9f966333b45ad656c27632
UngHeung <t01047377477@gmail.com>
2022-11-13 22:41:58 +0900

    refactor : (Lotto) 로또 번호 개수 만큼 출력 기능 및 메시지 추가

--- src/main/java/lotto/RandomOutput.java
+++ src/main/java/lotto/RandomOutput.java
@@ -8,6 +8,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RandomOutput {
 	private final int count;
 	private final List<List<Integer>> lottoNumbers;
+	private static StringBuilder builder;
 	
 	RandomOutput(int count) {
 		this.count = count;
@@ -16,9 +17,18 @@ public class RandomOutput {
 	
 	private List<List<Integer>> randomNumberslist(int count) {
 		List<List<Integer>> numbersList = new ArrayList<>();
+		List<Integer> randomNumbers;
+		
+		builder.append(count + "개를 구매했습니다.\n");
 		
 		for(int i = 0; i < count; i++) {
-			numbersList.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+			randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+			numbersList.add(randomNumbers);
+			builder.append(randomNumbers);
+			
+			if(i != count - 1) {
+				builder.append("\n");
+			}
 		}
 		
 		return numbersList;
@@ -32,5 +42,7 @@ public class RandomOutput {
 		return lottoNumbers;
 	}
 	
-	
+	public String toString() {
+		return builder.toString();
+	}
 }
