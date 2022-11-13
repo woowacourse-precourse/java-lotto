f5321d20fa61654f0771104b2c54046aca7834c4
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-13 16:09:34 +0900

    fix(Random): 클래스명이랑 라이브러리 클래스명이랑 겹쳐 오류 발생할 수 있음

--- src/main/java/lotto/Utils/RandomUtil.java
+++ src/main/java/lotto/Utils/RandomUtil.java
@@ -2,10 +2,9 @@ package lotto.Utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class Random {
+public class RandomUtil {
 
     public static List<Integer> makeRandomNumbers(){
         List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
--- src/test/java/lotto/Utils/RandomUtilTest.java
+++ src/test/java/lotto/Utils/RandomUtilTest.java
@@ -3,12 +3,11 @@ package lotto.Utils;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-class RandomTest {
+class RandomUtilTest {
 
     @Test
     void 랜덤_리스트_길이_확인() {
-        assertThat(Random.makeRandomNumbers().size()).isEqualTo(6);
+        assertThat(RandomUtil.makeRandomNumbers().size()).isEqualTo(6);
     }
 }
