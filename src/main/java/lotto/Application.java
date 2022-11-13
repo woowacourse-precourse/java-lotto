45bb1af3a5d562ad9d2dcb935d5bf748c5d73ec8
조미정 <ing06164@naver.com>
2022-11-13 21:13:25 +0900

    feat: isValidPurchase()-구매 금액이 유효한 형태인지 확인

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> numbers;
+        System.out.println("로또 프로그램입니다. \n금액을 입력해 주세요");
+        String purchase = Console.readLine();           // 구매 금액
+
+        if(!isValidPurchase(purchase)){
+            System.out.println("잘못된 입력입니다! 수량에 맞는 금액을 입력해 주세요!");
+            purchase = Console.readLine();
+        }
+    }
+    static boolean isValidPurchase(String purchase) {
+        if (Integer.parseInt(purchase) % 1000 != 0) {
+            return false;
+        }return true;
     }
 }
