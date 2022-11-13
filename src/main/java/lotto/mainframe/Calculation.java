25190857d52848bb1986a376c96c8afeece38cdd
Namyunsuk <yunsuk0328@naver.com>
2022-11-14 00:25:00 +0900

    feat(Calculation): 일치 수 계산 기능 구현

--- src/main/java/lotto/mainframe/Calculation.java
+++ src/main/java/lotto/mainframe/Calculation.java
@@ -1,5 +1,51 @@
 package lotto.mainframe;
 
+import java.util.List;
+
 public class Calculation {
 
+    private final int ZERO_COUNT = 0;
+    private int threeMatchCount;
+    private int fourMatchCount;
+    private int fiveMatchCount;
+    private int fivePlusBonusMatchCount;
+    private int sixMatchCount;
+
+    public void setCCountZero() {
+        threeMatchCount = ZERO_COUNT;
+        fourMatchCount = ZERO_COUNT;
+        fiveMatchCount = ZERO_COUNT;
+        fivePlusBonusMatchCount = ZERO_COUNT;
+        sixMatchCount = ZERO_COUNT;
+    }
+
+    public void calculateResult(List<List<Integer>> lotteries, int bonusNumber,
+            List<Integer> answer) {
+        setCCountZero();
+        for (List<Integer> lotto : lotteries) {
+            int count = (int) lotto.stream()
+                    .filter(number -> answer.contains(number))
+                    .count();
+            boolean haveBonusNumber = lotto.contains(bonusNumber);
+            setCount(count, haveBonusNumber);
+            if (count == 6) {
+                sixMatchCount++;
+            }
+        }
+    }
+
+    public void setCount(int count, boolean haveBonusNumber) {
+        if (count == 3) {
+            threeMatchCount++;
+        }
+        if (count == 4) {
+            fourMatchCount++;
+        }
+        if (count == 5) {
+            if (haveBonusNumber) {
+                fivePlusBonusMatchCount++;
+            }
+            fiveMatchCount++;
+        }
+    }
 }
