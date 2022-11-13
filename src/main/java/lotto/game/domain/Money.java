c44f9d2e931da464060a78758a9c71fb11647956
seokjin8678 <rnrn8678@naver.com>
2022-11-13 22:26:44 +0900

    style: 구현 순서 정리

--- src/main/java/lotto/game/domain/Money.java
+++ src/main/java/lotto/game/domain/Money.java
@@ -15,6 +15,13 @@ public class Money {
         this.value = value;
     }
 
+    private void validate(long value) {
+        if (value % 1000 == 0) {
+            return;
+        }
+        throw new IllegalArgumentException(DIVISIBLE_1000_ONLY_MESSAGE);
+    }
+
     public static Money of(long value) {
         return new Money(value);
     }
@@ -24,13 +31,6 @@ public class Money {
             throw new IllegalArgumentException(NUMBER_ONLY_MESSAGE);
         }
         return Money.of(Long.parseLong(input));
-    }
-
-    private void validate(long value) {
-        if (value % 1000 == 0) {
-            return;
-        }
-        throw new IllegalArgumentException(DIVISIBLE_1000_ONLY_MESSAGE);
     }
 
     public long getValue() {
