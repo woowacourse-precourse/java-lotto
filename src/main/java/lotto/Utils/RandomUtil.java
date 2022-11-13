db783cc445b504eb67e10052a398e8638e7a2cef
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-13 16:23:12 +0900

    fix(RandomUtil): 정렬되도록 변경

--- src/main/java/lotto/Utils/RandomUtil.java
+++ src/main/java/lotto/Utils/RandomUtil.java
@@ -3,11 +3,13 @@ package lotto.Utils;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RandomUtil {
 
     public static List<Integer> makeRandomNumbers(){
-        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        return randomNumbers;
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6).stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
