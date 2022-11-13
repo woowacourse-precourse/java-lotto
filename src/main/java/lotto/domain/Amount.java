e2e78d5aa4cde6a62f789babf49b58aab60170bc
saint6839 <saint6839@gmail.com>
2022-11-14 00:35:23 +0900

    feat(domain) : 구매 금액에 해당하는만큼 로또 수량을 반환하는 메서드 구현
    - 예외 문자열 상수 변수 처리
    - getter 추가

--- src/main/java/lotto/domain/Amount.java
+++ src/main/java/lotto/domain/Amount.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
 public class Amount {
+    private static final int LOTTO_PRICE = 1000;
+    private static final String ERROR_CANT_DIVIDE_INTO_1000_MESSAGE = "[ERROR] 금액은 1000원 단위로만 입력 가능합니다.";
     int value;
 
     public Amount(int value) {
@@ -9,9 +11,18 @@ public class Amount {
     }
 
     private void validateCanDivideIntoOneThousand(int amount) {
-        if (amount % 1000 == 0) {
+        if (amount % LOTTO_PRICE == 0) {
             return;
         }
-        throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위로만 입력 가능합니다.");
+        System.out.println(ERROR_CANT_DIVIDE_INTO_1000_MESSAGE);
+        throw new IllegalArgumentException(ERROR_CANT_DIVIDE_INTO_1000_MESSAGE);
+    }
+
+    public int calculateLottoCount() {
+        return value / LOTTO_PRICE;
+    }
+
+    public int getValue() {
+        return value;
     }
 }
