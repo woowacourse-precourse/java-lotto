19e44a89b7dcb1af17a17afa8f427eca3bcee6bb
cookienc <kim76744@naver.com>
2022-11-13 15:31:03 +0900

    feat(model): 생성된 번호 중 중복된 숫자 검증 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -2,12 +2,15 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
     private static final int LOTTO_START = 1;
     private static final int LOTTO_END = 45;
     private static final int LOTTO_SIZE = 6;
+    public static final String ERROR_GENERATED_DIFFERENT_LOTTO_SIZE = "[ERROR] 생성된 로또 번호의 개수가 다릅니다.";
+    public static final String ERROR_GENERATED_DUPLICATED_LOTTO_NUMBERS = "[ERROR] 중복된 로또 번호가 생성 되었습니다.";
 
     private final List<Integer> numbers;
 
@@ -17,9 +20,29 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validateSize(numbers);
+        validateDuplicateNumbers(numbers);
+    }
+
+    private static void validateSize(List<Integer> numbers) {
+        if (isDifferentSize(numbers)) {
+            throw new IllegalArgumentException(ERROR_GENERATED_DIFFERENT_LOTTO_SIZE);
         }
+    }
+
+    private static boolean isDifferentSize(List<Integer> numbers) {
+        return numbers.size() != LOTTO_SIZE;
+    }
+
+    private static void validateDuplicateNumbers(List<Integer> numbers) {
+        HashSet<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (hasDuplicateNumbers(uniqueNumbers)) {
+            throw new IllegalArgumentException(ERROR_GENERATED_DUPLICATED_LOTTO_NUMBERS);
+        }
+    }
+
+    private static boolean hasDuplicateNumbers(HashSet<Integer> uniqueNumbers) {
+        return uniqueNumbers.size() != LOTTO_SIZE;
     }
 
     public static Lotto create() {
