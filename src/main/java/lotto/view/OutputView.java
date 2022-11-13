af5e9d74d1488bd52bdfbf9188561acc8e669193
Choi-JY <Choi-JY1107@github.com>
2022-11-13 20:58:59 +0900

    fix: 구입 금액이 아닌 발행한 로또 수량 출력하도록 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,6 +6,7 @@ import lotto.dto.LottoStatsDTO;
 import lotto.dto.LottoTicketDTO;
 
 public class OutputView {
+    private static final Integer LOTTO_PRICE = 1000;
     private static final String INPUT_PAID_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String INFO_LOTTO_COUNT = "\n%d개를 구매했습니다.\n";
     private static final String INPUT_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.";
@@ -18,7 +19,7 @@ public class OutputView {
     }
 
     public static void printPaidLotto(Integer lottoCount, LottoTicketDTO lottoTicketDTO) {
-        System.out.printf(INFO_LOTTO_COUNT, lottoCount);
+        System.out.printf(INFO_LOTTO_COUNT, lottoCount / LOTTO_PRICE);
         printLottoNumbers(lottoTicketDTO);
     }
 
