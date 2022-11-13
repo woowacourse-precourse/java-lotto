5f8c98225d560210acb32317807e2246411fcadd
JungHun Jang <ijj5258@naver.com>
2022-11-13 22:27:29 +0900

    feat : 입력받은 금액이 숫자인지 확인하는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,13 +3,19 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-	
+
 	private static final String BUY_SENTENCE = "구입금액을 입력해 주세요.";
-	
+
 	public static int inputMoney() {
 		System.out.println(BUY_SENTENCE);
-        String money = Console.readLine();
-        
-        return Integer.parseInt(money);
-    }
+		String money = Console.readLine();
+		validateInteger(money);
+		return Integer.parseInt(money);
+	}
+
+	private static void validateInteger(String string) {
+		if (!string.chars().allMatch(Character::isDigit)) {
+			throw new IllegalArgumentException("정수만 입력 가능합니다.");
+		}
+	}
 }
