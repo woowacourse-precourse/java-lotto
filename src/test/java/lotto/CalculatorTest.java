94a0eb6e9f65acdaecd5b822c76ed5a64e9573fb
kevstevie <jianna442@gmail.com>
2022-11-13 21:00:02 +0900

    feat: Calculator 정답 계산 함수 케이스

---
+++ src/test/java/lotto/CalculatorTest.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+
+    @Test
+    void 정답_계산() {
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculator calc = new Calculator(1000L);
+        Calculator.calculateLottos(4, true);
+        Calculator.calculateLottos(5,true);
+        Calculator.calculateLottos(5,false);
+        calc.printResult();
+        assertThat(out.toString()).contains("3개 일치 (5,000원) - 0개")
+                .contains("4개 일치 (50,000원) - 1개")
+                .contains("5개 일치 (1,500,000원) - 1개")
+                .contains("5개 일치, 보너스 볼 일치 (30,000,000원) - 1개");
+    }
+}
