3cda701f04410000ca35a6a5e13c84bb547f0996
ywj9811 <ywj9811@naver.com>
2022-11-14 08:26:40 +0900

    test: BuyLottoTest 생성
    BuyLotto에 대한 테스트 작성

---
+++ src/test/java/lotto/service/BuyLottoTest.java
@@ -0,0 +1,42 @@
+package lotto.service;
+
+import lotto.exception.BonusNumberException;
+import lotto.exception.BuyMoneyException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class BuyLottoTest {
+    private BuyLotto buyLotto;
+
+    @BeforeEach
+    public void setUp() {
+        buyLotto = new BuyLotto();
+    }
+
+    @Test
+    @DisplayName("숫자가_아닌_경우")
+    public void notNumber() {
+        assertThatThrownBy(() -> new BuyMoneyException().notNumber("12a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("구매_금액_단위")
+    public void buyMoney() {
+        assertThatThrownBy(() -> new BuyMoneyException().notDivideThousands("1200"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또_구매_개수")
+    public void buyLottos() {
+        int quantity = 5;
+        assertThat(buyLotto.getMyLottoNumbers(quantity).size())
+                .isEqualTo(5);
+    }
+}
