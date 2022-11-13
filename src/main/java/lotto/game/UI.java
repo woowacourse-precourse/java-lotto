0079182fbc3fae45e512461052c7a3e877d1b8cb
seokjin8678 <rnrn8678@naver.com>
2022-11-13 13:17:21 +0900

    fix: 출력문 오타 수정

--- src/main/java/lotto/game/UI.java
+++ src/main/java/lotto/game/UI.java
@@ -30,12 +30,12 @@ public class UI {
         System.out.println("---");
         System.out.printf("3개 일치 (5,000원) - %d개%n", totalWinnings.getOrDefault(LottoGrade.FIFTH, 0));
         System.out.printf("4개 일치 (50,000원) - %d개%n", totalWinnings.getOrDefault(LottoGrade.FOURTH, 0));
-        System.out.printf("5개 일치 (5,000원) - %d개%n", totalWinnings.getOrDefault(LottoGrade.THIRD, 0));
+        System.out.printf("5개 일치 (1,500,000원) - %d개%n", totalWinnings.getOrDefault(LottoGrade.THIRD, 0));
         System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개%n", totalWinnings.getOrDefault(LottoGrade.SECOND, 0));
         System.out.printf("6개 일치 (2,000,000,000원) - %d개%n", totalWinnings.getOrDefault(LottoGrade.FIRST, 0));
     }
 
     public static void printProfitRate(double profitRate) {
-        System.out.printf("총 수익률은 %.1f%% 입니다.", profitRate);
+        System.out.printf("총 수익률은 %.1f%%입니다.", profitRate);
     }
 }
