6c75239f8b3d1b5760aae1e64745ee5015469c06
Woojin Lee <dhkep03@gmail.com>
2022-11-13 18:11:13 +0900

    feat(UserInterface): 입력한 구매 금액 검증 로직 추가

--- src/main/java/lotto/ui/UserInterface.java
+++ src/main/java/lotto/ui/UserInterface.java
@@ -1,6 +1,7 @@
 package lotto.ui;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Validator;
 
 public class UserInterface {
     private static final String INPUT_MONEY_ANNOUNCEMENT = "구입금액을 입력해 주세요.";
@@ -8,6 +9,7 @@ public class UserInterface {
     public static String getInputMoney() {
         System.out.println(INPUT_MONEY_ANNOUNCEMENT);
         String inputMoneyRaw = Console.readLine();
+        Validator.checkInputMoney(inputMoneyRaw);
         return inputMoneyRaw;
     }
 }
