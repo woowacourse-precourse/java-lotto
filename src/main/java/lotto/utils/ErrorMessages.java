d96dbea3a0af8e6156f7c820a0217af8f24b2fec
twoosky <lhjae3@naver.com>
2022-11-14 03:37:59 +0900

    feat(view): 구입금액 관련 에러 메시지 추가

--- src/main/java/lotto/utils/ErrorMessages.java
+++ src/main/java/lotto/utils/ErrorMessages.java
@@ -6,9 +6,12 @@ public class ErrorMessages {
     public static final String LOTTO_NUMBERS_OUT_OF_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 
     public static final String WINNING_NUMBERS_SEPARATOR = "[ERROR] 당첨 번호는 쉼표(,)를 기준으로 구분해야 합니다.";
+
     public static final String BONUS_NUMBER_OUT_OF_RANGE = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
     public static final String BONUS_NUMBER_DUPLICATED = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
     public static final String BONUS_NUMBER_NOT_INTEGER = "[ERROR] 보너스 번호는 숫자여야 합니다.";
 
     public static final String MONEY_NOT_INTEGER = "[ERROR] 구입 금액은 숫자여야 합니다.";
+    public static final String MONEY_OUT_OF_RANGE = "[ERROR] 1000원 이상 구매해야 합니다.";
+    public static final String MONEY_NOT_DIVIDE_UNIT = "[ERROR] 구입 금액은 1000원 단위여야 합니다.";
 }
