fdcd1b6daa780a4633775dc58043174f61855bc5
leesky <leehaneul0623@gmail.com>
2022-11-13 15:13:20 +0900

    feat(lotto) lotto 모델 유효성 검사 구현
    직접 생성자로 만들 경우 검증하기 위해서 유효성 검사 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,9 +15,10 @@ public class Lotto {
 
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        if (numbers.size() != 6) throw new IllegalArgumentException("로또 번호 6개를 입력해주세요.");
+
+        Set<Integer> set = new HashSet<>(numbers);
+        if (set.size() != 6) throw new IllegalArgumentException("로또의 각 번호는 중복이 없어야 합니다.");
     }
 
     public List<Integer> getNumbers() {
