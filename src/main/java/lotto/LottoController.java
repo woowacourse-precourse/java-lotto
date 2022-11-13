80b2e733336fbddcccf34dcf58c51676e0293e30
Ilchul Kim <huj0159@gmail.com>
2022-11-13 13:46:55 +0900

    feat: check parameter is 0 and throw IllegalArgumentException

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -11,16 +11,22 @@ public class LottoController {
         validate(userMoney);
     }
 
-    private void isDigit(String userInput){
+    private void isDigit(String userInput) {
         String pattern = "^[0-9]*$";
         if (!Pattern.matches(pattern, userInput)) {
-            throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주셔야 합니다");
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다");
         }
     }
 
     private void validate(String userInput) {
-        if (Integer.parseInt(userInput) % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 1,000 단위의 금액을 입력해야합니다");
+        int money = Integer.parseInt(userInput);
+
+        if (money == 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000 이상의 금액을 입력해야 합니다");
+        }
+
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000 단위의 금액을 입력해야 합니다");
         }
     }
 }
