6c98db65403ab4f3ed79f480046977ff9c448f39
CAEL0 <kjongmin26@gmail.com>
2022-11-13 17:14:19 +0900

    feat(LottoConstants): 로또 번호의 숫자 개수 정의

--- src/main/java/lotto/constant/LottoConstants.java
+++ src/main/java/lotto/constant/LottoConstants.java
@@ -4,6 +4,7 @@ public class LottoConstants {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
 
+    public static final int LOTTO_NUMBER_LENGTH = 6;
     public static final int WINNING_NUMBER_LENGTH = 6;
     public static final int BONUS_NUMBER_LENGTH = 1;
 
