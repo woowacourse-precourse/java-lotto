f3c3bbb3570009c28e10fef933c3e3d9f1c16e0c
jiwon1217 <koagjw@gmail.com>
2022-11-13 22:46:03 +0900

    feat: 보너스 번호가 1~45까지의 범위에 포함되도록 예외처리

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -8,11 +8,16 @@ public class WinningLotto {
     private static final String INVALID_INPUT_PATTERN = "[ERROR] 입력 형식과 맞지 않습니다.";
     private static final Pattern INPUT_PATTERN = Pattern.compile("^[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+$");
     private static final String BONUS_NUMBER_IS_DUPLICATED_WITH_WINNING_NUMBER = "[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.";
+    private static final String INVALID_NUMBER_RANGE = "[ERROR] 로또 번호의 범위는 1 ~ 45까지 입니다.";
     private final Lotto winningLotto;
+    private final int bonusNumber;
 
-    public WinningLotto(String winningLotto) {
+    public WinningLotto(String winningLotto, int bonusNumber) {
         isValidInputPattern(winningLotto);
         this.winningLotto = convert(winningLotto);
+        isDuplicatedWithWinningNumbers(bonusNumber);
+        isValidNumberRange(bonusNumber);
+        this.bonusNumber = bonusNumber;
     }
 
     public Lotto getWinningLotto() {
@@ -34,6 +39,12 @@ public class WinningLotto {
     private void isDuplicatedWithWinningNumbers(int bonusNumber) {
         if (this.winningLotto.isContain(bonusNumber)) {
             throw new IllegalArgumentException(BONUS_NUMBER_IS_DUPLICATED_WITH_WINNING_NUMBER);
+        }
+    }
+
+    private void isValidNumberRange(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException(INVALID_NUMBER_RANGE);
         }
     }
 }
