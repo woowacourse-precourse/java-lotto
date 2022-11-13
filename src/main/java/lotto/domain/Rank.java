8b3a4f259fdd246b48728eea84dcad1e701842c8
taekwon-dev <taekwon_dev@naver.com>
2022-11-13 18:47:11 +0900

    feat: (Rank) 3개 미만 일치하는 경우를 표기하기 위한 상수 선언
    - UNRANK("3개 미만 일치 (0원))

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -5,7 +5,8 @@ public enum Rank {
     SECOND_PLACE("5개 일치, 보너스 볼 일치 (30,000,000원)"),
     THIRD_PLACE("5개 일치 (1,500,000원)"),
     FOURTH_PLACE("4개 일치 (50,000원)"),
-    FIFTH_PLACE("3개 일치 (5,000원)");
+    FIFTH_PLACE("3개 일치 (5,000원)"),
+    UNRANK("3개 미만 일치 (0원)");
 
     private final String reward;
 
