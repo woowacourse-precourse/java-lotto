28a3633f3cf039843fbf4179018b586a62d357be
BaeDongHyeon <sasd730@gmail.com>
2022-11-13 23:05:46 +0900

    test: 로또 번호 테스트 케이스 추가
    - 정상적인 입력 값은 정렬한다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,8 +3,10 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -28,6 +30,13 @@ class LottoTest {
     void createLottoByInRange() {
         assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 46)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("올바른 번호는 정렬한다.")
+    @Test
+    void createLottoBySortedNumber() {
+        Lotto lotto = new Lotto(List.of(6, 4, 5, 3, 1, 2));
+        assertThat(lotto.toString()).isEqualTo(Arrays.asList(1, 2, 3, 4, 5, 6).toString());
     }
 
     // 아래에 추가 테스트 작성 가능
