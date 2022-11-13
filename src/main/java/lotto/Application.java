07a30255ac1c903a36c75493eed4bd92732d8e79
yoonjoonseok <yjsygm@naver.com>
2022-11-13 15:58:45 +0900

    로또 번호를 구입금액만큼 생성한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,15 +1,32 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
     	String money = getMoneyFromUser();
+    	List<Lotto> lottoNumbers = createLottoNumbers(money);
         // TODO: 프로그램 구현
     }
     
     public static String getMoneyFromUser() {
     	String result = Console.readLine();
     	return result;
+    }
+    
+    public static List<Lotto> createLottoNumbers(String money) {
+    	int count = Integer.parseInt(money)/1000;
+    	List<Lotto> lottoNumbers = new ArrayList<>();
+    	
+    	for(int i=0; i<count;i++) {
+    		Lotto lottoNumber = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        	lottoNumbers.add(lottoNumber);
+    	}
+
+    	return lottoNumbers;
     }
 }
