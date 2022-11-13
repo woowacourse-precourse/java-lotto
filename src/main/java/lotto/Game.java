1c6e72c267c401e5d281a77b6b4bf9862412ed1d
nn98 <jkllhgb@gmail.com>
2022-11-14 00:24:33 +0900

    Feat: 가독성 향상을 위한 지역 변수 선언/입력 번호 중복 확인/당첨 결과 확인/당첨 결과 출력/수익률 출력

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,21 +1,21 @@
 package lotto;
 
-import constance.Texts;
+import static constance.Texts.*;
+import static constance.Values.*;
 
 import java.util.List;
-
-import static constance.Texts.LOTTO_USER_PURCHASE_RESULT;
 
 public class Game {
 
     private final Checker checker = new Checker();
     private User user;
     private WinningLotto winningLotto;
-    public void run(){
+
+    public void run() {
         this.play();
     }
 
-    public void play(){
+    public void play() {
 
         String amount;
         String numbers;
@@ -23,34 +23,75 @@ public class Game {
         int checkedAmount;
         List<Integer> checkedNumbers;
         int checkedBonusNumber;
+        List<Lotto> lotteries;
+        List<Result> results;
+        int winnings;
+        int printBonus;
+        int winningBonus;
+        List<List<Result>> userResults;
+        int lotteryCount;
+        float lotteryStat;
 
-        System.out.println(Texts.LOTTO_USER_INPUT_PURCHASE_AMOUNT);
+        System.out.println(LOTTO_USER_INPUT_PURCHASE_AMOUNT);
         amount = camp.nextstep.edu.missionutils.Console.readLine();
 
-        checkedAmount=checker.checkAmountInput(amount);
+        checkedAmount = checker.checkAmountInput(amount);
 
-        user=new User(checkedAmount);
+        user = new User(checkedAmount);
 
         System.out.println();
-        System.out.println(String.format(LOTTO_USER_PURCHASE_RESULT,user.getLotteryCount(),user.getLotteryList()));
+        System.out.println(String.format(LOTTO_USER_PURCHASE_RESULT, user.getLotteryCount(), user.getLotteryList()));
 
-        System.out.println(Texts.LOTTO_USER_INPUT_WINNING_NUMBERS);
+        System.out.println(LOTTO_USER_INPUT_WINNING_NUMBERS);
         numbers = camp.nextstep.edu.missionutils.Console.readLine();
 
         checkedNumbers = checker.checkNumbersInput(numbers);
 
         System.out.println();
-        System.out.println(Texts.LOTTO_USER_INPUT_BONUS_NUMBERS);
+        System.out.println(LOTTO_USER_INPUT_BONUS_NUMBERS);
         bonusNumber = camp.nextstep.edu.missionutils.Console.readLine();
 
-        checkedBonusNumber=checker.checkNumber(bonusNumber);
+        checkedBonusNumber = checker.checkNumber(bonusNumber);
+        checker.checkDuplicated(checkedNumbers, checkedBonusNumber);
 
-        winningLotto=new WinningLotto(checkedNumbers,checkedBonusNumber);
+        winningLotto = new WinningLotto(checkedNumbers, checkedBonusNumber);
 
-        System.out.println("!--Test: "+winningLotto);
+        System.out.println("!--Test: " + winningLotto);
+
+        lotteries = user.getLotteries();
+        results = checker.checkResults(lotteries, winningLotto);
+        winnings = checker.checkWinnings(results);
+
+        user.setResults(results);
+        user.setWinnings(winnings);
+
+        printBonus = 1;
+        winningBonus = 2;
+        userResults = user.getResults();
+        for(List<Result> results1:userResults) System.out.println("!--Test: "+results1);
+
+        lotteryCount = user.getLotteryCount();
+        lotteryStat = checker.checkLotteryStat(winnings, lotteryCount);
+        user.setLotteryStat(lotteryStat);
 
         System.out.println();
-        System.out.println(Texts.LOTTO_GAME_WINNING_STATS);
+        System.out.println(LOTTO_GAME_WINNING_STATS);
+        System.out.println(LOTTO_GAME_DIVISION);
+        for (int i = MIN_COINCIDE; i < WINNINGS.length; ++i) {
+            String coincideMessage;
+            if (i == 6 && printBonus > 0) {
+                i -= 1;
+                coincideMessage = String.format(LOTTO_GAME_COINCIDE_RESULT, i, LOTTO_GAME_COINCIDE_BONUS,
+                        DECIMAL_FORMAT.format(WINNINGS[i + winningBonus]), userResults.get(i).size());
+                printBonus = 0;
+                winningBonus = 0;
+            } else {
+                coincideMessage = String.format(LOTTO_GAME_COINCIDE_RESULT, i, "",
+                        DECIMAL_FORMAT.format(WINNINGS[i]), userResults.get(i).size());
+            }
+            System.out.println(coincideMessage);
+        }
+        System.out.println(String.format(LOTTO_GAME_LOTTERY_RETURN, lotteryStat));
 
     }
 }
