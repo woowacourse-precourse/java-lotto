4230976852ae2c2f724a9270ae3f8d6e3de7c0b6
DaWoon Lee <qkgk61@gmail.com>
2022-11-13 16:39:58 +0900

    Feat : Lotto 번호를 오름차순으로 정렬하여 출력하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
-package lotto;
+package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -17,4 +18,9 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    @Override
+    public String toString() {
+        Collections.sort(numbers);
+        return numbers.toString();
+    }
 }
