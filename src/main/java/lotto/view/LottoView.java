e31cff25ad1ef9bbe242c1ef1f557d03820de803
Qulip <ilkwon.ryu@gmail.com>
2022-11-14 00:33:53 +0900

    feat: 구입할 금액 입력받기 기능 추가

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -2,8 +2,13 @@ package lotto.view;
 
 import static lotto.LottoConstant.GAME_START_MESSAGE;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class LottoView {
     public void printStartMessage() {
         System.out.println(GAME_START_MESSAGE);
+    }
+    public String getLottoPurchaseAmount(){
+        return Console.readLine();
     }
 }
