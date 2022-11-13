4c9e0d78be3ca9e273e1c341ace6979c667ecad3
1eejisoo <jisu991012@gmail.com>
2022-11-14 03:12:53 +0900

    feat: 로또 구매 금액 유효성 검증 - 입력 값이 숫자인가

--- src/main/java/controller/MoneyController.java
+++ src/main/java/controller/MoneyController.java
@@ -3,14 +3,23 @@ package controller;
 import domain.Money;
 import service.MoneyService;
 
+import java.util.regex.Pattern;
+
 public class MoneyController {
     MoneyService moneyService = new MoneyService();
+    private final static String FORMAT = "^[0-9]*$";
 
     public Money getPurchaseAmount(String purchaseAmount) {
+        if (!isNumber(purchaseAmount))
+            throw new IllegalArgumentException("구매 금액은 숫자여야 합니다.");
         return moneyService.saveMoney(purchaseAmount);
     }
 
     public int getAvailablePurchaseNumber(Money money) {
         return moneyService.getAvailablePurchaseNumber(money);
+    }
+
+    public boolean isNumber(String purchaseAmount) {
+        return Pattern.matches(FORMAT, purchaseAmount);
     }
 }
