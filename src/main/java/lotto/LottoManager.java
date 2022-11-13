142dcaf930f1abb895af9e2189cf01f96715ede2
JungWoo <rlawjddn103@naver.com>
2022-11-13 21:50:31 +0900

    feat(LottoManager): 당첨 정보를 저장할 Rank Map 초기화

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -3,18 +3,27 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoManager {
 
     private List<Lotto> lotties;
-    private int buyingMoney;
+    private final int buyingMoney;
+    private Map<Rank,Integer> ranks;
 
     public LottoManager(int buyingMoney) {
         this.buyingMoney = buyingMoney;
         lotties = new ArrayList<>();
         for (int i = 0; i < buyingMoney / 1000; i++) {
             lotties.add(generateRandomLotto());
+        }
+
+        ranks = new HashMap<>();
+        Rank[] allRanks = Rank.values();
+        for (Rank rank: allRanks) {
+            ranks.put(rank,0);
         }
     }
 
