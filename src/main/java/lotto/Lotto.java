b4132cfc875dd491e1b0b1709ea09b7e81d2c878
Ilchul Kim <huj0159@gmail.com>
2022-11-14 04:09:45 +0900

    feat: compare lotto with win number

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,8 +17,35 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public int compareWithAnswer(List<Integer> winNumbers, int bonusNumber) {
+        int correctCount = 0;
+        for (int winNumber : winNumbers) {
+            if (numbers.contains(winNumber)) {
+                correctCount++;
+            }
+        }
+
+        //이걸 대체 어떻게 줄이지?
+        if (correctCount < 3) {
+            return 0;
+        }
+        if (correctCount == 3) {
+            return 5;
+        }
+        if (correctCount == 4) {
+            return 4;
+        }
+        if (correctCount == 5) {
+            if (numbers.contains(bonusNumber)) {
+                return 2;
+            }
+            return 3;
+        }
+        return 1;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         return numbers.toString();
     }
 }
