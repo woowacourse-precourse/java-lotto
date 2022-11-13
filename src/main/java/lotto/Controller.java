ae759c13a0065745c1c6aa08a6a19d201827f4b5
osy9757 <osy9757@gmail.com>
2022-11-13 18:44:41 +0900

    chore : 파일 위치 변경

---
+++ src/main/java/lotto/Controller.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import lotto.print.PrintInputMessage;
+import lotto.print.PrintOutputMessage;
+
+public class Controller {
+
+    public void start() {
+
+    }
+}
--- src/main/java/lotto/print/PrintOutputMessage.java
+++ src/main/java/lotto/print/PrintOutputMessage.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.print;
 
 import java.text.DecimalFormat;
 
