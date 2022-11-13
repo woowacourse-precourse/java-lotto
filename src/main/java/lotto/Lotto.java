78412edf39ad808a7e8e22dcda067f30633d22d6
dohyun <ldh19738@gmail.com>
2022-11-13 17:28:29 +0900

    feat(Lotto): 로또 번호 중복 예외 처리하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,23 +1,33 @@
 package lotto;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplication(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개만 가능합니다.");
+        }
+    }
+    // TODO: 추가 기능 구현
+    private void validateDuplication(List<Integer> numbers) {
+        Set<Integer> nonDulicationNumbers = new HashSet<>(numbers);
+
+        if (nonDulicationNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
     public List<Integer> setLotto() {
         return numbers;
     }
