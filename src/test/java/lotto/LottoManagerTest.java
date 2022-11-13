4e67a85f02399118f52b86c81fa5376c6bbec612
JungWoo <rlawjddn103@naver.com>
2022-11-13 23:48:39 +0900

    test(LottoManagerTest): 여섯개의 숫자 전부 맞췄을 때 테스트 추가

---
+++ src/test/java/lotto/LottoManagerTest.java
@@ -0,0 +1,44 @@
+package lotto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoManagerTest {
+
+    private static LottoManager lottoManager;
+
+    @BeforeEach
+    void init(){
+        lottoManager = new LottoManager(1000);
+    }
+
+    @DisplayName("전부 일치하면 Rank 가 WIN_SIX 이어야 한다.")
+    @Test
+    void calculateLottoRanksBySIX() {
+        List<Lotto> lottos = lottoManager.getLottos();
+        Lotto lotto = lottos.get(0);
+        lottoManager.calculateTotalRanks(lotto,1);
+
+        assertThat(lottoManager.getRanks().get(Rank.WIN_SIX)).isEqualTo(1);
+    }
+
+    @DisplayName("하나만 다르고 bonus 번호가 불일치하면, Rank 가 WIN_FIVE 이어야 한다.")
+    @Test
+    void calculateEarningRateBySIX() {
+        List<Lotto> lottos = lottoManager.getLottos();
+        Lotto lotto = lottos.get(0);
+        lottoManager.calculateTotalRanks(lotto,0);
+        System.out.println("lottoManager = " + lottoManager.getRanks());
+        float earningRate = lottoManager.calculateEarningRate();
+
+        assertThat(earningRate).isEqualTo(2000000F);
+    }
+
+}
