99bd9ead4d4daef6386fb75a654ec7db72b0d752
lacred <lacred@isang-us-MacBook-Pro.local>
2022-11-14 00:41:42 +0900

    <feat> : 로또 금액 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+       int lottoCount = lottoPurchase(20000);
+
+    }
+
+    public static int lottoPurchase(int money){
+
+        if(money%1000 != 0){
+            new IllegalArgumentException("[ERROR] 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+
+
+        return money/1000;
     }
 }
