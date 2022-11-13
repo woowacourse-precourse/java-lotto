e6e9b57d804fcc4fe126553c55b6d07271d796b6
60jong <rudwhd515@gmail.com>
2022-11-13 20:04:06 +0900

    feat(LottoFactory) : '구매한 로또 수 만큼 로또 번호를 생성할 수 있다.' 기능 구현

--- src/main/java/lotto/LottoFactory.java
+++ src/main/java/lotto/LottoFactory.java
@@ -11,6 +11,18 @@ public class LottoFactory {
     private final int LOTTO_END_NUMBER = 45;
     private final int LOTTO_NUMBER_COUNT = 6;
 
+    public List<Lotto> createLottos(int quantity) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        while (lottos.size() < quantity) {
+            List<Integer> lottoNumbers = createLottoNumbers();
+            Lotto lotto = new Lotto(lottoNumbers);
+            lottos.add(lotto);
+        }
+
+        return lottos;
+    }
+
     public List<Integer> createLottoNumbers() {
         return Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, LOTTO_NUMBER_COUNT);
     }
