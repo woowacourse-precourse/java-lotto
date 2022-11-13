904644e3c73b9da8e9758fc00935c3dfd8ec26db
twoosky <lhjae3@naver.com>
2022-11-14 00:11:51 +0900

    feat(domain): 당첨 번호에 대한 로또 인스턴스 생성

--- src/main/java/lotto/domain/LottoWin.java
+++ src/main/java/lotto/domain/LottoWin.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static lotto.utils.ErrorMessages.WINNING_NUMBERS_SEPARATOR;
 
@@ -10,11 +12,23 @@ public class LottoWin {
 
     public LottoWin(String numbers) {
         validateSeparator(numbers);
+        this.winningNumbers = generateLotto(numbers);
     }
 
     private void validateSeparator(String numbers) {
         if(!numbers.contains(SEPARATOR)) {
             throw new IllegalArgumentException(WINNING_NUMBERS_SEPARATOR);
         }
+    }
+
+    private Lotto generateLotto(String numbers) {
+        List<Integer> lottoNumbers = numbersToList(numbers);
+        return new Lotto(lottoNumbers);
+    }
+
+    private List<Integer> numbersToList(String numbers) {
+        return Arrays.stream(numbers.split(SEPARATOR))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
