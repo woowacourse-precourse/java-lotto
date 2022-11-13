b671d0ba748f987782e742959e954bd6303b12bf
kokodak <kokodakadokok@gmail.com>
2022-11-13 23:29:35 +0900

    feat(lotto): numbers 검증 기능 강화

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,12 +13,19 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (!isSizeSix(numbers) || isDuplicate(numbers)) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean isSizeSix(List<Integer> numbers) {
+        return numbers.size() == 6;
+    }
+
+    private boolean isDuplicate(List<Integer> numbers) {
+        Set<Integer> duplicateChecker = new HashSet<>(numbers);
+        return duplicateChecker.size() != 6;
+    }
 
     public List<Integer> getNumbers() {
         return numbers;
