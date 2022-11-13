bec09ccff88473e80130eef52b4eb4fcc258f7b8
jamangstangs <jamangsong@gmail.com>
2022-11-14 00:37:06 +0900

    refactor(ResultLotto): calculateStatics() 계산에 Enum을 활용하여 계산

--- src/main/java/lotto/RaffleStaticsCalculator.java
+++ src/main/java/lotto/RaffleStaticsCalculator.java
@@ -5,10 +5,16 @@ import java.util.List;
 public class RaffleStaticsCalculator {
 
     public double calculateStatics(List<Integer> lottoResult, int lottosSize) {
-        // TODO: lottoResult 리스트 리팩토링 필요, ENUM으로 리팩터링 필요
-        List<Integer> winningMoney = List.of(0, 2000000000, 30000000, 1500000, 50000, 5000);
         int totalWinningMoney = 0;
-        int totalLottoBuyMoney = lottosSize* 1000;
+        int totalLottoBuyMoney = lottosSize * 1000;
+
+        List<Integer> winningMoney = List.of(
+                0,
+                Rank.FIRST.getRewardInt(),
+                Rank.SECOND.getRewardInt(),
+                Rank.THIRD.getRewardInt(),
+                Rank.FOURTH.getRewardInt(),
+                Rank.FIFTH.getRewardInt());
 
         for (int i = 1; i <= 5; i++) {
             totalWinningMoney += winningMoney.get(i) * lottoResult.get(i);
