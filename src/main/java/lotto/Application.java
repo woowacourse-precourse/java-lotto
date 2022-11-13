64978391becc0a32f1468868cad88a745c448b60
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-11-13 20:10:28 +0900

    feat: 로또 구매 수량 반환 함수 추가
    금액을 입력받아 1000으로 나누어 떨어지지 않다면
    IllegalArgumentException 발생
    1000으로 나누어 떨어진 값 반환

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static Integer getLottoBuyCount() {
+        Integer money;
+
+        System.out.println("구입금액을 입력해 주세요.");
+        try {
+            money = Integer.parseInt(Console.readLine());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+        }
+
+        if (money % 1000 > 0)
+            throw new IllegalArgumentException("1000으로 나누어지는 값을 입력해야 합니다.");
+
+        return money / 1000;
     }
 }
