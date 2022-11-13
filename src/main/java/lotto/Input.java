4ff429a24a2efd4f40b787032e091a64e3cdad52
lacram <chemin9898@gmail.com>
2022-11-13 13:54:37 +0900

    refactor: 예시에 맞게 출력하도록 줄바꿈 문자 추가

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -5,18 +5,18 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
 
     // 로또 구입 금액을 입력받는 기능
-    public static String enterLottoPurchaseAmout() {
+    public static String enterLottoPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
     // 당첨 번호를 입력 받는 기능
     public static String enterLottoWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println("\n당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
     // 보너스 번호를 입력 받는 기능
     public static String enterLottoBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println("\n보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
 }
