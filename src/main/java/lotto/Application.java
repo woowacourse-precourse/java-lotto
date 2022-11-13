1dbfc08e5a6963769d21df705b3b5f7eddb38639
조미정 <ing06164@naver.com>
2022-11-13 23:48:22 +0900

    refactor: generateLotto()-로또 생성하고 오름차순 정렬

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         List<List<Integer>> numbers = new ArrayList<>();
+
         System.out.println("로또 프로그램입니다. \n금액을 입력해 주세요");
         String purchase = Console.readLine();           // 구매 금액
 
@@ -24,15 +25,8 @@ public class Application {
         int amount = Integer.parseInt(purchase) / 1000;
         System.out.println(amount+"개를 구매했습니다.");
 
-        for(int i=0; i<amount; i++){
-            // 로또 번호 생성
-            numbers.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-        }
-
-        for(int i=0; i<numbers.size(); i++){
-            Collections.sort(numbers.get(i));     // 오름차순 정렬
-            System.out.println(numbers.get(i));
-        }
+        // 로또 생성
+        generateLotto(numbers, amount);
 
         // 당첨 번호 입력(콤마로 구분)
         System.out.println("당첨 번호를 입력해 주세요.");
@@ -51,7 +45,6 @@ public class Application {
         // 보너스 번호
         System.out.println("보너스 번호를 입력해 주세요.");
         String bonusNumber = Console.readLine();
-
     }
 
     // 구매 금액이 유효한지 확인하고 리턴
@@ -59,5 +52,17 @@ public class Application {
         if (Integer.parseInt(purchase) % 1000 != 0) {
             return false;
         }return true;
+    }
+
+    static void generateLotto(List<List<Integer>> numbers, int amount){
+        
+        // 로또 번호 생성
+        for(int i=0; i<amount; i++){
+            numbers.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        }
+
+        for(int i=0; i<numbers.size(); i++){
+            Collections.sort(numbers.get(i));     // 오름차순 정렬
+        }
     }
 }
