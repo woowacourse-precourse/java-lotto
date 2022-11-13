93e821ca82c20a030988fdd52bbcc15865696d82
1eejisoo <jisu991012@gmail.com>
2022-11-14 04:23:08 +0900

    refactor: 메서드에서 에러 출력 기능을 분리

--- src/main/java/service/BonusService.java
+++ src/main/java/service/BonusService.java
@@ -5,15 +5,13 @@ import domain.Lotto;
 
 public class BonusService {
     public Bonus createBonusNumber(int bonusNumber, Lotto lotto) {
-        if (!isNotLottoNumbers(bonusNumber, lotto))
-            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
+        isNotLottoNumbers(bonusNumber, lotto);
         return new Bonus(bonusNumber);
     }
 
-    public boolean isNotLottoNumbers(int bonusNumber, Lotto lotto) {
+    public void isNotLottoNumbers(int bonusNumber, Lotto lotto) {
         if (lotto.isContain(bonusNumber)) {
             throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
         }
-        return true;
     }
 }
