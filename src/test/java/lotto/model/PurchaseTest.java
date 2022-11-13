ea1cffe6cf52d794a2e9e247913122c16eaad910
김현재 <41482946+galug@users.noreply.github.com>
2022-11-13 18:05:32 +0900

    fix(purchasetest): 로또 구매 기능 검증에 대한 테스트 수정
    구매 기능의 자료형 수정에 따른 테스트 변경

--- src/test/java/lotto/model/PurchaseTest.java
+++ src/test/java/lotto/model/PurchaseTest.java
@@ -3,6 +3,8 @@ package lotto.model;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -11,52 +13,24 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PurchaseTest {
 
-    private static final Purchase purchase= new Purchase();
+    private static final Purchase purchase = new Purchase();
 
 
     @DisplayName("사용자가 지불한 금액을 로또의 개수로 바꿔준다.")
     @Test
     void changeToLotto() {
-        String moneyInput = "14000";
+        int moneyInput = 1000;
 
         int numberOfLotto = purchase.changeToLotto(moneyInput);
 
-        Assertions.assertThat(numberOfLotto).isEqualTo(14);
+        Assertions.assertThat(numberOfLotto).isEqualTo(1);
     }
 
-    @DisplayName("사용자가 지불한 금액에 대한 입력이 숫자가 아닌경우 예외가 발생한다.")
-    @Test
-    void changeToLottoByNotNumberMoney() {
-        String moneyInput = "1a000";
-
-        assertThatThrownBy(() -> purchase.changeToLotto(moneyInput))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("사용자가 지불한 금액이 0원인 경우 예외가 발생한다.")
-    @Test
-    void changeToLottoByZeroWon() {
-        String moneyInput = "000";
-
-        assertThatThrownBy(() -> purchase.changeToLotto(moneyInput))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("사용자가 지불한 금액이 1000원 보다 작을 경우 예외가 발생한다.")
-    @Test
-    void changeToLottoByLessThanThousandWon() {
-        String moneyInput = "100";
-
-        assertThatThrownBy(() -> purchase.changeToLotto(moneyInput))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("사용자가 지불한 금액이 1000원단위로 끊기지 않을 경우 예외가 발생한다.")
-    @Test
-    void changeToLottoByNotMultipleOfThousand() {
-        String moneyInput = "120001";
-
-        assertThatThrownBy(() -> purchase.changeToLotto(moneyInput))
+    @DisplayName("사용자가 지불한 금액이 입력 요구 사항에 맞지 않을 경우 예외를 발생시킨다.")
+    @ParameterizedTest
+    @CsvSource({"000","100","12001"})
+    void changeToLottoByNotNumberMoney(int money) {
+        assertThatThrownBy(() -> purchase.changeToLotto(money))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
