5ec54a847b2fefd635365fb65b946161a9ea804f
Hong-yeonggwang <chamsk0808@naver.com>
2022-11-13 20:37:54 +0900

    refactor: buyLotto(수익률 계산하는 부분 함수 분리) feat: calProfitRateRun(수익률 계산하는 함수) feat: printLottoNum(구매한 로또의 번호를 출력)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,7 +6,13 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
-//사용자가 로또는 몇개를 구매했는지 확인하는 메서드
+    public static int inputpurchase(){
+        System.out.println("구입금액을 입력해 주세요.");
+        String userInput = Console.readLine();
+        int returnData = Integer.valueOf(userInput);
+        return returnData;
+    }
+    //사용자가 로또는 몇개를 구매했는지 확인하는 메서드
     public static int countBuyGame(int buyMoney){
         int returnData = buyMoney / 1000;
         int remainder = buyMoney % 1000;
@@ -17,20 +23,38 @@ public class Application {
     }
     public static Lotto[] buyLotto (int buyCnt){
         Lotto[] returnData = new Lotto[buyCnt];
-
         for(int i = 0; i < buyCnt; i++){
             List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             returnData[i] = new Lotto(lottoNumber);
         }
-
         return returnData;
+    }
+    public static void calProfitRateRun(Lotto[] lottos){
+        LottoWin lottoWin = LottoWin.getInstance();
+        for(Lotto lotto : lottos) {
+            int[] sameCnt = lotto.checkWin();
+            int[] expenseWithProfit = lotto.expenseWithProfit(sameCnt);
+            lottoWin.addExpenseWithprofit(expenseWithProfit);
+        }
+    }
+    public static void printLottoNum(Lotto[] lottos){
+        System.out.println(lottos.length+"개를 구매했습니다.");
+        for(Lotto lotto : lottos){
+            List<Integer> lottoNums = lotto.getNumbers();
+            System.out.println(lottoNums);
+        }
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Lotto[] a = buyLotto(13);
-        for(int i = 0; i < a.length; i++){
-            System.out.println(a[i].getNumbers());
-        }
+
+        int purchaseAmont = inputpurchase();
+        int buyGameCnt = countBuyGame(purchaseAmont);
+        Lotto[] buyGames = buyLotto(buyGameCnt);
+        printLottoNum(buyGames);
+        calProfitRateRun(buyGames);
+        LottoWin lottoWin = LottoWin.getInstance();
+        lottoWin.profitRateCal();
+
     }
 }
