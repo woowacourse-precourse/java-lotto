66f13284a90cd20009e813f908c16595188848c2
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2022-11-13 17:56:14 +0900

    test : (WinningStatisticsTest) countWinningLottos function test

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -1,7 +1,5 @@
 package lotto;
 
-import java.util.List;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +7,10 @@ import lotto.domain.WinningLotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 public class WinningLottoTest {
-    @DisplayName("당첨 로또를 생성합니다.")
+    @DisplayName("당첨 로또 번호와 일치하는 발행 로또 번호의 갯수를 카운트한다.")
     @Test
     void createLottoByDuplicatedNumber() {
         WinningLotto winningLotto = new WinningLotto(List.of(1,2,3,4,5,6), 7);
