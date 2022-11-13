36f95e5337ded6c973893c083c910adf74365a64
twoosky <lhjae3@naver.com>
2022-11-14 04:10:34 +0900

    refactor(domain): 로또 번호 예외 처리 테스트에 MethodSource 적용

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,8 +3,14 @@ package lotto;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static lotto.utils.ErrorMessages.LOTTO_NUMBERS_OUT_OF_RANGE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -26,10 +32,19 @@ class LottoTest {
     }
 
     @DisplayName("로또 번호가 1~45 사이의 값이 아니면 예외가 발생한다.")
-    @Test
-    void createLottoByOutOfRange() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 87, 6)))
+    @ParameterizedTest
+    @MethodSource("generateData")
+    void createLottoByOutOfRange(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(LOTTO_NUMBERS_OUT_OF_RANGE);
+    }
+
+    private static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 87, 6)),
+                Arguments.of(List.of(0, 1, 2, 3, 4, 5)),
+                Arguments.of(List.of(-1, 1, 2, 3, 4, 5))
+        );
     }
 }
