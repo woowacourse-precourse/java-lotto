ed64834834dd88c5918cdfe2fa9eaba8a3083a30
jagajang <kosb7544@gmail.com>
2022-11-13 19:59:59 +0900

    dev: add lotto price IO method

--- src/main/java/lotto/IOManager.java
+++ src/main/java/lotto/IOManager.java
@@ -1,4 +1,21 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.config.LottoConfig;
+
 public class IOManager {
+    public static int buyLotto() throws IllegalArgumentException {
+        int price;
+
+        try {
+            price = Integer.parseInt(Console.readLine());
+        } catch(Exception e) {
+            throw new IllegalArgumentException();
+        }
+
+        if(price % LottoConfig.LOTTO_PRICE != 0)
+            throw new IllegalArgumentException();
+
+        return price / LottoConfig.LOTTO_PRICE;
+    }
 }
