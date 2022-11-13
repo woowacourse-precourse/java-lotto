ac0ffd7fc07f78e5ebc87827c311755d9624baa1
sohyundoh <lyny123@naver.com>
2022-11-13 14:30:54 +0900

    feat($generateLotto) : Problem2. 로또 번호 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
@@ -28,5 +32,10 @@ public class Lotto {
         checkMoney(Money);
         int numBuy = Integer.valueOf(Money)/1000;
         return numBuy;
+    }
+    public static List<Integer> generateLotto(){
+        List<Integer> Lotto = Randoms.pickUniqueNumbersInRange(1,45,6);
+        Collections.sort(Lotto);
+        return Lotto;
     }
 }
