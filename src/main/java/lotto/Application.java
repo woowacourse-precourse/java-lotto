bc2c61a01634816ceef10237ba7e23b3b8886cc3
DongWooE <ehddn977@naver.com>
2022-11-14 06:25:17 +0900

    [feat] 로또 금액에 따라서 배열을 만드는 메소드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,19 @@
 package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     int insertedNumber;
+    List<List<Integer>> randomLottos;
+
     public static void main(String[] args) {
         Application application = new Application();
         application.printInsertPrice();
+        application.insertPriceAndSaveNumber();
+        application.makeArray();
     }
 
     private void printInsertPrice(){
@@ -17,5 +24,11 @@ public class Application {
         String s = Console.readLine();
         insertedNumber = Integer.parseInt(s);
         if(insertedNumber > 0 || insertedNumber%1000 != 0) Exception.of(Exception.LOTTO_PRICE_01);
+    }
+
+    private void makeArray(){
+        int dividedNumber = insertedNumber / 1000;
+        for(int i =0; i < dividedNumber; i++)
+            randomLottos.add(new ArrayList<>());
     }
 }
