8aaf16a6a55a97b9d8796cc43d9654eeb094ff09
KimYongJun <rladydwns129@naver.com>
2022-11-13 16:05:22 +0900

    feat: 로또 번호 입력에 대해 예외 처리 메소드 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -11,10 +12,26 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (isSixNumbers(numbers) || isDuplicated(numbers)) {
             throw new IllegalArgumentException();
         }
     }
-
     // TODO: 추가 기능 구현
+
+    private boolean isSixNumbers(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            return true;
+        }
+        return false;
+    }
+    private boolean isDuplicated(List<Integer> numbers) {
+        HashSet<Integer> NOT_DUPLICATED_NUMBERS = new HashSet<>();
+        for (int i : numbers) {
+            NOT_DUPLICATED_NUMBERS.add(i);
+        }
+        if (NOT_DUPLICATED_NUMBERS.size() != 6) {
+            return true;
+        }
+        return false;
+    }
 }
