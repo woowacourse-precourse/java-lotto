5c6bcb2e4c9d60f9c8f7c0d3d5fa27e9b13584b5
YuHayeon <youha0930@gmail.com>
2022-11-13 22:19:56 +0900

    refactor(Lottos): 인스턴스 변수 quantity -> price 변경
    수익 계산을 위해서 price를 가지고 있는 것이 더 낫다고 판단하여 변경

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -10,24 +10,24 @@ public class Lottos {
     private static final int LOTTO_PRICE = 1000;
     private static final String QUANTITY_NOTIFICATION_MESSAGE = "개를 구매했습니다.\n";
 
-    private Integer quantity;
+    private Integer price;
 
     private List<Lotto> lottoNumbers;
 
     public Lottos() {
-        this.quantity = countLottoQuantityByInputPrice();
+        this.price = countLottoQuantityByInputPrice();
         this.lottoNumbers = generateLottoNumbersByQuantity();
     }
 
     public Integer countLottoQuantityByInputPrice() {
         String price = InputView.lottoPriceInput();
         LottoPriceException.checkLottoPrice(price);
-        return Integer.parseInt(price) / LOTTO_PRICE;
+        return Integer.parseInt(price);
     }
 
     public List<Lotto> generateLottoNumbersByQuantity() {
         List<Lotto> lottoNumbers = new ArrayList<>();
-        for (int i = 0; i < this.quantity; i++) {
+        for (int i = 0; i < price / LOTTO_PRICE; i++) {
             lottoNumbers.add(new Lotto(LottoGenerator.generateLottoNumbers()));
         }
         return lottoNumbers;
@@ -43,7 +43,7 @@ public class Lottos {
 
     @Override
     public String toString() {
-        String quantityMessage = this.quantity + QUANTITY_NOTIFICATION_MESSAGE;
+        String quantityMessage = price / LOTTO_PRICE + QUANTITY_NOTIFICATION_MESSAGE;
         String numbersString = lottoNumbers.toString();
         return quantityMessage + numbersString.substring(1, numbersString.length() - 1).replace("], [", "]\n[");
     }
