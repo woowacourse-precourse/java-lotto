3120cb801be0239a5ab40bd057d27699a4da9f47
Sehbeom <j3beom@gmail.com>
2022-11-13 22:53:45 +0900

    test(controller): 수익률 계산 기능 테스트
    수익률 계산 기능 구현 전 테스트 코드 작성
    기능 구현 후속 예정

--- src/test/java/lotto/domain/controller/LottoAnalystTest.java
+++ src/test/java/lotto/domain/controller/LottoAnalystTest.java
@@ -38,6 +38,8 @@ public class LottoAnalystTest {
         }
     }
 
+    // ====== 당첨 통계 기능 테스트 ======
+
     @Test
     @DisplayName("당첨통계 - 3개 미만으로 일치하면 통계가 모두 0의 값을 갖는다.")
     void makeWinningStatistics_UnderFifthRankTest() {
@@ -89,4 +91,88 @@ public class LottoAnalystTest {
 
         assertThat(statisticsActual).isEqualTo(statisticsExpected);
     }
+
+    // ====== 수익률 계산 기능 테스트 ======
+
+    @Test
+    @DisplayName("수익률 - 수익률은 소수점 한자리로 반환된다.")
+    void calculateRateOfReturn_FormatTest() {
+        int purchaseAmount = 6000;
+
+        int numberOfLottos = purchaseAmount / LottoIssuer.PRICE_OF_LOTTO;
+        lottos = LottoIssuer.issueLottos(numberOfLottos);
+
+        lottoAnalyst.makeWinningStatistics(lottos, winningLottoExample);
+
+        String expectedRegex = "^[0-9]+(.)[0-9]$";
+        String actual = Double.toString(lottoAnalyst.calculateRateOfReturn());
+
+        assertThat(actual).matches(expectedRegex);
+    }
+
+    @Test
+    @DisplayName("수익률 - 100% 미만 테스트")
+    void calculateRateOfReturn_UnderHundredPercentTest() {
+        int purchaseAmount = 6000;
+        int numberOfNotWin = (purchaseAmount / LottoIssuer.PRICE_OF_LOTTO) - 1;
+        int fifthRankReturn = RankingInformation.FIFTH_PLACE.getAmountOfReturn();
+
+        setOneFifthLottoAndNotWinLottosConsistingOf(numberOfNotWin);
+
+        lottoAnalyst.makeWinningStatistics(lottos, winningLottoExample);
+
+        double expected = (double)fifthRankReturn / (double)purchaseAmount;
+        expected = Math.round(expected * 10) / 10.0;
+
+        double actual = lottoAnalyst.calculateRateOfReturn();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("수익률 - 100% 테스트")
+    void calculateRateOfReturn_HundredPercentTest() {
+        int purchaseAmount = 5000;
+        int numberOfNotWin = (purchaseAmount / 1000) - 1;
+        int fifthRankReturn = RankingInformation.FIFTH_PLACE.getAmountOfReturn();
+
+        setOneFifthLottoAndNotWinLottosConsistingOf(numberOfNotWin);
+
+        lottoAnalyst.makeWinningStatistics(lottos, winningLottoExample);
+
+        double expected = (double)fifthRankReturn / (double)purchaseAmount;
+        expected = Math.round(expected * 10) / 10.0;
+
+        double actual = lottoAnalyst.calculateRateOfReturn();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("수익률 - 100% 초과 테스트")
+    void calculateRateOfReturn_OverHundredPercentTest() {
+        int purchaseAmount = 3000;
+        int numberOfNotWin = (purchaseAmount / 1000) - 1;
+        int fifthRankReturn = RankingInformation.FIFTH_PLACE.getAmountOfReturn();
+
+        setOneFifthLottoAndNotWinLottosConsistingOf(numberOfNotWin);
+
+        lottoAnalyst.makeWinningStatistics(lottos, winningLottoExample);
+
+        double expected = (double)fifthRankReturn / (double)purchaseAmount;
+        expected = Math.round(expected * 10) / 10.0;
+
+        double actual = lottoAnalyst.calculateRateOfReturn();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    void setOneFifthLottoAndNotWinLottosConsistingOf(int numberOfNotWin) {
+        for (int i = 0; i < numberOfNotWin; i++) {
+            lottos.add(LottoGameExample.getNotWinNumber());
+        }
+
+        lottos.add(ExampleLottosAndRanks.FIFTH_PLACE.getLotto());
+    }
+
 }
