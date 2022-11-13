569b7df339ad8c4b6769cd65b919b06376c04b3c
osy9757 <osy9757@gmail.com>
2022-11-13 16:26:04 +0900

    feat : 구매한 양 출력해주는 함수 구현

---
+++ src/main/java/lotto/PrintOutputMessage.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import java.text.DecimalFormat;
+
+public class PrintOutputMessage {
+
+    private static final String PRINT_PURCHASE_AMOUNT = "%d개를 구매했습니다.";
+
+    private static final String PRINT_WINNING_STATISTICS = "당첨 통계\n---";
+
+    private static final String PRINT_WINNING_PRIZE_AMOUNT = "%d개 일치 (%d원) - %d개";
+
+    private static final String PRINT_TOTAL_PROFIT_RATE = "총 수익률은 %.2f입니다.";
+
+    DecimalFormat formatWinningPrize = new DecimalFormat("###,###");
+
+    public static void printLottoAccount(int lottoAccount) {
+        System.out.printf(PRINT_PURCHASE_AMOUNT,lottoAccount);
+    }
+
+}
