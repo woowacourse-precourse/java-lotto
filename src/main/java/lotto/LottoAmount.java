fdc43139279979a5bd8641a4c3d4901f99e2d93e
wugawuga <dnr3655@naver.com>
2022-11-13 23:03:28 +0900

    style: 띄어쓰기 수정

--- src/main/java/lotto/LottoAmount.java
+++ src/main/java/lotto/LottoAmount.java
@@ -4,7 +4,7 @@ public class LottoAmount {
     private static final int ZERO = 0;
     private static final int LOTTO_MINIMUM_AMOUNT = 1000;
     private static final String AMOUNT_UNIT_ERROR = "[ERROR] " + LOTTO_MINIMUM_AMOUNT + "원 단위로 입력하세요.";
-    private static final String LOTTO_MINIMUM_AMOUNT_ERROR = "[ERROR] 최소금액은" + LOTTO_MINIMUM_AMOUNT + "원 입니다.";
+    private static final String LOTTO_MINIMUM_AMOUNT_ERROR = "[ERROR] 최소금액은 " + LOTTO_MINIMUM_AMOUNT + "원 입니다.";
 
     private final int purchaseAmount;
 
