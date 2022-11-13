6a8485c6c193618a7f376e81d16cf956574aeac6
KyungKyu Park <rudrb0505@gmail.com>
2022-11-13 18:05:02 +0900

    feat(Rank): 맞춘 개수에 따라 등수를 부여하는 기능 구현

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Arrays;
+
 public enum Rank {
 	FIRST(2000000000),
 	SECOND(30000000),
@@ -15,7 +17,19 @@ public enum Rank {
 	}
 
 	public static Rank setRank(int matchCount, boolean matchBonus) {
-		return FIRST;
+		if (matchCount == 6) {
+			return FIRST;
+		}
+		if (matchCount == 5) {
+			return matchBonus ? SECOND : THIRD;
+		}
+		if (matchCount == 4) {
+			return FOURTH;
+		}
+		if (matchCount == 3) {
+			return FIFTH;
+		}
+		return NOTHING;
 	}
 
 	public int getMoney() {
