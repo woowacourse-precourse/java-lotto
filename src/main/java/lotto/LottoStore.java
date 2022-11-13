91015dc090c238ce7c5e458053a6945b60c12c0d
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-13 13:27:03 +0900

    feat(LottoStore): 로또 번호 랜덤 생성 및 출력 기능 호출
    docs: 로또 번호 알려주기 (출력) 완료 체크

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -1,17 +1,36 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoStore {
     private static InputView inputView;
+    private static OutputView outputView;
 
     public LottoStore() {
         inputView = new InputView();
+        outputView = new OutputView();
     }
 
     public void playLottoGame() {
         PurchaseAmount purchaseAmount = setPurchaseAmount();
+        int numberOfLottosPurchased = purchaseAmount.getNumberOfLottoPurchased();
+
+        PurchaseLottos purchaseLottos = setPurchaseLottos(numberOfLottosPurchased);
+        List<Lotto> lottos = purchaseLottos.getPurchaseLottos();
+
+        printPurchaseInform(numberOfLottosPurchased, lottos);
     }
 
     private PurchaseAmount setPurchaseAmount() {
         return new PurchaseAmount(inputView.enterAmount());
+    }
+
+    private PurchaseLottos setPurchaseLottos(int numberOfLottoPurchased) {
+        return new PurchaseLottos(numberOfLottoPurchased);
+    }
+
+    private void printPurchaseInform(int numberOfLottosPurchased, List<Lotto> lottos) {
+        outputView.printNumberOfLottosPurchased(numberOfLottosPurchased);
+        outputView.printLottosList(lottos);
     }
 }
