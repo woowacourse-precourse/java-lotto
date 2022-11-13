2ac0f638dcddc631f6b1a6f373ddc82450afc489
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-13 23:51:57 +0900

    feat: 입력값 크기 판단 기능 구현
    입력값이 1000원 미만일 경우 IllegalArgumentException 발생시키는 API 개발

--- src/main/java/lotto/util/PurchaseAmtValidator.java
+++ src/main/java/lotto/util/PurchaseAmtValidator.java
@@ -2,9 +2,12 @@ package lotto.util;
 
 import org.junit.platform.commons.util.StringUtils;
 
-public class PurchaseAmtValidator implements Validator{
+public class PurchaseAmtValidator implements Validator {
     private final String EMPTY_ERROR = "[ERROR] 금액을 입력해주세요.";
     private final String NUMERIC_ERROR = "[ERROR] 숫자만 입력해주세요.";
+    private final String LOW_AMT_ERROR = "[ERROR] 천 원 이상 입력해주세요.";
+    private final int MIN_UNIT = 1000;
+
 
     @Override
     public void validate(String input) {
@@ -13,6 +16,10 @@ public class PurchaseAmtValidator implements Validator{
         }
         if (!input.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException(NUMERIC_ERROR);
+        }
+        int inputAmt = Integer.parseInt(input);
+        if (inputAmt < MIN_UNIT) {
+            throw new IllegalArgumentException(LOW_AMT_ERROR);
         }
     }
 }
