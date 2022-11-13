f5d45c881f11263ae57d604ac7974a3547c70f68
Cyma-s <ssol0319@naver.com>
2022-11-13 16:47:51 +0900

    feat(LottoInputView): add function that verifies money is divided by 1000

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -1,7 +1,10 @@
 package lotto.view;
 
+import static lotto.model.InputErrorMessage.INVALID_FORMAT_MONEY;
+import static lotto.model.InputErrorMessage.MONEY_IS_NOT_DIVIDED;
+
 import camp.nextstep.edu.missionutils.Console;
-import lotto.model.InputErrorMessage;
+import lotto.model.Constant;
 
 public class LottoInputView {
     private final static String INPUT_MONEY_OF_LOTTO = "구입금액을 입력해 주세요.";
@@ -10,11 +13,12 @@ public class LottoInputView {
         System.out.println(INPUT_MONEY_OF_LOTTO);
         String lottoMoney = Console.readLine();
         validateMoney(lottoMoney);
+        validateMoneyIsDivided(lottoMoney);
     }
 
     public void validateMoney(String uncheckedMoney) {
         if(!isNumberString(uncheckedMoney)) {
-            throw new IllegalArgumentException(InputErrorMessage.INVALID_FORMAT_MONEY.getErrorMessage());
+            throw new IllegalArgumentException(INVALID_FORMAT_MONEY.getErrorMessage());
         }
     }
 
@@ -26,5 +30,12 @@ public class LottoInputView {
             }
         }
         return false;
+    }
+
+    public void validateMoneyIsDivided(String uncheckedMoney) {
+        int money = Integer.parseInt(uncheckedMoney);
+        if(money % Constant.MONEY_UNIT != 0) {
+            throw new IllegalArgumentException(MONEY_IS_NOT_DIVIDED.getErrorMessage());
+        }
     }
 }
