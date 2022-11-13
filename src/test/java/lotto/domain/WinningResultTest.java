51e35cf60346a0ea75a1866927a0539537595275
JJongmen <pjy90123@gmail.com>
2022-11-13 16:56:58 +0900

    test: 발행한 로또 번호가 몇 등에 당첨됬는지 확인하는 기능의 테스트 코드 작성

--- src/test/java/lotto/domain/WinningResultTest.java
+++ src/test/java/lotto/domain/WinningResultTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -8,10 +9,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningResultTest {
+    private final Lotto WINNING_NUMBERS = new Lotto(createLottoNumbers(List.of(1, 2, 3, 4, 5, 6)));
+
     @DisplayName("당첨 번호에 보너스 번호가 포함되면 예외가 발생한다.")
     @Test
     void createWinningResultByDuplicatedBonusNumber() {
-        assertThatThrownBy(() -> new WinningResult(new Lotto(createLottoNumbers(List.of(1, 2, 3, 4, 5, 6))), new LottoNumber(3)))
+        assertThatThrownBy(() -> new WinningResult(WINNING_NUMBERS, new LottoNumber(3)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 보너스 번호가 당첨 번호에 포함되어 있습니다.");
     }
@@ -19,9 +22,19 @@ class WinningResultTest {
     @DisplayName("보너스 번호가 1~45 범위 밖이면 예외가 발생한다.")
     @Test
     void createWinningResultByRange() {
-        assertThatThrownBy(() -> new WinningResult(new Lotto(createLottoNumbers(List.of(1, 2, 3, 4, 5, 6))), new LottoNumber(55)))
+        assertThatThrownBy(() -> new WinningResult(WINNING_NUMBERS, new LottoNumber(55)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 1~45 범위 밖의 숫자가 포함되어 있습니다.");
+    }
+
+    @DisplayName("당첨 번호와 로또 번호가 같으면 1등이다.")
+    @Test
+    void check1stLotto() {
+        WinningResult winningResult = new WinningResult(WINNING_NUMBERS, new LottoNumber(7));
+
+        Rank rank = winningResult.check(WINNING_NUMBERS);
+
+        assertThat(rank).isEqualTo(Rank.FIRST);
     }
 
     private List<LottoNumber> createLottoNumbers(List<Integer> numbers) {
