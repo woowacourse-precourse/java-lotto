1acc79423ce380ca7ea20f596195260e46bf8215
Jihoon9809 <dkdb521@naver.com>
2022-11-13 15:45:47 +0900

    유저의 금액, 유저의 번호 메소드로 분리 및 ,기준 문자열 분리 확인

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,20 +9,31 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-
-        System.out.println("구입금액을 입력해 주세요.");
-        int userPrice = Integer.parseInt(Console.readLine()); // '.'의 개수가 많아 메소드를 생성하여 추가 수정 필요,
+        int userPrice = inputUserPrice();
 
         Buy userBuy = new Buy(userPrice);
         userBuy.printRandomLotto();
 
+        List<String> userNumbers = inputNumbers();
+
+        for(String a : userNumbers) System.out.println(a);
+//        System.out.println(userNumbers.get(2));
+//        Lotto userLotto = new Lotto(userNumbers);
+    }
+
+    private static int inputUserPrice(){
+        System.out.println("구입금액을 입력해 주세요.");
+        String price = Console.readLine();
+        int userPrice = Integer.parseInt(price);
+
+        return userPrice;
+    }
+    private static List<String> inputNumbers(){
         System.out.println("당첨 번호를 입력해 주세요.");
         String userSelecteNumbers = Console.readLine();
         String[] numbers = userSelecteNumbers.split(",");
-        List<String> userNumbers = new ArrayList<String>(Arrays.asList(numbers));
+        List<String> userNumbers = new ArrayList<String>(Arrays.asList(numbers)); // 로또 클래스의 생성자의 매게변수가 List<String>이므로 정답을 비교할때 자료형을 변환한다.
 
-        for(String a : userNumbers) System.out.println(a);
-
-//        Lotto userLotto = new Lotto(userNumbers);
+        return userNumbers;
     }
 }
