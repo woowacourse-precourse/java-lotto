4247f15d01543c04950c9137819a9b418e0ae714
jeongwle <dldl4568@naver.com>
2022-11-14 07:47:38 +0900

    refactor: NumberGenerator 간단한 수정
    정렬하는 코드 삭제
    인라인으로 리턴하도록 수정

--- src/main/java/lottoMachine/NumberGenerator.java
+++ src/main/java/lottoMachine/NumberGenerator.java
@@ -5,7 +5,6 @@ import static lottoMachine.enums.Numbers.LOTTO_NUMBER_END;
 import static lottoMachine.enums.Numbers.WINNING_NUMBER_SIZE;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Collections;
 import java.util.List;
 
 public class NumberGenerator {
@@ -14,15 +13,8 @@ public class NumberGenerator {
     }
 
     public static List<Integer> createRandomNumber() {
-        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(
+        return Randoms.pickUniqueNumbersInRange(
                 LOTTO_NUMBER_START.getValue(), LOTTO_NUMBER_END.getValue(),
                 WINNING_NUMBER_SIZE.getValue());
-        sortAscending(randomNumbers);
-        return randomNumbers;
     }
-
-    private static void sortAscending(List<Integer> randomNumbers) {
-        Collections.sort(randomNumbers);
-    }
-
 }
