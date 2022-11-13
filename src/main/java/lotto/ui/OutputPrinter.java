06b249bd14e53d0c94af7fce928d2e301970d51a
KSH <tkdgns543@gmail.com>
2022-11-14 04:48:45 +0900

    feat(ui): 총 구매 번호 출력 구현

--- src/main/java/lotto/ui/OutputPrinter.java
+++ src/main/java/lotto/ui/OutputPrinter.java
@@ -1,4 +1,15 @@
 package lotto.ui;
 
+import java.util.List;
+
 public class OutputPrinter {
+    OutputPrinter() {}
+
+    public void printPurchasedNumbers(long bundleSize, List<List<Integer>> lottoBundle) {
+        System.out.println(bundleSize+"개를 구매했습니다.");
+
+        for (List<Integer> singleLotto : lottoBundle) {
+            System.out.println(singleLotto);
+        }
+    }
 }
