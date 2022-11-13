4693f522ec6ebc42c34a95dc8dd3e5a95c87d803
ParkJoungWoo <jug78@naver.com>
2022-11-13 21:56:16 +0900

    feat(lotto/Application.java) : Application 기능 작성
    로또 게임 진행 기능을 작성했습니다.
    
    - main 메소드에 lotto 기능을 작성했습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import ui.View;
+import user.User;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        User user = new User();
+        LottoSeller lottoSeller = new LottoSeller();
+
+        lottoSeller.giveLottos(user);
+        user.printPurchaseResult();
+
+        lottoSeller.lottoDraw();
+        lottoSeller.givePrize(user);
+
+        user.printRatio();
     }
 }
