ed516ac559251bc4cad0addbb774873e03552c71
Park shinyeong <syngh503@gmail.com>
2022-11-13 21:04:54 +0900

    Feat(RankingService): 랭킹 설정 메소드 구현
    사용자 당첨 번호와 로또 번호 목록을 비교해 랭킹을 세팅하는 getRanking() 메소드를 구현하였습니다.
    
    Relate to: #9

--- src/main/java/lotto/Controller/RankingService.java
+++ src/main/java/lotto/Controller/RankingService.java
@@ -1,4 +1,30 @@
 package lotto.Controller;
 
+import lotto.Domain.Lotto;
+import lotto.Domain.Player;
+import lotto.Domain.Rank;
+import lotto.View.OutputView;
+import lotto.type.RankType;
+
+import java.util.List;
+
 public class RankingService {
+    private Rank rank;
+    private Player player;
+
+    public RankingService(Rank rank, Player player) {
+        this.rank = rank;
+        this.player = player;
+    }
+
+    public void getRanking() {
+        List<Lotto> lottos = player.getLottos();
+        for (Lotto lotto : lottos) {
+            List<Integer> lottoNumber = lotto.getLottoNumbers();
+            int rank = this.rank.countWinningNumber(lottoNumber, player.getPlayerLotto());
+            boolean isBonusContain = this.rank.isBonusBallContain(lottoNumber, player.getPlayerBonusNumber());
+            RankType rankType = RankType.setRankType(rank, isBonusContain);
+            this.rank.setRank(rankType);
+        }
+    }
 }
