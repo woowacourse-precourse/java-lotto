ac98b6cdf4bcd54d68281b657ef4a2eecb11ef89
Woojin Lee <dhkep03@gmail.com>
2022-11-14 01:15:50 +0900

    feat(UserInterface): 당첨 결과 출력

--- src/main/java/lotto/ui/UserInterface.java
+++ src/main/java/lotto/ui/UserInterface.java
@@ -2,6 +2,8 @@ package lotto.ui;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import java.util.Map;
+import lotto.constant.WinningResult;
 import lotto.domain.Validator;
 
 public class UserInterface {
@@ -9,6 +11,7 @@ public class UserInterface {
     private static final String PUBLISH_LOTTOS_ANNOUNCEMENT = "개를 구매했습니다.";
     private static final String INPUT_WINNING_NUMBERS_ANNOUNCEMENT = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER_ANNOUNCEMENT = "보너스 번호를 입력해 주세요.";
+    private static final String WINNING_STATISTICS_ANNOUNCEMENT = "당첨 통계\n---";
 
     public static String getInputMoney() {
         System.out.println(INPUT_MONEY_ANNOUNCEMENT);
@@ -40,5 +43,16 @@ public class UserInterface {
         String bonusNumberRaw = Console.readLine();
 //        Validator.checkInputBonusNumber(bonusNumberRaw);
         return bonusNumberRaw;
+    }
+
+    public static void printResult(Map<WinningResult, Integer> results) {
+        System.out.println();
+        System.out.println(WINNING_STATISTICS_ANNOUNCEMENT);
+        for(WinningResult result: results.keySet()) {
+            if(result == WinningResult.RANK_NONE) {
+                continue;
+            }
+            System.out.println(result.getAnnouncement() + " - " + results.get(result) + "개");
+        }
     }
 }
