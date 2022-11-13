a75db42ec7e4881c7793ee1c33935f43d520468b
saehim <saeilee.vxv@gmail.com>
2022-11-13 18:42:53 +0900

    feat(lotto) : 로또 구입 개수, 결과 출력 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,22 @@
 package lotto;
 
+import lotto.controller.Generator;
+import lotto.domain.Lotto;
 import lotto.view.Input;
+import lotto.view.Messages;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         int Money = Input.askMoney();
-        int lotto = Input.inputMoney(Money);
+        int lottoCount = Input.inputMoney(Money);
+
+        System.out.println(Messages.PRINT_BUY_LOTTO(lottoCount));;
+
+        List<Lotto> lottos = Generator.getLottos(lottoCount);
+
+        System.out.println(Messages.PRINT_LOTTOS(lottos));
+
     }
 }
