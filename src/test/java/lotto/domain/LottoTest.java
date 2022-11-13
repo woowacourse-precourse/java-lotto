4ec22d08a8e192a0217ee5ac6281c7ae018ac070
skydreamer21 <skydreamer210@gmail.com>
2022-11-13 15:45:56 +0900

    test(LottoTest): 범위 이외의 숫자에 대한 예외 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,14 +1,30 @@
-package lotto;
+package lotto.domain;
 
 import lotto.domain.Lotto;
+import lotto.constant.ValidationErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
+    @DisplayName("1에서 45 사이의 번호가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 46, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]")
+                .hasMessageContaining("1부터 45까지의 숫자만 허용됩니다.");
+
+
+        assertThat(ValidationErrorMessage.DUPLICATE_NUMBERS.getErrorMessage())
+                .isEqualTo("[ERROR] 중복된 숫자가 없도록 입력해주세요.");
+    }
+
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
