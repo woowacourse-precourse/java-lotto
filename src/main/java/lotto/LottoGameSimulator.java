760e34f178d59547d996a3c17a7e809bb0807745
Kim kyunghee <kkh2415@naver.com>
2022-11-13 13:50:13 +0900

    feat(ui): 구입 금액, 당첨 번호, 보너스 번호 입력 기능
    구현 목록:
      - Console.readLine()를 호출하는 LottoScanners 입력 클래스 구현

--- src/main/java/lotto/LottoGameSimulator.java
+++ src/main/java/lotto/LottoGameSimulator.java
@@ -2,48 +2,59 @@ package lotto;
 
 import lotto.domain.Lotto;
 import lotto.ui.Display;
+import lotto.ui.LottoScanners;
 
 import java.util.List;
 
 public class LottoGameSimulator {
 
     private final Display display;
+    private final LottoScanners scanners;
 
     public LottoGameSimulator() {
         this.display = new Display();
+        this.scanners = new LottoScanners();
     }
 
     public void start() {
-        requestPriceProcess();
-        announcePurchasedLottoProcess();
+        List<Lotto> lottoPacks = buyLotto();
+        checkLottoNumbers(lottoPacks);
 
-        requestWinningNumberProcess();
-        requestBonusNumberProcess();
+        setLottoWinningNumbers();
 
-        announceLottoStatProcess();
+        checkLottoResult();
     }
 
-    public void requestPriceProcess() {
+    public List<Lotto> buyLotto() {
+        String price = requestPriceProcess();
+
+        return List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+    }
+
+    public String requestPriceProcess() {
         display.printRequestPrice();
+        String price = scanners.inputPrice();
+        display.printNewLine();
+
+        return price;
+    }
+
+    public void checkLottoNumbers(List<Lotto> lottoPacks) {
+        display.printPurchasedLottoPacks(lottoPacks);
         display.printNewLine();
     }
 
-    public void announcePurchasedLottoProcess() {
-        display.printPurchasedLottoPacks(List.of(new Lotto(List.of(1,2,3,4,5,6))));
-        display.printNewLine();
-    }
-
-    public void requestWinningNumberProcess() {
+    public void setLottoWinningNumbers() {
         display.printRequestWinningNumber();
+        String winingNumber = scanners.inputWiningNumber();
         display.printNewLine();
-    }
 
-    public void requestBonusNumberProcess() {
         display.printRequestBonusNumber();
+        String bonusNumber = scanners.inputBonusNumber();
         display.printNewLine();
     }
 
-    public void announceLottoStatProcess() {
+    public void checkLottoResult() {
         display.printWinningList(0,0,0,0,0);
         display.printEarningLate(70);
         display.printNewLine();
---
+++ src/main/java/lotto/ui/LottoScanners.java
@@ -0,0 +1,21 @@
+package lotto.ui;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoScanners {
+
+    public String inputPrice() {
+        String input = Console.readLine();
+        return input;
+    }
+
+    public String inputWiningNumber() {
+        String input = Console.readLine();
+        return input;
+    }
+
+    public String inputBonusNumber() {
+        String input = Console.readLine();
+        return input;
+    }
+}
