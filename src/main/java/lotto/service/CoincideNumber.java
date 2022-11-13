a47bf12f64605325d28de267258087e1f10967d2
yoonjuho <dbswn990@naver.com>
2022-11-13 22:47:55 +0900

    feat(CoincideNumber) : Enum에 당첨금액 추가

--- src/main/java/lotto/service/CoincideNumber.java
+++ src/main/java/lotto/service/CoincideNumber.java
@@ -6,18 +6,20 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum CoincideNumber {
-    THREE_COINCIDE("3개 일치 (5,000원) - ",5,0),
-    FOUR_COINCIDE("4개 일치 (50,000원) - ",4,0),
-    FIVE_COINCIDE("5개 일치 (1,500,000원) - ",3,0),
-    FIVE_BONUS_COINCIDE("5개 일치, 보너스 볼 일치 (30,000,000원) - ",2,0),
-    SIX_COINCIDE("6개 일치 (2,000,000,000원) - ",1,0);
+    THREE_COINCIDE("3개 일치 (5,000원) - ",5,0,5000),
+    FOUR_COINCIDE("4개 일치 (50,000원) - ",4,0,50000),
+    FIVE_COINCIDE("5개 일치 (1,500,000원) - ",3,0,1500000),
+    FIVE_BONUS_COINCIDE("5개 일치, 보너스 볼 일치 (30,000,000원) - ",2,0,30000000),
+    SIX_COINCIDE("6개 일치 (2,000,000,000원) - ",1,0,2000000000);
     private String phrase;
     private int rank;
     private int count;
-    CoincideNumber(String phrase, int rank,int count) {
+    private int money;
+    CoincideNumber(String phrase, int rank,int count, int money) {
         this.phrase = phrase;
         this.rank = rank;
         this.count = count;
+        this.money = money;
     }
     private int rank(){
         return rank;
@@ -35,6 +37,9 @@ public enum CoincideNumber {
             Stream.of(values()).collect(Collectors.toMap(CoincideNumber::rank, Function.identity()));
     public static void plusCountOfRank(int rank){
         BY_RANK.get(rank).plusCount();
+    }
+    public int calculateRevenue(){
+        return this.count * this.money;
     }
 
 }
