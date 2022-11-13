172010feaaca6a956b3c1aeb779c992d0bdbef2f
wonseokjee <wsji9404@gmail.com>
2022-11-13 16:46:13 +0900

    Feat(CreateLotto): 생성 textWinningNumber, textBounsNumber 함수
    해당 문자열을 출력하는 textWinningNumber, textBounsNumber 함수 생성

--- src/main/java/lotto/CreateLotto.java
+++ src/main/java/lotto/CreateLotto.java
@@ -5,8 +5,21 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class CreateLotto {
-    public List<Integer> CreateRandomBall() {
+    final List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    public List<Integer> createRandomBall() {
         return numbers;
     }
-    List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+    public void winningNumber() {
+
+    }
+
+    public void textWinningNumber() {
+        System.out.println("당첨 번호를 입력해주세요");
+    }
+
+    public void textBounsNumber() {
+        System.out.println("보너스 번호를 입력해주세요");
+    }
+
 }
