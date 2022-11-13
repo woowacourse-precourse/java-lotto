2716e8a15fc8e8d38ce2e1d93fc630e1afe95c8b
Ridealist <cjsn92@gmail.com>
2022-11-13 23:55:08 +0900

    refactor: MVC패턴 적용_UI 로직 view 패키지 분리

---
+++ src/main/java/lotto/view/output/BuyListView.java
@@ -0,0 +1,16 @@
+package lotto.view.output;
+
+import lotto.model.Lotto;
+import java.util.List;
+
+public class BuyListView {
+
+    public static void show(List<Lotto> lotteryNumbers, int purchaseAmount) {
+        System.out.println();
+        System.out.printf("%d개를 구매했습니다.%n", purchaseAmount);
+        for (Lotto lotto : lotteryNumbers) {
+            List<Integer> lottoNumbers = lotto.getLottoNumbers();
+            System.out.println(lottoNumbers);
+        }
+    }
+}
