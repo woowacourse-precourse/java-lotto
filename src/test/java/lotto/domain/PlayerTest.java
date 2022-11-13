0bf79b17f63a4c71ad9ed0f1d9321986c9c3e3f0
win9 <krrr8@naver.com>
2022-11-14 03:19:21 +0900

    Test(PlayerTest): 입력하는 당첨번호가 의도한대로 결과를 출력하는지 테스트 완료
    입력한 당첨번호가 1~45사이가 아니고, 숫자가 아니라면 해당 오류를 출력하고,
    올바른 숫자이면 테스트가 통과하도록 한다.

--- src/test/java/lotto/domain/PlayerTest.java
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -99,6 +99,36 @@ class PlayerTest {
         Assertions.assertTrue(prizeNumbers.containsAll(List.of(1,2,4,5,6,7)));
     }
 
+    /**
+     * 보너스 숫자 입력 테스트
+     */
+    @DisplayName("1~45 사이가 아닌 숫자를 입력하면 에러가 발생한다.")
+    @Test
+    void bonusNumbersOverRangeInputException() {
+        ScannerInput("46");
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            player.inputBonusNumber();
+        });
+    }
+
+    @DisplayName("숫자를 입력하지 않으면 오류가 발생한다.")
+    @Test
+    void bonusNumbersNotNumberInputException() {
+        ScannerInput("A");
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            player.inputBonusNumber();
+        });
+    }
+
+    @DisplayName("1~45사이의 올바른 숫자를 입력하면 통과한다.")
+    @Test
+    void bonusNumbersInput() {
+        ScannerInput("5");
+        player.inputBonusNumber();
+        int bonusNumber = player.getBonusNumber();
+        Assertions.assertEquals(bonusNumber, 5);
+    }
+
     private void ScannerInput(String systemInput) {
         InputStream input = generateUserInput(systemInput);
         System.setIn(input);
