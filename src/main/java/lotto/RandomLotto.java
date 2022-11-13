62fde24c6150f23fcba61bfc48a035de673467f0
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-11-14 04:21:29 +0900

    feat<RandomLotto>: 발행할 티켓 수만큼 로또 번호를 생성하는 코드 추가

---
+++ src/main/java/lotto/RandomLotto.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RandomLotto {
+    private final List<List<Integer>> randomNumbers;
+    private static final ExceptionHandler exceptionHandler = new ExceptionHandler();
+
+    public RandomLotto(List<List<Integer>> randomNumbers) {
+        validateRandomNumberDuplicate(randomNumbers);
+        this.randomNumbers = randomNumbers;
+    }
+
+    public static List<List<Integer>> creatRandomNumber(int lottoQuantity) {
+        List<Integer> randomNumber;
+        List<List<Integer>> randomNumbersPr = new ArrayList<>();
+        for (int i = 0; i < lottoQuantity; i++) {
+            randomNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(randomNumber);
+            randomNumbersPr.add(randomNumber);
+        }
+        return randomNumbersPr;
+    }
+
+    private void validateRandomNumberDuplicate(List<List<Integer>> numbers) {
+        for (int i = 1; i < numbers.size(); i++) {
+            if (numbers.get(i-1).equals(numbers.get(i))) {
+                exceptionHandler.foolExceaption("RandomlottoCondition");
+            }
+        }
+    }
+}
