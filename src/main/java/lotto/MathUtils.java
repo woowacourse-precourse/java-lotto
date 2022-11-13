eb93cbb09cefe1864b53d6e0c2149b5fc886bc5a
anso33 <hyeon131812@gmail.com>
2022-11-13 19:28:36 +0900

    [FEAT](MathUtils): function that calculate available number of lotto

--- src/main/java/lotto/MathUtils.java
+++ src/main/java/lotto/MathUtils.java
@@ -1,2 +1,12 @@
-package lotto;public class MathUtils {
+package lotto;
+
+public class MathUtils {
+
+
+  public static int numberOfLotto(int money) {
+    int ticket = 0;
+
+    ticket = money / 1000;
+    return ticket;
+  }
 }
