994f87df0767feea2dbeeb59edd505cd34c6e397
d0ri123 <lsh81550@gmail.com>
2022-11-13 14:24:31 +0900

    feat(lotto): 사용자가 로또 번호와 보너스 번호를 입력하는 기능 구현

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -19,10 +19,26 @@ public class User {
     }
 
     public void userNumbers(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        String numbers = sc.nextLine();
 
+        for(String num : numbers.split(",")){
+            userNumbers.add(Integer.valueOf(num));
+        }
+    }
+
+    public List<Integer> getUserNumbers(){
+        return this.userNumbers;
     }
 
     public void userBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        this.bonusNumber = sc.nextInt();
+    }
 
+    public int getBonusNumber(){
+        return this.bonusNumber;
     }
 }
