74a56512df1789752d971149d06df8f626653376
kimminsu <xjfcnfw3@naver.com>
2022-11-13 13:14:51 +0900

    test: 로또 머신 유저 보너스 입력 예외 테스트

--- src/test/java/lotto/lottoMachine/LottoMachineUserBonusTest.java
+++ src/test/java/lotto/lottoMachine/LottoMachineUserBonusTest.java
@@ -1,14 +1,31 @@
-package lotto.user;
+package lotto.lottoMachine;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import lotto.user.validation.ExceptionMessage;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import lotto.lottomachine.LottoMachine;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class UserBonusTest {
+public class LottoMachineUserBonusTest {
+
+    private static final String ERROR = "[ERROR]";
+    private static LottoMachine lottoMachine;
+    private OutputStream out;
+
+    @BeforeEach
+    void init() {
+        lottoMachine = new LottoMachine();
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+    }
+
     @DisplayName("보너스를 숫자로 입력하지 않으면 예외가 발생한다.")
     @Test
     void InputBonusByNotNumber() {
@@ -17,10 +34,11 @@ public class UserBonusTest {
         InputStream in = new ByteArrayInputStream(userAmount.getBytes());
         System.setIn(in);
 
+        // when
+        lottoMachine.inputBonus();
+
         // when then
-        assertThatThrownBy(User::inputBonusNumber)
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining(ExceptionMessage.ONLY_NUMBER.toString());
+        assertThat(out.toString()).contains(ERROR);
     }
 
     @DisplayName("보너스는 1~45가 아니면 예외가 발생한다.")
@@ -31,9 +49,10 @@ public class UserBonusTest {
         InputStream in = new ByteArrayInputStream(userAmount.getBytes());
         System.setIn(in);
 
+        // when
+        lottoMachine.inputUserBonus();
+
         // when then
-        assertThatThrownBy(User::inputBonusNumber)
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining(ExceptionMessage.NUMBER_RANGE.toString());
+        assertThat(out.toString()).contains(ERROR);
     }
 }
