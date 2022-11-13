8a95499cceecda2ef131cb63ddd56f3277bf9648
HiiWee <wpdlzhf159@gmail.com>
2022-11-14 03:50:50 +0900

    feat: 0으로 시작하거나, 0이 입력되면 예외 발생 기능 추가

--- src/main/java/lotto/constants/message/ExceptionMessage.java
+++ src/main/java/lotto/constants/message/ExceptionMessage.java
@@ -4,7 +4,9 @@ public class ExceptionMessage {
     public static final String ERROR = "[ERROR] ";
     public static final String NUMERIC_LENGTH = "하나의 로또는 6개의 숫자로 이루어져야 합니다.";
     public static final String DUPLICATED_NUMBER = "로또의 번호는 중복될 수 없습니다.";
-    public static final String NON_NUMERIC_INPUT = "숫자를 입력해야 합니다.";
+    public static final String NON_NUMERIC_INPUT = "숫자만 입력해야 합니다.";
+    public static final String ZERO_INPUT = "0원을 입력할 수 없습니다.";
+    public static final String START_WItH_ZERO = "금액은 0으로 시작할 수 없습니다.";
 
     private ExceptionMessage() {
     }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -8,6 +8,8 @@ public class LottoController {
     public static final String ONLY_NUMBER = "^[0-9]*$";
     public static final int NOTHING = 0;
     public static final int LOTTO_PRICE = 1000;
+    public static final int ZERO = 0;
+    public static final int FIRST_INDEX = 0;
 
     private final LottoService lottoService = new LottoService();
     private final InputView inputView = new InputView();
@@ -22,10 +24,24 @@ public class LottoController {
         if (!inputPrice.matches(ONLY_NUMBER)) {
             throw new IllegalArgumentException(ExceptionMessage.ERROR + ExceptionMessage.NON_NUMERIC_INPUT);
         }
+        if (isZero(Integer.parseInt(inputPrice))) {
+            throw new IllegalArgumentException(ExceptionMessage.ERROR + ExceptionMessage.ZERO_INPUT);
+        }
+        if (isStartWithZero(inputPrice.charAt(FIRST_INDEX))) {
+            throw new IllegalArgumentException(ExceptionMessage.ERROR + ExceptionMessage.START_WItH_ZERO);
+        }
         if (!isValidPrice(Integer.parseInt(inputPrice))) {
             inputView.printInputPriceAgain();
             startLottoApplication();
         }
+    }
+
+    private boolean isStartWithZero(char firstNumber) {
+        return Integer.parseInt(String.valueOf(firstNumber)) == ZERO;
+    }
+
+    private boolean isZero(int inputPrice) {
+        return inputPrice == ZERO;
     }
 
     private boolean isValidPrice(int inputPrice) {
