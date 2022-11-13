2eceed4e651444f9439e01fd974f5346b87f358b
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-13 16:56:49 +0900

    refactor: 수익률계산기능
    - 결과종합리스트에서 로또테이블의 개수를 가져와서 수익률 계산

--- src/main/java/lotto/domain/ProfitCalculator.java
+++ src/main/java/lotto/domain/ProfitCalculator.java
@@ -5,12 +5,17 @@ import java.util.List;
 public class ProfitCalculator {
 
     public float getRateOfReturn(List<Integer> sumOfResult) {
+        int amount = 0;
         float sum = 0;
         int[] prizeMoney = {5_000, 50_000, 1_500_000, 30_000_000, 2_000_000_000};
         for (int i = 0; i < sumOfResult.size(); i++) {
+            if (i == sumOfResult.size() - 1) {
+                amount = sumOfResult.get(i);
+                continue;
+            }
             sum += sumOfResult.get(i) * prizeMoney[i];
         }
-        return sum / (8 * Lotto.PRICE) * 100;
+        return sum / (amount * Lotto.PRICE) * 100;
     }
 }
 
