181f476ac4b4db0d275a98cc43798a2ea8ad6845
Jeongthingamajig <netpetet@naver.com>
2022-11-13 20:19:51 +0900

    Refactor : UserMoneyService 클래스 리팩토링
    유저가 돈을 입력하면 살 수 있는 로또 개수를 따로 가져와서 사용했어야 했던 것을 LotteryDraw 에서 구하고 사용하게 변경

--- src/main/java/lotto/service/UserMoneyService.java
+++ src/main/java/lotto/service/UserMoneyService.java
@@ -5,34 +5,28 @@ import lotto.domain.ValidUserInput;
 
 public class UserMoneyService {
 
-    private final String userInput = User.getUserInputMoney();
-    private final int userMoney = Integer.parseInt(userInput);
+    private static final String userInput = User.getUserInputMoney();
 
     // 생성자에서 검증
     public UserMoneyService() {
         ValidUserInput.ValidInputNumberInteger(userInput);
         ValidUserInput.ValidInputNumberOverZero(userInput);
+        int userMoney = Integer.parseInt(userInput);
         ValidInputMoneyOverThousand(userMoney);
-    }
-
-    public String getUserMoney(){ // 인스턴수 사용 없이 쓰기 위해 static
-        return userInput;
-    }
-
-    // 여기선 string 으로 주고 뷰에서 integer 파싱해도 되는데
-    // 자동생성 로또 번호에도 써야 하기 때문에 int 로 바꾼 뒤에 보내줌
-    public int getUserLottoCount(){ // 로또 갯수는 1000으로 떨어지는지 추가로 검증
         ValidInputMoneyDivideThousand(userMoney);
+
+    }
+    public int getUserMoney(){ // 인스턴수 사용 없이 쓰기 위해 static
         return Integer.parseInt(userInput);
     }
 
-    public void ValidInputMoneyOverThousand(int inputMoney){
+    private void ValidInputMoneyOverThousand(int inputMoney){
         if(inputMoney < 1000){
             throw new IllegalArgumentException("[ERROR] 입력한 금액이 1000보다 작습니다.");
         }
     }
 
-    public void ValidInputMoneyDivideThousand(int inputMoney){
+    private void ValidInputMoneyDivideThousand(int inputMoney){
         if((inputMoney % 1000) != 0){
             throw new IllegalArgumentException("[ERROR] 입력한 금액이 1000으로 나누어 지지 않습니다.");
         }
