abdbac660639dbd7ec3e5fcd071ea968ae6fa460
sonsoowon <yuwoo1018@gmail.com>
2022-11-13 17:30:55 +0900

    feat(LottoValidator): 로또 번호가 1보다 작거나 45보다 큰 경우 예외 발생

--- src/main/java/lotto/valid/LottoValidator.java
+++ src/main/java/lotto/valid/LottoValidator.java
@@ -9,13 +9,26 @@ import java.util.Set;
 public enum LottoValidator {
     INSTANCE;
     private static final int LOTTO_SIZE = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
 
+    private static final String NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.";
     private static final String LOTTO_SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호의 개수는 6개여야 합니다.";
     private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복되지 않아야 합니다.";
 
     public static void validate(List<Integer> numbers) {
+        checkRange(numbers);
         checkSize(numbers);
         checkDuplicate(numbers);
+    }
+
+    private static void checkRange(List<Integer> numbers) {
+        boolean incorrectNumberExists = numbers.stream()
+                .anyMatch(number -> number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER);
+
+        if (incorrectNumberExists) {
+            throw new IllegalArgumentException(NUMBER_RANGE_ERROR_MESSAGE);
+        }
     }
 
     private static void checkSize(List<Integer> numbers) {
@@ -26,7 +39,7 @@ public enum LottoValidator {
 
     private static void checkDuplicate(List<Integer> numbers) {
         Set<Integer> distinctNumbers = new HashSet<>(numbers);
-        if(distinctNumbers.size() != numbers.size()){
+        if (distinctNumbers.size() != numbers.size()) {
             throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
         }
     }
