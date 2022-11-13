84b863c7345746a4407e1be2f042ce16f87dc15d
skydreamer21 <skydreamer210@gmail.com>
2022-11-14 00:06:20 +0900

    test(LotteryTicketRepositoryTest): LotteryTicketRepository 싱글톤 구현으로 인한 테스트 코드 수정
    [세부사항]
    - 싱글톤 구현으로 인해 각 테스트 이후에 저장소를 초기화해주는 afterEach 코드 추가

--- src/test/java/lotto/repository/LotteryTicketRepositoryTest.java
+++ src/test/java/lotto/repository/LotteryTicketRepositoryTest.java
@@ -1,8 +1,7 @@
 package lotto.repository;
 
 import lotto.domain.LotteryTicket;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,10 +9,16 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.*;
 
 public class LotteryTicketRepositoryTest {
+    @AfterEach
+    void afterEach() {
+        LotteryTicketRepository repository = LotteryTicketRepository.getInstance();
+        repository.clearStore();
+    }
+
     @DisplayName("저장된 로또 티켓을 lotteryId로 조회한다.")
     @Test
     void findByValidLotteryId() {
-        LotteryTicketRepository repository = new LotteryTicketRepository();
+        LotteryTicketRepository repository = LotteryTicketRepository.getInstance();
         LotteryTicket lotteryTicket = new LotteryTicket(List.of(1, 2, 3, 4, 5, 6));
         String lotteryId = lotteryTicket.getLotteryId();
 
@@ -26,7 +31,7 @@ public class LotteryTicketRepositoryTest {
     @DisplayName("존재하지 않는 lotteryId로 조회시 반환되는 LotteryTicket이 없다.")
     @Test
     void findByNonExistentLotteryId() {
-        LotteryTicketRepository repository = new LotteryTicketRepository();
+        LotteryTicketRepository repository = LotteryTicketRepository.getInstance();
         String lotteryId = "nothing";
 
         assertThat(repository.findByLotteryId(lotteryId))
