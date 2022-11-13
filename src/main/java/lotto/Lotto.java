748ed42394ac2745b2bb254609410d436008a549
Jae Ung Jung <roosa0210@gmail.com>
2022-11-14 06:17:11 +0900

    feat(Lotto.num_lotto): 발행할 로또의 수를 반환하는 함수 생성
    입력받은 돈에 따른 로또의 숫자를 반환하며 1000원으로 나눠떨어지지 않는 경우 예외 처리하는 함수 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class Lotto {
@@ -14,6 +16,15 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public int num_lotto(int money) {
+        int num = 0;
+        num = money / 1000 ;
+        if (num % money != 0) {
+            throw new IllegalArgumentException();
+        }
+        return num;
     }
 
     // TODO: 추가 기능 구현
