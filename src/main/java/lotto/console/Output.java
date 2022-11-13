f24253f84f39e951ee5d656de7d8f3892a4f07a8
jaehyeon <jhjh0022@naver.com>
2022-11-14 01:15:27 +0900

    feat: 에러 메시지 출력해주는 기능 구현

--- src/main/java/lotto/console/Output.java
+++ src/main/java/lotto/console/Output.java
@@ -5,61 +5,65 @@ import lotto.domain.Lotto;
 import java.util.List;
 
 public class Output {
-    public static void askPurchaseAmount() {
+    public void askPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public static void printLotto(List<Lotto> lottos) {
+    public void printLotto(List<Lotto> lottos) {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.value());
         }
     }
 
-    public static void printEmptyLine() {
+    public void printEmptyLine() {
         System.out.println(" ");
     }
 
-    public static void announceNumber(int number) {
+    public void announceNumber(int number) {
         System.out.println(number + "개를 구매했습니다.");
     }
 
-    public static void askWinningNumber() {
+    public void askWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
-    public static void askBonusNumber() {
+    public void askBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
-    public static void printWinningStatistics() {
+    public void printWinningStatistics() {
         System.out.println("당첨 통계");
     }
 
-    public static void printLine() {
+    public void printLine() {
         System.out.println("---");
     }
 
-    public static void printThree(int threeNumber) {
-        System.out.println("3개 일치 (5,000원) - " + threeNumber);
+    public void printThree(int threeNumber) {
+        System.out.println("3개 일치 (5,000원) - " + threeNumber+"개");
     }
 
-    public static void printFour(int fourNumber) {
-        System.out.println("4개 일치 (50,000원) - " + fourNumber);
+    public void printFour(int fourNumber) {
+        System.out.println("4개 일치 (50,000원) - " + fourNumber+"개");
     }
 
-    public static void printFive(int fiveNumber) {
-        System.out.println("5개 일치 (1,500,000원) - " + fiveNumber);
+    public void printFive(int fiveNumber) {
+        System.out.println("5개 일치 (1,500,000원) - " + fiveNumber+"개");
     }
 
-    public static void printFiveFlus(int fiveFlusNumber) {
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + fiveFlusNumber);
+    public void printFiveFlus(int fiveFlusNumber) {
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + fiveFlusNumber+"개");
     }
 
     public void printSix(int sixNumber) {
-        System.out.println("6개 일치 (2,000,000,000원) - " + sixNumber);
+        System.out.println("6개 일치 (2,000,000,000원) - " + sixNumber+"개");
     }
 
-    public void reportRateOfReturn() {
-        System.out.println("총 수익률은" + "입니다.");
+    public void reportRateOfReturn(double totalReturn) {
+        System.out.println("총 수익률은 " + totalReturn + "%입니다.");
+    }
+
+    public void printErrorMessage(String message){
+        System.out.println("[ERROR]"+ message);
     }
 }
