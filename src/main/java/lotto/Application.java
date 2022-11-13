e50f8486ea4fd8cb46d53989d0b6a942afb338fd
DongWooE <ehddn977@naver.com>
2022-11-14 06:13:09 +0900

    [feat] 로또 금액을 입력받는 메소드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,9 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
+    int insertedNumber;
     public static void main(String[] args) {
         Application application = new Application();
         application.printInsertPrice();
@@ -8,5 +11,11 @@ public class Application {
 
     private void printInsertPrice(){
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void insertPriceAndSaveNumber(){
+        String s = Console.readLine();
+        insertedNumber = Integer.parseInt(s);
+        if(insertedNumber > 0 || insertedNumber%1000 != 0) Exception.of(Exception.LOTTO_PRICE_01);
     }
 }
