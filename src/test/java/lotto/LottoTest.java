77336bd2d37ab18cd986d767368c87eebb651397
saint6839 <saint6839@gmail.com>
2022-11-14 01:38:51 +0900

    test(domain) : 로또 번호가 1부터 45를 벗어날 경우 예외를 던지는 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +23,14 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 1부터 45까지의 범위를 벗어난 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void validateNumberRange() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
