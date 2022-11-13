1fa3b06fa7b1322e20132a33ea1b035aef9d6859
splitube <voicesplit@daum.net>
2022-11-13 23:22:30 +0900

    feat(Prize): 각 요소에 당첨금액 추가
    출력을 위한 String이 아닌 Long타입의 금액추가

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,19 +1,21 @@
 package lotto;
 
 public enum Prize {
-    FIRST(6, "2,000,000,000원"),
-    SECOND(5, "30,000,000원"),
-    THIRD(5, "1,500,000원"),
-    FOURTH(4, "50,000원"),
-    FIFTH(3, "5,000"),
-    NONE(0, "0");
+    FIRST(6, "2,000,000,000원", 2000000000L),
+    SECOND(5, "30,000,000원", 30000000L),
+    THIRD(5, "1,500,000원", 1500000L),
+    FOURTH(4, "50,000원", 50000L),
+    FIFTH(3, "5,000", 5000L),
+    NONE(0, "0", 0L);
 
     final int matches;
     final String price;
+    final long money;
 
-    Prize(int matches, String price) {
+    Prize(int matches, String price, long money) {
         this.matches = matches;
         this.price = price;
+        this.money = money;
     }
 
     static String prizeInfo(Prize prize) {
