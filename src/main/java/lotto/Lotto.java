ce7541fdaaab819ee70582b94856a2753930aa0e
KimYongJun <rladydwns129@naver.com>
2022-11-13 18:45:45 +0900

    feat: 로또 예외 사항(1 ~ 45 이외의 수가 들어온 경우) 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,10 +12,11 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (isSixNumbers(numbers) || isDuplicated(numbers)) {
+        if (isSixNumbers(numbers) || isDuplicated(numbers) || isInRange(numbers)) {
             throw new IllegalArgumentException();
         }
     }
+
     // TODO: 추가 기능 구현
 
     private boolean isSixNumbers(List<Integer> numbers) {
@@ -31,6 +32,15 @@ public class Lotto {
         }
         if (NOT_DUPLICATED_NUMBERS.size() != 6) {
             return true;
+        }
+        return false;
+    }
+
+    private boolean isInRange(List<Integer> numbers) {
+        for (int i : numbers) {
+            if (i < 1 || i > 45) {
+                return true;
+            }
         }
         return false;
     }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,4 +24,19 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @Test
+    @DisplayName("1보다 작은 숫자가 있으면 예외가 발생한다.")
+    void createLottoBySmallNumber() {
+        //given
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 44)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("45보다 큰 숫자가 있으면 예외가 발생한다.")
+    void createLottoByLargeNumber() {
+        //given
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
