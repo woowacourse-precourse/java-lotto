9159dc06986b55a387e42fe34a6b286136f7fba2
Kayoung <rkdkdudjd@gmail.com>
2022-11-14 01:05:05 +0900

    feat(Lotto): 예외 메시지 정의

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -24,21 +24,21 @@ public class Lotto {
     private void validateUniqueness(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
         if (uniqueNumbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호에 중복된 숫자가 있습니다.");
         }
     }
 
     private void validateRangeOfNumbers(List<Integer> numbers) {
         numbers.forEach(number -> {
             if (number < MIN_NUMBER || number > MAX_NUMBER) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("범위를 벗어나는 로또 번호가 있습니다.");
             }
         });
     }
 
     private void validateInputSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호의 개수가 올바르지 않습니다.");
         }
     }
 }
