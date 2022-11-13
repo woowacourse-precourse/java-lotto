7bb5d7632860f08b9d861614b0fa47a596543bc3
LeerangKim <pwpw102@gmail.com>
2022-11-13 10:04:33 +0900

    feat : 로또 당첨 번호 및 보너스 번호 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import lotto.model.BonusLotto;
+import lotto.model.Lotto;
 import lotto.model.LottoFactory;
 import lotto.model.Money;
 import lotto.view.InputView;
@@ -17,6 +19,9 @@ public class Application {
 
             LottoFactory lottoFactory = new LottoFactory(money.getLottoAmount());
             outputView.lottoFactoryOutput(money.getLottoAmount(), lottoFactory.getLottos());
+
+            Lotto lotto = new Lotto(inputView.inputLotto());
+            BonusLotto bonusLotto = new BonusLotto(inputView.inputBonusLotto());
 
         } catch (IllegalArgumentException e){
             System.out.println(e.getMessage());
--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,34 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+    public Lotto(String[] numbers) {
+        this.numbers = validate(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private List<Integer> validate(String[] numbers) {
+        if (numbers.length != 6) {
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해주세요.");
         }
+
+        List<Integer> lottos = new ArrayList<>();
+
+        for(int i = 0; i < 6; i++){
+            try {
+                int number = Integer.parseInt(numbers[i]);
+                if(number <= 0 || number > 45){
+                    throw  new IllegalArgumentException();
+                }
+                lottos.add(number);
+            } catch (IllegalArgumentException e){
+                throw  new IllegalArgumentException("[ERROR] 알맞은 숫자를 입력해주세요.");
+            }
+        }
+        return lottos;
     }
 
-    // TODO: 추가 기능 구현
 }
