ae843487ea1cdeb94aafa1bca7f9e6882aefc373
plusjob70 <plusjob70@gmail.com>
2022-11-14 00:36:29 +0900

    style(Error): 에러문구 변경

--- src/main/java/lotto/utils/Error.java
+++ src/main/java/lotto/utils/Error.java
@@ -6,7 +6,7 @@ public enum Error {
     EXIST_DUPLICATE_NUMBER("중복된 번호가 포함되어 있습니다."),
     NOT_SIX_NUMBERS("로또 번호는 6개여야 합니다."),
     NOT_LOTTO_NUMBER("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    CAN_NOT_SPLIT("숫자는 콤마로 구분되어야 합니다.");
+    CAN_NOT_SPLIT("6개의 번호는 공백 없는 콤마로 구분되어야 합니다.");
 
     private final String description;
 
--- src/main/java/lotto/utils/LottoInformation.java
+++ src/main/java/lotto/utils/LottoInformation.java
@@ -6,7 +6,7 @@ public class LottoInformation {
     public final static int LOTTO_BONUS_NUMBER_COUNT = 1;
     public final static int LOTTO_START_NUMBER = 1;
     public final static int LOTTO_END_NUMBER = 45;
-    public final static String LOTTO_PATTERN = "[1-9]|[1-3][0-9]|4[0-5]";
+    public final static String LOTTO_PATTERN = "([1-9]|[1-3][0-9]|4[0-5])";
     public final static String SIX_LOTTO_PATTERN;
 
     static {
