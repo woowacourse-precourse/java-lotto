9ee30995c203136100fae1cab73b4ffb2a038973
Cyma-s <ssol0319@naver.com>
2022-11-13 16:46:53 +0900

    feat(InputErrorMessage): add enum type

--- src/main/java/lotto/model/InputErrorMessage.java
+++ src/main/java/lotto/model/InputErrorMessage.java
@@ -4,7 +4,8 @@ public enum InputErrorMessage {
     INVALID_DIGITS_COUNT("로또 번호는 6자리여야 합니다."),
     DUPLICATE_NUMBERS("당첨 번호의 숫자가 중복됩니다."),
     OUT_OF_RANGE_NUMBER("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    INVALID_FORMAT_MONEY("구입 금액은 숫자여야 합니다.");
+    INVALID_FORMAT_MONEY("구입 금액은 숫자여야 합니다."),
+    MONEY_IS_NOT_DIVIDED("구입 금액은 1000원 단위여야 합니다.");
     private static final String ERROR_HEADER = "[ERROR] ";
     private final String errorMessage;
 
