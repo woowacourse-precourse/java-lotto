e663b86e492225addd581654d7a32d26b7ecca1a
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-13 12:45:20 +0900

    test(lotto) : BuyLotto 테스트 추가

---
+++ src/test/java/lotto/domain/BuyLottoTest.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class BuyLottoTest {
+
+    @DisplayName("금액에 맞는 개수의 로또를 구입했는지 체크")
+    @Test
+     void case1() {
+        BuyLotto buyLotto = new BuyLotto(8000);
+        assertThat(buyLotto.getCount()).isEqualTo(8);
+    }
+
+    @DisplayName("금액에 맞는 개수의 로또를 구입했는지 체크")
+    @Test
+    void case2() {
+        BuyLotto buyLotto = new BuyLotto(4000);
+        assertThat(buyLotto.getCount()).isEqualTo(4);
+    }
+
+    @DisplayName("금액이 1,000원 단위가 아닐 때")
+    @Test
+    void case3() {
+        assertThatThrownBy(() -> new BuyLotto(1230))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
