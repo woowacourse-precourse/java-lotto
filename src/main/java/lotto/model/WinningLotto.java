954d34e28f22d916d30d9006e842a96572dede97
jiwon1217 <koagjw@gmail.com>
2022-11-13 21:59:50 +0900

    refactor: getWinningLotto() 메서드 추가

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 public class WinningLotto {
     private static final String INVALID_INPUT_PATTERN = "[ERROR] 입력 형식과 맞지 않습니다.";
     private static final Pattern INPUT_PATTERN = Pattern.compile("^[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+$");
-
     private final Lotto winningLotto;
 
     public WinningLotto(String winningLotto) {
         isValidInputPattern(winningLotto);
         this.winningLotto = convert(winningLotto);
+    }
+
+    public Lotto getWinningLotto() {
+        return winningLotto;
     }
 
     private void isValidInputPattern(String winningLotto) {
