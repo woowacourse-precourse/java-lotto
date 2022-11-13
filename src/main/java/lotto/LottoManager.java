bdb3fcdcb720b7eaa773950e4fd7684afdf0d490
JungWoo <rlawjddn103@naver.com>
2022-11-13 21:40:25 +0900

    feat(LottoManager): LottoManager 클래스 구현 및 로또 생성 기능 구현 및 생성자에서 적용

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,2 +1,25 @@
-package lotto;public class LottoManager {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoManager {
+
+    private List<Lotto> lotties;
+    private int buyingMoney;
+
+    public LottoManager(int buyingMoney) {
+        this.buyingMoney = buyingMoney;
+        lotties = new ArrayList<>();
+        for (int i = 0; i < buyingMoney / 1000; i++) {
+            lotties.add(generateRandomLotto());
+        }
+    }
+
+    private Lotto generateRandomLotto(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
 }
