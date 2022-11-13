4eeba2586d2820e2969155c9a8f2a230850b350f
kimJaeHyeon <kmr2644@gmail.com>
2022-11-13 21:47:32 +0900

    feat: 로또 번호 갯수 상수필드로 추가

--- src/main/java/lotto/constant/LottoConstant.java
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -3,4 +3,5 @@ package lotto.constant;
 public class LottoConstant {
     public static final int MAX_LOTTO_NUMBER = 45;
     public static final int LOW_LOTTO_NUMBER = 1;
+    public static final int LOTTO_SIZE = 7;
 }
