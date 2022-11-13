1d167289e1b4178f86f73fb17fe5ddd473aeb9d5
Limjongho <whdgh9873@icloud.com>
2022-11-13 16:23:14 +0900

    refactor(Prize): 열거형 상수 네이밍 오타 정정

--- src/main/java/domain/Prize.java
+++ src/main/java/domain/Prize.java
@@ -2,10 +2,10 @@ package domain;
 
 public enum Prize {
     firstPrize(1, 2000000000,"6개 일치 (2,000,000,000원)"),
-    secondPrice(2, 30000000,"5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    thirdPrice(3, 1500000,"5개 일치 (1,500,000원)"),
-    fourthPrice(4, 50000,"4개 일치 (50,000원)"),
-    fifthPrice(5, 5000,"3개 일치 (5,000원)"),
+    secondPrize(2, 30000000,"5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    thirdPrize(3, 1500000,"5개 일치 (1,500,000원)"),
+    fourthPrize(4, 50000,"4개 일치 (50,000원)"),
+    fifthPrize(5, 5000,"3개 일치 (5,000원)"),
     nothing(0, 0,"미당첨");
 
     public final int ranking;
