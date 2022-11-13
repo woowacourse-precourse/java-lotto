dea1ef8b2d8ab7d7fb7d75252358d0806e27fc62
KimYongJun <rladydwns129@naver.com>
2022-11-13 18:58:44 +0900

    refactor: 유효성 검사 시 boolean 리턴하는 것을 각 메소드에서 예외 처리 하는 것으로 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,36 +12,33 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (isSixNumbers(numbers) || isDuplicated(numbers) || isInRange(numbers)) {
-            throw new IllegalArgumentException();
-        }
+        isSixNumbers(numbers);
+        isDuplicated(numbers);
+        isInRange(numbers);
     }
 
     // TODO: 추가 기능 구현
 
-    private boolean isSixNumbers(List<Integer> numbers) {
+    private void isSixNumbers(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            return true;
+            throw new IllegalArgumentException();
         }
-        return false;
     }
-    private boolean isDuplicated(List<Integer> numbers) {
+    private void isDuplicated(List<Integer> numbers) {
         HashSet<Integer> NOT_DUPLICATED_NUMBERS = new HashSet<>();
         for (int i : numbers) {
             NOT_DUPLICATED_NUMBERS.add(i);
         }
         if (NOT_DUPLICATED_NUMBERS.size() != 6) {
-            return true;
+            throw new IllegalArgumentException();
         }
-        return false;
     }
 
-    private boolean isInRange(List<Integer> numbers) {
+    private void isInRange(List<Integer> numbers) {
         for (int i : numbers) {
             if (i < 1 || i > 45) {
-                return true;
+                throw new IllegalArgumentException();
             }
         }
-        return false;
     }
 }
