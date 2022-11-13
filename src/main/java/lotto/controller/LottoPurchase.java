a399c3fc1a23ecd8df07cb62203af6bd89a2245b
jipyo0106 <68218081+jipyo0106@users.noreply.github.com>
2022-11-14 05:40:29 +0900

    feat: 로또 구매 금액 유효성 검사

--- src/main/java/lotto/controller/LottoPurchase.java
+++ src/main/java/lotto/controller/LottoPurchase.java
@@ -3,6 +3,8 @@ package lotto.controller;
 import camp.nextstep.edu.missionutils.Console;
 
 public class LottoPurchase {
+    public static final int LOTTO_PRICE = 1000;
+
     private String getPurchaseAmountInput() {
         return Console.readLine();
     }
@@ -10,6 +12,12 @@ public class LottoPurchase {
     private void userInputIsNumber(String userInput) {
         if (!userInput.matches("[0-9]*")) {
             throw new IllegalArgumentException("[ERROR] 구매 금액은 숫자만 입력할수 있습니다.");
+        }
+    }
+
+    private void lottoPriceUnitsValidate(int purchaseAmount) {
+        if (purchaseAmount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 " + LOTTO_PRICE + "원 단위로 입력 가능합니다.");
         }
     }
 }
