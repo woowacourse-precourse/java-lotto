172c761166861d48bb6a343f7399cb368da9828c
jiwon1217 <koagjw@gmail.com>
2022-11-13 21:45:24 +0900

    feat: 당첨 번호가 중복되지 않도록 예외처리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -6,11 +6,13 @@ import java.util.List;
 public class Lotto {
     private static final String INVALID_LOTTO_SIZE = "[ERROR] 로또는 6개의 숫자로 이루어져야 합니다.";
     private static final String INVALID_NUMBER_RANGE = "[ERROR] 로또 번호의 범위는 1 ~ 45까지 입니다.";
+    private static final String DUPLICATE_LOTTO_NUMBER = "[ERROR] 로또의 6개의 수는 중복될 수 없습니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         isValidLottoSize(numbers);
         isValidNumberRange(numbers);
+        isDuplicateNumber(numbers);
         this.numbers = numbers;
     }
 
@@ -29,6 +31,16 @@ public class Lotto {
             if (number < 1 || number > 45) {
                 throw new IllegalArgumentException(INVALID_NUMBER_RANGE);
             }
+        }
+    }
+
+    private void isDuplicateNumber(List<Integer> numbers) {
+        int uniqueNumberCount = (int) numbers.stream()
+                .distinct()
+                .count();
+
+        if (numbers.size() != uniqueNumberCount) {
+            throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBER);
         }
     }
 }
