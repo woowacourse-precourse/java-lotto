0ac1a741810d716e775236e23ed5b65178322eae
kyliecamila <kyliecamila@naver.com>
2022-11-13 21:11:58 +0900

    [feat]당첨번호 입력받기(Lotto사용) 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,41 +10,51 @@ public class Application {
     private static final String ERROR_MESSAGE = "[ERROR]";
     private static final String PRICE_MESSAGE = "값이 1000으로 나누어지지 않습니다.";
     private static final String ANSWER_MESSAGE = "번호가 6개가 아닙니다";
+
     public static void main(String[] args) {
-        int price = inputScanner();
-        validatePrice(price);
+        int myPrice = priceInput();
+        List<Integer> lottoNumbers = lottoNumAnswerInput();
+        Lotto myLotto = new Lotto(lottoNumbers);
     }
 
-    public static void validatePrice(int price){
-        if (price%1000!=0) {
-            throw new IllegalArgumentException(ERROR_MESSAGE+PRICE_MESSAGE);
+    public static void validatePrice(int price) {
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + PRICE_MESSAGE);
         }
     }
-    public static int inputScanner(){
+
+    public static int priceInput() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(INPUT_MESSAGE);
-        return scanner.nextInt();
+        int price = scanner.nextInt();
+        validatePrice(price);
+        return price;
     }
-    public static void validateInputForm(String input){
-        int count=0;
+
+    public static void validateInputForm(String input) {
+        int count = 0;
         for (int charIndex = 0; charIndex < input.length(); charIndex++) {
-            if (input.charAt(charIndex)==',') {
+            if (input.charAt(charIndex) == ',') {
                 count++;
             }
         }
-        if(count!=5){
+        if (count != 5) {
             throw new IllegalArgumentException(ANSWER_MESSAGE);
         }
 
     }
-    public static List<Integer> LottoNumAnswerInput(){
+
+    public static List<Integer> lottoNumAnswerInput() {
         Scanner scanner = new Scanner(System.in);
         List<Integer> lottoNumbers = new ArrayList<>();
-        int lottoNum=1;
         System.out.println(LOTTO_ANSWER_INPUT_MESSAGE);
         String lottoInput = scanner.nextLine();
         validateInputForm(lottoInput);
+        String[] tempInput = lottoInput.split(",");
 
+        for (String num : tempInput) {
+            lottoNumbers.add(Integer.valueOf(num));
+        }
         return lottoNumbers;
     }
 }
