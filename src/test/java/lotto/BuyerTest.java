72751bab210555b33701a52f9c36dd85f856de7f
lee2963 <sodel2963@naver.com>
2022-11-13 20:54:17 +0900

    test: 구입 금액 1000원 단위가 아닐시 에러 발생 테스트

--- src/test/java/lotto/BuyerTest.java
+++ src/test/java/lotto/BuyerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,17 +14,14 @@ class BuyerTest {
     @DisplayName("구입 금액이 숫자가 아닐 시 예외 발생")
     @Test
     void inputBuyLottoMoney() {
-        assertThatThrownBy(() -> new Buyer().va)
+        assertThatThrownBy(() -> new Buyer().validateBuyLottoMoney("1000j"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-
+    @DisplayName("구입 금액이 1000원 단위가 아닐시 예외 발생")
     @Test
-    void calculateLotteryNum() {
-    }
-
-
-    @Test
-    void printBuyLotto() {
+    void inputBuyLottoMoneyIsNot1000() {
+        assertThatThrownBy(() -> new Buyer().validateBuyLottoMoney("2001"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
