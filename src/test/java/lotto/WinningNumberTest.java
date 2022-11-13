f6d7550bb6606ceb1a253e7cf6df3977a5d370c2
viyott-lover <jackk0616@gmail.com>
2022-11-13 22:15:05 +0900

    test: 기존 테스트 2개로 개수 확인과 중복검사 2가지로 분리

--- src/test/java/lotto/WinningNumberTest.java
+++ src/test/java/lotto/WinningNumberTest.java
@@ -11,9 +11,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class WinningNumberTest {
 
     @Test
-    @DisplayName("입력한 당첨 번호 및 보너스 번호가 조건에 부합하는지")
-    public void winningNumberTest() {
-        String lottoNumbers = "2,3,4,5,6";
+    @DisplayName("입력한 당첨 번호 및 보너스 번호의 개수가 맞는지")
+    public void winningNumberCountTest() {
+        String lottoNumbers = "1,2,3,4,5";
+        String bonusNumbers = "7";
+        WinningNumber wn = new WinningNumber(lottoNumbers, bonusNumbers);
+
+        assertThatThrownBy(() -> new WinningNumber(lottoNumbers, bonusNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Test
+    @DisplayName("입력한 당첨 번호 및 보너스 번호에 중복이 없는지")
+    public void winningNumberDuplicateTest() {
+        String lottoNumbers = "1,2,3,4,5,5";
         String bonusNumbers = "7";
         WinningNumber wn = new WinningNumber(lottoNumbers, bonusNumbers);
 
