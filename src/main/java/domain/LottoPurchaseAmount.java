aeb3ba48b81d9b4b53de2aa6cd85e69853012703
김선동 <mok03127@gmail.com>
2022-11-14 00:40:26 +0900

    feat: 로또 구입 금액 입력 기능 - 입력받은 숫자가 1000으로 나누어 떨어지지 않는 경우 예외 처리

--- src/main/java/domain/LottoPurchaseAmount.java
+++ src/main/java/domain/LottoPurchaseAmount.java
@@ -2,9 +2,8 @@ package domain;
 
 public class LottoPurchaseAmount {
     private static final int MIN_COST = 1000;
-    private static final String NOT_NUMBER_ERROR = "금액은 천 단위 숫자만 입력 가능합니다.";
-    private static final String NOT_DIVISIBLE_NUMBER_ERROR = "금액은 " + MIN_COST + "원 단위로 입력해야 합니다.";
-    private static final double DECIMAL = 1.0;
+    private static final String NOT_NUMBER_ERROR = "[ERROR] 금액은 천 단위 숫자만 입력 가능합니다.";
+    private static final String NOT_DIVISIBLE_BY_THOUSAND_ERROR = "[ERROR] 금액은 " + MIN_COST + "원 단위로 입력해야 합니다.";
 
     private final int amount;
 
@@ -15,7 +14,7 @@ public class LottoPurchaseAmount {
 
     private void validateInputMoney(int amount) {
         isAmountEqualsWithZero(amount);
-        
+        isDivisibleByThousand(amount);
     }
 
     private void isAmountEqualsWithZero(int amount) {
@@ -24,4 +23,9 @@ public class LottoPurchaseAmount {
         }
     }
 
+    private void isDivisibleByThousand(int amount) {
+        if (amount % MIN_COST != 0) {
+            throw new IllegalArgumentException(NOT_DIVISIBLE_BY_THOUSAND_ERROR);
+        }
+    }
 }
