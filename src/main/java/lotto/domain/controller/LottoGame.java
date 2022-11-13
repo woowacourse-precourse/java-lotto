91a96d7e0d8c24cf5a62d5428b47cb515269e7ed
Sehbeom <j3beom@gmail.com>
2022-11-14 01:44:00 +0900

    refactor(controller): 게임 운영 기능 전체 리팩토링
    과정 별로 나누어 함수 분리
    로또, 당첨 로또, 당첨 통계 멤버 필드 추가

--- src/main/java/lotto/domain/controller/LottoGame.java
+++ src/main/java/lotto/domain/controller/LottoGame.java
@@ -1,5 +1,6 @@
 package lotto.domain.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import lotto.constants.RankingInformation;
@@ -11,27 +12,46 @@ public class LottoGame {
 
     private LottoGuide lottoGuide;
     private LottoAnalyst lottoAnalyst;
+    private List<Lotto> lottos;
+    private WinningLotto winningLotto;
+    private Map<RankingInformation, Integer> statistics;
 
     public LottoGame() {
         lottoGuide = new LottoGuide();
         lottoAnalyst = new LottoAnalyst();
+        lottos = new ArrayList<>();
     }
 
     public void playLottoGame() {
+        issueLottosAndInform();
+
+        setWinningLotto();
+
+        makeStatistics();
+
+        informStatisticsAndRateOfReturn();
+    }
+
+    private void issueLottosAndInform() {
         int purchaseAmount = lottoGuide.getPurchaseAmount();
 
-        List<Lotto> lottos = LottoIssuer.issueLottos(purchaseAmount);
+        lottos = LottoIssuer.issueLottos(purchaseAmount);
 
         lottoGuide.informIssuedLottos(lottos);
+    }
 
+    private void setWinningLotto() {
         List<Integer> winningNumbers = lottoGuide.getWinningNumbers();
         int bonusNumber = lottoGuide.getBonusNumber();
 
-        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+    }
 
-        Map<RankingInformation, Integer> statistics = lottoAnalyst.makeWinningStatistics(lottos,
-                winningLotto);
+    private void makeStatistics() {
+        statistics = lottoAnalyst.makeWinningStatistics(lottos, winningLotto);
+    }
 
+    private void informStatisticsAndRateOfReturn() {
         double rateOfReturn = lottoAnalyst.calculateRateOfReturn();
 
         lottoGuide.informWinningStatistics(statistics);
