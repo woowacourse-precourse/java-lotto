9e6c7e624e92736c389da0d1330707d545d5dc5c
qdtjj4 <76ylo1@gmail.com>
2022-11-14 03:00:16 +0900

    feat: 로또 수량을 구하는 기능 구현
    금액이 부족하거나 남는 금액이 존재할 경우 예외처리함

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -20,4 +20,20 @@ public class Application {
             throw new IllegalArgumentException("[ERROR] 구입금액은 양의 정수여야 합니다.");
         }
     }
+
+    static int quantityOfLotto(int purchaseAmount) {
+        return purchaseAmount / 1000;
+    }
+
+    static void validateEnoughMoney(int purchaseAmount) {
+        if (purchaseAmount < 1000) {
+            throw new IllegalArgumentException("[ERROR] 구입금액이 부족합니다.");
+        }
+    }
+
+    static void validateChangeIsZero(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 남는 금액이 존재합니다.");
+        }
+    }
 }
