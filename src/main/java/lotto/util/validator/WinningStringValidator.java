7ab4fce397f6a444ff68e50044a7e2a17d24d09f
siwookim97 <tldn12gh@gmail.com>
2022-11-14 03:38:39 +0900

    test: 보너스 번호를 입력받는 기능 테스트
    보너스 번호를 입력받는 기능 테스트
     - 모든 예외는 IllegalArgumentException 이다
     - 보너스 번호까지 입력받고 당첨 번호와 중복되면 예외 발생
     - 숫자를 제외한 다른 입력값은 예외 발생

--- src/main/java/lotto/util/validator/WinningStringValidator.java
+++ src/main/java/lotto/util/validator/WinningStringValidator.java
@@ -1,26 +1,32 @@
 package lotto.util.validator;
 
 import lotto.util.Constants;
+import lotto.util.Utils;
 
 public class WinningStringValidator {
     private String winningString;
+    private String bonusString;
 
-    public WinningStringValidator(String winningString) {
+    public WinningStringValidator(String winningString, String bonusString) {
         this.winningString = winningString;
+        this.bonusString = bonusString;
         isLengthZero();
         isIntOrComma();
         isCorrectComma();
+        isIntBonus();
+        isBonusDuplicated();
+        isBonusCorrectRange();
     }
 
-    public void isLengthZero() {
+    private void isLengthZero() {
         try {
             winningString.charAt(0);
         } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException(Constants.ERROR_WINNING_NUMBER_LEGNTH_ZERO);
+            throw new IllegalArgumentException(Constants.ERROR_WINNING_NUMBER_LENGTH_ZERO);
         }
     }
 
-    public void isIntOrComma() {
+    private void isIntOrComma() {
         for (int index = 0; index < winningString.length(); index++) {
             if (winningString.charAt(index) == ',') {
                 continue;
@@ -32,12 +38,34 @@ public class WinningStringValidator {
         }
     }
 
-    public void isCorrectComma() {
+    private void isCorrectComma() {
         if (winningString.charAt(0) == ',') {
             throw new IllegalArgumentException(Constants.ERROR_WINNING_NUMBER_COMMA);
         }
         if (winningString.charAt(winningString.length() - 1) == ',') {
             throw new IllegalArgumentException(Constants.ERROR_WINNING_NUMBER_COMMA);
+        }
+    }
+
+    private void isIntBonus() {
+        try {
+            Utils.convertToInt(bonusString);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(Constants.ERROR_BONUS_NUMBER_INT);
+        }
+    }
+
+    private void isBonusDuplicated() {
+        if (Utils.isContain(bonusString, Utils.convertToList(winningString))) {
+            throw new IllegalArgumentException(Constants.ERROR_BONUS_NUMBER_DUPLICATED);
+        }
+    }
+
+    private void isBonusCorrectRange() {
+        int check = Utils.convertToInt(bonusString);
+
+        if ((check > 45) || (check < 1)) {
+            throw new IllegalArgumentException(Constants.ERROR_BONUS_NUMBER_Range);
         }
     }
 }
