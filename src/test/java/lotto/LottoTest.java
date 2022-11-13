289d768a990a5369793c54782df30fc37eef248c
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-13 14:33:22 +0900

    <test>(LottoTest.java): 중복 검사 테스트 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,15 @@
 package lotto;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.in;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +27,23 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호 중복 검사 테스트")
+    @Test
+    void 로또_번호_중복_검사() {
+
+        Domain domain = new Domain();
+        List<String> input = Arrays.asList("1", "2", "3", "4", "5", "5");
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            domain.validateOverlapping(input);
+        });
+    }
+    @DisplayName("로또 번호 중복 검사 테스트: 중복 없을 때")
+    @Test
+    void 로또_번호_중복_검사2() {
+        Domain domain = new Domain();
+        List<String> input = Arrays.asList("1", "2", "3", "4", "5", "6");
+
+        domain.validateOverlapping(input);
+    }
 }
