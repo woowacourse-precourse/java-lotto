0ed824d54649c911d4499c180ebea3ab863b343a
ohhyungsuh <ohhyungsuh@gmail.com>
2022-11-14 02:53:33 +0900

    feat: 당첨 번호 및 보너스 번호 입력 기능 추가

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -4,37 +4,43 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WinningNumber {
 
     private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요";
 
-    public List<String> initWinningNumber(){
-        List<String> winningNumber = getWinningNumber();
+    public List<Integer> initWinningNumber(){
+        List<Integer> winningNumber = getWinningNumber();
         return winningNumber;
     }
 
-    public String initBonusNumber(){
+    public Integer initBonusNumber(){
         return getBonusNumber();
     }
 
-    private static List<String> getWinningNumber(){
+    private static List<Integer> getWinningNumber(){
         System.out.println(INPUT_WINNING_NUMBER);
         String winningNumber = Console.readLine();
         return parsingWinningNumber(winningNumber);
     }
 
-    private static List<String> parsingWinningNumber(String winningNumber){
+    private static List<Integer> parsingWinningNumber(String winningNumber){
         String parsedWinningNumber = winningNumber.replaceAll(" ", "");
         List<String> winningNumberList = Arrays.asList(parsedWinningNumber.split(","));
-        return winningNumberList;
+        return convertType(winningNumberList);
     }
 
-    private static String getBonusNumber(){
+    private static List<Integer> convertType(List<String> winningNumberList){
+        List<Integer> result = winningNumberList.stream()
+                .map(Integer::valueOf).collect(Collectors.toList());
+        return result;
+    }
+
+    private static Integer getBonusNumber(){
         System.out.println(INPUT_BONUS_NUMBER);
-        String bonusNumber = Console.readLine();
-        return bonusNumber;
+        return Integer.parseInt(Console.readLine());
     }
 
 }
