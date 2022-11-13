7b9232d55b12fd7b7a2857975bbb2c83362dc47c
saint6839 <saint6839@gmail.com>
2022-11-14 00:20:36 +0900

    feat(domain) : 사용자가 입력한 구매 금액이 1000으로 나누어 떨이지지 않을 경우 예외를 검증하는 메서드 추가

--- src/main/java/lotto/domain/Amount.java
+++ src/main/java/lotto/domain/Amount.java
@@ -4,6 +4,14 @@ public class Amount {
     int value;
 
     public Amount(int value) {
+        validateCanDivideIntoOneThousand(value);
         this.value = value;
+    }
+
+    private void validateCanDivideIntoOneThousand(int amount) {
+        if (amount % 1000 == 0) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위로만 입력 가능합니다.");
     }
 }
