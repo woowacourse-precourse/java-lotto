d191079198840d220b79f73c1110a34e429f574f
Woojin Lee <dhkep03@gmail.com>
2022-11-13 18:02:24 +0900

    feat(UserInterface): 구매 금액 입력

--- src/main/java/lotto/ui/UserInterface.java
+++ src/main/java/lotto/ui/UserInterface.java
@@ -1,4 +1,13 @@
 package lotto.ui;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class UserInterface {
+    private static final String INPUT_MONEY_ANNOUNCEMENT = "구입금액을 입력해 주세요.";
+
+    public static String getInputMoney() {
+        System.out.println(INPUT_MONEY_ANNOUNCEMENT);
+        String inputMoneyRaw = Console.readLine();
+        return inputMoneyRaw;
+    }
 }
