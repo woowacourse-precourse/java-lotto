7ef99a27a6384fc80779c6b6b073e8db3f2b6303
be-student <presentsong@naver.com>
2022-11-13 15:45:45 +0900

    feat(LottoCreator): 로또 생성기 기능 추가

--- src/main/java/lotto/constant/LottoConstants.java
+++ src/main/java/lotto/constant/LottoConstants.java
@@ -2,12 +2,13 @@ package lotto.constant;
 
 public enum LottoConstants {
     LOTTO_START_INCLUSIVE(1),
-    LOTTO_END_INCLUSIVE(45);
+    LOTTO_END_INCLUSIVE(45),
+    LOTTO_LENGTH(6);
 
     private final int value;
 
     LottoConstants(int value) {
-        this.value=value;
+        this.value = value;
     }
 
     public int value() {
---
+++ src/main/java/lotto/domain/LottoCreator.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import lotto.constant.LottoConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LottoCreator {
+    private final RandomUniqueNumbers random;
+
+    public LottoCreator(RandomUniqueNumbers random) {
+        this.random = random;
+    }
+
+    public Lottos createLottos(Money money) {
+        int count = money.ableToBuy();
+        List<Lotto> lottos = new ArrayList<>();
+        IntStream.range(0, count)
+                .forEach(ignored -> addLotto(lottos));
+        return new Lottos(lottos);
+    }
+
+    private void addLotto(List<Lotto> lottos) {
+        List<Integer> numbers = random.pickUniquesInRange(
+                LottoConstants.LOTTO_START_INCLUSIVE.value(),
+                LottoConstants.LOTTO_END_INCLUSIVE.value(),
+                LottoConstants.LOTTO_LENGTH.value());
+        lottos.add(new Lotto(numbers));
+    }
+}
