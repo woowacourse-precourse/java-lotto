675d9b87e9505983a6180e82d5114339a731988a
jipyo0106 <68218081+jipyo0106@users.noreply.github.com>
2022-11-14 06:07:49 +0900

    feat: 당첨 로또 번호 유효성 검사 기능 구현

--- src/main/java/lotto/controller/EnterWinningNumber.java
+++ src/main/java/lotto/controller/EnterWinningNumber.java
@@ -6,4 +6,10 @@ public class EnterWinningNumber {
     private String getWinningNumberInput() {
         return Console.readLine();
     }
+
+    private void isEachNumberBetween1AND45(int number) {
+        if (number > 45 || number < 1) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
 }
