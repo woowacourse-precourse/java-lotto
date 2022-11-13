9689779846e578477ce287b41c8ee219e77ab8dc
whdcks420 <whdcks420@gmail.com>
2022-11-13 22:36:02 +0900

    refact(validator): 보너스 번호 입력기능 수정

--- src/main/java/lotto/validator/BonusNumberValidator.java
+++ src/main/java/lotto/validator/BonusNumberValidator.java
@@ -1,2 +1,19 @@
-package lotto.validator;public class BonusNumberValidator {
+package lotto.validator;
+
+import lotto.domain.Lotto;
+
+import static lotto.validator.ExceptionStatus.*;
+
+public class BonusNumberValidator {
+
+    public static void validate(String bonusNumber, Lotto winLotto) {
+        LottoValidator.validateCorrectRange(Integer.parseInt(bonusNumber));
+        validateContainBonusNumber(bonusNumber, winLotto);
+    }
+
+    private static void validateContainBonusNumber(String bonusNumber, Lotto winLotto) {
+        if (winLotto.getNumbers().contains(Integer.parseInt(bonusNumber))) {
+            throw new IllegalArgumentException(NO_CONTAIN_WINNING_LOTTO_NUMBERS.getMessage());
+        }
+    }
 }
