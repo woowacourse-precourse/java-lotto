8d31213e1bd8a5b095aa22af68222939c528a6e4
Ilchul Kim <huj0159@gmail.com>
2022-11-13 16:23:28 +0900

    feat: read userWinNumber and return Lotto class if number is valid

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,7 +1,7 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
@@ -12,10 +12,32 @@ public class LottoController {
         validateUserMoney(userMoney);
     }
 
+    public Lotto readWinNumber(String userWinNumber) {
+        StringTokenizer st = new StringTokenizer(userWinNumber, ",");
+        ArrayList<Integer> winNumber = new ArrayList<>();
+
+        while (st.hasMoreTokens()) {
+            String singleNumber = st.nextToken();
+            isDigit(singleNumber);
+            validateWinNumber(singleNumber);
+            winNumber.add(Integer.parseInt(singleNumber));
+        }
+
+        Collections.sort(winNumber);
+        return new Lotto(winNumber);
+    }
+
     private void isDigit(String userInput) {
         String pattern = "^[0-9]*$";
         if (!Pattern.matches(pattern, userInput)) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다");
+        }
+    }
+
+    private void validateWinNumber(String userInput) {
+        int incomingNumber = Integer.parseInt(userInput);
+        if (incomingNumber < 1 || incomingNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1과 45사이여야 합니다");
         }
     }
 
