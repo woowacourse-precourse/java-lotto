4cd4e132f13da57e158a4e5cb29e64a74b619867
210_reverof <sheltonwon@gmail.com>
2022-11-13 22:12:26 +0900

    feat: add lotto buying function (function2)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,16 +1,36 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
 
     public static int getLottoCount(String input) {
-        if (!input.matches("[0-9]+")) throw new IllegalArgumentException("[ERROR] 입력 금액은 숫자로만 이루어져야 합니다.");
+        if (!input.matches("[0-9]+")) {
+            throw new IllegalArgumentException("[ERROR] 입력 금액은 숫자로만 이루어져야 합니다.");
+        }
 
         int money = Integer.parseInt(input);
-        if (money % 1000 != 0) throw new IllegalArgumentException("[ERROR] 입력 금액은 1000원 단위로 입력해야 합니다.");
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 입력 금액은 1000원 단위로 입력해야 합니다.");
+        }
 
         return money / 1000;
+    }
+
+    public static List<Lotto> buyLotto(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers);
+            lottos.add(new Lotto(numbers));
+        }
+
+        return lottos;
     }
 }
