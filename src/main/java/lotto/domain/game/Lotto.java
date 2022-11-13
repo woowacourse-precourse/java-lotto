b358554f7f5e7241c42d032f25baac303bf64ae2
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-14 06:45:10 +0900

    feat&fix(game/Lotto.java): 당첨번호 6개에 중복이 없는지 확인하는 함수 구현 & validateSizeWinningNumber 함수 수정

--- src/main/java/lotto/domain/game/Lotto.java
+++ src/main/java/lotto/domain/game/Lotto.java
@@ -10,12 +10,19 @@ public class Lotto {
 
     public Lotto(List<Integer> winningNumbers) {
         validateSizeWinningNumber(winningNumbers);
+        validateDuplicate(winningNumbers);
         this.numbers = winningNumbers;
     }
 
-    private void validateSizeWinningNumber(List<Integer> numbers) {
+    public void validateSizeWinningNumber(List<Integer> numbers) {
         if (numbers.size() != LOTTO_COUNT) {
-            throw new IllegalArgumentException(NO_MATCH_LOTTO_COUNT.print());
+            throw new IllegalArgumentException(NO_MATCH_LOTTO_COUNT.print(LOTTO_COUNT));
+        }
+    }
+
+    public void validateDuplicate(List<Integer> numbers){
+        if(numbers.size() != numbers.stream().distinct().count()){
+            throw new IllegalArgumentException(DUPLICATION_WINNING_NUMBER.print(LOTTO_COUNT));
         }
     }
 
