d067dc2274a755163998e04e7c3b56d7eb2cdb79
UngHeung <t01047377477@gmail.com>
2022-11-14 05:32:36 +0900

    feat : (Lotto) 에러 메시지 출력 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 public class Lotto {
 	private final List<Integer> numbers;
+	private String errorMessage = "[ERROR] ";
 
 	public Lotto(List<Integer> numbers) {
 		validate(numbers);
@@ -16,21 +17,33 @@ public class Lotto {
 	
 	private void validate(List<Integer> numbers) {
 		if (numbers.size() != 6) {
-			throw new IllegalArgumentException();
+			errorMessage += "6개의 번호를 입력해 주세요.";
+			throwException();
 		}
 
 		Set<Integer> duplicateCheck = new HashSet<>();
 		
 		for (int number : numbers) {
 			if(number < 1 || 45 < number) {
-				throw new IllegalArgumentException();
+				errorMessage += "1 이상 45 이하의 숫자를 선택해 주세요.";
+				throwException();
 			}
 			
 			duplicateCheck.add(number);
 		}
 		
 		if (duplicateCheck.size() != 6) {
+			errorMessage += "번호가 중복 되지 않도록 선택해 주세요.";
+			throwException();
+		}
+	}
+	
+	private void throwException() {
+		try {
 			throw new IllegalArgumentException();
+		} catch(IllegalArgumentException e) {
+			System.out.println(errorMessage);
+			return;
 		}
 	}
 	
