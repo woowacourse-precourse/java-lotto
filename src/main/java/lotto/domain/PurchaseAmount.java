61153869f532d1e8ff9fdd31ce5d7c1390483033
yubin <dbqls2821@gmail.com>
2022-11-13 12:21:41 +0900

    refactor(PurchaseAmount): 구매 금액에 따른 로또의 갯수를 구하는 메소드 추가

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.domain.constant.BoundaryValue;
 import lotto.domain.io.Multiple1000Validator;
 import lotto.domain.io.NumberValidator;
 
@@ -8,15 +9,21 @@ public class PurchaseAmount {
   private final int amount;
 
   public PurchaseAmount(String amount) {
-    this.amount = validate(amount);
+    validate(amount);
+    this.amount = getLottoNum(amount);
   }
 
-  private int validate(String amount) {
+  private void validate(String amount) {
     NumberValidator numberValidator = new NumberValidator();
     Multiple1000Validator multiple1000Validator = new Multiple1000Validator();
 
     int input = numberValidator.validate(amount);
     multiple1000Validator.validate(input);
-    return input;
+  }
+
+  private int getLottoNum(String input) {
+    int money = Integer.parseInt(input);
+    money /= BoundaryValue.LOTTO_PRICE.getBoundaryValue();
+    return money;
   }
 }
