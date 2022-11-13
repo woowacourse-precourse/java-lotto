34d79e0417fc97c5dafc225c033c3e995f73098c
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-11-13 19:37:11 +0900

    feat: 당첨번호 입력 시 예외 처리 구현

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -1,6 +1,12 @@
 package lotto;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Exception {
+    public static final int MIN_WINNING_NUMBER = 1;
+    public static final int MAX_WINNING_NUMBER = 45;
+    private static final int WINNING_NUMBER_SIZE = 6;
 
 
     public void checkPurchaseInput(String str) {
@@ -12,11 +18,26 @@ public class Exception {
             throw new IllegalArgumentException(ExceptionType.INVALID_PURCHASE_COST.getErrorMsg());
         }
     }
+    public void checkWinningNumberInput(String numbers) {
+        String[] winningNumbers = numbers.split(",");
 
-    private boolean isValidPurchaseCost(String str) {
-        int cost = Integer.parseInt(str);
+        if (!isValidLength(winningNumbers)){
+            throw new IllegalArgumentException(ExceptionType.INVALID_LENGTH.getErrorMsg());
+        }
 
-        return (cost % 1000) == 0;
+        if (haveDuplicatedNumber(winningNumbers)) {
+            throw new IllegalArgumentException(ExceptionType.DUPLICATED_NUMBER.getErrorMsg());
+        }
+
+        for (String winningNumber : winningNumbers) {
+            if (!isNumberFormat(winningNumber)) {
+                throw new IllegalArgumentException(ExceptionType.INVALID_FORMAT.getErrorMsg());
+            }
+
+            if (!isValidNumberRange(winningNumber)) {
+                throw new IllegalArgumentException(ExceptionType.INVALID_RANGE.getErrorMsg());
+            }
+        }
     }
 
     private boolean isNumberFormat(String str) {
@@ -30,6 +51,36 @@ public class Exception {
 
     private boolean isNumber(char c) {
         return '0' <= c && c <= '9';
+    }
+
+    private boolean isValidPurchaseCost(String str) {
+        int cost = Integer.parseInt(str);
+
+        return (cost % 1000) == 0;
+    }
+
+    private boolean isValidLength(String[] str) {
+        return str.length == WINNING_NUMBER_SIZE;
+    }
+
+    private boolean haveDuplicatedNumber(String[] winningNumbers) {
+        Set<String> storedNumbers = new HashSet<>();
+
+        for (String winningNumber : winningNumbers) {
+            if (storedNumbers.contains(winningNumber)) {
+                return true;
+            }
+
+            storedNumbers.add(winningNumber);
+        }
+
+        return true;
+    }
+
+    private boolean isValidNumberRange(String str) {
+        int number = Integer.parseInt(str);
+
+        return MIN_WINNING_NUMBER <= number && number <= MAX_WINNING_NUMBER;
     }
 
 }
--- src/main/java/lotto/ExceptionType.java
+++ src/main/java/lotto/ExceptionType.java
@@ -12,10 +12,31 @@ public enum ExceptionType {
         public String getErrorMsg (){
             return INVALID_COST_ERROR_MSG;
         }
+    },
+
+    INVALID_LENGTH {
+        public String getErrorMsg (){
+            return INVALID_LENGTH_ERROR_MSG;
+        }
+    },
+
+    DUPLICATED_NUMBER {
+        public String getErrorMsg (){
+            return DUPLICATED_NUMBER_ERROR_MSG;
+        }
+    },
+
+    INVALID_RANGE {
+        public String getErrorMsg (){
+            return INVALID_RANGE_ERROR_MSG;
+        }
     };
 
-    public static final String INVALID_FORMAT_ERROR_MSG ="입력형식이 맞지 않습니다.";
+    public static final String INVALID_FORMAT_ERROR_MSG ="당첨번호는 총 6개여야 합니다.";
     public static final String INVALID_COST_ERROR_MSG ="입력이 1000의 배수가 아닙니다.";
+    private static final String INVALID_LENGTH_ERROR_MSG = "입력의 길이가 맞지 않습니다.";
+    private static final String DUPLICATED_NUMBER_ERROR_MSG = "당첨번호에 중복된 수가 있으면 안됩니다.";
+    private static final String INVALID_RANGE_ERROR_MSG = "입력의 범위가 1 ~ 45가 아닙니다.";
 
     public abstract String getErrorMsg();
 }
