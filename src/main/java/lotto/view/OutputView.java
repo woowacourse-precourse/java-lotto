47165ba8c41058f7a3f319b8ebe2438d8b74baf2
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-13 22:15:08 +0900

    feat(OutputView): 당첨 개수 기준 오름차순으로 내역을 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,7 @@ import lotto.domain.Lotties;
 import lotto.domain.Lotto;
 import lotto.domain.LottoRank;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -20,6 +21,21 @@ public class OutputView {
                     .collect(Collectors.toList());
 
             System.out.println(collect);
+        }
+    }
+
+
+    public static void printWinResult(Map<LottoRank, Integer> winLottoInfo) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        List<LottoRank> collect = winLottoInfo.keySet()
+                .stream()
+                .sorted(Comparator.comparingInt(LottoRank::matchCount))
+                .collect(Collectors.toList());
+
+        for (LottoRank rank : collect) {
+            System.out.println(rank + "-" + winLottoInfo.get(rank) + "개");
         }
     }
 }
