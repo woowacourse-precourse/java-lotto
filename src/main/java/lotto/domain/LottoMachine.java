c416b3e7a92db4b5b203e1e346c2016bd24b8c9b
Woojin Lee <dhkep03@gmail.com>
2022-11-13 18:34:08 +0900

    feat(LottoMachine): 구매 금액에 맞는 로또 발행 구현 완료

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,7 +1,12 @@
 package lotto.domain;
 
 import static lotto.constant.LottoRule.AMOUNT_OF_ONE_LOTTO;
+import static lotto.constant.LottoRule.COUNT_OF_NUMBERS;
+import static lotto.constant.LottoRule.END_NUMBER;
+import static lotto.constant.LottoRule.START_NUMBER;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
@@ -13,5 +18,22 @@ public class LottoMachine {
 
     private static int getCountOfLottos(int inputMoney) {
         return inputMoney / AMOUNT_OF_ONE_LOTTO;
+    }
+
+    private static List<Lotto> publishLottosByCount(int countOfLottos) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int count = 0; count < countOfLottos; count++) {
+            List<Integer> randomNumbers = getRandomNumbers();
+            Lotto lotto = new Lotto(randomNumbers);
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
+
+    private static List<Integer> getRandomNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                START_NUMBER, END_NUMBER, COUNT_OF_NUMBERS
+        );
+        return numbers;
     }
 }
