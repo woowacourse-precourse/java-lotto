52816c7326b4c1a2457a474bb5b45ddeb20a5b24
lvalentine6 <ysr1227@naver.com>
2022-11-13 15:55:09 +0900

    feat : 클래스 뼈대 추가

---
+++ src/main/java/lotto/Input.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public static String userInput() {
+        String input = Console.readLine();
+        return input;
+    }
+
+
+}
