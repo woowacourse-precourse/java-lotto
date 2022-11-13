1ff8ece2554f9fd55689044f9f286dd74ff6c5f8
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-13 13:41:58 +0900

    feat(lotto) : 에러 메시지 추가

--- src/main/java/lotto/constants/ErrorMessages.java
+++ src/main/java/lotto/constants/ErrorMessages.java
@@ -1,5 +1,8 @@
 package lotto.constants;
 
 public class ErrorMessages {
-    public static final String PRICE = "[ERROR] : 로또 금액을 1,000원 단위로 입력하세요";
+    public static final String PRICE = "[ERROR] 로또 금액을 1,000원 단위로 입력하세요.";
+    public static final String RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String DUPLICATE = "[ERROR] 로또 번호에 중복이 존재합니다.";
+    public static final String SIZE = "[ERROR] 로또 번호의 개수가 6개가 아닙니다.";
 }
