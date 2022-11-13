4f50452551caee882349d6ddce3c2b3eec75aeab
ykmxxi <ykmxxi97@gmail.com>
2022-11-13 16:50:31 +0900

    feat: 가이드 메시지 출력 기능 구현

---
+++ src/main/java/lotto/view/print/PrintGuideMessage.java
@@ -0,0 +1,23 @@
+package lotto.view.print;
+
+public class PrintGuideMessage {
+    public static void printStartGuide() {
+        System.out.println(GuideMessage.AMOUNT_INPUT_MESSAGE.getMessage());
+    }
+
+    public static void printPurchaseGuide(int quantity) {
+        System.out.printf(GuideMessage.PURCHASE_MESSAGE.getMessage(), quantity);
+    }
+
+    public static void printWinningsGuide() {
+        System.out.println(GuideMessage.WINNINGS_INPUT_MESSAGE.getMessage());
+    }
+
+    public static void printBonusGuide() {
+        System.out.println(GuideMessage.BONUS_INPUT_MESSAGE.getMessage());
+    }
+
+    public static void printStatisticsGuide() {
+        System.out.println(GuideMessage.STATISTICS_MESSAGE.getMessage());
+    }
+}
