2872031a611d766e85a0db1027d62b8c42fa682c
Kimhyeyun <cqqwer@gmail.com>
2022-11-13 20:10:27 +0900

    Feat: 결과를 저장할 클래스 생성

---
+++ src/main/java/lotto/domain/Result.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+
+public class Result {
+    private final Map<Rank, Integer> result = new EnumMap<Rank, Integer>(Rank.class);
+
+    public Result() {
+        initResult();
+    }
+
+    private void initResult() {
+        for (Rank rank : Rank.values()) {
+            result.put(rank, 0);
+        }
+    }
+}
