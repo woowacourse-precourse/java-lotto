b4e643609b357cb3a22c1dac2eee139a56e66b7c
Kayoung <rkdkdudjd@gmail.com>
2022-11-14 02:26:21 +0900

    refactor(Lotto): 상수를 static으로 선언
    + 예외메시지 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,13 +6,17 @@ import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
-    private final int LOTTO_SIZE = 6;
-    private final int MIN_NUMBER = 1;
-    private final int MAX_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
     private void validate(List<Integer> numbers) {
@@ -24,21 +28,21 @@ public class Lotto {
     private void validateUniqueness(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
         if (uniqueNumbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException("로또 번호에 중복된 숫자가 있습니다.");
+            throw new IllegalArgumentException("중복된 숫자가 있습니다.");
         }
     }
 
     private void validateRangeOfNumbers(List<Integer> numbers) {
         numbers.forEach(number -> {
             if (number < MIN_NUMBER || number > MAX_NUMBER) {
-                throw new IllegalArgumentException("범위를 벗어나는 로또 번호가 있습니다.");
+                throw new IllegalArgumentException("범위를 벗어나는 번호가 있습니다.");
             }
         });
     }
 
     private void validateInputSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException("로또 번호의 개수가 올바르지 않습니다.");
+            throw new IllegalArgumentException("번호의 개수가 올바르지 않습니다.");
         }
     }
 }
