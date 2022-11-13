b5ea45fe935e5f091e4a9646eea03ab088fbcf50
aksmf1442 <aksmf1442@naver.com>
2022-11-13 20:12:57 +0900

    test(로또): 돈을 통해서 몇 개의 로또 용지를 구매할 수 있는지 테스트 구현

---
+++ src/main/java/lotto/domain/seller/Country.java
@@ -0,0 +1,9 @@
+package lotto.domain.seller;
+
+public class Country implements Seller{
+
+    @Override
+    public int findLottoCountByMoney(int money) {
+        return 0;
+    }
+}
---
+++ src/main/java/lotto/domain/seller/Seller.java
@@ -0,0 +1,6 @@
+package lotto.domain.seller;
+
+public interface Seller {
+
+    int findLottoCountByMoney(int money);
+}
