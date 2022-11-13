217f5e65bf0f7fb7a23501be63a0b65f845f413b
박준용 <jo101044@naver.com>
2022-11-13 20:59:31 +0900

    test: "LottoTest 테스트 코드 추가"
    추가된 문자열을 입력받는 생성자와 관련된 테스트 코드를 추가 하였다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -25,4 +26,18 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @DisplayName("로또 번호에 문자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedCharacter() {
+        assertThatThrownBy(() -> new Lotto("1,2,3,4,5,a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 입력값과 같은지 비교한다.")
+    @Test
+    void compareLottoNumber() {
+        assertThat(new Lotto(List.of(1, 2, 3, 4, 5, 6))
+                .getNumbers()).contains(1, 2, 3, 4, 5, 6);
+    }
 }
