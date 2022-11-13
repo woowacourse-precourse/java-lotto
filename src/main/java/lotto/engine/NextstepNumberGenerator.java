a15ebb0cb75997b7ddda728c9b371b40030922b6
UJ15 <dbwogml15@naver.com>
2022-11-14 05:04:31 +0900

    refactor : change access specifier private -> public

--- src/main/java/lotto/engine/NextstepNumberGenerator.java
+++ src/main/java/lotto/engine/NextstepNumberGenerator.java
@@ -5,9 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class NextstepNumberGenerator implements NumberGenerator {
-    private static final int START_RANGE_NUMBER = 1;
-    private static final int END_RANGE_NUMBER = 45;
-    private static final int COUNT = 6;
+    public static final int START_RANGE_NUMBER = 1;
+    public static final int END_RANGE_NUMBER = 45;
+    public static final int COUNT = 6;
 
     @Override
     public List<Integer> generate() {
