1781691a4b1ec831774a87c875d4c62c6ddfe12c
Heesom <heesom.hs@gmail.com>
2022-11-13 19:10:19 +0900

    test(domain) : 로또 번호 자동으로 발행하는 기능 테스트 추가

---
+++ src/test/java/lotto/Domain/IssuanceLottoTest.java
@@ -0,0 +1,34 @@
+package lotto.Domain;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Set;
+
+public class IssuanceLottoTest {
+    SoftAssertions softAssertions = new SoftAssertions();
+    IssuanceLotto issuanceLotto = new IssuanceLotto(5);
+    List<Set<Integer>> lottos = issuanceLotto.getIssuanceLotto();
+
+    @DisplayName("로또 발행 개수가 정확한지 테스트")
+    @Test
+    void createLottosSize() {
+        softAssertions.assertThat(lottos.size())
+                .as("로또 발행 개수")
+                .isEqualTo(5);
+        softAssertions.assertAll();
+    }
+
+    @DisplayName("로또안의 숫자 개수가 정확한지 테스트")
+    @Test
+    void createLottoNumSize() {
+        for(Set<Integer> lotto : lottos) {
+        softAssertions.assertThat(lotto.size())
+                .as("로또 안 숫자 개수")
+                .isEqualTo(6);
+        }
+        softAssertions.assertAll();
+    }
+}
