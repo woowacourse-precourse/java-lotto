0a5044ba77fa6955ef40581b5ff48d06c63d8168
leesky <leehaneul0623@gmail.com>
2022-11-13 18:57:51 +0900

    feat(lottoController) : 유효성 검사 구현
    입력값의 잘못된 값이 들어오는 것을 막기 위헤서 유효성 검사 구현 :
     - 콤마를 이용해서 숫자를 제대로 입력했는지 검사
     - 숫자가 아닌 값을 검사

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -16,4 +16,18 @@ public class LottoController {
     private void getResult() {
 
     }
+
+    private void validateAnswerInput(String input) {
+        try {
+            for (String str : input.split(",")) {
+                Integer.parseInt(str);
+            }
+        } catch (Exception err) {
+            throw new IllegalArgumentException("콤마(,)를 이용해 6개의 숫자를 입력해주세요.");
+        }
+    }
+
+    private void validateNumber(String input) {
+        if (!input.matches("^[0-9]*$")) throw new IllegalArgumentException("숫자만 입력하실 수 있습니다.");
+    }
 }
