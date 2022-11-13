80758a3daeb681cfdccc98fadc7c0fa3bce5db30
anneu <b.imnuj@gmail.com>
2022-11-14 00:47:26 +0900

    test: 로또와 보너스 번호를 비교해 일치하는지 구하는 기능 테스트

--- src/test/java/lotto/BonusTest.java
+++ src/test/java/lotto/BonusTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -14,5 +15,13 @@ class BonusTest {
 
         assertThatThrownBy(() -> new Bonus(3, winningLotto))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호와 보너스 번호를 비교해 일치하는지 확인한다.")
+    @Test
+    void isMatch() {
+        Bonus bonus = new Bonus(7, new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        boolean actual = bonus.isMatch(List.of(7, 8, 9, 10, 11, 12));
+        assertThat(actual).isTrue();
     }
 }
