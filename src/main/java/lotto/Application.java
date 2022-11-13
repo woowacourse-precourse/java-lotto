96ead6f1bd8ec7d5d0f1af3296eef554618afc52
kyliecamila <kyliecamila@naver.com>
2022-11-13 17:54:28 +0900

    [feat] 로또 구매 입력 받기 + 예외처리 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import java.util.Scanner;
+
 public class Application {
+    private static final String INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    private static final String PRICE_MESSAGE = "값이 1000으로 나누어지지 않습니다.";
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int price = inputScanner();
+        validatePrice(price);
+    }
+
+    public static void validatePrice(int price){
+        if (price%1000!=0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE+PRICE_MESSAGE);
+        }
+    }
+    public static int inputScanner(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(INPUT_MESSAGE);
+        return scanner.nextInt();
     }
 }
