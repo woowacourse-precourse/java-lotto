6b46a785919d9b1432bc61d7c910921277c08ef4
kevstevie <jianna442@gmail.com>
2022-11-13 18:43:13 +0900

    refactor: 인스턴스 static 으로 수정

--- src/main/java/lotto/Calculator.java
+++ src/main/java/lotto/Calculator.java
@@ -2,10 +2,11 @@ package lotto;
 
 public class Calculator {
 
-    long income = 0;
-    long[] placesCount = new long[6];
+    static long income = 0;
+    static long[] placesCount = new long[6];
 
     private Calculator(){}
+
 
 
 }
