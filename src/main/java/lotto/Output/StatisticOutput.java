637e6511c57aa8ed5caf8a802cdbd5362e28497f
Heesom <heesom.hs@gmail.com>
2022-11-13 22:36:15 +0900

    feat(StatisticOutput) : 당첨 수량 출력하는 기능 추가

--- src/main/java/lotto/Output/StatisticOutput.java
+++ src/main/java/lotto/Output/StatisticOutput.java
@@ -1,11 +1,11 @@
 package lotto.Output;
 
 public class StatisticOutput {
-    public static void getStatisticOutput(int three, int four, int five_NoBonus, int five_YesBonus, int six) {
+    public static void getStatisticOutput(int three, int four, int five, int six_YesBonus, int six) {
         PrintOutput.WIN_THREE.printMessage(three);
         PrintOutput.WIN_FOUR.printMessage(four);
-        PrintOutput.WIN_FIVE_NOBALL.printMessage(five_NoBonus);
-        PrintOutput.WIN_FIVE_YESBALL.printMessage(five_YesBonus);
+        PrintOutput.WIN_FIVE_NOBALL.printMessage(five);
+        PrintOutput.WIN_FIVE_YESBALL.printMessage(six_YesBonus);
         PrintOutput.WIN_SIX.printMessage(six);
     }
 }
