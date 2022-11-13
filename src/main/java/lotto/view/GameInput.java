f9c4d96ba8eab3c9dae7098551168cb12c8caa0d
Namyunsuk <yunsuk0328@naver.com>
2022-11-13 18:58:13 +0900

    feat(GameInput): 사용자로부터 값을 입력받는 기능 구현

--- src/main/java/lotto/view/GameInput.java
+++ src/main/java/lotto/view/GameInput.java
@@ -1,5 +1,27 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class GameInput {
 
+    public static void getUserMoneyInput() {
+        GameOutput.printMoneyInput();
+        String userInput = Console.readLine();
+        //예외처리
+        //값 저장 추가 예정
+    }
+
+    public static void getUserNumberInput() {
+        GameOutput.printNumberInput();
+        String userInput = Console.readLine();
+        //예외처리
+        //값 저장 추가 예정
+    }
+
+    public static void getUserBonusNumberInput() {
+        GameOutput.printBonusNumberInput();
+        String userInput = Console.readLine();
+        //예외처리
+        //값 저장 추가 예정
+    }
 }
