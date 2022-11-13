ac9128afbaee21cf145f048800fc1daab9a63a8d
Jihoon9809 <dkdb521@naver.com>
2022-11-13 12:28:24 +0900

    고정된 구입금액을  사용자로 부터 입력받은 금액으로 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,17 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
 
-        Buy buy = new Buy(8000);
-        buy.printRandomLotto();
+        System.out.println("구입금액을 입력해 주세요.");
+        int userPrice = Integer.parseInt(Console.readLine());
 
+        Buy userBuy = new Buy(userPrice);
+        userBuy.printRandomLotto();
+
+        System.out.println("당첨 번호를 입력해 주세요.");
     }
 }
--- src/main/java/lotto/Buy.java
+++ src/main/java/lotto/Buy.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
