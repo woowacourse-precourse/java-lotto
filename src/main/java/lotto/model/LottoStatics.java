1004c983b0a8c9bead14d85e4a1eba7e7d892407
marinesnow34 <m4rinesnow34@gmail.com>
2022-11-13 14:23:06 +0900

    feat: 통계 처리 클래스 생성

--- src/main/java/lotto/model/LottoStatics.java
+++ src/main/java/lotto/model/LottoStatics.java
@@ -1,2 +1,20 @@
-package lotto.model;public class LottoStatics {
+package lotto.model;
+
+import java.util.HashMap;
+
+public class LottoStatics {
+    private static final int DEFAULT_WINNING = 0;
+    private HashMap<Winning, Integer> staticsInfo;
+
+    public LottoStatics() {
+        initialMap();
+    }
+
+    private void initialMap() {
+        HashMap<Winning, Integer> staticsInfo = new HashMap<>();
+        for (Winning value : Winning.values()) {
+            staticsInfo.put(value, DEFAULT_WINNING);
+        }
+        this.staticsInfo = staticsInfo;
+    }
 }
