62e6ba97fda1817dcc912523215f4d0aca20c5f4
hyeonseog-won <smreo75@gmail.com>
2022-11-13 18:57:56 +0900

    feat: 사용자의 입력을 받고 Integer로 변환하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int money = toInteger(Console.readLine());
+
+    }
+
+    private static int toInteger(String input) {
+        int money = Integer.parseInt(input);
+        if (money < 1000) {
+            throw new IllegalArgumentException("[ERROR] 1,000원 이상을 입력해 주세요.");
+        }
+
+        if (money%1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000원으로 나누어 떨어지는 금액만 입력해 주세요.");
+        }
+
+        return money;
     }
 }
