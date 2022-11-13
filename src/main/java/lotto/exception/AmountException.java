4a7a993e7c746f8076555841aa2bf3401f301a62
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-13 10:21:10 +0900

    feat(exception): 구입금액이 1000원 단위가 아닌 경우 예외 기능 구현

--- src/main/java/lotto/exception/AmountException.java
+++ src/main/java/lotto/exception/AmountException.java
@@ -9,4 +9,9 @@ public class AmountException {
     private static boolean isNumeric(String input) {
         return input.chars().allMatch(Character::isDigit);
     }
+
+    private static boolean isAccurateUnit(String input) {
+        if(Integer.parseInt(input) % 1000 != 0) return false;
+        return true;
+    }
 }
