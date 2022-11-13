9dad0cff60ebc3caa08d024a8483049cb0c99c83
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-13 21:52:06 +0900

    refactor(OutputView): 발행한 로또 출력시 오름차순으로 정렬하는 위치 변경
    Lotto의 toString 메서드에서 OutputView로 이동

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,11 @@ package lotto.view;
 
 import lotto.domain.Lotties;
 import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class OutputView {
 
@@ -9,7 +14,12 @@ public class OutputView {
         System.out.println(lotties.getLottiesSize());
 
         for (Lotto lotto : lotties.getLotties()) {
-            System.out.println(lotto);
+            List<Integer> collect = lotto.getNumbers()
+                    .stream()
+                    .sorted()
+                    .collect(Collectors.toList());
+
+            System.out.println(collect);
         }
     }
 }
