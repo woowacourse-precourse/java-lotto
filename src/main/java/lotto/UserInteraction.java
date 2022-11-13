f4835eb7f9541350679f4b7d82c69652c9dfa674
poter0501 <poter0501@gmail.com>
2022-11-13 20:30:04 +0900

    feat: 정수 리스트, 정수형 자료를 입력으로 받아 정수가 리스트에 포함되어 있으면 예외 throw 하는 기능 구현

--- src/main/java/lotto/UserInteraction.java
+++ src/main/java/lotto/UserInteraction.java
@@ -18,38 +18,46 @@ public class UserInteraction {
         input = Console.readLine();
         return input;
     }
-    public int tryParseInt(String input){
-        int changedInput=0;
+
+    public int tryParseInt(String input) {
+        int changedInput = 0;
         input = input.trim();
         try {
-            changedInput=Integer.parseInt(input);
-        }catch (Exception e){
+            changedInput = Integer.parseInt(input);
+        } catch (Exception e) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해야만 합니다.");
         }
         return changedInput;
     }
-    public List<Integer> tryParseIntegers(String input){
+
+    public List<Integer> tryParseIntegers(String input) {
         List<String> splitInput = List.of(input.split(","));
         List<Integer> resultNumbers = new ArrayList<>();
-        for (String numStr: splitInput) {
+        for (String numStr : splitInput) {
             try {
                 numStr = numStr.trim();
                 resultNumbers.add(tryParseInt(numStr));
-            }catch (Exception e){
+            } catch (Exception e) {
                 throw new IllegalArgumentException("[ERROR] 숫자를 입력해야만 합니다.");
             }
         }
         return resultNumbers;
     }
-    public void checkNumbers(List<Integer> numbers){
+
+    public void checkNumbers(List<Integer> numbers) {
         HashSet<Integer> tempSet = new HashSet<>(numbers);
-        if (tempSet.size()!=6){
+        if (tempSet.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 서로 다른 6자리의 숫자를 입력해야 합니다.");
         }
-        for (int number: tempSet) {
-            if (number<=0 && number>45){
+        for (int number : tempSet) {
+            if (number <= 0 && number > 45) {
                 throw new IllegalArgumentException("[ERROR] 1~45 사이의 숫자를 입력 해야만 합니다.");
             }
+        }
+    }
+    public void checkBonusNumber(List<Integer> numbers, int bonusNumber){
+        if (numbers.contains(bonusNumber)){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨번호와 중복 될 수 없습니다.");
         }
     }
 
