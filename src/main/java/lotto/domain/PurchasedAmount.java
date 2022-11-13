0bdadf72ac0cb141443fe28da73e839d86e36a88
Kimhyeyun <cqqwer@gmail.com>
2022-11-13 19:05:30 +0900

    Feat: 구매 금액 유효성 검사

--- src/main/java/lotto/domain/PurchasedAmount.java
+++ src/main/java/lotto/domain/PurchasedAmount.java
@@ -1,4 +1,25 @@
 package lotto.domain;
 
 public class PurchasedAmount {
+    private int price;
+
+    public PurchasedAmount(String inputPrice) {
+        validate(inputPrice);
+        this.price = Integer.parseInt(inputPrice);
+    }
+
+    private void validate(String inputPrice) {
+        /*숫자 검사*/
+        if (!validateFormat(inputPrice)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (!validateMinimum(inputPrice)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (!validateUnit(inputPrice)) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
