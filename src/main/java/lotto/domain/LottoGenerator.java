6a6d6994b6ebacc6ca933e50f2e69a0309f3c66d
yubin <dbqls2821@gmail.com>
2022-11-13 13:29:52 +0900

    feat(LottoGenerator): 로또의 랜덤수를 생성하는 클래스 추가

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.constant.BoundaryValue;
+import lotto.domain.constant.LottoInfo;
+
+import java.util.List;
+
+public class LottoGenerator {
+  public List<Integer> generate(int num) {
+    return Randoms.pickUniqueNumbersInRange(BoundaryValue.LOTTO_MIN_NUMBER.getBoundaryValue(),
+                                            BoundaryValue.LOTTO_MAX_NUMBER.getBoundaryValue(),
+                                            LottoInfo.LOTTO_LENGTH.getValue());
+  }
+}
