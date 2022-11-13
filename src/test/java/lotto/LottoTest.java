9dd1c6cc2d9c8ef9c81f1b51ea9a891d409323ca
kanghyki <kanghyki@gmail.com>
2022-11-14 05:04:07 +0900

    test(Lotto): 로또 클래스 테스트 추가
    	     - 빈 리스트로 로또를 생성
    	     - 범위를 벗어나는 번호로 로또를 생성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,5 +24,22 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("1~45의 범위를 벗어나는 로또 번호는 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRangeNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("빈 리스트로 로또를 생성하면 예외가 발생한다.")
+    @Test
+    void createLottoByEmptyList() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
     // 아래에 추가 테스트 작성 가능
 }
