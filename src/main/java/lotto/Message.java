80aff4804016e4954294c8b475dadb56b2fc1412
sjh910805@gmail.com <sjh910805@gmail.com>
2022-11-13 18:27:15 +0900

    feat: 에러 메시지를 위한 클래스 생성

---
+++ src/main/java/lotto/Message.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class Message {
+    public static final String ERROR_RANGE_OUT = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String ERROR_NOT_UNIQUE = "[ERROR] 로또 번호는 중복 되지 않은 숫자여야 합니다.";
+    public static final String ERROR_SIZE_IS_NOT_SIX = "[ERROR] 로또 번호는 6개의 숫자여야 합니다.";
+
+}
