c4d85dd353bfbc9e439aa6bc1f3558b575dce741
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2022-11-13 22:47:02 +0900

    feat(lotto): 번호 리스트 혹은 번호 입력하는 기능에 번호 개수 유효성 판단 기능 추가

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -4,18 +4,28 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import lotto.utils.Logger;
+import lotto.utils.Logger.LogType;
 
 public class LottoMachine {
+    private static final String INPUT_NUMBER_SIZE_ERROR_MESSAGE = "필요한 번호 개수만큼 입력하지 않았습니다.";
     public List<Integer> draw(int length) {
         if(length > 0) {
-            List<String> inputs = inputNumber();
+            List<String> inputs = inputNumber(length);
         }
         return List.of();
     }
 
-    private List<String> inputNumber() {
+    private List<String> inputNumber(int length) {
         List<String> inputs = Arrays.asList(Console.readLine().split(","));
-        return inputs.stream().filter(s -> !s.isBlank()).collect(Collectors.toList());
+        inputs = inputs.stream().filter(s -> !s.isBlank()).collect(Collectors.toList());
+
+        if(inputs.size() != length){
+            Logger.log(INPUT_NUMBER_SIZE_ERROR_MESSAGE, LogType.ERROR);
+            throw new IllegalArgumentException();
+        }
+
+        return inputs;
     }
 
     private List<Integer> formatStringToNumber(List<String> numbers) {
