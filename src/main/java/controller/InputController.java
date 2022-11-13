745e8610906c78594842715af8fd5374744b61e5
egoing <dudqk9696@naver.com>
2022-11-13 19:44:58 +0900

    refactor : split api 이용해 당첨 번호 구하도록 리팩토링

--- src/main/java/controller/InputController.java
+++ src/main/java/controller/InputController.java
@@ -42,24 +42,21 @@ public class InputController {
     public List<Integer> insertLottoNumber() {
         List<Integer> lottonumber = new ArrayList<>();
         String strlottonumber = Console.readLine();
-        int numberidx = 0;
         inputValidator.checkDigitNumber(strlottonumber);
-        for(int i=0; i<strlottonumber.length(); i++) {
-            if(strlottonumber.charAt(i) == ',') {
-                lottonumber.add(StringToIntNumber(strlottonumber,numberidx,i));
-                numberidx = i+1;
-            }
-        }
-        lottonumber.add(StringToIntNumber(strlottonumber,numberidx,strlottonumber.length()));
+
+        String[] numbers = strlottonumber.split(",");
+        lottonumber = StringToIntNumber(numbers);
         inputValidator.checkWinNumber(lottonumber);
 
         return lottonumber;
     }
 
-    public int StringToIntNumber(String strlottonumber,int firstidx,int lastidx) {
-        int resultnumber;
-        resultnumber = Integer.parseInt(strlottonumber.substring(firstidx,lastidx));
-        return resultnumber;
+    public List<Integer> StringToIntNumber(String strlottonumber[]) {
+        List<Integer> numbers = new ArrayList<>();
+        for(int i=0; i<strlottonumber.length; i++) {
+            numbers.add(Integer.parseInt(strlottonumber[i]));
+        }
+        return numbers;
     }
 
 }
