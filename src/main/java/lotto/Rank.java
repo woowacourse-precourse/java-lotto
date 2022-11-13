9757f544576665d885d37743acd3008d70cb9ab7
fakerdeft <whakswp123@naver.com>
2022-11-14 03:15:14 +0900

    feat(Rank): add enum클래스 Rank 생성자 추가

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,25 @@
+package lotto;
+
+public enum Rank {
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 1_50_000),
+    BONUS(5, 30_000_000),
+    SIX(6, 2_000_000_000);
+
+    private final int winningNumbers;
+    private final int prize;
+
+    Rank(int winningNumbers, int prize) {
+        this.winningNumbers = winningNumbers;
+        this.prize = prize;
+    }
+
+    public int getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}
