a3a556bf4c03a7bded003b5bfd554cc7dfca3bd4
pbkdpwls <pbkdpwls@naver.com>
2022-11-13 21:29:52 +0900

    [Code] 기능 추가; 로또 랜덤번호 발행, 예외처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,12 @@
 package lotto;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int STATIC_LOTTO_NUMBERS_SIZE = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -17,4 +20,31 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+    public static Lotto newLotto() {
+        return new Lotto(randomLottoNumbers());
+    }
+
+    private static List<Integer> randomLottoNumbers() {
+        List<Integer> lottoNumbers = new ArrayList<Integer>();
+        while (lottoNumbers.size() < STATIC_LOTTO_NUMBERS_SIZE) {
+            lottoNumbers.add(RandomNum(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER));
+        }
+        return lottoNumbers;
+    }
+
+    public static int RandomNum(final int startNum, final int endNum) {
+        Random RANDOM = new Random();
+        if (startNum < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (startNum > endNum) {
+            throw new IllegalArgumentException();
+        }
+        if (startNum == endNum) {
+            return startNum;
+        }
+        return startNum + RANDOM.nextInt(endNum - startNum + 1);
+    }
+
 }
