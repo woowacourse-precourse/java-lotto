a58bfa43d011831866f17943f93af277cf9b727f
kokodak <kokodakadokok@gmail.com>
2022-11-13 22:33:41 +0900

    refactor(lottoController): lottoStart() 메서드 내용 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -16,18 +16,24 @@ public class LottoController {
             int purchaseAmount = InputView.getPurchaseAmount();
             LottoGenerator lottoGenerator = new LottoGenerator(purchaseAmount);
             Lottos lottos = new Lottos(lottoGenerator.generateLottos());
-
-            OutputView.printHowManyLottoUserPurchased(lottoGenerator.getLottoQuantity());
-            OutputView.printLottos(lottos);
-
+            printLottosInformation(lottoGenerator, lottos);
             WinningLotto winningLotto = new WinningLotto(InputView.getLottoNumber(), InputView.getLottoBonusNumber());
-
             Map<WinningRank, Integer> winningDetails = WinningStatistics.getWinningDetails(lottos, winningLotto);
-            OutputView.printWinningStatistics();
-            OutputView.printWinningDetails(winningDetails);
-            OutputView.printLottoYield(WinningStatistics.getLottoYield(winningDetails, purchaseAmount));
+            printWinningInformation(winningDetails, purchaseAmount);
+
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private void printLottosInformation(LottoGenerator lottoGenerator, Lottos lottos) {
+        OutputView.printHowManyLottoUserPurchased(lottoGenerator.getLottoQuantity());
+        OutputView.printLottos(lottos);
+    }
+
+    private void printWinningInformation(Map<WinningRank, Integer> winningDetails, int purchaseAmount) {
+        OutputView.printWinningStatistics();
+        OutputView.printWinningDetails(winningDetails);
+        OutputView.printLottoYield(WinningStatistics.getLottoYield(winningDetails, purchaseAmount));
     }
 }
