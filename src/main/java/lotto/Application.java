bdf60c81ea3b61ad945e13d50cd15f0a249a7d6c
K-Diger <kitten.diger@gmail.com>
2022-11-14 02:52:07 +0900

    refactor : 클라이언트 코드 개선

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,10 +3,26 @@ package lotto;
 import lotto.application.in.LottoStarter;
 
 public class Application {
+    // 입력을 여기서 받도록한다.
     public static void main(String[] args) {
-        LottoStarter lottoStarter = new LottoStarter();
+        ContextFactory contextFactory = new ContextFactory();
+
+        LottoStarter lottoStarter = new LottoStarter(
+                contextFactory.getPurchaseMoneyInput(),
+                contextFactory.getLottoNumberGenerator(),
+                contextFactory.getLottoPrinter(),
+                contextFactory.getWinningNumberInput(),
+                contextFactory.getBonusNumberInput(),
+                contextFactory.getLottoMatcher(),
+                contextFactory.getWinningStatistic(),
+                contextFactory.getWinningStatisticPrinter(),
+                contextFactory.getYieldCalculator(),
+                contextFactory.getYieldPrinter());
+
         try {
             lottoStarter.start();
-        } catch (IllegalArgumentException exception) {}
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
