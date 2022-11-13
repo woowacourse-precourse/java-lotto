1c138f7682f5ba521778334f6a1dc3d08f7dde34
lacred <lacred@isang-us-MacBook-Pro.local>
2022-11-14 00:54:17 +0900

    <refactor> : lottoPurchase 함수 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
        int lottoCount = lottoPurchase(20000);
-
     }
 
     public static int lottoPurchase(int money){
@@ -14,8 +13,10 @@ public class Application {
         if(money%1000 != 0){
             new IllegalArgumentException("[ERROR] 금액은 1,000원 단위로 입력해야 합니다.");
         }
+        int value = money/1000;
+        System.out.println(value+ "개를 구매했습니다.");
 
-
-        return money/1000;
+        return value;
     }
+
 }
