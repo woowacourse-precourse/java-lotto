370233962c4dcddd19b68ee8c463267e5f3c6741
Jeangyu <gjwlsrb1700@naver.com>
2022-11-14 00:17:35 +0900

    [FEAT: lotto] 당첨 숫자를 제대로 입력했는지 확인하는 메서드 구현

--- src/main/java/lotto/utils/Validator.java
+++ src/main/java/lotto/utils/Validator.java
@@ -1,10 +1,27 @@
 package lotto.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Validator {
-    public static boolean validateInteger(String input){
+    public static final int WINNINGNUMS = 6
+    public static void validateInteger(String input){
         if (! input.matches("[0-9]+")) {
-            return false;
+            throw new IllegalArgumentException("숫자가 아닙니다.");
         }
-        return true;
+    }
+
+    public static void validateWinningNumbers(List<String> numbers){
+        for (String number : numbers){
+            validateInteger(number);
+        }
+
+        validateArraySize(numbers, WINNINGNUMS);
+    }
+
+    public static void validateArraySize(List<String> numbers, int size){
+        if (numbers.size() != size){
+            throw new IllegalArgumentException("숫자의 개수가 잘못 되었습니다.");
+        }
     }
 }
