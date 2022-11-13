e172af256e39ad2fca37061ae5036d65bce4660f
yoonjuho <dbswn990@naver.com>
2022-11-13 18:28:32 +0900

    feat(LottoService) : 로또 번호 생성

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,11 +1,38 @@
 package lotto.service;
 
+import lotto.domain.Issue;
+import lotto.domain.Lotto;
+import lotto.ui.Input;
+import lotto.ui.Output;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoService {
+    List<Lotto> history;
+    Output output;
+    Input input;
+    Issue issue;
+    Lotto lotto;
+    public LottoService(){
+        output = new Output();
+        input = new Input();
+    }
     //구입 금액 안내 문구 출력
     //구입 금액 입력
-
+    public void create(){
+        output.moneyInform();
+        issue = new Issue(input.money());
+        history = issue.lotto();
+    }
     //구입 개수 안내 문구 출력
     //구입 번호 출력
+    public void showHistory(){
+        output.amount(Integer.toString(history.size()));
+        output.lottoHistory(history);
+    }
+
+
 
     //당첨 번호 안내 문구 출력
     //당첨 번호 입력
