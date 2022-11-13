3519d72af24ab332656d5fcdadfffedd6f54a6a8
sdsr <dlxodnr1212@gmail.com>
2022-11-14 01:28:14 +0900

    feat: BuyLotto 구현

--- src/main/java/lotto/domain/BuyLotto.java
+++ src/main/java/lotto/domain/BuyLotto.java
@@ -1,6 +1,14 @@
 package lotto.domain;
 
 public class BuyLotto {
+    private static final int MININUM_LOTTO_PURCHASE = 1000;
 
+    public static Integer howMuchBuy(String inputHowMuch){
+        return howMuchStringToInt(inputHowMuch)/MININUM_LOTTO_PURCHASE;
+    }
+
+    private static  Integer howMuchStringToInt(String inputHowMuch) {
+        return Integer.parseInt(inputHowMuch);
+    }
     
 }
