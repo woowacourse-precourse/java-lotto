8bb6a4fc7152478eda9ada562bf6e8e90e73f784
ChaCha3088 <cha3088@gmail.com>
2022-11-14 00:39:29 +0900

    fix(/src/main/java/lotto/Lotto.java): fix method 중복되지 않는 7개의 1~45까지 숫자를 검증하는 메소드의 매개변수 타입 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -18,12 +18,9 @@ public class Lotto {
      * 1에서 45까지 서로 다른 임의의 수 7개인지 검증
      * @param playerNumbers 사용자가 입력한 로또 숫자
      */
-    private static void ValidatePlayerNumber(List<Integer> playerNumbers) {
-        // 집합에 게임 플레이어가 입력한 숫자를 넣는다.
-        HashSet<Integer> playerNumber = Model.MakeListToSet(playerNumbers);
-
+    private static void ValidatePlayerNumber(HashSet<Integer> playerNumbers) {
         // 집합의 크기가 7이 아니거나 1에서 45까지의 숫자가 아닌 숫자를 포함하면 예외를 발생한다.
-        if (playerNumber.size() != 7 || Collections.min(playerNumber) < 1 || Collections.max(playerNumber) > 45) {
+        if (playerNumbers.size() != 7 || Collections.min(playerNumbers) < 1 || Collections.max(playerNumbers) > 45) {
             throw new IllegalArgumentException("[ERROR] 입력된 당첨 번호와 보너스 번호가 유효하지 않습니다.");
         }
     }
