7d82cdcb5dedcec3b2820f9ce048c970b46d9341
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-13 22:48:52 +0900

    feat(test): 당첨 번호 입력이 올바르지 않을 경우 테스트 구현

--- src/test/java/lotto/service/ConvertorTest.java
+++ src/test/java/lotto/service/ConvertorTest.java
@@ -14,11 +14,35 @@ public class ConvertorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("구입금액이 1,000원으로 나누어 떨어지지 않는 경우")
+    @DisplayName("구입금액에 숫자가 들어오지 않는 경우")
     @Test
     void getQuantityByNotNumber() {
         //then
         assertThatThrownBy(() -> Convertor.getQuantity("100a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("6개의 숫자가 아닐 경우")
+    @Test
+    void getNumbersByWrongSize() {
+        //then
+        assertThatThrownBy(() -> Convertor.getNumbers("1,2,3,4"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자 범위가 1~45자리가 아닐 경우")
+    @Test
+    void getNumbersByWrongRange() {
+        //then
+        assertThatThrownBy(() -> Convertor.getNumbers("1,2,3,4,77,2"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("중복된 숫자일 경우")
+    @Test
+    void getNumbersByDuplication() {
+        //then
+        assertThatThrownBy(() -> Convertor.getNumbers("1,2,3,4,4,4"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
