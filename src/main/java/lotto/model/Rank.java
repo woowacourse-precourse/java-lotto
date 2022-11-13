84242569cfdc224b48d98dea96ca96add3d39b1b
SHEOMM <esh0821@snu.ac.kr>
2022-11-13 18:20:34 +0900

    refactor(rank): 가격 표시를 위해 상금을 integer에서 String으로 변경

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,21 +1,25 @@
 package lotto.model;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public enum Rank {
 
-    FIRST_RANK(6, Bonus.FALSE, 2000000000),
-    SECOND_RANK(5, Bonus.TRUE, 30000000),
-    THIRD_RANK(5, Bonus.FALSE, 1500000),
-    FOURTH_RANK(4, Bonus.FALSE, 50000),
-    FIFTH_RANK(3, Bonus.FALSE, 5000),
-    NOT_WINNER(-1, Bonus.FALSE, 0);
+    FIFTH_RANK(3, Bonus.FALSE, "5,000원"),
+    FOURTH_RANK(4, Bonus.FALSE, "50,000원"),
+    THIRD_RANK(5, Bonus.FALSE, "1,500,000원"),
+    SECOND_RANK(5, Bonus.TRUE, "30,000,000원"),
+    FIRST_RANK(6, Bonus.FALSE, "2,000,000,000원"),
+    NOT_WINNER(-1, Bonus.FALSE, "-1");
 
-    private int matchedNumber;
-    private Bonus bonus;
-    private int price;
+    private final int matchedNumber;
+    private final Bonus bonus;
+    private final String price;
 
-    Rank(int matchedNumber, Bonus bonus, int price){
+    Rank(int matchedNumber, Bonus bonus, String price){
         this.matchedNumber = matchedNumber;
         this.bonus = bonus;
         this.price = price;
@@ -29,11 +33,16 @@ public enum Rank {
                 .orElse(NOT_WINNER);
     }
 
+    public static List<Rank> rankFormatter(){
+        return Arrays.stream(Rank.values())
+                .collect(Collectors.toUnmodifiableList());
+    }
+
     public int getMatchedNumber() {
         return matchedNumber;
     }
 
-    public int getPrice() {
+    public String getPrice() {
         return price;
     }
 
