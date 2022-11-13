3cc492bf9c57c03cbaa4fe688b42a10965fe8d8b
qdeu22 <qdeu22@gmail.com>
2022-11-13 20:37:58 +0900

    feat(lotto): 당첨 기준에 따라 순위가 정하는 기능구현

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,2 +1,50 @@
-package lotto.domain;public enum Rank {
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public enum Rank {
+    NOT(0, 0),
+    FIFTH(5_000, 3),
+    FOURTH(50_000, 4),
+    THIRD(1_500_000, 5),
+    SECOND(30_000_000, 5),
+    FIRST(2_000_000_000, 6);
+
+    private static final int SPECIAL_CASE = 5;
+
+    private final int prize;
+    private final int matchCount;
+
+    Rank(int prize, int matchCount) {
+        this.prize = prize;
+        this.matchCount = matchCount;
+    }
+
+    public static Rank find(int matchCount, boolean isBonusBall) {
+        if (matchCount == SPECIAL_CASE) {
+            return checkBonus(isBonusBall);
+        }
+        return Arrays.stream(values())
+                .filter(value -> value.matchCount == matchCount)
+                .findFirst()
+                .orElse(NOT);
+    }
+
+    private static Rank checkBonus(boolean isBonusBall) {
+        if (isBonusBall) {
+            return Rank.SECOND;
+        }
+        return Rank.THIRD;
+    }
+
+    public int getPrize() {
+        return this.prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
 }
--- src/main/java/lotto/domain/Referee.java
+++ src/main/java/lotto/domain/Referee.java
@@ -1,4 +1,38 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Referee {
+    private double totalPrize;
+
+    public Referee() {
+        this.totalPrize = 0.0;
+    }
+
+    public Map<Rank, Integer> findNumber(List<List<Integer>> generatorNumbers, int bonusNumber, Lotto lotto) {
+        Judgement judgement = new Judgement();
+        Map<Rank, Integer> ranking = new HashMap<>();
+        for (List<Integer> number : generatorNumbers) {
+            int matchCount = judgement.correctNumbers(number, lotto.getNumbers());
+            boolean isBonus = judgement.correctBonus(number, bonusNumber);
+            compareNumber(ranking, matchCount, isBonus);
+        }
+        return ranking;
+    }
+
+    private void compareNumber(Map<Rank, Integer> ranking, int matchCount, boolean isBonus) {
+        Rank rank = Rank.find(matchCount, isBonus);
+        ranking.put(rank, ranking.getOrDefault(rank, 0) + 1);
+        sumPrize(rank);
+    }
+
+    private void sumPrize(Rank rank) {
+        totalPrize += rank.getPrize();
+    }
+
+    public double getTotalPrize() {
+        return totalPrize;
+    }
 }
