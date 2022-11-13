8b482ea91cb14aa8e7f426270019923f2a70d5e7
saehim <saeilee.vxv@gmail.com>
2022-11-13 17:22:17 +0900

    feat(lotto) : 돈입력 관련 에러 메시지

---
+++ src/main/java/lotto/view/Messages.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class Messages {
+    // ERROR
+    public static final String  ERROR_NOT_DIVDE1000 = "[ERROR] 금액은 1,000원 단위로 입력할 수 있습니다.";
+    public static final String  ERROR_NULL = "[ERROR] 0이 아닌 값을 입력하세요.";
+}
