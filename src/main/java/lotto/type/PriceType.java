c0369aa319f87918f47185868cfec8c26940c570
win9 <krrr8@naver.com>
2022-11-14 03:14:07 +0900

    Refactor(Type): 출력을 좀더 쉽게 할 수 있도록 수정
    콘솔에 상태를 출력할 때 다른 부수적인 문자를 추가하지 않고,
    enum만을 이용하여 출력 할 수 있도록 수정

--- src/main/java/lotto/type/PriceType.java
+++ src/main/java/lotto/type/PriceType.java
@@ -1,11 +1,11 @@
 package lotto.type;
 
 public enum PriceType {
-    FIRST("2,000,000,000원"),
-    SECOND("30,000,000원"),
-    THIRD("1,500,000원"),
-    FOURTH("50,000원"),
-    FIFTH("5,000원");
+    FIRST("(2,000,000,000원)"),
+    SECOND("(30,000,000원)"),
+    THIRD("(1,500,000원)"),
+    FOURTH("(50,000원)"),
+    FIFTH("(5,000원)");
 
     private final String price;
 
