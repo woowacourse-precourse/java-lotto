c7acb5230af24cbf19a798acb3cc40aa0bcf2d3e
이정우 <acg6138@naver.com>
2022-11-13 15:14:54 +0900

    feat(Validate): 검증을 담당하는 클래스 생성

---
+++ src/main/java/lotto/domain/Validate.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import java.util.List;
+
+import static lotto.domain.Lotto.LOTTO_NUMS_SIZE;
+
+public class Validate {
+    public static boolean isInRange(int number) {
+        return 1 <= number && number <= 45;
+    }
+
+    public static boolean isNotContains(List<Integer> nums, int num) {
+        return !nums.contains(num);
+    }
+
+    public static boolean isNotDuplicate(List<Integer> nums) {
+        return nums.stream()
+                .distinct()
+                .count() == LOTTO_NUMS_SIZE;
+    }
+}
