e94379fe8f510716c0fa1416789389c3894d64de
EUNSH1 <tkdgus968@naver.com>
2022-11-14 02:45:41 +0900

    feat : 1부터 45까지 서로 다른 6개의 임의의 수를 생성하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,17 @@
 package lotto;
 
+import java.util.List;
+import lotto.domain.LottoNumbers;
 import lotto.domain.PlayerLottoAmount;
 import lotto.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
-        PlayerLottoAmount playerLottoAmount = new PlayerLottoAmount(InputView.inputPlayerAmount());
-        int result = playerLottoAmount.calculateLottoCount();
-        System.out.println(result);
+//        PlayerLottoAmount playerLottoAmount = new PlayerLottoAmount(InputView.inputPlayerAmount());
+//        int result = playerLottoAmount.calculateLottoCount();
+//        System.out.println(result);
+        LottoNumbers lottoNumbers = new LottoNumbers();
+        List<Integer> list = lottoNumbers.setRandomNumbers();
+        System.out.println(list);
     }
 }
