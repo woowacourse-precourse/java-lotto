9d4defea42a8262493ddd42b356a46b9737ac834
bsh1603 <bsh1603@github.com>
2022-11-13 22:15:09 +0900

    Feat: 자동 로또 티켓 전체 구하기

--- src/main/java/Service/LottoService.java
+++ src/main/java/Service/LottoService.java
@@ -1,8 +1,12 @@
 package Service;
 
+import domain.Ticket;
 import domain.UserMoneyValidate;
+import lotto.Lotto;
+import utils.RandomNumberLottoGenerator;
 import view.RequestUser;
 import camp.nextstep.edu.missionutils.Console;
+import view.SystemMessage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,14 +15,37 @@ import java.util.Set;
 public class LottoService {
 
     UserMoneyValidate userMoneyValidate = new UserMoneyValidate();
+    SystemMessage systemMessage = new SystemMessage();
 
-    public void start_lotto(){
-
+    public void set_lotto(){
+        int userMoney = getUserMoney();
+        int tickets = buyTickets(userMoney);
+        getAllLotto(tickets);
     }
 
     private int getUserMoney() throws IllegalArgumentException{
         RequestUser.requestMoney();
         String userInput = Console.readLine();
         return userMoneyValidate.validateGetMoney(userInput);
+    }
+
+    public int buyTickets(int money){
+        Ticket ticket = new Ticket();
+        int ticketNum = ticket.getTicketNum(money);
+        return ticketNum;
+    }
+
+    public List<List<Integer>> getAllLotto(int tickets){
+        List<List<Integer>> allTicket = new ArrayList<>();
+        for (int i = 0; i < tickets; i++) {
+            RandomNumberLottoGenerator randomNumberLottoGenerator = new RandomNumberLottoGenerator();
+            Set<Integer> generate = randomNumberLottoGenerator.generate();
+            List<Integer> randomNum = randomNumberLottoGenerator.convertSetToList(generate);
+            allTicket.add(randomNum);
+            Lotto lotto = new Lotto(randomNum);
+            lotto.numbersAscending(randomNum);
+            systemMessage.ticketNumberMessage(randomNum);
+        }
+        return allTicket;
     }
 }
--- src/main/java/domain/Ticket.java
+++ src/main/java/domain/Ticket.java
@@ -5,8 +5,7 @@ public class Ticket {
     public UserMoneyValidate userMoney = new UserMoneyValidate();
     final int THOUSAND = 1000;
 
-    public int getTicketNum(String input){
-        int totalMoney = userMoney.validateGetMoney(input);
-        return totalMoney / THOUSAND;
+    public int getTicketNum(int input){
+        return input / THOUSAND;
     }
 }
