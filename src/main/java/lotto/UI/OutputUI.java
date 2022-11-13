6cfcdd8cfc53a89c020a3165535bca356717c1a9
jamangstangs <jamangsong@gmail.com>
2022-11-14 00:39:37 +0900

    refactor(OutputUI): printWinnerResult(), enum을 활용한 출력방식으로 리팩터링

--- src/main/java/lotto/UI/OutputUI.java
+++ src/main/java/lotto/UI/OutputUI.java
@@ -1,6 +1,8 @@
 package lotto.UI;
 
 import lotto.Lotto;
+import lotto.Rank;
+import lotto.ResultLotto;
 
 import java.util.List;
 
@@ -14,28 +16,24 @@ public class OutputUI {
     }
 
     public void printLottos(List<Lotto> lottos) {
-        // TODO: Lotto 번호를 형식에 맞게 출력해야함.
         for (Lotto lotto : lottos) {
             List<Integer> lottoNumbers = lotto.getNumbers();
             System.out.println(lottoNumbers);
         }
     }
 
-    public void printWinnerCounts(List<Integer> winnerCounts, double returnRate) {
-        printWinnerResult(winnerCounts);
-        printReturnReate(returnRate);
+    public void printWinnerResult(ResultLotto resultLotto) {
+        Rank[] ranks = Rank.values();
+        for (Rank rank : ranks) {
+            System.out.println(String.format("%d개 일치 (%s) - %d개",
+                    rank.getHitCount(),
+                    rank.getRewardString(),
+                    resultLotto.getResultWithRank(rank.getRankInt())));
+
+        }
     }
 
-    private void printWinnerResult(List<Integer> winnerCounts) {
-        //TODO: ENUM으로 Refactoring 예정
-        System.out.println("3개 일치 (5,000원) - 0개");
-        System.out.println("4개 일치 (50,000원) - 0개");
-        System.out.println("5개 일치 (1,500,000원) - 0개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - 0개");
-        System.out.println("6개 일치 (2,000,000,000원) - 0개");
-    }
-
-    private void printReturnReate(double returnRate) {
+    public void printReturnReate(double returnRate) {
         System.out.println("총 수익률은 " + returnRate +"%입니다.");
     }
 }
