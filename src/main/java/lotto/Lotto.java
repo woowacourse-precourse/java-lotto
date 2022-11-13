ee1c47c1e5fbd1ca5c505fd23e8334c37dadac19
kanghyki <kanghyki@gmail.com>
2022-11-14 00:19:31 +0900

    fix(Lotto): 인자로 들어온 List를 new 키워드를 통해 생성 후 정렬하도록 변경     - Array.asList 또는 list.of로 초기화된 리스트에서 발생하는 UnsupportedOperationException 에러 해결

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,8 +13,11 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        Collections.sort(numbers);
-        this.numbers = numbers;
+
+        List<Integer> newNumbers = new ArrayList<>(numbers);
+        Collections.sort(newNumbers);
+
+        this.numbers = newNumbers;
     }
 
     public List<Integer> getNumbers() {
