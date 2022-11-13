5cf6ef39ce7e574dc62075657e4d7c32b2d8435f
JaehongDev <println@kakao.com>
2022-11-13 15:46:54 +0900

    refactor (IoSystem) : 입출력을 생성하는 시스템으로 인스턴스를 생성하도록 변경

--- src/main/java/lotto/system/IoSystem.java
+++ src/main/java/lotto/system/IoSystem.java
@@ -3,17 +3,17 @@ package lotto.system;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class IoSystem{
+public class IoSystem {
 
-    public static String input() {
+    public String input() {
         return Console.readLine();
     }
 
-    public static void print(final Object message) {
+    public void print(final Object message) {
         System.out.print(message);
     }
 
-    public static void println(final Object message) {
+    public void println(final Object message) {
         System.out.println(message);
     }
 }
