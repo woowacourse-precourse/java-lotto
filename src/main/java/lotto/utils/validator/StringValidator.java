0be0d2abad71c86041372b0f83d889b25b9b1076
plusjob70 <plusjob70@gmail.com>
2022-11-14 00:39:13 +0900

    fix(StringValidator): 로또 번호 판별 버그 해결

--- src/main/java/lotto/utils/validator/StringValidator.java
+++ src/main/java/lotto/utils/validator/StringValidator.java
@@ -6,7 +6,7 @@ import static lotto.utils.LottoInformation.SIX_LOTTO_PATTERN;
 
 public class StringValidator {
     public static void checkSplitSixArgs(String input) {
-        if (input.matches(SIX_LOTTO_PATTERN)) {
+        if (!input.matches(SIX_LOTTO_PATTERN)) {
             System.out.println(Error.CAN_NOT_SPLIT);
             throw new IllegalArgumentException();
         }
