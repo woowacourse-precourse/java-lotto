a08c13a1fc0dd2ba166675188de60eea26f64a54
jiwon <ljwon77@daum.net>
2022-11-13 19:34:34 +0900

    feat: check match count and save result
    일치하는 개수를 체크한 다음, 해당 개수에 맞춰서 enum 타입의 LottoResult에 저장하는 기능을 추가하였습니다.

--- src/main/java/lotto/LottoPrize.java
+++ src/main/java/lotto/LottoPrize.java
@@ -5,24 +5,44 @@ import lotto.util.MessageUtil;
 import java.util.List;
 import java.util.function.Predicate;
 
+import static lotto.constant.LottoResult.*;
+
 public class LottoPrize {
 
     private final MessageUtil messageUtil = new MessageUtil();
 
     private final LottoWinningNumber winningNumInfo;
 
-    private static final Integer BONUS_AVAILABLE_NUM = 5;
-
     public LottoPrize(LottoWinningNumber lottoWinningNumber) {
         this.winningNumInfo = lottoWinningNumber;
     }
+
     public void getLottoStats(List<Lotto> userLottos) {
         messageUtil.printWinningStats();
 
         for(Lotto userLotto : userLottos) {
             List<Integer> userLottoNums = userLotto.getLottoNumbers();
             int matchCount = getMatchCount(userLottoNums);
-            matchCount  = checkBonusNumber(userLottoNums, matchCount);
+            addMatchCount(checkBonusNumber(userLottoNums, matchCount));
+        }
+
+    }
+
+    private void addMatchCount(int matchCount) {
+        if (matchCount == THREE_COUNT.getNumber()) {
+            THREE_COUNT.addMatchCount();
+        }
+        if (matchCount == FOUR_COUNT.getNumber()) {
+            FOUR_COUNT.addMatchCount();
+        }
+        if (matchCount == FIVE_COUNT.getNumber()) {
+            FIVE_COUNT.addMatchCount();
+        }
+        if (matchCount == FIVE_COUNT_WITH_BONUS.getNumber()) {
+            FIVE_COUNT_WITH_BONUS.addMatchCount();
+        }
+        if (matchCount == SIX_COUNT.getNumber()) {
+            SIX_COUNT.addMatchCount();
         }
     }
 
@@ -35,14 +55,16 @@ public class LottoPrize {
     }
 
     private int checkBonusNumber(List<Integer> userNums, int matchCount) {
-        if(matchCount == BONUS_AVAILABLE_NUM && userNums.contains(matchCount)) {
+        if(matchCount == FIVE_COUNT.getNumber() && userNums.contains(matchCount)) {
             return matchCount * 10;
         }
+
         return matchCount;
     }
 
     public String computeEarningRate(int purchaseAmount, int winningAmount) {
         double earningRate = (double) (purchaseAmount / winningAmount) * 100;
+
         return String.format("%.1f", earningRate);
     }
 
