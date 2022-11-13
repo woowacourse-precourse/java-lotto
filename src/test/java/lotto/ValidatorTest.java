b3aa7350aeac3ca8746f78470ebe173c18c080d3
yun <yshyeon102@gmail.com>
2022-11-14 05:49:25 +0900

    test(validator): 로또 입력 부분 검증 테스트 작성

--- src/test/java/lotto/ValidatorTest.java
+++ src/test/java/lotto/ValidatorTest.java
@@ -26,4 +26,39 @@ class ValidatorTest {
         Validator.validateMoney("2000");
         Validator.validateMoney("3000");
     }
+
+    @Test
+    void 당첨_로또_입력시_제대로된_형식으로_입력하지_않은_경우_예외_발생_쉼표X() {
+        assertThatThrownBy(() -> Validator.validateLotto("1 2 3 4 5 6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionType.LOTTO_INPUT_FORMAT_EXCEPTION.getMessage());
+    }
+
+    @Test
+    void 당첨_로또_입력시_제대로된_형식으로_입력하지_않은_경우_예외_발생_숫자6개_X() {
+        assertThatThrownBy(() -> Validator.validateLotto("1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionType.LOTTO_INPUT_FORMAT_EXCEPTION.getMessage());
+    }
+
+    @Test
+    void 당첨_로또_입력시_1보다_작은_숫자를_포함하는_경우_예외_발생() {
+        assertThatThrownBy(() -> Validator.validateLotto("0,1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionType.LOTTO_RANGE_EXCEPTION.getMessage());
+    }
+
+    @Test
+    void 당첨_로또_입력시_45보다_큰_숫자를_포함하는_경우_예외_발생() {
+        assertThatThrownBy(() -> Validator.validateLotto("1,2,3,48,49,50"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionType.LOTTO_RANGE_EXCEPTION.getMessage());
+    }
+
+    @Test
+    void 로또_숫자_안에_중복된_숫자가_있는_경우_예외_발생() {
+        assertThatThrownBy(() -> Validator.validateLotto("1,2,3,4,5,5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionType.LOTTO_DUPLICATE_EXCEPTION.getMessage());
+    }
 }
