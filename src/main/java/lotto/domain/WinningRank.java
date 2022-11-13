9ff496812d4f46e7162013f9254b9f23fdd9ca12
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-13 15:30:55 +0900

    feat(WinningRank): 당첨 여부 확인 구현
    - 맞춘 번호 수, 보너스 번호 일치 여부로 당첨 여부 확인 function 구현

--- src/main/java/lotto/domain/WinningRank.java
+++ src/main/java/lotto/domain/WinningRank.java
@@ -2,15 +2,21 @@ package lotto.domain;
 
 import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public enum WinningRank {
-    NONE_RANKED(6, 0, ""),
-    FIFTH_RANK(5, 5_000, "3개 일치"),
-    FOURTH_RANK(4, 50_000, "4개 일치"),
-    THIRD_RANK(3, 1_500_000, "5개 일치"),
-    SECOND_RANK(2, 30_000_000, "5개 일치, 보너스 볼 일치"),
-    FIRST_RANK(1, 2_000_000_000, "6개 일치");
-    // todo: 각각 (일치하는 번호 수, 보너스 번호 일치 여부) 로 당첨 true, false 반환 function 구현
+    NONE_RANKED(6, 0, "",
+            (duplicatedNumberCount, isContainBonusNumber) -> (duplicatedNumberCount < 3)),
+    FIFTH_RANK(5, 5_000, "3개 일치",
+            (duplicatedNumberCount, isContainBonusNumber) -> (duplicatedNumberCount == 3)),
+    FOURTH_RANK(4, 50_000, "4개 일치",
+            (duplicatedNumberCount, isContainBonusNumber) -> (duplicatedNumberCount == 4)),
+    THIRD_RANK(3, 1_500_000, "5개 일치",
+            (duplicatedNumberCount, isContainBonusNumber) -> (duplicatedNumberCount == 5 && !isContainBonusNumber)),
+    SECOND_RANK(2, 30_000_000, "5개 일치, 보너스 볼 일치",
+            (duplicatedNumberCount, isContainBonusNumber) -> (duplicatedNumberCount == 5 && isContainBonusNumber)),
+    FIRST_RANK(1, 2_000_000_000, "6개 일치",
+            (duplicatedNumberCount, isContainBonusNumber) -> (duplicatedNumberCount == 6));
 
 
     private static final String DECIMAL_FORMAT = "###,###";
@@ -18,11 +24,13 @@ public enum WinningRank {
     private final int rank;
     private final int prizeMoney;
     private final String winningRule;
+    private final BiFunction<Integer, Boolean, Boolean> isWinExpression;
 
-    WinningRank(int rank, int prizeMoney, String winningRule) {
+    WinningRank(int rank, int prizeMoney, String winningRule, BiFunction<Integer, Boolean, Boolean> isWinExpression) {
         this.rank = rank;
         this.prizeMoney = prizeMoney;
         this.winningRule = winningRule;
+        this.isWinExpression = isWinExpression;
     }
 
     public static WinningRank findByRank(int rankNum) {
@@ -34,6 +42,10 @@ public enum WinningRank {
 
     public int getPrizeMoney() {
         return prizeMoney;
+    }
+
+    public boolean isWin(int duplicatedNumberCount, boolean isContainBonusNumber) {
+        return isWinExpression.apply(duplicatedNumberCount, isContainBonusNumber).booleanValue();
     }
 
 
