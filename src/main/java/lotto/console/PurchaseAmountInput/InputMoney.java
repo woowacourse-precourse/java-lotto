27d5f248dd246222f32f50c1f3098260e1168435
jaehyeon <jhjh0022@naver.com>
2022-11-13 20:49:43 +0900

    feat: 입력받는 돈이 0 원일 때 검증하는 기능 구현

--- src/main/java/lotto/console/PurchaseAmountInput/InputMoney.java
+++ src/main/java/lotto/console/PurchaseAmountInput/InputMoney.java
@@ -7,6 +7,7 @@ public class InputMoney {
     public int getAndVerifyMoney() {
         int purchaseMoney = getPurchaseMoney();
         verifyCommonMultipleOf1000(purchaseMoney);
+        verifyEmptyMoney(purchaseMoney);
 
         return purchaseMoney;
     }
@@ -18,6 +19,12 @@ public class InputMoney {
     private void verifyCommonMultipleOf1000(int purchaseNumber) {
         if (purchaseNumber % 1000 != 0) {
             throw new RuntimeException("[ERROR] 1000원 단위로 입력해야 합니다.");
+        }
+    }
+
+    private void verifyEmptyMoney(int purchaseNumber) {
+        if (purchaseNumber == 0) {
+            throw new RuntimeException("[ERROR] 돈을 내세요");
         }
     }
 }
