b5c143a2afacc9a1d183744037e3da54e1ff0222
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-13 23:00:44 +0900

    refactor: 사용하지 않는 EMPTY를 지우고, 당첨 결과를 출력할 메시지를 보너스 매칭과 연관 있는 경우와 없는 경우로 구분함

--- src/main/java/lotto/constant/UserOutputMessage.java
+++ src/main/java/lotto/constant/UserOutputMessage.java
@@ -1,11 +1,10 @@
 package lotto.constant;
 
 public enum UserOutputMessage {
-    EMPTY_STRING(""),
     POSTFIX_NUMBER_OF_PURCHASES("개를 구매했습니다."),
     PREFIX_WINNING_RESULT("당첨 통계\n" + "---"),
-    WINNIGN_RESULT("%d개 일치%s (%s원) - %s개\n"),
-    BONUS_NUMBER_MATCHES(", 보너스 볼 일치"),
+    WINNING_RESULT("%d개 일치 (%s원) - %s개\n"),
+    WINNIGN_RESULT_WITH_BONUS_MATCHES("%d개 일치, 보너스 볼 일치 (%s원) - %s개\n"),
     RATE_OF_RETURN("총 수익률은 %.1f%%입니다.");
 
     private final String message;
