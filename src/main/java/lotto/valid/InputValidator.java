898117bde66e82205fdc844722185f776cc3dce5
sonsoowon <yuwoo1018@gmail.com>
2022-11-14 01:05:54 +0900

    feat(InputValidator): 입력된 당첨 번호가 잘못된 형식인 경우 예외 발생

--- src/main/java/lotto/valid/InputValidator.java
+++ src/main/java/lotto/valid/InputValidator.java
@@ -2,11 +2,17 @@ package lotto.valid;
 
 import lotto.domain.LottoSalesman;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum InputValidator {
     INSTANCE;
     private static final String NOT_NATURAL_NUMBER_ERROR_MESSAGE = "[ERROR] 0이상의 정수를 입력해야 합니다.";
     private static final String MONEY_ERROR_MESSAGE = "[ERROR] 1000원 단위의 금액을 입력해야 합니다.";
     private static final String NON_DIGIT = "\\D";
+    private static final String JACKPOT_NUMBER_INPUT_FORMAT = "(\\d+),(\\d+),(\\d+),(\\d+),(\\d+),(\\d+)";
+    private static final String JACKPOT_NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 올바른 형식으로 입력해야 합니다.";
 
     public static void validateMoney(String moneyInput) {
         checkNonDigit(moneyInput);
@@ -15,6 +21,13 @@ public enum InputValidator {
             throw new IllegalArgumentException(MONEY_ERROR_MESSAGE);
         }
     }
+
+    public static void validateJackpotNumberFormat(String jackpotNumberInput) {
+        if (!jackpotNumberInput.matches(JACKPOT_NUMBER_INPUT_FORMAT)) {
+            throw new IllegalArgumentException(JACKPOT_NUMBER_FORMAT_ERROR_MESSAGE);
+        }
+    }
+
 
     private static void checkNonDigit(String string) {
         if (string.matches(NON_DIGIT)) {
--- src/test/java/lotto/valid/InputValidatorTest.java
+++ src/test/java/lotto/valid/InputValidatorTest.java
@@ -1,11 +1,15 @@
 package lotto.valid;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputValidatorTest {
     @DisplayName("로또 구입 금액이 1000으로 나누어 떨어지지 않으면 예외가 발생한다")
@@ -23,4 +27,11 @@ public class InputValidatorTest {
                 .isThrownBy(() -> InputValidator.validateMoney(moneyInput));
     }
 
+    @DisplayName("입력된 당첨 번호가 잘못된 형식인 경우 예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5.6", "1,,3,4,5,6"})
+    void jackpotNumberInputByIncorrectFormat(String jackpotNumber) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputValidator.validateJackpotNumberFormat(jackpotNumber));
+    }
 }
