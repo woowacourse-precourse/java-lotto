05e6b4bf5d2575ecffc88c50e66fed7c83dc46c9
Haechan Lee <dlgockssla@gmail.com>
2022-11-14 02:46:47 +0900

    feat: 로또 구매 금액 입력

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import lotto.view.InputView;
+
 public class LottoController {
 
     private final LottoService lottoService;
@@ -15,6 +17,13 @@ public class LottoController {
     }
 
     private void buyLottos() {
+        String inputMoney = InputView.inputMoney();
+        int money;
+        try {
+            money = Integer.parseUnsignedInt(inputMoney);
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException("[ERROR] 음수를 제외한 숫자만 입력해주세요.");
+        }
     }
 
     private void setWinningNumbers() {
