82f22b2c534de8a08f83d0dd9bdbc5fdc520f92d
CAEL0 <kjongmin26@gmail.com>
2022-11-13 17:00:17 +0900

    feat(OutputView): 당첨 내역 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import lotto.Lotto;
 
+import static lotto.constant.LottoConstants.*;
 import static lotto.constant.OutputConstants.*;
 
 public class OutputView {
@@ -33,5 +34,30 @@ public class OutputView {
     public static void printStatistics() {
         System.out.println();
         System.out.println(STATISTICS);
+    }
+
+    public static void printFifthPlace(int fifthPlaceCount) {
+        System.out.printf(FIFTH_PLACE.toString(), FIFTH_PRIZE, fifthPlaceCount);
+        System.out.println();
+    }
+
+    public static void printFourthPlace(int fourthPlaceCount) {
+        System.out.printf(FOURTH_PLACE.toString(), FOURTH_PRIZE, fourthPlaceCount);
+        System.out.println();
+    }
+
+    public static void printThirdPlace(int thirdPlaceCount) {
+        System.out.printf(THIRD_PLACE.toString(), THIRD_PRIZE, thirdPlaceCount);
+        System.out.println();
+    }
+
+    public static void printSecondPlace(int secondPlaceCount) {
+        System.out.printf(SECOND_PLACE.toString(), SECOND_PRIZE, secondPlaceCount);
+        System.out.println();
+    }
+
+    public static void printFirstPlace(int firstPlaceCount) {
+        System.out.printf(FIRST_PLACE.toString(), FIRST_PRIZE, firstPlaceCount);
+        System.out.println();
     }
 }
