9645c3c8a5165d3e30932487d0109a8238cbcd34
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-11-13 17:36:02 +0900

    feat: user에게 당첨번호 입력받기

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -2,18 +2,32 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class User{
     public static int money = Integer.parseInt(Console.readLine());
 
-    private final List<Integer> userNumList;
+    private final List<List<Integer>> userNumList;
 
-    public User(List<Integer> userNumList){
+    public User(List<List<Integer>> userNumList){
         this.userNumList = userNumList;
+        for (int i = 0; i< money / 1000; i++) {
+            String str = Console.readLine();
+            String tempList[] = str.split(",");
+
+            userNumList.add(stringToNum(tempList));
+        }
     }
 
-    String str = Console.readLine();
+
+    public static List<Integer> stringToNum(String[] strlist){
+        List<Integer> numList = new ArrayList<>();
+        for (int i = 0; i < strlist.length; i++){
+            numList.add(Integer.parseInt(strlist[i]));
+        }
+        return numList;
+    }
 
     public static int bonusNum = Integer.parseInt(Console.readLine());
 
