70264a3db361ea4da6d4fd6b17edf03a73a1236f
KimJaeSung <behappy0916@naver.com>
2022-11-13 18:38:12 +0900

    docs: 기존 Exceptions 클래스 삭제
    기존 Exceptions 클래스를 삭제하고
    예외를 각 클래스에 넣음

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,35 +1,48 @@
 package lotto.domain;
 
-
 public class Money {
     private static final int LOTTO_PRICE = 1000;
-    private static final String ERROR_INPUT_NUMBER = "[ERROR] 숫자만 입력해주세요.";
+    private static final String ERROR_INPUT_MONEY = "[ERROR] 숫자만 입력해주세요.";
     private static final String NOT_VALID_MONEY = "[ERROR] 1000원 단위로 입력해주세요.";
     private final int money;
 
     public Money(String money) {
+        try {
+            validateMoney(money);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            throw new IllegalArgumentException();
+        }
         this.money = convertToInt(money);
-        validateMoney(this.money);
     }
 
-    public void validateMoney(int money) {
-        if(calculator(money)){
+    public void validateMoney(String money) {
+        if (!isDigit(money)) {
+            throw new IllegalArgumentException(ERROR_INPUT_MONEY);
+        }
+        if (!isCalculator(money)) {
             throw new IllegalArgumentException(NOT_VALID_MONEY);
         }
     }
 
-    private boolean calculator(int money) {
-        return money % LOTTO_PRICE != 0 || money < 0;
+    public static int convertToInt(String money) {
+        return Integer.parseInt(money);
     }
 
-    private int convertToInt(String money) {
-        try {
-            return Integer.parseInt(money);
-        } catch (Exception e) {
-            throw new IllegalArgumentException(ERROR_INPUT_NUMBER);
+    private boolean isCalculator(String money) {
+        int convertMoney = convertToInt(money);
+        if (convertMoney % 1000 != 0 || convertMoney < 0) {
+            return false;
         }
+        return true;
     }
 
+    public static boolean isDigit(String money) {
+        if (money != null && !money.matches("-?\\d+")) {
+            return false;
+        }
+        return true;
+    }
 
 
 }
