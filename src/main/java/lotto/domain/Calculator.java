6d9d5b1743e38eb8a64f3a12f6377076288c6d04
jaehyeon <jhjh0022@naver.com>
2022-11-14 01:18:51 +0900

    fix: 수익률 구하는 식 수정

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 public class Calculator {
 
-    public static double df(List<Integer> rewardList, int money) {
+    public double df(List<Integer> rewardList, int money) {
         double moneys = money;
         double total = totalRewardmoney(rewardList);
 
-        return (double) Math.round((total / moneys) * 10000) / 1000;
+        return (double) Math.round(((total / moneys) * 100)*10) / 10.0;
     }
 
-    private static int totalRewardmoney(List<Integer> rewardList) {
+    private int totalRewardmoney(List<Integer> rewardList) {
         int sum = 0;
         for (int money : rewardList) {
             sum += money;
