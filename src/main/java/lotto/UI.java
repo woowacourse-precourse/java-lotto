f9a5c8db893d18291f00d6ca4cc89aec257c6d8b
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-11-14 04:07:08 +0900

    feat (UI) : 당첨 번호 Lotto에 set

--- src/main/java/lotto/UI.java
+++ src/main/java/lotto/UI.java
@@ -1,10 +1,21 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UI {
     public static void setMoney() {
         String money = Console.readLine();
         User user = new User(money);
+    }
+
+    public static void setLottoNumbers() {
+        String inputNumbers = Console.readLine();
+        List<Integer> numbers = Arrays.stream(inputNumbers.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        Lotto lotto = new Lotto(numbers);
     }
 }
