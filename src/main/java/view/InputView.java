0a69f3f0622c5a930eb53c5a3afeac478b15313d
김선동 <mok03127@gmail.com>
2022-11-14 00:15:01 +0900

    feat: 로또 구입 금액을 입력 받는 기능 추가
    숫자가 아닌 값 입력할 경우 view에서 예외 처리

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,29 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요." + System.lineSeparator();
+    private static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요." + System.lineSeparator();
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요." + System.lineSeparator();
+    private static final String INPUT_MONEY_TYPE_ERROR_MESSAGE = "[ERROR] 입력은 숫자여야만 합니다."
+
+    public static int inputMoney() {
+        System.out.println(MONEY_INPUT_MESSAGE);
+        String money = Console.readLine();
+        
+        // 입력이 숫자인지만 확인
+        isDigit(money);
+
+        return Integer.parseInt(money);
+    }
+
+    private static void isDigit(String money){
+        for(int i=0; i<money.length(); i++){
+            if(!Character.isDigit(money.charAt(i))){
+                throw new IllegalArgumentException(INPUT_MONEY_TYPE_ERROR_MESSAGE);
+            }
+        }
+    }
+
+}
