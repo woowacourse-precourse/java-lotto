dae28f2658c2e331b6ef31b05ba69e931cf9b73d
UJ15 <dbwogml15@naver.com>
2022-11-14 07:41:04 +0900

    feat : create printLottos in ouput class

--- src/main/java/lotto/io/ConsoleOutput.java
+++ src/main/java/lotto/io/ConsoleOutput.java
@@ -1,9 +1,13 @@
 package lotto.io;
 
+import java.util.List;
+import lotto.model.Lotto;
+
 public class ConsoleOutput implements Output {
     private static final String PURCHASE_PROMPT = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
+    private static final String PURCHASE_TEXT = "개를 구매했습니다.";
 
     @Override
     public void printPurchasePrompt() {
@@ -18,5 +22,12 @@ public class ConsoleOutput implements Output {
     @Override
     public void printBonusNumberPrompt() {
         System.out.println(BONUS_NUMBER_PROMPT);
+    }
+
+    @Override
+    public void printLottos(int count, List<Lotto> lottos) {
+        System.out.println(count + PURCHASE_TEXT);
+
+        lottos.forEach(System.out::println);
     }
 }
--- src/main/java/lotto/io/Output.java
+++ src/main/java/lotto/io/Output.java
@@ -1,9 +1,14 @@
 package lotto.io;
 
+import java.util.List;
+import lotto.model.Lotto;
+
 public interface Output {
     void printPurchasePrompt();
 
     void printWinningNumberPrompt();
 
     void printBonusNumberPrompt();
+
+    void printLottos(int count, List<Lotto> lottos);
 }
