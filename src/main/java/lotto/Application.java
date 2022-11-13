58d21337e439a73a300357b8639c1ebd79fc7e5d
JunginPark4364 <wjddlsee@gmail.com>
2022-11-14 05:50:30 +0900

    feat: 로또 시작

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.validator.Validator;
+import lotto.view.LottoView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new LottoController(new LottoView(new Validator())).start();
     }
 }
