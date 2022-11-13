c70eb7d8ab086f6b170a0d794c2bfaa820a70bff
Hayoon <hy97@sju.ac.kr>
2022-11-13 18:42:57 +0900

    test(NumberGeneratorTest): ParameterizedTest로 로또 번호 크기 계산 테스트코드 구현

--- src/test/java/lotto/NumberGeneratorTest.java
+++ src/test/java/lotto/NumberGeneratorTest.java
@@ -2,15 +2,37 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NumberGeneratorTest {
+    @DisplayName("로또 번호 입력 시 서로 다른 6개의 숫자여야 한다.")
+    @ParameterizedTest
+    @MethodSource("generateData")
+    void calculatorLottoSize(List<Integer> input, int expectedSize) {
+        HashSet<Integer> hashSet = new HashSet<>(input);
+        assertThat(hashSet.size()).isEqualTo(expectedSize);
+    }
+
+    static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6), 6),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 5), 5),
+                Arguments.of(Arrays.asList(1, 1, 2, 3, 3, 4), 4)
+        );
+    }
 
     @DisplayName("구매 금액만큼 로또 수량을 발행한다.")
     @Test
@@ -24,9 +46,8 @@ public class NumberGeneratorTest {
     @Test
     void convertedDataTypeStringToListInteger() {
         String[] inputValue = "1,2,3,4,5,6".split(",");
-        ;
-        List<Integer> test = new ArrayList<>();
         final NumberGenerator numberGenerator = new NumberGenerator();
+        List<Integer> test = new ArrayList<>();
         assertThat(numberGenerator.convertedLottoNumbers(List.of(inputValue)).getClass().getName())
                 .isEqualTo(test.getClass().getName());
     }
