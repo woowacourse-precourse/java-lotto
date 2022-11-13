3e2b69ebc2029ba2c475ad5e3304d3a858f1e2d2
jingyu9804 <swc05161@gmail.com>
2022-11-14 00:59:32 +0900

    refactor(lotto) : 유효하지 않은 로또 번호 에러 메시지 상수 추가

--- src/main/java/lotto/model/ErrorMessage.java
+++ src/main/java/lotto/model/ErrorMessage.java
@@ -2,7 +2,8 @@ package lotto.model;
 
 public enum ErrorMessage {
     NOT_STRING_ERROR_MESSAGE("[ERROR] 로또 금액은 숫자여야 합니다."),
-    NOT_DIVIDED_BY_THOUSAND_ERROR_MESSAGE("[ERROR] 1000원으로 나누어 떨어져야 합니다.");
+    NOT_DIVIDED_BY_THOUSAND_ERROR_MESSAGE("[ERROR] 1000원으로 나누어 떨어져야 합니다."),
+    INVALID_LOTTO_NUMBER_ERROR_MESSAGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
 
     private final String message;
 
