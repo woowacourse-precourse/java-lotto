a28aa51c0e80acdb289c430bc72906be6e23ceb9
황민주 <minjoo@hwangminjooui-MacBookAir.local>
2022-11-13 17:09:12 +0900

    feat(Exception): checkDistinctNumber (src/main/java/lotto/Exception.java)
    checkDistinctNumber 메소드 추가:
    -숫자 리스트에 특정 숫자가 포함되어있으면 예외로 처리하기 위해 추가함

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.Collections;
+import java.util.List;
 
 abstract public class Exception extends Throwable {
     public static long checkNumeric(String input){
@@ -14,13 +15,19 @@ abstract public class Exception extends Throwable {
         return number;
     }
 
-    public void checkDivisible(long dividend, long divisor){
+    public static void checkDivisible(long dividend, long divisor){
         long remainder = dividend % divisor;
         if(remainder!=0) throw new IllegalArgumentException();
     }
 
-    public void checkNumberRange(int minimum, int maximum, int number){
+    public static void checkNumberRange(int minimum, int maximum, int number){
         if (!(minimum<=number && number<=maximum)){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void checkDistinctNumber(List<Integer> numbers, int toCheckNumber){
+        if(numbers.contains(toCheckNumber)){
             throw new IllegalArgumentException();
         }
     }
