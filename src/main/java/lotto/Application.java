4e98d041268b67f188d98dd644ed94d15e496c36
dmajorcode <dianekang93@gmail.com>
2022-11-13 14:52:05 +0900

    add: defining functions needed 🎉

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 프로그램 시작
+        createNum();
+    }
+
+    // 당첨 번호 생성
+    public static void createNum(){
+        List<Integer> WinningNum = Randoms.pickUniqueNumbersInRange(1,45,6);
+        System.out.println(WinningNum);
     }
 }
