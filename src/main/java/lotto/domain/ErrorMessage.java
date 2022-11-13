6371ed50d1e6ce79280db482b7b3f70934a0b0c7
twoosky <lhjae3@naver.com>
2022-11-13 22:14:37 +0900

    feat(lotto): 에러 메시지 상수 선언

---
+++ src/main/java/lotto/domain/ErrorMessage.java
@@ -0,0 +1,7 @@
+package lotto.domain;
+
+public class ErrorMessage {
+    public static final String ERROR_OVER_SIZE = "[ERROR] 로또 번호는 6개여야 합니다.";
+    public static final String ERROR_DUPLICATED = "[ERROR] 로또 번호에 중복된 숫자가 존재합니다.";
+    public static final String ERROR_OVER_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+}
