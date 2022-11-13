1447546282736e99cb3fe7c0a06a2b03a2e50a5a
eugene1204 <s1061001@naver.com>
2022-11-13 18:59:29 +0900

    feat : winLotto메소드 생성

--- src/main/java/lotto/calculateLotto.java
+++ src/main/java/lotto/calculateLotto.java
@@ -27,6 +27,27 @@ public class calculateLotto {
     int convertBonus(String bonusLotto){
         return Integer.valueOf(bonusLotto);
     }
-    
+    List<Integer> winLotto(List<Lotto> lottos){
 
+        int[] correctNumber = {0,0,0,0,0};
+        for (Lotto lotto : lottos){
+            int count =0;
+            Set<Integer> set= new HashSet<>(inputLottto.getNumbers());
+            for(int i = 0; i<lotto.getNumbers().size();i++){
+                if(!set.add(lotto.getNumbers().get(i))) count++;
+            }
+            if(count<3) count=-1;
+            if(count>=3) count = count -3;
+            if(count == 6) count = 4;
+            if(lotto.getNumbers().contains(bonusLotto) && count== 5) {
+                count = 3;
+            }
+            System.out.println(count);
+            if(count>=0) correctNumber[count] +=1;
+        }
+
+        return Arrays.stream(correctNumber)
+                .boxed()
+                .collect(Collectors.toList());
+    }
 }
