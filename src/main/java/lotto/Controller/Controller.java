a357655f752e3f5d5a7e2d136f7c515b712c16ac
Park shinyeong <syngh503@gmail.com>
2022-11-13 22:30:08 +0900

    Fix(Controller): 예외 처리 시 로그 출력
    예외 발생 시 로그 메시지가 출력될 수 있도록 수정하였습니다.

--- src/main/java/lotto/Controller/Controller.java
+++ src/main/java/lotto/Controller/Controller.java
@@ -3,10 +3,11 @@ package lotto.Controller;
 import lotto.Domain.Player;
 import lotto.Domain.Rank;
 
+
 public class Controller {
-    LottoService lottoService;
-    PlayerService playerService;
-    RankingService rankingService;
+    private LottoService lottoService;
+    private PlayerService playerService;
+    private RankingService rankingService;
 
     public Controller() {
         Player player = new Player();
@@ -16,9 +17,14 @@ public class Controller {
         this.rankingService = new RankingService(rank, player);
     }
 
-    public void play() throws IllegalArgumentException {
-        this.lottoService.lottoService();
-        this.playerService.winningNumberService();
-        this.rankingService.rankService();
+    public void play() {
+        try{
+            this.lottoService.lottoService();
+            this.playerService.winningNumberService();
+            this.rankingService.rankService();
+        } catch(IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
+
     }
 }
