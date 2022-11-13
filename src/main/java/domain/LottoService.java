e5e527e4e8d9719895ae98ac31c671ff55afb957
Limjongho <whdgh9873@icloud.com>
2022-11-13 17:12:47 +0900

    feat(LottoService): 로또 등수 별 당첨 내역 반환 기능 추가

--- src/main/java/domain/LottoService.java
+++ src/main/java/domain/LottoService.java
@@ -2,9 +2,7 @@ package domain;
 
 import lotto.Lotto;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static util.Constant.LOTTO_PRICE;
@@ -12,30 +10,34 @@ import static util.Constant.LOTTO_PRICE;
 public class LottoService {
 
     private int totalEarning;
-    private List<Prize> userPrizes;
     private int money = 0;
 
     public LottoService() {
         this.totalEarning = 0;
-        this.userPrizes = new ArrayList<>();
     }
 
-    public Prize getResultOfLotto(
+    public Map<Prize, Integer> getWinningDetails(
             List<Integer> winningNumbers,
-            List<Integer> userNumbers,
+            List<Lotto> lottos,
             int bonusNumber
     ) {
-        Result result = new Result();
-        int matchingCount = result.countMatchingNumber(winningNumbers, userNumbers);
-        boolean checkBonusNumber = false;
-        if (matchingCount == 5) {
-            checkBonusNumber = result.isMatchingBonusNumber(userNumbers, bonusNumber);
+        List<Prize> prizes = new ArrayList<>();
+        for (Lotto lotto : lottos) {
+            Prize resultOfLotto = getResultOfLotto(winningNumbers, lotto.getUserLottoNumbers(), bonusNumber);
+            prizes.add(resultOfLotto);
         }
-        Rank rank = result.getRank(matchingCount, checkBonusNumber);
-        Prize prize = result.getPrize(rank);
-        userPrizes.add(prize);
-        earnMoney(prize.rankingPrize);
-        return prize;
+
+        Map<Prize, Integer> resultMap = new HashMap<>();
+        initResultMap(resultMap);
+
+        for (Prize prize : prizes) {
+            if (prize.equals(Prize.nothing)) {
+                continue;
+            }
+            int numberOfPrize = resultMap.get(prize);
+            resultMap.replace(prize, numberOfPrize + 1);
+        }
+        return resultMap;
     }
 
     public List<Lotto> publishLotto(int numberOfLotto) {
@@ -59,7 +61,32 @@ public class LottoService {
         return money / LOTTO_PRICE;
     }
 
+    private Prize getResultOfLotto(
+            List<Integer> winningNumbers,
+            List<Integer> userNumbers,
+            int bonusNumber
+    ) {
+        Result result = new Result();
+        int matchingCount = result.countMatchingNumber(winningNumbers, userNumbers);
+        boolean checkBonusNumber = false;
+        if (matchingCount == 5) {
+            checkBonusNumber = result.isMatchingBonusNumber(userNumbers, bonusNumber);
+        }
+        Rank rank = result.getRank(matchingCount, checkBonusNumber);
+        Prize prize = result.getPrize(rank);
+        earnMoney(prize.rankingPrize);
+        return prize;
+    }
+
     private void earnMoney(int prize) {
         this.totalEarning += prize;
+    }
+
+    private void initResultMap(Map<Prize, Integer> resultMap) {
+        resultMap.put(Prize.firstPrize, 0);
+        resultMap.put(Prize.secondPrize, 0);
+        resultMap.put(Prize.thirdPrize, 0);
+        resultMap.put(Prize.fourthPrize, 0);
+        resultMap.put(Prize.fifthPrize, 0);
     }
 }
