55f5f5047f7e7f4377d583c0a3025bf69c70c4c7
Kimhyeyun <cqqwer@gmail.com>
2022-11-13 19:33:49 +0900

    Feat: 로또 번호 중복 여부 검증

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,17 +2,28 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.constant.Message.DUPLICATE_LOTTO_NUMBER_MESSAGE;
+import static lotto.constant.Message.INVALID_LOTTO_NUMBER_SIZE_MESSAGE;
+
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        int count = (int) numbers.stream().distinct().count();
+        if (count < 6) {
+            throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBER_MESSAGE);
+        }
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_SIZE_MESSAGE);
         }
     }
 
