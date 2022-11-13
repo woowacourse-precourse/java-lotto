e42dee4999dac5f49e2e48cf094c1cb1dd8d312f
nn98 <jkllhgb@gmail.com>
2022-11-14 00:28:04 +0900

    Feat: 수익률 인스턴스 추가/로또 발권 메소드명 수정/결과 저장용 메소드 구현

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,12 +1,12 @@
 package lotto;
 
+import static constance.Values.*;
+
 import camp.nextstep.edu.missionutils.Randoms;
-import constance.Texts;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static constance.Texts.LOTTO_USER_PURCHASE_RESULT;
 
 public class User {
 
@@ -14,29 +14,79 @@ public class User {
     private List<List<Result>> results;
     private int lotteryCount;
     private int winnings;
+    private float lotteryStat;
 
-    public User(int amount){
+    public User(int amount) {
         this.setLotteryCount(amount);
-        this.setLotteries();
+        this.makeLotteries();
+        results=new ArrayList<>();
+        this.setResultsReady();
+        winnings = 0;
     }
-    private void setLotteryCount(int amount){
-        this.lotteryCount=amount/1000;
+
+    private void setLotteryCount(int amount) {
+        this.lotteryCount = amount / 1000;
     }
-    private void setLotteries(){
-        this.lotteries=new ArrayList<>();
-        for(int i=0;i<lotteryCount;++i){
-            lotteries.add(new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6)));
+
+    private void makeLotteries() {
+        this.lotteries = new ArrayList<>();
+        for (int i = 0; i < lotteryCount; ++i) {
+            lotteries.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
         }
     }
-    public String getLotteryList(){
-        StringBuffer resultBuffer=new StringBuffer();
-        for(Lotto lotto:lotteries){
-            resultBuffer.append(lotto+"\n");
+
+    public List<Lotto> getLotteries() {
+        List<Lotto> lotteries = new ArrayList<>();
+        for (Lotto lotto : this.lotteries) {
+            Lotto copyLotto = new Lotto(lotto.getNumbers());
+            lotteries.add(copyLotto);
+        }
+        return lotteries;
+    }
+
+    public String getLotteryList() {
+        StringBuffer resultBuffer = new StringBuffer();
+        for (Lotto lotto : lotteries) {
+            resultBuffer.append(lotto + "\n");
         }
         return resultBuffer.toString();
     }
 
     public int getLotteryCount() {
         return lotteryCount;
+    }
+
+    public void setResultsReady() {
+        for (int i = 0; i < WINNINGS.length; ++i) {
+            results.add(new ArrayList<>());
+        }
+    }
+
+    public void setResults(List<Result> results) {
+        for (Result result : results) {
+            int coincideCount = result.getCoincideCount();
+            int bonus = result.getBonus();
+            int bonusCount = 0;
+            if (bonus > 0) {
+                bonusCount = 2;
+            }
+            this.results.get(coincideCount + bonusCount).add(result);
+        }
+    }
+
+    public void setWinnings(int winnings) {
+        this.winnings = winnings;
+    }
+
+    public List<List<Result>> getResults() {
+        return results;
+    }
+
+    public float getLotteryStat() {
+        return lotteryStat;
+    }
+
+    public void setLotteryStat(float lotteryStat) {
+        this.lotteryStat = lotteryStat;
     }
 }
