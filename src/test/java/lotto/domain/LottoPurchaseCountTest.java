a8376b596fb3436d5ae984529368d967d0e3679a
CMHyuk <alsgur9710@naver.com>
2022-11-14 01:15:29 +0900

    test(LottoPurchaseCountTest): 오류 메시지 확인 테스트 추가

--- src/test/java/lotto/domain/LottoPurchaseCountTest.java
+++ src/test/java/lotto/domain/LottoPurchaseCountTest.java
@@ -19,14 +19,15 @@ class LottoPurchaseCountTest {
     @DisplayName("로또 개수 확인 테스트")
     @Test
     void checkLottoCount() {
-        int count = lottoPurchaseCount.getLottoCount(5000);
+        int count = lottoPurchaseCount.getLottoPurchaseCount(5000);
         assertThat(count).isSameAs(5);
     }
 
     @DisplayName("로또 금액 입력 예외 테스트")
     @Test
     void checkInputLottoMoneyException() {
-        assertThatThrownBy(() -> lottoPurchaseCount.getLottoCount(5500))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> lottoPurchaseCount.getLottoPurchaseCount(5500))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1000원 단위로 입력해주세요.");
     }
 }
