b212208b2749b7161dd07c988ed7392f03f36a29
ckn1070 <ckn1070@naver.com>
2022-11-14 01:27:34 +0900

    feat(lotto): implement a feature of calculating earning rate

--- src/main/java/lotto/ControlLottoGame.java
+++ src/main/java/lotto/ControlLottoGame.java
@@ -7,6 +7,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ControlLottoGame {
+    private final int THREE_CORRECT = 5000;
+    private final int FOUR_CORRECT = 50000;
+    private final int FIVE_CORRECT = 1500000;
+    private final int FIVE_BONUS_CORRECT = 30000000;
+    private final int SIX_CORRECT = 2000000000;
     private int quantity;
     private List<Integer> winningCount = Arrays.asList(0, 0, 0, 0, 0);  // 3개, 4개, 5개, 6개, 5+1개
 
@@ -20,18 +25,31 @@ public class ControlLottoGame {
         this.quantity = input / 1000;
     }
 
-    public void winningIndexToCount (List<List<Integer>> comparedNumber, List<Integer> winningNumber, int bonusNumber) {
+    public int calculateEarningRate(List<Integer> winningCount, int quantity) {
+        int payedPrice = quantity * 1000;
+        int earnedPrice = 0;
+
+        earnedPrice += winningCount.get(0) * THREE_CORRECT;
+        earnedPrice += winningCount.get(1) * FOUR_CORRECT;
+        earnedPrice += winningCount.get(2) * FIVE_CORRECT;
+        earnedPrice += winningCount.get(3) * SIX_CORRECT;
+        earnedPrice += winningCount.get(4) * FIVE_BONUS_CORRECT;
+
+        return earnedPrice;
+    }
+
+    public List<Integer> winningIndexToCount (List<List<Integer>> comparedNumber, List<Integer> winningNumber, int bonusNumber) {
         List<Integer> totalCount = checkWinningTotal(comparedNumber, winningNumber, bonusNumber);
         for (int i = 0; i < totalCount.size(); i++){
             int index = totalCount.get(i);
             int beforeCount = winningCount.get(index);
             this.winningCount.set(index, beforeCount + 1);
         }
+        return this.winningCount;
     }
 
-    public List<Integer> checkWinningTotal (List<List<Integer>> comparedNumber, List<Integer> winningNumber, int bonusNumber) {
+    private List<Integer> checkWinningTotal (List<List<Integer>> comparedNumber, List<Integer> winningNumber, int bonusNumber) {
         List<Integer> totalCount = new ArrayList<>();
-        int countIndex;
 
         for (int i = 0; i < comparedNumber.size(); i++) {
             int correctCount = checkWinning(comparedNumber.get(i), winningNumber);
@@ -46,7 +64,7 @@ public class ControlLottoGame {
         return totalCount;
     }
 
-    public int checkWinning(List<Integer> comparedNumber, List<Integer> winningNumber) {
+    private int checkWinning(List<Integer> comparedNumber, List<Integer> winningNumber) {
         int correctCount = 0;
 
         for (int i = 0; i < winningNumber.size(); i++) {
@@ -57,7 +75,7 @@ public class ControlLottoGame {
         return correctCount;
     }
 
-    public boolean checkWinningBonus(List<Integer> comparedNumber, int bonusNumber) {
+    private boolean checkWinningBonus(List<Integer> comparedNumber, int bonusNumber) {
         if (comparedNumber.contains(bonusNumber)) {
             return true;
         }
