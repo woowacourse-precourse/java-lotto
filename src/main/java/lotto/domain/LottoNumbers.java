0f7d766d40079c9376d158d91ecaf3a264127bc7
great-park <cksgh1735@naver.com>
2022-11-14 00:48:36 +0900

    fix: 오류 메시지 오타 수정

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 public class LottoNumbers {
     private static final String NUMBERS_SIZE_IS_NOT_SIX = "로또의 번호가 6개가 아닙니다.";
-    private static final String NUMBERS_IS_NOT_IN_RANGE = "로또의 번호 중 1부터 45까지의 범위 내 숫자가 포함되었습니다.";
+    private static final String NUMBERS_IS_NOT_IN_RANGE = "로또의 번호 중 1부터 45까지의 범위 밖의 숫자가 포함되었습니다.";
     private static final String DUPLICATE_NUMBERS_EXIST = "로또의 번호 중 중복된 숫자가 포함되었습니다.";
     private static final int MINIMUM_LOTTO_NUMBER = 1;
     private static final int MAXIMUM_LOTTO_NUMBER = 45;
