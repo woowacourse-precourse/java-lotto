b43a628bfeeeaf727a4c5b7cab76cc59816a94bb
HamSeungChan <head0618@naver.com>
2022-11-13 09:46:26 +0900

    "feat : 당첨 번호 입력 & List로 변환 기능 구현"

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,13 +2,32 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
 
   private String purchaseMoney;
+  private String winningNumbers;
 
   public int enterPurchaseMoney() {
     this.purchaseMoney = Console.readLine();
     return Integer.parseInt(purchaseMoney);
   }
+
+
+  public List<Integer> enterWinningNumbers() {
+    this.winningNumbers = Console.readLine();
+    return changeTypeForSystem(replaceComma(winningNumbers));
+  }
+
+  private String replaceComma(String winningNumbers) {
+    return winningNumbers.replace(",", "");
+  }
+
+  private List<Integer> changeTypeForSystem(String winningNumbers) {
+    return Arrays.asList(Integer.parseInt(winningNumbers));
+  }
 }
+
 
