bc8cacdb913752743b19ca64a3248029574b1a15
smc2315 <choeyongtae085@gmail.com>
2022-11-14 01:22:07 +0900

    feat: add Lotto validation

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -2,7 +2,7 @@ package lotto.model;
 
 import java.util.List;
 
-import static lotto.utils.ExceptionMessage.LOTTO_NUMBERS_NOT_SIX;
+import static lotto.utils.ExceptionMessage.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,8 +13,29 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateLottoCount(numbers);
+        validateLottoDistinct(numbers);
+        validateLottoRange(numbers);
+    }
+    private void validateLottoCount(List<Integer> numbers){
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException(LOTTO_NUMBERS_NOT_SIX);
+            throw new IllegalArgumentException(LOTTO_NUMBERS_NOT_SIX_EXCEPTION);
+        }
+    }
+
+    private void validateLottoDistinct(List<Integer> numbers){
+        int distinctCount =(int) numbers.stream()
+                .distinct()
+                .count();
+        if(distinctCount!=numbers.size()){
+            throw new IllegalArgumentException(LOTTO_NUMBERS_NOT_Distinct_EXCEPTION);
+        }
+    }
+    private void validateLottoRange(List<Integer> numbers){
+        for(int number : numbers){
+            if(number>45||number<1){
+                throw new IllegalArgumentException(LOTTO_NUMBERS_ARRANGE_EXCEPTION);
+            }
         }
     }
 
