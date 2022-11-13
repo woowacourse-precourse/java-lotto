9113da7475679193f3ce4dfbf8b629e933a5c08e
UngHeung <t01047377477@gmail.com>
2022-11-14 05:03:32 +0900

    refactor : (Lotto) 사용자 입력 클래스 코드 리팩토링

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -8,18 +8,27 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class UserInput {
 	
+	private Money money;
+	private Numbers numbers;
+	private Lotto lotto;
+	private Bonus bonus;
+	private Statistics statistics;
+	
 	public UserInput() {
 		inputMoney();
 		inputWinningNumbers();
 		inputBonus();
+		
+		statistics = new Statistics(numbers.getNumbers(), lotto.getLotto(), money.getMoney(), bonus.getBonus());
+		System.out.println(statistics.toString());
 	}
 	
 	private void inputMoney() {
 		System.out.println("구입금액을 입력해 주세요.");
-		Money money = new Money(Integer.parseInt(Console.readLine()));
+		money = new Money(Integer.parseInt(Console.readLine()));
 		
 		int lottoCount = money.getLottoCount();
-		Numbers numbers = new Numbers(lottoCount);
+		numbers = new Numbers(lottoCount);
 		
 		System.out.println(lottoCount + "개를 구매했습니다.");
 		System.out.println(numbers.toString());
@@ -35,11 +44,11 @@ public class UserInput {
 			winningNumbers.add(Integer.parseInt(token.nextToken()));
 		}
 		
-		Lotto lotto = new Lotto(winningNumbers);
+		lotto = new Lotto(winningNumbers);
 	}
 	
 	private void inputBonus() {
 		System.out.println("보너스 번호를 입력해 주세요.");
-		Bonus bonus = new Bonus(Integer.parseInt(Console.readLine()));
+		bonus = new Bonus(Integer.parseInt(Console.readLine()));
 	}
 }
