0b435593f0e5e474490889191a11a392b70c4d02
Park shinyeong <syngh503@gmail.com>
2022-11-13 23:11:54 +0900

    Feat(Lotto): validate 함수 구현
    입력값이 중복되지 않는 6자리 수인지 확인하는 validate 메소드를 구현하였습니다.
    
    Closes: #2

--- src/main/java/lotto/Domain/Lotto.java
+++ src/main/java/lotto/Domain/Lotto.java
@@ -1,21 +1,22 @@
 package lotto.Domain;
 
+import Utils.InputUtils;
 import Utils.RandomUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<Integer> numbers) throws IllegalArgumentException {
         validate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        final String ERROR_MESSAGE = "[ERROR] 중복하지 않는 6개의 숫자를 입력해야 합니다.";
+        if (!InputUtils.isNumberLengthSix(numbers) || !RandomUtils.isUniqueNumber(numbers)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
         }
     }
 
