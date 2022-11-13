89d3110a468e19740fd1f22090df47d24d104498
KangSungBum <peter9706@naver.com>
2022-11-13 13:56:08 +0900

    refactor(WinningPlace.java): 출력 요구사항에 맞게 수정
    출력 요구사항에 맞게 각 enum value message 수정
    - n개 일치 (당첨금) -
    - 형식으로 변경

--- src/main/java/lotto/WinningPlace.java
+++ src/main/java/lotto/WinningPlace.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum WinningPlace {
-    FIRST_PLACE(6,0,2000000000,"6개 번호 일치"),
-    SECOND_PLACE(5,1,30000000,"5개 번호 + 보너스 번호 일치"),
-    THIRD_PLACE(5,0,1500000,"5개 번호 일치"),
-    FOURTH_PLACE(4,0,50000,"4개 번호 일치"),
-    FIFTH_PLACE(3,0,5000,"3개 번호 일치"),
+    FIRST_PLACE(6,0,2000000000,"6개 일치 (2,000,000,000원) - "),
+    SECOND_PLACE(5,1,30000000,"5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD_PLACE(5,0,1500000,"5개 일치 (1,500,000원) - "),
+    FOURTH_PLACE(4,0,50000,"4개 일치 (50,000원) - "),
+    FIFTH_PLACE(3,0,5000,"3개 일치 (5,000원) - "),
     NOT_IN_PLACE(0, 0, 0, "순위 안에 들지 못했습니다.");
 
     private Integer correspondingNumber;
