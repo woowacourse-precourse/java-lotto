58d93ead40187a4b3c73f097396a864ff2fc0c99
CAEL0 <kjongmin26@gmail.com>
2022-11-13 16:26:48 +0900

    refactor(ExceptionConstants): '[ERROR]'를 생성자에서 붙이는 방식으로 수정

--- src/main/java/lotto/constant/ExceptionConstants.java
+++ src/main/java/lotto/constant/ExceptionConstants.java
@@ -4,20 +4,19 @@ import static lotto.constant.LottoConstants.*;
 
 public enum ExceptionConstants {
 
-    PREFIX("[ERROR] "),
+    PURCHASE_AMOUNT_NOT_NATURAL_NUMBER_EXCEPTION("구입금액은 자연수여야 합니다."),
+    PURCHASE_AMOUNT_INDIVISIBILITY_EXCEPTION(String.format("구입금액은 %d의 배수여야 합니다.", LOTTO_PRICE)),
 
-    PURCHASE_AMOUNT_NOT_NATURAL_NUMBER_EXCEPTION(PREFIX + "구입금액은 자연수여야 합니다."),
-    PURCHASE_AMOUNT_INDIVISIBILITY_EXCEPTION(PREFIX + String.format("구입금액은 %d의 배수여야 합니다.", LOTTO_PRICE)),
-
-    WINNING_NUMBER_FORMAT_EXCEPTION(PREFIX + ""),
-    LOTTO_NUMBER_NOT_NATURAL_NUMBER_EXCEPTION(PREFIX + "로또 번호는 자연수여야 합니다."),
-    LOTTO_NUMBER_RANGE_EXCEPTION(PREFIX + String.format("로또 번호는 %d이상 %d이하여야 합니다.", MIN_NUMBER, MAX_NUMBER)),
-    LOTTO_NUMBER_DUPLICATED_EXCEPTION(PREFIX + "로또 번호는 서로 중복되지 않아야 합니다.");
+    WINNING_NUMBER_FORMAT_EXCEPTION(""),
+    LOTTO_NUMBER_NOT_NATURAL_NUMBER_EXCEPTION("로또 번호는 자연수여야 합니다."),
+    LOTTO_NUMBER_RANGE_EXCEPTION(String.format("로또 번호는 %d이상 %d이하여야 합니다.", MIN_NUMBER, MAX_NUMBER)),
+    LOTTO_NUMBER_DUPLICATED_EXCEPTION("로또 번호는 서로 중복되지 않아야 합니다.");
 
     private final String message;
+    private static final String PREFIX = "[ERROR] ";
 
     ExceptionConstants(String message) {
-        this.message = message;
+        this.message = PREFIX + message;
     }
 
     @Override
