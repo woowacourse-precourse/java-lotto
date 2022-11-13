f4580cbf0a0f5f27dca5b18ef7d0b6a4e5470cfb
EUNSH1 <tkdgus968@naver.com>
2022-11-14 02:58:18 +0900

    feat : 사용자 복권 구입 티켓장수 출력 폼 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,14 +4,10 @@ import java.util.List;
 import lotto.domain.LottoNumbers;
 import lotto.domain.PlayerLottoAmount;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-//        PlayerLottoAmount playerLottoAmount = new PlayerLottoAmount(InputView.inputPlayerAmount());
-//        int result = playerLottoAmount.calculateLottoCount();
-//        System.out.println(result);
-        LottoNumbers lottoNumbers = new LottoNumbers();
-        List<Integer> list = lottoNumbers.setRandomNumbers();
-        System.out.println(list);
+
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_LOTTO_AMOUNT = "구입금액을 입력해 주세요.";
-    private static final String TYPE_ERROR = "[ERROR] 금액은 숫자만 입력 가능합니다.";
 
     public static int inputPlayerAmount(){
         System.out.println(INPUT_LOTTO_AMOUNT);
