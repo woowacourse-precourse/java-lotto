79b0e809cb83be6d7e2967c1aedafe519ca48afc
SHEOMM <esh0821@snu.ac.kr>
2022-11-13 18:50:46 +0900

    feat: Lotteries 생성 전체를 클래스 구현
    팩토리 패턴을 적용하여 Lotteries 생성 단일화

---
+++ src/main/java/lotto/domain/MakeLotteriesFactory.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.model.Lotto;
+
+public class MakeLotteriesFactory {
+
+    private final MakeLotteriesService makeLotteriesService;
+
+    public MakeLotteriesFactory(String input){
+        PurchaseService purchaseService = new PurchaseService();
+        purchaseService.parsePurchaseInput(input);
+        makeLotteriesService = new MakeLotteriesService();
+        makeLotteriesService.makeLotteries(purchaseService.getPurchaseAmount());
+    }
+
+    public List<Lotto> getLotteries(){
+        return makeLotteriesService.getLotteries();
+    }
+}
