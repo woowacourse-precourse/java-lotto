961d415b17bd266ef8598f2c7abe2f6bca8916cf
UngHeung <t01047377477@gmail.com>
2022-11-14 05:33:30 +0900

    feat : (Lotto) Money 클래스 예외 오류 메시지 출력 기능 추가

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -3,6 +3,7 @@ package lotto;
 public class Money {
 	private final int MONEY;
 	private int lottoCount;
+	private String errorMessage = "[ERROR] ";
 	
 	public Money(int money) {
 		validate(money);
@@ -12,7 +13,17 @@ public class Money {
 	
 	private void validate(int money) {
 		if (money % 1_000 != 0) {
+			errorMessage += "구매 후 남는 돈이 없도록 입력해 주세요.";
+			throwException();
+		}
+	}
+	
+	private void throwException() {
+		try {
 			throw new IllegalArgumentException();
+		} catch(IllegalArgumentException e) {
+			System.out.println(errorMessage);
+			return;
 		}
 	}
 	
