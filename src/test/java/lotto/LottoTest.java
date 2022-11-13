03b57363d5862bba2ae3acf789a23588ac6128df
unknown <ymecca12@gmail.com>
2022-11-13 23:23:32 +0900

    test : 초과범위시 6자리 일괄확인

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -35,13 +35,27 @@ class LottoTest {
                 .hasMessageContaining("[ERROR] 1,000원 단위로 입력해주세요");
     }
 
-    @DisplayName("당첨,로또 번호를 1~45 사이의 숫자를 입력하지 않는 경우 예외 발생")
+    @DisplayName("로또 번호를 1~45 사이의 숫자를 입력하지 않는 경우 예외 발생")
     @Test
-    void createLottoNumberOutOfUnderBound(){
+    void createLottoNumberOutOfBound(){
         Integer number = 0;
         assertThatThrownBy(() -> {
             if(number < 1 || number > 45){
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    @DisplayName("범위 초과 숫자 입력시 6자리 일괄 확인 후 예외 발생")
+    @Test
+    void createBonusNumberOutOfBound(){
+        List<Integer> prizeNumbers = List.of(1,3,24,35,21,46);
+        assertThatThrownBy(() -> {
+            for (Integer prizeNumber : prizeNumbers) {
+                if(prizeNumber < 1 || prizeNumber > 45){
+                    throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                }
             }
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
