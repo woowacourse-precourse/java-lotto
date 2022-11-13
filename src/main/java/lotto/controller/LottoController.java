fa6539b00458a653060bd68becad1ad36aec60ba
Devtist <yt930310@snu.ac.kr>
2022-11-13 14:12:47 +0900

    feat: 발행할 수 있는 수량만큼 로또를 발행하는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,10 @@
 package lotto.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
 import lotto.domain.PurchasingAmount;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -7,9 +12,27 @@ import lotto.view.OutputView;
 public class LottoController {
 	InputView inputView = new InputView();
 	OutputView outputView = new OutputView();
-	public void excute() {
+	int numberOfTickets;
+	List<Lotto> lotteries;
+	public void control() {
+		calculateNumberOfTickets();
+		issueLotteries();
+	}
+	private void calculateNumberOfTickets() {
 		PurchasingAmount purchasingAmount = new PurchasingAmount(Integer.parseInt(inputView.getPurchasingAmount()));
-		int numberOfTickets = purchasingAmount.getNumberOfTickets();
+		numberOfTickets = purchasingAmount.getNumberOfTickets();
 		outputView.printNumberOfTickets(numberOfTickets);
+	}
+
+	void issueLotteries() {
+		LottoGenerator lottoGenerator = new LottoGenerator();
+		lotteries = new ArrayList<>();
+		while (lotteries.size() != numberOfTickets) {
+			lotteries.add(new Lotto(lottoGenerator.getNumbers()));
+		}
+	}
+
+	List<Lotto> getLotteries() {
+		return lotteries;
 	}
 }
