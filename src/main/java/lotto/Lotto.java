8fd436d0a88cc9a3124d3bd2f059d5946ff6913b
haisley77 <qhfg3145@naver.com>
2022-11-13 16:10:05 +0900

    feat (calculatePrize(), setPrize()): calculate and set lotto prize

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,12 +3,33 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
 public class Lotto {
+    enum statistics {
+        prize1(1,2000000000),
+        prize2(2,30000000),
+        prize3(3,1500000),
+        prize4(4,50000),
+        prize5(5,5000),
+        noprize(0,0);
+
+        private int rank;
+        private int money;
+
+        statistics(int rank, int money){
+            this.rank = rank;
+            this.money = money;
+        }
+    }
     private static int number_of_lotto;
     private static List<List<Integer>> purchased_lotto_numbers;
     private static int bonus_number = 0;
+    private static statistics win_lotto;
+    private static ArrayList<Integer> status_of_win = new ArrayList<>(5);
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -51,6 +72,49 @@ public class Lotto {
     private void enterBonusNumber() {
         bonus_number = Integer.parseInt(Console.readLine());
     }
+
+    private void calculatePrize() {
+        int count = 0;
+        for (int i = 0;i<purchased_lotto_numbers.size();i++){
+            for (int j = 0; j<purchased_lotto_numbers.get(i).size();j++){
+                if (numbers.contains(purchased_lotto_numbers.get(i).get(j)){
+                    count++;
+                }
+            }
+
+            setPrize(count,purchased_lotto_numbers.get(i));
+
+            if (win_lotto.rank!=0){
+                // 로또 당첨 통계를 계산해 status_of_win 리스트에 저장
+                status_of_win.add(count-1,status_of_win.get(count-1)+1);
+            }
+        }
+    }
+
+    private void setPrize(int count, List<Integer> my_lotto) {
+        if (count==5){
+            if (my_lotto.contains(bonus_number)){
+                win_lotto = statistics.prize2;
+            }
+            if (!my_lotto.contains(bonus_number)){
+                win_lotto = statistics.prize3;
+            }
+        }
+        if (count == 6) {
+            win_lotto = statistics.prize1;
+        }
+        if (count == 4){
+            win_lotto = statistics.prize4;
+        }
+        if (count == 3){
+            win_lotto = statistics.prize5;
+        }
+        if (count <=2 || count >= 7){
+            win_lotto = statistics.noprize;
+        }
+        return;
+    }
+
 
 
 
