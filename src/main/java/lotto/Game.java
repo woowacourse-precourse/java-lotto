9af1f42355add0dd86a1bb670cbed254af620ff6
nn98 <jkllhgb@gmail.com>
2022-11-13 19:25:38 +0900

    Feat: 사용자/사용자 로또/당첨 로또 생성, 정상적 생성 확인

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,11 +1,56 @@
 package lotto;
 
+import constance.Texts;
+
+import java.util.List;
+
+import static constance.Texts.LOTTO_USER_PURCHASE_RESULT;
+
 public class Game {
+
+    private final Checker checker = new Checker();
+    private User user;
+    private WinningLotto winningLotto;
     public void run(){
         this.play();
     }
 
     public void play(){
-        User user=new User();
+
+        String amount;
+        String numbers;
+        String bonusNumber;
+        int checkedAmount;
+        List<Integer> checkedNumbers;
+        int checkedBonusNumber;
+
+        System.out.println(Texts.LOTTO_USER_INPUT_PURCHASE_AMOUNT);
+        amount = camp.nextstep.edu.missionutils.Console.readLine();
+
+        checkedAmount=checker.checkAmountInput(amount);
+
+        user=new User(checkedAmount);
+
+        System.out.println();
+        System.out.println(String.format(LOTTO_USER_PURCHASE_RESULT,user.getLotteryCount(),user.getLotteryList()));
+
+        System.out.println(Texts.LOTTO_USER_INPUT_WINNING_NUMBERS);
+        numbers = camp.nextstep.edu.missionutils.Console.readLine();
+
+        checkedNumbers = checker.checkNumbersInput(numbers);
+
+        System.out.println();
+        System.out.println(Texts.LOTTO_USER_INPUT_BONUS_NUMBERS);
+        bonusNumber = camp.nextstep.edu.missionutils.Console.readLine();
+
+        checkedBonusNumber=checker.checkNumber(bonusNumber);
+
+        winningLotto=new WinningLotto(checkedNumbers,checkedBonusNumber);
+
+        System.out.println("!--Test: "+winningLotto);
+
+        System.out.println();
+        System.out.println(Texts.LOTTO_GAME_WINNING_STATS);
+
     }
 }
