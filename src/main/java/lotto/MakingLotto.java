22dfabb6b6222244d7ea6260a314de17301ed676
CJ <choijun0627@naver.com>
2022-11-13 18:24:18 +0900

    <feat>(MakingLotto.java): 기능 요구 사항 5번 구현

--- src/main/java/lotto/MakingLotto.java
+++ src/main/java/lotto/MakingLotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -16,7 +17,11 @@ public class MakingLotto {
     }
 
     public List<List<Integer>> getAllLotto(int LottoCount){
-        return null;
+        List<List<Integer>> allLotto=new ArrayList<>();
+        for(int index=0;index<LottoCount;index++){
+            allLotto.add(makeLotto());
+        }
+        return allLotto;
     }
 
 }
