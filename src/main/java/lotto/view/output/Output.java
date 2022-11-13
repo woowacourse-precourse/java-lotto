9f2ca0981aa1fdd3bcbdc5937b78aa7960b058f0
5uhwann <5uhwann27@gmail.com>
2022-11-13 16:07:12 +0900

    feat(출력): 발행 로또숫자 및 번호 출력 구현

--- src/main/java/lotto/view/output/Output.java
+++ src/main/java/lotto/view/output/Output.java
@@ -1,6 +1,11 @@
 package lotto.view.output;
 
+import java.util.List;
+import lotto.domain.lotto.lotto.Lotto;
+
 public interface Output {
 
     void printMessage(String outputMessage);
+
+    void printPublishedLotto(List<Lotto> publishedLotto);
 }
--- src/main/java/lotto/view/output/OutputImpl.java
+++ src/main/java/lotto/view/output/OutputImpl.java
@@ -1,9 +1,20 @@
 package lotto.view.output;
 
+import java.util.List;
+import lotto.domain.lotto.lotto.Lotto;
+
 public class OutputImpl implements Output{
 
     @Override
     public void printMessage(String outputMessage) {
         System.out.println(outputMessage);
+    }
+
+    @Override
+    public void printPublishedLotto(List<Lotto> publishedLotto) {
+        System.out.println(publishedLotto.size()+"개를 구매했습니다.");
+        for (Lotto lotto : publishedLotto) {
+            System.out.println(lotto.getLottoNumbers());
+        }
     }
 }
