7e4cacbc3ef5627b58e478228691b5f8945d0126
abbd2 <ajkety00@gmail.com>
2022-11-13 16:09:06 +0900

    Feat(ShowErrorMessage): "[ERROR} 당첨 번호는 중복될 수 없습니다."를 출력하는 showDuplicateNumber 메서드 구현

--- src/main/java/lotto/ShowErrorMessage.java
+++ src/main/java/lotto/ShowErrorMessage.java
@@ -6,6 +6,7 @@ public class ShowErrorMessage {
     private static final String OUT_RANGE_LOTTO_NUMBER_MESSAGE = "로또 번호는 1부터 45 사이의 정수여야 합니다.";
     private static final String NOT_DIVIDE_MESSAGE = "금액은 1,000으로 나누어 떨어져야 합니다.";
     private static final String LOTTO_NUMBER_COUNT = "당첨 번호의 개수는 6개여야 합니다.";
+    private static final String DUPLICATE_LOTTO_NUBMER = "당첨 번호는 중복될 수 없습니다.";
 
     public static void showOutRangeLottoNumber() {
         System.out.println(ERROR_MESSAGE + OUT_RANGE_LOTTO_NUMBER_MESSAGE);
@@ -16,6 +17,11 @@ public class ShowErrorMessage {
     }
 
     public static void showLottoNumberCount() {
-        System.out.println(LOTTO_NUMBER_COUNT);
+        System.out.println(ERROR_MESSAGE + LOTTO_NUMBER_COUNT);
     }
+
+    public static void showDuplicateLottoNumber() {
+        System.out.println(ERROR_MESSAGE + DUPLICATE_LOTTO_NUBMER);
+    }
+
 }
