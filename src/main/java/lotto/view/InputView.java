6936b8d2858f54dbaca49b49f0936e83b2392975
jiwon1217 <koagjw@gmail.com>
2022-11-13 22:34:22 +0900

    feat: 보너스 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,8 +3,10 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String NEW_LINE = "\n";
     private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public static String inputMoney() {
         System.out.println(INPUT_MONEY);
@@ -14,5 +16,11 @@ public class InputView {
     public static String inputWinningLottoNumbers() {
         System.out.println(INPUT_WINNING_LOTTO_NUMBERS);
         return Console.readLine();
+    }
+
+    public static Integer inputBonusNumber() {
+        System.out.printf(NEW_LINE);
+        System.out.println(INPUT_BONUS_NUMBER);
+        return Integer.parseInt(Console.readLine());
     }
 }
