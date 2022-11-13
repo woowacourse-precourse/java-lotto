774ea6702878b81d848e64b404d35749aa62d2f4
wath1457 <wath1457@gmail.com>
2022-11-13 15:44:46 +0900

    feat(Bonus): 보너스 번호와 당첨번호가 중복된 경우 예외처리

--- src/main/java/lotto/domain/Bonus.java
+++ src/main/java/lotto/domain/Bonus.java
@@ -1,10 +1,19 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Bonus {
     private final int bonusNumber;
 
-    public Bonus(int bonusNumber) {
+    public Bonus(int bonusNumber, Lotto winningLotto) {
+        validateBonusDuplication(winningLotto, bonusNumber);
         this.bonusNumber = bonusNumber;
     }
-
+    private static void validateBonusDuplication(Lotto winningLotto, int bonusNumber) {
+        List<Integer> lottoNumbers =  winningLotto.getLotto();
+        if (lottoNumbers.contains(bonusNumber)) {
+            System.out.println("[ERROR} 보너스 번호는 담첨 번호에 포함될 수 없습니다.");
+            throw new IllegalArgumentException("[ERROR] 잘못된 보너스 번호");
+        }
+    }
 }
