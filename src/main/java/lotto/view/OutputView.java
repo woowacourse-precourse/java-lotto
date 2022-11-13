d1f009671caf10f60f92de23aa868cb3ef376fc1
Minjune <minjune8506@gmail.com>
2022-11-13 19:16:21 +0900

    feat(Lotto): 등수별 당첨 로또 개수 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
+import lotto.LottoResult;
 import lotto.Lottos;
+import lotto.Rank;
 
 public class OutputView {
 
@@ -9,4 +11,13 @@ public class OutputView {
         System.out.println(lottos);
     }
 
+    public static void printRanks(LottoResult lottoResult) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.printf("3개 일치 (5,000원) - %d개\n", lottoResult.get(Rank.FIFTH.name()));
+        System.out.printf("4개 일치 (50,000원) - %d개\n", lottoResult.get(Rank.FOURTH.name()));
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", lottoResult.get(Rank.THIRD.name()));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", lottoResult.get(Rank.SECOND.name()));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", lottoResult.get(Rank.FIRST.name()));
+    }
 }
