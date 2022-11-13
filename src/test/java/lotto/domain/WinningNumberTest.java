e3fad666dc4002a3b40b40d49bddbba338dee74f
MINJU <alswn9938@gmail.com>
2022-11-14 01:36:35 +0900

    test(WinningNumber): 보너스 번호 입력 함수

---
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class WinningNumberTest {
+
+    @Nested
+    @DisplayName("보너스 번호 관련 검증 로직")
+    class ValidateBonus{
+        WinningNumber winningNumber = new WinningNumber(Arrays.asList(1,2,3,4,5,6));
+
+        @Test
+        @DisplayName("중복 숫자 입력시 예외 발생")
+        void 중복_숫자(){
+            // given
+            int bonus = 1;
+            // when
+            // then
+            assertThatThrownBy(() -> winningNumber.updateBonus(bonus))
+                            .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("범위 벗어난 숫자 입력시 예외 발생")
+        void 범위_벗어난_숫자(){
+            // given
+            int bonus = 49;
+            // when
+            // then
+            assertThatThrownBy(() -> winningNumber.updateBonus(bonus))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+}
