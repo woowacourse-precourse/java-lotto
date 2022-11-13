ceecb08ff5a5b6090c2fbcf3df0790c6aa811c92
Heejun-0219 <heejun0219@gmail.com>
2022-11-14 00:02:02 +0900

    feat(Application.run) : 컨트롤러를 통해 로또 뽑기를 시작한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.Controller.Lottocontroller;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Lottocontroller lottocontroller = new Lottocontroller();
+        lottocontroller.run();
     }
 }
