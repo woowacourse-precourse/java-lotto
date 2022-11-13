34add55558cd86bb29e5bc8c55846e5766f4f9c8
3juhwan <13selfesteem91@naver.com>
2022-11-14 00:59:33 +0900

    feat(lotto): 로또 구매자 구현

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -4,10 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    public List<Lotto> lottos = new ArrayList<>();
-    public Integer numberOfLotto;
+    private List<Lotto> lottoInHand = new ArrayList<>();
+    private Integer numberOfLotto = 0;
 
-    public void buyLotto() {
+    public void buyLotto(Lotto lotto) {
+        lottoInHand.add(lotto);
+        numberOfLotto++;
+    }
 
+    public List<Lotto> getLottoInHand() {
+        return lottoInHand;
+    }
+
+    public Integer getNumberOfLotto() {
+        return numberOfLotto;
     }
 }
