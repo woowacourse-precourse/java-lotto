f31fb4090b5f270edf9f91409f325f6c8e59a445
dldbdud314 <dldbdud314@naver.com>
2022-11-13 13:01:10 +0900

    feat(Lotto): validation 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,12 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,8 +15,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validateSize(numbers.size());
+        validateDuplicate(numbers);
+    }
+
+    private void validateSize(int size) {
+        if (size != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("로또 번호 개수는 6개로 제한됩니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+        if (nonDuplicateNumbers.size() != LOTTO_NUMBERS_SIZE){
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
         }
     }
 
