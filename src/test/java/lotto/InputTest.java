323501ece3d2ea786ab10836259e633b9824892a
hyun-seung <ardimento22@naver.com>
2022-11-14 03:09:30 +0900

    [TEST] 로또 구입 금액을 입력받는다.

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -5,8 +5,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 
-import lotto.controller.MoneyController;
-import lotto.domain.Money;
+import camp.nextstep.edu.missionutils.Console;
 import lotto.utils.ExceptionType;
 import lotto.view.Input;
 import org.junit.jupiter.api.AfterEach;
@@ -17,38 +16,42 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.MockedStatic;
 
 public class InputTest {
-    private MockedStatic<Input> input;
+    private MockedStatic<Console> console;
     @BeforeEach
     void before() {
-        input = mockStatic(Input.class);
+        console = mockStatic(Console.class);
     }
 
     @AfterEach
     void after() {
-        input.close();
+        console.close();
     }
 
-    @DisplayName("정확한 금액을 입력받는 경우 Money 객체가 return된다.")
+    @DisplayName("정확한 숫자를 입력한 경우 해당 숫자가 return된다.")
     @ParameterizedTest
     @ValueSource(strings = {"1000", "3000", "80000"})
-    void inputMoney(String target) {
-        when(Input.readLine()).thenReturn(target);
+    void readNumber(String target) {
+        when(Console.readLine()).thenReturn(target);
 
-        MoneyController moneyController = new MoneyController();
-        Money money = moneyController.getMoney();
-
-        assertThat(money.getMoney()).isEqualTo(Integer.parseInt(target));
+        assertThat(Input.readNumber()).isEqualTo(Integer.parseInt(target));
     }
 
     @DisplayName("숫자가 아닌 것을 입력받는 경우 에러가 발생한다")
     @ParameterizedTest
     @ValueSource(strings = {"!1102", "err0r", "string"})
-    void inputMoneyByNotNumber(String target) {
-        when(Input.readLine()).thenReturn(target);
+    void readNumberByNotNumber(String target) {
+        when(Console.readLine()).thenReturn(target);
 
-        MoneyController moneyController = new MoneyController();
-
-        assertThatThrownBy(moneyController::getMoney).isInstanceOf(IllegalArgumentException.class)
+        assertThatThrownBy(Input::readNumber).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionType.IS_NOT_NUMBER.getMessage());
+    }
+
+    @DisplayName("정확한 문자열을 입력한 경우 해당 문자열이 return된다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6", "3000", "string"})
+    void readLine(String target) {
+        when(Console.readLine()).thenReturn(target);
+
+        assertThat(Input.readLine()).isEqualTo(target);
     }
 }
