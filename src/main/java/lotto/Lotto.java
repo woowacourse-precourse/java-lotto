d9e69e5c252977ac27e8b1aa3bfaa0cac2b131d6
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-11-13 20:50:10 +0900

    feat: 중복 숫자 검사 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,11 +1,15 @@
 package lotto;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        numbers = new ArrayList<>(numbers);
+        numbers.sort(Comparator.naturalOrder());
         validate(numbers);
         this.numbers = numbers;
     }
@@ -13,6 +17,17 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6)
             throw new IllegalArgumentException("6개의 숫자를 입력해야 합니다.");
+
+        checkNumberDuplicated(numbers);
     }
 
+    private void checkNumberDuplicated(List<Integer> numbers) {
+        Integer beforeNumber = -1;
+
+        for (Integer number: numbers) {
+            if (beforeNumber == number)
+                throw new IllegalArgumentException("중복되지 않는 숫자를 입력해야 합니다.");
+            beforeNumber = number;
+        }
+    }
 }
