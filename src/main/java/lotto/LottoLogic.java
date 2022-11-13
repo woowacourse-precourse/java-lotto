2a1cfae28ffcdc164fabbed63150af8cce253fb3
chaerim <qkrdbsk28@naver.com>
2022-11-14 04:58:57 +0900

    feat: 로또 생성 기능 추가

--- src/main/java/lotto/LottoLogic.java
+++ src/main/java/lotto/LottoLogic.java
@@ -1,13 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoLogic {
+    private final static int START_LOTTO = 1;
+    private final static int END_LOTTO = 45;
+    private final static int LOTTO_NUMBER = 6;
     private static final int ONE_LOTTO = 1000;
     private int money;
     private int numberOfLotto;
+    private List<Lotto> buyLottoList;
 
     public LottoLogic(int money) {
         this.money = money;
         this.numberOfLotto = calculateNumberOfLotto();
+        this.buyLotto();
     }
 
     private int calculateNumberOfLotto() {
@@ -16,5 +25,15 @@ public class LottoLogic {
 
     public int getNumberOfLotto() {
         return this.numberOfLotto;
+    }
+
+    private void buyLotto() {
+        this.buyLottoList = new ArrayList<>();
+        for (int i = 0; i < this.numberOfLotto; i++) {
+            this.buyLottoList.add(new Lotto(createLottoNumber()));
+        }
+    }
+    private List<Integer> createLottoNumber() {
+        return Randoms.pickUniqueNumbersInRange(START_LOTTO, END_LOTTO, LOTTO_NUMBER);
     }
 }
