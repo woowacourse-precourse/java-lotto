4fe261efb3702fffa09e0ed0ca00aff750bcd091
EUNJEONGMUN <munej26@gmail.com>
2022-11-13 12:13:42 +0900

    feat: MatchCount 클래스 생성 시 예외 처리 로직 구현

--- src/main/java/constant/Constant.java
+++ src/main/java/constant/Constant.java
@@ -3,6 +3,7 @@ package constant;
 public class Constant {
     public static final int INITIAL_NUMBER = 0;
     public static final int LOTTO_SIZE = 6;
+    public static final int BONUS_SIZE = 1;
     public static final int LOTTO_NUMBER_MIN = 1;
     public static final int LOTTO_NUMBER_MAX = 45;
     public static final int MONEY_UNIT = 1000;
--- src/main/java/lotto/domain/MatchCount.java
+++ src/main/java/lotto/domain/MatchCount.java
@@ -1,12 +1,27 @@
 package lotto.domain;
 
+import static constant.Constant.*;
+
 public class MatchCount {
     private int winningCount;
     private int bonusCount;
 
     public MatchCount(int winningCount, int bonusCount) {
+        valid(winningCount, bonusCount);
         this.winningCount = winningCount;
         this.bonusCount = bonusCount;
+    }
+
+    private void valid(int winningCount, int bonusCount) {
+        if (winningCount<INITIAL_NUMBER || winningCount>LOTTO_SIZE) {
+            throw new IllegalArgumentException();
+        }
+        if (bonusCount<INITIAL_NUMBER || bonusCount>BONUS_SIZE) {
+            throw new IllegalArgumentException();
+        }
+        if (winningCount+bonusCount>LOTTO_SIZE) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public boolean isSameResult(MatchCount o) {
