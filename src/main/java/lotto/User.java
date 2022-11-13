ecbff183bb4260b66983e38f79e4d847632782bb
d0ri123 <lsh81550@gmail.com>
2022-11-14 08:21:20 +0900

    fix(lotto): 복권 구매 개수 출력하는 기능을 User로 이동 및 구매금액이 1000단위가 아닐 때 예외 발생 구현

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class User {
     private int lottoPrice;
+    private int lottoEA;
     private List<Integer> userNumbers;
     private int bonusNumber;
 
@@ -14,18 +15,27 @@ public class User {
         this.lottoPrice = sc.nextInt();
     }
 
+    public void printLottoEA(){
+        lottoEA = lottoPrice/1000;
+        if((lottoEA%=1000) != 0)
+            throw new IllegalArgumentException();
+        System.out.println(lottoEA + "개를 구매했습니다.");
+    }
+
     public int getLottoPrice(){
         return this.lottoPrice;
     }
 
-    public void userNumbers(){
+    public List<Integer> userNumbers(){
         System.out.println("당첨 번호를 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
-        String numbers = sc.nextLine();
+        String nums = sc.nextLine();
 
-        for(String num : numbers.split(",")){
+        for(String num : nums.split(",")){
             this.userNumbers.add(Integer.valueOf(num));
         }
+
+        return this.userNumbers;
     }
 
     public List<Integer> getUserNumbers(){
