26823b9215707fd3fe98a385a9786014c7bf8c7b
UJ15 <dbwogml15@naver.com>
2022-11-14 06:36:10 +0900

    test : add compare winning number test

--- src/test/java/lotto/model/WinningNumberTest.java
+++ src/test/java/lotto/model/WinningNumberTest.java
@@ -2,8 +2,11 @@ package lotto.model;
 
 import static lotto.Application.ERROR_PREFIX;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -22,16 +25,46 @@ class WinningNumberTest {
     private static final String BONUS_FORMAT_ERROR_MESSAGE =
             ERROR_PREFIX + "보너스 숫자는 1~45 숫자를 당첨 숫자와 중복되지 않게 입력해주세요";
 
+    WinningNumber winningNumber;
+
+    @BeforeEach
+    void setUp() {
+        String validNumbers = "1,2,3,4,5,6";
+        String validBonusNumber = "7";
+
+        winningNumber = WinningNumber.of(validNumbers, validBonusNumber);
+    }
+
     static class ofNullAndEmptySourceProvider implements ArgumentsProvider {
         String validNumbers = "1,2,3,4,5,6";
         String validBonusNumber = "1";
+
         @Override
         public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
             return Stream.of(
                     Arguments.of(null, validBonusNumber),
                     Arguments.of(validNumbers, null),
                     Arguments.of("", validBonusNumber),
-                    Arguments.of(validNumbers,"")
+                    Arguments.of(validNumbers, "")
+            );
+        }
+    }
+
+    static class CompareToWinningNumberSourceProvider implements ArgumentsProvider {
+        Lotto threeNumberCorrectLotto = new Lotto(List.of(1, 2, 3, 10, 11, 12));
+        Lotto fourNumberCorrectLotto = new Lotto(List.of(1, 2, 3, 4, 11, 12));
+        Lotto fiveNumberCorrectLotto = new Lotto(List.of(1, 2, 3, 4, 5, 12));
+        Lotto fiveAndBonusNumberCorrectLotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        Lotto sixNumberCorrectLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    Arguments.of(threeNumberCorrectLotto, Winning.THREE),
+                    Arguments.of(fourNumberCorrectLotto, Winning.FOUR),
+                    Arguments.of(fiveNumberCorrectLotto, Winning.FIVE),
+                    Arguments.of(fiveAndBonusNumberCorrectLotto, Winning.FIVE_BONUS),
+                    Arguments.of(sixNumberCorrectLotto, Winning.SIX)
             );
         }
     }
@@ -82,4 +115,36 @@ class WinningNumberTest {
         }
     }
 
+    @Nested
+    @DisplayName("compareToWinningNumber 메서드는")
+    class DescribeCompareToWinningNumber {
+        @Test
+        @DisplayName("null을 인자로 받으면 IllegalArgumentException을 반환한다.")
+        void throwIllegalArgumentException() {
+            Lotto nullArgument = null;
+
+            Assertions.assertThatThrownBy(() -> winningNumber.compareToWinningNumber(nullArgument))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @ArgumentsSource(CompareToWinningNumberSourceProvider.class)
+        @DisplayName("유효한 lotto객체를 인자로 받으면 당첨된 수의 Winning을 반환한다.")
+        void returnWinning(Lotto lotto, Winning expect) {
+
+            Optional<Winning> actual = winningNumber.compareToWinningNumber(lotto);
+
+            Assertions.assertThat(actual).isEqualTo(Optional.of(expect));
+        }
+
+        @Test
+        @DisplayName("당첨되지 않은 유효한 lotto객체를 인자로 받으면 Optional.empty()를 반환한다.")
+        void returnOptionalEmpty() {
+            Lotto lostLotto = new Lotto(List.of(11, 12, 13, 14, 15, 16));
+
+            Optional<Winning> actual = winningNumber.compareToWinningNumber(lostLotto);
+
+            Assertions.assertThat(actual).isEqualTo(Optional.empty());
+        }
+    }
 }
