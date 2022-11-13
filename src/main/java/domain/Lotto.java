fef43f8f7418285af1f62403e004e2621f94d781
Kim-Jong-Gyu <whdrb2643@gmail.com>
2022-11-13 17:33:47 +0900

    feat(lotto): 로또 번호에 중복된 값이 들어갈 경우 예외처리 기능 추가
    로또 번호에 중복된 값이 들어갈 경우 예외처리 기능 추가:
    - validateOverlap(List<Integer> numbers) : Set을 이용한 중복값 제거를 통해 길이 차이를 이용한 중복값 체크

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -3,6 +3,7 @@ package domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
@@ -11,14 +12,30 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateOverlap(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        try {
+            if (numbers.size() != 6)
+                throw new IllegalArgumentException();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 로또 번호 길이가 잘못되었습니다");
+            System.exit(0);
         }
+
     }
 
     // TODO: 추가 기능 구현
+    private void validateOverlap(List<Integer> numbers) {
+        HashSet<Integer> numbersOverlap = new HashSet<>(numbers);
+        try {
+            if (numbers.size() != numbersOverlap.size())
+                throw new IllegalArgumentException();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 로또 번호에 중복된 값이 있습니다");
+            System.exit(0);
+        }
+    }
 }
