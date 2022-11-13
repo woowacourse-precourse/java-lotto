adc6d1acf46f99fac2c8eb48df95ad1297105ee1
nakalic <nakalic28@gmail.com>
2022-11-13 21:46:28 +0900

    feat(InputView): 구입금액을 입력받는 기능 구현

---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static int getAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+
+        return getMoney(Console.readLine());
+    }
+
+    public static int getMoney(String input) {
+        int money = Integer.parseInt(input);
+        System.out.println();
+
+        return money / 1000;
+    }
+}
