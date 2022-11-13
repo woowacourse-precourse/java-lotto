79aabc593bdaa3e9a2083f6db87af492b3342174
qdtjj4 <76ylo1@gmail.com>
2022-11-14 02:08:04 +0900

    feat: 구입금액 입력 기능 구현
    문자열 형태로 입력받아 변환하는 과정에서 숫자가 아닌 경우 예외를 발생시킴

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    static int inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String purchaseAmount = Console.readLine();
+        return convertToNumber(purchaseAmount);
+    }
+
+    static int convertToNumber(String purchaseAmount) {
+        try {
+            return Integer.parseInt(purchaseAmount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구입금액은 양의 정수여야 합니다.");
+        }
     }
 }
