2f8d981edae59d698d5380ca1bfbf2cb696b0f09
hyun-seung <ardimento22@naver.com>
2022-11-14 02:15:36 +0900

    [STYLE] 로또 구입 금액을 입력받는다.

--- src/main/java/lotto/controller/MoneyController.java
+++ src/main/java/lotto/controller/MoneyController.java
@@ -1,15 +1,12 @@
 package lotto.controller;
 
-import static lotto.utils.Converter.stringToInt;
-
 import lotto.domain.Money;
 import lotto.view.Input;
 
 public class MoneyController {
 
     public Money getMoney() {
-        String line = Input.readLine();
-        int convertedLine = stringToInt(line);
-        return new Money(convertedLine);
+        int number = Input.readNumber();
+        return new Money(number);
     }
 }
