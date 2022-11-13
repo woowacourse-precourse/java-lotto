ed959b92df39d12f39aa635d553f14559973e887
egoing <dudqk9696@naver.com>
2022-11-13 20:02:14 +0900

    feat : Enum 클래스 당첨 숫자와 당첨 금액 연결 기능 구현

--- src/main/java/util/WinNum.java
+++ src/main/java/util/WinNum.java
@@ -1,5 +1,21 @@
 package util;
 
 public enum WinNum {
-    Three,Four,Five,Six,Bonus,Nothing
+    Three("5000"),
+    Four("50000"),
+    Five("1500000"),
+    Bonus("30000000"),
+    Six("2000000000"),
+
+    Nothing("0")
+    ;
+    private final String label;
+
+    WinNum(String label) {
+        this.label = label;
+    }
+
+    public String label() {
+        return label;
+    }
 }
