7c9f01e0562edf9c8a864d7d1585f5e8a0bc340b
Bumsoo <rhqjatn2398@naver.com>
2022-11-13 21:58:57 +0900

    feat(lotto): 구입금액만큼 로또를 발행한다. 발행한 각 로또의 번호들 사이에 중복은 없다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,16 @@
 package lotto;
 
 
+import java.util.List;
+
 public class Application {
     public static int LOTTERY_PRICE = 1000;
 
     public static void main(String[] args) {
         InputOutput inputOutput = new InputOutput(LOTTERY_PRICE);
+        LottoPublisher lottoPublisher = new LottoPublisher(LOTTERY_PRICE);
 
         int money = inputOutput.moneyInput();
-
-
+        List<Lotto> lottos = lottoPublisher.publish(money);
     }
 }
--- src/main/java/lotto/LottoPublisher.java
+++ src/main/java/lotto/LottoPublisher.java
@@ -1,2 +1,46 @@
-package lotto;public class LottoPublisher {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class LottoPublisher {
+    private static final int LOTTERY_NUM_CNT = 6;
+    private static final int START_NUM = 1;
+    private static final int END_NUM = 45;
+    private final int LOTTERY_PRICE;
+
+    public LottoPublisher(int lotteryPrice) {
+        this.LOTTERY_PRICE = lotteryPrice;
+    }
+
+    public List<Lotto> publish(int money) {
+        int lottoCnt = money / LOTTERY_PRICE;
+
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCnt; i++) {
+            Lotto lotto = new Lotto(generateNums());
+            lottos.add(lotto);
+        }
+
+        return lottos;
+    }
+
+    private List<Integer> generateNums() {
+        List<Integer> nums = new ArrayList<>();
+        while (nums.size() != LOTTERY_NUM_CNT) {
+            int num = Randoms.pickNumberInRange(START_NUM, END_NUM);
+
+            if (nums.contains(num)) {
+                continue;
+            }
+
+            nums.add(num);
+        }
+
+        nums.sort(Comparator.naturalOrder());
+
+        return nums;
+    }
 }
