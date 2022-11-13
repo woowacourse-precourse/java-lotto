4ee29073f24f118e8ca3e2ea0dbd0cc97c3bdba2
jiwon1217 <koagjw@gmail.com>
2022-11-13 22:08:42 +0900

    refactor: 접근제어자 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,8 +3,8 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
-    public static final String INPUT_WINNING_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
 
     public static String inputMoney() {
         System.out.println(INPUT_MONEY);
