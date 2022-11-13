54c97990821b250fa238b14864b9da5b9f42cc4c
parkjungyoon <tbr06057@naver.com>
2022-11-13 15:00:34 +0900

    feat(Domain): 구매한 로또 번호와 당첨 번호 비교하는 메서드

---
+++ src/main/java/lotto/domain/CompareLotto.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+public class CompareLotto {
+    private final static int LOTTO_COUNT = 6;
+
+    public int getMatchingLottoNumberCount(Lotto purchaseLotto, Lotto lottoNumbers) {
+        int matchingNumber = 0;
+        for (int i = 0; i < LOTTO_COUNT; i++) {
+            if (lottoNumbers.contains(purchaseLotto.get(i))) {
+                matchingNumber++;
+            }
+        }
+        return matchingNumber;
+    }
+}
