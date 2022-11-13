e0c664a27dfcec7d80f753a2ab0359f3f91ef62b
bearjun05 <bearjun05@naver.com>
2022-11-14 01:02:58 +0900

    feat(Lotto) : 로또 개수 세기 유효 검사 기능 구현

--- src/main/java/lotto/domain/LotteryShop.java
+++ src/main/java/lotto/domain/LotteryShop.java
@@ -1,17 +1,27 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LotteryShop {
-
+    private static final int LOTTO_PRICE = 1000;
     private int lottoCnt;
+    public List<Integer> lottoNum;
 
-    public LotteryShop (int totalPayment) {
+    public LotteryShop(int totalPayment) {
         countLotto(totalPayment);
     }
 
     private void countLotto(int totalPayment) {
         validate(totalPayment);
+        lottoCnt = totalPayment / LOTTO_PRICE;
     }
 
     private void validate(int totalPayment) {
+        if (totalPayment % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException();
+        }
+        if (totalPayment - LOTTO_PRICE < 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }
