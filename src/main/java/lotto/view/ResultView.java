43e3b7a7f25d26954d7cad0f90ca80ff256d37cb
Longstick0 <789456jang@naver.com>
2022-11-13 20:44:23 +0900

    fix: 당첨 통계 결과물이 Set 클래스로 정의되어 있어서 뒤죽박죽 섞여 나오는 버그 -> 순서대로 나오게 변경

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -10,9 +10,7 @@ import java.util.Map;
 
 public class ResultView {
 
-    private static final String ERROR_MESSAGE_FORMAT = "[ERROR] %s";
-    private static final String PRE_SCORE_MESSAGE = "당첨 통계\n" +
-            "---";
+    private static final String PRE_SCORE_MESSAGE = "당첨 통계\n---";
     private static final String MONEY_FORMAT = "###,###";
     private static final String SCORE_MESSAGE_FORMAT = "%s (%s원) - %d개";
     private static final String PURCHASE_LOTTO_NUM_FORMAT = "%s개를 구매했습니다.";
@@ -33,16 +31,14 @@ public class ResultView {
     public void showWinningResult(WinningResult winningResult) {
         System.out.println(PRE_SCORE_MESSAGE);
         DecimalFormat decimalFormat = new DecimalFormat(MONEY_FORMAT);
+        Map<Winning, Integer> winningResultMap = winningResult.getWinningResultMap();
 
-        for (Map.Entry<Winning, Integer> winningIntegerEntry : winningResult.getWinningResultMap().entrySet()) {
-            Winning key = winningIntegerEntry.getKey();
+        for (Winning winning : Winning.values()) {
             String format = String.format(SCORE_MESSAGE_FORMAT,
-                    key.getMatchMessage(),
-                    decimalFormat.format(key.getPrizeMoney()),
-                    winningIntegerEntry.getValue());
+                    winning.getMatchMessage(),
+                    decimalFormat.format(winning.getPrizeMoney()),
+                    winningResultMap.get(winning));
             System.out.println(format);
         }
     }
-
-
 }
