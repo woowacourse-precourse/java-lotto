ff82dee0713fd1fb19decec968942a70cdd0b221
namtndus <rnrmf0721@gmail.com>
2022-11-13 17:45:07 +0900

    feat(InputView): 금액을 묻는 함수 구현

--- src/main/java/ConstDto/InputOutputConstDto.java
+++ src/main/java/ConstDto/InputOutputConstDto.java
@@ -2,18 +2,18 @@ package ConstDto;
 
 public class InputOutputConstDto {
 
-    private final static String BUY_LOTTO_MESSAGE = "구입금액을 입력해 주세요.";
-    private final static String PRINT_LOTTO_SIZE_MESSAGE = "개를 구매했습니다.";
-    private final static String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    private final static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    private final static String WINNING_STATS = "당첨 통계";
-    private final static String THREE_MATCHES = "3개 일치 (5,000원) - ";
-    private final static String FOUR_MATCHES = "4개 일치 (50,000원) - ";
-    private final static String FIVE_MATCHES = "5개 일치 (1,500,000원) - ";
-    private final static String FIVE_BONUS_NUMBER_MATCHES = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
-    private final static String SIX_MATCHES = "6개 일치 (2,000,000,000원) - ";
-    private final static String DEPENDENT_NOUN = "개";
-    private final static String OUTPUT_YIELD = "총 수익률은 ";
-    private final static String LAST_WORD = "입니다.";
+    public final static String BUY_LOTTO_MESSAGE = "구입금액을 입력해 주세요.";
+    public final static String PRINT_LOTTO_SIZE_MESSAGE = "개를 구매했습니다.";
+    public final static String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public final static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public final static String WINNING_STATS = "당첨 통계";
+    public final static String THREE_MATCHES = "3개 일치 (5,000원) - ";
+    public final static String FOUR_MATCHES = "4개 일치 (50,000원) - ";
+    public final static String FIVE_MATCHES = "5개 일치 (1,500,000원) - ";
+    public final static String FIVE_BONUS_NUMBER_MATCHES = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    public final static String SIX_MATCHES = "6개 일치 (2,000,000,000원) - ";
+    public final static String DEPENDENT_NOUN = "개";
+    public final static String OUTPUT_YIELD = "총 수익률은 ";
+    public final static String LAST_WORD = "입니다.";
 
 }
---
+++ src/main/java/View/InputView.java
@@ -0,0 +1,11 @@
+package View;
+
+import static ConstDto.InputOutputConstDto.BUY_LOTTO_MESSAGE;
+
+public class InputView {
+
+    public void printLotoBuyMoney(){
+        System.out.println(BUY_LOTTO_MESSAGE);
+    }
+
+}
