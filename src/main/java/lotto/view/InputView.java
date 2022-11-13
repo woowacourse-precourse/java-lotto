0a4bb3155b78446779cc1510520ac22e40138bf7
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-13 12:09:05 +0900

    feat(InputView): 사용자 입력 관련 기능 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,18 @@
+package lotto.view;
+
+import static lotto.message.InputMessageConstants.*;
+
+public class InputView {
+
+	public void printPurchaseAmountMessage() {
+		System.out.print(PURCHASE_AMOUNT_MESSAGE);
+	}
+
+	public void printWinningNumberMessage() {
+		System.out.print(WINNING_NUMBER_MESSAGE);
+	}
+
+	public void printBonusNumberMessage() {
+		System.out.print(BONUS_NUMBER_MESSAGE);
+	}
+}
