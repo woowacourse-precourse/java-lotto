9fe95041fbc56f4b569c7686d4b534918746192d
recicoco068 <gkapf838@gmail.com>
2022-11-13 23:09:00 +0900

    feat(Utils.java) : 1 ~ 45 중 6개의 번호를 뽑는 함수 구현

--- src/main/java/lotto/Utils.java
+++ src/main/java/lotto/Utils.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Utils {
     public static final int START_NUMBER = 1;
     public static final int END_NUMBER = 45;
+    public static final int NUMBER_COUNT = 6;
 
     public static boolean validateLottoNumbers(List<Integer> numbers) {
         for (int n : numbers) {
@@ -13,5 +15,9 @@ public class Utils {
             }
         }
         return true;
+    }
+
+    public static List<Integer> pickUnique6Numbers() {
+        return Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, NUMBER_COUNT);
     }
 }
