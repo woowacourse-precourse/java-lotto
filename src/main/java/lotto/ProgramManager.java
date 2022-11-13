0a03cf7c8923c51a6b888ca93c831e2e374709ed
euna <christinejung10@daum.net>
2022-11-14 07:11:04 +0900

    feat(ProgramManager): add try-catch to print error messages

--- src/main/java/lotto/ProgramManager.java
+++ src/main/java/lotto/ProgramManager.java
@@ -14,15 +14,21 @@ public class ProgramManager {
     private static final String ASK_PREDICT_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String ASK_PREDICT_LOTTO_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
+    // 당첨 통계
     private static final String LOTTO_STATISTICS_MESSAGE_TITLE = "당첨 통계\n---\n";
     private static final String LOTTO_STATISTICS_MESSAGE_BODY_SAME_NUMBER_PRE = "개 일치";
     private static final String LOTTO_STATISTICS_MESSAGE_BODY_SAME_NUMBER_BONUS_BALL = ", 보너스 볼 일치";
     private static final String LOTTO_STATISTICS_MESSAGE_BODY_REWARD_PRE = " (";
     private static final String LOTTO_STATISTICS_MESSAGE_BODY_REWARD_POST = "원) - ";
     private static final String LOTTO_STATISTICS_MESSAGE_BODY_REWARD_NUMBER = "개\n";
-
     private static final String LOTTO_STATISTICS_MESSAGE_BODY_REVENUE_RATE_PRE = "총 수익률은 ";
     private static final String LOTTO_STATISTICS_MESSAGE_BODY_REVENUE_RATE_POST = "%입니다.";
+
+    // 에러 메세지
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    private static final String ERROR_MESSAGE_WRONG_PRICE_VALUE = " 로또는 " + ProgramManager.LOTTO_PRICE + "원 단위로 구매하실 수 있습니다.";
+    private static final String ERROR_MESSAGE_WRONG_LOTTO_VALUE = " 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String ERROR_MESSAGE_WRONG_BONUS_VALUE = " 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
 
     private static int price;
     private static int numberOfLotto;
@@ -48,7 +54,11 @@ public class ProgramManager {
 
     private static void buyLotto() {
         System.out.println(ASK_PRICE);
-        price = UserManager.getInput_price();
+        try {
+            price = UserManager.getInput_price();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(ERROR_MESSAGE + ERROR_MESSAGE_WRONG_PRICE_VALUE);
+        }
         numberOfLotto = price / LOTTO_PRICE;
         System.out.println("\n" + numberOfLotto + NUMBER_OF_LOTTO_MESSAGE);
     }
@@ -63,11 +73,19 @@ public class ProgramManager {
     private static void getUserPredictLottoNumbers() {
         // 당첨 번호 입력받기
         System.out.println(ASK_PREDICT_LOTTO_NUMBER);
-        userPredictLottoNumbers = UserManager.getInput_predictLottoNumber();
+        try {
+            userPredictLottoNumbers = UserManager.getInput_predictLottoNumber();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(ERROR_MESSAGE + ERROR_MESSAGE_WRONG_LOTTO_VALUE);
+        }
 
         // 보너스 번호 입력받기
         System.out.println(ASK_PREDICT_LOTTO_BONUS_NUMBER);
-        userPredictBonusNumber = UserManager.getInput_predictBonusLottoNumber();
+        try {
+            userPredictBonusNumber = UserManager.getInput_predictBonusLottoNumber();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(ERROR_MESSAGE + ERROR_MESSAGE_WRONG_BONUS_VALUE);
+        }
     }
 
     private static void getLottoStatistics() {
