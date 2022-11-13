2917b99899c72aea548a902a4974c5372a839333
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-13 21:36:01 +0900

    feat(main/Operation): 수익과 수익률을 계산한다.
    - calculateEarning(): 수익
    - calculateEarningRate(): 수익률=수익/구입금액
    
    해결: 2. 당첨 통계 계산- d), 수익률을 계산한다.

--- src/main/java/lotto/Operation.java
+++ src/main/java/lotto/Operation.java
@@ -6,11 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Operation {
-    static final int MATCH3 = 3;
-    static final int MATCH4 = 4;
-    static final int MATCH5 = 5;
-    static final int MATCH6 = 6;
-    static final int MATCH5_BONUS = 0;
 
     static List<Lotto> buyLotto(int amount) {
         List<Lotto> lottos = new ArrayList<>();
@@ -31,5 +26,22 @@ public class Operation {
         }
         return numbers;
     }
+
+    static int calculateEarning(Match match) {
+        int earning = 0;
+        earning += match.match3 * 5000;
+        earning += match.match4 * 50000;
+        earning += match.match5 * 1500000;
+        earning += match.match5Bonus * 30000000;
+        earning += match.match6 * 2000000000;
+        return earning;
+    }
+
+    static double calculateEarningRate(int purchaseAmount, int earning){
+        double earningRate = (double) earning/ (double) purchaseAmount;
+        earningRate= Math.round(earningRate * 1000) / 10.0;
+        return earningRate;
+    }
+
 
 }
