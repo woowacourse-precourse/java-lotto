00775392960e9ce39a8a06fc3372dd67393be687
60jong <rudwhd515@gmail.com>
2022-11-13 20:21:02 +0900

    feat(Application) : 로또 프로그램 실행 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 협력 객체 초기화
+        LottoFactory lottoFactory = new LottoFactory();
+        LottoService lottoService = new LottoServiceImpl(lottoFactory);
+        LottoController lottoController = new LottoController(lottoService);
+        LottoProgram lottoProgram = new LottoProgram(lottoController);
+
+        // 로또 프로그램 실행
+        lottoProgram.run();
     }
 }
