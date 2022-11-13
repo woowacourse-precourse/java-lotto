960b8d1b71ba2af1e38e770a938c9e3e3f6105a9
jamangstangs <jamangsong@gmail.com>
2022-11-14 05:55:53 +0900

    feat(IllegalLottoNumberException): LottoNumber에 관한 CustomException 생성

---
+++ src/main/java/lotto/exception/IllegalLottoNumberException.java
@@ -0,0 +1,13 @@
+package lotto.exception;
+
+import java.util.NoSuchElementException;
+
+public class IllegalLottoNumberException extends IllegalArgumentException {
+
+    public IllegalLottoNumberException() {
+    }
+
+    public IllegalLottoNumberException(String s) {
+        throw new NoSuchElementException();
+    }
+}
