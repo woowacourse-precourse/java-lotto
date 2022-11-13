008366721cd7fe1774996f797690f82cfb010112
dohyun <ldh19738@gmail.com>
2022-11-13 23:35:34 +0900

    refactor(Lotto): 예외 처리 메세지를 상수 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,11 +1,12 @@
 package lotto;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Lotto {
+    private static final String INVALIDATE_NUMBER_SIZE_MESSAGE = "[ERROR] 로또 번호는 6개만 가능합니다.";
+    private static final String INVALIDATE_DUPLICATION_MESSAGE = "[ERROR] 로또 번호는 중복될 수 없습니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,15 +17,15 @@ public class Lotto {
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개만 가능합니다.");
+            throw new IllegalArgumentException(INVALIDATE_NUMBER_SIZE_MESSAGE);
         }
     }
     // TODO: 추가 기능 구현
     private void validateDuplication(List<Integer> numbers) {
-        Set<Integer> nonDulicationNumbers = new HashSet<>(numbers);
+        Set<Integer> nonDuplicationNumbers = new HashSet<>(numbers);
 
-        if (nonDulicationNumbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        if (nonDuplicationNumbers.size() != 6) {
+            throw new IllegalArgumentException(INVALIDATE_DUPLICATION_MESSAGE);
         }
     }
 
@@ -34,9 +35,5 @@ public class Lotto {
 
     public void printLotto() {
         System.out.println(numbers);
-    }
-
-    public void ascendingOrderSort() {
-        Collections.sort(numbers);
     }
 }
