5af16e354263d49374700db5daad32b46fb24232
poter0501 <poter0501@gmail.com>
2022-11-13 21:54:10 +0900

    feat(User): 구입금액이 검증하는 기능 구현

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -9,4 +9,22 @@ public class User {
     private float earningRate;
 
 
+    public User(int price){
+        validatePrice(price);
+        this.price=price;
+    }
+    private void validatePrice(int price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 음수일 수 없습니다.");
+        }
+        if (price <= 1000) {
+            throw new IllegalArgumentException("[ERROR] 최소 구입 금액은 1000원입니다.");
+        }
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위로 입력 할 수 있습니다.");
+        }
+
+    }
+
+
 }
