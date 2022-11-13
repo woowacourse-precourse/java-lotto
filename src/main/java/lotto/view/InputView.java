70ed612d0e256781b38580c3b31281a1fac7563f
Yunsik-Choi <choiys0311@gmail.com>
2022-11-13 19:33:44 +0900

    ⚡feat : InputView 입력 가이드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,6 +7,9 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final String REST = ",";
+    private static final String INPUT_WINNING_LOTTO_GUIDE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_GUIDE = "보너스 번호를 입력해 주세요.";
+    private static final String INPUT_PURCHASE_MONEY_GUIDE = "구입금액을 입력해 주세요.";
 
     private final InputViewValidator inputViewValidator;
 
@@ -15,6 +18,7 @@ public class InputView {
     }
 
     public int inputPurchaseMoney() {
+        System.out.println(INPUT_PURCHASE_MONEY_GUIDE);
         String text = Console.readLine();
         inputViewValidator.validateNumber(text);
         int inputValue = Integer.parseInt(text);
@@ -23,6 +27,7 @@ public class InputView {
     }
 
     public List<Integer> inputWinningLotto() {
+        System.out.println(INPUT_WINNING_LOTTO_GUIDE);
         String text = Console.readLine();
         inputViewValidator.validateContainRest(text);
         inputViewValidator.validateSplitByRestNumber(text);
@@ -33,6 +38,7 @@ public class InputView {
     }
 
     public int inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_GUIDE);
         String text = Console.readLine();
         inputViewValidator.validateNumber(text);
         return Integer.parseInt(text);
