0b5731e18e0da97a45f9cbe2ffd2ee86bb6214af
yejin <kwonyj1022@naver.com>
2022-11-13 14:39:41 +0900

    feat: Bonus 클래스 - 보너스 번호 유효성 검사 구현

---
+++ src/main/java/lotto/Bonus.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.List;
+
+public class Bonus {
+    private final int bonus;
+
+    Bonus(int bonus) {
+        validate(bonus);
+        this.bonus = bonus;
+    }
+
+    private void validate(int bonus) {
+        if (!numbersInRange(bonus)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private boolean numbersInRange(int bonus) {
+        return (bonus >= 1) && (bonus <= 45);
+    }
+}
