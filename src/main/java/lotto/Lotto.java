df56bd99f051c7503848f790fa4449e4e334472f
bando99 <bando991225@gmail.com>
2022-11-13 19:37:27 +0900

    로또별 등수 처리기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,11 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-
+    private int rank;
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
@@ -17,4 +18,32 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public void printLotto(){
+        System.out.println(numbers);
+    }
+
+    public int checkWin(WinningNumber winNum){
+        int cnt = 0;
+        for(int i = 0 ; i < numbers.size(); i++){
+            List<Integer> winnumber = winNum.getNumbers();
+            if(winnumber.contains(this.numbers.get(i))){
+                cnt = cnt +1;
+            }
+        }
+        if(cnt == 3){
+            rank = 5;
+        }
+        if(cnt == 4){
+            rank = 4;
+        }
+        if(cnt == 5){
+            rank = 3;
+            if(this.numbers.contains(winNum.getBonusNumber())){rank = 2;}
+        }
+        if(cnt == 6){
+            rank = 1;
+        }
+        return rank;
+    }
+
 }
