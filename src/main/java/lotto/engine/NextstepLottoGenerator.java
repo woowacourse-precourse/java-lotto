89271c275590b14d8efe795551a28efd9c0bec1a
UJ15 <dbwogml15@naver.com>
2022-11-14 00:07:25 +0900

    feat : create NextstepLottoGenerator class with generate method

---
+++ src/main/java/lotto/engine/NextstepLottoGenerator.java
@@ -0,0 +1,19 @@
+package lotto.engine;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NextstepLottoGenerator implements NumberGenerator {
+    private static final int START_RANGE_NUMBER = 1;
+    private static final int END_RANGE_NUMBER = 45;
+    private static final int COUNT = 6;
+
+    @Override
+    public List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(START_RANGE_NUMBER, END_RANGE_NUMBER, COUNT).stream()
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+}
