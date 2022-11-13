8496dc631a4c2a44547778d20dc81ea8f197740d
wlsh44 <k87913j@gmail.com>
2022-11-13 18:07:07 +0900

    test: 잘못된 테스트 코드 수정

--- src/test/java/lotto/domain/LottoSummaryTest.java
+++ src/test/java/lotto/domain/LottoSummaryTest.java
@@ -76,11 +76,11 @@ class LottoSummaryTest {
         List<Rank> ranks = List.of(FIFTH, NOTHING, NOTHING, NOTHING, NOTHING, NOTHING, NOTHING, NOTHING);
         Money money = new Money(8000);
         String expect = "3개 일치 (5,000원) - 1개\n" +
-                "4개 일치 (50,000원) - 2개\n" +
+                "4개 일치 (50,000원) - 0개\n" +
                 "5개 일치 (1,500,000원) - 0개\n" +
                 "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n" +
                 "6개 일치 (2,000,000,000원) - 0개\n" +
-                "총 수익률은 62.0%입니다.";
+                "총 수익률은 62.5%입니다.";
 
         //when
         String summary = new LottoSummary(ranks, money).summaryLottoResult();
