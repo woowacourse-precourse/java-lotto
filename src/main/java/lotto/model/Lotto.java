e52c582cfff44cc78c5504cf6faea64d70bf6d3b
leesky <leehaneul0623@gmail.com>
2022-11-13 15:17:51 +0900

    feat(lotto) : 추첨 기능 구현
    로또 결과를 확인하기 위해서 추첨 기능 구현 :
     - 맞춘 결과 별 등수 검증 테스트 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -21,14 +21,27 @@ public class Lotto {
         if (set.size() != 6) throw new IllegalArgumentException("로또의 각 번호는 중복이 없어야 합니다.");
     }
 
+    private int getCorrectCount(List<Integer> answer) {
+        int count = 0;
+        for (Integer number : getNumbers()) {
+            if (answer.contains(number)) count += 1;
+        }
+        return count;
+    }
+
     public List<Integer> getNumbers() {
         return numbers;
     }
 
     public int draw(List<Integer> answer, int bonus) {
+        int correct = getCorrectCount(answer);
+        if (correct == 6) return 1;
+        if (correct == 5 && getNumbers().contains(bonus)) return 2;
+        if (correct == 5) return 3;
+        if (correct == 4) return 4;
+        if (correct == 3) return 5;
         return 0;
     }
-
 
     @Override
     public String toString() {
