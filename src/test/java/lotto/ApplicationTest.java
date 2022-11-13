c15cd27512547c18087093db8b29e738b9ec9295
seokjin8678 <rnrn8678@naver.com>
2022-11-13 22:10:16 +0900

    style: 코드 서식 정리
    - ExceptionConst 클래스 상수 맨 위로 이동
    - ApplicationTest 공백 수정

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -49,8 +49,8 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
-                runException("1000j");
-                assertThat(output()).contains(ERROR_MESSAGE);
+            runException("1000j");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
