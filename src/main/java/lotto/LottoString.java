6523f1972bb0000ead9592168a6ca0ebe89141c1
peterysh <52229539+peterysh@users.noreply.github.com>
2022-11-13 16:46:08 +0900

    refactor: move error message from LottoString class to ErrorMessage class

--- src/main/java/lotto/LottoString.java
+++ src/main/java/lotto/LottoString.java
@@ -8,8 +8,7 @@ public class LottoString {
     public static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
     public static String WINNING_STATISTICS = "당첨 통계\n---";
     public static String UNMATCH_PRICE_UNIT = "[ERROR] 구입 금액은 천원 단위의 숫자 입니다.";
-    public static String NUMBER_INPUT_ERROR = "[ERROR] 로또 번호는 6자리 1부터 45 사이의 숫자만으로 ,로 구분되어야 합니다.";
-    public static String NUMBER_MAKER_ERROR = "[ERROR] 로또 번호는 6자리 1부터 45 사이의 숫자만으로 구성 되어야 합니다.";
+
     public static String purchaserMessage(int price){
         return String.format("%d개를 구매했습니다.", price);
     }
---
+++ src/main/java/lotto/global/ErrorMessage.java
@@ -0,0 +1,7 @@
+package lotto.global;
+
+public class ErrorMessage {
+    public static String SERVER_INTERNAL_ERROR = "[ERROR] 서버 내부 오류입니다.";
+    public static String NUMBER_INPUT_ERROR = "[ERROR] 로또 번호는 6자리 1부터 45 사이의 숫자만으로 ,로 구분되어야 합니다.";
+    public static String NUMBER_MAKER_ERROR = "[ERROR] 로또 번호는 6자리 1부터 45 사이의 숫자만으로 구성 되어야 합니다.";
+}
