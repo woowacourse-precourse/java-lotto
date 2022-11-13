b21e6a02f9b258a6492be636daf8ad7ccc5fc926
keon0711 <fnc6488@gmail.com>
2022-11-14 01:29:06 +0900

    feat(lotto): validate bonus number

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -8,7 +8,17 @@ public class WinningLotto {
 
     public WinningLotto(Lotto lotto, int bonusNumber) {
         winninglotto = lotto;
+        validateBonusNumber(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(int bonusNumber) {
+        if (bonusNumber < 0 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 0에서 45 사이의 숫자를 입력해야 합니다.");
+        }
+        if (winninglotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 당첨번호에 존재하는 숫자를 입력하면 안됩니다.");
+        }
     }
 
     public int countMatchNumber(Lotto lotto) {
