aee9a2a15e8a97f28f72e628edbcf5e1e71b8e89
1eejisoo <jisu991012@gmail.com>
2022-11-14 04:42:57 +0900

    feat: 예외 상황 시 에러 문구 출력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,14 @@
 package lotto;
 
-import controller.MoneyController;
 import view.GameView;
 
 public class Application {
     public static void main(String[] args) {
         GameView gameView = new GameView();
-        gameView.start();
+        try {
+            gameView.start();
+        } catch (Exception e) {
+            System.out.println("[ERROR] " + e.getMessage());
+        }
     }
 }
