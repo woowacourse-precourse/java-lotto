3e55492c5e881b7904c9ee5896f3c54549ca7f64
inwoo <korean5078@gmail.com>
2022-11-13 21:24:13 +0900

    refactor : getNumberOfLotto 함수 리팩토링

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,15 +5,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("구입금액을 입력해 주세요.");
         int NumberOfLotto = getNumberOfLotto();
     }
 
     public static int getNumberOfLotto() {
+        System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         int money = 0;
-        if (!input.matches("[+-]?\\d*(\\.\\d+)?"))
+        if (!input.matches("[+-]?\\d*(\\.\\d+)?")) {
             throw new IllegalArgumentException("[ERROR] 금액을 제대로 입력해주세요.");
+        }
         money = Integer.parseInt(input);
         return money / 1000;
     }
