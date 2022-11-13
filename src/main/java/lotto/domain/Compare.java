ad46f61d4fdf4606f4ac95df0147088010824477
Lee-SungMin <saevers26@gmail.com>
2022-11-13 23:04:44 +0900

    feat(lotto): 당첨 번호와 사용자가 구매한 로또 번호 비교 기능 구현

---
+++ src/main/java/lotto/domain/Compare.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Compare {
+    public int matchCount(List<Integer> lotto, List<Integer> winning){ // 당첨 번호
+        int result = 0;
+        for(int i = 0; i < winning.size() - 1; i++){
+            int winningNumber = winning.get(i);
+            if(lotto.contains(winningNumber)){
+                result += 1;
+            }
+        }
+        return result;
+    }
+
+    public boolean matchBonus(List<Integer> lotto, List<Integer> winning){ // 보너스 번호
+        return lotto.contains(winning.get(6));
+    }
+}
