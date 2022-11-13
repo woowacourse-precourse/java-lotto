f031708866c978730ba23ea59a188354ccc22c5e
wjh2012 <wjh2012@naver.com>
2022-11-13 14:05:48 +0900

    feat(LottoRaffler): 6개의 숫자를 비교하는 기능

---
+++ src/main/java/lotto/LottoRaffler.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoRaffler {
+
+    private int first;
+    private int second;
+    private int third;
+    private int fourth;
+    private int fifth;
+
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public LottoRaffler(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private Long checkWinningNumbers(List<Integer> raffleNumbers) {
+        return raffleNumbers.stream().filter(winningNumbers::contains).count();
+    }
+}
