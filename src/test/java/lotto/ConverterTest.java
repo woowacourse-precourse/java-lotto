281fc1a1ca44ef4ebba8efb4a189f7d9aa8807ba
opuopuo <pureuml.dev@gmail.com>
2022-11-14 07:14:57 +0900

    test(converterTest): 보너스 번호 숫자인지 확인하는 코드 작성

--- src/test/java/lotto/ConverterTest.java
+++ src/test/java/lotto/ConverterTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import static lotto.domain.ErrorMessage.BONUS_NOT_NUMBER_ERROR;
 import static lotto.domain.ErrorMessage.INPUT_EMPTY_ERROR;
 import static lotto.domain.ErrorMessage.SEPARATOR_ERROR;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -13,27 +14,27 @@ import org.junit.jupiter.params.provider.ValueSource;
 class ConverterTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
-    @DisplayName("구입금액 입력이 없으면 예외가 발생한다")
+    @DisplayName("구입금액 입력이 없으면 예외가 발생한다.")
     @ParameterizedTest
     @EmptySource
-    void emptyToMoney(String input) {
+    void emptyMoney(String input) {
         assertThatThrownBy(() -> Converter.changeToMoney(input))
                 .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(
                         String.valueOf(INPUT_EMPTY_ERROR));
     }
 
-    @DisplayName("구입금액 예외 테스트")
+    @DisplayName("구입금액은 자연수가 아니고, 2,147,483,000가 넘어가면 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"test", "\b", "100000000000"})
-    void moneyConstructorTest(String input) {
+    @ValueSource(strings = {"test", "\b", "1000.10", "-1000", "100000000000"})
+    void moneyNumberTest(String input) {
         assertThatThrownBy(() -> Converter.changeToMoney(input))
                 .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(ERROR_MESSAGE);
     }
 
-    @DisplayName("당첨번호 입력이 없으면 예외가 발생한다")
+    @DisplayName("당첨번호 입력이 없으면 예외가 발생한다.")
     @ParameterizedTest
     @EmptySource
-    void emptyToWinningNumber(String input) {
+    void emptyWinningNumber(String input) {
         assertThatThrownBy(() -> Converter.changeToWinningNumber(input))
                 .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(
                         String.valueOf(INPUT_EMPTY_ERROR));
@@ -46,5 +47,23 @@ class ConverterTest {
         assertThatThrownBy(() -> Converter.changeToWinningNumber(input))
                 .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(
                         String.valueOf(SEPARATOR_ERROR));
+    }
+
+    @DisplayName("보너스 번호 입력이 없으면 예외가 발생한다.")
+    @ParameterizedTest
+    @EmptySource
+    void emptyBonusNumber(String input) {
+        assertThatThrownBy(() -> Converter.changeToWinningNumber(input))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(
+                        String.valueOf(INPUT_EMPTY_ERROR));
+    }
+
+    @DisplayName("보너스 번호는 양의 정수가 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"12.3", "4 ", "-10"})
+    void bonusNotNumber(String input) {
+        assertThatThrownBy(() -> Converter.changeToBonusNumber(input))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(
+                        String.valueOf(BONUS_NOT_NUMBER_ERROR));
     }
 }
