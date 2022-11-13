c2f9d5eb93c95c7f93f1d5568a380e8fa9fedad2
HiiWee <wpdlzhf159@gmail.com>
2022-11-14 04:13:36 +0900

    test: 구매 금액 입력시 발생하는 모든 예외 검증 테스트 추가

---
+++ src/test/java/lotto/validator/InputValidatorTest.java
@@ -0,0 +1,30 @@
+package lotto.validator;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import lotto.constants.message.ExceptionMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class InputValidatorTest {
+
+    private final InputValidator inputValidator = new InputValidator();
+    private final String ONLY_NUMBER = "-123," + ExceptionMessage.ERROR + ExceptionMessage.NON_NUMERIC_INPUT;
+    private final String START_WITH_ZERO = "012," + ExceptionMessage.ERROR + ExceptionMessage.START_WItH_ZERO;
+    private final String NO_ZERO = "0," + ExceptionMessage.ERROR + ExceptionMessage.ZERO_INPUT;
+
+
+    @DisplayName("구매 금액 입력시 발생하는 모든 예외 검증 테스트")
+    @ParameterizedTest
+    @CsvSource({ONLY_NUMBER, START_WITH_ZERO, NO_ZERO})
+    void 잘못된_구매금액_입력시_모든_예외_발생(String inputPrice, String exceptionMessage) {
+        // given
+        // when
+        // then
+        assertThatThrownBy(() -> inputValidator.validateInputPrice(inputPrice))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(exceptionMessage);
+    }
+}
