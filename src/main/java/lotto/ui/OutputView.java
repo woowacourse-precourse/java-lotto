065337916a777f29445b146a5758ac380345aabd
JungWoo <rlawjddn103@naver.com>
2022-11-14 00:10:16 +0900

    feat(OutputView): 공백 라인 출력 기능 추가

--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class OutputView {
     private static final String BUYING_N_AMOUNT = "개를 구매헸습니다.";
 
+    public static void outputBlankLine(){
+        System.out.println();
+    }
     public static void outputBuyingAmount(int buyingMoney) {
         System.out.println(buyingMoney/1000 + BUYING_N_AMOUNT);
     }
