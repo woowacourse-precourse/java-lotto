1c176645a5d17e139d6b6b606aa19db84b45a7d1
김현재 <41482946+galug@users.noreply.github.com>
2022-11-13 17:56:48 +0900

    refactor(purchase): money의 자료형을 변경
    모델은 철저히 비즈니스 로직에 집중

--- src/main/java/lotto/model/Purchase.java
+++ src/main/java/lotto/model/Purchase.java
@@ -4,16 +4,16 @@ public class Purchase {
     private static final int LOTTO_PRICE = 1000;
     private static final String INPUT_PRICE_ERROR = "[ERROR] 잘못된 가격을 입력하셨습니다.";
 
-    public int changeToLotto(String moneyInput) {
-        validate(moneyInput);
-
-        int money = Integer.parseInt(moneyInput);
+    public int changeToLotto(int money) {
+        validate(money);
         return money / LOTTO_PRICE;
     }
 
-    private void validate(String input) {
-        String regex = "^[0-9]{1,}000$";
-        if (!input.matches(regex)) {
+    private void validate(int money) {
+        if (money < LOTTO_PRICE) {
+            throw new IllegalArgumentException(INPUT_PRICE_ERROR);
+        }
+        if (money % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(INPUT_PRICE_ERROR);
         }
     }
