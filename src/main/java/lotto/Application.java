a8efd09a1e1e53530074a49cb41970a72569cc04
LEE-haeuk <rkwksmswhdk@naver.com>
2022-11-13 18:32:19 +0900

    feat: add getting the number of lottoes

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
-public class Application {
-    public static void main(String[] args) {
+import camp.nextstep.edu.missionutils.Console;
 
+public class Application {
+
+    public static void main(String[] args) {
+        Integer numberOfLottoes =
+                getTheNumberOfLottoesAsMuchThePurchaseAmount(
+                        Integer.parseInt(Console.readLine())
+                );
+    }
+
+    public static Integer getTheNumberOfLottoesAsMuchThePurchaseAmount(Integer purchaseAmount) {
+        validatePurchaseAmount(purchaseAmount);
+        return purchaseAmount / 1000;
+
+    }
+
+    public static void validatePurchaseAmount(Integer purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액이 1000의 배수가 아닙니다.");
+        }
     }
 }
