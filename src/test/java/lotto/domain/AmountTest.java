34e82c1ae6fa3c3a9325af8d10a9653b5c5179cc
saint6839 <saint6839@gmail.com>
2022-11-14 00:33:43 +0900

    test(domain) : 사용자가 입력한 구매 금액에 해당하는 만큼 로또 수량이 잘 반환되는지 테스트 작성

--- src/test/java/lotto/domain/AmountTest.java
+++ src/test/java/lotto/domain/AmountTest.java
@@ -2,17 +2,19 @@ package lotto.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AmountTest {
     @Test
     void validateCanDivideIntoOneThousand_구매_금액이_1000으로_나누어_떨어지지_않으면_예외를_던지는지_테스트() {
         //given
-        int amount = 1200;
+        int value = 1200;
+        int price = 1000;
         //when
         //then
         assertThatThrownBy(() -> {
-                      if (amount % 1000 == 0) {
+                      if (value % price == 0) {
                           return;
                       }
                       System.out.println("[ERROR] 금액은 1000원 단위로만 입력 가능합니다.");
@@ -20,5 +22,16 @@ class AmountTest {
                   })
                   .isInstanceOf(IllegalArgumentException.class)
                   .hasMessageContaining("[ERROR]");
+    }
+
+    @Test
+    void calculateLottoCount_구매_금액을_로또_금액으로_나누었을때_로또_수량이_잘_반환되는지_테스트() {
+        //given
+        int value = 2000;
+        int price = 1000;
+        //when
+        int lottoCount = value / price;
+        //then
+        assertThat(lottoCount).isEqualTo(2);
     }
 }
