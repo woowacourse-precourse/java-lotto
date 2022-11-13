a226faf3258ff013a8d149ead10130d77575843b
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-13 15:30:51 +0900

    Feat : "추가 티켓 수에 맞는 로또 숫자 발행"
    User_Lotttery_Number() 에서 로또 티켓에 맞는 로또를 발행하고 숫자를 Ticket_Print() 에서 오름차순으로 정렬하여 출력하였습니다.

--- src/main/java/lotto/I_O_System.java
+++ src/main/java/lotto/I_O_System.java
@@ -1,9 +1,12 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.TooManyListenersException;
 
 public class I_O_System {
     static final int lottey_price = 1000;
@@ -15,6 +18,7 @@ public class I_O_System {
 
     private String Enter_Number = "0";
     private int Money_Enter = 0;
+    private int Ticket =0;
 
     public boolean Enter_Price() {
         Enter_Number = Console.readLine();
@@ -25,8 +29,8 @@ public class I_O_System {
         if (!left_money()) {
             return false;
         }
-        Money_Enter /= lottey_price;
-        System.out.println(Money_Enter + Sell_Messge);
+        Ticket =Money_Enter / lottey_price;
+        User_Lottery_Number();
         return true;
     }
 
@@ -68,9 +72,26 @@ public class I_O_System {
         }
 
         Lotto lotto = new Lotto(Winning_Number);
+        System.out.println(Winning_Number);
 
+    }
+    public void User_Lottery_Number(){
+        System.out.println(Ticket + Sell_Messge);
+        for(int i=0;i<Ticket;i++)
+        {
+            List<Integer> number = Randoms.pickUniqueNumbersInRange(1,45,6);
+            Ticket_Print(number);
+            List<List<Integer>> mylist = new ArrayList<>();
+            mylist.add(number);
+        }
+    }
 
-
+    public void Ticket_Print(List<Integer> number)
+    {
+        List<Integer> Print_Number = new ArrayList<>();
+        Print_Number.addAll(number);
+        Collections.sort(Print_Number);
+        System.out.println(Print_Number);
     }
 
 
