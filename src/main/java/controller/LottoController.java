eff45c463b7652a388ac50931b1e6899db508cdb
chnlee <leechan6022@gmail.com>
2022-11-14 03:18:29 +0900

    feat(LottoController) : 로또 구입 금액, 개수, 번호 생성 기능에 대한 컨트롤러 구현

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,2 +1,32 @@
-package controller;public class LottoController {
+package controller;
+
+import lotto.LottoPurchase;
+import lotto.LottoTickets;
+import org.mockito.internal.matchers.Null;
+import view.Input;
+import view.Output;
+
+public class LottoController {
+    public void implement() {
+        LottoPurchase purchase = inputMoney();
+        LottoTickets lottoTickets = changeTickets(purchase);
+    }
+
+    private LottoPurchase inputMoney() {
+        try {
+            return new LottoPurchase(Input.inputMoney());
+        } catch (IllegalArgumentException exception) {
+            Output.outputException(exception);
+            return inputMoney();
+        }
+    }
+
+    private LottoTickets changeTickets(LottoPurchase money) {
+        int count = money.calculateTicketCount();
+        Output.outputTicketCount(count);
+
+        LottoTickets lottoTickets = new LottoTickets(count);
+        Output.outputTicket(lottoTickets);
+        return lottoTickets;
+    }
 }
