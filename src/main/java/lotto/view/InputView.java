c022392079cf8ca1bb27d68f5c77dd7e1316d7f3
Jeangyu <gjwlsrb1700@naver.com>
2022-11-14 00:23:03 +0900

    [FEAT: lotto] 당첨 숫자를 입력하는 view 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,14 +1,18 @@
 package lotto.view;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import static lotto.utils.Validator.validateInteger;
+import static lotto.utils.Validator.validateWinningNumbers;
 
 public class InputView {
 
     public static final Scanner SCANNER = new Scanner(System.in);
 
-    public int printWriteBudget(){
+    public static int writeBudget(){
         System.out.println("구입금액을 입력해 주세요.");
         String userInput = SCANNER.nextLine();
 
@@ -16,4 +20,26 @@ public class InputView {
 
         return Integer.valueOf(userInput);
     }
+
+    public static List<String> writeWinningNumbers(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String userInput = SCANNER.nextLine();
+
+        List<String> splitInput = splitWinningNumbers(userInput);
+
+        validateWinningNumbers(splitInput);
+
+        return splitInput;
+    }
+
+    private static List<String> splitWinningNumbers(String userInput){
+        List<String> result = new ArrayList<>();
+
+        for (String input : userInput.split(",")){
+            result.add(input);
+        }
+
+        return result;
+    }
+
 }
