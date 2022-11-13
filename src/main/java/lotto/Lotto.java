1d7a64f98c1bdbbca58d87baf81e80ea648fd7aa
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-13 12:30:55 +0900

    Feat($Lotto) 로또 번호 예외처리 메시지 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,13 +14,16 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         // 6자리가 아니면 예외처리
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리여야 합니다.");
         }
         // 중복된 값이 있으면 예외처리
         if (numbers.stream().distinct().collect(Collectors.toList()).size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 값이 존재합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }
