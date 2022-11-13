c7babfa6919c048bc3bcd114f426f1c3dfe47a0f
chanminmaxscaler <jcm04@naver.com>
2022-11-13 19:58:49 +0900

    feat: Lotto 클래스에 로또 번호의 중복을 확인하는 기능, 당첨번호와 보너스번호를 맞춰 같은 숫자를 세는 기능을 구현한다
    로또 번호의 중복을 확인하는 기능을 validate 메소드로 구현한다.
    validate 메소드는 hasDuplication 메소드로 입력된 리스트에 중복되는 숫자가 있는지 확인한다.
    중복되는 숫자가 있는 경우 IllegalArgumentException을 발생시킨다.
    
    당첨번호와 보너스 번호와 같은 숫자를 세는 기능은 countNumbersConsistentWith 메소드로 구현한다.
    countNumbersConsistentWith 메소드는 당첨번호와 로또 번호를 맞춰보고 같은 숫자의 개수를 구한다.
    같은 숫자의 개수가 5일 경우, 보너스 번호를 맞춰본다.
    같은 숫자의 개수를 반환한다.

--- src/main/java/lotto/domain/lottomachine/lottoticket/Lotto.java
+++ src/main/java/lotto/domain/lottomachine/lottoticket/Lotto.java
@@ -1,5 +1,8 @@
 package lotto.domain.lottomachine.lottoticket;
 
+import lotto.domain.lottomachine.winningnumber.WinningNumber;
+import lotto.domain.lottomachine.winningnumber.WinningNumbers;
+
 import java.util.List;
 
 public class Lotto {
@@ -12,9 +15,34 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        if (hasDuplication(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있을 수 없습니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean hasDuplication(List<Integer> numbers) {
+        return numbers.size() != numbers.stream()
+                .distinct()
+                .count();
+    }
+
+    public String countNumbersConsistentWith(WinningNumbers winningNumbers, WinningNumber bonusNumber) {
+        String count = winningNumbers.countSameValue(numbers);
+        String bonusCount = bonusNumber.checkSameValue(numbers);
+        if (isFive(count)) {
+            return count + bonusCount;
+        }
+        return count;
+    }
+
+    private boolean isFive(String number) {
+        return number.equals("5");
+    }
+
+    @Override
+    public String toString() {
+        return "" + numbers;
+    }
 }
