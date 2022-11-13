5fc83c2a3cc2a22bd0519de4f2edf956a448a64a
김동규 <2016025196@hanyang.ac.kr>
2022-11-13 23:21:30 +0900

    로또 가격 enum 추가

--- src/main/java/constant/LottoNumber.java
+++ src/main/java/constant/LottoNumber.java
@@ -4,7 +4,8 @@ public enum LottoNumber {
     MIN_LOTTO_NUMBER(1),
     MAX_LOTTO_NUMBER(45),
     REQUIRE_LOTTO_NUMBER_COUNT(6),
-    AVAIL_BONUS_NUMBER_COUNT(1);
+    AVAIL_BONUS_NUMBER_COUNT(1),
+    LOTTO_PRICE(1000);
 
     private final int value;
     LottoNumber(int value) {
