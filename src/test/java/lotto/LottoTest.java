9b062b321260009c0e76f62c2c9c4e88115e4b59
Kayoung <rkdkdudjd@gmail.com>
2022-11-14 01:05:19 +0900

    test(Lotto): 예외 상황 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -8,20 +8,35 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    @DisplayName("로또 번호의 개수가 부족하면 예외가 발생한다.")
+    @Test
+    void createLottoByLessSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 번호의 개수가 올바르지 않습니다.");
+    }
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 번호의 개수가 올바르지 않습니다.");
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 번호에 중복된 숫자가 있습니다.");
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 범위를 벗어나는 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByNumbersOutOfRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 4, 19, 46, 44)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("범위를 벗어나는 로또 번호가 있습니다.");
+    }
 }
