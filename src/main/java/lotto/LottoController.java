3918b1e55f4866fce358553378b236d3f87b6fe8
Haechan Lee <dlgockssla@gmail.com>
2022-11-14 03:54:44 +0900

    feat: 당첨 번호와 보너스 번호 입력

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -2,6 +2,7 @@ package lotto;
 
 import java.util.List;
 
+import lotto.util.LottoParser;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -25,7 +26,7 @@ public class LottoController {
         try {
             money = Integer.parseUnsignedInt(inputMoney);
         } catch (NumberFormatException nfe) {
-            throw new IllegalArgumentException("[ERROR] 음수를 제외한 숫자만 입력해주세요.");
+            throw new IllegalArgumentException("[ERROR] 음수를 제외한 정수만 입력해주세요.");
         }
 
         List<Lotto> lottos = lottoService.buyLottos(money);
@@ -34,6 +35,11 @@ public class LottoController {
 
     private void setWinningNumbers() {
 
+        String inputWinningNumbers = InputView.inputWinningNumbers();
+        List<Integer> winningNumbers = LottoParser.parseToIntegers(inputWinningNumbers);
+
+        String inputBonusNumber = InputView.inputBonusNumber();
+        int bonusNumber = LottoParser.parseToBonumNumber(inputBonusNumber);
     }
 
     private void result() {
