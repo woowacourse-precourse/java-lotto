bd7750d148283ba0cebc2f8103025203c9ce416b
jeongwle <dldl4568@naver.com>
2022-11-14 07:56:28 +0900

    refactor: 테스트 방식 수정
    에러를 던지는 것을 확인하는 방향으로 코드를 작성했었으나
    과제가 의도한 방향과 맞지 않아 에러메세지를 확인하는 코드로 변경

--- src/test/java/lottoMachine/LottoMachineTest.java
+++ src/test/java/lottoMachine/LottoMachineTest.java
@@ -2,7 +2,6 @@ package lottoMachine;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import lotto.Application;
@@ -20,44 +19,45 @@ public class LottoMachineTest extends NsTest {
     }
 
     @Test
-    void 당첨번호의_형식이_지켜지지_않을_경우_예외가_발생합니다() {
+    void 당첨번호의_형식이_지켜지지_않을_경우_에러메세지_출력() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> run("5000", "1,2,3,4,5", "7"))
-                    .isInstanceOf(IllegalArgumentException.class);
+            run("5000", "1,2,3,4,5", "7");
+            assertThat(output()).contains("[ERROR]");
         });
     }
 
     @Test
-    void 당첨번호가_중복될_경우_예외가_발생합니다() {
+    void 당첨번호가_중복될_경우_에러메세지_출력() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> run("5000", "1,2,3,4,5,5", "7"))
-                    .isInstanceOf(IllegalArgumentException.class);
+            run("5000", "1,2,3,4,5,5", "7");
+            assertThat(output()).contains("[ERROR]");
         });
     }
 
     @Test
-    void 당첨번호의_범위가_1에서_45사이가_아닐_경우_예외가_발생합니다() {
+    void 당첨번호의_범위가_1에서_45사이가_아닐_경우_에러메세지_출력() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> run("5000", "1,2,3,4,45,46", "7"))
-                    .isInstanceOf(IllegalArgumentException.class);
+            run("5000", "1,2,3,4,45,46", "7");
+            assertThat(output()).contains("[ERROR]");
         });
     }
 
     @Test
-    void 보너스번호가_하나가_아니면_예외가_발생합니다() {
+    void 보너스번호가_하나가_아니면_에러메세지_출력() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> run("5000", "1,2,3,4,5,6", "7,8"))
-                    .isInstanceOf(IllegalArgumentException.class);
+            run("5000", "1,2,3,4,5,6", "7,8");
+            assertThat(output()).contains("[ERROR]");
         });
     }
 
     @Test
-    void 보너스번호가_당첨번호와_중복되면_에외가_발생합니다() {
+    void 보너스번호가_당첨번호와_중복되면_에러메세지_출력() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> run("5000", "1,2,3,4,5,6", "6"))
-                    .isInstanceOf(IllegalArgumentException.class);
+            run("5000", "1,2,3,4,5,6", "6");
+            assertThat(output()).contains("[ERROR]");
         });
     }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
