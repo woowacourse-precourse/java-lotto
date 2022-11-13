351b670743db8330b9c733e3503da1f2de326eb6
1eejisoo <jisu991012@gmail.com>
2022-11-14 03:20:56 +0900

    feat: 당첨 번호 유효성 검증 - 1~45 범위의 숫자인가

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -7,12 +7,21 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("당첨 번호는 6개의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
         }
     }
 
