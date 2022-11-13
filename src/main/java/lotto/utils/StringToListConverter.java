a79ddbbf7724474e673c9a6d5bd44f648e000cd6
JHS <wnghdtjr129@naver.com>
2022-11-13 20:04:12 +0900

    feat(StringToConverter): 변환 로직 기능 분리구현

---
+++ src/main/java/lotto/utils/StringToListConverter.java
@@ -0,0 +1,23 @@
+package lotto.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class StringToListConverter {
+    public static List<Integer> convert(String winningNumber) {
+        String[] winningNumbers = validateSeparator(winningNumber);
+        return Arrays.stream(winningNumbers)
+                .map(Integer::parseInt)
+                .collect(toList());
+    }
+
+    public static String[] validateSeparator(String winningNumber) {
+        String[] winningNumbers = winningNumber.split(",");
+        if (winningNumbers.length != 6) {
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 쉼표(,)를 이용하여 구분하여 입력해 주시기 바랍니다");
+        }
+        return winningNumbers;
+    }
+}
