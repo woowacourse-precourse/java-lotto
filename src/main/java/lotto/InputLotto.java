f6c0468885b7f32d7c24ed4e95433efda65897c0
abbd2 <ajkety00@gmail.com>
2022-11-14 00:01:44 +0900

    Refactor(InputLotto): toIntList 메서드 내의 outRangeLottoNumber 제거

--- src/main/java/lotto/InputLotto.java
+++ src/main/java/lotto/InputLotto.java
@@ -39,9 +39,7 @@ public class InputLotto {
 
     public List<Integer> toIntList(List<String> stringsNumberList){
         List<Integer> numberList = new ArrayList<>();
-        ExceptionHandling exceptionHandling = new ExceptionHandling();
         for(String stringsNumber : stringsNumberList){
-            exceptionHandling.outRangeLottoNumber(stringsNumber);
             numberList.add(toInts(stringsNumber));
         }
         return numberList;
@@ -49,7 +47,7 @@ public class InputLotto {
 
     public int toInts(String value){
         ExceptionHandling exceptionHandling = new ExceptionHandling();
-        exceptionHandling.isNumeric(value); // 숫자인지 검사
+        exceptionHandling.outRangeLottoNumber(value); // 숫자인지 검사
         return Integer.parseInt(value);
     }
 
