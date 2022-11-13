d68da1e6c470e3e6b83bf74bfc2b1a3633a1ff57
yoon <beatus96@naver.com>
2022-11-14 06:12:30 +0900

    refactor(Rank) : enum 상수 순서 변경
    이유
    : 출력시, 작은 수 순대로 출력하기 위해서

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,11 +1,11 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST(6,false,2_000_000_000, "6개 일치 (2,000,000,000원) - ") ,
-    SECOND(5,true,30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    THIRD(5,false,1_500_000, "5개 일치 (1,500,000원) - "),
-    FOURTH(4,false,50_000, "4개 일치 (50,000원) - "),
-    FIFTH(3,false,5_000, "3개 일치 (5,000원) - ");
+    FIFTH(3,false,5_000, "3개 일치 (5,000원) - %d개"),
+    FOURTH(4,false,50_000, "4개 일치 (50,000원) - %d개"),
+    THIRD(5,false,1_500_000, "5개 일치 (1,500,000원) - %d개"),
+    SECOND(5,true,30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    FIRST(6,false,2_000_000_000, "6개 일치 (2,000,000,000원) - %d개");
 
     private final int matchCount;
     private final boolean hasBonusNumber;
@@ -36,4 +36,5 @@ public enum Rank {
     public String toString() {
         return this.sentence;
     }
+
 }
