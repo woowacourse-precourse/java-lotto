c3bd9703ac4fddaa8e99c618b48654c159ab60fa
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-13 11:42:49 +0900

    feat(WinningLotto): 보너스 번호 리스트 검증 기능 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import static lotto.message.ErrorMessageConstants.*;
+import static lotto.restrict.RestrictConstants.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class WinningLotto extends Lotto {
@@ -8,10 +12,44 @@ public class WinningLotto extends Lotto {
 
 	public WinningLotto(Lotto lotto, List<Integer> bonusNumbers) {
 		super(lotto.getNumbers());
+		validate(bonusNumbers);
 		this.bonusNumbers = bonusNumbers;
 	}
 
 	public List<Integer> getBonusNumbers() {
 		return bonusNumbers;
+	}
+
+	private void validate(List<Integer> bonusNumbers) {
+		if (bonusNumbers.size() != BONUS_QUANTITY) {
+			throw new IllegalArgumentException(String.format(SIZE_ERROR_MESSAGE_FORMAT, BONUS_QUANTITY));
+		}
+		if (checkRangeBonusNumbers(bonusNumbers)) {
+			throw new IllegalArgumentException(String.format(RANGE_ERROR_MESSAGE_FORMAT, MIN_NUMBER, MAX_NUMBER));
+		}
+		if (checkDuplicateBonusNumbers(bonusNumbers)) {
+			throw new IllegalArgumentException(String.format(DUPLICATION_ERROR_MESSAGE));
+		}
+	}
+
+	private boolean checkRangeBonusNumbers(List<Integer> bonusNumbers) {
+		for (int bonusNumber : bonusNumbers) {
+			if (bonusNumber < MIN_NUMBER || MAX_NUMBER < bonusNumber) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean checkDuplicateBonusNumbers(List<Integer> bonusNumbers) {
+		List<Integer> duplicateBonusNumbers = new ArrayList<>(super.getNumbers());
+
+		for (int bonusNumber : bonusNumbers) {
+			if (duplicateBonusNumbers.contains(bonusNumber)) {
+				return true;
+			}
+			duplicateBonusNumbers.add(bonusNumber);
+		}
+		return false;
 	}
 }
