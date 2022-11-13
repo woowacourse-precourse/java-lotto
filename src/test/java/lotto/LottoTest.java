a915be1c9ef55fb22cb99ffbae97216a7332f409
alivejuicy <kkddhh1998@gmail.com>
2022-11-13 13:09:46 +0900

    Test : 로또 랜덤 숫자 6개가 맞는지 테스트 후 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,14 @@
 package lotto;
 
+import lotto.reward.Lotto;
+import lotto.reward.RandomNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.Console;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +27,18 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호 입력 성공한 경우")
+    @Test
+    void createLottoSucceedInput() {
+        Console console = System.console();
+
+    }
+
+    //로또 랜덤 숫자 6개를 확인한다.
+    @DisplayName("로또 자동 출력 번호 테스트하기")
+    @Test
+    void checkLottoSize(){
+        assertThat(new Lotto(new RandomNumbers()).getLottoSixNumbers()).hasSize(6);
+    }
     // 아래에 추가 테스트 작성 가능
 }
