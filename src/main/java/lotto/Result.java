8eaec293a82cdd8978f5e7081c2b009a55b7806a
ghltjd369 <ghltjd369@gmail.com>
2022-11-13 14:35:34 +0900

    feat : 당첨 내역 출력하는 기능 구현

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -2,12 +2,31 @@ package lotto;
 
 import java.util.List;
 
+enum Winning {
+    FIRST(2_000_000_000, 0),
+    SECOND(30_000_000, 0),
+    THIRD(1_500_000, 0),
+    FOURTH(50_000, 0),
+    FIFTH(5_000, 0);
+
+    public int prize;
+    public int cnt;
+
+    Winning(int prize, int cnt) {
+        this.prize = prize;
+        this.cnt = cnt;
+    }
+}
+
 public class Result {
     public static void compareLottoNumber(List<Lotto> myLotto, List<Integer> winningNumber, int bonusNumber) {
         for (Lotto lotto : myLotto) {
             int winning = countWinningNumber(lotto, winningNumber);
             boolean bonus = containBonusNumber(lotto, bonusNumber);
+            checkTheResult(winning, bonus);
         }
+
+        printTheResult();
     }
 
     public static int countWinningNumber(Lotto lotto, List<Integer> winningNumber) {
@@ -19,6 +38,10 @@ public class Result {
         return cnt;
     }
 
+    public static boolean containBonusNumber(Lotto lotto, int bonusNumber) {
+        return lotto.containNumber(bonusNumber);
+    }
+
     public static int containWinningNumber(Lotto lotto, int winningNumber) {
         if (lotto.containNumber(winningNumber)) {
             return 1;
@@ -26,8 +49,31 @@ public class Result {
         return 0;
     }
 
-    public static boolean containBonusNumber(Lotto lotto, int bonusNumber) {
-        return lotto.containNumber(bonusNumber);
+    public static void checkTheResult(int winning, boolean bonus) {
+        if (winning == 6) {
+            Winning.FIRST.cnt++;
+        }
+        if (winning == 5 && bonus == true) {
+            Winning.SECOND.cnt++;
+        }
+        if (winning == 5 && bonus == false) {
+            Winning.THIRD.cnt++;
+        }
+        if (winning == 4) {
+            Winning.FOURTH.cnt++;
+        }
+        if (winning == 3) {
+            Winning.FIFTH.cnt++;
+        }
     }
+
+    public static void printTheResult() {
+        System.out.println("3개 일치 (5,000원) -  " + Winning.FIFTH.cnt + "개");
+        System.out.println("4개 일치 (50,000원) -  " + Winning.FOURTH.cnt + "개");
+        System.out.println("5개 일치 (1,500,000원) -  " + Winning.THIRD.cnt + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) -  " + Winning.SECOND.cnt + "개");
+        System.out.println("6개 일치 (2,000,000,000원) -  " + Winning.FIRST.cnt + "개");
+    }
+
 }
 
