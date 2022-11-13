90e81c71e94e67da721b605c55b4f561f2c6c5f0
맹순영 <symaeng98@naver.com>
2022-11-13 16:52:58 +0900

    test: 테스트 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,4 +27,59 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("맞은 개수와 보너스 정답 유무를 확인한다.")
+    @Test
+    void checkHitCountAndBonusHitCount1() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumbers = List.of(1,2,5,7,9,10);
+        int bonus = 11;
+
+        List<Integer> hitCountAndBonusHitCount = Application.checkLottery(lotto, winningNumbers, bonus);
+        int hitCount = hitCountAndBonusHitCount.get(0);
+        int bonusHitCount = hitCountAndBonusHitCount.get(1);
+
+        assertThat(hitCount).isEqualTo(3);
+        assertThat(bonusHitCount).isEqualTo(0);
+    }
+
+    @DisplayName("맞은 개수와 보너스 정답 유무를 확인한다.")
+    @Test
+    void checkHitCountAndBonusHitCount2() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumbers = List.of(1,2,5,7,9,10);
+        int bonus = 6;
+
+        List<Integer> hitCountAndBonusHitCount = Application.checkLottery(lotto, winningNumbers, bonus);
+        int hitCount = hitCountAndBonusHitCount.get(0);
+        int bonusHitCount = hitCountAndBonusHitCount.get(1);
+
+        assertThat(hitCount).isEqualTo(3);
+        assertThat(bonusHitCount).isEqualTo(1);
+    }
+
+    @DisplayName("등수 별 개수를 확인한다.")
+    @Test
+    void checkRankCount1() {
+        List<Lotto> lotteries = new ArrayList<>();
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        lotteries.add(lotto);
+        List<Integer> winningNumbers = List.of(1,2,5,7,9,10);
+        int bonus = 6;
+
+        List<Integer> rankCount = Application.getRankCount(lotteries, winningNumbers, bonus);
+        assertThat(rankCount.toString()).isEqualTo("[0, 0, 0, 0, 0, 1]");
+    }
+
+    @DisplayName("등수 별 개수를 확인한다.")
+    @Test
+    void checkRankCount2() {
+        List<Lotto> lotteries = new ArrayList<>();
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        lotteries.add(lotto);
+        List<Integer> winningNumbers = List.of(1,2,5,6,9,10);
+        int bonus = 11;
+
+        List<Integer> rankCount = Application.getRankCount(lotteries, winningNumbers, bonus);
+        assertThat(rankCount.toString()).isEqualTo("[0, 0, 0, 0, 1, 0 ]");
+    }
 }
