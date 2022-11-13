b18a34b632a4f8f431d6b20ea8bf11ac1aa8de32
ESeokhwan <huan99@hanmail.net>
2022-11-14 02:04:49 +0900

    test: LottoTest 추가
    - autoLottoGenerateTest() : 자동 로또 생성 테스트
    - checkSecondLottoTest() : 2등 로또 확인 테스트
    - checkThirdLottoTest() : 3등 로또 확인 테스트
    - checkFourthLottoTest() : 4등 로또 확인 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,12 +1,17 @@
 package lotto;
 
 import lotto.model.lotto.Lotto;
+import lotto.model.lotto.LottoGenerator;
+import lotto.model.statistics.LottoResult;
+import lotto.model.winningnumbers.WinningNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,5 +29,55 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("자동 로또 생성 테스트")
+    @Test
+    void autoLottoGenerateTest() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    Lotto lotto = LottoGenerator.autoGenerate();
+                    assertThat(lotto.getNumbers()).isEqualTo(List.of(13, 14, 16, 38, 42, 45));
+                },
+                List.of(38, 16, 42, 13, 14, 45)
+        );
+    }
+
+    @DisplayName("2등 로또 확인 테스트")
+    @Test
+    void checkSecondLottoTest() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    Lotto lotto = LottoGenerator.autoGenerate();
+                    WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 13, 14, 16, 38, 42), 45);
+                    assertThat(lotto.check(winningNumbers)).isEqualTo(LottoResult.SECOND);
+                },
+                List.of(38, 16, 42, 13, 14, 45)
+        );
+    }
+
+    @DisplayName("3등 로또 확인 테스트")
+    @Test
+    void checkThirdLottoTest() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    Lotto lotto = LottoGenerator.autoGenerate();
+                    WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 13, 14, 16, 38, 42), 3);
+                    assertThat(lotto.check(winningNumbers)).isEqualTo(LottoResult.THIRD);
+                },
+                List.of(38, 16, 42, 13, 14, 45)
+        );
+    }
+
+    @DisplayName("4등 로또 확인 테스트")
+    @Test
+    void checkFourthLottoTest() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    Lotto lotto = LottoGenerator.autoGenerate();
+                    WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 3, 13, 16, 42, 43), 45);
+
+                    assertThat(lotto.check(winningNumbers)).isEqualTo(LottoResult.FOURTH);
+                },
+                List.of(38, 16, 42, 13, 14, 45)
+        );
+    }
 }
