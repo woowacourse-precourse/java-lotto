024af175c7f5eb3a3c9eb5bfe17075d99f17acd7
splitube <voicesplit@daum.net>
2022-11-13 18:29:12 +0900

    feat(ErrorMessage): 에러 메시지 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -1,9 +1,11 @@
 package lotto;
 
 public enum ErrorMessage {
-    NOT_A_NUMBER("[ERROR] 구입금액은 숫자여야 합니다."),
+    MONEY_NOT_A_NUMBER("[ERROR] 구입금액은 숫자여야 합니다."),
     DIVIDE_DISABLE("[ERROR] 천원 단위가 아닙니다."),
-    NOT_ENOUGH_NUM("[ERROR] 6개의 번호가 필요합니다.");
+    NOT_ENOUGH_NUM("[ERROR] 6개의 번호가 필요합니다."),
+    WINNING_NUM_NOT_A_NUMBER("[ERROR] 당첨 번호는 숫자여야 합니다."),
+    NUMBER_OUT_OF_RANGE("[ERROR] 번호는 1부터 45사이의 숫자여야 합니다.");
 
     final String message;
 
