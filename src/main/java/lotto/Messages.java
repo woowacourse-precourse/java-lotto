94013cddf6aada4873958de2b7f09c8cf438d259
splitube <voicesplit@daum.net>
2022-11-13 23:19:54 +0900

    refactor(Message): 줄바꿈 추가

--- src/main/java/lotto/Messages.java
+++ src/main/java/lotto/Messages.java
@@ -3,8 +3,9 @@ package lotto;
 public enum Messages {
     REQUEST_MONEY("구입금액을 입력해 주세요."),
     HOW_MANY_SOLD("개를 구매했습니다."),
-    ENTER_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
-    ENTER_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+    ENTER_WINNING_NUMBER("\n당첨 번호를 입력해 주세요."),
+    ENTER_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
+    PRIZE_RESULT("\n당첨 통계\n---");
 
     final String message;
 
