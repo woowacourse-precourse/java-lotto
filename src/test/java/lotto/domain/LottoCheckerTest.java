d5bd84ccd94d951d811696250c5b51072389cfa6
KimSangHyeok <tkdgur0906@naver.com>
2022-11-13 18:19:51 +0900

    Test(lotto): 당첨번호 개수 비교 테스트

--- src/test/java/lotto/domain/LottoCheckerTest.java
+++ src/test/java/lotto/domain/LottoCheckerTest.java
@@ -1,16 +1,33 @@
 package lotto.domain;
 
-import org.assertj.core.api.Assertions;
+import lotto.Model.Lotto;
+import lotto.Model.WinningLotto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class LottoCheckerTest {
+    LottoChecker lottoChecker;
+
+    @BeforeEach
+    void setUp() {
+        lottoChecker = new LottoChecker();
+    }
 
     @Test
-    void 수익률_테스트(){
-        LottoChecker lottoChecker = new LottoChecker();
-        assertThat(lottoChecker.profitRate(8000,5000)).isEqualTo(62.5);
+    void 수익률_테스트() {
+        assertThat(lottoChecker.profitRate(8000, 5000)).isEqualTo(62.5);
     }
+
+    @Test
+    void 당첨_개수_비교() {
+        Lotto userLotto = new Lotto(new ArrayList<>(List.of(1,2,3,4,5,6)));
+        WinningLotto winningLotto = new WinningLotto(new Lotto(new ArrayList<>(List.of(3,4,5,6,7,8))), 10);
+        assertThat(lottoChecker.lottoCompare(userLotto,winningLotto)).isEqualTo(4);
+    }
+
 }
