e7b6c0301c1689b0f47e84f0018eefe4e056d7ac
greeng00se <thegreengoose7@gmail.com>
2022-11-13 15:32:15 +0900

    feat(LottoResult): LottoPrize에 대한 값을 반환하는 getCount 메서드 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,12 +1,18 @@
 package lotto.domain;
 
 import java.util.EnumMap;
+import java.util.Map;
 
 public class LottoResult {
+    private static final Long ZERO = 0L;
 
-    private final EnumMap<LottoPrize, Integer> result;
+    private EnumMap<LottoPrize, Long> result = new EnumMap<>(LottoPrize.class);
 
-    public LottoResult(EnumMap<LottoPrize, Integer> result) {
-        this.result = result;
+    public LottoResult(Map<LottoPrize, Long> result) {
+        this.result.putAll(result);
+    }
+
+    public Integer getCount(LottoPrize lottoPrize) {
+        return result.getOrDefault(lottoPrize, ZERO).intValue();
     }
 }
--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -1,10 +1,32 @@
 package lotto.domain;
 
+import static lotto.domain.LottoPrize._1ST_PRIZE;
+import static lotto.domain.LottoPrize._2ND_PRIZE;
+
+import java.util.Map;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("LottoResult 클래스")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class LottoResultTest {
+
+    @Test
+    void getCount_메서드는_LottoPrize에_대한_값이_있다면_해당_값을_반환한다() {
+        Map<LottoPrize, Long> lottoPrizeToInteger = Map.of(_1ST_PRIZE, 3L);
+        LottoResult lottoResult = new LottoResult(lottoPrizeToInteger);
+
+        Assertions.assertThat(lottoResult.getCount(_1ST_PRIZE)).isEqualTo(3);
+    }
+
+    @Test
+    void getCount_메서드는_LottoPrize에_대한_값이_없다면_0을_반환한다() {
+        Map<LottoPrize, Long> lottoPrizeToInteger = Map.of(_1ST_PRIZE, 1L);
+        LottoResult lottoResult = new LottoResult(lottoPrizeToInteger);
+
+        Assertions.assertThat(lottoResult.getCount(_2ND_PRIZE)).isEqualTo(0);
+    }
 }
