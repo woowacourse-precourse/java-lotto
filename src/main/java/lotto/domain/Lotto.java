2732a82933bbf2743ef40d0842ccfc9e8fccccbd
hossang <zkzk104@naver.com>
2022-11-13 18:38:52 +0900

    refactor: Lotto 클래스 인스턴스 변수 타입 변경하기

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,22 +1,27 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final Set<Integer> numbers;
 
-    public List<Integer> getNumbers() {
+    public Set<Integer> getNumbers() {
         return numbers;
     }
 
-    public Lotto(List<Integer> numbers) {
-        isSixNumbers(numbers);
-        this.numbers = numbers;
-        Collections.sort(numbers);
+    public Lotto(List<Integer> unValidatedNumbers) {
+        Collections.sort(unValidatedNumbers);
+        this.numbers = new LinkedHashSet<>(unValidatedNumbers);
+        validateSixNumber(numbers);
     }
 
-    private void isSixNumbers(List<Integer> numbers) {
+    private void validateSixNumber(Set<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(Error.IS_NOT_SIX.getMessage());
         }
