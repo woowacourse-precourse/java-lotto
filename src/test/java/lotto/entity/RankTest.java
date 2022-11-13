bcedab14e849a24bd6aa226554cc3619c66c66cd
woo-chang <clllickme@naver.com>
2022-11-13 18:43:54 +0900

    test(Rank): 개수, 보너스에 따라 알맞은 결과와 출력을 반환하는지 테스트

---
+++ src/test/java/lotto/entity/RankTest.java
@@ -0,0 +1,207 @@
+package lotto.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RankTest {
+
+    @DisplayName("맞은 개수가 6개이고 보너스 번호가 틀린 경우 1등이다.")
+    @Test
+    void first() {
+        //given
+        Rank rank = Rank.FIRST;
+
+        //when
+        boolean result = rank.win(6, false);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("맞은 개수가 5개이고 보너스 번호가 맞은 경우 2등이다.")
+    @Test
+    void second() {
+        //given
+        Rank rank = Rank.SECOND;
+
+        //when
+        boolean result = rank.win(5, true);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("맞은 개수가 5개이고 보너스 번호가 틀린 경우 3등이다.")
+    @Test
+    void third() {
+        //given
+        Rank rank = Rank.THIRD;
+
+        //when
+        boolean result = rank.win(5, false);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("맞은 개수가 4개이고 보너스 번호가 맞은 경우 4등이다.")
+    @Test
+    void fourthCase1() {
+        //given
+        Rank rank = Rank.FOURTH;
+
+        //when
+        boolean result = rank.win(4, true);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("맞은 개수가 4개이고 보너스 번호가 틀린 경우 4등이다.")
+    @Test
+    void fourthCase2() {
+        //given
+        Rank rank = Rank.FOURTH;
+
+        //when
+        boolean result = rank.win(4, false);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("맞은 개수가 3개이고 보너스 번호가 맞은 경우 5등이다.")
+    @Test
+    void fifthCase1() {
+        //given
+        Rank rank = Rank.FIFTH;
+
+        //when
+        boolean result = rank.win(3, true);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("맞은 개수가 3개이고 보너스 번호가 틀린 경우 5등이다.")
+    @Test
+    void fifthCase2() {
+        //given
+        Rank rank = Rank.FIFTH;
+
+        //when
+        boolean result = rank.win(3, false);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("맞은 개수가 2개이고 보너스 번호가 맞은 경우 등수가 없다.")
+    @Test
+    void noneCase1() {
+        //given
+        Rank rank = Rank.NONE;
+
+        //when
+        boolean result = rank.win(2, true);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("맞은 개수가 2개이고 보너스 번호가 틀린 경우 등수가 없다.")
+    @Test
+    void noneCase2() {
+        //given
+        Rank rank = Rank.NONE;
+
+        //when
+        boolean result = rank.win(2, false);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("맞은 개수가 1개이고 보너스 번호가 맞은 경우 등수가 없다.")
+    @Test
+    void noneCase3() {
+        //given
+        Rank rank = Rank.NONE;
+
+        //when
+        boolean result = rank.win(1, true);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("맞은 개수가 1개이고 보너스 번호가 틀린 경우 등수가 없다.")
+    @Test
+    void noneCase4() {
+        //given
+        Rank rank = Rank.NONE;
+
+        //when
+        boolean result = rank.win(1, false);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("맞은 개수가 0개이고 보너스 번호가 맞은 경우 등수가 없다.")
+    @Test
+    void noneCase5() {
+        //given
+        Rank rank = Rank.NONE;
+
+        //when
+        boolean result = rank.win(0, true);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("맞은 개수가 0개이고 보너스 번호가 틀린 경우 등수가 없다.")
+    @Test
+    void noneCase6() {
+        //given
+        Rank rank = Rank.NONE;
+
+        //when
+        boolean result = rank.win(0, false);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("보너스 볼과 관계 없는 등수인 경우 보너스 볼 정보 없이 출력한다.")
+    @Test
+    void printRank() {
+        //given
+        Rank rank = Rank.FIRST;
+        String output = String.format("%d개 일치 (%,d원)", rank.getCount(), rank.getPrize());
+
+        //when
+        String result = rank.toString();
+
+        //then
+        assertThat(result).isEqualTo(output);
+    }
+
+    @DisplayName("보너스 볼과 관계 있는 등수인 경우 보너스 볼 정보 포함해서 출력한다.")
+    @Test
+    void printRankWithBonus() {
+        //given
+        Rank rank = Rank.SECOND;
+        String output = String.format("%d개 일치, 보너스 볼 일치 (%,d원)", rank.getCount(), rank.getPrize());
+
+        //when
+        String result = rank.toString();
+
+        //then
+        assertThat(result).isEqualTo(output);
+    }
+
+}
