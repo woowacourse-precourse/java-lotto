2e87b6eb8c7b21d523020d5a40b0d9ac77304df1
fakerdeft <whakswp123@naver.com>
2022-11-14 03:16:30 +0900

    feat(Lotto): add Lotto 생성자 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,9 +14,17 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또는 6자리 입니다.");
         }
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 중복되는 숫자가 입력되었습니다.");
+        }
+        for (int i = 0; i < 6; i++) {
+            if (numbers.get(i) < 1 || numbers.get(i) > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.");
+            }
+        }
     }
 
-    public List<Integer> getNumbers(){
+    public List<Integer> getNumbers() {
         return numbers;
     }
 
