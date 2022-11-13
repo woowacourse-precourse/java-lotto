4991c335c0db04633763d0cd6e73f6affb07c6a6
이정우 <acg6138@naver.com>
2022-11-13 15:06:03 +0900

    refactor(Money): Money 로직을 Utils로 이동

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -7,16 +7,8 @@ public class Money {
         this.money = validate(input);
     }
 
-    public int processInput(String input) {
-        try{
-            return Integer.parseInt(input);
-        }catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액에는 숫자만 입력해야 합니다.");
-        }
-    }
-
     public int validate(String input) {
-        int money = processInput(input);
+        int money = Utils.textToNumber(input);
         if (money < MONEY_DIVIDE_UNIT) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원보다 커야합니다.");
         }
