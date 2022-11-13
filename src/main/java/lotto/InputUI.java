9c50b0e54deee5f2c1698a101308bf0ff0116d36
jamangstangs <jamangsong@gmail.com>
2022-11-13 16:55:13 +0900

    feat(InputUI): getMoney() 메서드 구현

--- src/main/java/lotto/InputUI.java
+++ src/main/java/lotto/InputUI.java
@@ -1,15 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputUI {
 
+    private static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해주세요.";
+    private static final String LOTTO_BUY_RESULT_MESSAGE = "개를 구매했습니다.";
+    private static final String NORMAL_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+
     public InputUI() {
-    }
-
-    public void getMoney() {
 
     }
 
-    public void validateMoney() {
+    public int getMoney() {
+        System.out.println(MONEY_INPUT_MESSAGE);
+        String money = Console.readLine();
+        isValidatedMoney(money);
+        return Integer.parseInt(money);
+    }
+
+    private void isValidatedMoney(String money) {
 
     }
 
