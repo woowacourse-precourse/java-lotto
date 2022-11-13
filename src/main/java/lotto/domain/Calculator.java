e905853f8501437fdab8dd55b2ae979ac51b224f
3juhwan <13selfesteem91@naver.com>
2022-11-14 01:13:08 +0900

    feat(lotto): 통계 계산기 구현

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -1,14 +1,20 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Calculator {
+    private static List<Integer> digit = Arrays.asList(5000, 50000, 1500000, 30000000, 2000000000);
 
-    private Integer getWinningAmount (List<Integer> results) {
-        return 0;
+    private static Integer getWinningAmount(List<Integer> results) {
+        int winningAmount = 0;
+        for(int i=0; i<digit.size(); i++) {
+            winningAmount += results.get(i) * digit.get(i);
+        }
+        return winningAmount;
     }
 
-    public float getEarningRate (List<Integer> results) {
-        return 0;
+    public static float getEarningRate(List<Integer> results) {
+        return Math.round(getWinningAmount(results) / results.size());
     }
 }
