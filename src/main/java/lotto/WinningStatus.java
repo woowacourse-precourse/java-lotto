423b92b797f9e8241bd944f0e1026f6f812ce150
d0ri123 <lsh81550@gmail.com>
2022-11-13 15:45:40 +0900

    feat: 당첨 케이스를 enum에 작성

---
+++ src/main/java/lotto/WinningStatus.java
@@ -0,0 +1,25 @@
+package lotto;
+
+public enum WinningStatus {
+    CASE1(3,5000),
+    CASE2(4,50_000),
+    CASE3(5,1_500_000),
+    CASE4(5,30_000_000),
+    CASE5(6,2_000_000_000);
+
+    private int matchNumber;
+    private int winningPrice;
+
+    WinningStatus(int matchNumber, int winningPrice){
+        this.matchNumber = matchNumber;
+        this.winningPrice = winningPrice;
+    }
+
+    public int getMatchNumber(){
+        return matchNumber;
+    }
+
+    public int getWinningPrice(){
+        return winningPrice;
+    }
+}
