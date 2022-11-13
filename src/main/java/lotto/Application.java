c369cf6f57d6b68f7d8f78cb1859694e72fa78c0
taehyun <didls1643@gmail.com>
2022-11-13 20:34:54 +0900

    feat: make lotto Number

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public List<Lotto> makeLottoNumber(List<Lotto> lottos, int num){
+        for(int i = 0; i < num; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+        }
+        return lottos;
     }
 }
