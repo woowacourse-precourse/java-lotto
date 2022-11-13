c619497286bec3eddb4528c062244940e43ab226
jjmin9797 <jjmin9797@naver.com>
2022-11-14 01:15:19 +0900

    feat(UserService.java): 당첨된 복권에 따라 상금을 구하는 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/service/UserService.java
+++ src/main/java/lotto/service/UserService.java
@@ -1,9 +1,29 @@
 package lotto.service;
 
+import lotto.domain.Prize;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class UserService {
+
+    private final static ArrayList<Prize> PRIZE_TYPE = new ArrayList<>(Arrays.asList(
+            Prize.FIRST_PLACE,
+            Prize.SECOND_PLACE,
+            Prize.THIRD_PLACE,
+            Prize.FOURTH_PLACE,
+            Prize.FIFTH_PLACE));
 
     public String calculateTheRateOfReturn(int prizeMoney, int purchaseMoney) {
         float rateOfReturn = ((float) prizeMoney / (float) purchaseMoney) * 100;
         return String.format("%.1f", rateOfReturn);
+    }
+
+    public int calculateUserPrize(ArrayList<Integer> numberOfWins) {
+        int totalPrizeMoney = 0;
+        for (int i = 0; i < 5; i++) {
+            totalPrizeMoney += PRIZE_TYPE.get(i).getPrizeMoney() * numberOfWins.get(i);
+        }
+        return totalPrizeMoney;
     }
 }
