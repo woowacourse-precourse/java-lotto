9f96f9e1f53043d53b474690a536ec83ac8e15c4
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-13 13:30:33 +0900

    feat(OutputView): 당첨 통계 출력 기능 추가

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class OutputView {
     private static final String NUMBER_OF_LOTTOS_PURCHASED_MESSAGE = "개를 구매했습니다.";
+    private static final String START_WINNING_STATICS_MESSAGE = "당첨 통계\n---";
+    private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %f입니다.";
 
     public void printNumberOfLottosPurchased(int numberOfLottosPurchased) {
         System.out.println(numberOfLottosPurchased + NUMBER_OF_LOTTOS_PURCHASED_MESSAGE);
@@ -13,5 +15,17 @@ public class OutputView {
         for (Lotto lotto: lottos) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public void printStartStatics() {
+        System.out.println(START_WINNING_STATICS_MESSAGE);
+    }
+
+    public void printStatics(String message) {
+        System.out.println(message);
+    }
+
+    public void printRateOfReturn(double rateOfReturn) {
+        System.out.printf((RATE_OF_RETURN_MESSAGE) + "%n", rateOfReturn);
     }
 }
