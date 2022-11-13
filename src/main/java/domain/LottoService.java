f7b364d13075104230191f02fddcc473b66e76fb
Limjongho <whdgh9873@icloud.com>
2022-11-13 15:55:46 +0900

    feat(LottoService): 로또 발행 기능 추가

--- src/main/java/domain/LottoService.java
+++ src/main/java/domain/LottoService.java
@@ -1,29 +1,60 @@
 package domain;
 
-import ui.UserInput;
+import lotto.Lotto;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static util.Constant.LOTTO_PRICE;
 
 public class LottoService {
 
-    private final UserInput userInput;
-    private final Result result;
+    private int totalEarning;
+    private List<Prize> userPrizes;
+    private int money=0;
 
-    public LottoService(UserInput userInput, Result result) {
-        this.userInput = userInput;
-        this.result = result;
+    public LottoService() {
+        this.totalEarning = 0;
+        this.userPrizes = new ArrayList<>();
     }
 
-    public Prize getResultOfLotto(List<Integer> userNumbers) {
-        List<Integer> winningNumbers = InputConverter.getWinningNumbers(userInput.enterWinningLottoNumber());
-        int bonusNumber = InputConverter.getNumber(userInput.enterBonusNumber());
-
+    public Prize getResultOfLotto(
+            List<Integer> winningNumbers,
+            List<Integer> userNumbers,
+            int bonusNumber
+    ) {
+        Result result = new Result();
         int matchingCount = result.countMatchingNumber(winningNumbers, userNumbers);
         boolean checkBonusNumber = false;
         if (matchingCount == 5) {
             checkBonusNumber = result.isMatchingBonusNumber(userNumbers, bonusNumber);
         }
         Rank rank = result.getRank(matchingCount, checkBonusNumber);
-        return result.getPrize(rank);
+        Prize prize = result.getPrize(rank);
+        userPrizes.add(prize);
+        earnMoney(prize.rankingPrize);
+        return prize;
+    }
+
+    public List<Lotto> publishLotto(int numberOfLotto) {
+        List<Lotto> publishedLotto = new ArrayList<>();
+        while (numberOfLotto > 0) {
+            List<Integer> userLottoNumbers = NumberGenerator.createUniqueRandomNumbers();
+            Lotto lotto = new Lotto(userLottoNumbers);
+
+            publishedLotto.add(lotto);
+            numberOfLotto--;
+        }
+        return publishedLotto;
+    }
+
+    public int getNumberOfLotto(String moneyInput) {
+        int money = InputConverter.getMoney(moneyInput);
+        this.money = money;
+        return money / LOTTO_PRICE;
+    }
+
+    private void earnMoney(int prize) {
+        this.totalEarning += prize;
     }
 }
