162ef5af4d081e7f3255638705167c571d05b564
1eejisoo <jisu991012@gmail.com>
2022-11-14 03:50:51 +0900

    feat: 보너스 번호 유효성 검증 - 한 개의 숫자인가

--- src/main/java/controller/BonusController.java
+++ src/main/java/controller/BonusController.java
@@ -4,10 +4,20 @@ import domain.Bonus;
 import domain.Lotto;
 import service.BonusService;
 
+import java.util.regex.Pattern;
+
 public class BonusController {
+    private static final String FORMAT = "^[0-9]$";
     private final BonusService bonusService = new BonusService();
 
     public Bonus createBonusNumber(String bonusNumber, Lotto lotto) {
+        isOneNumber(bonusNumber);
         return bonusService.createBonusNumber(Integer.parseInt(bonusNumber), lotto);
+    }
+
+    public void isOneNumber(String bonusNumber) {
+        if (!Pattern.matches(FORMAT, bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호는 한 개의 숫자여야 합니다.");
+        }
     }
 }
