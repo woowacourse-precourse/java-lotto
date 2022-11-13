e3bad8b644caed28ed3cc7e497adc1cc9194b056
qdeu22 <qdeu22@gmail.com>
2022-11-13 19:55:46 +0900

    refactor(lotto): 이중 리스트를 이용하여 여러개의 로또번호를 담아서 반환하도록 변경

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -2,11 +2,16 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NumberGenerator {
-    public List<Integer> createNumbers() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        return numbers;
+    public List<List<Integer>> createNumbers(int count) {
+        List<List<Integer>> number = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            number.add(numbers);
+        }
+        return number;
     }
 }
