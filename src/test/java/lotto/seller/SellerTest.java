eeacb85d50025c0b45c22941a54d124cd2935a5d
aksmf1442 <aksmf1442@naver.com>
2022-11-13 20:24:21 +0900

    refactor(로또): Lotto 클래스 패키지 변경

--- src/test/java/lotto/seller/SellerTest.java
+++ src/test/java/lotto/seller/SellerTest.java
@@ -2,19 +2,20 @@ package lotto.seller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import lotto.domain.seller.Seller;
 import lotto.domain.seller.Country;
+import lotto.domain.seller.Seller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SellerTest {
 
-    @DisplayName("구입 금액을 통해서 로또를 몇 장 구매할 수 있는지 찾기")
+    @DisplayName("[성공] 구입 금액을 통해서 로또를 몇 장 구매할 수 있는지 찾기")
     @Test
-    void findLottoCountByMoney(){
+    void findLottoCountByMoney() {
         Seller seller = new Country();
         int money = 10000;
         int lottoCount = seller.findLottoCountByMoney(money);
         assertThat(lottoCount).isEqualTo(10);
     }
+    
 }
