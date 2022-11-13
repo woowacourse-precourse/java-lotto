a4286856a804ba40255a67ecf2d28e410b6cc506
60jong <rudwhd515@gmail.com>
2022-11-13 22:06:03 +0900

    feat(LottoProgram) : '로또 구매 수량을 출력할 수 있다.' 기능 추가

--- src/main/java/lotto/LottoProgram.java
+++ src/main/java/lotto/LottoProgram.java
@@ -2,21 +2,57 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoProgram {
+    private final String ORDER_QUANTITY_ALERT_MESSAGE = "%d개를 구매했습니다.\n";
 
     private LottoController lottoController;
 
     public LottoProgram(LottoController lottoController) {
         this.lottoController = lottoController;
     }
-    public void run() {
-        int orderPrice = this.getOrderPrice();
-        int orderQuantity = lottoController.getOrderQuantity(orderPrice);
 
-        lottoController.createLottos(orderQuantity);
+    // 로또 프로그램 실행
+    public void run() {
+        List<Lotto> userLottos = createUserLottos();
+        WinningLotto winningLotto = createWinningLotto();
     }
 
-    public int getOrderPrice() {
+    public List<Lotto> createUserLottos() {
+        int orderPrice = getOrderPrice();
+        int orderQuantity = lottoController.getOrderQuantity(orderPrice);
+        alertOrderQuantity(orderQuantity);
+
+        return lottoController.createLottos(orderQuantity);
+    }
+
+    private void alertOrderQuantity(int orderQuantity) {
+        System.out.printf(ORDER_QUANTITY_ALERT_MESSAGE, orderQuantity);
+    }
+
+    private int getOrderPrice() {
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public WinningLotto createWinningLotto() {
+        List<Integer> lottoNumbers = getLottoNumbers();
+        return lottoController.createWinningLotto(lottoNumbers, getBonusNumber());
+    }
+
+    private List<Integer> getLottoNumbers() {
+        List<Integer> lottoNumbers = new ArrayList<>();
+
+        String[] splittedNumbers = Console.readLine().split(",");
+        for (String splittedNumber : splittedNumbers) {
+            lottoNumbers.add(Integer.parseInt(splittedNumber));
+        }
+
+        return lottoNumbers;
+    }
+
+    private int getBonusNumber() {
         return Integer.parseInt(Console.readLine());
     }
 
