61f07f9a3380655fd29970c630b3b61f3cddc2bd
jagajang <kosb7544@gmail.com>
2022-11-13 21:09:18 +0900

    dev: print generated lotto list

--- src/main/java/lotto/IOManager.java
+++ src/main/java/lotto/IOManager.java
@@ -2,15 +2,33 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.config.LottoConfig;
+import lotto.message.IOMessage;
+
+import java.util.List;
 
 public class IOManager {
     public static int buyLotto() throws IllegalArgumentException {
+        System.out.println(IOMessage.ASK_PRICE);
+
         try {
             int price = Integer.parseInt(Console.readLine());
+            System.out.println(IOMessage.EMPTY_LINE);
 
             return price / LottoConfig.LOTTO_PRICE;
         } catch(Exception e) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public static void printLottoList(List<Lotto> lottoList) {
+        System.out.println(String.valueOf(lottoList.size()) + IOMessage.LOTTO_BUY_NUM);
+        for(Lotto lotto : lottoList) {
+            printLotto(lotto);
+        }
+        System.out.println(IOMessage.EMPTY_LINE);
+    }
+
+    private static void printLotto(Lotto lotto) {
+        System.out.println(lotto.toString());
     }
 }
--- src/main/java/lotto/message/IOMessage.java
+++ src/main/java/lotto/message/IOMessage.java
@@ -5,8 +5,7 @@ public enum IOMessage {
     ASK_PRICE("구입금액을 입력해 주세요."),
     LOTTO_BUY_NUM("개를 구매했습니다."),
     ASK_ANSWER("당첨 번호를 입력해 주세요."),
-    ASK_BONUS("보너스 번호를 입력해 주세요."),
-    ;
+    ASK_BONUS("보너스 번호를 입력해 주세요.");
 
     private final String message;
 
