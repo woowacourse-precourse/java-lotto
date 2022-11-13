996ca0f042b7320afa02b8b535888763094333b6
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-11-13 22:02:51 +0900

    test: 문자열 생성자 테스트 케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class LottoTest {
+class LottoTest extends NsTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -38,4 +40,32 @@ class LottoTest {
     void 숫자_범위_테스트3() {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
     }
+
+    @Test
+    void 문자열_생성자_테스트1() {
+        Lotto lotto = new Lotto("1,2,3,4,5,6");
+        lotto.printNumbers();
+        assertThat(output()).contains("[1, 2, 3, 4, 5, 6]");
+    }
+
+    @Test
+    void 문자열_생성자_테스트2() {
+        assertThatThrownBy(() -> new Lotto("1,2,3,4,5, 6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 문자열_생성자_테스트3() {
+        assertThatThrownBy(() -> new Lotto("1,2,3,4,5.6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 문자열_생성자_테스트4() {
+        assertThatThrownBy(() -> new Lotto("1,2,3,4,5.6,7"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Override
+    public void runMain() { }
 }
