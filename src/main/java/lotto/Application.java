31563da49468dbc9d18c673fa934992b969c8fb0
DongWooE <ehddn977@naver.com>
2022-11-14 05:35:21 +0900

    [feat] 로또 금액 입력 안내 문구 출력 메소드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,11 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application application = new Application();
+        application.printInsertPrice();
+    }
+
+    private void printInsertPrice(){
+        System.out.println("구입금액을 입력해 주세요.");
     }
 }
