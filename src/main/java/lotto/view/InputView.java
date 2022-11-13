bcfa1fd2b54167d006756035ebe1963babc4d26f
dhkim <akdlzmf1123@gmail.com>
2022-11-14 00:16:51 +0900

    feat(InputView): 당첨 번호 입력받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,10 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
     private static InputView instance;
 
@@ -16,10 +20,33 @@ public class InputView {
     }
 
     public int inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+
         String purchaseAmount = Console.readLine();
 
         Validator.validatePurchaseAmount(purchaseAmount);
 
         return Integer.parseInt(purchaseAmount);
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+
+        String winningNumbers = Console.readLine();
+
+        Validator.validateWinningNumbersFormat(winningNumbers);
+
+        return toNumbers(winningNumbers);
+    }
+
+    private List<Integer> toNumbers(String winningNumbers) {
+        List<Integer> numbers = Arrays.stream(winningNumbers.split(","))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+
+        Validator.validateWinningNumbersRange(numbers);
+
+        return numbers;
     }
 }
--- src/main/java/lotto/view/Validator.java
+++ src/main/java/lotto/view/Validator.java
@@ -1,13 +1,46 @@
 package lotto.view;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class Validator {
 
     private static final String PATTERN_PURCHASE_AMOUNT = "^[0-9]+$";
+    public static final String PATTERN_WINNING_NUMBERS = "^[0-9]+(,[0-9]+){5}$";
+    public static final int WINNING_NUMBER_LOWER_BOUNDS = 1;
+    public static final int WINNING_NUMBER_UPPER_BOUNDS = 45;
     public static final String ERR_PURCHASE_AMOUNT = "[ERROR] 구매 금액은 1000원 단위여야 합니다. 예시) 7000(O), 15000(O), 6500(X)";
+    public static final String ERR_WINNING_NUMBERS_FORMAT =  "[ERROR] 당첨 번호는 ','로 구분된 6개의 숫자입니다. 예시) 1,2,3,4,5,6(O)";
+    public static final String ERR_WINNING_NUMBERS_RANGE = "[ERROR] 당첨 번호는 1 ~ 45 사이의 숫자입니다. 예시) 1,7,13,25,38,43(O), 0,2,3,4,46,48,90(X)";
 
     public static void validatePurchaseAmount(String purchaseAmount) {
         if (!purchaseAmount.matches(PATTERN_PURCHASE_AMOUNT)) {
             throw new IllegalArgumentException(ERR_PURCHASE_AMOUNT);
         }
+    }
+
+    public static void validateWinningNumbersFormat(String winningNumbers) {
+        if (!winningNumbers.matches(PATTERN_WINNING_NUMBERS)) {
+            throw new IllegalArgumentException(ERR_WINNING_NUMBERS_FORMAT);
+        }
+    }
+
+    public static void validateWinningNumbersRange(List<Integer> winningNumbers) {
+        if (isOutOfRange(winningNumbers)) {
+            throw new IllegalArgumentException(ERR_WINNING_NUMBERS_RANGE);
+        }
+
+        if (hasDuplicate(winningNumbers)) {
+            throw new IllegalArgumentException(ERR_WINNING_NUMBERS_RANGE);
+        }
+    }
+
+    private static boolean hasDuplicate(List<Integer> winningNumbers) {
+        return new HashSet<>(winningNumbers).size() != 6;
+    }
+
+    private static boolean isOutOfRange(List<Integer> winningNumbers) {
+        return winningNumbers.stream()
+                .anyMatch(number -> number < WINNING_NUMBER_LOWER_BOUNDS || number > WINNING_NUMBER_UPPER_BOUNDS);
     }
 }
