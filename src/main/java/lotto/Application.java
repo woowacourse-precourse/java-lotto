034f4575173b3855429da2d14b2fab63233b0afc
seonghoo1217 <seonghoo1217@naver.com>
2022-11-13 18:59:07 +0900

    fix : 영문자의 경우 원하는 에러타입과 맞지않게 NumberFormatException이 발생
    예외처리 구문을 최초로 실행하기전 BigInteger 타입으로 입력된 값을 변경하기 때문에 기존코드는 에러를 발생시키면 버그를 일으킨다.
    
    또한 코드의 효율성을 높이고자 BuyLotto 클래스의 prepare_money 함수에 전달하는 방식으로 리팩터링
    
    - 매개변수를 받아 테스트코드를 작성하는 효율을 높이기 위함이기도하다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,10 @@
 package lotto;
 
+import static lotto.util.InputImport.*;
 
 public class Application {
     public static void main(String[] args) {
-        BuyLotto.prepare_money();
+        System.out.println("구입금액을 입력해 주세요.");
+        BuyLotto.prepare_money(ReadLine());
     }
 }
--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -5,17 +5,19 @@ import java.math.BigInteger;
 public class Validation {
 
 	public static boolean money_validation(String validation_param){
-		BigInteger money = new BigInteger(validation_param);
-		BigInteger target = BigInteger.valueOf(1000);
-		if (validation_param.matches("^[0-9]*$")
-										&&
-												money.remainder(target).compareTo(BigInteger.valueOf(0))==0){
+		if (validation_param.matches("^[0-9]*$")){
+			BigInteger money = new BigInteger(validation_param);
+			BigInteger target = BigInteger.valueOf(1000);
+			if (money_remainder_result_equal_zero(money,target)){
+				return true;
+			}
+		}
+		return false;
+	}
+	public static boolean money_remainder_result_equal_zero(BigInteger money,BigInteger target){
+		if (money.remainder(target).compareTo(BigInteger.valueOf(0))==0){
 			return true;
 		}
 		return false;
 	}
-
-
-
-
 }
