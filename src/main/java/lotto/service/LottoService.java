27c520d0e66afd73e854c3d7515adf451dbb9453
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-13 12:00:38 +0900

    feat(LottoService): 로또의 순위를 결정하는 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -8,8 +8,13 @@ import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
+import lotto.domain.LottoRankStatus;
+import lotto.domain.WinningLotto;
 
 public class LottoService {
+
+	private int matchCount;
+	private int bonusMatchCount;
 
 	public Lotto makeLotto() {
 		List<Integer> randomLottoNumbers = new ArrayList<>(makeRandomLottoNumbers());
@@ -18,7 +23,53 @@ public class LottoService {
 		return new Lotto(randomLottoNumbers);
 	}
 
+	public LottoRankStatus determineLottoRankStatus(Lotto lotto, WinningLotto winningLotto) {
+		if (lotto == null || winningLotto == null) {
+			return null;
+		}
+		List<Integer> winningLottoNumbers = winningLotto.getNumbers();
+		List<Integer> winningLottoBonusNumbers = winningLotto.getBonusNumbers();
+		matchCount = compare(lotto, winningLottoNumbers);
+		bonusMatchCount = compare(lotto, winningLottoBonusNumbers);
+		return determineOverFifthRank();
+	}
+
 	private List<Integer> makeRandomLottoNumbers() {
 		return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LIMIT_SIZE);
+	}
+
+	private LottoRankStatus determineOverFifthRank() {
+		if (matchCount == FIFTH_RANK_CONDITION) {
+			return LottoRankStatus.FIFTH_RANK;
+		}
+		if (matchCount == FOURTH_RANK_CONDITION) {
+			return LottoRankStatus.FOURTH_RANK;
+		}
+		if (matchCount == THIRD_RANK_CONDITION && bonusMatchCount != SECOND_RANK_BONUS_CONDITION) {
+			return LottoRankStatus.THIRD_RANK;
+		}
+		return determineOverSecondRank();
+	}
+
+	private LottoRankStatus determineOverSecondRank() {
+		if (matchCount == SECOND_RANK_CONDITION && bonusMatchCount == SECOND_RANK_BONUS_CONDITION) {
+			return LottoRankStatus.SECOND_RANK;
+		}
+		if (matchCount == FIRST_RANK_CONDITION) {
+			return LottoRankStatus.FIRST_RANK;
+		}
+		return LottoRankStatus.OUT_OF_RANK;
+	}
+
+	private int compare(Lotto lotto, List<Integer> numbers) {
+		int containCount = 0;
+		List<Integer> lottoNumbers = lotto.getNumbers();
+
+		for (int lottoNumber : lottoNumbers) {
+			if (numbers.contains(lottoNumber)) {
+				containCount += 1;
+			}
+		}
+		return containCount;
 	}
 }
