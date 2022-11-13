b7b8662d595e29801effa525780bcffbe72be91d
안광민 <creativeduck0518@gmail.com>
2022-11-13 20:20:43 +0900

    feat(Error): 사용자 입력값 예외 추가

--- src/main/java/lotto/data/Error.java
+++ src/main/java/lotto/data/Error.java
@@ -4,12 +4,13 @@ public enum Error {
     OUT_OF_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     DUPLICATE_NUMBER("로또 번호는 중복되지 않아야 합니다."),
     OVER_SIZE("로또 번호는 6개이어야 합니다."),
-    INVALID_PRICE("금액은 1,000원으로 나누어 떨어져야 합니다.");
+    INVALID_PRICE("금액은 1,000원으로 나누어 떨어져야 합니다."),
+    INVALID_INPUT("입력값은 숫자이어야 합니다.");
 
     private String message;
 
     Error(String message) {
-        this.message = message;
+        this.message = "[ERROR] " + message;
     }
 
     public String getMessage() {
