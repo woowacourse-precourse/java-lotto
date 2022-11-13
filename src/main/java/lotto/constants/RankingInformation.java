8a56c476c93c9703b62c38a6ad18d6f43fcfb635
Sehbeom <j3beom@gmail.com>
2022-11-13 22:10:14 +0900

    refactor(constants): 순위정보 열거상수 수익 필드 추가
    수익률 계산 기능을 위해 순위정보 열거상수에 수익 필드 추가
    amountOfReturn

--- src/main/java/lotto/constants/RankingInformation.java
+++ src/main/java/lotto/constants/RankingInformation.java
@@ -1,18 +1,20 @@
 package lotto.constants;
 
 public enum RankingInformation {
-    FIFTH_PLACE("3개 일치 (5,000원) - ", 3),
-    FOURTH_PLACE("4개 일치 (50,000원) - ", 4),
-    THIRD_PLACE("5개 일치 (1,500,000원) - ", 5),
-    SECOND_PLACE("5개 일치, 보너스 볼 일치 (30,000,000원) - ", 5),
-    FIRST_PLACE("6개 일치 (2,000,000,000원) - ", 6);
+    FIFTH_PLACE("3개 일치 (5,000원) - ", 3, 5_000),
+    FOURTH_PLACE("4개 일치 (50,000원) - ", 4, 50_000),
+    THIRD_PLACE("5개 일치 (1,500,000원) - ", 5, 1_500_000),
+    SECOND_PLACE("5개 일치, 보너스 볼 일치 (30,000,000원) - ", 5, 30_000_000),
+    FIRST_PLACE("6개 일치 (2,000,000,000원) - ", 6, 2_000_000_000);
 
     private final String statisticSentence;
     private final int numberOfMatch;
+    private final int amountOfReturn;
 
-    RankingInformation(String sentence, int number) {
+    RankingInformation(String sentence, int number, int amount) {
         this.statisticSentence = sentence;
         this.numberOfMatch = number;
+        this.amountOfReturn = amount;
     }
 
     public String getStatisticSentence() {
@@ -21,5 +23,9 @@ public enum RankingInformation {
 
     public int getNumberOfMatch() {
         return this.numberOfMatch;
+    }
+
+    public int getAmountOfReturn() {
+        return this.amountOfReturn;
     }
 }
