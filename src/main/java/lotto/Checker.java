ed841c6278bd815ef144d7c988acc1f0ba8e7405
nn98 <jkllhgb@gmail.com>
2022-11-13 19:23:22 +0900

    Feat: User/Lotto 공통 체크 메소드, 개별 체크 메소드 분리

--- src/main/java/lotto/Checker.java
+++ src/main/java/lotto/Checker.java
@@ -2,13 +2,15 @@ package lotto;
 
 import constance.Texts;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Checker {
-    public int checkInputNumber(String input) {
+    public int checkNumber(String input) {
 
         int number = checkNumeric(input);
 
-        checkNegative(number);
-        checkAmount(number);
+        this.checkNegative(number);
 
         return number;
 
@@ -16,11 +18,25 @@ public class Checker {
 
     public int checkAmountInput(String input) {
 
-        int amount = checkInputNumber(input);
+        int amount = checkNumber(input);
 
-        checkRemain(amount);
+        this.checkPayment(amount);
+        this.checkRemain(amount);
 
         return amount;
+    }
+
+    public List<Integer> checkNumbersInput(String input) {
+
+        List<Integer> numbers=new ArrayList<>();
+        String[] inputs = input.split(",");
+
+        for(String s:inputs){
+            int number=this.checkNumber(s);
+            numbers.add(number);
+        }
+
+        return numbers;
     }
 
     public int checkNumeric(String input) {
@@ -28,20 +44,20 @@ public class Checker {
         try {
             result = Integer.parseInt(input);
         } catch (Exception e) {
-            throw new IllegalArgumentException(String.format(Texts.LOTTO_CHECKER_ERROR, Texts.LOTTO_CHECKER_ERROR_NON_NUMERIC));
+            throw new IllegalArgumentException(String.format(Texts.LOTTO_CHECKER_ERROR, input, Texts.LOTTO_CHECKER_ERROR_NON_NUMERIC));
         }
         return result;
     }
 
     public void checkNegative(int input) {
         if (input < 0) {
-            throw new IllegalArgumentException(String.format(Texts.LOTTO_CHECKER_ERROR, Texts.LOTTO_CHECKER_ERROR_NEGATIVE));
+            throw new IllegalArgumentException(String.format(Texts.LOTTO_CHECKER_ERROR, input, Texts.LOTTO_CHECKER_ERROR_NEGATIVE));
         }
     }
 
-    public void checkAmount(int input) {
+    public void checkPayment(int input) {
         if (input < 1000) {
-            throw new IllegalArgumentException(String.format(Texts.LOTTO_CHECKER_ERROR, Texts.LOTTO_CHECKER_ERROR_LACK_OF_AMOUNT));
+            throw new IllegalArgumentException(String.format(Texts.LOTTO_CHECKER_ERROR, input, Texts.LOTTO_CHECKER_ERROR_LACK_OF_AMOUNT));
         }
     }
 
@@ -49,7 +65,7 @@ public class Checker {
         int remain;
         if (input % 1000 > 0) {
             remain = input % 1000;
-            System.out.println(String.format(Texts.LOTTO_CHECKER_EXCEPTION_AMOUNT_REMAIN, remain));
+            System.out.println(String.format(Texts.LOTTO_CHECKER_EXCEPTION_AMOUNT_REMAIN, input, remain));
             input -= remain;
         }
         return input;
