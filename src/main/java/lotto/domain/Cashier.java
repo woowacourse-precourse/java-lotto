694ab12d4a30215898721be2c74d54e114ef8c34
Woojin Lee <dhkep03@gmail.com>
2022-11-13 17:37:28 +0900

    feat(Cashier): 로또 구매 요청

--- src/main/java/lotto/domain/Cashier.java
+++ src/main/java/lotto/domain/Cashier.java
@@ -1,4 +1,14 @@
 package lotto.domain;
 
+import java.util.List;
+import lotto.ui.UserInterface;
+
 public class Cashier {
+    private List<Lotto> lottos;
+    private Account account;
+
+    public void buyLottos() {
+        int inputMoney = UserInterface.getInputMoney();
+        this.lottos = LottoMachine.publish(inputMoney);
+    }
 }
