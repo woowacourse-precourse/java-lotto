a94eec9910521ce82f8df7ca83dfc73efaaed824
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-13 16:14:26 +0900

    test(service): getLottoRankResultsByLottos method
    test by
    - detect each ranked lotto

--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -25,4 +25,22 @@ class LottoServiceTest {
         //then
         assertEquals(6, uniqueLottoNumbers.size());
     }
+
+    @Test
+    void getLottoRankResultsByLottosTest() {
+        //given
+        Lotto lotto1 = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto lotto2 = new Lotto(List.of(1,2,3,4,5,7));
+        Lotto lotto3 = new Lotto(List.of(1,2,3,4,5,8));
+        Lotto lotto4 = new Lotto(List.of(3,4,5,6,7,8));
+        Lotto lotto5 = new Lotto(List.of(3,4,5,7,8,9));
+
+        //when
+        List<Lotto> lottos = List.of(lotto1,lotto2,lotto3,lotto4,lotto5);
+        List<Integer> lottoRankResult = lottoService.getLottoRankResultsByLottos(lottos);
+        System.out.println(lottoRankResult);
+
+        //then
+        assertEquals(lottoRankResult, List.of(1,1,1,1,1));
+    }
 }
