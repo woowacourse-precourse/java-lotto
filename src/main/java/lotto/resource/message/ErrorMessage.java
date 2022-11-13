0fd9d2a39afec4af14aa84ce9292442d7a8f993e
senor14 <dkssud940@gmail.com>
2022-11-13 22:34:58 +0900

    feat(lotto): 구매 금액 입력 로직 구현

---
+++ src/main/java/lotto/resource/message/ErrorMessage.java
@@ -0,0 +1,20 @@
+package lotto.resource.message;
+
+public enum ErrorMessage {
+
+    PAYOUT_IS_NOT_INTEGER_OR_NUMBER_OUT_OF_RANGE_MESSAGE("입력된 값이 숫자가 아니거나 범위를 벗어났습니다."),
+    PAYOUT_IS_NOT_DIVISIBLE_BY_THOUSANDS_MESSAGE("입력된 값이 1,000원 단위가 아닙니다."),
+
+    NUMBER_IS_NOT_BETWEEN_1_AND_45_MESSAGE("입력된 값이 1,000원 단위가 아닙니다."),
+    NUMBER_IS_NOT_POSITIVE_MESSAGE("입력된 값이 양수가 아닙니다.");
+
+    private final String value;
+
+    ErrorMessage(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return "[ERROR] " + value;
+    }
+}
