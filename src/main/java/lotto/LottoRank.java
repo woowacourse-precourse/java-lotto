686d3f48774a44d082d2822604c1ab51af480c2e
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-13 17:18:10 +0900

    fix(VO): reorder LottoRank ordinary
    changed order
    - origin : first, second, third, fourth, fifth
    - changed : fifth, fourth, third, second, first

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -4,34 +4,34 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum LottoRank {
-    FIRST(2_000_000_000L,
-            Arrays.asList(
-                    new LottoHitsCountVO(6, true),
-                    new LottoHitsCountVO(6, false)
-            ),
-            "6개 일치 (2,000,000,000원)"),
-    SECOND(30_000_000L,
-            Arrays.asList(
-                    new LottoHitsCountVO(5, true)
-            ),
-            "5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    THIRD(1_500_000L,
-            Arrays.asList(
-                    new LottoHitsCountVO(5, false)
-            ),
-            "5개 일치 (1,500,000원)"),
-    FOURTH(50_000L,
-            Arrays.asList(
-                    new LottoHitsCountVO(4, true),
-                    new LottoHitsCountVO(4, false)
-            ),
-            "4개 일치 (50,000원)"),
     FIFTH(5_000L,
             Arrays.asList(
                     new LottoHitsCountVO(3, true),
                     new LottoHitsCountVO(3, false)
             ),
             "3개 일치 (5,000원)"),
+    FOURTH(50_000L,
+            Arrays.asList(
+                    new LottoHitsCountVO(4, true),
+                    new LottoHitsCountVO(4, false)
+            ),
+            "4개 일치 (50,000원)"),
+    THIRD(1_500_000L,
+            Arrays.asList(
+                    new LottoHitsCountVO(5, false)
+            ),
+            "5개 일치 (1,500,000원)"),
+    SECOND(30_000_000L,
+            Arrays.asList(
+                    new LottoHitsCountVO(5, true)
+            ),
+            "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    FIRST(2_000_000_000L,
+            Arrays.asList(
+                    new LottoHitsCountVO(6, true),
+                    new LottoHitsCountVO(6, false)
+            ),
+            "6개 일치 (2,000,000,000원)"),
     NOTHING(0L, null, "");
 
     private Long prize;
