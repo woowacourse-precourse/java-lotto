cd50fff985e747f1e6a866e404abc5824a3c687c
Kim-Jong-Gyu <whdrb2643@gmail.com>
2022-11-13 19:31:59 +0900

    feat(lotto): 당첨 관련 enum 클래스 생성 기능 추가
    당첨 관련 enum 클래스 생성 기능 추가:
    - Result enum class: 등수별 조건과 금액 그리고 맞춘 개수 세개의 변수를 가지는 RESULT 객체 생성

--- src/main/java/controller/Result.java
+++ src/main/java/controller/Result.java
@@ -1,5 +1,30 @@
 package controller;
 
 public enum Result {
+    FIRST("6개 번호 일치", "2,000,000,000원", 6),
+    SECOND("5개 번호 + 보너스 번호 일치","30,000,000,000원",5),
+    THIRD("5개 번호 일치","1,500,000", 5),
+    FOURTH("4개 번호 일치","50,000",4),
+    FIFTH("3개 번호 일치","5,000",3);
 
+    private final String condition;
+    private final String winningCount;
+    private final int winningNumber;
+    Result(String condition, String winningCount, int winningNumber) {
+        this.condition = condition;
+        this.winningCount = winningCount;
+        this.winningNumber = winningNumber;
+    }
+
+    public String getCondition() {
+        return this.condition;
+    }
+
+    public String getWinningCount() {
+        return this.winningCount;
+    }
+
+    public int getWinningNumber() {
+        return this.winningNumber;
+    }
 }
