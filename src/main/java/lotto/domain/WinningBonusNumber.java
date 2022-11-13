7f47e74718431a639a02588ce585e8d6e925dd98
Reznov <reznov03048@gmail.com>
2022-11-14 01:20:25 +0900

    feat: 당첨번호와 보너스 번호의 에러 메시지 추가

--- src/main/java/lotto/domain/WinningBonusNumber.java
+++ src/main/java/lotto/domain/WinningBonusNumber.java
@@ -8,6 +8,11 @@ import java.util.Set;
 public class WinningBonusNumber {
     private List<Integer> winningNumbers;
     private int bonusNumber;
+    private final String ERROR_WINNING_NUMBER_DUPLICATED = "[ERROR] 당첨 번호는 중복되면 안됩니다.";
+    private final String ERROR_WINNING_NUMBER_LENGTH = "[ERROR] 당첨 번호는 6자리여야 합니다.";
+    private final String ERROR_WINNING_NUMBER_RANGE = "[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private final String ERROR_BONUS_NUMBER_DUPLICATED = "[ERROR] 보너스 번호는 당첨 번호와 중복되면 안됩니다.";
+    private final String ERROR_BONUS_NUMBER_RANGE = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
 
     public WinningBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
         validateWinningNumbers(winningNumbers);
@@ -35,13 +40,13 @@ public class WinningBonusNumber {
         }
 
         if (checkDuplicated.size() != Number.LOTTO_NUMBER_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_WINNING_NUMBER_DUPLICATED);
         }
     }
 
     private void validateNumbersLength(List<Integer> winningNumbers) {
         if (winningNumbers.size() != Number.LOTTO_NUMBER_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_WINNING_NUMBER_LENGTH);
         }
     }
 
@@ -50,19 +55,19 @@ public class WinningBonusNumber {
         int max = winningNumbers.stream().sorted(Comparator.reverseOrder()).findFirst().get();
 
         if (min < Number.LOTTO_NUMBER_MIN || max > Number.LOTTO_NUMBER_MAX) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_WINNING_NUMBER_RANGE);
         }
     }
 
     private void validateBonusNumberRange(int bonusNumber) {
         if (bonusNumber < Number.LOTTO_NUMBER_MIN || bonusNumber > Number.LOTTO_NUMBER_MAX) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_BONUS_NUMBER_RANGE);
         }
     }
 
     private void validateDuplicatedBonusNumber(int bonusNumber) {
         if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_BONUS_NUMBER_DUPLICATED);
         }
     }
 
