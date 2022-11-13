617a69d56e5467649ac5660e37d531273042f51e
Devtist <yt930310@snu.ac.kr>
2022-11-13 13:24:40 +0900

    feat: 발행한 로또 수량을 출력하는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,10 +2,14 @@ package lotto.controller;
 
 import lotto.domain.PurchasingAmount;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
 	InputView inputView = new InputView();
-	PurchasingAmount purchasingAmount = new PurchasingAmount(Integer.parseInt(inputView.getPurchasingAmount()));
-	int numberOfTickets = purchasingAmount.getNumberOfTickets();
-
+	OutputView outputView = new OutputView();
+	public void excute() {
+		PurchasingAmount purchasingAmount = new PurchasingAmount(Integer.parseInt(inputView.getPurchasingAmount()));
+		int numberOfTickets = purchasingAmount.getNumberOfTickets();
+		outputView.printNumberOfTickets(numberOfTickets);
+	}
 }
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+import lotto.constant.OutputMessage;
+
+public class OutputView {
+	public void printNumberOfTickets(int numberOfTickets) {
+		System.out.println();
+		System.out.printf(OutputMessage.DISPLAYING_PURCHASING_QUANTITY.message(), numberOfTickets);
+		System.out.println();
+	}
+}
