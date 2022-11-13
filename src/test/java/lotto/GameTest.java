e796ce6bca4a8301e717383f8375cf30187d28aa
watterricecake <fruturum@nate.com>
2022-11-14 02:13:46 +0900

    test: rate output

--- src/test/java/lotto/GameTest.java
+++ src/test/java/lotto/GameTest.java
@@ -42,6 +42,37 @@ public class GameTest extends NsTest {
                 List.of(1, 3, 5, 14, 22, 45)
         );
     }
+
+    @Test
+    void allWinningCaseTest() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("15000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "3개 일치 (5,000원) - 5개",
+                            "4개 일치 (50,000원) - 4개",
+                            "5개 일치 (1,500,000원) - 3개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 2개",
+                            "6개 일치 (2,000,000,000원) - 1개"
+                    );
+                },
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(1, 2, 3, 4, 5, 7),
+                List.of(1, 2, 3, 4, 5, 7),
+                List.of(1, 2, 3, 4, 5, 42),
+                List.of(1, 2, 3, 4, 5, 40),
+                List.of(1, 2, 3, 4, 5, 41),
+                List.of(1, 2, 3, 4, 8, 9),
+                List.of(1, 2, 3, 4, 8, 9),
+                List.of(1, 2, 3, 4, 8, 9),
+                List.of(1, 2, 3, 4, 8, 9),
+                List.of(1, 2, 3, 9, 10, 11),
+                List.of(1, 2, 3, 9, 10, 11),
+                List.of(1, 2, 3, 9, 10, 11),
+                List.of(1, 2, 3, 9, 10, 11),
+                List.of(1, 2, 3, 9, 10, 11)
+        );
+    }
     @Override
     protected void runMain() {
         Application.main(new String[]{});
