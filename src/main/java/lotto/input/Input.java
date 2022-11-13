1dd505352164e82f59584384f9dbd94cc9b2725b
Ridealist <cjsn92@gmail.com>
2022-11-13 14:08:30 +0900

    refactor(Input): 사용자 입력값 요청하는 기능 분리

---
+++ src/main/java/lotto/input/Input.java
@@ -0,0 +1,18 @@
+package lotto.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public static String askWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return input;
+    }
+
+    public static String askBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return input;
+    }
+}
