618d8ffe45e752d3c4bcc778ab294e9ca6d60d1e
Woojin Lee <dhkep03@gmail.com>
2022-11-13 18:53:15 +0900

    test(LottoMachine): 유닛 테스트 구현 완료

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,7 +1,33 @@
 package lotto.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static lotto.constant.LottoRule.AMOUNT_OF_ONE_LOTTO;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoMachineTest {
+    @DisplayName("생성한 객체가 로또가 맞는지 확인한다.")
+    @Test
+    void publishLotto() {
+        // given
+        int inputMoney = 1000;
+        // when
+        List<Lotto> lottos = LottoMachine.publish(inputMoney);
+        // then
+        assertThat(lottos.get(0)).isInstanceOf(Lotto.class);
+    }
 
+    @DisplayName("입력 금액에 해당하는 개수만큼 로또를 구매했는지 확인한다.")
+    @Test
+    void publishLottosByAmount() {
+        // given
+        int inputMoney = AMOUNT_OF_ONE_LOTTO * 5;
+        // when
+        List<Lotto> lottos = LottoMachine.publish(inputMoney);
+        int countLottosByAmount = inputMoney / AMOUNT_OF_ONE_LOTTO;
+        // then
+        assertThat(lottos.size()).isEqualTo(countLottosByAmount);
+    }
 }
