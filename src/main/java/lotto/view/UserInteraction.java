e8e75e8c4ee68159a0d8542c03ae1bf0cef8d333
sora5421 <21800804@handong.edu>
2022-11-13 22:57:29 +0900

    feat : 구입금액 입력받기와 구매한 로또 번호 출력하기 추가

--- src/main/java/lotto/view/UserInteraction.java
+++ src/main/java/lotto/view/UserInteraction.java
@@ -1,8 +1,23 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class UserInteraction {
     public UserInteraction() {
     }
 
+    public String money() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        return input;
+    }
 
+    public void showLotto(int count, List<List<Integer>> numbers) {
+        System.out.println("\n" + count + "개를 구매했습니다.");
+        for(int i = 0; i < count; i++) {
+            System.out.println(numbers.get(i));
+        }
+    }
 }
