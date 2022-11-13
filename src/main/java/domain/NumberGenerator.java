59a68c148e41531bdcc009d026006336018e04f0
Limjongho <whdgh9873@icloud.com>
2022-11-13 18:36:48 +0900

    refactor(NumberGenerator): 로또 번호 생성 시 사용하는 라이브러리 요구사항에 맞게 변경

--- src/main/java/domain/NumberGenerator.java
+++ src/main/java/domain/NumberGenerator.java
@@ -3,19 +3,18 @@ package domain;
 import camp.nextstep.edu.missionutils.Randoms;
 import util.Constant;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static util.Constant.MAXIMUM_NUMBER;
+import static util.Constant.MINIMUM_NUMBER;
 
 public class NumberGenerator {
 
     public static List<Integer> createUniqueRandomNumbers() {
-        Set<Integer> LottoNumbers = new HashSet<>();
-        while (LottoNumbers.size() < 6) {
-            int number = Randoms.pickNumberInRange(Constant.MINIMUM_NUMBER, Constant.MAXIMUM_NUMBER);
-            LottoNumbers.add(number);
-        }
-        return new ArrayList<>(LottoNumbers);
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MINIMUM_NUMBER, MAXIMUM_NUMBER, 6);
+        return lottoNumbers.stream()
+                .sorted(Comparator.comparing(Math::abs))
+                .collect(Collectors.toList());
     }
 }
