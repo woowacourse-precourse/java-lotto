edc68f48b274c2fad7488f29175d3c9ec1c932fb
Choi-JY <Choi-JY1107@github.com>
2022-11-14 01:48:44 +0900

    feat: 보너스 번호 검증 기능 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -3,14 +3,33 @@ package lotto.domain;
 import java.util.List;
 
 public class WinningLotto{
-    private static final Integer RANK_TO_LIST = 3;
-    private static final Integer MAXIMUM_RANK = 6;
+    private static final int LOTTO_MINIMUM_NUMBER = 1;
+    private static final int LOTTO_MAXIMUM_NUMBER = 45;
+    private static final int RANK_TO_LIST = 3;
+    private static final int MAXIMUM_RANK = 6;
+    private static final String ERROR_PREFIX_MASSAGE = "[ERROR] ";
+    private static final String ERROR_RANGE_MASSAGE = "로또 번호는 1부터 45까지입니다.";
+    private static final String ERROR_DUPLICATE_MASSAGE = "로또 번호에 중복된 숫자는 없습니다.";
     private static Lotto winningLottoNumber;
     private static Integer bonusNumber;
 
     public WinningLotto(Lotto winningLottoNumber, Integer bonusNumber) {
+	validateBonusNumber(bonusNumber);
+	validateBonusNumberDuplicate(winningLottoNumber, bonusNumber);
 	this.winningLottoNumber = winningLottoNumber;
 	this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(Integer bonusNumber) {
+	if (bonusNumber < LOTTO_MINIMUM_NUMBER || bonusNumber > LOTTO_MAXIMUM_NUMBER) {
+	    throw new IllegalArgumentException(ERROR_PREFIX_MASSAGE + ERROR_RANGE_MASSAGE);
+	}
+    }
+
+    private void validateBonusNumberDuplicate(Lotto winningLottoNumber, Integer bonusNumber) {
+	if (winningLottoNumber.getLottoNumbers().contains(bonusNumber)) {
+	    throw new IllegalArgumentException(ERROR_PREFIX_MASSAGE + ERROR_DUPLICATE_MASSAGE);
+	}
     }
 
     public static Integer compareWinningLotto(Lotto lottoNumbers) {
