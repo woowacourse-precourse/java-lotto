8570670b264ad4e95a6df7b7fdd8eb731b5be298
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2022-11-13 16:21:40 +0900

    feat : (UI) input winning numbers

--- src/main/java/lotto/ui/UI.java
+++ src/main/java/lotto/ui/UI.java
@@ -1,11 +1,16 @@
 package lotto.ui;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
 import lotto.utility.Exception;
 
 public class UI {
     private final String INPUT_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    private final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     private String input(){
         return Console.readLine();
@@ -13,6 +18,26 @@ public class UI {
 
     private void output(String message){
         System.out.println(message);
+    }
+
+    private void stringSplitWithCheckException(List<Integer> winningNumbers){
+        List<String> winningNumberStrings = new ArrayList<>(Arrays.asList(input().split(",")));
+        for(String numberString : winningNumberStrings){
+            try{Exception.checkInputIsNum(numberString);}
+            catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+                winningNumbers.clear();
+                stringSplitWithCheckException(winningNumbers);
+            }
+            int winningNumber = Integer.valueOf(numberString);
+            try{Exception.checkWinningNumberRange(winningNumber);}
+            catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+                winningNumbers.clear();
+                stringSplitWithCheckException(winningNumbers);
+            }
+            winningNumbers.add(winningNumber);
+        }
     }
     
     public int inputPurchasePrice(){
@@ -30,5 +55,22 @@ public class UI {
             return inputPurchasePrice();
         }
         return price;
+    }
+
+    public List<Integer> inputWinningNumbers(){
+        output(INPUT_WINNING_NUMBER_MESSAGE);
+        List<Integer> winningNumbers = new ArrayList<>(6);
+        stringSplitWithCheckException(winningNumbers);
+        try{Exception.checkNumberOfNumbers(winningNumbers);}
+        catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return inputWinningNumbers();
+        }
+        try{Exception.checkDuplicateNumber(winningNumbers);}
+        catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return inputWinningNumbers();
+        }
+        return winningNumbers;
     }
 }
--- src/main/java/lotto/utility/Exception.java
+++ src/main/java/lotto/utility/Exception.java
@@ -1,11 +1,17 @@
 package lotto.utility;
 
+import java.util.HashSet;
+import java.util.List;
+
 public interface Exception {
     public final int ZERO_ASCII = 48;
     public final int NINE_ASCII = 57;
 
     public final String INPUT_IS_NOT_NUM_EXCEPTION_MESSAGE = "[ERROR] 입력 값이 숫자가 아닙니다.";
     public final String PRICE_CAN_NOT_DIVIDE_EXCEPTION_MESSAGE = "[ERROR] 입력 값이 1,000원 단위가 아닙니다.";
+    public final String EXCEED_WINNING_NUMBER_RANGE_EXCEPTION_MESSAGE = "[ERROR] 당첨 번호의 범위를 초과하였습니다.";
+    public final String Number_IS_NOT_SIX_EXCEPTION_MESSAGE = "[ERROR] 번호가 6개가 아닙니다.";
+    public final String DUPLICATE_NUMBER_EXIST_EXCEPTION_MESSAGE = "[ERROR] : 중복된 숫자가 있습니다.";
 
     public static void checkInputIsNum(String input){
         for(int index = 0; index < input.length(); ++index)
@@ -16,5 +22,24 @@ public interface Exception {
     public static void checkPriceCanDivide(int price){
         if(price % 1000 != 0)
             throw new IllegalArgumentException(PRICE_CAN_NOT_DIVIDE_EXCEPTION_MESSAGE);
+    }
+
+    public static void checkWinningNumberRange(int number){
+        if(number < 1 || number > 45)
+            throw new IllegalArgumentException(EXCEED_WINNING_NUMBER_RANGE_EXCEPTION_MESSAGE);
+    }
+
+    public static void checkNumberOfNumbers(List<Integer> numbers){
+        if(numbers.size() != 6)
+            throw new IllegalArgumentException(Number_IS_NOT_SIX_EXCEPTION_MESSAGE);
+    }
+
+    public static void checkDuplicateNumber(List<Integer> numbers){
+        HashSet<Integer> set = new HashSet<>(12);
+        for(Integer number : numbers){
+            if(set.contains(number))
+                throw new IllegalArgumentException(DUPLICATE_NUMBER_EXIST_EXCEPTION_MESSAGE);
+            set.add(number);
+        }
     }
 }
