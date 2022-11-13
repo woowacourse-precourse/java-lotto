ed487a808acffd14922b50923089fae3c5377ddb
ensk26 <tjensk26@gmail.com>
2022-11-14 02:44:22 +0900

    test(LottoNumberTest): 로또 번호 오름차순으로 나오는지 확인하는 테스트 수정
    기존 테스트 수정  to LottoNumberTest
    - List.of ->Arrays.asList 로 변경
    - UnsupportedOperationException 예외가 발생
    
    BREAKING CHANGE: List of는 temp List임으로 값을 추가, 변경 삭제 시 오류가 발생한다. 그래서 new로 생성시키는 list를 이용해야 한다.

--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -3,7 +3,7 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,7 +12,7 @@ class LottoNumberTest {
     @DisplayName("구입한 로또 번호가 오름차순으로 나오는지")
     @Test
     void getLottoNumbers() {
-        LottoNumber lottoNumber = new LottoNumber(List.of(9, 8, 7, 6, 5, 4));
+        LottoNumber lottoNumber = new LottoNumber(Arrays.asList(9, 8, 7, 6, 5, 4));
 
         assertThat(lottoNumber.getNumbers()).containsExactly(4, 5, 6, 7, 8, 9);
     }
