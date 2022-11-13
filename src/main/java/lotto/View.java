7f0dbb3b34a1571fed3d99ade745bb2907ce852e
tjddnr7760 <tjddnr7760@naver.com>
2022-11-13 23:46:23 +0900

    refactor : View 클래스 생성자 추가, 리턴 타입 수정, 함수 분리

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -2,9 +2,14 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class View {
+    View(){
+
+    }
+
     int inputMoneyAmount() {
         System.out.println("구입 금액을 입력해 주세요.");
         int money = Integer.parseInt(Console.readLine());
@@ -13,24 +18,31 @@ public class View {
         return money;
     }
 
-    void printPurchasedLotto(int number, int[][] lottogather) {
+    void printPurchasedLottoCount(int number) {
         System.out.println(number + "개를 구매했습니다.");
-
-        for(int[] data : lottogather) {
-            System.out.println(Arrays.toString(data));
-        }
     }
 
-    int[] inputWinningNum() {
-        String[] winningnum = Console.readLine().split(",");
+    void printPurchasedLotto(List<Integer> lottogather) {
+        System.out.println(lottogather.toString());
+    }
+
+    List<Integer> inputWinningNum() {
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] winningnums = Console.readLine().split(",");
         System.out.println();
 
-        int[] winningnums = new int[winningnum.length];
-        for(int index = 0; index < winningnum.length; index++) {
-            winningnums[index] = Integer.parseInt(winningnum[index]);
+        int[] winnningnumi = new int[winningnums.length];
+        for(int index = 0; index < winningnums.length; index++) {
+            winnningnumi[index] = Integer.parseInt(winningnums[index]);
         }
 
-        return winningnums;
+        List<Integer> result = new ArrayList<>();
+        for(int element : winnningnumi) {
+            result.add(element);
+        }
+
+        return result;
     }
 
     int inputBonusNum() {
