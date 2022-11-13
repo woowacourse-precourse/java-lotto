4d94c77c10c7a7f43fa6fe1d93845939ab48c033
choyeram <choyeram8@gmail.com>
2022-11-13 22:49:30 +0900

    feat: 로또 구매 금액 입력

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,13 +1,20 @@
 package lotto.controller;
 
+import lotto.domain.User;
 import lotto.util.Printer;
 import lotto.view.LottoMessage;
 
 public class LottoController {
     private Printer printer = new Printer();
+    private User user = new User();
 
     public void init() {
         printer.print(LottoMessage.INPUT_AMOUNT.getMessage());
+        sell();
+    }
+
+    private void sell() {
+        int amount = user.buy();
     }
 
 }
---
+++ src/main/java/lotto/domain/User.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class User {
+    private List<Lotto> lottos;
+
+    public int buy() {
+        String amount = Console.readLine();
+        return Integer.parseInt(amount);
+    }
+}
