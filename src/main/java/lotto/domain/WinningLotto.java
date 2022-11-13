70b0618bd612cdc1f35bd7453d2cd23f73a70ce9
정핸모 <gusah009@naver.com>
2022-11-13 22:22:59 +0900

    refactor: 유효성 check 하는 로직 분리

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -15,18 +15,26 @@ public class WinningLotto {
     }
 
     static void validate(Lotto lotto, int bonusNumber) {
+        checkOutOfRange(bonusNumber);
+        checkDuplicate(lotto, bonusNumber);
+    }
+
+    private static void checkOutOfRange(int bonusNumber) {
         if (isInvalidRange(bonusNumber)) {
             String errorMessage = String.format("보너스 숫자는 %d부터 %d 사이의 숫자여야 합니다.", START_LOTTO_NUMBER, END_LOTTO_NUMBER);
-            throw new IllegalArgumentException(errorMessage);
-        }
-        if (isDuplicateNumber(lotto, bonusNumber)) {
-            String errorMessage = String.format("보너스 숫자(%d)가 중복됩니다.", bonusNumber);
             throw new IllegalArgumentException(errorMessage);
         }
     }
 
     private static boolean isInvalidRange(int bonusNumber) {
         return START_LOTTO_NUMBER > bonusNumber || bonusNumber > END_LOTTO_NUMBER;
+    }
+
+    private static void checkDuplicate(Lotto lotto, int bonusNumber) {
+        if (isDuplicateNumber(lotto, bonusNumber)) {
+            String errorMessage = String.format("보너스 숫자(%d)가 중복됩니다.", bonusNumber);
+            throw new IllegalArgumentException(errorMessage);
+        }
     }
 
     private static boolean isDuplicateNumber(Lotto lotto, int bonusNumber) {
