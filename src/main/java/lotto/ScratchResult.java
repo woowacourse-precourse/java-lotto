23a362fc4e9e566ce6b9d8b22299a6929dbf29db
hodomaroo <enghemdwkd@gmail.com>
2022-11-13 14:46:14 +0900

    [Feat] : EarningInfo -> ScratchResult 클래스명 변경
    EarningInfo 클래스명이 총 수익과 수익률, 각 등수 별 당첨 횟수를
    나타내기에는 부적절하다고 판단, 클래스명을 보다 직관적으로
    변경하였습니다.

---
+++ src/main/java/lotto/ScratchResult.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import java.util.Map;
+
+public class ScratchResult {
+    private final Map<LottoGrade, Integer> countOfEachGrade;
+    private final long totalEarning;
+    private final double earningRate;
+
+    public ScratchResult(Map<LottoGrade, Integer> countOfEachGrade, long totalEarning, double earningRate) {
+        this.countOfEachGrade = countOfEachGrade;
+        this.totalEarning = totalEarning;
+        this.earningRate = earningRate;
+    }
+
+    public int getCountOfGrade(LottoGrade grade){
+        return countOfEachGrade.getOrDefault(grade, 0);
+    }
+
+    public long getTotalEarning() {
+        return totalEarning;
+    }
+
+    public double getEarningRate() {
+        return earningRate;
+    }
+}
