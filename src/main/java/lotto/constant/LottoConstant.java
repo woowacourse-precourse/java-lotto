e9dda295e095da9b2e5db527f22a5b769f441dfd
Devtist <yt930310@snu.ac.kr>
2022-11-13 13:34:01 +0900

    feat: 로또를 발행하는 기능 구현

--- src/main/java/lotto/constant/LottoConstant.java
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -2,4 +2,9 @@ package lotto.constant;
 
 public class LottoConstant {
 	public static final int PRICE_OF_LOTTO = 1000;
+
+	public static final int MINIMUM_LOTTO_NUMBER = 1;
+	public static final int MAXIMUM_LOTTO_NUMBER = 45;
+
+	public static final int COUNT_OF_LOTTO_NUMBERS = 6;
 }
---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.constant.LottoConstant;
+
+public class LottoGenerator {
+	private List<Integer> numbers;
+
+	private void generateLotto() {
+		numbers = Randoms.pickUniqueNumbersInRange(LottoConstant.MINIMUM_LOTTO_NUMBER,
+			LottoConstant.MAXIMUM_LOTTO_NUMBER, LottoConstant.COUNT_OF_LOTTO_NUMBERS);
+	}
+
+	public List<Integer> getNumbers() {
+		generateLotto();
+		return numbers;
+	}
+}
