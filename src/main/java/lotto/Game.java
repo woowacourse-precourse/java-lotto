e60da22d3a2a1ea3d25a7b2b25bbf6ea23a7c6fe
YongGwan <32181771@dankook.ac.kr>
2022-11-13 20:24:14 +0900

    feat : 당첨 번호 예외 사항 처리 로직 구현

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -6,12 +6,33 @@ import java.util.List;
 import java.util.Set;
 
 public class Game {
-    
+
+    private static final int lottoLength = 6;
     public void play(Set<List<Integer>> lottoNumbers) {
 
         System.out.println("\n당첨 번호를 입력해주세요");
         String inputValue = Console.readLine();
-        List<String> inputSplitValues = List.of(inputValue.trim().split(","));
-        System.out.println(inputSplitValues);
+        List<String> winningLottoNumber = isValidWinningNumbers(List.of(inputValue.trim().split(",")));
+        System.out.println(winningLottoNumber);
+    }
+
+    private List<String> isValidWinningNumbers (List<String> inputSplitValues) throws IllegalArgumentException {
+
+        if (inputSplitValues.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+
+        for(int index = 0; index < lottoLength; index++) {
+            if(!isNumeric(inputSplitValues.get(index))) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        return inputSplitValues;
+    }
+
+    private boolean isNumeric(String lottoNum) {
+
+        return Integer.parseInt(lottoNum) >= 1 && Integer.parseInt(lottoNum) <= 45;
     }
 }
