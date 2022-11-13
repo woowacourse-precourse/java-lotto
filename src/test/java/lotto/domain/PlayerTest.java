907c396930ed4a29be609d838d274d887a66103b
win9 <krrr8@naver.com>
2022-11-14 03:37:29 +0900

    Test(PrizeTest): 테스팅 분리 후 테스트 완료
    Prize클래스 추가로 PlayerTest에 있던 종속적인 테스트를
    모두 Prize쪽으로 옮기고 테스트를 완료

--- src/test/java/lotto/domain/PlayerTest.java
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -36,97 +36,9 @@ class PlayerTest {
     @Test
     void moneyInputZeroException() {
         ScannerInput("0");
-        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
             player.inputMoney();
         });
-    }
-
-    /**
-     * 당첨번호 입력 테스트
-     */
-    @DisplayName("6자리 이상의 당첨 번호를 입력하면 예외가 발생한다.")
-    @Test
-    void prizeNumbersLengthOutOfBoundInputException() {
-        ScannerInput("1,2,3,4,5,6,7");
-        Assertions.assertThrows(IllegalArgumentException.class, ()->{
-            player.inputPrizeNumbers();
-        });
-    }
-
-    @DisplayName("6자리 이하의 당첨 번호를 입력하면 예외가 발생한다.")
-    @Test
-    void prizeNumbersLessLengthInputException() {
-        ScannerInput("1,2,3,4");
-        Assertions.assertThrows(IllegalArgumentException.class, ()->{
-            player.inputPrizeNumbers();
-        });
-    }
-
-    @DisplayName("쉼표로 구분하지 않으면 예외가 발생한다.")
-    @Test
-    void prizeNumbersNotSplitCommaInputException() {
-        ScannerInput("1234");
-        Assertions.assertThrows(IllegalArgumentException.class, ()->{
-            player.inputPrizeNumbers();
-        });
-    }
-
-    @DisplayName("45가 넘는 숫자를 입력하면 예외가 발생한다.")
-    @Test
-    void prizeNumbersOverRangeInputException() {
-        ScannerInput("1,2,4,5,7,46");
-        Assertions.assertThrows(IllegalArgumentException.class, ()->{
-            player.inputPrizeNumbers();
-        });
-    }
-
-    @DisplayName("1보다 작은 숫자를 입력하면 예외가 발생한다.")
-    @Test
-    void prizeNumbersLessRangeInputException() {
-        ScannerInput("1,2,4,-1,5,4,6");
-        Assertions.assertThrows(IllegalArgumentException.class, ()->{
-            player.inputPrizeNumbers();
-        });
-    }
-
-    @DisplayName("1~45 사이의 6자리 숫자를 쉼표로 구분하여 입력하면 통과한다.")
-    @Test
-    void prizeNumbersInput() {
-        ScannerInput("1,2,4,5,6,7");
-        player.inputPrizeNumbers();
-        List<Integer> prizeNumbers = player.getPrizeNumbers();
-        Assertions.assertEquals(prizeNumbers.size(), 6);
-        Assertions.assertTrue(prizeNumbers.containsAll(List.of(1,2,4,5,6,7)));
-    }
-
-    /**
-     * 보너스 숫자 입력 테스트
-     */
-    @DisplayName("1~45 사이가 아닌 숫자를 입력하면 에러가 발생한다.")
-    @Test
-    void bonusNumbersOverRangeInputException() {
-        ScannerInput("46");
-        Assertions.assertThrows(IllegalArgumentException.class, ()->{
-            player.inputBonusNumber();
-        });
-    }
-
-    @DisplayName("숫자를 입력하지 않으면 오류가 발생한다.")
-    @Test
-    void bonusNumbersNotNumberInputException() {
-        ScannerInput("A");
-        Assertions.assertThrows(IllegalArgumentException.class, ()->{
-            player.inputBonusNumber();
-        });
-    }
-
-    @DisplayName("1~45사이의 올바른 숫자를 입력하면 통과한다.")
-    @Test
-    void bonusNumbersInput() {
-        ScannerInput("5");
-        player.inputBonusNumber();
-        int bonusNumber = player.getBonusNumber();
-        Assertions.assertEquals(bonusNumber, 5);
     }
 
     private void ScannerInput(String systemInput) {
