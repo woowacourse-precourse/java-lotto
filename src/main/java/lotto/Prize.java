55687470bfa3588d240b3fcf586ff9a57b3725a9
Jeangyu <gjwlsrb1700@naver.com>
2022-11-13 17:17:54 +0900

    [FEAT] 당첨 정보를 enum형태로 추가

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,5 +1,24 @@
 package lotto;
 
 enum Prize {
-    FIRST, SECOND, THIRD, FORTH, FIFTH;
+    FIRST(2000000000, 1),
+    SECOND(30000000, 2),
+    THIRD(1500000, 3),
+    FORTH(50000, 4),
+    FIFTH(5000, 5);
+
+    private final int prize;
+    private final int name;
+    Prize(int money, int name){
+        this.prize = money;
+        this.name = name;
+    }
+
+    public int getPrize(){
+        return prize;
+    }
+
+    public int getName(){
+        return name;
+    }
 }
