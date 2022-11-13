f5e1542bb9f08f2986c87de38ec5f1146440beed
Choi-JY <Choi-JY1107@github.com>
2022-11-13 17:33:25 +0900

    feat: 구매한 로또 번호 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,28 @@
 package lotto.view;
 
+import java.util.List;
+
+import lotto.domain.Lotto;
+import lotto.dto.LottoStatsDTO;
+
 public class OutputView {
     private static final String INPUT_PAID_AMOUNT = "구입금액을 입력해 주세요.";
-    private static final String INFO_PAID_LOTTO = "\n%d개를 구매했습니다.";
-    private static final String INPUT_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+    private static final String INFO_LOTTO_COUNT = "\n%d개를 구매했습니다.\n";
+    private static final String INPUT_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.\n";
+    private static final String INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.\n";
 
     public static void printInputAmount() {
         System.out.println(INPUT_PAID_AMOUNT);
+    }
+
+    public static void printPaidLotto(Integer lottoCount, List<Lotto> paidLotto) {
+        System.out.printf(INFO_LOTTO_COUNT, lottoCount);
+        printLottoNumbers(paidLotto);
+    }
+
+    private static void printLottoNumbers(List<Lotto> paidLotto) {
+        for (Lotto lottoNumber: paidLotto) {
+            System.out.println(lottoNumber.getLottoNumbers());
+        }
     }
 }
