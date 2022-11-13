d5b58d266903737494f3ebdc14ac0503599756c0
jagajang <kosb7544@gmail.com>
2022-11-13 22:55:15 +0900

    fix: fix output message

--- src/main/java/lotto/IOManager.java
+++ src/main/java/lotto/IOManager.java
@@ -25,7 +25,7 @@ public class IOManager {
     }
 
     public static void printLottoList(List<Lotto> lottoList) {
-        System.out.println(String.valueOf(lottoList.size()) + IOMessage.LOTTO_BUY_NUM);
+        System.out.println(String.valueOf(lottoList.size()) + IOMessage.LOTTO_BUY_NUM.getMessage());
         for(Lotto lotto : lottoList) {
             printLotto(lotto);
         }
