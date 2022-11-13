5b0f0c9e277145d1a682055e850a36efb0c22d45
whdcks420 <whdcks420@gmail.com>
2022-11-13 22:37:41 +0900

    refactor(validator): 로또 번호 유효성 검사 기능 수정

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -1,2 +1,41 @@
-package lotto.validator;public class LottoValidator {
+package lotto.validator;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static lotto.validator.ExceptionStatus.*;
+
+public class LottoValidator {
+
+    private static final int NUMBER_OF_LOTTO_NUMBERS = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
+    public static void validate(List<Integer> numbers) {
+        validateExactSize(numbers);
+        validateLottoNumberOverlap(numbers);
+        validateCorrectRange(numbers);
+    }
+
+    public static void validateCorrectRange(Integer number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException(NUMBER_RANGE_EXCEPTION.getMessage());
+        }
+    }
+
+    private static void validateCorrectRange(List<Integer> numbers) {
+        numbers.forEach(LottoValidator::validateCorrectRange);
+    }
+
+    private static void validateExactSize(List<Integer> numbers) {
+        if (numbers.size() != NUMBER_OF_LOTTO_NUMBERS) {
+            throw new IllegalArgumentException(NO_OVER_SIX_NUMBERS.getMessage());
+        }
+    }
+
+    private static void validateLottoNumberOverlap(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != NUMBER_OF_LOTTO_NUMBERS) {
+            throw new IllegalArgumentException(NO_DUPLICATE_NUMBER.getMessage());
+        }
+    }
 }
