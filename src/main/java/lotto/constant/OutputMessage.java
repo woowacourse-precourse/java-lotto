0b45f7ce3adcc90c78b2b096e8fa87ad10454754
jiwon <ljwon77@daum.net>
2022-11-13 20:40:46 +0900

    feat: change output message about purchase, bonus result
    구매했을 때 나오는 메시지에 대한 오타 수정 및 보너스 결과에 대한 메시지를 추가하였습니다.

--- src/main/java/lotto/constant/OutputMessage.java
+++ src/main/java/lotto/constant/OutputMessage.java
@@ -2,9 +2,10 @@ package lotto.constant;
 
 public enum OutputMessage {
 
-    PURCHASE_COUNT("개를 구매하였습니다.")
+    PURCHASE_COUNT("개를 구매했습니다.")
     , WINNING_STATS("당첨 통계\n---")
-    , WINNING_STATS_RESULT("%d개 일치 (%d원) - %d개")
+    , WINNING_STATS_RESULT("%d개 일치 (%s원) - %d개\n")
+    , WINNING_STATS_BONUS_RESULT("5개 일치, 보너스 볼 일치 (%s원) - %d개\n")
     , EARNING_RATE("총 수익률은 %.1f%%입니다.")
     ;
 
