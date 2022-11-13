615d17b1646a45e962a981756c8383e48c5d0aa6
Ilchul Kim <huj0159@gmail.com>
2022-11-13 16:24:59 +0900

    test: compare lotto of same number is equal

--- src/test/java/lotto/LottoControllerTest.java
+++ src/test/java/lotto/LottoControllerTest.java
@@ -1,10 +1,11 @@
 package lotto;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoControllerTest {
@@ -29,5 +30,28 @@ public class LottoControllerTest {
     void isInputNumberConsistOfDigit() {
         assertThatThrownBy(() -> lc.readUserMoney("1칠60"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("형식에 맞지 않는 당첨 번호 입력시 예외 발생")
+    @Test
+    void isWinNumberUnvalid() {
+        assertThatThrownBy(() -> lc.readWinNumber("0,1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> lc.readWinNumber("0,1,2,3,4"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("형식에 맞는 번호 입력시 로또가 생성")
+    @Test
+    void isWinNumberValid() {
+        //given
+        Lotto validLotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
+
+        //when
+        Lotto createdLotto = lc.readWinNumber("6,5,4,3,2,1");
+
+        //then
+        assertThat(createdLotto.toString()).isEqualTo(validLotto.toString());
     }
 }
