78d7a781f7a4a9cc8ed258c10195754324f95cbd
hwanee__ <fkfkthth@naver.com>
2022-11-13 19:54:09 +0900

    feat: 잘못된 (,) 기준 예외 처리 구현
    (,)의 개수가 5개가 아닐시 예외처리

--- src/main/java/lotto/domain/WinLotto.java
+++ src/main/java/lotto/domain/WinLotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.Lotto;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class WinLotto {
+    private static final char COMMA = ',';
     private List<Integer> winningNumbers;
 
     public WinLotto() {
@@ -15,11 +17,29 @@ public class WinLotto {
 
     public void setWinningNumbers(){
         String input = Console.readLine();
+        validateComma(input);
+
         this.winningNumbers = Arrays.stream(input.split(","))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+        new Lotto(winningNumbers);
     }
 
+    public void validateComma(String input){
+        if(countComma(input,COMMA)!=5){
+            throw new IllegalArgumentException("[ERROR] 잘못된 (,)기준 입니다.");
+        }
+    }
 
+    public int countComma(String input,char comma){
+        int count = 0;
+
+        for (int i=0;i<input.length();i++){
+            if(input.charAt(i) == comma){
+                count ++;
+            }
+        }
+        return count;
+    }
 
 }
