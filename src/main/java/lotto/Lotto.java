084793806f050b6227d4405e9ad904fad9262439
nn98 <jkllhgb@gmail.com>
2022-11-14 00:25:22 +0900

    Feat: validate 메소드 checker 클래스로 이전

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,15 +7,9 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
 
     public List<Integer> getNumbers() {
         List<Integer> copyNumbers = new ArrayList<>();
