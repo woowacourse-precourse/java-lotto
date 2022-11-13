9d4ebed02801fff4c63a3eaba0e7dda2b768a098
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-13 23:59:52 +0900

    test: 금액(값)을 입력받아 금액에 맞게 로또의 갯수를 생성하는 기능의 테스트케이스 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import user.BuyLotto;
 
 import java.util.List;
 
@@ -36,5 +38,47 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(0,2,3,4,5,45)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+}
+
+class InputTest {
+    @DisplayName("금액에 맞게 로또를 발행해야 한다.")
+    @Test
+    void countLotto1 (){
+        int input = 3000;
+        BuyLotto buyLotto = new BuyLotto();
+        int num = buyLotto.countHowMany(input);
+
+        Assertions.assertThat(num).isEqualTo(3);
+    }
+
+    @DisplayName("1000원 단위로 나누어 떨어지지 않으면 예외로 처리한다.")
+    @Test
+    void countLotto2 (){
+        int input = 3100;
+        BuyLotto buyLotto = new BuyLotto();
+        assertThatThrownBy(() -> buyLotto.countHowMany(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 1000원 단위로 입력해야 합니다.");
+    }
+
+    @DisplayName("값을 잘못 입력하면 IllegalArgumentException를 발생시킨다.")
+    @Test
+    void countLotto3 (){
+        int input = -1000;
+        BuyLotto buyLotto = new BuyLotto();
+        assertThatThrownBy(() -> buyLotto.countHowMany(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 값을 잘못 입력하였습니다.");
+    }
+
+    @DisplayName("값을 잘못 입력하면 IllegalArgumentException를 발생시킨다.")
+    @Test
+    void countLotto4 (){
+        int input = 0;
+        BuyLotto buyLotto = new BuyLotto();
+        assertThatThrownBy(() -> buyLotto.countHowMany(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 값을 잘못 입력하였습니다.");
     }
 }
