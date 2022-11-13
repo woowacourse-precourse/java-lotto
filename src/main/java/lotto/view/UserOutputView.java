d54573f9f018c7ba4b1473330703edbe94684994
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-13 23:05:05 +0900

    feat: 당첨 결과와 통계를 출력하는 기능 추가

--- src/main/java/lotto/view/UserOutputView.java
+++ src/main/java/lotto/view/UserOutputView.java
@@ -1,6 +1,10 @@
 package lotto.view;
 
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import lotto.constant.LottoWinningRanking;
 import lotto.constant.UserOutputMessage;
 
 public class UserOutputView {
@@ -13,4 +17,44 @@ public class UserOutputView {
         System.out.println(lottoNumbers);
     }
 
+    public static void printWinningResult(Map<LottoWinningRanking, Integer> winningResult) {
+        System.out.println(UserOutputMessage.PREFIX_WINNING_RESULT.getMessage());
+        for (LottoWinningRanking ranking : LottoWinningRanking.values()) {
+            if (ranking == LottoWinningRanking.NOTHING) {
+                continue;
+            }
+
+            if (ranking.isMatchesOfBonusNumber()) {
+                printWinningResultWithBonusMatches(ranking, winningResult.get(ranking));
+                continue;
+            }
+            printWinningResult(ranking, winningResult.get(ranking));
+        }
+    }
+
+    public static void printRateOfReturn(double userRateOfReturn) {
+        System.out.printf(UserOutputMessage.RATE_OF_RETURN.getMessage(), userRateOfReturn);
+    }
+
+    private static void printWinningResultWithBonusMatches(LottoWinningRanking winningRanking, int quantity) {
+        System.out.printf(
+                UserOutputMessage.WINNIGN_RESULT_WITH_BONUS_MATCHES.getMessage(),
+                winningRanking.getNumberOfMatches(),
+                integerToStringWithCommas(winningRanking.getWinningAmount()),
+                quantity
+        );
+    }
+
+    private static void printWinningResult(LottoWinningRanking winningRanking, int quantity) {
+        System.out.printf(
+                UserOutputMessage.WINNING_RESULT.getMessage(),
+                winningRanking.getNumberOfMatches(),
+                integerToStringWithCommas(winningRanking.getWinningAmount()),
+                quantity
+        );
+    }
+
+    private static String integerToStringWithCommas(int number) {
+        return NumberFormat.getNumberInstance(Locale.US).format(number);
+    }
 }
