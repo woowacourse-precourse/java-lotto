5abb445198ffb4c5d77a16cc1c6d8231a5ee32c1
JHS <wnghdtjr129@naver.com>
2022-11-13 20:04:42 +0900

    refactor(ProjectView): 메소드 static 제거

--- src/main/java/lotto/view/ProjectView.java
+++ src/main/java/lotto/view/ProjectView.java
@@ -5,33 +5,35 @@ import lotto.domain.IssuedLotto;
 import lotto.domain.WinningResult;
 
 public class ProjectView {
-    public static String askPurchaseMoney(){
+    private static final String NEXT_LINE = "\n";
+
+    public String askPurchaseMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
-    public static void sell(IssuedLotto issuedLotto){
-        System.out.println("\n" + issuedLotto.getTotalCount() + "개를 구매했습니다.");
+    public void sell(IssuedLotto issuedLotto) {
+        System.out.println(NEXT_LINE + issuedLotto.getTotalCount() + "개를 구매했습니다.");
         System.out.println(issuedLotto);
     }
 
-    public static String getWinningNumber() {
-        System.out.println("\n당첨 번호를 입력해 주세요.");
+    public String getWinningNumber() {
+        System.out.println(NEXT_LINE + "당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
-    public static String getBonusNumber() {
-        System.out.println("\n보너스 번호를 입력해 주세요.");
+    public String getBonusNumber() {
+        System.out.println(NEXT_LINE + "보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
-    public static void printResult(WinningResult result, int money) {
-        System.out.println("\n당첨 통계\n---");
+    public void printResult(WinningResult result, int money) {
+        System.out.println(NEXT_LINE + "당첨 통계" + NEXT_LINE + "---");
         System.out.print(result);
         System.out.println("총 수익률은 " + result.getRateOfReturn(money) + "%입니다.");
     }
 
-    public static void printErrorMessage(String message) {
+    public void printErrorMessage(String message) {
         System.out.println(message);
     }
 }
