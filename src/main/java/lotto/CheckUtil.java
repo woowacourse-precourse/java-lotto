ddf998e95047f9fd9c7cbd2dd80cef5174056040
jingyu9804 <swc05161@gmail.com>
2022-11-14 00:35:40 +0900

    feat(lotto) : 중복 체크 기능 추가

---
+++ src/main/java/lotto/CheckUtil.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import static lotto.model.ErrorMessage.*;
+import static lotto.model.GameNumber.THOUSAND;
+
+public class CheckUtil {
+
+    public static int checkIsString(String input) throws IllegalArgumentException{
+        try {
+            return Integer.valueOf(input);
+        }
+        catch (NumberFormatException e){
+            throw new IllegalArgumentException(NOT_STRING_ERROR_MESSAGE.getMessage());
+        }
+    }
+    public static void checkIsDivedByTHOUSAND(int price) throws IllegalArgumentException{
+        int divided = Math.floorMod(price,THOUSAND.getNumber());
+        if(divided!=0){
+            throw new IllegalArgumentException(NOT_DIVIDED_BY_THOUSAND_ERROR_MESSAGE.getMessage());
+        }
+    }
+}
