a54054f1baf00633221654a8b212edaf362078c1
Kid-Chang <pch3310@gmail.com>
2022-11-14 03:20:45 +0900

    feat(LottoGenerator): 금액 만큼 로또번호를 만들어주는 메서드 생성

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
@@ -8,5 +9,17 @@ public class LottoGenerator {
     // 한장의 로또를 만들어주는 메서드
     public static List<Integer> createLottoNumber() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    // 금액만큼 로또번호를 만들어주는 메서드
+    public List<List<Integer>> createLottos(int receivedMoney) {
+        int ticketCount = receivedMoney / 1000;
+        List<List<Integer>> lottoTicket = new ArrayList<>();
+
+        while (ticketCount != 0) {
+            lottoTicket.add(createLottoNumber());
+            ticketCount--;
+        }
+        return lottoTicket;
     }
 }
