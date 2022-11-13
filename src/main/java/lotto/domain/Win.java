c8fd7d7eaa8b9a745ec01081430b9f9c2876dba0
Jzakka <mouse4786@gmail.com>
2022-11-14 00:40:29 +0900

    refactor: 열거형 필드 변경
    필터링을 단순화 하기 위해 열거형 상수와 필드 및 메서드를 변경

--- src/main/java/lotto/domain/Win.java
+++ src/main/java/lotto/domain/Win.java
@@ -6,26 +6,35 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public enum Win {
-    NON(0), ONE(0), TWO(0),
-    THREE(5_000),
-    FOUR(50_000),
-    FIVE(1_500_000),
-    SIX(2_000_000_000),
-    FIVE_BONUS(30_000_000);
+    NONE(0, -1, true, -1),
+    FIFTH(5_000, 3, true, 5),
+    FIFTH_WITHOUT_BONUS(5_000, 3, false, 5),
+    FOURTH(50_000, 4, true, 4),
+    FOURTH_WITHOUT_BONUS(50_000, 4, false, 4),
+    THIRD(1_500_000, 5, false, 3),
+    SECOND(30_000_000, 5, true, 2),
+    FIRST(2_000_000_000, 6, true, 1),
+    FIRST_WITHOUT_BONUS(2_000_000_000, 6, false, 1);
 
     private final Integer PRIZE;
+    private final Integer MATCH;
+    private final boolean BONUS;
+    private final Integer RANK;
     private Integer count = 0;
 
-    Win(Integer PRIZE) {
+    Win(Integer PRIZE, Integer MATCH, boolean BONUS, Integer RANK) {
         this.PRIZE = PRIZE;
+        this.MATCH = MATCH;
+        this.BONUS = BONUS;
+        this.RANK = RANK;
     }
 
     private void setCount(Integer count) {
         this.count = count;
     }
 
-    public Integer getCount() {
-        return count;
+    public Integer getRANK() {
+        return RANK;
     }
 
     public static void initializeCount(){
@@ -35,9 +44,9 @@ public enum Win {
 
     private static Win getWin(int matchCount, boolean bonus){
         return Arrays.stream(Win.values())
-                .filter(win -> !(matchCount == 5 && bonus) && win.ordinal() == matchCount)
+                .filter(win -> win.MATCH == matchCount && win.BONUS == bonus)
                 .findAny()
-                .orElse(FIVE_BONUS);
+                .orElse(NONE);
     }
 
     public static Integer getPRIZE(int matchCount, boolean bonus) {
@@ -62,11 +71,11 @@ public enum Win {
 
     private static void writeTable(List<Integer> winHistory) {
         List<Win> history = Arrays.stream(Win.values()).collect(Collectors.toList());
-        winHistory.add(0);                      // 0등 - 더미데이터
-        winHistory.add(history.get(6).count);   // 1등
-        winHistory.add(history.get(7).count);   // 2등
-        winHistory.add(history.get(5).count);   // 3등
-        winHistory.add(history.get(4).count);   // 4등
-        winHistory.add(history.get(3).count);   // 5등
+        winHistory.add(0);                                              // 0등 - 더미데이터
+        winHistory.add(history.get(8).count + history.get(7).count);    // 1등
+        winHistory.add(history.get(6).count);                           // 2등
+        winHistory.add(history.get(5).count);                           // 3등
+        winHistory.add(history.get(4).count + history.get(3).count);    // 4등
+        winHistory.add(history.get(2).count + history.get(1).count);    // 5등
     }
 }
