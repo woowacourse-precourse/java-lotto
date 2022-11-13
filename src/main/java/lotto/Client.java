2c6c0e566b0ad28c255e4b01f626cd70e2dbbf20
JerryK026 <kskyung0624@gmail.com>
2022-11-13 13:37:05 +0900

    refactor(Client) : buy 메서드
    buy 내부의 로직을 parseStringToInt(),parseStringToInt()를 활용해 분리한다

--- src/main/java/lotto/Client.java
+++ src/main/java/lotto/Client.java
@@ -11,17 +11,26 @@ public class Client {
     public int buy() {
         String moneyAmountString = clientInput.get();
 
+        int moneyAmount = parseStringToInt(moneyAmountString);
+
+        ThousandMultipleOrElseThrow(moneyAmount);
+
+        return moneyAmount;
+    }
+
+    private int parseStringToInt(String moneyAmountString) {
         int moneyAmount;
         try {
             moneyAmount = Integer.parseInt(moneyAmountString);
         } catch (Exception e) {
             throw new IllegalArgumentException(e);
         }
+        return moneyAmount;
+    }
 
+    private void ThousandMultipleOrElseThrow(int moneyAmount) {
         if (moneyAmount % 1000 != 0) {
             throw new IllegalArgumentException();
         }
-
-        return moneyAmount;
     }
 }
