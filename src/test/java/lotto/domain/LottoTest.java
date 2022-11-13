88f1feae20392822e0a312725cd1a73d2ba75109
hossang <zkzk104@naver.com>
2022-11-14 00:32:11 +0900

    test: LottoTest 추가하기

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,11 +1,17 @@
 package lotto.domain;
 
 import lotto.domain.Lotto;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -19,10 +25,33 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    void 로또_번호_문자열로_입력() {
+        Lotto lotto = new Lotto("1,2,3,4,5,6");
+        assertThat(lotto.getNumbers()).contains(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    void 로또_번호에_문자_존재() {
+        assertThatThrownBy(() -> new Lotto("1,2,3,4,5,a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호_중복_문자열로_입력() {
+        Lotto lotto = new Lotto("1,2,3,4,5,6,6");
+        assertThat(lotto.getNumbers()).contains(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    @Disabled
+    void list_of_정렬하기 () {
+        SortedSet<Integer> treeSet = new TreeSet<>(List.of(3, 2, 1, 5, 4, 5, 5));
+        assertThat(treeSet).isEqualTo(List.of(1, 2, 3, 4, 5));
+
+    }
 }
