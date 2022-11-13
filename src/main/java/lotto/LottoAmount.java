961dd1ea85960b2d878426b924148bbdb2d85c17
은지 <midzy0228@gmail.com>
2022-11-13 22:18:02 +0900

    feat(구입 금액): 1000원 이상인지 확인 기능 구현

--- src/main/java/lotto/LottoAmount.java
+++ src/main/java/lotto/LottoAmount.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class LottoAmount {
 
+    private final int MIN_MONEY = 1_000;
+    private final int MAX_MONEY = 100_000;
+    private final int LOTTO_TICKET_PRICE = 1000;
     private int purchaseAmount;
     private int lottoTickets;
 
@@ -23,23 +26,29 @@ public class LottoAmount {
     }
 
     private void validate(int number) {
+        if (! validMinRange(number)) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 최소 1,000원 입니다.");
+        }
         if (! validUnit(number)) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위의 숫자를 입력하셔야 합니다.");
         }
-        if (! validRange(number)) {
+        if (! validMaxRange(number)) {
             throw new IllegalArgumentException("[ERROR] 로또는 최대 100,000원까지 구매하실 수 있습니다.");
         }
     }
 
     private int exchangeTicket(int number) {
-        return number / 1000;
+        return number / LOTTO_TICKET_PRICE;
     }
 
     private boolean validUnit(int number) {
-        return number % 1000 == 0;
+        return number % LOTTO_TICKET_PRICE == 0;
     }
 
-    private boolean validRange(int number) {
-        return 1000 <= number && number <= 100000;
+    private boolean validMaxRange(int number) {
+        return number <= MAX_MONEY;
+    }
+    private boolean validMinRange(int number) {
+        return MIN_MONEY <= number;
     }
 }
