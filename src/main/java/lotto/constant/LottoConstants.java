9fa13617c3ac06b4a85bb07113a86c8e0ec6ebda
situodys <situodys@gmail.com>
2022-11-13 18:48:40 +0900

    chore(LottoConstants): 오탈자 수정

--- src/main/java/lotto/constant/LottoConstants.java
+++ src/main/java/lotto/constant/LottoConstants.java
@@ -3,7 +3,7 @@ package lotto.constant;
 public class LottoConstants {
     public static final String INVALID_LOTTO_SIZE_MSG = "로또 번호 개수가 잘못되었습니다.";
     public static final String DUPLICATE_NUMBER_EXIST_MSG = "로또 내부 중복된 값이 존재합니다.";
-    public static final String INVALID_RANGED_LOTTO_MSG = "잘못된 범위의 로또 번호가 존재합ㄴ디ㅏ";
+    public static final String INVALID_RANGED_LOTTO_MSG = "잘못된 범위의 로또 번호가 존재합니다.";
 
 
     public static final int LOTTO_SIZE = 6;
