879eaa293aec48b99e3d23d7dbd81ab65d58ca9a
wonwoo6y <wonwoo6y@gmail.com>
2022-11-14 02:21:27 +0900

    test: 구입금액에 따른 로또 수량 테스트 코드 작성

---
+++ src/test/java/function/lottoTicketsTest.java
@@ -0,0 +1,17 @@
+package function;
+
+import java.io.ByteArrayInputStream;
+import lotto.Ui;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class lottoTicketsTest {
+    @DisplayName("정상적인 입력")
+    @Test
+    void correctInput() {
+        int purchaseAmount = 5000;
+        assertThat(Ui.getLottoTickets(5000)).isEqualTo(5);
+    }
+}
