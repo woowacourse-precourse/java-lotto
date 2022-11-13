7ae1dcf02f0edf3fc3130e29fd685320d3827d25
dong young <dong5854@gmail.com>
2022-11-14 04:49:52 +0900

    test: 보너스 번호를 포함하는 로또 당첨 번호의 개수를 확인하는 matchWithListIncludeBonus 함수 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -27,15 +27,30 @@ class LottoTest {
     @DisplayName("복수의 로또의 당첨 개수를 확인한다.")
     @Test
     void LottoMatchList() {
-        List<Integer> winNumbers = List.of(1,2,3,4,5,6);
+        List<Integer> winNumbers = List.of(1, 2, 3, 4, 5, 6);
         Lotto winLotto = new Lotto(winNumbers);
         winLotto.setBonusNumber(45);
 
-        Lotto lotto1 = new Lotto(List.of(1,7,8,9,10,11));
-        Lotto lotto2 = new Lotto(List.of(1,2,3,14,15,16));
+        Lotto lotto1 = new Lotto(List.of(1, 7, 8, 9, 10, 11));
+        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 14, 15, 16));
         List<Lotto> boughtLottos = List.of(lotto1, lotto2);
 
         assertThat(winLotto.matchWithList(boughtLottos, 1)).isEqualTo(1);
         assertThat(winLotto.matchWithList(boughtLottos, 3)).isEqualTo(1);
+    }
+
+    @DisplayName("보너스 번호 포함하는 복수의 로또 당첨 개수를 확인한다.")
+    @Test
+    void LottoMatchWihListIncludeBonus() {
+        List<Integer> winNumbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto winLotto = new Lotto(winNumbers);
+        winLotto.setBonusNumber(7);
+
+        Lotto lotto1 = new Lotto(List.of(1, 7, 11, 10, 8, 9));
+        Lotto lotto2 = new Lotto(List.of(1, 2, 8, 16, 15, 7));
+        List<Lotto> boughtLottos = List.of(lotto1, lotto2);
+
+        assertThat(winLotto.matchWihListIncludeBonus(boughtLottos, 1)).isEqualTo(1);
+        assertThat(winLotto.matchWihListIncludeBonus(boughtLottos, 2)).isEqualTo(1);
     }
 }
