fad62f6d9c868bd50aaa539edefab8c6c2774d3d
wath1457 <wath1457@gmail.com>
2022-11-13 23:44:08 +0900

    feat(Prize): 당첨정보 enum 클래스 생성, 당첨 정보 반환 추가
    Prize enum 클래스
    - 당첨금액 접근자 추가
    - 당첨번호 일치 개수에 따른 당첨정보 반환

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -3,16 +3,19 @@ package lotto.domain;
 import java.util.stream.Stream;
 
 public enum Prize {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000),
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
     NONE(-1, 0);
 
     private final int matchingCount;
     private final int prizeMoney;
 
+    public int getPrizeMoney() {
+        return this.prizeMoney;
+    }
 
     Prize(int matchingCount, int prizeMoney) {
         this.matchingCount = matchingCount;
@@ -20,7 +23,7 @@ public enum Prize {
     }
 
     public static Prize getPrize(int lottoMatchCount, boolean bonusMatching) {
-        if (lottoMatchCount == THIRD.matchingCount && bonusMatching) {
+        if (lottoMatchCount == THIRD.matchingCount && !bonusMatching) {
             return THIRD;
         }
         return Stream.of(values())
