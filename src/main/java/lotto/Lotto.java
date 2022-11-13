217f07d6cca94c6df72e1b531f426e5ebad2eace
Jihoon9809 <dkdb521@naver.com>
2022-11-13 20:19:22 +0900

    로또 클래스 중복숫자 예외처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -8,14 +10,19 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        checkOverlap(numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR]");
         }
     }
 
+    private void checkOverlap(List<Integer> numbers){
+        Set<Integer> checkNumbers = new HashSet<Integer>(numbers);
+        if(checkNumbers.size() != numbers.size()) throw new IllegalArgumentException("[ERROR]");
+    }
 
     // TODO: 추가 기능 구현
 }
