0b335be7bde9f7b11b8e5eefbcaa283ddbf12946
alpacaHyun <flaehdan@gmail.com>
2022-11-13 21:36:11 +0900

    type : feat ✨ 로또 몇 개를 샀는지 확인
    body :
    
    footer :

---
+++ src/main/java/lotto/LottoInput.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class LottoInput {
+    static final String BOUGHT_LOTTO = "개를 구매했습니다.";
+    public static int inputMoney; // 구입금액
+    public static int numberOfLotto;
+
+    // 구입금액 입력
+    public static List<Integer> inputNumber(){
+        System.out.println("구입 금액을 입력하세요.");
+        String inputNum = Console.readLine();
+        Validator.validateInteger(inputNum); // 정수만 입력되었는지 확인
+        inputMoney = Integer.parseInt(inputNum);
+        Validator.validateInputMoney(inputMoney); // 1000원 단위로 구입했는지 확인
+        Validator.validateNegativePrice(inputMoney); // 음수로 입력했는지 확인
+
+        return inputNumber();
+    }
+
+    // 구입한 로또 개수
+    public static int theNumberOfLotto(){
+        numberOfLotto = inputMoney / 1000;
+        System.out.println(numberOfLotto + BOUGHT_LOTTO);
+
+        return numberOfLotto;
+    }
+}
