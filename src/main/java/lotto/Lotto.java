7034f5df1d50d653f353dc8ce47eac2fa67b2e88
NoseSSG <dmlcks1998@naver.com>
2022-11-13 23:36:46 +0900

    [REFACTOR] 줄나눔 정리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -19,9 +19,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+        if (numbers.size() != 6) throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
     }
 
     // TODO: 추가 기능 구현
@@ -33,18 +31,14 @@ public class Lotto {
             temp = numbers.get(i);
             number.add(temp);
         }
-        if (number.size() !=6 ){
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복 될 수 없습니다.");
-        }
+        if (number.size() !=6 ) throw new IllegalArgumentException("[ERROR] 로또 번호는 중복 될 수 없습니다.");
     }
 
     private void overcheck(List<Integer> numbers){
         int temp;
         for(int i =0;i < numbers.size();i++){
             temp = numbers.get(i);
-            if(temp>45 || temp <1) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-            }
+            if(temp>45 || temp <1) throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
