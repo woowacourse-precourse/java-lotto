c26718614061f94144521eb63ed53949e331f2cc
keon0711 <fnc6488@gmail.com>
2022-11-14 03:58:35 +0900

    refactor(lotto): add function to check 2nd or 3rd place

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -7,27 +7,39 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public enum Ranking {
-    FIRST_PLACE(2_000_000_000, (matchNumbers, bonusNumber) -> matchNumbers == 6),
-    SECOND_PLACE(30_000_000, (matchNumbers, bonusNumber) -> matchNumbers == 5 && bonusNumber),
-    THIRD_PLACE(1_500_000, (matchNumbers, bonusNumber) -> matchNumbers == 5 && !bonusNumber),
-    FOURTH_PLACE(50_000, (matchNumbers, bonusNumber) -> matchNumbers == 4),
-    FIFTH_PLACE(5_000, (matchNumbers, bonusNumber) -> matchNumbers == 3),
-    NOT_RANKED(0, (matchNumbers, bonusNumber) -> matchNumbers < 6);
+    FIRST_PLACE(2_000_000_000, 6),
+    SECOND_PLACE(30_000_000, 5),
+    THIRD_PLACE(1_500_000, 5),
+    FOURTH_PLACE(50_000, 4),
+    FIFTH_PLACE(5_000, 3),
+    NOT_RANKED(0, 0);
 
     private final int reward;
-    private final BiPredicate<Integer, Boolean> rankCondition;
+    private final int numberOfMatchNumbers;
 
-    Ranking(int reward, BiPredicate<Integer, Boolean> rankCondition) {
+    Ranking(int reward, int numberOfMatchNumbers) {
         this.reward = reward;
-        this.rankCondition = rankCondition;
+        this.numberOfMatchNumbers = numberOfMatchNumbers;
     }
 
     public static Ranking getRank(int numberOfMatchNumbers, boolean bonusNumberMatch) {
+        if (numberOfMatchNumbers == 5) {
+            return checkBonusNumber(bonusNumberMatch);
+        }
+
         return Arrays.stream(Ranking.values())
-                .filter(rank -> rank.rankCondition.test(numberOfMatchNumbers, bonusNumberMatch))
+                .filter(ranking -> ranking.numberOfMatchNumbers == numberOfMatchNumbers)
                 .findAny()
                 .orElse(NOT_RANKED);
     }
+
+    private static Ranking checkBonusNumber(boolean bonusNumberMatch) {
+        if (bonusNumberMatch) {
+            return SECOND_PLACE;
+        }
+        return THIRD_PLACE;
+    }
+
 
     public int getReward() {
         return reward;
