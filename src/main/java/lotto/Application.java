9bff0fad42a9d3f834510e610efbfe50e2bad7f7
Sangkyun Seo <tkdrbs0802@naver.com>
2022-11-13 17:08:29 +0900

    feat: 구입금액 입력 기능 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
+    static int pay = 0;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        buyLotto();
+        //List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
+    public static void buyLotto(){
+        System.out.println("구입금액을 입력해 주세요.");
+        pay = Integer.parseInt(Console.readLine());
+    }
+
 }
