1f789a05265e586622f511c835a1bc8d68d0896e
win9 <krrr8@naver.com>
2022-11-14 04:25:17 +0900

    Fix(Player): 오류를 출력하지 못하는 오류 수정
    getText()를 통하여 오류내용을 콘솔에 정확히 출력하도록 한다.

--- src/main/java/lotto/valid/Validation.java
+++ src/main/java/lotto/valid/Validation.java
@@ -9,25 +9,25 @@ public class Validation {
 
     public static void purchaseNumberValid(int number){
         if (number % 1000 != 0 || number == 0) {
-            throw new IllegalArgumentException(String.valueOf(ErrorType.INVALID_PRICE_INPUT));
+            throw new IllegalArgumentException(ErrorType.INVALID_PRICE_INPUT.getText());
         }
     }
 
     public static void isGeneratedNumberValid(List<Integer> numbers){
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException(String.valueOf(ErrorType.INVALID_NUMBER_DIGITS));
+            throw new IllegalArgumentException(ErrorType.INVALID_NUMBER_DIGITS.getText());
         }
     }
 
     public static void prizeNumbersValid(List<Integer> prizeNumbers){
         if (prizeNumbers.size() != 6) {
-            throw new IllegalArgumentException(String.valueOf(ErrorType.INVALID_NUMBER_DIGITS));
+            throw new IllegalArgumentException(ErrorType.INVALID_NUMBER_DIGITS.getText());
         }
 
         for (int splitNum : prizeNumbers) {
             if (splitNum < NumberBoundaryType.MINIMUM_INDEX.getBoundary()
                     || splitNum > NumberBoundaryType.MAXIMUM_INDEX.getBoundary()) {
-                throw new IllegalArgumentException(String.valueOf(ErrorType.INVALID_NUMBER_RANGE));
+                throw new IllegalArgumentException(ErrorType.INVALID_NUMBER_RANGE.getText());
             }
         }
     }
@@ -35,11 +35,11 @@ public class Validation {
     public static void bonusNumberValid(String bonus) {
         if (Integer.parseInt(bonus) < NumberBoundaryType.MINIMUM_INDEX.getBoundary()
                 || Integer.parseInt(bonus) > NumberBoundaryType.MAXIMUM_INDEX.getBoundary()) {
-            throw new IllegalArgumentException(String.valueOf(ErrorType.INVALID_NUMBER_RANGE));
+            throw new IllegalArgumentException(ErrorType.INVALID_NUMBER_RANGE.getText());
         }
 
         if (bonus.equals("[^a-zA-Z]")){
-            throw new IllegalArgumentException(String.valueOf(ErrorType.INVALID_INPUT_TYPE));
+            throw new IllegalArgumentException(ErrorType.INVALID_INPUT_TYPE.getText());
         }
 
     }
