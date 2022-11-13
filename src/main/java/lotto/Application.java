f3eb42ec5b8eedeb5fadc2c7f2f43e036af513b2
YongGwan <32181771@dankook.ac.kr>
2022-11-13 17:30:47 +0900

    feat : 구매 금액 입력, 로또 개수 생성 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
+
         // TODO: 프로그램 구현
+
+
+        System.out.println("구매 금액을 입력해주세요");
+        int purchaseAmount = Integer.parseInt(Console.readLine());
+        Game game = new Game();
+        game.play(purchaseAmount/1000);
     }
+
 }
---
+++ src/main/java/lotto/Game.java
@@ -0,0 +1,11 @@
+package lotto;
+
+
+public class Game {
+
+    public void play(int lottoCount) {
+        System.out.println("게임 시작~");
+        System.out.println(lottoCount);
+
+    }
+}
