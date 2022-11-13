5b2597be3e5d4b83203c1d1a8a41ea075cfa04c4
yoon <beatus96@naver.com>
2022-11-14 04:08:21 +0900

    feat(Rank) : 로또의 당첨등수와 enum의 상수가 일치하는지 확인 기능 추가
    로또의 당첨 등수와 일치하는 enum값을 얻기위해 만들었다.
    
    사용 방식 :
    1. enum의 상수를 순회하면서,
    2. 로또의 당첨 번호 개수, 보너스 번호 여부가
    3. enum의 상수의 값과 일치하는지 확인한다.
    4. 일치하면, 해당 enum(=등수)를 로또에 부여한다.

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+public enum Rank {
+    FIRST(6,false,2_000_000_000, "6개 일치 (2,000,000,000원) - ") ,
+    SECOND(5,true,30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(5,false,1_500_000, "5개 일치 (1,500,000원) - "),
+    FOURTH(4,false,50_000, "4개 일치 (50,000원) - "),
+    FIFTH(3,false,5_000, "3개 일치 (5,000원) - ");
+
+    private final int matchCount;
+    private final boolean hasBonusNumber;
+    private final int prize;
+    private final String sentence;
+
+    Rank(int matchCount, boolean hasBonusNumber, int prize, String sentence) {
+        this.matchCount = matchCount;
+        this.hasBonusNumber = hasBonusNumber;
+        this.prize = prize;
+        this.sentence = sentence;
+    }
+
+    public int getPrize() {
+        return this.prize;
+    }
+
+    public boolean hasSameRankBy(int matchCount, boolean hasBonusNumber) {
+        return this.matchCount == matchCount && this.hasBonusNumber == hasBonusNumber;
+    }
+
+    @Override
+    public String toString() {
+        return this.sentence;
+    }
+}
