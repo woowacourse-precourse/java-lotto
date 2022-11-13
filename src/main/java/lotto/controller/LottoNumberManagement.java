3575afd0e8126c68c2f77ba5ce863cf24744913d
HamSeungChan <head0618@naver.com>
2022-11-13 10:17:49 +0900

    "feat : 보너스 번호 예외 검사 기능 구현"

--- src/main/java/lotto/controller/LottoNumberManagement.java
+++ src/main/java/lotto/controller/LottoNumberManagement.java
@@ -12,6 +12,8 @@ public class LottoNumberManagement {
   private final int LOTTO_NUMBERS_SIZE = 6;
 
   private List<Integer> numbers;
+  private List<Integer> winningNumbers;
+  private int bonusNumber;
 
   public LottoNumberManagement() {
   }
@@ -20,9 +22,25 @@ public class LottoNumberManagement {
     this.numbers = Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, LOTTO_NUMBERS_SIZE);
   }
 
+  public void setWinningNumbers(List<Integer> winningNumbers) {
+    validate(winningNumbers);
+    this.winningNumbers = winningNumbers;
+  }
+
+  public void setBonusNumber(int bonusNumber) {
+    validateBonusNumber(bonusNumber);
+    this.bonusNumber = bonusNumber;
+  }
+
+
   private void validate(List<Integer> numbers) {
     checkNumbersRange(numbers);
     checkDuplicateNumbers(numbers);
+  }
+
+  private void validateBonusNumber(int bonusNumber) {
+    checkBonusNumbersRange(bonusNumber);
+    checkDuplicateBonusNumber(winningNumbers, bonusNumber);
   }
 
   private void checkNumbersRange(List<Integer> numbers) {
@@ -36,6 +54,18 @@ public class LottoNumberManagement {
   private void checkDuplicateNumbers(List<Integer> numbers) {
     if (numbers.size() != numbers.stream().distinct().count()) {
       throw new IllegalArgumentException("[ERROR] 중복된 번호를 사용하지 않아야 합니다.");
+    }
+  }
+
+  private void checkBonusNumbersRange(int number) {
+    if (LOTTO_START_NUMBER > number || number > LOTTO_END_NUMBER) {
+      throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+  }
+
+  private void checkDuplicateBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
+    if (winningNumbers.contains(bonusNumber)) {
+      throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호와 겹치지 않아야 합니다");
     }
   }
 
