fd0b9dfd3a5de75e746d62223059dadb4322d998
Jeonwoojae <dnwo0719@icloud.com>
2022-11-13 23:55:50 +0900

    feat:구입 금액을 입력 받아 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void validate(int value){
+        if (value % 1000 !=0) {
+            System.out.println("[ERROR] 구입 금액은 1000원 단위입니다.");
+            throw new IllegalArgumentException();
+        }
     }
+    public static void main(String[] args) {
+        System.out.println("구입금액을 입력해 주세요.");
+        int buyingValue = Integer.parseInt(Console.readLine());
+        validate(buyingValue);
+   }
 }
