acef1f4b6df81952dbbfd44a4854131178dee716
cookienc <kim76744@naver.com>
2022-11-14 00:18:05 +0900

    feat(controller, service): 당첨 번호와 보너스 번호 입력을 받아 Result를 출력하는 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,6 +2,7 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.model.Lottos;
+import lotto.model.Result;
 import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -11,9 +12,30 @@ public class LottoController {
     private static LottoService lottoService = new LottoService();
 
     public static void run() {
+        inputMoneyAmount();
+        Result result = inputWinningNumber();
+    }
+
+    private static void inputMoneyAmount() {
         InputView.printInputMoneyMessage();
         String input = Console.readLine();
         Lottos lottos = lottoService.purchaseLottos(input);
         OutputView.printLottosHistory(lottos);
+    }
+
+    private static Result inputWinningNumber() {
+        String winningNumbers = inputWinningNumbers();
+        String bonusNumber = inputBonusNumber();
+        return lottoService.saveResult(winningNumbers, bonusNumber);
+    }
+
+    private static String inputWinningNumbers() {
+        InputView.printInputWinningNumbers();
+        return Console.readLine();
+    }
+
+    private static String inputBonusNumber() {
+        InputView.printInputBonusNumber();
+        return Console.readLine();
     }
 }
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,9 +1,36 @@
 package lotto.service;
 
 import lotto.model.Lottos;
+import lotto.model.Result;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class LottoService {
     public Lottos purchaseLottos(String money) {
-        return Lottos.purchaseLottos(Integer.parseInt(money));
+        return Lottos.purchaseLottos(stringToInt(money));
+    }
+
+    public Result saveResult(String winningNumbers, String bonusNumber) {
+        List<Integer> winnings = convertToList(winningNumbers);
+        int bonus = stringToInt(bonusNumber);
+
+        return Result.of(winnings, bonus);
+    }
+
+    private static List<Integer> convertToList(String winningNumbers) {
+        return Arrays.stream(splitNumbers(winningNumbers))
+                .map(LottoService::stringToInt)
+                .collect(toList());
+    }
+
+    private static String[] splitNumbers(String winningNumbers) {
+        return winningNumbers.split(",");
+    }
+
+    private static int stringToInt(String number) {
+        return Integer.parseInt(number);
     }
 }
