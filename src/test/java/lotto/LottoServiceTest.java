0dc6531c13845400aca55d7700a14d56501abacd
Qulip <ilkwon.ryu@gmail.com>
2022-11-14 01:13:24 +0900

    test: 숫자가 아닌 입력을 받은 경우 테스트 추가

---
+++ src/test/java/lotto/LottoServiceTest.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class LottoServiceTest {
+    @Nested
+    @DisplayName("입력값 유효성 검증 테스트")
+    class InputValueTest {
+        @Test
+        @DisplayName("[실패] - 숫자가 아닌 입력을 받은 경우")
+        void notNumberInput() {
+            // given
+            final String input = "INVALID";
+
+            // when
+
+            // then
+            assertThrows(IllegalStateException.class, () -> LottoService.amountInputToInteger(input));
+        }
+    }
+}
