0cf9b4867bce6c49660fc299792bd314c6c04263
akskflwn <akskflwn@gmail.com>
2022-11-14 03:38:39 +0900

    refactor : 당첨 내역을 출력하는 기능 수정 문자열 상수화,출력 로직 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,41 +4,54 @@ import lotto.domain.Lotto;
 import lotto.domain.Lottos;
 import lotto.domain.Rank;
 import lotto.domain.Result;
+import lotto.domain.Yield;
 
 public class OutputView {
+    private static final String PURCHASE_INFO = "%d개를 구매했습니다.\n";
+    private static final String LOTTO_START_SURFACE = "[";
+    private static final String LOTTO_END_SURFACE = "]";
+    private static final String LOTTO_DELIMITER = ", ";
+    private static final int LOTTO_DELETE_INDEX = 2;
+    private static final String LOTTO_RESULT_INFO = "%d개 일치 (%s원) - %d개\n";
+    private static final String LOTTO_RESULT_SECOND_INFO = "%d개 일치, 보너스 볼 일치 (%s원) - %d개\n";
     private static final String PROFIT_MESSAGE = "총 수익률은 %.1f%%입니다.";
 
-    public static void printLottos(Lottos lottos, int lottoCnt) {
-        System.out.printf(lottoCnt + "개를 구매했습니다.\n");
+    public static void printLottos(Lottos lottos) {
+        System.out.printf(PURCHASE_INFO,lottos.get().size());
         for (Lotto lotto : lottos.get()) {
             printLotto(lotto);
         }
     }
 
     private static void printLotto(Lotto lotto) {
-        StringBuilder sb = new StringBuilder("[");
+        StringBuilder sb = new StringBuilder(LOTTO_START_SURFACE);
         for (int number : lotto.get()) {
-            sb.append(number + ", ");
+            sb.append(number + LOTTO_DELIMITER);
         }
-        sb.delete(sb.length() - 2, sb.length()).append("]");
+        sb.delete(sb.length() - LOTTO_DELETE_INDEX, sb.length()).append(LOTTO_END_SURFACE);
         System.out.println(sb);
     }
 
     public static void printResult(Result result) {
         System.out.println("당첨 통계\n---");
-        System.out.printf("3개 일치 (5,000원) - %d개\n", result.getRankCount(Rank.FIFTH));
-        System.out.printf("4개 일치 (50,000원) - %d개\n", result.getRankCount(Rank.FOURTH));
-        System.out.printf("5개 일치 (1,500,000원) - %d개\n", result.getRankCount(Rank.THIRD));
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n",
-            result.getRankCount(Rank.SECOND));
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", result.getRankCount(Rank.FIRST));
+        for(Rank rank : Rank.values()){
+            printRank(rank,result.getRankCount(rank));
+        }
     }
 
-    public static void printProfit(float prize, float money) {
-        System.out.printf(PROFIT_MESSAGE, (prize/money) * 100);
+    private static void printRank(Rank rank, int rankCount) {
+        if(rank.equals(Rank.NOTHING)){
+            return;
+        }
+        if(rank.equals(Rank.SECOND)) {
+            System.out.printf(LOTTO_RESULT_SECOND_INFO,rank.getCount(),rank.getPrize(),rankCount);
+            return;
+        }
+        System.out.printf(LOTTO_RESULT_INFO,rank.getCount(),rank.getPrize(),rankCount);
+
     }
 
-    public static void printYield(double yield) {
-        System.out.printf(PROFIT_MESSAGE,yield);
+    public static void printYield(Yield yield) {
+        System.out.printf(PROFIT_MESSAGE,yield.getYield());
     }
 }
