807d35ae307a45104364811cd6682784e023e7bb
ensk26 <tjensk26@gmail.com>
2022-11-13 21:26:26 +0900

    feat(Application): 로또 발급 금액 단위 확인 메서드
    새로운 메서드 추가 to Application
    - 로또 번호 발급 금액이 1000원 단위가 아니고, 1000원 보다 작으면 예외 발생
    - IllegalArgumentException 예외 발생

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,19 @@
 package lotto;
 
 public class Application {
+    private final int moneyUnit = 1000;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
 
     public void getLotto(int money) {
 
+    }
+
+    private void validMoney(int money) {
+        if (money < moneyUnit || money % moneyUnit != 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }
