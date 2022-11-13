13988b9c723cbab07f557e9bdff5bfa526709a77
JaehongDev <println@kakao.com>
2022-11-13 15:49:11 +0900

    refactor (Ranking) : 등수가 가질 수 있는 금액을 Long Type으로 변경

--- src/main/java/lotto/domain/settlementsysyem/Ranking.java
+++ src/main/java/lotto/domain/settlementsysyem/Ranking.java
@@ -4,20 +4,21 @@ import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import lotto.domain.money.Money;
 
 public enum Ranking {
-    NOT_WINNING(0, List.of(), ""),
-    FIFTH_WINNING(5000, List.of(new Score(2, 1), new Score(3, 0)), "3개 일치"),
-    FOURTH_WINNING(50000, List.of(new Score(3, 1), new Score(4, 0)), "4개 일치"),
-    THIRD_WINNING(15000000, List.of(new Score(4, 1), new Score(5, 0)), "5개 일치"),
-    SECOND_WINNING(300_000_000, List.of(new Score(5, 1)), "5개 일치, 보너스 볼 일치"),
-    FIRST_WINNING(20_000_000, List.of(new Score(6, 0)), "6개 일치");
+    NOT_WINNING(0L, List.of(), ""),
+    FIFTH_WINNING(5000L, List.of(new Score(2, 1), new Score(3, 0)), "3개 일치"),
+    FOURTH_WINNING(50000L, List.of(new Score(3, 1), new Score(4, 0)), "4개 일치"),
+    THIRD_WINNING(1_500_000L, List.of(new Score(4, 1), new Score(5, 0)), "5개 일치"),
+    SECOND_WINNING(30_000_000L, List.of(new Score(5, 1)), "5개 일치, 보너스 볼 일치"),
+    FIRST_WINNING(2_000_000_000L, List.of(new Score(6, 0)), "6개 일치");
 
-    private final Integer prize;
+    private final Long prize;
     private final List<Score> score;
     private final String result;
 
-    Ranking(final Integer rank, final List<Score> score, final String result) {
+    Ranking(final Long rank, final List<Score> score, final String result) {
         this.prize = rank;
         this.score = score;
         this.result = result;
@@ -41,7 +42,12 @@ public enum Ranking {
     }
 
     public String result() {
-        String prize = String.valueOf(this.prize).replaceAll("\\B(?=(\\d{3})+(?!\\d))", ",");
-        return this.result + " (" + prize + ") ";
+        String prize = String.valueOf(this.prize)
+                .replaceAll("\\B(?=(\\d{3})+(?!\\d))", ",");
+        return this.result + " (" + prize + "원) ";
+    }
+
+    public Money calculateProfits(Integer value) {
+        return new Money(String.valueOf(prize * value));
     }
 }
