d4852d73305d8f7ceff4e14bba3505642ee9c688
KimTaeKang57 <zizoen123@gmail.com>
2022-11-13 14:19:31 +0900

    refac: 2등 당첨금 변경

--- src/main/java/lotto/LottoReward.java
+++ src/main/java/lotto/LottoReward.java
@@ -6,7 +6,7 @@ public enum LottoReward {
     FIFTH(3, 5000),
     FOURTH(4, 50_000),
     THIRD(5, 1_500_000),
-    SECOND(5, 3_000_000),
+    SECOND(5, 30_000_000),
     FIRST(6, 2_000_000_000);
 
     private final int matchingNumbers;
