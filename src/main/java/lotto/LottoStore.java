121c79c874dd51d8cbc20da7f3e337668df4533c
Your Name <you@example.com>
2022-11-13 15:31:44 +0900

    feat(lottoStore) : 사용자 로또 구입금액 입력 문구 출력 추가

---
+++ src/main/java/lotto/LottoStore.java
@@ -0,0 +1,16 @@
+package lotto;
+
+/**
+ * 사용자에게 로또 구입과 발행을 관리
+ *
+ * @version 1.0.0
+ * @author chlskreh2
+ */
+public class LottoStore {
+    /**
+     * 사용자에게 로또 구입금액을 입력해 달라는 문구 출력
+     */
+    public void printBuyAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+}
