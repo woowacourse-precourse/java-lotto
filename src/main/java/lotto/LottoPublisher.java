11ac713c98c856b542a29ffaa4a68a1bfa8b7b99
hodomaroo <enghemdwkd@gmail.com>
2022-11-13 15:04:53 +0900

    [Feat] : LottoPublisher 구현 완료.
    로또 및 로또목록을 생성해 반환하는 기능을 수행하는 LottoPublisher
    클래스의 구현을 완료하였습니다.
    
    LottoPublisherTest 의 테스트 코드를 통한 검증을 완료하였습니다

--- src/main/java/lotto/LottoPublisher.java
+++ src/main/java/lotto/LottoPublisher.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -12,19 +13,24 @@ import java.util.stream.Stream;
  */
 public class LottoPublisher {
 
-    public static List<Lotto> publishLottos(int money){
+    public static List<Lotto> publishLottos(int money) {
         validateMultipleOfLottoCost(money);
-
         int lottoCount = money / Lotto.LOTTO_COST;
-        return new ArrayList<>(Stream.generate(() -> publishLotto()).limit(lottoCount).collect(Collectors.toList()));
-    }
-    public static Lotto publishLotto(){
-        return new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6));
+
+        List<Lotto> lottos = Stream.generate(() -> publishLotto()).limit(lottoCount)
+                .collect(Collectors.toUnmodifiableList());
+
+        IoManager.printLottoPublishInfo(lottoCount, lottos);
+        return lottos;
     }
 
-    private static void validateMultipleOfLottoCost(int money){
-        if(money % Lotto.LOTTO_COST != 0 || money / Lotto.LOTTO_COST == 0){
-            throw new IllegalArgumentException("[ERROR] 구매 금액은 " + Lotto.LOTTO_COST +"의 배수여야 합니다");
+    public static Lotto publishLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    }
+
+    private static void validateMultipleOfLottoCost(int money) {
+        if (money % Lotto.LOTTO_COST != 0 || money / Lotto.LOTTO_COST == 0) {
+            throw new IllegalArgumentException("구매 금액은 " + Lotto.LOTTO_COST + "의 배수여야 합니다");
         }
     }
 }
