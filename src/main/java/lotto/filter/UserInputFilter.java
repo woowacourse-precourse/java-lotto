1aef51a73b0e3d7914044445c4e4e9ff97110a83
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-13 11:55:39 +0900

    feat(UserInputFilter): 유저 입력을 구분자로 구분해서 정수 리스트로 변환 기능 추가

--- src/main/java/lotto/filter/UserInputFilter.java
+++ src/main/java/lotto/filter/UserInputFilter.java
@@ -1,6 +1,10 @@
 package lotto.filter;
 
 import static lotto.message.ErrorMessageConstants.*;
+import static lotto.restrict.RestrictConstants.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserInputFilter {
 
@@ -17,6 +21,31 @@ public class UserInputFilter {
 			result += userInputChar - '0';
 		}
 		return result;
+	}
+
+	public List<Integer> splitStringToDelimiter(String userInputs, int size) {
+		List<Integer> result = new ArrayList<>();
+
+		String[] splitUserInput = splitUserInputs(userInputs, size);
+		for (String userInput : splitUserInput) {
+			Long number = changeStringToLong(userInput);
+			if (number == null) {
+				throw new IllegalArgumentException(USER_INPUT_ERROR_MESSAGE);
+			}
+			result.add(number.intValue());
+		}
+		return result;
+	}
+
+	private String[] splitUserInputs(String userInputs, int size) {
+		if (userInputs == null) {
+			throw new IllegalArgumentException(USER_INPUT_ERROR_MESSAGE);
+		}
+		String[] splitUserInput = userInputs.split(DELIMITER);
+		if (splitUserInput.length != size) {
+			throw new IllegalArgumentException(USER_INPUT_ERROR_MESSAGE);
+		}
+		return splitUserInput;
 	}
 
 	private void checkUserInputChar(char userInputChar) {
