92bebe9aa18fef726819ceaa2980ca04ec9371b5
taeyun-work <wmfajr1357@gmail.com>
2022-11-13 15:39:49 +0900

    feat: 로또 등수 계산에 필요한 기능들을 담은 Generator 클래스 구현
    맞춘 개수 계산, 해당 로또의 등수 계산, 수익률을 계산하는
    Generator 클래스를 구현함

---
+++ src/main/java/lotto/domain/Generator.java
@@ -0,0 +1,51 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import lotto.Ranking;
+
+public class Generator {
+    private static final String WINNINGCOUNT = "winningCount";
+    private static final String BONUSCOUNT = "bonusCount";
+
+    private Generator() {
+    }
+
+    private static HashMap<String, Integer> judgeWinningCount(Lotto lotto) {
+        List<Integer> winningNumbers = Lotto.getWinningNumbers();
+        int bonusNumber = Lotto.getBonusNumber();
+
+        final int[] winningCount = { 0 };
+        final int[] bonusCount = { 0 };
+        lotto.getNumbers()
+                .stream()
+                .mapToInt(Integer::intValue)
+                .forEach(number -> {
+                    if (winningNumbers.contains(number)) {
+                        winningCount[0]++;
+                    }
+                    if (bonusNumber == number) {
+                        bonusCount[0]++;
+                    }
+                });
+
+        HashMap<String, Integer> result = new HashMap<String, Integer>();
+        result.put(WINNINGCOUNT, winningCount[0]);
+        result.put(BONUSCOUNT, bonusCount[0]);
+        return result;
+    }
+
+    public static void calculateRank(Buyer buyer) {
+        for (Lotto lotto : buyer.getLottoPurchased()) {
+            HashMap<String, Integer> correct = judgeWinningCount(lotto);
+            Ranking rank = Ranking.getRank(correct.get(WINNINGCOUNT), correct.get(BONUSCOUNT));
+            Buyer.setWinningSummary(buyer, rank.getLabel());
+        }
+
+    }
+
+    public static double profitCalculate(int purchased, long totalPrize) {
+        double result = ((double) totalPrize / (long) purchased) * 100;
+        return result;
+    }
+}
