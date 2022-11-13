95024285e084223fd11be2959f8f655fa806fec5
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-13 17:40:57 +0900

    feat : 생성한 로또의 Size 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import static lotto.domain.Lotto.createLotto;
 import static lotto.view.WinningInput.getWinningResult;
 import static lotto.view.WinningInput.winningCount;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.util.ArrayList;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -27,11 +29,11 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호 범위 1~45의 숫자가 아닐 경우 예외 발생")
+    @DisplayName("로또 생성 메서드는 6개의 번호를 생성")
     @Test
-    void createLottoByRange() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 70)))
-                .isInstanceOf(IllegalArgumentException.class);
+    void createLottoTestSize() {
+        List<Lotto> lotto = new ArrayList<>(List.of(createLotto()));
+        assertThat(lotto.get(0).getNumbers().size() == 6).isTrue();
     }
 
     @DisplayName("로또 번호와 일치하는 숫자 개수를 구하는 메서드 테스트")
