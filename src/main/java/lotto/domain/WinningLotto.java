e206fe985460ec377d3764d6751eb64cb9632bf2
Soomin Lim <augustth511@gmail.com>
2022-11-13 18:16:09 +0900

    feat(LottoGameResult): 당첨 통계 기능 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -6,11 +6,11 @@ import static lotto.domain.ErrorMessage.BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE;
 import static lotto.domain.ErrorMessage.LOTTO_NUMBER_RANGE_ERROR_MESSAGE;
 
 public class WinningLotto {
-    private final Lotto winningNumbers;
+    private final Lotto winningLotto;
     private final int bonusNumber;
 
-    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
-        this.winningNumbers = new Lotto(winningNumbers);
+    public WinningLotto(List<Integer> winningLotto, int bonusNumber) {
+        this.winningLotto = new Lotto(winningLotto);
 
         validateBonusNumber(bonusNumber);
         this.bonusNumber = bonusNumber;
@@ -28,18 +28,26 @@ public class WinningLotto {
     }
 
     private void validateDuplicationWithWiningNumbers(int bonusNumber) {
-        if (winningNumbers.getNumbers()
+        if (winningLotto.getNumbers()
                 .stream()
                 .anyMatch(number -> number == bonusNumber)) {
             throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE);
         }
     }
 
-    public Lotto getWinningNumbers() {
-        return winningNumbers;
+    public Rank compare(Lotto lotto) {
+        int count = countSameNumbers(lotto);
+        boolean isBonus = lotto.getNumbers().contains(bonusNumber);
+
+        Rank rank = Rank.findRank(count, isBonus);
+        return rank;
     }
 
-    public int getBonusNumber() {
-        return bonusNumber;
+    private int countSameNumbers(Lotto lotto) {
+        long count = winningLotto.getNumbers()
+                .stream()
+                .filter(number -> lotto.getNumbers().contains(number))
+                .count();
+        return (int)count;
     }
 }
