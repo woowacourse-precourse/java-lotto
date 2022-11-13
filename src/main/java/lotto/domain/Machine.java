c4e92c6d41c65a02a70bd138cb85c34f944dd9d0
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-13 11:38:20 +0900

    fix: 에러 메시지 출력 후 정상 종료 하도록 수정

--- src/main/java/lotto/domain/Machine.java
+++ src/main/java/lotto/domain/Machine.java
@@ -8,17 +8,21 @@ import java.util.List;
 public class Machine {
 
     public void start() {
-        int quantity = Convertor.getQuantity(InputView.amount());
-        List<Lotto> lottos = LottoGenerator.getLottos(quantity);
-        OutputView.showLottoNumber(lottos);
+        try {
+            int quantity = Convertor.getQuantity(InputView.amount());
+            List<Lotto> lottos = LottoGenerator.getLottos(quantity);
+            OutputView.showLottoNumber(lottos);
 
-        List<Integer> winningNumbers = Convertor.getNumbers(InputView.winningNumbers());
-        int bonusNumber = Convertor.getNumber(InputView.bonusNumber());
+            List<Integer> winningNumbers = Convertor.getNumbers(InputView.winningNumbers());
+            int bonusNumber = Convertor.getNumber(InputView.bonusNumber());
 
-        List<Rank> ranks = RankGenerator.getRanks();
-        CompareLotto.compare(ranks, lottos, winningNumbers, bonusNumber);
-        double yield = Calculator.getYield(ranks, quantity);
-        OutputView.result(ranks, yield);
+            List<Rank> ranks = RankGenerator.getRanks();
+            CompareLotto.compare(ranks, lottos, winningNumbers, bonusNumber);
+            double yield = Calculator.getYield(ranks, quantity);
+            OutputView.result(ranks, yield);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 
