b3f9164e474c170784b9aa27daaa5f69798a52c1
qdeu22 <qdeu22@gmail.com>
2022-11-13 20:46:29 +0900

    feat(lotto): 프로그램 실행 기능 구현

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -1,4 +1,39 @@
 package lotto.domain;
 
+import java.util.List;
+import java.util.Map;
+
 public class LottoGame {
+    private final Input input;
+    private final Output output;
+
+    public LottoGame() {
+        this.input = new Input();
+        this.output = new Output();
+    }
+
+    public void start() {
+        int purchase = input.inputPurchase();
+
+        output.buyLottoCount(purchase);
+
+        NumberGenerator generator = new NumberGenerator();
+        List<List<Integer>> generatorNumbers = generator.createNumbers(purchase / 1000);
+
+        output.everyLottoNumber(purchase, generatorNumbers);
+
+        Lotto lotto = new Lotto(input.inputLottoNumber());
+        System.out.println();
+
+        int bonusNumber = lotto.bonusValidate(input.inputBonusNumber());
+        System.out.println();
+
+        Referee referee = new Referee();
+
+        Map<Rank, Integer> ranking = referee.findNumber(generatorNumbers, bonusNumber, lotto);
+
+        output.result(purchase, referee, ranking);// 출력
+    }
+
+
 }
