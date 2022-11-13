6459205fac7e4c980afa9604d4e857fab78290a1
GilHun <suleemon1@naver.com>
2022-11-13 16:23:01 +0900

    test(BuyerTest): 구매자 구입 테스트 추가

---
+++ src/test/java/lotto/domain/BuyerTest.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BuyerTest {
+    @Test
+    void 구매한_로또_갯수_테스트(){
+        // given
+        String testBuyerMoney = "6000";
+        int expected = 6;
+
+        // when
+        Buyer testBuyer = new Buyer();
+        InputStream in = new ByteArrayInputStream(testBuyerMoney.getBytes(StandardCharsets.UTF_8));
+        System.setIn(in);
+        testBuyer.inputPurchaseMoney();
+
+        // then
+        assertThat(testBuyer.getHaveCount()).isEqualTo(expected);
+    }
+
+
+}
