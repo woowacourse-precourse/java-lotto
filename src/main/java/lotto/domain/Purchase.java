9acd230eac406b0f7d482d9b2323013092f6a5d3
eyeofsol <kingjihong@naver.com>
2022-11-13 11:25:20 +0900

    feat(Purchase): 로또 여러개를 생성하는 기능 구현 완료
    로또를 여러개 생성하는 기능 구현을 완료했습니다.
    
    private List<Lotto> buyLottoes(int money)
    private void createLottoes(List<Lotto> lottoes, int lottoAmount)
    
    구매한 로또 수량에 따라 랜덤으로 6자리의 로또를 생성하도록 구현했습니다.

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -17,8 +17,21 @@ public class Purchase {
     }
 
     private List<Lotto> buyLottoes(int money) {
+        List<Lotto> lottoes = new ArrayList<>();
+        int lottoAmount = getLottoAmount(money);
+
+        //TODO: 메서드 이름 추후에 한번 더 고민해보자.
+        createLottoes(lottoes, lottoAmount);
 
         return lottoes;
+    }
+
+    private void createLottoes(List<Lotto> lottoes, int lottoAmount) {
+        for (int i = 0; i < lottoAmount; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            lottoes.add(lotto);
+        }
     }
 
     public int getLottoAmount(int money) {
