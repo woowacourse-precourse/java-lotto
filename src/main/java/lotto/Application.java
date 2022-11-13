4183793b7daae28374267993970c064144c71299
JaehongDev <println@kakao.com>
2022-11-13 16:07:55 +0900

    feature (Application) : 게임 실행 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.domain.game.LottoGame;
+import lotto.system.IoSystem;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        IoSystem io = new IoSystem();
+        try {
+            var game = new LottoGame(io);
+            game.execute();
+        } catch (IllegalArgumentException exception) {
+            io.println(exception.getMessage());
+        }
     }
 }
