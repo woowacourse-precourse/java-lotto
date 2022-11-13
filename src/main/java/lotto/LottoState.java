763a00f73a19829f32002ca455850db45531fa51
Sunghyun-0 <zkdldk95@naver.com>
2022-11-14 00:25:48 +0900

    LottoState 메세지 추가

--- src/main/java/lotto/LottoState.java
+++ src/main/java/lotto/LottoState.java
@@ -1,15 +1,19 @@
 package lotto;
 
 public enum LottoState {
-    NONE(0),
-    MATCH_ONE(1),
-    MATCH_TWO(2),
-    MATCH_THREE(3),
-    MATCH_FOUR(4),
-    MATCH_FIVE(5),
-    MATCH_SIX(6),
-    MATCH_FIVE_WITH_BONUS(7);
+    MATCH_THREE("3개 일치 (5,000원) - "),
+    MATCH_FOUR("4개 일치 (50,000원) - "),
+    MATCH_FIVE("5개 일치 (1,500,000원) - "),
+    MATCH_SIX("6개 일치 (2,000,000,000원) - "),
+    MATCH_FIVE_WITH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - ");
 
-    LottoState(double howMuchYouHit){
+    private final String name;
+
+    LottoState(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
     }
 }
