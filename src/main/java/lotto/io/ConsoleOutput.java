4a8d8564acae89b2eed53222ec2e0baff2d9f8d6
UJ15 <dbwogml15@naver.com>
2022-11-14 07:01:33 +0900

    feat : create print winning, bonus number input prompt

--- src/main/java/lotto/io/ConsoleOutput.java
+++ src/main/java/lotto/io/ConsoleOutput.java
@@ -2,9 +2,21 @@ package lotto.io;
 
 public class ConsoleOutput implements Output {
     private static final String PURCHASE_PROMPT = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
 
     @Override
     public void printPurchasePrompt() {
         System.out.println(PURCHASE_PROMPT);
+    }
+
+    @Override
+    public void printWinningNumberPrompt() {
+        System.out.println(WINNING_NUMBER_PROMPT);
+    }
+
+    @Override
+    public void printBonusNumberPrompt() {
+        System.out.println(BONUS_NUMBER_PROMPT);
     }
 }
--- src/main/java/lotto/io/Output.java
+++ src/main/java/lotto/io/Output.java
@@ -2,4 +2,8 @@ package lotto.io;
 
 public interface Output {
     void printPurchasePrompt();
+
+    void printWinningNumberPrompt();
+
+    void printBonusNumberPrompt();
 }
