d4b6f139bac609d3a013b8695874274637fc6390
w0ntae <bitcampcoding@gmail.com>
2022-11-14 00:22:54 +0900

    feat(lotto): Add gen
    Add method to generate List of unique numbers

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -33,5 +35,9 @@ public class Lotto {
             return money / 1000;
         }
         return 0;
+    }
+    
+    public List<Integer> gen(){
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }
