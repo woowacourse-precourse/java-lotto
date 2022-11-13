68291925643ed017db3f64aab0af2c7399153c16
CAEL0 <kjongmin26@gmail.com>
2022-11-13 16:29:02 +0900

    feat(OutputView): 구입금액 입력 메시지 출력

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import static lotto.constant.OutputConstants.*;
+
+public class OutputView {
+
+    public static void printPurchaseAmountMessage() {
+        System.out.println(INPUT_PURCHASE_AMOUNT);
+    }
+}
