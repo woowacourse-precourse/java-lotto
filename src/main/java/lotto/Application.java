40432e65c476dad4ce9395095976bdfa2c4f2128
hoonn9 <hun9935@gmail.com>
2022-11-13 20:20:02 +0900

    feat(Application): 로또 시작, 예외 처리 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.domain.LottoGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            LottoGame lottoGame = new LottoGame();
+            lottoGame.start();
+        } catch (IllegalArgumentException e) {
+            printErrorMessage(e);
+        }
+    }
+    
+    private static void printErrorMessage(Exception e) {
+        System.out.println("[ERROR] " + e.getMessage());
     }
 }
