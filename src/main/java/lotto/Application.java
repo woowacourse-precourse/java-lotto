a14b7846129cf1318c189b583352e08307e240ca
조미정 <ing06164@naver.com>
2022-11-13 21:26:21 +0900

    feat: 입력한 금액의 수량만큼 로또 번호 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,11 +11,26 @@ public class Application {
         System.out.println("로또 프로그램입니다. \n금액을 입력해 주세요");
         String purchase = Console.readLine();           // 구매 금액
 
+        // 유효한 구매 금액인지 확인 - 1000원 단위로 나누어지지 않으면 잘못된 입력
         if(!isValidPurchase(purchase)){
             System.out.println("잘못된 입력입니다! 수량에 맞는 금액을 입력해 주세요!");
             purchase = Console.readLine();
         }
+
+        // 로또 수량
+        int amount = Integer.parseInt(purchase) / 1000;
+        System.out.println(amount+"개를 구매했습니다.");
+
+        for(int i=0; i<amount; i++){
+            // 로또 번호 생성
+            numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+            Lotto purchasedLotto = new Lotto(numbers);
+            System.out.println(purchasedLotto.getNumbers());
+        }
     }
+
+    // 구매 금액이 유효한지 확인하고 리턴
     static boolean isValidPurchase(String purchase) {
         if (Integer.parseInt(purchase) % 1000 != 0) {
             return false;
