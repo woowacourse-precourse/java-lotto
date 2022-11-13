c25b0f3b781d51740819629393bf8bc971f960e4
poter0501 <poter0501@gmail.com>
2022-11-13 21:16:38 +0900

    feat(User): User 클래스 생성 및 인스턴스 변수들 선언

---
+++ src/main/java/lotto/User.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private List<Lotto> lottos;
+    private int price;
+    private float earningRate;
+
+
+}
