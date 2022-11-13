096ef336e4f1f47286ed239a621f4be0dc6d9f3c
skydreamer21 <skydreamer210@gmail.com>
2022-11-14 00:23:36 +0900

    feat(LotterySellService): User의 구매 금액 만큼 로또 구매 기능 구현
    [세부사항]
    - 구현 메서드
    1. sell
    2. registerLotteryTicket : LotteryTicketRepository에 판매된 티켓 등록

--- src/main/java/lotto/service/LotterySellService.java
+++ src/main/java/lotto/service/LotterySellService.java
@@ -2,14 +2,29 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.LotteryTicket;
+import lotto.domain.User;
 import lotto.domain.constant.LottoProperty;
+import lotto.repository.LotteryTicketRepository;
 
 import java.util.List;
 
 public class LotterySellService {
+    public void sell(User user) {
+        while (user.hasMoney()) {
+            LotteryTicket lotteryTicket = createLotteryTicket();
+            registerLotteryTicket(lotteryTicket);
+            user.buyLotteryTicket(createLotteryTicket());
+        }
+    }
+
     public LotteryTicket createLotteryTicket() {
         List<Integer> lotteryNumbers = generateLotteryNumbers();
         return new LotteryTicket(lotteryNumbers);
+    }
+
+    private void registerLotteryTicket(LotteryTicket lotteryTicket) {
+        LotteryTicketRepository repository = LotteryTicketRepository.getInstance();
+        repository.addLotteryTicket(lotteryTicket);
     }
 
     private List<Integer> generateLotteryNumbers() {
