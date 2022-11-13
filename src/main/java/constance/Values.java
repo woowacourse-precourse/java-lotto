54e2b0c78ef5658e5ca71fcf179497f4c75243f7
nn98 <jkllhgb@gmail.com>
2022-11-14 00:20:05 +0900

    Feat: Values-상수 valiable 초기화

--- src/main/java/constance/Values.java
+++ src/main/java/constance/Values.java
@@ -1,4 +1,11 @@
 package constance;
 
+import java.text.DecimalFormat;
+
 public class Values {
+    public static final int NUMBERS_SIZE = 6;
+    public static final int MIN_COINCIDE = 3;
+    public static final int MAX_COINCIDE = 6;
+    public static final int[] WINNINGS = {0, 0, 0, 5000, 50000, 1500000, 2000000000, 30000000};
+    public static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("###,###");
 }
