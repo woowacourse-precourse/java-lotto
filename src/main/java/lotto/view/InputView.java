0474d72e312e77f0da2dd49dca20bec5c34531cf
Jeangyu <gjwlsrb1700@naver.com>
2022-11-13 23:54:09 +0900

    [FEAT: lotto] 구입 금액을 입력하는 view 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,19 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+import java.util.Scanner;
+
+import static lotto.utils.Validator.validateInteger;
+
+public class InputView {
+
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public int printWriteBudget(){
+        System.out.println("구입금액을 입력해 주세요.");
+        String userInput = SCANNER.nextLine();
+
+        validateInteger(userInput);
+
+        return Integer.valueOf(userInput);
+    }
 }
