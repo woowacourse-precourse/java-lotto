328d3948920907db7cd7ea75fd31274affaf89a5
ijung-won <dlwnddnjs96@gmail.com>
2022-11-13 23:05:49 +0900

    refactor(Application): 단계에 맞춰 로또 게임 진행 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,9 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        User user = new User();
+        Lotto lotto;
+
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
+        user.buyingLotto(input);
         System.out.println("당첨 번호를 입력해 주세요.");
         String inputLottoNum = Console.readLine();
+        lotto = new Lotto(user.choosingLottoNumber(inputLottoNum));
     }
 }
