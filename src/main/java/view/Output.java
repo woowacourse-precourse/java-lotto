9dab8ff5976bf416ab2450c647f4192a13f20554
chnlee <leechan6022@gmail.com>
2022-11-14 01:42:39 +0900

    feat(Output) : 티켓 개수 출력 메세지

--- src/main/java/view/Output.java
+++ src/main/java/view/Output.java
@@ -1,2 +1,8 @@
-package view;public class Output {
+package view;
+
+public class Output {
+    private static final String OUTPUT_TICKET_COUNT = "개를 구매했습니다.";
+    public static void outputTicketCount(int count) {
+        System.out.println(count + OUTPUT_TICKET_COUNT);
+    }
 }
