a672602a2c9fd91f2cc783a61f839dff9d0f08a0
SeokHwan An <asok369258@naver.com>
2022-11-14 00:00:17 +0900

    feat : 여러개의 로또를 가지고 있는 객체 추가

---
+++ src/main/java/lotto/Lottos.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import java.util.List;
+
+public class Lottos {
+
+    private List<Lotto> lottos;
+
+    // 로또 여러개를 저장하는 기능
+    public void addLotto(Lotto lotto) {
+        lottos.add(lotto);
+    }
+
+}
