00512ab4d61588ec367343db16067247fddb12d1
gusu9 <suhan.ku9@gmail.com>
2022-11-14 00:23:16 +0900

    feat: Add buyer input

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,9 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Lotto;
 
@@ -11,20 +11,29 @@ public class Application {
     public static void main(String[] args) {
 
         Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+//        Buyer buyer = new Buyer();
+        List<Integer> lottoNumbers;
 
-        List<Integer> lottoNumbers = lotto.numbers();
-        System.out.println(lottoNumbers);
+        System.out.println("구입금액을 입력해주세요: ");
+        int money = Integer.parseInt(Console.readLine());
 
-        Collection<Integer> regularLotto = new ArrayList(lottoNumbers);
+        int quantity = money/1000;
 
-        List<Integer> bonusNum = new ArrayList<>();
-        while (bonusNum.size()<1){
-            int bonus = Randoms.pickNumberInRange(1,45);
-            if(!bonusNum.contains(lottoNumbers)){
-                bonusNum.add(bonus);
-            }
+        for (int i = 0; i<quantity; i++) {
+            lottoNumbers = lotto.numbers();
+            System.out.println(lottoNumbers);
         }
-        System.out.println(bonusNum);
+
+        System.out.println("당첨번호를 입력해주세요: ");
+        int buyerNum = Integer.parseInt(Console.readLine());
+        int[] digits = Stream.of(String.valueOf(buyerNum).split("")).mapToInt(Integer::parseInt).toArray();
+        List<Integer> buyer = new ArrayList<>();
+
+        for (int order = 0; order < digits.length; order++) {
+            buyer.add(digits[order]);
+        }
+        Set<Integer> buyerSet = new HashSet<>(buyer);
+
 
 
 
@@ -44,5 +53,15 @@ public class Application {
 ////            System.out.println("Independent");
 ////        }
 //
+//    }
+//List<Integer> bonusNum = new ArrayList<>();
+//
+//    while (bonusNum.size()<1){
+//        int bonus = Randoms.pickNumberInRange(1,45);
+//        if(!bonusNum.contains(lottoNumbers)){
+//            bonusNum.add(bonus);
+//            System.out.println(bonusNum);
+//
+//        }
 //    }
 }
