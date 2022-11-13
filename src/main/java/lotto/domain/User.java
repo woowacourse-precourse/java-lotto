7859722b0c68b3c61af8c79f39981ff58357a1e0
limzzum <chfhddl1234@naver.com>
2022-11-13 19:01:49 +0900

    refactor : 당첨 번호와 보너스 번호 입력 함수 분리

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -14,48 +14,65 @@ public class User {
     private List<Integer> winningNumber;
     private Integer bonusNumber;
 
+
     public User(List<Lotto> lottos) {
         this.lottos = lottos;
     }
 
-    public void inputWinningNumber(){
+    public void inputWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
         List<String> numbers = List.of(input.split(","));
-        //validateLottoPlusBonusNum(numbers);
-        for(String num: numbers){
+        validateLottoNum(numbers);
+        for (String num : numbers) {
             winningNumber.add(Integer.parseInt(num));
         }
-        System.out.println("보너스 번호를 입력해 주세요.");
-        String bonusInput = Console.readLine();
 
     }
 
-    private void validateLottoNum(List<String> numbers){
-        if(numbers.size()!=6){
-            throw  new IllegalArgumentException("[ERROR] 로또의 번호가 6개여야 합니다");
+    public List<Integer> getWinningNumber() {
+        return winningNumber;
+    }
+
+    public void inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusInput = Console.readLine();
+        validateBonusNum(bonusInput);
+        bonusNumber = Integer.parseInt(bonusInput);
+
+    }
+
+    public Integer getBonusNumber() {
+        return bonusNumber;
+    }
+
+
+    private void validateLottoNum(List<String> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또의 번호가 6개여야 합니다");
         }
 
-        if(Set.copyOf(numbers).size() != 6 ){
-            throw  new IllegalArgumentException("[ERROR] 로또 번호와 보너스 번호는 중복이 없어야 합니다");
+        if (Set.copyOf(numbers).size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호와 보너스 번호는 중복이 없어야 합니다");
         }
-        for(int i=0; i<numbers.size(); i++){
+        for (int i = 0; i < numbers.size(); i++) {
             String num = numbers.get(i);
-            if(num.charAt(i) <1 || num.charAt(i)>45){
+            if (num.charAt(i) < 1 || num.charAt(i) > 45) {
                 throw new IllegalArgumentException("[ERROR] 1 ~ 45 숫자만 입력 가능합니다");
             }
         }
 
     }
-    private void validateBonusNum(String num){
-        if(num.length() != 1){
-            throw  new IllegalArgumentException("[ERROR] 보너스 번호는 한 개입니다");
+
+    private void validateBonusNum(String num) {
+        if (num.length() != 1) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 한 개입니다");
         }
 
-        if(winningNumber.contains(bonusNumber)){
-            throw  new IllegalArgumentException("[ERROR] 로또 번호와 보너스 번호는 중복이 없어야 합니다");
+        if (winningNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호와 보너스 번호는 중복이 없어야 합니다");
         }
-        if( num.charAt(0)<1 || num.charAt(0)>45){
+        if (num.charAt(0) < 1 || num.charAt(0) > 45) {
             throw new IllegalArgumentException("[ERROR] 1 ~ 45 숫자만 입력 가능합니다");
         }
 
