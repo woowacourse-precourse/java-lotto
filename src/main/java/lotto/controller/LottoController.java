7051cf582e2e71e8b8515182d16b8840ca5de3cd
kokodak <kokodakadokok@gmail.com>
2022-11-13 22:05:23 +0900

    feat(lottoController): 예외가 발생했을 때 에러 문구를 출력하는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -12,18 +12,22 @@ import lotto.view.OutputView;
 public class LottoController {
 
     public void lottoStart() {
-        int purchaseAmount = InputView.getPurchaseAmount();
-        LottoGenerator lottoGenerator = new LottoGenerator(purchaseAmount);
-        Lottos lottos = new Lottos(lottoGenerator.generateLottos());
+        try {
+            int purchaseAmount = InputView.getPurchaseAmount();
+            LottoGenerator lottoGenerator = new LottoGenerator(purchaseAmount);
+            Lottos lottos = new Lottos(lottoGenerator.generateLottos());
 
-        OutputView.printHowManyLottoUserPurchased(lottoGenerator.getLottoQuantity());
-        OutputView.printLottos(lottos);
+            OutputView.printHowManyLottoUserPurchased(lottoGenerator.getLottoQuantity());
+            OutputView.printLottos(lottos);
 
-        WinningLotto winningLotto = new WinningLotto(InputView.getLottoNumber(), InputView.getLottoBonusNumber());
+            WinningLotto winningLotto = new WinningLotto(InputView.getLottoNumber(), InputView.getLottoBonusNumber());
 
-        Map<WinningRank, Integer> winningDetails = WinningStatistics.getWinningDetails(lottos, winningLotto);
-        OutputView.printWinningStatistics();
-        OutputView.printWinningDetails(winningDetails);
-        OutputView.printLottoYield(WinningStatistics.getLottoYield(winningDetails, purchaseAmount));
+            Map<WinningRank, Integer> winningDetails = WinningStatistics.getWinningDetails(lottos, winningLotto);
+            OutputView.printWinningStatistics();
+            OutputView.printWinningDetails(winningDetails);
+            OutputView.printLottoYield(WinningStatistics.getLottoYield(winningDetails, purchaseAmount));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
