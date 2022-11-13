4318f964fb0efe727a9b1eb2113dbf8437943aa6
MoonJeWoong <munjin0201@naver.com>
2022-11-13 23:11:55 +0900

    feat(application): 전체 프로그램 단위 예외처리 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,10 @@ import lotto.controller.Controller;
 public class Application {
     public static void main(String[] args) {
         Controller controller = new Controller();
-        controller.run();
+        try {
+            controller.run();
+        } catch (Exception e) {
+            System.out.println("[ERROR] 프로그램을 종료합니다.");
+        }
     }
 }
