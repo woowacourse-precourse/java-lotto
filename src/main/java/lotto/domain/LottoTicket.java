93cba989fc52f94c3f8b707a5ef4ef7a907e9288
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-11-13 19:26:37 +0900

    refactor: LottoTicket 상수화

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -8,6 +8,9 @@ import java.util.stream.Collectors;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoTicket {
+	private static final int START_RANGE = 1;
+	private static final int END_RANGE = 45;
+	private static final int LOTTO_COUNT = 6;
 	private final List<Lotto> lottoTickets = new ArrayList<>();
 
 	public LottoTicket(int ticketNumber) {
@@ -16,7 +19,8 @@ public class LottoTicket {
 
 	private void createLottoTickets(int ticketNumber) {
 		for (int i = 0; i < ticketNumber; i++) {
-			List<Integer> lottoNumber = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+			List<Integer> lottoNumber = new ArrayList<>(
+				Randoms.pickUniqueNumbersInRange(START_RANGE, END_RANGE, LOTTO_COUNT));
 			Collections.sort(lottoNumber);
 			Lotto lotto = new Lotto(lottoNumber);
 			lottoTickets.add(lotto);
