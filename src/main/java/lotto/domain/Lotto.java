f55aa560962633e72483f3c91f8c1219ae53ef79
sonsoowon <yuwoo1018@gmail.com>
2022-11-13 19:10:35 +0900

    feat(OutputView): 로또 영수증 출력
    - Lotto.toString() 재정의
    - 로또 구매 수량 출력 메서드 캡슐화

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import lotto.valid.LottoValidator;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,4 +14,12 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+    @Override
+    public String toString(){
+        return numbers.stream()
+                .sorted()
+                .collect(Collectors.toList())
+                .toString();
+    }
 }
--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -1,12 +1,25 @@
 package lotto.ui;
 
-import lotto.domain.LottoMachine;
+
+import lotto.domain.Lotto;
+
+import java.util.List;
 
 public enum OutputView {
     INSTANCE;
-    private static final String PURCHASE_LOTTO_MESSAGE = "개를 구매했습니다";
+    private static final String LOTTO_QUANTITY_MESSAGE_FORMAT = "개를 구매했습니다";
 
-    public static void printPurchasedLottoCount(int purchasedLottoCount) {
-        System.out.println(purchasedLottoCount + PURCHASE_LOTTO_MESSAGE);
+    public static void printLottoReceipts(List<Lotto> lotteries){
+        printLottoQuantity(lotteries);
+        printLotteries(lotteries);
+    }
+    private static void printLottoQuantity(List<Lotto> lotteries) {
+        System.out.println(lotteries.size() + LOTTO_QUANTITY_MESSAGE_FORMAT);
+    }
+
+    private static void printLotteries(List<Lotto> lotteries){
+        for(Lotto lotto : lotteries){
+            System.out.println(lotto.toString());
+        }
     }
 }
