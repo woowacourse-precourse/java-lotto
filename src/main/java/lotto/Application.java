8ef2febcafa59c8d1ebe390e14e5e20aed099a44
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-13 20:39:39 +0900

    fix(Application): fix name typo

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,7 +3,7 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         LottoConsole lottoConsole = new LottoConsole();
-        LottoGame lottoGame = new LottoGame(lottoConsole.getParchaseAmount());
+        LottoGame lottoGame = new LottoGame(lottoConsole.getPurchaseAmount());
         lottoConsole.printLottos(lottoGame.getLottos());
         lottoGame.play(lottoConsole.getWinningLottoNumbers(), lottoConsole.getBonusLottoNumber());
         lottoConsole.printResult(lottoGame.getLottoResult());
