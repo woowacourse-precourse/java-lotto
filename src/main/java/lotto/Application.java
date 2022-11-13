65da548fbd679a429eea72e18faa595fecde61b5
psy-choi <84831081+psy-choi@users.noreply.github.com>
2022-11-13 21:43:26 +0900

    feat : [#1] 메소드 1 작성(get_lotto_numbers)
    몇개의 로또를 사서 로또의 번호를 각각 출력하고 이를 리스트로 return 하는
    메소드이다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,37 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
+
+    public static ArrayList<Lotto> get_lotto_numbers(int input_money){
+
+        ArrayList<Lotto> lottos = new ArrayList<>();
+        int number = input_money / 1000;
+        System.out.printf("%d개를 구매했습니다.\n", number);
+
+        for (int i=0; i < number; i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            lotto.sort();
+            lotto.print_lotto();
+            lottos.add(lotto);
+        }
+
+        return lottos;
+    }
     public static void main(String[] args) {
+
+        System.out.println("구입 금액을 입력해주세요.");
+        Scanner sc = new Scanner(System.in);
+        int input_money = sc.nextInt();
+        if ((input_money % 1000) > 0){
+            System.out.println("[ERROR] 로또 구입 금액은 1000원 단위로 받을 수 있습니다.");
+            throw new IllegalArgumentException();
+        }
+        get_lotto_numbers(input_money);
         // TODO: 프로그램 구현
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -10,10 +11,23 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    public void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public void sort(){
+        Collections.sort(this.numbers);
+    }
+
+    public void print_lotto(){
+        System.out.println(this.numbers);
+    }
+
+
+    private int get_lotto_number(List<Integer> numbers, int i){
+        return numbers.get(i);
     }
 
     // TODO: 추가 기능 구현
