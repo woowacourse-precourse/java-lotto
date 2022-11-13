72c7587e4f9e318add81a4d75228b21dfcdecb90
hyeonseog-won <smreo75@gmail.com>
2022-11-14 00:26:01 +0900

    refactor: user 로직 user class로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,37 +1,7 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
-        System.out.println("구입금액을 입력해 주세요.");
-        int money = toInteger(Console.readLine());
-        int lottoSheetCount = money / 1000;
 
-        List<Lotto> lottos = new ArrayList<>();
-        for (int ticket = 0; ticket < lottoSheetCount; ticket++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            lottos.add(new Lotto(numbers));
-        }
-
-        System.out.println(lottos.toString());
-
-    }
-
-    private static int toInteger(String input) {
-        int money = Integer.parseInt(input);
-        if (money < 1000) {
-            throw new IllegalArgumentException("[ERROR] 1,000원 이상을 입력해 주세요.");
-        }
-
-        if (money % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 1,000원으로 나누어 떨어지는 금액만 입력해 주세요.");
-        }
-
-        return money;
     }
 }
---
+++ src/main/java/lotto/User.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private List<Lotto> lottos = new ArrayList<>();
+    private int money;
+
+    public void boyLotto() {
+        System.out.println("구입금액을 입력해 주세요.");
+        money = toInteger(Console.readLine());
+        int lottoSheetCount = money / 1000;
+
+        for (int ticket = 0; ticket < lottoSheetCount; ticket++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(numbers));
+        }
+    }
+
+    private int toInteger(String input) {
+        int money = Integer.parseInt(input);
+        if (money < 1000) {
+            throw new IllegalArgumentException("[ERROR] 1,000원 이상을 입력해 주세요.");
+        }
+
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000원으로 나누어 떨어지는 금액만 입력해 주세요.");
+        }
+
+        return money;
+    }
+}
