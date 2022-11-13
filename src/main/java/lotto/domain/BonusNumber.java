4e874c54708275c3f303ed6d2f98e94b38211471
be-student <presentsong@naver.com>
2022-11-13 15:07:53 +0900

    feat(BonusNumber): 보너스 넘버 검증 작업 추가

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,10 +1,26 @@
 package lotto.domain;
 
+import lotto.constant.LottoConstants;
+
 public class BonusNumber {
     private final int number;
 
     public BonusNumber(int number) {
-        this.number=number;
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(int number) {
+        isInRange(number);
+    }
+
+    private void isInRange(int number) {
+        if (number < LottoConstants.LOTTO_START_INCLUSIVE.value()) {
+            throw new IllegalArgumentException();
+        }
+        if (number > LottoConstants.LOTTO_END_INCLUSIVE.value()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public boolean isBonusNumber(int other) {
