e2ffc25eb0a631f35bd17d6f0cb6e6d1f2fba794
Danden1 <goh1211@ajou.ac.kr>
2022-11-14 00:30:36 +0900

    feat : LottoRanking test 코드 추가
    1,2,3,4,5 등 테스트 완료

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,4 +25,29 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("순위 체크 함수")
+    @Test
+    void getLottoRanking(){
+        List<Integer> lotto = List.of(1,2,3,4,5,6);
+
+        //1등 테스트
+        assertThat(LottoRanking.caculateRanking(lotto, 7, List.of(1,2,3,4,5,6)))
+                .isEqualTo(LottoRanking.FIRST);
+
+        // 2등 테스트
+        assertThat(LottoRanking.caculateRanking(lotto, 7, List.of(2,3,4,5,7,6)))
+                .isEqualTo(LottoRanking.SECOND);
+
+        // 3등 테스트
+        assertThat(LottoRanking.caculateRanking(lotto, 7, List.of(2,3,4,5,6,8)))
+                .isEqualTo(LottoRanking.THIRD);
+
+        // 4등 테스트
+        assertThat(LottoRanking.caculateRanking(lotto, 7, List.of(2,3,4,5,9,8)))
+                .isEqualTo(LottoRanking.FORTH);
+
+        // 5등 테스트
+        assertThat(LottoRanking.caculateRanking(lotto, 7, List.of(2,3,4,10,8,9)))
+                .isEqualTo(LottoRanking.FIFTH);
+    }
 }
