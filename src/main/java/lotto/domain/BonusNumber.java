bb18789a1a58d89de38a6af7ba149a7297cd3764
twoosky <lhjae3@naver.com>
2022-11-14 02:34:00 +0900

    feat(domain): 보너스 번호 숫자 아닌 경우 예외 처리

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -2,20 +2,29 @@ package lotto.domain;
 
 import java.util.List;
 
-import static lotto.utils.ErrorMessages.BONUS_NUMBER_DUPLICATED;
-import static lotto.utils.ErrorMessages.BONUS_NUMBER_OUT_OF_RANGE;
+import static lotto.utils.ErrorMessages.*;
 
 public class BonusNumber {
     private final int number;
 
-    public BonusNumber(String number, Lotto winningNumbers) {
-        int bonusNumber = Integer.parseInt(number);
-        validateRange(bonusNumber);
-        validateDuplicatedByWinningNumbers(bonusNumber, winningNumbers);
-        this.number = bonusNumber;
+    public BonusNumber(String number) {
+        validateInteger(number);
+        validateRange(number);
+        //validateDuplicatedByWinningNumbers(bonusNumber, winningNumbers);
+        this.number = Integer.parseInt(number);
     }
 
-    private void validateRange(int bonusNumber) {
+    private void validateInteger(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(BONUS_NUMBER_NOT_INTEGER);
+        }
+    }
+
+    private void validateRange(String number) {
+        int bonusNumber = Integer.parseInt(number);
+        System.out.println(bonusNumber);
         if (bonusNumber < 1 || bonusNumber > 45) {
             throw new IllegalArgumentException(BONUS_NUMBER_OUT_OF_RANGE);
         }
