4ebd475472f9100fd1d2247ffd80d24c7451b34b
CAEL0 <kjongmin26@gmail.com>
2022-11-14 00:11:00 +0900

    refactor(ExceptionConstants): 자연수가 아닌 입력에 대한 예외 통합

--- src/main/java/lotto/constant/ExceptionConstants.java
+++ src/main/java/lotto/constant/ExceptionConstants.java
@@ -4,14 +4,12 @@ import static lotto.constant.GameConstants.*;
 
 public enum ExceptionConstants {
 
-    PURCHASE_AMOUNT_NOT_NATURAL_NUMBER_EXCEPTION("구입금액은 자연수여야 합니다."),
-    PURCHASE_AMOUNT_INDIVISIBILITY_EXCEPTION(String.format("구입금액은 %d의 배수여야 합니다.", LOTTO_PRICE)),
-
-    NOT_NATURAL_NUMBER_EXCEPTION("각 번호는 자연수여야 합니다."),
+    NOT_NATURAL_NUMBER_EXCEPTION("자연수를 입력해야 합니다."),
     RANGE_EXCEPTION(String.format("각 번호는 %d이상 %d이하여야 합니다.", MIN_NUMBER, MAX_NUMBER)),
     DUPLICATED_EXCEPTION("각 번호는 서로 중복되지 않아야 합니다."),
     LEADING_ZERO_EXCEPTION("각 번호는 0으로 시작할 수 없습니다."),
 
+    PURCHASE_AMOUNT_INDIVISIBILITY_EXCEPTION(String.format("구입금액은 %d의 배수여야 합니다.", LOTTO_PRICE)),
     LOTTO_NUMBER_LENGTH_EXCEPTION(String.format("로또 번호는 %d개이어야 합니다.", LOTTO_NUMBER_LENGTH)),
     WINNING_NUMBER_LENGTH_EXCEPTION(String.format("당첨 번호는 %d개이어야 합니다.", WINNING_NUMBER_LENGTH));
 
