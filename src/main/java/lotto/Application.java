68723b6ba77c6b6edf9124500e9e1ce0f2ef6ea1
rohsik2 <rohsik2@gmail.com>
2022-11-13 19:37:20 +0900

    feat: main function and program

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,8 +10,42 @@ import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        String money = Console.readLine();
+        int nLotto = buyTicket(money);
+
+        List<Lotto> tickets = generateLottos(nLotto);
+        printTickets(tickets);
+        System.out.println(nLotto + "개를 구매했습니다.");
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String target = Console.readLine();
+        List<Integer> winningNumbers = getWinningNumbers(target);
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String userInputBonus = Console.readLine();
+        int bonus = convertBonus(userInputBonus, winningNumbers);
+
+        System.out.println("당첨 통계");
+        Stats stat = new Stats(tickets, winningNumbers, bonus);
+        stat.printStatus();
     }
+
+    public static int convertBonus(String userInputBonus, List<Integer> winningNumbers) {
+        try {
+            int bonus = Integer.parseInt(userInputBonus);
+            if (winningNumbers.contains(bonus)) {
+                throw new IllegalArgumentException("[ERROR] 보너스 숫자는 당첨번호와 중복될 수 없습니다.");
+            }
+            if (bonus < 1 || bonus > 45) {
+                throw new IllegalArgumentException("[ERROR] 숫자는 1 ~ 45 중 하나의 숫자여야 합니다.");
+            }
+            return bonus;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 보너스 숫자는 정수여야 합니다.");
+        }
+    }
+
     public static int buyTicket(String money) {
         try {
             int intMoney = Integer.parseInt(money);
@@ -27,6 +61,11 @@ public class Application {
         }
     }
 
+    public static void printTickets(List<Lotto> tickets) {
+        for (Lotto ticket : tickets)
+            System.out.println(ticket);
+    }
+
     public static List<Lotto> generateLottos(int nLotto) {
         List<Lotto> tickets = new ArrayList<>();
         for (int i = 0; i < nLotto; i++) {
@@ -35,6 +74,7 @@ public class Application {
         }
         return tickets;
     }
+
     public static List<Integer> getWinningNumbers(String userInput) throws IllegalArgumentException {
         String[] splitedInputs = userInput.trim().split(",");
         List<Integer> winnings = new ArrayList<>();
