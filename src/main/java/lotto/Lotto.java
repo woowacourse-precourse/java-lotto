039cfb6a9b234d2917cfd1bf4e000ae931c2b49d
yoon <beatus96@naver.com>
2022-11-13 17:04:27 +0900

    fix(Lotto) : 로또 번호 중복 확인 기능 수정
    #1.
    Set.of(numbers).size() == LOTTO_NUMBER; 이 항상 false를 반환
    오류 원인 : Set.of의 사용
    오류 해결: Set.copyOf 사용
    
    Closed #1

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,6 @@
 package lotto;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Lotto {
     private final int LOTTO_NUMBER = 6;
@@ -15,14 +12,49 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        sort(numbers);
+//        sort(numbers);
         this.numbers = numbers;
     }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (!isValidLotto(numbers)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // 에러 원인 : pickUniqueNumbersInRange()메서드의 final List<Integer> numbers = new ArrayList<>(); 바꿀수 없음!!
+    private void sort(List<Integer> numbers) {
+//        numbers.sort(Integer::compare);
+        Collections.sort(numbers);
+    }
+
+    private boolean isCorrectSize(List<Integer> numbers) {
+
+        return numbers.size() == LOTTO_NUMBER;
+    }
+
+    private boolean isNotDuplicated(List<Integer> numbers) {
+        return Set.copyOf(numbers).size() == LOTTO_NUMBER;
+//        return Set.of(numbers).size() == LOTTO_NUMBER;        ?????copyof랑 다른점?>???
+    }
+
+    private boolean isInRange(List<Integer> numbers) {
+        return numbers.stream().allMatch(v -> LOTTO_MIN <= v && v <= LOTTO_MAX);
+    }
+
+    private boolean isValidLotto(List<Integer> numbers) {
+        return isCorrectSize(numbers) && isInRange(numbers) && isNotDuplicated(numbers);
+    }
+    // TODO: 추가 기능 구현
 
     public int calculateMatchCount(List<Integer> winningLotto) {
         int matchCount = 0;
         for(Integer lottoNumber : numbers){
-            if(winningLotto.contains(numbers)){
+            if(winningLotto.contains(lottoNumber)){
                 matchCount++;
             }
         }
@@ -30,6 +62,12 @@ public class Lotto {
     }
 
     //로또 등수 정하기
+    public Map<Integer,String> rankMap = Map.of(
+        6, "FIRST",
+        5,"THIRD",
+        4,"FOURTH",
+        3,"FIFTH"
+    );
     public void setRank(List<Integer> winningLotto, int bonusNumber){
         int matchCount = calculateMatchCount(winningLotto);
         if(matchCount ==5 && numbers.contains(bonusNumber)){
@@ -42,38 +80,4 @@ public class Lotto {
     public String getRank() {
         return rank;
     }
-
-    public Map<Integer,String> rankMap = Map.of(
-        6, "FIRST",
-        5,"THIRD",
-        4,"FOURTH",
-        3,"FIFTH"
-    );
-
-    private void validate(List<Integer> numbers) {
-        if (!isValidLotto(numbers)) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void sort(List<Integer> numbers) {
-        numbers.sort(Integer::compare);
-    }
-
-    private boolean isCorrectSize(List<Integer> numbers) {
-        return numbers.size() == LOTTO_NUMBER;
-    }
-
-    private boolean isNotDuplicated(List<Integer> numbers) {
-        return Set.of(numbers).size() == LOTTO_NUMBER;
-    }
-
-    private boolean isInRange(List<Integer> numbers) {
-        return numbers.stream().allMatch(v -> LOTTO_MIN <= v && v <= LOTTO_MAX);
-    }
-
-    private boolean isValidLotto(List<Integer> numbers) {
-        return isCorrectSize(numbers) && isInRange(numbers) && isNotDuplicated(numbers);
-    }
-    // TODO: 추가 기능 구현
 }
