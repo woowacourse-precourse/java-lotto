234f3782633589ffa281f635ab1fe596dd1d4544
Kayoung <rkdkdudjd@gmail.com>
2022-11-14 00:58:57 +0900

    feat(Lotto): 로또 발행 시 범위를 벗어난 숫자 예외처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,14 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private final int LOTTO_SIZE = 6;
+    private final int MIN_NUMBER = 1;
+    private final int MAX_NUMBER = 45;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -11,10 +16,21 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateInputSize(numbers);
+        validateRangeOfNumbers(numbers);
+    }
+
+    private void validateRangeOfNumbers(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException();
+            }
+        });
+    }
+
+    private void validateInputSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException();
         }
     }
-
-    // TODO: 추가 기능 구현
 }
