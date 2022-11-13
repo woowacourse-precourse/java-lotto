d704ec00f8f17788c41db8d928d696983f5aa473
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-13 19:08:03 +0900

    feat(IputNumber): InputNumber 클래스 선언
    - 클래스 메서드 선언
    - 기능 목록 추가

---
+++ src/main/java/lotto/inputNumber/InputNumber.java
@@ -0,0 +1,25 @@
+package lotto.inputNumber;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputNumber {
+
+    public static List<Integer> createNumber() {
+        return new ArrayList<>();
+    }
+
+    private static List<Integer> inputNumber() {
+        return new ArrayList<>();
+    }
+
+    private static void validate(String numbers) {
+        // try catch
+    }
+
+    private static void printInputMessage() {
+
+    }
+}
