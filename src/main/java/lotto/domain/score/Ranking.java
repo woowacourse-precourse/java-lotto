5362009a9da3eef3fa73e66f3560f28df86582b5
JaehongDev <println@kakao.com>
2022-11-13 19:50:12 +0900

    refactor (Ranking) : 등수의 콤마 표시 방법 변경 및 랭킹스코어를 가지도록 변경

--- src/main/java/lotto/domain/score/Ranking.java
+++ src/main/java/lotto/domain/score/Ranking.java
@@ -1,24 +1,25 @@
 package lotto.domain.score;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lotto.domain.money.Money;
 
 public enum Ranking {
-    NOT_WINNING(0L, List.of(), ""),
-    FIFTH_WINNING(5000L, List.of(new Score(2, 1), new Score(3, 0)), "3개 일치"),
-    FOURTH_WINNING(50000L, List.of(new Score(3, 1), new Score(4, 0)), "4개 일치"),
-    THIRD_WINNING(1_500_000L, List.of(new Score(4, 1), new Score(5, 0)), "5개 일치"),
-    SECOND_WINNING(30_000_000L, List.of(new Score(5, 1)), "5개 일치, 보너스 볼 일치"),
-    FIRST_WINNING(2_000_000_000L, List.of(new Score(6, 0)), "6개 일치");
+    NOT_WINNING(0L, RankingScore.NOTING, "낙첨"),
+    FIFTH_WINNING(5000L, RankingScore.FIFTH_SCORE, "3개 일치"),
+    FOURTH_WINNING(50000L, RankingScore.FOURTH_SCORE, "4개 일치"),
+    THIRD_WINNING(1_500_000L, RankingScore.THIRD_SCORE, "5개 일치"),
+    SECOND_WINNING(30_000_000L, RankingScore.SECOND_SCORE, "5개 일치, 보너스 볼 일치"),
+    FIRST_WINNING(2_000_000_000L, RankingScore.FIRST_SCORE, "6개 일치");
 
     private final Long prize;
-    private final List<Score> score;
+    private final RankingScore score;
     private final String result;
 
-    Ranking(final Long rank, final List<Score> score, final String result) {
+    Ranking(final Long rank, final RankingScore score, final String result) {
         this.prize = rank;
         this.score = score;
         this.result = result;
@@ -26,28 +27,35 @@ public enum Ranking {
 
     public static Ranking calculateScore(Score score) {
         return Arrays.stream(Ranking.values())
-                .filter(ranking -> ranking.score.contains(score))
+                .filter(ranking -> ranking.hasScore(score))
                 .findAny()
                 .orElse(NOT_WINNING);
     }
 
     public static Map<Ranking, Integer> generateList() {
-        Map<Ranking, Integer> list = new LinkedHashMap<>();
-        for (var ranking : Ranking.values()) {
-            if (ranking.prize != 0) {
-                list.put(ranking, 0);
-            }
-        }
-        return list;
+        return Arrays.stream(Ranking.values())
+                .filter(Ranking::isNotNothing)
+                .collect(Collectors.toMap(ranking -> ranking, (ranking -> 0), (x, y) -> y, LinkedHashMap::new));
     }
 
     public String result() {
-        String prize = String.valueOf(this.prize)
-                .replaceAll("\\B(?=(\\d{3})+(?!\\d))", ",");
-        return this.result + " (" + prize + "원) ";
+        var decimalFormat = new DecimalFormat("###,###");
+        var prize = decimalFormat.format(this.prize);
+
+        return String.format("%s (%s원)", result, prize);
     }
 
     public Money calculateProfits(Integer value) {
         return Money.generateMoney(prize * value);
     }
+
+    private boolean isNotNothing() {
+        return !score.equals(RankingScore.NOTING);
+    }
+
+    private boolean hasScore(Score score) {
+        return this.score.contains(score);
+    }
+
+
 }
