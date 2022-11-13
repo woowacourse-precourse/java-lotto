496bf8cbec4ec119d9ef548608a2718db430f6c4
HiiWee <wpdlzhf159@gmail.com>
2022-11-14 04:02:06 +0900

    refactor: 컨트롤러 예외 검증 부분 클래스로 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,49 +2,28 @@ package lotto.controller;
 
 import lotto.constants.message.ExceptionMessage;
 import lotto.service.LottoService;
+import lotto.validator.InputValidator;
 import lotto.views.InputView;
 
 public class LottoController {
-    public static final String ONLY_NUMBER = "^[0-9]*$";
-    public static final int NOTHING = 0;
-    public static final int LOTTO_PRICE = 1000;
-    public static final int ZERO = 0;
-    public static final int FIRST_INDEX = 0;
 
     private final LottoService lottoService = new LottoService();
+    private final InputValidator inputValidator = new InputValidator();
     private final InputView inputView = new InputView();
 
     public void startLottoApplication() {
         String inputPrice = inputView.inputPrice();
-        validateInputPrice(inputPrice);
+        if (!inputValidator.validateInputPrice(inputPrice)) {
+            restartLottoApplication();
+            return;
+        }
         lottoService.buyLottos(Integer.parseInt(inputPrice));
     }
 
-    private void validateInputPrice(String inputPrice) {
-        if (!inputPrice.matches(ONLY_NUMBER)) {
-            throw new IllegalArgumentException(ExceptionMessage.ERROR + ExceptionMessage.NON_NUMERIC_INPUT);
-        }
-        if (isZero(Integer.parseInt(inputPrice))) {
-            throw new IllegalArgumentException(ExceptionMessage.ERROR + ExceptionMessage.ZERO_INPUT);
-        }
-        if (isStartWithZero(inputPrice.charAt(FIRST_INDEX))) {
-            throw new IllegalArgumentException(ExceptionMessage.ERROR + ExceptionMessage.START_WItH_ZERO);
-        }
-        if (!isValidPrice(Integer.parseInt(inputPrice))) {
-            inputView.printInputPriceAgain();
-            startLottoApplication();
-        }
+    private void restartLottoApplication() {
+        inputView.printInputPriceAgain();
+        startLottoApplication();
     }
 
-    private boolean isStartWithZero(char firstNumber) {
-        return Integer.parseInt(String.valueOf(firstNumber)) == ZERO;
-    }
 
-    private boolean isZero(int inputPrice) {
-        return inputPrice == ZERO;
-    }
-
-    private boolean isValidPrice(int inputPrice) {
-        return inputPrice % LOTTO_PRICE == NOTHING;
-    }
 }
---
+++ src/main/java/lotto/validator/InputValidator.java
@@ -0,0 +1,36 @@
+package lotto.validator;
+
+import lotto.constants.message.ExceptionMessage;
+
+public class InputValidator {
+    public static final String ONLY_NUMBER = "^[0-9]*$";
+    public static final int NOTHING = 0;
+    public static final int LOTTO_PRICE = 1000;
+    public static final int ZERO = 0;
+    public static final int FIRST_INDEX = 0;
+
+    public boolean validateInputPrice(String inputPrice) {
+        if (!inputPrice.matches(ONLY_NUMBER)) {
+            throw new IllegalArgumentException(ExceptionMessage.ERROR + ExceptionMessage.NON_NUMERIC_INPUT);
+        }
+        if (isZero(Integer.parseInt(inputPrice))) {
+            throw new IllegalArgumentException(ExceptionMessage.ERROR + ExceptionMessage.ZERO_INPUT);
+        }
+        if (isStartWithZero(inputPrice.charAt(FIRST_INDEX))) {
+            throw new IllegalArgumentException(ExceptionMessage.ERROR + ExceptionMessage.START_WItH_ZERO);
+        }
+        return isValidPrice(Integer.parseInt(inputPrice));
+    }
+
+    private boolean isStartWithZero(char firstNumber) {
+        return Integer.parseInt(String.valueOf(firstNumber)) == ZERO;
+    }
+
+    private boolean isZero(int inputPrice) {
+        return inputPrice == ZERO;
+    }
+
+    private boolean isValidPrice(int inputPrice) {
+        return inputPrice % LOTTO_PRICE == NOTHING;
+    }
+}
