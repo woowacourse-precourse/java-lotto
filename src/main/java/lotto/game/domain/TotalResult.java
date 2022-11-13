b13023199b2e3cfeb12aedb8cf7f801eeba70bb6
seokjin8678 <rnrn8678@naver.com>
2022-11-14 08:05:29 +0900

    refactor: stream 사용하여 코드 간략화

--- src/main/java/lotto/game/domain/TotalResult.java
+++ src/main/java/lotto/game/domain/TotalResult.java
@@ -1,33 +1,24 @@
 package lotto.game.domain;
 
-import java.util.HashMap;
+import static java.util.stream.Collectors.*;
+
 import java.util.List;
 import java.util.Map;
 
 public class TotalResult {
-    private final Map<LottoGrade, Integer> totalWinnings;
+    private final Map<LottoGrade, Long> totalWinnings;
 
-    private TotalResult(Map<LottoGrade, Integer> totalWinnings) {
+    private TotalResult(Map<LottoGrade, Long> totalWinnings) {
         this.totalWinnings = totalWinnings;
     }
 
     public static TotalResult of(List<LottoGrade> grades) {
-        Map<LottoGrade, Integer> totalWinnings = new HashMap<>();
-        for (LottoGrade grade : grades) {
-            addWinning(totalWinnings, grade);
-        }
-        return new TotalResult(totalWinnings);
+        Map<LottoGrade, Long> collect = grades.stream()
+                .collect(groupingBy(grade -> grade, counting()));
+        return new TotalResult(collect);
     }
 
-    private static void addWinning(Map<LottoGrade, Integer> totalWinnings, LottoGrade grade) {
-        if (totalWinnings.containsKey(grade)) {
-            totalWinnings.put(grade, totalWinnings.get(grade) + 1);
-            return;
-        }
-        totalWinnings.put(grade, 1);
-    }
-
-    public Integer getCount(LottoGrade grade) {
-        return totalWinnings.getOrDefault(grade, 0);
+    public Long getCount(LottoGrade grade) {
+        return totalWinnings.getOrDefault(grade, 0L);
     }
 }
