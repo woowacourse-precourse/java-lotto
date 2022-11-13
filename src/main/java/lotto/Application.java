75d159a2e445650d29b515c49f26ebc76eccec69
inwoo <korean5078@gmail.com>
2022-11-13 21:21:01 +0900

    feat : 사용자의 구입 금액 입력하는 함수 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int NumberOfLotto = getNumberOfLotto();
+    }
+
+    public static int getNumberOfLotto() {
+        String input = Console.readLine();
+        int money = 0;
+        if (!input.matches("[+-]?\\d*(\\.\\d+)?"))
+            throw new IllegalArgumentException("[ERROR] 금액을 제대로 입력해주세요.");
+        money = Integer.parseInt(input);
+        return money / 1000;
     }
 }
