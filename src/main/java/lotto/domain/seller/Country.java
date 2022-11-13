2b8568ff57dd08e860f8c6b9bfcf782f7f7dfac9
aksmf1442 <aksmf1442@naver.com>
2022-11-13 20:14:35 +0900

    feat(로또): 돈을 통해서 몇 개의 로또 용지를 구매할 수 있는지 기능 구현

--- src/main/java/lotto/domain/seller/Country.java
+++ src/main/java/lotto/domain/seller/Country.java
@@ -1,9 +1,10 @@
 package lotto.domain.seller;
 
 public class Country implements Seller{
+    private static final int LOTTO_AMOUNT = 1000;
 
     @Override
     public int findLottoCountByMoney(int money) {
-        return 0;
+        return money / LOTTO_AMOUNT;
     }
 }
