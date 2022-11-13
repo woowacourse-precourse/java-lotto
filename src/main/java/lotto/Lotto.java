892e2e79c0306aa39fee7f83836400bd00418ff3
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-13 18:34:37 +0900

    feat(validate): Lotto validate 메서드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,7 +11,13 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (!checkSize(numbers)) {
+            throw new IllegalArgumentException();
+        }
+        if (!checkDuplicate(numbers)) {
+            throw new IllegalArgumentException();
+        }
+        if (!checkRange(numbers)) {
             throw new IllegalArgumentException();
         }
     }
@@ -20,17 +26,17 @@ public class Lotto {
         return numbers;
     }
 
-    private boolean checkSize() {
+    private boolean checkSize(List<Integer> numbers) {
         return numbers.size() == 6;
     }
 
-    private boolean checkDuplicate() {
+    private boolean checkDuplicate(List<Integer> numbers) {
         return numbers.stream()
                 .distinct()
                 .count() == 6;
     }
 
-    private boolean checkRange() {
+    private boolean checkRange(List<Integer> numbers) {
         return numbers.stream()
                 .filter(number -> number >= 1)
                 .filter(number -> number <= 46)
