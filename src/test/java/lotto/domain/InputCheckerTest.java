f56da22b94e0a651c7397bf516692b5d398a1d1b
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-14 01:51:55 +0900

    Test : 추가 입력한 값을 확인하는 함수 테스트
    checkInputMoney 함수 예외처리와 true 반환 확인하는 테스트 추가

--- src/test/java/lotto/domain/InputCheckerTest.java
+++ src/test/java/lotto/domain/InputCheckerTest.java
@@ -15,7 +15,7 @@ class InputCheckerTest{
         inputChecker=new InputChecker();
     }
 
-    @DisplayName("입력한 숫자가 정수가 아닌 경우 예외 발생 확인")
+    @DisplayName("입력한 값이 정수가 아닌 경우 예외 발생 확인")
     @Test
     void isDigitTest(){
         assertThatThrownBy(()->inputChecker.isDigit("1000j"))
@@ -32,5 +32,19 @@ class InputCheckerTest{
     @Test
     void isMultipleThousandTest2(){
         assertThat(inputChecker.isMultipleThousand(1000)).isTrue();
+    }
+
+    @DisplayName("입력한 값이 숫자아니거나 1000으로 나누어떨어지지 않으면 예외 발생하는지 확인")
+    @Test
+    void checkInputMoney1(){
+        assertThatThrownBy(()->inputChecker.checkInputMoney("1004"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->inputChecker.checkInputMoney("hi"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("입력한 값이 숫자이고 1000으로 나누어떨어지면 true를 반환하는지 확인")
+    @Test
+    void checkInputMoney2(){
+        assertThat(inputChecker.checkInputMoney("14000")).isTrue();
     }
 }
