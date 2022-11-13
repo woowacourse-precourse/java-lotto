90e89cb9bf63039a693bcd3c782a4b7f0fc15e82
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-13 21:41:48 +0900

    feat(PrintManager): 출력 담당 클래스 생성

---
+++ src/main/java/lotto/PrintManager.java
@@ -0,0 +1,28 @@
+package lotto;
+
+public class PrintManager {
+    public void printRequestPrice() {
+
+    }
+
+    public void printRequestWinningNumber() {
+
+    }
+
+    public void printRequestBonusNumber() {
+
+    }
+
+    public void printWinningStatus() {
+        printWinningList();
+        printYield();
+    }
+
+    private void printWinningList() {
+
+    }
+
+    private void printYield() {
+
+    }
+}
