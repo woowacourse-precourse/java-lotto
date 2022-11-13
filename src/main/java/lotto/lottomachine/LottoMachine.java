887bbd79ca39135d77151e723710a94b25634535
kimminsu <xjfcnfw3@naver.com>
2022-11-13 12:41:17 +0900

    feat: 로또 머신 로또 종이 생성과 유저 숫자 입력 기능 구현

--- src/main/java/lotto/lottomachine/LottoMachine.java
+++ src/main/java/lotto/lottomachine/LottoMachine.java
@@ -1,6 +1,11 @@
 package lotto.lottomachine;
 
+import java.util.Collections;
+import java.util.List;
+import lotto.domain.validation.LottoNumbersValidation;
+import lotto.lottopaper.LottoPaper;
 import lotto.user.User;
+import lotto.user.UserUtil;
 import lotto.user.validation.UserMoneyValidation;
 
 public class LottoMachine {
@@ -8,13 +13,17 @@ public class LottoMachine {
     private static final int EXCEPTION_NUMBER = -1;
     private static final int INIT_MONEY = 0;
     private int lottoPrice;
+    private boolean machineStatus;
 
     public LottoMachine() {
         lottoPrice = INIT_MONEY;
+        machineStatus = true;
     }
 
     public void start() {
         buyLotto();
+        LottoPaper lottoPaper = GeneratorLottoPaper.generateLottoPaper(lottoPrice);
+        List<Integer> userNumbers = inputUserNumbers();
     }
 
     public void buyLotto() {
@@ -25,6 +34,23 @@ public class LottoMachine {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             lottoPrice = EXCEPTION_NUMBER;
+            machineStatus = false;
         }
+    }
+
+    public List<Integer> inputUserNumbers() {
+        if (!machineStatus) {
+            return Collections.emptyList();
+        }
+        try {
+            String userLottoNumbers = User.inputLottoNumbers();
+            List<Integer> lottoNumbers = UserUtil.convertUserInputToNumbers(userLottoNumbers);
+            LottoNumbersValidation.validateLottoNumbers(lottoNumbers);
+            return lottoNumbers;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            machineStatus = false;
+        }
+        return Collections.emptyList();
     }
 }
