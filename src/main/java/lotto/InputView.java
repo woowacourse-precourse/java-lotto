c908aee6cb81e0e6574585ca4bc87f5527333e71
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-13 23:28:02 +0900

    style: 입출력 메서드를 모두 static으로 수정
    입출력 객체는 프로그램의 시작부터 끝까지 필요하다 판단하여 static으로 수정했다.

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -7,17 +7,17 @@ public class InputView {
     private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public String enterAmount() {
+    public static String enterAmount() {
         System.out.println(INPUT_AMOUNT_MESSAGE);
         return Console.readLine();
     }
 
-    public String enterWinningNumber() {
+    public static String enterWinningNumber() {
         System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
         return Console.readLine();
     }
 
-    public String enterBonusNumber() {
+    public static String enterBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         return Console.readLine();
     }
--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -7,25 +7,25 @@ public class OutputView {
     private static final String START_WINNING_STATICS_MESSAGE = "당첨 통계\n---";
     private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %s%%입니다.";
 
-    public void printNumberOfLottosPurchased(int numberOfLottosPurchased) {
+    public static void printNumberOfLottosPurchased(int numberOfLottosPurchased) {
         System.out.println(numberOfLottosPurchased + NUMBER_OF_LOTTOS_PURCHASED_MESSAGE);
     }
 
-    public void printLottosList(List<Lotto> lottos) {
+    public static void printLottosList(List<Lotto> lottos) {
         for (Lotto lotto: lottos) {
             System.out.println(lotto.getNumbers());
         }
     }
 
-    public void printStartStatics() {
+    public static void printStartStatics() {
         System.out.println(START_WINNING_STATICS_MESSAGE);
     }
 
-    public void printStatics(String message) {
+    public static void printStatics(String message) {
         System.out.println(message);
     }
 
-    public void printRateOfReturn(String rateOfReturn) {
+    public static void printRateOfReturn(String rateOfReturn) {
         System.out.printf(RATE_OF_RETURN_MESSAGE, rateOfReturn);
     }
 }
