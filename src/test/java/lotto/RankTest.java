1e58bf4da1982b9b428c6c908684dc0482eb569e
JungWoo <rlawjddn103@naver.com>
2022-11-13 19:11:00 +0900

    test(RankTest): 맞춘 개수에 따른 순위 반환 테스트 추가

---
+++ src/test/java/lotto/RankTest.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RankTest {
+
+    @Test
+    void getRankMiss() {
+        assertThat(Rank.getRank(1,true)).isEqualTo(Rank.MISS);
+    }
+
+    @Test
+    void getRankThree() {
+        assertThat(Rank.getRank(3,true)).isEqualTo(Rank.WIN_THREE);
+    }
+
+    @Test
+    void getRankFour() {
+        assertThat(Rank.getRank(4,true)).isEqualTo(Rank.WIN_FOUR);
+    }
+
+    @Test
+    void getRankFiveWithBonus() {
+        assertThat(Rank.getRank(5,true)).isEqualTo(Rank.WIN_FIVE_WITH_BONUS5);
+    }
+
+    @Test
+    void getRankFiveWithoutBonus() {
+        assertThat(Rank.getRank(5,false)).isEqualTo(Rank.WIN_FIVE);
+    }
+
+    @Test
+    void getRankSix() {
+        assertThat(Rank.getRank(6,false)).isEqualTo(Rank.WIN_SIX);
+    }
+}
