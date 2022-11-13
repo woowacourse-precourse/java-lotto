fc539568029cb7c0cf3519ad7b365a8d0ab40453
glass <glass7312@gmail.com>
2022-11-13 16:36:21 +0900

    refactor

--- src/main/java/Controller/mainController.java
+++ src/main/java/Controller/mainController.java
@@ -1,16 +1,21 @@
-package domain;
+package Controller;
 
 import Service.gradeService;
+import domain.Grade;
 import lotto.Lotto;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static Service.LottoService.lottos;
+import static domain.Game.bonusNumber;
+import static Controller.GameController.getWinning_numbers;
+import static domain.Grade.getGrade;
 
-public class Controller {
 
-    public static List<gradeService.grade> grades=new ArrayList<>();
+public class mainController {
+
+    public static List<Grade.Lottogrades> grades = new ArrayList<>();
     public static int first_count = 0;
     public static int second_count = 0;
     public static int third_count = 0;
@@ -19,17 +24,17 @@ public class Controller {
 
     public static void setGrades() {
         for (Lotto lotto : lottos) {
-            grades.add(gradeService.getGrade(lotto.get_hitCount(Game.getWinning_numbers()), lotto.bonus_hit(Game.bonusNumber)));
+            grades.add(getGrade(lotto.get_hitCount(getWinning_numbers()), lotto.bonus_hit(bonusNumber)));
         }
     }
 
     public static void grade_count() {
-        for (gradeService.grade grade : grades) {
-            if (gradeService.grade.fifth == grade) fifth_count++;
-            if (gradeService.grade.fourth == grade) fourth_count++;
-            if (gradeService.grade.third == grade) third_count++;
-            if (gradeService.grade.second == grade) second_count++;
-            if (gradeService.grade.first == grade) first_count++;
+        for (Grade.Lottogrades lottograde : grades) {
+            if (lottograde.fifth == lottograde) fifth_count++;
+            if (lottograde.fourth == lottograde) fourth_count++;
+            if (lottograde.third == lottograde) third_count++;
+            if (lottograde.second == lottograde) second_count++;
+            if (lottograde.first == lottograde) first_count++;
         }
     }
 
