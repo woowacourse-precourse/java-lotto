b26d5764ecca23ee0d5da05550c5c67d243a4f77
Kimhyeyun <cqqwer@gmail.com>
2022-11-13 19:01:55 +0900

    Feat: 구매 금액 클래스 생성

--- src/main/java/lotto/domain/PurchasedLottos.java
+++ src/main/java/lotto/domain/PurchasedLottos.java
@@ -1,4 +1,10 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PurchasedLottos {
+    private final List<Lotto> purchasedLottos = new ArrayList<>();
+
+
 }
