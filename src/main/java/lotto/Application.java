9fc23279881d168dca86c2d016ab3afe5a03450c
UJ15 <dbwogml15@naver.com>
2022-11-14 03:32:44 +0900

    feat : add ERROR_PREFIX in Application

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,8 @@
 package lotto;
 
 public class Application {
+    public static final String ERROR_PREFIX = "[ERROR] ";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
