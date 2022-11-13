3aaf4037a63aecc5dcccf637aabf054e5a96090a
CMHyuk <alsgur9710@naver.com>
2022-11-13 13:51:54 +0900

    feat(Validator): 로또 당첨 번호 입력 검증

--- src/main/java/lotto/validation/Validator.java
+++ src/main/java/lotto/validation/Validator.java
@@ -1,13 +1,22 @@
 package lotto.validation;
 
+import java.util.List;
+
 public class Validator {
 
     private static final String FIRST_NUMBER_ZERO = "첫 자리는 0을 입력할 수 없습니다.";
     private static final String PATTERN = "^[0-9]+$";
     private static final String INPUT_NUMBER = "숫자를 입력해주세요.";
     private static final String INPUT_THOUSAND_UNIT = "1000원 단위로 입력해주세요.";
+    private static final String FROM_ONE_TO_FORTY_FIVE = "1부터 45 사이의 숫자여야 합니다.";
+    private static final String DUPLICATION_NUMBER = "중복된 숫자는 입력할 수 없습니다.";
+    private static final String INPUT_SIX_NUMBERS = "숫자 6개를 입력해주세요.";
     private static final int THOUSAND = 1000;
     private static final int ZERO = 0;
+    private static final int LAST_NUMBER = 45;
+    private static final int FIRST_NUMBER = 1;
+    private static final int LOTTO_SIZE = 6;
+
 
     public static void validateInputMoney(String money) {
         isFirstNumberZero(money);
@@ -29,6 +38,32 @@ public class Validator {
     public static void isDividedByThousand(int money) {
         if (money % THOUSAND != ZERO) {
             throw new IllegalArgumentException(INPUT_THOUSAND_UNIT);
+        }
+    }
+
+    public static void validateLottoNumber(List<Integer> lottoNumbers) {
+        isCorrectSize(lottoNumbers);
+        isDuplicatedLottoNumber(lottoNumbers);
+        isCorrectRangeLottoNumber(lottoNumbers);
+    }
+
+    private static void isCorrectRangeLottoNumber(List<Integer> lottoNumbers) {
+        lottoNumbers.forEach(number -> {
+            if (number > LAST_NUMBER || number < FIRST_NUMBER) {
+                throw new IllegalArgumentException(FROM_ONE_TO_FORTY_FIVE);
+            }
+        });
+    }
+
+    private static void isDuplicatedLottoNumber(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != lottoNumbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(DUPLICATION_NUMBER);
+        }
+    }
+
+    private static void isCorrectSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(INPUT_SIX_NUMBERS);
         }
     }
 }
