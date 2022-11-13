71c33259703080c6c16b7c7cb2e692ef961f814a
kwYoohae <gocksdl7979@gmail.com>
2022-11-13 17:49:37 +0900

    fix: 에러처리 방식 변경
    - 문제의 test-case에 따라서 IllegalArgumentException을 발생시키고 ErrorMessage를 로그로 출력을 해야합니다
    - 메세지를 넣어서 IllegalArgumentException을 발생시킵니다.
    - 이후 try-catch로 받아서 IllegalArgumentException에 담긴 메세지를 가져와서 출력합니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,9 +5,11 @@ import lotto.views.Input;
 
 public class Application {
     public static void main(String[] args) {
-
         GameManager gameManager = new GameManager(new Input());
-
-        gameManager.run();
+        try{
+            gameManager.run();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
