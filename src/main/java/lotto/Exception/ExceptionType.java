d60ec4adda54a77414fb209dcdf5648c2be6e8ea
yun <yshyeon102@gmail.com>
2022-11-14 05:09:47 +0900

    feat(ExceptionType): 당첨 로또 입력 시 발생할 수 있는 예외 타입 추가

--- src/main/java/lotto/Exception/ExceptionType.java
+++ src/main/java/lotto/Exception/ExceptionType.java
@@ -3,6 +3,11 @@ package lotto.Exception;
 public enum ExceptionType {
     MONEY_TYPE_EXCEPTION("[ERROR] 돈은 정수값이 입력되어야 합니다."),
     MONEY_VALUE_EXCEPTION("[ERROR] 돈은 1,000원 단위로 입력되어야 합니다."),
+    LOTTO_INPUT_FORMAT_EXCEPTION("[ERROR] 입력 형식은 숫자 6개와 사이에 쉼표를 넣어 입력해야 합니다."),
+    LOTTO_INPUT_COUNT_EXCEPTION("[ERROR] 6개의 숫자가 입력되어야 합닌다."),
+    LOTTO_RANGE_EXCEPTION("[ERROR] 숫자는 1이상 45이하여야 합니다."),
+    LOTTO_DUPLICATE_EXCEPTION("[ERROR] 중복된 숫자는 입력될 수 없습니다."),
+    ;
 
     private final String message;
 
