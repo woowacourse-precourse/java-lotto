b66af44af06e220bddd30ce89e7af767db38d5c1
JaehongDev <println@kakao.com>
2022-11-13 20:43:56 +0900

    refactor (RandomLottoSystem)  : 랜덤 로또 시스템에서 관련 기능을 처리하도록 구현

--- src/main/java/lotto/system/RandomLottoSystem.java
+++ src/main/java/lotto/system/RandomLottoSystem.java
@@ -1,18 +1,35 @@
 package lotto.system;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.lotto.LottoBundle;
+import lotto.domain.money.Money;
 
 public class RandomLottoSystem {
+    private static final Integer START_INCLUSIVE = 1;
+    private static final Integer END_INCLUSIVE = 45;
+    private static final Integer COUNT = 6;
+
     private final IoSystem io;
 
     public RandomLottoSystem(IoSystem ioSystem) {
         this.io = ioSystem;
     }
 
-    public LottoBundle generateLottoBundle(List<List<Integer>> numbers) {
-        io.println(numbers.size() + "개를 구매했습니다.");
+    private List<List<Integer>> generateRandomNumbers(final Long repeatCount) {
+        List<List<Integer>> numbers = new ArrayList<>();
 
+        for (int i = 0; i < repeatCount; i++) {
+            numbers.add(Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT));
+        }
+        return numbers;
+    }
+
+    public LottoBundle generateLottoBundle(Money money) {
+        var numbers = this.generateRandomNumbers(money.calculateAvailablePurchaseCount());
+
+        io.println(numbers.size() + "개를 구매했습니다.");
         var lottoBundle = new LottoBundle(numbers);
 
         io.println(lottoBundle.listUpLotto());
