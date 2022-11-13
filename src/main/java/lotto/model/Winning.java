7600740cce66760a5eb4218892fef155bce33a41
marinesnow34 <m4rinesnow34@gmail.com>
2022-11-13 14:12:03 +0900

    feat: enum 활용 가능하게 변경

--- src/main/java/lotto/model/Winning.java
+++ src/main/java/lotto/model/Winning.java
@@ -1,13 +1,31 @@
 package lotto.model;
 
 public enum Winning {
-    FIRST(2_000_000_000, "1등"),
-    SECOND(30_000_000, "2등"),
-    THIRD(1_500_000, "3등"),
-    FOURTH(50_000, "4등"),
-    FIFTH(5_000, "5등");
+    FIRST(6, 2_000_000_000, "1등"),
+    SECOND(5, 30_000_000, "2등"),
+    THIRD(5, 1_500_000, "3등"),
+    FOURTH(4, 50_000, "4등"),
+    FIFTH(3, 5_000, "5등");
 
+    private final int sameCount;
+    private final int prize;
+    private final String detail;
 
-    Winning(int prize, String detail) {
+    Winning(int sameCount, int prize, String detail) {
+        this.sameCount = sameCount;
+        this.prize = prize;
+        this.detail = detail;
+    }
+
+    public int getSameCount() {
+        return sameCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getDetail() {
+        return detail;
     }
 }
