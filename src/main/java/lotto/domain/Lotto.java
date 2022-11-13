951694fb7c733b820dfe09a95d99d631d10929d3
This2sho <zx00018@naver.com>
2022-11-14 04:57:12 +0900

    feat(Lotto): 로또 번호에 중복된 숫자 유효성 검사 로직 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,10 +14,19 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateOverSize(numbers);
+        validateDuplicate(numbers);
     }
-    
+
     private void validateOverSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> numbersToSet = new HashSet<>(numbers);
+
+        if (numbersToSet.size() != numbers.size()) {
             throw new IllegalArgumentException();
         }
     }
