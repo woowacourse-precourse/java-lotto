815944449a1e18cbda4dbc921fe00b805f617149
anneu <b.imnuj@gmail.com>
2022-11-14 00:12:56 +0900

    feat: 로또 객체를 비교하는 equals, hashcode 기능 오버라이딩

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,7 @@ package lotto;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,6 +15,23 @@ public class Lotto {
 
     public boolean isExistNumber(int number) {
         return numbers.contains(number);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(numbers, lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
     }
 
     @Override
