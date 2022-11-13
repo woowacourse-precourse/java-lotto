710b751e83cbcd8abeb9443579b912c4acddb552
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-13 16:11:39 +0900

    feat(service): getLottoRankResultsByLottos method
    sub methods
    - getLottoHitsCount : get LottoHitsCount By lotto object
    - countSameValueOfLists : count sameValues of two lists

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -28,4 +28,41 @@ public class LottoService {
 
         return lotto;
     }
+
+    /**
+     * Lotto 리스트를 파라미터로 받아 등수 정보를 정수 리스트로 반환
+     * @param lottos : 등수 정보 리스트로 변환할 로또 리스트
+     * @return 각 등수 별 당첨 개수
+     */
+    public List<Integer> getLottoRankResultsByLottos(List<Lotto> lottos) {
+        List<Integer> lottoRankResults = new ArrayList<>(
+                Collections.nCopies(LottoRank.values().length-1, 0)
+        );
+        for (Lotto lotto : lottos){
+            LottoHitsCountVO lottoHitsCount = getLottoHitsCount(lotto);
+            int ordinal = LottoRank.findByLottoHitsCountVO(lottoHitsCount).ordinal();
+            if (ordinal != LottoRank.NOTHING.ordinal()){
+                lottoRankResults.set(ordinal, lottoRankResults.get(ordinal)+1);
+            }
+        }
+
+        return lottoRankResults;
+    }
+
+    private LottoHitsCountVO getLottoHitsCount(Lotto lotto){
+        List<Integer> lottoNumbers = lotto.getNumbers();
+        int hitsCount = countSameValueOfLists(this.winningNumbers, lottoNumbers);
+        boolean hitsBonus = lottoNumbers.contains(this.bonusNumber);
+
+        return new LottoHitsCountVO(hitsCount, hitsBonus);
+    }
+
+    private int countSameValueOfLists(List<Integer> former, List<Integer> latter){
+        Set uniqueFormerNumbers = new HashSet(former);
+        Set uniqueLetterNumbers = new HashSet(latter);
+        uniqueFormerNumbers.retainAll(uniqueLetterNumbers);
+
+        return uniqueFormerNumbers.size();
+    }
+
 }
