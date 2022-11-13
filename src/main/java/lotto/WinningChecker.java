2f9c5c54dad8f26b401c9bba25911ac6a17c0edc
이형우 <dlguddn3024@gmail.com>
2022-11-13 19:00:35 +0900

    Feature: 당첨 여뷰와 등수를  확인하는WinnigChecker 클래스 구현

--- src/main/java/lotto/WinningChecker.java
+++ src/main/java/lotto/WinningChecker.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class WinningChecker {
 
@@ -20,5 +21,32 @@ public class WinningChecker {
         }
 
         return count;
+    }
+
+    private Optional<Winning> checkPrize(List<Integer> lotto, List<Integer> winnigNumber, int bonusNumber) {
+        int checkWinning = checkDuplicatedNumber(lotto, winnigNumber, bonusNumber);
+
+        if (checkWinning == 3) {
+            return Optional.of(Winning.FIFTH);
+        }
+        if (checkWinning == 4) {
+            return Winning.FOURTH;
+        }
+        if (checkWinning == 5) {
+            return Winning.THIRD;
+        }
+        if (checkWinning == 6 && lotto.contains(bonusNumber)) {
+            return Winning.SECOND;
+        }
+        if (checkWinning == 6 && !lotto.contains(bonusNumber)) {
+            return Winning.FIRST;
+        }
+
+        Optional<Winning> optionalWinning = !!!!!;
+        if (optionalWinning.isPresent()) {
+            Winning winning = optionalWinning.get();
+        }
+
+        return Optional.empty();
     }
 }
