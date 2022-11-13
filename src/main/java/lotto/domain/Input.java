02b1e35997ff290592e4d2353b2e6fa7ed0343f3
ONE <sodlapdlf13@gmail.com>
2022-11-13 15:19:01 +0900

    feat(lotto): 구매 금액 입력에 대한 예외처리 추가

--- src/main/java/lotto/domain/Input.java
+++ src/main/java/lotto/domain/Input.java
@@ -6,6 +6,27 @@ public class Input {
 
     public static int purchaseAmount() {
         String amount = Console.readLine();
+
+        if (!isValidAmount(amount)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 구매 금액 입니다.");
+        }
+
         return Integer.parseInt(amount);
+    }
+
+    private static boolean isValidAmount(String number) {
+        return isNumeric(number) && isPositive(number) && isDividedBy1000(number);
+    }
+
+    private static boolean isNumeric(String number) {
+        return number.chars().allMatch(Character::isDigit);
+    }
+
+    private static boolean isPositive(String number) {
+        return Integer.parseInt(number) > 0;
+    }
+
+    private static boolean isDividedBy1000(String number) {
+        return Integer.parseInt(number) % 1000 == 0;
     }
 }
