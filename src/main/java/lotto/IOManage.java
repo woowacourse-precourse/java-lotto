2e483a2f19fe665efc4445640b6160ea1c1a88d2
ckn1070 <ckn1070@naver.com>
2022-11-14 03:23:36 +0900

    feat(lotto): add feature of output the generated lotto numbers

--- src/main/java/lotto/IOManage.java
+++ src/main/java/lotto/IOManage.java
@@ -3,16 +3,33 @@ package lotto;
 import lotto.ControlLottoGame;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class IOManage {
+    private int inputPrice;
+    private List<List<Integer>> generatedLotto;
 
     public void IOManage() {
     }
 
-    public int inputPayment() {
+    public void inputPayment() {
         System.out.println(Message.INPUT_PAYMENT.get());
         String input = Console.readLine();
         int inputPrice = Integer.parseInt(input);
 
-        return inputPrice;
+        this.inputPrice = inputPrice;
+    }
+
+    public void outputLottoGeneration(int inputPrice) {
+        ControlLottoGame control = new ControlLottoGame();
+        int quantity = control.priceToQuantity(inputPrice);
+        GenerateLotto generation = new GenerateLotto(quantity);
+        List<List<Integer>> lottoNumbers = generation.getLottoNumbers();
+        this.generatedLotto = lottoNumbers;
+
+        System.out.println(quantity + Message.OUTPUT_BUYING.get());
+        for (int i = 0; i < lottoNumbers.size(); i++) {
+            System.out.println(lottoNumbers.get(i));
+        }
     }
 }
