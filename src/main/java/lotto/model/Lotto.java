424dec45309b2166dafb4a9d8671fdfa9d34b42a
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2022-11-13 20:18:39 +0900

    feat(Lotto): add print message when size of lotto number is not six

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,6 +3,9 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
+    private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final String NUMBER_IS_NOT_SIX = "로또가 6개보다 많거나 적은 수의 번호를 가진 채 발행됐습니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -15,8 +18,12 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_PREFIX + NUMBER_IS_NOT_SIX);
         }
     }
 }
