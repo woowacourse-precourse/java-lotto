49e85697dea207ca5ea4d2bfd6e51050f69f02ee
jinny-l <jinny.l.dev@gmail.com>
2022-11-13 20:40:29 +0900

    refactor(Service): 로또 게임 로직을 담당하는 객체로 변경
    - 참고:
    기존에 담당했던 다른 메소드는 Controller에서 담당 예정

--- src/main/java/lotto/model/Service.java
+++ src/main/java/lotto/model/Service.java
@@ -5,71 +5,29 @@ import java.util.Arrays;
 import java.util.List;
 import lotto.constant.Constant;
 import lotto.util.Convertor;
-import lotto.util.LottoGenerator;
-import lotto.util.PlayerInput;
 import lotto.util.WinnerInfo;
 
 /*
- * 로또 게임 로직을 담당하는 클래스
- * 1. 구입 금액에 따른 로또 구매 개수 리턴
- * 2. 구매 개수만큼 로또(보너스 번호 포함) 발행
- * 3. 로또 당첨 결과 리턴
- * 4. 로또 수익률 리턴
+ * 로또 게임 로직을 담당하는 객체
+ * : 로또 당첨 결과 리턴
  */
 
 public class Service {
 
-    public static int getMoney() {
-        Money money = new Money(PlayerInput.getInteger());
-        return money.getMoney();
+    private final int lottoAmount;
+    private final List<List<Integer>> lottoNumbers;
+    private final List<Integer> playerLottoNumbers;
+    private final int playerBonusNumber;
+
+    public Service(int lottoAmount, List<List<Integer>> lottoNumbers, List<Integer> playerLottoNumbers,
+                   int playerBonusNumber) {
+        this.lottoAmount = lottoAmount;
+        this.lottoNumbers = lottoNumbers;
+        this.playerLottoNumbers = playerLottoNumbers;
+        this.playerBonusNumber = playerBonusNumber;
     }
 
-    public static int getLottoAmount(int money) {
-        return money / Constant.LOTTO_PRICE;
-    }
-
-    public static List<List<Integer>> getLottoNumbers(int lottoAmount) {
-        List<List<Integer>> lottoNumbers = new ArrayList<>();
-
-        for (int i = 0; i < lottoAmount; i++) {
-            Lotto lotto = new Lotto(LottoGenerator.generateLottoNumbers());
-            lottoNumbers.add(lotto.getNumbers());
-        }
-        return lottoNumbers;
-    }
-
-    public static List<Integer> getBonusNumber(int lottoAmount) {
-        List<Integer> bonusNumbers = new ArrayList<>();
-
-        for (int i = 0; i < lottoAmount; i++) {
-            BonusNumber bonusNumber = new BonusNumber(LottoGenerator.generateBonusNumber());
-            bonusNumbers.add(bonusNumber.getNumber());
-        }
-        return bonusNumbers;
-    }
-
-    public static List<Integer> getWinningLottoNumber() {
-        Lotto lotto = new Lotto(PlayerInput.getLottoNumbers());
-        return lotto.getNumbers();
-    }
-
-    public static int getWinningBonusNumber() {
-        BonusNumber bonusNumber = new BonusNumber(PlayerInput.getInteger());
-        return bonusNumber.getNumber();
-    }
-
-    public static double getProfitRates(int money, int[] lottoResult) {
-        double winningAmount = 0;
-
-        for (int i = 0; i < lottoResult.length; i++) {
-            winningAmount += lottoResult[i] * WinnerInfo.values()[i].getPrizeInformation();
-        }
-        return (winningAmount / money) * 100;
-    }
-    
-    public static int[] getLottoResult(
-            int lottoAmount, List<List<Integer>> lottoNumbers, List<Integer> playerLottoNumbers,
-            int playerBonusNumber) {
+    public int[] getLottoResult() {
 
         int[] result = new int[Constant.WINNER_NUMBER];
         Arrays.fill(result, 0);
@@ -86,7 +44,7 @@ public class Service {
         return result;
     }
 
-    private static int checkRank(List<Integer> lottoNumbers, List<Integer> playerLottoNumbers, int playerBonusNumber) {
+    private int checkRank(List<Integer> lottoNumbers, List<Integer> playerLottoNumbers, int playerBonusNumber) {
         int matchedCount = getMatchedCount(lottoNumbers, playerLottoNumbers);
 
         for (int i = 0; i < WinnerInfo.values().length; i++) {
@@ -101,8 +59,8 @@ public class Service {
         return -1; // Rank 승리 조건과 일치하는 것이 없으면 -1 반환
     }
 
-    private static int checkBonusNumber(List<Integer> lottoNumbers, List<Integer> playerLottoNumbers,
-                                        int playerBonusNumber) {
+    private int checkBonusNumber(List<Integer> lottoNumbers, List<Integer> playerLottoNumbers,
+                                 int playerBonusNumber) {
         List<Integer> checkBonusNumber = new ArrayList<>(playerLottoNumbers);
         checkBonusNumber.removeAll(lottoNumbers);
 
@@ -112,7 +70,7 @@ public class Service {
         return WinnerInfo.RANK3.ordinal();
     }
 
-    private static int getMatchedCount(List<Integer> lottoNumbers, List<Integer> playerLottoNumbers) {
+    private int getMatchedCount(List<Integer> lottoNumbers, List<Integer> playerLottoNumbers) {
         List<Integer> intersection = new ArrayList<>(playerLottoNumbers);
         intersection.retainAll(lottoNumbers);
 
