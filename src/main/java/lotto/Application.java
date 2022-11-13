97d129cb40bcf21fdb5b9faf9e041c6a0c53c511
yoonjoonseok <yjsygm@naver.com>
2022-11-13 15:40:35 +0900

    사용자로부터 구입 금액을 입력 받는다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
+    	String money = getMoneyFromUser();
         // TODO: 프로그램 구현
+    }
+    
+    public static String getMoneyFromUser() {
+    	String result = Console.readLine();
+    	return result;
     }
 }
