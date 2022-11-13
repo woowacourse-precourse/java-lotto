21dbb43f7bd6918e6d4ccc2324d44fa4b9bb9844
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-14 00:05:58 +0900

    feat: 입력값 숫자 판단 기능 구현
    입력값이 숫자로만 이루어져 있지 않다면 IllegalArgumentException 발생시키는 API 개발

--- src/main/java/lotto/util/BonusNumberValidator.java
+++ src/main/java/lotto/util/BonusNumberValidator.java
@@ -4,11 +4,15 @@ import org.junit.platform.commons.util.StringUtils;
 
 public class BonusNumberValidator implements Validator {
     private final String EMPTY_ERROR = "[ERROR] 보너스 번호를 입력해주세요.";
+    private final String NUMERIC_ERROR = "[ERROR] 숫자만 입력해주세요.";
 
     @Override
     public void validate(String input) {
         if (StringUtils.isBlank(input)) {
             throw new IllegalArgumentException(EMPTY_ERROR);
+        }
+        if (!input.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException(NUMERIC_ERROR);
         }
     }
 }
