4aa2b0ca9f5fe5789fb41129069118f8df41c3e2
이정우 <acg6138@naver.com>
2022-11-13 20:05:12 +0900

    feat(Validate): 리스트 단위로 번호의 범위를 검증하는 기능 추가

--- src/main/java/lotto/domain/Validate.java
+++ src/main/java/lotto/domain/Validate.java
@@ -9,6 +9,12 @@ public class Validate {
         return 1 <= number && number <= 45;
     }
 
+    public static boolean isInRangeAll(List<Integer> nums) {
+        return nums.stream()
+                .filter(Validate::isInRange)
+                .count() == LOTTO_NUMS_SIZE;
+    }
+
     public static boolean isNotContains(List<Integer> nums, int num) {
         return !nums.contains(num);
     }
