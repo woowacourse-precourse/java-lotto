66c55f2e8c0d220a6bca6306d0d995dc9f4b4f83
Namyunsuk <yunsuk0328@naver.com>
2022-11-13 22:58:43 +0900

    refactor(LottoNumberGeneration): Lotto클래스로 기능 통합 및 삭제

--- src/main/java/lotto/mainframe/Lotto.java
+++ src/main/java/lotto/mainframe/Lotto.java
@@ -1,8 +1,10 @@
 package lotto.mainframe;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,5 +18,7 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static List<Integer> generateLotto() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
 }
