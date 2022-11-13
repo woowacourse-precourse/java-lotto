ad7a231f78b40fdfff70fd88dd133ccd5d933090
fakerdeft <whakswp123@naver.com>
2022-11-14 03:14:29 +0900

    feat(countPurchaseQuantity): add 구매 수량 메서드 추가

--- src/main/java/lotto/domain/Cash.java
+++ src/main/java/lotto/domain/Cash.java
@@ -3,9 +3,26 @@ package lotto.domain;
 import lotto.Exceptions;
 
 public class Cash {
-    public static int countPurchaseQuantity(String readLine){
+    public static int countPurchaseQuantity(String readLine) {
         Exceptions.excludeUnderThousand(readLine);
         return Integer.parseInt(readLine) / 1000;
     }
 
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
