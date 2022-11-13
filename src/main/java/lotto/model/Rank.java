a7489db2b99536dcbcdd151f433d71a4d5729456
SHEOMM <esh0821@snu.ac.kr>
2022-11-13 16:36:17 +0900

    feat(Rank): 보너스 타입과 매칭된 숫자에 따라 랭크 반환하는 함수 구현

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,12 +1,15 @@
 package lotto.model;
 
+import java.util.Arrays;
+
 public enum Rank {
 
     FIRST_RANK(6, Bonus.FALSE, 2000000000),
     SECOND_RANK(5, Bonus.TRUE, 30000000),
     THIRD_RANK(5, Bonus.FALSE, 1500000),
     FOURTH_RANK(4, Bonus.FALSE, 50000),
-    FIFTH_RANK(3, Bonus.FALSE, 5000);
+    FIFTH_RANK(3, Bonus.FALSE, 5000),
+    NOT_WINNER(-1, Bonus.FALSE, 0);
 
     private int matchedNumber;
     private Bonus bonus;
@@ -16,6 +19,14 @@ public enum Rank {
         this.matchedNumber = matchedNumber;
         this.bonus = bonus;
         this.price = price;
+    }
+
+    public static Rank getRank(int matchedNumber, Bonus bonus){
+        return Arrays.stream(Rank.values())
+                .filter( it -> (it.matchedNumber == matchedNumber)
+                                    && it.bonus == bonus)
+                .findFirst()
+                .orElse(NOT_WINNER);
     }
 
     public int getMatchedNumber() {
