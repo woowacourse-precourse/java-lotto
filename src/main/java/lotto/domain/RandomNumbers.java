3c26f9d213117886752cd633f4fdb3eec6af2d79
parkjungyoon <tbr06057@naver.com>
2022-11-14 01:45:05 +0900

    fix(Domain): 불변 컬렉션 수정하는 코드 오류 수정 (새로운 리스트 생성)

--- src/main/java/lotto/domain/RandomNumbers.java
+++ src/main/java/lotto/domain/RandomNumbers.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,7 +11,8 @@ public class RandomNumbers {
 
     public List<Integer> generateRandomNumbers() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, LOTTO_COUNT);
-        Collections.sort(lottoNumbers);
-        return lottoNumbers;
+        List<Integer> modifiableLottoNumbers = new ArrayList<Integer>(lottoNumbers);
+        Collections.sort(modifiableLottoNumbers);
+        return modifiableLottoNumbers;
     }
 }
