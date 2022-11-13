c31e6b918cdab6721d60835777deb8365e8d2058
daeun <gain3436@gmail.com>
2022-11-13 17:20:26 +0900

    feat : 로또 번호 오름차순 정렬 기능 추가

--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -2,10 +2,10 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
-import lotto.view.InputView;
 import lotto.view.OutputView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoGameService {
@@ -16,9 +16,14 @@ public class LottoGameService {
         for (int i=0; i<ticketCnt; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             lottoList.add(new Lotto(numbers));
-            outputView.printLottoNumbers(numbers);
+            outputView.printLottoNumbers(sortAscending(numbers));
         }
         return lottoList;
+    }
+
+    private List<Integer> sortAscending(List<Integer> numbers) {
+        Collections.sort(numbers);
+        return numbers;
     }
 
 }
