ab1f93f7fe29e4141b63430df7fbded9366cdfd5
skydreamer21 <skydreamer210@gmail.com>
2022-11-14 01:38:38 +0900

    feat(OutputView): 구매한 로또의 정보를 출력하는 기능 구현
    [세부사항]
    - 출력 요소 및 형식
    1. 구매 수량 출력
    2. 각 로또의 번호는 오름차순으로 출력
    
    - 구현 메서드 : showPurchaseList, showLotteryNumbers

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,25 @@
+package lotto.view;
+
+import lotto.view.constant.PurchasePhrase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OutputView {
+    static final String PURCHASE_PHRASE = PurchasePhrase.PURCHASE.getPhrase();
+    static final String LOTTERY_NUMBER_PHRASE = PurchasePhrase.LOTTERY_NUMBER.getPhrase();
+
+    public static void showPurchaseList(int purchaseCount, List<List<Integer>> lotteryTickets) {
+        System.out.println(String.format(PURCHASE_PHRASE, purchaseCount));
+        for (List<Integer> lotteryTicket : lotteryTickets) {
+            showLotteryNumbers(lotteryTicket);
+        }
+    }
+
+    private static void showLotteryNumbers (List<Integer> lotteryTicket) {
+        List<Integer> lotteryNumbers = new ArrayList<>(lotteryTicket);
+        Collections.sort(lotteryNumbers);
+        System.out.println(String.format(LOTTERY_NUMBER_PHRASE, lotteryNumbers.toArray(new Integer[0])));
+    }
+}
