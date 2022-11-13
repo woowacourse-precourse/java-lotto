e9f16c1be41e2d08b019bf1e1ef4c02daa0b1807
lee2963 <sodel2963@naver.com>
2022-11-13 21:16:58 +0900

    Test: 당첨 번호가 유의미한 숫자(1~45)가 아닐시 에러 발생 테스트

--- src/test/java/lotto/LotteryMachineTest.java
+++ src/test/java/lotto/LotteryMachineTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LotteryMachineTest {
@@ -20,6 +21,16 @@ class LotteryMachineTest {
         lotteryMachine.makeWinningLottoNum(splitWinningNum);
         Assertions.assertThat(lotteryMachine.getLotto().getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
 
+    }
+
+    @DisplayName("당첨 번호가 유의미한 숫자(1~45)가 아닐시 에러 발생")
+    @Test
+    void createWinningLottoNumByNotCorrectNum() {
+        String inputWinningNum = "1,2,3,4,5,0";
+        LotteryMachine lotteryMachine = new LotteryMachine();
+        String[] splitWinningNum = inputWinningNum.split(",");
+        assertThatThrownBy(() -> lotteryMachine.makeWinningLottoNum(splitWinningNum))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
