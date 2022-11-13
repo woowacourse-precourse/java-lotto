63c676b4fcf5138396d01d265bb6b570a9e8ffe0
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-13 12:25:59 +0900

    feat: 기능구현 목록 3번 3.2 입력받은 입력받은 수가 6자리가 아니거나, 중복된 수 입력, 1~45까지의 수가 아니라면 예외발생

--- src/main/java/lotto/logic/WinningNumberInput.java
+++ src/main/java/lotto/logic/WinningNumberInput.java
@@ -1,12 +1,41 @@
 package lotto.logic;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.ExceptionValidation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class WinningNumberInput {
+    private List<Integer> WinningNum;
 
-    public String inputWinningNum(){
+    private String inputWinningNum() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String winningnum = Console.readLine();
         return winningnum;
+    }
+
+    private List<String> dividecomma() {
+        String input = inputWinningNum();
+        String[] result = input.split(",");
+        List<String> winnninginput = new ArrayList<>(Arrays.asList(result));
+        return winnninginput;
+    }
+
+    private List<Integer> convertoInt() {
+        WinningNum = new ArrayList<>();
+        List<String> Stringinput = dividecomma();
+        for (int i = 0; i < Stringinput.size(); i++) {
+            int s = Integer.parseInt(Stringinput.get(i));
+            WinningNum.add(s);
+        }
+        return WinningNum;
+    }
+
+    public List<Integer> getWinningNum() {
+        List<Integer> winningnum = convertoInt();
+        ExceptionValidation.inputException(winningnum);
+        return WinningNum;
     }
 }
