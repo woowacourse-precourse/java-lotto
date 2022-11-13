f88dd0fed9d0a3391b5bb17afddd562598c24bf9
User <uoon97@hanmil.net>
2022-11-13 21:36:17 +0900

    발행번호 추출, 당첨 통계 기록

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+/* lotto class를 사용해 구현 */
+/* java enum을 사용할 것 */
+/* else를 쓰지말 것. */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,11 @@
 package lotto;
 
-import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
 
 public class Lotto {
+    //final은 값이 변하지 않는다. 객체를 생성하고 Lotto를 호출했을 때 넣은 numbers 그대로 유지하게 된다.
     private final List<Integer> numbers;
-
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
@@ -17,4 +18,43 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    // 당첨 통계 생성
+    public int[] lottoStatistics(int bonusNumber, List<HashSet<Integer>> lottoNumbers){
+        int[] statistics = new int[6];
+        HashSet winningNumbers = winningNumber(bonusNumber);
+        validateNumberRange(winningNumbers);
+        for(HashSet autoLotto: lottoNumbers){
+            autoLotto.retainAll(winningNumbers);
+            if (autoLotto.size() >= 3){
+                statistics[autoLotto.size()] += 1;
+            }
+        }
+        return statistics;
+    }
+
+    // 로또 자동번호 생성
+    public List<HashSet<Integer>> lottoNumber(int numberOfPurchases){
+        List<HashSet<Integer>> lottoNumbers = new ArrayList<>();
+        for(int i = 0; i < numberOfPurchases; i++){
+            lottoNumbers.add((HashSet<Integer>) Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            System.out.println(lottoNumbers.get(lottoNumbers.size()-1));
+        }
+        return lottoNumbers;
+    }
+    // 보너스 번호 포함 당첨번호 생성
+    private HashSet winningNumber(int bonusNumber){
+        HashSet<Integer> winningNumbers = new HashSet<>(numbers);
+        winningNumbers.add(bonusNumber);
+        return winningNumbers;
+    }
+
+    // 보너스 번호 포함 당첨번호 범위 확인
+    private void validateNumberRange(HashSet<Integer> winningNumbers) {
+        winningNumbers.stream().filter(n -> n > 45 || n < 0).findAny()
+                .ifPresent(m -> {
+                    throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                });
+    }
+
+
 }
