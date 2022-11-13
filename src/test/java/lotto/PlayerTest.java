790e4e00da922eea56619ccdc8567d50ff059b50
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-13 20:30:29 +0900

    test(PlayerTest): 입력값에 따른 로또 발행 수량 테스트 코드 작성

--- src/test/java/lotto/PlayerTest.java
+++ src/test/java/lotto/PlayerTest.java
@@ -4,12 +4,19 @@ import lotto.domain.Player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayerTest {
-    @DisplayName("로또 구입 금액은 1,000원으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @DisplayName("구입 금액에 따른 로또 발행 수량 구하기")
     @Test
-    void createLottoByOverSize() {
+    void createLottoCount() {
+        assertThat(new Player(8000).getLottoCount())
+                .isEqualTo(8);
+    }
+    @DisplayName("구입 금액이 1,000원으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void createPlayerByIndivisibleWonUnit() {
         assertThatThrownBy(() -> new Player(1001))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 구입 금액은 1,000원 단위만 가능합니다.");
