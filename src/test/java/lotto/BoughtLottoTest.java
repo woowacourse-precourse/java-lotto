eb88b3dff6c13916b6bad29616fe9e59f01d7702
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-13 17:50:38 +0900

    test(BoughtLotto): BoughtLotto 클래스 test 코드 작성
    - 주어진 로또의 랜덤번호 형식에 맞게 로또번호가 생성되었는지 확인

--- src/test/java/lotto/BoughtLottoTest.java
+++ src/test/java/lotto/BoughtLottoTest.java
@@ -1,2 +1,22 @@
-package lotto;public class BoughtLottoTest {
+package lotto;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.User.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BoughtLottoTest {
+
+    @DisplayName("생성된 랜덤 변수 내에 중복값이 있거나 1부터 46 사이의 자연수가 아니거나, 6개 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void BoughtLottoTest() {
+        new BoughtLotto();
+    }
+
 }
