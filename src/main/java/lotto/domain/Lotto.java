cb87dfed48ed2cd3dac71f8b530f2d631d8c824e
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-14 03:13:44 +0900

    Feat : 추가 로또 번호 범위 예외 처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,19 +2,29 @@ package lotto.domain;
 
 import lotto.ErrorMessage;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(ErrorMessage.LOTTO_SIZE_ERROR.print());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers){
+        for(int i=0; i<numbers.size(); i++){
+            if(numbers.get(i)>=1 && numbers.get(i)<=45){
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_RANGE_ERROR.print());
+            }
         }
     }
 }
