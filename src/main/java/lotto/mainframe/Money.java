c4214017b4d3c8e0d4bdecedd729015ac7a2f6dc
Namyunsuk <yunsuk0328@naver.com>
2022-11-13 19:11:55 +0900

    feat(Money): 상금 액수를 상수로 관리하는 기능 구현

--- src/main/java/lotto/mainframe/Money.java
+++ src/main/java/lotto/mainframe/Money.java
@@ -1,4 +1,20 @@
 package lotto.mainframe;
 
 public enum Money {
+
+    FIRST_PRIZE("2,000,000,000원"),
+    SECOND_PRIZE("30,000,000원"),
+    THIRD_PRIZE("1,500,000원"),
+    FOURTH_PRIZE("50,000원"),
+    FIFTH_PRIZE("5,000원");
+
+    public String money;
+
+    Money(String money) {
+        this.money = money;
+    }
+
+    public String getMoney() {
+        return money;
+    }
 }
