5cc26ac4ceb0ce6e7b17e6415f2962880dcde759
NoseSSG <dmlcks1998@naver.com>
2022-11-13 23:17:58 +0900

    [REFACTOR] 결과출력함수 정리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -133,11 +133,16 @@ public class Application {
         return price;
     }
     static void write_result(List<Integer> count_rank){
+        String output_temp;
+        int index_temp;
+        int count_temp;
         System.out.println("당첨 통계");
         System.out.println("---");
         for(RankType r : RankType.values()){
-            System.out.print(r.getResult() + " - "+count_rank.get(r.getCheck()) +"개\n");
-//            System.out.println(" - "+count_rank.get(r.getCheck()) +"개");
+            output_temp = r.getResult();
+            index_temp = r.getCheck();
+            count_temp = count_rank.get(index_temp);
+            System.out.print(output_temp + " - " + count_temp +"개\n");
         }
     }
     static void calculate_price(int price,int money){
