0c4bd926a0a6d9f84bd6241154696a425c16cbc7
bearjun05 <bearjun05@naver.com>
2022-11-14 01:36:31 +0900

    feat(Lotto) : 구입금액 입력 & 로또 구매 번호 출력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.LotteryShop;
+import lotto.domain.Lotto;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int totalPayment = Integer.parseInt(readLine());
+
+        LotteryShop lotteryShop = new LotteryShop();
+        int totalLottoCnt = lotteryShop.countLotto(totalPayment);
+        List<Lotto> lottos = new ArrayList<>();
+
+        System.out.println(totalLottoCnt + "개를 구매했습니다.");
+        for (int i = 0; i < totalLottoCnt; i++) {
+            List<Integer> lottoNum = lotteryShop.makeLotto();
+            lottos.add(new Lotto(lottoNum));
+            System.out.println(lottoNum);
+        }
     }
 }
