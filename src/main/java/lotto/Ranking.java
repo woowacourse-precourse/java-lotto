9d2058b3438e8b268c14a84007abaffc05ef5895
jjdone <ehdlfaos@gmail.com>
2022-11-13 16:31:52 +0900

    feat: Ranking 클래스 생성
    enum 클래스

---
+++ src/main/java/lotto/Ranking.java
@@ -0,0 +1,19 @@
+package lotto;
+
+public enum Ranking {
+    FIRST(2_000_000_000, 6, false),
+    SECOND(30_000_000, 5, true),
+    THIRD(1_500_000, 5, false),
+    FOURTH(50_000, 4, false),
+    FIFTH(5_000, 3, false);
+
+    private int prize;
+    private int count;
+    private boolean hasBonusNumber;
+
+    Ranking(int prize, int count, boolean hasBonusNumber) {
+        this.prize = prize;
+        this.count = count;
+        this.hasBonusNumber = hasBonusNumber;
+    }
+}
