8b539b621cf8225683dd67268e8e03af029a963b
math0917 <math0917@naver.com>
2022-11-13 20:19:44 +0900

    test(UserLottoService): 수정 테스트

--- src/test/java/lotto/service/UserLottoServiceTest.java
+++ src/test/java/lotto/service/UserLottoServiceTest.java
@@ -11,13 +11,10 @@ import org.junit.jupiter.api.Test;
 
 class UserLottoServiceTest {
 
-//    @DisplayName("money에 따라서 생성되는 리스트의 개수가 적절한지 확인한다.")
-//    @Test
-//    void createUserLotto() {
-//        UserLottoService userLottoService = new UserLottoService(
-//                String.valueOf(IntConstant.LOTTO_MONEY_PER_ONE.getValue() * 10));
-//
-//        List<Lotto> lottoNumber = userLottoService.createLottoNumber();
-//        Assertions.assertThat(lottoNumber.size()).isEqualTo(10);
-//    }
+    @DisplayName("money에 따라서 생성되는 리스트의 개수가 적절한지 확인한다.")
+    @Test
+    void createUserLotto() {
+        List<Lotto> lottoNumber = UserLottoService.createLottoNumber( 10);
+        Assertions.assertThat(lottoNumber.size()).isEqualTo(10);
+    }
 }
