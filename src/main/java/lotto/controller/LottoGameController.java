101844e8c6e0963f5e501f12b95cd4f5a18d4a42
jihun-24k <gi020477@gmail.com>
2022-11-13 22:58:25 +0900

    Refactor: 로또게임 도메인 로직 담당 서비스 클래스 추가

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,9 +1,17 @@
 package lotto.controller;
 
+import lotto.service.LottoGameService;
+
 public class LottoGameController {
 
-    public int inputMoney(String userInput) {
-        return 0;
+    private final LottoGameService lottoGameService;
+
+    public LottoGameController() {
+        this.lottoGameService = new LottoGameService();
+    }
+
+    public int inputMoney(final String money) {
+        return lottoGameService.inputMoney(money);
     }
 
     public void printLottoTickets(int lottoTicketCount) {
---
+++ src/main/java/lotto/service/LottoGameService.java
@@ -0,0 +1,12 @@
+package lotto.service;
+
+public class LottoGameService {
+
+    public int inputMoney(final String money) {
+        validMoney(money);
+    }
+
+    private void validMoney(String money) {
+
+    }
+}
