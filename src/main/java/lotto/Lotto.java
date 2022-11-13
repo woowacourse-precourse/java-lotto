fad443c18b76f4df09adb2b11225cd7b1616d4f7
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-11-14 06:30:26 +0900

    refactor<Lotto>: 클래스 병합

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,11 +16,15 @@ public class Lotto {
         return lottoQuantity;
     }
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<Integer> numbers, int number) {
         validateCntNumber(numbers);
         validateDuplicate(numbers);
         validateNumberCondition(numbers);
+        validateBonusDuplicate(numbers, number);
+        validateBonusNumberCondition(number);
         Collections.sort(numbers);
+
+        numbers.add(number);
         this.numbers = numbers;
     }
 
@@ -44,6 +48,20 @@ public class Lotto {
             if (numbers.get(i-1) < 1 || numbers.get(i-1) > 45) {
                 exceptionHandler.foolExceaption("lottoCondition");
             }
+        }
+    }
+
+    private void validateBonusDuplicate(List<Integer> numbers, int number) {
+        for (int compareNumber : numbers) {
+            if (compareNumber == number) {
+                exceptionHandler.foolExceaption("lottoDuplicate");
+            }
+        }
+    }
+
+    private void validateBonusNumberCondition(int number) {
+        if (number < 1 || number > 45) {
+            exceptionHandler.foolExceaption("lottoCondition");
         }
     }
 }
