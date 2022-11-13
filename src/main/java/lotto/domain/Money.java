c7fbc55b72adccd7d0dbf771f6e859879b238e18
wlsh44 <k87913j@gmail.com>
2022-11-13 20:27:59 +0900

    fix: 구입 금액이 음수일 경우 예외 처리하도록 수정

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -3,7 +3,8 @@ package lotto.domain;
 public class Money {
 
     private static final int LOTTO_PRICE = 1000;
-    private static final String MONEY_ERROR_MESSAGE = "구입 금액은 1000원으로 나누어 떨어져야 합니다.";
+    private static final String NOT_DIVIDED_BY_LOTTO_PRICE_ERROR = "구입 금액은 1000원으로 나누어 떨어져야 합니다.";
+    private static final String NEGATIVE_MONEY_ERROR = "구입 금액은 음수일 수 없습니다.";
 
     private final int money;
 
@@ -13,8 +14,19 @@ public class Money {
     }
 
     private void validateMoney(int money) {
+        validateDividedByLottoPrice(money);
+        validateNotNegative(money);
+    }
+
+    private void validateNotNegative(int money) {
+        if (money < 0) {
+            throw new IllegalArgumentException(NEGATIVE_MONEY_ERROR);
+        }
+    }
+
+    private void validateDividedByLottoPrice(int money) {
         if (money % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException(MONEY_ERROR_MESSAGE);
+            throw new IllegalArgumentException(NOT_DIVIDED_BY_LOTTO_PRICE_ERROR);
         }
     }
 
