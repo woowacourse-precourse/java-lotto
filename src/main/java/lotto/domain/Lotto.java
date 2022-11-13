73d70d265782efa19d647e8b1327c2965ca3efd9
DaWoon Lee <qkgk61@gmail.com>
2022-11-13 19:52:54 +0900

    Fix : Lotto 번호가 6자리가 아닌 경우 예외 처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -14,7 +14,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] Lotto의 당첨 번호는 6개입니다.");
         }
     }
 
