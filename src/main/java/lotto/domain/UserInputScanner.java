6f762afb1a191c31a21e1b362cf37d15ee5df466
HyunggiPark <phg9587@naver.com>
2022-11-13 15:17:07 +0900

    refact(core): validate 하는 메서드들을 Validator로 이동

--- src/main/java/lotto/domain/UserInputScanner.java
+++ src/main/java/lotto/domain/UserInputScanner.java
@@ -7,8 +7,10 @@ public class UserInputScanner {
         System.out.println("구입금액을 입력해 주세요.");
         String userInput = readLine();
 
+        Validator validator = new Validator();
+
         try {
-            validateUserInput(userInput);
+            validator.validateUserInput(userInput);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return -1;
@@ -16,32 +18,4 @@ public class UserInputScanner {
 
         return Integer.parseInt(userInput);
     }
-
-    private void validateUserInput(String userInput) {
-        String verifiedUserInput = verifyUserInputIsDigit(userInput);
-        verifyUserInputIsMultipleOf1000(verifiedUserInput);
-    }
-
-    private void verifyUserInputIsMultipleOf1000(String verifiedUserInput) {
-        try {
-            if (Integer.parseInt(verifiedUserInput) % 1000 != 0) {
-                throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력을 해주세요.");
-            }
-        }catch (NumberFormatException e) {
-            throw new NumberFormatException("[ERROR] 2^31-1 까지의 범위만 입력이 가능합니다.");
-        }
-    }
-
-    private String verifyUserInputIsDigit(String userInput) {
-        for (int idx = 0; idx < userInput.length(); idx++) {
-            int eachNum = userInput.charAt(idx);
-
-            if (!Character.isDigit(eachNum)) {
-                throw new IllegalArgumentException("[ERROR] \"숫자\" 만 입력해주세요 (쉼표도 제외입니다.)");
-            }
-        }
-
-        return userInput;
-    }
-
 }
