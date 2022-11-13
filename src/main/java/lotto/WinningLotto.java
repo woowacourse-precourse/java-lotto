21f055f9613842e17a7b4fb1f55c2ebb79a6f90d
wugawuga <dnr3655@naver.com>
2022-11-13 22:15:38 +0900

    refactor: BonusLotto -> WinningLotto 수정 기존 Lotto 상속

---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,20 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningLotto extends Lotto {
+    private final int bonus;
+
+    public WinningLotto(List<Integer> winningNumber, int bonus) {
+        super(winningNumber);
+        if (this.checkBonus(bonus)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호랑 중복될 수 없어요.");
+        }
+        this.checkLottoRange(bonus);
+        this.bonus = bonus;
+    }
+
+    public boolean checkBonus(Lotto lotto) {
+        return lotto.checkBonus(bonus);
+    }
+}
