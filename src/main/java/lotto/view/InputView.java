7364036f8e46b74360c4cc602a3854bb10ff6e21
wwwndus <simock22@gmail.com>
2022-11-13 21:33:02 +0900

    feat(InputView): 구입 금액을 입력 받음

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,13 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public static String userInputMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
+
+        String inputMoney = Console.readLine();
+        return  inputMoney;
+    }
 }
