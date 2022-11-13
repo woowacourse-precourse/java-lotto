0ce4b22a5258a275643b64c247ce281ae83ca355
recicoco068 <gkapf838@gmail.com>
2022-11-13 22:15:36 +0900

    feat(Lotto.java) : 로또 번호를 문자열 형태로 반환하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,5 +16,12 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public String toStringLotto() {
+        StringBuilder lotto = new StringBuilder("[");
+        for (int n : this.numbers) {
+            lotto.append(String.valueOf(n)).append(", ");
+        }
+        lotto = new StringBuilder(lotto.substring(0, lotto.length() - 2) + "]");
+        return lotto.toString();
+    }
 }
