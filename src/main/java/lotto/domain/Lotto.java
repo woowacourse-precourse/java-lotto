ddfac8c6cb0be7643bcc8ba2321006fe3dfec233
hossang <zkzk104@naver.com>
2022-11-13 21:32:51 +0900

    refactor: WinningNumber에 있는 당첨 번호 예외 메소드 Lotto로 옮기기

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,18 +7,51 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final Set<Integer> numbers;
-
-    public Set<Integer> getNumbers() {
-        return numbers;
-    }
 
     public Lotto(List<Integer> unValidatedNumbers) {
         Collections.sort(unValidatedNumbers);
         this.numbers = new LinkedHashSet<>(unValidatedNumbers);
         validateSixNumber(numbers);
+    }
+    //지금 넘버가 너무 많아
+    public Lotto(String input) {
+        this.numbers = toIntSet(separateCommas(input));
+
+    }
+
+    public Set<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private Set<Integer> toIntSet(Set<String> separateCommas) {
+        validateNumbers(separateCommas);
+        Set<Integer> unValidatedNumbers = separateCommas.stream().map(number -> Integer.parseInt(number)).collect(Collectors.toSet());
+        validateRange(unValidatedNumbers);
+        validateSixNumber(unValidatedNumbers);
+        return unValidatedNumbers;
+    }
+
+    private void validateRange(Set<Integer> unValidatedNumbers) {
+        if (unValidatedNumbers.stream().anyMatch(range -> range < 1 || range > 45)) {
+            throw new IllegalArgumentException(Error.IS_NOT_RANGE.getMessage());
+        }
+    }
+
+    private void validateNumbers(Set<String> separateCommas) {
+        for (String separateComma : separateCommas) {
+            if (separateComma.chars().allMatch(Character::isDigit)) {
+                throw new IllegalArgumentException(Error.IS_NOT_NUMBER.getMessage());
+            }
+        }
+    }
+
+    private Set<String> separateCommas(String input) {
+        Set<String> inputs = new HashSet<>(Arrays.asList(input.split(",")));
+        return inputs;
     }
 
     private void validateSixNumber(Set<Integer> numbers) {
--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -10,51 +10,6 @@ public class WinningNumber {
     private Lotto winningNumber;
 
     public void inputWinningNumber(String input) {
-        isNumber(separateCommas(input));
-        if (!isNumber(input)) {
-            throw new IllegalArgumentException(Error.IS_NOT_NUMBER.getMessage());
-        }
-        if (!isRangeNumber()) {
-            throw new IllegalArgumentException(Error.IS_NOT_RANGE.getMessage());
-        }
-        if (!isSixNumber()) {
-
-        }
-
-    }
-
-    private void isSixNumber() {
-        if (winningNumber.size() == 6) {
-            throw new IllegalArgumentException(Error.IS_NOT_UNIQUE.getMessage());
-        }
-    }
-
-    private boolean isRangeNumber() {
-        return winningNumber.stream().allMatch(a -> a >= 1 && a <= 45);
-    }
-
-    private List<String> separateCommas(String input) {
-        List<String> inputs = new ArrayList<>(Arrays.asList(input.split(",")));
-        validateStrings(inputs);
-        return inputs;
-    }
-
-    private void validateStrings(List<String> inputs) {
-        if (inputs.size() != 6) {
-            throw new IllegalArgumentException(Error.IS_NOT_SIX.getMessage());
-        }
-    }
-
-    private boolean isNumber(String[] inputs) { //인덴트 초과
-        Set<Integer> numbers = new HashSet<>();
-        for (String input : inputs) {
-            for (int i = 0; i < input.length(); i++) {
-                if (input.charAt(i) < '0' || input.charAt(i) > '9') {
-                    return false;
-                }
-            }
-            numbers.add(Integer.parseInt(input)); //메소드 두개인데
-        }
-        return true;
+        winningNumber = new Lotto(input);
     }
 }
