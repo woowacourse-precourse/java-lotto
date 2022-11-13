3106173d4f01a94eda4913dcb841fa257347a648
ParkJoungWoo <jug78@naver.com>
2022-11-13 21:55:03 +0900

    feat(user/User.java) : User 클래스 기능 추가
    User 클래스에 구매량을 반환하는 기능을 추가했습니다.
    
    - User의 seedMoney를 통해 구매 수량을 반환하는 메소드 추가

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,12 +1,13 @@
 package user;
 
-import camp.nextstep.edu.missionutils.Console;
 import lotto.Lotto;
 import ui.View;
 
 import java.util.List;
 
 public class User {
+
+    private final int BASE_MONEY = 1000;
     private List<Lotto> lottos;
     private Money seedMoney;
     private Money prizeMoney;
@@ -14,6 +15,10 @@ public class User {
     public User() {
         seedMoney = new Money();
         prizeMoney = new Money(0);
+    }
+
+    public int getLottoMount() {
+        return seedMoney.getMoney() / BASE_MONEY;
     }
 
     public void setLottos(List<Lotto> lottos) {
@@ -27,11 +32,15 @@ public class User {
     public String getRatio() {
         double prize = (double) prizeMoney.getMoney();
         double seed = (double) seedMoney.getMoney();
-        return String.format("%.1f", prize / seed);
+        return String.format("%.1f", prize / seed * 100);
     }
 
-    public Money getSeedMoney() {
-        return seedMoney;
+    public void printPurchaseResult() {
+        View.printPurchaseMount(this);
+        View.printLottoList(this);
+    }
+    public void printRatio(){
+        View.printTotalPrize(this);
     }
 
     public List<Lotto> getLottos() {
