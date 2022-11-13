22901d29235d9ec255c78c85a379bd6cfa808c3f
glass <glass7312@gmail.com>
2022-11-13 16:35:32 +0900

    refactor:클래스 분리

---
+++ src/main/java/domain/Grade.java
@@ -0,0 +1,29 @@
+package domain;
+
+import Service.gradeService;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Grade {
+    public static final int THREE_HIT = 3;
+    public static final int FOUR_HIT = 4;
+    public static final int FIVE_HIT = 5;
+    public static final int SIX_HIT = 6;
+
+    public static enum Lottogrades {first, second, third, fourth, fifth, lose}
+
+   // private final List<Lottogrades> winning_amount = Arrays.asList(grade.first, gradeService.Lottogrades.second, gradeService.Lottogrades.third, gradeService.Lottogrades.fourth, gradeService.Lottogrades.fifth);
+
+    public static Lottogrades getGrade(int hitCount, boolean bonus_hit) {
+
+        if (hitCount == THREE_HIT) return Lottogrades.fifth;
+        if (hitCount == FOUR_HIT) return Lottogrades.fourth;
+        if (hitCount == FIVE_HIT && !bonus_hit) return Lottogrades.third;
+        if (hitCount == FIVE_HIT && bonus_hit) return Lottogrades.second;
+        if (hitCount == SIX_HIT) return Lottogrades.first;
+
+        return Lottogrades.lose;
+
+    }
+}
