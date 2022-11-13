0fafaeb5f47fe6eb2fefda768ef5b35cc2ea3f4d
TSTP <86605720+seokjin0404@users.noreply.github.com>
2022-11-13 23:47:59 +0900

    test: ProfitTest 테스트케이스 추가

--- src/test/java/lotto/domain/ProfitTest.java
+++ src/test/java/lotto/domain/ProfitTest.java
@@ -13,4 +13,17 @@ public class ProfitTest {
         history.report(Result.NONE);
         assertThat(history.calculateProfitRate()).isEqualTo("166.7");
     }
+    @Test
+    public void HistoryTest2() {
+        History history = History.newInstance();
+        history.report(Result.NONE);
+        history.report(Result.NONE);
+        history.report(Result.NONE);
+        history.report(Result.NONE);
+        history.report(Result.NONE);
+        history.report(Result.NONE);
+        history.report(Result.NONE);
+        history.report(Result.THREE);
+        assertThat(history.calculateProfitRate()).isEqualTo("62.5");
+    }
 }
