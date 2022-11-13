7ef6968931b6d1de538489332e49416a92228e91
greeng00se <thegreengoose7@gmail.com>
2022-11-13 23:37:07 +0900

    feat(LottoResult): 구매 금액 대비 이익률을 계산해주는 calculateProfitRatio 메서드 추가

--- docs/README.md
+++ docs/README.md
@@ -73,8 +73,9 @@
 - [x]  로또 등수 및 상금에 대한 정보를 가지고 있는 LottoPrize Enum 추가
     - [x] 맞춘개수를 입력받아 LottoPrize를 반환하는 메서드 추가
     - [x] 맞춘개수를 입력받아 해당 등수의 총 당첨금액을 계산하는 calculatePrize 메서드 추가
-- [x]  로또 결과에 대한 정보를 가지고 있는 LottoResult 클래스 추가 EnumMap<LottoPrize, Integer>의 일급 컬렉션
+- [x]  로또 결과에 대한 정보를 가지고 있는 LottoResult 클래스 추가 EnumMap<LottoPrize, Long>의 일급 컬렉션
     - [x] LottoPrize에 대한 값을 반환하는 getCount 메서드 추가
+    - [x] 구매 금액 대비 이익률을 계산해주는 calculateProfitRatio 메서드 추가
 - [x]  당첨 로또 번호를 가지고 있고, 로또를 검증하는 LottoMachine 클래스 추가
     - [x] 로또 결과 확인하는 check 메서드 추가
 - [x]  입력값을 검증하는 InputValidator 클래스 추가
--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.Map;
 
 public class LottoResult {
     private static final Long ZERO = 0L;
+    private static final double DEFAULT_RATIO = 100.0;
 
     private EnumMap<LottoPrize, Long> result = new EnumMap<>(LottoPrize.class);
 
@@ -12,7 +14,23 @@ public class LottoResult {
         this.result.putAll(result);
     }
 
-    public Integer getCount(LottoPrize lottoPrize) {
-        return result.getOrDefault(lottoPrize, ZERO).intValue();
+    public double calculateProfitRatio() {
+        return (calculateTotalPrize() / (double) calculatePayAmount()) * DEFAULT_RATIO;
+    }
+
+    private Long calculateTotalPrize() {
+        return Arrays.stream(LottoPrize.values())
+                .map(prize -> prize.calculatePrize(this.getCount(prize)))
+                .reduce(ZERO, Long::sum);
+    }
+
+    private Long calculatePayAmount() {
+        return LottoSeller.LOTTO_PRICE * Arrays.stream(LottoPrize.values())
+                .map(this::getCount)
+                .reduce(ZERO, Long::sum);
+    }
+
+    public Long getCount(LottoPrize lottoPrize) {
+        return result.getOrDefault(lottoPrize, ZERO);
     }
 }
