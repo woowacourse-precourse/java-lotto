e0620f973b8106cde7909f6038deb27f149a4a52
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-13 20:38:24 +0900

    fix(LottoConsole): modify methods to return Number data after validation

--- src/main/java/lotto/LottoConsole.java
+++ src/main/java/lotto/LottoConsole.java
@@ -1,24 +1,56 @@
 package lotto;
 
+import lotto.ui.Input;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoConsole {
+    private final Input input;
+    private final Output output;
+    private final Validator validator;
+
     public LottoConsole() {
-        Input input = new Input();
-        Output output = new Output();
-        Validator validator = new Validator();
+        this.input = new Input();
+        this.validator = new Validator();
     }
 
-    public Integer getPurchaseAmount() {
-        String purchaseAmount = input.getPurchaseAmount();
-        return validator.getPurchaseAmountAfterValidation(purchaseAmount);
+    private void validate(String input) {
+        this.validator.throwIfEmtpy(input);
+        this.validator.throwIfNotNumber(input);
     }
 
-    public Integer getWinningLottoNumbers() {
-        String winningLottoNumbers = input.getWinningLottoNumbers();
-        return validator.getWinningLottoNumbersAfterValidation(winningLottoNumbers);
+    private Integer convertInputToNumber(String input) {
+        return Integer.parseInt(input);
     }
 
-    public Integer getBonusLottoNumber() {
-        String bonusLottoNumber = input.getBonusLottoNumber();
-        return validator.getBonusLottoNumberAfterValidation(bonusLottoNumber);
+    private List<Number> convertInputToNumbers(String input) {
+        String[] inputs = input.split(",");
+        List<Number> numbers = new ArrayList<>();
+        for (String number : inputs) {
+            this.validate(number);
+            numbers.add(new LottoNumber(convertInputToNumber(number)));
+        }
+        return numbers;
+    }
+
+    public PurchaseAmount getPurchaseAmount() {
+        String input = this.input.getPurchaseAmount();
+        this.validate(input);
+        Integer purchaseAmount = this.convertInputToNumber(input);
+        return new PurchaseAmount(purchaseAmount);
+    }
+
+    public Lotto getWinningLottoNumbers() {
+        String input = this.input.getWinningLottoNumbers();
+        List<Number> numbers = this.convertInputToNumbers(input);
+        return new Lotto(numbers);
+    }
+
+    public LottoNumber getBonusLottoNumber() {
+        String input = this.input.getBonusLottoNumber();
+        this.validate(input);
+        Integer bonusNumber = this.convertInputToNumber(input);
+        return new LottoNumber(bonusNumber);
     }
 }
