98012c12eb664507757b8f4a043dd75845d07e41
stk346 <stk346@gamil.com>
2022-11-13 20:12:32 +0900

    feat: 당첨 내역 및 수익률을 한 화면에 출력하도록 기능 통합.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import view.InputView;
+import view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int amount = InputView.showBuyingAmountMessage();
+        String winningInputNumbers = InputView.showEnterWinningNumberMessage();
+        String winningBonusNumber = InputView.showEnterBonusNumberMessage();
+        Player player = new Player(amount);
+        OutputView.showLottoNumbers(player);
+
+        WinningNumbers winningNumbers = new WinningNumbers(winningInputNumbers, winningBonusNumber);
+        player.playTheLottery(winningNumbers);
+        OutputView.showLotteryResults(player);
+        OutputView.showLotteryYield(player);
     }
 }
