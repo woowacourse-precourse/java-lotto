86eea897a6629b2454af803c88eaa1c27f7e70e4
SHEOMM <esh0821@snu.ac.kr>
2022-11-13 16:43:16 +0900

    test: 로또 등수 반환 테스트

--- src/test/java/lotto/EachNumberCalculateServiceTest.java
+++ src/test/java/lotto/EachNumberCalculateServiceTest.java
@@ -4,9 +4,11 @@ import java.util.LinkedList;
 import java.util.List;
 import lotto.domain.EachNumberCalculateService;
 import lotto.model.Lotto;
+import lotto.model.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EachNumberCalculateServiceTest {
@@ -37,6 +39,38 @@ public class EachNumberCalculateServiceTest {
     void 로또_숫자_6개_일치_테스트(){
         Lotto lottery = new Lotto(new LinkedList<>(List.of(1, 2, 3, 4, 5, 6)));
         assertEquals(6, eachNumberCalculateService.howManyMatchesNumber(lottery));
+    }
+
+    @DisplayName("로또 당첨 실패 테스트")
+    @Test
+    void 로또_당첨_실패_테스트(){
+        Lotto lottery = new Lotto(new LinkedList<>(List.of(7, 10, 11, 16, 22, 43)));
+        assertThat(eachNumberCalculateService.calculate(lottery))
+                .isEqualTo(Rank.NOT_WINNER);
+    }
+
+    @DisplayName("로또 3등 테스트")
+    @Test
+    void 로또_3등_테스트(){
+        Lotto lottery = new Lotto(new LinkedList<>(List.of(1, 2, 3, 4, 5, 7)));
+        assertThat(eachNumberCalculateService.calculate(lottery))
+                .isEqualTo(Rank.THIRD_RANK);
+    }
+
+    @DisplayName("로또 2등 테스트")
+    @Test
+    void 로또_2등_테스트(){
+        Lotto lottery = new Lotto(new LinkedList<>(List.of(1, 2, 3, 4, 5, 45)));
+        assertThat(eachNumberCalculateService.calculate(lottery))
+                .isEqualTo(Rank.SECOND_RANK);
+    }
+
+    @DisplayName("로또 1등 테스트")
+    @Test
+    void 로또_1등_테스트(){
+        Lotto lottery = new Lotto(new LinkedList<>(List.of(1, 2, 3, 4, 5, 6)));
+        assertThat(eachNumberCalculateService.calculate(lottery))
+                .isEqualTo(Rank.FIRST_RANK);
     }
 
 }
