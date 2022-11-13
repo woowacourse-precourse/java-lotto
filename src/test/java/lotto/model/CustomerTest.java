3467b4f372800a2230893188d788b11b8e9ccb5c
Jin409 <sualng123@gmail.com>
2022-11-14 00:35:50 +0800

    test(CustomerTest): 갖고 있는 모든 로또들을 오름차순으로 정렬하여 문자열로 반환하는지 확인하는 테스트코드 추가

--- src/test/java/lotto/model/CustomerTest.java
+++ src/test/java/lotto/model/CustomerTest.java
@@ -1,11 +1,14 @@
 package lotto.model;
 
+import static lotto.constants.LottoConstant.LOTTERY_NUMBER_SEPARATOR;
 import static lotto.constants.LottoConstant.LOTTO_PRICE;
 import static lotto.validator.ErrorMessages.INVALID_PAID_MONEY_ERROR_MESSAGE;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -18,6 +21,11 @@ public class CustomerTest {
     @BeforeEach
     void setUp() {
         int paidMoney = 3000;
+        customer = new Customer(paidMoney);
+    }
+
+    void setUpForCheckGetAllLottoToString() {
+        int paidMoney = 1000;
         customer = new Customer(paidMoney);
     }
 
@@ -35,16 +43,20 @@ public class CustomerTest {
     }
 
     @Test
-    @DisplayName("로또 번호들에 따라 로또의 개수만큼 알맞은 순위를 반환한다.")
-    void checkJudgeLottoRanks() {
+    @DisplayName("갖고 있는 모든 로또들을 오름차순으로 정렬하여 문자열로 반환한다.")
+    void checkGetAllLottoToString() {
         //given
-        List<Integer> winningNumber = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
-        int bonusNumber = 7;
+        setUpForCheckGetAllLottoToString();
 
         //when
-        List<Rank> ranks = customer.judgeLottoRanks(winningNumber, bonusNumber);
+        String result = customer.getAllLottoToString();
+
+        result = result.replaceAll(" ", "");
+        result = result.substring(1, result.length() - 2);
+        String[] tempResult = result.split(LOTTERY_NUMBER_SEPARATOR);
+        int[] afterResult = Arrays.stream(tempResult).mapToInt(Integer::parseInt).toArray();
 
         //then
-        assertEquals(ranks.size(), paidMoney / LOTTO_PRICE);
+        assertThat(afterResult[0] < afterResult[1]).isTrue();
     }
 }
