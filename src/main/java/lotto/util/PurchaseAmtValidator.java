65b50f68beb1015b69aee0197507037bab7cf08a
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-13 23:55:11 +0900

    feat: 입력값 1000원으로 나누는 기능 구현
    입력값이 1000원으로 나누어지지 않는 경우 IllegalArgumentException 발생시키는 API 개발

--- src/main/java/lotto/util/PurchaseAmtValidator.java
+++ src/main/java/lotto/util/PurchaseAmtValidator.java
@@ -6,6 +6,7 @@ public class PurchaseAmtValidator implements Validator {
     private final String EMPTY_ERROR = "[ERROR] 금액을 입력해주세요.";
     private final String NUMERIC_ERROR = "[ERROR] 숫자만 입력해주세요.";
     private final String LOW_AMT_ERROR = "[ERROR] 천 원 이상 입력해주세요.";
+    private final String PURCHASE_AMT_ERROR = "[ERROR] 1000원 단위를 입력해주세요.";
     private final int MIN_UNIT = 1000;
 
 
@@ -20,6 +21,9 @@ public class PurchaseAmtValidator implements Validator {
         int inputAmt = Integer.parseInt(input);
         if (inputAmt < MIN_UNIT) {
             throw new IllegalArgumentException(LOW_AMT_ERROR);
+        }
+        if (inputAmt % MIN_UNIT != 0) {
+            throw new IllegalArgumentException(PURCHASE_AMT_ERROR);
         }
     }
 }
