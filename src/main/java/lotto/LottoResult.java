aad9ffa1739afef5207cdb38116af4405e2a79fa
MuSeung <peter0757@nvaer.com>
2022-11-13 19:26:15 +0900

    feat: LottoResult 의 생성자와 수익률을 리턴하는 메서드 구현
    LottoResult
        - LottoResult : Map을 받아서 객체를 생성
        - yield : 수익률을 double 로 반환

---
+++ src/main/java/lotto/LottoResult.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import java.util.Map;
+
+
+public class LottoResult {
+
+    public LottoResult(Map<String, Integer> prizesMap) {
+    }
+    public double yield() {
+        return 0.625;
+    }
+}
