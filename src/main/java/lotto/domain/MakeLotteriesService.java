cfbc8a65a2ebf2c91f57491a5ae2bc15b8e004f2
SHEOMM <esh0821@snu.ac.kr>
2022-11-13 18:04:30 +0900

    fix: 리스트 선언이 안되어서 nullException 발생 해결

--- src/main/java/lotto/domain/MakeLotteriesService.java
+++ src/main/java/lotto/domain/MakeLotteriesService.java
@@ -1,22 +1,25 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.LinkedList;
 import java.util.List;
+import lotto.model.Lotto;
 
 public class MakeLotteriesService {
-    private List<List<Integer>> lotteries;
+    private List<Lotto> lotteries;
 
     public void makeLotteries(int lotteriesNumber){
+        lotteries = new LinkedList<>();
         for (int lottoCount = 0; lottoCount < lotteriesNumber; lotteriesNumber++){
             lotteries.add(makeLotto());
         }
     }
 
-    public List<List<Integer>> getLotteries() {
+    public List<Lotto> getLotteries() {
         return lotteries;
     }
 
-    private List<Integer> makeLotto(){
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    private Lotto makeLotto(){
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
     }
 }
