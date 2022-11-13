21f82fd94b1260ba910d62236e8d5aac7bbf760b
KTH-96 <taehyun960@gmail.com>
2022-11-13 14:23:32 +0900

    feat(LottoPurchaseDto): 구매수에 맞게 로또 구매하기 기능 구현

--- src/main/java/lotto/controller/dto/LottoPurchaseDto.java
+++ src/main/java/lotto/controller/dto/LottoPurchaseDto.java
@@ -1,5 +1,8 @@
 package lotto.controller.dto;
 
+import static camp.nextstep.edu.missionutils.Randoms.*;
+
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.Lotto;
@@ -12,13 +15,25 @@ public class LottoPurchaseDto {
 
     public LottoPurchaseDto(int paymentAmount) {
         this.purchaseNumbers = validDivision(paymentAmount);
-        this.lottos = new ArrayList<>();
+        this.lottos = purchaseLottos(purchaseNumbers);
+    }
+
+    private List<Lotto> purchaseLottos(int purchaseNumbers) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int lottoTicket = 0; lottoTicket < purchaseNumbers; lottoTicket++) {
+            lottos.add(generateLotto());
+        }
+        return lottos;
+    }
+
+    private Lotto generateLotto() {
+        return new Lotto(pickUniqueNumbersInRange(1, 45, 6));
     }
 
     private int validDivision(int paymentAmount) {
         if (paymentAmount % LOTTO_BASIC_AMOUNT != 0) {
             throw new IllegalArgumentException("[ERROR] 지불금액이 1000으로 나누어 떨어지지 않습니다.");
         }
-        return paymentAmount/LOTTO_BASIC_AMOUNT;
+        return paymentAmount / LOTTO_BASIC_AMOUNT;
     }
 }
