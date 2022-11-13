a0f5e6c834642f627e4b96c40b23e466daba3378
swonny <wonny921@gmail.com>
2022-11-13 12:00:22 +0900

    test(CommonExceptionTest): 로또 숫자 범위 검사 테스트 추가

--- src/test/java/ExceptionCollections/CommonExceptionTest.java
+++ src/test/java/ExceptionCollections/CommonExceptionTest.java
@@ -17,4 +17,28 @@ public class CommonExceptionTest {
                 () -> CommonException.hasCharacters("1a")
         );
     }
+
+    @DisplayName("구매금액 단위를 확인한다.")
+    @Test
+    void 구매금액_단위_테스트() {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> CommonException.validatePaymentUnit(10230)
+        );
+    }
+
+    @DisplayName("최소 번호보다 작은 번호는 예외처리한다.")
+    @Test
+    void 최소_숫자_범위_예외_테스트() {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> CommonException.validateNumberRange(0)
+        );
+    }
+
+    @DisplayName("최대 번호보다 큰 번호는 예외처리한다.")
+    @Test
+    void 최대_숫자_범위_예외_테스트() {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> CommonException.validateNumberRange(47)
+        );
+    }
 }
