8c07189f072257b8cc4fe7b0abe1d0148f53279a
recicoco068 <gkapf838@gmail.com>
2022-11-13 22:37:24 +0900

    test(UtilsTest.java) : 로또 번호가 1 ~ 45 사이인지 확인하는 함수 테스트

--- src/test/java/lotto/UtilsTest.java
+++ src/test/java/lotto/UtilsTest.java
@@ -1,4 +1,17 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class UtilsTest {
-  
+    @DisplayName("로또 번호가 1 ~ 45 범위를 넘으면 false")
+    @Test
+    void 로또_번호_유효_범위_넘으면_false() {
+        List<Integer> numbers = List.of(1, 2, 3, 48, 5, 6);
+        assertThat(Utils.validateLottoNumbers(numbers)).isFalse();
+    }
 }
