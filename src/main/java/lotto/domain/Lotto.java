640c55beb146dde6f556f673eaa973f0938d1047
twoosky <lhjae3@naver.com>
2022-11-13 22:12:15 +0900

    feat(lotto): 로또 번호 중복 시 예외 처리 구현

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static lotto.domain.ErrorMessage.*;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        validateDuplicated(numbers);
+        validateNumbersRange(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(ERROR_OVER_SIZE);
+        }
+    }
+
+    private void validateDuplicated(List<Integer> numbers) {
+        Set<Integer> number = new HashSet<>(numbers);
+        if (number.size() != 6) {
+            throw new IllegalArgumentException(ERROR_DUPLICATED);
+        }
+    }
+}
