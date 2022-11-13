c82f7be196c999c9ac88e5e08e88d9de4431c95a
Cho SangHyun <98shcho@naver.com>
2022-11-13 12:53:08 +0900

    feat(보너스번호입력 예외처리): 입력한 값이 유효범위가 아니면 오류를 내는 기능 추가

--- src/main/java/lotto/BonusNumberValidator.java
+++ src/main/java/lotto/BonusNumberValidator.java
@@ -5,7 +5,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class BonusNumberValidator {
+    private static int MIN_BONUS_NUMBER = 1;
+    private static int MAX_BONUS_NUMBER = 45;
     private static String NUMERIC_ERROR_MESSAGE = "숫자를 입력해야 합니다";
+    private static String RANGE_ERROR_MESSAGE = String.format("%d ~ %d 사이의 숫자들로 입력해야 합니다",
+            MIN_BONUS_NUMBER, MAX_BONUS_NUMBER);
 
     private static boolean isNumeric(String input) {
         for (int index = 0; index < input.length(); index++) {
@@ -16,9 +20,17 @@ public class BonusNumberValidator {
         return true;
     }
 
+    private static boolean isInValidRange(String input) {
+        int number = Integer.parseInt(input);
+        return (MIN_BONUS_NUMBER < number && number < MAX_BONUS_NUMBER);
+    }
+
     public static void validate(String bonusNumberInput) {
         if (!isNumeric(bonusNumberInput)) {
             throw new IllegalArgumentException(NUMERIC_ERROR_MESSAGE);
+        }
+        if (!isInValidRange(bonusNumberInput)) {
+            throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
         }
     }
 }
