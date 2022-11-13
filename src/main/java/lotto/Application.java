46b9c2df86015f5fdbccbb75244a4f031b495cfa
plusjob70 <plusjob70@gmail.com>
2022-11-14 02:27:48 +0900

    style: 불필요한 주석 제거

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,20 +10,18 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // 구입 금액 입력
         int money = Input.insertMoney();
+
         Ticket ticket = new Ticket(money);
-        // 금액에 따라 로또 수량 및 번호 출력
         Output.showTicket(ticket);
-        // 당첨 결과를 기록하는 정답지 생성
+
         Answer answer = new Answer();
-        // 당첨 번호 입력
         List<Integer> winningNumbers = Input.insertWinningNumbers();
         answer.setWinningNumbers(winningNumbers);
-        // 보너스 번호 입력
+
         int bonusNumber = Input.insertBonusNumber();
         answer.setBonusNumber(bonusNumber);
-        // 당첨 통계 출력
+
         Statistics statistics = new Statistics(ticket, answer);
         Output.showWinningStatistics(statistics);
     }
