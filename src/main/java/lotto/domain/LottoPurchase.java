e49b633c82a7256486edb81271919e690641084a
Choi-JY <Choi-JY1107@github.com>
2022-11-14 00:36:24 +0900

    feat: 복권 당첨인지 확인하는 기능 구현

--- src/main/java/lotto/domain/LottoPurchase.java
+++ src/main/java/lotto/domain/LottoPurchase.java
@@ -3,11 +3,28 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import lotto.dto.LottoStatsDTO;
+
 public class LottoPurchase {
-    public List<Lotto> lottery;
+    private static final Integer LOTTO_MINIMUM_RANK = 5;
+    private static List<Lotto> lottery;
+    private static List<Integer> rankCounts = new ArrayList<>(List.of(0, 0, 0, 0, 0));
 
     public LottoPurchase(List<Lotto> lottery) {
 	this.lottery = lottery;
+    }
+
+    public void compareNumberByWinning() {
+        for (Lotto lotto: lottery) {
+            compareRankByNumber(lotto);
+        }
+    }
+
+    private void compareRankByNumber(Lotto lotto) {
+        int rank = WinningLotto.prepareWinningLotto(lotto);
+        if (rank <= LOTTO_MINIMUM_RANK) {
+            rankCounts.set(rank - 1, rankCounts.get(rank) + 1);
+        }
     }
 
     public List<Lotto> getLottery() {
--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -3,10 +3,42 @@ package lotto.domain;
 import java.util.List;
 
 public class WinningLotto extends Lotto{
-    private final Integer bonusNumber;
+    private static final Integer MINIMUM_RANK = 8;
+    private static List<Integer> lottoNumbers;
+    private static Integer bonusNumber;
 
     public WinningLotto(List<Integer> lottoNumbers, Integer bonusNumber) {
 	super(lottoNumbers);
 	this.bonusNumber = bonusNumber;
     }
+
+    public static Integer prepareWinningLotto(Lotto lottoNumbers) {
+	int rank = MINIMUM_RANK;
+	for (Integer lottoNumber: lottoNumbers.getLottoNumbers()) {
+	    rank -= isContainWinningNumber(lottoNumber);
+	}
+	rank -= isNotContainBonusNumber(rank, lottoNumbers.getLottoNumbers());
+	return rank;
+    }
+
+    private static int isContainWinningNumber(Integer lottoNumber) {
+	if (lottoNumbers.contains(lottoNumber)) {
+	    return 1;
+	}
+	if (lottoNumber == bonusNumber) {
+	    return 1;
+	}
+	return 0;
+    }
+
+    private static int isNotContainBonusNumber(int rank, List<Integer> lottoNumbers) {
+	if (rank != 2) {
+	    return 0;
+	}
+	if (!lottoNumbers.contains(bonusNumber)) {
+	    return 1;
+	}
+	return 0;
+    }
+
 }
