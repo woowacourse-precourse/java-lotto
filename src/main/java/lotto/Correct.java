1f72c3081c1fe8ea73f1380763584eb8d8bfc5b8
kevstevie <jianna442@gmail.com>
2022-11-13 18:46:06 +0900

    refactor: 연산을 위한 place 인스턴스 추가

--- src/main/java/lotto/Correct.java
+++ src/main/java/lotto/Correct.java
@@ -2,19 +2,21 @@ package lotto;
 
 public enum Correct {
 
-    CORRECT_THREE(3, false, 5000L),
-    CORRECT_FOUR(4, false, 50000L),
-    CORRECT_FIVE(5, false, 1500000L),
-    CORRECT_FIVE_BONUS(5, true, 30000000L),
-    CORRECT_SIX(6, false, 2000000000L);
+    CORRECT_THREE(3, false, 5000L , 5),
+    CORRECT_FOUR(4, false, 50000L, 4),
+    CORRECT_FIVE(5, false, 1500000L, 3),
+    CORRECT_FIVE_BONUS(5, true, 30000000L, 2),
+    CORRECT_SIX(6, false, 2000000000L, 1);
 
     final int correct;
     final boolean isBonusCorrect;
     final long prize;
+    final int place;
 
-    Correct(int correct, boolean isBonusCorrect, long prize) {
+    Correct(int correct, boolean isBonusCorrect, long prize, int place) {
         this.correct = correct;
         this.isBonusCorrect = isBonusCorrect;
         this.prize = prize;
+        this.place = place;
     }
 }
