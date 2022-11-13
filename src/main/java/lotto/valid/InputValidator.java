b80e0dfcd0f5bd11eb36ec2ed469672fca17c44f
sonsoowon <yuwoo1018@gmail.com>
2022-11-14 00:39:44 +0900

    feat(InputValidator): 로또 구입 금액 입력이 문자를 포함하는 경우 예외 발생
    - 문자를 포함하는 경우를 별도의 예외로 분리
    - convertStringToInteger 메서드 삭제
    - checkNonDigit 메서드 추가

--- src/main/java/lotto/valid/InputValidator.java
+++ src/main/java/lotto/valid/InputValidator.java
@@ -4,22 +4,22 @@ import lotto.domain.LottoSalesman;
 
 public enum InputValidator {
     INSTANCE;
+    private static final String NOT_NATURAL_NUMBER_ERROR_MESSAGE = "[ERROR] 0이상의 정수를 입력해야 합니다.";
     private static final String MONEY_ERROR_MESSAGE = "[ERROR] 1000원 단위의 금액을 입력해야 합니다.";
+    private static final String NON_DIGIT = "\\D";
 
     public static void validateMoney(String moneyInput) {
-        int money = convertStringToInteger(moneyInput, MONEY_ERROR_MESSAGE);
+        checkNonDigit(moneyInput);
+        int money = Integer.parseInt(moneyInput);
         if (money < 0 || money % LottoSalesman.MONEY_UNIT != 0) {
             throw new IllegalArgumentException(MONEY_ERROR_MESSAGE);
         }
     }
 
-    private static Integer convertStringToInteger(String string, String errorMessage) {
-        try {
-            return Integer.parseInt(string);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(errorMessage);
+    private static void checkNonDigit(String string) {
+        if (string.matches(NON_DIGIT)) {
+            throw new IllegalArgumentException(NOT_NATURAL_NUMBER_ERROR_MESSAGE);
         }
     }
-
 
 }
--- src/test/java/lotto/valid/InputValidatorTest.java
+++ src/test/java/lotto/valid/InputValidatorTest.java
@@ -8,15 +8,19 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class InputValidatorTest {
-    private static final String MONEY_ERROR_MESSAGE = "[ERROR] 1000원 단위의 금액을 입력해야 합니다.";
-
-
-    @DisplayName("로또 구입 금액이 1000으로 나누어 떨어지는 0 이상의 수가 아니면 예외가 발생한다")
-    @ParameterizedTest
-    @ValueSource(strings = {"1500", "1,000", "-4000"})
-    void doesNotInputMoneyUnit1000(String string) {
+    @DisplayName("로또 구입 금액이 1000으로 나누어 떨어지지 않으면 예외가 발생한다")
+    @Test
+    void nonDigitInMoney() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> InputValidator.validateMoney(string))
-                .withMessage(MONEY_ERROR_MESSAGE);
+                .isThrownBy(() -> InputValidator.validateMoney("4500"));
     }
+
+    @DisplayName("입력된 로또 구입 금액이 정수 외의 문자를 포함하는 경우 예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,000", "-4000"})
+    void doesNotInputMoneyUnit1000(String moneyInput) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputValidator.validateMoney(moneyInput));
+    }
+
 }
