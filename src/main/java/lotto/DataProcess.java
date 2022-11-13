99e78fefa3d495063bf1dcaabe59d99d3c132676
SimSimEEE <rebe123@naver.com>
2022-11-14 03:07:48 +0900

    feat(DataProcess.class): function to get lotto number

---
+++ src/main/java/lotto/DataProcess.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataProcess {
+    private static final int MAXIMUM_NUMBER_OF_LOTTO = 45;
+    private static final int MINIMUM_NUMBER_OF_LOTTO = 1;
+    private static final int NUMBER_OF_LOTTO_NUMBER = 6;
+
+    public static List<List<Integer>> GetLottoNumber(int count) {
+        List<List<Integer>> numbers = new ArrayList<>();
+        for(int i = 0; i < count; i++)
+            numbers.add(Randoms.pickUniqueNumbersInRange(MINIMUM_NUMBER_OF_LOTTO, MAXIMUM_NUMBER_OF_LOTTO, NUMBER_OF_LOTTO_NUMBER));
+        return numbers;
+    }
+}
