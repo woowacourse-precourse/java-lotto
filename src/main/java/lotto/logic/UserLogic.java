bd7537dd6162eb80bca2e1e81ff73e981a70e29f
Jzakka <mouse4786@gmail.com>
2022-11-13 23:36:16 +0900

    feat: user 기능 추가

--- src/main/java/lotto/logic/UserLogic.java
+++ src/main/java/lotto/logic/UserLogic.java
@@ -1,6 +1,9 @@
 package lotto.logic;
 
+import lotto.domain.Lotto;
 import lotto.domain.User;
+
+import java.util.List;
 
 public class UserLogic {
     private static UserLogic userLogic = new UserLogic();
@@ -13,5 +16,25 @@ public class UserLogic {
     }
     public User makeUser(Integer money) {
         return new User(money);
+    }
+
+    public List<Lotto> userLottos(User user) {
+        return user.getLottos();
+    }
+
+    public Integer userLottoCounts(User user) {
+        return user.getLottoCount();
+    }
+
+    public void addLotto(User user, Lotto lotto) {
+        user.addLotto(lotto);
+    }
+
+    public void increasePrize(User user, Integer prize){
+        user.increasePrize(prize);
+    }
+
+    public double userYield(User user) {
+        return user.getYield();
     }
 }
