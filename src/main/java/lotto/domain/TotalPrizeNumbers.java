3c225f9cca708ca8460376c4e1b873dd7e246e56
great-park <cksgh1735@naver.com>
2022-11-14 02:02:37 +0900

    fix: 번호 종류별 오류 메시지 분류

--- src/main/java/lotto/domain/TotalPrizeNumbers.java
+++ src/main/java/lotto/domain/TotalPrizeNumbers.java
@@ -12,16 +12,19 @@ public class TotalPrizeNumbers {
     private final String PRIZENUMBER_SIZE_IS_NOT_SIX = "당첨 번호의 개수가 6개가 아닙니다.";
     private final String DUPLICATE_PRIZENUMBER_EXISTS = "당첨 번호 중 중복된 숫자가 포함되었습니다.";
     private final String PRIZENUMEBR_IS_NOT_IN_RANGE = "당첨 번호가 1부터 45까지의 범위 밖의 숫자가 포함되었습니다.";
+    private final String BONUSNUMEBR_IS_NOT_IN_RANGE = "보너스 번호가 1부터 45까지의 범위 밖의 숫자가 포함되었습니다.";
     private final String BONUSNUMBER_OVERLAPS = "보너스 번호가 입력한 당첨 번화 중 하나와 중복됩니다.";
     private final int MINIMUM_PRIZENUMBER = 1;
     private final int MAXIMUM_PRIZENUMBER = 45;
     private final int LOTTO_TOTAL_COUNT = 6;
 
+    private final List<PrizeNumber> totalPrizeNumbers;
+
     public TotalPrizeNumbers(List<Integer> prizeNumbers, Integer bonusNumber) {
         validatePrizeNumbers(prizeNumbers);
         validateBonusNumber(bonusNumber, prizeNumbers);
 
-        List<PrizeNumber> totalPrizeNumbers = createTotalPrizeNumbers(prizeNumbers, bonusNumber);
+        this.totalPrizeNumbers = createTotalPrizeNumbers(prizeNumbers, bonusNumber);
     }
 
     public void validatePrizeNumbers(List<Integer> prizeNumbers) {
@@ -41,7 +44,7 @@ public class TotalPrizeNumbers {
 
     private void validateBonusNumber(Integer bonusNumber, List<Integer> prizeNumbers) {
         if (bonusNumber < MINIMUM_PRIZENUMBER || bonusNumber > MAXIMUM_PRIZENUMBER) {
-            throw new IllegalArgumentException(PRIZENUMEBR_IS_NOT_IN_RANGE);
+            throw new IllegalArgumentException(BONUSNUMEBR_IS_NOT_IN_RANGE);
         }
         if (prizeNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(BONUSNUMBER_OVERLAPS);
