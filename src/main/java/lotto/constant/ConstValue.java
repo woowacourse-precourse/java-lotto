45c590980970c49695641d13c5f36db110919998
Kimhyeyun <cqqwer@gmail.com>
2022-11-13 20:59:34 +0900

    Refactor: REGEX에서 // -> \\로 변경

--- src/main/java/lotto/constant/ConstValue.java
+++ src/main/java/lotto/constant/ConstValue.java
@@ -6,6 +6,6 @@ public final class ConstValue {
     public static final int LOTTO_NUMBER_MAXIMUM = 45;
     public static final int PURCHASED_AMOUNT_UNIT = 1000;
 
-    public static final String DIGIT_REGEX = "^[//d]+$";
-    public static final String WINNING_NUMBER_INPUT_PATTERN = "^[//d]+,[//d]+,[//d]+,[//d]+,[//d]+,[//d]+$";
+    public static final String DIGIT_REGEX = "^[\\d]+$";
+    public static final String WINNING_NUMBER_INPUT_PATTERN = "^[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+$";
 }
