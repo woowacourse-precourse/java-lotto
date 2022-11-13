4e389001075c9ca8b1b3ca93aaca28a26e85790c
hwanee__ <fkfkthth@naver.com>
2022-11-13 19:30:33 +0900

    feat: 로또 번호 범위 초과 예외 구현 & 테스트 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,6 +11,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         validateDuplicate(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
@@ -25,6 +26,14 @@ public class Lotto {
 
         if (numbers.size() != checkDuplicate.size() ) {
             throw new IllegalArgumentException("[ERROR] 로또 번호 중 중복 숫자가 존재합니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for(int i=0;i<numbers.size();i++){
+            if(numbers.get(i)<1 || numbers.get(i)>45){
+                throw new IllegalArgumentException("[ERROR] 범위를 초과한 숫자가 존재합니다.");
+            }
         }
     }
 
