1b1c7c50528305a6ea05b708e23e90e7484420b5
taekwon-dev <taekwon_dev@naver.com>
2022-11-13 17:41:36 +0900

    feat: (Prize) 1등부터 5등까지 각 상수가 가져야 하는 값으로 로또 번호 몇 개를 맞춰야 하는지와 상금 정보 추가
    (ex. 6개 일치 (2,000,000,000원))

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,9 +1,15 @@
 package lotto.domain;
 
 public enum Prize {
-    FIRST_PLACE,
-    SECOND_PLACE,
-    THIRD_PLACE,
-    FOURTH_PLACE,
-    FIFTH_PLACE
+    FIRST_PLACE("6개 일치 (2,000,000,000원)"),
+    SECOND_PLACE("5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD_PLACE("5개 일치 (1,500,000원)"),
+    FOURTH_PLACE("4개 일치 (50,000원)"),
+    FIFTH_PLACE("3개 일치 (5,000원)");
+
+    private final String reward;
+
+    Prize(String reward) {
+        this.reward = reward;
+    }
 }
