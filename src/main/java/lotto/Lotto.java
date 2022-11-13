cc4dc47454f087eb3dd5feea31af0c65dfeb7e5c
haisley77 <qhfg3145@naver.com>
2022-11-13 16:57:20 +0900

    fix (printPurchasedLotto()): add sorting in order function

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -54,29 +55,33 @@ public class Lotto {
 
     // TODO: 추가 기능 구현
 
-    private void enterPriceOfLotto(){
+    private static void enterPriceOfLotto(){
         System.out.println("구입금액을 입력해주세요.");
         int price_of_lotto = Integer.parseInt(Console.readLine());
         calculateNumOfLotto(price_of_lotto);
     }
 
-    private void calculateNumOfLotto(int price_of_lotto){
+    private static void calculateNumOfLotto(int price_of_lotto){
         number_of_lotto = price_of_lotto / 1000;
         System.out.println("\n" + number_of_lotto + "개를 구매했습니다.");
     }
 
-    private void createRandomNumbers() {
+    private static void createRandomNumbers() {
         for (int count = 0; count < number_of_lotto; count++) {
             List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1,45,6);
             purchased_lotto_numbers.add(lotto);
         }
     }
 
-    private void printPurchasedLotto() {
+    private static void printPurchasedLotto() {
         for (int lotto = 0; lotto < purchased_lotto_numbers.size(); lotto++){
+            Collections.sort(purchased_lotto_numbers.get(lotto));
             System.out.println(purchased_lotto_numbers.get(lotto));
         }
     }
+
+
+
 
     private void enterBonusNumber() {
         bonus_number = Integer.parseInt(Console.readLine());
