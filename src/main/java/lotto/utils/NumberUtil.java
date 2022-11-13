92513286747c68c0240fad5452f2de60c8dc9349
Kid-Chang <pch3310@gmail.com>
2022-11-14 02:51:16 +0900

    fix(NumberUtil): 유틸의 클래스명 변경 및 스태틱 설정

--- src/main/java/lotto/utils/NumberUtil.java
+++ src/main/java/lotto/utils/NumberUtil.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class Number {
-    public List<Integer> createLottoNumber() {
+public class NumberUtil {
+    public static List<Integer> createLottoNumber() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }
