8310f6c38fd7f1afeeb357d33be9c6650224a86b
KangSungBum <peter9706@naver.com>
2022-11-13 16:11:46 +0900

    feat(Lotto.java): 로또 중복 숫자 포함 확인 함수 추가
    validateRedundantNumber 함수 추가
    - streams 사용하여 중복 요소 제거
    - 중복 제거 후 리스트 사이즈 6 안될 시 throw new IllegalArgumentException

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,17 +5,29 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateRedundantNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRedundantNumber(List<Integer> numbers) {
+        List<Integer> removeRedundantNumber = numbers.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        if (removeRedundantNumber.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
