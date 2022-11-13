891e8b60e383c4be5bbdfa8cfbef3746e0085353
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-13 18:00:13 +0900

    style: BoughtLottoTest 내에서 사용하지 않는 라이브러리 삭제

--- src/test/java/lotto/BoughtLottoTest.java
+++ src/test/java/lotto/BoughtLottoTest.java
@@ -1,15 +1,7 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import lotto.User.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.lang.reflect.Method;
-import java.util.List;
-
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
-import static org.assertj.core.api.Assertions.assertThat;
 
 class BoughtLottoTest {
 
