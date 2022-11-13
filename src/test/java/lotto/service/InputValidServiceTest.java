abfe648e15f6f1225e9ac4af26e65584dd7c8d07
suheonjoo <suheonj95@naver.com>
2022-11-13 21:18:34 +0900

    test(InputValidServiceTest): InputValidService 테스트 추가

--- src/test/java/lotto/service/InputValidServiceTest.java
+++ src/test/java/lotto/service/InputValidServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputValidServiceTest {
 
@@ -16,11 +18,9 @@ class InputValidServiceTest {
 		inputValidService = new InputValidService();
 	}
 
-	// @ParameterizedTest(name = "로토 번호가 1~45 가 아닌 경우 테스트")
-	// @ValueSource(strings = {"@#", })
-	@DisplayName("보너스 번호 valid 오류 테스트")
+	@DisplayName("보너스 번호 valid 오류 테스트 - 숫자가 아닌경우, 1~45 범위가 아닌 경우")
 	@Test
-	void validateBonusTest() {
+	void validateBonusErrorTest() {
 		Assertions.assertAll(
 			() -> assertThatThrownBy(() -> inputValidService.validateBonus("!@ ")),
 			() -> assertThatThrownBy(() -> inputValidService.validateBonus("46")),
@@ -28,8 +28,15 @@ class InputValidServiceTest {
 		);
 	}
 
+	@ParameterizedTest(name = "올바른 보너스 번호 테스트")
+	@ValueSource(strings = {"1", "2", "3"})
+	void validateBonusTest(String bonus) {
+		Assertions.assertDoesNotThrow(() -> inputValidService.validateBonus(bonus));
+	}
+
+	@DisplayName("당첨 번호 valid 오류 테스트 - 숫자가 아닌 경우, 1~45 범위가 아닌 경우, 6개의 숫자를 입력하지 않은경우")
 	@Test
-	void validAnswerTest() {
+	void validAnswerErrorTest() {
 		Assertions.assertAll(
 			() -> assertThatThrownBy(() -> inputValidService.validAnswer("1,2,3,4,5,6,7")),
 			() -> assertThatThrownBy(() -> inputValidService.validAnswer("1,2,3,!,5,6")),
@@ -37,12 +44,24 @@ class InputValidServiceTest {
 		);
 	}
 
+	@ParameterizedTest(name = "올바른 당첨 번호 테스트")
+	@ValueSource(strings = {"1,2,3,4,5,6", "1,2,3,4,5,45"})
+	void validAnswerTest(String answer) {
+		Assertions.assertDoesNotThrow(() -> inputValidService.validAnswer(answer));
+	}
+
+	@DisplayName("사용자가 입력한 돈 valid 테스트 - 1000으로 나눠지는지, 숫자인지 확인하는 테스트")
 	@Test
-	void validateUserMoneyTest() {
-		//given
+	void validateUserMoneyErrorTest() {
+		Assertions.assertAll(
+			() -> assertThatThrownBy(() -> inputValidService.validateUserMoney("6!!")),
+			() -> assertThatThrownBy(() -> inputValidService.validateUserMoney("412312"))
+		);
+	}
 
-		//when
-
-		//then
+	@ParameterizedTest(name = "올바른 사용자 지불 금액 테스트")
+	@ValueSource(strings = {"1231231000", "1000"})
+	void validateUserMoneyTest(String userMoneyPrice) {
+		Assertions.assertDoesNotThrow(() -> inputValidService.validateUserMoney(userMoneyPrice));
 	}
 }
