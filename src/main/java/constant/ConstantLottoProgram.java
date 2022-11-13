ae496c516ebdf408d71fbe402470d7236fc79715
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-13 13:23:08 +0900

    feat(CosntantLottoProgram) : 상수를 구현.
    로또 입력 숫자 최소, 최댓값 설정.
    로또 입력번호 길이 설정.

---
+++ src/main/java/constant/ConstantLottoProgram.java
@@ -0,0 +1,24 @@
+package constant;
+
+public enum ConstantLottoProgram {
+    RANGE_OF_LOTTO_NUMBER(1,45),LOTTO_LENGTH(6),LOTTO_LENGTH_WITH_BONUNS(7);
+    int length;
+    int minimum;
+    int maximum;
+    ConstantLottoProgram(int length){
+        this.length = length;
+    }
+    ConstantLottoProgram(int minimum, int maximum){
+        this.maximum = maximum;
+        this.minimum = minimum;
+    }
+    public int getMax(){
+        return maximum;
+    }
+    public int getLength(){
+        return length;
+    }
+    public int getMin(){
+        return minimum;
+    }
+}
