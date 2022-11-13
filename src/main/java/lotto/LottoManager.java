bcce923510321da1f2198d781ab3d254b7014f8b
JH-TT <leejongho9803@gmail.com>
2022-11-14 01:47:43 +0900

    feat(LottoManager) : 수익률을 계산해서 당첨 내역을 출력하는 함수 구현

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,19 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class LottoManager {
 
     private final int FIRST_PRIZE = 2_000_000_000;
     private final int SECOND_PRIZE = 30_000_000;
-    private final int THIRD_PRIZE = 1_500_000;
+    private final int THIRD_PRIZE = 1500000;
     private final int FOURTH_PRIZE = 50000;
     private final int FIFTH_PRIZE = 5000;
     private int[] prize = new int[]{0, 0, 0, 0, 0, 0};
-    private Judge judge;
+    private int[] rewards = new int[]{FIRST_PRIZE, SECOND_PRIZE, THIRD_PRIZE, FOURTH_PRIZE, FIFTH_PRIZE};
+    private final Judge judge;
+    private final LottoGenerator lottoGenerator;
+    private int amount;
+    private float yield;
 
     public LottoManager() {
         this.judge = new Judge();
+        this.lottoGenerator = new LottoGenerator();
     }
 
     public void compare(List<Lotto> lottoList, Lotto winningNumbers, int bonusNumber) {
@@ -24,6 +31,11 @@ public class LottoManager {
             bonus = lotto.getNumbers().contains(bonusNumber);
             checkResult(correct, bonus);
         }
+    }
+
+    public void inputAmount() {
+        String a = Console.readLine();
+        lottoGenerator.createLottoNumbers(a);
     }
 
     private void isFirst(int correct) {
@@ -63,7 +75,21 @@ public class LottoManager {
         isFifth(correct);
     }
 
-    public void printResult() {
+    private void calcYield() {
+        int total = 0;
+        for (int i = 0; i < prize.length; i++) {
+            total += prize[i] * rewards[i];
+        }
+        yield = (total / amount) * 100;
+    }
 
+    public void printResult() {
+        System.out.println("당첨 통계");
+        System.out.println("3개 일치 (5,000원) - " + prize[4] + "개");
+        System.out.println("4개 일치 (50,000원) - " + prize[3] + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + prize[2] + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + prize[1] + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + prize[0] + "개");
+        System.out.println(String.format("총 수익률은 %.2f입니다.", yield));
     }
 }
