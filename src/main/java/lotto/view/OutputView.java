627012c56a28ca96fa30886c5c47367c2ebafefd
Jeangyu <gjwlsrb1700@naver.com>
2022-11-14 00:39:10 +0900

    [FEAT: lotto] 당첨 통계를 출력하는 view 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -11,4 +11,14 @@ public class OutputView {
         }
         System.out.println();
     }
+
+    public static void writeWinningResult(List<Integer> numbers){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - "+numbers.get(0)+"개");
+        System.out.println("4개 일치 (50,000원) - "+numbers.get(1)+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+numbers.get(2)+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+numbers.get(3)+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+numbers.get(4)+"개");
+    }
 }
