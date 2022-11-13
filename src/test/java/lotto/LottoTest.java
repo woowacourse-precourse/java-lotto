3f986de36e4396f5832cec566b714818213b989f
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-13 17:48:51 +0900

    test : 로또 객체 선언하여 활용하는 방법으로 수정

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,7 +6,6 @@ import static lotto.view.WinningInput.winningCount;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import java.util.ArrayList;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +14,7 @@ import lotto.domain.Lotto;
 import java.util.List;
 
 class LottoTest {
+    private final Lotto lotto = createLotto();
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -32,24 +32,28 @@ class LottoTest {
     @DisplayName("로또 생성 메서드는 6개의 번호를 생성")
     @Test
     void createLottoTestSize() {
-        List<Lotto> lotto = new ArrayList<>(List.of(createLotto()));
-        assertThat(lotto.get(0).getNumbers().size() == 6).isTrue();
+        assertThat(lotto.getNumbers().size() == 6).isTrue();
     }
 
     @DisplayName("로또 생성 메서드는 1~45 범위의 숫자를 생성")
     @Test
     void createLottoTestByRange() {
-        List<Lotto> lotto = new ArrayList<>(List.of(createLotto()));
-        List<Integer> number = lotto.get(0).getNumbers();
-        for (int i = 0; i < lotto.get(0).getNumbers().size(); i++) {
+        List<Integer> number = lotto.getNumbers();
+        for (int i = 0; i < lotto.getNumbers().size(); i++) {
             assertThat(number.get(i) > 0 && number.get(i) < 46).isTrue();
         }
     }
 
-    @DisplayName("로또 번호와 일치하는 숫자 개수를 구하는 메서드 테스트")
+    @DisplayName("로또 번호와 일치하는 숫자 개수를 카운팅")
     @Test
     void getWinningResultTest() {
         getWinningResult(List.of(List.of(8, 21, 23, 41, 42, 43), List.of(3, 5, 11, 16, 32, 38)), List.of(8, 21, 23, 30, 32,38));
         assertThat(winningCount == 3).isTrue();
+    }
+
+    @DisplayName("로또 번호와 일치하는 보너스 번호가 있는지 확인")
+    @Test
+    void getBonusResultTest() {
+
     }
 }
