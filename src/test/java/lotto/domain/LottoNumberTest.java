5fc7d6defabf9f74270aeb64f9373fddfd2be195
EUNJEONGMUN <munej26@gmail.com>
2022-11-14 00:47:36 +0900

    test: 에러 메시지 상수 처리

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static constant.ErrorMessage.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,35 +18,35 @@ class LottoNumberTest {
     void 당첨번호_비정상입력_숫자x() {
         assertThatThrownBy(() -> new LottoNumber("1*,2,3,4,5,6", "7"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 숫자를 입력해주세요.");
+                .hasMessage(NOT_NUMBER);
     }
 
     @Test
     void 당첨번호_비정상입력_숫자범위초과() {
         assertThatThrownBy(() -> new LottoNumber("1,2,3,4,5,46", "7"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 1에서 45사이의 숫자를 입력해주세요.");
+                .hasMessage(NOT_LOTTO_NUMBER_RANGE);
     }
 
     @Test
     void 당첨번호_비정상입력_숫자범위미달() {
         assertThatThrownBy(() -> new LottoNumber("1,2,3,4,5,0", "7"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 1에서 45사이의 숫자를 입력해주세요.");
+                .hasMessage(NOT_LOTTO_NUMBER_RANGE);
     }
 
     @Test
     void 당첨번호_비정상입력_공백포함() {
         assertThatThrownBy(() -> new LottoNumber("1,2,3,4,5, ", "7"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 숫자를 입력해주세요.");
+                .hasMessage(NOT_NUMBER);
     }
 
     @Test
     void 당첨번호_비정상입력_숫자뒤공백포함() {
         assertThatThrownBy(() -> new LottoNumber("1 ,2,3,4,5,6", " "))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 숫자를 입력해주세요.");
+                .hasMessage(NOT_NUMBER);
     }
 
     @Test
