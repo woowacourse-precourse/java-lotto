5c07fd6c1c05acb96ecf9632395439bb105a3b26
chordpli <chordplaylist@gmail.com>
2022-11-13 13:57:31 +0900

    chore: add comment

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,26 @@
 package lotto;
 
 public class Application {
+    Lotto lotto;
+    User user = new User();
+    
+    public void run(){
+        // 로또 당첨 번호 초기화
+
+        // 구입 금액 입력
+
+        // 발행 로또 출력
+            // 로또 수량 출력
+            // 로또 번호 출력
+
+        // 당첨 번호 입력 (Lotto)
+
+        // 보너스 번호 입력
+
+        // 당첨 통계 출력
+            // 일치 항목 출력
+            // 수익률 출력
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
