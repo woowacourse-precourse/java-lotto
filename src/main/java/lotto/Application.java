52846aef12cc38317701a7da0091fe71a3e4c18d
Choi Seongwon <vlflalektzja@naver.com>
2022-11-13 22:58:25 +0900

    feat: 로또 구입 금액 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int money = Integer.parseInt(Console.readLine());
+        if (money % 1000 != 0) {
+            System.out.println("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+            throw new IllegalArgumentException();
+        }
+
     }
 }
