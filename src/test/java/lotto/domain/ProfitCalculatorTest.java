dae5064b551e8af62f275ada262c3a4a54fb98b2
5nam <ohnam90@gmail.com>
2022-11-13 14:58:14 +0900

    test: 수익률을 계산하여 소수점 둘째자리에서 반올림하여 리턴한다.

---
+++ src/test/java/lotto/domain/ProfitCalculatorTest.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ProfitCalculatorTest {
+    ProfitCalculator profitCalculator = new ProfitCalculator();
+    @DisplayName("수익률을 계산하여 소수점 둘째자리에서 반올림하여 리턴한다.")
+    @Test
+    void 수익률_계산() {
+        int pay = 8000;
+        int profit = 5000;
+        assertThat(profitCalculator.profitPercent(pay, profit)).isEqualTo("62.5%");
+    }
+}
