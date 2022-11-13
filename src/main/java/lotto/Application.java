7f513e2fad6759faafa246cc2c98cf759017f56a
cksdud8028 <cksdud8028@naver.com>
2022-11-13 20:10:07 +0900

    feat: add inputPurchaseAmount

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.input.Input;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public static Input inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int purchaseMoney = Integer.parseInt(Console.readLine());
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<String> winningNumbers = Arrays.asList(Console.readLine().split(","));
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonusNumber = Integer.parseInt(Console.readLine());
+        Input input = new Input(purchaseMoney, winningNumbers, bonusNumber);
+    }
+
 }
