3a240cc27d3cba4856163044fc9f9143a2dc5e42
HiiWee <wpdlzhf159@gmail.com>
2022-11-14 02:32:36 +0900

    feat: 컴퓨터는 입력금액 / 1,000 개의 로또를 발행하는 기능 추가

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,12 @@
+package lotto.service;
+
+import lotto.repository.LottoRepository;
+
+public class LottoService {
+    public static final int LOTTO_PRICE = 1000;
+    private final LottoRepository lottoRepository = LottoRepository.getInstance();
+
+    public void buyLottos(int inputPrice) {
+        lottoRepository.generateLottos(inputPrice / LOTTO_PRICE);
+    }
+}
