9a9ca827dccfd72818d306d9397f4e5326c134c3
Jaesun Park <41979078+ppakjae@users.noreply.github.com>
2022-11-13 21:59:46 +0900

    feat: 사용자의 구입 금액에 해당하는 만큼 로또 발행

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoManager {
     private InputManager inputManager;
+    private List<Lotto> lottos;
 
     public LottoManager(){
         this.inputManager = new InputManager();
@@ -10,6 +16,7 @@ public class LottoManager {
     public void startLotto() {
         int amount = inputManager.getPurchasingAmount();
         int count = countBuyableLotto(amount);
+        lottos = buyLotto(count);
     }
 
     public int countBuyableLotto(int amount) {
@@ -17,5 +24,16 @@ public class LottoManager {
         count = amount / 1000;
         
         return count;
+    }
+
+    public List<Lotto> buyLotto(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+        }
+        return lottos;
     }
 }
