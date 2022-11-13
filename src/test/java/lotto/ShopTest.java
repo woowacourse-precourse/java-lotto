bc182ca961418a615245b996913b54b1f9d1ad09
minaver <psmjoshua@gmail.com>
2022-11-14 01:23:49 +0900

    test(ShopTest): Shop이 생성되고 lottoPayed의 각각의 번호 숫자 범위가 정확한지 확인

--- src/test/java/lotto/ShopTest.java
+++ src/test/java/lotto/ShopTest.java
@@ -5,6 +5,7 @@ import lotto.model.Shop;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -13,7 +14,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class ShopTest {
     @DisplayName("Shop이 생성되고 lottoPayed의 수가 정확한지 확인")
     @Test
-    void getLottoPayed() {
+    void getLottoPayedSize() {
         // given
         final int price = 8000;
         final int size = price / Constant.lottoAmount;
@@ -24,6 +25,24 @@ public class ShopTest {
 
         // then
         assertThat(lottoPayed.size() == size).isTrue();
+    }
+
+    @DisplayName("Shop이 생성되고 lottoPayed의 각각의 번호 숫자 범위가 정확한지 확인")
+    @Test
+    void getLottoPayedNumberRange() {
+        // given
+        final int price = 8000;
+        final int size = price / Constant.lottoAmount;
+        final Shop shop = new Shop(price);
+
+        // when
+        final List<Lotto> lottoPayed = shop.getLottoPayed();
+        List<Integer> testNumbers = new ArrayList<>();
+        lottoPayed.stream()
+                .map(v -> testNumbers.addAll(v.getNumbers()));
+
+        // then
+        assertThat(testNumbers.stream().allMatch(n -> n >= 1 && n <= 45)).isTrue();
     }
 
     @DisplayName("Shop에 제시된 price가 1000원 단위로 떨어지지 않으면 예외가 발생한다.")
