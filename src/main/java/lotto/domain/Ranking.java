e3564299d1c6d4b4b9116f460f88d1095b3a0d72
keon0711 <fnc6488@gmail.com>
2022-11-14 03:48:58 +0900

    fix(lotto): check second place and third place by bonus number

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -1,28 +1,30 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public enum Ranking {
-    FIRST_PLACE(6, false, 2_000_000_000),
-    SECOND_PLACE(5, true, 30_000_000),
-    THIRD_PLACE(5, false, 1_500_000),
-    FOURTH_PLACE(4, false, 50_000),
-    FIFTH_PLACE(3, false, 5_000),
-    NOT_RANKED(0, false, 0);
+    FIRST_PLACE(2_000_000_000, (matchNumbers, bonusNumber) -> matchNumbers == 6),
+    SECOND_PLACE(30_000_000, (matchNumbers, bonusNumber) -> matchNumbers == 5 && bonusNumber),
+    THIRD_PLACE(1_500_000, (matchNumbers, bonusNumber) -> matchNumbers == 5 && !bonusNumber),
+    FOURTH_PLACE(50_000, (matchNumbers, bonusNumber) -> matchNumbers == 4),
+    FIFTH_PLACE(5_000, (matchNumbers, bonusNumber) -> matchNumbers == 3),
+    NOT_RANKED(0, (matchNumbers, bonusNumber) -> matchNumbers < 6);
 
-    private final int matchNumbers;
-    private final boolean bonusNumber;
     private final int reward;
+    private final BiPredicate<Integer, Boolean> rankCondition;
 
-    Ranking(int matchNumbers, boolean bonusNumber, int reward) {
-        this.matchNumbers = matchNumbers;
-        this.bonusNumber = bonusNumber;
+    Ranking(int reward, BiPredicate<Integer, Boolean> rankCondition) {
         this.reward = reward;
+        this.rankCondition = rankCondition;
     }
 
     public static Ranking getRank(int numberOfMatchNumbers, boolean bonusNumberMatch) {
         return Arrays.stream(Ranking.values())
-                .filter(ranking -> ranking.matchNumbers == numberOfMatchNumbers && ranking.bonusNumber == bonusNumberMatch)
+                .filter(rank -> rank.rankCondition.test(numberOfMatchNumbers, bonusNumberMatch))
                 .findAny()
                 .orElse(NOT_RANKED);
     }
