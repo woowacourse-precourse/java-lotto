4dc3f43c5d4e00c974b4fa34b6466d16b7713500
1eejisoo <jisu991012@gmail.com>
2022-11-14 03:16:11 +0900

    feat: 당첨 번호 유효성 검증 - 쉼표로 구분된 숫자들인가

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -3,10 +3,19 @@ package controller;
 import domain.Lotto;
 import service.LottoService;
 
+import java.util.regex.Pattern;
+
 public class LottoController {
+    private static final String FORMAT = "^[0-9],[0-9],[0-9],[0-9],[0-9],[0-9]$";
     private final LottoService lottoService = new LottoService();
 
     public Lotto createWinningNumber(String winningNumber) {
+        if (!isNumber(winningNumber))
+            throw new IllegalArgumentException("당첨 번호는 쉼표로 구분된 숫자여야 합니다.");
         return lottoService.createWinningNumber(winningNumber);
+    }
+
+    public boolean isNumber(String winningNumber) {
+        return Pattern.matches(FORMAT, winningNumber);
     }
 }
