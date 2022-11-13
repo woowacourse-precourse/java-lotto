b8477810115cf7b1ed338562bfea9b94b1ee99c0
jaehyeon <jhjh0022@naver.com>
2022-11-13 20:39:17 +0900

    feat: 데이터들 처리해주고 출력해주는 기능 구현

--- src/main/java/lotto/controller/Processing.java
+++ src/main/java/lotto/controller/Processing.java
@@ -1,2 +1,48 @@
-package lotto.controller;public class Processing {
+package lotto.controller;
+
+import lotto.console.Output;
+import lotto.domain.*;
+
+import java.util.List;
+
+public class Processing {
+    private final FindNumber findNumber = new FindNumber();
+    private final Rank rank = new Rank();
+    private final Output output = new Output();
+    private final Counter counter = new Counter();
+    private final Calculator calculator = new Calculator();
+    private final Reward reward = new Reward();
+
+
+    public void printAndDataProcessing(List<Lotto> lottos, List<Integer> winNumbers, int bonus, int money) {
+        output.printWinningStatistics();
+        output.printLine();
+        List<Integer> ranking = makeRanking(lottos, winNumbers, bonus);
+        printNumberOfMatches(ranking);
+        output.reportRateOfReturn(calculateTotalReturn(ranking, money));
+
+    }
+
+    private double calculateTotalReturn(List<Integer> ranking, int money) {
+
+        return calculator.df(reward.rewardMoneys(ranking), money);
+
+    }
+
+    private List<Integer> makeRanking(List<Lotto> lottos, List<Integer> winNumbers, int bonus) {
+        List<Integer> sameNumber = findNumber.findSameNumber(lottos, winNumbers, bonus);
+
+        return rank.rankNumbers(sameNumber);
+
+    }
+
+    private void printNumberOfMatches(List<Integer> ranking) {
+        output.printThree(counter.countFifth(ranking));
+        output.printFour(counter.countFourth(ranking));
+        output.printFive(counter.countThird(ranking));
+        output.printFiveFlus(counter.countSecond(ranking));
+        output.printSix(counter.countFirst(ranking));
+    }
+
+
 }
