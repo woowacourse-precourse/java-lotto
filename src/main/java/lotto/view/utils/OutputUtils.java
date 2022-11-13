e5337cb57cc211f96ad7dc82d4038188eb4a7df8
kingseungwooya <ksw_0505@nate.com>
2022-11-13 15:27:32 +0900

    feat(utils): create i/o utills

---
+++ src/main/java/lotto/view/utils/OutputUtils.java
@@ -0,0 +1,13 @@
+package lotto.view.utils;
+
+public class OutputUtils {
+
+    public static void output(String message) {
+        System.out.print(message);
+    }
+
+    public static void outputLine(String message) {
+        System.out.println(message);
+    }
+
+}
