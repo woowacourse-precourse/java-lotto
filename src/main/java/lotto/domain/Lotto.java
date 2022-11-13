5cfadb0b33259079f3c35084a22b7e48b7920111
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-13 19:09:03 +0900

    feat(Lotto): 입력된 숫자가 6개가 아닌 경우 예외 처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,19 +2,27 @@ package lotto.domain;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validateNumberSize(numbers);
+        validate(numbers);
         numbers.sort(Comparator.naturalOrder());
         this.numbers = numbers;
     }
 
+    private void validate(List<Integer> numbers) {
+        validateNumberSize(numbers);
+    }
+
     private void validateNumberSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또의 길이는 6이어야 합니다.");
+        final int LOTTO_NUMBER_SIZE = 6;
+        final String SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호의 개수는 6이어야 합니다.";
+
+        if (numbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
         }
     }
 
