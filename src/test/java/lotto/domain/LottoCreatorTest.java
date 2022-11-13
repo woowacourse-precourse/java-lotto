382dd62217f502d4fab27e1545a022775416affe
be-student <presentsong@naver.com>
2022-11-13 15:44:43 +0900

    test(LottoCreator): 로또 생성기 테스트 추가

---
+++ src/test/java/lotto/domain/LottoCreatorTest.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoCreatorTest {
+    @DisplayName("로또 생성기는 돈을 받아 그만큼의 로또를 생성해줌")
+    @ValueSource(ints = {1000, 2000, 1234000})
+    @ParameterizedTest
+    void case1(int input) {
+        RandomUniqueNumbers missionRandom = new MissionRandom();
+        LottoCreator lottoCreator = new LottoCreator(missionRandom);
+        assertThat(lottoCreator.createLottos(new Money(input)).getLottos().size()).isEqualTo(input / 1000);
+    }
+}
