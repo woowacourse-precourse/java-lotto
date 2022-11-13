907fc933f4ec6e5f822d151fcf25478b7832a564
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-11-14 03:11:50 +0900

    feat (User) : 로또 구매 금액 저장

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,4 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+// enum 사용해보자
 public class User {
+
+    private int money = 0;
+
+    private void setMoney() {
+        String input = Console.readLine();
+        this.money = Integer.parseInt(input);
+    }
+
 }
