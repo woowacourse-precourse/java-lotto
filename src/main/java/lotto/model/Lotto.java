12164951632abc0449196593b42530b37cdf2386
jiwon1217 <koagjw@gmail.com>
2022-11-13 21:26:24 +0900

    feat: 당첨 번호가 1~45까지의 범위에 포함되도록 예외처리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -4,10 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+    private static final String INVALID_LOTTO_SIZE = "[ERROR] 로또는 6개의 숫자로 이루어져야 합니다.";
+    private static final String INVALID_NUMBER_RANGE = "[ERROR] 로또 번호의 범위는 1 ~ 45까지 입니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        isValidLottoSize(numbers);
+        isValidNumberRange(numbers);
         this.numbers = numbers;
     }
 
@@ -15,9 +18,17 @@ public class Lotto {
         return Collections.unmodifiableList(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
+    private void isValidLottoSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_LOTTO_SIZE);
+        }
+    }
+
+    private void isValidNumberRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(INVALID_NUMBER_RANGE);
+            }
         }
     }
 }
--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,18 +1,32 @@
 package lotto.model;
 
+import java.util.Arrays;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class WinningLotto {
     private static final String INVALID_INPUT_PATTERN = "[ERROR] 입력 형식과 맞지 않습니다.";
     private static final Pattern INPUT_PATTERN = Pattern.compile("^[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+$");
 
+    private final Lotto winningLotto;
+
     public WinningLotto(String winningLotto) {
         isValidInputPattern(winningLotto);
+        convert(winningLotto);
+        this.winningLotto = new Lotto(Arrays.stream(winningLotto.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList()));
     }
 
     private void isValidInputPattern(String winningLotto) {
         if (!INPUT_PATTERN.matcher(winningLotto).matches()) {
             throw new IllegalArgumentException(INVALID_INPUT_PATTERN);
         }
+    }
+
+    private Lotto convert(String winningLotto) {
+        return new Lotto(Arrays.stream(winningLotto.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList()));
     }
 }
