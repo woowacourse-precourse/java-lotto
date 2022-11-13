dd47692491733a2b553c39840f387cf19400203c
Invidam <hansupark0208@gmail.com>
2022-11-13 15:25:08 +0900

    refactor(LottoWithBounsNumTest): reform 적용

--- src/test/java/lotto/domain/lotto/LottoWithBonusNumTest.java
+++ src/test/java/lotto/domain/lotto/LottoWithBonusNumTest.java
@@ -1,14 +1,12 @@
 package lotto.domain.lotto;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class LottoWithBonusNumTest {
 
     @Test
     void 중복_검사() {
-        for(int i=0;i<1000;++i) {
+        for (int i = 0; i < 1000; ++i) {
             new LottoWithBonusNum();
         }
     }
