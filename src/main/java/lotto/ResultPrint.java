148063beebfdc9a4c72b90a05d219881149506fc
MuSeung <peter0757@nvaer.com>
2022-11-14 08:47:31 +0900

    feat: ResultPrint가 2등을 출력하는 기능을 구현
    ResultPrint
        - printingResult : 5개 일치, 보너스 볼 일치 (30,000,000원) - 1개 을 출력하도록 함

---
+++ src/main/java/lotto/ResultPrint.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import java.util.Map;
+
+public class ResultPrint {
+
+    public ResultPrint(Map<String, Integer> prizesMap) {
+    }
+    public void printingResult() {
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - 1개");
+    }
+}
