04498f7df2b4600f7bc42ae0def102e008677222
KimJaeSung <behappy0916@naver.com>
2022-11-13 18:45:37 +0900

    refactor: import삭제

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -1,7 +1,6 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import lotto.domain.Money;
 import org.junit.jupiter.api.Test;
