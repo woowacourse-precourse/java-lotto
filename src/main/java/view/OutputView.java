e0b588ac8d6352a244fe4e8a8684986a8f18db0c
egoing <dudqk9696@naver.com>
2022-11-13 16:47:12 +0900

    feat : 당첨 결과 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,8 +2,12 @@ package view;
 
 import domain.LottoNumberList;
 import domain.LottoPurAmount;
+import domain.MatchingNumber;
+import util.ResultView;
+import util.WinNum;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class OutputView {
@@ -19,6 +23,17 @@ public class OutputView {
     public static void printAllLottoNumber(List<List<Integer>> lottonumberlist) {
         for(int i=0; i<lottonumberlist.size(); i++) {
             System.out.println(lottonumberlist.get(i).toString());
+        }
+    }
+
+    public static void printResult(MatchingNumber matchingNumber) {
+        ResultView[] values = ResultView.values();
+        WinNum[] values2 = WinNum.values();
+        HashMap<String,Integer> numberList = matchingNumber.getMatchingNumber();
+
+        for(int i=0; i< values.length; i++) {
+            String str = ResultView.valueOf(values2[i].toString()).label() + numberList.get(values2[i].toString()) + "개";
+            System.out.println(str);
         }
     }
 }
