6c90503ee5f637f9a0d057645a1746099f28a37c
kang jihyeong <her4386@gmail.com>
2022-11-13 10:38:55 +0900

    Feat / 보너스 번호 입력 및 입력값 검증

--- src/main/java/lotto/business/PickLotto.java
+++ src/main/java/lotto/business/PickLotto.java
@@ -18,6 +18,13 @@ public class PickLotto {
         return Arrays.stream(inputLottoNumber.split(",")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
     }
 
+    public int bonusNumber(){
+        String inputBonusNumber = inputBonusNumber();
+        validateBonusNumberIsRightNumber(inputBonusNumber);
+
+        return Integer.parseInt(inputBonusNumber);
+    }
+
     private String inputLottoNumber(){
         Output.printInputWinningNumber();
         return readLine();
@@ -37,16 +44,29 @@ public class PickLotto {
     private void validateInputLottoNumberIsRightSeparator(String inputLottoNumber){
         for(String number : inputLottoNumber.split(",")){
             try{
-                validateInputLottoNumberIsBetween(Integer.parseInt(number));
+                validateInputNumberIsBetween(Integer.parseInt(number));
             }catch (Exception e){
                 throw new IllegalArgumentException("[ERROR] 쉼표(,)로 구분된 숫자를 입력해야합니다.");
             }
         }
     }
 
-    private void validateInputLottoNumberIsBetween(int number){
+    private void validateInputNumberIsBetween(int number){
         if(number < Setting.LOTTO_MIN_NUMBER && number > Setting.LOTTO_MAX_NUMBER){
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 번호는 "+Setting.LOTTO_MIN_NUMBER+"부터 "+ Setting.LOTTO_MAX_NUMBER +" 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private String inputBonusNumber(){
+        Output.printBonusNumber();
+        return readLine();
+    }
+
+    private void validateBonusNumberIsRightNumber(String input){
+        try{
+            validateInputNumberIsBetween(Integer.parseInt(input));
+        }catch (Exception e){
+            throw new IllegalArgumentException("[ERROR] 하나의 숫자를 입력해야합니다.");
         }
     }
 
