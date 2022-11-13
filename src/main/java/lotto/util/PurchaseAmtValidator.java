19bc98e5abb404bca0993317f3f720a735192ddb
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-13 23:50:31 +0900

    feat: 입력값 숫자 확인 기능 구현
    입력값이 숫자로만 이루어져있지 않는 경우 IllegalArgumentException 발생시키는 API 개발

--- src/main/java/lotto/util/PurchaseAmtValidator.java
+++ src/main/java/lotto/util/PurchaseAmtValidator.java
@@ -4,11 +4,15 @@ import org.junit.platform.commons.util.StringUtils;
 
 public class PurchaseAmtValidator implements Validator{
     private final String EMPTY_ERROR = "[ERROR] 금액을 입력해주세요.";
+    private final String NUMERIC_ERROR = "[ERROR] 숫자만 입력해주세요.";
 
     @Override
     public void validate(String input) {
         if (StringUtils.isBlank(input)) {
             throw new IllegalArgumentException(EMPTY_ERROR);
+        }
+        if (!input.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException(NUMERIC_ERROR);
         }
     }
 }
