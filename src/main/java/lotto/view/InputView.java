557891990aef025d41f1c26e6f1fe194a40858b6
jiwon1217 <koagjw@gmail.com>
2022-11-13 20:40:54 +0900

    feat: 당첨 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    public static final String INPUT_WINNING_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
 
     public static String inputMoney() {
         System.out.println(INPUT_MONEY);
+        return Console.readLine();
+    }
+
+    public static String inputWinningLottoNumbers() {
+        System.out.println(INPUT_WINNING_LOTTO_NUMBERS);
         return Console.readLine();
     }
 }
