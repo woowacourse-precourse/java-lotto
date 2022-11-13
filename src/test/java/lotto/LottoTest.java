41d49d00fbd5201392440845fc7961c723af9e0a
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2022-11-13 20:40:44 +0900

    test(LottoTest): add test case "validateBetween1And45"

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -24,4 +27,11 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1에서 45 사이가 아닌 숫자가 있으면 예외가 발생한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,4,5,99", "0,10,20,30,40,45"})
+    void createLottyByOutOFRangeNumber(int a, int b, int c, int d, int e, int f) {
+        assertThatThrownBy(() -> new Lotto(List.of(a, b, c, d, e, f)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
