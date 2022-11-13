145e85e0f3fe2b28333a63457df8a6fdb76befbc
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-14 02:49:39 +0900

    feat : 테스트 코드 추가 작성
    checkBonusInputType() 구현

--- src/test/java/lotto/LottoCompanyTest.java
+++ src/test/java/lotto/LottoCompanyTest.java
@@ -12,28 +12,35 @@ public class LottoCompanyTest {
 
     @DisplayName("보너스번호가 여러 개 입력되면 예외 발생.")
     @Test
-    void 보너스번호갯수오류() {
+    void checkBonusInputLength() {
         assertThatThrownBy(() ->  lottoCompany.validateBonusOneNumber("3,4"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스번호에 숫자가 아닌 값이 입력되면 예외 발생.")
+    @Test
+    void checkBonusInputType() {
+        assertThatThrownBy(() ->  lottoCompany.validateBonusOneNumber("c"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("보너스번호의 값이 1보다 작거나 45보다 크면 예외 발생.")
     @Test
-    void 보너스번호1미만() {
+    void checkBonusUnderRange() {
         assertThatThrownBy(() ->  lottoCompany.validateBonusRange(0))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("보너스번호의 값이 1보다 작거나 45보다 크면 예외 발생.")
     @Test
-    void 보너스번호45초과() {
+    void checkBonusOverRange() {
         assertThatThrownBy(() ->  lottoCompany.validateBonusRange(46))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("보너스번호와 당첨 번호가 중복되면 예외 발생.")
     @Test
-    void 보너스번호와당첨번호중복() {
+    void checkBonusDuplicateWithWinningNumbers() {
         assertThatThrownBy(() ->  lottoCompany.validateBonus("1", List.of(1,2,3,4,5,6)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
