2deb73a20407fc25a4f2ec525583edc02f123a5f
1eejisoo <jisu991012@gmail.com>
2022-11-14 04:05:28 +0900

    test: 금액 형식 예외 테스트 추가

--- src/test/java/domain/MoneyTest.java
+++ src/test/java/domain/MoneyTest.java
@@ -1,21 +1,38 @@
 package domain;
 
+import controller.MoneyController;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class MoneyTest {
+    MoneyController moneyController = new MoneyController();
+
     @Test
     void 금액_단위_예외_테스트1() {
         assertThatThrownBy(() -> new Money(9500))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("구입 금액은 1,000원 단위여야 합니다.");
+                .hasMessageContaining("구매 금액은 1,000원 단위여야 합니다.");
     }
 
     @Test
     void 금액_단위_예외_테스트2() {
         assertThatThrownBy(() -> new Money(12345))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("구입 금액은 1,000원 단위여야 합니다.");
+                .hasMessageContaining("구매 금액은 1,000원 단위여야 합니다.");
+    }
+
+    @Test
+    void 금액_형식_예외_테스트1() {
+        assertThatThrownBy(() ->  moneyController.getPurchaseAmount("1000 1000"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("구매 금액은 숫자여야 합니다.");
+    }
+
+    @Test
+    void 금액_형식_예외_테스트2() {
+        assertThatThrownBy(() ->  moneyController.getPurchaseAmount("abc"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("구매 금액은 숫자여야 합니다.");
     }
 }
