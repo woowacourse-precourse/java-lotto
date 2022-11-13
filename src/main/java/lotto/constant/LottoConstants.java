48d24a71e8f2a202c65791855183bb1cc83488dc
CAEL0 <kjongmin26@gmail.com>
2022-11-13 14:51:28 +0900

    feat(LottoConstants): 로또 게임에 사용되는 상수들 정의

---
+++ src/main/java/lotto/constant/LottoConstants.java
@@ -0,0 +1,17 @@
+package lotto.constant;
+
+public class LottoConstants {
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+
+    public static final int WINNING_NUMBER_LENGTH = 6;
+    public static final int BONUS_NUMBER_LENGTH = 1;
+
+    public static final int LOTTO_PRICE = 1000;
+
+    public static final int FIFTH_PRIZE = 5_000;
+    public static final int FOURTH_PRIZE = 50_000;
+    public static final int THIRD_PRIZE = 1_500_000;
+    public static final int SECOND_PRIZE = 30_000_000;
+    public static final int FIRST_PRIZE = 2_000_000_000;
+}
