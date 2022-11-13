ad83888d440775a39441b3307e5de0e392207099
Devtist <yt930310@snu.ac.kr>
2022-11-14 00:28:32 +0900

    test: 각 등수별 당첨 개수를 구하는 기능에 대한 테스트코드 작성

---
+++ src/test/java/lotto/domain/WinningStatisticsCompilerTest.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+import lotto.constant.WinningRating;
+
+class WinningStatisticsCompilerTest {
+
+	@Test
+	void getCountsOfWins() {
+		List<Lotto> lotteries = List.of(
+			new Lotto(List.of(2, 12, 15, 21, 27, 37)),
+			new Lotto(List.of(4, 17, 22, 27, 41, 43)),
+			new Lotto(List.of(5, 17, 19, 22, 32, 41)),
+			new Lotto(List.of(2, 17, 19, 22, 32, 41)));
+		WinningAndBonusNumbers winningAndBonusNumbers;
+		winningAndBonusNumbers = new WinningAndBonusNumbers("5,17,19,22,32,41");
+		WinningAndBonusNumbers winningAndBonusNumbers1 = new WinningAndBonusNumbers(2);
+		WinningStatisticsCompiler winningStatisticsCompiler = new WinningStatisticsCompiler(lotteries,
+			winningAndBonusNumbers1);
+		assertThat(new ArrayList<>(winningStatisticsCompiler.getCountsOfWins().values())).isEqualTo(
+			List.of(1, 0, 0, 1, 1));
+	}
+}
