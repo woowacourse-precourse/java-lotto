65e7fbdcede4fbe5bab9e24d26a06e894a91539c
이정우 <acg6138@naver.com>
2022-11-13 22:00:26 +0900

    feat(UserLottos): 입력한 금액에 맞게 로또를 자동(랜덤)으로 발행하는 클래스 구현

---
+++ src/main/java/lotto/domain/UserLottos.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static lotto.domain.Lotto.LOTTO_NUMS_SIZE;
+import static lotto.domain.Lotto.MIN_LOTTO_NUM;
+import static lotto.domain.Lotto.MAX_LOTTO_NUM;
+
+public class UserLottos {
+    private final List<Lotto> lottos;
+
+    public UserLottos(int ticketCount) {
+        validate(ticketCount);
+        this.lottos = createLottos(ticketCount);
+    }
+
+    public void validate(int ticketCount) {
+        if (ticketCount <= 0) {
+            throw new IllegalArgumentException("[ERROR] 발행할 로또 개수는 적어도 하나 이상이여야 합니다.");
+        }
+    }
+
+    public List<Lotto> createLottos(int ticketCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < ticketCount; i++) {
+            List<Integer> nums = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUM, MAX_LOTTO_NUM, LOTTO_NUMS_SIZE);
+            nums.sort(Comparator.naturalOrder());
+            lottos.add(new Lotto(nums));
+        }
+        return lottos;
+    }
+
+    public List<Lotto> get() {
+        return lottos;
+    }
+}
