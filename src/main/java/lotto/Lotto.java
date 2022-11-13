8d17ee3e7c918070319eb53b7148548ee916b913
KimTaeKang57 <zizoen123@gmail.com>
2022-11-13 13:49:26 +0900

    feat: Lotto 클래스 예외사항 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,8 @@
 package lotto;
 
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,6 +14,16 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+        Integer max = numbers.stream().max(Integer::compareTo).orElseThrow();
+        Integer min = numbers.stream().min(Integer::compareTo).orElseThrow();
+        if (min < 1 || max > 45) {
+            throw new IllegalArgumentException(String.format("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."));
+        }
+
+        Set<Integer> set = new HashSet<>(numbers);
+        if (set.size() != numbers.size()) {
             throw new IllegalArgumentException();
         }
     }
