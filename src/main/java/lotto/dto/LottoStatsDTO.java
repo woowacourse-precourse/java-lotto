ac7ba33fad2b309c01c5fef50bc628a23139f424
Choi-JY <Choi-JY1107@github.com>
2022-11-13 17:04:26 +0900

    feat: 로또 당첨 통계 저장하는 DTO 구현

---
+++ src/main/java/lotto/dto/LottoStatsDTO.java
@@ -0,0 +1,46 @@
+package lotto.dto;
+
+import java.util.List;
+
+public class LottoStatsDTO {
+    private final Integer matchThreeNumbers;
+    private final Integer matchFourNumbers;
+    private final Integer matchFiveNumbers;
+    private final Integer matchFiveNumbersAndBonus;
+    private final Integer matchSixNumbers;
+    private final Integer yieldPercent;
+
+    public LottoStatsDTO(Integer matchThreeNumbers, Integer matchFourNumbers, Integer matchFiveNumbers,
+	Integer matchFiveNumbersAndBonus, Integer matchSixNumbers, Integer yieldPercent) {
+	this.matchThreeNumbers = matchThreeNumbers;
+	this.matchFourNumbers = matchFourNumbers;
+	this.matchFiveNumbers = matchFiveNumbers;
+	this.matchFiveNumbersAndBonus = matchFiveNumbersAndBonus;
+	this.matchSixNumbers = matchSixNumbers;
+	this.yieldPercent = yieldPercent;
+    }
+
+    public Integer getMatchThreeNumbers() {
+	return matchThreeNumbers;
+    }
+
+    public Integer getMatchFourNumbers() {
+	return matchFourNumbers;
+    }
+
+    public Integer getMatchFiveNumbers() {
+	return matchFiveNumbers;
+    }
+
+    public Integer getMatchFiveNumbersAndBonus() {
+	return matchFiveNumbersAndBonus;
+    }
+
+    public Integer getMatchSixNumbers() {
+	return matchSixNumbers;
+    }
+
+    public Integer getYieldPercent() {
+	return yieldPercent;
+    }
+}
