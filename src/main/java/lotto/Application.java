a6d83c88580d6d0d18642638543677eaeacd46f9
H4R1B0 <qkrguswns25@naver.com>
2022-11-13 21:11:15 +0900

    feat: check input purchase money

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,4 +4,15 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    private static boolean checkInputPurchaseMoney(String inputPurchaseMoney) {
+        try {
+            Validation.isIncludeNotNumber(inputPurchaseMoney);
+            Validation.isMultipleOf1000(inputPurchaseMoney);
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+        return true;
+    }
 }
