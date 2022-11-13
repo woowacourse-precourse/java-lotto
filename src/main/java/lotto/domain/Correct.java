c43a49b201be3c98e54c1def4d05e03687c7143c
bearjun05 <bearjun05@naver.com>
2022-11-14 04:11:04 +0900

    feat(Lotto) : 일치 하는 번호 타입 이넘 타입으로 구현

--- src/main/java/lotto/domain/Correct.java
+++ src/main/java/lotto/domain/Correct.java
@@ -1,5 +1,16 @@
 package lotto.domain;
 
 public enum Correct {
-    THREE, FOUR, FIVE, FIVE_BONUS, SIX
+    THREE(0), FOUR(0), FIVE(0), FIVE_BONUS(0), SIX(0), NOTHING(0);
+
+    private int cnt;
+    Correct(int cnt) {
+        this.cnt = cnt;
+    }
+    public int getCnt() {
+        return cnt;
+    }
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
+    }
 }
