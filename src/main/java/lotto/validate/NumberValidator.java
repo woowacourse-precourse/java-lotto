17ec03e697c1f20404aa037e3a41870d91ed3c49
ys200209 <lys200209@naver.com>
2022-11-13 17:46:04 +0900

    feat(lotto): 사용자가 구입 금액을 입력했을 때, 검증 케이스 추가
    추가한 기능
    - 공통 예외
      - 입력에 공백이 있다면 예외 발생
      - 입력에 문자가 섞여있다면 예외 발생
    - 사용자가 로또 구입 금액을 검증할 때 예외
      - 구입 금액으로 마이너스를 입력했다면 예외 발생
      - 구입 금액이 1000원 단위가 아니라면 예외 발생

--- src/main/java/lotto/validate/NumberValidator.java
+++ src/main/java/lotto/validate/NumberValidator.java
@@ -1,20 +1,41 @@
 package lotto.validate;
 
+import static lotto.error.ErrorType.*;
+
+import java.util.regex.Pattern;
 import lotto.error.ErrorType;
 
 public class NumberValidator {
 
-    public void validateBuyMoney(String money) {
-        if (Integer.parseInt(money) % 1000 != 0) {
-            throw new IllegalArgumentException(ErrorType.NOT_DIVIDE.showMessage());
+    /**
+     * 공통 검증 로직입니다.
+     */
+    private void validateCommon(String number) {
+        if (Pattern.matches(".*\\s.*", number)) {
+            throw new IllegalArgumentException(NOT_BLANK.showMessage());
         }
 
+        if (Pattern.matches(".*\\D.*", number)) {
+            throw new IllegalArgumentException(ONLY_NUMBERS.showMessage());
+        }
+    }
+
+    public void validateBuyMoney(String money) {
+        validateCommon(money);
+
         if (Integer.parseInt(money) < 0) {
-            throw new IllegalArgumentException(ErrorType.LOWER_MONEY.showMessage());
+            throw new IllegalArgumentException(LOWER_MONEY.showMessage());
+        }
+
+        if (Integer.parseInt(money) % 1000 != 0) {
+            throw new IllegalArgumentException(NOT_DIVIDE.showMessage());
         }
     }
 
     public void validateInputNumber(String input) {
+        validateCommon(input);
+
+
 
     }
 
