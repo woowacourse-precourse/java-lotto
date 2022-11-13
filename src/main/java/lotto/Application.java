e60e1356d4012dbf781655fbe38cc04fc9340738
whdcks420 <whdcks420@gmail.com>
2022-11-14 03:47:06 +0900

    feat: 로또게임 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.domain.LottoApplication;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            LottoApplication lottoApplication = new LottoApplication();
+            lottoApplication.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
