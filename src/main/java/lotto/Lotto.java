1815fba74c8d205d25aa35012edfd109293c6f66
Minjune <minjune8506@gmail.com>
2022-11-13 12:23:16 +0900

    feat(Lotto): 로또 번호를 검사하는 기능 추가
    - 6개의 숫자로 되어있는지 검사
    - 1 ~ 45 사이의 숫자인지 검사
    - 중복되지 않는 숫자인지 검사

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public class Lotto {
 
+    public static final int NUMBER_COUNT = 6;
+    public static final int NUMBER_RANGE_START = 1;
+    public static final int NUMBER_RANGE_END = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,12 +16,40 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateSize(numbers);
+        validateNonDuplicate(numbers);
+        validateNumberRange(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != NUMBER_COUNT) {
+            System.out.println("[ERROR] 6개의 숫자를 입력해야 합니다.");
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateNonDuplicate(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (numbers.indexOf(number) != numbers.lastIndexOf(number)) {
+                System.out.println("[ERROR] 중복된 숫자는 입력할 수 없습니다.");
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (!isInRange(number)) {
+                System.out.println("[ERROR] 1 ~ 45 사이의 숫자만 입력할 수 있습니다.");
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private boolean isInRange(int number) {
+        return NUMBER_RANGE_START <= number && number <= NUMBER_RANGE_END;
+    }
+    
     @Override
     public String toString() {
         return numbers.toString();
