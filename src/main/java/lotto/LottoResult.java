840c9598fc82124f17c80267c2b8a450e4254797
MuSeung <peter0757@nvaer.com>
2022-11-13 20:10:02 +0900

    feat: LottoResult에 총 당첨금이랑 총 로또 구매금을 계산해 저장하는 기능 구현
    LottoResult
        - LottoResult : Map에서 각 키를 각 등 수별로 각각 더해 당첨금과 구매금을 계산하여 totalWinnings, totalPurchase에 저장
        - yield : totalWinnings / totalPurchase로 수익률을 리턴;

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -2,12 +2,29 @@ package lotto;
 
 import java.util.Map;
 
-
 public class LottoResult {
 
+    double totalWinnings;
+    double totalPurchase;
+
     public LottoResult(Map<String, Integer> prizesMap) {
+        double winningsSum = 0;
+        double purchaseSum = 0;
+        winningsSum += prizesMap.get("FIRST") * 2000000000;
+        purchaseSum += prizesMap.get("FIRST");
+        winningsSum += prizesMap.get("SECOND") * 30000000;
+        purchaseSum += prizesMap.get("SECOND");
+        winningsSum += prizesMap.get("THIRD") * 1500000;
+        purchaseSum += prizesMap.get("THIRD");
+        winningsSum += prizesMap.get("FOURTH") * 50000;
+        purchaseSum += prizesMap.get("FOURTH");
+        winningsSum += prizesMap.get("FIFTH") * 5000;
+        purchaseSum += prizesMap.get("FIFTH");
+        purchaseSum += prizesMap.get("NOTHING");
+        totalWinnings = winningsSum;
+        totalPurchase = purchaseSum * Payment.LOTTO_PRICE;
     }
     public double yield() {
-        return 0.625;
+        return totalWinnings / totalPurchase;
     }
 }
