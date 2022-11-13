dcf0c28e58e74db12c3f2e747c721509a6e4c5d9
2jun0 <soo28819@gmail.com>
2022-11-13 23:12:56 +0900

    Test(LottoSeller): LottoSeller 테스트 이름 추가.

--- src/test/java/lotto/domain/lotto/LottoSellerTest.java
+++ src/test/java/lotto/domain/lotto/LottoSellerTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -14,8 +13,7 @@ class LottoSellerTest {
     @Nested
     class buyLottos {
 
-        @DisplayName("1000으로 나누어 떨어지지 않는 값을 입력할 때")
-        @ParameterizedTest
+        @ParameterizedTest(name = "1000으로 나누어 떨어지지 않는 값을 입력할 때: {0}")
         @CsvSource(value = {"100", "1001", "1100"})
         void caseError1(int money) {
             LottoSeller lottoSeller = new LottoSeller();
@@ -24,8 +22,7 @@ class LottoSellerTest {
                     .hasMessage("로또 구앱 금액은 1,000원 단위로 입력해야 합니다.");
         }
 
-        @DisplayName("0이나 음수의 값을 입력할 때")
-        @ParameterizedTest
+        @ParameterizedTest(name = "0이나 음수의 값을 입력할 때: {0}")
         @CsvSource(value = {"0", "-1000", "-10000", "-1234"})
         void caseError2(int money) {
             LottoSeller lottoSeller = new LottoSeller();
@@ -34,8 +31,7 @@ class LottoSellerTest {
                     .hasMessage("로또 구매 금액은 0보다 커야 합니다.");
         }
 
-        @DisplayName("1000으로 나누어지는 값을 입력할 때")
-        @ParameterizedTest
+        @ParameterizedTest(name = "1000으로 나누어지는 값을 입력할 때: {0}")
         @CsvSource(value = {"1000, 1", "2000, 2", "10000, 10", "500000, 500"})
         void caseSuccess(int money, int cnt) {
             LottoSeller lottoSeller = new LottoSeller();
