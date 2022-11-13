6df29843ef57ff58d6e6b55f3a2bf525f5bba1fd
JUHA <khabh@naver.com>
2022-11-13 11:50:43 +0900

    feat(LottoEstimator): 수익률 계산 기능 구현

--- src/main/java/lotto/domain/LottoEstimator.java
+++ src/main/java/lotto/domain/LottoEstimator.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class LottoEstimator {
+    private final static int MIN_MATCH_NUMBERS = 3;
     private final WinningLotto winningLotto;
     private final HashMap<LottoRank, Integer> rankStatics = new HashMap<>();
 
@@ -16,7 +17,7 @@ public class LottoEstimator {
         }
     }
 
-    public void rankLotteries(List<Lotto> lotteries) {
+    public void estimateLotteries(List<Lotto> lotteries) {
         lotteries.forEach(this::rankLotto);
     }
 
@@ -25,12 +26,27 @@ public class LottoEstimator {
         int matchCount = lotto.getMatchCountWith(winningLotto.numbers());
         boolean hasBonusNumber = lotto.hasBonusNumber(winningLotto.bonusNumber());
 
+        if (matchCount < MIN_MATCH_NUMBERS) {
+            return;
+        }
+
         for (LottoRank lottoRank : LottoRank.values()) {
             if (lottoRank.matchLottoRank(matchCount, hasBonusNumber)) {
                 int currentRankCount = rankStatics.get(lottoRank);
                 rankStatics.put(lottoRank, ++currentRankCount);
-                break;
+                return;
             }
         }
+    }
+
+    public float estimateRateOfProfit(int purchasePrice) {
+        float profit = 0;
+
+        for (LottoRank lottoRank : LottoRank.values()) {
+            profit += rankStatics.get(lottoRank) * lottoRank.price;
+        }
+        float rateOfProfit = (profit / purchasePrice) * 100;
+
+        return Math.round(rateOfProfit * 10) / 10.0f;
     }
 }
