af308e6b929bafe617f13ddb63e656afeac9b5bd
leesky <leehaneul0623@gmail.com>
2022-11-13 12:27:04 +0900

    chore(all) : 코드 초기화 및 문서 정리
    MVC로 재구현을 위해 코드 초기화 :
     - 문서 정리
     - 코드 초기화

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,6 @@
 package lotto;
 
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,41 +10,11 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
-
-    public int draw(List<Integer> answer, int bonus) {
-        int correct = getCorrectCount(answer);
-        if (correct == 6) return 1;
-        if (correct == 5 && getNumbers().contains(bonus)) return 2;
-        if (correct == 5) return 3;
-        if (correct == 4) return 4;
-        if (correct == 3) return 5;
-        return 0;
-    }
-
-    private int getCorrectCount(List<Integer> answer) {
-        int count = 0;
-        for (Integer number : getNumbers()) {
-            if (answer.contains(number)) count += 1;
-        }
-        return count;
-    }
-
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
-
-        Set<Integer> set = new HashSet<>(numbers);
-        if (set.size() != 6) {
-            throw new IllegalArgumentException();
-        }
     }
 
-    @Override
-    public String toString() {
-         return numbers.toString();
-    }
+    // TODO: 추가 기능 구현
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,10 @@
 package lotto;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,84 +18,10 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호의 6개 번호가 일치하면 1등이다.")
-    @Test
-    void winLottoBySixNumber() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        List<Integer> answer = List.of(1, 2, 3, 4, 5, 6);
-        int actual = lotto.draw(answer, 7);
-        assertThat(actual).isEqualTo(1);
-    }
-
-    @DisplayName("로또 번호의 5개 번호가 일치하고 보너스 번호가 일치하면 2등이다.")
-    @Test
-    void winLottoByFiveNumberAndBonusNumber() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
-        List<Integer> answer = List.of(1, 2, 3, 4, 5, 6);
-        int actual = lotto.draw(answer, 7);
-        assertThat(actual).isEqualTo(2);
-    }
-
-
-    @DisplayName("로또 번호의 5개 번호만 일치하면 3등이다.")
-    @Test
-    void winLottoByFiveCorrectNumber() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 12));
-        List<Integer> answer = List.of(1, 2, 3, 4, 5, 6);
-        int actual = lotto.draw(answer, 7);
-        assertThat(actual).isEqualTo(3);
-    }
-
-    @DisplayName("로또 번호의 4개 번호만 일치하면 4등이다.")
-    @Test
-    void winLottoByFourCorrectNumber() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 22, 23));
-        List<Integer> answer = List.of(1, 2, 3, 4, 5, 6);
-        int actual = lotto.draw(answer, 7);
-        assertThat(actual).isEqualTo(4);
-    }
-
-
-    @DisplayName("로또 번호의 3개 번호만 일치하면 5등이다.")
-    @Test
-    void winLottoByThreeCorrectNumber() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 22, 23, 24));
-        List<Integer> answer = List.of(1, 2, 3, 4, 5, 6);
-        int actual = lotto.draw(answer, 7);
-        assertThat(actual).isEqualTo(5);
-    }
-
-    @DisplayName("로또 번호의 2개 번호만 일치하면 낙첨이다.(0)")
-    @Test
-    void loseLottoByTwoCorrectNumber() {
-        Lotto lotto = new Lotto(List.of(1, 2, 22, 23, 24, 25));
-        List<Integer> answer = List.of(1, 2, 3, 4, 5, 6);
-        int actual = lotto.draw(answer, 7);
-        assertThat(actual).isEqualTo(0);
-    }
-
-    @DisplayName("로또 번호의 1개 번호만 일치하면 낙첨이다.(0)")
-    @Test
-    void loseLottoByOneCorrectNumber() {
-        Lotto lotto = new Lotto(List.of(1, 22, 23, 24, 25, 26));
-        List<Integer> answer = List.of(1, 2, 3, 4, 5, 6);
-        int actual = lotto.draw(answer, 7);
-        assertThat(actual).isEqualTo(0);
-    }
-
-    @DisplayName("로또 번호 중 아무것도 일치하지 않으면 낙첨이다.(0)")
-    @Test
-    void loseLottoByNonCorrectNumber() {
-        Lotto lotto = new Lotto(List.of(22, 23, 24, 25, 26, 27));
-        List<Integer> answer = List.of(1, 2, 3, 4, 5, 6);
-        int actual = lotto.draw(answer, 7);
-        assertThat(actual).isEqualTo(0);
-    }
-
-
+    // 아래에 추가 테스트 작성 가능
 }
-
