5e0ca45e8dfbff0c8de3aedc6bd8b86856a1df97
wonwoo6y <wonwoo6y@gmail.com>
2022-11-14 04:27:21 +0900

    feat: 로또 발행 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,7 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,9 +13,11 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR]: 로또의 사이즈는 6이여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
--- src/main/java/lotto/LottoVendingMachine.java
+++ src/main/java/lotto/LottoVendingMachine.java
@@ -1,10 +1,46 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoVendingMachine {
+
+    public static final int LOTTO_NUMBERS_SIZE = 6;
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 45;
 
     public static int getLottoTickets(int purchaseAmount) {
         int lottoTickets = purchaseAmount / 1000;
         System.out.printf("\n%d개를 구매했습니다.\n", lottoTickets);
         return lottoTickets;
+    }
+
+    public static List<Lotto> getLottos(int lottoTickets) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int count = 0; count < lottoTickets; count++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_VALUE, MAX_VALUE, LOTTO_NUMBERS_SIZE);
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+        }
+        printLottos(lottos);
+        return lottos;
+    }
+
+    private static void printLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            printLotto(lotto);
+        }
+    }
+
+    private static void printLotto(Lotto lotto) {
+        String output = "[";
+        for (int number : lotto.getNumbers()) {
+            output += String.valueOf(number);
+            output += ", ";
+        }
+        output = output.substring(0, output.length() - 2); // ", ".length() == 2 마지막 ", " 제거
+        output += "]";
+        System.out.println(output);
     }
 }
