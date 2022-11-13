ff51f077c83f3422298c9a345a5f58bc97f3a4a1
jaehyeon <jhjh0022@naver.com>
2022-11-13 21:13:09 +0900

    feat: 돈이 없다 =0 상수 추가

--- src/main/java/lotto/domain/chart/NumberChart.java
+++ src/main/java/lotto/domain/chart/NumberChart.java
@@ -4,7 +4,8 @@ public enum NumberChart {
     MIN_NUMBER(1),
     MAX_NUMBER(45),
     LOTTO_MONEY(1000),
-    LOTTO_SIZE(6);
+    LOTTO_SIZE(6),
+    EMPTY_MONEY(0);
 
     private final int value;
 
