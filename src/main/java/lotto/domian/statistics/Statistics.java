5afcaa98f5ed9ce86a08d8aee944bceaa7beabd5
DongwookKim0823 <dwkim0823@naver.com>
2022-11-13 19:53:29 +0900

    refactor(lotto): Statistics 클래스에서 등수를 반환하는 기능 분리

--- src/main/java/lotto/domian/statistics/Statistics.java
+++ src/main/java/lotto/domian/statistics/Statistics.java
@@ -1,36 +1,4 @@
 package lotto.domian.statistics;
 
-import java.util.List;
-
 public class Statistics {
-
-    public String checkLottoGrade(int correctNumbers) {
-        if (correctNumbers == 6) {
-            return "FIRST";
-        }
-        if (correctNumbers == 4) {
-            return "FOURTH";
-        }
-        if (correctNumbers == 3) {
-            return "FIFTH";
-        }
-        return null;
-    }
-
-    public String checkLottoNumbersContainBonusNumber(List<Integer> lottoNumbers, int bonusNumber) {
-        if (lottoNumbers.contains(bonusNumber)) {
-            return "SECOND";
-        }
-        return "THIRD";
-    }
-
-    public int countCorrectNumbers(List<Integer> winningNumbers, List<Integer> lottoNumbers) {
-        int correctNumbers = 0;
-        for (int lottoNumber : lottoNumbers) {
-            if (winningNumbers.contains(lottoNumber)) {
-                correctNumbers++;
-            }
-        }
-        return correctNumbers;
-    }
 }
