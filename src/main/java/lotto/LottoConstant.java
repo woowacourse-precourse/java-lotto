05aef194700661aea5e375c32ed5fbc33f72c2c3
Qulip <ilkwon.ryu@gmail.com>
2022-11-14 00:24:37 +0900

    feat: 게임 시작 메세지 출력 기능 구현

---
+++ src/main/java/lotto/LottoConstant.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class LottoConstant {
+    public static final String GAME_START_MESSAGE = "구입금액을 입력해 주세요.";
+
+    private LottoConstant() {
+    }
+}
---
+++ src/main/java/lotto/view/LottoView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+import static lotto.LottoConstant.GAME_START_MESSAGE;
+
+public class LottoView {
+    public void printStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+}
