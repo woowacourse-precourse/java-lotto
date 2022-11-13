01128e406dad17125efcb60ae812c093752b9bb3
JUNBEOM_LEE <bom1215@naver.com>
2022-11-13 11:48:05 +0900

    feat(Class UI): Money_Validate
    Add Money_Validate method to Class UI to filter the user money

--- src/main/java/lotto/UI.java
+++ src/main/java/lotto/UI.java
@@ -12,7 +12,17 @@ public class UI {
         return money;
     }
 
-
+    private void money_validate(String Input) {
+        try {
+            Integer money = Integer.valueOf(Input);
+            Integer remainder = money % 1000;
+            if (remainder != 0) {
+                throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위의 숫자여야 합니다.");
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위의 숫자여야 합니다.");
+        }
+    }
 //    public static void main(String[] args) {
 //        UI UI = new UI();
 //        UI.money();
