55a365d216507a4f6fd4af2d972edce0cee3a9ca
jamangstangs <jamangsong@gmail.com>
2022-11-14 00:35:52 +0900

    refactor(Rank): rank를 숫자로 표현하기 위해 필드 추가

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,20 +1,26 @@
 package lotto;
 
 public enum Rank {
-    FIRST(6,2000000000, "2,000,000,000원"),
-    SECOND(5, 30000000, "30,000,000,000원"),
-    THIRD(5, 1500000, "1,500,000원"),
-    FOURTH(4, 50000, "50,000원"),
-    FIFTH(3, 5000, "5,000원");
+    FIFTH(5, 3, 5000, "5,000원"),
+    FOURTH(4, 4, 50000, "50,000원"),
+    THIRD(3, 5, 1500000, "1,500,000원"),
+    SECOND(2, 5, 30000000, "30,000,000,000원"),
+    FIRST(1, 6, 2000000000, "2,000,000,000원");
 
+    private int rankInt;
     private int hitCount;
     private int rewardInt;
     private String rewardString;
 
-    Rank(int hitCount, int rewardInt, String rewardString) {
+    Rank(int rankInt, int hitCount, int rewardInt, String rewardString) {
+        this.rankInt = rankInt;
         this.hitCount = hitCount;
         this.rewardInt = rewardInt;
         this.rewardString = rewardString;
+    }
+
+    public int getRankInt() {
+        return rankInt;
     }
 
     public int getHitCount() {
