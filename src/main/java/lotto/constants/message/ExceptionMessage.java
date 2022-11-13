613c1b99fd3c2422f8b30df8bdd0a88f8b796706
HiiWee <wpdlzhf159@gmail.com>
2022-11-14 02:55:32 +0900

    feat: 숫자가 아닌 수를 입력하면 IllegaArgumentException예외 발생

---
+++ src/main/java/lotto/constants/message/ExceptionMessage.java
@@ -0,0 +1,11 @@
+package lotto.constants.message;
+
+public class ExceptionMessage {
+    public static final String ERROR = "[ERROR] ";
+    public static final String NUMERIC_LENGTH = "하나의 로또는 6개의 숫자로 이루어져야 합니다.";
+    public static final String DUPLICATED_NUMBER = "로또의 번호는 중복될 수 없습니다.";
+    public static final String NON_NUMERIC_INPUT = "숫자를 입력해야 합니다.";
+
+    private ExceptionMessage() {
+    }
+}
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,14 +1,25 @@
 package lotto.controller;
 
+import lotto.constants.message.ExceptionMessage;
 import lotto.service.LottoService;
 import lotto.views.InputView;
 
 public class LottoController {
+    public static final String ONLY_NUMBER = "^[0-9]*$";
+
     private final LottoService lottoService = new LottoService();
     private final InputView inputView = new InputView();
 
     public void startLottoApplication() {
         String inputPrice = inputView.inputPrice();
+        validateInputPrice(inputPrice);
         lottoService.buyLottos(Integer.parseInt(inputPrice));
     }
+
+    private void validateInputPrice(String inputPrice) {
+        if (!inputPrice.matches(ONLY_NUMBER)) {
+            throw new IllegalArgumentException(ExceptionMessage.ERROR + ExceptionMessage.NON_NUMERIC_INPUT);
+        }
+    }
+
 }
