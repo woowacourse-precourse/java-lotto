ca8f906fe627a6555900d73fb71acfe2b1f7eba2
김태용 <amico741@naver.com>
2022-11-13 16:39:43 +0900

    feat: 당첨 번호 생성 view 기능 추가

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -6,6 +6,8 @@ import lotto.domain.Lottos;
 public class View {
     private static final String WELCOME_MESSAGE = "구입 금액을 입력해 주세요.";
     private static final String COUNT_MESSAGE = "개를 구매하였습니다.";
+    private static final String INPUT_WINNING_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_MESSAGE= "보너스 번호를 입력해주세요.";
     public void printWelcomeMessage() {
         System.out.println(WELCOME_MESSAGE);
     }
@@ -16,5 +18,13 @@ public class View {
 
     public void printLottosInfoByBuyer(Lottos lottos) {
         lottos.forEach((lotto) -> System.out.println(lotto.getNumbers()));
+    }
+
+    public void printInputWinningMessage() {
+        System.out.println(INPUT_WINNING_MESSAGE);
+    }
+
+    public void printInputBonusMessage() {
+        System.out.println(INPUT_BONUS_MESSAGE);
     }
 }
