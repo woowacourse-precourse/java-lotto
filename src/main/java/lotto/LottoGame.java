b5276425046e7ede75a741495460341d7e76bb68
nakalic <nakalic28@gmail.com>
2022-11-13 23:19:09 +0900

    feat(Reward): 구매한 로또와 당첨 번호를 비교하여 당첨을 반환하는 기능 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -2,9 +2,19 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoGame {
+
+    private final Map<Reward, Integer> result = new LinkedHashMap<>() {{
+        put(Reward.FIFTH, 0);
+        put(Reward.FOURTH, 0);
+        put(Reward.THIRD, 0);
+        put(Reward.SECOND, 0);
+        put(Reward.FIRST, 0);
+    }};
 
     public List<Lotto> publishLotto(int count) {
         List<Lotto> lotto = new ArrayList<>();
@@ -13,5 +23,23 @@ public class LottoGame {
             lotto.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
         }
         return lotto;
+    }
+
+    public void calculateResult(List<Lotto> userLotto, Lotto winningNumber, int bonusNumber) {
+        for (int i = 0; i < userLotto.size(); i++) {
+            int bingo = userLotto.get(i)
+                    .getNumbers()
+                    .stream()
+                    .filter(s -> winningNumber.isContain(s))
+                    .mapToInt(n -> 1)
+                    .sum();
+
+            boolean bonus = userLotto.get(i).isContain(bonusNumber);
+
+            if (3 <= bingo && bingo <= 6) {
+                result.put((Reward.getRanKing(bingo, bonus)), result.get(Reward.getRanKing(bingo, bonus)) + 1);
+            }
+        }
+
     }
 }
--- src/main/java/lotto/Reward.java
+++ src/main/java/lotto/Reward.java
@@ -1,12 +1,15 @@
 package lotto;
 
 
+import java.util.Arrays;
+
 public enum Reward {
     FIRST(6, 2000000000),
     SECOND(5, 30000000),
     THIRD(5, 1500000),
     FOURTH(4, 50000),
-    FIFTH(3, 5000);
+    FIFTH(3, 5000),
+    BLANK(0,0);
 
     private final int count;
     private final int reward;
@@ -14,6 +17,17 @@ public enum Reward {
     Reward(int count, int reward) {
         this.count = count;
         this.reward = reward;
+    }
+
+    public static Reward getRanKing(int hit, boolean bonus) {
+        if (hit == THIRD.count && bonus) {
+            return SECOND;
+        }
+
+        return Arrays.stream(values())
+                .filter(ranking -> ranking.count == hit)
+                .findAny()
+                .orElse(BLANK);
     }
 
     public int getCount() {
@@ -24,4 +38,3 @@ public enum Reward {
         return reward;
     }
 }
-
