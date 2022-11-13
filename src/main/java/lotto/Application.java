ec28f79b7d8fe78af2e4ce434aec30871ac3be6c
jasonoh22 <jasonoh22@naver.com>
2022-11-13 12:50:30 +0900

    FEAT[java-lotto]: 당첨 갯수 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,51 @@
 package lotto;
 
+import lotto.domain.Compare;
+import lotto.domain.GenerateLotto;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GenerateLotto generator =  new GenerateLotto();
+        List<List<Integer>> lottoNumbers = new ArrayList<>();
+        int buyLotto = askLottoPurchase();
+        int maxCorrect = 0;
+        System.out.println(buyLotto+"개를 구매했습니다.");
+        for (int i = 0; i < buyLotto; i++) {
+            List<Integer> lottoNumber = generator.createLotto();
+            lottoNumbers.add(lottoNumber);
+        }
+        for (int i = 0; i < lottoNumbers.size(); i++) {
+            System.out.println(lottoNumbers.get(i));
+        }
+        List<Integer> askLottoNum = askLottoNum();
+        Compare compare = new Compare();
+        for (int i = 0; i < lottoNumbers.size(); i++) {
+            int result = compare.correctCount(lottoNumbers.get(i),askLottoNum);
+            if(result > maxCorrect){
+                maxCorrect = result;
+            }
+        }
+
+        System.out.println(maxCorrect+"개가 일치합니다.");
+    }
+
+    public static int askLottoPurchase(){
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        int number = Integer.parseInt(input);
+
+        return number/1000;
+    }
+
+    public static List<Integer> askLottoNum(){
+        List<Integer> result = new ArrayList<>();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        for(String number : input.split(",")){
+            result.add(Integer.valueOf(number));
+        }
+        return result;
     }
 }
