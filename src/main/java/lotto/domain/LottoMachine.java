2f98ab19468e14098b39d49d92a2460b790c7e8b
choyeram <choyeram8@gmail.com>
2022-11-13 23:49:54 +0900

    feat: 추출한 난수를 로또로 저장

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,7 +12,9 @@ public class LottoMachine {
     private static final int SIZE = 6;
 
     public List<Lotto> issueLottos(int lottoNum) {
+        List<Lotto> lottos = new ArrayList<>();
         List<Integer> numbers = getRandomNumbers();
+        lottos.add(new Lotto(numbers));
 
         return null;
     }
