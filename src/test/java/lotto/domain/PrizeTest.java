3ff754596b9788f1fbe03322937933685fc0adf1
EUNJEONGMUN <munej26@gmail.com>
2022-11-13 12:42:45 +0900

    test: 로또 번호 비교에 따른 Prize 타입 정상 반환 테스트 코드 작성

--- src/test/java/lotto/domain/PrizeTest.java
+++ src/test/java/lotto/domain/PrizeTest.java
@@ -1,7 +1,5 @@
-package lotto;
+package lotto.domain;
 
-import lotto.domain.MatchCount;
-import lotto.domain.Prize;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,17 +9,40 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PrizeTest {
+
     @DisplayName("로또 번호 비교에 따른 Prize 타입 테스트")
     @Test
     void findPrizeTypeTest() {
         Prize threeMatch = Prize.findPrizeType(new MatchCount(3, 0));
-        Prize answer = Prize.THREE_MATCH;
+        Prize threeMatchAnswer = Prize.THREE_MATCH;
+        Prize threeMatch2 = Prize.findPrizeType(new MatchCount(3, 1));
+        Prize threeMatch2Answer = Prize.THREE_MATCH;
+
+        Prize fourMatch = Prize.findPrizeType(new MatchCount(4, 0));
+        Prize fourMatchAnswer = Prize.FOUR_MATCH;
+        Prize fourMatch2 = Prize.findPrizeType(new MatchCount(4, 1));
+        Prize fourMatch2Answer = Prize.FOUR_MATCH;
+
+        Prize fiveMatch = Prize.findPrizeType(new MatchCount(5, 0));
+        Prize fiveMatchAnswer = Prize.FIVE_MATCH;
 
         Prize fiveBonusMatch = Prize.findPrizeType(new MatchCount(5, 1));
-        Prize answer2 = Prize.FIVE_BONUS_MATCH;
+        Prize fiveBonusMatchAnswer = Prize.FIVE_BONUS_MATCH;
 
-        assertThat(threeMatch).isEqualTo(answer);
-        assertThat(fiveBonusMatch).isEqualTo(answer2);
+        Prize sixMatch = Prize.findPrizeType(new MatchCount(6, 0));
+        Prize sixMatchAnswer = Prize.SIX_MATCH;
+
+        Prize noneMatch = Prize.findPrizeType(new MatchCount(2, 0));
+        Prize noneMatchAnswer = Prize.NONE;
+
+        assertThat(threeMatch).isEqualTo(threeMatchAnswer);
+        assertThat(threeMatch2).isEqualTo(threeMatch2Answer);
+        assertThat(fourMatch).isEqualTo(fourMatchAnswer);
+        assertThat(fourMatch2).isEqualTo(fourMatch2Answer);
+        assertThat(fiveMatch).isEqualTo(fiveMatchAnswer);
+        assertThat(fiveBonusMatch).isEqualTo(fiveBonusMatchAnswer);
+        assertThat(sixMatch).isEqualTo(sixMatchAnswer);
+        assertThat(noneMatch).isEqualTo(noneMatchAnswer);
     }
 
 
