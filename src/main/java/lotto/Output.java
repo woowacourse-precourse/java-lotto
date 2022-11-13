1c4514e3a84c263f238809b9501f2e619ed1847a
lacram <chemin9898@gmail.com>
2022-11-13 13:55:16 +0900

    refactor: 예시에 맞게 발행된 로또를 출력하도록 함수 추가

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -3,14 +3,15 @@ package lotto;
 import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Output {
 
     // 발행한 로또 수량 및 번호를 출력하는 기능
-    public static void printPurchasedLotto(int purchaseAmount, List<List<Integer>> lotto) {
+    public static void printPurchasedLotto(long purchaseAmount, List<Lotto> lottos) {
         System.out.println(purchaseAmount/1000 + "개를 구매했습니다.");
-        for (List<Integer> lott : lotto) {
-            System.out.println(lott.toString());
+        for (Lotto lotto : lottos) {
+            printLotto(lotto);
         }
     }
 
@@ -18,7 +19,7 @@ public class Output {
     public static void printLottoWinningDetails(long purchaseAmount, Map<LottoMatch, Long> winningDetails) {
         long winningAmount = 0L;
         DecimalFormat df = new DecimalFormat("###,###");
-        System.out.println("당첨 통계");
+        System.out.println("\n당첨 통계");
         System.out.println("---");
         for (Map.Entry<LottoMatch, Long> entry : winningDetails.entrySet()) {
             System.out.println(entry.getKey().getName() + " (" + df.format(entry.getKey().getValue()) + "원 ) - " + entry.getValue() + "개");
@@ -27,6 +28,12 @@ public class Output {
         System.out.println("총 수익률은 " + Calculate.getProfitRate(purchaseAmount, winningAmount) + "%입니다.");
     }
 
+    public static void printLotto(Lotto lotto) {
+        String numbers = lotto.getNumbers().stream().
+                map(Object::toString).
+                collect(Collectors.joining(", "));
+        System.out.println("[" + numbers + "]");
+    }
 }
 
 
