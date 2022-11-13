e0e39ab71ab3c03de5381997abbb68c7f0def4ce
SHEOMM <esh0821@snu.ac.kr>
2022-11-13 16:44:38 +0900

    feat: 로또 등수 반환 구현

--- src/main/java/lotto/domain/EachNumberCalculateService.java
+++ src/main/java/lotto/domain/EachNumberCalculateService.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.LinkedList;
+import lotto.model.Bonus;
 import lotto.model.Lotto;
 import lotto.model.Rank;
 
@@ -9,21 +10,37 @@ public class EachNumberCalculateService {
     private final static int BONUSNUMBER = 5;
 
     private Lotto winningLottery;
+    private int bonusNumber;
 
-    public EachNumberCalculateService(Lotto lottery){
+    public EachNumberCalculateService(Lotto lottery, int bonusNumber){
         winningLottery = lottery;
+        this.bonusNumber = bonusNumber;
     }
 
+    public Rank calculate(Lotto lottery){
+        int matchedNumber = howManyMatchesNumber(lottery);
+        Bonus bonus = Bonus.FALSE;
+
+        if(isNeedBonusCheck(matchedNumber) && isContainsBonus(lottery)){
+            bonus = Bonus.TRUE;
+        }
+
+        return Rank.getRank(matchedNumber, bonus);
+    }
 
     public int howManyMatchesNumber(Lotto lottery){
         return lottery.getNumbers().stream()
-                .filter( number -> winningLottery.getNumbers().contains(number))
+                .filter( number -> winningLottery.getNumbers().contains(number) )
                 .toArray()
                 .length;
     }
 
     private boolean isNeedBonusCheck(int matchedNumber){
         return matchedNumber == BONUSNUMBER;
+    }
+
+    private boolean isContainsBonus(Lotto lottery){
+        return lottery.getNumbers().contains(bonusNumber);
     }
 
 }
