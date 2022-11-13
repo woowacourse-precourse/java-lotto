4e4fff2fffbaa8921de683bdaa00701e3cd73e75
leeyongjun <leeyongjun0310@naver.com>
2022-11-14 03:26:18 +0900

    feat(lotto): 구입금액이 1000으로 나누어 떨어지지 않는지 확인하는 기능 구현

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -2,6 +2,13 @@ package lotto.domain;
 
 public class Purchase {
     public static int getLottoCount(int purchaseAmount) {
+        checkNotDivide(purchaseAmount);
         return purchaseAmount / Constant.LOTTO_PRICE;
+    }
+
+    private static void checkNotDivide(int purchaseAmount){
+        if ((purchaseAmount % Constant.LOTTO_PRICE) != 0){
+            throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다.");
+        }
     }
 }
