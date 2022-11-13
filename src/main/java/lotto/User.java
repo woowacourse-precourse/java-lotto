1d02d180e476f2456b4d90ecbb84f6e84a92642a
ijung-won <dlwnddnjs96@gmail.com>
2022-11-13 21:17:08 +0900

    feat(User): 구매한 로또 리스트 생성 메소드 구현

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,12 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class User {
-    List<List<Integer>> boughtLotto;
+    private List<List<Integer>> boughtLotto;
+    private int lottoCount;
 
     // 로또 구매 개수 구현
     public int buyingLotto(int money){
         return money / 1000;
+    }
+
+    // 구매한 크기만큼의 로또 제공 메소드
+    public List<List<Integer>> generatingLotto(List<List<Integer>> boughtLotto, int lottoCount){
+        for(int i = 0; i <lottoCount; i++){
+            while (boughtLotto.get(i).size() < 6){
+                int lottoNum = Randoms.pickNumberInRange(1,45);
+                if(!boughtLotto.get(i).contains(lottoNum)) boughtLotto.get(i).add(lottoCount);
+            }
+        }
+        return boughtLotto;
     }
 }
