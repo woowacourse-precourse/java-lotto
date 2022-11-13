1c4f5bd7ded8337abdc61ffaa55895bd9b1c75f9
taeyun-work <wmfajr1357@gmail.com>
2022-11-13 20:06:05 +0900

    test: Validator.winningNumberFormat 유닛테스트 작성

---
+++ src/test/java/lotto/ValidatorTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static lotto.domain.Validator.winningNumberDuplicate;
+import static lotto.domain.Validator.winningNumberFormat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ValidatorTest {
+    @DisplayName("당첨 번호를 형식에 맞게 입력하지 않으면 예외가 발생한다")
+    @Test
+    void winningNumberFormatValidation() {
+        assertThatThrownBy(() -> winningNumberFormat("1,2?3,4,123,4"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}
