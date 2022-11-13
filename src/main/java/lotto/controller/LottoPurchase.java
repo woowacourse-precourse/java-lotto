0956a6e558d7cfbe182c03ab26d535b7f0015894
jipyo0106 <68218081+jipyo0106@users.noreply.github.com>
2022-11-14 04:50:56 +0900

    feat: 로또 구매 금액 입력 기능 구현

---
+++ src/main/java/lotto/controller/LottoPurchase.java
@@ -0,0 +1,9 @@
+package lotto.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoPurchase {
+    private String getPurchaseAmountInput() {
+        return Console.readLine();
+    }
+}
