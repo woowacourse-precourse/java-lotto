c2d84e6ab0a4b6e45c0d70b546b2d21f294c4acd
khj1 <dhepd8849@gmail.com>
2022-11-13 13:11:16 +0900

    refactor: 당첨 통계 출력문을 상수로 처리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,9 +8,20 @@ import lotto.utils.ErrorMessage;
 import java.util.Map;
 
 public class OutputView {
+    public static final String DIVIDING_LINE = "---";
+    public static final String INFORM_PURCHASE_COUNT = "%d개를 구매했습니다.";
+    public static final String INFORM_PROFIT = "총 수익률은 %.1f%%입니다.";
+    public static final String INFORM_FIRST_PLACE_STATISTIC = "6개 일치 (2,000,000,000원) - %d개";
+    public static final String INFORM_SECOND_PLACE_STATISTIC = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+    public static final String INFORM_THIRD_PLACE_STATISTIC = "5개 일치 (1,500,000원) - %d개";
+    public static final String INFORM_FOURTH_PLACE_STATISTIC = "4개 일치 (50,000원) - %d개";
+    public static final String INFORM_FIFTH_PLACE_STATISTIC = "3개 일치 (5,000원) - %d개";
+    public static final String WINNING_STATISTIC = "당첨 통계";
+    public static final String WINNING_OVERVIEW_INTRO = WINNING_STATISTIC;
+
     public static void informPurchaseCount(Lottos lottos) {
         int count = lottos.countTotal();
-        System.out.println(String.format("%d개를 구매했습니다.", count));
+        System.out.println(String.format(INFORM_PURCHASE_COUNT, count));
     }
 
     public static void printIssuedLottos(Lottos lottos) {
@@ -27,20 +38,20 @@ public class OutputView {
     }
 
     private static void printProfit(double profit) {
-        System.out.print(String.format("총 수익률은 %.1f%%입니다.", profit));
+        System.out.print(String.format(INFORM_PROFIT, profit));
     }
 
     private static void printWinningCounts(Map<LottoResult, Integer> winningCounts) {
-        System.out.println(String.format("3개 일치 (5,000원) - %d개", winningCounts.get(LottoResult.FIFTH_PLACE)));
-        System.out.println(String.format("4개 일치 (50,000원) - %d개", winningCounts.get(LottoResult.FOURTH_PLACE)));
-        System.out.println(String.format("5개 일치 (1,500,000원) - %d개", winningCounts.get(LottoResult.THIRD_PLACE)));
-        System.out.println(String.format("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개", winningCounts.get(LottoResult.SECOND_PLACE)));
-        System.out.println(String.format("6개 일치 (2,000,000,000원) - %d개", winningCounts.get(LottoResult.FIRST_PLACE)));
+        System.out.println(String.format(INFORM_FIFTH_PLACE_STATISTIC, winningCounts.get(LottoResult.FIFTH_PLACE)));
+        System.out.println(String.format(INFORM_FOURTH_PLACE_STATISTIC, winningCounts.get(LottoResult.FOURTH_PLACE)));
+        System.out.println(String.format(INFORM_THIRD_PLACE_STATISTIC, winningCounts.get(LottoResult.THIRD_PLACE)));
+        System.out.println(String.format(INFORM_SECOND_PLACE_STATISTIC, winningCounts.get(LottoResult.SECOND_PLACE)));
+        System.out.println(String.format(INFORM_FIRST_PLACE_STATISTIC, winningCounts.get(LottoResult.FIRST_PLACE)));
     }
 
     private static void printOverViewIntro() {
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        System.out.println(WINNING_OVERVIEW_INTRO);
+        System.out.println(DIVIDING_LINE);
     }
 
     public static void printErrorMessage(IllegalArgumentException error) {
