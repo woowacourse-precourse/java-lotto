e9eb7308aef8115ffa0e251f3dccd25a6ae29c7e
hwanee__ <fkfkthth@naver.com>
2022-11-14 02:29:11 +0900

    docs: 보너스 번호 예외 처리 구현
    보너스 번호 범위 초과 / 보너스 번호와 당첨 번호 중복 예외 처리 구현

--- src/main/java/lotto/domain/WinLotto.java
+++ src/main/java/lotto/domain/WinLotto.java
@@ -16,7 +16,7 @@ public class WinLotto {
     public WinLotto() {
     }
 
-    public void setWinningNumbers(){
+    public void setWinningNumbers() {
         String input = Console.readLine();
         validateComma(input);
 
@@ -26,25 +26,38 @@ public class WinLotto {
         new Lotto(winningNumbers);
     }
 
-    public void setBounsNumber(){
+    public void setBounsNumber() {
         bonusNumber = Integer.parseInt(Console.readLine());
+        validateBonusNumberRange(bonusNumber);
+        validateDuplicateBonusNumber(bonusNumber,winningNumbers);
     }
 
-    public void validateComma(String input){
-        if(countComma(input,COMMA)!=5){
+    public void validateComma(String input) {
+        if (countComma(input, COMMA) != 5) {
             throw new IllegalArgumentException("[ERROR] 잘못된 (,)기준 입니다.");
         }
     }
 
-    public int countComma(String input,char comma){
+    public int countComma(String input, char comma) {
         int count = 0;
 
-        for (int i=0;i<input.length();i++){
-            if(input.charAt(i) == comma){
-                count ++;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == comma) {
+                count++;
             }
         }
         return count;
     }
 
+    private void validateBonusNumberRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호가 범위를 초과하였습니다.");
+        }
+    }
+
+    private void validateDuplicateBonusNumber(int number,List<Integer> numbers) {
+        if (numbers.contains(number)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호 중 보너스 번호와 중복되는 번호가 존재합니다.");
+        }
+    }
 }
