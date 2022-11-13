1c99938bbd92fc4952d197adbb955026e69ee783
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-13 16:45:29 +0900

    fix(OutputView): println->printf 로 바꿈

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,13 +3,13 @@ package lotto.view;
 import java.util.List;
 
 public class OutputView {
-    public static final String NUMBER_OF_USER_GAME_COUNT="개를 구매했습니다.";
+    public static final String NUMBER_OF_USER_GAME_COUNT="%d개를 구매했습니다.\n";
     public static final String START_OF_RESULT="당첨 통계";
     public static final String LINE_BREAK_OF_RESULT="---";
 
 
     public static void printGameCount(int userGameCnt){
-        System.out.println(Integer.toString(userGameCnt)+NUMBER_OF_USER_GAME_COUNT);
+        System.out.printf(NUMBER_OF_USER_GAME_COUNT, userGameCnt);
     }
 
     public static void printRandomNumbers(List<List<Integer>> randomNumbers) {
