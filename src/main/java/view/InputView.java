e61a6a57411995f5e6ec0dc88099d488ff2e7fd2
stk346 <stk346@gamil.com>
2022-11-13 20:20:40 +0900

    feat: 인풋에 대한 정보를 받고 출력하는 InputView 클래스 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,54 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String BUYING_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String ENTER_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+
+    public static int showBuyingAmountMessage() {
+        System.out.println(BUYING_AMOUNT_MESSAGE);
+        String s = Console.readLine();
+        int convertedAmount = getConvertedAmount(s);
+        validateIsNaturalNumber(convertedAmount);
+        return convertedAmount;
+    }
+
+    private static void validateIsNaturalNumber(int convertedAmount) {
+        if (convertedAmount < 0) {
+            throw new IllegalArgumentException("금액은 음수가 될 수 없습니다.");
+        }
+    }
+
+    private static int getConvertedAmount(String s) {
+        int convertedAmount;
+        try {
+            convertedAmount = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("금액은 숫자만 입력할 수 있습니다.");
+        }
+        return convertedAmount;
+    }
+
+    public static String showEnterBonusNumberMessage() {
+        System.out.println(BONUS_NUMBER_MESSAGE);
+        String stringBonusNumber = Console.readLine();
+        validateIsDigit(stringBonusNumber);
+        return stringBonusNumber;
+    }
+
+    private static void validateIsDigit(String stringBonusNumber) {
+        try {
+            Integer.parseInt(stringBonusNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("번호는 숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    public static String showEnterWinningNumberMessage() {
+        System.out.println(ENTER_WINNING_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
+}
