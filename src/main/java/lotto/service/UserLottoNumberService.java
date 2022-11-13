1a1d6cfafca1e942d9d779fe93a6703b8b1d54d9
Jeongthingamajig <netpetet@naver.com>
2022-11-13 20:20:43 +0900

    Refactor : UserLottoNumberService 클래스 리팩토링
    값을 가져올때 하나하나 돌면서 검증을 했던 것을 생성자에서 검증을 하도록 변경

--- src/main/java/lotto/service/UserLottoNumberService.java
+++ src/main/java/lotto/service/UserLottoNumberService.java
@@ -8,18 +8,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class UserLottoNumberService {
-    private final String[] lottoNumber = User.getUserInputLottoNumber();
+    private static final String[] lottoNumber = User.getUserInputLottoNumber();
 
-    // 생성자에서 검증하고 싶지만 "," 로 나뉘기 때문에 어쩔 수 없이 안에서 검증
     public List<Integer> getLottoNumber(){
         List<Integer> lottoNumbers = new ArrayList<>(Collections.emptyList());
 
-        for (String number : lottoNumber) { // 입력받은 로또 번호 숫자인지 검증
-            ValidUserInput.ValidInputNumberInteger(number);
-            ValidUserInput.ValidInputNumberOverZero(number);
-            ValidInputNumberOverZero(number);
-            ValidInputNumberRange(number);
-
+        for (String number : lottoNumber) {
             lottoNumbers.add(Integer.parseInt(number)); //통과했다면 List<>에 넣어줌
         }
         Collections.sort(lottoNumbers);
@@ -29,6 +23,12 @@ public class UserLottoNumberService {
 
     public UserLottoNumberService() {
         ValidInputNumberSize(lottoNumber);
+        for (String number : lottoNumber) { // 입력받은 로또 번호 검증
+            ValidUserInput.ValidInputNumberInteger(number);
+            ValidUserInput.ValidInputNumberOverZero(number);
+            ValidInputNumberOverZero(number);
+            ValidInputNumberRange(number);
+        }
     }
 
     public void ValidInputNumberSize(String[] input){
