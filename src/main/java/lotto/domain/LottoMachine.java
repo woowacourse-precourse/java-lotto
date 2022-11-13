98b2e2df7c3e28ba1bdeb533e5dc9bf5238658f7
fakerdeft <whakswp123@naver.com>
2022-11-13 14:57:42 +0900

    feat(getWinningNumbers): change 당첨 번호 예외처리 변경

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,16 +1,39 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Exceptions;
 import lotto.Lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class LottoMachine {
-    public static void pickLottoNumbers(int countLotto){
-        for(int i = 0; i < countLotto; i++){
-            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6));
+    public static List<List<Integer>> pickLottoNumbers(int countLotto) {
+        List<List<Integer>> lottoList = new ArrayList<>();
+        for (int i = 0; i < countLotto; i++) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             Collections.sort(lotto.getNumbers());
+            lottoList.add(lotto.getNumbers());
             System.out.println(lotto.getNumbers());
         }
+        return lottoList;
     }
+
+    public static List<String> getWinningNumbers(String readLine) {
+        List<String> winningNumbers;
+
+        winningNumbers = List.of(readLine.split(","));
+        Exceptions.excludeNoLottoNumbers(winningNumbers);
+        return winningNumbers;
+    }
+
+    public static List<String> getBonusNumber(String readLine) {
+        List<String> bonusNumber = new ArrayList<>();
+
+        Exceptions.getOutExcludingNumbers(readLine);
+        bonusNumber.add(readLine);
+        return bonusNumber;
+    }
+
 }
