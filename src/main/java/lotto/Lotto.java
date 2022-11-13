4acc9de487d45bc0c2d7d9cff1661a16bbb68867
anso33 <hyeon131812@gmail.com>
2022-11-13 19:27:12 +0900

    [FEAT](Lotto): function that calculate winning rank

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,18 +3,40 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+  private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+  public Lotto(List<Integer> numbers) {
+    validate(numbers);
+    this.numbers = numbers;
+  }   // Lotto(InputNumber.get)
+
+  private void validate(List<Integer> numbers) {
+    if (numbers.size() != 6) {
+      throw new IllegalArgumentException();
     }
+  }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+//  private boolean haveBonusNumber(LottoAnswer lottoAnswer) {
+//    return numbers.contains(lottoAnswer.getBonusNumber());
+//  }
+
+  public int winningRanking(LottoAnswer lottoAnswer) {
+    int winning_numbers = 0;
+
+    for (int i = 0; i < 6; i++) {
+      if (lottoAnswer.haveNumber(numbers.get(i))) {
+        winning_numbers++;
+      }
     }
+    if (winning_numbers == 5 && numbers.contains(lottoAnswer.getBonusNumber())) {
+      return 7;
+    }
+    return winning_numbers;
+  }
 
-    // TODO: 추가 기능 구현
+  public void printLottoNumbers() {
+
+  }
+
+
 }
