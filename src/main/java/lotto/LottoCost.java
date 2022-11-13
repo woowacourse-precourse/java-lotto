6f80b49387c1900a2aee2f664dbc46741661e7da
tjddnr7760 <tjddnr7760@naver.com>
2022-11-13 20:20:26 +0900

    test : LottoTest 테스트 함수 추가, LottoCostTest 테스트 함수 추가

--- src/main/java/lotto/LottoCost.java
+++ src/main/java/lotto/LottoCost.java
@@ -1,2 +1,17 @@
-package lotto;public class LottoCost {
+package lotto;
+
+import java.util.List;
+
+public class LottoCost {
+    private final int cost;
+
+    public LottoCost(int cost) {
+        this.cost = cost;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
