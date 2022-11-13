bcd1086f3a1f747320a920eeb2f76a2cd9a8adc6
Your Name <you@example.com>
2022-11-13 15:56:17 +0900

    feat(lottoStore) : 사용자 로또 구입금액 입력 추가
    구입 금액 숫자인지 검증, 구입 금액 1,000 단위인지 검증 추가

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자에게 로또 구입과 발행을 관리
  *
@@ -12,5 +14,31 @@ public class LottoStore {
      */
     public void printBuyAmount() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    /**
+     * 사용자에게 로또 구입금액 입력 받기
+     */
+    public int inputBuyAmount() {
+        String buyAmountBill = Console.readLine();
+        int buyAmount = validateConvertNumber(buyAmountBill);
+        validateThousandUnits(buyAmount);
+        return buyAmount;
+    }
+
+    private int validateConvertNumber(String buyAmountBill) {
+        try {
+            return Integer.valueOf(buyAmountBill);
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 구입 금액은 숫자를 입력하셔야 합니다.");
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private void validateThousandUnits(int buyAmount) {
+        if (buyAmount % 1000 > 0) {
+            System.out.println("[ERROR] 구입 금액은 1,000 단위로 입력하셔야 합니다.");
+            throw new IllegalArgumentException();
+        }
     }
 }
