b15ec5f7a66876bc7e28e277984e7abcfbc3748a
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-13 15:15:12 +0900

    feat(test): 당첨 결과 계산 테스트 기능 구현

--- src/test/java/lotto/domain/CompareLottoTest.java
+++ src/test/java/lotto/domain/CompareLottoTest.java
@@ -21,9 +21,9 @@ public class CompareLottoTest {
         List<Rank> ranks = RankGenerator.getRanks();
         List<Lotto> lottos = new ArrayList<>();
         lottos.add(new Lotto(List.of(1,2,3,4,5,6)));
-        List<Integer> winningNumbers1 = List.of(1,2,3,4,5,6);
+        List<Integer> winningNumbers = List.of(1,2,3,4,5,6);
         //when
-        CompareLotto.compare(ranks, lottos, winningNumbers1, 10);
+        CompareLotto.compare(ranks, lottos, winningNumbers, 10);
         //then
         assertThat(ranks.stream()
                 .filter(r -> r.getRanking() == 1)
@@ -39,14 +39,68 @@ public class CompareLottoTest {
         List<Rank> ranks = RankGenerator.getRanks();
         List<Lotto> lottos = new ArrayList<>();
         lottos.add(new Lotto(List.of(1,2,3,4,5,6)));
-        List<Integer> winningNumbers2 = List.of(1,2,3,4,5,9);
+        List<Integer> winningNumbers = List.of(1,2,3,4,5,9);
         //when
-        CompareLotto.compare(ranks, lottos, winningNumbers2, 6);
+        CompareLotto.compare(ranks, lottos, winningNumbers, 6);
         //then
         assertThat(ranks.stream()
                 .filter(r -> r.getRanking() == 2)
                 .findFirst().get()
                 .getTotalReward())
                 .isEqualTo(RankRewardStatus.rank2.getReward());
+    }
+
+    @DisplayName("당첨 결과를 계산한다. 3등일 때")
+    @Test
+    void compare3() {
+        //given
+        List<Rank> ranks = RankGenerator.getRanks();
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(List.of(1,2,3,4,5,6)));
+        List<Integer> winningNumbers = List.of(1,2,3,4,5,9);
+        //when
+        CompareLotto.compare(ranks, lottos, winningNumbers, 45);
+        //then
+        assertThat(ranks.stream()
+                .filter(r -> r.getRanking() == 3)
+                .findFirst().get()
+                .getTotalReward())
+                .isEqualTo(RankRewardStatus.rank3.getReward());
+    }
+
+    @DisplayName("당첨 결과를 계산한다. 4등일 때")
+    @Test
+    void compare4() {
+        //given
+        List<Rank> ranks = RankGenerator.getRanks();
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(List.of(1,2,3,4,5,6)));
+        List<Integer> winningNumbers2 = List.of(1,2,3,4,8,9);
+        //when
+        CompareLotto.compare(ranks, lottos, winningNumbers2, 6);
+        //then
+        assertThat(ranks.stream()
+                .filter(r -> r.getRanking() == 4)
+                .findFirst().get()
+                .getTotalReward())
+                .isEqualTo(RankRewardStatus.rank4.getReward());
+    }
+
+    @DisplayName("당첨 결과를 계산한다. 5등일 때")
+    @Test
+    void compare5() {
+        //given
+        List<Rank> ranks = RankGenerator.getRanks();
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(List.of(1,2,3,4,5,6)));
+        List<Integer> winningNumbers2 = List.of(1,2,3,7,8,9);
+        //when
+        CompareLotto.compare(ranks, lottos, winningNumbers2, 6);
+        //then
+        assertThat(ranks.stream()
+                .filter(r -> r.getRanking() == 5)
+                .findFirst().get()
+                .getTotalReward())
+                .isEqualTo(RankRewardStatus.rank5.getReward());
     }
 }
