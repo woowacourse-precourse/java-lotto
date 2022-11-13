1e73d5089dc07908fc7b980a50a6f276c196bc21
Nugur1i <duckhee092@naver.com>
2022-11-14 00:11:09 +0900

    test(LottoTest): generate amount each win list

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -141,6 +141,21 @@ class LottoTest {
 
     }
 
+    @DisplayName("각 등수를 몇 번씩 맞췄는지, 리스트로 반환한다.")
+    @Test
+    void generateAmountEachWinList() {
+
+        List<Integer> winList = new ArrayList<>(Arrays.asList(3,5,7,6,4,2,1,4,2,6,4,2,6));
+
+        List<Integer> amountEachWinList = new ArrayList<>(Arrays.asList(1,3,1,3,1));
+
+        Printer printer = new Printer();
+
+        assertEquals(printer.knowAmountEachWin(winList),amountEachWinList);
+
+
+    }
+
 
 
 }
