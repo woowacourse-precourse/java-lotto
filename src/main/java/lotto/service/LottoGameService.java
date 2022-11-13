00ee3dc52afb592adfcda6d74825caaecb5f19f7
jihun-24k <gi020477@gmail.com>
2022-11-13 23:17:21 +0900

    Feat(LottoGameService): 구입 금액 유효성 검사

--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -1,12 +1,25 @@
 package lotto.service;
 
+import java.util.regex.Pattern;
+import lotto.message.ErrorMessage;
+
 public class LottoGameService {
 
     public int inputMoney(final String money) {
         validMoney(money);
+        return 0;
     }
 
-    private void validMoney(String money) {
+    private void validMoney(final String money) {
+        if (!Pattern.matches("^[0-9]*$", money)) {
+            throw new IllegalArgumentException(
+                ErrorMessage.PURCHASE_AMOUNT_NOT_NUMBER.getErrorMessage());
+        }
 
+        Integer value = Integer.valueOf(money);
+        if (value / 1000 != 0) {
+            throw new IllegalArgumentException(
+                ErrorMessage.PURCHASE_AMOUNT_DIVIDE_1000WON.getErrorMessage());
+        }
     }
 }
