f142218c2c89433305359e0c1fed2e1a3524055b
김동규 <2016025196@hanyang.ac.kr>
2022-11-13 23:22:16 +0900

    LottoBuyer 클래스 작성

--- src/main/java/lotto/LottoBuyer.java
+++ src/main/java/lotto/LottoBuyer.java
@@ -1,4 +1,47 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static constant.LottoNumber.LOTTO_PRICE;
+import static constant.LottoNumber.MIN_LOTTO_NUMBER;
+import static constant.LottoNumber.MAX_LOTTO_NUMBER;
+import static constant.LottoNumber.REQUIRE_LOTTO_NUMBER_COUNT;
+
 public class LottoBuyer {
+    private int money;
+    private List<Lotto> buyersLotto;
+    public LottoBuyer(){
+        buyersLotto = new ArrayList<>();
+    }
+    public void getMoney(){
+        String moneyInput = Console.readLine();
+        InputUtility.isDigit(moneyInput);
+        Integer buyerMoney = InputUtility.convertToInteger(moneyInput);
+        canDivMoneyByPrice(buyerMoney);
+        this.money = buyerMoney;
+    }
+    public void buyLotto(){
+        int lottoCount = money/LOTTO_PRICE.getValue();
+        for(int i=0; i<lottoCount; ++i){
+            List<Integer> newLottoNumber = makeRandomLottoNumber();
+            buyersLotto.add(new Lotto(newLottoNumber));
+        }
+    }
+    private List<Integer> makeRandomLottoNumber(){
+        List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER.getValue(),
+                MAX_LOTTO_NUMBER.getValue(), REQUIRE_LOTTO_NUMBER_COUNT.getValue());
+        lottoNumber.sort(null);
+        return lottoNumber;
+    }
+    private void canDivMoneyByPrice(Integer money){
+        if(money % LOTTO_PRICE.getValue() != 0){
+            throw new IllegalArgumentException("[ERROR] : 돈이 "+LOTTO_PRICE.getValue()+"로 나누어 떨어지지 않습니다.");
+        }
+    }
+
 }
