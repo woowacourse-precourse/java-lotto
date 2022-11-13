43ea75de3ef61e1b805145975f2eb47ff73435ad
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-14 06:13:23 +0900

    feat(view/InputString.java): 상수 문자열 구현

--- src/main/java/lotto/domain/game/Lotto.java
+++ src/main/java/lotto/domain/game/Lotto.java
@@ -1,16 +1,16 @@
-package lotto.game;
+package lotto.domain.game;
 
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(String lottoNumbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateIntLottoNumbers(String lottoNumbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
---
+++ src/main/java/lotto/view/InputString.java
@@ -0,0 +1,39 @@
+package lotto.view;
+
+public enum InputString {
+    PURCHASE_MONEY_MESSAGE {
+        @Override
+        public String toString(){
+            return "구입금액을 입력해 주세요.";
+        }
+    },
+
+    WINNING_NUMBER_MESSAGE {
+        @Override
+        public String toString(){
+            return "당첨 번호를 입력해 주세요.";
+        }
+    },
+
+    BONUS_NUMBER_MESSAGE {
+        @Override
+        public String toString(){
+            return "보너스 번호를 입력해 주세요.";
+        }
+    },
+
+    NO_NUMBER_INPUT {
+        @Override
+        public String toString(){
+            return "숫자만 입력해주세요.";
+        }
+    },
+
+    NO_NUMBER_AND_COMMA_INPUT {
+        @Override
+        public String toString(){
+            return "숫자와 콤마만 입력해주세요.";
+        }
+    }
+
+}
