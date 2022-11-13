28bbcff135ed966506290487bed96c0f346fc147
hodomaroo <enghemdwkd@gmail.com>
2022-11-13 15:08:17 +0900

    [Feat] : LottoPocket 기능 목록 선언 완료
    Docs 명세에 따라 LottoPocket 클래스의 각 기능별 메서드를 선언하였습니다;

--- src/main/java/lotto/LottoPocket.java
+++ src/main/java/lotto/LottoPocket.java
@@ -1,10 +1,34 @@
 package lotto;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 플레이어가 발행한 전체 로또 목록을 관리하는 클래스
  */
 public class LottoPocket {
+
     private List<Lotto> lottoGroup;
+
+    public LottoPocket(List<Lotto> lottoGroup) {
+        this.lottoGroup = lottoGroup;
+    }
+
+    public ScratchResult getScratchResult(WinningLotto winningLotto) {
+        return null;
+    }
+
+    private Map<LottoGrade, Integer> getNumberOfWinsByGrade(WinningLotto winningLotto) {
+        return null;
+    }
+
+    private long getTotalEarning(Map<LottoGrade, Integer> numberOfWinsByGrade) {
+        return 0;
+    }
+
+    private double getEarningRate(double totalEarning) {
+        return 0;
+    }
 }
