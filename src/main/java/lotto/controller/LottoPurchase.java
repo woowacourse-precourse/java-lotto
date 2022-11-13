8590b62b1db9b72e330911a468a9e7528f73c7bd
jipyo0106 <68218081+jipyo0106@users.noreply.github.com>
2022-11-14 04:55:36 +0900

    feat: 구매 금액 숫자 유효성 검사 기능 구현

--- src/main/java/lotto/controller/LottoPurchase.java
+++ src/main/java/lotto/controller/LottoPurchase.java
@@ -6,4 +6,10 @@ public class LottoPurchase {
     private String getPurchaseAmountInput() {
         return Console.readLine();
     }
+
+    private void userInputIsNumber(String userInput) {
+        if (!userInput.matches("[0-9]*")) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 숫자만 입력할수 있습니다.");
+        }
+    }
 }
