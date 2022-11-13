45d833b51c8101ff70f5b9aa46f84b575fcf6877
seheonCho <whtpgjs94@gmail.com>
2022-11-13 19:56:10 +0900

    feat(MoneyInput): 숫자만으로 이루어져 있는지 판별하는 기능

--- docs/README.md
+++ docs/README.md
@@ -8,7 +8,7 @@
 - [ ] 로또의 결과를 가져온다. Lotto#getResult()
 - [ ] 로또 구입 금액만큼 로또를 발행한다. LottoMachine#LottoForAmount()
 - [x] 로또 구입 금액을 입력 받는다. MoneyInput#getPurchaseAmount()
-- [ ] 숫자만으로 이루어져 있는지 판별한다. MoneyInput#validateConvertingMoney()
+- [x] 숫자만으로 이루어져 있는지 판별한다. MoneyInput#validateConvertingMoney()
 - [x] 1,000원으로 나누어 떨어지는지 검증한다. Money#validatePurchaseAmount()
 - [ ] 로또 구입 금액을 가져온다. Money#getMoney()
 - [ ] 구입 금액에 해당하는 만큼 로또를 발행한다. LottoMachine#issue()
--- src/main/java/input/MoneyInput.java
+++ src/main/java/input/MoneyInput.java
@@ -1,13 +1,28 @@
 package input;
 
 import camp.nextstep.edu.missionutils.Console;
+import util.ExceptionMessage;
 
 public class MoneyInput {
 
     public MoneyInput() {}
 
     public int getPurchaseAmount() {
-        return Integer.parseInt(Console.readLine());
+        String input = Console.readLine();
+        int money = validateConvertingMoney(input);
+        return money;
+    }
+
+    private int validateConvertingMoney(String input) {
+        int money = 0;
+
+        try {
+            money = Integer.parseInt(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ExceptionMessage.NOT_MONEY_FORMAT);
+        }
+
+        return money;
     }
 
 }
