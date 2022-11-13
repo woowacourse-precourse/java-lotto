474987a275c1983515b3393bb964de217d418659
thousandcows <milcamboo@gmail.com>
2022-11-13 21:38:48 +0900

    test: create payment validation test

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +25,16 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("입력된 금액이 1000으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void validatePaymentTest() {
+        int validPayment = 140000;
+        int inValidPayment = 14123;
+
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThat(lotto.validatePayment(validPayment)).isEqualTo(true);
+        assertThat(lotto.validatePayment(inValidPayment)).isInstanceOf(IllegalArgumentException.class);
+
+    }
 }
