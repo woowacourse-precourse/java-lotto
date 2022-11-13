beb16c7012551d68d788bb0fb8aea4dc7b252c8b
TaeHwan <kimta2hwan@gmail.com>
2022-11-13 15:09:25 +0900

    feat: 보너스 번호 입력 안내 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,6 +7,7 @@ public class OutputView {
     private static final String PURCHASE_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String PURCHASE_RESULT_MESSAGE = "%d개를 구매했습니다.\n";
     private static final String WINNIG_NUMBERS_ENTERING_GUIDE_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBERS_ENTERING_GUIDE_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static void printPurchaseGuideMessage() {
         System.out.println(PURCHASE_GUIDE_MESSAGE);
@@ -22,5 +23,9 @@ public class OutputView {
 
     public static void printWinningNumbersEnteringGuideMessage() {
         System.out.println(WINNIG_NUMBERS_ENTERING_GUIDE_MESSAGE);
+    }
+
+    public static void printBonusNumbersEnteringGuideMessage() {
+        System.out.println(BONUS_NUMBERS_ENTERING_GUIDE_MESSAGE);
     }
 }
