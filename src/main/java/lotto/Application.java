57f673cc7d2f3c13ef1ef1e81a6fb90c8465127c
JoSihun <kepler452k@naver.com>
2022-11-13 21:23:02 +0900

    feat(lotto): implement function of generating lotto numbers move Lotto.java into package domain add function validateInRangedNumbers() in class Lotto.java add function validateDuplicatedNumbers() in class Lotto.java add function getNumbers() in class Lotto.java
    add class LottoGenerator.java into package domain
    add function createLottoNumbers() in class LottoGenerator.java
    add function createLottos() in class LottoGenerator.java
    
    add function printPurchasedLottos() in class Application.java

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,19 +1,36 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
 import lotto.domain.Money;
 import lotto.domain.Ticket;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Money purchasePrice = new Money(askPurchasePrice());
         Ticket tickets = new Ticket(purchasePrice);
+
+        LottoGenerator generator = new LottoGenerator();
+        List<Lotto> lottos = generator.createLottos(tickets);
+        printPurchasedLottos(lottos);
+
     }
 
     public static String askPurchasePrice() {
         System.out.println("구입금액을 입력해 주세요.");
         String purchasePrice = Console.readLine();
+        System.out.println();
         return purchasePrice;
+    }
+
+    public static void printPurchasedLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }
