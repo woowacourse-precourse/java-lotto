e9158037058dcf2b3655b72202c2a53b2881fe1c
jinny-l <jinny.l.dev@gmail.com>
2022-11-13 20:54:45 +0900

    refactor(LottoGenerator): 사용하지 않는 메소드 삭제

--- src/main/java/lotto/util/LottoGenerator.java
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -8,13 +8,6 @@ import lotto.constant.Constant;
 
 public class LottoGenerator {
 
-    public static int generateBonusNumber() {
-        List<Integer> randomNumber = Randoms.pickUniqueNumbersInRange(
-                Constant.LOTTO_START_NUMBER, Constant.LOTTO_END_NUMBER, Constant.BONUS_NUMBER_SIZE);
-
-        return randomNumber.get(0); // TODO: get(0) 안했을 때 왜 size 6이 리턴되는지?
-    }
-
     public static List<Integer> generateLottoNumbers() {
         List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(
                 Constant.LOTTO_START_NUMBER, Constant.LOTTO_END_NUMBER, Constant.LOTTO_SIZE)
