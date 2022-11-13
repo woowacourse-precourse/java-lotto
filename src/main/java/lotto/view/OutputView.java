3c06d73b24cf13891acab861cf72aced60a3d9f9
TaeHwan <kimta2hwan@gmail.com>
2022-11-13 15:56:12 +0900

    style: 오타 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,16 +6,16 @@ public class OutputView {
 
     private static final String PURCHASE_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String PURCHASE_RESULT_MESSAGE = "%d개를 구매했습니다.\n";
-    private static final String WINNIG_NUMBERS_ENTERING_GUIDE_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String BONUS_NUMBERS_ENTERING_GUIDE_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String WINNING_NUMBERS_ENTERING_GUIDE_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_ENTERING_GUIDE_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String YIELD_MESSAGE = "총 수익률은 %.1f%입니다.\n";
 
     public static void printPurchaseGuideMessage() {
         System.out.println(PURCHASE_GUIDE_MESSAGE);
     }
 
-    public static void printPurchaseResultMessage(int numberOfPurchasedLottey) {
-        System.out.printf(PURCHASE_RESULT_MESSAGE, numberOfPurchasedLottey);
+    public static void printPurchaseResultMessage(int numberOfPurchasedLottery) {
+        System.out.printf(PURCHASE_RESULT_MESSAGE, numberOfPurchasedLottery);
     }
 
     public static void printLottoNumbers(List<Integer> lottoNumbers) {
@@ -23,11 +23,11 @@ public class OutputView {
     }
 
     public static void printWinningNumbersEnteringGuideMessage() {
-        System.out.println(WINNIG_NUMBERS_ENTERING_GUIDE_MESSAGE);
+        System.out.println(WINNING_NUMBERS_ENTERING_GUIDE_MESSAGE);
     }
 
     public static void printBonusNumbersEnteringGuideMessage() {
-        System.out.println(BONUS_NUMBERS_ENTERING_GUIDE_MESSAGE);
+        System.out.println(BONUS_NUMBER_ENTERING_GUIDE_MESSAGE);
     }
 
     public static void printYieldMessage(double yield) {
