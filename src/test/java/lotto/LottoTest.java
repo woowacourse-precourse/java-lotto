f99a082dbd930f393b42d6cc3a42c050c1785133
KimJaeSung <behappy0916@naver.com>
2022-11-13 18:35:09 +0900

    refactor: 기존의 중복 test삭제 및 보완
    중복_체크 test를 삭제
    로또_크기_체크 test를 삭제
    숫자_이외에_값_입력_체크 변환

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,7 +2,8 @@ package lotto;
 
 import lotto.domain.Lotto;
 import lotto.domain.Money;
-import lotto.exception.Exceptions;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,10 +13,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7).toString()))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -23,27 +25,17 @@ class LottoTest {
     @Test
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5).toString()))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     // 아래에 추가 테스트 작성 가능
     @Test
-    void 중복_체크(){
-        boolean overlap = Exceptions.isOverLapNumber("1,2,3,4,5,5");
-        assertThat(overlap).isFalse();
-    }
-
-    @Test
     void 숫자_이외에_값_입력_체크(){
-        boolean correct = Exceptions.isRightLottoNumbersType("1,2,3,4,5,a");
-        assertThat(correct).isFalse();
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 'd', 'c', 'b', 'a').toString()))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 로또_크기_체크(){
-        boolean correctSize = Exceptions.isCorrectSize("1,2,3,4,5");
-        assertThat(correctSize).isFalse();
-    }
+
 
 }
