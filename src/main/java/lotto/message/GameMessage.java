980c8238e870152434ff6896a2d689e19c20a7a3
jihun-24k <gi020477@gmail.com>
2022-11-13 19:58:05 +0900

    Refactor: MessageList -> GameMessage로 네이밍 수정

--- src/main/java/lotto/message/GameMessage.java
+++ src/main/java/lotto/message/GameMessage.java
@@ -1,6 +1,6 @@
 package lotto.message;
 
-public enum MessageList {
+public enum GameMessage {
     PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     NUMBER_OF_PURCHASES("개를 구매했습니다."),
     WINNING_NUMBER("당첨 번호를 입력해 주세요."),
@@ -10,7 +10,7 @@ public enum MessageList {
 
     private final String message;
 
-    MessageList(String message) {
+    GameMessage(String message) {
         this.message = message;
     }
 
