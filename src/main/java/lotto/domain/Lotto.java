6dde2f329208e7d63156543db2d34600f65358a4
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2022-11-13 18:00:38 +0900

    refactor : (Lotto) add check duplicate number function

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -8,11 +9,21 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        checkDuplicateNumber();
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkDuplicateNumber(){
+        HashSet<Integer> set = new HashSet<>(12);
+        for(Integer number : numbers){
+            if(set.contains(number))
+                throw new IllegalArgumentException();
+            set.add(number);
         }
     }
 
