c14abf4941bbb77ee5e60bf750d9a173484ab838
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-14 08:57:46 +0900

    FIX:Lotto.java/validate()->validateOverNum() FEAT:Lotto.java/create validateNotMatchMoney()

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Lotto {
@@ -10,9 +11,19 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateOverNum(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            System.out.println("[ERROR] 로또 번호의 개수가 6개 초과");
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNotMatchMoney(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if (numbers.get(i) == numbers.get(i + 1)) {
+                System.out.println("[ERROR] 로또 번호에 중복된 숫자 존재");
+                throw new IllegalArgumentException();
+            }
         }
     }
 
