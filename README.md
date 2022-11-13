1fa524c85de38d5c1abd1a0cc5a8a9009a01be5e
jaewoo <wjdwodn0722@naver.com>
2022-11-13 21:43:26 +0900

    feat : 만든 로또 번호에 중복이 있는 지 검사하는 기능 추가

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 - [v] 받은 돈을 계산하여 몇 개의 6자리 수열을 만들지 계산한다. - Store#countLines 
   - [v] 받은 돈이 1000원 단위로 떨어지는지 확인한다. - Store#validate
   - [v] 6자리의 중복이 없는 수열들을 생성한다. - Store#makeNumbers
-- [ ] 구매한 로또의 당첨내역을 계산한다. - Lotto#confirmNumbers
+- [v] 구매한 로또의 당첨내역을 계산한다. - Lotto#confirmNumbers
+  - [v] 만든 로또 번호가 중복이 있는 지 검사한다. -Lotto#checkRedundancy
 - [ ] 입력받은 당첨번호로 구매한 로또의 당첨내역을 판단하고 수익률을 계산한다. - Judge#countTotal
      - [ ] 당첨내역으로 총 당첨금을 계산한다. - Judge#countPrizeMoney
      - [ ] 당청금과 구매금으로 수익률을 계산한다. - Judge#countEarningsRate
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,17 +1,28 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkRedundancy(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkRedundancy(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if (set.size() != numbers.size()) {
             throw new IllegalArgumentException();
         }
     }
