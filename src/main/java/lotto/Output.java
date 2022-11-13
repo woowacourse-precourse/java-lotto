a50c5066efc0209417395b43f2582c14958f7dcf
kimtaehoonDev <taehoon_dev@naver.com>
2022-11-14 01:11:44 +0900

    refactor(Result): 내부 저장소를 TreeMap으로 변경

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import lotto.domain.Lotto;
@@ -14,7 +15,12 @@ public class Output {
     }
 
     public static void showResult(Result result) {
-        System.out.println(result);
+        Map<Rank, Integer> resultInformation = result.returnInfo();
+        List<String> messages = new ArrayList<>();
+        resultInformation.keySet().stream().filter(rank -> rank != Rank.NOTHING)
+                .forEach(rank ->
+                        messages.add(rank + " - " + NumberFormatter.format(resultInformation.get(rank)) + "개"));
+        System.out.println(String.join("\n", messages));
         System.out.println("총 수익률은 " + NumberFormatter.format(result.calculateYield()) + "%입니다.");
     }
 }
--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -2,37 +2,21 @@ package lotto.domain;
 
 import static lotto.Constant.LOTTO_PRICE;
 
-import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import lotto.NumberFormatter;
+import java.util.TreeMap;
 
 public class Result {
     private Map<Rank, Integer> result;
 
     public Result() {
-        result = new HashMap<>();
+        result = new TreeMap<>();
         Arrays.stream(Rank.values()).forEach(rank -> result.put(rank, 0));
     }
 
     public void record(Integer duplicatedCnt, boolean hasBonusNumber) {
         Rank rank = Rank.valueOf(duplicatedCnt, hasBonusNumber);
         result.put(rank, result.get(rank) + 1);
-    }
-
-    @Override
-    public String toString() {
-        List<String> messages = new ArrayList<>();
-        // TODO 순서 보장되는 Map 있었음. 해당 방식 찾아보기 (NOTHING뺴고 돌려야함)
-        messages.add(Rank.FIFTH + " - " + NumberFormatter.format(result.get(Rank.FIFTH)) + "개");
-        messages.add(Rank.FOURTH + " - " + NumberFormatter.format(result.get(Rank.FOURTH)) + "개");
-        messages.add(Rank.THIRD + " - " + NumberFormatter.format(result.get(Rank.THIRD)) + "개");
-        messages.add(Rank.SECOND + " - " + NumberFormatter.format(result.get(Rank.SECOND)) + "개");
-        messages.add(Rank.FIRST + " - " + NumberFormatter.format(result.get(Rank.FIRST)) + "개");
-        return String.join("\n", messages);
     }
 
     public Double calculateYield() {
@@ -47,5 +31,7 @@ public class Result {
         return Math.round(yield * 10) / 10.0;
     }
 
-
+    public Map<Rank, Integer> returnInfo() {
+        return result;
+    }
 }
