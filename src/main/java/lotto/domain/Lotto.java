f066ca8bf6d82340c55bb2cfc3883b3c39f48d05
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-11-13 17:16:48 +0900

    feat(User): 로또 구입 금액 및 보너스 번호 입력받기

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
-package lotto;
+package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
 
 
 public class Lotto {
--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,5 +1,21 @@
+package lotto.domain;
+
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class User{
-    public static 
+    public static int money = Integer.parseInt(Console.readLine());
+
+    private final List<Integer> userNumList;
+
+    public User(List<Integer> userNumList){
+        this.userNumList = userNumList;
+    }
+
+    String str = Console.readLine();
+
+    public static int bonusNum = Integer.parseInt(Console.readLine());
+
+
 }
