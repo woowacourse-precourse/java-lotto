0f5ace4025682ac46cfaa1cc6ac473789bd67b90
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-13 17:44:35 +0900

    refactor: 구매금액 입력 메소드 controller로 분리
    main 메소드에 실행하는 로직을 컨트롤러 단으로 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,26 +1,30 @@
 package lotto;
 
+import lotto.domain.LottoRank;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static lotto.view.InputView.lottoPurchaseMoney;
+
 public class Application {
     public static void main(String[] args) {
 
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
+        LottoRank lottoRank;
 
         outputView.printPriceInputMessage();
-        int lottoPurchaseMoney = inputView.inputLottoPurchaseMoney();
-        int lottoChance = inputView.getLottoChance(lottoPurchaseMoney);
+
+
 
         List<Integer> winningNumbers = inputView.createWinningNumber();
         int bonusNumber = inputView.inputBonusNumber();
 
-        inputView.allLottoNumber = inputView.createAllGameLottoNumber(lottoChance);
-        outputView.printLottoPurchaseCompleteMessage(lottoChance);
+        inputView.allLottoNumber = inputView.createAllGameLottoNumber(lottoTickets);
+        outputView.printLottoPurchaseCompleteMessage(lottoTickets);
         outputView.printLottoNumber(inputView.allLottoNumber);
 
         for (int i = 0; i < inputView.allLottoNumber.size(); i++) {
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,31 @@
 package lotto.controller;
 
+import lotto.domain.LottoRank;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class LottoController {
+
+    private final int LOTTO_ONE_GAME_PRICE = 1000;
+
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    LottoRank lottoRank;
+
+    // 게임을 실행하는 메소드
+    public void run(){
+        int inputLottoMoney = inputLottoPurchaseMoney();
+        int lottoTickets = createLottoTickets(inputLottoMoney);
+    }
+
+    private int inputLottoPurchaseMoney() {
+        int lottoPurchaseMoney = inputView.inputLottoPurchaseMoney();
+        return lottoPurchaseMoney;
+    }
+
+    private int createLottoTickets (int inputMoney) {
+        return inputMoney / LOTTO_ONE_GAME_PRICE;
+    }
+
+
 }
