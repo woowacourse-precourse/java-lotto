5ce310e250e29b4a9917a58cae537c3607e92561
Devtist <yt930310@snu.ac.kr>
2022-11-13 18:47:44 +0900

    feat: 등수의 조건과 상금을 열거형으로 선언

---
+++ src/main/java/lotto/constant/WinningRating.java
@@ -0,0 +1,24 @@
+package lotto.constant;
+
+public enum WinningRating {
+	FIFTH(3, "5,000원"),
+	FOURTH(4, "50,000원"),
+	THIRD(5, "1,500,000원"),
+	SECOND(5, "30,000,000원"),
+	FIRST(6, "2,000,000,000원")
+
+	private final int countOfMatchedNumber;
+	private final String prizeMoney;
+	WinningRating(int countOfMatchedNumber, String prizeMoney) {
+		this.countOfMatchedNumber = countOfMatchedNumber;
+		this.prizeMoney = prizeMoney;
+	}
+
+	public int getCountOfMatchedNumber() {
+		return countOfMatchedNumber;
+	}
+
+	public String getPrizeMoney() {
+		return prizeMoney;
+	}
+}
