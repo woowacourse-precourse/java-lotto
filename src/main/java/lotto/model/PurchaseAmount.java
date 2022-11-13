1dab63fe7b096d321abb880dd7fbb30a8d2a6156
dhkim <akdlzmf1123@gmail.com>
2022-11-13 21:51:54 +0900

    refactor(PurchaseAmount): amount를 purchaseAmount로 이름 변경

--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -3,10 +3,10 @@ package lotto.model;
 public class PurchaseAmount {
 
     public static final String ERR_PURCHASE_AMOUNT = "[ERROR] 구매 금액은 1000원 단위여야 합니다. 예시) 7000(O), 15000(O), 6500(X)";
-    private final int amount;
-    public PurchaseAmount(int amount) {
-        this.amount = amount;
-        validate(amount);
+    private final int purchaseAmount;
+    public PurchaseAmount(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+        validate(purchaseAmount);
     }
 
     private void validate(int amount) {
@@ -19,10 +19,10 @@ public class PurchaseAmount {
     }
 
     public int getNumberOfLottos() {
-        return amount / Lotto.PRICE;
+        return purchaseAmount / Lotto.PRICE;
     }
 
     public double calculateProfitRate(int winningAmount) {
-        return (double) winningAmount / amount * 100;
+        return (double) winningAmount / purchaseAmount * 100;
     }
 }
