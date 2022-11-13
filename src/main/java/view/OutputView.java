6117171bfe999f901710dc94fed3818a5da67c7a
Kimhyeyun <cqqwer@gmail.com>
2022-11-13 21:16:04 +0900

    Feat: 구매한 로또 자동 생성 번호 출력 메서드 생성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,9 +1,30 @@
 package view;
 
+import lotto.domain.PurchasedLottos;
+
+import java.util.Arrays;
+
 import static lotto.constant.Message.ERROR_MESSAGE;
+import static lotto.constant.Message.PURCHASE_LOTTO_MESSAGE;
 
 public class OutputView {
     public void getErrorMessage(String message) {
         System.out.println(ERROR_MESSAGE + message);
+    }
+
+    public void printPurchasedLottos(int count, PurchasedLottos purchasedLottos) {
+        System.out.println(count + PURCHASE_LOTTO_MESSAGE);
+
+        purchasedLottos.getPurchasedLottos()
+                .forEach(purchasedLotto -> {
+                    System.out.println(
+                            Arrays.toString(
+                                    purchasedLotto
+                                            .getNumbers()
+                                            .stream()
+                                            .toArray()
+                            )
+                    );
+                });
     }
 }
