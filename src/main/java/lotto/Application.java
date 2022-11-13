59ab21058a277a85541a34457b207eb87490b7e1
inwoo <korean5078@gmail.com>
2022-11-13 21:45:00 +0900

    feat : 사용자의 로또 번호 생성하는 함수인 makeLottoNumbers, makeBunchOfLotto 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,18 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int NumberOfLotto = getNumberOfLotto();
+
+        List<Lotto> lottoList = makeBunchOfLotto(NumberOfLotto);
     }
 
     public static int getNumberOfLotto() {
@@ -17,5 +24,19 @@ public class Application {
         }
         money = Integer.parseInt(input);
         return money / 1000;
+    }
+
+    public static Lotto makeLottoNumbers() {
+        Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6));
+
+        return lotto;
+    }
+
+    public static List<Lotto> makeBunchOfLotto(int number) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < number; i++) {
+            lottos.add(makeLottoNumbers());
+        }
+        return lottos;
     }
 }
