b0012e067c815ce87dac211fd8bcee7a08dceeee
jeomxon <jeomxon@gmail.com>
2022-11-13 20:02:15 +0900

    feat: 구매 금액에 대해 숫자만 입력받도록 예외처리

--- src/main/java/lotto/domain/purchase/Purchase.java
+++ src/main/java/lotto/domain/purchase/Purchase.java
@@ -1,32 +1,44 @@
-package lotto.domain;
+package lotto.domain.purchase;
 
-import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class Purchase {
+    private static final String NOT_INTEGER_ERROR = "[ERROR] 정수 값이 입력되어야 합니다.";
     private static final String PURCHASE_AMOUNT_ERROR = "[ERROR] 구입 금액은 1,000원 단위여야 합니다.";
 
     private final int purchaseAmount;
 
+    public Purchase(String input) {
+        validateInteger(input);
 
-    public Purchase(final int purchaseAmount) {
-        validate(purchaseAmount);
+        int purchaseAmount = Integer.parseInt(input);
+        validateMoneyUnit(purchaseAmount);
+
         this.purchaseAmount = purchaseAmount;
     }
 
-    public int getPurchaseAmount() {
+    public int getAmount() {
         return purchaseAmount;
     }
 
+    public int numOfLotto(int purchaseAmount) {
+        int num = purchaseAmount / 1000;
+        return num;
+    }
 
+    private void validateInteger(String input) {
+        String regex = "^[0-9]+$";
 
-    private void validate(int money) {
-        if (money % 1000 != 0) {
-            throw new IllegalArgumentException(PURCHASE_AMOUNT_ERROR);
+        if (!input.matches(regex)) {
+            System.out.println(NOT_INTEGER_ERROR);
+            throw new NoSuchElementException(NOT_INTEGER_ERROR);
         }
     }
 
-    private int numOfLotto(int money) {
-        int num = money / 1000;
-        return num;
+    private void validateMoneyUnit(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            System.out.println(PURCHASE_AMOUNT_ERROR);
+            throw new IllegalArgumentException(PURCHASE_AMOUNT_ERROR);
+        }
     }
 }
