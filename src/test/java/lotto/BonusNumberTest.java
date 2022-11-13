f2b0cb81864e6fe052b2bf46bec6b25ffa16cc34
twoosky <lhjae3@naver.com>
2022-11-14 02:43:37 +0900

    test(domain): 입력받은 보너스 번호 정수형 변환 테스트

--- src/test/java/lotto/BonusNumberTest.java
+++ src/test/java/lotto/BonusNumberTest.java
@@ -4,10 +4,12 @@ import lotto.domain.BonusNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static lotto.utils.ErrorMessages.BONUS_NUMBER_NOT_INTEGER;
 import static lotto.utils.ErrorMessages.BONUS_NUMBER_OUT_OF_RANGE;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BonusNumberTest {
@@ -26,5 +28,13 @@ public class BonusNumberTest {
         assertThatThrownBy(() -> new BonusNumber(number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(BONUS_NUMBER_OUT_OF_RANGE);
+    }
+
+    @DisplayName("입력받은 보너스 번호를 정수형으로 변환한다.")
+    @ParameterizedTest
+    @CsvSource({"1,1", "45,45", "12,12"})
+    void getBonusNumber(String number, int expected) {
+        BonusNumber bonusNumber = new BonusNumber(number);
+        assertThat(bonusNumber.getNumber()).isEqualTo(expected);
     }
 }
