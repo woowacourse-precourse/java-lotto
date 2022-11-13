956042268a5170c88625c7381022db7b82555de9
nswon <swnam0729@gmail.com>
2022-11-13 22:27:54 +0900

    refactor: 쓸데없는 코드 삭제

--- src/main/java/lotto/domain/Buyer.java
+++ src/main/java/lotto/domain/Buyer.java
@@ -25,7 +25,7 @@ public class Buyer {
 
     public void createLottos(int amount) {
         int count = countOfBuyLottos(amount);
-        IntStream.range(1, countOfBuyLottos(amount)).forEach(value -> lottos.add(Lotto.generateLotto()));
+        IntStream.range(1, count).forEach(value -> lottos.add(Lotto.generateLotto()));
         OutputView.printPurchase(count, lottos);
     }
 
