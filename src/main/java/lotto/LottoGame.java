e88e056be88a4baac7aaaa29f24c81ca1c4b2344
sugamypapa <sk000801@naver.com>
2022-11-13 17:47:37 +0900

    feat: 게임 전체를 실행하는 클래스 생성

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,4 +1,29 @@
 package lotto;
 
+import domain.Lottos;
+import view.Input;
+import view.Output;
+
 public class LottoGame {
+    private Lottos lottos;
+
+    private Input input;
+
+    private Output output;
+
+    public LottoGame() {
+        input = new Input();
+        output = new Output();
+    }
+
+    public void startGame() {
+        input.returnNumber(input.enterPrice());
+        lottos = new Lottos(input.getNumber());
+        output.printLottoList(lottos.getLottos());
+        String[] answer = input.enterAnswer();
+        input.returnAnswer(answer);
+        input.returnBonus(input.enterBonus(), answer);
+        output.setRankList(lottos.getLottos(), input.getAnswer(), input.getBonus());
+        output.printResult(input.getNumber());
+    }
 }
