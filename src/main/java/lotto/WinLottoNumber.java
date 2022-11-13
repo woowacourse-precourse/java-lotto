50f37f61c5212aafecbdc5e40fa43877a7a85ebe
abbd2 <ajkety00@gmail.com>
2022-11-13 19:41:57 +0900

    Feat(WinLottoNumber): 로또 번호와 당첨 번호와 비교하여 나온 같은 번호들을 저장하는 WinLottoNumber 클래스 구현

---
+++ src/main/java/lotto/WinLottoNumber.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import java.util.List;
+
+public class WinLottoNumber {
+
+    private List<Integer> winLottoNumber;
+
+    public WinLottoNumber(List<Integer> winLottoNumber){
+        this.winLottoNumber = winLottoNumber;
+    }
+
+    public List<Integer> getWinLottoNumber(){
+        return this.winLottoNumber;
+    }
+
+}
