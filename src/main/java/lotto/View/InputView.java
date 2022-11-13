c3e00ed650b03ac6f8c8fd7464302a520a67cbe9
SangWoo Yun <64531982+Awarduuu@users.noreply.github.com>
2022-11-13 14:41:49 +0900

    feat(InputView): add the function to ask WinningLotto

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -5,10 +5,16 @@ import lotto.Utils.Util;
 public class InputView {
 
     public static final String PRINT_ASK_AMOUT = "구입금액을 입력해주세요.";
-
+    public static final String PRINT_ASK_WINNING_LOTTO = "당첨금액을 입력해주세요.";
     public static String purchaseAmountInput(){
         System.out.println(PRINT_ASK_AMOUT);
         return Util.getInput();
     }
+
+    public static String WinningLottoInput(){
+        System.out.println(PRINT_ASK_WINNING_LOTTO);
+        return Util.getInput();
+    }
+
 
 }
