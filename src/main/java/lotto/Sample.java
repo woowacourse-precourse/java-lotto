5e5217f69afb85a8637fb8c367410105bac24bed
wonho-seo <sjdlrnen0507@naver.com>
2022-11-13 16:50:53 +0900

    fix : fix Lotto-valid-001
    나중을 위해 static으로 구현하지않음

--- src/main/java/lotto/Sample.java
+++ src/main/java/lotto/Sample.java
@@ -1,9 +1,7 @@
 package lotto;
 
 public class Sample {
-
-
-    public static int stringToInt(String line){ // Lotto-valid-001
+    public int stringToInt(String line){ // Lotto-valid-001
         try{
             int number = Integer.parseInt(line);
             if (number % 1000 != 0)
--- src/test/java/lotto/SampleTest.java
+++ src/test/java/lotto/SampleTest.java
@@ -5,19 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class SampleTest {
+    private final Sample sample;
 
+    public SampleTest(){
+        this.sample = new Sample();
+    }
     @Test
     void stringToIntTest1(){
-        assertThatThrownBy(() -> Sample.stringToInt("12ab12")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> sample.stringToInt("12ab12")).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void stringToIntTest2(){
-        assertThatThrownBy(() -> Sample.stringToInt("123452853740958342000")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> sample.stringToInt("123452853740958342000")).isInstanceOf(IllegalArgumentException.class);
     }
     @Test
 
     void stringToIntTest3(){
-        assertThatThrownBy(() -> Sample.stringToInt("135700")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> sample.stringToInt("135700")).isInstanceOf(IllegalArgumentException.class);
     }
 }
