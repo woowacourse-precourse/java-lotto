f38435c0139b2dfffe17768286b35b2bddb87d2d
DaWoon Lee <qkgk61@gmail.com>
2022-11-13 17:11:19 +0900

    Feat : 몇 등에 당첨되었는지를 파악하는 기능 구현

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -1,17 +1,33 @@
 package lotto.domain;
 
+import java.util.Optional;
+
 public enum Winning {
 
-    FIRST(2_000_000_000L),
-    SECOND(30_000_000L),
-    THIRD(1_500_000L),
-    FOURTH(50_000L),
-    FIFTH(5_000L);
+    FIRST(6, 0, 2_000_000_000L),
+    SECOND(5, 1, 30_000_000L),
+    THIRD(5, 0, 1_500_000L),
+    FOURTH(4, 0, 50_000L),
+    FIFTH(  3, 0, 5_000L);
 
+    private final int sameCount;
+    private final int bonusCount;
     private final Long winnings;
 
-    Winning(Long winnings) {
+    Winning(int sameCount, int bonusCount, Long winnings) {
+        this.sameCount = sameCount;
+        this.bonusCount = bonusCount;
         this.winnings = winnings;
+    }
+
+    public Optional<Winning> getWinning(int sameCount, int bonusCount) {
+        for (Winning current : Winning.values()) {
+            if (current.sameCount == sameCount && current.bonusCount == bonusCount) {
+                return Optional.of(current);
+            }
+        }
+
+        return Optional.empty();
     }
 
     public Long getWinnings() {
