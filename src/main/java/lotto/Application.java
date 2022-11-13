13dd3095670e88d2cb42fe9dac532e169dd0b914
michaelp3 <chan-park@hotmail.com>
2022-11-14 02:11:05 +0900

    feat(Application.java): 구입금액 입력 UI 기능 구현
    구입 금액 입력을 받는 UI 기능 구현 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,18 @@
 package lotto;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         printInputMoney();
+        int money = getInputMoneyFromUser();
+    }
+
+    private static int getInputMoneyFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        //TODO: 구입금액 입력 검증
+        return Integer.parseInt(input);
     }
 
     private static void printInputMoney() {
