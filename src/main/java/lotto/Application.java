610695d18391b78a2c9198e05a0f13136dff8d75
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2022-11-13 17:58:47 +0900

    feat : (Application) main implementation

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import lotto.domain.Publisher;
+import lotto.domain.WinningLotto;
+import lotto.domain.WinningStatistics;
+import lotto.ui.UI;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        UI ui = new UI();
+        Publisher lottoPublisher;
+        WinningLotto winningLotto;
+        WinningStatistics lottoStatistics;
+        
+        lottoPublisher = new Publisher(ui.inputPurchasePrice());
+        ui.outputPurchaseLotto(lottoPublisher.getLottos());
+        winningLotto = new WinningLotto(ui.inputWinningNumbers(), ui.inputBonusNumber());
+        lottoStatistics = new WinningStatistics(lottoPublisher, winningLotto);
+        ui.outputWinningInfo(lottoStatistics.getNumberOfWinningLottos());
+        ui.outputEarningRate(lottoStatistics.getEarningRate());
     }
 }
