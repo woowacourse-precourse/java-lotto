64a8f051e1b572fde413b41d8fc3159838d2f052
kokomong2 <ko9603@naver.com>
2022-11-14 07:03:58 +0900

    feat(lotto) : 로또번호 예외 처리 추가
    1부터 45사이의 숫자가 아닐경우 예외 처리
    로또 번호중 중복된 번호가 있을경우 예외 처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,36 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private int bonusNumber;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        numbers.forEach(this::rangeCheck);
+        isDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또번호는 6개로 입력해주세요");
+        }
+    }
+
+    private void rangeCheck(int number){
+        if(number<1||number>45){
+            throw new IllegalArgumentException("[ERROR] 로또번호는 1부터 45사이입니다.");
+        }
+    }
+
+    private void isDuplicate(List<Integer> numbers){
+        Set<Integer> testNumbers = new HashSet<>(numbers);
+        if(numbers.size()!= testNumbers.size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
         }
     }
 
