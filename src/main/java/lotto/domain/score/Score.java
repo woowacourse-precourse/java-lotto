f88ef550f845d2731c579e334292e33feae7b232
JaehongDev <println@kakao.com>
2022-11-13 18:38:07 +0900

    refactor (Score) : getter 제거

--- src/main/java/lotto/domain/score/Score.java
+++ src/main/java/lotto/domain/score/Score.java
@@ -6,29 +6,13 @@ public class Score {
     private final Integer normal;
     private final Integer bonus;
 
-    protected Score(final Integer normal, final Integer bonus) {
+    public Score(final Integer normal, final Integer bonus) {
         this.normal = normal;
         this.bonus = bonus;
     }
 
     public static Score generateEmptyScore() {
         return new Score(0, 0);
-    }
-
-    public Integer getNormal() {
-        return normal;
-    }
-
-    public Integer getBonus() {
-        return bonus;
-    }
-
-    public Ranking generateRank() {
-        return Ranking.calculateScore(this);
-    }
-
-    public Score plusNormal() {
-        return new Score(this.normal + 1, this.bonus);
     }
 
     @Override
@@ -40,14 +24,22 @@ public class Score {
             return false;
         }
         Score score = (Score) o;
-        return Objects.equals(getNormal(), score.getNormal()) && Objects.equals(getBonus(),
-                score.getBonus());
+        return Objects.equals(normal, score.normal) && Objects.equals(bonus, score.bonus);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getNormal(), getBonus());
+        return Objects.hash(normal, bonus);
     }
+
+    public Ranking generateRank() {
+        return Ranking.calculateScore(this);
+    }
+
+    public Score plusNormal() {
+        return new Score(this.normal + 1, this.bonus);
+    }
+
 
     public Score plusBonus() {
         return new Score(this.normal, this.bonus + 1);
