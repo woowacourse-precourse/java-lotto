f65266c34681bdca59e801c88941399f2238215e
210_reverof <sheltonwon@gmail.com>
2022-11-13 21:53:25 +0900

    feat: add getting Lotto Count by the input function (function1)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,4 +4,13 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public static int getLottoCount(String input) {
+        if (!input.matches("[0-9]+")) return -1;
+
+        int money = Integer.parseInt(input);
+        if (money % 1000 != 0) return -1;
+
+        return money / 1000;
+    }
 }
