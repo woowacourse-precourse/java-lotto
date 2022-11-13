5d4b8d5da58fd92beb7262097672a7acac032a34
hossang <zkzk104@naver.com>
2022-11-13 17:41:45 +0900

    feat: 담첨 번호를 , 기준으로 분리 시키기

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,15 +1,13 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.Set;
-import java.util.StringTokenizer;
 
 public class WinningNumber {
-    private Set<Integer> winningNumber;
+    private Lotto winningNumber;
 
     public void inputWinningNumber(String input) {
-        if (!isSixStrings(input)) {
-            throw new IllegalArgumentException(Error.IS_NOT_SIX.getMessage());
-        }
+        isNumber(separateCommas(input));
         if (!isNumber(input)) {
             throw new IllegalArgumentException(Error.IS_NOT_NUMBER.getMessage());
         }
@@ -17,38 +15,39 @@ public class WinningNumber {
             throw new IllegalArgumentException(Error.IS_NOT_RANGE.getMessage());
         }
         if (!isSixNumber()) {
-            throw new IllegalArgumentException(Error.IS_NOT_UNIQUE.getMessage());
+
         }
 
     }
 
-    private boolean isSixNumber() {
-        return winningNumber.size() == 6;
+    private void isSixNumber() {
+        if (winningNumber.size() == 6) {
+            throw new IllegalArgumentException(Error.IS_NOT_UNIQUE.getMessage());
+        }
     }
 
     private boolean isRangeNumber() {
         return winningNumber.stream().allMatch(a -> a >= 1 && a <= 45);
     }
 
-    private boolean isSixStrings(String input) {
-        StringTokenizer st = new StringTokenizer(input, ",");
-        return st.countTokens() == 6;
+    private String[] separateCommas(String input) {
+        String[] inputs = input.split(",");
+        if (inputs.length != 6) {
+            throw new IllegalArgumentException(Error.IS_NOT_SIX.getMessage());
+        }
+        return inputs;
     }
 
-    private boolean isNumber(String input) { //인덴트 초과
-        String[] numbers = input.split(",");
-        for (String number : numbers) {
-            for (int i = 0; i < number.length(); i++) {
-                if (number.charAt(i) < '0' || number.charAt(i) > '9') {
+    private boolean isNumber(String[] inputs) { //인덴트 초과
+        Set<Integer> numbers = new HashSet<>();
+        for (String input : inputs) {
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) < '0' || input.charAt(i) > '9') {
                     return false;
                 }
             }
-            winningNumber.add(Integer.parseInt(number)); //메소드 두개인데
+            numbers.add(Integer.parseInt(input)); //메소드 두개인데
         }
         return true;
-    }
-
-    public Set<Integer> getWinningNumber() {
-        return winningNumber;
     }
 }
