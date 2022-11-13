a07096a4589dd38f6e69baf289626a7d497cca79
김태용 <amico741@naver.com>
2022-11-13 13:44:29 +0900

    refactor: Money 클래스 예외처리 구문 방식 변경

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -8,45 +8,37 @@ public class Money {
     public static final String NOT_MOD_ZERO_MESSAGE = "[ERROR] 1000으로 나누어지는 금액이 아닙니다.";
     public static final String REJECT_ZERO = "[ERROR] 구입 금액에 0을 입력할 수 없습니다.";
     public static final String REJECT_NOT_NUMBERS = "[ERROR] 구입 금액에 숫자를 제외한 다른 입력이 올 수 없습니다.";
-    private final int money;
+    private int money;
 
     public Money(String input) {
-        validateConsistOfNumbers(input);
-        validateMoneyZero(input);
-        this.money = validateMoney(input);
-    }
-
-    public int validateMoney(String input) {
         try {
-            int money = Integer.parseInt(input);
-            if (Math.floorMod(Math.round(money), STANDARD_WON) != 0) {
-                throw new IllegalArgumentException(NOT_MOD_ZERO_MESSAGE);
-            }
-            return money;
-        } catch (IllegalArgumentException e) {
+            validateConsistOfNumbers(input);
+            validateMoneyZero(input);
+            validateMoney(input);
+            this.money = Integer.parseInt(input);
+        } catch (IllegalArgumentException e){
+            this.money = 0;
             System.out.println(e.getMessage());
-            return 0;
         }
     }
 
-    public void validateConsistOfNumbers(String input) {
+    public void validateMoney(String input) {
+        int money = Integer.parseInt(input);
+        if (Math.floorMod(money, STANDARD_WON) != 0) {
+            throw new IllegalArgumentException(NOT_MOD_ZERO_MESSAGE);
+        }
+    }
+
+    public void validateConsistOfNumbers(String input) throws IllegalArgumentException {
         boolean result = Pattern.matches("^[0-9]*$", input);
-        try {
-            if (!result) {
-                throw new IllegalArgumentException(REJECT_NOT_NUMBERS);
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+        if (!result) {
+            throw new IllegalArgumentException(REJECT_NOT_NUMBERS);
         }
     }
 
-    public void validateMoneyZero(String input) {
-        try {
-            if (input.equals(String.valueOf(ZERO_WON))) {
-                throw new IllegalArgumentException(REJECT_ZERO);
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+    public void validateMoneyZero(String input) throws IllegalArgumentException {
+        if (input.equals(String.valueOf(ZERO_WON))) {
+            throw new IllegalArgumentException(REJECT_ZERO);
         }
     }
 
