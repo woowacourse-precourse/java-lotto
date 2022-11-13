3274f2ef66c5d0febdedb2cf223f658a130d4fff
goldcollar95 <yjs7298@naver.com>
2022-11-14 00:03:28 +0900

    feat(Lotto): 일치한 개수의 총합개수를 나타내는 countNumber()메서드구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -20,30 +20,33 @@ public class Lotto {
         }
     }
 
-    public static void inputBuyPirce1(){
+    public static void inputBuyPirce1() {
         System.out.println("구입금액을 입력해주세요");
     }
 
-    public static void buySucess(int Countlotto){
+    public static void buySucess(int Countlotto) {
         System.out.println(Countlotto + "개를 구매하였습니다.");
     }
 
-    public static int buyScuessCount(int Countlotto){
+    public static int buyScuessCount(int Countlotto) {
         return Countlotto / 1000;
     }
-    public static void sayLottoNumber(){
+
+    public static void sayLottoNumber() {
         System.out.println("당첨 번호를 입력해주세요");
     }
-    public static void bonusSay(){
+
+    public static void bonusSay() {
         System.out.println("보너스 번호를 입력해주세요");
     }
-    public static void LottoResultMessage(){
+
+    public static void LottoResultMessage() {
         System.out.println("\n당첨 통계\n---");
     }
 
 
     //당첨통계
-    public static void toResult( Integer number, int correct, int Countlotto) {
+    public static void toResult(Integer number, int correct, int Countlotto) {
         User user = new User();
         // [1,2,3,4,5,6] 처럼 로또번호 입력값
         String lottoInput = user.lottoListSix();
@@ -59,57 +62,69 @@ public class Lotto {
         }
     }
 
-        // 해당하는 숫자에 도착하면 나오는 결과값 한번에 출력
-        public int[] ScoreNumber(List<Integer> correct, List<Boolean> BonusBoolean ) {
+    // 해당하는 숫자에 도착하면 나오는 결과값 한번에 출력
+    public int[] ScoreNumber(List<Integer> correct, List<Boolean> BonusBoolean) {
 
 
-            int[] CountNumber = {1,2,3,4,5};
-            for (int index = 0; index < correct.size(); index++) {
-                if (correct.get(index) == 3) {
-                    CountNumber[0]++;
-                }if (correct.get(index) == 4) {
-                    CountNumber[1]++;
-                }
-                if (correct.get(index) == 5 && !BonusBoolean.get(index)) {
-                    CountNumber[2]++;
-                }
-                if (correct.get(index) == 5 && BonusBoolean.get(index)) {
-                    CountNumber[3]++;
-                }
-                if (correct.get(index) == 6) {
-                    CountNumber[4]++;
-                }
+        int[] CountNumber = {1, 2, 3, 4, 5};
+        for (int index = 0; index < correct.size(); index++) {
+            if (correct.get(index) == 3) {
+                CountNumber[0]++;
             }
-            return CountNumber;
+            if (correct.get(index) == 4) {
+                CountNumber[1]++;
+            }
+            if (correct.get(index) == 5 && !BonusBoolean.get(index)) {
+                CountNumber[2]++;
+            }
+            if (correct.get(index) == 5 && BonusBoolean.get(index)) {
+                CountNumber[3]++;
+            }
+            if (correct.get(index) == 6) {
+                CountNumber[4]++;
+            }
         }
+        return CountNumber;
+    }
 
 
-        public int[] TotalNumber(List<Integer> correct, List<Boolean> BonusCorrect){
+    public int[] TotalNumber(List<Integer> correct, List<Boolean> BonusCorrect) {
 
 
-            int[] totalNumber = {};
+        int[] totalNumber = {};
 
-            for(int index = 0; index < correct.size(); index++){
-                    if(correct.get(index) == 3){
-                        totalNumber[0]+=5000;
-                    }
-                    if(correct.get(index) == 4){
-                        totalNumber[1]+=50000;
-                    }
-                    if(correct.get(index) == 5 && !BonusCorrect.get(index)){
-                        totalNumber[2]+=1500000;
-                    }
-                    if(correct.get(index) == 5 && BonusCorrect.get(index)){
-                        totalNumber[3]+=30000000;
-                    }
-                    if(correct.get(index) == 6){
-                        totalNumber[4]+=2000000000;
-                    }
+        for (int index = 0; index < correct.size(); index++) {
+            if (correct.get(index) == 3) {
+                totalNumber[0] += 5000;
             }
+            if (correct.get(index) == 4) {
+                totalNumber[1] += 50000;
+            }
+            if (correct.get(index) == 5 && !BonusCorrect.get(index)) {
+                totalNumber[2] += 1500000;
+            }
+            if (correct.get(index) == 5 && BonusCorrect.get(index)) {
+                totalNumber[3] += 30000000;
+            }
+            if (correct.get(index) == 6) {
+                totalNumber[4] += 2000000000;
+            }
+        }
 
         return totalNumber;
 
     }
+
+
+        public int countNumber(List<Integer> countNum){
+            int countN = 0;
+            for(int index = 0; index < countNum.size(); index++){
+                if(countN == countNum.get(index))
+                    countN++;
+            }
+            return countN;
+        }
+
 
         public void decimalPointCirculator(int decimalpoint1, int decimalpoint2){
 
@@ -120,6 +135,8 @@ public class Lotto {
         System.out.println("총 수익률은"+filed/100 +"%입니다.");
 
         }
+
+
 
 }
 
