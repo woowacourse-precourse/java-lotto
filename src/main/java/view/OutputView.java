a709d4863552043f9ef2b799b5c53e71f0b3f482
Kimhyeyun <cqqwer@gmail.com>
2022-11-13 21:03:58 +0900

    Feat: 에러 시 문구 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,9 @@
 package view;
 
+import static lotto.constant.Message.ERROR_MESSAGE;
+
 public class OutputView {
+    public void getErrorMessage(String message) {
+        System.out.println(ERROR_MESSAGE + message);
+    }
 }
