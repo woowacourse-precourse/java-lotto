4f5679201f891e4ae05009980c810d7c4788279a
swonny <wonny921@gmail.com>
2022-11-13 11:26:11 +0900

    feat(CommonException): 로또 구입 금액 단위 예외 처리 메소드 테스트 추가

--- src/main/java/ExceptionCollections/CommonException.java
+++ src/main/java/ExceptionCollections/CommonException.java
@@ -3,13 +3,18 @@ package ExceptionCollections;
 import enumCollections.Exceptions;
 
 public class CommonException {
-    public static final String regularExpression = "^[0-9]*$";
+    public static final String REGULAR_EXPRESSION = "^[0-9]*$";
+    static final int PRICE_PER_LOTTO = 1000;
 
-    public static void hasCharacters(String numbers) {
-        if (!numbers.matches(regularExpression)) {
+    public static void hasCharacters(String number) {
+        if (!number.contains(REGULAR_EXPRESSION)) {
             throw new IllegalArgumentException(Exceptions.getMessage(Exceptions.CHARACTER_INCLUDED));
         }
     }
 
-
+    public static void validatePaymentUnit(int payment) {
+        if (payment % PRICE_PER_LOTTO != 0) {
+            throw new IllegalArgumentException(Exceptions.getMessage(Exceptions.WRONG_MONEY_UNIT));
+        }
+    }
 }
