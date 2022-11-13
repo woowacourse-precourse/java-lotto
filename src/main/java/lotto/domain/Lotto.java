02e95cada05e7499a275635d0a9ed10fa73699b0
HiiWee <wpdlzhf159@gmail.com>
2022-11-14 02:03:57 +0900

    feat: 로또 중복검사 에외 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,8 +2,11 @@ package lotto.domain;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import lotto.constants.message.ExceptionMessage;
 
 public class Lotto {
+    public static final int LOTTO_COUNT = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,9 +15,19 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != LOTTO_COUNT) {
+            throw new IllegalArgumentException(ExceptionMessage.ERROR + ExceptionMessage.NUMERIC_LENGTH);
         }
+        if (isDuplicated(numbers)) {
+            throw new IllegalArgumentException(ExceptionMessage.ERROR + ExceptionMessage.DUPLICATED_NUMBER);
+        }
+    }
+
+    private boolean isDuplicated(List<Integer> numbers) {
+        List<Integer> distinctNumbers = numbers.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        return distinctNumbers.size() != LOTTO_COUNT;
     }
 
     public List<Integer> getNumbers() {
