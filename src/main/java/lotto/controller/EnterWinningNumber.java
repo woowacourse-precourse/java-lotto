b58918d18c7098e6953d9c6a7b6b11fb803eef21
jipyo0106 <68218081+jipyo0106@users.noreply.github.com>
2022-11-14 06:10:31 +0900

    feat: 당첨 로또 길이 유효성 검사

--- src/main/java/lotto/controller/EnterWinningNumber.java
+++ src/main/java/lotto/controller/EnterWinningNumber.java
@@ -2,7 +2,11 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class EnterWinningNumber {
+    public static final int LOTTO_SIZE = 6;
+    
     private String getWinningNumberInput() {
         return Console.readLine();
     }
@@ -10,6 +14,12 @@ public class EnterWinningNumber {
     private void isEachNumberBetween1AND45(int number) {
         if (number > 45 || number < 1) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void lottoSizeValidation(List<Integer> lotto) {
+        if (lotto.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리만 입력 가능합니다.");
         }
     }
 }
