b9a3f8ed6ed0f16756890f93ba8a023779f72ddb
SHEOMM <esh0821@snu.ac.kr>
2022-11-13 18:31:18 +0900

    feat: 전체 로또 계산 기능 추가

---
+++ src/main/java/lotto/domain/CalculateAllWinningService.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import lotto.model.Lotto;
+import lotto.model.Rank;
+
+public class CalculateAllWinningService {
+
+    private final EachNumberCalculateService eachNumberCalculateService;
+
+    public CalculateAllWinningService(Lotto winningLottery, int bonus){
+        eachNumberCalculateService = new EachNumberCalculateService(winningLottery, bonus);
+    }
+
+    public Map<Rank, Integer> calculate(List<Lotto> lotteries){
+        List<Rank> ranks = getAllWinningLotto(lotteries);
+        Map<Rank, Integer> results = winningFormatter();
+        ranks.forEach(
+                rank -> results.put(rank, results.get(rank) + 1)
+        );
+        return results;
+    }
+
+    public List<Rank> getAllWinningLotto(List<Lotto> lotteries){
+        List<Rank> answer = new LinkedList<>();
+        lotteries.forEach(lotto ->
+                answer.add(eachNumberCalculateService.calculate(lotto))
+        );
+        return answer;
+    }
+
+    private Map<Rank, Integer> winningFormatter(){
+        List<Rank> rankResults = Rank.rankFormatter();
+        Map<Rank, Integer> results = new HashMap<>();
+        rankResults.forEach(
+                rank -> results.put(rank, 0)
+        );
+        return results;
+    }
+}
