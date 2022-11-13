d7c0b8c0f64dfe8aeb43e1ce255eefdaaa87e7f3
seonghoo1217 <seonghoo1217@naver.com>
2022-11-13 22:36:46 +0900

    feat(validation) : 사용자가 직접 입력하는 당첨 번호에 대한 테스트 코드 작성
    사용자가 입력하는 당첨번호는 다음과 같은 제약사항을 가진다
    
    1. 총 6자리의 숫자가 입력되어야한다.
    2. 각 숫자들은 ','(콤마)로 구분되어야한다.
    3. 오직 숫자만 입력이 가능하다.

--- src/test/java/lotto/validation/ValidationTest.java
+++ src/test/java/lotto/validation/ValidationTest.java
@@ -1,6 +1,7 @@
 package lotto.validation;
 
 import lotto.BuyLotto;
+import lotto.Jackpot;
 import lotto.Validation;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,20 +14,34 @@ class ValidationTest {
 
     public static BuyLotto byl;
 
+    public static Jackpot jp;
+
     @BeforeAll
     static void import_initialize(){
         byl=new BuyLotto();
+        jp = new Jackpot();
     }
 
 
     @ParameterizedTest
     //given
     @ValueSource(strings = {"a","abc","한글_되나요?","이것도안되야함","1234","121200000000002"})
-    void validation_user_input_money_유저의_입력값이_유효하지_않을경우_예외발생(String source) {
+    void validation_user_input_money_유저의_금액_입력값이_유효하지_않을경우_예외발생(String source) {
         //when
         assertThatThrownBy(()-> byl.prepare_money(source))
                 .isInstanceOf(IllegalArgumentException.class)
                 //then
-                .hasMessageContaining("[ERROR]","1000원 단위","숫자");
+                .hasMessageContaining("[ERROR] 1000원단위의 숫자를 입력해야합니다");
+    }
+
+    @ParameterizedTest
+    //given
+    @ValueSource(strings = {"1","123456","a,b,c,d,e,f","가,나,다,라,마,바","1,2,3,4,5"})
+    void validation_user_input_jackpot_number_유저의_당첨번호_입력값이_유효하지_않을경우_예외발생(String source) {
+        //when
+        assertThatThrownBy(()-> jp.jackpot_number_output_and_validate(source))
+                .isInstanceOf(IllegalArgumentException.class)
+                //then
+                .hasMessageContaining("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 하며 6자리를 입력해야합니다.");
     }
 }
