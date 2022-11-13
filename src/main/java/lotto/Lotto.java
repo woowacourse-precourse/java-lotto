484a1eb6ec756f1fd1617267fb32735a2c6ae4b3
UngHeung <t01047377477@gmail.com>
2022-11-13 21:25:52 +0900

    refactor : (Lotto) validate() 메소드 중복 확인 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,30 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+	public Lotto(List<Integer> numbers) {
+		validate(numbers);
+		this.numbers = numbers;
+	}
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    // TODO: 추가 기능 구현
+	private void validate(List<Integer> numbers) {
+		
+		Set<Integer> numberSet = new HashSet<>();
+		for(int number : numbers) {
+			numberSet.add(number);
+		}
+		
+		if (numberSet.size() != 6) {
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	public List<Integer> getLotto() {
+		return numbers;
+	}
 }
