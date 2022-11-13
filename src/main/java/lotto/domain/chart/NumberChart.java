fca410efd7ee514c814be992ae7d0e803da8c2d0
jaehyeon <jhjh0022@naver.com>
2022-11-13 21:00:01 +0900

    feat: 숫자 상수 차트 구현

--- src/main/java/lotto/domain/chart/NumberChart.java
+++ src/main/java/lotto/domain/chart/NumberChart.java
@@ -1,5 +1,19 @@
 package lotto.domain.chart;
 
-public enum Number {
-    
+public enum NumberChart {
+    MIN_NUMBER(1),
+    MAX_NUMBER(45),
+    LOTTO_MONEY(1000),
+    LOTTO_SIZE(6);
+
+    private final int value;
+
+    NumberChart(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
+
