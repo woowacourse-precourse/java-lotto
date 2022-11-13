a85be916a5b98292a5551621501cf1eb43a2773e
JUNBEOM_LEE <bom1215@naver.com>
2022-11-13 11:09:47 +0900

    feat(Class Application): Lotto numbers
    Add Lotto numbers method to Class Application

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
+
+    public List<Integer> Lotto_number(){
+
+    List<Integer> Lotto_numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    Collections.sort(Lotto_numbers);
+    return Lotto_numbers;    
+    }
+
     public static void main(String[] args) {
+
+
         // TODO: 프로그램 구현
     }
 }
