cb70470add9e72f54fbf94f4578c2e13ad6a2d8a
Ridealist <cjsn92@gmail.com>
2022-11-13 18:35:22 +0900

    feat(Application): 테스트 통과 확인 후 로또 게임 전체 구현 완료(ver 1.0)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,42 @@
 package lotto;
 
+import java.util.List;
+import lotto.domain.Calculator;
+import lotto.domain.Lottery;
+import lotto.input.BonusNumber;
+import lotto.input.Input;
+import lotto.input.Purchase;
+import lotto.input.WinningNumber;
+import lotto.output.BuyList;
+import lotto.output.Summary;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            String inputAmount = Input.askAmount();
+            Purchase pc = new Purchase(inputAmount);
+            Lottery lottery = new Lottery(pc.getAmount());
+            BuyList.show(lottery.getLotteryNumbers(), pc.getAmount());
+
+            String inputWinningNumbers = Input.askWinningNumbers();
+            WinningNumber wn = new WinningNumber(inputWinningNumbers);
+            List<Integer> winningNumbers = wn.getWinningNumbers();
+
+            String inputBonusNumber = Input.askBonusNumber();
+            BonusNumber bn = new BonusNumber(inputBonusNumber, winningNumbers);
+            int bonusNumber = bn.getBonusNumber();
+
+            Calculator cal = new Calculator();
+            for (int i = 0; i < lottery.getLotteryNumbers().size(); i++) {
+                List<Integer> lotto = lottery.getLotteryNumbers().get(i).getLottoNumbers();
+                cal.updateResult(lotto, winningNumbers, bonusNumber);
+            }
+
+            Summary.showResult(cal.firstWin, cal.secondWin, cal.thirdWin, cal.fourthWin, cal.fifthWin);
+            Summary.showEarning(pc.getAmount(), cal.firstWin, cal.secondWin, cal.thirdWin, cal.fourthWin, cal.fifthWin);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
