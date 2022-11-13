3fc67774d5273a94d0db32cae89b216132198231
sjh910805@gmail.com <sjh910805@gmail.com>
2022-11-13 21:45:52 +0900

    feat: 당첨 통계 내역 출력 기능

--- src/main/java/lotto/Player.java
+++ src/main/java/lotto/Player.java
@@ -47,7 +47,7 @@ public class Player {
         System.out.println("5개 일치 (1,500,000원) - " + result[0] + "개");
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + result[0] + "개");
         System.out.println("6개 일치 (2,000,000,000원) - " + result[0] + "개");
-
+        System.out.println("총 수익률은 " + yield + "%입니다.");
     }
 
 
