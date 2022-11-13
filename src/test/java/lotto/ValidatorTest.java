9b01a09320bdd0b36ab419f593cd86041f569b30
yun <yshyeon102@gmail.com>
2022-11-14 05:54:56 +0900

    test(validator): 보너스 숫자 예외 검증 테스트 작성

--- src/test/java/lotto/ValidatorTest.java
+++ src/test/java/lotto/ValidatorTest.java
@@ -3,6 +3,8 @@ package lotto;
 import lotto.Exception.ExceptionType;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ValidatorTest {
@@ -60,5 +62,18 @@ class ValidatorTest {
         assertThatThrownBy(() -> Validator.validateLotto("1,2,3,4,5,5"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionType.LOTTO_DUPLICATE_EXCEPTION.getMessage());
+    }
+
+    @Test
+    void 보너스_숫자_입력시_숫자가_아닌_문자를_입력하는_경우_예외_발생() {
+        assertThatThrownBy(() -> Validator.validateBonusNumber(List.of(1, 2, 3, 4, 5, 6), "a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionType.BONUS_INPUT_TYPE_EXCEPTION.getMessage());
+    }
+    @Test
+    void 보너스_숫자가_로또번호에_포함된_수인_경우_예외_발생() {
+        assertThatThrownBy(() -> Validator.validateBonusNumber(List.of(1, 2, 3, 4, 5, 6), "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionType.BONUS_DUPLICATE_EXCEPTION.getMessage());
     }
 }
