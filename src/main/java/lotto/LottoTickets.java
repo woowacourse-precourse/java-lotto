c91b8425bb72152319299db635b2de77346c7132
chnlee <leechan6022@gmail.com>
2022-11-14 03:14:37 +0900

    feat(LottoTickets) : 개수에 따른 로또 번호 출력 기능 구현

--- src/main/java/lotto/LottoTickets.java
+++ src/main/java/lotto/LottoTickets.java
@@ -1,2 +1,23 @@
-package lotto;public class LottoTickets {
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoTickets {
+    private static final int COUNT_NUM = 1;
+    private final List<Lotto> lottos = new ArrayList<>();
+    public LottoTickets(int count) {
+        getTickets(count);
+    }
+    public void getTickets(int count) {
+        while (count > 0) {
+            lottos.add(LottoGenerate.generateLotto());
+            count -= COUNT_NUM;
+        }
+    }
+    public List<Lotto> getLotto() {
+        return Collections.unmodifiableList(lottos);
+    }
 }
