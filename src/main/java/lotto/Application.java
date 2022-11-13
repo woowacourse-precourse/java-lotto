49c3f8a7377a64ae67a230de7895859523e99429
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-13 21:41:06 +0900

    refactor: controller 클래스 게임 실행 메소드 구현
    controller를 통해 로또 게임이 실행될 수 있도록 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,38 +1,16 @@
 package lotto;
 
-import lotto.domain.LottoRank;
+import lotto.controller.LottoController;
 import lotto.view.InputView;
 import lotto.view.OutputView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static lotto.view.InputView.lottoPurchaseMoney;
 
 public class Application {
     public static void main(String[] args) {
 
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        LottoRank lottoRank;
+        LottoController lottoController = new LottoController();
 
-        outputView.printPriceInputMessage();
-
-
-
-        List<Integer> winningNumbers = inputView.createWinningNumber();
-        int bonusNumber = inputView.inputBonusNumber();
-
-        inputView.allLottoNumber = inputView.createAllGameLottoNumber(lottoTickets);
-        outputView.printLottoPurchaseCompleteMessage(lottoTickets);
-        outputView.printLottoNumber(inputView.allLottoNumber);
-
-        for (int i = 0; i < inputView.allLottoNumber.size(); i++) {
-            inputView.compareWinningNumber(inputView.allLottoNumber.get(i),winningNumbers,bonusNumber);
-        }
-        outputView.printLottoRankResult(inputView.lottoResult);
-        int lottoEarningMoney = inputView.calculrateLottoEarningMoney(inputView.lottoResult);
-        String lottoEarningRate = inputView.calculrateLottoEarningRate(lottoEarningMoney, lottoPurchaseMoney);
-        outputView.printLottoEarningRate(lottoEarningRate);
+        lottoController.run();
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,6 +4,10 @@ import lotto.domain.LottoRank;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
+
+import static lotto.view.InputView.lottoPurchaseMoney;
+
 public class LottoController {
 
     private final int LOTTO_ONE_GAME_PRICE = 1000;
@@ -14,18 +18,34 @@ public class LottoController {
 
     // 게임을 실행하는 메소드
     public void run(){
-        int inputLottoMoney = inputLottoPurchaseMoney();
-        int lottoTickets = createLottoTickets(inputLottoMoney);
-    }
+        outputView.printPriceInputMessage();
 
-    private int inputLottoPurchaseMoney() {
-        int lottoPurchaseMoney = inputView.inputLottoPurchaseMoney();
-        return lottoPurchaseMoney;
+        int inputLottoMoney = inputView.inputLottoPurchaseMoney();
+        int lottoTickets = createLottoTickets(inputLottoMoney);
+        List<Integer> winningNumbers = createWinningNumbers();
+        int bonusNumber = inputView.inputBonusNumber();
+        inputView.allLottoNumber = inputView.createAllGameLottoNumber(lottoTickets);
+
+        outputView.printLottoPurchaseCompleteMessage(lottoTickets);
+        outputView.printLottoNumber(inputView.allLottoNumber);
+
+        for (int i = 0; i < inputView.allLottoNumber.size(); i++) {
+            inputView.compareWinningNumber(inputView.allLottoNumber.get(i),winningNumbers,bonusNumber);
+        }
+
+        outputView.printLottoRankResult(inputView.lottoResult);
+        int lottoEarningMoney = inputView.calculrateLottoEarningMoney(inputView.lottoResult);
+        String lottoEarningRate = inputView.calculrateLottoEarningRate(lottoEarningMoney, lottoPurchaseMoney);
+        outputView.printLottoEarningRate(lottoEarningRate);
+
+
     }
 
     private int createLottoTickets (int inputMoney) {
         return inputMoney / LOTTO_ONE_GAME_PRICE;
     }
 
-
+    private List<Integer> createWinningNumbers () {
+        return inputView.createWinningNumber();
+    }
 }
