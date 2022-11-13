cf9d4755b97a7aed842125054ca9afb82a99aafc
Jimin <thswlals0219@gmail.com>
2022-11-13 20:51:39 +0900

    test(validator): 당첨 금액 입력값을 검사하는 테스트 코드 추가

--- src/test/java/lotto/util/InputValidatorTest.java
+++ src/test/java/lotto/util/InputValidatorTest.java
@@ -74,7 +74,7 @@ class InputValidatorTest {
 
     @Nested
     @DisplayName("구입 금액이 유효하지 않으면 예외가 발생한다.")
-    class isValidatePurchaseAmountTest {
+    class ValidatePurchaseAmountTest {
         @Test
         void 숫자가_아닌_경우_예외_테스트() {
             assertThatThrownBy(() -> inputValidator.validatePurchaseAmount("1000aa"))
@@ -90,6 +90,22 @@ class InputValidatorTest {
         @Test
         void 나누어_떨어지지_않을_경우_예외_테스트() {
             assertThatThrownBy(() -> inputValidator.validatePurchaseAmount("1234"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("당첨 금액 입력 시, 숫자와 콤마로만 이루어져 있지 않으면 예외가 발생한다.")
+    class ValidateWinningNumberTest {
+        @Test
+        void 문자가_있을_경우_예외_테스트() {
+            assertThatThrownBy(() -> inputValidator.validateWinningNumber("1,2,3,4,5,a"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 공백이_있을_경우_예외_테스트() {
+            assertThatThrownBy(() -> inputValidator.validateWinningNumber("1,2,3,4, 5,7"))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
