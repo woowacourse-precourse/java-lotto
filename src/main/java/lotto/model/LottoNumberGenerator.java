eed2ad3f6089716c5c61976c840a61fa46a12224
ChoiYeonho0903 <903yh@naver.com>
2022-11-14 02:48:07 +0900

    refactor: LottoNumberGenerator class 리펙토링

--- src/main/java/lotto/model/LottoNumberGenerator.java
+++ src/main/java/lotto/model/LottoNumberGenerator.java
@@ -5,18 +5,8 @@ import java.util.List;
 
 public class LottoNumberGenerator {
 
-    private final List<Integer> numbers;
-
-    public LottoNumberGenerator() {
-        this.numbers = generateNumbers();
-    }
-
-    private List<Integer> generateNumbers() {
+    public static List<Integer> generateNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
-    }
-
-    public List<Integer> getNumber() {
-        return numbers;
     }
 
 }
