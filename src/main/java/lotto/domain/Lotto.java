98788d0e2d549fce1b6085a1c8a05927f00ab612
buddy <jang_taehwan@naver.com>
2022-11-14 02:06:10 +0900

    refactor(Lotto): 매직 넘버 enum 적용

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -12,16 +12,28 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LottoConstant.NUMBERS_SIZE.getValue()) {
             throw new IllegalArgumentException("로또는 6개의 숫자만 입력 가능합니다.");
         }
-        if (new HashSet<>(numbers).size() != 6) {
+        if (new HashSet<>(numbers).size() != LottoConstant.NUMBERS_SIZE.getValue()) {
             throw new IllegalArgumentException("로또는 중복된 숫자로 구성될 수 없습니다.");
+        }
+        for (int number : numbers) {
+            validateNumber(number);
         }
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
+    private void validateNumber(int number) {
+        if (number > LottoConstant.MAX_NUMBER.getValue()) {
+            throw new IllegalArgumentException(LottoConstant.MAX_NUMBER.getValue() + "보다 큰 값은 입력될 수 없습니다.");
+        }
+        if (number < LottoConstant.MIN_NUMBER.getValue()) {
+            throw new IllegalArgumentException(LottoConstant.MIN_NUMBER.getValue() + "보다 낮은 값은 입력될 수 없습니다.");
+        }
     }
 }
---
+++ src/main/java/lotto/domain/LottoConstant.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+public enum LottoConstant {
+
+    MIN_NUMBER(1),
+    MAX_NUMBER(45),
+    NUMBERS_SIZE(6);
+
+    private final int value;
+
+    LottoConstant(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
