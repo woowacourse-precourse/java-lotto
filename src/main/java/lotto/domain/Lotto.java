36b798f1436c01254d9570012576d7eca2be1234
peterysh <52229539+peterysh@users.noreply.github.com>
2022-11-13 16:46:42 +0900

    feat: add Lotto class getter

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import lotto.LottoString;
+import lotto.global.ErrorMessage;
+
 import java.util.List;
 
 public class Lotto {
@@ -12,13 +15,17 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_MAKER_ERROR);
         }
         for(Integer num : numbers){
             if(num < 0 || num > 45)
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(ErrorMessage.NUMBER_MAKER_ERROR);
         }
     }
 
     // TODO: 추가 기능 구현
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
