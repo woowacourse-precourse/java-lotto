61e6b0eea5c2f7da4da5ddbba3c3913e58a20090
DaWoon Lee <qkgk61@gmail.com>
2022-11-13 19:52:14 +0900

    Feat : 보너스 번호가 당첨 번호와 중복되는지 검사하여 예외 처리

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.aspect.Validator;
 
 public class WinningLotto extends Lotto {
 
@@ -8,6 +9,7 @@ public class WinningLotto extends Lotto {
 
     public WinningLotto(List<Integer> numbers, int bonusNumber) {
         super(numbers);
+        Validator.validateBonusNumber(numbers, bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
