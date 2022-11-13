b88d16c7c1037e8ccb725de75ad37926ca2c5f31
poter0501 <poter0501@gmail.com>
2022-11-13 21:08:42 +0900

    feat(Lotto): 당첨번호와 보너스 번호를 입력으로 받아 몇개가 일치 하는 지 반환하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
@@ -24,6 +26,24 @@ public class Lotto {
         if (numbersCount != tempNumbers.size()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호로 중복된 숫자가 존재 합니다.");
         }
+    }
+
+    public List<Integer> countCoincidence(List<Integer> winningNumbers, int bonusNumber) {
+        int countWin = 0;
+        int countBonus = 0;
+
+        HashSet<Integer> tempWinningNumbers = new HashSet<>(winningNumbers);
+        for (int number : this.numbers) {
+            if (tempWinningNumbers.contains(number)) {
+                countWin++;
+            }
+            if (number == bonusNumber) {
+                countBonus = 1;
+            }
+
+        }
+
+        return new ArrayList<Integer>(List.of(countWin, countBonus));
     }
 
     // TODO: 추가 기능 구현
