8f6e1525a23e0ac0e036135e7710a97cf99695db
thousandcows <milcamboo@gmail.com>
2022-11-13 22:54:06 +0900

    test: create test for calculateYield function

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
@@ -112,4 +113,27 @@ class LottoTest {
 
     }
 
+    @DisplayName("수익률을 계산한 뒤 그 값을 리턴한다.")
+    @Test
+    void calculateYield() {
+
+        int totalPayment = 14000;
+
+        double prizeOne = 10000;
+        double prizeTwo = 10000000;
+        double prizeThree = 1500000;
+        double prizeFour = 50000;
+
+        String answerOne = String.format("%.1f", prizeOne / totalPayment);
+        String answerTwo = String.format("%.1f", prizeOne / totalPayment);
+        String answerThree = String.format("%.1f", prizeOne / totalPayment);
+        String answerFour = String.format("%.1f", prizeOne / totalPayment);
+
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+
+        assertThat(lotto.calculateYield(prizeOne / totalPayment)).isEqualTo(answerOne);
+        assertThat(lotto.calculateYield(prizeTwo / totalPayment)).isEqualTo(answerTwo);
+        assertThat(lotto.calculateYield(prizeThree / totalPayment)).isEqualTo(answerThree);
+        assertThat(lotto.calculateYield(prizeFour / totalPayment)).isEqualTo(answerFour);
+    }
 }
