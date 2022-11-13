a2b26febea845a10000c42b28737955265e46232
chj <bluedog129@gmail.com>
2022-11-13 12:01:50 +0900

    docs : 입력기능 구현할 클래스와 함수명 작성

---
+++ src/main/java/lotto/MoneyCalculator.java
@@ -0,0 +1,14 @@
+package lotto;
+
+public class MoneyCalculater {
+    private int receivedMoney = 0;
+
+
+    public MoneyCalculater() {
+    }
+
+    public int inputMoney() {
+
+        return 0;
+    }
+}
