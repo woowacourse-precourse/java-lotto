e6c30be493bf6a0a0c1c4245df61f49b54546290
Sehbeom <j3beom@gmail.com>
2022-11-13 19:58:41 +0900

    feat(controller): 당첨통계 기능 구현
    당첨통계 테스트 통과 코드 작성
    리팩토링 후속 예정

--- src/main/java/lotto/domain/controller/LottoIssuer.java
+++ src/main/java/lotto/domain/controller/LottoIssuer.java
@@ -4,10 +4,28 @@ import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import lotto.constants.RankingInformation;
 import lotto.domain.model.Lotto;
+import lotto.domain.model.WinningLotto;
 
 public class LottoIssuer {
+
+    private Map<RankingInformation, Integer> statistics;
+
+    public LottoIssuer() {
+        initStatistics();
+    }
+
+    private void initStatistics() {
+        statistics = new HashMap<>();
+
+        for (RankingInformation oneRank : RankingInformation.values()) {
+            statistics.put(oneRank, 0);
+        }
+    }
 
     public List<Lotto> issueLottos(int numberOfLottos) {
         List<Lotto> lottos = new ArrayList<>();
@@ -26,5 +44,40 @@ public class LottoIssuer {
         oneLottoNumbers.sort(Comparator.naturalOrder());
 
         return new Lotto(oneLottoNumbers);
+    }
+
+    public Map<RankingInformation, Integer> makeWinningStatistics(List<Lotto> lottos,
+            WinningLotto winningLotto) {
+        List<Integer> winningNumbers = winningLotto.getWinningNumbers();
+        int bonusNumber = winningLotto.getBonusNumber();
+
+        RankingInformation[] rankingInformations = RankingInformation.values();
+
+        for (Lotto oneLotto : lottos) {
+            List<Integer> lottoNumbers = new ArrayList<>();
+            lottoNumbers.addAll(oneLotto.getNumbers());
+            boolean containsBonus = lottoNumbers.contains(bonusNumber);
+            lottoNumbers.retainAll(winningNumbers);
+
+            int rankIndex = lottoNumbers.size() - 3;
+
+            if (rankIndex == 3) {
+                rankIndex++;
+            }
+
+            if (rankIndex == 2) {
+                if (containsBonus) {
+                    rankIndex++;
+                }
+            }
+
+            if (rankIndex >= 0) {
+                RankingInformation rank = rankingInformations[rankIndex];
+                int count = statistics.get(rank) + 1;
+                statistics.put(rank, count);
+            }
+        }
+
+        return statistics;
     }
 }
