032da6f36bb94de30899283abd45875735f50ef0
skydreamer21 <skydreamer210@gmail.com>
2022-11-13 21:04:38 +0900

    test(UserTest): buyLotteryTicket 메서드 테스트 코드 추가
    [세부사항]
    - 테스트 사항
        1. buyLotteryTicket 이후 User의 money가 1000원 감소한다.
        2. buyLotteryTicket 이후 lotteryTickets 리스트에 lotteryTicket이 추가된다.

--- src/test/java/lotto/domain/UserTest.java
+++ src/test/java/lotto/domain/UserTest.java
@@ -3,6 +3,9 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class UserTest {
@@ -13,5 +16,31 @@ public class UserTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("1000원 단위로")
                 .hasMessageNotContaining("[ERROR]");
+    }
+
+    @DisplayName("buyLotteryTicket 이후 User의 money가 1000원 감소한다.")
+    @Test
+    void decreaseMoneyAfterPurchase() {
+        User user = new User(2000);
+        LotteryTicket lotteryTicket = new LotteryTicket(List.of(1, 2, 3, 4, 5, 6));
+
+        List<LotteryTicket> lotteryTickets = new ArrayList<>();
+        lotteryTickets.add(lotteryTicket);
+
+        user.buyLotteryTicket(lotteryTicket);
+        assertThat(user.getMoney()).isEqualTo(1000);
+    }
+
+    @DisplayName("buyLotteryTicket 이후 lotteryTickets 리스트에 lotteryTicket이 추가된다.")
+    @Test
+    void addLotteryTicketAfterPurchase() {
+        User user = new User(2000);
+        LotteryTicket lotteryTicket = new LotteryTicket(List.of(1, 2, 3, 4, 5, 6));
+
+        List<LotteryTicket> lotteryTickets = new ArrayList<>();
+        lotteryTickets.add(lotteryTicket);
+
+        user.buyLotteryTicket(lotteryTicket);
+        assertThat(user.getLotteryTickets()).isEqualTo(lotteryTickets);
     }
 }
