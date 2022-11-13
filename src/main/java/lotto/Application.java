e0f7128db3da1238dc812871fa2d0f825790d306
이건회 <rg970604@naver.com>
2022-11-13 19:48:33 +0900

    feat(Application.java) : 게임 실행 순서

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,35 @@
 package lotto;
 
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import lotto.domain.LottoPieces;
+import lotto.domain.Lottos;
+import lotto.domain.Prize;
+import lotto.domain.ProfitCalculator;
+import lotto.strategy.LottoGenerateStrategy;
+import lotto.strategy.RandomLottoGenerateStrategy;
+import lotto.view.PrintResult;
+import lotto.view.UserInput;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int money = UserInput.InputMoney();
+        LottoPieces lottoPieces = new LottoPieces(money);
+        int pieces = lottoPieces.pieces;
+        PrintResult.PrintPieces(pieces);
+
+        LottoGenerateStrategy lottoGenerateStrategy = new RandomLottoGenerateStrategy();
+        Lottos lottos = new Lottos(pieces, lottoGenerateStrategy);
+        PrintResult.PrintLottos(lottos);
+
+        Lotto winningLotto = new Lotto(UserInput.InputWinningNums());
+        int bonusNum = UserInput.InputBonusNum();
+
+        Map<Prize,Integer> prizeBoard = LottoMachine.checkPrizes(winningLotto,lottos,bonusNum);
+        PrintResult.PrintPrizeStatistic(prizeBoard);
+        float profit = ProfitCalculator.calculateProfitRate(prizeBoard,money);
+        PrintResult.PrintProfitRate(profit);
     }
 }
