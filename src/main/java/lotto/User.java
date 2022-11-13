a94b0d15ddf8e2ff1698d360143684f5dcb204d4
d0ri123 <lsh81550@gmail.com>
2022-11-13 13:29:26 +0900

    feat: 사용자가 로또 구매금액을 입력하는 기능 구현

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class User {
     private int lottoPrice;
@@ -8,7 +9,9 @@ public class User {
     private int bonusNumber;
 
     public void buyLotto(){
-
+        System.out.println("구매 금액을 입력해주세요.");
+        Scanner sc = new Scanner(System.in);
+        this.lottoPrice = sc.nextInt();
     }
 
     public void userNumbers(){
