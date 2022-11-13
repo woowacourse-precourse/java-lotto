74e71132c1ee76f776dc7040d7e8965d538aca7b
Hong-yeonggwang <chamsk0808@naver.com>
2022-11-13 18:51:07 +0900

    faet: checkWin( 당첨번호와 번호가 몇개 일치하는지 확인하는 변수 배열을 반환[일치하는번호의 갯수, 보너스 번호 일치하는지]),expenseWithProfit([구매금액,당첨금액]을반환)

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,7 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,16 +17,40 @@ public class Lotto {
         }
     }
 
-    public int checkWin() {
+    public int[] checkWin() {
         LottoWin lottoWin = LottoWin.getInstance();
         List<Integer> winNumber = lottoWin.getLottoNum();
-        int sameCnt = 0;
-        for(int user : this.numbers){
-            if(winNumber.contains(user)){
-                sameCnt += 1;
+        final int nomalSameIndex = 1;
+        final int bounsSameIndex = 1;
+        int[] sameCnt = new int[2];
+        for(int i = 0; i<this.numbers.size() -1; i++){
+            if(winNumber.contains(this.numbers.get(i))){
+                sameCnt[nomalSameIndex] += 1;
             }
         }
+        if(winNumber.contains(this.numbers.get(this.numbers.size()))){
+            sameCnt[bounsSameIndex] += 1;
+        }
         return sameCnt;
+    }
+
+    public int[] expenseWithProfit(int[] sameCnt){
+        int[] returnData = {1000,0};
+        int[] profitTable = {0,0,0,5000,50000,1500000,30000000,2000000000};
+        returnData[1] = profitTable[sameCnt[0]];
+        if(sameCnt[0] == 5){
+            if(sameCnt[1] == 0){
+                returnData[1] = profitTable[sameCnt[0]];
+            }else if(sameCnt[1] == 1){
+                returnData[1] = profitTable[sameCnt[0]+1];
+            }
+        } else if(sameCnt[0] == 6){
+            returnData[1] = profitTable[sameCnt[0]+1];
+        }
+        return returnData;
+    }
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
     // TODO: 추가 기능 구현
