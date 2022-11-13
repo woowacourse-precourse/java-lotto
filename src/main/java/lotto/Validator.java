0b649fc5c573ad8c67159a6b78870bfe75fe11aa
hyeongcheolkim <tori1753@naver.com>
2022-11-13 20:28:47 +0900

    feat: Validator클래스에 예외상황 추가

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -1,8 +1,32 @@
 package lotto;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class Validator {
 
     public boolean isBetween1And45(Integer number) {
         return 1 <= number && number <= 45;
+    }
+
+    public boolean isContainDuplicateNumber(List<Integer> numbers) {
+        return new HashSet(numbers).size() != numbers.size();
+    }
+
+    public boolean isAllDigit(String string) {
+        return string.chars()
+                .mapToObj(e -> (char) e)
+                .allMatch(Character::isDigit);
+    }
+
+    public boolean isProperNumbersLineFormat(String string, String delimiter) {
+        long count = string.chars()
+                .mapToObj(e -> (char) e)
+                .filter(e -> e.equals(delimiter.charAt(0)))
+                .count();
+        boolean validFormat = string.chars()
+                .mapToObj(e -> (char) e)
+                .allMatch(e -> Character.isDigit(e) || e.equals(delimiter.charAt(0)));
+        return count == 5L && validFormat;
     }
 }
