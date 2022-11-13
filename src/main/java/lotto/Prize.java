2c81143ff612f5664d62900a068b7ca817258534
Jeangyu <gjwlsrb1700@naver.com>
2022-11-13 17:49:48 +0900

    [FEAT] 당첨 되지 않은 경우 추가

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,24 +1,36 @@
 package lotto;
 
-enum Prize {
-    FIRST(2000000000, 1),
-    SECOND(30000000, 2),
-    THIRD(1500000, 3),
-    FORTH(50000, 4),
-    FIFTH(5000, 5);
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    private final int prize;
+enum Prize {
+    FIRST("2,000,000,000", 1, 7),
+    SECOND("30,000,000", 2, 6),
+    THIRD("1,500,000", 3, 5),
+    FORTH("50,000", 4, 4),
+    FIFTH("5,000", 5, 3),
+    NOTHING("0", 6, 0);
+
+    private final String prize;
     private final int name;
-    Prize(int money, int name){
+    private final int count;
+
+    Prize(String money, int name, int count){
         this.prize = money;
         this.name = name;
+        this.count = count;
     }
 
-    public int getPrize(){
+    public String getPrize(){
         return prize;
     }
 
     public int getName(){
         return name;
+    }
+
+    public int getCount(){
+        return count;
     }
 }
