c5c1ae8607364c1fccc7e42332665f82a378e0e1
heozzi <hz0819@naver.com>
2022-11-13 12:20:16 +0900

    1차 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,86 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
+    static List<List<Integer>> ListArrary = new ArrayList<>();
+    static String Regex = "[^0-9]";
+    public static int Insertpurchase() {
+        String strVal = Console.readLine();
+        if (strVal.replaceAll(Regex,"").length()!=strVal.length()) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값을 입력하였습니다.");
+        }
+        int intval = Integer.parseInt(strVal);
+
+        if (intval%1000>=1) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위가 아닙니다.");
+        }
+        return (int)intval/1000;
+    }
+    public static List<Integer> RandomChoice() {
+        List<Integer> Lottonum = new ArrayList<>();
+        while (Lottonum.size() < 6) {
+            int randomNumber = Randoms.pickNumberInRange(1, 45);
+            if (!Lottonum.contains(randomNumber)) {
+                Lottonum.add(randomNumber);
+            }
+        }
+        Collections.sort(Lottonum);
+        return Lottonum;
+    }
+    public static void MakeLotto(int Lottonumber) {
+        for (int i=0;i<Lottonumber;i++) {
+            List<Integer> TmpList = RandomChoice();
+            ListArrary.add(TmpList);
+            System.out.println(TmpList);
+        }
+    }
+    public static List<Integer> InsertLotto() {
+        List<Integer> AnswerLottoInt = new ArrayList<>();
+        String AnswerLotto = Console.readLine();
+        String[] SplitStr = AnswerLotto.split(",");
+
+        for (int i=0; i<SplitStr.length; i++) {
+            if (SplitStr[i].replaceAll(Regex,"").length()!=SplitStr[i].length()) {
+                throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값을 입력하였습니다.");
+            }
+            int tmpInt = Integer.parseInt(SplitStr[i]);
+            if (tmpInt<=0 || tmpInt>=46) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+            AnswerLottoInt.add(tmpInt);
+        }
+        return AnswerLottoInt;
+    }
+    public static int InsertBounus() {
+        String Bonus = Console.readLine();
+
+        if (Bonus.replaceAll(Regex,"").length()!=Bonus.length()) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값을 입력하였습니다.");
+        }
+        int BonusNum = Integer.parseInt(Bonus);
+        if (BonusNum<=0 || BonusNum>=46) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        return BonusNum;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int Lottonumber = Insertpurchase();
+        System.out.printf("\n%d개를 구매했습니다.\n",Lottonumber);
+        MakeLotto(Lottonumber);
+        System.out.println("당첨 번호를 입력해주 주세요.");
+        List<Integer> AnswerLotto = InsertLotto();
+        Lotto A = new Lotto(AnswerLotto);
+        System.out.println("보너스 번호를 입력해주세요.");
+        int BonousNum = InsertBounus();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        A.Statistics(ListArrary,BonousNum);
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -14,6 +15,61 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        for (int i=0;i<numbers.size();i++) {
+            if (i!=numbers.indexOf(numbers.get(i))) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+    public int CheckLotto(List<Integer> ListLotto) {
+        int Count = 0;
+        for (int i=0; i<ListLotto.size();i++) {
+            int TmpInt = (int)ListLotto.get(i);
+            if (numbers.contains(TmpInt)) Count+=1;
+        }
+        return Count;
+    }
+    public int CheckValue(int Count,Boolean BonusCheck) {
+        if (Count==3) return 0;
+        if (Count==4) return 1;
+        if (Count==5 && BonusCheck) return 3;
+        if (Count==5) return 2;
+        if (Count==6) return 4;
+        return -1;
+    }
+
+    public int PrintResult(List<Integer> Result){
+        int SumValue = 0;
+        String[] amount = {" (5,000원)"," (50,000원)"," (1,500,000원)",", 보너스 볼 일치 (30,000,000원)"," (2,000,000,000원)"};
+        int[] intamount = {5000,50000,1500000,30000000,2000000000};
+        for (int i=0;i<Result.size();i++) {
+            if (i>=3) System.out.printf("%d개 일치%s - %d개\n",i+2,amount[i],Result.get(i));
+            else System.out.printf("%d개 일치%s - %d개\n",i+3,amount[i],Result.get(i));
+            SumValue+=intamount[i]*(int)Result.get(i);
+        }
+        return SumValue;
+    }
+    public Boolean yield(int Total,int Sumval) {
+        if (Sumval==0) {
+            System.out.println("총 수익률은 0%입니다.");
+            return true;
+        }
+        float yieldval = (float)Sumval/(Total*1000);
+        System.out.printf("총 수익률은 %.1f%%입니다.\n",yieldval*100);
+        return true;
+    }
+
+    public void Statistics(List<List<Integer>> Lottoes,int BonusNum) {
+        List<Integer> Result = new ArrayList<>();
+        for (int i=0;i<5;i++) Result.add(0);
+        for (List ListLotto : Lottoes) {
+            int Count = CheckLotto(ListLotto);
+            int Judg = CheckValue(Count,ListLotto.contains(BonusNum));
+            if (Judg<0) continue;
+            Result.set(Judg,Result.get(Judg)+1);
+        }
+        int SumValue = PrintResult(Result);
+        yield(Lottoes.size(),SumValue);
     }
 
     // TODO: 추가 기능 구현
