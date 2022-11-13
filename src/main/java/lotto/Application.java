6813d70453c801113568f5d26376ec7cf81e2e21
SeongchanCho <wordnumperiod@gmail.com>
2022-11-13 09:34:06 +0900

    feat(Application.java): 로또번호 Random pick 기능, 로또 정답 입력 받는 기능 구현 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,24 +1,56 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
-    public static void main(String[] args) {
-        int money=getMoneyInput();
 
+    public static void main(String[] args) {
+        int money = getMoneyInput();
+        int lottoTicketNum = calculateHowManyTicketUserCanBuy(money);
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < lottoTicketNum; i++) {
+            lottoList.add(new Lotto(pickLottoNumbersByRandom()));
+        }
+        List<WinningStatistic> statistics = new ArrayList<>();
+//        for (int i = 0; i < lottoTicketNum; i++) {
+//            statistics.add(LottoMarker.produceWinningStatistic())
+//        }
     }
 
-    private static int getMoneyInput(){
-        int moneyInput=getIntegerInput();
-        if(moneyInput<0){
+    private static int getMoneyInput() {
+        int moneyInput = getIntegerInput();
+        if (moneyInput < 0) {
             throw new IllegalArgumentException();
         }
         return moneyInput;
     }
 
-    private static int getIntegerInput(){
-        try{
+    private static int getIntegerInput() {
+        try {
             return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static int calculateHowManyTicketUserCanBuy(int money) {
+        return money / LottoWinNo.price;
+    }
+
+    private static List<Integer> pickLottoNumbersByRandom() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    private static List<Integer> getLottoNumbersInput() {
+        String oneLine = Console.readLine();
+        try {
+            return Arrays.stream(oneLine.split(",")).map(Integer::parseInt)
+                .collect(Collectors.toList());
         }catch(NumberFormatException e){
             throw new IllegalArgumentException();
         }
