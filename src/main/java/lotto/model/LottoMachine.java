725b68852cc90ba32fc83672930e5e838999e0de
leesky <leehaneul0623@gmail.com>
2022-11-13 15:57:31 +0900

    feat(lottoMachine) : 유효성검사 구현
    올바른 값의 입력을 위해서 유효성 검사 구현 :
     - 당첨번호와 보너스번호 중복 검사 기능 및 테스트코드 작성
     - 당첨번호 중 중복번호 확인 검사 기능 및 테스트코드 작성
     - 입력된 번호 범위 검사 기능 및 테스트코드 작성
     - 입력된 금액 검사 기능 및 테스트코드 작성

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -2,22 +2,39 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class LottoMachine {
     private List<Integer> answer;
     private int bonus;
 
+    private void validate(List<Integer> answer, int bonus) {
+        if (answer.contains(bonus)) throw new IllegalArgumentException("당첨 번호와 중복되지 않는 보너스 번호를 입력해주세요.");
+        if (answer.size() != 6) throw new IllegalArgumentException("당첨 번호는 6개의 숫자입니다.");
+
+        validateNumber(bonus);
+        validateOverlap(answer);
+        answer.forEach(this::validateNumber);
+    }
+
+    private void validateNumber(int number) {
+        if (number > 45 || number < 1) throw new IllegalArgumentException("번호는 1이상 45이하 입니다.");
+    }
+
+    private void validateOverlap(List<Integer> answer) {
+        Set<Integer> set = new HashSet<>(answer);
+        if (set.size() != 6) throw new IllegalArgumentException("로또의 각 번호는 중복이 없어야 합니다.");
+    }
+
     public void setLuckyNumber(List<Integer> answer, int bonus) {
+        validate(answer, bonus);
         this.answer = answer;
         this.bonus = bonus;
     }
 
     public List<Lotto> publish(long price) {
+        if (price < 1000) throw new IllegalArgumentException("입력하신 금액으로는 복권을 구매할 수 없습니다.");
         List<Lotto> lottos = new ArrayList<>();
         while (price >= 1000) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
--- src/test/java/lotto/model/LottoMachineTest.java
+++ src/test/java/lotto/model/LottoMachineTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoMachineTest {
     private static LottoMachine lottoMachine;
@@ -32,7 +33,7 @@ class LottoMachineTest {
 
     @DisplayName("주어진 가격으로 적절하게 로또를 발행하는 지 검증한다.")
     @ParameterizedTest(name = "로또 발행 테스트[{index}] -> 주어진 값이 {0}원이라면 로또를 {1}장 발행한다.")
-    @CsvSource(value = {"20000:20", "10000:10", "1000:1", "2000:2", "500:0", "0:0", "12000:12", "12500:12"}, delimiter = ':')
+    @CsvSource(value = {"20000:20", "10000:10", "1000:1", "2000:2", "12000:12", "12500:12"}, delimiter = ':')
     void lottoMachineProduceLottoProperly(int source, int expected) {
         int actual = lottoMachine.publish(source).size();
         assertThat(actual).isEqualTo(expected);
@@ -93,5 +94,69 @@ class LottoMachineTest {
             boolean actual = isSortedByASC(numbers);
             assertThat(actual).isTrue();
         });
+    }
+
+    @DisplayName("당첨 번호가 6개의 숫자로 이루어지지 않는다면 예외를 발생한다.")
+    @Test
+    void notSixNumberThenReturningException() {
+        assertThatThrownBy(() -> lottoMachine.setLuckyNumber(List.of(1, 2, 3, 4), 5))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("당첨 번호는 6개의 숫자입니다.");
+    }
+
+    @DisplayName("보너스 번호가 1 미만이라면 예외를 발생한다.")
+    @Test
+    void bonusNumberIsLessThenOneReturningException() {
+        assertThatThrownBy(() -> lottoMachine.setLuckyNumber(List.of(1, 2, 3, 4, 5, 6), 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("번호는 1이상 45이하 입니다.");
+    }
+
+    @DisplayName("당첨 번호의 각 자리가 1 미만이라면 예외를 발생한다.")
+    @Test
+    void luckyNumberIsLessThenOneReturningException() {
+        assertThatThrownBy(() -> lottoMachine.setLuckyNumber(List.of(1, 2, 3, -1, 5, 6), 7))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("번호는 1이상 45이하 입니다.");
+    }
+
+    @DisplayName("보너스 번호가 45 초과라면 예외를 발생한다.")
+    @Test
+    void bonusNumberIsGreaterThenFortyFiveReturningException() {
+        assertThatThrownBy(() -> lottoMachine.setLuckyNumber(List.of(1, 2, 3, 4, 5, 6), 46))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("번호는 1이상 45이하 입니다.");
+    }
+
+    @DisplayName("당첨 번호의 각 자리가 45 초과라면 예외를 발생한다.")
+    @Test
+    void luckyNumberIsGreaterThenFortyFiveReturningException() {
+        assertThatThrownBy(() -> lottoMachine.setLuckyNumber(List.of(1, 2, 3, 46, 5, 6), 7))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("번호는 1이상 45이하 입니다.");
+    }
+
+    @DisplayName("당첨 번호와 보너스 번호가 중복되면 예외가 발생한다.")
+    @Test
+    void containOverlapNumberBetweenBonusAndLuckyNumbersThenReturningException() {
+        assertThatThrownBy(() -> lottoMachine.setLuckyNumber(List.of(1, 2, 3, 4, 5, 6), 6))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("당첨 번호와 중복되지 않는 보너스 번호를 입력해주세요.");
+    }
+
+    @DisplayName("입력한 금액이 1000보다 작다면 로또를 발생할 수 없으므로 예외를 발생한다.")
+    @Test
+    void moneyLessThenOneThousandReturningException() {
+        assertThatThrownBy(() -> lottoMachine.publish(-100))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력하신 금액으로는 복권을 구매할 수 없습니다.");
+    }
+
+    @DisplayName("입력한 당첨번호에 중복이 있다면 예외를 발생시킨다.")
+    @Test
+    void containOverlapNumberInLuckyNumbersThenReturningException() {
+        assertThatThrownBy(() -> lottoMachine.setLuckyNumber(List.of(1, 1, 2, 3, 4, 5), 7))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또의 각 번호는 중복이 없어야 합니다.");
     }
 }
