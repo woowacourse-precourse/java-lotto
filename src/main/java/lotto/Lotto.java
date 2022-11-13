adb9a4e4fdaabca849546c7429cfe1a308eb1338
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2022-11-13 21:27:29 +0900

    feat(lotto): 로또 길이가 올바른지 확인하는 기능에 로그 메시지 출력 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,6 +8,7 @@ import lotto.utils.Logger.LogType;
 
 public class Lotto {
     private static final String EXISTED_LOTTO_ERROR_MESSAGE = "로또 번호가 존재하지 않습니다.";
+    private static final String LOTTO_LENGTH_ERROR_MESSAGE = "로또 번호는 " + LOTTO_LENGTH + "개의 숫자로 이루어져야 합니다.";
 
     private final List<Integer> numbers;
 
@@ -26,6 +27,7 @@ public class Lotto {
 
     private void checkLottoLength(List<Integer> numbers) {
         if (numbers.size() != LOTTO_LENGTH) {
+            Logger.log(LOTTO_LENGTH_ERROR_MESSAGE, LogType.ERROR);
             throw new IllegalArgumentException();
         }
     }
