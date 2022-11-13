b74ab0c072894320e1d59906ea5ddd96a071d0f1
Jin409 <sualng123@gmail.com>
2022-11-13 17:14:01 +0800

    refactor(ErrorMessages): 형태 에러 메시지가 로또 번호와 금액을 포괄적으로 나타낼 수 있도록 수정

--- src/main/java/lotto/validator/ErrorMessages.java
+++ src/main/java/lotto/validator/ErrorMessages.java
@@ -12,7 +12,7 @@ public class ErrorMessages {
     public static final String DUPLICATE_LOTTO_NUMBER_ERROR_MESSAGE = ERROR_MESSAGE_PREFIX + "로또 번호는 중복되지 않아야 합니다.";
     public static final String INVALID_RANGE_ERROR_MESSAGE =
             ERROR_MESSAGE_PREFIX + "로또 번호는 " + VALID_MIN_RANGE + "부터 " + VALID_MAX_RANGE + "사이의 숫자여야 합니다";
-    public static final String INVALID_TYPE_ERROR_MESSAGE = ERROR_MESSAGE_PREFIX + "로또 번호는 정수여야 합니다.";
+    public static final String INVALID_TYPE_ERROR_MESSAGE = ERROR_MESSAGE_PREFIX + "정수를 입력해야 합니다.";
     public static final String INVALID_PAID_MONEY_ERROR_MESSAGE =
             ERROR_MESSAGE_PREFIX + "지불된 금액은 " + LOTTO_PRICE + "원 단위여야 합니다.";
     public static final String DUPLICATE_BONUS_NUMBER_ERROR_MESSAGE =
