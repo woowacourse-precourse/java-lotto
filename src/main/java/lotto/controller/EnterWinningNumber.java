2a9caebc27f6b4f8d919c6b992ac6448e990b175
jipyo0106 <68218081+jipyo0106@users.noreply.github.com>
2022-11-14 06:01:19 +0900

    feat: 로또 당첨 번호 입력 기능 구현

---
+++ src/main/java/lotto/controller/EnterWinningNumber.java
@@ -0,0 +1,9 @@
+package lotto.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class EnterWinningNumber {
+    private String getWinningNumberInput() {
+        return Console.readLine();
+    }
+}
