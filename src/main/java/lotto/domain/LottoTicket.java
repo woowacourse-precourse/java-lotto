62c48c19f67a3c46a9f6678bc1b5939826ca57f5
greeng00se <thegreengoose7@gmail.com>
2022-11-13 16:26:34 +0900

    feat(LottoTicket): 로또 결과 확인하는 check 메서드 추가

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,7 +1,12 @@
 package lotto.domain;
 
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class LottoTicket {
 
@@ -19,6 +24,12 @@ public class LottoTicket {
     public List<List<Integer>> value() {
         return ticket.stream()
                 .map(Lotto::value)
-                .collect(Collectors.toList());
+                .collect(toList());
+    }
+
+    public LottoResult check(Lotto winningNumbers, LottoNumber bonusNumber) {
+        return ticket.stream()
+                .map(lotto -> lotto.check(winningNumbers, bonusNumber))
+                .collect(collectingAndThen(groupingBy(identity(), counting()), LottoResult::new));
     }
 }
--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -1,30 +1,72 @@
 package lotto.domain;
 
+import static lotto.domain.LottoPrize._1ST_PRIZE;
+import static lotto.domain.LottoPrize._2ND_PRIZE;
+import static lotto.domain.LottoPrize._3RD_PRIZE;
+import static lotto.domain.LottoPrize._4TH_PRIZE;
+import static lotto.domain.LottoPrize._5TH_PRIZE;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayName("LottoTicket 클래스")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class LottoTicketTest {
 
-    @Test
-    void value_메서드는_로또_번호_리스트들을_반환한다() {
+    private LottoTicket lottoTicket;
+
+    @BeforeEach
+    void setup() {
         List<Lotto> lottos = List.of(
                 new Lotto(List.of(1, 2, 3, 4, 5, 6)),
                 new Lotto(List.of(1, 2, 3, 7, 8, 9)),
                 new Lotto(List.of(10, 20, 30, 40, 44, 45))
         );
-        LottoTicket lottoTicket = new LottoTicket(lottos);
+        lottoTicket = new LottoTicket(lottos);
+    }
 
+    @Test
+    void value_메서드는_로또_번호_리스트들을_반환한다() {
         assertThat(lottoTicket.value()).contains(
                 List.of(1, 2, 3, 4, 5, 6),
                 List.of(1, 2, 3, 7, 8, 9),
                 List.of(10, 20, 30, 40, 44, 45)
         );
     }
+
+    @ParameterizedTest
+    @MethodSource("winningNumbersAndBonusNumber")
+    void check_메서드는_당첨_번호와_보너스_번호를_받아_LottoResult를_반환한다(
+            Lotto winningNumbers,
+            LottoNumber bonusNumber,
+            List<Integer> prizeCount) {
+        LottoResult result = lottoTicket.check(winningNumbers, bonusNumber);
+
+        assertThat(result.getCount(_1ST_PRIZE)).isEqualTo(prizeCount.get(0));
+        assertThat(result.getCount(_2ND_PRIZE)).isEqualTo(prizeCount.get(1));
+        assertThat(result.getCount(_3RD_PRIZE)).isEqualTo(prizeCount.get(2));
+        assertThat(result.getCount(_4TH_PRIZE)).isEqualTo(prizeCount.get(3));
+        assertThat(result.getCount(_5TH_PRIZE)).isEqualTo(prizeCount.get(4));
+    }
+
+    static List<Arguments> winningNumbersAndBonusNumber() {
+        return Arrays.asList(
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), LottoNumber.valueOf(7), List.of(1, 0, 0, 0, 1)),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 43)), LottoNumber.valueOf(6), List.of(0, 1, 0, 0, 1)),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 8)), LottoNumber.valueOf(7), List.of(0, 0, 1, 1, 0)),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 40, 41)), LottoNumber.valueOf(7), List.of(0, 0, 0, 1, 1)),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 40, 41, 42)), LottoNumber.valueOf(7), List.of(0, 0, 0, 0, 2)),
+                Arguments.of(new Lotto(List.of(37, 38, 39, 40, 41, 42)), LottoNumber.valueOf(7), List.of(0, 0, 0, 0, 0))
+        );
+    }
+
 }
