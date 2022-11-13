6a1554beea463476ec26c523cc21defd6bc10d8a
Sunghyun-0 <zkdldk95@naver.com>
2022-11-13 18:18:54 +0900

    로또 결과 출력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -42,10 +42,14 @@ public class Application {
 
     public static Map<String,Integer> getAllResult(List<String> counts){
         Map<String,Integer> allResult = new HashMap<>();
-
-        for(String count : counts) {
-            allResult.put(count, 0);
-        }
+            allResult.put("0", 0);
+            allResult.put("1", 0);
+            allResult.put("2", 0);
+            allResult.put("3", 0);
+            allResult.put("4", 0);
+            allResult.put("5", 0);
+            allResult.put("6", 0);
+            allResult.put("7", 0);
 
         for(String count : counts) {
             allResult.put(count, allResult.get(count) + 1);
@@ -64,8 +68,15 @@ public class Application {
         if (winningNumbers.size() != 6) {
             throw new IllegalArgumentException("잘못된 입력입니다.");
         }
-
         return winningNumbers;
+    }
+
+    public static void printResult(Map<String,Integer> allResult){
+        System.out.println("3개 일치 (5,000원) - " + allResult.get("3") + "개");
+        System.out.println("4개 일치 (50,000원) - " + allResult.get("4") + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + allResult.get("5") + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + allResult.get("7") + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + allResult.get("6") + "개");
     }
 
     public static void main(String[] args) {
@@ -85,13 +96,18 @@ public class Application {
 
         System.out.println("\n" +"보너스 번호를 입력해주세요.");
         Integer bonusNumber = Integer.parseInt(Console.readLine());
+        if(winningNumbers.contains(bonusNumber)){
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
 
         List<String> counts = new ArrayList<>();
         for(Lotto lotto : Lottos){
             counts.add(compareMyLotto(lotto,winningNumbers,bonusNumber));
         }
 
-        System.out.println(getAllResult(counts));
+        System.out.println("당첨 통계\n___\n");
+        //
+        printResult(getAllResult((counts)));
 
         // TODO: 프로그램 구현
     }
