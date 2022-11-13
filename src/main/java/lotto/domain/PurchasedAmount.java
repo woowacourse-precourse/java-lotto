6bb7dec8aa0cc67e2f3d25eb0ffe9b809a2aeab2
Kimhyeyun <cqqwer@gmail.com>
2022-11-13 19:11:30 +0900

    Feat: 구입 금액 최소 값 확인

--- src/main/java/lotto/domain/PurchasedAmount.java
+++ src/main/java/lotto/domain/PurchasedAmount.java
@@ -3,7 +3,9 @@ package lotto.domain;
 import java.util.regex.Pattern;
 
 import static lotto.constant.ConstValue.DIGIT_REGEX;
+import static lotto.constant.ConstValue.PURCHASED_AMOUNT_UNIT;
 import static lotto.constant.Message.INVALID_MONEY_INPUT_FORMAT_MESSAGE;
+import static lotto.constant.Message.LESS_THAN_MINIMUM_MONEY_INPUT_MESSAGE;
 
 public class PurchasedAmount {
     private int price;
@@ -20,12 +22,21 @@ public class PurchasedAmount {
         }
 
         if (!validateMinimum(inputPrice)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LESS_THAN_MINIMUM_MONEY_INPUT_MESSAGE);
         }
 
         if (!validateUnit(inputPrice)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private boolean validateMinimum(String inputPrice) {
+        int price = Integer.parseInt(inputPrice);
+
+        if (price < PURCHASED_AMOUNT_UNIT) {
+            return false;
+        }
+        return true;
     }
 
     private boolean validateDigit(String inputPrice) {
