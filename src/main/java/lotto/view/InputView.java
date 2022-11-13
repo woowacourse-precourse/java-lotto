8cd28ac822b513d8f1a1f0bc70ad8dee7ad85273
CAEL0 <kjongmin26@gmail.com>
2022-11-13 20:15:59 +0900

    refactor(InputView): 입력 메시지 출력 위치 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,22 +2,24 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import static lotto.view.OutputView.*;
+import static lotto.constant.OutputConstants.*;
 
 public class InputView {
 
     public static String getPurchaseAmount() {
-        printPurchaseAmountMessage();
+        System.out.println(INPUT_PURCHASE_AMOUNT);
         return Console.readLine();
     }
 
     public static String getWinningNumber() {
-        printWinningNumber();
+        System.out.println();
+        System.out.println(INPUT_WINNING_NUMBER);
         return Console.readLine();
     }
 
     public static String getBonusNumber() {
-        printBonusNumber();
+        System.out.println();
+        System.out.println(INPUT_BONUS_NUMBER);
         return Console.readLine();
     }
 }
