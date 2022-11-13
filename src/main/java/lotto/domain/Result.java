bc1326d5f049add7d344c3871afc6a1c9145b116
kimtaehoonDev <taehoon_dev@naver.com>
2022-11-14 00:47:30 +0900

    fix(Result): 소수점 둘째자리에서 반올림하도록 수정

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -43,7 +43,8 @@ public class Result {
             totalOutcome += rank.getMoney() * rankCnt;
             totalInput += LOTTO_PRICE * rankCnt;
         }
-        return Double.valueOf(totalOutcome) / totalInput * 100;
+        Double yield =  Double.valueOf(totalOutcome) / totalInput * 100;
+        return Math.round(yield * 10) / 10.0;
     }
 
 
