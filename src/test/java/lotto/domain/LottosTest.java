150fdbaed5b07a91e68bd37940323b12731bf255
saint6839 <saint6839@gmail.com>
2022-11-14 01:18:25 +0900

    test(domain) : 구매한 로또들이 잘 정렬 되어서 반환되는지 테스트

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,9 +1,11 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,5 +23,26 @@ class LottosTest {
         //then
         assertThat(lottos.size())
                   .isEqualTo(lottoCount);
+    }
+
+    @Test
+    void checkLottos_구매한_로또들이_정렬되어서_반환되는지_테스트() {
+        //given
+        List<Lotto> lottos = new ArrayList<>(List.of(new Lotto(List.of(6, 5, 4, 3, 2, 1))));
+        Lottos purchasedLottos = new Lottos();
+        //when
+        for (Lotto lotto : lottos) {
+            List<Integer> sortedNumbers = new ArrayList<>(lotto.getNumbers());
+            Collections.sort(sortedNumbers);
+            purchasedLottos.getLottos().add(new Lotto(sortedNumbers));
+        }
+        //then
+        for (int i = 0; i < 6; i++) {
+            assertThat(purchasedLottos.getLottos()
+                                      .get(0)
+                                      .getNumbers()
+                                      .get(i)).isEqualTo(i+1);
+        }
+
     }
 }
