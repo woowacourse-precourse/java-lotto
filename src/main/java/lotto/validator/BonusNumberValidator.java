28a2247e686a84828f36952d08d2476e8663ab04
baexxbin <soo3886@gmail.com>
2022-11-13 22:11:24 +0900

    feat(validator): 보너스 번호 입력값 유효성 검사 작성

---
+++ src/main/java/lotto/validator/BonusNumberValidator.java
@@ -0,0 +1,37 @@
+package lotto.validator;
+
+import static lotto.validator.ErrorMessage.*;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class BonusNumberValidator {
+	private static final Pattern pattern = Pattern.compile("[0-9]+");
+
+	public Integer runBonusNumberValidator(String bonus, List<Integer> winningNumbers) {
+		int bonusNumber = Integer.parseInt(bonus);
+
+		if (isNotConsistOneData(bonus)) {
+			throw new IllegalArgumentException(ONE_INPUT.getErrorMessage());
+		}
+		if (isOutBoundsNumber(bonusNumber)) {
+			throw new IllegalArgumentException(NOT_LOTTO_NUMBER_RANGE.getErrorMessage());
+		}
+		if (isDuplicateWinningNumbers(bonusNumber, winningNumbers)) {
+			throw new IllegalArgumentException(NOT_DUPLICATED.getErrorMessage());
+		}
+		return bonusNumber;
+	}
+
+	private boolean isNotConsistOneData(String bonus) {
+		return !pattern.matcher(bonus).matches();
+	}
+
+	private boolean isOutBoundsNumber(int bonusNumber) {
+		return bonusNumber < 0 || bonusNumber > 45;
+	}
+
+	private boolean isDuplicateWinningNumbers(int bonusNumber, List<Integer> winningNumbers) {
+		return winningNumbers.stream().anyMatch(a -> a.equals(bonusNumber));
+	}
+}
