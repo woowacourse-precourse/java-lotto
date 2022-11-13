04693546185596553d35b564a0986035d9a35e26
K-Diger <kitten.diger@gmail.com>
2022-11-14 02:20:24 +0900

    refactor : Lotto 클래스 내 존재하던 로또 번호 발급 메서드를 분리하여 추상화 하였다.

---
+++ src/main/java/lotto/application/output/lottoprinter/LottoPrinter.java
@@ -0,0 +1,8 @@
+package lotto.application.output.lottoprinter;
+
+import java.util.List;
+
+public interface LottoPrinter {
+
+    void getGeneratedLottoNumber(List<List<Integer>> generatedLottoNumber);
+}
---
+++ src/main/java/lotto/application/output/lottoprinter/LottoPrinterImpl.java
@@ -0,0 +1,14 @@
+package lotto.application.output.lottoprinter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LottoPrinterImpl implements LottoPrinter {
+
+    @Override
+    public void getGeneratedLottoNumber(List<List<Integer>> generatedLottoNumber) {
+        for (List<Integer> eachLottoPaper : generatedLottoNumber) {
+            System.out.println(Arrays.toString(Arrays.stream(eachLottoPaper.toArray()).sorted().toArray()));
+        }
+    }
+}
