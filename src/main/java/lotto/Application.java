d3c04c98933f30745fc902e0af00c5695a380016
Sangkyun Seo <tkdrbs0802@naver.com>
2022-11-13 18:06:07 +0900

    feat: 구매복권 개수 구하는 기능 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,13 +6,19 @@ import java.util.List;
 
 public class Application {
     static int pay = 0;
+    static int tickets = 0;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         buyLotto();
+        tickets = ticketCal(pay);
         //List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
     public static void buyLotto() {
         System.out.println("구입금액을 입력해 주세요.");
         pay = Integer.parseInt(Console.readLine());
+    }
+
+    public static int ticketCal(int pay){
+        return pay/1000;
     }
 }
