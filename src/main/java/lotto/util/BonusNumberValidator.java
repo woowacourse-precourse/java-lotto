eed9f8d3dcff5d39814a36157c1c77f98c7cd06f
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-14 00:06:59 +0900

    feat: 입력값 패턴 매칭 판단 기능 구현
    입력값이 1-45 사이의 숫자가 아닌 경우 IllegalArgumentException 발생시키는 API 개발

--- src/main/java/lotto/util/BonusNumberValidator.java
+++ src/main/java/lotto/util/BonusNumberValidator.java
@@ -3,8 +3,10 @@ package lotto.util;
 import org.junit.platform.commons.util.StringUtils;
 
 public class BonusNumberValidator implements Validator {
+    private final String BONUS_NUM_PATTERN = "^[1-9]$|^[1-3][0-9]$|^4[0-5]$";
     private final String EMPTY_ERROR = "[ERROR] 보너스 번호를 입력해주세요.";
     private final String NUMERIC_ERROR = "[ERROR] 숫자만 입력해주세요.";
+    private final String NON_MATCHING_ERROR = "[ERROR] 패턴 형식에 맞게 입력해주세요.";
 
     @Override
     public void validate(String input) {
@@ -13,6 +15,9 @@ public class BonusNumberValidator implements Validator {
         }
         if (!input.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException(NUMERIC_ERROR);
+        }
+        if (!input.matches(BONUS_NUM_PATTERN)) {
+            throw new IllegalArgumentException(NON_MATCHING_ERROR);
         }
     }
 }
