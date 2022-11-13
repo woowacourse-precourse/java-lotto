acaf427ba36749243b8f586c92b76fce299dfbac
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-13 14:47:13 +0900

    <test>(LottoTest.java): 겹치는 수 개수와 등수 계산 메서드 테스트 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.Assertions.in;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -38,6 +38,7 @@ class LottoTest {
             domain.validateOverlapping(input);
         });
     }
+
     @DisplayName("로또 번호 중복 검사 테스트: 중복 없을 때")
     @Test
     void 로또_번호_중복_검사2() {
@@ -45,5 +46,52 @@ class LottoTest {
         List<String> input = Arrays.asList("1", "2", "3", "4", "5", "6");
 
         domain.validateOverlapping(input);
+    }
+
+    @DisplayName("getNumberOfOverlapping 기능 테스트")
+    @Test
+    void 겹치는_개수에_따른_반환() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        List<Integer> numbers = Arrays.asList(7, 8, 9, 10, 11, 12, 13);
+
+        assertThat(lotto.getNumberOfOverlapping(numbers) ==  0);
+        numbers = Arrays.asList(1, 8, 9, 10, 11, 12);
+        assertThat(lotto.getNumberOfOverlapping(numbers) ==  1);
+        numbers = Arrays.asList(1, 2, 9, 10, 11, 12);
+        assertThat(lotto.getNumberOfOverlapping(numbers) ==  2);
+        numbers = Arrays.asList(1, 2, 3, 10, 11, 12);
+        assertThat(lotto.getNumberOfOverlapping(numbers) ==  3);
+        numbers = Arrays.asList(1, 2, 3, 4, 11, 12);
+        assertThat(lotto.getNumberOfOverlapping(numbers) ==  4);
+        numbers = Arrays.asList(1, 2, 3, 4, 5, 12);
+        assertThat(lotto.getNumberOfOverlapping(numbers) ==  5);
+        numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertThat(lotto.getNumberOfOverlapping(numbers) ==  6);
+    }
+
+    @DisplayName("getRank 기능 테스트")
+    @Test
+    void 겹치는_개수에_따른_등수_반환() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        List<Integer> numbers = Arrays.asList(7, 8, 9, 10, 11, 12);
+        int bonus = -1;
+
+        assertThat(lotto.getRank(numbers, bonus) ==  8);
+        numbers = Arrays.asList(1, 8, 9, 10, 11, 12);
+        assertThat(lotto.getRank(numbers, bonus) ==  7);
+        numbers = Arrays.asList(1, 2, 9, 10, 11, 12);
+        assertThat(lotto.getRank(numbers, bonus) ==  6);
+        numbers = Arrays.asList(1, 2, 3, 10, 11, 12);
+        assertThat(lotto.getRank(numbers, bonus) ==  5);
+        numbers = Arrays.asList(1, 2, 3, 4, 11, 12);
+        assertThat(lotto.getRank(numbers, bonus) ==  4);
+        numbers = Arrays.asList(1, 2, 3, 4, 5, 12);
+        assertThat(lotto.getRank(numbers, bonus) ==  3);
+        numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertThat(lotto.getRank(numbers, bonus) ==  1);
+
+        bonus = 7;
+        numbers = Arrays.asList(1, 2, 3, 4, 5, 7);
+        assertThat(lotto.getRank(numbers, bonus) ==  2);
     }
 }
