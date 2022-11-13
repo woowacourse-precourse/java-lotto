ffaea01119d6bdb4fcb3a023bfed9d8d4dfe2756
seokjin8678 <rnrn8678@naver.com>
2022-11-13 14:46:26 +0900

    fix: 잘못된 문자열 상수 수정

--- src/main/java/lotto/game/ExceptionHandler.java
+++ src/main/java/lotto/game/ExceptionHandler.java
@@ -3,8 +3,8 @@ package lotto.game;
 public class ExceptionHandler {
     private static final String ERROR = "[ERROR] ";
     public static final String DIVISIBLE_1000_ONLY_MESSAGE = "1000으로 나눠 떨어지는 금액만 입력해주세요.";
-    public static final String NUMBER_ONLY_MESSAGE = "1000으로 나눠 떨어지는 금액만 입력해주세요.";
-    public static final String BONUS_NUMBER_DUPLICATE_MESSAGE = "숫자만 입력해주세요.";
+    public static final String NUMBER_ONLY_MESSAGE = "숫자만 입력해주세요.";
+    public static final String BONUS_NUMBER_DUPLICATE_MESSAGE = "보너스 번호와 중복이 있습니다.";
     public static final String ILLEGAL_LOTTO_NUMBER_MESSAGE = "올바른 로또 번호가 아닙니다.";
 
     public static void throwException(String message) {
