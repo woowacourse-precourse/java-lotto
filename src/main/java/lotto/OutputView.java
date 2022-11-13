389ef1c64afb22e286ee97ad52edfa2a6b375bae
nakalic <nakalic28@gmail.com>
2022-11-13 23:20:30 +0900

    feat(OutputView): 당첨 내역을 출력하는 기능 구현

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.text.DecimalFormat;
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     public static void printUserLotto(List<Lotto> userLotto) {
@@ -13,5 +15,27 @@ public class OutputView {
 
     public static void printAmount(int amount) {
         System.out.printf("%d개를 구매했습니다.\n", amount);
+    }
+
+    public static void printStatistics(int amount, Map<Reward, Integer> result) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        printResult(result);
+    }
+
+    public static void printResult(Map<Reward, Integer> result) {
+        DecimalFormat decimalFormat = new DecimalFormat("###,###");
+        for (Reward r : result.keySet()) {
+
+            if (r == Reward.SECOND) {
+                System.out.println(
+                        r.getCount() + "개 일치, 보너스 볼 일치 (" + decimalFormat.format(r.getReward()) + "원) - " + result.get(
+                                r) + "개");
+                continue;
+            }
+            System.out.println(
+                    r.getCount() + "개 일치 (" + decimalFormat.format(r.getReward()) + "원) - " + result.get(r) + "개");
+        }
     }
 }
