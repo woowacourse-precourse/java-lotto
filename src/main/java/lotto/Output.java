9dffa83335267190a9401c0a41988902b3aa9679
lvalentine6 <ysr1227@naver.com>
2022-11-14 01:34:50 +0900

    feat : 결과 출력 기능
    빌헹한 로또 수량과 로또 번호 목록을 오름차순 정렬하는 기능 구현

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import java.util.List;
+
 public class Output {
-    public static void startPrint() {
+    public static void printPurchaseAmount() {
         System.out.println("구입 금액을 입력해 주세요.");
     }
+
+    public static void printWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void printLottoBundle(List<Lotto> lottos, int purchaseAmount) {
+        System.out.println();
+        System.out.println(purchaseAmount + "개를 구매했습니다.");
+
+        for(Lotto l : lottos) {
+            System.out.println(l.getNumbers());
+        }
+    }
+
 }
