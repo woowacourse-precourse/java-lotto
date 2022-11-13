5e8218951677e01cb67068d9a62c76a98e90646d
jeongwle <dldl4568@naver.com>
2022-11-14 08:37:10 +0900

    chore: 주석 삭제

--- src/main/java/lottoMachine/LottoMachine.java
+++ src/main/java/lottoMachine/LottoMachine.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 import lotto.Lotto;
 import lottoMachine.device.LottoMachineDevices;
-import lottoMachine.enums.Reward;
 
 public class LottoMachine {
 
