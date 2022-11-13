998c2d907875f490b837fd4df402d55af4cb5896
taeyun-work <wmfajr1357@gmail.com>
2022-11-13 15:12:17 +0900

    feat: 구매한 로또 갯수 출력

--- src/main/java/lotto/LottoProgram.java
+++ src/main/java/lotto/LottoProgram.java
@@ -2,8 +2,11 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.regex.Pattern;
+import lotto.domain.Buyer;
 
 public class LottoProgram {
+
+    private static Buyer buyer = new Buyer();
     public static void init() throws IllegalArgumentException {
         System.out.println("구입금액을 입력해 주세요.");
         String purchaseAmout = Console.readLine();
@@ -12,5 +15,11 @@ public class LottoProgram {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요");
         }
         int purchaseAmoutCash = Integer.parseInt(purchaseAmout);
+
+        buyer.setTotalPurchaseAmout(purchaseAmoutCash);
+
+        int numbersOfLotto = purchaseAmoutCash / 1000;
+        buyer.setLottoPurchasedCount(numbersOfLotto);
+        System.out.println(buyer.getLottoPurchasedCount() + "개를 구매했습니다.");
     }
 }
