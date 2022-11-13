f22e7125be300d60b990bfd91bc4c432bf430e5f
seonghoo1217 <seonghoo1217@naver.com>
2022-11-13 21:43:41 +0900

    refactor(BuyLotto) : 에러문이 두번 씩 출력되기에 출력 로그 삭제
    - 에러 발생시 Exception에 포함된 Message가 출력됨으로 별도의 출력 로그 삭제
    
    Refactor(commit_id) : #3be1a42

--- src/main/java/lotto/BuyLotto.java
+++ src/main/java/lotto/BuyLotto.java
@@ -8,7 +8,6 @@ public class BuyLotto {
 
 	public static void prepare_money(String user_input_value) throws IllegalArgumentException {
 		if (!Validation.money_validation(user_input_value)){
-			System.out.println("[ERROR] 1000원단위의 숫자를 입력해야합니다");
 			throw new IllegalArgumentException("[ERROR] 1000원단위의 숫자를 입력해야합니다");
 		}
 		BigInteger money= new BigInteger(user_input_value);
