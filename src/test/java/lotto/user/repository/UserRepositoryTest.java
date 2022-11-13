89a1ad0a0f2b2e922637468c8f7fbfca6ef6c53c
chosunghyun18 <krismon@naver.com>
2022-11-13 19:36:01 +0900

    test(lotterymachine): add the testcode about the validate of user input the target number

--- src/test/java/lotto/user/repository/UserRepositoryTest.java
+++ src/test/java/lotto/user/repository/UserRepositoryTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import lotto.lottery.repository.LottoRepository;
 import lotto.user.domain.User;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserRepositoryTest {
