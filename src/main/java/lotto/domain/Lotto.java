d58fdd9f062af961739d466113da1e8bf1e3e16c
이정우 <acg6138@naver.com>
2022-11-13 22:14:03 +0900

    refactor(Lotto): Lotto 번호 검증 조건 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.domain.Validate.*;
+
 public class Lotto {
     public static final int LOTTO_NUMS_SIZE = 6;
     public static final int MIN_LOTTO_NUM = 1;
@@ -14,10 +16,14 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUMS_SIZE) {
             throw new IllegalArgumentException();
         }
+        if (!isInRangeAll(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 자동으로 생성된 로또 번호의 범위가 올바르지 않습니다.");
+        }
+        if (!isNotDuplicate(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 자동으로 생성된 로또 번호 중 중복되는 숫자가 있습니다.");
+        }
     }
-
-
 }
