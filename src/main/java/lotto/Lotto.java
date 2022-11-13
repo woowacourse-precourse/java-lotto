e72da0a8836cd6cc52bc9e3a05acee45f369ff4a
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-13 18:24:21 +0900

    feat(checkRange): Lotto 클래스의 checkRange 메서드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -27,4 +27,11 @@ public class Lotto {
     private boolean checkDuplicate() {
         return numbers.stream().distinct().count() != 6;
     }
+
+    private boolean checkRange() {
+        return numbers.stream()
+                .filter(number -> number >= 1)
+                .filter(number -> number <= 46)
+                .count() == 6;
+    }
 }
