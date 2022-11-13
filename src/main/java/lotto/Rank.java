f0a66a5eb6490242effbc8d1337d7d14eab41954
JungWoo <rlawjddn103@naver.com>
2022-11-14 01:15:01 +0900

    refactor(Rank): 상수 선언 순서 변경

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -3,12 +3,13 @@ package lotto;
 import java.util.Arrays;
 
 public enum Rank {
-    WIN_SIX(6, 2_000_000_000, "6개 일치"),
-    WIN_FIVE_WITH_BONUS5(5, 30_000_000, "5개 일치, 보너스 볼 일치"),
-    WIN_FIVE(5, 1_500_000, "5개 일치"),
-    WIN_FOUR(4, 50_000, "4개 일치"),
+
+    MISS(0, 0, "3개 미만 일치"),
     WIN_THREE(3, 5_000, "3개 일치"),
-    MISS(0, 0, "3개 미만 일치");
+    WIN_FOUR(4, 50_000, "4개 일치"),
+    WIN_FIVE(5, 1_500_000, "5개 일치"),
+    WIN_FIVE_WITH_BONUS5(5, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    WIN_SIX(6, 2_000_000_000, "6개 일치");
 
     private final int count;
     private final int winningReward;
