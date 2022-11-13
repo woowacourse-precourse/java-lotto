4bf92b84b0242b678e0ce43001481dd50cb14697
은지 <midzy0228@gmail.com>
2022-11-14 00:32:00 +0900

    refactor: 예외 상황 시 에러 메시지 출력 후 종료 기능 메소드 분리

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -4,29 +4,43 @@ import java.util.List;
 
 public class LottoController {
 
-    public void start() {
-        try {
-            InputView inputView = new InputView();
-            LottoAmount lottoAmount = inputAmount(inputView.getPurchaseAmount());
-            List<List<Integer>> lotto = lottoAmount.buyLotto();
-            Lotto winningLotto = inputLotto(inputView.getWinningNumbers());
-            BonusNumber bonusNumber = inputBonusNumber(inputView.getBonusNumber());
+    private final InputView inputView;
 
-            Statistics statistics = new Statistics(winningLotto.getWinningLotto(), bonusNumber.getBonusNumber());
-            statistics.compare(lotto, lottoAmount.getPurchaseAmount());
+    public LottoController(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void run() {
+        try {
+            start();
         } catch (IllegalArgumentException errorMessage) {
             System.out.println(errorMessage);
         }
     }
-    private LottoAmount inputAmount(int number) {
+
+    private void start() {
+
+        LottoAmount lottoAmount = inputAmount();
+        List<List<Integer>> lotto = lottoAmount.buyLotto();
+        Lotto winningLotto = inputLotto();
+        BonusNumber bonusNumber = inputBonusNumber();
+
+        Statistics statistics = new Statistics(winningLotto.getWinningLotto(), bonusNumber.getBonusNumber());
+        statistics.compare(lotto, lottoAmount.getPurchaseAmount());
+    }
+
+    private LottoAmount inputAmount() {
+        int number = inputView.getPurchaseAmount();
         return new LottoAmount(number);
     }
 
-    private Lotto inputLotto(List<Integer> numbers) {
+    private Lotto inputLotto() {
+        List<Integer> numbers = inputView.getWinningNumbers();
         return new Lotto(numbers);
     }
 
-    private BonusNumber inputBonusNumber(int number) {
+    private BonusNumber inputBonusNumber() {
+        int number = inputView.getBonusNumber();
         return new BonusNumber(number);
     }
 }
