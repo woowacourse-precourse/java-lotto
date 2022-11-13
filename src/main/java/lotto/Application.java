eaf8ca760c3f091a6398df4429f2e3865bcf6db2
Sangkyun Seo <tkdrbs0802@naver.com>
2022-11-13 19:08:25 +0900

    feat: 당첨번호 입력 기능 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,11 +10,13 @@ public class Application {
     static int pay = 0;
     static int tickets = 0;
     static List<List<Integer>> allLottoNumbers = new ArrayList<>();
+    static List<String> winningNumber = new ArrayList<>();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         buyLotto();
         tickets = ticketCal(pay);
         getLottoNumber(tickets);
+
 
     }
     public static void buyLotto() {
@@ -23,6 +25,7 @@ public class Application {
     }
 
     public static int ticketCal(int pay){
+
         return pay/1000;
     }
 
@@ -31,5 +34,19 @@ public class Application {
             List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             allLottoNumbers.add(lottoNumbers);
         }
+    }
+
+    public static void inputWinningNumber(){
+        String inputNumber;
+        String[] processNumber;
+        System.out.println("당첨 번호를 입력해 주세요.");
+
+        inputNumber = Console.readLine();
+        processNumber = inputNumber.split(",");
+
+        for(int i=0;i<processNumber.length;i++){
+            winningNumber.add(processNumber[i]);
+        }
+
     }
 }
