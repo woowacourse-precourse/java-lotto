43bad56fe87a0f90c7d32966e664880f80446923
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-11-14 00:58:28 +0900

    Feat(application) : 기능4 - WinningNumberController 호출 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,14 @@
 package lotto;
 
 import lotto.Controller.LottoController;
+import lotto.Controller.WinningNumberController;
 
 public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
+        WinningNumberController winningNumberController = new WinningNumberController();
 
         lottoController.start();
+        winningNumberController.start();
     }
 }
