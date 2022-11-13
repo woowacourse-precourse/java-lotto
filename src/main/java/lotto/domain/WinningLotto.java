a7e337c1b4cd6ba830bbdcbff0ebe3ed23cdb9ca
taekwon-dev <taekwon_dev@naver.com>
2022-11-13 17:33:12 +0900

    feat: (WinningLotto) 보너스 번호가 로또 당첨 번호에 포함되어 있는지 검증

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -5,7 +5,14 @@ public class WinningLotto {
     private final BonusNumber bonusNumber;
 
     public WinningLotto(Lotto winningLotto, BonusNumber bonusNumber) {
+        validateWinningLottoContainsBonusNumber(winningLotto, bonusNumber);
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateWinningLottoContainsBonusNumber(Lotto winningLotto, BonusNumber bonusNumber) {
+        if (winningLotto.containsBonusNumber(bonusNumber.getNumber())) {
+            throw new IllegalArgumentException();
+        }
     }
 }
