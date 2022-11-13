51054efe6b9a540a279d7cafbc59f03744f0c154
BaeDongHyeon <sasd730@gmail.com>
2022-11-13 18:35:20 +0900

    feat: 당첨 번호 검사 기능 추가
    - 당첨 번호가 6개가 아닌 경우 예외 처리
    - 당첨 번호가 중복되는 경우 예외 처리
    - 당첨 번호가 1~45의 정수가 아닌 경우 예외 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,7 +15,18 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개만 입력 할 수 있습니다.");
+        }
+
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복되는 번호는 입력 할 수 없습니다.");
+        }
+
+        int max = Collections.max(uniqueNumbers);
+        int min = Collections.min(uniqueNumbers);
+        if (min < 1 || 45 < max) {
+            throw new IllegalArgumentException("[ERROR] 1~45의 정수만 입력 할 수 있습니다.");
         }
     }
 
