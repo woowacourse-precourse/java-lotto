2b582599c39449904e42ec510d7f141ecc622e52
hyun-seung <ardimento22@naver.com>
2022-11-14 03:34:01 +0900

    [REFACTOR] 당첨 내역과 수익률을 출력한다. refactor by : 클래스 분리 / Controller 분리

--- src/main/java/lotto/Mission.java
+++ src/main/java/lotto/Mission.java
@@ -1,34 +1,23 @@
 package lotto;
 
-import java.util.EnumMap;
 import java.util.List;
 import lotto.controller.LottoController;
 import lotto.controller.MoneyController;
 import lotto.controller.ResultController;
 import lotto.controller.WinningController;
 import lotto.domain.Lotto;
-import lotto.domain.LottoMaker;
 import lotto.domain.Money;
-import lotto.domain.Rank;
-import lotto.domain.ResultRepository;
 import lotto.domain.WinningLotto;
 import lotto.view.Output;
 
 public class Mission {
-
-    Output output;
-    LottoMaker lottoMaker;
-    ResultRepository repository;
-
+    
     MoneyController moneyController;
     WinningController winningController;
     LottoController lottoController;
     ResultController resultController;
 
     Mission() {
-        output = new Output();
-        lottoMaker = new LottoMaker();
-        repository = new ResultRepository();
         moneyController = new MoneyController();
         winningController = new WinningController();
         lottoController = new LottoController();
@@ -41,7 +30,7 @@ public class Mission {
     }
 
     private void play() {
-        Money money = getMoney();
+        Money money = moneyController.getMoney();
 
         List<Lotto> lottos = lottoController.getLottos(money);
 
@@ -49,37 +38,7 @@ public class Mission {
 
         resultController = new ResultController(winningLotto, lottos);
         resultController.sratchLottos();
-
-        int priceMoney = repository.getTotalMoney();
-
-        output.printLottoResultTitle();
-        EnumMap<Rank, Integer> result = repository.getResult();
-        result.forEach(this::callOutputAboutRank);
-
-        double rate = getRate(money.getMoney(), priceMoney);
-
-        output.printTotalRate(rate);
-    }
-
-    private Money getMoney() {
-        output.printGetMoney();
-        return moneyController.getMoney();
-    }
-
-    private double getRate(int inputMoney, int priceMoney) {
-        double result = (double) priceMoney / inputMoney;
-        result *= 100;
-        return result;
-    }
-
-    private void callOutputAboutRank(Rank rank, Integer count) {
-        if (rank == Rank.ZERO) {
-            return;
-        }
-        if (rank == Rank.SECOND) {
-            output.printLottoResultAboutRankWithBonus(rank.getCountOfMatch(), rank.getMoney(), count);
-            return;
-        }
-        output.printLottoResultAboutRank(rank.getCountOfMatch(), rank.getMoney(), count);
+        resultController.printResult();
+        resultController.printRate(money);
     }
 }
