efdd4a42ff7a69f2aac984f4050892db92f284cb
nswon <swnam0729@gmail.com>
2022-11-13 22:48:56 +0900

    feat: 통계 객체 생성

---
+++ src/main/java/lotto/domain/Stats.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import java.util.List;
+
+public enum Stats {
+    THREE(),
+    FOUR(),
+    FIVE(),
+    FIVE_AND_BONUS(),
+    SIX();
+
+    public void compare(List<Integer> winLottoNumbers, int bonusNumber) {
+
+    }
+
+    public void profit() {
+
+    }
+}
