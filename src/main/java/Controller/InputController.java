bc9499724c6d0240319e2426111462ea0ecbc475
namtndus <rnrmf0721@gmail.com>
2022-11-14 06:57:05 +0900

    feat(inputMoney): 금액을 받으면 살수 있는 로또의 갯수를 반환하는 함수 불러서 그 결과를 반환하는 코드 구현

--- src/main/java/Controller/InputController.java
+++ src/main/java/Controller/InputController.java
@@ -1,17 +1,21 @@
 package Controller;
 
 import Dto.Money;
+import Services.CalculatedService;
 import View.InputView;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputController {
 
-    private InputView inputView = new InputView();
+    private final InputView inputView = new InputView();
+    private final CalculatedService calculatedService = new CalculatedService();
 
-    public void inputMoney(){
+    public int inputMoney(){
         inputView.printLotoBuyMoney();
         String inputNumber = readLine();
         Money money = new Money(inputNumber);
+        int lottoSize = calculatedService.lottoSize(money);
+        return lottoSize;
     }
 }
