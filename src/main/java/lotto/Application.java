4554c62b0efd22911be8c0ae1eac87ef13cdc959
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-13 15:21:52 +0900

    feat(main/Input): 사용자에게 로또 당첨 번호 입력받기
    - Input: getPurchaseAmount rename, getWinningNumbers(), getBonusNumber()
    
    해결:
    2. 당첨 통계 계산: - a, b 당첨 번호 및 보너스 번호 입력받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        int amount=Input.purchaseAmount();
+        int amount=Input.getPurchaseAmount();
         List<Lotto> lottos = Operation.buyLotto(amount);
         for(Lotto lotto: lottos) {
             lotto.printLotto();
--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -2,8 +2,26 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Input {
-    public static int purchaseAmount() {
+    public static int getPurchaseAmount() {
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
+
+    public static List<Integer> getWinningNumbers() {
+        String input = Console.readLine();
+        String[] split = input.split(",");
+        List<Integer> numbers =new ArrayList<>();
+        for (String str: split){
+             numbers.add(Integer.parseInt(str));
+        }
+        return numbers;
+    }
+
+    public static int getBonusNumber(){
         String input = Console.readLine();
         return Integer.parseInt(input);
     }
