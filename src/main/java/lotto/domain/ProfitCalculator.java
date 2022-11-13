00d1a9d73c9cc6467bcdddb865d5f36050abef11
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-13 18:43:02 +0900

    refactor: 당첨금액 상수화

--- src/main/java/lotto/domain/ProfitCalculator.java
+++ src/main/java/lotto/domain/ProfitCalculator.java
@@ -3,11 +3,18 @@ package lotto.domain;
 import java.util.List;
 
 public class ProfitCalculator {
+    private static final int FIRST_PRIZE_MONEY = 2_000_000_000;
+    private static final int SECOND_PRIZE_MONEY = 30_000_000;
+    private static final int THIRD_PRIZE_MONEY = 1_500_000;
+    private static final int FOURTH_PRIZE_MONEY = 50_000;
+    private static final int FIFTH_PRIZE_MONEY = 5_000;
+    private static final int ONE_HUNDRED = 100;
 
     public float getRateOfReturn(List<Integer> sumOfResult) {
         int amount = 0;
         float sum = 0;
-        int[] prizeMoney = {5_000, 50_000, 1_500_000, 30_000_000, 2_000_000_000};
+        int[] prizeMoney = {
+                FIFTH_PRIZE_MONEY, FOURTH_PRIZE_MONEY, THIRD_PRIZE_MONEY, SECOND_PRIZE_MONEY, FIRST_PRIZE_MONEY};
         for (int i = 0; i < sumOfResult.size(); i++) {
             if (i == sumOfResult.size() - 1) {
                 amount = sumOfResult.get(i);
@@ -15,7 +22,7 @@ public class ProfitCalculator {
             }
             sum += sumOfResult.get(i) * prizeMoney[i];
         }
-        return sum / (amount * Lotto.PRICE) * 100;
+        return sum / (amount * Lotto.PRICE) * ONE_HUNDRED;
     }
 }
 
