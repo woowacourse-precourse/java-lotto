b37a0c1003dec1e1a59d4aac643bed5cda133201
JerryK026 <kskyung0624@gmail.com>
2022-11-13 15:31:00 +0900

    feat(LottoAgency) : issue() 구현
    issue 테스트를 통과하는 issue() 구현

---
+++ src/main/java/lotto/LottoAgency.java
@@ -0,0 +1,41 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoAgency {
+
+    private NumberGenerator numberGenerator;
+    private int amount;
+
+    public LottoAgency(int money, NumberGenerator numberGenerator) {
+        this.amount = issueAmount(money);
+        this.numberGenerator = numberGenerator;
+    }
+
+    public List<List<Integer>> issue() {
+        List<List<Integer>> lottos = new ArrayList<>();
+
+        for (int lottoIndex = 0; lottoIndex < amount; lottoIndex++) {
+            List<Integer> generatedNumbers = numberGenerator.generate();
+
+            if (isDuplicated(generatedNumbers)) {
+                lottoIndex--;
+                this.numberGenerator = new NumberGeneratorRandom();
+                continue;
+            }
+
+            lottos.add(generatedNumbers);
+        }
+
+        return lottos;
+    }
+
+    private int issueAmount(int money) {
+        return money / 1000;
+    }
+
+    private boolean isDuplicated(List<Integer> numbers) {
+        return numbers.size() != numbers.stream().distinct().count();
+    }
+}
