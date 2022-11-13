035fe9be028a506d5631978a635269415dad3c91
HiiWee <wpdlzhf159@gmail.com>
2022-11-14 00:48:34 +0900

    feat: 1~45범위의 오름차순 정렬된 로또 번호 생성 기능 추가

---
+++ src/main/java/lotto/repository/LottoRepository.java
@@ -0,0 +1,40 @@
+package lotto.repository;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import lotto.domain.Lotto;
+
+public class LottoRepository {
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 45;
+    public static final int LOTTO_COUNT = 6;
+
+    private static final LottoRepository instance = new LottoRepository();
+    private static final List<Lotto> store = new ArrayList<>();
+
+    private LottoRepository() {
+    }
+
+    public static LottoRepository getInstance() {
+        return instance;
+    }
+
+    public void generateLottos(int countToGenerate) {
+        for (int count = 0; count < countToGenerate; count++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, LOTTO_COUNT);
+            Collections.sort(numbers);
+            store.add(new Lotto(numbers));
+        }
+    }
+
+    public List<Lotto> findAll() {
+        return new ArrayList<>(store);
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
+
