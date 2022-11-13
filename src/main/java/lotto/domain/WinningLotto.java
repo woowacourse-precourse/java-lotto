12f7e3c599941bc2176932a179d8521c0bce6ca9
Donguk <95donguk.dev@gmail.com>
2022-11-14 04:58:45 +0900

    feat: 당첨 번호와 보너스 번호 범위가 1에서 45까지가 아닌 경우 예외 처리

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,13 +1,35 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.view.message.ExceptionMessage;
 
 public class WinningLotto {
+    private static final int BEGIN_NUMBER = 1;
+    private static final int END_NUMBER = 45;
+
     private final Lotto lotto;
     private final int bonusNumber;
 
     public WinningLotto(List<Integer> winningNumbers, Integer bonusNumber) {
+        validate(winningNumbers, bonusNumber);
         lotto = new Lotto(winningNumbers);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(List<Integer> winningNumbers, Integer bonusNumber) {
+        winningNumbers.stream().filter(this::isInvalidNumber).forEach(number -> raiseInvalidNumberException());
+
+        if (isInvalidNumber(bonusNumber)) {
+            raiseInvalidNumberException();
+        }
+    }
+
+    private void raiseInvalidNumberException() {
+        System.out.println(ExceptionMessage.INVALID_NUMBER.getMessage());
+        throw new IllegalArgumentException();
+    }
+
+    private boolean isInvalidNumber(Integer number) {
+        return number < BEGIN_NUMBER || number > END_NUMBER;
     }
 }
