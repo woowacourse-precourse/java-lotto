a3c4781d3e81838129f98fc178e8d06d1c8bd796
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-14 00:43:19 +0900

    feat(Parser): 입력받은 문자열로부터 당첨 번호 파싱 구현
    입력받은 문자열로부터 숫자 리스트를 파싱하여 반환하는 기능 구현

---
+++ src/main/java/lotto/Parser.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Parser {
+    public static List<Integer> numbers(String consoleInput) {
+        String[] inputs = consoleInput.split(",");
+        List<Integer> numbers = new ArrayList<>();
+        Arrays.stream(inputs).forEach(input -> numbers.add(number(input)));
+        return numbers;
+    }
+
+    private static int number(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
