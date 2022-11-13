61c558ff4e0117ba995b9b5c250e705d7937f932
sonsoowon <yuwoo1018@gmail.com>
2022-11-13 18:46:10 +0900

    feat(LottoSalesman): 로또 구매 수량만큼의 로또를 발행한다
    - 로또 구매 수량을 구하는 메서드 private 선언 및 해당 테스트 삭제

--- src/main/java/lotto/domain/LottoSalesman.java
+++ src/main/java/lotto/domain/LottoSalesman.java
@@ -1,11 +1,27 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum LottoSalesman {
     INSTANCE;
     public static final int MONEY_UNIT = 1000;
 
-    public static int countPurchasedLotto(int money){
+    private static int countPurchasedLotto(int money) {
         return money / MONEY_UNIT;
+    }
+
+    public static List<Lotto> sales(int money) {
+        List<Lotto> lotteries = new ArrayList<>();
+
+        int quantity = countPurchasedLotto(money);
+        while (quantity > 0) {
+            Lotto lotto = LottoIssuer.issue();
+            lotteries.add(lotto);
+            quantity--;
+        }
+
+        return lotteries;
     }
 
 }
--- src/test/java/lotto/domain/LottoSalesmanTest.java
+++ src/test/java/lotto/domain/LottoSalesmanTest.java
@@ -3,13 +3,15 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoSalesmanTest {
-    @DisplayName("로또 구매 수량을 구한다")
+    @DisplayName("로또 구매 수량만큼의 로또를 발행한다")
     @Test
-    void countPurchasedLotto(){
-        assertThat(LottoSalesman.countPurchasedLotto(14000))
-                .isEqualTo(14);
+    void salesLottoByCorrectQuantity() {
+        List<Lotto> lotteries = LottoSalesman.sales(14000);
+        assertThat(lotteries.size()).isEqualTo(14);
     }
 }
