06890079410d3e07019a19e63015a3bd798ad69d
1eejisoo <jisu991012@gmail.com>
2022-11-14 04:18:57 +0900

    test: 당첨 번호 예외 테스트 추가

---
+++ src/test/java/domain/LottoTest.java
@@ -0,0 +1,42 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoTest {
+    @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("당첨 번호는 6개의 숫자여야 합니다.");
+    }
+
+    @DisplayName("당첨 번호가 1~45 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRange1() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 46, 6)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    @DisplayName("당첨 번호가 1~45 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRange2() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 0, 6)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("당첨 번호 가운데 중복된 숫자가 없어야 합니다.");
+    }
+}
