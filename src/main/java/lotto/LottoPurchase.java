814e1c82476389468a9b6e7aa83fc91f2b9d915a
chnlee <leechan6022@gmail.com>
2022-11-14 03:20:22 +0900

    refactor(LottoPurchase) : 로또 티켓 기능 이동

--- src/main/java/lotto/LottoPurchase.java
+++ src/main/java/lotto/LottoPurchase.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,26 +11,30 @@ public class LottoPurchase {
     private static final int ZERO =  0;
     private static final double DECIMAL_POINT = 1.0;
     private static  final int PERCENT = 100;
+    private static final String MONEY_NUMBER_ERROR = "[ERROR] 구매한 금액은 숫자이여야 합니다.";
     private static final String MONEY_DIVIDED_ERROR = "[ERROR] 구매한 금액은 1000원 단위이여야 합니다.";
     private static final String MONEY_MINIMUM_ERROR = "[ERROR] 구매한 금액은 1000원 이상이여야 합니다.";
-    private static final String MONEY_NUMBER_ERROR = "[ERROR] 구매한 금액은 숫자입니다.";
-    private final String money;
-
+    private int money;
     public LottoPurchase(String money) {
         validatePurchase(money);
-        this.money = money;
+        this.money = parse(money);
     }
-
-    public int calculateTicketNumber() {
-        return parse(money) / MIN_MONEY;
+    public int calculateTicketCount() {
+        return money / MIN_MONEY;
     }
     public double calculateMargin(int sum) {
-        return DECIMAL_POINT * PERCENT * sum / parse(money);
+        return DECIMAL_POINT * PERCENT * sum / money;
     }
     private void validatePurchase(String money) {
+        validateNumber(money);
         validateDivide(money);
         validateMinimum(money);
-        validateNumber(money);
+    }
+
+    private void validateNumber(String money) {
+        if (!Pattern.matches(REGEX,money)) {
+            throw new IllegalArgumentException(MONEY_NUMBER_ERROR.toString());
+        }
     }
 
     private void validateDivide(String money) {
@@ -43,13 +49,6 @@ public class LottoPurchase {
         }
     }
 
-    private void validateNumber(String money) {
-        Pattern pattern = Pattern.compile(REGEX);
-        Matcher matcher = pattern.matcher(money);
-        if (!matcher.find()) {
-            throw new IllegalArgumentException(MONEY_NUMBER_ERROR.toString());
-        }
-    }
     private int parse(String money) {
         return Integer.parseInt(money);
     }
