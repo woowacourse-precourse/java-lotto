9240109f1abdbc9ac4d6ac9fe702998c8c90cbe7
JaehongDev <println@kakao.com>
2022-11-13 19:49:32 +0900

    feature (RankingScore) : 랭킹스코어 클래스는 등수의 스코어목록을 가지는 클래스입니다.

---
+++ src/main/java/lotto/domain/score/RankingScore.java
@@ -0,0 +1,22 @@
+package lotto.domain.score;
+
+import java.util.List;
+
+public enum RankingScore {
+    NOTING(List.of()),
+    FIFTH_SCORE(List.of(new Score(2, 1), new Score(3, 0))),
+    FOURTH_SCORE(List.of(new Score(3, 1), new Score(4, 0))),
+    THIRD_SCORE(List.of(new Score(4, 1), new Score(5, 0))),
+    SECOND_SCORE(List.of(new Score(5, 1))),
+    FIRST_SCORE(List.of(new Score(6, 0)));
+
+    private final List<Score> scores;
+
+    RankingScore(final List<Score> scores) {
+        this.scores = scores;
+    }
+
+    public boolean contains(Score score) {
+        return this.scores.contains(score);
+    }
+}
