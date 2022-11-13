9c1c4a16e2a200048981a8131cc406570f332c3f
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-13 11:59:18 +0900

    test<LottoCompany>: 보너스 번호와 당첨 번호가 중복되면 예외가 발생 테스트 케이스 추가

---
+++ src/test/java/lotto/model/LottoCompanyTest.java
@@ -0,0 +1,20 @@
+package lotto.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoCompanyTest {
+    private LottoCompany makeLottoCompany(String winningLottoInput, String bonusNumberInput) {
+        Lotto winningLotto = new WinningLotto(winningLottoInput).toLotto();
+        LottoNumber bonusNumber = new LottoNumber(bonusNumberInput);
+        return new LottoCompany(winningLotto, bonusNumber);
+    }
+
+    @DisplayName("보너스 번호와 당첨 번호가 중복되면 예외가 발생한다.")
+    @Test
+    void createLottoCompanyByDuplicateNumber() {
+        Assertions.assertThatThrownBy(() -> makeLottoCompany("1,2,3,4,5,6", "6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
