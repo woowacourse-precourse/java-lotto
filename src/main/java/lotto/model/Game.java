0487cf0091bb34b0040008beedd86dfc14022324
Frog-Slayer <Frog-Slayer@github.com>
2022-11-14 01:25:03 +0900

    feat(Game) : 입력된 구매 금액이 장당 가격으로 나누어 떨어질 수 없는 경우 예외 처리

--- src/main/java/lotto/model/Game.java
+++ src/main/java/lotto/model/Game.java
@@ -5,17 +5,24 @@ public class Game {
     public static final Integer maximumLottoNumber = 45;
     public static final Integer numberToDraw = 6;
 
-    private Integer budget;
+    private Integer budget = 0;
 
+    public Game(){
+
+    }
 
     public Integer getBudget() {
         return budget;
     }
 
     public void setBudget(Integer budget){
+        validateBudget(budget);
         this.budget = budget;
     }
 
+    private void validateBudget(Integer budget){
+        if (budget % price != 0) throw new IllegalArgumentException();
+    }
 
 
 
