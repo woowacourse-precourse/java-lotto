110ac1eb81215902efc73c1ea5fd4ba5c7a3b362
poter0501 <poter0501@gmail.com>
2022-11-13 20:48:37 +0900

    feat(Lotto): 로또 번호가 서로 다른 숫자들인지 검증 하는 기능 구현
    구현한 기능을 생성자에서 사용하여 로또 클래스의 인스턴스가 생성될때 서로 다른 수인지 확인하도록 코드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -7,12 +8,21 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkNumbersAreDifferent(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkNumbersAreDifferent(List<Integer> numbers) {
+        int numbersCount = numbers.size();
+        HashSet<Integer> tempNumbers = new HashSet<>(numbers);
+        if (numbersCount != tempNumbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호로 중복된 숫자가 존재 합니다.");
         }
     }
 
