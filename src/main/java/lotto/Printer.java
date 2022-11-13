47cc7202614d0234c8257b4f7d103ca4e81d2db5
Nugur1i <duckhee092@naver.com>
2022-11-13 20:21:00 +0900

    feat(Printer): print lotto list by ascending order

--- src/main/java/lotto/Printer.java
+++ src/main/java/lotto/Printer.java
@@ -1,11 +1,20 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Printer {
 
-    public void printUserLottoNumber(List<Integer> lottoInput){
+    public void printUserLottoNumber(List<List<Integer>> lottoInput){
 
+        int amountLotto = lottoInput.size();
+
+        System.out.println(amountLotto + "개를 구매하셨습니다.");
+
+        for(int i =0; i<amountLotto; i++){
+            Collections.sort(lottoInput.get(i));
+            System.out.println(lottoInput.get(i));
+        }
 
     }
 
