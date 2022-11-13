ec3fa8708f7cd98dbfc3178443928c8741c25c39
jihong <clara5091@naver.com>
2022-11-14 08:51:42 +0900

    feat(Prize): 상금 관련 enum 클래스 구현
    맞춘 개수에 따른 상금을 알아오는 기능

---
+++ src/main/java/lotto/Prize.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import java.util.Arrays;
+
+public enum Prize {
+    NONE(2, 0),
+    FIFTH(3, 5000),
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    FIRST(6, 2_000_000_000);
+
+    private final int number;
+    private final int prize;
+
+    Prize(int number, int prize){
+        this.number=number;
+        this.prize=prize;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public static Prize findPrize(int number, boolean bonus){
+        Prize result = Arrays.stream(Prize.values())
+                .filter(x->x.number==number)
+                .findFirst()
+                .orElse(NONE);
+        if(result==THIRD&&bonus){
+            result=SECOND;
+        }
+        return result;
+    }
+}
