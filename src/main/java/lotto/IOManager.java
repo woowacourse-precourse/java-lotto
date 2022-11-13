7e96bf623e49a147929daae1fa5ab885aba33709
jagajang <kosb7544@gmail.com>
2022-11-13 20:02:14 +0900

    fix: refactor code to throw Exception

--- src/main/java/lotto/IOManager.java
+++ src/main/java/lotto/IOManager.java
@@ -5,17 +5,12 @@ import lotto.config.LottoConfig;
 
 public class IOManager {
     public static int buyLotto() throws IllegalArgumentException {
-        int price;
-
         try {
-            price = Integer.parseInt(Console.readLine());
+            int price = Integer.parseInt(Console.readLine());
+
+            return price / LottoConfig.LOTTO_PRICE;
         } catch(Exception e) {
             throw new IllegalArgumentException();
         }
-
-        if(price % LottoConfig.LOTTO_PRICE != 0)
-            throw new IllegalArgumentException();
-
-        return price / LottoConfig.LOTTO_PRICE;
     }
 }
