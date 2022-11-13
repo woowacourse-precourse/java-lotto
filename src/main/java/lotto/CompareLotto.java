a09a905554b30710f61eb402690670357acad8b3
alpacaHyun <flaehdan@gmail.com>
2022-11-13 23:19:05 +0900

    type : feat ✨ 로또 번호 비교하기
    body : 로또 번호 비교하기
    
    footer :

---
+++ src/main/java/lotto/CompareLotto.java
@@ -0,0 +1,59 @@
+package lotto;
+
+public class CompareLotto {
+
+    public static int first, second, third, fourth, fifth;
+
+    //내 로또와 비교
+    public static void compareLotto() {
+        for (int i = 0; i < LottoInput.numberOfLotto; i++) {
+            if (Application.winningNumbers.contains(Lotto.bonusNum)) {
+                int checkLotto = Application.winningNumbers.size();
+                if (checkLotto == 3) {
+                    fifth++;
+                }
+                if (checkLotto == 4) {
+                    fourth++;
+                }
+                if (checkLotto == 5) {
+                    second++;
+                }
+                if (checkLotto == 6) {
+                    first++;
+                }
+            }
+            if (!Application.winningNumbers.contains(Lotto.bonusNum)) {
+                int checkLotto = Application.winningNumbers.size();
+                if (checkLotto == 3) {
+                    fifth++;
+                }
+                if (checkLotto == 4) {
+                    fourth++;
+                }
+                if (checkLotto == 5) {
+                    third++;
+                }
+                if (checkLotto == 6) {
+                    first++;
+                }
+            }
+        }
+    }
+
+    public static void printCompareLotto() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + fifth);
+        System.out.println("4개 일치 (50,000원) - " + fourth);
+        System.out.println("5개 일치 (1,500,000원) - " + third);
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) -" + second);
+        System.out.println("6개 일치 (2,000,000,000) - " + first);
+    }
+
+    public static void printRateOfReturn(){
+        double getReturn = (fifth * 5000) + (fourth * 50000) + (third * 1500000) + (second * 30000000) + (first * 2000000000);
+        double rateOfReturn = (getReturn / userInputMoney)*100;
+        System.out.println("총 수익률은 " + rateOfReturn + "%입니다.");
+
+    }
+}
