0b81130f2d5634d3800e4a0bf58793a9f3c558aa
twoosky <lhjae3@naver.com>
2022-11-14 00:59:22 +0900

    test(domain): 당첨 번호를 리스트로 변환해주는 기능 테스트 작성

--- src/test/java/lotto/LottoWinningNumbersTest.java
+++ src/test/java/lotto/LottoWinningNumbersTest.java
@@ -1,10 +1,18 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import lotto.domain.LottoWinningNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static lotto.utils.ErrorMessages.WINNING_NUMBERS_SEPARATOR;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoWinningNumbersTest {
@@ -14,5 +22,22 @@ public class LottoWinningNumbersTest {
         assertThatThrownBy(() -> new LottoWinningNumbers("123456"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(WINNING_NUMBERS_SEPARATOR);
+    }
+
+    @DisplayName("로또 당첨 번호를 리스트로 변환한다.")
+    @ParameterizedTest
+    @MethodSource("generateData")
+    void WinningNumbersToList(String input, List<Integer> expectedNumbers) {
+        LottoWinningNumbers winningNumbers = new LottoWinningNumbers(input);
+        Lotto numbers = winningNumbers.getWinningNumbers();
+        assertThat(numbers.getNumbers())
+                .isEqualTo(expectedNumbers);
+    }
+
+    private static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of("1,2,3,4,5,6", List.of(1, 2, 3, 4, 5, 6)),
+                Arguments.of("4,45,1,29,23,18", List.of(4, 45, 1, 29, 23, 18))
+        );
     }
 }
