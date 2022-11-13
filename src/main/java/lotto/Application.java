d7a3af69ac79f63395b645e51676c11281105e3b
KSH <tkdgns543@gmail.com>
2022-11-14 06:04:05 +0900

    feat(lotto): 메인 메서드에 전체 로직 종합

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,38 @@
 package lotto;
 
+import lotto.domain.Buyer;
+import lotto.domain.Lotto;
+import lotto.domain.WinningNumbers;
+import lotto.ui.NumbersReceiver;
+import lotto.ui.OutputPrinter;
+
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        NumbersReceiver numbersReceiver = new NumbersReceiver();
+        Buyer buyer = new Buyer();
+        OutputPrinter outputPrinter = new OutputPrinter();
+
+        long startMoney = numbersReceiver.insertMoney();
+        long lottosBundleSize = buyer.countLottos(startMoney);
+
+        List<List<Integer>> purchasedLottos = buyer.receiveLottos(lottosBundleSize);
+        outputPrinter.printPurchasedNumbers(lottosBundleSize,purchasedLottos);
+
+        List<Integer> basicWinningNumbers = numbersReceiver.setBasicNumbers();
+        int bonusNumber = numbersReceiver.setBonusNumber();
+
+        Lotto lotto = new Lotto(basicWinningNumbers);
+        List<Integer> winningCounter = lotto.countMatches(purchasedLottos);
+        List<Boolean> winningBonus = lotto.checkBonusMatch(bonusNumber,purchasedLottos);
+
+        Map<WinningNumbers, Long> winningCaseStatistics = lotto.countWinningCase(winningCounter, winningBonus);
+        double rawRewardRate = lotto.calculateRewardRate(winningCounter, winningBonus, startMoney);
+
+        outputPrinter.printWinningStatistics(winningCaseStatistics);
+        outputPrinter.printRewardRate(rawRewardRate);
+
     }
 }
