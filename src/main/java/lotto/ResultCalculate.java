92c4fb498d02b46adc53305ed4c20023356493e9
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-13 13:54:15 +0900

    feat(ResultCalculate) : 두개의 번호들을 비교해서같은 몇개가 같은지 찾는 기능 구현.

--- src/main/java/lotto/ResultCalculate.java
+++ src/main/java/lotto/ResultCalculate.java
@@ -1,8 +1,20 @@
 package lotto;
 
+import java.util.List;
 public class ResultCalculate {
-    public static void resultCalculate(){
-
+    public static void resultCalculate(List<Lotto> purchaseLottos, List<Integer> drawLotto ){
+        for(Lotto lotto :purchaseLottos){
+            compareTwoNumbers(lotto, drawLotto);
+        }
+    }
+    public static int compareTwoNumbers(Lotto lotto, List<Integer> drawLotto){
+        int sameNumberCount =0;
+        for( int nowNumber:drawLotto){
+            if(lotto.getNumbers().contains(nowNumber)){
+                sameNumberCount++;
+            }
+        }
+        return sameNumberCount;
     }
     public static void profitCalculate(){
 
