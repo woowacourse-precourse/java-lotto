2a5f2eb0a2cf262d8c915c59d61a6f788a6603c8
nn98 <jkllhgb@gmail.com>
2022-11-13 19:28:42 +0900

    Feat: 보너스 번호 정의, 생성자 코드 추가/getter 추가

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -3,7 +3,20 @@ package lotto;
 import java.util.List;
 
 public class WinningLotto extends Lotto{
+    private int bonusNumber;
     public WinningLotto(List<Integer> numbers) {
         super(numbers);
+    }
+    public WinningLotto(List<Integer> numbers,int bonusNumber) {
+        super(numbers);
+        this.bonusNumber=bonusNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "WinningLotto{" +
+                "numbers=" + this.getNumbers() +
+                "bonusNumber=" + bonusNumber +
+                '}';
     }
 }
