12d577d17ccf18794456a41b3b086b2b7e89906a
rvlwldev <rvlwlasd77@gmail.com>
2022-11-13 17:05:44 +0900

    feat: Validation클래스 구현

--- src/main/java/lotto/Utils/Validation.java
+++ src/main/java/lotto/Utils/Validation.java
@@ -1,17 +1,18 @@
 package lotto.Utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static lotto.Enum.ErrorMessage.OUT_OF_LENGTH;
+import static lotto.Enum.ErrorMessage.*;
 import static lotto.Enum.LottoProperty.*;
 
 public class Validation {
 
     private List<Integer> numbers;
-    private List<Integer> winningNumbers;
 
     public void validateMoney(String inputMoney) {
-
+        checkNumberValues(inputMoney);
+        checkPayMoney(inputMoney);
     }
 
     public void validateLottoNumberList(List<Integer> numbers) {
@@ -22,20 +23,52 @@ public class Validation {
         checkDuplicationOfLottoNumbers();
     }
 
-    public void validateWinningNumbers(String inputWinningNumbers) {
+    public void validateWinningNumberArray(String[] winningNumbersArray) {
+        List<Integer> winningNumberList = new ArrayList<>();
+
+        for (String winningNumber : winningNumbersArray) {
+            winningNumberList.add(checkNumberValues(winningNumber));
+        }
+        validateLottoNumberList(winningNumberList);
     }
 
     public void validateBonusNumber(String inputBonusNumber) {
+        int parsedNumber = checkNumberValues(inputBonusNumber);
+        checkRangeOfLottoNumbers(parsedNumber);
+        checkDuplicationOfLottoNumbers(parsedNumber);
     }
 
+    private int checkNumberValues(String stringNumber) {
+        int parsedNumber;
+
+        try {
+            parsedNumber = Integer.parseInt(stringNumber);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(NOT_INPUT_INTEGER_MONEY_VALUE.toMessage());
+        }
+
+        return parsedNumber;
+    }
+
+    private void checkPayMoney(String inputMoney) {
+        int intMoney = Integer.parseInt(inputMoney);
+        if (intMoney % ONE_LOTTO_COST.getValue() != 0) {
+            throw new IllegalArgumentException(NOT_PROPER_PAY_MONEY.toMessage());
+        }
+    }
 
     private void checkLengthOfLottoNumbers() {
         if (numbers.size() != LOTTO_NUMBER_LENGTH.getValue()) {
-            throw new IllegalArgumentException(OUT_OF_LENGTH.toMessage());
+            throw new IllegalArgumentException(NOT_PROPER_WINNING_NUMBER_LENGTH.toMessage());
         }
     }
 
     private void checkRangeOfLottoNumbers() {
+        for (int number : numbers) {
+            if (MIN_LOTTO_NUMBER.getValue() > number || number > MAX_LOTTO_NUMBER.getValue()) {
+                throw new IllegalArgumentException(OUT_OF_LENGTH.toMessage());
+            }
+        }
     }
 
     private void checkRangeOfLottoNumbers(int lottoNumber) {
@@ -45,7 +78,19 @@ public class Validation {
     }
 
     private void checkDuplicationOfLottoNumbers() {
+        List<Integer> checkDuplicatedNumberList = new ArrayList<>();
+
+        for (int number : numbers) {
+            if (checkDuplicatedNumberList.contains(number)) {
+                throw new IllegalArgumentException(DUPLICATED_NUMBERS.toMessage());
+            }
+            checkDuplicatedNumberList.add(number);
+        }
     }
 
-
+    private void checkDuplicationOfLottoNumbers(int bonusNumber) {
+        if(numbers.contains(bonusNumber)){
+            throw new IllegalArgumentException(DUPLICATED_BONUS_NUMBERS.toMessage());
+        }
+    }
 }
