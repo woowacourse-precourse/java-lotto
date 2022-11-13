39f1225aac247817191666720cdffaab8a64cd27
UngHeung <t01047377477@gmail.com>
2022-11-14 05:33:01 +0900

    feat : (Lotto) 보너스 클래스 예외 오류 메시지 출력 기능 추가

--- src/main/java/lotto/Bonus.java
+++ src/main/java/lotto/Bonus.java
@@ -2,6 +2,7 @@ package lotto;
 
 public class Bonus {
 	private final int BONUS;
+	private String errorMessage = "[ERROR] ";
 	
 	public Bonus(int bonus) {
 		validate(bonus);
@@ -10,7 +11,17 @@ public class Bonus {
 	
 	private void validate(int bonus) {
 		if (bonus < 1 || 45 < bonus) {
+			errorMessage += "1 이상 45 이하의 숫자를 선택해 주세요.";
+			throwException();
+		}
+	}
+	
+	private void throwException() {
+		try {
 			throw new IllegalArgumentException();
+		} catch(IllegalArgumentException e) {
+			System.out.println(errorMessage);
+			return;
 		}
 	}
 	
