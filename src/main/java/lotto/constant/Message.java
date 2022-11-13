d17f940b6a7c34d27c2869a7dfa406ed1e625168
WonSeok-dd <cluejws@gmail.com>
2022-11-14 00:00:43 +0900

    feat(Message): 사용자 구입금액 입력 문구 추가
    기능 구현
    - 1-1: `구입금액을 입력해 주세요.` 를 출력한다.

---
+++ src/main/java/lotto/constant/Message.java
@@ -0,0 +1,10 @@
+package lotto.constant;
+
+public class Message {
+
+    private static final String INPUT_USER_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static void printInputUserMoney(){
+        System.out.println(INPUT_USER_MONEY_MESSAGE);
+    }
+}
