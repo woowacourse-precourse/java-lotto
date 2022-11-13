0a6591b67cdb6b48807408273b65a648aea41b15
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-13 18:40:55 +0900

    feat : Lotto 구현 주요 기능 클래스에서 번호 비교 확인 기능
    입력한 당첨 번호 리스트와 출력된 리스트를 비교하여 중복된 수를 출력한 메서드

--- src/main/java/lotto/CoreProcess.java
+++ src/main/java/lotto/CoreProcess.java
@@ -2,15 +2,25 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class CoreProcess {
+
+    ProcessData bring = new ProcessData();
 
     public List<Integer> setLottoNumber() {
         List<Integer> LottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return LottoNumber;
     }
 
-
+    public int compareNumber(List<Integer> Lotto, List<Integer> pickedNumber) {
+        List<Integer> overlapList = Lotto.stream()
+                .filter(old -> pickedNumber.stream()
+                        .anyMatch(Predicate.isEqual(old)))
+                .collect(Collectors.toList());
+        return overlapList.size();
+    }
 
 }
 
