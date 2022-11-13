7eb1bfac3b817b1e5f32bfbf51f0b400b0052079
chordpli <chordplaylist@gmail.com>
2022-11-13 23:42:08 +0900

    test: 금액이 1,000원 단위로 떨어지지 않을 때 예외 확인

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -7,10 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class UserTest {
+    Application app = new Application();
     User user = new User();
     Lotto lotto = new Lotto(user.getLottoNumbers());
 
@@ -20,7 +22,7 @@ class UserTest {
         List<User> userLotto = new ArrayList<>();
 
         for (int i = 0; i < 10000; i++) {
-            userLotto.add(0, new User());
+            userLotto.add(i, new User());
         }
 
         for (User u : userLotto) {
@@ -30,5 +32,17 @@ class UserTest {
             assertThatCode(() -> lotto.validateOverlap(u.userNumber))
                     .doesNotThrowAnyException();
         }
+    }
+
+    @DisplayName("금액이 1,000원 단위로 떨어지지 않을 때 예외 확인")
+    @Test
+    void moneyException() {
+        User user = new User();
+
+        assertThatCode(() -> app.lottoCount(13000))
+                .doesNotThrowAnyException();
+
+        assertThatThrownBy(() -> app.lottoCount(13200))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
