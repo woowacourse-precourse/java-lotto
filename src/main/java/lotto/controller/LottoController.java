30abd2295d7bbdfc27cf80de052e0fe20cbbe2f7
JJongmen <pjy90123@gmail.com>
2022-11-13 23:50:08 +0900

    refactor: 로또를 발행하는 기능의 반환 값 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,10 +1,14 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoIssueMachine;
 import lotto.domain.Money;
 import lotto.view.InputView;
 
 public class LottoController {
     public void run() {
         Money money = InputView.inputMoney();
+        List<Lotto> lottos = LottoIssueMachine.issue(money.calculateQuantity());
     }
 }
--- src/main/java/lotto/domain/LottoIssueMachine.java
+++ src/main/java/lotto/domain/LottoIssueMachine.java
@@ -1,15 +1,24 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class LottoPos {
+public class LottoIssueMachine {
     private static final int SIZE = 6;
     private static final int RANGE_BEGIN = 1;
     private static final int RANGE_END = 45;
 
-    public Lotto issue() {
+    public static List<Lotto> issue(int quantity) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int count = 1; count <= quantity; count++) {
+            lottos.add(makeLotto());
+        }
+        return lottos;
+    }
+
+    private static Lotto makeLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(RANGE_BEGIN, RANGE_END, SIZE);
         return new Lotto(numbers.stream()
                 .sorted()
