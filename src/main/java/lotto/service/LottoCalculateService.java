6cb6607d92e0c4f122250f312510cb59329bde3e
sosow0212 <sosow0212@naver.com>
2022-11-13 14:27:00 +0900

    refactor : 로또 몇 개 맞혔는지 반환해주는 메소드로 if문 리팩토링

--- src/main/java/lotto/service/LottoCalculateService.java
+++ src/main/java/lotto/service/LottoCalculateService.java
@@ -15,18 +15,13 @@ public class LottoCalculateService {
     private static final int FIVE_NUMBER_MATCHES = 5;
     private static final int SIX_NUMBER_MATCHES = 6;
     private static final int BONUS_ENUM_LABEL = 7;
-    private static final OutputView outputView = new OutputView();
+    private final OutputView outputView = new OutputView();
     private Map<Winning, Integer> countOfWinning = new HashMap<>();
 
     public void calculateWinning(User user, Lotto lotto) {
         countLottoWinningCount(user, lotto);
         outputView.responseWinningHistory(countOfWinning);
         outputView.responseYieldOfLotto(getYieldOfLotto(user, user.getWinningPrice()));
-    }
-
-    public double getYieldOfLotto(User user, int totalPrice) {
-        double yield = ((double) totalPrice / user.getBuyingPrice() * 100.0);
-        return yield;
     }
 
     public void countLottoWinningCount(User user, Lotto lotto) {
@@ -36,27 +31,28 @@ public class LottoCalculateService {
                 .forEach(userLotto -> countLottoWinning(user, userLotto.getNumbers(), lotto));
     }
 
+    public double getYieldOfLotto(User user, int totalPrice) {
+        double yield = ((double) totalPrice / user.getBuyingPrice() * 100.0);
+        return yield;
+    }
+
     public void countLottoWinning(User user, List<Integer> numbers, Lotto lotto) {
         int countContainsOfLotto = countUserNumbersContainLotto(numbers, lotto.getNumbers());
-
-        if (countContainsOfLotto == THREE_NUMBER_MATCHES || countContainsOfLotto == FOUR_NUMBER_MATCHES || countContainsOfLotto == SIX_NUMBER_MATCHES) {
-            inputWinningLotto(Winning.FIND.valueOf(countContainsOfLotto));
+        if (isNumberMatchedNormal(countContainsOfLotto)) {
+            inputCountOfWinning(Winning.FIND.valueOf(countContainsOfLotto));
             user.addWinningPrice(Winning.FIND.valueOf(countContainsOfLotto).getPrice());
             return;
         }
-
-        if (countContainsOfLotto == FIVE_NUMBER_MATCHES && numbers.contains(lotto.getBonusNumber())) {
-            inputWinningLotto(Winning.FIND.valueOf(BONUS_ENUM_LABEL));
+        if (isNumberMatchedFiveWithBonus(countContainsOfLotto, numbers, lotto)) {
+            inputCountOfWinning(Winning.FIND.valueOf(BONUS_ENUM_LABEL));
             user.addWinningPrice(Winning.FIND.valueOf(BONUS_ENUM_LABEL).getPrice());
             return;
         }
-
-        if (countContainsOfLotto == FIVE_NUMBER_MATCHES) {
-            inputWinningLotto(Winning.FIND.valueOf(countContainsOfLotto));
+        if (isNumberMatchedFive(countContainsOfLotto, numbers, lotto)) {
+            inputCountOfWinning(Winning.FIND.valueOf(countContainsOfLotto));
             user.addWinningPrice(Winning.FIND.valueOf(countContainsOfLotto).getPrice());
             return;
         }
-
     }
 
     public int countUserNumbersContainLotto(List<Integer> userNumbers, List<Integer> lottoNumbers) {
@@ -69,7 +65,29 @@ public class LottoCalculateService {
         return count;
     }
 
-    public void inputWinningLotto(Winning winning) {
+    public void inputCountOfWinning(Winning winning) {
         countOfWinning.put(winning, countOfWinning.getOrDefault(winning, 0) + 1);
     }
+
+    public boolean isNumberMatchedNormal(int countContainsOfLotto) {
+        if (countContainsOfLotto == THREE_NUMBER_MATCHES || countContainsOfLotto == FOUR_NUMBER_MATCHES || countContainsOfLotto == SIX_NUMBER_MATCHES) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isNumberMatchedFiveWithBonus(int countContainsOfLotto, List<Integer> numbers, Lotto lotto) {
+        if (countContainsOfLotto == FIVE_NUMBER_MATCHES && numbers.contains(lotto.getBonusNumber())) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isNumberMatchedFive(int countContainsOfLotto, List<Integer> numbers, Lotto lotto) {
+        if (countContainsOfLotto == FIVE_NUMBER_MATCHES && !numbers.contains(lotto.getBonusNumber())) {
+            return true;
+        }
+        return false;
+    }
+
 }
