1f9d959420f474507b20ee13d150f9eb575078ec
recicoco068 <gkapf838@gmail.com>
2022-11-13 23:09:45 +0900

    test(UtilsTest.java) : 1 ~ 45 중 6개 번호를 뽑는 기능 테스트

--- src/test/java/lotto/UtilsTest.java
+++ src/test/java/lotto/UtilsTest.java
@@ -3,7 +3,9 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +15,13 @@ class UtilsTest {
     void 로또_번호_유효_범위_넘으면_false() {
         List<Integer> numbers = List.of(1, 2, 3, 48, 5, 6);
         assertThat(Utils.validateLottoNumbers(numbers)).isFalse();
+    }
+
+    @DisplayName("랜덤 숫자 6개 확인")
+    @Test
+    void 랜덤_숫자_6개() {
+        List<Integer> numbers = Utils.pickUnique6Numbers();
+        Set<Integer> check = new HashSet<>(numbers);
+        assertThat(check.size()).isEqualTo(6);
     }
 }
