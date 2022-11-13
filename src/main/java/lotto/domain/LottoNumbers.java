80c1996cc798bc89d27963c7f4e9b395e2c1945d
KimJaeSung <behappy0916@naver.com>
2022-11-13 18:38:41 +0900

    refactor: 랜덤으로 생성하는 로또 예외처리 삭제

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -4,18 +4,15 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import lotto.exception.Exceptions;
+
 
 public class LottoNumbers {
     private List<Integer> lottoNumbers;
 
-    public List<Integer> createRandomNumbers(String numbers) {
-        lottoNumbers = new ArrayList<>();
-        if (!Exceptions.validLottoNumbers(numbers)) {
-            throw new IllegalArgumentException("입력 오류");
-        }
+    public List<Integer> createRandomNumbers() {
         lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return lottoNumbers;
     }
+
 
 }
