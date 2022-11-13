84e345382ce8b3f73a3efe73ac5cb53f8c145447
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-13 16:09:55 +0900

    feat(main/Operation): 사용자의 전체 로또를 받아 당첨 번호와 비교한다.
    - calculateMatches(): winning 숫자와 bonus 숫자를 받아서 lotto numbers와 비교하여 결과를 map에 저장해 출력한다.
    
    해결: 2. 당첨 통계 계산- c-1), c-2), 사용자의 로또 전체에 대해 비교한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,18 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        int amount=Input.getPurchaseAmount();
+        int amount = Input.getPurchaseAmount();
         List<Lotto> lottos = Operation.buyLotto(amount);
-        for(Lotto lotto: lottos) {
+        for (Lotto lotto : lottos) {
             lotto.printLotto();
         }
+        List<Integer> winning = Input.getWinningNumbers();
+        int bonus = Input.getBonusNumber();
+        HashMap<Integer, Integer> result = Operation.calculateMatches(lottos, winning, bonus);
+        System.out.println(result);
     }
 }
--- src/main/java/lotto/Operation.java
+++ src/main/java/lotto/Operation.java
@@ -3,13 +3,20 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Operation {
+    static final int MATCH3 = 3;
+    static final int MATCH4 = 4;
+    static final int MATCH5 = 5;
+    static final int MATCH6 = 6;
+    static final int MATCH5_BONUS = 0;
+
     static List<Lotto> buyLotto(int amount) {
         List<Lotto> lottos = new ArrayList<>();
-        for (int i=0; i<(amount/1000); i++){
-            Lotto lotto= new Lotto(generateNumbers());
+        for (int i = 0; i < (amount / 1000); i++) {
+            Lotto lotto = new Lotto(generateNumbers());
             lottos.add(lotto);
         }
         return lottos;
@@ -26,4 +33,39 @@ public class Operation {
         return numbers;
     }
 
+    static HashMap<Integer, Integer> calculateMatches(List<Lotto> lottos, List<Integer> winning, int bonus) {
+        HashMap<Integer, Integer> result = new HashMap<>();
+        result.put(MATCH3, 0);
+        result.put(MATCH4, 0);
+        result.put(MATCH5, 0);
+        result.put(MATCH6, 0);
+        result.put(MATCH5_BONUS, 0);
+
+        for (Lotto lotto : lottos) {
+            int match = lotto.matchWinning(winning);
+            if (lotto.matchBonus(bonus)) {
+                if (match == 5) {
+                    int value = result.get(MATCH5_BONUS) + 1;
+                    result.put(MATCH5_BONUS, value);
+                }
+            }
+            if (match == MATCH3) {
+                int value = result.get(MATCH3) + 1;
+                result.put(MATCH3, value);
+            }
+            if (match == MATCH4) {
+                int value = result.get(MATCH4) + 1;
+                result.put(MATCH4, value);
+            }
+            if (match == MATCH5) {
+                int value = result.get(MATCH5) + 1;
+                result.put(MATCH5, value);
+            }
+            if (match == MATCH6) {
+                int value = result.get(MATCH6) + 1;
+                result.put(MATCH6, value);
+            }
+        }
+        return result;
+    }
 }
