3abe64731e5d9c45d2f02bb1d4ece862306b71a2
lvalentine6 <ysr1227@naver.com>
2022-11-14 06:02:34 +0900

    feat : 예외 처리 기능
    로또 번호에 중복된 숫자가 있으면 예외 처리 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,17 +1,30 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateOverlap(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateOverlap(List<Integer> numbers) {
+        Set<Integer> overlap = new HashSet<>();
+        for(int i : numbers) {
+            overlap.add(i);
+        }
+        if (numbers.size() != overlap.size()) {
             throw new IllegalArgumentException();
         }
     }
