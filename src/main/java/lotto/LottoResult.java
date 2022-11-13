3af9aa3b848a1f4a744b9defe8c5701ffea02de2
Minjune <minjune8506@gmail.com>
2022-11-13 19:12:03 +0900

    feat(Lotto): 로또 계산 결과 저장 기능 구현
    - LottoResult class

---
+++ src/main/java/lotto/LottoResult.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LottoResult {
+
+    private final Map<String, Integer> result;
+
+    public LottoResult() {
+        result = new HashMap<>();
+    }
+
+    public void add(Rank rank) {
+        if (!result.containsKey(rank.name())) {
+            result.put(rank.name(), 1);
+            return;
+        }
+        int count = result.get(rank.name());
+        result.put(rank.name(), count + 1);
+    }
+
+    public int get(String rankName) {
+        Integer count = result.get(rankName);
+
+        if (count == null) {
+            return 0;
+        }
+        return count;
+    }
+}
