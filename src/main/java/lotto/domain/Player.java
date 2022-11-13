b520727ccbecffd0fc52e534445f71700cd0887e
CAEL0 <kjongmin26@gmail.com>
2022-11-13 21:46:27 +0900

    feat(Player): 구매 금액이 0으로 시작하는지 검증

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -12,4 +12,10 @@ public class Player {
             throw new IllegalArgumentException(PURCHASE_AMOUNT_NOT_NATURAL_NUMBER_EXCEPTION.toString());
         }
     }
+
+    private void validateLeadingZero(String purchaseAmount) {
+        if (!Pattern.matches(NO_LEADING_ZERO_REGEX, purchaseAmount)) {
+            throw new IllegalArgumentException(PURCHASE_AMOUNT_WITH_LEADING_ZERO.toString());
+        }
+    }
 }
