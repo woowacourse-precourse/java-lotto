74ee80d922c2740fa328c3ead31e71c0d5ead450
JungWoo <rlawjddn103@naver.com>
2022-11-13 15:24:06 +0900

    feat(InputViewValidation): 로또 구입 금액 입력예외처리 기능 추가 및 적용

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -5,9 +5,13 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String REQUEST_MONEY = "구입금액을 입력해 주세요.";
 
-    public static String inputBuyingAmount() {
+    public static int inputBuyingAmount() {
         System.out.println(REQUEST_MONEY);
-        return Console.readLine();
+        String buyingAmount = Console.readLine();
+        InputViewValidation.validateIsInteger(buyingAmount);
+        InputViewValidation.validateIs1000(buyingAmount);
+
+        return Integer.parseInt(buyingAmount);
     }
 
 }
---
+++ src/main/java/lotto/ui/InputViewValidation.java
@@ -0,0 +1,23 @@
+package lotto.ui;
+
+import java.util.regex.Pattern;
+
+public class InputViewValidation {
+
+    static final String ERROR_NOT_INTEGER_FORMAT = "[ERROR] 숫자만 입력해주세요.";
+    static final String ERROR_NOT_1000 = "[ERROR] 1000단위 숫자만 입력해주세요.";
+    static final Pattern NUMBER_PATTERN = Pattern.compile("^[1-9]*$");
+
+
+    public static void validateIsInteger(String input) {
+        if (!NUMBER_PATTERN.matcher(input).find()) {
+            throw new IllegalArgumentException(ERROR_NOT_INTEGER_FORMAT);
+        }
+    }
+
+    public static void validateIs1000(String input) {
+        if (input.charAt(input.length() - 1) != '0' || input.charAt(input.length() - 2) != '0' || input.charAt(input.length() - 3) != '0') {
+            throw new IllegalArgumentException(ERROR_NOT_1000);
+        }
+    }
+}
