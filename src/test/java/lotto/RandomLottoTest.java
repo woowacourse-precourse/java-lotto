b4350fdf11492bda1c89f04c1aeb8e89235a2158
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-11-14 04:21:59 +0900

    test<RandomLottoTest>: 발행되는 로또와 관련된 테스트 코드 추가

---
+++ src/test/java/lotto/RandomLottoTest.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class RandomLottoTest {
+    @DisplayName("발행된 로또 중에 동일한 번호로 이루어진 것이 있다면 예외를 발생시킨다.")
+    @Test
+    void randomNumberByDuplicatedNumber() {
+        assertThatThrownBy(() -> new RandomLotto(List.of(List.of(1, 2, 3, 4, 5, 6), List.of(1,2,3,4,5,6))))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("발행 로또 갯수만큼 로또의 번호응 생성한다.")
+    @Test
+    void wrongOfLottoAmount() {
+        int ticket = 5;
+        RandomLotto.creatRandomNumber(ticket);
+    }
+}
