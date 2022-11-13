6c72c64da534e8a8938b85d69be0d17be124fe4a
baektmdduq <97cowtop@naver.com>
2022-11-13 19:09:57 +0900

    feat : 사용자의 구입금액 및 그에 맞는 로또 개수 구하는 기능 구현
    구입금액이 숫자가 아니라면 예외 처리
    로또는 1장 당 천 원
    구입금액이 천 원 단위가 아니면 예외 처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int payment = correctPayment(UserInputUi.ENTERCOST.getAnswer());
+        payment = getNumberOfLottery(payment);
+        System.out.println("payment : " + payment);
+    }
+
+    public static int getNumberOfLottery(int payment){
+        return payment/1000;
+    }
+
+    public static int correctPayment(String payment){
+        int cost;
+        try{
+            cost = Integer.parseInt(payment);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요..");
+        }
+
+        if(!(cost % 1000 == 0)){
+            throw new IllegalArgumentException("[ERROR] 천 원 단위만 입력해주세요.");
+        }
+
+        return cost;
     }
 }
