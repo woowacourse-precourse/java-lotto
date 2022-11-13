31c0af78d14ec801f89e31f7c49352450565046f
Ridealist <cjsn92@gmail.com>
2022-11-14 00:04:50 +0900

    refactor(Application): MVC패턴 적용_클래스 이름 변경 적용, 전체 로직 정리

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -54,6 +54,14 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 예외_테스트2() {
+        assertSimpleTest(() -> {
+            runException(" ");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
