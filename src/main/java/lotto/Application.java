6400e86d0d82ab58c58f9e11d8a1eac939fa4a93
tlsqj <rnjstlsqja@inha.edu>
2022-11-13 16:59:50 +0900

    feat(Application): main 함수 구현
    -구입금액을 입력받고 1000원으로 나누어 떨어지지 않을 때 오류를 발생
    -구입금액을 1000으로 나누어 로또 구입 갯수 확인
    -당첨번호와 보너스 번호 입력받음

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,44 @@
 package lotto;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        int purchasedValue;
+        String userInputLotto;
+        String[] userInputLottoTmp;
+        int[] userInputLottoNumber = new int[6];
+        int userInputBonusNumber;
+
+        System.out.println("구입금액을 입력해 주세요.");
+        purchasedValue = scanner.nextInt();
+
+        if (purchasedValue % 1000 != 0){
+            System.out.println("[ERROR] 로또 구입 금액은 1000원으로 나누어 떨어져야 합니다.");
+            throw new IllegalArgumentException();
+        }
+
+        int lottoCount = purchasedValue / 1000;
+        System.out.println("\n" +lottoCount+ "개를 구매했습니다.");
+
+        // TODO: 로또 구매 갯수만큼 로또 생성
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        userInputLotto = scanner.nextLine();
+        userInputLottoTmp = userInputLotto.split(",");
+        for (int i = 0; i < userInputLottoTmp.length; i++){
+            userInputLottoNumber[i] = Integer.parseInt(userInputLottoTmp[i]);
+        }
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        userInputBonusNumber = scanner.nextInt();
+
+        // TODO: 사용자가 입력한 당첨 번호, 보너스 번호와 생성한 당첨 번호 비교
+
+        // TODO: 비교값에 따라 당첨 내역 출력
+
+        // TODO: 수익률 계산 및 출력
     }
 }
