d52c060bf341eb85734839ad2b756f050f0574bb
DongwookKim0823 <dwkim0823@naver.com>
2022-11-13 18:56:41 +0900

    feat(lotto): 발행한 로또에 보너스 번호 존재 유무를 확인하는 기능

--- src/main/java/lotto/domian/statistics/Statistics.java
+++ src/main/java/lotto/domian/statistics/Statistics.java
@@ -4,13 +4,20 @@ import java.util.List;
 
 public class Statistics {
 
+    private String checkLottoNumbersContainBonusNumber(List<Integer> lottoNumbers, int bonusNumber) {
+        if (lottoNumbers.contains(bonusNumber)) {
+            return "SECOND";
+        }
+        return "THIRD";
+    }
+
     public int countCorrectNumbers(List<Integer> winningNumbers, List<Integer> lottoNumbers) {
-        int count = 0;
+        int correctNumbers = 0;
         for (int lottoNumber : lottoNumbers) {
             if (winningNumbers.contains(lottoNumber)) {
-                count++;
+                correctNumbers++;
             }
         }
-        return count;
+        return correctNumbers;
     }
 }
