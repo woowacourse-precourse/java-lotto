459bb51e3b2999a5dbbccdf41ff7b224094b8adc
rvlwldev <rvlwlasd77@gmail.com>
2022-11-13 20:15:29 +0900

    test: 당첨번호 입력 테스트케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,16 @@
 package lotto;
 
 import lotto.Domain.Lotto;
+import lotto.Utils.LottoInspector;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Scanner;
 
+import static lotto.Enum.ErrorMessage.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -25,4 +30,35 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("당첨번호를 입력할때 예외를 확인한다.")
+    @Test
+    void inputWinningNumbers () {
+        LottoInspector inspector = new LottoInspector();
+
+        String input01 ="1,2,3,4,5,5";
+        assertThatThrownBy(() -> inspector.winningNumberToList(input01))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(DUPLICATED_NUMBERS.toMessage());
+
+        String input02 ="1,2,3,4,5,6,7";
+        assertThatThrownBy(() -> inspector.winningNumberToList(input02))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_PROPER_WINNING_NUMBER_LENGTH.toMessage());
+
+        String input03 ="1,2,3,4,5";
+        assertThatThrownBy(() -> inspector.winningNumberToList(input03))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_PROPER_WINNING_NUMBER_LENGTH.toMessage());
+
+        String input04 ="1,2,삼,4,5,6";
+        assertThatThrownBy(() -> inspector.winningNumberToList(input03))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_INPUT_INTEGER_MONEY_VALUE.toMessage());
+
+
+    }
+
+    public static InputStream createUserInput(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
 }
