cc11cfd6540cdb69223bd888f17ad6961f98e0fb
twoosky <lhjae3@naver.com>
2022-11-14 01:49:11 +0900

    feat(domain): 당첨 번호와 중복된 보너스 번호 예외 처리

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -11,12 +11,20 @@ public class BonusNumber {
     public BonusNumber(String number, Lotto winningNumbers) {
         int bonusNumber = Integer.parseInt(number);
         validateRange(bonusNumber);
+        validateDuplicatedByWinningNumbers(bonusNumber, winningNumbers);
         this.number = bonusNumber;
     }
 
     private void validateRange(int bonusNumber) {
         if (bonusNumber < 1 || bonusNumber > 45) {
             throw new IllegalArgumentException(BONUS_NUMBER_OUT_OF_RANGE);
+        }
+    }
+
+    private void validateDuplicatedByWinningNumbers(int bonusNumber, Lotto winningNumbers) {
+        List<Integer> numbers = winningNumbers.getNumbers();
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATED);
         }
     }
 }
