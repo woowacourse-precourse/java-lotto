1d7a20b7ceb3a546294f021c0604ded4751074c8
Frog-Slayer <Frog-Slayer@github.com>
2022-11-14 01:17:42 +0900

    feat(Game) : 구매금액(budget)의 get/set

---
+++ src/main/java/lotto/model/Game.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+public class Game {
+    public static final Integer price = 1000;
+    public static final Integer maximumLottoNumber = 45;
+    public static final Integer numberToDraw = 6;
+
+    private Integer budget;
+
+
+    public Integer getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Integer budget){
+        this.budget = budget;
+    }
+
+
+
+
+
+}
