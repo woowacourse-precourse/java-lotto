4e7d4fa56e2aa4b8775f6ef3c6aa5eb1e2b55515
BoyCho <iijokv@naver.com>
2022-11-13 17:20:04 +0900

    feat : Lotto 예외 처리 추가  - 로또 길이가 6이 아니면 예외  - 로또 숫자가 중복이 된다면 예외  - 로또 숫자가 오름차순이 아니라면 예외

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,37 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        lengthCheck(numbers);
+        duplicationCheck(numbers);
+        ascendingOrderCheck(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void lengthCheck(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자 길이가 6이 아닙니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void duplicationCheck(List<Integer> numbers) {
+        for (Integer num : numbers) {
+            if (Collections.frequency(numbers, num) != 1) {
+                throw new IllegalArgumentException("숫자가 중복되었습니다.");
+            }
+        }
+    }
+
+    private void ascendingOrderCheck(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if (numbers.get(i) > numbers.get(i + 1)) {
+                throw new IllegalArgumentException("오름차순 정렬이 아닙니다.");
+            }
+        }
+    }
 }
