588cf1b434baa345f4ea9c6f0ddeef3f81929558
1eejisoo <jisu991012@gmail.com>
2022-11-14 03:53:06 +0900

    feat: 보너스 번호 유효성 검증 - 1~45 범위의 숫자인가

--- src/main/java/domain/Bonus.java
+++ src/main/java/domain/Bonus.java
@@ -4,6 +4,13 @@ public class Bonus {
     private final int bonusNumber;
 
     public Bonus(int bonusNumber) {
+        validateRange(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    public void validateRange(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45 ) {
+            throw new IllegalArgumentException("보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 }
