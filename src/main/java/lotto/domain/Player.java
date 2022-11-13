eb52ad8587bc822d31cbf5b7b513dfba8b421888
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-13 20:26:30 +0900

    feat(Player): 입력값에 따른 로또 발행 수량 구하기

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -3,18 +3,27 @@ package lotto.domain;
 public class Player {
     private final int payment;
 
+
+
+    private final int lottoCount;
+    final int WON_UNIT = 1000;
+
     public Player(int money) {
         validateWonUnit(money);
         this.payment = money;
+        this.lottoCount = money / WON_UNIT;
     }
 
     private void validateWonUnit(int money) {
-        final int WON_UNIT = 1000;
         final String WON_UNIT_ERROR_MESSAGE = "[ERROR] 구입 금액은 1,000원 단위만 가능합니다.";
 
         if (money % WON_UNIT != 0) {
             throw new IllegalArgumentException(WON_UNIT_ERROR_MESSAGE);
         }
+    }
+
+    public int getLottoCount() {
+        return lottoCount;
     }
 
 }
