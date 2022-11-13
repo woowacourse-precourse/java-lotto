b13c3cc949275e81705087b678065293954227d0
JaehongDev <println@kakao.com>
2022-11-13 18:35:37 +0900

    refactor (Money) : 금액이 정해진 방식대로 생성되도록 메서드를 제공하는 기능 구현

--- src/main/java/lotto/domain/money/Money.java
+++ src/main/java/lotto/domain/money/Money.java
@@ -8,26 +8,44 @@ public class Money {
     private static final Long MIN_NUMBER = 1000L;
     private final Long amount;
 
-    public Money(final String input) {
-        validateInputIsNumber(input);
-
-        Long money = Long.valueOf(input);
-
-        //validateMoneyIsGreaterThanMinNumber(money);
-        validateMoneyIsDivideMinNUmber(money);
-
-        this.amount = money;
-    }
-
-    public Money(final Long amount) {
+    private Money(final Long amount) {
         //validateMoneyIsGreaterThanMinNumber(amount);
-        validateMoneyIsDivideMinNUmber(amount);
+        validateMoneyIsDivideMinNumber(amount);
 
         this.amount = amount;
     }
 
-    public static Money zero() {
-        return new Money("0");
+    public static Money generatePurchaseLottoMoney(final String input) {
+        validateInputIsNumber(input);
+        Long money = Long.valueOf(input);
+
+        validateMoneyIsGreaterThanMinNumber(money);
+        validateMoneyIsDivideMinNumber(money);
+        return new Money(money);
+    }
+
+    public static Money generateMoney(final Long money) {
+        return new Money(money);
+    }
+
+    private static void validateInputIsNumber(final String input) {
+        try {
+            Long.valueOf(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(ERROR_MONEY_IS_NUMBER);
+        }
+    }
+
+    private static void validateMoneyIsDivideMinNumber(Long money) {
+        if (money % MIN_NUMBER != 0) {
+            throw new IllegalArgumentException(ERROR_MONEY_DIVIDE_MIN_NUMBER);
+        }
+    }
+
+    private static void validateMoneyIsGreaterThanMinNumber(Long money) {
+        if (money < MIN_NUMBER) {
+            throw new IllegalArgumentException(ERROR_MONEY_GREATER_THAN_MIN_NUMBER);
+        }
     }
 
     public Long calculateAvailablePurchaseCount() {
@@ -36,35 +54,11 @@ public class Money {
 
     /**
      * ROI는 투자대비 수익률로 이익금액/원금 * 100을 나타냅니다.
-     *
-     * @param amount
-     * @return
      */
     public Double calculateROI(Money amount) {
         var ROI = ((double) amount.amount / this.amount) * 100;
         return Math.round(ROI * 100) / 100.0;
     }
-
-    private void validateMoneyIsDivideMinNUmber(Long money) {
-        if (money % MIN_NUMBER != 0) {
-            throw new IllegalArgumentException(ERROR_MONEY_DIVIDE_MIN_NUMBER);
-        }
-    }
-
-    private void validateMoneyIsGreaterThanMinNumber(Long money) {
-        if (money < MIN_NUMBER) {
-            throw new IllegalArgumentException(ERROR_MONEY_GREATER_THAN_MIN_NUMBER);
-        }
-    }
-
-    private void validateInputIsNumber(final String input) {
-        try {
-            Long.valueOf(input);
-        } catch (Exception e) {
-            throw new IllegalArgumentException(ERROR_MONEY_IS_NUMBER);
-        }
-    }
-
 
     public Money plus(Money money) {
         return new Money(this.amount + money.amount);
