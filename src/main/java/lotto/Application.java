80235f9e344b0fb68186403a04680e21094d1af5
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-11-14 01:03:29 +0900

    [ADD] 로또 - Application.java 상수 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,18 +1,33 @@
 package lotto;
 
 public class Application {
-    private final static String MESSAGE_BUDGET = "구입금액을 입력해 주세요.";
-    private final static String MESSAGE_BOUGHT = "개를 구매했습니다.";
-    private final static String MESSAGE_JACKPOT = "당첨 번호를 입력해 주세요.";
-    private final static String MESSAGE_BONUS = "보너스 번호를 입력해 주세요.";
-    private final static String MESSAGE_RESULT = "당첨 통계%n---";
+    private enum Messages {
+        DELIMITER(","),
+        BUDGET("구입금액을 입력해 주세요."),
+        BOUGHT("개를 구매했습니다."),
+        JACKPOT("당첨 번호를 입력해 주세요."),
+        BONUS("보너스 번호를 입력해 주세요."),
+        RESULT("당첨 통계%n---%n"),
+        FIRST("6개 일치 (2,000,000,000원) - %d개%n"),
+        SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개%n"),
+        THIRD("5개 일치 (1,500,000원) - %d개%n"),
+        FOURTH("4개 일치 (50,000원) - %d개%n"),
+        FIFTH("3개 일치 (5,000원) - %d개%n"),
+        RATE("총 수익률은 %.1f%%입니다."),
+        EXCEPTION_HEADER("[ERROR] ");
 
-    private final static String RESULT_FIRST = "개 일치 (2,000,000,000원) - %f개";
-    private final static String RESULT_SECOND = "개 일치 (30,000,000원) - %f개";
-    private final static String RESULT_THIRD = "개 일치 (1,500,000원) - %f개";
-    private final static String RESULT_FOURTH = "개 일치 (50,000원) - %f개";
-    private final static String RESULT_FIFTH = "개 일치 (5,000원) - %f개";
-    private final static String RESULT_EARNING_RATE = "총 수익률은 %.1f%입니다.";
+        private final String message;
+
+        Messages(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+    private final static int PRICE_PER_LOTTERY = 1000;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
