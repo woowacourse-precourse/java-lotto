7ac3b0b43f0060541911e9e98d44b669022e33cf
HiiWee <wpdlzhf159@gmail.com>
2022-11-14 07:03:53 +0900

    feat: 당첨 기능 예외 관련 메시지 추가

--- src/main/java/lotto/constants/message/ExceptionMessage.java
+++ src/main/java/lotto/constants/message/ExceptionMessage.java
@@ -7,6 +7,8 @@ public class ExceptionMessage {
     public static final String NON_NUMERIC_INPUT = "숫자만 입력해야 합니다.";
     public static final String ZERO_INPUT = "0원을 입력할 수 없습니다.";
     public static final String START_WItH_ZERO = "금액은 0으로 시작할 수 없습니다.";
+    public static final String WINNING_FORMAT_ERROR = "'1,2,3,4,5,6'과 같은 형식으로 입력해야 합니다.";
+    public static final String OUT_OF_BOUNDS = "1~45사이의 숫자를 입력해야 합니다.";
 
     private ExceptionMessage() {
     }
