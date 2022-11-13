4512b28873b1fbef21d53fd0c6589f433582e7fa
hojunking96 <junsong96@naver.com>
2022-11-13 18:49:34 +0900

    fix(java-lotto) : 출력문 오타 수정

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static lotto.Result.getTotalPrize;
+
 
 public class Output {
 
@@ -14,8 +16,8 @@ public class Output {
     }
 
     public static void printNumbers(Lotto lotto) {
-        List <Integer> numberForPrint = new ArrayList<>();
-        for(int e : lotto.getNums())
+        List<Integer> numberForPrint = new ArrayList<>();
+        for (int e : lotto.getNums())
             numberForPrint.add(e);
         Collections.sort(numberForPrint);
         System.out.println(numberForPrint);
@@ -30,15 +32,16 @@ public class Output {
     }
 
     public static void printEnd(Result result, int money) {
+        int total = getTotalPrize();
+        float rev = total / (float) money;
+
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.println("3개 일치 (5,000)원 - " + result.getFifth() + "개");
-        System.out.println("4개 일치 (50,000)원 - " + result.getFourth() + "개");
-        System.out.println("5개 일치 (1,500,000)원 - " + result.getThird() + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000)원 - " + result.getSecond() + "개");
+        System.out.println("3개 일치 (5,000원) - " + result.getFifth() + "개");
+        System.out.println("4개 일치 (50,000원) - " + result.getFourth() + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + result.getThird() + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + result.getSecond() + "개");
         System.out.println("6개 일치 (2,000,000,000원) - " + result.getFirst() + "개");
-        int total = result.getFifth() * 5000 + result.getFourth() * 50000 + result.getThird() * 1500000 + result.getSecond() * 30000000 + result.getFirst() * 2000000000;
-        float rev = total / money;
-        System.out.println("총 수익률은 " + Math.round(rev * 10) / 10.0 + "입니다.");
+        System.out.println("총 수익률은 " + Math.round(rev * 1000) / 10.0 + "%입니다.");
     }
 }
