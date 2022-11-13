dd971660d7c3b576104902e3f43b3b36512f0c66
kingseungwooya <ksw_0505@nate.com>
2022-11-13 21:55:35 +0900

    feat(view): add  printlottos

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,8 @@
 package lotto.view;
 
+import lotto.domain.model.Lottos;
+import lotto.view.utils.OutputUtils;
+
 public class OutputView {
 
     private static final String ERROR_HEAD_MESSAGE = "[ERROR]";
@@ -8,7 +11,15 @@ public class OutputView {
     }
 
     public static void printException(Exception exception) {
-        System.out.println(ERROR_HEAD_MESSAGE + exception.getMessage());
+        OutputUtils.outputLine(ERROR_HEAD_MESSAGE + exception.getMessage());
         throw new IllegalArgumentException();
+    }
+
+    public static void printLottos(Lottos lottos) {
+        int quantity = lottos.size();
+        OutputUtils.outputLine(quantity + "개를 구매했습니다.");
+        for (int index = 0; index < quantity; index++) {
+            OutputUtils.outputLine(lottos.getLotto(index).toString());
+        }
     }
 }
