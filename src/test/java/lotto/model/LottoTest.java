4090a39322b9cd6e0bd80cfc656c8900cef2dfb1
Jin409 <sualng123@gmail.com>
2022-11-13 16:59:59 +0800

    test(LottoTest): 특정 숫자가 포함되어있는지 여부를 올바르게 반환하는지 확인하는 테스트 코드 추가

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.ArrayList;
@@ -8,6 +9,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoTest {
+    private List<Integer> numbers;
+    private Lotto testLotto;
+
+    void setUp() {
+        numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+        testLotto = new Lotto(numbers);
+    }
+
     @Test
     @DisplayName("유효하지 않은 로또 번호이라면 객체가 생성되지 않고 예외를 반환한다.")
     void checkLottoConstructorWithInvalidNumbers() {
@@ -17,5 +26,19 @@ public class LottoTest {
         //when, then
         assertThatThrownBy(() -> new Lotto(invalidNumbers))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("특정 숫자가 포함되어있는지 여부를 올바르게 반환한다.")
+    void checkIsContain() {
+        //given
+        setUp();
+        int compareNumber = 3;
+
+        //when
+        boolean result = testLotto.isContain(compareNumber);
+
+        //then
+        assertThat(result).isEqualTo(true);
     }
 }
