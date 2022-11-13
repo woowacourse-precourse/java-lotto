c34fc7901973f3989effe7462b468f7c05c7a6da
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-13 23:44:22 +0900

    feat: 당첨 번호 및 보너스 번호 입력 기능 구현
    당첨 번호 및 보너스 번호 입력 받은 후 LinkedList<Integer> 형으로 반환하는 API 개발

--- src/main/java/lotto/io/Input.java
+++ src/main/java/lotto/io/Input.java
@@ -1,5 +1,7 @@
 package lotto.io;
 
+import java.util.LinkedList;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Input {
@@ -11,5 +13,33 @@ public class Input {
 
         System.out.printf("\n%d개를 구매했습니다.\n", Integer.parseInt(amount) / MIN_UNIT);
         return Integer.parseInt(amount);
+    }
+
+    public LinkedList<Integer> enterWinningNumber() {
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        String number = readLine();
+
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        String bonus = readLine();
+
+        LinkedList<Integer> winningNumList = makeWinningNumberList(number, bonus);
+
+        if (winningNumList.subList(0, winningNumList.size() - 1).contains(Integer.parseInt(bonus))) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호와 보너스 번호가 중복됩니다.");
+        }
+
+        return winningNumList;
+
+    }
+
+    private LinkedList<Integer> makeWinningNumberList(String winningNum, String bonusNum) {
+        LinkedList<Integer> winningNumWithBonusList = new LinkedList<>();
+
+        for (String wn : winningNum.split(",")) {
+            winningNumWithBonusList.add(Integer.parseInt(wn));
+        }
+        winningNumWithBonusList.add(Integer.parseInt(bonusNum));
+
+        return winningNumWithBonusList;
     }
 }
