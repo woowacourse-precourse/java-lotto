bd5470b035ed1de60f084c34323e3a7e271738fd
KIMSEI1124 <workju1124@gmail.com>
2022-11-13 22:40:49 +0900

    refactor: UserService 주석 추가

--- src/main/java/lotto/service/UserService.java
+++ src/main/java/lotto/service/UserService.java
@@ -29,6 +29,8 @@ public class UserService {
 
     public List<Integer> inputNumbersToList(String inputNumbers) {
         List<Integer> numbers = new ArrayList<>(Collections.emptyList());
+        // 쉼표 사이에 공백이 있으면 예외처리
+        // 숫자나 쉼표가 아니면 예외처라
         numbers = toList(inputNumbers);
         return numbers;
     }
@@ -45,6 +47,7 @@ public class UserService {
     public void addBonusNumber(String inputBonusNumber) {
         inputException.notDigitException(inputBonusNumber);
         // 1 ~ 45 확인후 아니면 예외처리
+        // 중복 숫자가 있으면 예외처리
         user.addNumber(Integer.parseInt(inputBonusNumber));
     }
 
