4ac066bc7328d7a0937f4accfdec0c45cebda3cd
Namyunsuk <yunsuk0328@naver.com>
2022-11-13 23:16:54 +0900

    feat(LottoNumberGenerate): 로또 번호 생성 기능 구현

--- src/main/java/lotto/mainframe/LottoNumberGenerate.java
+++ src/main/java/lotto/mainframe/LottoNumberGenerate.java
@@ -1,5 +1,21 @@
 package lotto.mainframe;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class LottoNumberGenerate {
 
+    private Lotto lotto;
+
+    public List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    public void generateLotto(int lottoAmount) {
+        List<Integer> lottoNumbers;
+        for (int i = 0; i < lottoAmount; i++) {
+            lottoNumbers = generateLottoNumbers();
+            lotto = new Lotto(lottoNumbers);
+        }
+    }
 }
