073bba99e08d84794e94c0cb480c0c54e3d25e4b
Jin409 <sualng123@gmail.com>
2022-11-13 17:04:20 +0800

    test(CalculatorTest): Calculator 관련 테스트 코드 추가
    - 당첨 번호와 로또의 값에 따라 알맞은 순위를 반환하는지 확인
    - 로또별 등수를 이용하여 등수별 개수를 이용하여 고객의 당첨 현황을
    반환하는지 확인

--- src/test/java/lotto/utils/CalculatorTest.java
+++ src/test/java/lotto/utils/CalculatorTest.java
@@ -1,10 +1,16 @@
 package lotto.utils;
 
+import static lotto.model.Rank.FIFTH_PLACE;
+import static lotto.model.Rank.FIRST_PLACE;
+import static lotto.model.Rank.FOURTH_PLACE;
 import static lotto.model.Rank.SECOND_PLACE;
+import static lotto.model.Rank.THIRD_PLACE;
+import static lotto.utils.Calculator.calculateCountsByRanks;
 import static lotto.utils.Calculator.calculateLottoRank;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.List;
+import java.util.Map;
 import lotto.model.Lotto;
 import lotto.model.Rank;
 import org.junit.jupiter.api.DisplayName;
@@ -24,5 +30,26 @@ public class CalculatorTest {
 
         //then
         assertThat(rank).isEqualTo(SECOND_PLACE);
+    }
+
+    @Test
+    @DisplayName("로또별 등수를 이용하여 등수별 개수를 이용하여 고객의 당첨 현황을 반환한다.")
+    void checkCalculateCountsByRanks() {
+        //given
+        List<Rank> customerRanks = List.of(FIRST_PLACE, SECOND_PLACE, THIRD_PLACE);
+
+        //when
+        Map<Rank, Integer> result = calculateCountsByRanks(customerRanks);
+
+        //then
+        assertThat(result).isEqualTo(
+                Map.of(
+                        FIRST_PLACE, 1,
+                        SECOND_PLACE, 1,
+                        THIRD_PLACE, 1,
+                        FOURTH_PLACE, 0,
+                        FIFTH_PLACE, 0
+                )
+        );
     }
 }
