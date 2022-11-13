61f3223ada0ff7f7b4d042849572f295742bff18
sonsoowon <yuwoo1018@gmail.com>
2022-11-14 02:36:15 +0900

    feat(InputView): 당첨 번호와 보너스 번호 입력
    - JackpotBonus 클래스 추가

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -2,6 +2,7 @@ package lotto.ui;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import lotto.domain.JackpotBonus;
 import lotto.valid.InputValidator;
 import lotto.valid.LottoValidator;
 
@@ -13,7 +14,9 @@ public enum InputView {
     INSTANCE;
     private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_JACKPOT_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String JACKPOT_NUMBER_SEPARATOR = ",";
+
     public static int inputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
         String money = Console.readLine();
@@ -22,7 +25,15 @@ public enum InputView {
 
         return Integer.parseInt(money);
     }
-    public static List<Integer> inputJackpotNumbers() {
+
+    public static JackpotBonus inputJackpotBonus(){
+        List<Integer> jackpotNumbers = inputJackpotNumbers();
+        int bonusNumber = inputBonusNumber();
+
+        return new JackpotBonus(jackpotNumbers, bonusNumber);
+    }
+
+    private static List<Integer> inputJackpotNumbers() {
         System.out.println(INPUT_JACKPOT_NUMBERS_MESSAGE);
         String jackpotNumberInput = Console.readLine();
 
@@ -36,5 +47,13 @@ public enum InputView {
         LottoValidator.validate(jackpotNumbers);
 
         return jackpotNumbers;
+    }
+    private static int inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        String bonusNumber = Console.readLine();
+
+        InputValidator.validateBonusNumber(bonusNumber);
+
+        return Integer.parseInt(bonusNumber);
     }
 }
