76762de5530ebda4ec39eb683ee726d9be7f02a8
HiiWee <wpdlzhf159@gmail.com>
2022-11-14 01:15:49 +0900

    test: 생성된 로또 번호가 1~45사이의 숫자인지 확인하는 반복 테스트

--- src/test/java/lotto/repository/LottoRepositoryTest.java
+++ src/test/java/lotto/repository/LottoRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.List;
 import java.util.stream.Collectors;
 import lotto.domain.Lotto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
@@ -19,7 +20,7 @@ class LottoRepositoryTest {
 
     @DisplayName("1개의 로또 생성시 중복 없는지 반복 테스트")
     @RepeatedTest(100)
-    void 로또1_발행_반복_테스트() {
+    void 로또1_중복번호_반복_테스트() {
         // given
         lottoRepository.generateLottos(1);
         List<Lotto> lottos = lottoRepository.findAll();
@@ -33,5 +34,29 @@ class LottoRepositoryTest {
 
         // then
         assertThat(distinctLotto.size()).isEqualTo(6);
+    }
+
+    @DisplayName("생성된 로또의 번호가 1~45의 숫자인지 반복 테스트")
+    @RepeatedTest(100)
+    void 로또1_숫자범위_반복_테스트() {
+        // given
+        lottoRepository.generateLottos(1);
+        List<Lotto> lottos = lottoRepository.findAll();
+
+        // when
+        int max = lottos.get(0)
+                .getNumbers()
+                .get(5);
+        int min = lottos.get(0)
+                .getNumbers()
+                .get(0);
+
+        // then
+        Assertions.assertAll(
+                () -> {
+                    assertThat(max).isLessThanOrEqualTo(45);
+                    assertThat(min).isGreaterThanOrEqualTo(1);
+                }
+        );
     }
 }
