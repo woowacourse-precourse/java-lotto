6793a71ae78452b586da91490a4e1a5d38772914
watterricecake <fruturum@nate.com>
2022-11-14 04:33:59 +0900

    fix: UnsupportedOperationException in NsTest
    Deep copy numbers and return

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Lotto {
@@ -18,15 +20,14 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
-    public void printNumbers(){
-        System.out.println(numbers);
-    }
-
-    public void sortNumbers(){
-        Collections.sort(numbers);
-    }
 
     public List<Integer> getNumbers(){
         return numbers;
+    }
+    public List<Integer> sortedNumbers(){
+        List<Integer> newNumbers = new ArrayList<>();
+        this.numbers.stream().forEach(number-> newNumbers.add(number));
+        newNumbers.sort(Comparator.naturalOrder());
+        return newNumbers;
     }
 }
