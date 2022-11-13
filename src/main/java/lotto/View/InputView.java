d53fdd9a273d31dc102646224355b4e3a64bc63e
jihong <clara5091@naver.com>
2022-11-14 07:37:08 +0900

    feat(inputView): 로또번호 입력 기능 구현

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,19 +1,34 @@
 package lotto.View;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.Lotto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
-    public int getAmount(){
-        System.out.println("구앱 금액을 입력해 주세요.");
+    public int getAmount() {
+        System.out.println("구입 금액을 입력해 주세요.");
         return validateNumber(Console.readLine());
     }
 
-    private int validateNumber(String input){
+    private int validateNumber(String input) {
         try {
             return Integer.parseInt(input);
-        } catch (NumberFormatException ok){
+        } catch (NumberFormatException ok) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
         }
     }
 
+    public Lotto getLottoNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return new Lotto(validateNumbers(Console.readLine()));
+    }
+
+    private List<Integer> validateNumbers(String input){
+        return Arrays.stream(input.split(","))
+                .map(this::validateNumber)
+                .collect(Collectors.toList());
+    }
 }
