b3963914e8009fb60bdbb1da9f6ffd6e47f24791
Jae Ung Jung <roosa0210@gmail.com>
2022-11-14 07:57:58 +0900

    feat(Application.java): main 함수 내용 변경
    Controller를 이용해 main 함수를 짧게 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,22 +6,8 @@ public class Application {
 
 
     public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.run();
 
-        System.out.println("구입금액을 입력해 주세요");
-        String money_input = Console.readLine();
-        int money = Integer.MAX_VALUE;
-        int num = 0;
-        try {
-            money = Integer.parseInt(money_input);
-            num = User.num_lotto(money);
-        } catch (IllegalArgumentException e) {
-            System.out.println("[Error]" + e.getMessage());
-        }
-
-        System.out.println(num + "개를 구입했습니다.");
-
-
-
-        // TODO: 프로그램 구현
     }
 }
