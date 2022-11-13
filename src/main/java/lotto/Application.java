91efba6e2abd437de821d845deb12b5dfad3f768
H4R1B0 <qkrguswns25@naver.com>
2022-11-13 21:40:31 +0900

    feat: make the overall flow of lotto

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,56 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int[] counts = new int[6];
+        int bonus = 0;
+        long purchaseMoney = 0, lottoCounts = 0, profit = 0;
+        String inputPurchaseMoney = "", inputBonus = "", inputWinNumbers = "", profitRate = "";
+        List<Integer> winNumbers = new ArrayList<>();
+        List<Lotto> purchasedLotteries = new ArrayList<>();
+
+        System.out.println(Notification.INPUT_PURCHASE_AMOUNT.getMessage());
+        inputPurchaseMoney = Console.readLine();
+        if (!checkInputPurchaseMoney(inputPurchaseMoney))
+            return;
+        purchaseMoney = Long.parseLong(inputPurchaseMoney);
+
+        lottoCounts = purchaseMoney / 1000;
+        System.out.println(String.format(Notification.PURCHASE_COUNT.getMessage(), lottoCounts));
+        for (int i = 0; i < lottoCounts; i++) {
+            Lotto lotto = new Lotto(Creation.lotto());
+            purchasedLotteries.add(lotto);
+            System.out.println(lotto.getLotto());
+        }
+
+        System.out.println(Notification.INPUT_WIN_NUMBERS.getMessage());
+        inputWinNumbers = Console.readLine();
+        if (!checkInputWinNumbers(inputWinNumbers))
+            return;
+        winNumbers = Calculation.toIntegerList(inputWinNumbers);
+        if (!checkWinNumbers(winNumbers))
+            return;
+
+        System.out.println(Notification.INPUT_BONUS_NUMBER.getMessage());
+        inputBonus = Console.readLine();
+        if (!checkInputBonus(winNumbers, inputBonus))
+            return;
+
+        for (Lotto purchasedLotto : purchasedLotteries) {
+            int rank = Calculation.compareLotto(winNumbers, bonus, purchasedLotto.getLotto());
+            counts[rank]++;
+        }
+
+        profit = Calculation.getProfit(counts);
+        profitRate = Calculation.getProfitRate(purchaseMoney, profit);
+
+        String winResult = Calculation.getWinResult(counts, profitRate);
+        System.out.println(winResult);
     }
 
     private static boolean checkInputPurchaseMoney(String inputPurchaseMoney) {
