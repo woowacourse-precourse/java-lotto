32d0e68cd8eb67124aaa5ced8eaf72fd650ee5a2
Kayoung <rkdkdudjd@gmail.com>
2022-11-14 02:35:03 +0900

    feat(LottoComparator): Lotto와 WinningNumber를 비교하는 기능

---
+++ src/main/java/lotto/LottoResult.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class LottoResult {
+    public int matchCount;
+    public boolean isBonusMatched;
+
+    public LottoResult(int matchCount, boolean isBonusMatched) {
+        this.matchCount = matchCount;
+        this.isBonusMatched = isBonusMatched;
+    }
+}
