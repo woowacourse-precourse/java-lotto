a74746df2834ed7ee41bb38e3cdfb2b620171670
JungWoo <rlawjddn103@naver.com>
2022-11-13 15:03:43 +0900

    fix(InputView): 로또 구입 금액 입력 받기 전 안내 문구 추가

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -3,8 +3,10 @@ package lotto.ui;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String REQUEST_MONEY = "구입금액을 입력해 주세요.";
 
     public static String inputBuyingAmount() {
+        System.out.println(REQUEST_MONEY);
         return Console.readLine();
     }
 
