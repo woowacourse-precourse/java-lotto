f5c391a195690a223e93b2823d446c7fd33b2688
taekwon-dev <taekwon_dev@naver.com>
2022-11-13 17:23:45 +0900

    feat: (BonusNumber) 보너스 번호가 1~45 범위 내의 수인지 검증

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,9 +1,19 @@
 package lotto.domain;
 
+import static lotto.domain.Lotto.LOTTO_END_NUMBER;
+import static lotto.domain.Lotto.LOTTO_START_NUMBER;
+
 public class BonusNumber {
     private final int number;
 
     public BonusNumber(int number) {
+        validateNumberInRange(number, LOTTO_START_NUMBER, LOTTO_END_NUMBER);
         this.number = number;
+    }
+
+    private void validateNumberInRange(int number, int startInclusive, int endinclusive) {
+        if (number < startInclusive || number > endinclusive) {
+            throw new IllegalArgumentException();
+        }
     }
 }
