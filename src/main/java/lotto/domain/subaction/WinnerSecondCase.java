96f21c2f665d995b0ddc9e4c63be27a7ac6a0ac4
alstjr706 <alstjr706@naver.com>
2022-11-14 06:28:13 +0900

    refactor : (로또 게임) 두번째 당첨 로직 리팩토링

--- src/main/java/lotto/domain/subaction/WinnerSecondCase.java
+++ src/main/java/lotto/domain/subaction/WinnerSecondCase.java
@@ -5,20 +5,30 @@ import java.util.Collections;
 import java.util.List;
 
 public class WinnerSecondCase {
+    private static final int SECOND_PRIZE_CONDITION = 5;
+    private static final int INITIAL_GENERATION = 5;
+    private static final int SECOND_PRIZE_INDEX = 3;
+    private static final int BONUS_GUNBOAT_ONDITION = 1;
+
     public List<Integer> CaseCount(List<Integer> sameNumberCount, List<Integer> bonusCase) {
-        List<Integer> result = new ArrayList<>(Collections.nCopies(5, 0));
+        List<Integer> result = new ArrayList<>(Collections.nCopies(INITIAL_GENERATION, 0));
         int secondCaseCount = 0;
 
         for (int correctNumber : sameNumberCount) {
-            if (correctNumber == 5) {
-                for (int bonusIndexNumber : bonusCase) {
-                    if (bonusIndexNumber >= 1) {
-                        secondCaseCount++;
-                    }
-                }
+            if (correctNumber == SECOND_PRIZE_CONDITION) {
+                secondCaseCount = getSecondCaseCount(bonusCase, secondCaseCount);
             }
         }
-        result.set(3, secondCaseCount);
+        result.set(SECOND_PRIZE_INDEX, secondCaseCount);
         return result;
+    }
+
+    private int getSecondCaseCount(List<Integer> bonusCase, int secondCaseCount) {
+        for (int bonusIndexNumber : bonusCase) {
+            if (bonusIndexNumber >= BONUS_GUNBOAT_ONDITION) {
+                secondCaseCount++;
+            }
+        }
+        return secondCaseCount;
     }
 }
