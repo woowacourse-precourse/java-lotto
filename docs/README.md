5c45aa2c0b9074b2f77cd58177612a601c352d5c
yenawee <yaena0319@naver.com>
2022-11-13 20:53:32 +0900

    feat : 당첨번호 입력

--- docs/README.md
+++ docs/README.md
@@ -2,7 +2,7 @@
 
 ### 입력
 [✅] 로또 구입 금액 입력. 1000으로 나눠떨어지지 않는 경우 예외 처리 - User#lotteryPurchaseAmount<br>
-[] 당첨번호 입력. 쉼표 기준 - User#winningNumber<br>
+[✅] 당첨번호 입력. 쉼표 기준 - User#winningNumber<br>
 [] 보너스 번호 입력 - User#bonusNumber <br>
 [] 입력값들 숫자 아닐때 예외처리 <br>
 [] 로또 번호 1~45 사이 아니면 예외처리 <br>
--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -2,6 +2,10 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class User {
     static final int LOTTO_PRICE = 1000;
 
@@ -10,12 +14,25 @@ public class User {
     }
 
     int lotteryPurchaseAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         int lotteryPurchaseAmount = Integer.parseInt(input);
         if (lotteryPurchaseAmount % 1000 != 0){
             throw new IllegalArgumentException();
         }
         return (lotteryPurchaseAmount / LOTTO_PRICE);
+    }
+
+    List<Integer> winningNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        String[] splitInput = input.split(",");
+        List<Integer> winningNumber = new ArrayList<Integer>();
+        for (String number : splitInput) {
+            winningNumber.add(Integer.parseInt(number));
+        }
+        return winningNumber;
+
     }
 
 }
