894ec49b4322870863e8894b1423c7939889f265
nn98 <jkllhgb@gmail.com>
2022-11-13 14:50:18 +0900

    Feat: 상수 Texts - 메시지 및 format 선언/초기화

--- src/main/java/constance/Texts.java
+++ src/main/java/constance/Texts.java
@@ -1,4 +1,19 @@
 package constance;
 
 public class Texts {
+    public static final String LOTTO_USER_INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String LOTTO_USER_PURCHASE_RESULT = "%n개를 구매했습니다.";
+    public static final String LOTTO_USER_INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String LOTTO_USER_INPUT_BONUS_NUMBERS = "보너스 번호를 입력해 주세요.";
+    public static final String LOTTO_GAME_WINNING_STATS = "당첨 통계";
+    public static final String LOTTO_GAME_DIVISION = "---";
+    public static final String LOTTO_GAME_COINCIDE_RESULT = "%n개 일치%s (%s원) - %n개";
+    public static final String LOTTO_GAME_LOTTERY_RETURN = "총 수익률은 %.1f입니다.";
+    public static final String LOTTO_GAME_ERROR = "[ERROR] %s 게임을 종료합니다.";
+    public static final String LOTTO_GAME_ERROR_ILLEGAL = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String LOTTO_GAME_ERROR_NEGATIVE = "입력값은 양수여야 합니다.";
+    public static final String LOTTO_GAME_ERROR_NON_NUMERIC = "입력값은 숫자여야 합니다.";
+    public static final String LOTTO_GAME_ERROR_LACK_OF_AMOUNT = "금액이 부족합니다. 로또 구입을 위해 최소 1000원이 필요합니다.";
+    public static final String LOTTO_GAME_ERROR_DUPLICATED = "입력한 번호 중 중복값이 존재합니다.";
+    public static final String LOTTO_GAME_EXCEPTION_AMOUNT_REMAIN = "로또 구매 후 잔액이 존재합니다. %d원을 반환합니다.";
 }
