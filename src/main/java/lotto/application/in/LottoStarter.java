ce64d720048a838116803c5b727e9cd55dc67d96
K-Diger <kitten.diger@gmail.com>
2022-11-14 02:51:47 +0900

    refactor : LottoStarter 클래스 내 생성자 주입 및 메서드 네이밍 변경사항 반영

--- src/main/java/lotto/application/in/LottoStarter.java
+++ src/main/java/lotto/application/in/LottoStarter.java
@@ -1,32 +1,65 @@
 package lotto.application.in;
 
+import lotto.application.input.bonusnumber.BonusNumberInput;
+import lotto.application.input.winningnumber.WinningNumberInput;
+import lotto.application.output.lottoprinter.LottoPrinter;
+import lotto.application.output.statisticprinter.WinningStatisticPrinter;
+import lotto.application.output.yieldprinter.YieldPrinter;
+import lotto.application.service.lottomatcher.LottoMatcher;
+import lotto.application.input.purchasemoney.PurchaseMoneyInput;
+import lotto.application.service.lottonumber.LottoNumberGenerator;
+import lotto.application.service.statistic.WinningStatistic;
 import lotto.application.service.yield.YieldCalculator;
-import lotto.domain.client.Client;
-import lotto.application.service.lottohost.LottoHost;
-import lotto.domain.lotto.Lotto;
 
 import java.util.List;
 import java.util.Map;
 
 public class LottoStarter {
 
-    private final LottoHost lottoHost = new LottoHost();
-    private final YieldCalculator yieldCalculator = new YieldCalculator();
+    private final PurchaseMoneyInput purchaseMoneyInput;
+    private final LottoNumberGenerator lottoNumberGenerator;
+    private final LottoPrinter lottoPrinter;
+    private final WinningNumberInput winningNumberInput;
+    private final BonusNumberInput bonusNumberInput;
+    private final LottoMatcher lottoMatcher;
+    private final WinningStatistic winningStatistic;
+    private final WinningStatisticPrinter winningStatisticPrinter;
+    private final YieldCalculator yieldCalculator;
+    private final YieldPrinter yieldPrinter;
+
+    public LottoStarter(
+            PurchaseMoneyInput purchaseMoneyInput, LottoNumberGenerator lottoNumberGenerator, LottoPrinter lottoPrinter,
+            WinningNumberInput winningNumberInput, BonusNumberInput bonusNumberInput,
+            LottoMatcher lottoMatcher, WinningStatistic winningStatistic, WinningStatisticPrinter winningStatisticPrinter, YieldCalculator yieldCalculator, YieldPrinter yieldPrinter) {
+
+        this.purchaseMoneyInput = purchaseMoneyInput;
+        this.lottoNumberGenerator = lottoNumberGenerator;
+        this.lottoPrinter = lottoPrinter;
+        this.winningNumberInput = winningNumberInput;
+        this.bonusNumberInput = bonusNumberInput;
+        this.lottoMatcher = lottoMatcher;
+        this.winningStatistic = winningStatistic;
+        this.winningStatisticPrinter = winningStatisticPrinter;
+        this.yieldCalculator = yieldCalculator;
+        this.yieldPrinter = yieldPrinter;
+    }
 
     public void start() {
-        int purchaseNumber = Client.getPurchaseMoney();
-        List<List<Integer>> generatedLottoNumber = Lotto.generateLottoNumber(purchaseNumber);
-        Lotto.getGeneratedLottoNumber(generatedLottoNumber);
+        int purchaseMoney = purchaseMoneyInput.inputPurchaseMoney();
 
-        List<Integer> winningLotto = Lotto.inputWinningLottoNumbers();
-        Lotto lotto = new Lotto(winningLotto);
-        int bonusWinningNumber = lotto.inputWinningLottoBonusNumber(winningLotto);
+        List<List<Integer>> generatedLottoNumber = lottoNumberGenerator.generateLottoNumber(purchaseMoney);
+        lottoPrinter.getGeneratedLottoNumber(generatedLottoNumber);
 
-        List<List<Integer>> matchResult = lottoHost.matchWinningLotto(
+        List<Integer> winningLotto = winningNumberInput.inputWinningLottoNumbers();
+        int bonusWinningNumber = bonusNumberInput.inputWinningLottoBonusNumber(winningLotto);
+
+        List<List<Integer>> matchResult = lottoMatcher.matchWinningLotto(
                 generatedLottoNumber, winningLotto, bonusWinningNumber);
 
-        Map<String, Integer> statistic = yieldCalculator.extractStatistic(matchResult);
-        yieldCalculator.showStatistic(statistic);
-        yieldCalculator.showYield(statistic, purchaseNumber);
+        Map<String, Integer> statistic = winningStatistic.extractStatistic(matchResult);
+        winningStatisticPrinter.showStatistic(statistic);
+
+        double yield = yieldCalculator.extractYield(statistic, purchaseMoney);
+        yieldPrinter.showYield(yield);
     }
 }
