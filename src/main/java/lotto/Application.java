ff9cfb0b32e6f53dbbeb525604e4e04bafab9740
kyliecamila <kyliecamila@naver.com>
2022-11-13 18:21:17 +0900

    [feat] 로또 당첨번호 쉼표로 구분되어있는지 확인하는 예외처리함수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,15 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     private static final String INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String LOTTO_ANSWER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String ERROR_MESSAGE = "[ERROR]";
     private static final String PRICE_MESSAGE = "값이 1000으로 나누어지지 않습니다.";
+    private static final String ANSWER_MESSAGE = "번호가 6개가 아닙니다";
     public static void main(String[] args) {
         int price = inputScanner();
         validatePrice(price);
@@ -20,5 +24,27 @@ public class Application {
         Scanner scanner = new Scanner(System.in);
         System.out.println(INPUT_MESSAGE);
         return scanner.nextInt();
+    }
+    public static void validateInputForm(String input){
+        int count=0;
+        for (int charIndex = 0; charIndex < input.length(); charIndex++) {
+            if (input.charAt(charIndex)==',') {
+                count++;
+            }
+        }
+        if(count!=5){
+            throw new IllegalArgumentException(ANSWER_MESSAGE);
+        }
+
+    }
+    public static List<Integer> LottoNumAnswerInput(){
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> lottoNumbers = new ArrayList<>();
+        int lottoNum=1;
+        System.out.println(LOTTO_ANSWER_INPUT_MESSAGE);
+        String lottoInput = scanner.nextLine();
+        validateInputForm(lottoInput);
+
+        return lottoNumbers;
     }
 }
