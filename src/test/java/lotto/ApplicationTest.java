4e1a7f00be619a26663d1262e3856a966cd80739
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-13 12:18:58 +0900

    test(ApplicationTest): 어플리케이션 실행 로직 테스트 추가

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -10,51 +10,242 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest extends NsTest {
-    private static final String ERROR_MESSAGE = "[ERROR]";
+	private static final String ERROR_MESSAGE = "[ERROR]";
 
-    @Test
-    void 기능_테스트() {
-        assertRandomUniqueNumbersInRangeTest(
-                () -> {
-                    run("8000", "1,2,3,4,5,6", "7");
-                    assertThat(output()).contains(
-                            "8개를 구매했습니다.",
-                            "[8, 21, 23, 41, 42, 43]",
-                            "[3, 5, 11, 16, 32, 38]",
-                            "[7, 11, 16, 35, 36, 44]",
-                            "[1, 8, 11, 31, 41, 42]",
-                            "[13, 14, 16, 38, 42, 45]",
-                            "[7, 11, 30, 40, 42, 43]",
-                            "[2, 13, 22, 32, 38, 45]",
-                            "[1, 3, 5, 14, 22, 45]",
-                            "3개 일치 (5,000원) - 1개",
-                            "4개 일치 (50,000원) - 0개",
-                            "5개 일치 (1,500,000원) - 0개",
-                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-                            "6개 일치 (2,000,000,000원) - 0개",
-                            "총 수익률은 62.5%입니다."
-                    );
-                },
-                List.of(8, 21, 23, 41, 42, 43),
-                List.of(3, 5, 11, 16, 32, 38),
-                List.of(7, 11, 16, 35, 36, 44),
-                List.of(1, 8, 11, 31, 41, 42),
-                List.of(13, 14, 16, 38, 42, 45),
-                List.of(7, 11, 30, 40, 42, 43),
-                List.of(2, 13, 22, 32, 38, 45),
-                List.of(1, 3, 5, 14, 22, 45)
-        );
-    }
+	@Test
+	void 기능_테스트() {
+		assertRandomUniqueNumbersInRangeTest(
+				() -> {
+					run("8000", "1,2,3,4,5,6", "7");
+					assertThat(output()).contains(
+							"8개를 구매했습니다.",
+							"[8, 21, 23, 41, 42, 43]",
+							"[3, 5, 11, 16, 32, 38]",
+							"[7, 11, 16, 35, 36, 44]",
+							"[1, 8, 11, 31, 41, 42]",
+							"[13, 14, 16, 38, 42, 45]",
+							"[7, 11, 30, 40, 42, 43]",
+							"[2, 13, 22, 32, 38, 45]",
+							"[1, 3, 5, 14, 22, 45]",
+							"3개 일치 (5,000원) - 1개",
+							"4개 일치 (50,000원) - 0개",
+							"5개 일치 (1,500,000원) - 0개",
+							"5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+							"6개 일치 (2,000,000,000원) - 0개",
+							"총 수익률은 62.5%입니다."
+					);
+				},
+				List.of(8, 21, 23, 41, 42, 43),
+				List.of(3, 5, 11, 16, 32, 38),
+				List.of(7, 11, 16, 35, 36, 44),
+				List.of(1, 8, 11, 31, 41, 42),
+				List.of(13, 14, 16, 38, 42, 45),
+				List.of(7, 11, 30, 40, 42, 43),
+				List.of(2, 13, 22, 32, 38, 45),
+				List.of(1, 3, 5, 14, 22, 45)
+		);
+	}
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() -> {
-            runException("1000j");
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
-    }
+	@Test
+	void 예외_테스트() {
+		assertSimpleTest(() -> {
+			runException("1000j");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+	}
+	@Test
+	void firstRankTest() {
+		assertRandomUniqueNumbersInRangeTest(
+			() -> {
+				run("1000", "1,2,3,4,5,6", "7");
+				assertThat(output()).contains(
+					"1개를 구매했습니다.",
+					"[1, 2, 3, 4, 5, 6]",
+					"3개 일치 (5,000원) - 0개",
+					"4개 일치 (50,000원) - 0개",
+					"5개 일치 (1,500,000원) - 0개",
+					"5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+					"6개 일치 (2,000,000,000원) - 1개",
+					"총 수익률은 200,000,000.0%입니다."
+				);
+			},
+			List.of(1, 2, 3, 4, 5, 6)
+		);
+	}
 
-    @Override
+	@Test
+	void secondRankTest() {
+		assertRandomUniqueNumbersInRangeTest(
+			() -> {
+				run("1000", "1,2,3,4,5,6", "7");
+				assertThat(output()).contains(
+					"1개를 구매했습니다.",
+					"[1, 2, 3, 4, 5, 7]",
+					"3개 일치 (5,000원) - 0개",
+					"4개 일치 (50,000원) - 0개",
+					"5개 일치 (1,500,000원) - 0개",
+					"5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+					"6개 일치 (2,000,000,000원) - 0개",
+					"총 수익률은 3,000,000.0%입니다."
+				);
+			},
+			List.of(1, 2, 3, 4, 5, 7)
+		);
+	}
+
+	@Test
+	void thirdRankTest() {
+		assertRandomUniqueNumbersInRangeTest(
+			() -> {
+				run("1000", "1,2,3,4,5,6", "7");
+				assertThat(output()).contains(
+					"1개를 구매했습니다.",
+					"[1, 2, 3, 4, 5, 8]",
+					"3개 일치 (5,000원) - 0개",
+					"4개 일치 (50,000원) - 0개",
+					"5개 일치 (1,500,000원) - 1개",
+					"5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+					"6개 일치 (2,000,000,000원) - 0개",
+					"총 수익률은 150,000.0%입니다."
+				);
+			},
+			List.of(1, 2, 3, 4, 5, 8)
+		);
+	}
+
+	@Test
+	void fourthRankTest() {
+		assertRandomUniqueNumbersInRangeTest(
+			() -> {
+				run("1000", "1,2,3,4,5,6", "7");
+				assertThat(output()).contains(
+					"1개를 구매했습니다.",
+					"[1, 2, 3, 4, 8, 9]",
+					"3개 일치 (5,000원) - 0개",
+					"4개 일치 (50,000원) - 1개",
+					"5개 일치 (1,500,000원) - 0개",
+					"5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+					"6개 일치 (2,000,000,000원) - 0개",
+					"총 수익률은 5,000.0%입니다."
+				);
+			},
+			List.of(1, 2, 3, 4, 8, 9)
+		);
+	}
+
+	@Test
+	void fifthRankTest() {
+		assertRandomUniqueNumbersInRangeTest(
+			() -> {
+				run("1000", "1,2,3,4,5,6", "7");
+				assertThat(output()).contains(
+					"1개를 구매했습니다.",
+					"[1, 2, 3, 8, 9, 10]",
+					"3개 일치 (5,000원) - 1개",
+					"4개 일치 (50,000원) - 0개",
+					"5개 일치 (1,500,000원) - 0개",
+					"5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+					"6개 일치 (2,000,000,000원) - 0개",
+					"총 수익률은 500.0%입니다."
+				);
+			},
+			List.of(1, 2, 3, 8, 9, 10)
+		);
+	}
+
+	@Test
+	void outOfRankTest() {
+		assertRandomUniqueNumbersInRangeTest(
+			() -> {
+				run("1000", "1,2,3,4,5,6", "7");
+				assertThat(output()).contains(
+					"1개를 구매했습니다.",
+					"[1, 2, 8, 9, 10, 11]",
+					"3개 일치 (5,000원) - 0개",
+					"4개 일치 (50,000원) - 0개",
+					"5개 일치 (1,500,000원) - 0개",
+					"5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+					"6개 일치 (2,000,000,000원) - 0개",
+					"총 수익률은 0.0%입니다."
+				);
+			},
+			List.of(1, 2, 8, 9, 10, 11)
+		);
+	}
+
+	@Test
+	void amountExceptionTest() {
+		assertSimpleTest(() -> {
+			runException("-1000");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+		assertSimpleTest(() -> {
+			runException("1200");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+		assertSimpleTest(() -> {
+			runException("abc");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+	}
+
+	@Test
+	void lottoNumberExceptionTest() {
+		assertSimpleTest(() -> {
+			runException("1000", "123456");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+		assertSimpleTest(() -> {
+			runException("1000", "1.2.3.4.5.6");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+		assertSimpleTest(() -> {
+			runException("1000", "1,2,3,4,5.6");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+		assertSimpleTest(() -> {
+			runException("1000", "1,2,a,4,5,6");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+		assertSimpleTest(() -> {
+			runException("1000", "1, 2, 3, 4, 5, 6");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+		assertSimpleTest(() -> {
+			runException("1000", "0,1,2,3,4,5");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+		assertSimpleTest(() -> {
+			runException("1000", "1,2,3,4,5,46");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+	}
+
+	@Test
+	void bonusNumbersExceptionTest() {
+		assertSimpleTest(() -> {
+			runException("1000", "1,2,3,4,5,6", "a");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+		assertSimpleTest(() -> {
+			runException("1000", "1,2,3,4,5,6", "7,8");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+		assertSimpleTest(() -> {
+			runException("1000", "1,2,3,4,5,6", "0");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+		assertSimpleTest(() -> {
+			runException("1000", "1,2,3,4,5,6", "46");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+		assertSimpleTest(() -> {
+			runException("1000", "1,2,3,4,5,6", "1");
+			assertThat(output()).contains(ERROR_MESSAGE);
+		});
+	}
+
+	@Override
     public void runMain() {
         Application.main(new String[]{});
     }
