850bd7fc1d377bca59ff79cba22e05619cc19c95
JungWoo <rlawjddn103@naver.com>
2022-11-14 01:35:27 +0900

    feat(OutputView): 수익률 출력 기능 추가

--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -8,6 +8,8 @@ import java.util.Map;
 public class OutputView {
     private static final String BUYING_N_AMOUNT = "개를 구매헸습니다.";
     private static final String WINNING_STAT = "당첨통계\n---\n";
+    private static final String EARNING_RATE_START = "총 수익률은 ";
+    private static final String EARNING_RATE_END = "%입니다.";
 
     public static void outputBlankLine() {
         System.out.println();
@@ -25,7 +27,7 @@ public class OutputView {
         System.out.println(WINNING_STAT);
         Rank[] ranks = Rank.values();
         for (Rank rank : ranks) {
-            if (rank.equals(Rank.MISS)){
+            if (rank.equals(Rank.MISS)) {
                 continue;
             }
             String line = "";
@@ -34,7 +36,11 @@ public class OutputView {
         }
     }
 
-    private static String toCashFormat(int cash){
-        return String.format("%,d",cash);
+    public static void outputEarningRate(String rate) {
+        System.out.println(EARNING_RATE_START + rate + EARNING_RATE_END);
+    }
+
+    private static String toCashFormat(int cash) {
+        return String.format("%,d", cash);
     }
 }
