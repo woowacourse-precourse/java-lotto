93204eb928fb9c203235576c8cfaeb33f1d11f89
es <mascoeskim@gmail.com>
2022-11-13 15:43:21 +0900

    feat(calculateResult): 유저번호와 당첨번호를 비교 구현

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -6,13 +6,21 @@ import java.util.Objects;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Output {
-	public static void output(int numberOfGame, ArrayList userNumber, ArrayList winningNumber, int bonusNumber) {
-		outputQuantityAndNumber(numberOfGame, userNumber);
+	public static void output(int numberOfGame, ArrayList<Objects> userNumber, ArrayList<Integer> winningNumber, int bonusNumber) {
+		quantityAndNumberOutput(numberOfGame, userNumber);
+		winningResultOutput(userNumber,winningNumber,bonusNumber);
 	}
-	public static void outputQuantityAndNumber(int numberOfGame, ArrayList userNumber) {
+	public static void quantityAndNumberOutput(int numberOfGame, ArrayList<Objects> userNumber) {
 		System.out.println(numberOfGame+"개를 구매했습니다.");
 		for (int i=0;userNumber.size()>i;i++){
 			System.out.println(userNumber.get(i));
 		}
+	}
+	public static void winningResultOutput(ArrayList<Objects> userNumber, ArrayList<Integer> winningNumber, int bonusNumber) {
+
+	}
+
+	public static String calculateResult(ArrayList<Objects> userNumber, ArrayList<Integer> winningNumber, int bonusNumber){
+
 	}
 }
