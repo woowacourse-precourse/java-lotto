aaf26b009cb7d3723a315b589e8ee3eb9277c2e3
sujin0607 <goweg0607@gmail.com>
2022-11-14 02:35:35 +0900

    feat: 로또 에러사항 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,17 +1,36 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        isCorrectRange(numbers);
+        isNotDuplication(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isCorrectRange(List<Integer> numbers){
+        for (Integer number : numbers) {
+            if(0 >= number && number > 45){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void isNotDuplication(List<Integer> numbers){
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if(numbers.size() != numberSet.size()){
             throw new IllegalArgumentException();
         }
     }
