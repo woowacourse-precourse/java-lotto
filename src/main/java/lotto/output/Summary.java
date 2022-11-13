606c22c3047a951248407534292de95d9f3c2104
Ridealist <cjsn92@gmail.com>
2022-11-13 18:33:57 +0900

    fix(Summary): 통계결과 출력 ui 띄어쓰기 오류 수정, 수익률 소수점 버림 문제 해결

--- src/main/java/lotto/output/Summary.java
+++ src/main/java/lotto/output/Summary.java
@@ -24,19 +24,19 @@ public class Summary {
         System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.printf("3개 일치 (5,000원) -  %d개", fifth);
+        System.out.printf("3개 일치 (5,000원) - %d개", fifth);
         System.out.println();
-        System.out.printf("4개 일치 (50,000원) -  %d개", fourth);
+        System.out.printf("4개 일치 (50,000원) - %d개", fourth);
         System.out.println();
-        System.out.printf("5개 일치 (1,500,000원) -  %d개", third);
+        System.out.printf("5개 일치 (1,500,000원) - %d개", third);
         System.out.println();
-        System.out.printf("5개 일치, 보너스 볼 일치치 (30,000,000원) -  %d개", second);
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개", second);
         System.out.println();
-        System.out.printf("6개 일치 (2,000,000,000원)  -  %d개", first);
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개", first);
     }
 
-    public static void showEarning(int purchaseAmount, int first, int second, int third, int fourth, int fifth) {
-        int prizeMoney = 0;
+    public static void showEarning(int amount, int first, int second, int third, int fourth, int fifth) {
+        float prizeMoney = 0;
 
         prizeMoney += first * Prize.FIRST.getPrize();
         prizeMoney += second * Prize.SECOND.getPrize();
@@ -44,7 +44,7 @@ public class Summary {
         prizeMoney += fourth * Prize.FOURTH.getPrize();
         prizeMoney += fifth * Prize.FIFTH.getPrize();
 
-        float f = (float) (prizeMoney / purchaseAmount * 1_000) * 100;
+        float f = (prizeMoney / (amount * 1_000)) * 100;
         System.out.println();
         System.out.println("총 수익률은 " + String.format("%.1f", f) + "%입니다.");
     }
