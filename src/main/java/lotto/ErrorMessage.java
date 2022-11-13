382a5f98aad4d3bdcf340bf1d54e75590ff92365
splitube <voicesplit@daum.net>
2022-11-13 18:46:16 +0900

    feat(ErrorMessage): 보너스 번호 생성 관련 오류 메시지 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -5,7 +5,10 @@ public enum ErrorMessage {
     DIVIDE_DISABLE("[ERROR] 천원 단위가 아닙니다."),
     NOT_ENOUGH_NUM("[ERROR] 6개의 번호가 필요합니다."),
     WINNING_NUM_NOT_A_NUMBER("[ERROR] 당첨 번호는 숫자여야 합니다."),
-    NUMBER_OUT_OF_RANGE("[ERROR] 번호는 1부터 45사이의 숫자여야 합니다.");
+    NUMBER_OUT_OF_RANGE("[ERROR] 번호는 1부터 45사이의 숫자여야 합니다."),
+    BONUS_NUM_NOT_A_NUMBER("[ERROR] 보너스 번호는 숫자여야 합니다."),
+    WINNING_NUM_CONTAINS_BONUS_NUM("[ERROR] 당첨 번호 중에 보너스 번호가 존재합니다");
+
 
     final String message;
 
