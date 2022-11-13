ce586ec9fdabdde358c91f446e8385faee82b924
michaelp3 <chan-park@hotmail.com>
2022-11-14 02:58:10 +0900

    test(FunctionTest.java): 구입 금액 입력 검증 기능 테스트 작성
    구입 금액 입력 기능과 하위 기능들의 테스트 작성
    1. 구입 금액 입력 검증
    	ㄱ. Integer 여부 검증
    	ㄴ. 0 이상의 금액 여부 검증
    	ㄷ. 1000으로 나누어지는 숫자 여부 검증

---
+++ src/test/java/lotto/FunctionTest.java
@@ -0,0 +1,48 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FunctionTest {
+
+    @Test
+    void validateMoneyInputTest() {
+        String input = "18000";
+        assertThat(Application.validateMoneyInput(input)).isEqualTo(18_000);
+    }
+
+    @Test
+    void validateMoneyInputTest_예외처리() {
+        String input = "-1";
+        assertThatThrownBy(() -> {
+            Application.validateMoneyInput(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void inMoneyRangeTest_FalseCase() {
+        int input = -1;
+        assertThat(Application.inMoneyRange(input)).isEqualTo(false);
+    }
+
+    @Test
+    void inMoneyRangeTest_TrueCase() {
+        int input = 8000;
+        assertThat(Application.inMoneyRange(input)).isEqualTo(true);
+    }
+
+    @Test
+    void divisibleByThousandTest_TrueCase() {
+        int input = 5000;
+        assertThat(Application.divisibleByThousand(input)).isEqualTo(true);
+    }
+
+    @Test
+    void divisibleByThousandTest_FalseCase() {
+        int input = 5001;
+        assertThat(Application.divisibleByThousand(input)).isEqualTo(false);
+    }
+}
