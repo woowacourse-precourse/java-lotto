bd27eadd31222be600953335ea3fcf340d4d3cd2
Jimin <thswlals0219@gmail.com>
2022-11-13 22:09:30 +0900

    feat(lotto): 보너스 번호 입력값을 검사하는 기능 구현
    숫자가 1~45사이인지 검사

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,10 +1,13 @@
 package lotto.model;
 
+import lotto.util.constants.LottoConstants;
+
 public class WinningLotto {
     private Lotto winningNumber;
     private int bonusNumber;
 
     public WinningLotto(Lotto winningNumber, int bonusNumber) {
+        validate(bonusNumber);
         this.winningNumber = winningNumber;
         this.bonusNumber = bonusNumber;
     }
@@ -15,6 +18,16 @@ public class WinningLotto {
 
     public int getBonusNumber() {
         return bonusNumber;
+    }
+
+    private void validate(int bonusNumber) {
+        if (!isSameRangeAsLottoNumber(bonusNumber)) {
+            throw new IllegalArgumentException(LottoConstants.ERROR_IS_NOT_SAME_RANGE_LOTTO_NUMBER);
+        }
+    }
+
+    private boolean isSameRangeAsLottoNumber(int bonusNumber) {
+        return LottoConstants.LOTTO_NUMBER_MIN <= bonusNumber && bonusNumber <= LottoConstants.LOTTO_NUMBER_MAX;
     }
 
 }
