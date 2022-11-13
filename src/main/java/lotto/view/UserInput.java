1bb38de4c07e01e9b13ef8e203dbf37909842a9d
이건회 <rg970604@naver.com>
2022-11-13 19:47:10 +0900

    feat(UserInput.java) : 보너스 번호 입력

--- src/main/java/lotto/view/UserInput.java
+++ src/main/java/lotto/view/UserInput.java
@@ -8,37 +8,47 @@ import java.util.stream.Collectors;
 public class UserInput {
 
     public static int InputMoney() {
-        System.out.println("구입금액을 입력해 주세요");
+        System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         validateIsNumeric(input);
         return Integer.parseInt(input);
     }
 
     public static List<Integer> InputWinningNums() {
+        System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
         validateContainsDelimiter(input);
-        String [] winningnums = input.split(",");
+        String[] winningnums = input.split(",");
         validateIsAllNumeric(winningnums);
         return Arrays.stream(winningnums)
             .mapToInt(Integer::parseInt)
             .boxed()
             .collect(Collectors.toList());
     }
+
+    public static int InputBonusNum() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        validateIsNumeric(input);
+        return Integer.parseInt(input);
+    }
+
     private static void validateIsNumeric(String input) {
         if (!input.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다");
         }
     }
 
-    private static void validateIsAllNumeric(String [] input){
-        for (String number : input){
-            if (!Character.isDigit(number.charAt(0))){
+    private static void validateIsAllNumeric(String[] input) {
+        for (String number : input) {
+            if (!Character.isDigit(number.charAt(0))) {
                 throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다");
             }
         }
     }
+
     private static void validateContainsDelimiter(String input) {
-        if (!input.contains(",")){
+        if (!input.contains(",")) {
             throw new IllegalArgumentException("[ERROR] 지정된 구분자가 포함되지 않았습니다");
         }
     }
