f772b505a9193a7c363ceaacdd5ca2d5b87147c2
DaWoon Lee <qkgk61@gmail.com>
2022-11-13 21:16:19 +0900

    Feat : 게임 실행을 위해 필요한 객체 생성 및 게임 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import lotto.logic.Exchanger;
+import lotto.logic.LottoMachine;
+import lotto.logic.LottoService;
+import lotto.view.View;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoMachine lottoMachine = LottoMachine.getInstance();
+        LottoService lottoService = LottoService.getInstance();
+        Exchanger exchanger = Exchanger.getInstance();
+        View view = View.getInstance();
+        Game game = new Game(lottoMachine, lottoService, exchanger, view);
+
+        try {
+            game.play();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
+
 }
