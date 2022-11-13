8e5c0c76e4bffdf79f792e0f3b0a112b798d98c9
leeyongjun <leeyongjun0310@naver.com>
2022-11-14 02:49:33 +0900

    refactor(lotto): 불필요한 정렬 삭제

--- src/main/java/lotto/domain/Generator.java
+++ src/main/java/lotto/domain/Generator.java
@@ -17,7 +17,6 @@ public class Generator {
         List<List<Integer>> playerLotto = new ArrayList<>();
         for (int i = 0; i < lottoCount; i++) {
             List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            Collections.sort(lotto);
             playerLotto.add(lotto);
         }
         printLotto(playerLotto);
