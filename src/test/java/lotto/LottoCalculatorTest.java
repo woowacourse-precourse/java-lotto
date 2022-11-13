4b90f29b67a97453eea1937590c030f77073fa21
ostar11 <cjw5877@gmail.com>
2022-11-13 19:03:24 +0900

    test(LottoCalculaorTest): compare메서드 테스트 추가

--- src/test/java/lotto/LottoCalculatorTest.java
+++ src/test/java/lotto/LottoCalculatorTest.java
@@ -8,4 +8,12 @@ import org.junit.jupiter.api.Test;
 
 class LottoCalculatorTest {
 
+    @Test
+    void compare() {
+        LottoCalculator lottoCalculator = new LottoCalculator();
+        int input = lottoCalculator.compare(Arrays.asList(1, 2, 3, 4, 5, 6), Arrays.asList(1, 2, 3, 15, 25, 30), 45);
+        int result = 0;
+
+        assertThat(input).isEqualTo(result);
+    }
 }
