2153ca25e98970bca7966079c2d239109fe2ce9d
twoosky <lhjae3@naver.com>
2022-11-14 03:14:20 +0900

    feat(domain): 구입 금액 1000원 미만인 경우 예외 처리

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,8 +1,11 @@
 package lotto.domain;
 
 import static lotto.utils.ErrorMessages.MONEY_NOT_INTEGER;
+import static lotto.utils.ErrorMessages.MONEY_OUT_OF_RANGE;
 
 public class Money {
+    private static final int MIN_RANGE = 1000;
+    private static final int MONEY_UNIT = 1000;
     private final int money;
 
     public Money(String money) {
@@ -15,6 +18,13 @@ public class Money {
             Integer.parseInt(money);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(MONEY_NOT_INTEGER);
+        }
+    }
+
+    private void validateRange(String inputMoney) {
+        int money = Integer.parseInt(inputMoney);
+        if (money < MIN_RANGE) {
+            throw new IllegalArgumentException(MONEY_OUT_OF_RANGE);
         }
     }
 }
