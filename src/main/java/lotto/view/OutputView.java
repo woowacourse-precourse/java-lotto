8f1b78efaa61752cd2531a81387483faca4a5f51
TaeHwan <kimta2hwan@gmail.com>
2022-11-13 15:07:54 +0900

    feat: 당첨 번호 입력 안내 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,6 +6,7 @@ public class OutputView {
 
     private static final String PURCHASE_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String PURCHASE_RESULT_MESSAGE = "%d개를 구매했습니다.\n";
+    private static final String WINNIG_NUMBERS_ENTERING_GUIDE_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public static void printPurchaseGuideMessage() {
         System.out.println(PURCHASE_GUIDE_MESSAGE);
@@ -17,5 +18,9 @@ public class OutputView {
 
     public static void printLottoNumbers(List<Integer> lottoNumbers) {
         System.out.println(lottoNumbers.toString());
+    }
+
+    public static void printWinningNumbersEnteringGuideMessage() {
+        System.out.println(WINNIG_NUMBERS_ENTERING_GUIDE_MESSAGE);
     }
 }
