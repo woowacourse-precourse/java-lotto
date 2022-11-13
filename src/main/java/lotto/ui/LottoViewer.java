a98e95ca896260bcc33e3832ff196944a648a794
whdcks420 <whdcks420@gmail.com>
2022-11-14 03:48:02 +0900

    refactor(ui): 로또 뷰어 수정

--- src/main/java/lotto/ui/LottoViewer.java
+++ src/main/java/lotto/ui/LottoViewer.java
@@ -1,36 +1,50 @@
 package lotto.ui;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoCount;
 
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class LottoViewer {
+
+    private static String LINE_FEED = "\n";
+
     public static void showPurchaseAmountInputText() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
     public static void showCountOfLottoTickets(int count) {
-        System.out.println("\n" + count + "개를 구매했습니다.");
+        printLineFeed();
+        System.out.println(count + "개를 구매했습니다.");
     }
 
     public static void showWinningLottoNumberInputText() {
-        System.out.println("\n당첨 번호를 입력해 주세요.");
+        printLineFeed();
+        System.out.println("당첨 번호를 입력해 주세요.");
     }
 
     public static void showBonusLottoNumberInputText() {
-        System.out.println("\n보너스 번호를 입력해 주세요.");
+        printLineFeed();
+        System.out.println("보너스 번호를 입력해 주세요.");
     }
 
-    public static void showYield(double yield) {
+    public static void showYield(String yield) {
         System.out.println("총 수익률은 " + yield + "%입니다.");
     }
 
     public static void showLottoTickets(List<Lotto> lottoTickets) {
-        IntStream.range(0, lottoTickets.size()).forEach(index -> System.out.println(lottoTickets.get(index).getNumbers()));
+        lottoTickets.forEach(lotto -> System.out.println(lotto.getNumbers()));
     }
 
-    public static void showWinningStatistics() {
-        System.out.println("\n당첨 통계\n" + "---\n");
+    private static void printLineFeed() {
+        System.out.println();
+    }
+
+    public static void showWinningStatisticsText(LottoCount lottoCount) {
+        System.out.println("3개 일치 (5,000원) - " + lottoCount.getFifth() + "개");
+        System.out.println("4개 일치 (50,000원) - " + lottoCount.getFourth() + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + lottoCount.getThird() + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + lottoCount.getSecond() + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + lottoCount.getFirst() + "개");
     }
 }
