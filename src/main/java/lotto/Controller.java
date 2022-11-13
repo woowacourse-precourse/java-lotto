e06aa19ac80d59e97dba40d9bbfdbb0ada755005
h0ngwon <w___on@naver.com>
2022-11-13 17:06:28 +0900

    feat(Controller): 구입 금액 검증 기능

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -2,21 +2,25 @@ package lotto;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-import net.bytebuddy.pool.TypePool.Resolution.Illegal;
-
 public class Controller {
     private static final String ERROR_MESSAGE = "[ERROR] ";
     private static final String PURCHASE_LOTTO = "구입금액을 입력해 주세요.";
     private static final String NOT_NUMBER = "숫자가 아닙니다.";
+    private static final String NOT_INSEPARABLE = "구입 금액이 1000원 단위가 아닙니다.";
 
     public void start() {
         String amount;
         amount = readLine();
-
+        isValidateNumber(amount);
+        isValidatePurchase(amount);
     }
 
     public void isValidatePurchase(String userInput) {
+        int amount = Integer.parseInt(userInput);
 
+        if(amount % 1000 != 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + NOT_INSEPARABLE);
+        }
     }
 
     public void isValidateNumber(String userInput) {
