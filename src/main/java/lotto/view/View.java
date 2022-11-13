c746d3fbf571d76b90d5abe9231acdd82b04eaa8
hongsikcho <jjdd9758@naver.com>
2022-11-14 00:04:35 +0900

    refactor :2 DecimalFormat 도입하여 금액 출력

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -4,9 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.Lotto;
 
 import javax.swing.plaf.IconUIResource;
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class View {
+    private static final DecimalFormat decFormat = new DecimalFormat("###,###");
     public static String printUserInputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
@@ -32,12 +34,12 @@ public class View {
     public static void printWinningResult(List<Integer> result, List<String> winningMoney) {
         for (int i = 0; i < 4; i++) {
             if (i == 3) {
-                System.out.printf("%d개 일치, 보너스 볼 일치 (%s원) - %d개", 5, winningMoney.get(i + 1), result.get(i + 1));
+                System.out.printf("%d개 일치, 보너스 볼 일치 (%s원) - %d개", 5, decFormat.format(winningMoney.get(i + 1)), result.get(i + 1));
                 System.out.println();
-                System.out.printf("%d개 일치 (%s원) - %d개", i + 3, winningMoney.get(i), result.get(i));
+                System.out.printf("%d개 일치 (%s원) - %d개", i + 3, decFormat.format(winningMoney.get(i)), result.get(i));
                 continue;
             }
-            System.out.printf("%d개 일치 (%s원) - %d개", i + 3, winningMoney.get(i), result.get(i));
+            System.out.printf("%d개 일치 (%s원) - %d개", i + 3, decFormat.format(winningMoney.get(i)), result.get(i));
             System.out.println();
         }
     }
