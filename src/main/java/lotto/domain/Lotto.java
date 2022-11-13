ca41064e8862c9b15e948798fe774a90650e1620
hossang <zkzk104@naver.com>
2022-11-13 17:05:18 +0900

    refactor: 로또들을 Lotto를 통해서 생성하도록 변경하기

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,25 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    public Lotto(List<Integer> numbers) {
+        isSixNumbers(numbers);
+        this.numbers = numbers;
+        Collections.sort(numbers);
+    }
+
+    private void isSixNumbers(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(Error.IS_NOT_SIX.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
 }
--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -3,26 +3,23 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Lottos {
 
-    private List<List<Integer>> lottos;
+    private List<Lotto> lottos;
 
     public Lottos() {
         this.lottos = new ArrayList<>();
     }
 
-    public List<List<Integer>> getLottos() {
+    public List<Lotto> getLottos() {
         return lottos;
     }
 
     public void buy(int quantity) {
         for (int count = 0; count < quantity; count++) {
-            lottos.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-            Collections.sort(lottos.get(count));
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
         }
 
     }
