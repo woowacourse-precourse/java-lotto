cfd9cb9a07534d78ac58f7bf13c4f9dc5f083c07
KimTaeKang57 <zizoen123@gmail.com>
2022-11-13 13:39:27 +0900

    refac: LottoApplication final 상수 추가

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -6,6 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoApplication {
+    private final String BONUS_BALL_RESULT_MESSAGE = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개";
+    private final String RESULT_MESSAGE = "%d개 일치 (%,d원) - %d개";
+    private final String WINNING_NUMBER_INPUT = "당첨 번호를 입력해주세요.";
+    private final String DUPLICATE_NUMBER_ERROR = "[ERROR] 로또 번호가 중복됩니다.";
+    private final String MONEY_INPUT = "구입금액을 입력해 주세요.";
+    private final String BONUS_NUMBER_INPUT = "보너스 번호를 입력해 주세요.";
+
     private RandomLotto randomLottos;
     private RandomLottoGenerator randomLottoGenerator;
     private WinningLotto winningLotto;
@@ -14,7 +21,7 @@ public class LottoApplication {
     private int bonusNumber;
 
     public void run() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(MONEY_INPUT);
         int money = Integer.parseInt(Console.readLine());
         if (money % 1000 != 0) {
             throw new IllegalArgumentException();
@@ -42,19 +49,19 @@ public class LottoApplication {
     }
 
     private int inputBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(BONUS_NUMBER_INPUT);
         bonusNumber = Integer.parseInt(Console.readLine());
         return bonusNumber;
     }
 
     private Lotto inputAndGetLotto() {
-        System.out.println("당첨 번호를 입력해주세요.");
+        System.out.println(WINNING_NUMBER_INPUT);
         List<Integer> inputWinningLotto = new ArrayList<>();
         String[] split = Console.readLine().split(",");
 
         for (int idx = 0; idx < split.length; idx++) {
             if (inputWinningLotto.contains(Integer.parseInt(split[idx]))) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호가 중복됩니다.");
+                throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR);
             }
             inputWinningLotto.add(Integer.parseInt(split[idx]));
         }
@@ -74,11 +81,11 @@ public class LottoApplication {
         System.out.println("---");
         for (LottoReward lottoReward : LottoReward.values()) {
             if (lottoReward == LottoReward.SECOND) {
-                System.out.println(String.format("%d개 일치, 보너스 볼 일치 (%,d원) - %d개",
+                System.out.println(String.format(BONUS_BALL_RESULT_MESSAGE,
                         lottoReward.getMatchingNumbers(), lottoReward.getReward(), lottoReward.getCount()));
                 continue;
             }
-            System.out.println(String.format("%d개 일치 (%,d원) - %d개",
+            System.out.println(String.format(RESULT_MESSAGE,
                     lottoReward.getMatchingNumbers(), lottoReward.getReward(), lottoReward.getCount()));
         }
     }
