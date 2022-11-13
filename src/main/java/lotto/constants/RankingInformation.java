79fde0b57669ff7958b2ccaa42f57cf9b56e13a7
Sehbeom <j3beom@gmail.com>
2022-11-13 20:54:46 +0900

    refactor(constants): 당첨 순위 정보 열거 상수 리팩토링
    당첨 순위 정보 RankingInformation Enum 수정
    당첨 통계 기능에서 활용하기 위해
    각 열거상수 객체마다 일치 개수 멤버 상수 추가

--- src/main/java/lotto/constants/RankingInformation.java
+++ src/main/java/lotto/constants/RankingInformation.java
@@ -1,19 +1,25 @@
 package lotto.constants;
 
 public enum RankingInformation {
-    FIFTH_PLACE("3개 일치 (5,000원) - "),
-    FOURTH_PLACE("4개 일치 (50,000원) - "),
-    THIRD_PLACE("5개 일치 (1,500,000원) - "),
-    SECOND_PLACE("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    FIRST_PLACE("6개 일치 (2,000,000,000원) - ");
+    FIFTH_PLACE("3개 일치 (5,000원) - ", 3),
+    FOURTH_PLACE("4개 일치 (50,000원) - ", 4),
+    THIRD_PLACE("5개 일치 (1,500,000원) - ", 5),
+    SECOND_PLACE("5개 일치, 보너스 볼 일치 (30,000,000원) - ", 5),
+    FIRST_PLACE("6개 일치 (2,000,000,000원) - ", 6);
 
     private final String statisticSentence;
+    private final int numberOfMatch;
 
-    RankingInformation(String sentence) {
+    RankingInformation(String sentence, int number) {
         this.statisticSentence = sentence;
+        this.numberOfMatch = number;
     }
 
     public String getStatisticSentence() {
         return this.statisticSentence;
+    }
+
+    public int getNumberOfMatch() {
+        return this.numberOfMatch;
     }
 }
