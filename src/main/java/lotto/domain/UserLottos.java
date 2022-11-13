da93ed29e366b6a36590f2057a90eda74513d286
yoon <beatus96@naver.com>
2022-11-14 03:12:31 +0900

    refactor(UserLottos) : 로또 생성도메인 수정
    수정 이유: 금액을 입력받고,
    금액에 해당하는 로또를 생성하는 문제를 하나의 해결해야 할 문제(비즈니스)라고 보았다.
    따라서, UserLottos라는 새로운 도메인(클래스)로 분리하였다.
    
    *설명
    UserLottos의 인스턴스 생성시, 금액이 필요하다.
    - 로또를 추첨하기에 유효한 금액인지 체크하고
    - 금액만큼 로또를 생성하여,
    - 인스턴스 변수인 Set<Lotto> userLottos에 생성한 로또들을 저장한다.

---
+++ src/main/java/lotto/domain/UserLottos.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserLottos {
+    private final int LOTTO_PRICE = 1000;
+    private int count;
+    private Set<Lotto> userLottos;
+
+    public UserLottos(int money) {
+        validateMoney(money);
+        setCount(money);
+        createLottos(count);
+    }
+
+    public void validateMoney(int money) {
+        if(money < LOTTO_PRICE  && money%LOTTO_PRICE !=0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void setCount(int money) {
+
+        this.count = money/LOTTO_PRICE;
+    }
+
+    public void createLottos(int count) {
+        userLottos = new HashSet<>();
+
+        while (userLottos.size() != count) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            userLottos.add(lotto);
+        }
+    }
+}
