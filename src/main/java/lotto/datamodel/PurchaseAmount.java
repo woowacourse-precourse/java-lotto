8cb26a3a0c724fff0e77ac7e48f5b5d693af3494
sora5421 <21800804@handong.edu>
2022-11-13 19:20:18 +0900

    feat (PurchaseAmount) : 구매금액 유효성 검사 및 구매갯수 생성

--- src/main/java/lotto/datamodel/PurchaseAmount.java
+++ src/main/java/lotto/datamodel/PurchaseAmount.java
@@ -1,4 +1,32 @@
 package lotto.datamodel;
 
 public class PurchaseAmount {
+
+    private int moneyCount;
+    public PurchaseAmount(String input) {
+        int money = numberValid(input);
+        moneyCount = moneyValid(money);
+    }
+
+    public int numberValid(String input) {
+        int check;
+        try{
+            check = Integer.parseInt(input);
+        }catch(Exception e) {
+            throw new IllegalArgumentException();
+        }
+        return check;
+    }
+
+    public int moneyValid(int money){
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+        return money / 1000;
+    }
+
+    public int getMoneyCount() {
+        return moneyCount;
+    }
+
 }
