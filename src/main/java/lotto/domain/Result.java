1a3fe39eece81673fad6f69dbead826faed6a392
Kimhyeyun <cqqwer@gmail.com>
2022-11-13 21:51:18 +0900

    Feat: get메서드 생성 & 수익률 계산을 위한 메서드 생성

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -24,4 +24,18 @@ public class Result {
     public void add(Rank rank) {
         result.put(rank, result.getOrDefault(rank, 0) + 1);
     }
+
+    public float calculateProfit(int price) {
+        return ((float) getTotalReward() / (float) price) * 100;
+    }
+
+    private long getTotalReward() {
+        return result
+                .entrySet()
+                .stream()
+                .mapToLong(entry ->
+                    entry.getKey().getReward() * entry.getValue()
+                )
+                .sum();
+    }
 }
