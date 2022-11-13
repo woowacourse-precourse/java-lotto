a8079324a3ee636187f3d6b8c21d362e74031a02
EUNJEONGMUN <munej26@gmail.com>
2022-11-14 01:40:18 +0900

    test: getMatchResult 메서드가 Lotto클래스 안으로 이동함에 따른 테스트코드 수정

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -58,16 +58,22 @@ class LottoNumberTest {
     @Test
     void 당첨번호_비교_테스트() {
         LottoNumber lottoNumber = new LottoNumber("1,2,3,4,5,6", "7");
-        MatchCount matchResult = lottoNumber.getMatchResult(new Lotto(List.of(1, 2, 3, 10, 11, 7)));
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 10, 11, 7));
+
+        MatchCount matchResult = lotto.getMatchResult(lottoNumber);
         MatchCount answer = new MatchCount(3, 1);
+
         assertTrue(answer.isSameResult(matchResult));
     }
 
     @Test
     void 당첨번호_비교_테스트_비정상() {
         LottoNumber lottoNumber = new LottoNumber("1,2,3,4,5,6", "7");
-        MatchCount matchResult = lottoNumber.getMatchResult(new Lotto(List.of(1, 2, 3, 10, 11, 7)));
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 10, 11, 7));
+
+        MatchCount matchResult = lotto.getMatchResult(lottoNumber);
         MatchCount answer = new MatchCount(4, 1);
+
         assertFalse(answer.isSameResult(matchResult));
     }
 }
