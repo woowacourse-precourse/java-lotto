544d981a8a079c2a82e2c3d61958586b5fd6200f
keon0711 <fnc6488@gmail.com>
2022-11-13 21:20:25 +0900

    refactor(lotto): remove function to sort

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -12,7 +12,6 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        Collections.sort(numbers);
         validate(numbers);
         this.numbers = numbers;
     }
