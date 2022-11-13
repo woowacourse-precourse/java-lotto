a5df1bb3ca1de453793c6b5f214544bb16f3facc
Hyunseok Lee <hyun104467@naver.com>
2022-11-14 00:48:56 +0900

    feat(lotto): checkMoneyUnit

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,9 +1,25 @@
 package lotto.domain;
 
 public class LottoGenerator {
-
-    public LottoGenerator() {
-
+    int generateCount;
+    // List<Integer> numbers;
+    public LottoGenerator(int money) {
+        try {
+            if (checkMoneyUnit(money)) {
+                generateCount = money / 1000;
+            }
+        }
+        catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("ERROR");
+        }
     }
+
+    private Boolean checkMoneyUnit(int money) throws IllegalArgumentException {
+        if ((money % 1000) != 0) {
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
 
 }
