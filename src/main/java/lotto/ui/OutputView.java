3b56800e4552658b9e88af77733947cb77393f90
JungWoo <rlawjddn103@naver.com>
2022-11-14 00:08:37 +0900

    feat(OutputView): 로또 출력 기능 추가

--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -1,9 +1,14 @@
 package lotto.ui;
 
+import java.util.List;
+
 public class OutputView {
-    private static String BUYING_N_AMOUNT = "개를 구매헸습니다.";
+    private static final String BUYING_N_AMOUNT = "개를 구매헸습니다.";
 
     public static void outputBuyingAmount(int buyingMoney) {
         System.out.println(buyingMoney/1000 + BUYING_N_AMOUNT);
+    }
+    public static void outputLotto(List<Integer> numbers) {
+        System.out.println(numbers);
     }
 }
