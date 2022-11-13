79319d4b8547e9582ddf544166ae9a7b577ac6d3
Heesom <heesom.hs@gmail.com>
2022-11-13 21:00:25 +0900

    test(MoneyTest) : 로또 구입 금액 산정 기능 테스트 케이스 추가

---
+++ src/test/java/lotto/Domain/MoneyTest.java
@@ -0,0 +1,29 @@
+package lotto.Domain;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+    SoftAssertions softAssertions = new SoftAssertions();
+
+    @DisplayName("1000원으로 나눠지지 않으면 에러가 발생한다")
+    @Test
+    void createMoneyNotThousand() {
+        softAssertions.assertThatThrownBy(() -> new Money("100"))
+                .hasMessageContaining("ERROR")
+                .isInstanceOf(IllegalArgumentException.class);
+
+        softAssertions.assertAll();
+    }
+
+    @DisplayName("문자가 입력되면 에러가 발생한다")
+    @Test
+    void createMoneyChar() {
+        softAssertions.assertThatThrownBy(() -> new Money("1000a"))
+                .hasMessageContaining("ERROR")
+                .isInstanceOf(IllegalArgumentException.class);
+
+        softAssertions.assertAll();
+    }
+}
