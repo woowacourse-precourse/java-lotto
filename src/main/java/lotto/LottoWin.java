d7035792ee04e2d35018ed6222e0f14502cefca2
Hong-yeonggwang <chamsk0808@naver.com>
2022-11-13 20:54:36 +0900

    style:profitRateCal(프린트문 문구변경)

--- src/main/java/lotto/LottoWin.java
+++ src/main/java/lotto/LottoWin.java
@@ -8,6 +8,8 @@ public class LottoWin {
     static LottoWin lotto;
     private List<Integer> lottoNum;
 
+    public List<int[]> expenseWithprofit = new ArrayList<int[]>();
+
     private LottoWin(){
         System.out.println("당첨 번호를 입력해 주세요.");
         String userInput = Console.readLine();
@@ -32,5 +34,25 @@ public class LottoWin {
         }
         return returnData;
     }
+    public void addExpenseWithprofit(int[] inputData){
+        this.expenseWithprofit.add(inputData);
+    }
+//    수익률은 당첨금액/구입금액*100
+    public void profitRateCal(){
+        final int expenseIndex = 0;
+        final int profitIndex = 1;
+        double profitRate;
+        int expenseSum = 0;
+        int profitSum = 0;
+        for(int[] userInfo : this.expenseWithprofit){
+            expenseSum += userInfo[expenseIndex];
+            profitSum += userInfo[profitIndex];
+        }
+        profitRate = ((double)profitSum / expenseSum) * 100;
+        System.out.println(String.format("총 수익률은 %.2f입니다.", profitRate));
+    }
 
+    public List<Integer> getLottoNum() {
+        return lottoNum;
+    }
 }
