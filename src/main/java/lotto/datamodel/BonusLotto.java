9b64c1a37854b21b22fefbf39779ed525b849703
sora5421 <21800804@handong.edu>
2022-11-14 07:14:34 +0900

    feat : 보너스 숫자가 당첨 숫자와 중복되는지 확인하는 기능

--- src/main/java/lotto/datamodel/BonusLotto.java
+++ src/main/java/lotto/datamodel/BonusLotto.java
@@ -1,4 +1,19 @@
 package lotto.datamodel;
 
+import java.util.List;
+
 public class BonusLotto {
+    private final int bonusNumber;
+    public BonusLotto(int number, List<Integer> winnumbers) {
+        reduplication(number, winnumbers);
+        this.bonusNumber = number;
+    }
+
+    private void reduplication(int number, List<Integer> winnumbers) {
+        if(winnumbers.contains(number) == true) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
 }
