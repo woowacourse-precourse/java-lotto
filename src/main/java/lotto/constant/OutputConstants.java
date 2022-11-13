71c8fd69b89ce2eb1d9c9472e66ac2ef5b15ad94
CAEL0 <kjongmin26@gmail.com>
2022-11-13 15:17:46 +0900

    refactor(OutputConstants): MONEY -> PURCHASE_AMOUNT로 수정

--- src/main/java/lotto/constant/OutputConstants.java
+++ src/main/java/lotto/constant/OutputConstants.java
@@ -2,7 +2,7 @@ package lotto.constant;
 
 public enum OutputConstants {
 
-    INPUT_MONEY("구입금액을 입력해 주세요."),
+    INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     PURCHASE_QUANTITY("%d개를 구매했습니다."),
 
     INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
