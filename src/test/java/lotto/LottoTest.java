14d64e735e49c08686e7d2beb4a4bd6233642bf8
hyun-seung <ardimento22@naver.com>
2022-11-14 03:04:26 +0900

    [TEST] 구매 금액에 해당하는 만큼 로또를 발행한다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,19 @@
 package lotto;
 
+import lotto.controller.LottoController;
 import lotto.domain.Lotto;
+import lotto.domain.Money;
 import lotto.utils.ExceptionType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static lotto.utils.Constant.LOTTO_PRICE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -32,5 +38,18 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 80)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionType.NOT_WITHIN_THE_RANGE.getMessage());
+    }
+
+    @DisplayName("넣은 금액으로 구매할 수 있는 로또의 개수와 출력된 로또의 개수가 일치한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 9, 13, 19})
+    void checkBuyingLotto(int input) {
+        LottoController lottoController = new LottoController();
+        int myMoney = input * LOTTO_PRICE;
+        Money money = new Money(myMoney);
+
+        List<Lotto> lottos = lottoController.getLottos(money);
+
+        assertThat(lottos.size()).isEqualTo(input);
     }
 }
