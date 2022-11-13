f2b1653353bd447fad0a590583e85d06210b8edd
buddy <jang_taehwan@naver.com>
2022-11-14 01:08:31 +0900

    test(Lotto): 정상 및 예외 테스트 케이스 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,27 +1,52 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
-    @Test
-    void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+
+    @Nested
+    @DisplayName("예외 테스트")
+    class ExceptionTest {
+
+        @Test
+        @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+        void createLottoByOverSize() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+        void createLottoByDuplicatedNumber() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("로또 번호의 개수가 6개 미만이면 예외가 발생한다.")
+        void createLottoByShortSize() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+    @Nested
+    @DisplayName("정상 동작 테스트")
+    class NormalTest {
+
+        @Test
+        @DisplayName("로또 번호의 중복이 없고, 6개의 숫자가 들어온다면 정상 동작한다.")
+        void createCorrectLotto() {
+            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+            assertThat(lotto).isInstanceOf(Lotto.class);
+        }
     }
 
-    // 아래에 추가 테스트 작성 가능
+
 }
