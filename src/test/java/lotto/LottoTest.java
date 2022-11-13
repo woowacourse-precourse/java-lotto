d06c375f557937a5768e229b9f0598fb89319cf1
keon0711 <fnc6488@gmail.com>
2022-11-14 00:47:44 +0900

    test(lotto): test for lotto number sort

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -25,4 +26,12 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또번호가  정령되는지 확인")
+    @Test
+    void sortLottoNumbers() {
+        Lotto lotto = new Lotto(List.of(45,13,12,23,29,1));
+
+        assertThat(lotto.getNumbers())
+                .isSorted();
+    }
 }
