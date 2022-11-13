97dc110206055eb729be8ef26d61dc02144341f2
1eejisoo <jisu991012@gmail.com>
2022-11-14 03:18:58 +0900

    refactor: 원시값 포장, 생성자 내부 코드 순서 변경

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -2,15 +2,17 @@ package domain;
 
 public class Money {
     private final static int UNIT = 1000;
+    private final static int ZERO = 0;
     private final int money;
 
     public Money(int money) {
+        validateOneThousandUnit(money);
         this.money = money;
-        validateOneThousandUnit();
+
     }
 
-    public void validateOneThousandUnit() {
-        if (money % UNIT != 0) {
+    public void validateOneThousandUnit(int money) {
+        if (money % UNIT != ZERO) {
             throw new IllegalArgumentException("구입 금액은 1,000원 단위여야 합니다.");
         }
     }
