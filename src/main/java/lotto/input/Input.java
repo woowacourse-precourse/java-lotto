81f42ea352ef778ba01ac734bcc32cc3f0dc4e2d
Ridealist <cjsn92@gmail.com>
2022-11-13 14:58:46 +0900

    fix(Input): 실행 예시 ui에 맞게 줄 바꿈 추가

--- src/main/java/lotto/input/Input.java
+++ src/main/java/lotto/input/Input.java
@@ -5,12 +5,14 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
 
     public static String askWinningNumbers() {
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
         return input;
     }
 
     public static String askBonusNumber() {
+        System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
         return input;
