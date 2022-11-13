d930639fdfe088db0b9d70580c00fe7a52c76501
TaeHwan <kimta2hwan@gmail.com>
2022-11-13 14:48:58 +0900

    feat: 구입 안내 출력 기능 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class OutputView {
+
+    private static final String PURCHASE_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static void printPurchaseGuideMessage() {
+        System.out.println(PURCHASE_GUIDE_MESSAGE);
+    }
+}
