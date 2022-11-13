364f03d9ac39ba9973634b6e1f9b001b57226909
taeyun-work <wmfajr1357@gmail.com>
2022-11-13 18:29:04 +0900

    refactor: 직접 사용하던 숫자들을 상수 클래스 변수로 수정

--- src/main/java/lotto/domain/Buyer.java
+++ src/main/java/lotto/domain/Buyer.java
@@ -3,11 +3,13 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import lotto.Constants.NUMBER;
+import lotto.Ranking;
 
 public class Buyer {
-    private int totalPurchaseAmout = 0;
-    private int lottoPurchasedCount = 0;
-    private long totalWinningPrize = 0;
+    private int totalPurchaseAmout = NUMBER.ZERO;
+    private int lottoPurchasedCount = NUMBER.ZERO;
+    private long totalWinningPrize = NUMBER.ZERO;
     private List<Lotto> lottoPurchased;
     private HashMap<String, Integer> winningSummary;
 
@@ -17,12 +19,12 @@ public class Buyer {
         this.lottoPurchased = new ArrayList<Lotto>();
         this.winningSummary = new HashMap<String, Integer>() {
             {
-                put("1ST", 0);
-                put("2ND", 0);
-                put("3RD", 0);
-                put("4TH", 0);
-                put("5TH", 0);
-                put("LOSE", 0);
+                put(Ranking.FIRST.getLabel(), NUMBER.ZERO);
+                put(Ranking.SECOND.getLabel(), NUMBER.ZERO);
+                put(Ranking.THIRD.getLabel(), NUMBER.ZERO);
+                put(Ranking.FOURTH.getLabel(), NUMBER.ZERO);
+                put(Ranking.FIFTH.getLabel(), NUMBER.ZERO);
+                put(Ranking.LOSE.getLabel(), NUMBER.ZERO);
             }
         };
     }
@@ -33,7 +35,7 @@ public class Buyer {
 
     public static void setWinningSummary(Buyer buyer, String Ranking) {
         int prevCount = buyer.winningSummary.get(Ranking).intValue();
-        buyer.winningSummary.put(Ranking, prevCount + 1);
+        buyer.winningSummary.put(Ranking, prevCount + NUMBER.ONE);
     }
 
     public void setTotalPurchaseAmout(int totalPurchaseAmout) {
