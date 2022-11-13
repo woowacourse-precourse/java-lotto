3cf1f2aea957f2a0adca70a8a2b28dd70e331b2d
Woojin Lee <dhkep03@gmail.com>
2022-11-14 00:56:46 +0900

    feat(Account): 결과 저장 상태 추가

--- src/main/java/lotto/domain/Account.java
+++ src/main/java/lotto/domain/Account.java
@@ -1,4 +1,21 @@
 package lotto.domain;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import lotto.constant.WinningResult;
+
 public class Account {
+    private Map<WinningResult, Integer> results = new LinkedHashMap();
+    private int count;
+    private int winningAmount;
+
+    public Account() {
+        this.results.put(WinningResult.RANK_NONE, 0);
+        this.results.put(WinningResult.RANK_5, 0);
+        this.results.put(WinningResult.RANK_4, 0);
+        this.results.put(WinningResult.RANK_3, 0);
+        this.results.put(WinningResult.RANK_2, 0);
+        this.results.put(WinningResult.RANK_1, 0);
+        this.winningAmount = 0;
+    }
 }
