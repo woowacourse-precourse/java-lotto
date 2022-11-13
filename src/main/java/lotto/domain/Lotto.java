6052739affb792cb16a383e8e3849ad18825ea4c
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-13 11:36:24 +0900

    feat(Lotto): 로또 숫자 리스트 검증 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,23 +1,54 @@
 package lotto.domain;
 
+import static lotto.domain.message.ErrorMessageConstants.*;
+import static lotto.restrict.RestrictConstants.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+	public Lotto(List<Integer> numbers) {
+		validate(numbers);
+		this.numbers = numbers;
+	}
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
+		if (numbers.size() != LIMIT_SIZE) {
+			throw new IllegalArgumentException(String.format(SIZE_ERROR_MESSAGE_FORMAT, LIMIT_SIZE));
+		}
+		if (checkRangeNumbers(numbers)) {
+			throw new IllegalArgumentException(String.format(RANGE_ERROR_MESSAGE_FORMAT, MIN_NUMBER, MAX_NUMBER));
+		}
+		if (checkDuplicateNumbers(numbers)) {
+			throw new IllegalArgumentException(String.format(DUPLICATION_ERROR_MESSAGE));
+		}
+	}
 
-    // TODO: 추가 기능 구현
+	// TODO: 추가 기능 구현
 	public List<Integer> getNumbers() {
 		return numbers;
+	}
+
+	private boolean checkRangeNumbers(List<Integer> numbers) {
+		for (int number : numbers) {
+			if (number < MIN_NUMBER || MAX_NUMBER < number) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean checkDuplicateNumbers(List<Integer> numbers) {
+		List<Integer> duplicateNumbers = new ArrayList<>();
+
+		for (int number : numbers) {
+			if (duplicateNumbers.contains(number)) {
+				return true;
+			}
+			duplicateNumbers.add(number);
+		}
+		return false;
 	}
 }
---
+++ src/main/java/lotto/domain/message/ErrorMessageConstants.java
@@ -0,0 +1,10 @@
+package lotto.domain.message;
+
+public interface ErrorMessageConstants {
+
+	String USER_INPUT_ERROR_MESSAGE = "[ERROR] 잘못된 입력입니다.\n";
+	String RANGE_ERROR_MESSAGE_FORMAT = "[ERROR] 로또 번호는 %d부터 %d 사이의 숫자여야 합니다.\n";
+	String UNIT_ERROR_MESSAGE_FORMAT = "[ERROR] 구입금액은 %d 단위여야 합니다.\n";
+	String DUPLICATION_ERROR_MESSAGE = "[ERROR] 중복된 로또 번호입니다.\n";
+	String SIZE_ERROR_MESSAGE_FORMAT = "[ERROR] 번호는 %d개만 입력 가능합니다.\n";
+}
