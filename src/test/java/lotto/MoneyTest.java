a1857e417cee0d4e68ff13a38e6991aff6ff0df3
saewoo1 <wooozooo1123@gmail.com>
2022-11-13 15:21:54 +0900

    test(MoneyTest): 카운트된 로또의 개수 검사 테스트코드

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -12,9 +12,32 @@ class MoneyTest {
   @DisplayName("들어온 String type의 Money가 숫자로만 이루어져 있는지 검사")
   @Test
   void isOnlyDigitsMoney() {
-    assertThatThrownBy(() -> Money.checkMoneyIsDigit("12a4"))
+    String notDigit = "12a4";
+    assertThatThrownBy(() -> Money.checkMoneyIsDigit(notDigit))
             .isInstanceOf(IllegalArgumentException.class);
+  }
 
+  @DisplayName("1000원 단위로 나누어지는지 검사")
+  @Test
+  void isDividableNumber() {
+    String test1 = "1400";
+    String test2 = "300";
+    String test3 = "1000";
+    String test4 = "140000";
+    assertThatThrownBy(() -> Money.countLotto(test1))
+            .isInstanceOf(IllegalArgumentException.class);
+    assertThatThrownBy(() -> Money.countLotto(test2))
+            .isInstanceOf(IllegalArgumentException.class);
+    assertThatThrownBy(() -> Money.countLotto(test3)) // 통과 케이스
+            .isInstanceOf(IllegalArgumentException.class);
+    assertThatThrownBy(() -> Money.countLotto(test4))
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @DisplayName("카운트 된 로또의 개수")
+  @Test
+  void lottoCount() {
+    assertThat(Money.countLotto("4000")).isEqualTo(4);
   }
 
   @Test
