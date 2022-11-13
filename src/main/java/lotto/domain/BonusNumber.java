53eb0b4b44ec363c290b2eb6c6f65338d6debd4e
csj <koreaxman@naver.com>
2022-11-13 15:44:56 +0900

    feat: create range of numbers entered exception

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -2,8 +2,22 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class BonusNumber {
-    public void inputBonusNumber() {
-        String input = Console.readLine();
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
+    public int inputBonusNumber() {
+        int input = Integer.parseInt(Console.readLine());
+        validateNumber(input);
+        return input;
     }
+
+    private void validateNumber(int number) {
+        if ((number < MIN_NUMBER) || (number > MAX_NUMBER)) {
+            throw new IllegalArgumentException("[ERROR] 입력된 로또 보너스 번호가 1 ~ 45 사이가 아닙니다.");
+        }
+    }
+
 }
