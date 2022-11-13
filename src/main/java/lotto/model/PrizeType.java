c0c4aaaf798b914842a4dfa120e050745a83a3a7
Kid-Chang <pch3310@gmail.com>
2022-11-14 04:50:05 +0900

    feat(prizeType): 수상의 타입을 확인하는 메서드 구현

--- src/main/java/lotto/model/PrizeType.java
+++ src/main/java/lotto/model/PrizeType.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import java.util.Arrays;
+
 public enum PrizeType {
     PRIZE1(6, 0, 2_000_000_000),
     PRIZE2(5, 1, 30_000_000),
@@ -15,5 +17,13 @@ public enum PrizeType {
         this.matchCount = matchCount;
         this.bonusCount = bonusCount;
         this.rewardMoney = rewardMoney;
+    }
+
+    public static PrizeType getRank(int matchCount, int bonusCount) {
+        return Arrays.stream(values())
+            .filter(prize -> prize.matchCount == matchCount)
+            .filter(prize -> prize.bonusCount == bonusCount)
+            .findFirst()
+            .orElse(null);
     }
 }
