2b20f224f19f46859768065e4380c5bfdeb52aa0
KimYongJun <rladydwns129@naver.com>
2022-11-13 19:18:25 +0900

    feat: 입력 금액의 유효성을 판별하는 Money 클래스 구현

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,38 @@
+package lotto;
+
+public class Money {
+    private final int PRICE_OF_LOTTO = 1_000;
+    private int money;
+
+    public Money(String input) {
+        validate(input);
+        this.money = convertToMoney(input);
+    }
+
+    private void validate(String input) {
+        int inputMoney = 0;
+        try {
+            inputMoney = Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException();
+        }
+        canDivideByUnit(inputMoney);
+        isEnoughMoney(inputMoney);
+    }
+
+    private void canDivideByUnit(int input) {
+        if (input % PRICE_OF_LOTTO != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isEnoughMoney(int input) {
+        if (input < PRICE_OF_LOTTO) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int convertToMoney(String input) {
+        return Integer.parseInt(input);
+    }
+}
