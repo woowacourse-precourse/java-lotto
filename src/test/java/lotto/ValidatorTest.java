c6d409187148008c04b20029efd339b9ed7ea029
taeyun-work <wmfajr1357@gmail.com>
2022-11-13 20:17:26 +0900

    test: Validator.bonusNumber 유닛테스트 작성

--- src/test/java/lotto/ValidatorTest.java
+++ src/test/java/lotto/ValidatorTest.java
@@ -1,13 +1,24 @@
 package lotto;
 
+import java.util.Arrays;
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static lotto.domain.Validator.payment;
+import static lotto.domain.Validator.bonusNumber;
 import static lotto.domain.Validator.winningNumberDuplicate;
 import static lotto.domain.Validator.winningNumberFormat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ValidatorTest {
+
+    @DisplayName("구매 금액을 형식에 맞게 입력하지 않으면 예외가 발생한다")
+    @Test
+    void paymentValidation() {
+        assertThatThrownBy(() -> payment("123456"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
     @DisplayName("당첨 번호를 형식에 맞게 입력하지 않으면 예외가 발생한다")
     @Test
     void winningNumberFormatValidation() {
@@ -21,4 +32,21 @@ public class ValidatorTest {
         assertThatThrownBy(() -> winningNumberDuplicate(new Integer[] {1,2,3,4,5,5}))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("보너스 번호를 형식에 맞게 입력하지 않으면 예외가 발생한다")
+    @Test
+    void bonusNumberValidation() {
+        assertThatThrownBy(() -> bonusNumber("100"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 당첨번호 내에 존재하면 예외가 발생한다")
+    @Test
+    void bonusNumberValidation_2() {
+        Lotto.setWinningNumbers(Arrays.asList(1,2,3,4,5,6));
+        assertThatThrownBy(() -> bonusNumber("6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
