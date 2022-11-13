e5630d0e1d853a60c3a3e4f7d68351e75b4fcdb5
jaewoo <wjdwodn0722@naver.com>
2022-11-13 21:10:07 +0900

    feat : 복권번호를 확인하여 등수를 계산하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,5 +17,27 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public int confirmNumbers(List<Integer> numbers) {
+        int sameNumberCount = 0;
+        int bonusNumber = 0;
 
+        for (Integer number : this.numbers) {
+            if (numbers.contains(number)) {
+                sameNumberCount++;
+            }
+        }
+
+        if (this.numbers.contains(numbers.get(6))) {
+            bonusNumber = 1;
+        }
+
+        return defineRank(sameNumberCount, bonusNumber);
+    }
+
+    private int defineRank(int sameNumberCount, int bonusNumber) {
+        if (sameNumberCount == 6) {
+            return 7 - sameNumberCount + bonusNumber;
+        }
+        return 7 - sameNumberCount;
+    }
 }
