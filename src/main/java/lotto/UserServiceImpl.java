5b62bc4cec5dddd8106fe3db0e3ece21449a9be5
qkrdmswl <eunji303@naver.com>
2022-11-14 08:37:05 +0900

    feat : get user lotto numbers and payment

--- src/main/java/lotto/UserServiceImpl.java
+++ src/main/java/lotto/UserServiceImpl.java
@@ -7,30 +7,29 @@ public class UserServiceImpl implements UserService{
     private static int cntOfLotto = 0;
     private static List<Integer> userNumList;
     private static int bonus;
-    private CheckInputIllegal checkInputIllegal;
+    private CheckInputIllegal checkInputIllegal = new CheckInputIllegal();
 
     @Override
     public int getCntOfLotto(int userPayment) {
-        checkInputIllegal.checkUserPayIllegal(userPayment);
         cntOfLotto = userPayment / 1000;
         return cntOfLotto;
     }
 
     @Override
     public List<Integer> getUserNums(String userInput) {
-        checkInputIllegal.checkUserInputIllegal(userInput);
+//        checkInputIllegal.checkUserInputIllegal(userInput);
         List<String> userInputList = Arrays.asList(userInput.split(","));
         userNumList = userInputList.stream()
                 .map(string -> Integer.parseInt(string))
                 .collect(Collectors.toList());
 
-        checkInputIllegal.checkUserNumListIllegal(userNumList);
+//        checkInputIllegal.checkUserNumListIllegal(userNumList);
         return userNumList;
     }
 
     @Override
     public int getUserBonus(String userBonus) {
-        checkInputIllegal.checkUserBonusIllegal(userBonus);
+//        checkInputIllegal.checkUserBonusIllegal(userBonus);
         bonus = Integer.parseInt(userBonus);
         return bonus;
     }
