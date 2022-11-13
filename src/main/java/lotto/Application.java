9e702e76cabaa3d0570f545d564f5575a5d46048
fakerdeft <whakswp123@naver.com>
2022-11-14 03:44:07 +0900

    feat(Application): change 결과 출력 메서드 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -40,15 +40,15 @@ public class Application {
 
         System.out.println("당첨 통계");
         System.out.println("---");
-        for (Rank rank : Rank.values()) {
-            prize += rank.getPrize() * totalResult.get(0).get(i);
-            i++;
-        }
         System.out.println("3개 일치 (5,000원) - " + totalResult.get(0).get(0) + "개");
         System.out.println("4개 일치 (50,000원) - " + totalResult.get(0).get(1) + "개");
         System.out.println("5개 일치 (1,500,000원) - " + totalResult.get(0).get(2) + "개");
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + totalResult.get(0).get(3) + "개");
         System.out.println("6개 일치 (2,000,000,000원) - " + totalResult.get(0).get(4) + "개");
+        for (Rank rank : Rank.values()) {
+            prize += rank.getPrize() * totalResult.get(0).get(i);
+            i++;
+        }
         printEarningsRate(totalResult.get(1), prize);
     }
 
