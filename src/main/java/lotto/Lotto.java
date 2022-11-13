010d38293fd622da5472815f2ad7e65761b45d5b
qkrdmswl <eunji303@naver.com>
2022-11-14 01:27:48 +0900

    feat(Lotto) : make new Lotto

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Lotto {
@@ -12,9 +13,16 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또의 번호는 6개여야 합니다.");
+        } else if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또의 번호는 중복되지 않아야 합니다.");
         }
     }
 
     // TODO: 추가 기능 구현
+    public Lotto makeLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Lotto lotto = new Lotto(numbers);
+        return lotto;
+    }
 }
