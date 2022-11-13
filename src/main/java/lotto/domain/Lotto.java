f43bfa34caaa063112013836acfc500463e2498a
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-13 09:58:35 +0900

    feat(exception): 당첨 번호 입력 예외 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.exception.LottoException;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,14 +10,8 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        LottoException.validateLotto(numbers);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
     }
 
     // TODO: 추가 기능 구현
--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -5,19 +5,32 @@ import java.util.List;
 import java.util.Set;
 
 public class LottoException {
-    private static final String ERROR_MESSAGE = "[ERROR] ";
-    private static final String LENGTH_EXCEPTION_MESSAGE = "자리의 수만 가능합니다.";
+    private static final String ERROR_MESSAGE = "[ERROR] 로또 번호는 ";
+    private static final String LENGTH_EXCEPTION_MESSAGE = " 자리의 수만 가능합니다.";
+    private static final String RANGE_EXCEPTION_MESSAGE = " 사이의 숫자여야 합니다.";
+    private static final String DUPLICATE_EXCEPTION_MESSAGE = "중복이 불가능 합니다.";
 
     public static void validateLotto(List<Integer> numbers) {
+        StringBuilder message = new StringBuilder(ERROR_MESSAGE);
 
+        if(!isAccurateSize(numbers)) {
+            message.append(6).append(LENGTH_EXCEPTION_MESSAGE);
+            throw new IllegalArgumentException(message.toString());
+        }else if(hasDuplication(numbers)) {
+            message.append(DUPLICATE_EXCEPTION_MESSAGE);
+            throw new IllegalArgumentException(message.toString());
+        }else if(isAccurateRange(numbers)) {
+            message.append(1).append("부터 ").append(45).append(RANGE_EXCEPTION_MESSAGE);
+            throw new IllegalArgumentException(message.toString());
+        }
     }
 
-    private boolean isAccurateSize(List<Integer> numbers) {
+    private static boolean isAccurateSize(List<Integer> numbers) {
         if(numbers.size() != 6) return false;
         return true;
     }
 
-    private boolean hasDuplication(List<Integer> numbers) {
+    private static boolean hasDuplication(List<Integer> numbers) {
         Set<Integer> stack = new HashSet<>();
         for (Integer number : numbers) {
             if(!stack.add(number)) return true;
@@ -25,7 +38,7 @@ public class LottoException {
         return false;
     }
 
-    private boolean isAccurateRange(List<Integer> numbers) {
+    private static boolean isAccurateRange(List<Integer> numbers) {
         for (Integer number : numbers) {
             if(!(1 <= number && number <= 45)) return false;
         }
