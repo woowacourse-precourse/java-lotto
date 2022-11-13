5ea94ac5bec7a949413c2acde013e0cbfcceb401
woo-chang <clllickme@naver.com>
2022-11-13 19:03:35 +0900

    refactor(Rank): 당첨되지 않은 경우 필터링 로직 추가, Comparator 메서드 활용

--- src/main/java/lotto/entity/Result.java
+++ src/main/java/lotto/entity/Result.java
@@ -4,7 +4,6 @@ import static lotto.LottoApplication.PRICE;
 
 import java.util.Comparator;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.StringJoiner;
 
 public class Result {
@@ -17,21 +16,21 @@ public class Result {
 
     public double calculateRateOfReturn() {
         double totalPrize = getTotalPrize();
-        int purchase = getPurchase();
+        long purchase = getPurchase();
         return totalPrize / purchase * 100;
     }
 
     private double getTotalPrize() {
         return result.entrySet()
             .stream()
-            .mapToInt(entry -> entry.getKey().getPrize() * entry.getValue())
+            .mapToLong(entry -> entry.getKey().getPrize() * entry.getValue())
             .sum();
     }
 
-    private int getPurchase() {
-        int count = result.values()
+    private long getPurchase() {
+        long count = result.values()
             .stream()
-            .mapToInt(i -> i)
+            .mapToLong(i -> i)
             .sum();
         return count * PRICE;
     }
@@ -41,14 +40,10 @@ public class Result {
         StringJoiner output = new StringJoiner("\n");
         result.entrySet()
             .stream()
-            .sorted(new Comparator<Entry<Rank, Integer>>() {
-                @Override
-                public int compare(Entry<Rank, Integer> o1, Entry<Rank, Integer> o2) {
-                    return o1.getKey().getPrize() - o2.getKey().getPrize();
-                }
-            })
+            .filter(entry -> !entry.getKey().equals(Rank.NONE))
+            .sorted(Comparator.comparingLong(o -> o.getKey().getPrize()))
             .forEach(
-                entry -> output.add(entry.getKey().toString() + " - " + entry.getValue() + "개"));
+                entry -> output.add(entry.getKey() + " - " + entry.getValue() + "개"));
         return output.toString();
     }
 
