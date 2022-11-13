e67a858800ceb0d5892755c65e522cdf0cf34b76
Cyma-s <ssol0319@naver.com>
2022-11-13 17:49:53 +0900

    feat(LottoInputView): add function that gets bonus number input and verifies input

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import static lotto.model.InputErrorMessage.BONUS_NUMBER_IS_NOT_NUMBER;
 import static lotto.model.InputErrorMessage.INVALID_FORMAT_MONEY;
 import static lotto.model.InputErrorMessage.INVALID_LOTTO_NUMBERS;
 import static lotto.model.InputErrorMessage.MONEY_IS_NOT_DIVIDED;
@@ -11,6 +12,7 @@ import lotto.model.Constant;
 public class LottoInputView {
     private final static String INPUT_MONEY_OF_LOTTO = "구입금액을 입력해 주세요.";
     private final static String INPUT_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
+    private final static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public String getLottoAmount() {
         System.out.println(INPUT_MONEY_OF_LOTTO);
@@ -27,6 +29,13 @@ public class LottoInputView {
         return lottoNumbers;
     }
 
+    public String getBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER);
+        String bonusNumber = Console.readLine();
+        validateBonusIsNumber(bonusNumber);
+        return bonusNumber;
+    }
+
     public void validateMoney(String uncheckedMoney) {
         if(!isNumberString(uncheckedMoney)) {
             throw new IllegalArgumentException(INVALID_FORMAT_MONEY.getErrorMessage());
@@ -34,6 +43,9 @@ public class LottoInputView {
     }
 
     public boolean isNumberString(String uncheckedInput) {
+        if(uncheckedInput.isBlank()) {
+            return false;
+        }
         for (int i = 0; i < uncheckedInput.length(); i++) {
             char uncheckedCharacter = uncheckedInput.charAt(i);
             if(!Character.isDigit(uncheckedCharacter)) {
@@ -55,6 +67,12 @@ public class LottoInputView {
         boolean isNumber = isNumberString(uncheckedNumbers);
         if(!isNumber) {
             throw new IllegalArgumentException(INVALID_LOTTO_NUMBERS.getErrorMessage());
+        }
+    }
+
+    public void validateBonusIsNumber(String uncheckedInput) {
+        if(!isNumberString(uncheckedInput)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_IS_NOT_NUMBER.getErrorMessage());
         }
     }
 }
