9d87b71ea3cfb48b20fa2bc94a475c363ad21748
cookienc <kim76744@naver.com>
2022-11-13 19:53:27 +0900

    feat(model): Result의 당첨번호와 보너스 번호 검증 메서드 추가

--- src/main/java/lotto/model/Result.java
+++ src/main/java/lotto/model/Result.java
@@ -1,11 +1,17 @@
 package lotto.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static java.lang.Math.toIntExact;
 
 public class Result {
+    public static final String ENTER_RIGHT_SIZE_NUMBER = "[ERROR] 6개의 숫자를 입력해주세요.";
+    public static final String ENTER_UNIQUE_NUMBER = "[ERROR] 중복된 숫자를 입력하실 수 없습니다.";
+    public static final String ENTER_UNIQUE_BONUS_NUMBER = "[ERROR] 보너스 숫자는 당첨 번호와 중복될 수 없습니다.";
+
     private List<Integer> winningNumbers = new ArrayList<>();
     private int bonusNumber;
 
@@ -18,7 +24,41 @@ public class Result {
     }
 
     public static Result of(List<Integer> winningNumbers, int bonusNumber) {
+        validateWinningNumbers(winningNumbers);
+        validateBonusNumber(winningNumbers, bonusNumber);
         return new Result(winningNumbers, bonusNumber);
+    }
+
+    private static void validateWinningNumbers(List<Integer> winningNumbers) {
+        validateSize(winningNumbers);
+        validateDuplicateNumbers(winningNumbers);
+    }
+
+    private static void validateSize(List<Integer> numbers) {
+        if (isDifferentSize(numbers)) {
+            throw new IllegalArgumentException(ENTER_RIGHT_SIZE_NUMBER);
+        }
+    }
+
+    private static boolean isDifferentSize(List<Integer> numbers) {
+        return numbers.size() != 6;
+    }
+
+    private static void validateDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (hasDuplicateNumbers(uniqueNumbers)) {
+            throw new IllegalArgumentException(ENTER_UNIQUE_NUMBER);
+        }
+    }
+
+    private static boolean hasDuplicateNumbers(Set<Integer> uniqueNumbers) {
+        return uniqueNumbers.size() != 6;
+    }
+
+    private static void validateBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ENTER_UNIQUE_BONUS_NUMBER);
+        }
     }
 
     public Prize compare(Lotto lotto) {
--- src/test/java/lotto/model/ResultTest.java
+++ src/test/java/lotto/model/ResultTest.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -9,6 +10,8 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("Result에 있는 메서드를 테스트한다.")
 class ResultTest {
@@ -35,5 +38,30 @@ class ResultTest {
 
     private static Lotto createLotto() {
         return new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @DisplayName("Result가 중복된 숫자를 가지면 오류가 발생한다.")
+    @Test
+    void createWhenHaveDuplicateNumber() {
+        assertThatThrownBy(() -> Result.of(List.of(1, 2, 2, 3, 4, 5), 6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Result의 보너스 숫자가 당첨 번호와 겹치면 오류가 발생한다.")
+    @Test
+    void createWhenDuplicateBonusNumber() {
+        assertThatThrownBy(() -> Result.of(List.of(1, 2, 3, 4, 5, 6), 3))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Result의 당첨 번호의 숫자가 6과 다르면 오류가 발생한다.")
+    @Test
+    void createWhenWinningNumbersHasDifferentSize() {
+        assertAll(
+                () -> assertThatThrownBy(() -> Result.of(List.of(1, 2, 3, 4, 5, 6, 7), 3))
+                        .isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(() -> Result.of(List.of(1, 2, 3, 4, 5), 3))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 }
