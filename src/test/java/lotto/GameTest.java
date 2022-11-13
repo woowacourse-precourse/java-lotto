4afd81f660784d3bffa24de778f7c99caa8b1077
watterricecake <fruturum@nate.com>
2022-11-13 17:21:22 +0900

    test: test makeWinningTest method

--- src/test/java/lotto/GameTest.java
+++ src/test/java/lotto/GameTest.java
@@ -1,4 +1,24 @@
 package lotto;
 
-public class GameTest {
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static lotto.Game.makeWinning;
+import static lotto.Game.winning;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameTest extends NsTest {
+    @Test
+    void makeWinningTest(){
+        makeWinning("1,2,3,4,5,6");
+        assertThat(winning).isEqualTo(
+                List.of(1,2,3,4,5,6)
+        );
+    }
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
 }
