18fb6e1c3a5948dcf2419c6f1b56b4bcc545fc45
KimSangHyeok <tkdgur0906@naver.com>
2022-11-13 16:57:53 +0900

    Test(lotto): lottoGenerator 테스트 beforeEach 사용

--- src/test/java/lotto/domain/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import lotto.Model.Lotto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,23 +9,25 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 class LottoGeneratorTest {
+    LottoGenerator lottoGenerator;
+    @BeforeEach
+    void setUp(){
+        lottoGenerator = new LottoGenerator();
+    }
     @Test
     void 로또묶음_개수_테스트() {
-        LottoGenerator lottoGenerator = new LottoGenerator();
         List<Lotto> lottoBundle = lottoGenerator.generateLottoBundle(3000);
         assertThat(lottoBundle.size()).isEqualTo(3);
     }
 
     @Test
     void 천원단위_오류_테스트() {
-        LottoGenerator lottoGenerator = new LottoGenerator();
         assertThatThrownBy(() -> lottoGenerator.generateLottoBundle(1300))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 영원이하_오류_테스트(){
-        LottoGenerator lottoGenerator = new LottoGenerator();
         assertThatThrownBy(() -> lottoGenerator.generateLottoBundle(0))
                 .isInstanceOf(IllegalArgumentException.class);
 
