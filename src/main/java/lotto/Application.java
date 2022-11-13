6e469c8bea2fadd6991cecf45f7a04f3e3c59abf
NoseSSG <dmlcks1998@naver.com>
2022-11-13 23:40:09 +0900

    [REFACTOR] 이해를돕기 위한 주석 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -84,12 +84,14 @@ public class Application {
         return bonus;
     }
     static void check_bonus(Lotto lotto,int bonus){
+        //보너스 번호가 당첨번호와 겹치는지 확인
         lotto.check_bonus(bonus);
     }
     static boolean check_second(List<Integer>lotto_num, int bonus){
         if(lotto_num.contains(bonus)) return true;
         return false;
     }
+
     static float check_same_num(Lotto lotto,List<Integer> lotto_num,int bonus){
         float result;
         result = lotto.check_rank(lotto_num);
@@ -98,11 +100,13 @@ public class Application {
         }
         return result;
     }
+
     static void check_result(Lotto lotto, List<List<Integer>> lotto_nums, int bonus,int money){
         List<Integer> count_rank = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0));
         float result;
         int price = 0;
         List<Integer> temp;
+
         for (int i = 0;i < lotto_nums.size();i++){
             temp = lotto_nums.get(i);
             result = check_same_num(lotto,temp,bonus);
@@ -116,6 +120,7 @@ public class Application {
     static List<Integer> count_result (List<Integer> count_rank, float result){
         int index_temp;
         int count_temp;
+
         for(RankType r: RankType.values()){
             if(r.getNum()==result){
                 index_temp = r.getCheck();
@@ -138,8 +143,10 @@ public class Application {
         String output_temp;
         int index_temp;
         int count_temp;
+
         System.out.println("당첨 통계");
         System.out.println("---");
+
         for(RankType r : RankType.values()){
             output_temp = r.getResult();
             index_temp = r.getCheck();
@@ -161,6 +168,7 @@ public class Application {
         List<Integer> winning_num;
         List<Integer> count_rank;
         int bonus;
+
         try {
             money = check_money();
             chance = money2chance(money);
