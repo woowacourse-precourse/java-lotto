cc79e82146b7181f96793da39c1e98d3ccef3201
jjdone <ehdlfaos@gmail.com>
2022-11-13 14:40:18 +0900

    feat: 구매자 로또 설정 구현

--- src/main/java/lotto/Buyer.java
+++ src/main/java/lotto/Buyer.java
@@ -1,8 +1,51 @@
 package lotto;
 
+import java.util.*;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Buyer {
 
-    public Buyer() {}
+    private ArrayList<ArrayList<Integer>> listOfNumbers;
 
+    public Buyer() {
+    }
 
+    public void setListOfLotto(String buyAmount) {
+        int amount = stringToInt(buyAmount);
+        int count = countCalculation(amount);
+        listOfNumbers = new ArrayList<>();
+        for(int i=0 ; i<count; i++){
+            addLottoNumbers();
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> getListOfNumbers() {
+        return listOfNumbers;
+    }
+
+    private void addLottoNumbers() {
+        ArrayList<Integer> lotto = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        listOfNumbers.add(lotto);
+    }
+
+    private int stringToInt(String buyAmount) {
+        int amount = Integer.parseInt(buyAmount);
+        return amount;
+    }
+
+    private int countCalculation(int amount) {
+        if(!isCollectedAmount(amount)){
+            throw new IllegalArgumentException();
+        }
+        return amount/1000;
+    }
+
+    private boolean isCollectedAmount(int amount) {
+        if(amount % 1000 == 0){
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -1,14 +1,14 @@
 package lotto;
 
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class LottoStore {
     private static final String BUY_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String BOUGHT_NUMBER_MESSAGE = "개를 구매했습니다.";
+    private static final String BOUGHT_NUMBER_MESSAGE = "%d개를 구매했습니다.";
     private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String WINNING_STATISTICS = "당첨 통계\n---\n" +
@@ -16,21 +16,40 @@ public class LottoStore {
             "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n" + "6개 일치 (2,000,000,000원) - %d개";
     private static final String RATE_OF_RETURN = "총 수익률은 %f%입니다.";
 
+    private List<ArrayList<Integer>> buyer;
+
     public LottoStore() {}
 
     public void buyAndDraw() {
-        buyMessage();
+        buyLotto();
+        Lotto lotto = new Lotto(inputWinningNumbers());
+    }
 
+    private void buyLotto() {
+        buyMessage();
+        String buyAmount = Console.readLine();
+        Buyer lottoLists = new Buyer();
+        lottoLists.setListOfLotto(buyAmount);
+        buyer = lottoLists.getListOfNumbers();
+        printBoughtLotto();
+    }
+
+    private void printBoughtLotto() {
+        System.out.printf(BOUGHT_NUMBER_MESSAGE, buyer.size());
+        for(int i=0 ; i<buyer.size() ; i++){
+            System.out.println(buyer.get(i));
+        }
     }
 
     private void buyMessage() {
         System.out.println(BUY_MESSAGE);
     }
 
-    private void inputWinningNumbers() {
+    private List<Integer> inputWinningNumbers() {
+        System.out.println(WINNING_NUMBERS_MESSAGE);
         String input = Console.readLine();
         List<Integer> winningNumbers = stringToIntegerList(input);
-        Lotto lotto = new Lotto(winningNumbers);
+        return winningNumbers;
     }
 
     private List<Integer> stringToIntegerList(String string) {
