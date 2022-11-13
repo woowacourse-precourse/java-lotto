cf07aa0dedd05e237c312e7b72f17a1339fb5459
seonghoo1217 <seonghoo1217@naver.com>
2022-11-13 20:10:06 +0900

    refactor(PickNumber) : 복권 발행 후 복권 리스트를 추후 비교하기 위해 이중리스트에 저장
    - 추후 비교로직을 위해 이중리스트에 발행한 복권 리스트를 저장함
    - 요구사항 진행도를 추가적으로 표시
    
    Refactor(commit_id) : #631a870

--- src/main/java/lotto/PickNumber.java
+++ src/main/java/lotto/PickNumber.java
@@ -2,15 +2,21 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PickNumber {
     public static void pick_number_of_lotto(int pages_of_lotto){
         StringBuilder sb=new StringBuilder();
+        List<List<Integer>> purchased_lotto=new ArrayList<>();
+
         sb.append('\n').append(pages_of_lotto+"개를 구매했습니다.").append('\n');
+
         for (int i=0;i<pages_of_lotto;i++){
             List<Integer> page_of_lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            purchased_lotto.add(page_of_lotto);
             sb.append(page_of_lotto).append('\n');
         }
+
     }
 }
