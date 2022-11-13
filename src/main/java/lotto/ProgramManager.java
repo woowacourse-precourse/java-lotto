8b76dcdbf6831d816e797184df587e62e7e445f1
euna <christinejung10@daum.net>
2022-11-14 06:47:23 +0900

    feat(ProgramManager): add methods to print lotto statistics

--- src/main/java/lotto/ProgramManager.java
+++ src/main/java/lotto/ProgramManager.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 // 로또 게임 진행자
@@ -13,7 +14,15 @@ public class ProgramManager {
     private static final String ASK_PREDICT_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String ASK_PREDICT_LOTTO_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    private static final String LOTTO_STATISTICS_MESSAGE = "당첨 통계\n---";
+    private static final String LOTTO_STATISTICS_MESSAGE_TITLE = "당첨 통계\n---\n";
+    private static final String LOTTO_STATISTICS_MESSAGE_BODY_SAME_NUMBER_PRE = "개 일치";
+    private static final String LOTTO_STATISTICS_MESSAGE_BODY_SAME_NUMBER_BONUS_BALL = ", 보너스 볼 일치";
+    private static final String LOTTO_STATISTICS_MESSAGE_BODY_REWARD_PRE = " (";
+    private static final String LOTTO_STATISTICS_MESSAGE_BODY_REWARD_POST = "원) - ";
+    private static final String LOTTO_STATISTICS_MESSAGE_BODY_REWARD_NUMBER = "개\n";
+
+    private static final String LOTTO_STATISTICS_MESSAGE_BODY_REVENUE_RATE_PRE = "총 수익률은 ";
+    private static final String LOTTO_STATISTICS_MESSAGE_BODY_REVENUE_RATE_POST = "%입니다.";
 
     private static int price;
     private static int numberOfLotto;
@@ -67,7 +76,37 @@ public class ProgramManager {
     }
 
     private static void printLottoStatistics() {
-        System.out.println(LOTTO_STATISTICS_MESSAGE);
+        String lottoStatisticsMessage
+                = LOTTO_STATISTICS_MESSAGE_TITLE +
+                getLottoRankResultMessage() +
+                getLottoRevenueRateMessage();
+        System.out.println(lottoStatisticsMessage);
+    }
+
+    private static String getLottoRevenueRateMessage() {
+        return LOTTO_STATISTICS_MESSAGE_BODY_REVENUE_RATE_PRE + String.format("%.1f", lottoRevenueRate) + LOTTO_STATISTICS_MESSAGE_BODY_REVENUE_RATE_POST;
+    }
+
+    private static String getLottoRankResultMessage() {
+        DecimalFormat decFormat = new DecimalFormat("###,###");
+        StringBuilder lottoRankResultMessage = new StringBuilder();
+
+        for (LottoManager.Rank rank : LottoManager.Rank.values()) {
+            lottoRankResultMessage.append(rank.getNumberOfSameLottoNumber());
+            lottoRankResultMessage.append(LOTTO_STATISTICS_MESSAGE_BODY_SAME_NUMBER_PRE);
+
+            if (rank.toString().compareTo("second") == 0) {
+                lottoRankResultMessage.append(LOTTO_STATISTICS_MESSAGE_BODY_SAME_NUMBER_BONUS_BALL);
+            }
+            lottoRankResultMessage.append(LOTTO_STATISTICS_MESSAGE_BODY_REWARD_PRE);
+
+            int rewardPrice = LottoManager.Reward.valueOf(rank.toString()).getRewardPrice();
+            lottoRankResultMessage.append(decFormat.format(rewardPrice));
+            lottoRankResultMessage.append(LOTTO_STATISTICS_MESSAGE_BODY_REWARD_POST);
+            lottoRankResultMessage.append(lottoRankResult[rank.getIndexInRankResult()]);
+            lottoRankResultMessage.append(LOTTO_STATISTICS_MESSAGE_BODY_REWARD_NUMBER);
+        }
+        return lottoRankResultMessage.toString();
     }
 
 }
