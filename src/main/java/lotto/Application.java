bb86daff1562484eaaa132396a95e666071a60ee
splitube <voicesplit@daum.net>
2022-11-14 00:51:48 +0900

    feat(Application): 로또 게임 진행 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,21 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoKiosk lottoKiosk = new LottoKiosk();
+        WinningNumberMaker winningNumberMaker = new WinningNumberMaker();
+        try{
+            lottoKiosk.insertMoney();
+            lottoKiosk.sellLotto();
+            winningNumberMaker.make();
+            winningNumberMaker.makeBonusNumber();
+            LottoChecker lottoChecker = new LottoChecker(winningNumberMaker.showWinningNumbers(),
+                    winningNumberMaker.showBonusNumber());
+            lottoChecker.insertLottos(lottoKiosk.showAllLotto());
+            lottoChecker.saveAllResult();
+            lottoChecker.printEachPrizeWinCount();
+            lottoChecker.printProfitRate();
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
