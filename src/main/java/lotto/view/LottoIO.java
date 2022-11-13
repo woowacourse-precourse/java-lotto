527611d71f3074f29778688af849ff5648fca15f
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-13 16:42:29 +0900

    fix(LottoIO): Fix print format
    Feature fixed:
    - Fix print format -> show input to console output

--- src/main/java/lotto/view/LottoIO.java
+++ src/main/java/lotto/view/LottoIO.java
@@ -18,7 +18,8 @@ public class LottoIO {
     public void inputLottoAnswer() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
-        System.out.println();
+        System.out.println(input + "\n");
+
         if (checkInvalidNumbers(input)) {
             System.out.println("[ERROR] 당첨번호는 쉼표(,)를 기준으로 6개의 숫자를 입력해 주세요.");
             throw new IllegalArgumentException();
@@ -35,7 +36,8 @@ public class LottoIO {
     public void inputBonusAnswer() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
-        System.out.println();
+        System.out.println(input + "\n");
+
         if (checkInvalidBonus(input)) {
             System.out.println("[ERROR] 보너스 번호는 1부터 45 사이의 숫자를 한 개만 입력해 주세요.");
             throw new IllegalArgumentException();
