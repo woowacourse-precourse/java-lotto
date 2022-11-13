940075422e0a68f425050d6dd3e2cb24dc1a7117
parkjungyoon <tbr06057@naver.com>
2022-11-13 14:58:51 +0900

    refactor(Domain): 랜덤 숫자 개수 상수로 변경

--- src/main/java/lotto/domain/RandomNumbers.java
+++ src/main/java/lotto/domain/RandomNumbers.java
@@ -6,8 +6,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class RandomNumbers {
+    private final static int LOTTO_COUNT = 6;
+
     public List<Integer> generateRandomNumbers() {
-        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, LOTTO_COUNT);
         Collections.sort(lottoNumbers);
         return lottoNumbers;
     }
