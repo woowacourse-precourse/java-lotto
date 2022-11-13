08be11ef3d3ce51927b398e4e37cbb1118a173aa
정핸모 <gusah009@naver.com>
2022-11-13 22:39:22 +0900

    refactor: Player 클래스를 만들어서 Player의 역할은 Player에게 위임

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,37 +1,32 @@
 package lotto.service;
 
-import static lotto.domain.Lotto.END_LOTTO_NUMBER;
-import static lotto.domain.Lotto.LOTTO_NUMBER_COUNT;
-import static lotto.domain.Lotto.LOTTO_PRICE;
-import static lotto.domain.Lotto.START_LOTTO_NUMBER;
 import static lotto.util.Printer.printMessage;
 import static lotto.util.Printer.printPurchaseLottosInfo;
 import static lotto.util.Printer.printResult;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import lotto.domain.Lotto;
+import lotto.domain.Player;
 import lotto.domain.Rank;
 import lotto.domain.WinningLotto;
 import lotto.repository.RankRepository;
 
 public class LottoService {
 
-    int purchasePrice;
-    List<Lotto> playerPurchaseLottos = new ArrayList<>();
+    Player player;
     WinningLotto winningLotto;
 
     public void play() {
         printPurchaseGuideMessage();
-        setPlayerPurchaseLottos(purchaseLottos());
-        printPurchaseLottosInfo(playerPurchaseLottos);
-        setWinningLotto(inputWinningLotto());
+        player = Player.purchase();
+        printPurchaseLottosInfo(player.getPurchaseLottos());
+        winningLotto = inputWinningLotto();
         List<Rank> lottoResults = getResult();
-        printResult(lottoResults, purchasePrice);
+        printResult(lottoResults, player.getPurchasePrice());
     }
 
     private static void printPurchaseGuideMessage() {
@@ -40,15 +35,11 @@ public class LottoService {
 
     private List<Rank> getResult() {
         List<Rank> lottoResults = new ArrayList<>();
-        for (Lotto playerPurchaseLotto : playerPurchaseLottos) {
+        for (Lotto playerPurchaseLotto : player.getPurchaseLottos()) {
             RankRepository.getRank(playerPurchaseLotto, winningLotto)
                     .ifPresent(lottoResults::add);
         }
         return lottoResults;
-    }
-
-    private void setWinningLotto(WinningLotto inputWinningLotto) {
-        winningLotto = inputWinningLotto;
     }
 
     WinningLotto inputWinningLotto() {
@@ -91,41 +82,5 @@ public class LottoService {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("보너스 번호는 반드시 숫자여야 합니다.", e);
         }
-    }
-
-    void setPlayerPurchaseLottos(List<Lotto> lottos) {
-        playerPurchaseLottos.clear();
-        playerPurchaseLottos.addAll(lottos);
-    }
-
-    List<Lotto> purchaseLottos() {
-        this.purchasePrice = inputPurchasePrice();
-        if (purchasePrice % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException(String.format("로또는 %d원 단위로 나누어 떨어져야 합니다.", LOTTO_PRICE));
-        }
-        return generateLottos(purchasePrice / LOTTO_PRICE);
-    }
-
-    int inputPurchasePrice() {
-        String playerInput = Console.readLine();
-        try {
-            return Integer.parseInt(playerInput);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("로또 구매 금액은 반드시 숫자여야 합니다.", e);
-        }
-    }
-
-    List<Lotto> generateLottos(int count) {
-        List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            lottos.add(generateLotto());
-        }
-        return lottos;
-    }
-
-    Lotto generateLotto() {
-        List<Integer> uniqueNumbers = Randoms.pickUniqueNumbersInRange(START_LOTTO_NUMBER, END_LOTTO_NUMBER,
-                LOTTO_NUMBER_COUNT);
-        return new Lotto(uniqueNumbers);
     }
 }
