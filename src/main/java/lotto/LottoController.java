f115a6e5025a05149cbd4710b53ae85a51d46283
Ilchul Kim <huj0159@gmail.com>
2022-11-14 01:34:49 +0900

    feat: print all lotto

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,15 +1,33 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class LottoController {
+    private LinkedList<Lotto> lottoPool = new LinkedList<>();
 
-    public void readUserMoney(String userMoney) {
+    public int readUserMoney(String userMoney) {
         isDigit(userMoney);
         validateUserMoney(userMoney);
+
+        return Integer.parseInt(userMoney) / 1000;
+    }
+
+    public void createRandomLotto(int count) {
+        for (int i = 0; i < count; i++) {
+            List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(randomNumbers);
+            Lotto newLotto = new Lotto(randomNumbers);
+            lottoPool.add(newLotto);
+        }
+    }
+
+    public void printLottoPool() {
+        for (Lotto lotto : lottoPool) {
+            System.out.println(lotto.toString());
+        }
     }
 
     public Lotto readWinNumber(String userWinNumber) {
