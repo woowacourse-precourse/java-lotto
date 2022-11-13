6461ec3116e697ac09d9733c6521cdc5381aead1
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-11-14 03:48:33 +0900

    feat (User) : money가 숫자인지 체크, int로 저장

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -5,9 +5,19 @@ import camp.nextstep.edu.missionutils.Console;
 // enum 사용해보자
 public class User {
 
-    private final String money;
+    private final String inputMoney;
+    static int money;
 
-    public User(String money) {
-        this.money = money;
+    public User(String inputMoney) {
+        this.inputMoney = inputMoney;
+        setMoneyNumber();
+    }
+
+    private void setMoneyNumber() {
+        try {
+            money = Integer.parseInt(inputMoney);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 금액으로 숫자를 입력해주세요.");
+        }
     }
 }
