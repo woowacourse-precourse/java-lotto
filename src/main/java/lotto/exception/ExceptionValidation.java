26634771ba9f209977bd00fbceb2b8711576960b
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-13 12:25:19 +0900

    feat: 기능구현 목록 3번 3.2 입력받은 입력받은 수가 6자리가 아니거나, 중복된 수 입력, 1~45까지의 수가 아니라면 예외발생

--- src/main/java/lotto/exception/ExceptionValidation.java
+++ src/main/java/lotto/exception/ExceptionValidation.java
@@ -1,10 +1,46 @@
 package lotto.exception;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ExceptionValidation {
 
     public static void validateException(int input) {
         if ((input % 1000) != 0) {
             throw new IllegalArgumentException("[ERROR] 복권 하나는 1000원입니다. 1000원 단위로 입력해주세요.");
         }
+    }
+
+    public static void inputException(List<Integer> winnningNum) {
+        inputsizeException(winnningNum);
+        inputduplicateException(winnningNum);
+        inputrangeException(winnningNum);
+    }
+
+    private static boolean inputsizeException(List<Integer> input) {
+        if (input.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 6자리의 수를 입력해주세요.");
+        }
+        return false;
+    }
+
+    private static boolean inputduplicateException(List<Integer> input) {
+        for (int i = 0; i < input.size(); i++) {
+            int frequency = Collections.frequency(input, input.get(i));
+            if (input.size() == 6 && frequency >= 2) {
+                throw new IllegalArgumentException("[ERROR] 중복되지 않는 6자리 수를 입력해주세요.");
+            }
+        }
+        return false;
+    }
+
+    private static boolean inputrangeException(List<Integer> input) {
+        for (int i = 0; i < input.size(); i++) {
+            if (input.get(i) > 45 || input.get(i) < 1) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+        return false;
     }
 }
