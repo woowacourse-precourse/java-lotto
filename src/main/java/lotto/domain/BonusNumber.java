4b2472e73bd4736e889bbe6fb3ad1593be3cd3bb
kwYoohae <gocksdl7979@gmail.com>
2022-11-13 23:02:45 +0900

    feat: 보너스 번호는 2자리 숫자 이상일 때 맨앞에 0이 들어있는지 검증하는 기능 구현

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -10,6 +10,7 @@ public class BonusNumber {
 
     public BonusNumber(String input) {
         validateCheckNumeric(input);
+        validateFirstNumberIsNotZero(input);
         bonusNumber = Integer.parseInt(input);
     }
 
@@ -20,7 +21,14 @@ public class BonusNumber {
     private void validateCheckNumeric(String input) {
         String[] split = input.split(Constant.REGEX_INPUT);
 
-        if(!Arrays.stream(split).allMatch(number -> number.charAt(0) >= '0' && number.charAt(0) <= '9'))
+        if (!Arrays.stream(split).allMatch(number -> number.charAt(0) >= '0' && number.charAt(0) <= '9'))
             throw new IllegalArgumentException(ErrorMessage.LOTTO_INPUT_MUST_NUMBER);
+    }
+
+    private void validateFirstNumberIsNotZero(String input) {
+        String[] split = input.split(Constant.REGEX_INPUT);
+
+        if (split.length > 1 && split[0].equals("0"))
+            throw new IllegalArgumentException(ErrorMessage.FIRST_NUMBER_IS_NOT_ZERO);
     }
 }
