983bafdc6ecb00db5e51a41ead42a7d14aa3fb19
YongGwan <32181771@dankook.ac.kr>
2022-11-13 17:33:08 +0900

    feat : 로또 개수에 맞는 중복된 값이 없는 6개의 랜덤한 로또 번호 생성

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,11 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
 
 public class Game {
 
     public void play(int lottoCount) {
+
         System.out.println("게임 시작~");
         System.out.println(lottoCount);
 
+        for (int count = 0; count < lottoCount; count++) {
+
+            List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            System.out.println(lottoNumber);
+        }
     }
 }
