62eb81351d1afd133b0803811143ab63da7c065e
lee <whddlsk123@naver.com>
2022-11-14 02:10:01 +0900

    feat(createLottoResultMessage) : 각각 등수에 대한 당첨 메시지를 만들어 반환한다

--- src/main/java/lotto/ui/LottoOutput.java
+++ src/main/java/lotto/ui/LottoOutput.java
@@ -1,6 +1,9 @@
 package lotto.ui;
 
 import lotto.domain.LottoRank;
+import lotto.domain.LottoService;
+import lotto.domain.UserLotto;
+
 import java.util.List;
 
 public class LottoOutput {
@@ -25,20 +28,37 @@ public class LottoOutput {
         sb.append(Messages.OUTPUT_LOTTO_RESULT.getMessage());
 
         for(LottoRank rank : LottoRank.values()) {
-            int count = countRank(rank, lottoResult);
-
-            if(rank != LottoRank.BLANK) {
-                sb.append(rank.getMessage())
-                        .append(count);
-            }
+            sb.append(createLottoResultMessage(rank, lottoResult));
         }
 
         System.out.println(sb);
     }
 
-    public int countRank(LottoRank rank, List<LottoRank> lottoRanks) {
+    public String createLottoResultMessage(LottoRank rank, List<LottoRank> lottoResult) {
+        StringBuilder sb = new StringBuilder();
+
+        long count = countRank(rank, lottoResult);
+
+        if(rank != LottoRank.BLANK) {
+            sb.append(rank.getMessage())
+                    .append(count)
+                    .append("\n");
+        }
+        return sb.toString();
+    }
+
+    public long countRank(LottoRank rank, List<LottoRank> lottoRanks) {
         long count = lottoRanks.stream()
                 .filter(lottoRank -> lottoRank == rank)
                 .count();
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        LottoOutput output = new LottoOutput();
+        List<LottoRank> lottoRanks = List.of(LottoRank.FIFTH, LottoRank.BLANK, LottoRank.FIRST);
+
+        output.printLottoResult(lottoRanks, "45");
     }
 }
--- src/main/java/lotto/ui/Messages.java
+++ src/main/java/lotto/ui/Messages.java
@@ -5,7 +5,7 @@ public enum Messages {
     INPUT_LOTTE_NUMBER("당첨 번호를 입력해 주세요"),
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요"),
     OUTPUT_USER_LOTTO("개를 구매했습니다."),
-    OUTPUT_LOTTO_RESULT("당첨 통계\n" + "---");
+    OUTPUT_LOTTO_RESULT("당첨 통계\n" + "---\n");
 
     private final String message;
 
