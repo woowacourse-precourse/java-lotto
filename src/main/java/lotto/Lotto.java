7f2c83c0c98a4ef11fd64d2d995a306d21ed8048
hwanee__ <fkfkthth@naver.com>
2022-11-13 15:56:06 +0900

    feat: 로또 숫자 중복 검사
    6개의 숫자가 중복되는지 검사한다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 public class Lotto {
@@ -8,12 +10,21 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> checkDuplicate = new HashSet<>(numbers);
+
+        if (numbers.size() != checkDuplicate.size() ) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호 중 중복 숫자가 존재합니다.");
         }
     }
 
