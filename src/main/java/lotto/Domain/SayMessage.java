5155ba1e8fa83e4a8345dece4470a4dfe7e5044b
Polite-Jehu <werber112@kyonggi.ac.kr>
2022-11-13 22:36:25 +0900

    feat(Lotto): add enum to avoid duplication of message output function
    SayMessage

---
+++ src/main/java/lotto/Domain/SayMessage.java
@@ -0,0 +1,13 @@
+package lotto.Domain;
+
+public enum SayMessage {
+    BUY("구입금액을 입력해 주세요."),
+    ANSWER("당첨 번호를 입력해 주세요."),
+    BONUS("보너스 번호를 입력해 주세요.");
+
+    private final String sayMessage;
+    SayMessage(String sayMessage){
+        this.sayMessage = sayMessage;
+    }
+    public String getSayMessage(){ return sayMessage;}
+}
