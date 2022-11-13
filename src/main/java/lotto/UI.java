51b515e8beed47a7b54b1dbf4c6b71df8628ed94
JUNBEOM_LEE <bom1215@naver.com>
2022-11-13 15:49:08 +0900

    feat(Class UI): Answer_Number
    Add Answer_Number method to Class UI

--- src/main/java/lotto/UI.java
+++ src/main/java/lotto/UI.java
@@ -2,17 +2,18 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class UI {
 
     public Integer money(){
         String Input = Console.readLine();
-        money_validate(Input);
+        Money_Validate(Input);
         Integer money = Integer.valueOf(Input);
         return money;
     }
 
-    private void money_validate(String Input) {
+    private void Money_Validate(String Input) {
         try {
             Integer money = Integer.valueOf(Input);
             Integer remainder = money % 1000;
@@ -23,9 +24,27 @@ public class UI {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위의 숫자여야 합니다.");
         }
     }
+
+    public List<Integer> Answer_Number() {
+        List<Integer> Answer_numbers = new ArrayList();
+
+        String Input = Console.readLine();
+        String[] Input_Array = Input.split(",");
+        for (int index = 0; index < Input_Array.length; index++) {
+            number_validate(Input_Array[index]);
+            Integer number = Integer.valueOf(Input_Array[index]);
+            Answer_numbers.add(number);
+        }
+        Lotto L = new Lotto(Answer_numbers);
+        return Answer_numbers;
+    }
+
+
+
+
 //    public static void main(String[] args) {
 //        UI UI = new UI();
-//        UI.money();
+//        UI.Answer_Number();
 //    }
-
-}
+//
+//}
