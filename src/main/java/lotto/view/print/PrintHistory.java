9d0c69954d7299ef18372f1448379c33cc45b730
ykmxxi <ykmxxi97@gmail.com>
2022-11-13 15:19:30 +0900

    feat: 당첨 내역 및 수익률을 출력하는 기능 구현

---
+++ src/main/java/lotto/view/print/PrintHistory.java
@@ -0,0 +1,19 @@
+package lotto.view.print;
+
+import lotto.domain.LottoHistory;
+import lotto.domain.LottoRanking;
+
+public class PrintHistory {
+    private static final String YIELD_MESSAGE = "총 수익률은 %s%%입니다.%n";
+
+    public static void printWinningHistory(LottoHistory history) {
+        for (WinningHistoryMessage msg : WinningHistoryMessage.values()) {
+            int count = history.getWinningCount(history.getWinningHistory(), LottoRanking.valueOf(msg.getRanking()));
+            System.out.printf(msg.getMessage() + "%d개\n", count);
+        }
+    }
+
+    public static void printYield(String yield) {
+        System.out.printf(YIELD_MESSAGE, yield);
+    }
+}
