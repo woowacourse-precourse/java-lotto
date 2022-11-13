f9006368418f6c3446fc88cb48e8f49ce1b75044
lvalentine6 <ysr1227@naver.com>
2022-11-14 05:46:40 +0900

    feat : 사용자 입력 기능
    사용자게에 당첨 번호 입력 받는 기능 구현

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,16 +1,24 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Input {
+    static final int DIVIDENUMBER = 1000;
 
     public static int userInput() {
-        Output.startPrint();
+        Output.printPurchaseAmount();
         String input = Console.readLine();
         InputException.isValidNumber(input);
         InputException.isValidDivide(Integer.valueOf(input));
-        return Integer.valueOf(input);
+        return Integer.valueOf(input) / DIVIDENUMBER;
     }
 
-
+    public static String winningNumberInput() {
+        Output.printWinningNumber();
+        String input = Console.readLine();
+        InputException.isValidWinningNumberLength(input);
+        return input;
+    }
 }
--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -8,15 +8,15 @@ public class Output {
     }
 
     public static void printWinningNumber() {
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
     public static void printLottoBundle(List<Lotto> lottos, int purchaseAmount) {
         System.out.println();
         System.out.println(purchaseAmount + "개를 구매했습니다.");
-
-        for(Lotto l : lottos) {
-            System.out.println(l.getNumbers());
+        for(Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
         }
     }
 
