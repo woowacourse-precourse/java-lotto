98952743ea10500cd72c7adc7e7657d8e64203fc
이연재 <leeyj0304@gmail.com>
2022-11-13 22:47:04 +0900

    feat: add features to take inputs of the purchased price from the user

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final String INPUT_NUMBER_MESSAGE = "구입금액을 입력해 주세요.";
+
 
     public Lotto(List<Integer> numbers) {
+        this.numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         validate(numbers);
-        this.numbers = numbers;
+
     }
 
     private void validate(List<Integer> numbers) {
@@ -17,4 +23,8 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public static String UserInput() {
+        System.out.print(INPUT_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
 }
