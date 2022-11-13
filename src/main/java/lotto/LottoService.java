2721be676a518ebe4fea0cb7de4c1eb253aa57ba
code-tree <dltjdqls1200@naver.com>
2022-11-13 18:52:19 +0900

    feat: 구입 금액만큼 1000원당 1장의 로또를 생성

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -2,11 +2,28 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoService {
     private final LottoManager lottoManager = new LottoManager();
+    private final RandomGenerator randomGenerator = new RandomGenerator();
 
-    public void purchase() {
+    public List<Lotto> purchase() {
         lottoManager.printPurchaseMessage();
         Money money = new Money(Console.readLine());
+
+        return createLotteries(money);
+    }
+
+    private List<Lotto> createLotteries(Money money) {
+        int buyQuantity = money.getWon() / 1000;
+        List<Lotto> lotteries = new ArrayList<>(buyQuantity);
+
+        for (int i = 0; i < buyQuantity; i++) {
+            lotteries.add(new Lotto(randomGenerator.createRandomNumbers()));
+        }
+
+        return lotteries;
     }
 }
