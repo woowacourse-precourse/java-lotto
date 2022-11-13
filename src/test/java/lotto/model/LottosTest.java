2f2d239b57e1a849e93e0f2182dba9ceddb09072
dhkim <akdlzmf1123@gmail.com>
2022-11-13 21:06:02 +0900

    test(LottosTest): Lottos 총 상금 구하는 기능에서 테스트 추가 & 중복 제거

--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -1,29 +1,39 @@
 package lotto.model;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottosTest {
-    @Test
-    void 구매한_복권들의_상금을_모두_더하는_기능() {
-        WinningNumbers winningNumbers = new WinningNumbers(
-                List.of(1, 2, 3, 4, 5, 6), 7
+    WinningNumbers winningNumbers = new WinningNumbers(
+            List.of(1, 2, 3, 4, 5, 6), 7
+    );
+
+    @ParameterizedTest(name = "총 상금은 {1}원")
+    @MethodSource("lottosAndWinningAmount")
+    void 구매한_복권들의_상금을_모두_더하는_기능(Lottos lottos, int winningAmount) {
+        assertThat(lottos.getWinningAmount(winningNumbers)).isEqualTo(winningAmount);
+    }
+
+    static Stream<Arguments> lottosAndWinningAmount() {
+        return Stream.of(
+            Arguments.of(new Lottos(List.of(
+                    new Lotto(List.of(1,2,3,4,5,6))
+                    , new Lotto(List.of(1,3,7,4,5,6))
+                    , new Lotto(List.of(1,12,23,4,35,7))
+            )), 2_030_000_000)
+                , Arguments.of(new Lottos(List.of(
+                        new Lotto(List.of(1,2,3,13,5,6))
+                        , new Lotto(List.of(1,28,13,4,5,6))
+                        , new Lotto(List.of(21,2,3,4,7,12))
+                )), 1_555_000)
         );
-
-        Lottos lottos = new Lottos(List.of(
-                new Lotto(List.of(1,2,3,4,5,6))
-                , new Lotto(List.of(1,3,7,4,5,6))
-//                , new Lotto(List.of(1,2,3,13,5,6))
-//                , new Lotto(List.of(1,28,13,4,5,6))
-//                , new Lotto(List.of(21,2,3,4,7,12))
-                , new Lotto(List.of(1,12,23,4,35,7))
-        ));
-
-        assertThat(lottos.getWinningAmount()).isEqualTo(2_030_000_000);
     }
 }
