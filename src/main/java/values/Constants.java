c2a6f15fd414e830469efd63b734461982fe8ae2
JUN <chltmdwns96@naver.com>
2022-11-14 00:27:10 +0900

    fix : add variable INPUT_BONUS_NUMBER

--- src/main/java/values/Constants.java
+++ src/main/java/values/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
     public static class Console {
         public static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
         public static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+        public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
         public static final String OUTPUT_CNT = "개를 구매했습니다.";
         public static final String STATISTICS = "당첨 통계\n---";
     }
