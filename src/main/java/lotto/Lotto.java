e5ab7fdb3b9f0497bb93494fce26a403d5b30c49
JungWoo <rlawjddn103@naver.com>
2022-11-13 20:49:50 +0900

    feat(Lotto): Lotto 중복 된 수, 수의 범위 예외 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import lotto.ui.InputViewValidation;
+
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -7,16 +10,34 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateUniqueNumbers(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(InputViewValidation.ERROR_LOTTO_SIZE);
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validateUniqueNumbers(List<Integer> numbers){
+        HashSet<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != 6){
+            throw new IllegalArgumentException(InputViewValidation.ERROR_DUPLICATE_NUMBER);
+        }
+    }
+
+    private void validateRange(List<Integer> numbers){
+        for (int number: numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(InputViewValidation.ERROR_LOTTO_NUMBER_RANGE);
+            }
+        }
+    }
+
+
     public List<Integer> getNumbers() {
         return numbers;
     }
