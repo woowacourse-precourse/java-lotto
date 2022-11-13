613db2bd839db0b2289bd1eee02190fdb6874e4a
apptie <appti@naver.com>
2022-11-14 03:14:43 +0900

    test: MethodArgument Map 생성 기능 공통 처리

--- src/test/java/lotto/domain/argument/CalculateRevenueArgument.java
+++ src/test/java/lotto/domain/argument/CalculateRevenueArgument.java
@@ -3,6 +3,7 @@ package lotto.domain.argument;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
+import lotto.helper.util.TestArgumentUtils;
 import lotto.util.ranking.LottoRanking;
 import org.junit.jupiter.params.provider.Arguments;
 
@@ -20,22 +21,11 @@ public final class CalculateRevenueArgument {
     static Stream<Arguments> calculateRevenuePercentArgument() {
         return Stream.of(
                 Arguments.of(
-                        createArgumentMap(List.of(0, 0, 0, 0, 1)), "8000", "62.5"),
+                        TestArgumentUtils.createArgumentMap(List.of(0, 0, 0, 0, 1)), "8000", "62.5"),
                 Arguments.of(
-                        createArgumentMap(List.of(1, 0, 0, 1, 1)), "10000", "20000550.0"),
+                        TestArgumentUtils.createArgumentMap(List.of(1, 0, 0, 1, 1)), "10000", "20000550.0"),
                 Arguments.of(
-                        createArgumentMap(List.of(1, 1, 1, 1, 1)), "500000", "406311.0")
-        );
-    }
-
-    static Map<LottoRanking, Integer> createArgumentMap(List<Integer> numberOfWins) {
-        return Map.of(
-                LottoRanking.RANKING_FIRST, numberOfWins.get(RANKING_FIRST_INDEX),
-                LottoRanking.RANKING_SECOND, numberOfWins.get(RANKING_SECOND_INDEX),
-                LottoRanking.RANKING_THIRD, numberOfWins.get(RANKING_THIRD_INDEX),
-                LottoRanking.RANKING_FOURTH, numberOfWins.get(RANKING_FOURTH_INDEX),
-                LottoRanking.RANKING_FIFTH, numberOfWins.get(RANKING_FIFTH_INDEX),
-                LottoRanking.RANKING_NOTHING, 0
+                        TestArgumentUtils.createArgumentMap(List.of(1, 1, 1, 1, 1)), "500000", "406311.0")
         );
     }
 }
--- src/test/java/lotto/domain/argument/LottoStoreTestArgument.java
+++ src/test/java/lotto/domain/argument/LottoStoreTestArgument.java
@@ -3,6 +3,7 @@ package lotto.domain.argument;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
+import lotto.helper.util.TestArgumentUtils;
 import lotto.util.ranking.LottoRanking;
 import org.junit.jupiter.params.provider.Arguments;
 
@@ -23,24 +24,13 @@ public final class LottoStoreTestArgument {
                 List.of(LottoRanking.RANKING_FIFTH,
                     LottoRanking.RANKING_FOURTH,
                     LottoRanking.RANKING_THIRD), "3000",
-                createArgumentMap(List.of(0, 0, 1, 1, 1))),
+                TestArgumentUtils.createArgumentMap(List.of(0, 0, 1, 1, 1))),
             Arguments.of(
                 List.of(LottoRanking.RANKING_FIFTH,
                     LottoRanking.RANKING_FIRST,
                     LottoRanking.RANKING_THIRD,
                     LottoRanking.RANKING_FIFTH,
                     LottoRanking.RANKING_SECOND), "5000",
-                createArgumentMap(List.of(1, 1, 1, 0, 2))));
-    }
-
-    private static Map<LottoRanking, Integer> createArgumentMap(List<Integer> numberOfWins) {
-        return Map.of(
-            LottoRanking.RANKING_FIRST, numberOfWins.get(RANKING_FIRST_INDEX),
-            LottoRanking.RANKING_SECOND, numberOfWins.get(RANKING_SECOND_INDEX),
-            LottoRanking.RANKING_THIRD, numberOfWins.get(RANKING_THIRD_INDEX),
-            LottoRanking.RANKING_FOURTH, numberOfWins.get(RANKING_FOURTH_INDEX),
-            LottoRanking.RANKING_FIFTH, numberOfWins.get(RANKING_FIFTH_INDEX),
-            LottoRanking.RANKING_NOTHING, 0
-        );
+                TestArgumentUtils.createArgumentMap(List.of(1, 1, 1, 0, 2))));
     }
 }
