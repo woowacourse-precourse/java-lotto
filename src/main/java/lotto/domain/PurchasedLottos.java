bce00c57e9d21e61caa33cb30ab913ea2beffeb1
Kimhyeyun <cqqwer@gmail.com>
2022-11-13 19:18:21 +0900

    Feat: 구입 로또 번호 자동 생성 기능 구현

--- src/main/java/lotto/domain/PurchasedLottos.java
+++ src/main/java/lotto/domain/PurchasedLottos.java
@@ -1,10 +1,30 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static lotto.constant.ConstValue.*;
 
 public class PurchasedLottos {
     private final List<Lotto> purchasedLottos = new ArrayList<>();
 
+    public PurchasedLottos(int count) {
+        generateLottos(count);
+    }
 
+    private void generateLottos(int count) {
+        for (int i = 0; i < count; i++) {
+            purchasedLottos.add(generateLotto());
+        }
+    }
+
+    private Lotto generateLotto() {
+        final List<Integer> numbers = Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MINIMUM, LOTTO_NUMBER_MAXIMUM, LOTTO_SIZE);
+        Collections.unmodifiableList(numbers);
+
+        return new Lotto(numbers);
+    }
 }
