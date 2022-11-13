e8037bd69e7ad40efe51dd4c0a42249eafe98cc6
KJE <12181501@inha.edu>
2022-11-14 01:35:26 +0900

    feat: Application - makeLottoList 함수 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,16 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
-
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
+import java.util.List;
 import static java.lang.Integer.parseInt;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
-        String money =Console.readLine();
+        String moneyString =Console.readLine();
+        int moneyInt = moneyExceptionHandling(moneyString);
     }
 
     public static int moneyExceptionHandling(String moneyString){
@@ -24,5 +27,14 @@ public class Application {
             throw new IllegalArgumentException();
         }
         return moneyInt/1000;
+    }
+
+    public static List<List<Integer>> makeLottoList(int num){
+        List<List<Integer>> lottos = new java.util.ArrayList<>(Collections.emptyList());
+        for(int i = 0; i<num;i++){
+            List<Integer> newLotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(newLotto);
+        }
+        return lottos;
     }
 }
