ff83cf21361a447a3a4ba5aac1f6132f58648e74
limzzum <chfhddl1234@naver.com>
2022-11-13 20:34:02 +0900

    feat : 로또 숫자 중복체크 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,7 +15,12 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if (Set.copyOf(numbers).size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호와 보너스 번호는 중복이 없어야 합니다");
+        }
+    }
+    public List<Integer> getNumbers(){
+        return numbers;
     }
 
-    // TODO: 추가 기능 구현
 }
