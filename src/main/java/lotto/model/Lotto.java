54f8051e330550823b675463bd72a069ca85e24f
Jimin <thswlals0219@gmail.com>
2022-11-13 18:47:16 +0900

    feat(lotto): 로또 번호가 유일한(중복되지 않는) 숫자인지 검사하는 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -16,12 +16,13 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        validateSize(numbers);
+        validateSize(numbers.size());
         validateRange(numbers);
+        validateUniqueNumber(numbers);
     }
 
-    private void validateSize(List<Integer> numbers) {
-        if (!isSameSizeAsLotto(numbers.size())) {
+    private void validateSize(int numberSize) {
+        if (!isSameSizeAsLotto(numberSize)) {
             throw new IllegalArgumentException(LottoConstants.ERROR_IS_NOT_SAME_LOTTO_SIZE);
         }
     }
@@ -41,4 +42,13 @@ public class Lotto {
                 .allMatch(num -> num >= LottoConstants.LOTTO_NUMBER_MIN && num <= LottoConstants.LOTTO_NUMBER_MAX);
     }
 
+    private void validateUniqueNumber(List<Integer> numbers) {
+        if (isDuplicateNumber(numbers)) {
+            throw new IllegalArgumentException(LottoConstants.ERROR_IS_NOT_UNIQUE_LOTTO_NUMBER);
+        }
+    }
+
+    private boolean isDuplicateNumber(List<Integer> numbers) {
+        return numbers.stream().distinct().count() != LottoConstants.LOTTO_TICKET_SIZE;
+    }
 }
--- src/main/java/lotto/util/constants/LottoConstants.java
+++ src/main/java/lotto/util/constants/LottoConstants.java
@@ -10,4 +10,5 @@ public final class LottoConstants {
 
     public static final String ERROR_IS_NOT_SAME_LOTTO_SIZE = "[ERROR] 로또 번호의 개수는 6개여야 합니다.";
     public static final String ERROR_IS_NOT_SAME_RANGE_LOTTO_NUMBER = "[ERROR] 로또 번호의 범위는 1 ~ 45 사이여야 합니다.";
+    public static final String ERROR_IS_NOT_UNIQUE_LOTTO_NUMBER = "[ERROR] 로또 번호는 중복되지 않는 유일한 숫자여야 합니다.";
 }
