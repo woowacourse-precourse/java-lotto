383bb9092d471f67ed2e64beb7d3b50d86999c41
seonghoo1217 <seonghoo1217@naver.com>
2022-11-13 21:53:30 +0900

    refactor(Jackpot) : 출력문 수정 및 에러 메세지 수정
    출력양식에 따른 출력문 추가와 에러 메세지를 직관적으로 변경하였다.
    
    Refactor(commit_id) : #204f5b1

--- src/main/java/lotto/Jackpot.java
+++ src/main/java/lotto/Jackpot.java
@@ -5,16 +5,18 @@ import static lotto.util.InputImport.*;
 public class Jackpot {
 
     public static void input_jackpot_number(){
+        System.out.println("당첨 번호를 입력해 주세요.");
         jackpot_number_output_and_validate(ReadLine());
     }
 
     private static void jackpot_number_output_and_validate(String jackpot_number){
 
+
         if (!Validation.jackpot_number_validation(jackpot_number)){
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 하며 6자리를 입력해야합니다.");
         }
 
-        System.out.println("\n"+jackpot_number);
+        System.out.println(jackpot_number);
     }
 
 }
