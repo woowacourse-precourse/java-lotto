5257a1e44bd5a98abb103372d7d0adcc25b1ad7e
sora5421 <21800804@handong.edu>
2022-11-13 21:16:36 +0900

    feat (MakeLottoNumber) : 만들어야하는 개수를 생성자로 받아 로또를 생성한후 반환한다

--- src/main/java/lotto/datamodel/MakeLottoNumber.java
+++ src/main/java/lotto/datamodel/MakeLottoNumber.java
@@ -1,4 +1,22 @@
 package lotto.datamodel;
 
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class MakeLottoNumber {
+    private final int lottoCount;
+    public MakeLottoNumber(int lottoCount){
+        this.lottoCount = lottoCount;
+    }
+
+    public List<List<Integer>> makeNumbers() {
+        List<List<Integer>> lottoNumbers = new ArrayList<List<Integer>>();
+        for(int i = 0; i < lottoCount; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoNumbers.add(numbers);
+        }
+        return lottoNumbers;
+    }
+
 }
