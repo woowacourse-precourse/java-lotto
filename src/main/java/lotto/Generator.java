cc710f39928195eb983ce13c3d81be92cb2c90b2
swonny <wonny921@gmail.com>
2022-11-13 21:00:02 +0900

    refactor(Generator, MoneyAdministrator): 기능 분리에 따라 Generator 기능 이동

--- src/main/java/lotto/Generator.java
+++ src/main/java/lotto/Generator.java
@@ -6,31 +6,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Generator extends LottoKiosk {
-    static final int PRICE_PER_LOTTO = 1000;
-
-    public static List<Lotto> getMoney(String payment) {
-        validatePayment(Integer.valueOf(payment));
-        return generate(Integer.valueOf(payment));
-    }
-
-    public static List<Lotto> generate(int payment) {
+public class Generator extends Kiosk {
+    public static List<Lotto> generate(int numberOfLottos) {
         List<Lotto> lottos = new ArrayList<>();
-        for (int lottoNumber = 0; lottoNumber < countLottos(payment); lottoNumber++) {
+        for (int lottoNumber = 0; lottoNumber < numberOfLottos; lottoNumber++) {
             List<Integer> lottoNumbers = new ArrayList<>(pickLottoNumbers());
             lottos.add(new Lotto(orderNumbers(lottoNumbers)));
         }
         return lottos;
-    }
-
-    public static void validatePayment(int payment) {
-        if (payment % PRICE_PER_LOTTO != 0) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    public static int countLottos(int payment) {
-        return payment / PRICE_PER_LOTTO;
     }
 
     public static List<Integer> pickLottoNumbers() {
--- src/main/java/lotto/MoneyAdministrator.java
+++ src/main/java/lotto/MoneyAdministrator.java
@@ -1,2 +1,22 @@
-package lotto;public class MoneyAdministrator {
+package lotto;
+
+import ExceptionCollections.CommonException;
+
+import java.util.List;
+
+public class MoneyAdministrator {
+    static final int PRICE_PER_LOTTO = 1000;
+
+    public static int getMoney(String payment) {
+        validatePayment(Integer.valueOf(payment));
+        return Integer.valueOf(payment);
+    }
+
+    public static void validatePayment(int payment) {
+        CommonException.validatePaymentUnit(payment);
+    }
+
+    public static int countLottos(int payment) {
+        return payment / PRICE_PER_LOTTO;
+    }
 }
