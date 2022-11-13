308036b6218db2f782faef006f2ce673b15356c3
lvalentine6 <ysr1227@naver.com>
2022-11-14 01:30:29 +0900

    feat : 로또 선택 기능
    1~45 범위에서 중복되지 않는 6개를 숫자를 선택하는 기능 구현

---
+++ src/main/java/lotto/GenerateNumber.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
+import java.util.List;
+
+public class GenerateNumber {
+    static final int STARTNUMBER = 1;
+    static final int ENDNUMBER = 45;
+    static final int COUNT = 6;
+
+    public static List<Integer> creatNumber() {
+        List<Integer> bundle = Randoms.pickUniqueNumbersInRange(STARTNUMBER, ENDNUMBER, 6);
+        Collections.sort(bundle);
+        return bundle;
+    }
+}
