330cc341bf2c910e5edd3c6f8bffc9793bb3c99d
ostar11 <cjw5877@gmail.com>
2022-11-13 22:19:55 +0900

    feat(LottoManager): printWinningDetails 구현
    당첨 내역 출력

--- src/main/java/lotto/view/LottoManager.java
+++ src/main/java/lotto/view/LottoManager.java
@@ -1,7 +1,10 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 import lotto.Lotto;
+import lotto.LottoCalculator;
+import lotto.Rank;
 
 public class LottoManager {
     public int inputLottoCount() {
@@ -27,5 +30,13 @@ public class LottoManager {
     public String inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
+    }
+
+    public void printWinningDetails(LottoCalculator lottoCalculator) {
+        System.out.println("3개 일치 (5,000원) - " + lottoCalculator.getRankCount().get(0) + "개");
+        System.out.printf("4개 일치 (50,000원) - " + lottoCalculator.getRankCount().get(1) + "개");
+        System.out.printf("5개 일치 (1,500,000원) - " + lottoCalculator.getRankCount().get(2) + "개");
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - " + lottoCalculator.getRankCount().get(3) + "개");
+        System.out.printf("6개 일치 (2,000,000,000원) - " + lottoCalculator.getRankCount().get(4) + "개");
     }
 }
