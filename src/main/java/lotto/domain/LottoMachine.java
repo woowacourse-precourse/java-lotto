ad5c699d845b409e30b98f9fc97be1f898905072
Woojin Lee <dhkep03@gmail.com>
2022-11-13 18:15:43 +0900

    feat(LottoMachine): 구매 금액에 맞는 로또 발행 로직 작성

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,4 +1,11 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LottoMachine {
+    public static List<Lotto> publish(int inputMoney) {
+        int countOfLottos = getCountOfLottos(inputMoney);
+        List<Lotto> lottos = publishLottosByCount(countOfLottos);
+        return lottos;
+    }
 }
