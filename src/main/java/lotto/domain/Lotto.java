7b400b7f9309bb85dbc70c7051ec72d8a7c388e9
csj <koreaxman@naver.com>
2022-11-13 16:23:59 +0900

    feat: create duplicate winning numbers and bonus number

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,12 +6,22 @@ public class Lotto {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     private final List<Integer> numbers;
+    BonusNumber bonusNumber = new BonusNumber();
 
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
         validateNumbers(numbers);
         validateDuplicateNumbers(numbers);
         this.numbers = numbers;
+    }
+
+    public void validateDuplicateWinningNumbersAndBonusNumber() {
+        int number = bonusNumber.inputBonusNumber();
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) == number) {
+                throw new IllegalArgumentException("[ERROR] 로또 당첨 번호와 로또 보너스 번호가 중복됩니다.");
+            }
+        }
     }
 
     private void validateSize(List<Integer> numbers) {
@@ -30,7 +40,6 @@ public class Lotto {
     }
 
     private void validateDuplicateNumbers(List<Integer> winningNumbers) {
-        int count=0;
         for (int i = 0; i < winningNumbers.size(); i++) {
             validateDuplicateNumberFor(winningNumbers, i);
         }
