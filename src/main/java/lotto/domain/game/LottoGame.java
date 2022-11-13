c47ada754a9e66620ca7f8ad8ce3eb75cbea7a78
JaehongDev <println@kakao.com>
2022-11-13 16:07:38 +0900

    feature (LottoGame) : 로또 게임 기능 구현

---
+++ src/main/java/lotto/domain/game/LottoGame.java
@@ -0,0 +1,45 @@
+package lotto.domain.game;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.game.answerlottosystem.AnswerLottoSystem;
+import lotto.domain.game.randomlottosystem.RandomLottoSystem;
+import lotto.domain.money.Money;
+import lotto.domain.settlementsysyem.SettlementSystem;
+import lotto.system.IoSystem;
+
+public class LottoGame {
+    private final IoSystem io;
+
+    public LottoGame(IoSystem ioSystem) {
+        this.io = ioSystem;
+    }
+
+    public void execute() {
+        var money = new Money(io.input());
+
+        var randomLottoSystem = new RandomLottoSystem(io);
+        var answerLottoSystem = new AnswerLottoSystem(io);
+        var settlementSystem = new SettlementSystem();
+
+        var result = settlementSystem.result(
+                randomLottoSystem.generateLottoBundle(
+                        this.generateRandomNumbers(money.calculateAvailablePurchaseCount())),
+                answerLottoSystem.generateAnswerLotto()
+        );
+
+        var profits = settlementSystem.calculateProfits();
+        io.println(result);
+        io.print(String.format("총 수익률은 %.1f%%입니다.", money.calculateROI(profits)));
+    }
+
+    private List<List<Integer>> generateRandomNumbers(final Long repeatCount) {
+        List<List<Integer>> numbers = new ArrayList<>();
+
+        for (int i = 0; i < repeatCount; i++) {
+            numbers.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        }
+        return numbers;
+    }
+}
