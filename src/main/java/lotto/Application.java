0129cdeb552c39f2bb8154a199a182a3b71e0c8b
YongGwan <32181771@dankook.ac.kr>
2022-11-13 17:31:43 +0900

    feat : 구매 금액 확인 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,11 +7,21 @@ public class Application {
 
         // TODO: 프로그램 구현
 
-
         System.out.println("구매 금액을 입력해주세요");
         int purchaseAmount = Integer.parseInt(Console.readLine());
         Game game = new Game();
-        game.play(purchaseAmount/1000);
+        game.play(isValidPurchaseAmount(purchaseAmount) / 1000);
     }
 
+    static private int isValidPurchaseAmount(int inputValue) throws IllegalArgumentException {
+
+        if (inputValue == 0) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 0보다 커야합니다.");
+        }
+
+        if (inputValue % 1000 != 0)
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000단위로 입력해야 합니다.");
+
+        return inputValue;
+    }
 }
