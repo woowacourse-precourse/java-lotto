6fb921cb07ed53be16877124b81fdebd0e39de62
splitube <voicesplit@daum.net>
2022-11-13 18:46:43 +0900

    feat(Message): 보너스 번호 생성 관련 메시지 추가

--- src/main/java/lotto/Messages.java
+++ src/main/java/lotto/Messages.java
@@ -3,7 +3,8 @@ package lotto;
 public enum Messages {
     REQUEST_MONEY("구입금액을 입력해 주세요."),
     HOW_MANY_SOLD("개를 구매했습니다."),
-    ENTER_WINNING_NUMBER("당첨 번호를 입력해 주세요.");
+    ENTER_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    ENTER_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
 
     final String message;
 
