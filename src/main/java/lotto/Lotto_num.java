defdceb12df9aa5330f23ae1b3e254eb64549fad
jwj <real980312@gmail.com>
2022-11-13 23:32:28 +0900

    <feat> 구매금액 메서드단위로 분리 및 오류호출메서드 생성

--- src/main/java/lotto/Lotto_num.java
+++ src/main/java/lotto/Lotto_num.java
@@ -6,18 +6,44 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Lotto_num {
 
-    private final List<Lotto_num> nums;
+    private static final int MIN_COST = 1000;
+
+    private final int money;
+
+    public Lotto_num(int money){
+        validateNum(money);
+        this.money = money;
+    }
 
     public void Game() {
-        String lotto_num = getUserInput();
-        int lotto_nums = Integer.parseInt(lotto_num);
-        if ((lotto_nums%1000) != 0) {
-            throw new IllegalArgumentException("[ERROR]구매금액은 1천원 단위입니다.");
-        }
-        lotto_nums = lotto_nums/1000;
+//        String lotto_num = getUserInput();
+//        int lotto_nums = Integer.parseInt(lotto_num);
+//        if ((lotto_nums%1000) != 0) {
+//            throw new IllegalArgumentException("[ERROR]구매금액은 1천원 단위입니다.");
+//        }
+//        money = lotto_nums/1000;
 
 
     }
+
+    private void validateNum(int money) {
+        validateMinMoney(money);
+        validateDivisible(money);
+    }
+
+    private void validateMinMoney(int money) {
+        if (money < MIN_COST) {
+            throw new IllegalArgumentException("[ERROR]구매금액은 최소 1천원입니다.");
+        }
+    }
+
+    private void validateDivisible(int money) {
+        if (money % MIN_COST !=0) {
+            throw new IllegalArgumentException("[ERROR]구매금액은 1천원 단위입니다.");
+        }
+    }
+
+
 
     public String getUserInput() {
         System.out.println("구매금액을 입력해주세요");
