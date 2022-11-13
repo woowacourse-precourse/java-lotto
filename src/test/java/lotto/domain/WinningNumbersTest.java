532719565612728f99b212c5d8de938c83a13ff2
iamjooon2 <iamjooon2@gmail.com>
2022-11-13 23:37:59 +0900

    test: 당첨번호에 존재하는 보너스 번호가 입력될 때 예외처리하는 테스트 작성

---
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningNumbersTest {
+    @DisplayName("당첨 번호에 존재하는 보너스 번호가 입력되면 예외를 발생한다.")
+    @Test
+    void createMoneyWithUnmatchingUnit() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
