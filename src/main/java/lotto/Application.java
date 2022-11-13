8e7f4a545ef9d7605543f88e22e4e68099b85373
Donguk <95donguk.dev@gmail.com>
2022-11-14 00:38:30 +0900

    feat: 애플리케이션 에러 처리 기능 및 에러 메시지 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,14 +5,19 @@ import java.util.List;
 import lotto.domain.LottoGenerator;
 import lotto.domain.Price;
 import lotto.view.InputView;
+import lotto.view.message.ExceptionMessage;
 
 public class Application {
     private static List<Lotto> lottoTicket = new ArrayList<>();
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        Price price = new Price(inputView.askPrice());
-        LottoGenerator lottoGenerator = new LottoGenerator(price);
-        lottoTicket.addAll(lottoGenerator.generateLotto());
+        try {
+            InputView inputView = new InputView();
+            Price price = new Price(inputView.askPrice());
+            LottoGenerator lottoGenerator = new LottoGenerator(price);
+            lottoTicket.addAll(lottoGenerator.generateLotto());
+        } catch (IllegalArgumentException ignored) {
+            System.out.println(ExceptionMessage.APPLICATION_EXIT.getMessage());
+        }
     }
 }
--- src/main/java/lotto/view/message/ExceptionMessage.java
+++ src/main/java/lotto/view/message/ExceptionMessage.java
@@ -3,7 +3,8 @@ package lotto.view.message;
 public enum ExceptionMessage {
     NON_NUMERIC_VALUE("숫자가 아닌 값을 입력하셨습니다."),
     NON_UNITS_OF_ONE_THOUSAND_WON("1000원 단위가 아닌 가격입니다."),
-    NO_MONEY("금액이 0원 입니다.");
+    NO_MONEY("금액이 0원 입니다."),
+    APPLICATION_EXIT("에러로 인해 시스템을 종료합니다.");
 
     private static final String ERROR_MESSAGE = "[ERROR]";
 
