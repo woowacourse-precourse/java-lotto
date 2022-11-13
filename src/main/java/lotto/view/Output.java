229423825a2097822f16a9712963b663e655fccf
Reznov <reznov03048@gmail.com>
2022-11-13 15:55:32 +0900

    feat: 최종 당첨 통계를 출력하는 기능 구현

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,5 +1,13 @@
 package lotto.view;
 
+import lotto.domain.Ranking;
+import lotto.domain.Result;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Output {
 
     static final String OUTPUT_RESULT_TITLE = "당첨 통계";
@@ -11,6 +19,36 @@ public class Output {
     public static void printTitle() {
         System.out.println(OUTPUT_RESULT_TITLE);
         System.out.println(OUTPUT_RESULT_LINE);
+    }
+
+    public static void printWinningStatistic(Result result) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(Map.Entry<Ranking, Integer> entry : getResultEntryList(result)) {
+            Ranking nowRanking = entry.getKey();
+            int count = entry.getValue();
+            makeWinningResult(nowRanking, count, stringBuilder);
+        }
+        System.out.println(stringBuilder);
+    }
+
+    private static List<Map.Entry<Ranking, Integer>> getResultEntryList(Result result) {
+        return result.getResult().entrySet().stream()
+                .filter(set -> set.getKey() != Ranking.NOTHING)
+                .sorted(Comparator.comparingInt(i -> i.getKey().getMoney()))
+                .collect(Collectors.toList());
+    }
+
+    private static void makeWinningResult(Ranking ranking, int count, StringBuilder stringBuilder) {
+        String winningTitle = String.format("%d개 일치", ranking.getCount());
+        stringBuilder.append(winningTitle);
+
+        if(ranking.isHasBonusNumber()) {
+            String hasBonus = ", 보너스 볼 일치";
+            stringBuilder.append(hasBonus);
+        }
+
+        String winningMoney = String.format(" (%d원) - %d개", ranking.getMoney(), count);
+        stringBuilder.append(winningMoney).append("\n");
     }
 
 }
