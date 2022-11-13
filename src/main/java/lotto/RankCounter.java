faafa523b173574874b68626c0849c063b65f72a
dooboocookie <bkh3030@gmail.com>
2022-11-13 14:52:28 +0900

    feat: 당첨된 Rank의 갯수를 보관하는 RankCounter 생성 및 Rank 갯수를 세는 기능 추가

---
+++ src/main/java/lotto/RankCounter.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import java.util.HashMap;
+
+public class RankCounter {
+    private HashMap<Rank, Integer> rankCounter;
+
+    public RankCounter() {
+        HashMap<Rank, Integer> rankCounter = new HashMap<>();
+        for (Rank rank : Rank.values()) {
+            rankCounter.put(rank, 0);
+        }
+    }
+
+    public void drawLotto(User user, WinningNumbers winningNumbers) {
+        Lotto lotto;
+        int count;
+        Rank rank;
+        for (int indexLottos = 0; indexLottos < user.getLottoCount(); indexLottos++) {
+            lotto = user.findLotto(indexLottos);
+            rank = winningNumbers.rankingLotto(lotto);
+            count = rankCounter.getOrDefault(rank, 0);
+            rankCounter.put(rank, count + 1);
+        }
+    }
+}
--- src/main/java/lotto/ScoreCounter.java
+++
@@ -1,7 +0,0 @@
-package lotto;
-
-import java.util.HashMap;
-
-public class ScoreCounter {
-    private HashMap<Score, Integer> scoreCounter;
-}
