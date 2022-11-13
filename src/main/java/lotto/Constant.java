060419075937dfce4890aef217c21b9eed3c6f09
bnbnac <bnbnac@naver.com>
2022-11-13 21:20:35 +0900

    feat: 구입금액을 입력받아 검증하고 발행 매수를 반환한다.

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -4,9 +4,11 @@ public class Constant {
     public static final Integer MIN_NUMBER = 1;
     public static final Integer MAX_NUMBER = 45;
     public static final Integer TICKET_PRICE = 1000;
-    public  static final Integer SIZE = 6;
+    public static final Integer SIZE = 6;
 
     public static final String ASK_PRICE = "구입금액을 입력해 주세요.";
     public static final String ASK_WIN_NUMBERS = "당첨 번호를 입력해 주세요.";
     public static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public static final String ERROR = "[ERROR]";
 }
--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -2,15 +2,35 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.text.NumberFormat;
 import java.util.List;
 
 public class User {
     public Integer askTickets() {
+        Integer tickets;
+
         System.out.println(Constant.ASK_PRICE);
         String input = Console.readLine();
-        // validate
-        // divide 1000
-        return null;
+
+        tickets = validate(input);
+        return tickets;
+    }
+
+    public Integer validate(String input) {
+        Integer price = 0;
+        try {
+            price = Integer.valueOf(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(
+                    Constant.ERROR + " 로또 1장의 가격은 " + NumberFormat.getInstance().format(Constant.TICKET_PRICE)
+                            + "원입니다. 구입금액을 " + Constant.TICKET_PRICE + "의 배수로 입력해주세요.");
+        }
+        if ((price % Constant.TICKET_PRICE != 0) || (price <= 0)) {
+            throw new IllegalArgumentException(
+                    Constant.ERROR + " 로또 1장의 가격은 " + NumberFormat.getInstance().format(Constant.TICKET_PRICE)
+                            + "원입니다. 구입금액을 " + Constant.TICKET_PRICE + "의 배수로 입력해주세요.");
+        }
+        return price / Constant.TICKET_PRICE;
     }
 
     public List<Integer> askWinNumbers() {
