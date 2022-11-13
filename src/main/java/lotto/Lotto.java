adaf20e37da38a0968328413828dac80ac730bdd
조미정 <ing06164@naver.com>
2022-11-14 00:10:03 +0900

    feat: string을 받는 생성자 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -9,6 +10,17 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+    }
+
+    public Lotto(String numbers){
+        List<String> winnings = List.of(numbers.split(","));
+        List<Integer> win = new ArrayList<>();
+
+        for(int i=0; i<winnings.size(); i++){
+            win.add(Integer.parseInt(winnings.get(i)));
+        }
+        validate(win);
+        this.numbers = win;
     }
 
     private void validate(List<Integer> numbers) {
