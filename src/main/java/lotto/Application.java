d43388e83b48795586a7f0ae45b8366b87584880
이승훈 <qwqw8019@naver.com>
2022-11-13 14:22:34 +0900

    feature: create input money 로또 구입 금액을 입력받는다. 구입 금액은 1,000원 단위로 입력 받으며 1,000원으로 나누어 떨어지지 않는 경우 예외 처리한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int money = inputMoney();
+    }
+
+    public static int inputMoney() {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.next();
+        int money;
+
+        try {
+            money = checkInput(inputString);
+        } catch (Exception e) {
+            String errorMessage = "[ERROR] 잘못된 입력이 들어왔습니다.";
+            throw new IllegalArgumentException(errorMessage);
+        }
+
+        return money;
+    }
+
+    public static int checkInput(String input) {
+        int inputInteger = Integer.parseInt(input);
+        if (inputInteger % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+        return Integer.parseInt(input);
     }
 }
