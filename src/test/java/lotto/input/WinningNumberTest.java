45df80eb2428b531950e064991a3fd2332ccb4c9
Ridealist <cjsn92@gmail.com>
2022-11-13 13:58:37 +0900

    refactor(WinningNumber): 사용자 입력값 받는 로직 분리, 유효성 검증된 값 저장할 맴버 필드 추가

--- src/test/java/lotto/input/WinningNumberTest.java
+++ src/test/java/lotto/input/WinningNumberTest.java
@@ -2,7 +2,6 @@ package lotto.input;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
@@ -10,28 +9,29 @@ class WinningNumberTest {
 
     @Test
     void validate_notCommaInput_Fail() {
-        assertThatThrownBy(() -> WinningNumber.validate("1-2-3-4-5-6"))
+        assertThatThrownBy(() -> new WinningNumber("1-2-3-4-5-6"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 당첨 번호는 콤마(',')로 숫자를 이어서 입력해야 합니다.");
     }
 
     @Test
     void validate_SizeNotSixInput_Fail() {
-        assertThatThrownBy(() -> WinningNumber.validate("1,2,3,4,5"))
+        assertThatThrownBy(() -> new WinningNumber("1,2,3,4,5"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 당첨 번호는 6개를 입력해야 합니다.");
     }
 
     @Test
     void validate_NotNumberInput_Fail() {
-        assertThatThrownBy(() -> WinningNumber.validate("1,2,3,4,a,6"))
+        assertThatThrownBy(() -> new WinningNumber("1,2,3,4,a,6"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 당첨 번호는 숫자여야 합니다.");
     }
 
     @Test
-    void getWinningNumbers() {
-        assertThat(WinningNumber.getWinningNumbers("1,2,3,4,5,6"))
+    void getWinningNumbers_validInput_Success() {
+        WinningNumber wn = new WinningNumber("1,2,3,4,5,6");
+        assertThat(wn.getWinningNumbers())
                 .isEqualTo(Arrays.asList(1,2,3,4,5,6));
     }
 }
