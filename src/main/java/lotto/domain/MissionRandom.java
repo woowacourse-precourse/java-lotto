0bb0ad4aec7e3d99377342c14e177013c5275bf4
be-student <presentsong@naver.com>
2022-11-13 15:33:17 +0900

    feat(Random): 랜덤하게 생성해주는 라이브러리 사용

---
+++ src/main/java/lotto/domain/MissionRandom.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class MissionRandom implements RandomUniqueNumbers {
+
+    @Override
+    public List<Integer> pickUniquesInRange(int startInclusive, int endInclusive, int count) {
+        return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, count);
+    }
+}
