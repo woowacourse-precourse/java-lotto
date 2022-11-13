d4a38de74ca931f8f157bfe25a0c7b7159a7e966
신채원 <cw121@naver.com>
2022-11-14 03:37:06 +0900

    feat: 당첨 번호 입력 기능 구현

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -2,15 +2,29 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static lotto.practice.InputView.inputPurchase;
+import static lotto.practice.InputView.inputWinLotto;
 
 public class InputView {
 
     private static final String INPUT_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WIN_LOTTO_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public static int inputPurchase() {
         System.out.println(INPUT_PURCHASE_MESSAGE);
         String purchase = Console.readLine();
         return Integer.parseInt(purchase);
+    }
+
+    public static List<Integer> inputWinLotto() {
+        System.out.println(INPUT_WIN_LOTTO_MESSAGE);
+        String lottoNumbers = Console.readLine();
+        return Arrays.stream(lottoNumbers.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
