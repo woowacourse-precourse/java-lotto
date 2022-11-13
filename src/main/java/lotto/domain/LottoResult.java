dc8058790c96b0bfe2704c50577891f4db666446
khj1 <dhepd8849@gmail.com>
2022-11-13 14:59:14 +0900

    refactor: 로또 결과에 필요한 상수 값을 추가하고 기능을 분리

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,39 +1,66 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.Map;
+
 public enum LottoResult {
-    FIRST_PLACE(2000000000),
-    SECOND_PLACE(30000000),
-    THIRD_PLACE(1500000),
-    FOURTH_PLACE(50000),
-    FIFTH_PLACE(5000),
-    LAST_PLACE(0);
+    FIRST_PLACE(2000000000, 6),
+    SECOND_PLACE(30000000, 5),
+    THIRD_PLACE(1500000, 5),
+    FOURTH_PLACE(50000, 4),
+    FIFTH_PLACE(5000, 3),
+    LAST_PLACE(0, 0);
 
     private final int amount;
+    private final int matchingCount;
 
-    LottoResult(int amount) {
+    LottoResult(int amount, int matchingCount) {
         this.amount = amount;
+        this.matchingCount = matchingCount;
     }
 
     public static LottoResult of(int matchingCount, boolean containsBonusNumber) {
-        if (matchingCount == 6) {
-            return FIRST_PLACE;
+        if (isLastPlace(matchingCount)) {
+            return LAST_PLACE;
         }
-        if (matchingCount == 5) {
+        if (isSecondOrThirdPlace(matchingCount)) {
             if (containsBonusNumber) {
                 return SECOND_PLACE;
             }
             return THIRD_PLACE;
         }
-        if (matchingCount == 4) {
-            return FOURTH_PLACE;
-        }
-        if (matchingCount == 3) {
-            return FIFTH_PLACE;
-        }
-        return LAST_PLACE;
+        return getResultByMatchingCount(matchingCount);
+    }
+
+    private static boolean isSecondOrThirdPlace(int matchingCount) {
+        return matchingCount == SECOND_PLACE.getMatchingCount();
+    }
+
+    private static boolean isLastPlace(int matchingCount) {
+        return matchingCount < FIFTH_PLACE.getMatchingCount();
+    }
+
+    private static LottoResult getResultByMatchingCount(int matchingCount) {
+        LottoResult[] results = values();
+        return Arrays.stream(results)
+                .filter(result -> result.hasSameMatchingCount(matchingCount))
+                .findFirst()
+                .get();
+    }
+
+    private boolean hasSameMatchingCount(int matchingCount) {
+        return this.matchingCount == matchingCount;
     }
 
     public int getAmount() {
         return amount;
+    }
+
+    public int getMatchingCount() {
+        return matchingCount;
+    }
+
+    public boolean winLotto() {
+        return matchingCount >= FIFTH_PLACE.getMatchingCount();
     }
 }
