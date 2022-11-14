b136ec76c82263f8665f4d9bff83a77193987550
im-shung <seoyoung1113@gmail.com>
2022-11-15 01:15:45 +0900

    move: :truck: 패키지 이동

--- src/test/java/lotto/util/NumberGeneratorTest.java
+++ src/test/java/lotto/util/NumberGeneratorTest.java
@@ -3,7 +3,7 @@ package lotto.util;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import lotto.util.NumberGenerator;
+import lotto.domain.NumberGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
