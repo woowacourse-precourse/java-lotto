a505706d3374f17a6e266e3989c70a1c1e285bb6
LAPTOP-4NC4E864\yongs <yongs170@naver.com>
2022-11-14 14:00:02 +0900

    refactor(Validation) : 값과 메세지의 상수화

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -6,14 +6,18 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Validation {
+    public static final int LOTTO_COUNT = 6;
+
+    public static final String LOTTO_NUMBER_RANGE_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String NUMBER_COUNT_MESSAGE = "[ERROR] 로또 번호는 6개입니다.";
    public static void numberCountValidation(List<Integer> numbers){
-       if(numbers.size() != 6) throw new IllegalArgumentException("[ERROR] 로또 번호는 6개입니다.");
+       if(numbers.size() != LOTTO_COUNT) throw new IllegalArgumentException(NUMBER_COUNT_MESSAGE);
    }
    public static void numberRangeValidation(List<Integer> numbers){
        int numbersCount = numbers.stream()
                .filter(element->element >= 1 && element <= 45)
                .collect(Collectors.toList()).size();
 
-       if(numbersCount != 6) throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+       if(numbersCount != LOTTO_COUNT) throw new IllegalArgumentException(LOTTO_NUMBER_RANGE_MESSAGE);
    }
 }
