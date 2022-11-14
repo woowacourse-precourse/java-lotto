11e6d3e67999fd1f0b8a521dc39256aeefa56aa9
neppiness <0414kjh@naver.com>
2022-11-14 23:05:15 +0900

    test: add IsNoOfDigitsExceeds9MethodTest (src/main/java/lotto)
    Method to test if input length exceeds 9

---
+++ src/test/java/lotto/IsInputLengthExceeds9MethodTest.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import static lotto.Application.isNoOfDigitsExceeds9;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class IsInputLengthExceeds9MethodTest {
+    private static final String ERROR_MESSAGE = "[ERROR] 번호는 1이상 45이하의 자연수로 구성해야 합니다.";
+
+    @Test
+    void isInputLengthExceeds9_메소드_예외_테스트1() {
+        String numberInput = "3141592653589793238462643383279502884197";
+        assertThatThrownBy(() -> isNoOfDigitsExceeds9(numberInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @Test
+    void isInputLengthExceeds9_메소드_예외_테스트2() {
+        String numberInput = "1234567890";
+        assertThatThrownBy(() -> isNoOfDigitsExceeds9(numberInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @Test
+    void isInputLengthExceeds9_메소드_정상_입력_테스트1() {
+        String numberInput = "123456789";
+        isNoOfDigitsExceeds9(numberInput);
+    }
+}
