fd49d67c0ff778547a024993230e2e4c0aff51dd
bsh1603 <bsh1603@github.com>
2022-11-15 02:49:13 +0900

    Feat: 동작 controller 생성

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -6,15 +6,8 @@ import Service.WinningService;
 public class LottoController {
 
     LottoService lottoService = new LottoService();
-    WinningService winningService = new WinningService();
 
     public void run(){
-        setLotto();
-//        setWin();
+        lottoService.lottoGame();
     }
-
-    private void setLotto(){
-        lottoService.set_lotto();
-    }
-//    private void setWin() {lottoService.set_winning();}
 }
