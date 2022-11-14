b3f3e61c01df716e1ea47cf300aa6219ecc11ed2
c1ce <chalicenucelar@git.hub>
2022-11-15 03:16:32 +0900

    fix: User가 입력한 값이 숫자가 아닐경우 문제발생 이에따라 예외처리진행

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -3,19 +3,24 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
-    private final int lottoPirce = 1000;
+    private final int lottoPrice = 1000;
     public int buyMoney;
     public int buyCount;
     public User(){
         System.out.println("구입금액을 입력해 주세요.");
-        buyMoney = Integer.valueOf(Console.readLine());
+        try{
+            buyMoney = Integer.valueOf(Console.readLine());
+        }catch (NumberFormatException e){
+            System.out.println("[ERROR] 숫자를 입력해야합니다. 다음기회를 노려보세요.");
+        }
+
         validate();
-        buyCount = buyMoney/lottoPirce;
+        buyCount = buyMoney/lottoPrice;
         System.out.println(String.format("%d개를 구매했습니다.",buyCount));
     }
     public void validate(){
-        if (buyMoney%lottoPirce != 0 ){
-            String errorPrint =String.format("[ERROR] 금액은 %d원 단위입니다.",lottoPirce);
+        if (buyMoney%lottoPrice != 0 ){
+            String errorPrint =String.format("[ERROR] 금액은 %d원 단위입니다.",lottoPrice);
             throw new IllegalArgumentException(errorPrint);
         }
     }
