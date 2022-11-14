d415f610921559b91399b4aef9e549f95c9f5d38
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-14 19:11:15 +0900

    refactor(Application): main() 메서드를 LottoGameController 클래스의 메서드로 분할

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,30 +1,13 @@
 package lotto;
 
-import java.util.List;
-import lotto.view.InputView;
-import lotto.view.OutputView;
+import lotto.controller.LottoGameController;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         try {
-            int purchaseAmount = InputView.askPurchaseAmount();
-            Quantity quantity = new Quantity(purchaseAmount);
-            LottosGenerator lottosGenerator = new LottosGenerator();
-            List<Lotto> lottos = lottosGenerator.createLottos(quantity);
-
-            OutputView.printLottoQuantity(quantity);
-            OutputView.printLottos(lottos);
-
-            List<Integer> playerNumbers = InputView.askNumbers();
-            int bonusNumber = InputView.askBonusNumber(playerNumbers);
-
-            Referee referee = new Referee();
-            List<WinningStat> winningStats = referee.compare(lottos, playerNumbers, bonusNumber);
-            Profit profit = new Profit(winningStats);
-
-            OutputView.printStatistics(winningStats);
-            OutputView.printProfit(profit, quantity);
+            LottoGameController lottoGameController = new LottoGameController();
+            lottoGameController.gameStart();
         } catch (IllegalArgumentException illegalArgumentException) {
             System.out.println(illegalArgumentException.getMessage());
         }
