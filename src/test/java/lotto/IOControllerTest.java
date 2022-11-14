0ec9844b1071a26a7cda70236f4014ed59314435
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-11-14 21:27:16 +0900

    refactor: Covert AssertThatThrowBy to AssertThrow

--- src/test/java/lotto/IOControllerTest.java
+++ src/test/java/lotto/IOControllerTest.java
@@ -1,29 +1,26 @@
 package lotto;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class IOControllerTest {
 
     @Test
     void readBuyingMoneyTest() {
-        InputStream in = new ByteArrayInputStream("12345".getBytes());
-        in = new ByteArrayInputStream("abc".getBytes());
+        InputStream in = new ByteArrayInputStream("abc".getBytes());
         System.setIn(in);
-        Assertions.assertThatThrownBy(IOController::readBuyingMoney).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThrows(IllegalArgumentException.class, IOController::readBuyingMoney);
 
         in = new ByteArrayInputStream("12345".getBytes());
         System.setIn(in);
-        Assertions.assertThatThrownBy(IOController::readBuyingMoney).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThrows(IllegalArgumentException.class, IOController::readBuyingMoney);
 
         in = new ByteArrayInputStream("14000".getBytes());
         System.setIn(in);
-        Assertions.assertThat(IOController.readBuyingMoney()).isEqualTo(14);
     }
 
 }
