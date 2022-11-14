9ad5c9c3a456a6f988d637c0cf5721c42de2c3fe
MinSungKim <timkim01@naver.com>
2022-11-14 15:06:36 +0900

    feat: 사용자 입력이 숫자인지에 대한 유효성검사 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -14,24 +14,5 @@ public class Application {
         }
     }
 
-    // 로또번호 자동생성 후 List 타입으로 반환
-    public List<Integer> createLottoNumbers(){
-        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1,45,6);
 
-        return lottoNumbers;
-    }
-
-    // 로또 번호 List 오름차순으로 정렬
-    public List<Integer> sortLottoNumbers(List<Integer> lottoNumbers){
-        Collections.sort(lottoNumbers);
-
-        return lottoNumbers;
-    }
-
-    // 로또 번호 List를 Lotto객체로 변환
-    public Lotto listToLottoObject(List<Integer> lottoNumbers){
-        Lotto lotto = new Lotto(lottoNumbers);
-
-        return lotto;
-    }
 }
