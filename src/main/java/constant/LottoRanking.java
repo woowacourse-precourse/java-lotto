6d06205da373d4b7decccb53bcc6cba3049ced32
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-14 19:15:02 +0900

    feat(LottoRanking) : eNum 값들 추가.
    총상금을 저장하는 index를 의미하는 상수 추가.

--- src/main/java/constant/LottoRanking.java
+++ src/main/java/constant/LottoRanking.java
@@ -1,10 +1,16 @@
 package constant;
 
 public enum LottoRanking {
-    FIRST(1,6,2*(int)Math.pow(10,10)),SECOND(2,5,30000000),
+    FIRST(1,6,2_000_000_000),SECOND(2,5,30_000_000),
     THIRD(3,5,1_500_000), FOURTH(4,4,50_000),
-    FIFTH(5,3,5_000), NOTHING(-1,2,0);
+    FIFTH(5,3,5_000), NOTHING(-1,2,0),PRICE(0);
     int value,sameNumberCount,price;
+    LottoRanking(int value){
+        this.value = value;
+    }
+    public int getIndex(){
+        return value;
+    }
     LottoRanking(int ranking, int sameNumberCount, int price){
         this.value = ranking;
         this.sameNumberCount = sameNumberCount;
