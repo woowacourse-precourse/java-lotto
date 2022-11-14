3efe6a49431a1f541c82e109e6a250496184b6a5
CAEL0 <kjongmin26@gmail.com>
2022-11-14 17:30:50 +0900

    refactor(OutputView): 당첨 결과 출력 메서드를 하나로 통합

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,7 +2,6 @@ package lotto.view;
 
 import lotto.domain.Lotto;
 
-import static lotto.constant.GameConstants.*;
 import static lotto.constant.OutputConstants.*;
 
 public class OutputView {
@@ -22,29 +21,8 @@ public class OutputView {
         System.out.println(STATISTICS);
     }
 
-    public static void printFifthPlace(int fifthPlaceCount) {
-        System.out.printf(FIFTH_PLACE.toString(), FIFTH_PRIZE, fifthPlaceCount);
-        System.out.println();
-    }
-
-    public static void printFourthPlace(int fourthPlaceCount) {
-        System.out.printf(FOURTH_PLACE.toString(), FOURTH_PRIZE, fourthPlaceCount);
-        System.out.println();
-    }
-
-    public static void printThirdPlace(int thirdPlaceCount) {
-        System.out.printf(THIRD_PLACE.toString(), THIRD_PRIZE, thirdPlaceCount);
-        System.out.println();
-    }
-
-    public static void printSecondPlace(int secondPlaceCount) {
-        System.out.printf(SECOND_PLACE.toString(), SECOND_PRIZE, secondPlaceCount);
-        System.out.println();
-    }
-
-    public static void printFirstPlace(int firstPlaceCount) {
-        System.out.printf(FIRST_PLACE.toString(), FIRST_PRIZE, firstPlaceCount);
-        System.out.println();
+    public static void printResult(String result) {
+        System.out.println(result);
     }
 
     public static void printProfitRate(float profitRate) {
