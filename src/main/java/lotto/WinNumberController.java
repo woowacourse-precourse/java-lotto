7bebaba2ad78e23505f318371bb0da2b9796a991
kimilchul <huj0159@gmail.com>
2022-11-14 11:57:11 +0900

    refactor: separate into independent class

--- src/main/java/lotto/WinNumberController.java
+++ src/main/java/lotto/WinNumberController.java
@@ -1,10 +1,40 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
-public class WinNumber {
-    private List<Integer> winNumber;
-    private int bonusNumber = 0;
+public class WinNumberController {
 
-    
+    public List<Integer> readWinNumber(String userWinNumber) {
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+
+        StringTokenizer st = new StringTokenizer(userWinNumber, ",");
+        ArrayList<Integer> winNumber = new ArrayList<>();
+
+        while (st.hasMoreTokens()) {
+            String singleNumber = st.nextToken();
+            isDigit(singleNumber);
+            winNumber.add(Integer.parseInt(singleNumber));
+        }
+
+        Collections.sort(winNumber);
+        return winNumber;
+    }
+
+
+    public Integer readBoundNumber(String userBonusNumber) {
+        isDigit(userBonusNumber);
+
+        return Integer.parseInt(userBonusNumber);
+    }
+
+    private void isDigit(String userInput) {
+        String pattern = "^[0-9]*$";
+        if (!Pattern.matches(pattern, userInput)) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다");
+        }
+    }
 }
