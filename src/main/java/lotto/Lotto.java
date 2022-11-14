daed0ce82c4395a54c0e26b62480cd19c13137be
jjongwa <troas96@naver.com>
2022-11-14 16:21:56 +0900

    feat: 로또 구매 추가 예외처리
    1~45 범위 이외의 숫자가 포함된 리스트가 들어왔을 경우 예외처리하는 checkNumberInRange() 추가
    
    validate()에서 중복을 검사하는 로직을 checkDuplicate()로 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,15 +16,25 @@ public class Lotto {
             throw new IllegalArgumentException("6개 숫자를 입력해주세요");
         }
         List<Integer> compareNumbers = new ArrayList<>();
-        for(int number : numbers){
-            if(compareNumbers.contains(number)){
-                throw new IllegalArgumentException("6개 숫자는 서로 달라야 합니다.");
-            }
+        for (int number : numbers) {
+            checkDuplicate(compareNumbers, number);
+            checkNumberInRange(number);
             compareNumbers.add(number);
         }
     }
 
     // TODO: 추가 기능 구현
+    private void checkDuplicate(List<Integer> compareNumbers, int number) {
+        if (compareNumbers.contains(number)) {
+            throw new IllegalArgumentException("6개 숫자는 서로 달라야 합니다.");
+        }
+    }
+
+    private void checkNumberInRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
 
     public List<Integer> getLottoNumbers() {
         return this.numbers;
