4b5a4c57fae5bd024895aac1c7c50651b22bc7ef
임원정 <oddish0513@gmail.com>
2022-11-14 22:50:36 +0900

    fix(lotto): 중복수 예외처리 안되는 버그 수정

--- src/main/java/lotto/domain/model/Lotto.java
+++ src/main/java/lotto/domain/model/Lotto.java
@@ -1,18 +1,27 @@
 package lotto.domain.model;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
 	private final List<Integer> numbers;
 
 	public Lotto(List<Integer> numbers) {
-		validate(numbers);
+		validateSize(numbers);
+		validateOverlap(numbers);
 		this.numbers = numbers;
 	}
 
-	private void validate(List<Integer> numbers) {
+	private void validateOverlap(List<Integer> numbers) {
+		HashSet<Integer> numberSet = new HashSet<>(numbers);
+
+		if (numberSet.size() != numbers.size())
+			throw new IllegalArgumentException("중복된 수가 있음");
+	}
+
+	private void validateSize(List<Integer> numbers) {
 		if (numbers.size() != 6) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("크기가 6이 아님");
 		}
 	}
 
