c15659986566ab2aba322979f9edf9f5964cc43d
Kangbin <hz0819@naver.com>
2022-11-14 14:11:46 +0900

    최종완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -61,7 +61,6 @@ public class Application {
         }
         return BonusNum;
     }
-
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
         int Lottonumber = Insertpurchase();
