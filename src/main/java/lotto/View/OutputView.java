2b3f0baad809b1e04be6e35d08b466362b8a03b0
Sieun Park <psieun0606@gmail.com>
2022-11-14 09:13:55 +0900

    feat: 로또 구매 수량 출력 기능 추가

---
+++ src/main/java/lotto/View/OutputView.java
@@ -0,0 +1,7 @@
+package lotto.View;
+
+public class OutputView {
+    public void printTheNumberOfLotto(int theNumberOfLotto) {
+        System.out.println(String.format("%d개를 구매했습니다.", theNumberOfLotto));
+    }
+}
