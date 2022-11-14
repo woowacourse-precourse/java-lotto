f39a0882652affdd134fbcdcb41a985c3209c425
DongwookKim0823 <dwkim0823@naver.com>
2022-11-14 22:06:16 +0900

    fix(lotto): lottoTest에서 정렬 관련 오류가 발생하는 문제 수정

--- src/main/java/lotto/domian/lotto/Lotto.java
+++ src/main/java/lotto/domian/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domian.lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,9 +8,15 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        Collections.sort(numbers);
         validateLottoNumbers(numbers);
+        numbers = sortNumbers(numbers);
         this.numbers = numbers;
+    }
+
+    private List<Integer> sortNumbers(List<Integer> numbers) {
+        List<Integer> tempNumbers = new ArrayList<>(numbers);
+        Collections.sort(tempNumbers);
+        return tempNumbers;
     }
 
     private void validateLottoNumbers(List<Integer> numbers) {
