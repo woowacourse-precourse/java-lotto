6a7ccce619ce41a63427a257bc786ba697975097
jingyu9804 <swc05161@gmail.com>
2022-11-14 17:37:36 +0900

    feat(lotto) : 로또 번호 저장하는 클래스 추가

---
+++ src/main/java/lotto/model/LottoNumberStorage.java
@@ -0,0 +1,21 @@
+package lotto.model;
+
+import lotto.controller.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoNumberStorage {
+    private MathUtil mathUtil = new MathUtil();
+    private RandomUtil randomUtil = new RandomUtil();
+    public List<List<Integer>> lottoNumbers(int purchasePrice){
+        List<List<Integer>> lottos = new ArrayList<>();
+        Lotto lotto;
+        for(int i=0;i<mathUtil.divide(purchasePrice);i++){
+            List<Integer> list = randomUtil.makeRandomNumber();
+            lotto = new Lotto(list);
+            lottos.add(lotto.getNumbers());
+        }
+        return lottos;
+    }
+}
