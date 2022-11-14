40dead4534c3243cef9f981163bf011d85d3d423
woogi <dnrlwo11@gmail.com>
2022-11-14 21:51:54 +0900

    feat : 출력해야할 문구 정리

---
+++ src/main/java/messages/ExceptionMessages.java
@@ -0,0 +1,9 @@
+package messages;
+
+public class ExceptionMessage {
+    public static final String WRONG_PAYMENT = "[ERROR] 구입 금액은 1,000원 단위여야 합니다.";
+    public static final String WRONG_PAYMENT_START_WITH_ZERO = "[ERROR] 구입 금액은 0으로 시작할 수 없습니다.";
+    public static final String WRONG_NUM = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String WRONG_BONUS = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
+
+}
