9ef3e9e8e57805003123c76930eec73089af8ab3
jjmin9797 <jjmin9797@naver.com>
2022-11-14 17:51:07 +0900

    fix(RandomLottoService.java): 디버그 테스트 중 Arrays.asList로 생성된 값을 sort로 변경 할 수 없는 오류가 발생하여 ArrayList<Integer> 로 변경 후 sort 진행
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/service/RandomLottoService.java
+++ src/main/java/lotto/service/RandomLottoService.java
@@ -3,18 +3,16 @@ package lotto.service;
 import lotto.domain.Lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class RandomLottoService {
     public static ArrayList<Lotto> randomLotteryGeneration(int number) {
         ArrayList<Lotto> lotties = new ArrayList<>();
         for (int i = 0; i < number; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            numbers.sort(Comparator.naturalOrder());
-            Lotto lotto = new Lotto(numbers);
+            ArrayList<Integer> lottoNumbers = new ArrayList<>(numbers);
+            Collections.sort(lottoNumbers);
+            Lotto lotto = new Lotto(lottoNumbers);
             lotties.add(lotto);
         }
         return lotties;
