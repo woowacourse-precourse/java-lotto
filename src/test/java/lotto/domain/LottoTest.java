2632940b4ab54e8c6f28c5cb0f802939d25864de
seoyoung <sy9723@korea.ac.kr>
2022-11-14 23:31:59 +0900

    test: 예외 처리 테스트 추가 ([보너스 번호] 입력된 번호가 1부터 45 사이의 숫자가 아닌 경우)

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -35,4 +35,11 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6)).validateBonusNumber("a"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("보너스 번호가 1부터 45 사이의 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void createBonusByOverRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6)).validateBonusNumber("47"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
