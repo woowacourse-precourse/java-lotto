be2a37fdcd163a764532f06bd867efc49b773e76
akskflwn <akskflwn@gmail.com>
2022-11-15 03:42:11 +0900

    refactor(Application) : 에러 발생시 에러 메세지 출력 후 종료하도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,8 +3,14 @@ package lotto;
 import lotto.controller.LottoGameController;
 
 public class Application {
+
     public static void main(String[] args) {
         LottoGameController lottoGameController = new LottoGameController();
-        lottoGameController.run();
+
+        try {
+            lottoGameController.run();
+        } catch (IllegalArgumentException error) {
+            System.out.println(error.getMessage());
+        }
     }
 }
