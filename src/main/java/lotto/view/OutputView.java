2a3757a1d03038768fd46ba0e90f6e06c81a4df6
yujaemin <woawoa0513@gmail.com>
2022-11-14 20:43:21 +0900

    feat: 보너스 번호 입력 안내 출력문
    로또 보너스 번호를 입력하도록 안내하는 출력문

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,4 +9,8 @@ public class OutputView {
     public static void printWinningNumberInputNotice() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
+
+    public static void printBonusNumberInputNotice() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
 }
