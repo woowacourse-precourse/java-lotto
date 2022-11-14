f8775654028094dc1fae3721a4ec0df1f661b9e3
YUSEONJUN <seonjun0906@gmail.com>
2022-11-14 23:22:14 +0900

    feat : 맵 기본 값 put

---
+++ src/main/java/lotto/StoreMap.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class StoreMap {
+
+	public SortedMap<Rank, Integer> StoreMap;
+
+	public StoreMap() {
+		StoreMap = new TreeMap<>();
+		StoreMap.put(Rank.MISS, 0);
+		StoreMap.put(Rank.FIRST, 0);
+		StoreMap.put(Rank.SECOND, 0);
+		StoreMap.put(Rank.THIRD, 0);
+		StoreMap.put(Rank.FOURTH, 0);
+		StoreMap.put(Rank.FIFTH, 0);
+	}
+}
