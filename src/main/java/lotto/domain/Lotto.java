a788e1cc8fe7a53bce4b1e45aa89fa3028f8568f
HiiWee <wpdlzhf159@gmail.com>
2022-11-15 03:56:02 +0900

    feat: 사용자가 구매한 각 로또번호와 당첨 번호를 비교해 당첨 결과 반환 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,16 +2,21 @@ package lotto.domain;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import lotto.constants.enums.WinResultStatus;
 import lotto.constants.message.ExceptionMessage;
 
 public class Lotto {
     public static final int LOTTO_COUNT = 6;
     public static final String START_SYMBOL = "[";
-    public static final int ONE = 1;
-    public static final String COMMA = ",";
     private static final String END_SYMBOL = "]";
+    public static final String COMMA = ",";
     private static final String SPACE = " ";
+    public static final int ONE = 1;
+    public static final int HIT_THREE = 3;
+    public static final int HIT_FIVE = 5;
+    private static final int SECOND_PLACE_COUNT = 7;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -20,6 +25,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        // todo 분리하기
         if (numbers.size() != LOTTO_COUNT) {
             throw new IllegalArgumentException(ExceptionMessage.ERROR + ExceptionMessage.NUMERIC_LENGTH);
         }
@@ -50,5 +56,31 @@ public class Lotto {
         return lottoResult.append(numbers.get(numbers.size() - ONE))
                 .append(END_SYMBOL)
                 .toString();
+    }
+
+    public WinResultStatus getWinResult(WinningNumber winningNumber) {
+        int hitCount = getHitCount(winningNumber.getNumbers());
+        if (hitCount < HIT_THREE) {
+            return WinResultStatus.NOTHING;
+        }
+        return convertHitCountToWinResult(hitCount, winningNumber);
+    }
+
+    private WinResultStatus convertHitCountToWinResult(int hitCount, WinningNumber winningNumber) {
+        if (isSecondPlace(hitCount, winningNumber)) {
+            return WinResultStatus.getWinResultStatus(SECOND_PLACE_COUNT);
+        }
+        return WinResultStatus.getWinResultStatus(hitCount);
+    }
+
+    private int getHitCount(List<Integer> winningLotto) {
+        return (int) numbers.stream()
+                .filter(buyNumber -> winningLotto.stream()
+                        .anyMatch(Predicate.isEqual(buyNumber)))
+                .count();
+    }
+
+    private boolean isSecondPlace(int hitCount, WinningNumber winningNumber) {
+        return hitCount == HIT_FIVE && numbers.contains(winningNumber.getBonusNumber());
     }
 }
--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class WinnerNumber {
+public class WinningNumber {
     public static final String COMMA = ",";
     public static final int LOTTO_NUMBER = 0;
     public static final int BONUS_NUMBER = 1;
@@ -13,7 +13,7 @@ public class WinnerNumber {
     private final List<Integer> numbers;
     private final int bonusNumber;
 
-    public WinnerNumber(List<String> winnerNumber) {
+    public WinningNumber(List<String> winnerNumber) {
         validator.validateLottoNumber(winnerNumber.get(LOTTO_NUMBER));
         this.numbers = creatNumbers(winnerNumber.get(LOTTO_NUMBER));
         validator.validateBonusNumber(winnerNumber.get(BONUS_NUMBER), numbers);
@@ -29,5 +29,9 @@ public class WinnerNumber {
 
     public int getBonusNumber() {
         return bonusNumber;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 }
