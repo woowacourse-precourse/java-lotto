60f02ef23d066d351fb803a1ed169ed4f4428aaa
cocomozila <joseyun94@naver.com>
2022-11-14 18:59:55 +0900

    feat(LottoService): 로또 구매하는 기능

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,27 @@
+package lotto.service;
+
+import lotto.view.LottoView;
+
+public class LottoService {
+
+    private LottoView lottoView;
+
+    public LottoService() {
+        this.lottoView = new LottoView();
+    }
+
+    // 로또 구매하는 기능
+    public void buyLotto() {
+
+    }
+
+    // 당첨 번호 입력하는 기능
+    public void inputWinningNumber() {
+
+    }
+
+    // 당첨 결과 보여주는 기능
+    public void showLottoYield() {
+
+    }
+}
