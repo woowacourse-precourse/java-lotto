e58b195a8dfbecf4f693daf5219ff1168c9d7ffd
tseop <77163842+tseop@users.noreply.github.com>
2022-11-14 11:44:44 +0900

    refactor(lotto): 변수 타입 수정
    로또 금액 long -> int

--- src/main/java/lotto/input/InputValidator.java
+++ src/main/java/lotto/input/InputValidator.java
@@ -4,31 +4,24 @@ import java.util.List;
 import lotto.exception.LottoException;
 
 public class InputValidator {
-    private static final long LOTTO_PRICE = 1_000L;
+    private static final int LOTTO_PRICE = 1_000;
 
-    public void validationZeroAmount(long amount) {
-        if (amount % LOTTO_PRICE != 0 && amount != 0) {
-            throw new LottoException(this.getClass());
-        }
-    }
-
-    public void validationAmount(long amount) {
-        if (amount % 1000 != 0) {
-            throw new LottoException(this.getClass());
-        }
-    }
-
-    public void validationInputLength(String input) {
+    public void validationInputLength(String input) { // 입력한 값이 있는지 확인
         if (input.length() == 0) {
             throw new LottoException(this.getClass());
         }
     }
 
-    public void validationNumberInRange(List<Integer> numbers) {
-        for (int number : numbers) {
-            if (!(1 <= number && number <= 45)) {
-                throw new LottoException(this.getClass());
-            }
+    public void validationZeroAmount(int amount) { // 금액이 0원인지 확인
+        if (amount % LOTTO_PRICE != 0 && amount != 0) {
+            throw new LottoException(this.getClass());
         }
     }
+
+    public void validationAmount(int amount) { // 금액이 1000의 배수인지 확인
+        if (amount % LOTTO_PRICE != 0) {
+            throw new LottoException(this.getClass());
+        }
+    }
+
 }
