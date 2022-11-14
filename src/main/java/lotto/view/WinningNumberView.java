1b849aac72751dff9aeba1421b41bbae698f2409
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-15 07:05:15 +0900

    feat: 당첨 번호 유효성 검사 기능 추가

--- src/main/java/lotto/view/WinningNumberView.java
+++ src/main/java/lotto/view/WinningNumberView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class WinningNumberView {
@@ -9,9 +10,75 @@ public class WinningNumberView {
     public List<Integer> inputWinningNumber() {
 
         System.out.println("당첨 번호를 입력해 주세요.");
-        String winningNumber = Console.readLine();
+        String input = Console.readLine();
+        String winningNumbers = inputStringParsing(input);
+        validateWinningNumber(winningNumbers);
 
-        return null;
+        List<Integer> winningNumberList = new LinkedList<>();
+        for (int index = 0; index < winningNumbers.length(); index++) {
+            winningNumberList.add(Integer.valueOf(winningNumbers.charAt(index)));
+        }
+
+        return winningNumberList;
+    }
+
+    private String inputStringParsing(String input) {
+
+        String[] inputStrings = input.split(",");
+        String[] winningNumbers = deleteBlank(inputStrings);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String winningNumber : winningNumbers) {
+            stringBuilder.append(winningNumber);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public void validateWinningNumber(String winningNumbers) {
+
+        checkLength(winningNumbers);
+        checkNumber(winningNumbers);
+        checkDuplicate(winningNumbers);
+    }
+
+    private void checkDuplicate(String winningNumbers) {
+
+        char winningNumber;
+
+        for (int index = 0; index < winningNumbers.length(); index++) {
+            winningNumber = winningNumbers.charAt(index);
+            if (winningNumbers.contains(String.valueOf(winningNumber))) {
+                throw new IllegalArgumentException("[ERROR] 당첨번호는 중복되어서는 안됩니다")
+            }
+        }
+    }
+
+    private void checkNumber(String winningNumbers) {
+
+        char winningNumber;
+        for (int index = 0; index < winningNumbers.length(); index++) {
+            winningNumber = winningNumbers.charAt(index);
+            if (winningNumber < '1' || winningNumber > '9') {
+                throw new IllegalArgumentException("[ERROR] 당첨번호는 1~9사이여야 합니다");
+            }
+        }
+    }
+
+    public String[] deleteBlank(String[] inputStrings) {
+
+        for (int index = 0; index < inputStrings.length; index++) {
+            inputStrings[index] = inputStrings[index].trim();
+        }
+
+        return inputStrings;
+    }
+
+    private void checkLength(String winningNumbers) {
+
+        if (winningNumbers.length() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨번호의 개수는 6개 입니다. , 을 사용하여 당첨번호를 나눠주세요.");
+        }
     }
 
     public int inputBonusNumber() {
