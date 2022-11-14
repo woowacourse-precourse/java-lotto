44ac82e24e711cf7979f7c01d39562fbff2b9832
heesang <dlagmltkd12@naver.com>
2022-11-15 03:15:08 +0900

    test: LottoValidationUtilsTest 로또 번호 중 46이 있으면 예외 발생

--- src/test/java/lotto/domain/LottoValidationUtilsTest.java
+++ src/test/java/lotto/domain/LottoValidationUtilsTest.java
@@ -27,8 +27,16 @@ class LottoValidationUtilsTest {
 
     @DisplayName("로또 번호 중 0이 있으면 예외 발생")
     @Test
-    void validateRange() {
+    void validateMinimumRange() {
         assertThatThrownBy(() -> LottoValidationUtils.validate(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1~45 사이의 숫자 이어야 합니다.");
+    }
+
+    @DisplayName("로또 번호 중 46이 있으면 예외 발생")
+    @Test
+    void validateMaximumRange() {
+        assertThatThrownBy(() -> LottoValidationUtils.validate(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 로또 번호는 1~45 사이의 숫자 이어야 합니다.");
     }
