22215ca0b127cac453d5141d20ffcb3d59ec9da4
dvlpr0 <rladjswkd@naver.com>
2022-11-14 21:44:47 +0900

    feat: 당첨 번호에 대한 유효성 검사 기능 추가

--- src/main/java/lotto/ExceptionMessage.java
+++ src/main/java/lotto/ExceptionMessage.java
@@ -1,7 +1,8 @@
 package lotto;
 
 public enum ExceptionMessage {
-    PURCHASE_AMOUNT_EXCEPTION("구매 금액은 1000원 이상, 1000원의 배수여야 합니다.");
+    PURCHASE_AMOUNT_EXCEPTION("구매 금액은 1000원 이상, 1000원의 배수여야 합니다."),
+    WINNING_NUMBER_EXCEPTION("당첨 번호는 [1,45] 범위의 유일한 숫자 6개로 이루어져야 합니다.");
 
     private final String MESSAGE;
     private final String ERROR = "[ERROR] ";
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,12 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    private static final int SMALLEST_LOTTERY_NUMBER = 1;
+    private static final int BIGGEST_LOTTERY_NUMBER = 45;
     private static final int LOTTERY_NUMBER_COUNT = 6;
 
     private final List<Integer> numbers;
@@ -12,10 +16,30 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != LOTTERY_NUMBER_COUNT) {
-            throw new IllegalArgumentException();
+    private void validateNumberRange(List<Integer> numbers) {
+        int i = 0;
+        int size = numbers.size() - 1;
+        int currentElement = numbers.get(i);
+
+        while (i < size && (SMALLEST_LOTTERY_NUMBER <= currentElement && currentElement <= BIGGEST_LOTTERY_NUMBER)) {
+            currentElement = numbers.get(i++);
         }
+        if (i != size) {
+            throw (new IllegalArgumentException(ExceptionMessage.WINNING_NUMBER_EXCEPTION
+                    .toString()));
+        }
+    }
+
+    private void validateUniqueness(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != LOTTERY_NUMBER_COUNT) {
+            throw (new IllegalArgumentException(ExceptionMessage.WINNING_NUMBER_EXCEPTION
+                    .toString()));
+        }
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateNumberRange(numbers);
+        validateUniqueness(numbers);
     }
 
 }
