db40b458a0c2405e3d0ed3e65371f71fb3b7bd22
yujaemin <woawoa0513@gmail.com>
2022-11-14 21:23:55 +0900

    feat: 사용자의 구입 금액에 따른 로또 생성 함수 구현
    사용자가 입력한 구매 금액만큼의 로또를 생성하여 List로 반환한다.

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,11 +1,26 @@
 package lotto.controller;
 
-import static lotto.utils.Constants.*;
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+import static lotto.utils.Constants.*;
+import static lotto.utils.Converter.convertToCountsOfLotto;
 
+import java.util.ArrayList;
 import java.util.List;
+import lotto.Lotto;
 
 public class LottoGame {
+
+    public List<Lotto> generateLottoByPurchaseAmount(int purchaseAmount) {
+        int lottoCount = convertToCountsOfLotto(purchaseAmount);
+
+        ArrayList<Lotto> lottos = new ArrayList<>();
+
+        for (int count = 0; count < lottoCount; count++) {
+            lottos.add(new Lotto(generateLottoNumbers()));
+        }
+
+        return lottos;
+    }
 
     private List<Integer> generateLottoNumbers() {
         return pickUniqueNumbersInRange(LOTTO_MIN, LOTTO_MAX, COUNT_OF_NUMBERS_IN_LOTTO);
