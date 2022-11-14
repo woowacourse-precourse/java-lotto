c4c642efd69c9da58dc4b69c9cd96804f15e9130
zcae1020 <zcae1020@naver.com>
2022-11-14 12:50:34 +0900

    fix(lotto): 구입금액을 입력받을시 안내구문 출력 추가

--- src/main/java/lotto/verifier/PurchaseManager.java
+++ src/main/java/lotto/verifier/PurchaseManager.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class PurchaseManager {
 
     public int askAmountOfMoney() throws IllegalArgumentException{
+        System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
 
         validateInput(input);
