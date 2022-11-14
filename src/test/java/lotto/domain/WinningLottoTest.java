baa01161af8c752e717e58e3c2f7d800a6cdedc5
Dabeen Jeong <hello70825@gmail.com>
2022-11-14 23:11:25 +0900

    test: 보너스 번호가 범위를 벗어나면 예외가 발생하는지 확인하는 테스트를 추가

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -2,8 +2,12 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -14,5 +18,20 @@ class WinningLottoTest {
     void createWinningLottoByDuplicateBonusNumber() {
         assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6, 7), 5))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 범위를 벗어나면 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("generateWrongRangeNumber")
+    void createLottoByWrongRangeNumber(List<Integer> numbers, Integer bonusNumber) {
+        assertThatThrownBy(() -> new WinningLotto(numbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> generateWrongRangeNumber() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 0),
+                Arguments.of(List.of(10, 11, 12, 45, 37, 23), 50)
+        );
     }
 }
