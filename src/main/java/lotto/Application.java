b67e4ec9903a0651a5ef89f089321ca34ec5c354
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-14 23:21:42 +0900

    3주차 로또 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,7 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Controller c = new Controller();
+        c.run();
     }
 }
--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -67,7 +67,7 @@ public class View {
             return i / 1000;
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 로또 구입 금액이 올바르지 않습니다.");
-            return inputPrice();
+            throw e;
         }
     }
 
