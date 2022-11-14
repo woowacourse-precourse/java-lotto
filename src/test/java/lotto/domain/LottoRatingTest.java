b72357e617411a4bffc4876cc5f0d7d0922bfbea
Donguk <95donguk.dev@gmail.com>
2022-11-15 03:37:41 +0900

    test: 당첨 기준 확인 테스트

--- src/test/java/lotto/domain/LottoRatingTest.java
+++ src/test/java/lotto/domain/LottoRatingTest.java
@@ -2,9 +2,14 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoRatingTest {
     @DisplayName("로또 번호와 당첨 번호 비교 테스트")
@@ -16,5 +21,31 @@ class LottoRatingTest {
         LottoRating lottoRating = new LottoRating(lottoTicket, winningLotto);
 
         assertThat(lottoRating.compareLotto(lotto)).isEqualTo(3);
+    }
+
+    @DisplayName("당첨 기준 확인 테스트")
+    @ParameterizedTest
+    @MethodSource("generateNumberParameter")
+    void checkWinningTest(List<Integer> lottoNumbers, List<Integer> winningNumbers, int bonusNumber, String tier) {
+        Lotto lotto = new Lotto(lottoNumbers);
+        List<Lotto> lottoTicket = List.of(lotto);
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        LottoRating lottoRating = new LottoRating(lottoTicket, winningLotto);
+
+        lottoRating.rate();
+
+        assertThat(lottoRating.getWinningCountRepository().keySet().stream().findFirst().orElse(null)).isEqualTo(tier);
+    }
+
+    static Stream<Arguments> generateNumberParameter() {
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 2, 3, 24, 35, 36), winningNumbers, bonusNumber, "5등"),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 33, 40), winningNumbers, bonusNumber, "4등"),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 40), winningNumbers, bonusNumber, "3등"),
+                Arguments.of(Arrays.asList(1, 2, 3, 5, 6, 7), winningNumbers, bonusNumber, "2등"),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6), winningNumbers, bonusNumber, "1등")
+        );
     }
 }
