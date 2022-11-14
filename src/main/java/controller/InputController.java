341974361962add800f3a7d3c74235bddb76deb4
egoing <dudqk9696@naver.com>
2022-11-14 15:31:55 +0900

    refactor : 로또 번호생성시 Lotto 클래스 사용하도록 변경

--- src/main/java/controller/InputController.java
+++ src/main/java/controller/InputController.java
@@ -2,44 +2,45 @@ package controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-import domain.LottoPurAmount;
+import lotto.Lotto;
 import util.Constant;
+import view.InputView;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InputController {
 
     InputValidator inputValidator = new InputValidator();
 
     public Integer insertLottoAmount() {
+        InputView.printLottoPrice();
         String stringpuramount = Console.readLine();
-        Integer lottopuramount = Integer.parseInt(stringpuramount);
         inputValidator.checkDigitPrice(stringpuramount);
+        Integer lottopuramount = Integer.parseInt(stringpuramount);
+
         inputValidator.checkDividePrice(lottopuramount);
         return lottopuramount;
     }
 
-    public List<List<Integer>> makeLottoNumberList(LottoPurAmount lottoPurAmount) {
-        List<List<Integer>> makeLottoNumberList = new ArrayList<>();
-        for(int i=0; i<lottoPurAmount.getLottopuramount()/Constant.DIVIDEPRICE; i++) {
-            List<Integer> lottonumber = new ArrayList<>();
-            lottonumber = makeLottoNumber();
-            makeLottoNumberList.add(lottonumber);
-        }
-        return makeLottoNumberList;
+    public List<Lotto> makeLottoNumberList(int lottoPurAmount) {
+        List<Lotto> lottoNumbers = new ArrayList<>();
+        lottoNumbers = makeLottoNumber(lottoPurAmount);
+        return lottoNumbers;
     }
 
-    public List<Integer> makeLottoNumber() {
-        List<Integer> lottonumber = new ArrayList<>();
-        lottonumber = Randoms.pickUniqueNumbersInRange(Constant.MINLOTTOVALUE, Constant.MAXLOTTOVALUE, Constant.LOTTONUMBERQUANTITY);
-        lottonumber.sort(Comparator.naturalOrder());
-        return lottonumber;
+    public List<Lotto> makeLottoNumber(int lottoPurAmount) {
+        int lottoNumbers = lottoPurAmount / 1000;
+        return IntStream.range(0, lottoNumbers)
+                .mapToObj(i -> Randoms.pickUniqueNumbersInRange(1, 45, 6)).map(Lotto::new)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> insertLottoNumber() {
+        InputView.printInputLottoNumber();
         List<Integer> lottonumber = new ArrayList<>();
         String strlottonumber = Console.readLine();
         inputValidator.checkDigitNumber(strlottonumber);
