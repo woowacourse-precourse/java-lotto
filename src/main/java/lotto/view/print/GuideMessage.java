77d134c5157a051b6c6f204238c65ef5995533f7
ykmxxi <ykmxxi97@gmail.com>
2022-11-14 19:03:23 +0900

    refactor: 가이드 메시지 수정

--- src/main/java/lotto/view/print/GuideMessage.java
+++ src/main/java/lotto/view/print/GuideMessage.java
@@ -4,7 +4,7 @@ public enum GuideMessage {
     AMOUNT_INPUT_MESSAGE("구입금액을 입력해 주세요. (1,000원 단위)"),
     PURCHASE_MESSAGE("%d개를 구매했습니다.%n"),
     WINNINGS_INPUT_MESSAGE("당첨 번호를 입력해 주세요. (당첨 번호는 1 ~ 45 숫자 6개 이고, 쉼표(,)를 기준으로 공백없이 구분해주세요.)"),
-    BONUS_INPUT_MESSAGE("보너스 번호를 입력해 주세요. (보너스 번호는 1개 이고, 1 ~ 45 숫자 입니다."),
+    BONUS_INPUT_MESSAGE("보너스 번호를 입력해 주세요. (보너스 번호는 1개 이고, 1 ~ 45 숫자 입니다.)"),
     STATISTICS_MESSAGE("당첨 통계\n---");
 
     private final String message;
