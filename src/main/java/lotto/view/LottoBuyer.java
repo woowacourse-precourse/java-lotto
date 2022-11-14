c790916983a29e356b33f7325388064fd3a2eb6f
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-14 16:09:20 +0900

    refactor : 테스트 에러원인 예외처리 및 코드 수정

--- src/main/java/lotto/view/LottoBuyer.java
+++ src/main/java/lotto/view/LottoBuyer.java
@@ -9,31 +9,48 @@ import lotto.domain.ErrorMessage;
 import lotto.domain.Lotto;
 
 public class LottoBuyer {
-    public static int purchaseLottoCount;
+    public static long purchaseAmount;
+    private static final int LOTTO_PRICE = 1000;
 
     public static List<List<Integer>> purchaseLotto() {
         List<List<Integer>> lottos = new ArrayList<>();
-        System.out.println(Message.PURCHASE_PRICE.getMessage());
-        String purchasePrice = readLine();
-        System.out.println();
-        purchaseLottoCount = Integer.parseInt(purchasePrice) / 1000;
-        validateDivideNumber(purchasePrice);
-        System.out.println(purchaseLottoCount + Message.PURCHASE_COUNT.getMessage());
+        getPurchaseAmount();
+        System.out.println(purchaseAmount + Message.PURCHASE_COUNT.getMessage());
         printLottos(lottos);
         return lottos;
     }
 
+    private static void getPurchaseAmount() {
+        System.out.println(Message.PURCHASE_PRICE.getMessage());
+        String inputPrice = readLine();
+        if (validateInputPrice(inputPrice)) {
+            return;
+        }
+        purchaseAmount = Integer.parseInt(inputPrice) / LOTTO_PRICE;
+    }
+
     private static void printLottos(List<List<Integer>> lottos) {
-        for (int i = 0; i < purchaseLottoCount; i++) {
+        for (int i = 0; i < purchaseAmount; i++) {
             lottos.add(Lotto.createLotto().getNumbers());
             System.out.println(lottos.get(i));
         }
         System.out.println();
     }
 
-    private static void validateDivideNumber(String purchasePrice) {
-        if (Integer.parseInt(purchasePrice) % 1000 != 0) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_PRICE.getErrorMessage());
+    private static boolean validateInputPrice(String inputPrice) {
+        try {
+            Integer.parseInt(inputPrice);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ErrorMessage.ERROR_INPUT_PRICE.getErrorMessage());
+            return true;
+        }
+        validateDivideNumber(inputPrice);
+        return false;
+    }
+
+    private static void validateDivideNumber(String inputPrice) {
+        if (Integer.parseInt(inputPrice) % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_PRICE_THOUSAND_UNITS.getErrorMessage());
         }
     }
 }
