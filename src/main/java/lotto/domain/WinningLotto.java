64b2dca8cbc1b2022da38ae04e33b64573b589f8
KimTaeKang57 <zizoen123@gmail.com>
2022-11-14 17:11:10 +0900

    fix: WinningLotto 수정

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 public class WinningLotto {
     private Lotto lotto;
@@ -12,11 +12,11 @@ public class WinningLotto {
 
     private void validate(Lotto lotto, int bonusNumber) {
         if (lotto.containsNumber(bonusNumber)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 보너스 번호가 이미 로또 번호에 포함됩니다.");
         }
 
         if (bonusNumber < 1 || bonusNumber > 45) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 1과45 사이의 숫자가 입력되어야 합니다.");
         }
     }
 
