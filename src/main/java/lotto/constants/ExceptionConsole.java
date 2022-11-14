be569138ed6cadc55589c483a1c73bef13ebf3ce
alstjr706 <alstjr706@naver.com>
2022-11-14 21:01:21 +0900

    feat : (로또 게임) 예외 처리 관련 로그 구현

---
+++ src/main/java/lotto/constants/ExceptionConsole.java
@@ -0,0 +1,18 @@
+package lotto.constants;
+
+public class ExceptionConsole {
+
+    public void NON_NUMERIC_CHARACTERS() {
+        System.out.println(ExceptionMessage.NON_NUMERIC_CHARACTERS);
+    }
+
+    public void PAYMENT_AMOUNT_ZERO() {
+        System.out.println(ExceptionMessage.PAYMENT_AMOUNT_ZERO);
+    }
+
+    public void THOUSAND_WON_UNIT_EXCEPTION() {
+        System.out.println(ExceptionMessage.THOUSAND_WON_UNIT_EXCEPTION);
+    }
+
+
+}
--- src/main/java/lotto/constants/ExceptionMessage.java
+++ src/main/java/lotto/constants/ExceptionMessage.java
@@ -1,17 +1,24 @@
 package lotto.constants;
 
-public class ExceptionMessage {
+public final class ExceptionMessage {
+    public static final String NON_NUMERIC_CHARACTERS = "[ERROR] 숫자가 아닌 문자를 입력 하였습니다.";
+    public static final String PAYMENT_AMOUNT_ZERO = "[ERROR] 지불하실 금액은 0보다 커야 합니다.";
+    public static final String THOUSAND_WON_UNIT_EXCEPTION = "[ERROR] 금액이 천원 단위가 아니거나 천원 미만을 입력 하였습니다.";
+
+
+
 
     static final String draftMessages1 = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     static final String draftMessages2 = "[ERROR] 중복되는 값을 입력하였습니다.";
-    static final String draftMessages3 = "[ERROR] 금액이 천원 단위가 아니거나 천원 미만을 입력 하였습니다.";
+
 
     static final String draftMessages4 = "[ERROR] 로또 번호는 6개이어야 합니다.";
-    static final String draftMessages5 = "[ERROR] 0으로 나눌 수 없습니다.";
 
-    static final String draftMessages6 = "[ERROR] 숫자가 아닌 문자를 입력 하였습니다.";
+
+
     static final String draftMessages7 = "[ERROR] 특수 문자를 입력 하였습니다.";
     static final String draftMessages8 = "[ERROR] 보너스 번호는 1개만 입력하여야 합니다.";
+
 
 
 }
