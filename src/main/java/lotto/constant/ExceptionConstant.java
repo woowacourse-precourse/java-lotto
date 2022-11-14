d0789cb154baa5b408c35d31ebf217ff9e7cd368
김연진 <love_homebody@naver.com>
2022-11-15 01:19:32 +0900

    feat: 당첨 번호 입력 시 예외 처리 기능 구현

--- src/main/java/lotto/constant/ExceptionConstant.java
+++ src/main/java/lotto/constant/ExceptionConstant.java
@@ -6,7 +6,8 @@ public enum ExceptionConstant {
     NUMBER_INPUT_ERROR("[ERROR] 숫자 입력만 가능합니다."),
     COUNT_ERROR("[ERROR] 6개 숫자 입력만 가능합니다."),
     BONUS_COUNT_ERROR("[ERROR] 보너스 볼은 하나만 추첨 가능합니다."),
-    INVALID_FORMAT_ERROR("[ERROR] 잘못된 형식의 입력입니다.");
+    INVALID_FORMAT_ERROR("[ERROR] 잘못된 형식의 입력입니다."),
+    DUPLICATE_NUMBER_ERROR("[ERROR] 중복된 수 입력입니다.");
 
     private final String message;
 
--- src/main/java/lotto/validation/InputValidation.java
+++ src/main/java/lotto/validation/InputValidation.java
@@ -1,7 +1,10 @@
 package lotto.validation;
 
-import static lotto.constant.ExceptionConstant.NUMBER_INPUT_ERROR;
-import static lotto.constant.ExceptionConstant.PRICE_ERROR;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static lotto.constant.ExceptionConstant.*;
 import static lotto.constant.LottoConstant.PRICE;
 
 public class InputValidation {
@@ -20,5 +23,33 @@ public class InputValidation {
             return true;
         }
         throw new IllegalArgumentException(PRICE_ERROR.getMessage());
+    }
+
+    public static void isValidLottoWinInput(List<String> userInput) {
+        isCorrectCount(userInput);
+        isAllNumber(userInput);
+        isDuplication(userInput);
+    }
+
+    public static void isCorrectCount(List<String> userInput) {
+        if (userInput.size() != 6) {
+            throw new IllegalArgumentException(INVALID_FORMAT_ERROR.getMessage());
+        }
+    }
+
+    public static void isAllNumber(List<String> userInput) {
+        for (String number: userInput) {
+            isNumber(number);
+        }
+    }
+
+    public static void isDuplication(List<String> userInput) {
+        Set<String> notDuplicationNumber = new HashSet<>();
+        for(String input: userInput) {
+            notDuplicationNumber.add(input);
+        }
+        if (notDuplicationNumber.size() != 6) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR.getMessage());
+        }
     }
 }
