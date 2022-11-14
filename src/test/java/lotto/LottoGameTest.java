576e4b6322bcb338880d365a578f9a37bd3ae330
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-15 01:51:13 +0900

    Test : 수정 입력받은 금액에 맞게 로또가 발행되는지 개수 확인 테스트
    LottoGameTest에서가 아니라 UserLottoTest에서 하도록 수정

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -29,34 +29,6 @@ class LottoGameTest extends NsTest {
             assertThat(output()).contains("14개를 구매했습니다.");
         });
     }
-    @DisplayName("구매한 로또 개수만큼 로또 번호를 발행하는지 확인")
-    @Test
-    void getLottoNumberList(){
-        assertRandomUniqueNumbersInRangeTest(
-                () -> {
-                    run("8000");
-                    assertThat(output()).contains(
-                            "8개를 구매했습니다.",
-                            "[8, 21, 23, 41, 42, 43]",
-                            "[3, 5, 11, 16, 32, 38]",
-                            "[7, 11, 16, 35, 36, 44]",
-                            "[1, 8, 11, 31, 41, 42]",
-                            "[13, 14, 16, 38, 42, 45]",
-                            "[7, 11, 30, 40, 42, 43]",
-                            "[2, 13, 22, 32, 38, 45]",
-                            "[1, 3, 5, 14, 22, 45]"
-                    );
-                },
-                List.of(8, 21, 23, 41, 42, 43),
-                List.of(3, 5, 11, 16, 32, 38),
-                List.of(7, 11, 16, 35, 36, 44),
-                List.of(1, 8, 11, 31, 41, 42),
-                List.of(13, 14, 16, 38, 42, 45),
-                List.of(7, 11, 30, 40, 42, 43),
-                List.of(2, 13, 22, 32, 38, 45),
-                List.of(1, 3, 5, 14, 22, 45)
-        );
-    }
     @DisplayName("입력 받은 당첨 로또 번호가 1~45 사이 숫자가 아닌 경우 예외처리 확인")
     @Test
     void getLottoWinningNumberInput1(){
