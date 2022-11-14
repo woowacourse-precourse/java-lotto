021eadb4ea16772713d16ecd4634cb56a7fcba8f
tseop <77163842+tseop@users.noreply.github.com>
2022-11-15 00:11:05 +0900

    test(lotto): LottoCalculator test 진행

--- src/main/java/lotto/lotto/LottoCalculator.java
+++ src/main/java/lotto/lotto/LottoCalculator.java
@@ -7,18 +7,16 @@ public class LottoCalculator {
     private long amount;
     private long profit;
 
-    private static final Buy buy = new Buy();
-
     public LottoCalculator(int amount) {
         this.amount = amount;
     }
 
 
-    public double getRate() { // 수익률
+    public double getRate() {
         return (double) profit / amount * 100;
     }
 
-    public void calPrizeProfit(Map<Rank, Integer> prize) { // 수익 계산
+    public void calPrizeProfit(Map<Rank, Integer> prize) {
         for (Rank rank : Rank.values()) {
             profit += rank.getWinProfit() * prize.get(rank);
         }
