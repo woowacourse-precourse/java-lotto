6be8a1311f331b392d2cb7dbd7954dd262c52fa3
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-14 18:24:10 +0900

    feat : print method 구현

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -1,4 +1,19 @@
 package lotto;
 
 public class View {
+    public void printLine(String s) {
+        try {
+            System.out.println(s);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void print(String s) {
+        try {
+            System.out.print(s);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+    }
 }
