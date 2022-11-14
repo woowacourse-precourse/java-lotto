013cb15d5a1766962a4e3c31ed469ff8d76b5788
Yeongseon Park <yseon406@gmail.com>
2022-11-15 04:28:42 +0900

    refactor: 로또 번호 오름차순 정렬

--- src/main/java/lotto/ui/Printer.java
+++ src/main/java/lotto/ui/Printer.java
@@ -2,6 +2,7 @@ package lotto.ui;
 
 import lotto.Lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Printer {
@@ -9,6 +10,7 @@ public class Printer {
     public void printLotto(int amount, List<Lotto> myLotto) {
         System.out.println(amount + Message.BUY_LOTTO);
         for (Lotto one : myLotto) {
+            Collections.sort(one.getNumbers());
             System.out.println(one.getNumbers());
         }
     }
