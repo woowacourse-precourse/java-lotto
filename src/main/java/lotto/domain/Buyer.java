5c9b5f810e7a4067457c3fef2910ca77d9d7ea4e
kbm6609 <kbm6609@nvaer.com>
2022-11-14 22:51:06 +0900

    feat<lotto> : 로또 구매 금액단위 유효성 검증 기능 구현

--- src/main/java/lotto/domain/Buyer.java
+++ src/main/java/lotto/domain/Buyer.java
@@ -1,12 +1,19 @@
 package lotto.domain;
 
 public class Buyer {
+    private static final int PURCHASE_AMOUNT_UNIT = 1000;
+
     int lottoAmount;
 
-    static void validConsistByNumber(String purchaseAmount){
+    static void validConsistByNumber(String purchaseAmount) {
         try {
             Integer.parseInt(purchaseAmount);
         } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+    static void validDivisionByThousand(int purchaseAmount){
+        if (purchaseAmount % PURCHASE_AMOUNT_UNIT != 0) {
             throw new IllegalArgumentException();
         }
     }
