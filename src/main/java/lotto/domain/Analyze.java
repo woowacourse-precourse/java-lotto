57b540481fdba7bc7e3163a71c93bdea94312bb7
WonSubSub <boxer1995@naver.com>
2022-11-14 20:03:42 +0900

    feat: 사용자의 로또 번호 일치 갯수를 구하는 기능 추가

--- src/main/java/lotto/domain/Analyze.java
+++ src/main/java/lotto/domain/Analyze.java
@@ -1,30 +1,36 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Analyze {
 
-    private final List<Integer> prizenumber;
-    private final List<List> userlottos;
+    private final List<Integer> prizeNumber;
+    private final List<List> userLottos;
     private final Integer bonus;
 
-    Analyze(List<Integer> prizenumber, Integer bonus, List<List> userlottos){
-        this.prizenumber = prizenumber;
+    public Analyze(List<Integer> prizeNumber, Integer bonus, List<List> userLottos){
+        this.prizeNumber = prizeNumber;
         this.bonus = bonus;
-        this.userlottos = userlottos;
+        this.userLottos = userLottos;
     }
 
-    public List<Integer> userlotto(int i){
-        return userlottos.get(i);
-    }
-
-    public void matchCount(List<Integer> userlotto){
-        for (int i = 0; i < userlotto.size(); i++){
-            int count = 0;
-            if (prizenumber.contains(userlotto(i).get(i))) {
+    public Integer matchCount(List<Integer> userLotto){
+        int count = 0;
+        for (int i = 0; i < prizeNumber.size(); i++){
+            if (prizeNumber.contains(userLotto.get(i))) {
                 count += 1;
             }
         }
+        return count;
+    }
+
+    public List<Integer> matchCountPerLotto(){
+        List<Integer> matchPerLotto = new ArrayList<>();
+        for (int i = 0; i < userLottos.size(); i++){
+            matchPerLotto.add(matchCount(userLottos.get(i)));
+        }
+        return matchPerLotto;
     }
 
 
