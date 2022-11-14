c7684f34d25104291e82ad3c49bc1d3a3273dee8
jjmin9797 <jjmin9797@naver.com>
2022-11-14 19:12:38 +0900

    feat(WinningNumbersException): 당첨 번호 안에 공백이 있는지 확인하는 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/service/exceptionType/WinningNumbersException.java
+++ src/main/java/lotto/service/exceptionType/WinningNumbersException.java
@@ -4,6 +4,7 @@ public class WinningNumbersException {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
     private static final String EXCEPTION_WITH_NOT_SIX_ITEMS_MESSAGE = " \',\'를 기준으로 6개의 항목을 입력해야합니다.";
+    private static final String BLANK_EXCEPTION_MESSAGE = " 당첨 번호에 공백은 존재할 수 없습니다.";
     public void checkIfTheWinningNumbersHasSixCharactersBasedOnTheConma(String winningNumbers) {
         String[] numbers = winningNumbers.split(",");
         if (numbers.length != 6) {
@@ -11,4 +12,12 @@ public class WinningNumbersException {
             throw new IllegalArgumentException();
         }
     }
+
+    public void checkForSpacesInWinningNumbers(String winningNumbers) {
+        if (winningNumbers.contains(" ")) {
+            System.out.println(ERROR_MESSAGE + BLANK_EXCEPTION_MESSAGE);
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
