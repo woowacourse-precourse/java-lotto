8451634746786804b69d1742b7258d0234c83a80
parkjungyoon <tbr06057@naver.com>
2022-11-14 13:50:39 +0900

    refactor(Domain): 랜덤 숫자 정렬 방식 변경, 원본 데이터는 유지하고 정렬(Stream API 사용)

--- src/main/java/lotto/domain/RandomNumbers.java
+++ src/main/java/lotto/domain/RandomNumbers.java
@@ -2,17 +2,15 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RandomNumbers {
     private final static int LOTTO_COUNT = 6;
 
     public List<Integer> generateRandomNumbers() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, LOTTO_COUNT);
-        List<Integer> modifiableLottoNumbers = new ArrayList<Integer>(lottoNumbers);
-        Collections.sort(modifiableLottoNumbers);
-        return modifiableLottoNumbers;
+        List<Integer> sortedLottoNumbers = lottoNumbers.stream().sorted().collect(Collectors.toList());
+        return sortedLottoNumbers;
     }
 }
