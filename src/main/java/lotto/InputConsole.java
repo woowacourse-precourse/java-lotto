627a8fd5f5e61679c4877986d0be8fcb267bccbe
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-14 21:39:11 +0900

    feat(InputConsole): 당첨번호를 유저로 부터 입력 받는 기능 구현

--- src/main/java/lotto/InputConsole.java
+++ src/main/java/lotto/InputConsole.java
@@ -2,6 +2,11 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class InputConsole {
 
     public static final String PURCHASE_MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
@@ -12,5 +17,18 @@ public class InputConsole {
         System.out.println(PURCHASE_MONEY_INPUT_MESSAGE);
         int money = Integer.parseInt(Console.readLine());
         return money;
+    }
+
+    public static List<Integer> winningNumber() {
+        System.out.println(WINNING_NUMBER_INPUT_MESSAGE);
+        String numbers = Console.readLine();
+        List<Integer> winningNumbers = splitNumber(numbers);
+        return winningNumbers;
+    }
+
+    private static List<Integer> splitNumber(String numbers) {
+        int[] splitNumbers = Stream.of(numbers.split(",")).mapToInt(Integer::parseInt).toArray();
+        List<Integer> winningNumbers = Arrays.stream(splitNumbers).boxed().collect(Collectors.toList());
+        return winningNumbers;
     }
 }
