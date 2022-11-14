2131cdd4ba8fd8ad389b3a21d7aef0cdb0ee0532
wonseokjee <wsji9404@gmail.com>
2022-11-14 10:35:10 +0900

    Refactor(PlayLotto): 수정 playLottoTicketCount 함수
    playLottoTicketCount에 티켓수에 맞는 for문 돌아갈때마다 인스턴스 생성해서 티켓수만큼의 랜덤로또번호 생성하고 오름차순 정렬.

--- src/main/java/lotto/PlayLotto.java
+++ src/main/java/lotto/PlayLotto.java
@@ -1,13 +1,15 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PlayLotto {
-    CreateLotto createLotto = new CreateLotto();
-    public List<List<Integer>> playLottoTicketCount(Integer TicketCount) {
+    public List<List<Integer>> playLottoTicketCount(Integer ticketCount) {
         List<List<Integer>> allLottoNumber = new ArrayList<>();
-        for (int i = 0; i < TicketCount; i++) {
+        for (int i = 0; i < ticketCount; i++) {
+            CreateLotto createLotto = new CreateLotto();
+            Collections.sort(createLotto.createRandomBall());
             allLottoNumber.add(createLotto.createRandomBall());
         }
         return allLottoNumber;
