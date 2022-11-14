738b024b8d6b49aee0a03e5ff5fef6425ffdc9e8
Laeng <Laeng@users.noreply.github.com>
2022-11-14 16:24:49 +0900

    feat(lotto): 문자열을 통한 Lotto 인스턴스 생성 구현
    `1,2,3,4,5,6`와 같은 숫자와 쉼표로 구성된 텍스트를 통해 Lotto 인스턴스 생성이 가능하도록 했습니다.
    
    BREAKING CHANGE:
    MVC 패턴을 준수하고자 Lotto 클래스의 위치가 변경되었습니다.

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,61 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Env;
+import lotto.Validator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public static Lotto from(String input) {
+        input = input.trim();
+        List<String> raw = List.of(input.split(","));
+
+        return new Lotto(parseIntList(raw));
+    }
+
+    private static List<Integer> parseIntList(List<String> original) {
+        try {
+            return original
+                    .stream()
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력한 내용 중 숫자가 아닌 문자가 있어요.");
+        }
+    }
+
+    public Lotto(List<Integer> numbers) {
+        this.validate(numbers);
+        this.numbers = this.sort(numbers);
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (Validator.isNotCorrectLottoNumberSize(numbers, false)) {
+            throw new IllegalArgumentException("5개 이하의 숫자를 입력했어요. 6개 숫자를 입력해 주세요.");
+        }
+
+        if (Validator.isNotCorrectLottoNumberSize(numbers, true)) {
+            throw new IllegalArgumentException("중복된 숫자를 입력했어요. 중복 없는 6개 숫자를 입력해 주세요.");
+        }
+
+        if (Validator.hasNotCorrectRangeNumbers(numbers)) {
+            throw new IllegalArgumentException("1 ~ 45 숫자를 입력해 주세요.");
+        }
+    }
+
+    private List<Integer> sort(List<Integer> numbers) {
+        return numbers
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+}
--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -2,10 +2,13 @@ package lotto.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -18,10 +21,29 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @ParameterizedTest(name = "규칙을 준수한 문자열을 통해 로또 번호를 생성한다.")
+    @ValueSource(strings = {
+            "1,2,3,4,5,6",
+            "40,41,42,43,44,45"
+    })
+    void createLottoByValidString(String input) {
+        assertDoesNotThrow(() -> Lotto.from(input));
+    }
+
+    @ParameterizedTest(name = "규칙을 준수하지 않은 문자열을 통해 로또 번호를 생성한다.")
+    @ValueSource(strings = {
+            "1,2,3",
+            "-1,-2,-3,-4,-5,-6",
+            "가나다",
+            "가,나,다,라,마,바",
+            "47, 2, 54, 3, 1, 6",
+    })
+    void createLottoByInvalidString(String input) {
+        assertThatThrownBy(() -> Lotto.from(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
