d673b7b61d12418f77c0ea3c0b17cf1390cff8e0
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-14 19:28:20 +0900

    feat develop : Controller 로또 번호 생성 구현

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -1,4 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class Controller {
+    private List<Lotto> lottoNumbers;
+    private List<Integer> winningNumbers;
+
+    public void makeLottoNumbers(int number) {
+        for (int i = 0; i < number; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoNumbers.add(new Lotto(numbers));
+        }
+    }
 }
