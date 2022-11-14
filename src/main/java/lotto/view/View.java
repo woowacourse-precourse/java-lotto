a9dbc11d83edcda5de6876bb8d579a97e4ddce2c
HyeonjungYun <guswnd9971@gmail.com>
2022-11-15 01:37:35 +0900

    "style(View): 주석 추가"

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -7,6 +7,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class View {
 
+    // 로또 번호 비교한 모든 결과를 출력하는 메서드
     public void printResult (HashMap<Integer, Integer> prizeRankings, double totalProfit) {
 
         System.out.printf("3개 일치 (5,000원) - %d개\n", prizeRankings.get(Ranking.FIFTH.getPrize()));
@@ -17,6 +18,7 @@ public class View {
         System.out.printf("총 수익률은 %.1f%%입니다.\n", totalProfit*100);
     }
 
+    // 생성된 로또 번호를 출력하는 메서드
     public void printUncheckedLotto (List<List<Integer>> usersLottos) {
         System.out.printf("%d개를 구매했습니다.\n", usersLottos.size());
 
@@ -25,18 +27,21 @@ public class View {
         }
     }
 
+    // 보너스 번호를 입력하는 메서드
     public String inputBonusNumber () {
         System.out.println("보너스 번호를 입력해주세요.");
 
         return Console.readLine();
     }
 
+    // 돈을 입력하는 메서드
     public String inputMoney () {
         System.out.println("구입 금액을 입력해주세요.");
 
         return Console.readLine();
     }
 
+    // 로또 번호를 입력하는 메서드
     public  String inputLottoNumber () {
         System.out.println("당첨 번호를 입력해주세요.");
 
