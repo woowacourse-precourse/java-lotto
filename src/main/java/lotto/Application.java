741163585d93a32735d15917e86c6b7db2e2e3f1
Kayoung <rkdkdudjd@gmail.com>
2022-11-14 23:12:46 +0900

    refactor(LottoSystem): 에러 접두어를 상수로 정의

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,14 @@
 package lotto;
 
 public class Application {
+    private static final String ERROR_PREFIX = "[ERROR] ";
+
     public static void main(String[] args) {
         LottoSystem lottoSystem = new LottoSystem();
         try {
             lottoSystem.execute();
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] "+e.getMessage());
+            System.out.println(ERROR_PREFIX + e.getMessage());
         }
     }
 }
