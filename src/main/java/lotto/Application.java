8cce95f15df472cd4fc276cc43b67848987dc8e7
MinHyeok Moon <odo27@MinHyeoks-MacBook-Air.local>
2022-11-14 23:30:11 +0900

    feat: 로또 게임 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,48 @@
 package lotto;
 
+import lotto.domain.*;
+
+import java.util.List;
+
 public class Application {
+    private final Shop shop;
+    private Calculator calculator;
+
+    private Application() {
+        shop = new Shop();
+    }
+
+    private void start() {
+        int money = Player.getMoney();
+        if (money == 0) {
+            return;
+        }
+        List<Lotto> lottos = shop.buyLottos(money);
+        List<Integer> winningNumbers = Player.getWinningNumbers();
+        if (winningNumbers == null) {
+            return;
+        }
+        int bonusNumber = Player.getBonusNumber();
+        Result result = calculateResult(lottos, winningNumbers, bonusNumber);
+        System.out.println(result.toString());
+        printReturnRate(money);
+    }
+
+    private void printReturnRate(int money) {
+        System.out.printf("총 수익률은 %.1f", calculator.returnRate(money));
+        System.out.println("%입니다.");
+    }
+
+    private Result calculateResult(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber) {
+        calculator = new Calculator(winningNumbers, bonusNumber);
+        for (Lotto lotto : lottos) {
+            calculator.compare(lotto);
+        }
+        return calculator.getResult();
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application app = new Application();
+        app.start();
     }
 }
