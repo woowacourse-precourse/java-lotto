cf885e9f042ee3cdb6c40d85825cc666521e9dd2
sosow0212 <sosow0212@naver.com>
2022-11-14 09:14:38 +0900

    refactor : 공통으로 들어오는 "ERROR" 메시지를 상수화

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -9,6 +9,7 @@ public class LottoException {
     private final static int PERMITTED_LOTTO_LENGTH = 6;
     private final static int MIN_LOTTO_NUMBER = 1;
     private final static int MAX_LOTTO_NUMBER = 45;
+    private final static String ERROR = "[ERROR] ";
 
     public void validateLottoNumbers(List<Integer> numbers) {
         validateLottoNumbersLength(numbers);
@@ -23,7 +24,7 @@ public class LottoException {
 
     public void validateLottoNumbersLength(List<Integer> numbers) {
         if (numbers.size() != PERMITTED_LOTTO_LENGTH) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개 입력해주세요.");
+            throw new IllegalArgumentException(ERROR + "로또 번호는 6개 입력해주세요.");
         }
     }
 
@@ -31,27 +32,27 @@ public class LottoException {
         Set<Integer> numbers = new HashSet<>();
         inputNumbers.stream().map(number -> numbers.add(number)).collect(Collectors.toList());
         if (numbers.size() != PERMITTED_LOTTO_LENGTH) {
-            throw new IllegalArgumentException("[ERROR] 번호 중에 중복 번호가 있습니다.");
+            throw new IllegalArgumentException(ERROR + "번호 중에 중복 번호가 있습니다.");
         }
     }
 
     public void validateNumbersPermittedRange(List<Integer> numbers) {
         for (int number : numbers) {
             if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
-                throw new IllegalArgumentException("[ERROR] 로또의 범위는 1~45입니다.");
+                throw new IllegalArgumentException(ERROR + "로또의 범위는 1~45입니다.");
             }
         }
     }
 
     public void validateBonusNumberPermittedRange(int bonusNumber) {
         if (bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
-            throw new IllegalArgumentException("[ERROR] 로또의 범위는 1~45입니다.");
+            throw new IllegalArgumentException(ERROR + "로또의 범위는 1~45입니다.");
         }
     }
 
     public void validateBonusNumberOverlapNumbers(List<Integer> numbers, int bonusNumber) {
         if (numbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호가 중복됩니다.");
+            throw new IllegalArgumentException(ERROR + "보너스 번호가 중복됩니다.");
         }
     }
 }
--- src/main/java/lotto/exception/UserBuyingException.java
+++ src/main/java/lotto/exception/UserBuyingException.java
@@ -4,6 +4,7 @@ import java.util.NoSuchElementException;
 
 public class UserBuyingException {
     private final static int LOTTO_PRICE = 1000;
+    private final static String ERROR = "[ERROR] ";
 
     public void validateBuyingException(String buyingPrice) {
         validIsNumber(buyingPrice);
@@ -13,14 +14,14 @@ public class UserBuyingException {
     public void validPrice(String pay) {
         int buyingPrice = Integer.parseInt(pay);
         if (buyingPrice % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException("[ERROR] 구매는 1,000원 단위로 가능합니다.");
+            throw new IllegalArgumentException(ERROR + "구매는 1,000원 단위로 가능합니다.");
         }
     }
 
     public static void validIsNumber(String input) {
         for (int i = 0; i < input.length(); i++) {
             if (!Character.isDigit(input.charAt(i))) {
-                System.out.println("[ERROR] 입력은 숫자만 가능합니다.");
+                System.out.println(ERROR + "입력은 숫자만 가능합니다.");
                 throw new NoSuchElementException();
             }
         }
