26171bf04e6337d67459699dc4655761ea3ff567
Sieun Park <psieun0606@gmail.com>
2022-11-15 04:25:44 +0900

    feat: 구입 금액에 대한 정보 저장하는 기능 추가

---
+++ src/main/java/lotto/Domain/PurchasingLotto.java
@@ -0,0 +1,10 @@
+package lotto.Domain;
+
+public class PurchasingLotto {
+
+    private int amountOfLotto;
+
+    public PurchasingLotto(int amountOfLotto) {
+        this.amountOfLotto = amountOfLotto;
+    }
+}
