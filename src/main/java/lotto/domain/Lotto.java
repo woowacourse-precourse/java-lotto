43cf6b7551d23b87a6747d55113419a9be0288c9
This2sho <zx00018@naver.com>
2022-11-14 20:49:51 +0900

    feat(Lotto): 로또 번호에 1~45만 사용되도록 유효성 검사 로직 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
+    private static final int MAX_NUMBER = 45;
+    private static final int MIN_NUMBER = 1;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -15,6 +17,7 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         validateOverSize(numbers);
         validateDuplicate(numbers);
+        validateNumber(numbers);
     }
 
     private void validateOverSize(List<Integer> numbers) {
@@ -28,6 +31,14 @@ public class Lotto {
 
         if (numbersToSet.size() != numbers.size()) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumber(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException();
+            }
         }
     }
 }
