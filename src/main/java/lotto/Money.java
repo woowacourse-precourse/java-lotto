29d4aa5ec81c5e97e54b42bb3b1121e5aebd9d59
younghyun-na <younghyunna@naver.com>
2022-11-14 22:56:25 +0900

    :sparkles: feat: 입력된 로또 구입 금액이 잘못됐을 시 에러 메시지 출력하는 기능 추가

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -2,13 +2,28 @@ package lotto;
 
 public class Money {
 
-  private int money;
+  private final int money;
 
-  public Money(){
+  public Money(int money) {
+    validate(money);
     this.money = money;
   }
 
-  public int getMoney(){
-    return money;
+  public void validate(int money) {
+    validateMinimum(money);
+    validateDevided(money);
   }
+
+  public void validateMinimum(int money) {
+    if (money < 1000) {
+      throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 이상이어야 합니다.");
+    }
+  }
+
+  public void validateDevided(int money) {
+    if (money % 1000 != 0) {
+      throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원으로 나누어 떨어져야 합니다.");
+    }
+  }
+
 }
