94f81fb00232dcd4a129eb08476266b0d1106ef1
Woojin Lee <dhkep03@gmail.com>
2022-11-14 21:56:46 +0900

    test(InputValidator): 입력 금액 유효성 검사 테스트 추가 - 0으로 시작

--- src/test/java/lotto/ui/InputValidatorTest.java
+++ src/test/java/lotto/ui/InputValidatorTest.java
@@ -24,6 +24,13 @@ class InputValidatorTest {
             assertThatThrownBy(() -> InputValidator.checkInputMoney("1000000000"))
                     .isInstanceOf(IllegalArgumentException.class);
         }
+
+        @DisplayName("0으로 시작하는 숫자 -> 예외 발생")
+        @Test
+        void StartWithZero() {
+            assertThatThrownBy(() -> InputValidator.checkInputMoney("01000"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
     @DisplayName("당첨 번호의 유효성을 검사한다.")
