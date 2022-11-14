bede3902c5acf90a1b4cfdadb026c4f564014d5e
Sangin <ksiisk99@gmail.com>
2022-11-14 13:34:15 +0900

    feat: 구매한 로또 출력

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -4,6 +4,8 @@ import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
+
 public class Controller {
     private final InputView inputView;
     private final OutputView outputView;
@@ -23,9 +25,17 @@ public class Controller {
             money = inputView.inputMoney();
             purchaserId = lottoService.generatePurchaser(money);
             lottoService.buyLotto(purchaserId);
+            printLottos(lottoService.findPurchaserLottos(purchaserId));
         } catch (IllegalArgumentException e) {
             outputView.printException(e.getMessage());
             return;
+        }
+    }
+
+    private void printLottos(List<List<Integer>> purchaserLottos) {
+        outputView.printLottoQuantity(purchaserLottos.size());
+        for(List<Integer> purchaserLotto : purchaserLottos){
+            outputView.printLotto(purchaserLotto);
         }
     }
 
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,26 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
     public void printException(String message) {
         System.out.println("[ERROR] " + message);
+    }
+
+    public void printLottoQuantity(int lottoQuantity){
+        System.out.println(String.valueOf(lottoQuantity)+"개를 구매했습니다.");
+    }
+
+    public void printLotto(List<Integer> purchaserLotto) {
+        System.out.print("[");
+
+        for(int i=0;i<6;i++){
+            System.out.print(purchaserLotto.get(i));
+            if(i!=5){
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println("]");
     }
 }
