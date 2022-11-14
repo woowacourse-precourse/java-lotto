19723918c6044f01641f8862f71ff6d37790c6a1
tlsqj <rnjstlsqja@inha.edu>
2022-11-14 22:23:11 +0900

    fix(StartLotto.java): lottoArray 수정
    -lottoArray 의 행이 0으로 고정되는 현상
    -lottoArray = new int[lottoCount][6]; 을 lottoCount 를 입력받은 이후로 위치

--- src/main/java/lotto/StartLotto.java
+++ src/main/java/lotto/StartLotto.java
@@ -7,13 +7,14 @@ public class StartLotto {
 
     private int purchasedValue;
     private int lottoCount;
-    private int[][] lottoArray = new int[lottoCount][6];
+    private int[][] lottoArray;
     private int[] userInputLottoNumber = new int[6];
     private int userInputBonusNumber;
 
     public void startLotto(){
         this.purchasedValue = input.buyLotto();
         this.lottoCount = input.checkHowManyLotto(purchasedValue);
+        lottoArray = new int[lottoCount][6];
         makeLotto.makeLottoNumber(lottoArray, lottoCount);
         input.userInputLottoNumber(userInputLottoNumber, userInputBonusNumber);
         check.numberCompare(lottoCount, lottoArray, userInputLottoNumber, userInputBonusNumber, purchasedValue);
