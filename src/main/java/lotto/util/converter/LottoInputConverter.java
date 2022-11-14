648b4b02f784b3887c10e3073f6161f20b5511ee
hyun0404woo <hyun0404woo@gmail.com>
2022-11-14 20:54:41 +0900

    feat: 로또시스템 입력 변환에 필요한 메서드 기본형 생성
    - 구매 가격 입력
    - 당첨 로또 번호 입력
    - 보너스 번호 입력

--- src/main/java/lotto/util/converter/LottoInputConverter.java
+++ src/main/java/lotto/util/converter/LottoInputConverter.java
@@ -1,10 +1,21 @@
 package lotto.util.converter;
 
 import lotto.util.ConstValue.LottoConst;
+import lotto.util.Validator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoInputConverter {
+
+    private final static String LOTTO_MAIN_NUMBER_INPUT_PATTERN =
+            String.format("^[0-9]+(,[0-9]+){5}");
+
+    private final static String LOTTO_BONUS_NUMBER_INPUT_PATTERN =
+            String.format("^[0-9]+");
+
+    private final static String USER_BUY_MONEY_PATTERN =
+            String.format("^[0-9]+");
 
     private final Converter<String, List<Integer>> stringToIntListConverter;
     private final Converter<String, Integer> stringToIntConverter;
@@ -12,5 +23,20 @@ public class LottoInputConverter {
     public LottoInputConverter() {
         stringToIntListConverter = new StringToIntListConverter(LottoConst.LOTTO_MAIN_NUMBER_DELIMETER);
         stringToIntConverter = new StringToIntConverter();
+    }
+
+    public int userLottoPurchaseCostToInt(final String input) {
+        final List<Validator<String>> validators = new ArrayList<>();
+        return stringToIntConverter.convert(input, validators);
+    }
+
+    public List<Integer> winningLottoNumberToList(final String input) {
+        final List<Validator<String>> validators = new ArrayList<>();
+        return stringToIntListConverter.convert(input, validators);
+    }
+
+    public int bonusLottoNumberToInt(final String input) {
+        final List<Validator<String>> validators = new ArrayList<>();
+        return stringToIntConverter.convert(input, validators);
     }
 }
