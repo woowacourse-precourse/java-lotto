a8056c8dc98314775c0fc1417962267c7f45ef9c
krkr5628 <krkr5628@gmail.com>
2022-11-14 12:54:34 +0900

    v3.0 Application의 출력 기능만 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,10 +6,26 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("구매금액을 입력해 주세요.");
         String input_price = Console.readLine().trim();
-        int price = Integer.parseInt(input_price);
-        // 입력값 검수
-        int buy_payper = price/1000;
-        System.out.println(buy_payper + "개를 구매했습니다.");
-        //Lotto.computer 에서 램덤수 6세트 받기
+        System.out.println(input_price);
+        /*
+        입력값 검수
+        */
+        int input_price_calculation = Integer.parseInt(input_price);
+        int buy_paper = input_price_calculation/1000;
+        System.out.println("\n" + buy_paper + "개를 구매했습니다.");
+        /*
+        Lotto.computer 에서 램덤수 6세트 받기
+        */
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        String input_number = Console.readLine().trim();
+        System.out.println(input_number);
+        //
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        String input_bonus_number = Console.readLine().trim();
+        System.out.println(input_bonus_number);
+        /*
+        당첨분석
+         */
+        System.out.println("\n당첨 통계\n---");
     }
 }
