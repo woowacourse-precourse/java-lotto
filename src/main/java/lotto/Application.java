17e8e9486b5d2eb7b4e1b44d74e19197cab1a273
baekeunsun <eunsun5460@gmail.com>
2022-11-14 19:04:51 +0900

    refactor(model/Bonus): bonus모델 클래스로 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,7 @@ package lotto;
 import lotto.controller.Compare;
 import lotto.controller.Issue;
 import lotto.controller.Yield;
+import lotto.model.Bonus;
 import lotto.model.Lotto;
 import lotto.view.InputView;
 
@@ -13,8 +14,9 @@ public class Application {
         Issue user = new Issue();
         List<Integer> numbers = InputView.InputWinNumber();
         Lotto winLotto = new Lotto(numbers);
-        int bonusLotto = InputView.InputBonusNumber();
-        Compare userCompare = new Compare(user.userLotto, winLotto,bonusLotto);
+        int bonusNumber = InputView.InputBonusNumber();
+        Bonus bonusLotto = new Bonus(bonusNumber);
+        Compare userCompare = new Compare(user.userLotto, winLotto,bonusLotto.bonusNumber);
         new Yield(user.userPrice,userCompare);
     }
 }
---
+++ src/main/java/lotto/model/Bonus.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Bonus {
+
+    public final int bonusNumber;
+
+    public Bonus(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+}
