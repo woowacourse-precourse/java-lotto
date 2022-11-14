b61efcf26c8938a8f8ed88831da513a5e8257c60
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-14 17:33:07 +0900

    feat(input view): 예외 사항 추가
    예외 사항 추가
    - 숫자가 아닌 값이 들어올 경우

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,10 +10,16 @@ public class InputView {
     private static final String INPUT_MONEY_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String INPUT_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String NO_NUMBERIC_VALUE_ERROR = "[ERROR] 숫자가 아닌 값은 입력할 수 없습니다.";
+
 
     public static int moneyAmount(){
         System.out.println(INPUT_MONEY_AMOUNT);
-        return Integer.parseInt(Console.readLine());
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (Exception e){
+            throw new IllegalArgumentException(NO_NUMBERIC_VALUE_ERROR);
+        }
     }
 
     public static List<Integer> lottoNumbers(){
@@ -23,7 +29,11 @@ public class InputView {
 
     public static int bonusNumber(){
         System.out.println(INPUT_BONUS_NUMBER);
-        return Integer.parseInt(Console.readLine());
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (Exception e){
+            throw new IllegalArgumentException(NO_NUMBERIC_VALUE_ERROR);
+        }
     }
 
     private static List<Integer> splitLottoNumbers(String lotto){
