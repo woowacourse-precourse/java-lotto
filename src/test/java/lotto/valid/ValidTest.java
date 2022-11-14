8708d152ef755134169e668a1d8cc8815ee11dad
leewoojin <zmfpdl64@naver.com>
2022-11-14 18:35:45 +0900

    Test: Valid 테스트 작성
    Valid가 가지고 있는 예외 케이스를 작성했다.
    1. size (6)
    2. number (숫자인가)
    3. range(1 ~ 45) 인가
    4. under_thousand(1000미만 단위)
    5. empty 값이 존재하는가

--- src/test/java/lotto/valid/ValidTest.java
+++ src/test/java/lotto/valid/ValidTest.java
@@ -1,4 +1,52 @@
+package lotto.valid;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+
 class ValidTest {
-  
+
+    @Test
+    void check_size() {
+        String str = "1,2,3,4,5,6,7";
+        assertThatThrownBy(() -> Valid.check_size(str))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Test
+    void check_number() {
+        String str = "1,2,3,4,a,6";
+        assertThatThrownBy(() -> Valid.check_number(str))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void check_Overlap() {
+        String str = "1,2,3,4,5,5";
+        assertThatThrownBy(() -> Valid.check_Overlap(str))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void check_Range() {
+        String str = "0,2,3,4,5,45";
+        assertThatThrownBy(() -> Valid.check_Range(str))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void check_under_thousand() {
+        String str = "1300";
+        assertThatThrownBy(() -> Valid.check_under_thousand(str))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void check_empty() {
+        String str = "1,2,3,4,,6";
+        assertThatThrownBy(() -> Valid.check_empty(str))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
