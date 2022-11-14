2d4138d26c9c66ca8c87728e353843983f8b2f39
dener8 <8dener@gmail.com>
2022-11-14 21:28:18 +0900

    feat(NumbersMaker): 구매한 만큼의 로또 번호를 생성하는 makePurchaserNumbers() 구현

--- src/main/java/lotto/NumbersMaker.java
+++ src/main/java/lotto/NumbersMaker.java
@@ -21,7 +21,17 @@ public class NumbersMaker {
         return bonusNumber;
     }
 
+    public List<List<Integer>> makePurchaserNumbers(int purchaseAmount) {
+        List<List<Integer>> purchaserNumbers = new ArrayList<>();
+        List<Integer> oneGameNumbers;
+        int gameCycle = purchaseAmount / 10;
 
+        for (int i = 0; i < gameCycle; i++) {
+            oneGameNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            purchaserNumbers.add(oneGameNumbers);
+        }
+        return purchaserNumbers;
+    }
 
 
 }
