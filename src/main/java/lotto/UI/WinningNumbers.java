ac3f15c30984e5f8ce5cbed45bd98fe6f7bd4b0d
canbeyour <wuzucy@gmail.com>
2022-11-15 03:33:43 +0900

    feat: add ability to get winning numbers

---
+++ src/main/java/lotto/UI/WinningNumbers.java
@@ -0,0 +1,30 @@
+package lotto.UI;
+
+import lotto.Lotto;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class WinningNumbers {
+    private final List<Integer> numbers;
+    public WinningNumbers(String input) {
+        List<Integer>numbers = convertInputToNumbers(input);
+        this.numbers = new Lotto(numbers).getNumbers();
+    }
+    public static List<Integer> convertInputToNumbers(String input) {
+        Error error = new Error();
+        List<Integer> result = new ArrayList<>();
+        try {
+            result = Arrays.stream(input.split(","))
+                    .map(s->Integer.valueOf(s))
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            error.outOfRange();
+        }
+        return result;
+    }
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+}
+
