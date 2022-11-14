001e30210a5323f4fab12974eb5c56c70b21b785
Beginner <dkstmdwn17@naver.com>
2022-11-14 23:48:40 +0900

    feat : 로또 게임 머신의 비즈니스 로직을 작성하였다.
    로또 게임에 필요한 데이터를 관리하는 LottoRepository에 접근해서 원하는 비즈니스 로직을 구현하는 서비스 계층의 행위를 구성하였다.
    
    LottoGameMachineServiceImpl를 통해 이 행위를 구체적으로 구현한다.

--- src/main/java/lotto/service/LottoGameMachineServiceImpl.java
+++ src/main/java/lotto/service/LottoGameMachineServiceImpl.java
@@ -1,4 +1,54 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
+import lotto.domain.enumeration.Rating;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
 public class LottoGameMachineServiceImpl implements LottoGameMachineService{
+    @Override
+    public void enterMoney(int money) {
+
+    }
+
+    @Override
+    public void enterTheWinningNumbers(Lotto winningNumbers) {
+
+    }
+
+    @Override
+    public void enterBonusNumber(int bonusNumber) {
+
+    }
+
+    @Override
+    public LinkedHashMap<Rating, Integer> calculateStatistics() {
+        return null;
+    }
+
+    @Override
+    public int readLottoQuantity() {
+        return 0;
+    }
+
+    @Override
+    public List<Lotto> readLottoNumbers() {
+        return null;
+    }
+
+    @Override
+    public int readMoney() {
+        return 0;
+    }
+
+    @Override
+    public List<Integer> readWinningNumbers() {
+        return null;
+    }
+
+    @Override
+    public int readBonusNumber() {
+        return 0;
+    }
 }
