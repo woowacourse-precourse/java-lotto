6e2229fb3c7a78ab546716a44557fe768c0a63a9
bellljun3395 <195850@jnu.ac.kr>
2022-11-14 15:26:41 +0900

    feat(CorrectLottoNumbers): 구현 compareLottoNumber
    - 외부값 lotto와 correctLottoNumbers를 비교하여 Ranking을 반환한다.

--- src/main/java/lotto/domain/lotto/correctLotto/CorrectLottoNumbers.java
+++ src/main/java/lotto/domain/lotto/correctLotto/CorrectLottoNumbers.java
@@ -11,6 +11,8 @@ public class CorrectLottoNumbers {
     private final String WARM_CORRECT_LOTTO_NUMBERS_SIZE = WARM + "로또 당첨 번호는 7개이어야 합니다.";
     private final int CORRECT_LOTTO_NUMBER_SIZE = 7;
     private final int BONUS_INDEX = 6;
+    private final int COUNT_INITIAL_VALUE = 0;
+    private final int COUNT_BRANCH_VALUE = 6;
 
 
     private final List<LottoNumber> correctLottoNumbers;
@@ -32,5 +34,37 @@ public class CorrectLottoNumbers {
     private void checkDuplicatedNumber(List<LottoNumber> lottoNumbers) {
         Validate.validateDuplicateNumber(lottoNumbers,
                 (baseSize, compareLottoNumbers) -> baseSize != compareLottoNumbers.size());
+    }
+
+    public Ranking compareLottoNumber(Lotto lotto) {
+        List<Integer> lottoNumbers = lotto.getNumbers();
+        int count = COUNT_INITIAL_VALUE;
+        boolean isContainBonus = false;
+
+        for (Integer lottoNumberValue : lottoNumbers) {
+            LottoNumber lottoNumber = new LottoNumber(lottoNumberValue);
+            count = isContainCorrectNumber(count, lottoNumber);
+        }
+
+        if (count == COUNT_BRANCH_VALUE) {
+            isContainBonus = this.isContainBonus(lottoNumbers);
+        }
+
+        return Ranking.makeRanking(count, isContainBonus);
+    }
+
+    private int isContainCorrectNumber(int count, LottoNumber lottoNumber) {
+        List<LottoNumber> baseCorrectLottoNumbers = this.correctLottoNumbers;
+        if (baseCorrectLottoNumbers.contains(lottoNumber)) {
+            count++;
+        }
+        return count;
+    }
+
+    private boolean isContainBonus(List<Integer> lottoNumbers) {
+        if (lottoNumbers.contains(this.bonusNumber.getLottoNumber())) {
+            return true;
+        }
+        return false;
     }
 }
