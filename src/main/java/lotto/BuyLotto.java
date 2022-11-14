ec7ac26d7da4c26a453ce81c04eb5d7cfd260071
ongguri <rkdtjsdyd321@naver.com>
2022-11-14 16:43:27 +0900

    로또 구입과 예외처리

---
+++ src/main/java/lotto/BuyLotto.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+public class BuyLotto {
+    private final int price = 1000;
+    private final String money;
+
+    public BuyLotto() {
+        System.out.println("구입금액을 입력해 주세요.");
+        this.money = Console.readLine();
+        checkMoney(money);
+    }
+
+    public void checkMoney(String money) {
+        try{
+            int check_money = Integer.parseInt(money);
+            if(check_money < 1000) {
+                throw new IllegalArgumentException("[ERROR] 1000원 이상 입력해 주세요.");
+            }
+        } catch (Exception e){
+            throw new IllegalArgumentException("[ERROR] 구입금액은 정수만 입력해 주세요");
+        }
+    }
+}
