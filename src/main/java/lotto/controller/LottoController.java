b18b343f5f1b486ba9b2c5c022952ff0742f4f22
Qulip <ilkwon.ryu@gmail.com>
2022-11-14 21:30:41 +0900

    feat: 로또 구매 내역 출력 LottoController, LottoView 연결

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -16,6 +16,10 @@ public class LottoController {
         return LottoService.amountInputToInteger(input);
     }
 
+    public static void printLottoPurchaseAmount(int amount, List<Lotto> lottoNumbers) {
+        LottoView.printLottoPurchaseResult(amount, lottoNumbers);
+    }
+
     public static int getLottoPurchaseCount(int amount) {
         return LottoService.getLottoPurchaseCount(amount);
     }
