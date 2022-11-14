f4b2a637d71cd6810338bff58a5d2dbbfb27b015
MinjeongKong <kong1301@ajou.ac.kr>
2022-11-15 01:48:54 +0900

    refactor: 상수 순서 변경

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,12 +1,13 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST(6, false, 2000000000),
-    SECOND(5,true,30000000),
-    THIRD(5,false,1500000),
-    FOURTH(4, false, 50000),
+    NONE(0,false,0),
     FIFTH(3,false,5000),
-    NONE(0,false,0);
+    FOURTH(4, false, 50000),
+    THIRD(5,false,1500000),
+    SECOND(5,true,30000000),
+    FIRST(6, false, 2000000000);
+
 
     private final int cnt;
     private final boolean bonus;
