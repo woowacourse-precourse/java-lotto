2ec716c21937574f9035ccec6e6233ed8645b499
mirasoy <nkskjjbb@gmail.com>
2022-11-14 17:37:20 +0900

    [Feat](LottoGameController) : 로또 발행 진행 프로세스

--- src/main/java/lotto/LottoGameController.java
+++ src/main/java/lotto/LottoGameController.java
@@ -1,49 +1,61 @@
 package lotto;
 
+import java.util.List;
+
+import dto.IssueLottoInfo;
 import dto.LottoGameInfo;
 import util.InputUtil;
 
 /**
-* 
-* 로또 게임 진행
-* 
-* @author mirasoy
-* @version 0.1, 작업 내용
-*/
+ * 
+ * 로또 게임 진행
+ * 
+ * @author mirasoy
+ * @version 0.1, 작업 내용
+ */
 public class LottoGameController {
-	
+
 	private LottoGameInfo lottoGameInfo;
-	
+
 	public LottoGameController() {
 		lottoGameInfo = new LottoGameInfo();
 	}
-	
-	
-	//구입금액 입력받기
-	public void inputPurchaseAmount(){
+
+	// 구입금액 입력받기
+	public void inputPurchaseAmount() {
 		long inputPrice = InputUtil.inputPurchaseAmount();
 		lottoGameInfo.setInputPrice(inputPrice);
 	}
 
-	//로또 발행하기
-	public void issueLotto(){
-	   // TODO: 추가 기능 구현
+	// 로또 발행하기
+	public void issueLotto() {
+
+		List<IssueLottoInfo> issueLottoInfo = lottoGameInfo.getIssueLotto();
+		for (int i = 0; i < lottoGameInfo.getInputPrice() / 1000; i++) {
+			issueLottoInfo.add(new IssueLottoInfo());
+		}
+		lottoGameInfo.setIssueLotto(issueLottoInfo);
+
 	}
-	
-	//당첨번호 입력받기
-	public void inputWinningNumber(){
+
+	// 발행번호 출력하기 
+	public void printIssueLotto() {
 		// TODO: 추가 기능 구현
 	}
 	
-	//보너스 번호 입력받기
-	public void inputBonusNumber(){
+	// 당첨번호 입력받기
+	public void inputWinningNumber() {
 		// TODO: 추가 기능 구현
 	}
-	
-	//결과 출력하기
-	public void printResult(){
+
+	// 보너스 번호 입력받기
+	public void inputBonusNumber() {
 		// TODO: 추가 기능 구현
 	}
-	
+
+	// 결과 출력하기
+	public void printResult() {
+		// TODO: 추가 기능 구현
+	}
 
 }
