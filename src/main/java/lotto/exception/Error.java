99d5d47bb9ae8ae330bcfd168ef3b872e0e9f4bc
im-shung <seoyoung1113@gmail.com>
2022-11-15 04:19:41 +0900

    feat: :sparkles: 에러 코드 추가

--- src/main/java/lotto/exception/Error.java
+++ src/main/java/lotto/exception/Error.java
@@ -3,7 +3,8 @@ package lotto.exception;
 public enum Error {
     WRONG_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     OVER_SIZE("로또 번호의 개수는 6개여야만 합니다."),
-    NOT_DUPLICATE("로또 번호는 중복되지 않아야 합니다.");
+    NOT_DUPLICATE("로또 번호는 중복되지 않아야 합니다."),
+    WRONG_AMOUNT("금액을 1,000단위로 입력해야 합니다.");
 
     private final String message;
 
@@ -12,6 +13,6 @@ public enum Error {
     }
 
     public String getMessage() {
-        return "[Error]" + message;
+        return "[ERROR] " + message;
     }
 }
