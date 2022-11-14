cf5a79dd0b23de18ee56640781b48211d0b834bc
박승민 <tmddudals369@naver.com>
2022-11-14 11:25:05 +0900

    feat: 로또 번호 오름차순 정렬 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import lotto.domain.Validator;
 
 public class Lotto {
@@ -8,10 +11,15 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         Validator.checkDuplicate(numbers);
-        this.numbers = numbers;
+        this.numbers = sortNumbers(numbers);
     }
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    private List<Integer> sortNumbers(List<Integer> numbers) {
+        Set<Integer> sort = new TreeSet<>(numbers);
+        return new ArrayList<>(sort);
     }
 }
