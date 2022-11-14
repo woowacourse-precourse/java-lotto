62215010df0be81d86b157f952483dd8d7888e38
wonseokjee <wsji9404@gmail.com>
2022-11-15 01:03:47 +0900

    Feat(Application): 추가 main 함수
    아직 전체적으로 연결되어있지 않음.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,38 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CreateLotto createLotto = new CreateLotto();
+        PlayLotto playLotto = new PlayLotto();
+
+        createLotto.textTicketPrice();
+        Integer money = createLotto.Money(createLotto.inputMoney());
+
+        /*8개를 구매했습니다.*/
+        User user = new User(money);
+        user.textTicketcount();
+        List<List<Integer>> allTicket = playLotto.allTicket(money);
+
+        /*당첨번호입력*/
+        createLotto.textWinningNumber();
+        createLotto.winningNumber(createLotto.inputWinningNumber());
+
+        /*보너스 번호 입력*/
+        createLotto.textBounsNumber();
+        createLotto.bonusNumber(createLotto.inputBonusNumber());
+
+        /*당첨통계*/
+        List<Integer> correctNumber = playLotto.allTicketCheckCorrectNumber(allTicket, createLotto.inputWinningNumber(), createLotto.inputBonusNumber());
+        playLotto.print5th(correctNumber);
+        playLotto.print4th(correctNumber);
+        playLotto.print3rd(correctNumber);
+        playLotto.print2nd(correctNumber);
+        playLotto.print1st(correctNumber);
+
+        double yield = playLotto.yield(money, correctNumber);
+        playLotto.printYield(yield);
     }
 }
