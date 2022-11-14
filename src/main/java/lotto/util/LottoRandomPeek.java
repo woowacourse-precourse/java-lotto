c8b67e7ceb2ee931018bd56a2d2b8bedf7327e1c
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-14 23:00:15 +0900

    feat: LottoRandomPeek 클래스 생성

---
+++ src/main/java/lotto/util/LottoRandomPeek.java
@@ -0,0 +1,10 @@
+package lotto.util;
+
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoRandomPeek {
+    private final List<Lotto> randomLottos = new ArrayList<>();
+}
