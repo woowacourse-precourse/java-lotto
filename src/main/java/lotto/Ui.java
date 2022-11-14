701901cc41a4048b53dfe39dfa8811a9f4887779
ensk26 <tjensk26@gmail.com>
2022-11-14 23:15:49 +0900

    feat(Ui): 당첨 번호를 입력받는 메서드
    새로운 메서드 추가  to Ui
    - 관련 문구 출력과 입력
    - 받은 당첨번호를 리스트로 변환해서 반환

--- src/main/java/lotto/Ui.java
+++ src/main/java/lotto/Ui.java
@@ -2,6 +2,8 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Ui {
 
     public int moneyInput() {
@@ -10,5 +12,13 @@ public class Ui {
         ChangeNumberType changeNumberType = new ChangeNumberType(moneyInput);
 
         return changeNumberType.getNumber();
+    }
+
+    public List<Integer> lottoInput() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String moneyInput = Console.readLine();
+        ChangeNumberType changeNumberType = new ChangeNumberType(moneyInput);
+
+        return changeNumberType.getNumbers();
     }
 }
