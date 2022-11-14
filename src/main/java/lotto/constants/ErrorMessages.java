e95db84550428cb1e6f452e435cccf3fea3d327a
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-14 13:22:34 +0900

    feat(lotto) : 보너스 번호 중복 체크 추가

--- src/main/java/lotto/constants/ErrorMessages.java
+++ src/main/java/lotto/constants/ErrorMessages.java
@@ -5,4 +5,5 @@ public class ErrorMessages {
     public static final String RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     public static final String DUPLICATE = "[ERROR] 로또 번호에 중복이 존재합니다.";
     public static final String SIZE = "[ERROR] 로또 번호의 개수가 6개가 아닙니다.";
+    public static final String BONUS_DUPLICATE = "[ERROR] 보너스 번호가 중복입니다.";
 }
--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.constants.ErrorMessages;
+
 import java.util.List;
 
 public class WinningLotto extends Lotto {
@@ -7,7 +9,18 @@ public class WinningLotto extends Lotto {
 
     public WinningLotto(List<Integer> numbers, int bonusNumber) {
         super(numbers);
+        validateBonus(numbers, bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonus(List<Integer> numbers, int bonusNumber) {
+        if (checkBonusDuplicate(numbers, bonusNumber)) {
+            throw new IllegalArgumentException(ErrorMessages.BONUS_DUPLICATE);
+        }
+    }
+
+    private boolean checkBonusDuplicate(List<Integer> numbers, int bonusNumber) {
+        return numbers.contains(bonusNumber);
     }
 
     public int getBonusNumber() {
