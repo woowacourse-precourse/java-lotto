01ee6c4dc0c5358581381785c07a403f83820527
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-14 11:37:09 +0900

    feat(main/Input): input validation 추가
    -validNumber()

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -8,21 +8,31 @@ import java.util.List;
 public class Input {
     public static int getPurchaseAmount() {
         String input = Console.readLine();
+        validNumber(input);
         return Integer.parseInt(input);
     }
 
     public static List<Integer> getWinningNumbers() {
         String input = Console.readLine();
         String[] split = input.split(",");
-        List<Integer> numbers =new ArrayList<>();
-        for (String str: split){
-             numbers.add(Integer.parseInt(str));
+        List<Integer> numbers = new ArrayList<>();
+        for (String str : split) {
+            numbers.add(Integer.parseInt(str));
         }
         return numbers;
     }
 
-    public static int getBonusNumber(){
+    public static int getBonusNumber() {
         String input = Console.readLine();
         return Integer.parseInt(input);
+    }
+
+    private static void validNumber(String input) throws IllegalArgumentException {
+        try {
+            Integer.parseInt(input);
+        } catch (Exception e) {
+            System.out.println("[ERROR] 숫자를 입력하세요.");
+            throw new IllegalArgumentException();
+        }
     }
 }
