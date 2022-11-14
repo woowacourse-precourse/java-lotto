063dace013c9fa8dc5b7f53f7e32de388bcb3129
hayeon <qorgkdus12@naver.com>
2022-11-14 15:29:50 +0900

    feat(main): Input money from user

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,14 @@
 package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
+
+
+
 public class Application {
     public static void main(String[] args) {
         // 입력
+        int money = Integer.parseInt(Console.readLine());
+
         // 입력 수 만큼 6개의 랜덤 숫자 생성
         // 사용자로부터 번호 입력 받음
         // 보너스 번호
