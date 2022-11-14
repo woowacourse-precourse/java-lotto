7df940b1379fb70b4f59053540ba43e370fbb79c
01192mg <01192mg@gmail.com>
2022-11-14 19:53:08 +0900

    feat(Application): 당첨 내역 출력하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,6 +15,19 @@ public class Application {
         List<Lotto> purchasedLottos = LottoShop.purchase(money);
         printPurchasedLottos(purchasedLottos);
         println();
+
+        println("당첨 번호를 입력해 주세요.");
+        Lotto winningNumber = InputHandler.readWinningNumber();
+        println();
+
+        println("보너스 번호를 입력해 주세요.");
+        Bonus bonus = InputHandler.readBonusNumber();
+        println();
+
+        println("당첨 통계");
+        println("---");
+        Map<Rank, Integer> winningHistory = getWinningHistory(winningNumber, bonus, purchasedLottos);
+        printWinningHistory(winningHistory);
     }
 
     private static void printPurchasedLottos(List<Lotto> purchasedLottos) {
@@ -23,6 +39,17 @@ public class Application {
         });
     }
 
+    private static Map<Rank, Integer> getWinningHistory(Lotto winningNumber, Bonus bonus, List<Lotto> purchasedLottos) {
+        Map<Rank, Integer> winningHistory = new LinkedHashMap<>();
+        Arrays.stream(Rank.values()).forEach(rank -> winningHistory.put(rank, 0));
+        purchasedLottos.forEach(ticket -> LottoCalculator.calculateRank(winningNumber, bonus, ticket)
+                .ifPresent(rank -> winningHistory.put(rank, winningHistory.get(rank) + 1)));
+        return winningHistory;
+    }
+
+    private static void printWinningHistory(Map<Rank, Integer> winningHistory) {
+        winningHistory.forEach((rank, count) -> System.out.println(rank + " - " + count + "개"));
+    }
 
     private static void println() {
         System.out.println();
--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -4,11 +4,11 @@ import java.text.NumberFormat;
 import java.util.Optional;
 
 public enum Rank {
-    FIRST(2_000_000_000, "6개 일치"),
-    SECONDS(30_000_000, "5개 일치, 보너스 볼 일치"),
-    THIRD(1_500_000, "5개 일치"),
+    FIFTH(5_000, "3개 일치"),
     FOURTH(50_000, "4개 일치"),
-    FIFTH(5_000, "3개 일치");
+    THIRD(1_500_000, "5개 일치"),
+    SECONDS(30_000_000, "5개 일치, 보너스 볼 일치"),
+    FIRST(2_000_000_000, "6개 일치");
 
     private final int prize;
     private final String condition;
