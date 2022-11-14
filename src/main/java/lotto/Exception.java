a71600a84f7916b96d1cc486fc3d1e0cdc45bfd0
ghltjd369 <ghltjd369@gmail.com>
2022-11-14 10:27:29 +0900

    refactor : 오류 메시지를 상수 처리하여 사용

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -4,13 +4,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Exception {
+    public static final String PURCHASE_MONEY_INTEGER = "[ERROR] 구입 금액은 정수를 입력해야 합니다.";
+    public static final String WINING_NUMBER_INTEGER = "[ERROR] 당첨 번호는 정수를 입력해야 합니다.";
+
     public static int isInteger(String str) {
         int integer;
 
         try {
             integer = Integer.parseInt(str);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 정수를 입력해야 합니다.");
+            throw new IllegalArgumentException(PURCHASE_MONEY_INTEGER);
         }
 
         return integer;
@@ -30,7 +33,7 @@ public class Exception {
                     .map(s -> Integer.parseInt(s))
                     .collect(Collectors.toList());
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자를 입력해야 합니다.");
+            throw new IllegalArgumentException(WINING_NUMBER_INTEGER);
         }
     }
 }
