9d396b269640ff24f4dedf334a5faa9bed1d905b
Kayoung <rkdkdudjd@gmail.com>
2022-11-14 23:16:36 +0900

    refactor(WinningNumber): 문자열을 상수로 정의

--- src/main/java/lotto/LottoComparator.java
+++ src/main/java/lotto/LottoComparator.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class LottoComparator {
     public static ComparisonToWinningNumberResult compare(Lotto generatedLotto, WinningNumber winningNumber) {
-        int matchCount = (int) winningNumber.number.getNumbers().stream()
+        int matchCount = (int) winningNumber.mainNumbers.getNumbers().stream()
                 .filter(number -> generatedLotto.getNumbers().contains(number))
                 .count();
         boolean isBonusMatched = generatedLotto.getNumbers().contains(winningNumber.bonusNumber);
--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -3,18 +3,19 @@ package lotto;
 import java.util.List;
 
 public class WinningNumber {
-    Lotto number;
+    private static final String DUPLICATE_WITH_MAIN_NUMBER = "보너스 번호가 당첨 번호와 중복됩니다.";
+    public final Lotto mainNumbers;
     int bonusNumber;
 
     public WinningNumber(List<Integer> numbers, int bonusNumber) {
         validate(numbers, bonusNumber);
-        number = new Lotto(numbers);
+        mainNumbers = new Lotto(numbers);
         this.bonusNumber = bonusNumber;
     }
 
     private void validate(List<Integer> numbers, int bonusNumber) {
         if (numbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("보너스 번호가 당첨 번호와 중복됩니다.");
+            throw new IllegalArgumentException(DUPLICATE_WITH_MAIN_NUMBER);
         }
     }
 }
