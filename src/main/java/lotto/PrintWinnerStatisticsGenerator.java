66ad77d5ce49b1b4ed3fb0cb1a40bbce81cb1844
jonghun <jonghuncu@gmail.com>
2022-11-14 13:30:59 +0900

    feat(lotto) : 당첨 통계 내역을 출력 기능
    PrintWinnerStatisticsGenerator#printWinner()

---
+++ src/main/java/lotto/PrintWinnerStatisticsGenerator.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PrintWinnerStatisticsGenerator {
+    public static void printWinner(List<Integer> resultNumber) {
+        List<Integer> winnerStatistics = new ArrayList<>();
+        System.out.println("당첨통계");
+        System.out.println("---");
+        System.out.println();
+        for(int i=1; i<7; i++) {
+            winnerStatistics.add(Collections.frequency(resultNumber, i));
+        }
+        System.out.printf("3개 일치 (5,000원) - %d개\n", winnerStatistics.get(2));
+        System.out.printf("4개 일치 (50,000원) - %d개\n", winnerStatistics.get(3));
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", winnerStatistics.get(4));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", winnerStatistics.get(4));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", winnerStatistics.get(5));
+
+    }
+}
