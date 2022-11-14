a749b648b9e9b481b8c0843d2d5a660f28700c53
K-jun98 <alap_u@naver.com>
2022-11-14 20:14:30 +0900

    refactor - 애너테이션의 일관성유지

--- src/test/java/lotto/domain/LottoMoneyTest.java
+++ src/test/java/lotto/domain/LottoMoneyTest.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.Test;
 class LottoMoneyTest {
 
 
-    @Test
     @DisplayName("LottoMoney 유효성 검증 살패 테스트")
+    @Test
     void validateTest() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new LottoMoney("1111"));
     }
 
-    @Test
     @DisplayName("LottoMoney 생성 성공 테스트")
+    @Test
     void createLottoMoneyTest() {
         Assertions.assertDoesNotThrow(() -> new LottoMoney("8000"));
     }
