9058c63fe3a56f07c8a5e605563caec2a99369d8
metorg <metrog950@gmail.com>
2022-11-14 15:35:16 +0900

    refactor: 입력받을때 readline으로 수정

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class View {
@@ -7,18 +11,37 @@ public class View {
     private final Scanner scanner;
 
     public Exception exception=new Exception();
+    public LottoMaker lottoMaker= new LottoMaker();
 
     public View(Scanner scanner) {
         this.scanner = scanner;;
     }
 
     public int requestMoney(){
-        int inputMoney = scanner.nextInt();
-        if(exception.isNumeric(inputMoney)){
+        int inputMoney = StringtoIntegerInput(Console.readLine());
+        if(exception.isNumeric(inputMoney)&&exception.divThousandOfMoney(inputMoney)
+        &&exception.lessThanThousand(inputMoney)){
+
             return inputMoney;
         }
         throw new IllegalArgumentException("ERROR");
     }
 
+    public List<Integer> requestLottoNumber(){
 
+        String inputNumber = Console.readLine();
+        System.out.println(inputNumber);
+        return lottoMaker.splitNumber(inputNumber);
+
+    }
+
+    public int requestBonusLottoNumber(){
+        int inputNumber = StringtoIntegerInput(Console.readLine());
+        if(exception.lottoValidator(inputNumber))return inputNumber;
+        throw new IllegalArgumentException("ERROR");
+    }
+
+    public int StringtoIntegerInput(String input){
+        return Integer.parseInt(input);
+    }
 }
