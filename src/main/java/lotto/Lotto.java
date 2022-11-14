5ad8cd83034fd9c8307ab32cfa779deacf4296ff
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-15 02:05:29 +0900

    feat: Add isDistinct function to velidate number list

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,7 +14,18 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if (!isDistinct(numbers)) {
+            throw new IllegalArgumentException();
+        }
     }
 
-    // TODO: 추가 기능 구현
+    /**
+     * Verify numbers contains number that have been used once.
+     * @param numbers the list of integer to be checked.
+     * @return true if no duplicate value exists.
+     */
+    private static boolean isDistinct(List<Integer> numbers) {
+        return numbers.stream().distinct()
+            .count() == numbers.size();
+    }
 }
