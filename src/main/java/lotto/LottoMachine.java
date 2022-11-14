ad5ecf47e81bc8653a68f9da192accf4b5d71557
wonju-dev <wonjulee.dev@gmail.com>
2022-11-14 14:02:23 +0900

    feat(LottoMachine) "랜덤 로또 번호 생성"기능 구현
    getRandomLottos은 파라미터로 전달된 money와 규칙에 따라, 랜덤하게
    생성된 로또 번호를 토대로 로또를 생성합니다.

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -2,13 +2,26 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
 
+    public static final Integer UNIT = 1000;
     private static final Integer NUMBER_RANGE_BOT = 1;
     private static final Integer NUMBER_RANGE_TOP = 45;
     private static final Integer LOTTO_NUMBER_SIZE = 6;
+
+    public static List<Lotto> getRandomLottos(Integer money) {
+        List<Lotto> lottos = new ArrayList<>();
+        Integer numberOfLotto = money % UNIT;
+
+        for (int count = 0; count < numberOfLotto; count++) {
+            lottos.add(Lotto.numberOf(getRandomLottoNumbers()));
+        }
+
+        return lottos;
+    }
 
     private static List<Integer> getRandomLottoNumbers() {
         return Randoms.pickUniqueNumbersInRange(NUMBER_RANGE_BOT, NUMBER_RANGE_TOP, LOTTO_NUMBER_SIZE);
