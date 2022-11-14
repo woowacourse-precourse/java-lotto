d32fe9167e274ac669ebb0a8bbf54d98a69cce15
sunghyuki <sunghyuk1609@gmail.com>
2022-11-15 01:39:23 +0900

    feat : 당첨 번호 입력 기능 추가

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -2,15 +2,29 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
     private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_NOT_DIGIT_MESSAGE = "[ERROR] 입력 값은 숫자가 아닙니다.";
+    private static final String INPUT_WINNING_LOTTO_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String COMMA = ",";
+    private static final String WHITESPACE = "\\s";
+    private static final String EMPTY = "";
 
     public static int inputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
         String input = Console.readLine();
         validateIsDigit(input);
         return Integer.parseInt(Console.readLine());
+    }
+
+    public static List<Integer> inputWinningLotto() {
+        System.out.println(INPUT_WINNING_LOTTO_MESSAGE);
+        String input = Console.readLine();
+        return parse(input);
     }
 
     private static void validateIsDigit(String input) {
@@ -20,5 +34,19 @@ public class InputView {
                 throw new IllegalArgumentException(INPUT_NOT_DIGIT_MESSAGE);
             }
         }
+    }
+
+    private static List<Integer> parse(String input) {
+        return Arrays.stream(split(removeBlank(input)))
+                     .map(Integer::parseInt)
+                     .collect(Collectors.toList());
+    }
+
+    private static String[] split(String input) {
+        return input.split(COMMA);
+    }
+
+    private static String removeBlank(String input) {
+        return input.replaceAll(WHITESPACE, EMPTY);
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private static final String LOTTO_NUMBER_RANGE_MISMATCH_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final String LOTTO_SIZE_MISMATCH_MESSAGE = "[ERROR] 로또 한 장의 번호 개수는 6개입니다.";
+    private static final String LOTTO_IS_NOT_UNIQUE_MESSAGE ="[ERROR] 로또 번호에 중복이 존재할 수 없습니다.";
     public static final int MIN_VALUE = 1;
     public static final int MAX_VALUE = 45;
     private static final int LOTTO_SIZE = 6;
@@ -13,12 +16,21 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validateLottoNumbersRange(numbers);
+        validateLottoIsUnique(numbers);
         validateLottoSize(numbers);
         this.numbers = numbers;
     }
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    private void validateLottoIsUnique(List<Integer> numbers) {
+        Set<Integer> lottoNumberSet = new HashSet<>(numbers);
+
+        if (lottoNumberSet.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(LOTTO_IS_NOT_UNIQUE_MESSAGE);
+        }
     }
 
     private void validateLottoSize(List<Integer> numbers) {
