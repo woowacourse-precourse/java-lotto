10cc876c0321858fcc3d5c5a03bb1d0af91812ca
mrbonk97 <mrbonk97@gmail.com>
2022-11-15 01:23:00 +0900

    feat: UI 기능 구현
    UI 기능 완성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,18 +7,12 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-
         UI ui = new UI();
-        int x = ui.GetMoney();
-        System.out.println(x);
 
-        List<Integer> al = ui.GetLotto();
-        for(var e: al) System.out.println(e);
+        System.out.println(ui.GetMoney());
+        System.out.println(ui.GetLottoNumber().toString());
+        System.out.println(ui.GetBonusNumber());
 
-        int y = ui.GetBonusNumber();
-        System.out.println(y);
-
-        ;
         // TODO: 프로그램 구현
     }
 }
--- src/main/java/lotto/ui/UI.java
+++ src/main/java/lotto/ui/UI.java
@@ -1,63 +1,55 @@
 package lotto.ui;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import lotto.exception.UIException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class UI {
-    static List<Integer> ans;
+    static private List<Integer> lottoNumber;
+    private final UIException uiException = new UIException();
 
     public int GetMoney(){
         System.out.println("돈을 입력하세요");
         String money = readLine();
-        CheckIfInteger(money);
-        CheckIf1000(money);
+        uiException.notNumberException(money);
+        uiException.notMultipleOf1000Exception(money);
+
         return Integer.parseInt(money);
     }
 
 
-    public List<Integer> GetLotto(){
+    public List<Integer> GetLottoNumber(){
         System.out.println("로또 번호를 입력하세요");
         String lotto = readLine();
-//        CheckLottoLength(lotto);
-        List<String> stringList = new ArrayList<>(Arrays.asList(lotto.split(",")));
-        ans = stringList.stream().map(Integer::parseInt).collect(Collectors.toList());
-        return ans;
+
+        List<String> lottoNumberString = new ArrayList<>(Arrays.asList(lotto.split(",")));
+        List<Integer> lottoNumberInt = new ArrayList<>();
+        uiException.CheckLottoLength(lottoNumberString);
+        for(String e: lottoNumberString)
+        {
+            uiException.notNumberException(e);
+            lottoNumberInt.add(Integer.parseInt(e));
+        }
+        uiException.overBoundaryException(lottoNumberInt);
+        uiException.duplicateNumberException(lottoNumberInt);
+        return lottoNumberInt;
 
     }
 
     public int GetBonusNumber(){
         System.out.println("보너스 번호를 입력하세요");
         int bonus = Integer.parseInt(readLine());
-        if(ans.contains(bonus)) throw new IllegalArgumentException("[ERROR] 보너스 숫자 겹침");
+        uiException.BonusNumberException(lottoNumber, bonus);
 
         return bonus;
     }
 
-    public void CheckLottoLength(String lotto){
-        if(lotto.length() == 11) return;
-        throw new IllegalArgumentException("[ERROR] 유저숫자 입력 - 로또 번호 길이 오류");
-    }
 
 
-    public void CheckIfInteger(String money){
-        try{
-            int moneyInt = Integer.parseInt(money);
-        } catch(NumberFormatException e){
-            throw new IllegalArgumentException("[ERROR] 유저숫자 입력 - 문법 오류");
-        }
-    }
-
-    private void CheckIf1000(String money){
-        int moneyInt = Integer.parseInt(money);
-        if(moneyInt % 1000 == 0) return;
-        throw new IllegalArgumentException("[ERROR] 유저 숫자 입력 - 1000단위로 오류");
-    }
 
 
 }
