f167ea77c363ec5297a5ee146a7f6e7d34205b64
beombu <sunil1369@bible.ac.kr>
2022-11-15 06:47:21 +0900

    Feat(Output) : 로또 당첨 번호 요청 메시지 출력 기능

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -5,16 +5,21 @@ import java.util.List;
 public class Output {
     public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String COUNT_LOTTO_MESSAGE = "%d개 구매했습니다.\n";
+    public static final String INPUT_PRIZE_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.\n";
 
-    public static void printInputMoney(){
+    public static void printInputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
     }
 
-    public static void printCountLotto(int countLotto){
+    public static void printCountLotto(int countLotto) {
         System.out.printf(COUNT_LOTTO_MESSAGE, countLotto);
     }
 
-    public static void printLottoNumbers(List<Integer> lottoNumbers){
+    public static void printLottoNumbers(List<Integer> lottoNumbers) {
         System.out.println(lottoNumbers);
+    }
+
+    public static void printInputPrizeNumbers() {
+        System.out.println(INPUT_PRIZE_NUMBERS_MESSAGE);
     }
 }
