8d1f07c9bd392b4a8d49ee47ea62e5baac963815
wonjunYou <youwjune@gmail.com>
2022-11-15 00:31:59 +0900

    feat(LottoTickets) : 구매한 로또 개수에 맞게 로또 티켓들을 생성하는 기능

---
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import lotto.utils.LottoNumberGenerator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoTickets {
+
+    private final List<Lotto> lottoTickets;
+
+    public LottoTickets(int lottoCount, LottoNumberGenerator lottoNumberGenerator) {
+        this.lottoTickets = generateLottoTickets(lottoCount, lottoNumberGenerator);
+    }
+
+    private List<Lotto> generateLottoTickets(int lottoCount, LottoNumberGenerator lottoNumberGenerator) {
+        List<Lotto> lottoTickets = new ArrayList<>();
+
+        for (int i = 0; i < lottoCount; i++) {
+            Lotto lotto = new Lotto(lottoNumberGenerator.generate());
+
+            lottoTickets.add(lotto);
+        }
+
+        return lottoTickets;
+    }
+
+    public int getCount() {
+        return lottoTickets.size();
+    }
+}
+
