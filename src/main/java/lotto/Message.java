03eba3553e922bff77f2fc00364fe70cf0732fd2
김태용 <amico741@naver.com>
2022-11-14 16:34:20 +0900

    feat: 메시지 상수값 추가

--- src/main/java/lotto/Message.java
+++ src/main/java/lotto/Message.java
@@ -1,7 +1,7 @@
 package lotto;
 
 public enum Message {
-    
+
     WELCOME_MESSAGE("구입 금액을 입력해 주세요."),
     COUNT_MESSAGE("개를 구매했습니다."),
     INPUT_WINNING_MESSAGE("당첨 번호를 입력해 주세요."),
@@ -15,8 +15,13 @@ public enum Message {
     RANGE_EXCEED_MESSAGE("[ERROR] 숫자는 1 ~ 45로 입력할 수 있습니다."),
     LENGTH_MESSAGE("[ERROR] 6개의 숫자를 입력해야 합니다."),
     OVERLAP_MESSAGE("[ERROR] 중복되는 숫자가 존재합니다."),
-    OVERLAP_BONUS_NUMBER_MESSAGE("[ERROR] 보너스 숫자가 당첨번호에 존재합니다.");
-
+    OVERLAP_BONUS_NUMBER_MESSAGE("[ERROR] 보너스 숫자가 당첨번호에 존재합니다."),
+    SAME_THREE_COUNT_MESSAGE("3개 일치 (5,000원) - %d개\n"),
+    SAME_FOUR_COUNT_MESSAGE("4개 일치 (50,000원) - %d개\n"),
+    SAME_FIVE_COUNT_MESSAGE("5개 일치 (1,500,000원) - %d개\n"),
+    SAME_FIVE_BONUS_COUNT_MESSAGE("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
+    SAME_SIX_COUNT_MESSAGE("6개 일치 (2,000,000,000원) - %d개\n"),
+    TOTAL_YIELD_MESSAGE("총 수익률은 %.1f%%입니다.\n");
     private final String value;
     Message(String value){
         this.value = value;
