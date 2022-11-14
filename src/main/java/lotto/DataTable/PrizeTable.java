631495112271b4ac5b0c1eebccc03b296bf2cc40
baksohyeon <bori12370@gmail.com>
2022-11-14 17:56:20 +0900

    feat: make prize table using enum

---
+++ src/main/java/lotto/DataTable/PrizeTable.java
@@ -0,0 +1,43 @@
+package lotto.DataTable;
+
+import javax.lang.model.element.Element;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum PrizeTable {
+    FirstPlace(6, 2000000000),
+    SecondPlace(5, 30000000),
+    SecondPlaceBonus(4, 30000000),
+    ThirdPlace(3, 50000),
+    FourthPlace(2, 5000);
+
+    public final int prize;
+    public final int count;
+    public static final Map<Integer, Integer> AMOUNT_BY_RANK = new HashMap<>();
+
+    private PrizeTable(int count, int prize) {
+        this.count = count;
+        this.prize = prize;
+    }
+
+
+    static {
+        for (PrizeTable prizeTable : values()) {
+            AMOUNT_BY_RANK.put(prizeTable.count, prizeTable.prize);
+        }
+    }
+
+
+    public static Map<Integer, Integer> setScoreTable() {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (PrizeTable p : values()) {
+            map.put(p.count, 0);
+        }
+        return map;
+    }
+
+
+    public static Integer valueOfPrize(int count) {
+        return AMOUNT_BY_RANK.get(count);
+    }
+}
