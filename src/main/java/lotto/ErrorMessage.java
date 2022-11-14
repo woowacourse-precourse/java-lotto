eb360e7d3264c1e1dddbf2352f0df07717b75a0c
dener8 <8dener@gmail.com>
2022-11-14 17:23:40 +0900

    feat(ErrorMessage): 에러 메세지를 모아둔 클래스 구현
    1. 1~45인지
    2. 번호가 총 6개인지

---
+++ src/main/java/lotto/ErrorMessage.java
@@ -0,0 +1,6 @@
+package lotto;
+
+public class ErrorMessage {
+    public static final String OUT_OF_RANGE_ERROR = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String OUT_OF_SIZE_ERROR = "[ERROR] 로또 번호는 총 6개여야 합니다.";
+}
