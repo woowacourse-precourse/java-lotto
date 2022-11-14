6a0b5c6e8ffac221a50f41706c1db796b24f4810
anso33 <hyeon131812@gmail.com>
2022-11-14 14:14:31 +0900

    [FIX](MathUtils): fix calcYield formula

--- src/main/java/lotto/MathUtils.java
+++ src/main/java/lotto/MathUtils.java
@@ -13,7 +13,10 @@ public class MathUtils {
   public static double calcYield(int reward, int money) {
     double yield = 0;
 
-    yield = money / reward * 100;
+    if (reward == 0)
+      return 0;
+    yield = (double)reward / (double)money;
+    yield *= 100;
     return yield;
   }
 }
