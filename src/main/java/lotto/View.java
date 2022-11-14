83911b2db0c177a7fb4cd81b42e49468b762838d
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-14 18:41:04 +0900

    feat : 로또번호입력 Method 구현

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -23,12 +23,29 @@ public class View {
         int i;
         try {
             i = Integer.parseInt(Console.readLine());
-            if(i%1000 != 0){
+            if (i % 1000 != 0) {
                 throw new IllegalArgumentException();
             }
             return Integer.parseInt(Console.readLine());
         } catch (IllegalArgumentException e) {
-            System.out.println("[Error] 로또 구입 금액이 올바르지 않습니다.");;
+            System.out.println("[Error] 로또 구입 금액이 올바르지 않습니다.");
+            throw e;
+        }
+    }
+
+    public String[] inputLottoNumber() {
+        try {
+            String[] ss = Console.readLine().split(",");
+            for (String s : ss) {
+                int i = Integer.parseInt(s);
+                if (i < 1 || i > 45) {
+                    throw new IllegalArgumentException();
+                }
+            }
+            return ss;
+        } catch (IllegalArgumentException e) {
+            System.out.println("[Error] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw e;
         }
     }
 }
