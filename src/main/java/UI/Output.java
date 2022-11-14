cdf663f13b3ae45a7bf52247c40b8e77ad629d4a
p <gustjr2332@naver.com>
2022-11-14 13:16:05 +0900

    feat(Output) : 출력 기능, 생성된 로또의 갯수와 번호 출력 기능 추가

--- src/main/java/UI/Output.java
+++ src/main/java/UI/Output.java
@@ -1,5 +1,7 @@
 package UI;
 
+import java.util.List;
+
 public class Output {
     public static final String INPUTPRICE = "구입금액을 입력해 주세요.";
     public static final String INPUTWINNINGNUMBERS = "당첨 번호를 입력해 주세요.";
@@ -18,6 +20,14 @@ public class Output {
     }
 
     //구매한 내역 출력
+    public static void printRandomLottoNumbers(List<List<Integer>> randomLottoNumbers) {
+        int numberOfLotto = randomLottoNumbers.size();
+        System.out.printf("%d 개를 구매했습니다\n", numberOfLotto);
+
+        for (List<Integer> number : randomLottoNumbers) {
+            System.out.println(number);
+        }
+    }
 
 
     //당첨통계 출력
