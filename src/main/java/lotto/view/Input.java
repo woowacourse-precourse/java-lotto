ad4b9e58d30b391ecd658b1fa1ed3c4d925ceb08
beombu <sunil1369@bible.ac.kr>
2022-11-15 07:24:03 +0900

    Feat(Input) : 당첨 번호 입력 기능

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -2,8 +2,20 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Input {
-    public static int inputMoney(){
+    public static int inputMoney() {
         return Integer.parseInt(Console.readLine());
+    }
+
+    public static List<Integer> inputPrizeNumbers() {
+        String numbers = Console.readLine();
+
+        return Stream.of(numbers.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
