3fb575328e50f642edb30c5c32cb6f9ec35d3ac3
osm <osm0822@naver.com>
2022-11-14 22:10:02 +0900

    feat(Lotto) : validation 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,11 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int NUMBER_CNT = 6;
+    private static final int NUMBER_MIN = 1;
+    private static final int NUMBER_MAX = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,13 +16,18 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != NUMBER_CNT) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 너무 많습니다.");
+        }
+        for (int i : numbers) {
+            if (i < Lotto.NUMBER_MIN || i > Lotto.NUMBER_MAX) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
         }
     }
 
     // TODO: 추가 기능 구현
-    public List<Integer> getNumbers(){
+    public List<Integer> getNumbers() {
         return this.numbers;
     }
 }
