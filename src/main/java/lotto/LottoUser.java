151fc7ded6789928abf711089f7fa12d26b5b903
osm <osm0822@naver.com>
2022-11-14 21:35:50 +0900

    feat(LottoUser) : 로또를 구매할 돈의 양 설정 구현

---
+++ src/main/java/lotto/LottoUser.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoUser {
+    private long money;
+    private LottoSystem lottoSystem;
+
+    public LottoUser(long money, LottoSystem lottoSystem) {
+        this.money = money;
+        this.lottoSystem = lottoSystem;
+    }
+
+}
