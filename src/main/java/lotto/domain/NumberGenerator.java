7dad54c0fd9eaa40a3ef1108c61be89511684aef
brotherstone97 <brotherstone97@gmail.com>
2022-11-15 03:42:28 +0900

    feat(NumberGenerator): 랜덤 숫자 생성

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -1,9 +1,18 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class NumberGenerator {
-    public List<Integer> createRandomNumbers(){
-        return null;
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+    public List<Integer> createRandomNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, LOTTO_NUMBER_COUNT);
+        Collections.sort(numbers);
+        return numbers;
     }
 }
