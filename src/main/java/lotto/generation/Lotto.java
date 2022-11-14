a1e4ace403701ba51cdd76595f21e43c2609baae
jin8073 <jin8073@gmail.com>
2022-11-14 23:44:52 +0900

    feat(Lotto): 생성된 당첨 번호에 중복이 있을 경우 예외 발생

--- src/main/java/lotto/generation/Lotto.java
+++ src/main/java/lotto/generation/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.generation;
 
+import lotto.exception.IllegalArgument;
+
 import java.util.Comparator;
 import java.util.List;
 
@@ -13,7 +15,10 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 1부터 45까지의 숫자 6개를 입력하세요.");
+        }
+        if (IllegalArgument.isRedundancy(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 1부터 45까지의 중복되지 않는 숫자만 입력 가능합니다.");
         }
     }
 
