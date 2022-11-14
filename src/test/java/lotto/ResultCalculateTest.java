7c625239100516e086c6fc38518d0777ab4158ee
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-14 17:31:34 +0900

    Test(ResultCalculateTest) : 두개의 번호들을 비교해서같은 몇개가 같은지 찾는 기능 수정 및 추가.
    등수 자체를 리턴하는 구현으로 인한 테스트 케이스 수정 및 추가.

--- src/test/java/lotto/ResultCalculateTest.java
+++ src/test/java/lotto/ResultCalculateTest.java
@@ -1,12 +1,26 @@
 package lotto;
 
+import net.bytebuddy.matcher.CollectionSizeMatcher;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 class ResultCalculateTest {
+    @DisplayName("List는 선언시 빈 공간이 할당되는가?")
+    @Test
+    void 선언만하고_값넣어보기(){
+        List<Integer> example = new LinkedList<>();
+        assertThatThrownBy(()->example.set(6,1))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+        List<Integer> example2 = new ArrayList<>(7);
+        assertThatThrownBy(()->example2.set(6,10))
+             .isInstanceOf(IndexOutOfBoundsException.class);
+    }
     @DisplayName("2가지의 일련 번호들 사이에 같은 번호의 수 찾기")
     @Test
     void 모든번호가_같음() {
@@ -16,9 +30,13 @@ class ResultCalculateTest {
     }
     @Test
     void 일부만_같음(){
+        assertThat(2)
+                .isEqualTo(ResultCalculate.
+                        compareTwoNumbers(new Lotto(Arrays.asList(1,2,3,4,5,10)),List.of(1,2,3,4,11,7,10))
+                );
         assertThat(3)
                 .isEqualTo(ResultCalculate.
-                        compareTwoNumbers(new Lotto(Arrays.asList(1,2,3,4,5,10)),List.of(1,2,3,4,5,6,7))
+                        compareTwoNumbers(new Lotto(Arrays.asList(1,2,3,4,5,10)),List.of(1,2,3,4,11,10,7))
                 );
         assertThat(4)
                 .isEqualTo(ResultCalculate.
