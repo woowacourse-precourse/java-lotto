deb708cbd1b70f44d995b70aff3586317672d8f0
swjeong98 <tree5678@konkuk.ac.kr>
2022-11-14 22:10:13 +0900

    feat(Application) : 로또 게임 시작하도록 코드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,8 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Manager Wooteco = new Manager();
+        Player swJeong98 = new Player(Wooteco);
+        swJeong98.LottoGamestart();
     }
 }
