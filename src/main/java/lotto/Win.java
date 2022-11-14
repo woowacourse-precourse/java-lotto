cafd39bbd28380538de33815e5c50c39b284b773
dohyun <ldh19738@gmail.com>
2022-11-14 16:47:32 +0900

    refactor(Win): 출력문을 printf로 바꾸고 모든 출력 문자열 상수 처리

--- src/main/java/lotto/Win.java
+++ src/main/java/lotto/Win.java
@@ -1,28 +1,28 @@
 package lotto;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.text.DecimalFormat;
-
 public class Win {
     private static final String WIN_STATISTICS_MESSAGE = "당첨 통계";
     private static final String TITLE_DISTINGUISHING_ROLE = "---";
-    private static final String PUTTING_COMMAS_IN_THREE_NUMBERS = "###,###";
+    private static final String FIFTH_GRADE_STATISTICS_MESSAGE = "3개 일치 (5,000원) - %d개%n";
+    private static final String FOURTH_GRADE_STATISTICS_MESSAGE = "4개 일치 (50,000원) - %d개%n";
+    private static final String THIRD_GRADE_STATISTICS_MESSAGE = "5개 일치 (1,500,000원) - %d개%n";
+    private static final String SECOND_GRADE_STATISTICS_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개%n";
+    private static final String FIRST_GRADE_STATISTICS_MESSAGE = "6개 일치 (2,000,000,000원) - %d개%n";
+    private static final String RATE_OF_RETURNS_MESSAGE = "총 수익률은 %2.1f%s입니다.";
+    public static final String PERCENT_CHARACTER = "%";
 
     public void statisticsMessage() {
         System.out.println('\n' + WIN_STATISTICS_MESSAGE);
         System.out.println(TITLE_DISTINGUISHING_ROLE);
     }
 
-    private void printStatistics(@NotNull CountNumberOfWins countNumberOfWins) {
-        DecimalFormat decimalFormat = new DecimalFormat(PUTTING_COMMAS_IN_THREE_NUMBERS);
-
-        System.out.println("3개 일치 (" + decimalFormat.format(Grade.FIFTH.getPrizeMoney()) + "원) - " + countNumberOfWins.getFifthPlace() + "개");
-        System.out.println("4개 일치 (" + decimalFormat.format(Grade.FOURTH.getPrizeMoney()) + "원) - " + countNumberOfWins.getFourthPlace() + "개");
-        System.out.println("5개 일치 (" + decimalFormat.format(Grade.THIRD.getPrizeMoney()) + "원) - " + countNumberOfWins.getThirdPlace() + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (" + decimalFormat.format(Grade.SECOND.getPrizeMoney()) + "원) - " + countNumberOfWins.getSecondPlace() + "개");
-        System.out.println("6개 일치 (" + decimalFormat.format(Grade.FIRST.getPrizeMoney()) +"원) - " + countNumberOfWins.getFirstPlace() + "개");
-        System.out.println("총 수익률은 " + countNumberOfWins.rateOfReturn() + "%입니다.");
+    private void printStatistics(CountNumberOfWins countNumberOfWins) {
+        System.out.printf(FIFTH_GRADE_STATISTICS_MESSAGE, countNumberOfWins.getFifthPlace());
+        System.out.printf(FOURTH_GRADE_STATISTICS_MESSAGE, countNumberOfWins.getFourthPlace());
+        System.out.printf(THIRD_GRADE_STATISTICS_MESSAGE, countNumberOfWins.getThirdPlace());
+        System.out.printf(SECOND_GRADE_STATISTICS_MESSAGE, countNumberOfWins.getSecondPlace());
+        System.out.printf(FIRST_GRADE_STATISTICS_MESSAGE, countNumberOfWins.getFirstPlace());
+        System.out.printf(RATE_OF_RETURNS_MESSAGE, countNumberOfWins.rateOfReturn(), PERCENT_CHARACTER);
     }
 
     public void statistics() {
