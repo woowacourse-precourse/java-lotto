511fad9c40116ef74f9759bc4d00fffc2f27f313
whdcks420 <whdcks420@gmail.com>
2022-11-14 23:21:16 +0900

    refactor: code line 정리, 상수 변수명  refactroing

--- src/main/java/lotto/domain/LottoReward.java
+++ src/main/java/lotto/domain/LottoReward.java
@@ -1,19 +1,31 @@
 package lotto.domain;
 
-public enum LottoRewardMessage {
-    FIRST("6개 일치 (2,000,000,000원) - "),
-    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    THIRD("5개 일치 (1,500,000원) - "),
-    FOURTH("4개 일치 (50,000원) - "),
-    FIFTH("3개 일치 (5,000원) - ");
+public enum LottoReward {
+    FIRST("6개 일치 (2,000,000,000원) - ", 2000000000, 6),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - ", 30000000, 5),
+    THIRD("5개 일치 (1,500,000원) - ", 1500000, 5),
+    FOURTH("4개 일치 (50,000원) - ", 50000, 4),
+    FIFTH("3개 일치 (5,000원) - ", 5000, 3);
 
     private final String rewardMessage;
+    private final int reward;
+    private final int matchCount;
 
-    LottoRewardMessage(String rewardMessage) {
+    LottoReward(String rewardMessage, int reward, int matchCount) {
         this.rewardMessage = rewardMessage;
+        this.reward = reward;
+        this.matchCount = matchCount;
     }
 
     public String getRewardMessage() {
         return rewardMessage;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
     }
 }
--- src/main/java/lotto/domain/LottoStatistics.java
+++ src/main/java/lotto/domain/LottoStatistics.java
@@ -1,35 +1,45 @@
 package lotto.domain;
 
+import lotto.utils.FormatYieldNumber;
+
 import java.util.Map;
 
-public class LottoCount {
+import static lotto.domain.LottoReward.*;
+
+public class LottoStatistics {
+    private static final String LINE_FEED = "\n";
+    private static final String PIECE = "개";
+    private static final int LOTTO_PRICE = 1000;
+    private static final int PERCENTAGE = 100;
+
     private int first;
     private int second;
     private int third;
     private int fourth;
     private int fifth;
     private final Map<Lotto, Integer> rank;
-    private static final String LINE_FEED = "\n";
 
-    public LottoCount(Map<Lotto, Integer> rank, int bonusNumber) {
+    public LottoStatistics(Map<Lotto, Integer> rank, int bonusNumber) {
         this.rank = rank;
-        rank.entrySet().forEach(lottoTicket -> countLottoCondition(bonusNumber, lottoTicket));
+        rank.entrySet().forEach(lottoTicket -> countWinningLotto(bonusNumber, lottoTicket));
     }
 
-    private void countLottoCondition(int bonusNumber, Map.Entry<Lotto, Integer> lottoTicket) {
-        if (isRightLottoNumberCount(lottoTicket, 6)) {
+    private void countWinningLotto(int bonusNumber, Map.Entry<Lotto, Integer> lottoTicket) {
+        if (isRightLottoNumberCount(lottoTicket, FIRST.getMatchCount())) {
             first++;
         }
-        if (isRightLottoNumberCount(lottoTicket, 5) && isBonusNumberContain(lottoTicket, bonusNumber)) {
+        if (isRightLottoNumberCount(lottoTicket, SECOND.getMatchCount()) &&
+                isBonusNumberContain(lottoTicket, bonusNumber)) {
             second++;
         }
-        if (isRightLottoNumberCount(lottoTicket, 5) && !isBonusNumberContain(lottoTicket, bonusNumber)) {
+        if (isRightLottoNumberCount(lottoTicket, THIRD.getMatchCount()) &&
+                !isBonusNumberContain(lottoTicket, bonusNumber)) {
             third++;
         }
-        if (isRightLottoNumberCount(lottoTicket, 4)) {
+        if (isRightLottoNumberCount(lottoTicket, FOURTH.getMatchCount())) {
             fourth++;
         }
-        if (isRightLottoNumberCount(lottoTicket, 3)) {
+        if (isRightLottoNumberCount(lottoTicket, FIFTH.getMatchCount())) {
             fifth++;
         }
     }
@@ -43,19 +53,19 @@ public class LottoCount {
     }
 
     public String getYield() {
-        return String.format("%.1f", ((double) this.first * 2000000000 +
-                this.second * 30000000 +
-                this.third * 1500000 +
-                this.fourth * 50000 +
-                this.fifth * 5000) / (this.rank.size() * 1000) * 100);
+        return FormatYieldNumber.get(((double) this.first * FIRST.getReward() +
+                this.second * SECOND.getReward() +
+                this.third * THIRD.getReward() +
+                this.fourth * FOURTH.getReward() +
+                this.fifth * FIFTH.getReward()) / (this.rank.size() * LOTTO_PRICE) * PERCENTAGE);
     }
 
     @Override
     public String toString() {
-        return "3개 일치 (5,000원) - " + fifth + "개" + LINE_FEED +
-                "4개 일치 (50,000원) - " + fourth + "개" + LINE_FEED +
-                "5개 일치 (1,500,000원) - " + third + "개" + LINE_FEED +
-                "5개 일치, 보너스 볼 일치 (30,000,000원) - " + second + "개" + LINE_FEED +
-                "6개 일치 (2,000,000,000원) - " + first + "개";
+        return FIFTH.getRewardMessage() + fifth + PIECE + LINE_FEED +
+                FOURTH.getRewardMessage() + fourth + PIECE + LINE_FEED +
+                THIRD.getRewardMessage() + third + PIECE + LINE_FEED +
+                SECOND.getRewardMessage() + second + PIECE + LINE_FEED +
+                FIRST.getRewardMessage() + first + PIECE;
     }
 }
