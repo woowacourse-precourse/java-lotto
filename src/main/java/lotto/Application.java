333996025974abeb26239f2426fef851350c1be2
MuSeung <peter0757@nvaer.com>
2022-11-14 17:51:43 +0900

    feat: Application의 play에 전반적인 흐름을 정리하고 main에서 실행시켜 기능 실행
    Application
        - main : 정리된 기능들을 실행시켜 에러가 발생하면 종료하도록 try-catch 사용
        - play : buy - situation - result 순으로 진행하며 readLine으로 입력을 받는다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,39 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.List;
+import lotto.buy.Payment;
+import lotto.input.InputConvert;
+import lotto.result.LottoCompare;
+import lotto.result.ResultPrint;
+import lotto.situation.Lotto;
+import lotto.situation.QuickPick;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            play();
+        } catch (IllegalArgumentException ignored) {
+        }
     }
+    private static void play() {
+        System.out.println("구입금액을 입력해 주세요.");
+        Payment payment = new Payment(InputConvert.oneNumber(readLine()));
+        System.out.println("\n" + payment.purchase() + "개를 구매했습니다.");
+        List<List<Integer>> quickPicks = QuickPick.bulkPick(payment.purchase());
+        for (List<Integer> quickPick : quickPicks) {
+            System.out.println(quickPick);
+        }
+
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        Lotto lotto = new Lotto(InputConvert.lottoNumber(readLine()));
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        int bonusNumber = InputConvert.oneNumber(readLine());
+
+        LottoCompare lottoCompare = new LottoCompare(quickPicks, lotto.getNumbers(), bonusNumber);
+        ResultPrint.printingResult(lottoCompare.getPrizesMap());
+    }
+
 }
