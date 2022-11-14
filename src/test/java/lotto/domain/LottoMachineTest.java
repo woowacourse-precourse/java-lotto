fef0a0b08b536a15f146338f8a3ac263a3245462
choyeram <choyeram8@gmail.com>
2022-11-14 16:15:55 +0900

    refactor: 테스트 메서드 이름 변경 및 불필요한 import 삭제

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class LottoMachineTest {
@@ -15,7 +14,7 @@ public class LottoMachineTest {
     private final User user = new User();
 
     @BeforeEach
-    void issueLotto() {
+    void init() {
         List<Integer> numbers = new ArrayList<>(List.of(1,2,3,4,5,6));
         user.receive(List.of(new Lotto(numbers)));
     }
