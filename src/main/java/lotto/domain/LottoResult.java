78638dd493e06ad64fbbcc0cd83fa2d547c21025
3juhwan <13selfesteem91@naver.com>
2022-11-15 01:33:27 +0900

    refactor(lotto): 반복문, 순위 계산 리팩토링

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -5,51 +5,31 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoResult {
-    private static int FIRST_PLACE = 5;
-    private static int SECOND_PLACE = 4;
-    private static int THIRD_PLACE = 3;
-    private static int FOURTH_PLACE = 2;
-    private static int FIFTH_PLACE = 1;
-    private static int SIXTH_PLACE = 0;
 
     public static List<Integer> getResult(List<Lotto> lottos, List<Integer> winningNumber, Integer bonusNumber) {
-        int[] result = {0, 0, 0, 0, 0, 0};
-        for (List<Integer> numbers : lottos.stream().map(lotto -> lotto.getNumbers()).collect(Collectors.toList())) {
-            int index = compareNumber(numbers, winningNumber, bonusNumber);
+        int[] result = {0, 0, 0, 0, 0, 0, 0};
+
+        for (Lotto lotto : lottos) {
+            int index = getLottoPlace(lotto.getNumbers(), winningNumber, bonusNumber);
             result[index]++;
         }
+
         return Arrays.stream(result).boxed().collect(Collectors.toList());
     }
 
-    public static int compareNumber(List<Integer> lottoNumbers, List<Integer> winningNumbers, Integer bonusNumber) {
-        int matchingCount = 0;
+    public static int getLottoPlace(List<Integer> lottoNumbers, List<Integer> winningNumbers, Integer bonusNumber) {
+        Integer takePlace = 7 - (int) lottoNumbers.stream()
+                .filter(number -> winningNumbers.contains(number))
+                .count();
 
-        for (Integer winningNumber : winningNumbers) {
-            if (lottoNumbers.contains(winningNumber)) {
-                matchingCount++;
-            }
+        if (takePlace == 1) {
+            return takePlace;
         }
 
-        if (matchingCount == 6) {
-            return FIRST_PLACE;
+        if (lottoNumbers.contains(bonusNumber)) {
+            return takePlace;
         }
 
-        if (winningNumbers.contains(bonusNumber)) {
-            matchingCount++;
-        }
-
-        switch (matchingCount) {
-            case 6:
-                return SECOND_PLACE;
-            case 5:
-                return THIRD_PLACE;
-            case 4:
-                return FOURTH_PLACE;
-            case 3:
-                return FIFTH_PLACE;
-            default:
-                return SIXTH_PLACE;
-        }
-
+        return Math.min(takePlace + 1, 6);
     }
 }
