ee8e12886a9674ffac5fcaabc22483f98a588bb1
wonseokjee <wsji9404@gmail.com>
2022-11-14 11:22:37 +0900

    Feat(PlayLotto): 추가 compareTicketAndWinningNumber,compareTicketAndBonusNumber 함수
    티켓번호와 당첨번호가 몇개 일치하는지 세는 함수, 티켓번호에 보너스 번호가 있는지 확인하는 함수

--- src/main/java/lotto/PlayLotto.java
+++ src/main/java/lotto/PlayLotto.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class PlayLotto {
+    CreateLotto createLotto = new CreateLotto();
     public List<List<Integer>> playLottoTicketCount(Integer ticketCount) {
         List<List<Integer>> allLottoNumber = new ArrayList<>();
         for (int i = 0; i < ticketCount; i++) {
@@ -15,4 +16,28 @@ public class PlayLotto {
         return allLottoNumber;
     }
 
+/*    public void seprateTicket(List<List<Integer>> RandomLottoTicket) {
+        for (List<Integer> eachTicket : RandomLottoTicket) {
+
+        }
+    }*/
+
+    public int compareTicketAndWinningNumber(List<Integer> ticket, String winningNumber) {
+        List<Integer> winning = createLotto.winningNumber(winningNumber);
+        int countWinning = 0;
+        for (Integer ticketNumber : ticket) {
+            if (winning.contains(ticketNumber)) {
+                countWinning ++;
+            }
+        }
+        return countWinning;
+    }
+    public boolean compareTicketAndBonusNumber(List<Integer> ticket, String bonus) {
+        Integer bonusNumber = createLotto.bonusNumber(bonus);
+        boolean countbonus = false;
+        if (ticket.contains(bonusNumber)) {
+            countbonus = true;
+        }
+        return countbonus;
+    }
 }
