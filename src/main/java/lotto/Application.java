08097bdb8d23c20bdd9d25740f9755bbdfd1ba2c
bearjun05 <bearjun05@naver.com>
2022-11-15 00:48:36 +0900

    feat(Lotto) : 구현부 예외처리 메세지 출력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,8 +4,8 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import lotto.domain.Confirm;
 import lotto.domain.CorrectNum;
 import lotto.domain.Draw;
 import lotto.domain.LotteryShop;
@@ -14,38 +14,41 @@ import lotto.domain.RateOfProfit;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("구입금액을 입력해 주세요.");
-        int amount = Integer.parseInt(readLine());
+        try {
+            System.out.println("구입금액을 입력해 주세요.");
+            int amount = Integer.parseInt(Confirm.validateDigit(readLine()));
+            LotteryShop lotteryShop = new LotteryShop();
+            int totalLottoCnt = lotteryShop.countLotto(amount);
+            List<Lotto> lottos = new ArrayList<>();
 
-        LotteryShop lotteryShop = new LotteryShop();
-        int totalLottoCnt = lotteryShop.countLotto(amount);
-        List<Lotto> lottos = new ArrayList<>();
+            System.out.println(totalLottoCnt + "개를 구매했습니다.");
+            for (int i = 0; i < totalLottoCnt; i++) {
+                List<Integer> lottoNum = lotteryShop.makeLotto();
+                lottos.add(new Lotto(lottoNum));
+                System.out.println(lottoNum);
+            }
 
-        System.out.println(totalLottoCnt + "개를 구매했습니다.");
-        for (int i = 0; i < totalLottoCnt; i++) {
-            List<Integer> lottoNum = lotteryShop.makeLotto();
-            lottos.add(new Lotto(lottoNum));
-            System.out.println(lottoNum);
+            System.out.println("당첨 번호를 입력해 주세요.");
+            List<String> winNum = Arrays.asList(readLine().split(","));
+
+            System.out.println("보너스 번호를 입력해 주세요.");
+            int bonusNum = Integer.parseInt(readLine());
+
+            System.out.println("당첨 통계");
+            System.out.println("---");
+            for (int i = 0; i < lottos.size(); i++) {
+                List<Integer> lotto = lottos.get(i).getNumbers();
+                CorrectNum correctNumType = (CorrectNum) Draw.findCorrectType(lotto, winNum, bonusNum);
+                correctNumType.setCnt(correctNumType.Cnt() + 1);
+            }
+            System.out.println("3개 일치 (5,000원) - " + CorrectNum.THREE.Cnt() + "개");
+            System.out.println("4개 일치 (50,000원) - " + CorrectNum.FOUR.Cnt() + "개");
+            System.out.println("5개 일치 (1,500,000원) - " + CorrectNum.FIVE.Cnt() + "개");
+            System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + CorrectNum.FIVE_BONUS.Cnt() + "개");
+            System.out.println("6개 일치 (2,000,000,000원) - " + CorrectNum.SIX.Cnt() + "개");
+            System.out.println(String.format("총 수익률은 %.1f%%입니다.", RateOfProfit.calculate(amount)));
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
         }
-
-        System.out.println("당첨 번호를 입력해 주세요.");
-        List<String> winNum = Arrays.asList(readLine().split(","));
-
-        System.out.println("보너스 번호를 입력해 주세요.");
-        int bonusNum = Integer.parseInt(readLine());
-
-        System.out.println("당첨 통계");
-        System.out.println("---");
-        for (int i = 0; i < lottos.size(); i++) {
-            List<Integer> lotto = lottos.get(i).getNumbers();
-            CorrectNum correctNumType = (CorrectNum) Draw.findCorrectType(lotto, winNum, bonusNum);
-            correctNumType.setCnt(correctNumType.Cnt() + 1);
-        }
-        System.out.println("3개 일치 (5,000원) - " + CorrectNum.THREE.Cnt() + "개");
-        System.out.println("4개 일치 (50,000원) - " + CorrectNum.FOUR.Cnt() + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + CorrectNum.FIVE.Cnt() + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + CorrectNum.FIVE_BONUS.Cnt() + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + CorrectNum.SIX.Cnt() + "개");
-        System.out.println(String.format("총 수익률은 %.1f%%입니다.", RateOfProfit.calculate(amount)));
     }
 }
