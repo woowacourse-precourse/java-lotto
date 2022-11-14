ad51d877eff26f03cc4d5c4b78f444e81e3afabc
RyoKuMan <ryokuman@naver.com>
2022-11-14 23:18:19 +0900

    feature ( LotteryShop ) : add generate_lottery

--- src/main/java/lotto/LotteryShop.java
+++ src/main/java/lotto/LotteryShop.java
@@ -1,4 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LotteryShop {
+    public static List<Lotto> generate_lottery(int number_of_tickets) {
+        List<Lotto> tickets = new ArrayList<>();
+
+        for (int i = 0; i < number_of_tickets; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            System.out.println(numbers);
+            tickets.add(new Lotto(numbers));
+        }
+
+        return tickets;
+    }
 }
