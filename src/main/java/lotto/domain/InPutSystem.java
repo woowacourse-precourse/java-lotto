0412145ccf06535ca1fad62bc948250660b8734d
josolha <josolha@nate.com>
2022-11-15 01:48:03 +0900

    feat(InPutSystem) : 입력 처리 기능 클래스 생성

---
+++ src/main/java/lotto/domain/InPutSystem.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InPutSystem {
+    public String inputNumber() {
+        return Console.readLine();
+    }
+
+}
