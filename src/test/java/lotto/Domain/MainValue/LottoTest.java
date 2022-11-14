357eb87c76a9bc75e82ce60a00ffe2b824d15fdc
Heesom <heesom.hs@gmail.com>
2022-11-14 18:02:11 +0900

    refactor(LottoTest, MoneyTest) : 유지보수 용이하게 코드 수정

--- src/test/java/lotto/Domain/MainValue/LottoTest.java
+++ src/test/java/lotto/Domain/MainValue/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto.Domain.MainValue;
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,19 +9,39 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    SoftAssertions softAssertions = new SoftAssertions();
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+        List<Integer> lotto = List.of(1, 2, 3, 4, 5, 6 ,7);
+        assertThatThrownBy(() -> new Lotto(lotto))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+        List<Integer> lotto = List.of(1, 2, 3, 4, 5, 5);
+
+        assertThatThrownBy(() -> new Lotto(lotto))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("1~45 이외의 번호 입력시 예외가 발생한다")
+    @Test
+    void createLottoOutOfRange() {
+        List<Integer> lottoMax = List.of(1, 2, 3, 4, 5, 56);
+        List<Integer> lottoMin = List.of(1, 2, 3, 4, 5, 0);
+
+        softAssertions
+                .assertThatThrownBy(() -> new Lotto(lottoMax))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        softAssertions
+                .assertThatThrownBy(() -> new Lotto(lottoMin))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        softAssertions.assertAll();
+    }
+
 }
--- src/test/java/lotto/Domain/MainValue/MoneyTest.java
+++ src/test/java/lotto/Domain/MainValue/MoneyTest.java
@@ -1,29 +1,33 @@
 package lotto.Domain.MainValue;
 
-import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class MoneyTest {
-    SoftAssertions softAssertions = new SoftAssertions();
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+public class MoneyTest {
     @DisplayName("1000원으로 나눠지지 않으면 에러가 발생한다")
     @Test
     void createMoneyNotThousand() {
-        softAssertions.assertThatThrownBy(() -> new Money("100"))
-                .hasMessageContaining("ERROR")
+        String str = "100";
+        assertThatThrownBy(() -> new Money(str))
                 .isInstanceOf(IllegalArgumentException.class);
-
-        softAssertions.assertAll();
     }
 
     @DisplayName("문자가 입력되면 에러가 발생한다")
     @Test
     void createMoneyChar() {
-        softAssertions.assertThatThrownBy(() -> new Money("1000a"))
-                .hasMessageContaining("ERROR")
+        String str = "10000a";
+        assertThatThrownBy(() -> new Money(str))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
 
-        softAssertions.assertAll();
+    @DisplayName("돈을 입력하면 로또 발행수량이 리턴되는지 확인")
+    @Test
+    void checkReturnValue() {
+        String str = "7000";
+
+        assertThat(new Money(str).getUserlottonum()).isEqualTo(7);
     }
 }
