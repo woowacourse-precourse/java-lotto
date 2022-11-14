2cfafb78ffdf16e688067041fe019088f5fa0896
ohhonggi <5ghdrl@naver.com>
2022-11-15 06:03:01 +0900

    feat($LottoIssuer): Add generateAllLotto function
    This function issues a lotto as much as the purchase amount entered by the user.
    
    Closes feat_3

--- src/main/java/lotto/domain/LottoIssuer.java
+++ src/main/java/lotto/domain/LottoIssuer.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class LottoIssuer {
@@ -9,6 +10,15 @@ public class LottoIssuer {
     public static Lotto generateLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return new Lotto(numbers);
+    }
+
+    public static List<Lotto> generateAllLotto(int amount) {
+        int lottoNumber = amount / 1000;
+        List<Lotto> lottos = new LinkedList<>();
+        for (int i = 0; i < lottoNumber; i++) {
+            lottos.add(generateLotto());
+        }
+        return lottos;
     }
 
 }
