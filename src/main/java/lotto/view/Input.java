591969412f637c3ce9a723cc0533e774188dc183
Isu Shin <isu010614@naver.com>
2022-11-15 06:45:51 +0900

    feat: 사용자에게 당첨번호와 보너스 번호를 입력받는 기능 추가
    -예외 처리: 숫자가 아닌 문자 입력 시 메시지 출력 및 예외 발생 후 프로그램 종료

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -2,7 +2,11 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+
+import static java.util.stream.Collectors.toList;
 
 public class Input {
     public static int getPrice() {
@@ -15,8 +19,30 @@ public class Input {
         validateNumber(number);
         return Integer.parseInt(number);
     }
+
     private static void validateNumber(final String numbers) throws IllegalArgumentException {
         if (!Pattern.matches("^[0-9]*$", numbers)) {
+            System.out.println("[ERROR] Only enter numbers.");
+            throw new IllegalArgumentException("[ERROR] Only enter numbers.");
+        }
+    }
+
+    public static List<Integer> getWinningNumber() {
+        System.out.println(System.lineSeparator() + Message.WINNING_NUMBER.print());
+        return stringToList(Console.readLine());
+    }
+
+    public static List<Integer> getBonusNumber() {
+        System.out.println(System.lineSeparator() + Message.BONUS_NUMBER.print());
+        return stringToList(Console.readLine());
+    }
+
+    private static List<Integer> stringToList(String numbers) {
+        try {
+            return Arrays.stream(numbers.split(","))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+        } catch (Exception e) {
             System.out.println("[ERROR] Only enter numbers.");
             throw new IllegalArgumentException("[ERROR] Only enter numbers.");
         }
