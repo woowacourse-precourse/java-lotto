654b3a689b2160387b5425c62dd5ded1d09105fb
p <gustjr2332@naver.com>
2022-11-14 13:04:04 +0900

    feat(CreateRandomLotto) : 구입금액에 맞게 로또를 오름차순으로 생성하는 기능

---
+++ src/main/java/Service/CreateRandomLotto.java
@@ -0,0 +1,23 @@
+package Service;
+
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CreateRandomLotto {
+
+    public static List<List<Integer>> createRandomLottoNumber(int price) {
+        int numberOfLotto = price / 1000;
+        List<List<Integer>> randomLottoNumbers = new ArrayList<>();
+
+        for (int count = 0; count < numberOfLotto; count++) {
+            List<Integer> randomLottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(randomLottoNumber);
+            randomLottoNumbers.add(randomLottoNumber);
+        }
+        return randomLottoNumbers;
+    }
+}
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,12 @@
-package lotto;
+package Exception;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,9 +16,23 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자만 입력해주세요");
         }
+        if (Set.copyOf(numbers).size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자를 입력하지마세요.");
+        }
+        for(int number : numbers){
+            if (!Pattern.matches("^[0-9]*$", String.valueOf(number))) {
+                throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
+            }
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 1~45사이의 숫자만 입력해주세요.");
+            }
+        }
+
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
