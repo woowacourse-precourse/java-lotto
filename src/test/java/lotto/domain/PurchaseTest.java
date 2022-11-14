bde644c9d78e498aafa0618a97261f5c195db8d2
leeyongjun <leeyongjun0310@naver.com>
2022-11-14 18:46:45 +0900

    test(lotto): 구입금액이 1000으로 나누어 떨어지지 않는지 확인하는 테스트 코드 작성

--- src/test/java/lotto/domain/PurchaseTest.java
+++ src/test/java/lotto/domain/PurchaseTest.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 class PurchaseTest {
@@ -11,5 +13,12 @@ class PurchaseTest {
     void getLottoCount_정상동작() {
         int input = Purchase.getLottoCount(5000);
         assertThat(input).isEqualTo(5);
+    }
+
+    @DisplayName("구입금액이 1000으로 나누어떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void getLottoCount_잘못된_입력() {
+        assertThatThrownBy(() -> Purchase.getLottoCount(5500))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
