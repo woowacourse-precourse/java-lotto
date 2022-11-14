0fba8ba9e91a3aa626b7611008743b6e1236426a
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-11-14 16:17:00 +0900

    add : 유저가 구매한 로또 리스트를 param으로 받아 출력해주는 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,5 +10,10 @@ import java.text.DecimalFormat;
 import java.util.List;
 
 public class OutputView {
-
+    public void showUserLotto(List<Lotto> list){
+        System.out.println(String.format(BUY_COUNT, list.size()));
+        for(Lotto lotto : list){
+            System.out.println(transLottoNumbersToForm(lotto.getNumbers()));
+        }
+    }
 }
