3a905db37cf1802fdc1f6993b98c0cf25b5f0104
ensk26 <tjensk26@gmail.com>
2022-11-15 05:50:37 +0900

    test(GetValidateInputTest): 테스트 메서드 이름 변경, 돈 단위 검증 테스트 추가 - 테스트 메서드 이름 더 직관적으로 변경 - 돈 단위 검증하는 메서드를 ApplicationTest -> GetValidateInputTest 으로 이동

--- src/test/java/lotto/GetValidateInputTest.java
+++ src/test/java/lotto/GetValidateInputTest.java
@@ -9,9 +9,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class GetValidateInputTest {
 
+    @DisplayName("금액이 1000원 단위가 아니면 예외가 발생한다.")
+    @Test
+    void getMoneyByWrongMoneyUnit() {
+        String number = "12345";
+        GetValidateInput getValidateInput = new GetValidateInput(number);
+
+        assertThatThrownBy(() -> getValidateInput.getMoney())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("숫자가 아닌 수를 넣었을때 예외가 발생한다.")
     @Test
-    void getNumberByNotNumber() {
+    void getMoneyByNotNumber() {
         String number = "112+";
         GetValidateInput getValidateInput = new GetValidateInput(number);
 
@@ -21,7 +31,7 @@ class GetValidateInputTest {
 
     @DisplayName("숫자가 아닌 수들을 넣었을때 예외가 발생한다.")
     @Test
-    void getNumberByNotNumbers() {
+    void getNumbersByNotNumbers() {
         String number = "1,2,3,+,5,6";
         GetValidateInput getValidateInput = new GetValidateInput(number);
 
@@ -31,7 +41,7 @@ class GetValidateInputTest {
 
     @DisplayName("보너스 번호가, 로또에 포함된 수이면 예외가 발생한다.")
     @Test
-    void getNumberByContainBonus() {
+    void getBonuseByContainBonus() {
         String number = "17";
         List<Integer> lotto = List.of(1, 2, 3, 4, 5, 17);
         GetValidateInput getValidateInput = new GetValidateInput(number);
@@ -42,7 +52,7 @@ class GetValidateInputTest {
 
     @DisplayName("보너스 번호가 범위 안에 있는지 확인 발생한다.")
     @Test
-    void getNumberByNotInRange() {
+    void getBonuseByNotInRange() {
         String number = "17";
         List<Integer> lotto = List.of(1, 2, 3, 49, 5, 17);
         GetValidateInput getValidateInput = new GetValidateInput(number);
