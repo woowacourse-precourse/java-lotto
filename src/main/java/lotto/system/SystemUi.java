e829ad71c34e09ea865b05d5bb25a1d9fadcd2a0
jin8073 <jin8073@gmail.com>
2022-11-14 14:37:04 +0900

    feat(SystemUi): 발행한 로또 개수 출력

--- src/main/java/lotto/system/SystemUi.java
+++ src/main/java/lotto/system/SystemUi.java
@@ -1,13 +1,21 @@
 package lotto.system;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.generation.Lotto;
+
+import java.util.List;
 
 public class SystemUi {
 
     private static final String START_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String ANNOUNCE_LOTTO_NUMBER_MESSAGE = "개를 구매했습니다.";
 
     public static int getPurchaseAmount() {
         System.out.println(START_MESSAGE);
         return new Parser().validatePurchaseAmount(Console.readLine());
+    }
+
+    public static void printLotto(List<Lotto> lottos) {
+        System.out.println(lottos.size()+ANNOUNCE_LOTTO_NUMBER_MESSAGE);
     }
 }
