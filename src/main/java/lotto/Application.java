cf56bee64a2bd67bf98e6fb56883e51d6b148e89
jwjin95 <jwjin95@yonsei.ac.kr>
2022-11-15 01:26:37 +0900

    feat: enum 활용한 결과 계산, 로또 구매 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -12,17 +12,52 @@ public class Application {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 
-    private Integer getResult(List<Integer> winningNumbers, List<Integer> lottoNumbers) {
+    private static List<Lotto> buyLottos(Integer purchaseAmount) {
+        List<Lotto> allLotto = new ArrayList<>();
+        for (int i = 0; i < purchaseAmount / 1000; i++) {
+            Lotto lotto = new Lotto(getRandomNumbers());
+            allLotto.add(lotto);
+        }
+        return allLotto;
+    }
+
+    private static Rank getResult(List<Integer> winningNumbers, Integer bonusNumber,
+        List<Integer> lottoNumbers) {
         int result = 0;
         for (Integer number : lottoNumbers) {
             if (winningNumbers.contains(number)) {
                 result += 1;
             }
         }
-        return result;
+        if (result == 6) return Rank.FIRST;
+        if (result == 5 && getBonusNumberResult(bonusNumber, lottoNumbers)) return Rank.SECOND;
+        if (result == 5) return Rank.THIRD;
+        if (result == 4) return Rank.FOURTH;
+        if (result == 3) return Rank.FIFTH;
+
+        return Rank.NONE;
     }
 
-    private boolean getBonusNumberResult(Integer bonusNumber, Integer lottoBonusNumber) {
-        return bonusNumber.equals(lottoBonusNumber);
+    private static Map<Rank, Integer> getResultOfAll(List<Lotto> lottos,
+        List<Integer> winningNumbers, Integer bonusNumber) {
+        Map<Rank, Integer> map = new EnumMap<>(Rank.class);
+        for (Lotto lotto : lottos) {
+            Rank result = getResult(winningNumbers, bonusNumber, lotto.getNumbers());
+            map.put(result, map.getOrDefault(result, 0)+1);
+        }
+
+        return map;
+    }
+
+    private static boolean getBonusNumberResult(Integer bonusNumber, List<Integer> lottoNumbers) {
+        return lottoNumbers.contains(bonusNumber);
+    }
+
+    private static Double calculateYield(Map<Rank, Integer> map, Integer purchaseAmount) {
+        double winAmount = 0;
+        for (Rank rank : map.keySet()) {
+            winAmount += rank.getPrize() * map.get(rank);
+        }
+        return Math.round(winAmount / purchaseAmount * 1000) / (double)10;
     }
 }
