adfe7c0103cd684d9af5fef09ce22d3552b28816
Be-student <presentsong@naver.com>
2022-11-14 11:59:19 +0900

    chore: 변수명 수정

--- src/main/java/lotto/domain/LottoCreator.java
+++ src/main/java/lotto/domain/LottoCreator.java
@@ -7,16 +7,17 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 final class LottoCreator {
-    private final PickNumbers random;
+    private static final String NULL_MESSAGE = "숫자를 뽑는 방식에 null 이 들어왔습니다";
+    private final PickNumbers pickNumbers;
 
-    public LottoCreator(PickNumbers random) {
-        validate(random);
-        this.random = random;
+    public LottoCreator(PickNumbers pickNumbers) {
+        validate(pickNumbers);
+        this.pickNumbers = pickNumbers;
     }
 
-    private void validate(PickNumbers random) {
-        if (random == null) {
-            throw new IllegalArgumentException();
+    private void validate(PickNumbers pickNumbers) {
+        if (pickNumbers == null) {
+            throw new IllegalArgumentException(NULL_MESSAGE);
         }
     }
 
@@ -29,7 +30,7 @@ final class LottoCreator {
     }
 
     private void addLotto(List<Lotto> lottos) {
-        List<Integer> numbers = random.pickUniquesInRange(
+        List<Integer> numbers = pickNumbers.pickUniquesInRange(
                 LottoConstants.LOTTO_START_INCLUSIVE.value(),
                 LottoConstants.LOTTO_END_INCLUSIVE.value(),
                 LottoConstants.LOTTO_LENGTH.value());
