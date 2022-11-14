4b92db05d742e0f45cb7791c04b173a7828ed954
solesy <tkddbs9801@gmail.com>
2022-11-14 15:35:10 +0900

    test(test): refactor 함에 따른 테스트코드 수정

--- src/test/java/lotto/ui/InputMoneyTest.java
+++ src/test/java/lotto/ui/InputMoneyTest.java
@@ -10,27 +10,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class InputMoneyTest extends NsTest{
     private static final String ERROR_MESSAGE = "[ERROR]";
     @Test
-    void 구입금액_음수() {
-        assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("-246"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessage(ERROR_MESSAGE + " 음수가 아닌 정수를 입력해야 합니다.");
-        });
-    }
-    @Test
     void 구입금액_정수아닌수() {
         assertSimpleTest(() -> {
             assertThatThrownBy(() -> runException("25.0"))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage(ERROR_MESSAGE + " 정수를 입력해야 합니다.");
-        });
-    }
-    @Test
-    void 구입금액_1000의배수아닌수() {
-        assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("1700"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessage(ERROR_MESSAGE + " 구입금액은 1000으로 나누어 떨어져야 합니다.");
         });
     }
     @Override
