20a5694dfddede13df78a3173d10e84ba5fe1f70
saint6839 <saint6839@gmail.com>
2022-11-14 19:56:21 +0900

    test(domain) : 구매한 로또 수 만큼 결과 값을 담은 Results를 반환하는지 테스트

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -17,7 +17,8 @@ class LottosTest {
         //when
         lottos.generateLottos(lottoCount);
         //then
-        assertThat(lottos.getLottos().size()).isEqualTo(lottoCount);
+        assertThat(lottos.getLottos()
+                         .size()).isEqualTo(lottoCount);
     }
 
     @DisplayName("구매한 로또들이 정렬되어서 반환되는지 테스트한다.")
@@ -25,7 +26,7 @@ class LottosTest {
     void checkLottos() {
         //given
         Lottos lottos = new Lottos();
-        lottos.add(new Lotto(List.of(6,5,4,3,2,1)));
+        lottos.add(new Lotto(List.of(6, 5, 4, 3, 2, 1)));
 
         //when
         Lottos purchasedLottos = lottos.checkLottos();
@@ -34,7 +35,24 @@ class LottosTest {
             assertThat(purchasedLottos.getLottos()
                                       .get(0)
                                       .getNumbers()
-                                      .get(i)).isEqualTo(i+1);
+                                      .get(i)).isEqualTo(i + 1);
         }
+    }
+
+    @DisplayName("당첨 로또와 보너스를 비교하여 구매한 로또 수 만큼 결과를 반환하는지 테스트한다.")
+    @Test
+    void generateResults() {
+        //given
+        Lottos lottos = new Lottos();
+        lottos.add(new Lotto(List.of(6, 5, 4, 3, 2, 1)));
+        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Bonus bonus = new Bonus(7);
+        //when
+        Results results = lottos.generateResults(winningLotto, bonus);
+        //then
+        assertThat(results.getResults()
+                          .size()).isEqualTo(2);
     }
 }
