6b600cb05e37fbab038d89ab9333241a63af759e
recicoco068 <gkapf838@gmail.com>
2022-11-15 02:05:52 +0900

    feat(Error.java) : 에러 메시지 Enum 클래스 생성

---
+++ src/main/java/lotto/Error.java
@@ -0,0 +1,19 @@
+package lotto;
+
+public enum Error {
+    RANGE_ERROR("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    COUNT_ERROR("[ERROR] 로또 번호는 6개여야 합니다."),
+    ONLY_NUMBER_ERROR("[ERROR] 숫자만 입력해야 합니다."),
+    DUPLICATE_ERROR("[ERROR] 로또 번호는 중복된 숫자가 있어선 안 됩니다."),
+    INVALID_FORMAT("[ERROR] 정해진 형식이 아닙니다. 숫자와 콤마로만 입력해 주세요.");
+
+    private final String message;
+
+    Error(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
