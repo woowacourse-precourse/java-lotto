a086770f091ef607f4ceab3310c38ccb8471265f
saint6839 <saint6839@gmail.com>
2022-11-14 14:54:17 +0900

    test(domain) : 당첨 로또와 구매 로또를 비교하여 결과가 잘 반환되는지 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,14 @@
 package lotto;
 
+import lotto.domain.Bonus;
 import lotto.domain.Lotto;
+import lotto.domain.Result;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -32,5 +35,22 @@ class LottoTest {
         //then
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호와 보너스 번호를 입력 받았을 때 일치하는 결과가 잘 반환되는지 테스트한다.")
+    @Test
+    void generateResult() {
+        //given
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto winningLotto = new Lotto(winningNumbers);
+        Bonus bonus = new Bonus(7);
+
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        //when
+        Result result = lotto.generateResult(winningLotto, bonus);
+
+        //then
+        assertThat(result.getMatchCount()).isEqualTo(5);
+        assertThat(result.isBonus()).isTrue();
     }
 }
