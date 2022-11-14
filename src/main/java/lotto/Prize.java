58bbf3b50256000d264dec58a24df06fc9330ea1
hoin <apicad0307@gmail.com>
2022-11-15 03:07:22 +0900

    feat(lotto/Prize.java): 등수를 표현할 Prize enum 생성

---
+++ src/main/java/lotto/Prize.java
@@ -0,0 +1,19 @@
+package lotto;
+
+public enum Prize {
+    FIRST("6개 일치", 2_000_000_000),
+    SECOND("5개 일치, 보너스 볼 일치", 30_000_000),
+    THIRD("5개 일치", 1_500_000),
+    FORTH("4개 일치", 50_000),
+    FIFTH("3개 일치", 5_000),
+    NOTHING("3개 미만 일치", 0);
+
+    private String matchCount;
+    private Integer reward;
+
+    Prize(String matchCount, Integer reward) {
+        this.matchCount = matchCount;
+        this.reward = reward;
+    }
+
+}
