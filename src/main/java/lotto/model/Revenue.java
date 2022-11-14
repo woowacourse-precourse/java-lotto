5c46bed189841123b5b13d200eae5ef8aa731563
gomljo <gomljo0307@naver.com>
2022-11-15 02:22:59 +0900

    feat(Revenue.java): 집계 정보를 업데이트하는 기능 구현

--- src/main/java/lotto/model/Revenue.java
+++ src/main/java/lotto/model/Revenue.java
@@ -19,11 +19,19 @@ public class Revenue {
     public int getRevenue(){
         return revenue;
     }
+    public HashMap<String, Integer> getRevenueStatics(){
+        return revenueStatics;
+    }
 
-    private void initializeRevenueStatics(){
+    public void initializeRevenueStatics(){
         List<String> rankNames=Rank.getRankNames();
         for(String rankName: rankNames){
             revenueStatics.put(rankName, ZERO_COUNT);
         }
+    }
+
+    public void updateRevenueStatics(String rankName){
+        int count = revenueStatics.get(rankName);
+        revenueStatics.put(rankName, ++count);
     }
 }
