6cc81130fd87597df91e03df2b6a50fd4f159f29
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-15 03:46:57 +0900

    test(LottoTest): 로또 출력시 나오는 문자열 테스트 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,4 +25,10 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호를 오름차순으로 정렬하여 출력한다.")
+    @Test
+    void printLottoByAscendingOrder() {
+        Lotto lotto = new Lotto(List.of(6, 1, 2, 3, 4, 5));
+        assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
+    }
 }
