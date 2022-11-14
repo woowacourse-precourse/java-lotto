748784fddabf2200c6c1fccf2d597890fb8a00eb
syahn <496300@naver.com>
2022-11-15 01:53:16 +0900

    feat: bonus 번호 검증 기능 구현
    - 로또 당첨 번호와 중복되지 않아야 함
    - 1부터 45까지의 숫자 중 하나여야 함

---
+++ src/main/java/lotto/Bonus.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import java.util.List;
+
+public class Bonus {
+    private final int bonus;
+
+    public Bonus(int number, List<Integer> lottoNumbers) {
+        validate(number, lottoNumbers);
+        this.bonus = number;
+    }
+
+    private void validate(int number, List<Integer> lottoNumbers) {
+        validateDuplicate(number, lottoNumbers);
+        validateRange(number);
+    }
+
+    void validateDuplicate(int number, List<Integer> lottoNumbers) {
+        if (lottoNumbers.contains(number)) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_DUPLICATE_ERROR.getMessage());
+        }
+    }
+
+    void validateRange(int number) {
+        if (number < 1 | number > 45) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_RANGE_ERROR.getMessage());
+        }
+    }
+
+    public int getBonus() {
+        return this.bonus;
+    }
+}
