8bca5ec89dfcec2d58fb04ff3765e75d45ed60a9
starwook <star.wookk@gmail.com>
2022-11-15 02:09:01 +0900

    feat(User.java): Add checkIfCharIsNumber Method
    User.java에 checkIfCharIsNumber 메소드를 추가하여라
    - 인수 char이 0~9사이의 char이 아니라면
    IllegalArgumentException()을 날려라

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,13 +1,27 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WinningNumber {
+    private static final char rest = ',';
+    private static final char zero = '0';
+    private static final char nine = '9';
+    private static final int maxNum = 45;
+    private static final int minNum = 0;
+    private static final int mulNum = 10;
+
     private final List<Integer> numbers;
 
-    public WinningNumber(List<Integer> numbers){
+    private WinningNumber(List<Integer> numbers){
         validate(numbers);
         this.numbers = numbers;
+    }
+
+    private void checkIfCharIsNumber(char c){
+        if(c<zero || c>nine){
+            throw new IllegalArgumentException();
+        }
     }
 
     private void validate(List<Integer> numbers){
