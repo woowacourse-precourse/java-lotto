f37eca1075dc1710e1e49693029bca89d85b9995
This2sho <zx00018@naver.com>
2022-11-15 03:26:30 +0900

    feat(LotteryMachine): 로또 지정 개수 만큼 자동 발행 기능 구현

--- src/main/java/lotto/domain/LotteryMachine.java
+++ src/main/java/lotto/domain/LotteryMachine.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.LinkedList;
 import java.util.List;
 
 public class LotteryMachine {
@@ -8,5 +9,13 @@ public class LotteryMachine {
 	public static Lotto autoGenerate() {
 		List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
 		return new Lotto(numbers);
+	}
+
+	public static List<Lotto> autoGenerate(int lottoCount) {
+		List<Lotto> lottos = new LinkedList<>();
+		for (int i = 0; i < lottoCount; i++) {
+			lottos.add(autoGenerate());
+		}
+		return lottos;
 	}
 }
