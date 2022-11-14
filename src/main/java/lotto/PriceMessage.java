cbe0016ccb86c61869f932f344ae92287afcc2f9
YIMHYEONGSUP <hs.yim@illb.co.kr>
2022-11-14 19:19:49 +0900

    feat - 로또 금액 메세지 유틸

--- src/main/java/lotto/PriceMessage.java
+++ src/main/java/lotto/PriceMessage.java
@@ -2,21 +2,16 @@ package lotto;
 
 public enum PriceMessage {
 
-    RIGHT_THREE(5000 , "3개 일치 (5,000원) - "),
-    RIGHT_FOUR(50000 , "4개 일치 (5,0000원) - "),
-    RIGHT_FIVE(1500000 , "5개 일치 (1,500,000원) - "),
-    RIGHT_FIVE_BONUS(30000000, "5개 일치 , 보너스 볼 일치(30,000,000원) -"),
-    RIGHT_ALL(2000000000, "6개 일치 (2,000,000,000원) - ");
+    RIGHT_THREE("3개 일치 (5,000원) - "),
+    RIGHT_FOUR("4개 일치 (50,000원) - "),
+    RIGHT_FIVE("5개 일치 (1,500,000원) - "),
+    RIGHT_FIVE_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    RIGHT_ALL("6개 일치 (2,000,000,000원) - ");
 
-    private int price;
     private String message;
 
-    PriceMessage(final int price, final String message) {
-        this.price = price;
+    PriceMessage(final String message) {
         this.message = message;
-    }
-    public int price() {
-        return price;
     }
     public String message() {
         return message;
