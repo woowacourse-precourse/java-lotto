89a185e6aa984cfe819974a5c8f251e8f067efc9
ESeokhwan <huan99@hanmail.net>
2022-11-14 20:35:31 +0900

    feat: 당첨 내역 설명을 얻어오는 기능 추가

--- src/main/java/lotto/model/statistics/LottoStatistics.java
+++ src/main/java/lotto/model/statistics/LottoStatistics.java
@@ -2,9 +2,11 @@ package lotto.model.statistics;
 
 import lotto.model.payment.Payment;
 
+import java.text.DecimalFormat;
 import java.util.Map;
 
 public class LottoStatistics {
+    private static final DecimalFormat decimalFormatter = new DecimalFormat("###,###.#");
     private final Payment payment;
     private final Map<LottoResult, Integer> resultMap;
 
@@ -14,8 +16,19 @@ public class LottoStatistics {
     }
 
     public String getHistoryDescription() {
-        // TODO 구현 필요
-        return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        for (LottoResult lottoResult: LottoResult.values()) {
+            if(lottoResult.equals(LottoResult.MISS)) {
+                continue;
+            }
+
+            int resultCount = resultMap.getOrDefault(lottoResult, 0);
+            stringBuilder.append(lottoResult.getDescription())
+                    .append(" - ")
+                    .append(resultCount)
+                    .append("개");
+        }
+        return stringBuilder.toString();
     }
 
     public String getReturnRateDescription() {
