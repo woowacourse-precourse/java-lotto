1f2f4352bd1fade723bde173281dc05c24dbc53e
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-15 01:37:57 +0900

    refactor: 클래스 변수를 인스턴스 변수로 수정

--- src/main/java/lotto/domain/Judgement.java
+++ src/main/java/lotto/domain/Judgement.java
@@ -3,9 +3,8 @@ package lotto.domain;
 import java.util.List;
 
 public class Judgement {
-    public int result = 0;
-    public boolean second = false;
     public int rankLotto(List<Integer> lottery, List<Integer> winningNumber) {
+        int result = 0;
         for (int i=0; i<lottery.size(); i++){
             int correctNumber = winningNumber.get(i);
             if (lottery.contains(correctNumber)) {
@@ -14,7 +13,8 @@ public class Judgement {
         }
         return result;
     }
-    public boolean secondPrize(List<Integer> lottery, int bonus) {
+    public boolean secondPrize(List<Integer> lottery, int bonus, int result) {\
+        boolean second = false;
         if (result == 5) {
             if (lottery.contains(bonus)){
                 second = true;
