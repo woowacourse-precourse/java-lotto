65899fd3d60bcc5818e26d12c97e0b8ff7a63dd9
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-14 22:56:46 +0900

    feat(Input) : add function that get purchase amount

---
+++ src/main/java/ui/Input.java
@@ -0,0 +1,11 @@
+package ui;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    int getMoney(){
+        int money = Integer.parseInt(Console.readLine());
+        return money;
+    }
+}
---
+++ src/main/java/ui/Output.java
@@ -0,0 +1,16 @@
+package ui;
+
+public class Output {
+    private static final String PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String THREE_WINNING = "3개 일치 (5,000원) - ";
+    private static final String FOUR_WINNING = "4개 일치 (50,000원) - ";
+    private static final String FIVE_WINNING = "5개 일치 (1,500,000원) - ";
+    private static final String FIVE_AND_BONUS_WINNING = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    private static final String SIX_WINNING = "6개 일치 (2,000,000,000원) - ";
+
+    void printPriceInputMessage(){
+        System.out.println(PRICE_INPUT_MESSAGE);
+    }
+}
