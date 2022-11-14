992bd58a5ba1cee0ddb336ffda03fe5f2507f0a0
jijongkwon <help3451@naver.com>
2022-11-14 22:37:43 +0900

    feat : 로또 번호 중복확인 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
@@ -7,6 +7,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkDuplication(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +17,9 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void checkDuplication(List<Integer> numbers){
+        if(numbers.size() != numbers.stream().distinct().count()){
+            throw new IllegalArgumentException();
+        }
+    }
 }
