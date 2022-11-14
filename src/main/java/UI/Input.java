32740828997ff8e6b80e027da7a453e565dcafb6
p <gustjr2332@naver.com>
2022-11-14 15:36:17 +0900

    feat(Input) : 당첨번호 입력 기능 추가

--- src/main/java/UI/Input.java
+++ src/main/java/UI/Input.java
@@ -1,7 +1,12 @@
 package UI;
 
+import Exception.LottoNumberException;
 import Exception.PriceException;
 import camp.nextstep.edu.missionutils.Console;
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Input {
 
@@ -11,4 +16,19 @@ public class Input {
         PriceException.checkException(price);
         return Integer.valueOf(price);
     }
+
+    public static List<Integer> winningNumbers() {
+        Output.inputWinningNumber();
+        String[] numbers = Console.readLine().split(",");
+
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String number : numbers) {
+            LottoNumberException.checkException(number);
+            winningNumbers.add(Integer.valueOf(number));
+        }
+        Lotto lotto = new Lotto(winningNumbers);
+
+        return lotto.getNumbers();
+    }
+
 }
