c7e0d918352c56baf6572e3f82c36850bd4cbde4
JUNBEOM_LEE <bom1215@naver.com>
2022-11-15 00:54:22 +0900

    feat(Class Application): Change Lotto numbers
    chage the type of return from 1D list to 2D list

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,21 +2,33 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
 
 public class Application {
+    UI UI = new UI();
+    HashMap<Integer, Integer> Lotto_Choice_Result = new HashMap<>();
 
-    public List<Integer> Lotto_number(){
 
-    List<Integer> Lotto_numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-    Collections.sort(Lotto_numbers);
-    return Lotto_numbers;    
+
+
+    public List<List<Integer>> Lotto_number(Integer money){
+        Integer number_of_lotto_paper  = money / 1000;
+        List<List<Integer>> Total_Lotto_numbers = new ArrayList<>();
+
+        for (Integer i = 0; i < number_of_lotto_paper; i++){
+            List<Integer> Lotto_numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(Lotto_numbers);
+            Total_Lotto_numbers.add(Lotto_numbers);
+        }
+        UI.Lotto_Number_Print(number_of_lotto_paper,Total_Lotto_numbers);
+        return Total_Lotto_numbers;
     }
+
+
 
     public static void main(String[] args) {
 
-
-        // TODO: 프로그램 구현
+       // TODO: 프로그램 구현
     }
 }
