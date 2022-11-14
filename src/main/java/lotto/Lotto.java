8eabd46d788198b1dab53c6711b85e99674de046
dydckd <toofriend09@naver.com>
2022-11-14 10:29:02 +0900

    feat : 로또 번호 출력시 오름차순으로 출력
    - Lotto클래스에 numbers를 초기화 할때 오름차순의로 되도록 생성자에 numbers를 오름차순으로 정렬하는 메서드를 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,14 +9,31 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateNum(numbers);
+        sortAsc(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개 입니다.");
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validateNum(List<Integer> numbers) {
+        if(numbers.size() != numbers.stream().distinct().count()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않습니다.");
+        }
+    }
+
+    public List<Integer> sortAsc(List<Integer> numbers){
+        Collections.sort(numbers);
+        return numbers;
+    }
+    public String toString(){
+        return String.valueOf(numbers);
+    }
+
+
 }
