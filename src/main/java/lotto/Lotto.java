c9408740c5f5eb7640a80c5de914996d9297b60d
alpacaHyun <flaehdan@gmail.com>
2022-11-14 20:43:02 +0900

    type : feat ✨ 입력받은 로또의 범위 구하기
    body :
    
    footer :

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,7 +14,7 @@ public class Lotto {
     private static final String WINNING_LOTTO = "당첨 번호를 입력해주세요.";
     private static final String NEGATIVE_PRICE_ERROR = "음수인 금액은 입력할 수 없습니다.";
     private static final String PUT_BONUS_NUMBER = "보너스 번호를 입력해주세요";
-    public static int bonusNum ;
+    public static int bonusNum;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -22,15 +22,15 @@ public class Lotto {
     }
 
     // 우승 숫자 입력받기
-    public static List<Integer> inputLotto(List<Integer> lotto){
+    public static List<Integer> inputLotto(List<Integer> lotto) {
         System.out.println(WINNING_LOTTO);
         String num = Console.readLine();
-        String [] inputNum = num.split(",");
+        String[] inputNum = num.split(",");
         try {
             for (int i = 0; i < inputNum.length; i++) {
                 lotto.add(Integer.parseInt(inputNum[i]));
             }
-        }catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR + INVALID_LOTTO_NUMBERS);
         }
         Validator.validateWinningNumberInput(inputNum); // 6자리인지 확인
@@ -40,12 +40,12 @@ public class Lotto {
 
 
     // 보너스 번호 입력받기
-    public static int bonusNum(){
+    public static int bonusNum() {
         System.out.println(PUT_BONUS_NUMBER);
         String bonus = Console.readLine();
-        try{
-        bonusNum = Integer.parseInt(bonus);
-        }catch(Exception e){
+        try {
+            bonusNum = Integer.parseInt(bonus);
+        } catch (Exception e) {
             throw new IllegalArgumentException(INVALID_LOTTO_NUMBERS);
         }
 
@@ -56,24 +56,35 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(ERROR + INVALID_WINNING_NUMBER_LENGTH_ERROR);
         }
-        if(validateDuplicate(numbers)){
+        if (validateDuplicate(numbers)) {
             throw new IllegalArgumentException(ERROR + "로또 번호는 중복되지 않아야 합니다.");
         }
-        if(validateRange(numbers)){
+        if (validateRange(numbers)) {
             throw new IllegalArgumentException(ERROR + "로또 번호는 1부터 45 사이의 숫자입니다.");
         }
 
     }
+
     // 로또 중복 확인
-    static boolean validateDuplicate(List<Integer> numbers){
-        for(int a : numbers){
-            if(numbers.indexOf(a) != numbers.lastIndexOf(a)){
+    static boolean validateDuplicate(List<Integer> numbers) {
+        for (int a : numbers) {
+            if (numbers.indexOf(a) != numbers.lastIndexOf(a)) {
                 return true;
             }
         }
         return false;
     }
 
-    static boolean
+
+    // 1부터 45까지의 수
+    static boolean validateRange(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) < 1 || numbers.get(i) > 45){
+                return true;
+            }
+        }
+
+        return false;
+    }
 
 }
