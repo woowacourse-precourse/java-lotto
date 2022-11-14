cec8c0a42b4b18cf4ea79e3d8d4442ceb4704da7
heesang <dlagmltkd12@naver.com>
2022-11-15 08:05:02 +0900

    test: LottoNumberTest int 형 변환 테스트

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -7,9 +7,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoNumberTest {
 
-    @DisplayName("equals 메소드 확인")
+    @DisplayName("equals 메소드 테스트")
     @Test
     void equals() {
         assertThat(new LottoNumber(1)).isEqualTo(new LottoNumber(1));
+    }
+
+    @DisplayName("int 형 변환 테스트")
+    @Test
+    void mapToInt() {
+        assertThat(new LottoNumber(1).mapToInt()).isEqualTo(1);
     }
 }
