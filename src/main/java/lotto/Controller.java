018346431c6b82b6b4d1935ecddc199957318b05
osy9757 <osy9757@gmail.com>
2022-11-14 17:10:26 +0900

    feat : 컨트롤러에서 LottoAmount.java를 이용하여 구매금액을 입력받는 함수 구현

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -2,10 +2,23 @@ package lotto;
 
 import lotto.print.PrintInputMessage;
 import lotto.print.PrintOutputMessage;
+import lotto.LottoAmount;
 
 public class Controller {
-
     public void start() {
-
+        LottoAmount purchaseAmount = inputLottoAmount();
     }
+
+    private LottoAmount inputLottoAmount() {
+        try {
+            return new LottoAmount(PrintInputMessage.getPurchaseAmount());
+        } catch (IllegalArgumentException e) {
+            PrintOutputMessage.printException(e);
+            return  inputLottoAmount();
+        }
+    }
+
+
+
+
 }
