3b69a10d7a72e3fa28c575fefa280dd98109c877
hum02 <ehdgma02@gmail.com>
2022-11-14 15:21:47 +0900

    test(lotto): 수익률 계산 테스트 코드 수정
    퍼센트에이지 비교이기에 100 곱한 값과 비교.
    소수점까지 나오는 값 case추가

--- src/test/java/lotto/LottoResultTest.java
+++ src/test/java/lotto/LottoResultTest.java
@@ -19,15 +19,16 @@ class LottoResultTest {
         EnumMap<Rank, Integer> ranks = new EnumMap<>(Rank.class);
         ranks.put(RANK_FOUR, 2);  // 상금 100_000
         ranks.put(RANK_TWO, 1);   // 상금 30_000_000
-        ranks.put(RANK_ONE, 1);  // 상금 2_000_000_000
         LottoResult result = new LottoResult(ranks);
 
         //when
         float buyPrice1 = 3_000;
         float buyPrice2 = 10_000;
+        float buyPrice3 = 30_101_000;
 
         //then
-        assertThat(result.calculateEarningRatio(3_000)).isEqualTo((float)2_030_100_000/buyPrice1);
-        assertThat(result.calculateEarningRatio(10_000)).isEqualTo((float)2_030_100_000/buyPrice2);
+        assertThat(result.calculateEarningRatio(3_000)).isEqualTo(100 * (30_100_000 / buyPrice1));
+        assertThat(result.calculateEarningRatio(10_000)).isEqualTo(100 * (30_100_000 / buyPrice2));
+        assertThat(result.calculateEarningRatio(30_101_000)).isEqualTo(100 * (30_100_000 / buyPrice3));
     }
 }
