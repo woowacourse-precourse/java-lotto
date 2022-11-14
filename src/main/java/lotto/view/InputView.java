b20aa4c8277931984a79049ae4211a8bc3deb6da
pdot715 <pdot715@gmail.com>
2022-11-14 21:31:27 +0900

    refactor(Lotto):Lotto 클래스 도메인 패키지 하위로 이동 및 소스코드 정리

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+    private static final String PURCHASE_LOTTO_INPUT_MESSAGE = "구입금액을 입력해주세요.";
+    private static final String PURCHASE_LOTTO_COUNT_MESSAGE = "당첨 번호를 입력후재세요"
+
+
+}
