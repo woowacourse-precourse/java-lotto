752ba32a768b2290879ac0261c4ed86fe8a86d37
seung-hun2 <jungsh2328@naver.com>
2022-11-14 20:47:50 +0900

    - publish 함수 - 발행한 로또 수량 및 번호를 리턴하고 출력하는 함수

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,6 +6,10 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int count = Lotto.amount();
-
+        if(count!=0) {
+            List<List<Integer>> ticket = Lotto.publish(count);
+            List<Integer> winNum = Lotto.createWinningnum();
+            Lotto.createBonusnum(winNum);
+        }
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -56,4 +56,15 @@ public class Lotto {
         }
         return winNum;
     }
+    public static List<List<Integer>> publish(int count){
+        System.out.println(count+"개를 구매했습니다.");
+        List<List<Integer>> ticket = new ArrayList<>();
+        for(int i=0;i<count;i++){
+            List<Integer> a = Randoms.pickUniqueNumbersInRange(1,45,6);
+            new Lotto(a);
+            ticket.add(a);
+            System.out.println(a);
+        }
+        return ticket;
+    }
 }
