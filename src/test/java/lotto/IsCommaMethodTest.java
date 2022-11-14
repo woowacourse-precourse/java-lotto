e1c2c9eb14bb7f46b5d201edda92d9db76722241
neppiness <0414kjh@naver.com>
2022-11-14 22:10:07 +0900

    test: add IsCommaMethodTest (src/main/java/lotto)
    Methods to check if character is comma

--- src/test/java/lotto/IsCommaMethodTest.java
+++ src/test/java/lotto/IsCommaMethodTest.java
@@ -7,20 +7,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class IsCommaMethodTest {
     @Test
-    void isComma_메소드_예외_테스트1() {
-        char charAtIndex = '!';
-        assertThat(isComma(charAtIndex)).isEqualTo(false);
+    void isComma_메소드_정상_입력_테스트() {
+        char character = ',';
+        assertThat(isComma(character)).isEqualTo(true);
     }
 
     @Test
-    void isComma_메소드_예외_테스트2() {
-        char charAtIndex = ' ';
-        assertThat(isComma(charAtIndex)).isEqualTo(false);
-    }
-
-    @Test
-    void isComma_메소드_정상_입력_테스트3() {
-        char charAtIndex = ',';
-        assertThat(isComma(charAtIndex)).isEqualTo(true);
+    void isComma_메소드_예외_테스트() {
+        for(char asciicode = 0; asciicode < 256; asciicode++) {
+            if(asciicode == ',') continue;
+            assertThat(isComma(asciicode)).isEqualTo(false);
+        }
     }
 }
