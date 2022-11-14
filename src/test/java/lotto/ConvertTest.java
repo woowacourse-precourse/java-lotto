5e3cf7335e3d2d325b68aaec8eecc44b3a0bec7c
impati <impati0716@icloud.com>
2022-11-14 22:29:44 +0900

    test : 당첨 번호 변환 테스트

--- src/test/java/lotto/ConvertTest.java
+++ src/test/java/lotto/ConvertTest.java
@@ -14,4 +14,16 @@ class ConvertTest {
         Integer convertNumber = Convert.costConvert("1234");
         Assertions.assertThat(convertNumber).isEqualTo(1234);
     }
+    @Test
+    @DisplayName("당첨 번호 변환 테스트 ")
+    public void winningConvert() throws Exception{
+        Lotto lotto = Convert.winningConvert("1,2,3,4,5,6");
+        Assertions.assertThat(lotto.getNumbers().get(0)).isEqualTo(1);
+        Assertions.assertThat(lotto.getNumbers().get(1)).isEqualTo(2);
+        Assertions.assertThat(lotto.getNumbers().get(2)).isEqualTo(3);
+        Assertions.assertThat(lotto.getNumbers().get(3)).isEqualTo(4);
+        Assertions.assertThat(lotto.getNumbers().get(4)).isEqualTo(5);
+        Assertions.assertThat(lotto.getNumbers().get(5)).isEqualTo(6);
+
+    }
 }
