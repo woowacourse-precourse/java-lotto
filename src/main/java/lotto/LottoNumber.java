a0ad00c83b64acb56b4ab83c2a159adb0dd879a1
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-14 13:03:02 +0900

    feat(LottoNumber): add method used in sort

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -1,12 +1,19 @@
 package lotto;
 
-public class LottoNumber extends Number {
+public class LottoNumber extends Number implements Comparable<LottoNumber> {
+
+    @Override
+    public int compareTo(LottoNumber lottoNumber) {
+        return this.getNumber() - lottoNumber.getNumber();
+    }
+
     LottoNumber(Integer number) {
         this.validate(number);
         this.setNumber(number);
     }
 
     private void validate(Integer number) {
-        this.validator.throwIfNotInRange(this.getNumber());
+        this.validator.throwIfNotInRange(number);
     }
+
 }
