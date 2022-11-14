139f3ebfb03917f757ddff5df4c0be9c3797e67d
tlsqj <rnjstlsqja@inha.edu>
2022-11-14 20:58:01 +0900

    feat(Check.java) Check 클래스 생성
    - 생성된 lotto 와 입력받은 당첨 번호, 보너스 번호 비교
    - 통계 계산
    - 수익률 계산

---
+++ src/main/java/lotto/Check.java
@@ -0,0 +1,48 @@
+package lotto;
+
+public class Check {
+    static Output output = new Output();
+
+    public static void numberCompare(int lottoCount, int[][] lottoArray, int[] userInputLottoNumber, int userInputBonusNumber, int purchasedValue){
+        int[] correctCount = new int[5];
+
+        for(int i = 0; i < lottoCount; i++){
+            int countTmp = 0;
+
+            countTmp = countTmpCounter(lottoArray[i], userInputLottoNumber, countTmp);
+
+            winningStatisticsMaker(lottoArray[i], userInputBonusNumber, correctCount, countTmp);
+        }
+        output.winningStatisticPrinter(correctCount);
+        output.yieldCalculation(correctCount, purchasedValue);
+    }
+
+    public static int countTmpCounter(int[] lottoArray, int[] userInputLottoNumber, int countTmp){
+        for (int i = 0; i < 6; i++){
+            for (int j = 0; j < 6; j++){
+                if (lottoArray[i] == userInputLottoNumber[j]){
+                    countTmp++;
+                }
+            }
+        }
+
+        return countTmp;
+    }
+
+    public static void winningStatisticsMaker(int[] lottoArray, int userInputBonusNumber, int[] correctCount, int countTmp){
+        for (int i = 3; i <= 6; i++){
+            if (countTmp == i){
+                correctCount[i - 3]++;
+            }
+        }
+
+        if (countTmp == 5){
+            for (int i = 0; i < 6; i++){
+                if (lottoArray[i] == userInputBonusNumber){
+                    correctCount[2]--;
+                    correctCount[3]++;
+                }
+            }
+        }
+    }
+}
