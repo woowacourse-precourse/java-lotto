d4c26789ef26ca4d076f4569d677bc14d15f74d0
roasted-penguin <swkwon25@hanyang.ac.kr>
2022-11-15 03:49:46 +0900

    feat Lotto, LottoTest's testcase

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
+
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +13,11 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        int min = Collections.min(numbers);
+        int max = Collections.max(numbers);
+        if(numbers.size()!=6) throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력 해주세요.");
+        if(numbers.size() != numbers.stream().distinct().count()) throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다!");
+        if(min<1||min>45) throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        if(max<1||max>45) throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
-
-    // TODO: 추가 기능 구현
 }
