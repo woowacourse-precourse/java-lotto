32487af0ff5c45cf1a6045f41524710582690481
DevJay1024 <devjay1024@gmail.com>
2022-11-14 18:42:25 +0900

    refactor: 사용자 입력 예외처리 문구에 기본 문구 추가 "[ERROR]"

--- src/main/java/lotto/InvalidInputMessage.java
+++ src/main/java/lotto/InvalidInputMessage.java
@@ -2,6 +2,8 @@ package lotto;
 
 public class InvalidInputMessage {
 
+    public static String ERR_DEFAULT = "[ERROR] ";
+
     public static String ERR_MONEY_OVER_RANGE = "로또 구매금액이 허용 범위 이내여야 합니다.";
     public static String ERR_MONEY_UNIT = "로또 구매금액은 1000원 단위로 입력해야 합니다.";
     public static String ERR_EMPTY_INPUT = "입력값이 비어있습니다.";
