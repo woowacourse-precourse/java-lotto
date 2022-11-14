c8aa4d008a0a21c21377c59e0c0e980c176deb51
wonju-dev <wonjulee.dev@gmail.com>
2022-11-14 15:16:56 +0900

    feat(RightFormatCond): 올바른 형식의 입력인지 검증하는 로직 구현
    RightFormatCond는 사용자 입력값이 올바른 형태인지 확인합니다.

---
+++ src/main/java/lotto/condition/RightFormatCond.java
@@ -0,0 +1,17 @@
+package lotto.condition;
+
+import lotto.LottoMachine;
+
+public class RightFormatCond implements Condition {
+
+    private static final Condition instance = new RightFormatCond();
+
+    public static Condition getInstance() {
+        return instance;
+    }
+
+    @Override
+    public Boolean isSatisfied(String input) {
+        return input.split(", ").length == LottoMachine.LOTTO_NUMBER_SIZE;
+    }
+}
