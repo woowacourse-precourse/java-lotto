0b3f558c43c666fac1d5a87f915d7d1bcb9154ea
yxnxx <jh4rgh0@gmail.com>
2022-11-15 00:10:16 +0900

    feat(InputView): get winning numbers

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -2,12 +2,17 @@ package lotto.controller;
 
 import lotto.model.GenerateLotto;
 import lotto.model.Money;
+import lotto.model.WinningNumbers;
 import lotto.view.OutputView;
 public class LottoGame {
     public void play() {
         Money money = new Money();
         money.setInputMoney();
+
         GenerateLotto generateLotto = new GenerateLotto();
         OutputView.lottoTicketsOut();
+
+        WinningNumbers winningNumbers = new WinningNumbers();
+        winningNumbers.setWinningNumbers();
     }
 }
---
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -0,0 +1,14 @@
+package lotto.model;
+
+import lotto.view.InputView;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private static List<Integer> winningNumbers;
+
+    public void setWinningNumbers() {
+        String input = InputView.getWinningNumbers();
+
+    }
+}
