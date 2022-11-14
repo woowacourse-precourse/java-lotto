e5d2f7a4900133df8a6ca8e2dd8ac007080a1f51
hoin <apicad0307@gmail.com>
2022-11-15 03:22:41 +0900

    feat(lotto/Game.java): 출력문구 상수화 기능 구현

---
+++ src/main/java/lotto/Game.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class Game {
+    private static final String INPUT_PRICE = "구입금액을 입력해주세요";
+    private static final String BUY_COUNT = "개를 구매했습니다.";
+    private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+}
