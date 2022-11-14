f631f2bce404d120bae0afd30cdfee95301e5a49
Yoobi <quddlr96@naver.com>
2022-11-15 01:49:52 +0900

    test(yield): add test code for yield validation

--- src/test/java/lotto/YieldTest.java
+++ src/test/java/lotto/YieldTest.java
@@ -1,7 +1,20 @@
 package lotto;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 
 class YieldTest {
 
+    @DisplayName("수익률 = (이익/구매금액)*100 이다.")
+    @Test
+    void createYieldRateTest() {
+        double purchasePrice = 123000;
+        double profits = 12300;
+        double expected = 10;
+        Yield yield = new Yield(purchasePrice, profits);
+        assertThat(yield.getYieldRate()).isEqualTo(expected);
+    }
 }
