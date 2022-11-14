00b0e0f88f3d5a7ecfecfd3e40fab04666f7d983
5uhwann <5uhwann27@gmail.com>
2022-11-15 04:38:54 +0900

    refactor(로또회사): 2등 당첨정보 수정
    5개, 보너스 볼 일치 -> 5개 일치, 보너스 볼 일치

--- src/main/java/lotto/domain/lotto/lottocompany/Win.java
+++ src/main/java/lotto/domain/lotto/lottocompany/Win.java
@@ -3,7 +3,7 @@ package lotto.domain.lotto.lottocompany;
 public enum Win {
     first("6개 일치", 6,
             2_000_000_000, "2,000,000,000원"),
-    second("5개, 보너스 볼 일치", 5,
+    second("5개 일치, 보너스 볼 일치", 5,
             30_000_000, "30,000,000원"),
     third("5개 일치", 5,
             1_500_000, "1,500,000원"),
