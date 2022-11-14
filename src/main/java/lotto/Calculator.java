c3ccf8b12d9ebccb0f40a2f654d22f93743ecd52
YooByeongIk <quddlr96@naver.com>
2022-11-14 23:15:11 +0900

    feat(lotto): add method to print history

--- src/main/java/lotto/Calculator.java
+++ src/main/java/lotto/Calculator.java
@@ -62,4 +62,14 @@ public class Calculator {
         List<Integer> purchaseLottoNumbers = purchaseLotto.getNumbers();
         return purchaseLottoNumbers.contains(bonusNumber);
     }
+
+    public void printHistory() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + history.get("3") + "개");
+        System.out.println("4개 일치 (50,000원) - " + history.get("4") + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + history.get("5") + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + history.get("5+") + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + history.get("6") + "개");
+    }
 }
