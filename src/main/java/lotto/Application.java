b4909a97abcf4902834e246d0ce34dda8241a890
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-14 17:51:31 +0900

    feat: 로또 게임 중 예외에 대한 처리 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,10 @@ import lotto.game.LottoGame;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        new LottoGame().start();
+        try {
+            new LottoGame().start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
