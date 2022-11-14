5776bc1c23fbe01a8261b44612bb5dffacf8966d
jw0293 <k38836919@gmail.com>
2022-11-14 13:09:05 +0900

    feat : Add exception Mod 1000 [1000원으로 나누어 떨어지지 않는 경우 예외처리]

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -1,2 +1,12 @@
-package lotto;public class Exception {
+package lotto;
+
+public class Exception {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    public void validatePrice(int price){
+        if(price % 1000 != 0){
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 로또 금액은 1000원 단위로 나누어떨어져야합니다.");
+        }
+    }
 }
