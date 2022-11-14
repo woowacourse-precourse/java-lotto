2b7b1fe76fee91b5dcb7c93fa01ec230fd6be381
kimilchul <huj0159@gmail.com>
2022-11-14 14:12:44 +0900

    feat: find duplicated number

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,18 +1,30 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        findDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void findDuplicate(List<Integer> numbers){
+        Set<Integer> numberPool = new HashSet<>();
+        for (Integer number : numbers){
+            if(!numberPool.add(number)){
+                throw new IllegalArgumentException("[ERROR] 로또에 중복되는 수가 있습니다");
+            }
         }
     }
 
