4c0729c3bdfed7951382ee2d0f83298e35bbbcc6
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-11-15 01:26:14 +0900

    feat: Enum으로 각 등수와 상금 관계를 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,32 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Function;
 
 public class Application {
+    public enum LotteryWon {
+        FirstPlace(value -> value * 2_000_000_000),
+        SecondPlace(value -> value * 30_000_000),
+        ThirdPlace(value -> value * 1_500_000),
+        FourthPlace(value -> value * 50_000),
+        FifthPlace(value -> value * 5000);
+
+        private Function<Integer, Integer> expression;
+
+        LotteryWon(Function<Integer, Integer> expression) {
+            this.expression = expression;
+        }
+
+        public int calculateIncome(int value) {
+            return expression.apply(value);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
         String input = readInput();
@@ -23,7 +42,7 @@ public class Application {
 
         input = readInput();
         int bonusNum = parseInt(input);
-        if (pickedNumbers.contains(bonusNum) || bonusNum<1 || bonusNum >45){
+        if (pickedNumbers.contains(bonusNum) || bonusNum < 1 || bonusNum > 45) {
             throw new IllegalArgumentException("[Error] Number should be in the range of 1 ~ 45");
         }
 
@@ -31,23 +50,25 @@ public class Application {
 
     }
 
-    
+
+
+   
 
     public static List<Integer> validatePickedNumbers(String input) {
-        String [] numbers = input.split(",");
+        String[] numbers = input.split(",");
         List<Integer> pickedNumbers = new ArrayList<Integer>();
 
-        if (numbers.length != 6){
+        if (numbers.length != 6) {
             throw new IllegalArgumentException("[Error] pick 6 different number between 1 to 45");
         }
 
-        for (int i = 0; i<numbers.length; i++){
+        for (int i = 0; i < numbers.length; i++) {
             int number = parseInt(numbers[i]);
 
-            if (pickedNumbers.contains(number)){
+            if (pickedNumbers.contains(number)) {
                 throw new IllegalArgumentException("[Error] Duplicated number exists");
             }
-            if (number<1 || number>45){
+            if (number < 1 || number > 45) {
                 throw new IllegalArgumentException("[Error] Number should be in the range of 1 ~ 45");
             }
             pickedNumbers.add(number);
@@ -56,15 +77,15 @@ public class Application {
         return pickedNumbers;
     }
 
-    public static void printLottos(List lottos){
+    public static void printLottos(List lottos) {
         StringBuilder lottoNumbers = new StringBuilder();
-        for (int i = 0; i<lottos.size(); i++){
+        for (int i = 0; i < lottos.size(); i++) {
             lottoNumbers.append("[");
             Lotto lottoItem = (Lotto) lottos.get(i);
 
-            for (int j = 0; j<lottoItem.getNumbers().size(); j++){
+            for (int j = 0; j < lottoItem.getNumbers().size(); j++) {
                 int number = lottoItem.getNumbers().get(j);
-                lottoNumbers.append(String.valueOf(number)+", ");
+                lottoNumbers.append(String.valueOf(number) + ", ");
             }
 
             lottoNumbers.append("]\n");
@@ -73,11 +94,11 @@ public class Application {
         System.out.print(lottoNumbers);
     }
 
-    public static List<Lotto> buyLotto(List lottos, int money){
-        int numberOfLottos = (money/1000);
+    public static List<Lotto> buyLotto(List lottos, int money) {
+        int numberOfLottos = (money / 1000);
 
-        for (int i = 0; i<numberOfLottos; i++){
-            List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+        for (int i = 0; i < numberOfLottos; i++) {
+            List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Lotto lottoItem = new Lotto(randomNumbers);
             lottos.add(lottoItem);
         }
@@ -85,27 +106,27 @@ public class Application {
         return lottos;
     }
 
-    public static int validateMoneyInput(String input){
+    public static int validateMoneyInput(String input) {
         int money = parseInt(input);
-        boolean isUnitOf1K = ((money % 1000)==0);
+        boolean isUnitOf1K = ((money % 1000) == 0);
 
-        if (isUnitOf1K==false){
+        if (isUnitOf1K == false) {
             throw new IllegalArgumentException("[Error] Money should end with 000");
         }
 
         return money;
     }
 
-    public static int parseInt(String input){
+    public static int parseInt(String input) {
         try {
             int money = Integer.parseInt(input);
             return money;
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[Error] Only numbers are allowed");
         }
     }
 
-    public static String readInput(){
+    public static String readInput() {
         String input = Console.readLine();
         return input;
     }
