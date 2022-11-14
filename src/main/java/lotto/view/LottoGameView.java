dd7d13f63dc28cbf2efde9d39e5801ae32da3aa1
gomljo <gomljo0307@naver.com>
2022-11-14 13:45:46 +0900

    feat(LottoGameView.java): 추첨 번호 입력받는 기능 구현

--- src/main/java/lotto/view/LottoGameView.java
+++ src/main/java/lotto/view/LottoGameView.java
@@ -1,11 +1,15 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.model.Buyer;
 
-public class LottoGameViewer {
+public class LottoGameView{
 
     public void askPurchaseAmount(){
         String purchaseAmount = Console.readLine();
     }
-
+    
+    public void askRaffleNumbers(){
+        String raffleNumbers= Console.readLine();
+    }
 }
