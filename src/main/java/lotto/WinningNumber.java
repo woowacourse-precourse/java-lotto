76fae00c1f5347e906a88011ebc4ece58030deeb
BoyCho <iijokv@naver.com>
2022-11-15 02:01:28 +0900

    feat : 입력 받은 당첨 번호, 보너스 번호 변환 기능 추가  - 변환 시 숫자가 아닐 경우 에러 처리

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -15,6 +16,14 @@ public class WinningNumber {
         bonusNumber = 0;
     }
 
+    public void createWinningNumbers() {
+        transformInputWinningNumbers(inputWinningNumbers());
+    }
+
+    public void createBonusNumber() {
+        transformBonusNumbers(inputBonusNumber());
+    }
+
     private String inputWinningNumbers() {
         System.out.println(Message.EnterWinningNumberMessage);
         return Console.readLine();
@@ -24,6 +33,29 @@ public class WinningNumber {
         System.out.println(Message.EnterBonusNumberMessage);
         return Console.readLine();
     }
+
+    private void transformInputWinningNumbers(String inputWinningNumbers) {
+        List<String> modifiedInputWinningNumbers = List.of(inputWinningNumbers.split(Message.Comma));
+
+        for (String s : modifiedInputWinningNumbers) {
+            try {
+                int transformedwinningNumber = Integer.parseInt(s);
+                winningNumbers.add(transformedwinningNumber);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(Message.IsNotNumberMessage);
+            }
+        }
+    }
+
+    private void transformBonusNumbers(String inputBonusNumber) {
+        try {
+            int transformedBonusNumber = Integer.parseInt(inputBonusNumber);
+            bonusNumber = transformedBonusNumber;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(Message.IsNotNumberMessage);
+        }
+    }
+
 
     public List<Integer> getWinningNumbers() {
         return winningNumbers;
