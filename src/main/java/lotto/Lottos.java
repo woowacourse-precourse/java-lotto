25b9a09bc13dfc4e5e8d0f4a0f2dcbe0c42083fa
신채원 <cw121@naver.com>
2022-11-14 22:11:57 +0900

    refactor: 사용자 입력 함수 분리

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -6,26 +6,22 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static lotto.InputView.inputPurchase;
-
 public class Lottos {
 
     private static final String OUTPUT_PURCHASE_COUNT_MESSAGE = "개를 구매했습니다.";
 
     private static List<Integer> createSingleRandomLotto() {
-        List<Integer> lotto = new ArrayList<>();
-        lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         Collections.sort(lotto);
         return lotto;
 
     }
 
-    public static List<List<Integer>> createRandomLottos() {
-        int n = inputPurchase() / 1000;
-        System.out.println(n + OUTPUT_PURCHASE_COUNT_MESSAGE);
+    public static List<List<Integer>> createRandomLottos(int purchase) {
+        System.out.println(purchase + OUTPUT_PURCHASE_COUNT_MESSAGE);
         List<List<Integer>> lottos = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < purchase; i++) {
             lottos.add(createSingleRandomLotto());
         }
         return lottos;
