e0572253d4247c5c0e27dca34360f891f6eac1b4
alstjr706 <alstjr706@naver.com>
2022-11-14 12:51:58 +0900

    refactor : (로또 게임) 매니저 클래스 메시지 추가

--- src/main/java/lotto/domain/menu/LottoGameMachine.java
+++ src/main/java/lotto/domain/menu/LottoGameMachine.java
@@ -1,6 +1,7 @@
 package lotto.domain.menu;
 
 import lotto.Lotto;
+import lotto.constants.SystemConsole;
 import lotto.domain.RandomLottoGenerator;
 import lotto.domain.WinningNumberAndLotto;
 import lotto.domain.subaction.WinningNumberPlace;
@@ -15,22 +16,31 @@ public class LottoGameMachine {
     private final LottoTickets lottoTickets;
     private final RandomLottoGenerator randomLottoGenerator;
     private final WinningNumber winningNumber;
+    private final SystemConsole systemConsole;
 
-    public LottoGameMachine(LottoTickets lottoTickets, RandomLottoGenerator randomLottoGenerator, WinningNumber winningNumber) {
+    public LottoGameMachine(LottoTickets lottoTickets, RandomLottoGenerator randomLottoGenerator,
+            WinningNumber winningNumber, SystemConsole systemConsole) {
+
         this.lottoTickets = lottoTickets;
         this.randomLottoGenerator = randomLottoGenerator;
         this.winningNumber = winningNumber;
+        this.systemConsole = systemConsole;
     }
 
     public void run() {
+        systemConsole.GAME_START_MESSAGE();
         int lottos = lottoTickets.getPaid();
-        List<List<Integer>> randomLotto = randomLottoGenerator.createTicket(lottos);
+
+        systemConsole.COMPLETED_PURCHASE(lottos);
+        List<List<Integer>> randomLotto = randomLottoGenerator.createTicket(lottos, systemConsole);
+
+        systemConsole.WINNING_NUMBERS();
         Lotto lottoAnswer = winningNumber.getCorrect();
 
         WinningNumberAndLotto winningNumberAndLotto = new WinningNumberAndLotto(new WinningNumberPlace());
         List<Integer> sameNumberCount = winningNumberAndLotto.comparison(randomLotto, lottoAnswer);
 
-        LottoGameManagement lottoGameManagement = new LottoGameManagement(new AdditionalNumber());
+        LottoGameManagement lottoGameManagement = new LottoGameManagement(new AdditionalNumber(), new SystemConsole());
         lottoGameManagement.bonusConfirmation(randomLotto, lottoAnswer, sameNumberCount);
     }
 }
