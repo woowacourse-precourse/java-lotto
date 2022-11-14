fca180a55227f4e0a02c188c4f57a89f5e067543
jjmin9797 <jjmin9797@naver.com>
2022-11-14 15:29:32 +0900

    feat(GameProgressView.java): 구매한 로또 목록을 보여주는 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/view/GameProgressView.java
+++ src/main/java/lotto/view/GameProgressView.java
@@ -1,5 +1,9 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+
 public class GameProgressView {
     public static final String PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String COUNT_MESSAGE = "개를 구매했습니다.";
@@ -10,5 +14,11 @@ public class GameProgressView {
 
     public static void showCountMessage(int count){
         System.out.println(count+COUNT_MESSAGE);
+    }
+
+    public static void showBuyLottery(ArrayList<Lotto> lottery){
+        for (Lotto lotto : lottery) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }
