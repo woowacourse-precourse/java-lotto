b0569ff2f482445231e2f4c5c868d1e15da43730
taekwon-dev <taekwon_dev@naver.com>
2022-11-14 20:24:12 +0900

    feat: (WinningLotto) 예외 발생과 관련한 메시지 출력 시 ErrorType 활용

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.domain.ErrorType.ERROR_WINNING_LOTTO_CONTAIN_BONUS;
+
 public class WinningLotto {
     private final Lotto winningLotto;
     private final Bonus bonus;
@@ -14,7 +16,7 @@ public class WinningLotto {
 
     private void validateWinningLottoContainsBonusNumber(Lotto winningLotto, Bonus bonus) {
         if (winningLotto.containsBonusNumber(bonus.getNumber())) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_WINNING_LOTTO_CONTAIN_BONUS.getErrorMessage());
         }
     }
 
