4eccf200071cae2ac46dd8f11e6a22f6f9767726
Beginner <dkstmdwn17@naver.com>
2022-11-14 20:30:47 +0900

    feat : 로또 객체에서 유효성을 검사하는 부분의 로직을 추가하였다.
    6자리 숫자가 아닌경우와, 중복된 숫자를 입력했을 경우 IllegalArgumentException을 던진다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -10,11 +10,20 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validate(List<Integer> numbers){
+        if(numbers.size() != 6){
+            throw new IllegalArgumentException("6자리 숫자가 아닙니다.");
+        }
+        if(numbers.stream().distinct().count() != 6){
+            throw new IllegalArgumentException("중복된 숫자를 입력하셨습니다.");
         }
     }
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 
-    // TODO: 추가 기능 구현
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }
