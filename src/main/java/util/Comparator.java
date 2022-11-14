c2ac6858a8ddd9a87f703127d49ca4eb75869ef9
Identityless <rlandwnd555@naver.com>
2022-11-14 21:30:15 +0900

    fix : doIntCompare 인터페이스 반환형 int로 변경

--- src/main/java/util/Comparator.java
+++ src/main/java/util/Comparator.java
@@ -3,5 +3,5 @@ package util;
 import java.util.List;
 
 public interface Comparator {
-    List<Integer> doIntCompare(List<Integer> standard, List<Integer> target);
+    int doIntCompare(List<Integer> standard, List<Integer> target);
 }
