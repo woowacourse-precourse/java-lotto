0819442df58dfffb67c71cceb3eb2e3e788fce0a
hongjinwon <herenever@naver.com>
2022-11-15 03:53:52 +0900

    feat(model): WinningNumbers
    당첨번호와 보너스 번호를 가진 객체인 WinningNumbers 구현

---
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -0,0 +1,28 @@
+package lotto.model;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private final List<Integer> winningNumbers;
+    private final Integer bonusNumber;
+
+    public WinningNumbers(List<Integer> winningNumbers, Integer bonusNumber){
+        validate(winningNumbers,bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public Integer getBonusNumber() {
+        return bonusNumber;
+    }
+    private void validate(List<Integer> winningNumbers,Integer bonusNumber){
+        if (winningNumbers.contains(bonusNumber)){
+            throw new IllegalArgumentException();
+        }
+    }
+}
