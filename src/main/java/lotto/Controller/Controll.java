f78d055e687db61460f03c3175085329bd66714e
aossuper7 <aossuper7@naver.com>
2022-11-14 23:46:26 +0900

    feat: 입력받는 기능

--- src/main/java/lotto/Controller/Controll.java
+++ src/main/java/lotto/Controller/Controll.java
@@ -1,11 +1,14 @@
 package lotto.Controller;
 
 import lotto.Constant.Message;
+
+import lotto.View.InputView;
 import lotto.View.OutputView;
 
 public class Controll {
 
     private OutputView outputView = new OutputView();
+    private InputView inputView = new InputView();
 
     public void run() {
         try {
@@ -17,6 +20,6 @@ public class Controll {
 
     private void buyTicket() {
         outputView.printMessage(Message.START.getMessage());
-
+        inputView.inputNum();
     }
 }
