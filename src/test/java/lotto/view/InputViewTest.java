956a53d4f2135e5fbf62538cf43734da16337f03
slbin-park <smpts00@naver.com>
2022-11-14 21:22:03 +0900

    test(InputViewTest) : 당첨번호 입력 테스트 ValidatorTest에서 이동

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -42,6 +42,42 @@ public class InputViewTest {
     }
 
     @Test
+    void 당첨번호_입력_정수_배열로_변환() {
+        String inputNumber = "1,2,3,4,5,6";
+        List answerList = List.of(1, 2, 3, 4, 5, 6);
+        System.setIn(new ByteArrayInputStream(inputNumber.getBytes()));
+        List list = inputView.inputWinningNumber();
+        Assertions.assertEquals(answerList, list);
+    }
+
+    @Test
+    void 문자열을_입력시_에러() {
+        String inputNumbers = "a,2,3,4,5,6";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            System.setIn(new ByteArrayInputStream(inputNumbers.getBytes()));
+            inputView.inputWinningNumber();
+        });
+    }
+
+    @Test
+    void 숫자개수가_작을경우_에러() {
+        String inputNumbers = "1,2,3,4,5,";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            System.setIn(new ByteArrayInputStream(inputNumbers.getBytes()));
+            inputView.inputWinningNumber();
+        });
+    }
+
+    @Test
+    void 중복된_숫자가_있을경우_에러() {
+        String inputNumbers = "1,2,3,4,5,5";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            System.setIn(new ByteArrayInputStream(inputNumbers.getBytes()));
+            inputView.inputWinningNumber();
+        });
+    }
+
+    @Test
     void 보너스번호_정상_입력() {
         String inputNumber = "10";
         int answer = 10;
