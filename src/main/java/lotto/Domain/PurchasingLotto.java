9bd5ef64636074c6c7ea3abfb235b546e881d97e
Sieun Park <psieun0606@gmail.com>
2022-11-15 04:31:41 +0900

    bug: 생성자 매개변수 수정

--- src/main/java/lotto/Domain/PurchasingLotto.java
+++ src/main/java/lotto/Domain/PurchasingLotto.java
@@ -2,10 +2,11 @@ package lotto.Domain;
 
 public class PurchasingLotto {
 
+    private int purchase;
     private int amountOfLotto;
 
-    public PurchasingLotto(int amountOfLotto) {
-        this.amountOfLotto = amountOfLotto;
+    public PurchasingLotto(int purchase) {
+        this.purchase = purchase;
     }
 
     public int calculateNumberOfLotto(int purchasingAmount) {
