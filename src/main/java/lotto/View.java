5cb3ddd605b8547a4e87b1ee94e734953252f255
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-14 14:30:15 +0900

    feat(View): 입력값이 숫자인지 확인하는 validateIsNumber 메소드 구현

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -5,7 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class View {
     private void validateIsDivisible(int money) {
         if (money % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000으로 나눠떨어져야 한다");
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000으로 나눠떨어져야 합니다");
+        }
+    }
+    private void validateIsNumber(String input) {
+        String[] splitInput = input.split(",");
+        for (String value : splitInput) {
+            try {
+                int numbers = Integer.parseInt(value);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("[ERROR] 숫자만 입력이 가능합니다");
+            }
         }
     }
 }
