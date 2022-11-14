ef26599fd2414eed3b64fb5cf75a0b121b35d74a
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-14 22:16:37 +0900

    style: 가독성을 위해 공백추가

--- src/test/java/lotto/service/DrawLottoServiceTest.java
+++ src/test/java/lotto/service/DrawLottoServiceTest.java
@@ -50,18 +50,23 @@ public class DrawLottoServiceTest {
     }
 
     @DisplayName("나의 로또와 당첨 로또를 비교하여 잘 저장되는지 확인한다.")
-    @ParameterizedTest(name = "[{index}]등 {8}")
+    @ParameterizedTest(name = "{index}등 {8}")
     @CsvSource({"1,2,3,4,5,6,7,4,6개 일치", "1,2,3,4,5,45,6,3,5개일치+보너스일치", "1,2,3,4,5,9,5,2,5개일치", "1,2,3,4,8,9,4,1,4개일치",
             "1,2,3,7,8,9,3,0,3개일치"})
     void compareLottoToWinLottoTest(int num1, int num2, int num3, int num4, int num5, int num6, int rankCount,
                                     int rank, String message) {
+        //given
         Lotto myLotto = new Lotto(List.of(num1, num2, num3, num4, num5, num6));
         List<Integer> winLotto = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumber = 45;
         Map<Integer, Integer> winingCount = new HashMap<>();
         winingCount.put(rankCount, 1);
+
+        //when
         drawLottoService.compareLottoToWiningLotto(winLotto, myLotto, bonusNumber);
         drawLottoService.setWiningCount();
+
+        //then
         assertThat(winingCount.get(rankCount)).isEqualTo(drawLottoService.getWiningCount().get(rank));
     }
 }
