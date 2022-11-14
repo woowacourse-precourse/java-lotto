6f8de65b9843a46c0d5f457a29126e40090d59fa
limecats0331 <limecats0331@gmail.com>
2022-11-14 11:35:57 +0900

    test(Manager): 일차하는 갯수 결과를 리턴하는 메서드 테스트
    테스트 케이스
    - 모든 갯수에 대해서 잘 들어가는지
    - 출력 범위가 원하는 범위 내에 잘 들어 오는지

--- src/test/java/lotto/ManagerTest.java
+++ src/test/java/lotto/ManagerTest.java
@@ -49,4 +49,53 @@ class ManagerTest {
                     assertThat(lotto.size()).isEqualTo(6);
                 });
     }
+
+    @DisplayName("1~6개가 일치하는 입력의 경우")
+    @Test
+    void matchEachCount() throws Exception {
+        //Given
+        Manager manager = new Manager();
+        manager.getPurchasePrice(6000);
+
+        //When
+        List<Integer> lotto = List.of(1, 2, 3, 4, 5, 6, 7);
+        List<List<Integer>> purchased = List.of(
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(1, 2, 3, 4, 5, 8),
+                List.of(1, 2, 3, 4, 8, 9),
+                List.of(1, 2, 3, 8, 9, 10),
+                List.of(1, 2, 8, 9, 10, 11),
+                List.of(1, 8, 9, 10, 11, 12),
+                List.of(8, 9, 10, 11, 12, 13)
+        );
+
+        int[] matchCount = manager.findMatchCount(lotto, purchased);
+
+        //Then
+        assertThat(matchCount).isEqualTo(new int[]{1, 1, 1, 1});
+    }
+
+    @DisplayName("3개 일치가 3 6개 일치가 3")
+    @Test
+    void matchDifferently() throws Exception {
+        //Given
+        Manager manager = new Manager();
+        manager.getPurchasePrice(6000);
+
+        //When
+        List<Integer> lotto = List.of(1, 2, 3, 4, 5, 6, 7);
+        List<List<Integer>> purchased = List.of(
+                List.of(1, 2, 3, 8, 9, 10),
+                List.of(1, 2, 8, 9, 10, 6),
+                List.of(1, 8, 9, 10, 5, 6),
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(1, 7, 3, 4, 5, 6),
+                List.of(7, 2, 3, 4, 5, 6)
+        );
+
+        int[] matchCount = manager.findMatchCount(lotto, purchased);
+
+        //Then
+        assertThat(matchCount).isEqualTo(new int[]{3, 0, 0, 3});
+    }
 }
