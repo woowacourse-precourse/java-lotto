9a887ec3d919cc0319d955fbcacad267a4d71ef1
lvalentine6 <ysr1227@naver.com>
2022-11-15 06:09:40 +0900

    feat : Application main 메서드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,27 @@
 package lotto;
 
-public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+import java.util.List;
 
-        int PurchaseAmount = Input.userInput();
-        System.out.println("메인 끝");
+public class Application {
+    static final int DIVIDE_NUMBER = 1000;
+
+    public static void main(String[] args) {
+        try {
+            int purchaseAmount = Input.userInput();
+            int purchaseQuantity = purchaseAmount / DIVIDE_NUMBER;
+            List<Lotto> lottoBundle = Generator.creatLottoBundle(purchaseQuantity);
+            Output.printLottoBundle(lottoBundle, purchaseQuantity);
+            List<Integer> winningNumber = Generator.creatWinningNumber(Input.winningNumberInput());
+            int bonusNumber = Input.bonusNumberInput();
+            InputException.isValidBonusNumberOverlap(winningNumber, bonusNumber);
+            winningNumber = Generator.addBonusNumber(winningNumber, bonusNumber);
+            Compare.sliceBundle(lottoBundle, winningNumber);
+            Output.printPreResult();
+            Calculator.addPrize();
+            Output.printResult();
+            Output.printRate(Calculator.rateOfReturn(purchaseAmount));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
