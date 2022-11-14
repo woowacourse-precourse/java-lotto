9f539a801ce31156cceed53e91ad3c5b6887c7f7
aksmf1442 <aksmf1442@naver.com>
2022-11-14 15:50:38 +0900

    test(로또): 로또 객체가 가진 숫자 6개를 오름차순하는 기능 테스트 구현

--- src/test/java/lotto/domain/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/lotto/LottoGeneratorTest.java
@@ -2,6 +2,8 @@ package lotto.domain.lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +14,14 @@ public class LottoGeneratorTest {
     public void createLottoWithRandomNumbers() {
         Lotto lotto = LottoGenerator.createLottoWithRandomNumbers();
         assertThat(lotto.getNumbers().size()).isEqualTo(6);
+    }
+
+    @DisplayName("[성공] 랜덤한 로또 번호 6개를 생성한 로또 객체 만들기 - 정렬 확인")
+    @Test
+    public void createLottoWithSortedRandomNumbers() {
+        Lotto lotto = LottoGenerator.createLottoWithRandomNumbers();
+        List<Integer> originNumbers = lotto.getNumbers();
+        List<Integer> sortedNumbers = lotto.getNumbers().stream().sorted().collect(Collectors.toList());
+        assertThat(originNumbers).isEqualTo(sortedNumbers);
     }
 }
