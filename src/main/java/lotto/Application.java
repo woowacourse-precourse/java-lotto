f688fb2a4ce071a7405948df7f8f31e88e02bcbe
이건회 <rg970604@naver.com>
2022-11-15 08:07:24 +0900

    refactor(Application.java) : 메인 애플리케이션에 게임 실행 메소드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,35 +1,10 @@
 package lotto;
 
-import java.util.Map;
-import lotto.domain.Lotto;
-import lotto.domain.LottoMachine;
-import lotto.domain.LottoPieces;
-import lotto.domain.Lottos;
-import lotto.domain.Prize;
-import lotto.domain.ProfitCalculator;
-import lotto.strategy.LottoGenerateStrategy;
-import lotto.strategy.RandomLottoGenerateStrategy;
-import lotto.view.PrintResult;
-import lotto.view.UserInput;
+import lotto.game.GameStartOrExit;
 
 public class Application {
 
     public static void main(String[] args) {
-        int money = UserInput.InputMoney();
-        LottoPieces lottoPieces = new LottoPieces(money);
-        int pieces = lottoPieces.pieces;
-        PrintResult.PrintPieces(pieces);
-
-        LottoGenerateStrategy lottoGenerateStrategy = new RandomLottoGenerateStrategy();
-        Lottos lottos = new Lottos(pieces, lottoGenerateStrategy);
-        PrintResult.PrintLottos(lottos);
-
-        Lotto winningLotto = new Lotto(UserInput.InputWinningNums());
-        int bonusNum = UserInput.InputBonusNum();
-
-        Map<Prize,Integer> prizeBoard = LottoMachine.checkPrizes(winningLotto,lottos,bonusNum);
-        PrintResult.PrintPrizeStatistic(prizeBoard);
-        float profit = ProfitCalculator.calculateProfitRate(prizeBoard,money);
-        PrintResult.PrintProfitRate(profit);
+        GameStartOrExit.gameStart();
     }
 }
