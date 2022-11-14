cc0b565c361c49b6a6837803e8bc876a730eb36d
This2sho <zx00018@naver.com>
2022-11-14 23:01:45 +0900

    test(Lotto): 로또에 숫자 하나가 포함되는지 확인하는 기능 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -28,5 +31,16 @@ class LottoTest {
     void createLottoByInvalidNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
             .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "{index} : 로또 번호에 {0}가 있는지 확인 (결과 : {1})")
+    @CsvSource({"1, true", "6, true", "7, false", "45, false"})
+    void 로또_번호_포함_테스트(int number, boolean expected) {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        //when
+        boolean actual = lotto.isMatch(number);
+        //then
+        assertThat(actual).isEqualTo(expected);
     }
 }
