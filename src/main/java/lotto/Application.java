0092049bd82aed400a4b32504af909f087ac2e79
kylew1004 <kylew1004@gmail.com>
2022-11-14 17:16:55 +0900

    refactor: main에 수익률계산 및 순위 클래스 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,22 +1,46 @@
 package lotto;
 
 import view.UserInterface;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+
     public static void main(String[] args) {
         try {
             int payment = UserInterface.readPayment();
             List<Lotto> boughtLotteries = buyLotteries(payment / 1000);
             UserInterface.printBoughtLotteries(boughtLotteries);
 
-            LottoCheck lottoRound = makeLottoRound();
-            LottoResult statistics = makeStatistics(boughtLotteries, lottoRound);
+            LottoCheck lottoCheck = makeLottoRound();
+            LottoResult statistics = makeStatistics(boughtLotteries, lottoCheck);
 
             UserInterface.printStatistics(statistics);
         } catch (IllegalArgumentException e) {
             UserInterface.printError(e);
         }
+    }
+
+    private static List<Lotto> buyLotteries(int num) {
+        List<Lotto> lotteries = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            lotteries.add(Lotto.createRandomLotto());
+        }
+        return lotteries;
+    }
+
+    private static LottoCheck makeLottoRound() throws IllegalArgumentException {
+        Lotto luckyNumbers = UserInterface.readLuckyNumbers();
+        int bonusNumber = UserInterface.readBonusNumber();
+        return new LottoCheck(luckyNumbers, bonusNumber);
+    }
+
+    private static LottoResult makeStatistics(List<Lotto> boughtLotteries, LottoCheck lottoCheck) {
+        LottoResult statistics = new LottoResult();
+        for (Lotto lotto : boughtLotteries) {
+            statistics.add(lottoCheck.getRank(lotto));
+        }
+        return statistics;
     }
 }
