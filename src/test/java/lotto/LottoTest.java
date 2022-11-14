589294257d78b8040d9a9f7d835e0bd7137a9540
famo1245 <kandallee38@gmail.com>
2022-11-14 17:55:38 +0900

    예외 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,16 @@
 package lotto;
 
 import org.junit.jupiter.api.DisplayName;
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class LottoTest {
+class LottoTest extends NsTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -23,5 +26,29 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 정렬이 안되어 있다")
+    @Test
+    void unsortedNumber() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("1000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "[8, 21, 23, 41, 42, 43]"
+                    );
+                },
+                List.of(21, 8, 23, 41, 42, 43)
+        );
+    }
+
+    @DisplayName("입력 금액이 정수가 아니면 에러가 발생한다")
+    @Test
+    void inputNotIntegerMoney() {
+        assertThatThrownBy(() -> run("1000j"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
 }
