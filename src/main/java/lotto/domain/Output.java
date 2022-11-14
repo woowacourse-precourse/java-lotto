d76f5872779ceda309920d987c4ae7c63303bb3a
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-15 03:25:46 +0900

    feat: 출력문 기능 구현

--- src/main/java/lotto/domain/Output.java
+++ src/main/java/lotto/domain/Output.java
@@ -11,4 +11,16 @@ public class Output {
         }
         return countOfPrize;
     }
+    public void writeDown(double earningRate){
+        Calculator calculator = new Calculator();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - "+countOfPrize.getOrDefault(3,0)+"개");
+        System.out.println("4개 일치 (50,000원) - "+countOfPrize.getOrDefault(4,0)+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+countOfPrize.getOrDefault(5,0)+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+countOfPrize.getOrDefault(7,0)+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+countOfPrize.getOrDefault(6,0)+"개");
+        System.out.println("총 수익률은 "+earningRate+"%입니다.");
+
+    }
 }
