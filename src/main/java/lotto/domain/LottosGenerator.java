c07c312d9ef494c87898f7d644c2cbe66962e6be
twoosky <lhjae3@naver.com>
2022-11-15 02:51:23 +0900

    feat(domain): 로또 생성 객체 및 로또 번호 생성 메서드 구현

---
+++ src/main/java/lotto/domain/LottosGenerator.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import lotto.utils.RandomNumbersGenerator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottosGenerator {
+    private final List<Lotto> lottos;
+
+    public LottosGenerator(LottoCount lottoCount) {
+        this.lottos = generate(lottoCount.getCount());
+    }
+
+    private List<Lotto> generate(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i = 0; i < lottoCount; i++) {
+            List<Integer> numbers = generateLottoNumbers();
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
+
+    private List<Integer> generateLottoNumbers() {
+        List<Integer> numbers = RandomNumbersGenerator.generate();
+        sortNumbers(numbers);
+        return numbers;
+    }
+
+    private void sortNumbers(List<Integer> numbers) {
+        Collections.sort(numbers);
+    }
+}
