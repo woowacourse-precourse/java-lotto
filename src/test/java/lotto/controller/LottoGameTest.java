03e7d881b6fb77884b4739d2ae6096078fa44f4e
yujaemin <woawoa0513@gmail.com>
2022-11-14 21:37:54 +0900

    test: 구매 금액에 맞는 개수의 로또 생성하는지 확인
    7,000원의 로또를 구매하면 총 7장의 로또를 생성하는지 확인한다.

---
+++ src/test/java/lotto/controller/LottoGameTest.java
@@ -0,0 +1,21 @@
+package lotto.controller;
+
+import static lotto.utils.Constants.LOTTO_PRICE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lotto.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class LottoGameTest {
+    @DisplayName("구매 금액 1,000원 당 한 장씩의 로또를 생성한다.")
+    @Test
+    void 구매금액에_맞는_개수의_로또_생성() {
+        List<Lotto> lottos = LottoGame.generateLottoByPurchaseAmount(LOTTO_PRICE * 7);
+        int lottoCount = lottos.size();
+
+        assertThat(lottoCount).isEqualTo(7);
+    }
+}
