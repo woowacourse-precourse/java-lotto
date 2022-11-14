e9e9870156c97ce6751b3285318a2a87facc88a5
dhkim <akdlzmf1123@gmail.com>
2022-11-14 19:15:37 +0900

    refactor(LottoTest): 테스트를 더 이해하기 쉽도록 변수, 테스트 이름 변경

--- src/test/java/lotto/model/LottoPrizeTest.java
+++ src/test/java/lotto/model/LottoPrizeTest.java
@@ -10,20 +10,19 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoPrizeTest {
-    @ParameterizedTest(name = "로또 {1} Place 당첨")
+    @ParameterizedTest(name = "비교 결과 로또 {1}등 당첨")
     @MethodSource("lottoResultAndPrize")
-    void CompareResult가_몇등_당첨인지_판단하는_기능(CompareResult lottoResult, LottoPrize lottoPrize) {
-        assertThat(LottoPrize.toPrize(lottoResult)).isEqualTo(lottoPrize);
+    void CompareResult가_몇_등_당첨인지_판단하는_기능(CompareResult lottoResult, LottoPrize expected) {
+        assertThat(LottoPrize.toPrize(lottoResult)).isEqualTo(expected);
     }
 
-    @ParameterizedTest(name = "{0} 당첨 상금 {1}원")
-    @CsvSource({"FIRST,2_000_000_000", "SECOND,30_000_000", "THIRD,1_500_000", "FOURTH,50_000", "FIFTH,5_000"})
-    void 당첨_상금_반환_기능(String prize, int moneyAmount) {
-        assertThat(LottoPrize.valueOf(prize).getWinningAmount()).isEqualTo(moneyAmount);
+    @ParameterizedTest(name = "{0}등인 경우 당첨 상금 {1}원")
+    @CsvSource({"FIRST,2_000_000_000", "SECOND,30_000_000", "THIRD,1_500_000", "FOURTH,50_000", "FIFTH,5_000", "LOSE,0"})
+    void 당첨_상금_반환_기능(LottoPrize prize, int expected) {
+        assertThat(prize.getWinningAmount()).isEqualTo(expected);
     }
 
     static Stream<Arguments> lottoResultAndPrize() {
-
         return Stream.of(
                 Arguments.of(new CompareResult(6, false), LottoPrize.FIRST)
                 , Arguments.of(new CompareResult(5, true), LottoPrize.SECOND)
