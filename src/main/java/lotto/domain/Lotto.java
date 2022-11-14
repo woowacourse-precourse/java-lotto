8454e67ffceb3ffd61d59425763d7d8ce9afcd3a
parkjungyoon <tbr06057@naver.com>
2022-11-15 02:49:11 +0900

    refactor(Domain): validation 코드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,17 +2,21 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.validator.NumbersValidator.*;
+
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicateNumber(numbers);
+        validateRangeNumbers(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개입니다.");
         }
     }
 
