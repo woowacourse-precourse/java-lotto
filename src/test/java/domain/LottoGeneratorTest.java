6f150b520c9b6dcc7c0d6d61ccb509693aced2e4
김선동 <mok03127@gmail.com>
2022-11-15 02:34:20 +0900

    test: 로또 생성 테스트

---
+++ src/test/java/domain/LottoGeneratorTest.java
@@ -0,0 +1,31 @@
+package domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoGeneratorTest {
+
+    private static LottoGenerator lottoGenerator;
+
+    @BeforeEach
+    void beforeEach() {
+        lottoGenerator = new LottoGenerator();
+    }
+
+    @DisplayName("생성할 로또의 수와 생성된 로또의 수 비교")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1, 1",
+            "8, 8"
+    })
+    void checkNumberOfLottos(int numberOfLottoTickets, int lottoSize) {
+        assertThat(lottoGenerator.generateLottoTickets(numberOfLottoTickets).getLottoTickets().size()).isEqualTo(lottoSize);
+    }
+}
