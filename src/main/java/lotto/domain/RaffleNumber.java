589ad56758c4482cb6fead94dcf7478e35695499
Yeongseon Park <yseon406@gmail.com>
2022-11-15 06:04:03 +0900

    feat(lotto): 당첨 번호 유효성 검사 기능 추가

--- src/main/java/lotto/domain/RaffleNumber.java
+++ src/main/java/lotto/domain/RaffleNumber.java
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.ui.Message;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RaffleNumber {
 
@@ -17,9 +19,32 @@ public class RaffleNumber {
     public List<Integer> setWinningNumber(String userInput) {
         List<Integer> winningNumber = new ArrayList<>();
         for (String number : userInput.split(",")) {
+            validateRange(number);
             winningNumber.add(Integer.valueOf(number));
         }
+        hasSameNumber(winningNumber);
+        validateSize(winningNumber);
         return winningNumber;
+    }
+
+    private void validateRange(String number) {
+        int parseNumber = Integer.parseInt(number);
+        if (1 > parseNumber || parseNumber > 45) {
+            throw new IllegalArgumentException(Message.WINNING_RANGE_ERROR);
+        }
+    }
+
+    private void hasSameNumber(List<Integer> numbers) {
+        Set<Integer> sameNumber = new HashSet<>(numbers);
+        if (sameNumber.size() < 6) {
+            throw new IllegalArgumentException(Message.WINNING_OVERLAP_ERROR);
+        }
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(Message.WINNING_SIZE_ERROR);
+        }
     }
 
     public int inputBonusNumber() {
--- src/main/java/lotto/ui/Message.java
+++ src/main/java/lotto/ui/Message.java
@@ -5,4 +5,7 @@ public class Message {
     public static final String INPUT_WINNING = "당첨 번호를 입력해 주세요";
     public static final String INPUT_BONUS = "보너스 번호를 입력해 주세요.";
     public static final String MONEY_ERROR = "[ERROR] 로또 구입은 1,000원 단위만 가능합니다.";
+    public static final String WINNING_RANGE_ERROR = "[ERROR] 당첨 번호의 범위는 1~45입니다.";
+    public static final String WINNING_OVERLAP_ERROR = "[ERROR] 당첨 번호는 중복될 수 없습니다.";
+    public static final String WINNING_SIZE_ERROR = "[ERROR] 당첨 번호는 6개입니다.";
 }
