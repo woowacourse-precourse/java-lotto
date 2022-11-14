7fa41b632d6ec4850e7f70b9955b0389773f0dc3
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-11-14 23:09:37 +0900

    test: add lotto amount test
    - 로또 구매에 대한 테스트 추가
    - 1000으로 나눠 떨어지지 않는 경우에 대한 테스트 추가
    - 구매 성공에 대한 테스트 추가

---
+++ src/main/java/lotto/logic/LottoAmount.java
@@ -0,0 +1,9 @@
+package lotto.logic;
+
+public class LottoAmount {
+    public static final Integer LOTTO_PRICE = 1000;
+
+    public static Integer getLottoAmount(Long money) {
+        return null;
+    }
+}
