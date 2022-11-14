7ef9feca1014976191b2b533cbe0212e7b9ea76d
Jae Ung Jung <roosa0210@gmail.com>
2022-11-14 16:23:47 +0900

    feat(Lotto.validate): 예외처리 항목추가
    중복된 숫자가 입력된 경우와 범위를 벗어나는 숫자를 입력한 경우 예외처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,7 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,7 +13,17 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        Set<Integer> numbersSet = new HashSet<>(numbers);
         if (numbers.size() != 6) {
+            System.out.println("[Error]당첨번호는 6자리이여야 합니다.");
+            throw new IllegalArgumentException();
+        }
+        if (numbers.size() != numbersSet.size()){
+            System.out.println("[Error]중복되지 않은 숫자를 입력해야 합니다.");
+            throw new IllegalArgumentException();
+        }
+        if(Collections.min(numbers) < 1 || Collections.max(numbers) >45 ) {
+            System.out.println("[Error]1~45 범위의 값을 입력해야 합니다.");
             throw new IllegalArgumentException();
         }
     }
