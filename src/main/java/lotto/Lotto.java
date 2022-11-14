059bb092ca5f9fbdb9ed171ce49e684e45f2a744
JeongHunHui <deca3248@gmail.com>
2022-11-14 23:50:39 +0900

    refactor: LottoValidator에 있는 상수를 Lotto로 이동

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,6 +5,9 @@ import java.util.List;
 import static lotto.LottoValidator.*;
 
 public class Lotto {
+  public static final int NUMBER_COUNT = 6;
+  public static final int MIN_NUMBER = 1;
+  public static final int MAX_NUMBER = 45;
   private final List<Integer> numbers;
 
   public Lotto(List<Integer> numbers) {
--- src/main/java/lotto/LottoValidator.java
+++ src/main/java/lotto/LottoValidator.java
@@ -2,38 +2,37 @@ package lotto;
 
 import java.util.List;
 
+import static lotto.Lotto.*;
+
 public class LottoValidator {
-    private static final int NUMBER_COUNT = 6;
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
 
-    public static void validate(List<Integer> numbers) {
-        checkNumbersCount(numbers);
-        checkHasDuplicateNumber(numbers);
-        checkIsNumbersInRange(numbers);
-    }
+  public static void validate(List<Integer> numbers) {
+    checkNumbersCount(numbers);
+    checkHasDuplicateNumber(numbers);
+    checkIsNumbersInRange(numbers);
+  }
 
-    private static void checkNumbersCount(List<Integer> numbers) {
-        if (numbers.size() != NUMBER_COUNT) {
-            throw new IllegalArgumentException("숫자의 개수가 잘못되었습니다!");
-        }
+  private static void checkNumbersCount(List<Integer> numbers) {
+    if (numbers.size() != NUMBER_COUNT) {
+      throw new IllegalArgumentException("숫자의 개수가 잘못되었습니다!");
     }
+  }
 
-    private static void checkHasDuplicateNumber(List<Integer> numbers) {
-        if(numbers.stream().distinct().count() != numbers.size()) {
-            throw new IllegalArgumentException("중복되는 숫자가 있습니다!");
-        }
+  private static void checkHasDuplicateNumber(List<Integer> numbers) {
+    if (numbers.stream().distinct().count() != numbers.size()) {
+      throw new IllegalArgumentException("중복되는 숫자가 있습니다!");
     }
+  }
 
-    private static void checkIsNumbersInRange(List<Integer> numbers) {
-        for (int number : numbers) {
-            if(!isInRange(number)){
-                throw new IllegalArgumentException("범위를 벗어나는 숫자가 있습니다!");
-            }
-        }
+  private static void checkIsNumbersInRange(List<Integer> numbers) {
+    for (int number : numbers) {
+      if (!isInRange(number)) {
+        throw new IllegalArgumentException("범위를 벗어나는 숫자가 있습니다!");
+      }
     }
+  }
 
-    private static boolean isInRange(int number){
-        return number >= MIN_NUMBER && number <= MAX_NUMBER;
-    }
+  private static boolean isInRange(int number) {
+    return number >= MIN_NUMBER && number <= MAX_NUMBER;
+  }
 }
