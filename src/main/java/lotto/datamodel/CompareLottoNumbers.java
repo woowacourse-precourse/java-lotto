af8c7d18c3ef82e2c3eaba386565e622348a0c9a
sora5421 <21800804@handong.edu>
2022-11-14 23:00:50 +0900

    feat : 로또 숫자들과 당첨번호를 비교하여 일치하는 개수 생성

--- src/main/java/lotto/datamodel/CompareLottoNumbers.java
+++ src/main/java/lotto/datamodel/CompareLottoNumbers.java
@@ -1,4 +1,57 @@
 package lotto.datamodel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CompareLottoNumbers {
+
+    private final List<Integer> numbers;
+    private final int bonusNumber;
+    public CompareLottoNumbers(List<Integer> numbers, int bonusNumber) {
+        this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> calculator(int lottoCount, List<List<Integer>> lottoNumbers) {
+        Rank score;
+        List<Integer> price = new ArrayList<>();
+        for(int i = 0; i < lottoCount; i++){
+            score = checkSameNumber(lottoNumbers.get(i));
+            if(score != Rank.NONE){
+                price.add(score.getAmount());
+            }
+        }
+        return price;
+    }
+
+    public Rank checkSameNumber(List<Integer> oneNumbers) {
+        int count = 0;
+        boolean bonusValid = false;
+
+        for(int i = 0; i < 6; i ++){
+            if(oneNumbers.contains(numbers.get(i))){
+                count++;
+            }
+            if(oneNumbers.contains(bonusNumber)){
+                bonusValid = true;
+            }
+        }
+
+        if(count == 6) {
+            return Rank.FIRST;
+        }
+        if(count == 5 && bonusValid == true) {
+            return Rank.SECOND;
+        }
+        if(count == 5) {
+            return Rank.THIRD;
+        }
+        if(count == 4){
+            return Rank.FOURTH;
+        }
+        if(count == 3){
+            return Rank.FIFTH;
+        }
+        return Rank.NONE;
+    }
 }
