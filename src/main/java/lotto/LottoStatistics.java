1b03b00fff106dcc03149305eed9e4320427ae81
iamsseaa <homingsea@gmail.com>
2022-11-14 18:37:13 +0900

    feat: 총 로또 당첨 금액 구하는 기능

--- src/main/java/lotto/LottoStatistics.java
+++ src/main/java/lotto/LottoStatistics.java
@@ -6,7 +6,29 @@ import java.util.Map;
 
 public class LottoStatistics {
 
+    private int winningMoney = 0;
+    private Map<String, Integer> countMatchingMoney = new HashMap<>();
 
+    LottoStatistics() {
+        countMatchingMoney.put("0", 0);
+        countMatchingMoney.put("1", 0);
+        countMatchingMoney.put("2", 0);
+        countMatchingMoney.put("3", 5000);
+        countMatchingMoney.put("4", 50000);
+        countMatchingMoney.put("5", 1500000);
+        countMatchingMoney.put("bonus", 30000000);
+        countMatchingMoney.put("6", 2000000000);
+    }
+
+    public void countWinningMoney(List<Lotto> lottos, Lotto winningLotto, int bonus) {
+        for (Lotto lotto : lottos) {
+            String key = Integer.toString(lotto.countMatchingNumber(winningLotto.getNumbers()));
+            if (key.equals("5")) {
+                key = checkBonusNumber(lotto, bonus, key);
+            }
+            winningMoney += countMatchingMoney.get(key);
+        }
+    }
 
     public String checkBonusNumber(Lotto lotto, int bonus, String key) {
         String newKey = key;
