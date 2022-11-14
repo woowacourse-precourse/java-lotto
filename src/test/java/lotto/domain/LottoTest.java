b2afe238be8c71b5ec0148ad62388ef5ab531cd2
yoon <beatus96@naver.com>
2022-11-14 18:25:51 +0900

    test(Lotto) : 일치하는 번호 개수 세기 기능 테스트 추가
    로또 클래스 테스트 추가
    1. 로또 번호 리스트와 일치하는 번호 개수 세기 테스트
    - @ParameterizedTest
    - @MethodSource("provideWinningNumbersAndExpected")
    
    사용해서,
    Stream<Arguments>를 매개변수로 제공하면,
    Stream<Arguments> 순회하면서 각 Arguments 요소를 인자로 사용한다.

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class LottoTest {
+    private static final Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+
+    @DisplayName("제공된 로또번호와 일치하는 번호 개수 세기 기능 테스트")
+    @ParameterizedTest
+    @MethodSource("provideWinningNumbersAndExpected")
+    void calculateMatchCountTest(List<Integer> winningNumbers,int expected) {
+        assertThat(lotto.calculateMatchCount(winningNumbers))
+            .isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideWinningNumbersAndExpected() {
+        return Stream.of(
+            Arguments.of(List.of(1,2,3,4,5,6) , 6),
+            Arguments.of(List.of(1,26,3,45,15,6) , 3),
+            Arguments.of(List.of(13,21,32,24,15,6) , 1),
+            Arguments.of(List.of(1,2,3,4,5,11) , 5)
+        );
+    }
+
+
+}
