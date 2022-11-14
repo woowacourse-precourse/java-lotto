62f9f4f035010acfc86fee007e2f718dad73bc17
dvlpr0 <rladjswkd@naver.com>
2022-11-14 19:07:11 +0900

    test: 로또 구입 금액 유효성 검사에 대한 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,6 +3,8 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -24,4 +26,15 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("입력된 구매 금액 배수인 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void readPurchaseAmountTest() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        String input = "12345";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+
+        System.setIn(in);
+        assertThatThrownBy(lotto::readPurchaseAmount)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
