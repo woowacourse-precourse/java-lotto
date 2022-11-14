170ac52079767b4187ff49d242febb31549208bb
recicoco068 <gkapf838@gmail.com>
2022-11-15 01:26:42 +0900

    feat(UserInterface.java) : 당첨 통계를 출력하는 기능 구현

--- src/main/java/lotto/UserInterface.java
+++ src/main/java/lotto/UserInterface.java
@@ -1,8 +1,11 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class UserInterface {
@@ -10,6 +13,31 @@ public class UserInterface {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.toStringLotto());
         }
+    }
+
+    public void printResult(Map<WinningResult, Integer> statistics, double revenue) {
+        List<WinningResult> winningResults = Arrays.asList(WinningResult.values());
+        Collections.reverse(winningResults);
+        System.out.print("당첨 통계\n---");
+        for (WinningResult wr : winningResults) {
+            printStatistics(wr, wr.getMatchCount(), wr.getReward(), statistics.getOrDefault(wr, 0));
+        }
+        printRevenueRate(revenue);
+    }
+
+    private void printStatistics(WinningResult wr, int matchCount, int reward, int winningCount) {
+        if (wr.equals(WinningResult.LOSER)) {
+            return;
+        }
+        if (wr.equals(WinningResult.SECOND)) {
+            System.out.printf("\n%d개 일치, 보너스 볼 일치 (%,d원) - %d개", matchCount, reward, winningCount);
+            return;
+        }
+        System.out.printf("\n%d개 일치 (%,d원) - %d개", matchCount, reward, winningCount);
+    }
+
+    private void printRevenueRate(double revenue) {
+        System.out.printf("\n총 수익률은 %.1f%%입니다.\n", revenue * 100);
     }
 
     public int inputOnlyNumber() {
