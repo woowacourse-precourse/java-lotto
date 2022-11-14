7e0619af79198c7a4e119655ec1a0759fb12f1d4
can019 <jys01012@gmail.com>
2022-11-14 14:55:16 +0900

    feat(model): string list to int list implementation
    success test and failure test passed

--- src/main/java/lotto/LottoUtil.java
+++ src/main/java/lotto/LottoUtil.java
@@ -1,12 +1,23 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoUtil {
 
     public int StringToInt(String input) {
-        if(!isNumeric(input))
+        if (!isNumeric(input))
             throw new IllegalArgumentException("[ERROR] 입력이 숫자가 아닙니다.");
 
         return Integer.parseInt(input);
+    }
+
+    public List<Integer> StringListToIntList(List<String> input) {
+        List<Integer> result = new ArrayList<>();
+        for (String current : input) {
+            result.add(this.StringToInt(current));
+        }
+        return result;
     }
 
     public static boolean isNumeric(String str) {
--- src/test/java/lotto/LottoUtilTest.java
+++ src/test/java/lotto/LottoUtilTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -33,4 +35,28 @@ public class LottoUtilTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("String list를 integer list로 변환.")
+    @Test
+    void convertStringListToIntList() {
+        List<String> input1 = List.of("1234", "3000");
+        List<String> input2 = List.of("3333", "4310", "4528");
+
+
+        assertAll(() -> assertThat(this.lottoUtil.StringListToIntList(input1))
+                        .isEqualTo(List.of(1234, 3000)),
+                () -> assertThat(this.lottoUtil.StringListToIntList(input2))
+                        .isEqualTo(List.of(3333, 4310, 4528)));
+    }
+
+    @DisplayName("정수로 변환이 불가능한 String이 포함된 list를 integer list로 변환 시 예외가 발생한다.")
+    @Test
+    void convertStringListToIntListException() {
+        List<String> input1 = List.of("1234", "ab34");
+        List<String> input2 = List.of("3333", "4310", "452b");
+
+        assertAll(() -> assertThatThrownBy(()->this.lottoUtil.StringListToIntList(input1))
+                        .isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(()->this.lottoUtil.StringListToIntList(input2))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
 }
