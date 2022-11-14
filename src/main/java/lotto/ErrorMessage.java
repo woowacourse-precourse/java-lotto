daadac0290057afd636db8e900b111011c6d4396
JerryK026 <kskyung0624@gmail.com>
2022-11-15 08:27:15 +0900

    fix(Client) : 불필요한 콤마 에러 처리 메서드 삭제
    수가 범위 안에 없거나, 숫자 개수가 모자라면 예외처리하기 때문에 콤마에 대한 테스트를 진행할 필요가 없다

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -9,7 +9,6 @@ public class ErrorMessage {
     // Client
     public static final String MONEY_NOT_INTEGER = "[ERROR] 금액 입력은 정수여야 합니다.";
     public static final String MONEY_NOT_THOUSANDS_MULTIPLE = "[ERROR] 금액 입력은 1,000의 배수여야 합니다.";
-    public static final String NUMBERS_SHOULD_BE_SPLITTED_WITH_COMMA = "[ERROR] 로또 입력 번호는 ,를 기준으로 구분되어야 합니다.";
     public static final String NUMBERS_NOT_INTEGERS = "[ERROR] 로또 입력 번호는 숫자여야 합니다.";
     public static final String BONUS_NOT_INTEGER = "[ERROR] 보너스 번호는 정수여야 합니다.";
     public static final String BONUS_NOT_IN_RANGE = "[ERROR] 보너스 번호는 1보다 크고 46보다 작은 숫자여야 합니다.";
