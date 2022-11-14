10fcaf2ba4fd849f908a985f6195d35e450a61d3
오승윤 <>
2022-11-14 23:19:06 +0900

    feat (Lotto) : 로또 책임 클래스 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,65 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Lotto {
+
+    private static final String SIZE_ERROR_MESSAGE = "로또 번호는 6개 입니다";
+    private static final String DUPLICATE_ERROR_MESSAGE = "로또 번호는 서로 중복될 수 없습니다";
+    private final List<LottoNumber> lottoNumbers;
+
+    private Lotto(List<Integer> numbers) {
+        validate(numbers);
+
+        lottoNumbers = new LinkedList<>();
+
+        for (Integer number : numbers) {
+            lottoNumbers.add(LottoNumber.generateFromRawValue(number));
+        }
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        boolean duplicated = numbers.stream()
+                .distinct()
+                .count() != numbers.size();
+
+        if (duplicated) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+
+    private static Lotto generate() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1
+                , 45
+                , 6));
+    }
+
+    public static List<Lotto> generateByAmount(Integer totalAmount) {
+        List<Lotto> lottos = new LinkedList<>();
+
+        for (int amount = 0; amount < totalAmount; amount++) {
+            lottos.add(Lotto.generate());
+        }
+
+        return lottos;
+    }
+
+    public static Lotto generateFromRawValues(List<Integer> rawValues) {
+        return new Lotto(rawValues);
+    }
+
+}
---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoTest {
+
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> Lotto.generateFromRawValues(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> Lotto.generateFromRawValues(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
