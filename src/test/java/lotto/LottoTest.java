5ab4f3069e17b47edf8dee6243fcffc5c0146727
김연진 <love_homebody@naver.com>
2022-11-15 01:20:02 +0900

    test: 당첨 번호 입력 시 예외 발생 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import lotto.domain.WinNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,4 +26,21 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("당첨 번호 입력시 숫자 이외의 입력이 발생할 경우 예외 발생")
+    @Test
+    void notIntegerInput() {
+        assertThrows(IllegalArgumentException.class, () -> new WinNumber("1,2,3,4,5,A"));
+    }
+
+    @DisplayName("당첨 번호 입력시 정해진 형태로 입력하지 않을 경우 예외 발생")
+    @Test
+    void invalidFormat() {
+        assertThrows(IllegalArgumentException.class, () -> new WinNumber("1,2,3,4,5 6"));
+    }
+
+    @DisplayName("당첨 번호 입력시 중복된 수를 입력하는 경우 예외 발생")
+    @Test
+    void isNotDuplicate() {
+        assertThrows(IllegalArgumentException.class, () -> new WinNumber("1,2,3,4,5,5"));
+    }
 }
