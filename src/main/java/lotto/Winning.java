fb6a3577a82a29cf5f64ed12028c0861ee93bcb9
h0ngwon <w___on@naver.com>
2022-11-14 21:36:44 +0900

    feat(Winning) : 보너스 번호 범위 검증 기능

--- src/main/java/lotto/Winning.java
+++ src/main/java/lotto/Winning.java
@@ -7,9 +7,10 @@ public class Winning {
     private static final String ERROR_MESSAGE = "[ERROR] ";
     private static final String LOTTO_OUT_OF_RANGE_ERROR = "로또 번호는 6개여야 합니다.";
     private static final String WINNING_NOT_NUMBER_ERROR = "입력 값이 숫자가 아닙니다.";
-    private static final String WINNING_NOT_IN_RANGE_ERROR = "당첨번호는 1 ~ 45 사이의 수 여야 합니다.";
+    private static final String WINNING_NOT_IN_RANGE_ERROR = "당첨번호는 1 ~ 45 사이의 수 이어야 합니다.";
     private static final String WINNING_DUPLICATED = "당첨번호에 중복이 있습니다.";
     private static final String BONUS_NUMBER_NOT_NUMBER = "보너스 번호는 숫자여야 합니다.";
+    private static final String BONUS_NUMBER_OUT_OF_RANGE = "보너스 번호는 1 ~ 45 사이의 수 이어야 합니다.";
 
     public Winning() {
     }
@@ -51,7 +52,7 @@ public class Winning {
     private static void isValidateWinningNumber(String num) {
         try {
             Integer.parseInt(num);
-        }catch(NumberFormatException e) {
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_MESSAGE + WINNING_NOT_NUMBER_ERROR);
         }
     }
@@ -59,13 +60,13 @@ public class Winning {
     private static void isValidateWinningNumberRange(String num) {
         int result = Integer.parseInt(num);
 
-        if(result < 1 || result > 45) {
+        if (result < 1 || result > 45) {
             throw new IllegalArgumentException(ERROR_MESSAGE + WINNING_NOT_IN_RANGE_ERROR);
         }
     }
 
     private static void isDuplicatedWinningNumber(List<Integer> numbers) {
-        if(numbers.stream().distinct().count() != 6) {
+        if (numbers.stream().distinct().count() != 6) {
             throw new IllegalArgumentException(ERROR_MESSAGE + WINNING_DUPLICATED);
         }
     }
@@ -73,8 +74,16 @@ public class Winning {
     private static void isValidateBonusNumber(String bonusNumber) {
         try {
             Integer.parseInt(bonusNumber);
-        }catch(NumberFormatException e) {
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_MESSAGE + BONUS_NUMBER_NOT_NUMBER);
+        }
+    }
+
+    private static void isValidateBonusNumberRange(String bonusNumber) {
+        int number = Integer.parseInt(bonusNumber);
+
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + BONUS_NUMBER_OUT_OF_RANGE);
         }
     }
 }
