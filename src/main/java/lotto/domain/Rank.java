c15bcd78435f7be37d089d7ef7b4932d42259ba4
sonsoowon <yuwoo1018@gmail.com>
2022-11-15 01:55:49 +0900

    refactor(Rank): 필드/메서드명 변경, 중복 제거

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -21,10 +21,10 @@ public enum Rank {
             NO_LUCK, 0
     ));
 
-    private static final String RANK_MESSAGE_FORMAT = "%d개 일치 (%s원)";
-    private static final String SECOND_RANK_MESSAGE_FORMAT = "%d개 일치, 보너스 볼 일치 (%s원)";
-
+    private static final String RANK_INFO_MESSAGE_FORMAT = "%d개 일치 (%s원)";
+    private static final String SECOND_RANK_INFO_MESSAGE_FORMAT = "%d개 일치, 보너스 볼 일치 (%s원)";
     private static final DecimalFormat rewardFormat = new DecimalFormat("###,###");
+
     private final int rank;
     private final int reward;
 
@@ -33,11 +33,12 @@ public enum Rank {
         this.reward = reward;
     }
 
-    public static Rank of(int jackpotCnt, boolean hasBonus) {
+    public static Rank getInstanceOf(int jackpotCnt, boolean hasBonus) {
+        if (jackpotCntMap.get(THIRD) == jackpotCnt && !hasBonus) {
+            return THIRD;
+        }
+
         for (Rank rank : Rank.values()) {
-            if (jackpotCntMap.get(THIRD) == jackpotCnt && !hasBonus) {
-                return THIRD;
-            }
             if (jackpotCntMap.get(rank) == jackpotCnt) {
                 return rank;
             }
@@ -47,14 +48,18 @@ public enum Rank {
 
     public String getInfo() {
         String reward = rewardFormat.format(this.reward);
+        String infoFormat = RANK_INFO_MESSAGE_FORMAT;
         if (this.equals(SECOND)) {
-            return String.format(SECOND_RANK_MESSAGE_FORMAT, jackpotCntMap.get(SECOND), reward);
+            infoFormat = SECOND_RANK_INFO_MESSAGE_FORMAT;
         }
-        return String.format(RANK_MESSAGE_FORMAT, jackpotCntMap.get(this), reward);
+
+        return String.format(infoFormat, jackpotCntMap.get(this), reward);
     }
-    public int getRankNumber(){
+
+    public int getRankNumber() {
         return rank;
     }
+
     public int getReward() {
         return reward;
     }
