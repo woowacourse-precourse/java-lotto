e29cceb43dacd1d7c45159ca9e05e4c968b3630f
ljh <dusk5588@naver.com>
2022-11-15 03:22:28 +0900

    feat(Controller) : 게임 흐름을 제어하는 클래스 구현

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,7 +1,41 @@
 package Controller;
 
+import Domain.*;
+import View.OutputView;
+
+import java.util.List;
+
 public class Controller {
     public void run(){
+        LottoList lottoList = new LottoList();
+        Winning winning = new Winning();
+        Bonus bonus = new Bonus(winning);
 
+        calculateLottoResult(lottoList, winning, bonus);
+
+    }
+
+    public void calculateLottoResult(LottoList lottoList, Winning winning, Bonus bonus){
+        int[] ranking = new int[6];
+        for(Lotto eachLotto : lottoList.getLottoList()){
+            int compareLotto = compareMyLottoAndWinningNumber(eachLotto.getLottoNumbers(), winning.getNumbers());
+            boolean compareBonus = compareMyLottoAndBonusNumber(eachLotto.getLottoNumbers(), bonus.getBonusNumber());
+            int a = Prize.convertCountToPrizeMoney(compareLotto, ranking, compareBonus);
+        }
+        OutputView.printLottoResult(ranking);
+    }
+
+    public int compareMyLottoAndWinningNumber(List<Integer> myLottoNumber, List<Integer> winningNumber){
+        int countSameNumber = 0;
+        for(int eachWinningNumber : winningNumber){
+            if(myLottoNumber.contains(eachWinningNumber)){
+                countSameNumber += 1;
+            }
+        }
+        return countSameNumber;
+    }
+
+    public boolean compareMyLottoAndBonusNumber(List<Integer> myLottoNumber, int bonusNumber){
+        return myLottoNumber.contains(bonusNumber);
     }
 }
