e863a16da5479f4cd308b7a00d0482dec0b01da1
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-11-15 06:26:28 +0900

    feat: 등수에 따라 상금 가져오는 기능 추가

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,2 +1,20 @@
-package lotto;public enum Prize {
+package lotto;
+
+public enum Prize {
+    FIRST(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FOURTH(50_000),
+    FIFTH(5_000),
+    NOTHING(0);
+
+    final public int prize;
+
+    public Integer getPrize() {
+        return prize;
+    }
+
+    Prize(int prize) {
+        this.prize = prize;
+    }
 }
