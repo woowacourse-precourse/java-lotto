0f9cf9ed0dee4b5224cf8cc7d2cfa33bc4d54d33
leeyongjun <leeyongjun0310@naver.com>
2022-11-14 18:43:54 +0900

    test(lotto): 로또 개수를 정상적으로 구하는지 확인하는 테스트 코드 구현

--- src/test/java/lotto/domain/PurchaseTest.java
+++ src/test/java/lotto/domain/PurchaseTest.java
@@ -1,7 +1,15 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 class PurchaseTest {
-
+    @DisplayName("로또 개수를 정상적으로 구하는지 확인한다.")
+    @Test
+    void getLottoCount_정상동작() {
+        int input = Purchase.getLottoCount(5000);
+        assertThat(input).isEqualTo(5);
+    }
 }
