02227700d1d10e3c83e4b8e0aa0ac5407bcdef1c
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-15 07:52:17 +0900

    test(service): 빈도수 생성시 0이더라도 모든 당첨이 key에 존재해야함 테스트 완료

--- src/test/java/lotto/layer/LottoServiceTest.java
+++ src/test/java/lotto/layer/LottoServiceTest.java
@@ -1,12 +1,21 @@
 package lotto.layer;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mockStatic;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
 import lotto.domain.Money;
+import lotto.domain.WinningLotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
 
 class LottoServiceTest {
 
@@ -41,4 +50,25 @@ class LottoServiceTest {
         // then
         assertThat(lotteries.size()).isEqualTo(tableLotteries.size());
     }
+
+    @DisplayName("당첨 빈도수가 0이여도 모든 당첨이 key 값에 존재해야함")
+    @Test
+    void lottoServiceMakeFrequencyMustHasAllWinningLottoKeys() {
+        // given
+        MockedStatic<Randoms> randoms = mockStatic(Randoms.class);
+        List<Integer> expectValues = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+        Lotto inputLotto = new Lotto(expectValues);
+        LottoNumber bonusNumber = LottoNumber.getInstance(7);
+        given(Randoms.pickUniqueNumbersInRange(1, 45, 6)).willReturn(expectValues);
+
+        // when
+        Map<WinningLotto, Integer> frequency = lottoService.getWinningLottoFrequency(inputLotto,
+                bonusNumber);
+
+        // then
+        assertThat(frequency).containsKeys(WinningLotto.values());
+        randoms.close();
+    }
+
+
 }
