8f3c52329676534503fd03226ebaa5fa31f378b2
kimJaeHyeon <kmr2644@gmail.com>
2022-11-14 11:22:55 +0900

    feat: money 변수 감싸는 래퍼클래스 구현

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+public class Money {
+
+    private final Long amount;
+
+    private Money(Long money) {
+        this.amount = money;
+
+    }
+
+    public static Money of(Long money) {
+        return new Money(money);
+    }
+    public  Double calcYield(Long prizeMoney) {
+        return (double) (this.amount / prizeMoney * 100);
+    }
+
+}
