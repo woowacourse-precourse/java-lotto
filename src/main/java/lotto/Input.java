e5cebb7fa8a8bc90020cfa27e26bf2ee214463c1
meunrud <y3a0522@gmail.com>
2022-11-15 01:37:45 +0900

    :sparkles: input 예외사항 구현

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Input {
@@ -13,7 +14,7 @@ public class Input {
         return Integer.parseInt(amountOfMoney);
     }
 
-    public String  inputWinningLottosNumber(){
+    public String inputWinningLottosNumber(){
         return Console.readLine();
     }
 
@@ -23,11 +24,37 @@ public class Input {
 
 
 
-//    public void checkMoneyUnit(int money){
-//        if(!(money / CHEON_WON == 0)){
-//            throw new IllegalArgumentException("[ERROR] 금액은 천원 단위로 입력해주세요");
-//        }
-//    }
+    public void checkMoneyUnit(int money){
+        if(!(money / CHEON_WON == 0)){
+            throw new IllegalArgumentException("[ERROR] 금액은 천원 단위로 입력해주세요");
+        }
+    }
+
+    public void containsComma(String winningNumbers){
+        if(!winningNumbers.contains(",")){
+            throw new IllegalArgumentException("[ERROR] 숫자는 ,로 구분해서 입력해주세요");
+        }
+    }
+
+    public void checkOverlap(String winningNumbers){
+        String [] numberCheck = winningNumbers.split(",");
+        numberCheck = Arrays.stream(numberCheck).distinct().toArray(String[]::new);
+        if(!(numberCheck.length == 6)){
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    public void bonusOverlapWithWinning(String winningNumbers, String bonusNumber){
+        if(winningNumbers.contains(bonusNumber)){
+            throw new IllegalArgumentException("[ERROR] 보너스 숫자는 당첨 번호와 중복 될 수 없습니다.");
+        }
+    }
+
+    public void checkBonusNumberLength(String bonusNumber){
+        if(!(bonusNumber.length() == 1)){
+            throw new IllegalArgumentException("[ERROR] 한개의 보너스 숫자를 입력해주세요.");
+        }
+    }
 
 
 }
