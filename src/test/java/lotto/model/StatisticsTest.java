c2f3bf06f200537be7bcaf4f706207180dad82d8
김현재 <41482946+galug@users.noreply.github.com>
2022-11-14 18:07:49 +0900

    test(statisticstest): 당첨된 총 금액 계산 기능 테스트 추가

--- src/test/java/lotto/model/StatisticsTest.java
+++ src/test/java/lotto/model/StatisticsTest.java
@@ -1,15 +1,12 @@
 package lotto.model;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 class StatisticsTest {
@@ -17,13 +14,13 @@ class StatisticsTest {
             "4개 일치 (50,000원) - 1개\n" +
             "5개 일치 (1,500,000원) - 0개\n" +
             "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개\n" +
-            "6개 일치 (2,000,000,000원) - 1개\n";
+            "6개 일치 (2,000,000,000원) - 1개";
 
     private final List<Lotto> lottos = new ArrayList<>();
-    private final WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+    private final WinningLotto winningLotto = new WinningLotto(List.of(6, 5, 3, 4, 2, 1), 7);
 
     @BeforeEach
-    void beforeAll() {
+    void beforeEach() {
         List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
         lottos.add(new Lotto(lottoNumbers));
         for (int i = 0; i < 4; i++) {
@@ -33,47 +30,24 @@ class StatisticsTest {
         }
     }
 
-    @DisplayName("로또들 중 당첨 로또의 개수를 세는 기능이 동작한다.")
-    @Test
-    void countFirstRank() {
-        Statistics statistics = new Statistics(lottos, winningLotto);
-
-        Map map = statistics.countRank();
-        Assertions.assertThat(map.get(Rank.FIRST)).isEqualTo(1);
-    }
-
-    @DisplayName("로또들 중 당첨 로또의 개수를 세는 기능이 동작한다.")
-    @Test
-    void countSecondRank() {
-        Statistics statistics = new Statistics(lottos, winningLotto);
-
-        Map map = statistics.countRank();
-        Assertions.assertThat(map.get(Rank.SECOND)).isEqualTo(1);
-    }
-
-    @DisplayName("로또들 중 당첨 로또의 개수를 세는 기능이 동작한다.")
-    @Test
-    void countThirdRank() {
-        Statistics statistics = new Statistics(lottos, winningLotto);
-
-        Map map = statistics.countRank();
-        Assertions.assertThat(map.get(Rank.THIRD)).isEqualTo(0);
-    }
-
-    @DisplayName("로또들 중 당첨 로또의 개수를 세는 기능이 동작한다.")
-    @Test
-    void countNothingRank() {
-        Statistics statistics = new Statistics(lottos, winningLotto);
-
-        Map map = statistics.countRank();
-        Assertions.assertThat(map.get(Rank.NOTHING)).isNull();
-    }
-
+    @DisplayName("올바른 통계 결과를 내는지 확인한다.")
     @Test
     void testToString() {
-        Statistics statistics = new Statistics(lottos, winningLotto);
+        RankCount rankCount = new RankCount(lottos, winningLotto);
+        Statistics statistics = new Statistics(rankCount);
 
-        statistics.countRank();
         Assertions.assertThat(statistics.toString()).isEqualTo(COMPARE_STATISTICS);
+    }
+
+    @DisplayName("컴퓨터가 올바른 ")
+    @Test
+    void computeRightTotalPrice() {
+        RankCount rankCount = new RankCount(lottos, winningLotto);
+        Statistics statistics = new Statistics(rankCount);
+
+        long resultTotalPrice = statistics.computeTotalPrice();
+        long expectedTotalPrice = 2030055000L;
+
+        Assertions.assertThat(resultTotalPrice).isEqualTo(expectedTotalPrice);
     }
 }
