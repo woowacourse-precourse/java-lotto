ed7c8aeec2f015aad310c0cc8e06620e5261c809
ryanha <ryanha@handong.ac.kr>
2022-11-14 23:56:39 +0900

    refactor(domain): 손익률 계산 시 사용하는 값들을 Policy와 연동

--- src/main/java/lotto/domain/MarginCalculator.java
+++ src/main/java/lotto/domain/MarginCalculator.java
@@ -1,30 +1,23 @@
 package lotto.domain;
 
-import java.util.List;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MarginCalculator {
-    private static int[] winningAmount; // 당첨금액
-
-    public MarginCalculator() {
-        winningAmount = new int[6];
-        winningAmount[1] = 2000000000;
-        winningAmount[2] = 30000000;
-        winningAmount[3] = 1500000;
-        winningAmount[4] = 50000;
-        winningAmount[5] = 5000;
-    }
-
-    public double getMarginRatio(List<Integer> winningInfo, int lottoCount) {
+    public static double getMarginRatio(Map<String, Integer> winningInfo, int lottoCount) {
         long totalAmount = getTotalWinningAmount(winningInfo);
-        int paymentAmount = lottoCount * LottoStore.PRICE;
+        int paymentAmount = lottoCount * Policy.PRICE;
 
         return (double)totalAmount / paymentAmount * 100;
     }
 
-    private long getTotalWinningAmount(List<Integer> winningInfo) {
+    private static long getTotalWinningAmount(Map<String, Integer> winningInfo) {
         long totalAmount = 0;
-        for(int i=1 ; i<winningInfo.size() ; i++) {
-            totalAmount += (long)winningAmount[i] * winningInfo.get(i);
+        Iterator<String> keys = winningInfo.keySet().iterator();
+
+        while(keys.hasNext()) {
+            String key = keys.next();
+            totalAmount += Policy.WinningAmount.valueOf(key).getAmount() * winningInfo.get(key);
         }
         return totalAmount;
     }
