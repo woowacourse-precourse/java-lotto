276312d7e998495b9b8dad5148ecc3b655d1b05a
DevJay1024 <devjay1024@gmail.com>
2022-11-14 18:46:47 +0900

    refactor: 사용자 입력 예외처리 문구 변수 이름 변경
    보다 정확하고 명료하게 예외상황을 전달할 수 있도록 변수명 변경

--- src/main/java/lotto/InvalidInputMessage.java
+++ src/main/java/lotto/InvalidInputMessage.java
@@ -7,9 +7,9 @@ public class InvalidInputMessage {
     public static String ERR_MONEY_OVER_RANGE = "로또 구매금액이 허용 범위 이내여야 합니다.";
     public static String ERR_MONEY_UNIT = "로또 구매금액은 1000원 단위로 입력해야 합니다.";
     public static String ERR_EMPTY_INPUT = "입력값이 비어있습니다.";
-    public static String ERR_NOT_NUMBER = "숫자만 입력해야 합니다.";
-    public static String ERR_NUMBER_OVER_RANGE = "로또 번호 범위는 1~45여야 합니다.";
+    public static String ERR_NOT_NUMERIC = "숫자만 입력해야 합니다.";
+    public static String ERR_NUMBER_RANGE = "로또 번호 범위는 1~45여야 합니다.";
     public static String ERR_WINNING_NUMBER_FORMAT = "로또 번호의 구분자는 반점(,)이어야 합니다.";
-    public static String ERR_NOT_ENOUGH_COUNT = "로또 한 줄에 번호는 6개여야 합니다.";
+    public static String ERR_NUMBERS_COUNT = "로또 한 줄에 번호는 6개여야 합니다.";
     public static String ERR_DUPLICATED_NUMBER = "당첨 번호와 보너스 번호는 중복되지 않아야 합니다.";
 }
