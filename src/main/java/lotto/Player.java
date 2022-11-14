b4f6b6b8d5c431d6d182c4e90f2f9cf50bbda166
swjeong98 <tree5678@konkuk.ac.kr>
2022-11-14 21:36:22 +0900

    feat(Player) : 사용자가 입력한 당첨 번호의 유효성을 확인하는 기능 구현

--- src/main/java/lotto/Player.java
+++ src/main/java/lotto/Player.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.*;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Player {
@@ -7,7 +8,7 @@ public class Player {
     private int PurchaseMoney;
     private int total_lotto;
     private Manager manager;
-
+    private List<Integer> winning;
     public Player(Manager manager){
         this.manager = manager;
     }
@@ -26,5 +27,21 @@ public class Player {
 
     public void calculateTotalLotto(){
         this.total_lotto = this.PurchaseMoney / this.LOTTO_PRICE;
+    }
+    public void checkWinningNumber(String input){
+        String[] arr = input.split(",");
+        if(arr.length != manager.LOTTO_LENGTH)
+            throw new IllegalArgumentException("[ERROR] : 당첨번호는 쉼표(,)를 기준으로 6개로 구분되어야 합니다.");
+        List<String> list = Arrays.asList(arr);
+        if(!manager.ValidityOfNumber(list))
+            throw new IllegalArgumentException("[ERROR] : 당첨번호는 반드시 숫자로만 이루어져야 합니다.");
+        List<Integer> winnings = new ArrayList<>();
+        for(String s : list)
+            winnings.add(Integer.parseInt(s));
+        if(!manager.ValidRangeOfLottoNumber(winnings))
+            throw new IllegalArgumentException("[ERROR] : 당첨번호는 1 ~ 45 범위에 해당하는 숫자여야 합니다.");
+        if(manager.OverlapOfLottoNumber(winnings))
+            throw new IllegalArgumentException("[ERROR] : 6개의 당첨번호는 모두 다른 숫자여야 합니다.");
+        this.winning = winnings;
     }
 }
