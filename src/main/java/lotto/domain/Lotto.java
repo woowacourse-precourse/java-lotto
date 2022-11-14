4489847955bcdabb4b50faeb8c5e2a3bddb714f8
kkirby54 <sdk926@gmail.com>
2022-11-15 05:13:45 +0900

    feat: domain 패키지로 변경

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            System.out.println("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException();
+        }
+        if (numbers.stream().filter(n -> 1 <= n && n <= 45).count() != 6) {
+            System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 자연수여야 합니다.");
+            throw new IllegalArgumentException();
+        }
+        if (numbers.stream().distinct().count() != 6) {
+            System.out.println("[ERROR] 로또 번호는 중복이 없어야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // TODO: 추가 기능 구현
+}
