b75cebb77dacf62468037162d3a3d07df4dcfacd
youswim <youswim96@gmail.com>
2022-11-15 01:14:02 +0900

    feat(lotto): 입력 금액 유효성 검사 기능 추가

--- src/main/java/lotto/InputValidator.java
+++ src/main/java/lotto/InputValidator.java
@@ -2,8 +2,18 @@ package lotto;
 
 public class InputValidator {
 
-    public static boolean validatePurchaseMoney() {
-        return false;
+    static final int MIN_MONEY = 1_000;
+
+    public static Integer validatePurchaseMoney(String purchaseMoney) {
+        int money = 0;
+        try {
+            money = Integer.parseInt(purchaseMoney);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(ExceptionHandler.WORNG_PURCHASE_MONEY);
+        }
+        if(money % MIN_MONEY != 0)
+            throw new IllegalArgumentException(ExceptionHandler.WORNG_PURCHASE_MONEY);
+        return money;
     }
 
     public static boolean validateSixNumber() {
