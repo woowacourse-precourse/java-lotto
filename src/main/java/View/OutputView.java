d706c1d1ea309812b84fe7b86fd52ce969b8518a
ljh <dusk5588@naver.com>
2022-11-15 03:11:55 +0900

    feat(OutputView) : 화면의 결과를 출력하는 클래스 OutputView 구현

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,4 +1,38 @@
 package View;
 
+import java.util.List;
+
 public class OutputView {
+    public static void printNumberOfLotto(int numberOfLotto, List<Lotto> lottoList){
+        System.out.printf("%d개를 구매했습니다.\n", numberOfLotto);
+        printEachLottoNum(lottoList);
+    }
+
+    public static void printEachLottoNum(List<Lotto> lottoList){
+        for(Lotto lotto : lottoList){
+            System.out.print("[");
+            int len = lotto.getLottoNumbers().size();
+            for(int i=0; i<len-1; i++){
+                System.out.print(lotto.getLottoNumbers().get(i) + ", ");
+            }
+            System.out.print(lotto.getLottoNumbers().get(len-1));
+            System.out.println("]");
+        }
+        System.out.println();
+    }
+
+    public static void printLottoResult(int[] countList){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + countList[5] + "개");
+        System.out.println("4개 일치 (50,000원) - " + countList[4] + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + countList[3] + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + countList[2] + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + countList[1] + "개");
+    }
+
+    public static void printIncomeRate(int numberOfLotto, int income){
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", (income * 100.0) / (numberOfLotto * 1000));
+    }
+
 }
