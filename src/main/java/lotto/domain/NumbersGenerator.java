10564cd8e0f63076a6bccf4e19858dc9f7944050
qkdk <86948395+qkdk@users.noreply.github.com>
2022-11-14 14:39:41 +0900

    fix: 로또 숫자 정렬기능 수정

--- src/main/java/lotto/domain/NumbersGenerator.java
+++ src/main/java/lotto/domain/NumbersGenerator.java
@@ -1,19 +1,14 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Comparator;
 import java.util.List;
 import lotto.Constants;
 
 public class NumbersGenerator {
 
     public List<Integer> generatorNumbers() {
-        return sortNumbers(Randoms.pickUniqueNumbersInRange(Constants.MIN_NUMBER_RANGE.getValue(),
-                Constants.MAX_NUMBER_RANGE.getValue(), Constants.NUMBERS_LENGTH.getValue()));
+        return Randoms.pickUniqueNumbersInRange(Constants.MIN_LOTTO_NUMBER_RANGE.getValue(),
+                Constants.MAX_LOTTO_NUMBER_RANGE.getValue(), Constants.LOTTO_NUMBERS_LENGTH.getValue());
     }
 
-    private List<Integer> sortNumbers(List<Integer> numbers) {
-        numbers.sort(Comparator.naturalOrder());
-        return numbers;
-    }
 }
