e22c998e8d0a6d9bd8d5cea22bf8cc96c0bb0cc7
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2022-11-14 16:55:47 +0900

    test(InputValidatorTest): add test case to validate winning number

--- src/test/java/lotto/InputValidatorTest.java
+++ src/test/java/lotto/InputValidatorTest.java
@@ -4,6 +4,10 @@ import lotto.model.InputValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -49,6 +53,29 @@ public class InputValidatorTest {
         @Test
         void inputWinningNumberContainsBlackElement() {
             assertThatThrownBy(() -> inputValidator.convertStringIntoList("0,1,10,,20,30"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+        @Test
+        void WinningNumberSizeIsOver() {
+            assertThatThrownBy(() -> inputValidator.validateWinningNumber(List.of(1, 2, 3, 4, 5, 6, 7)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("당첨 번호의 개수가 6개보다 적으면 예외가 발생한다.")
+        @Test
+        void WinningNumberSizeIsUnder() {
+            assertThatThrownBy(() -> inputValidator.validateWinningNumber(List.of(1, 2, 3, 4, 5)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("당첨 번호의 범위가 1~45를 벗어나면 예외가 발생한다.")
+        @ParameterizedTest
+        @CsvSource(value = {"1,2,3,4,5,99", "0,10,20,30,40,45"})
+        void inputWinningNumber4(int a, int b, int c, int d, int e, int f) {
+            List<Integer> winningNumber = List.of(a, b, c, d, e, f);
+            assertThatThrownBy(() -> inputValidator.validateWinningNumber(winningNumber))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
