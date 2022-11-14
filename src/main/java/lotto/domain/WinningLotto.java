9141bd94cb7b84b8ab18cf03df8dbbc10df76906
hgo641 <hgo641@gmail.com>
2022-11-15 04:20:56 +0900

    feat: 유저가 구매한 모든 로또들의 당첨 내역을 구하는 기능 구현

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,56 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WinningLotto {
+    Lotto winningLottoNumbers;
+    int bonusNumber;
+
+    private WinningLotto(Lotto winningLottoNumbers, int bonusNumber) {
+        this.winningLottoNumbers = winningLottoNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static WinningLotto of(Lotto winningLottoNumbers, String bonusNumber){
+        int convertedBonusNumber = convertBonusNumber(bonusNumber);
+        validateBonusNumberDuplicate(winningLottoNumbers, convertedBonusNumber);
+        return new WinningLotto(winningLottoNumbers, convertedBonusNumber);
+    }
+
+    private static int convertBonusNumber(String bonusNumber){
+        try{
+            Integer.parseInt(bonusNumber);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("보너스 번호는 숫자만 입력할 수 있습니다.");
+        }
+        return Integer.parseInt(bonusNumber);
+    }
+
+    public static void validateBonusNumberDuplicate(Lotto winningLottoNumbers, int bonusNumber){
+        if(winningLottoNumbers.isContain(bonusNumber)){
+            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public Map<Rank, Integer> getUserLottoScore(List<Lotto> userLottos){
+        Map<Rank, Integer> userLottoScore = new HashMap<>();
+        for(Rank rank : Rank.values()){
+            userLottoScore.put(rank, 0);
+        }
+        userLottos.forEach(userLotto -> {
+            Rank rank = Rank.of(winningLottoNumbers.getMatchedCount(userLotto), isBonus(userLotto));
+            userLottoScore.put(rank, userLottoScore.get(rank) + 1);
+        });
+        userLottos.remove(Rank.NOTHING);
+        return userLottoScore;
+    }
+
+    private boolean isBonus(Lotto userLotto){
+        if(userLotto.isContain(bonusNumber)){
+            return true;
+        }
+        return false;
+    }
+}
