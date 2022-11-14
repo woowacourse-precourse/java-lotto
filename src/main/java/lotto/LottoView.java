24cdcce20dea352e5ddcea857ce35026810f87d0
yun <yshyeon102@gmail.com>
2022-11-14 11:01:54 +0900

    fix(LottoView): 수익률 출력에 사용되는 메세지 패턴 변경
    수익률이 0%일 때, .0%로 출력되는 문제가 발생하여 패턴을 변경

--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -16,7 +16,7 @@ public class LottoView {
     private static final String REQUEST_WINNING_LOTTO_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String REQUEST_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String STATISTICS_INFO_MESSAGE = "당첨 통계\n---";
-    private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 {0,number,.#}%입니다.";
+    private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 {0,number,#.#}%입니다.";
     private static final String BLANK_LINE = "\n";
 
     public static final List<String> PRIZE_DESC = Arrays.stream(WinningType.values())
