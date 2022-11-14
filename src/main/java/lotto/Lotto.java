c62a15bf56e0009bdb7d36b277241a72dd849e38
ChaCha3088 <cha3088@gmail.com>
2022-11-14 15:09:00 +0900

    feat(/src/main/java/lotto/Lotto.java): change method Lotto 당첨 통계 출력 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,7 +3,9 @@ package lotto;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,5 +14,21 @@ public class Lotto {
         HashSet<Integer> playerNumbers = Model.MakeListToSet(numbers);
         ValidatePlayerNumber(playerNumbers);
         this.numbers = numbers;
+
+        // 로또 번호 분석
+        LinkedHashMap<String, Integer> lottoResult = Model.AnalyzePlayerNumbers(numbers);
+
+        // 당첨 통계 출력
+        View.Output("당첨 통계");
+        View.Output("---");
+        List<String> lottoResultKeys = new ArrayList<>(lottoResult.keySet());
+        List<String> lottoReward = new ArrayList<>(LottoReward.values());
+        for (int i = 0; i < 5; i++) {
+            String key = lottoResultKeys.get(i);
+            Integer result = lottoResult.get(i);
+            String reward = lottoReward.get(i);
+            View.Output(key+"개 일치 ("+reward+") - "+result+"개");
+        }
+
     }
 }
