3b188fcb852263a2b1a41862b27124396b5b0f48
Seokhyun Lee <lseokhyun99@gmail.com>
2022-11-14 21:39:43 +0900

    refactor: 하드 코딩된 값 상수화
    하드 코딩되었던 값들을
    static final을 이용해 상수화하였다.

--- src/main/java/lotto/LottoStatistics.java
+++ src/main/java/lotto/LottoStatistics.java
@@ -4,43 +4,49 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LottoStatistics {
-    public static List<Integer> resultWinningLottery(List<Lotto> lotto, List<Integer> winningNumber, Integer bonusNumber){
-        List<Integer> lottoResult = Arrays.asList(0,0,0,0,0);
+    private static final int LOTTO_PRICE = 1000;
+    private static final int PERCENTAGE = 100;
+    private static final int ROUND_TO_SECOND_PLACE = 10;
+
+    public static List<Integer> resultWinningLottery(List<Lotto> lotto, List<Integer> winningNumber, Integer bonusNumber) {
+        List<Integer> lottoResult = Arrays.asList(0, 0, 0, 0, 0);
 
         int rank;
-        for(int i = 0 ; i < lotto.size() ; i++) {
-            rank = lotto.get(i).lottoRanking(winningNumber,bonusNumber);
-            if( rank != 0 )
-                lottoResult.set( rank-1 , lottoResult.get(rank-1) + 1 );
+        for (int i = 0; i < lotto.size(); i++) {
+            rank = lotto.get(i).lottoRanking(winningNumber, bonusNumber);
+            if (rank != 0)
+                lottoResult.set(rank - 1, lottoResult.get(rank - 1) + 1);
         }
 
         return lottoResult;
     }
-    public static double calculateLottoEarningsRate(List<Integer> lottoResult, int lottoCount){
-        double purchaseAmount = lottoCount * 1000;
+
+    public static double calculateLottoEarningsRate(List<Integer> lottoResult, int lottoCount) {
+        double purchaseAmount = lottoCount * LOTTO_PRICE;
         double earning = 0;
 
         String rank;
-        for(int i = 0 ; i < lottoResult.size() ; i++){
-            rank = "RANK"+(i+1);
+        for (int i = 0; i < lottoResult.size(); i++) {
+            rank = "RANK" + (i + 1);
             earning += lottoResult.get(i) * LottoInformation.valueOf(rank).getPrizeMoney();
         }
 
-        double earningRate = earning / purchaseAmount * 100;
-        earningRate = Math.round(earningRate*10) / 10.0;
+        double earningRate = earning / purchaseAmount * PERCENTAGE;
+        earningRate = (double) Math.round(earningRate * ROUND_TO_SECOND_PLACE) / ROUND_TO_SECOND_PLACE;
 
         return earningRate;
     }
-    public static void winningStatistics(List<Integer> lottoResult, double earningRate){
+
+    public static void winningStatistics(List<Integer> lottoResult, double earningRate) {
         System.out.println("\n당첨 통계");
         System.out.println("---");
 
         String rank;
-        for(int i = lottoResult.size()-1 ; i >= 0 ; i--) {
+        for (int i = lottoResult.size() - 1; i >= 0; i--) {
             rank = "RANK" + (i + 1);
             System.out.print(LottoInformation.valueOf(rank).getResultMsg());
-            System.out.println(" - " + lottoResult.get(i)+"개");
+            System.out.println(" - " + lottoResult.get(i) + "개");
         }
-        System.out.println("총 수익률은 "+earningRate+"%입니다.");
+        System.out.println("총 수익률은 " + earningRate + "%입니다.");
     }
 }
