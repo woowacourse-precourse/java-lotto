4b39536a1cfcc9c951a65608e38111c50be3944d
Laeng <Laeng@users.noreply.github.com>
2022-11-14 18:32:43 +0900

    feat(lotto): Lotto 를 저장하는 Ticket 구현
    newInstance 는 테스트를 위해 만들어졌으므로 getInstance 정적 메서드 사용을 권장합니다.

---
+++ src/main/java/lotto/model/Ticket.java
@@ -0,0 +1,35 @@
+package lotto.model;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Ticket {
+    private static Ticket ticket;
+
+    private final List<Lotto> database;
+
+    private Ticket() {
+        this.database = new LinkedList<>();
+    }
+
+    public static Ticket getInstance() {
+        if (ticket == null) {
+            ticket = newInstance();
+        }
+
+        return ticket;
+    }
+
+    public static Ticket newInstance() {
+        return new Ticket();
+    }
+
+    public boolean insert(Lotto lotto) {
+        return this.database.add(lotto);
+    }
+
+    public List<Lotto> getAll() {
+        return Collections.unmodifiableList(this.database);
+    }
+}
---
+++ src/test/java/lotto/model/TicketTest.java
@@ -0,0 +1,30 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketTest {
+    @DisplayName("데이터를 삽입한다.")
+    @Test
+    void insert() {
+        Ticket db = Ticket.newInstance();
+        Lotto lotto = Lotto.fromRandomNumbers();
+
+        assertThat(db.insert(lotto)).isTrue();
+    }
+
+    @DisplayName("모든 데이터를 가져온다.")
+    @Test
+    void getAll() {
+        Ticket db = Ticket.newInstance();
+
+        for(int count = 0; count < 10; count++) {
+            db.insert(Lotto.fromRandomNumbers());
+        }
+
+        assertThat(db.getAll().size()).isEqualTo(10);
+    }
+}
