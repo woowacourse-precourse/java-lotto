807224d6aa7965ba8bf6afc37337b6ea9defad90
gomljo <gomljo0307@naver.com>
2022-11-14 14:42:59 +0900

    rafactor(RaffleNumber.java): 생성자 함수 수정
    가독성을 위해 추첨 번호 에러 처리와 변환 기능을 모아 함수로 작성

--- src/main/java/lotto/model/RaffleNumber.java
+++ src/main/java/lotto/model/RaffleNumber.java
@@ -9,20 +9,33 @@ public class RaffleNumber {
     private static final String NUMERIC_PATTERN = "[\\d+,]";
     private static final String NUMERIC = "\\d+";
     private static final String BLANK = "";
-    private static final String DELIMITER=",";
+    private static final String DELIMITER = ",";
     private static final String ENTER_WRONG_LETTERS_MESSAGE = "[ERROR] 금액을 잘못 입력하셨습니다. 프로그램을 종료합니다.";
-    private static final int PROPER_QUANTITY=6;
-    private static final String WRONG_QUANTITY_ERROR_MESSAGE="[ERROR] 추첨 번호가 6개가 아닙니다. 프로그램을 종료합니다.";
-    private static final String DUPLICATE_ERROR_MESSAGE="[ERROR] 숫자가 중복되었습니다. 프로그램을 종료합니다.";
-    private final List<Integer> raffleNumbers;
+    private static final int PROPER_QUANTITY = 6;
+    private static final String WRONG_QUANTITY_ERROR_MESSAGE = "[ERROR] 추첨 번호가 6개가 아닙니다. 프로그램을 종료합니다.";
+    private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 숫자가 중복되었습니다. 프로그램을 종료합니다.";
+    private List<Integer> raffleNumbers;
+    private final int bonusNumber;
 
     public RaffleNumber(String raffleNumbers, String bonusNumber) {
-        validateProperRaffleNumber(raffleNumbers, NUMERIC_PATTERN);
-        List<String> separatedRaffleNumber = separateRaffleNumberByComma(raffleNumbers);
+        this.raffleNumbers=createRaffleNumber(raffleNumbers);
+        validateProperRaffleNumber(bonusNumber, NUMERIC);
+
+        this.bonusNumber = Integer.parseInt(bonusNumber);
+    }
+    public List<Integer> createRaffleNumber(String raffleNumber){
+        validateProperRaffleNumber(raffleNumber, NUMERIC_PATTERN);
+        List<String> separatedRaffleNumber=List.of(raffleNumber.split(DELIMITER));
         validateProperQuantity(separatedRaffleNumber);
         validateDuplicatedNumbers(separatedRaffleNumber);
-        this.raffleNumbers = transform(separatedRaffleNumber, Integer::parseInt);
+        return transform(separatedRaffleNumber, Integer::parseInt);
+    }
+    public List<Integer> getRaffleNumbers() {
+        return raffleNumbers;
+    }
 
+    public int getBonusNumber() {
+        return bonusNumber;
     }
 
     private void validateProperQuantity(List<String> raffleNumbers) {
@@ -34,29 +47,25 @@ public class RaffleNumber {
     private void validateDuplicatedNumbers(List<String> numbers) {
         int numberOfUniqueNumbers;
         Stream<String> lottoNumbers = numbers.stream();
-        numberOfUniqueNumbers= (int)lottoNumbers.distinct().count();
+        numberOfUniqueNumbers = (int) lottoNumbers.distinct().count();
         if (numberOfUniqueNumbers != PROPER_QUANTITY) {
             throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
         }
     }
 
-    private void validateProperRaffleNumber(String givenNumber, String pattern) {
-        String numberRemovedLetters=givenNumber.replaceAll(pattern, BLANK);
-        if (!numberRemovedLetters.equals(BLANK)) {
-            throw new IllegalArgumentException(ENTER_WRONG_LETTERS_MESSAGE);
-        }
-    }
-
-    public List<String> separateRaffleNumberByComma(String raffleNumber){
+    public List<String> separateRaffleNumberByComma(String raffleNumber) {
         return List.of(raffleNumber.split(DELIMITER));
     }
 
-    public List<Integer> getRaffleNumbers() {
-        return raffleNumbers;
+    private <T, U> List<U> transform(List<T> list, Function<T, U> function) {
+        return list.stream().map(function).collect(Collectors.toList());
     }
 
-    private <T, U> List<U> transform(List<T> list, Function<T, U> function){
-        return list.stream().map(function).collect(Collectors.toList());
+    private void validateProperRaffleNumber(String givenNumber, String pattern) {
+        String numberRemovedLetters = givenNumber.replaceAll(pattern, BLANK);
+        if (!numberRemovedLetters.equals(BLANK)) {
+            throw new IllegalArgumentException(ENTER_WRONG_LETTERS_MESSAGE);
+        }
     }
 
 }
