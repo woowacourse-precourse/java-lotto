8a364c2936d4cfdfde5fd29e91f78e3e83b498c3
jwjin95 <jwjin95@yonsei.ac.kr>
2022-11-14 16:34:38 +0900

    feat: random 숫자 추출 메서드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    private List<Integer> getRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+
 }
