55fa53182f1bbff480937f83cdad674294cc0dde
yeop0740 <ssucgoo@gmail.com>
2022-11-14 18:14:31 +0900

    feat(Application): 구매한 로또 번호를 모두 출력하는 기능 구현(printLottos)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -44,4 +44,12 @@ public class Application {
 
         return result;
     }
+
+    public static void printLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
 }
