087c4495b14632ce591bed95ad8ad7f0ecd64322
h0ngwon <w___on@naver.com>
2022-11-14 20:53:12 +0900

    feat(Winning) : 당첨번호 범위 검증 기능

--- src/main/java/lotto/Winning.java
+++ src/main/java/lotto/Winning.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 public class Winning {
     private static final String ERROR_MESSAGE = "[ERROR] ";
-    private static final String WINNING_OUT_OF_RANGE_ERROR = "로또 번호는 6개여야 합니다.";
+    private static final String LOTTO_OUT_OF_RANGE_ERROR = "로또 번호는 6개여야 합니다.";
     private static final String WINNING_NOT_NUMBER_ERROR = "입력 값이 숫자가 아닙니다.";
+    private static final String WINNING_NOT_IN_RANGE_ERROR = "당첨번호는 1 ~ 45 사이의 수 여야 합니다.";
 
     public Winning() {
     }
@@ -17,6 +18,7 @@ public class Winning {
 
         for (String num : numbers) {
             isValidateWinningNumber(num);
+            isValidateWinningNumberRange(num);
             winningNumbers.add(Integer.parseInt(num));
         }
 
@@ -27,7 +29,7 @@ public class Winning {
 
     public static void isValidateWinningNumberSize(List<Integer> winningNumbers) {
         if (winningNumbers.size() != 6) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + WINNING_OUT_OF_RANGE_ERROR);
+            throw new IllegalArgumentException(ERROR_MESSAGE + LOTTO_OUT_OF_RANGE_ERROR);
         }
     }
 
@@ -36,6 +38,14 @@ public class Winning {
             Integer.parseInt(num);
         }catch(NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_MESSAGE + WINNING_NOT_NUMBER_ERROR);
+        }
+    }
+
+    public static void isValidateWinningNumberRange(String num) {
+        int result = Integer.parseInt(num);
+
+        if(result < 1 || result > 45) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + WINNING_NOT_IN_RANGE_ERROR);
         }
     }
 }
