5de7b81758a31d12d7f65a5585d6d2dc8397d66f
skydreamer21 <skydreamer210@gmail.com>
2022-11-14 14:06:39 +0900

    feat(UserController): 로또 구매를 담당하는 Controller 구현
    [세부사항]
    - 구현 메서드 : buyLotteryTicket

---
+++ src/main/java/lotto/controller/UserController.java
@@ -0,0 +1,19 @@
+package lotto.controller;
+
+import lotto.domain.User;
+import lotto.service.LotterySellService;
+import lotto.service.UserService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class UserController {
+    static UserService userService = new UserService();
+    static LotterySellService lotterySellService = new LotterySellService();
+
+    public static void buyLotteryTicket() {
+        int money = InputView.takeMoneyInput();
+        User user = userService.createUser(money);
+        lotterySellService.sell(user);
+        OutputView.showPurchaseList(user.findAllLotteryNumbers());
+    }
+}
