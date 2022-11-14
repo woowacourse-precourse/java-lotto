954274ec229db5eaa604d597a8477253aead0e8e
yujaemin <woawoa0513@gmail.com>
2022-11-15 00:21:14 +0900

    feat: static 메소드로 변경

--- src/main/java/lotto/service/RewardService.java
+++ src/main/java/lotto/service/RewardService.java
@@ -2,7 +2,7 @@ package lotto.service;
 
 public class RewardService {
 
-    public double calculateRateOfReturn(int purchaseAmount, int reward) {
+    public static double calculateRateOfReturn(int purchaseAmount, int reward) {
         double rateOfReturn = ((double)reward / purchaseAmount) * 100 - 100;
         return Math.round((rateOfReturn * 10) / 10.0);
     }
