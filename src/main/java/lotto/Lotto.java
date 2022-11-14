3bca881c87a6f1de0f6c970c937ed02dc505dcac
JeongHunHui <deca3248@gmail.com>
2022-11-14 23:17:53 +0900

    feat: 로또에 중복되는 숫자가 있는지 체크하는 검증 메소드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,23 +3,28 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
-    private static final int NUMBER_COUNT = 6;
-    private final List<Integer> numbers;
+  private static final int NUMBER_COUNT = 6;
+  private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+  public Lotto(List<Integer> numbers) {
+    validate(numbers);
+    this.numbers = numbers;
+  }
+
+  private void validate(List<Integer> numbers) {
+    checkNumbersCount(numbers);
+    checkHasDuplicateNumber(numbers);
+  }
+
+  private void checkNumbersCount(List<Integer> numbers) {
+    if (numbers.size() != NUMBER_COUNT) {
+      throw new IllegalArgumentException("로또 숫자의 개수가 잘못되었습니다!");
     }
+  }
 
-    private void validate(List<Integer> numbers) {
-        checkNumbersCount();
+  private void checkHasDuplicateNumber(List<Integer> numbers) {
+    if(numbers.stream().distinct().count() != numbers.size()) {
+      throw new IllegalArgumentException("로또 숫자의 개수가 잘못되었습니다!");
     }
-
-    private void checkNumbersCount(){
-        if (numbers.size() != NUMBER_COUNT) {
-            throw new IllegalArgumentException("로또 숫자의 개수가 잘못되었습니다!");
-        }
-    }
-
-    // TODO: 추가 기능 구현
+  }
 }
