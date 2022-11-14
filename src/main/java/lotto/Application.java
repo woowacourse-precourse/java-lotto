4885d41bfe9000a0fb3c8a9a169c0acb16b9d702
JH-web115 <kimjjong321@naver.com>
2022-11-15 01:38:20 +0900

    refactor(Application): modify code for error message output

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,11 @@ import lotto.controller.ApplicationController;
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationController applicationController = new ApplicationController();
-        applicationController.startApplication();
+        try {
+            ApplicationController applicationController = new ApplicationController();
+            applicationController.startApplication();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
