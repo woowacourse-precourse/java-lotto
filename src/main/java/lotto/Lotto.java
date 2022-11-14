7205c3bc67f1637d8dd5dcc83f146fbc4ee4c9d3
dvlpr0 <rladjswkd@naver.com>
2022-11-14 17:54:14 +0900

    feat: 로또 구입 금액 입력 받는 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,7 +2,10 @@ package lotto;
 
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Lotto {
+    private static final String AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,5 +19,11 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static String readPurchaseAmount() {
+        String purchaseAmount;
+
+        System.out.println(AMOUNT_MESSAGE);
+        purchaseAmount = readLine();
+        return (purchaseAmount);
+    }
 }
