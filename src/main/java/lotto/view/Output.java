91eb5f2d843e5074965a4ec3960a19f607862f48
Beginner <dkstmdwn17@naver.com>
2022-11-14 20:28:58 +0900

    feat : 출력을 담당하는 기능을 가진 클래스를 작성하였다.
    3가지 출력문을 담당하는 메서드를 작성하였다.
    
    printNumberOfLotto() : 로또를 몇 개 구매했는지 출력한다.
    printLottoNumbers() : 로또 번호를 출력한다.
    printStaticsMessage() : 당첨 통계 메시지를 출력한다.

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,4 +1,28 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+import lotto.domain.enumeration.Rating;
+
+import java.text.NumberFormat;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 public class Output {
+
+    public void printNumberOfLotto(int lottoQuantity){
+        System.out.println(lottoQuantity + "개를 구매했습니다.");
+    }
+
+
+    public void printLottoNumbers(List<Lotto> lottoNumbers){
+        for(Lotto lotto : lottoNumbers){
+            System.out.println(lotto);
+        }
+    }
+
+    public void printStatisticsMessage(){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+
 }
