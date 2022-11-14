443a20e6a511e7688112999034bc687c0bbc35e4
yoon <beatus96@naver.com>
2022-11-14 22:15:56 +0900

    test(WinningLottos) : 로또의 당첨 등수 정하기 기능 테스트 추가
    당첨_로또 클래스 테스트 추가
    
    1. 매개변수로 넣은 로또의 당첨 등수가 올바른지 테스트
    - 당첨번호와 보너스번호를 지정하고,
    - @MethodSource("provideLottoAndRank") 사용하여,
    제공한 로또의 등수가 예상한 등수와 일치하는지 확인한다
    
    #4. 일치하지 않는 오류 발생
    - 3등(당첨번호 5개 일치,보너스번호 false)을 예상했으나, 5등(3개 일치)으로 매김

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -25,13 +25,38 @@ class WinningLottoTest {
     @ParameterizedTest
     @MethodSource("provideDuplicatedNumbersAndBonusNumber")
     void validateBonusNumberTest(List<Integer> winningNumbers, int bonusNumber) {
-        assertThatThrownBy(()->{
-            WinningLotto.validateBonusNumber(winningNumbers, bonusNumber);
-        }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->
+            WinningLotto.validateBonusNumber(winningNumbers, bonusNumber))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
 
-    //getRankOf 테스트 : 로또에 맞는 등수 얻어오는지 테스트
+    //getRankOf 테스트
+    @DisplayName("예상한 등수(enum)를 반환하는지 테스트")
+    @ParameterizedTest
+    @MethodSource("provideLottoAndRank")
+    void getRankofTest(List<Integer> lottoNumbers, Rank expected) {
+        WinningLotto winningLotto = new WinningLotto(List.of(11,12,13,14,15,16), 17);
+
+        Lotto lotto = new Lotto(lottoNumbers);
+        assertThat(winningLotto.getRankof(lotto)).isEqualTo(expected);
+    }
+
+    private static  Stream<Arguments> provideLottoAndRank() {
+        return Stream.of(
+            Arguments.of(List.of(11,12,13,14,15,16), FIRST),
+            Arguments.of(List.of(11,12,13,14,15,17), SECOND),
+            Arguments.of(List.of(11,12,13,14,15,1), THIRD),
+            Arguments.of(List.of(11,12,13,14,1,17), FOURTH), //보너스번호 같은경우
+            Arguments.of(List.of(11,12,13,14,1,2), FOURTH),
+            Arguments.of(List.of(11,12,13,1,2,17), FIFTH),
+            Arguments.of(List.of(11,12,13,1,2,3), FIFTH),
+            Arguments.of(List.of(11,12,1,2,3,17), null),
+            Arguments.of(List.of(11,1,2,3,4,17), null),
+            Arguments.of(List.of(1,2,3,4,5,17), null)
+        );
+    }
+
 
     //생성자 테스트 : 유효하지 않은 리스트 넣었을 때, 예외발생하는지 테스트 tmp파일 참고
 }
