1a42f5e48101c83ecc8af7df111711d8a615f27c
yu-heejin <jkgt0521@naver.com>
2022-11-14 14:06:17 +0900

    feat: 로또 수량 및 번호 출력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,10 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static String inputPurchaseAmount() {
@@ -10,10 +14,29 @@ public class Application {
         return amount;
     }
 
+    public static List<Integer> makeRandomNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+        return numbers;
+    }
+
+    public static List<List<Integer>> saveRandomNumbers(int count, List<List<Integer>> randomNumbers) {
+        for (int i = 1; i <= count; i++) {
+            randomNumbers.add(makeRandomNumbers());
+        }
+        return randomNumbers;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int amount = Integer.parseInt(inputPurchaseAmount());
-        System.out.println(amount);
+        int count = amount / 1000;
 
+        System.out.println(count + "개를 구매했습니다.");
+        List<List<Integer>> randomNumbers = new ArrayList<List<Integer>>();
+
+        randomNumbers = saveRandomNumbers(count, randomNumbers);
+
+        System.out.println(randomNumbers);
     }
 }
