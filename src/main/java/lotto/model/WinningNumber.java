c7fd7cbd68c634910387901c86f801da5886b190
Lee-Jiseung <ljss98@naver.com>
2022-11-14 23:58:02 +0900

    feat: 당첨 번호 입력을 List로 전환

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -3,11 +3,23 @@ package lotto.model;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.validation.InputValidation;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class WinningNumber {
     private final InputValidation inputValidation = new InputValidation();
+    private List<Integer> winningNumbers;
 
     public void inputWinningNumbers() {
         String numbers = Console.readLine();
         inputValidation.validateWinningNumbers(numbers);
+        winningNumbers = num2list(numbers);
+    }
+
+    private List<Integer> num2list(String numbers) {
+        return Arrays.stream(numbers.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
