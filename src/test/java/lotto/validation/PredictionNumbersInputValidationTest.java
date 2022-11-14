3965e4cf586f48f9027d446d9c147cb518d08de4
desfox <desfox99@naver.com>
2022-11-15 03:14:57 +0900

    test(PredictionNumbersInputValidationTest): 예측 번호 중 중복값 없는지 검증하는 로직 테스트

--- src/test/java/lotto/validation/PredictionNumbersInputValidationTest.java
+++ src/test/java/lotto/validation/PredictionNumbersInputValidationTest.java
@@ -1,8 +1,12 @@
 package lotto.validation;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -18,6 +22,13 @@ public class PredictionNumbersInputValidationTest {
     @Test
     void validateInputFormatTest() {
         assertThatThrownBy(() -> predictionNumbersInputValidation.validateInputFormat("123456"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("에측 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void validateNoDuplicateNumbersTest() {
+        assertThatThrownBy(() -> predictionNumbersInputValidation.validateNoDuplicateNumbers(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
