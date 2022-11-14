1b59499fab2b13e97359cb8a8824935133e6ec53
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-15 01:07:32 +0900

    feat(InputNumber) : add a function that parse userInput about bonus number to integer list

--- src/main/java/controller/InputNumber.java
+++ src/main/java/controller/InputNumber.java
@@ -5,12 +5,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputNumber {
-    public List<Integer> parseWinningNumber(String userInput){
+    public List<Integer> parseWinningNumber(String userInput) {
         List<String> parsedString = new ArrayList<>(Arrays.asList(userInput.split("\\s*,\\s*")));
         List<Integer> winningNumber = new ArrayList<>();
-        for(String parsed : parsedString){
+        for (String parsed : parsedString) {
             winningNumber.add(Integer.parseInt(parsed));
         }
         return winningNumber;
+    }
+
+    public List<Integer> parseBonusNumber(String userInput){
+        List<Integer> bonusNumber = new ArrayList<>();
+        bonusNumber.add(Integer.parseInt(userInput));
+        return bonusNumber;
     }
 }
