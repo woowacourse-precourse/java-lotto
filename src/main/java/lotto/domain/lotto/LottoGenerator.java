a97ed24743dbc56a13342eecff3ab1012f7176d2
aksmf1442 <aksmf1442@naver.com>
2022-11-14 15:51:53 +0900

    feat(로또): 로또 객체가 가진 숫자 6개를 오름차순하는 기능 구현

--- src/main/java/lotto/domain/lotto/LottoGenerator.java
+++ src/main/java/lotto/domain/lotto/LottoGenerator.java
@@ -2,11 +2,13 @@ package lotto.domain.lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoGenerator {
 
     public static Lotto createLottoWithRandomNumbers() {
         List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        return new Lotto(randomNumbers);
+        List<Integer> sortedNumbers = randomNumbers.stream().sorted().collect(Collectors.toList());
+        return new Lotto(sortedNumbers);
     }
 }
