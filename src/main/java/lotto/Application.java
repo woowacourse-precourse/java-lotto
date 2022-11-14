fe367e007b1a96fc1fb418febba07bfbdeec0b9c
yeop0740 <ssucgoo@gmail.com>
2022-11-14 10:30:41 +0900

    feat(Application): 잘못된 금액 입력시 에러 출력 기능 구현(validate)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        validate(input);
+        int money = Integer.parseInt(input);
+
+    }
+
+    public static void validate(String input) {
+        int money = -1;
+        try {
+            money = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR]");
+            new IllegalArgumentException("[ERROR]");
+        }
+
+        if (money % 1000 != 0) {
+            System.out.println("[ERROR]");
+            new IllegalArgumentException("[ERROR]");
+        }
     }
 }
