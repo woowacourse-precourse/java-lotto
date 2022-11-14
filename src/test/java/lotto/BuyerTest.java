f42d32d548adebad1e6b2e03a5654a07eafca2b0
jjdone <ehdlfaos@gmail.com>
2022-11-14 22:28:08 +0900

    test: Buyer에서의 테스트 생성

--- src/test/java/lotto/BuyerTest.java
+++ src/test/java/lotto/BuyerTest.java
@@ -1,4 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class BuyerTest {
-  
+    private static Buyer buyer = new Buyer();
+
+    @DisplayName("1000 단위가 아니다")
+    @Test
+    void 단위_테스트() {
+        assertThatThrownBy(() -> {
+            buyer.countCalculation(1100);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("공백을 입력했을 때")
+    @Test
+    void 공백_테스트() {
+        assertThrows(IllegalArgumentException.class,
+                () -> buyer.setListOfLotto(""));
+    }
 }
