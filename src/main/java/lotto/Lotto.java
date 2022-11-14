12c04c0618bcedaaf255d08dae9f2c2453ed0034
chaos0103 <lyt1228@naver.com>
2022-11-14 17:00:00 +0900

    refactor(Lotto): 숫자 중복 검증 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,6 +7,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -21,4 +22,9 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void duplicate(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
