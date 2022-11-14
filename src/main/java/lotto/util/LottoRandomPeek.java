bbf8ea8352793d6aa9fe27af24da6d15b7e642f2
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-14 23:04:11 +0900

    feat: drawLotto 메소드 작성

--- src/main/java/lotto/util/LottoRandomPeek.java
+++ src/main/java/lotto/util/LottoRandomPeek.java
@@ -1,10 +1,22 @@
 package lotto.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
+import lotto.enums.IntEnum;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Randoms.*;
+import static lotto.enums.IntEnum.*;
+
 public class LottoRandomPeek {
     private final List<Lotto> randomLottos = new ArrayList<>();
+
+    public void drawLotto() {
+        List<Integer> randomNumbers = pickUniqueNumbersInRange(
+                START_NUMBER.getValue(), LAST_NUMBER.getValue(), LOTTO_SIZE.getValue());
+        randomLottos.add(new Lotto(randomNumbers));
+    }
+
 }
