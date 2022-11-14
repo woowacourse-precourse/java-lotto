5c0dd433d56eba26a69e36545edf061cea04a5da
sdsr <dlxodnr1212@gmail.com>
2022-11-14 15:33:17 +0900

    fix: error message 수정

--- src/main/java/lotto/exception/CheckMoneyCountException.java
+++ src/main/java/lotto/exception/CheckMoneyCountException.java
@@ -1,7 +1,10 @@
 package lotto.exception;
 
 public class CheckMoneyCountException extends IllegalArgumentException {
+    private static final String ERROR_MESSAGE = "[ERROR] 1000원 단위의 숫자를 입력해 주세요";
+
     public CheckMoneyCountException() {
-        super("[ERROR] : 1000원 단위의 숫자를 입력해 주세요");
+        super(ERROR_MESSAGE);
+        System.out.println(ERROR_MESSAGE);
     }
 }
--- src/main/java/lotto/exception/CheckThisNumException.java
+++ src/main/java/lotto/exception/CheckThisNumException.java
@@ -1,7 +1,10 @@
 package lotto.exception;
 
-public class CheckThisNumException extends NumberFormatException {
+public class CheckThisNumException extends IllegalArgumentException {
+    private static final String ERROR_MESSAGE = "[ERROR] 숫자를 입력해 주세요";
+
     public CheckThisNumException() {
-        super("[ERROR] : 숫자를 입력해 주세요");
+        super(ERROR_MESSAGE);
+        System.out.println(ERROR_MESSAGE);
     }
 }
