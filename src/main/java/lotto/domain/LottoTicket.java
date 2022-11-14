a6e8cc5fc1931be440547bd513cd65a6bcb83a30
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-15 06:50:17 +0900

    refactor: 로또 티켓 수에 따른 로또 번호 생성 메소드 제거
    로또 한 게임의 수를 받은 후 Controller에서 여러개를 생성할 수 있도록 수정

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -7,32 +7,20 @@ import java.util.List;
 public class LottoTicket {
 
 
-    private int LOTTO_MIN_NUMBER = 1;
-    private int LOTTO_MAX_NUMBER = 45;
-    private int LOTTO_SIZE = 6;
-    private List<List<Integer>> allGameLottoNumbers;
-    private List<Integer> oneGameLottoNumbers;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
     private List<Integer> lottoNumbers;
     private int lottoTickets;
 
 
-    private LottoTicket(List<List<Integer>> allGameLottoNumbers, List<Integer> oneGameLottoNumbers, List<Integer> lottoNumbers, int lottoTickets) {
-        this.allGameLottoNumbers = allGameLottoNumbers;
-        this.oneGameLottoNumbers = oneGameLottoNumbers;
+    private LottoTicket(List<Integer> lottoNumbers, int lottoTickets) {
         this.lottoNumbers = lottoNumbers;
         this.lottoTickets = lottoTickets;
     }
 
-    public List<List<Integer>> generateAllGameLottoNumber(int lottoTickets) {
-        for (int i = 0; i < lottoTickets; i++) {
-            allGameLottoNumbers.add(createLottoOneGameNumber());
-        }
-        return allGameLottoNumbers;
-    }
-
-    private List<Integer> createLottoOneGameNumber() {
-        List<Integer> oneGameNumbers = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_SIZE);
-        return oneGameNumbers;
+    public static List<Integer> createLottoOneGameNumber() {
+        return Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_SIZE);
     }
 
 }
