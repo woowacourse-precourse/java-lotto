72fc423d1f20bb1253eefe13d935d0b2f9d422df
heesang <dlagmltkd12@naver.com>
2022-11-15 00:10:51 +0900

    test: LottoTest 내 compare 테스트 로직 AnswerTest로 이동

--- src/test/java/lotto/domain/AnswerTest.java
+++ src/test/java/lotto/domain/AnswerTest.java
@@ -13,6 +13,66 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AnswerTest {
 
+    @DisplayName("1개의 로또와 당첨 번호를 비교하여 1등 결과를 반환하는 기능")
+    @Test
+    void calculateAnswerFirst() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Answer answer = new Answer(List.of(6, 5, 4, 3, 2, 1), 7);
+
+        LottoResult lottoResult = answer.compare(lotto);
+        assertThat(lottoResult.isFirst()).isTrue();
+    }
+
+    @DisplayName("1개의 로또와 당첨 번호를 비교하여 2등 결과를 반환하는 기능")
+    @Test
+    void calculateAnswerSecond() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        Answer answer = new Answer(List.of(6, 5, 4, 3, 2, 1), 7);
+
+        LottoResult lottoResult = answer.compare(lotto);
+        assertThat(lottoResult.isSecond()).isTrue();
+    }
+
+    @DisplayName("1개의 로또와 당첨 번호를 비교하여 3등 결과를 반환하는 기능")
+    @Test
+    void calculateAnswerThird() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
+        Answer answer = new Answer(List.of(6, 5, 4, 3, 2, 1), 7);
+
+        LottoResult lottoResult = answer.compare(lotto);
+        assertThat(lottoResult.isThird()).isTrue();
+    }
+
+    @DisplayName("1개의 로또와 당첨 번호를 비교하여 4등 결과를 반환하는 기능")
+    @Test
+    void calculateAnswerFourth() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 7, 8));
+        Answer answer = new Answer(List.of(6, 5, 4, 3, 2, 1), 7);
+
+        LottoResult lottoResult = answer.compare(lotto);
+        assertThat(lottoResult.isFourth()).isTrue();
+    }
+
+    @DisplayName("1개의 로또와 당첨 번호를 비교하여 5등 결과를 반환하는 기능")
+    @Test
+    void calculateAnswerFifth() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 8, 9, 7));
+        Answer answer = new Answer(List.of(6, 5, 4, 3, 2, 1), 7);
+
+        LottoResult lottoResult = answer.compare(lotto);
+        assertThat(lottoResult.isFifth()).isTrue();
+    }
+
+    @DisplayName("1개의 로또와 당첨 번호를 비교하여 이외의 결과를 반환하는 기능")
+    @Test
+    void calculateAnswerElse() {
+        Lotto lotto = new Lotto(List.of(7, 8, 9, 10, 11, 12));
+        Answer answer = new Answer(List.of(6, 5, 4, 3, 2, 1), 7);
+
+        LottoResult lottoResult = answer.compare(lotto);
+        assertThat(lottoResult.isElse()).isTrue();
+    }
+
     @MethodSource("provideLottosAnswer")
     @ParameterizedTest
     void calculateLottosAnswer(int index, LottoResult lottoResult) {
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -13,66 +13,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTest {
 
-    @DisplayName("1개의 로또와 당첨 번호를 비교하여 1등 결과를 반환하는 기능")
-    @Test
-    void calculateAnswerFirst() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        Answer answer = new Answer(List.of(6, 5, 4, 3, 2, 1), 7);
-
-        LottoResult lottoResult = answer.compare(lotto);
-        assertThat(lottoResult.isFirst()).isTrue();
-    }
-
-    @DisplayName("1개의 로또와 당첨 번호를 비교하여 2등 결과를 반환하는 기능")
-    @Test
-    void calculateAnswerSecond() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
-        Answer answer = new Answer(List.of(6, 5, 4, 3, 2, 1), 7);
-
-        LottoResult lottoResult = answer.compare(lotto);
-        assertThat(lottoResult.isSecond()).isTrue();
-    }
-
-    @DisplayName("1개의 로또와 당첨 번호를 비교하여 3등 결과를 반환하는 기능")
-    @Test
-    void calculateAnswerThird() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
-        Answer answer = new Answer(List.of(6, 5, 4, 3, 2, 1), 7);
-
-        LottoResult lottoResult = answer.compare(lotto);
-        assertThat(lottoResult.isThird()).isTrue();
-    }
-
-    @DisplayName("1개의 로또와 당첨 번호를 비교하여 4등 결과를 반환하는 기능")
-    @Test
-    void calculateAnswerFourth() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 7, 8));
-        Answer answer = new Answer(List.of(6, 5, 4, 3, 2, 1), 7);
-
-        LottoResult lottoResult = answer.compare(lotto);
-        assertThat(lottoResult.isFourth()).isTrue();
-    }
-
-    @DisplayName("1개의 로또와 당첨 번호를 비교하여 5등 결과를 반환하는 기능")
-    @Test
-    void calculateAnswerFifth() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 8, 9, 7));
-        Answer answer = new Answer(List.of(6, 5, 4, 3, 2, 1), 7);
-
-        LottoResult lottoResult = answer.compare(lotto);
-        assertThat(lottoResult.isFifth()).isTrue();
-    }
-
-    @DisplayName("1개의 로또와 당첨 번호를 비교하여 이외의 결과를 반환하는 기능")
-    @Test
-    void calculateAnswerElse() {
-        Lotto lotto = new Lotto(List.of(7, 8, 9, 10, 11, 12));
-        Answer answer = new Answer(List.of(6, 5, 4, 3, 2, 1), 7);
-
-        LottoResult lottoResult = answer.compare(lotto);
-        assertThat(lottoResult.isElse()).isTrue();
-    }
-
     @DisplayName("한개의 로또를 오름차순으로 정렬하는 기능")
     @CsvSource(value = {"0:4", "1:7", "2:9", "3:10", "4:12", "5:14"}, delimiter = ':')
     @ParameterizedTest
