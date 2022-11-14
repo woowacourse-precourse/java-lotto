b22b6aad0ad38492e9077dd3c9a22b57f082a234
choyeram <choyeram8@gmail.com>
2022-11-14 16:03:39 +0900

    feat: 예외 처리 try~catch문 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,10 @@ import lotto.controller.LottoController;
 public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
-        lottoController.init();
+        try {
+            lottoController.init();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
