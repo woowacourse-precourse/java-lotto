711453c5f93bf9e9c3f07f92245269706269a0ac
wwwndus <simock22@gmail.com>
2022-11-14 23:41:58 +0900

    feat(Computer): 1부터 45까지의 서로 다른 임의의 수 6개를 생성하여 리스트로 저장

--- src/main/java/lotto/model/Computer.java
+++ src/main/java/lotto/model/Computer.java
@@ -1,4 +1,25 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Computer {
+
+    private List<List<Integer>> randomNumbers;
+
+    public List<List<Integer>> createRandomNumbers(int inputMoney) {
+
+        List<List<Integer>> computerRandomNumbers = new ArrayList<>();
+
+        for (int i = 0; i < (inputMoney/1000); i++) {
+            List<Integer> randomNumbers = new ArrayList<>();
+            randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            computerRandomNumbers.add(randomNumbers);
+        }
+        return computerRandomNumbers;
+    }
 }
