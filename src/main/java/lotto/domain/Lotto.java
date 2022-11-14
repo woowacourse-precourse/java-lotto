e1be795a15e82c7d95db0ce761dd2bfd3b9db52c
ohhonggi <5ghdrl@naver.com>
2022-11-15 05:43:26 +0900

    feat($Lotto): Add validateDuplication function
    This function checks for duplicate numbers.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,12 +3,14 @@ package lotto.domain;
 import java.util.List;
 
 import static lotto.constants.ErrorCode.INVALID_NUMBER_OF_DIGITS;
+import static lotto.constants.ErrorCode.INVALID_NUMERIC_CONFIGURATION;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplication(numbers);
         this.numbers = numbers;
     }
 
@@ -18,6 +20,14 @@ public class Lotto {
         }
     }
 
+    private void validateDuplication(List<Integer> numbers) {
+        for (int i = 0; i<numbers.size(); i++) {
+            List<Integer> subList = numbers.subList(i+1, numbers.size());
+            if (subList.contains(numbers.get(i))) {
+                throw new IllegalArgumentException(INVALID_NUMERIC_CONFIGURATION);
+            }
+        }
+    }
 
     // TODO: 추가 기능 구현
     @Override
