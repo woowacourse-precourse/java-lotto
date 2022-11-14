dbfa6e9c4678442057fa8338382e3222270f2105
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-11-15 03:22:41 +0900

    feat: 구입 금액만큼 로또를 발행하는 기능 추가 purchaseAmount의 크기만큼 로또를 발행하여 Lotto 타입 리스트에 담아 반환하는 issueLotto() 메소드를 구현합니다.

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -2,11 +2,22 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGame {
     public List<Integer> generateLottoNumbers() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return lottoNumbers;
+    }
+
+    public List<Lotto> issueLotto(int purchaseAmount) {
+        List<Lotto> issuedLotto = new ArrayList<Lotto>(purchaseAmount);
+        for(int i=0; i<purchaseAmount; i++) {
+            List<Integer> newLottoNumbers = generateLottoNumbers();
+            Lotto newLotto = new Lotto(newLottoNumbers);
+            issuedLotto.add(newLotto);
+        }
+        return issuedLotto;
     }
 }
