2a142d5081981bbf44cc6313c31cd89e189ace0c
정승구 <jeongseung-gu@jeongseung-guui-MacBookAir.local>
2022-11-14 21:21:58 +0900

    fix: lotto 주석 제거

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,9 +4,11 @@ import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -19,10 +21,19 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("생성한 로또 번호를 가져올 때 문제없이 반환되는지 테스트.")
+    @Test
+    void checkMethodRetunValue() {
+        List<Integer> input = List.of(1,2,3,4,5,6);
+        Lotto lotto = new Lotto(input);
+
+        List<Integer> expected = input;
+        List<Integer> actual = lotto.getLotto();
+
+        assertEquals(expected, actual);
+    }
 }
