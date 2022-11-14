1ff2c24892bf82340f1185fd44a51060e3147616
Hyunstone <hyun104467@naver.com>
2022-11-14 23:11:21 +0900

    feat(lotto): 메인메소드 사용자입력 파트 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,21 @@
 package lotto;
 
+import lotto.domain.LottoGenerator;
+import lotto.domain.WinningNumber;
+
 public class Application {
     public static void main(String[] args) {
+        LottoGenerator lottoGenerator;
+        WinningNumber winningNumber;
 
+        System.out.println("구입금액을 입력해주세요.");
+        lottoGenerator = new LottoGenerator(Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine()));
 
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winNumber = camp.nextstep.edu.missionutils.Console.readLine();
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusNumber = camp.nextstep.edu.missionutils.Console.readLine();
+        
+        winningNumber = new WinningNumber(winNumber, Integer.valueOf(bonusNumber));
     }
 }
