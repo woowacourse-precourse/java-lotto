c002c27dbd0e4aa803d6af474477f92333d793cc
alswn <alswn9938@gmail.com>
2022-11-14 21:03:49 +0900

    style(Lotto, Lottos): Ranking 관련 함수 Lotto, Lottos에 종속
    일급컬렉션(Lotto, Lottos)가 wrapping하고 있는 컬렉션의 내용 외부에 노출되지 않도록
    일급 컬렉션 내부에
    Ranking 로직 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -27,4 +27,58 @@ public class Lotto {
     public boolean contains(int number){
         return numbers.contains(number);
     }
+
+    public RankingType getRankingType(WinningLotto winningLotto){
+        int matchingPoint = getMatchingPoint(winningLotto);
+
+        RankingType rankingTypeCandidate =
+                getRankingTypeCandidate(matchingPoint);
+
+        if(rankingTypeCandidate == RankingType.SECOND || rankingTypeCandidate == RankingType.THIRD){
+            rankingTypeCandidate = determineSecondOrThird(winningLotto);
+        }
+
+        return rankingTypeCandidate;
+    }
+
+    private int getMatchingPoint(WinningLotto winningNumber) {
+        int matchingPoint = 0;
+        for (Integer number : numbers) {
+            matchingPoint += getExtraMatchingPoint(number, winningNumber);
+        }
+        return matchingPoint;
+    }
+
+
+    private int getExtraMatchingPoint(int number, WinningLotto winningLotto){
+        if(winningLotto.contains(number)){
+            return 1;
+        }
+        return 0;
+    }
+
+    private static RankingType getRankingTypeCandidate(int matchingPoint) {
+        for (RankingType rankingType : RankingType.values()) {
+            if(matchingPoint == rankingType.getCondition()){
+                return rankingType;
+            }
+        }
+        return RankingType.DROP_OUT;
+    }
+
+    private RankingType determineSecondOrThird(WinningLotto winningLotto) {
+        if(isMatchWithBonus(winningLotto)){
+            return RankingType.SECOND;
+        }
+        return RankingType.THIRD;
+    }
+
+    private boolean isMatchWithBonus(WinningLotto winningLotto){
+        for (Integer number : numbers) {
+            if(winningLotto.isBonusEqualTo(number)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -6,14 +6,13 @@ import java.util.List;
 import java.util.Map;
 
 public class Lottos {
-    Ranking ranking = new Ranking();
     private final List<Lotto> lottos;
 
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
     }
 
-    public String getLottosString(){
+    public String getLottosString() {
         StringBuilder sb = new StringBuilder();
         for (Lotto lotto : lottos) {
             sb.append(lotto.getNumbersString());
@@ -22,30 +21,39 @@ public class Lottos {
         return sb.toString().trim();
     }
 
-    private List<RankingType> getLottosRanking(WinningLotto winningNumber){
-        List<RankingType> rankings = new ArrayList<>();
-        for (Lotto lotto : lottos) {
-            RankingType ranking = this.ranking.getRanking(lotto, winningNumber);
-            rankings.add(ranking);
-        }
-        return rankings;
+    public Map<RankingType, Integer> getRankingTypeCounts(WinningLotto winningNumber) {
+        Map<RankingType, Integer> rankingTypeCounts = getInitRankingTypeCounts();
+
+        List<RankingType> lottosRankingTypes = getLottosRankingTypes(winningNumber);
+
+        updateRankingTypeCounts(rankingTypeCounts, lottosRankingTypes);
+        return rankingTypeCounts;
     }
 
-    public Map<RankingType, Integer> getRankingTypeCounts(WinningLotto winningNumber){
+    private Map<RankingType, Integer> getInitRankingTypeCounts() {
         Map<RankingType, Integer> rankingTypeCounts = new HashMap<>();
-        initCountByRankingType(rankingTypeCounts);
-        List<RankingType> lottosRanking = getLottosRanking(winningNumber);
-        for (RankingType rankingType : lottosRanking) {
-            int beforeCount = rankingTypeCounts.get(rankingType);
-            rankingTypeCounts.put(rankingType, beforeCount + 1);
+        RankingType[] values = RankingType.values();
+        for (RankingType value : values) {
+            rankingTypeCounts.put(value, 0);
         }
         return rankingTypeCounts;
     }
 
-    private void initCountByRankingType(Map<RankingType, Integer> map){
-        RankingType[] values = RankingType.values();
-        for (RankingType value : values) {
-            map.put(value, 0);
+    private List<RankingType> getLottosRankingTypes(WinningLotto winningNumber) {
+        List<RankingType> lottosRankingTypes = new ArrayList<>();
+
+        for (Lotto lotto : lottos) {
+            RankingType rankingType = lotto.getRankingType(winningNumber);
+            lottosRankingTypes.add(rankingType);
+        }
+        return lottosRankingTypes;
+    }
+
+    private static void updateRankingTypeCounts(Map<RankingType, Integer> rankingTypeCounts,
+                                                List<RankingType> lottosRankingTypes) {
+        for (RankingType rankingType : lottosRankingTypes) {
+            int originCount = rankingTypeCounts.get(rankingType);
+            rankingTypeCounts.put(rankingType, originCount + 1);
         }
     }
 }
