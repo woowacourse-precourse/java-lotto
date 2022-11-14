3e1410ef13bfdcdf8d69720d939034d7c14031c1
hyunho12 <hyundho12@gmail.com>
2022-11-14 20:03:57 +0900

    feat: 로또번호 자동생성기능 완성

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -1,18 +1,35 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoNumber {
-    private int lottoNumber;
-
+    private int lottoAmount;
+    private List<Lotto> lottoNumber = new ArrayList<>();
 
     public void LottoBuyPriceInput(){
         System.out.println("구입금액을 입력해 주세요.");
         String buyPrice = Console.readLine();
+        System.out.println();
         int lottoNum = Integer.parseInt(buyPrice) / 1000;
-        lottoNumber = lottoNum;
+        lottoAmount = lottoNum;
         System.out.println(lottoNum + "개를 구매했습니다.");
     }
 
+    public void InputLottoNumber(){
+        for(int i = 0; i < lottoAmount; i++){
+            lottoNumber.add(autoGenerate());
+        }
+        for(Lotto lotto : lottoNumber){
+            System.out.println(lotto.getNumbers());
+        }
+    }
 
+    private Lotto autoGenerate(){
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1,45,6));
+        return new Lotto(numbers);
+    }
 }
