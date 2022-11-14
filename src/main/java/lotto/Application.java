f7caf6810197f2185bebb46145176f5962f22a89
TaeHwan <kimta2hwan@gmail.com>
2022-11-14 19:13:13 +0900

    refactor: 실행 결과 예시에 맞춰 출력 형식 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.domain.PurchaseMoney;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView.printPurchaseGuideMessage();
+        PurchaseMoney purchaseMoney = InputView.inputPurchaseMoney();
+
+        OutputView.printPurchaseResultMessage(purchaseMoney.getLottoCount());
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,9 +5,9 @@ import java.util.List;
 public class OutputView {
 
     private static final String PURCHASE_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String PURCHASE_RESULT_MESSAGE = "%d개를 구매했습니다.\n";
-    private static final String WINNING_NUMBERS_ENTERING_GUIDE_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String BONUS_NUMBER_ENTERING_GUIDE_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String PURCHASE_RESULT_MESSAGE = "\n%d개를 구매했습니다.\n";
+    private static final String WINNING_NUMBERS_ENTERING_GUIDE_MESSAGE = "\n당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_ENTERING_GUIDE_MESSAGE = "\n보너스 번호를 입력해 주세요.";
     private static final String YIELD_MESSAGE = "총 수익률은 %.1f%입니다.\n";
 
     public static void printPurchaseGuideMessage() {
