c59a92d36317a97d354d9a397f8345ae1e5cf607
ShinKwang <sklee0206@naver.com>
2022-11-14 17:57:03 +0900

    refactor: Divide feature to validation for size of lottery numbers
    복권은 6개의 번호를 가지고 있어야 한다. 따라서 numbers의 size가 6개인지 확인한다. 이전의 validate메소드의 해당 기능을 담으면 너무 추상적이다. 따라서 validateSize 메소드로 분리해 세분화한다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,10 +11,13 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        validateSize(numbers);
     }
 
     // TODO: 추가 기능 구현
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 개수는 6개여야 합니다.");
+        }
+    }
 }
