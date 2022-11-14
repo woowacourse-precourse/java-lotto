f508c0911c501e85c1faa62e3e8b57401380341e
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-14 17:51:27 +0900

    test: 로또 전반적인 예외 사항 테스트
    로또 전반적인 예외 사항 테스트
    - 기능
    - 범위
    - 구분자
    - 단위
    - 개수

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -47,9 +47,69 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 기능_테스트_2(){
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("5000", "10,13,17,27,40,45", "7");
+                    assertThat(output()).contains(
+                            "5개를 구매했습니다.",
+                            "[7, 17, 27, 37, 40, 45]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[10, 13, 17, 27, 42, 45]",
+                            "3개 일치 (5,000원) - 0개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 1개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 630000.0%입니다."
+                    );
+                },
+                List.of(7, 17, 27, 37, 40, 45),
+                List.of(5, 3, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 10, 17, 42, 27, 45)
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
             runException("1000j");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 범위_벗어나는_숫자_예외_테스트(){
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,33,44,55,66", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 구분자_쉼표가_아닌_경우_예외_테스트(){
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3.4.5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 천원_단위가_아닌_경우_예외_테스트(){
+        assertSimpleTest(() -> {
+            runException("1234");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 당첨번호_개수가_6개가_아닌_경우_예외_테스트(){
+        assertSimpleTest(() -> {
+            runException("2000", "1,2,3", "6");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
