4288f4c600d8887f9d0367c4156bdb68c620fd06
Dasol Jung <greteljds0251@gmail.com>
2022-11-14 19:20:33 +0900

    feat: Get purchase amount

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Money {
+    public String getAmount() {
+        String amount;
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(ir);
+        try {
+            amount = br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return amount;
+    }
+}
