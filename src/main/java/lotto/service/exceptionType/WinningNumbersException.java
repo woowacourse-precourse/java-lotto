59cefb99d97feddf278d6817835696c41dcf8583
jjmin9797 <jjmin9797@naver.com>
2022-11-14 19:22:37 +0900

    feat(WinningNumbersException): 중복된 번호가 있는지 확인하는 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/service/exceptionType/WinningNumbersException.java
+++ src/main/java/lotto/service/exceptionType/WinningNumbersException.java
@@ -1,10 +1,13 @@
 package lotto.service.exceptionType;
 
+import java.util.Arrays;
+
 public class WinningNumbersException {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
     private static final String EXCEPTION_WITH_NOT_SIX_ITEMS_MESSAGE = " \',\'를 기준으로 6개의 항목을 입력해야합니다.";
     private static final String BLANK_EXCEPTION_MESSAGE = " 당첨 번호에 공백은 존재할 수 없습니다.";
+    private static final String DUPLICATE_NUMBER_EXCEPTION_MESSAGE = " 중복된 당첨 번호가 존재합니다.";
     public void checkIfTheWinningNumbersHasSixCharactersBasedOnTheConma(String winningNumbers) {
         String[] numbers = winningNumbers.split(",");
         if (numbers.length != 6) {
@@ -20,4 +23,11 @@ public class WinningNumbersException {
         }
     }
 
+    public static void checkForDupliCateWinningNumbers(String winningNumbers) {
+        String[] numbers = winningNumbers.split(",");
+        if (numbers.length != Arrays.stream(numbers).distinct().count()){
+            System.out.println(ERROR_MESSAGE + DUPLICATE_NUMBER_EXCEPTION_MESSAGE);
+            throw new IllegalArgumentException();
+        }
+    }
 }
