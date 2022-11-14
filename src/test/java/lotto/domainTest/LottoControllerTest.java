c1352582eeadd83a194e369e33745a313d536dc3
lee <whddlsk123@naver.com>
2022-11-14 14:51:42 +0900

    test(lottoNumbericTest) : 숫자가 아닌 값을 입력한경우 예외가 발생하는지 테스트 추가

--- src/test/java/lotto/domainTest/LottoControllerTest.java
+++ src/test/java/lotto/domainTest/LottoControllerTest.java
@@ -2,6 +2,7 @@ package lotto.domainTest;
 
 import lotto.LottoController;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -11,17 +12,20 @@ public class LottoControllerTest {
     LottoController controller = new LottoController();
 
     @Test
-    void initLotto() {
+    @DisplayName("입력한 당첨번호가 숫자가 아닌경우")
+    void lottoNumericTest() {
         //given(준비)
-        String input = "1,2,3,4,5,6";
+        String input = "1,2,3,4,A,6";
         InputStream in = new ByteArrayInputStream(input.getBytes());
 
         //when(실행)
-        System.setIn(in);
-        controller.initLotto();
 
         //then(검증)
-        Assertions.assertThat(controller.getLotto().getNumbers()).containsExactly(1,2,3,4,5,6);
+        try {
+            System.setIn(in);
+            controller.initLotto();
+        } catch (Exception e) {
+            Assertions.assertThat(e.getMessage()).contains("[ERROR]");
+        }
     }
-
 }
