32fc7f27c5d56f9b1b78910cd7ca927f28dd91a9
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-14 18:00:35 +0900

    feat(ConstantLottoProgram) : 기능을 세분화.

--- src/main/java/lotto/ResultCalculate.java
+++ src/main/java/lotto/ResultCalculate.java
@@ -5,11 +5,20 @@ import java.util.List;
 import constant.LottoRanking;
 public class ResultCalculate {
     public static void resultCalculate(List<Lotto> purchaseLottos, List<Integer> drawLotto ){
+        synthesizeRanking(purchaseLottos,drawLotto);
+
+    }
+    public static List<Integer> synthesizeRanking(List<Lotto> purchaseLottos, List<Integer> drawLotto){
         List<Integer> ranks = new LinkedList<>();
+        for(int i=0; i<6; i++){
+            ranks.add(0);
+        }
         for(Lotto lotto :purchaseLottos){
             int sameNumberCount = compareTwoNumbers(lotto, drawLotto);
-            ranks.set(sameNumberCount,ranks.get(sameNumberCount)+1); // List의 초기화가 필요한지.
+            if(sameNumberCount != -1)
+                ranks.set(sameNumberCount,ranks.get(sameNumberCount)+1);
         }
+        return ranks;
     }
     public static int compareTwoNumbers(Lotto lotto, List<Integer> drawLotto){
         int sameNumberCount = 7;
@@ -18,6 +27,12 @@ public class ResultCalculate {
                 sameNumberCount--;
             }
         }
+        if(sameNumberCount>=5) return -1;
+        return calculateRanking(lotto, drawLotto,sameNumberCount);
+    }
+    private static int calculateRanking(Lotto lotto, List<Integer> drawLotto, int sameNumberCount){
+        if(sameNumberCount==1)
+            return LottoRanking.FIRST.getRanking();
         if(sameNumberCount==2){
             if(lotto.getNumbers().contains(drawLotto.get(6))) {
                 return LottoRanking.SECOND.getRanking();
@@ -26,7 +41,7 @@ public class ResultCalculate {
         }
         if(sameNumberCount==3)
             return LottoRanking.FOURTH.getRanking();
-        return sameNumberCount;
+        return LottoRanking.FIFTH.getRanking();
     }
     public static void profitCalculate(){
 
