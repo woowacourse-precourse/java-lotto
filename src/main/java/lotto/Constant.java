f0aacb6d8b7a60b9a7e9561e05d7f418b7d37f26
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-11-15 03:45:30 +0900

    feat (UI) : 첫 출력문과 사용자 로또번호 출력

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -2,4 +2,5 @@ package lotto;
 
 public class Constant {
     public static final Integer LOTTO_NUMBER_SIZE = 6;
+    public static final String MESSAGE_INIT_LOTTO = "구입금액을 입력해 주세요.";
 }
--- src/main/java/lotto/UI.java
+++ src/main/java/lotto/UI.java
@@ -1,15 +1,27 @@
 package lotto;
 
+import static lotto.Constant.*;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class UI {
     public static List<Lotto> getUserNumbers() {
+        System.out.println(MESSAGE_INIT_LOTTO);
         String money = Console.readLine();
         User user = new User(money);
         return user.getUserLotto();
+    }
+
+    public static void printUserLotto(List<Lotto> userNumbers) {
+        for(Lotto lotto : userNumbers) {
+            List<Integer> tempNumbers = lotto.getNumbers();
+            Collections.sort(tempNumbers);
+            System.out.println(tempNumbers);
+        }
     }
 
     public static Lotto getLottoNumbers() {
