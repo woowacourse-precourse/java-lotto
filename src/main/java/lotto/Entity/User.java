c999ce4f3fb882d742560cb747820b01eed1d388
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-11-14 16:13:33 +0900

    add : 실질적 로또를 구매한 객체로써, Lotto, 구매할 금액, 구매한 로또 개수를 필드로 가짐

---
+++ src/main/java/lotto/Entity/User.java
@@ -0,0 +1,14 @@
+package lotto.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private static final int MONEY_UNIT = 1000;
+
+    private List<Lotto> lottos;
+    private int money;
+    private int buyCount;
+
+
+}
