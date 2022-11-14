033fbfa15d66b6e4a85eb090196352ce5c1049c1
kimilchul <huj0159@gmail.com>
2022-11-14 11:53:40 +0900

    refactor: seperate winNumber code into independent class

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 
 public class LottoController {
 
-    public int readUserMoney(String userMoney) {
+    public Integer readUserMoney(String userMoney) {
         isDigit(userMoney);
         validateUserMoney(userMoney);
 
@@ -28,32 +28,10 @@ public class LottoController {
     }
 
 
-    public Lotto readWinNumber(String userWinNumber) {
-        StringTokenizer st = new StringTokenizer(userWinNumber, ",");
-        ArrayList<Integer> winNumber = new ArrayList<>();
-
-        while (st.hasMoreTokens()) {
-            String singleNumber = st.nextToken();
-            isDigit(singleNumber);
-            validateWinNumber(singleNumber);
-            winNumber.add(Integer.parseInt(singleNumber));
-        }
-
-        Collections.sort(winNumber);
-        return new Lotto(winNumber);
-    }
-
     private void isDigit(String userInput) {
         String pattern = "^[0-9]*$";
         if (!Pattern.matches(pattern, userInput)) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다");
-        }
-    }
-
-    private void validateWinNumber(String userInput) {
-        int incomingNumber = Integer.parseInt(userInput);
-        if (incomingNumber < 1 || incomingNumber > 45) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1과 45사이여야 합니다");
         }
     }
 
