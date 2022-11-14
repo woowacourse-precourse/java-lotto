c24bc40e86518f9a86caf48f987559e23cce0412
YooByeongIk <quddlr96@naver.com>
2022-11-14 21:38:05 +0900

    feat(lotto): add method to get purchase price from user input value

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static int getPurchasePrice(){
+        String input = Console.readLine();
+        return Integer.parseInt(input);
     }
 }
