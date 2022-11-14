fe906bf226293bc2aa2f844c86a36900d00f9993
이병3 <101378867+SongJSeop@users.noreply.github.com>
2022-11-14 23:58:08 +0900

    기능 2-2. 리스트 오름차순 정렬 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Application {
@@ -26,6 +27,7 @@ public class Application {
 
     static List<Integer> MakeNumbers() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
         return numbers;
     }
 }
