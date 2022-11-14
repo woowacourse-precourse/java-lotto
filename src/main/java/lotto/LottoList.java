4ee13ffb424ca2ef82936c4cc9f86ff3b635ddcf
vcho1958 <gusckd57@gmail.com>
2022-11-15 05:40:04 +0900

    feat(LottoList): 중복없이 랜덤한 1~45사이의 수로 생성하는 기능

--- src/main/java/lotto/LottoList.java
+++ src/main/java/lotto/LottoList.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class LottoList {
@@ -11,15 +14,15 @@ public class LottoList {
         this.amount = 0;
     }
 
-    public void printLottoList(){
-        System.out.println(amount+"개를 구매했습니다.");
-        for(var lotto:lottos){
+    public void printLottoList() {
+        System.out.println(amount + "개를 구매했습니다.");
+        for (var lotto : lottos) {
 //            TODO: lotto.printLottoNumbers();
         }
     }
 
-    public void generateLotto(){
-//        TODO: List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-
+    public void generateLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        new Lotto(numbers);
     }
 }
