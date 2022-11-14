64f5607abf068bf305e67c52e354b36ec13f0e38
jjmin9797 <jjmin9797@naver.com>
2022-11-14 15:38:55 +0900

    feat(WinningStatsView.java): 구분선 출력 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/view/WinningStatsView.java
+++ src/main/java/lotto/view/WinningStatsView.java
@@ -3,8 +3,13 @@ package lotto.view;
 public class WinningStatsView {
 
     public static final String WINNING_STATS_MESSAGE = "당첨 통계";
+    public static final String CONTOUR = "---";
 
     public static void showWinningStatsMessage() {
         System.out.println(WINNING_STATS_MESSAGE);
+    }
+
+    public static void showContour() {
+        System.out.println(CONTOUR);
     }
 }
