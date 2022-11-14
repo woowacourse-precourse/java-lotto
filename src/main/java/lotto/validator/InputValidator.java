93801eed129b4dead7f4c7bfca3af7dc42f99311
kim0914 <kosa0914@naver.com>
2022-11-14 18:59:43 +0900

    refactor(InputValidator): 보너스 번호, 입력 금액 검증 함수 분리

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -2,10 +2,28 @@ package lotto.validator;
 
 public class InputValidator {
 
-    public static void checkDigit(String money) throws IllegalArgumentException {
-        final String regex = "^[0-9]+$";
-        if (!money.matches(regex)) {
+    public static void checkMoney(String money) throws IllegalArgumentException {
+        if (isNotDigit(money)) {
             throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
         }
+
+        if (isOverMax(money)) {
+            throw new IllegalArgumentException("입력할 수 있는 금액의 최대값을 넘었습니다.");
+        }
+    }
+
+    public static void checkBonusNumber(String number) {
+        if (isNotDigit(number)) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    private static boolean isNotDigit(String number) {
+        final String regex = "^[0-9]+$";
+        return !number.matches(regex);
+    }
+
+    private static boolean isOverMax(String money) {
+        return Long.parseLong(money) > Long.MAX_VALUE;
     }
 }
