b29ab119c987c99c221ca3fc4e69f8a5a072fcea
yujaemin <woawoa0513@gmail.com>
2022-11-15 00:16:35 +0900

    feat: 접근제어자 변경
    외부에서 접근하는 메소드만 public으로 수정한다.

--- src/main/java/lotto/service/WinningService.java
+++ src/main/java/lotto/service/WinningService.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class WinningService {
     private static final int NUMBER_OF_RANKING = Ranking.values().length;
 
-    public int[] compareAllLottoWithWinningNumbers(List<Lotto> lottos, WinningNumber winningNumber) {
+    public static int[] compareAllLottoWithWinningNumbers(List<Lotto> lottos, WinningNumber winningNumber) {
         int[] rankingCount = new int[NUMBER_OF_RANKING + 1];
 
         for (Lotto lotto : lottos) {
@@ -20,7 +20,7 @@ public class WinningService {
         return rankingCount;
     }
 
-    public int compareWithWinningNumbers(Lotto lotto, WinningNumber winningNumber) {
+    private static int compareWithWinningNumbers(Lotto lotto, WinningNumber winningNumber) {
         List<Integer> winningNumbers = winningNumber.getNumbers();
         int bonusNumber = winningNumber.getBonusNumber();
 
@@ -35,7 +35,7 @@ public class WinningService {
         return NUMBER_OF_RANKING + 3 - matchingNumberCount;
     }
 
-    public int compareWithoutBonusNumber(Lotto lotto, List<Integer> winningNumbers) {
+    private static int compareWithoutBonusNumber(Lotto lotto, List<Integer> winningNumbers) {
         int matchingNumberCount = 0;
         for (int playerNumber : lotto.getNumbers()) {
             if (winningNumbers.contains(playerNumber)) {
@@ -45,7 +45,7 @@ public class WinningService {
         return matchingNumberCount;
     }
 
-    public boolean hasBonusNumber(Lotto lotto, int bonusNumber) {
+    private static boolean hasBonusNumber(Lotto lotto, int bonusNumber) {
         return lotto.getNumbers().contains(bonusNumber);
     }
 }
