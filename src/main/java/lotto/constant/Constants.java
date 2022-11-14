daa2b1b25dec9ab25f838311d4af682d72837efe
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-14 20:54:23 +0900

    feat: 반올림 계산과 관련된 숫자들을 상수화

--- src/main/java/lotto/constant/Constants.java
+++ src/main/java/lotto/constant/Constants.java
@@ -4,6 +4,11 @@ public class Constants {
 
     public static final int UNIT_OF_LOTTO_PURCHASE = 1000;
     public static final String WINNING_LOTTO_NUMBER_INPUT_SEPARATOR = ",";
+    public static final int PERCENT = 100;
+    /**
+     * 반올림 하고싶은 자리만큼 1 뒤에 0을 붙이면 된다. Ex) 2번째 자리에서 반올림은 10 3번째 자리에서 반올림은 100
+     */
+    public static final double ROUND_DIGIT = 10.;
 
     private Constants() {
     }
--- src/main/java/lotto/domain/WinningStatistics.java
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -1,10 +1,14 @@
 package lotto.domain;
 
+import lotto.constant.Constants;
+
 public class WinningStatistics {
     private final double rateOfReturn;
 
     public WinningStatistics(int totalWinningMoney, int inputMoney) {
-        this.rateOfReturn = Math.round(((double) totalWinningMoney / (double) inputMoney * 100) * 10) / 10.;
+        this.rateOfReturn = Math.round(
+                ((double) totalWinningMoney / (double) inputMoney * Constants.PERCENT) * Constants.ROUND_DIGIT
+        ) / Constants.ROUND_DIGIT;
     }
 
     public double getRateOfReturn() {
