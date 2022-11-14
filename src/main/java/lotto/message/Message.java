ac8d07a9b5a25361e4c64d494fa86d190500a767
cocomozila <joseyun94@naver.com>
2022-11-14 20:48:23 +0900

    feat(Message): 시스템 메세지 추가

---
+++ src/main/java/lotto/message/Message.java
@@ -0,0 +1,14 @@
+package lotto.message;
+
+public class Message {
+
+    private Message() {}
+
+    public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String PURCHASE_LOTTO_AMOUNT_MESSAGE = "\n%d개를 구매했습니다.";
+    public static final String INPUT_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+
+    
+    
+}
