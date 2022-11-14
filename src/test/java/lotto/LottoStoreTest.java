5e5c8edb147beb7d586159fe745476ef91cf30f6
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-14 12:18:33 +0900

    test(LottoStore): 로또 개수만큼 숫자 배열 생성 테스트 코드 작성

---
+++ src/test/java/lotto/LottoStoreTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import lotto.domain.LottoStore;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoStoreTest {
+    @DisplayName("로또 개수만큼 숫자 배열 생성")
+    @Test
+    void createNumbersAsLottoCounts() {
+        assertThat(LottoStore.saleLotto(3).size())
+                .isEqualTo(3);
+    }
+}
