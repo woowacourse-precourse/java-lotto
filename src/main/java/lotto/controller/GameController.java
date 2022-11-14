4974ddbc890f8d65088bdddedf84a7b011d7a350
Lee-Jiseung <ljss98@naver.com>
2022-11-14 23:12:43 +0900

    feat: 구매 로또 뽑기

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,15 +1,22 @@
 package lotto.controller;
 
+import lotto.model.Computer;
+import lotto.model.Lotto;
 import lotto.model.User;
 import lotto.view.Message;
 
 public class GameController {
     private final Message message = new Message();
     private final User user = new User();
+    private final Computer computer = new Computer();
 
     public void buyLotto() {
         message.inputPrice();
         user.inputPrice();
         message.countLotto(user.getCountLotto());
+
+        for (int i = 0; i < user.getCountLotto(); i++) {
+            Lotto lotto = computer.buyLotto();
+        }
     }
 }
---
+++ src/main/java/lotto/model/Computer.java
@@ -0,0 +1,13 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class Computer {
+
+    public Lotto buyLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
+}
