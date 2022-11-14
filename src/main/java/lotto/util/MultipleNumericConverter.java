72195a7fc7d0aab4d8e07fd115c95620bd620378
woo-chang <clllickme@naver.com>
2022-11-14 18:54:23 +0900

    refactor(MultipleNumericConverter): 구분자에 대한 명칭을 명확하게 드러내도록 수정

--- src/main/java/lotto/util/MultipleNumericConverter.java
+++ src/main/java/lotto/util/MultipleNumericConverter.java
@@ -6,12 +6,12 @@ import java.util.stream.Collectors;
 
 public class MultipleNumericConverter implements Converter<String, List<Integer>> {
 
-    private static final String REGEX = ",";
+    private static final String SEPARATOR = ",";
     private static final String ERROR_MESSAGE = "숫자로 변환할 수 없는 값이 포함되어 있습니다. 입력: %s";
 
     @Override
     public List<Integer> convert(String target) {
-        return Arrays.stream(target.split(REGEX))
+        return Arrays.stream(target.split(SEPARATOR))
             .map(this::convertToNumeric)
             .collect(Collectors.toList());
     }
