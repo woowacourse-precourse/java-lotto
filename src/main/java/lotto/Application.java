214300a16f7af6964e5c48c913b9c3ed11fc4d70
bfly_notebook <kkyxw@soongsil.ac.kr>
2022-11-14 18:30:03 +0900

    refactor: add the try-catch statement to observe requirement specification - print the message that start '[ERROR]'

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,33 +1,38 @@
 package lotto;
 
-import static lotto.Input.bonusNumberInput;
-import static lotto.Input.purchasePriceInput;
-import static lotto.Input.winningNumbersInput;
-import static lotto.Output.printLotteries;
-import static lotto.Output.printWinningStatistics;
+import lotto.domain.Lotto;
+import lotto.domain.QuickPickGenerator;
+import lotto.domain.WinningNumbersIncludingBonus;
+
+import static lotto.view.Input.bonusNumberInput;
+import static lotto.view.Input.purchasePriceInput;
+import static lotto.view.Input.winningNumbersInput;
+import static lotto.view.Output.printLotteries;
+import static lotto.view.Output.printWinningStatistics;
 
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        int purchasePrice = purchasePriceInput();
-        int purchaseCount = purchasePrice / 1000;
+        try {
+            int purchasePrice = purchasePriceInput();
+            printNewLine();
+            List<Lotto> lotteries = QuickPickGenerator.quickPick(purchasePrice / 1_000);
+            printLotteries(lotteries);
 
-        System.out.println();
+            Lotto winningNumbers = winningNumbersInput();
+            printNewLine();
+            int bonusNumber = bonusNumberInput();
 
-        List<Lotto> lotteries = QuickPickGenerator.quickPick(purchaseCount);
-        printLotteries(lotteries);
-
-        System.out.println();
-
-        Lotto winningNumbers = new Lotto(winningNumbersInput());
-        System.out.println();
-        int bonusNumber = bonusNumberInput();
-
-        WinningNumbersIncludingBonus winnings = new WinningNumbersIncludingBonus(winningNumbers, bonusNumber);
-
-        printWinningStatistics(lotteries, winnings);
+            WinningNumbersIncludingBonus winnings = new WinningNumbersIncludingBonus(winningNumbers, bonusNumber);
+            printNewLine();
+            printWinningStatistics(lotteries, winnings);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
+    private static void printNewLine() {
+        System.out.println();
+    }
 }
