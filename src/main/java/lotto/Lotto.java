1b4c4c9d8e8bd98560cffa900ec206cfe577375c
BaeDongHyeon <sasd730@gmail.com>
2022-11-14 11:11:18 +0900

    refactor: 검증 기능 분리
    - NumberValidate 적용

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,34 +1,16 @@
 package lotto;
 
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        NumberValidate.countSixValidate(numbers);
+        NumberValidate.duplicateValidate(numbers);
+        NumberValidate.inRangeValidate(numbers);
         this.numbers = numbersSort(numbers);
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개만 입력 할 수 있습니다.");
-        }
-
-        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-        if (uniqueNumbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 중복되는 번호는 입력 할 수 없습니다.");
-        }
-
-        int max = Collections.max(uniqueNumbers);
-        int min = Collections.min(uniqueNumbers);
-        if (min < 1 || 45 < max) {
-            throw new IllegalArgumentException("[ERROR] 1~45의 정수만 입력 할 수 있습니다.");
-        }
     }
 
     private List<Integer> numbersSort(List<Integer> numbers) {
