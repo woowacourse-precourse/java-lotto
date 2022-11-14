a5d708cb28d7a978d7f458c1e98d3f83c4dabe4d
This2sho <zx00018@naver.com>
2022-11-14 21:41:39 +0900

    feat(Salesman): 로또 구입 금액이 1000원으로 나누어 떨어지지 않는 경우 예외 발생 로직 구현

--- src/main/java/lotto/domain/Salesman.java
+++ src/main/java/lotto/domain/Salesman.java
@@ -5,12 +5,23 @@ public class Salesman {
 	private final static int LOTTO_PRICE = 1000;
 
 	public int sellLotto(int purchaseBudget) {
-		validateLittleBudget(purchaseBudget);
+		validate(purchaseBudget);
 		return purchaseBudget / LOTTO_PRICE;
+	}
+
+	private void validate(int purchaseBudget) {
+		validateLittleBudget(purchaseBudget);
+		validateIndivisibleBudget(purchaseBudget);
 	}
 
 	private void validateLittleBudget(int purchaseBudget) {
 		if (purchaseBudget < LOTTO_PRICE) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void validateIndivisibleBudget(int purchaseBudget) {
+		if (purchaseBudget % LOTTO_PRICE != 0) {
 			throw new IllegalArgumentException();
 		}
 	}
