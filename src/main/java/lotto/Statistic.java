0658dbb7b6ee3c322cc7ce840e2a507dc3879707
김태용 <amico741@naver.com>
2022-11-14 11:45:10 +0900

    feat: statistic enum 클래스 추가

---
+++ src/main/java/lotto/Statistic.java
@@ -0,0 +1,16 @@
+package lotto;
+
+public enum Statistic {
+    SAME_SIX(6), SAME_FIVE_BONUS(5), SAME_FIVE(5), SAME_FOUR(4), SAME_THREE(3),
+    SIX_PRICE(2000000000), FIVE_BONUS_PRICE(30000000), FIVE_PRICE(1500000),
+    FOUR_PRICE(50000), THREE_PRICE(5000);
+
+    private final int value;
+    Statistic(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
