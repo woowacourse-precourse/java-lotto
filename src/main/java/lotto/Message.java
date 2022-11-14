0846d54b4d818a5b18605979bdf202d504d10fac
ma-an-jong <alswhd1113@gmail.com>
2022-11-14 18:19:47 +0900

    fix(Message): 입력의 유효하지 않을때 에러 메시지를 함수로 변경하여 제공
    BREAKING CHANGE: 입력이 유효하지 않을때 에러 메시지를 변수를 통해 제공하던 방법을 메소드로 제공하는 방법으로 변경
    
    Before:
    
        INPUT_INVALID_MESSAGE : "[ERROR] 입력 형식이 맞지 않습니다. 게임을 종료합니다.\n";
    
    After:
    
        INPUT_INVALID_MESSAGE : "[ERROR] 입력 형식이 맞지 않습니다. 게임을 종료합니다.\n";
        getInputInvalidErrorMessage() : String.join(DELIMITER,ERROR_HEADER,INPUT_INVALID_MESSAGE_BODY);
    
        입력에 대한 에러 메세지 제공 방법을 변수에서 함수로 변경

--- src/main/java/lotto/Message.java
+++ src/main/java/lotto/Message.java
@@ -4,14 +4,13 @@ import lotto.Lotto.Lotto;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Message {
     public static final String GAME_START_MESSAGE = "게임시작\n";
     public static final String GAME_END_MESSAGE = "게임 종료\n";
 
     public static final String LOTTO_PURCHASE_REQUEST_MESSAGE = "로또 구매 금액을 입력해주세요(1장에 1000원) : ";
-    public static final String INPUT_INVALID_MESSAGE = "[ERROR] 입력 형식이 맞지 않습니다. 게임을 종료합니다.\n";
+    public static final String INPUT_INVALID_MESSAGE_BODY = "입력 형식이 맞지 않습니다. 게임을 종료합니다.\n";
     public static final String WINNING_LOTTO_NUMBERS_INPUT_REQUEST_MESSAGE = "당첨 번호를 ','로 구분하여 입력해 주세요 : ";
     public static final String BONUS_NUMBER_REQUEST_MESSAGE = "보너스 번호를 입력해 주세요 : ";
     private static final String ERROR_HEADER = "[ERROR]";
@@ -19,8 +18,13 @@ public class Message {
     private static final String LOTTO_SIZE_ERROR_MESSAGE_BODY = "로또 번호의 개수는 6이여야 합니다.\n";
     private static final String LOTTO_NUMBER_OUT_OF_BOUND_ERROR_MESSAGE_BODY = "로또 번호는 1부터 45 사이의 숫자여야 합니다.\n";
     private static final String LOTTO_NUMBER_DUPLICATION_ERROR_MESSAGE_BODY = "로또 번호에 중복된 숫자가 존재합니다.\n";
-
     public static final String STATISTICS_MESSAGE = "--------- 당첨 통계 ---------\n";
+
+    public static String getInputInvalidErrorMessage()
+    {
+        return String.join(DELIMITER,ERROR_HEADER,INPUT_INVALID_MESSAGE_BODY);
+    }
+
     public static String getLottoSizeErrorMessage()
     {
         return String.join(DELIMITER,ERROR_HEADER,LOTTO_SIZE_ERROR_MESSAGE_BODY);
@@ -33,7 +37,6 @@ public class Message {
     {
         return String.join(DELIMITER,ERROR_HEADER,LOTTO_NUMBER_DUPLICATION_ERROR_MESSAGE_BODY);
     }
-
     public static String getPurchaseLottoSizeMessage(List<Lotto> purchaseLotties)
     {
         return purchaseLotties.size() + "개를 구매했습니다.\n";
@@ -45,7 +48,6 @@ public class Message {
         for (Lotto lotto : lotties) {
             sb.append(lotto.toString() + "\n");
         }
-
         return sb.toString();
     }
 
@@ -63,7 +65,6 @@ public class Message {
         for (Prize prizeValue : prizeValues) {
             sb.append(prizeValue.toString() + " - " + map.getOrDefault(prizeValue,0) + "개\n");
         }
-
         return sb.toString();
     }
 
