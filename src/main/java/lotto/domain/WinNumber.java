61d9b6f3cfb87440d6286adb17cfe246eefeba6e
jini_11 <jieun21124@naver.com>
2022-11-14 16:39:39 +0900

    feat: 보너스 번호 예외 처리 기능 구현

--- src/main/java/lotto/domain/WinNumber.java
+++ src/main/java/lotto/domain/WinNumber.java
@@ -5,20 +5,49 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class WinNumber {
-    private static final String TYPE_ERROR_MESSAGE = "[ERROR] 숫자, 쉼표 외의 문자를 입력하셨습니다.";
+    private static final String WIN_NUMBER_TYPE_ERROR_MESSAGE = "[ERROR] 숫자, 쉼표 외의 문자를 입력하셨습니다.";
+    private static final String BONUS_NUMBER_TYPE_ERROR_MESSAGE = "[ERROR] 숫자 외의 문자를 입력하셨습니다.";
+    private static final String SAME_ERROR_MESSAGE = "[ERROR] 보너스 번호는 당첨 번호와 중복되면 안 됩니다.";
+    private static final String RANGE_ERROR_MESSAGE = "[ERROR] 보너스 번호는 1~45 사이 숫자이어야 합니다.";
+
     private Lotto winNumbers;
     private int bonusNumber;
 
     public WinNumber(String winNumbers, String bonusNumber) {
-        validateType(winNumbers);
+        validateWinNumbersType(winNumbers);
+        validateBonusNumbers(bonusNumber);
+        validateSame(winNumbers, bonusNumber);
 
         this.winNumbers = new Lotto(toLotto(winNumbers));
         this.bonusNumber = Integer.parseInt(bonusNumber);
     }
 
-    private void validateType(String number) {
-        if (!number.matches("^[0-9,]*$")) {
-            throw new IllegalArgumentException(TYPE_ERROR_MESSAGE);
+    private void validateWinNumbersType(String winNumber) {
+        if (!winNumber.matches("^[0-9,]*$")) {
+            throw new IllegalArgumentException(WIN_NUMBER_TYPE_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateBonusNumbers(String bonusNumber) {
+        validateBonusNumberType(bonusNumber);
+        validateRange(bonusNumber);
+    }
+
+    private void validateSame(String winNumbers, String bonusNumber) {
+        if (winNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(SAME_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateBonusNumberType(String bonusNumber) {
+        if (!bonusNumber.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException(BONUS_NUMBER_TYPE_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateRange(String number) {
+        if (Integer.parseInt(number) < 1 || Integer.parseInt(number) > 45) {
+            throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
         }
     }
 
