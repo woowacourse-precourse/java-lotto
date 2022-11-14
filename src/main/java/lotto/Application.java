049a3b65104f214f760490b3a1e6ebb5abfe8d3c
이병3 <101378867+SongJSeop@users.noreply.github.com>
2022-11-14 14:40:20 +0900

    기능 1-1. 구입 금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int user_buy_num = HowManyLotto();
+    }
+
+    static int HowManyLotto() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        int input_money = Integer.parseInt(input);
+        return input_money / 1000;
     }
 }
