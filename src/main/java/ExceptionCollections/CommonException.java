e5cca8055862e8b447c5fd7eefa6ee79efc69d15
swonny <wonny921@gmail.com>
2022-11-14 12:31:42 +0900

    fix(CommonException): 정규표현식 메소드 contains에서 matches로 변경

--- src/main/java/ExceptionCollections/CommonException.java
+++ src/main/java/ExceptionCollections/CommonException.java
@@ -8,17 +8,10 @@ public class CommonException {
     static final String NUMBER = "^[0-9]*$";
     static final int LOTTO_MINIMUM_NUMBER = 1;
     static final int LOTTO_MAXIMUM_NUMBER = 45;
-    static final int PRICE_PER_LOTTO = 1000;
 
     public static void hasCharacters(String number) {
-        if (!number.contains(NUMBER)) {
+        if (!number.matches(NUMBER)) {
             throw new IllegalArgumentException(Exceptions.getMessage(Exceptions.CHARACTER_INCLUDED));
-        }
-    }
-
-    public static void validatePaymentUnit(int payment) {
-        if (payment % PRICE_PER_LOTTO != 0) {
-            throw new IllegalArgumentException(Exceptions.getMessage(Exceptions.WRONG_MONEY_UNIT));
         }
     }
 
@@ -38,5 +31,9 @@ public class CommonException {
         if (numbers.contains(number)) {
             throw new IllegalArgumentException(Exceptions.getMessage(Exceptions.HAS_DUPLICATED_NUMBER));
         }
+    }
+
+    public static int stringToInteger(String value) {
+        return Integer.valueOf(value);
     }
 }
