f062e5dab0caca78704b2754ee19edbe3279c63d
desfox <desfox99@naver.com>
2022-11-14 18:51:31 +0900

    test(PurchasePriceInputValidationTest): 구입금액이 숫자로만 입력되었는지 검증하는 로직 테스트

---
+++ src/test/java/lotto/validation/PurchasePriceInputValidationTest.java
@@ -0,0 +1,23 @@
+package lotto.validation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PurchasePriceInputValidationTest {
+    private PurchasePriceInputValidation purchasePriceInputValidation;
+
+    @BeforeEach
+    public void initialize() {
+        purchasePriceInputValidation = new PurchasePriceInputValidation();
+    }
+
+    @DisplayName("구입금액 입력 시 숫자가 아닌 문자가 있을 경우, 예외가 발생한다.")
+    @Test
+    void validateConsistedOnlyOfNumbersTest() {
+        assertThatThrownBy(() -> purchasePriceInputValidation.validateConsistedOnlyOfNumbers("800a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
