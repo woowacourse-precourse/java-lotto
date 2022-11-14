60dfe3bb3da9c899260eb8c44d4f837b0eb415ae
recicoco068 <gkapf838@gmail.com>
2022-11-14 13:11:38 +0900

    test(LottoMachineTest.java) : 당첨 번호 얻는 함수 테스트

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -1,7 +1,9 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +16,20 @@ class LottoMachineTest {
         int input = 8000;
         int lottoCnt = lottoMachine.getLottoBuyCount(input);
         assertThat(lottoCnt).isEqualTo(input / LottoInfo.PRICE.getValue());
+    }
+
+    @DisplayName("올바른 당첨 번호 입력 테스트")
+    @Test
+    void 올바른_당첨_번호_입력() {
+        List<Integer> input = List.of(1, 2, 3, 4, 5, 6);
+        assertThat(lottoMachine.getWinningNumbers(input)).isEqualTo(input);
+    }
+
+    @DisplayName("유효하지 않은 당첨 번호 입력 테스트")
+    @Test
+    void 유효하지_않은_당첨_번호_입력() {
+        List<Integer> input = List.of(1, 2, 3, 4, 5, 6, 7);
+        assertThatThrownBy(() -> lottoMachine.getWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
