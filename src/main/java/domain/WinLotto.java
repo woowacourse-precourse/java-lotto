43ad4083a4cef239a83cfc5b5bac710519348f68
seheonCho <whtpgjs94@gmail.com>
2022-11-15 00:48:21 +0900

    feat(WinLotto): 로또를 집계하는 기능 구현

--- src/main/java/domain/WinLotto.java
+++ src/main/java/domain/WinLotto.java
@@ -1,18 +1,39 @@
 package domain;
 
+import util.LottoRank;
+
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class WinLotto {
 
     private final List<Lotto> lotteries;
     private final Lotto winnerNumber;
     private final int bonusNumber;
+    private final Map<LottoRank, Integer> result;
 
     public WinLotto(List<Lotto> lotteries, Lotto winnerNumber, int bonusNumber) {
         this.lotteries = lotteries;
         this.winnerNumber = winnerNumber;
         this.bonusNumber = bonusNumber;
+        this.result = setResult();
     }
 
+    private Map<LottoRank, Integer> setResult() {
+        Map<LottoRank, Integer> numberOfRank = new EnumMap<>(LottoRank.class);
+        final int DEFAULT_ZERO = 0;
+        final int ONE = 1;
+
+        lotteries.stream().forEach(lotto -> {
+            LottoRank rank =
+                    LottoRank.getRank(lotto, lotto.numberOfSameNumbers(winnerNumber.getNumbers()), bonusNumber);
+
+            Integer numRank = numberOfRank.getOrDefault(rank, DEFAULT_ZERO);
+            numberOfRank.put(rank, numRank + ONE);
+        });
+
+        return numberOfRank;
+    }
 
 }
