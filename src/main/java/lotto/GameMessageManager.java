16759c2fb024615ea33df00260f6e88c72d0adf4
JeongHunHui <deca3248@gmail.com>
2022-11-15 03:38:59 +0900

    feat: 보너스 번호를 입력해달라는 메세지 추가

--- src/main/java/lotto/GameMessageManager.java
+++ src/main/java/lotto/GameMessageManager.java
@@ -4,6 +4,7 @@ public class GameMessageManager {
   public static void printBlankLine() {
     System.out.println();
   }
+
   public static void printInputBuyAmountMessage() {
     System.out.println(GameMessage.INPUT_BUY_AMOUNT_MESSAGE.getMessage());
   }
@@ -16,10 +17,15 @@ public class GameMessageManager {
     System.out.println(GameMessage.INPUT_PRIZE_NUMBER_MESSAGE.getMessage());
   }
 
+  public static void printInputBonusNumberMessage() {
+    System.out.println(GameMessage.INPUT_BONUS_NUMBER_MESSAGE.getMessage());
+  }
+
   private enum GameMessage {
     INPUT_BUY_AMOUNT_MESSAGE("구입금액을 입력해 주세요."),
     BUY_COUNT_MESSAGE("개를 구매했습니다."),
-    INPUT_PRIZE_NUMBER_MESSAGE("당첨 번호를 입력해 주세요.");
+    INPUT_PRIZE_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요.");
 
     String message;
 
