743e50299f54848fff67419a22e5f335c8cb8d2c
younghyun-na <younghyunna@naver.com>
2022-11-14 17:16:39 +0900

    :sparkles: feat: 등수당 당첨된 로또 개수를 계산하는 기능 추가

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Game {
 
@@ -12,12 +14,18 @@ public class Game {
     this.lotteries = new ArrayList<>();
   }
 
+  private final Map<Ranking, Integer> winningResult = new EnumMap<>(Ranking.class);
+
   // 맞춘 개수와 보너스 넘버 포함 여부로 랭킹 알아보기
   public Ranking valueOf(int num, boolean hasBonusNumber) {
     for (Ranking ranking : Ranking.values()) {
       if (ranking.getNumber() == num) {
         if (num == 5) {
-          fiveSameNumber(hasBonusNumber);
+          if (hasBonusNumber) {
+            return Ranking.SECOND;
+          }
+          else
+            return Ranking.THIRD;
         }
         return ranking;
       }
@@ -25,17 +33,36 @@ public class Game {
     return null;
   }
 
-  // 5개일 때 보너스 넘버 포함 여부 나누는 메서드
-  public Ranking fiveSameNumber(boolean hasBonusNumber) {
-    if (hasBonusNumber) {
-      return Ranking.SECOND;
-    }
-    else
-      return Ranking.THIRD;
-  }
-
   public boolean hasBonusNumber(Lotto lotto, int bonusNumber) {
     return lotto.getNumbers().contains(bonusNumber);
   }
 
+  // 등수별 0명으로 초기화
+  public void initialWinningResult() {
+    for (Ranking ranking : Ranking.values()) {
+      winningResult.put(ranking, 0);
+    }
+  }
+
+  // 로또 한 개 당첨 번호와 돌아가면서 비교하고 같은 수의 개수 반환
+  public int compare(Lotto lotto, Lotto winningNumber) {
+    int sameNumber = lotto.getNumbers().stream()
+            .filter(target -> winningNumber.getNumbers().stream().anyMatch(Predicate.isEqual(target)))
+            .collect(Collectors.toList())
+            .size();
+    return sameNumber;
+  }
+
+  // 로또 전체를 당첨 번호와 돌아가면서 비교하고 등수당 당첨 개수 구하는 메서드
+  public Map<Ranking, Integer> compareAll(List<Lotto> lotteries, Lotto winningNumber, int bonusNumber) {
+    initialWinningResult();
+    for (Lotto lotto : lotteries) {
+      int number = compare(lotto, winningNumber);
+      if (number > 2) {
+        Ranking ranking = valueOf(number, hasBonusNumber(lotto,bonusNumber));
+        winningResult.put(ranking, winningResult.get(ranking) + 1);
+      }
+    }
+    return winningResult;
+  }
 }
