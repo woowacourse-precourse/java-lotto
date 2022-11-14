eb75bfd495669297d9ebb3b506132e58c63767a1
dong young <dong5854@gmail.com>
2022-11-14 23:17:56 +0900

    chore: remove blank line

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,7 +11,6 @@ public class Application {
     public static void main(String[] args) {
         try {
             Integer money = LottoUI.purchase();
-
             List<Lotto> lottos = Lotto.purchase(money);
             PrintSortedLottos(lottos);
             List<Integer> winNumbers = LottoUI.receiveWinNumbers();
