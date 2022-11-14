fde63ad026471cc68e0a328387056a6354094866
June <68291395+june-777@users.noreply.github.com>
2022-11-14 20:31:37 +0900

    feat: 로또 게임 진행 기능 구현

--- src/main/java/controller/LottoGameController.java
+++ src/main/java/controller/LottoGameController.java
@@ -7,30 +7,41 @@ import view.InputLottoPurchaseAmount;
 import view.OutputLotto;
 
 public class LottoGameController {
-	
+
 	private InputLottoPurchaseAmount inputLotto = new InputLottoPurchaseAmount();
 	private LottoTotalCountCalculator lottoTotalCountCalculator = new LottoTotalCountCalculator();
 	private OutputLotto outputLotto = new OutputLotto();
 	private LottoRandomNumberGenerator randomGenerator = new LottoRandomNumberGenerator();
 
+	public void gameProgress() {
+		long lottoPurchaseAmount = lottoPurchaseAmountInput();
+		long lottoTotalCount = lottoTotalCount(lottoPurchaseAmount);
+		lottoTotalCountPrint(lottoTotalCount);
+
+		for (int lottoCount = 0; lottoCount < lottoTotalCount; lottoCount++) {
+			Lotto lotto = lottoCreate();
+			lottoPrint(lotto);
+		}
+	}
+
 	private long lottoPurchaseAmountInput() {
 		long purchaseAmount = inputLotto.readLottoPurchaseAmount();
 		return purchaseAmount;
 	}
-	
+
 	private long lottoTotalCount(long purchaseAmount) {
 		return lottoTotalCountCalculator.calculateLottoTotalCount(purchaseAmount);
 	}
-	
+
 	private void lottoTotalCountPrint(long totalCount) {
 		outputLotto.showLottoCount(totalCount);
 	}
-	
+
 	private Lotto lottoCreate() {
 		Lotto lotto = new Lotto(randomGenerator.randomUniqueNumbersGenerate());
 		return lotto;
 	}
-	
+
 	private void lottoPrint(Lotto lotto) {
 		outputLotto.showLotto(lotto);
 	}
