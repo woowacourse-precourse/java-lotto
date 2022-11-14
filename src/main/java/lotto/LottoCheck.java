be2c178b9df686a3bada59635ba164b9ff7b9099
ssssudan <kankgm0324@gmail.com>
2022-11-14 23:39:09 +0900

    <feat>: enum을 통해 해당 등수에 대한 당첨금을 구하고, 수익률을 계산

--- src/main/java/lotto/LottoCheck.java
+++ src/main/java/lotto/LottoCheck.java
@@ -8,15 +8,18 @@ public class LottoCheck {
     private List<List<Integer>> lottoNums;
     private List<Integer> winningNum;
 
+    public int totalPrice;
     public List<String> checkRank;
 
     public LottoCheck(List<List<Integer>> lottoNums, List<Integer> winningNum, int bonusNum) {
+        totalPrice = 0;
         checkRank = new ArrayList<>();
         this.lottoNums = lottoNums;
         this.winningNum = winningNum;
         this.bonusNum = bonusNum;
 
         lottoCheck();
+        result();
     }
 
     private void lottoCheck() {
@@ -44,5 +47,40 @@ public class LottoCheck {
             return (Integer.toString(matchNum) + "+");
         }
         return Integer.toString(matchNum);
+    }
+
+    private void result() {
+        List<String> allResult = List.of("3", "4", "5", "5+", "6");
+
+        for (String res : allResult) {
+            int check = 0;
+            String price = Winnings.getTotalWinnings(res);
+
+            for (String realRes : checkRank) {
+                if (res.equals(realRes))
+                    check++;
+            }
+
+            if (res != "5+")
+                printResult(Integer.parseInt(res), price, check);
+
+            else
+                printSecondResult(check);
+
+            if (check > 0) {
+                price = price.replace(",", "");
+                totalPrice += (Integer.parseInt(price) * check);
+            }
+        }
+    }
+
+    private void printResult(int match, String price, int amount) {
+        System.out.printf("%d개 일치 (%s원) - %d개", match, price, amount);
+        System.out.println();
+    }
+
+    private void printSecondResult(int amount) {
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개", amount);
+        System.out.println();
     }
 }
--- src/main/java/lotto/Progress.java
+++ src/main/java/lotto/Progress.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 public class Progress {
     public static void run() {
-        BuyAmount buyAmount = new BuyAmount(UserInput.inputMoney());
+        int money = UserInput.inputMoney();
+        BuyAmount buyAmount = new BuyAmount(money);
         int buyLotto = buyAmount.buyLotto;  // 로또 구매 수량
+
         System.out.println(buyLotto + "개를 구매했습니다.");
 
         LottoNumbers lottoNumbers = new LottoNumbers(buyLotto);
-        for (List<Integer> lotto : lottoNumbers.lottoNums) {
-            System.out.println(lotto);
-        }
 
         WinningNumberToList winningNumberToList = new WinningNumberToList();
         winningNumberToList.setWinNumbers(UserInput.inputWinNumber()); // 당첨 번호 입력 후 리스트 저장
@@ -24,5 +23,9 @@ public class Progress {
 
         LottoCheck lottoCheck = new LottoCheck(lottoNumbers.lottoNums, winningNumberToList.winNumbers, bonusNum);
 
+        System.out.printf("총 수익률은 %.1f%%입니다.", 100 * lottoCheck.totalPrice / (float) money);
+
     }
 }
+
+
