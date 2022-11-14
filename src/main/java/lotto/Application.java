3a5cab1efda0e025cf28093fa69201021fad1401
phj1120 <hyeonj1998@naver.com>
2022-11-14 11:40:08 +0900

    feat(lotto): 구입할 금액 입력 받는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        String line = Console.readLine();
+        int amountToBuy = Integer.parseInt(line);
+        // TODO 1000 단위로 떨어지지 않을 경우 예외 발생
+
+
+
     }
 }
