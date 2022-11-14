a6fd6f9037d2879ba0740cfd44c68336627ccd5c
CAEL0 <kjongmin26@gmail.com>
2022-11-14 17:32:11 +0900

    refactor(GameConstants): 필요 없는 상수 삭제

--- src/main/java/lotto/constant/GameConstants.java
+++ src/main/java/lotto/constant/GameConstants.java
@@ -6,15 +6,8 @@ public class GameConstants {
 
     public static final int LOTTO_NUMBER_LENGTH = 6;
     public static final int WINNING_NUMBER_LENGTH = 6;
-    public static final int BONUS_NUMBER_LENGTH = 1;
 
     public static final int LOTTO_PRICE = 1_000;
-
-    public static final int FIFTH_PRIZE = 5_000;
-    public static final int FOURTH_PRIZE = 50_000;
-    public static final int THIRD_PRIZE = 1_500_000;
-    public static final int SECOND_PRIZE = 30_000_000;
-    public static final int FIRST_PRIZE = 2_000_000_000;
 
     public static final String NATURAL_NUMBER_REGEX = "[0-9]+";
     public static final String NO_LEADING_ZERO_REGEX = "[1-9][0-9]*";
