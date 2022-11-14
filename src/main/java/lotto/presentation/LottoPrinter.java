1d69eecfb3c9c9a7848e0b9bbe0ab8b68b46f451
안광민 <creativeduck0518@gmail.com>
2022-11-14 13:08:20 +0900

    feat(LottoPrinter): 로또 수량 출력 기능 구현

--- src/main/java/lotto/presentation/LottoPrinter.java
+++ src/main/java/lotto/presentation/LottoPrinter.java
@@ -8,4 +8,8 @@ public class LottoPrinter {
         String[] prints = OutputPrint.OUTPUT_TOTAL_PROFIT.getMessage().split("/");
         System.out.println(prints[0] + profit + prints[1]);
     }
+
+    public void printLottoCount(int count) {
+        System.out.println(count + OutputPrint.OUTPUT_LOTTO_COUNT.getMessage());
+    }
 }
