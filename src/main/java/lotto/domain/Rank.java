6a6a2cb706fcb6b35723eb00790df983e95d1988
0chil <0@chll.it>
2022-11-14 16:43:05 +0900

    refactor(Rank): 보너스 번호 필드 제거

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,28 +3,32 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
-    FIFTH(3, false, 5_000),
-    NONE(2, false, 0);
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(2, 0);
 
     private final int matchCount;
-    private final boolean hasBonusNumber;
     private final long prize;
 
-    Rank(int matchCount, boolean hasBonusNumber, long prize) {
+    Rank(int matchCount, long prize) {
         this.matchCount = matchCount;
-        this.hasBonusNumber = hasBonusNumber;
         this.prize = prize;
     }
 
     public static Rank from(long matchCount, boolean hasBonusNumber) {
+        if (isThird(matchCount, hasBonusNumber)) {
+            return THIRD;
+        }
         return Arrays.stream(Rank.values())
                 .filter(rank -> rank.matchCount == matchCount)
-                .filter(rank -> rank.hasBonusNumber == hasBonusNumber)
-                .findAny()
+                .findFirst()
                 .orElse(NONE);
+    }
+
+    private static boolean isThird(long matchCount, boolean hasBonusNumber) {
+        return matchCount == THIRD.matchCount && !hasBonusNumber;
     }
 }
