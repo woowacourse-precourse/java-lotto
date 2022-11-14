e4579d01c0cf112daba10505b5beafc068b2ff80
wormjoo <wormjoo@naver.com>
2022-11-15 00:55:08 +0900

    feat: 사용자가 구매한 로또 번호에 보너스 번호가 포함되었는지를 확인하는 기능 구현

--- src/main/java/lotto/domain/Judgement.java
+++ src/main/java/lotto/domain/Judgement.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Judgement {
     private int matchedCount = 0;
+    private int matchedBonus = 0; // 0일 경우 unmatched, 1일 경우 macthed
 
     private void compareLottoWithLucky(List<Integer> lottoNumbers, List<Integer> luckyNumbers) {
         for (int number : lottoNumbers) {
@@ -14,6 +15,18 @@ public class Judgement {
     private void addMatchedCount(int number, List<Integer> luckyNumbers) {
         if (luckyNumbers.contains(number)) {
             matchedCount++;
+        }
+    }
+
+    private void compareLottoWithBonus(List<Integer> lottoNumbers, int bonusNumber) {
+        for (int number : lottoNumbers) {
+            checkMatchedBonus(number, bonusNumber);
+        }
+    }
+
+    private void checkMatchedBonus(int number, int bonusNumber) {
+        if (number == bonusNumber) {
+            matchedBonus = 1;
         }
     }
 }
