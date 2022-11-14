ca5e79146c938772559d3d8c2fd922136225d42a
Gibeom Nam <rlqja0523@naver.com>
2022-11-14 23:27:04 +0900

    test: 개인적으로 궁금한 것 테스트

--- src/test/java/lotto/service/LottoGeneratorTest.java
+++ src/test/java/lotto/service/LottoGeneratorTest.java
@@ -4,7 +4,10 @@ import lotto.entity.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -53,5 +56,52 @@ public class LottoGeneratorTest {
 
         // then
         assertEquals(numbers.size(), deduplicatedCount);
+    }
+
+    @Test
+    @DisplayName("랜덤으로 번호를 100번 생성할 때 어떤 번호가 가장 많이 뽑힐까")
+    void numberCount() {
+        Map<Integer, Integer> numberAndCnt = initMap();
+
+        List<Lotto> lotto = LottoGenerator.generateMyLotto(100);
+        for (Lotto myLotto : lotto) {
+            increaseMatchingNumberCount(numberAndCnt, myLotto.getNumbers());
+        }
+
+        List<Integer> maxAppearance = getMaxAppearanceNumbers(numberAndCnt);
+        System.out.println("# Max Appearance Numbers: " + maxAppearance);
+        System.out.println("# Count: " + numberAndCnt.get(maxAppearance.get(0)));
+    }
+
+    private Map<Integer, Integer> initMap() {
+        Map<Integer, Integer> numberAndCnt = new HashMap<>();
+        for (int i = 1; i < 46; i++) {
+            numberAndCnt.put(i, 0);
+        }
+        return numberAndCnt;
+    }
+
+    private void increaseMatchingNumberCount(Map<Integer, Integer> numberAndCnt, List<Integer> numbers) {
+        for (int number : numbers) {
+            numberAndCnt.merge(number, 1, Integer::sum);
+        }
+    }
+
+    private List<Integer> getMaxAppearanceNumbers(Map<Integer, Integer> numberAndCnt) {
+        int maxCnt = 0;
+        List<Integer> maxAppearance = new ArrayList<>();
+        for (int i = 1; i < 46; i++) {
+            int cnt = numberAndCnt.get(i);
+            System.out.println(i + ": " + cnt);
+
+            if (maxCnt < cnt) {
+                maxCnt = cnt;
+                maxAppearance.clear();
+                maxAppearance.add(i);
+            } else if (maxCnt == cnt) {
+                maxAppearance.add(i);
+            }
+        }
+        return maxAppearance;
     }
 }
