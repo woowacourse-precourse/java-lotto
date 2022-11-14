7d8d740ae7912183c1dbe840db1ef19c8e1e11a0
SeokHwan An <asok369258@naver.com>
2022-11-14 09:30:33 +0900

    feat : 당첨 번호와 로또 번호를 비교하여 같은 수의 개수를 반환하는 기능 추가

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Result {
@@ -12,4 +13,18 @@ public class Result {
         this.prize = 0;
         this.result = new HashMap<>();
     }
+
+    // 당첨 번호와 로또 번호를 비교하여 같은 수의 개수를 반환하는 기능
+    public int checkMatchCount(Lotto lotto, Lotto winningLotto) {
+        List<Integer> purchasedLottoNumbers = lotto.getNumbers();
+        List<Integer> winningLottoNumbers = winningLotto.getNumbers();
+        int matchCount = 0;
+        for (Integer number : purchasedLottoNumbers) {
+            if (winningLottoNumbers.contains(number)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
+
 }
