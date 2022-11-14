9d8b28bb63501e753a699b3a9f0d8c0cce77c4f1
DDing77 <devseo2021@gmail.com>
2022-11-15 05:37:23 +0900

    refactor(lotto) : 로또 패키지 위치 변경

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,5 +1,7 @@
-package lotto;
+package domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -15,6 +17,4 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
-
-    // TODO: 추가 기능 구현
 }
