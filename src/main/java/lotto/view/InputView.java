40b4dbf8f5c9eb8ebb13a396dbf38bd68a9821d7
Jenn Kim <grdd0419@gmail.com>
2022-11-15 03:03:53 +0900

    refactor: 당첨 번호를 입력받는 로직을 inputView 클래스로 이동

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,10 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
 
@@ -10,6 +14,16 @@ public class InputView {
         return convertStrToInt(Console.readLine());
     }
 
+    public static Lotto readWinningNumbers() {
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningNumberInput = Console.readLine();
+
+        List<Integer> winningNumbers = convertStrToIntList(winningNumberInput);
+
+        return new Lotto(winningNumbers);
+    }
+
     public static int convertStrToInt(String input) {
         try {
             return Integer.parseInt(input);
@@ -17,4 +31,19 @@ public class InputView {
             throw new IllegalArgumentException("[ERROR] 입력은 숫자여야 합니다");
         }
     }
+
+    public static List<Integer> convertStrToIntList(String input) {
+
+        List<Integer> integers = new ArrayList<>();
+
+        try {
+            String[] splitInput = input.split(",");
+            for(String i : splitInput)
+                integers.add(convertStrToInt(i));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] Winning number error");
+        }
+        return integers;
+    }
+
 }
