29b9718bb9bc655dd155d1d936cd887b64beeee8
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-14 17:29:52 +0900

    fix(Application): 잚못된 입력시 에러 메시지를 출력하지 않고 게임이 종료되지 않는 에러 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,22 +7,26 @@ import lotto.view.OutputView;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int purchaseAmount = InputView.askPurchaseAmount();
-        Quantity quantity = new Quantity(purchaseAmount);
-        LottosGenerator lottosGenerator = new LottosGenerator();
-        List<Lotto> lottos = lottosGenerator.createLottos(quantity);
+        try {
+            int purchaseAmount = InputView.askPurchaseAmount();
+            Quantity quantity = new Quantity(purchaseAmount);
+            LottosGenerator lottosGenerator = new LottosGenerator();
+            List<Lotto> lottos = lottosGenerator.createLottos(quantity);
 
-        OutputView.printLottoQuantity(quantity);
-        OutputView.printLottos(lottos);
+            OutputView.printLottoQuantity(quantity);
+            OutputView.printLottos(lottos);
 
-        List<Integer> playerNumbers = InputView.askNumbers();
-        int bonusNumber = InputView.askBonusNumber(playerNumbers);
+            List<Integer> playerNumbers = InputView.askNumbers();
+            int bonusNumber = InputView.askBonusNumber(playerNumbers);
 
-        Referee referee = new Referee();
-        List<WinningStat> winningStats = referee.compare(lottos, playerNumbers, bonusNumber);
-        Profit profit = new Profit(winningStats);
+            Referee referee = new Referee();
+            List<WinningStat> winningStats = referee.compare(lottos, playerNumbers, bonusNumber);
+            Profit profit = new Profit(winningStats);
 
-        OutputView.printStatistics(winningStats);
-        OutputView.printProfit(profit, quantity);
+            OutputView.printStatistics(winningStats);
+            OutputView.printProfit(profit, quantity);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+        }
     }
 }
