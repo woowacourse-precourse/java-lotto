c5b0f9b8ce973a8cddf54e171a7e867a8977640a
Heesom <heesom.hs@gmail.com>
2022-11-14 23:58:35 +0900

    refactor(Application) : Application 메소드 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,58 +1,25 @@
 package lotto;
 
-import lotto.Domain.MainValue.Bonus;
-import lotto.Domain.MainValue.Lotto;
-import lotto.Domain.MainValue.Money;
-import lotto.Domain.MainValue.PublishLotto;
+import lotto.Domain.Statistic.Profit;
 import lotto.Domain.Statistic.Statistic;
-import lotto.Input.InputNumber;
-import lotto.Output.Profit;
-import lotto.Output.MainValue.PublishedLottoOutput;
-
-import java.util.List;
-import java.util.Set;
+import lotto.Output.Statistic.StatisticOutput;
 
 public class Application {
-    public List<Integer> sixNum;
-    public int bonusNum;
-    public List<Set<Integer>> publishedLottos;
-
     public static final String ERROR = "[ERROR]";
     public static void main(String[] args) {
         try {
-        Application application = new Application();
-        application.get_SixNum_BonusNum_PublishedLotto();
-
+            Game game = new Game();
+            game.play();
         Statistic statistic =
-                new Statistic(application.sixNum, application.bonusNum, application.publishedLottos);
-
+                new Statistic(game.sixNum, game.bonusNum, game.publishedLottos);
+        StatisticOutput statisticOutput = new StatisticOutput();
+        statisticOutput.getStatisticOutput(statistic.prizeRanks);
         Profit profit = new Profit();
-        profit.printProfit(statistic.prizeRanks,application.publishedLottos.size()*1000);
+        profit.printProfit(statistic.prizeRanks,game.publishedLottos.size());
         } catch (IllegalArgumentException e) {
             System.out.println(ERROR+e.getMessage());
         }
 
-
-
-    }
-
-
-    public void get_SixNum_BonusNum_PublishedLotto() throws IllegalArgumentException {
-        InputNumber inputNumber = new InputNumber();
-        Money money = new Money(inputNumber.insertMoney());
-        int userMoneyLottoNum = money.getUserlottonum();
-
-        PublishLotto publishedLottos = new PublishLotto(userMoneyLottoNum);
-        PublishedLottoOutput publishedLottoOutput =
-                new PublishedLottoOutput(publishedLottos.getPublishedLotto(),userMoneyLottoNum);
-        publishedLottoOutput.printPublishedLotto();
-
-        Lotto sixNum = new Lotto(inputNumber.insertSixNum());
-        Bonus bonusNum = new Bonus(inputNumber.insertOneNum(), sixNum.getNumbers());
-
-        this.sixNum = sixNum.getNumbers();
-        this.bonusNum = bonusNum.getNumber();
-        this.publishedLottos = publishedLottos.getPublishedLotto();
     }
 
 }
