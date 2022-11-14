cfca177db0e905e7be4a6ed3703c62e1d3ae864b
김선동 <mok03127@gmail.com>
2022-11-14 22:08:59 +0900

    feat: 보너스 번호 예외 처리 기능

--- src/main/java/domain/WinningTicket.java
+++ src/main/java/domain/WinningTicket.java
@@ -5,7 +5,10 @@ import lotto.Lotto;
 import java.util.List;
 
 public class WinningTicket extends Lotto {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private static final String WINNING_NUMBER_AND_BONUS_DUPLICATED_ERROR = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다";
+    private static final String BONUS_NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
     private final Lotto winningNumbers;
     private final int bonusNumber;
 
@@ -13,6 +16,7 @@ public class WinningTicket extends Lotto {
         super(winningTicket);
         validateDuplicateNumber(winningTicket, bonusNumber);
         this.winningNumbers = new Lotto(winningTicket);
+        validateBonusNumber(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
@@ -26,5 +30,9 @@ public class WinningTicket extends Lotto {
         }
     }
 
-
+    private void validateBonusNumber(int bonusNumber) {
+        if (bonusNumber < MIN_NUMBER || bonusNumber > MAX_NUMBER) {
+            throw new IllegalArgumentException(BONUS_NUMBER_RANGE_ERROR_MESSAGE);
+        }
+    }
 }
