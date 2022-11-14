7146f47a9b32cc01ba4b548350c5cfa76af60197
davinc71998 <davinc71998@gmail.com>
2022-11-15 01:38:02 +0900

    Test: 추가 로또 번호 개수가 6개인지 확인하는 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,4 +25,11 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("생성된 로또 번호의 개수가 6개인지 확인")
+    @Test
+    void createLottoByRightSize() {
+        Lotto testRightLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(testRightLotto.numberRightCompare(List.of(1, 2, 3, 4, 5, 6))).isEqualTo(true);
+    }
+
 }
