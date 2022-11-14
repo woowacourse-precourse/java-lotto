8ce3876eb4dc0c2090228ae67dc37396cceb5072
baekeunsun <eunsun5460@gmail.com>
2022-11-14 16:08:59 +0900

    feat(controller/Compare): 보너스볼 비교
    - (Compare/CountLotto): 5개 맞았을 경우 보너스볼과 비교
    - (Compare/isBonus): 사용자 로또에 보너스볼이 있는지 확인

--- src/main/java/lotto/controller/Compare.java
+++ src/main/java/lotto/controller/Compare.java
@@ -6,15 +6,19 @@ import java.util.List;
 
 public class Compare {
     int count;
+    boolean bonusHit;
 
     public Compare(List<List<Integer>> userLotto, Lotto winLotto, int bonusLotto) {
         for (int paper = 0; paper < userLotto.size(); paper++) {
             this.count = CountLotto(userLotto.get(paper),winLotto,paper);
             System.out.println(count);
+            if (count == 5) {
+                bonusHit = IsBonus(userLotto.get(paper), bonusLotto);
+            }
         }
     }
-    
-    public int CountLotto(List<Integer> nowUserLotto, Lotto winLotto, int paper) {
+
+    private int CountLotto(List<Integer> nowUserLotto, Lotto winLotto, int paper) {
         int count = 0;
         for (int number = 0; number < 6; number++) {
             if (winLotto.Contain(nowUserLotto.get(number))) {
@@ -22,5 +26,12 @@ public class Compare {
             }
         }
         return count;
+    }
+
+    private boolean IsBonus(List<Integer> nowUserLotto, int bonusLotto) {
+        if (nowUserLotto.contains(bonusLotto)) {
+            return true;
+        }
+        return false;
     }
 }
