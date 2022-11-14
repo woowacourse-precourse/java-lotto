2cdd88a60f6b6bdd399e458de01c5db96a872616
ChoiYeonho0903 <903yh@naver.com>
2022-11-15 03:50:04 +0900

    refactor: 하드코딩 상수로 전환

--- src/main/java/lotto/view/LottoConsole.java
+++ src/main/java/lotto/view/LottoConsole.java
@@ -10,63 +10,52 @@ import lotto.model.Lottos;
 
 public class LottoConsole {
 
+    private static final String READ_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요";
+    private static final String READ_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String READ_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String PRINT_LOTTO_NUMBERS_MESSAGE = "%d개를 구매했습니다.\n";
+    private static final String PRINT_LOTTO_RESULT_MESSAGE = "당첨 통계\n---\n";
+    private static final String PRINT_LOTTO_RESULT_WINNING_COUNT_MESSAGE = "%s - %d개\n";
+    private static final String PRINT_LOTTO_RESULT_YIELD_MESSAGE = "총 수익률은 %.1f%%입니다.\n";
+    private static final String PRINT_ERROR_MESSAGE = "[ERROR] %s\n";
+
     public static String readPurchaseAmount() {
+        System.out.println(READ_PURCHASE_AMOUNT_MESSAGE);
         return Console.readLine();
     }
 
     public static String readWinningNumber() {
+        System.out.println(READ_WINNING_NUMBER_MESSAGE);
         return Console.readLine();
     }
 
     public static String readBonusNumber() {
+        System.out.println(READ_BONUS_NUMBER_MESSAGE);
         return Console.readLine();
     }
 
-    public static void printPurchaseAmountInput() {
-        System.out.println("구입금액을 입력해 주세요");
-    }
-
     public static void printLottos(Lottos lottos) {
-        System.out.printf("%d개를 구매했습니다.\n", lottos.getLottos().size());
+        System.out.printf(PRINT_LOTTO_NUMBERS_MESSAGE, lottos.getLottos().size());
         for(Lotto lotto : lottos.getLottos()) {
             System.out.println(lotto.getNumbers());
         }
     }
 
-    public static void printWinningNumberInput() {
-        System.out.println("당첨 번호를 입력해 주세요.");
-    }
-
-    public static void printBonusNumberStr() {
-        System.out.println("보너스 번호를 입력해 주세요.");
-    }
-
     public static void printLottoResult(LottoResult lottoResult) {
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        StringBuilder sb = new StringBuilder();
+        sb.append(PRINT_LOTTO_RESULT_MESSAGE);
+
         Map<LottoRank, Integer> winCount = lottoResult.getWinCount();
         List<LottoRank> lottoRanks = LottoRank.getLottoRankList();
-        StringBuilder sb = new StringBuilder();
         for(LottoRank lottoRank : lottoRanks) {
-            sb.append(lottoRank.getCorrectCount())
-                .append("개 일치");
-            if(lottoRank.isBonus()) {
-                sb.append(", 보너스 볼 일치");
-            }
-            sb.append(" (")
-                .append(lottoRank.getRewardStr())
-                .append("원) - ")
-                .append(winCount.get(lottoRank))
-                .append("개\n");
+            sb.append(String.format(PRINT_LOTTO_RESULT_WINNING_COUNT_MESSAGE, lottoRank.getRewardMessage(), winCount.get(lottoRank)));
         }
-        System.out.print(sb.toString());
-        System.out.printf("총 수익률은 %.1f%%입니다.\n", lottoResult.getYield());
+
+        sb.append(String.format(PRINT_LOTTO_RESULT_YIELD_MESSAGE, lottoResult.getYield()));
+        System.out.print(sb);
     }
 
     public static void printError(RuntimeException exception) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("[ERROR] ")
-            .append(exception.getMessage());
-        System.out.println(sb.toString());
+        System.out.printf(PRINT_ERROR_MESSAGE, exception.getMessage());
     }
 }
