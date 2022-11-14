8d386dc1eb9c54f44b091934b8bf93ee37e6c743
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-14 23:42:25 +0900

    feat: LottoService 클래스 생성

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,28 @@
+package lotto.service;
+
+import lotto.domain.Buyer;
+import lotto.domain.WinLotto;
+import lotto.generate.BuyerGenerate;
+import lotto.generate.WinLottoGenerate;
+import lotto.ouput.RandomPeekOutput;
+import lotto.ouput.StatisticsOutput;
+import lotto.util.LottoRandomPeek;
+import lotto.util.Statistics;
+
+public class LottoService {
+    public void play() throws IllegalArgumentException {
+
+        Buyer buyer = new BuyerGenerate().generate();
+        LottoRandomPeek lottoRandomPeek = new LottoRandomPeek();
+        lottoRandomPeek.generateRandom(buyer.getBuyLottoCount());
+        new RandomPeekOutput().output(lottoRandomPeek.getRandomLottos());
+
+        WinLotto winLotto = new WinLottoGenerate().generate();
+
+        Statistics statistics = new Statistics();
+        statistics.compareNumber(lottoRandomPeek.getRandomLottos(), winLotto);
+        StatisticsOutput output = new StatisticsOutput();
+        output.outputWiningHistory(statistics.getRank());
+        output.outputProfit(statistics.calculateProfitRate(buyer));
+    }
+}
