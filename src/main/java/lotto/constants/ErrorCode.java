04f7ea8c8f00ac9991e7c48b30eb1778364d150f
ohhonggi <5ghdrl@naver.com>
2022-11-15 05:29:02 +0900

    chore($ErrorCode): Create ErrorCode class
    This class has a type of error message.
    
    error message list
    - NOT_NUMBER
    - UNSUITABLE_UNIT
    - INVALID_NUMBER_OF_DIGITS
    - INVALID_NUMERIC_CONFIGURATION

---
+++ src/main/java/lotto/constants/ErrorCode.java
@@ -0,0 +1,15 @@
+package lotto.constants;
+
+public class ErrorCode {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    public static final String NOT_NUMBER = ERROR_MESSAGE + " 숫자를 입력해야 합니다.";
+    public static final String UNSUITABLE_UNIT = ERROR_MESSAGE +
+            " 로또 금액은 1000원 단위이어야 합니다.";
+    public static final String INVALID_NUMBER_OF_DIGITS = ERROR_MESSAGE +
+            " 로또 번호는 총 6개로 구성되어야 합니다.";
+    public static final String INVALID_NUMERIC_CONFIGURATION = ERROR_MESSAGE +
+            " 로또 번호는 중복되지 않는 숫자로 구성되어야 합니다.";
+
+
+}
