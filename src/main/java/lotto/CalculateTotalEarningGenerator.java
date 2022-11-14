cbe3428298471fe2055b7836385263eac2cc4390
jonghun <jonghuncu@gmail.com>
2022-11-15 01:04:04 +0900

    feat(lotto) : long 타입 변경 기능

--- src/main/java/lotto/CalculateTotalEarningGenerator.java
+++ src/main/java/lotto/CalculateTotalEarningGenerator.java
@@ -2,7 +2,8 @@ package lotto;
 
 public class CalculateTotalEarningGenerator {
     public static void calculateTotalEarning(int userMoney, int totalEarningMoney) {
-        double earningRate = (double) (totalEarningMoney*100)/userMoney;
+        long LongTypetotalEarningMoney = totalEarningMoney;
+        double earningRate = (double) (LongTypetotalEarningMoney*100)/userMoney;
         System.out.printf("총 수익률은 %.1f", earningRate);
         System.out.println("%입니다.");
     }
