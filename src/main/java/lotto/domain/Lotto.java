52bac0da483aa15d3c0fc68e364c34d1deabae26
desfox <desfox99@naver.com>
2022-11-14 21:12:58 +0900

    feat(Lotto): 로또 번호 검증

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,19 +1,17 @@
 package lotto.domain;
 
+import lotto.validation.LottoValidation;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+        LottoValidation lottoValidation = new LottoValidation();
+        lottoValidation.validate(numbers);
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        this.numbers = numbers;
     }
 
     public List<Integer> getNumbers() { return numbers; }
