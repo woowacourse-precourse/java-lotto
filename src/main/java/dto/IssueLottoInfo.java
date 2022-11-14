b5783716da44982cb68f5ce0d61442d1201009ec
mirasoy <nkskjjbb@gmail.com>
2022-11-14 17:33:38 +0900

    [Feat](IssueLottoInfo) : IssueLottoInfo 생성자 변경 - 생성시 발행번호자동생성

--- src/main/java/dto/IssueLottoInfo.java
+++ src/main/java/dto/IssueLottoInfo.java
@@ -1,11 +1,14 @@
 package dto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class IssueLottoInfo {
 	
 	private Lotto issueLottoNumber; //발행된 로또 번호
 	private int winningRanking;		//당첨 등수 (0일 경우에 당첨 x)
 	
-	public IssueLottoInfo(Lotto issueLottoNumber){
+	public IssueLottoInfo(){
+		Lotto issueLottoNumber =  new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
 		this.issueLottoNumber = issueLottoNumber;
 		this.winningRanking = 0;
 	}
