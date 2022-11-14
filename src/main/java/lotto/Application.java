150507cbdca4ba7efa0b59d312646dce11476055
Devtist <yt930310@snu.ac.kr>
2022-11-14 19:57:05 +0900

    style: 불필요한 주석 제거

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,8 +4,6 @@ import lotto.controller.LottoController;
 
 public class Application {
 	public static void main(String[] args) {
-		// TODO: 프로그램 구현
-		// TODO: 테스트코드 메소드 이름 다시 할 것
 		LottoController lottoController = new LottoController();
 		lottoController.control();
 	}
