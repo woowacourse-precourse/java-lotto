afb30a9972e16a2b8300a446ff586e9c704b46db
minhee020 <minhee020@naver.com>
2022-11-14 14:03:17 +0900

    refactor : winningResult의 초기 세팅 분리해서 구현

--- src/main/java/lotto/WinningResult.java
+++ src/main/java/lotto/WinningResult.java
@@ -1,34 +1,11 @@
 package lotto;
 
-import java.text.DecimalFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
-enum Rank{
-     NONE(0, 0), FIRST(6, 2_000_000_000), SECOND(5, 30_000_000), THIRD(5, 1_500_000), FOURTH(4, 50_000), FIFTH(3, 5_000);
-     int winningAmount;
-     int matchCount;
-     Rank(int matchCount, int winningAmount){
-          this.matchCount = matchCount;
-          this.winningAmount = winningAmount;
-     }
-     public int getWinningAmount(){
-          return winningAmount;
-     }
-
-     public String getDottedWinningAmount(){
-          DecimalFormat decimalFormat = new DecimalFormat("###,###");
-          String dottedWinningAmount = decimalFormat.format(winningAmount);
-          return dottedWinningAmount;
-     }
-     public int getMatchCount() {
-          return matchCount;
-     }
-}
 public class WinningResult {
      WinningNumber winningNumber;
      private HashMap<Rank, Integer> rankCount;
-     private int winningRate;
 
      WinningResult(WinningNumber winningNumber) {
           this.winningNumber = winningNumber;
@@ -39,33 +16,38 @@ public class WinningResult {
      }
 
      private HashMap<Rank, Integer> countRank(List<Rank> rank) {
-          this.rankCount = new HashMap<>();
-          rankCount.put(Rank.NONE, 0);
-          rankCount.put(Rank.FIRST, 0);
-          rankCount.put(Rank.SECOND, 0);
-          rankCount.put(Rank.THIRD, 0);
-          rankCount.put(Rank.FOURTH, 0);
-          rankCount.put(Rank.FIFTH, 0);
+          this.rankCount = setRankCount();
 
           for (Rank rankNum : rank) {
                rankCount.put(rankNum, rankCount.get(rankNum) + 1);
           }
-
           return rankCount;
      }
 
+     private HashMap<Rank, Integer> setRankCount() {
+          HashMap<Rank, Integer> result = new HashMap<>();
+          result.put(Rank.NONE, 0);
+          result.put(Rank.FIRST, 0);
+          result.put(Rank.SECOND, 0);
+          result.put(Rank.THIRD, 0);
+          result.put(Rank.FOURTH, 0);
+          result.put(Rank.FIFTH, 0);
+
+          return result;
+     }
+
      float winningRate(Integer purchaseAmount) {
-          int winningAmout = 0;
-          float result = 0f;
+          int winningAmount = 0;
+          float result;
 
           Iterator<Map.Entry<Rank, Integer>> entry = rankCount.entrySet().iterator();
 
           while (entry.hasNext()) {
                Map.Entry<Rank, Integer> element = entry.next();
-               winningAmout += element.getKey().getWinningAmount() * element.getValue();
+               winningAmount += element.getKey().getWinningAmount() * element.getValue();
           }
 
-          result = winningAmout / (float)purchaseAmount * 100;
+          result = winningAmount / (float)purchaseAmount * 100;
 
           return result;
      }
