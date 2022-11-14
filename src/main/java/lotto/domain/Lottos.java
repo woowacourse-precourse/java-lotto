f7482ec780ede4af2d9ed060be6eca02a839d534
Jenn Kim <grdd0419@gmail.com>
2022-11-15 02:11:03 +0900

    feat: 로또 묶음을 개별 클래스로 관리하기 위해 Lottos 클래스 추가

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+
+    private final List<Lotto> inputLottos;
+
+    public Lottos(int number) {
+        this.inputLottos = purchaseLotto(number);
+    }
+
+    public static List<Lotto> purchaseLotto(int number) {
+
+        List<Lotto> lottoTickets = new ArrayList<>();
+
+        for (int i = 0; i < number; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto l = new Lotto(numbers);
+            l.sortElements();
+            lottoTickets.add(l);
+        }
+
+        return lottoTickets;
+    }
+}
