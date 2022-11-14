c97ad011e28957d19cae25e1b2cf43a05771e3b0
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-15 00:57:53 +0900

    fix(OutputConsole): 로또 번호가 정렬이 가능하도록 코드 수정

--- src/main/java/lotto/OutputConsole.java
+++ src/main/java/lotto/OutputConsole.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -10,7 +11,7 @@ public class OutputConsole {
     public static void printLottoList(List<Lotto> lottos) {
         System.out.println(lottos.size() + "개를 구매했습니다.");
         for (int i = 0; i < lottos.size(); i++) {
-            List<Integer> lottoNumbers= lottos.get(i).getNumbers();
+            List<Integer> lottoNumbers = new ArrayList<>(lottos.get(i).getNumbers());
             Collections.sort(lottoNumbers);
             String lottoNumbersMessage = lottoNumbers.stream().map(String::valueOf).collect(Collectors.joining(", "));
             System.out.println("[" + lottoNumbersMessage + "]");
@@ -24,7 +25,7 @@ public class OutputConsole {
             if (key == Rank.NOTHING) {
                 continue;
             }
-            System.out.println(key.getScoreText() + "- " + rankResult.get(key) +"개");;
+            System.out.println(key.getScoreText() + " - " + rankResult.get(key) +"개");;
         }
     }
 
