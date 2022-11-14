bf2def3fb1a571661176d2a73d449cc754d2bbee
MuSeung <peter0757@nvaer.com>
2022-11-14 09:25:04 +0900

    feat: ResultPrint가 각 등수별 금액과 당첨수를 출력하는 기능을 구현
    ResultPrint
        - printingResult : Map의 value를 이요아혀 각 등수의 당첨수를 알맞게 출력

--- src/main/java/lotto/ResultPrint.java
+++ src/main/java/lotto/ResultPrint.java
@@ -4,9 +4,15 @@ import java.util.Map;
 
 public class ResultPrint {
 
+    private Map<String, Integer> prizes;
     public ResultPrint(Map<String, Integer> prizesMap) {
+        prizes = prizesMap;
     }
     public void printingResult() {
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - 1개");
+        System.out.print("3개 일치 (5,000원) - " + prizes.get("FIFTH") + "개");
+        System.out.println("4개 일치 (50,000원) - " + prizes.get("FOURTH") + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + prizes.get("THIRD") + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + prizes.get("SECOND") + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + prizes.get("FIRST") + "개");
     }
 }
