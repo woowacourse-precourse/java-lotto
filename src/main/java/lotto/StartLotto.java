ff08a73140c8b376046e79525b168a95fc230c1f
UngHeung <t01047377477@gmail.com>
2022-11-14 16:36:06 +0900

    feat : (Lotto) 파일명수정

--- src/main/java/lotto/StartLotto.java
+++ src/main/java/lotto/StartLotto.java
@@ -6,49 +6,77 @@ import java.util.StringTokenizer;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class UserInput {
-	
+public class StartLotto {
+
 	private Money money;
 	private Numbers numbers;
 	private Lotto lotto;
 	private Bonus bonus;
 	private Statistics statistics;
-	
-	public UserInput() {
+
+	public StartLotto() {
 		inputMoney();
 		inputWinningNumbers();
 		inputBonus();
-		
+
 		statistics = new Statistics(numbers.getNumbers(), lotto.getLotto(), money.getMoney(), bonus.getBonus());
 		System.out.println(statistics.toString());
 	}
-	
+
 	private void inputMoney() {
 		System.out.println("구입금액을 입력해 주세요.");
-		money = new Money(Integer.parseInt(Console.readLine()));
-		
+		String input = Console.readLine();
+		validateMoney(input);
+
+		money = new Money(Integer.parseInt(input));
 		int lottoCount = money.getLottoCount();
 		numbers = new Numbers(lottoCount);
-		
+
 		System.out.println(lottoCount + "개를 구매했습니다.");
 		System.out.println(numbers.toString());
 	}
-	
+
 	private void inputWinningNumbers() {
 		System.out.println("당첨 번호를 입력해 주세요.");
 		StringTokenizer token = new StringTokenizer(Console.readLine(), ",");
-		
+
 		List<Integer> winningNumbers = new ArrayList<>();
-		
+
 		while(token.hasMoreTokens()) {
-			winningNumbers.add(Integer.parseInt(token.nextToken()));
+			String item = token.nextToken();
+			validateNumbers(item);
+			winningNumbers.add(Integer.parseInt(item));
 		}
-		
+
 		lotto = new Lotto(winningNumbers);
 	}
-	
+
 	private void inputBonus() {
 		System.out.println("보너스 번호를 입력해 주세요.");
-		bonus = new Bonus(Integer.parseInt(Console.readLine()));
+		String input = Console.readLine();
+		validateNumbers(input);
+
+		bonus = new Bonus(Integer.parseInt(input));
+	}
+
+	private void validateMoney(String input) {
+		for (int i = 0; i < input.length(); i++) {
+			if(input.charAt(i)  < 48 || 57 < input.charAt(i)) {
+				throwException(ErrorMessage.TYPE_ERROR);
+			}
+		}
+	}
+
+	private void validateNumbers(String input) {
+		for (int i = 0; i < input.length(); i++) {
+			if(input.charAt(i)  < 49 || 57 < input.charAt(i)) {
+				throwException(ErrorMessage.TYPE_ERROR);
+			}
+		}
+	}
+
+	private void throwException(ErrorMessage errorType) {
+		errorType.printErrorMessage();
+		throw new IllegalArgumentException();
 	}
 }
