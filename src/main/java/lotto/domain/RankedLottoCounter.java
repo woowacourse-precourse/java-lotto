941cfda99a8ff90bc098463492ea6fb80210b7b2
keon0711 <fnc6488@gmail.com>
2022-11-15 03:55:24 +0900

    style(lotto): Rename class WinningsCalculator to RankedLottoCounter

--- src/main/java/lotto/domain/RankedLottoCounter.java
+++ src/main/java/lotto/domain/RankedLottoCounter.java
@@ -4,24 +4,23 @@ import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
-public class WinningsCalculator {
+public class RankedLottoCounter {
     private Map<Ranking, Integer> numberOfRankedLottos;
     private WinningLotto winningLotto;
 
-    public WinningsCalculator(List<Lotto> lottos, WinningLotto winningLotto) {
+    public RankedLottoCounter(WinningLotto winningLotto) {
         this.winningLotto = winningLotto;
         init();
-        countRankedLottos(lottos);
     }
 
     private void init() {
-        numberOfRankedLottos = new EnumMap<Ranking, Integer>(Ranking.class);
+        numberOfRankedLottos = new EnumMap<>(Ranking.class);
         for (Ranking ranking : Ranking.values()) {
             numberOfRankedLottos.put(ranking, 0);
         }
     }
 
-    private  Map<Ranking, Integer> countRankedLottos(List<Lotto> lottos) {
+    public  Map<Ranking, Integer> countRankedLottos(List<Lotto> lottos) {
         for (Lotto lotto : lottos) {
             int numberOfMatchNumbers = winningLotto.countMatchNumber(lotto);
             boolean bonusNumberMatch = winningLotto.checkBonusNumber(lotto);
@@ -30,18 +29,6 @@ public class WinningsCalculator {
 
             numberOfRankedLottos.put(ranking, currentCount + 1);
         }
-        return numberOfRankedLottos;
-    }
-
-    public long calculateWinnings() {
-        long winnings = 0L;
-        for (Map.Entry<Ranking, Integer> entry : numberOfRankedLottos.entrySet()) {
-            winnings += (long) entry.getKey().getReward() * entry.getValue();
-        }
-        return winnings;
-    }
-
-    public Map<Ranking, Integer> getNumberOfRankedLottos() {
         return numberOfRankedLottos;
     }
 }
