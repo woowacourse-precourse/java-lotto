22345492800ac3d81f024545294e7825103ddf4f
Dasol Jung <greteljds0251@gmail.com>
2022-11-14 21:20:40 +0900

    feat: Get lotto win numbers

---
+++ src/main/java/lotto/GetLotto.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetLotto {
+    public String getLottoNumbers() {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(ir);
+        String getNumbers;
+
+        try {
+            getNumbers = br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return getNumbers;
+    }
+
+    public List<Integer> cutNumbers(String getNumbers) {
+        String[] lottoNumbers = getNumbers.split(",");
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < lottoNumbers.length; i++) {
+            numbers.add(Integer.parseInt(lottoNumbers[i]));
+        }
+        return numbers;
+    }
+}
