889f3fe79e6af5ab528439f9e94f793b166eb7fe
YE-EUN <musiclove529@naver.com>
2022-11-15 06:20:50 +0900

    feat(Grade) : matchCount 인스턴스 변수 추가
    - 일치해야하는 번호 개수 나타냄

--- src/main/java/lotto/domain/Grade.java
+++ src/main/java/lotto/domain/Grade.java
@@ -1,17 +1,19 @@
 package lotto.domain;
 
 public enum Grade {
-    FIRST("6개 일치", 2000000000),
-    SECOND("5개 일치, 보너스 볼 일치", 30000000),
-    THIRD("5개 일치", 1500000),
-    FOURTH("4개 일치", 50000),
-    FIFTH("3개 일치", 5000);
+    FIRST("6개 일치", 6, 2000000000),
+    SECOND("5개 일치, 보너스 볼 일치", 5,30000000),
+    THIRD("5개 일치", 5,1500000),
+    FOURTH("4개 일치", 4,50000),
+    FIFTH("3개 일치", 3,5000);
 
     private final String condition;
+    private final int matchCount;
     private final int prize;
 
-    Grade(String condition, int prize) {
+    Grade(String condition, int matchCount, int prize) {
         this.condition = condition;
+        this.matchCount = matchCount;
         this.prize = prize;
     }
 
