a3ff1ab7a3154a7d1f33c3e3fbadcfc697e3a6df
namtndus <rnrmf0721@gmail.com>
2022-11-14 15:20:34 +0900

    refactor(OutputView): printLottoSize의 기능 변경
    매개변수를 통해 로또의 갯수를 출력으로 변경 됨

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,15 +1,10 @@
 package View;
 
-import Controller.InputController;
-
 import static Dto.ConstDto.InputOutputConstDto.PRINT_LOTTO_SIZE_MESSAGE;
 
 public class OutputView {
 
-    private final InputController inputController = new InputController();
-
-    public void printLottoSize(){
-       int lottoSize = inputController.inputMoney();
+    public void printLottoSize(int lottoSize){
         System.out.println(lottoSize+PRINT_LOTTO_SIZE_MESSAGE);
     }
 }
