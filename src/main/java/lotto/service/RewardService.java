e6d16fddcb8249abaaa028d743d3d5c1f1ca1f42
yujaemin <woawoa0513@gmail.com>
2022-11-14 23:55:47 +0900

    feat: 당첨금 Service 생성
    당첨금과 관련된 비즈니스 로직을 다루는 Service 생성

---
+++ src/main/java/lotto/service/RewardService.java
@@ -0,0 +1,9 @@
+package lotto.service;
+
+public class RewardService {
+
+    public double calculateRateOfReturn(int purchaseAmount, int reward) {
+        double rateOfReturn = ((double)reward / purchaseAmount) * 100 - 100;
+        return Math.round((rateOfReturn * 10) / 10.0);
+    }
+}
