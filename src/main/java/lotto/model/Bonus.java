32026214e934d9efd8a9cd08ca22e72fe6c2508a
HyeonjungYun <guswnd9971@gmail.com>
2022-11-14 20:05:06 +0900

    "refactor(Bonus): 출력문 추가"

--- src/main/java/lotto/model/Bonus.java
+++ src/main/java/lotto/model/Bonus.java
@@ -30,23 +30,23 @@ public class Bonus {
 
     public void checkBonusNumberRange(int bonusNumber){
         if (bonusNumber < 1 || bonusNumber > 45){
-            throw new IllegalArgumentException
-                    ("[ERROR]: 보너스 번호는 반드시 1~45 이내에 숫자여야 합니다.");
+            System.out.println(PrintError.BONUS_RANGE);
+            throw new IllegalArgumentException();
         }
     }
 
     public void checkBonusDuplicatedNumber(int bonusNumber, List<Integer> lotto){
         if (lotto.contains(bonusNumber)){
-            throw new IllegalArgumentException
-                    ("[ERROR]: 보너스 번호는 입력한 당첨번호와 중복되어선 안됩니다.");
+            System.out.println(PrintError.BONUS_OVERLAP);
+            throw new IllegalArgumentException();
         }
     }
 
     private void isRealNumber (String inputBonusNumber) {
         for (char element: inputBonusNumber.toCharArray()) {
             if (element < 48 || element > 57) {
-                throw new IllegalArgumentException
-                        ("[ERROR]: 숫자만 입력할 수 있습니다.");
+                System.out.println(PrintError.NUMBER_SIZE);
+                throw new IllegalArgumentException();
             }
         }
     }
