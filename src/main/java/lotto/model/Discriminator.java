5c78c6ef2b0c828a43aaaa03ca31e62de548f273
gomljo <gomljo0307@naver.com>
2022-11-14 17:00:41 +0900

    feat(Discriminator.java): 2개 이하로 맞춘 로또를 결과에서 제외하는 기능 구현

--- src/main/java/lotto/model/Discriminator.java
+++ src/main/java/lotto/model/Discriminator.java
@@ -1,8 +1,11 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Discriminator {
+    private static final List<List<Integer>> MATCHES = new ArrayList<>();
+    private static final int LEAST_MATCH_COUNT = 3;
     private static final int ZERO = 0;
     private static final int COUNT = 1;
     private static final int SECOND_RANK_CONDITION = 5;
@@ -32,5 +35,11 @@ public class Discriminator {
             numberOfBonusNumberMatches += COUNT;
         }
         return numberOfBonusNumberMatches;
+    }
+
+    public void addMatchOverThree(int raffleNumberMatches, int bonusNumberMatches) {
+        if (raffleNumberMatches > LEAST_MATCH_COUNT) {
+            MATCHES.add(List.of(raffleNumberMatches, bonusNumberMatches));
+        }
     }
 }
