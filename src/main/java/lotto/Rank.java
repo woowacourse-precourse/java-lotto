b519a0cfb05d45b4e6f6c02620ec43c90c0fddae
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-14 15:39:00 +0900

    <refactor>(Rank.java): Rank Enum 상수 추가
     - 겹치는 수 개수 저장하는 overlappingNumber 변수와, 보너스볼 일치 개수 저장하는 bonusBall 변수 작성

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,12 +1,17 @@
 package lotto;
 
-public enum Rank {
-    FIRST(2000000000), SECOND(30000000), THIRD(1500000),
-    FORTH(50000), FIFTH(5000);
-    Rank(int reward) {
+public enum Rank { //여기에 겹치는 개수 변수도 추가하는 건 어떰??
+    FIRST(2000000000, 6, 0), SECOND(30000000, 5, 1), THIRD(1500000, 5, 0),
+    FORTH(50000, 4, 0), FIFTH(5000, 3, 0);
+
+    Rank(int reward, int overlappingNumber, int bonusBall) {
         this.reward = reward;
+        this.overlappingNumber = overlappingNumber;
+        this.bonusBall = bonusBall;
     }
     private final int reward;
+    private final int overlappingNumber;
+    private final int bonusBall;
 
     public int getReward() {
         return reward;
