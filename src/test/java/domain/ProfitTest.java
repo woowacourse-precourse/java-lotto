92b9904771cd6ed9146fc747a139d197c2b94d7e
seheonCho <whtpgjs94@gmail.com>
2022-11-14 21:24:49 +0900

    refactor(ProfitTest): 테스트 메서드 표출 이름 변경
    수익률 계산 소수점 두번째에서 반올림이 되는지 확인하는 메서드
    Before: [empty]
    After: 수익률 계산 소수점 두번째에서 반올림이 되는지 확인
    
    로직상의 변화는 없습니다

--- src/test/java/domain/ProfitTest.java
+++ src/test/java/domain/ProfitTest.java
@@ -9,7 +9,7 @@ class ProfitTest {
 
 
 
-    @DisplayName("")
+    @DisplayName("수익률 계산 소수점 두번째에서 반올림이 되는지 확인")
     @Test
     void calculatePercentageOfProfit() {
         String percentageOfprofit = "62.5";
