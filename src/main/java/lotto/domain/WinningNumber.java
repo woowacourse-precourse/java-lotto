0ec918252debd9c9d4c6fbe1414510adab5edc96
starwook <star.wookk@gmail.com>
2022-11-15 02:26:09 +0900

    feat(User.java): Add StringToIntegerList Method
    User.java에StringToIntegerList 메소드를 추가하여라
    1. 모아둔 숫자를 0으로 시작한다
    2. 각
    char마다
    - ','라면 모우둔 숫자가 1~45사이인지 검사한다 -> 맞다면 모우둔 숫자를 numbers에 더해주고 모아둔
    숫자를 0으로 초기화
    - ','가 아니라면 0~9인지 검사한다
    - 맞다면 모아둔 숫자가 0임과 동시에 char이 0인지
    검사한다
    위 메소드들에서 다 통과하였으면 모아둔 숫자를 10곱한 후에 char을 int형으로 바꾸어 더해준다

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -8,7 +8,8 @@ public class WinningNumber {
     private static final char zero = '0';
     private static final char nine = '9';
     private static final int maxNum = 45;
-    private static final int minNum = 0;
+    private static final int initNumber = 0;
+    private static final int minNum = 1;
     private static final int mulNum = 10;
 
     private final List<Integer> numbers;
@@ -18,8 +19,31 @@ public class WinningNumber {
         this.numbers = numbers;
     }
 
-    private void checkIfNumberStartsWithZero(int temporaryInt, char newInt ){
-        if(temporaryInt == minNum && newInt == zero){
+    private List<Integer> StringToIntegerList(String string){
+        List<Integer> numbers = new ArrayList<>();
+        int temporaryInt = initNumber;
+        for(int i=0;i<string.length();i++){
+            char temporaryLetter = string.charAt(i);
+            if(temporaryLetter == rest ){
+                checkIfNumberInRange(temporaryInt);
+                numbers.add(temporaryInt);
+                temporaryInt = initNumber;
+                continue;
+            }
+            checkIfCharIsNumber(temporaryLetter);
+            checkIfNumberStartsWithZero(temporaryInt, temporaryLetter);
+            temporaryInt = temporaryLetter *mulNum + (temporaryLetter -zero);
+        }
+        return numbers;
+    }
+
+    private void checkIfNumberInRange(int i){
+        if(i<minNum ||i>maxNum){
+            throw new IllegalArgumentException();
+        }
+    }
+    private void checkIfNumberStartsWithZero(int temporaryNumber, char newNumber ){
+        if(temporaryNumber == initNumber && newNumber == zero){
             throw new IllegalArgumentException();
         }
     }
