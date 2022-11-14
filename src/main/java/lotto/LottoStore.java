317ee0b17088ef4c0f1f47d328f7a6bce4a410ba
iamsseaa <homingsea@gmail.com>
2022-11-14 13:52:34 +0900

    feat: 당첨 번호 입력을 위한 안내문 출력

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -6,4 +6,8 @@ public class LottoStore {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
+    public void printInputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
 }
