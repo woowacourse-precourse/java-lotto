78514a64d2f9c8ef5b5350ae2c45d3252824ee48
JaeHong-Ahn <63873742+JaeHong-Ahn@users.noreply.github.com>
2022-11-14 20:23:19 +0900

    <feat> 당첨통계
    추후에 메서드로 분리할 예정
    
    2등과 3등을 확실하게 구분하는 메서드를 따로 정의 -> 겹치는 숫자가 5개일 때, 보너스 번호까지 당첨이면 +2를 하여 1등과 차별을 둠.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -28,10 +28,17 @@ public class Application extends Lotto {
         List<Integer> myLotto = null;
         String price = "";
 
+        int firstPrize = 0;
+        int secondPrize = 0;
+        int thirdPrize = 0;
+        int fourthPrize = 0;
+        int fifthPrize = 0;
+
         System.out.println("구입금액을 입력해 주세요.");
         price = Console.readLine();     //구입금액
 
         int howManyTimes = (Integer.parseInt(price))/1000;
+        int[] findPrize = new int[howManyTimes];        //구매한 로또가 각각 몇등인지 담겨있음
         int[][] allMyLotto = new int[howManyTimes][6];
 
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
@@ -50,8 +57,52 @@ public class Application extends Lotto {
 
         System.out.println("당첨 번호를 입력해주세요.");
         String prizeNum = Console.readLine();
+        //split으로 String 배열에 넣어야함
+        String[] getPrizeNum = prizeNum.split(",");
 
         System.out.println("보너스 번호를 입력해주세요.");
         String bonusNum = Console.readLine();
+        for(int i = 0 ; i < howManyTimes; i ++){
+            int cntEqualNumbers = 0;
+            for(int j = 0 ; j < 6 ; j++){
+                if(getPrizeNum[j].equals(allMyLotto[i][j])){
+                    cntEqualNumbers += 1;
+                }
+            }
+            if(cntEqualNumbers == 5){
+                for(int j = 0 ; j < 6 ; j++){
+                    if(getPrizeNum[j].equals(bonusNum)){
+                        cntEqualNumbers += 2;
+                    }
+                }
+            }
+            findPrize[i] += cntEqualNumbers;
+        }
+
+        for(int i = 0 ; i < findPrize.length; i ++){
+            if(findPrize[i] == 6){
+                firstPrize += 1;
+            }
+            else if(findPrize[i] == 7){
+                secondPrize += 1;
+            }
+            else if(findPrize[i] == 5){
+                thirdPrize += 1;
+            }
+            else if(findPrize[i] == 4){
+                fourthPrize += 1;
+            }
+            else if(findPrize[i] == 3){
+                fifthPrize += 1;
+            }
+        }
+
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + fifthPrize + "개");
+        System.out.println("4개 일치 (50,000원) - " + fourthPrize + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + thirdPrize + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + secondPrize + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + firstPrize + "개");
     }
 }
