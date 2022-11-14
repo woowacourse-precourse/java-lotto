8b7e0abcf266361f64c230dbc3a74d50b09ece72
Woojin Lee <dhkep03@gmail.com>
2022-11-14 21:49:32 +0900

    test(InputValidator): 당첨 숫자 유효성 검사 테스트 구현 완료

--- src/test/java/lotto/ui/InputValidatorTest.java
+++ src/test/java/lotto/ui/InputValidatorTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class InputValidatorTest {
     @DisplayName("입력 금액의 유효성을 검사한다.")
     @Nested
-    class inputMoney {
+    class InputMoney {
 
         @DisplayName("숫자가 아닌 문자가 포함 -> 예외 발생")
         @Test
@@ -22,6 +22,32 @@ class InputValidatorTest {
         @Test
         void Over9Digits() {
             assertThatThrownBy(() -> InputValidator.checkInputMoney("1000000000"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @DisplayName("당첨 숫자의 유효성을 검사한다.")
+    @Nested
+    class WinningNumbers {
+
+        @DisplayName("공백 포함 -> 예외 발생")
+        @Test
+        void NotNumber() {
+            assertThatThrownBy(() -> InputValidator.checkInputWinningNumbers("1,2, 3,4,5,6"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("숫자가 아닌 문자 입력 -> 예외 발생")
+        @Test
+        void Over9Digits() {
+            assertThatThrownBy(() -> InputValidator.checkInputWinningNumbers("1,2,3,4,5,a"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("5개의 숫자 입력 -> 예외 발생")
+        @Test
+        void HaveLengthOf5() {
+            assertThatThrownBy(() -> InputValidator.checkInputWinningNumbers("1,2,3,4,5"))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
