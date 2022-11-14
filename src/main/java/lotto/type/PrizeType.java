9b58bc84e0340e9764bfe01bf0e3116e9a284715
Laeng <Laeng@users.noreply.github.com>
2022-11-15 06:43:47 +0900

    fix(lotto): 로또 상금 정보가 제대로 표출되지 않는 문제 수정

--- src/main/java/lotto/type/PrizeType.java
+++ src/main/java/lotto/type/PrizeType.java
@@ -1,6 +1,7 @@
 package lotto.type;
 
 import lotto.model.Score;
+import lotto.util.Lang;
 
 public enum PrizeType {
     FIRST_PRIZE(Score.of(6, 0), 2_000_000_000),
@@ -24,5 +25,20 @@ public enum PrizeType {
 
     public int getReward() {
         return this.reward;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append(Lang.format(Lang.VIEW_MATCH_LOTTO_NUMBERS, condition.getMatchLotto()));
+
+        if (condition.hasBonusNumber()) {
+            result.append(Lang.VIEW_MATCH_BONUS_NUMBER);
+        }
+
+        String reward = String.valueOf(this.reward).replaceAll("\\B(?=(\\d{3})+(?!\\d))", ",");
+        result.append(Lang.format(Lang.VIEW_MONEY_UNIT, reward));
+
+        return result.toString();
     }
 }
