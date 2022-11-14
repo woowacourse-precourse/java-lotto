d96e1e668ffb10b0a64de559b7bab966e033257e
0chil <0@chll.it>
2022-11-15 01:05:57 +0900

    refactor(Lotto): 자료형을 Set으로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -8,45 +8,41 @@ import java.util.stream.Collectors;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
-    private static final int LOTTO_NUMBERS_LENGTH = 6;
-    private static final String LOTTO_NUMBERS_LENGTH_EXCEPTION_MESSAGE = "로또 번호는 " + LOTTO_NUMBERS_LENGTH + "개입니다";
-    private static final String DUPLICATE_EXCEPTION_MESSAGE = "로또 번호에 중복이 있습니다";
+    private static final int NUMBERS_LENGTH = 6;
+    private static final String NUMBERS_LENGTH_EXCEPTION_MESSAGE =
+            "로또 번호는 중복을 제외한 " + NUMBERS_LENGTH + "개입니다.";
 
-    private final List<LottoNumber> numbers;
+    private final Set<LottoNumber> numbers;
 
     public Lotto(Integer... numbers) {
-        this(List.of(numbers));
+        this(Set.of(numbers));
     }
 
     public Lotto(List<Integer> numbers) {
+        this(new HashSet<>(numbers));
+    }
+
+    public Lotto(Set<Integer> numbers) {
         validateLength(numbers);
-        validateNoDuplicate(numbers);
         this.numbers = numbers.stream()
                 .map(LottoNumber::new)
-                .collect(Collectors.toList());
+                .collect(Collectors.toUnmodifiableSet());
     }
 
     public static Lotto createRandomized() {
         return new Lotto(Randoms.pickUniqueNumbersInRange(LottoNumber.RANGE_MINIMUM, LottoNumber.RANGE_MAXIMUM,
-                LOTTO_NUMBERS_LENGTH));
+                NUMBERS_LENGTH));
     }
 
-    private void validateLength(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_NUMBERS_LENGTH) {
-            throw new IllegalArgumentException(LOTTO_NUMBERS_LENGTH_EXCEPTION_MESSAGE);
+    private void validateLength(Set<Integer> numbers) {
+        if (numbers.size() != NUMBERS_LENGTH) {
+            throw new IllegalArgumentException(NUMBERS_LENGTH_EXCEPTION_MESSAGE);
         }
     }
 
-    private void validateNoDuplicate(List<Integer> numbers) {
-        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-        if (uniqueNumbers.size() != numbers.size()) {
-            throw new IllegalArgumentException(DUPLICATE_EXCEPTION_MESSAGE);
-        }
-    }
-
-    public long countMatchesWith(Lotto winningLotto) {
+    public long countMatchesWith(Lotto other) {
         return numbers.stream()
-                .filter(winningLotto::contains)
+                .filter(other::contains)
                 .count();
     }
 
