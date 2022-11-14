33ff404c4fde200b6e5767da95eb7d9638a5d419
zaysverse <zeze@jbnu.ac.kr>
2022-11-14 17:29:03 +0900

    feat(lotto): 로또 구입 금액을 입력받고 로또의 수량을 계산한다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입 금액을 입력해주세요.");
+        int purchaseAmount = Integer.parseInt(Console.readLine());
+        int lottoQuantity = purchaseAmount/1000;
+        try {
+            if(purchaseAmount%1000!=0)
+                throw new IllegalArgumentException();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR]");
+        }
     }
 }
