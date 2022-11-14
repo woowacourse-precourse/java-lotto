f681e5d6e10f9e037c1e60b91bdf746c6addf84d
heesang <dlagmltkd12@naver.com>
2022-11-15 03:01:35 +0900

    test: 리팩토링으로 인해 LottoValidationUtilsTest에 있는 로직 LottoTest로 이동

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -10,8 +10,41 @@ import java.util.List;
 
 import static lotto.dto.LottoResponseDtos.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTest {
+
+    @DisplayName("로또 번호의 개수가 6개가 아니면 예외 발생")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호의 개수는 6개 이어야 합니다.");
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외 발생")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호에는 중복 된 숫자가 없어야합니다.");
+    }
+
+    @DisplayName("로또 번호 중 0이 있으면 예외 발생")
+    @Test
+    void createLottoByOutMinimumRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1~45 사이의 숫자 이어야 합니다.");
+    }
+
+    @DisplayName("로또 번호 중 46이 있으면 예외 발생")
+    @Test
+    void createLottoByOutMaximumRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1~45 사이의 숫자 이어야 합니다.");
+    }
 
     @DisplayName("한개의 로또를 오름차순으로 정렬하는 기능")
     @CsvSource(value = {"0,4", "1,7", "2,9", "3,10", "4,12", "5,14"})
