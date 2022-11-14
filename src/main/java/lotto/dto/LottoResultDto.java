f44c569a7cc8fe6193dd6d385691ba1e154d9d5c
Choi-JJunho <junho5336@gmail.com>
2022-11-14 20:24:11 +0900

    feat(LottoResultDto): add LottoResultDto

---
+++ src/main/java/lotto/dto/LottoResultDto.java
@@ -0,0 +1,27 @@
+package lotto.dto;
+
+import lotto.domain.Validation;
+
+import java.util.List;
+
+public class LottoResultDto {
+    private static final Validation validator = new Validation();
+    private final List<Integer> basicNumbers;
+    private final Integer bonusNumber;
+
+    public LottoResultDto(List<Integer> basicNumbers, Integer bonusNumber) {
+        validator.validateBasicNumber(basicNumbers);
+        validator.validateBonusNumber(basicNumbers, bonusNumber);
+        this.basicNumbers = basicNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> getBasicNumbers() {
+        return basicNumbers;
+    }
+
+    public Integer getBonusNumber() {
+        return bonusNumber;
+    }
+
+}
