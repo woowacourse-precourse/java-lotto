7f1c4d8376d071a2e5b87b4609498241bb17c4f5
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-15 03:03:00 +0900

    feat(Ranking): enum 클래스 setup

--- src/main/java/lotto/Ranking.java
+++ src/main/java/lotto/Ranking.java
@@ -1,2 +1,21 @@
-package lotto;public enum Ranking {
+package lotto;
+
+public enum Ranking {
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - ", false),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - ", true),
+    THIRD(5, 1500000, "5개 일치 (1,500,000원) - ", false),
+    FOURTH(4, 50000, "4개 일치 (50,000원) - ", false),
+    FIFTH(3, 5000, "3개 일치 (5,000원) - ", false);
+
+    private int match, prize;
+    private String description;
+    private boolean bonusMatch;
+
+    Ranking(int match, int prize, String description, boolean bonusMatch) {
+        this.match = match;
+        this.prize = prize;
+        this.description = description;
+        this.bonusMatch = bonusMatch;
+    }
+
 }
