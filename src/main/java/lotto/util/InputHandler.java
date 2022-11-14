d7cadf5877b7265fd9ec2eb0f97f28051c929fae
sangho-jeon <jeonsangho1587@gmail.com>
2022-11-14 15:05:14 +0900

    feat      : adding validation (range, size, duplication)

--- src/main/java/lotto/util/InputHandler.java
+++ src/main/java/lotto/util/InputHandler.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,21 +15,46 @@ public class InputHandler {
     public static List<Integer> getWinningNumber() {
         String userInput = Console.readLine();
         List<Integer> winningNumber = validateType(userInput);
+        validateSize(winningNumber);
+        validateRange(winningNumber);
+        validateUnique(winningNumber);
         return winningNumber;
     }
 
-    private static List<Integer> validateType(String userInput) throws IllegalArgumentException {
+    public static int getBonusNumber() throws ExceptionHandler {
+        String userInput = Console.readLine();
+        return Integer.parseInt(userInput);
+    }
+
+    private static List<Integer> validateType(String userInput) throws ExceptionHandler {
         List<Integer> validatedNumbers;
         validatedNumbers = Arrays.stream(userInput.split(",")).map(Integer::parseInt).collect(Collectors.toList());
         System.out.println(validatedNumbers);
         return validatedNumbers;
     }
 
-    private void validateRange(List<Integer> userInput) {
-
+    private static void validateSize(List<Integer> userInput) {
+        if (userInput.size() != CONSTANTS.LOTTO_COUNT.getNumbers()) {
+            throw new ExceptionHandler("number exception");
+        }
     }
 
-    private void validateUnique(List<Integer> userInput) {
+    private static void validateRange(List<Integer> userInput) {
+        for (Integer number :
+                userInput) {
+            if (number > CONSTANTS.MAXIMUM.getNumbers() || number < CONSTANTS.MINIMUM.getNumbers()) {
+                throw new ExceptionHandler("number exception");
+            }
+        }
+    }
 
+    private static void validateUnique(List<Integer> userInput) {
+        HashSet<Integer> numbers = new HashSet<>();
+        for (Integer number :
+                userInput) {
+            if (numbers.add(number)) {
+                throw new ExceptionHandler("test");
+            }
+        }
     }
 }
