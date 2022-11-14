e71cec32bea175cd0a460244a8f93ed6614b348f
이병3 <101378867+SongJSeop@users.noreply.github.com>
2022-11-14 14:55:43 +0900

    기능 1-3. 사용자 발행 로또 수량 출력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,8 +11,13 @@ public class Application {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         int input_money = Integer.parseInt(input);
+        int user_buy_num = input_money / 1000;
+
         if (input_money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위의 금액을 입력해 주세요.");
-        } return input_money / 1000;
+        }
+
+        System.out.println("\n" + user_buy_num + "개를 구매했습니다.");
+        return user_buy_num;
     }
 }
