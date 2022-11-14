f4495af51fa4c8fbf4cd9ded8480a30cfb27d0d5
devMuscle <hongsbien@naver.com>
2022-11-14 21:23:41 +0900

    refactor: User 생성자 변경에 따른 코드 수정

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,18 +9,26 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class UserTest {
+    private List<Lotto> lottos;
+    private Lotto lotto;
+
+    @BeforeEach
+    void setUp() {
+        lotto = new Lotto(List.of(1,2,3,4,5,6));
+        lottos.add(lotto);
+    }
 
     @DisplayName("숫자가 아닌 것을 입력하면 예외가 발생한다.")
     @Test
     void inputByNotNumber() {
-        assertThatThrownBy(() -> new User("ABC"))
+        assertThatThrownBy(() -> new User("ABC", lottos))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("숫자가 아닌 것을 입력하면 예외가 발생한다.")
     @Test
     void inputByNotDividedBy1000() {
-        assertThatThrownBy(() -> new User("15001"))
+        assertThatThrownBy(() -> new User("15001", lottos))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
