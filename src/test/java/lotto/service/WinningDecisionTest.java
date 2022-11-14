a6f5a12437573f4b9ed816945e0977dde6b8219c
Gibeom Nam <rlqja0523@naver.com>
2022-11-15 00:05:13 +0900

    test: 당첨 내역 출력 결과 테스트

--- src/test/java/lotto/service/WinningDecisionTest.java
+++ src/test/java/lotto/service/WinningDecisionTest.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WinningDecisionTest {
@@ -55,5 +56,26 @@ public class WinningDecisionTest {
         // then
         Map<Integer, Integer> winningPrizes = winningDecision.getWinningPrizes();
         assertEquals(3, winningPrizes.get(WinningResult.MATCH_THREE.getPrize()));
+    }
+
+    @Test
+    @DisplayName("당첨 내역을 출력한다.")
+    void showWinningDecisionTest() {
+        // given
+        winningDecision.setMyPrizes();
+
+        // when
+        winningDecision.showWinningDecision();
+
+        // then
+        String expected = "당첨 통계\n" +
+                "---\r\n" + // for Windows
+                "3개 일치 (5,000원) - 3개\n" +
+                "4개 일치 (50,000원) - 0개\n" +
+                "5개 일치 (1,500,000원) - 0개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n" +
+                "6개 일치 (2,000,000,000원) - 0개\n" +
+                "총 수익률은 100.0%입니다.";
+        assertThat(output.toString()).isEqualTo(expected);
     }
 }
