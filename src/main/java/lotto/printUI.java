8179e469add1b326c19f342669526200b33bc0f8
Sunghyun-0 <zkdldk95@naver.com>
2022-11-15 03:54:11 +0900

    출력 기능 printUI로 이동

--- src/main/java/lotto/printUI.java
+++ src/main/java/lotto/printUI.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static lotto.LottoState.*;
 
+import java.util.List;
 import java.util.Map;
 
 public class printUI {
@@ -21,6 +22,20 @@ public class printUI {
         System.out.println("당첨 통계\n___");
     }
 
+    public static void printMoneyErrorMessage(){
+        System.out.println("[ERROR] 잘못된 금액이 입력되었습니다.");
+    }
+
+    public static void printInputErrorMessage(){
+        System.out.println("[ERROR] 잘못된 번호가 입력되었습니다.");
+    }
+
+    public static void printLottos(List<Lotto> Lottos){
+        for(Lotto lotto : Lottos){
+            System.out.println(lotto.asSortedList());
+        }
+    }
+
     public static void printResult(Map<String,Integer> allResult){
         System.out.println(MATCH_THREE.getName() + allResult.get("3") + "개");
         System.out.println(MATCH_FOUR.getName() + allResult.get("4") + "개");
@@ -29,12 +44,7 @@ public class printUI {
         System.out.println(MATCH_SIX.getName() + allResult.get("6") + "개");
     }
 
-    public static void printProfitRate(Map<String,Integer> allResult , int money){
-        int totalProfit = allResult.get("3") * 5000 +
-                allResult.get("4") * 50000 +
-                allResult.get("5") * 1500000 +
-                allResult.get("7") * 30000000 +
-                allResult.get("6") * 2000000000;
-        System.out.println("총 수익률은 " + ((double)totalProfit / money * 100) + "%입니다.");
+    public static void printProfitRate(String totalProfitRate){
+        System.out.println("총 수익률은 " + totalProfitRate + "%입니다.");
     }
 }
