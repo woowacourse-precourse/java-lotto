6bc21ae1e5760fd7d4409c89ee568432afd1e81a
Ran <lan4250@naver.com>
2022-11-14 14:30:20 +0900

    FEAT/ inputPurchaseAmount()를 이용해서 로또 구입금액 입력 받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+
+    static long purchaseAmount = 0;
+    static int numberOfTickets = 0;
+
+
+    public static void inputPurchaseAmount() {
+
+        String strAmount = Console.readLine();
+        long amount = Long.parseLong(strAmount);
+
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("1000의 배수만 입력 가능합니다.");
+        }
+        purchaseAmount = amount;
+        numberOfTickets = (int) amount / 1000;
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        inputPurchaseAmount();
+
     }
 }
