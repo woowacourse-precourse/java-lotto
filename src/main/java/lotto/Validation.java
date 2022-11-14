6393fc9a1255ea4ba1c5711a89ce9b0734437771
LAPTOP-4NC4E864\yongs <yongs170@naver.com>
2022-11-14 14:03:36 +0900

    feat(Validation) : numberDuplicateValidation 기능 구현
    로또 번호중 중복된 수가 있으면 예외 발생

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -10,14 +10,22 @@ public class Validation {
 
     public static final String LOTTO_NUMBER_RANGE_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     public static final String NUMBER_COUNT_MESSAGE = "[ERROR] 로또 번호는 6개입니다.";
+    public static final String NUMBER_DUPLICATE_MESSAGE = "[ERROR] 로또 번호는 중복될 수 없습니다.";
    public static void numberCountValidation(List<Integer> numbers){
        if(numbers.size() != LOTTO_COUNT) throw new IllegalArgumentException(NUMBER_COUNT_MESSAGE);
    }
    public static void numberRangeValidation(List<Integer> numbers){
        int numbersCount = numbers.stream()
-               .filter(element->element >= 1 && element <= 45)
+               .filter(number->number >= 1 && number <= 45)
                .collect(Collectors.toList()).size();
 
        if(numbersCount != LOTTO_COUNT) throw new IllegalArgumentException(LOTTO_NUMBER_RANGE_MESSAGE);
+   }
+
+   public static void numberDuplicateValidation(List<Integer> numbers){
+       int numbersCount = numbers.stream()
+               .filter(number->number >= 1 && number <= 45)
+               .collect(Collectors.toSet()).size();
+       if(numbersCount != LOTTO_COUNT) throw new IllegalArgumentException(NUMBER_DUPLICATE_MESSAGE);
    }
 }
