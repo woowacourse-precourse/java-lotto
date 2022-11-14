e1260dd31c1c61af98755c78b04768f54b7b6d17
June <68291395+june-777@users.noreply.github.com>
2022-11-14 19:49:15 +0900

    refactor(로또 구입금액 입력): 로또 생성 명령 메소드콜 기능 삭제
    View와 Model의 의존성을 낮추기 위해 Controller에서 명령을 내리는 방법으로 리팩토링

--- src/main/java/view/InputLottoPurchaseAmount.java
+++ src/main/java/view/InputLottoPurchaseAmount.java
@@ -8,17 +8,15 @@ import controller.LottoCreate;
 
 public class InputLottoPurchaseAmount {
 	
-	public static long readLottoPurchaseAmount() {
+	public long readLottoPurchaseAmount() {
 		System.out.println(INPUT_TEXT_PURCHASE_AMOUNT);
 		String purchaseAmount = Console.readLine();
 		long purchaseAmountResult = validate(purchaseAmount);
 		
-		LottoCreate lottoCreate = new LottoCreate();
-		lottoCreate.createLotto(purchaseAmountResult);
 		return purchaseAmountResult;
 	}
 
-	private static long validate(String purchaseAmount) {
+	private long validate(String purchaseAmount) {
 		validateNumeric(purchaseAmount);
 		long purchaseAmountResult = Long.parseLong(purchaseAmount);
 		validateMinimumPurchaseAmount(purchaseAmountResult);
@@ -28,26 +26,26 @@ public class InputLottoPurchaseAmount {
 		return purchaseAmountResult;
 	}
 
-	private static void validateNumeric(String purchaseAmount) {
+	private void validateNumeric(String purchaseAmount) {
 		String regex = ALL_CHARACTERS_EXCEPT_NUMBERS;
 		if (!purchaseAmount.matches(regex)) {
 			throw new IllegalArgumentException(ERROR_PURCHASE_AMOUNT_NON_NUMERIC);
 		}
 	}
 
-	private static void validateMinimumPurchaseAmount(long purchaseAmountResult) {
+	private void validateMinimumPurchaseAmount(long purchaseAmountResult) {
 		if (purchaseAmountResult < PURCHASE_AMOUNT_MINIMUM) {
 			throw new IllegalArgumentException(ERROR_PURCHASE_AMOUNT_LESS_THAN_MINIMUM);
 		}
 	}
 
-	private static void validateMaximumPurchaseAmount(long purchaseAmountResult) {
+	private void validateMaximumPurchaseAmount(long purchaseAmountResult) {
 		if (purchaseAmountResult > PURCHASE_AMOUNT_MAXIMUM) {
 			throw new IllegalArgumentException(ERROR_PURCHASE_AMOUNT_MORE_THAN_MAXIMUM);
 		}
 	}
 
-	private static void validatePurchaseAmountUnit(long purchaseAmountResult) {
+	private void validatePurchaseAmountUnit(long purchaseAmountResult) {
 		if (purchaseAmountResult % PURCHASE_AMOUNT_UNIT != 0) {
 			throw new IllegalArgumentException(ERROR_PURCHASE_AMOUNT_INVALID_UNIT);
 		}
