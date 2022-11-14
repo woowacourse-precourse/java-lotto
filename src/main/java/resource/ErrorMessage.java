38d46e9ecb979ba5ef898a0e56f0de671f649e0d
dener8 <8dener@gmail.com>
2022-11-14 22:31:52 +0900

    rename: message 관련 (ErrorMessage, GeneralMessage) 모두 resource 패키지로 이동

--- src/main/java/resource/ErrorMessage.java
+++ src/main/java/resource/ErrorMessage.java
@@ -3,4 +3,6 @@ package lotto;
 public class ErrorMessage {
     public static final String OUT_OF_RANGE_ERROR = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     public static final String OUT_OF_SIZE_ERROR = "[ERROR] 로또 번호는 총 6개여야 합니다.";
+    public static final String OUT_OF_PURCHASE_AMOUNT_ERROR = "[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.";
+    public static final String DUPLICATE_ERROR = "[ERROR] 중복된 번호는 선택할 수 없습니다.";
 }
