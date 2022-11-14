d3e3afed18bfa814c58f590cf5bfc80f2fc4a03a
ryanha <ryanha@handong.ac.kr>
2022-11-15 00:24:57 +0900

    refactor(domain): Validator에서 불필요한 반환값 삭제, throws 매커니즘 적용

--- src/main/java/lotto/domain/Validator.java
+++ src/main/java/lotto/domain/Validator.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 import lotto.view.IOProcessor;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -9,38 +8,28 @@ import java.util.Set;
 
 public class Validator {
 
-    public static boolean amountUnitIsValid(String userInput, int unit) {
+    public static void amountIsValid(String userInput, int unit) throws IllegalArgumentException {
         isNumber(userInput);
         int amount = Integer.parseInt(userInput);
-        if(amount % unit != 0) {
-            IOProcessor.printErrorMessage("구매금액의 단위가 " + unit + "원이 아닙니다.");
-            throw new IllegalArgumentException();
-        }
-        return true;
+        amountUnitIsValid(amount, unit);
     }
 
-    public static List<Integer> winningNumberIsValid(String userInput) {
+    public static List<Integer> winningNumberIsValid(String userInput) throws IllegalArgumentException {
         String[] numbers = winningNumberFormatIsValid(userInput);
         List<Integer> winningNumbers = new ArrayList<>();
         for(String number : numbers) {
             winningNumbers.add(Integer.parseInt(number));
         }
         for(int number : winningNumbers) {
-            if(!isValidRange(number, 1, 45)) {
-                IOProcessor.printErrorMessage("당첨번호 값의 범위가 올바르지 않습니다.");
-                throw new IllegalArgumentException();
-            }
+            isValidRange(number, 1, 45);
         }
         return eachNumberIsUnique(winningNumbers);
     }
 
-    public static int bonusNumberIsValid(String userInput, List<Integer> winningNumber) {
+    public static int bonusNumberIsValid(String userInput, List<Integer> winningNumber) throws IllegalArgumentException {
         isNumber(userInput);
         int bonusNumber = Integer.parseInt(userInput);
-        if(!isValidRange(bonusNumber, 1, 45)) {
-            IOProcessor.printErrorMessage("입력한 값의 범위가 올바르지 않습니다.");
-            throw new IllegalArgumentException();
-        }
+        isValidRange(bonusNumber, 1, 45);
         if(winningNumber.contains(bonusNumber)) {
             IOProcessor.printErrorMessage("입력한 보너스 번호가 이미 당첨번호에 있습니다.");
             throw new IllegalArgumentException();
@@ -48,14 +37,20 @@ public class Validator {
         return bonusNumber;
     }
 
-    private static boolean isNumber(String userInput) {
+    private static void isNumber(String userInput) {
         for (int i = 0; i < userInput.length(); i++) {
             if (!Character.isDigit(userInput.charAt(i))) {
                 IOProcessor.printErrorMessage("입력 내용이 숫자로 구성되어 있지 않습니다.");
                 throw new IllegalArgumentException();
             }
         }
-        return true;
+    }
+
+    private static void amountUnitIsValid(int amount, int unit) {
+        if(amount % unit != 0) {
+            IOProcessor.printErrorMessage("구매금액의 단위가 " + unit + "원이 아닙니다.");
+            throw new IllegalArgumentException();
+        }
     }
 
     private static String[] winningNumberFormatIsValid(String userInput) {
@@ -72,8 +67,11 @@ public class Validator {
         return numbers;
     }
 
-    private static boolean isValidRange(int number, int start, int end) {
-        return (number >= start && number <= end);
+    private static void isValidRange(int number, int start, int end) {
+        if (number < start || number > end) {
+            IOProcessor.printErrorMessage("입력한 값의 범위가 올바르지 않습니다.");
+            throw new IllegalArgumentException();
+        }
     }
 
     private static List<Integer> eachNumberIsUnique(List<Integer> numbers) {
