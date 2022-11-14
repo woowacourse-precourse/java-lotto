c4b3c31fabc112941d364be0ef3b42985950b47d
bellljun3395 <195850@jnu.ac.kr>
2022-11-14 15:11:15 +0900

    feat(LottoNumbers): 구현 validateDuplicatedNumber
    - lottoNumbers의 중복을 검증한다.

--- src/main/java/lotto/domain/lotto/LottoNumbers.java
+++ src/main/java/lotto/domain/lotto/LottoNumbers.java
@@ -10,10 +10,12 @@ public class LottoNumbers {
     private final int LOTTO_NUMBER_SIZE = 6;
     private final String WARM_LOTTO_NUMBERS_SIZE = WARM + "로또 번호는 " + LOTTO_NUMBER_SIZE + "개이어야 합니다.";
 
+
     private List<LottoNumber> lottoNumbers;
 
     public LottoNumbers(List<LottoNumber> lottoNumbers) {
         validate(lottoNumbers);
+        validateDuplicatedNumber(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
     }
 
@@ -21,9 +23,15 @@ public class LottoNumbers {
         return lottoNumbers;
     }
 
+
     private void validate(List<LottoNumber> numbers) {
         Validate.validateSize(numbers, WARM_LOTTO_NUMBERS_SIZE,
                 lottoNumbers -> lottoNumbers.size() != LOTTO_NUMBER_SIZE);
+    }
+
+    private void validateDuplicatedNumber(List<LottoNumber> lottoNumbers) {
+        Validate.validateDuplicateNumber(lottoNumbers,
+                (baseSize, compareLottoNumbers) -> baseSize != compareLottoNumbers.size());
     }
 
 }
