5c180cdfbc88ae99bd3e3581a6d319891d6471bf
rvlwldev <rvlwlasd77@gmail.com>
2022-11-14 19:57:17 +0900

    fix: Application에서 실행 시 try-catch로 메세지 나오게 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,7 +5,10 @@ import lotto.Domain.LottoShop;
 public class Application {
     public static void main(String[] args) {
         LottoShop shop = new LottoShop();
-
-        shop.purchase();
+        try {
+            shop.purchase();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
