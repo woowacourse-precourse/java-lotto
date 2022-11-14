6756449a2547355382e92c355a64386c93673f1d
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-11-14 23:13:06 +0900

    fix(View): View 수정

--- src/main/java/lotto/view/GameOutput.java
+++ src/main/java/lotto/view/GameOutput.java
@@ -1,22 +1,47 @@
 package lotto.view;
 
+import lotto.utils.RewardType;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class GameOutput {
+    private static final String lotto_num = "개를 구매했습니다.";
+    private static final String count = "개";
+    private static final String fifth_result = "3개 일치 (5,000원) - ";
+    private static final String fourth_result = "4개 일치 (50,000원) - ";
+    private static final String third_result = "5개 일치 (1,500,000원) - ";
+    private static final String second_result = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    private static final String first_result = "6개 일치 (2,000,000,000원) - ";
+    private static final String rating_result1 = "총 수익률은 ";
+    private static final String rating_result2 = "%입니다.";
+
+
     public static void getLottoNum(int n){
         System.out.println("${n}개를 구매했습니다.");
     }
-    public static void getResult(){
-        System.out.println("당첨 통계")
+    public static void getDetailResults(int money, List<Integer> RewardList){
+        getDetailResult1(RewardList);
+        getDetailResult2(money, RewardList);
+
     }
-    public static void getDetailResult(int n, int count){
-        if (n==3){
-            System.out.println("${n}개 일치", "(5000원)", '-',"${count}개")
+    public static void getDetailResult1(List<Integer> RewardList){
+        if (RewardList.subList(1,6)!=Arrays.asList(0,0,0,0,0)){
+            System.out.println(first_result+RewardList.get(1)+count);
+            System.out.println(second_result+RewardList.get(2)+count);
+            System.out.println(third_result+RewardList.get(3)+count);
+            System.out.println(fourth_result+RewardList.get(4)+count);
+            System.out.println(fifth_result+RewardList.get(5)+count);
         }
-        if (n==4){
-            System.out.println("${n}개 일치", "(50000원)", '-',"${count}개")
-        }
-        //여기 상수처리
+    }
+    public static void getDetailResult2(int money,List<Integer> RewardList){
+        int totalmoney=0;
+        totalmoney+=RewardList.get(1)*RewardType.FIRST.getreward();
+        totalmoney+=RewardList.get(2)*RewardType.SECOND.getreward();
+        totalmoney+=RewardList.get(3)*RewardType.THIRD.getreward();
+        totalmoney+=RewardList.get(4)*RewardType.FOURTH.getreward();
+        totalmoney+=RewardList.get(5)*RewardType.FIFTH.getreward();
 
-
-        System.out.println("총 수익률은 ${}%입니다.");
+        System.out.println(rating_result1+money/totalmoney+rating_result2);
     }
 }
