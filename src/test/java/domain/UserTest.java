2fa1aa1ce510bc8f77511262839fb178afff3541
Kim-Jong-Gyu <whdrb2643@gmail.com>
2022-11-14 22:45:50 +0900

    docs(README): 기능 구현 목록 추가
    기능 구현 목록 추가:
    - 유저의 로또 번호를 알려준다.

--- src/test/java/domain/UserTest.java
+++ src/test/java/domain/UserTest.java
@@ -7,9 +7,4 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
-    @DisplayName("금액의 입력값이 천 단위가 아닐시 에러가 발생한다.")
-    @Test
-    void inputUserMoneyIncorrect() {
-        assertThatThrownBy(() -> new User(1500)).isInstanceOf(IllegalArgumentException.class).isEqualTo("[ERROR] 금액 입력값은 1000원 단위여야 합니다");
-    }
 }
