ba6fdd47dcaac04288c953f82da48d0a2a9daf74
bomin1996 <bomin199628@gmail.com>
2022-11-14 20:01:59 +0900

    <feat> : 구매된 로또 번호생성, 당첨번호 입력 기능.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,20 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("구입금액을 입력해 주세요.");
         Integer numOfLotto = isUserPaymentValid(userPayment());
+        System.out.println(numOfLotto+"개를 구매했습니다.");
+        List<List<Integer>> userLottoTotal = userLottoNumber(numOfLotto);
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<Integer> prize = prizeNumber(Console.readLine());
     }
     public static String userPayment () {
         return Console.readLine();
@@ -25,5 +33,25 @@ public class Application {
             System.out.println("ERROR");
         }
         return numberOfLotto/1000;
+    }
+    public static List<List<Integer>> userLottoNumber (Integer numOfLotto) {
+        List<List<Integer>> userLotto = new ArrayList<>();
+        for ( int i = 0 ; i < numOfLotto ; i++) {
+            List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lottoNumberResult = new Lotto(lottoNumber);
+            List<Integer> lottoNum = new ArrayList<>(lottoNumberResult.getNumbers());
+            Collections.sort(lottoNum);
+            System.out.println(lottoNum);
+            userLotto.add(lottoNumberResult.getNumbers());
+        }
+        return userLotto;
+    }
+    public static List<Integer> prizeNumber (String prizeInput) {
+        List<String> list = Arrays.asList(prizeInput.split(","));
+        List<Integer> prizeNumberResult = list.stream()
+                .map(s -> Integer.parseInt(s))
+                .collect(Collectors.toList());
+
+        return prizeNumberResult;
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,6 +15,8 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
-
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
     // TODO: 추가 기능 구현
 }
