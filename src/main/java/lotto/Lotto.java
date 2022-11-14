d1f2981575df221cfbaa4203b29dba2546aed69d
최형준 <choihyoungjun@choehyeongjun-ui-MacBookPro.local>
2022-11-14 23:14:24 +0900

    resoleved fail

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,8 +11,21 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        try {
+            if (numbers.size() != 6) {
+                throw new IllegalArgumentException("[ERROR] 로또번호는 6개여야 합니다.");
+            }
+            for (int i = 0; i < 6; i++) {
+                if (numbers.get(i) > 45) {
+                    throw new IllegalArgumentException("[ERROR] 로또번호는 46보다 작아야합니다");
+                } else if (numbers.get(i) < 1) {
+                    throw new IllegalArgumentException("[ERROR] 로또번호는 0보다 커야합니다");
+                } else if (numbers.size() != numbers.stream().distinct().count()) {
+                    throw new IllegalArgumentException("[ERROR] 로또번에 중복된 숫자가 존재합니다");
+                }
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 로또번호 입력이 잘못됐습니다.");
         }
     }
 
