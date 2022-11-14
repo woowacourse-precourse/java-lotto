bbdab7011f6ac726a60aca4d89a609f709ed4878
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-15 03:30:42 +0900

    feat(Program): 유저의 구입 금액을 입력받아 로또 셋업하는 메소드 lottoSetup 구현

--- src/main/java/lotto/Program.java
+++ src/main/java/lotto/Program.java
@@ -1,2 +1,14 @@
-package lotto;public class Program {
+package lotto;
+
+public class Program {
+    View view = new View();
+    ListOfLottos lottoList;
+    int numOfLottos;
+    public void setup() {
+        view.printMoneyInputMsg();
+        numOfLottos = view.getMoneyInput();
+        view.printNumberOfLottosMsg(numOfLottos);
+        lottoList = new ListOfLottos(numOfLottos);
+        lottoList.printAllLottos();
+    }
 }
