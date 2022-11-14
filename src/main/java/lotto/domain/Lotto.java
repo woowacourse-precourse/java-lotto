6dd53d09dd2ba7ba2bd9a85eb618efe3e5251879
hyun0404woo <hyun0404woo@gmail.com>
2022-11-14 21:18:47 +0900

    feat: 로또 생성 시 유효성 검사를 위한 nested enum 클래스 생성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,15 +2,22 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.util.ConstValue.LottoConst;
+import lotto.util.Validator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static lotto.domain.Lotto.LottoNumberValidator.validate;
+import static lotto.exception.ExceptionType.*;
+import static lotto.util.ConstValue.LottoConst.LOTTO_LENGTH;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(final List<Integer> numbers) {
+        validate(numbers);
         this.numbers = numbers;
     }
 
@@ -22,4 +29,45 @@ public class Lotto {
         return new Lotto(sortedLottoNumber);
     }
 
+    public enum LottoNumberValidator {
+
+        LOTTO_NUMBER_SIZE((List<Integer> numbers) -> {
+            if (numbers.size() != LOTTO_LENGTH) {
+                throw new IllegalArgumentException(LOTTO_NUMBER_LENGTH_ERROR.getMessage());
+            }
+        }),
+
+        LOTTO_NUMBER_DISTINCT((List<Integer> numbers) -> {
+            final int distinctListSize = (int) numbers.stream().distinct().count();
+            if (numbers.size() != distinctListSize) {
+                throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATE.getMessage());
+            }
+        }),
+
+        LOTTO_NUMBER_RANGE((List<Integer> numbers) -> {
+            final List<Integer> outOfRangeNumbers = numbers.stream()
+                    .filter(LottoNumberValidator::isLottoNumberOutOfRange)
+                    .collect(Collectors.toList());
+            if (outOfRangeNumbers.size() > 0) {
+                throw new IllegalArgumentException(LOTTO_NUMBER_OUT_OF_RANGE.getMessage() +
+                        "\n잘못된 숫자 : " + outOfRangeNumbers.stream().map(String::valueOf)
+                        .collect(Collectors.joining(","))
+                );
+            }
+        });
+
+        private final Validator<List<Integer>> validator;
+
+        LottoNumberValidator(final Validator<List<Integer>> validator) {
+            this.validator = validator;
+        }
+
+        public static void validate(final List<Integer> numbers) {
+            Arrays.stream(values()).forEach(v -> v.validator.validate(numbers));
+        }
+
+        public static boolean isLottoNumberOutOfRange(final int number) {
+            return number < LottoConst.START_NUMBER || number > LottoConst.END_NUMBER;
+        }
+    }
 }
