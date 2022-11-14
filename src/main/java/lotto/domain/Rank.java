21602506a36e01aebf0c3d7ea631e52ebfb9fc6e
Isu Shin <isu010614@naver.com>
2022-11-15 06:59:41 +0900

    feat: 로또 당첨 개수와 각 당첨금을 담은 enum 추가

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+public enum Rank {
+    NOTHING(0, 0),
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 1_500_000),
+    FIVE_AND_BONUS(5, 30_000_000),
+    SIX(6, 2_000_000_000);
+
+    private final int matchNumber;
+    private final int prize;
+
+    Rank(final int matchNumber, final int prize) {
+        this.matchNumber = matchNumber;
+        this.prize = prize;
+    }
+
+    public int matchNumber() {
+        return matchNumber;
+    }
+
+    public int prize() {
+        return prize;
+    }
+}
