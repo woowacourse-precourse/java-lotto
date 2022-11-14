eb9dc62e4fd3c7a2db5b6d2c48b91d6f0787e37e
olrlobt <317tmdgjs@naver.com>
2022-11-14 11:23:55 +0900

    feat: 보너스 번호를 입력 받음

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -24,6 +24,7 @@ public class LottoController {
 
         String inputWinningNumber = message.inputWinningNumberMessage();
         lottoService.setWinningNumber(inputWinningNumber);
+        String inputBonusNumber = message.inputBonusNumberMessage();
     }
 
 }
--- src/main/java/lotto/view/Message.java
+++ src/main/java/lotto/view/Message.java
@@ -21,7 +21,14 @@ public class Message {
     }
 
     public String inputWinningNumberMessage() {
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String inputBonusNumberMessage() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
 }
