6da4ec8aa9278f32919a109723bef65a5c9b29b8
wormjoo <wormjoo@naver.com>
2022-11-15 01:17:10 +0900

    feat: 당첨 번호, 보너스 번호의 일치 값에 따른 등수 HashMap 구현

--- src/main/java/lotto/domain/LottoManager.java
+++ src/main/java/lotto/domain/LottoManager.java
@@ -3,7 +3,10 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,9 +14,17 @@ public class LottoManager {
     private final LottoGenerator lottoGenerator;
     private List<Integer> luckyNumbers = new ArrayList<>();
     private int bonusNumber = 0;
+    private final Map<List<Integer>, String> prize = new HashMap<>();
 
     public LottoManager() {
         lottoGenerator = new LottoGenerator();
+        prize.put(Arrays.asList(6, 0), "FIRST");
+        prize.put(Arrays.asList(5, 1), "SECOND");
+        prize.put(Arrays.asList(5, 0), "THIRD");
+        prize.put(Arrays.asList(4, 0), "FOURTH");
+        prize.put(Arrays.asList(4, 1), "FOURTH");
+        prize.put(Arrays.asList(3, 0), "FIFTH");
+        prize.put(Arrays.asList(3, 1), "FIFTH");
     }
 
     public List<Lotto> createLottos(int count) {
