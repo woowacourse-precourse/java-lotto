7cc04e2f7b6df9fdde4a7d5d5948d97c5aa1ffe6
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-14 19:48:42 +0900

    refactoring : Enum Rank 수정

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,5 +1,25 @@
 package lotto;
 
 public enum Rank {
-    FIRST, SECOND, THIRD, FORTH, FIFTH
+    THIRD(6, 5_000),
+    FORTH(5, 50_000),
+    FIFTH(3, 1_500_000),
+    FIFTH_BONUS(3, 30_000_000),
+    SIXTH(3, 2_000_000_000);
+
+    private int sameNumber;
+    private int prizeMoney;
+
+    Rank(int sameNumber, int prizeMoney) {
+        this.sameNumber = sameNumber;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public int getSameNumber() {
+        return sameNumber;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
 }
