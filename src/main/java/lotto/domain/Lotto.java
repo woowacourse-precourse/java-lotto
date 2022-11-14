bff3dd2f805f542d74daff72b198541ff9d1aa0a
Woojin Lee <dhkep03@gmail.com>
2022-11-15 00:29:09 +0900

    feat(Lotto): 유효성 검사 로직 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,12 @@
 package lotto.domain;
 
+import static lotto.constant.LottoRule.COUNT_OF_NUMBERS;
+import static lotto.constant.LottoRule.END_NUMBER;
+import static lotto.constant.LottoRule.START_NUMBER;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.ui.UserInterface;
 
 public class Lotto {
@@ -17,10 +23,33 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        checkCountOfNumbers(numbers);
+        checkRangeOfNumberInNumbers(numbers);
+        checkDuplicationOfNumbers(numbers);
+    }
+
+    private static void checkCountOfNumbers(List<Integer> numbers) {
+        if (numbers.size() == COUNT_OF_NUMBERS) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 로또 번호의 개수가 규칙과 다릅니다.");
+    }
+
+    private void checkRangeOfNumberInNumbers(List<Integer> numbers) {
+        for (int number: numbers) {
+            if (START_NUMBER <= number && number <= END_NUMBER) {
+                continue;
+            }
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 범위가 규칙과 다릅니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private static void checkDuplicationOfNumbers(List<Integer> numbers) {
+        Set<Integer> numbersDeleteDuplication = new HashSet<>(numbers);
+        if (numbers.size() == numbersDeleteDuplication.size()) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 로또 번호에 중복이 있습니다.");
+    }
+
 }
