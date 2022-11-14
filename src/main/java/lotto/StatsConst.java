1af15430420be75ae73a5b8205078d237de56bd2
impati <impati0716@icloud.com>
2022-11-14 20:39:49 +0900

    feat(StatsConst) : 통계 상수 정의

---
+++ src/main/java/lotto/StatsConst.java
@@ -0,0 +1,14 @@
+package lotto;
+
+public class StatsConst {
+    public static final int FIRST_SCORE = 60;
+    public static final int SECOND_SCORE = 51;
+    public static final int THIRD_SCORE = 50;
+    public static final int FOUR_SCORE = 40;
+    public static final int FIVE_SCORE = 30;
+    public static final int FIRST_MONEY = 2_000_000_000;
+    public static final int SECOND_MONEY = 30_000_000;
+    public static final int THIRD_MONEY = 1_500_000;
+    public static final int FOUR_MONEY = 50_000;
+    public static final int FIVE_MONEY = 5_000;
+}
