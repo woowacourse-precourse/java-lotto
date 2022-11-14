f59811d5ba65898c744cd0fc3e94bb13da81e30b
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-15 06:19:00 +0900

    feat: add bonus number part

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -8,23 +8,28 @@ public class LottoResult {
         int matchCount = 0;
         for (int numberIndex = 0; numberIndex < 6; numberIndex++) {
             if (ticketNumber.contains(winningNumber.get(numberIndex))) {
-                matchCount++; // 티켓 당 몇개가 맞았는지 적혀있는 리스트 생성
+                matchCount++;
             }
         }
         return matchCount;
     }
 
-    public static LinkedHashMap<Integer, Integer> compareResult(List<Integer> winningNumber, List<List<Integer>> ticketNumber) {
+    public static LinkedHashMap<Integer, Integer> compareResult(List<Integer> winningNumber, List<List<Integer>> ticketNumber,int bonusNumber) {
         LinkedHashMap<Integer, Integer> resultByMatchedNumber = new LinkedHashMap<>();
         List<Integer> results = new ArrayList<>();
 
         for(int ticketIndex=0; ticketIndex<ticketNumber.size(); ticketIndex++){
-            results.add(compareLotteryNumbers(winningNumber,ticketNumber.get(ticketIndex)));
+            int counter= compareLotteryNumbers(winningNumber,ticketNumber.get(ticketIndex));
+            if(counter==5 && ticketNumber.get(ticketIndex).contains(bonusNumber)){
+                counter = 7;
+            }
+            results.add(counter);
         }
 
-        for (int matchingNumbers = 3; matchingNumbers < 7; matchingNumbers++) {
+        for (int matchingNumbers = 3; matchingNumbers < 8; matchingNumbers++) {
             resultByMatchedNumber.put(matchingNumbers,Collections.frequency(results, matchingNumbers));
         }
+
         return resultByMatchedNumber;
     }
 
@@ -33,6 +38,14 @@ public class LottoResult {
         return earnedRatio;
     }
 
+ /*    public static void lotteryPrize(LinkedHashMap<Integer, Integer> resultList){
+        PrizeList prizes[] = PrizeList.values();
 
+         System.out.printf("%d개 일치 (%d원) - %d개",prizeResult.rank(),prizeResult.money(),resultList.get(prizeResult.rank()));
+      for(PrizeList prizeResult:prizes){
+            System.out.printf("%d개 일치 (%d원) - %d개",prizeResult.rank(),prizeResult.money(),resultList.get(prizeResult.rank()));
+        }
+
+    }*/
 
 }
