e84d40578439e0853900bde2d00083e89ff5d91c
jaehee329 <jaehee329@yonsei.ac.kr>
2022-11-14 16:40:31 +0900

    feat(OutMsg): 당첨 통계 관련 문자열 추가

--- src/main/java/lotto/MyView.java
+++ src/main/java/lotto/MyView.java
@@ -40,4 +40,8 @@ public class MyView {
 		model.addAttribute("bonusNumber", bonus);
 		System.out.println();
 	}
+
+	public void printAnalysis() {
+
+	}
 }
--- src/main/java/lotto/OutMsg.java
+++ src/main/java/lotto/OutMsg.java
@@ -5,7 +5,12 @@ public enum OutMsg {
 	BUY_SUFFIX("개를 구매했습니다."),
 	ANSWER_REQUEST("당첨 번호를 입력해 주세요."),
 	BONUS_REQUEST("보너스 번호를 입력해 주세요."),
-	ANALYSIS_PREFIX("당첨 통계\n---");
+	ANALYSIS_PREFIX("당첨 통계\n---"),
+	THREE_HIT("3개 일치 (5,000원) - "),
+	FOUR_HIT("4개 일치 (50,000원) - "),
+	FIVE_HIT_WO_BONUS("5개 일치 (1,500,000원) - "),
+	FIVE_HIT_W_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+	SIX_HIT("6개 일치 (2,000,000,000원) - ");
 
 	private String message;
 
