3cd6a42551b6500646e122bb4054a016e1169f5d
Gibeom Nam <rlqja0523@naver.com>
2022-11-15 03:53:33 +0900

    test: 에러 메시지가 같은지 테스트

--- src/test/java/lotto/exception/CommonExceptionTest.java
+++ src/test/java/lotto/exception/CommonExceptionTest.java
@@ -3,9 +3,11 @@ package lotto.exception;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CommonExceptionTest {
+    private final String notIntMessage = "[ERROR] 숫자를 입력해야 합니다.";
 
     @Test
     @DisplayName("숫자 이외의 값은 에러가 발생한다.")
@@ -15,5 +17,20 @@ public class CommonExceptionTest {
 
         // when, then
         assertThrows(IllegalArgumentException.class, () -> CommonException.checkInteger(inputNumber));
+    }
+
+    @Test
+    @DisplayName("숫자 이외의 값을 받아 에러가 발생하면 [ERROR]로 시작하는 메세지를 반환한다.")
+    void errorMessageTest1() {
+        // given
+        String inputNumber = "일";
+
+        try {
+            // when
+            CommonException.checkInteger(inputNumber);
+        } catch (IllegalArgumentException e) {
+            // then
+            assertThat(e.getMessage()).isEqualTo(notIntMessage);
+        }
     }
 }
