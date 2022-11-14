bf5af1daf5f96a3a90a8884388ea3d16f69daded
채희찬 <eqholic1125@gmail.com>
2022-11-14 22:55:41 +0900

    feat: 구매 갯수 출력 기능 구현

--- src/main/java/lotto/Printer/Printer.java
+++ src/main/java/lotto/Printer/Printer.java
@@ -2,11 +2,16 @@ package lotto.Printer;
 
 public class Printer {
     private static final String PURCHASE_AMOUNT_REQUEST_MESSAGE = "구매금액을 입력해 주세요.";
-
+    private static final String PURCHASE_MESSAGE = "%개를 구매했습니다.";
 
     public void requestPurchaseAmount(){
         System.out.println(PURCHASE_AMOUNT_REQUEST_MESSAGE);
     }
+
+    public void printPurchaseMessage(int numberOfLottos){
+        System.out.printf(PURCHASE_MESSAGE, numberOfLottos);
+    }
+
 
 
 }
