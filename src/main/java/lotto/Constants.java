1c66e52256b739271dc5d2bc07b1a4afcd1fff16
seonpilKim <ksp970306@gmail.com>
2022-11-14 18:21:17 +0900

    Refactor: 공통 상수 위치 이동

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -3,6 +3,7 @@ package lotto;
 public final class Constants {
 
 	public static final String COMMA = ",";
+	public static final String EMPTY = " ";
 
 	private Constants() {
 	}
--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -1,9 +1,10 @@
 package lotto;
 
+import static lotto.Constants.*;
+
 public class ErrorMessage {
 
 	private static final String PREFIX = "[ERROR]";
-	private static final String EMPTY = " ";
 	private static final String INVALID_LOTTO_NUMBER = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 	private static final String INVALID_NUMBER = "숫자가 아닙니다.";
 	private static final String NOT_UNITS_OF_1000 = "1,000원 단위의 금액이여야 합니다.";
