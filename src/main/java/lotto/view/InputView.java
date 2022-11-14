151d4851987f870f3194a719398621278c419517
kim0914 <kosa0914@naver.com>
2022-11-14 19:00:48 +0900

    refactor(InputView): 출력문 함수 분리 및 출력문 내용 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,30 +8,34 @@ import java.util.List;
 
 public class InputView {
 
-    public static final String USER_MONEY_INPUT_MESSAGE = "구입 금액을 입력하세요: ";
-    public static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력하세요: ";
-    public static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력하세요: ";
+    public static final String USER_MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static long getUserMoney() {
-        System.out.print(USER_MONEY_INPUT_MESSAGE);
+        printMessage(USER_MONEY_INPUT_MESSAGE);
 
         String money = Console.readLine();
-        InputValidator.checkDigit(money);
+        InputValidator.checkMoney(money);
         return Long.valueOf(money);
     }
 
     public static List<Integer> getWinningNumber() {
-        System.out.print(WINNING_NUMBER_INPUT_MESSAGE);
+        printMessage(WINNING_NUMBER_INPUT_MESSAGE);
 
         String winningNumber = Console.readLine();
         return Parser.convertStringToIntegerList(winningNumber);
     }
 
     public static int getBonusNumber() {
-        System.out.print(BONUS_NUMBER_INPUT_MESSAGE);
-        String bonusNumber = Console.readLine();
+        printMessage(BONUS_NUMBER_INPUT_MESSAGE);
 
-        InputValidator.checkDigit(bonusNumber);
+        String bonusNumber = Console.readLine();
+        InputValidator.checkBonusNumber(bonusNumber);
         return Integer.valueOf(bonusNumber);
+    }
+
+    public static void printMessage(String message){
+        System.out.println(message);
     }
 }
