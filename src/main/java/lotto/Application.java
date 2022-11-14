a1ce4141372035f766f5393509f14b90a3c06e44
junyong <jymoon1121@gmail.com>
2022-11-14 23:19:22 +0900

    feat(lotto): 기능 목록을 위한 뼈대 구성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.domain.Executor;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Executor executor = new Executor();
+        executor.startGame();
     }
 }
---
+++ src/main/java/lotto/domain/Executor.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+import lotto.view.Printer;
+
+public class Executor {
+    private boolean flag = true;
+    public void startGame() {
+        Printer.inputBuyAmount();
+    }
+}
