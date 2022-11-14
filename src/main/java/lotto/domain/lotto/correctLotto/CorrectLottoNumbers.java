68c402f548580dc512ef5cc5b75ea68e28fa0732
bellljun3395 <195850@jnu.ac.kr>
2022-11-14 15:23:19 +0900

    feat(CorrectLottoNumbers): 구현 validate
    - number의 갯수 검증

--- src/main/java/lotto/domain/lotto/correctLotto/CorrectLottoNumbers.java
+++ src/main/java/lotto/domain/lotto/correctLotto/CorrectLottoNumbers.java
@@ -7,6 +7,9 @@ import lotto.domain.lotto.util.Validate;
 import java.util.List;
 
 public class CorrectLottoNumbers {
+    private final String WARM = "[ERROR] ";
+    private final String WARM_CORRECT_LOTTO_NUMBERS_SIZE = WARM + "로또 당첨 번호는 7개이어야 합니다.";
+    private final int CORRECT_LOTTO_NUMBER_SIZE = 7;
     private final int BONUS_INDEX = 6;
 
 
@@ -15,8 +18,13 @@ public class CorrectLottoNumbers {
     private final LottoNumber bonusNumber;
 
     public CorrectLottoNumbers(List<LottoNumber> correctLottoNumbers) {
+        validate(correctLottoNumbers);
         this.correctLottoNumbers = correctLottoNumbers;
         this.bonusNumber = correctLottoNumbers.get(BONUS_INDEX);
     }
 
+    private void validate(List<LottoNumber> numbers) {
+        Validate.validateSize(numbers, WARM_CORRECT_LOTTO_NUMBERS_SIZE,
+                (lottoNumbers) -> lottoNumbers.size() != CORRECT_LOTTO_NUMBER_SIZE);
+    }
 }
