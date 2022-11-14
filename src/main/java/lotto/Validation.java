07451bb2ad5c1de8a3b95f05aa8d21edcff8d34e
dong yeol <ehfql6363@naver.com>
2022-11-15 02:39:13 +0900

    FEAT(Validation.java) : Create Validation class
    Signed-off-by: dong yeol <ehfql6363@naver.com>

---
+++ src/main/java/lotto/Validation.java
@@ -0,0 +1,45 @@
+package lotto;
+
+import java.util.List;
+
+import static Comment.ErrorMessage.*;
+
+public class Validation {
+    public static void minPriceValidation(int price) {
+        if (price < 1_000) {
+            throw new IllegalArgumentException(MIN_COST_ERROR);
+        }
+    }
+
+    public static void maxPriceValidation(int price) {
+        if (price > 100_000) {
+            throw new IllegalArgumentException(MAX_COST_ERROR);
+        }
+    }
+
+    public static void monetaryUnitValidation(int price) {
+        if (price % 1_000 != 0) {
+            throw new IllegalArgumentException(MONETARY_UNIT_ERROR);
+        }
+    }
+
+    public static void lengthValidation(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(NUMBER_LENGTH_ERROR);
+        }
+    }
+
+    public static void overlapValidation(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException(NUMBER_OVERLAP_ERROR);
+        }
+    }
+
+    public static void rangeValidation(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(NUMBER_OUT_OF_RANGE_ERROR);
+            }
+        }
+    }
+}
