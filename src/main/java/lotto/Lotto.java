0af10aee491691aa0851b0064406af20ba68ad15
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-14 18:25:07 +0900

    feat: 로또 구매 번호 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,21 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Lotto {
+    private static final String ERROR_NUMBER_COUNT = "로또 번호는 6개여야 합니다.";
+    private static final String ERROR_NUMBER_RANGE = "로또 번호는 1 ~ 45 여야 합니다.";
     private final List<Integer> numbers;
+    private static final List<Integer> initLottoNumbers = new ArrayList<>();
+
+    static {
+        for (int i = 1; i <= 45; i++) {
+            initLottoNumbers.add(i);
+        }
+    }
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -12,9 +24,15 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_NUMBER_COUNT);
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static List<Integer> generateLottoNumbers() {
+        Collections.shuffle(initLottoNumbers);
+        List<Integer> lottoNumbers = initLottoNumbers.subList(0, 45);
+        Collections.sort(lottoNumbers);
+
+        return lottoNumbers;
+    }
 }
