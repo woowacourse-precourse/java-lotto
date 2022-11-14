20639350f31b2d498609526310b841d1e55a61b1
JuSeong1130 <jsmoon1130@naver.com>
2022-11-15 06:15:38 +0900

    feat(LottoAutoGenerator): 금액에 맞는 로또 개수 기능 구현

--- src/main/java/lotto/domain/lotto/LottoAutoGenerator.java
+++ src/main/java/lotto/domain/lotto/LottoAutoGenerator.java
@@ -1,13 +1,27 @@
 package lotto.domain.lotto;
 
+import static lotto.util.constant.Constant.LOTTO_PURCHASE_AMOUNT;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import lotto.domain.money.Money;
 
 public class LottoAutoGenerator {
 
+
+	public List<Lotto> createLottoAccordingToTheAmount(Money money) {
+		List<Lotto> lottos = new ArrayList<>();
+		int count = money.getLottoPurchaseCount();
+		while (count > 0) {
+			lottos.add(createLottoByAuto());
+			count--;
+		}
+		return lottos;
+	}
+
 	public Lotto createLottoByAuto() {
-		List<Integer> numbers =Randoms.pickUniqueNumbersInRange(1, 45, 6);
+		List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
 		return new Lotto(numbers);
 	}
 }
