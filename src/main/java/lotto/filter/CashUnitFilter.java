d1f63e3e46f1f7f4ccf948f29d7d6c9426e86d1c
lcw3176 <lcw3176@naver.com>
2022-11-14 12:17:46 +0900

    chore: 하드코딩된 화폐 단위 상수화

--- src/main/java/lotto/filter/CashUnitFilter.java
+++ src/main/java/lotto/filter/CashUnitFilter.java
@@ -1,11 +1,14 @@
 package lotto.filter;
 
 public class CashUnitFilter implements Filter {
+
+    private static final int CASH_UNIT = 1000;
+
     @Override
     public void doFilter(String cash) {
         int numberVerifiedCash = Integer.parseInt(cash);
 
-        if (numberVerifiedCash == 0 || numberVerifiedCash % 1000 != 0) {
+        if (numberVerifiedCash == 0 || numberVerifiedCash % CASH_UNIT != 0) {
             throw new IllegalArgumentException("입력한 숫자가 1,000원 단위가 아닙니다");
         }
     }
