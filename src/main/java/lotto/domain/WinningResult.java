d1e58133bcb89663d1db6be4da5e09dc87813364
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-14 16:10:27 +0900

    feat(winnning result): 당첨 결과 기능 추가
    당첨 결과
    - 당첨 결과값 얻기
    - 초기화
    - 결과값으로 통계내리기
    - 수익률 계산

---
+++ src/main/java/lotto/domain/WinningResult.java
@@ -0,0 +1,59 @@
+package lotto.domain;
+
+import java.util.*;
+
+public class WinningResult {
+    private final Map<Rank, Integer> winningResult = new EnumMap<Rank, Integer>(Rank.class);
+    private final LottoTicket tickets;
+    private final Lotto winningLottoNumbers;
+    private final LottoBonus bonus;
+
+    public WinningResult(LottoTicket tickets, Lotto winningLottoNumbers, LottoBonus bonus){
+        this.tickets = tickets;
+        this.winningLottoNumbers = winningLottoNumbers;
+        this.bonus = bonus;
+        putInit();
+        putValues();
+    }
+
+    public Map<Rank, Integer> getWinningResult() {
+        return winningResult;
+    }
+
+    private void putInit(){
+        Rank[] values = Rank.values();
+        for (Rank value : values) {
+            winningResult.put(value, 0);
+        }
+    }
+
+    private void putValues(){
+        List<Rank> ranks = calculateRank();
+        for (Rank rank : ranks) {
+            winningResult.put(rank, winningResult.getOrDefault(rank, 0) + 1);
+        }
+    }
+
+    private List<Rank> calculateRank(){
+        List<Integer> sameLottoNumbers = tickets.countSameLottoNumbers(winningLottoNumbers);
+        List<Boolean> sameLottoBonus = tickets.checkSameLottoBonus(bonus.getBonus());
+        List<Rank> ranks = new ArrayList<>();
+
+        for (int i = 0; i < tickets.getTickets(); i++){
+            ranks.add(Rank.getRanking(sameLottoNumbers.get(i), sameLottoBonus.get(i)));
+        }
+
+        return ranks;
+    }
+
+    public long prizeSum(){
+        return winningResult.entrySet().stream().
+                mapToLong(entry -> entry.getKey().multiple(entry.getValue())).sum();
+    }
+
+    public float profit(int money){
+        long prizeSum = prizeSum();
+        return ((float) prizeSum / (float) money) * 100;
+    }
+
+}
