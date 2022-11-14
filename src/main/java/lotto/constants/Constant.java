1d775c4d9d25aa1ed5e072c8ca68fc85dba0f83a
xi_jjun <rlawowns97@naver.com>
2022-11-14 18:05:03 +0900

    feat: 수익률을 계산하는 기능 추가

--- src/main/java/lotto/constants/Constant.java
+++ src/main/java/lotto/constants/Constant.java
@@ -27,4 +27,19 @@ public class Constant {
 	}};
 
 	public static final long NOT_COUNTED = 0L;
+	protected static final int FIRST_RANK_WINNING_AMOUNT = 2_000_000_000;
+	protected static final int SECOND_RANK_WINNING_AMOUNT = 30_000_000;
+	protected static final int THIRD_RANK_WINNING_AMOUNT = 1_500_000;
+	protected static final int FOURTH_RANK_WINNING_AMOUNT = 50_000;
+	protected static final int FIFTH_RANK_WINNING_AMOUNT = 5_000;
+	public static final Map<Rank, Integer> WINNING_AMOUNTS = new HashMap<>() {{
+		put(Rank.FIRST, FIRST_RANK_WINNING_AMOUNT);
+		put(Rank.SECOND, SECOND_RANK_WINNING_AMOUNT);
+		put(Rank.THIRD, THIRD_RANK_WINNING_AMOUNT);
+		put(Rank.FOURTH, FOURTH_RANK_WINNING_AMOUNT);
+		put(Rank.FIFTH, FIFTH_RANK_WINNING_AMOUNT);
+	}};
+
+	public static final int PERCENT_UNIT_MULTIPLIER = 100;
+	public static final int ROUND_TO_SECOND_DECIMAL_PLACE_FACTOR = 10;
 }
--- src/main/java/lotto/models/LottoResult.java
+++ src/main/java/lotto/models/LottoResult.java
@@ -5,14 +5,34 @@ import java.util.Map;
 import static lotto.constants.Constant.*;
 
 public class LottoResult {
-	double earningRate;
-	Map<Rank, Long> lotteriesResult;
+	private int totalEarnedMoney = 0;
+	private final Map<Rank, Long> lotteriesResult;
 
 	public LottoResult(Map<Rank, Long> lotteriesResult) {
 		this.lotteriesResult = lotteriesResult;
+		for (Map.Entry<Rank, Long> rankLongEntry : lotteriesResult.entrySet()) {
+			totalEarnedMoney += WINNING_AMOUNTS.get(rankLongEntry.getKey()) * rankLongEntry.getValue();
+		}
 	}
 
 	public int getRankCount(Rank rank) {
 		return Math.toIntExact(lotteriesResult.getOrDefault(rank, NOT_COUNTED));
+	}
+
+	public double getEarningRate(int purchaseAmount) {
+		double result = (double) totalEarnedMoney / purchaseAmount;
+		result = makeUnitByPercent(result);
+
+		return roundAtSecondDecimalPlace(result);
+	}
+
+	private double roundAtSecondDecimalPlace(double result) {
+		long round = Math.round(result * ROUND_TO_SECOND_DECIMAL_PLACE_FACTOR);
+
+		return (double) round / ROUND_TO_SECOND_DECIMAL_PLACE_FACTOR;
+	}
+
+	private double makeUnitByPercent(double result) {
+		return result * PERCENT_UNIT_MULTIPLIER;
 	}
 }
