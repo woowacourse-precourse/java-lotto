4e9c615113c12ded68c4e467fb88ad2f77020c49
SeongukBaek <bsu6235@gmail.com>
2022-11-14 18:48:51 +0900

    feat: implement class Validator
    Added new methods:
    - isInRange(List<Integer> numbers)
    - isInRange(int number)

--- src/main/java/lotto/ui/Validator.java
+++ src/main/java/lotto/ui/Validator.java
@@ -1,6 +1,11 @@
 package lotto.ui;
 
+import java.util.List;
+
 public class Validator {
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 45;
+
     public static boolean isNumber(char input) {
         if (!Character.isDigit(input)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자여야 합니다.");
@@ -15,5 +20,18 @@ public class Validator {
             throw new IllegalArgumentException("[ERROR] 구매 금액은 숫자여야 합니다.");
         }
         return true;
+    }
+
+    public static boolean isInRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (!isInRange(number)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 ~ 45 사이의 숫자여야 합니다.");
+            }
+        }
+        return true;
+    }
+
+    private static boolean isInRange(int number) {
+        return START_NUMBER <= number && number <= END_NUMBER;
     }
 }
