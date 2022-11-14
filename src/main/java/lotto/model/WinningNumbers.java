dd8dbaa811e530972a0452449c86226e0f386ed0
yxnxx <jh4rgh0@gmail.com>
2022-11-15 00:56:23 +0900

    feat(WinningNumbers): set bonus number

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -10,6 +10,7 @@ import java.util.Set;
 
 public class WinningNumbers {
     private static List<Integer> winningNumbers;
+    private static int bonusNumber;
     private static final int MAX_NUM = 45;
     private static final int MIN_NUM = 1;
     private static final int NUM_SIZE = 6;
@@ -17,6 +18,8 @@ public class WinningNumbers {
     public void setWinningNumbers() {
         String input = InputView.getWinningNumbers();
         winningNumbers = splitNumbers(input);
+        input = InputView.getBonusNumber();
+        bonusNumber = isBonusValid(input);
     }
     private List<Integer> splitNumbers(String input) {
         List<String> winningNumSplit = List.of(input.split(","));
@@ -37,9 +40,7 @@ public class WinningNumbers {
         if(numbers.size() != NUM_SIZE) throw new IllegalArgumentException(Error.SIZE.getError());
 
         for(Integer num: numbers) {
-            if((num > MAX_NUM) || (num < MIN_NUM)) {
-                throw new IllegalArgumentException(Error.LOTTO_RANGE.getError());
-            }
+            isNumInRange(num);
         }
     }
     private void isDuplicate(List<Integer> numbers) {
@@ -47,5 +48,24 @@ public class WinningNumbers {
         if (numberSet.size() != numbers.size()) {
             throw new IllegalArgumentException(Error.DUPLICATE.getError());
         }
+    }
+    private void isNumInRange(int num) {
+        if((num > MAX_NUM) || (num < MIN_NUM)) {
+            throw new IllegalArgumentException(Error.LOTTO_RANGE.getError());
+        }
+    }
+    private Integer isBonusValid(String number) {
+        int num;
+        try{
+            num = Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(Error.LOTTO_NUMERIC.getError());
+        }
+
+        isNumInRange(num);
+        if(winningNumbers.contains(num)) {
+            throw new IllegalArgumentException(Error.DUPLICATE.getError());
+        }
+        return num;
     }
 }
