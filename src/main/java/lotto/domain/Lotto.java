9893d8341a5e612119300f3a19cee01e466066a9
dldbdud314 <dldbdud314@naver.com>
2022-11-14 13:11:22 +0900

    feat(Lotto): 1~45 사이의 수만 통과하도록 validation 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,11 +1,18 @@
 package lotto.domain;
 
+import static lotto.ui.ConsoleMessage.INVALID_LOTTO_SIZE;
+import static lotto.ui.ConsoleMessage.LOTTO_NUMBER_DUPLICATE;
+import static lotto.ui.ConsoleMessage.LOTTO_NUMBER_OUT_OF_RANGE;
+import static lotto.ui.LottoConsole.printErrorMessage;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Lotto {
     private static final int LOTTO_NUMBERS_SIZE = 6;
+    public static final int START_NUMBER = 1;
+    public static final int END_NUMBER = 45;
 
     private final List<Integer> numbers;
 
@@ -16,24 +23,39 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateSize(numbers.size());
+        validateNumbersInRange(numbers);
         validateDuplicate(numbers);
     }
 
     private void validateSize(int size) {
         if (size != LOTTO_NUMBERS_SIZE) {
-            throw new IllegalArgumentException("로또 번호 개수는 6개로 제한됩니다.");
+            printErrorMessage(INVALID_LOTTO_SIZE.toString());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumbersInRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            validateSingleNumberInRange(number);
+        }
+    }
+
+    private void validateSingleNumberInRange(Integer number) {
+        if (START_NUMBER > number || number > END_NUMBER) {
+            printErrorMessage(LOTTO_NUMBER_OUT_OF_RANGE.toString());
+            throw new IllegalArgumentException();
         }
     }
 
     private void validateDuplicate(List<Integer> numbers) {
         Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
-        if (nonDuplicateNumbers.size() != LOTTO_NUMBERS_SIZE){
-            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+        if (nonDuplicateNumbers.size() != LOTTO_NUMBERS_SIZE) {
+            printErrorMessage(LOTTO_NUMBER_DUPLICATE.toString());
+            throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
-    public List<Integer> getNumbers(){
+    public List<Integer> getNumbers() {
         return numbers;
     }
 }
