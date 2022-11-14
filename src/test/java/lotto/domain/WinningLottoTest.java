34a1047af20d62a3bae1087e5748d47a434ca277
jipyo0106 <68218081+jipyo0106@users.noreply.github.com>
2022-11-14 22:55:42 +0900

    feat: 로또 비교 테스트

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -15,4 +15,21 @@ class WinningLottoTest {
         int correctCount = winningLotto.lottoContainsCount(List.of(2, 3, 4, 5, 6, 7));
         assertThat(correctCount).isEqualTo(5);
     }
+
+    @DisplayName("로또 비교 테스트")
+    @Test
+    void lottoRankTest() {
+        List<Integer> lotto = List.of(1, 2, 3, 4, 5, 6);
+        int bonus = 7;
+        List<Lotto> lottos = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(2, 3, 4, 5, 6, 7)),
+                new Lotto(List.of(45, 12, 44, 32, 5, 7))
+        );
+
+        WinningLotto winningLotto = new WinningLotto(lotto, bonus);
+        winningLotto.compareLotto(lottos);
+        
+        assertThat(winningLotto.getLottoRank()).isEqualTo(List.of(1, 1, 0, 0, 0, 1));
+    }
 }
