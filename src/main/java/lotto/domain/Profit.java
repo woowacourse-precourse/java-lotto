dc66fa4904e78016414ce51b213f8c6b18ca4397
YuHayeon <youha0930@gmail.com>
2022-11-15 00:13:40 +0900

    refactor(Profit): 하드코딩한 메시지, 퍼센트 계산을 위한 값을 상수로 변경

--- src/main/java/lotto/domain/Profit.java
+++ src/main/java/lotto/domain/Profit.java
@@ -2,6 +2,9 @@ package lotto.domain;
 
 public class Profit {
 
+    public static final String PROFIT_MESSAGE = "총 수익률은 %.1f%%입니다.";
+    public static final double PERCENT_NUMBER = 10.0;
+
     private Double profit;
 
     public Profit(Lottos lottos, WinningResult winningResult) {
@@ -10,11 +13,11 @@ public class Profit {
 
     public Double calculateProfit(Lottos lottos, WinningResult winningResult) {
         Double totalPrize = winningResult.calculateTotalPrize();
-        return lottos.calculateProfit(totalPrize) * 10;
+        return lottos.calculateProfit(totalPrize) * 10.0;
     }
 
     @Override
     public String toString() {
-        return String.format("총 수익률은 %.1f%%입니다.", (double) Math.round(profit) / 10.0);
+        return String.format(PROFIT_MESSAGE, (double) Math.round(profit) / PERCENT_NUMBER);
     }
 }
