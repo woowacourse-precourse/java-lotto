88b7164a633f47d4633bdbe1e3338824fe2f480a
Yeongseon Park <yseon406@gmail.com>
2022-11-15 02:14:10 +0900

    feat(lotto): 발행한 로또 출력 기능 구현

--- src/main/java/lotto/ui/Printer.java
+++ src/main/java/lotto/ui/Printer.java
@@ -1,4 +1,15 @@
 package lotto.ui;
 
+import lotto.Lotto;
+
+import java.util.List;
+
 public class Printer {
+
+    public void printLotto(int amount, List<Lotto> myLotto) {
+        System.out.println(amount + Message.BUY_LOTTO);
+        for (Lotto one : myLotto) {
+            System.out.println(one.getNumbers());
+        }
+    }
 }
