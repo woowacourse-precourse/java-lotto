59bade635ad7f5f1bb036300d706eca1cb3e4cdc
devMuscle <hongsbien@naver.com>
2022-11-14 21:20:43 +0900

    feat: 당첨 번호, 보너스번호를 담앙하는 Class 생성

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,2 +1,19 @@
-package lotto;public class Prize {
+package lotto;
+
+import java.util.List;
+
+public class Prize extends Lotto{
+    private final Integer bonusNumber;
+
+    public Prize(List<Integer> numbers, Integer bonusNumber) {
+        super(numbers);
+        validateBonusNumber(numbers, bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(List<Integer> numbers, Integer bonusNumber) {
+        if(numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호와 중복");
+        }
+    }
 }
