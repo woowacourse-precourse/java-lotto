fe59a82a442663708d7767ec61437a920d03e7de
iamsseaa <homingsea@gmail.com>
2022-11-14 13:57:12 +0900

    feat: 구입 금액으로 구매할 로또 구매 개수 구하는 기능

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -1,6 +1,8 @@
 package lotto;
 
 public class LottoStore {
+    private int money;
+    private int purchaseNumber;
 
     public void printInputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
@@ -12,5 +14,10 @@ public class LottoStore {
 
     public void printInputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public void makePurchaseNumber(int money) {
+        this.money = money;
+        purchaseNumber = money / 1000;
     }
 }
