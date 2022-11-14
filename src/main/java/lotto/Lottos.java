b5acf0f9efb9c1c78e5a7fcc196f741a439fb403
SeokHwan An <asok369258@naver.com>
2022-11-14 11:13:23 +0900

    feat : 여러개의 로또를 모아두는 객체 생성자 추가

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -1,10 +1,15 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lottos {
 
     private List<Lotto> lottos;
+
+    public Lottos() {
+        this.lottos = new ArrayList<>();
+    }
 
     // 로또 여러개를 저장하는 기능
     public void addLotto(Lotto lotto) {
