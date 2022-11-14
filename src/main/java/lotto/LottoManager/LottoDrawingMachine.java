a77105d425311edfd776218855e69c1238099469
skck0226 <skck0226@ajou.ac.kr>
2022-11-14 16:01:31 +0900

    [refactor] LottoDrawingMachine 스태틱 클래스로 변경, 테스트 수정

--- src/main/java/lotto/LottoManager/LottoDrawingMachine.java
+++ src/main/java/lotto/LottoManager/LottoDrawingMachine.java
@@ -11,57 +11,33 @@ import static lotto.Enum.Constant.*;
 import static lotto.Enum.Error.*;
 
 public class LottoDrawingMachine {
-    private final List<Integer> winning_numbers;
-    private final int bonus_number;
+    private static List<Integer> winning_numbers;
+    private static int bonus_number;
 
-    public LottoDrawingMachine() {
+    public static void drawLotto() {
         List<String> drawn_lines = drawLottoNumbers();
 
-        String winning_number_line = drawn_lines.get(ZERO.getIntValue());
-        String bonus_number_line = drawn_lines.get(ONE.getIntValue());
+        validateDrawnNumbersPattern(drawn_lines);
 
-        validateDrawnNumbersPattern(winning_number_line.trim(), bonus_number_line.trim());
-
-        winning_numbers =
-                Arrays.stream(winning_number_line.split(","))
-                        .map(Integer::parseInt)
-                        .collect(Collectors.toList());
-        bonus_number = Integer.parseInt(bonus_number_line);
-
-        validateDrawnNumbers(winning_numbers, bonus_number);
+        validateAllDrawnNumbers(drawn_lines);
     }
 
-    // Only For Test
-    public LottoDrawingMachine(List<String> drawn_lines) {
-        String winning_number_line = drawn_lines.get(ZERO.getIntValue());
-        String bonus_number_line = drawn_lines.get(ONE.getIntValue());
+    public static void drawLottoForTest(List<String> drawn_lines) {
+        validateDrawnNumbersPattern(drawn_lines);
 
-        validateDrawnNumbersPattern(winning_number_line.trim(), bonus_number_line.trim());
-
-        winning_numbers =
-                Arrays.stream(winning_number_line.split(","))
-                        .map(Integer::parseInt)
-                        .collect(Collectors.toList());
-        bonus_number = Integer.parseInt(bonus_number_line);
-
-        validateDrawnNumbers(winning_numbers, bonus_number);
+        validateAllDrawnNumbers(drawn_lines);
     }
 
-    public List<Integer> getWinning_numbers() {
-        return winning_numbers;
-    }
-
-    public int getBonus_number() {
-        return bonus_number;
-    }
-
-    private List<String> drawLottoNumbers() {
+    private static List<String> drawLottoNumbers() {
         String winning_number_line = Console.readLine();
         String bonus_number_line = Console.readLine();
         return List.of(winning_number_line, bonus_number_line);
     }
 
-    private void validateDrawnNumbersPattern(String winning_number_line, String bonus_number_line) {
+    private static void validateDrawnNumbersPattern(List<String> drawn_lines) {
+        String winning_number_line = drawn_lines.get(ZERO.getIntValue()).trim();
+        String bonus_number_line = drawn_lines.get(ONE.getIntValue()).trim();
+
         if (!winning_number_line.matches(REGEX_WINNING_NUMBER_FORMAT.getValue())) {
             throw new IllegalArgumentException(DRAW_LOTTO_FORMAT_ERROR.getMessage());
         }
@@ -70,19 +46,29 @@ public class LottoDrawingMachine {
         }
     }
 
-    private void validateDrawnNumbers(List<Integer> winning_numbers, int bonus_number) {
-        List<Integer> drawn_numbers =
-                Stream.concat(winning_numbers.stream(), Stream.of(bonus_number))
-                        .collect(Collectors.toList());
+    private static void validateAllDrawnNumbers(List<String> drawn_lines) {
+        List<Integer> all_drawn_numbers = makeDrawnLinesToNumbers(drawn_lines);
 
-        if (drawn_numbers.stream()
-                .filter(this::validateDrawnNumberRange)
+        if (all_drawn_numbers.stream()
+                .filter(LottoDrawingMachine::validateDrawnNumberRange)
                 .count() != FIVE.getIntValue()) {
             throw new IllegalArgumentException(DRAW_LOTTO_RANGE_ERROR.getMessage());
         }
     }
 
-    private boolean validateDrawnNumberRange(Integer num) {
+    private static List<Integer> makeDrawnLinesToNumbers(List<String> drawn_lines) {
+        winning_numbers =
+                Arrays.stream(drawn_lines.get(ZERO.getIntValue()).split(","))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+        bonus_number = Integer.parseInt(drawn_lines.get(ONE.getIntValue()));
+
+        return Stream.concat(
+                winning_numbers.stream(), Stream.of(bonus_number)
+                ).collect(Collectors.toList());
+    }
+
+    private static boolean validateDrawnNumberRange(Integer num) {
         return num > ZERO.getIntValue() && num <= FOURTY_FIVE.getIntValue();
     }
 }
--- src/test/java/lotto/LottoDrawTest.java
+++ src/test/java/lotto/LottoDrawTest.java
@@ -13,36 +13,36 @@ public class LottoDrawTest {
     @DisplayName("로또 당첨 번호 입력 시 여섯개의 숫자가 ','로 구분되어 입력되어야한다")
     @Test
     void 로또추첨_예외처리_테스트1() {
-        assertThatThrownBy(() -> new LottoDrawingMachine(List.of("1,2,3,4,5,6,", "7")))
+        assertThatThrownBy(() -> LottoDrawingMachine.drawLottoForTest(List.of("1,2,3,4,5,6,", "7")))
                 .isInstanceOf(IllegalArgumentException.class);
 
-        assertThatThrownBy(() -> new LottoDrawingMachine(List.of("1,2,3,4,5/6", "7")))
+        assertThatThrownBy(() -> LottoDrawingMachine.drawLottoForTest(List.of("1,2,3,4,5/6", "7")))
                 .isInstanceOf(IllegalArgumentException.class);
 
-        assertThatThrownBy(() -> new LottoDrawingMachine(List.of("b,2,3,4,5,a", "7")))
+        assertThatThrownBy(() -> LottoDrawingMachine.drawLottoForTest(List.of("b,2,3,4,5,a", "7")))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 당첨 보너스 번호 입력 시 한개의 숫자만 입력되어야한다")
     @Test
     void 로또추첨_예외처리_테스트2() {
-        assertThatThrownBy(() -> new LottoDrawingMachine(List.of("1,2,3,4,5,6,", ",12")))
+        assertThatThrownBy(() -> LottoDrawingMachine.drawLottoForTest(List.of("1,2,3,4,5,6,", ",12")))
                 .isInstanceOf(IllegalArgumentException.class);
 
-        assertThatThrownBy(() -> new LottoDrawingMachine(List.of("1,2,3,4,5/6", "a")))
+        assertThatThrownBy(() -> LottoDrawingMachine.drawLottoForTest(List.of("1,2,3,4,5/6", "a")))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 당첨 번호 입력 시 1부터 45까지의 숫자로 입력해야한다")
     @Test
     void 로또추첨_예외처리_테스트3() {
-        assertThatThrownBy(() -> new LottoDrawingMachine(List.of("1,2,3,4,5,46", "7")))
+        assertThatThrownBy(() -> LottoDrawingMachine.drawLottoForTest(List.of("1,2,3,4,5,46", "7")))
                 .isInstanceOf(IllegalArgumentException.class);
 
-        assertThatThrownBy(() -> new LottoDrawingMachine(List.of("0,2,3,4,5,45", "7")))
+        assertThatThrownBy(() -> LottoDrawingMachine.drawLottoForTest(List.of("0,2,3,4,5,45", "7")))
                 .isInstanceOf(IllegalArgumentException.class);
 
-        assertThatThrownBy(() -> new LottoDrawingMachine(List.of("1,2,3,4,5,45", "46")))
+        assertThatThrownBy(() -> LottoDrawingMachine.drawLottoForTest(List.of("1,2,3,4,5,45", "46")))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
