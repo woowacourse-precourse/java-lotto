d0b15c6597637a754fba831585b05ce926b270be
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-14 10:18:59 +0900

    Teat : 추가 입력한 금액만큼 로또 번호를 발행하는지 확인 테스트

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -2,8 +2,12 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,6 +20,7 @@ class LottoGameTest extends NsTest {
         lottoGame = new LottoGame();
     }
 
+    @DisplayName("입력한 값에 따라 제대로 된 개수를 출력하는지 확인")
     @Test
     void getMoneyInputTest(){
         assertSimpleTest(() -> {
@@ -23,9 +28,37 @@ class LottoGameTest extends NsTest {
             assertThat(output()).contains("14개를 구매했습니다.");
         });
     }
+    @DisplayName("구매한 로또 개수만큼 로또 번호를 발행하는지 확인")
+    @Test
+    void getLottoNumberList(){
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("8000");
+                    assertThat(output()).contains(
+                            "8개를 구매했습니다.",
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[13, 14, 16, 38, 42, 45]",
+                            "[7, 11, 30, 40, 42, 43]",
+                            "[2, 13, 22, 32, 38, 45]",
+                            "[1, 3, 5, 14, 22, 45]"
+                    );
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(7, 11, 30, 40, 42, 43),
+                List.of(2, 13, 22, 32, 38, 45),
+                List.of(1, 3, 5, 14, 22, 45)
+        );
+    }
 
     @Override
     protected void runMain() {
-        lottoGame.getMoneyInput();
+        lottoGame.startGame();
     }
 }
