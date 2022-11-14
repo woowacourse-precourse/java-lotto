7efaf55d95696f2fce0a564f0c946afd4e74326a
starwook <star.wookk@gmail.com>
2022-11-15 00:34:25 +0900

    feat(User.java): Add User.java and validate method
    User 클래스를 생성하고 Lotto.java 코드를 베껴 맴벼변수, validate 함수를 따라하여라

---
+++ src/main/java/lotto/User.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import java.util.List;
+
+public class User {
+    private final List<Integer> numbers;
+
+    public User(List<Integer> numbers){
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers){
+        if(numbers.size() != 6){
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
