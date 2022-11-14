64a659fe0d52ac676f65d84d4718e1443c6320e0
0chil <0@chll.it>
2022-11-14 17:56:02 +0900

    feat(InputView): 당첨번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,12 +1,17 @@
 package lotto.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
     private final static Pattern NUMBER_ONLY_PATTERN = Pattern.compile("\\d+");
+    private final static Pattern LOTTO_NUMBERS_FORMAT = Pattern.compile("\\d,\\d,\\d,\\d,\\d,\\d");
+    private static final String LOTTO_NUMBERS_SEPARATOR = ",";
 
     public long promptAmount() {
         System.out.println("구입금액을 입력해 주세요.");
@@ -18,6 +23,21 @@ public class InputView {
     private void validateNumber(String input) {
         if (!NUMBER_ONLY_PATTERN.matcher(input).matches()) {
             throw new IllegalArgumentException("숫자를 입력하셔야 합니다");
+        }
+    }
+
+    public List<Integer> promptWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        validateLottoNumbersFormat(input);
+        return Arrays.stream(input.split(LOTTO_NUMBERS_SEPARATOR))
+                .map(Integer::parseInt)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private void validateLottoNumbersFormat(String input) {
+        if (!LOTTO_NUMBERS_FORMAT.matcher(input).matches()) {
+            throw new IllegalArgumentException("로또번호 형식과 맞지 않습니다");
         }
     }
 }
