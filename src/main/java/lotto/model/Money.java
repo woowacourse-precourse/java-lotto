d7dcb03837e31fe9f7642a0ad337d7b259bcf2ea
yxnxx <jh4rgh0@gmail.com>
2022-11-14 20:09:11 +0900

    feat(Money): check input money is number

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -1,6 +1,22 @@
 package lotto.model;
 
+import lotto.view.InputView;
+
 public class Money {
+    private static int inputMoney;
+
+    public void setInputMoney() {
+        String input = InputView.purchaseInput();
+        int money = isMoneyNumber(input);
+    }
+    public Integer isMoneyNumber(String money) {
+        try {
+            return Integer.parseInt(money);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 숫자만 입력 가능합니다.");
+        }
+    }
     // 입력 받은 money 유효성 검사
     // 로또 발행 수량 구하기
+
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String PURCHASE_MONEY = "구입금액을 입력해 주세요.";
 
-    public String purchaseInput() {
+    public static String purchaseInput() {
         System.out.println(PURCHASE_MONEY);
         return Console.readLine();
     }
