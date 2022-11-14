d70a2cc28900b37712fe3b4d21c5912fa5bfc997
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-14 17:57:22 +0900

    feat(Game): 구입 금액에 맞게 로또 n개 생성하는 generateListOfLottos 메소드 구현

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -6,4 +6,15 @@ import java.util.ArrayList;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Game {
+    private List<Lotto> listOfLottos;
+
+    public List<Lotto> generateListOfLottos(int size) {
+        List<Lotto> listOfLottos = new ArrayList<>();
+        Lotto lotto;
+        for (int idx = 0; idx < size; idx++) {
+            lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            listOfLottos.add(lotto);
+        }
+        return listOfLottos;
+    }
 }
