ee07080e48720a8fef373df83465427744b06bcb
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-15 03:07:34 +0900

    test: 구매 금액이 1,000 이하일때 예외처리 발생하는지 테스트

--- src/test/java/lotto/view/LottoServiceViewTest.java
+++ src/test/java/lotto/view/LottoServiceViewTest.java
@@ -1,4 +1,35 @@
+package lotto.view;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 class LottoServiceViewTest {
-  
+
+    LottoServiceView lottoServiceView;
+
+    @BeforeEach
+    void beforeEach() {
+        lottoServiceView = new LottoServiceView();
+    }
+
+    @ParameterizedTest
+    @DisplayName("유효성 검사 테스트 - 최소금액 검사")
+    @CsvSource(value = {"123", "567", "4", "56", "0", "1"})
+    void checkMinimumAmount(String inputMoney) {
+
+        assertThatThrownBy(() -> lottoServiceView.validateInputMoney(inputMoney))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 최소 구매 금액은 1,000원 이상입니다.");
+    }
 }
