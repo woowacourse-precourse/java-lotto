6cb8b976584c6547289a56ee1b2f005f628cde74
jin8073 <jin8073@gmail.com>
2022-11-14 21:22:27 +0900

    test(IllegalArgument): 예외 처리 테스트 케이스 추가

--- src/test/java/lotto/exception/IllegalArgumentTest.java
+++ src/test/java/lotto/exception/IllegalArgumentTest.java
@@ -1,6 +1,9 @@
 package lotto.exception;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class IllegalArgumentTest {
@@ -23,5 +26,20 @@ public class IllegalArgumentTest {
     @Test
     void 천원_단위_아닌_숫자_입력() {
         assertThat(IllegalArgument.isThousandWon("1234")).isFalse();
+    }
+
+    @Test
+    void 범위_밖_숫자_포함() {
+        assertThat(IllegalArgument.isInRange(Arrays.asList(1, 3, 46, 4, 7, 9))).isFalse();
+    }
+
+    @Test
+    void 중복_포함_리스트() {
+        assertThat(IllegalArgument.isRedundancy(Arrays.asList(1, 3, 3, 4, 5, 6))).isTrue();
+    }
+
+    @Test
+    void 중복_없는_리스트() {
+        assertThat(IllegalArgument.isRedundancy(Arrays.asList(1, 2, 3, 4, 5, 6))).isFalse();
     }
 }
