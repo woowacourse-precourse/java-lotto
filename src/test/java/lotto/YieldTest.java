b1bac3248d73d81cd95c6d31c91df85d73fd53fc
yejin <kwonyj1022@naver.com>
2022-11-14 18:59:09 +0900

    test: YieldTest

---
+++ src/test/java/lotto/YieldTest.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class YieldTest {
+
+    @Test
+    void calculateYield() {
+        int myLottoCnt = 8;
+        int totalPrize = Win.FIFTH.prize();
+        Yield yield = new Yield(myLottoCnt, totalPrize);
+        assertEquals(62.5, yield.yield);
+    }
+
+}
