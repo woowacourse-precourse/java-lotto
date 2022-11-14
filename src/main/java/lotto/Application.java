11f8db37d44cae8a71e69235d9a3304c2e11c003
JuneYub <ajsjdlwj0123@gmail.com>
2022-11-15 00:53:52 +0900

    <feat> 예외사항 발생 시 IllegalArgument 반응 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,35 +2,56 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Application {
+    static final WinningNumber winningNumber = new WinningNumber();
+    static final Cash cash = new Cash();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        WinningNumber winningNumber = new WinningNumber();
-        Cash cash = new Cash();
         PrintLotto printLotto = new PrintLotto();
+        try {
+            String inputCash = insertMoney(); // 구입 금액 입력
 
-        int[] winningLotto;
-        int bnsNumber = 0;
+            cash.Input(inputCash);
+            printLotto.issuance(cash.cnt); // 투입된 현금만큼 복권 발행
+            System.out.println("");
 
+            Lotto winningLotto = inputWinningNumber(); // 당첨 번호 입력
+            int bnsNumber = inputBonusNumber(); // 보너스 번호 입력
+
+            printLotto.startPrintList(cash.money, winningLotto.getNumbers(), bnsNumber); // 당첨 통계
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 잘못된 값을 입력하셨습니다");
+        }
+
+
+
+    }
+
+    public static String insertMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         String inputCash = Console.readLine();
         System.out.println("");
+        return inputCash;
+    }
 
-        cash.Input(inputCash);
-        printLotto.issuance(cash.cnt); // 투입된 현금만큼 복권 발행
-        System.out.println("");
-
+    public static Lotto inputWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String winningString = Console.readLine();
-        winningLotto = winningNumber.inputWinningNum(winningString);
+        Lotto winningLotto = winningNumber.inputWinningNum(winningString);
         System.out.println("");
+        return winningLotto;
+    }
 
+    public static int inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String BonusStr = Console.readLine();
-        bnsNumber = winningNumber.inputBonusNum(BonusStr);
+        int bnsNumber = winningNumber.inputBonusNum(BonusStr);
         System.out.println("");
-
-        printLotto.startPrintList(cash.money, winningLotto, bnsNumber);
-
+        return bnsNumber;
     }
+
+
 }
