22dc08cbf2b74e7374c5d4a6ced1581f4e621b92
ESeokhwan <huan99@hanmail.net>
2022-11-14 22:05:51 +0900

    feat: 콘솔에 여러 개의 변수를 리스트 형식으로 출력하는 기능 추가

--- src/main/java/lotto/view/ConsoleOutputView.java
+++ src/main/java/lotto/view/ConsoleOutputView.java
@@ -12,10 +12,27 @@ public class ConsoleOutputView {
     }
 
     public void println(List<Integer> integers) {
-        // TODO 구현 필요
+        System.out.println(integersToString(integers));
     }
 
     public void printlnError(String errorText) {
         // TODO 구현 필요
+    }
+
+    private String integersToString(List<Integer> integers) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for(int i = 0; i < integers.size() - 1; i++) {
+            stringBuilder.append(integers.get(i)).append(", ");
+        }
+
+        int lastIndex = integers.size() - 1;
+        if(lastIndex < 0) {
+            stringBuilder.append("]");
+            return stringBuilder.toString();
+        }
+
+        stringBuilder.append(integers.get(lastIndex)).append("]");
+        return stringBuilder.toString();
     }
 }
