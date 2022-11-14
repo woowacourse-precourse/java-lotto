257ef89603e8fa7efab90f146bd9aa015f662f83
KNH <nh2004@naver.com>
2022-11-15 08:07:40 +0900

    fix(Message) : 구매 문구 수정

--- src/main/java/lotto/view/Message.java
+++ src/main/java/lotto/view/Message.java
@@ -2,7 +2,7 @@ package lotto.view;
 
 public enum Message {
     SELLING_MSG("구입 금액을 입력해 주세요."),
-    BUYING_MSG("개 구매했습니다."),
+    BUYING_MSG("개를 구매했습니다."),
     CHOOSE_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
     CHOOSE_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
 
