5454d6d7d83064a32d75613d0d72e3b3f3688481
sjkim <seoungjoo01@gmail.com>
2022-11-15 00:57:27 +0900

    feat : 3번 - 구매된 로또 정보 출력 기능 구현
    OutputView
    - 구매된 로또 개수를 출력한다.
    - 각 로또의 번호를 출력한다.

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -1,4 +1,24 @@
 package lotto.View;
 
+import lotto.Domain.LotteryTickets;
+import lotto.Domain.Lotto;
+import lotto.Domain.Message;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OutputView {
+    public static void showPurchasedLottos(LotteryTickets lotteryTickets) {
+        System.out.printf((Message.LOTTO_PURCHASE_NUM_MESSAGE) + "%n", lotteryTickets.getLottoCount());
+
+        for(Lotto lotto : lotteryTickets.getLottoTickets()){
+            showPurchasedLotto(lotto);
+        }
+    }
+
+    private static void showPurchasedLotto(Lotto lotto) {
+        List<String> lottoNums = lotto.getNumbers().stream().map(lottoNum -> Integer.toString(lottoNum)).collect(Collectors.toList());
+        String lottoPrintMessage = Message.LOTTO_MESSAGE_PREFIX + String.join(Message.LOTTO_MESSAGE_SEPERATOR,lottoNums) + Message.LOTTO_MESSAGE_SUFFIX;
+        System.out.println(lottoPrintMessage);
+    }
 }
