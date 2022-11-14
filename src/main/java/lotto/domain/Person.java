a895bd184f030c8d09aa0ac48845df0274243ef9
jay <jaewhan232@naver.com>
2022-11-14 20:00:40 +0900

    Implement : 맞은 로또 번호 개수를 반환하는 countCorrectLottoNumber() 메소드 구현

--- src/main/java/lotto/domain/Person.java
+++ src/main/java/lotto/domain/Person.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Person {
     private List<Lotto> lottos;
@@ -8,4 +10,14 @@ public class Person {
     public void buyLottos(LottoMachine lottoMachine) {
         this.lottos = lottoMachine.sellLottos();
     }
+
+    private int countCorrectLottoNumber(Lotto lotto) {
+        List<Integer> winningLottoNumber = WinningLotto.getWinningLottoNumber();
+        Set<Integer> count = lotto.getLottoNumbers()
+                .stream()
+                .filter(winningLottoNumber::contains)
+                .collect(Collectors.toSet());
+        return count.size();
+    }
+
 }
