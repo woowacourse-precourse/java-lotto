e92df8fa787b12e142a8cbb3894df90b54cfb8e5
yu-heejin <jkgt0521@naver.com>
2022-11-14 13:42:51 +0900

    feat: 로또 구입 금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static String inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String amount = Console.readLine();
+
+        return amount;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String amount = inputPurchaseAmount();
+        System.out.println(amount);
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class Lotto {
