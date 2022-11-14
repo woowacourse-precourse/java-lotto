7dda8075b03a6ee6d598c46cacf0a92ec02af00e
dsaf2007 <dsaf2007@naver.com>
2022-11-15 01:55:20 +0900

    feat : 로또 번호 오름차순 출력 기능 추가 로또 구입 개수가 제대로 반환되지 않던 오류 해결

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -3,29 +3,30 @@ package lotto;
 import java.util.List;
 import java.util.ArrayList;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 
 /*
- * 무작위의 Lotto 번호를 생성, 저장하는 클래스
+ * 무작위의 Lotto 번호를 생성하는 클래스
  */
 public class LottoMachine{
 
-    private List<Integer> createLottoNum()
+    public List<Integer> createLottoNum()
     {
         List<Integer> lottoNumber = new ArrayList<Integer>();
-        for(int i = 0; i < 6; i++)
-        {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            lottoNumber = numbers;
-        }
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        lottoNumber = numbers;
+        Collections.sort(lottoNumber);
+        //System.out.println(numbers.toString());
         return lottoNumber;
     }
 
-    public List<List<Integer>> getLottoNum(int numOfLotto)
+    public List<Lotto> createLottoTickets(int numOfLotto)
     {
-        List<List<Integer>> lottoNumbers = new ArrayList<List<Integer>>();
+        List<Lotto> lottoNumbers = new ArrayList<Lotto>();
         for(int i = 0; i < numOfLotto; i++)
-        {
-            lottoNumbers.add(createLottoNum());
+        {   
+            Lotto temp = new Lotto(createLottoNum());
+            lottoNumbers.add(temp);
         }
         return lottoNumbers;
     }
