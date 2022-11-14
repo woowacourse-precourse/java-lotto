6219781bc0fc0db685e9bf6f85902272b4b2cbd8
ESeokhwan <huan99@hanmail.net>
2022-11-14 22:35:10 +0900

    feat: 콘솔 입력 관련 기능 추가

--- src/main/java/lotto/view/ConsoleInputView.java
+++ src/main/java/lotto/view/ConsoleInputView.java
@@ -2,15 +2,54 @@ package lotto.view;
 
 import java.util.ArrayList;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 
 public class ConsoleInputView {
     public int readInteger() {
-        // TODO 구현 필요
-        return 0;
+        String inputText = Console.readLine();
+        if(isNotInt(inputText)) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+
+        inputText = inputText.trim();
+        return Integer.parseInt(inputText);
     }
 
     public List<Integer> readIntegers() {
-        // TODO 구현 필요
-        return new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+
+        String inputText = Console.readLine();
+        if(isNotIntegerList(inputText)) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+
+        inputText = inputText.trim();
+        String[] splitByComma = inputText.split(",");
+
+        for(String piece: splitByComma) {
+            output.add(Integer.parseInt(piece));
+        }
+        return output;
+    }
+
+    private boolean isNotInt(String input) {
+        input = input.trim();
+        return !input.matches("-?\\d+");
+    }
+
+    private boolean isNotIntegerList(String input) {
+        input = input.trim();
+        if(input.startsWith(",") || input.endsWith(",")) {
+            return true;
+        }
+
+        String[] splitByComma = input.split(",");
+        for (String piece : splitByComma) {
+            piece = piece.trim();
+            if(isNotInt(piece)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
