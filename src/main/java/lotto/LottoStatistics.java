2d2c0f43795effeb45b7e13fe721c92f71f100c3
iamsseaa <homingsea@gmail.com>
2022-11-14 18:35:49 +0900

    feat: 일치하는 개수가 5개일 때 보너스 번호를 확인하는 기능

--- src/main/java/lotto/LottoStatistics.java
+++ src/main/java/lotto/LottoStatistics.java
@@ -1,2 +1,18 @@
-package lotto;public class LottoStatistics {
+package lotto;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoStatistics {
+
+
+
+    public String checkBonusNumber(Lotto lotto, int bonus, String key) {
+        String newKey = key;
+        if (lotto.existsNumber(bonus)) {
+            newKey = "bonus";
+        }
+        return newKey;
+    }
 }
