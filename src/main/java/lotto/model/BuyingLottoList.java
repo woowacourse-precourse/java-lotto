f85b0184327747423b9bd4c96122e8420a92c497
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-15 05:24:06 +0900

    style: 자바 컨벤션 규칙 지키기 위한 클래스명 수정

--- src/main/java/lotto/model/BuyingLottoList.java
+++ src/main/java/lotto/model/BuyingLottoList.java
@@ -2,11 +2,11 @@ package lotto.model;
 
 import java.util.List;
 
-public class buyingLottoList {
+public class BuyingLottoList {
 
     private final List<Lotto> lottoList;
 
-    public buyingLottoList(List<Lotto> lottoList) {
+    public BuyingLottoList(List<Lotto> lottoList) {
         this.lottoList = lottoList;
     }
 
