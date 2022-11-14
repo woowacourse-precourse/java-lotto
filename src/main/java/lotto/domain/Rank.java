800d6b3ed19140a1ee8d2a0bedc19fac023c9958
MinjeongKong <kong1301@ajou.ac.kr>
2022-11-14 16:47:49 +0900

    feat: 당첨 등수 찾기

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -5,7 +5,8 @@ public enum Rank {
     SECOND(5,true,30000000),
     THIRD(5,false,1500000),
     FOURTH(4, false, 50000),
-    FIFTH(3,false,5000);
+    FIFTH(3,false,5000),
+    NONE(0,false,0);
 
     private final int cnt;
     private final boolean bonus;
@@ -15,5 +16,24 @@ public enum Rank {
         this.cnt = cnt;
         this.bonus = bonus;
         this.reward = reward;
+    }
+
+    public Rank findRank(int cnt, boolean bonus) {
+        if (FIRST.cnt == cnt) {
+            return FIRST;
+        }
+        if (SECOND.cnt == cnt) {
+            if (bonus == true) {
+                return SECOND;
+            }
+            return THIRD;
+        }
+        if (FOURTH.cnt == cnt) {
+            return FOURTH;
+        }
+        if (FIFTH.cnt == cnt) {
+            return FIFTH;
+        }
+        return NONE;
     }
 }
