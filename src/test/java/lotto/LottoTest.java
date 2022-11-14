c36f8b07585fb70cac0c531ab4655754f7e171d8
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-15 00:06:17 +0900

    test(LottoTest) : 로또번호가 6개 넘어가면서 중복된 값이 있을 때 예외처리를 하는가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -23,5 +23,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    @DisplayName("로또 번호 개수가 6개가 넘어가면서 중복된 숫자가 있으면 예외가 발생한다.")
+    void createLottoByOverSizeAndDuplicateNumber() throws Exception {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
