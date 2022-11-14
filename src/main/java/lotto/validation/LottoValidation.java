5a70b1875d1b5a59bb80466ab9863cc0ce94adae
desfox <desfox99@naver.com>
2022-11-14 21:04:01 +0900

    feat(LottoValidation): 로또의 번호 중 중복값 없는지 검증하는 로직 구현

--- src/main/java/lotto/validation/LottoValidation.java
+++ src/main/java/lotto/validation/LottoValidation.java
@@ -10,4 +10,17 @@ public class LottoValidation {
             throw new IllegalArgumentException("[ERROR] 번호의 개수는 6개여야 합니다.");
         }
     }
+
+    public void validateNoDuplicateNumbers(List<Integer> lottoNumbers) {
+        for (int currIdx = 0; currIdx < QUANTITY_OF_DIGITS; currIdx++) {
+            int nextIdx = currIdx + 1;
+            List<Integer> ElementsAfterCurrIdx = lottoNumbers.subList(nextIdx, QUANTITY_OF_DIGITS);
+
+            if (ElementsAfterCurrIdx.contains(lottoNumbers.get(currIdx))) {
+                throw new IllegalArgumentException("[ERROR] 중복된 번호는 입력할 수 없습니다.");
+            }
+        }
+    }
+
+
 }
