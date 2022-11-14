3f4f465eaf82bc40b02f7331626979a5c42dff13
정승구 <jeongseung-gu@jeongseung-guui-MacBookAir.local>
2022-11-14 19:32:26 +0900

    feat: lotto테스트 케이스에 맞게 조건문 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,9 +13,12 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        Set<Integer> checkSet = new HashSet<>(numbers);
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if (checkSet.size() != numbers.size())
+            throw new IllegalArgumentException();
     }
 
     public List<Integer> getLotto() {
