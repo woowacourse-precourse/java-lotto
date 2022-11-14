63408f6fe8a981f448f96956c9eae112955ff398
splitube <voicesplit@daum.net>
2022-11-14 17:01:10 +0900

    test(WinningNumberMaker): 테스트 추가, 테스트 수정

--- src/test/java/lotto/WinningNumberMakerTest.java
+++ src/test/java/lotto/WinningNumberMakerTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class WinningNumberMakerTest {
+    private final String ERROR_MESSAGE = "[ERROR]";
+
     @DisplayName("\",\"로 입력값 나누기")
     @Test
     void divideByComma() {
@@ -29,7 +31,7 @@ class WinningNumberMakerTest {
         assertThat(inputSplitByComma).containsExactlyElementsOf(numbers);
     }
 
-    @DisplayName("[오류 테스트] 입력 당첨번호가 숫자가 아님")
+    @DisplayName("[오류 테스트] 당첨 번호 입력값이 숫자가 아님")
     @Test
     void winningNumberNotNum() {
         //given
@@ -37,26 +39,28 @@ class WinningNumberMakerTest {
         String winningNumberStr = "a";
         //when
         //then
-        assertThatThrownBy(() -> {
-            winningNumberMaker.validateWinningNumberStr(winningNumberStr);
-        }).hasMessage(ErrorMessage.WINNING_NUM_NOT_A_NUMBER.message);
+        assertThatThrownBy(() -> winningNumberMaker.validateWinningNumberStr(winningNumberStr))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.WINNING_NUM_NOT_A_NUMBER.message)
+                .hasMessageContaining(ERROR_MESSAGE);
     }
 
-    @DisplayName("[오류 테스트] 입력 당첨번호가 범위를 넘어감")
+    @DisplayName("[오류 테스트] 당첨 번호가 범위를 넘어감")
     @Test
     void winningNumberOutOfRange() {
         //given
         WinningNumberMaker winningNumberMaker = new WinningNumberMaker();
         String winningNumberStr = "46";
         //when //then
-        assertThatThrownBy(() -> {
-            winningNumberMaker.validateWinningNumberStr(winningNumberStr);
-        }).hasMessage(ErrorMessage.NUMBER_OUT_OF_RANGE.message);
+        assertThatThrownBy(() -> winningNumberMaker.validateWinningNumberStr(winningNumberStr))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.NUMBER_OUT_OF_RANGE.message)
+                .hasMessageContaining(ERROR_MESSAGE);
     }
 
-    @DisplayName("[오류 테스트] 당첨번호가 6개가 아님")
+    @DisplayName("[오류 테스트] 총 번호의 갯수가 6개보다 적음")
     @Test
-    void validateInputSplitByComma() {
+    void winningNumberLessThanSix() {
         //입력값 세팅
         String[] numbers = {"1", "4", "7", "20", "34"};
         ByteArrayInputStream byteInput = new ByteArrayInputStream(String.join(",", numbers).getBytes());
@@ -67,9 +71,29 @@ class WinningNumberMakerTest {
         winningNumberMaker.divideByComma();
         //when
         //then
-        assertThatThrownBy(() -> {
-            winningNumberMaker.validateInputSplitByComma();
-        }).hasMessage(ErrorMessage.NOT_ENOUGH_NUM.message);
+        assertThatThrownBy(() -> winningNumberMaker.validateInputSplitByComma())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.NOT_SIX_NUM.message)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @DisplayName("[오류 테스트] 총 번호의 갯수가 6개보다 많음")
+    @Test
+    void winningNumberMoreThanSix() {
+        //입력값 세팅
+        String[] numbers = {"1", "4", "7", "20", "34", "35", "36"};
+        ByteArrayInputStream byteInput = new ByteArrayInputStream(String.join(",", numbers).getBytes());
+        System.setIn(byteInput);
+        //given
+        WinningNumberMaker winningNumberMaker = new WinningNumberMaker();
+        winningNumberMaker.getWinningNumberInput();
+        winningNumberMaker.divideByComma();
+        //when
+        //then
+        assertThatThrownBy(() -> winningNumberMaker.validateInputSplitByComma())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.NOT_SIX_NUM.message)
+                .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @DisplayName("입력값으로 당첨번호 6개 저장")
@@ -90,9 +114,9 @@ class WinningNumberMakerTest {
         assertThat(winningNumbers).containsExactlyElementsOf(expected);
     }
 
-    @DisplayName("[오류 테스트] 입력한 보너스 번호가 숫자가 아님")
+    @DisplayName("[오류 테스트] 보너스 번호 입력값이 숫자가 아님")
     @Test
-    void bonusNumberInputNotANum(){
+    void bonusNumberInputNotANum() {
         //입력값 정의
         String input = "1a";
         ByteArrayInputStream byteInput = new ByteArrayInputStream(input.getBytes());
@@ -102,12 +126,13 @@ class WinningNumberMakerTest {
         winningNumberMaker.getBonusNumberInput();
         assertThatThrownBy(() -> winningNumberMaker.validateBonusNumberInput())
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.BONUS_NUM_NOT_A_NUMBER.message);
+                .hasMessage(ErrorMessage.BONUS_NUM_NOT_A_NUMBER.message)
+                .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @DisplayName("[오류 테스트] 입력한 보너스 번호가 범위를 벗어남")
     @Test
-    void bonusNumberOutOfRange(){
+    void bonusNumberOutOfRange() {
         //입력값 정의
         String input = "47";
         ByteArrayInputStream byteInput = new ByteArrayInputStream(input.getBytes());
@@ -117,12 +142,13 @@ class WinningNumberMakerTest {
         //when //then
         assertThatThrownBy(() -> winningNumberMaker.makeBonusNumber())
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.NUMBER_OUT_OF_RANGE.message);
+                .hasMessage(ErrorMessage.NUMBER_OUT_OF_RANGE.message)
+                .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @DisplayName("[오류 테스트] 입력한 보너스 번호가 당첨번호에 존재")
     @Test
-    void winningNumbersContainsBonusNumber(){
+    void winningNumbersContainsBonusNumber() {
         //입력값 세팅
         String[] numbers = {"1", "4", "7", "8", "20", "34"};
         String bonusNumberInput = "20";
@@ -134,6 +160,7 @@ class WinningNumberMakerTest {
         //when //then
         assertThatThrownBy(() -> winningNumberMaker.makeBonusNumber())
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.WINNING_NUM_CONTAINS_BONUS_NUM.message);
+                .hasMessage(ErrorMessage.WINNING_NUM_CONTAINS_BONUS_NUM.message)
+                .hasMessageContaining(ERROR_MESSAGE);
     }
 }
