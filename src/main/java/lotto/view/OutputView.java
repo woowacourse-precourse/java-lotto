08a25c27bbbb0108dc93b8423e074c0ef8579106
desfox <desfox99@naver.com>
2022-11-14 20:11:40 +0900

    feat(OutputView): 당첨 번호 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,13 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
     public void printPurchasePriceMessage() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void printWinningNumbers(List<Integer> winningNumbers) {
+        System.out.println("winningNumbers = " + winningNumbers);
     }
 }
