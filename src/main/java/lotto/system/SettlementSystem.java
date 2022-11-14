d06053bcae575583642593f248c023914a17b33e
JaehongDev <println@kakao.com>
2022-11-14 20:33:30 +0900

    refactor (SettlementSystem) : UI로직과 비즈니스 로직 분리

--- src/main/java/lotto/system/SettlementSystem.java
+++ src/main/java/lotto/system/SettlementSystem.java
@@ -7,6 +7,10 @@ import lotto.domain.money.Money;
 import lotto.domain.score.Ranking;
 
 public class SettlementSystem {
+    private final static String MESSAGE_ALL_PROFITS = "총 수익률은 %.1f%%입니다.";
+    private final static String MESSAGE_WINNING_STATISTICS = "당첨 통계";
+    private final static String MESSAGE_HYPHEN = "---";
+    private static final String MESSAGE_AMOUNT = " - %d개";
     private final Map<Ranking, Integer> settlement;
     private final IoSystem io;
 
@@ -16,6 +20,21 @@ public class SettlementSystem {
     }
 
     public void printCalculateProfits(Money money) {
+        Money profits = calculateProfits();
+
+        io.printBeforeNextLine(String.format(MESSAGE_ALL_PROFITS, money.calculateROI(profits)));
+    }
+
+
+    public void printSettlementResult(LottoBundle lottoBundle, AnswerLotto answerLotto) {
+        String result = settlementResult(lottoBundle, answerLotto);
+
+        io.printBeforeNextLine(MESSAGE_WINNING_STATISTICS);
+        io.printBeforeNextLine(MESSAGE_HYPHEN);
+        io.printBeforeNextLine(result);
+    }
+
+    private Money calculateProfits() {
         var profits = Money.generateMoney(0L);
 
         for (var rankAdnCount : settlement.entrySet()) {
@@ -23,34 +42,38 @@ public class SettlementSystem {
                     .calculateProfits(rankAdnCount.getValue())
             );
         }
+        
+        return profits;
+    }
 
-        io.printBeforeNextLine(String.format("총 수익률은 %.1f%%입니다.", money.calculateROI(profits)));
+    private String settlementResult(LottoBundle lottoBundle, AnswerLotto answerLotto) {
+        this.startRaffle(lottoBundle, answerLotto);
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (var rankAndCount : settlement.entrySet()) {
+            var rankScoreResult = rankAndCount.getKey()
+                    .result();
+            var rankScoreCount = String.format(MESSAGE_AMOUNT, rankAndCount.getValue());
+
+            stringBuilder.append(rankScoreResult)
+                    .append(rankScoreCount);
+        }
+
+        return stringBuilder.toString()
+                .trim();
     }
 
     private void startRaffle(LottoBundle lottoBundle, AnswerLotto answerLotto) {
         for (var randomLotto : lottoBundle.getLottoBundle()) {
             var rank = answerLotto.compareLotto(randomLotto)
                     .generateRank();
+
             if (settlement.containsKey(rank)) {
                 settlement.put(rank, settlement.get(rank) + 1);
             }
         }
     }
 
-    public void printSettlementResult(LottoBundle lottoBundle, AnswerLotto answerLotto) {
-        this.startRaffle(lottoBundle, answerLotto);
 
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (var rankAndCount : settlement.entrySet()) {
-            stringBuilder.append(rankAndCount.getKey().result())
-                    .append(String.format(" - %d개\n", rankAndCount.getValue()));
-        }
-
-        var result = stringBuilder.toString().trim();
-
-        io.printBeforeNextLine("당첨 통계");
-        io.printBeforeNextLine("---");
-        io.printBeforeNextLine(result);
-    }
 }
