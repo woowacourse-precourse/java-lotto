b86135bb0fd4aa1009d4af61f6088581543e7947
Dabeen Jeong <hello70825@gmail.com>
2022-11-14 20:52:37 +0900

    feat: 1,000원 단위의 금액을 입력하는지 확인하는 메소드를 추가

--- src/main/java/lotto/view/InputBuyLottoView.java
+++ src/main/java/lotto/view/InputBuyLottoView.java
@@ -1,11 +1,14 @@
 package lotto.view;
 
 import lotto.exception.BuyLottoFormatException;
+import lotto.exception.BuyLottoMoneyFormat;
 
 import java.util.regex.Pattern;
 
 public final class InputBuyLottoView extends InputView {
     private static final Pattern PATTERN = Pattern.compile("\\d+");
+    private static final Integer THOUSAND = 1000;
+    private static final Integer ZERO = 0;
 
     public String getValue() {
         String result = inputValue();
@@ -15,11 +18,20 @@ public final class InputBuyLottoView extends InputView {
 
     private void validate(String inputValue) {
         validateFormat(inputValue);
+        validateMoneyFormat(inputValue);
     }
 
     private void validateFormat(String inputValue) {
         if (!PATTERN.matcher(inputValue).matches()) {
             throw new BuyLottoFormatException();
+        }
+    }
+
+    private void validateMoneyFormat(String inputValue) {
+        int money = Integer.parseInt(inputValue);
+
+        if (money % THOUSAND == ZERO && money / THOUSAND > ZERO) {
+            throw new BuyLottoMoneyFormat();
         }
     }
 }
