3840f8e0f3b477c87535ef41aadbcfc0008acb31
TaemHam <tk4003950@gmail.com>
2022-11-14 20:35:05 +0900

    Docs: Todo 주석 삭제

--- src/test/java/lotto/entity/LottoTest.java
+++ src/test/java/lotto/entity/LottoTest.java
@@ -19,7 +19,6 @@ class LottoTest {
     @DisplayName("번호에 중복된 숫자는 없다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
