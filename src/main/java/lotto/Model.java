83d8ee23b44821881f5e67518c490af89c451639
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-14 19:06:04 +0900

    feat : Model 구현

--- src/main/java/lotto/Model.java
+++ src/main/java/lotto/Model.java
@@ -1,4 +1,15 @@
 package lotto;
 
+import java.util.List;
+
 public class Model {
+    private List<int[]> lottoNumbers;
+
+    public Model(List<int[]> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    public List<int[]> getLottoNumbers() {
+        return lottoNumbers;
+    }
 }
