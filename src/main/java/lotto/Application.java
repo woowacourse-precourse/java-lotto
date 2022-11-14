dd48cb223f6beb46b300f082737a5b7a91ab5af1
vcho1958 <gusckd57@gmail.com>
2022-11-15 06:09:08 +0900

    feat(Application): LottoManager 동작 추가 - LottoManager 인스턴스 생성 - buyLotto() 기능 작동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,12 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoManager lottoManager = new LottoManager();
+        try {
+            lottoManager.buyLotto();
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
