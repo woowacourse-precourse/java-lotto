21c23475932a393315d90a47cbc3e9bab53e4519
Geun <rlarjs220@naver.com>
2022-11-14 23:32:25 +0900

    예외 사항 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -12,9 +13,18 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("ERROR : 로또 번호는 6개의 숫자로 이루어져야합니다. ");
+        }
+        for(int i = 0; i < numbers.size(); i++) {
+            if (!(numbers.get(i) >= 1 && numbers.get(i) <= 45)) {
+                throw new IllegalArgumentException("ERROR : 로또 번호는 1~45 사이의 숫자입니다..");
+            }
+
+            if (Collections.frequency(numbers, numbers.get(i)) != 1) {
+                throw new IllegalArgumentException("ERROR : 로또 번호는 중복될 수 없습니다.");
+            }
+
+            // TODO: 추가 기능 구현
         }
     }
-
-    // TODO: 추가 기능 구현
 }
