64590b03ac128927fe00d2aba2c2c7ab918d97ee
Basak <chgy2131@naver.com>
2022-11-15 02:44:38 +0900

    fix(Buyer): 수익률 추정식이 잘못되었던 문제 수정
    - 정수끼리 나눠서 소숫점이 누락되던 오류 수정
    - 백분율로 나타내기 위해 100을 곱해주도록 수정

--- src/main/java/lotto/Buyer.java
+++ src/main/java/lotto/Buyer.java
@@ -1,51 +1,80 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
-public class Buyer {
-    private int seedMoney;
-    private int totalMoney;
-    private float rate;
-    private List<Lotto> myLottos;
-    private List<Integer> myPrize;
 
-    public void buyLotto() {
-        inputSeedMoney();
-        validateSeedMoney();
+public class Buyer {
+    private List<Lotto> myLottos;
+    private Store store;
+
+    Buyer(Store store) {
+        this.store = store;
     }
 
-    private void inputSeedMoney() {
-        this.seedMoney = seedMoney;
+    // 입력된 돈으로 로또를 구입한다.
+    public void buyLotto(int seedMoney) {
+        this.myLottos = store.sellLotto(seedMoney);
+    }
+
+    public int inputSeedMoney() {
+        printInputSeedMoney();
+        String input = Console.readLine();
+        validateSeedMoney(input);
+        return Integer.parseInt(input);
     }
 
     private void printInputSeedMoney() {
-        System.out.println("");
+        System.out.println("구입금액을 입력해 주세요.");
     }
 
-    private void validateSeedMoney() {
+    private void validateSeedMoney(String input) {
+        // 숫자로만 이루어져있지 않으면 예외 발생
+        if (!input.matches("^[0-9]+$")) {
+            throw new IllegalArgumentException();
+        }
     }
 
-    public void checkPrize() {
-        requestPrizeToStore();
+    public void checkPrize(List<Lotto> lottos) {
+        LottoResult lottoResult = requestPrizeToStore(lottos);
+        printPrize(lottoResult);
     }
 
-    private void requestPrizeToStore() {
+    private LottoResult requestPrizeToStore(List<Lotto> lottos) {
+        return store.checkPrize(lottos);
     }
 
-    private void printPrize() {
-        System.out.println("");
+    private void printPrize(LottoResult result) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for(Prize prize : Prize.values()) {
+            System.out.println(prize.getMatching() + "개 일치 (" + prize.getPrizeMoney() + ") - " + "" + "개");
+        }
     }
 
-    public void checkRate() {
+    public float checkRate(int seedMoney, int lastMoney) {
+        float rate = calcRate(seedMoney, lastMoney);
+        rate = roundRate(rate);
+        printRate(rate);
+        return rate;
     }
 
-    private void calcRate() {
+    private float calcRate(int seedMoney, int lastMoney) {
+        float rate = (float)lastMoney / seedMoney * 100;
+        rate = roundRate(rate);
+        return rate;
     }
 
-    private void roundRate() {
+    private float roundRate(float f) {
+        int r = Math.round(f * 100);
+        return (float)r / 100;
     }
 
-    private void printRate() {
-        System.out.println("");
+    private void printRate(float rate) {
+        System.out.println("총 수익률은 " + rate + "%입니다.");
+    }
+
+    public List<Lotto> getMyLottos() {
+        return myLottos;
     }
 }
