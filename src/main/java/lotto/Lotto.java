85cba1758b5ae0304d3147a095d243efa28a3ef2
dvlpr0 <rladjswkd@naver.com>
2022-11-14 19:05:42 +0900

    feat: 로또 구입 금액에 대한 유효성 검사 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,7 +5,11 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Lotto {
-    private static final String AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    private static final String INVALID_PURCHASE_AMOUNT_MESSAGE = ERROR_MESSAGE + " 구매 금액은 1,000원 단위여야 합니다.";
+    private static final char INVALID_MOST_SIGNIFICANT_DIGIT = '0';
+    private static final int SMALLEST_UNIT = 1000;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -19,11 +23,35 @@ public class Lotto {
         }
     }
 
-    public static String readPurchaseAmount() {
+    private boolean isDigits(String userInput) {
+        int len = userInput.length();
+        int i = 0;
+
+        while (i < len && Character.isDigit(userInput.charAt(i))) {
+            i++;
+        }
+        return (i == len);
+    }
+
+    private boolean isValidFormat(String userInput) {
+        return (userInput.charAt(0) != INVALID_MOST_SIGNIFICANT_DIGIT);
+    }
+
+    private boolean isMultipleOfThousand(String userInput) {
+        return (Integer.parseInt(userInput) % SMALLEST_UNIT == 0);
+    }
+
+    private void validatePurchaseAmount(String purchaseAmount) {
+        if (!isDigits(purchaseAmount) || !isValidFormat(purchaseAmount) || !isMultipleOfThousand(purchaseAmount))
+            throw (new IllegalArgumentException(INVALID_PURCHASE_AMOUNT_MESSAGE));
+    }
+
+    public String readPurchaseAmount() {
         String purchaseAmount;
 
-        System.out.println(AMOUNT_MESSAGE);
+        System.out.println(PURCHASE_AMOUNT_MESSAGE);
         purchaseAmount = readLine();
+        validatePurchaseAmount(purchaseAmount);
         return (purchaseAmount);
     }
 }
