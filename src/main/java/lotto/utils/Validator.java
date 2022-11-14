90c7f7a766b9a536190d08772979981ec2d1cee4
Jeangyu <gjwlsrb1700@naver.com>
2022-11-15 00:31:18 +0900

    [FEAT: lotto] 중복된 숫자가 있는지 확인하는 validate 함수 구현

--- src/main/java/lotto/utils/Validator.java
+++ src/main/java/lotto/utils/Validator.java
@@ -1,7 +1,9 @@
 package lotto.utils;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validator {
     public static final int WINNING_NUMS = 6;
@@ -15,13 +17,20 @@ public class Validator {
         for (String number : numbers){
             validateInteger(number);
         }
-
+        validateDuplicate(numbers);
         validateArraySize(numbers, WINNING_NUMS);
     }
 
     public static void validateArraySize(List<String> numbers, int size){
         if (numbers.size() != size){
             throw new IllegalArgumentException("숫자의 개수가 잘못 되었습니다.");
+        }
+    }
+
+    public static void validateDuplicate(List<String> numbers){
+        Set<String> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != WINNING_NUMS){
+            throw new IllegalArgumentException("중복된 숫자가 존재합니다.");
         }
     }
 }
