60c30656f7aa081cc3e4a942a077529372cb424e
jini_11 <jieun21124@naver.com>
2022-11-15 03:29:59 +0900

    test: 등수 초기화 테스트 케이스 추가

--- src/test/java/lotto/domain/RankCalculatorTest.java
+++ src/test/java/lotto/domain/RankCalculatorTest.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +12,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RankCalculatorTest {
 
-    @DisplayName("로또 번호들과 당첨 번호를 입력하면 등수를 반환한다.")
-    @Test
-    void calculateRankTest() {
-        List<Lotto> lottoTickets = List.of(
+    private List<Lotto> lottoTickets;
+    private UserNumber userNumber;
+    private RankCalculator rankCalculator;
+
+    @BeforeEach
+    void setUp() {
+        lottoTickets = List.of(
                 new Lotto(List.of(8, 21, 23, 41, 42, 43)),
                 new Lotto(List.of(3, 5, 11, 16, 32, 38)),
                 new Lotto(List.of(1, 3, 5, 14, 22, 45)),
@@ -22,11 +27,14 @@ public class RankCalculatorTest {
                 new Lotto(List.of(8, 21, 23, 41, 42, 43))
         );
 
-        UserNumber userNumber = new UserNumber("1,2,3,4,5,6", "7");
+        userNumber = new UserNumber("1,2,3,4,5,6", "7");
+        rankCalculator = new RankCalculator(lottoTickets, userNumber);
+    }
 
-        RankCalculator rankCalculator = new RankCalculator(lottoTickets, userNumber);
-
-        Map<Rank, Integer> ranks = Map.ofEntries(
+    @DisplayName("로또 번호들과 당첨 번호를 입력하면 등수를 반환한다.")
+    @Test
+    void calculateRankTest() {
+        Map<Rank, Integer> expectedRank = Map.ofEntries(
                 Map.entry(Rank.FIRST, 0),
                 Map.entry(Rank.SECOND, 0),
                 Map.entry(Rank.THIRD, 0),
@@ -35,6 +43,20 @@ public class RankCalculatorTest {
                 Map.entry(Rank.NONE, 5)
         );
 
-        assertThat(rankCalculator.getRanks()).isEqualTo(ranks);
+        assertThat(rankCalculator.getRanks()).isEqualTo(expectedRank);
+    }
+
+    @DisplayName("등수를 초기화시킨다.")
+    @Test
+    void initialRankTest() {
+        Map<Rank, Integer> expectedRank = Map.ofEntries(
+                Map.entry(Rank.FIRST, 0),
+                Map.entry(Rank.SECOND, 0),
+                Map.entry(Rank.THIRD, 0),
+                Map.entry(Rank.FOURTH, 0),
+                Map.entry(Rank.FIFTH, 0),
+                Map.entry(Rank.NONE, 0)
+        );
+        assertThat(rankCalculator.initializeRank()).isEqualTo(expectedRank);
     }
 }
