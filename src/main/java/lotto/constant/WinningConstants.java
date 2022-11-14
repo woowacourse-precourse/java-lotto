1e13f64c449e1aabcc1d3e81eec4f6936e6846ce
CAEL0 <kjongmin26@gmail.com>
2022-11-14 17:13:51 +0900

    feat(WinningConstants): 1등부터 5등 데이터 추가

--- src/main/java/lotto/constant/WinningConstants.java
+++ src/main/java/lotto/constant/WinningConstants.java
@@ -2,6 +2,12 @@ package lotto.constant;
 
 public enum WinningConstants {
 
+    FIFTH_PLACE(3, false, 5_000),
+    FOURTH_PLACE(4, false, 50_000),
+    THIRD_PLACE(5, false, 1_500_000),
+    SECOND_PLACE(5, true, 30_000_000),
+    FIRST_PLACE(6, false, 2_000_000_000);
+
     private final int requiredWinningNumber;
     private final boolean hasBonusNumber;
     private final int reward;
