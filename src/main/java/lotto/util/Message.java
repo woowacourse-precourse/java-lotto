40fb1aa63075a0e5dab843fcdd05104ad42fe7e4
JeonSangYong <tkddyd420@naver.com>
2022-11-15 02:53:28 +0900

    fix : 출력 메시지 줄바꿈 추가

--- src/main/java/lotto/util/Message.java
+++ src/main/java/lotto/util/Message.java
@@ -3,9 +3,9 @@ package lotto.util;
 public class Message {
     public static final String INPUT_BUY_LOTTO_PRICE = "구입금액을 입력해 주세요.";
     public static final String BUY_LOTTO = System.lineSeparator() + "%d개를 구매했습니다." + System.lineSeparator();
-    public static final String INPUT_WIN_LOTTO_NUMBER = "당첨 번호를 입력해 주세요." + System.lineSeparator();
-    public static final String INPUT_BONUS_LOTTO_NUMBER = "보너스 번호를 입력해 주세요." + System.lineSeparator();
-    public static final String STATS_WIN_LOTTO = "당첨 통계" + System.lineSeparator() + "---" + System.lineSeparator();
+    public static final String INPUT_WIN_LOTTO_NUMBER = System.lineSeparator() + "당첨 번호를 입력해 주세요." + System.lineSeparator();
+    public static final String INPUT_BONUS_LOTTO_NUMBER = System.lineSeparator() + "보너스 번호를 입력해 주세요." + System.lineSeparator();
+    public static final String STATS_WIN_LOTTO = System.lineSeparator() + "당첨 통계" + System.lineSeparator() + "---" + System.lineSeparator();
     public static final String STATS_RESULT = "%d개 일치 (%s원) - %d개" + System.lineSeparator();
     public static final String STATS_RESULT_BONUS = "%d개 일치, 보너스 볼 일치 (%s원) - %d개" + System.lineSeparator();
     public static final String YIELD_RESULT = "총 수익률은 %.1f%%입니다.";
