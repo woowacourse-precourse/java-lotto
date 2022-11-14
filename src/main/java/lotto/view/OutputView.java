7afd37d321306a5bd98917bb1b09d628818d4248
ParangBird <seekerman@naver.com>
2022-11-14 22:22:15 +0900

    feat: 사용자 당첨 번호 요청 메시지 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 public class OutputView {
     public final static String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    public final static String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     private void printlnMessage(String message) {
 
@@ -13,8 +14,13 @@ public class OutputView {
         System.out.print(message);
     }
 
-    public void printPurchaseAmount() {
+    public void printInputPurchaseAmount() {
 
         printlnMessage(INPUT_PURCHASE_AMOUNT_MESSAGE);
+    }
+
+    public void printInputWinningNumber() {
+
+        printlnMessage(INPUT_WINNING_NUMBER_MESSAGE);
     }
 }
