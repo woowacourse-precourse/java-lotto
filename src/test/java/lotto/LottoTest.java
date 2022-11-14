f47aba40fa380cd187736e62293685b30b7eb3fe
DevJay1024 <devjay1024@gmail.com>
2022-11-14 19:22:42 +0900

    test: 유효한 로또 한줄 (Lotto) 객체 생성 기능 검증을 위한 테스트 케이스 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,9 +5,19 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
+    @DisplayName("로또는 유효하게 생성된다.")
+    @Test
+    void createAndGetLotto() {
+        List<Integer> expected = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> actual = new Lotto(List.of(1, 2, 3, 4, 5, 6)).getNumbers();
+        assertThat(actual).isEqualTo(expected);
+    }
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -15,13 +25,48 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호의 개수가 6개 보다 작으면 예외가 발생한다.")
+    @Test
+    void createLottoByBelowSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 허용 범위보다 작으면 예외가 발생한다.")
+    @Test
+    void createLottoByLowerNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 0, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 허용 범위보다 크면 예외가 발생한다.")
+    @Test
+    void createLottoByUpperNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Getter로 가져온 로또 한 줄에 번호를 추가하려고 하면 예외가 발생한다.")
+    @Test
+    void getLottoNumbersForAddMoreNumber() {
+        assertThatThrownBy(() -> {
+            new Lotto(List.of(1, 2, 3, 4, 5, 6)).getNumbers().add(32);
+        }).isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @DisplayName("Getter로 가져온 로또 한 줄에 번호를 제거하려고 하면 예외가 발생한다.")
+    @Test
+    void getLottoNumbersForReplaceNumber() {
+        assertThatThrownBy(() -> {
+            new Lotto(List.of(1, 2, 3, 4, 5, 6)).getNumbers().remove(0);
+        }).isInstanceOf(UnsupportedOperationException.class);
+    }
+
 }
