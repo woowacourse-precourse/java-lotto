eb33ab1eff509e135d8fc04533922b9e79f42b73
Miseong Kim <miseongk@naver.com>
2022-11-14 23:32:04 +0900

    fix(lotto): add exception for size and duplicated numbers

--- src/main/java/lotto/util/Validation.java
+++ src/main/java/lotto/util/Validation.java
@@ -1,9 +1,17 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Validation {
     private static final String INPUT_NAN_ERROR_MSG = "[ERROR] 문자가 아닌 숫자를 입력해주세요.";
     private static final String INPUT_UNIT_ERROR_MSG = "[ERROR] 금액을 1000원 단위로 입력해주세요.";
     private static final String INPUT_LOTTO_NUMBER_RANGE_ERROR_MSG = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String INPUT_LOTTO_NUMBER_SIZE_ERROR_MSG = "[ERROR] 로또 번호 6개를 입력해주세요.";
+    private static final String INPUT_LOTTO_NUMBER_DUPLICATE_ERROR_MSG = "[ERROR] 로또 번호 6개는 서로 다른 숫자여야 합니다.";
+
 
     public static void validateMoney(String input) {
         if (isInteger(input)) {
@@ -20,8 +28,9 @@ public class Validation {
                 int number = Integer.parseInt(s);
                 checkInRange(number);
             }
-
         }
+        checkSize(numbers);
+        checkDuplicate(numbers);
     }
 
     public static void validateBonusNumbers(String input) {
@@ -44,6 +53,21 @@ public class Validation {
     private static void checkUnit(int money) {
         if (money % 1000 != 0) {
             throw new IllegalArgumentException(INPUT_UNIT_ERROR_MSG);
+        }
+    }
+
+    private static void checkSize(String[] numbers) {
+        if (numbers.length != 6) {
+            throw new IllegalArgumentException(INPUT_LOTTO_NUMBER_SIZE_ERROR_MSG);
+        }
+    }
+
+    private static void checkDuplicate(String[] input) {
+        List<String> numbers = Arrays.asList(input);
+        Set<String> check = new HashSet<>(numbers);
+
+        if (numbers.size() != check.size()) {
+            throw new IllegalArgumentException(INPUT_LOTTO_NUMBER_DUPLICATE_ERROR_MSG);
         }
     }
 
