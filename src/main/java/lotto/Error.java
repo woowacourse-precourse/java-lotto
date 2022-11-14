84871bdd5ad9c7f874c9b4d76315f11a6a2206ea
Yoobi <quddlr96@naver.com>
2022-11-15 00:54:53 +0900

    refactor(lotto): add error code and message

--- src/main/java/lotto/Error.java
+++ src/main/java/lotto/Error.java
@@ -1,7 +1,7 @@
 package lotto;
 
 public enum Error {
-
+    INVALID_LIST("[ERROR] 6개의 숫자를 입력하세요."),
     INVALID_PRICE("[ERROR] 구입 금액은 1000원 단위의 숫자입니다."),
     INVALID_INPUT_VALUE("[ERROR] 각 번호는 1~45 사이의 숫자여야 합니다."),
     INPUT_DUPLICATE("[ERROR] 중복된 숫자가 존재합니다.");
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,7 +16,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(Error.INVALID_LIST.getMessage());
         }
     }
 
