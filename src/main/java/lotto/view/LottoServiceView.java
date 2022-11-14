2254db76a9ba3296b01e863e04d961535daee624
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-15 01:41:06 +0900

    feat: 로또 구입 금액을 입력받는다

---
+++ src/main/java/lotto/view/LottoServiceView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoServiceView {
+
+    public int inputMoney() {
+
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+
+        return 1;
+    }
+}
