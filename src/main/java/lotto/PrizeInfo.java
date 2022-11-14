bfa910d533f101b58bc7dff47361ccdb4573ff0f
Yujin830 <rladbwls30@naver.com>
2022-11-14 23:58:13 +0900

    Feat: enum에 출력, 상금 정보 저장

--- src/main/java/lotto/PrizeInfo.java
+++ src/main/java/lotto/PrizeInfo.java
@@ -1,5 +1,18 @@
 package lotto;
 
 public enum PrizeInfo {
+    Fifth("3개 일치","(5,000원)",5000),
+    Fourth("4개 일치","(50,000원)",50000),
+    Third("5개 일치","(1,500,000원)",1500000),
+    Second("5개 일치, 보너스 볼 일치","(30,000,000원)",30000000),
+    One("6개 일치", "(2,000,000,000원)", 2000000000);
 
+    private final String count, money;
+    private final int realmoney;
+
+    private PrizeInfo(String count, String money, int realmoney){
+        this.count = count;
+        this.money = money;
+        this.realmoney = realmoney;
+    }
 }
