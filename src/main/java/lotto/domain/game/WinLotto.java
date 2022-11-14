c914ca820f7d659c2af2c7896600c72419ea9301
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-15 02:44:55 +0900

    feat(game/WinLotto.java): 생성자, 인스턴스변수 구현하고 당첨내역 초기화하는 함수 구현

---
+++ src/main/java/lotto/domain/game/WinLotto.java
@@ -0,0 +1,33 @@
+package lotto.domain.game;
+
+import lotto.domain.player.Player;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toMap;
+
+public class WinLotto {
+    private final List<Integer> winningNumber;
+    private final int bonusNumber;
+    private final List<List<Integer>> lottoTickets;
+    private final long userMoney;
+    private Map<Ranking, Long> winningResult = new EnumMap<>(Ranking.class);
+
+    public WinLotto(List<Integer> lotto, Player player){
+        int lottoBonusNumberIndex = lotto.size()-1;
+        bonusNumber = lotto.get(lottoBonusNumberIndex);
+        lotto.remove(lottoBonusNumberIndex);
+        winningNumber = lotto;
+        this.lottoTickets = player.getLottoTickets();
+        this.userMoney = player.getMoney();
+        winningResult = initWinningResult();
+    }
+
+    private Map<Ranking, Long> initWinningResult() {
+        return Arrays.stream(Ranking.values()).collect(toMap(ranking -> ranking, ranking -> 0L));
+    }
+
+}
