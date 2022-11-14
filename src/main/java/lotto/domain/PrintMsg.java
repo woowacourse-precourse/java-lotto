e00aa27f786c4df755867337a74c8f4e358b4896
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-15 04:08:56 +0900

    refactor(PrintMsg): PrintMsg 클래스 구성

--- src/main/java/lotto/domain/PrintMsg.java
+++ src/main/java/lotto/domain/PrintMsg.java
@@ -1,2 +1,16 @@
-package lotto.domain;public class PrintMsg {
+package lotto.domain;
+
+public class PrintMsg {
+    public void printMoneyInputMsg() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+    public void printNumberOfLottosMsg(int numOfLottos) {
+        System.out.println(numOfLottos+"개를 구매했습니다.");
+    }
+    public void printWinnerInputMsg() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+    public void printBonusInputMsg() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
 }
--- src/main/java/lotto/domain/Program.java
+++ src/main/java/lotto/domain/Program.java
@@ -3,22 +3,23 @@ package lotto.domain;
 import java.util.List;
 
 public class Program {
+    PrintMsg printMsg = new PrintMsg();
     View view = new View();
     int numOfLottos, bonus;
     ListOfLottos lottoList;
     List<Integer> winner;
 
     public void lottoSetup() {
-        view.printMoneyInputMsg();
+        printMsg.printMoneyInputMsg();
         numOfLottos = view.getMoneyInput();
-        view.printNumberOfLottosMsg(numOfLottos);
+        printMsg.printNumberOfLottosMsg(numOfLottos);
         lottoList = new ListOfLottos(numOfLottos);
         lottoList.printAllLottos();
     }
     public void winSetup() {
-        view.printWinnerInputMsg();
+        printMsg.printWinnerInputMsg();
         winner = view.getWinnerInput();
-        view.printBonusInputMsg();
+        printMsg.printBonusInputMsg();
         bonus = view.getBonusInput(winner);
     }
 
