a4d367c1693ac11cb3e578cffc6272ef9c1a7ea3
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-14 12:51:42 +0900

    feat: 사용자의 입금 금액 타입을 클래스 타입으로 변경

---
+++ src/test/java/lotto/domain/PaymentTest.java
@@ -0,0 +1,67 @@
+package lotto.domain;
+
+import static lotto.constant.Constants.UNIT_OF_LOTTO_PURCHASE;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.constant.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class PaymentTest {
+
+    @Test
+    @DisplayName("0원을 입력했을 때 예외를 테스트")
+    void when_user_input_is_zero() {
+        // given
+        int inputMoney = 0;
+
+        // when, then
+        assertThatThrownBy(() -> new Payment(inputMoney))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.USER_INPUT_MONEY_IS_ZERO.getMessage());
+    }
+
+    @Nested
+    @DisplayName("입력한 구입 금액이")
+    class If_input_value_to_purchase {
+
+        /**
+         * 만약 구매 단위가 1원이라면 나머지가 생길 수 없기 때문에 테스트가 항상 통과한다
+         */
+        @Test
+        @DisplayName("규칙에 맞지 않다면 예외를 던진다")
+        void does_not_comply_with_rule_then_throw_exception() {
+            if (UNIT_OF_LOTTO_PURCHASE == 1) {
+                return;
+            }
+
+            // given
+            int inputMoney = UNIT_OF_LOTTO_PURCHASE - 1;
+
+            // when, then
+            assertThatThrownBy(() -> new Payment(inputMoney))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.UNIT_OF_LOTTO_PURCHASE_DOES_NOT_COMPLY_WITH_RULE.getMessage());
+        }
+
+        /**
+         * 만약 구매 단위가 1원이라면 나머지가 생길 수 없기 때문에 테스트가 항상 통과한다
+         */
+        @Test
+        @DisplayName("규칙에 맞다면 예외를 던지지 않는다")
+        void does_comply_with_rule_then_does_not_throw_exception() {
+            if (UNIT_OF_LOTTO_PURCHASE == 1) {
+                return;
+            }
+
+            // given
+            int inputMoney = UNIT_OF_LOTTO_PURCHASE;
+
+            // when, then
+            assertThatCode(() -> new Payment(inputMoney))
+                    .doesNotThrowAnyException();
+        }
+    }
+}
