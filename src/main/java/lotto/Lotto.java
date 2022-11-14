d5477a812d8a078de0243088f1d1c006341d45db
a1dotls <meeae9408@gmail.com>
2022-11-14 20:42:33 +0900

    feat: 구매 금액 입력 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,5 +17,18 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public int purchase() {
+    	System.out.println("구매금액을 입력해 주세요.");
+    	int purchaseAmount = Integer.parseInt(Console.readLine());
+    	purchaseValidate(purchaseAmount);
+    	return purchaseAmount;
+    }
+    
+    private void purchaseValidate(int purchaseAmount) throws IllegalArgumentException {
+    	if(purchaseAmount % 1000 != 0) {
+    		System.out.println("[ERROR] 1,000원 단위로 입력해주세요.");
+    		throw new IllegalArgumentException();
+    	}
+    }
+    
 }
