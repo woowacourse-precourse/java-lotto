d9436dc329fca071366bf0bbfe7194d2fc14a792
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-15 00:16:22 +0900

    feat : RandomLotto 생성 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.assertj.core.util.Sets;
 
 import java.util.List;
@@ -7,16 +8,22 @@ import java.util.Set;
 
 public class Lotto {
     private static final int LOTTO_SIZE = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
     private static final String LOTTO_SIZE_IS_NOT_6 = "[ERROR] 로또 번호 개수가 6이 아닙니다.";
-    private static final String IS_DUPLICATED_LOTTO_NUMBER = "중복되는 로또 번호가 있습니다.";
-    private final List<LottoNumber> numbers;
+    private static final String IS_DUPLICATED_LOTTO_NUMBER = "[ERROR] 중복되는 로또 번호가 있습니다.";
+    private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateLottoValue(numbers);
         this.numbers = numbers;
     }
 
-    private void validateLottoValue(List<LottoNumber> numbers) {
+    public static Lotto generateRandomLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_SIZE));
+    }
+
+    private void validateLottoValue(List<Integer> numbers) {
         validateLottoSize(numbers);
         validateIsNotDuplicatedNumberInLotto(numbers);
     }
