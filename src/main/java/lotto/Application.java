cad303097aa53f9b17fdc944bf2da2d924812815
오승윤 <>
2022-11-15 00:21:19 +0900

    feat (Application) : 메인에 실행 코드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.controller.LottoSystemController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoSystemController lottoSystemController = new LottoSystemController();
+        lottoSystemController.run();
     }
 }
