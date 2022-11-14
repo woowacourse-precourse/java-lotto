141460a458eebb52d728cc72b6850f2287425634
Gibeom Nam <rlqja0523@naver.com>
2022-11-15 04:15:26 +0900

    feat: 사용자에게 구입금액을 입력받는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.PurchaseAmount;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static int askPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String purchaseAmount = Console.readLine();
+        PurchaseAmount.checkException(purchaseAmount);
+        System.out.println();
+        return Integer.parseInt(purchaseAmount);
     }
 }
