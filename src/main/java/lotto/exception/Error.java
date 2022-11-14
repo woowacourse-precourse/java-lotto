6bc052f224304d13b4260f190348dd89e29b02a6
im-shung <seoyoung1113@gmail.com>
2022-11-15 03:37:53 +0900

    feat: :sparkles: 에러 메시지 출력하는 Error 클래스 생성

---
+++ src/main/java/lotto/exception/Error.java
@@ -0,0 +1,17 @@
+package lotto.exception;
+
+public enum Error {
+    WRONG_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    OVER_SIZE("로또 번호의 개수는 6개여야만 합니다."),
+    NOT_DUPLICATE("로또 번호는 중복되지 않아야 합니다.");
+
+    private final String message;
+
+    Error(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return "[Error]" + message;
+    }
+}
