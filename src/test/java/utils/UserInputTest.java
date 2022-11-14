b3f3af6cd5c1cdb827c4ec0b3d52299819b5b6dc
MinSungKim <timkim01@naver.com>
2022-11-14 17:02:19 +0900

    test: 구입금액이 숫자인지 유효성검사 테스트코드 작성

---
+++ src/test/java/utils/UserInputTest.java
@@ -0,0 +1,26 @@
+package utils;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserInputTest {
+
+    @Test
+    void 구입금액이_숫자인지_유효성검사_테스트(){
+        String input = "abc";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            UserInput.inputMoney();
+        });
+        assertEquals("[ERROR] 숫자만 입력해주세요.", exception.getMessage());
+    }
+
+}
