c07b846d652b42f67e9f5366a958852945e1a5d0
whdcks420 <whdcks420@gmail.com>
2022-11-14 22:50:11 +0900

    feat: 수익금 포멧 기능

--- src/main/java/lotto/utils/FormatYieldNumber.java
+++ src/main/java/lotto/utils/FormatYieldNumber.java
@@ -1,2 +1,12 @@
-package lotto.utils;public class FormatYieldNumber {
+package lotto.utils;
+
+import java.text.DecimalFormat;
+
+public class FormatYieldNumber {
+
+    private static final DecimalFormat decimalFormat = new DecimalFormat("#,##0.0");
+
+    public static String get(double yield) {
+        return decimalFormat.format(yield);
+    }
 }
