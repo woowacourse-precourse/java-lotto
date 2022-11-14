0e65cf51c4c031e34b351d98c8628016b276f1ed
이건회 <rg970604@naver.com>
2022-11-15 07:52:18 +0900

    feat(GameStartOrExit.java) : 게임 실행 종료 시점 관리

---
+++ src/main/java/lotto/game/GameStartOrExit.java
@@ -0,0 +1,12 @@
+package lotto.game;
+
+public class GameStartOrExit {
+    public static void gameStart(){
+        try {
+            GamePlay.play();
+        } catch (Exception ignore) {
+            System.out.println("[ERROR]" + ignore.getMessage());
+        }
+    }
+
+}
