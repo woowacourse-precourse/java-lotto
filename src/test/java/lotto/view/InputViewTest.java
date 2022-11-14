e52472377116ea8316b110e88d63b1028aab9238
skydreamer21 <skydreamer210@gmail.com>
2022-11-14 19:21:41 +0900

    test(InputViewTest): 당첨 번호 6자리 입력 기능 테스트 추가
    [세부사항]
    - 테스트 목록
    1. 숫자 앞 뒤에 공백이 있는 winningNumber 입력은 정상 처리
    2. winning number 입력으로 숫자가 아닌 값이 입력된 경우 에러를 발생
    3. winning number 입력으로 쉼표 사이에 공백이 있을 경우 에러를 발생
    
    - 주의 사항 : 테스트 개별 실행 필요

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -3,6 +3,7 @@ package lotto.view;
 import org.junit.jupiter.api.*;
 
 import java.io.ByteArrayInputStream;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -34,5 +35,33 @@ public class InputViewTest {
         assertThatThrownBy(InputView::takeMoneyInput)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자만 입력해주세요.");
+    }
+
+    @DisplayName("숫자 앞 뒤에 공백이 있는 winningNumber 입력은 정상 처리한다.")
+    @Test
+    void winningNumberInputWithSpace() {
+        String input = " 1,2,3, 4, 5, 6 \n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        assertThat(InputView.takeWinningNumberInput()).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @DisplayName("winning number 입력으로 숫자가 아닌 값이 입력된 경우 에러를 발생시킨다.")
+    @Test
+    void winningNumberInputWithNonNumericValue() {
+        String input = "1,2,a12,4\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        assertThatThrownBy(InputView::takeWinningNumberInput)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자만 입력해주세요.");
+    }
+
+    @DisplayName("winning number 입력으로 쉼표 사이에 공백이 있을 경우 에러를 발생시킨다.")
+    @Test
+    void winningNumberInputWithEmptyValue() {
+        String input = "1, ,3,4\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        assertThatThrownBy(InputView::takeWinningNumberInput)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력된 요소 중 비어있는 값이 있습니다.");
     }
 }
