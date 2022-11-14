ca1d23ee8aa0179cca8f9075b9bffd70a3116630
DongWooE <ehddn977@naver.com>
2022-11-15 03:18:04 +0900

    [test] 10번 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +24,52 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("일치하는 개수가 3개이다")
+    @Test
+    void createLottoAndCalcResultIs3() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        //when
+        MatchDto matchDto = lotto.calcMatches(List.of(1, 2, 3, 7, 8, 9), 42);
+        //then
+        assertThat(matchDto.getMatchCount()).isEqualTo(3);
+        assertThat(matchDto.isBonusState()).isEqualTo(false);
+    }
+
+    @DisplayName("일치하는 개수가 4개이다")
+    @Test
+    void createLottoAndCalcResultIs4() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        //when
+        MatchDto matchDto = lotto.calcMatches(List.of(1, 2, 3, 4, 8, 9), 42);
+        //then
+        assertThat(matchDto.getMatchCount()).isEqualTo(4);
+        assertThat(matchDto.isBonusState()).isEqualTo(false);
+    }
+
+    @DisplayName("일치하는 개수가 5개이다")
+    @Test
+    void createLottoAndCalcResultIs5() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        //when
+        MatchDto matchDto = lotto.calcMatches(List.of(1, 2, 3, 4, 5, 9), 42);
+        //then
+        assertThat(matchDto.getMatchCount()).isEqualTo(5);
+        assertThat(matchDto.isBonusState()).isEqualTo(false);
+    }
+
+    @DisplayName("일치하는 개수가 5개이고 BonusState가 true이다")
+    @Test
+    void createLottoAndCalcResultIs5AndBonusStateTrue() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 42));
+        //when
+        MatchDto matchDto = lotto.calcMatches(List.of(1, 2, 3, 4, 5, 9), 42);
+        //then
+        assertThat(matchDto.getMatchCount()).isEqualTo(5);
+        assertThat(matchDto.isBonusState()).isEqualTo(true);
+    }
     // 아래에 추가 테스트 작성 가능
 }
