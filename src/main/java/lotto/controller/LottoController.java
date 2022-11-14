1529f898e5a6fe7c66f54832976f9cecbe7871bf
Jeeseob <jeeseob5761@gmail.com>
2022-11-14 16:55:06 +0900

    feat: 로또 게임 로직 처리(입력) 객체에 함수 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,34 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.model.Lotto;
+import lotto.service.LottoService;
+
+import java.util.List;
+
 /**
  * @Author : Jeeseob
  * @CreateAt : 2022/11/14
  */
 public class LottoController {
+    private final LottoService lottoService;
+
+    public LottoController(LottoService lottoService) {
+        this.lottoService = lottoService;
+    }
+
+    // TODO: 구입금액 입력
+    public int inputPurchaseAmount() {
+        return 8;
+    }
+
+    //TODO: 당첨 번호 입력
+    public Lotto inputWinningNumbers() {
+        return new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    //TODO: 보너스 번호 입력
+    public int inputBonusNumber() {
+        return 7;
+    }
 }
--- src/main/java/lotto/view/LottoGame.java
+++ src/main/java/lotto/view/LottoGame.java
@@ -1,5 +1,9 @@
 package lotto.view;
 
+import lotto.controller.LottoController;
+import lotto.model.Lotto;
+import lotto.service.LottoService;
+
 /**
  * @Author : Jeeseob
  * @CreateAt : 2022/11/14
@@ -9,14 +13,15 @@ public class LottoGame {
     private static final String NUMBER_OF_BOUGHT_LOTTO = "개를 구매했습니다.";
     private static final String INPUT_PRIZE_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private final LottoController lottoController = new LottoController(new LottoService());
     public void start() {
         System.out.println(INPUT_PURCHASE_AMOUNT);
-        // TODO: 구입 금액 입력 받기
-        // TODO: 구매한 로또 개수 출력
+        int purchaseAmount = lottoController.inputPurchaseAmount();     // 구입 금액 입력 받기
+        System.out.println(purchaseAmount + NUMBER_OF_BOUGHT_LOTTO);    // 구매한 로또 개수 출력
         System.out.println(INPUT_PRIZE_NUMBER);
-        // TODO: 당첨 번호 입력 받기
+        Lotto winningLotto = lottoController.inputWinningNumbers();     // 당첨 번호 입력 받기
         System.out.println(INPUT_BONUS_NUMBER);
-        // TODO: 보너스 번호 입력 받기
+        int bonusNumber = lottoController.inputBonusNumber();           // 보너스 번호 입력 받기
         // TODO: 당첨 통계 출력
     }
 }
