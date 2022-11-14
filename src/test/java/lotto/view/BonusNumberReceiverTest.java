4f49ce8a0d2fcc01d6267c8384ffb6c570a98825
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-15 01:27:02 +0900

    Revert "feat(main\java\lotto\view\BonusNumberReceiver) : implement BonusNumberReceiver"
    This reverts commit 0c42bb08f026cc342e07df28a308df8bf3640b9c.

--- src/test/java/lotto/view/BonusNumberReceiverTest.java
+++ src/test/java/lotto/view/BonusNumberReceiverTest.java
@@ -1,7 +1,6 @@
 package lotto.view;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,10 +15,6 @@ public class BonusNumberReceiverTest {
     @DisplayName("보너스 번호를 입력받는다.")
     @Test
     void receiveBonusNumber() {
-        String expected = "7";
-        InputStream in = new ByteArrayInputStream(expected.getBytes());
-        System.setIn(in);
 
-        assertThat(Integer.parseInt(expected)).isEqualTo(BonusNumberReceiver.receiveBonusNumber());
     }
 }
