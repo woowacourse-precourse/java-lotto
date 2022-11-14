0056762a140083b0891ed73bd5909ab0baf79252
vcho1958 <gusckd57@gmail.com>
2022-11-15 06:49:56 +0900

    feat(AnswerNumberList): 1~45 사이 숫자인지 검증하는 기능 추가

--- src/main/java/lotto/AnswerNumberList.java
+++ src/main/java/lotto/AnswerNumberList.java
@@ -9,5 +9,15 @@ public class AnswerNumberList {
     public AnswerNumberList(List<Long> answerNumbers, Long bonusNumber) {
         this.winningNumbers = answerNumbers;
         this.bonusNumber = bonusNumber;
+        validate();
+    }
+
+    private void checkRangeOfNumber(Long number){
+        if(number < 1 || number > 45)throw new IllegalArgumentException("[ERROR] 1~45 안의 수가 아닙니다.");
+    }
+
+    private void validate(){
+        this.winningNumbers.forEach(this::checkRangeOfNumber);
+        checkRangeOfNumber(bonusNumber);
     }
 }
