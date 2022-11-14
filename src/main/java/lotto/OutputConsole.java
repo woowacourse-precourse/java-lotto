9d1d8ee963df3066c21d19c4df3932f44746b656
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-14 22:53:29 +0900

    feat(OutputConsole): 구매한 로또 수량 및 번호를 출력하는 기능 구현

--- src/main/java/lotto/OutputConsole.java
+++ src/main/java/lotto/OutputConsole.java
@@ -2,8 +2,19 @@ package lotto;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputConsole {
 
     public static final String PURCHASE_HISTORY_OUTPUT_MESSAGE = "8개를 구매했습니다.";
+
+    public static void printLottoList(List<Lotto> lottos) {
+        System.out.println(PURCHASE_HISTORY_OUTPUT_MESSAGE);
+        for (int i = 0; i < lottos.size(); i++) {
+            List<Integer> lottoNumbers= lottos.get(i).getNumbers();
+            Collections.sort(lottoNumbers);
+            String lottoNumbersMessage = lottoNumbers.stream().map(String::valueOf).collect(Collectors.joining(", "));
+            System.out.println("[" + lottoNumbersMessage + "]");
+        }
+    }
 }
