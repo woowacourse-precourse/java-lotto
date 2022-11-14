753912492c4c1c05d89baf90919d28f01d2257b0
kang jihyeong <her4386@gmail.com>
2022-11-15 03:32:55 +0900

    Feat / 통계 멘트 출력 기능

--- src/main/java/lotto/ui/Output.java
+++ src/main/java/lotto/ui/Output.java
@@ -1,5 +1,11 @@
 package lotto.ui;
 
+import lotto.setting.WinningEnum;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 public class Output {
 
     public static void printInputMoney(){
@@ -7,7 +13,11 @@ public class Output {
     }
 
     public static void printBoughtLotto(int lottoCount){
-        System.out.printf("%d개를 구매했습니다.\n",lottoCount);
+        System.out.printf("%d개를 구매했습니다.\n", lottoCount);
+    }
+
+    public static void printBoughtLottoNumbers(List<Integer> lotto){
+        System.out.println(String.format("%s", lotto.toString()));
     }
 
     public static void printInputWinningNumber(){
@@ -18,18 +28,21 @@ public class Output {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
-    public static void printExceptionError(){
-        System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-    }
-
-    public static void printWinningStatistics(){
+    public static void printWinningStatistics(Map<WinningEnum, Integer> statistics){
         System.out.println("당첨 통계");
         System.out.println("---");
 
-        // TODO
-        // 당첨 내역 출력
-        // n개 일치, k원, - i개
+        for(WinningEnum winning : WinningEnum.values()){
+            if(winning.isHaveToCorrectBonusNumber()) {
+                System.out.println(String.format("%d개 일치, 보너스 볼 일치 (%,d원) - %d개", winning.getCorrectNumberCount(), winning.getWinningAmount(), Optional.ofNullable(statistics.get(winning)).orElse(0)));
+            }
+            else{
+                System.out.println(String.format("%d개 일치 (%,d원) - %d개", winning.getCorrectNumberCount(), winning.getWinningAmount(), Optional.ofNullable(statistics.get(winning)).orElse(0)));
+            }
+        }
     }
 
-
+    public static void printYield(double yield){
+        System.out.println(String.format("총 수익률은 %.1f%%입니다.", yield));
+    }
 }
