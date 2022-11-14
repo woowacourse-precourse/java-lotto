0dae08c25874cde86f5d2db1e8545f5887cf6ff2
youngin <bluest12@naver.com>
2022-11-14 16:32:56 +0900

    feat: Create input method
    금액 입력, 정수로 변환하는 메서드 생성

--- src/main/java/lotto/Purchaser.java
+++ src/main/java/lotto/Purchaser.java
@@ -1,15 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Purchaser {
-    public void purchaseCount() {
+    private int purchaseSum;
+    public String inputPurchaseAmount() {
+        System.out.println("구입금액을 1000원 단위로 입력해 주세요.");
+        return Console.readLine();
+    }
+    public int changePurchaseAmount() {
+        purchaseSum = Integer.parseInt(inputPurchaseAmount());
+        return purchaseSum;
+    }
+
+
+
+    public
+
+    public List<Integer> inputLottoNum() {
 
     }
 
-    public void inputLottoNum() {
-
-    }
-
-    public void inputBonusNum() {
+    public int inputBonusNum() {
 
     }
 }
