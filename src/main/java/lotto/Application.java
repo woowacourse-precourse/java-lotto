8ca68a8267705be62e8693ca0a248ac47d44a4e2
anneu <b.imnuj@gmail.com>
2022-11-14 18:04:24 +0900

    fix: 예외 발생 시 예외 메세지만 출력되고 프로그램이 종료되도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,10 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         LottoGameController lottoGameController = new LottoGameController();
-        lottoGameController.start();
+        try {
+            lottoGameController.start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
