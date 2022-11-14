c8be85095a2456b034b8c5d25726882e45f99cd7
jonghun <jonghuncu@gmail.com>
2022-11-15 01:32:59 +0900

    feat(lotto) : 구매금액, 로또 번호 - 잘못된 값 입력시 예외 처리 기능
    [예외 발생 시 로그를 남기고, 프로그램이 종료되는 상황을 생각]

--- src/main/java/lotto/PrintInputMoneyGenerator.java
+++ src/main/java/lotto/PrintInputMoneyGenerator.java
@@ -2,17 +2,22 @@ package lotto;
 
 public class PrintInputMoneyGenerator {
     public static int printInputMoney() {
-        int inputMoney;
+        int inputMoney = 0;
 
         try {
             inputMoney = InputUserMoney.InputMoney();
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            e.getStackTrace();
-            throw e;
-        } finally {
-            System.out.println("[ERROR] 잘못된 입력");
+            System.out.println("[ERROR] 구매금액은 숫자여야합니다.");
+//            throw e;
+            /**
+             * throw e 의 역할이 예외를 처리? 하는 역할
+             * 예외를 발생시키는 것과 강제로 throw를 통해 발생시키는 것은 다른것 ??
+             * 예외 발생 시 로그를 남기고, 프로그램이 종료되는 상황을 생각해보라
+             */
         }
+//        finally { // 이렇게도 시도해봄 ..
+//            System.out.println("[ERROR] 구매금액은 숫자여야 합니다.");
+//        }
 
         try {
             boolean divideBoolean = DivideInputMoneyGenerator.divideInputMoney(inputMoney);
