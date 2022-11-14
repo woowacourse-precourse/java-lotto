f7712e360a13b78749a10c57c38877603edbf3c8
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-14 11:14:15 +0900

    feat: 구매금액, 로또 자동번호 생성, 당첨번호 입력 화면 출력 기능 구현
    start 메서드를 이용하여 사용자로부터 구매금액, 당첨번호를 입력받고 로또 자동번호를 출력하는 기능 개발

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -5,6 +5,7 @@ import lotto.util.BonusNumberValidator;
 import lotto.util.PurchaseAmtValidator;
 import lotto.util.WinningNumberValidator;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class LottoMachine {
@@ -12,9 +13,20 @@ public class LottoMachine {
     private final Input input;
     private final NumberGenerator numberGenerator;
 
+    private LinkedList<Integer> winningNumWithBonusList;
+    private List<Lotto> quickPicks;
+    private int purchaseAmt;
+
     public LottoMachine() {
         this.input = new Input(new PurchaseAmtValidator(), new WinningNumberValidator(), new BonusNumberValidator());
         this.numberGenerator = new NumberGenerator();
+    }
+
+    public void start() {
+        purchaseAmt = input.enterPurchaseAmt();
+        quickPicks = numberGenerator.getQuickPick(purchaseAmt);
+        winningNumWithBonusList = input.enterWinningNumber();
+
     }
 
 }
