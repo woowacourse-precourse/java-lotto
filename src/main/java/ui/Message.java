c0f22eed696a9f18d198752bbe85cf333aaa7c2c
ch200203 <ch200205@naver.com>
2022-11-14 12:34:11 +0900

    feat : input Message 구현

--- src/main/java/ui/Message.java
+++ src/main/java/ui/Message.java
@@ -1,10 +1,20 @@
 package ui;
 
 public class Message {
-    private static final String INPUT_PRICE = "구입 금액을 입력하세요.";
-    // private static final String
+
+    private static final String INPUT_PRICE_MESSAGE = "구입 금액을 입력하세요.";
+    private static final String INPUT_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요";
 
     public void INPUT_PRCIE_MESSAGE() {
-        System.out.println(INPUT_PRICE);
+        System.out.println(INPUT_PRICE_MESSAGE);
+    }
+
+    public void INPUT_NUMBER_MESSAGE() {
+        System.out.println(INPUT_PRICE_MESSAGE);
+    }
+
+    public void INPUT_BONUS_NUMBER_MESSAGE() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
     }
 }
