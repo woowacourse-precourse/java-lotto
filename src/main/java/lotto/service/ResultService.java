d1c041607cda8c9ae87a9207fe4594896228852d
choijoohee213 <choijoohee@naver.com>
2022-11-14 22:53:36 +0900

    fix: 1~45 범위 체크 오류 수정

--- src/main/java/lotto/service/ResultService.java
+++ src/main/java/lotto/service/ResultService.java
@@ -1,12 +1,12 @@
 package lotto.service;
 
-import static lotto.Constants.MAXIMUM_WINNING_NUMBER;
-import static lotto.Constants.MINIMUM_WINNING_NUMBER;
-import static lotto.Constants.WINNING_NUMBERS_IS_NOT_IN_RANGE_ERROR_MESSAGE;
-import static lotto.Constants.WINNING_NUMBERS_IS_NOT_NUMBER_ERROR_MESSAGE;
-import static lotto.Constants.WINNING_NUMBERS_SEPARATOR;
-import static lotto.Constants.WINNING_NUMBERS_SIZE;
-import static lotto.Constants.WINNING_NUMBERS_SIZE_IS_INVALID_ERROR_MESSAGE;
+import static lotto.Constants.LOTTO_NUMBERS_IS_NOT_IN_RANGE_ERROR_MESSAGE;
+import static lotto.Constants.LOTTO_NUMBERS_IS_NOT_NUMBER_ERROR_MESSAGE;
+import static lotto.Constants.LOTTO_NUMBERS_SIZE;
+import static lotto.Constants.LOTTO_NUMBERS_SIZE_IS_INVALID_ERROR_MESSAGE;
+import static lotto.Constants.LOTTO_WINNING_NUMBERS_SEPARATOR;
+import static lotto.Constants.MAXIMUM_LOTTO_NUMBER;
+import static lotto.Constants.MINIMUM_LOTTO_NUMBER;
 
 public class ResultService {
     private static ResultService instance;
@@ -22,22 +22,22 @@ public class ResultService {
     }
 
     public void validate(String winningNumbers) {
-        String[] numbers = winningNumbers.split(WINNING_NUMBERS_SEPARATOR);
+        String[] numbers = winningNumbers.split(LOTTO_WINNING_NUMBERS_SEPARATOR);
         for (String number : numbers) {
             validateNumber(number);
         }
-        if (numbers.length != WINNING_NUMBERS_SIZE) {
-            throw new IllegalArgumentException(WINNING_NUMBERS_SIZE_IS_INVALID_ERROR_MESSAGE);
+        if (numbers.length != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(LOTTO_NUMBERS_SIZE_IS_INVALID_ERROR_MESSAGE);
         }
     }
 
     public void validateNumber(String number) {
         if (!number.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException(WINNING_NUMBERS_IS_NOT_NUMBER_ERROR_MESSAGE);
+            throw new IllegalArgumentException(LOTTO_NUMBERS_IS_NOT_NUMBER_ERROR_MESSAGE);
         }
         int parsed = Integer.parseInt(number);
-        if (parsed <= MINIMUM_WINNING_NUMBER || parsed >= MAXIMUM_WINNING_NUMBER) {
-            throw new IllegalArgumentException(WINNING_NUMBERS_IS_NOT_IN_RANGE_ERROR_MESSAGE);
+        if (parsed < MINIMUM_LOTTO_NUMBER || parsed > MAXIMUM_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(LOTTO_NUMBERS_IS_NOT_IN_RANGE_ERROR_MESSAGE);
         }
     }
 }
