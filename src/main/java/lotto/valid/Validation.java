6e6669fd9c19e62f375f3fecf37b91d32e678ef6
win9 <krrr8@naver.com>
2022-11-14 19:50:19 +0900

    Feat(Valid): 중복체크 validation추가와 정규식 변경
    중복체크 validation을 추가하여 당첨번호 입력시
    중복된 숫자를 발견하면 오류를출력하도록 한다.
    숫자매칭 정규식을 사용하여 문자가 오면 오류가 생기도록 변경한다.

--- src/main/java/lotto/valid/Validation.java
+++ src/main/java/lotto/valid/Validation.java
@@ -1,5 +1,6 @@
 package lotto.valid;
 
+import lotto.Util.OutputPrint;
 import lotto.type.ErrorType;
 import lotto.type.NumberBoundaryType;
 
@@ -7,8 +8,12 @@ import java.util.List;
 
 public class Validation {
 
-    public static void purchaseNumberValid(int number){
-        if (number % 1000 != 0 || number == 0) {
+    public static void purchaseNumberValid(String number){
+        if (!number.matches("-?\\d+(\\.\\d+)?")){
+            throw new IllegalArgumentException();
+        }
+        int numberConversion = Integer.parseInt(number);
+        if (numberConversion % 1000 != 0 || numberConversion == 0) {
             throw new IllegalArgumentException(ErrorType.INVALID_PRICE_INPUT.getText());
         }
     }
@@ -17,13 +22,14 @@ public class Validation {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(ErrorType.INVALID_NUMBER_DIGITS.getText());
         }
+        isDuplicated(numbers);
     }
 
     public static void prizeNumbersValid(List<Integer> prizeNumbers){
         if (prizeNumbers.size() != 6) {
             throw new IllegalArgumentException(ErrorType.INVALID_NUMBER_DIGITS.getText());
         }
-
+        isDuplicated(prizeNumbers);
         for (int splitNum : prizeNumbers) {
             if (splitNum < NumberBoundaryType.MINIMUM_INDEX.getBoundary()
                     || splitNum > NumberBoundaryType.MAXIMUM_INDEX.getBoundary()) {
@@ -32,15 +38,20 @@ public class Validation {
         }
     }
 
+    private static void isDuplicated(List<Integer> prizeNumbers) {
+        if(prizeNumbers.size() != prizeNumbers.stream().distinct().count()){
+            throw new IllegalArgumentException(ErrorType.INVALID_DUPLICATED_NUMBER.getText());
+        }
+    }
+
     public static void bonusNumberValid(String bonus) {
+        if (!bonus.matches("-?\\d+(\\.\\d+)?")){
+            throw new IllegalArgumentException(ErrorType.INVALID_INPUT_TYPE.getText());
+        }
+
         if (Integer.parseInt(bonus) < NumberBoundaryType.MINIMUM_INDEX.getBoundary()
                 || Integer.parseInt(bonus) > NumberBoundaryType.MAXIMUM_INDEX.getBoundary()) {
             throw new IllegalArgumentException(ErrorType.INVALID_NUMBER_RANGE.getText());
         }
-
-        if (bonus.equals("[^a-zA-Z]")){
-            throw new IllegalArgumentException(ErrorType.INVALID_INPUT_TYPE.getText());
-        }
-
     }
 }
