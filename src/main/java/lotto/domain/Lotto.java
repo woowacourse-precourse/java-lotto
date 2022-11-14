a4d5668bc0d64d24c8398210dbc2f051731d1e89
TaeHwan <kimta2hwan@gmail.com>
2022-11-14 15:33:01 +0900

    refactor: Lotto 객체 VO객체로 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -17,4 +18,23 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+    public List<Integer> getLotto() {
+        return numbers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Lotto lotto = (Lotto) o;
+
+        return Objects.equals(lotto, lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
 }
