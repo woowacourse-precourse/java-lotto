393ce10dafb3d19ef2c7c6866ad7ee939c4491f0
iamsseaa <homingsea@gmail.com>
2022-11-14 18:48:00 +0900

    fix: Map 값 수정

--- src/main/java/lotto/LottoStatistics.java
+++ src/main/java/lotto/LottoStatistics.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,17 +8,17 @@ import java.util.Map;
 public class LottoStatistics {
 
     private int winningMoney = 0;
-    private Map<String, Integer> countMatchingMoney = new HashMap<>();
+    private Map<String, List<Integer>> countMatchingMoney = new HashMap<>();
 
     LottoStatistics() {
-        countMatchingMoney.put("0", 0);
-        countMatchingMoney.put("1", 0);
-        countMatchingMoney.put("2", 0);
-        countMatchingMoney.put("3", 5000);
-        countMatchingMoney.put("4", 50000);
-        countMatchingMoney.put("5", 1500000);
-        countMatchingMoney.put("bonus", 30000000);
-        countMatchingMoney.put("6", 2000000000);
+        countMatchingMoney.put("0", List.of(0, 0));
+        countMatchingMoney.put("1", List.of(0, 0));
+        countMatchingMoney.put("2", List.of(0, 0));
+        countMatchingMoney.put("3", List.of(5000, 0));
+        countMatchingMoney.put("4", List.of(50000, 0));
+        countMatchingMoney.put("5", List.of(1500000, 0));
+        countMatchingMoney.put("bonus", List.of(30000000, 0));
+        countMatchingMoney.put("6", List.of(2000000000, 0));
     }
 
     public void countWinningMoney(List<Lotto> lottos, Lotto winningLotto, int bonus) {
@@ -26,7 +27,8 @@ public class LottoStatistics {
             if (key.equals("5")) {
                 key = checkBonusNumber(lotto, bonus, key);
             }
-            winningMoney += countMatchingMoney.get(key);
+            winningMoney += countMatchingMoney.get(key).get(0);
+            countMatchingMoney.get(key).set(1, countMatchingMoney.get(key).get(1) + 1);
         }
     }
 
