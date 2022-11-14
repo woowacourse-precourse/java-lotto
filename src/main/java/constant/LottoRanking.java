ac6c58c6627913c11f2d9da921d06de35d15ad72
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-14 18:34:38 +0900

    feat(LottoRanking) : eNum 값들 추가.
    같은 숫자 수, 상금 상수 추가.

--- src/main/java/constant/LottoRanking.java
+++ src/main/java/constant/LottoRanking.java
@@ -1,12 +1,22 @@
 package constant;
 
 public enum LottoRanking {
-    FIRST(1), SECOND(2), THIRD(3), FOURTH(4), FIFTH(5);
-    int value;
-    LottoRanking(int rankingValue){
-        this.value = rankingValue;
+    FIRST(1,6,2*(int)Math.pow(10,10)),SECOND(2,5,30000000),
+    THIRD(3,5,1_500_000), FOURTH(4,4,50_000),
+    FIFTH(5,3,5_000), NOTHING(-1,2,0);
+    int value,sameNumberCount,price;
+    LottoRanking(int ranking, int sameNumberCount, int price){
+        this.value = ranking;
+        this.sameNumberCount = sameNumberCount;
+        this.price = price;
     }
     public int getRanking(){
         return value;
+    }
+    public int getSameNumberCount(){
+        return sameNumberCount;
+    }
+    public int getPrice(){
+        return price;
     }
 }
