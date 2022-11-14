d6eb7acc9d1af4c0a3f921d46cb6b9fe28c3d000
kim0914 <kosa0914@naver.com>
2022-11-15 00:26:39 +0900

    test(LottoMachineTest): order 함수 인자 quantity -> money 변경

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,40 +10,43 @@ import static org.assertj.core.api.Assertions.*;
 
 class LottoMachineTest {
 
+    @DisplayName("입력된_금액_만큼의_로또를_생성한다1")
     @Test
-    void 입력된_수량_만큼의_로또를_생성한다1() {
+    void createLottoByInputMoney() {
         // given
         long money = 10_000;
         long quantity = money / LOTTO_PRICE_UNIT;
 
         // when
-        List<Lotto> lottos = LottoMachine.order(quantity);
+        List<Lotto> lottos = LottoMachine.order(money);
 
         // then
         assertThat(lottos.size()).isEqualTo(quantity);
     }
 
+    @DisplayName("입력된_금액_만큼의_로또를_생성한다2")
     @Test
-    void 입력된_수량_만큼의_로또를_생성한다2() {
+    void createLottoByLongTypeRangeInputMoney() {
         // given
         long money = 2_500_000_000L;
         long quantity = money / LOTTO_PRICE_UNIT;
 
         // when
-        List<Lotto> lottos = LottoMachine.order(quantity);
+        List<Lotto> lottos = LottoMachine.order(money);
 
         // then
         assertThat(lottos.size()).isEqualTo(quantity);
     }
 
+    @DisplayName("로또를 생성했을때 오름차순 정렬된 로또를 반환한다")
     @Test
-    void 로또를_생성했을때_오름차순_정렬된_로또를_반환한다() {
+    void createAscendingOrderedLotto() {
         // given
         long money = 10_000;
         long quantity = money / LOTTO_PRICE_UNIT;
 
         // when
-        List<Lotto> lottos = LottoMachine.order(quantity);
+        List<Lotto> lottos = LottoMachine.order(money);
         long sortedLottoQuantity = lottos.stream()
                 .filter(lotto -> isAscendingOrder(lotto.getNumbers()))
                 .count();
