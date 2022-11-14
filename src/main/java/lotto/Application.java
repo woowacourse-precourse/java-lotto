a946d0abe000743df8b4053da439ffceedd91afc
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-14 22:23:08 +0900

    feat(main): main 메서드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import User.User;
+import YieldMachine.YieldMachine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        User user = new User();
+        user.buyLottos();
+        user.printLottos();
+        YieldMachine yieldMachine = new YieldMachine();
+        yieldMachine.run(user);
     }
 }
