f6bab55cf1b8b669bd7bf1414806b76651824f1a
hdsung98 <hdsung98@gmail.com>
2022-11-14 15:50:01 +0900

    feat: create utility methods in Class inputUtil

--- src/main/java/lotto/InputUtil.java
+++ src/main/java/lotto/InputUtil.java
@@ -1,5 +1,29 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+
 public class InputUtil {
 
+    public static final String ERROR_INVALID_INPUT = "[ERROR] Invalid Input";
+
+    public Integer inputNumber() {
+        Integer outcome;
+
+        String number = Console.readLine();
+        checkValidity(number);
+        outcome = Integer.parseInt(number);
+        return outcome;
+    }
+    public ArrayList<Integer> inputWinningNumbers() {
+        String numbers = Console.readLine();
+        ArrayList<Integer> checkedNumbers = checkNumbersValidity(numbers);
+        return checkedNumbers;
+    }
+    private void checkValidity(String target) throws IllegalArgumentException{
+
+    }
+    private ArrayList<Integer> checkNumbersValidity(String target) throws IllegalArgumentException{
+
+    }
 }
--- src/main/java/lotto/OutputUtil.java
+++ src/main/java/lotto/OutputUtil.java
@@ -1,25 +1,28 @@
 package lotto;
 
 public class OutputUtil {
-    public static final String PURCHASE_INPUT  = "구입금액을 입력해 주세요.";
+    public static final String PURCHASE_INPUT = "구입금액을 입력해 주세요.";
     public static final String PURCHASE_RESULT = "개를 구매했습니다.";
     public static final String WINNING_NUMBER_NOTICE = "당첨 번호를 입력해 주세요.";
     public static final String BONUS_NOTICE = "보너스 번호를 입력해 주세요.";
 
-    public void purchaseNotice(){
+    public void purchaseNotice() {
         System.out.println(PURCHASE_INPUT);
     }
-    public void purchaseResult(Purchase purchase){
+
+    public void purchaseResult(Purchase purchase) {
         // PURCHASE_RESULT+=purchase.lottoSize;
         System.out.println(PURCHASE_RESULT);
-        for(;;){
-          //System.out.println(lotto);
+        for (; ; ) {
+            //System.out.println(lotto);
         }
     }
-    public void winningNumberNotice(){
+
+    public void winningNumberNotice() {
         System.out.println(WINNING_NUMBER_NOTICE);
     }
-    public void bonusNotice(){
+
+    public void bonusNotice() {
         System.out.println(BONUS_NOTICE);
     }
 }
