27ca09c5156e0a78d2726e4833db459b27ce8354
ohhonggi <5ghdrl@naver.com>
2022-11-15 05:04:37 +0900

    feat($Lotto): Override toString function
    This function outputs the Lotto numbers

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -16,5 +16,18 @@ public class Lotto {
         }
     }
 
+
     // TODO: 추가 기능 구현
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("[");
+
+        for (int i = 0; i < numbers.size()-1; i++) {
+            builder.append(numbers.get(i) + ", ");
+        }
+        builder.append(numbers.get(numbers.size() - 1) + "]");
+
+        return builder.toString();
+    }
+
 }
