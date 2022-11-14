06339d44aed1579409c9f9982bdf1763d5b2cb7e
SeongukBaek <bsu6235@gmail.com>
2022-11-15 03:14:44 +0900

    feat: implement class Calculator
    Added new fields and method:
    - Map<Result, Integer> resultCounts
    - int earn
    - stringToInt(String money)

--- src/main/java/lotto/util/Calculator.java
+++ src/main/java/lotto/util/Calculator.java
@@ -6,30 +6,37 @@ import lotto.domain.Result;
 import java.util.*;
 
 public class Calculator {
+    private static Map<Result, Integer> resultCounts;
+    private static int earn;
+
     public static Map<Result, Integer> calculateResult(List<Lotto> lottos, Set<Integer> winningNumbers, int bonusNumber) {
-        Map<Result, Integer> resultCounts = initializeMap();
+        initializeMap();
+        earn = 0;
 
         for (Lotto lotto : lottos) {
             Result result = lotto.computeResult(winningNumbers, bonusNumber);
             resultCounts.put(result, resultCounts.get(result) + 1);
+            earn += stringToInt(result.getMoney());
         }
 
         return resultCounts;
     }
 
-    private static Map<Result, Integer> initializeMap() {
-        Map<Result, Integer> resultCounts = new EnumMap<>(Result.class);
+    private static void initializeMap() {
+        resultCounts = new EnumMap<>(Result.class);
 
         resultCounts.put(Result.THREE, 0);
         resultCounts.put(Result.FOUR, 0);
         resultCounts.put(Result.FIVE, 0);
         resultCounts.put(Result.FIVEWITHBONUS, 0);
         resultCounts.put(Result.SIX, 0);
-
-        return resultCounts;
     }
 
-    public static double calculateYield(int money, int earn) {
+    private static int stringToInt(String money) {
+        return Integer.parseInt(money.replace(",", ""));
+    }
+
+    public static double calculateYield(int money) {
         if (money == 0 || earn == 0) {
             return 0;
         }
