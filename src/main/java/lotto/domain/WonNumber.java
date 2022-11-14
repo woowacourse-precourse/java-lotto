1609ed08a68307f9d1d3adf0ad80d9558e1abc47
josolha <josolha@nate.com>
2022-11-15 01:53:35 +0900

    feat(WonNumber) : 당첨금액 입력 클래스와 예외기능 추가

--- src/main/java/lotto/domain/WonNumber.java
+++ src/main/java/lotto/domain/WonNumber.java
@@ -4,36 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WonNumber {
-    public List<Integer> wonNumber =new ArrayList<>();
-    public InPutSystem inPutSystem =new InPutSystem();
+    public List<Integer> wonNumber = new ArrayList<>();
+
+    public int bonusNumber;
+    public InPutSystem inPutSystem = new InPutSystem();
 
     public WonNumber() {
         sixNumber();
         validate(wonNumber);
     }
-    public void sixNumber(){
-        for (String numbers : inPutSystem.inputNumber().split(",")){
+
+    public void sixNumber() {
+        for (String numbers : inPutSystem.inputNumber().split(",")) {
             wonNumber.add(Integer.parseInt(numbers));
         }
     }
-    public void bonusNumber(){
-        int addLastNumber  = Integer.parseInt(inPutSystem.inputNumber());
-        wonNumber.add(addLastNumber);
-    }
-    public void validate(List<Integer> wonNumber){
 
-        if(wonNumber.size() != 6 ){
-            throw new IllegalArgumentException();
+    public void bonusNumber() {
+        int bonusNumber = Integer.parseInt(inPutSystem.inputNumber());
+    }
+
+    public void validate(List<Integer> wonNumber) {
+
+        if (wonNumber.size() != 6) {
+            throw new IllegalArgumentException(PrintMessages.ERROR_SIZE.getMessage());
         }
-        if(wonNumber.size() != wonNumber.stream().distinct().count()){
-            throw new IllegalArgumentException();
-        }
-        for (int wonNumbers : wonNumber) {
-            if (Character.isDigit(wonNumbers) || wonNumbers == 0) {
-                throw new IllegalArgumentException();
-            }
+        if (wonNumber.size() != wonNumber.stream().distinct().count()) {
+            throw new IllegalArgumentException(PrintMessages.ERROR_DUPLICATE.getMessage());
         }
     }
+
     public List<Integer> getWonNumber() {
         return wonNumber;
     }
