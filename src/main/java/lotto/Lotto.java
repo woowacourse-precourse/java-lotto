01f0ae444b137b2a4f87c344896e9efdb50683b1
donghae <donghae1999@gmail.com>
2022-11-14 18:33:46 +0900

    fix: lotto 값 최소치 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,7 +15,7 @@ public class Lotto {
         String[] inputSplitAnswer = inputAnswer.split(",");
         for (String inputNumber : inputSplitAnswer) {
             int number = Integer.parseInt(inputNumber);
-            if (number >= 0 && number <= 45) {
+            if (number > 0 && number <= 45) {
                 lottoAnswer.add(number);
                 continue;
             }
@@ -24,13 +24,13 @@ public class Lotto {
         return lottoAnswer;
     }
 
-    private void validateCount(List<Integer> numbers) throws IllegalArgumentException {
+    public void validateCount(List<Integer> numbers) throws IllegalArgumentException {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호 6개를 입력해주세요.");
         }
     }
 
-    private void validateDup(List<Integer> numbers) throws IllegalArgumentException {
+    public void validateDup(List<Integer> numbers) throws IllegalArgumentException {
         for (int number : numbers) {
             if (numbers.contains(number) && numbers.indexOf(number) != numbers.lastIndexOf(number)) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않는 숫자입니다.");
@@ -45,7 +45,7 @@ public class Lotto {
         numbers.add(Integer.parseInt(bonusNumber));
     }
 
-    private void validateBonusNumberInput(String bonusNumber) throws IllegalArgumentException {
+    public void validateBonusNumberInput(String bonusNumber) throws IllegalArgumentException {
         for (int i = 0; i < bonusNumber.length(); i++) {
             if (bonusNumber.charAt(i) >= 48 && bonusNumber.charAt(i) <= 57) {
                 continue;
@@ -54,15 +54,15 @@ public class Lotto {
         }
     }
 
-    private void validateBonusNumberRange(String bonusNumber) throws IllegalArgumentException {
+    public void validateBonusNumberRange(String bonusNumber) throws IllegalArgumentException {
         int bonusNum = Integer.parseInt(bonusNumber);
-        if (bonusNum>=0 && bonusNum<=45) {
+        if (bonusNum>0 && bonusNum<=45) {
             return;
         }
         throw new IllegalArgumentException("[ERROR] 보너스 숫자는 1부터 45사이의 숫자입니다.");
     }
 
-    private void validateBonusNumberDup(String bonusNumber) throws IllegalArgumentException {
+    public void validateBonusNumberDup(String bonusNumber) throws IllegalArgumentException {
         if (numbers.contains(Integer.parseInt(bonusNumber))) {
             throw new IllegalArgumentException("[ERROR] 보너스 숫자는 로또번호와 중복되지 않아야합니다.");
         }
--- src/main/java/lotto/Purchase.java
+++ src/main/java/lotto/Purchase.java
@@ -6,16 +6,20 @@ public class Purchase {
 
     public Purchase(String price) throws IllegalArgumentException {
         validatePurchase(price);
+        validatePurchaseUnit(price);
         this.price = Integer.parseInt(price);
     }
 
-    private void validatePurchase(String price) throws IllegalArgumentException {
+    public void validatePurchase(String price) throws IllegalArgumentException {
         for (int i = 0; i < price.length(); i++) {
             if (price.charAt(i) >= 48 && price.charAt(i) <= 57) {
                 continue;
             }
             throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자로만 입력하여야합니다.");
         }
+    }
+
+    public void validatePurchaseUnit(String price) throws IllegalArgumentException{
         if (Integer.parseInt(price) % 1000 == 0) {
             return;
         }
