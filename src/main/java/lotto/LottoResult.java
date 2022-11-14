57820a2e1dfdfe0ebeef2bdebdd4e05cdf295591
ensk26 <tjensk26@gmail.com>
2022-11-15 01:21:11 +0900

    refactor(LottoResult): enum 값 순서 변경, moeny 타입 변경, index 반환 추가
    기존 메서드 수정 to LottoResult
    - 출력값 순서에 맞춰 재 배열
    - money의 설명은 detail에 합치고, money는 실제 로또 당첨 금액을 적어 놓음
    - money로 로또 총 금액을 구하기 위해
    - index 값을 이용해 해당 로또가 몇개 있는지 로또 결과 리스트에서 찾아오기 위해

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,19 +1,19 @@
 package lotto;
 
 public enum LottoResult {
-    FIRSTPLACE(6, false, 0, "6개 번호 일치", "2,000,000,000"),
-    SECONDPLACE(5, true, 1, "5개 번호 + 보너스 번호 일치", "30,000,000"),
-    THIRDPLACE(5, false, 2, "5개 번호 일치", "1,500,000"),
-    FOURTHPLACE(4, false, 3, "4개 번호 일치", "50,000"),
-    FIFTHPLACE(3, false, 4, "3개 번호 일치", "5,000");
+    FIFTHPLACE(3, false, 4, "3개 번호  (5,000원) - ", 5000),
+    FOURTHPLACE(4, false, 3, "4개 번호 일치 (50,000원) - ", 50000),
+    THIRDPLACE(5, false, 2, "5개 번호 일치 (1,500,000원) - ", 1500000),
+    SECONDPLACE(5, true, 1, "5개 번호 + 보너스 번호 일치 (30,000,000원) - ", 30000000),
+    FIRSTPLACE(6, false, 0, "6개 번호 일치 (2,000,000,000원) - ", 2000000000);
 
     private final int count;
     private final boolean bonus;
     private final int index;
     private final String detail;
-    private final String money;
+    private final int money;
 
-    LottoResult(int count, boolean bonus, int index, String detail, String money) {
+    LottoResult(int count, boolean bonus, int index, String detail, int money) {
         this.count = count;
         this.bonus = bonus;
         this.index = index;
@@ -25,7 +25,13 @@ public enum LottoResult {
         return detail;
     }
 
-    public String getMoney() {
+    public int getMoney() {
         return money;
     }
+
+    public int getIndex() {
+        return index;
+    }
+
+    //count랑 같으면 index 반환
 }
