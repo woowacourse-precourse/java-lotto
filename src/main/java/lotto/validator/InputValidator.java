bccab27bdad82246eb9786d52c2d9581efbf94f0
java-saeng <gusdn85554@naver.com>
2022-11-14 16:11:07 +0900

    fix : 숫자가 아닌 다른 것을 입력할 경우 IllegalArgumentException을 NoSuchElementException으로 핸들링

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,18 +1,28 @@
 package lotto.validator;
 
+import java.util.NoSuchElementException;
+
 import static lotto.global.ExceptionConstants.CANNOT_INPUT_ZERO;
 import static lotto.global.ExceptionConstants.CAN_ONLY_MULTIPLY_1000;
 
 public class InputValidator extends CommonValidator {
 
     public static void validatePurchaseInput(String number, long divisor) {
-        isAllNumber(number);
+        try {
+            isAllNumber(number);
+        } catch (IllegalArgumentException ignore) {
+            throw new NoSuchElementException();
+        }
         isZero(number);
         restEqualZero(number, divisor);
     }
 
     public static void validateBounsInput(String number) {
-        isAllNumber(number);
+        try {
+            isAllNumber(number);
+        } catch (IllegalArgumentException ignore) {
+            throw new NoSuchElementException();
+        }
         isRangeTrue(Integer.parseInt(number));
     }
 
