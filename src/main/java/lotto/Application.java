5a280ce322b148c5e0a3ac9a8d1081ea247b6a02
hyungkya <hyungkya@uci.edu>
2022-11-14 16:11:10 +0900

    feat: 희망 구매가격 입력받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private final static String inputPriceMessage = "구입금액을 입력해 주세요.";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static String readPrice(String outputMessage) {
+        System.out.println(outputMessage);
+        return Console.readLine();
     }
 }
