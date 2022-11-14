8312989abe4a7972f8e3a1a02c02d1e4e0fe9ec7
yujaemin <woawoa0513@gmail.com>
2022-11-14 20:43:08 +0900

    feat: 당첨 번호 입력 안내 출력문
    로또 당첨 번호를 입력하도록 안내하는 출력문

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public static void printLottoPurchaseInputNotice() {
         System.out.println("구입금액을 입력해 주세요.");
     }
+
+    public static void printWinningNumberInputNotice() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
 }
