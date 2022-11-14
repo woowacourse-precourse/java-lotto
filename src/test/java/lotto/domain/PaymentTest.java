2b88a9e6bdf6f6fc76e23ef79f08dc5e874febbe
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-14 15:20:15 +0900

    feat: 기능 목록에서 입금 금액에 있는 3가지 검증 기능 추가

--- src/test/java/lotto/domain/PaymentTest.java
+++ src/test/java/lotto/domain/PaymentTest.java
@@ -5,11 +5,40 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.constant.ErrorMessage;
+import lotto.validator.UserInputValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class PaymentTest {
+
+    @Nested
+    @DisplayName("사용자가 입력한 문자열이")
+    class If_user_input_is {
+
+        @Test
+        @DisplayName("숫자가 아닐 경우 예외를 던진다")
+        void not_numeric() {
+            // given
+            String input = "로또게임";
+
+            // when, then
+            assertThatThrownBy(() -> UserInputValidator.validateStringIsNumeric(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.MONEY_IS_ALLOWED_ONLY_NUMERIC.getMessage());
+        }
+
+        @Test
+        @DisplayName("숫자일 경우 예외를 던지지 않는다")
+        void numeric() {
+            // given
+            String input = String.valueOf(UNIT_OF_LOTTO_PURCHASE);
+
+            // when, then
+            assertThatCode(() -> UserInputValidator.validateStringIsNumeric(input))
+                    .doesNotThrowAnyException();
+        }
+    }
 
     @Test
     @DisplayName("0원을 입력했을 때 예외를 테스트")
