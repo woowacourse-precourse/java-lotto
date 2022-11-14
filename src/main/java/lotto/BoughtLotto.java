7d3cd07076b76f0f13aec9f9156810179f4be671
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-14 22:43:47 +0900

    fix: printLottoNumber 수정
    - List.of로 전달받은 값 정렬 후 출력

--- src/main/java/lotto/BoughtLotto.java
+++ src/main/java/lotto/BoughtLotto.java
@@ -5,6 +5,7 @@ import lotto.randomNumber.RandomNumber;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BoughtLotto extends Lotto{
     public BoughtLotto() {
@@ -17,7 +18,9 @@ public class BoughtLotto extends Lotto{
 
     public void printLottoNumber() {
         List<Integer> numbers = super.getLotto();
-//        numbers.sort(Comparator.naturalOrder());
+        numbers = numbers.stream()
+                .collect(Collectors.toList());
+        numbers.sort(Comparator.naturalOrder());
         System.out.println(numbers);
     }
 }
