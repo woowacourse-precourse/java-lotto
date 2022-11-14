adde91e595aeb2362a622212600c10408a38bcf5
jonghun <jonghuncu@gmail.com>
2022-11-14 13:42:04 +0900

    feat(lotto) : 1-45 범위의 숫자 중 당첨번호와 중복되지 않는 보너스 번호를 입력 기능
    BonusNumberGenerator#createBonusNumber()

--- src/main/java/lotto/BonusNumberGenerator.java
+++ src/main/java/lotto/BonusNumberGenerator.java
@@ -8,7 +8,7 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 public class BonusNumberGenerator {
     public static int createBonusNumber(List<Integer> CorrectNumber) {
         while (true) {
-            System.out.println("보너스 번호를 입력해 주세요");
+            System.out.println("보너스 번호를 입력해 주세요.");
             String InputBonusNumber = readLine();
             int bonusNumber = Integer.parseInt(InputBonusNumber);
             if (!CorrectNumber.contains(bonusNumber)) {
