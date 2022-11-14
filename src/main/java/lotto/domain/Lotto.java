dad2ece0e5fdb8c96083e958ee07f2a6bf4964aa
footdev <ekclstkfka5@gmail.com>
2022-11-14 23:52:09 +0900

    feat: Lotto 클래스 구현
    유효한 로또 번호인지 검증하고 상태를 저장하는 Lotto 클래스 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,19 +1,45 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
+    public static final String COUNT_ERROR_MESSAGE = "로또 번호의 개수는 6개여야 합니다.";
+    public static final String DUPLICATION_ERROR_MESSAGE = "로또 번호는 중복되지 않아야 합니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicatedNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(Validation.ERROR_MESSAGE + " " + COUNT_ERROR_MESSAGE);
         }
     }
-    // TODO: 추가 기능 구현
+
+    private void validateDuplicatedNumber(List<Integer> numbers) {
+        long count = countDuplicatedNumber(numbers);
+        if(count != numbers.size()) {
+            throw new IllegalArgumentException(Validation.ERROR_MESSAGE + " " + DUPLICATION_ERROR_MESSAGE);
+        }
+    }
+
+    private long countDuplicatedNumber(List<Integer> numbers) {
+        return numbers
+                .stream()
+                .distinct()
+                .count();
+    }
+
+    @Override
+    public String toString() {
+        return this.numbers
+                .stream()
+                .sorted()
+                .collect(Collectors.toList())
+                .toString();
+    }
 }
