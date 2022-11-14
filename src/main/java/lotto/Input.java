cb686896495f060da2af72e5a34f5971ef0930e0
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-14 16:24:24 +0900

    feat(main/Input): 예외 사항 판단 기능 추가, 메소드 분리
    - stringToInt(): String으로 들어온 input이 int로 변환될 수 있는지 확인한 후 int로 변환하여 리턴
    - toIntegerList(): String[]으로 들어온 값을 int 리스트로 변환한다.
    - getInput(): 콘솔에 입력된 값을 int로 바꾸어 리턴한다.
    - validWinning6Numbers(): 입력된 당첨 번호가 쉼표로 구분된 6개의 문자인지 확인한다.
    - validWinningRepetition(): 입력된 당첨 번호가 서로 중복되지 않는지 확인한다.
    
    해결: 4. 예외 사항 - c), d), e), 당첨 번호에서 예외 사항들

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -3,17 +3,30 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Input {
-    private static int getInputToInteger() {
-        String input = Console.readLine();
+    private static int stringToInt(String input) {
         validNumber(input);
         return Integer.parseInt(input);
     }
 
+    private static List<Integer> toIntegerList(String[] split) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String str : split) {
+            numbers.add(stringToInt(str));
+        }
+        return numbers;
+    }
+
+    private static int getInput() {
+        String input = Console.readLine();
+        return stringToInt(input);
+    }
+
     public static int getPurchaseAmount() {
-        int input = getInputToInteger();
+        int input = getInput();
         validPurchase(input);
         return input;
     }
@@ -21,31 +34,47 @@ public class Input {
     public static List<Integer> getWinningNumbers() {
         String input = Console.readLine();
         String[] split = input.split(",");
-        List<Integer> numbers = new ArrayList<>();
-        for (String str : split) {
-            numbers.add(Integer.parseInt(str));
-        }
-        return numbers;
+        validWinning6Numbers(split);
+        validWinningRepetition(split);
+        return toIntegerList(split);
     }
 
     public static int getBonusNumber() {
-        int input = getInputToInteger();
+        int input = getInput();
         return input;
     }
 
     private static void validNumber(String input) throws IllegalArgumentException {
         try {
             Integer.parseInt(input);
-        } catch (Exception e) {
-            System.out.println("[ERROR] 잘못된 입력입니다.");
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 입력 값이 숫자가 아닙니다.");
+            throw new IllegalArgumentException();
+        } catch (Exception e){
+            System.out.println("[ERROR] 입력 값이 잘못되었습니다.");
             throw new IllegalArgumentException();
         }
     }
 
     private static void validPurchase(int input) throws IllegalArgumentException {
         if (input % 1000 != 0) {
+            System.out.println("[ERROR] 1000원으로 나누어지는 수가 아닙니다.");
             throw new IllegalArgumentException();
         }
     }
 
+    private static void validWinning6Numbers(String[] split) throws IllegalArgumentException {
+        if (split.length != 11) {
+            System.out.println("[ERROR] 쉼표로 구분된 6개의 문자가 아닙니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validWinningRepetition(String[] split) throws IllegalArgumentException{
+        HashSet<String> removeDuplication = new HashSet<>(List.of(split));
+        if (removeDuplication.size()!=6){
+            System.out.println("[ERROR] 중복되는 당첨 번호가 있습니다.");
+            throw new IllegalArgumentException();
+        }
+    }
 }
