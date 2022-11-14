5a90a82261b19edb19523d72921fcec4f41f47b9
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-14 18:59:59 +0900

    refactor(Quantity): Quantity 유효성 검사 기능 중복 제거

--- src/main/java/lotto/Quantity.java
+++ src/main/java/lotto/Quantity.java
@@ -1,22 +1,18 @@
 package lotto;
 
+import static lotto.util.Validator.validatePositiveNumber;
+import static lotto.util.Validator.validateUnitStandard;
+
 public class Quantity {
 
-    // TODO: 2022-11-10 Quantity가 존재해야하는지?, != 0을 어떻게 정의할지 고민하기
     private static final int UNIT_STANDARD = 1_000;
-    private static final String UNIT_STANDARD_ERROR = "[ERROR] 구입금액은 1,000원 단위입니다.";
 
     private final int number;
 
     public Quantity(int purchaseAmount) {
-        validate(purchaseAmount);
+        validatePositiveNumber(purchaseAmount);
+        validateUnitStandard(purchaseAmount);
         this.number = calculateQuantity(purchaseAmount);
-    }
-
-    public void validate(int purchaseAmount) {
-        if (purchaseAmount % UNIT_STANDARD != 0) {
-            throw new IllegalArgumentException(UNIT_STANDARD_ERROR);
-        }
     }
 
     public int calculateQuantity(int purchaseAmount) {
