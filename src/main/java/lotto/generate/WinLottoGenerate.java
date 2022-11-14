0b08b74b2a07a70da21c337dad7fb08817d7d2a1
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-14 22:48:23 +0900

    feat: WinLottoGenreate 클래스 생성, askWinNumberPrint 메소드 작성

---
+++ src/main/java/lotto/generate/WinLottoGenerate.java
@@ -0,0 +1,11 @@
+package lotto.generate;
+
+import lotto.enums.StringEnum;
+
+import static lotto.enums.StringEnum.*;
+
+public class WinLottoGenerate {
+    public void askWinNumberPrint() {
+        System.out.println(WIN_NUMBER.getMessage());
+    }
+}
