a606bf76ac23503a252f907a42f21ffbc2c3df0b
abbd2 <ajkety00@gmail.com>
2022-11-14 18:34:11 +0900

    Feat(WinLotto): 로또 번호에 보너스 번호가 포함인지를 구분하는 isBonusNumber 변수의 getter&setter 메서드 구현, winLottoCount를 int 형으로 변경

--- src/main/java/lotto/WinLotto.java
+++ src/main/java/lotto/WinLotto.java
@@ -4,23 +4,32 @@ import java.util.List;
 
 public class WinLotto {
 
-    private List<Integer> winLottoCount;
-    private int bonusNumber = 0;
+    private int winLottoCount = 0;
+    private static int bonusNumber = 0;
+    private boolean isBonusNumber;
 
-    public WinLotto(List<Integer> winLottoCount) {
-        this.winLottoCount = winLottoCount;
+    public int getWinLottoCount() {
+        return this.winLottoCount;
     }
 
-    public List<Integer> getWinLottoNumber() {
-        return this.winLottoCount;
+    public void setWinLottoCount(int winLottoCount) {
+        this.winLottoCount = winLottoCount;
     }
 
     public int getBonusNumber() {
         return this.bonusNumber;
     }
 
-    public void setBonusNumber(int bonusNumber){
+    public void setBonusNumber(int bonusNumber) {
         this.bonusNumber = bonusNumber;
+    }
+
+    public boolean getIsBonusNumber() {
+        return isBonusNumber;
+    }
+
+    public void setIsBonusNumber(boolean isBonusNumber) {
+        this.isBonusNumber = isBonusNumber;
     }
 
 }
