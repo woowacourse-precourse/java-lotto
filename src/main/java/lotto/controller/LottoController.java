18b2324bdd74292fd03cfb5e323562f3a1fe4776
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-14 17:16:54 +0900

    feat(LottoController): 전체 로직 구현
    - 로또를 발행하는 로직
    - 당첨과 보너스 번호를 입력받는 로직
    - 사용자 당첨 번호와 로또를 비교해 당첨 개수를 구하는 로직
    - 당첨 통계를 내서 출력하는 로직

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,64 @@
+package lotto.controller;
+
+import lotto.domain.Lotties;
+import lotto.domain.Lotto;
+import lotto.domain.Statistics;
+import lotto.domain.UserLotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+
+public class LottoController {
+
+    private final Statistics statistics;
+
+    private Lotties lotties;
+
+    private UserLotto userLotto;
+
+    public LottoController(Statistics statistics) {
+        this.statistics = statistics;
+    }
+
+    public void run() {
+        try {
+            int purchase = InputView.inputLottoPurchaseAmount();
+            lotties = createLottiesByPurchaseAmount(purchase);
+
+            userLotto = inputWinAndBonusNumber();
+            findMatchAndBonusCount();
+
+            statistics.findTotalWinAmount();
+            printResults(purchase);
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public Lotties createLottiesByPurchaseAmount(int purchase) {
+        Lotties lotties = Lotties.create(purchase);
+        OutputView.printLottoList(lotties);
+        return lotties;
+    }
+
+    public UserLotto inputWinAndBonusNumber() {
+        List<Integer> winNumber = InputView.inputUserWinNumber();
+        int bonusNumber = InputView.inputUserBonusNumber();
+        return new UserLotto(winNumber, bonusNumber);
+    }
+
+    public void findMatchAndBonusCount() {
+        for (Lotto lotto : lotties.getLotties()) {
+            int matchCount = lotto.findLottoWinCount(userLotto.getLotto());
+            int bonusCount = userLotto.findBonusNumberWinCount(lotto.getNumbers());
+            statistics.setLottoWinResultToMap(matchCount, bonusCount);
+        }
+    }
+
+    public void printResults(int purchaseAmount) {
+        OutputView.printWinResult(statistics.getWinLottoInfoMap());
+        OutputView.printYieldResult(statistics.findLottoYield(purchaseAmount));
+    }
+}
