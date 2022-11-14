9c580998070ad6cfe0f0518004b7b0755f920b2b
YUSEONJUN <seonjun0906@gmail.com>
2022-11-15 00:30:12 +0900

    test : 등수 정의에 따른 상금 테스트

--- src/test/java/lotto/RankTest.java
+++ src/test/java/lotto/RankTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 public class RankTest {
 
+
 	@Test
 	public void enum_CountTest() {
 		assertThat(Rank.FIRST.getCountOfMatch()).isEqualTo(6);
@@ -14,5 +15,15 @@ public class RankTest {
 		assertThat(Rank.FOURTH.getCountOfMatch()).isEqualTo(4);
 		assertThat(Rank.FIFTH.getCountOfMatch()).isEqualTo(3);
 		assertThat(Rank.MISS.getCountOfMatch()).isEqualTo(0);
+	}
+
+	@Test
+	public void enum_MoneyTest() {
+		assertThat(Rank.FIRST.getWinningMoney()).isEqualTo(2000000000);
+		assertThat(Rank.SECOND.getWinningMoney()).isEqualTo(30000000);
+		assertThat(Rank.THIRD.getWinningMoney()).isEqualTo(1500000);
+		assertThat(Rank.FOURTH.getWinningMoney()).isEqualTo(50000);
+		assertThat(Rank.FIFTH.getWinningMoney()).isEqualTo(5000);
+		assertThat(Rank.MISS.getWinningMoney()).isEqualTo(0);
 	}
 }
