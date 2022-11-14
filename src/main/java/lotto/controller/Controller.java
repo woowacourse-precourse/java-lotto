b1a841b65de143226f0cc2e5827f1941eb7d71ab
sujin0607 <goweg0607@gmail.com>
2022-11-14 22:59:45 +0900

    feat: 랜덤 숫자 생성 함수 생성

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -2,17 +2,29 @@ package lotto.controller;
 
 import lotto.console.InputConsole;
 import lotto.console.OutputConsole;
+import lotto.domain.Lotto;
 import lotto.domain.LottoManager;
+
+import java.util.List;
 
 public class Controller {
     private InputConsole input = new InputConsole();
     private OutputConsole output = new OutputConsole();
     private LottoManager lottoManager = new LottoManager();
 
-    public void findLottoCount(){
+    private Lotto lotto;
+    public int findLottoCount(){
         String inputMoney = input.Money();
         int lottoCount = lottoManager.countLotto(inputMoney);
         output.lottoCount(lottoCount);
+        return lottoCount;
     }
+
+    public void makeRandomNum(int lottoCount){
+        List<List<Integer>> randomNumbers = lottoManager.makeRandomNumbers(lottoCount);
+        output.randomNumbers(randomNumbers);
+    }
+
+
     
 }
