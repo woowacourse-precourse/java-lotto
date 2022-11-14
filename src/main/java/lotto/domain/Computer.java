1b9271567ca5eb27fd22ceedb6aa97f54e628e57
CAEL0 <kjongmin26@gmail.com>
2022-11-14 16:08:54 +0900

    feat(Computer): 당첨 번호 검증

--- src/main/java/lotto/domain/Computer.java
+++ src/main/java/lotto/domain/Computer.java
@@ -1,12 +1,29 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.util.Validator;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static lotto.constant.GameConstants.*;
 
 public class Computer {
+    private List<Integer> winningNumbers;
+
+    private void validate(List<String> winningNumbers) {
+        for (String winningNumber : winningNumbers) {
+            Validator.validateNaturalNumber(winningNumber);
+            Validator.validateLeadingZero(winningNumber);
+        }
+        this.winningNumbers = winningNumbers.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        Validator.validateLength(this.winningNumbers, WINNING_NUMBER_LENGTH);
+        Validator.validateRange(this.winningNumbers, WINNING_NUMBER_LENGTH);
+        Validator.validateDuplication(this.winningNumbers, WINNING_NUMBER_LENGTH);
+    }
 
     public static Lotto issueLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_NUMBER_LENGTH);
