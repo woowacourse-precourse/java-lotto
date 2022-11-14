dcbef224de80d3b03017478ff6d958d28aecee4c
ttaehee <ttaehee1105@gmail.com>
2022-11-14 18:41:58 +0900

    fix : 구입금액 0원인 경우 유효성 체크 수정

--- src/main/java/lotto/domain/Amount.java
+++ src/main/java/lotto/domain/Amount.java
@@ -3,7 +3,8 @@ package lotto.domain;
 import lotto.utils.Validator;
 
 public class Amount {
-    private static final String WRONG_PURCHASE_AMOUNT = "[ERROR] 1000 단위만 입력할 수 있습니다.";
+    private static final String UNDER_ZERO_AMOUNT = "[ERROR] 금액이 너무 적습니다.";
+    private static final String THOUSAND_AMOUNT = "[ERROR] 1000 단위만 입력할 수 있습니다.";
     public static final int LOTTO_AMOUNT = 1000;
     private final int purchaseAmount;
 
@@ -11,12 +12,19 @@ public class Amount {
         Validator.isOneNumber(purchaseAmount);
         int amount = Integer.parseInt(purchaseAmount);
         checkThousand(amount);
+        checkUnderZero(amount);
         this.purchaseAmount = amount;
+    }
+
+    private void checkUnderZero(int inputAmount) {
+        if (inputAmount <= 0) {
+            throw new IllegalArgumentException(UNDER_ZERO_AMOUNT);
+        }
     }
 
     private void checkThousand(int inputAmount) {
         if (inputAmount % 1000 != 0) {
-            throw new IllegalArgumentException(WRONG_PURCHASE_AMOUNT);
+            throw new IllegalArgumentException(THOUSAND_AMOUNT);
         }
     }
 
