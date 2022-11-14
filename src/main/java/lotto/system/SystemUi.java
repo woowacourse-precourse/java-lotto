9ea379b6f3e54990036ea21a1b48bda836c9a47e
jin8073 <jin8073@gmail.com>
2022-11-14 13:51:38 +0900

    refactor(SystemUi): 메소드 static으로 변경 및 반환 값 변경
    SystemUi의 메소드를 static으로 변경하고, 입력 받은 값을 검증하여 정수형으로 반환하도록 변경했습니다.

--- src/main/java/lotto/system/SystemUi.java
+++ src/main/java/lotto/system/SystemUi.java
@@ -6,8 +6,8 @@ public class SystemUi {
 
     private static final String START_MESSAGE = "구입금액을 입력해 주세요.";
 
-    public String getPurchaseAmount() {
+    public static int getPurchaseAmount() {
         System.out.println(START_MESSAGE);
-        return Console.readLine();
+        return new Parser().validatePurchaseAmount(Console.readLine());
     }
 }
