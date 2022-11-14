6a04b8351659a183633b0fbd3ac1c77c40a49b60
MinSungKim <timkim01@naver.com>
2022-11-14 16:21:31 +0900

    feat: 당첨번호 리스트로 변환 기능 구현

--- src/main/java/Utils/UserInput.java
+++ src/main/java/Utils/UserInput.java
@@ -1,19 +1,25 @@
 package Utils;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.Lotto;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserInput {
 
-    // 사용자 입력 받기
-    public String getUserInput() {
-        String userInput = Console.readLine();
+    // 당첨번호 입력, 유효성검사 후 로또 객체 생성
+    public Lotto inputLottoNumber() {
+        String userInputLottoNumbers = Console.readLine();
 
-        return userInput;
+        validateInputLottoNumbers(userInputLottoNumbers);
+        List<Integer> lottoNumbers = inputLottoNumberToList(userInputLottoNumbers);
+        Lotto lotto = new Lotto(lottoNumbers);
+
+        return lotto;
     }
 
     // 사용자 입력이 숫자인지 유효성 검사
@@ -45,25 +51,15 @@ public class UserInput {
         }
     }
 
-    // 입력되는 당첨번호에 중복값이 있는 경우 유효성 검사
-    private void validateDuplicateInputLottoNumbers(String userInput) {
-        int lottoNumbersCount = getLottoNumbersSet(userInput).size();
-
-        if (lottoNumbersCount != 6) {
-            throw new IllegalArgumentException("[ERROR] 숫자가 중복되지 않게 입력해주세요.");
-        }
-    }
-
-    // 당첨번호를 Set 으로 반환
-    private HashSet<String> getLottoNumbersSet(String userInput) {
+    // 입력되는 당첨번호를 리스트로 변환
+    private List<Integer> inputLottoNumberToList(String userInput) {
         StringTokenizer st = new StringTokenizer(userInput, ",");
-        HashSet<String> lottoNumbers = new HashSet<>();
+        List<Integer> lottoNumbers = new ArrayList<>();
 
-        while (st.hasMoreTokens()) {
-            lottoNumbers.add(st.nextToken());
+        while (st.hasMoreTokens()){
+            lottoNumbers.add(Integer.parseInt(st.nextToken()));
         }
 
         return lottoNumbers;
     }
-
 }
