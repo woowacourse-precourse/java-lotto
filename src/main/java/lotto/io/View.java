7065e71519cb862e23e4c9a6ef5777d8d01e3181
ttaehee <ttaehee1105@gmail.com>
2022-11-14 16:28:24 +0900

    feat : 당첨내역 출력 기능 구현

--- src/main/java/lotto/io/View.java
+++ src/main/java/lotto/io/View.java
@@ -1,14 +1,19 @@
 package lotto.io;
 
 import lotto.model.Lotto;
+import lotto.model.LottoRank;
 
 import java.util.List;
+import java.util.Map;
 
 public class View {
     private static final String REQUEST_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
     private static final String GET_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String GET_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String RESULT_INFO = "당첨 통계\n---";
+    private static final String EACH_RESULT = "%d개 일치 (%,d원) - %d개";
+    private static final String BONUS_RESULT = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개";
     private final Input input;
     private final Output output;
 
@@ -45,5 +50,32 @@ public class View {
     public void printError(String message) {
         output.printOutput(message);
     }
-}
 
+    public void showResult(Map<LottoRank, Integer> lottoResult) {
+        output.printOutput(RESULT_INFO);
+        lottoResult.entrySet()
+                .stream()
+                .filter(result -> !result.getKey().equals(LottoRank.SIXTH))
+                .forEach(this::showAllResult);
+    }
+
+    private void showAllResult(Map.Entry<LottoRank, Integer> result) {
+        if (result.getKey().getReward() == 30000000) {
+            showBonus(result);
+            return;
+        }
+        showCommon(result);
+    }
+
+    private void showBonus(Map.Entry<LottoRank, Integer> result) {
+        output.printOutput(
+                String.format(BONUS_RESULT, result.getKey().getMatchCount(),
+                        result.getKey().getReward(), result.getValue()));
+    }
+
+    private void showCommon(Map.Entry<LottoRank, Integer> result) {
+        output.printOutput(
+                String.format(EACH_RESULT, result.getKey().getMatchCount(),
+                        result.getKey().getReward(), result.getValue()));
+    }
+}
--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -3,12 +3,12 @@ package lotto.model;
 import java.util.stream.Stream;
 
 public enum LottoRank {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
+    SIXTH(0, 0),
     FIFTH(3, 5000),
-    SIXTH(0, 0);
+    FOURTH(4, 50000),
+    THIRD(5, 1500000),
+    SECOND(5, 30000000),
+    FIRST(6, 2000000000);
 
     private int matchCount;
     private int reward;
