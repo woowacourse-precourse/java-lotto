be48c867c243d9d1f4954481a3c4717b0c30400c
JAEHYUN <wogustkfkd11@gmail.com>
2022-11-14 23:36:49 +0900

    final GetLottoNumber.java commit

--- src/main/java/lotto/GetLottoNumber.java
+++ src/main/java/lotto/GetLottoNumber.java
@@ -7,64 +7,68 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GetLottoNumber {
-    static String win_number; //당첨번호
-    static int[] win_num; // 당첨번호
-    static int BonusNumber; //보너스번호
+    static String strWinNum; //당첨번호
+    static int[] winNum; // 당첨번호
+    static int bonusNumber; //보너스번호
     List<Integer> temp = new ArrayList<>();
 
-    public List<Integer> GetLotto(List<Integer> numbers){
-        int cnt =0;
+    public GetLottoNumber(List<Integer> numbers) {
+    }
+
+    public List<Integer> GetLotto(List<Integer> numbers) {
+        int cnt = 0;
         temp = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        for(int i =0; i<temp.size(); i++)
-            if(temp.get(i) >= 1 && temp.get(i) <= 45){
-                cnt++;
-            }
-        if(cnt == temp.size()){
-            numbers.addAll(temp);
-            System.out.println(numbers);
-        }
-        else{
-            System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-            throw new IllegalArgumentException();
-        }
-        cnt =0;
+        numbers.addAll(temp);
+        System.out.println(numbers);
         return numbers;
     }
-    public String WinNumInput(){ // 당첨번호 입력
-        win_num = new int[6];
+
+    public String WinNumInput() { // 당첨번호 입력
+        winNum = new int[6];
         String[] temp;
-        win_number = Console.readLine();
-        temp = win_number.split(",");
-        for(int i =0; i< 6; i++){
-            win_num[i] = Integer.parseInt(temp[i]);
+        strWinNum = Console.readLine();
+        temp = strWinNum.split(",");
+
+        if (temp.length != 6) {
+            System.out.println("[ERROR] 입력하신 당첨 번호는 6개가 아닙니다.");
+            return "0";
         }
-        if(win_num.length != 6){
-            System.out.println("[ERROR] 당첨 번호는 6개의 숫자여야 합니다.");
-            throw new IllegalArgumentException();
+
+        try {
+            for (int i = 0; i < 6; i++)
+                winNum[i] = Integer.parseInt(temp[i]);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 당첨 번호는 숫자여야 합니다.");
+            return "0";
         }
-        return win_number;
+        return strWinNum;
     }
-    public int[] WinNum(){ // 당첨번호 배열에 저장
-        win_num = new int[6];
-        String[] temp = new String[win_num.length];
-        temp = win_number.split(",");
-        for(int i =0; i< 6; i++){
-            win_num[i] = Integer.parseInt(temp[i]);
-            if(win_num[i] < 1 || win_num[i] > 45){
+
+    public int[] WinNum(String strWinNum) { // 당첨번호 배열에 저장
+        if(strWinNum.equals("0")){
+            return new int[]{0};
+        }
+
+        winNum = new int[6];
+        String[] temp = strWinNum.split(",");
+
+        for (int i = 0; i < 6; i++) {
+            winNum[i] = Integer.parseInt(temp[i]);
+            if (winNum[i] < 1 || winNum[i] > 45) {
                 System.out.println("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
-                throw new IllegalArgumentException();
+                return new int[]{0};
             }
         }
-        return win_num;
+        return winNum;
     }
 
     public int Bonus() {// 보너스번호
-        BonusNumber = Integer.parseInt(Console.readLine());
-        if (BonusNumber < 1 || BonusNumber > 45) {
+        bonusNumber = Integer.parseInt(Console.readLine());
+        if (bonusNumber < 1 || bonusNumber > 45) {
             System.out.println("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
-            throw new IllegalArgumentException();
+            return 0;
         }
-        return BonusNumber;
+        return bonusNumber;
     }
 
 }
