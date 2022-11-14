12309b1b90a05036c6f3d539ee77768ea33a6651
p <gustjr2332@naver.com>
2022-11-14 11:22:55 +0900

    feat(Output) : 출력 기능 초기 세팅

---
+++ src/main/java/UI/Output.java
@@ -0,0 +1,25 @@
+package UI;
+
+public class Output {
+    public static final String INPUTPRICE = "구입금액을 입력해 주세요.";
+    public static final String INPUTWINNINGNUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String INPUTBONUSNUMBER = "보너스 번호를 입력해 주세요";
+
+    public static void inputPrice() {
+        System.out.println(INPUTPRICE);
+    }
+
+    public static void inputWinningNumber() {
+        System.out.println(INPUTWINNINGNUMBERS);
+    }
+
+    public static void inputBounsNumber() {
+        System.out.println(INPUTBONUSNUMBER);
+    }
+
+    //구매한 내역 출력
+
+
+    //당첨통계 출력
+
+}
