f877fb78e90c2c628a8dd84e912ec4e60f4b90d2
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-15 04:42:51 +0900

    feat(view/OutputView.java): 구입한 로또 수량, 번호 출력하는 함수 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,24 @@
+package lotto.view;
+
+import lotto.domain.game.Ranking;
+import lotto.domain.player.Player;
+
+import java.util.List;
+import java.util.Map.Entry;
+
+import static lotto.view.OutputString.*;
+
+public class OutputView {
+    public void printLottoTickets(Player player){
+        List<List<Integer>> lottoTickets = player.getLottoTickets();
+        int count = lottoTickets.size();
+        System.out.println(TICKET_QUANTITY_MESSAGE.print(count));
+        printEachLottoTickets(lottoTickets);
+    }
+
+    public void printEachLottoTickets(List<List<Integer>> lottoTickets) {
+        for (List<Integer> lottoTicket : lottoTickets) {
+            System.out.println(lottoTicket);
+        }
+    }
+}
