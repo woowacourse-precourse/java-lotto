3063e75a31300686354907b3d9249eef22dd5279
YUSEONJUN <seonjun0906@gmail.com>
2022-11-14 23:32:41 +0900

    feat : enum을 이용한 등수 정의

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import java.util.Comparator;
+
+public enum Rank implements Comparator<Rank> {
+	FIRST(6, 2_000_000_000),
+	SECOND(5, 30_000_000),
+	THIRD(5, 1_500_000),
+	FOURTH(4, 50_000),
+	FIFTH(3, 5_000),
+	MISS(0, 0);
+
+	private final int countOfMatch;
+	private final int winningMoney;
+
+	Rank(int countOfMatch, int winningMoney) {
+		this.countOfMatch = countOfMatch;
+		this.winningMoney = winningMoney;
+	}
+
+
+
+}
