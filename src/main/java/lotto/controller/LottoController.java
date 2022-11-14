11df581a957880c5657045f74b8fa293bd2e3644
yangjiyoung <0112jyoung@naver.com>
2022-11-14 21:40:59 +0900

    refactor: delete useless variable

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,5 @@
 package lotto.controller;
 
-import lotto.Lotto;
 import lotto.domain.LottoQuantity;
 import lotto.domain.LottoRanking;
 import lotto.domain.Lottos;
@@ -14,9 +13,10 @@ public class LottoController {
     public List<Integer> userLottoNumbers;
     public int userLottoQuantity;
     public int userLottoMoney;
-    public Lottos lottos=new Lottos(userLottoQuantity);
-    public LottoQuantity lottoQuantity=new LottoQuantity();
+    public Lottos lottos = new Lottos(userLottoQuantity);
+    public LottoQuantity lottoQuantity = new LottoQuantity();
     public Map<LottoRanking, Integer> lottoRanking;
+
     public void startLotto() {
         getUserMoney();
         printLottoQuantity();
@@ -29,41 +29,43 @@ public class LottoController {
     }
 
     public void getUserMoney() {
-        userLottoMoney=InputView.requestPurchaseMoney();
+        userLottoMoney = InputView.requestPurchaseMoney();
     }
 
 
     public void printLottoQuantity() {
-        LottoQuantity lottoQuantity=new LottoQuantity();
-        lottoQuantity.setLottoQuantityNumber(userLottoMoney/1000);
-        userLottoQuantity=lottoQuantity.getLottoQuantityNumber();
+        lottoQuantity.setLottoQuantityNumber(userLottoMoney / 1000);
+        userLottoQuantity = lottoQuantity.getLottoQuantityNumber();
         OutputView.printPurchaseLottoSet(userLottoQuantity);
     }
-    public void printLottos(){
+
+    public void printLottos() {
         lottos.initLottos(userLottoQuantity);
         OutputView.printPurchaseLottoNumbers(lottos.getLottos());
     }
 
-    public void getUserLottoNumbers(){
+    public void getUserLottoNumbers() {
         userLottoNumbers = InputView.requestLottoNumbers();
-        //System.out.println("userLottoNumbers: "+userLottoNumbers);
     }
-    public void getUserBonusNumber(){
+
+    public void getUserBonusNumber() {
         userLottoNumbers.add(InputView.requestBonusNumber());
     }
 
-    public void compareLottos(){
-        lottoRanking=lottos.calculateLottos(userLottoNumbers);
+    public void compareLottos() {
+        lottoRanking = lottos.calculateLottos(userLottoNumbers);
     }
-    public void getLottoResult(){
+
+    public void getLottoResult() {
         OutputView.printLottoResult(lottoRanking);
     }
-    public void getLottoResultRate(){
-        int totalLottoMoney=0;
-        for(LottoRanking ranking:lottoRanking.keySet()){
-            totalLottoMoney+=ranking.getLottoWinningMoney()*lottoRanking.get(ranking);
+
+    public void getLottoResultRate() {
+        int totalLottoMoney = 0;
+        for (LottoRanking ranking : lottoRanking.keySet()) {
+            totalLottoMoney += ranking.getLottoWinningMoney() * lottoRanking.get(ranking);
         }
-        float percent=(float)totalLottoMoney/(userLottoMoney)*100;
+        float percent = (float) totalLottoMoney / (userLottoMoney) * 100;
         OutputView.printLottoProfitRate(percent);
     }
 }
