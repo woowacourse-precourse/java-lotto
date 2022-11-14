0e54a572ec3a4e177f52ac8fd604113f8d16aedd
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-14 16:12:34 +0900

    fix(ticket): 로또 티켓 관련 함수 추가
    로또 티켓 관련 함수
    - 외부에서 티켓 접근 가능
    - 로또 번호와 같은 번호 개수 세기
    - 보너스 번호와 같은 번호 세기

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -5,16 +5,44 @@ import lotto.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class LottoTicket {
     private static final int LOTTO_MIN = 1;
     private static final int LOTTO_MAX = 45;
     private static final int LOTTO_COUNT = 6;
 
+    private final int tickets;
     private final List<List<Integer>> lottoTickets = new ArrayList<>();
 
     public LottoTicket(int tickets){
+        this.tickets = tickets;
         buyLottoTickets(tickets);
+    }
+
+    public int getTickets(){
+        return tickets;
+    }
+
+    public List<Integer> countSameLottoNumbers(Lotto lottoNumbers){
+        List<Integer> count = new ArrayList<>();
+        for (List<Integer> lottoTicket : lottoTickets) {
+            List<Integer> result = lottoTicket.stream()
+                    .filter(tmp -> lottoNumbers.getNumbers().stream().anyMatch(Predicate.isEqual(tmp)))
+                    .collect(Collectors.toList());
+            count.add(result.size());
+        }
+
+        return count;
+    }
+
+    public List<Boolean> checkSameLottoBonus(int bonus){
+        List<Boolean> result = new ArrayList<>();
+        for (List<Integer> lottoTicket : lottoTickets) {
+            result.add(lottoTicket.stream().anyMatch(lottoNumber -> lottoNumber.equals(bonus)));
+        }
+        return result;
     }
 
     private void buyLottoTickets(int tickets){
