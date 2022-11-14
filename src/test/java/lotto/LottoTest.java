0dba20cb18294e75a68f131c50ccc877621b6bc0
ChanghyunKim <variety82p@gamil.com>
2022-11-14 22:54:37 +0900

    test(LottoTest) : 당첨번호 입력형식이 다른 케이스 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,21 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static lotto.utils.Messages.ERROR_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class LottoTest {
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+class LottoTest extends NsTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -23,5 +31,36 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("당첨 번호 형식이 다르면 예외가 발생한다")
+    @Test
+    void createWrongWinningNumber() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("2000", "1,2,3,4,5, 6");
+                    assertThat(output()).contains(
+                            "2개를 구매했습니다.",
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            ERROR_MESSAGE
+                    );
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38)
+        );
+    }
+
+//    @DisplayName("구매 금액이 1000의 배수가 아닐경우 예외가 발생한다")
+//    @Test
+//    void createNonMultipleOfThousandsInput(){
+//        assertSimpleTest(() -> {
+//            runException("1200");
+//            assertThat(output()).contains(ERROR_MESSAGE);
+//        });
+//    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
     // 아래에 추가 테스트 작성 가능
 }
