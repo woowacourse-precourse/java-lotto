b2a3ad648b68602e1917020284d6b75b2e02e2f4
cocomozila <joseyun94@naver.com>
2022-11-14 23:47:39 +0900

    feat(Setting): 문자 입력기준 추가

--- src/main/java/lotto/setting/Setting.java
+++ src/main/java/lotto/setting/Setting.java
@@ -7,4 +7,6 @@ public class Setting {
     public static final int LOTTO_MIN_NUMBER = 1;
     public static final int LOTTO_MAX_NUMBER = 45;
     public static final int LOTTO_SIZE = 6;
+    public static final int INPUT_WINNING_NUMBER_COMMA_COUNT = 5;
+    public static final char SPLIT_CHARACTER = ',';
 }
