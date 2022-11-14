69dcaf9706b4a8ccfe209b319f36a01fb345a900
황민주 <minjoo@hwangminjooui-MacBookAir.local>
2022-11-14 12:45:36 +0900

    feat(Win): key 필드 추가하고 getKey 메소드 추가함, enum의 이름 형식 바꿈 (src/main/java/lotto/Win.java)
    key 필드 추가하고 getKey 메소드 추가:
    -결과를 리스트에 저장할 때 각 enum에 따라 key 값이 필요해 key 필드 추가함
    -key필드의 값을 조회하기 위해 getKey 메소드 추가함
    
    enum의 이름 형식 수정:
    -(WIN+맞춘 숫자의 개수+맞춘 보너스 숫자의 개수)을 enum의 이름 형식으로 수정해 5가지의 당첨 결과의 enum으로 표현함

--- src/main/java/lotto/Win.java
+++ src/main/java/lotto/Win.java
@@ -3,20 +3,26 @@ package lotto;
 import java.util.function.Function;
 
 public enum Win {
-    firstPlace(6, 2000000000, num -> num * 2000000000),
-    secondPlace(5, 30000000, num -> num * 30000000),
-    thirdPlace(5, 1500000, num -> num * 1500000),
-    fourthPlace(4, 50000, num -> num * 50000),
-    fifthPlace(3, 5000, num -> num * 5000);
+    WIN60(0,6, 2000000000, num -> num * 2000000000),
+    WIN51(1,5, 30000000, num -> num * 30000000),
+    WIN50(2, 5, 1500000, num -> num * 1500000),
+    WIN40(3, 4, 50000, num -> num * 50000),
+    WIN30(4, 3, 5000, num -> num * 5000);
 
+    private int key;
     private int cntCorrectNumber;
     private int prize;
     private Function<Long, Long> totalPrize;
 
-    Win(int cntCorrectNumber, int prize, Function <Long, Long> totalPrize){
+    Win(int key, int cntCorrectNumber, int prize, Function <Long, Long> totalPrize){
+        this.key = key;
         this.cntCorrectNumber = cntCorrectNumber;
         this.prize = prize;
         this.totalPrize = totalPrize;
+    }
+
+    public int getKey(){
+        return key;
     }
 
     public int getCntCorrectNumber() {
