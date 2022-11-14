f52e11148cc02fbc1d4cc37dff8530565ec32e3c
desfox <desfox99@naver.com>
2022-11-14 21:38:45 +0900

    feat(OutputView): 당첨 번호 입력 메시지 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,4 +10,8 @@ public class OutputView {
     public void printWinningNumbers(List<Integer> winningNumbers) {
         System.out.println(winningNumbers);
     }
+
+    public void printPredictionNumbersMessage() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
 }
