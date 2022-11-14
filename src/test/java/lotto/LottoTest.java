3a39a8d0d3a3d094529dd5451c2a400a4498993d
TaemHam <tk4003950@gmail.com>
2022-11-14 16:34:12 +0900

    Refactor: Lotto 클래스 패키지 변경

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,6 @@
 package lotto;
 
-import lotto.data.dto.Lotto;
+import lotto.data.entity.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.List;
