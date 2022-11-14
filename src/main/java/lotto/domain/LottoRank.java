b5ee03524f8dbd014d1b97c07398a53a5d93738a
jmjmjames <chungjm0711@gmail.com>
2022-11-14 15:39:05 +0900

    feat(LottoRank): 로또 Rank 클래스 뼈대 생성

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+
+public enum LottoRank {
+
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    REST(0, 0);
+
+    private final int matchCount;
+    private final int prizeMoney;
+
+    LottoRank(int matchCount, int prizeMoney) {
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+    }
+}
