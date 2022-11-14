2649d41949b599855acf693f49525313f9772fad
taekwon-dev <taekwon_dev@naver.com>
2022-11-14 16:42:48 +0900

    fix: (Statistics) Map 구현체 TreeMap으로 변경
    - HashMap to TreeMap
    - Map.Key 정렬 활용 목적

--- src/main/java/lotto/domain/Statistics.java
+++ src/main/java/lotto/domain/Statistics.java
@@ -1,12 +1,12 @@
 package lotto.domain;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 import static lotto.domain.Rank.*;
 
 public class Statistics {
-    private Map<Rank, Integer> rankRecord = new HashMap<>(){{
+    private Map<Rank, Integer> rankRecord = new TreeMap<>(){{
         put(FIRST_PLACE, 0);
         put(SECOND_PLACE, 0);
         put(THIRD_PLACE, 0);
