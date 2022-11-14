9fa4cb318860f05cb55bf2a4af23241bee2500ae
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-15 08:45:34 +0900

    feat(view): view 구현

---
+++ src/main/java/lotto/layer/View.java
@@ -0,0 +1,12 @@
+package lotto.layer;
+
+public class View {
+
+    protected void print(String message) {
+        System.out.println(message);
+    }
+
+    protected void printEmptyLine() {
+        System.out.println();
+    }
+}
