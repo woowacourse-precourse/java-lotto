4939ac17fc8f6b942814f749684ae803f1fa9a18
ParkRootSeok <g_e_un@naver.com>
2022-11-14 17:46:11 +0900

    feat(Checker) : 당첨 통계 출력 기능 추가

--- src/main/java/lotto/domain/Checker.java
+++ src/main/java/lotto/domain/Checker.java
@@ -11,12 +11,12 @@ public class Checker {
 
     private static final String INSERT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String INSERT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    private static final String WINNING_STATS = "당첨 통계";
+    private static final String WINNING_STATS = "당첨 통계\n---";
     private static final String THREE_WINNING = "3개 일치 (5,000원) - ";
     private static final String FOUR_WINNING = "4개 일치 (50,000원) - ";
-    private static final String FIVE_WINNING = "5개 일치 (1,500,000원)";
-    private static final String FIVE_WINNING_ONE_BONUS = "5개 일치, 보너스 볼 일치 (30,000,000원)";
-    private static final String SIX_WINNING = "6개 일치 (2,000,000,000원)";
+    private static final String FIVE_WINNING = "5개 일치 (1,500,000원) - ";
+    private static final String FIVE_WINNING_ONE_BONUS = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    private static final String SIX_WINNING = "6개 일치 (2,000,000,000원) - ";
     private static final String SHOW_RETURN_RATE_FRONT = "총 수익률은 ";
     private static final String SHOW_RETURN_RATE_BACK = "입니다.";
 
@@ -26,6 +26,7 @@ public class Checker {
         put(4, 0);
         put(5, 0);
         put(6, 0);
+        put(7, 0);
     }};
     private int bonusNumber;
     private int key;
@@ -88,7 +89,7 @@ public class Checker {
 
     private void checkBonusNumber(List<Integer> lotto) {
         if (lotto.contains(bonusNumber)) {
-            key++;
+            key += 2;
         }
     }
 
@@ -98,11 +99,35 @@ public class Checker {
 
     public void showWinningStats() {
 
+        System.out.println(WINNING_STATS);
+
+        for (Entry<Integer, Integer> entry : winningStats.entrySet()) {
+
+            if (entry.getKey() == 3) {
+                System.out.println(THREE_WINNING + entry.getValue());
+            }
+
+            if (entry.getKey() == 4) {
+                System.out.println(FOUR_WINNING + entry.getValue());
+            }
+
+            if (entry.getKey() == 5) {
+                System.out.println(FIVE_WINNING  + entry.getValue());
+            }
+
+            if (entry.getKey() == 7) {
+                System.out.println(FIVE_WINNING_ONE_BONUS  + entry.getValue());
+            }
+
+            if (entry.getKey() == 6) {
+                System.out.println(SIX_WINNING  + entry.getValue());
+            }
+
+        }
     }
 
     public float calculateRateOfReturn() {
         return returnRate;
     }
-
 
 }
