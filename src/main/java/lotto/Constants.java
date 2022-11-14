e8f18dd31bd71ee79d210959d172bb7b25515e5e
chaos0103 <lyt1228@naver.com>
2022-11-14 14:07:42 +0900

    refactor(Lotto): 상수를 별도로 분리

---
+++ src/main/java/lotto/Constants.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public final class Constants {
+
+    public static final int NUMBER_MIN_RANGE = 1;
+    public static final int NUMBER_MAX_RANGE = 45;
+    public static final int LOTTO_NUMBERS_COUNT = 6;
+}
--- src/main/java/lotto/domain/impl/NumberGeneratorImpl.java
+++ src/main/java/lotto/domain/impl/NumberGeneratorImpl.java
@@ -6,11 +6,13 @@ import lotto.domain.NumberGenerator;
 import java.util.Collections;
 import java.util.List;
 
+import static lotto.Constants.*;
+
 public class NumberGeneratorImpl implements NumberGenerator {
 
     @Override
     public List<Integer> createRandomNumbers() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(NUMBER_MIN_RANGE, NUMBER_MAX_RANGE, LOTTO_NUMBERS_COUNT);
         Collections.sort(numbers);
         return numbers;
     }
