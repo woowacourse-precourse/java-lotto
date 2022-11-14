467f0e5b6b0d70d159c7755edd91dbffe3616e5c
Haaae <101785628+Haaae@users.noreply.github.com>
2022-11-14 15:47:26 +0900

    feat(Application): 복권 구입 금액을 입력받는다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
         // TODO: 프로그램 구현
         // 사용자에게 복권 구입 금액을 입력받는다.
-        int ticketPrice = getTicketPrice();
-        Publisher publisher = new Publisher(ticketPrice);
+        int purchaseAmount = getPurchaseAmount();
+        Publisher publisher = new Publisher(purchaseAmount);
         // 구입하는 복권 수만큼 복권 번호를 사용자로부터 입력받는다.
         issueLotteries(publisher);
         // 구입하는 복권 수량과 정렬된 복권 번호를 출력한다.
@@ -20,8 +20,13 @@ public class Application {
         UI.printResult(result);
 
     }
-    static int getTicketPrice() throws IllegalArgumentException {
-       return 0;
+    static int getPurchaseAmount() throws IllegalArgumentException {
+        try {
+            int purchaseAmount = UI.getAnswerInInteger(Request.purchaseAmount.value());
+            return purchaseAmount;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
     }
 
     static void issueLotteries(Publisher publisher) {
