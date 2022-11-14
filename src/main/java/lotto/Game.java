2768b3bddc67213f48c7b0248e609e7542f6d42f
metorg <metrog950@gmail.com>
2022-11-14 16:00:16 +0900

    feat: 금액에 맞는 로또 번호들 저장 메서드 생성

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,10 +16,19 @@ public class Game {
     public void run(){
         init();
         int money = view.requestMoney();
+        System.out.println(money);
+        lottoNumberSave(money);
+
     }
 
-
-
+    public void lottoNumberSave(int money){
+        for(int i=0;i<money/1000;i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+            Lotto lotto = new Lotto(numbers);
+            total_lotto.add(lotto);
+        }
+    }
+    
     private void init(){
         final Scanner scanner = new Scanner(System.in);
         view= new View(scanner);
