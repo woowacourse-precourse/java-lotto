55fc9e91dd569ee43cb35e3028cdaf09be59b690
kimJaeHyeon <kmr2644@gmail.com>
2022-11-14 09:54:27 +0900

    feat: 로또를 리스트에 저장하고 로또가 자기고있는 랜덤정수를 반환하는 메서드 구현

--- src/main/java/lotto/domain/LottoRepository.java
+++ src/main/java/lotto/domain/LottoRepository.java
@@ -1,2 +1,19 @@
-package lotto.domain;public class LottoRepository {
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoRepository {
+    private final List<Lotto> lottos = new ArrayList<>();
+
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public List<Integer> save(Lotto lotto) {
+        lottos.add(lotto);
+        return lotto.getNumbers();
+    }
+
 }
