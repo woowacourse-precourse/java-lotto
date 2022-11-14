bfb54c4f2834d8a0960ca0b6d2f1d8354965a301
jjmin9797 <jjmin9797@naver.com>
2022-11-14 16:42:19 +0900

    feat(UserController.java): 사용자가 구매한 로또 보여주는 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/controller/UserController.java
+++ src/main/java/lotto/controller/UserController.java
@@ -3,11 +3,13 @@ package lotto.controller;
 import lotto.repository.UserRepository;
 import lotto.service.UserService;
 import lotto.view.GameProgressView;
+import lotto.view.WinningStatsView;
 
 public class UserController {
     private static UserRepository userRepository = new UserRepository();
     private static UserService userService = new UserService();
     private static GameProgressView gameProgressView = new GameProgressView();
+    private static WinningStatsView winningStatsView = new WinningStatsView();
 
     public void savePurchaseAmount() {
         gameProgressView.showPurchaseMessage();
@@ -15,5 +17,10 @@ public class UserController {
         //TODO : 예외 처리 필요
         userRepository.setUserPurchaseMoney(Integer.parseInt(purchaseAmount));
         userRepository.setUserNumberOfLotties(userService.findTheNumberOfLotteryTicketsPurchased(purchaseAmount));
+    }
+
+    public void showPurchaseInfo() {
+        winningStatsView.showLineBreak();
+        gameProgressView.showCountMessage(userRepository.getUser().getTotalNumberOflotties());
     }
 }
