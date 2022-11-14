196da417aa8195e1af08c55558b9ea604e3a3029
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-15 00:12:02 +0900

    test(winninglotto): 정보가 일치하면 당첨 객체 반환 기능 테스트 완료

---
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -0,0 +1,107 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningLottoTest {
+
+    @DisplayName("1등 매칭 테스트.")
+    @Test
+    void WinningLottoMatchPlace1() {
+        // given
+        int commonMatch = 6;
+        boolean bonusMatch = false;
+
+        // when
+        WinningLotto winningLotto = WinningLotto.matchOf(commonMatch, bonusMatch);
+
+        // then
+        assertThat(winningLotto).isEqualTo(WinningLotto.PLACE_1);
+    }
+
+    @DisplayName("2등 매칭 테스트.")
+    @Test
+    void WinningLottoMatchPlace2() {
+        // given
+        int commonMatch = 5;
+        boolean bonusMatch = true;
+
+        // when
+        WinningLotto winningLotto = WinningLotto.matchOf(commonMatch, bonusMatch);
+
+        // then
+        assertThat(winningLotto).isEqualTo(WinningLotto.PLACE_2);
+    }
+
+    @DisplayName("1등 매칭 테스트.")
+    @Test
+    void WinningLottoMatchPlace3() {
+        // given
+        int commonMatch = 5;
+        boolean bonusMatch = false;
+
+        // when
+        WinningLotto winningLotto = WinningLotto.matchOf(commonMatch, bonusMatch);
+
+        // then
+        assertThat(winningLotto).isEqualTo(WinningLotto.PLACE_3);
+    }
+
+    @DisplayName("4등 매칭 테스트.")
+    @Test
+    void WinningLottoMatchPlace4() {
+        // given
+        int commonMatch = 4;
+        boolean bonusMatch = false;
+
+        // when
+        WinningLotto winningLotto = WinningLotto.matchOf(commonMatch, bonusMatch);
+
+        // then
+        assertThat(winningLotto).isEqualTo(WinningLotto.PLACE_4);
+    }
+
+    @DisplayName("5등 매칭 테스트.")
+    @Test
+    void WinningLottoMatchPlace5() {
+        // given
+        int commonMatch = 3;
+        boolean bonusMatch = false;
+
+        // when
+        WinningLotto winningLotto = WinningLotto.matchOf(commonMatch, bonusMatch);
+
+        // then
+        assertThat(winningLotto).isEqualTo(WinningLotto.PLACE_5);
+    }
+
+    @DisplayName("2, 3등이 아닌 경우 bonus는 영향을 받지 않는다.")
+    @Test
+    void WinningLottoMatchNotEffectBonusWhenPlace2Or3() {
+        // given
+        int commonMatch = 3;
+        boolean bonusMatch = true;
+
+        // when
+        WinningLotto winningLotto = WinningLotto.matchOf(commonMatch, bonusMatch);
+
+        // then
+        assertThat(winningLotto).isEqualTo(WinningLotto.PLACE_5);
+    }
+
+    @DisplayName("매칭 정보가 없는 경우 null을 반환한다.")
+    @Test
+    void WinningLottoMatchWhenNotMatchingReturnNull() {
+        // given
+        int commonMatch = 1;
+        boolean bonusMatch = true;
+
+        // when
+        WinningLotto winningLotto = WinningLotto.matchOf(commonMatch, bonusMatch);
+
+        // then
+        assertThat(winningLotto).isNull();
+    }
+}
