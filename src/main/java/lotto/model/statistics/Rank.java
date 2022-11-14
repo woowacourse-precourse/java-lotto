de29d031a6fcffecda5bd054ad654923d1f22c2c
hongjinwon <herenever@naver.com>
2022-11-15 04:44:43 +0900

    feat(model): Rank
    각각의 발행된 로또와 당첨번호 및 보너스의 일치 갯수에 따른 순위를 부여하는 Rank 구현

---
+++ src/main/java/lotto/model/statistics/Rank.java
@@ -0,0 +1,35 @@
+package lotto.model.statistics;
+
+import java.util.Arrays;
+import java.util.function.BiPredicate;
+
+public enum Rank {
+
+    FIRST(6,2_000_000_000,(correctNumCount,isMatchBonus)-> correctNumCount == 6),
+    SECOND(5,30_000_000,(correctNumCount,isMatchBonus)-> correctNumCount == 5 && isMatchBonus),
+    THIRD(5,1_500_000,(correctNumCount,isMatchBonus)-> correctNumCount == 5 && !isMatchBonus),
+    FOURTH(4,50_000,(correctNumCount,isMatchBonus)-> correctNumCount == 4),
+    FIFTH(3,5_000,(correctNumCount,isMatchBonus)-> correctNumCount == 3),
+    FAIL(0,0,(correctNumCount,isMatchBonus)-> correctNumCount < 3);
+
+    private final Integer sameCount;
+    private final Integer prize;
+    private final BiPredicate<Integer,Boolean> matchCase;
+
+    Rank(Integer sameCount,Integer prize, BiPredicate<Integer,Boolean> matchCase){
+        this.sameCount = sameCount;
+        this.prize = prize;
+        this.matchCase = matchCase;
+    }
+
+    public static Rank of(Integer sameCount,Boolean isMatchBonus){
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchCase.test(sameCount, isMatchBonus))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException());
+    }
+
+    public Integer getPrize() {
+        return prize;
+    }
+}
