142bfae3cf80e4d6c51a298681f98543b793b718
whdcks420 <whdcks420@gmail.com>
2022-11-14 23:24:18 +0900

    refactor: code line 정리

--- src/test/java/lotto/utils/FormatLottoNumberTest.java
+++ src/test/java/lotto/utils/FormatLottoNumberTest.java
@@ -1,4 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FormatLottoNumberTest {
-  
+
+    @DisplayName("당첨번호 입력 포멧팅 테스트")
+    @Test
+    void winningNumberInputTest() {
+        List<Integer> list = FormatLottoNumber.get("1,2,3,4,5,6");
+        assertEquals("123456", list.stream().map(String::valueOf).collect(Collectors.joining()));
+    }
 }
