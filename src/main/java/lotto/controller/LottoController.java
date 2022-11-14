929ec29e323e58c6f99f71485028e3ddf2345abd
LeeJejune <leejejune0625@gmail.com>
2022-11-14 21:47:07 +0900

    feat: 로또 게임의 전반을 담당하는 controller 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,19 @@
+package lotto.controller;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoAmount;
+import lotto.domain.LottoResult;
+import lotto.domain.LottoTicket;
+import lotto.domain.WinningNumbers;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import static lotto.view.InputView.inputWinningNumbers;
+import static lotto.view.OutputView.printException;
+import static lotto.view.OutputView.printLottoResult;
+import static lotto.view.OutputView.printLottoTicketCount;
+import static lotto.view.OutputView.printProfit;
+import static lotto.view.OutputView.printResultIntro;
+
+public class LottoController {
+}
