953999473fbbd85e0b9285a568df1c3826ff010e
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-14 22:43:08 +0900

    feat: Buyer generate 메소드 작성

--- src/main/java/lotto/generate/BuyerGenerate.java
+++ src/main/java/lotto/generate/BuyerGenerate.java
@@ -1,5 +1,7 @@
 package lotto.generate;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Buyer;
 import lotto.enums.StringEnum;
 
 import static lotto.enums.StringEnum.*;
@@ -7,5 +9,12 @@ import static lotto.enums.StringEnum.*;
 public class BuyerGenerate {
     public void askBuyPricePrint() {
         System.out.println(PRICE.getMessage());
+    }
+
+    public Buyer generate() {
+        askBuyPricePrint();
+        String input = Console.readLine();
+        int buyPrice = Integer.parseInt(input);
+        return new Buyer(buyPrice);
     }
 }
