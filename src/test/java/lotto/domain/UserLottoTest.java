50da456bd8be30c2d04481811de5f82604a76485
wonseok <tkddls23@konkuk.ac.kr>
2022-11-15 04:26:55 +0900

    test: 로또 구매 금액이 유효한지 검증

---
+++ src/test/java/lotto/domain/UserLottoTest.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import lotto.validation.InputValidation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class UserLottoTest {
+
+    @DisplayName("예외 처리 : 구매금액이 유효한지 검증")
+    @ParameterizedTest
+    @ValueSource(ints = {0,1050,100})
+    void isDigitInputValidation(int input) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new UserLotto(input));
+    }
+}
