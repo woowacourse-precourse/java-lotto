6dc6e3f0d8698e7e17109190286597c1f7315ea3
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-14 23:20:32 +0900

    feat(lotto): 로또 6개의 로또 번호를 입력으로 받도록 구현

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lotto {
+
+    public static final int LOTTO_LENGTH = 6;
+    private static final String ERROR_MATCH_LENGTH = "로또는 6개의 번호만 입력 가능합니다.";
+    private static final String ERROR_DUPLICATE_VALUE = "로또에는 중복된 번호를 입력할 수 없습니다.";
+    private final List<LottoNumber> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        List<LottoNumber> lottoNumbers = generateLottoNumber(numbers);
+        validateNumbersLength(lottoNumbers);
+        validateDuplicateValue(lottoNumbers);
+        this.numbers = lottoNumbers;
+    }
+
+    private List<LottoNumber> generateLottoNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .map(LottoNumber::getInstance)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    private void validateNumbersLength(List<LottoNumber> numbers) {
+        if (numbers.size() != LOTTO_LENGTH) {
+            throw new IllegalArgumentException(ERROR_MATCH_LENGTH);
+        }
+    }
+
+    private void validateDuplicateValue(List<LottoNumber> lottoNumbers) {
+        List<LottoNumber> list = new ArrayList<>();
+        for (LottoNumber lottoNumber : lottoNumbers) {
+            if (!list.isEmpty() && list.contains(lottoNumber)) {
+                throw new IllegalArgumentException(ERROR_DUPLICATE_VALUE);
+            }
+            list.add(lottoNumber);
+        }
+    }
+}
