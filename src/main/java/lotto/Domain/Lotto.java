01d060bdd3034dfba6bd1a1b05800397520a25eb
rvlwldev <rvlwlasd77@gmail.com>
2022-11-14 17:37:49 +0900

    fix: 랜덤로또번호 정렬 기능 수정

--- src/main/java/lotto/Domain/Lotto.java
+++ src/main/java/lotto/Domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.Domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,14 +9,19 @@ public class Lotto extends LottoShop {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        this.numbers = sortLottoList(numbers);
 
-        Collections.sort(numbers);
-        this.numbers = numbers;
-
-        soldLottoList.add(numbers);
+        soldLottoList.add(this.numbers);
     }
 
     private void validate(List<Integer> numbers) {
         inspection.validateLottoNumberList(numbers);
+    }
+
+    private List<Integer> sortLottoList(List<Integer> numbers) {
+        List<Integer> sortedList = new ArrayList<>(numbers);
+        Collections.sort(sortedList);
+
+        return sortedList;
     }
 }
