3c6ebbbe242a6ebc5eda8460fcea95c5922ece3b
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-14 17:07:38 +0900

    feat(Validator): 인자가 양수인지 확인하는 기능 구현

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -9,12 +9,20 @@ public class Validator {
     private static final int ZERO = 0;
     private static final int PLAYER_NUMBERS_COUNT_STANDARD = 6;
 
+    private static final String NOT_POSITIVE_NUMBER_ERROR_MESSAGE = "[ERROR] 0보다 작으면 안됩니다.";
     private static final String PURCHASE_AMOUNT_UNIT_ERROR_MESSAGE = "구입금액은 1,000원 단위여야 합니다.";
     private static final String PLAYER_NUMBERS_COUNT_STANDARD_ERROR_MESSAGE = "당첨 번호는 6개를 입력해야 합니다.";
     private static final String PLAYER_NUMBERS_RANGE_ERROR_MESSAGE = "당첨 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final String PLAYER_NUMBERS_DUPLICATION_ERROR_MESSAGE = "당첨 번호에 중복된 번호가 존재합니다.";
     private static final String BONUS_NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final String BONUS_NUMBER_DUPLICATION_ERROR_MESSAGE = "당첨 번호에 중복된 번호가 존재합니다.";
+
+    public static void validatePositiveNumber(int number) {
+        boolean isPositive = number > 0;
+        if (!isPositive) {
+            throw new IllegalArgumentException(NOT_POSITIVE_NUMBER_ERROR_MESSAGE);
+        }
+    }
 
     public static void validateUnitStandard(int purchaseAmount) {
         boolean isPurchaseAmountNotFollowUnitStandard = purchaseAmount % UNIT_STANDARD != ZERO;
