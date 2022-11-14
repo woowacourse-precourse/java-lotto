0ff008b1b5a949087b9850c80a29526845aa8298
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-14 11:27:55 +0900

    feat: 당첨번호와 로또 번호 사이 중복되는 번호 셈 및 당첨 금액 계산 기능 구현

--- src/main/java/lotto/domain/Statistics.java
+++ src/main/java/lotto/domain/Statistics.java
@@ -1,9 +1,57 @@
 package lotto.domain;
 
+import java.util.EnumMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Statistics {
-    public void winningStatistics(List<Lotto> quickPicks, LinkedList<Integer> winningNumWithBonusList, int purchaseAmt) {
+    private final int INITIAL_VALUE = 0;
+    private final Map<Rank, Integer> rankMap = new EnumMap<>(Rank.class);
+    private int totalPrize;
+
+    public Statistics() {
+        rankMapPut();
     }
+
+    private void rankMapPut() {
+        rankMap.put(Rank.FIRST, INITIAL_VALUE);
+        rankMap.put(Rank.SECOND, INITIAL_VALUE);
+        rankMap.put(Rank.THIRD, INITIAL_VALUE);
+        rankMap.put(Rank.FOURTH, INITIAL_VALUE);
+        rankMap.put(Rank.FIFTH, INITIAL_VALUE);
+    }
+
+    public void winningStatistics(List<Lotto> quickPicks, LinkedList<Integer> winningNumWithBonusList, int purchaseAmt) {
+        for (Lotto quickPick : quickPicks) {
+            compareNum(quickPick, winningNumWithBonusList);
+        }
+    }
+
+    private void compareNum(Lotto quickPick, LinkedList<Integer> winningNumWithBonusList) {
+        List<Integer> winningNumList = winningNumWithBonusList.subList(0, winningNumWithBonusList.size() - 1);
+        int bonus = winningNumWithBonusList.getLast();
+        boolean bonusMatch = false;
+        long count = quickPick.getNumbers().stream().filter(winningNumList::contains).count();
+
+        if (quickPick.getNumbers().contains(bonus)) {
+            bonusMatch = true;
+        }
+        calculateTotalPrize((int) count, bonusMatch);
+    }
+
+    private void calculateTotalPrize(int count, boolean bonusMatch) {
+        Rank rank = Rank.getRank(count, bonusMatch);
+        totalPrize += rank.getPrize();
+
+        calculateMatchCount(rank);
+    }
+
+    private void calculateMatchCount(Rank rank) {
+        if (rankMap.containsKey(rank)) {
+            rankMap.replace(rank, rankMap.get(rank) + 1);
+        }
+
+    }
+
 }
