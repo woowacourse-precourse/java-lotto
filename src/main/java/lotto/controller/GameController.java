491b3a0cca6a418fff823dca2fcbfdd9974ee32f
Lee-Jiseung <ljss98@naver.com>
2022-11-14 22:23:40 +0900

    feat: 구입금액 입력받기

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,11 +1,14 @@
 package lotto.controller;
 
+import lotto.model.User;
 import lotto.view.Message;
 
 public class GameController {
     private final Message message = new Message();
+    private final User user = new User();
 
     public void buyLotto() {
         message.inputPrice();
+        user.inputPrice();
     }
 }
---
+++ src/main/java/lotto/model/User.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
+public class User {
+
+    public void inputPrice() {
+        String price = Console.readLine();
+    }
+}
