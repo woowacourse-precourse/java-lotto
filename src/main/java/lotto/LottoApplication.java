331eb4e3b289a5f39b150a1b3baa0c197baccb84
jjmin9797 <jjmin9797@naver.com>
2022-11-14 17:53:36 +0900

    feat(Application): 메인 메서드 로직 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,2 +1,19 @@
-package lotto;public class LottoApplication {
+package lotto;
+
+import lotto.controller.UserController;
+import lotto.view.GameProgressView;
+import lotto.view.WinningStatsView;
+
+public class LottoApplication {
+    private static UserController userController = new UserController();
+
+    public static void run() {
+        userController.savePurchaseAmount();
+        userController.showPurchaseInfo();
+        userController.randomLotteryIssuanceAndShow();
+        userController.enterWinningNumbersAndSave();
+        userController.enterBonusNumberAndSave();
+        userController.showWinningStatisticsAndSave();
+        userController.showRateOfReturnAndSave();
+    }
 }
