e821ef44cc0eb5c0c0881361bb21abcb52525508
hong-sile <gurwns9325@naver.com>
2022-11-14 18:48:51 +0900

    refactor(OutputView): 코드의 가독성을 높임
    무의미하게 출력메세지를 저장했던 상수를 없앰
    print문에 포함되어 있던 수를 계산하는 로직을 print문 밖으로 빼내어 가독성 높임

--- src/main/java/lotto/view/OutputViewConsole.java
+++ src/main/java/lotto/view/OutputViewConsole.java
@@ -5,19 +5,18 @@ import lotto.domain.Lottery;
 import lotto.domain.Rank;
 
 public class OutputViewConsole implements OutputView {
-    private static final String ASK_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String ASK_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String ASK_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-
     @Override
     public void printGeneratedLottery(Integer purchaseAmount, String lotteryInfo) {
-        System.out.println(purchaseAmount / Lottery.LOTTO_PRICE + "개를 구매했습니다.");
+        int lotterySize = purchaseAmount / Lottery.LOTTO_PRICE;
+        System.out.println(lotterySize + "개를 구매했습니다.");
         System.out.println(lotteryInfo);
     }
 
     @Override
     public void printWinningHistory() {
         DecimalFormat decimalFormat = new DecimalFormat("###,###");
+        System.out.println("당첨 통계");
+        System.out.println("---");
         for (Rank rank : Rank.values()) {
             System.out.print(rank.getMatchCount() + "개 일치");
             if (rank.isBonusMatch()) {
@@ -29,26 +28,28 @@ public class OutputViewConsole implements OutputView {
 
     @Override
     public void printProfitRatio(Integer purchaseAmount) {
-        DecimalFormat ratioFormat = new DecimalFormat("###,###.#%");
+        DecimalFormat ratioFormat = new DecimalFormat("###,###.0%");
+        double profitRatio;
         double profit = 0;
         for (Rank rank : Rank.values()) {
             profit += rank.getCount() * rank.getPrize();
         }
-        System.out.printf("총 수익률은 %s입니다.",ratioFormat.format(profit / purchaseAmount));
+        profitRatio = profit / purchaseAmount;
+        System.out.printf("총 수익률은 %s입니다.", ratioFormat.format(profitRatio));
     }
 
     @Override
     public void printAskPurchaseAmountMessage() {
-        System.out.println(ASK_PURCHASE_AMOUNT_MESSAGE);
+        System.out.println("구입금액을 입력해 주세요.");
     }
 
     @Override
     public void printAskWinningNumbersMessage() {
-        System.out.println(ASK_WINNING_NUMBERS_MESSAGE);
+        System.out.println("당첨 번호를 입력해 주세요.");
     }
 
     @Override
     public void printAskBonusNumberMessage() {
-        System.out.println(ASK_BONUS_NUMBER_MESSAGE);
+        System.out.println("보너스 번호를 입력해 주세요.");
     }
 }
