fc03fe86b00b4f4e6d1b038abeeb41bb74cb98c4
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-14 21:57:03 +0900

    refactoring : View 결과 출력문 최종 수정

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -40,10 +40,10 @@ public class View {
             for (int i = 0; i < values.length; i++) {
                 if (i == 3) {
                     System.out.println(values[i].getSameNumber() + "개 일치, 보너스 볼 일치 "
-                            + "(" + df.format(values[i].getPrizeMoney()) + ")" + " - " + values[i].getCount() + "개");
+                            + "(" + df.format(values[i].getPrizeMoney()) + "원)" + " - " + values[i].getCount() + "개");
                 } else {
                     System.out.println(values[i].getSameNumber() + "개 일치 "
-                            + "(" + df.format(values[i].getPrizeMoney()) + ")" + " - " + values[i].getCount() + "개");
+                            + "(" + df.format(values[i].getPrizeMoney()) + "원)" + " - " + values[i].getCount() + "개");
                 }
             }
             System.out.println("총 수익률은 " + yield + "%입니다.");
