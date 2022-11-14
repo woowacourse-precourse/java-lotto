f0d9bd0f9e57019d7399869c6ea8b409eef4650b
drunkenhw <drunkenhw@gmail.com>
2022-11-14 23:05:59 +0900

    test(money): 금액을 나누는 테스트 추가

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -10,7 +10,7 @@ class MoneyTest {
     @DisplayName("로또 구입 금액 단위가 아니면 예외를 발생한다.")
     @Test
     void buyLottoEx() {
-        Money money = new Money(12500);
+        Money money = Money.of(12500);
 
         assertThatThrownBy(money::exchangeLotto).isInstanceOf(IllegalArgumentException.class);
     }
@@ -18,10 +18,21 @@ class MoneyTest {
     @DisplayName("로또 구입 금액만큼 로또 갯수를 반환한다.")
     @Test
     void buyLotto() {
-        Money money = new Money(12000);
+        Money money = Money.of(12000);
 
         int lottos = money.exchangeLotto();
 
         assertThat(lottos).isEqualTo(12);
+    }
+
+    @DisplayName("금액끼리 나눈다.")
+    @Test
+    void divide() {
+        Money money = Money.of(10);
+        Money other = Money.of(2);
+
+        double result = money.divide(other);
+
+        assertThat(result).isEqualTo(5);
     }
 }
