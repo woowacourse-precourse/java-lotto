faa33ad927c2d0c23d9f8a3162661b7d01899263
gomljo <gomljo0307@naver.com>
2022-11-15 01:15:05 +0900

    feat(Rank.java): 로또 번호가 추첨 번호를 맞춘 상태를 정의한 보상표와 비교하는 기능 구현

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -7,7 +8,7 @@ import java.util.List;
 public enum Rank {
     FIRST(List.of(6,0), 2000000000),
     SECOND(List.of(5,1),30000000),
-    THIRD(List.of(5,0),30000000),
+    THIRD(List.of(5,0),1500000),
     FOURTH(List.of(4,0),50000),
     FIFTH(List.of(3,0), 5000),
     NOTHING(Collections.EMPTY_LIST,0);
@@ -18,7 +19,8 @@ public enum Rank {
         this.REVENUE=revenue;
         this.NUMBER_OF_MATCHES=numberOfMatches;
     }
-
-
+    public static boolean isSame(Rank rank, List<Integer> matches){
+        return Arrays.equals(rank.NUMBER_OF_MATCHES.toArray(), matches.toArray());
+    }
 
 }
