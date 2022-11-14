feb55163fd1a9555ab00ef7f71a75ea024b31fbf
dohyun <ldh19738@gmail.com>
2022-11-14 09:44:50 +0900

    feat(Win): 당첨 통계 결과 출력하는 기능 추가

--- src/main/java/lotto/Win.java
+++ src/main/java/lotto/Win.java
@@ -1,61 +1,24 @@
 package lotto;
 
-import java.util.List;
+import java.text.DecimalFormat;
 
 public class Win {
-    private int firstPlace;
-    private int secondPlace;
-    private int thirdPlace;
-    private int fourthPlace;
-    private int fifthPlace;
+    private static final String WIN_STATISTICS_MESSAGE = "당첨 통계";
+    private static final String PUTTING_COMMAS_IN_THREE_NUMBERS = "###,###";
 
-    public Win() {
-        this.firstPlace = 0;
-        this.secondPlace = 0;
-        this.thirdPlace = 0;
-        this.fourthPlace = 0;
-        this.fifthPlace = 0;
+    public void statisticsMessage() {
+        System.out.println('\n' + WIN_STATISTICS_MESSAGE);
+        System.out.println("---");
     }
 
-    public void threeMatchMessage() {
-        System.out.println("3개 일치 (5,000원) - ");
-    }
+    private void printStatistics(CountNumberOfWins countNumberOfWins) {
+        DecimalFormat decimalFormat = new DecimalFormat(PUTTING_COMMAS_IN_THREE_NUMBERS);
 
-    public void fourMatchMessage() {
-        System.out.println("4개 일치 (50,000원) - ");
-    }
-
-    public void fiveMatchMessage() {
-        System.out.println("5개 일치 (1,500,000원) - ");
-    }
-
-    public void fiveAndBonusMatchMessage() {
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - ");
-    }
-
-    public void sixMatchMessage() {
-        System.out.println("6개 일치 (2,000,000,000원) - ");
-    }
-
-    public int numberThatMatchesWinNumber(List<Integer> lottoNumbers, List<Integer> winNumbers) {
-        int matchCount = 0;
-
-        for (int winNumber : winNumbers) {
-            if (lottoNumbers.contains(winNumber)) {
-                matchCount++;
-            }
-        }
-
-        return matchCount;
-    }
-
-    public int numberThatMatchesBonusNumber(List<Integer> lottoNumbers, int bonusNumber) {
-        int bonusMatchCount = 0;
-
-        if (lottoNumbers.contains(bonusNumber)) {
-            bonusMatchCount++;
-        }
-
-        return bonusMatchCount;
+        System.out.println("3개 일치 (" + decimalFormat.format(Grade.FIFTH.getPrizeMoney()) + "원) - " + countNumberOfWins.getFifthPlace() + "개");
+        System.out.println("4개 일치 (" + decimalFormat.format(Grade.FOURTH.getPrizeMoney()) + "원) - " + countNumberOfWins.getFourthPlace() + "개");
+        System.out.println("5개 일치 (" + decimalFormat.format(Grade.THIRD.getPrizeMoney()) + "원) - " + countNumberOfWins.getThirdPlace() + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (" + decimalFormat.format(Grade.SECOND.getPrizeMoney()) + "원) - " + countNumberOfWins.getSecondPlace() + "개");
+        System.out.println("6개 일치 (" + decimalFormat.format(Grade.FIRST.getPrizeMoney()) +"원) - " + countNumberOfWins.getFirstPlace() + "개");
+        System.out.println("총 수익률은 " + countNumberOfWins.rateOfReturn() + "%입니다.");
     }
 }
--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -6,12 +6,18 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 
 public class WinningNumbers {
+    private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String WINNING_NUMBER_CLASSIFICATION_CRITERIA = ",";
+    private static final String INVALIDATE_RANGE_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String INVALIDATE_DUPLICATION = "[ERROR] 중복되는 수를 입력할 수 없습니다.";
+
     private void inputWinningNumberMessage() {
-        System.out.println("당첨 번호를 입력해 주세요");
+        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
     }
 
     private void inputBonusNumberMessage() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
     }
 
     public List<Integer> input() {
@@ -19,11 +25,11 @@ public class WinningNumbers {
 
         List<Integer> winningNumbers = new ArrayList<>();
         String inputNumber = Console.readLine();
-        String[] inputNumberArr = inputNumber.split(",");
+        String[] inputNumberArr = inputNumber.split(WINNING_NUMBER_CLASSIFICATION_CRITERIA);
 
         for (String number : inputNumberArr) {
-            inputRangeException(Integer.parseInt(number));
-            inputDuplicationException(winningNumbers, Integer.parseInt(number));
+            validateRange(Integer.parseInt(number));
+            validateDuplication(winningNumbers, Integer.parseInt(number));
             winningNumbers.add(Integer.parseInt(number));
         }
 
@@ -35,20 +41,20 @@ public class WinningNumbers {
 
         int bonusNumber = Integer.parseInt(Console.readLine());
 
-        inputRangeException(bonusNumber);
+        validateRange(bonusNumber);
 
         return bonusNumber;
     }
 
-    private void inputRangeException(int number) {
+    private void validateRange(int number) {
         if(number < 1 || number > 45) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(INVALIDATE_RANGE_MESSAGE);
         }
     }
 
-    private void inputDuplicationException(List<Integer> winning, int number) {
+    private void validateDuplication(List<Integer> winning, int number) {
         if (winning.contains(number)) {
-            throw new IllegalArgumentException("[ERROR] 중복되는 수를 입력할 수 없습니다.");
+            throw new IllegalArgumentException(INVALIDATE_DUPLICATION);
         }
     }
 }
