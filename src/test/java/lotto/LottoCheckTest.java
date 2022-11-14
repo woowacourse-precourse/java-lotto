7bba0dd7d399ca1d51f31b3f38c0bfb7121b9a83
kylew1004 <kylew1004@gmail.com>
2022-11-14 17:26:26 +0900

    test: 당첨번호에 따른 테스트 케이스 추가

---
+++ src/test/java/lotto/LottoCheckTest.java
@@ -0,0 +1,82 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoCheckTest {
+
+    void createLottoCheckByOverRangeBonusNumber() {
+        Lotto winningNum = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNums = 47;
+
+        assertThatThrownBy(() -> new LottoCheck(winningNum, bonusNums))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    void createLottoCheckByByDuplicateWithBonusNumber() {
+        Lotto winningNum = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNums = 5;
+
+        assertThatThrownBy(() -> new LottoCheck(winningNum, bonusNums))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    void compareByLoseNumber() {
+        Lotto winningNum = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNums = 10;
+        LottoCheck lottoCheck = new LottoCheck(winningNum, bonusNums);
+
+        Lotto loseNum = new Lotto(List.of(5, 6, 7, 8, 9, 10));
+        assertThat(lottoCheck.getRank(loseNum)).isEqualTo(LottoRank.LOSE);
+    }
+
+    void compareByFifthPlaceNumber() {
+        Lotto winningNum = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNums = 10;
+        LottoCheck lottoCheck = new LottoCheck(winningNum, bonusNums);
+
+        Lotto loseNum = new Lotto(List.of(5, 6, 1, 8, 9, 10));
+        assertThat(lottoCheck.getRank(loseNum)).isEqualTo(LottoRank.FIFTH);
+    }
+
+    void compareByFourthPlaceNumber() {
+        Lotto winningNum = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNums = 10;
+        LottoCheck lottoCheck = new LottoCheck(winningNum, bonusNums);
+
+        Lotto loseNum = new Lotto(List.of(1, 3, 5, 6, 9, 10));
+        assertThat(lottoCheck.getRank(loseNum)).isEqualTo(LottoRank.FOURTH);
+    }
+    
+    void compareByThirdPlaceNumber() {
+        Lotto winningNum = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNums = 10;
+        LottoCheck lottoCheck = new LottoCheck(winningNum, bonusNums);
+
+        Lotto loseNum = new Lotto(List.of(1, 3, 4, 5, 6, 12));
+        assertThat(lottoCheck.getRank(loseNum)).isEqualTo(LottoRank.THIRD);
+    }
+
+    void compareBySecondPlaceNumber() {
+        Lotto winningNum = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNums = 10;
+        LottoCheck lottoCheck = new LottoCheck(winningNum, bonusNums);
+
+        Lotto loseNum = new Lotto(List.of(1, 3, 4, 5, 6, 10));
+        assertThat(lottoCheck.getRank(loseNum)).isEqualTo(LottoRank.SECOND);
+    }
+
+    void compareByFirstPlaceNumber() {
+        Lotto winningNum = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNums = 10;
+        LottoCheck lottoCheck = new LottoCheck(winningNum, bonusNums);
+
+        Lotto loseNum = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lottoCheck.getRank(loseNum)).isEqualTo(LottoRank.FIRST);
+    }
+}
