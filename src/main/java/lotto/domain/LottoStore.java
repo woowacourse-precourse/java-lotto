5b3077c3a8a41914a5ab13a5b1f2659369562c86
ryanha <ryanha@handong.ac.kr>
2022-11-14 22:38:14 +0900

    refactor(domain): 로또의 가격을 내부적인 상수가 아닌 Policy에서 참조하도록 개선

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -1,15 +1,10 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import lotto.view.IOProcessor;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoStore {
-
-    static final int PRICE = 1000; // 로또의 가격
-
     private final List<Lotto> lottoBoard; // 발행한 로또정보
 
     public LottoStore() {
@@ -18,7 +13,7 @@ public class LottoStore {
 
     public void purchaseLotto(int amount) {
         lottoBoard.clear(); // 구매하기 이전 로또정보를 초기화 함
-        int count = amount / PRICE; // 구매할 수 있는 로또의 수를 구함
+        int count = amount / Policy.PRICE; // 구매할 수 있는 로또의 수를 구함
         for (int i = 0; i < count; i++) {
             lottoBoard.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
         }
@@ -26,9 +21,5 @@ public class LottoStore {
 
     public List<Lotto> getPurchasedLotto() {
         return this.lottoBoard;
-    }
-
-    public int getPrice() {
-        return this.PRICE;
     }
 }
