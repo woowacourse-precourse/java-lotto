f50a30b65521a6ae2ce1f4aab1ceb5728aa14c13
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-15 05:03:42 +0900

    test: count tickets by the number of matched numbers

--- src/test/java/lotto/LottoResultTest.java
+++ src/test/java/lotto/LottoResultTest.java
@@ -1,9 +1,11 @@
 package lotto;
 
+import com.sun.source.tree.AssertTree;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,6 +19,18 @@ class LottoResultTest {
         int result = LottoResult.compareLotteryNumbers(winningNumbers, ticketNumbers);
 
         assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void compareResultTest() {
+        List<Integer> winningNumbers = new ArrayList<>(Arrays.asList(4, 8, 10, 19, 28, 40));
+        List<List<Integer>> ticketNumbers = new ArrayList<>(Arrays.asList(Arrays.asList(1, 4, 8, 10, 19, 45),//4
+                Arrays.asList(4, 8, 10, 19, 28, 40), Arrays.asList(4, 8, 9, 10, 19, 31)));
+        LinkedHashMap<Integer, Integer> result = new LinkedHashMap<>();
+
+        result = LottoResult.compareResult(winningNumbers, ticketNumbers);
+        System.out.println(result);
+        assert(result.get(4)).equals(2);
     }
 
 }
