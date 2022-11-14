111fc69d511b82fdb36e479b8a134c6668b8537e
ingpyo <juninpyo72@gmail.com>
2022-11-14 11:44:56 +0900

    feat:당첨된 로또 개수 출력

--- src/main/java/lotto/PrintMessage.java
+++ src/main/java/lotto/PrintMessage.java
@@ -1,8 +1,12 @@
 package lotto;
 
+import static lotto.LottoRank.*;
+
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class PrintMessage {
 	public void purchasePriceMessage() {
@@ -19,6 +23,21 @@ public class PrintMessage {
 
 	public void InputNumberMessage() {
 		System.out.println(Constant.NUMBER_MESSAGE_QUESTION);
+	}
+
+	public void printResultPrize(Map<Integer, Integer> prize) {
+		System.out.println(Constant.WINNING_STATISTICS);
+		System.out.println(Constant.WINNING_STATISTICS_DIVISION);
+		System.out.printf(Constant.WIN_MESSAGE_FRAME, FIFTH_PLACE.getSameNumber(),
+			NumberFormat.getInstance().format(FIFTH_PLACE.getWinnerPrize()), prize.get(FIFTH_PLACE.getWinnerPrize()));
+		System.out.printf(Constant.WIN_MESSAGE_FRAME, FOURTH_PLACE.getSameNumber(),
+			NumberFormat.getInstance().format(FOURTH_PLACE.getWinnerPrize()), prize.get(FOURTH_PLACE.getWinnerPrize()));
+		System.out.printf(Constant.WIN_MESSAGE_FRAME, THIRD_PLACE.getSameNumber(),
+			NumberFormat.getInstance().format(THIRD_PLACE.getWinnerPrize()), prize.get(THIRD_PLACE.getWinnerPrize()));
+		System.out.printf(Constant.WIN_BONUS_MESSAGE_FRAME, SECOND_PLACE.getSameNumber(),
+			NumberFormat.getInstance().format(SECOND_PLACE.getWinnerPrize()), prize.get(SECOND_PLACE.getWinnerPrize()));
+		System.out.printf(Constant.WIN_MESSAGE_FRAME, FIRST_PLACE.getSameNumber(),
+			NumberFormat.getInstance().format(FIRST_PLACE.getWinnerPrize()), prize.get(FIRST_PLACE.getWinnerPrize()));
 	}
 
 	public void printPurchaseLotto(List<List<Integer>> purchaseLotto) {
