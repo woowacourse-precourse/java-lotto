0edf3007dca00f194370c57a63563c18b09b7bff
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-11-14 23:54:15 +0900

    feat: All readInput Method and InputValidate Method

--- src/main/java/lotto/IOController.java
+++ src/main/java/lotto/IOController.java
@@ -2,7 +2,12 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class IOController {
+
     public static int readBuyingMoney() {
         String input = Console.readLine();
         int buyingMoney;
@@ -16,4 +21,37 @@ public class IOController {
         }
         return buyingMoney / 1000;
     }
+
+    public static List<Integer> readWinningLotto() throws IllegalArgumentException {
+        String input = Console.readLine();
+        String pattern = "^([1-9]?[0-9],){5}[1-9]?[0-9]$";
+        if (!Pattern.matches(pattern, input)) {
+            throw new IllegalArgumentException("[ERROR] 쉼표로 구분된 6개의 숫자를 입력해야 합니다.");
+        }
+        String[] splitedInputs = input.split(",");
+        List<Integer> lottoNumbers = new ArrayList<>();
+        for (String splitedInput : splitedInputs) {
+            int lottoNumber = Integer.parseInt(splitedInput);
+            validateNumberRange(lottoNumber);
+            lottoNumbers.add(lottoNumber);
+        }
+        return lottoNumbers;
+    }
+
+    public static int readBonusNumber() throws IllegalArgumentException {
+        String input = Console.readLine();
+        String pattern = "^[1-9]?[0-9]$";
+        if (!Pattern.matches(pattern, input)) {
+            throw new IllegalArgumentException("[ERROR] 최대 두자리 숫자를 입력해야 합니다.");
+        }
+        validateNumberRange(Integer.parseInt(input));
+        return Integer.parseInt(input);
+    }
+
+    public static void validateNumberRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
 }
