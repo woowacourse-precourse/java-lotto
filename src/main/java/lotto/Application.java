cd8a20860b9f3f679793987dff941e13d9e5ab6a
im-shung <seoyoung1113@gmail.com>
2022-11-15 05:19:05 +0900

    refactor: :recycle: GameRunner 클래스 생성해 main 함수 로직 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,39 +1,9 @@
 package lotto;
 
-import static lotto.view.InputView.*;
-import static lotto.view.OutputView.*;
-
-import java.util.List;
-import lotto.domain.Lotto;
-import lotto.domain.Player;
-import lotto.domain.PrizeCount;
-import lotto.domain.Profit;
-import lotto.domain.Purchase;
-
 public class Application {
 
     public static void main(String[] args) {
-        askPurchaseAmount();
-
-        Purchase purchase = new Purchase(getInt());
-        purchase.printCount();
-
-        List<Lotto> lottos = purchase.createLottos();
-        printLottos(lottos);
-
-        int lottoBonusNumber = purchase.createLottoBonusNumber();
-
-        askNumbers();
-        List<Integer> numbers = getNumbers();
-
-        askBonusNumber();
-        int bonusNumber = getInt();
-
-        PrizeCount prizeCount = new PrizeCount();
-        Player player = new Player(numbers, bonusNumber);
-
-        player.compare(prizeCount, lottos, lottoBonusNumber);
-        Profit profit = new Profit(prizeCount, purchase.getAmount());
-
+        GameRunner gameRunner = new GameRunner();
+        gameRunner.start();
     }
 }
---
+++ src/main/java/lotto/GameRunner.java
@@ -0,0 +1,82 @@
+package lotto;
+
+import static lotto.view.InputView.getInt;
+import static lotto.view.InputView.getNumbers;
+import static lotto.view.OutputView.askBonusNumber;
+import static lotto.view.OutputView.askNumbers;
+import static lotto.view.OutputView.askPurchaseAmount;
+import static lotto.view.OutputView.printLottos;
+import static lotto.view.OutputView.printWinningStatisticsMessage;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.Player;
+import lotto.domain.PrizeCount;
+import lotto.domain.Profit;
+import lotto.domain.Purchase;
+
+public class GameRunner {
+
+    private Purchase purchase;
+    private PrizeCount prizeCount;
+    private Player player;
+    private Profit profit;
+
+    private List<Lotto> lottos;
+    private List<Integer> numbers;
+    private int lottoBonusNumber;
+    private int bonusNumber;
+
+    public void start() {
+        inputPurchaseAmount();
+
+        purchaseLottos();
+
+        inputNumbers();
+
+        inputBonusNumber();
+
+        makeResult();
+
+        printResult();
+    }
+
+    private void inputPurchaseAmount() {
+        askPurchaseAmount();
+
+        purchase = new Purchase(getInt());
+        purchase.printCount();
+    }
+
+    private void purchaseLottos() {
+        lottos = purchase.createLottos();
+        printLottos(lottos);
+
+        lottoBonusNumber = purchase.createLottoBonusNumber();
+    }
+
+    private void inputNumbers() {
+        askNumbers();
+        numbers = getNumbers();
+    }
+
+    private void inputBonusNumber() {
+        askBonusNumber();
+        bonusNumber = getInt();
+    }
+
+    private void makeResult() {
+        prizeCount = new PrizeCount();
+
+        player = new Player(numbers, bonusNumber);
+        player.compare(prizeCount, lottos, lottoBonusNumber);
+
+        profit = new Profit(prizeCount, purchase.getAmount());
+    }
+
+    private void printResult() {
+        printWinningStatisticsMessage();
+        System.out.println(prizeCount);
+        System.out.println(profit);
+    }
+}
