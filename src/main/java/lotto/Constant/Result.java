31cd98a5cd8d89cf45d8594c98fc5ff48ebe9a3b
aossuper7 <aossuper7@naver.com>
2022-11-15 00:56:06 +0900

    feat: 로또 결과 enum 적용

---
+++ src/main/java/lotto/Constant/Result.java
@@ -0,0 +1,49 @@
+package lotto.Constant;
+
+import java.util.function.IntConsumer;
+import java.util.function.IntToLongFunction;
+
+public enum Result {
+    THREE("3", value -> value * 5_000L,
+        count -> {
+            System.out.println("3개 일치 (5,000원) - " + count + "개");
+        }),
+    FOUR("4", value -> value * 50_000L,
+        count -> {
+            System.out.println("4개 일치 (50,000원) - " + count + "개");
+        }),
+    FIVE("5", value -> value * 1_500_000L,
+        count -> {
+            System.out.println("5개 일치 (1,500,000원) - " + count + "개");
+        }),
+    FIVE_B("5B", value -> value * 30_000_000L,
+        count -> {
+            System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + count + "개");
+        }),
+    SIX("6", value -> value * 2_000_000_000L,
+        count -> {
+            System.out.println("6개 일치 (2,000,000,000원) - " + count + "개");
+        });
+
+    private String matchNumber;
+    private IntToLongFunction countingMoney;
+    private IntConsumer print;
+
+    Result(String matchNumber, IntToLongFunction countingMoney, IntConsumer print) {
+        this.matchNumber = matchNumber;
+        this.countingMoney = countingMoney;
+        this.print = print;
+    }
+
+    public String getMatchNumber() {
+        return matchNumber;
+    }
+
+    public long calculrate(int value) {
+        return countingMoney.applyAsLong(value);
+    }
+
+    public void printResult(int count) {
+        print.accept(count);
+    }
+}
