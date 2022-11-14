2830674daf92ab5ff62f1324e7991374554b431d
hyun-seung <ardimento22@naver.com>
2022-11-14 22:43:38 +0900

    [REFACTOR] 값을 하드코딩하지 않는다.

--- src/main/java/lotto/utils/Validation.java
+++ src/main/java/lotto/utils/Validation.java
@@ -2,6 +2,9 @@ package lotto.utils;
 
 public class Validation {
 
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_LAST_NUMBER = 45;
+
     public static void validateIsNumber(String target) {
         String numberRegex = "^[0-9]*$";
         if (!target.matches(numberRegex)) {
@@ -10,7 +13,7 @@ public class Validation {
     }
 
     public static void validateNumberInLottoRange(int number) {
-        if (number < 1 || number > 45) {
+        if (number < LOTTO_START_NUMBER || number > LOTTO_LAST_NUMBER) {
             throw new IllegalArgumentException(ExceptionType.NOT_WITHIN_THE_RANGE.getMessage());
         }
     }
