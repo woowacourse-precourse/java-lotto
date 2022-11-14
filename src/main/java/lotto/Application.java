1708aa9907156dffbace9d09b4911ab22a4955ad
alswn <alswn9938@gmail.com>
2022-11-15 01:03:01 +0900

    feat(Application): add 에러 처리 관련 로직

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,7 +5,11 @@ import lotto.controller.LottoController;
 public class Application {
 
     public static void main(String[] args) {
-        LottoController game = new LottoController();
-        game.start();
+        try {
+            LottoController game = new LottoController();
+            game.start();
+        } catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
