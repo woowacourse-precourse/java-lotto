13d50264ad018e5c86bfe97e0ba8f6381a04b43b
This2sho <zx00018@naver.com>
2022-11-14 21:34:33 +0900

    test(Salesman): 로또 구입 금액이 1000원보다 작은 경우 예외 발생 검증

--- src/test/java/lotto/domain/SalesmanTest.java
+++ src/test/java/lotto/domain/SalesmanTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SalesmanTest {
@@ -23,5 +24,13 @@ public class SalesmanTest {
 		int expected = 3;
 	    //then
 		assertThat(lottoCount).isEqualTo(expected);
+	}
+
+	@DisplayName("로또 구입 금액이 1000원 보다 작으면 예외가 발생한다.")
+	@Test
+	public void 작은_구입_금액_테스트() {
+		int purchaseBudget = 500;
+		assertThatThrownBy(() -> salesman.sellLotto(purchaseBudget))
+			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
