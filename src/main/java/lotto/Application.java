ca363fa313b7258d0d701010feafb528f1687223
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-14 16:16:20 +0900

    <refactor>(Application.java): main 함수 기능 분리
     - manageLottoProcess() 메서드로 게임 진행 과정 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,28 +7,27 @@ public class Application {
     static UI ui = new UI();
     private static final String ERROR_MESSAGE = "[ERROR] ";
 
-
     public static void main(String[] args) {
         try {
-            // 1. 구입 금액 입력받기
-            String moneyInput = ui.getInputMoney();
-            domain.validatePriceInput(moneyInput);
-            int numberOfLottos = Integer.parseInt(moneyInput) / 1000;
-            // 2. 자동생성된 로또번호 리스트 저장 후 출력하기
-            List<Lotto> lottos = domain.createRandomLottoNumbers(numberOfLottos);
-            ui.printLottoNumber(lottos);
-            // 3. 당첨 번호 입력받기
-            String winningNumberInput = ui.getInputWinningNumber();
-            domain.validateWinningNumberInput(winningNumberInput);
-            List<Integer> winningNumbers = domain.trimLottoNumberInput(winningNumberInput);
-            // 4. 보너스 번호 입력받기
-            String bonusNumberInput = ui.getInputBonusNumber();
-            domain.validateNumberRange(bonusNumberInput);
-            int bonusNumber = Integer.parseInt(bonusNumberInput);
-            // 5. 당첨 통계, 수익률 출력하기
-            ui.printWinningStats(domain.getRankList(lottos, winningNumbers, bonusNumber), numberOfLottos);
+            manageLottoProcess();
         } catch (IllegalArgumentException e) {
             System.out.println(ERROR_MESSAGE + e.getMessage());
+            return;
         }
+    }
+
+    private static void manageLottoProcess() {
+        String moneyInput = ui.getInputMoney();
+        domain.validatePriceInput(moneyInput);
+        int numberOfLottos = Integer.parseInt(moneyInput) / 1000;
+        List<Lotto> lottos = domain.createRandomLottoNumbers(numberOfLottos);
+        ui.printLottoNumber(lottos);
+        String winningNumberInput = ui.getInputWinningNumber();
+        domain.validateWinningNumberInput(winningNumberInput);
+        List<Integer> winningNumbers = domain.trimLottoNumberInput(winningNumberInput);
+        String bonusNumberInput = ui.getInputBonusNumber();
+        domain.validateNumberRange(bonusNumberInput);
+        int bonusNumber = Integer.parseInt(bonusNumberInput);
+        ui.printWinningStats(domain.getRankList(lottos, winningNumbers, bonusNumber), numberOfLottos);
     }
 }
