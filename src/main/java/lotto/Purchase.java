73a0a74a170d1b9393eee194fe1dc1e8714602e5
syahn <496300@naver.com>
2022-11-15 01:14:50 +0900

    feat: 로또 구매 개수 확인 기능 구현
    - InputView에서 입력받은 로또 금액에 대해 구매 개수를 확인함

---
+++ src/main/java/lotto/Purchase.java
@@ -0,0 +1,24 @@
+package lotto;
+
+public class Purchase {
+    private String input;
+    private int money;
+    private int lottoCount;
+
+    public Purchase(int money) {
+        this.money = money;
+        checkLottoCount();
+    }
+
+    void checkLottoCount() {
+        this.lottoCount = money / 1000;
+    }
+
+    public int getLottoCount() {
+        return this.lottoCount;
+    }
+
+    public int getCost() {
+        return this.money;
+    }
+}
