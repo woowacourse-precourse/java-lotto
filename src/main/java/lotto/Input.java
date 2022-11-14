594be4f35a9e22f7a2f4639ca16303528b72c154
rabbit0216 <rabbit1999k@gmail.com>
2022-11-14 19:12:16 +0900

    feat: 로또 구입 금액 체크하는 함수 구현

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -8,4 +8,30 @@ public class Input {
         inputValidate(money);
         return Integer.parseInt(money);
     }
+
+    void inputValidate(String money) {
+        if (!isNumeric(money)) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+        }
+
+        int convertedMoney = Integer.parseInt(money);
+        if (!divideByThousand(convertedMoney)) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000 단위여야 합니다.");
+        }
+    }
+
+    boolean isNumeric(String money) {
+        boolean answer = true;
+        for(int idx = 0; idx < money.length(); idx++){
+            if(!Character.isDigit(money.charAt(idx))){
+                answer = false;
+            }
+        }
+        return answer;
+    }
+
+    boolean divideByThousand(int money) {
+        int rem = money % 1000;
+        return rem == 0;
+    }
 }
