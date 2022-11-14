fd8c0be372aef22c6819f9dac31f76dfff7873bc
seonpilKim <ksp970306@gmail.com>
2022-11-14 17:06:31 +0900

    Feat: Seller 클래스 추가
    금액에 맞게 로또 복권을 판매하는 객체
    로또 가격, 안내 멘트를 관리

---
+++ src/main/java/lotto/domain/Seller.java
@@ -0,0 +1,9 @@
+package lotto.domain;
+
+public class Seller {
+
+	public static final int LOTTO_PRICE = 1_000;
+	public static final String SELL_ANNOUNCEMENT = "구입금액을 입력해 주세요.";
+	public static final String PURCHASE_ANNOUNCEMENT = "%s개를 구매했습니다.";
+
+}
