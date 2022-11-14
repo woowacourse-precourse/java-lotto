5e2679cbd400fa22aef058a18a323bbc32267f2a
dener8 <8dener@gmail.com>
2022-11-14 22:20:47 +0900

    feat(Lotto): 입력된 당첨번호에 중복된 수가 있는지 확인하는 validateDuplicate() 구현
    모든 예외를 한번에 확인하는 validate()에 validateDuplicate()를 삽입함

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,11 @@
 package lotto;
 
+import java.nio.charset.IllegalCharsetNameException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static lotto.ErrorMessage.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +16,34 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
     }
 
     // TODO: 추가 기능 구현
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(OUT_OF_SIZE_ERROR);
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalCharsetNameException(OUT_OF_RANGE_ERROR);
+            }
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> singleNumbers = new HashSet<>(numbers);
+        if (singleNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR);
+        }
+    }
+
+
+
+
 }
