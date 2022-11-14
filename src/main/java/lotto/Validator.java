d4644756c02036bff7efc62840f163b5323cc527
Laeng <Laeng@users.noreply.github.com>
2022-11-14 14:27:01 +0900

    feat(lotto): 입력 값 검증을 위한 로직 추가
    숫사 갯수, 대량의 숫자 범위, 특정 숫자 범위, 잔돈 여부, 숫자 음수 여부를 확인할 수 있는 정적 메서드를 추가하였습니다.
    Validator 클래스에서는 검증을 위한 로직을 구현하고, 검증을 위해 사용되는 상수들은 Env 클래스에 선언해주십시오.

---
+++ src/main/java/lotto/Validator.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Validator {
+    public static boolean isNotCorrectLottoNumberSize(List<Integer> numbers, boolean deduplicate) {
+        numbers = new LinkedList<>(numbers);
+
+        if (deduplicate) {
+            numbers = numbers
+                    .stream()
+                    .distinct()
+                    .collect(Collectors.toList());
+        }
+
+        return (numbers.size() != Env.LOTTO_NUMBERS);
+    }
+
+    public static boolean hasNotCorrectRangeNumbers(List<Integer> numbers) {
+        return numbers
+                .stream()
+                .anyMatch(number -> hasNotCorrectRangeNumber(number));
+    }
+
+    public static boolean hasNotCorrectRangeNumber(int number) {
+        return (number < Env.LOTTO_FIRST_NUMBER || number > Env.LOTTO_LAST_NUMBER);
+    }
+
+    public static boolean hasLeftMoneyAfterBuyLotto(int money) {
+        return (money % Env.LOTTO_PRICE) != 0;
+    }
+
+    public static boolean isNegativeNumber(int number) {
+        return number < 0;
+    }
+}
---
+++ src/test/java/lotto/ValidatorTest.java
@@ -0,0 +1,89 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ValidatorTest {
+    @ParameterizedTest(name = "숫자 갯수 테스트")
+    @MethodSource("isNotCorrectLottoNumberSizeData")
+    void isNotCorrectLottoNumberSize(List<Integer> numbers, boolean deduplicate, boolean result) {
+        assertThat(Validator.isNotCorrectLottoNumberSize(numbers, deduplicate)).isEqualTo(result);
+    }
+
+    static Stream<Arguments> isNotCorrectLottoNumberSizeData() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2), false, true),
+                Arguments.of(List.of(1, 1, 1, 1, 1, 1), false, false),
+                Arguments.of(List.of(1, 1, 1, 1, 1, 1), true, true),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), true, false)
+        );
+    }
+
+    @ParameterizedTest(name = "대량의 숫자 범위 테스트")
+    @MethodSource("hasNotCorrectRangeNumbersData")
+    void hasNotCorrectRangeNumbers(List<Integer> numbers, boolean result) {
+        assertThat(Validator.hasNotCorrectRangeNumbers(numbers)).isEqualTo(result);
+    }
+
+    static Stream<Arguments> hasNotCorrectRangeNumbersData() {
+        return Stream.of(
+                Arguments.of(List.of(-1, 2), true),
+                Arguments.of(List.of(1, 47), true),
+                Arguments.of(List.of(1, 45), false),
+                Arguments.of(List.of(2, 43), false)
+        );
+    }
+
+    @ParameterizedTest(name = "특정 숫자 범위 테스트")
+    @MethodSource("hasNotCorrectRangeNumberData")
+    void hasNotCorrectRangeNumber() {
+
+    }
+
+    static Stream<Arguments> hasNotCorrectRangeNumberData() {
+        return Stream.of(
+                Arguments.of(56, false),
+                Arguments.of(-1, false),
+                Arguments.of(1, true),
+                Arguments.of(45, true),
+                Arguments.of(2, true)
+        );
+    }
+
+    @ParameterizedTest(name = "잔돈이 남는지 테스트")
+    @MethodSource("hasLeftMoneyAfterBuyLottoData")
+    void hasLeftMoneyAfterBuyLotto(int money, boolean result) {
+        assertThat(Validator.hasLeftMoneyAfterBuyLotto(money)).isEqualTo(result);
+    }
+
+    static Stream<Arguments> hasLeftMoneyAfterBuyLottoData() {
+        return Stream.of(
+                Arguments.of(56, true),
+                Arguments.of(1000, false),
+                Arguments.of(12_000, false),
+                Arguments.of(-10_000, false)
+        );
+    }
+
+    @ParameterizedTest(name = "숫자 음수 테스트")
+    @MethodSource("isNegativeNumberData")
+    void isNegativeNumber(int number, boolean result) {
+        assertThat(Validator.isNegativeNumber(number)).isEqualTo(result);
+    }
+
+    static Stream<Arguments> isNegativeNumberData() {
+        return Stream.of(
+                Arguments.of(56, false),
+                Arguments.of(-1, true),
+                Arguments.of(0, false)
+        );
+    }
+
+}
