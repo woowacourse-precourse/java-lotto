892b9ecf0b7750dcd2f956d4e3ddf0d96a931762
WonjunS <seowj0710@gmail.com>
2022-11-14 23:47:27 +1100

    update: 프로그램 실행을 위한 메서드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.Result;
+import lotto.view.Input;
+
+import java.util.List;
+
+import static lotto.utils.Generator.generateLotto;
+import static lotto.view.Input.enterBonusNumber;
+import static lotto.view.Input.enterWinNumbers;
+import static lotto.view.Output.printLottoList;
+import static lotto.view.Output.printStats;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            int price = Input.purchaseAmount();
+            int count = price / 1000;
+
+            List<Lotto> lottoList = generateLotto(count);
+            printLottoList(count, lottoList);
+
+            Result result = new Result(enterWinNumbers());
+            result.setBonus(enterBonusNumber());
+
+            printStats(price, lottoList, result);
+
+        } catch(IllegalArgumentException e) {
+            System.out.println(e);
+        }
     }
 }
