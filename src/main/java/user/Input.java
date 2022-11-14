9f1934a8393148205b8d2dcce6e005d0c5309a39
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-14 11:59:38 +0900

    feat: 유저의 입력값을 String -> Integer 형변환 기능 구현

--- src/main/java/user/Input.java
+++ src/main/java/user/Input.java
@@ -2,15 +2,16 @@ package user;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static message.ErrorMessage.*;
 
 public class Input {
-    public List<String> userLotto(String str) {
+    public List<Integer> userLotto(String str) {
         if (!(str.contains(","))) throw new IllegalArgumentException(USER_INPUT_ERROR.getStatus());
 
         String[] arr = str.split(",");
-        List<String> userInput = Arrays.asList(arr);
+        List<Integer> userInput = Arrays.stream(arr).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
 
         if (userInput.size() != 6) throw new IllegalArgumentException(INPUT_COUNT_ERROR.getStatus());
         return userInput;
