cd68312c0a4d1bb052dcab0f5647b417cdcb1d25
taekwon-dev <taekwon_dev@naver.com>
2022-11-14 14:24:13 +0900

    feat: (NumberScanner) 쉼표(,)를 기준으로 당첨 번호 입력 값 나누기

--- src/main/java/lotto/view/scanner/NumberScanner.java
+++ src/main/java/lotto/view/scanner/NumberScanner.java
@@ -1,13 +1,27 @@
 package lotto.view.scanner;
 
+import java.util.Collections;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static java.lang.Integer.parseInt;
 
 public class NumberScanner {
+    private static final String DELIMITER = ",";
+
     public String inputPayment() {
         String won = readLine();
         validateParseInt(won);
         return won;
+    }
+
+    public List<Integer> inputWinningLottoNumbers() {
+        String[] winningLottoNumberSplitBy = splitByDelimiter(readLine());
+        return Collections.emptyList();
+    }
+
+    private String[] splitByDelimiter(String winningLottoNumbers) {
+        return winningLottoNumbers.split(DELIMITER);
     }
 
     private void validateParseInt(String input) {
