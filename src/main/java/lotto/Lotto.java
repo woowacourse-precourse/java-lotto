aa5f8dd49432c1302d119d077b3bc2c96248e224
Seiyon Park <seiyonpdev@gmail.com>
2022-11-15 08:21:00 +0900

    feat(Lotto): 메소드 outOfNumberRange 구현
    로또 번호가 범위를 벗어났는지 확인하는 메소드 outOfNumberRange를 구현했다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,7 @@ package lotto;
 import java.util.*;
 
 public class Lotto {
+    static final int LOTTO_NUMBER_LENGTH = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,7 +13,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) throws IllegalArgumentException {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUMBER_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력하세요");
         } else if (hasDuplication(numbers)) {
             throw new IllegalArgumentException("[ERROR] 중복되는 숫자가 있습니다");
@@ -25,6 +26,18 @@ public class Lotto {
         Set<Integer> DuplicationCheck = new HashSet<>(numbers);
 
         return numbers.size() != DuplicationCheck.size();
+    }
+
+    private boolean outOfNumberRange(List<Integer> numbers) {
+        boolean outOfRange = false;
+
+        for (int index = 0; index < LOTTO_NUMBER_LENGTH; index++) {
+            if (numbers.get(index) < 1 || numbers.get(index) > 45) {
+                outOfRange = true;
+                break;
+            }
+        }
+        return outOfRange;
     }
 
     void printNumbers() {
