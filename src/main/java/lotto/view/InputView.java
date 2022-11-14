61f6a86d8944dd8834a50728f0cf3e7833ddd482
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-15 00:23:54 +0900

    refactor(InputView): 당첨번호 관련 상수 및 메서드 이름 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String INPUT_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String INPUT_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public static String inputMoney() {
@@ -13,8 +13,8 @@ public class InputView {
         return Console.readLine();
     }
 
-    public static String inputLottoNumber() {
-        System.out.println(INPUT_LOTTO_NUMBER);
+    public static String inputWinningNumber() {
+        System.out.println(INPUT_WINNING_NUMBER);
         return Console.readLine();
     }
 
