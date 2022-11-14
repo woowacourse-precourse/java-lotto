7c3d911a23cb4efa16e35ddf3a460be2ff0a8922
2jun0 <soo28819@gmail.com>
2022-11-14 23:37:42 +0900

    Fix(Match): 당첨 통계가 거꾸로 출력되는 문제 해결.

--- src/main/java/lotto/domain/match/Reward.java
+++ src/main/java/lotto/domain/match/Reward.java
@@ -7,11 +7,11 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum Reward {
-    FIRST(6, 2_000_000_000, false),
-    SECOND(5, 30_000_000, true),
-    THIRD(5, 1_500_000, false),
+    FIFTH(3, 5_000, false),
     FOURTH(4, 50_000, false),
-    FIFTH(3, 5_000, false);
+    THIRD(5, 1_500_000, false),
+    SECOND(5, 30_000_000, true),
+    FIRST(6, 2_000_000_000, false);
 
     private final long reward;
     private final int match;
