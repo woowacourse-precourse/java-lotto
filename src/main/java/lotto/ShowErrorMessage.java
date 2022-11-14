fc5f8abbe8980cc3246522ebee48f440ff10cdbb
abbd2 <ajkety00@gmail.com>
2022-11-14 20:01:28 +0900

    Rename(ShowErrorMessage): ShowErrorMessage를 enum으로 수정

--- src/main/java/lotto/ShowErrorMessage.java
+++ src/main/java/lotto/ShowErrorMessage.java
@@ -1,32 +1,43 @@
 package lotto;
 
-public class ShowErrorMessage {
+public enum ShowErrorMessage {
 
-    private static final String ERROR_MESSAGE = "[ERROR] ";
-    private static final String OUT_RANGE_LOTTO_NUMBER_MESSAGE = "로또 번호는 1부터 45 사이의 정수여야 합니다.";
-    private static final String NOT_DIVIDE_MESSAGE = "금액은 1,000으로 나누어 떨어져야 합니다.";
-    private static final String LOTTO_NUMBER_COUNT = "당첨 번호의 개수는 6개여야 합니다.";
-    private static final String DUPLICATE_LOTTO_NUBMER = "당첨 번호는 중복될 수 없습니다.";
-    private static final String CONTAINS_BONUS_NUMBER = "보너스 번호는 당첨번호와 중복될 수 없습니다.";
+    ERROR_MESSAGE("[ERROR] "),
+
+    OUT_RANGE_LOTTO_NUMBER_MESSAGE("로또 번호는 1부터 45 사이의 정수여야 합니다."),
+
+    NOT_DIVIDE_MESSAGE("금액은 1,000으로 나누어 떨어져야 합니다."),
+
+    LOTTO_NUMBER_COUNT("당첨 번호의 개수는 6개여야 합니다."),
+
+    DUPLICATE_LOTTO_NUBMER("당첨 번호는 중복될 수 없습니다."),
+
+    CONTAINS_BONUS_NUMBER("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+
+    private final String errorScript;
+
+    ShowErrorMessage(String errorScript) {
+        this.errorScript = errorScript;
+    }
 
     public static void showOutRangeLottoNumber() {
-        System.out.println(ERROR_MESSAGE + OUT_RANGE_LOTTO_NUMBER_MESSAGE);
+        System.out.println(ERROR_MESSAGE.errorScript + OUT_RANGE_LOTTO_NUMBER_MESSAGE.errorScript);
     }
 
     public static void showNotDivide() {
-        System.out.println(ERROR_MESSAGE + NOT_DIVIDE_MESSAGE);
+        System.out.println(ERROR_MESSAGE.errorScript + NOT_DIVIDE_MESSAGE.errorScript);
     }
 
     public static void showLottoNumberCount() {
-        System.out.println(ERROR_MESSAGE + LOTTO_NUMBER_COUNT);
+        System.out.println(ERROR_MESSAGE.errorScript + LOTTO_NUMBER_COUNT.errorScript);
     }
 
     public static void showDuplicateLottoNumber() {
-        System.out.println(ERROR_MESSAGE + DUPLICATE_LOTTO_NUBMER);
+        System.out.println(ERROR_MESSAGE.errorScript + DUPLICATE_LOTTO_NUBMER.errorScript);
     }
 
     public static void showContainsBonusNumber() {
-        System.out.println(ERROR_MESSAGE + CONTAINS_BONUS_NUMBER);
+        System.out.println(ERROR_MESSAGE.errorScript + CONTAINS_BONUS_NUMBER.errorScript);
     }
 
 }
