0abf740c9ebec02c6a76c4d7a961282ddd20b20e
footdev <ekclstkfka5@gmail.com>
2022-11-15 01:13:54 +0900

    feat: Enum 구현
    맟춘 개수와 당첨액을 상태로 가지는 상수의 모음을 Enum 클래스로 구현함.

--- src/main/java/lotto/domain/Rating.java
+++ src/main/java/lotto/domain/Rating.java
@@ -1,4 +1,43 @@
 package lotto.domain;
 
+import java.util.Arrays;
+
 public enum Rating {
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    NONE(0, 0);
+
+    private final int count;
+    private final int reward;
+
+    Rating(final int count, final int reward) {
+        this.count = count;
+        this.reward = reward;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public static Rating getRating(final int count, final boolean bonusCount) {
+        if(count == THIRD.count && !bonusCount) {
+            return THIRD;
+        }
+
+        if(count < FIFTH.count) {
+            return NONE;
+        }
+
+        return Arrays.stream(Rating.values())
+                .filter(rating -> count == rating.count)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 }
