fc73931e189e08617fcc9b33eaadb11c057b1762
JeonSangYong <tkddyd420@naver.com>
2022-11-14 23:55:21 +0900

    feat : 로또 구입 금액만큼 로또 번호 자동 발행

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -2,12 +2,22 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoGenerator {
+    public static final int LOTTO_PRICE = 1000;
 
-    public static Lotto makeSortedRandomNumbers() {
+    public static List<Lotto> sellLottos(int money) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < money / LOTTO_PRICE; i++) {
+            lottos.add(makeSortedRandomNumbers());
+        }
+        return lottos;
+    }
+
+    private static Lotto makeSortedRandomNumbers() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
                 .stream()
                 .sorted()
--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -2,7 +2,6 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class User {
@@ -14,6 +13,10 @@ public class User {
         // 숫자 입력이 아닐 때 예외처리
         // 1000원으로 나누어 떨어지지 않을 때 예외처리
         this.money = Integer.parseInt(money);
-        // 로또 구입 금액만큼 로또 번호 자동 발행
+        buyLotto();
+    }
+
+    private void buyLotto() {
+        this.lottos = LottoGenerator.sellLottos(money);
     }
 }
