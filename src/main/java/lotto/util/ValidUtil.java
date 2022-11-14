e3b2e83bad8dbed33b735b897d596fc111ce006f
ohhonggi <5ghdrl@naver.com>
2022-11-15 03:05:30 +0900

    feat($ValidUtil): Implement validAmountUnit function
    This function verifies whether the lotto purchase amount
     is divided into 1,000 won.

--- src/main/java/lotto/util/ValidUtil.java
+++ src/main/java/lotto/util/ValidUtil.java
@@ -1,12 +1,21 @@
 package lotto.util;
 
 
+import static lotto.constants.AmountCode.AMOUNTS_UNIT;
+
 public class ValidUtil {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
     public static void validInputAmounts(String input) {
         if (!input.matches("^[0-9]+$")) {
-            throw new IllegalArgumentException(ERROR_MESSAGE +" 숫자를 입력해야 합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 숫자를 입력해야 합니다.");
+        }
+    }
+
+    public static void validAmountUnit(int amount) {
+        if (amount % AMOUNTS_UNIT != 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE +
+                    " 로또 금액은 1000원 단위이어야 합니다.");
         }
     }
 
