319158cf9e3514f34d51e5ab05604e13b19cb9ea
dydtmd6801 <dydtmd6801@naver.com>
2022-11-14 15:06:03 +0900

    test(LottoTest): 로또 구입 금액에 따른 로또 장 수 테스트
    decideToBuyLotto, 로또 구입 금액에 따른 로또 장 수 기능 테스트
    로또 구입 금액을 8000원으로하고 예상 값을 8로 설정

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,25 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import domain.Server;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import view.Client;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
+    Server server;
+    Client client;
+    @BeforeEach
+    void setClass(){
+        server = new Server();
+        client = new Client();
+    }
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -24,4 +36,9 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 구입 금액에 따른 로또 장수 테스트")
+    @Test
+    void decideToBuyLottoTest() {
+        assertThat(server.decideToBuyLotto(8000)).isEqualTo(8);
+    }
 }
