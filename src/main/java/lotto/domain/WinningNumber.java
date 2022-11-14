d65daf39293c5158a8bc745e02dcbce5e0f0703f
cocomozila <joseyun94@naver.com>
2022-11-14 23:11:09 +0900

    feat(WinningNumber): 예외처리 Controller로 넘기기

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -10,19 +10,19 @@ public class WinningNumber {
     private Lotto lotto;
 
     // 당첨 번호 생성 기능
-    public void inputWinningNumber(String input) {
+    public void inputWinningNumber(String input) throws IllegalArgumentException {
         validateInput(input);
         List<Integer> numbers = stringToWinningNumber(input);
         this.lotto = new Lotto(numbers);
     }
 
     // 올바른 당첨 번호 입력값인지 검증하는 기능
-    private void validateInput(String input) {
+    private void validateInput(String input) throws IllegalArgumentException {
         validateFiveComma(input);
     }
 
     // 쉼표(,)가 5개인지 확인하는 기능
-    private void validateFiveComma(String input) {
+    private void validateFiveComma(String input) throws IllegalArgumentException {
         int commaCount = 0;
         for (int index = 0; index < input.length(); index++) {
             if (isComma(input.charAt(index))) {
@@ -35,12 +35,12 @@ public class WinningNumber {
     }
 
     // 쉼표(,)인지 확인하는 기능
-    private boolean isComma(char digit) {
+    private boolean isComma(char digit) throws IllegalArgumentException {
         return digit == Setting.SPLIT_CHARACTER;
     }
     
     // 쉼표(,)로 나눈 6개의 문자를 숫자로 변환하는 기능
-    private List<Integer> stringToWinningNumber(String input) {
+    private List<Integer> stringToWinningNumber(String input) throws IllegalArgumentException {
         List<Integer> list = new ArrayList<>();
         String[] numbers = input.split(String.valueOf(Setting.SPLIT_CHARACTER));
         for (String number : numbers) {
@@ -51,7 +51,7 @@ public class WinningNumber {
     }
 
     // 문자열이 숫자인지 검증
-    private void checkNumber(String number) {
+    private void checkNumber(String number) throws IllegalArgumentException {
         checkBlank(number);
         for (int index = 0; index < number.length(); index++) {
             if (!Character.isDigit(number.charAt(index))) {
@@ -61,7 +61,7 @@ public class WinningNumber {
     }
 
     // 문자열이 공백인지 검증
-    private void checkBlank(String number) {
+    private void checkBlank(String number) throws IllegalArgumentException {
         if (number.equals("")) {
             throw new IllegalArgumentException("[ERROR] 공백을 입력할 수 없습니다.");
         }
