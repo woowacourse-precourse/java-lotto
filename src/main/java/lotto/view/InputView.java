ea6762080ca14547cdc246aad87a57bf76ada8c4
KimYongJun <rladydwns129@naver.com>
2022-11-14 21:08:50 +0900

    feat: 입력 담당하는 InputView 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,46 @@
 package lotto.view;
 
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
+
+    private static final String DELIMITER = ",";
+
+    private String readLine() {
+        return camp.nextstep.edu.missionutils.Console.readLine();
+    }
+
+    private void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public String requestMoney() {
+        return readLine();
+    }
+
+    public List<Integer> requestWinningNumbers() {
+        List<Integer> winningNumbers;
+        try {
+            winningNumbers = convertToLottoNumbers(readLine());
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException();
+        }
+        return winningNumbers;
+    }
+
+    private List<Integer> convertToLottoNumbers(String input) {
+        List<Integer> lottoNumbers = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(input, DELIMITER);
+        while (tokenizer.hasMoreTokens()) {
+            lottoNumbers.add(Integer.parseInt(tokenizer.nextToken()));
+        }
+        return lottoNumbers;
+    }
+
+    public String requestBonusNumber() {
+        return readLine();
+    }
 }
