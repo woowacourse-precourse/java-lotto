e7ee1f299c393d0327a67e847fdfce41a3d12305
twoosky <lhjae3@naver.com>
2022-11-15 02:48:42 +0900

    refactor(view): input 메서드명 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,17 +8,17 @@ public class InputView {
     private InputView() {
     }
 
-    public static String money() {
+    public static String inputMoney() {
         System.out.println(INPUT_MONEY);
         return Console.readLine();
     }
 
-    public static String winningNumbers() {
+    public static String inputWinningNumbers() {
         System.out.println(INPUT_WINNING_NUMBER);
         return Console.readLine();
     }
 
-    public static String bonusBallNumber() {
+    public static String inputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER);
         return Console.readLine();
     }
