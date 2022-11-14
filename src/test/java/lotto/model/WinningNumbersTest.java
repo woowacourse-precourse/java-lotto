2e63d08a8a4457352ffd94cfd8e956713c8c7690
dhkim <akdlzmf1123@gmail.com>
2022-11-14 19:42:55 +0900

    test(WinningNumbersTest): 보너스 번호가 당첨 번호와 중복이면 예외 던지는 기능 테스트 추가

--- src/test/java/lotto/model/WinningNumbersTest.java
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -5,12 +5,21 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WinningNumbersTest {
     WinningNumbers winningNumbers = new WinningNumbers(
             List.of(1, 2, 3, 4, 5, 6)
             , 7
     );
+
+    @Test
+    void 보너스_번호가_당첨_번호와_중복이면_예외_던지기() {
+        assertThatThrownBy(() -> {
+            new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 5);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
 
     @Test
     void 임의의_숫자가_당첨번호인지_보너스인지_확인하는_기능() {
