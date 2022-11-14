d8434093b568b0cb5e8bbe7a7c8c53b2d82edba1
Ridealist <cjsn92@gmail.com>
2022-11-15 04:36:34 +0900

    refactor(Application): 컨트롤러 메소드 적용해 로직 단순화 및 명료화

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,40 +1,16 @@
 package lotto;
 
-import java.util.List;
-import lotto.controller.Calculator;
 import lotto.model.BonusNumber;
-import lotto.model.Lottery;
-import lotto.model.Purchase;
-import lotto.model.WinningNumber;
-import lotto.view.InputView;
-import lotto.view.output.BuyListView;
-import lotto.view.output.SummaryView;
+import lotto.model.LottoTickets;
+import lotto.model.WinningNumbers;
 
 public class Application {
     public static void main(String[] args) {
         try {
-            String inputAmount = InputView.askPurchaseAmount();
-            Purchase purchase = new Purchase(inputAmount);
-            Lottery lottery = new Lottery(purchase.getAmount());
-            BuyListView.show(lottery.getLotteryNumbers(), purchase.getAmount());
-
-            String inputWinningNumbers = InputView.askWinningNumbers();
-            WinningNumber wn = new WinningNumber(inputWinningNumbers);
-            List<Integer> winningNumbers = wn.getWinningNumbers();
-
-            String inputBonusNumber = InputView.askBonusNumber();
-            BonusNumber bn = new BonusNumber(inputBonusNumber, winningNumbers);
-            int bonusNumber = bn.getBonusNumber();
-
-            Calculator cal = new Calculator();
-            for (int i = 0; i < purchase.getAmount(); i++) {
-                List<Integer> lotto = lottery.getLotteryNumbers().get(i).getLottoNumbers();
-                cal.updateResult(lotto, winningNumbers, bonusNumber);
-            }
-
-            SummaryView.showResult(cal.firstWin, cal.secondWin, cal.thirdWin, cal.fourthWin, cal.fifthWin);
-            SummaryView.showEarning(purchase.getAmount(), cal.firstWin, cal.secondWin, cal.thirdWin, cal.fourthWin,
-                    cal.fifthWin);
+            LottoTickets lottoTickets = Controller.createLottoTickets();
+            WinningNumbers winningNumbers = Controller.createWinningLotto();
+            BonusNumber bonusNumber = Controller.createBonusNumber(winningNumbers);
+            Controller.createSummary(lottoTickets, winningNumbers, bonusNumber);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
