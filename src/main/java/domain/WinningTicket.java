81c35d042f0a8b1c023d4d2ee8b09bfc32c81322
김선동 <mok03127@gmail.com>
2022-11-14 22:08:05 +0900

    fix: 답지, 보너스 번호 예외 처리 수정

--- src/main/java/domain/WinningTicket.java
+++ src/main/java/domain/WinningTicket.java
@@ -4,21 +4,27 @@ import lotto.Lotto;
 
 import java.util.List;
 
-public class WinningTicket {
-    private static final String WINNING_NUMBER_AND_BONUS_DUPLICATED_ERROR = "보너스 번호는 당첨 번호와 중복될 수 없습니다";
+public class WinningTicket extends Lotto {
+    private static final String WINNING_NUMBER_AND_BONUS_DUPLICATED_ERROR = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다";
     private final Lotto winningNumbers;
     private final int bonusNumber;
 
-    public WinningTicket(List<Integer> winningTicket,int bonusNumber) {
+    public WinningTicket(List<Integer> winningTicket, int bonusNumber) {
+        super(winningTicket);
         validateDuplicateNumber(winningTicket, bonusNumber);
         this.winningNumbers = new Lotto(winningTicket);
         this.bonusNumber = bonusNumber;
     }
 
-    private void validateDuplicateNumber(List<Integer> winningTicket,int bonusNumber) {
+    public boolean checkBonus(Lotto lotto) {
+        return lotto.checkContains(bonusNumber);
+    }
+
+    private void validateDuplicateNumber(List<Integer> winningTicket, int bonusNumber) {
         if (winningTicket.contains(bonusNumber)) {
             throw new IllegalArgumentException(WINNING_NUMBER_AND_BONUS_DUPLICATED_ERROR);
         }
     }
+
 
 }
