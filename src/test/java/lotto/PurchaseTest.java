0612d1e32a9f2add10f6638f3e121bf9fa6dacb7
Yeongseon Park <yseon406@gmail.com>
2022-11-14 14:53:05 +0900

    test: 사용자 금액 유효성 확인 기능 test

---
+++ src/test/java/lotto/PurchaseTest.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import lotto.domain.Purchase;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PurchaseTest {
+
+    @Test
+    void isValidTest() {
+        int money = 14000;
+        assertThat(new Purchase().isValid(money)).isTrue();
+    }
+}
