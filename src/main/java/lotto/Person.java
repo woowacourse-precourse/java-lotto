fbce4956d9b1a9ff2d98979bd9448a2678ad4e47
sun007021 <sun007021@gmail.com>
2022-11-14 14:33:49 +0900

    feat (Person) : 번호 오름차순 정렬

--- src/main/java/lotto/Person.java
+++ src/main/java/lotto/Person.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Person {
@@ -11,6 +12,7 @@ public class Person {
 
     private List<Integer> createRandomLottoNum (){
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        numbers.sort(Comparator.naturalOrder());
         return numbers;
     }
 }
