973f652b89465ed6512e9d2910c367449c9a2a7c
phj1120 <hyeonj1998@naver.com>
2022-11-14 14:50:09 +0900

    test(lotto): 입력 받은 개수에 맞게 로또 발행 하는 기능 테스트

--- src/test/java/lotto/MachineTest.java
+++ src/test/java/lotto/MachineTest.java
@@ -3,23 +3,36 @@ package lotto;
 import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.Machine;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 public class MachineTest {
 
+    Machine machine;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine();
+    }
+
+
     @DisplayName("로또의 개수 확인")
     @Test
     void checkNumCountLotto() {
-        Machine machine = new Machine();
         Lotto lotto = machine.draw();
         List<Integer> numbers = lotto.getNumbers();
         Assertions.assertThat(numbers.size()).isEqualTo(6);
     }
 
+    @DisplayName("발행한 개수 확인")
+    @Test
+    void checkDrawCount() {
+        int count = 10;
+        List<Lotto> lottos = machine.draw(count);
+        Assertions.assertThat(lottos.size()).isEqualTo(count);
+    }
 
 }
