fd789f24d0572209b51888042ca90131b9027dee
DevJay1024 <devjay1024@gmail.com>
2022-11-14 18:14:37 +0900

    refactor: 사용자 입력 예외처리 문구 기능 변경 (문구 상세화)
    어플리케이션 재실행 시, 사용자가 스스로 입력을 고칠 수 있도록 수정

--- src/main/java/lotto/InvalidInputMessage.java
+++ src/main/java/lotto/InvalidInputMessage.java
@@ -2,7 +2,12 @@ package lotto;
 
 public class InvalidInputMessage {
 
-    public static String ERR_USER_MONEY = "로또 구매금액이 유효하지 않습니다.";
-    public static String ERR_WINNING_NUMBERS = "로또 당첨번호 입력값이 유효하지 않습니다.";
-    public static String ERR_BONUS_NUMBER = "보너스 입력값이 유효하지 않습니다.";
+    public static String ERR_MONEY_OVER_RANGE = "로또 구매금액이 허용 범위 이내여야 합니다.";
+    public static String ERR_MONEY_UNIT = "로또 구매금액은 1000원 단위로 입력해야 합니다.";
+    public static String ERR_EMPTY_INPUT = "입력값이 비어있습니다.";
+    public static String ERR_NOT_NUMBER = "숫자만 입력해야 합니다.";
+    public static String ERR_NUMBER_OVER_RANGE = "로또 번호 범위는 1~45여야 합니다.";
+    public static String ERR_WINNING_NUMBER_FORMAT = "로또 번호의 구분자는 반점(,)이어야 합니다.";
+    public static String ERR_NOT_ENOUGH_COUNT = "로또 한 줄에 번호는 6개여야 합니다.";
+    public static String ERR_DUPLICATED_NUMBER = "당첨 번호와 보너스 번호는 중복되지 않아야 합니다.";
 }
