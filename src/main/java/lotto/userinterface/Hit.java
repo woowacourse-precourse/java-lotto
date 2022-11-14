8782c04851ca943da6849c05432a23c56759527e
wonju-dev <wonjulee.dev@gmail.com>
2022-11-14 21:20:03 +0900

    feat(Hit): count로 Hit 반환 기능 구현

--- src/main/java/lotto/userinterface/Hit.java
+++ src/main/java/lotto/userinterface/Hit.java
@@ -1,19 +1,42 @@
 package lotto.userinterface;
 
-public enum Hit {
-    THREE(5_000),
-    FOUR(50_000),
-    FIVE(15_000_000),
-    FIVE_BONUS(30_000_000),
-    SIZE(2_000_000_000);
+import java.util.HashMap;
+import java.util.Map;
 
+public enum Hit {
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 15_000_000),
+    FIVE_BONUS(5, 30_000_000),
+    SIZE(6, 2_000_000_000);
+
+    private final static Map<Integer, Hit> map;
+    private Integer count;
     private Integer prize;
 
-    Hit(Integer prize) {
+    static {
+        map = new HashMap<>();
+        for (Hit hit : Hit.values()) {
+            map.put(hit.count, hit);
+        }
+    }
+
+    Hit(Integer count, Integer prize) {
+        this.count = count;
         this.prize = prize;
     }
 
     public Integer getPrize() {
         return prize;
+    }
+
+    public static Hit getByCount(Integer count, Boolean hitBonus) {
+        Hit hit = map.get(count);
+
+        if (hitBonus && hit.count == 5) {
+            return FIVE_BONUS;
+        }
+
+        return hit;
     }
 }
