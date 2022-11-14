99001ed161843027ac953b4d73a4566aaa57ccaf
오승윤 <>
2022-11-14 23:56:47 +0900

    feat (OutputView) : 출력 책임 클래스 추가

---
+++ src/main/java/lotto/view/outputView/OutputViewImple.java
@@ -0,0 +1,34 @@
+package lotto.view.outputView;
+
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
+
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Map;
+
+public class OutputViewImple implements OutputView {
+    
+    @Override
+    public void printPurchasedLottos(List<Lotto> lottos) {
+        //TODO: 구현
+    }
+
+    @Override
+    public void printWinningStatus(Map<Rank, Integer> winningStatus) {
+        //TODO: 구현
+    }
+
+
+    @Override
+    public void printReturnRatio(double returnRatio) {
+        //TODO: 구현
+    }
+
+    @Override
+    public void printErrorMessage(Exception e) {
+        //TODO: 구현
+    }
+
+
+}
