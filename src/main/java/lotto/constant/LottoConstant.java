04975a4de84329881ed64d6817dff008a0adfd06
kimJaeHyeon <kmr2644@gmail.com>
2022-11-14 22:35:33 +0900

    refactor: 로또번호 갯수 에서 당첨번호 갯수로 변경

--- src/main/java/lotto/constant/LottoConstant.java
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -3,5 +3,5 @@ package lotto.constant;
 public class LottoConstant {
     public static final int MAX_LOTTO_NUMBER = 45;
     public static final int LOW_LOTTO_NUMBER = 1;
-    public static final int LOTTO_SIZE = 7;
+    public static final int WINNING_NUMBER_SIZE = 6;
 }
