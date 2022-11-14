89ab07aaa0572094ec319682c62cdb5f33cd1594
junyong <jymoon1121@gmail.com>
2022-11-14 23:30:27 +0900

    refactor: 로또 가격 final 상수로 변경

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -3,8 +3,8 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
-    private int money;
-
+    private final int LOTTO_PRICE = 1000;
+    private int paid;
     public User() {
 
     }
@@ -12,9 +12,10 @@ public class User {
         String input = Console.readLine();
         int money = Integer.parseInt(input);
         validateMoney(money);
+        this.paid = Integer.parseInt(input);
     }
     public void validateMoney(int money) {
-        if(money % 1000 != 0) {
+        if(money % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
         }
     }
