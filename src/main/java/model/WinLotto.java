124e8e9d3d520adb07cba93fa846304998c65a41
gyuturn <rbtjrrbals@gmail.com>
2022-11-14 22:03:01 +0900

    refactor($model): 해당객체 Lotto에 대한 상속 풀기
    요구사항을 만족하기 위해서는 상속을 풀고 재사용되는 함수는 protected로 구성하여 재사용하는 방식
    
    RF-4-1
    추가항목
    -WinLotto만의 numbers 변수 추가

--- src/main/java/model/WinLotto.java
+++ src/main/java/model/WinLotto.java
@@ -1,32 +1,32 @@
 package model;
 
+import static model.Lotto.LOTTO_SIZE;
+import static model.Lotto.hasDuplicateNum;
+import static model.Lotto.isNumRangeLotto;
+
 import java.util.ArrayList;
 import java.util.List;
 import util.message.ErrorMessage;
-import view.PrintView;
 
-public class WinLotto extends Lotto {
+public class WinLotto  {
     private int bonusNumber;
+    private final List<Integer> numbers;
 
     public WinLotto(List<Integer> numbers,int bonusNumber) {
         validate(numbers,bonusNumber);
-        sortAscend(numbers);
         this.numbers = numbers;
         this.bonusNumber = bonusNumber;
     }
 
     private void validate(List<Integer> numbers, int bonusNumber) {
         List<Integer> numbersForValid = getNumbersForValid(numbers, bonusNumber);
-        if (numbers.size() != 6) {
-//            PrintView.error(ErrorMessage.LOTTO_LENGTH_SIZE_NOT_SIX);
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(ErrorMessage.LOTTO_LENGTH_SIZE_NOT_SIX);
         }
         if (hasDuplicateNum(numbersForValid)) {
-//            PrintView.error(ErrorMessage.HAS_NOT_DUPLICATE_NUM);
             throw new IllegalArgumentException(ErrorMessage.HAS_NOT_DUPLICATE_NUM);
         }
         if (!isNumRangeLotto(numbersForValid)) {
-//            PrintView.error(ErrorMessage.IS_NOT_LOTTO_RANGE_NUM);
             throw new IllegalArgumentException(ErrorMessage.IS_NOT_LOTTO_RANGE_NUM);
         }
     }
@@ -39,5 +39,9 @@ public class WinLotto extends Lotto {
 
     public int getBonusNumber() {
         return bonusNumber;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 }
