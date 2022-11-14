bf26a93e0a9c10a052da58b7d022bb9241213f92
jini_11 <jieun21124@naver.com>
2022-11-14 23:56:33 +0900

    test: 로또 범위 검증 테스트 케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,5 +24,17 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호의 범위가 45보다 크면 예외가 발생한다.")
+    @Test
+    void createLottoByOverRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 100, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 범위가 1보다 작으면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 0, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
