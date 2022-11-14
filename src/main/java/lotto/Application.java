6382357cc418749d3531ea8a30fc1c37f8a9bd96
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-14 17:31:57 +0900

    feat(main): 예외 사항 에러 메시지 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,6 +7,11 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         LottoController lottoController = new LottoController();
-        lottoController.start();
+        try {
+            lottoController.start();
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
