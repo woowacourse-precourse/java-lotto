5df7820b88666de62e1dcb39752db3f5cd14b699
impati <impati0716@icloud.com>
2022-11-14 22:27:41 +0900

    feat(Convert) : winningConvert 기능

--- src/main/java/lotto/Convert.java
+++ src/main/java/lotto/Convert.java
@@ -1,8 +1,19 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Convert {
     public static Integer costConvert(String cost){
         InputValidation.numberFormatExceptionValidation(cost);
         return Integer.parseInt(cost);
+    }
+    public static Lotto winningConvert(String numbers){
+        InputValidation.winningNumberValidation(numbers);
+        return new Lotto(
+                Arrays.stream(numbers.split(","))
+                        .map(number->Integer.parseInt(number))
+                        .collect(Collectors.toList()));
     }
 }
