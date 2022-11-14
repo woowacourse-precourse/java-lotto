5b68afbebfc4e34ec0910dbc51fa9416f94aae56
SeongukBaek <bsu6235@gmail.com>
2022-11-14 18:45:28 +0900

    test: add new tests for Validator.isNumber() and group the tests

--- src/test/java/lotto/ui/ValidatorTest.java
+++ src/test/java/lotto/ui/ValidatorTest.java
@@ -2,27 +2,52 @@ package lotto.ui;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class ValidatorTest {
-    @DisplayName("주어진 문자가 숫자가 아닌 경우")
-    @Test
-    void isNumberTest_숫자가아닌경우() {
-        char input = 'n';
+    @Nested
+    class isNumberForChar {
+        @DisplayName("주어진 문자가 숫자가 아닌 경우")
+        @Test
+        void isNumberTest_숫자가아닌경우() {
+            char input = 'n';
 
-        // then
-        Assertions.assertThatThrownBy(
-                    () -> Validator.isNumber(input)
-                )
-                .isInstanceOf(IllegalArgumentException.class);
+            Assertions.assertThatThrownBy(
+                            () -> Validator.isNumber(input)
+                    )
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("주어진 문자가 숫자인 경우")
+        @Test
+        void isNumberTest_숫자인경우() {
+            char input = '4';
+
+            Assertions.assertThat(Validator.isNumber(input)).isTrue();
+        }
     }
 
-    @DisplayName("주어진 문자가 숫자인 경우")
-    @Test
-    void isNumberTest_숫자인경우() {
-        char input = '4';
+    @Nested
+    class isNumberForString {
+        @DisplayName("주어진 문자열이 숫자가 아닌 경우")
+        @Test
+        void isNumberTest_숫자가아닌경우() {
+            String input = "asdfadf1";
 
-        // then
-        Assertions.assertThat(Validator.isNumber(input)).isTrue();
+            Assertions.assertThatThrownBy(
+                            () -> Validator.isNumber(input)
+                    )
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("주어진 문자열이 숫자인 경우")
+        @Test
+        void isNumberTest_숫자인경우() {
+            String input = "1234";
+
+            Assertions.assertThat(Validator.isNumber(input)).isTrue();
+        }
     }
+
 }
