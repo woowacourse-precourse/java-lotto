8f157eabe93b58c0694fd372d31f16ef2dcc282a
jaehyeon <jhjh0022@naver.com>
2022-11-15 01:11:47 +0900

    fix: 수익률 구한는 식 수정, 메서드 이름, 자료형 수정

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 public class Calculator {
 
-    public double df(List<Integer> rewardList, int money) {
-        double moneys = money;
+    public String getyield(List<Integer> rewardList, int money) {
         double total = totalRewardmoney(rewardList);
+        double totalReturn = (total / money) * 100;
 
-        return (double) Math.round(((total / moneys) * 100) * 10) / 10.0;
+        //return (double) Math.round(((total / (double) money) * 100) * 10) / 10.0;
+        return String.format("%.1f", totalReturn);
+
     }
 
-    private int totalRewardmoney(List<Integer> rewardList) {
+    private double totalRewardmoney(List<Integer> rewardList) {
         int sum = 0;
         for (int money : rewardList) {
             sum += money;
