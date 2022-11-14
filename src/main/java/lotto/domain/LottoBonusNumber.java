935ce07c6e33f50f9b12f5ac29a12b2a0fb4d547
Yunsik-Choi <choiys0311@gmail.com>
2022-11-14 11:59:15 +0900

    ♻️ refactor : LottoBonusNumber 필드 이름 변경

--- src/main/java/lotto/domain/LottoBonusNumber.java
+++ src/main/java/lotto/domain/LottoBonusNumber.java
@@ -7,14 +7,14 @@ import lotto.exception.domain.LottoBonusNumberRangeException;
 public class LottoBonusNumber {
     private static final int LOTTO_BONUS_NUMBER_MIN = 1;
     private static final int LOTTO_BONUS_NUMBER_MAX = 45;
-    private static final Map<Integer, LottoBonusNumber> lottoNumberCash;
+    private static final Map<Integer, LottoBonusNumber> lottoBonusNumberCash;
 
     private final int number;
 
     static {
-        lottoNumberCash = new HashMap<>();
+        lottoBonusNumberCash = new HashMap<>();
         for (int number = LOTTO_BONUS_NUMBER_MIN; number <= LOTTO_BONUS_NUMBER_MAX; number++) {
-            lottoNumberCash.put(number, new LottoBonusNumber(number));
+            lottoBonusNumberCash.put(number, new LottoBonusNumber(number));
         }
     }
 
@@ -25,7 +25,7 @@ public class LottoBonusNumber {
 
     public static LottoBonusNumber of(int number) {
         validateNumberRange(number);
-        return lottoNumberCash.get(number);
+        return lottoBonusNumberCash.get(number);
     }
 
     private static void validateNumberRange(int number) {
