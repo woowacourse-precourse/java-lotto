6b3684edfaabe5b976bf6a77592301fbc60c1b0a
cocomozila <joseyun94@naver.com>
2022-11-14 19:56:06 +0900

    feat(Money): 유효한 숫자인지 검증 기능 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -11,15 +11,38 @@ public class Money {
         this.money = Long.parseLong(input);
     }
 
+    // 유효한 입력값인지 검증 기능
     private void validateInput(String input) throws IllegalArgumentException {
         for (int i = 0; i < input.length(); i++) {
             checkDigit(input.charAt(i));
         }
     }
 
+    // 숫자인지 검증 기능
     private void checkDigit(char digit) throws IllegalArgumentException {
         if (!Character.isDigit(digit)) {
             throw new IllegalArgumentException("[ERROR] 금액은 숫자만 입력 가능합니다.");
+        }
+    }
+
+    // 유효한 숫자인지 검증 기능
+    private void validMoney(String input) {
+        long number = Long.parseLong(input);
+        checkOver1000(number);
+        validateNumberUnit(number);
+    }
+
+    // 1000이상인지 확인
+    private void checkOver1000(long number) {
+        if (number < Setting.MIN_PURCHASE_AMOUNT) {
+            throw new IllegalArgumentException("[ERROR] 1000 이상의 금액만 가능합니다.");
+        }
+    }
+
+    // 1000단위 숫자인지 확인
+    private void validateNumberUnit(long number) {
+        if (number % Setting.PURCHASE_AMOUNT_UNIT != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000 단위의 금액만 가능합니다.");
         }
     }
 }
