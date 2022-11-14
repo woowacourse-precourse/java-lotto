97e7c6b9a4257b8158ade28a8861900e56fbf32f
h0ngwon <w___on@naver.com>
2022-11-14 20:58:12 +0900

    feat(Winning) : 당첨번호 중복검사 기능

--- src/main/java/lotto/Winning.java
+++ src/main/java/lotto/Winning.java
@@ -8,6 +8,7 @@ public class Winning {
     private static final String LOTTO_OUT_OF_RANGE_ERROR = "로또 번호는 6개여야 합니다.";
     private static final String WINNING_NOT_NUMBER_ERROR = "입력 값이 숫자가 아닙니다.";
     private static final String WINNING_NOT_IN_RANGE_ERROR = "당첨번호는 1 ~ 45 사이의 수 여야 합니다.";
+    private static final String WINNING_DUPLICATED = "당첨번호에 중복이 있습니다.";
 
     public Winning() {
     }
@@ -17,14 +18,23 @@ public class Winning {
         List<Integer> winningNumbers = new ArrayList<>();
 
         for (String num : numbers) {
-            isValidateWinningNumber(num);
-            isValidateWinningNumberRange(num);
+            validateNumber(num);
             winningNumbers.add(Integer.parseInt(num));
         }
 
-        isValidateWinningNumberSize(winningNumbers);
+        validateNumbers(winningNumbers);
 
         return winningNumbers;
+    }
+
+    private static void validateNumbers(List<Integer> winningNumbers) {
+        isDuplicatedWinningNumber(winningNumbers);
+        isValidateWinningNumberSize(winningNumbers);
+    }
+
+    private static void validateNumber(String num) {
+        isValidateWinningNumber(num);
+        isValidateWinningNumberRange(num);
     }
 
     public static void isValidateWinningNumberSize(List<Integer> winningNumbers) {
@@ -46,6 +56,12 @@ public class Winning {
 
         if(result < 1 || result > 45) {
             throw new IllegalArgumentException(ERROR_MESSAGE + WINNING_NOT_IN_RANGE_ERROR);
+        }
+    }
+
+    public static void isDuplicatedWinningNumber(List<Integer> numbers) {
+        if(numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + WINNING_DUPLICATED);
         }
     }
 }
