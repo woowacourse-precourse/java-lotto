67204fb6eab7b445f5fdcf93679b24cf8a9a610b
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-11-15 05:03:29 +0900

    fix (Rank) : 보너스 숫자의 중복확인위해 deep copy로 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-//        User lotto = UI.getUser();
-//        Lotto lotto2 = UI.getLottoNumbers();
-//        int lotto3 = UI.getBonusNumber();
+        User user = UI.getUser();
+        Rank rank = new Rank();
+        rank.run(user);
     }
 }
--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -12,25 +12,25 @@ public class Rank {
     private final Lotto lottoNumbers;
 
     public Rank() {
-        this.bonusNumber = UI.getBonusNumber();
         this.lottoNumbers = UI.getLottoNumbers();
+        this.bonusNumber = UI.getBonusNumber();
         validateBonusNumber();
     }
 
     private void validateBonusNumber() {
-        List<Integer> lottoNum = lottoNumbers.getNumbers();
+        List<Integer> lottoNum = new ArrayList<Integer>(lottoNumbers.getNumbers());
         lottoNum.add(bonusNumber);
         lottoNum = lottoNum.stream().distinct().collect(Collectors.toList());
         if (lottoNum.size() != (LOTTO_NUMBER_SIZE + 1)) {
             throw new IllegalArgumentException("[ERROR] 당첨번호와 중복되지 않는 보너스 번호를 입력해주세요.");
         }
 
-        List<Integer> bonusNumberInList = new ArrayList<>(Arrays.asList(bonusNumber));
-        Lotto lotto = new Lotto(bonusNumberInList);
-        lotto.isNumbersInRange(bonusNumberInList);
+        Lotto lottoForUsingMethod = new Lotto(lottoNumbers.getNumbers());
+        List<Integer> checkBonus = new ArrayList<>(Arrays.asList(bonusNumber));
+        lottoForUsingMethod.isNumbersInRange(checkBonus);
     }
-//
-//    public void run () {
-//        List<Lotto> userLotto = User.getUserLotto();
-//    }
+
+    public void run (User user) {
+
+    }
 }
