8eb9940b1e6bf209e5f2b4e9efa6a849a05a5ae0
JeongHunHui <deca3248@gmail.com>
2022-11-14 23:25:08 +0900

    feat: 로또에 범위를 벗어나는 숫자가 있는지 체크하는 검증 메소드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Lotto {
   private static final int NUMBER_COUNT = 6;
+  private static final int MIN_NUMBER = 1;
+  private static final int MAX_NUMBER = 45;
   private final List<Integer> numbers;
 
   public Lotto(List<Integer> numbers) {
@@ -14,17 +16,30 @@ public class Lotto {
   private void validate(List<Integer> numbers) {
     checkNumbersCount(numbers);
     checkHasDuplicateNumber(numbers);
+    checkIsNumbersInRange(numbers);
   }
 
   private void checkNumbersCount(List<Integer> numbers) {
     if (numbers.size() != NUMBER_COUNT) {
-      throw new IllegalArgumentException("로또 숫자의 개수가 잘못되었습니다!");
+      throw new IllegalArgumentException("숫자의 개수가 잘못되었습니다!");
     }
   }
 
   private void checkHasDuplicateNumber(List<Integer> numbers) {
     if(numbers.stream().distinct().count() != numbers.size()) {
-      throw new IllegalArgumentException("로또 숫자의 개수가 잘못되었습니다!");
+      throw new IllegalArgumentException("중복되는 숫자가 있습니다!");
     }
+  }
+
+  private void checkIsNumbersInRange(List<Integer> numbers) {
+    for (int number : numbers) {
+      if(!isInRange(number)){
+        throw new IllegalArgumentException("범위를 벗어나는 숫자가 있습니다!");
+      }
+    }
+  }
+
+  private boolean isInRange(int number){
+    return number >= MIN_NUMBER && number <= MAX_NUMBER;
   }
 }
