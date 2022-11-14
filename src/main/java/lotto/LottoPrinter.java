8af01e20bfb5b4bafc95217aec04ebd00a923296
BaeDongHyeon <sasd730@gmail.com>
2022-11-14 10:05:13 +0900

    feat: 발행된 로또를 출력하여 보여주기
    - 발행된 로또 리스트를 모두 출력

--- src/main/java/lotto/LottoPrinter.java
+++ src/main/java/lotto/LottoPrinter.java
@@ -2,11 +2,20 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class LottoPrinter {
     public static String inputer(String message) {
         System.out.println(message);
         String inputValue = Console.readLine();
         System.out.println();
         return inputValue;
+    }
+
+    public static void lottosPrint(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
     }
 }
