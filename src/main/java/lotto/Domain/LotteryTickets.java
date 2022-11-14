bbfee495faf1902b1210794b918cfd020b200cf1
sjkim <seoungjoo01@gmail.com>
2022-11-15 00:42:12 +0900

    feat : 2번 - 로또 발행(구매) 기능
    LotteryTickets
    - 구입 금액 만큼 로또를 발행한다.
    - 로또의 번호는 1~45 사이의 숫자로 중복 숫자가 없이 발행한다.
    - 로또의 번호는 오름차순으로 정렬해서 저장한다.

---
+++ src/main/java/lotto/Domain/LotteryTickets.java
@@ -0,0 +1,38 @@
+package lotto.Domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class LotteryTickets {
+
+    public List<Lotto> lottoTickets;
+
+    public LotteryTickets(int totalPurchaseNum) {
+        this.lottoTickets = createLottery(totalPurchaseNum);
+    }
+
+    public List<Lotto> createLottery(int totalPurchaseNum) {
+        List<Lotto> newLottoTickets = new ArrayList<>();
+        for (int i = 0; i < totalPurchaseNum; i++) {
+            newLottoTickets.add(makeNewLotto());
+        }
+        return newLottoTickets;
+    }
+
+    private Lotto makeNewLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> sortedNum = numbers.stream().sorted().collect(Collectors.toList());
+        return new Lotto(sortedNum);
+    }
+
+    public int getLottoCount() {
+        return lottoTickets.size();
+    }
+
+    public List<Lotto> getLottoTickets() {
+        return lottoTickets;
+    }
+
+}
