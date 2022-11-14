9bd42b7ac9dd418b7cf620d1188b31ec5ed28c19
wltks98 <wltks98@hanmail.net>
2022-11-14 16:27:26 +0900

    test : LottoMachine 단위 테스트 구현

---
+++ src/test/java/lotto/LottoMachineTest.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import lotto.domain.LottoMachine;
+import lotto.domain.LottoRank;
+
+public class LottoMachineTest {
+
+	@DisplayName("issueLottoNumber는 랜덤한 6개의 로또 번호를 생성한다")
+	@Test
+	void issueLottoNumber() {
+		List<Integer> results = LottoMachine.issueLottoNumber();
+		assertThat(results).hasSize(6);
+	}
+}
