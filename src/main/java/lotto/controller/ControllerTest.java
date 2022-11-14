c6452bf55e6c3d0a49505b6efb6245b26247043e
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-14 20:55:33 +0900

    type:refactor 테스트코드 method 이름 수정

--- src/main/java/lotto/controller/ControllerTest.java
+++ src/main/java/lotto/controller/ControllerTest.java
@@ -11,61 +11,61 @@ import static org.assertj.core.api.Assertions.*;
 
 public class ControllerTest {
     @Nested
-    class LottoCalculatorTest {
+    class RankOfThisLottoTest {
         final List<Integer> winningLotto = List.of(1,2,3,4,5,6);
         final int bonusNumber = 7;
 
         @Test
-        void rankOfLotto0() {
+        void nothing1() {
             List<Integer> myLotto = List.of(7,8,9,10,11,12);
             assertThat(LottoCalculator.rankOfThisLotto(myLotto, winningLotto, bonusNumber))
                     .isEqualTo(RankOfLotto.NOTHING.ordinal());
         }
 
         @Test
-        void rankOfLotto1() {
+        void nothing2() {
             List<Integer> myLotto = List.of(1,8,9,10,11,12);
             assertThat(LottoCalculator.rankOfThisLotto(myLotto, winningLotto, bonusNumber))
                     .isEqualTo(RankOfLotto.NOTHING.ordinal());
         }
 
         @Test
-        void rankOfLotto2() {
+        void nothing3() {
             List<Integer> myLotto = List.of(1,2,9,10,11,12);
             assertThat(LottoCalculator.rankOfThisLotto(myLotto, winningLotto, bonusNumber))
                     .isEqualTo(RankOfLotto.NOTHING.ordinal());
         }
 
         @Test
-        void rankOfLotto3() {
+        void fifthRank() {
             List<Integer> myLotto = List.of(1,2,3,10,11,12);
             assertThat(LottoCalculator.rankOfThisLotto(myLotto, winningLotto, bonusNumber))
                     .isEqualTo(RankOfLotto.FIFTHPRIZE.ordinal());
         }
 
         @Test
-        void rankOfLotto4() {
+        void fourthRank() {
             List<Integer> myLotto = List.of(1,2,3,4,11,12);
             assertThat(LottoCalculator.rankOfThisLotto(myLotto, winningLotto, bonusNumber))
                     .isEqualTo(RankOfLotto.FOURTHPRIZE.ordinal());
         }
 
         @Test
-        void rankOfLotto5() {
+        void thidRank() {
             List<Integer> myLotto = List.of(1,2,3,4,5,12);
             assertThat(LottoCalculator.rankOfThisLotto(myLotto, winningLotto, bonusNumber))
                     .isEqualTo(RankOfLotto.THIRDPRIZE.ordinal());
         }
 
         @Test
-        void rankOfLotto5PlusBonus() {
+        void secondRank() {
             List<Integer> myLotto = List.of(1,2,3,4,5,7);
             assertThat(LottoCalculator.rankOfThisLotto(myLotto, winningLotto, bonusNumber))
                     .isEqualTo(RankOfLotto.SECONDPRIZE.ordinal());
         }
 
         @Test
-        void rankOfLotto6() {
+        void firstRank() {
             List<Integer> myLotto = List.of(1,2,3,4,5,6);
             assertThat(LottoCalculator.rankOfThisLotto(myLotto, winningLotto, bonusNumber))
                     .isEqualTo(RankOfLotto.FIRSTPRIZE.ordinal());
