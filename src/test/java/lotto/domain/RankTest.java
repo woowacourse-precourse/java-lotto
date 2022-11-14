2dad980998b63a1ca145565fe53ac477afd62778
iamjooon2 <iamjooon2@gmail.com>
2022-11-14 19:19:13 +0900

    refactor: 하나의 함수에서 하나의 경우만 테스트 하도록 분리

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -6,22 +6,43 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RankTest {
-    @DisplayName("등수와 당첨 갯수를 잘 매치시키는지 확인한다")
+    @DisplayName("등수와 당첨 갯수를 잘 매치되는지 확인한다1 .")
     @Test
-    void getRankMatchTest() {
+    void getRankMatchTest1() {
         assertThat(Rank.FIRST.getMatch()).isEqualTo(6);
-        assertThat(Rank.SECOND.getMatch()).isEqualTo(5);
+    }
+
+    @DisplayName("등수와 당첨 갯수를 잘 매치되는지 확인한다 2.")
+    @Test
+    void getRankMatchTest2() {
         assertThat(Rank.NOTHING.getMatch()).isEqualTo(0);
     }
 
-    @DisplayName("등수 상금과 실제 금액이 일치하는지 확인한다")
+    @DisplayName("등수와 당첨 갯수를 잘 매치되는지 확인한다 3 . ")
     @Test
-    void getRankPriceTest() {
+    void getRankMatchTest3() {
+        assertThat(Rank.SECOND.getMatch()).isEqualTo(5);
+    }
+
+    @DisplayName("등수 상금과 실제 금액이 일치하는지 확인한다 1. ")
+    @Test
+    void getRankPriceTest1() {
         assertThat(Rank.FIRST.getPrice().getMoney())
                 .isEqualTo(new Money(2_000_000_000).getMoney());
+    }
+
+    @DisplayName("등수 상금과 실제 금액이 일치하는지 확인한다 2. ")
+    @Test
+    void getRankPriceTest2() {
         assertThat(Rank.SECOND.getPrice().getMoney())
                 .isEqualTo(new Money(30_000_000).getMoney());
+    }
+
+    @DisplayName("등수 상금과 실제 금액이 일치하는지 확인한다 3. ")
+    @Test
+    void getRankPriceTest3() {
         assertThat(Rank.NOTHING.getPrice().getMoney())
                 .isEqualTo(new Money(0).getMoney());
     }
+
 }
--- src/test/java/lotto/domain/ShopTest.java
+++ src/test/java/lotto/domain/ShopTest.java
@@ -6,12 +6,21 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ShopTest {
-    @DisplayName("구입한 금액에 맞는 수량만큼 로또를 발행한다.")
+    @DisplayName("구입한 금액에 맞는 양의 로또를 발행한다1.")
     @Test
-    void checkLottoPurchasingQuantity() {
+    void checkLottoPurchasingQuantity1() {
         assertThat(new Shop(new Money(1000)).buyLotto().size()).isEqualTo(1);
-        assertThat(new Shop(new Money(2000)).buyLotto().size()).isEqualTo(2);
-        assertThat(new Shop(new Money(30000)).buyLotto().size()).isEqualTo(30);
     }
 
+    @DisplayName("구입한 금액에 맞는 양의 로또를 발행한다2.")
+    @Test
+    void checkLottoPurchasingQuantity2() {
+        assertThat(new Shop(new Money(2000)).buyLotto().size()).isEqualTo(2);
+    }
+
+    @DisplayName("구입한 금액에 맞는 양의 로또를 발행한다3.")
+    @Test
+    void checkLottoPurchasingQuantity3() {
+        assertThat(new Shop(new Money(30000)).buyLotto().size()).isEqualTo(30);
+    }
 }
