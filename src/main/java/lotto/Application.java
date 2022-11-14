1d86dfbef6bd9de5b208da903b3f5f9ed206c61e
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-11-14 22:33:43 +0900

    feat: 사용자 입력 함수와 사용자 금액 validate 함수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,41 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = readInput();
+
+        final int MONEY = validateMoneyInput(input);
+        
+
     }
+
+    public static String readInput(){
+        String input = Console.readLine();
+        return input;
+    }
+
+    public static int validateMoneyInput(String input){
+        int money = parseInt(input);
+        boolean isUnitOf1K = ((money % 1000)==0);
+
+        if (isUnitOf1K==false){
+            throw new IllegalArgumentException("[Error] Money should end with 000");
+        }
+
+        return money;
+    }
+
+    public static int parseInt(String input){
+        try {
+            int money = Integer.parseInt(input);
+            return money;
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("[Error] Only numbers are allowed");
+        }
+    }
+
 }
