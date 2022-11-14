9a19ea21ea0c8e4de5709d06737561ccbac47d6a
skydreamer21 <skydreamer210@gmail.com>
2022-11-14 13:30:34 +0900

    feat(InputView): money 입력을 받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,31 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.constant.ValidationErrorMessage;
 import lotto.view.constant.InputGuidePhrase;
 
 public class InputView {
-    public static void printPurchaseMoneyRequest() {
+    static final String EMPTY_VALUE_ERROR_MESSAGE = ValidationErrorMessage.EMPTY_VALUE.getErrorMessage();
+    static final String NOT_NUMBER_ERROR_MESSAGE = ValidationErrorMessage.NOT_NUMBER.getErrorMessage();
+
+    public static int takeMoneyInput() {
+        printPurchaseMoneyRequest();
+        String input = Console.readLine().trim();
+        validate(input);
+        return Integer.parseInt(input);
+    }
+
+    private static void printPurchaseMoneyRequest() {
         System.out.println(InputGuidePhrase.PURCHASE_MONEY_REQUEST.getText());
+    }
+
+    private static void validate (String input) {
+        if (Validator.isEmpty(input)) {
+            throw new IllegalArgumentException(EMPTY_VALUE_ERROR_MESSAGE);
+        }
+
+        if (!Validator.isNumber(input)) {
+            throw new IllegalArgumentException(NOT_NUMBER_ERROR_MESSAGE);
+        }
     }
 }
