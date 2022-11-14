b891dcf3cb20d0f71eb406982941239330f9e458
SeongukBaek <bsu6235@gmail.com>
2022-11-14 21:56:04 +0900

    feat: implement class Lotto
    Added new method:
    - computeResult(Set<Integer> winningNumbers, int bonusNumber)

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
+import lotto.ui.Validator;
+
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +14,21 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != 6 || !Validator.isInRange(numbers) || Validator.isDuplicate(numbers)) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    public Result computeResult(Set<Integer> winningNumbers, int bonusNumber) {
+        int score = (int) numbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+
+        score *= 10;
+        if (score == 50 && numbers.contains(bonusNumber)) {
+            score++;
+        }
+
+        return Result.getResult(score);
+    }
 }
