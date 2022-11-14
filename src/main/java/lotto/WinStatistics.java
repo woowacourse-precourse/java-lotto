a0c1dc11405583e71296b81524effd6257e30ecf
abbd2 <ajkety00@gmail.com>
2022-11-15 01:59:31 +0900

    Feat(WinStatistics): 당첨된 번호의 개수를 비교하여 정산하는 WinStatistics 메서드 구현

--- src/main/java/lotto/WinStatistics.java
+++ src/main/java/lotto/WinStatistics.java
@@ -2,18 +2,39 @@ package lotto;
 
 public enum WinStatistics {
 
-    UNSUCCESSFUL(0, 0),
-    FIFTH(3, 5000),
-    FOURTH(4, 50000),
-    THIRD(5, 1500000),
-    SECOND(5, 30000000),
-    FIRST(6, 2000000000);
+    UNSUCCESSFUL(0, "0"),
+    FIFTH(3, "5,000원"),
+    FOURTH(4, "50,000원"),
+    THIRD(5, "1,500,000원"),
+    SECOND(5, "30,000,000원"),
+    FIRST(6, "2,000,000,000원");
 
-    private int matchCount;
-    private int prizeMoney;
+    private final int matchCount;
+    private final String prizeMoney;
 
-    WinStatistics(final int matchCount, final int prizeMoney) {
+    WinStatistics(final int matchCount, final String prizeMoney) {
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
     }
+
+    public static WinStatistics winCounter(int matchCount, boolean bonusNumber) {
+        if (matchCount == WinStatistics.SECOND.matchCount) {
+            return isBonusNumber(bonusNumber);
+        }
+
+        for (WinStatistics winValue : WinStatistics.values()) {
+            if (winValue.matchCount == matchCount) {
+                return winValue;
+            }
+        }
+        return WinStatistics.UNSUCCESSFUL;
+    }
+
+    public static  WinStatistics isBonusNumber(boolean bonusNumber) {
+        if (bonusNumber) {
+            return WinStatistics.SECOND;
+        }
+        return WinStatistics.THIRD;
+    }
+
 }
