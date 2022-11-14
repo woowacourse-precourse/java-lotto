3ee12c0b1271c354034cb01a03f78dd7cb40c3bb
moonchaewon <codnjs8989@gmail.com>
2022-11-15 04:24:02 +0900

    feat(money): implement function to get lotto ticket count with money

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,10 +1,14 @@
 package lotto;
 
+import lotto.domain.LottoMoney;
+
 public class LottoController {
 
   public void run() {
     System.out.println("LottoController.run()");
-    System.out.println(InputLotto.inputPurchaseMoney());
+    LottoMoney lottoMoney = new LottoMoney(InputLotto.inputPurchaseMoney());
+    System.out.println(lottoMoney.getMoney());
+    lottoMoney.calculateLottoTicketCount();
     System.out.println(InputLotto.inputWinningNumber());
     System.out.println(InputLotto.inputBonusNumber());
   }
---
+++ src/main/java/lotto/domain/LottoMoney.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+public class LottoMoney {
+
+  private int money;
+  private static int MONEY_UNIT = 1000;
+  
+  public LottoMoney(int money) {
+    validateMoney(money);
+    this.money = money;
+  }
+
+  public int getMoney() {
+    return this.money;
+  }
+
+  public void calculateLottoTicketCount() {
+    try {
+      int lottoTicketCount = money / MONEY_UNIT;
+      System.out.println("LottoMoney.calculateLottiTicketCount(): " + lottoTicketCount);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("[ERROR] calculateLottoTicketCount");
+    }
+  }
+
+  private void validateMoney(int money) {
+    if (isLessThanMoneyUnit(money)) {
+      throw new IllegalArgumentException("[ERROR] 입력한 로또 구입 금액이 1,000원 미만입니다.");
+    } else if (!isDivideByMoneyUnit(money)) {
+      throw new IllegalArgumentException("[ERROR] 입력한 로또 구입 금액이 1,000원 단위가 아닙니다.");
+    }
+  }
+
+  private Boolean isLessThanMoneyUnit(int money) {
+    if (money < MONEY_UNIT) {
+      return true;
+    }
+    return false;
+  }
+
+  private Boolean isDivideByMoneyUnit(int money) {
+    if (money % MONEY_UNIT == 0) {
+      return true;
+    }
+    return false;
+  }
+}
