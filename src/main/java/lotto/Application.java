fafb3f545ec2afd0d0da64cbcc96b7b8046cbf5a
JaeHong-Ahn <63873742+JaeHong-Ahn@users.noreply.github.com>
2022-11-14 17:05:50 +0900

    <feat> 구입금액 입력받은 만큼 로또를 구매
    추후에 refactor 할때 코드 개선이 필요해보임

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,37 @@
 package lotto;
 
-public class Application {
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class Application extends Lotto {
+
+    public String price = "";
+
+    public Application(List<Integer> numbers) {
+        super(numbers);
+    }
+
+    public void getLottoPrice(){
+        System.out.println("구입금액을 입력해 주세요.");
+        price = Console.readLine();
+    }
+
+    public void buyLotto(){
+        int costs = Integer.parseInt(price);
+        costs = costs / 1000;
+        System.out.println(costs + "개를 구매했습니다.");
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String price = "";
+        System.out.println("구입금액을 입력해 주세요.");
+        price = Console.readLine();     //구입금액
+        int howManyTimes = (Integer.parseInt(price))/10;
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+        Application application = new Application(numbers);
+        application.randomLotto(price);
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -16,5 +20,13 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void randomLotto(String price){
+        int cost = (Integer.parseInt(price))/1000;
+        for(int i = 0 ; i < cost; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            System.out.println(numbers);
+        }
+
+    }
+
 }
