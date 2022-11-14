207189eee0215ac9fd0b9925cb02b63a8dd0dac0
oyoungsun <plusmate@naver.com>
2022-11-14 12:30:19 +0900

    feat : enum Ranking

--- src/main/java/lotto/Ranking.java
+++ src/main/java/lotto/Ranking.java
@@ -1,16 +1,32 @@
 package lotto;
 
 public enum Ranking {
-    FIRST(1),
-    SECOND(2),
-    THIRD(3),
-    FOURTH(4),
-    FIFTH(5);
+    FIRST(1, "6개 일치 (2,000,000,000원)", 2000000000),
+    SECOND(2, "5개 일치, 보너스 볼 일치 (30,000,000원)", 30000000),
+    THIRD(3, "5개 일치 (1,500,000원)", 1500000),
+    FOURTH(4, "4개 일치 (50,000원)", 50000),
+    FIFTH(5, "3개 일치 (5,000원)", 5000);
 
     private final int ranking;
+    private final String match;
+    private final int money;
 
-    Ranking(int ranking) {
+    Ranking(int ranking, String match, int money) {
         this.ranking = ranking;
+        this.match = match;
+        this.money = money;
+    }
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public String getMatch() {
+        return match;
+    }
+
+    public int getMoney() {
+        return money;
     }
 }
 
