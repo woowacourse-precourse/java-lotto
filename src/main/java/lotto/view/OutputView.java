2a4db09f0f647c732535f75fb17b56903154bbbe
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-11-14 16:16:37 +0900

    add : param으로 받은 데이터들을 화면에 출력해주는 클래스

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+import static lotto.util.Message.*;
+import static lotto.util.Util.*;
+
+import lotto.Entity.Lotto;
+import lotto.Entity.ResultLotto;
+import lotto.util.MatchMoney;
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class OutputView {
+
+}
