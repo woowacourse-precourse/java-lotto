18ea4b6949c0dec3ee91f57f0411e082bed51b01
YSEO <1park4170@gmail.com>
2022-11-14 19:54:48 +0900

    feat<constant values> : values appear frequently integrate into one class, as constant value
    - MIN_LOTTO_NUM = 1
    - MAX_LOTTO_NUM = 45
    - LOTTO_LENGTH = 6
    - PRICE_PER_LOTTO = 1000

---
+++ src/main/java/lotto/ConstValue.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class ConstValue {
+    public static final int MIN_LOTTO_NUM = 1;
+    public static final int MAX_LOTTO_NUM = 45;
+
+    public static final int LOTTO_LENGTH = 6;
+    public static final int PRICE_PER_LOTTO = 1000;
+}
