502226fb861b506619bddcc80dd4478bb5677076
Yujin830 <rladbwls30@naver.com>
2022-11-15 00:00:47 +0900

    Feat: private로 선언한 변수 내보내주기

--- src/main/java/lotto/PrizeInfo.java
+++ src/main/java/lotto/PrizeInfo.java
@@ -7,12 +7,16 @@ public enum PrizeInfo {
     Second("5개 일치, 보너스 볼 일치","(30,000,000원)",30000000),
     One("6개 일치", "(2,000,000,000원)", 2000000000);
 
-    private final String count, money;
+    private final String count, printmoney;
     private final int realmoney;
 
-    private PrizeInfo(String count, String money, int realmoney){
+    private PrizeInfo(String count, String printmoney, int realmoney){
         this.count = count;
-        this.money = money;
+        this.printmoney = printmoney;
         this.realmoney = realmoney;
     }
+
+    public String getCount() { return this.count; }
+    public String getPrintMoney() { return this.printmoney; }
+    public int getRealMoney() { return this.realmoney; }
 }
