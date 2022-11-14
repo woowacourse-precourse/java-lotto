2ec68152c1a145b740ec61e11ff0eff148686b68
Torres-09 <dla612@naver.com>
2022-11-14 18:50:27 +0900

    feat : 당첨 번호 입력 기능
    Input class - > EnterWinningNumber()

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -2,11 +2,26 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Input {
-    public static int purchaseAmount() {
+    // 구입 금액 입력 기능
+    public static int EnterPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String purchaseAmount = Console.readLine();
 
         return Integer.valueOf(purchaseAmount);
+    }
+
+    // 당첨 번호 입력 기능
+    public static List<Integer> EnterWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+
+        String winningNumber = Console.readLine();
+
+        return Arrays.stream(winningNumber.split(",")).map(i -> Integer.valueOf(i)).collect(Collectors.toList());
     }
 }
