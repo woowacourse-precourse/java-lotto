69db522391c698a09a3875f84f7fac5c0e6b4af2
gyumin159 <gyumin159@naver.com>
2022-11-14 17:16:12 +0900

    fix : 2등과 3등이 같게 나오는 것을 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,7 +9,6 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        Collections.sort(numbers); // 오름차순 정렬
         this.numbers = numbers;
     }
 
@@ -18,10 +18,24 @@ public class Lotto {
         }
     }
     public void printLotto(){
-        System.out.println(this.numbers);
+        List<Integer> sortedLotto = new ArrayList<>(this.numbers);
+        Collections.sort(sortedLotto);
+        System.out.println(sortedLotto);
     }
 
     public int compareWithWinningNumbers(List<Integer> winningNumbers){
+        int bonusNumber = winningNumbers.get(6);
+        int sameNumCount = getSameNumCount(winningNumbers);
+        if (sameNumCount == 5 && hasBonusNumber(bonusNumber)) {
+            return 1;
+        }
+        if (sameNumCount <= 5) { // 2등과 3등 둘다 sameNumCount 가 5이기 때문에 3등부터 1씩 뺌
+            sameNumCount--;
+        }
+        return whichPlace(sameNumCount);
+    }
+
+    private int getSameNumCount(List<Integer> winningNumbers) {
         List<Integer> withoutBonusNumber = winningNumbers.subList(0,6);
         int sameNumCount = 0;
         for (Integer num :this.numbers){
@@ -29,15 +43,19 @@ public class Lotto {
                 sameNumCount++;
             }
         }
-        return whichPlace(sameNumCount);
+        return sameNumCount;
     }
 
     private int whichPlace(int sameNumCount){ // 같은 번호가 얼마나 맞았는지를 통해 몇 등인지 계산
         int place = Math.abs(sameNumCount - 6);
-        if (place > 3) { // 5등 이하, 즉 같은 번호가 3개도 안되는 경우들
-            return 4;
+        if (place > 4) { // 5등 이하, 즉 같은 번호가 3개도 안되는 경우들
+            return 5;
         }
         return place;
+    }
+
+    public boolean hasBonusNumber(int bonusNumber){
+        return this.numbers.contains(bonusNumber);
     }
 
 }
