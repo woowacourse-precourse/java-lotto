c7270cb156868042b4f247d4b28b91290066af89
buddy <jang_taehwan@naver.com>
2022-11-14 17:06:55 +0900

    refactor(LottoSeller): LottoNumberCreator 를 의존하지 않도록 수정

--- src/main/java/lotto/application/LottoSeller.java
+++ src/main/java/lotto/application/LottoSeller.java
@@ -1,8 +1,12 @@
 package lotto.application;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import lotto.domain.Lotto;
+import lotto.domain.LottoConstant;
 
 public class LottoSeller {
 
@@ -26,10 +30,9 @@ public class LottoSeller {
         long sellableLotto = amount / LOTTO_PRICE;
 
         List<Lotto> lottos = new ArrayList<>();
-        LottoNumberCreator lottoNumberCreator = LottoNumberCreator.getInstance();
 
         for (int i = 0; i < sellableLotto; i++) {
-            List<Integer> numbers = lottoNumberCreator.getNumbers();
+            List<Integer> numbers = createRandomNumbers();
             Lotto lotto = new Lotto(numbers);
 
             lottos.add(lotto);
@@ -41,5 +44,15 @@ public class LottoSeller {
         if (amount % 1000 != 0) {
             throw new IllegalArgumentException("로또 구매 금액은 1,000 단위로만 입력 가능합니다.");
         }
+    }
+
+    private List<Integer> createRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(
+                LottoConstant.MIN_NUMBER.getValue(),
+                LottoConstant.MAX_NUMBER.getValue(),
+                LottoConstant.NUMBERS_SIZE.getValue())
+            .stream()
+            .sorted(Comparator.reverseOrder())
+            .collect(Collectors.toList());
     }
 }
