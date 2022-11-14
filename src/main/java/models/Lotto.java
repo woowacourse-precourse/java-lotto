e8ff49f0a5c247f4535b1327f70d56afb232abfe
myungsikyoon <yms8795@gmail.com>
2022-11-14 16:12:10 +0900

    refactor : 유효성 검증 코드를 validation 패키지로 분리.

--- src/main/java/models/Lotto.java
+++ src/main/java/models/Lotto.java
@@ -1,8 +1,6 @@
 package models;
 
-import constants.LottoConstant;
-import system.process.exception.IllegalArgument;
-import utils.IsCollection;
+import models.validation.LottoValidation;
 
 import java.util.Collections;
 import java.util.List;
@@ -11,36 +9,9 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        LottoValidation.validate(numbers);
         Collections.sort(numbers);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        validateSize(numbers);
-        validateInRange(numbers);
-        validateDuplication(numbers);
-    }
-
-    private void validateSize(List<Integer> numbers) {
-        if (IsCollection.isSetSize(numbers, LottoConstant.NUMBERS_SIZE)) {
-            return;
-        }
-        IllegalArgument.handleException(IllegalArgument.NOT_SET_SIZE.getMessage());
-    }
-
-    private void validateInRange(List<Integer> numbers) {
-        if (IsCollection.isNumbersInRange(numbers, LottoConstant.NUMBER_START, LottoConstant.NUMBER_END)) {
-            return;
-        }
-        IllegalArgument.handleException(IllegalArgument.NOT_IN_RANGE.getMessage());
-
-    }
-
-    private void validateDuplication(List<Integer> numbers) {
-        if (IsCollection.isDuplication(numbers)) {
-            IllegalArgument.handleException(IllegalArgument.DUPLICATION.getMessage());
-        }
     }
 
     public List<Integer> getNumbers() {
