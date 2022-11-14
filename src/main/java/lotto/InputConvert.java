31677cef8c75004536621bd8d609bb1b27e6e374
MuSeung <peter0757@nvaer.com>
2022-11-14 13:10:30 +0900

    feat: InputConvert이 예외 상황일때만 예외를 발생시키고 리스트를 반환하도록 기능 구현
    InputConvert
        - lottoNumber : 숫자 이외의 문자를 모두 없앤 문자열의 길이와 원래 문자열의 길이의 차이보다 ,로 분리된 문자열의 개수가 1개 더 많으면 정상적인 문자열이 입력 된 것임을 이용하여 판별후 예외 발생하도록 하고 List를 반환하도록 함.

--- src/main/java/lotto/InputConvert.java
+++ src/main/java/lotto/InputConvert.java
@@ -5,9 +5,14 @@ import java.util.List;
 public class InputConvert {
 
     public static List<Integer> lottoNumber(String input) {
-        if(true) {
+        if(input.length() == 0) {
             throw new IllegalArgumentException();
         }
-        return List.of();
+        String onlyNumber = input.replaceAll("[^0-9]", "");
+        String[] numbers = input.split(",");
+        if(input.length() - onlyNumber.length() + 1 != numbers.length){
+            throw new IllegalArgumentException();
+        }
+        return List.of(1,2,3,4,5,6);
     }
 }
