4e7edc1a743d9f7f09e88f4e975c9a1eca2cee60
Donguk <95donguk.dev@gmail.com>
2022-11-15 05:26:48 +0900

    style(ExceptionMessage): 에러 메시지 출력 스타일 변경

--- src/main/java/lotto/resource/message/ExceptionMessage.java
+++ src/main/java/lotto/resource/message/ExceptionMessage.java
@@ -7,12 +7,12 @@ public enum ExceptionMessage {
     NON_UNITS_OF_ONE_THOUSAND_WON(Constant.PRICE_PER_LOTTO.getValue() + "원 단위가 아닌 금액입니다."),
     NO_MONEY("금액이 0원 입니다."),
     APPLICATION_EXIT("에러로 인해 시스템을 종료합니다."),
-    INVALID_NUMBER("번호의 범위가 " + Constant.BEGIN_NUMBER.getValue() + "~"
-            + Constant.END_NUMBER.getValue() + "까지가 아닌 숫자입니다."),
+    INVALID_NUMBER("로또 번호는 " + Constant.BEGIN_NUMBER.getValue() + "부터 "
+            + Constant.END_NUMBER.getValue() + " 사이의 숫자여야 합니다."),
     DUPLICATE_NUMBER("중복된 번호가 있습니다."),
     INVALID_NUMBER_COUNT("로또 번호의 개수가 " + Constant.REQUIRED_NUMBER_COUNT.getValue() + "개가 아닙니다.");
 
-    private static final String ERROR_MESSAGE = "[ERROR]";
+    private static final String ERROR_MESSAGE = "[ERROR] ";
 
     private final String message;
 
