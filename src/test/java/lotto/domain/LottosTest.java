a3544942532afde7c4061dae9695f97f1639c7c2
saint6839 <saint6839@gmail.com>
2022-11-14 19:46:18 +0900

    test(domain) : Lottos의 메서드를 호출하여 테스트하도록 변경

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,43 +1,34 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottosTest {
-    @DisplayName("구매한 수량에 맞게 총 로또 수가 잘 생성되는지 테스트")
+    @DisplayName("구매한 수량에 맞게 로또들이 리스트에 잘 추가되어지는지 테스트한다.")
     @Test
     void generateLottos() {
         //given
+        Lottos lottos = new Lottos();
         int lottoCount = 8;
-        List<Lotto> lottos = new ArrayList<>();
         //when
-        for (int i = 0; i < lottoCount; i++) {
-            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
-        }
+        lottos.generateLottos(lottoCount);
         //then
-        assertThat(lottos.size())
-                  .isEqualTo(lottoCount);
+        assertThat(lottos.getLottos().size()).isEqualTo(lottoCount);
     }
 
-    @DisplayName("구매한 로또들이 정렬되어서 반환되는지 테스트")
+    @DisplayName("구매한 로또들이 정렬되어서 반환되는지 테스트한다.")
     @Test
     void checkLottos() {
         //given
-        List<Lotto> lottos = new ArrayList<>(List.of(new Lotto(List.of(6, 5, 4, 3, 2, 1))));
-        Lottos purchasedLottos = new Lottos();
+        Lottos lottos = new Lottos();
+        lottos.add(new Lotto(List.of(6,5,4,3,2,1)));
+
         //when
-        for (Lotto lotto : lottos) {
-            List<Integer> sortedNumbers = new ArrayList<>(lotto.getNumbers());
-            Collections.sort(sortedNumbers);
-            purchasedLottos.getLottos().add(new Lotto(sortedNumbers));
-        }
+        Lottos purchasedLottos = lottos.checkLottos();
         //then
         for (int i = 0; i < 6; i++) {
             assertThat(purchasedLottos.getLottos()
@@ -45,6 +36,5 @@ class LottosTest {
                                       .getNumbers()
                                       .get(i)).isEqualTo(i+1);
         }
-
     }
 }
