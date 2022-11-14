691c40ddca16a52148ff85e5f90728a86939e87b
desfox <desfox99@naver.com>
2022-11-14 18:59:38 +0900

    feat(InputView): 구입금액 입력 검증

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.validation.PurchasePriceInputValidation;
 
 public class InputView {
     OutputView outputView = new OutputView();
@@ -8,6 +9,9 @@ public class InputView {
     public int userInputPurchasePrice() {
         outputView.printPurchasePriceMessage();
         String inputPurchasePrice = Console.readLine();
+
+        PurchasePriceInputValidation purchasePriceInputValidation = new PurchasePriceInputValidation();
+        purchasePriceInputValidation.validate(inputPurchasePrice);
 
         int purchasePrice = Integer.parseInt(inputPurchasePrice);
         return purchasePrice;
