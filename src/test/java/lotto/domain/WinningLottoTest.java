8f29e07d052bd7c03357c26a5871dba9588ab58e
yoon <beatus96@naver.com>
2022-11-14 23:08:26 +0900

    test(WinningLotto) : 유효하지 않은 값 입력시 생성자 기능 테스트 추가
    당첨로또 클래스 테스트 추가
    
    3. 유효하지 않은 로또번호나 보너스 번호로 객체 생성 시도시 예외 발생하는지 테스트
    
    #5. 보너스 번호가 45 이상이어도 예외 발생하지 않음

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -14,49 +14,71 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WinningLottoTest {
 
-    private static Stream<Arguments> provideDuplicatedNumbersAndBonusNumber() {
+    private static Stream<Arguments> provide_WinningNumbers_And_DuplicatedBonusNumber() {
         return Stream.of(
-            Arguments.of(List.of(5,6,7,8,9,10), 5),
-            Arguments.of(List.of(7,8,9,10,11,12), 12)
+            Arguments.of(List.of(5, 6, 7, 8, 9, 10), 5),
+            Arguments.of(List.of(7, 8, 9, 10, 11, 12), 12)
         );
     }
 
+    private static Stream<Arguments> provide_Lotto_And_Rank() {
+        return Stream.of(
+            Arguments.of(List.of(11, 12, 13, 14, 15, 16), FIRST),
+            Arguments.of(List.of(11, 12, 13, 14, 15, 17), SECOND),
+            Arguments.of(List.of(11, 12, 13, 14, 15, 1), THIRD),
+            Arguments.of(List.of(11, 12, 13, 14, 1, 17), FOURTH), //보너스번호 같은경우
+            Arguments.of(List.of(11, 12, 13, 14, 1, 2), FOURTH),
+            Arguments.of(List.of(11, 12, 13, 1, 2, 17), FIFTH),
+            Arguments.of(List.of(11, 12, 13, 1, 2, 3), FIFTH),
+            Arguments.of(List.of(11, 12, 1, 2, 3, 17), null),
+            Arguments.of(List.of(11, 1, 2, 3, 4, 17), null),
+            Arguments.of(List.of(1, 2, 3, 4, 5, 17), null)
+        );
+    }
+
+    private static Stream<Arguments> provide_InvalidWinningNumbers_And_BonusNumber() {
+        return Stream.of(
+            //유효하지 않은 로또번호 (개수 / 중복 / 범위)
+            Arguments.of(List.of(1, 2, 3), 45),
+            Arguments.of(List.of(123456), 45),
+            Arguments.of(List.of(7, 8, 9, 10, 11, 11), 45),
+            Arguments.of(List.of(7, 8, 9, 10, 77, 58), 45),
+            //유효하지 않은 로또번호와 중복 보너스 번호
+            Arguments.of(List.of(1, 2, 3, 12), 12),
+            //유효하지 않은 보너스 번호
+            Arguments.of(List.of(1, 2, 3, 4,5,6), 55)
+        );
+    }
+
+    //보너스 번호 중복 테스트
     @DisplayName("당첨번호와 보너스 번호가 중복되면 예외가 발생한다")
     @ParameterizedTest
-    @MethodSource("provideDuplicatedNumbersAndBonusNumber")
+    @MethodSource("provide_WinningNumbers_And_DuplicatedBonusNumber")
     void validateBonusNumberTest(List<Integer> winningNumbers, int bonusNumber) {
-        assertThatThrownBy(()->
+        assertThatThrownBy(() ->
             WinningLotto.validateBonusNumber(winningNumbers, bonusNumber))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
-
     //getRankOf 테스트
     @DisplayName("예상한 등수(enum)를 반환하는지 테스트")
     @ParameterizedTest
-    @MethodSource("provideLottoAndRank")
+    @MethodSource("provide_Lotto_And_Rank")
     void getRankofTest(List<Integer> lottoNumbers, Rank expected) {
-        WinningLotto winningLotto = new WinningLotto(List.of(11,12,13,14,15,16), 17);
+        WinningLotto winningLotto = new WinningLotto(List.of(11, 12, 13, 14, 15, 16), 17);
 
         Lotto lotto = new Lotto(lottoNumbers);
         assertThat(winningLotto.getRankof(lotto)).isEqualTo(expected);
     }
 
-    private static  Stream<Arguments> provideLottoAndRank() {
-        return Stream.of(
-            Arguments.of(List.of(11,12,13,14,15,16), FIRST),
-            Arguments.of(List.of(11,12,13,14,15,17), SECOND),
-            Arguments.of(List.of(11,12,13,14,15,1), THIRD),
-            Arguments.of(List.of(11,12,13,14,1,17), FOURTH), //보너스번호 같은경우
-            Arguments.of(List.of(11,12,13,14,1,2), FOURTH),
-            Arguments.of(List.of(11,12,13,1,2,17), FIFTH),
-            Arguments.of(List.of(11,12,13,1,2,3), FIFTH),
-            Arguments.of(List.of(11,12,1,2,3,17), null),
-            Arguments.of(List.of(11,1,2,3,4,17), null),
-            Arguments.of(List.of(1,2,3,4,5,17), null)
-        );
+    //생성자 테스트
+    @DisplayName("유효하지 않은 로또 번호를 넣어 객체 생성을 시도하면, 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("provide_InvalidWinningNumbers_And_BonusNumber")
+    void createWinningLottoTest(List<Integer> winningNumbers, int bonusNumber) {
+        assertThatThrownBy(() -> new WinningLotto(winningNumbers, bonusNumber))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
 
-    //생성자 테스트 : 유효하지 않은 리스트 넣었을 때, 예외발생하는지 테스트 tmp파일 참고
 }
