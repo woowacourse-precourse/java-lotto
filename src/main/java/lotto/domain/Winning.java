8add58a9138f53e1704988a9eb3b45cf9fef573d
MinjeongKong <kong1301@ajou.ac.kr>
2022-11-14 15:43:37 +0900

    feat: 당첨번호 입력 유효성 검사

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -5,16 +5,23 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Winning {
+public class Winning extends Valid {
     private List<Integer> numbers;
     private int bonus;
 
     public Winning() {
     }
 
-    public void pickNum() {
+    public void pickNum() throws IllegalArgumentException {
         String[] input = Console.readLine().split(",");
         this.numbers = saveNumbers(input);
+        validate(this.numbers);
+    }
+
+    private void validate(List<Integer> numbers) throws IllegalArgumentException {
+        isCorrectSize(numbers);
+        isCorrectRange(numbers);
+        isNoDuplicate(numbers);
     }
 
     private List<Integer> saveNumbers(String[] numbers) {
