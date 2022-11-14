ea75e8b5d3dfb94411f2223b33007f6902f0914d
JaehongDev <println@kakao.com>
2022-11-14 19:50:09 +0900

    test (RandomLottoSystem) : 랜덤한 로또를 생성하는 시스템이 정상적으로 출력하는지 검증

--- src/test/java/lotto/system/RandomLottoSystemTest.java
+++ src/test/java/lotto/system/RandomLottoSystemTest.java
@@ -2,13 +2,13 @@ package lotto.system;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class RandomLottoSystemTest {
@@ -25,20 +25,16 @@ class RandomLottoSystemTest {
         System.setOut(standardOut);
     }
 
-    @Test
-    void 사용자가_금액을_입력하면_입력한_금액만큼_랜덤한_로또를_만들어냅니다() {
-      /*  var numbers = List.of(
-                List.of(1, 2, 3, 4, 5, 6),
-                List.of(2, 3, 4, 5, 6, 7)
-        );
-        var randomLottoSystem = new RandomLottoSystem(new IoSystemImpl());
-        randomLottoSystem.generateLottoBundle(numbers);
+    @ParameterizedTest
+    @CsvSource(value = {"1000:1", "2000:2", "30000:30", "100000:100", "9999000:9999"}, delimiterString = ":")
+    void 사용자가_금액을_입력하면_입력한_금액만큼_랜덤한_로또를_만들어냅니다(final String moneyInput, final String countInput) {
+        var fakeIoSystem = new IoSystemFake(moneyInput);
+        var randomLottoSystem = new RandomLottoSystem(fakeIoSystem);
 
-        var actual = outputStreamCaptor.toString();
+        randomLottoSystem.generateLottoBundle();
 
-        var expected = "2개를 구매했습니다.\n"
-                + "[1, 2, 3, 4, 5, 6]\n"
-                + "[2, 3, 4, 5, 6, 7]\n";
-        Assertions.assertThat(actual).isEqualTo(expected);*/
+        var actual = outputStreamCaptor.toString().split("\n");
+        var expected = new String[]{"구입금액을 입력해 주세요.", countInput + "개를 구매했습니다."};
+        Assertions.assertThat(actual).contains(expected);
     }
 }
