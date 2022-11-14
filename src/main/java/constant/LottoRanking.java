43d5572a74340a052e9c9023372fb8a8276a0d09
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-14 17:33:03 +0900

    fix(LottoRanking) : 각 등수에대한 상수를 리턴하도록 고침.

--- src/main/java/constant/LottoRanking.java
+++ src/main/java/constant/LottoRanking.java
@@ -4,7 +4,7 @@ public enum LottoRanking {
     FIRST(1), SECOND(2), THIRD(3), FOURTH(4), FIFTH(5);
     int value;
     LottoRanking(int rankingValue){
-        this.value = value;
+        this.value = rankingValue;
     }
     public int getRanking(){
         return value;
