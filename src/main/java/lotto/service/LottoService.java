a010d1523e72ac2e44be3088f0a7627cfb7b5718
cocomozila <joseyun94@naver.com>
2022-11-14 20:10:05 +0900

    feat(LottoService): 로또 구매하는 기능 로직 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,18 +1,24 @@
 package lotto.service;
 
+import lotto.domain.LottoMachine;
 import lotto.view.LottoView;
 
 public class LottoService {
 
     private LottoView lottoView;
+    private LottoMachine lottoMachine;
 
     public LottoService() {
         this.lottoView = new LottoView();
+        this.lottoMachine = new LottoMachine();
     }
 
     // 로또 구매하는 기능
-    public void buyLotto() {
-
+    public void buyLotto() throws IllegalArgumentException  {
+        lottoView.displayInputMoney();
+        lottoMachine.inputMoney();
+        int numberOfLottoPurchases = lottoMachine.getNumberOfLottoPurchases();
+        lottoView.displayLottoAmount(numberOfLottoPurchases);
     }
 
     // 당첨 번호 입력하는 기능
