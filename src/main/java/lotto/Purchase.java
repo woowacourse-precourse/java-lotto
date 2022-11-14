597c8f706dd82edadc356753371855b523cbf4ca
LAPTOP-4NC4E864\yongs <yongs170@naver.com>
2022-11-14 14:19:42 +0900

    feat(Purchase) : costValidation 도입 및 로또 구입 구현

--- src/main/java/lotto/Purchase.java
+++ src/main/java/lotto/Purchase.java
@@ -1,2 +1,22 @@
-package lotto;public class Purchase {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Purchase {
+    List<Lotto> myLotto = new ArrayList<>();
+
+    public void lotto(Integer cost){
+        Validation.costValidation(cost);
+        Integer count = cost / 1000;
+        while(count != 0) {
+            myLotto.add(new Lotto(Randoms.pickUniqueNumbersInRange(Validation.RANGE_START, Validation.RANGE_END, Validation.LOTTO_COUNT)));
+            count -=1;
+        }
+    }
+
+
+
 }
