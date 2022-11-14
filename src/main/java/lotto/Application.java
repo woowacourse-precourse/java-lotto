609a540b34037e53043f83ed2f9cf53c9543c19a
ckn1070 <ckn1070@naver.com>
2022-11-15 00:17:53 +0900

    feat(lotto): add initializing lotto game

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,14 +2,7 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        IOManage io = new IOManage();
-
-        io.inputPayment();
-        io.outputLottoGeneration();
-        io.inputWinningNumbers();
-        io.inputBonusNumber();
-        io.outputStatistic();
-        io.outputEarningRate();
         // TODO: 프로그램 구현
+        new IOManage();
     }
 }
