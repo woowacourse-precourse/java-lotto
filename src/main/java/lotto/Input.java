e4a7725e441a0430e2a6d5343fb9850637b71ef0
meunrud <y3a0522@gmail.com>
2022-11-15 01:19:59 +0900

    :sparkles: 당첨번호 입력받기 구현

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -2,10 +2,28 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Input {
+
+    public static final int CHEON_WON = 1000;
 
     public int inputMoney(){
         String amountOfMoney = Console.readLine();
         return Integer.parseInt(amountOfMoney);
     }
+
+    public String  inputWinningLottosNumber(){
+        return Console.readLine();
+    }
+
+
+
+//    public void checkMoneyUnit(int money){
+//        if(!(money / CHEON_WON == 0)){
+//            throw new IllegalArgumentException("[ERROR] 금액은 천원 단위로 입력해주세요");
+//        }
+//    }
+
+
 }
