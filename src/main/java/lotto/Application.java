10bab2c0eac38c9ddc5fbf762f4a70b81acffe60
limzzum <chfhddl1234@naver.com>
2022-11-15 01:28:08 +0900

    fix : try/catch문 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,18 +8,22 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        LottoMachine machine = new LottoMachine();
-        machine.getInputMoney();
-        List<Lotto> lottos = machine.createLottoNumber();
+        try {
+            LottoMachine machine = new LottoMachine();
+            machine.getInputMoney();
+            List<Lotto> lottos = machine.createLottoNumber();
 
-        User user = new User(lottos);
-        user.inputWinningNumber();
-        user.inputBonusNumber();
+            User user = new User(lottos);
+            user.inputWinningNumber();
+            user.inputBonusNumber();
 
-        machine.printLottos();
-        machine.getResult(user.getWinningNumber(),user.getBonusNumber());
+            machine.printLottos();
+            machine.getResult(user.getWinningNumber(), user.getBonusNumber());
 
-        machine.printResult();
+            machine.printResult();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
 
