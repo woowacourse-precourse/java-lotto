e2cbb5868a29d1f5478123d60a957b780e8d433e
JuSeong1130 <jsmoon1130@naver.com>
2022-11-15 06:15:10 +0900

    test(LottoAutoGenerator): 금액에 맞는 로또 개수 리턴 테스트

--- src/test/java/lotto/domain/lotto/LottoAutoGeneratorTest.java
+++ src/test/java/lotto/domain/lotto/LottoAutoGeneratorTest.java
@@ -1,18 +1,33 @@
 package lotto.domain.lotto;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
+import lotto.domain.money.Money;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoAutoGeneratorTest {
 
 	private LottoAutoGenerator lottoAutoGenerator;
+
 	@BeforeEach
 	void setUp() {
 		lottoAutoGenerator = new LottoAutoGenerator();
 	}
 
+	@DisplayName("금액에 맞는 로또 개수 리턴")
+	@Test
+	void createLottoAccordingToTheAmount() {
+		List<Lotto> lottos = lottoAutoGenerator.createLottoAccordingToTheAmount(
+				new Money("8000"));
+		assertEquals(8,lottos.size());
+	}
+
+
+	@DisplayName("로또 자동 생성")
 	@Test
 	void 로또_자동_생성() {
 		Lotto lotto = lottoAutoGenerator.createLottoByAuto();
