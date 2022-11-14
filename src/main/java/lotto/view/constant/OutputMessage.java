467e9305d213b2d2d011feeafc348e9060bea9d2
jihwan <sgh002400@naver.com>
2022-11-14 22:05:33 +0900

    feat(Input,OutputMessage): 입출력시 필요한 메시지 상수 생성

---
+++ src/main/java/lotto/view/constant/OutputMessage.java
@@ -0,0 +1,13 @@
+package lotto.view.constant;
+
+public class OutputMessage {
+	public static final String OUTPUT_PURCHASE_AMOUNT = "%d개를 구매했습니다.\n";
+	public static final String OUTPUT_WINNING_STATISTICS = "당첨 통계";
+	public static final String OUTPUT_WINNING_STATISTICS_DIVIDER = "---";
+	public static final String OUTPUT_WINNING_STATISTICS_FORMAT_5TH = "3개 일치 (5,000원) - %d개\n";
+	public static final String OUTPUT_WINNING_STATISTICS_FORMAT_4TH = "4개 일치 (50,000원) - %d개\n";
+	public static final String OUTPUT_WINNING_STATISTICS_FORMAT_3RD = "5개 일치 (1,500,000원) - %d개\n";
+	public static final String OUTPUT_WINNING_STATISTICS_FORMAT_2ND = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n";
+	public static final String OUTPUT_WINNING_STATISTICS_FORMAT_1ST = "6개 일치 (2,000,000,000원) - %d개\n";
+	public static final String OUTPUT_PROFIT_RATE = "총 수익률은 %.1f%%입니다.";
+}
