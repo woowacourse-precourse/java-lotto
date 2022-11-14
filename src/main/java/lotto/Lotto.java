4d6ce21dfb1e5a5676aa26bec3460987b4dd226b
hwi215 <buc06137@naver.com>
2022-11-15 01:56:43 +0900

    feat: [기능2] private -> public으로 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,8 @@ package lotto;
 
 import java.util.List;
 
+import static lotto.view.Input.input;
+
 public class Lotto {
     private final List<Integer> numbers;
 
@@ -14,6 +16,15 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public static int lottoNum(){
+        int amount = input();
+        if(amount % 1000 != 0){
+            throw new IllegalArgumentException();
+        }
+        int num = amount / 1000;
+        return num;
     }
 
     // TODO: 추가 기능 구현
