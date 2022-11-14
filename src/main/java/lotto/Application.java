d55d9015ca7d23b4920973fbbbb0aa8df6f33830
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-15 01:23:13 +0900

    Move : 이동 LottoGame controller 패키지로

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,6 @@
 package lotto;
 
-import lotto.view.InputView;
-import lotto.view.OutputView;
+import lotto.controller.LottoGame;
 
 public class Application {
     public static void main(String[] args){
--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.controller.LottoGame;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,6 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueN
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class LottoGameTest extends NsTest {
     LottoGame lottoGame;
