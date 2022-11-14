acc9e0adbfe1f0e8430239b780916494f232c696
ohhonggi <5ghdrl@naver.com>
2022-11-15 03:12:14 +0900

    refactor($ValidUtil): Modify return type
    This change causes the function in ValidUtil
     to have a return value.
    
    function
    - validInputAmounts
    - validAmountUnit

--- src/main/java/lotto/util/ValidUtil.java
+++ src/main/java/lotto/util/ValidUtil.java
@@ -6,17 +6,19 @@ import static lotto.constants.AmountCode.AMOUNTS_UNIT;
 public class ValidUtil {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
-    public static void validInputAmounts(String input) {
+    public static String validInputAmounts(String input) {
         if (!input.matches("^[0-9]+$")) {
             throw new IllegalArgumentException(ERROR_MESSAGE + " 숫자를 입력해야 합니다.");
         }
+        return input;
     }
 
-    public static void validAmountUnit(int amount) {
+    public static int validAmountUnit(int amount) {
         if (amount % AMOUNTS_UNIT != 0) {
             throw new IllegalArgumentException(ERROR_MESSAGE +
                     " 로또 금액은 1000원 단위이어야 합니다.");
         }
+        return amount;
     }
 
 }
