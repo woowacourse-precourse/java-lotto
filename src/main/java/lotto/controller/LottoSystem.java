67997d4fb810893fb75b0db696147be1661c2438
sora5421 <21800804@handong.edu>
2022-11-15 01:23:05 +0900

    feat : Lotto system 추가

--- src/main/java/lotto/controller/LottoSystem.java
+++ src/main/java/lotto/controller/LottoSystem.java
@@ -1,13 +1,49 @@
 package lotto.controller;
 
-import lotto.datamodel.WinningNumbers;
+import lotto.datamodel.*;
+import lotto.view.UserInteraction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoSystem {
     public LottoSystem() {
     }
 
     public void start(){
-        WinningNumbers winNumbers = new WinningNumbers();
+        UserInteraction user = new UserInteraction();
+
+        int lottoCount;
+        int aNumber;
+        List<List<Integer>> lottoNumbers;
+        List<Integer> numbers;
+
+        //input the money
+        String input = user.money();
+        try{
+            PurchaseAmount purchase = new PurchaseAmount(input);
+            lottoCount = purchase.getMoneyCount();
+
+            MakeLottoNumber makeLotto = new MakeLottoNumber(lottoCount);
+            lottoNumbers = makeLotto.makeNumbers();
+            user.showLotto(lottoCount, lottoNumbers);
+
+            numbers =  user.winningNumbers();
+            Lotto win = new Lotto(numbers);
+
+            aNumber = user.bonusNumber();
+            BonusLotto bonus = new BonusLotto(aNumber, win.getNumbers());
+
+            CompareLottoNumbers compare = new CompareLottoNumbers(win.getNumbers(), bonus.getBonusNumber());
+            List<Integer> prize = compare.calculator(lottoCount, lottoNumbers);
+
+            user.printResult(prize, lottoCount);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] : 입력값을 정확하게 입력하세요");
+        }
+
+
 
     }
 }
