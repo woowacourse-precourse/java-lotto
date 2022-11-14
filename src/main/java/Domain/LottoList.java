08814ba5d1eb1fc5f2cbd6c312f471409b6d486e
ljh <dusk5588@naver.com>
2022-11-15 03:16:26 +0900

    feat(LottoList) : 여러개의 로또 번호들을 저장하는 클래스 구현

--- src/main/java/Domain/LottoList.java
+++ src/main/java/Domain/LottoList.java
@@ -1,5 +1,40 @@
 package Domain;
 
-public class LottoList {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class LottoList {
+    tatic final int LOTTO_PRICE = 1000;
+
+    private int lottoPlayNumber;
+    private List<Lotto> lottoList;
+
+    public LottoList(){
+        InputView.printBuyingLottoMessage();
+        lottoPlayNumber = getLottoPlayNumber();
+        lottoList = makeRandomLottoNumbers(lottoPlayNumber);
+        OutputView.printNumberOfLotto(lottoPlayNumber, lottoList);
+    }
+
+    private int getLottoPlayNumber(){
+        String readMoneyValue = InputView.readInputLine();
+        Validation.validateMoneyInput(readMoneyValue);
+        int money = Integer.parseInt(readMoneyValue);
+        return money / LOTTO_PRICE;
+    }
+
+    private List<Lotto> makeRandomLottoNumbers(int lottoPlayNumber){
+        List<Lotto> lottoList = new ArrayList<>();
+        for(int playLottoCount=0; playLottoCount<lottoPlayNumber; playLottoCount++){
+            List<Integer> playLotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(playLotto);
+            lottoList.add(new Lotto(playLotto));
+        }
+        return lottoList;
+    }
+
+    public List<Lotto> getLottoList(){
+        return lottoList;
+    }
 }
