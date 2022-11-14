89ce0d64543082566380ca93e8cc9f700be5b7ef
채희찬 <eqholic1125@gmail.com>
2022-11-14 22:59:46 +0900

    feat: 당첨 번호 입력 요청 기능 구현

--- src/main/java/lotto/Printer/Printer.java
+++ src/main/java/lotto/Printer/Printer.java
@@ -3,6 +3,7 @@ package lotto.Printer;
 public class Printer {
     private static final String PURCHASE_AMOUNT_REQUEST_MESSAGE = "구매금액을 입력해 주세요.";
     private static final String PURCHASE_MESSAGE = "%개를 구매했습니다.";
+    private static final String LOTTO_NUMBER_REQUEST_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public void requestPurchaseAmount(){
         System.out.println(PURCHASE_AMOUNT_REQUEST_MESSAGE);
@@ -10,6 +11,10 @@ public class Printer {
 
     public void printPurchaseMessage(int numberOfLottos){
         System.out.printf(PURCHASE_MESSAGE, numberOfLottos);
+    }
+
+    public void requestLottoNumber(){
+        System.out.println(LOTTO_NUMBER_REQUEST_MESSAGE);
     }
 
 
