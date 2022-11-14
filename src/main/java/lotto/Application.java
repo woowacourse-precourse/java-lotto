354e25f51cc2088b884d45d792b7b3ce40f318ca
chaos0103 <lyt1228@naver.com>
2022-11-14 17:13:22 +0900

    refactor(Lotto): Application#main 메서드 추출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -21,40 +21,23 @@ public class Application {
     private static final LottoService lottoService = new LottoServiceImpl();
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         try {
-            System.out.println("구입금액을 입력해 주세요.");
             int amount = requestAmount();
-
             List<Lotto> userLottos = issue.createLotto(amount);
-            System.out.println(String.format("%d개를 구매했습니다.", userLottos.size()));
-            for (Lotto lotto : userLottos) {
-                System.out.println(lotto.getNumbers());
-            }
-
-            System.out.println("당첨 번호를 입력해 주세요.");
-            List<Integer> prizeNumbers = requestPrizeNumbers();
-            Lotto prizeLotto = new Lotto(prizeNumbers);
-
-            System.out.println("보너스 번호를 입력해 주세요.");
+            printLottos(userLottos);
+            Lotto prizeLotto = requestPrizeNumbers();
             int bonusNumber = requestBonusNumber();
-
             Map<Rank, Integer> results = lottoService.compare(userLottos, prizeLotto, bonusNumber);
-            System.out.println("당첨 통계");
-            System.out.println("---");
-            for (Rank rank : Arrays.asList(FIFTH, FOURTH, THIRD, SECOND, FIRST)) {
-                int count = results.getOrDefault(rank, 0);
-                System.out.println(rank.getMessage(count));
-            }
-
+            printResults(results);
             double rate = lottoService.calculateRate(results, amount);
-            System.out.println(String.format("총 수익률은 %.1f%%입니다.", rate));
+            printRate(rate);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
 
     private static int requestAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         validate(input);
         return Integer.parseInt(input);
@@ -68,20 +51,41 @@ public class Application {
         }
     }
 
-    private static List<Integer> requestPrizeNumbers() {
+    private static Lotto requestPrizeNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
 
         List<Integer> prizeNumbers = new ArrayList<>();
         for (String number : input.split(",")) {
             prizeNumbers.add(Integer.valueOf(number));
         }
-
-        return prizeNumbers;
+        return new Lotto(prizeNumbers);
     }
 
     private static int requestBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
         validate(input);
         return Integer.parseInt(input);
+    }
+
+    private static void printLottos(List<Lotto> lottos) {
+        System.out.println(String.format("%d개를 구매했습니다.", lottos.size()));
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    private static void printResults(Map<Rank, Integer> results) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (Rank rank : Arrays.asList(FIFTH, FOURTH, THIRD, SECOND, FIRST)) {
+            int count = results.getOrDefault(rank, 0);
+            System.out.println(rank.getMessage(count));
+        }
+    }
+
+    private static void printRate(double rate) {
+        System.out.println(String.format("총 수익률은 %.1f%%입니다.", rate));
     }
 }
