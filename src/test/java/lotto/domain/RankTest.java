b8f6fa4a51204cabcf93a260c3391d3c4602a498
junhei Lee <jun2ee22@naver.com>
2022-11-14 17:35:42 +0900

    test(domain): 각 등수가 잘 지정되는지 확인하는 테스트 추가

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -1,2 +1,35 @@
-package lotto.domain;public class RankTest {
+package lotto.domain;
+
+import lotto.standard.Place;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RankTest {
+
+    private Rank rank = new Rank(List.of(1, 2, 3, 4, 5, 6));
+    private int bonusNumber = 7;
+    private List<List<Integer>> userLottoTickets = List.of(
+            List.of(1, 2, 3, 4, 5, 6),  //1등
+            List.of(1, 2, 3, 4, 5, 7),  //2등
+            List.of(1, 2, 3, 4, 5, 8),  //3등
+            List.of(1, 2, 3, 4, 7, 8),  //4등
+            List.of(1, 2, 3, 7, 8, 9)); //5등
+
+
+    @Test
+    void countWinningRanksTest() {
+        Map<Place, Integer> result = new HashMap<>();
+        for (Place rank : Place.values()) {
+            result.put(rank, 1);
+        }
+
+        rank.countWinningRanks(userLottoTickets, bonusNumber);
+
+        Assertions.assertThat(Rank.winningRanks).isEqualTo(result);
+    }
+
 }
