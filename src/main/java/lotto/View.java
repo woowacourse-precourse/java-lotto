52c7e3425785e9b5eb169a56e8758018033ff1a3
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-14 20:52:00 +0900

    refactoring : View 결과 출력 parameter type 수정

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -32,7 +32,7 @@ public class View {
         }
     }
 
-    public void printResult(Rank[] values, int yield) {
+    public void printResult(Rank[] values, String yield) {
         try {
             printLine("당첨 통계\n---");
             for (Rank rank : values) {
