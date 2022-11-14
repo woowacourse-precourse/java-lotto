d9db67e466aca8928e2a58f31cd5a73b8615d022
Yejin Shin <shinyejin0212@gmail.com>
2022-11-14 18:16:11 +0900

    feat(WinningRank): 당첨 내역 getter 추가

--- src/main/java/lotto/domain/WinningRank.java
+++ src/main/java/lotto/domain/WinningRank.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+
 public enum WinningRank {
     SIX_MATCH(6, false, 2000000000),
     FIVE_MATCH_BONUS(5, false, 30000000),
@@ -15,6 +19,12 @@ public enum WinningRank {
         this.matchingCount = matchingCount;
         this.containsBonusNumber = containsBonusNumber;
         this.winningAmount = winningPrice;
+    }
+
+    public static Map<WinningRank, Integer> getWinningDetails() {
+        Map<WinningRank, Integer> winningDetails = new EnumMap<>(WinningRank.class);
+        Arrays.stream(values()).forEach(winningRank -> winningDetails.put(winningRank, 0));
+        return winningDetails;
     }
 
     public int getMatchingCount() {
