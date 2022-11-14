54481a4675898763769682df5f08223fb2438aa9
swjeong98 <tree5678@konkuk.ac.kr>
2022-11-14 17:41:48 +0900

    feat(Manager) : 주어진 리스트에 대해 중복되는 원소가 있는지 판단하는 기능 구현

--- src/main/java/lotto/Manager.java
+++ src/main/java/lotto/Manager.java
@@ -1,9 +1,21 @@
 package lotto;
 
-import java.util.List;
+import java.util.*;
 import lotto.Lotto;
 
 public class Manager {
+    public final int LOTTO_MIN = 1;
+    public final int LOTTO_MAX = 45;
+    public final int LOTTO_LENGTH = 6;
+
+    public final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    public final String PURCHASE_MENT = "개를 구매했습니다.";
+    public final String INPUT_WINNING = "당첨 번호를 입력해 주세요.";
+    public final String INPUT_BONUS = "보너스 번호를 입력해 주세요.";
+    public final String INIT_RESULT = "당첨 통계\n---\n";
+    public final String INIT_WINNING_RATE = "총 수익률은 ";
+    public final String CLOSE_WINNING_RATE = "%입니다.";
+
     public boolean isNumber(String str){
         for(int i=0; i<str.length(); i++){
             if(str.charAt(i) >= '0' && str.charAt(i) <= '9') continue;
@@ -19,11 +31,19 @@ public class Manager {
         }
         return true;
     }
-    
+
     public boolean ValidRangeOfLottoNumber(List<Integer> list){
         for(int i=0; i<list.size(); i++){
-            if(list.get(i) < 1 || list.get(i) > 45) return false;
+            if(list.get(i) < LOTTO_MIN || list.get(i) > LOTTO_MAX) return false;
         }
         return true;
+    }
+
+    public boolean OverlapOfLottoNumber(List<Integer> list){
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<list.size();i++){
+            set.add(list.get(i));
+        }
+        return set.size() != LOTTO_LENGTH;
     }
 }
