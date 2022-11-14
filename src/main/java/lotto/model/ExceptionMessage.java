9e964d39cd3deedee957a125a1c024110add6920
ChoiYeonho0903 <903yh@naver.com>
2022-11-15 03:18:14 +0900

    feat: 예외 처리 메시지 enum class 추가

---
+++ src/main/java/lotto/model/ExceptionMessage.java
@@ -0,0 +1,18 @@
+package lotto.model;
+
+public enum ExceptionMessage {
+    WRONG_INPUT("잘못된 입력입니다."),
+    OUT_OF_BOUNDARY("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATE_NUMBER("중복된 숫자가 입력되었습니다."),
+    ;
+
+    private final String exceptionMessage;
+
+    ExceptionMessage(String exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
+
+    public String getExceptionMessage() {
+        return exceptionMessage;
+    }
+}
