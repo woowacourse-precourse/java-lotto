3d218f54384e1fce1197c5956a30657e35383383
CAEL0 <kjongmin26@gmail.com>
2022-11-14 20:32:33 +0900

    refactor(Lotto): numbers -> lottoNumbers로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -8,28 +8,28 @@ import java.util.stream.Collectors;
 import static lotto.constant.GameConstants.*;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<Integer> lottoNumbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+    public Lotto(List<Integer> lottoNumbers) {
+        validate(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        Validator.validateLength(numbers, LOTTO_NUMBERS_LENGTH);
-        Validator.validateRange(numbers, LOTTO_NUMBERS_LENGTH);
-        Validator.validateDuplication(numbers, LOTTO_NUMBERS_LENGTH);
+    private void validate(List<Integer> lottoNumbers) {
+        Validator.validateLength(lottoNumbers, LOTTO_NUMBERS_LENGTH);
+        Validator.validateRange(lottoNumbers, LOTTO_NUMBERS_LENGTH);
+        Validator.validateDuplication(lottoNumbers, LOTTO_NUMBERS_LENGTH);
     }
 
     @Override
     public String toString() {
-        return numbers.stream()
+        return lottoNumbers.stream()
                 .sorted()
                 .collect(Collectors.toList())
                 .toString();
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
+    public List<Integer> getLottoNumbers() {
+        return lottoNumbers;
     }
 }
