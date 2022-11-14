4686d9616b3a84134b1c9da63cb7be0d0641bec8
yubin <dbqls2821@gmail.com>
2022-11-15 02:29:09 +0900

    test(RangeValidatorTest): 로또 당첨 번호 입력값의 범위가 1부터 45사이가 아닐 때 예외를 출력하는 테스트 코드 추가

--- src/test/java/lotto/domain/io/RangeValidatorTest.java
+++ src/test/java/lotto/domain/io/RangeValidatorTest.java
@@ -22,4 +22,21 @@ class RangeValidatorTest {
       .hasMessageContaining(ExceptionMessage.INPUT_NUMBER_RANGE_IS_NOT_CORRECT.getMessage());
   }
 
+  @ParameterizedTest
+  @MethodSource("provideInput")
+  void 로또_당첨_번호_입력값의_범위가_1부터_45사이가_아니라면_예외를_출력(List<Integer> input) {
+    RangeValidator rangeValidator = new RangeValidator();
+    assertThatThrownBy(() -> rangeValidator.validate(input))
+      .isInstanceOf(IllegalArgumentException.class)
+      .hasMessageContaining(ExceptionMessage.INPUT_NUMBER_RANGE_IS_NOT_CORRECT.getMessage());
+  }
+
+  static Stream<Arguments> provideInput() {
+    return Stream.of(
+      Arguments.of(List.of(0,1,2,3,4)),
+      Arguments.of(List.of(-1,0,2,3,4,5,6,7)),
+      Arguments.of(List.of(46,1,2,3,4,5,6))
+    );
+  }
+
 }
