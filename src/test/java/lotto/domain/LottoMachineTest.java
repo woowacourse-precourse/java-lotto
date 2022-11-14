6af8f30fba79b22a5b2c76fb94765a5cb6a8b872
limzzum <chfhddl1234@naver.com>
2022-11-14 22:49:05 +0900

    test : 랜덤 로또 생성 테스트 추가

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,37 +1,48 @@
 package lotto.domain;
 
+import lotto.Lotto;
+import lotto.utils.Validate;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
 
 import java.io.ByteArrayInputStream;
 import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoMachineTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"1100", "100", "3500", "12001", "50"})
+    @ValueSource(strings = {"1100", "100", "3500", "12001", "50", "abcd", "12cd", " ", "9900", "0"})
+    @DisplayName("구입 금액을 잘못 입력하면 오류 발생")
     void validateInputMoney(String input) throws Exception {
-        String data = input;
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
         LottoMachine lottoMachine = new LottoMachine();
-        Method validateMoney = lottoMachine.getClass().getDeclaredMethod("validateMoney", String.class);
-        validateMoney.setAccessible(true);
 
-        Assertions.assertThatThrownBy(()->
-                validateMoney.invoke(lottoMachine,data)).getRootCause()
+        Assertions.assertThatThrownBy(() ->
+                lottoMachine.getInputMoney())
                         .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void createLottoNumber() {
-    }
 
-    @Test
-    void printLottos() {
+    @ParameterizedTest
+    @CsvSource({"1000,1", "2000,2", "3000,3", "5000,5"})
+    void createLottoNumber(String input, String size) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        LottoMachine lottoMachine = new LottoMachine();
+
+        lottoMachine.getInputMoney();
+        Assertions.assertThat(lottoMachine.createLottoNumber().size()).isEqualTo(Integer.valueOf(size));
+
+
     }
 
     @Test
@@ -42,7 +53,4 @@ class LottoMachineTest {
     void calculateRevenue() {
     }
 
-    @Test
-    void printResult() {
-    }
 }
