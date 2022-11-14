dff8a6137fa7882b58b18ead75235b1faf23e4b4
soyunim <togksk123@naver.com>
2022-11-15 00:57:08 +0900

    feat: 로또 구입 금액 입력 및 예외 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static lotto.message.gameMessages.*;
+import static lotto.message.errorMessages.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        buyLotto();
     }
+
+    public static int buyLotto(){
+        System.out.println(AMOUNT_MONEY_INPUT);
+        String moneyInput = Console.readLine();
+        int buyAmount = 0;
+        checkPurchaseException(moneyInput);
+        buyAmount = Integer.parseInt(moneyInput)/1000;
+        return buyAmount;
+    }
+    private static void checkPurchaseException(String moneyInput){
+        int money = 0;
+        try{
+            money = Integer.parseInt(moneyInput);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException(USER_INPUT_ERROR);
+        }
+        if(money%1000!=0){
+            throw new IllegalArgumentException(USER_INPUT_ERROR);
+        }
+    }
+
 }
