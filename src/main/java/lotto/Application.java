d694cd853e808b5be18894ea9926f3baaf797b99
wonwoo6y <wonwoo6y@gmail.com>
2022-11-14 23:42:37 +0900

    fix: 테스트 코드 runException시 NoSuchElementException 에러 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,9 +6,13 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        int purchaseAmount = Ui.inputPurchaseAmount();
-        int lottoTickets = LottoVendingMachine.getLottoTickets(purchaseAmount);
-        List<Lotto> lottos = LottoVendingMachine.getLottos(lottoTickets);
-        LottoDrawMachine lottoDrawMachine = new LottoDrawMachine();
+        try {
+            int purchaseAmount = Ui.inputPurchaseAmount();
+            int lottoTickets = LottoVendingMachine.getLottoTickets(purchaseAmount);
+            List<Lotto> lottos = LottoVendingMachine.getLottos(lottoTickets);
+            LottoDrawMachine lottoDrawMachine = new LottoDrawMachine();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
     }
 }
