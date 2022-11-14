1dd1273bf7ce8f64c65b70d6c033bafb37fce789
Gibeom Nam <rlqja0523@naver.com>
2022-11-14 23:22:03 +0900

    test: 로또 번호가 정렬되어 있는지 테스트

--- src/test/java/lotto/service/LottoGeneratorTest.java
+++ src/test/java/lotto/service/LottoGeneratorTest.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LottoGeneratorTest {
+
+    private List<Lotto> lotto = LottoGenerator.generateMyLotto(1);
+    private List<Integer> numbers = lotto.get(0).getNumbers();
 
     @Test
     @DisplayName("구매 개수에 맞게 로또가 생성된다.")
@@ -21,5 +25,16 @@ public class LottoGeneratorTest {
 
         // then
         assertEquals(purchaseCount, lotto.size());
+    }
+
+    @Test
+    @DisplayName("랜덤으로 생성된 로또 번호는 정렬되어 있다.")
+    void sortTest() {
+        // then
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            int curNumber = numbers.get(i);
+            int nextNumber = numbers.get(i + 1);
+            assertTrue(curNumber < nextNumber);
+        }
     }
 }
