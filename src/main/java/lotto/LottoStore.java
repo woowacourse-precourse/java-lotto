42464f990e24098979359bc5c04a7df681a63fa4
inhwanK <inhan1009@naver.com>
2022-11-14 23:29:56 +0900

    feat(LottoStore): 로또 구입 및 발행을 책임지는 클래스 추가

---
+++ src/main/java/lotto/LottoStore.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class LottoStore {
+
+
+}
