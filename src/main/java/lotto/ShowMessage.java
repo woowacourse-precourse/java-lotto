4777c0386933b04dc6eee1a94380c8196dc6ccb8
abbd2 <ajkety00@gmail.com>
2022-11-14 20:02:03 +0900

    Rename(ShowMessage): ShowMessage 클래스를 enum으로 수정

--- src/main/java/lotto/ShowMessage.java
+++ src/main/java/lotto/ShowMessage.java
@@ -2,21 +2,27 @@ package lotto;
 
 import java.util.List;
 
-public class ShowMessage {
+public enum ShowMessage {
 
-    private static final String INPUT_AMOUNT_BUY_MESSAGE = "구입 금액을 입력해 주세요.";
-    private static final String HOW_MANY_BUY_MESSAGE = "개를 구매했습니다.";
-    private static final String INPUT_LOTTO_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-    private static final String LOTTO_STATISTICS_MESSAGE = "당첨 통계\n---";
+    INPUT_AMOUNT_BUY_MESSAGE("구입 금액을 입력해 주세요."),
+    HOW_MANY_BUY_MESSAGE("개를 구매했습니다."),
+    INPUT_LOTTO_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요."),
+    LOTTO_STATISTICS_MESSAGE("당첨 통계\n---");
+
+    private final String script;
+
+    ShowMessage(String script) {
+        this.script = script;
+    }
 
     public static void showInputAmountBuy() {
-        System.out.println(INPUT_AMOUNT_BUY_MESSAGE);
+        System.out.println(INPUT_AMOUNT_BUY_MESSAGE.script);
     }
 
     public static void showHowManyBuy(long number) {
         System.out.println();
-        System.out.println(number + HOW_MANY_BUY_MESSAGE);
+        System.out.println(number + HOW_MANY_BUY_MESSAGE.script);
     }
 
     public static void showLottoNumber(List<Lotto> lottoNumbers) {
@@ -27,15 +33,14 @@ public class ShowMessage {
 
     public static void showInputLottoNumber() {
         System.out.println();
-        System.out.println(INPUT_LOTTO_NUMBER_MESSAGE);
+        System.out.println(INPUT_LOTTO_NUMBER_MESSAGE.script);
     }
 
     public static void showInputBonusNumber() {
-        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE.script);
     }
 
     public static void showLottoStatistics() {
-        System.out.println(LOTTO_STATISTICS_MESSAGE);
+        System.out.println(LOTTO_STATISTICS_MESSAGE.script);
     }
-
 }
