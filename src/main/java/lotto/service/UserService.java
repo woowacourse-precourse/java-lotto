bfa7b296b17b0a9e1d244ddd9116a8a5c8624cc8
skydreamer21 <skydreamer210@gmail.com>
2022-11-14 13:46:55 +0900

    fix(UserService): createUser을 non-static 메서드로 변경

--- src/main/java/lotto/service/UserService.java
+++ src/main/java/lotto/service/UserService.java
@@ -3,7 +3,7 @@ package lotto.service;
 import lotto.domain.User;
 
 public class UserService {
-    public static User createUser(int money) {
+    public User createUser(int money) {
         return new User(money);
     }
 }
