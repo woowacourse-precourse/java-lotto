83f78d372c89d98fe0a52faacd50220b4892159d
kdkdhoho <hkim4410@naver.com>
2022-11-14 13:53:41 +0900

    test(ManagerTest): Manager 클래스 변경에 따른 일부 테스트 코드 수정 및 삭제

--- src/test/java/lotto/domain/ManagerTest.java
+++ src/test/java/lotto/domain/ManagerTest.java
@@ -1,16 +1,13 @@
 package lotto.domain;
 
 import lotto.domain.enums.Number;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertSame;
 
 class ManagerTest {
 
@@ -22,57 +19,32 @@ class ManagerTest {
     }
 
     @Test
-    void 구입금액에_따른_로또_장수_반환_테스트() {
-        int money = 8000;
+    void 구입금액에_따른_로또_반환_테스트() {
+        double money = 8000;
         int expect = 8;
-        int result = manager.changeLottoCount(money);
+        List<Lotto> lottos = manager.changeLottos(money);
 
-        assertThat(result).isEqualTo(expect);
+        assertThat(lottos.size()).isEqualTo(expect);
     }
 
     @Test
-    void 구입금액에_따른_로또_장수_반환_테스트_0() {
-        int money = 0;
+    void 구입금액에_따른_로또_반환_테스트_0() {
+        double money = 0;
         int expect = 0;
-        int result = manager.changeLottoCount(money);
+        List<Lotto> lottos = manager.changeLottos(money);
 
-        assertThat(result).isEqualTo(expect);
-    }
-
-    @Test
-    void 잘못된_구입금액_입력_100단위() {
-        int money = 8500;
-        Assertions.assertThatIllegalArgumentException()
-                .isThrownBy(() -> manager.changeLottoCount(money));
-    }
-
-    @Test
-    void 잘못된_구입금액_입력_음수() {
-        int money = -10000;
-        Assertions.assertThatIllegalArgumentException()
-                .isThrownBy(() -> manager.changeLottoCount(money));
+        assertThat(lottos.size()).isEqualTo(expect);
     }
 
     @Test
     void getCorrectCountTest() {
-        List<Integer> userLotto = new ArrayList<>(List.of(6,5,4,3,2,1));
-        String winningNumbers = "1,2,3,4,5,7";
-        manager.setWinningNumbers(winningNumbers);
+        Lotto userLotto = new Lotto(new ArrayList<>(List.of(6,5,4,3,2,1)));
+        manager.setWinningNumbers("1,2,3,4,5,7");
 
         int expect = 5;
         int result = manager.getCorrectCount(userLotto);
 
         assertThat(result).isEqualTo(expect);
-    }
-
-    @Test
-    void isCorrectBonusTest() {
-        List<Integer> userLotto = new ArrayList<>(List.of(6,5,4,3,2,1));
-        manager.setBonusNumber("1");
-
-        boolean result = manager.isCorrectBonus(userLotto);
-
-        assertThat(result).isEqualTo(true);
     }
 
     @Test
