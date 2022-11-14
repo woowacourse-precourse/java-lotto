e3af100397d79d0ed374d293934cacb5510e31b5
aossuper7 <aossuper7@naver.com>
2022-11-15 01:32:26 +0900

    feat: 5개 맞았다면 보너스 번호 비교하는 기능

--- src/main/java/lotto/Model/Comparison.java
+++ src/main/java/lotto/Model/Comparison.java
@@ -1,13 +1,28 @@
 package lotto.Model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Comparison {
 
+    private Map<String, Integer> savedResult = new HashMap<>();
+
     public void compareLotto(List<Lotto> lottoNumber, WinningTicket ticket) {
         for (Lotto lotto : lottoNumber) {
+            boolean bonusCheck = false;
             int sum = compareLottoNum(lotto.getNumbers(), ticket.getWinningTicket());
+            if (sum == 5) {
+                bonusCheck = compareLottoNum(lotto.getNumbers(), ticket.getBonusNum());
+            }
         }
+    }
+
+    private boolean compareLottoNum(List<Integer> numbers, int bonusNum) {
+        if (numbers.contains(bonusNum)) {
+            return true;
+        }
+        return false;
     }
 
     private int compareLottoNum(List<Integer> numbers, Lotto ticket) {
