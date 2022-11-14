8f6f18b9d98c2154da9704d603ccbe3f08f5da77
baksohyeon <bori12370@gmail.com>
2022-11-14 21:32:33 +0900

    refactor: add error handling and separator variable

--- src/main/java/lotto/service/executeLotto/LotteryNumbersDraw.java
+++ src/main/java/lotto/service/executeLotto/LotteryNumbersDraw.java
@@ -8,11 +8,15 @@ public class LotteryNumbersDraw {
     // userInput 3, 5, 7, 8, 19, 36
     // string 에서 불필요한 띄어쓰기는 제거하고 값을 받기
 
-
+    private static final String SEPARATOR = ",";
     public List<Integer> drawNumbers(String userInput) {
-        String deletedBlankSpaceString = userInput.replaceAll("\\s", "");
-        return Arrays.stream(deletedBlankSpaceString.split(","))
-                .map(Integer::parseInt).sorted().collect(Collectors.toList());
+        try {
+            String deletedBlankSpaceString = userInput.replaceAll("\\s", "");
+            return Arrays.stream(deletedBlankSpaceString.split(SEPARATOR))
+                    .map(Integer::parseInt).sorted().collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자와 공백 그리고'" + SEPARATOR + "' 만을 입력해주세요.");
+        }
     }
 
     public int getBonusNumber(int bonus) {
