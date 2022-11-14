3e7dbb080b437492c65a549c69a7606535fd4be5
abbd2 <ajkety00@gmail.com>
2022-11-15 03:58:52 +0900

    Feat(Application): Lotto의 초기 값 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Lotto lotto = new Lotto(List.of(0, 1, 2, 3, 4, 5));
+        lotto.startLotto();
     }
 }
