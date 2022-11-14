f153b0a3d7b16ef5cf37428dd2c91fa4a7b7240d
JuSeong1130 <jsmoon1130@naver.com>
2022-11-15 05:59:56 +0900

    feat(Money): money 유효성 검증 및 로또 구매 개수 기능 구현
    돈으로 살수 있는 로또 구매 개수 리턴 기능, 로또 게임에 맞는 유효성검증(1000원단위,숫자,0원이상)

---
+++ src/main/java/lotto/domain/money/Money.java
@@ -0,0 +1,55 @@
+package lotto.domain.money;
+
+import static lotto.util.constant.Constant.LOTTO_PURCHASE_AMOUNT;
+
+public class Money {
+
+
+	private static final String LOTTO_PURCHASE_AMOUNT_NOT_ZERO = "[ERROR] 금액은 0원이 되면 안됩니다.";
+	private static final String LOTTO_PURCHASE_AMOUNT_NOT_1000_UNIT =
+			"[ERROR] 구입 금액은 " + LOTTO_PURCHASE_AMOUNT + "원단위 여야 합니다.";
+	private static final String LOTTO_PURCHASE_AMOUNT_IS_NUMBER = "[ERROR] 구매 금액은 숫자 여야 합니다.";
+	int money;
+
+	public Money(String money) {
+		validate(money);
+	}
+
+	public int getLottoPurchaseCount() {
+		return money / LOTTO_PURCHASE_AMOUNT;
+	}
+
+	public void validate(String money) {
+		isNumber(money);
+		this.money = Integer.parseInt(money);
+		isGreaterThanZero(this.money);
+		isRemainderZero(this.money);
+
+	}
+
+	public void isGreaterThanZero(int money) {
+		int quotientNumber = money / LOTTO_PURCHASE_AMOUNT;
+		if (quotientNumber == 0) {
+			throw new IllegalArgumentException(LOTTO_PURCHASE_AMOUNT_NOT_ZERO);
+		}
+	}
+
+	public void isRemainderZero(int money) {
+		int remainderNumber = money % LOTTO_PURCHASE_AMOUNT;
+		if (remainderNumber != 0) {
+			throw new IllegalArgumentException(LOTTO_PURCHASE_AMOUNT_NOT_1000_UNIT);
+		}
+	}
+
+	public void isNumber(String money) {
+		try {
+			Integer.parseInt(money);
+		} catch (Exception e) {
+			throw new IllegalArgumentException(LOTTO_PURCHASE_AMOUNT_IS_NUMBER);
+		}
+	}
+
+	public int getMoney() {
+		return money;
+	}
+}
