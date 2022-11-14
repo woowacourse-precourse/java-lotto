dbd87b7da17b975998cc1edbfc9c4ed2c23c2d5d
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-14 13:39:28 +0900

    feat(OutputView): 로또 당첨 결과 출력 기능 구현

--- src/main/java/lotto/MatchingType.java
+++ src/main/java/lotto/MatchingType.java
@@ -1,23 +1,28 @@
 package lotto;
 
+import static lotto.PrizeType.*;
+
 import java.util.Arrays;
 import java.util.function.Function;
 
 public enum MatchingType {
-    THREE_MATCH(3, correctCount -> correctCount * 5_000L),
-    FOUR_MATCH(4, correctCount -> correctCount * 50_000L),
-    FIVE_MATCH(5, correctCount -> correctCount * 1_500_000L),
-    FIVE_WITH_BONUS_MATCH(5, correctCount -> correctCount * 30_000_000L),
-    SIX_MATCH(6, correctCount -> correctCount * 2_000_000_000L),
-    NOT_MATCH(0, correctCount -> correctCount);
+    THREE_MATCH(3, correctCount -> correctCount * 5_000L, FIVE_THOUSAND),
+    FOUR_MATCH(4, correctCount -> correctCount * 50_000L, FIFTY_THOUSAND),
+    FIVE_MATCH(5, correctCount -> correctCount * 1_500_000L, ONE_AND_A_HALF_MILLION),
+    FIVE_WITH_BONUS_MATCH(5, correctCount -> correctCount * 30_000_000L, THIRTY_MILLION),
+    SIX_MATCH(6, correctCount -> correctCount * 2_000_000_000L, TWO_BILLION),
+    NOT_MATCH(0, correctCount -> correctCount, NONE);
 
     private final int matchCount;
 
     private final Function<Long, Long> expression;
 
-    MatchingType(int matchCount, Function<Long, Long> expression) {
+    private final PrizeType prizeType;
+
+    MatchingType(int matchCount, Function<Long, Long> expression, PrizeType prizeType) {
         this.matchCount = matchCount;
         this.expression = expression;
+        this.prizeType = prizeType;
     }
 
     public static MatchingType findByCorrectCount(int correctCount) {
@@ -34,6 +39,10 @@ public enum MatchingType {
 
     public int getMatchCount() {
         return matchCount;
+    }
+
+    public PrizeType getPrizeType() {
+        return prizeType;
     }
 
     public boolean isFiveMatch() {
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,11 +2,16 @@ package lotto.view;
 
 import java.util.List;
 import lotto.Lotto;
+import lotto.MatchingType;
+import lotto.PrizeType;
 import lotto.Quantity;
+import lotto.WinningStat;
 
 public class OutputView {
 
     private static final String LOTTO_QUANTITY_MESSAGE = "개를 구매했습니다.";
+    private static final String LOTTO_STAT_MESSAGE = "당첨 통계";
+    private static final String DIVIDING_LINE_MESSAGE = "---";
 
     public static void printLottoQuantity(Quantity quantity) {
         String result = quantity.getNumber()
@@ -18,5 +23,19 @@ public class OutputView {
 
     public static void printLottos(List<Lotto> lottos) {
         lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
+    }
+
+    public static void printStatistics(List<WinningStat> winningStats) {
+        System.out.println(LOTTO_STAT_MESSAGE);
+        System.out.println(DIVIDING_LINE_MESSAGE);
+
+        for (WinningStat winningStat : winningStats) {
+            MatchingType matchingType = winningStat.getMatchingType();
+            int count = winningStat.getCount();
+            PrizeType prizeType = matchingType.getPrizeType();
+
+            String result = matchingType.getMatchCount() + "개 일치 (" + prizeType.getMoney() + ")원 - " + count + "개";
+            System.out.println(result);
+        }
     }
 }
