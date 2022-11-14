933af2b1f3d52aa57789c5641c672fabab0ef0cc
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-15 03:50:30 +0900

    feat: 로또 구입 기능 구현 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.view.LottoServiceView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoServiceView lottoServiceView = new LottoServiceView();
+        int numberOfLotto = lottoServiceView.buyLotto();
     }
 }
