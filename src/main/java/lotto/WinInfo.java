3e2c73c5053d164a8511c58f45bf2c9128a048af
osm <osm0822@naver.com>
2022-11-14 15:57:00 +0900

    feat(WinInfo) : 당첨조건 검사 구현

--- src/main/java/lotto/WinInfo.java
+++ src/main/java/lotto/WinInfo.java
@@ -1,12 +1,20 @@
 package lotto;
 
-public class WinInfo{
+public class WinInfo {
     private int matchCnt;
     private int bonusMatchCnt;
     private long winMoney;
-    public WinInfo(int matchCnt, int bonusMatchCnt, long winMoney){
+
+    public WinInfo(int matchCnt, int bonusMatchCnt, long winMoney) {
         this.matchCnt = matchCnt;
         this.bonusMatchCnt = bonusMatchCnt;
         this.winMoney = winMoney;
+    }
+
+    public boolean match(int matchCnt, int bonusMatchCnt) {
+        if (this.matchCnt == matchCnt && this.bonusMatchCnt == bonusMatchCnt) {
+            return true;
+        }
+        return false;
     }
 }
