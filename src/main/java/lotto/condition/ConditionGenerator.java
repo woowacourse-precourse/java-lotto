b0df5144931fde13098dcbfd9b7e8e20aafcf25e
wonju-dev <wonjulee.dev@gmail.com>
2022-11-15 02:40:29 +0900

    feat(ConditionGenerator): 사용자 입력값 검증을 위한 조건 목록을 관리하는 클래스 생성
    ConditionGenerator는 사용자 입력값을 검증하기 위한 조건들을 관리하는 클래스 입니다.
    각 메소드를 통해 조건 목록을 사용할 수 있습니다.

---
+++ src/main/java/lotto/condition/ConditionGenerator.java
@@ -0,0 +1,36 @@
+package lotto.condition;
+
+import lotto.condition.lottonumber.IsAllNumberInRangeCond;
+import lotto.condition.lottonumber.NoDuplicateNumberCond;
+import lotto.condition.lottonumber.RightFormatCond;
+import lotto.condition.money.IsAllNumberCond;
+import lotto.condition.money.MinimumMoneyCond;
+import lotto.condition.money.MoneyDivisionCond;
+
+import java.util.List;
+
+public class ConditionGenerator {
+
+    public static List<Condition> getMoneyCondition() {
+        return List.of(
+                IsAllNumberCond.getInstance(),
+                MinimumMoneyCond.getInstance(),
+                MoneyDivisionCond.getInstance()
+        );
+    }
+
+    public static List<Condition> getLottoNumberCondition() {
+        return List.of(
+                RightFormatCond.getInstance(),
+                IsAllNumberInRangeCond.getInstance(),
+                NoDuplicateNumberCond.getInstance()
+        );
+    }
+
+    public static List<Condition> getBonusNumberCondition() {
+        return List.of(
+                IsAllNumberInRangeCond.getInstance(),
+                NoDuplicateNumberCond.getInstance()
+        );
+    }
+}
