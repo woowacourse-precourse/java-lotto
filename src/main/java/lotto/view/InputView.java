0272e46279b33167a0935590684a19336ddf3252
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-15 02:22:12 +0900

    feat : 입력값의 타입을 확인하고 적절하지 않으면 RuntimeException을 던진다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,24 +3,18 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final int MINIMUM_MONEY_VALUE = 1000;
-    private static final int MONEY_UNIT = 1000;
-    private static final String LOTTO_MONEY_IS_UNDER_MINIMUM_MONEY_VALUE = "[ERROR] 로또 구입 금액이 1000원 이하입니다.";
-    private static final String LOTTO_MONEY_IS_NOT_DIVIDED_BY_MONEY_VALUE = "[ERROR] 로또 구입 금액이 1000원으로 나누어 떨어지지 않습니다.";
+    private static final String INTEGER_REGEX = "-?\\\\d+";
+    private static final String INPUT_TYPE_IS_NOT_PROPER = "[ERROR] 잘못된 입력 형식입니다.";
 
     public static Integer inputMoney() {
-        int money = Integer.parseInt(Console.readLine());
-        validateMoneyValue(money);
-        return money;
+        String money = Console.readLine();
+        validateMoneyType(money);
+        return Integer.parseInt(money);
     }
 
-    private static int validateMoneyValue(int money) {
-        if (money < MINIMUM_MONEY_VALUE) {
-            throw new IllegalArgumentException(LOTTO_MONEY_IS_UNDER_MINIMUM_MONEY_VALUE);
+    private static void validateMoneyType(String money) {
+        if (!money.matches(INTEGER_REGEX)) {
+            throw new RuntimeException(INPUT_TYPE_IS_NOT_PROPER);
         }
-        if (money % MONEY_UNIT != 0) {
-            throw new IllegalArgumentException(LOTTO_MONEY_IS_NOT_DIVIDED_BY_MONEY_VALUE);
-        }
-        return money;
     }
 }
