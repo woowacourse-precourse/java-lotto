60a5b83923d251cdf32aae557da8c64747164575
hyungkya <hyungkya@uci.edu>
2022-11-14 16:51:07 +0900

    feat: 입력 가격을 수량으로 변환하기

--- src/main/java/lotto/Simulator.java
+++ src/main/java/lotto/Simulator.java
@@ -1,8 +1,15 @@
 package lotto;
 
 public class Simulator {
+    private int quantity;
+
     public Simulator(int price) {
         validatePrice(price);
+        this.quantity = toQuantity(price);
+    }
+
+    public int getQuantity() {
+        return this.quantity;
     }
 
     private void validatePrice(int price) {
@@ -12,5 +19,9 @@ public class Simulator {
         if (price <= 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 0보다 커야합니다.");
         }
+    }
+
+    private int toQuantity(int price) {
+        return price / 1000;
     }
 }
