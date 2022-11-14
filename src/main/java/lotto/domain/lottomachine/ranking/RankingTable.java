056c054f6aa7787e1edb70bd18a993981e971138
chanminmaxscaler <jcm04@naver.com>
2022-11-14 18:03:53 +0900

    feat: RankingTable 클래스에 로또의 수익률을 계산하는 기능을 구현한다
    RankingTable 클래스는 Map<Ranking, Integer> frequenciesByRank를 유일한 멤버변수로 갖는 일급 컬렉션이다.
    로또의 수익률을 계산하는 기능을 calculateRateOfReturn, calculateCurrentValue, changeToRateOfReturnFormat 메소드로 구현한다.
    
    calculateRateOfReturn 메소드는 로또의 구입 금액을 입력받는다. 로또의 구입 금액과 현재 당첨 로또의 총 수익을 BigDecimal 객체로 생성한다.
    BigDecimal 객체의 divide 연산을 한 후, 해당 수를 changeToRateOfReturnFormat에 전달한 값을 반환한다.
    
    calculateCurrentValue 메소드는 현재 당첨 로또의 총 수익을 스트림 연산을 활용해 BigDecimal 객체로 반환한다.
    
    changeToRateOfReturnFormat 메소드는 BigDecimal 객체를 입력받아, DecimalFormat 객체를 활용해 "###,###.0%"의 형식을 적용한 문자열로 전환한다.
    이후 문자열을 반환한다.

---
+++ src/main/java/lotto/domain/lottomachine/ranking/RankingTable.java
@@ -0,0 +1,35 @@
+package lotto.domain.lottomachine.ranking;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class RankingTable {
+    private final Map<Ranking, Integer> frequenciesByRank;
+
+    public RankingTable(Map<Ranking, Integer> frequenciesByRank) {
+        this.frequenciesByRank = frequenciesByRank;
+    }
+
+    public String calculateRateOfReturn(int money) {
+        BigDecimal currentValue = calculateCurrentValue();
+        BigDecimal originalValue = BigDecimal.valueOf(money);
+        return changeToRateOfReturnFormat(currentValue.divide(originalValue, 3, RoundingMode.HALF_UP));
+    }
+
+    private BigDecimal calculateCurrentValue() {
+        return frequenciesByRank.entrySet()
+                .stream()
+                .map(entry -> BigDecimal.valueOf(entry.getKey().getCash()).multiply(BigDecimal.valueOf(entry.getValue())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private String changeToRateOfReturnFormat(BigDecimal rateOfReturn) {
+        DecimalFormat formatter = new DecimalFormat("###,###.0%");
+        return formatter.format(rateOfReturn);
+    }
+}
