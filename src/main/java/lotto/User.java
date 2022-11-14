9cc6ef22296d9fc376263ad7e93298e0f55178d3
kmw2378 <rlarla677@gmail.com>
2022-11-14 23:57:19 +0900

    [feat] User - 사용자가 입력할 모든 값들을 필드로 구현 - 임의로 생성되는 로또 번호또한 필드로 구현

---
+++ src/main/java/lotto/User.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private int amount;
+    private int count;
+    private Number number;
+    private int yield;
+    private List<Lotto> lottoList = new ArrayList<>();
+}
