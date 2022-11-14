c2af3f8ca12cc688935748a58be9d585df5746b4
youngkyunIm <yee0230@naver.com>
2022-11-14 19:22:51 +0900

    test(LottoReference) : get yield

--- src/test/java/lotto/domain/LottoReferenceTest.java
+++ src/test/java/lotto/domain/LottoReferenceTest.java
@@ -1,13 +1,28 @@
 package lotto.domain;
 
+import static lotto.domain.LottoReference.NOPE;
+import static lotto.domain.LottoReference.THREE;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoReferenceTest {
 
-    @Test
-    void getPrizeByCorrectCount_3_5000() {
-        assertThat((LottoReference.getPrizeByCorrectCount(3))).isEqualTo(5_000);
+    @DisplayName("Get Yield")
+    @ParameterizedTest(name = "[{index}] input {0} ")
+    @CsvSource(value = {"7,1", "6,2", "5,3"})
+    void getYield(int nope, int three) {
+        Map<LottoReference, Integer> result = new HashMap<>();
+
+        result.put(NOPE, nope);
+        result.put(THREE, three);
+
+        float yield = (float) THREE.getPrize() * three / 8000 * 100;
+
+        assertThat(LottoReference.getYield(result)).isEqualTo(yield);
     }
 }
