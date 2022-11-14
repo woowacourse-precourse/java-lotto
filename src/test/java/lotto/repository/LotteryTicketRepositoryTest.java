8d5f767d815d0f2edd49456d5cb8f9b8e7986032
skydreamer21 <skydreamer210@gmail.com>
2022-11-15 01:25:52 +0900

    test(LotteryTicketRepositoryTest): 추첨 전 당첨 결과 테스트 추가
    [세부사항]
    - 테스트 목록
    1. 로또 추첨 전에 당첨 결과 조회시 LotteryRank.BEFORE_DRAW을 반환

--- src/test/java/lotto/repository/LotteryTicketRepositoryTest.java
+++ src/test/java/lotto/repository/LotteryTicketRepositoryTest.java
@@ -1,10 +1,10 @@
 package lotto.repository;
 
+import lotto.constant.LotteryRank;
 import lotto.domain.LotteryTicket;
 import org.junit.jupiter.api.*;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -13,6 +13,7 @@ public class LotteryTicketRepositoryTest {
     void afterEach() {
         LotteryTicketRepository repository = LotteryTicketRepository.getInstance();
         repository.clearStore();
+        repository.clearDrawResult();
     }
 
     @DisplayName("저장된 로또 티켓을 lotteryId로 조회한다.")
@@ -36,5 +37,18 @@ public class LotteryTicketRepositoryTest {
 
         assertThat(repository.findByLotteryId(lotteryId))
                 .isNotPresent();
+    }
+
+    @DisplayName("로또 추첨 전에 당첨 결과 조회시 LotteryRank.BEFORE_DRAW을 반환한다.")
+    @Test
+    void findRankOfLotteryTicketBeforeDraw() {
+        LotteryTicketRepository repository = LotteryTicketRepository.getInstance();
+        LotteryTicket lotteryTicket = new LotteryTicket(List.of(1, 2, 3, 4, 5, 6));
+        String lotteryId = lotteryTicket.getLotteryId();
+
+        repository.addLotteryTicket(lotteryTicket);
+        assertThat(repository.findRankById(lotteryId))
+                .isPresent()
+                .hasValue(LotteryRank.BEFORE_DRAW);
     }
 }
