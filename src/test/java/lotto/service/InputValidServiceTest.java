e09ef765285a810b95d18dd6525589a37e6c2dcc
suheonjoo <suheonj95@naver.com>
2022-11-14 14:36:36 +0900

    test(InputValidServiceTest): 체크예외, 언체크 예외 구분을 두어 리팩토링을 하였기에 InputValidService는 언체크 예외로 처리했으니 관련 테스트는 언체크 인스턴스로 구분하여 테스트 수정

--- src/test/java/lotto/service/InputValidServiceTest.java
+++ src/test/java/lotto/service/InputValidServiceTest.java
@@ -21,11 +21,9 @@ class InputValidServiceTest {
 	@DisplayName("보너스 번호 valid 오류 테스트 - 숫자가 아닌경우, 1~45 범위가 아닌 경우")
 	@Test
 	void validateBonusErrorTest() {
-		Assertions.assertAll(
-			() -> assertThatThrownBy(() -> inputValidService.validateBonus("!@ ")),
-			() -> assertThatThrownBy(() -> inputValidService.validateBonus("46")),
-			() -> assertThatThrownBy(() -> inputValidService.validateBonus("-1"))
-		);
+		assertThatThrownBy(() -> inputValidService.validateBonus("!@ ")).isInstanceOf(RuntimeException.class);
+		assertThatThrownBy(() -> inputValidService.validateBonus("46")).isInstanceOf(RuntimeException.class);
+		assertThatThrownBy(() -> inputValidService.validateBonus("-1")).isInstanceOf(RuntimeException.class);
 	}
 
 	@ParameterizedTest(name = "올바른 보너스 번호 테스트")
@@ -37,11 +35,9 @@ class InputValidServiceTest {
 	@DisplayName("당첨 번호 valid 오류 테스트 - 숫자가 아닌 경우, 1~45 범위가 아닌 경우, 6개의 숫자를 입력하지 않은경우")
 	@Test
 	void validAnswerErrorTest() {
-		Assertions.assertAll(
-			() -> assertThatThrownBy(() -> inputValidService.validAnswer("1,2,3,4,5,6,7")),
-			() -> assertThatThrownBy(() -> inputValidService.validAnswer("1,2,3,!,5,6")),
-			() -> assertThatThrownBy(() -> inputValidService.validAnswer("1,2,3,4,5,100"))
-		);
+		assertThatThrownBy(() -> inputValidService.validAnswer("1,2,3,4,5,6,7")).isInstanceOf(RuntimeException.class);
+		assertThatThrownBy(() -> inputValidService.validAnswer("1,2,3,!,5,6")).isInstanceOf(RuntimeException.class);
+		assertThatThrownBy(() -> inputValidService.validAnswer("1,2,3,4,5,100")).isInstanceOf(RuntimeException.class);
 	}
 
 	@ParameterizedTest(name = "올바른 당첨 번호 테스트")
@@ -53,10 +49,8 @@ class InputValidServiceTest {
 	@DisplayName("사용자가 입력한 돈 valid 테스트 - 1000으로 나눠지는지, 숫자인지 확인하는 테스트")
 	@Test
 	void validateUserMoneyErrorTest() {
-		Assertions.assertAll(
-			() -> assertThatThrownBy(() -> inputValidService.validateUserMoney("6!!")),
-			() -> assertThatThrownBy(() -> inputValidService.validateUserMoney("412312"))
-		);
+		assertThatThrownBy(() -> inputValidService.validateUserMoney("6!!")).isInstanceOf(RuntimeException.class);
+		assertThatThrownBy(() -> inputValidService.validateUserMoney("412312")).isInstanceOf(RuntimeException.class);
 	}
 
 	@ParameterizedTest(name = "올바른 사용자 지불 금액 테스트")
