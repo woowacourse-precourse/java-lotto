222c36ef407bed84f12f4d159822eb6f99f1f230
anso33 <hyeon131812@gmail.com>
2022-11-14 14:15:10 +0900

    [FEAT](Application): main logic

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+  public static void main(String[] args) {
+    User user = new User();
+    user.setLottos();
+    LottoAnswer lottoAnswer = new LottoAnswer();
+    user.printResult(lottoAnswer);
+  }
 }
