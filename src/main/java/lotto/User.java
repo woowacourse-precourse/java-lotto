59e226ea1b2269fee35ef1248bc0489507a7a961
poter0501 <poter0501@gmail.com>
2022-11-14 22:24:48 +0900

    feat(WinningRanking): WinningRanking enum 구현

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -5,11 +5,43 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+enum WinningRanking{
+    FIRST_WITHOUT_BONUS(6,0, 2_000_000_000),
+    FIRST_WITH_BONUS(6,1, 2_000_000_000),
+    SECOND(5,1, 30_000_000),
+    THIRD_WITHOUT_BONUS(5,0, 1_500_000),
+    THIRD_WITH_BONUS(5,1, 1_500_000),
+    FOURTH_WITHOUT_BONUS(4,0, 50_000),
+    FOURTH_WITH_BONUS(4,1, 50_000),
+    FIFTH_WITHOUT_BONUS(3,0, 5_000),
+    FIFTH_WITH_BONUS(3,1, 5_000);
+
+    private final int countMatches;
+    private final int isMatchBonus;
+    private final int prize;
+    WinningRanking(int countMatches, int isMatchBonus, int prize){
+        this.countMatches=countMatches;
+        this.isMatchBonus=isMatchBonus;
+        this.prize=prize;
+    }
+    public List<Integer> getRanking(){
+        return new ArrayList<>(List.of(countMatches, isMatchBonus));
+    }
+    public int getCountMatches(){
+        return countMatches;
+    }
+    public int getIsMatchBonus(){
+        return isMatchBonus;
+    }
+    public int getPrize(){
+        return prize;
+    }
+
+}
 public class User {
     private List<Lotto> lottos;
     private int price;
     private float earningRate;
-    private int[] winningCount= new int[5];;
 
 
     public User(int price) {
