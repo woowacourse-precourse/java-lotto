b2400a5d7e0ef6579c959d094b7c6d7e8c35c1be
DevJay1024 <devjay1024@gmail.com>
2022-11-15 05:12:54 +0900

    feat: 로또 추첨의 흐름 관리 기능 구현 및 어플리케이션 동작시 실행하도록 설정

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,9 +1,51 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LottoManager {
+
+    UserInterface ui = new UserInterface();
+
+    public void run() {
+        int purchasingAmount = ui.requestPurchaseAmount() / Lotto.PRICE;
+        List<Lotto> userLottos = makeLottosBy(purchasingAmount);
+        ui.showPurchasedResult(userLottos);
+
+        LottoJudge lottoJudge = new LottoJudge(
+                ui.requestWinningNumbers(), ui.requestBonusNumber());
+
+        Map<LottoPlace, Integer> records = new HashMap<>();
+        initRecords(records);
+
+        for(Lotto userLotto : userLottos) {
+            LottoPlace resultPlace = lottoJudge.judgePlace(userLotto);
+            records.put(resultPlace, records.get(resultPlace)+1);
+        }
+
+        ui.showLottoStatistics(records, analyzeMarginRate(records));
+    }
+
+    private List<Lotto> makeLottosBy(int purchasingAmount){
+        List<Lotto> lottos = new ArrayList<>();
+        for(int index=0; index<purchasingAmount; ++index) {
+            List<Integer> lottoCandidate =
+                    Randoms.pickUniqueNumbersInRange(
+                            LottoNumber.LOTTO_MIN_NUMBER, LottoNumber.LOTTO_MAX_NUMBER, Lotto.NUMBERS_COUNT);
+            lottos.add(new Lotto(lottoCandidate));
+        }
+        return lottos;
+    }
+
+    private void initRecords(Map<LottoPlace, Integer> records){
+        for(LottoPlace place : LottoPlace.values()){
+            records.put(place, 0);
+        }
+    }
 
     public double analyzeMarginRate(Map<LottoPlace, Integer> winningRecords) {
         int countPurchased = winningRecords.entrySet().stream()
