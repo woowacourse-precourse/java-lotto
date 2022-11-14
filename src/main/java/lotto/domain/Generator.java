02dc39f365d66c2878d757357e5e5458e8a8c4d8
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-15 04:55:34 +0900

    refactor: 오류처리를 위한 new ArrayList()선언

--- src/main/java/lotto/domain/Generator.java
+++ src/main/java/lotto/domain/Generator.java
@@ -5,16 +5,17 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 public class Generator {
-    public List<List<Integer>> lottery = new ArrayList<List<Integer>>();
+    public List<List<Integer>> lotterys = new ArrayList<List<Integer>>();
     public List<Integer> createRandomLotto() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         Collections.sort(numbers);
         return numbers;
     }
     public List<List<Integer>> createLottoByPay(int pay) {
         for (int i=0; i<pay; i++) {
-            lottery.add(createRandomLotto());
+            lotterys.add(createRandomLotto());
         }
-        return lottery;
+        return lotterys;
     }
+
 }
