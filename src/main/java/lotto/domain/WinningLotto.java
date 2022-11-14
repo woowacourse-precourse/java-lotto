c505c9f01f96058b36e546709ba370399c2daafa
olrlobt <317tmdgjs@naver.com>
2022-11-14 11:47:04 +0900

    feat: 보너스 번호가 당첨 번호와 중복될 경우, 예외 발생

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class WinningLotto {
 
     private Lotto winningLotto;
@@ -8,5 +10,14 @@ public class WinningLotto {
     public WinningLotto(Lotto winningLotto, int bonusNumber) {
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
+        checkContainsBonusNumber();
+    }
+
+    public void checkContainsBonusNumber() {
+        List<Integer> winningNumbers= winningLotto.getNumbers();
+
+        if(winningNumbers.contains(bonusNumber)){
+            throw new IllegalArgumentException();
+        }
     }
 }
