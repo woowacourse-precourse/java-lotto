b8d7426c0399158bc9f5b8e633883b4ff82217c8
drunkenhw <drunkenhw@gmail.com>
2022-11-15 02:41:30 +0900

    feat(WinningLotto): 보너스 번호가 중복되면 예외를 발생시키는 기능 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -6,8 +6,15 @@ public class WinningLotto {
     private final int bonusNumber;
 
     public WinningLotto(Lotto winningLotto, int bonusNumber) {
+        validateDuplicate(winningLotto, bonusNumber);
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateDuplicate(Lotto lotto,int bonusNumber) {
+        if (lotto.contains(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호는 중복될 수 없습니다");
+        }
     }
 
     public int matchCount(Lotto lotto) {
