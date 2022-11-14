e617b37e0e29b8c05b4cc117cce858b803952b32
Yujin830 <rladbwls30@naver.com>
2022-11-14 23:06:21 +0900

    Feat: 로또 구입 갯수 저장

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,10 +4,12 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
         String input = camp.nextstep.edu.missionutils.Console.readLine();
-        int buymoney = Integer.parseInt(input);
+        int buymoney = Integer.parseInt(input), buylotto;
 
         if(buymoney%1000 != 0)
             throw new IllegalArgumentException("[ERROR] 금액은 천원 단위여야 합니다.");
+
+        buylotto = buymoney / 1000;
         // TODO: 프로그램 구현
     }
 }
