f7c58182cc5883af85e1bdfc70cd1a3b96acfe5b
impati <impati0716@icloud.com>
2022-11-14 22:14:25 +0900

    feat(Application) : 구매한 로또 출력 적용

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,10 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+
         programStart();
         Integer cost = Convert.costConvert(Console.readLine());
+
         Purchase purchase = new Purchase();
         purchase.lotto(cost);
+
+        Render.purchaseLottoResultPrint(purchase.getMyLotto());
+
+
+
 
     }
     public static void programStart(){
