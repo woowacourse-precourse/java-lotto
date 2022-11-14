57d43977ec86c86bca7fa9e12bd8264640cabed5
mirasoy <nkskjjbb@gmail.com>
2022-11-14 22:45:02 +0900

    [Feat](LottoGameInfo) : 당첨결과 필드 추가

--- src/main/java/dto/LottoGameInfo.java
+++ src/main/java/dto/LottoGameInfo.java
@@ -1,6 +1,8 @@
 package dto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoGameInfo {
 	
@@ -8,10 +10,27 @@ public class LottoGameInfo {
 	private List<IssueLottoInfo> issueLotto; // 발행된 로또 정보
 	private Lotto winningNumber; // 당첨번호
 	private int bonusNumber;	// 보너스 당첨번호
-	private float rateReturn; 	// 수익률
+	private Map<Integer, Integer> result; // 당첨결과 <등수, 당첨 갯수>
 	
+	public LottoGameInfo(){
+		
+		result = new HashMap<Integer, Integer>();
+		result.put(1, 0);
+		result.put(2, 0);
+		result.put(3, 0);
+		result.put(4, 0);
+		result.put(5, 0);
+		
+	}
 	
-	
+	public Map<Integer, Integer> getResult() {
+		return result;
+	}
+
+	public void setResult(Map<Integer, Integer> result) {
+		this.result = result;
+	}
+
 	public long getInputPrice() {
 		return inputPrice;
 	}
@@ -36,12 +55,5 @@ public class LottoGameInfo {
 	public void setBonusNumber(int bonusNumber) {
 		this.bonusNumber = bonusNumber;
 	}
-	public float getRateReturn() {
-		return rateReturn;
-	}
-	public void setRateReturn(float rateReturn) {
-		this.rateReturn = rateReturn;
-	}
-	
 	
 }
