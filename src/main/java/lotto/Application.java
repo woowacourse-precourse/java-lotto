15e717c557ff9360ce1fea908b0f9efa45eb5a79
Namyunsuk <yunsuk0328@naver.com>
2022-11-15 00:37:38 +0900

    style(Application): formatting

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,11 +3,12 @@ package lotto;
 import lotto.domain.Game;
 
 public class Application {
+
     public static void main(String[] args) {
         Game lottoGame = new Game();
         try {
             lottoGame.start();
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
