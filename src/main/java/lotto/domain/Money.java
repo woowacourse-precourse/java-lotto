1ba4b27ee0773d85743084a93fdff614c4ce471c
bellljun3395 <195850@jnu.ac.kr>
2022-11-14 15:35:07 +0900

    feat(Money): 구현 validateMoneyUnit
    - 돈 입력 단위 검증

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,12 +1,16 @@
 package lotto.domain;
 
 public class Money {
+    private final String WARM = "[ERROR] ";
+    private final String WARM_NOT_VALID_MONEY_UNIT = WARM + "1000원 단위로만 돈일 입력할 수 있습니다.";
     private final int ZERO = 0;
+    private final int LOTTO_PRICE = 1000;
 
     private int currentMoney;
     private int paidMoney;
 
     public Money(int money) {
+        validateMoneyUnit(money);
         this.currentMoney = money;
         this.paidMoney = ZERO;
     }
@@ -21,6 +25,13 @@ public class Money {
 
     public int getInitialMoney() {
         return currentMoney + paidMoney;
+    }
+
+    private void validateMoneyUnit(int money) {
+        if (money % LOTTO_PRICE != ZERO) {
+            System.out.println(WARM_NOT_VALID_MONEY_UNIT);
+            throw new IllegalArgumentException(WARM_NOT_VALID_MONEY_UNIT);
+        }
     }
 
 }
