bd6d7395273bf78fe995f081ff4aae06f48266d9
ChoiYeonho0903 <903yh@naver.com>
2022-11-15 03:14:27 +0900

    feat: 당첨 금액 예외 처리 기능 추가

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -1,7 +1,9 @@
 package lotto.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumber {
     private final List<Integer> winningNumbers;
@@ -10,19 +12,57 @@ public class WinningNumber {
     public WinningNumber(String winningNumberStr, String bonusNumberStr) {
         winningNumbers = parseWinningNumberStr(winningNumberStr);
         bonusNumber = bonusNumberStrToInt(bonusNumberStr);
+        validateBoundary();
+        validateDuplicateWinningNumbersAndBonusNumber();
+        validateWinningNumberSize();
+    }
+
+    private void validateBoundary() {
+        for (Integer number : winningNumbers) {
+            if(number < 1 || number > 45) {
+                throw new IllegalArgumentException(ExceptionMessage.OUT_OF_BOUNDARY.getExceptionMessage());
+            }
+        }
+    }
+
+    private void validateDuplicateWinningNumbersAndBonusNumber() {
+        Set<Integer> checkNumber = new HashSet<>();
+        for (Integer number : winningNumbers) {
+            if(checkNumber.contains(number)) {
+                throw new IllegalArgumentException(ExceptionMessage.DUPLICATE_NUMBER.getExceptionMessage());
+            }
+            checkNumber.add(number);
+        }
+        if(checkNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATE_NUMBER.getExceptionMessage());
+        }
     }
 
     private List<Integer> parseWinningNumberStr(String winningNumberStr) {
         List<Integer> newWinningNumbers = new ArrayList<>();
         String[] numbers = winningNumberStr.split(",");
-        for(String number : numbers) {
-            newWinningNumbers.add(Integer.parseInt(number));
+        try {
+            for(String number : numbers) {
+                newWinningNumbers.add(Integer.parseInt(number));
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionMessage.WRONG_INPUT.getExceptionMessage());
         }
         return newWinningNumbers;
     }
 
+    private void validateWinningNumberSize() {
+        if(winningNumbers.size() != 6) {
+            throw new IllegalArgumentException(ExceptionMessage.WRONG_INPUT.getExceptionMessage());
+        }
+    }
+
     private int bonusNumberStrToInt(String bonusNumberStr) {
-        return Integer.parseInt(bonusNumberStr);
+        try {
+            return Integer.parseInt(bonusNumberStr);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionMessage.WRONG_INPUT.getExceptionMessage());
+        }
     }
 
     public List<Integer> getWinningNumbers() {
--- src/test/java/lotto/model/WinningNumberTest.java
+++ src/test/java/lotto/model/WinningNumberTest.java
@@ -21,4 +21,66 @@ class WinningNumberTest {
         assertThat(winningNumber.getWinningNumbers()).containsOnly(1,2,3,4,5,6);
         assertThat(winningNumber.getBonusNumber()).isEqualTo(7);
     }
+
+    @Test
+    @DisplayName("숫자와 ',' 이외의 문자가 들어오는 경우 예외 발생")
+    void createWinningNumber_Exception1() {
+        // Given
+        String winningNumberStr = "1,2,3,4,5,6j";
+        String bonusNumberStr = "7";
+
+        // When // Then
+        assertThatThrownBy(() -> new WinningNumber(winningNumberStr, bonusNumberStr))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("1~45 범위의 숫자가 들어오는 경우 예외 발생")
+    void createWinningNumber_Exception2() {
+        // Given
+        String winningNumberStr = "1,2,3,4,5,46";
+        String bonusNumberStr = "7";
+
+        // When // Then
+        assertThatThrownBy(() -> new WinningNumber(winningNumberStr, bonusNumberStr))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("중복된 숫자가 들어오는 경우 예외 발생")
+    void createWinningNumber_Exception3() {
+        // Given
+        String winningNumberStr = "1,2,3,4,5,6";
+        String bonusNumberStr = "6";
+
+        // When // Then
+        assertThatThrownBy(() -> new WinningNumber(winningNumberStr, bonusNumberStr))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+
+    @Test
+    @DisplayName("당첨번호 크기가 6이 아닌 경우 예외 발생")
+    void createWinningNumber_Exception4() {
+        // Given
+        String winningNumberStr = "1,2,3,4,5,6,7";
+        String bonusNumberStr = "8";
+
+        // When // Then
+        assertThatThrownBy(() -> new WinningNumber(winningNumberStr, bonusNumberStr))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("bonusNumber에 숫자 이외의 문자가 들어오는 경우 예외 발생")
+    void createWinningNumber_Exception5() {
+        // Given
+        String winningNumberStr = "1,2,3,4,5,6";
+        String bonusNumberStr = "6j";
+
+        // When // Then
+        assertThatThrownBy(() -> new WinningNumber(winningNumberStr, bonusNumberStr))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 }
