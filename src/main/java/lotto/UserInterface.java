f9dd06b701efbcd9487819160bc4c129357c42be
recicoco068 <gkapf838@gmail.com>
2022-11-14 12:34:29 +0900

    feat(UserInterface.java) : 당첨 번호를 입력 받는 기능 구현

--- src/main/java/lotto/UserInterface.java
+++ src/main/java/lotto/UserInterface.java
@@ -1,6 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserInterface {
     public int inputPaidMoney() {
@@ -12,7 +15,22 @@ public class UserInterface {
         return Integer.parseInt(input);
     }
 
+    public List<Integer> inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        if (!validateInputNumberAndComma(input)) {
+            throw new IllegalArgumentException();
+        }
+        return Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
     private boolean validateInputOnlyNumber(String input) {
         return input.matches("[0-9]*");
+    }
+
+    private boolean validateInputNumberAndComma(String input) {
+        return input.matches("[,0-9]*");
     }
 }
