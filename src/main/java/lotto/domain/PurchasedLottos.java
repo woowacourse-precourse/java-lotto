3b70b28c377cf7cd5a55d9eab80486a7d7272b30
jeonghoon <shb03323@naver.com>
2022-11-15 00:01:32 +0900

    fix: 구입한 로또들에 대한 테스트 진행
    - 구입한 로또의 숫자들이 오름차순으로 정렬되어 있는지 확인

--- src/main/java/lotto/domain/PurchasedLottos.java
+++ src/main/java/lotto/domain/PurchasedLottos.java
@@ -1,11 +1,13 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class PurchasedLottos {
     private final List<Lotto> purchasedLottos;
 
     public PurchasedLottos(List<Lotto> lottos) {
+        sortAllLottos(lottos);
         this.purchasedLottos = lottos;
     }
 
@@ -13,7 +15,13 @@ public class PurchasedLottos {
         return purchasedLottos;
     }
 
-    private int getCount(int purchasePrice) {
-        return purchasePrice / LottoCondition.PRICE.getNumber();
+    private void sortAllLottos(List<Lotto> lottos) {
+        for (Lotto lotto: lottos) {
+            sortNumbers(lotto.getNumbers());
+        }
+    }
+
+    private void sortNumbers(List<Integer> numbers) {
+        Collections.sort(numbers);
     }
 }
--- src/test/java/lotto/PurchasedLottosTest.java
+++ src/test/java/lotto/PurchasedLottosTest.java
@@ -2,53 +2,36 @@ package lotto;
 
 import lotto.domain.Lotto;
 import lotto.domain.PurchasedLottos;
-import lotto.message.ErrorMessage;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PurchasedLottosTest {
-    private static final int EACH_LOTTO_PRICE = PurchasedLottos.PRICE;
-    private PurchasedLottos purchasedLottos;
-    private int purchasePrice;
-
-    @BeforeEach
-    void setPurchasedLotto() {
-        purchasePrice = 2000;
-        purchasedLottos = new PurchasedLottos(purchasePrice);
-    }
-
-    @DisplayName("로또 구입 금액이 로또 한 개당 가격에 나누어 떨어지지 않으면 예외가 발생한다.")
+    @DisplayName("구매한 로또들의 번호는 오름차순으로 정렬되어있다.")
     @Test
-    void 구입금액이_로또_개당_가격의_배수가_아닌_경우_예외처리() {
-        purchasePrice = 2500;
+    void checkPurchasedLottoisSorted() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(6);
+        numbers.add(5);
+        numbers.add(4);
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(1);
 
-        assertThatThrownBy(() -> purchasedLottos = new PurchasedLottos(purchasePrice))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.INVALID_PURCHASE_PRICE.getMessage());
-    }
+        Lotto lotto = new Lotto(numbers);
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(lotto);
 
-    @DisplayName("구입한 로또의 개수는 (구입 금액 / 로또 개당 가격)과 일치한다.")
-    @Test
-    void 로또가_구입금액에_맞게_생성되는지_확인() {
-        assertThat(purchasedLottos.getPurchasedLottos().size()).isEqualTo(purchasePrice / EACH_LOTTO_PRICE);
-    }
+        PurchasedLottos purchasedLottos = new PurchasedLottos(lottos);
+        List<Integer> purchasedNumbers = purchasedLottos.getPurchasedLottos().get(0).getNumbers();
+        List<Integer> sortedNumbers = numbers.stream().sorted().collect(Collectors.toList());
 
-    @DisplayName("구매한 로또의 번호는 오름차순으로 정렬된다.")
-    @Test
-    void 로또_번호가_오름차순으로_정렬되는지_확인() {
-        Lotto lotto = purchasedLottos.getPurchasedLottos().get(0);
-        List<Integer> numbers = lotto.getNumbers();
-        List<Integer> sortedNumbers = numbers.stream()
-                .sorted()
-                .collect(Collectors.toList());
-
-        assertThat(numbers).isEqualTo(sortedNumbers);
+        assertThat(purchasedNumbers).isEqualTo(sortedNumbers);
     }
 }
