cbf37e63901b66cdd42d2a24e813eb15be98689c
dong yeol <ehfql6363@naver.com>
2022-11-14 21:15:11 +0900

    FEAT(ErrorMessage.java) : Create ErrorMessage class
    Signed-off-by: dong yeol <ehfql6363@naver.com>

---
+++ src/main/java/Comment/ErrorMessage.java
@@ -0,0 +1,11 @@
+package Comment;
+
+public class ErrorMessage {
+    public static final String NUMBER_LENGTH_ERROR = "로또 번호는 총 6개의 숫자로 구성되어 있습니다.";
+    public static final String NUMBER_OVERLAP_ERROR = "로또 번호는 중복되지 않습니다.";
+    public static final String NUMBER_OUT_OF_RANGE_ERROR = "로또 번호는 1에서 45사이의 수 입니다.";
+    public static final String MIN_COST_ERROR = "로또 최소 구매 금액은 1,000원 입니다.";
+    public static final String MAX_COST_ERROR = "로또는 1회 최대 100,000원까지 구매 가능합니다.";
+    public static final String MONETARY_UNIT_ERROR = "저희 복권 판매점은 거슬러 주지 않습니다.";
+    public static final String INPUT_LOTTO_ERROR = "로또 번호를 쉼표(,)로 구분해서 입력해주세요.";
+}
