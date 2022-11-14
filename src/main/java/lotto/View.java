7b6acbcea06fc0dc89858f120cf7d765e676b25e
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-14 14:26:36 +0900

    feat(View): validateIsDivisible 구현

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -3,5 +3,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class View {
-
+    private void validateIsDivisible(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000으로 나눠떨어져야 한다");
+        }
+    }
 }
