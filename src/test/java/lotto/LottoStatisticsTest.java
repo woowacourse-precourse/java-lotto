8da37b44c7f79f2d88764b4368545dc10f983554
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-15 05:24:55 +0900

    Test(LottoStatisticsTest) : 당첨된 총 상금 금액 반환 테스트 코드 추가 #113
    LottoStatistics클래스의 public BigDecimal getRateOfReturn() 메소드의 테스트 코드를 추가했습니다.
    
    Ref : #97

--- src/test/java/lotto/LottoStatisticsTest.java
+++ src/test/java/lotto/LottoStatisticsTest.java
@@ -9,6 +9,7 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
@@ -45,24 +46,52 @@ class LottoStatisticsTest {
 
     @Test
     @DisplayName("총 상금 확인 테스트")
-    void getTotalPrizeTest(){
+    void getTotalPrizeTest() {
         List<List<Integer>> testNumbers = new ArrayList<>();
         Integer bonusNumber = 4;
         lottoStatistics.setBonusNumber(bonusNumber);
 
-        testNumbers.add(List.of(1,2,3));            //5등
-        testNumbers.add(List.of(1,2));              //4등
+        testNumbers.add(List.of(1, 2, 3));            //5등
+        testNumbers.add(List.of(1, 2));              //4등
         testNumbers.add(List.of(1));            //3등
         testNumbers.add(List.of(1, bonusNumber));   //2등
         testNumbers.add(List.of());                 //1등
 
         Iterator testNumberIter = testNumbers.iterator();
-        while (testNumberIter.hasNext()){
-            List<Integer> tmp = (List<Integer>)testNumberIter.next();
+        while (testNumberIter.hasNext()) {
+            List<Integer> tmp = (List<Integer>) testNumberIter.next();
             lottoStatistics.rankCount(tmp);
         }
         BigInteger totalPrize = lottoStatistics.getTotalPrize();
         assertThat(totalPrize)
                 .isEqualTo(2_031_555_000);
+    }
+
+    @Test
+    @DisplayName("총 수익률 계산 확인")
+    void getRateOfReturnTest() {
+        List<List<Integer>> testNumbers = new ArrayList<>();
+        Integer bonusNumber = 4;
+        lottoStatistics.setBonusNumber(bonusNumber);
+
+        testNumbers.add(List.of(1, 2, 3));            //5등
+        testNumbers.add(List.of(1, 2));              //4등
+        testNumbers.add(List.of(1));            //3등
+        testNumbers.add(List.of(1, bonusNumber));   //2등
+        testNumbers.add(List.of());                 //1등
+
+        Iterator testNumberIter = testNumbers.iterator();
+        while (testNumberIter.hasNext()) {
+            List<Integer> tmp = (List<Integer>) testNumberIter.next();
+            lottoStatistics.rankCount(tmp);
+        }
+        BigInteger totalPrize =
+                lottoStatistics.getTotalPrize();
+        lottoStatistics.setMoney(BigInteger.valueOf(5000));
+        BigDecimal rateReturn =
+                lottoStatistics.getRateOfReturn();
+
+        assertThat(lottoStatistics.getRateOfReturn())
+                .isEqualTo(BigDecimal.valueOf(40631100));
     }
 }
