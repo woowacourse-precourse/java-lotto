3131c8d735faae1ffbe8e3abe2abd221cd574702
안광민 <creativeduck0518@gmail.com>
2022-11-14 14:35:24 +0900

    feat(RewardCalculator): 등수에 따라 당첨금 계산 기능 구현

--- src/main/java/lotto/domain/RewardCalculator.java
+++ src/main/java/lotto/domain/RewardCalculator.java
@@ -1,10 +1,25 @@
 package lotto.domain;
 
+import lotto.data.LottoRank;
+
+import java.util.Map;
+
 public class RewardCalculator {
 
     private double createProfitInFirstDecimal(long totalRevenue, long price) {
         double profit = (double) totalRevenue / price * 100;
 
         return Math.round(profit * 10) / (double) 10;
+    }
+
+    private long createTotalRevenue(Map<LottoRank, Integer> winningCounts) {
+        long totalRevenue = 0L;
+
+        for (LottoRank lottoRank : winningCounts.keySet()) {
+            int winningAmount = winningCounts.get(lottoRank);
+            totalRevenue += winningAmount * lottoRank.getReward();
+        }
+
+        return totalRevenue;
     }
 }
