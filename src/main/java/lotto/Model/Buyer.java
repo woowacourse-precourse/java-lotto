eefc680dd7d225e7b5f8e9d6c49efadefba900e1
aossuper7 <aossuper7@naver.com>
2022-11-14 23:53:56 +0900

    feat: 구입금액이 천원 단위인지 0원 인지 확인 후 예외처리 기능

--- src/main/java/lotto/Model/Buyer.java
+++ src/main/java/lotto/Model/Buyer.java
@@ -1,12 +1,22 @@
 package lotto.Model;
 
+import lotto.Constant.Message;
 import lotto.Utils.Convert;
 
 public class Buyer {
 
+    private static final int THOUSAND_UNIT = 1000;
+    private static final int ZERO = 0;
     private final int money;
 
     public Buyer(final String money) {
         this.money = Convert.StrToInt(money);
+        isThousandUnit(this.money);
+    }
+
+    private void isThousandUnit(final int money) {
+        if (money % THOUSAND_UNIT != ZERO || money == ZERO) {
+            throw new IllegalArgumentException(Message.ERROR_THOUSAND_UNIT.getMessage());
+        }
     }
 }
