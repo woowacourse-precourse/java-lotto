b2b5ebc3e5b2b14e3fa103b97972103a8119077d
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-15 02:11:49 +0900

    feat: 입력 받은 구매금액이 유효한 값인지 확인
    - 문제가 있는 경우 예외처리 발생

--- src/main/java/lotto/view/LottoServiceView.java
+++ src/main/java/lotto/view/LottoServiceView.java
@@ -8,7 +8,48 @@ public class LottoServiceView {
 
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
-
+        validateInputMoney(input);
         return 1;
+    }
+
+    private void validateInputMoney(String input) {
+
+        checkMinimunAmount(input);
+        checkOnlyInteger(input);
+        checkOverFlow(input);
+        checkMonetaryUnit(input);
+    }
+
+    private void checkMinimunAmount(String input) {
+        if (input.length() < 4) {
+            throw new IllegalArgumentException("[ERROR] 최소 구매 금액은 1,000원 이상입니다.");
+        }
+    }
+
+    private void checkMonetaryUnit(String input) {
+
+        for (int i = 0; i < 3; i++) {
+            if (!(input.charAt(i) == '0')) {
+                throw new IllegalArgumentException("[ERROR] 구매 금액의 최소 단위는 1,000 입니다. 1,000 단위로 다시 입력해주세요");
+            }
+        }
+    }
+
+    private void checkOverFlow(String input) {
+
+        if (input.length() > 9) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 최대 10억 미만으로 입력해야 합니다.");
+        }
+    }
+
+    private void checkOnlyInteger(String input) {
+
+        char value;
+        for (int i = 0; i < input.length(); i++) {
+            value = input.charAt(i);
+            if (value < '0' || value > '9') {
+                throw new IllegalArgumentException("[ERROR] 구매 금액은 숫자만 입력되어야 합니다");
+            }
+        }
     }
 }
