c254af121783be4b4b8a5500bd6050c370c5c490
kimJaeHyeon <kmr2644@gmail.com>
2022-11-14 22:49:42 +0900

    feat: 게임시작 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,14 @@
 package lotto;
 
+import lotto.domain.LottoRepository;
+import lotto.service.LottoService;
+import lotto.view.View;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoGame lottoGame = new LottoGame(new View(), new LottoService(new LottoRepository()));
+        lottoGame.start();
 
     }
 }
