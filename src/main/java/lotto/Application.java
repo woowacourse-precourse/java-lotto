fa9ac7452375f4a616ff9469e0540e8206ad3fca
CAEL0 <kjongmin26@gmail.com>
2022-11-14 22:51:24 +0900

    feat(Application): 메인 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.GameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        playGame();
+    }
+
+    public static void playGame() {
+        GameController gameController = new GameController();
+        gameController.run();
     }
 }
