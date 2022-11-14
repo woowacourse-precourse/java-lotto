2bf3a84428983ef77dd10a73393f1e0cfd3c45db
alstjr706 <alstjr706@naver.com>
2022-11-14 12:51:03 +0900

    refactor : (로또 게임) 리스트 정렬 스트림으로 리팩토링

--- src/main/java/lotto/domain/RandomLottoGenerator.java
+++ src/main/java/lotto/domain/RandomLottoGenerator.java
@@ -3,15 +3,16 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import lotto.constants.SystemConsole;
 
 public class RandomLottoGenerator {
     private static final int LOTTO_START_NUM = 1;
     private static final int LOTTO_END_NUM = 45;
     private static final int LOTTO_SIZE_NUM = 6;
 
-    public List<List<Integer>> createTicket(int ticketsCount) {
+    public List<List<Integer>> createTicket(int ticketsCount, SystemConsole systemConsole) {
         List<List<Integer>> randomLotto = new ArrayList<>();
 
         for (int ticket = 0; ticket < ticketsCount; ticket++) {
@@ -19,12 +20,20 @@ public class RandomLottoGenerator {
         }
 
         lottoTheorem(randomLotto);
+        consoleOutput(randomLotto);
+
         return randomLotto;
+    }
+
+    private void consoleOutput(List<List<Integer>> randomLotto) {
+        for (List<Integer> lottoTickets : randomLotto) {
+            System.out.println(lottoTickets);
+        }
     }
 
     public void lottoTheorem(List<List<Integer>> randomLotto) {
         for (List<Integer> randomNumber : randomLotto) {
-            Collections.sort(randomNumber);
+            randomNumber.stream().sorted().collect(Collectors.toList());
         }
     }
 }
