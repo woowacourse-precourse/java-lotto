24dc71ec2faa7e27bc188f6e79bf65b31f090ec5
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-14 23:37:40 +0900

    test(lotto): 로또 생성시 중복되지 않고 오름차순으로 6개의 로또 번호 생성 테스트 완료

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,9 +1,10 @@
-package lotto;
+package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import java.util.List;
-import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,10 +19,28 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호는 오름차순으로 정렬되어야한다.")
+    @Test
+    void createLottoSortAsc() {
+        // given
+        Lotto lotto = new Lotto(List.of(5, 1, 2, 10, 4, 7));
+        List<LottoNumber> temp = new ArrayList<>();
+
+        // when
+        for (LottoNumber lottoNumber : lotto) {
+            temp.add(lottoNumber);
+        }
+
+        // then
+        for (int i = 0; i < temp.size() - 1; ++i) {
+            LottoNumber beforeNumber = temp.get(i);
+            LottoNumber afterNumber = temp.get(i + 1);
+            assertThat(beforeNumber.compareTo(afterNumber)).isLessThanOrEqualTo(-1);
+        }
+    }
+
 }
