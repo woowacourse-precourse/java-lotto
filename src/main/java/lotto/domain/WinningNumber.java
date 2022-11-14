b3f511025d8dcd411563f1ff78a4fbeca4a7e876
footdev <ekclstkfka5@gmail.com>
2022-11-14 23:15:19 +0900

    feat: WinningNumber 클래스 구현
    정답번호의 상태를 가지는 클래스 구현함.

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,4 +1,21 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class WinningNumber {
+    private final List<Integer> number;
+    private final int bonus;
+
+    public WinningNumber(List<Integer> number, int bonus) {
+        this.number = number;
+        this.bonus = bonus;
+    }
+
+    public List<Integer> getNumber() {
+        return number;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
 }
