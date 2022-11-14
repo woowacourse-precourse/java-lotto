ca6419ec7e24e409dbd87a18fc6ef50a628c6c58
jagajang <kosb7544@gmail.com>
2022-11-14 16:19:29 +0900

    dev: add RANKING message

--- src/main/java/lotto/IO/message/IOMessage.java
+++ src/main/java/lotto/IO/message/IOMessage.java
@@ -1,11 +1,18 @@
 package lotto.IO.message;
 
+import lotto.config.LottoConfig;
+
 public enum IOMessage {
     EMPTY_LINE(""),
     ASK_PRICE("구입금액을 입력해 주세요."),
     LOTTO_BUY_NUM("개를 구매했습니다."),
     ASK_ANSWER("당첨 번호를 입력해 주세요."),
-    ASK_BONUS("보너스 번호를 입력해 주세요.");
+    ASK_BONUS("보너스 번호를 입력해 주세요."),
+    RANK_0("6개 일치 (" + LottoConfig.awardsToString.get(0) + "원) - "),
+    RANK_1("5개 일치, 보너스 볼 일치 (" + LottoConfig.awardsToString.get(1) + "원) - "),
+    RANK_2("5개 일치 (" + LottoConfig.awardsToString.get(2) + "원) - "),
+    RANK_3("4개 일치 (" + LottoConfig.awardsToString.get(3) + "원) - "),
+    RANK_4("3개 일치 (" + LottoConfig.awardsToString.get(4) + "원) - ");
 
     private final String message;
 
--- src/main/java/lotto/config/LottoConfig.java
+++ src/main/java/lotto/config/LottoConfig.java
@@ -4,5 +4,8 @@ import java.util.List;
 
 public class LottoConfig {
     public static final int LOTTO_PRICE = 1000;
-    public static final List<Integer> awards = List.of(2000000000, 30000000, 1500000, 50000, 5000);
+    public static final List<Integer> awards =
+            List.of(2000000000, 30000000, 1500000, 50000, 5000);
+    public static final List<String> awardsToString =
+            List.of("2,000,000,000", "30,000,000", "1,500,000", "50,000", "5,000");
 }
