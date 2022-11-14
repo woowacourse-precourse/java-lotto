d8ddfe327c55e0838036d0e9744c73768697020b
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-14 21:36:09 +0900

    test(Lotto): 로또가 몇 등인지 알맞게 알려주는지 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +24,45 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또가 1등인지 확인한다")
+    @Test
+    void 로또_1등() {
+        //given
+        int money = 3000;
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        //when
+        Rank lottoRank = lotto.matchRank(winningNumber, bonusNumber);
+        //then
+        assertThat(lottoRank).isEqualTo(Rank.FIRST);
+    }
+
+    @DisplayName("로또가 2등인지 확인한다")
+    @Test
+    void 로또_2등() {
+        //given
+        int money = 3000;
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        //when
+        Rank lottoRank = lotto.matchRank(winningNumber, bonusNumber);
+        //then
+        assertThat(lottoRank).isEqualTo(Rank.SECOND);
+    }
+
+    @DisplayName("로또가 낙첨인지 확인한다")
+    @Test
+    void 로또_낙첨() {
+        //given
+        int money = 3000;
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        Lotto lotto = new Lotto(List.of(8, 9, 10, 11, 12, 13));
+        //when
+        Rank lottoRank = lotto.matchRank(winningNumber, bonusNumber);
+        //then
+        assertThat(lottoRank).isEqualTo(Rank.NOTHING);
+    }
 }
