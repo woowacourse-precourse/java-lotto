a658f01974df1477505f62a41f5302c6e97c9f4c
CAEL0 <kjongmin26@gmail.com>
2022-11-14 18:29:23 +0900

    feat(WinningConstants): 등수 추가

--- src/main/java/lotto/constant/WinningConstants.java
+++ src/main/java/lotto/constant/WinningConstants.java
@@ -2,18 +2,20 @@ package lotto.constant;
 
 public enum WinningConstants {
 
-    FIFTH_PLACE(3, false, 5_000),
-    FOURTH_PLACE(4, false, 50_000),
-    THIRD_PLACE(5, false, 1_500_000),
-    SECOND_PLACE(5, true, 30_000_000),
-    FIRST_PLACE(6, false, 2_000_000_000);
+    FIFTH_PLACE(5, 3, false, 5_000),
+    FOURTH_PLACE(4, 4, false, 50_000),
+    THIRD_PLACE(3, 5, false, 1_500_000),
+    SECOND_PLACE(2, 5, true, 30_000_000),
+    FIRST_PLACE(1, 6, false, 2_000_000_000);
 
+    private final int place;
     private final int requiredWinningNumber;
     private final boolean hasBonusNumber;
     private final int reward;
     private final String message;
 
-    WinningConstants(int requiredWinningNumber, boolean hasBonusNumber, int reward) {
+    WinningConstants(int place, int requiredWinningNumber, boolean hasBonusNumber, int reward) {
+        this.place = place;
         this.requiredWinningNumber = requiredWinningNumber;
         this.hasBonusNumber = hasBonusNumber;
         this.reward = reward;
@@ -31,6 +33,10 @@ public enum WinningConstants {
 
         String message = stringBuilder.toString();
         return String.format(message, requiredWinningNumber, reward);
+    }
+
+    public int getPlace() {
+        return place;
     }
 
     public int getRequiredWinningNumber() {
