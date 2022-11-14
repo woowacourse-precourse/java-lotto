833728cf90c4b148b7e6536a71382923d2d16201
dev_kong <ahdjd5@gmail.com>
2022-11-14 23:00:53 +0900

    refactor(LottoFactory): remove unused import

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -1,8 +1,5 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
