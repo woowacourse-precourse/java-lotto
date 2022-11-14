3917d60dd8a26a18eef66166d5d1a6ba352d26c4
Dabeen Jeong <hello70825@gmail.com>
2022-11-14 15:05:05 +0900

    refactor: 로또 번호의 개수가 6개인지 확인하는 메소드를 이동

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +13,12 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호의 개수가 6개가 아닙니다.");
         }
     }
-
-    // TODO: 추가 기능 구현
 }
