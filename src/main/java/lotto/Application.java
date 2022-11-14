7c2f75aefcdd3170e92f602dde7d1352170817fd
shpark30 <seohyeonpark30.gmail.com>
2022-11-14 23:43:12 +0900

    feat: 로또 프로그램 도메인 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,35 @@
 package lotto;
 
+import lotto.domain.Bag;
+import lotto.domain.Bank;
+import lotto.domain.BankTeller;
+import lotto.domain.Customer;
+import lotto.domain.LottoCompany;
+import lotto.domain.LottoPrinter;
+import lotto.domain.LottoReader;
+import lotto.domain.LottoStore;
+import lotto.domain.LottoStoreClerk;
+import lotto.domain.PrizeCalculator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            // 로또 구입
+            Customer customer = new Customer(new Bag());
+            LottoStoreClerk clerk = new LottoStoreClerk(new LottoPrinter());
+            LottoStore lottoStore = new LottoStore(clerk);
+            lottoStore.sellLotto(customer);
+
+            // 당첨 번호 추첨
+            LottoCompany lottoCompany = new LottoCompany();
+            lottoCompany.draw();
+
+            // 당첨금 확인
+            BankTeller bankTeller = new BankTeller(new PrizeCalculator(), new LottoReader());
+            Bank bank = new Bank(bankTeller);
+            bank.givePrize(customer, lottoCompany);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
