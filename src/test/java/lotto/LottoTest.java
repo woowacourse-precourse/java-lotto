a68a58f81ed0efcbd257266115b6f18534a16d3a
daeun <gain3436@gmail.com>
2022-11-15 02:56:19 +0900

    test : 로또번호 정상 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -30,5 +31,11 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 100, 4, 5, 6)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-    
+
+    @DisplayName("정상 로또 번호 입력시 예외가 발생하지 않는다.")
+    @Test
+    void createLottoBySuccess() {
+        assertDoesNotThrow(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+    }
+
 }
