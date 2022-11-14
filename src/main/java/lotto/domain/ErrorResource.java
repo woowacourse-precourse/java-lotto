00963c1c136c4ac6f3a7bdcce714dfded08d674d
starwook <star.wookk@gmail.com>
2022-11-15 03:02:27 +0900

    feat(ErrorResource.java): add ErrorResource.java
    에러 메시지 출력을 위한 상수가 있는 클래스 추가

---
+++ src/main/java/lotto/domain/ErrorResource.java
@@ -0,0 +1,6 @@
+package lotto.domain;
+
+public class ErrorResource {
+    public static final String errorStart = "[ERROR]";
+    public static final String numberCountNotFit = "숫자의 크기가 " +Resoure.numberSize +"이 아닙니다";
+}
