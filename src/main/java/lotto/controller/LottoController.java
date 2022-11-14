5416c14255778f48bfd5d01db9f4827a01fe16a9
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-15 08:05:25 +0900

    refactor: 로또 게임 실행 로직 분리
    결과 계산 메소드, 수익률 계산 메소드 추가 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,51 +1,75 @@
 package lotto.controller;
 
 import lotto.domain.LottoRank;
-import lotto.view.InputView;
-import lotto.view.OutputView;
+import lotto.domain.LottoTicket;
+import lotto.domain.WinningNumbers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static lotto.view.InputView.lottoPurchaseMoney;
+import static lotto.view.InputView.*;
+import static lotto.view.OutputView.*;
 
 public class LottoController {
 
     private final int LOTTO_ONE_GAME_PRICE = 1000;
+    private static final int PERCENTAGE_CONVERTER = 100;
+    private Map<LottoRank, Integer> lottoResult = new HashMap<>();
 
-    InputView inputView = new InputView();
-    OutputView outputView = new OutputView();
-    LottoRank lottoRank;
-
-    // 게임을 실행하는 메소드
-    public void run(){
-        outputView.printPriceInputMessage();
-
-        int inputLottoMoney = inputView.inputLottoPurchaseMoney();
+    public void run() {
+        int inputLottoMoney = inputLottoPurchaseMoney();
         int lottoTickets = createLottoTickets(inputLottoMoney);
-        List<Integer> winningNumbers = createWinningNumbers();
-        int bonusNumber = inputView.inputBonusNumber();
-        inputView.allLottoNumber = inputView.createAllGameLottoNumber(lottoTickets);
+        List<Integer> winningNumber = inputWinningNumber();
+        int bonusNumber = inputBonusNumber();
 
-        outputView.printLottoPurchaseCompleteMessage(lottoTickets);
-        outputView.printLottoNumber(inputView.allLottoNumber);
-
-        for (int i = 0; i < inputView.allLottoNumber.size(); i++) {
-            inputView.compareWinningNumber(inputView.allLottoNumber.get(i),winningNumbers,bonusNumber);
-        }
-
-        outputView.printLottoRankResult(inputView.lottoResult);
-        int lottoEarningMoney = inputView.calculrateLottoEarningMoney(inputView.lottoResult);
-        String lottoEarningRate = inputView.calculrateLottoEarningRate(lottoEarningMoney, lottoPurchaseMoney);
-        outputView.printLottoEarningRate(lottoEarningRate);
-
-
+        WinningNumbers winningNumbers = new WinningNumbers(winningNumber, bonusNumber);
+        printLottoPurchaseCompleteMessage(lottoTickets);
+        calculrateLotto(lottoTickets,winningNumbers);
+        int lottoEarningMoney = calculrateLottoEarningMoney();
+        double lottoEarningRate = calculrateLottoEarningRate(lottoEarningMoney, lottoPurchaseMoney);
+        printLottoEarningRate(lottoEarningRate);
     }
 
-    private int createLottoTickets (int inputMoney) {
+    private int createLottoTickets(int inputMoney) {
         return inputMoney / LOTTO_ONE_GAME_PRICE;
     }
 
-    private List<Integer> createWinningNumbers () {
-        return inputView.createWinningNumber();
+    private void calculrateLotto(int lottoTickets, WinningNumbers winningNumbers) {
+        lottoResultMap();
+        for (int i = 0; i < lottoTickets; i++) {
+            List<Integer> lotto = LottoTicket.createLottoOneGameNumber();
+            LottoRank lottoRank = winningNumbers.calculrateRank(lotto);
+            calculrateResult(lottoRank, winningNumbers, lotto);
+        }
     }
+
+    private void lottoResultMap() {
+        for (LottoRank lottoRank : LottoRank.values()) {
+            lottoResult.put(lottoRank, 0);
+        }
+    }
+
+    private void calculrateResult(LottoRank lottoRank, WinningNumbers winningNumbers, List<Integer> lotto) {
+        lottoRank = winningNumbers.calculrateRank(lotto);
+        lottoResult.put(lottoRank, lottoResult.getOrDefault(lottoRank, 0) + 1);
+        printLottoNumber(lotto);
+    }
+
+    private int calculrateLottoEarningMoney() {
+        int earningMoney = 0;
+        for (LottoRank lottoRank : lottoResult.keySet()) {
+            if (lottoResult.get(lottoRank) >= 1) {
+                earningMoney += lottoResult.get(lottoRank) * lottoRank.getRewardMoney();
+            }
+        }
+        printLottoResult(lottoResult);
+        return earningMoney;
+    }
+
+    public static double calculrateLottoEarningRate(int earningMoney, int purchaseMoney) {
+        double earningRate = (double) earningMoney / purchaseMoney * PERCENTAGE_CONVERTER;
+        return earningRate;
+    }
+
 }
