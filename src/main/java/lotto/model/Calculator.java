3288aea3dd5806966eedf1c45efe1d6db228d042
wwwndus <simock22@gmail.com>
2022-11-15 01:15:04 +0900

    feat(Calculator): 로또 당첨 개수와 상금을 곱하여 총 상금 구하기, 총 상금과 구입 금액을 나누어 수익률 계산하기

--- src/main/java/lotto/model/Calculator.java
+++ src/main/java/lotto/model/Calculator.java
@@ -1,4 +1,22 @@
 package lotto.model;
 
 public class Calculator {
+
+    public int getReward(int[] REWARD_LEVEL, int[] result) {
+        int reward = 0;
+
+        for (int i = 0; i <= 4; i++) {
+            if (result[i] != 0) {
+                reward += (REWARD_LEVEL[i] * result[i]);
+            }
+        }
+        return reward;
+    }
+
+    public String getRate(int inputMoney, int reward) {
+        double rateWithPoint = (reward * 1.0 / inputMoney);
+
+        String rate = String.valueOf(Math.round(rateWithPoint * 10000) / 100.0);
+        return rate;
+    }
 }
