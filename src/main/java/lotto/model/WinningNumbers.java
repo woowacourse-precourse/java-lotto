cfdbaee6b12e1807ba170021b992cad4ff734319
dhkim <akdlzmf1123@gmail.com>
2022-11-14 17:07:54 +0900

    feat(WinningNumbers): 보너스 번호가 당첨 번호와 중복인지 확인하는 기능 추가

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -3,12 +3,21 @@ package lotto.model;
 import java.util.List;
 
 public class WinningNumbers {
+    private static final String ERR_DUPLICATE_BONUS_NUMBER = "[ERROR] 당첨 번호와 보너스 번호가 중복입니다.";
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
 
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+
+        validateNoDuplicateBonusNumber(winningNumbers, bonusNumber);
+    }
+
+    private void validateNoDuplicateBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ERR_DUPLICATE_BONUS_NUMBER);
+        }
     }
 
     private boolean isWinningNumber(int number) {
