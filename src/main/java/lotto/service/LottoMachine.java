3addeb98cd0d980069643f86c70749c4679b16e6
bellljun3395 <195850@jnu.ac.kr>
2022-11-14 16:19:12 +0900

    feat(LottoMachine): 구현 makeLottos
    - Money를 받아 그 값에 따라 알맞는 개수의 lotto를 발행해준다.

---
+++ src/main/java/lotto/service/LottoMachine.java
@@ -0,0 +1,32 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Money;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoNumber;
+import lotto.domain.lotto.LottoNumbers;
+import lotto.domain.lotto.Lottos;
+import lotto.domain.lotto.correctLotto.CorrectLottoNumbers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    private static final int ZERO = 0;
+    private static final int LOTTO_PRICE = 1000;
+    private static final int START_RANDOM = 1;
+    private static final int END_RANDOM = 45;
+    private static final int RANDOM_COUNT = 6;
+
+    public static Lottos makeLottos(Money money) {
+        int count = money.getCurrentMoney() / LOTTO_PRICE;
+        money.paid(LOTTO_PRICE, count);
+
+        ArrayList<Lotto> lottos = new ArrayList<>();
+        for (int i = ZERO; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_RANDOM, END_RANDOM, RANDOM_COUNT);
+            lottos.add(new Lotto(numbers));
+        }
+        return new Lottos(lottos);
+    }
+}
