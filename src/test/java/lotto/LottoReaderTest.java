e17728ae314d498c7331034e3bd1c511120424db
anneu <b.imnuj@gmail.com>
2022-11-14 17:17:04 +0900

    test: 로또 당첨 결과를 구하는 기능 테스트

---
+++ src/test/java/lotto/LottoReaderTest.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import static lotto.LottoRank.RANK1;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoReaderTest {
+    private LottoReader lottoReader;
+
+    @BeforeEach
+    void setUp() {
+        lottoReader = new LottoReader();
+    }
+
+    @DisplayName("로또 당첨 결과를 구합니다.")
+    @Test
+    void createLottoResult() {
+        // given
+        List<Lotto> publishedLotto = new ArrayList<>();
+        publishedLotto.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Bonus bonus = new Bonus(10, winningLotto);
+
+        // when
+        Map<LottoRank, Integer> lottoResult = lottoReader.createLottoResult(publishedLotto, winningLotto, bonus);
+
+        // then
+        assertThat(lottoResult).containsEntry(RANK1, 1);
+    }
+}
