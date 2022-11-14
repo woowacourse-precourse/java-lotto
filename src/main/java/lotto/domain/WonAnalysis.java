3ac67a32db2e365c8ce36df6e1b21a2cc8458991
josolha <josolha@nate.com>
2022-11-15 03:16:18 +0900

    refactor(WonAnalysis) : 변수이름 수정

--- src/main/java/lotto/domain/WonAnalysis.java
+++ src/main/java/lotto/domain/WonAnalysis.java
@@ -9,26 +9,23 @@ public class WonAnalysis {
 
     }
 
-    public List<Rank> Result(List<Integer> lotto, List<List<Integer>> buyLottos, int bonusNumber) {
-
+    public List<Rank> createLottoResult(List<Integer> lotto, List<List<Integer>> userLotto) {
         List<Rank> lottoResult = new ArrayList<>();
 
-        for (List<Integer> numbers : buyLottos) {
+        for (List<Integer> numbers : userLotto) {
+            int count = countMatchNumber(lotto, numbers);
+            boolean bonusNumber = checkBonusNumber(count, numbers);
 
-            int count = cntMatch(lotto, numbers);
-
-            boolean hasbonusNumber = hasBonus(count, numbers, bonusNumber);
-
-            lottoResult.add(lottoRank(count, hasbonusNumber));
+            lottoResult.add(rankLotto(count, bonusNumber));
         }
 
         return lottoResult;
     }
 
-    public int cntMatch(List<Integer> lotto, List<Integer> buyLotto) {
+    public int countMatchNumber(List<Integer> lotto, List<Integer> userLotto) {
         int count = 0;
 
-        for (int number : buyLotto) {
+        for (int number : userLotto) {
             if (lotto.contains(number)) {
                 count++;
             }
@@ -36,35 +33,50 @@ public class WonAnalysis {
         return count;
     }
 
-    public boolean hasBonus(int matchNumber, List<Integer> buyLotto, int bonusNumber) {
+    /**
+     * 입력받는 lotto가 bonus 번호를 가지고있으면 true, 아니면 false 반환
+     *
+     * @param userLotto
+     * @return
+     */
+    public boolean checkBonusNumber(int matchNumber, List<Integer> userLotto) {
         if (matchNumber == Rank.SECOND.getRANK_NUMBER()) {
-            if (buyLotto.contains(bonusNumber)) {
+            if (userLotto.contains(BonusNumber.BONUS_NUMBER.getNumber())) {
                 return true;
             }
         }
         return false;
     }
 
-    public Rank lottoRank(int count, boolean hasbonusNumber) {
+    public Rank rankLotto(int count, boolean bonusNumber) {
         for (Rank rank : Rank.values()) {
-            if (count == rank.getRANK_NUMBER() && hasbonusNumber == rank.getBONUS_NUMBER()) {
+
+            if (count == rank.getRANK_NUMBER() && bonusNumber == rank.getBONUS_NUMBER()) {
                 return rank;
             }
         }
+
         return Rank.BLANK;
     }
 
-    public int totalProfit(List<Rank> lottoResult) {
+    public int getLottoAmount(int money) {
+        int amount = money / 1000;
+
+        return amount;
+    }
+
+    public int calculateProfit(List<Rank> lottoResult) {
         int profit = 0;
 
         for (Rank rank : lottoResult) {
             profit += rank.getPROFIT();
         }
+
         return profit;
     }
 
-    public String totalPercent(List<Rank> lottoResult) {
-        long profit = totalProfit(lottoResult);
+    public String calculatePercent(List<Rank> lottoResult) {
+        long profit = calculateProfit(lottoResult);
         int inputMoney = lottoResult.size() * 1000;
 
         double result = (profit * 100) / (double) inputMoney;
