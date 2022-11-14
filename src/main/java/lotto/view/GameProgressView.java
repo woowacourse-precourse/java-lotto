1de3f4459131c8fe574ea96c9304ed352a97b9ad
jjmin9797 <jjmin9797@naver.com>
2022-11-14 15:32:53 +0900

    feat(GameProgressView.java): 당첨 번호 입력시 메시지 출력 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/view/GameProgressView.java
+++ src/main/java/lotto/view/GameProgressView.java
@@ -7,18 +7,24 @@ import java.util.ArrayList;
 public class GameProgressView {
     public static final String PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String COUNT_MESSAGE = "개를 구매했습니다.";
+    public static final String ENTER_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public static void showPurchaseMessage() {
         System.out.println(PURCHASE_MESSAGE);
     }
 
-    public static void showCountMessage(int count){
-        System.out.println(count+COUNT_MESSAGE);
+    public static void showCountMessage(int count) {
+        System.out.println(count + COUNT_MESSAGE);
     }
 
-    public static void showBuyLottery(ArrayList<Lotto> lottery){
+    public static void showBuyLottery(ArrayList<Lotto> lottery) {
         for (Lotto lotto : lottery) {
             System.out.println(lotto.getNumbers());
         }
     }
+
+    public static void showEnterWinningNumbersMessage() {
+        System.out.println(ENTER_WINNING_NUMBERS_MESSAGE);
+    }
+
 }
