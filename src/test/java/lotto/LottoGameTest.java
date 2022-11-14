753120f0f0085eae4cb285463e1a71ae48d1031b
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-15 02:19:56 +0900

    test : 로또 구입 금액만큼 로또를 생성하면 성공이다.

---
+++ src/test/java/lotto/LottoGameTest.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoGameTest {
+    @Test
+    @DisplayName("로또 구입 금액만큼 로또를 생성하면 성공이다.")
+    void createLottoAsMoney() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
+        int money = 14000;
+
+        LottoGame lottoGame = new LottoGame(new Money(money));
+
+        Field field = lottoGame.getClass().getDeclaredField("amount");
+        field.setAccessible(true);
+
+        int lottoAmount = (int) field.get(lottoGame);
+
+        Method method = lottoGame.getClass().getDeclaredMethod("getLottoAmount", int.class);
+        method.setAccessible(true);
+        method.invoke(lottoGame, money);
+
+        assertThat(lottoAmount).isEqualTo(14);
+    }
+}
