551729008630592de06f2924dddddbb9df2a9eb6
baksohyeon <bori12370@gmail.com>
2022-11-15 01:11:03 +0900

    refactor: amend package name
    comment: delete unnecessary comment

--- src/main/java/lotto/service/LottoMachine/LotteryNumbersDraw.java
+++ src/main/java/lotto/service/LottoMachine/LotteryNumbersDraw.java
@@ -1,13 +1,10 @@
-package lotto.service.executeLotto;
+package lotto.service.LottoMachine;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LotteryNumbersDraw {
-    // userInput 3, 5, 7, 8, 19, 36
-    // string 에서 불필요한 띄어쓰기는 제거하고 값을 받기
-
     private static final String SEPARATOR = ",";
     public List<Integer> drawNumbers(String userInput) {
         try {
