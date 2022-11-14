f0f18d0dc225b07a3617316aaa1f963c48f4d172
hgo641 <hgo641@gmail.com>
2022-11-14 15:18:42 +0900

    feat: 당첨 번호와 보너스 번호를 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,10 +4,24 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_MONEY_MESSAGE = "구입 금액을 입력해주세요.";
+    private static final String INPUT_WINNiNG_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static String inputMoneyByUser(){
         System.out.println(INPUT_MONEY_MESSAGE);
         String userMoney = Console.readLine();
         return userMoney;
+    }
+
+    public static String inputWinningNumbers(){
+        System.out.println(INPUT_WINNiNG_NUMBERS_MESSAGE);
+        String winningNumbers = Console.readLine();
+        return winningNumbers;
+    }
+
+    public static String inputBonusNumber(){
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        String bonusNumber = Console.readLine();
+        return bonusNumber;
     }
 }
