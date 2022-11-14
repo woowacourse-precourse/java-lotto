701a3bcdcaca0a8ad73099b503861f9e8c1abdfe
areyouhun <areyouhun@gmail.com>
2022-11-15 04:36:29 +0900

    test: winner 클래스의 파라미터 타입 변경에 따른 테스트 코드 수정

--- src/test/java/lotto/ConvertorTest.java
+++ src/test/java/lotto/ConvertorTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ConvertorTest {
-    @DisplayName("입력값에 숫자가 아닌 문자가 있으면 예외 발생")
+    @DisplayName("가격 입력 시 숫자가 아닌 문자가 있으면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1300k", "이만원", "1ooo", "twentythousand", "!7000"})
     void nonNumeric(String input) {
@@ -18,12 +18,30 @@ class ConvertorTest {
                 .hasMessageContaining(ERROR_TITLE + NON_NUMERIC);
     }
 
-    @DisplayName("입력값이 천 원 단위가 아니면 예외 발생")
+    @DisplayName("가격 입력 시 천 원 단위가 아니면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"10005", "50010", "2080332900", "1300"})
     void nonDividedByThousand(String input) {
         assertThatThrownBy(() -> Convertor.toPurchasePrice(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_TITLE + NOT_IN_THOUSANDS);
+    }
+
+    @DisplayName("당첨 번호 입력 시 6개의 쉼표를 사용하지 않으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1.2.3.4.5.6", "12345,6", "1&2&3&4&5&6", "1+2+3+4+5+6", "12345and6", "1,2,3,4,5,6,7"})
+    void winningNumbersWithoutComma(String input) {
+        assertThatThrownBy(() -> Convertor.separate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_TITLE + NO_COMMAS_FOUND);
+    }
+
+    @DisplayName("당첨 번호 입력 시 숫자를 올바르게 쓰지 않으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5, 6:45", "1,!!,3,4,5,6:45", "하나,2,3,4,5,6:45", "one,2,3,4,5,6:45"})
+    void winningNumbersWithoutProperNumbers(String input) {
+        assertThatThrownBy(() -> Convertor.separate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_TITLE + NON_NUMERIC);
     }
 }
--- src/test/java/lotto/WinnerTest.java
+++ src/test/java/lotto/WinnerTest.java
@@ -3,62 +3,33 @@ package lotto;
 import static lotto.Constants.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import lotto.machine.Winner;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class WinnerTest {
-    @DisplayName("당첨 번호 입력 시 쉼표를 사용하지 않으면 예외가 발생한다.")
-    @ParameterizedTest
-    @CsvSource(value = {"1.2.3.4.5.6:45", "12345,6:45", "1&2&3&4&5&6:45", "1+2+3+4+5+6:45", "12345and6:45"},
-            delimiter = ':')
-    void winningNumbersWithoutComma(String winningNumbers, String bonusNumber) {
-        assertThatThrownBy(() -> new Winner(winningNumbers, bonusNumber))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_TITLE + NO_COMMAS_FOUND);
-    }
-
-    @DisplayName("당첨 번호 입력 시 숫자를 올바르게 쓰지 않으면 예외가 발생한다.")
-    @ParameterizedTest
-    @CsvSource(value = {"1,2,3,4,5, 6:45", "1,!!,3,4,5,6:45", "하나,2,3,4,5,6:45", "one,2,3,4,5,6:45"},
-            delimiter = ':')
-    void winningNumbersWithoutPropervalue(String winningNumbers, String bonusNumber) {
-        assertThatThrownBy(() -> new Winner(winningNumbers, bonusNumber))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_TITLE + NON_NUMERIC);
-    }
-
-    @DisplayName("보너스 번호 입력 시 숫자를 올바르게 쓰지 않으면 예외가 발생한다.")
-    @Test
-    void inValidBonusNumber() {
-        String winningNumbers = "1,2,3,4,5,6";
-        String bonusNumber = "seven";
-
-        assertThatThrownBy(() -> new Winner(winningNumbers, bonusNumber))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_TITLE + NON_NUMERIC);
-    }
+    private static final List<Integer> WINNING_NUMBERS = List.of(1, 2, 3, 4, 5, 6);
 
     @DisplayName("보너스 번호 입력 시 1에서 45 사이의 숫자를 입력하지 않으면 예외가 발생한다.")
-    @Test
-    void bonusNumberOutOfRange() {
-        String winningNumbers = "1,2,3,4,5,6";
-        String bonusNumber = "46";
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46, 899, 1234})
+    void bonusNumberOutOfRange(int number) {
+        Integer bonusNumber = number;
 
-        assertThatThrownBy(() -> new Winner(winningNumbers, bonusNumber))
+        assertThatThrownBy(() -> new Winner(WINNING_NUMBERS, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_TITLE + OUT_OF_RANGE);
     }
 
     @DisplayName("보너스 번호 입력 시 당첨 번호와 겹치면 예외가 발생한다.")
-    @Test
-    void bonusNumberIncludedInWinningNumbers() {
-        String winningNumbers = "1,2,3,4,5,6";
-        String bonusNumber = "6";
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void bonusNumberIncludedInWinningNumbers(int number) {
+        Integer bonusNumber = number;
 
-        assertThatThrownBy(() -> new Winner(winningNumbers, bonusNumber))
+        assertThatThrownBy(() -> new Winner(WINNING_NUMBERS, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_TITLE + DUPLICATE_NUMBERS);
     }
