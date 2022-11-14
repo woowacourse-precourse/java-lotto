298e94ae2625edc0487236efe81b8f69c0bab75f
viyott-lover <jackk0616@gmail.com>
2022-11-15 00:06:32 +0900

    test: 로또 금액에 대한 예외 처리

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -9,17 +9,24 @@ import static org.assertj.core.api.Assertions.*;
 class UserTest {
 
     @Test
-    @DisplayName("로또 구입 금액이 1000으로 나누어 떨어지지 않으면 예외 처리")
-    void numberOfLotto() {
-        String input = "20001";
-        User user = new User(input);
+    @DisplayName("로또 구입 금액이 숫자가 아니면 예외 처리")
+    void isDegitInput() {
+        String input = "1234j";
 
-        assertThatThrownBy(() -> user.validatePurchaseAmount(input))
+        assertThatThrownBy(() -> new User(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.ERROR_SIGN.getErrorMessage()
+                        + " " + ErrorMessages.IS_NOT_NUMBER.getErrorMessage());
+    }
+
+    @Test
+    @DisplayName("로또 구입 금액이 1000으로 나누어 떨어지지 않으면 예외 처리")
+    void divideInput() {
+        String input = "20001";
+
+        assertThatThrownBy(() -> new User(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessages.ERROR_SIGN.getErrorMessage()
                         + " " + ErrorMessages.DIVIDE_ERROR.getErrorMessage());
     }
-
-    // 2. 로또를 긁어서 당첨된다면 당첨된 등수를 저장
-    // 3. 구입 금액과 총 당첨금을 비교하여 수익률 정산
 }
