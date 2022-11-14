22fc542381deab8b59d586aa76bc055b9d129f7d
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-15 04:13:02 +0900

    feat: 발행하는 로또 개수 만큼 각각의 로또 번호 발행

--- src/main/java/lotto/domain/LottoService.java
+++ src/main/java/lotto/domain/LottoService.java
@@ -1,2 +1,37 @@
-package lotto.domain;public class LottoService {
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.Lotto;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LottoService {
+
+    public List<Lotto> generateLottoList(int numberOfLotto) {
+
+        List<Lotto> lottoList = new LinkedList<>();
+
+        for (int i = 0; i < numberOfLotto; i++) {
+            Lotto lotto = generateLotto();
+            lottoList.add(lotto);
+        }
+
+        return lottoList;
+    }
+
+    public Lotto generateLotto() {
+
+        List<Integer> lottoNumbers = generateLottoNumbers();
+        Lotto lotto = new Lotto(lottoNumbers);
+
+        return lotto;
+    }
+
+    public List<Integer> generateLottoNumbers() {
+
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+        return lottoNumbers;
+    }
 }
