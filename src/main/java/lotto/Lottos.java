bcbda3d1ae2579919344d65042e71c9feaec3d17
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-15 03:10:17 +0900

    feat : 로또 구입 금액만큼 로또를 생성한다.

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -3,14 +3,13 @@ package lotto;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LottoTicket {
+public class Lottos {
     private static final String LOTTO_TICKET_SIZE_IS_NOT_LOTTO_AMOUNT = "[ERROR] 랜덤으로 생성된 로또 티켓의 로또 개수가 구입한 로또 개수와 다릅니다.";
 
     private final List<Lotto> lottos;
 
-    public LottoTicket(List<Lotto> lottos, int lottoAmount) {
-        validateLottoTicketSize(lottos, lottoAmount);
-        this.lottos = generateLottoTicket(lottoAmount);
+    public Lottos(int lottoAmount) {
+        this.lottos = generateLottos(lottoAmount);
     }
 
     private void validateLottoTicketSize(List<Lotto> lottos, int lottoAmount) {
@@ -19,11 +18,15 @@ public class LottoTicket {
         }
     }
 
-    private List<Lotto> generateLottoTicket(int lottoAmount) {
+    private List<Lotto> generateLottos(int lottoAmount) {
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < lottoAmount; i++) {
             lottos.add(Lotto.generateRandomLotto());
         }
+        return lottos;
+    }
+
+    public List<Lotto> getLottos() {
         return lottos;
     }
 }
