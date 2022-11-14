f8242ba7ef77efd14b0322e5a2d6d19347c46e94
Frog-Slayer <Frog-Slayer@github.com>
2022-11-14 23:03:41 +0900

    feat(Game): 당첨번호와 일치하는 번호의 수와 보너스 번호를 가지고 있는 여부를 통해 당첨 등수를 반환하는 기능

--- src/main/java/lotto/model/Game.java
+++ src/main/java/lotto/model/Game.java
@@ -3,13 +3,23 @@ package lotto.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Game {
     public static final Integer price = 1000;
     public static final Integer maximumLottoNumber = 45;
     public static final Integer numberToDraw = 6;
 
+    public enum Rank{
+        FIRST,
+        SECOND,
+        THIRD,
+        FOURTH,
+        FIFTH,
+        NONE
+    }
 
     private Lotto winningLotto;
     private Integer bonus = 0;
@@ -36,9 +46,6 @@ public class Game {
         this.bonus = bonus;
     }
 
-
-
-
     private void validateBonusByRange(Integer bonus){
         if (bonus > maximumLottoNumber || bonus < 1) throw new IllegalArgumentException();
     }
@@ -47,7 +54,14 @@ public class Game {
         if (winningLotto.contains(bonus)) throw new IllegalArgumentException();
     }
 
-
+    private Rank getLottoRank(Integer matchedNumberCount, Boolean hasBonusNumber){
+        if (matchedNumberCount == 6) return Rank.FIRST;
+        if (matchedNumberCount == 5 && hasBonusNumber) return Rank.SECOND;
+        if (matchedNumberCount == 5) return Rank.THIRD;
+        if (matchedNumberCount == 4) return Rank.FOURTH;
+        if (matchedNumberCount == 3) return Rank.FIFTH;
+        return Rank.NONE;
+    }
 
 
 }
