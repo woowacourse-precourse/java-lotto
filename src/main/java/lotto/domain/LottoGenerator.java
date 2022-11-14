bbc35613c4fd8d60a15cdca7c0f4ae61ee8d5abe
wormjoo <wormjoo@naver.com>
2022-11-15 03:48:51 +0900

    fix: 로또 번호 stream()을 적용하여 정렬되도록 수정

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -4,11 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoGenerator {
     public List<Integer> createLottoNumbers() {
         List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        Collections.sort(randomNumbers);
-        return randomNumbers;
+        return randomNumbers.stream().sorted().collect(Collectors.toList());
     }
 }
