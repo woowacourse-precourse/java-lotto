962ef0d915405b43f12c4a6b78903097a10c28ab
jjmin9797 <jjmin9797@naver.com>
2022-11-14 15:22:39 +0900

    feat(GameProgressView.java): 로또 구매시 메시지 출력 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

---
+++ src/main/java/lotto/view/GameProgressView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class GameProgressView {
+    public static final String PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static void showPurchaseMessage() {
+        System.out.println(PURCHASE_MESSAGE);
+    }
+}
