e783dcba409270c4cefba7d78caffabdcffa4067
Yujin830 <rladbwls30@naver.com>
2022-11-14 23:22:27 +0900

    Feat: Random클래스와 main클래스 연결

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,7 +6,9 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         int buylotto = askmoney();
+        List<List<Integer>> lottonumbers = new ArrayList<>();
         Random random = new Random();
+        lottonumbers = random.createAllLotto(buylotto);
         // TODO: 프로그램 구현
     }
     public static int askmoney(){
