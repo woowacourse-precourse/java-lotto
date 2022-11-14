dd251cb84046588be3cf2e4502c35084b92d4484
ryanha <ryanha@handong.ac.kr>
2022-11-15 00:49:33 +0900

    test(domain): 당첨여부확인 테스트 케이스 분할 및 수정, 추가

--- src/test/java/lotto/domain/WinningCheckerTest.java
+++ src/test/java/lotto/domain/WinningCheckerTest.java
@@ -8,29 +8,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WinningCheckerTest {
+
     @Test
-    @DisplayName("당첨 여부를 정확히 판단하는 지 테스트")
-    void check_test() {
+    @DisplayName("1등 당첨의 경우 정확하게 이를 판정하는 가?")
+    void check_winning_one_test() {
         List<Integer> winningNumber = new ArrayList<>(List.of(1, 2, 3, 4 ,5, 6));
         int bonusNumber = 10;
         List<Integer> winningOne = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
-        List<Integer> winningTwo = new ArrayList<>(List.of(1, 2, 3, 4, 5, 10));
-        List<Integer> winningThree = new ArrayList<>(List.of(1, 2, 3, 4, 5, 13));
-        List<Integer> winningFour = new ArrayList<>(List.of(1, 2, 3, 4, 7, 13));
-        List<Integer> winningFive = new ArrayList<>(List.of(1, 2, 3, 7, 9, 13));
-        List<Integer> winningNone = new ArrayList<>(List.of(1, 2, 7, 8, 9, 10));
 
         assertThat(WinningChecker.check(winningNumber, bonusNumber, winningOne))
-                .isEqualTo(1);
+                .isEqualTo("WINNING_ONE");
+    }
+
+    @Test
+    @DisplayName("2등 당첨의 경우 정확하게 이를 판정하는 가?")
+    void check_winning_two_test() {
+        List<Integer> winningNumber = new ArrayList<>(List.of(1, 2, 3, 4 ,5, 6));
+        int bonusNumber = 10;
+        List<Integer> winningTwo = new ArrayList<>(List.of(1, 2, 3, 4, 5, 10));
+
         assertThat(WinningChecker.check(winningNumber, bonusNumber, winningTwo))
-                .isEqualTo(2);
+                .isEqualTo("WINNING_TWO");
+    }
+
+    @Test
+    @DisplayName("3등 당첨의 경우 정확하게 이를 판정하는 가?")
+    void check_winning_three_test() {
+        List<Integer> winningNumber = new ArrayList<>(List.of(1, 2, 3, 4 ,5, 6));
+        int bonusNumber = 10;
+        List<Integer> winningThree = new ArrayList<>(List.of(1, 2, 3, 4, 5, 13));
+
         assertThat(WinningChecker.check(winningNumber, bonusNumber, winningThree))
-                .isEqualTo(3);
+                .isEqualTo("WINNING_THREE");
+    }
+
+    @Test
+    @DisplayName("4등 당첨의 경우 정확하게 이를 판정하는 가?")
+    void check_winning_four_test() {
+        List<Integer> winningNumber = new ArrayList<>(List.of(1, 2, 3, 4 ,5, 6));
+        int bonusNumber = 10;
+        List<Integer> winningFour = new ArrayList<>(List.of(1, 2, 3, 4, 7, 13));
+
         assertThat(WinningChecker.check(winningNumber, bonusNumber, winningFour))
-                .isEqualTo(4);
+                .isEqualTo("WINNING_FOUR");
+    }
+
+    @Test
+    @DisplayName("5등 당첨의 경우 정확하게 이를 판정하는 가?")
+    void check_winning_five_test() {
+        List<Integer> winningNumber = new ArrayList<>(List.of(1, 2, 3, 4 ,5, 6));
+        int bonusNumber = 10;
+        List<Integer> winningFive = new ArrayList<>(List.of(1, 2, 3, 7, 9, 13));
+
         assertThat(WinningChecker.check(winningNumber, bonusNumber, winningFive))
-                .isEqualTo(5);
+                .isEqualTo("WINNING_FIVE");
+    }
+
+    @Test
+    @DisplayName("당첨되지 않은 경우 정확하게 이를 판정하는 가?")
+    void check_winning_none_test() {
+        List<Integer> winningNumber = new ArrayList<>(List.of(1, 2, 3, 4 ,5, 6));
+        int bonusNumber = 10;
+        List<Integer> winningNone = new ArrayList<>(List.of(1, 2, 7, 8, 9, 10));
+
         assertThat(WinningChecker.check(winningNumber, bonusNumber, winningNone))
-                .isEqualTo(0);
+                .isEqualTo("null");
     }
 }
