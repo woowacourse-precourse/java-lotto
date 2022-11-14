771f1db21664a86b9bd61e0ea7e96ad61649b88f
ShinKwang <sklee0206@naver.com>
2022-11-14 17:29:09 +0900

    refactor: Add validation test for validateByLotteryPrice
    validateByLotteryPrice 메소드 검증을 통과하는 메소드, 통과하지 못하고 IllegalArgumentException이 발생하는 테스트를 작성한다.

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -23,7 +23,7 @@ class MoneyTest {
 
     @Test
     void 콤마를_넣으면_에러발생() {
-        //때로는 1,000 이라고 입력할 수 있다.
+        //때로는 1,000 이라고 입력할 수도 있다.
         //given
         String moneyWithComma = "2,000";
 
@@ -32,5 +32,25 @@ class MoneyTest {
                 .isInstanceOf(IllegalArgumentException.class).hasMessage("[ERROR] 구입금액은 숫자로만 입력해야 합니다.");
     }
 
+    @Test
+    void 로또구입금액_단위로_입력하지않으면_에러발생() {
+        //given
+        String inputMoney = "12321";
 
+        //when, then
+        assertThatThrownBy(() -> new Money(inputMoney))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("[ERROR] 구입금액은 1000원 단위여야만 합니다.");
+    }
+
+    @Test
+    void 로또구입금액_단위로_입력() {
+        //given
+        String inputMoney = "123000";
+
+        //when
+        Money money = new Money(inputMoney);
+
+        //then
+        assertThat(money.getMoney()).isEqualTo(123000);
+    }
 }
