7b7d7703d0325433d0b21fc0b8401a5bbacf76e5
whibaek <jibaek0513@gmail.com>
2022-11-14 23:34:40 +0900

    refactor(Application): 프로그램 실행 위해 게임시작

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,37 +1,12 @@
 package lotto;
 
-import lotto.View.Screen;
-import lotto.domain.*;
+import lotto.View.Game;
 
 public class Application {
     public static void main(String[] args) {
-
         try {
             // TODO: 프로그램 구현
-            Screen screen = new Screen();
-
-            // User Part
-            User user = new User(screen.readMoney());
-            user.makeLottos();
-
-            // Computer Part
-            LottoAnswer lottoAnswer = new LottoAnswer(screen.readLotto(), screen.readBonus());
-
-            screen.printLottoCount(user.getNumOfLotto());
-            screen.printLottos(user.getLottos());
-
-            // Calculate Part
-            Result result = new Result(user.getUserMoney());
-            Calculator calculator = new Calculator();
-            for (Lotto lotto : user.getLottos()) {
-                calculator.clearCount();
-                calculator.checkCount(lotto, lottoAnswer);
-                result.checkRanks(calculator);
-            }
-            result.calcAllResult();
-
-            // Result Part ( print )
-            screen.printResult(result);
+            new Game().start();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
