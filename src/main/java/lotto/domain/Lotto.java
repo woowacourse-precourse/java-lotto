f74095ed6507c3178170efd114aaa8432f8e84ac
Daeeun Ko <7273322@naver.com>
2022-11-15 00:21:09 +0900

    feat: validate if lotto numbers is duplicate

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -13,10 +13,21 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (!isInBound(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 개수가 잘못되었습니다.");
+        }
+
+        if (isDuplicate(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 중복되었습니다.");
         }
     }
 
     // TODO: 추가 기능 구현
+    private boolean isInBound(List<Integer> numbers) {
+        return numbers.size() == Config.LOTTO_NUMBER_COUNT;
+    }
+
+    private boolean isDuplicate(List<Integer> numbers) {
+        return numbers.stream().distinct().count() != Config.LOTTO_NUMBER_COUNT;
+    }
 }
