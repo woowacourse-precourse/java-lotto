44a1333eb795309252ea4fd3e9c0f44b9a404880
YSEO <1park4170@gmail.com>
2022-11-14 17:03:15 +0900

    refactor<compute lottery result, convert it into Map > : add constructor, add exception for duplicated bonus with winning numbers
    it is better to get winningnumber and bonus when constructing instacne, so make constructor and add features to it, meanwhile, check if bonus and winningNumbers are same
    
    before : get winningNumbers and bonus not from constructor, no exception for duplication
    after : get winningNumbers and bonus from constructor, add exception for duplication

--- src/main/java/lotto/domain/PrizeListGenerator.java
+++ src/main/java/lotto/domain/PrizeListGenerator.java
@@ -9,38 +9,53 @@ import java.util.List;
 import java.util.Map;
 
 public class PrizeListGenerator {
+    private static final String BONUS_DUPLICATED_WITH_WINNING_NUMBER = "[ERROR] 보너스 번호와 당첨 번호는 중복되서는 안됩니다.";
+
     private Map<Enum, Integer> winnerResult = new HashMap<>();
     private Map<Integer, Integer> winningNumbers;
     private int bonus;
 
-    public void getWinningNumbers() {
-        winningNumbers = WinnerNumberGetter.getLotteryWinningNumbersInput();
-        bonus = WinnerNumberGetter.getBonusInput();
+    private int score;
+
+    public PrizeListGenerator() throws IllegalArgumentException{  /* 생성자로 이 기능 옮긴 것 커밋에 반영해야함 */
+            winningNumbers = WinnerNumberGetter.getLotteryWinningNumbersInput();
+            bonus = WinnerNumberGetter.getBonusInput();
+
+            if (winningNumbers.containsKey(bonus)) {
+                throw new IllegalArgumentException(BONUS_DUPLICATED_WITH_WINNING_NUMBER);
+            }
     }
 
     public void iterateLotteriesForStatistic(List<Lotto> lotteries) {
-        int score = 0;
-
+        score = 0;
         for (Lotto lottery : lotteries) { // 당첨 계산하는 제일 핵심로직  시간복잡도 개선방법 생각해보기
             List<Integer> numbers = lottery.getNumbers();
-
-            for (int eachNum : numbers) {
-                if (winningNumbers.containsKey(eachNum)) {
-                    score += 2;
-                }
-            }
+            checkIfLotteryContainsWinningnumber(numbers);
 
             if (score == 10 && numbers.contains(bonus)) {
                 score += 1;
             }
-
-            Enum<PrizeStandards> prizeStandardsEnum = getCorrespondingPrizeStandard(score);
-            winnerResult.computeIfPresent(prizeStandardsEnum, (k, v) -> v + 1);
-            winnerResult.computeIfAbsent(prizeStandardsEnum, v -> 1);
-
+            makeNoteToWinnerResult();
             score = 0;
         }
+        noteNotRecordedPrizeStandardsToWinnerResult();
+    }
 
+    private void checkIfLotteryContainsWinningnumber(List<Integer> numbers) {
+        for (int eachNum : numbers) {
+            if (winningNumbers.containsKey(eachNum)) {
+                score += 2;
+            }
+        }
+    }
+
+    private void makeNoteToWinnerResult() {
+        Enum<PrizeStandards> prizeStandardsEnum = getCorrespondingPrizeStandard(score);
+        winnerResult.computeIfPresent(prizeStandardsEnum, (k, v) -> v + 1);
+        winnerResult.computeIfAbsent(prizeStandardsEnum, v -> 1);
+    }
+
+    private void noteNotRecordedPrizeStandardsToWinnerResult() {
         for (PrizeStandards i : PrizeStandards.values()) {
             if (!winnerResult.containsKey(i)) {
                 winnerResult.put(i, 0);
@@ -48,7 +63,7 @@ public class PrizeListGenerator {
         }
     }
 
-    public Enum<PrizeStandards> getCorrespondingPrizeStandard(int score) {
+    private Enum<PrizeStandards> getCorrespondingPrizeStandard(int score) { // private으로 변경 커밋필요
         return PrizeStandards.getProperStandard(score);
     }
 
