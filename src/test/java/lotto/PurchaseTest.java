7a544cd4f9d073c62afca85d93ff40cafd260557
Yeongseon Park <yseon406@gmail.com>
2022-11-15 05:01:26 +0900

    test: 사용자 입력 금액 예외처리 테스트

--- src/test/java/lotto/PurchaseTest.java
+++ src/test/java/lotto/PurchaseTest.java
@@ -1,16 +1,29 @@
 package lotto;
 
 import lotto.domain.Purchase;
+import lotto.ui.Message;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PurchaseTest {
 
+    @DisplayName("로또 구입 금액이 정상적으로 입력된 경우")
     @Test
-    void isValidTest() {
+    void validMoney() {
         int money = 14000;
         assertThat(new Purchase().isValid(money)).isTrue();
+    }
+
+    @DisplayName("로또 구입 금액이 1,000원 단위가 아닌 경우 예외 발생")
+    @Test
+    void invalidMoney() {
+        assertThatThrownBy(() -> new Purchase().isValid(1500))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Message.MONEY_ERROR);
     }
 
     @Test
