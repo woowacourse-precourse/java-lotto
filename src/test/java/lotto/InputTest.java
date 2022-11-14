43d962e37f7b17c8c569ea37b20daa89fb31bb72
dooboocookie <bkh3030@gmail.com>
2022-11-14 17:26:20 +0900

    test: int와 long 입력 받을 때 값과 예외에 대한 테스트 작성

---
+++ src/test/java/lotto/InputTest.java
@@ -0,0 +1,58 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputTest {
+
+    @DisplayName("readInteger() 값 테스트")
+    @Test
+    void readIntegerTest() {
+        InputStream in = new ByteArrayInputStream("1000".getBytes());
+        System.setIn(in);
+
+        assertThat(Input.readInteger()).isEqualTo(1000).isInstanceOf(Integer.class);
+    }
+
+    @DisplayName("readLong() 값 테스트")
+    @Test
+    void readLongTest() {
+        InputStream in = new ByteArrayInputStream("1000".getBytes());
+        System.setIn(in);
+
+        assertThat(Input.readLong()).isEqualTo(1000).isInstanceOf(Long.class);
+    }
+
+    @DisplayName("숫자가 아닌 값을 입력 시 readInteger() 예외 발생 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"aaaa", "1000a", "a1000", "10a00"})
+    void readIntegerExceptionTest(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(() -> Input.readInteger())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @DisplayName("숫자가 아닌 값을 입력 시 readLong() 예외 발생 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"aaaa", "1000a", "a1000", "10a00"})
+    void readLongExceptionTest(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(() -> Input.readLong())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
