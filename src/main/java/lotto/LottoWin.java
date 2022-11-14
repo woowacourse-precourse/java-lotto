d828a2e5fc09933d3e3b714b295bea99739134b2
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-11-15 07:25:46 +0900

    feat: 각 로또당 상금 hashmap 생성기능 추가

--- src/main/java/lotto/LottoWin.java
+++ src/main/java/lotto/LottoWin.java
@@ -1,14 +1,41 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoWin {
-    public static Integer getTotalPrize(List<Lotto> lottos, Lotto luckyNums, int bonusNum) {
-        int prizeMoney = 0;
+    public static Map<Integer, Integer> getPrizeMap(List<Lotto> lottos, Lotto luckyNums, int bonusNum) {
+        Map<Integer, Integer> prizeMap = new HashMap<>();
+        int tempMoney = 0;
 
         for (Lotto lotto : lottos) {
-            prizeMoney += getPrizeMoney(lotto, luckyNums, bonusNum);
+            tempMoney = getPrizeMoney(lotto, luckyNums, bonusNum);
+            prizeMap.put(tempMoney, prizeMap.getOrDefault(tempMoney, 0) + 1);
         }
-        return prizeMoney;
+
+        return prizeMap;
+    }
+
+    public static Integer getPrizeMoney(Lotto target, Lotto luckyNums, int bonusNum) {
+        int winningNum = target.getWinningNum(luckyNums);
+        boolean bonus = target.hasBonus(bonusNum);
+
+        if (winningNum == 3) {
+            return Prize.FIFTH.getPrize();
+        }
+        if (winningNum == 4) {
+            return Prize.FOURTH.getPrize();
+        }
+        if (winningNum == 5 && !bonus) {
+            return Prize.THIRD.getPrize();
+        }
+        if (winningNum == 5 && bonus) {
+            return Prize.SECOND.getPrize();
+        }
+        if (winningNum == 6) {
+            return Prize.FIRST.getPrize();
+        }
+        return Prize.NOTHING.getPrize();
     }
 }
