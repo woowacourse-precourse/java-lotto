8d8d89274fcdc8592fc56aeedfdba93eb44f2a05
MinSungKim <timkim01@naver.com>
2022-11-14 14:38:12 +0900

    feat: 로또 번호 생성 및 오름차순 정렬 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,10 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         try {
@@ -7,5 +12,19 @@ public class Application {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    // 로또번호 자동생성 후 List 타입으로 반환
+    public List<Integer> createLottoNumbers(){
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+
+        return lottoNumbers;
+    }
+
+    // 로또 번호 List 오름차순으로 정렬
+    public List<Integer> sortLottoNumbers(List<Integer> lottoNumbers){
+        Collections.sort(lottoNumbers);
+
+        return lottoNumbers;
     }
 }
