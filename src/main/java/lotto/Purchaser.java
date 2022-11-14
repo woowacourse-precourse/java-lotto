f19f074a51b66ec6daa8819d28cf41ce42f344f8
youngin <bluest12@naver.com>
2022-11-14 17:01:38 +0900

    feat: Create new method
    구매금액을 구매 횟수로 환산하는 메서드 생성

--- src/main/java/lotto/Purchaser.java
+++ src/main/java/lotto/Purchaser.java
@@ -18,6 +18,18 @@ public class Purchaser {
         return purchaseSum;
     }
 
+    public int dividePurchaseSum() {
+        purchaseSum = changeInt();
+        if (purchaseCount / 1000 >= 1)
+            purchaseCount = purchaseSum / 1000;
+        else if (!(purchaseCount / 1000 >= 1))
+            System.out.println("[ERROR] 구매 금액을 최소 1000원이상 입력해주세요.");
+        else if (!(purchaseCount % 1000 == 0)) {
+            System.out.println("[ERROR] 구매금액을 1000원 단위로 입력해주세요.");
+        }
+        return purchaseCount;
+    }
+
     public List<Integer> inputLottoNum() {
 
     }
