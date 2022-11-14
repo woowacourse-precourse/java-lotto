646f531b9af7748d7faafdf757dd3f72a6652d65
yejin <kwonyj1022@naver.com>
2022-11-14 18:15:12 +0900

    enum Win 클래스 구현 완료

---
+++ src/main/java/lotto/Win.java
@@ -0,0 +1,25 @@
+package lotto;
+
+public enum Win {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000);
+
+    private int sameNumberCnt;
+    private int prize;
+
+    Win(int sameNumberCnt, int prize) {
+        this.sameNumberCnt = sameNumberCnt;
+        this.prize = prize;
+    }
+
+    public int sameNumberCnt() {
+        return sameNumberCnt;
+    }
+
+    public int prize() {
+        return prize;
+    }
+}
