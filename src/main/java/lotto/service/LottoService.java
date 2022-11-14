61f3b7cccc5bef83a85e178139fb7ae29ff7f666
yujaemin <woawoa0513@gmail.com>
2022-11-14 23:56:20 +0900

    feat: 로또 생성 Service 생성
    로또 생성과 관련된 비즈니스 로직 구현한 서비스 생성

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,28 @@
+package lotto.service;
+
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+import static lotto.utils.Constants.*;
+import static lotto.utils.Converter.convertToCountsOfLotto;
+
+public class LottoService {
+    public static List<Lotto> generateLottoByPurchaseAmount(int purchaseAmount) {
+        int lottoCount = convertToCountsOfLotto(purchaseAmount);
+
+        ArrayList<Lotto> lottos = new ArrayList<>();
+
+        for (int count = 0; count < lottoCount; count++) {
+            lottos.add(new Lotto(generateLottoNumbers()));
+        }
+
+        return lottos;
+    }
+
+    private static List<Integer> generateLottoNumbers() {
+        return pickUniqueNumbersInRange(LOTTO_MIN, LOTTO_MAX, COUNT_OF_NUMBERS_IN_LOTTO);
+    }
+}
