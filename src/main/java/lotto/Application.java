7230d9efc0f222dc5327305b9c0c04977c080888
c4fiber <qudcjf153@gmail.com>
2022-11-14 21:44:32 +0900

    feat(Application): 메인 메소드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoStore lottoStore = new LottoStore();
+        Wallet wallet = new Wallet();
+
+        // 구입금액 입력
+        wallet.setMoney(Console.readLine());
+
+        // 로또판매점에서 내 지갑으로 로또를 판매
+        wallet.buyLottos( lottoStore.sellLottos(wallet.getMoney()) );
+        wallet.showMyLottos();
+
+        lottoStore.setWinNumbers(Console.readLine());
+        lottoStore.setBonusNumber(Console.readLine());
+
+        for (Lotto lotto : wallet.getMyLottos()) {
+            wallet.addReward(lottoStore.calcLottoReward(lotto));
+        }
+
+        wallet.showStatistics();
     }
 }
