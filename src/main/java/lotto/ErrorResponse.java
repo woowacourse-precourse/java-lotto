b6efc7e9c8a2bd23ee5db991833221258bb2ac3b
KangSungBum <peter9706@naver.com>
2022-11-14 16:32:17 +0900

    feat(ErrorResponse.java): 새로운 enum value 추가
    Error value: INPUT_BOUNS_NUMBER_EXISTS 추가
    - 사용자가 입력한 보너스 번호가 이전 입력한 당첨 번호에 존재할 시 발생하는 예외

--- src/main/java/lotto/ErrorResponse.java
+++ src/main/java/lotto/ErrorResponse.java
@@ -7,6 +7,7 @@ public enum ErrorResponse {
     INPUT_BUYING_RANGE_ERROR("[ERROR] 1000으로 나누어지는 단위의 구입 금액을 입력해야 합니다."),
     INPUT_WITHOUT_COMMA_ERROR("[ERROR] 당첨번호는 쉽표(,)로 구분하여 입력해야 합니다."),
     INPUT_CONTAINS_CHAR_ERROR("[ERROR] 입력은 문자를 포함할 수 없습니다."),
+    INPUT_BONUS_NUMBER_EXISTS("[ERROR] 입력한 보너스 번호가 이미 당첨 번호에 포함되어 있습니다.")
     ;
 
     private String errorMessage;
