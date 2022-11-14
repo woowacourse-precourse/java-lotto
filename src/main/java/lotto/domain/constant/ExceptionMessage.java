5db0e0f716bba380aa8e82dcb1e06cd1bcb8b4cb
yubin <dbqls2821@gmail.com>
2022-11-15 02:32:16 +0900

    feat(ExceptionMessage): ExceptionMessage에 입력값이 null인경우 출력할 메시지 추가

--- src/main/java/lotto/domain/constant/ExceptionMessage.java
+++ src/main/java/lotto/domain/constant/ExceptionMessage.java
@@ -1,6 +1,7 @@
 package lotto.domain.constant;
 
 public enum ExceptionMessage {
+  INPUT_IS_NULL("[ERROR] 입력값이 null입니다."),
   INPUT_IS_NOT_SEPARATED_BY_COMMAS("[ERROR] 입력값은 단일 콤마로 구분되지 않았습니다."),
   INPUT_IS_NOT_SIX_NUMBERS("[ERROR] 입력값의 숫자는 6개가 아닙니다."),
   INPUT_IS_NOT_NUMBER("[ERROR] 입력값은 숫자가 아닙니다."),
