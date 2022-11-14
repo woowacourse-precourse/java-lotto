9dd80e5735191fe222aa2a2a985f22498413b665
Ran <lan4250@naver.com>
2022-11-15 01:21:32 +0900

    FEAT/ List에 대해 로또 번호가 될 수 있는지 유효성 검사

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,11 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -10,11 +13,24 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+
+    private void validate(List<Integer> numbers) {      // 리스트 사이즈가 6이 아니면 예외 처리한다.
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호는 6개로 구성되어야 합니다.");
         }
+
+        // 중복인 경우, 예외 처리한다.
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int n : numbers) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+
+            if (map.get(n) > 1) {
+                throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+
+            }
+        }
+
     }
 
-    // TODO: 추가 기능 구현
 }
