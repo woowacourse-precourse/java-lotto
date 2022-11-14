d1f50352148718444a84f07e475b09d4a5971fdd
skydreamer21 <skydreamer210@gmail.com>
2022-11-15 03:38:35 +0900

    feat(LotteryDrawService): 판매된 티켓의 당첨 결과 업데이트 구현
    [세부사항]
    - 구현 메서드
    1. updateResult : 로또 티켓의 순위 정보를 저장
    2. rank : 로또 티켓의 순위를 반환
    3. countNumberOfMatches
    4. hasBonusNumber

--- src/main/java/lotto/service/LotteryDrawService.java
+++ src/main/java/lotto/service/LotteryDrawService.java
@@ -1,11 +1,41 @@
 package lotto.service;
 
+import lotto.constant.LotteryRank;
+import lotto.domain.LotteryTicket;
 import lotto.domain.WinningNumber;
+import lotto.repository.LotteryTicketRepository;
 
 import java.util.List;
 
 public class LotteryDrawService {
     public WinningNumber draw (List<Integer> numbers, int bonusNumber) {
         return new WinningNumber(numbers, bonusNumber);
+    }
+
+    public void updateResult (WinningNumber winningNumber) {
+        LotteryTicketRepository repository = LotteryTicketRepository.getInstance();
+        List<String> lotteryTicketIds = repository.findAllTicketId();
+
+        for (String id : lotteryTicketIds) {
+            LotteryTicket ticket = repository.findTicketById(id).get();
+            LotteryRank rank = rank(ticket, winningNumber);
+            repository.updateResultById(id, rank);
+        }
+    }
+
+    public LotteryRank rank (LotteryTicket lotteryTicket, WinningNumber winningNumber) {
+        List<Integer> ticketNumbers = lotteryTicket.getNumbers();
+        List<Integer> winningNumbers = winningNumber.getNumbers();
+        int numberOfMatches = countNumberOfMatches(ticketNumbers, winningNumbers);
+        boolean hasBonusNumber = hasBonusNumber(ticketNumbers, winningNumber.getBonusNumber());
+        return LotteryRank.findRank(numberOfMatches, hasBonusNumber);
+    }
+
+    private int countNumberOfMatches (List<Integer> ticketNumbers, List<Integer> winningNumbers) {
+        return (int) ticketNumbers.stream().filter(winningNumbers::contains).count();
+    }
+
+    private boolean hasBonusNumber (List<Integer> ticketNumbers, int bonusNumber) {
+        return ticketNumbers.contains(bonusNumber);
     }
 }
