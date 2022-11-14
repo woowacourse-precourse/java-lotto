db7d3e175a57ba8dec1d8ddaa9be0bf7a34d452e
kdkdhoho <hkim4410@naver.com>
2022-11-14 14:53:09 +0900

    refactor(Manager): ManagerService 추가로 인한 함수 위치 이동

--- src/main/java/lotto/domain/Manager.java
+++ src/main/java/lotto/domain/Manager.java
@@ -3,15 +3,12 @@ package lotto.domain;
 import lotto.domain.enums.Number;
 import lotto.util.ExceptionHandler;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Manager {
 
     private List<Integer> winningNumbers = new ArrayList<>();
     private int bonusNumber;
-
-    LottoMachine lottoMachine = new LottoMachine();
 
     public void setWinningNumbers(String input) {
         String[] numbers = input.split(ExceptionHandler.COMMA);
@@ -29,67 +26,26 @@ public class Manager {
         this.bonusNumber = bonusNumber;
     }
 
-    public List<Lotto> changeLottos(double money) {
-        List<Lotto> lottos = new ArrayList<>();
-        int totalCount = (int)money / 1000;
-
-        for (int repeat = 1; repeat <= totalCount; repeat++) {
-            Lotto lotto = lottoMachine.generateLotto();
-            lottos.add(lotto);
-        }
-
-        return lottos;
-    }
-
-    public void judgeResult(User user) {
-        List<Lotto> userLottos = user.getLottos();
-        for (int i = 0; i < userLottos.size(); i++) {
-            int correctCount = getCorrectCount(userLottos.get(i));
-            boolean isCorrectBonus = isCorrectBonus(userLottos.get(i));
-            int rank = judgeRank(correctCount, isCorrectBonus);
-            user.updateRankAndWinningAmount(rank);
-        }
-    }
-
-    public int getCorrectCount(Lotto lotto) {
-        List<Integer> lottoNumbers = lotto.getNumbers();
+    public int countCorrect(Lotto userLotto) {
+        List<Integer> lottoNumbers = userLotto.getNumbers();
 
         int correctCount = 0;
         for (int i = 0; i < lottoNumbers.size(); i++) {
-            Integer lottoNumber = lottoNumbers.get(i);
-            if (isCorrectNumber(lottoNumber)) {
+            Integer userLottoNumber = lottoNumbers.get(i);
+            if (isCorrectNumber(userLottoNumber)) {
                 correctCount++;
             }
         }
         return correctCount;
     }
 
-    private boolean isCorrectNumber(Integer lottoNumber) {
-        return winningNumbers.contains(lottoNumber);
+    private boolean isCorrectNumber(Integer userLottoNumber) {
+        return winningNumbers.contains(userLottoNumber);
     }
 
-    private boolean isCorrectBonus(Lotto userLotto) {
+    public boolean isCorrectBonus(Lotto userLotto) {
         List<Integer> numbers = userLotto.getNumbers();
         return numbers.contains(bonusNumber);
-    }
-
-    public int judgeRank(int correctCount, boolean isCorrectBonus) {
-        if (correctCount == Number.THREE.getValue()) {
-            return Number.FIVE.getValue();
-        }
-        if (correctCount == Number.FOUR.getValue()) {
-            return Number.FOUR.getValue();
-        }
-        if (!isCorrectBonus && correctCount == Number.FIVE.getValue()) {
-            return Number.THREE.getValue();
-        }
-        if (isCorrectBonus && correctCount == Number.FIVE.getValue()) {
-            return Number.TWO.getValue();
-        }
-        if (correctCount == Number.SIX.getValue()) {
-            return Number.ONE.getValue();
-        }
-        return Number.ZERO.getValue();
     }
 
     public int getWinningAmount(int rank) {
