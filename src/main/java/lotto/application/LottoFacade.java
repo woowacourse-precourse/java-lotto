ef7f7138b66fe22d4a9324671279973af36be4d5
5witchkr <hob4419@gmail.com>
2022-11-14 23:49:55 +0900

    feat(LottoFacade): buyLotto 기능과 테스트

--- src/main/java/lotto/application/LottoFacade.java
+++ src/main/java/lotto/application/LottoFacade.java
@@ -1,11 +1,17 @@
 package lotto.application;
 
-import lotto.domain.LottoProcessor;
-import lotto.domain.LottoProcessorImpl;
+import lotto.domain.*;
+import java.util.List;
 
-public class Facade {
+
+public class LottoFacade {
 
     private final LottoProcessor lottoProcessor = new LottoProcessorImpl();
+    private final MoneyProcessor moneyProcessor = new MoneyProcessorImpl();
 
 
+    public List<Lotto> buyLotto(Integer money) {
+        Integer count = moneyProcessor.calculateLottoCount(money);
+        return lottoProcessor.createLottoByCount(count);
+    }
 }
---
+++ src/test/java/lotto/LottoFacadeTest.java
@@ -0,0 +1,49 @@
+package lotto;
+
+import lotto.application.LottoFacade;
+import lotto.domain.Lotto;
+import lotto.domain.LottoProcessorImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoFacadeTest {
+
+    private LottoFacade lottoFacade;
+
+    @TestFactory
+    @DisplayName("LottoFacade buyLotto Test")
+    Stream<DynamicTest> lottoFacadeBuyLottoTest() {
+        lottoFacade = new LottoFacade();
+
+        return Stream.of(
+                DynamicTest.dynamicTest("5000원 금액의 경우 5장을 구매한다.", () -> {
+                    final Integer money = 5000;
+
+                    List<Lotto> result = lottoFacade.buyLotto(money);
+
+                    assertThat(result.size()).isEqualTo(5);
+                }),
+                DynamicTest.dynamicTest("1000원 금액의 경우 1장을 구매한다.", () -> {
+                    final Integer money = 1000;
+
+                    List<Lotto> result = lottoFacade.buyLotto(money);
+
+                    assertThat(result.size()).isEqualTo(1);
+                }),
+                DynamicTest.dynamicTest("3500원 금액의 경우 예외를 던져준다.", () -> {
+                    final Integer money = 3500;
+
+                    assertThatThrownBy(() -> lottoFacade.buyLotto(money))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("1000원 단위의 금액만 가능합니다.");
+                })
+        );
+    }
+}
