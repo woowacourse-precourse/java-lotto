1c47608a320a25e8d37509c582ab5e726becef0e
lcw3176 <lcw3176@naver.com>
2022-11-14 20:23:03 +0900

    fix: 공백 대처 및 잘못된 숫자 검증 로직 수정

--- src/main/java/lotto/filter/NumberWithDotFilter.java
+++ src/main/java/lotto/filter/NumberWithDotFilter.java
@@ -1,5 +1,7 @@
 package lotto.filter;
 
+import lotto.util.DigitUtil;
+
 public class NumberWithDotFilter implements Filter {
 
     private static final String SPLITTER = ",";
@@ -10,11 +12,11 @@ public class NumberWithDotFilter implements Filter {
         String[] numbers = numberWithDot.split(SPLITTER);
 
         if (numbers.length != VALID_SIZE) {
-            throw new IllegalArgumentException("입력한 숫자의 형식이 잘못되었거나 갯수가 부족합니다");
+            throw new IllegalArgumentException("입력한 숫자의 형식이 잘못되었거나 갯수가 다릅니다");
         }
 
         for (String i : numbers) {
-            if (!Character.isDigit(i.charAt(0))) {
+            if (i.equals("") || !DigitUtil.isDigit(i)) {
                 throw new IllegalArgumentException("입력한 문자가 숫자가 아닙니다.");
             }
         }
