3e9e6aada1ca3f07934e76c7178ac6b6d7e3dca0
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-11-14 23:44:29 +0900

    test<LottoTest>: 당첨 금액 확인 관련 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +11,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 
 import static org.assertj.core.api.Assertions.*;
 
-class LottoTest {
+class LottoTest extends NsTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -50,5 +51,25 @@ class LottoTest {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5), 6))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 1~45 이외의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void checkWinning() {
+        List<Integer> number = List.of(1,2,3,4,5,6,7);
+        List<List<Integer>> numbers = List.of(List.of(1,2,3,8,9,10), List.of(1,2,3,4,8,9), List.of(1,2,3,4,5,8,9), List.of(1,2,3,4,7,8), List.of(1,2,3,4,5,6));
+        new WinningLotto(number, numbers);
+        assertThat(output()).contains(
+                "3개 일치 (5,000원) - 1개",
+                "4개 일치 (50,000원) - 1개",
+                "5개 일치 (1,500,000원) - 1개",
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                "6개 일치 (2,000,000,000원) - 1개"
+        );
+    }
+
+    @Override
+    protected void runMain() {
+
     }
 }
