7650c8543978a1b98843160cb613ac2f63a5d61d
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-11-14 16:15:02 +0900

    add : 사용자의 입력을 받아 출력하는 클래스

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import static lotto.util.Message.*;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.util.Validator;
+import java.util.List;
+
+public class InputView {
+    private static final Validator validator = new Validator();
+
+
+}
