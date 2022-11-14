3974801d26817e70db1819be35325660086dad36
이건회 <rg970604@naver.com>
2022-11-15 07:45:17 +0900

    feat(Money.java) : 금액 관련 원시값 포장

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import lotto.util.errorMessage.ErrorMessage;
+
+public class Money {
+
+    private static final int MIN_UNIT = 1000;
+
+    private final int money;
+
+    public Money(int money) {
+        validateMoney(money);
+        validateMinimumMoney(money);
+        this.money = money;
+    }
+
+    private void validateMinimumMoney(int money){
+        if (money<MIN_UNIT){
+            throw new IllegalArgumentException(ErrorMessage.MININUM_PAYMENT);
+        }
+    }
+
+    private void validateMoney(int money) {
+        if (cannotPay(money)) {
+            throw new IllegalArgumentException(ErrorMessage.PAYMENT_UNIT_ERROR);
+        }
+    }
+
+    private static boolean cannotPay(int money) {
+        return money % MIN_UNIT != 0;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}
