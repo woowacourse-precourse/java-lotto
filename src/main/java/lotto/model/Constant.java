42ac2ce6da36b796a60ecb6eb04f1de1dfff0fc9
Cyma-s <ssol0319@naver.com>
2022-11-15 01:22:17 +0900

    feat(Constant): add prize constant and rounded number

--- src/main/java/lotto/model/Constant.java
+++ src/main/java/lotto/model/Constant.java
@@ -5,4 +5,10 @@ public class Constant {
     public static final int NUMBER_RANGE_END = 45;
     public static final int LOTTO_NUMBER_COUNT = 6;
     public static final int MONEY_UNIT = 1000;
+    public static final int FIFTH_PRIZE = 5000;
+    public static final int FOURTH_PRIZE = 50000;
+    public static final int THIRD_PRIZE = 1500000;
+    public static final int SECOND_PRIZE = 30000000;
+    public static final int FIRST_PRIZE = 2000000000;
+    public static final double ROUNDED_NUMBER = 100.0;
 }
