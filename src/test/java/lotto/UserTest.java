96efdd6a2ee83f11447c51f3ca4b574db363649f
= <kimje1109@gmail.com>
2022-11-14 21:25:59 +0900

    test: 당첨 번호 입력 테스트

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -2,8 +2,13 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.InstanceOfAssertFactories.ARRAY;
+import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
+import static org.assertj.core.api.InstanceOfAssertFactories.array;
 
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import org.junit.jupiter.api.Test;
 
@@ -31,5 +36,52 @@ class UserTest {
 
         User user = new User();
         assertThatThrownBy(() -> user.setPurchasePrice()).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨_번호_입력(){
+        List<Integer> answer = List.of(1,2,3,4,5,6);
+        String userInput = "1,2,3,4,5,6";
+
+        ByteArrayInputStream input = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(input);
+
+        User user = new User();
+        user.setWinningNumbers();
+
+        assertThat(user.winningNumbers).isEqualTo(answer);
+    }
+
+    @Test
+    void 당첨_번호_예외처리1(){
+        String userInput = "1,2,3,4,5,6,7";
+
+        ByteArrayInputStream input = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(input);
+
+        User user = new User();
+        assertThatThrownBy(() -> user.setWinningNumbers()).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨_번호_예외처리2(){
+        String userInput = "1,2,3,4,5";
+
+        ByteArrayInputStream input = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(input);
+
+        User user = new User();
+        assertThatThrownBy(() -> user.setWinningNumbers()).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨_번호_예외처리3(){
+        String userInput = "1,2,3,4,5,a";
+
+        ByteArrayInputStream input = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(input);
+
+        User user = new User();
+        assertThatThrownBy(() -> user.setWinningNumbers()).isInstanceOf(IllegalArgumentException.class);
     }
 }
