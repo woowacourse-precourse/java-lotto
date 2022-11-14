cd06fc3e3b7e68559221ecb258e916d48112e2e2
이건회 <rg970604@naver.com>
2022-11-15 07:50:49 +0900

    refactor(PrintResult.java) : 매직스트링 선언, 포맷팅

--- src/main/java/lotto/view/PrintResult.java
+++ src/main/java/lotto/view/PrintResult.java
@@ -7,8 +7,15 @@ import lotto.domain.Prize;
 
 public class PrintResult {
 
+    private static final String PIECES_COUNT_MENT = "개를 구매했습니다.";
+    private static final String WINNINGMENT_START = "당첨 통계";
+    private static final String LINE = "---";
+    private static final String RANKCOUNT_FORMAT = "%s (%s원) - %s개";
+    private static final String PROFITRATE_FORMAT = "총 수익률은 %s%%입니다.";
+
+
     public static void PrintPieces(int pieces) {
-        System.out.println(pieces + "개를 구매했습니다.");
+        System.out.println(pieces + PIECES_COUNT_MENT);
     }
 
     public static void PrintLottos(Lottos lottos) {
@@ -18,18 +25,22 @@ public class PrintResult {
     }
 
     public static void PrintPrizeStatistic(Map<Prize, Integer> prizeBoard) {
-        System.out.println("당첨 통계");
-        System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - " + prizeBoard.get(Prize.THREE) + "개");
-        System.out.println("4개 일치 (50,000원) - " + prizeBoard.get(Prize.FOUR) + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + prizeBoard.get(Prize.FIVE) + "개");
+        System.out.println(WINNINGMENT_START);
+        System.out.println(LINE);
         System.out.println(
-            "5개 일치, 보너스 볼 일치 (30,000,000원) - " + prizeBoard.get(Prize.FIVE_AND_BOUNS) + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + prizeBoard.get(Prize.ALL) + "개");
+            String.format(RANKCOUNT_FORMAT, "3개 일치", "5,000", prizeBoard.get(Prize.THREE)));
+        System.out.println(
+            String.format(RANKCOUNT_FORMAT, "4개 일치", "50,000", prizeBoard.get(Prize.FOUR)));
+        System.out.println(
+            String.format(RANKCOUNT_FORMAT, "5개 일치", "1,500,000", prizeBoard.get(Prize.FIVE)));
+        System.out.println(String.format(RANKCOUNT_FORMAT, "5개 일치, 보너스 볼 일치", "30,000,000",
+            prizeBoard.get(Prize.FIVE_AND_BOUNS)));
+        System.out.println(
+            String.format(RANKCOUNT_FORMAT, "6개 일치", "2,000,000,000", prizeBoard.get(Prize.ALL)));
     }
 
     public static void PrintProfitRate(float profitRate) {
-        System.out.println("총 수익률은 " + profitRate + "%입니다.");
+        System.out.println(String.format(PROFITRATE_FORMAT, profitRate));
     }
 
 }
