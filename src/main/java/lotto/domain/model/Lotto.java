798f3a9667a19ffa6d00d6015ea7d25b715047b5
kingseungwooya <ksw_0505@nate.com>
2022-11-14 23:25:22 +0900

    feat(model): add validation of lotto

--- src/main/java/lotto/domain/model/Lotto.java
+++ src/main/java/lotto/domain/model/Lotto.java
@@ -3,6 +3,11 @@ package lotto.domain.model;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 45;
+    private static final int NUMBER_COUNT = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,9 +16,27 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != NUMBER_COUNT) {
+            throw new IllegalArgumentException("로또의 번호는 총 6개여야 합니다.");
         }
+        if (!isValidNumber(numbers)) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (isDuplicated(numbers)) {
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private boolean isDuplicated(List<Integer> numbers) {
+        Long distinctSize = numbers.stream()
+                .distinct()
+                .count();
+        return distinctSize != NUMBER_COUNT;
+    }
+
+    private boolean isValidNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .allMatch(i -> MINIMUM_NUMBER <= i && i <= MAXIMUM_NUMBER);
     }
 
     public int size() {
