b3f63ec7ad1b18020722d5d115cca9e148cee501
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-14 11:11:02 +0900

    feat: LottoMachine 생성자를 통해 numberGenerator 객체 생성 기능 구현

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -5,12 +5,16 @@ import lotto.util.BonusNumberValidator;
 import lotto.util.PurchaseAmtValidator;
 import lotto.util.WinningNumberValidator;
 
+import java.util.List;
+
 public class LottoMachine {
 
     private final Input input;
+    private final NumberGenerator numberGenerator;
 
     public LottoMachine() {
         this.input = new Input(new PurchaseAmtValidator(), new WinningNumberValidator(), new BonusNumberValidator());
+        this.numberGenerator = new NumberGenerator();
     }
 
 }
