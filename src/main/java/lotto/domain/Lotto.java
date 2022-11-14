a97305d1a07c5824f6148e4ad60b4cf2b4d86938
jini_11 <jieun21124@naver.com>
2022-11-14 16:46:20 +0900

    refactor: 당첨 번호 예외 처리 메시지 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,8 +6,8 @@ import java.util.Set;
 
 public class Lotto {
 
-    private static final String SIZE_ERROR_MESSAGE = "[ERROR] 6개의 숫자가 생성되지 않았습니다.";
-    private static final String RANGE_ERROR_MESSAGE = "[ERROR] 지정된 숫자의 범위를 벗어났습니다.";
+    private static final String SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개의 숫자로 구성되어야 합니다.";
+    private static final String RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1~45 사이의 숫자로 구성되어야 합니다.";
     private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 중복된 숫자가 있습니다.";
 
     private final List<Integer> numbers;
