2f31e4529e7c0a85c6b356b766b4024053a524d6
p <gustjr2332@naver.com>
2022-11-14 23:11:03 +0900

    test(JudgeWinningOfLotto) : 당첨 순위 찾기 테스트 추가

--- src/test/java/Service/JudgeWinningOfLottoTest.java
+++ src/test/java/Service/JudgeWinningOfLottoTest.java
@@ -1,10 +1,13 @@
 package Service;
 
+import lotto.Ranking;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class JudgeWinningOfLottoTest {
 
@@ -16,7 +19,7 @@ class JudgeWinningOfLottoTest {
         int match = judgeWinningOfLotto.compareMatchNumbers(List.of(1, 2, 3, 4, 5, 6), List.of(1, 2, 3, 44, 35, 26));
         int result = 3;
 
-        Assertions.assertThat(match).isEqualTo(result);
+        assertThat(match).isEqualTo(result);
     }
 
     @DisplayName("로또 숫자 0개 일치")
@@ -27,28 +30,54 @@ class JudgeWinningOfLottoTest {
         int match = judgeWinningOfLotto.compareMatchNumbers(List.of(1, 2, 3, 4, 5, 6), List.of(11, 12, 13, 14, 15, 16));
         int result = 0;
 
-        Assertions.assertThat(match).isEqualTo(result);
+        assertThat(match).isEqualTo(result);
 
     }
+
     @DisplayName("보너스 번호 포함")
     @Test
-    void containBounsNumber(){
+    void containBounsNumber() {
         JudgeWinningOfLotto judgeWinningOfLotto = new JudgeWinningOfLotto();
 
         boolean bonusStatus = judgeWinningOfLotto.isContainBounsNumber(List.of(1, 2, 3, 4, 5, 6), 6);
         boolean result = true;
 
-        Assertions.assertThat(bonusStatus).isEqualTo(result);
+        assertThat(bonusStatus).isEqualTo(result);
     }
 
     @DisplayName("보너스 번호 미포함")
     @Test
-    void notContainBounsNumber(){
+    void notContainBounsNumber() {
         JudgeWinningOfLotto judgeWinningOfLotto = new JudgeWinningOfLotto();
 
         boolean bonusStatus = judgeWinningOfLotto.isContainBounsNumber(List.of(1, 2, 3, 4, 5, 6), 10);
         boolean result = false;
 
-        Assertions.assertThat(bonusStatus).isEqualTo(result);
+        assertThat(bonusStatus).isEqualTo(result);
     }
+
+    @DisplayName("당첨 순위 2등")
+    @Test
+    void findRankSecond() {
+        JudgeWinningOfLotto judgeWinningOfLotto = new JudgeWinningOfLotto();
+
+        Ranking lottoRanking = judgeWinningOfLotto.findLottoRanking(5, true);
+        Ranking result = Ranking.SECOND;
+
+        assertThat(lottoRanking).isEqualTo(result);
+
+    }
+
+    @DisplayName("당첨 순위 3등")
+    @Test
+    void findRankThrid() {
+        JudgeWinningOfLotto judgeWinningOfLotto = new JudgeWinningOfLotto();
+
+        Ranking lottoRanking = judgeWinningOfLotto.findLottoRanking(5, false);
+        Ranking result = Ranking.THRID;
+
+        assertThat(lottoRanking).isEqualTo(result);
+
+    }
+
 }
