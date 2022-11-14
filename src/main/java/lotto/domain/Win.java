a61ac592b09ffe4acc7e234e6662d7c2976f8bd7
Jzakka <mouse4786@gmail.com>
2022-11-14 23:46:16 +0900

    refactor: 필드 및 메서드 정리
    불필요한 필드, 메서드 삭제
    
    writeTable 의 코드를 직관적으로 변경

--- src/main/java/lotto/domain/Win.java
+++ src/main/java/lotto/domain/Win.java
@@ -3,38 +3,31 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public enum Win {
-    NONE(0, -1, true, -1),
-    FIFTH(5_000, 3, true, 5),
-    FIFTH_WITHOUT_BONUS(5_000, 3, false, 5),
-    FOURTH(50_000, 4, true, 4),
-    FOURTH_WITHOUT_BONUS(50_000, 4, false, 4),
-    THIRD(1_500_000, 5, false, 3),
-    SECOND(30_000_000, 5, true, 2),
-    FIRST(2_000_000_000, 6, true, 1),
-    FIRST_WITHOUT_BONUS(2_000_000_000, 6, false, 1);
+    NONE(0, -1, true),
+    FIFTH(5_000, 3, true),
+    FIFTH_WITHOUT_BONUS(5_000, 3, false),
+    FOURTH(50_000, 4, true),
+    FOURTH_WITHOUT_BONUS(50_000, 4, false),
+    THIRD(1_500_000, 5, false),
+    SECOND(30_000_000, 5, true),
+    FIRST(2_000_000_000, 6, true),
+    FIRST_WITHOUT_BONUS(2_000_000_000, 6, false);
 
     private final Integer PRIZE;
     private final Integer MATCH;
     private final boolean BONUS;
-    private final Integer RANK;
     private Integer count = 0;
 
-    Win(Integer PRIZE, Integer MATCH, boolean BONUS, Integer RANK) {
+    Win(Integer PRIZE, Integer MATCH, boolean BONUS) {
         this.PRIZE = PRIZE;
         this.MATCH = MATCH;
         this.BONUS = BONUS;
-        this.RANK = RANK;
     }
 
     private void setCount(Integer count) {
         this.count = count;
-    }
-
-    public Integer getRANK() {
-        return RANK;
     }
 
     public static void initializeCount(){
@@ -69,13 +62,12 @@ public enum Win {
         return countTable;
     }
 
-    private static void writeTable(List<Integer> winHistory) {
-        List<Win> history = Arrays.stream(Win.values()).collect(Collectors.toList());
-        winHistory.add(0);                                              // 0등 - 더미데이터
-        winHistory.add(history.get(8).count + history.get(7).count);    // 1등
-        winHistory.add(history.get(6).count);                           // 2등
-        winHistory.add(history.get(5).count);                           // 3등
-        winHistory.add(history.get(4).count + history.get(3).count);    // 4등
-        winHistory.add(history.get(2).count + history.get(1).count);    // 5등
+    private static void writeTable(List<Integer> table) {
+        table.add(0);                                                  // 0등 - 더미데이터
+        table.add(Win.FIRST.count + Win.FIRST_WITHOUT_BONUS.count);    // 1등
+        table.add(Win.SECOND.count);                                   // 2등
+        table.add(Win.THIRD.count);                                    // 3등
+        table.add(Win.FOURTH.count + Win.FOURTH_WITHOUT_BONUS.count);  // 4등
+        table.add(Win.FIFTH.count + Win.FIFTH_WITHOUT_BONUS.count);    // 5등
     }
 }
