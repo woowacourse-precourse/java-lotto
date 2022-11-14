5910585fa04dbcde3f0229b46aad1e875e91f282
heesang <dlagmltkd12@naver.com>
2022-11-15 03:10:31 +0900

    test: LottoValidationUtilsTest 로또 번호의 개수가 6개가 아니면 예외 발생

--- src/test/java/lotto/domain/LottoValidationUtilsTest.java
+++ src/test/java/lotto/domain/LottoValidationUtilsTest.java
@@ -9,6 +9,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoValidationUtilsTest {
 
-
-
+    @DisplayName("로또 번호의 개수가 6개가 아니면 예외 발생")
+    @Test
+    void validateSize() {
+        assertThatThrownBy(() -> LottoValidationUtils.validate(List.of(1, 2, 3)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호의 개수는 6개 이어야 합니다.");
+    }
 }
