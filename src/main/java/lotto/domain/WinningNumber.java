bf75e22c5f41b10cc73cba3a90f43719f160b795
cocomozila <joseyun94@naver.com>
2022-11-14 23:00:10 +0900

    feat(WinningNumber): 문자열이 공백인지 검증하는 기능

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -13,6 +13,7 @@ public class WinningNumber {
     public void inputWinningNumber(String input) {
         validateInput(input);
         List<Integer> numbers = stringToWinningNumber(input);
+        this.lotto = new Lotto(numbers);
     }
 
     // 올바른 당첨 번호 입력값인지 검증하는 기능
@@ -35,13 +36,13 @@ public class WinningNumber {
 
     // 쉼표(,)인지 확인하는 기능
     private boolean isComma(char digit) {
-        return digit == ',';
+        return digit == Setting.SPLIT_CHARACTER;
     }
     
     // 쉼표(,)로 나눈 6개의 문자를 숫자로 변환하는 기능
     private List<Integer> stringToWinningNumber(String input) {
         List<Integer> list = new ArrayList<>();
-        String[] numbers = input.split(",");
+        String[] numbers = input.split(String.valueOf(Setting.SPLIT_CHARACTER));
         for (String number : numbers) {
             checkNumber(number);
             list.add(Integer.parseInt(number));
@@ -51,10 +52,18 @@ public class WinningNumber {
 
     // 문자열이 숫자인지 검증
     private void checkNumber(String number) {
+        checkBlank(number);
         for (int index = 0; index < number.length(); index++) {
             if (!Character.isDigit(number.charAt(index))) {
                 throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
             }
+        }
+    }
+
+    // 문자열이 공백인지 검증
+    private void checkBlank(String number) {
+        if (number.equals("")) {
+            throw new IllegalArgumentException("[ERROR] 공백을 입력할 수 없습니다.");
         }
     }
 }
