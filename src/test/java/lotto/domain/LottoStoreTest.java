a41efa287e72895dcba6eba3eee92309cf50ce51
ryanha <ryanha@handong.ac.kr>
2022-11-15 00:32:33 +0900

    test(domain): LottoStore 테스트 케이스 수정

--- src/test/java/lotto/domain/LottoStoreTest.java
+++ src/test/java/lotto/domain/LottoStoreTest.java
@@ -4,19 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
 public class LottoStoreTest {
     @Test
-    @DisplayName("로또를 구매하는 기능 테스트")
-    void purchase_lotto_test() {
+    @DisplayName("로또 구매 시 로또번호를 저장한 리스트를 반환하는 가?")
+    void lotto_purchase_test() {
         LottoStore store = new LottoStore();
         int amount = 13000;
         store.purchaseLotto(amount);
 
         assertThat(store.getPurchasedLotto())
-                .hasSize(amount/store.getPrice())
+                .hasSize(amount / Policy.PRICE)
                 .isInstanceOf(List.class);
     }
 }
