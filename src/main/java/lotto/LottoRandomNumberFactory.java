52d92bfc804e296dc9f3ce4af1ebb313599f5838
sungjun kim <dd8873@naver.com>
2022-11-14 21:11:59 +0900

    chore : LOTTO_COUNT 접근제어자 public 으로 수정

--- src/main/java/lotto/LottoRandomNumberFactory.java
+++ src/main/java/lotto/LottoRandomNumberFactory.java
@@ -8,7 +8,7 @@ public class LottoRandomNumberFactory {
 
     public static final int START_INCLUSIVE = 1;
     public static final int END_INCLUSIVE = 45;
-    private static final int LOTTO_COUNT = 6;
+    public static final int LOTTO_COUNT = 6;
 
     public List<Integer> createLottoRandomNumbers() {
         return Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, LOTTO_COUNT);
