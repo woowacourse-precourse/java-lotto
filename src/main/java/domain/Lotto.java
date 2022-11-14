0cfb40fe54fe4663bc7933d2aa93f42653197602
JUN <chltmdwns96@naver.com>
2022-11-14 15:48:12 +0900

    refactor : separate feature - move getRank() to Rank class - move inputMoney() to Money class

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -45,20 +45,5 @@ public class Lotto {
 
         return bonusNumber;
     }
-
-    public static Rank getRank(){
-        int bonusNumber = generateBonusNumber();
-        Lotto winningLotto = generateWinningLotto();
-        int lottoCnt = getLottoCntByMoney(inputMoney());
-        List<Lotto> lottos = generateLotto(lottoCnt);
-
-        Rank rank = compareLottoNumber(winningLotto, bonusNumber, lottos);
-        return rank;
-    }
-
-    public static Money inputMoney(){
-        int money = Integer.parseInt(Console.readLine());
-        return new Money(money);
-    }
 }
 
