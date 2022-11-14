c13321ac822f91445a109f5da7ab6bb6df0b84d6
vcho1958 <gusckd57@gmail.com>
2022-11-15 06:22:59 +0900

    chore(AnswerNumberList): bonusNumber final로 변환

--- src/main/java/lotto/AnswerNumberList.java
+++ src/main/java/lotto/AnswerNumberList.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class AnswerNumberList {
     private final List<Long> answerNumbers;
-    private Long bonusNumber;
+    private final Long bonusNumber;
 
     public AnswerNumberList(List<Long> answerNumbers, Long bonusNumber) {
         this.answerNumbers = answerNumbers;
