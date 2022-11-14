3d0c6d6136b885835379cbd930dfb01404051506
poter0501 <poter0501@gmail.com>
2022-11-14 22:55:08 +0900

    feat(WinningRanking): 각 등수를 나타내는 order 변수 추가

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -6,20 +6,22 @@ import java.util.Collections;
 import java.util.List;
 
 enum WinningRanking{
-    FIRST_WITHOUT_BONUS(6,0, 2_000_000_000),
-    FIRST_WITH_BONUS(6,1, 2_000_000_000),
-    SECOND(5,1, 30_000_000),
-    THIRD_WITHOUT_BONUS(5,0, 1_500_000),
-    THIRD_WITH_BONUS(5,1, 1_500_000),
-    FOURTH_WITHOUT_BONUS(4,0, 50_000),
-    FOURTH_WITH_BONUS(4,1, 50_000),
-    FIFTH_WITHOUT_BONUS(3,0, 5_000),
-    FIFTH_WITH_BONUS(3,1, 5_000);
+    FIRST_WITHOUT_BONUS(1,6,0, 2_000_000_000),
+    FIRST_WITH_BONUS(1,6,1, 2_000_000_000),
+    SECOND(2,5,1, 30_000_000),
+    THIRD_WITHOUT_BONUS(3,5,0, 1_500_000),
+    THIRD_WITH_BONUS(3,5,1, 1_500_000),
+    FOURTH_WITHOUT_BONUS(4,4,0, 50_000),
+    FOURTH_WITH_BONUS(4,4,1, 50_000),
+    FIFTH_WITHOUT_BONUS(5,3,0, 5_000),
+    FIFTH_WITH_BONUS(5,3,1, 5_000);
 
+    private final int order;
     private final int countMatches;
     private final int isMatchBonus;
     private final int prize;
-    WinningRanking(int countMatches, int isMatchBonus, int prize){
+    WinningRanking(int order, int countMatches, int isMatchBonus, int prize){
+        this.order=order;
         this.countMatches=countMatches;
         this.isMatchBonus=isMatchBonus;
         this.prize=prize;
