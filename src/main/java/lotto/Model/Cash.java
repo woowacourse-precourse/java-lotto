91843e00643b27f5b274542e6e2f2880c5e4c47b
RumbleKAT <reki318@naver.com>
2022-11-14 22:47:24 +0900

    (refactor) 1-2. 현금 부모 객체 생성을 통한 리펙토링

--- src/main/java/lotto/Model/Cash.java
+++ src/main/java/lotto/Model/Cash.java
@@ -1,32 +1,22 @@
 package lotto.Model;
 
-public class Cash {
-    private int cash;
-
-    public Cash(String cash){
-        this.cash = validate(cash);
-    }
+public class Cash extends Money{
     private int validate(String cash){
         int result = 0;
         try {
             result = Integer.parseInt(cash);
         }catch (NumberFormatException numberFormatException){
-            throw new IllegalArgumentException("[ERROR] 현금은 숫자로만 입력이 되어야합니다.");
+            System.out.println("[ERROR] 현금은 숫자로만 입력이 되어야합니다.");
         }
         if(result % 1000 != 0)
             throw new IllegalArgumentException("[ERROR] 로또 구입 가격은 1000으로 나누어 떨어져야 합니다.");
         return result;
     }
-
-    public int getCash() {
-        return cash;
+    public Cash(String cash){
+        this.setMoney(validate(cash));
     }
-
     public int getLottoCnt() {
-        return cash / 1000;
+        return this.getMoney() / 1000;
     }
 
-    public void setCash(int cash) {
-        this.cash = cash;
-    }
 }
