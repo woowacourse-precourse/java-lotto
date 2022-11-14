6dc573c8d1f43f33ec526972404fd10a8c509708
kkm5291 <kkm23125291@gmail.com>
2022-11-15 01:02:08 +0900

    [FEAT] ErrorMessage : 숫자 입력 에러 메세지 추가

--- src/main/java/util/ErrorMessage.java
+++ src/main/java/util/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     DUPLICATE_NUMBER("중복된 숫자는 잘못된 입력값입니다. 다시 입력해주세요"),
     WINNING_NUMBER_COUNT_ERROR("당첨번호 숫자는 6개를 입력해주세요."),
     BONUS_NUMBER_COUNT_ERROR("보너스 번호는 1개만 입력해주세요."),
-    RANGE_ERROR("로또 숫자 범위를 벗어난 숫자입니다. 다시 입력해주세요.");
+    RANGE_ERROR("로또 숫자 범위를 벗어난 숫자입니다. 다시 입력해주세요."),
+    NUMERIC_ERROR("숫자만 입력해주세요.");
 
     private final String errorMessage;
     private static final String startErrorMessage = "[ERROR] ";
