e1c36864cad86b8d72cd65a64b450f7d502560af
hhiyeon <hhyyeon0214@gmail.com>
2022-11-15 04:19:14 +0900

    feat: rank-create lotto rank enum

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,4 +1,32 @@
 package lotto.domain;
 
 public enum Rank {
+    FIRST_RANK(1, 6, 2000000000),
+    SECOND_RANK(2, 7, 30000000),
+    THIRD_RANK(3, 5, 1500000),
+    FOURTH_RANK(4, 4, 50000),
+    FIFTH_RANK(5, 3, 5000),
+    NOTHING(6, 0, 0);
+
+    private final int rank;
+    private final int match;
+    private final int prize;
+
+    Rank(int rank, int match, int prize) {
+        this.rank = rank;
+        this.match = match;
+        this.prize = prize;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public int getMatch() {
+        return match;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
 }
