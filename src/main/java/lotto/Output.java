d219578ff739cf8835a06f403e0cb6d4f8be636e
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-14 22:18:58 +0900

    refactor: add empty line

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -2,6 +2,11 @@ package lotto;
 
 public class Output {
     public static void outputNumberOfLotto(int numberOfLottos) {
+        printEmptyLine();
         System.out.println(numberOfLottos + "개를 구매했습니다.");
+    }
+
+    public static void printEmptyLine() {
+        System.out.println();
     }
 }
