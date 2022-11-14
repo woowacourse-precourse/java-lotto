bf08be0a8b2d376de091a812b99d40aaa11d6a37
osm <osm0822@naver.com>
2022-11-14 16:13:12 +0900

    feat(LottoSystem) : 로또가격, WinInfo정보 구현

--- src/main/java/lotto/LottoSystem.java
+++ src/main/java/lotto/LottoSystem.java
@@ -3,6 +3,15 @@ package lotto;
 import java.util.List;
 
 public class LottoSystem {
+    public static final int price = 1000;
+    public static final List<WinInfo> winInfomations = List.of(
+            new WinInfo(6,0,2000000000),
+            new WinInfo(5,1,30000000),
+            new WinInfo(5,0,1500000),
+            new WinInfo(4,0,50000),
+            new WinInfo(3,0,5000)
+    );
+
     private List<Integer> winNumbers;
     private int bonusNumbers;
 
