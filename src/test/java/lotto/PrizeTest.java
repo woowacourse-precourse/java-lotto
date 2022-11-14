e2a72b3dc4806ef3b6a794268760e92ab62dae2a
devMuscle <hongsbien@naver.com>
2022-11-14 21:22:25 +0900

    feat: 당첨번호, 보너스 번호 검증 기능 테스트

--- src/test/java/lotto/PrizeTest.java
+++ src/test/java/lotto/PrizeTest.java
@@ -8,10 +8,25 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PrizeTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+
+    @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createPrizeByOverSize() {
         assertThatThrownBy(() -> new Prize(List.of(1, 2, 3, 4, 5, 6, 7), 10))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createPrizeByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Prize(List.of(1, 2, 3, 3, 6, 7), 10))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호의 숫자와 중복되면 예외가 발생한다.")
+    @Test
+    void createPrizeByBonusNumberAndWinningNumberOverlap() {
+        assertThatThrownBy(() -> new Prize(List.of(1, 2, 3, 4, 6, 7), 3))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
