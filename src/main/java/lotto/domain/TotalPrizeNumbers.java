62cdaaf5cd149f5f1cdbddf01a5a3814928c0424
great-park <cksgh1735@naver.com>
2022-11-14 17:09:08 +0900

    fix: 오류 메시지 오타 수정

--- src/main/java/lotto/domain/TotalPrizeNumbers.java
+++ src/main/java/lotto/domain/TotalPrizeNumbers.java
@@ -13,7 +13,7 @@ public class TotalPrizeNumbers {
     private static final String DUPLICATE_PRIZENUMBER_EXISTS = "당첨 번호 중 중복된 숫자가 포함되었습니다.";
     private static final String PRIZENUMEBR_IS_NOT_IN_RANGE = "당첨 번호가 1부터 45까지의 범위 밖의 숫자가 포함되었습니다.";
     private final String BONUSNUMEBR_IS_NOT_IN_RANGE = "보너스 번호가 1부터 45까지의 범위 밖의 숫자가 포함되었습니다.";
-    private final String BONUSNUMBER_OVERLAPS = "보너스 번호가 입력한 당첨 번화 중 하나와 중복됩니다.";
+    private final String BONUSNUMBER_OVERLAPS = "보너스 번호가 입력한 당첨 번호 중 하나와 중복됩니다.";
     private static final int MINIMUM_PRIZENUMBER = 1;
     private static final int MAXIMUM_PRIZENUMBER = 45;
     private static final int LOTTO_TOTAL_COUNT = 6;
