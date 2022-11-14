e362284b016a95fac1b62eda5fcb8e3e3ba57ae9
ohhonggi <5ghdrl@naver.com>
2022-11-15 06:18:39 +0900

    feat($Output): Add printGeneratedLotto function
    This function outputs the issued lotto.
    
    Closes feat_4

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,5 +1,13 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
+import java.util.List;
+
 public class Output {
+
+    public static void printGeneratedLotto(List<Lotto> lottoList) {
+        lottoList.forEach( lotto -> System.out.println(lotto.toString()));
+    }
 
 }
