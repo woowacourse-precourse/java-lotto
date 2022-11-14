796d0bb1f120dd4daa1886c0a119f5788ef189c9
beombu <sunil1369@bible.ac.kr>
2022-11-15 06:34:08 +0900

    Feat(Output) : 금액 요청 메시지 출력 기능

---
+++ src/main/java/lotto/view/Output.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class Output {
+    public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+
+
+    public static void printInputMoney(){
+        System.out.println(INPUT_MONEY_MESSAGE);
+    }
+}
