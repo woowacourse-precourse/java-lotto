30b760b4bb8d11cc579458d10bcd4e14c8d620fc
Identityless <rlandwnd555@naver.com>
2022-11-14 20:51:36 +0900

    fix : 단일 로또 객체가 아닌 입력된 갯수 만큼의 로또 리스트 반환하도록 수정

--- src/main/java/util/LottoMachine.java
+++ src/main/java/util/LottoMachine.java
@@ -3,8 +3,15 @@ package util;
 import camp.nextstep.edu.missionutils.Randoms;
 import domain.Lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoMachine {
-    public static Lotto getLotto() {
-        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    public static List<Lotto> getLotto(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i = 0 ; i < count ; i++) {
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+        return lottos;
     }
 }
