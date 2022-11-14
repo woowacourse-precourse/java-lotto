4911f977b5578fda7f770bb4f814de21c1324bc5
daeun <gain3436@gmail.com>
2022-11-15 03:05:32 +0900

    test : 구매금액 예외 테스트 작성

---
+++ src/test/java/lotto/MoneyTest.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import lotto.domain.Bonus;
+import lotto.domain.Money;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class MoneyTest {
+
+    @DisplayName("금액이 1000원 단위가 아니면 예외가 발생한다.")
+    @Test
+    void createMoneyByNotDivided() {
+        assertThatThrownBy(() -> new Money(777))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("금액이 0원 이하면 예외가 발생한다.")
+    @Test
+    void createMoneyByNegative() {
+        assertThatThrownBy(() -> new Money(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
