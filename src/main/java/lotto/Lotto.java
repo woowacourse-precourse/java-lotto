00b0addbc55cbf48e8f1fc9f4f67c3738622302c
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-14 15:23:07 +0900

    REFACTOR:Lotto.java/rename method

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,14 +1,13 @@
 package lotto;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateOverNum(numbers);
-        validateNotMatchMoney(numbers);
+        validateDuplicate(numbers);
         validateNotMatchNum(numbers);
         sorting(numbers);
         this.numbers = numbers;
@@ -21,7 +20,7 @@ public class Lotto {
         }
     }
 
-    private void validateNotMatchMoney(List<Integer> numbers) {
+    private void validateDuplicate(List<Integer> numbers) {
         for (int i = 0; i < numbers.size() - 1; i++) {
             if (numbers.get(i) == numbers.get(i + 1)) {
                 System.out.println("[ERROR] 로또 번호에 중복된 숫자 존재");
