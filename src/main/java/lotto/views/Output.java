739e7eca8ad4a2b18939446bee01cacc7220a529
kwYoohae <gocksdl7979@gmail.com>
2022-11-14 22:20:14 +0900

    fix: 출력 형식 빼먹은 부분 수정

--- src/main/java/lotto/views/Output.java
+++ src/main/java/lotto/views/Output.java
@@ -8,10 +8,10 @@ public class Output {
     public void printPrize(Prize prize) {
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.println(Ranking.FIFTH_PLACE.getPrintSentence() + " - " + prize.getValue(Ranking.FIFTH_PLACE));
-        System.out.println(Ranking.FOURTH_PLACE.getPrintSentence() + " - " + prize.getValue(Ranking.FOURTH_PLACE));
-        System.out.println(Ranking.THIRD_PLACE.getPrintSentence() + " - " + prize.getValue(Ranking.THIRD_PLACE));
-        System.out.println(Ranking.SECOND_PLACE.getPrintSentence() + " - " + prize.getValue(Ranking.SECOND_PLACE));
-        System.out.println(Ranking.FIRST_PLACE.getPrintSentence() + " - " + prize.getValue(Ranking.FIRST_PLACE));
+        System.out.println(Ranking.FIFTH_PLACE.getPrintSentence() + " - " + prize.getValue(Ranking.FIFTH_PLACE) + "개");
+        System.out.println(Ranking.FOURTH_PLACE.getPrintSentence() + " - " + prize.getValue(Ranking.FOURTH_PLACE) + "개");
+        System.out.println(Ranking.THIRD_PLACE.getPrintSentence() + " - " + prize.getValue(Ranking.THIRD_PLACE) + "개");
+        System.out.println(Ranking.SECOND_PLACE.getPrintSentence() + " - " + prize.getValue(Ranking.SECOND_PLACE) + "개");
+        System.out.println(Ranking.FIRST_PLACE.getPrintSentence() + " - " + prize.getValue(Ranking.FIRST_PLACE) + "개");
     }
 }
