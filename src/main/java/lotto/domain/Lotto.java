f490837bac2dc57056a90b19fe1492aa3646161c
hyun0404woo <hyun0404woo@gmail.com>
2022-11-14 21:15:14 +0900

    feat: 새로운 로또를 만드는 정적 메서드 생성
    - 1~45 사이의 6자리 숫자로 중복 없이 구성된 리스트
    - 오름차순 정렬된 상태로 반환
    - pickUniqueNumbersInRange 메서드가 불변 리스트를 반환하기 때문에 새로운 리스트 객체를 생성해서 정렬

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,25 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.util.ConstValue.LottoConst;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
+    public Lotto(final List<Integer> numbers) {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+    public static Lotto createRandomLotto() {
+        final List<Integer> lottoNumber =
+                Randoms.pickUniqueNumbersInRange(LottoConst.START_NUMBER, LottoConst.END_NUMBER, LottoConst.LOTTO_LENGTH);
+        List<Integer> sortedLottoNumber = new ArrayList<>(lottoNumber);
+        sortedLottoNumber.sort(Integer::compareTo);
+        return new Lotto(sortedLottoNumber);
     }
 
-    // TODO: 추가 기능 구현
 }
