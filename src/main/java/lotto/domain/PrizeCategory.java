efe28a830b58829f90bfdcc1c9479c0e178eed11
WonSubSub <boxer1995@naver.com>
2022-11-14 23:27:59 +0900

    refactor: enum에 상금 변수를 추가
    수익률을 계산하기 위해 상금을 int로 저장하는 변수를 추가하였다.

--- src/main/java/lotto/domain/PrizeCategory.java
+++ src/main/java/lotto/domain/PrizeCategory.java
@@ -1,16 +1,18 @@
 package lotto.domain;
 
 public enum PrizeCategory {
-    Three("5,000원", 3),
-    Four("50,000원", 4),
-    Five("1,500,000원", 5),
-    Sixbonus("30,000,000원", 5),
-    Six("2,000,000,000원", 6);
+    THREE("5,000원", 5000, 3),
+    FOUR("50,000원", 50000, 4),
+    FIVE("1,500,000원", 1500000, 5),
+    SIXBONUS("30,000,000원", 30000000, 5),
+    SIX("2,000,000,000원", 2000000000, 6);
 
-    String winnings;
+    String winningsPrint;
+    int winnings;
     int matchCount;
 
-    PrizeCategory(String winnings, int matchCount) {
+    PrizeCategory(String winningsPrint, int winnings, int matchCount) {
+        this.winningsPrint = winningsPrint;
         this.winnings = winnings;
         this.matchCount = matchCount;
     }
