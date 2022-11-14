1ed23a1b82849fef8226bf180f5185da1eacda69
wnsgh12s <junho1273@gmail.com>
2022-11-14 14:33:08 +0900

    Feat: 추가 게임시작과 구입 금액 입력 메시지 출력

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,13 @@
+package lotto.controller;
+
+import lotto.domain.LottoGame;
+import lotto.view.InputMessage;
+
+public class LottoGameController {
+    private final InputMessage inputMessage = new InputMessage();
+
+    public void lottoGameStart() {
+        LottoGame lottoGame = new LottoGame();
+        inputMessage.gameStart();
+    }
+}
---
+++ src/main/java/lotto/view/InputMessage.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class InputMessage {
+    private final String GAME_START = "게임 시작";
+    private final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요";
+    public void gameStart() {
+        System.out.println(GAME_START);
+        System.out.println(PURCHASE_AMOUNT_MESSAGE);
+    }
+}
