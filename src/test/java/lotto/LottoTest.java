2009e15bcda6c5443e12112e955bab5fdc14603e
Your Name <you@example.com>
2022-11-14 20:29:32 +0900

    test(lottoNumberCreator) : 로또 보너스 번호가 로또 당첨 번호중 중복된 숫자 입력 시 예외 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -136,4 +136,20 @@ class LottoTest {
         assertThatThrownBy(() -> lottoNumberCreator.createLottoBonusBall(lottoBonusNumber, lottoWinNumber))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("로또 보너스 번호가 로또 당첨 번호 중 중북된 숫자를 입력 시 예외가 발생한다.")
+    @Test
+    void inputLottoBonusNumberByDuplicatedNumber() {
+        String money = "1";
+        InputStream is = new ByteArrayInputStream(money.getBytes());
+        System.setIn(is);
+
+        LottoNumberCreator lottoNumberCreator = new LottoNumberCreator();
+        Lotto lottoWinNumber = lottoNumberCreator.createLottoWinNumber("1, 2, 3, 4, 5, 6");
+        InputUi inputUi = new InputUi();
+        String lottoBonusNumber = inputUi.inputLottoBonusNumber();
+
+        assertThatThrownBy(() -> lottoNumberCreator.createLottoBonusBall(lottoBonusNumber, lottoWinNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
