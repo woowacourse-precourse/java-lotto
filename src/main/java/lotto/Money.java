e15277483f3e3a022d803e66c5b7f26fb00bf0b2
ShinKwang <sklee0206@naver.com>
2022-11-14 15:46:08 +0900

    feat: Make validation feature for money
    로또 구입 금액 입력이 숫자인지 검증하는 로직을 만든다. readLine으로 들어오기 때문에 어떤 문자나 들어올 수 있다. 따라서 숫자만 들어올 수 있도록 숫자인지 확인하는 검증 로직을 만든다.

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -8,9 +8,17 @@ public class Money {
         validate(inputMoney);
         this.money = Integer.parseInt(inputMoney);
     }
-
     private void validate(String inputMoney) {
-
-
+        validateForDigit(inputMoney);
+    }
+    private void validateForDigit(String inputMoney) {
+        for (char number : inputMoney.toCharArray()) {
+            checkDigit(number);
+        }
+    }
+    private void checkDigit(char number) {
+        if (!Character.isDigit(number)) {
+            throw new IllegalArgumentException("[ERROR] 구입금액은 공백없이 숫자로만 입력해야 합니다.");
+        }
     }
 }
