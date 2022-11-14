f3b5b96a17f66dc6e4979ba9831aa122372e9af3
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-14 11:19:39 +0900

    feat(Computer) : lottos 출력하는 기능 추가 printLottos() 메소드

--- src/main/java/lotto/domain/Computer.java
+++ src/main/java/lotto/domain/Computer.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class Computer {
     // 2주차 피드백 반영
+    static final String BUY_STR = "개를 구매했습니다.";
     static final int STRAT_RANGE = 1;
     static final int END_RANGE = 45;
     static final int LOTTO_NUMBER_LENGTH = 6;
@@ -31,13 +32,21 @@ public class Computer {
             lottos.add(lotto);
             times -= 1;
         }
-
+        printLottos(lottos);
         return lottos;
     }
 
     public int countTimes(int money){
         int times = money / LOTTO_CHARGE;
-        System.out.println(times +"개를 구매했습니다.");
+        // 실행 결과 예시와 맞추기 위해 한 칸 띄우기
+        System.out.println();
+        System.out.println(times + BUY_STR);
         return times;
+    }
+
+    public void printLottos(List<List<Integer>> lottos){
+        for (List<Integer> lotto : lottos){
+            System.out.println(lotto);
+        }
     }
 }
