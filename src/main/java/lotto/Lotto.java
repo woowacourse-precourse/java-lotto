01720dcca240f29416c19243cfda1f7be2910b9e
thousandcows <milcamboo@gmail.com>
2022-11-14 20:36:03 +0900

    feat: add more validation logics to validate function
    Added two more logic
    - checkNumberRange: check if the number is from 1 to 45
    - checkDuplication

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -14,11 +15,33 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+        checkDuplication(numbers);
+
+        for (Integer number : numbers) {
+            checkNumberRange(number);
+        }
     }
 
     public void validatePayment(int payment) {
         if (payment % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위로 입력할 수 있습니다.");
+        }
+    }
+
+    private void checkNumberRange(int n) {
+        if (n < 1 || n > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void checkDuplication(List<Integer> lottoNumbers) {
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        lottoNumbers.stream().map(hashSet::add);
+
+        if (hashSet.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자는 입력할 수 없습니다.");
         }
     }
 }
