452d0dc05eb5be106045832436a705693d23818c
JuSeong1130 <jsmoon1130@naver.com>
2022-11-15 04:32:40 +0900

    feat(LottoAutoGenerator): 로또 자동 생성 기능 구현

--- src/main/java/lotto/domain/lotto/LottoAutoGenerator.java
+++ src/main/java/lotto/domain/lotto/LottoAutoGenerator.java
@@ -1,5 +1,13 @@
 package lotto.domain.lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoAutoGenerator {
 
+	public Lotto createLottoByAuto() {
+		List<Integer> numbers =Randoms.pickUniqueNumbersInRange(1, 45, 6);
+		return new Lotto(numbers);
+	}
 }
