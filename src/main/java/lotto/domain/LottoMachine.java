76b188c7895f364ecac1e1afad4145fd150fa420
cocomozila <joseyun94@naver.com>
2022-11-14 20:34:47 +0900

    feat(LottoMachine): 금액 만큼 로또 구매하기

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -3,9 +3,12 @@ package lotto.domain;
 import lotto.setting.Setting;
 import lotto.util.Input;
 
+import java.util.List;
+
 public class LottoMachine {
 
     private Money money;
+    private LottoStore lottoStore;
 
 //  돈 입력 기능
     public void inputMoney() throws IllegalArgumentException {
@@ -13,12 +16,19 @@ public class LottoMachine {
     }
 
 //  구매 개수 반환 기능
-    public int getNumberOfLottoPurchases() {
+    public int getLottoCount() {
         return (int)(money.getMoney() / Setting.PURCHASE_AMOUNT_UNIT);
     }
 
-//    구매한 로또 반환 기능
-//    public List<List<Integer>> getLottos() {}
+//  금액 만큼 로또 구매하기
+    public void buyLottos(int lottoCount) {
+        this.lottoStore = new LottoStore(lottoCount);
+    }
+
+    // 구매한 로또 반환 기능
+    public List<List<Integer>> getPurchaseLottos() {
+        return lottoStore.getLottos();
+    }
 
 //    당첨 결과 반환 기능
 //    public List<Integer> getLottoResult() {}
