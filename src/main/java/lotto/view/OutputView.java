2d84b1b7c1b7be0af961972600ab90408fa6b7ad
yujaemin <woawoa0513@gmail.com>
2022-11-14 22:01:28 +0900

    feat: 로또 구매 결과 출력 함수 구현
    사용자가 구매한 로또의 개수와 모든 로또의 번호를 출력한다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,8 @@
 package lotto.view;
 
+import lotto.Lotto;
+import java.util.List;
+
 public class OutputView {
 
     public static void printLottoPurchaseInputNotice() {
@@ -12,5 +15,13 @@ public class OutputView {
 
     public static void printBonusNumberInputNotice() {
         System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public static void printLottoPurchaseResult(List<Lotto> lottos) {
+        System.out.printf("%d개를 구매했습니다.\n", lottos.size());
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.toString());
+        }
+        System.out.println();
     }
 }
