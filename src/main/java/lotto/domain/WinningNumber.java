4de020811d04683de44ddda2e6c555d9c94bd89b
HyunseokLee99 <hyun104437@gmail.com>
2022-11-14 16:49:13 +0900

    feat(lotto): 당첨번호 숫자 범위 체크 구현

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -31,6 +31,7 @@ public class WinningNumber {
 
     private Boolean checkDuplicateNumber(List<Integer> winNumbers) throws IllegalAccessException {
         List<Integer> tempWinNumber = new ArrayList<>(winNumbers);
+        tempWinNumber.add(bonusNumber);
         Set<Integer> numSet = new HashSet<>(tempWinNumber);
 
         if (numSet.size() != tempWinNumber.size()) {
@@ -43,10 +44,24 @@ public class WinningNumber {
     private Boolean checkWinNumberSize(List<Integer> winNumber) throws IllegalAccessException {
         if (winNumber.size() != 6) {
             System.out.println("당첨번호 숫자는 6개입니다!");
-            throw new IllegalAccessException("[ERROR]: NOT CORRECT UNIT!");
+            throw new IllegalAccessException("[ERROR]: NOT CORRECT SIZE!");
         }
         return true;
     }
 
+    private Boolean checkNumbersRange(List<Integer> winNumbers) throws IllegalAccessException {
+        for (Integer winNumber: winNumbers) {
+            if (!checkCorrectNumber(winNumber)){
+                throw new IllegalAccessException("[ERROR]: NOT CORRECT WINNING NUMBER!");
+            }
+        }
+        if (!checkCorrectNumber(bonusNumber)) {
+            throw new IllegalAccessException("[ERROR]: NOT CORRECT BONUS NUMBER!");
+        }
+        return true;
+    }
 
+    private Boolean checkCorrectNumber(int number) {
+        return number >= 1 && number <= 45;
+    }
 }
