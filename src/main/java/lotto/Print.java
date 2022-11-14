5ca0bbb177daac2cd5c48e797073831db35aee21
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-14 15:28:44 +0900

    refactor(main/Print): 결과는 이미 계산된 earningRate를 받아 그대로 출력

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -23,9 +23,7 @@ public class Print {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
-    static void result(int purchaseAmount, Match match){
-        int earning=Operation.calculateEarning(match);
-        double earningRate = Operation.calculateEarningRate(purchaseAmount, earning);
+    static void result(double earningRate, Match match){
         System.out.println("당첨 통계\n---");
         System.out.printf("3개 일치 (5,000원) - %d개\n", match.match3);
         System.out.printf("4개 일치 (50,000원) - %d개\n", match.match4);
