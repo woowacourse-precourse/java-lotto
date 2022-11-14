0ab043d7a9b52a51dec772bc22cd464935bc6b3f
= <kimje1109@gmail.com>
2022-11-14 23:12:42 +0900

    feat: 로또를 발행하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,39 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Application {
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Notice notice = new Notice();
+        User user = new User();
+
+        notice.purchasePrice();
+        user.setPurchasePrice();
+        int purchaseNumber = user.getPurchaseNumber();
+        notice.purchaseNumber(purchaseNumber);
+        ArrayList<Lotto> lottos = getLottos(purchaseNumber);
+
+//        for (Lotto lotto : lottos) {
+//            System.out.println(lotto.getNumbers());
+//        }
+    }
+
+    public static ArrayList<Lotto> getLottos(int purchaseNumber) {
+        ArrayList<Lotto> lottos = new ArrayList<>();
+
+        for (int index = 0; index < purchaseNumber; index++) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            lottos.add(lotto);
+        }
+
+        return lottos;
     }
 }
