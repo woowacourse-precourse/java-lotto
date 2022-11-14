fff35c743496f0594013b47730b18af6023091b8
gayeonP <pagaye@naver.com>
2022-11-14 17:24:05 +0900

    로또 구입 금액 & 보너스 번호 입력 받는 기능 구현

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -10,16 +10,19 @@ import java.util.stream.Stream;
 
 public class LottoManager {
     private Lotto lotto;
-    List<Integer> user_input = new ArrayList<>();
+    private int cash = 0;
+    private List<Integer> user_input = new ArrayList<>();
+    private int bonus_num = 0;
 
     public void LottoStart() {
-
     }
 
     private void LottoInput() {
+        this.cash = Integer.valueOf(Console.readLine());
         String[] input = Console.readLine().split(",");
         for (int i = 0; i < input.length; i++) {
             this.user_input.add(Integer.valueOf(input[i]));
         }
+        this.bonus_num = Integer.valueOf(Console.readLine());
     }
 }
