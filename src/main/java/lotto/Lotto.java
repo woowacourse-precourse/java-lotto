e2ab8dda79a7eebc5b5acdab32e972222baafc5d
woogi <dnrlwo11@gmail.com>
2022-11-14 21:46:14 +0900

    feat : 로또 validate기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Lotto {
@@ -12,7 +13,9 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또의 번호는 6개여야 합니다.");
+        } else if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또의 번호는 중복되지 않아야 합니다.");
         }
     }
 
