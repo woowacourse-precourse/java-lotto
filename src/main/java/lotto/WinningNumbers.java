99f153cbcb42d7f4bfa7121f1e276c8d196bd9b3
jh868 <2weeks9947@naver.com>
2022-11-14 10:13:10 +0900

    refactor: 클래스 추출

---
+++ src/main/java/lotto/WinningNumbers.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private final Lotto WinningNumber;
+    private final int BonusNumber;
+
+    public WinningNumbers(Lotto winningNumber, int bonusNumber) {
+        validate(winningNumber, bonusNumber);
+        this.WinningNumber = winningNumber;
+        this.BonusNumber = bonusNumber;
+    }
+    private void validate(Lotto winningNumber, int bonusNumber){
+        if (bonusNumber < 1 || bonusNumber > 45){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (winningNumber.getNumbers().stream().anyMatch(number -> number.equals(bonusNumber))){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 겹치지 않아햐 합니다.");
+        }
+    }
+    List<Integer> getWinningNumber(){
+        return this.WinningNumber.getNumbers();
+    }
+    int getBonusNumber(){
+        return this.BonusNumber;
+    }
+}
