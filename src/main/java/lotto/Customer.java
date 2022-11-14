2a93a92261b0cf8869d9fe1547404a4d15b0ed3c
splitube <voicesplit@daum.net>
2022-11-14 15:21:31 +0900

    feat(Customer): 고객 객체 구현

---
+++ src/main/java/lotto/Customer.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import java.util.List;
+
+public class Customer {
+    private List<Lotto> myLottos;
+
+    public void insertMoney(LottoKiosk lottoKiosk) throws IllegalArgumentException {
+        lottoKiosk.getMoney();
+    }
+
+    public void getLotto(List<Lotto> lottos) {
+        this.myLottos = lottos;
+    }
+
+    public void insertLottos(LottoChecker lottoChecker){
+        lottoChecker.insertLottos(myLottos);
+    }
+}
