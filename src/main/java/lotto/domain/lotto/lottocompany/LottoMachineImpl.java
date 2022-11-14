517f2d3df1b590a3243328147955891141f3e125
5uhwann <5uhwann27@gmail.com>
2022-11-15 08:48:20 +0900

    refactor(로또회사): 로또 기계 컬렉션 구현체 변경
    List -> ArrayList

--- src/main/java/lotto/domain/lotto/lottocompany/LottoMachineImpl.java
+++ src/main/java/lotto/domain/lotto/lottocompany/LottoMachineImpl.java
@@ -3,6 +3,7 @@ package lotto.domain.lotto.lottocompany;
 import static lotto.domain.lotto.lotto.LottoInfo.*;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import lotto.domain.lotto.lotto.Lotto;
@@ -15,11 +16,10 @@ public class LottoMachineImpl implements LottoMachine {
     }
 
     private List<Integer> pickLottoNumbers() {
-        List<Integer> pickedLottoNumbers = Randoms.pickUniqueNumbersInRange(
-                MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_SIZE);
 
+        ArrayList<Integer> pickedLottoNumbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(
+                MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_SIZE));
         pickedLottoNumbers.sort(Comparator.naturalOrder());
-
         return pickedLottoNumbers;
     }
 }
