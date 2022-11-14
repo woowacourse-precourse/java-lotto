7101fe6b8b0d83ccae4a77f396d9149c6fa94e0c
hyungkya <hyungkya@uci.edu>
2022-11-14 16:28:50 +0900

    fix: 희망 구매가격 입력받기
    Change return type to int. Now throws error if parseInt() fails.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,15 +1,22 @@
 package lotto;
 
+import static java.lang.Integer.parseInt;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     private final static String inputPriceMessage = "구입금액을 입력해 주세요.";
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int price = readPrice(inputPriceMessage);
     }
 
-    private static String readPrice(String outputMessage) {
+    private static int readPrice(String outputMessage) {
         System.out.println(outputMessage);
-        return Console.readLine();
+        try {
+            return parseInt(Console.readLine());
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 구입금액은 숫자여야 합니다.");
+        }
     }
 }
