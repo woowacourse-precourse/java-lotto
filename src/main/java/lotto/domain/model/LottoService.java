c18b4d75d0026a85500936dd1380b166d94c35da
kingseungwooya <ksw_0505@nate.com>
2022-11-14 22:07:03 +0900

    refactor(service): refacotor service

--- src/main/java/lotto/domain/model/LottoService.java
+++ src/main/java/lotto/domain/model/LottoService.java
@@ -2,13 +2,11 @@ package lotto.domain.model;
 
 import lotto.domain.generator.LottoGenerator;
 import lotto.domain.model.request.MoneyRequest;
-import lotto.domain.validator.RequestValidator;
 
 public class LottoService {
 
     // 로또를 생성하고 로또들을 반환
     public Lottos getGeneratedLottoList(MoneyRequest moneyRequest) {
-        RequestValidator.moneyValidation(moneyRequest);
         LottoGenerator generator = new LottoGenerator(moneyRequest);
         return generator.getLottos();
     }
