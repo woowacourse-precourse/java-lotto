3cd7caa5eb775fd0747ad29a0cfb4a4f3ad725bf
YoosunMoon <muzikue@naver.com>
2022-11-14 20:29:19 +0900

    feat: 로또 구입 개수만큼 임의의 로또 번호 발행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     private static final int LOTTO_PRICE = 1000;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
     private static final String MONEY_TYPE_ERROR_MESSAGE = "[ERROR] 숫자가 아닌 값을 입력하였습니다.";
     private static final String MONEY_UNIT_ERROR_MESSAGE = "[ERROR] 구입 금액은 1000원 단위입니다.";
 
     public static void main(String[] args) {
         int inputMoney = 0;
+        List<Lotto> lottos = new ArrayList<>();
         String userInput = Console.readLine();
 
         try {
@@ -20,5 +28,11 @@ public class Application {
         if (inputMoney % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(MONEY_UNIT_ERROR_MESSAGE);
         }
+
+        for (int cnt = 0, lottoQuantity = inputMoney / LOTTO_PRICE; cnt < lottoQuantity; cnt++) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT));
+            lottos.add(lotto);
+        }
     }
 }
+
