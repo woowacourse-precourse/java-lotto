caebd59d4e48c7d96e3d87e8a3b49ab4d9245b7f
ttaehee <ttaehee1105@gmail.com>
2022-11-14 19:18:57 +0900

    test : 로또 도메인 단위테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,12 +1,13 @@
 package lotto;
 
-import lotto.model.Lotto;
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -19,10 +20,32 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또번호는 오름차순으로 정렬된다")
+    @Test
+    void sortNumbers() {
+        //given
+        List<Integer> numbers = List.of(42,35,45,32,34,21);
+
+        //when
+        Lotto lotto = new Lotto(numbers);
+
+        //then
+        assertThat(lotto.toString()).isEqualTo("[21, 32, 34, 35, 42, 45]");
+    }
+
+    @DisplayName("로또가 생성된다.")
+    @Test
+    void createLotto() {
+        //given
+
+        //when
+        Lotto lotto = Lotto.createLotto();
+
+        //then
+        assertThat(lotto.getNumbers().isEmpty()).isFalse();
+    }
 }
