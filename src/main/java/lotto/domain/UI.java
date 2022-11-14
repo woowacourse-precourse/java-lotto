55e811d161a77067c1346ab4c8d82ac64daeed40
Haaae <101785628+Haaae@users.noreply.github.com>
2022-11-14 17:18:58 +0900

    feat(UI): 구매한 복권 출력하는 기능 구현

--- src/main/java/lotto/domain/UI.java
+++ src/main/java/lotto/domain/UI.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Map;
+import java.util.Comparator;
 
 public class UI {
 
@@ -16,6 +18,16 @@ public class UI {
     public static String getAnswer(String request) {
         System.out.print(request);
         return Console.readLine().trim();
+    }
+
+    public static void printLotteries(Map<Lotto, Bonus> lotteries) {
+        System.out.println();
+        System.out.println(lotteries.size() + "개를 구매했습니다.");
+        for (Lotto lotto: lotteries.keySet()) {
+            lotto.getNumbers().sort(Comparator.naturalOrder());
+            System.out.println(lotto);
+        }
+        System.out.println();
     }
 
 }
