593dba0f1cfcd16cc906218ade18ff36e744a787
eastsage <dpwk8@naver.com>
2022-11-15 00:45:25 +0900

    test: 당첨 등수를 확인에 대한 테스트 추가
    여러개의 로또의 당첨을 확인하는 CheckLottosPrize에 대한 test 추가

--- src/test/java/lotto/domain/CheckLottosPrizeTest.java
+++ src/test/java/lotto/domain/CheckLottosPrizeTest.java
@@ -1,31 +1,44 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CheckLottosPrizeTest {
-    private List<Lotto> lottos = new ArrayList<Lotto>(List.of(
-            new Lotto(List.of(8, 21, 23, 41, 42, 43)),
-            new Lotto(List.of(3, 5, 11, 16, 32, 38)),
-            new Lotto(List.of(7, 11, 16, 35, 36, 44)),
-            new Lotto(List.of(1, 8, 11, 31, 41, 42)),
-            new Lotto(List.of(13, 14, 16, 38, 42, 45)),
-            new Lotto(List.of(7, 11, 30, 40, 42, 43)),
-            new Lotto(List.of(2, 13, 22, 32, 38, 45)),
-            new Lotto(List.of(1, 3, 5, 14, 22, 45)))
-    );
-    private Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-    private Integer bonusNumber = 7;
-    private CheckLottosPrize checkLottoPrize = new CheckLottosPrize(lottos, winningNumber, bonusNumber);
-    /**
-     * getAnswerCount
-     * getRank
-     * getTotalPrize
-     */
+    private Lotto winningNumbers;
+    private Integer bonusNumber;
+    private List<Lotto> lottos;
+    private CheckLottosPrize checkLottosPrize;
+
+    @BeforeEach
+    void testInit() {
+        winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        bonusNumber = 45;
+        lottos = new ArrayList<>();
+        lottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        lottos.add(new Lotto(Arrays.asList(2, 3, 4, 5, 6, 45)));
+        lottos.add(new Lotto(Arrays.asList(2, 3, 4, 5, 6, 7)));
+        lottos.add(new Lotto(Arrays.asList(3, 4, 5, 6, 7, 8)));
+        lottos.add(new Lotto(Arrays.asList(4, 5, 6, 7, 8, 9)));
+        checkLottosPrize = new CheckLottosPrize(lottos, winningNumbers, bonusNumber);
+    }
 
     @Test
-    void getAnswerCount_LottoAndWinningNumberMatchFourTimes_Return2(Lotto lotto) {
-        checkLottoPrize();
+    @DisplayName("번호가 일치하는 수에 따라 당첨 등수가 정해지는지 확인한다.")
+    void checkAllRankOfLottos() {
+        Map<String, Integer> totalPrize = checkLottosPrize.getTotalPrize();
+        assertThat(totalPrize).contains(
+                entry("FIRST", 1),
+                entry("SECOND", 1),
+                entry("THIRD", 1),
+                entry("FOURTH", 1),
+                entry("FIFTH", 1));
     }
 }
