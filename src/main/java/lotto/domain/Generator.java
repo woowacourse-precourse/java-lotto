3fa4b69af7ec1974fdc9d72778e5bd234066c708
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-14 23:38:07 +0900

    feat: 중복되지 않는 6개의 숫자 뽑는 기능 구현

---
+++ src/main/java/lotto/domain/Generator.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generator {
+    public List<List<Integer>> lottery = new ArrayList<List<Integer>>();
+    public List<Integer> createRandomLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
+    
+}
