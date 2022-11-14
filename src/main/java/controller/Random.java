5aa72272ecadc15133cc896c5f2d0f28ea6b2278
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-15 00:24:10 +0900

    feat(Random) : add a function that gets lotto number

--- src/main/java/controller/Random.java
+++ src/main/java/controller/Random.java
@@ -2,11 +2,20 @@ package controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Random {
     public static List<Integer> getRandomNumber() {
         List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return randomNumbers;
+    }
+
+    public static List<List<Integer>> getLottoNumber(int purchaseQuantity) {
+        List<List<Integer>> lottoNumbers = new ArrayList<>();
+        for (int i = 0; i < purchaseQuantity; i++) {
+            lottoNumbers.add(getRandomNumber());
+        }
+        return lottoNumbers;
     }
 }
--- src/test/java/lotto/RandomTest.java
+++ src/test/java/lotto/RandomTest.java
@@ -1,2 +1,15 @@
-package lotto;public class RandomTest {
+package lotto;
+
+import controller.Random;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomTest {
+    @DisplayName("로또번호를 구매수량만큼 뽑았는지 확인")
+    @Test
+    void 로또수량에_맞게_뽑았는지_확인하는_테스트(){
+        assertThat(Random.getLottoNumber(6).size()).isEqualTo(6);
+    }
 }
