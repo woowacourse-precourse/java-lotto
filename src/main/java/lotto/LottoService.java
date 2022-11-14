0ea68db12780472bccb88946cf344b1d79b5980b
kkirby54 <sdk926@gmail.com>
2022-11-15 02:32:33 +0900

    feat: 로또 발행
    - 당첨 번호와의 비교를 위해 발행한 로또를 List<Lotto> lottoRepository에 저장한다.

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -1,17 +1,40 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoService {
     private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_NUMBER_SIZE = 6;
+
+    private final List<Lotto> lottoRepository;
 
     public LottoService() {
+        lottoRepository = new ArrayList<>();
     }
 
-    public void init(){
+    public void init() {
         long lottoQuantity = purchaseLotto();
+
+        publishLottos(lottoQuantity);
     }
 
-    private long purchaseLotto(){
+    public long purchaseLotto() {
         Long purchaseMoney = UserInput.getPurchaseMoney();
         return purchaseMoney / LOTTO_PRICE;
+    }
+
+    public static List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, LOTTO_NUMBER_SIZE);
+    }
+
+    public void publishLottos(long lottoQuantity) {
+        for (int i = 0; i < lottoQuantity; i++) {
+            Lotto lotto = new Lotto(generateLottoNumbers());
+            lottoRepository.add(lotto);
+        }
     }
 }
