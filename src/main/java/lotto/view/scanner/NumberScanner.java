4696e9b0f0355c9bb9261e251df1dc64c0b54e98
taekwon-dev <taekwon_dev@naver.com>
2022-11-14 14:12:19 +0900

    feat: (NumberScanner) 로또 구매 금액을 입력 받는다

---
+++ src/main/java/lotto/view/scanner/NumberScanner.java
@@ -0,0 +1,9 @@
+package lotto.view.scanner;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class NumberScanner {
+    public String inputPayment() {
+        return readLine();
+    }
+}
