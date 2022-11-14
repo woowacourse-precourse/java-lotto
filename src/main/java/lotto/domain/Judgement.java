271115d7322ae7a1bd01ffd19affebbe4d434803
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-15 01:56:41 +0900

    feat: 당첨 갯수 저장하는 기능 구현

--- src/main/java/lotto/domain/Judgement.java
+++ src/main/java/lotto/domain/Judgement.java
@@ -1,8 +1,10 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Judgement {
+    public List<Integer> winningTable = new ArrayList<>();
     public int rankLotto(List<Integer> lottery, List<Integer> winningNumber) {
         int result = 0;
         for (int i=0; i<lottery.size(); i++){
@@ -13,7 +15,7 @@ public class Judgement {
         }
         return result;
     }
-    public boolean secondPrize(List<Integer> lottery, int bonus, int result) {\
+    public boolean secondPrize(List<Integer> lottery, int bonus, int result) {
         boolean second = false;
         if (result == 5) {
             if (lottery.contains(bonus)){
@@ -21,5 +23,12 @@ public class Judgement {
             }
         }
         return second;
+    }
+    public List<Integer> makeWinningTable(List<List<Integer>> lotterys, List<Integer> winningNumber){
+        for (List<Integer> lottery : lotterys){
+            int result = rankLotto(lottery, winningNumber);
+            winningTable.add(result);
+        }
+        return winningTable;
     }
 }
