d74cde13ee12d4f0d3fcf61f2b0d0dd4c7ebefe3
JasmineAir <gofoxfox@naver.com>
2022-11-14 17:30:55 +0900

    2-2.구입 금액 입력 값 형식에 따른 예외 처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -16,9 +16,35 @@ public class Application {
         System.out.println("구입 금액을 입력해주세요.");
     }
 
+    public static String Input(){
+        return Console.readLine();
+    }
+
+    public static void Number_exception(String purchase_Amount){
+        for(int i=0; i<purchase_Amount.length(); i++){
+            if((int)(purchase_Amount.charAt(i))>57 || (int)(purchase_Amount.charAt(i))<48){
+                throw new IllegalArgumentException("[Error] 자연수 값을 입력해주세요.");
+            }
+        }
+    }
+
+    public static void Unit_exception(String purchase_Amount){
+        if((Integer.parseInt(purchase_Amount)%1000)!=0){
+            throw new IllegalArgumentException("[Error] 1000원 단위로 입력해주세요.");
+        }
+    }
+
+//    public static int purchase_Amount(){
+//
+//    }
+
     public static void main(String[] args) {
         issue_Number();
         purchase_Output();
+        String purchase_Amount = Input();
+        Number_exception(purchase_Amount);
+        Unit_exception(purchase_Amount);
+
 
 
 
