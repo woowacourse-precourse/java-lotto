36ca20d0e71337e727a17c8653da79e8fa088905
beombu <sunil1369@bible.ac.kr>
2022-11-15 06:49:15 +0900

    Feat(Output) : 보너스 번호 요청 메시지 출력 기능

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -6,6 +6,7 @@ public class Output {
     public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String COUNT_LOTTO_MESSAGE = "%d개 구매했습니다.\n";
     public static final String INPUT_PRIZE_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.\n";
+    public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static void printInputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
@@ -21,5 +22,9 @@ public class Output {
 
     public static void printInputPrizeNumbers() {
         System.out.println(INPUT_PRIZE_NUMBERS_MESSAGE);
+    }
+
+    public static void printInputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
     }
 }
