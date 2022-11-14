5c1559b0b772482ee827cd325f746c207c353b33
masiljangajji <hjk172262@gmail.com>
2022-11-14 22:45:53 +0900

    feat: 상수 추가
    상수값을 관리하는 Message Class에 상수 추가

--- src/main/java/lotto/Message.java
+++ src/main/java/lotto/Message.java
@@ -2,11 +2,21 @@ package lotto;
 
 public enum Message {
 
+    THREE("3개 일치 (5,000원) - "),
+    FOUR("4개 일치 (50,000원) - "),
+    FIVE("5개 일치 (1,500,000원) - "),
+    FIVE_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    SIX("6개 일치 (2,000,000,000원) - "),
     START("구입금액을 입력해 주세요."),
-    TOTAL("개를 구매했습니다."),
+    BUY("개를 구매했습니다."),
     INPUT("당첨 번호를 입력해 주세요. "),
     BONUS("보너스 번호를 입력해 주세요. "),
-    STATISTICS("당첨 통계 ");
+    STATISTICS("당첨 통계 "),
+    AMOUNT("AMOUNT"),
+    WINNING("WINNING"),
+    BONUSNUMBER("BONUS"),
+    YIELD("총 수익률은 ");
+
 
     private final String message;
 
@@ -17,5 +27,6 @@ public enum Message {
     public String getMessage() {
         return message;
     }
+
 
 }
