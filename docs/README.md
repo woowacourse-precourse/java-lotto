b12ab624402bc95f1ba84bfc6d0f7497338bba82
juy4556 <kws05050@naver.com>
2022-11-15 01:15:52 +0900

    feat: 총 수익 계산을 위한 Prize enum 클래스 생성

--- docs/README.md
+++ docs/README.md
@@ -14,4 +14,4 @@
 - [X] 로또 게임 객체 내 당첨 통계 출력 메소드 printWinningStatistics 생성
 - [X] user 객체 내 수익률 계산 메소드 calculateEarningRate 생성
 - [X] 로또 게임 객체 내 총 수익 계산 메소드 calculateTotalEarning 생성
-- [ ] 총 수익 계산을 위한 Prize enum 클래스 생성
+- [X] 총 수익 계산을 위한 Prize enum 클래스 생성
---
+++ src/main/java/domain/Prize.java
@@ -0,0 +1,21 @@
+package domain;
+
+import java.util.function.Function;
+
+public enum Prize {
+    CALCULATE_THREE(value -> value * 5000),
+    CALCULATE_FOUR(value -> value * 50000),
+    CALCULATE_FIVE(value -> value * 1500000),
+    CALCULATE_SIX(value -> value * 2000000000),
+    CALCULATE_BONUS(value -> value * 30000000);
+
+    private Function<Integer, Integer> expression;
+
+    Prize(Function<Integer, Integer> expression) {
+        this.expression = expression;
+    }
+
+    public float calculate(Integer value) {
+        return expression.apply(value);
+    }
+}
