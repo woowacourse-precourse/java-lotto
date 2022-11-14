14e5776d854e0639e651781dc0fbe38685d0ada3
JohnPrk <uo3641493@gmail.com>
2022-11-14 17:44:23 +0900

    feat(Application) : 프로덕션 코드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import lotto.domain.*;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            InputView inputView = new InputView();
+            int moneyForLottos = inputView.inputMoneyForLottos();
+            LottoSalesMachine lottoSalesMachine = new LottoSalesMachine();
+            Lottos lottos = new Lottos(lottoSalesMachine.createLottos(moneyForLottos));
+            OutputView outputView = new OutputView();
+            outputView.printLottoNumbers(lottos);
+            Lotto winningLotto = inputView.inputWinningLottoNumber();
+            BonusNumber bonusNumber = inputView.inputBonusLottoNumber(winningLotto);
+            LottoResults lottoResults = lottos.compareToLottoNumbers(winningLotto, bonusNumber);
+            outputView.printLottoResults(lottoResults);
+            outputView.printRatioOfReturn(lottoResults);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+        }
     }
 }
