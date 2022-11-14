c19fa02c20e0c7a5e955160d04e3abec86a6a169
devMuscle <hongsbien@naver.com>
2022-11-14 09:57:25 +0900

    feat(inputByNotNumber): 숫자 아닌값 입력시 예외 발생 테스트

---
+++ src/test/java/lotto/UserTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UserTest {
+
+    @DisplayName("숫자가 아닌 것을 입력하면 예외가 발생한다.")
+    @Test
+    void inputByNotNumber() {
+        assertThatThrownBy(() -> new User("ABC"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+}
