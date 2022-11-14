0f7a626e394d925e8e60ccd561e297d0857bcce6
drunkenhw <drunkenhw@gmail.com>
2022-11-15 02:34:08 +0900

    feat(Lotto): 중복된 숫자를 입력하면 예외를 발생하는 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,13 +7,23 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또는 6자리를 입력해야 합니다");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        long uniqueCount = numbers.stream()
+                .distinct()
+                .count();
+        if (uniqueCount != numbers.size()) {
+            throw new IllegalArgumentException("중복된 숫자는 입력할 수 없습니다");
         }
     }
 
