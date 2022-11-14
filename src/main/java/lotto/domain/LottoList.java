076e67f3f53ae232aa338eef2a036f1f349b31c2
hingjae <ljh23270@naver.com>
2022-11-14 21:22:07 +0900

    로또 번호 발급 : 로또 리스트에 추가, 로또 출력

--- src/main/java/lotto/domain/LottoList.java
+++ src/main/java/lotto/domain/LottoList.java
@@ -1,2 +1,23 @@
-package lotto.domain;public class LottoList {
+package lotto.domain;
+
+import lotto.Lotto;
+import lotto.view.ConsoleOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoList {
+    List<Lotto> lottoList = new ArrayList<>();
+
+    public void addLottoList(Lotto lotto) {
+        this.lottoList.add(lotto);
+    }
+
+    public void printLottoList(int amount) {
+        ConsoleOutput output = new ConsoleOutput();
+        output.LottoAmount(amount);
+        for(Lotto lotto : this.lottoList){
+            System.out.println(lotto.getNumbers());
+        }
+    }
 }
