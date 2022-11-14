adefb571bef6b72a660e396f9c3505f091d9ec95
aossuper7 <aossuper7@naver.com>
2022-11-15 00:08:24 +0900

    feat: 로또 당첨번호 문자열을 리스트로 변환하는 기능

--- src/main/java/lotto/Utils/Convert.java
+++ src/main/java/lotto/Utils/Convert.java
@@ -7,8 +7,6 @@ import lotto.Constant.Message;
 public class Convert {
 
     private static final String COMMA = ",";
-    private static final int MIN_NUM = 1;
-    private static final int MAX_NUM = 45;
 
     public static int StrToInt(final String number) {
         try {
@@ -24,15 +22,8 @@ public class Convert {
 
         for (String num : number) {
             int n = StrToInt(num);
-            isLottoNumber(n);
             lottoNum.add(n);
         }
         return lottoNum;
-    }
-
-    private static void isLottoNumber(final int num) {
-        if (!(MIN_NUM <= num && num <= MAX_NUM)) {
-            throw new IllegalArgumentException(Message.ERROR_ONE_TO_FORTYFIVE.getMessage());
-        }
     }
 }
