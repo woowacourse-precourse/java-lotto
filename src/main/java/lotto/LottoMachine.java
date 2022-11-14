96d0c711953ff0bf563e9392d369839f0af61dc9
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-14 21:26:37 +0900

    feat(LottoMachine): 서로 다른 중복이 없는 6개의 번호(1~45)를 생성하는 기능 구현

---
+++ src/main/java/lotto/LottoMachine.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoMachine {
+
+    private List<Integer> generateNumber() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
+}
