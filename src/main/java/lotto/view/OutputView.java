a2b0eb8a5f795de2d7c54deb9b6fe8610556204d
kim0914 <kosa0914@naver.com>
2022-11-14 15:07:12 +0900

    feat(OutputView): 로또 게임 결과를 보여주는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
 import lotto.domain.LottoMachine;
+import lotto.domain.Rank;
+
+import java.util.List;
 
 public class OutputView {
 
@@ -12,5 +15,20 @@ public class OutputView {
         System.out.println(lottoMachine.getCount() + BUY_RESULT_MESSAGE);
         lottoMachine.getLottos().stream()
                 .forEach(lotto -> System.out.println(lotto));
+    }
+
+    public static void showGameResult(List<Rank> results, int inputMoney) {
+        System.out.println(STATISTIC_RESULT_MESSAGE);
+        System.out.println(LINE_CHANGE_MESSAGE);
+
+        for (Rank rank : Rank.getValidRanks()) {
+            System.out.println(rank.getMessage() + rank.calculateCount(results) + "개");
+        }
+
+        System.out.println("총 수익률은 " + String.format("%.1f", getEarningRate(Rank.calculateEarning(results), inputMoney)) + "%입니다.");
+    }
+
+    private static double getEarningRate(int profit, int inputMoney) {
+        return (double) (profit * 100) / inputMoney;
     }
 }
