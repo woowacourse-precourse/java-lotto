00035321bd755cc9c065696efc611153f9beb777
Egenieee <hhgg0925@naver.com>
2022-11-14 13:50:03 +0900

    feat: 입력받은 금액으로 총 몇 개의 로또를 구매했는지 계산하는 기능 추가

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,7 +1,14 @@
 package lotto;
 
 public class LottoGame {
+    private static final int ONE_LOTTO_PRICE = 1000;
+
     public void getLottoPrice() {
         int lottoPrice = InputView.getLottoPrice();
+        int lottoCount = getLottoCount(lottoPrice);
+    }
+
+    private int getLottoCount(int lottoPrice) {
+        return lottoPrice / ONE_LOTTO_PRICE;
     }
 }
