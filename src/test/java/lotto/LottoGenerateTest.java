eb863e6a78c0f97debc668b98652d5af2c1f99f1
JeongHunHui <deca3248@gmail.com>
2022-11-15 02:17:50 +0900

    style: 코드 포매팅

--- src/test/java/lotto/LottoGenerateTest.java
+++ src/test/java/lotto/LottoGenerateTest.java
@@ -9,10 +9,10 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static lotto.LottoGenerator.getRandomLottoListByCount;
 
 public class LottoGenerateTest {
-    @DisplayName("개수가 주어졌을 때 에러없이 로또들이 잘 생성이 되어야한다.")
-    @ParameterizedTest(name ="{index} {displayName} count:{0}")
-    @ValueSource(ints = {3,6,9})
-    void generateLottoListTest(int count){
-        assertDoesNotThrow(() -> getRandomLottoListByCount(count));
-    }
+  @DisplayName("개수가 주어졌을 때 에러없이 로또들이 잘 생성이 되어야한다.")
+  @ParameterizedTest(name = "{index} {displayName} count:{0}")
+  @ValueSource(ints = {3, 6, 9})
+  void generateLottoListTest(int count) {
+    assertDoesNotThrow(() -> getRandomLottoListByCount(count));
+  }
 }
