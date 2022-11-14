beb5a0c5eb6a4e6f8c94b37d9965afe9535aceae
ChanghyunKim <variety82p@gamil.com>
2022-11-14 22:28:45 +0900

    feat(Lotto) : 로또 중복 숫자 검증 메서드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static lotto.utils.Messages.ERROR_MESSAGE;
@@ -10,6 +12,7 @@ public class Lotto {
     private final List<Integer> numbers;
     public Lotto(List<Integer> numbers) {
         validateLottoSize(numbers);
+        validateDuplicateNumber(numbers);
         this.numbers = numbers;
     }
 
@@ -20,6 +23,12 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicateNumber(List<Integer> numbers){
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+        if(nonDuplicateNumbers.size() != 6){
+            throw new IllegalArgumentException(ERROR_MESSAGE + "중복된 숫자입니다.");
+        }
+    }
 
     public List<Integer> getLottoNumber(){
         return Collections.unmodifiableList(numbers);
