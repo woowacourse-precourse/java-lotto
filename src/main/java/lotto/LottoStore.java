ccc17b7c47050634beb4453081db09528a353da0
inhwanK <inhan1009@naver.com>
2022-11-14 23:38:18 +0900

    feat(LottoStore): 받은 금액에 맞게 발행 수 계산하는 메서드 및 개수 출력 메서드 추가

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -8,5 +8,16 @@ import java.util.Objects;
 
 public class LottoStore {
 
+    private static final Integer LOTTO_AMOUNT_EACH = 1000;
+
+    private int countLottos(Integer amount) {
+        Integer numOfLotto = amount / LOTTO_AMOUNT_EACH;
+        announceNumberOfIssuedLotto(numOfLotto);
+        return numOfLotto;
+    }
+
+    private void announceNumberOfIssuedLotto(Integer numOfLotto) {
+        System.out.printf("%d개를 구매했습니다.\n", numOfLotto);
+    }
 
 }
