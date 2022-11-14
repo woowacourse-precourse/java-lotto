e1100ac5fd1c61ab1d1b47f245ea99adb4d2dcdf
im-shung <seoyoung1113@gmail.com>
2022-11-15 02:51:46 +0900

    feat: :sparkles: 당첨 번호의 개수가 6개가 넘어가면 예외 발생

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -8,7 +8,20 @@ public class Player {
     private int bonusNumber;
 
     public Player(List<Integer> numbers, int bonusNumber) {
+        validate(numbers, bonusNumber);
         this.numbers = numbers;
         this.bonusNumber = bonusNumber;
     }
+
+    private void validate(List<Integer> numbers, int bonusNumber) {
+        checkSize(numbers);
+    }
+
+    private void checkSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
 }
