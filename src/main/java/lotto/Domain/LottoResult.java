ecc61c67aab27e205984ec354bea321df9a10346
sjkim <seoungjoo01@gmail.com>
2022-11-15 02:43:26 +0900

    feat : 6번 - 당첨 내역 계산 기능 구현

---
+++ src/main/java/lotto/Domain/LottoResult.java
@@ -0,0 +1,54 @@
+package lotto.Domain;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class LottoResult {
+    Map<Rank, Integer> result = new LinkedHashMap<>();
+
+    public LottoResult() {
+        for (Rank rank : Rank.values()) {
+            result.put(rank, 0);
+        }
+    }
+
+    public void calculateLottoRanks(LotteryTickets lotteryTickets, WinningNumber winningNumber) {
+
+        for (Lotto lotto : lotteryTickets.getLottoTickets()) {
+            boolean hasBonus = false;
+            int num = (int) lotto.getNumbers().stream().filter(x -> winningNumber.getWinningNumbers().contains(x)).count();
+            if (num == 5 && checkItHasBonus(lotto, winningNumber.getBonusNum())) {
+                hasBonus = true;
+            }
+            Rank rank = Rank.findByMatchCount(num, hasBonus);
+            result.put(rank, result.getOrDefault(rank, 0) + 1);
+        }
+
+    }
+
+    private boolean checkItHasBonus(Lotto lotto, int bonusNum) {
+        return lotto.getNumbers().contains(bonusNum);
+    }
+
+    public float calcMoney(int num) {
+        return getTotalMoney() / num;
+    }
+
+    public float getTotalMoney() {
+        return (float) result.entrySet()
+                .stream().
+                filter((Map.Entry<Rank, Integer> e) -> e.getValue() > 0).mapToLong(rank -> (long) rank.getKey().getPrice())
+                .sum();
+    }
+
+    public Map<Rank, Integer> getResult() {
+        return result.entrySet().stream()
+                .sorted(Comparator.comparing((Map.Entry<Rank, Integer> e) -> e.getKey().getPrice()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+    }
+
+    public void setResult(Map<Rank, Integer> result) {
+        this.result = result;
+    }
+}
---
+++ src/main/java/lotto/Domain/Rank.java
@@ -0,0 +1,43 @@
+package lotto.Domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    BOTTOM(0, 0);
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    private final int matchCount;
+    private final int price;
+
+    Rank(int matchCount, int price) {
+        this.matchCount = matchCount;
+        this.price = price;
+    }
+
+    public static Rank findByMatchCount(int num, boolean hasBonusNum) {
+        if (num < FIFTH.matchCount) {
+            return BOTTOM;
+        }
+        if (num == THIRD.matchCount && !hasBonusNum) {
+            return THIRD;
+        }
+        if (num == SECOND.matchCount && hasBonusNum) {
+            return SECOND;
+        }
+        return Arrays.stream(Rank.values()).filter(rank -> rank.getMatchCount() == num).findAny().orElse(BOTTOM);
+    }
+
+}
