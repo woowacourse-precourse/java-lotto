5adfdb08c615d681911872d287d9634f4f087c5a
jonghun <jonghuncu@gmail.com>
2022-11-14 22:00:15 +0900

    feat(lotto) : Lotto 메서드 당첨 숫자 입력 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,20 +6,17 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-//        InputUserMoney#InputMoney() - 사용자에게 구입금액을 입력
-//        UserNumberGenerator#createRandomNumbers() - 구입 금액 만큼 1-45숫자중 중복되지 않는 6개의 숫자를 생성
-//        CorrectNumberGenerator#createCorrectNumber() - 1-45 범위의 숫자 중 중복되지 않는 6개의 당첨번호를 생성
-//        BonusNumberGenerator#createBonusNumber() - 1-45 범위의 숫자 중 당첨번호와 중복되지 않는 보너스 번호를 입력
-//        CompareNumber#jugementNumber() - 구입한 내역의 숫자와 당첨숫자 비교 결과를 반환
 
         int InputMoney = PrintInputMoneyGenerator.printInputMoney();
         int InputMoenyForCal = InputMoney*1000;
 
+        // 사용자 구매시 당첨 숫자 출력
+
+
         List<Integer> returnNumber = CompareNumberWithBonus.jugementNumberWithBonus(InputMoney);
         PrintWinnerStatisticsGenerator.printWinner(returnNumber);
         int totalEarningMoney = EarningRateGenerator.printEarningRate(returnNumber);
+//        System.out.printf("구매금액은 %d원 입니다.\n", InputMoenyForCal);
         CalculateTotalEarningGenerator.calculateTotalEarning(InputMoenyForCal, totalEarningMoney);
-
-
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,7 +3,7 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<Integer> numbers; // 사용자가 생성하는 숫자들
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -17,4 +17,14 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+    public static List<Integer> CorrectLottoNumber() {
+        List<Integer> correctNumber = CorrectNumberGenerator.createCorrectNumber();
+        new Lotto(correctNumber);
+
+        return correctNumber;
+    }
+
+
+
 }
