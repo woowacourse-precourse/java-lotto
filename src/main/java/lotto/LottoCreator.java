6df0c4a73e6826fcf32ad6e64b0054f57718d9be
Egenieee <hhgg0925@naver.com>
2022-11-14 14:22:33 +0900

    feat: 구매 금액 만큼 로또 객체 생성하는 기능 추가
    - 로또 번호 오름차순으로 정렬하는 기능 추가

---
+++ src/main/java/lotto/LottoCreator.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LottoCreator {
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 45;
+
+    public static Lotto createLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, LOTTO_NUMBER_COUNT);
+        Collections.sort(numbers);
+        return new Lotto(numbers);
+    }
+}
--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,9 +1,22 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGame {
     private static final int ONE_LOTTO_PRICE = 1000;
 
     public static int getLottoCount(int lottoPrice) {
         return lottoPrice / ONE_LOTTO_PRICE;
+    }
+
+    public static List<Lotto> getLottos(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(LottoCreator.createLotto());
+        }
+
+        return lottos;
     }
 }
