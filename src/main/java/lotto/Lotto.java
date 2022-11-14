85052e008252fbfb7c620aea524e3cbdc25c4113
mardi2020 <mardi@khu.ac.kr>
2022-11-14 19:17:35 +0900

    feat(Lotto.java): 로또 번호 구매하기

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -25,5 +28,29 @@ public class Lotto {
         if (duplicated.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 중복된 숫자는 입력할 수 없습니다.");
         }
+    }
+
+    public static Lotto generateLotto() {
+        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> sortedLotto = lotto.stream().sorted().collect(Collectors.toList());
+
+        return new Lotto(sortedLotto);
+    }
+
+    public static List<Lotto> buyMyLottos(int count) {
+        System.out.println(count + "개를 구매했습니다.");
+        List<Lotto> buyLottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            buyLottos.add(Lotto.generateLotto());
+        }
+
+        for (Lotto buyLotto : buyLottos) {
+            buyLotto.printLotto();
+        }
+        return buyLottos;
+    }
+
+    public void printLotto() {
+        System.out.println(numbers);
     }
 }
