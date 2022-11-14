95ea12ccc02657d8a86fe2d5da10925480040314
lvalentine6 <ysr1227@naver.com>
2022-11-15 05:45:44 +0900

    feat : 로또 번호 비교 기능
    구매한 로또와 당첨 번호를 비교하여 순위를 반환하는 기능 구현

--- src/main/java/lotto/Compare.java
+++ src/main/java/lotto/Compare.java
@@ -1,4 +1,25 @@
 package lotto;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Compare {
+    static Map<Integer, Integer> compareLotto = new HashMap<>();
+
+    static void sliceBundle(List<Lotto> lottoBundle, List<Integer> winningNumber) {
+        for (int i = 0; i < lottoBundle.size(); i++) {
+            compareNumbers(lottoBundle.get(i), winningNumber);
+        }
+    }
+
+    public static void compareNumbers(Lotto lotto, List<Integer> winningNumber) {
+        int cnt = 0;
+        for (int i = 0; i < lotto.getNumbers().size(); i++) {
+            if (winningNumber.contains(lotto.getNumbers().get(i))) {
+                cnt++;
+            }
+        }
+        compareLotto.put(cnt, compareLotto.getOrDefault(cnt, 0) + 1);
+    }
 }
