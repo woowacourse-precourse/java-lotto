df98add7ef4ef7c02cc1e535d2befd3187f41631
TaemHam <tk4003950@gmail.com>
2022-11-14 11:51:11 +0900

    Feat: utils.DecimalFormatter 구현

---
+++ src/main/java/utils/DecimalFormatter.java
@@ -0,0 +1,11 @@
+package utils;
+
+import java.text.DecimalFormat;
+
+public class DecimalFormatter {
+    private static final DecimalFormat formatter = new DecimalFormat("###,###.#");
+
+    public static String formatNumber(Double number) {
+        return formatter.format(number);
+    }
+}
