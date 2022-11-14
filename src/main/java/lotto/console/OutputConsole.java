8452aebb0c855bdee99869a2329c644e7938707e
sujin0607 <goweg0607@gmail.com>
2022-11-14 21:20:56 +0900

    feat: 출력 클래스 생성 및 로또 개수 출력 메서드 추가

---
+++ src/main/java/lotto/console/OutputConsole.java
@@ -0,0 +1,13 @@
+package lotto.console;
+
+import java.util.List;
+
+/***
+ * 출력 클래스
+ */
+public class OutputConsole {
+    public void lottoCount(int lottoCount){
+        System.out.printf("%d개를 구매했습니다.\n",lottoCount);
+    }
+
+}
