064a10ed7d15962171efbb8d381a354d96fea656
song-wooseok <sws981202@gmail.com>
2022-11-14 16:46:10 +0900

    feat: 생성된 로또 번호들을 오름차순으로 정렬하는 기능

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,12 +8,21 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        sortNumber(numbers);
         this.numbers = numbers;
+    }
+
+    private void sortNumber(List<Integer> list){
+        Collections.sort(list);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+    @Override
+    public String toString(){
+        return numbers.toString();
     }
 }
