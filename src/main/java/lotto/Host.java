9380476109e302fe2cbbfe03bb6ebeafec955917
이한솔 <hansol8701@naver.com>
2022-11-14 16:06:58 +0900

    feat(Host): 스켈레톤 코드 작성

---
+++ src/main/java/lotto/Host.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import java.util.List;
+
+public class Host {
+
+    List<Integer> winNumbers;
+    int bonusNumber;
+
+    public void registerWinningInformation(){
+
+    }
+
+    public Lotto issueLotto(){
+        return null;
+    }
+
+    private boolean isIssued(List<Integer> numbers){
+        return false;
+    }
+}
