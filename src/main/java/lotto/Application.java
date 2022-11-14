833038a483a313523cb0b0e9a04f7bada2df3022
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-11-15 03:37:46 +0900

    feat: 시작 금액을 입력받기 위한 function 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,16 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
+    static final int lottoPrice = 1000;
+
+    public static Integer getMoney() {
+        int money = readMoney();
+        validMoney(money);
+        return money;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
