1c37f0bb2e525e41cbb7ea1e7d3ae1d02d5c39a6
korjun1993 <corojoon93@naver.com>
2022-11-14 19:07:21 +0900

    feat(Application) : 로또 구매 금액이 1,000원으로 나눠지는지 검사하는 기능 구현 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
 public class Application {
+    private static final int LOTTO_BUY_CURRENCY = 1000;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    /**
+     * 예외 처리를 위한 편의 기능
+     */
+    public static boolean isValidMoney(int money) {
+        return money % LOTTO_BUY_CURRENCY == 0;
     }
 }
