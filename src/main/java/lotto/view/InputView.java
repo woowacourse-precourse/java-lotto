991e9cef8b02a570176bfe8fa8a6443c1b4c8aaa
drunkenhw <drunkenhw@gmail.com>
2022-11-15 02:39:10 +0900

    feat(InputView): 1~45 사이의 숫자를 입력하지 않으면 예외를 발생하는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,32 +10,39 @@ public class InputView {
     public static int getMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
-        return convertToNumber(input);
+        return convertToLottoNumber(input);
     }
 
     public static List<Integer> getWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
         return Arrays.stream(input.split(","))
-                .map(InputView::convertToNumber)
+                .map(InputView::convertToLottoNumber)
                 .collect(Collectors.toList());
     }
 
     public static int getBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
-        return convertToNumber(input);
+        return convertToLottoNumber(input);
     }
 
-    private static int convertToNumber(String input) {
+    private static int convertToLottoNumber(String input) {
         validateNumber(input);
-        return Integer.parseInt(input);
+        int number = Integer.parseInt(input);
+        validateLottoNumber(number);
+        return number;
     }
 
     private static void validateNumber(String input) {
         if (!input.matches("^[0-9]*$") || input.isEmpty()) {
-            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요");
+            throw new IllegalArgumentException("숫자를 입력해주세요");
         }
     }
 
+    private static void validateLottoNumber(int input) {
+        if (input < 1 || input > 45) {
+            throw new IllegalArgumentException("1 ~ 45 사이의 숫자만 입력가능합니다.");
+        }
+    }
 }
