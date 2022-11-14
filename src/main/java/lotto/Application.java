d38fbe8db0681a38a735586dcf382533e36e72b3
kim0914 <kosa0914@naver.com>
2022-11-14 15:32:43 +0900

    feat(Application): 로또 게임을 실행하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.controller.GameController;
+import lotto.log.Log;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            GameController gameController = new GameController();
+            gameController.run();
+        } catch (IllegalArgumentException e) {
+            Log.error(e.getMessage());
+        }
     }
 }
