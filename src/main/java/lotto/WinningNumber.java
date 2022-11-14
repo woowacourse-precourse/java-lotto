e8f927780580cf87aa4c1f7282dad1b4035d2a4b
hdsung98 <hdsung98@gmail.com>
2022-11-14 17:55:10 +0900

    feat: create constructor and getters in Class WinningNumber

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -1,4 +1,32 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WinningNumber {
+    private Lotto mainNumbers;
+    private Integer bonusNumber;
+
+    public WinningNumber(List<Integer> announcedNumbers) {
+        List<Integer> materials = new ArrayList<>();
+        for(Integer announcedNumber : announcedNumbers){
+            materials.add(announcedNumber);
+        }
+        mainNumbers = new Lotto(materials);
+        bonusNumber = announcedNumbers.get(6);
+    }
+    public Lotto getMainNumber() {
+        List<Integer> materials = new ArrayList<>();
+        Lotto sendingLotto;
+        for(Integer number : mainNumbers.getLotto()){
+            materials.add(number);
+        }
+        sendingLotto = new Lotto(materials);
+        return sendingLotto;
+    }
+
+    public Integer getBonusNumber(){
+        Integer sendingBonus = this.bonusNumber;
+        return sendingBonus;
+    }
 }
