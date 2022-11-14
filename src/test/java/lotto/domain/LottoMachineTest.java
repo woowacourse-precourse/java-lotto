f69797b1e2097e12f6208f243f740a2890eb2899
youngkyunIm <yee0230@naver.com>
2022-11-14 18:55:58 +0900

    test(LottoMachine) : add check bill, normal

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,46 +1,59 @@
 package lotto.domain;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInListTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyList;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import lotto.Application;
+import lotto.domain.constants.ErrorCode;
+import lotto.domain.vo.Lotto;
+import lotto.domain.vo.Money;
+import lotto.veiw.InputHandler;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 class LottoMachineTest extends NsTest {
 
-    @Test
-    void LottoVendingMachine_Test_good() {
+
+    @DisplayName("Normal BuyLottoList Test")
+    @ParameterizedTest(name = "[{index}] input [{0}] ")
+    @ValueSource(strings = {"1,2,3,4,5,6", "1,10,20,30,40,45", "40,41,42,43,44,45"})
+    void LottoVendingMachine_Test(String inputString) {
+        InputHandler inputHandler = new InputHandler();
+
         assertRandomUniqueNumbersInRangeTest(
                 () -> {
-                    run("8000", "1,2,3,4,5,6", "7");
-                    assertThat(output()).contains(
-                            "8개를 구매했습니다.",
-                            "[8, 21, 23, 41, 42, 43]",
-                            "[3, 5, 11, 16, 32, 38]",
-                            "[7, 11, 16, 35, 36, 44]",
-                            "[1, 8, 11, 31, 41, 42]",
-                            "[13, 14, 16, 38, 42, 45]",
-                            "[7, 11, 30, 40, 42, 43]",
-                            "[2, 13, 22, 32, 38, 45]",
-                            "[1, 3, 5, 14, 22, 45]"
-                    );
+                    LottoMachine lottoMachine = new LottoMachine(new Money(1000));
+                    for (Lotto lotto : lottoMachine.getBuyLottoList()) {
+                        assertThat(lotto.getLottoNumbers()).isEqualTo(
+                                inputHandler.stringToList(inputString));
+                        System.out.println(lotto.getLottoNumbers());
+                    }
                 },
-                List.of(8, 21, 23, 41, 42, 43),
-                List.of(3, 5, 11, 16, 32, 38),
-                List.of(7, 11, 16, 35, 36, 44),
-                List.of(1, 8, 11, 31, 41, 42),
-                List.of(13, 14, 16, 38, 42, 45),
-                List.of(7, 11, 30, 40, 42, 43),
-                List.of(2, 13, 22, 32, 38, 45),
-                List.of(1, 3, 5, 14, 22, 45)
+                inputHandler.stringToList(inputString)
         );
+    }
+
+    @DisplayName("Unvalidated Bill")
+    @ParameterizedTest(name = "[{index}] input {0} ")
+    @ValueSource(ints = {500, 1234, 1001, 999})
+    public void validateRange_0or46_NOTINRANGE(int input) {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            new LottoMachine(new Money(input));
+        });
+        assertEquals(ErrorCode.NOT_BILL.getErrorMessage(), exception.getMessage());
     }
 
     @Override
     public void runMain() {
-        Application.main(new String[]{});
     }
 }
