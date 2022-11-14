c3f08beed3b363074ff119bd346507c6b8e69497
peterysh <52229539+peterysh@users.noreply.github.com>
2022-11-14 22:37:17 +0900

    feat: add bonus number error and price error message in ErrorMessage class

--- src/main/java/lotto/global/ErrorMessage.java
+++ src/main/java/lotto/global/ErrorMessage.java
@@ -4,4 +4,7 @@ public class ErrorMessage {
     public static String SERVER_INTERNAL_ERROR = "[ERROR] 서버 내부 오류입니다.";
     public static String NUMBER_INPUT_ERROR = "[ERROR] 로또 번호는 6자리 1부터 45 사이의 숫자만으로 ,로 구분되어야 합니다.";
     public static String NUMBER_MAKER_ERROR = "[ERROR] 로또 번호는 6자리 1부터 45 사이의 숫자만으로 구성 되어야 합니다.";
+    public static String BONUS_NUMBER_ERROR = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자 입니다.";
+
+    public static String UNMATCH_PRICE_UNIT = "[ERROR] 구입 금액은 천원 단위의 숫자 입니다.";
 }
