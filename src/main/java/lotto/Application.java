121b0f172df7fddeb6ba9570d427daf7247311c0
jmjmjames <chungjm0711@gmail.com>
2022-11-14 21:11:13 +0900

    feat(Application): controller, service 형식으로 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController(lottoService());
+        lottoController.run();
+    }
+    private static LottoService lottoService() {
+        return new LottoService();
     }
 }
