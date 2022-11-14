fee8d00715d51a6b5b6f07666c4d25d1f2b0983d
Yeongseon Park <yseon406@gmail.com>
2022-11-15 01:37:21 +0900

    test: compareNumbers Test 당첨 번호와 로또 번호를 비교하는 기능 테스트

--- src/test/java/lotto/RaffleTest.java
+++ src/test/java/lotto/RaffleTest.java
@@ -4,6 +4,8 @@ import lotto.domain.Raffle;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RaffleTest {
@@ -14,5 +16,14 @@ public class RaffleTest {
         String input = "1,2,3,4,5,6";
 
         assertThat(new Raffle().setWinningNumber(input)).contains(1,2,3,4,5,6);
+    }
+
+    @DisplayName("당첨 번호와 로또 번호를 비교한다")
+    @Test
+    void compareNumbersTest() {
+        List<Integer> lottoNumber = List.of(6,12,24,26,32,43);
+        List<Integer> winningNumber = List.of(2,6,10,12,25,32);
+        int result = 3;
+        assertThat(new Raffle().compareNumbers(lottoNumber, winningNumber)).isEqualTo(result);
     }
 }
