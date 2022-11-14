64ad2c408e2ac8cc4cbde5bea1478965b058e329
chaos0103 <lyt1228@naver.com>
2022-11-14 17:50:21 +0900

    refactor(Lotto): 테스트 중복 제거

--- src/test/java/lotto/domain/LottoServiceTest.java
+++ src/test/java/lotto/domain/LottoServiceTest.java
@@ -5,6 +5,8 @@ import lotto.Rank;
 import lotto.domain.impl.LottoServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.*;
 
@@ -17,59 +19,14 @@ class LottoServiceTest {
     private static final Lotto PRIZE_LOTTO = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
     private static final int BONUS_NUMBER = 7;
 
-    @Test
-    @DisplayName("6개 번호가 일치하면 1등이다.")
-    void lottoFirst() {
-        List<Lotto> userLottos = new ArrayList<>();
-        userLottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+    @ParameterizedTest
+    @CsvSource({"1,2,3,4,5,6,FIRST", "1,2,3,4,5,7,SECOND", "1,2,3,4,5,8,THIRD", "1,2,3,4,7,8,FOURTH", "1,2,3,7,8,9,FIFTH"})
+    void compare(int number1, int number2, int number3, int number4, int number5, int number6, Rank rank) {
+        List<Lotto> userLottos = List.of(new Lotto(Arrays.asList(number1, number2, number3, number4, number5, number6)));
 
         Map<Rank, Integer> results = lottoService.compare(userLottos, PRIZE_LOTTO, BONUS_NUMBER);
 
-        assertThat(results.getOrDefault(FIRST, 0)).isEqualTo(1);
-    }
-
-    @Test
-    @DisplayName("5개 번호와 보너스 번호가 일치하면 2등이다.")
-    void lottoSecond() {
-        List<Lotto> userLottos = new ArrayList<>();
-        userLottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7)));
-
-        Map<Rank, Integer> results = lottoService.compare(userLottos, PRIZE_LOTTO, BONUS_NUMBER);
-
-        assertThat(results.getOrDefault(SECOND, 0)).isEqualTo(1);
-    }
-
-    @Test
-    @DisplayName("5개 번호가 일치하면 3등이다.")
-    void lottoThird() {
-        List<Lotto> userLottos = new ArrayList<>();
-        userLottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 8)));
-
-        Map<Rank, Integer> results = lottoService.compare(userLottos, PRIZE_LOTTO, BONUS_NUMBER);
-
-        assertThat(results.getOrDefault(THIRD, 0)).isEqualTo(1);
-    }
-
-    @Test
-    @DisplayName("4개 번호가 일치하면 4등이다.")
-    void lottoFourth() {
-        List<Lotto> userLottos = new ArrayList<>();
-        userLottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 7, 8)));
-
-        Map<Rank, Integer> results = lottoService.compare(userLottos, PRIZE_LOTTO, BONUS_NUMBER);
-
-        assertThat(results.getOrDefault(FOURTH, 0)).isEqualTo(1);
-    }
-
-    @Test
-    @DisplayName("3개 번호가 일치하면 5등이다.")
-    void lottoFifth() {
-        List<Lotto> userLottos = new ArrayList<>();
-        userLottos.add(new Lotto(Arrays.asList(1, 2, 3, 7, 8, 9)));
-
-        Map<Rank, Integer> results = lottoService.compare(userLottos, PRIZE_LOTTO, BONUS_NUMBER);
-
-        assertThat(results.getOrDefault(FIFTH, 0)).isEqualTo(1);
+        assertThat(results.getOrDefault(rank, 0)).isEqualTo(1);
     }
 
     @Test
