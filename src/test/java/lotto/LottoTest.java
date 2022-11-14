a095566b22829d4f0e57e13467b99a8ffe3bf4f1
sangeun-b <baek0007@algonquinlive.com>
2022-11-15 07:35:12 +0900

    test: 로또 번호 범위 예외 발생 확인 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -25,4 +25,10 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1에서 45 사이가 아니면 예외가 발생한다.")
+    @Test
+    void createLottoNotBetweenRange(){
+        assertThatThrownBy(() -> new Lotto(List.of(0,1,2,3,4,5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
