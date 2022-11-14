dceb1f14654614437aee38f74833331c538cc6a2
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-14 13:02:08 +0900

    refactor(Application): modify to make the application run

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,10 +2,17 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        LottoConsole lottoConsole = new LottoConsole();
-        LottoGame lottoGame = new LottoGame(lottoConsole.getPurchaseAmount());
-        lottoConsole.printLottos(lottoGame.getLottos());
-        lottoGame.play(lottoConsole.getWinningLottoNumbers(), lottoConsole.getBonusLottoNumber());
-        lottoConsole.printResult(lottoGame.getLottoResult());
+        try {
+            LottoConsole lottoConsole = new LottoConsole();
+            LottoGame lottoGame = new LottoGame(lottoConsole.getPurchaseAmount());
+            lottoConsole.printBlank();
+            lottoConsole.printLottos(lottoGame.getLottos());
+            lottoConsole.printBlank();
+            lottoGame.play(lottoConsole.getWinningLottoNumbers(), lottoConsole.getBonusLottoNumber());
+            lottoConsole.printBlank();
+            lottoConsole.printResult(lottoGame.getResult());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
