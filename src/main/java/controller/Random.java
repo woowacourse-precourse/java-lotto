da08047e57b3d1941314f88eb096052a4d638849
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-14 23:59:30 +0900

    feat(Random) : add a function that returns unique 6 random numbers

---
+++ src/main/java/controller/Random.java
@@ -0,0 +1,13 @@
+package controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class Random {
+
+    public static List<Integer> getRandomNumber() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return randomNumbers;
+    }
+}
