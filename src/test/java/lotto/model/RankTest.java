0ed29451fb869c4b898d80c8ededa1658fba4755
김현재 <41482946+galug@users.noreply.github.com>
2022-11-14 17:44:45 +0900

    refactor(ranktest): 테스트 코드를 간결하게 수정
    CsvSource와 Parameterizedtest를 이용
      - 여러 개의 테스트 메서드를 같은 기능을 가지는 한개의 메서드로 축소

--- src/test/java/lotto/model/RankTest.java
+++ src/test/java/lotto/model/RankTest.java
@@ -3,6 +3,8 @@ package lotto.model;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class RankTest {
     private static final String FIRST_STATE = "6개 일치 (2,000,000,000원)";
@@ -11,72 +13,23 @@ class RankTest {
     private static final String FOURTH_STATE = "4개 일치 (50,000원)";
     private static final String FIFTH_STATE = "3개 일치 (5,000원)";
 
-    @DisplayName("맞춘 개수가 6개일 경우 1등을 반환하는지 확인한다.")
-    @Test
-    void createFirstRankWithMatchingNumber() {
-        Rank firstRank = Rank.of(6, false);
+    @DisplayName("맞춘 개수에 따른 등수를 반환한다. ")
+    @ParameterizedTest
+    @CsvSource({"6,false,FIRST", "5,true,SECOND", "5,false,THIRD",
+            "3,false,FIFTH", "2,false,NOTHING", "0,false,NOTHING"})
+    void createRankWithMatchingNumber(int matchingNumber, boolean hasBonusNumber, Rank rank) {
+        Rank firstRank = Rank.of(matchingNumber, hasBonusNumber);
 
-        Assertions.assertThat(firstRank).isEqualTo(Rank.FIRST);
-        Assertions.assertThat(firstRank.toString()).isEqualTo(FIRST_STATE);
+        Assertions.assertThat(firstRank).isEqualTo(rank);
+        Assertions.assertThat(firstRank.toString()).isEqualTo(rank.toString());
     }
 
-    @DisplayName("맞춘 개수가 5개 + 보너스 1개일 경우 1등을 반환하는지 확인한다.")
-    @Test
-    void createSecondRankWithMatchingNumber() {
-        Rank firstRank = Rank.of(5, true);
-
-        Assertions.assertThat(firstRank).isEqualTo(Rank.SECOND);
-        Assertions.assertThat(firstRank.toString()).isEqualTo(SECOND_STATE);
-    }
-
-    @DisplayName("맞춘 개수가 5개일 경우 3등을 반환하는지 확인한다.")
-    @Test
-    void createThirdRankWithMatchingNumber() {
-        Rank firstRank = Rank.of(5, false);
-
-        Assertions.assertThat(firstRank).isEqualTo(Rank.THIRD);
-        Assertions.assertThat(firstRank.toString()).isEqualTo(THIRD_STATE);
-    }
-
-    @DisplayName("맞춘 개수가 4개일 경우 4등을 반환하는지 확인한다.")
-    @Test
-    void createFourthRankWithMatchingNumber() {
-        Rank firstRank = Rank.of(4, false);
-
-        Assertions.assertThat(firstRank).isEqualTo(Rank.FOURTH);
-        Assertions.assertThat(firstRank.toString()).isEqualTo(FOURTH_STATE);
-    }
-
-    @DisplayName("맞춘 개수가 3개일 경우 5등을 반환하는지 확인한다.")
-    @Test
-    void createFifthRankWithMatchingNumber() {
-        Rank firstRank = Rank.of(3, false);
-
-        Assertions.assertThat(firstRank).isEqualTo(Rank.FIFTH);
-        Assertions.assertThat(firstRank.toString()).isEqualTo(FIFTH_STATE);
-    }
-
-    @DisplayName("맞춘 개수가 2개일 경우 꼴등임을 반환하는지 확인한다.")
-    @Test
-    void createNothingRankWithTwoMatchingNumber() {
-        Rank firstRank = Rank.of(2, false);
-
-        Assertions.assertThat(firstRank).isEqualTo(Rank.NOTHING);
-    }
-
-    @DisplayName("맞춘 개수가 1개일 경우 꼴등임을 반환하는지 확인한다.")
-    @Test
-    void createNothingRankWithOneMatchingNumber() {
-        Rank firstRank = Rank.of(1, false);
-
-        Assertions.assertThat(firstRank).isEqualTo(Rank.NOTHING);
-    }
-
-    @DisplayName("맞춘 개수가 0개일 경우 꼴등임을 반환하는지 확인한다.")
-    @Test
-    void createNothingRankWithZeroMatchingNumber() {
-        Rank firstRank = Rank.of(0, false);
-
-        Assertions.assertThat(firstRank).isEqualTo(Rank.NOTHING);
+    @DisplayName("맞춘 개수에 따른 상금의 합을 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"FIRST, 1, 2000000000", "FIRST,2,4000000000",
+            "SECOND, 1, 30000000", "SECOND, 2, 60000000",
+            "THIRD, 1, 1500000", "THIRD, 2, 3000000"})
+    public void computePrice(Rank rank, int numberOfRank, long result) {
+        Assertions.assertThat(rank.computePrice(numberOfRank)).isEqualTo(result);
     }
 }
