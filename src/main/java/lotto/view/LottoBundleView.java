b46bc680fd65bd4a2b426526388d9743b48167be
TaemHam <tk4003950@gmail.com>
2022-11-14 13:37:31 +0900

    Refactor: COUNT_MESSAGE를 MessageType으로 이동

--- src/main/java/lotto/view/LottoBundleView.java
+++ src/main/java/lotto/view/LottoBundleView.java
@@ -4,17 +4,16 @@ import java.util.List;
 import java.util.StringJoiner;
 import lotto.data.dto.LottoBundleDto;
 import lotto.data.dto.LottoDto;
+import lotto.type.MessageType;
 
 public class LottoBundleView {
 
     public LottoBundleView() {}
 
-    private final String COUNT_MESSAGE = "%d개를 구입하셨습니다.";
-
     public String stringify(LottoBundleDto lottoBundleDto) {
         StringJoiner stringJoiner = new StringJoiner("\n");
         List<LottoDto> lottos = lottoBundleDto.getLottos();
-        stringJoiner.add(String.format(COUNT_MESSAGE, lottos.size()));
+        stringJoiner.add(String.format(MessageType.COUNT_MESSAGE.getMessage(), lottos.size()));
         lottos.forEach(lottoDto -> stringJoiner.add(lottoDto.getNumbers().toString()));
         return stringJoiner.toString();
     }
