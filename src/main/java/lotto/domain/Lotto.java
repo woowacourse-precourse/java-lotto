966e8798dad9204ca795aa07f1dab4dddf007021
daeun <gain3436@gmail.com>
2022-11-14 16:04:57 +0900

    feat : 로또번호 중복 예외 처리, 범위 예외 처리, 개수 예외 처리 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -15,11 +16,28 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        checkSize(numbers);
+        checkDuplicate(numbers);
+        checkRange(numbers);
+    }
+
+    private void checkSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해 주세요.");
         }
     }
 
+    private void checkDuplicate(List<Integer> numbers) {
+        if(numbers.size() != numbers.stream().distinct().count()){
+            throw new IllegalArgumentException("[ERROR] 중복되지 않은 6개의 숫자를 입력해 주세요.");
+        }
+    }
 
-    // TODO: 추가 기능 구현
+    private void checkRange(List<Integer> numbers) {
+        List<Integer> outRangeList = numbers.stream().filter(n -> 1 > n || n > 45)
+                .collect(Collectors.toList());
+        if(outRangeList.size() > 0) {
+            throw new IllegalArgumentException("[ERROR] 1~45 사이의 숫자를 입력해 주세요.");
+        }
+    }
 }
