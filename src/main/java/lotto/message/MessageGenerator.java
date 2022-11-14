ec15a761fb584b6e35c4456290be752f7744fc55
wonju-dev <wonjulee.dev@gmail.com>
2022-11-15 03:24:10 +0900

    feat(MessageGenerator): 로또 추첨 결과 표시 기능 구현

--- src/main/java/lotto/message/MessageGenerator.java
+++ src/main/java/lotto/message/MessageGenerator.java
@@ -1,9 +1,32 @@
-package lotto;
+package lotto.message;
+
+import lotto.userinterface.Hit;
 
 public class MessageGenerator {
     private static final String PURCHASE_COUNT_MESSAGE_TEMPLATE = "개를 구매했습니다.";
 
-    public static String getPurchaseCountMessage(Integer count) {
+    public static String getPurchaseConfirmMessage(Integer count) {
         return count + PURCHASE_COUNT_MESSAGE_TEMPLATE;
+    }
+
+    public static String getEarningRateMessage(Float earningRate) {
+        return "총 수익률은 " + String.format("%.1f", earningRate) + "%입니다.";
+    }
+
+    public static String getLottoResultMessage(Hit hit, Integer count) {
+        String message = "";
+        int hitCount = hit.getHitCount();
+
+        message += hitCount + "개 일치";
+
+        if (hit.requireBonus()) {
+            message += ", 보너스 볼 일치";
+        }
+
+        message += " (" + hit.getPrize() + "원)";
+
+        message += " - " + count + "개";
+
+        return message;
     }
 }
