3440e49a777716d2b4786ee06b230ded83c3d946
wnsgh12s <junho1273@gmail.com>
2022-11-14 18:58:26 +0900

    Test: 당첨번호 입력값 체크 테스트 추가

--- src/test/java/lotto/domain/UserInputCheckTest.java
+++ src/test/java/lotto/domain/UserInputCheckTest.java
@@ -23,7 +23,7 @@ class UserInputCheckTest {
     }
 
     @Test
-    void 입력값이_숫자_인지_체크() {
+    void 문자열의_입력값이_숫자_인지_체크() {
         UserInputCheck.purchaseAmountCheck("1200K");
         assertThat(output.toString()).contains("[ERROR] 숫자가 아닌 값이 포함되어 있습니다.");
     }
@@ -38,5 +38,23 @@ class UserInputCheckTest {
     void 입력값이_천원으로_나누어_떨어지는지_체크() {
         UserInputCheck.purchaseAmountCheck("1201");
         assertThat(output.toString()).contains("[ERROR] 천원으로 나누어 떨어지지 않습니다.");
+    }
+
+    @Test
+    void 입력값의_길이가_6인지_체크() {
+        UserInputCheck.winningNumberCheck("1,2,3,4,5");
+        assertThat(output.toString()).contains("[ERROR] 길이가 6이 아닙니다.");
+    }
+
+    @Test
+    void 리스트_자료형의_입력값이_숫자_인지_체크() {
+        UserInputCheck.winningNumberCheck("1,2,3,4,5,#");
+        assertThat(output.toString()).contains("[ERROR] 숫자가 아닌 값이 포함되어 있습니다.");
+    }
+
+    @Test
+    void 입력값의_범위가_유효한지_체크() {
+        UserInputCheck.winningNumberCheck("1,2,3,4,5,49");
+        assertThat(output.toString()).contains("[ERROR] 유효한 범위가 아닙니다.");
     }
 }
