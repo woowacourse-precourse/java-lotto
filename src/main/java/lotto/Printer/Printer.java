6b2b804aa3442731795ee6f0e1a28ba3f9db9060
채희찬 <eqholic1125@gmail.com>
2022-11-14 22:51:11 +0900

    feat: 구매금액 입력 요청 기능 구현

---
+++ src/main/java/lotto/Printer/Printer.java
@@ -0,0 +1,12 @@
+package lotto.Printer;
+
+public class Printer {
+    private static final String PURCHASE_AMOUNT_REQUEST_MESSAGE = "구매금액을 입력해 주세요.";
+
+
+    public void requestPurchaseAmount(){
+        System.out.println(PURCHASE_AMOUNT_REQUEST_MESSAGE);
+    }
+
+
+}
