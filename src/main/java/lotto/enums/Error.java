5eaa3a762a2ce413c5b07b94d3a3cecd207bd826
yxnxx <jh4rgh0@gmail.com>
2022-11-15 00:33:52 +0900

    feat(WinningNumbers): set winning lotto numbers

--- src/main/java/lotto/enums/Error.java
+++ src/main/java/lotto/enums/Error.java
@@ -5,7 +5,9 @@ public enum Error {
     PURCHASE_MIN("구매 금액이 1000원 미만입니다."),
     PURCHASE_UNIT("구매 금액은 1000원 단위로만 입력 가능합니다."),
     SIZE("로또 번호는 6개의 숫자로만 이루어질 수 있습니다."),
-    DUPLICATE("로또 번호는 중복 값을 허용하지 않습니다.");
+    DUPLICATE("로또 번호는 중복 값을 허용하지 않습니다."),
+    LOTTO_NUMERIC("로또 번호는 숫자로만 입력 가능합니다."),
+    LOTTO_RANGE("로또 번호는 1에서 45 사이의 숫자로만 이루어질 수 있습니다.");
 
     private static final String DEFAULT_MESSAGE = "[ERROR] ";
     private final String msg;
--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -1,14 +1,51 @@
 package lotto.model;
 
+import lotto.enums.Error;
 import lotto.view.InputView;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumbers {
     private static List<Integer> winningNumbers;
+    private static final int MAX_NUM = 45;
+    private static final int MIN_NUM = 1;
+    private static final int NUM_SIZE = 6;
 
     public void setWinningNumbers() {
         String input = InputView.getWinningNumbers();
+        winningNumbers = splitNumbers(input);
+    }
+    private List<Integer> splitNumbers(String input) {
+        List<String> winningNumSplit = List.of(input.split(","));
+        List<Integer> newNumbers = new ArrayList<>();
+        for(String num: winningNumSplit) {
+            try{
+                newNumbers.add(Integer.parseInt(num));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(Error.LOTTO_NUMERIC.getError());
+            }
+        }
 
+        isNumbersValid(newNumbers);
+        isDuplicate(newNumbers);
+        return newNumbers;
+    }
+    private void isNumbersValid(List<Integer> numbers) {
+        if(numbers.size() != NUM_SIZE) throw new IllegalArgumentException(Error.SIZE.getError());
+
+        for(Integer num: numbers) {
+            if((num > MAX_NUM) || (num < MIN_NUM)) {
+                throw new IllegalArgumentException(Error.LOTTO_RANGE.getError());
+            }
+        }
+    }
+    private void isDuplicate(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != numbers.size()) {
+            throw new IllegalArgumentException(Error.DUPLICATE.getError());
+        }
     }
 }
