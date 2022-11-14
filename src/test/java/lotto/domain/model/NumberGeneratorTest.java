ec8c76bb22b806f371edd22d948195b70ff9786e
임원정 <oddish0513@gmail.com>
2022-11-14 23:05:09 +0900

    test(lotto): 중복 없는 랜덤 수 생성 테스트

---
+++ src/test/java/lotto/domain/model/NumberGeneratorTest.java
@@ -0,0 +1,20 @@
+package lotto.domain.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberGeneratorTest {
+
+	@Test
+	void 정상작동_테스트(){
+		NumberGenerator numberGenerator = new NumberGenerator();
+		List<Integer> randomNumbers = numberGenerator.createRandomNumbers();
+
+		HashSet<Integer> numberSet = new HashSet<>(randomNumbers);
+		assertEquals(numberSet.size(), randomNumbers.size());
+	}
+}
