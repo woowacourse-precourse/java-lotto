e1746596efa4715a30be89434e003f23d2e3ad18
KSH <tkdgns543@gmail.com>
2022-11-15 06:34:19 +0900

    refactor(Application): 당첨 통계와 출력 부분을 따로 메서드 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -13,23 +13,27 @@ public class Application {
     static NumbersReceiver numbersReceiver = new NumbersReceiver();
     static Buyer buyer = new Buyer();
     static OutputPrinter outputPrinter = new OutputPrinter();
+    static Lotto lotto;
+
     public static void main(String[] args) {
         try {
             long startMoney = numbersReceiver.insertMoney();
-
             List<List<Integer>> purchasedLottos = buyer.receiveLottos(startMoney);
             outputPrinter.printPurchasedNumbers(purchasedLottos);
 
             List<Integer> basicWinningNumbers = numbersReceiver.setBasicNumbers();
-            Lotto lotto = new Lotto(basicWinningNumbers);
+            lotto = new Lotto(basicWinningNumbers);
             int bonusNumber = numbersReceiver.setBonusNumber();
 
-            Map<WinningNumbers, Long> winningCaseStatistics = lotto.countWinningCase(bonusNumber, purchasedLottos);
-            double rawRewardRate = lotto.calculateRewardRate(bonusNumber, purchasedLottos, startMoney);
-            outputPrinter.printFinalResult(winningCaseStatistics, rawRewardRate);
-
+            executeLotto(bonusNumber, purchasedLottos, startMoney);
         } catch (IllegalArgumentException illegalArgumentException) {
             System.out.println(illegalArgumentException.getMessage());
         }
+    }
+
+    private static void executeLotto(int bonusNumber, List<List<Integer>> purchasedLottos, long startMoney) {
+        Map<WinningNumbers, Long> winningCaseStatistics = lotto.countWinningCase(bonusNumber, purchasedLottos);
+        double rawRewardRate = lotto.calculateRewardRate(bonusNumber, purchasedLottos, startMoney);
+        outputPrinter.printFinalResult(winningCaseStatistics, rawRewardRate);
     }
 }
