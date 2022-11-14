d37b18372d17ca899d4c1901454e51b076fba089
yujaemin <woawoa0513@gmail.com>
2022-11-14 23:17:02 +0900

    feat: 로또 당첨 번호와 비교 함수 구현
    로또 하나를 당첨 번호와 비교하여 등 수를 반환한다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -7,6 +7,22 @@ import lotto.domain.WinningNumber;
 import java.util.List;
 
 public class LottoService {
+    private static final int NUMBER_OF_RANKING = Ranking.values().length;
+
+    public int compareWithWinningNumbers(Lotto lotto, WinningNumber winningNumber) {
+        List<Integer> winningNumbers = winningNumber.getNumbers();
+        int bonusNumber = winningNumber.getBonusNumber();
+
+        int matchingNumberCount = compareWithoutBonusNumber(lotto, winningNumbers);
+
+        if (matchingNumberCount == Ranking.FIRST.getMatchingNumberCount()) {
+            return Ranking.FIRST.getPlace();
+        }
+        if (matchingNumberCount == Ranking.SECOND.getMatchingNumberCount() && hasBonusNumber(lotto, bonusNumber)) {
+            return Ranking.SECOND.getPlace();
+        }
+        return NUMBER_OF_RANKING + 3 - matchingNumberCount;
+    }
 
     public int compareWithoutBonusNumber(Lotto lotto, List<Integer> winningNumbers) {
         int matchingNumberCount = 0;
