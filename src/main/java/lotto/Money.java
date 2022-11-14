2549fd765e60ad56abd4f2447688a99db4664e2f
vcho1958 <gusckd57@gmail.com>
2022-11-15 04:18:56 +0900

    feat(Money): 금액을 입력받는 Money 클래스 정의 생성 시 값을 검사하고 올바르지 않으면 IllegalArgumentException 예외를 발생시킨다. - amount가 음수일 경우 - amount가 1000으로 나누어 떨어지지 않을 경우
    구입할 수 있는 로또의 개수를 반환하는 getAmountOfLotto 메소드를 정의한다.
    - amount / 1000의 값을 반환

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,18 @@
+package lotto;
+
+public class Money {
+    Long amount;
+    public Money(Long amount){
+        validate(amount);
+        this.amount = amount;
+    }
+
+    public Long getAmountOfLotto(){
+        return this.amount/1000;
+    }
+
+    private void validate(Long amount){
+        if(amount < 0)throw new IllegalArgumentException("[ERROR] 구입 금액이 양수가 아닙니다.");
+        if(amount % 1000 != 0)throw new IllegalArgumentException("[ERROR] 구입 금액이 천원단위가 아닙니다.");
+    }
+}
