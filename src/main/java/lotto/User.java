9d20b555faca0ccf8e14618e1830af687b713c1a
jw0293 <k38836919@gmail.com>
2022-11-14 11:10:37 +0900

    feat : Init User Information to input [입력을 통한 사용자 정보 설정 기능]

---
+++ src/main/java/lotto/User.java
@@ -0,0 +1,42 @@
+package lotto;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private static final String INFORMATION_MESSAGE = "구입금액을 입력해주세요.";
+    private static final String INFORMATION_LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
+
+    private int lottoCount;
+    private int price;
+    private final Exception exception;
+    private List<List<Integer>> randomLottoNumbers;
+
+    public User(){
+        exception = new Exception();
+        randomLottoNumbers = new ArrayList<>();
+    }
+
+    public void setPrice(){
+        System.out.println(INFORMATION_MESSAGE);
+        price = Integer.parseInt(Console.readLine());
+    }
+
+    public int getPrice(){
+        return this.price;
+    }
+
+    public void setLottoCount(){
+        exception.validatePrice(this.price);
+        this.lottoCount = price / 1000;
+        System.out.println(lottoCount + INFORMATION_LOTTO_COUNT_MESSAGE);
+    }
+
+    public int getLottoCount(){
+        return this.lottoCount;
+    }
+
+}
