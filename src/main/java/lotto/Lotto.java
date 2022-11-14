46e864ce6a9a0f443e5c92e1c2441a39122e84ec
gudals <kimtuti@naver.com>
2022-11-14 19:55:13 +0900

    feat(lotto) : add winning amount calculator function
    총 상금액을 구하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -28,6 +28,35 @@ public class Lotto {
      *
      * @return
      */
+    public void lottoOutput(){
+        //복권 당첨 확인
+
+        //복권 당첨 금액 확인
+
+        //수익율 계산
+    }
+    private Integer totalProfit(HashMap<Integer, Integer> winNumbers){
+        int result=0;
+        HashMap<Integer, Integer> winningMoney = getwinningMoney();
+        for (int key :winNumbers.keySet()){
+            result += winNumbers.get(key)*winningMoney.get(key);
+        }
+        return result;
+    }
+    private HashMap<Integer, Integer> getwinningMoney(){
+        HashMap<Integer, Integer> winningMoney = new HashMap<>();
+        winningMoney.put(0,0);
+        winningMoney.put(1,0);
+        winningMoney.put(2,0);
+        winningMoney.put(3,5000);//5
+        winningMoney.put(4,50000);//4
+        winningMoney.put(5,1500000);//3
+        winningMoney.put(6,2000000000);//1
+        winningMoney.put(7,30000000);//2
+        return winningMoney;
+    }
+
+
     public HashMap<Integer, Integer> gameConfirm(List<List<Integer>> lottoList, int bonus){
         HashMap<Integer, Integer> winNumbers = getwinnerSetMap();
         for (List<Integer> lottoNum : lottoList) {
