c13fe1a9aa4c9ee308a70d8d18542400339a7b40
areyouhun <areyouhun@gmail.com>
2022-11-15 06:36:39 +0900

    refactor(statistics): 당첨 통계 구하는 메서드 수정

--- src/main/java/lotto/consumer/Statistics.java
+++ src/main/java/lotto/consumer/Statistics.java
@@ -4,27 +4,37 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import lotto.machine.Ranks;
-import lotto.util.Calculator;
 
 public class Statistics {
+    private final static int INITIAL_COUNT = 0;
+
     private final Map<Integer, Integer> statistics;
 
-    public Statistics(Profit profit) {
-        statistics = createStatisticsBy(profit.getProfit());
+    public Statistics(List<Integer> totalProfit) {
+        statistics = createStatistics();
+        updateStatistics(totalProfit);
     }
 
     public Map<Integer, Integer> getStatistics() {
         return statistics;
     }
 
-    private Map<Integer, Integer> createStatisticsBy(List<Integer> profit) {
-        int initialWinCount = 0;
-        Map<Integer, Integer> initialStatistics = new LinkedHashMap<>();
-        for (Ranks ranks : Ranks.values()) {
-            initialStatistics.put(ranks.getPrizeMoney(), initialWinCount);
+    private void updateStatistics(List<Integer> totalProfit) {
+        for (Integer profit : totalProfit) {
+            if (statistics.containsKey(profit)) {
+                int winCount = statistics.get(profit);
+                statistics.replace(profit, winCount + 1);
+            }
         }
+    }
 
-        Calculator.updateStatistics(initialStatistics, profit);
+    private Map<Integer, Integer> createStatistics() {
+        int winCount = INITIAL_COUNT;
+        final Map<Integer, Integer> initialStatistics = new LinkedHashMap<>();
+
+        for (Ranks ranks : Ranks.values()) {
+            initialStatistics.put(ranks.getPrizeMoney(), winCount);
+        }
         return initialStatistics;
     }
 }
