e5c34ff01408e26ff7766651633670a6440459a6
baekeunsun <eunsun5460@gmail.com>
2022-11-14 18:38:26 +0900

    feat(controller/Yield): 수익률 소수점 둘째 자리에서 반올림하여 출력
    - (Yield/YieldLotto): 수익률 소수점 둘째자리에서 반올림하여 계산

--- src/main/java/lotto/controller/Yield.java
+++ src/main/java/lotto/controller/Yield.java
@@ -1,15 +1,22 @@
 package lotto.controller;
 
+import lotto.view.OutputView;
+
 public class Yield {
-    int yield;
+    double yield;
 
     public Yield(int userPrice, Compare userCompare) {
         int totalRevenue = SumWinLotto(userCompare.fifth, userCompare.fourth, userCompare.third, userCompare.second, userCompare.first);
-        yield = (totalRevenue/userPrice)*100;
+        yield = YieldLotto(totalRevenue, userPrice);
+        OutputView.OutputYield(yield);
     }
 
     public int SumWinLotto(int fifth, int fourth, int third, int second, int first) {
         int totalRevenue = (fifth*5000) + (fourth*50000) + (third*1500000) + (second*30000000) + (first*2000000000);
         return totalRevenue;
+    }
+
+    private double YieldLotto(int totalRevenue, int userPrice) {
+        return Math.round((totalRevenue / (double) userPrice * 100) * 10) / 10.0;
     }
 }
