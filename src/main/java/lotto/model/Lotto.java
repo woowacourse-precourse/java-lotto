102fe2598b19ac27a44e82fda44175974505497e
ParangBird <seekerman@naver.com>
2022-11-15 02:21:38 +0900

    feat: 로또 출력 오름차순

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,6 +3,7 @@ package lotto.model;
 import lotto.service.LottoService;
 import lotto.view.OutputView;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -29,6 +30,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        Collections.sort(this.numbers);
     }
 
     private void validate(List<Integer> numbers) {
