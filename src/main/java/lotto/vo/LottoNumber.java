86a8fab12e2bb74a38c41022dcdbdd9c8ef6e1a2
TaeHwan <kimta2hwan@gmail.com>
2022-11-14 16:32:16 +0900

    feat: 범위 밖의 번호 존재 시 예외 발생 기능 구현

--- src/main/java/lotto/vo/LottoNumber.java
+++ src/main/java/lotto/vo/LottoNumber.java
@@ -4,10 +4,21 @@ import java.util.Objects;
 
 public class LottoNumber {
 
+    private static final String LOTTO_NUMBER_ERROR_MESSAGE = "[ERROR] 로또의 번호는 1에서 45의 수이어야 합니다.";
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
     private final int number;
 
-    public LottoNumber(final int  number) {
+    public LottoNumber(final int number) {
+        validatePositiveLottoNumber(number);
         this.number = number;
+    }
+
+    private void validatePositiveLottoNumber(final int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_ERROR_MESSAGE);
+        }
     }
 
     public int getLottoNumber() {
--- src/test/java/lotto/vo/LottoNumberTest.java
+++ src/test/java/lotto/vo/LottoNumberTest.java
@@ -1,5 +1,28 @@
 package lotto.vo;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class LottoNumberTest {
 
+    @ParameterizedTest(name = "{displayName} {index}")
+    @ValueSource(ints = {-1, 0, 46})
+    @DisplayName("예외 발생 테스트")
+    public void exceptionTest() {
+        int lottoNumber = 0;
+
+        assertThatThrownBy(() -> new LottoNumber(lottoNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "{displayName} {index}")
+    @ValueSource(ints = {1, 25, 45})
+    @DisplayName("정상 실행 테스트")
+    public void normalTest(int number) {
+        assertDoesNotThrow(() -> new LottoNumber(number));
+    }
 }
