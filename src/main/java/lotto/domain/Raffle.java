fe3a90caf3c189fe553332a2dde77a2bfb12e715
Yeongseon Park <yseon406@gmail.com>
2022-11-15 01:32:14 +0900

    feat(lotto): 로또 번호와 당첨 번호 비교 구현

--- src/main/java/lotto/domain/Raffle.java
+++ src/main/java/lotto/domain/Raffle.java
@@ -14,7 +14,7 @@ public class Raffle {
 
     public List<Integer> setWinningNumber(String userInput) {
         List<Integer> winningNumber = new ArrayList<>();
-        for(String number : userInput.split(",")){
+        for (String number : userInput.split(",")) {
             winningNumber.add(Integer.valueOf(number));
         }
         return winningNumber;
@@ -23,5 +23,15 @@ public class Raffle {
     public int inputBonusNumber() {
         String userInput = Console.readLine();
         return Integer.parseInt(userInput);
+    }
+
+    public int compareNumbers(List<Integer> lotto, List<Integer> winningNumber) {
+        int count = 0;
+        for (int number = 0; number < 6; number++) {
+            if (winningNumber.contains(lotto.get(number))) {
+                count++;
+            }
+        }
+        return count;
     }
 }
