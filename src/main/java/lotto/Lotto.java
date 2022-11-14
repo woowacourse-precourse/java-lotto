a4b3e4d3a9cfb73c72129510ba18eaa619933754
Seokhyun Lee <lseokhyun99@gmail.com>
2022-11-14 21:01:51 +0900

    refactor: 하드 코딩된 값 상수화
    하드 코딩되었던 값들을
    static final을 이용해 상수화하였다.
    
    lottoRanking() 메소드의
    조건문 중 winningNumberCount와 비교하는 부분은
    정수를 그대로 사용하는 것이 의도가 더 잘 보이는 것 같아
    상수화하지 않았다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,17 @@ package lotto;
 import java.util.*;
 
 public class Lotto {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    private static final String LOTTO_SIZE_ERROR = ERROR_MESSAGE + " 로또 번호는 6개 이어야 합니다.";
+    private static final String DUPLICATION_ERROR = ERROR_MESSAGE + " 로또 번호 6개는 서로 다른 숫자이어야 합니다.";
+    private static final int LOTTO_SIZE = 6;
+    private static final int RANK1 = 1;
+    private static final int RANK2 = 2;
+    private static final int RANK3 = 3;
+    private static final int RANK4 = 4;
+    private static final int RANK5 = 5;
+    private static final int NOT_WIN = 0;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,40 +23,43 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(LOTTO_SIZE_ERROR);
         }
     }
 
     // TODO: 추가 기능 구현
-    public int lottoRanking(List<Integer> winningNumber, Integer bonusNumber){
+    public int lottoRanking(List<Integer> winningNumber, Integer bonusNumber) {
         int winningNumberCount = howManyWinningNumbers(winningNumber);
         boolean haveBonusNum = haveBonusNumber(bonusNumber);
 
-        if(winningNumberCount == 6) return 1;
-        if((winningNumberCount == 5) && haveBonusNum ) return 2;
-        if(winningNumberCount == 5) return 3;
-        if(winningNumberCount == 4) return 4;
-        if(winningNumberCount == 3) return 5;
+        if (winningNumberCount == 6) return RANK1;
+        if ((winningNumberCount == 5) && haveBonusNum) return RANK2;
+        if ((winningNumberCount == 5) && !haveBonusNum) return RANK3;
+        if (winningNumberCount == 4) return RANK4;
+        if (winningNumberCount == 3) return RANK5;
 
-        return 0;
+        return NOT_WIN;
     }
-    private int howManyWinningNumbers(List<Integer> winningNumber){
+
+    private int howManyWinningNumbers(List<Integer> winningNumber) {
         int winningNumberCount = 0;
 
-        for(int i = 0 ; i < winningNumber.size() ; i++)
-            if(numbers.contains(winningNumber.get(i)))
+        for (int i = 0; i < winningNumber.size(); i++)
+            if (numbers.contains(winningNumber.get(i)))
                 winningNumberCount++;
 
         return winningNumberCount;
     }
-    private boolean haveBonusNumber(Integer bonusNumber){
-        if(numbers.contains(bonusNumber))
+
+    private boolean haveBonusNumber(Integer bonusNumber) {
+        if (numbers.contains(bonusNumber))
             return true;
 
         return false;
     }
-    public void printSortedLottoNumbers(){
+
+    public void printSortedLottoNumbers() {
         List<Integer> sortedNumbers = new ArrayList<>();
 
         sortedNumbers.addAll(this.numbers);
@@ -53,9 +67,10 @@ public class Lotto {
 
         System.out.println(sortedNumbers);
     }
+
     private void duplicateValidation(List<Integer> numbers) {
         Set<Integer> duplicationCheck = new HashSet<>(numbers);
-        if ( duplicationCheck.size() != numbers.size() )
-            throw new IllegalArgumentException("[ERROR] 로또 번호 6개는 서로 다른 숫자이어야 합니다.");
+        if (duplicationCheck.size() != numbers.size())
+            throw new IllegalArgumentException(DUPLICATION_ERROR);
     }
 }
