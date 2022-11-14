15ec83844a1db9ca828a7eb89b46dd86199582af
Cyma-s <ssol0319@naver.com>
2022-11-15 00:30:12 +0900

    feat(LottoGame): add a function that makes user lottos

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -7,22 +7,30 @@ import java.util.List;
 import lotto.model.Constant;
 import lotto.model.Lotto;
 import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
 
 public class LottoGame {
     private final LottoInputView lottoInputView;
+    private final LottoOutputView lottoOutputView;
     private final List<Lotto> lottos;
 
     public LottoGame() {
         this.lottoInputView = new LottoInputView();
+        this.lottoOutputView = new LottoOutputView();
         this.lottos = new ArrayList<>();
     }
     public void startLottoGame() {
         int userMoney = lottoInputView.getLottoAmount();
         issueLotto(userMoney);
+        lottoOutputView.printLottoCount(userMoney, lottos);
     }
 
     public void issueLotto(int userMoney) {
         int lottoPaperCount = userMoney / 1000;
+        makeUserLottos(lottoPaperCount);
+    }
+
+    public void makeUserLottos(int lottoPaperCount) {
         for(int i = 0; i<lottoPaperCount; i++) {
             List<Integer> randomLottoNumbers = makeRandomLottoNumbers();
             Lotto newUserLotto = new Lotto(randomLottoNumbers);
