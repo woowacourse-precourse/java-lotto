47ad4c718c8b6b7b12688f0a370aedfb6f5f0b09
xxeol2 <jhj01177@naver.com>
2022-11-14 23:00:27 +0900

    test: 로또 번호는 범위에 속한다

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,6 +3,7 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import lotto.exception.LottoNumberException;
 import lotto.exception.WrongLengthException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,10 +24,21 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호의 개수가 5개 이하이면 예외가 발생한다..")
+    @DisplayName("로또 번호의 개수가 5개 이하이면 예외가 발생한다.")
     @Test
     void createLottoByUnderSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
                 .isInstanceOf(WrongLengthException.class);
+    }
+
+    @DisplayName("로또 번호는 1이상 45 이하이다.")
+    @Test
+    void createLottoOutOfRange() {
+        assertThatThrownBy(() -> new LottoNumber(0))
+                .isInstanceOf(LottoNumberException.class);
+        assertThatThrownBy(() -> new LottoNumber(-1))
+                .isInstanceOf(LottoNumberException.class);
+        assertThatThrownBy(() -> new LottoNumber(46))
+                .isInstanceOf(LottoNumberException.class);
     }
 }
