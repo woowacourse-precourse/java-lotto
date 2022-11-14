6e6ce06a26eaa9c6612d9e2157b2db1623d23359
wwwndus <simock22@gmail.com>
2022-11-15 01:07:29 +0900

    feat(Compare): 일치하는 숫자 개수에 따라 count 증가

--- src/main/java/lotto/model/Compare.java
+++ src/main/java/lotto/model/Compare.java
@@ -1,4 +1,25 @@
 package lotto.model;
 
+import java.util.List;
+
 public class Compare {
+
+    public int[] getResult(int inputMoney, List<List<Integer>> computerRandomNumbers, String[] numbers, int bonusNumber ) {
+
+        int[] result = new int[5];
+        int count;
+
+        for (int i = 0; i < (inputMoney/1000) ; i++) {
+            count = 0;
+            count = getContainingNumber(computerRandomNumbers, numbers, count, i);
+        }
+        return result;
+    }
+
+    public int getContainingNumber(List<List<Integer>> computerRandomNumbers, String[] numbers, int count, int i) {
+        for (int j = 0; j < 6 ; j++) {
+            if (computerRandomNumbers.get(i).contains(Integer.parseInt(numbers[j]))) count++;
+        }
+        return count;
+    }
 }
