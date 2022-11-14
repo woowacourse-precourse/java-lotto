79f5807748e933f885e10fa1d58236c9568cd0aa
zeunala <zeunala@naver.com>
2022-11-15 01:54:42 +0900

    feat: 1~45의 중복 없는 조건 불만족시 예외 발생 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -8,14 +9,36 @@ public class Lotto {
     public static final int LOTTO_SIZE = 6; // 로또 한세트의 번호수
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    /**
+     * Lotto 생성자
+     *
+     * @param numbers Integer로 된 로또 번호 리스트
+     * @throws IllegalArgumentException 숫자의 범위나 개수가 유효하지 않거나 중복되는 숫자가 있을 경우
+     */
+    public Lotto(List<Integer> numbers) throws IllegalArgumentException {
         validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    /**
+     * 유효하지 않은 로또 번호가 들어올 경우 예외 발생
+     *
+     * @param numbers 검사할 로또 번호 List
+     * @throws IllegalArgumentException 숫자의 범위나 개수가 유효하지 않거나 중복되는 숫자가 있을 경우
+     */
+    private void validate(List<Integer> numbers) throws IllegalArgumentException {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(lotto.domain.LottoErrorCode.INVALID_LOTTO_SIZE.toString());
+        }
+
+        if (new HashSet<Integer>(numbers).size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(lotto.domain.LottoErrorCode.DUPLICATE_LOTTO_NUMBER.toString());
+        }
+
+        for (Integer number : numbers) {
+            if (number < LOTTO_START_NUMBER || number > LOTTO_END_NUMBER) {
+                throw new IllegalArgumentException(lotto.domain.LottoErrorCode.INVALID_LOTTO_RANGE.toString());
+            }
         }
     }
 
--- src/main/java/lotto/domain/LottoAnswer.java
+++ src/main/java/lotto/domain/LottoAnswer.java
@@ -8,25 +8,61 @@ public class LottoAnswer {
     private Lotto winningNumber;
     private Integer bonusNumber;
 
+    /**
+     * LottoAnswer 생성자
+     *
+     * @param numbersCommand 사용자의 당첨 번호 입력
+     * @param bonusCommand   사용자의 보너스 번호 입력
+     * @throws IllegalArgumentException 숫자 범위와 개수가 맞지 않거나, 숫자가 중복된 경우
+     */
     public LottoAnswer(String numbersCommand, String bonusCommand) throws IllegalArgumentException {
         List<Integer> numbers = commandToNumbers(numbersCommand);
         Integer bonus = commandToBonus(bonusCommand);
 
+        if (numbers.contains(bonus)) {
+            throw new IllegalArgumentException(LottoErrorCode.DUPLICATE_LOTTO_NUMBER.toString());
+        }
         winningNumber = new Lotto(numbers);
         bonusNumber = bonus;
     }
 
+    /**
+     * 사용자의 당첨 번호 입력을 Integer 리스트로 반환
+     *
+     * @param numbersCommand 쉼표(,)로 구분된 당첨 번호 입력
+     * @return Integer로 변환한 당첨 번호 리스트
+     * @throws IllegalArgumentException 숫자가 아닌 값이 있을 경우
+     */
     private List<Integer> commandToNumbers(String numbersCommand) throws IllegalArgumentException {
         List<String> numbersString = Arrays.asList(numbersCommand.split(","));
-        List<Integer> numbers = numbersString.stream()
-                .map(s -> Integer.parseInt(s))
-                .collect(Collectors.toList());
+        List<Integer> numbers;
+
+        try {
+            numbers = numbersString.stream()
+                    .map(s -> Integer.parseInt(s))
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(LottoErrorCode.INVALID_LOTTO_RANGE.toString());
+        }
 
         return numbers;
     }
 
+    /**
+     * 사용자의 보너스 번호 입력을 Integer로 반환
+     *
+     * @param bonusCommand 사용자의 보너스 번호 입력
+     * @return Integer로 변환한 보너스 번호
+     * @throws IllegalArgumentException 숫자가 아닌 값일 경우
+     */
     private Integer commandToBonus(String bonusCommand) throws IllegalArgumentException {
-        Integer bonus = Integer.parseInt(bonusCommand);
+        Integer bonus = 0;
+
+        try {
+            Integer.parseInt(bonusCommand);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(LottoErrorCode.INVALID_LOTTO_RANGE.toString());
+        }
 
         return bonus;
     }
