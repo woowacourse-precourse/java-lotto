16867945226a5c082e55d74085624c22a7feb475
zcae1020 <zcae1020@naver.com>
2022-11-14 10:47:48 +0900

    feat(lotto): 로또 등수를 나타내는 enum 클래스 구현

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,6 +1,17 @@
 package lotto;
 
 public enum Rank {
+    THREE(3, 5000),
+    FOUR(4, 50_000),
+    FIVE(5, 1_500_000),
+    BONUS(5, 30_000_000),
+    SIX(6, 2_000_000_000);
 
+    private final int matchingCount;
+    private final int prize;
 
+    Rank(int matchingCount, int prize) {
+        this.matchingCount = matchingCount;
+        this.prize = prize;
+    }
 }
