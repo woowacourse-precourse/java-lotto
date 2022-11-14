99b854bd847e8f9f781f3b276c8f246cd2b02a92
vcho1958 <gusckd57@gmail.com>
2022-11-15 04:29:03 +0900

    test(Money): Money 생성 테스트

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -14,4 +14,18 @@ class MoneyTest {
         Assertions.assertThatThrownBy(()->new Money(Long.valueOf(-1000)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("구매 금액이 1000으로 나누어 떨어지지 않을 때 예외를 반환합니다.")
+    @Test
+    void createMoneyWithNotDevidedNumberWithThousand(){
+        Assertions.assertThatThrownBy(()->new Money(Long.valueOf(1023)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("생성 테스트")
+    @Test
+    void createMoney(){
+        Assertions.assertThat(new Money(Long.valueOf(2000)))
+                .isInstanceOf(Money.class);
+    }
 }
