5f86ddce8463910317bf6e59f8590e040f047914
GwansuShin <happysun7080@naver.com>
2022-11-15 00:12:40 +0900

    refactor(Lotto,LottoValidator): Number 클래스 상속 제거
    기존에 상속했던 Number 클래스 제거
    검증 메소드를 Lotto 클래스로 이동
    NumberValidator -> LottoValidator로 이름 변경

--- src/main/java/lotto/user/Lotto.java
+++ src/main/java/lotto/user/Lotto.java
@@ -2,7 +2,6 @@ package lotto.user;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.util.Utilities;
-import lotto.util.Validator;
 
 import java.util.List;
 
@@ -10,7 +9,6 @@ public class Lotto {
     private static final int START_INCLUSIVE = 1;
     private static final int END_INCLUSIVE = 45;
     private static final int NUMBERS_SIZE = 6;
-
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -18,18 +16,31 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    private void validate(List<Integer> numbers) {
+        LottoValidator.validateNotNull(numbers);
+        LottoValidator.validateNumbersSize(numbers);
+        LottoValidator.validateNumbersNotRepeated(numbers);
+        LottoValidator.validateNumbersInRange(numbers);
+    }
+
     public static Lotto generate() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, NUMBERS_SIZE);
         return new Lotto(Utilities.sort(numbers));
     }
 
-    private static void validate(List<Integer> numbers) {
-        if (!Validator.isNotNull(numbers)
-                || !Validator.isListInSize(numbers, NUMBERS_SIZE)
-                || !LottoValidator.isNumbersNotRepeated(numbers)
-                || !LottoValidator.isNumbersInRange(numbers, START_INCLUSIVE, END_INCLUSIVE)) {
-            throw new IllegalArgumentException();
-        }
+    public static Lotto generate(String numbers) {
+        return new Lotto(Utilities.convertToList(numbers));
+    }
+
+    public int calculateDraw(Lotto draw) {
+        return (int) numbers
+                .stream()
+                .filter(draw.numbers::contains)
+                .count();
+    }
+
+    public int calculateBonus(Lotto draw, int bonus) {
+        return 1;
     }
 
     @Override
--- src/main/java/lotto/user/LottoValidator.java
+++ src/main/java/lotto/user/LottoValidator.java
@@ -1,19 +1,33 @@
 package lotto.user;
 
+import lotto.util.ErrorHandler;
+import lotto.util.ErrorMessage;
 import lotto.util.Validator;
 
 import java.util.List;
 
 public class LottoValidator extends Validator {
-    public static boolean isNumbersNotRepeated(List<Integer> numbers) {
-        return numbers
-                .stream()
-                .distinct()
-                .count() == numbers.size();
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 45;
+    private static final int NUMBERS_SIZE = 6;
+
+    public static void validateNumbersSize(List<Integer> list) {
+        if (list.size() != NUMBERS_SIZE) {
+            ErrorHandler.throwException(ErrorMessage.LOTTO_SIZE_EXCEEDED);
+        }
     }
 
-    public static boolean isNumbersInRange(List<Integer> numbers, int startInclusive, int endInclusive) {
-        return numbers
+    public static void validateNumbersNotRepeated(List<Integer> numbers) {
+        if (numbers
+                .stream()
+                .distinct()
+                .count() != numbers.size()) {
+            ErrorHandler.throwException(ErrorMessage.LOTTO_NUMBERS_REPEATED);
+        }
+    }
+
+    public static void validateNumbersInRange(List<Integer> numbers) {
+        if (numbers
                 .stream()
                 .anyMatch(number -> (number < START_INCLUSIVE || number > END_INCLUSIVE))) {
             ErrorHandler.throwException(ErrorMessage.LOTTO_NUMBERS_NOT_IN_RANGE);
