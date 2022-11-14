97a3cd4e6f2c7624617b60d1baeb3374f60cd8b5
ckn1070 <ckn1070@naver.com>
2022-11-15 03:28:27 +0900

    chore(lotto): convert real number form of float to double

--- src/main/java/lotto/IOManage.java
+++ src/main/java/lotto/IOManage.java
@@ -3,9 +3,10 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
-import java.util.regex.Pattern;
 
 public class IOManage {
+    private final ControlLottoGame control = new ControlLottoGame();
+
     private int inputPrice;
     private int quantity;
     private List<List<Integer>> generatedLotto;
@@ -25,20 +26,15 @@ public class IOManage {
     public boolean inputPayment() {
         System.out.println(Message.INPUT_PAYMENT.get());
         String input = Console.readLine();
-        try {
-            if (!Pattern.matches("^[0-9]*$", input)) {
-                throw new IllegalArgumentException(Message.ERROR.get() + Message.ERROR_INCORRECT_RANGE.get());
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return false;
+
+        if (control.validateNumber(input)){
+            inputPrice = Integer.parseInt(input);
+            return true;
         }
-        inputPrice = Integer.parseInt(input);
-        return true;
+        return false;
     }
 
     public void outputLottoGeneration() {
-        ControlLottoGame control = new ControlLottoGame();
         quantity = control.priceToQuantity(inputPrice);
 
         GenerateLotto generation = new GenerateLotto(quantity);
@@ -51,30 +47,27 @@ public class IOManage {
     }
 
     public void inputWinningNumbers() {
-        ControlLottoGame control = new ControlLottoGame();
         System.out.println(Message.INPUT_WINNING_NUMBER.get());
 
         String inputString = Console.readLine();
-        List<Integer> winningNumbers = control.inputToWinningNumbers(inputString);
+        List<Integer> numbers = control.inputToWinningNumbers(inputString);
 
-        Lotto lotto = new Lotto(winningNumbers);
-        winningNumbers = lotto.getNumbers();
-        this.winningNumbers = winningNumbers;
+        Lotto lotto = new Lotto(numbers);
+        numbers = lotto.getNumbers();
+        winningNumbers = numbers;
     }
 
     public void inputBonusNumber() {
-        ControlLottoGame control = new ControlLottoGame();
         System.out.println(Message.INPUT_BONUS_NUMBER.get());
 
         String inputString = Console.readLine();
-        this.bonusNumber = control.inputToBonusNumber(inputString, winningNumbers);
+        bonusNumber = control.inputToBonusNumber(inputString, winningNumbers);
     }
 
     public void outputStatistic() {
         CheckWinningLotto check = new CheckWinningLotto();
-        System.out.println(Message.OUTPUT_STATISTICS.get());
-
         List<Integer> winningCount = check.totalWinningCount(generatedLotto, winningNumbers, bonusNumber);
+        System.out.println(Message.OUTPUT_STATISTICS.get());
 
         System.out.println(Message.THREE_CORRECT.get() + winningCount.get(0) + Message.COUNT.get());
         System.out.println(Message.FOUR_CORRECT.get() + winningCount.get(1) + Message.COUNT.get());
@@ -84,10 +77,9 @@ public class IOManage {
     }
 
     public void outputEarningRate() {
-        ControlLottoGame control = new ControlLottoGame();
         CheckWinningLotto check = new CheckWinningLotto();
         List<Integer> winningCount = check.totalWinningCount(generatedLotto, winningNumbers, bonusNumber);
-        float earningRate = control.calculateEarningRate(winningCount, quantity);
+        double earningRate = control.calculateEarningRate(winningCount, quantity);
         System.out.println(Message.EARNING_RATE.get() + earningRate + Message.ENDING.get());
     }
 }
