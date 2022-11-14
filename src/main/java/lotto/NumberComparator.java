a935f6689737c6f16b718a2615af73ee158e1c53
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-11-14 20:35:19 +0900

    <feat>NumberComparator : 전반적인 기능 수정
    기능 요구사항에 대해 오해를 해 기능을 수정했습니다.

--- src/main/java/lotto/NumberComparator.java
+++ src/main/java/lotto/NumberComparator.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class NumberComparator {
     List<Integer> number = new ArrayList<>();
+    int bonus;
     List<Integer> result = new ArrayList<>();
 
     NumberComparator() {
@@ -30,19 +31,28 @@ public class NumberComparator {
         validateWinningNumber();
     }
 
-    void setWholeNumber() {
+    void setBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        String number = Console.readLine();
-
-        this.number.add(Integer.parseInt(number));
-        validateWholeNumber();
+        bonus = Integer.parseInt(Console.readLine());
+        validateBonus();
     }
 
     void compareNumbers(List<Lotto> bundle) {
         for (int index = 0; index < bundle.size(); index++) {
             List<Integer> lottoNumber = new ArrayList<>(bundle.get(index).get());
+            int count;
+
             lottoNumber.retainAll(number);
-            result.add(lottoNumber.size());
+            count = lottoNumber.size();
+            if(count == 6) {
+                count++;
+            }
+            if(count == 5) {
+                if (bundle.get(index).get().contains(bonus)) {
+                    count++;
+                }
+            }
+            result.add(count);
         }
     }
 
@@ -58,15 +68,15 @@ public class NumberComparator {
         }
     }
 
-    void validateWholeNumber() {
-        if (this.number.size() != 7) {
-            throw new IllegalArgumentException("[ERROR] 입력한 당첨 번호의 개수가 7개가 아닙니다.");
+    void validateBonus() {
+        List<Integer> wholeNumber = new ArrayList<>(number);
+
+        wholeNumber.add(bonus);
+        if (bonus< 1 || bonus > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
-        if (Collections.min(this.number) < 1 || Collections.max(this.number) > 45) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-        if (this.number.size() != this.number.stream().distinct().count()) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호에 중복된 숫자가 있습니다.");
+        if (wholeNumber.size() != wholeNumber.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.");
         }
     }
 }
