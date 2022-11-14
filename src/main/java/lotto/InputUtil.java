e1ac8c559881e0dc06cebe693c34dffdf4563871
JeongHunHui <deca3248@gmail.com>
2022-11-15 03:08:36 +0900

    feat: 사용자에게 문자열을 받아서 로또로 변환하는 메소드 구현

--- src/main/java/lotto/InputUtil.java
+++ src/main/java/lotto/InputUtil.java
@@ -2,9 +2,17 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputUtil {
   public static int getIntegerInput() {
     return stringToInt(Console.readLine());
+  }
+
+  public static Lotto getPrizeLotto(){
+    return stringToLotto(Console.readLine());
   }
 
   private static int stringToInt(String s) {
@@ -13,5 +21,20 @@ public class InputUtil {
     } catch (NumberFormatException e) {
       throw new IllegalArgumentException("숫자가 아닙니다.");
     }
+  }
+
+  private static Lotto stringToLotto(String str) {
+    List<String> strList = Arrays.asList(str.split(","));
+    return new Lotto(stringListToIntegerList(strList));
+  }
+
+  private static List<Integer> stringListToIntegerList(List<String> strList){
+    List<Integer> intList;
+    try {
+      intList = strList.stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("숫자가 아닙니다.");
+    }
+    return intList;
   }
 }
