839cfe61efa06ef6c000cedb0f200e5d5220ddc8
yujaemin <woawoa0513@gmail.com>
2022-11-14 22:51:48 +0900

    feat: 로또와 당첨 번호 보너스 번호 제외하고 비교 함수 구현
    하나의 로또를 보너스 번호를 제외하고 당첨 번호와 비교하여 몇 개의 번호가 일치하는지 찾은 뒤 일치하는 번호의 개수를 반환한다.

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,20 @@
+package lotto.service;
+
+import lotto.domain.Lotto;
+import lotto.domain.Ranking;
+import lotto.domain.WinningNumber;
+
+import java.util.List;
+
+public class LottoService {
+
+    public int compareWithoutBonusNumber(Lotto lotto, List<Integer> winningNumbers) {
+        int matchingNumberCount = 0;
+        for (int playerNumber : lotto.getNumbers()) {
+            if (winningNumbers.contains(playerNumber)) {
+                matchingNumberCount++;
+            }
+        }
+        return matchingNumberCount;
+    }
+}
