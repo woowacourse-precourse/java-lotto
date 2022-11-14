854acca9cc64215e5686d28ca77cfd7403a21ad9
yoon <beatus96@naver.com>
2022-11-15 03:47:39 +0900

    refactor(UserLottos) : 생성자 수정
    - 변수들을 상수화하고, 생성시 값이 초기화 되도록 생성자 수정하였다.
    
    - 매개변수로 입력받은, money를 변수로 저장하였다.

--- src/main/java/lotto/domain/UserLottos.java
+++ src/main/java/lotto/domain/UserLottos.java
@@ -7,41 +7,47 @@ import java.util.Set;
 
 public class UserLottos {
     private final int LOTTO_PRICE = 1000;
-    private int count;
-    private Set<Lotto> userLottos;
+    private final int count;
+    private final int money;
+    private final Set<Lotto> userLottos;
 
     public UserLottos(int money) {
         validateMoney(money);
-        setCount(money);
-        createLottos(count);
+        this.money = money;
+        this.count = setCount(money);
+        this.userLottos = createLottos(count);
     }
 
     public void validateMoney(int money) {
-        if(money < LOTTO_PRICE || money%LOTTO_PRICE !=0) {
+        if (money < LOTTO_PRICE || money % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException();
         }
     }
 
-    public void setCount(int money) {
-        this.count = money/LOTTO_PRICE;
+    public int setCount(int money) {
+        return money / LOTTO_PRICE;
     }
 
-    public void createLottos(int count) {
-        userLottos = new HashSet<>();
+    public Set<Lotto> createLottos(int count) {
+        Set<Lotto> userLottos = new HashSet<>();
 
         while (userLottos.size() != count) {
             Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             userLottos.add(lotto);
         }
+        return userLottos;
     }
 
+    //getter 모음
     public Set<Lotto> getUserLottos() {
-
         return userLottos;
     }
 
     public int getLottoCount() {
-
         return count;
+    }
+
+    public int getUserLottosMoney() {
+        return money;
     }
 }
