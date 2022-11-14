2687237c4f97c910a532e7c5a581aa087b17fdfa
impati <impati0716@icloud.com>
2022-11-14 22:34:48 +0900

    feat(Inputvalidation) : 보너스 번호 검증 기능

--- src/main/java/lotto/InputValidation.java
+++ src/main/java/lotto/InputValidation.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 public class InputValidation {
     public static final String NUMBER_FORMAT_MESSAGE = "[ERROR] 숫자만 입력해주세요.";
     public static final String COMMA_FORMAT_MESSAGE = "[ERROR] 당첨 번호는 ','로 구분해주세요.";
+    public static final String BONUS_MESSAGE = "[ERROR] 보너스 번호는 당첨 번호와 다른 숫자여야합니다.";
     public static boolean numberFormatExceptionValidation(String stringNumber){
         try{
             Integer.parseInt(stringNumber);
@@ -20,7 +21,11 @@ public class InputValidation {
                 .filter(element -> numberFormatExceptionValidation(element))
                 .map(element -> Integer.parseInt(element))
                 .collect(Collectors.toList()).size();
-        if(numberSize != LottoValidation.LOTTO_COUNT) throw new IllegalArgumentException(NUMBER_FORMAT_MESSAGE);
+        if(numberSize != LottoValidation.LOTTO_COUNT) throw new IllegalArgumentException(COMMA_FORMAT_MESSAGE);
+    }
+    public static void bonusNumberValidation(String bonus , Lotto winningNumber){
+        numberFormatExceptionValidation(bonus);
+        if(winningNumber.getNumbers().contains(bonus)) throw new IllegalArgumentException(BONUS_MESSAGE);
     }
 }
 
