eb95ca2194fb8c1044dade3365f9fcb993253f1a
Haaae <101785628+Haaae@users.noreply.github.com>
2022-11-14 15:25:30 +0900

    feat(Application): 기능에 따른 main 메서드의 흐름 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,32 @@
 package lotto;
 
+import lotto.domain.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 사용자에게 복권 구입 금액을 입력받는다.
+        int ticketPrice = getTicketPrice();
+        Publisher publisher = new Publisher(ticketPrice);
+        // 구입하는 복권 수만큼 복권 번호를 사용자로부터 입력받는다.
+        issueLotteries(publisher);
+        // 구입하는 복권 수량과 정렬된 복권 번호를 출력한다.
+        UI.printLotteries(publisher.getLotteries());
+        // 복권 추첨을 시행하고 결과를 확인한다.
+        List<Integer> result = publisher.make();
+        // 결과를 출력한다.
+        UI.printResult(result);
+
+    }
+    static int getTicketPrice() throws IllegalArgumentException {
+       return 0;
+    }
+
+    static void issueLotteries(Publisher publisher) {
+        while (publisher.getLotteries().size() < publisher.getTicketNumber()) {
+
+        }
     }
 }
