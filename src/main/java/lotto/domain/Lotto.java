83deb7a3cd12639a40334060adf9ec6994c72c9e
ChanghyunKim <variety82p@gamil.com>
2022-11-14 22:26:00 +0900

    refactor(Lotto) : 로또 사이즈 검정 메서드 이름 수정
    validate -> validateLottoSize

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,23 +4,32 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static lotto.utils.Messages.ERROR_MESSAGE;
+
 public class Lotto {
     private final List<Integer> numbers;
-
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoSize(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateLottoSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MESSAGE + "6자리가 아닙니다");
         }
     }
 
     // TODO: 추가 기능 구현
+
     public List<Integer> getLottoNumber(){
         return Collections.unmodifiableList(numbers);
     }
 
+
+//    public int correctLottoNumber(Lotto winningLotto, Lotto lotto){
+//        int correctCount;
+//        for(int i = 0; i < 6; i++){
+//            if()
+//        }
+//    }
 }
