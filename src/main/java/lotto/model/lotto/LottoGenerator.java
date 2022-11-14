d09c02ccb8b8e76db5c8cedbaa4ae44968598343
ESeokhwan <huan99@hanmail.net>
2022-11-14 12:54:15 +0900

    feat: LottoGenerator 추가

--- src/main/java/lotto/model/lotto/LottoGenerator.java
+++ src/main/java/lotto/model/lotto/LottoGenerator.java
@@ -1,10 +1,16 @@
 package lotto.model.lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class LottoGenerator {
     public static Lotto autoGenerate() {
-        // TODO 구현 필요
-        return new Lotto(new ArrayList<>());
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(Lotto.MINIMUM_NUMBER, Lotto.MAXIMUM_NUMBER, Lotto.LOTTO_NUMBER_LENGTH));
+
+        numbers.sort(Comparator.naturalOrder());
+        return new Lotto(numbers);
     }
 }
