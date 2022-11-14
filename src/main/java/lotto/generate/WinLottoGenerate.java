e898401cb8d66f7e90d0e197e5500d35f60bf9c0
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-14 22:55:11 +0900

    feat: askWinNumber 메소드 작성

--- src/main/java/lotto/generate/WinLottoGenerate.java
+++ src/main/java/lotto/generate/WinLottoGenerate.java
@@ -1,6 +1,12 @@
 package lotto.generate;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
 import lotto.enums.StringEnum;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static lotto.enums.StringEnum.*;
 
@@ -8,4 +14,16 @@ public class WinLottoGenerate {
     public void askWinNumberPrint() {
         System.out.println(WIN_NUMBER.getMessage());
     }
+
+    public void askBonusNumberPrint() {
+        System.out.println(StringEnum.BONUS_NUMBER.getMessage());
+    }
+    private Lotto askWinNumber() {
+        askWinNumberPrint();
+        String winNumberInput = Console.readLine();
+        String[] winNumber = winNumberInput.split(",");
+        List<Integer> numbers = Arrays.stream(winNumber).map(Integer::parseInt).collect(Collectors.toList());
+        return new Lotto(numbers);
+    }
+
 }
