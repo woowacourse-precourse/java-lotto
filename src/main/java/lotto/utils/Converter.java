d62a7fcb8f85b9a5f504428ff5fe39ce2121275d
yujaemin <woawoa0513@gmail.com>
2022-11-14 21:08:37 +0900

    feat: 로또 구매 금액 로또 개수로 변환 함수 구현
    사용자가 입력한 로또 구매 금액을 로또의 개수로 변환한다. (1,000원당 로또 한 장)

--- src/main/java/lotto/utils/Converter.java
+++ src/main/java/lotto/utils/Converter.java
@@ -1,5 +1,7 @@
 package lotto.utils;
 
+import static lotto.utils.Constants.LOTTO_PRICE;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,5 +10,9 @@ public class Converter {
     public static List<Integer> convertToIntegerList(String input) {
         String[] numbers = input.split(",");
         return Arrays.stream(numbers).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    public static int convertToCountsOfLotto(int lottoPurchaseAmount) {
+        return lottoPurchaseAmount / LOTTO_PRICE;
     }
 }
