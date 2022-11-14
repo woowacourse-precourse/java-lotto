b0c95632adf5d9c42f86b8ad7f3e401bd0f5285e
saint6839 <saint6839@gmail.com>
2022-11-14 17:35:14 +0900

    test(domain) : 불필요 import문 제거

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
