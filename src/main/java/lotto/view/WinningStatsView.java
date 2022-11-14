b7a73c1cb62b9c0a1bcc9cb867f4949cf9ab966b
jjmin9797 <jjmin9797@naver.com>
2022-11-14 15:37:26 +0900

    feat(WinningStatsView.java): 당첨 통계 헤더 출력 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

---
+++ src/main/java/lotto/view/WinningStatsView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class WinningStatsView {
+
+    public static final String WINNING_STATS_MESSAGE = "당첨 통계";
+
+    public static void showWinningStatsMessage() {
+        System.out.println(WINNING_STATS_MESSAGE);
+    }
+}
