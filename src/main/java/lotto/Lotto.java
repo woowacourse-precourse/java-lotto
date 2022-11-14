6cf0a669fed3f3dfcfeecfe465836f705205de92
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-11-15 02:00:03 +0900

    fix: lotto의 final 변수에 대한 sort 오류 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,8 +8,8 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        numbers = sortAscendingOrder(numbers);
         this.numbers = numbers;
-        sortAscendingOrder();
     }
 
     private void validate(List<Integer> numbers) {
@@ -34,12 +34,14 @@ public class Lotto {
         return 0;
     }
 
-    private void sortAscendingOrder(){
-        this.numbers.sort(new Comparator<Integer>() {
+    private List<Integer> sortAscendingOrder(List<Integer> numbers){
+        numbers.sort(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
                 return o1-o2;
             }
         });
+
+        return numbers;
     }
 }
