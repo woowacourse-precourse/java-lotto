6e8e33faaf73e434f6f9890193f447b6873d7d28
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-11-14 23:16:05 +0900

    feat: add input unit check
    - 구매 금액 단위를 검사하는 메소드 추가

--- src/main/java/lotto/logic/LottoAmount.java
+++ src/main/java/lotto/logic/LottoAmount.java
@@ -4,6 +4,14 @@ public class LottoAmount {
     public static final Integer LOTTO_PRICE = 1000;
 
     public static Integer getLottoAmount(Long money) {
+        checkInputUnit(money);
         return null;
+    }
+
+    private static void checkInputUnit(Long input) {
+        if (input % LOTTO_PRICE > 0) {
+            String message = String.format("로또 구매 금액은 %d단위로 작성해야 합니다.", LOTTO_PRICE);
+            throw new IllegalArgumentException(message);
+        }
     }
 }
