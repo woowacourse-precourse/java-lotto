7ed6d55af7cc48648c5b6838a2b23d175aa6abfe
bumwoo <gld55602@gmail.com>
2022-11-14 21:13:38 +0900

    refactor: 로또 번호 상수 class로 관리

---
+++ src/main/java/lotto/constant/LottoNumberConstant.java
@@ -0,0 +1,7 @@
+package lotto.constant;
+
+public class LottoNumberConstant {
+    public static final int START = 1;
+    public static final int END = 45;
+    public static final int COUNT = 6;
+}
