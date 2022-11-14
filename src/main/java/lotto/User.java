98f8773df29f7b960fd4141146b228d7318b3694
= <kimje1109@gmail.com>
2022-11-14 21:25:36 +0900

    feat: 당첨 번호를 입력 받는 기능 구현

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,11 +1,18 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class User {
     final static String PRICE_PATTERN = "^[0-9]*$";
+    final static int WINNING_NUMBER_SIZE = 6;
+    final static int BONUS_NUMBER_SIZE = 1;
+
     public int purchasePrice;
+    public List<Integer> winningNumbers;
 
     public User() {
     }
@@ -24,5 +31,25 @@ public class User {
         boolean isThousand = (Integer.parseInt(readLine) % 1000 == 0);
 
         return (isNumeric && isThousand);
+    }
+
+    public void setWinningNumbers() {
+        String winningNumbers = Console.readLine();
+        winningNumbers = winningNumbers.replace(",", "");
+        if (isValidWinningNumbers(winningNumbers)) {
+            List<String> list =  Arrays.asList(winningNumbers.split(""));
+            this.winningNumbers = list.stream()
+                    .map(s -> Integer.parseInt(s))
+                    .collect(Collectors.toList());
+            return;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public boolean isValidWinningNumbers(String winningNumbers) {
+        boolean isNumeric = Pattern.matches(PRICE_PATTERN, winningNumbers);
+        boolean isWinningNumberSize = (winningNumbers.length() == WINNING_NUMBER_SIZE);
+
+        return (isNumeric && isWinningNumberSize);
     }
 }
