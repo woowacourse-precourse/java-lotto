738adb2f5d523310ac0a762531ea2f8b4030e2de
heesang <dlagmltkd12@naver.com>
2022-11-15 03:12:59 +0900

    test: LottoValidationUtilsTest 로또 번호 중 중복 번호가 있으면 예외 발생

--- src/test/java/lotto/domain/LottoValidationUtilsTest.java
+++ src/test/java/lotto/domain/LottoValidationUtilsTest.java
@@ -16,4 +16,12 @@ class LottoValidationUtilsTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 로또 번호의 개수는 6개 이어야 합니다.");
     }
+
+    @DisplayName("로또 번호 중 중복 번호가 있으면 예외 발생")
+    @Test
+    void validateDuplication() {
+        assertThatThrownBy(() -> LottoValidationUtils.validate(List.of(1, 2, 3, 3, 3, 3)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호에는 중복 된 숫자가 없어야합니다.");
+    }
 }
