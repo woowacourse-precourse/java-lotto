b7304d13b023d900eabdfcff52cce99c4d277953
Umin <adorecamus@gmail.com>
2022-11-14 17:42:16 +0900

    feat: 발행할 로또 수량 계산

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,32 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("구입금액을 입력해 주세요.");
+        String money = Console.readLine();
+
     }
+
+    static int numberOfLotto(String money) {
+        int number = 0;
+        try {
+            number = Integer.parseInt(money);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+        }
+
+        if (number % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
+
+        return number;
+    }
+
 }
