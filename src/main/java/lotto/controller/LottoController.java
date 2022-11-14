d305cba85b3c3336841e903054fbb723b4bc4b6a
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-15 07:02:51 +0900

    fix(controller/LottoController.java): 당첨번호, 보너스 번호 입력받는 컨트롤러 함수 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -5,9 +5,12 @@ import lotto.domain.game.*;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
+
 public class LottoController {
     public void start(){
         Player player = buyLotto();
+        Lotto winningLotto = inputWinningLotto();
     }
 
     public Player buyLotto(){
@@ -15,5 +18,13 @@ public class LottoController {
         Player player = new Player(playerInputMoney);
         OutputView.printLottoTickets(player);
         return player;
+    }
+
+    public Lotto inputWinningLotto(){
+        List<Integer> inputWinningNumber = InputView.inputWinningNumber();
+        Lotto lotto = new Lotto(inputWinningNumber);
+        int inputBonusNumber = InputView.inputBonusNumber();
+        lotto.setBonusNumber(inputBonusNumber);
+        return lotto;
     }
 }
