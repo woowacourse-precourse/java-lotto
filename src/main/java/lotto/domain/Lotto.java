fff69fc72d10d0fc6736f16e172e3f43f1b7bdaa
Sinryuji <siler2474@gmail.com>
2022-11-14 22:09:03 +0900

    feat(Lotto): 로또 발행 시 중복 숫자 체크 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,9 +14,16 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리 숫자입니다.");
+        }
+        if (dupCheck(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복 된 숫자가 있습니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean dupCheck(List<Integer> numbers) {
+        Set<Integer> numSet = new HashSet<>(numbers);
+
+        return numbers.size() != numSet.size();
+    }
 }
