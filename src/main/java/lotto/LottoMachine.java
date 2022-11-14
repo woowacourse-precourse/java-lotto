e593b654076f41aca4b3bb8fd37ae685cd77a4b3
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-14 21:27:09 +0900

    feat(LottoMachine): 금액에 따라 로또를 구입하는 기능 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -2,9 +2,21 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
+
+    public static final int LOTTO_PRICE= 1000;
+
+    public List<Lotto> buy(int money) {
+        List<Lotto> buyLotto = new ArrayList<>();
+        int numberOfLotto = money / LOTTO_PRICE;
+        for (int i = 0; i < numberOfLotto; i++) {
+            buyLotto.add(new Lotto(generateNumber()));
+        }
+        return buyLotto;
+    }
 
     private List<Integer> generateNumber() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
