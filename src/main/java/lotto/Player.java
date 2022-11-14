d9889fb805fc98b12dd38c27916273c6951bf061
sjh910805@gmail.com <sjh910805@gmail.com>
2022-11-14 13:48:40 +0900

    fix: 생성자 함수 수정
    Player 인스턴스에 있는 참조 변수 player에 Manager 인스턴스를 생성하여 대입했어야 하는데, 새로운 참조변수를
    만들어서 대입하여, Player 인스턴스와 Manage 인스턴스가 전혀 연결이 안되고 있었음.

--- src/main/java/lotto/Player.java
+++ src/main/java/lotto/Player.java
@@ -7,14 +7,15 @@ public class Player {
     private int money;
     private double yield;
     private List<List<Integer>> lottoNumbers;
-    public Manager manager;
-    private int [] result;
+    private Manager manager;
+    private int[] result;
 
     public Player() {
-        Manager manager = new Manager();
+        manager = new Manager();
     }
 
     public void purchaseLotto() {
+        System.out.println("hello");
         manager.requestMoneyStatementPrint();
         int money = manager.inputMoney();
         if (money % 1000 != 0) {
@@ -29,16 +30,23 @@ public class Player {
             lottoNumbers.add(manager.generator());
         }
     }
+
     public void checkGetLottoNumbers() {
         System.out.println();
         System.out.println(numberOfPurchase + "개를 구매했습니다.");
         System.out.println(lottoNumbers);
         System.out.println();
     }
+
+    public void makeGenerate() {
+        manager.generateLotto();
+    }
+
     public void calculateResult() {
         result = manager.compareNumber(lottoNumbers);
         yield = manager.calculateYield(money, result);
     }
+
     public void showResult() {
         System.out.println("당첨 통계");
         System.out.println("---");
@@ -49,6 +57,5 @@ public class Player {
         System.out.println("6개 일치 (2,000,000,000원) - " + result[0] + "개");
         System.out.println("총 수익률은 " + yield + "%입니다.");
     }
-
 
 }
