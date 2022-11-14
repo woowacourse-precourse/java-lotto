0560a5afaa610f3ce417609ecf1c5b46227c1ceb
Choi-JJunho <junho5336@gmail.com>
2022-11-14 20:26:23 +0900

    feat(Application): add GameManager class

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,10 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
+
+        GameManager gameManager = new GameManager();
+        gameManager.play();
+
         // TODO: 프로그램 구현
     }
 }
