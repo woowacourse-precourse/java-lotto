c364c8f632a3d93545d0bca21d327cdab9a2a2ef
Cho SangHyun <98shcho@naver.com>
2022-11-15 02:23:37 +0900

    feat(예외처리): 발행되는 로또 검증기능 구현

---
+++ src/main/java/lotto/domain/validator/LottoNumbersValidator.java
@@ -0,0 +1,48 @@
+package lotto.domain.validator;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LottoNumbersValidator {
+    private static int LOTTO_NUMBERS_SIZE = 6;
+    private static int MIN_LOTTO_NUMBER = 1;
+    private static int MAX_LOTTO_NUMBER = 45;
+    private static String SIX_NUMBERS_ERROR_MESSAGE = "6개로 구성돼야 합니다";
+    private static String RANGE_ERROR_MESSAGE = String.format("%d ~ %d 사이의 숫자들로 구성돼야 합니다",
+            MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER);
+    private static String DUPLICATE_ERROR_MESSAGE = "중복없이 구성돼야 합니다";
+
+    private static boolean isSixInputs(List<Integer> numbers) {
+        return numbers.size() == LOTTO_NUMBERS_SIZE;
+    }
+
+    private static boolean isInValidRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < MIN_LOTTO_NUMBER || MAX_LOTTO_NUMBER < number) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isDuplicate(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (Collections.frequency(numbers, number) > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void validate(List<Integer> numbers) {
+        if (!isSixInputs(numbers)) {
+            throw new IllegalArgumentException(SIX_NUMBERS_ERROR_MESSAGE);
+        }
+        if (!isInValidRange(numbers)) {
+            throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
+        }
+        if (isDuplicate(numbers)) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+}
