5cfe79cae32c6c4c9beb5ae152cc43ef3ac2a601
김선동 <mok03127@gmail.com>
2022-11-14 16:33:18 +0900

    feat: 로또 구입 금액 1000으로 나누어 떨어지지 않는 경우 예외 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,11 +8,13 @@ public class Lotto {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     private static final String LOTTO_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String LOTTO_HAS_DUPLICATED_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복될 수 없습니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         validateLottoRange(numbers);
+        validateDuplicateCount(numbers);
 
         ArrayList<Integer> sortedNumbers = new ArrayList<>(numbers);
         Collections.sort(sortedNumbers);
@@ -25,8 +27,6 @@ public class Lotto {
         }
     }
 
-
-
     private void validateLottoRange(List<Integer> numbers) {
         for (Integer number : numbers) {
             if (number < MIN_NUMBER || number > MAX_NUMBER) {
@@ -35,5 +35,9 @@ public class Lotto {
         }
     }
 
-
+    private void validateDuplicateCount(List<Integer> numbers) {
+        if(numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(LOTTO_HAS_DUPLICATED_NUMBER_ERROR_MESSAGE);
+        }
+    }
 }
