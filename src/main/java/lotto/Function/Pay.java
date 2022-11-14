256bb84478164907639cc7c690c0a7ffcb1769f2
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-14 23:57:56 +0900

    FIX:Pay.java/fix error

--- src/main/java/lotto/Function/Pay.java
+++ src/main/java/lotto/Function/Pay.java
@@ -1,25 +1,36 @@
 package lotto.Function;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Pay {
-    private final int cash;
+    private final String cash;
 
-    public Pay(int cash) {
+    public Pay(String cash) {
+        cashChanger(cash);
         validateCash(cash);
         this.cash = cash;
     }
 
-    private void validateCash(int cash) {
-        if (0 < cash % 1000) {
-            System.out.println("[ERROR] 금액 오류 1000원 단위만 입력가능");
+    private void validateCash(String cash) {
+        if (0 < (Integer.parseInt(cash) % 1000)) {
+            System.out.println("[ERROR] 1000원 단위만 입력가능합니다.");
             throw new IllegalArgumentException();
         }
     }
 
-    public int makeLotto() {
-        int result = 0;
-        result = cash / 1000;
+    private void cashChanger(String cash) {
+        for(int i=0;i<cash.length();i++){
+            int n = cash.charAt(i);
+            if (57<n||n<48) {
+                System.out.println("[ERROR] 숫자만 입력이 가능합니다.");
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public int makeBuyNum() {
+        int result = Integer.parseInt(cash) / 1000;
         return result;
     }
 }
