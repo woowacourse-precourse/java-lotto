035bd3de5b9f16f219a6b58af61c1e002891ea53
Yeongseon Park <yseon406@gmail.com>
2022-11-15 06:45:19 +0900

    test: 보너스 번호 유효성 검사 테스트

--- src/test/java/lotto/RaffleNumberTest.java
+++ src/test/java/lotto/RaffleNumberTest.java
@@ -5,6 +5,8 @@ import lotto.ui.Message;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -14,8 +16,9 @@ public class RaffleNumberTest {
     @Test
     void setWinningNumberTest() {
         String input = "1,2,3,4,5,6";
-
-        assertThat(new RaffleNumber().setWinningNumber(input)).contains(1, 2, 3, 4, 5, 6);
+        RaffleNumber raffleNumber = new RaffleNumber();
+        raffleNumber.setWinningNumber(input);
+        assertThat(raffleNumber.winningNumber).contains(1, 2, 3, 4, 5, 6);
     }
 
     @DisplayName("당첨 번호의 범위가 1~45를 벗어난 경우 예외 처리한다")
@@ -40,5 +43,13 @@ public class RaffleNumberTest {
         assertThatThrownBy(() -> new RaffleNumber().setWinningNumber("1,2,3,4,5,6,7"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(Message.WINNING_SIZE_ERROR);
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호와 같은 경우 예외 처리한다")
+    @Test
+    void validateNumber() {
+        assertThatThrownBy(() -> new RaffleNumber().validateNumber(List.of(1, 2, 3, 4, 5, 6), 6))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Message.BONUS_NUMBER_ERROR);
     }
 }
