e1a6e85ffa0fc0d5245d8006f95a5cbb42d62a97
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-15 03:53:31 +0900

    feat(main\...\controller\Constants) : implement collections of Constants
    - collected constant values used in controller

---
+++ src/main/java/lotto/controller/Constants.java
@@ -0,0 +1,7 @@
+package lotto.controller;
+
+public class Constants {
+
+    public static final String INPUT_CORRECT_FORMAT_MONEY = "[ERROR] 1000원 단위의 알맞은 금액을 넣어주세요.";
+    public static final int MONEY_UNIT = 1000;
+}
