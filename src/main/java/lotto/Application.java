17ecc84076410dc7e18c79e81bd9ae53af96751c
01192mg <01192mg@gmail.com>
2022-11-14 19:45:27 +0900

    feat(Application): 발행한 로또 수량 및 번호 출력하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        println("구입금액을 입력해 주세요.");
+        Money money = InputHandler.readMoney();
+        println();
+
+        List<Lotto> purchasedLottos = LottoShop.purchase(money);
+        printPurchasedLottos(purchasedLottos);
+        println();
+    }
+
+    private static void printPurchasedLottos(List<Lotto> purchasedLottos) {
+        println(purchasedLottos.size() + "개를 구매했습니다.");
+        purchasedLottos.forEach(lotto -> {
+            ArrayList<Integer> numbers = new ArrayList<>(lotto.getNumbers());
+            numbers.sort(Integer::compareTo);
+            println(numbers);
+        });
+    }
+
+
+    private static void println() {
+        System.out.println();
+    }
+
+    private static void println(Object x) {
+        System.out.println(x);
     }
 }
