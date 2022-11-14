5bb6c2955d00977692bff3efa278ccc5e244bdb3
Kim-Jong-Gyu <whdrb2643@gmail.com>
2022-11-14 22:09:33 +0900

    컴퓨터가 유저에게 당첨 통계를 알려주는 함수 추가
    컴퓨터가 유저에게 당첨 통계를 알려주는 함수 추가:
    - resultLotto(HashMap<String, Integer> winningLotto): Enum 클래스를 이용해서 key 값을 세팅 후 결과 값 출력 하는 함수

--- src/main/java/controller/Result.java
+++ src/main/java/controller/Result.java
@@ -1,17 +1,19 @@
 package controller;
 
 public enum Result {
-    FIRST("6개 일치", "2,000,000,000원"),
-    SECOND("5개 일치, 보너스 볼 일치","30,000,000,000원"),
-    THIRD("5개 일치","1,500,000"),
-    FOURTH("4개 일치","50,000"),
-    FIFTH("3개 일치","5,000");
-
+    FIFTH("3개 일치","5,000", "3"),
+    FOURTH("4개 일치","50,000", "4"),
+    THIRD("5개 일치","1,500,000", "5"),
+    SECOND("5개 일치, 보너스 볼 일치","30,000,000,000원", "Bonus"),
+    FIRST("6개 일치", "2,000,000,000원", "6");
     private final String condition;
     private final String winningCount;
-    Result(String condition, String winningCount) {
+
+    private final String winningNumber;
+    Result(String condition, String winningCount, String winningNumber) {
         this.condition = condition;
         this.winningCount = winningCount;
+        this.winningNumber = winningNumber;
     }
 
     public String getCondition() {
@@ -20,5 +22,9 @@ public enum Result {
 
     public String getWinningCount() {
         return this.winningCount;
+    }
+
+    public String getWinningNumber() {
+        return this.winningNumber;
     }
 }
--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -2,8 +2,11 @@ package view;
 
 import controller.Result;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class ResultView {
-
+    public void resultLotto(HashMap<String, Integer> winningLotto) {
+        for (Result rank : Result.values())
+            System.out.println(rank.getCondition() + " (" + rank.getWinningCount() + ")" + " - " + winningLotto.get(rank.getWinningNumber()) + "개");
+    }
 }
