e74e857aec54bc1c139635b3a273e359ea650123
JHS <wnghdtjr129@naver.com>
2022-11-14 21:46:22 +0900

    refactor(Converter): 검증로직 regex 도입

--- src/main/java/lotto/utils/StringToIntConverter.java
+++ src/main/java/lotto/utils/StringToIntConverter.java
@@ -1,11 +1,11 @@
 package lotto.utils;
 
-import static lotto.resources.ValidationPattern.PATTERN;
+import static lotto.resources.ValidationPattern.NUMERIC;
 
 public class StringToIntConverter implements Converter<String, Integer> {
     @Override
     public Integer convert(String target) {
-        if (PATTERN.canConvert(target)) {
+        if (NUMERIC.isValid(target)) {
             return Integer.parseInt(target);
         }
         throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주시기 바랍니다.");
--- src/main/java/lotto/utils/StringToListConverter.java
+++ src/main/java/lotto/utils/StringToListConverter.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
+import static lotto.resources.ValidationPattern.WINNING_NUMBER;
 
 public class StringToListConverter implements Converter<String, List<Integer>> {
     @Override
@@ -15,10 +16,9 @@ public class StringToListConverter implements Converter<String, List<Integer>> {
     }
 
     public static String[] validateSeparator(String winningNumber) {
-        String[] winningNumbers = winningNumber.split(",");
-        if (winningNumbers.length != 6) {
-            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 쉼표(,)를 이용하여 구분하여 입력해 주시기 바랍니다");
+        if(WINNING_NUMBER.isValid(winningNumber)){
+            return winningNumber.split(",");
         }
-        return winningNumbers;
+        throw new IllegalArgumentException("[ERROR] 6개의 숫자를 쉼표(,)를 이용하여 구분하여 입력해 주시기 바랍니다");
     }
 }
