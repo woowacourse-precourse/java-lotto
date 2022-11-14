ddc8407a173231832360b4e5ddad50df2be76297
tseop <77163842+tseop@users.noreply.github.com>
2022-11-14 11:32:18 +0900

    feat(lotto): input 을 int로 변환하는 기능 구현

--- src/main/java/lotto/input/Input.java
+++ src/main/java/lotto/input/Input.java
@@ -1,9 +1,18 @@
 package lotto.input;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.LottoException;
 
 public class Input {
     public String request() {
         return Console.readLine();
+    }
+
+    public int convertInput(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new LottoException(this.getClass());
+        }
     }
 }
