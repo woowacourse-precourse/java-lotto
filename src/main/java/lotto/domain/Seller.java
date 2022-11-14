1334291dd7a59febf3a4464680e384a9319359d1
seonpilKim <ksp970306@gmail.com>
2022-11-14 17:59:25 +0900

    Refactor: 메소드 길이 단축

--- src/main/java/lotto/domain/Seller.java
+++ src/main/java/lotto/domain/Seller.java
@@ -10,15 +10,15 @@ public class Seller {
 	private static final String PURCHASE_ANNOUNCEMENT = "%s개를 구매했습니다.";
 
 	public List<Lotto> sellLottoTickets(int money) {
-		if (validate(money)) {
-			throw new IllegalArgumentException(ErrorMessage.isNotUnitsOf1000());
-		}
+		validate(money);
 		System.out.println(String.format(PURCHASE_ANNOUNCEMENT, money / LOTTO_PRICE));
 		return Lotto.newInstances(money / LOTTO_PRICE);
 	}
 
-	private boolean validate(Integer money) {
-		return money < LOTTO_PRICE || money % LOTTO_PRICE != 0;
+	private void validate(Integer money) {
+		if (money < LOTTO_PRICE || money % LOTTO_PRICE != 0) {
+			throw new IllegalArgumentException(ErrorMessage.isNotUnitsOf1000());
+		}
 	}
 
 }
