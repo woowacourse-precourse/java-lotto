a30dacbef99661805b913af952a2aa9e51150715
kimJaeHyeon <kmr2644@gmail.com>
2022-11-14 22:53:56 +0900

    fix: validator import 경로 변경

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import lotto.LottoValidator;
+import lotto.validator.LottoValidator;
 import lotto.type.Prize;
 
 public class View {
