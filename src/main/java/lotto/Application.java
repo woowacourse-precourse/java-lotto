ce61e0da6d18a25cc0fb1dc2a0a3b5e904167d0a
swa07016 <swa07016@naver.com>
2022-11-14 21:54:02 +0900

    feat: implement user input

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,36 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
 public class Application {
+
+    private static Integer purchaseAmount;
+    private static List<Integer> winningNumbers = new ArrayList<>();
+    private static Integer bonusNumber;
+
+    private static void enterPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        purchaseAmount = Integer.valueOf(readLine());
+    }
+
+    private static void enterWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] temp = readLine().split(",");
+        for(String num : temp) {
+            winningNumbers.add(Integer.valueOf(num));
+        }
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        bonusNumber = Integer.valueOf(readLine());
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        enterPurchaseAmount();
+        // generateLottoNumbers();
+        enterWinningNumbers();
     }
 }
