5fb3538a9b7351ce9cf84ab6938852e53a4f5ed9
Your Name <you@example.com>
2022-11-14 16:45:31 +0900

    fix: 5개 일치시 보너스 확인

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -13,6 +13,12 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해주세요.");
+        }
+    }
+
     private void checkDuplication(List<Integer> numbers) {
         List<Integer> tempNum = new ArrayList<>();
 
@@ -23,28 +29,27 @@ public class Lotto {
         });
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해주세요.");
-        }
-    }
-
     public void printLotto() {
         System.out.println(numbers);
     }
 
     public LottoPrize compare(List<Integer> winnigLotto, int bonusNum) {
 
-        int matches = 0;
-        for (int index = 0; index < 6; index++) {
-            if(winnigLotto.contains(numbers.get(index)))
-                matches++;
-        }
+        int matches = getMatchingSize(winnigLotto);
         LottoPrize prize = LottoPrize.NONE;
         prize = prize.getPrize(matches);
+        if (matches == 5 && !numbers.contains(bonusNum))
+            return LottoPrize.THIRD;
         return prize;
     }
 
-    // TODO: 추가 기능 구현
+    private int getMatchingSize(List<Integer> winnigLotto) {
+        int matches = 0;
+        for (int index = 0; index < 6; index++) {
+            if (winnigLotto.contains(numbers.get(index)))
+                matches++;
+        }
+        return matches;
+    }
 
 }
