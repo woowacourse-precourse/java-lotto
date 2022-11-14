675781107a4b721d087ff33ed782c8db8e0abe56
KIMSEI1124 <workju1124@gmail.com>
2022-11-14 12:51:41 +0900

    feat: 발행한 로또 수량 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -22,4 +22,12 @@ public class OutputView {
         System.out.println(SECOND_PLACE_QUANTITY.getMessage(SECOND_PLACE.getQuantity()));
         System.out.println(FIRST_PLACE_QUANTITY.getMessage(FIRST_PLACE.getQuantity()));
     }
+
+    public void lottoQuantityPrint(int quantity) {
+        System.out.println(quantity + "개를 구매했습니다.");
+    }
+
+    public void totalLottoNumbersPrint() {
+        System.out.println();
+    }
 }
