51d1c425006cf8057cf48aa5c607ff6bd4b1e010
TaemHam <tk4003950@gmail.com>
2022-11-14 11:31:52 +0900

    Feat: lotto.type.ConstantNumberType 구현

---
+++ src/main/java/lotto/type/StepType.java
@@ -0,0 +1,30 @@
+package lotto.type;
+
+public enum StepType {
+    INIT,
+    BUY_LOTTO,
+    DECIDE_WINNER,
+    DECIDE_BONUS,
+    GIVE_RESULT,
+    FINISH;
+
+    private StepType nextStep;
+    private StepType prevStep;
+
+    public StepType getNextStep() {
+        return this.nextStep;
+    }
+
+    public StepType getPrevStep() {
+        return prevStep;
+    }
+
+    static {
+        StepType[] stepTypes = StepType.values();
+        int bound = stepTypes.length - 1;
+        for (int index = 0; index < bound; index++) {
+            stepTypes[index].nextStep = stepTypes[index + 1];
+            stepTypes[index + 1].prevStep = stepTypes[index];
+        }
+    }
+}
