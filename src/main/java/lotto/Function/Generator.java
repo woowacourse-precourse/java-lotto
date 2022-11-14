1acaa2fb2b7d9432c49f80d780e912c173e481f7
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-14 23:55:41 +0900

    FEAT:Generator.java/revise makeNumbers()

--- src/main/java/lotto/Function/Generator.java
+++ src/main/java/lotto/Function/Generator.java
@@ -1,22 +1,27 @@
 package lotto.Function;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Generator {
-    private final List<List<Integer>> result;
+    List<List<Integer>> result = new ArrayList<>();
 
-    public Generator(List<List<Integer>> result) {
-        this.result = result;
+    public Generator() {
     }
 
     public List<List<Integer>> makeNumbers(int num) {
         while (0 < num) {
-            result.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            List<Integer> list = new ArrayList<>();
+            list.addAll(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            list.sort(Comparator.naturalOrder());
+            new Lotto(list);
+            result.add(list);
             num--;
         }
         return result;
     }
-
 }
