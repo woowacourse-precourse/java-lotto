48696985d9c8946a94df21b25c928a658da4c3c0
wwwndus <simock22@gmail.com>
2022-11-15 00:55:18 +0900

    feat(InputView): 당첨 번호와 보너스 번호 입력 받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,11 +5,28 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String WRITE_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String WRITE_LOTTO = "\n당첨 번호를 입력해 주세요.";
+    private static final String WRITE_BONUS = "\n보너스 번호를 입력해 주세요.";
 
     public static int getInputMoney() {
         System.out.println(WRITE_PURCHASE_AMOUNT);
 
         int inputMoney = Integer.parseInt(Console.readLine());
-        return  inputMoney;
+        return inputMoney;
+    }
+
+    public static String[] getNumbers() {
+        System.out.println(WRITE_LOTTO);
+
+        String[] numbers = Console.readLine().split(",");
+        return numbers;
+    }
+
+    public static int getBonusNumber() {
+        System.out.println(WRITE_BONUS);
+
+        int bonusNumber = Integer.parseInt(Console.readLine());
+        return bonusNumber;
+
     }
 }
