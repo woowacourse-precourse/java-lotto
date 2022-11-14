97f43b2892ba3b1f6d52eba20d886dd935935c06
ohhonggi <5ghdrl@naver.com>
2022-11-15 05:57:40 +0900

    feat($LottoIssuer): Add generateLotto function
    This function issues a lotto.
    
    Closes feat_2

--- src/main/java/lotto/domain/LottoIssuer.java
+++ src/main/java/lotto/domain/LottoIssuer.java
@@ -1,5 +1,14 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class LottoIssuer {
+
+    public static Lotto generateLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
 
 }
