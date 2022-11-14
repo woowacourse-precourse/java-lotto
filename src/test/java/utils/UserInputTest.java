ab9f071ec4c4eebe74614808c4da3954eee8406d
MinSungKim <timkim01@naver.com>
2022-11-14 17:03:48 +0900

    test: 구입금액이 1000 단위인지 유효성검사 테스트코드 작성

--- src/test/java/utils/UserInputTest.java
+++ src/test/java/utils/UserInputTest.java
@@ -23,4 +23,17 @@ class UserInputTest {
         assertEquals("[ERROR] 숫자만 입력해주세요.", exception.getMessage());
     }
 
+    @Test
+    void 구입금액이_1000단위인지_유효성검사_테스트(){
+        String input = "1500";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            UserInput.inputMoney();
+        });
+        assertEquals("[ERROR] 1,000원 단위로만 입력해주세요.", exception.getMessage());
+    }
+
 }
