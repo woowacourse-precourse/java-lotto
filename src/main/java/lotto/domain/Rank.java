4c1c236536fa0d93328a8ecb444c9dd60a7fc712
leeyongjun <leeyongjun0310@naver.com>
2022-11-14 18:16:40 +0900

    test(lotto): 1등이 당첨됐을때 정상동작하는지 확인하는 테스트 코드 작성

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,11 +1,11 @@
 package lotto.domain;
 
 public enum Rank {
-    fifth(3, 5000, "3개 일치 (5,000원) - "),
-    fourth(4, 50000, "4개 일치 (50,000원) - "),
-    third(5, 1500000, "5개 일치 (1,500,000원) - "),
-    Second(7, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    first(6, 2000000000, "6개 일치 (2,000,000,000원) - ");
+    FIFTH(3, 5000, "3개 일치 (5,000원) - "),
+    FOURTH(4, 50000, "4개 일치 (50,000원) - "),
+    THIRD(5, 1500000, "5개 일치 (1,500,000원) - "),
+    SECOND(7, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - ");
     private final int index;
     private final int winningsPrice;
     private final String text;
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,11 +1,14 @@
 package lotto.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,4 +27,12 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @DisplayName("1등이 당첨됐을 때 정상동작 하는지 확인한다.")
+    @Test
+    void getPrizeMoney_정상동작_1등_당첨(){
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        int prizeMoney = lotto.getPrizeMoney(Arrays.asList(Arrays.asList(1,2,3,4,5,6),Arrays.asList(7,8,9,10,11,12)), 7);
+        assertThat(prizeMoney).isEqualTo(Rank.FIRST.getWinningsPrice());
+    }
 }
