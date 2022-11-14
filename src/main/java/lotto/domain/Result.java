21731b3148e11069ac7f0b76cda56cbe0e93c296
Isu Shin <isu010614@naver.com>
2022-11-15 07:59:30 +0900

    feat: 각 등수별로 당첨된 횟수를 담은 LinkedHashMap을 반환하는 기능 추가

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -1,8 +1,20 @@
 package lotto.domain;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 
 public class Result {
+    public static LinkedHashMap<Rank, Integer> getWinningResult(final List<Integer> matchResult, final List<Boolean> hasBonus) {
+        LinkedHashMap<Rank, Integer> result = initResult();
+        for (int i = 0; i < matchResult.size(); i++) {
+            int match = matchResult.get(i);
+            boolean bonus = hasBonus.get(i);
+            Rank rank = Rank.find(match);
+            result.put(checkRank(match, bonus, rank), result.get(Rank.find(match)) + 1);
+        }
+        return result;
+    }
+
     private static LinkedHashMap<Rank, Integer> initResult() {
         LinkedHashMap<Rank, Integer> result = new LinkedHashMap<>();
         for (Rank rank : Rank.values()) {
