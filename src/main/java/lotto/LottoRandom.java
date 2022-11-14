eb5195f3cab8bade443ffb29bc821fe45826ce90
can019 <jys01012@gmail.com>
2022-11-14 17:46:20 +0900

    feat(model): validate purchase amount and convert to purchase count

--- src/main/java/lotto/LottoRandom.java
+++ src/main/java/lotto/LottoRandom.java
@@ -10,12 +10,23 @@ public class LottoRandom {
     private final int MIN_LOTTO_NUM = 1;
     private final int MAX_LOTTO_NUM = 45;
     private final int LOTTO_COUNT = 6;
+    private final int PURCHASE_AMOUNT_UNIT = 1000;
 
-    public List<List<Integer>> generateRandomLottoNumberList(int purchaseCount){
-        List<List<Integer>> lottoRandomList  = new ArrayList<>();;
+    public int calculatePurchaseCount(int purchaseAmount) {
+        validatePurchaseAmount(purchaseAmount);
+        return purchaseAmount / PURCHASE_AMOUNT_UNIT;
+    }
+
+    private void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount % PURCHASE_AMOUNT_UNIT != 0)
+            throw new IllegalArgumentException();
+    }
+
+    public List<List<Integer>> generateRandomLottoNumberList(int purchaseCount) {
+        List<List<Integer>> lottoRandomList = new ArrayList<>();
         List<Integer> numbers;
 
-        for(int i = 0; i < purchaseCount; i ++){
+        for (int i = 0; i < purchaseCount; i++) {
             numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUM, MAX_LOTTO_NUM, LOTTO_COUNT);
             Collections.sort(numbers);
             lottoRandomList.add(numbers);
