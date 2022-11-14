c648e8b8f428f8cf010bbdf2e0048503943c7ff9
anneu <b.imnuj@gmail.com>
2022-11-14 13:59:28 +0900

    feat: 로또 당첨 결과를 구하는 기능 구현

--- src/main/java/lotto/LottoReader.java
+++ src/main/java/lotto/LottoReader.java
@@ -1,12 +1,45 @@
 package lotto;
 
+import static lotto.LottoRank.RANK2;
+import static lotto.LottoRank.RANK3;
+import static lotto.LottoRank.findRank;
+import static lotto.LottoRank.values;
+
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class LottoReader {
-    public void createLottoResult(List<Lotto> publishedLotto, Lotto winningLotto, Bonus bonus) {
+    public Map<LottoRank, Integer> createLottoResult(List<Lotto> publishedLotto, Lotto winningLotto, Bonus bonus) {
         Map<Lotto, Integer> matchingResult = createMatchingResult(publishedLotto, winningLotto);
+
+        Map<LottoRank, Integer> lottoResult = new EnumMap<>(LottoRank.class);
+
+        for (LottoRank rank : values()) {
+            lottoResult.put(rank, 0);
+        }
+
+        for (Entry<Lotto, Integer> matchingResultEntry : matchingResult.entrySet()) {
+            Lotto lotto = matchingResultEntry.getKey();
+            Integer matchingCount = matchingResultEntry.getValue();
+
+            if (matchingCount == 5) {
+                if (lotto.isMatch(bonus)) {
+                    lottoResult.put(RANK2, lottoResult.get(RANK2) + 1);
+                }
+
+                lottoResult.put(RANK3, lottoResult.get(RANK3) + 1);
+            }
+
+            if (matchingCount >= 3) {
+                LottoRank findRank = findRank(matchingResultEntry.getValue());
+                lottoResult.put(findRank, lottoResult.get(findRank) + 1);
+            }
+        }
+
+        return lottoResult;
     }
 
     private Map<Lotto, Integer> createMatchingResult(List<Lotto> publishedLotto, Lotto winningLotto) {
