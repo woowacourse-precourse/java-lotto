abc3c4f0c3b2fe982083febd67d01665c307a2d4
Jongha Park <jongha2788@u.sogang.ac.kr>
2022-11-15 03:31:42 +0900

    feat(Input) 금액, 번호 입력 구현

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,6 +1,19 @@
 package lotto;
-
+import camp.nextstep.edu.missionutils.Console;
 public class Input {
-
-
+    public static String getPurchaseMoney(){
+        String purchasedMoney = Console.readLine();
+        // validate 해야함.
+        return purchasedMoney;
+    }
+    public static String getWinningNumber(){
+        String WinningNumber = Console.readLine();
+        // validate 해야 함
+        return WinningNumber;
+    }
+    public static String getBonusNumber(){
+        String BonusNumber = Console.readLine();
+        // validate 해야 함
+        return BonusNumber;
+    }
 }
