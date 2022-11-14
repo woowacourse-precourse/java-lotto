bd0139eee1f7b07be51c0340a75247dc222df4d4
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-14 16:39:33 +0900

    refactor(Lotties): 예외 처리 타입에 따라 분리
    문자열 타입으로 검사 필요한 것들은 InputView로 이동

--- src/main/java/lotto/domain/Lotties.java
+++ src/main/java/lotto/domain/Lotties.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 public final class Lotties {
 
-    private static final String REG_XP_DIGITS = "^[0-9]+$";
-
     private final List<Lotto> lotties = new ArrayList<>();
 
     private final int purchaseAmount;
@@ -18,32 +16,21 @@ public final class Lotties {
         addLottoToList(getLottoPublishCount());
     }
 
-    public static Lotties create(String purchase) {
-        validate(purchase);
-        return new Lotties(Integer.parseInt(purchase));
-    }
-
-    private static void validate(String purchase) {
-        hasDigitsOnly(purchase);
-        isDividedByLottoAmount(Integer.parseInt(purchase));
-    }
-
-    private static void hasDigitsOnly(String purchaseAmount) {
-        if (!purchaseAmount.matches(REG_XP_DIGITS)) {
-            throw new IllegalArgumentException(InputException.MONEY_NOT_DIGIT.message());
-        }
-    }
-
-    private static void isDividedByLottoAmount(int purchaseAmount) {
-        if (purchaseAmount % 1000 != 0) {
-            throw new IllegalArgumentException(InputException.MONEY_NOT_DIVIDE_BY_LOTTO_UNIT.message());
-        }
+    public static Lotties create(int purchase) {
+        isDividedByLottoAmount(purchase);
+        return new Lotties(purchase);
     }
 
     public int getLottoPublishCount() {
         return purchaseAmount / 1000;
     }
 
+
+    private static void isDividedByLottoAmount(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException(InputException.MONEY_NOT_DIVIDE_BY_LOTTO_UNIT.message());
+        }
+    }
 
     private void addLottoToList(int lottoCount) {
         for (int i = 0 ; i < lottoCount ; i++) {
--- src/test/java/lotto/domain/LottiesTest.java
+++ src/test/java/lotto/domain/LottiesTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import lotto.exception.InputException;
+import lotto.view.InputView;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,7 +17,7 @@ public class LottiesTest {
 
     @BeforeEach
     void init() {
-        String purchaseCount = "2000";
+        int purchaseCount = 2000;
         lotties = Lotties.create(purchaseCount);
     }
 
@@ -34,9 +35,24 @@ public class LottiesTest {
         Assertions.assertThat(lotties.getLottiesSize()).isEqualTo(2);
     }
 
-    @DisplayName("구입 금액 예외 처리 테스트")
+    @DisplayName("구입 금액 숫자 예외 처리 테스트")
     @Nested
-    class Exception {
+    class NumberException {
+
+        @DisplayName("구입 금액이 1000원 단위가 아닌 경우 예외를 터트린다.")
+        @Test
+        void isDividedByLottoAmount() {
+            int purchase = 11003;
+
+            Assertions.assertThatThrownBy( () -> Lotties.create(purchase))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(InputException.MONEY_NOT_DIVIDE_BY_LOTTO_UNIT.message());
+        }
+    }
+
+    @DisplayName("구입 금액 문자열 예외 처리 테스트")
+    @Nested
+    class StringException {
 
         @DisplayName("구입 금액이 숫자가 아닌 경우 예외를 터트린다.")
         @Test
@@ -44,21 +60,9 @@ public class LottiesTest {
             String purchase = "3!@# A";
             System.setIn(generateUserInput(purchase));
 
-            Assertions.assertThatThrownBy( () -> Lotties.create(purchase))
+            Assertions.assertThatThrownBy(InputView::inputLottoPurchaseAmount)
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining(InputException.MONEY_NOT_DIGIT.message());
-        }
-
-
-        @DisplayName("구입 금액이 1000원 단위가 아닌 경우 예외를 터트린다.")
-        @Test
-        void isDividedByLottoAmount() {
-            String purchase = "11003";
-            System.setIn(generateUserInput(purchase));
-
-            Assertions.assertThatThrownBy( () -> Lotties.create(purchase))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(InputException.MONEY_NOT_DIVIDE_BY_LOTTO_UNIT.message());
         }
     }
 
