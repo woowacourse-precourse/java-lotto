4747fe2ca02590701c3207dbf2330491b0194c72
jeomxon <jeomxon@gmail.com>
2022-11-14 22:22:06 +0900

    feat: 로또 번호에 중복을 확인하는 예외처리

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -1,11 +1,10 @@
 package lotto.domain.lotto;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
-    private static final String NOT_SIX_NUMBERS = "[ERROR] 로또 번호는 6개의 수로만 이루어져야 합니다.";
+    private static final String NOT_SIX_NUMBERS_ERROR = "[ERROR] 로또 번호는 6개의 수로만 이루어져야 합니다.";
+    private static final String HAS_DUPLICATED_NUMBERS_ERROR = "[ERROR] 로또 번호에는 중복이 없어야 합니다.";
 
     private final List<Integer> numbers;
 
@@ -14,7 +13,8 @@ public class Lotto {
     }
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSixDigits(numbers);
+        validateDuplicatedNumber(numbers);
 
         List<Integer> tempNumbers = new ArrayList<>(numbers);
         Collections.sort(tempNumbers);
@@ -22,9 +22,18 @@ public class Lotto {
         this.numbers = tempNumbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSixDigits(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException(NOT_SIX_NUMBERS);
+            throw new IllegalArgumentException(NOT_SIX_NUMBERS_ERROR);
+        }
+    }
+
+    private void validateDuplicatedNumber(List<Integer> numbers) {
+        Set<Integer> tempSet = new HashSet<>(numbers);
+
+        if (tempSet.size() != 6) {
+            System.out.println(HAS_DUPLICATED_NUMBERS_ERROR);
+            throw new IllegalArgumentException(HAS_DUPLICATED_NUMBERS_ERROR);
         }
     }
 }
