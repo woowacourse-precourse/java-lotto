3f3f43e5ae1c4b7266c78bd5511ed4d30464ba79
2do1 <dowon3036@naver.com>
2022-11-14 21:38:51 +0900

    feat: 구입 금액 입력 시 숫자를 입력하지 않았을 경우 예외처리 구현

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
 
+import static java.lang.Character.isDigit;
+
 public class User {
     static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
 
@@ -21,7 +23,15 @@ public class User {
     public int inputMoney() {
         System.out.println(INPUT_MONEY);
 
-        int money = Integer.parseInt(Console.readLine());
-        return money;
+        String money = Console.readLine();
+        checkInteger(money);
+
+        return Integer.parseInt(money);
+    }
+
+    public void checkInteger(String number) {
+        if(!number.matches("-?\\d+(\\.\\d+)?")) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
+        }
     }
 }
