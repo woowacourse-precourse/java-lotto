4895c7f009bc0a20f263ab3b5c174c8665ddd6aa
dohyun <ldh19738@gmail.com>
2022-11-14 15:55:47 +0900

    feat(Win): 전체 통계 출력을 실행하는 기능 구현

--- src/main/java/lotto/Win.java
+++ src/main/java/lotto/Win.java
@@ -1,17 +1,20 @@
 package lotto;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.text.DecimalFormat;
 
 public class Win {
     private static final String WIN_STATISTICS_MESSAGE = "당첨 통계";
+    private static final String TITLE_DISTINGUISHING_ROLE = "---";
     private static final String PUTTING_COMMAS_IN_THREE_NUMBERS = "###,###";
 
     public void statisticsMessage() {
         System.out.println('\n' + WIN_STATISTICS_MESSAGE);
-        System.out.println("---");
+        System.out.println(TITLE_DISTINGUISHING_ROLE);
     }
 
-    private void printStatistics(CountNumberOfWins countNumberOfWins) {
+    private void printStatistics(@NotNull CountNumberOfWins countNumberOfWins) {
         DecimalFormat decimalFormat = new DecimalFormat(PUTTING_COMMAS_IN_THREE_NUMBERS);
 
         System.out.println("3개 일치 (" + decimalFormat.format(Grade.FIFTH.getPrizeMoney()) + "원) - " + countNumberOfWins.getFifthPlace() + "개");
@@ -20,5 +23,16 @@ public class Win {
         System.out.println("5개 일치, 보너스 볼 일치 (" + decimalFormat.format(Grade.SECOND.getPrizeMoney()) + "원) - " + countNumberOfWins.getSecondPlace() + "개");
         System.out.println("6개 일치 (" + decimalFormat.format(Grade.FIRST.getPrizeMoney()) +"원) - " + countNumberOfWins.getFirstPlace() + "개");
         System.out.println("총 수익률은 " + countNumberOfWins.rateOfReturn() + "%입니다.");
+    }
+
+    public void statistics() {
+        BuyLotto buyLotto = new BuyLotto();
+        WinningNumbers winningNumbers = new WinningNumbers();
+        CountNumberOfWins countNumberOfWins = new CountNumberOfWins(buyLotto, winningNumbers);
+
+        countNumberOfWins.ranking();
+        countNumberOfWins.calculateSumOfPrize();
+        statisticsMessage();
+        printStatistics(countNumberOfWins);
     }
 }
