5e753b62cdf181d8a4f1756eb0a9185d82d7cf40
JH-TT <leejongho9803@gmail.com>
2022-11-15 00:17:13 +0900

    refactor(LottoGenerator) : 로또 생성 코드 분리

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -22,10 +22,20 @@ public class LottoGenerator {
         Notice.BUY_MESSAGE.buy(Integer.parseInt(amount) / 1000);
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < Integer.parseInt(amount) / 1000; i++) {
-            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+            lottos.add(generateLotto());
         }
         showAllLottos(lottos);
         return lottos;
+    }
+
+    public Lotto generateLotto() {
+        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> realLotto = new ArrayList<>();
+        for(Integer num : lotto) {
+            realLotto.add(num);
+        }
+        Collections.sort(realLotto);
+        return new Lotto(realLotto);
     }
 
     public Lotto changeToNumber(String numbers) {
