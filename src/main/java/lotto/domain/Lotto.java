db4a18e2e6d6248158750ee23d53304f644a6ca7
LeeJejune <leejejune0625@gmail.com>
2022-11-14 21:32:15 +0900

    feat: Lotto 상수 선언

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,5 @@
+    private static final String DUPLICATE_ERROR = "[ERROR] 로또 개수는 중복이 불가능합니다.";
+    private static final int LOTTO_COUNT = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final String DELIMITER = ",";
