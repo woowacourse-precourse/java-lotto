573a65763ad4f899d29f4b105cd1438fafd2282b
jhsseonn <hosu0125@gmail.com>
2022-11-14 10:58:47 +0900

    feat: LottoGame Domain 구현

---
+++ src/main/java/lotto/Domain/LottoGame.java
@@ -0,0 +1,40 @@
+package lotto.Domain;
+
+import java.util.List;
+
+public class LottoGame {
+
+    // Number -> Num으로 줄여서 변수명 적용
+
+    private Integer cash;
+    private Integer enterNum;
+    private List<Lotto> lottoList;
+    private List<Integer> winNumList;
+    private Integer bonusNum;
+    private Integer result;
+    private float earningRate;
+
+    public LottoGame(Integer cash, Integer enterNum, List<Lotto> lottoList, List<Integer> winNumList, Integer bonusNum, Integer result, float earningRate) {
+        this.cash = cash;
+        this.enterNum = enterNum;
+        this.lottoList = lottoList;
+        validateList(winNumList);
+        this.winNumList = winNumList;
+        validateBonusRange(bonusNum);
+        this.bonusNum = bonusNum;
+        this.result = result;
+        this.earningRate = earningRate;
+    }
+
+    private void validateList(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateBonusRange(Integer number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
