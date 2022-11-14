17d33735b11fe8efe62fb10149a3ebdca5d8e414
Reznov <reznov03048@gmail.com>
2022-11-14 18:09:34 +0900

    fix: 입력받은 금액이 정수인지 판단하고 예외 발생시키는 기능 수정

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -10,14 +10,30 @@ public class Input {
     private static final String INPUT_LOTTO_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String ERROR_NUMBER_FORMAT = "[ERROR] 올바른 숫자를 입력해 주세요.";
 
     public Input() {
     }
 
     public static int getInputPurchaseAmount() {
         System.out.println(INPUT_LOTTO_PURCHASE_AMOUNT);
+        String input = Console.readLine();
 
-        return Integer.parseInt(Console.readLine());
+        if(!isNumber(input)) {
+            throw new IllegalArgumentException(ERROR_NUMBER_FORMAT);
+        }
+
+        return Integer.parseInt(input);
+    }
+
+    private static boolean isNumber(String input) {
+        try{
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        return true;
     }
 
     public static String getInputWinningNumber() {
