af48da8903e52be3a24d3aa11558e6412a6606f8
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-15 00:13:38 +0900

    TEST:LottoTest.java/create test paytest

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,13 @@
 package lotto;
 
 import lotto.Function.Checker;
+import lotto.Function.Pay;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -28,6 +28,13 @@ class LottoTest {
     @Test
     void validateNotMatchNum() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 숫자가 1-45 이외라면 예외가 발생한다.")
+    @Test
+    void paytest() {
+        assertThatThrownBy(() -> new Pay("1000j"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
