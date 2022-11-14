cda62bc8d97395e9f74b1f4e2591538baa9bf20d
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-14 23:15:45 +0900

    test(lottonumber): 대소관계 기능 테스트 완료

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -17,6 +18,20 @@ class LottoNumberTest {
     @DisplayName("로또 넘버가 최소값보다 작은 경우")
     void createLottoNumberByLowerValue() {
         assertThatThrownBy(() -> LottoNumber.getInstance(LottoNumber.MIN_VALUE - 1));
+    }
+
+    @Test
+    @DisplayName("로또 넘버 대소 비교 테스트")
+    void biggerLottoNumberValue() {
+        // given
+        LottoNumber lottoNumber1 = LottoNumber.getInstance(1);
+        LottoNumber lottoNumber2 = LottoNumber.getInstance(2);
+
+        // when
+        int compareLottoNumber = lottoNumber1.compareTo(lottoNumber2);
+
+        // then
+        assertThat(compareLottoNumber).isEqualTo(-1);
     }
 
 }
