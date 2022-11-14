aaed519b4fb774ef49d789f8b0318ded1ca87e0b
recicoco068 <gkapf838@gmail.com>
2022-11-14 13:11:14 +0900

    feat(LottoMachine.java) : 당첨 번호 얻는 함수 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoMachine {
     public int getLottoBuyCount(int paidMoney) {
         return paidMoney / LottoInfo.PRICE.getValue();
+    }
+
+    public List<Integer> getWinningNumbers(List<Integer> numbers) {
+        Utils.validateLottoNumbers(numbers);
+        return numbers;
     }
 }
