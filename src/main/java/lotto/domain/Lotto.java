fdf6e127393fe090f4745c07c7f33bba3f5d5869
Lemon Soju <yckk980320@naver.com>
2022-11-15 00:51:57 +0900

    feat(Lotto): exception handling when creating a lotto object
    Added Exceptions
    - When duplicate numbers exist in Lotto numbers

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,6 +15,11 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+
+        Set<Integer> numSet = new HashSet<>(numbers);
+        if(numSet.size()!= numbers.size()){
+            throw new IllegalArgumentException("[ERROR] 입력된 로또 번호에 중복된 번호가 있습니다.");
         }
     }
 
