9c1691dc451cb63df0014ce5a61478b0efb3d44f
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-15 05:28:33 +0900

    feat: buyLottoList 에 발행한 로또 개수를 저장하는 필드 구현

--- src/main/java/lotto/model/BuyingLottoList.java
+++ src/main/java/lotto/model/BuyingLottoList.java
@@ -5,12 +5,18 @@ import java.util.List;
 public class BuyingLottoList {
 
     private final List<Lotto> lottoList;
+    private final int numberOfLotto;
 
     public BuyingLottoList(List<Lotto> lottoList) {
         this.lottoList = lottoList;
+        this.numberOfLotto = lottoList.size();
     }
 
     public List<Lotto> getLottoList() {
         return lottoList;
+    }
+
+    public int getNumberOfLotto() {
+        return numberOfLotto;
     }
 }
