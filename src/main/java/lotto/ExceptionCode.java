f2196983e4a1d2c63b4cb15678d29bd7eb4f9234
jungdoyoon <jungdo8016@gmail.com>
2022-11-14 19:53:00 +0900

    feat(ExceptionCode):예외 enum 정리 파일

---
+++ src/main/java/lotto/ExceptionCode.java
@@ -0,0 +1,17 @@
+package lotto;
+
+public enum ExceptionCode {
+
+    Not_Size("[ERROR] 로또번호가 6개가 아닙니다."),
+    Exists_Number("[ERROR] 중복된 로또번호가 있습니다."),
+    Over_Range_Number("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    Not_Number("[ERROR] 로또 번호는 숫자를 입력하셔야 합니다."),
+    Not_Money("[ERROR] 돈은 1,000원 단위로 입력하셔야 합니다."),
+    Over_Money("[ERROR] 돈의 최소값은 0원 입니다.");
+
+    String message;
+
+    ExceptionCode(String message) {
+        this.message = message;
+    }
+}
