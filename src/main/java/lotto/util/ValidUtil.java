7933f2f034604eb06be44cc0a051ba772e6ff905
ohhonggi <5ghdrl@naver.com>
2022-11-15 02:35:45 +0900

    feat($ValidUtil): Implement validInputAmounts function
    This function verifies that user input is a number.

--- src/main/java/lotto/util/ValidUtil.java
+++ src/main/java/lotto/util/ValidUtil.java
@@ -1,6 +1,13 @@
 package lotto.util;
 
+
 public class ValidUtil {
-    private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    public static void validInputAmounts(String input) {
+        if (!input.matches("^[0-9]+$")) {
+            throw new IllegalArgumentException(ERROR_MESSAGE +" 숫자를 입력해야 합니다.");
+        }
+    }
 
 }
