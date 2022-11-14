9c215fcd1878d4abf28f93aeee054e4345eb5da5
SeolYoungKim <nasur4da@gmail.com>
2022-11-14 19:06:18 +0900

    style(LottoConstans): 상수 선언문 간에 \n을 입력하여 좀 더 보기 좋게 변경

--- src/main/java/lotto/domain/constants/LottoConstants.java
+++ src/main/java/lotto/domain/constants/LottoConstants.java
@@ -6,7 +6,9 @@ public class LottoConstants {
     }
 
     public static final int MIN_NUMBER_OF_LOTTO = 1;
+
     public static final int MAX_NUMBER_OF_LOTTO = 45;
+
     public static final int COUNTS_OF_LOTTO_NUMBERS = 6;
 
     public static final int PRICE_OF_LOTTO = 1_000;
