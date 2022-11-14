ca75b2c87a3b3d72d96498c49b71b3e8bbb66c94
kylew1004 <kylew1004@gmail.com>
2022-11-14 16:39:46 +0900

    feat: 메시지 목록 작성

---
+++ src/main/java/messages/PrintMessages.java
@@ -0,0 +1,14 @@
+package messages;
+
+public final class PrintMessages {
+
+    public static final String INPUT_PAYMENT = "구입금액을 입력해주세요.";
+    public static final String INPUT_LUCKY_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public static final String BUY_COMPLETE = "%d개를 구매했습니다.";
+    public static final String START_PRINT_STATISTICS = "당첨 통계\n---";
+    public static final String RANK_INFORMATION = "%s (%s원) - %d개";
+    public static final String PRINT_YIELD_FORMAT = "총 수익률은 %.1f%%입니다.";
+
+}
