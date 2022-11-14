752eea96b296da0db1962ee832f5322ea08c635a
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-15 01:52:58 +0900

    FIX:Pay.java/revise

--- src/main/java/lotto/Function/Pay.java
+++ src/main/java/lotto/Function/Pay.java
@@ -1,10 +1,11 @@
 package lotto.Function;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.regex.Pattern;
 
 public class Pay {
-    private final String cash;
+    private String cash;
 
     public Pay(String cash) {
         cashChanger(cash);
@@ -14,17 +15,26 @@ public class Pay {
 
     private void validateCash(String cash) {
         if (0 < (Integer.parseInt(cash) % 1000)) {
-            System.out.println("[ERROR] 1000원 단위만 입력가능합니다.");
-            throw new IllegalArgumentException();
+            try {
+                throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 1000원 단위만 입력가능합니다.");
+                throw new NoSuchElementException();
+            }
         }
     }
 
-    private void cashChanger(String cash) {
-        for(int i=0;i<cash.length();i++){
+    public void cashChanger(String cash) {
+        for (int i = 0; i < cash.length(); i++) {
             int n = cash.charAt(i);
-            if (57<n||n<48) {
-                System.out.println("[ERROR] 숫자만 입력이 가능합니다.");
-                throw new IllegalArgumentException();
+            if (57 < n || n < 48) {
+                cash.replaceAll("^[\\d]", "");
+                try {
+                    throw new IllegalArgumentException();
+                } catch (IllegalArgumentException e) {
+                    System.out.println("[ERROR] 숫자만 입력이 가능합니다.");
+                    throw new NoSuchElementException();
+                }
             }
         }
     }
