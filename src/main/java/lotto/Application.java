fc39314fce5328cd67237610fb11aa8627436366
hayeon <qorgkdus12@naver.com>
2022-11-14 15:26:25 +0900

    feat(main): init settings

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 입력
+        // 입력 수 만큼 6개의 랜덤 숫자 생성
+        // 사용자로부터 번호 입력 받음
+        // 보너스 번호
+        // 당첨 내역
+        // 수익률
     }
 }
