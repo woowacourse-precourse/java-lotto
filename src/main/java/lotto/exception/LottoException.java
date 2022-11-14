28e5e15b06cc24300167d979adc862ce87183bb7
tseop <77163842+tseop@users.noreply.github.com>
2022-11-15 00:05:44 +0900

    refactor(lotto): Exception class refactoring
    - 사용 용도에 맞게 Exception 발생 시 매개변수로 메세지를 받아 용도에 맞는 에러 메세지를 발생시킨다.
    - String 값은 상수 모음 class로 이동

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -1,27 +1,13 @@
 package lotto.exception;
 
-import lotto.player.Player;
+
+import lotto.ui.Message;
 
 public class LottoException extends IllegalArgumentException {
-
-    private static final String FIRST_ERROR = "[ERROR] ";
-    private static final String INPUT_ERROR = "정상적인 값을 입력해주세요.";
-    private static final String UNEXPECTED_ERROR = "예상치 못한 에러가 발생하였습니다.";
-    private final Class c;
-    private String errorMsg = FIRST_ERROR;
-
-    public LottoException(Class c) {
-        this.c = c;
-        createErrorMsg();
-        throw new IllegalArgumentException(errorMsg);
-    }
-
-
-    private void createErrorMsg() {
-        if (c == Player.class) {
-            errorMsg += INPUT_ERROR;
-            return;
+    public LottoException(String msg) {
+        if (msg.isBlank()) {
+            msg = Message.UNEXPECTED_ERROR;
         }
-        errorMsg += UNEXPECTED_ERROR;
+        throw new IllegalArgumentException(Message.ERROR_MESSAGE + msg);
     }
 }
