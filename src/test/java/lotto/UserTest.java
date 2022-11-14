600577158d3d180e887c682740793632f5c3b1ad
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-11-14 21:31:52 +0900

    test: 기능1 로또 구입 금액 입력하기

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -9,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
+
     @DisplayName("로또 구입 금액이 1,000 원으로 나누어 떨어지지 않으면 예외 발생")
     @Test
     void createCost() {
