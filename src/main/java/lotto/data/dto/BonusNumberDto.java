f691a048e14f0f6871ef986358565ee250b6703a
TaemHam <tk4003950@gmail.com>
2022-11-14 10:57:54 +0900

    Feat: lotto.data.dto.BonusNumberDto 구현

---
+++ src/main/java/lotto/data/dto/BonusNumberDto.java
@@ -0,0 +1,36 @@
+package lotto.data.dto;
+
+import lotto.type.ExceptionType;
+import utils.Validator.StringValidator;
+
+public class BonusNumberDto {
+
+    private int bonusNumber;
+
+    BonusNumberDto(int bonusNumber){
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static BonusNumberDto createWithInput(String input) throws IllegalArgumentException{
+        validate(input);
+        return new BonusNumberDto(Integer.parseInt(input));
+    }
+
+    private static void validate(String input) throws IllegalArgumentException {
+        if (hasWrongNumberFormat(input)) {
+            throw ExceptionType.NUMBER.getException();
+        }
+    }
+
+    private static boolean hasWrongNumberFormat(String input) {
+        return !StringValidator.isNumeric(input);
+    }
+}
