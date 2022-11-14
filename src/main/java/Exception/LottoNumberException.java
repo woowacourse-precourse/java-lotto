462a787bc734bfe9b9b664dd65462e8de248127f
p <gustjr2332@naver.com>
2022-11-14 17:48:49 +0900

    feat(LottoNumberException) : 당첨 번호와 보너스 번호 예외처리

---
+++ src/main/java/Exception/LottoNumberException.java
@@ -0,0 +1,18 @@
+package Exception;
+
+import java.util.regex.Pattern;
+
+public class LottoNumberException {
+    private static String NOTNUMBER_ERRORMESSAGE = "[ERROR] 숫자만 입력해주세요.";
+    private static String OUTOFRANGE_ERRORMESSAGE = "[ERROR] 1~45사이의 숫자만 입력해주세요.";
+
+    public static void checkException(String number) {
+
+        if (!Pattern.matches("^[1-9]*$", String.valueOf(number))) {
+            throw new IllegalArgumentException(NOTNUMBER_ERRORMESSAGE);
+        }
+        if (Integer.valueOf(number) < 1 || Integer.valueOf(number) > 45) {
+            throw new IllegalArgumentException(OUTOFRANGE_ERRORMESSAGE);
+        }
+    }
+}
