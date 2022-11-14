0fea86b878e9d7af3956515d7a94bd9fbc3768a5
khj <bbb6254177@gmail.com>
2022-11-14 23:51:09 +0900

    style: modify function name for convention

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -1,14 +1,14 @@
 package lotto;
 
 public class Print {
-    public void Lotto(User user) {
+    public void lotto(User user) {
         System.out.printf("%d개를 구매했습니다.\n", user.getNumberOfLotto());
         for (Lotto lotto: user.getLotto()) {
             System.out.println(lotto.getLottoNum());
         }
     }
 
-    public void WinningStat(Calculator calc) {
+    public void winningStat(Calculator calc) {
         System.out.println("당첨 통계");
         System.out.println("---");
         System.out.printf(
@@ -20,7 +20,7 @@ public class Print {
                 calc.getCountByRank(1));
     }
 
-    public void Yield(Calculator calc) {
+    public void yield(Calculator calc) {
         System.out.printf("총 수익률은 %.1f%%입니다.", calc.getYield());
     }
 }
