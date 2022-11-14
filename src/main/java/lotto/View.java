9cbdd5640dc31139800c31891398532c22f3e921
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-14 18:33:58 +0900

    feat : 로또 구입 금액 입력 Method 구현

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class View {
     public void printLine(String s) {
         try {
@@ -14,6 +16,19 @@ public class View {
             System.out.print(s);
         } catch (IllegalArgumentException e) {
             System.out.println(e);
+        }
+    }
+
+    public int inputPrice() {
+        int i;
+        try {
+            i = Integer.parseInt(Console.readLine());
+            if(i%1000 != 0){
+                throw new IllegalArgumentException();
+            }
+            return Integer.parseInt(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println("[Error] 로또 구입 금액이 올바르지 않습니다.");;
         }
     }
 }
