7ba11a47452814ae634e745b48eea6ceb6e5900b
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-15 06:48:27 +0900

    style: 불필요한 주석 제거

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -2,40 +2,35 @@ package lotto.domain;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.List;
 
 public enum LottoRank {
-    FIRST(6, 2_000_000_000, "6개 일치 (2,000,000,000원) - "),
-    SECOND(5, 30_000_000,"5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    THIRD(5, 1_500_000,"5개 일치 (1,500,000원) - "),
-    FOURTH(4,50_000, "4개 일치 (50,000원) - "),
-    FIFTH(3, 5_000, "3개 일치 (5,000원) - "),
-    FAILED(0,0,"");
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4,50_000),
+    FIFTH(3, 5_000),
+    FAILED(0,0);
 
     private final int winningNumberCount;
     private final int rewardMoney;
-    private final String resultMessage;
 
-    LottoRank(int winningNumberCount, int rewardMoney, String resultMessage) {
+    LottoRank(int winningNumberCount, int rewardMoney) {
         this.winningNumberCount = winningNumberCount;
         this.rewardMoney = rewardMoney;
-        this.resultMessage = resultMessage;
     }
 
-    public static LottoRank getLottoRank(int winningNumberCount, boolean bonusNumber) {
+    public static LottoRank valueOf(int winningNumberCount, boolean bonusNumber) {
         if (winningNumberCount == SECOND.winningNumberCount && bonusNumber) {
             return SECOND;
         }
         return Arrays.stream(LottoRank.values())
-                .filter(LottoRank -> LottoRank.winningNumberCount == winningNumberCount)
+                .filter(LottoRank-> LottoRank.winningNumberCount == winningNumberCount && !bonusNumber)
                 .findFirst()
                 .orElse(FAILED);
     }
 
-    public int getRewardMoney() {
-        return rewardMoney;
-    }
-
-    public String getResultMessage() {
-        return resultMessage;
+    public int getRewardMoney () {
+        return this.rewardMoney;
     }
 }
