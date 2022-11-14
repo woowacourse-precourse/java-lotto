55f752efcd5cbbb30a7f5af201a674cc4de4faa9
swjeong98 <tree5678@konkuk.ac.kr>
2022-11-14 22:54:27 +0900

    fix(Player) : 예외 발생 후 에러 메세지 출력하는 기능 구현과 기존 예외 발생 지점을 이 기능으로 모두 수정 완료

--- src/main/java/lotto/Player.java
+++ src/main/java/lotto/Player.java
@@ -16,25 +16,24 @@ public class Player {
         this.manager = manager;
     }
 
+    public void QuitProgram(String message){
+        System.out.println(message);
+        throw new IllegalArgumentException();
+    }
+
     public void InputPurchaseMoney(){
         System.out.println(manager.INPUT_MONEY);
         String input = readLine();
         checkPurchaseMoney(input);
     }
     public void checkPurchaseMoney(String input){
-        if(!manager.isNumber(input)){
-            System.out.println("[ERROR] : 금액은 숫자로만 구성된 문자열이어야 합니다.");
-            throw new IllegalArgumentException();
-        }
+        if(!manager.isNumber(input))
+            QuitProgram("[ERROR] : 금액은 숫자로만 구성된 문자열이어야 합니다.");
         int money = Integer.parseInt(input);
-        if(money < 1000){
-            System.out.println("[ERROR] : 최소 1000원 이상의 금액을 입력해주세요.");
-            throw new IllegalArgumentException();
-        }
-        if(money % 1000 != 0){
-            System.out.println("[ERROR] : 금액은 1000원으로 나누어 떨어져야 합니다.");
-            throw new IllegalArgumentException();
-        }
+        if(money < 1000)
+            QuitProgram("[ERROR] : 최소 1000원 이상의 금액을 입력해주세요.");
+        if(money % 1000 != 0)
+            QuitProgram("[ERROR] : 금액은 1000원으로 나누어 떨어져야 합니다.");
         this.PurchaseMoney = money;
     }
     public void calculateTotalLotto(){
@@ -43,17 +42,17 @@ public class Player {
     public void checkWinningNumber(String input){
         String[] arr = input.split(",");
         if(arr.length != manager.LOTTO_LENGTH)
-            throw new IllegalArgumentException("[ERROR] : 당첨번호는 쉼표(,)를 기준으로 6개로 구분되어야 합니다.");
+            QuitProgram("[ERROR] : 당첨번호는 쉼표(,)를 기준으로 6개로 구분되어야 합니다.");
         List<String> list = Arrays.asList(arr);
         if(!manager.ValidityOfNumber(list))
-            throw new IllegalArgumentException("[ERROR] : 당첨번호는 반드시 숫자로만 이루어져야 합니다.");
+            QuitProgram("[ERROR] : 당첨번호는 반드시 숫자로만 이루어져야 합니다.");
         List<Integer> winnings = new ArrayList<>();
         for(String s : list)
             winnings.add(Integer.parseInt(s));
         if(!manager.ValidRangeOfLottoNumber(winnings))
-            throw new IllegalArgumentException("[ERROR] : 당첨번호는 1 ~ 45 범위에 해당하는 숫자여야 합니다.");
+            QuitProgram("[ERROR] : 당첨번호는 1 ~ 45 범위에 해당하는 숫자여야 합니다.");
         if(manager.OverlapOfLottoNumber(winnings))
-            throw new IllegalArgumentException("[ERROR] : 6개의 당첨번호는 모두 다른 숫자여야 합니다.");
+            QuitProgram("[ERROR] : 6개의 당첨번호는 모두 다른 숫자여야 합니다.");
         this.winning = winnings;
     }
 
@@ -66,14 +65,14 @@ public class Player {
 
     public void checkBonusNumber(String input){
         if(!manager.isNumber(input))
-            throw new IllegalArgumentException("[ERROR] : 보너스번호는 반드시 숫자로만 이루어져야 합니다.");
+            QuitProgram("[ERROR] : 보너스번호는 반드시 숫자로만 이루어져야 합니다.");
         List<Integer> list = new ArrayList<>();
         int bonus = Integer.parseInt(input);
         list.add(bonus);
         if(!manager.ValidRangeOfLottoNumber(list))
-            throw new IllegalArgumentException("[ERROR] : 보너스번호는 1 ~ 45 범위에 해당하는 숫자여야 합니다.");
+            QuitProgram("[ERROR] : 보너스번호는 1 ~ 45 범위에 해당하는 숫자여야 합니다.");
         if(winning.contains(bonus))
-            throw new IllegalArgumentException("[ERROR] : 보너스 번호는 입력한 당첨 번호와 중복되서는 안됩니다.");
+            QuitProgram("[ERROR] : 보너스 번호는 입력한 당첨 번호와 중복되서는 안됩니다.");
         this.Bonus = bonus;
     }
     public void InputBonusNumber(){
