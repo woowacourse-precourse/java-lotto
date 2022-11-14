d7c43dcb3b65035d9dec619d72996d4da010432f
yun <yshyeon102@gmail.com>
2022-11-14 11:23:32 +0900

    feat(LottoService): 입력한 금액에 대한 구매 가능 로또 개수 계산 메서드 구현

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -14,7 +14,12 @@ public class LottoService {
     public static final int LOOSE = -1;
 
     public static final List<Integer> PRIZES = List.of(FIFTH_PRIZE, FOURTH_PRIZE, THIRD_PRIZE, SECOND_PRIZE, FIRST_PRIZE);
+
     public static final List<Integer> PRIZE_MONEY = Arrays.stream(WinningType.values())
             .map(WinningType::getMoney)
             .collect(Collectors.toList());
+
+    public int calBuyingCount(int money) {
+        return money / 1000;
+    }
 }
