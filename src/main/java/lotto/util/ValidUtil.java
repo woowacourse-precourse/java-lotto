23b0e8bcfd75cc4486918325c4418ad2f55f0039
ohhonggi <5ghdrl@naver.com>
2022-11-15 05:30:51 +0900

    refactor($ValidUtil): Use constant

--- src/main/java/lotto/util/ValidUtil.java
+++ src/main/java/lotto/util/ValidUtil.java
@@ -2,21 +2,21 @@ package lotto.util;
 
 
 import static lotto.constants.AmountCode.AMOUNTS_UNIT;
+import static lotto.constants.ErrorCode.NOT_NUMBER;
+import static lotto.constants.ErrorCode.UNSUITABLE_UNIT;
 
 public class ValidUtil {
-    private static final String ERROR_MESSAGE = "[ERROR]";
 
     public static String validInputAmounts(String input) {
         if (!input.matches("^[0-9]+$")) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 숫자를 입력해야 합니다.");
+            throw new IllegalArgumentException(NOT_NUMBER);
         }
         return input;
     }
 
     public static int validAmountUnit(int amount) {
         if (amount % AMOUNTS_UNIT != 0) {
-            throw new IllegalArgumentException(ERROR_MESSAGE +
-                    " 로또 금액은 1000원 단위이어야 합니다.");
+            throw new IllegalArgumentException(UNSUITABLE_UNIT);
         }
         return amount;
     }
