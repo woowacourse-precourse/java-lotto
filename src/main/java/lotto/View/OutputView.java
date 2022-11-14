8e1b0e1258ddecc84f173c65ad94b8bef2041c7a
Sieun Park <psieun0606@gmail.com>
2022-11-14 09:46:22 +0900

    feat: 6자리 랜덤 로또 번호 출력 기능 추가

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -1,7 +1,18 @@
 package lotto.View;
 
+import lotto.Domain.LottoPublisher;
+
 public class OutputView {
+
+    LottoPublisher lottoPublisher;
+
     public void printTheNumberOfLotto(int theNumberOfLotto) {
         System.out.println(String.format("%d개를 구매했습니다.", theNumberOfLotto));
+    }
+
+    public void printLottoNumber(int theNumberOfLotto) {
+        for (int i = 0; i < theNumberOfLotto; i++) {
+            System.out.println(lottoPublisher.makeRandomLottoNumber());
+        }
     }
 }
