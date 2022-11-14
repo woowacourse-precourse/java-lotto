85847a3ad285de114c04fae6c798a68c5cf93522
Isu Shin <isu010614@naver.com>
2022-11-15 06:44:34 +0900

    feat: 사용자에게 숫자를 입력받는 기능 추가
    -예외 처리: 숫자가 아닌 문자 입력 시 메시지 출력 및 예외 발생 후 프로그램 종료

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,9 +1,25 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Pattern;
+
 public class Input {
     public static int getPrice() {
         System.out.println(Message.PURCHASE_AMOUNT.print());
         return getNumber();
     }
-    
+
+    private static int getNumber() {
+        String number = Console.readLine();
+        validateNumber(number);
+        return Integer.parseInt(number);
+    }
+    private static void validateNumber(final String numbers) throws IllegalArgumentException {
+        if (!Pattern.matches("^[0-9]*$", numbers)) {
+            System.out.println("[ERROR] Only enter numbers.");
+            throw new IllegalArgumentException("[ERROR] Only enter numbers.");
+        }
+    }
+
 }
