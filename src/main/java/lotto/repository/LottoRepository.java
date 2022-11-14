44f2fe217e9fced6c8bf9053184ae034a3a3caa9
Beginner <dkstmdwn17@naver.com>
2022-11-14 20:42:10 +0900

    feat : 로또 구매 금액이 입력되어 저장소에 저장되는 부분 구현
    구매 금액이 입력되면 입력을 담당하는 객체에서 유효성 검사를 마치고 들어오기 때문에 형식에 맞는 데이터가 들어오게 된다.
    
    payMoney() : 그 결과 money 변수에 그 금액이 저장되게 된다.
    
    calculateLottoQuantity() :동시에 로또를 몇 장 샀는지.
    
    createLottoNumber() : 그 로또의 번호를 생성해주는 기능도 구현.

--- src/main/java/lotto/repository/LottoRepository.java
+++ src/main/java/lotto/repository/LottoRepository.java
@@ -1,4 +1,42 @@
 package lotto.repository;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LottoRepository {
+    private int money;
+    private int lottoQuantity;
+    private final List<Lotto> lottoNumbers = new ArrayList<>();
+    private Lotto winningNumbers;
+    private int bonusNumber;
+
+    // 돈을 저장하다
+    public void payMoney(int money){
+        this.money = money;
+        calculateLottoQuantity();
+        createLottoNumber();
+    }
+
+    private void createLottoNumber() {
+        for(int i=0;i<lottoQuantity;i++){
+            this.lottoNumbers.add(new Lotto(generateLottoNumbers()));
+        }
+    }
+
+    private void calculateLottoQuantity() {
+        this.lottoQuantity = getMoney() / 1000;
+    }
+
+    private List<Integer> generateLottoNumbers() {
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return lottoNumbers.stream().sorted().collect(Collectors.toList());
+    }
+
+    public int getMoney() {
+        return money;
+    }
 }
