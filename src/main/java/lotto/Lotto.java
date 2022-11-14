f426b88fec5b24b77196ab66a5b0a2400114a09f
LAPTOP-4NC4E864\yongs <yongs170@naver.com>
2022-11-14 14:04:20 +0900

    feat(Lotto) : numberDuplicateValidation 적용

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -13,6 +13,7 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         Validation.numberCountValidation(numbers);
         Validation.numberRangeValidation(numbers);
+        Validation.numberDuplicateValidation(numbers);
     }
 
     // TODO: 추가 기능 구현
