4f7b2925d90c57b07173c2ecf12c4f5f240d6d20
qkdk <86948395+qkdk@users.noreply.github.com>
2022-11-14 17:21:57 +0900

    feat: winningNumber 검증기능 구현

--- src/main/java/lotto/domain/WinningLottoMachine.java
+++ src/main/java/lotto/domain/WinningLottoMachine.java
@@ -1,17 +1,57 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import lotto.Constants;
 
 public class WinningLottoMachine {
 
     public List<Integer> giveWinningLottoNumber(String input) {
-        return List.of(1);
+        validateNumberCanSplit(input);
+        List<Integer> winningNumberList = makeNumberList(input);
+        validateLottoNumberLength(winningNumberList);
+        validateLottoNumberRange(winningNumberList);
+
+        return winningNumberList;
     }
 
     public int giveWinningLottoBonusNumber(String input) {
         validateBlank(input);
         int bonusNumber = validateNumeric(input);
         return bonusNumber;
+    }
+
+    private void validateLottoNumberLength(List<Integer> numberList) {
+        if (numberList.size() != Constants.LOTTO_NUMBERS_LENGTH.getValue()) {
+            throw new IllegalArgumentException("6 개의 숫자가 아닙니다.");
+        }
+    }
+
+    private void validateLottoNumberRange(List<Integer> numberList) {
+        boolean rangeOverFlag = numberList.stream()
+                .anyMatch(number -> number < Constants.MIN_LOTTO_NUMBER_RANGE.getValue()
+                        || number > Constants.MAX_LOTTO_NUMBER_RANGE.getValue());
+
+        if (rangeOverFlag) {
+            throw new IllegalArgumentException("로또 숫자중 범위 밖의 숫자가 있습니다.");
+        }
+    }
+
+    private List<Integer> makeNumberList(String input) {
+        List<Integer> numberList = new ArrayList<>();
+
+        for (String element : input.split(",")) {
+            int number = validateNumeric(element);
+            numberList.add(number);
+        }
+
+        return numberList;
+    }
+
+    private void validateNumberCanSplit(String input) {
+        if (!input.contains(",")) {
+            throw new IllegalArgumentException("분할될 수 없는 입력입니다.");
+        }
     }
 
     private int validateNumeric(String input) {
--- src/test/java/lotto/domain/WinningLottoMachineTest.java
+++ src/test/java/lotto/domain/WinningLottoMachineTest.java
@@ -9,6 +9,47 @@ class WinningLottoMachineTest {
 
     private final WinningLottoMachine winningLottoMachine = new WinningLottoMachine();
 
+    @DisplayName("정답 로또 번호중 범위가 맞지 않는 수가 있으면 예외가 발생한다.")
+    @Test
+    void inputNumbersByOutOfRange() {
+        String input = "2,56,22,1,144,2";
+
+        assertThatThrownBy(() -> winningLottoMachine.giveWinningLottoNumber(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @DisplayName("정답 로또 번호의 개수가 6개가 아니면 예외가 발생한다.")
+    @Test
+    void inputNumbersByOverSizeOrUnderSize() {
+        String input = "2,56,22,1";
+
+        assertThatThrownBy(() -> winningLottoMachine.giveWinningLottoNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("6 개의 숫자가 아닙니다.");
+    }
+
+    @DisplayName("나누어진 정답 로또 번호의 입력중 숫자외의 문자가 있으면 예외가 발생한다.")
+    @Test
+    void inputNumbersByNonNumeric() {
+        String input = "2,56,22,1,a";
+
+        assertThatThrownBy(() -> winningLottoMachine.giveWinningLottoNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("수가 아닌 입력이 있습니다.");
+
+    }
+
+    @DisplayName("정답 로또 숫자의 입력이 , 로 나누어지지 않으면 예외가 발생한다.")
+    @Test
+    void inputNumbersNotContainSplitCharacter() {
+        String input = "144141.1515";
+
+        assertThatThrownBy(() -> winningLottoMachine.giveWinningLottoNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("분할될 수 없는 입력입니다.");
+    }
+
     @DisplayName("보너스 넘버의 입력이 0이면 예외가 발생힌다.")
     @Test
     void inputBonusNumberByBlank() {
