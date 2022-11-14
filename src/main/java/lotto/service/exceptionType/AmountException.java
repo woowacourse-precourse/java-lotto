6be0d2bf4d670186508b5477133d9c7f8a62aebf
jjmin9797 <jjmin9797@naver.com>
2022-11-14 18:47:57 +0900

    feat(AmountException): 금액이 양수인지 확인하는 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/service/exceptionType/AmountException.java
+++ src/main/java/lotto/service/exceptionType/AmountException.java
@@ -4,6 +4,8 @@ public class AmountException {
     public static final String ERROR_MESSAGE = "[ERROR]";
     public static final String FAILED_TO_CONVERT_NUMBER_MESSAGE = " 숫자만 입력 가능합니다.";
     public static final String INDIVISIBLE_EXCEPTION_MESSAGE = " 1000원 단위로만 입력 가능합니다.";
+    public static final String NEGATIVE_NUMBER_EXCEPTION_MESSAGE = " 1000원 이상만 입력 가능합니다.";
+
     public void checkIfAnAmountIsConvertibleToNumber(String purchaseAmount) {
         try {
             int money = Integer.parseInt(purchaseAmount);
@@ -14,8 +16,15 @@ public class AmountException {
     }
 
     public void checkIfAmountIsDivisibleByOneThousand(String purchaseAmount) {
-        if (Integer.parseInt(purchaseAmount)%1000 != 0) {
-            System.out.println(ERROR_MESSAGE+INDIVISIBLE_EXCEPTION_MESSAGE);
+        if (Integer.parseInt(purchaseAmount) % 1000 != 0) {
+            System.out.println(ERROR_MESSAGE + INDIVISIBLE_EXCEPTION_MESSAGE);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void checkIfAmountIsPositive(String purchaseAmount) {
+        if (Integer.parseInt(purchaseAmount) < 0) {
+            System.out.println(ERROR_MESSAGE + NEGATIVE_NUMBER_EXCEPTION_MESSAGE);
             throw new IllegalArgumentException();
         }
     }
