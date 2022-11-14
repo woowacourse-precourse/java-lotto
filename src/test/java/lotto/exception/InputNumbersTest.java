806230b12b60265cd9a2bdadd2b882f36d1c4847
Gibeom Nam <rlqja0523@naver.com>
2022-11-15 04:03:01 +0900

    test: 에러 메시지가 같은지 테스트

--- src/test/java/lotto/exception/InputNumbersTest.java
+++ src/test/java/lotto/exception/InputNumbersTest.java
@@ -3,9 +3,12 @@ package lotto.exception;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class InputNumbersTest {
+    private final String notSeparatedByCommaMessage = "[ERROR] 당첨번호는 쉼표로 구분되어야 합니다.";
+
 
     @Test
     @DisplayName("숫자가 쉼표로 구분되어 있지 않으면 에러가 발생한다.")
@@ -15,5 +18,20 @@ public class InputNumbersTest {
 
         // when, then
         assertThrows(IllegalArgumentException.class, () -> InputNumbers.checkException(numbers));
+    }
+
+    @Test
+    @DisplayName("쉼표로 구분되어 있지 않은 숫자를 입력받아 에러가 발생하면 [ERROR]로 시작하는 메세지를 반환한다.")
+    void errorMessageTest1() {
+        // given
+        String numbers = "41,23_11_9.13,2";
+
+        try {
+            // when
+            InputNumbers.checkException(numbers);
+        } catch (IllegalArgumentException e) {
+            // then
+            assertThat(e.getMessage()).isEqualTo(notSeparatedByCommaMessage);
+        }
     }
 }
