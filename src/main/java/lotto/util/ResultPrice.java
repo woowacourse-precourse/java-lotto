43ae9dd35715f6621e0e3466866d8ea621ab80d5
Choi-JJunho <junho5336@gmail.com>
2022-11-14 20:07:56 +0900

    feat(ResultPrice): add match_count

--- src/main/java/lotto/util/ResultPrice.java
+++ src/main/java/lotto/util/ResultPrice.java
@@ -1,20 +1,25 @@
 package lotto.util;
 
 public enum ResultPrice {
-    FIRST(2000000000),
-    SECOND(30000000),
-    THIRD(1500000),
-    FOURTH(50000),
-    FIFTH(5000);
+    FIRST(2000000000, 6),
+    SECOND(30000000, 5),
+    THIRD(1500000, 5),
+    FOURTH(50000, 4),
+    FIFTH(5000, 3);
 
-    private int price;
+    private final long price;
+    private final long match_count;
 
-    ResultPrice(int price) {
-
+    ResultPrice(long price, long match_count) {
+        this.price = price;
+        this.match_count = match_count;
     }
 
-    public int getPrice() {
+    public long getPrice() {
         return this.price;
     }
 
+    public long getMatch_count() {
+        return this.match_count;
+    }
 }
