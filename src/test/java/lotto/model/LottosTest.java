84b887068bf8fd0b10e11cc02d9bebcd50df80f1
dhkim <akdlzmf1123@gmail.com>
2022-11-14 19:29:24 +0900

    test(LottoTest): LottosResult 만드는 기능 테스트 수정
    테스트를 위한 생성자를 제거하기 위해

--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -14,10 +15,10 @@ class LottosTest {
             List.of(1, 2, 3, 4, 5, 6), 7
     );
 
-    @ParameterizedTest(name = "총 상금은 {1}원")
+    @ParameterizedTest(name = "구매한 로또의 총 상금은 {1}원")
     @MethodSource("lottosAndWinningAmount")
-    void 구매한_복권들의_상금을_모두_더하는_기능(Lottos lottos, int winningAmount) {
-        assertThat(lottos.calculateWinningAmount(winningNumbers)).isEqualTo(winningAmount);
+    void 구매한_로또들의_총_상금을_구하는_기능(Lottos lottos, int expected) {
+        assertThat(lottos.calculateWinningAmount(winningNumbers)).isEqualTo(expected);
     }
 
     static Stream<Arguments> lottosAndWinningAmount() {
@@ -27,6 +28,7 @@ class LottosTest {
                         , new Lotto(List.of(1, 3, 7, 4, 5, 6))
                         , new Lotto(List.of(1, 12, 23, 4, 35, 7))
                 )), 2_030_000_000)
+
                 , Arguments.of(new Lottos(List.of(
                         new Lotto(List.of(1, 2, 3, 13, 5, 6))
                         , new Lotto(List.of(1, 28, 13, 4, 5, 6))
@@ -35,24 +37,27 @@ class LottosTest {
         );
     }
 
-//    @Test
-//    void 복권들_당첨정보_만드는_기능() {
-//        Lottos lottos = new Lottos(List.of(
-//                new Lotto(List.of(1, 2, 3, 4, 5, 6))
-//                , new Lotto(List.of(1, 2, 3, 4, 5, 6))
-//                , new Lotto(List.of(1, 2, 3, 7, 5, 6))
-//                , new Lotto(List.of(11, 22, 3, 4, 5, 6))
-//                , new Lotto(List.of(11, 2, 3, 4, 55, 666))
-//                , new Lotto(List.of(1, 22, 33, 44, 5, 6))
-//                , new Lotto(List.of(10, 22, 33, 4, 5, 7))
-//        ));
-//
-//        LottosResult expected = new LottosResult(new ArrayList<>(
-//                List.of(
-//                        2, 1, 0, 1, 2
-//                )
-//        ));
-//
-//        assertThat(lottos.getLottosResult(winningNumbers)).isEqualTo(expected);
-//    }
+    @Test
+    void 로또들_당첨정보_만드는_기능() {
+        Lottos lottos = new Lottos(List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6))
+                , new Lotto(List.of(1, 2, 3, 4, 5, 6))
+                , new Lotto(List.of(1, 2, 3, 7, 5, 6))
+                , new Lotto(List.of(11, 22, 3, 4, 5, 6))
+                , new Lotto(List.of(11, 2, 3, 4, 45, 36))
+                , new Lotto(List.of(1, 22, 33, 44, 5, 6))
+                , new Lotto(List.of(10, 22, 33, 4, 5, 7))
+        ));
+
+        LottosResult expected = new LottosResult();
+        expected.winLotto(LottoPrize.FIRST);
+        expected.winLotto(LottoPrize.FIRST);
+        expected.winLotto(LottoPrize.SECOND);
+        expected.winLotto(LottoPrize.FOURTH);
+        expected.winLotto(LottoPrize.FIFTH);
+        expected.winLotto(LottoPrize.FIFTH);
+        expected.winLotto(LottoPrize.LOSE);
+        
+        assertThat(lottos.getLottosResult(winningNumbers)).isEqualTo(expected);
+    }
 }
