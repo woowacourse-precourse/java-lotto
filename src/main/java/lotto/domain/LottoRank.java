85c0e6851d9d0d3d2d4f8fc27e2f673cfe921518
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-15 07:49:40 +0900

    refactor: 당첨 결과 출력을 위해 LottoRank 클래스에 등수 별 문구 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -5,19 +5,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum LottoRank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4,50_000),
-    FIFTH(3, 5_000),
-    FAILED(0,0);
+    FIRST(6, 2_000_000_000, "6개 일치 (2,000,000,000원) - "),
+    SECOND(5, 30_000_000,"5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(5, 1_500_000,"5개 일치 (1,500,000원) - "),
+    FOURTH(4,50_000,"4개 일치 (50,000원) - "),
+    FIFTH(3, 5_000, "3개 일치 (5,000원) - "),
+    FAILED(0,0, "");
 
     private final int winningNumberCount;
     private final int rewardMoney;
+    private final String resultText;
 
-    LottoRank(int winningNumberCount, int rewardMoney) {
+    LottoRank(int winningNumberCount, int rewardMoney, String resultText) {
         this.winningNumberCount = winningNumberCount;
         this.rewardMoney = rewardMoney;
+        this.resultText = resultText;
     }
 
     public static LottoRank valueOf(int winningNumberCount, boolean bonusNumber) {
@@ -29,8 +31,14 @@ public enum LottoRank {
                 .findFirst()
                 .orElse(FAILED);
     }
+    public int getWinningNumberCount() {
+        return this.winningNumberCount;
+    }
 
     public int getRewardMoney () {
         return this.rewardMoney;
+    }
+    public String getResultText() {
+        return this.resultText;
     }
 }
