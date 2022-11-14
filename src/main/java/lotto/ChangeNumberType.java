743be1a5b98b5f98f730d14370d0632c82edd7f4
ensk26 <tjensk26@gmail.com>
2022-11-14 22:38:46 +0900

    feat(ChangeNumberType): 숫자로 반환 해주는 메서드
    새로운 메서드 추가  to ChangeNumberType
    - 숫자 int 형으로 반환 해준다. (금액, 보너스 번호)
    - 숫자 List 형으로 반환 해준다. (당첨 번호)

--- src/main/java/lotto/ChangeNumberType.java
+++ src/main/java/lotto/ChangeNumberType.java
@@ -1,9 +1,21 @@
 package lotto;
 
+import java.util.ArrayList;
+
 public class ChangeNumberType {
     private final String number;
 
     public ChangeNumberType(String number) {
         this.number = number;
     }
+
+    public int getNumber() {
+        return -1;
+    }
+
+    public ArrayList<Integer> getNumbers() {
+        return null;
+    }
+
+
 }
