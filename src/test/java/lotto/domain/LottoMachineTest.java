9fe6de17c209848b4a5655422d79e289e90b39bf
Woojin Lee <dhkep03@gmail.com>
2022-11-14 22:05:20 +0900

    test(LottoMachine): 입력 금액 유효성 검사 테스트 추가

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import static lotto.constant.LottoRule.AMOUNT_OF_ONE_LOTTO;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -29,5 +30,12 @@ class LottoMachineTest {
         int countLottosByAmount = inputMoney / AMOUNT_OF_ONE_LOTTO;
         // then
         assertThat(lottos.size()).isEqualTo(countLottosByAmount);
+    }
+
+    @DisplayName("입력 금액이 1,000의 배수가 아님 -> 예외님발생")
+    @Test
+    void isNotMultipleOf1000() {
+        assertThatThrownBy(() -> LottoMachine.publish(7001))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
