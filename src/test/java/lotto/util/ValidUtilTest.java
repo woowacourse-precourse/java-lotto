eb551064bbf72d44724227c296876e73af80b5e2
ohhonggi <5ghdrl@naver.com>
2022-11-15 03:06:27 +0900

    test($ValidUtilTest): Add validAmountUnit test case
    This test verifies whether the lotto purchase amount
    is divided into 1,000 won.

--- src/test/java/lotto/util/ValidUtilTest.java
+++ src/test/java/lotto/util/ValidUtilTest.java
@@ -14,4 +14,11 @@ class ValidUtilTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    @DisplayName("로또 구입 금액으로 1000원 단위에 금액를 입력하지 않으면 오류가 발생한다.")
+    void validAmountUnit() {
+        assertThatThrownBy(() -> ValidUtil.validAmountUnit(1010))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
 }
