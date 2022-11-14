88743bab4053368124acc9b1e674a5e31aa99a9e
yejin <kwonyj1022@naver.com>
2022-11-14 18:06:35 +0900

    feat: Compare 클래스 구현 완료

---
+++ src/main/java/lotto/Compare.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import java.util.List;
+
+public class Compare {
+    final int sameNumberCnt;
+    final boolean isBonusSame;
+    Compare(List<Integer> numbers, List<Integer> lotto, int bonus) {
+        sameNumberCnt = sameNumberCnt(numbers, lotto);
+        isBonusSame = isBonusSame(numbers, bonus);
+    }
+
+    private int sameNumberCnt(List<Integer> numbers, List<Integer> lotto) {
+        int sameNumberCnt = 0;
+        for (int i = 0; i < lotto.size(); i++) {
+            if (numbers.contains(lotto.get(i)))
+                sameNumberCnt++;
+        }
+        return sameNumberCnt;
+    }
+
+    private boolean isBonusSame(List<Integer> numbers, int bonus) {
+        boolean isBonusSame = false;
+        if (numbers.contains(bonus))
+            isBonusSame = true;
+        return isBonusSame;
+    }
+}
