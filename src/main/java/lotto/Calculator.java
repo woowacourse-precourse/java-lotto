5e6ce9280b1e4fac02002c851b68b63db1c2cc4d
wonju-dev <wonjulee.dev@gmail.com>
2022-11-15 01:43:45 +0900

    refactor(Calculator): 정답 번호, 랜덤 번호 비교 로직 리펙토링

--- src/main/java/lotto/Calculator.java
+++ src/main/java/lotto/Calculator.java
@@ -2,29 +2,31 @@ package lotto;
 
 import lotto.userinterface.Hit;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 
 public class Calculator {
 
     public static HashMap<Hit, Integer> getLottoResult(List<Lotto> randomLottos, Lotto answerLotto, Integer bonusNumber) {
         HashMap<Hit, Integer> result = getInitResult();
-        Set<Lotto> answer = Set.of(answerLotto);
+        List<Integer> answerNumbers = new ArrayList<>(answerLotto.getNumbers());
 
         for (Lotto randomLotto : randomLottos) {
-            Set<Lotto> random = Set.of(randomLotto);
-            random.retainAll(answer);
+            List<Integer> randomNumbers = new ArrayList<>(randomLotto.getNumbers());
+            Boolean hasBonus = randomNumbers.contains(bonusNumber);
 
-            Boolean bonusHit = random.contains(bonusNumber);
+            randomNumbers.retainAll(answerNumbers);
 
-            Hit hit = Hit.getByCount(random.size(), bonusHit);
-            result.put(hit, result.get(hit) + 1);
+            Hit hit = Hit.getHit(randomNumbers.size(), hasBonus);
+
+            if (hit != null) {
+                result.put(hit, result.get(hit) + 1);
+            }
         }
 
         return result;
     }
-
 
     private static HashMap<Hit, Integer> getInitResult() {
         HashMap<Hit, Integer> result = new HashMap<>();
@@ -33,7 +35,7 @@ public class Calculator {
         result.put(Hit.FOUR, 0);
         result.put(Hit.FIVE, 0);
         result.put(Hit.FIVE_BONUS, 0);
-        result.put(Hit.SIZE, 0);
+        result.put(Hit.SIX, 0);
 
         return result;
     }
