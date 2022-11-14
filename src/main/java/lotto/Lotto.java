0e8ba373cbbbbe80d090eec50b0d31a0d5e78fac
JerryK026 <kskyung0624@gmail.com>
2022-11-14 18:33:30 +0900

    refactor(Lotto) : validate() 중복 검사
    validate()에 로또 번호가 중복일 떄 IllegalArgumentException을 발생시킨다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,13 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
+
+    private static final String LOTTO_NUMBER_ERROR_MESSAGE = "로또 번호 개수는 6개여야 합니다.";
+    private static final String LOTTO_DUPLICATE_ERROR_MESSAGE = "로또 번호는 중복되어선 안됩니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,9 +17,16 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LOTTO_NUMBER_ERROR_MESSAGE);
+        }
+
+        if (isDuplicated(numbers)) {
+            throw new IllegalArgumentException(LOTTO_DUPLICATE_ERROR_MESSAGE);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean isDuplicated(List<Integer> numbers) {
+        return new HashSet<>(numbers).size() != numbers.size();
+    }
+
 }
