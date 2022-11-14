f80547714a32ce8f969512908549570974e8cb5c
Yujin830 <rladbwls30@naver.com>
2022-11-14 23:08:55 +0900

    Fix: 구입 금약과 로또 갯수 UI로직 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,14 +2,18 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
+        int buylotto = askmoney();
+        // TODO: 프로그램 구현
+    }
+    public static int askmoney(){
         System.out.println("구입금액을 입력해 주세요.");
         String input = camp.nextstep.edu.missionutils.Console.readLine();
         int buymoney = Integer.parseInt(input), buylotto;
 
         if(buymoney%1000 != 0)
             throw new IllegalArgumentException("[ERROR] 금액은 천원 단위여야 합니다.");
-
         buylotto = buymoney / 1000;
-        // TODO: 프로그램 구현
+        System.out.println("\n"+buylotto+"개를 구매했습니다.");
+        return buylotto;
     }
 }
