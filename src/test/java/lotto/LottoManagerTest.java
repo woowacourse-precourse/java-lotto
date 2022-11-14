54196988e2fc7c2e0083ca800456403290416b59
vcho1958 <gusckd57@gmail.com>
2022-11-15 05:10:24 +0900

    test(LottoManager): 구매금액에 따라 로또개수 메시지 출력

--- src/test/java/lotto/LottoManagerTest.java
+++ src/test/java/lotto/LottoManagerTest.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -20,8 +21,18 @@ class LottoManagerTest extends NsTest {
         });
     }
 
+    @Test
+    @DisplayName("올바른 구매금액 입력 시 구매 메시지 출력")
+    public void printAmountOfLotto(){
+        assertSimpleTest(()->{
+            run("3000");
+            assertThat(output()).contains("3개를 구매했습니다.");
+        });
+    }
+
+
     @Override
     public void runMain() {
-        new LottoManager();
+        new LottoManager().start();
     }
 }
