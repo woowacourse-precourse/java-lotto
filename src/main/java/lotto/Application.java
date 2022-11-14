d3fb4bfab3864e1f0beefe8f97702ba14d176d83
seoeunbae <enyoupark@gmail.com>
2022-11-14 19:43:12 +0900

    feat : 구현 예정 메소드 틀 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,4 +4,25 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public void gameStart(){
+        //TODO: 게임 시작 구현
+    }
+
+    public void saveInput(){
+        //TODO: 입력값 객체에 저장 구현
+    }
+
+    public void publishLotto(){
+        //TODO: 로또 발행 구현
+    }
+
+    public void calculateWinning(){
+        //TODO: 당첨 내역 계산 구현
+    }
+
+    public void calculateRate(){
+        //TODO: 수익률 계산 구현
+    }
+
 }
