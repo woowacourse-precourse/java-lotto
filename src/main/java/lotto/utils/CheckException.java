494699b42436f436e9b081bdf04fa652df4a52db
Go-Jaecheol <gojaech@naver.com>
2022-11-14 19:38:44 +0900

    refactor: checkAmount에서 구입 금액이 0 이하인 경우도 예외 처리

--- src/main/java/lotto/utils/CheckException.java
+++ src/main/java/lotto/utils/CheckException.java
@@ -3,7 +3,7 @@ package lotto.utils;
 public class CheckException {
     public static void checkAmount(int amount) throws IllegalArgumentException {
         try {
-            if ((amount % 1000) > 0) {
+            if (amount <= 0 || (amount % 1000) > 0) {
                 throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해주세요.");
             }
         } catch (NumberFormatException numberFormatException) {
