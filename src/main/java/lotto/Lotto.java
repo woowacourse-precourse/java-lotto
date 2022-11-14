b58aab9222442744e5bfcc5713910047f428e521
michaelp3 <chan-park@hotmail.com>
2022-11-15 01:27:41 +0900

    feat(Lotto.java, Message.java): 로또 번호 검증 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,12 +14,26 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateLottoNumbersSize(numbers);
+        validateLottoNumbersUniqueness(numbers);
+    }
+
+    private void validateLottoNumbersUniqueness(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (Integer number : numbers) {
+            set.add(number);
+        }
+        if (set.size() < numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateLottoNumbersSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
     public List<Integer> getNumbers() {
         List<Integer> clonedNumbers = new LinkedList<>(numbers);
         return clonedNumbers;
--- src/main/java/lotto/Message.java
+++ src/main/java/lotto/Message.java
@@ -2,7 +2,8 @@ package lotto;
 
 public enum Message {
     INPUT_MONEY("구입금액을 입력해 주세요."), ERROR_MONEY("[ERROR] 구입 금액은 0부터 2,147,483,647 사이의 숫자여야 합니다."), OUTPUT_LOTTO_NUMBER(
-            "개를 구매했습니다."),
+            "개를 구매했습니다."), ERROR_LOTTO_DUPLICATE("[ERROR] 로또 번호는 중복되지 않는 값이여야 합니다."), ERROR_LOTTO_NUMBERS_SIZE(
+            "[ERROR] 로또 번호는 6개 선택해야 합니다"),
     ;
 
     final String message;
