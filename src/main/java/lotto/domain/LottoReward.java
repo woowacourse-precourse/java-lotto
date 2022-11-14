4b3ccdc1789a996ac99e32e399b70c0bf1f11a20
whdcks420 <whdcks420@gmail.com>
2022-11-14 22:49:38 +0900

    test: 수익률 테스트

---
+++ src/main/java/lotto/domain/LottoReward.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+public enum LottoRewardMessage {
+    FIRST("6개 일치 (2,000,000,000원) - "),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD("5개 일치 (1,500,000원) - "),
+    FOURTH("4개 일치 (50,000원) - "),
+    FIFTH("3개 일치 (5,000원) - ");
+
+    private final String rewardMessage;
+
+    LottoRewardMessage(String rewardMessage) {
+        this.rewardMessage = rewardMessage;
+    }
+
+    public String getRewardMessage() {
+        return rewardMessage;
+    }
+}
