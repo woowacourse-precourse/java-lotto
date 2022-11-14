028af06e83d0be38b5832f08663188c5c812d457
seonpilKim <ksp970306@gmail.com>
2022-11-14 17:57:27 +0900

    Refactor: 메소드 네이밍 변경 및 static 제거

--- src/main/java/lotto/domain/Seller.java
+++ src/main/java/lotto/domain/Seller.java
@@ -9,16 +9,16 @@ public class Seller {
 	private static final int LOTTO_PRICE = 1_000;
 	private static final String PURCHASE_ANNOUNCEMENT = "%s개를 구매했습니다.";
 
-	private static boolean isNotUnitsOf1000(Integer money) {
-		return money < LOTTO_PRICE || money % LOTTO_PRICE != 0;
-	}
-
 	public List<Lotto> sellLottoTickets(int money) {
-		if (isNotUnitsOf1000(money)) {
+		if (validate(money)) {
 			throw new IllegalArgumentException(ErrorMessage.isNotUnitsOf1000());
 		}
 		System.out.println(String.format(PURCHASE_ANNOUNCEMENT, money / LOTTO_PRICE));
 		return Lotto.newInstances(money / LOTTO_PRICE);
+	}
+
+	private boolean validate(Integer money) {
+		return money < LOTTO_PRICE || money % LOTTO_PRICE != 0;
 	}
 
 }
