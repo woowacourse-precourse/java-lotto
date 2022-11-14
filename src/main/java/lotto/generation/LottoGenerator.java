458f8763db606350167019e339d815cd8304d284
jin8073 <jin8073@gmail.com>
2022-11-14 14:15:02 +0900

    feat(LottoGenerator): 입력 받은 구입 금액에 따라 로또 발행

---
+++ src/main/java/lotto/generation/LottoGenerator.java
@@ -0,0 +1,21 @@
+package lotto.generation;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGenerator {
+
+    public List<Lotto> generateLottos(int purchaseAmount) {
+        int numberOfLotto = purchaseAmount / 1000;
+        List<Lotto> lottos = new ArrayList<>();
+        for (int count = 0; count < numberOfLotto; count++) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
+}
