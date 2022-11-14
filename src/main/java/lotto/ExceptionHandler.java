d8decb0c4d81eee1fc62ddfc49d57396686c9999
youswim <youswim96@gmail.com>
2022-11-15 02:59:34 +0900

    feat(lotto): ExceptionHandler클래스 생성 & 에러 메세지 추가
    ExceptionHandler는 여러 에러 메세지의 상수 문자열을 저장하고 있음.

---
+++ src/main/java/lotto/ExceptionHandler.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class ExceptionHandler {
+
+    public final static String WORNG_PURCHASE_MONEY = "구입 금액은 숫자이고, 1,000원 단위입니다";
+    public final static String WORNG_WIN_NUMBERS = "당첨 숫자는 숫자이고, 콤마로 구분된 6자리 입니다";
+
+
+}
