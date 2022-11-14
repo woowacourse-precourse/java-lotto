1506810ee768c835257a7e52b43aea47eda529e4
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-11-14 16:15:28 +0900

    add : 로또를 구매할 금액을 입력고 int형으로 변환하는 기능

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,5 +9,10 @@ import java.util.List;
 public class InputView {
     private static final Validator validator = new Validator();
 
-
+    public int inputMoney(){
+        System.out.println(LOTTO_START);
+        String input = Console.readLine();
+        validator.moneyValid(input);
+        return Integer.parseInt(input);
+    }
 }
