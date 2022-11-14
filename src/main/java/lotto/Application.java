18993651b311d9b35009baaf82e95456df80546a
josolha <josolha@nate.com>
2022-11-15 01:57:35 +0900

    feat(Application) : 메인어플리케이션 기능 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,40 @@
 package lotto;
 
+import java.util.List;
+import lotto.domain.BuyLotto;
+import lotto.domain.Output;
+import lotto.domain.PrintMessages;
+import lotto.domain.Rank;
+import lotto.domain.WonAnalysis;
+import lotto.domain.WonNumber;
+
 public class Application {
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+
+            System.out.println(PrintMessages.INPUT_MONEY.getMessage());
+
+            BuyLotto by = new BuyLotto();
+            by.GenerateLottoNumber();
+            System.out.println(by.cntLotto + PrintMessages.LOTTO_COUNT.getMessage());
+            by.showLottos();
+
+            System.out.println(PrintMessages.INPUT_NUMBER.getMessage());
+            WonNumber wonNumber = new WonNumber();
+            System.out.println(PrintMessages.INPUT_BONUS_NUMBER.getMessage());
+            wonNumber.bonusNumber();
+
+            WonAnalysis wonAnalysis = new WonAnalysis();
+            List<Rank> Result = wonAnalysis.Result(wonNumber.wonNumber, by.lottoList, wonNumber.bonusNumber);
+
+            Output output = new Output();
+            output.printLottoResult(Result);
+            String percent = wonAnalysis.totalPercent(Result);
+            output.printPercent(percent);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
