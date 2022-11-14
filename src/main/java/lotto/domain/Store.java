e3d241f69f6fe71e3847bd5910e9d5af2d9588a4
ParkRootSeok <g_e_un@naver.com>
2022-11-14 23:36:34 +0900

    refactor(Store) : 로또 번호 발행 기능 로직 수정

--- src/main/java/lotto/domain/Store.java
+++ src/main/java/lotto/domain/Store.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static lotto.config.Constant.LOTTO_NUMBER_CAPACITY;
 
@@ -13,18 +14,18 @@ public class Store {
     private static final int LOTTO_START_NUMBER = 1;
     private static final int LOTTO_END_NUMBER = 45;
 
-    List<Integer> numbers = new ArrayList<>();
+    List<List<Integer>> numbers = new ArrayList<>();
 
-    public Store() {
+    public void drawNumber(int hasLotto) {
+        int i;
+        for (i = 0;i < hasLotto;i++) {
+            numbers.add(Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, LOTTO_NUMBER_CAPACITY));
+            numbers.get(i).stream().sorted().collect(Collectors.toList());
+        }
     }
 
-    public List<Integer> drawNumber() {
-
-        numbers = Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, LOTTO_NUMBER_CAPACITY);
-        Collections.sort(numbers);
-
+    public List<List<Integer>> getNumbers() {
         return numbers;
-
     }
 
 }
