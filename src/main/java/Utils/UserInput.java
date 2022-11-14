307d547f2cb741e8aa47cc2f4935db5860e8971e
MinSungKim <timkim01@naver.com>
2022-11-14 15:09:39 +0900

    feat: 사용자 입력금액이 1000단위인지 유효성검사 기능 구현

--- src/main/java/Utils/UserInput.java
+++ src/main/java/Utils/UserInput.java
@@ -14,11 +14,19 @@ public class UserInput {
     // 사용자 입력이 숫자인지 유효성 검사
     private void validateInputIsNum(String userInput) {
         try {
-            int StringToInt = Integer.parseInt(userInput);
-        } catch (Exception e){
+            Integer.parseInt(userInput);
+        } catch (Exception e) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
         }
+    }
 
+    // 사용자 입력금액이 1000으로 나눠 떨어지는지 유효성 검사
+    private void validateInputMoney(String userInput) {
+        int userInputMoney = Integer.parseInt(userInput);
+
+        if (userInputMoney % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000원 단위로만 입력해주세요.");
+        }
     }
 
 
