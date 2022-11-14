648dd703484e3d0ded1e84d8bac7210fdc8dede2
baekeunsun <eunsun5460@gmail.com>
2022-11-14 18:55:46 +0900

    feat(model/Lotto): 로또 번호가 1부터 45 사이가 아닐 경우 예외처리
    - (Lotto/InRange): 45보다 크거나 1보다 작으면 에러문구 출력

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        InRange(numbers);
         this.numbers = numbers;
     }
 
@@ -21,6 +23,15 @@ public class Lotto {
             return true;
         }
         return false;
+    }
+
+    private void InRange(List<Integer> numbers) {
+        if (Collections.max(numbers) > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 45를 넘어 범위를 벗어났습니다.");
+        }
+        if (Collections.min(numbers) < 1) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 1보다 작아 범위를 벗어났습니다.");
+        }
     }
 
     // TODO: 추가 기능 구현
