8970e257446796b137b9d70c719c356a3b4099ed
HyeonjungYun <guswnd9971@gmail.com>
2022-11-14 15:21:56 +0900

    "feat(Application): Model클래스에서 값을 받아 View클래스로 값을 보내는 메서드 checkLotto() 추가"

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Application {
+
+    private List<List<Integer>> LOTTOS;
+    private HashMap<Integer, Integer> PRIZE_RANKINGS;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        checkLotto();
+    }
+    public static void checkLotto () {
+        Application app = new Application();
+        View view = new View();
+
+        Model model = new Model();
+        Money money = new Money(view.inputMoney());
+        app.LOTTOS = model.getUsersLottos(money.getMoney());
+
+        view.printUncheckedLotto(app.LOTTOS);
+
+        Lotto lotto = new Lotto(view.inputLottoNumber());
+        Bonus bonus = new Bonus(view.inputBonusNumber(), lotto.getLottoNumbers());
+        app.PRIZE_RANKINGS = model.checkPrize(money.getMoney(), bonus.getBonus(), lotto.getLottoNumbers());
+
+        view.printResult(app.PRIZE_RANKINGS, model.getRateOfProfit());
     }
 }
