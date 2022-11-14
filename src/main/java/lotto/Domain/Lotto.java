8a31f1220d784d616dafc3f6f3dd9f69565a2b4e
jungbum05114@naver.com <jungbum05114@gmail.com>
2022-11-14 22:02:13 +0900

    overlap구현완료(로도번호중 중복숫자가 발생하면 안된다)

--- src/main/java/lotto/Domain/Lotto.java
+++ src/main/java/lotto/Domain/Lotto.java
@@ -1,12 +1,15 @@
 package lotto.Domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        overlap(numbers);
         this.numbers = numbers;
     }
 
@@ -15,6 +18,15 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
+
+    private void overlap(List<Integer> numbers){
+        Set<Integer> newNumList = new HashSet<>(numbers);
+        if(numbers.size()!=newNumList.size())
+        {
+            throw new IllegalArgumentException();
+        }
+    }
+
 
     // TODO: 추가 기능 구현
 }
