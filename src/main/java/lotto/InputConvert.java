1b0c9a80c6d95a327bd83cb2ce203cc85e2a0c08
MuSeung <peter0757@nvaer.com>
2022-11-14 11:23:35 +0900

    feat: InputConvert가 에러를 발생시키는 기능 구현
    InputConvert
        - lottoNumber : String을 받아서 List를 리턴하고 항상 에러가 발생하도록 기능 구현

---
+++ src/main/java/lotto/InputConvert.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import java.util.List;
+
+public class InputConvert {
+
+    public static List<Integer> lottoNumber(String input) {
+        if(true) {
+            throw new IllegalArgumentException();
+        }
+        return List.of();
+    }
+}
