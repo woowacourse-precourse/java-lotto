20f649aa23577e4681138a8e274c7d9bf5d74bcb
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-14 17:15:39 +0900

    feat($LottoTest) : implement test for money input
    - testing for money input is correctly carrying out

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,20 +1,25 @@
 package lotto;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
+    @Disabled
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
+    @Disabled
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
@@ -23,5 +28,12 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("입력 금액을 받는다.")
+    @Test
+    void CasherMoneyInput() {
+        int result = 8_000;
+        MoneyReceiver moneyReceiver = new MoneyReceiver;
+        assertThat(result).isEqualTo(moneyReceiver.getMoney);
+    }
+
 }
