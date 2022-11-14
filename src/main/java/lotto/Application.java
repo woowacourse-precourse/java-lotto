4c08e0a03868dffdcd43e516fd50123628040a46
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-14 11:15:38 +0900

    feat: 프로젝트 시작 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.domain.LottoMachine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoMachine lottoNews = new LottoMachine();
+        lottoNews.start();
     }
 }
