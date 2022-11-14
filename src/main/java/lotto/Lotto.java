9a3d441c995fcea8e0fdd1de293659eb6af2fd77
korjun1993 <corojoon93@naver.com>
2022-11-14 20:49:51 +0900

    feat(lotto) : 로또 번호가 1부터 45 사이의 값을 벗어나면 예외 처리하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Lotto {
     private static final int SIZE = 6;
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,12 +14,16 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (isInValidSize(numbers)) {
+        if (isInValidSize(numbers) || hasOutOfBoundNumber(numbers)) {
             throw new IllegalArgumentException();
         }
     }
 
     private static boolean isInValidSize(List<Integer> numbers) {
         return numbers.size() != SIZE;
+    }
+
+    private static boolean hasOutOfBoundNumber(List<Integer> numbers) {
+        return numbers.stream().anyMatch(x -> x < START_NUMBER || x > END_NUMBER);
     }
 }
