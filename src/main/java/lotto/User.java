16bb696c7135aa3c143598ee1b0d7fb1a173a2cc
changeme4585 <benscience@naver.com>
2022-11-14 16:06:52 +0900

    입력값 예외처리 기능 추가

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -16,7 +16,22 @@ public class User {
             }
             return 0;
          }
+        if (checkDevide(user) == 0) {
+            try {
+                throw new IllegalArgumentException("[ERROR] 천원단위로 입력해주세요");
+            }catch (Exception e){
+                System.out.println(e);
+            }
+            return 0;
+        }
         return Integer.valueOf(user)/1000;
+        }
+        public static int checkDevide(String user) {
+        //1000으로 나누어 떨어지는지 검사
+            if(Integer.valueOf(user)%1000!=0){
+                return 0;
+                }
+            return 1;
         }
         public static int checkNum (String num){  //숫자인지 판단
             for (int idx = 0; idx < num.length(); idx++) {
