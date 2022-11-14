a9a1dff0c52d93a33cd30b041c310fcc6ef9fe32
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-14 18:50:13 +0900

    feat(MoneyReceiver) : implement receiving money

---
+++ src/main/java/lotto/view/MoneyReceiver.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class MoneyReceiver {
+    public static String getMoney() {
+        String userInput = readLine();
+        return userInput;
+    }
+}
