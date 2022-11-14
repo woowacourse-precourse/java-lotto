55ca7ef6ae0cccce6b490f45146daa8ca5bcbaa6
vcho1958 <gusckd57@gmail.com>
2022-11-15 07:03:02 +0900

    test(AnswerNumberList): 보너스 번호가 당첨목록 안에 있을 경우 예외 반환

--- src/test/java/lotto/AnswerNumberListTest.java
+++ src/test/java/lotto/AnswerNumberListTest.java
@@ -11,12 +11,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AnswerNumberListTest extends NsTest {
-    private static final String ERROR_MESSAGE = "[ERROR]";
 
     @Test
     @DisplayName("당첨번호 목록에 음수가 있을 경우 예외발생")
     public void checkNegativeInWinningNumbers() {
-        assertThatThrownBy(() -> new AnswerNumberList(List.of(-2L, 3L, 2L, 1L, 4L, 5L), 4L)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new AnswerNumberList(List.of(-2L, 3L, 2L, 1L, 4L, 5L), 7L)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -34,7 +33,13 @@ class AnswerNumberListTest extends NsTest {
     @Test
     @DisplayName("당첨 번호 목록에 범위밖 수가 있을 경우 예외 발생")
     public void checkRangeInWinningNumbers() {
-        assertThatThrownBy(() -> new AnswerNumberList(List.of(46L, 3L, 2L, 1L, 4L, 5L), 4L)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new AnswerNumberList(List.of(46L, 3L, 2L, 1L, 4L, 5L), 7L)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("당첨 번호 목록에 보너스 번호가 있을 경우 예외 발생")
+    public void checkBonusNumberInWinningNumbers() {
+        assertThatThrownBy(() -> new AnswerNumberList(List.of(12L, 3L, 2L, 1L, 4L, 5L), 4L)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
