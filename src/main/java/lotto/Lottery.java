e9dadc92fe28cd3743892db152d884fefbbae37f
= <kimje1109@gmail.com>
2022-11-15 01:31:05 +0900

    feat: 당첨 내역을 계산하는 기능 구현

--- src/main/java/lotto/Lottery.java
+++ src/main/java/lotto/Lottery.java
@@ -2,9 +2,19 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import javax.swing.text.StyledEditorKit.BoldAction;
 
 public class Lottery {
     static ArrayList<Lotto> lottery = new ArrayList<>();
+    static HashMap<String, Integer> gradeCount = new HashMap<>() {{
+        put("FIFTH", 0);
+        put("FOURTH", 0);
+        put("THIRD", 0);
+        put("SECOND", 0);
+        put("FIRST", 0);
+    }};
 
     public Lottery(int purchaseNumber) {
         for (int index = 0; index < purchaseNumber; index++) {
@@ -15,5 +25,59 @@ public class Lottery {
 
     public ArrayList<Lotto> getLottery() {
         return lottery;
+    }
+
+    public int calcWinningCount(User user, Lotto lotto) {
+        List<Integer> winningNumbers = user.getWinningNumbers();
+        int matchWinningCount = 0;
+
+        for (int winningNumberIndex = 0; winningNumberIndex < 6; winningNumberIndex++) {
+            if (lotto.getNumbers().contains(winningNumbers.get(winningNumberIndex))) {
+                    matchWinningCount++;
+            }
+        }
+        return matchWinningCount;
+    }
+
+    public boolean isBonus(User user, Lotto lotto) {
+        List<Integer> winningNumbers = user.getWinningNumbers();
+        int bonusNumber = user.getBonusNumber();
+        boolean isBonus = false;
+
+        if (lotto.getNumbers().contains(bonusNumber)) {
+            isBonus = true;
+        }
+
+        return isBonus;
+    }
+
+    public void calcGrade(User user) {
+        int winningCount = 0;
+        boolean isBonus = false;
+
+        for (Lotto lotto : lottery) {
+            winningCount = calcWinningCount(user, lotto);
+            isBonus = isBonus(user, lotto);
+            addGrade(winningCount, isBonus);
+        }
+        System.out.println(gradeCount);
+    }
+
+    public void addGrade(int winningCount, boolean isBonus) {
+        if (winningCount == 6) {
+            gradeCount.put("FIRST", 1);
+        }
+        if (winningCount == 5 && isBonus) {
+            gradeCount.put("SECOND", 1);
+        }
+        if (winningCount == 5 && !isBonus) {
+            gradeCount.put("THIRD", 1);
+        }
+        if (winningCount == 4) {
+            gradeCount.put("FOURTH", 1);
+        }
+        if (winningCount == 3) {
+            gradeCount.put("FIFTH", 1);
+        }
     }
 }
