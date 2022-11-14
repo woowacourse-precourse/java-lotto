9a0a8f48708f5e8d02f8a3a0b657be8839508d6e
김태용 <amico741@naver.com>
2022-11-14 16:35:41 +0900

    feat: 로또게임 실행 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,15 @@
 package lotto;
 
 import lotto.controller.Controller;
+import lotto.domain.Buyer;
+import lotto.domain.Rank;
+import lotto.service.LottoService;
+import lotto.view.View;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        new Controller().startGame();
+        Controller controller = new Controller(new LottoService(), new Buyer(), new View(), new Rank());
+        controller.startGame();
     }
 }
--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,26 +1,36 @@
 package lotto.controller;
 
 import lotto.domain.Buyer;
+import lotto.domain.Rank;
 import lotto.service.LottoService;
 import lotto.view.View;
 
 public class Controller {
-    private final Buyer buyer;
     private final LottoService lottoService;
-
+    private final Buyer buyer;
     private final View view;
+    private final Rank rank;
 
-    public Controller() {
-        this.buyer = new Buyer();
-        this.lottoService = new LottoService();
-        this.view = new View();
+    public Controller(LottoService lottoService, Buyer buyer, View view, Rank rank) {
+        this.lottoService = lottoService;
+        this.buyer = buyer;
+        this.view = view;
+        this.rank = rank;
     }
 
     public void startGame() {
-        view.printWelcomeMessage();
-        String input = buyer.inputMoney();
-        lottoService.getLottosByMoney(buyer, input);
-        view.printLottoCountMessage(buyer.getLottoCount());
-        view.printLottosInfoByBuyer(buyer.getLottos());
+        try {
+            view.printWelcomeMessage();
+            lottoService.getLottosByMoney(buyer);
+            view.printLottoCountMessage(buyer.getLottoCount());
+            view.printLottosInfoByBuyer(buyer.getLottos());
+            lottoService.publishWinningNumbersWithBonusNumber(view);
+            view.printWinningStatics();
+            lottoService.getWinningStatics(buyer, rank);
+            view.printWinningCount(rank);
+            view.printRateOfReturn(buyer, rank);
+        } catch(IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
