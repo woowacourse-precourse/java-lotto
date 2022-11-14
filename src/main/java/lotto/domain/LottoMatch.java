68a08235b280dd8eb47f4109f0532bdfc27e37ee
Isu Shin <isu010614@naver.com>
2022-11-15 07:07:11 +0900

    feat: 로또 번호와 당첨 번호를 비교하여 개수를 카운트하는 기능 추가

--- src/main/java/lotto/domain/LottoMatch.java
+++ src/main/java/lotto/domain/LottoMatch.java
@@ -1,8 +1,18 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class LottoMatch {
+    public static List<Integer> putMatchNumbers(final List<Lotto> allLotto, final Lotto winningNumber) {
+        List<Integer> matchNumbers = new ArrayList<>();
+        for (Lotto lotto : allLotto) {
+            matchNumbers.add(countMatch(lotto, winningNumber));
+        }
+        return matchNumbers;
+    }
+
     public static int countMatch(final Lotto lotto, final Lotto winningNumber) {
         return (int) lotto.get()
                 .stream()
