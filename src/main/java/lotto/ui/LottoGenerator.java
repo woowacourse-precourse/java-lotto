90508b5a886ef46ac6ad1a005a1c3e99c3d6f13e
SeongukBaek <bsu6235@gmail.com>
2022-11-14 21:32:37 +0900

    refactor: modify method to return sorted List

--- src/main/java/lotto/ui/LottoGenerator.java
+++ src/main/java/lotto/ui/LottoGenerator.java
@@ -1,2 +1,14 @@
-package lotto.ui;public class LottoGenerator {
+package lotto.ui;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class LottoGenerator {
+    public static List<Integer> generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        numbers.sort(Comparator.naturalOrder());
+        return numbers;
+    }
 }
