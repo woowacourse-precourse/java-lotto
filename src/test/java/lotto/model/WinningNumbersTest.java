a163b43148670c2116abb4dd83b205c833544868
dhkim <akdlzmf1123@gmail.com>
2022-11-14 19:40:09 +0900

    test(WinningNumbersTest): private 메서드에 대한 테스트 제거

--- src/test/java/lotto/model/WinningNumbersTest.java
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -13,25 +13,14 @@ class WinningNumbersTest {
     );
 
     @Test
-    void 어떤_숫자가_당첨번호_혹은_보너스인지_확인하는_기능() {
+    void 임의의_숫자가_당첨번호인지_보너스인지_확인하는_기능() {
         CompareResult compareResult = new CompareResult(0, false);
         compareResult = winningNumbers.judgeWinningNumberOrBonus(1, compareResult);
         compareResult = winningNumbers.judgeWinningNumberOrBonus(7, compareResult);
         compareResult = winningNumbers.judgeWinningNumberOrBonus(0, compareResult);
         compareResult = winningNumbers.judgeWinningNumberOrBonus(9, compareResult);
         compareResult = winningNumbers.judgeWinningNumberOrBonus(3, compareResult);
+
         assertThat(compareResult).isEqualTo(new CompareResult(2, true));
     }
-
-//    @ParameterizedTest
-//    @CsvSource({"1,true", "2,true", "3,true", "4,true", "5,true", "6,true", "7,false", "0,false"})
-//    void 어떤_숫자가_당첨번호에_포함되는지_확인한다(int number, boolean expected) {
-//        assertThat(winningNumbers.isWinningNumber(number)).isEqualTo(expected);
-//    }
-//
-//    @ParameterizedTest
-//    @CsvSource({"7,true", "2,false", "3,false", "11,false", "-3,false", "20,false"})
-//    void 어떤_숫자가_보너스번호인지_확인한다(int number, boolean expected) {
-//        assertThat(winningNumbers.isBonusNumber(number)).isEqualTo(expected);
-//    }
 }
