4cf679e680c0db46741ab8a7389993d68da1d3c9
TaemHam <tk4003950@gmail.com>
2022-11-14 23:39:40 +0900

    Docs: InputReader 주석 README로 이동

--- src/main/java/utils/InputReader.java
+++ src/main/java/utils/InputReader.java
@@ -2,13 +2,6 @@ package utils;
 
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 만약 외부 라이브러리에 의존해야하는 기능이 있다면,
- * 라이브러리가 더이상 서비스되지 않을 때를 대비해
- * 메서드를 코드 내에 직접 삽입하지 않고
- * 따로 관리하는 것이 유지보수에 좋을 것 같다고 생각했습니다.
- */
-
 public class InputReader {
     public static String readLine () {
         return Console.readLine();
