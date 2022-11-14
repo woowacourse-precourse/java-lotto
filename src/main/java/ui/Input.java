e65527475b4a1f29a471495353b9d48133fabef1
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-15 01:17:11 +0900

    feat(Input) : add a function that gets input for winning number from user

--- src/main/java/ui/Input.java
+++ src/main/java/ui/Input.java
@@ -1,11 +1,20 @@
 package ui;
 
 import camp.nextstep.edu.missionutils.Console;
+import controller.InputNumber;
+
+import java.util.List;
 
 public class Input {
 
-    int getMoney(){
+    int getMoney() {
         int money = Integer.parseInt(Console.readLine());
         return money;
+    }
+
+    List<Integer> getWinningNumber(){
+        String userInput = Console.readLine();
+        List<Integer> winningNumber = InputNumber.parseWinningNumber(userInput);
+        return winningNumber;
     }
 }
