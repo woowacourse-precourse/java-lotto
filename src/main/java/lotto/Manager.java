4972b070b06fdfbee00abe322d740bf03cb3c9d2
swjeong98 <tree5678@konkuk.ac.kr>
2022-11-14 17:26:37 +0900

    feat(Manager) : 주어진 리스트에 대해 원소가 모두 숫자인지 판단하는 기능 구현

---
+++ src/main/java/lotto/Manager.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.List;
+import lotto.Lotto;
+
+public class Manager {
+    public boolean isNumber(String str){
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i) >= '0' && str.charAt(i) <= '9') continue;
+            return false;
+        }
+        return true;
+    }
+
+    public boolean ValidityOfNumber(List<String> list){
+        for(int i=0; i<list.size(); i++){
+            if(isNumber(list.get(i))) continue;
+            return false;
+        }
+        return true;
+    }
+}
