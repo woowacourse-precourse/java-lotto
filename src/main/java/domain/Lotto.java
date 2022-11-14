b135282af0495460571150001796a30cd3c51014
sugamypapa <sk000801@naver.com>
2022-11-14 11:04:55 +0900

    feat: 로또 예외 처리 메시지 추가

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,5 +1,7 @@
 package domain;
 
+import view.ValidMessage;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -16,31 +18,37 @@ public class Lotto {
         return this.numbers;
     }
 
-    private void validateLength(List<Integer> numbers) {
+    private boolean validateLength(List<Integer> numbers) {
+        boolean isValid = false;
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 숫자의 개수는 6개여야 합니다.");
+            isValid = true;
         }
+        return isValid;
     }
 
-    private void validateRepeat(List<Integer> numbers) {
+    private boolean validateRepeat(List<Integer> numbers) {
+        boolean isValid = false;
         for (int i = 0; i < numbers.size(); i++) {
             if (Collections.frequency(numbers, numbers.get(i)) > 1) {
-                throw new IllegalArgumentException("[ERROR] 로또 숫자는 중복이 되어선 안됩니다.");
+                isValid = true;
             }
         }
+        return isValid;
     }
 
-    private void validateOver(List<Integer> numbers) {
+    private boolean validateOver(List<Integer> numbers) {
+        boolean isValid = false;
         for (int i = 0; i < numbers.size(); i++) {
             if (numbers.get(i) < 1 && numbers.get(i) > 45) {
-                throw new IllegalArgumentException("[ERROR] 로또 숫자는 1부터 46까지의 수 중 하나입니다");
+                isValid = true;
             }
         }
+        return isValid;
     }
 
     private void checkValidateLotto(List<Integer> numbers) {
-        validateLength(numbers);
-        validateRepeat(numbers);
-        validateOver(numbers);
+        if (validateLength(numbers) || validateRepeat(numbers) || validateOver(numbers)) {
+            throw new IllegalArgumentException(ValidMessage.ValidGenerateLotto.getPrintMessage());
+        }
     }
 }
