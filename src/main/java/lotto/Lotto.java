e6f8236e0c6c9cfa7dd5e2f1f93c67afac6d8f89
김선동 <mok03127@gmail.com>
2022-11-14 16:07:09 +0900

    feat: 로또 번호의 숫자 범위 예외 처리 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,10 +5,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final String LOTTO_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateLottoRange(numbers);
+
         ArrayList<Integer> sortedNumbers = new ArrayList<>(numbers);
         Collections.sort(sortedNumbers);
         this.numbers = numbers;
@@ -19,4 +24,16 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
+
+
+
+    private void validateLottoRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException(LOTTO_RANGE_ERROR_MESSAGE);
+            }
+        }
+    }
+
+
 }
