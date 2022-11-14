67aee225119aff0a1117b2f25697e80493592191
gudals <kimtuti@naver.com>
2022-11-14 16:00:32 +0900

    feat(lotto): add powerball purchase function
    로또 구매 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class Lotto {
@@ -17,4 +19,16 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void buyLotto(int purchaseMoney){
+        int purchaseAmount = purchaseMoney/1000;
+        //purchaseAmount를 인자로 받아서 list를 반환하는 로또 뽑기 기능 추가
+
+    }
+    private int inputMoney(){
+        int purchaseMoney = Integer.parseInt(Console.readLine());
+        if (purchaseMoney%1000!=0){
+            throw new IllegalArgumentException("[ERROR] 1000원 단위의 금액을 입력하십시오.");
+        }
+        return purchaseMoney;
+    }
 }
