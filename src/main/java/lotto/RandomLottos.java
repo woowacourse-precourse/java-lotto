47aff8b09dbe28c7323ad2e22a1a131aba24c852
solesy <tkddbs9801@gmail.com>
2022-11-14 15:39:34 +0900

    feat: create class saving random lottos
    적절하지 않은 돈이 들어왔는지 확인한다.

---
+++ src/main/java/lotto/RandomLottos.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+public class RandomLottos {
+    private List<Lotto> randomLottos;
+    public RandomLottos(int money){
+        validateMoney(money);
+        for(int i = 0; i < money/1000; ++i) {
+            randomLottos.add(new Lotto(pickUniqueNumbersInRange(1, 45, 6)));
+        }
+    }
+    private void validateMoney(int money){
+        if(money < 0){
+            throw new IllegalArgumentException("[ERROR] 음수가 아닌 정수를 입력해야 합니다.");
+        }
+        if(money % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 구입금액은 1000으로 나누어 떨어져야 합니다.");
+        }
+    }
+    public List<Lotto> getRandomLottos(){
+        return this.randomLottos;
+    }
+}
