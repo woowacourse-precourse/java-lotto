eec0d9953bcc0d05bbd8cfb83d0a33936dda2946
im-shung <seoyoung1113@gmail.com>
2022-11-14 18:12:56 +0900

    test: :white_check_mark: 당첨 내역을 출력하는 테스트 구현

--- src/test/java/lotto/domain/PrizeCountTest.java
+++ src/test/java/lotto/domain/PrizeCountTest.java
@@ -13,16 +13,24 @@ class PrizeCountTest {
     @BeforeEach
     void setUp() {
         prizeCount.addPrizeCount(Prize.FIFTH);
-        prizeCount.addPrizeCount(Prize.FIFTH);
     }
-
 
     @DisplayName("$등수의 합산 금액이 맞는지 확인하기")
     @Test
     void getTotalPrizeMoney() {
-        assertThat(prizeCount.getTotalPrizeMoney(Prize.FIFTH)).isEqualTo(Prize.FIFTH.getMoney() * 2);
+        assertThat(prizeCount.getTotalPrizeMoney(Prize.FIFTH)).isEqualTo(
+                Prize.FIFTH.getMoney());
     }
 
-
+    @DisplayName("당첨 내역을 출력한다.")
+    @Test
+    void testToString() {
+        assertThat(prizeCount.toString()).isEqualTo(
+                "3개 일치 (5,000원) - 1개\n"
+                + "4개 일치 (50,000원) - 0개\n"
+                + "5개 일치 (1,500,000원) - 0개\n"
+                + "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n"
+                + "6개 일치 (2,000,000,000원) - 0개");
+    }
 
 }
