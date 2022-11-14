28e3c5bd4f48f52a84a6e259585730087889c75f
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-11-15 06:16:32 +0900

    feat: 총 상금 계산하기 위한 function 생성

--- src/main/java/lotto/LottoWin.java
+++ src/main/java/lotto/LottoWin.java
@@ -1,2 +1,14 @@
-package lotto;public enum LottoWin {
+package lotto;
+
+import java.util.List;
+
+public class LottoWin {
+    public static Integer getTotalPrize(List<Lotto> lottos, Lotto luckyNums, int bonusNum) {
+        int prizeMoney = 0;
+
+        for (Lotto lotto : lottos) {
+            prizeMoney += getPrizeMoney(lotto, luckyNums, bonusNum);
+        }
+        return prizeMoney;
+    }
 }
