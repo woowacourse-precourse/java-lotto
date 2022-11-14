6a30d88409dd034c50cc420052f2618170083745
dong yeol <ehfql6363@naver.com>
2022-11-15 02:40:41 +0900

    FEAT(Control.java) : Move validation method (to Validation class)
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/lotto/Control.java
+++ src/main/java/lotto/Control.java
@@ -2,23 +2,35 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import static lotto.Lotto.*;
+import static Comment.GameMessage.*;
+import static Comment.ErrorMessage.*;
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.Validation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.PatternSyntaxException;
 import java.util.stream.Collectors;
 
 public class Control {
-    private final String START_MESSAGE = "구입금액을 입력해 주세요.";
-    private final String CONFIRM_MESSAGE = "개를 구매했습니다.";
-    private final String INPUT_LOTTO = "당첨 번호를 입력해 주세요.";
-    private final String INPUT_BONUS = "보너스 번호를 입력해 주세요.";
-    private final String WINNING_STATISTICS = "당첨 통계\n---";
+
+
+    private int price;
+    private double reward;
+    private double rateOfReturn;
+    private int first = 0;
+    private int second = 0;
+    private int third = 0;
+    private int forth = 0;
+    private int fifth = 0;
+
+    private String winningNumber;
+    private String bonusNumber;
 
     private List<Lotto> lotteries;
-    private int price;
+    private Lotto winningNumbers;
 
     public Control() {
 
@@ -28,28 +40,40 @@ public class Control {
     public void play() {
         System.out.println(START_MESSAGE);
         String cost = readLine();
-        buyLotto(cost);
 
+        buyLotto(cost);
         createLotto();
 
         printMyLotto();
 
+        getWinningNumber();
+
+        checkWinningNumber();
+
+        printWinning();
     }
 
     /* 복권 구매 */
     private void buyLotto(String cost) {
-        price = Integer.parseInt(cost);
+        try {
+            price = Integer.parseInt(cost);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+        }
+
         buyValidation();
     }
 
     /* 지불한 금액만큼 로또 생성 */
     private void createLotto() {
-        int item = price % 1_000;
-        System.out.println(item + CONFIRM_MESSAGE);
+        lotteries = new ArrayList<>();
+
+        int item = price / 1_000;
+
         System.out.println();
+        System.out.println(item + CONFIRM_MESSAGE);
 
         for (int i = 0; i < item; i++) {
-            lotteries = new ArrayList<>();
             Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             lotteries.add(lotto);
         }
@@ -60,30 +84,96 @@ public class Control {
         for (Lotto lotto : lotteries) {
             System.out.println(lotto.getNumbers());
         }
-        System.out.println();
+    }
+
+    /* 당첨 번호 입력 */
+    private void getWinningNumber() {
+        System.out.println(INPUT_LOTTO);
+        winningNumber = readLine();
+
+        winningNumbers = new Lotto(stringToIntegerList(winningNumber));
+
+        System.out.println(INPUT_BONUS);
+        bonusNumber = readLine();
+    }
+
+    /* 당첨 통계 출력 */
+    private void printWinning() {
+        StringBuilder stringBuilder = new StringBuilder();
+        rateOfReturn = (reward / price) * 100; // 문제
+
+        System.out.println(WINNING_STATISTICS);
+        stringBuilder.append(WINNING_LEVEL_5).append(fifth).append("개\n");
+        stringBuilder.append(WINNING_LEVEL_4).append(forth).append("개\n");
+        stringBuilder.append(WINNING_LEVEL_3).append(third).append("개\n");
+        stringBuilder.append(WINNING_LEVEL_2).append(second).append("개\n");
+        stringBuilder.append(WINNING_LEVEL_1).append(first).append("개\n");
+        stringBuilder.append("총 수익률은 ").append(String.format("%.1f", rateOfReturn)).append("%입니다.");
+
+        System.out.println(stringBuilder);
+    }
+
+    /* 당첨 확인 */
+    private void checkWinningNumber() {
+        List<Integer> matchingNumber;
+
+        for (Lotto myLotto : lotteries) {
+            matchingNumber = winningNumbers.getNumbers().stream()
+                    .filter(old -> myLotto.getNumbers().stream()
+                            .anyMatch(Predicate.isEqual(old)))
+                    .collect(Collectors.toList());
+
+            int matchingCount = matchingNumber.size();
+
+            Rank rank = myLotto.getRank(matchingCount, Integer.parseInt(bonusNumber));
+
+            countRank(rank);
+        }
+    }
+
+    /* 통계 집계 */
+    private void countRank(Rank rank) {
+        if (rank.equals(Rank.FIRST)) {
+            first++;
+        }
+
+        if (rank.equals(Rank.SECOND)) {
+            second++;
+        }
+
+        if (rank.equals(Rank.THIRD)) {
+            third++;
+        }
+
+        if (rank.equals(Rank.FOURTH)) {
+            forth++;
+        }
+
+        if (rank.equals(Rank.FIFTH)) {
+            fifth++;
+        }
+
+        reward += rank.getWinningMoney();
     }
 
     /* 구매 검증 */
     private void buyValidation() {
-        if (price < 1_000) {
-            throw new IllegalArgumentException("[ERROR] 로또 최소 구매 금액은 1,000원 입니다.");
-        }
+        minPriceValidation(price);
 
-        if (price > 100_000) {
-            throw new IllegalArgumentException("[ERROR] 로또는 1회 최대 100,000원까지 구매 가능합니다.");
-        }
+        maxPriceValidation(price);
 
-        if (price % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 저희 복권 판매점은 거슬러 주지 않습니다.");
-        }
+        monetaryUnitValidation(price);
     }
 
     /* 문자열을 정수 리스트로 변환 */
     private List<Integer> stringToIntegerList(String lottoNumber) {
-        String[] lottoNumbers = lottoNumber.split(",");
-        return Arrays
-                .stream(lottoNumbers)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        try {
+            String[] lottoNumbers = lottoNumber.split(",");
+            return Arrays.stream(lottoNumbers)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (PatternSyntaxException e) {
+            throw new IllegalArgumentException(INPUT_LOTTO_ERROR);
+        }
     }
 }
