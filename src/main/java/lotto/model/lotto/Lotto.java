e769e7cef909feb5059a2c787ef71af948a27fab
ESeokhwan <huan99@hanmail.net>
2022-11-14 12:35:22 +0900

    feat: Lotto class 기본 메소드 추가
    - 생성자 추가 (validate 로직 포함)
    - numbers getter 추가

--- src/main/java/lotto/model/lotto/Lotto.java
+++ src/main/java/lotto/model/lotto/Lotto.java
@@ -1,7 +1,9 @@
 package lotto.model.lotto;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     public static final int LOTTO_NUMBER_LENGTH = 6;
@@ -12,19 +14,53 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
-    }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        this.numbers = new ArrayList<>();
+        this.numbers.addAll(numbers);
     }
 
     public List<Integer> getNumbers() {
-        // TODO 구현 필요
-        return new ArrayList<>();
+        List<Integer> output = new ArrayList<>(numbers);
+        return output;
     }
 
-    // TODO: 추가 기능 구현
+    private void validate(List<Integer> numbers) {
+        if(doesNotHaveRightLength(numbers)) {
+            throw new IllegalArgumentException("로또 번호는 " + Lotto.LOTTO_NUMBER_LENGTH + "개여야 합니다.");
+        }
+        if(hasNumberOutOfRange(numbers)) {
+            throw new IllegalArgumentException("로또 번호는 " + Lotto.MINIMUM_NUMBER + "부터 " + Lotto.MAXIMUM_NUMBER + " 사이의 숫자여야 합니다.");
+        }
+        if(haveDuplicate(numbers)) {
+            throw new IllegalArgumentException("로또 번호가 중복되어서는 안 됩니다.");
+        }
+    }
+
+    private boolean doesNotHaveRightLength(List<Integer> numbers) {
+        return numbers.size() != Lotto.LOTTO_NUMBER_LENGTH;
+    }
+
+    private boolean isOutOfRange(int number) {
+        return number < Lotto.MINIMUM_NUMBER || number > Lotto.MAXIMUM_NUMBER;
+    }
+
+    private boolean hasNumberOutOfRange(List<Integer> numbers) {
+        for(int number : numbers) {
+            if(isOutOfRange(number)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean haveDuplicate(List<Integer> numbers) {
+        Set<Integer> tmpSet = new HashSet<>();
+
+        for (int number : numbers) {
+            if (!tmpSet.add(number)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
