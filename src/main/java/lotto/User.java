344c608347d12dbea49d2676e5cf5b120a331c77
Danden1 <goh1211@ajou.ac.kr>
2022-11-14 22:43:02 +0900

    feat :사용자가 로또를 n 번 구매하는 기능 추가

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,19 +1,37 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class User {
+
+    private static  final int UNIT_PRICE = 1000;
     private int purchasePrice;
-    private List<List<Integer>> userLotto;
+    private List<List<Integer>> userLottos;
 
     User(int purchasePrice){
         validate(purchasePrice);
+        this.purchasePrice = purchasePrice;
 
+        int count = purchasePrice / UNIT_PRICE;
+
+        this.userLottos = purchaseLottos(count);
     }
 
     private void validate(int purchasePrice){
-        if (purchasePrice % 1000 > 0) {
+        if (purchasePrice % UNIT_PRICE > 0) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
         }
+    }
+
+    private List<List<Integer>> purchaseLottos(int count){
+        List<List<Integer>> lottos = new ArrayList<>();
+        for(int i = 0; i < count; i++){
+            lottos.add(Randoms.pickUniqueNumbersInRange(1,45,6));
+        }
+
+        return lottos;
     }
 }
