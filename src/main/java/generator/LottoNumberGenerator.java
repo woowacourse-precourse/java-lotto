703f1a8993023e92bafecb0356db5ef1688afb66
JunHyeon <dlwnsgus07@naver.com>
2022-11-14 20:49:21 +0900

    fix: 클래스 이름을 대문자로 수정

--- src/main/java/generator/LottoNumberGenerator.java
+++ src/main/java/generator/LottoNumberGenerator.java
@@ -5,7 +5,7 @@ import lotto.Lotto;
 
 import java.util.List;
 
-public class lottoNumberGenerator {
+public class LottoNumberGenerator {
     public static Lotto makeLottoNumberGenerator(){
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return new Lotto(numbers);
