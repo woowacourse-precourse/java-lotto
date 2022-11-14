8bac3e5a26f894ba433afbf3af1f2b61cd4cc624
ingpyo <juninpyo72@gmail.com>
2022-11-14 11:42:48 +0900

    feat:구입개수,구입 로또 번호 출력

--- src/main/java/lotto/PrintMessage.java
+++ src/main/java/lotto/PrintMessage.java
@@ -1,9 +1,24 @@
 package lotto;
 
-public class PrintMessage {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class PrintMessage {
 	public void purchasePriceMessage() {
 		System.out.println(Constant.PURCHASE_PRICE_MESSAGE_QUESTION);
 	}
 
+	public void purchaseMessage(int count) {
+		System.out.println(count + Constant.BOUGHT_LOTTO_TICKETS);
+	}
+
+	public void printPurchaseLotto(List<List<Integer>> purchaseLotto) {
+		for (List<Integer> onePurchaseLotto : purchaseLotto) {
+			List<Integer> copyPurchaseLotto = new ArrayList<>();
+			copyPurchaseLotto.addAll(onePurchaseLotto);
+			Collections.sort(copyPurchaseLotto);
+			System.out.println(copyPurchaseLotto);
+		}
+	}
 }
