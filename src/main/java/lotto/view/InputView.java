b6dd070fd0f8d634868e8d5420da56ef06fe2374
lee <higgs95@naver.com>
2022-11-14 17:15:09 +0900

    feat(view): 수익률 출력 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -22,19 +22,19 @@ public class InputView {
         System.out.println("보너스 번호를 입력해 주세요.");
         String inputBonusNumber = readLine();
         isDigit(inputBonusNumber);
-
         return Integer.parseInt(inputBonusNumber);
     }
 
     private static void isDigit(String input) {
         if (!input.matches("-?\\d+")) {
-            throw new IllegalArgumentException("[ERROR] 정수를 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 문자가 입력되었습니다.");
         }
     }
 
     private static void canDivideByKilo(String money) {
         if (Integer.parseInt(money) % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 천 원단위로 입력해 주세요.");
+            String ERROR = "[ERROR] 천 원단위로 입력해 주세요.";
+            throw new IllegalArgumentException(ERROR);
         }
     }
 
