ccdbdc2bef9420e984966e6b65d85503d3e945a7
oreo <ohsimon77@naver.com>
2022-11-15 00:35:24 +0900

    Feat : WinCount 메소드 추가하여 indent 줄이기

---
+++ src/main/java/lotto/Count.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Count {
+    public static int[] BonusCount(List<List<Integer>> GeneratedLotto) {
+        int[] count = new int[GeneratedLotto.size()];
+        for (int i = 0; i < GeneratedLotto.size(); i++) {
+            if (GeneratedLotto.get(i).contains(Lotto.BonusNumber)) {
+                count[i] += 1;
+            }
+        }
+        return count;
+    }
+
+    public static List<Integer> WinningCount(List<List<Integer>> GeneratedLotto, List<Integer> numbers) {
+        List<Integer> counts = new ArrayList<>();
+        int count;
+        for (List<Integer> lotto : GeneratedLotto) {
+            count = 0;
+            count = WinCount(lotto, numbers);
+            counts.add(count);
+        }
+        return counts;
+    }
+
+    public static int WinCount(List<Integer> lotto, List<Integer> numbers) {
+        int count = 0;
+        for (int j = 0; j < numbers.size(); j++) {
+            if (lotto.contains(numbers.get(j))) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
