238332d71988afed47748bd7e526613394e4440e
seoyoung <sy9723@korea.ac.kr>
2022-11-14 10:11:16 +0900

    feat: 기능 추가 (수익률을 계산한다)

--- src/main/java/lotto/domain/Statistics.java
+++ src/main/java/lotto/domain/Statistics.java
@@ -2,17 +2,17 @@ package lotto.domain;
 
 public class Statistics {
 
-    public final String rateOfReturn;
+    public final float rateOfReturn;
     public final String resultMessage;
 
-    public Statistics(int firstCount, int secondCount, int thirdCount, int fourthCount, int fifthCount) {
-        this.rateOfReturn = statisticsReturn(firstCount, secondCount, thirdCount, fourthCount, fifthCount);
+    public Statistics(int purchaseAmount, int firstCount, int secondCount, int thirdCount, int fourthCount, int fifthCount) {
+        this.rateOfReturn = statisticsReturn(purchaseAmount, firstCount, secondCount, thirdCount, fourthCount, fifthCount);
         this.resultMessage = statisticsMessage(firstCount, secondCount, thirdCount, fourthCount, fifthCount);
     }
 
-    private String statisticsReturn(int firstCount, int secondCount, int thirdCount, int fourthCount, int fifthCount) {
-        String rate = "";
-        return rate;
+    private float statisticsReturn(int purchaseAmount, int firstCount, int secondCount, int thirdCount, int fourthCount, int fifthCount) {
+        int income = 5000*fifthCount + 50000*fourthCount + 1500000*thirdCount + 30000000*secondCount + 2000000000*firstCount;
+        return (float) (income / purchaseAmount * 100);
     }
 
     private String statisticsMessage(int firstCount, int secondCount, int thirdCount, int fourthCount, int fifthCount) {
@@ -23,7 +23,7 @@ public class Statistics {
                 + "5개 일치 (1,500,000원) - " + thirdCount +"개\n"
                 + "5개 일치, 보너스 볼 일치 (30,000,000원) - " + secondCount +"개\n"
                 + "6개 일치 (2,000,000,000원) - " + firstCount +"개\n"
-                + "총 수익률은 " + rateOfReturn + "%입니다.\n";
+                + "총 수익률은 " + String.format("%.2f", rateOfReturn) + "%입니다.\n";
         return message;
     }
 }
