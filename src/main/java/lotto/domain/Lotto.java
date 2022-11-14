d38d70efcb01a1e1b4111e23ae9fbab287869ef3
footdev <ekclstkfka5@gmail.com>
2022-11-15 02:55:50 +0900

    refactor: 요구사항 개선
    Lotto 클래스에 필드를 추가할 수 없다는 요구사항을 만족하기 위해 다른 필드를 제거함.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Lotto {
-    public static final String COUNT_ERROR_MESSAGE = "로또 번호의 개수는 6개여야 합니다.";
-    public static final String DUPLICATION_ERROR_MESSAGE = "로또 번호는 중복되지 않아야 합니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,14 +14,14 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException(Validation.ERROR_MESSAGE + " " + COUNT_ERROR_MESSAGE);
+            throw new IllegalArgumentException(Validation.ERROR_MESSAGE);
         }
     }
 
     private void validateDuplicatedNumber(List<Integer> numbers) {
         long count = countDuplicatedNumber(numbers);
         if(count != numbers.size()) {
-            throw new IllegalArgumentException(Validation.ERROR_MESSAGE + " " + DUPLICATION_ERROR_MESSAGE);
+            throw new IllegalArgumentException(Validation.ERROR_MESSAGE);
         }
     }
 
