8b6cef06b82896e4cd202af081300dbf3068d86d
marinesnow34 <m4rinesnow34@gmail.com>
2022-11-14 20:02:55 +0900

    refactor: 불필요한 validator 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,33 +5,37 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import lotto.util.Validator;
 
 public class InputView {
+    private static final String ERROR_NOT_INT = "[ERROR] 숫자만 입력 가능합니다.";
     private static final String MESSAGE_INPUT_MONEY = "구입금액을 입력해 주세요.";
     private static final String MESSAGE_INPUT_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String MESSAGE_INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-
-    private static final Validator validator = new Validator();
-
-    public static String inputMoney() {
+    
+    public static int inputMoney() {
         System.out.println(MESSAGE_INPUT_MONEY);
-        return readLine();
+        return convertInt(readLine());
     }
 
     public static List<Integer> inputNumbers() {
         System.out.println(MESSAGE_INPUT_NUMBERS);
         String input = readLine();
-        validator.validateInputNumbers(input);
         return Arrays.stream(input.split(","))
-                .map(Integer::valueOf)
+                .map(InputView::convertInt)
                 .collect(Collectors.toList());
     }
 
     public static int inputBonusNumber() {
         System.out.println(MESSAGE_INPUT_BONUS_NUMBER);
-        String input = readLine();
-        validator.validateInput(input);
-        return Integer.parseInt(input);
+        return convertInt(readLine());
+    }
+
+    private static int convertInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println(ERROR_NOT_INT);
+            throw new IllegalArgumentException(ERROR_NOT_INT);
+        }
     }
 }
