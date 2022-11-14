f6b3204e5597d3c5987beb4cc2e6b9683601f845
yujaemin <woawoa0513@gmail.com>
2022-11-15 00:54:28 +0900

    feat: 당첨금 계산 함수 구현

--- src/main/java/lotto/service/RewardService.java
+++ src/main/java/lotto/service/RewardService.java
@@ -1,8 +1,24 @@
 package lotto.service;
 
+import static lotto.utils.Constants.FIRST_PLACE;
+import static lotto.utils.Constants.LAST_PLACE;
+
+import lotto.domain.Ranking;
+
 public class RewardService {
 
-    public static double calculateRateOfReturn(int purchaseAmount, int reward) {
+    public static long calcuateReward(int[] rankingCount) {
+        long totalReward = 0;
+
+        for (int ranking = FIRST_PLACE; ranking <= LAST_PLACE; ranking++) {
+            int reward = Ranking.values()[ranking - 1].getReward();
+            totalReward += (long) reward * rankingCount[ranking];
+        }
+
+        return totalReward;
+    }
+
+    public static double calculateRateOfReturn(int purchaseAmount, long reward) {
         double rateOfReturn = ((double)reward / purchaseAmount) * 100 - 100;
         return Math.round((rateOfReturn * 10) / 10.0);
     }
