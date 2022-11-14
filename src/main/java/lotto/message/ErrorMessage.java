80247519276b8889bc00e05255d1ad4e71c112c6
splitube <voicesplit@daum.net>
2022-11-14 17:18:53 +0900

    refactor(ErrorMessage): 에러메세지 내용 수정

--- src/main/java/lotto/message/ErrorMessage.java
+++ src/main/java/lotto/message/ErrorMessage.java
@@ -9,7 +9,7 @@ public enum ErrorMessage {
     NUMBER_OUT_OF_RANGE("[ERROR] 번호는 1부터 45사이의 숫자여야 합니다."),
     BONUS_NUM_NOT_A_NUMBER("[ERROR] 보너스 번호는 숫자여야 합니다."),
     WINNING_NUM_CONTAINS_BONUS_NUM("[ERROR] 당첨 번호 중에 보너스 번호가 존재합니다"),
-    DUPLICATE_EXIST("[ERROR] 로또 번호에 중복된 숫자가 존재합니다.");
+    DUPLICATE_EXIST("[ERROR] 중복된 번호가 존재합니다.");
 
 
     public final String message;
