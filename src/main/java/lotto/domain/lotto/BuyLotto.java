ea5f404dac3209168fb70c2223bab23f89bbc4df
seonghoo1217 <seonghoo1217@naver.com>
2022-11-14 18:59:02 +0900

    fix(BuyLotto) : 테스트 케이스 부합을 위한 출력문 수정
    fix(commit_id) : #0fbee48

--- src/main/java/lotto/domain/lotto/BuyLotto.java
+++ src/main/java/lotto/domain/lotto/BuyLotto.java
@@ -9,9 +9,10 @@ import java.math.BigInteger;
 public class BuyLotto {
 
 
-	public static void prepare_money(String user_input_value) throws IllegalArgumentException {
+	public static void prepare_money(String user_input_value) {
 		if (!Validation.money_validation(user_input_value)){
-			throw new IllegalArgumentException("[ERROR] 1000원단위의 숫자를 입력해야합니다");
+			System.out.println("[ERROR] 1000원 단위의 숫자를 입력하셔야됩니다.");
+			return;
 		}
 		BigInteger money= new BigInteger(user_input_value);
 		buy(money);
@@ -21,5 +22,4 @@ public class BuyLotto {
 		int page_of_lotto = money.divide(BigInteger.valueOf(1000)).intValue();
 		PickNumber.pick_number_of_lotto(page_of_lotto);
 	}
-
 }
