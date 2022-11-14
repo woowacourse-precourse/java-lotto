17a164214f871e85978a97e50059fab4634f90cf
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-14 22:35:04 +0900

    feat: 가독성을 위해 상수처리

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -5,6 +5,10 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 public class User {
+
+    private static final String NUMBER_FORMS = "^[0-9]*$";
+    private static final char FIRST_COUNT = '0';
+    private static final int LOTTO_COST = 1000;
     private static int buyAmount;
 
     private List<Lotto> myLotto = new ArrayList<>();
@@ -29,21 +33,21 @@ public class User {
     }
 
     private void validateBuyAmountForms(String buyAmount) {
-        if (!buyAmount.matches("^[0-9]*$")) {
+        if (!buyAmount.matches(NUMBER_FORMS)) {
             System.out.println("[ERROR] 금액은 숫자로만 이루어져야 합니다.");
             throw new NoSuchElementException();
         }
     }
 
     private void validateBuyAmountFirstNumber(String buyAmount) {
-        if (buyAmount.charAt(0) == '0') {
+        if (buyAmount.charAt(0) == FIRST_COUNT) {
             System.out.println("[ERROR] 금액의 첫자리가 0이면 안됩니다.");
             throw new IllegalArgumentException();
         }
     }
 
     private void validateBuyAmount(String buyAmount) {
-        if (Integer.parseInt(buyAmount) % 1000 != 0) {
+        if (Integer.parseInt(buyAmount) % LOTTO_COST != 0) {
             System.out.println("[ERROR] 금액은 1000원 단위로 나누어져야 합니다.");
             throw new IllegalArgumentException();
         }
