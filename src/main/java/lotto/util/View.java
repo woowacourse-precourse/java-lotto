6ab8ee13a58f609bf3dd86dc804286429088fc6f
hoonn9 <hun9935@gmail.com>
2022-11-14 14:36:14 +0900

    feat(View): 뷰 클래스 추가

---
+++ src/main/java/lotto/util/View.java
@@ -0,0 +1,11 @@
+package lotto.util;
+
+public class View {
+    public static void print(String message) {
+        System.out.print(message);
+    }
+    
+    public static void println(String message) {
+        System.out.println(message);
+    }
+}
