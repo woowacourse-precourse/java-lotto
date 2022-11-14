198045a5b966a65b2d78802050a17dd2f7082911
Hayoon <hy97@sju.ac.kr>
2022-11-14 20:50:24 +0900

    test(LottoTest): 로또 번호 유효범위 검증 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,12 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.service.LottoException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,9 +32,10 @@ class LottoTest {
     @DisplayName("보너스 번호가 로또 번호와 중복이 있으면 예외가 발생한다.")
     @Test
     void createLottoAndBonusWithDuplicatedNumber() {
-        Lotto.bonusNumber = 6;
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6))
-                .exceptionBonusNumberByDuplicatedLotto(Lotto.bonusNumber))
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        new LottoException();
+        assertThatThrownBy(() -> LottoException
+                .exceptionBonusNumberByDuplicatedLotto(lotto.getNumbers(), 6))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -51,11 +53,21 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호는 1~45 사이의 값이어야 한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, 46})
+    public void lottoNumberRangeExceptionTest(int number) throws IllegalArgumentException {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, number)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("보너스 번호가 1 ~ 45사이의 값이 아닐 경우 예외가 발생한다.")
     @Test
     void createOutOfRangeOfBonusNumber() {
         final int bonusNumber = 0;
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6))
+//        new Lotto(List.of(1, 2, 3, 4, 5, 6));
+//        new LottoException();
+        assertThatThrownBy(() -> LottoException
                 .exceptionBonusNumberOutOfRange(bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -67,18 +79,3 @@ class LottoTest {
         assertThat(String.format("%.1f", yield)).isEqualTo("62.6");
     }
 }
-/**
- * - [o] 당첨 번호 및 보너스 번호 입력 기능
- * <p>
- * 총 수익률은 소수점 둘째 자리에서 반올림하여 소수점 첫째 자리까지 출력한다.
- * <p>
- * - [o] 당첨 통계 계산 기능
- * - 3개 일치
- * - 4개 일치
- * - 5개 일치
- * - 보너스 볼 일치 여부
- * - 6개 일치
- * <p>
- * - [o] 총 수익률 계산 기능
- * - Enum 클래스로 구현
- */
