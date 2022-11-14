5d5efce82f2c370fea824150add8e2d474bf9eff
whrod <whrodtube@gmail.com>
2022-11-14 18:29:38 +0900

    Feat : Class Import

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,14 @@
 package lotto;
 
-import domain.Lotto.numbers;
+import static java.util.Arrays.asList;
+
+import java.util.List;
+import lotto.domain.Lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Test test =
-
+        List<Integer> nums = asList(1,2,3,4,5,6);
+        Lotto lottoTest = new Lotto(nums);
+        System.out.println(lottoTest.getNumbers());
     }
 }
