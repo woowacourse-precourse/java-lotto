5106c1ce8ab1e4daaeab63c10f408a90b54e3c9a
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-15 04:21:41 +0900

    feat(test\...\controller\CasherTest) : implement test for returnCorrectNumber

--- src/test/java/lotto/controller/CasherTest.java
+++ src/test/java/lotto/controller/CasherTest.java
@@ -1,8 +1,15 @@
 package lotto.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,5 +33,17 @@ public class CasherTest {
     void wrongInputToIssueTickets(String wrongInput) {
         assertThatThrownBy(() -> Casher.issueTickets(Integer.parseInt(wrongInput)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Tag("WinResultManager")
+    @DisplayName("일치하는 번호의 개수를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1,3,5,14,22,45:3", "3,5,11,16,32,38:2"})
+    void returnCorrectNumber(String numbers, int expected) {
+        List<Integer> winNumbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+        List<Integer> lottoNumber = new ArrayList<>(Arrays.stream(numbers.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList()));
+        assertThat(WinResultManager.returnCorrectNumber).isEqualTo(expected);
     }
 }
