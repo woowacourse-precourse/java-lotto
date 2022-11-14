0df87b4e6848ef7a3fbfb24f44911467e1c847d6
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-14 16:01:34 +0900

    test(LottoTest): 오름차순 정렬 테스트 케이스 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,11 +1,12 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
 	@DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,4 +25,10 @@ class LottoTest {
 	}
 
 	// 아래에 추가 테스트 작성 가능
+	@DisplayName("로또 번호가 오름차순으로 정렬된다.")
+	@Test
+	void createLottoBySort() {
+		Lotto lotto = new Lotto(List.of(6, 5, 4, 3, 2, 1));
+		assertEquals(lotto.getNumbers(), List.of(1, 2, 3, 4, 5, 6));
+	}
 }
