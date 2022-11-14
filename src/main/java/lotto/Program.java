dc9e625e78092652f297d8945f2a50b14c1e6bda
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-15 03:37:39 +0900

    feat(Program): 유저의 당첨 번호를 입력받아 랭킹 계산 전까지 셋업하는 메소드winSetup 구현

--- src/main/java/lotto/Program.java
+++ src/main/java/lotto/Program.java
@@ -1,14 +1,26 @@
 package lotto;
 
+import java.util.List;
+
 public class Program {
     View view = new View();
+    int numOfLottos, bonus;
     ListOfLottos lottoList;
-    int numOfLottos;
-    public void setup() {
+    List<Integer> winner;
+
+
+
+    public void lottoSetup() {
         view.printMoneyInputMsg();
         numOfLottos = view.getMoneyInput();
         view.printNumberOfLottosMsg(numOfLottos);
         lottoList = new ListOfLottos(numOfLottos);
         lottoList.printAllLottos();
+    }
+    public void winSetup() {
+        view.printWinnerInputMsg();
+        winner = view.getWinnerInput();
+        view.printBonusInputMsg();
+        bonus = view.getBonusInput(winner);
     }
 }
