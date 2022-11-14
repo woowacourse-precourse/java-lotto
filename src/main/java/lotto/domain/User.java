26c5e5501bc7adfcaad634624d948d7be6db07c9
lynne921 <dkfls921@gmail.com>
2022-11-15 05:06:32 +0900

    feat(User): 유저의 돈 입력 시 돈이 정수인지 판별하는 기능 추가
    - 유저가 입력한 돈이 정수인지 판별하는 기능 추가
    - 추가된 기능을 검증하는 테스트 코드 추가

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -4,14 +4,21 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class User {
     public int getMoneyInput() {
-        int money = Integer.parseInt(readLine());
-        validateMoney(money);
-        return money;
+        String money = readLine();
+        validateDataType(money);
+        validateMoney(Integer.parseInt(money));
+        return Integer.parseInt(money);
     }
 
     private void validateMoney(int money) {
         if (money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 입력된 돈이 1000원 단위가 아닙니다.");
+        }
+    }
+
+    private void validateDataType(String money) {
+        if (!money.matches("-?\\d+")) {
+            throw new IllegalArgumentException("[ERROR] 입력된 돈이 숫자가 아닙니다.");
         }
     }
 }
--- src/test/java/lotto/domain/UserTest.java
+++ src/test/java/lotto/domain/UserTest.java
@@ -15,30 +15,40 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class UserTest {
     private User user;
 
-    void setUp(int input) {
+    void setUp(String input) {
         user = new User();
         OutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
-        InputStream in = new ByteArrayInputStream(Integer.toString(input).getBytes());
+        InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
     }
 
     @DisplayName("유저의 돈이 정상적으로 입력된다.")
     @Test
     void createMoney() {
-        int input = 18000;
+        String input = "18000";
         setUp(input);
 
-        assertThat(user.getMoneyInput()).isEqualTo(input);
+        assertThat(user.getMoneyInput()).isEqualTo(Integer.parseInt(input));
     }
 
     @DisplayName("유저의 돈 입력이 1000원 단위가 아니라면 예외가 발생한다.")
     @Test
     void createMoneyByWrongUnit() {
-        int input = 1800;
+        String input = "1800";
         setUp(input);
 
         Throwable exception = assertThrows(IllegalArgumentException.class, user::getMoneyInput);
         assertEquals("[ERROR] 입력된 돈이 1000원 단위가 아닙니다.", exception.getMessage());
+    }
+
+    @DisplayName("유저의 돈 입력이 숫자가 아니라면 예외가 발생한다.")
+    @Test
+    void createMoneyByWrongType() {
+        String input = "abcd";
+        setUp(input);
+
+        Throwable exception = assertThrows(IllegalArgumentException.class, user::getMoneyInput);
+        assertEquals("[ERROR] 입력된 돈이 숫자가 아닙니다.", exception.getMessage());
     }
 }
