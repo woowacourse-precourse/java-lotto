c4cedc3961b9482c6efde43bc2f6d6e1ccfb5211
starwook <star.wookk@gmail.com>
2022-11-15 04:16:47 +0900

    feat(Reward.jav): Add Reward.jav
    Reward.java를 추가하라

---
+++ src/main/java/lotto/domain/Reward.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+public enum Reward {
+    THREE(3,"5,000원",false),
+    FOUR(4,"50,000원",false),
+    FIVE(5,"1,500,000원",false),
+    FIVE_BONUS(5,"30,000,000원",true),
+    SIX(6,"2,000,000,000원",true);
+
+    private int bonusNumber;
+    private boolean bonusCheck;
+    private int number;
+    private String bill;
+
+    Reward(int number, String bill,Boolean bonusCheck){
+        this.number = number;
+        this.bill = bill;
+        this.bonusCheck =bonusCheck;
+    }
+
+
+
+
+}
