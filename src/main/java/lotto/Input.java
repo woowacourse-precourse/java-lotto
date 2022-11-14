2ba3932d93bf8d8fa414f927b693957ab3391941
jaewoo <wjdwodn0722@naver.com>
2022-11-14 21:32:42 +0900

    test : 로또 당첨번호를 입력받는 기능 추가

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,16 +1,22 @@
 package lotto;
 
+import java.util.List;
+
 public class Input {
 
-    public static Integer receiveMoney(){
+    public static Integer receiveMoney() {
         String money = camp.nextstep.edu.missionutils.Console.readLine();
         return Integer.parseInt(money);
     }
 
-//    public static void receiveWinningNumbers(){
-//
-//    }
-//
+    public static void receiveWinningNumbers(List<Integer> winningNumbers) {
+        String number;
+        for (int i = 0; i < 6; i++) {
+            number = camp.nextstep.edu.missionutils.Console.readLine();
+            winningNumbers.add(Integer.parseInt(number));
+        }
+    }
+
 //    public static void receiveBonusNuber(){
 //
 //    }
