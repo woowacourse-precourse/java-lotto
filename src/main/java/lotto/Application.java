ee0cad8f59304056880edbe0172c6d33e3da0e7a
jagajang <kosb7544@gmail.com>
2022-11-14 17:57:40 +0900

    fix: change instance name

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,7 +3,7 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        LottoProgramManager lottoProgramManagero = new LottoProgramManager();
-        lottoProgramManagero.runProgram();
+        LottoProgramManager lottoProgramManager = new LottoProgramManager();
+        lottoProgramManager.runProgram();
     }
 }
