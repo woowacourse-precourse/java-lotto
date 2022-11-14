53716f4820771fe45f6106184179eb230797a35f
Gibeom Nam <rlqja0523@naver.com>
2022-11-15 04:13:39 +0900

    test: 로또 번호 개수 예외 테스트

--- src/test/java/lotto/entity/LottoTest.java
+++ src/test/java/lotto/entity/LottoTest.java
@@ -29,4 +29,11 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("로또 번호의 개수가 6개보다 적으면 예외가 발생한다.")
+    void createLottoByUnderSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
