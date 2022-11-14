6c066b5547605f5c3d2c5883559923ef8e866c82
ssssudan <kankgm0324@gmail.com>
2022-11-14 14:56:27 +0900

    <feat>: 로또 번호가 예외 사항에 대한 처리를 구현

--- src/main/java/lotto/WinningNumberToList.java
+++ src/main/java/lotto/WinningNumberToList.java
@@ -14,11 +14,8 @@ public class WinningNumberToList {
 
     public void setWinNumbers(String winNums) {
         StringTokenizer st = new StringTokenizer(winNums, ",");
-        for(int i=0; i<6; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            if(num<1 || num >45)
-                throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
-            winNumbers.add(num);
+        while (st.hasMoreTokens()) {
+            winNumbers.add(Integer.parseInt(st.nextToken()));
         }
         Collections.sort(winNumbers);
     }
