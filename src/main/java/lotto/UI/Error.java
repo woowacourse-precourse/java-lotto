ca9fc9d1520a4839c1aaae1693f0868dd943fdf6
canbeyour <wuzucy@gmail.com>
2022-11-15 03:26:46 +0900

    feat: add ability to throw error

---
+++ src/main/java/lotto/UI/Error.java
@@ -0,0 +1,15 @@
+package lotto.UI;
+
+public class Error {
+    public void amountInput() {
+        throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해 주세요.");
+    }
+
+    public void duplicated() {
+        throw new IllegalArgumentException("[ERROR] 중복되지 않은 숫자를 입력해 주세요");
+    }
+
+    public void outOfRange() {
+        throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+}
