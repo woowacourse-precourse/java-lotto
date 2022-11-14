a7fe7aaf947ed5e069e4b50aad48f02ac42d93b3
Your Name <you@example.com>
2022-11-14 16:44:35 +0900

    refactor: 함수 단위 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,25 +9,38 @@ public class Application {
         LottoCore lottoCore = new LottoCore();
         LottoInputOutput lottoIO = new LottoInputOutput();
 
+        moneyInput(buyer, lottoIO);
+
+        buyLottos(buyer, lottoCore, lottoIO);
+
+        setWinningLottoNums(lottoCore, lottoIO);
+
+        compareLottos(buyer, lottoCore, lottoIO);
+    }
+
+    private static void compareLottos(Buyer buyer, LottoCore lottoCore, LottoInputOutput lottoIO) {
+        Map<LottoPrize, Integer> prizeList = lottoCore.compareLotto(buyer.getLottos());
+        lottoIO.printWinningList(prizeList);
+        buyer.setWinmoney(prizeList);
+        lottoIO.printWinningRate(buyer.getWinMoney(), buyer.getMoney());
+    }
+
+    private static void setWinningLottoNums(LottoCore lottoCore, LottoInputOutput lottoIO) {
+        List<Integer> winnigLotto = lottoIO.getWinnigLottoAndCheck();
+        int bonus = lottoIO.getBonusNumberAndCheck(winnigLotto);
+        lottoCore.setWinnigLotto(winnigLotto);
+        lottoCore.setBonus(bonus);
+    }
+
+    private static void moneyInput(Buyer buyer, LottoInputOutput lottoIO) {
         buyer.setMoney(lottoIO.getMoneyInput());
         buyer.setLottoCount();
+    }
 
+    private static void buyLottos(Buyer buyer, LottoCore lottoCore, LottoInputOutput lottoIO) {
         List<Lotto> buyerLottos = lottoCore.createManyLotto(buyer.getLottoCount());
         lottoIO.printLottoCount(buyer.getLottoCount());
         lottoIO.printLotto(buyerLottos);
         buyer.setLottos(buyerLottos);
-
-        List<Integer> winnigLotto = lottoIO.getWinnigLottoAndCheck();
-        int bonus = lottoIO.getBonusNumberAndCheck(winnigLotto);
-
-        lottoCore.setWinnigLotto(winnigLotto);
-        lottoCore.setBonus(bonus);
-
-        Map<LottoPrize, Integer> prizeList = lottoCore.compareLotto(buyer.getLottos());
-        lottoIO.printWinningList(prizeList);
-
-        buyer.setWinmoney(prizeList);
-        lottoIO.printWinningRate(buyer.getWinMoney(), buyer.getMoney());
-
     }
 }
