18e73b8f10ce3a8ec5e14c6a830ca498e59aec5a
TaeHwan <kimta2hwan@gmail.com>
2022-11-14 17:43:50 +0900

    refactor: 입력값 VO객체로 포장

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,18 +1,52 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.vo.LottoNumber;
+import lotto.vo.PurchaseMoney;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class InputView {
 
-    public static String inputPurchaseAmount() {
-        return Console.readLine();
+    private static final String WINNING_NUMBERS_SPLIT_DELIMITER = ",";
+
+    public static PurchaseMoney inputPurchaseMoney() {
+        String input = Console.readLine();
+        return convertInputPurchaseMoney(input);
     }
 
-    public static String inputWinningNumbers() {
-        return Console.readLine();
+    private static PurchaseMoney convertInputPurchaseMoney(String input) {
+        int money = Integer.parseInt(input);
+        return new PurchaseMoney(money);
     }
 
-    public static String inputBonusNumber() {
-        return Console.readLine();
+    public static Set<LottoNumber> inputWinningNumbers() {
+        String input = Console.readLine();
+        return convertInputWinningNumbers(input);
+    }
+
+    private static Set<LottoNumber> convertInputWinningNumbers(String input) {
+        Set<LottoNumber> lottoNumbers = new HashSet<>();
+        String[] numbers = input.split(WINNING_NUMBERS_SPLIT_DELIMITER);
+
+        Arrays.stream(numbers)
+                    .forEach(s -> {
+                        int number = Integer.parseInt(s);
+                        lottoNumbers.add(new LottoNumber(number));
+                    });
+
+        return lottoNumbers;
+    }
+
+    public static LottoNumber inputBonusNumber() {
+        String input = Console.readLine();
+        return convertInputBonusNumber(input);
+    }
+
+    public static LottoNumber convertInputBonusNumber(String input) {
+        int number = Integer.parseInt(input);
+        return new LottoNumber(number);
     }
 }
