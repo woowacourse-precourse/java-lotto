21797840b60605de1d3b1a640c37da80c474ea9c
Cyma-s <ssol0319@naver.com>
2022-11-15 00:29:18 +0900

    feat(LottoOutputView): add a function that prints user lotto numbers

--- src/main/java/lotto/view/LottoOutputView.java
+++ src/main/java/lotto/view/LottoOutputView.java
@@ -6,8 +6,9 @@ import lotto.model.Lotto;
 public class LottoOutputView {
     private final static String LOTTO_COUNT_STRING = "개를 구매했습니다.";
 
-    public void printLottoCount(int lottoCount) {
+    public void printLottoCount(int lottoCount, List<Lotto> lottos) {
         System.out.println(lottoCount + LOTTO_COUNT_STRING);
+        printLottoNumbers(lottoCount, lottos);
     }
 
     public void printLottoNumbers(int lottoCount, List<Lotto> lottos) {
