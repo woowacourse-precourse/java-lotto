e933c03d680179f6dfe820b899ab1ee474002470
김선동 <mok03127@gmail.com>
2022-11-15 02:34:12 +0900

    test: 로또 구입 금액 테스트

---
+++ src/test/java/domain/LottoPurchaseAmountTest.java
@@ -0,0 +1,40 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class LottoPurchaseAmountTest {
+    
+    @Test
+    @DisplayName("로또 구입금액이 0원이면 예외가 발생한다.")
+    void inputMoneyEqualsWithZero() {
+        assertThatThrownBy(() -> new LottoPurchaseAmount(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 구입 금액이 1000으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {500, 999, 1001})
+    void inputMoneyNotDividedByThousand(int amount) {
+        assertThatThrownBy(() -> new LottoPurchaseAmount(amount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 구입 금액이 1000 단위로 입력되었을 경우 로또 개수 확인")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1000, 1",
+            "2000, 2",
+            "8000, 8"
+    })
+    void calculateNumberOfLottoTickets(int amount, int numberOfTickets) {
+        LottoPurchaseAmount lottoPurchaseAmount = new LottoPurchaseAmount(amount);
+        assertThat(lottoPurchaseAmount.calculateNumberOfLottoTickets()).isEqualTo(numberOfTickets);
+    }
+}
