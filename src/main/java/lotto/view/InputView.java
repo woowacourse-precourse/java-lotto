0e8ba27546ee9f7f487ee744b915e6c898a68415
Amyhds <dasomhan524@gmail.com>
2022-11-14 14:13:56 +0900

    refactor(inputview): 사용자 입력받는 클래스 분리

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.model.enums.GameMessage;
+
+public class InputView {
+    public static String buyLotto() {
+        System.out.println(GameMessage.PRICE_MESSAGE.getMessage());
+        return Console.readLine();
+    }
+
+}
