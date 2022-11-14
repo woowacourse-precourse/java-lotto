555fcf02b01138f0af81c88aa2504fd4d63febd3
bfly_notebook <kkyxw@soongsil.ac.kr>
2022-11-14 18:21:01 +0900

    feat: add the error constant about winning number count that is used in enum class 'Prize'

--- src/main/java/lotto/validator/ErrorConstants.java
+++ src/main/java/lotto/validator/ErrorConstants.java
@@ -9,4 +9,6 @@ public class ErrorConstants {
 	public static final String ERROR_OUT_OF_LOTTO_RANGE = ERROR + "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 	public static final String ERROR_DUPLICATION_LOTTO_NUMBER = ERROR + "로또 번호는 중복되지 않는 수로 이루어져야 합니다.";
 	public static final String ERROR_DUPLICATION_BONUS_NUMBER = ERROR + "보너스 번호는 당첨 번호와 중복되지 않는 수여야 합니다.";
+
+	public static final String ERROR_WINNING_NUMBER_COUNT = "[ERROR] 당첨 번호와 일치하는 개수에 오류가 있습니다.";
 }
