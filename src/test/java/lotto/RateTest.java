0a25006d344775fcd4c0829f38710b42452d4aba
minaver <psmjoshua@gmail.com>
2022-11-14 16:55:57 +0900

    test(RateTest): Rate 생성 후 earningRate가 소수점 둘째 자리에서 반올림하는지 확인

--- src/test/java/lotto/RateTest.java
+++ src/test/java/lotto/RateTest.java
@@ -5,6 +5,7 @@ import lotto.model.Rate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,5 +32,27 @@ public class RateTest {
 
         // then
         assertThat(earningRate == answerRate).isTrue();
+    }
+
+    @DisplayName("Rate 생성 후 earningRate가 소수점 둘째 자리에서 반올림하는지 확인한다.")
+    @Test
+    void getEarningRateRound() {
+        // given
+        int price = 6000;
+        Map<Rank,Integer> winningStats = new HashMap<>();
+        winningStats.put(Rank.first,0);
+        winningStats.put(Rank.second,0);
+        winningStats.put(Rank.third,0);
+        winningStats.put(Rank.fourth,0);
+        winningStats.put(Rank.fifth,1);
+
+        // when
+        Rate rate = new Rate(winningStats,price);
+        double earningRate = rate.getEarningRate();
+
+        // then
+        String earningRateWord = String.valueOf(earningRate);
+        int underZeroLength = earningRateWord.substring(earningRateWord.indexOf(".")).length();
+        assertThat(underZeroLength == 2).isTrue();
     }
 }
