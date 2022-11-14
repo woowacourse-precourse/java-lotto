cfb52849d53c64ad807148275c50a5a9574bfb3c
Kimhyeyun <cqqwer@gmail.com>
2022-11-14 17:58:44 +0900

    Fix: 테스트를 위해 검증 메서드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,12 +9,32 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        validate(numbers);
         this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateDuplicate(numbers);
+        validateNumbersSize(numbers);
     }
 
 
     // TODO: 추가 기능 구현
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        int count = (int) numbers.stream().distinct().count();
+        if (count < 6) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + DUPLICATE_LOTTO_NUMBER_MESSAGE);
+        }
+    }
+
+
+    private void validateNumbersSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_LOTTO_NUMBER_SIZE_MESSAGE);
+        }
     }
 }
