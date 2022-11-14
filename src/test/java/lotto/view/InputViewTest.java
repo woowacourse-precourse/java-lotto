d0fd0b2a4000c65aa00b441a7e84bcdd983811dd
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-15 02:22:57 +0900

    test : 입력값이 적절하지 않을 때, 적절한 에러 메시지를 출력하는지 확인한다.

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -10,8 +10,7 @@ import java.util.Scanner;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputViewTest {
-    private static final String LOTTO_MONEY_IS_UNDER_MINIMUM_MONEY_VALUE = "[ERROR] 로또 구입 금액이 1000원 이하입니다.";
-    private static final String LOTTO_MONEY_IS_NOT_DIVIDED_BY_MONEY_VALUE = "[ERROR] 로또 구입 금액이 1000원으로 나누어 떨어지지 않습니다.";
+    private static final String INPUT_TYPE_IS_NOT_PROPER = "[ERROR] 잘못된 입력 형식입니다.";
     Scanner scanner;
 
     public static InputStream generateUserInput(String input) {
@@ -19,28 +18,15 @@ class InputViewTest {
     }
 
     @Test
-    @DisplayName("최소 구입 금액 이하의 값을 입력했을 때 예외를 잘 발생시키는지 확인한다.")
+    @DisplayName("입력 형식이 적절하지 않으면 예러를 발생한다.")
     void inputLottoMoneyUnderMinimumMoney() {
-        String inputMoney = "900";
+        String inputMoney = "만사천원";
         InputStream in = generateUserInput(inputMoney);
         System.setIn(in);
         scanner = new Scanner(System.in);
 
         assertThatThrownBy(InputView::inputMoney)
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(LOTTO_MONEY_IS_UNDER_MINIMUM_MONEY_VALUE);
-    }
-
-    @Test
-    @DisplayName("로또 구입 금액이 1000원으로 나누어지지 않으면 예외를 잘 발생시키는지 확인한다.")
-    void inputLottoMoneyNotDividedBy1000() {
-        String inputMoney = "1100";
-        InputStream in = generateUserInput(inputMoney);
-        System.setIn(in);
-        scanner = new Scanner(System.in);
-
-        assertThatThrownBy(InputView::inputMoney)
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(LOTTO_MONEY_IS_NOT_DIVIDED_BY_MONEY_VALUE);
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage(INPUT_TYPE_IS_NOT_PROPER);
     }
 }
