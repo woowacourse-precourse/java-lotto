dce2a44bddab054efd1757aafde5d7853ba61033
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-15 03:27:20 +0900

    refactor(ListOfLottos): lotto generator를 constructor 형태로 변경

--- src/main/java/lotto/ListOfLottos.java
+++ src/main/java/lotto/ListOfLottos.java
@@ -8,17 +8,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class ListOfLottos {
     private List<Lotto> listOfLottos;
 
-    public List<Lotto> generateListOfLottos(int size) {
-        List<Lotto> listOfLottos = new ArrayList<>();
+    public ListOfLottos(int size) {
+        this.listOfLottos = new ArrayList<>();
         Lotto lotto;
         for (int idx = 0; idx < size; idx++) {
             lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             listOfLottos.add(lotto);
         }
-        return listOfLottos;
     }
     public void printAllLottos() {
-        for (Lotto lotto : listOfLottos) {
+        for (Lotto lotto : this.listOfLottos) {
             lotto.printLotto();
         }
     }
