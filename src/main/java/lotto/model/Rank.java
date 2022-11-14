4519bba73efe2e55f618b40565a0fc8896d8a83f
wxxhyeong <wxxhyeong@gmail.com>
2022-11-14 23:58:04 +0900

    feat: enum Rank 추가

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,5 +1,25 @@
 package lotto.model;
 
 public enum Rank {
-    first, second, third, fourth, fifth
+    first("6개 일치 (2,000,000,000원) - ",2000000000),
+    second("5개 일치, 보너스 볼 일치 (30,000,000원)",30000000),
+    third("5개 일치 (1,500,000원)", 1500000),
+    fourth("4개 일치 (50,000원)", 50000),
+    fifth("3개 일치 (5000원)", 5000);
+
+    final private String matchMessage;
+    final private Integer matchMoney;
+
+    public Integer getMatchMoney() {
+        return matchMoney;
+    }
+
+    public String getMatchMessage() {
+        return matchMessage;
+    }
+
+    private Rank(String matchMessage, Integer matchMoney) {
+        this.matchMessage = matchMessage;
+        this.matchMoney = matchMoney;
+    }
 }
