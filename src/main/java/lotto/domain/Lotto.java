b04e07148512dccd80213cdb62da0a36ae23aa0b
ryanha <ryanha@handong.ac.kr>
2022-11-14 22:36:14 +0900

    feat(domain): validate에 중복된 수가 없는 지를 검증할 수 있는 로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -9,11 +11,15 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
-        Collections.sort(numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+        Set<Integer> numberPool = new HashSet<>();
+        numberPool.addAll(numbers);
+        if(numberPool.size() != numbers.size()) {
             throw new IllegalArgumentException();
         }
     }
