0b128e512eb48ac9387e4c786d7ff17379c166ea
impati <impati0716@icloud.com>
2022-11-14 22:30:59 +0900

    feat(Application) : 당첨 번호 입력 적용

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,18 +6,24 @@ public class Application {
     public static void main(String[] args) {
 
         programStart();
-        Integer cost = Convert.costConvert(Console.readLine());
-
+        Integer cost = inputCost();
         Purchase purchase = new Purchase();
         purchase.lotto(cost);
 
         Render.purchaseLottoResultPrint(purchase.getMyLotto());
 
-
-
+        Lotto winningNumber = inputWinningNumber();
 
     }
     public static void programStart(){
         System.out.print("구입금액을 입력해주세요.");
     }
+    public static Integer inputCost() {
+        return Convert.costConvert(Console.readLine());
+    }
+    public static Lotto inputWinningNumber(){
+        return Convert.winningConvert(Console.readLine());
+    }
+
+
 }
