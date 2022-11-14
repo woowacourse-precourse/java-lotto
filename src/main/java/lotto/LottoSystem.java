ff947e05a7bee724386ba96ada235e79b2bfec11
osm <osm0822@naver.com>
2022-11-14 16:27:45 +0900

    feat(WinInfo) : 순위정보를 담도록 기능 추가

--- src/main/java/lotto/LottoSystem.java
+++ src/main/java/lotto/LottoSystem.java
@@ -5,11 +5,11 @@ import java.util.List;
 public class LottoSystem {
     public static final int price = 1000;
     public static final List<WinInfo> winInfomations = List.of(
-            new WinInfo(6,0,2000000000),
-            new WinInfo(5,1,30000000),
-            new WinInfo(5,0,1500000),
-            new WinInfo(4,0,50000),
-            new WinInfo(3,0,5000)
+            new WinInfo(6,0,2000000000,1),
+            new WinInfo(5,1,30000000,2),
+            new WinInfo(5,0,1500000,3),
+            new WinInfo(4,0,50000,4),
+            new WinInfo(3,0,5000,5)
     );
 
     private List<Integer> winNumbers;
@@ -25,4 +25,6 @@ public class LottoSystem {
     public void setBonusNumbers(int bonusNumbers) {
         this.bonusNumbers = bonusNumbers;
     }
+
+
 }
--- src/main/java/lotto/WinInfo.java
+++ src/main/java/lotto/WinInfo.java
@@ -1,14 +1,16 @@
 package lotto;
 
 public class WinInfo {
+
     private int matchCnt;
     private int bonusMatchCnt;
     private long winMoney;
-
-    public WinInfo(int matchCnt, int bonusMatchCnt, long winMoney) {
+    private int order;
+    public WinInfo(int matchCnt, int bonusMatchCnt, long winMoney, int order) {
         this.matchCnt = matchCnt;
         this.bonusMatchCnt = bonusMatchCnt;
         this.winMoney = winMoney;
+        this.order = order;
     }
 
     public boolean match(int matchCnt, int bonusMatchCnt) {
