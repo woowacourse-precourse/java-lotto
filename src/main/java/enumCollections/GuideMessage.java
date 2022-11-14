86a31d5be905718012c0560e917d7aaf9099a1e2
swonny <wonny921@gmail.com>
2022-11-14 14:37:54 +0900

    feat(GuideMessage): 보너스 번호 입력 메시지 추가

--- src/main/java/enumCollections/GuideMessage.java
+++ src/main/java/enumCollections/GuideMessage.java
@@ -1,17 +1,18 @@
 package enumCollections;
 
-public enum Messages {
+public enum GuideMessage {
     PURCHASE_INPUT("구매금액을 입력해 주세요."),
     NUMBER_OF_PURCHASE("개를 구매했습니다."),
-    WINNING_NUMBERS_INPUT("당첨 번호를 입력해 주세요.");
+    WINNING_NUMBERS_INPUT("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_INPUT("보너스 번호를 입력해 주세요.");
 
     private final String message;
 
-    Messages(String message) {
+    GuideMessage(String message) {
         this.message = message;
     }
 
     public static String getMessage(String situation) {
-        return Messages.valueOf(situation).message;
+        return GuideMessage.valueOf(situation).message;
     }
 }
