88631327b61fc81cd0b73ab1851f5743b95c78eb
Heesom <heesom.hs@gmail.com>
2022-11-14 20:15:11 +0900

    fix(StatisticOutput) : 같은 문구가 중복 출력 되어 수정

--- src/main/java/lotto/Output/Statistic/StatisticOutput.java
+++ src/main/java/lotto/Output/Statistic/StatisticOutput.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class StatisticOutput {
     public void getStatisticOutput(List<Integer> prizeRanks) {
+        int prizeRankIndex = 0;
         for(PrintWinning winMessage: PrintWinning.values()){
-            for(Integer prizeRank : prizeRanks){
-                System.out.printf(winMessage.message(),Integer.valueOf(prizeRank));
-            }
+            System.out.printf(winMessage.message(), prizeRanks.get(prizeRankIndex));
+            prizeRankIndex++;
         }
     }
 }
