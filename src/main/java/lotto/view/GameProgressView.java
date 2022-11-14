75bef94c2eea687e9742b384d1befe40ade671ff
jjmin9797 <jjmin9797@naver.com>
2022-11-14 15:34:47 +0900

    feat(GameProgressView.java): 보너스 번호 입력시 메시지 출력 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/view/GameProgressView.java
+++ src/main/java/lotto/view/GameProgressView.java
@@ -8,6 +8,7 @@ public class GameProgressView {
     public static final String PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String COUNT_MESSAGE = "개를 구매했습니다.";
     public static final String ENTER_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String ENTER_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static void showPurchaseMessage() {
         System.out.println(PURCHASE_MESSAGE);
@@ -25,6 +26,10 @@ public class GameProgressView {
 
     public static void showEnterWinningNumbersMessage() {
         System.out.println(ENTER_WINNING_NUMBERS_MESSAGE);
+    }
+
+    public static void showEnterBonusNumberMessage() {
+        System.out.println(ENTER_BONUS_NUMBER_MESSAGE);
     }
 
 }
