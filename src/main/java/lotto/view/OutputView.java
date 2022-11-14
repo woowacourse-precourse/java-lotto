f4a58a5e57dde1332c2dcda5f6d2d8081fd09187
jiwon1217 <koagjw@gmail.com>
2022-11-15 03:46:45 +0900

    refactor: 변수 분리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -13,18 +13,19 @@ public class OutputView {
     private static final String NEW_LINE = "\n";
     private static final String COMMA = ", ";
     private static final String NUMBER_OF_PURCHASED_LOTTOS = "%d개를 구매했습니다.";
-    private static final String STATISTIC_HEADER = "당첨 통계" + NEW_LINE + "---";
+    private static final String STATISTIC_HEADER = "당첨 통계";
+    private static final String STATISTIC_BORDER = "---";
     private static final String STATISTIC_BODY = "%d개 일치%s (%s원) - %d개";
     private static final String MATCH_BONUS_BALL = "보너스 볼 일치";
     private static final String TOTAL_YIELD = "총 수익률은 %.1f%%입니다.";
     private static final int ONE_HUNDRED = 100;
 
     private static final Map<Integer, String> winnings = new LinkedHashMap<>() {{
-        put(2000000000, "2,000,000,000");
-        put(30000000, "30,000,000");
-        put(1500000, "1,500,000");
-        put(50000, "50,000");
-        put(5000, "5,000");
+        put(2_000_000_000, "2,000,000,000");
+        put(30_000_000, "30,000,000");
+        put(1_500_000, "1,500,000");
+        put(50_000, "50,000");
+        put(5_000, "5,000");
     }};
 
     public static void printLottoCount(LottoCount lottoCount) {
@@ -43,6 +44,8 @@ public class OutputView {
     public static void printResult(Result result) {
         System.out.printf(NEW_LINE);
         System.out.println(STATISTIC_HEADER);
+        System.out.println(STATISTIC_BORDER);
+
         for (Rank rank : Rank.get()) {
             System.out.printf(STATISTIC_BODY, rank.getMatchCount(), getMatchBonusBall(rank),
                     winnings.get(rank.getWinnings()),
