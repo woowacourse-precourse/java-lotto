dfbefbefe685eeac1ba3501228cbb630eda5b573
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-15 00:00:01 +0900

    test: check the order of lotto number list

---
+++ src/test/java/lotto/LottoTicketTest.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoTicketTest {
+    @DisplayName("Throw Exception when there is change after buy Lottery.")
+    @Test
+    void changesLeftAfterBuyLotteryTest() {
+        assertThatThrownBy(() -> LottoTicket.countPaidLotteryTickets(3050)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Check that numbers are sorted in acending order.")
+    @Test
+    void lottoNumberOrderTest() {
+        List<List<Integer>> numberList = new ArrayList<>();
+        numberList = LottoTicket.paidLotteryNumberGenerator(3);
+        int ticketNo = 2;
+        int result=0;
+        for (int index = 0; index < 5; index++) {
+            if (numberList.get(ticketNo).get(index) > numberList.get(ticketNo).get(index + 1)) {
+                result = 1;
+            }
+        }
+        assertThat(result).isEqualTo(0);
+    }
+
+
+
+
+}
