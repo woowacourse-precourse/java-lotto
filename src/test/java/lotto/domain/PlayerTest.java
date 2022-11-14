2e9fbe5596222df7361d4ececc5252253fe21f75
win9 <krrr8@naver.com>
2022-11-14 20:14:22 +0900

    Test(PlayerTest): 금액입력 테스트 로직 변경 완료
    오류를 발생 시키지 않도록 변경함으로 테스트토 변경한다.
    오류가 발생하면 [ERROR]로 시작하는 문구가 포함되어 있는것을 체크하도록
    변경한다.

--- src/test/java/lotto/domain/PlayerTest.java
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.Application;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,8 +10,12 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.List;
 
-class PlayerTest {
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PlayerTest extends NsTest {
     private Player player = new Player();
+    private static final String ERROR_MESSAGE = "[ERROR]";
 
     /**
      * 금액 입력 테스트
@@ -17,9 +23,9 @@ class PlayerTest {
     @DisplayName("1000원으로 나누어 떨어지지 않으면 예외가 발생한다.")
     @Test
     void moneyInputNotDividedException() {
-        ScannerInput("1200");
-        Assertions.assertThrows(IllegalArgumentException.class, ()->{
-            player.inputMoney();
+        assertSimpleTest(() -> {
+            runException("1020");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
@@ -35,9 +41,18 @@ class PlayerTest {
     @DisplayName("0원을 입력할시 예외가 발생한다.")
     @Test
     void moneyInputZeroException() {
-        ScannerInput("0");
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            player.inputMoney();
+        assertSimpleTest(() -> {
+            runException("0");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("문자 입력할시 예외가 발생한다.")
+    @Test
+    void notMoneyInputZeroException() {
+        assertSimpleTest(() -> {
+            runException("AA");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
@@ -50,4 +65,8 @@ class PlayerTest {
         return new ByteArrayInputStream(input.getBytes());
     }
 
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
 }
