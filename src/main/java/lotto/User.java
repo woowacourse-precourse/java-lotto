ab4509004531cd1a7a1dabcee1666e99b0051ac4
wndyd <wndyd@JUYONGui-MacBookAir.local>
2022-11-14 10:27:50 +0900

    feat: User: printLotto() added

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,31 +1,44 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Comparator;
 
 public class User {
+    //Enum으로 3,4,5,5+불일치,6 나누기
     private int money;
     private List<Lotto> lottoBag;
 
-    public void getMoneyForLotto(){
+    public void getMoneyForLotto() {
         Scanner scanner = new Scanner(System.in);
         money = scanner.nextInt();
     }
 
-    public void buyLotto(){
+    public void buyLotto() {
         Seller seller = new Seller();
         seller.validateMoneyUnit(money);
         lottoBag = seller.sellLotto(money);
     }
 
-    public void printLotto(){
+    public void printLotto() {
+        List<Integer> sortedLotto;
         System.out.println(lottoBag.size() + "개를 구매했습니다.");
-        for(Lotto lotto: lottoBag){
-            System.out.println(lotto);
+        for (Lotto lotto : lottoBag) {
+            sortedLotto = lotto.getNumbers();
+            Collections.sort(sortedLotto);
+            System.out.println(sortedLotto);
         }
     }
 
-    public void compareLotto(){
+    public int compareLotto() {
+        Checker checker = new Checker();
+        for(Lotto userLotto: lottoBag)
+            for(int i = 0; i < userLotto.getNumbers().size(); i++)
+                for(int j = 0; j < checker.getWinNumber().size(); j++){
+                    if(userLotto.getNumbers().get(i) == checker.getWinNumber().get(j))
 
+                }
     }
 }
