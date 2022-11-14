d78d15d96d07666e3a414c6f5b31c585ad399915
MuSeung <peter0757@nvaer.com>
2022-11-14 09:14:26 +0900

    test: ResultPrint가 각 등수별 금액과 당첨수를 잘 출력하는 지 테스트하는 기능 구현
    printTwoSecond
        - 2등이 2개인 경우 2개가 당첨되었다고 출력이 되는지 테스트
    printAll
        - 각 등수별로 문구와 당첨수가 잘 출력이 되는지 테스트

--- src/test/java/lotto/ResultPrintTest.java
+++ src/test/java/lotto/ResultPrintTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ResultPrintTest {
+
     @DisplayName("2등 출력확인")
     @Test
     void printSecond() {
@@ -21,5 +22,34 @@ public class ResultPrintTest {
         System.setOut(new PrintStream(out));
         resultPrint.printingResult();
         assertThat(out.toString()).contains("5개 일치, 보너스 볼 일치 (30,000,000원) - 1개");
+    }
+    @DisplayName("2등 2개 출력확인")
+    @Test
+    void printTwoSecond() {
+        ResultPrint resultPrint = new ResultPrint(
+                Map.of("FIRST", 0, "SECOND", 2, "THIRD", 0,
+                        "FOURTH", 0, "FIFTH", 0, "NOTHING", 7)
+        );
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        resultPrint.printingResult();
+        assertThat(out.toString()).contains("5개 일치, 보너스 볼 일치 (30,000,000원) - 2개");
+    }
+    @DisplayName("1~5등 출력확인")
+    @Test
+    void printAll() {
+        ResultPrint resultPrint = new ResultPrint(
+                Map.of("FIRST", 1, "SECOND", 2, "THIRD", 3,
+                        "FOURTH", 5, "FIFTH", 8, "NOTHING", 7)
+        );
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        resultPrint.printingResult();
+        assertThat(out.toString())
+                .contains("3개 일치 (5,000원) - 8개")
+                .contains("4개 일치 (50,000원) - 5개")
+                .contains("5개 일치 (1,500,000원) - 3개")
+                .contains("5개 일치, 보너스 볼 일치 (30,000,000원) - 2개")
+                .contains("6개 일치 (2,000,000,000원) - 1개");
     }
 }
