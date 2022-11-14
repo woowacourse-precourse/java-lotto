cfec5321dffa00b2837e8eab729f678d890a06a5
sjkim <seoungjoo01@gmail.com>
2022-11-15 00:24:21 +0900

    feat: 1번 - 로또 구입 금액 입력 처리 기능
    InputView
    - 숫자가 아닌 다른 값이 입력 되었을 시 예외 처리

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,4 +1,24 @@
 package lotto.View;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.Domain.Message;
+
 public class InputView {
+    private static final int LOTTO_MIN_NUM = 1;
+    private static final int LOTTO_MAX_MUM = 45;
+
+    public static int inputPurchaseAmount() {
+        System.out.println(Message.INPUT_LOTTO_PURCHASE_AMOUNT);
+        String ans = Console.readLine();
+        return checkIsDigit(ans);
+    }
+
+    public static int checkIsDigit(String purchaseAmount) {
+        try {
+            return Integer.parseInt(purchaseAmount);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(Message.ERROR_PURCHASE_AMOUNT_IS_NOT_INT);
+        }
+
+    }
 }
