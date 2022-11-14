cd6fd51bf8c63996c698c0f8c56e7e9b43b1c6c0
josolha <josolha@nate.com>
2022-11-15 01:46:12 +0900

    feat(BuyLotto) : 입력 금액 만큼 로또 구매하는 클래스 구현

--- src/main/java/lotto/domain/BuyLotto.java
+++ src/main/java/lotto/domain/BuyLotto.java
@@ -1,16 +1,19 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BuyLotto {
 
+    public InPutSystem inPutSystem = new InPutSystem();
+
     public static final int LOTTO_PRICE = 1000;
 
     public List<List<Integer>> lottoList = new ArrayList<>();
-    public int cntLotto = 0;
+    public static int cntLotto = 0;
+
 
     public BuyLotto() {
         int price = Integer.parseInt(inputSystem());
@@ -22,24 +25,20 @@ public class BuyLotto {
 
         // 예외 처리 필요 ( 1000 단위 )
         if (price % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(PrintMessages.ERROR_MONEY_RANGE.getMessage());
         }
+        if (price < 1000) {
+            throw new IllegalArgumentException(PrintMessages.ERROR_MONEY_MIN.getMessage());
+        }
+
     }
 
     public void GenerateLottoNumber() {
         for (int i = 0; i < cntLotto; i++) {
-            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            List<Integer> randomList = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            Collections.sort(randomList);
+            Lotto lotto = new Lotto(randomList);
             lottoList.add(lotto.getNumbers());
         }
-    }
-
-    public void showLottos() {
-        for (List<Integer> list : lottoList) {
-            System.out.println(list);
-        }
-    }
-
-    public String inputSystem() {
-        return Console.readLine();
     }
 }
