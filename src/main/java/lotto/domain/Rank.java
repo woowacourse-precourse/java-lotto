0025a0aa527ff08f72d2e7bcc703c22a8929add4
이정우 <acg6138@naver.com>
2022-11-14 15:12:22 +0900

    feat(Rank): 당첨된 번호의 개수에 따른 등수와 금액을 가지는 Enum 생성

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+    LOSE(0, 0),
+    FIFTH(5000, 3),
+    FOURTH(50000, 4),
+    THIRD(1500000, 5),
+    SECOND(30000000, 5),
+    FIRST(2000000000, 6);
+
+    private final int prize;
+    private final int matchCount;
+
+    Rank(int prize, int matchCount) {
+        this.prize = prize;
+        this.matchCount = matchCount;
+    }
+
+    public Rank get(int matchCount, boolean bonus) {
+        if (matchCount == 5 && bonus) {
+            return SECOND;
+        }
+        return Arrays.stream(Rank.values())
+                .filter(r -> r.matchCount == matchCount)
+                .findFirst()
+                .orElse(LOSE);
+    }
+}
