a389a2742275c63c88d611c7a32fd3c1a67e829a
myungsikyoon <yms8795@gmail.com>
2022-11-14 16:10:44 +0900

    fix : 범위에서 벗어나는 값을 방지하기 위해 최대 구입할 수 있는 금액 한도 수정.

--- src/main/java/constants/LottoConstant.java
+++ src/main/java/constants/LottoConstant.java
@@ -1,16 +1,20 @@
 package constants;
 
 public class LottoConstant {
+    // 로또 번호 생성에 필요한 상수.
     public static final int NUMBERS_SIZE = 6;
     public static final int NUMBER_START = 1;
     public static final int NUMBER_END = 45;
 
+    // 로또 구매에 필요한 상수.
     public static final int LOTTO_PRICE = 1000;
-    public static final int MAX_LOTTO_PRICE_LENGTH = 7;
-    public static final int MAX_LOTTO_PRICE = 1000000;
+    public static final int MAX_AMOUNT_PAID_LENGTH = 6;
+    public static final int MAX_AMOUNT_PAID = 100000;
+
+    //입력한 당첨 번호를 분리하기 위해 필요한 상수.
     public static final int LEAST_COMMA_POSITION = 1;
-    public static final int MAX_WINNING_LOTTO_NUMBERS_LENGTH = 17;
-    public static final int MAX_WINNING_LOTTO_BONUS_NUMBERS_LENGTH = 2;
+    public static final int MAX_INPUT_NUMBERS_LENGTH = 17;
+    public static final int MAX_INPUT_BONUS_NUMBER_LENGTH = 2;
 
 
 }
