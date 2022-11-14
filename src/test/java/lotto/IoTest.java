70fc7c60ad12fa4220ae307525cb57d37174e05e
정승구 <jeongseung-gu@jeongseung-guui-MacBookAir.local>
2022-11-15 02:18:29 +0900

    feat:iotest 구현중

--- src/test/java/lotto/IoTest.java
+++ src/test/java/lotto/IoTest.java
@@ -1,5 +1,27 @@
 package lotto;
 
-public class IoTest {
+import lotto.model.Lotto;
+import lotto.view.InputUser;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class IoTest {
+    @Test
+    void inputException() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        InputUser user = new InputUser();
+        Method method = user.getClass().getDeclaredMethod("checkPurchase", String.class);
+        method.setAccessible(true);
+
+        try {
+            method.invoke(user,"1021");
+        } catch (InvocationTargetException e) {
+            e.getTargetException();
+        }
+    }
 }
