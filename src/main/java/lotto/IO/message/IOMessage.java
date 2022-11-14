aa4335f7556b530e45812b7ba0798a63c2590c67
jagajang <kosb7544@gmail.com>
2022-11-14 16:34:44 +0900

    refactor: change message for out.printf

--- src/main/java/lotto/IO/message/IOMessage.java
+++ src/main/java/lotto/IO/message/IOMessage.java
@@ -8,11 +8,11 @@ public enum IOMessage {
     LOTTO_BUY_NUM("개를 구매했습니다."),
     ASK_ANSWER("당첨 번호를 입력해 주세요."),
     ASK_BONUS("보너스 번호를 입력해 주세요."),
-    RANK_0("6개 일치 (" + LottoConfig.awardsToString.get(0) + "원) - "),
-    RANK_1("5개 일치, 보너스 볼 일치 (" + LottoConfig.awardsToString.get(1) + "원) - "),
-    RANK_2("5개 일치 (" + LottoConfig.awardsToString.get(2) + "원) - "),
-    RANK_3("4개 일치 (" + LottoConfig.awardsToString.get(3) + "원) - "),
-    RANK_4("3개 일치 (" + LottoConfig.awardsToString.get(4) + "원) - ");
+    RANK_0("6개 일치 (%,d원) - %d개\n"),
+    RANK_1("5개 일치, 보너스 볼 일치 (%,d원) - %d개\n"),
+    RANK_2("5개 일치 (%,d원) - %d개\n"),
+    RANK_3("4개 일치 (%,d원) - %d개\n"),
+    RANK_4("3개 일치 (%,d원) - %d개\n");
 
     private final String message;
 
