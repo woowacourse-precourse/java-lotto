2fd2841606095c17ecaa957acaae97c2422d1cc9
jmjmjames <chungjm0711@gmail.com>
2022-11-15 08:50:23 +0900

    feat(User): 사용자 구입 금액에 따른 로또 구매 기능 구현

---
+++ src/main/java/lotto/domain/User.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
+
+public class User {
+
+    private final int purchaseCount;
+    private final List<Lotto> lottos;
+    private final List<LottoRank> lottoRanks;
+
+    public User(int purchaseAmount) {
+        this.purchaseCount = purchaseAmount / Lotto.LOTTO_UNIT;
+        this.lottos = new ArrayList<>(this.purchaseCount);
+        this.lottoRanks = new ArrayList<>(this.purchaseCount);
+
+        purchaseLottos();
+    }
+
+    private void purchaseLottos() {
+        for (int i = 0; i < purchaseCount; i++) {
+            Lotto lotto = Lotto.create();
+            lottos.add(lotto);
+        }
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public List<LottoRank> getLottoRanks() {
+        return lottoRanks;
+    }
+}
