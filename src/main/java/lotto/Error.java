ab771dbe90f62f7f414ddb85d9de02a2911cc2ad
iamsseaa <homingsea@gmail.com>
2022-11-14 15:18:26 +0900

    feat: 빈칸으로 입력되었는지 검증

--- src/main/java/lotto/Error.java
+++ src/main/java/lotto/Error.java
@@ -1,2 +1,16 @@
-package lotto;public enum Error {
+package lotto;
+
+public enum Error {
+    INPUT_NOTHING("[ERROR] 아무것도 입력되지 않았습니다.")
+    ;
+
+    private final String message;
+
+    Error(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -7,4 +7,12 @@ public class User {
     public void inputMoney() {
         String money = Console.readLine();
     }
+
+
+
+    public void validateInputNothing(String input) {
+        if (input.equals("")) {
+            throw new IllegalArgumentException(Error.INPUT_NOTHING.getMessage());
+        }
+    }
 }
