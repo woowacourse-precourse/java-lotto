51a478fbd5365734e3f1dcf17a90c400c0d97c7b
DevJay1024 <devjay1024@gmail.com>
2022-11-15 01:16:27 +0900

    feat: 유효한 로또 구매금액 입력-반환 기능 구현

--- src/main/java/lotto/client/ConsoleUserInterface.java
+++ src/main/java/lotto/client/ConsoleUserInterface.java
@@ -2,8 +2,11 @@ package lotto.client;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.InvalidInputMessage;
+import lotto.Lotto;
 
 public class ConsoleUserInterface {
+
+    private final static String MSG_REQUEST_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
 
     public ConsoleUserInterface() {
     }
@@ -22,5 +25,31 @@ public class ConsoleUserInterface {
             );
         }
         return inputLine;
+    }
+
+    public int requestPurchaseAmount() {
+        output(MSG_REQUEST_PURCHASE_AMOUNT);
+        String input = input();
+        validatePurchasingAmount(input);
+        return Integer.parseInt(input);
+    }
+
+    private void validateAs_JavaInteger(String input){
+        try {
+            Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(
+                    InvalidInputMessage.ERR_DEFAULT + InvalidInputMessage.ERR_NOT_JAVA_INTEGER
+            );
+        }
+    }
+
+    private void validatePurchasingAmount(String input) {
+        validateAs_JavaInteger(input);
+        if (Integer.parseInt(input) % Lotto.PRICE != 0) {
+            throw new IllegalArgumentException(
+                    InvalidInputMessage.ERR_DEFAULT + InvalidInputMessage.ERR_MONEY_UNIT
+            );
+        }
     }
 }
