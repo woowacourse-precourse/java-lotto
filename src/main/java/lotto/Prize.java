c66cdf3dda3136339302666167b7153645dbe666
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-11-15 07:41:40 +0900

    fix: Prize에 일치갯수 추가

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,20 +1,26 @@
 package lotto;
 
 public enum Prize {
-    FIRST(2_000_000_000),
-    SECOND(30_000_000),
-    THIRD(1_500_000),
-    FOURTH(50_000),
-    FIFTH(5_000),
-    NOTHING(0);
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NOTHING(0, 0);
 
     final public int prize;
+    final public int numberOfLucky;
 
     public Integer getPrize() {
         return prize;
     }
 
-    Prize(int prize) {
+    public Integer getNumOfLucky() {
+        return numberOfLucky;
+    }
+
+    Prize(int numberOfLucky, int prize) {
+        this.numberOfLucky = numberOfLucky;
         this.prize = prize;
     }
 }
