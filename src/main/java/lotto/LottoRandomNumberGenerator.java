2650618c4cca4d8ac0053fcc6dbb4c9d8ce29dc0
June <68291395+june-777@users.noreply.github.com>
2022-11-14 16:04:55 +0900

    feat: 로또 난수 생성 및 유효성 검증 기능 구현

---
+++ src/main/java/lotto/LottoRandomNumberGenerator.java
@@ -0,0 +1,47 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LottoRandomNumberGenerator {
+
+	public List<Integer> randomUniqueNumbersGenerate() {
+		List<Integer> sixLengthRandomUniqueNumbers = pickUniqueNumbersInRange(1, 45, 6);
+		validate(sixLengthRandomUniqueNumbers);
+		return sixLengthRandomUniqueNumbers;
+	}
+
+	private void validate(List<Integer> sixLengthRandomUniqueNumbers) {
+		validateRange(sixLengthRandomUniqueNumbers);
+		validateSize(sixLengthRandomUniqueNumbers);
+		validateDuplicate(sixLengthRandomUniqueNumbers);
+	}
+
+	private void validateRange(List<Integer> randomNumbers) {
+		for (int randomNumber : randomNumbers) {
+			if (randomNumber < 1) {
+				throw new IllegalArgumentException("[ERROR] 난수 생성 시 최소값(1) 보다 작은 값이 있습니다.");
+			}
+			if (randomNumber > 45) {
+				throw new IllegalArgumentException("[ERROR] 난수 생성 시 최대값(45) 보다 큰 값이 있습니다.");
+			}
+		}
+	}
+
+	private void validateSize(List<Integer> randomNumbers) {
+		if (randomNumbers.size() != 6) {
+			throw new IllegalArgumentException("[ERROR] 생성한 난수의 개수가 6개가 아닙니다.");
+		}
+
+	}
+
+	private void validateDuplicate(List<Integer> randomNumbers) {
+		Set<Integer> uniqueNumbers = new HashSet<>(randomNumbers);
+		if (uniqueNumbers.size() != randomNumbers.size()) {
+			throw new IllegalArgumentException("[ERROR] 난수 생성 시 중복된 값이 있습니다.");
+		}
+	}
+}
