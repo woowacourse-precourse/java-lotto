88c0b97c9a8f825c69931b24b1eec7749a16ffa9
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-14 10:09:23 +0900

    Feat(LottoStatistics) : Rank enum 추가 #93
    Rank enum을 추가했습니다.
    
    public BigInteger getMoney() 메소드를 추가했습니다.
    public int getPrize()메소드를 추가했습니다.
    public void plusCount()메소드를 추가했습니다.
    public int getCount() 메소드를 추가했습니다.
    public int getMatch() 메소드를 추가했습니다.
    public String getPrintPrize() 메소드를 추가했습니다.

---
+++ src/main/java/lotto/LottoStatistics.java
@@ -0,0 +1,63 @@
+package lotto;
+
+import lotto.Lotto;
+import lotto.LottoBuyer;
+
+import java.util.List;
+import java.math.BigInteger;
+import java.util.Iterator;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+enum Rank {
+    FIFTH(3, 5_000, "5,000원"),
+    FOURTH(4, 50_000, "50,000원"),
+    THIRD(5, 1_500_000, "1,500,000원"),
+    SECOND(5,30_000_000, "30,000,000원"),
+    FIRST(6, 2_000_000_000, "2,000,000,000원");
+
+    private final int match;
+    private final int prize;
+
+    private final String printPrize;
+    private int count;
+
+    private BigInteger money;
+
+    Rank(int match, int prize, String printPrize) {
+        this.match = match;
+        this.prize = prize;
+        this.printPrize = printPrize;
+    }
+
+    public BigInteger getMoney(){
+        return this.money;
+    }
+
+    public int getPrize(){
+        return prize;
+    }
+
+    public void plusCount() {
+        this.count++;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    public int getMatch(){
+        return match;
+    }
+
+    public String getPrintPrize(){
+        return printPrize;
+    }
+
+}
+
+public class LottoStatistics {
+
+
+}
+
