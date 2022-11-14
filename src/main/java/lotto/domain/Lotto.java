9eceb7748c6acaa4a136e4d555709df7927a5305
Kimhyeyun <cqqwer@gmail.com>
2022-11-14 17:37:26 +0900

    Fix: 검증 메서드 삭제

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,38 +9,11 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        validateDuplicate(numbers);
-        validateRange(numbers);
-
         this.numbers = numbers;
     }
 
-    private void validateDuplicate(List<Integer> numbers) {
-        int count = (int) numbers.stream().distinct().count();
-        if (count < 6) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + DUPLICATE_LOTTO_NUMBER_MESSAGE);
-        }
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_LOTTO_NUMBER_SIZE_MESSAGE);
-        }
-    }
 
     // TODO: 추가 기능 구현
-    private void validateRange(List<Integer> numbers) {
-        int count = (int) numbers.stream()
-                .filter(number -> number >= LOTTO_NUMBER_MINIMUM)
-                .filter(number -> number <= LOTTO_NUMBER_MAXIMUM)
-                .count();
-
-        if (count != LOTTO_SIZE) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_NUMBER_INPUT_FORMAT_MESSAGE);
-        }
-    }
-
     public List<Integer> getNumbers() {
         return numbers;
     }
