1c64a3705189c1b6590888995701024bf997c505
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-15 00:50:24 +0900

    test(table): 정보를 담을 인스턴스 변수와 save, read 테스트 완료

---
+++ src/test/java/lotto/layer/TableTest.java
@@ -0,0 +1,44 @@
+package lotto.layer;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.Money;
+import lotto.domain.WinningLotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TableTest {
+
+    @DisplayName("테이블 정보 저장 및 읽기 테스트")
+    @Test
+    void TableSaveAndReadTest() {
+        // given
+        Table table = new Table();
+        saveTable(table);
+
+        //expect
+        assertThat(table.getLotteries()).isNotEmpty();
+        assertThat(table.getFrequency()).isNotEmpty();
+        assertThat(table.getMoney()).isNotNull();
+    }
+
+    private void saveTable(Table table) {
+        Money money = new Money(1000);
+        Map<WinningLotto, Integer> frequency = new HashMap<>();
+        frequency.put(WinningLotto.PLACE_1, 1);
+        List<Lotto> lotteries = new ArrayList<>();
+        Lotto lotto = new Lotto(List.of(1,2,3,45,5,6));
+        lotteries.add(lotto);
+
+        table.saveMoney(money);
+        table.saveFrequency(frequency);
+        table.saveLottoAll(lotteries);
+    }
+
+}
