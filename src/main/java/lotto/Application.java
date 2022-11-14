d7581fd61d794e3bcd86275a3d714f94527e4eba
minhongsun <alsghdtjs7@gmail.com>
2022-11-14 17:44:36 +0900

    feat : 로또 번호 및 출력문 구현
    - 처음 입력받은 금액만큼의 로또번호 구현
    - Lotto 매소드에 로또 번호 구현했음
    - 구현한 로또 번호 및 구매한 로또 갯수 출력문 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,20 @@
 package lotto;
 
 import lotto.Money;
+import lotto.Lotto;
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class Application {
     public static void main(String[] args) {
         int count = Money.calcmoney();
-
+        System.out.println(count + "개를 구매했습니다.");
+        for(int i=0 ; i<count ; i++){
+            Lotto lotto = new Lotto(new ArrayList<>());
+        }
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,26 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
         validate(numbers);
-        this.numbers = numbers;
-    }
+        System.out.println(numbers);
 
+        this.numbers = numbers;
+        }
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
-
     // TODO: 추가 기능 구현
 }
