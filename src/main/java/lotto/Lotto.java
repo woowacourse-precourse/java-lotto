3a53802fb827936659ddc01cda71f3044525b980
jungdoyoon <jungdo8016@gmail.com>
2022-11-14 19:53:27 +0900

    fix:예외 코멘트 정리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,17 +12,14 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            System.out.println("[ERROR] 로또 번호는 6 개여야 합니다.");
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ExceptionCode.Not_Size.message);
         }
         if (numbers.stream().distinct().count() != 6) {
-            System.out.println("[ERROR] 로또 번호는 중복되어서는 안됩니다.");
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ExceptionCode.Exists_Number.message);
         }
         for (int i = 0; i < numbers.size(); i++) {
             if (numbers.get(i) < 1 || numbers.get(i) > 46) {
-                System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(ExceptionCode.Over_Range_Number.message);
             }
         }
     }
--- src/main/java/lotto/UserMoney.java
+++ src/main/java/lotto/UserMoney.java
@@ -7,17 +7,26 @@ public class UserMoney {
     public int getUserMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         String money = Console.readLine();
-        moneyCheck(Integer.parseInt(money));
+
+        moneyCheck(checkInt(money));
 
         return Integer.parseInt(money);
     }
 
+    private int checkInt(String money) {
+        try {
+            return Integer.parseInt(money);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionCode.Not_Number.message);
+        }
+    }
+
     private void moneyCheck(int money) {
         if (money % 1000 != 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ExceptionCode.Not_Money.message);
         }
         if (money < 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ExceptionCode.Over_Money.message);
         }
     }
 }
