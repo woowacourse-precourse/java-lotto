d3122fa29f07537566a91232614b79ad6f7da618
3juhwan <13selfesteem91@naver.com>
2022-11-14 14:44:50 +0900

    feat(lotto): 로또 당첨 결과 계산 구현

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -14,7 +14,7 @@ public class LottoResult {
 
     public static List<Integer> getResult(List<Lotto> lottos, List<Integer> winningNumber, Integer bonusNumber) {
         int[] result = {0, 0, 0, 0, 0, 0};
-        for(List<Integer> numbers: lottos.stream().map(lotto -> lotto.getNumbers()).collect(Collectors.toList())) {
+        for (List<Integer> numbers : lottos.stream().map(lotto -> lotto.getNumbers()).collect(Collectors.toList())) {
             int index = compareNumber(numbers, winningNumber, bonusNumber);
             result[index]++;
         }
@@ -24,26 +24,31 @@ public class LottoResult {
     public static int compareNumber(List<Integer> lottoNumbers, List<Integer> winningNumbers, Integer bonusNumber) {
         int matchingCount = 0;
 
-        for(Integer winningNumber: winningNumbers) {
-            if(lottoNumbers.contains(winningNumber)) {
+        for (Integer winningNumber : winningNumbers) {
+            if (lottoNumbers.contains(winningNumber)) {
                 matchingCount++;
             }
         }
 
-        if(matchingCount==6) {
+        if (matchingCount == 6) {
             return FIRST_PLACE;
         }
 
-        if(winningNumbers.contains(bonusNumber)) {
+        if (winningNumbers.contains(bonusNumber)) {
             matchingCount++;
         }
 
         switch (matchingCount) {
-            case 6: return SECOND_PLACE;
-            case 5: return THIRD_PLACE;
-            case 4: return FOURTH_PLACE;
-            case 3: return FIFTH_PLACE;
-            default: return SIXTH_PLACE;
+            case 6:
+                return SECOND_PLACE;
+            case 5:
+                return THIRD_PLACE;
+            case 4:
+                return FOURTH_PLACE;
+            case 3:
+                return FIFTH_PLACE;
+            default:
+                return SIXTH_PLACE;
         }
 
     }
