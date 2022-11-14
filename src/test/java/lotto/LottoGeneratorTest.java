d30d5ae33989936f408f637ec6cadffe6c1d882d
dooboocookie <bkh3030@gmail.com>
2022-11-14 17:50:56 +0900

    test: 랜던 로또 번호 발생 Lotto의 예외 발생 없는지 반복 테스트

--- src/test/java/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -1,4 +1,19 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.*;
+
 class LottoGeneratorTest {
-  
+
+    @DisplayName("랜덤 넘버로 로또 생성시 Lotto의 예외 조건 안걸리는지 확인 반복 테스트")
+    @RepeatedTest(100)
+    void randomNumberRangeTest() throws Exception {
+        assertThatCode(() -> LottoGenerator.generatorLotto())
+                .doesNotThrowAnyException();
+    }
 }
