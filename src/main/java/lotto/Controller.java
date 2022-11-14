94edb6bc446db6a586063383d7827bee1c466abf
ChaCha3088 <cha3088@gmail.com>
2022-11-15 00:13:41 +0900

    refactor(/src/main/java/lotto/Controller.java): refactor LottoEnum 클래스 import 변경

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
-import lotto.Lotto.LottoReward;
+import lotto.LottoEnum.LottoReward;
 
 public class Controller {
     /**
