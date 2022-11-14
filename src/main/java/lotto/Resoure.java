2ab6cf57fac2299c3d5137c990db4752c8f97abd
starwook <star.wookk@gmail.com>
2022-11-15 01:06:22 +0900

    feat(Resource.java): Add Resource.java
    출력되는 모든 String들을 static final로 모아둔 Resource.java를 생성하라

---
+++ src/main/java/lotto/Resoure.java
@@ -0,0 +1,23 @@
+package lotto;
+
+public class Resoure {
+    public static final String greetings ="구입금액을 입력해주세요.";
+    public static final String countOfBuying ="개를 구매했습니다.";
+    public static final String inputOfWinningNumber = "당첨 번호를 입력해 주세요";
+    public static final String inputOfBonusNumber = "보너스 번호를 입력해 주세요.";
+    public static final String resultOfWinning = "당첨 통계\n---\n";
+    public static final String countOfCorrectNumberFront = "개 일치 ";
+    public static final String CountOfCorrectNumberFrontNotBonus ="(";
+    public static final String countOfCorrectNumberMiddle = ") - ";
+    public static final String countOfCorrectNumberBack ="개";
+    public static final String bonusCorrect =",보너스 볼 일치 (";
+    public static final String revenuePercentFront = "총 수익률은 ";
+    public static final String revenuePercentBack = "입니다.";
+
+    public static final String threeCorrect = "5,000원";
+    public static final String fourCorrect = "50,000원";
+    public static final String fiveCorrect = "1,500,000원";
+    public static final String fiveCorrectWithBonus = "30,000,000원";
+    public static final String sixCorrect ="2,000,000,000원";
+
+}
