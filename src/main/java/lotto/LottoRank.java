4131e655959fa53b4b8a5b8b4bdea2f17be563be
ingpyo <juninpyo72@gmail.com>
2022-11-14 11:15:03 +0900

    feat:당첨금,맞힌 개수 매핑하여 상수 정의

---
+++ src/main/java/lotto/LottoRank.java
@@ -0,0 +1,18 @@
+package lotto;
+
+public enum LottoRank {
+	FIRST_PLACE(6, 2_000_000_000),
+	SECOND_PLACE(5, 30_000_000),
+	THIRD_PLACE(5, 1_500_000),
+	FOURTH_PLACE(4, 50_000),
+	FIFTH_PLACE(3, 5_000);
+	private int sameNumber;
+	private int winnerPrize;
+
+	LottoRank(int sameNumber, int winnerPrice) {
+		this.sameNumber = sameNumber;
+		this.winnerPrize = winnerPrice;
+
+	}
+
+}
