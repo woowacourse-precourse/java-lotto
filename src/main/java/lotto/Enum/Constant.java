38f77feab9d9be8897ab0562f828bf36223b9a69
skck0226 <skck0226@ajou.ac.kr>
2022-11-14 17:56:14 +0900

    [refactor] Constant 상수명 역할을 잘 나타내게 수정

--- src/main/java/lotto/Enum/Constant.java
+++ src/main/java/lotto/Enum/Constant.java
@@ -1,17 +1,32 @@
 package lotto.Enum;
 
 public enum Constant {
-    ZERO("0"),
-    ONE("1"),
-    FIVE("5"),
-    SIX("6"),
-    SEVEN("7"),
-    FOURTY_FIVE("45"),
-    THOUSAND("1000"),
-    HUNDRED_THOUSAND("100000"),
+    THOUSAND_MODULAR_ZERO("0"),
+
+    ALL_DRAWN_NUMBERS_LENGTH("7"),
+
+    BONUS_NUMBER_MATCHED("1"),
+
+    THIRD_GRADE_NUMBER("5"),
+    SECOND_GRADE_NUMBER("7"),
+
+    ZERO_INDEX("0"),
+    ONE_INDEX("1"),
+
+    LOTTO_PURCHASE_MIN("1000"),
+    LOTTO_PURCHASE_MAX("100000"),
+
+    LOTTO_NUMBER_COUNT("6"),
+    LOTTO_NUMBER_MIN("1"),
+    LOTTO_NUMBER_MAX("45"),
+
     REGEX_ONLY_NUMBER("[0-9]+"),
     REGEX_WINNING_NUMBER_FORMAT("(\\d+,){5}\\d+"),
     REGEX_BONUS_NUMBER_FORMAT("\\d+"),
+
+    SPACE(" "),
+    EMPTY(""),
+
     MESSAGE_PURCHASE("개를 구매했습니다."),
     ;
 
