dd68fdc9df3f9270c032e699dfd797b147ad723c
Konahyeon <koo@inha.edu>
2022-11-14 14:24:41 +0900

    feat: 로또 번호 부여

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private static List<Lotto> lotto_list = new ArrayList<>(); //전체 로또 담을 리스트
+
+    public static List<Integer> makeRandom() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
