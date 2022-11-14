ac0511f8fbcd6603205dcce789072dd0f8f1d1aa
saint6839 <saint6839@gmail.com>
2022-11-14 14:57:56 +0900

    feat(domain) : Result와 Prize를 비교하기 위한 Predicate용 메서드 구현

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 public class Result {
+    private static final int SECOND_PRIZE_MATCH_COUNT = 5;
 
     private int matchCount;
     private boolean isBonus;
@@ -11,5 +12,24 @@ public class Result {
 
     public void matchBonus() {
         isBonus = true;
+    }
+
+    public boolean isProperPrize(Prize prize) {
+        if (prize.getMatchCount() == SECOND_PRIZE_MATCH_COUNT) {
+            return matchCount == prize.getMatchCount() && isBonus == prize.isBonus();
+        }
+        return matchCount == prize.getMatchCount();
+    }
+
+    public boolean isFiveHit() {
+        return matchCount == SECOND_PRIZE_MATCH_COUNT;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isBonus() {
+        return isBonus;
     }
 }
