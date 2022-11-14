9939a67b919bbb1d0ea83b33931f02ac1fbb5d35
오승윤 <>
2022-11-14 23:46:17 +0900

    feat (WinningResultCalculator) : 당첨 통계 책임 클래스 추가

---
+++ src/test/java/lotto/domain/WinningResultCalculatorTest.java
@@ -0,0 +1,52 @@
+package lotto.domain;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class WinningResultCalculatorTest {
+
+    WinningResultCalculator winningResultCalculator = new WinningResultCalculator();
+    WinningNumbers winningNumbers = new WinningNumbers();
+    Map<Rank, Integer> winningStatus;
+    Map<Rank, Integer> result;
+
+
+    @DisplayName("당첨 집계 출력")
+    @Test
+    void return_winncreateWinningStatusingStatus() {
+        assertThat(winningStatus).isEqualTo(result);
+    }
+
+    @BeforeEach
+    void init() {
+        List<Lotto> lottos = new LinkedList<>();
+        lottos.add(Lotto.generateFromRawValues(List.of(1, 2, 3, 4, 5, 7)));
+        lottos.add(Lotto.generateFromRawValues(List.of(1, 2, 3, 4, 5, 6)));
+        lottos.add(Lotto.generateFromRawValues(List.of(1, 2, 3, 4, 5, 7)));
+        lottos.add(Lotto.generateFromRawValues(List.of(1, 2, 3, 4, 5, 7)));
+
+        winningStatus = winningResultCalculator.createWinningStatus(lottos
+                , winningNumbers.generateFromRawValues(List.of(1, 2, 3, 4, 5, 6), 7));
+
+        result = new TreeMap<>();
+        result.put(Rank.FIRST, 1);
+        result.put(Rank.SECOND, 3);
+        result.put(Rank.THIRD, 0);
+        result.put(Rank.FOURTH, 0);
+        result.put(Rank.FIFTH, 0);
+        result.put(Rank.ZERO_MATCH, 0);
+
+    }
+
+}
