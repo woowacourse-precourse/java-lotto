b9771a804bbd32fa7e464bfd0e85925e13d5a0b7
JunHyeon <dlwnsgus07@naver.com>
2022-11-14 16:48:00 +0900

    feat : 발급하는 Lotto 번호의 유효성 검증기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,9 +14,12 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또번호는 총 6개여야 합니다.");
+        }
+        List<Integer> distinctNumbers = numbers.stream().distinct().collect(Collectors.toList());
+        if(distinctNumbers.size() != numbers.size()){
+            throw new IllegalArgumentException("[ERROR] 로또번호는 중복이 없어야 합니다.");
         }
     }
-
     // TODO: 추가 기능 구현
 }
