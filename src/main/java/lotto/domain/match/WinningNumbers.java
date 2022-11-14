051ebaa00da89dd7bb6adfad1d492673df8c31a4
2jun0 <soo28819@gmail.com>
2022-11-14 21:49:45 +0900

    Refactor(Match): 당첨번호의 validate 분리

--- src/main/java/lotto/domain/match/WinningNumbers.java
+++ src/main/java/lotto/domain/match/WinningNumbers.java
@@ -24,14 +24,24 @@ public class WinningNumbers {
     }
 
     private void validateStandardNumbers(List<Integer> numbers) {
+        validateStandardNumbersSize(numbers);
+        validateDuplicateStandardNumbers(numbers);
+        validateStandardNumbersRange(numbers);
+    }
+
+    private static void validateStandardNumbersSize(List<Integer> numbers) {
         if (numbers.size() != Lotto.NUMBER_COUNT) {
             throw new IllegalArgumentException("당첨번호는 6개여야 합니다.");
         }
+    }
 
+    private static void validateDuplicateStandardNumbers(List<Integer> numbers) {
         if (numbers.stream().distinct().count() != numbers.size()) {
             throw new IllegalArgumentException("당첨번호는 중복되면 안됩니다.");
         }
+    }
 
+    private void validateStandardNumbersRange(List<Integer> numbers) {
         if (!numbers.stream().allMatch(
                 number -> MIN_NUMBER <= number && number <= MAX_NUMBER)) {
             throw new IllegalArgumentException("당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
