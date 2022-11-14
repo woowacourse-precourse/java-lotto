1e7a1855f0a0b8c7fa63e9fc420dd177f5fb24f1
ljh <dusk5588@naver.com>
2022-11-15 03:15:40 +0900

    feat(Bonus) : 보너스 번호를 저장하는 클래스 구현

--- src/main/java/Domain/Bonus.java
+++ src/main/java/Domain/Bonus.java
@@ -1,4 +1,21 @@
 package Domain;
 
 public class Bonus {
+    private int number;
+
+    public Bonus(Winning winning){
+        this.number = getUniqueBonusNumber(winning);
+    }
+
+    public int getUniqueBonusNumber(Winning winning){
+        InputView.printInputBonusNumberMessage();
+        String readBonusNumber = InputView.readInputLine();
+        Validation.validateBonusNumberInput(readBonusNumber, winning.getNumbers());
+        int bonusNumber = Integer.parseInt(readBonusNumber);
+        return bonusNumber;
+    }
+
+    public int getBonusNumber(){
+        return this.number;
+    }
 }
