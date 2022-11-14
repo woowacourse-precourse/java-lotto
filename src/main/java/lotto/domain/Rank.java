87e02ddb03e7a3f8418123965e4b82e14fee2d5d
MinjeongKong <kong1301@ajou.ac.kr>
2022-11-15 04:32:22 +0900

    style: 중괄호 제거, 공백 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,11 +1,11 @@
 package lotto.domain;
 
 public enum Rank {
-    NONE(0,false,0),
-    FIFTH(3,false,5000),
+    NONE(0, false, 0),
+    FIFTH(3, false, 5000),
     FOURTH(4, false, 50000),
-    THIRD(5,false,1500000),
-    SECOND(5,true,30000000),
+    THIRD(5, false, 1500000),
+    SECOND(5, true, 30000000),
     FIRST(6, false, 2000000000);
 
 
@@ -20,21 +20,15 @@ public enum Rank {
     }
 
     public static Rank findRank(int cnt, boolean bonus) {
-        if (FIRST.cnt == cnt) {
-            return FIRST;
-        }
+        if (FIRST.cnt == cnt) return FIRST;
         if (SECOND.cnt == cnt) {
             if (bonus == SECOND.bonus) {
                 return SECOND;
             }
             return THIRD;
         }
-        if (FOURTH.cnt == cnt) {
-            return FOURTH;
-        }
-        if (FIFTH.cnt == cnt) {
-            return FIFTH;
-        }
+        if (FOURTH.cnt == cnt) return FOURTH;
+        if (FIFTH.cnt == cnt) return FIFTH;
         return NONE;
     }
 
