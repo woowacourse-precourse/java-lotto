6c94c06cea321e856086f61dc29cab985bca0e93
gnoesnooj <306yyy@naver.com>
2022-11-14 14:26:40 +0800

    refactor(LottoValidator) : 기능 분리에 따른 메소드 수정
    - Lotto, LottoGenerator, InputView 유효성 검사 기능 분리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,16 +1,18 @@
 package lotto.domain;
 
-import java.util.Collections;
+import static lotto.domain.LottoValidator.validateLottoNumbers;
+
 import java.util.List;
-import lotto.exception.NumberOutOfRangeException;
-import lotto.exception.NumbersDuplicatedException;
-import lotto.exception.NumbersNotMatchDigitException;
 
 public class Lotto {
     private final List<Integer> numbers;
 
+    private static final int SECOND = 7;
+
+    private static final int THIRD = 5;
+
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoNumbers(numbers);
         this.numbers = numbers;
     }
 
@@ -25,33 +27,14 @@ public class Lotto {
                 .filter(numbers::contains)
                 .count();
 
-        if (count == 5 && numbers.contains(bonusNumber)) {
-            count = 7;
+        if (isSecond(count, bonusNumber)) {
+            count = SECOND;
         }
 
         return count;
     }
 
-    private void validate(List<Integer> numbers){
-        validateNumbersMatchDigit(numbers);
-        validateNumberDuplicated(numbers);
-        validateNumbersOutOfRange(numbers);
-    }
-
-    private void validateNumberDuplicated(List<Integer> numbers){
-        if(numbers.stream().anyMatch(number -> Collections.frequency(numbers, number) > 1)){
-            throw new NumbersDuplicatedException();
-        }
-    }
-    private void validateNumbersMatchDigit(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new NumbersNotMatchDigitException();
-        }
-    }
-
-    private void validateNumbersOutOfRange(List<Integer> numbers) {
-        if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
-            throw new NumberOutOfRangeException();
-        }
+    private boolean isSecond(int count, int bonusNumber) {
+        return count == THIRD && numbers.contains(bonusNumber);
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,8 @@
 package lotto.view;
 
+import static lotto.domain.LottoValidator.validateLottoNumbers;
+import static lotto.rule.LottoRange.isOutOfRange;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -8,7 +11,6 @@ import lotto.exception.InputNotNumberException;
 import lotto.exception.InvalidAmountValueException;
 import lotto.exception.NumberOutOfRangeException;
 import lotto.exception.NumbersDuplicatedException;
-import lotto.exception.NumbersNotMatchDigitException;
 import lotto.exception.SmallAmountException;
 
 public class InputView {
@@ -17,7 +19,9 @@ public class InputView {
         try {
             String input = Console.readLine();
             int amount = Integer.parseInt(input);
+
             validateAmount(amount);
+
             return amount;
         } catch (NumberFormatException e) {
             throw new InputNotNumberException(); // 커스텀 예외 필요
@@ -28,14 +32,13 @@ public class InputView {
         try {
             String inputItems = Console.readLine();
             List<Integer> numbers = new ArrayList<>();
-
             String[] parsed = inputItems.split(","); // 파싱 오류가 발생할 일이 있을까 ?
 
             for (String s : parsed) {
                 numbers.add(Integer.parseInt(s));
             }
 
-            validateWinningNumbers(numbers);
+            validateLottoNumbers(numbers);
 
             return numbers;
         } catch (NumberFormatException e) {
@@ -49,6 +52,7 @@ public class InputView {
             int bonusNumber = Integer.parseInt(input);
 
             validateBonusNumber(numbers, bonusNumber);
+
             return bonusNumber;
         } catch (NumberFormatException e) {
             throw new InputNotNumberException();
@@ -72,37 +76,13 @@ public class InputView {
         }
     }
 
-    private void validateWinningNumbers(List<Integer> numbers) {
-        validateNumbersMatchDigit(numbers);
-        validateNumberDuplicated(numbers);
-        validateNumbersOutOfRange(numbers);
-    }
-
-    private void validateNumbersMatchDigit(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new NumbersNotMatchDigitException();
-        }
-    }
-
-    private void validateNumberDuplicated(List<Integer> numbers) {
-        if (numbers.stream().anyMatch(number -> Collections.frequency(numbers, number) > 1)) {
-            throw new NumbersDuplicatedException();
-        }
-    }
-
-    private void validateNumbersOutOfRange(List<Integer> numbers) {
-        if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
-            throw new NumberOutOfRangeException();
-        }
-    }
-
     private void validateBonusNumber(List<Integer> numbers, int bonusNumber) {
         validateBounsNumberOutOfRange(bonusNumber);
         validateBonusNumberDuplicated(numbers, bonusNumber);
     }
 
     private void validateBounsNumberOutOfRange(int bonusNumber) {
-        if (bonusNumber < 1 || bonusNumber > 45) {
+        if (isOutOfRange(bonusNumber)) {
             throw new NumberOutOfRangeException();
         }
     }
