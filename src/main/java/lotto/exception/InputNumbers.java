8ca1f92e4368029383373312aa392e33b2d03baf
Gibeom Nam <rlqja0523@naver.com>
2022-11-15 03:41:09 +0900

    feat: 쉼표로 구분되지 않을 경우 예외를 던지는 기능 구현

--- src/main/java/lotto/exception/InputNumbers.java
+++ src/main/java/lotto/exception/InputNumbers.java
@@ -2,6 +2,14 @@ package lotto.exception;
 
 public class InputNumbers extends CommonException {
 
+    private static void checkSeparationByComma(String numbers) {
+        long count = numbers.chars().filter(c -> c == ',').count();
+
+        if (count != 5) {
+            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_NOT_SEPARATED_BY_COMMA.getMessage());
+        }
+    }
+
     private static void checkNumberCount(String numbers) {
         String onlyNumber = numbers.replaceAll("[^0-9]", "");
 
