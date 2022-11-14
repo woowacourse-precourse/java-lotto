183e3b77bad715cbeacaae592857cf3f322ae7bf
c4fiber <qudcjf153@gmail.com>
2022-11-14 21:04:57 +0900

    refactor(LottoReward): 등수에 따른 상금 변경
    낙첨일경우 0원을 반환하도록 설정
    enum class 자체의 의미를 활용할 수 있도록 value를 하나로 줄임

--- src/main/java/lotto/LottoReward.java
+++ src/main/java/lotto/LottoReward.java
@@ -1,17 +1,16 @@
 package lotto;
 
 public enum LottoReward {
-    first(1, 2_000_000_000),
-    second(2, 30_000_000),
-    third(3, 1_500_000),
-    fourth(4, 50_000),
-    fifth(5, 5_000);
+    first(2_000_000_000),
+    second(30_000_000),
+    third(1_500_000),
+    fourth(50_000),
+    fifth(5_000),
+    zero(0);
 
-    private final int rank;
     private final int reward;
 
-    LottoReward(int rank, int reward) {
-        this.rank = rank;
+    LottoReward(int reward) {
         this.reward = reward;
     }
 }
