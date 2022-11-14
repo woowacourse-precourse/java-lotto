66dca7ce6fab99582c60b434cf1656d6eeb628f4
swonny <wonny921@gmail.com>
2022-11-14 22:34:29 +0900

    test(CalculatorTesst): 수익률 계산 테스트 추가

--- src/test/java/lotto/CalculatorTest.java
+++ src/test/java/lotto/CalculatorTest.java
@@ -20,14 +20,80 @@ public class CalculatorTest {
                 .isEqualTo(expected);
     }
 
-    @DisplayName("수익률 반올림 계산")
+    @DisplayName("수익률 소수점 두번째 자리에서 반올림되는 계산 테스트")
     @Test
-    void 수익률_반올림_계산_테스트() {
+    void 수익률_반올림_계산_테스트_1() {
         EnumMap<RankNumber, Integer> resultStatistics = Checker.initializeResultStatistics();
         resultStatistics.put(RankNumber.FIFTH, 2);
         float expected = 142.9f;
 
         assertThat(Calculator.getYield(7000, resultStatistics))
+                .isEqualTo(expected);
+    }
+
+    @DisplayName("소수점 첫번째 자리까지의 수익률 계산 테스트")
+    @Test
+    void 수익률_반올림_계산_테스트_2() {
+        EnumMap<RankNumber, Integer> resultStatistics = Checker.initializeResultStatistics();
+        resultStatistics.put(RankNumber.FIFTH, 1);
+        float expected = 250.0f;
+
+        assertThat(Calculator.getYield(2000, resultStatistics))
+                .isEqualTo(expected);
+    }
+
+    @DisplayName("수익률 1000% 이상 테스트")
+    @Test
+    void 수익률_반올림_계산_테스트_3() {
+        EnumMap<RankNumber, Integer> resultStatistics = Checker.initializeResultStatistics();
+        resultStatistics.put(RankNumber.FOURTH, 1);
+        float expected = 250.0f;
+
+        assertThat(Calculator.getYield(2000, resultStatistics))
+                .isEqualTo(expected);
+    }
+
+    @DisplayName("수익률 10,000% 이상 테스트")
+    @Test
+    void 수익률_반올림_계산_테스트_4() {
+        EnumMap<RankNumber, Integer> resultStatistics = Checker.initializeResultStatistics();
+        resultStatistics.put(RankNumber.THIRD, 1);
+        float expected = 75_000.0f;
+
+        assertThat(Calculator.getYield(2000, resultStatistics))
+                .isEqualTo(expected);
+    }
+
+    @DisplayName("수익률 100,000% 이상 테스트")
+    @Test
+    void 수익률_반올림_계산_테스트_5() {
+        EnumMap<RankNumber, Integer> resultStatistics = Checker.initializeResultStatistics();
+        resultStatistics.put(RankNumber.SECOND, 1);
+        float expected = 300_000.0f;
+
+        assertThat(Calculator.getYield(10_000, resultStatistics))
+                .isEqualTo(expected);
+    }
+
+    @DisplayName("수익률 1,000,000% 이상 테스트")
+    @Test
+    void 수익률_반올림_계산_테스트_6() {
+        EnumMap<RankNumber, Integer> resultStatistics = Checker.initializeResultStatistics();
+        resultStatistics.put(RankNumber.SECOND, 1);
+        float expected = 3_000_000.0f;
+
+        assertThat(Calculator.getYield(1_000, resultStatistics))
+                .isEqualTo(expected);
+    }
+
+    @DisplayName("수익률 10,000,000% 이상 테스트")
+    @Test
+    void 수익률_반올림_계산_테스트_7() {
+        EnumMap<RankNumber, Integer> resultStatistics = Checker.initializeResultStatistics();
+        resultStatistics.put(RankNumber.FIRST, 1);
+        float expected = 3_000_000.0f;
+
+        assertThat(Calculator.getYield(10_000, resultStatistics))
                 .isEqualTo(expected);
     }
 }
