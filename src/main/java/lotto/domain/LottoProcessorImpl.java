a1ecace133809f05d2eab56788cf1876eece00b2
5witchkr <hob4419@gmail.com>
2022-11-14 23:21:19 +0900

    refactor(RankEnum): rankEnum 변경

--- src/main/java/lotto/domain/LottoProcessorImpl.java
+++ src/main/java/lotto/domain/LottoProcessorImpl.java
@@ -3,8 +3,6 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
--- src/main/java/lotto/domain/RankEnum.java
+++ src/main/java/lotto/domain/RankEnum.java
@@ -2,20 +2,18 @@ package lotto.domain;
 
 public enum RankEnum {
 
-    FIRST(6, 2_000_000_000, true),
-    SECOND(5, 30_000_000, true),
-    THIRD(5, 1_500_000, false),
-    FOURTH(4,50_000, false),
-    FIFTH(3, 5_000, false);
+    FIRST(7, 2_000_000_000),
+    SECOND(6, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4,50_000),
+    FIFTH(3, 5_000);
     private final int matchNumber;
     private final int winnings;
-    private final Boolean bonus;
 
 
-    RankEnum(int matchNumber, int winnings, Boolean bonus) {
+    RankEnum(int matchNumber, int winnings) {
         this.matchNumber = matchNumber;
         this.winnings = winnings;
-        this.bonus = bonus;
     }
 
     public int getMatchNumber() {
@@ -24,10 +22,6 @@ public enum RankEnum {
 
     public int getWinnings() {
         return winnings;
-    }
-
-    public Boolean getBonus() {
-        return bonus;
     }
 }
 
