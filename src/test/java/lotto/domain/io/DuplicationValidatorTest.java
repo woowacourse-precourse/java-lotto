aefc30f6c27b2b357883167171e5fc21465d6b40
yubin <dbqls2821@gmail.com>
2022-11-15 00:08:36 +0900

    test(DuplicationValidatorTest): 보너스 번호와 당첨번호간에 중복되는 수가 없는 정상 작동 테스트 & 보너스 번호와 당첨번호간에 중복되는 수가 있는 예외 처리 테스트 추가

--- src/test/java/lotto/domain/io/DuplicationValidatorTest.java
+++ src/test/java/lotto/domain/io/DuplicationValidatorTest.java
@@ -50,4 +50,36 @@ class DuplicationValidatorTest {
     );
   }
 
+  @ParameterizedTest
+  @MethodSource("provideNormalWinningNumberAndBonusNumber")
+  void 입력받은_보너스_번호와_당첨_번호_간에_중복되는_수가_없는_정상_작동_테스트(List<Integer> winningNumber, int bonusNumber) {
+    DuplicationValidator duplicationValidator = new DuplicationValidator();
+    assertThatCode(() -> duplicationValidator.validate(winningNumber, bonusNumber))
+      .doesNotThrowAnyException();
+  }
+
+  static Stream<Arguments> provideNormalWinningNumberAndBonusNumber() {
+    return Stream.of(
+      Arguments.of(List.of(1,2,3,4,5,6), 7),
+      Arguments.of(List.of(45,46,47,48,49,50), 51),
+      Arguments.of(List.of(-1,-2,-3,-4,-5,-6), -7)
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideExceptionWinningNumberAndBonusNumber")
+  void 입력받은_보너스_번호와_당첨_번호_간에_중복되는_수가_포함되는_예외_처리_테스트(List<Integer> winningNumber, int bonusNumber) {
+    DuplicationValidator duplicationValidator = new DuplicationValidator();
+    assertThatThrownBy(() -> duplicationValidator.validate(winningNumber, bonusNumber))
+      .isInstanceOf(IllegalArgumentException.class)
+      .hasMessageContaining(ExceptionMessage.INPUT_IS_DUPLICATED.getMessage());
+  }
+
+  static Stream<Arguments> provideExceptionWinningNumberAndBonusNumber() {
+    return Stream.of(
+      Arguments.of(List.of(1,1,1,1,1,1), 1),
+      Arguments.of(List.of(1,2,1,3,4,5), 5),
+      Arguments.of(List.of(46,46,47,48,49,50,51,52,53), 49)
+    );
+  }
 }
