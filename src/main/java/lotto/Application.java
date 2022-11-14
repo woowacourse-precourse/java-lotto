9c2992d38e3b1283a32569188099930d61f3ddc6
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-14 23:49:02 +0900

    feat: LottoService play 메소드 실행 및 try catch 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.service.LottoService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try{
+            new LottoService().play();
+        }catch (IllegalArgumentException except){
+            System.out.println(except.getMessage());
+        }
     }
 }
