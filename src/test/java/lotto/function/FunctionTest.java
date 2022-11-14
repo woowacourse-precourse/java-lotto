946da81cb4ad6bf0e4d7c071a819847b62b8338a
seonghoo1217 <seonghoo1217@naver.com>
2022-11-15 03:07:57 +0900

    feat(FunctionTest) : 기존 등수를 HashMap으로 관리하는 것에 대한 테스트 케이스 작성
    테스트 케이스를 통해 정확히 HashMap에 값이 입력되는지 확인한다.

--- src/test/java/lotto/function/FunctionTest.java
+++ src/test/java/lotto/function/FunctionTest.java
@@ -3,9 +3,11 @@ package lotto.function;
 import lotto.domain.jackpot.JackpotResult;
 import lotto.domain.lotto.Lotto;
 
+import lotto.domain.prize.PrizeResult;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,6 +40,30 @@ class FunctionTest {
 		Lotto.compare_bonus_number(bonus_number, lottos, jr);
 		//then
 		assertThat(jr.return_bonus_count()).isEqualTo(1);
+	}
+
+	@Test
+	void MAP에_정확히_카운트_되는지_확인_1() {
+		//given
+		PrizeResult pr=new PrizeResult();
+		pr.prize_set_by_count(5,1);
+		//when
+		HashMap<String, Integer> hashMap = pr.prize_map_return();
+		int hit_five_and_bonus = hashMap.get("5개 일치, 보너스 볼 일치");
+		//then
+		assertThat(hit_five_and_bonus).isEqualTo(1);
+	}
+
+	@Test
+	void MAP에_정확히_카운트_되는지_확인_2() {
+		//given
+		PrizeResult pr=new PrizeResult();
+		pr.prize_set_by_count(4,0);
+		//when
+		HashMap<String, Integer> hashMap = pr.prize_map_return();
+
+		//then
+		assertThat(hashMap.get("3개 일치")).isEqualTo(null);
 	}
 
 	private static List<Integer> one_of_lotto() {
