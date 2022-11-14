a308b702f5ca32e0aef59d0a682438effae7c6df
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-14 12:49:27 +0900

    feat(Lotto): implement Lotto class with required methods
    what it is do:
    - validate argument before creating its field
    - sort numbers
    - convert input as Lotto Number to validate as Number
    - compare lotto numbers with winning lotto and bonus number
    -  check numbers has input Lotto Number by getting its value, not the class
    - return string to print numbers

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,74 @@
 package lotto;
 
 import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import lotto.ui.Validator;
+import java.util.Collections;
 
 public class Lotto {
-    private final List<Number> numbers;
+    private final List<LottoNumber> numbers;
 
-    public Lotto(List<Number> numbers) {
+    Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        List<LottoNumber> lottoNumbers = this.convertIntegersToNumbers(numbers);
+        this.sort(lottoNumbers);
+        this.numbers = lottoNumbers;
     }
 
-    private void validate(List<Number> numbers) {
+    private List<LottoNumber> convertIntegersToNumbers(List<Integer> numbers) {
+        return numbers.stream().map(LottoNumber::new).collect(Collectors.toList());
+    }
+
+    public List<LottoNumber> getNumbers() {
+        return this.numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
         Validator validator = new Validator();
-        validator.throwIfNotUnique(numbers);
         validator.throwIfNotSix(numbers);
+        validator.throwIfNotUnique(numbers);
     }
 
-    // TODO: 추가 기능 구현
+    private void sort(List<LottoNumber> numbers) {
+        Collections.sort(numbers);
+    }
+
+    private LottoNumber getLottoNumberAt(int index) {
+        return this.numbers.get(index);
+    }
+
+    public void checkBonusNumber(LottoNumber bonusNumber) {
+        Validator validator = new Validator();
+        List<Integer> integerNumbers = this.convertNumbersToIntegers();
+        validator.throwIfAlreadyHasNumber(integerNumbers, bonusNumber);
+    }
+
+    private List<Integer> convertNumbersToIntegers() {
+        return this.numbers.stream().map(LottoNumber::getNumber).collect(Collectors.toList());
+    }
+
+    public boolean contains(LottoNumber lottoNumber) {
+        List<Integer> integerNumbers = this.convertNumbersToIntegers();
+        return integerNumbers.contains(lottoNumber.getNumber());
+    }
+
+    public int getMatchCount(Lotto winningLotto, LottoNumber bonusLottoNumber) {
+        int matchCount = 0;
+        for (int i = 0; i < Constants.LOTTO_NUMBER_COUNT.getNumber(); i++) {
+            if (this.contains(winningLotto.getLottoNumberAt(i))) {
+                matchCount += 1;
+            }
+        }
+        if (matchCount == Constants.LOTTO_NUMBER_COUNT.getNumber() - 1) {
+            if (this.contains(bonusLottoNumber)) {
+                matchCount += 2;
+            }
+        }
+        return matchCount;
+    }
+
+    public String toString() {
+        return Arrays.toString(this.numbers.stream().map(LottoNumber::getNumber).toArray());
+    }
 }
