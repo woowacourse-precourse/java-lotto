947ff01eda36e3d50ed52bf97642bf5bc9e0e044
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-11-14 19:00:01 +0900

    feat(WinningLotto): 당첨 번호 검증 및 반환
    당첨번호와 보너스 번호를 입력받아 생성된 로또 값과 비교하여 반환한다
    Map<Rank,Integer> checking(List<Lotto> tickets): 구매한 로또 티켓을 당첨 번호와 비교하여 당첨 Map 을 반환한다
    int getMyWinningNumberCount(Lotto ticket): 당첨번호와 일치하는 로또 개수를 반환
    void addWinningCount(boolean checkBonus, int winningNumberCount): Map 에 당첨된 등수 개수를 저장한다
    Rank getRank(boolean checkBonus, int winningNumberCount): 당첨 등수를 비교한다

---
+++ src/main/java/domain/WinningLotto.java
@@ -0,0 +1,77 @@
+package domain;
+
+import Util.InputUtils;
+import lotto.Lotto;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+
+public class WinningLotto {
+    public static final int ZERO = 0;
+    private static final InputUtils utils = new InputUtils();
+    private final Lotto winningNumber;
+    private final int bonus;
+    private final Map<Rank,Integer> winningCount;
+
+    public WinningLotto() {
+        this.winningNumber = utils.StringToWinningLottoNumber();
+        this.bonus = utils.StringToBonus(this.winningNumber);
+        this.winningCount = new HashMap<>(){{
+                put(Rank.FIRST, ZERO);
+                put(Rank.SECOND, ZERO);
+                put(Rank.THIRD, ZERO);
+                put(Rank.FOURTH, ZERO);
+                put(Rank.FIFTH, ZERO);
+        }};
+    }
+
+    public Map<Rank,Integer> checking(List<Lotto> tickets){
+        for (Lotto ticket : tickets) {
+            boolean checkBonus = false;
+            int winningNumberCount = getMyWinningNumberCount(ticket);
+            if(ticket.getNumbers().contains(bonus)){
+                checkBonus = true;
+                winningNumberCount++;
+            }
+            addWinningCount(checkBonus, winningNumberCount);
+        }
+        return this.winningCount;
+    }
+
+    private void addWinningCount(boolean checkBonus, int winningNumberCount) {
+        if(winningNumberCount >= Rank.FIFTH.getWinningCount()){
+            Rank rank = getRank(checkBonus, winningNumberCount);
+            this.winningCount.put(rank, this.winningCount.get(rank) + 1);
+        }
+    }
+
+    private Rank getRank(boolean checkBonus, int winningNumberCount) {
+        if (winningNumberCount >= Rank.SECOND.getWinningMoney() && checkBonus) {
+            return Rank.SECOND;
+        }
+        if (winningNumberCount >= Rank.FIRST.getWinningCount()) {
+            return Rank.FIRST;
+        }
+        if (winningNumberCount >= Rank.THIRD.getWinningCount()) {
+            return Rank.THIRD;
+        }
+        if (winningNumberCount >= Rank.FOURTH.getWinningCount()) {
+            return Rank.FOURTH;
+        }
+        return Rank.FIFTH;
+    }
+
+    private int getMyWinningNumberCount(Lotto ticket) {
+        return (int) ticket.getNumbers()
+                .stream()
+                .filter(number -> winningNumber.getNumbers()
+                        .stream()
+                        .anyMatch(Predicate.isEqual(number))
+                )
+                .count();
+    }
+
+}
