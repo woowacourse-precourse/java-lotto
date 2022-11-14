00db8eca6dcf2f39237eb73aeb9443a627b4afad
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-11-14 17:41:33 +0900

    <feat>NumberComparator : 보너스 숫자 읽기 기능 추가

--- src/main/java/lotto/NumberComparator.java
+++ src/main/java/lotto/NumberComparator.java
@@ -29,9 +29,29 @@ public class NumberComparator {
         validateWinningNumber();
     }
 
+    void setWholeNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String number = Console.readLine();
+
+        this.number.add(Integer.parseInt(number));
+        validateWholeNumber();
+    }
+
     void validateWinningNumber() {
         if (this.number.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 입력한 당첨 번호의 개수가 6개가 아닙니다.");
+        }
+        if (Collections.min(this.number) < 1 || Collections.max(this.number) > 45) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (this.number.size() != this.number.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호에 중복된 숫자가 있습니다.");
+        }
+    }
+
+    void validateWholeNumber() {
+        if (this.number.size() != 7) {
+            throw new IllegalArgumentException("[ERROR] 입력한 당첨 번호의 개수가 7개가 아닙니다.");
         }
         if (Collections.min(this.number) < 1 || Collections.max(this.number) > 45) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
