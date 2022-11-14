90da6ec7b481160b3aed316c6f6e22c8783a75f9
solesy <tkddbs9801@gmail.com>
2022-11-15 04:44:58 +0900

    feat(Applicatioin): java-lotto 미션 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.ui.Input;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            RandomLottos randomLottos = new RandomLottos(Input.scanMoney());
+            randomLottos.printRandomLottos();
+
+            Lotto majorNumbers = new Lotto(Input.scanWinningLotto());
+
+            WinningLotto winningLotto = new WinningLotto(majorNumbers, Input.scanBonusNumber());
+
+            LottoStatistic lottoStatistic = new LottoStatistic(randomLottos, winningLotto);
+            lottoStatistic.printStatistic();
+        } catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
