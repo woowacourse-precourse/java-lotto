11f3ac2eae1bd7871443c6dbe0e6c704a83a665d
youswim <youswim96@gmail.com>
2022-11-15 01:27:03 +0900

    feat(lotto): 로또 번호 발행 기능 추가

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,11 +1,21 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
 
     public static List<Integer> makeLottoNumber() {
-        return null;
+        List<Integer> lottoNumbers = new ArrayList<>();
+        while (lottoNumbers.size() <= 6) {
+            int random = Randoms.pickNumberInRange(1, 45);
+            if (!lottoNumbers.contains(random)) {
+                lottoNumbers.add(random);
+            }
+        }
+        return lottoNumbers;
     }
 
 
