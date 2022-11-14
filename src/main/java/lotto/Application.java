0582d0be750f7a80b19397adc4ea864df3354bbb
Yeongseon Park <yseon406@gmail.com>
2022-11-15 07:06:02 +0900

    feat(lotto): Application 보너스 번호 입력 동작까지 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import lotto.domain.Generator;
+import lotto.domain.Purchase;
+import lotto.domain.RaffleNumber;
+import lotto.ui.Printer;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Purchase purchase = new Purchase();
+        purchase.inputMoney();
+        int amountOfLotto = purchase.canBuyLotto(purchase.money);
+
+        Generator generator = new Generator();
+        List<Lotto> myLotto = generator.createLottoList(amountOfLotto);
+
+        Printer printer = new Printer();
+        printer.printLotto(amountOfLotto, myLotto);
+
+        RaffleNumber raffleNumber = new RaffleNumber();
+        raffleNumber.inputWinningNumber();
+        raffleNumber.inputBonusNumber();
     }
 }
