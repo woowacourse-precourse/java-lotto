24d8cb362a865a21deaab2201104778248a5e76b
saint6839 <saint6839@gmail.com>
2022-11-14 21:52:53 +0900

    test(utils) : 숫자로 변경 불가능한 문자열이 들어왔을때 예외가 던져지는지 테스트 작성

--- src/test/java/lotto/utils/StringParserTest.java
+++ src/test/java/lotto/utils/StringParserTest.java
@@ -6,11 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class StringParserTest {
 
-    @DisplayName("숫자로 변경 가능한 문자와 ,로 구성된 문자열이 들어왔을때 리스트로 잘 반환되는지 테스트")
+    @DisplayName("숫자로 변경 가능한 문자와 ,로 구성된 문자열이 들어왔을때 리스트로 잘 반환되는지 테스트한다.")
     @Test
     void parseInputToIntegerListSuccess() {
         //given
@@ -20,5 +18,18 @@ class StringParserTest {
         //then
         Assertions.assertThat(parsedInputs.size())
                   .isEqualTo(6);
+    }
+
+    @DisplayName("숫자로 변경 불가능한 문자와 ,로 구성된 문자열이 들어왔을때 예외를 던지는지 테스트한다.")
+    @Test
+    void parseInputToIntegerListFail() {
+        //given
+        String input = "1,2,3,4,5,A";
+        //when
+        //then
+        Assertions.assertThatThrownBy(() -> {
+                      StringParser.parseInputToIntegerList(input);
+                  })
+                  .isInstanceOf(IllegalArgumentException.class);
     }
 }
