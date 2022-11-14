fe0c562500ffd9fd7d520febf0688ac07e752489
hyungkya <hyungkya@uci.edu>
2022-11-14 18:55:28 +0900

    refactor: 가격 입력을 숫자 입력으로 일반화
    Add several static string messages.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,22 +3,28 @@ package lotto;
 import static java.lang.Integer.parseInt;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
     private final static String inputPriceMessage = "구입금액을 입력해 주세요.";
+    private final static String inputWinningNumbersMessage = "당첨 번호를 입력해 주세요.";
+    private final static String priceNonNumericErrorMessage = "[ERROR] 구입금액은 숫자여야 합니다.";
+    private final static String bonusNonNumericErrorMessage = "[ERROR] 보너스 번호는 숫자여야 합니다.";
 
     public static void main(String[] args) {
-        int price = readPrice(inputPriceMessage);
+        int price = readNumber(inputPriceMessage, priceNonNumericErrorMessage);
         Simulator lottoSimulator = new Simulator(price);
         printPurchaseHistory(lottoSimulator);
     }
 
-    private static int readPrice(String outputMessage) {
+    private static int readNumber(String outputMessage, String errorMessage) {
         System.out.println(outputMessage);
         try {
             return parseInt(Console.readLine());
         } catch (Exception e) {
-            throw new IllegalArgumentException("[ERROR] 구입금액은 숫자여야 합니다.");
+            throw new IllegalArgumentException(errorMessage);
         }
     }
 
