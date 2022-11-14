8e1b0969911cbaa98d4c2c08d9c5a5545ae8884c
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-15 06:55:10 +0900

    fix(view/OutputView.java): 함수들 static선언해주고 printWinningStatistics함수는 ranking에 따라서 정렬되도록 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,33 +3,36 @@ package lotto.view;
 import lotto.domain.game.Ranking;
 import lotto.domain.player.Player;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import static lotto.view.OutputString.*;
 
 public class OutputView {
-    public void printLottoTickets(Player player){
+    public static void printLottoTickets(Player player){
         List<List<Integer>> lottoTickets = player.getLottoTickets();
         int count = lottoTickets.size();
         System.out.println(TICKET_QUANTITY_MESSAGE.print(count));
         printEachLottoTickets(lottoTickets);
     }
 
-    public void printEachLottoTickets(List<List<Integer>> lottoTickets) {
+    public static void printEachLottoTickets(List<List<Integer>> lottoTickets) {
         for (List<Integer> lottoTicket : lottoTickets) {
             System.out.println(lottoTicket);
         }
     }
 
-    public void printWinningStatistics(Player player){
-        for (Entry<Ranking, Long> entrySet : player.getWinningResult().entrySet()) {
-            System.out.println(entrySet.getKey().toString()
-                    + String.format(PHRASES_RANKING_COUNT.print(entrySet.getValue())));
-        }
+    public static void printWinningStatistics(Player player){
+        player.getWinningResult().entrySet().stream().filter(entry -> entry.getKey() != Ranking.DEFAULT)
+                .sorted(Collections.reverseOrder(Entry.comparingByKey()))
+                .forEach(entry -> System.out.println(entry.getKey().toString()
+                        + String.format(PHRASES_RANKING_COUNT.print(entry.getValue()))));
     }
 
-    public void printReturnRate(Player player){
+    public static void printReturnRate(Player player){
         System.out.println(RETURNRATE_MESSAGE.print(player.getReturnRate()));
     }
 }
