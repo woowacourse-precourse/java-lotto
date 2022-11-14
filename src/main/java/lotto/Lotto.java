55da74caa5e2a0b67131691f0e5fd9427ddaa8ce
sun007021 <sun007021@gmail.com>
2022-11-14 16:31:01 +0900

    style : 공백, 줄바꿈 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,7 +17,7 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
         Set<Integer> duplicateNumber = new HashSet<>(numbers);
-        if (numbers.size() != duplicateNumber.size()){
+        if (numbers.size() != duplicateNumber.size()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
         }
     }
--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -4,7 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
     private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    public static int inputPurchaseAmount(){
+
+    public static int inputPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT);
         String userInput = Console.readLine();
         int purchaseAmount = Integer.parseInt(userInput);
