64186a54383b078a74bab531bd039b8f639b2901
안광민 <creativeduck0518@gmail.com>
2022-11-14 13:09:18 +0900

    feat(LottoPrinter): 당첨 통계 출력 기능 구현

--- src/main/java/lotto/presentation/LottoPrinter.java
+++ src/main/java/lotto/presentation/LottoPrinter.java
@@ -1,6 +1,9 @@
 package lotto.presentation;
 
+import lotto.data.LottoRank;
 import lotto.data.OutputPrint;
+
+import java.util.Map;
 
 public class LottoPrinter {
 
@@ -11,5 +14,21 @@ public class LottoPrinter {
 
     public void printLottoCount(int count) {
         System.out.println(count + OutputPrint.OUTPUT_LOTTO_COUNT.getMessage());
+    }
+
+    public void printLottoResult(Map<LottoRank, Integer> winningCounts, double profit) {
+        printWinningStatistic();
+
+        for (LottoRank lottoRank : LottoRank.getLottoRanksDesc()) {
+            int winningAmount = winningCounts.get(lottoRank);
+            lottoRank.printLottoRankWithAmount(winningAmount);
+        }
+
+        printTotalProfit(profit);
+    }
+
+    public void printWinningStatistic() {
+        OutputPrint.OUTPUT_WINNING_STATISTIC.printMessage();
+        OutputPrint.OUTPUT_BORDER.printMessage();
     }
 }
