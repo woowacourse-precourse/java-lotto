34990aa7c1f25178a91fd5e52030416e693b42a5
Young37 <younghk37@gmail.com>
2022-11-15 03:16:37 +0900

    feat(domain): add the feature of checking integer range

--- src/main/java/lotto/domain/Judgment.java
+++ src/main/java/lotto/domain/Judgment.java
@@ -4,6 +4,7 @@ public class Judgment {
     private static final String NEGATIVE_INTEGER_ERROR_MESSAGE = "[ERROR] 로또 구입 금액은 양의 정수여야 합니다.";
     private static final String FAILED_TO_DIVIDE_BY_1000_ERROR_MESSAGE = "[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.";
     private static final String NO_5_COMMAS_ERROR_MESSAGE = "[ERROR] 당첨 번호는 , 다섯 개로 구성되어야 합니다.";
+    private static final String OUT_OF_BOUNDS_ERROR_MESSAGE = "[ERROR] 유효한 숫자 범위를 벗어났습니다.";
     
     public int countLottos(int money) {
         
@@ -29,6 +30,16 @@ public class Judgment {
         
         if (commaCount != 5) {
             throw new IllegalArgumentException(NO_5_COMMAS_ERROR_MESSAGE);
+        }
+    }
+    
+    public void checkRange(int number, int fromNumber, int toNumber) {
+        if (number < fromNumber) {
+            throw new IllegalArgumentException(OUT_OF_BOUNDS_ERROR_MESSAGE);
+        }
+        
+        if (toNumber < number) {
+            throw new IllegalArgumentException(OUT_OF_BOUNDS_ERROR_MESSAGE);
         }
     }
 }
