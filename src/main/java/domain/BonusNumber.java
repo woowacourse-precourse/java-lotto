a6b30df7d545231c9eb9ba44b9f2206b87e1ae52
bsh1603 <bsh1603@github.com>
2022-11-14 20:30:16 +0900

    Feat: 보너스 숫자 개수 확인

--- src/main/java/domain/BonusNumber.java
+++ src/main/java/domain/BonusNumber.java
@@ -1,9 +1,16 @@
 package domain;
 
+import exception.BonusExceptionType;
+
 public class BonusNumber {
 
     private final int BONUS_CNT = 1;
     private final int MIN_NUMBER = 1;
     private final int MAX_NUMBER = 45;
 
+    public void numberCount(String input){
+        if(input.length() != BONUS_CNT){
+            throw new IllegalArgumentException(BonusExceptionType.NOT_MATCH_BONUS_CNT.getErrorMessage());
+        }
+    }
 }
