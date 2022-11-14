eb385c79c82f2f42e3bed45ed2a6c5e04afc6ce1
Torres-09 <dla612@naver.com>
2022-11-14 18:41:14 +0900

    feat : 당첨 내역 계산 기능 추가
    checkLottoNumber : 당첨번호와 보너스 번호를 매개변수로 받는다.
    일치하는 결과를 List 형태로 리턴한다. get(0) -> 일치하는 당첨번호의 개수, get(1) -> 일치하는 보너스 번호의 개수

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -16,5 +19,17 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    // get(0) -> 일치하는 당첨번호의 개수, get(1) -> 일치하는 보너스 번호의 개수
+    public List<Integer> checkLottoNumber(List<Integer> lottoNumbers, int bonusNumber) {
+        List<Integer> lottoResult = new ArrayList<>();
+        numbers.removeAll(lottoNumbers);
+        lottoResult.add(6 - numbers.size());
+        if (numbers.contains(bonusNumber)) {
+            lottoResult.add(1);
+            return lottoResult;
+        }
+
+        lottoResult.add(0);
+        return lottoResult;
+    }
 }
