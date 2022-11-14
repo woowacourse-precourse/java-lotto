d5806409990d7bc2bda477a75b8e02d8ac442bc4
bumwoo <gld55602@gmail.com>
2022-11-14 21:18:22 +0900

    style: enum 컨벤션 조정

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -3,9 +3,12 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum WinningLotto {
-    FIRST(6, "6개 일치 (2,000,000,000원)", 2000000000), SECOND(5, "5개 일치, 보너스 볼 일치 (30,000,000원)", 30000000), THIRD(5,
-            "5개 일치 (1,500,000원)", 1500000), FOURTH(4, "4개 일치 (50,000원)", 50000), FIFTH(3, "3개 일치 (5,000원)",
-            5000), NOTHING(0, "miss", 0);
+    FIRST(6, "6개 일치 (2,000,000,000원)", 2000000000),
+    SECOND(5, "5개 일치, 보너스 볼 일치 (30,000,000원)", 30000000),
+    THIRD(5, "5개 일치 (1,500,000원)", 1500000),
+    FOURTH(4, "4개 일치 (50,000원)", 50000),
+    FIFTH(3, "3개 일치 (5,000원)", 5000),
+    NOTHING(0, "miss", 0);
 
     private int winningNuberCount;
     private String name;
