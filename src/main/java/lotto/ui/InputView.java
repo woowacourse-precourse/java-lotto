481177d5c44f4f8d7124e9c60e0e59ed695e3605
sonsoowon <yuwoo1018@gmail.com>
2022-11-15 00:02:22 +0900

    refactor(InputView): 불필요한 import 및 필드 삭제

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -2,20 +2,11 @@ package lotto.ui;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import lotto.domain.JackpotBonus;
-import lotto.valid.InputValidator;
-import lotto.valid.LottoValidator;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public enum InputView {
     INSTANCE;
     private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_JACKPOT_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-    private static final String JACKPOT_NUMBER_SEPARATOR = ",";
 
     public static String inputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
@@ -30,6 +21,7 @@ public enum InputView {
 
         return jackpotInput;
     }
+
     public static String inputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         String bonusInput = Console.readLine();
