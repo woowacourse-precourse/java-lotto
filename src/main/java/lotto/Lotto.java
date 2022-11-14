e36da38c9cb18748c9afba3a08f4102ce1e4407a
hyungkya <hyungkya@uci.edu>
2022-11-15 01:39:02 +0900

    feat: 두 개의 로또 번호 비교 후 등수 계산하기

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,6 +15,34 @@ public class Lotto {
         Collections.sort(this.numbers);
     }
 
+    public int compare(Lotto winning, int bonus) {
+        int count = 0;
+        for (int number : this.numbers) {
+            if (winning.numbers.contains(number)) {
+                count += 1;
+            }
+        }
+        return convertMatchCountToRank(count, this.contains(bonus));
+    }
+
+    private static int convertMatchCountToRank(int matchCount, boolean bonusMatch) {
+        if (matchCount == 3)
+            return 5;
+        if (matchCount == 4)
+            return 4;
+        if (matchCount == 5 & !bonusMatch)
+            return 3;
+        if (matchCount == 5 & bonusMatch)
+            return 2;
+        if (matchCount == 6)
+            return 1;
+        return 6;
+    }
+
+    public boolean contains(int number) {
+        return this.numbers.contains(number);
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리여야 합니다.");
