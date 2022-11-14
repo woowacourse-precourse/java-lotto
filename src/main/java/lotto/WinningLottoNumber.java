28dff126ed386e7c32e6e1fb6c02ecf0f8b6e5c8
JunHyeon <dlwnsgus07@naver.com>
2022-11-14 21:13:16 +0900

    feat: 당첨여부를 계산하는 기능 추가

--- src/main/java/lotto/WinningLottoNumber.java
+++ src/main/java/lotto/WinningLottoNumber.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class WinningLottoNumber {
@@ -19,6 +20,25 @@ public class WinningLottoNumber {
                 throw new IllegalArgumentException("[ERROR] 당첨번호와 보너스번호는 중복될 수 없습니다.");
             }
         }
+    }
+
+    public Integer correctCountCalculation(Lotto userLottoNumber){
+        Integer count = 0;
+        List<Integer> numbers = this.WinningLottoNumber.getNumbers();
+        for(Integer number : numbers){
+            if(userLottoNumber.getNumbers().stream().anyMatch(number::equals)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public Integer bonusCountCalculation(Lotto userLottoNumber){
+        Integer count = 0;
+        if(userLottoNumber.getNumbers().stream().anyMatch(this.bonusNumber.getBonusNumber()::equals)){
+            count++;
+        }
+        return count;
     }
 
     public Lotto getWinningLottoNumber(){
