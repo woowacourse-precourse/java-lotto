711c5455cd6f96d7f37d833b1a7a3a49de99699a
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-15 07:28:59 +0900

    feat(service): 입력값과 영속 데이터를 활용해 당첨 빈도수 생성 구현

--- src/main/java/lotto/layer/LottoService.java
+++ src/main/java/lotto/layer/LottoService.java
@@ -2,10 +2,13 @@ package lotto.layer;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
 import lotto.domain.Money;
+import lotto.domain.WinningLotto;
 
 public class LottoService {
 
@@ -27,5 +30,47 @@ public class LottoService {
         table.saveLottoAll(lotteries);
         table.saveMoney(money);
         return lotteries;
+    }
+
+    public Map<WinningLotto, Integer> getWinningLottoFrequency(Lotto commonLotto, LottoNumber bonusNumber) {
+        List<Lotto> lotteries = table.getLotteries();
+        Map<WinningLotto, Integer> frequency = initWinningLotto();
+        for (Lotto lotto : lotteries) {
+            WinningLotto winningLotto = getWinningLotto(commonLotto, bonusNumber, lotto);
+            if (winningLotto == null) {
+                continue;
+            }
+            frequency.put(winningLotto, frequency.getOrDefault(winningLotto, 0) + 1);
+        }
+        table.saveFrequency(frequency);
+        return frequency;
+    }
+
+    private Map<WinningLotto, Integer> initWinningLotto() {
+        Map<WinningLotto, Integer> frequency = new HashMap<>();
+        for (WinningLotto winningLotto : WinningLotto.values()) {
+            frequency.put(winningLotto, 0);
+        }
+        return frequency;
+    }
+
+    private WinningLotto getWinningLotto(Lotto commonLotto, LottoNumber bonusNumber, Lotto generatedLotto) {
+        int matchScore = calculateCommonMatchScore(commonLotto, generatedLotto);
+        boolean bonusNumberMatch = isBonusNumberMatch(bonusNumber, generatedLotto);
+        return WinningLotto.matchOf(matchScore, bonusNumberMatch);
+    }
+
+    private int calculateCommonMatchScore(Lotto commonLotto, Lotto generatedLotto) {
+        int commonMatchScore = 0;
+        for (LottoNumber lottoNumber : commonLotto) {
+            if (generatedLotto.contains(lottoNumber)) {
+                commonMatchScore++;
+            }
+        }
+        return commonMatchScore;
+    }
+
+    private boolean isBonusNumberMatch(LottoNumber bonusNumber, Lotto generatedLotto) {
+        return generatedLotto.contains(bonusNumber);
     }
 }
