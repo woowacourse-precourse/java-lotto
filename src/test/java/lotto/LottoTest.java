ab4b68f334d80722b919c8f2c0b60dd41073da6e
lecocococo <chemwang13@naver.com>
2022-11-14 21:52:26 +0900

    test(Lotto): 숫자 범위 확인과 로또의 등수 반환 테스트케이스 추가
    - 로또번호가 1~45 사이가 아닐때 IllegalArgumentException 발생하는지 테스트
    - 당첨번호와 비교 후 알맞은 등수가 결정되는지 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,14 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import lotto.domain.Rank;
+import lotto.domain.WinningNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,5 +27,42 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1~45 사이의 숫자가 아니라면 예외가 발생한다.")
+    @Test
+    void createLottoOutOfRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1,2,2,4,5,46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 1등부터 5등 당첨 확인 및 당첨 안된 경우도 확인")
+    @Test
+    void createLottoRank() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 45;
+
+        WinningNumbers winningNumbers = new WinningNumbers(winningLotto, bonusNumber);
+        assertThat(lotto.getRank(winningNumbers)).isEqualTo(Rank.FIRST);
+
+        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 45));
+        winningNumbers = new WinningNumbers(winningLotto, bonusNumber);
+        assertThat(lotto.getRank(winningNumbers)).isEqualTo(Rank.SECOND);
+
+        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        winningNumbers = new WinningNumbers(winningLotto, bonusNumber);
+        assertThat(lotto.getRank(winningNumbers)).isEqualTo(Rank.THIRD);
+
+        lotto = new Lotto(List.of(1, 2, 3, 4, 7, 8));
+        winningNumbers = new WinningNumbers(winningLotto, bonusNumber);
+        assertThat(lotto.getRank(winningNumbers)).isEqualTo(Rank.FOURTH);
+
+        lotto = new Lotto(List.of(1, 2, 3, 7, 8, 9));
+        winningNumbers = new WinningNumbers(winningLotto, bonusNumber);
+        assertThat(lotto.getRank(winningNumbers)).isEqualTo(Rank.FIFTH);
+
+        lotto = new Lotto(List.of(10, 11, 13, 7, 8, 9));
+        winningNumbers = new WinningNumbers(winningLotto, bonusNumber);
+        assertThat(lotto.getRank(winningNumbers)).isEqualTo(Rank.NONE);
+
+    }
 }
