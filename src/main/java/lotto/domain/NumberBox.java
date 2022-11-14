eaa9377dfefd6a034c26787940e18f9bea467b85
SeongukBaek <bsu6235@gmail.com>
2022-11-15 01:21:14 +0900

    feat: implement class NumberBox
    Added new methods:
    - validate(List<Integer> winningNumbers)
    - validate(List<Integer> winningNumbers, int bonusNumber)

--- src/main/java/lotto/domain/NumberBox.java
+++ src/main/java/lotto/domain/NumberBox.java
@@ -1,2 +1,44 @@
-package lotto.domain;public class NumberBox {
+package lotto.domain;
+
+import lotto.ui.Validator;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class NumberBox {
+    private static final String WRONG_LENGTH = "[ERROR] 로또 번호는 6개의 숫자여야 합니다.";
+    private static final String DUPLICATE_NUMBERS = "[ERROR] 로또 번호는 중복되지 않는 숫자들로 구성되어야 합니다.";
+    private static final String IN_WINNING_NUMBERS = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String NOT_IN_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private final Set<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public NumberBox(List<Integer> winningNumbers, int bonusNumber) {
+        validate(winningNumbers);
+        this.winningNumbers = new HashSet<>(winningNumbers);
+        validate(winningNumbers, bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private static void validate(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException(WRONG_LENGTH);
+        }
+        if (Validator.isDuplicate(winningNumbers)) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBERS);
+        }
+        if (!Validator.isInRange(winningNumbers)) {
+            throw new IllegalArgumentException(NOT_IN_RANGE);
+        }
+    }
+
+    private static void validate(List<Integer> winningNumbers, int bonusNumber) {
+        if (Validator.isDuplicate(winningNumbers, bonusNumber)) {
+            throw new IllegalArgumentException(IN_WINNING_NUMBERS);
+        }
+        if (!Validator.isInRange(bonusNumber)) {
+            throw new IllegalArgumentException(NOT_IN_RANGE);
+        }
+    }
 }
