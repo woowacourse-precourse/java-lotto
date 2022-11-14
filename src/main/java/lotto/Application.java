3489b4a915f6542cd6601ff064174f74a498ee5a
이승훈 <qwqw8019@naver.com>
2022-11-14 13:32:35 +0900

    feat: 당첨번호를 입력받는 기능 구현 -inputLottoNumber() -createLotto() -checkValidityOfLotto() 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,16 @@
 package lotto;
 
-
-
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int money = inputMoney();
-
+        Lotto lotto = inputLottoNumber();
 
     }
 
@@ -33,7 +35,6 @@ public class Application {
         return money;
     }
 
-
     public static int checkInput(String input) {
 
         int inputInteger = Integer.parseInt(input);
@@ -44,7 +45,51 @@ public class Application {
         return Integer.parseInt(input);
     }
 
+    public static Lotto inputLottoNumber() {
+        String inputNumberString = Console.readLine();
+        String[] numbers = inputNumberString.split(",");
+        Lotto lotto;
+        boolean inputLottoError = false;
+        try {
+            lotto = createLotto(numbers);
 
+        } catch (Exception e) {
+            inputLottoError = true;
+            throw new IllegalArgumentException();
+        } finally {
+            if (inputLottoError) {
+                System.out.println("[ERROR] 잘못된 로또번호 입력");
+            }
+        }
+        return lotto;
+    }
 
+    public static Lotto createLotto(String[] numbers) throws IllegalArgumentException {
+        List<Integer> lottoList = new ArrayList<>();
+        try {
+            for (String s : numbers) {
+                lottoList.add(Integer.parseInt(s));
+            }
+            lottoList.sort(Comparator.naturalOrder());
+            checkValidityOfLotto(lottoList);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
 
+        return new Lotto(lottoList);
+    }
+
+    public static void checkValidityOfLotto(List<Integer> lottoList) throws IllegalArgumentException {
+
+        if (lottoList.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < 5; i++) {
+            int currentNumber = lottoList.get(i);
+            int nextNumber = lottoList.get(i + 1);
+            if (currentNumber == nextNumber) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
