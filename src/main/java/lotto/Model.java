7f2c5835d4e1c80de497d6e66ba91b4f9b0e350a
ChaCha3088 <cha3088@gmail.com>
2022-11-14 19:28:41 +0900

    fix(/src/main/java/lotto/Model.java): fix method 중복되지 않는 6개의 로또 숫자를 생성하는 메소드 수정

--- src/main/java/lotto/Model.java
+++ src/main/java/lotto/Model.java
@@ -13,18 +13,11 @@ public class Model {
      * 1에서 45까지 서로 다른 임의의 수 6개의 로또 숫자를 생성한다.
      * @return 로또 숫자를 ArrayList로 return 한다.
      */
-    private static ArrayList<Integer> CreateLottoNumber() {
-        ArrayList<Integer> lottoNumber = new ArrayList<>();
-
+    private static List<Integer> CreateLottoNumber() {
         // 1에서 45까지 서로 다른 임의의 수 6개를 생성한다.
-        while (lottoNumber.size() < 6) {
-            int randomNumber = Randoms.pickNumberInRange(1, 45);
-            if (!lottoNumber.contains(randomNumber)) {
-                lottoNumber.add(randomNumber);
-            }
-        }
+        List<Integer> randomNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
 
-        return lottoNumber;
+        return randomNumber;
     }
 
     /**
