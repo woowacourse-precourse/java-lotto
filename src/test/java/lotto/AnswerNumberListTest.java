ea61f7f19dcdfd96a238b4eb309186220499634b
vcho1958 <gusckd57@gmail.com>
2022-11-15 06:54:23 +0900

    fix(AnswerNumberListTest): 예외 감지 메소드 변경 단위 테스트이기 때문에 assertSimple에서 assertThatThrownBy로 변경한다.

--- src/test/java/lotto/AnswerNumberListTest.java
+++ src/test/java/lotto/AnswerNumberListTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AnswerNumberListTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -15,38 +16,27 @@ class AnswerNumberListTest extends NsTest {
     @Test
     @DisplayName("당첨번호 목록에 음수가 있을 경우 예외발생")
     public void checkNegativeInWinningNumbers() {
-        assertSimpleTest(() -> {
-            new AnswerNumberList(List.of(-2L, 3L, 2L, 1L, 4L, 5L), 4L);
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
+        assertThatThrownBy(() -> new AnswerNumberList(List.of(-2L, 3L, 2L, 1L, 4L, 5L), 4L)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("보너스 번호에 음수가 있을 경우 예외발생")
     public void checkNegativeInBonusNumber() {
-        assertSimpleTest(() -> {
-            new AnswerNumberList(List.of(6L, 3L, 2L, 1L, 4L, 5L), -4L);
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
+        assertThatThrownBy(() -> new AnswerNumberList(List.of(6L, 3L, 2L, 1L, 4L, 5L), -4L)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("보너스 번호에 범위밖 수가 있을 경우 예외 발생")
     public void checkRangeInBonusNumber() {
-        assertSimpleTest(() -> {
-            new AnswerNumberList(List.of(6L, 3L, 2L, 1L, 4L, 5L), 47L);
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
+        assertThatThrownBy(() -> new AnswerNumberList(List.of(6L, 3L, 2L, 1L, 4L, 5L), 47L)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("당첨 번호 목록에 범위밖 수가 있을 경우 예외 발생")
     public void checkRangeInWinningNumbers() {
-        assertSimpleTest(() -> {
-            new AnswerNumberList(List.of(46L, 3L, 2L, 1L, 4L, 5L), 4L);
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
+        assertThatThrownBy(() -> new AnswerNumberList(List.of(46L, 3L, 2L, 1L, 4L, 5L), 4L)).isInstanceOf(IllegalArgumentException.class);
     }
+
     @Override
     protected void runMain() {
 
