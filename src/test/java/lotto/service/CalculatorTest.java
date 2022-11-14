a226ff0ee1a237da22ef2dfd5213266251dd57be
Gibeom Nam <rlqja0523@naver.com>
2022-11-14 23:34:41 +0900

    test: 수익률 계산 테스트

--- src/test/java/lotto/service/CalculatorTest.java
+++ src/test/java/lotto/service/CalculatorTest.java
@@ -19,4 +19,18 @@ public class CalculatorTest {
         // then
         assertEquals(10, purchaseCount);
     }
+
+    @Test
+    @DisplayName("총 당첨 금액과 로또 구매 금액을 가지고 수익률을 계산한다.")
+    void yieldTest() {
+        // given
+        long totalWinningPrize = 5000;
+        int purchaseAmount = 8000;
+
+        // when
+        String yield = Calculator.getYield(totalWinningPrize, purchaseAmount);
+
+        // then
+        assertEquals("62.5%", yield);
+    }
 }
