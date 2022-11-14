1c3b58324e220f55174188612118a0b0677a6980
bsh1603 <bsh1603@github.com>
2022-11-14 20:27:14 +0900

    Fix: [ERROR] 문구 추가

--- src/main/java/exception/LottoExceptionType.java
+++ src/main/java/exception/LottoExceptionType.java
@@ -1,9 +1,9 @@
 package exception;
 
 public enum LottoExceptionType {
-    NOT_MATCH_CNT("로또 숫자 개수가 6개가 아닙니다."),
-    NOT_MATCH_RANGE("로또 숫자 범위가 1~45 사이가 아닙니다."),
-    NOT_UNIQUE_NUMBER("로또 숫자가 중복되었습니다.");
+    NOT_MATCH_CNT("[ERROR] 로또 숫자 개수가 6개가 아닙니다."),
+    NOT_MATCH_RANGE("[ERROR] 로또 숫자 범위가 1~45 사이가 아닙니다."),
+    NOT_UNIQUE_NUMBER("[ERROR] 로또 숫자가 중복되었습니다.");
 
     private String errorMessage;
 
