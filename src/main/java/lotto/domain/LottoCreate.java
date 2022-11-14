b1bc97b8ab7b1790f2577911d138816491eeafb8
kjg1514 <kjg1514@naver.com>
2022-11-14 22:20:50 +0900

    feat : 로또번호 정렬

--- src/main/java/lotto/domain/LottoCreate.java
+++ src/main/java/lotto/domain/LottoCreate.java
@@ -1,15 +1,25 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+import lotto.view.InputView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoCreate {
     private List<Integer> lottoNumbers = new ArrayList<>();
 
-    public List<Integer> createLottonum() {
+    public List<Integer> createLottoNum() {
         lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        return lottoNumbers;
+        return sortLottoNumbers(lottoNumbers);
     }
+
+    private List<Integer> sortLottoNumbers(List<Integer> numbers) {
+        return numbers.stream().sorted().collect(Collectors.toList());
+    }
+
+
+
 }
