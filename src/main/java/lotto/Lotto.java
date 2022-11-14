b266ef6ec2b091ec2e2dd034c8ca4064b486e280
gudals <kimtuti@naver.com>
2022-11-14 19:19:46 +0900

    feat(lotto) : add winning check function
    일치개수 : 당첨복권수 의 맵 자료형을 반환하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Lotto {
@@ -17,6 +18,50 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+    /**
+     * 6개 일치 -> 1등
+     * 5개 일치 + bonus -> 2등
+     * 5개 일치 -> 3등
+     * 4개 일치 -> 4등
+     * 3개 일치 -> 5등
+     *
+     * @return
+     */
+    public HashMap<Integer, Integer> gameConfirm(List<List<Integer>> lottoList, int bonus){
+        HashMap<Integer, Integer> winNumbers = getwinnerSetMap();
+        for (List<Integer> lottoNum : lottoList) {
+            Integer fixnum = fixNumber(lottoNum);
+            if (fixnum==5&&lottoList.contains(bonus)){
+                winNumbers.put(7,winNumbers.get(7)+1);//2등
+            }
+            winNumbers.put(fixnum,winNumbers.get(fixnum)+1);
+        }
+        return winNumbers;
+    }
+
+    private static HashMap<Integer, Integer> getwinnerSetMap() {
+        HashMap<Integer, Integer> winNumbers = new HashMap<>();
+        winNumbers.put(0,0);
+        winNumbers.put(1,0);
+        winNumbers.put(2,0);
+        winNumbers.put(3,0);//5
+        winNumbers.put(4,0);//4
+        winNumbers.put(5,0);//3
+        winNumbers.put(6,0);//1
+        winNumbers.put(7,0);//2
+        return winNumbers;
+    }
+
+    private Integer fixNumber(List<Integer> lottoNum){
+        int winNum = 0;
+        for (int num : numbers){
+            if (lottoNum.contains(num)){
+                winNum++;
+            }
+        }
+        return winNum;
+    }
 
 
 }
