acd7e1a3fe46c30703e64d64c485510fbd9aa272
TaemHam <tk4003950@gmail.com>
2022-11-14 10:53:21 +0900

    Feat: LottoBundleDto.createWithInput 메서드 구현

--- src/main/java/lotto/data/dto/LottoBundleDto.java
+++ src/main/java/lotto/data/dto/LottoBundleDto.java
@@ -2,16 +2,69 @@ package lotto.data.dto;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lotto.type.ConstantNumberType;
+import lotto.type.ExceptionType;
+import utils.RandomNumberGenerator;
+import utils.Validator.IntegerValidator;
+import utils.Validator.StringValidator;
 
 public class LottoBundleDto {
 
-    private final List<Lotto> lottos;
+    private Long ownerId;
+    private final List<LottoDto> lottos;
 
-    public LottoBundleDto(List<Lotto> lottos) {
+    public LottoBundleDto(List<LottoDto> lottos) {
         this.lottos = Collections.unmodifiableList(lottos);
     }
 
-    public List<Lotto> getLottos() {
+    public static LottoBundleDto createWithInput(String input) throws IllegalArgumentException {
+        validate(input);
+        int buyableCount = getBuyableCount(Integer.parseInt(input));
+        List<LottoDto> lottos = generateRandomLottos(buyableCount);
+        return new LottoBundleDto(lottos);
+    }
+
+    private static void validate(String input) throws IllegalArgumentException {
+        if(hasErroneousNumberFormat(input)) {
+            throw ExceptionType.NUMBER.getException();
+        }
+        if(hasErroneousMoneyValue(Integer.parseInt(input))) {
+            throw ExceptionType.MONEY.getException();
+        }
+    }
+
+    private static boolean hasErroneousNumberFormat(String input) {
+        return !StringValidator.isNumeric(input);
+    }
+
+    private static boolean hasErroneousMoneyValue(int value) {
+        return !IntegerValidator.isDivisibleBy(value, ConstantNumberType.COST.getValue());
+    }
+
+    private static int getBuyableCount(int money){
+        return money / ConstantNumberType.COST.getValue();
+    }
+
+    private static List<LottoDto> generateRandomLottos(int count) {
+        return IntStream.range(0, count)
+                .mapToObj(number -> new LottoDto(RandomNumberGenerator.generateMultipleNumbers(
+                        ConstantNumberType.MIN.getValue(),
+                        ConstantNumberType.MAX.getValue(),
+                        ConstantNumberType.COUNT.getValue())))
+                .collect(Collectors.toList());
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public List<LottoDto> getLottos() {
         return lottos;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
     }
 }
