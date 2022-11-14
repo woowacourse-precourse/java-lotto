0b84a91ea85dd85b89688fb8075ca1daf9d8791c
footdev <ekclstkfka5@gmail.com>
2022-11-14 23:10:34 +0900

    refactor: 코드 수정
    Validation 클래스의 메소드들의 반환 타입을 boolean => void로 수정하고 상수들을 다른 곳에서도 쓰기 위해서 private => public으로 변경함.

--- src/main/java/lotto/domain/Validation.java
+++ src/main/java/lotto/domain/Validation.java
@@ -1,45 +1,40 @@
 package lotto.domain;
 
 public class Validation {
-    private static final String ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-    private static final int LOTTO_MONEY = 1000;
-    private static final int LOTTO_NUMBER = 6;
-    private static final int LOTTO_MIN = 1;
-    private static final int LOTTO_MAX = 45;
+    public static final String ERROR_MESSAGE = "[ERROR]";
+    public static final int LOTTO_MONEY = 1000;
+    public static final int LOTTO_NUMBER = 6;
+    public static final int LOTTO_MIN = 1;
+    public static final int LOTTO_MAX = 45;
 
-    public static boolean validateInteger(String input) {
+    public static void validateInteger(String input) {
         if (!input.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
-        return true;
     }
 
-    public static boolean isStartWithZero(String input) {
+    public static void isStartWithZero(String input) {
         if (input.charAt(0) == '0') {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
-        return true;
     }
 
-    public static boolean validateAmount(int input) {
+    public static void validateAmount(int input) {
         if (input % LOTTO_MONEY != 0) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
-        return true;
     }
 
-    public static boolean validateNumbers(int input) {
+    public static void validateNumbers(int input) {
         if(input != LOTTO_NUMBER) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
-        return true;
     }
 
-    public static boolean validateRange(int number) {
+    public static void validateRange(int number) {
         if(number < LOTTO_MIN || number > LOTTO_MAX) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
-        return true;
     }
 
 }
