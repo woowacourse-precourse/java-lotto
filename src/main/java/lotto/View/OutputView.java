3d9cd00a7094dec99e26b6d89d8c3be4d003f627
namtndus <rnrmf0721@gmail.com>
2022-11-14 19:11:42 +0900

    feat(OutputView): printLottoNumbers함수 구현
    컴퓨터가 만든 로또들을 출력하는 기능 구현

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -1,10 +1,21 @@
 package lotto.View;
 
+import lotto.Dto.Lotto;
+
+import java.util.List;
+
 import static lotto.Dto.ConstDto.InputOutputConstDto.PRINT_LOTTO_SIZE_MESSAGE;
 
 public class OutputView {
 
     public void printLottoSize(int lottoSize){
         System.out.println(lottoSize+PRINT_LOTTO_SIZE_MESSAGE);
+    }
+
+    public void printLottoNumbers(List<Lotto> lottos) {
+        for(Lotto lotto : lottos){
+            System.out.println(lotto.toString());
+        }
+        System.out.println();
     }
 }
