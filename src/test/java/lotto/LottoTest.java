bbbe4b675af0553a28768719f473752be17f7d10
abbd2 <ajkety00@gmail.com>
2022-11-15 03:56:06 +0900

    Test(LottoTest): Lotto의 리스트 값 변경

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,7 +3,6 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -13,7 +12,7 @@ class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(0, 0, 0, 0, 0, 0)))
+        assertThatThrownBy(() -> new Lotto(List.of(0, 0, 0, 0, 0, 0, 0)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -29,7 +28,7 @@ class LottoTest {
     @DisplayName("1000으로 나눈 몫을 반환, 1000으로 나누어떨어지지 않으면 IllegalArgumentExcpetion 발생")
     @Test
     void lottoCount() {
-        Lotto lotto = new Lotto(List.of(0, 0, 0, 0, 0, 0));
+        Lotto lotto = new Lotto(List.of(0, 1, 2, 3, 4, 5));
         assertThatThrownBy(() -> lotto.lottoCount(1001))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -37,16 +36,8 @@ class LottoTest {
     @DisplayName("리스트를 오름차순 정렬")
     @Test
     void sortLottoNumberAscending() {
-        Lotto lotto = new Lotto(List.of(0, 0, 0, 0, 0, 0));
+        Lotto lotto = new Lotto(List.of(0, 1, 2, 3, 4, 5));
         assertEquals(List.of(1, 2, 3, 4, 5, 6), lotto.sortLottoNumberAscending(List.of(5, 6, 2, 1, 3, 4)));
-    }
-
-    @DisplayName("로또 번호와 당첨 번호를 비교하여 같은 번호를 저장한 리스트를 반환")
-    @Test
-    void compareLotto() {
-        Lotto lotto = new Lotto(List.of(0, 0, 0, 0, 0, 0));
-        List<Lotto> lottoList = List.of(new Lotto(List.of(1, 11, 3, 5, 34, 43)));
-        assertEquals(List.of(1, 3, 5), lotto.compareLotto(lottoList.get(0), List.of(13, 1, 41, 5, 27, 3)).getWinLottoNumber());
     }
 
 }
