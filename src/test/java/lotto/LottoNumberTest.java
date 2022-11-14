c00b98ddb62308c6ed6d0bb7b0dd9cfe5fa84dd8
ttaehee <ttaehee1105@gmail.com>
2022-11-15 00:39:42 +0900

    test : 로또숫자 도메인 단위테스트

---
+++ src/test/java/lotto/LottoNumberTest.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import lotto.domain.LottoNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoNumberTest {
+    @DisplayName("1미만의 숫자면 예외가 발생한다.")
+    @Test
+    void underMin() {
+        assertThatThrownBy(() -> new LottoNumber(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("45초과인 숫자면 예외가 발생한다.")
+    @Test
+    void overMax() {
+        assertThatThrownBy(() -> new LottoNumber(60))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
