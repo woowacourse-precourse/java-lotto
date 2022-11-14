6e96765f1added2ed4b18154cec090aea0f85eaf
swjeong98 <tree5678@konkuk.ac.kr>
2022-11-14 23:09:53 +0900

    fix(Player) : 예외 발생 시 catch를 이용해 받은 후 에러 문구 출력 후 종료하도록 코드 수정

--- src/main/java/lotto/Player.java
+++ src/main/java/lotto/Player.java
@@ -12,12 +12,13 @@ public class Player {
     private List<Lotto> lottos;
     private int Bonus;
     private int Profit;
+    private String Error_message;
     public Player(Manager manager){
         this.manager = manager;
     }
 
     public void QuitProgram(String message){
-        System.out.println(message);
+        this.Error_message = message;
         throw new IllegalArgumentException();
     }
 
@@ -28,12 +29,12 @@ public class Player {
     }
     public void checkPurchaseMoney(String input){
         if(!manager.isNumber(input))
-            QuitProgram("[ERROR] : 금액은 숫자로만 구성된 문자열이어야 합니다.");
+            QuitProgram("[ERROR] 금액은 숫자로만 구성된 문자열이어야 합니다.");
         int money = Integer.parseInt(input);
         if(money < 1000)
-            QuitProgram("[ERROR] : 최소 1000원 이상의 금액을 입력해주세요.");
+            QuitProgram("[ERROR] 최소 1000원 이상의 금액을 입력해주세요.");
         if(money % 1000 != 0)
-            QuitProgram("[ERROR] : 금액은 1000원으로 나누어 떨어져야 합니다.");
+            QuitProgram("[ERROR] 금액은 1000원으로 나누어 떨어져야 합니다.");
         this.PurchaseMoney = money;
     }
     public void calculateTotalLotto(){
@@ -42,17 +43,17 @@ public class Player {
     public void checkWinningNumber(String input){
         String[] arr = input.split(",");
         if(arr.length != manager.LOTTO_LENGTH)
-            QuitProgram("[ERROR] : 당첨번호는 쉼표(,)를 기준으로 6개로 구분되어야 합니다.");
+            QuitProgram("[ERROR] 당첨번호는 쉼표(,)를 기준으로 6개로 구분되어야 합니다.");
         List<String> list = Arrays.asList(arr);
         if(!manager.ValidityOfNumber(list))
-            QuitProgram("[ERROR] : 당첨번호는 반드시 숫자로만 이루어져야 합니다.");
+            QuitProgram("[ERROR] 당첨번호는 반드시 숫자로만 이루어져야 합니다.");
         List<Integer> winnings = new ArrayList<>();
         for(String s : list)
             winnings.add(Integer.parseInt(s));
         if(!manager.ValidRangeOfLottoNumber(winnings))
-            QuitProgram("[ERROR] : 당첨번호는 1 ~ 45 범위에 해당하는 숫자여야 합니다.");
+            QuitProgram("[ERROR] 당첨번호는 1 ~ 45 범위에 해당하는 숫자여야 합니다.");
         if(manager.OverlapOfLottoNumber(winnings))
-            QuitProgram("[ERROR] : 6개의 당첨번호는 모두 다른 숫자여야 합니다.");
+            QuitProgram("[ERROR] 6개의 당첨번호는 모두 다른 숫자여야 합니다.");
         this.winning = winnings;
     }
 
@@ -65,14 +66,14 @@ public class Player {
 
     public void checkBonusNumber(String input){
         if(!manager.isNumber(input))
-            QuitProgram("[ERROR] : 보너스번호는 반드시 숫자로만 이루어져야 합니다.");
+            QuitProgram("[ERROR] 보너스번호는 반드시 숫자로만 이루어져야 합니다.");
         List<Integer> list = new ArrayList<>();
         int bonus = Integer.parseInt(input);
         list.add(bonus);
         if(!manager.ValidRangeOfLottoNumber(list))
-            QuitProgram("[ERROR] : 보너스번호는 1 ~ 45 범위에 해당하는 숫자여야 합니다.");
+            QuitProgram("[ERROR] 보너스번호는 1 ~ 45 범위에 해당하는 숫자여야 합니다.");
         if(winning.contains(bonus))
-            QuitProgram("[ERROR] : 보너스 번호는 입력한 당첨 번호와 중복되서는 안됩니다.");
+            QuitProgram("[ERROR] 보너스 번호는 입력한 당첨 번호와 중복되서는 안됩니다.");
         this.Bonus = bonus;
     }
     public void InputBonusNumber(){
@@ -89,14 +90,18 @@ public class Player {
     }
 
     public void LottoGamestart(){
-        InputPurchaseMoney();
-        calculateTotalLotto();
-        PurChaseLotto();
-        manager.PrintLotto(this.lottos);
-        InputWinningNumber();
-        InputBonusNumber();
-        manager.PrintGameResult(this.lottos, this.winning, this.Bonus);
-        Profit = manager.calculateTotalMoney(this.lottos, winning, Bonus);
-        manager.calculateReturnRate(this.PurchaseMoney, this.Profit);
+        try{
+            InputPurchaseMoney();
+            calculateTotalLotto();
+            PurChaseLotto();
+            manager.PrintLotto(this.lottos);
+            InputWinningNumber();
+            InputBonusNumber();
+            manager.PrintGameResult(this.lottos, this.winning, this.Bonus);
+            Profit = manager.calculateTotalMoney(this.lottos, winning, Bonus);
+            manager.calculateReturnRate(this.PurchaseMoney, this.Profit);
+        }catch(IllegalArgumentException e){
+            System.out.println(this.Error_message);
+        }
     }
 }
