10d9d2fcc031d91389d38bb88e84567b850d617f
Umin <adorecamus@gmail.com>
2022-11-14 17:43:08 +0900

    feat: 로또 숫자 예외 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -7,14 +10,20 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        notDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate(List<Integer> numbers) throws IllegalArgumentException {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void notDuplicate(List<Integer> numbers) {
+//        if () {
+//            throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자여야 합니다.");
+//        }
+    }
+
 }
