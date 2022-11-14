af91ec04e5c10637f11e8fe8d94f9c56a5291864
d0ri123 <lsh81550@gmail.com>
2022-11-14 09:21:08 +0900

    fix(lotto): NullpointerException 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,9 +7,6 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-    private static User user;
-    public static int lottoEA;
-    public static ArrayList<List<Integer>> lotteryOfUser;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -21,28 +18,4 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
-
-    public void buyer(User user){
-        this.user = user;
-    }
-
-    private void printLotto(){
-        int lottoPrice = user.getLottoPrice();
-        this.lottoEA = lottoPrice / 1000;
-        System.out.println(lottoEA + "개를 구매했습니다.");
-    }
-
-    private void printRandomNumbers(){
-        for(int ea=1; ea<lottoEA; ea++){
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
-            lotteryOfUser.add(numbers);
-            System.out.println(numbers);
-        }
-    }
-
-    public ArrayList<List<Integer>> getLotteryOfUser(){
-        return lotteryOfUser;
-    }
-
-
 }
--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,23 +1,28 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class User {
     private int lottoPrice;
     private int lottoEA;
+    private String winningNumbers;
     private List<Integer> userNumbers;
     private int bonusNumber;
+    private ArrayList<List<Integer>> lotteryOfUser;
 
-    public void buyLotto(){
-        System.out.println("구매 금액을 입력해주세요.");
+    public void enterPrice(){
+        System.out.println("구입금액을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
         this.lottoPrice = sc.nextInt();
     }
 
     public void printLottoEA(){
-        lottoEA = lottoPrice/1000;
-        if((lottoEA%=1000) != 0)
+        lottoEA = this.lottoPrice/1000;
+        if((lottoPrice%1000) != 0)
             throw new IllegalArgumentException();
         System.out.println(lottoEA + "개를 구매했습니다.");
     }
@@ -26,27 +31,44 @@ public class User {
         return this.lottoPrice;
     }
 
-    public List<Integer> userNumbers(){
-        System.out.println("당첨 번호를 입력해 주세요.");
-        Scanner sc = new Scanner(System.in);
-        String nums = sc.nextLine();
+    public void createLotto(){
+        for(int ea=0; ea<this.lottoEA; ea++){
+            System.out.println(randomNumbers());
+        }
+    }
 
+    private List<Integer> randomNumbers(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+        lotteryOfUser = new ArrayList<List<Integer>>();
+        this.lotteryOfUser.add(numbers);
+        return numbers;
+    }
+
+    public void enterWinningNumbers(){
+        System.out.println("당첨 번호를 입력해 주세요");
+        Scanner sc = new Scanner(System.in);
+        this.winningNumbers = sc.nextLine();
+        enter(winningNumbers);
+    }
+
+    public void enterBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        this.bonusNumber = sc.nextInt();
+    }
+
+    private void enter(String nums){
+        userNumbers = new ArrayList<>();
         for(String num : nums.split(",")){
             this.userNumbers.add(Integer.valueOf(num));
         }
-
-        return this.userNumbers;
     }
 
     public List<Integer> getUserNumbers(){
         return this.userNumbers;
     }
 
-    public void userBonusNumber(){
-        System.out.println("보너스 번호를 입력해 주세요.");
-        Scanner sc = new Scanner(System.in);
-        this.bonusNumber = sc.nextInt();
-    }
+
 
     public int getBonusNumber(){
         return this.bonusNumber;
