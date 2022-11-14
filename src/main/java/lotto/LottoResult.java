616077a60fc4c737ea8156b97c8e4a90b320dc7f
ensk26 <tjensk26@gmail.com>
2022-11-14 23:50:52 +0900

    refactor(LottoResult): 로또 결과 enum에 개수와 보너스, index 값 추가
    기존 메서드 수정 to LottoResult
    - if 문이 아닌 여기서 count, bonus 값에 따라 저장할 index를 반환해주는 방식을 사용하기 위해

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,16 +1,22 @@
 package lotto;
 
 public enum LottoResult {
-    FIRSTPLACE("6개 번호 일치", "2,000,000,000"),
-    SECONDPLACE("5개 번호 + 보너스 번호 일치", "30,000,000"),
-    THIRDPLACE("5개 번호 일치", "1,500,000"),
-    FOURTHPLACE("4개 번호 일치", "50,000"),
-    FIFTHPLACE("3개 번호 일치", "5,000");
+    FIRSTPLACE(6, false, 0, "6개 번호 일치", "2,000,000,000"),
+    SECONDPLACE(5, true, 1, "5개 번호 + 보너스 번호 일치", "30,000,000"),
+    THIRDPLACE(5, false, 2, "5개 번호 일치", "1,500,000"),
+    FOURTHPLACE(4, false, 3, "4개 번호 일치", "50,000"),
+    FIFTHPLACE(3, false, 4, "3개 번호 일치", "5,000");
 
+    private final int count;
+    private final boolean bonus;
+    private final int index;
     private final String detail;
     private final String money;
 
-    LottoResult(String detail, String money) {
+    LottoResult(int count, boolean bonus, int index, String detail, String money) {
+        this.count = count;
+        this.bonus = bonus;
+        this.index = index;
         this.detail = detail;
         this.money = money;
     }
