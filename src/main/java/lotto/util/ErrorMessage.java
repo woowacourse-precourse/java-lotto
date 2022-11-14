f8b1f52e19c291976e763df7cd60b568551e6daf
rt3310 <btac3310@gmail.com>
2022-11-15 01:32:10 +0900

    style: 에러 메세지 수정

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -3,11 +3,11 @@ package lotto.util;
 public enum ErrorMessage {
     INVALID_NUMBER_TYPE("0 이상의 숫자만 입력 가능합니다"),
     INVALID_UNIT_OF_MONEY("1000원 단위로만 입력 가능합니다"),
-    INVALID_LOTTO_NUMBER_COUNT("로또 번호는 6개까지 입력 가능합니다"),
+    INVALID_LOTTO_NUMBER_COUNT("로또 번호 개수는 6개이어야 합니다"),
     INVALID_LOTTO_NUMBER_RANGE("로또 번호는 1~45사이의 숫자만 입력 가능합니다"),
     MONEY_IS_ZERO("구입 금액이 0원이므로 게임을 종료합니다"),
     INVALID_NUMBER_OF_LOTTO("로또 개수는 1개 이상이어야 합니다"),
-    DUPLICATION_BONUS_NUMBER("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    DUPLICATION_BONUS_NUMBER("보너스 번호는 당첨 번호와 중복될 수 없습니다");
 
     private final String message;
 
