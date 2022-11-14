984785f51d57d5f4dcf800d3888dc4d22681de16
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-14 17:46:33 +0900

    Style : "정렬 main문"
    main문을 정렬하여 보기 더 좋게 하였습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,12 +2,11 @@ package lotto;
 
 public class Application {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         I_O_System IO = new I_O_System();
         // TODO: 프로그램 구현
         IO.Enter_Price();
         IO.Lottey_System();
-
     }
 
 }
