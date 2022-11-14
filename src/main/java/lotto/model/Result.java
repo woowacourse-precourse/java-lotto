23b0f6e8a6c6953d9b0bd8cc26787d31ceafaeff
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-14 14:21:50 +0900

    style(Result): Enum을 Result 클래스 내부에 선언

--- src/main/java/lotto/model/Result.java
+++ src/main/java/lotto/model/Result.java
@@ -1,8 +1,24 @@
 package lotto.model;
 
-import lotto.price;
-
 import java.util.List;
+
+enum price {
+    THREE(5_000),
+    FOUR(50_000),
+    FIVE(1_500_000),
+    FIVE_BONUS(3_000_000),
+    SIX(2_000_000_000);
+
+    private int priceMoney;
+
+    price(int priceMoney) {
+        this.priceMoney = priceMoney;
+    }
+
+    public int getPriceMoney() {
+        return priceMoney;
+    }
+}
 
 public class Result {
     private int[] resultArray;
@@ -60,5 +76,9 @@ public class Result {
 
     public int getPriceSum() {
         return priceSum;
+    }
+
+    public int[] getResultArray() {
+        return resultArray;
     }
 }
