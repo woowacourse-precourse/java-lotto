e181e926f51b8fc1e33977abc89aeba92a3f12d7
sora5421 <21800804@handong.edu>
2022-11-15 03:29:52 +0900

    feat (printResult) : enum 사용하는 것 없애고 입력값 받아서 사용하는 것으로 구현을 바꿈

--- src/main/java/lotto/view/UserInteraction.java
+++ src/main/java/lotto/view/UserInteraction.java
@@ -1,9 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.datamodel.Rank;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class UserInteraction {
@@ -47,23 +45,16 @@ public class UserInteraction {
         return Integer.parseInt(input);
     }
 
-    public void printResult(List<Integer> prize, int lottoCount) {
-        int frequency;
-        float totalPrize = 0;
+    public void printResult(int lottoCount, float totalPrize, List<String> moneyInfo, List<Integer> winingLotto) {
         System.out.println("\n 당첨 통계");
         System.out.println("---");
 
-        for (Rank rank : Rank.values()) {
-            if(rank != Rank.NONE){
-                frequency = Collections.frequency(prize, rank.getAmount());
-                totalPrize += ((float)rank.getAmount() / 1000 * frequency);
-                System.out.print(rank.getInformation());
-                System.out.print(" - ");
-                System.out.println(frequency+"개");
-            }
+        for(int prizeCount = 0; prizeCount < 5; prizeCount++){
+            System.out.print(moneyInfo.get(prizeCount));
+            System.out.print(" - ");
+            System.out.println(winingLotto.get(prizeCount)+"개");
         }
-
         System.out.println("총 수익률은 " + String.format("%.1f", totalPrize/lottoCount * 100)+"%입니다.");
     }
-    
+
 }
