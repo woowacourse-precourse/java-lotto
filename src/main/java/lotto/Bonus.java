26537103582ebe17f19c4325d30ccd66bcea1246
Jihoon9809 <dkdb521@naver.com>
2022-11-14 23:56:23 +0900

    보너스 입력 처리

--- src/main/java/lotto/Bonus.java
+++ src/main/java/lotto/Bonus.java
@@ -1,11 +1,20 @@
 package lotto;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Bonus {
-    private final int bouns;
+    private final int bonus;
 
-    public Bonus(int userBouns){
+    public Bonus(int userBonus){
 
-        this.bouns = userBonus;
+        this.bonus = userBonus;
+        checkOverRange(bonus);
+    }
+
+    private void checkOverRange(int bonus){
+            if(bonus > 46) throw new IllegalArgumentException("[ERROR]");
     }
 
 }
