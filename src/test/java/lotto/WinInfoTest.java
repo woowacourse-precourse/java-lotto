815e40d2cbf86e28ff6d92f57343483bbb8753f1
osm <osm0822@naver.com>
2022-11-14 20:23:14 +0900

    test(WinInfo) : get함수 테스트 추가

--- src/test/java/lotto/WinInfoTest.java
+++ src/test/java/lotto/WinInfoTest.java
@@ -20,7 +20,6 @@ class WinInfoTest {
         assertEquals(trg.match(matchedCnt, bonusMatchedCnt), result);
     }
 
-
     private static List<Arguments> matchTestProvider() {
         return List.of(
                 Arguments.of(6, 0, WinInfo.WIN1, true),
@@ -30,6 +29,28 @@ class WinInfoTest {
                 Arguments.of(3, 0, WinInfo.WIN5, true),
                 Arguments.of(2, 0, WinInfo.NO_WIN, false),
                 Arguments.of(-1, -1, WinInfo.NO_WIN, true)
+        );
+    }
+
+    @DisplayName("당첨 번호 일치 수와 보너스 번호 일치 수에 맞는 WinInfo를 반환하는지 테스트")
+    @ParameterizedTest
+    @MethodSource("getTestProvider")
+    void getTest(int matchedCnt, int bonusMatchedCnt, WinInfo trg) {
+        assertEquals(WinInfo.get(matchedCnt,bonusMatchedCnt), trg);
+    }
+
+    private static List<Arguments> getTestProvider() {
+        return List.of(
+                Arguments.of(6, 0, WinInfo.WIN1),
+                Arguments.of(5, 1, WinInfo.WIN2),
+                Arguments.of(5, 0, WinInfo.WIN3),
+                Arguments.of(4, 0, WinInfo.WIN4),
+                Arguments.of(3, 0, WinInfo.WIN5),
+                Arguments.of(3, 2, WinInfo.WIN5),
+                Arguments.of(2, 0, WinInfo.NO_WIN),
+                Arguments.of(2, 1, WinInfo.NO_WIN),
+                Arguments.of(2, 4, WinInfo.NO_WIN),
+                Arguments.of(1, 0, WinInfo.NO_WIN)
         );
     }
 
