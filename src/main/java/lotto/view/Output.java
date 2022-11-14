b30d59fad66e69a4840743d76f365fd7aa0321eb
ohhonggi <5ghdrl@naver.com>
2022-11-15 06:31:00 +0900

    refactor($Output): Add output statement

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Output {
 
     public static void printGeneratedLotto(List<Lotto> lottoList) {
+        System.out.println(lottoList.size()+"개를 구매했습니다.");
         lottoList.forEach( lotto -> System.out.println(lotto.toString()));
     }
 
