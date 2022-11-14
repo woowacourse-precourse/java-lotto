4ae2dfbd2b112fafc463d1ff58dc07babc681cbb
Kid-Chang <pch3310@gmail.com>
2022-11-15 04:45:17 +0900

    feat(Input): 돈 입력에 대한 예외처리 적용

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -5,13 +5,15 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.utils.CheckInput;
 
 public class Input {
-
     public static int getYourMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         // 예외처리 필요. 1000 단위로 나눠떨어지는지, 음수는 아닌지
-        int receivedMoney = Integer.parseInt(Console.readLine());
+        String inputValue = Console.readLine();
+        CheckInput.checkInputMoney(inputValue);
+        int receivedMoney = Integer.parseInt(inputValue);
         System.out.println(receivedMoney / 1000 + "개를 구매했습니다.");
         return receivedMoney;
     }
@@ -31,4 +33,5 @@ public class Input {
         // 예외처리 필요 1~45가 맞는지
         return Integer.parseInt(Console.readLine());
     }
+
 }
