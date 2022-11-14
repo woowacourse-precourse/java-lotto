99b7f12b148cd7065a3ea987c65a1974ad7592c4
yujaemin <woawoa0513@gmail.com>
2022-11-14 23:08:44 +0900

    feat: 필드 추가
    일치하는 번호의 수를 의미하는 matchingNumberCount 필드 추가

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -1,24 +1,30 @@
 package lotto.domain;
 
 public enum Ranking {
-    FIRST(1,"6개 일치", "2,000,000,000원"),
-    SECOND(2, "5개 일치, 보너스 볼 일치", "30,000,000원"),
-    THIRD(3, "5개 일치", "1,500,000원"),
-    FOURTH(4, "4개 일치", "50,000원"),
-    FIFTH(5, "3개 일치", "5,000원");
+    FIRST(1, 6,"6개 일치", "2,000,000,000원"),
+    SECOND(2, 5, "5개 일치, 보너스 볼 일치", "30,000,000원"),
+    THIRD(3, 5, "5개 일치", "1,500,000원"),
+    FOURTH(4, 4, "4개 일치", "50,000원"),
+    FIFTH(5, 3, "3개 일치", "5,000원");
 
     private final int place;
+    private final int matchingNumberCount;
     private final String condition;
     private final String reward;
 
-    Ranking(int place, String condition, String reward) {
+    Ranking(int place, int matchingNumberCount, String condition, String reward) {
         this.place = place;
+        this.matchingNumberCount = matchingNumberCount;
         this.condition = condition;
         this.reward = reward;
     }
 
     public int getPlace() {
         return place;
+    }
+
+    public int getMatchingNumberCount() {
+        return matchingNumberCount;
     }
 
     public String getCondition() {
