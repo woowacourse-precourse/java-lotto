130047c8280d599af856c0fb7b469b44c58c2cf2
beombu <sunil1369@bible.ac.kr>
2022-11-15 06:39:18 +0900

    Feat(Output) : 로또 갯수 메시지 출력 기능

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -2,9 +2,13 @@ package lotto.view;
 
 public class Output {
     public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
-
+    public static final String COUNT_LOTTO_MESSAGE = "%d개 구매했습니다.\n";
 
     public static void printInputMoney(){
         System.out.println(INPUT_MONEY_MESSAGE);
+    }
+
+    public static void printCountLotto(int countLotto){
+        System.out.printf(COUNT_LOTTO_MESSAGE, countLotto);
     }
 }
