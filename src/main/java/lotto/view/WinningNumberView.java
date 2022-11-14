efa3a31b9c38890cb410d0573e893a804a78e7b4
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-15 05:56:45 +0900

    feat: 사용자로 부터 로또 당첨 번호를 입력받는 기능 추가

--- src/main/java/lotto/view/WinningNumberView.java
+++ src/main/java/lotto/view/WinningNumberView.java
@@ -1,2 +1,16 @@
-package lotto.view;public class WinningNumberView {
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class WinningNumberView {
+
+    public List<Integer> inputWinningNumber() {
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningNumber = Console.readLine();
+        
+        return null;
+    }
 }
