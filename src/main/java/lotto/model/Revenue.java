4925e7151ed39f012affe7d9f6790b58cd4b83ea
gomljo <gomljo0307@naver.com>
2022-11-15 02:07:27 +0900

    feat(Revenue.java): 집계 정보를 등수 이름으로 초기화하는 기능 구현

--- src/main/java/lotto/model/Revenue.java
+++ src/main/java/lotto/model/Revenue.java
@@ -1,17 +1,29 @@
 package lotto.model;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Revenue {
-
+    private static final int ZERO_COUNT=0;
+    private static final int ZERO_REVENUE=0;
     private final List<List<Integer>> matches;
+    private final HashMap<String, Integer> revenueStatics=new HashMap<>();
+
     private int revenue;
 
     public Revenue(Discriminator discriminator){
         matches=discriminator.getMatches();
+        revenue=ZERO_REVENUE;
     }
 
     public int getRevenue(){
         return revenue;
+    }
+
+    private void initializeRevenueStatics(){
+        List<String> rankNames=Rank.getRankNames();
+        for(String rankName: rankNames){
+            revenueStatics.put(rankName, ZERO_COUNT);
+        }
     }
 }
