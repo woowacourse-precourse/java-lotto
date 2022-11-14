69035c5dbd5f2fc1f6f639e3607c302dc17499f6
chaos0103 <lyt1228@naver.com>
2022-11-14 16:28:47 +0900

    fix(Lotto): UnsupportedOperationException 해결

--- src/main/java/lotto/domain/impl/NumberGeneratorImpl.java
+++ src/main/java/lotto/domain/impl/NumberGeneratorImpl.java
@@ -3,6 +3,7 @@ package lotto.domain.impl;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.NumberGenerator;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,7 +14,7 @@ public class NumberGeneratorImpl implements NumberGenerator {
     @Override
     public List<Integer> createRandomNumbers() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(NUMBER_MIN_RANGE, NUMBER_MAX_RANGE, LOTTO_NUMBERS_COUNT);
-        Collections.sort(numbers);
+        Collections.sort(new ArrayList<>(numbers));
         return numbers;
     }
 }
