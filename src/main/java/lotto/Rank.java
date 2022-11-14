fabfd740f6dd390c43fae6cd5b23b65ca3a7ea00
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-14 20:30:09 +0900

    refactoring : Rank 상수 순서 변경

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -4,8 +4,8 @@ public enum Rank {
     THIRD(3, 5_000, 0),
     FORTH(4, 50_000, 0),
     FIFTH(5, 1_500_000, 0),
-    FIFTH_BONUS(5, 30_000_000, 0),
-    SIXTH(6, 2_000_000_000, 0);
+    SIXTH(6, 2_000_000_000, 0),
+    FIFTH_BONUS(5, 30_000_000, 0);
 
     private int sameNumber;
     private int prizeMoney;
