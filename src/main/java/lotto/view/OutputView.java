85dd79953c4321a2e4642a84991466f90fb72137
im-shung <seoyoung1113@gmail.com>
2022-11-15 02:36:57 +0900

    feat: :sparkles: 출력문들 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,6 +9,19 @@ public class OutputView {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
+    public static void askNumbers(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void askBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public static void printWinningStatisticsMessage(){
+        System.out.println("당첨 통계");
+        System.out.println("--");
+    }
+
     public static void printLottos(List<Lotto> lottos) {
         lottos.forEach(System.out::println);
     }
