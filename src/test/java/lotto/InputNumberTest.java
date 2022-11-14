8faf4dd0d985a0d428813f2726921c532015371b
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-15 01:38:25 +0900

    test(InputNumberTest) : test for appropriate length at winning number list

--- src/test/java/lotto/InputNumberTest.java
+++ src/test/java/lotto/InputNumberTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import controller.Calculate;
 import controller.InputNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class InputNumberTest {
     @DisplayName("당첨번호가 제대로 파싱되는지 확인")
@@ -19,5 +21,14 @@ public class InputNumberTest {
     @Test
     void 보너스번호_파싱_확인_테스트() {
         assertThat(InputNumber.parseWinningNumber("7")).isEqualTo(List.of(7));
+    }
+
+    @DisplayName("당첨번호가 6개가 아닐 때 예외 발생하는지 확인")
+    @Test
+    void 당첨번호_예외_테스트() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+                    InputNumber.parseWinningNumber("1,2,3,4,5,6,7");
+                })
+                .withMessage("[ERROR] 당첨번호는 6개여야 합니다.");
     }
 }
