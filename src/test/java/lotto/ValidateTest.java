1e2e4068345caf583a6503bae0c89cd492b15e7a
Your Name <dbsdbxkrzz@naver.com>
2022-11-15 01:02:51 +0900

    test(lotto): 구매 금액 유효성 검증 테스트 코드 추가
    구매 금액이 올바른 입력인지 확인하는 각각의 메서드에 대해 테스트 코드를 작성합니다
    의도한대로 동작하는지 확인합니다

--- src/test/java/lotto/ValidateTest.java
+++ src/test/java/lotto/ValidateTest.java
@@ -27,4 +27,46 @@ class ValidateTest {
 		assertThat(Validate.isOutOfRange(List.of(46,1,2,3,4,5))).isEqualTo(true);
 		assertThat(Validate.isOutOfRange(List.of(1,2,3,4,5,45))).isEqualTo(false);
 	}
+
+	@Test
+	void 금액이_숫자가_아닐경우(){
+		String invalidMoney = "123ab";
+		String validMoney = "4000";
+		assertThat(Validate.isNotNumeric(invalidMoney)).isEqualTo(true);
+		assertThat(Validate.isNotNumeric(validMoney)).isEqualTo(false);
+	}
+
+	@Test
+	void 금액이_부족한_경우(){
+		String invalidMoney = "999";
+		String validMoney = "1000";
+		assertThat(Validate.isNotEnough(invalidMoney)).isEqualTo(true);
+		assertThat(Validate.isNotEnough(validMoney)).isEqualTo(false);
+	}
+
+	@Test
+	void 천원단위로_나누어_떨어지지_않을_때(){
+		String invalidMoney = "3210";
+		String validMoney = "5000";
+		assertThat(Validate.isNotDivisible(invalidMoney)).isEqualTo(true);
+		assertThat(Validate.isNotDivisible(validMoney)).isEqualTo(false);
+	}
+
+	@Test
+	void 구매_금액_테스트(){
+		String notNumeric = "123ab";
+		assertThatThrownBy(() -> Validate.checkValidMoney(notNumeric))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(Message.ERROR.getMessage() + Message.NOT_NUMERIC.getMessage());
+
+		String notEnoughMoney = "486";
+		assertThatThrownBy(() -> Validate.checkValidMoney(notEnoughMoney))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(Message.ERROR.getMessage() + Message.NOT_ENOUGH_MONEY.getMessage());
+
+		String notDivisible = "3589";
+		assertThatThrownBy(() -> Validate.checkValidMoney(notDivisible))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(Message.ERROR.getMessage() + Message.NOT_DIVISIBLE_MONEY.getMessage());
+	}
 }
