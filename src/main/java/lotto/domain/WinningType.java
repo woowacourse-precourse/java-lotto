5347fe68896feef8624cdb6bd0304d418cd6e0d3
kokomong2 <ko9603@naver.com>
2022-11-14 23:52:26 +0900

    feat(lotto) : WinningType enum 클래스 추가
    matchingNumber에 맞는 문자열 출력하는 기능 추가

---
+++ src/main/java/lotto/domain/WinningType.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum WinningType {
+
+    MATCH3(3, "3개 일치 (5,000원) - "),
+    MATCH4(4,"4개 일치 (50,000원) - "),
+    MATCH5(5,"5개 일치 (1,500,000원) - "),
+    MATCH5_BONUS(6, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    MATCH6(7,"6개 일치 (2,000,000,000원) - ");
+
+    private final int matchNumber;
+    private final String winningMessage;
+
+    WinningType(int matchNumber, String winningMessage){
+        this.matchNumber = matchNumber;
+        this.winningMessage = winningMessage;
+    }
+
+    public static WinningType findByNumber(int matchNumber){
+        return Arrays.stream(WinningType.values())
+                .filter(winningType -> winningType.matchNumber==matchNumber)
+                .findAny()
+                .orElseThrow(()->new IllegalArgumentException("[ERROR] matchNumber 잘못입력했음."));
+    }
+
+    public String printWinningMessage(int count){
+        return this.winningMessage+ count+"개";
+    }
+
+
+}
