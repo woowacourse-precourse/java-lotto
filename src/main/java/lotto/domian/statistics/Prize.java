80837b48011851424a1cffb2c89755f82828cc51
DongwookKim0823 <dwkim0823@naver.com>
2022-11-15 04:12:39 +0900

    refactor(lotto): enum 필드 순서 변경

--- src/main/java/lotto/domian/statistics/Prize.java
+++ src/main/java/lotto/domian/statistics/Prize.java
@@ -6,11 +6,11 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum Prize {
-    FIRST(1, 2_000_000_000, "6개 일치 (2,000,000,000원) - "),
-    SECOND(2, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    THIRD(3, 1_500_000, "5개 일치 (1,500,000원) - "),
+    FIFTH(5, 5_000, "3개 일치 (5,000원) - "),
     FOURTH(4, 50_000, "4개 일치 (50,000원) - "),
-    FIFTH(5, 5_000, "3개 일치 (5,000원) - ");
+    THIRD(3, 1_500_000, "5개 일치 (1,500,000원) - "),
+    SECOND(2, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    FIRST(1, 2_000_000_000, "6개 일치 (2,000,000,000원) - ");
 
     private final int gradeNum;
     private final int price;
