fd48f783867e110afd441366283d113fafa045e4
desfox <desfox99@naver.com>
2022-11-15 01:47:04 +0900

    feat(InputView): 문자열 쉼표 기준으로 분리해 정수 리스트로 반환하는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,6 +3,13 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.validation.PurchasePriceInputValidation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class InputView {
     OutputView outputView = new OutputView();
 
@@ -16,4 +23,14 @@ public class InputView {
         int purchasePrice = Integer.parseInt(inputPurchasePrice);
         return purchasePrice;
     }
+
+    public static List<Integer> splitStringByCommaAndParseInt(String s) {
+        String blankRemoved = s.replaceAll(" ", "");
+        String[] splitByComma = blankRemoved.split(",");
+
+        Stream<String> stream = Arrays.stream(splitByComma);
+        IntStream parsedToInt = stream.mapToInt(Integer::parseInt);
+        return parsedToInt.boxed().collect(Collectors.toList());
+    }
+
 }
