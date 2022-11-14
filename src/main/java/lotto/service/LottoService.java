3748621248228bd98e27a91e540a11353dcef8ff
Lemon Soju <yckk980320@naver.com>
2022-11-15 00:40:24 +0900

    feat(InputView): lotto number input function
    After receiving the lotto number,
    create a lotto object and return it.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,15 +1,34 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.view.InputView.inputLottoNumber;
 import static lotto.view.InputView.inputMoney;
 
 public class LottoService {
     public static void lottoService() {
         int money;
+        Lotto inputLottoNumber;
         try {
             money = inputMoney();
+            inputLottoNumber = inputLottoNumber();
+
         } catch (IllegalArgumentException e) {
             System.out.println(e);
             return;
         }
+    }
+
+    public static List<Integer> stringToIntList(String input) {
+        String[] splitInput = input.split(",");
+        List<Integer> output = new ArrayList<>();
+
+        for (String e : splitInput) {
+            output.add(Integer.parseInt(e));
+        }
+        return output;
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,12 +1,22 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
 
 import static lotto.exception.InputException.inputMoneyException;
+import static lotto.service.LottoService.stringToIntList;
 
 public class InputView {
     public static int inputMoney() throws IllegalArgumentException {
         System.out.print("로또 구입 금액을 입력하시오: ");
         return inputMoneyException(Console.readLine());
     }
+
+    public static Lotto inputLottoNumber() throws IllegalArgumentException {
+        System.out.print("당첨 번호를 입력하시오 : ");
+        Lotto lotto;
+        lotto = new Lotto(stringToIntList(Console.readLine()));
+        return lotto;
+    }
 }
+
