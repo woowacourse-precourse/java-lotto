9823389c34698fbafbcef301026fbfde62edbdd0
yun <yshyeon102@gmail.com>
2022-11-14 10:53:07 +0900

    feat(LottoView): 메세지 출력 메서드 정의

--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -1,9 +1,13 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
 import lotto.domain.WinningType;
 
+import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class LottoView {
@@ -18,4 +22,41 @@ public class LottoView {
     public static final List<String> PRIZE_DESC = Arrays.stream(WinningType.values())
             .map(WinningType::getDesc)
             .collect(Collectors.toList());
+
+    public static void printWinningLottoRequest() {
+        System.out.println(BLANK_LINE + REQUEST_WINNING_LOTTO_MESSAGE);
+    }
+
+    public static void printBonusNumberRequest() {
+        System.out.println(BLANK_LINE + REQUEST_BONUS_NUMBER_MESSAGE);
+    }
+
+    public static void printBuyRequest() {
+        System.out.println(BUY_REQUEST_MESSAGE);
+    }
+
+    public static void printBuyNumber(int count) {
+        String message = MessageFormat.format(BUY_NUMBER_MESSAGE, count);
+        System.out.println(BLANK_LINE + message);
+    }
+
+    public static void printLottoNumbers(Lottos lottos) {
+        for (Lotto lotto : lottos.getLottos()) {
+            System.out.println(lotto);
+        }
+    }
+
+    public static void printWinningStatistics(List<Integer> result, double returnRate) {
+        System.out.println(STATISTICS_INFO_MESSAGE);
+        printResultOfWin(result);
+        System.out.println(MessageFormat.format(RATE_OF_RETURN_MESSAGE, returnRate));
+    }
+
+    private static void printResultOfWin(List<Integer> result) {
+        for (int pride = 5; pride > 0; pride--) {
+            int prideIndex = 5 - pride;
+            String message = MessageFormat.format(PRIZE_DESC.get(prideIndex), result.get(prideIndex));
+            System.out.println(message);
+        }
+    }
 }
