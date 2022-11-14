f7f990c2157bab3e64ffac6c52e6a031d6fc38bd
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-14 23:20:57 +0900

    test(lotto) : 수익률 계산 테스트 추가

---
+++ src/test/java/lotto/domain/ProfitTest.java
@@ -0,0 +1,50 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class ProfitTest {
+    Profit p;
+    @BeforeEach
+    void setUp() {
+        p = new Profit();
+    }
+
+    @DisplayName("수익률 계산 케이스1")
+    @Test
+    void case1() {
+        List<Integer> winningPrices = List.of(5000, 5000, 5000, 0, 0);
+
+        assertThat(p.calProfitRate(winningPrices, BuyLotto.LOTTO_PRICE,2)).isEqualTo(300.0);
+    }
+
+    @DisplayName("수익률 계산 케이스2")
+    @Test
+    void case2() {
+        List<Integer> winningPrices = List.of(5000, 0, 0, 0, 0, 0, 0 ,0);
+
+        assertThat(p.calProfitRate(winningPrices, BuyLotto.LOTTO_PRICE,2)).isEqualTo(62.5);
+    }
+
+
+    @DisplayName("수익률 계산 케이스3")
+    @Test
+    void case3() {
+        List<Integer> winningPrices = List.of(0, 0, 0, 0, 2000000000);
+
+        assertThat(p.calProfitRate(winningPrices, BuyLotto.LOTTO_PRICE,2)).isEqualTo(40000000.0);
+    }
+
+    @DisplayName("수익률 계산 케이스4")
+    @Test
+    void case4() {
+        List<Integer> winningPrices = List.of(0, 0, 0, 30000000, 2000000000);
+
+        assertThat(p.calProfitRate(winningPrices, BuyLotto.LOTTO_PRICE,2)).isEqualTo(40600000.0);
+    }
+}
