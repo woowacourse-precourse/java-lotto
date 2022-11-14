59055e054621a81acfe7c14f48f7fb6a514a5229
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-15 04:11:54 +0900

    fix(WinningNumbers): 로또 당첨 번호/보너스 번호 검증 로직 수정
    보너스 번호의 범위를 검증하지 않던 문제 수정

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -1,16 +1,21 @@
 package lotto;
 
-import java.util.List;
-
 public class WinningNumbers {
     final Lotto winningNumbers;
     final int bonusNumber;
 
-    public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+    public WinningNumbers(Lotto winningNumbers, int bonusNumber) {
+        validate(winningNumbers, bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(Lotto winningNumbers, int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException();
+        }
         if (winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException();
         }
-        this.winningNumbers = new Lotto(winningNumbers);
-        this.bonusNumber = bonusNumber;
     }
 }
