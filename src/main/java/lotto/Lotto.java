8c58c8a8754edf353c7e2177d681dd04f6105f1e
lms0806 <godmlzkf1@naver.com>
2022-11-14 22:00:29 +0900

    test:범위 초과 테스트 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -22,6 +22,12 @@ public class Lotto {
 		if (numbers.size() != numbers.stream().distinct().count()) {
 			throw new IllegalArgumentException("[ERROR]");
 		}
+		
+		for(int number : numbers) {
+			if(number < 1 || number > 45) {
+				throw new IllegalArgumentException("[ERROR]");
+			}
+		}
 	}
 
 	// TODO: 추가 기능 구현
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -30,6 +30,14 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+    
+    @DisplayName("로또 번호에 범위를 벗어난 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByNotRangeNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(0, 46, 1, 2, 3, 4)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     // 아래에 추가 테스트 작성 가능
 }
