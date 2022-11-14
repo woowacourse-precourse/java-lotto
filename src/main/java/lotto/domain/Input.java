50ee14a4daac2354be2837292d2f9f5867ab113d
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-15 01:59:17 +0900

    refactor: 구입 금액 클래스 변수화

--- src/main/java/lotto/domain/Input.java
+++ src/main/java/lotto/domain/Input.java
@@ -6,11 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Input {
+    public int pay = 0;
     public List<Integer> numbers = new ArrayList<>();
     public int bonus = 0;
     public int payForLotto() {
         System.out.println("구입금액을 입력해 주세요.");
-        int pay = Integer.valueOf(Console.readLine())/1000;
+        pay = Integer.valueOf(Console.readLine())/1000;
         return pay;
     }
     public List<Integer> winningNumber () {
