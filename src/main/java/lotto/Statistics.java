49ae78da364f263ad9aeb9f84fee69019c49f24f
Konahyeon <koo@inha.edu>
2022-11-15 02:07:09 +0900

    refactor: Statistics 클래스로 분리

---
+++ src/main/java/lotto/Statistics.java
@@ -0,0 +1,82 @@
+package lotto;
+
+import lotto.enums.StatEnum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Statistics {
+    public static boolean validBonus(List<Integer> tempLottoNumbers) {
+        if (tempLottoNumbers.contains(Game.BONUS)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static int correctCount(List<Integer> tempLottoNumbers) {
+        int count = 0;
+        List<Integer> winningNumbers = Game.winningLotto.getNumbers(); //당첨 로또 번호
+
+        for (int numberIdx = 0; numberIdx < StatEnum.LOTTO_NUMBER.getNumber(); numberIdx++) {
+            if (winningNumbers.contains(tempLottoNumbers.get(numberIdx))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void recordStat() {
+        int count = 0;
+        int element;
+
+        for (int listIdx = 0; listIdx < Game.getLottoList().size(); listIdx++) {
+            List<Integer> tempLottoNumbers = Game.getLottoList().get(listIdx).getNumbers(); //해당 lotto 클래스의 numbers
+            count = correctCount(tempLottoNumbers);
+
+            if (count == StatEnum.THREE_CORRECT.getNumber()) {
+                element = Game.record.get(StatEnum.THREE_CORRECT_INDEX.getNumber());
+                Game.record.set(StatEnum.THREE_CORRECT_INDEX.getNumber(), ++element);
+            }
+            else if (count == StatEnum.FOUR_CORRECT.getNumber()) {
+                element = Game.record.get(StatEnum.FOUR_CORRECT_INDEX.getNumber());
+                Game.record.set(StatEnum.FOUR_CORRECT_INDEX.getNumber(), ++element);
+            }
+            else if (count == StatEnum.FIVE_CORRECT.getNumber() && !validBonus(tempLottoNumbers)) { //보너스x
+                element = Game.record.get(StatEnum.FIVE_CORRECT_INDEX.getNumber());
+                Game.record.set(StatEnum.FIVE_CORRECT_INDEX.getNumber(), ++element);
+            }
+            else if (count == StatEnum.FIVE_CORRECT.getNumber() && validBonus(tempLottoNumbers)) { //보너스o
+                element = Game.record.get(StatEnum.FIVE_BONUS_CORRECT_INDEX.getNumber());
+                Game.record.set(StatEnum.FIVE_BONUS_CORRECT_INDEX.getNumber(), ++element);
+            }
+            else if (count == StatEnum.SIX_CORRECT.getNumber()) {
+                element = Game.record.get(StatEnum.SIX_CORRECT_INDEX.getNumber());
+                Game.record.set(StatEnum.SIX_CORRECT_INDEX.getNumber(), ++element);
+            }
+        }
+    }
+
+    public static void printStat() {
+        System.out.println(StatEnum.INFO.getMessage());
+        System.out.println(StatEnum.THREE_INFO.getMessage() + Game.record.get(0) + StatEnum.UNIT.getMessage());
+        System.out.println(StatEnum.FOUR_INFO.getMessage() + Game.record.get(1) + StatEnum.UNIT.getMessage());
+        System.out.println(StatEnum.FIVE_INFO.getMessage() + Game.record.get(2) + StatEnum.UNIT.getMessage());
+        System.out.println(StatEnum.FIVE_BONUS_INFO.getMessage() + Game.record.get(3) + StatEnum.UNIT.getMessage());
+        System.out.println(StatEnum.SIX_INFO.getMessage() + Game.record.get(4) + StatEnum.UNIT.getMessage());
+    }
+
+    public static void printYield() {
+        double earnMoney = 0;
+
+        List<Integer> money = new ArrayList<>(List.of(5000, 50000, 1500000, 30000000, 2000000000));
+
+        for (int idx = 0; idx < money.size(); idx++) {
+            earnMoney += (money.get(idx) * Game.record.get(idx));
+        }
+        double pay = Double.parseDouble(Game.payMoney);
+        double yield = earnMoney/pay * 100.0;
+
+        System.out.println("총 수익률은 " + String.format("%.1f", yield) + "%입니다.");
+    }
+}
