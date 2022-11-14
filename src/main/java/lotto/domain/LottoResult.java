615fce8a6df8303f6a02ca6916d23a47b6775058
i960107 <i960107@naver.com>
2022-11-14 22:43:03 +0900

    feat(LottoResultCalculator): 전체 로또에 대해서 수익률을 계산하는 기능 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -8,9 +8,9 @@ public class LottoResult {
     private final Long totalPrize;
     private final Double earningsRatio;
 
-    public LottoResult(int count, Map<Lotto, LottoRank> ranks) {
+    public LottoResult(Map<Lotto, LottoRank> ranks) {
         this.ranks = ranks;
-        this.totalCost = count * (long) (LottoRule.TICKET_COST);
+        this.totalCost = ranks.size() * (long) (LottoRule.TICKET_COST);
         this.totalPrize = LottoResultCalculator.calculatePrize(ranks);
         this.earningsRatio = LottoResultCalculator.calculateEarningsRatio(totalCost, totalPrize);
     }
--- src/main/java/lotto/domain/LottoResultCalculator.java
+++ src/main/java/lotto/domain/LottoResultCalculator.java
@@ -1,23 +1,33 @@
 package lotto.domain;
 
+import static java.lang.Math.round;
+
 import java.util.Map;
 import java.util.Map.Entry;
 
 public class LottoResultCalculator {
 
-    public static Long calculatePrize(Map<LottoRank, Integer> ranks) {
+    private LottoResultCalculator() {
+
+    }
+
+
+    public static Long calculatePrize(Map<Lotto, LottoRank> ranks) {
         Long totalPrize = 0L;
-        for (Entry<LottoRank, Integer> entry : ranks.entrySet()) {
-            totalPrize += ((long) entry.getKey().getPrize() * entry.getValue());
+        for (Entry<Lotto, LottoRank> rank : ranks.entrySet()) {
+            if (rank.getValue() == null) {
+                continue;
+            }
+            totalPrize += rank.getValue().getPrize();
         }
         return totalPrize;
     }
 
-    public static Double calculateEarningsRatio(Long totalCost, Long prize) {
+    public static Double calculateEarningsRatio(Long totalCost, Long totalPrize) {
         if (totalCost == 0) {
             return 0.0d;
         }
-        return ((double) prize / totalCost) * 100;
+        return round(((double) totalPrize / totalCost) * 100 * 10) / 10d;
     }
 
 }
