a9e84abc63733e9e4865274769ea1c6b5ca87c0e
juy4556 <kws05050@naver.com>
2022-11-15 00:51:26 +0900

    feat: 사용자 객체 내 당첨 번호 입력 메소드 생성

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,9 +1,13 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class User {
     private LottoGame lottoGame;
+    private Lotto winningLotto;
     private int buyPrice;
 
     public void startLottoGame() {
@@ -16,8 +20,24 @@ public class User {
         lottoGame.createLotto(buyPrice/1000);
         lottoGame.printLotteries(buyPrice/1000);
         System.out.println();
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        inputWinning();
+        System.out.println();
     }
+
     public void inputBuyPrice() {
         buyPrice = Integer.parseInt(readLine());
+    }
+
+    private void inputWinning() {
+        String[] numbers = readLine().split(",");
+        List<Integer> winningNumbers = new ArrayList<>();
+
+        for (String number : numbers) {
+            winningNumbers.add(Integer.valueOf(number));
+        }
+
+        winningLotto = new Lotto(winningNumbers);
     }
 }
