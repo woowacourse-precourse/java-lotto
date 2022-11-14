2e4a192fba4b915dc1753f385a8998efb865efc0
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-14 23:56:53 +0900

    STYLE:Lotto.java/revise class

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,19 +3,18 @@ package lotto;
 import java.util.*;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateOverNum(numbers);
         validateDuplicate(numbers);
         validateNotMatchNum(numbers);
-        sorting(numbers);
         this.numbers = numbers;
     }
 
     private void validateOverNum(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            System.out.println("[ERROR] 로또 번호의 개수가 6개 초과");
+            System.out.println("[ERROR] 로또 번호의 개수가 6개를 초과했습니다.");
             throw new IllegalArgumentException();
         }
     }
@@ -23,7 +22,7 @@ public class Lotto {
     private void validateDuplicate(List<Integer> numbers) {
         for (int i = 0; i < numbers.size() - 1; i++) {
             if (numbers.get(i) == numbers.get(i + 1)) {
-                System.out.println("[ERROR] 로또 번호에 중복된 숫자 존재");
+                System.out.println("[ERROR] 로또 나열번호에 중복된 숫자가 존재합니다.");
                 throw new IllegalArgumentException();
             }
         }
@@ -32,13 +31,9 @@ public class Lotto {
     private void validateNotMatchNum(List<Integer> numbers) {
         for (int i = 0; i < numbers.size(); i++) {
             if (45 < numbers.get(i) || numbers.get(i) < 1) {
-                System.out.println("[ERROR] 로또 번호가 1부터 45 이내에 있지 않음");
+                System.out.println("[ERROR] 로또 번호가 1부터 45 이내에 있지 않습니다.");
                 throw new IllegalArgumentException();
             }
         }
-    }
-
-    private void sorting(List<Integer> numbers) {
-        numbers.sort(Comparator.naturalOrder());
     }
 }
