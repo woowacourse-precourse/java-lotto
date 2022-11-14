791fc3c689464166c409118411cd1e082b62630e
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-11-14 22:23:44 +0900

    feat: 당첨 번호 입력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,11 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         Input input = new Input();
-        int purchaseAmount=input.getPurchaseAmount();
+        int purchaseAmount = input.getPurchaseAmount();
+        List<Integer> winningNumber = input.getWinningNumber();
     }
 }
--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -2,10 +2,26 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Input {
-    public int getPurchaseAmount(){
+    public int getPurchaseAmount() {
         String purchaseAmountInput = Console.readLine();
         int purchaseAmount = Integer.valueOf(purchaseAmountInput);
         return purchaseAmount;
+    }
+
+    public List getWinningNumber() {
+        String winningNumberInput = Console.readLine();
+        String[] splittedInput = winningNumberInput.split(",");
+        List<Integer> winningNumber = new ArrayList<Integer>();
+
+        for(int order=0; order<splittedInput.length; order++) {
+            winningNumber.add(Integer.valueOf(splittedInput[order]));
+        }
+
+        return winningNumber;
     }
 }
