d23a28b43d5b37ce1e7d7dcada95a098aaf57a93
phj1120 <hyeonj1998@naver.com>
2022-11-14 11:27:21 +0900

    docs(lotto): 문서 정리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,11 @@ package lotto;
 
 import java.util.List;
 
+/**
+ * - `Lotto`에 매개 변수가 없는 생성자를 추가할 수 없다.
+ * - `numbers`의 접근 제어자인 private을 변경할 수 없다.
+ * - `Lotto`에 필드(인스턴스 변수)를 추가할 수 없다.
+ */
 public class Lotto {
     private final List<Integer> numbers;
 
