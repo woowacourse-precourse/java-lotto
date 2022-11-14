4171fa4697237a2de064013827d3f898011bd52a
wltks98 <wltks98@hanmail.net>
2022-11-14 18:19:45 +0900

    refactor : WinningNumbers의 유효성 검사를 Lotto로 넘김

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,10 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Set;
 
 import lotto.exception.ErrorMessage;
+import lotto.view.OutputView;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,8 +15,35 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException(ErrorMessage.NUMBER_COUNT_ERROR.getMessage());
+        validateCount(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateCount(List<Integer> numbers){
+        if (numbers.size() != LottoRule.NUMBER_COUNT.getValue()){
+            OutputView.printError(ErrorMessage.NUMBER_COUNT_ERROR);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRange(List<Integer> numbers){
+        for(Integer number : numbers){
+            checkRange(number);
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers){
+        if(Set.copyOf(numbers).size()!= LottoRule.NUMBER_COUNT.getValue()){
+            OutputView.printError(ErrorMessage.DUPLICATE_NUMBER_ERROR);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkRange(Integer number){
+        if (number<LottoRule.MIN_NUMBER.getValue() || number>LottoRule.MAX_NUMBER.getValue()){
+            OutputView.printError(ErrorMessage.NUMBER_RANGE_ERROR);
+            throw new IllegalArgumentException();
         }
     }
 
@@ -22,5 +51,5 @@ public class Lotto {
         return numbers;
     }
 
-    // TODO: 추가 기능 구현
+
 }
--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,7 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
-import java.util.Set;
+
 
 import lotto.exception.ErrorMessage;
 import lotto.view.OutputView;
@@ -12,46 +12,22 @@ public class WinningNumbers {
 	private final int bonusNumber;
 
 	public WinningNumbers(List<Integer> numbers, int bonusNumber) {
-		validateWinningNumbers(numbers);
-		this.numbers = numbers;
+		this.numbers = new Lotto(numbers).getNumbers();
 
 		validateBonusNumber(bonusNumber);
 		this.bonusNumber = bonusNumber;
 	}
 
-	private void validateWinningNumbers(List<Integer> numbers) {
-		validateCount(numbers);
-		validateRange(numbers);
-		validateDuplicate(numbers);
-	}
 
 	private void validateBonusNumber(int bonusNumber) {
 		checkRange(bonusNumber);
 		validateDuplicate(bonusNumber);
 	}
 
-	private void validateCount(List<Integer> numbers){
-		if (numbers.size() != LottoRule.NUMBER_COUNT.getValue()){
-			OutputView.printError(ErrorMessage.NUMBER_COUNT_ERROR);
-			throw new IllegalArgumentException();
-		}
-	}
 
-	private void validateRange(List<Integer> numbers){
-		for(Integer number : numbers){
-			checkRange(number);
-		}
-	}
-
-	private void validateDuplicate(List<Integer> numbers){
-		if(Set.copyOf(numbers).size()!= LottoRule.NUMBER_COUNT.getValue()){
-			OutputView.printError(ErrorMessage.DUPLICATE_NUMBER_ERROR);
-			throw new IllegalArgumentException();
-		}
-	}
 
 	private void validateDuplicate(Integer number){
-		if(numbers.contains(number)){
+		if(this.numbers.contains(number)){
 			OutputView.printError(ErrorMessage.DUPLICATE_BONUS_NUMBER_ERROR);
 			throw new IllegalArgumentException();
 		}
@@ -65,7 +41,7 @@ public class WinningNumbers {
 	}
 
 	public List<Integer> getNumbers() {
-		return numbers;
+		return this.numbers;
 	}
 
 	public int getBonusNumber() {
