0c2e08cc71a5497b09a32512c3fd2464d6cba2ff
tseop <77163842+tseop@users.noreply.github.com>
2022-11-15 00:03:30 +0900

    rename(lotto): Lotto 변환기 사용 용도에 맞게 파일명 변경
    LottoValidator -> LottoConvertor

--- src/main/java/lotto/lotto/LottoConvertor.java
+++ src/main/java/lotto/lotto/LottoConvertor.java
@@ -1,20 +1,17 @@
 package lotto.lotto;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import lotto.exception.LottoException;
 import lotto.input.Input;
 
-public class LottoValidator {
+public class LottoConvertor {
     private static final Input input = new Input();
 
     public String[] splitNumbers(String input) { // 입력한 값을 String 배열로 넘긴다.
         return input.split(",");
     }
 
-    public List<Integer> convertNumbers(String[] numbers) { // String 배열을 List로 반환
+    public List<Integer> convertNumbers(String[] numbers) {
         List<Integer> prizeNumbers = new ArrayList<>();
         for (String number : numbers) {
             prizeNumbers.add(input.convertInput(number));
