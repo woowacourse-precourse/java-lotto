625f7c4c8f54665acfa0e7645005e3dfddf660f7
TaeHwan <kimta2hwan@gmail.com>
2022-11-14 15:47:43 +0900

    feat: 중복된 번호 입력 시 예외 발생 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,25 +1,28 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
+
+    private final Set<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = new HashSet<>(numbers);
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (new HashSet<>(numbers).size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
     // TODO: 추가 기능 구현
 
-    public List<Integer> getLotto() {
+    public Set<Integer> getLotto() {
         return numbers;
     }
 
