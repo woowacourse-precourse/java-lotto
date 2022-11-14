5ff8f94797075da37eae47379fe68b306fd4c62d
Namyunsuk <yunsuk0328@naver.com>
2022-11-14 22:20:38 +0900

    test(CalculationResult): 수익률 계산 테스트

--- src/test/java/lotto/CalculationResultTest.java
+++ src/test/java/lotto/CalculationResultTest.java
@@ -14,7 +14,9 @@ public class CalculationResultTest {
     private static final List<List<Integer>> lotteries = List.of(List.of(1,2,3,4,5,6),List.of(1,2,3,4,6,7));
     private static final List<Integer> answer = List.of(1,2,3,4,5,11);
     private static final int bonusNumber = 6;
-    private static final int ValidPrize = 30_050_000;
+    private static final int userInputMoney = 80000;
+    private static final int TOTAL_Prize = 30_050_000;
+    private static final double VALID_RATE = 37562.5f;
     private CalculationResult calculationResult;
 
     @BeforeEach
@@ -26,6 +28,12 @@ public class CalculationResultTest {
     @Test
     void checkPrizeCalculation() {
         calculationResult.calculateResult(lotteries,bonusNumber,answer);
-        assertThat(calculationResult.calculateTotalPrize()).isEqualTo(ValidPrize);
+        assertThat(calculationResult.calculateTotalPrize()).isEqualTo(TOTAL_Prize);
+    }
+
+    @DisplayName("수익률이 정확히 계산되는지 테스트")
+    @Test
+    void checkRateCalculation() {
+        assertThat(calculationResult.calculateRate(userInputMoney,TOTAL_Prize)).isEqualTo(VALID_RATE);
     }
 }
