4414af9d11c57ffb9c803f1089f07a7de5bd07a7
YooByeongIk <quddlr96@naver.com>
2022-11-14 21:41:33 +0900

    feat(lotto): add method to get winning lotto numbers from user input value

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,10 +8,20 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int purchasePrice = getPurchasePrice(Console.readLine());
+        List<Integer> winningLottoNumbers = getWinningLottoNumbers(Console.readLine());
     }
 
-    public static int getPurchasePrice(){
-        String input = Console.readLine();
+    public static int getPurchasePrice(String input){
         return Integer.parseInt(input);
+    }
+
+    public static List<Integer> getWinningLottoNumbers(String input){
+        String[] inputNumbers = input.split(",");
+        List<Integer> numbers = new ArrayList<>();
+        for(String inputNumber : inputNumbers){
+            numbers.add(Integer.parseInt(inputNumber));
+        }
+        return numbers;
     }
 }
