39a7f9962522bf0127bafb13ebd5c8a3e76a7a48
rabbit0216 <rabbit1999k@gmail.com>
2022-11-14 22:16:07 +0900

    test(lotto): 당첨번호와 동일한 숫자 개수 확인 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -29,6 +31,14 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
     // 아래에 추가 테스트 작성 가능
+
+    @DisplayName("당첨 번호와 동일한 숫자 개수를 확인한다.")
+    @Test
+    void checkCountOfSameNumber(){
+        Lotto playerLotto = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto winLotto = new Lotto(List.of(1,5,8,9,10,11));
+        int result = playerLotto.countSameNumber(winLotto.getNumbers());
+        assertThat(result == 2);
+    }
 }
