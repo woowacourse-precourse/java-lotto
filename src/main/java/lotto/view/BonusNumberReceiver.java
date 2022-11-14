25b91e96571397937a55cc3feab07e6123ce6472
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-15 01:30:53 +0900

    feat(main\java\lotto\view\BonusNumberReceiver) : implement BonusNumberReceiver

---
+++ src/main/java/lotto/view/BonusNumberReceiver.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class BonusNumberReceiver {
+    public static int receiveBonusNumber() {
+        return Integer.parseInt(readLine());
+    }
+}
