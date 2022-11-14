b2a7f9ac13e252edc5bfd29fc0ec386756f1bbb0
sjkim <seoungjoo01@gmail.com>
2022-11-15 00:08:01 +0900

    feat: MVC 패턴 구조 초기 폴더(패키지) 생성

---
+++ src/main/java/lotto/Controller/LottoController.java
@@ -0,0 +1,11 @@
+package lotto.Controller;
+
+import lotto.Service.LottoService;
+
+public class LottoController {
+
+    public LottoService lottoService = new LottoService();
+    public void startLotto() {
+
+    }
+}
