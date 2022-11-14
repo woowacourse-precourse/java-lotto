bba185625164e441688101d0b519c7e61d20674c
dhkim <akdlzmf1123@gmail.com>
2022-11-14 11:11:38 +0900

    test(LottosTest): 복권들 중 n등 당첨 복권 갯수 구하는 기능 테스트 추가

--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -24,16 +24,39 @@ class LottosTest {
 
     static Stream<Arguments> lottosAndWinningAmount() {
         return Stream.of(
-            Arguments.of(new Lottos(List.of(
-                    new Lotto(List.of(1,2,3,4,5,6))
-                    , new Lotto(List.of(1,3,7,4,5,6))
-                    , new Lotto(List.of(1,12,23,4,35,7))
-            )), 2_030_000_000)
+                Arguments.of(new Lottos(List.of(
+                        new Lotto(List.of(1,2,3,4,5,6))
+                        , new Lotto(List.of(1,3,7,4,5,6))
+                        , new Lotto(List.of(1,12,23,4,35,7))
+                )), 2_030_000_000)
                 , Arguments.of(new Lottos(List.of(
                         new Lotto(List.of(1,2,3,13,5,6))
                         , new Lotto(List.of(1,28,13,4,5,6))
                         , new Lotto(List.of(21,2,3,4,7,12))
                 )), 1_555_000)
+        );
+    }
+
+    @ParameterizedTest(name = "name")
+    @MethodSource("lottosAndPrizeAndCount")
+    void 복권들_중_n등_당첨된_복권_갯수_세는_기능(Lottos lottos, Prize prize, int count) {
+        assertThat(lottos.countWinningLottos(prize)).isEqualTo(count);
+    }
+
+    static Stream<Arguments> lottosAndPrizeAndCount() {
+        return Stream.of(
+                Arguments.of(new Lottos(List.of(
+                        new Lotto(List.of(1,2,3,7,8,9))
+                        , new Lotto(List.of(11,22,7,4,5,6))
+                        , new Lotto(List.of(1,12,23,4,35,7))
+                )), Prize.FIFTH, 3)
+                , Arguments.of(new Lottos(List.of(
+                        new Lotto(List.of(1,2,3,13,5,7))
+                        , new Lotto(List.of(11,2,13,4,5,6))
+                        , new Lotto(List.of(21,2,3,4,7,12))
+                        , new Lotto(List.of(1,2,3,4,5,7))
+                        , new Lotto(List.of(1,2,3,4,5,6))
+                )), Prize.FOURTH, 2)
         );
     }
 }
