bc3cfb6bd7659d2f80d9af39183d1a378c3d9451
myeongju00 <j51677767@gmail.com>
2022-11-14 15:11:57 +0900

    feat: 당첨번호 입력 메세지 추가

--- src/main/java/lotto/domain/InputMessage.java
+++ src/main/java/lotto/domain/InputMessage.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
 public enum InputMessage {
-    INPUT_MONEY("구입금액을 입력해 주세요.");
+    INPUT_MONEY("구입금액을 입력해 주세요."),
+    INPUT_WINNING_NUMBER("당첨 번호를 입력해주세요.");
+
 
     private final String message;
 
