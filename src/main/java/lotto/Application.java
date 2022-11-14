cff255a93385b78e08be97904fb0ac482edebff4
minji <heunhanireum@gmail.com>
2022-11-14 17:59:06 +0900

    feat: 입력받은 금액이 유효한지 확인

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,9 +6,27 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("구입금액을 입력해 주세요.");
+        Integer numOfLotto = isUserPaymentValid(userPayment());
     }
 
     public static String userPayment () {
         return Console.readLine();
+    }
+
+    public static Integer isUserPaymentValid (String userPayment) {
+        try {
+            Integer.parseInt(userPayment);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
+        Integer numberOfLotto = Integer.parseInt(userPayment);
+        try {
+            if (numberOfLotto%1000 != 0) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR]");
+        }
+        return numberOfLotto/1000;
     }
 }
