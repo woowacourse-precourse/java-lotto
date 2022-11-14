130b4b014b37cb315b616aef78d3f818aa7f011d
jiwon1217 <koagjw@gmail.com>
2022-11-15 03:46:27 +0900

    refactor: 숫자 단위에 구분자 추가

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -6,11 +6,11 @@ import java.util.stream.Collectors;
 
 public enum Rank {
     MISS(0, 0),
-    FIFTH(5000, 3),
-    FOURTH(50000, 4),
-    THIRD(1500000, 5),
-    SECOND(30000000, 5),
-    FIRST(2000000000, 6);
+    FIFTH(5_000, 3),
+    FOURTH(50_000, 4),
+    THIRD(1_500_000, 5),
+    SECOND(30_000_000, 5),
+    FIRST(2_000_000_000, 6);
 
     private final int winnings;
     private final int matchCount;
