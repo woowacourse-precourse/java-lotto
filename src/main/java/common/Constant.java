7ecd77939248d26670b64627570afbcd823c7981
박시균\psk98 <psg980331@naver.com>
2022-11-14 12:40:24 +0900

    Style: 상수메시지 패키지 이동 및 메소드화

--- src/main/java/common/Constant.java
+++ src/main/java/common/Constant.java
@@ -20,14 +20,7 @@ public final class Constant {
     public static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해주세요.";
     public static final String WINNING_STATISTICS_MESSAGE = "당첨 통계";
     public static final String UNDER_SCORE_MESSAGE = "----";
-    public static final String WINNING_LOTTO_THREE_MESSAGE = "3개 일치 (5,000원) - ";
-    public static final String WINNING_LOTTO_FOUR_MESSAGE = "4개 일치 (50,000원) - ";
-    public static final String WINNING_LOTTO_FIVE_MESSAGE = "5개 일치 (1,500,000원) - ";
-    public static final String WINNING_LOTTO_FIVE_BONUS_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
-    public static final String WINNING_LOTTO_SIX_MESSAGE = "6개 일치 (2,000,000,000원) - ";
 
-    public static void PLAYER_YIELD_MESSAGE(double yield){
-        System.out.println("총 수익률은"+ yield +"%입니다.");
-    }
+
 
 }
--- src/main/java/common/LottoGameMessage.java
+++ src/main/java/common/LottoGameMessage.java
@@ -3,11 +3,40 @@ package common;
 import domain.Lotto;
 import domain.LottoRank;
 import domain.Player;
-import java.util.Iterator;
+import lottoService.LottoService;
+
+
+import java.util.*;
 
 import static common.Constant.*;
 
 public class LottoGameMessage {
+
+    private static final LottoService lottoService = new LottoService();
+
+    public static void WINNING_LOTTO_THREE_MESSAGE(int winningLottoThreeCount){
+        System.out.println("3개 일치 (5,000원) - " + winningLottoThreeCount + "개");
+    };
+
+    public static void WINNING_LOTTO_FOUR_MESSAGE(int winningLottoFourCount){
+        System.out.println("4개 일치 (50,000원) - " + winningLottoFourCount + "개");
+    };
+
+    public static void WINNING_LOTTO_FIVE_MESSAGE(int winningLottoFiveCount){
+        System.out.println("5개 일치 (1,500,000원) - " + winningLottoFiveCount + "개");
+    };
+
+    public static void WINNING_LOTTO_FIVE_BONUS_MESSAGE(int winningLottoFiveAndBonusCount){
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + winningLottoFiveAndBonusCount + "개");
+    };
+
+    public static void WINNING_LOTTO_SIX_MESSAGE(int winningLottoSixCount){
+        System.out.println("6개 일치 (2,000,000,000원) - " + winningLottoSixCount + "개");
+    };
+
+    public static void PLAYER_YIELD_MESSAGE(String yield){
+        System.out.println("총 수익률은 "+ yield +"%입니다.");
+    }
 
     public void printPurchasePriceMessage(){
         System.out.println(PURCHASE_PRICE_MESSAGE);
@@ -29,20 +58,22 @@ public class LottoGameMessage {
 
         while(playerLottoNumbers.hasNext()){
             Lotto playerRandomLottoNumbers = playerLottoNumbers.next();
-            System.out.println(playerRandomLottoNumbers.getNumbers());
+            lottoService.printSortedPlayerLottoNumbers(playerRandomLottoNumbers.getNumbers());
         }
     }
+
+
     public void printLottoWinningStatistics(Player player){
         System.out.println(WINNING_STATISTICS_MESSAGE);
         System.out.println(UNDER_SCORE_MESSAGE);
-        System.out.println(WINNING_LOTTO_THREE_MESSAGE + player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_3,0));
-        System.out.println(WINNING_LOTTO_FOUR_MESSAGE + player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_4,0));
-        System.out.println(WINNING_LOTTO_FIVE_MESSAGE + player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_5,0));
-        System.out.println(WINNING_LOTTO_FIVE_BONUS_MESSAGE + player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_5_BONUS,0));
-        System.out.println(WINNING_LOTTO_SIX_MESSAGE + player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_6,0));
+        WINNING_LOTTO_THREE_MESSAGE(player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_3,0));
+        WINNING_LOTTO_FOUR_MESSAGE(player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_4,0));
+        WINNING_LOTTO_FIVE_MESSAGE(player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_5,0));
+        WINNING_LOTTO_FIVE_BONUS_MESSAGE(player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_5_BONUS,0));
+        WINNING_LOTTO_SIX_MESSAGE(player.getWinningLottoCount().getOrDefault(LottoRank.WIN_LOTTO_6,0));
     }
     public void printPlayerLottoYield(Player player){
-        double yield = Math.round(player.getLottoRevenue()/player.getLottoPrice() * 100);
-        Constant.PLAYER_YIELD_MESSAGE(yield);
+        String yield = String.format("%.1f", (double)player.getLottoRevenue()/player.getLottoPrice() * 100);
+        PLAYER_YIELD_MESSAGE(yield);
     }
 }
