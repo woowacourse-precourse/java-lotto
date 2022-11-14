038b9fa29819cc764bd103c5c8640c5b100b9a6b
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-14 13:53:49 +0900

    feat(Result): 당첨 횟수를 파악하는 기능 구현

--- src/main/java/lotto/model/Result.java
+++ src/main/java/lotto/model/Result.java
@@ -1,23 +1,51 @@
 package lotto.model;
 
+import lotto.price;
+
 import java.util.List;
 
 public class Result {
-    private int threeCount;
-    private int fourCount;
-    private int fiveCount;
-    private int sixCount;
+    private int[] resultArray;
     private double rateOfReturn;
+    private int sum;
 
     public Result() {
-        threeCount=0;
-        fourCount=0;
-        fiveCount=0;
-        sixCount=0;
         rateOfReturn=0;
+        resultArray=new int[6];
+        for(int i=0;i<6;i++)
+            resultArray[i]=0;
+        sum=0;
     }
 
-    public void calculatorResult(List<Integer> userNumber,List<Integer> lottoNumber,int bonusNumber){
+    public void calculatorResult(List<List<Integer>> randomNumbers, List<Integer> lottoNumber, int bonusNumber){
+        randomNumbers.stream()
+                .forEach(numberList->setResult(
+                        setResultArray((int)numberList.stream()
+                        .filter(number->lottoNumber.contains(number))
+                        .count(),
+                        (int)numberList.stream()
+                        .filter(number->number==bonusNumber)
+                        .count()))
+                );
+    }
+    private static int setResultArray(int number,int bonusNumber)
+    {
+        if(number==3)
+            return 1;
+        if(number==4)
+            return 2;
+        if(number==5) {
+            if (bonusNumber==1)
+                return 4;
+            return 3;
+        }
+        if(number==6)
+            return 5;
 
+        return 0;
+    }
+
+    private void setResult(int index){
+        resultArray[index]++;
     }
 }
