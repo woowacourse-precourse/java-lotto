e6dc2ac530eb9049d36f7e9d1f351c0fe2c8447c
jinyeong-afk <xoeh7002@naver.com>
2022-11-14 22:10:18 +0900

    feat: 보너스 번호 예외 처리 구현

--- src/main/java/lotto/valid/InputLottoValidator.java
+++ src/main/java/lotto/valid/InputLottoValidator.java
@@ -71,4 +71,30 @@ public class InputLottoValidator {
         }
     }
 
+    public void validateBonusNumber(String bonusNumber, List<Integer> winningNumbers) {
+        isDigit(bonusNumber);
+        checkCountBonusNumber(bonusNumber);
+        duplicateBonusNumber(bonusNumber, winningNumbers);
+        checkRangeBonusNumber(bonusNumber);
+    }
+
+    public void checkCountBonusNumber(String bonusNumber) {
+        if (bonusNumber.length() != 1) {
+            throw new IllegalArgumentException("[ERROR] 1개의 번호를 입력하여야 합니다.");
+        }
+    }
+
+    private void duplicateBonusNumber(String bonusNumber, List<Integer> winningNumbers) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호와 다른 번호를 입력하여야 합니다.");
+        }
+    }
+
+    private void checkRangeBonusNumber(String bonusNumber) {
+        int integerBonusNumber = Integer.parseInt(bonusNumber);
+        if (!(integerBonusNumber >= 1 && integerBonusNumber <= 45)) {
+            throw new IllegalArgumentException("[ERROR] 1부터 45 사이의 번호를 입력하여야 합니다.");
+        }
+    }
+
 }
