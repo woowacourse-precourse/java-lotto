d8eeb5626537a72c9cae273183594f3a7a7a0770
kkm5291 <kkm23125291@gmail.com>
2022-11-15 00:05:55 +0900

    [FEAT] Lotto : checkNumberDuplicate 로또 자동번호 중복여부 확인 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,19 +1,30 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         numberSize(numbers);
+        checkNumberDuplicate(numbers);
         this.numbers = numbers;
     }
 
     public static void numberSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    public static void checkNumberDuplicate(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.contains(numbers.get(i))) {
+                throw new IllegalArgumentException(" 중복된 숫자는 잘못된 입력값 입니다.");
+            }
         }
     }
 
