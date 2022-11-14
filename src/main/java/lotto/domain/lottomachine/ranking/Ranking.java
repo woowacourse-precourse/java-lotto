4df8ef74829c5e2cbbcfe4819b5d969765cd6213
chanminmaxscaler <jcm04@naver.com>
2022-11-14 17:49:37 +0900

    chore: Prize 클래스의 이름을 Ranking으로 변경한다
    패키지 구조도 변경한다. ranking 패키지로 옮긴다.

--- src/main/java/lotto/domain/lottomachine/ranking/Ranking.java
+++ src/main/java/lotto/domain/lottomachine/ranking/Ranking.java
@@ -1,9 +1,9 @@
-package lotto.domain.lottomachine.prize;
+package lotto.domain.lottomachine.ranking;
 
 import java.text.DecimalFormat;
 import java.util.Arrays;
 
-public enum Prize {
+public enum Ranking {
     FIRST_PLACE("1등", "6", 2_000_000_000),
     SECOND_PLACE("2등", "5B", 30_000_000),
     THIRD_PLACE("3등", "5", 1_500_000),
@@ -14,7 +14,7 @@ public enum Prize {
     private final String numberOfMatching;
     private final int cash;
 
-    Prize(String ranking, String numberOfMatching, int cash) {
+    Ranking(String ranking, String numberOfMatching, int cash) {
         this.ranking = ranking;
         this.numberOfMatching = numberOfMatching;
         this.cash = cash;
@@ -32,7 +32,7 @@ public enum Prize {
         return cash;
     }
 
-    public static Prize valueOfRanking(String ranking) {
+    public static Ranking valueOfRanking(String ranking) {
         return Arrays.stream(values())
                 .filter(value -> value.ranking.equals(ranking))
                 .findAny()
