9dee62bf9c0bae2d4c85dbbf0913a4cb85ab3945
dooboocookie <bkh3030@gmail.com>
2022-11-14 15:06:20 +0900

    refactor: 중복되던 숫자가 아닌 상황의 예외처리를 메소드로 묶음

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class Input {
     public static String readString() {
@@ -11,19 +12,17 @@ public class Input {
     }
 
     public static int readInteger() {
-        try {
-            return Integer.parseInt(readString());
-        } catch (Exception e) {
-            throw new IllegalArgumentException(Error.ERROR_INPUT_PARSING_NUMBER.getValue());
-        }
+        String input = readString();
+        validateNumber(input);
+        return Integer.parseInt(input);
     }
 
-    public static long readLong() {
-        try {
-            return Long.parseLong(readString());
-        } catch (Exception e) {
-            throw new IllegalArgumentException(Error.ERROR_INPUT_PARSING_NUMBER.getValue());
-        }
+    public static long readLong() throws IllegalArgumentException {
+
+        String input = readString();
+            validateNumber(input);
+
+        return Long.parseLong(input);
     }
 
     public static List<Integer> readListInteger(String seperator) {
@@ -31,12 +30,16 @@ public class Input {
         String[] inputs = input.split(seperator);
         List<Integer> separateInput = new ArrayList<>();
         for (int inputIndex = 0; inputIndex < inputs.length; inputIndex++) {
-            try {
-                separateInput.add(Integer.parseInt(inputs[inputIndex]));
-            } catch (Exception e) {
-                throw new IllegalArgumentException(Error.ERROR_INPUT_PARSING_NUMBER.getValue());
-            }
+            String elementInput = inputs[inputIndex];
+            validateNumber(input);
+            separateInput.add(Integer.parseInt(elementInput));
         }
         return separateInput;
+    }
+
+    private static void validateNumber(String input) throws IllegalArgumentException{
+        if (input.matches("[0-9]*[^0-9]+[0-9]*")) {
+            throw new IllegalArgumentException(Error.ERROR_INPUT_PARSING_NUMBER.getValue());
+        }
     }
 }
