24b6e4336e5937828f61b244b45b54d7ca3c8869
d0ri123 <lsh81550@gmail.com>
2022-11-14 19:07:37 +0900

    refactor(lotto): enum클래스 리팩토링. 비슷한 항목끼리 묶어줌

--- src/main/java/lotto/WinningStatus.java
+++ src/main/java/lotto/WinningStatus.java
@@ -1,18 +1,20 @@
 package lotto;
 
 public enum WinningStatus {
-    CASE1(3,5000),
-    CASE2(4,50_000),
-    CASE3(5,1_500_000),
-    CASE4(7,30_000_000),
-    CASE5(6,2_000_000_000);
+    CASE1(3,5000,"3개 일치 (5,000원) - "),
+    CASE2(4,50_000,"4개 일치 (50,000원) - "),
+    CASE3(5,1_500_000,"5개 일치 (1,500,000원) - "),
+    CASE4(7,30_000_000,"5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    CASE5(6,2_000_000_000,"6개 일치 (2,000,000,000원) - ");
 
     private int matchNumber;
     private int winningPrice;
+    private String format;
 
-    WinningStatus(int matchNumber, int winningPrice){
+    WinningStatus(int matchNumber, int winningPrice, String statisticsFormat){
         this.matchNumber = matchNumber;
         this.winningPrice = winningPrice;
+        this.format = statisticsFormat;
     }
 
     public int getMatchNumber(){
@@ -23,7 +25,8 @@ public enum WinningStatus {
         return winningPrice;
     }
 
-    public void winningStatistics(){
-
+    public String getFormat(){
+        return format;
     }
+
 }
