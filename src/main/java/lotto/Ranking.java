4b977ec547ae24d57e1cf46372a37a9b84460af1
taeyun-work <wmfajr1357@gmail.com>
2022-11-14 14:08:15 +0900

    style: 스타일 포맷

--- src/main/java/lotto/Ranking.java
+++ src/main/java/lotto/Ranking.java
@@ -81,12 +81,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum Ranking {
-    FIRST("1ST", 6, new int[] {0, 1}, 2_000_000_000L, "2,000,000,000"),
-    SECOND("2ND", 5, new int[] {1}, 30_000_000L, "30,000,000"),
-    THIRD("3RD", 5, new int[] {0}, 1_500_000L, "1,500,000"),
-    FOURTH("4TH", 4, new int[] {0, 1}, 50_000L, "50,000"),
-    FIFTH("5TH", 3, new int[] {0, 1}, 5_000L, "5,000"),
-    LOSE("LOST", -1, new int[] {0, 1}, 0L, "0");
+    FIRST("1ST", 6, new int[]{0, 1}, 2_000_000_000L, "2,000,000,000"),
+    SECOND("2ND", 5, new int[]{1}, 30_000_000L, "30,000,000"),
+    THIRD("3RD", 5, new int[]{0}, 1_500_000L, "1,500,000"),
+    FOURTH("4TH", 4, new int[]{0, 1}, 50_000L, "50,000"),
+    FIFTH("5TH", 3, new int[]{0, 1}, 5_000L, "5,000"),
+    LOSE("LOST", -1, new int[]{0, 1}, 0L, "0");
 
     private final String label;
     private final int winningCount;
@@ -105,7 +105,7 @@ public enum Ranking {
     public static Ranking getRank(int winningCount, int bonusCount) {
         return Arrays.stream(Ranking.values())
                 .filter(rank -> rank.winningCount == winningCount
-                            && Arrays.stream(rank.bonusCount).anyMatch(number -> number == bonusCount)
+                        && Arrays.stream(rank.bonusCount).anyMatch(number -> number == bonusCount)
                 )
                 .findAny()
                 .orElse(LOSE);
