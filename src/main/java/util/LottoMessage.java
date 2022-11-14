3a42d8ff3ef2334f40395558b005af37eb44ea07
seheonCho <whtpgjs94@gmail.com>
2022-11-15 02:32:39 +0900

    refactor(LottoMessage): 상수 오타 수정

--- src/main/java/util/LottoMessage.java
+++ src/main/java/util/LottoMessage.java
@@ -11,7 +11,7 @@ public class LottoMessage {
     public static final String SAMES_FORMAT = "%d개 일치";
     public static final String OPEN_BRACKET = "(";
     public static final String CLOSE_BRACKET = ")";
-    public static final String HYPHEN_WITH_SPACE = " ) ";
+    public static final String HYPHEN_WITH_SPACE = " - ";
     public static final String KOREA_BASICS_UNIT = "개";
 
 }
