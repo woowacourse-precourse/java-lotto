1f6b1eba139f0d95600f6d63003fbc8e3ea7d80e
MuSeung <peter0757@nvaer.com>
2022-11-14 10:25:56 +0900

    feat: ResultPrint 에 수익률을 출력하는 기능 구현
    ResultPrint
        - printingResult : LottoResult를 Map을 이용하여 만들고 printYield를 호출
        - printYield : 수익률을 출력하는 메서드
        - printYieldFormat :실수를 ###,###.0%의 형태로 출력하는 메서드

--- src/main/java/lotto/ResultPrint.java
+++ src/main/java/lotto/ResultPrint.java
@@ -9,11 +9,13 @@ import java.util.Map;
 public class ResultPrint {
 
     public static void printingResult(Map<String, Integer> prizesMap) {
+        LottoResult lottoResult = new LottoResult(prizesMap);
         List<LottoPrizes> lottoPrizes = List.of(FIFTH, FOURTH, THIRD, SECOND, FIRST);
         for (LottoPrizes lottoPrize : lottoPrizes) {
             int prizesCount = prizesMap.get(lottoPrize.name());
             printPrize(lottoPrize, prizesCount);
         }
+        printYield(lottoResult.yield());
     }
     private static void printPrize(LottoPrizes lottoPrizes, int prizesCount) {
         System.out.print(lottoPrizes.getCondition());
@@ -22,13 +24,22 @@ public class ResultPrint {
             System.out.print(", 보너스 볼 일치");
         }
         System.out.print(" (");
-        printMoneyFormat((int) lottoPrizes.money());
+        printMoneyFormat(lottoPrizes.money());
         System.out.print("원) - ");
         System.out.print(prizesCount);
         System.out.print("개\n");
     }
-    private static void printMoneyFormat(int money) {
+    private static void printMoneyFormat(double money) {
         DecimalFormat decimalFormat = new DecimalFormat("###,###");
-        System.out.print(decimalFormat.format(money));
+        System.out.print(decimalFormat.format((int) money));
+    }
+    private static void printYield(double yield) {
+        System.out.print("총 수익률은 ");
+        printYieldFormat(yield);
+        System.out.print("입니다.\n");
+    }
+    private static void printYieldFormat(double yield) {
+        DecimalFormat decimalFormat = new DecimalFormat("###,##0.0");
+        System.out.print(decimalFormat.format(yield * 100) + "%");
     }
 }
