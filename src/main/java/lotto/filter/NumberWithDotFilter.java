9afffc0df1a3ef192ed5628b3962e028caeecfd6
lcw3176 <lcw3176@naver.com>
2022-11-14 12:40:26 +0900

    feat: 당첨 번호 입력 필터

---
+++ src/main/java/lotto/filter/NumberWithDotFilter.java
@@ -0,0 +1,23 @@
+package lotto.filter;
+
+public class NumberWithDotFilter implements Filter {
+
+    private static final String SPLITTER = ",";
+    private static final int VALID_SIZE = 6;
+
+    @Override
+    public void doFilter(String numberWithDot) {
+        String[] numbers = numberWithDot.split(SPLITTER);
+        
+        if (numbers.length != VALID_SIZE) {
+            throw new IllegalArgumentException("입력한 숫자의 형식이 잘못되었거나 갯수가 부족합니다");
+        }
+
+        for (String i : numbers) {
+            if (!Character.isDigit(i.charAt(0))) {
+                throw new IllegalArgumentException("입력한 문자가 숫자가 아닙니다.");
+            }
+        }
+
+    }
+}
