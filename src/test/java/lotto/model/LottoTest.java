5048d5f307a4150e6d89fed612099f79bb7bc60c
dhkim <akdlzmf1123@gmail.com>
2022-11-14 12:42:06 +0900

    test(LottoTest): 복권 한 장의 상금을 구하는 기능 테스트 추가

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,8 +1,5 @@
 package lotto.model;
 
-import lotto.model.CompareResult;
-import lotto.model.Lotto;
-import lotto.model.WinningNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -18,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class LottoTest {
 
     WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -32,4 +30,22 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @ParameterizedTest(name = "로또 당첨 금액은 {1}입니다.")
+    @MethodSource("lottoAndMoney")
+    void 복권_상금_구하는_기능(Lotto lotto, int expected) {
+        assertThat(lotto.getWinningAmount(winningNumbers)).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> lottoAndMoney() {
+        return Stream.of(
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 2_000_000_000)
+                , Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 7)), 30_000_000)
+                , Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 8)), 1_500_000)
+                , Arguments.of(new Lotto(List.of(1, 2, 3, 4, 8, 9)), 50_000)
+                , Arguments.of(new Lotto(List.of(10, 2, 3, 4, 7, 9)), 5_000)
+                , Arguments.of(new Lotto(List.of(11, 2, 3, 10, 8, 9)), 0)
+        );
+    }
+
 }
