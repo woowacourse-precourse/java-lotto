aa66b6326ffa8e1d06d4e2110001861929d99b06
sieun <sieun960513@naver.com>
2022-11-14 16:11:33 +0900

    [lotto] 중복된 정렬 제거

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -23,7 +23,6 @@ public class Lotto {
 
     public static Lotto create() {
         Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-        Collections.sort(lotto.numbers);
         return lotto;
     }
 
