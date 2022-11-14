623fc64213471011523de57d2fcb6e8f667d0316
Seiyon Park <seiyonpdev@gmail.com>
2022-11-14 22:01:36 +0900

    feat(User): 메소드 inputAmount 구현
    구매 금액을 입력받을 메소드 inputAmount를 구현했다

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,17 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class User {
     static final int LOTTO_PRICE = 1000;
     int purchasingAmount = 0;
     int lottoTicketCount = 0;
 
     void inputAmount() {
+        int userInputAmount;
 
+        System.out.println("구입금액을 입력해 주세요.");
+        userInputAmount = Integer.parseInt(Console.readLine());
+        validate(userInputAmount);
+        this.purchasingAmount = userInputAmount;
     }
 
     void validate(int purchasingAmount) throws IllegalArgumentException{
         if (purchasingAmount % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException("[ERROR]구매 금액이 1000으로 나누어 떨어지지 않습니다");
+            throw new IllegalArgumentException("[ERROR] 구매 금액이 1000으로 나누어 떨어지지 않습니다");
         }
     }
 
