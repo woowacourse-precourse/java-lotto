5fe4f4adca4a7ddc708a3210914091429383bebc
can019 <jys01012@gmail.com>
2022-11-14 19:04:15 +0900

    feat(model): caculate lotto statistics implementation
    success test and failure test passed

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,5 +18,67 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public Map<LottoMatch, Integer> calculateLottoStatistics(List<List<Integer>> userLottoList, int bonusNumber) {
+        Map<LottoMatch, Integer> calculateResult = new HashMap<>();
+
+        for (List<Integer> current : userLottoList) {
+            LottoMatch currentResult = this.calculateSingleLottoResult(current, bonusNumber);
+            if (currentResult != null) {
+                if (calculateResult.containsKey(currentResult)) {
+                    int count = calculateResult.get(currentResult);
+                    calculateResult.replace(currentResult, count + 1);
+                } else {
+                    calculateResult.put(currentResult, 1);
+                }
+            }
+        }
+
+        return calculateResult;
+    }
+
+    private LottoMatch calculateSingleLottoResult(List<Integer> input, int bonusNumber) {
+        int count = 0;
+        boolean bonus = false;
+
+        for (int current : input) {
+            if (numbers.contains(current)) {
+                count += 1;
+            }
+        }
+
+        if (count == 5 && input.contains(bonusNumber))
+            bonus = true;
+
+        return convertLottoResultToEnum(count, bonus);
+    }
+
+    private LottoMatch convertLottoResultToEnum(int count, boolean bonus) {
+        if (count == 3) {
+            return LottoMatch.MATCH_THREE;
+        } else if (count == 4) {
+            return LottoMatch.MATCH_FOUR;
+        } else if (count == 5) {
+            if (bonus) {
+                return LottoMatch.MATCH_FIVE_WITH_BONUS;
+            }
+            return LottoMatch.MATCH_FIVE;
+        } else if (count == 6) {
+            return LottoMatch.MATCH_SIX;
+        }
+        return null;
+    }
+
+    public enum LottoMatch {
+        MATCH_THREE(0),
+        MATCH_FOUR(1),
+        MATCH_FIVE(2),
+        MATCH_FIVE_WITH_BONUS(3),
+        MATCH_SIX(4);
+
+        private final int value;
+
+        LottoMatch(int value) {
+            this.value = value;
+        }
+    }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
+import static lotto.Lotto.LottoMatch.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +27,24 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("정상적으로 수행되는 경우.")
+    @Test
+    void calculateLottoResultByProperInput() {
+        List<List<Integer>> generatedRandomLotto1 = List.of(List.of(1, 2, 3, 7, 8, 9));
+        List<List<Integer>> generatedRandomLotto2 = List.of(List.of(4, 5, 6, 7, 8, 9), List.of(1, 2, 3, 4, 7, 8),
+                List.of(1, 2, 3, 4, 5, 9), List.of(1, 2, 3, 4, 5, 7), List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> userInput = List.of(1, 2, 3, 4, 5, 6);
+        int bonus = 7;
+
+        Lotto lotto = new Lotto(userInput);
+        assertAll(
+                () -> assertThat(lotto.calculateLottoStatistics(generatedRandomLotto1, bonus)).
+                        isEqualTo(Map.of(MATCH_THREE, 1)),
+                () -> assertThat(lotto.calculateLottoStatistics(generatedRandomLotto2, bonus)).
+                        isEqualTo(Map.of(MATCH_THREE, 1, MATCH_FOUR, 1, MATCH_FIVE, 1,
+                                MATCH_FIVE_WITH_BONUS, 1, MATCH_SIX, 1))
+        );
+
+    }
     // 아래에 추가 테스트 작성 가능
 }
