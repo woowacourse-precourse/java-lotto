1b872c0282685a60b717b0be1a9cf350c1ed738e
JuSeong1130 <jsmoon1130@naver.com>
2022-11-15 06:16:15 +0900

    feat(Person): 로또 구매 기능 구현

--- src/main/java/lotto/domain/person/Person.java
+++ src/main/java/lotto/domain/person/Person.java
@@ -1,5 +1,24 @@
 package lotto.domain.person;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoAutoGenerator;
+import lotto.domain.money.Money;
+
 public class Person {
 
+	private Money money;
+	private List<Lotto> lottos;
+	private LottoAutoGenerator lottoAutoGenerator;
+
+	public Person(String price) {
+		this.money = new Money(price);
+		this.lottos = new ArrayList<>();
+		this.lottoAutoGenerator = new LottoAutoGenerator();
+	}
+
+	public void buyLotto() {
+		lottos = lottoAutoGenerator.createLottoAccordingToTheAmount(money);
+	}
 }
