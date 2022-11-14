9758c4ed8682350c094afb4a7b538d8c6071b5f0
Yeongseon Park <yseon406@gmail.com>
2022-11-14 14:18:31 +0900

    feat(lotto): 로또 구입 클래스 추가
    로또 구입 관련 기능 클래스인 Purchase 클래스 추가

---
+++ src/main/java/lotto/domain/Purchase.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+public class Purchase {
+    public void inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public boolean isValid() {
+        return false;
+    }
+
+    public int canBuyLotto() {
+        return 0;
+    }
+}
