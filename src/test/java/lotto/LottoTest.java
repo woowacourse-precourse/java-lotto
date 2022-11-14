3b18cb8bcbfe9f6e8454269442b9635d982d313c
younghyun-na <younghyunna@naver.com>
2022-11-14 17:18:18 +0900

    :white_check_mark: test: 등수당 당첨된 로또 개수를 계산하는 기능 테스트 성공

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -28,7 +28,7 @@ class LottoTest {
 
     @DisplayName("일치하는 번호 개수와 보너스 번호 포함 여부를 통해, 각 등수를 구한다.")
     @Test
-    void compareLotto() {
+    void getRanking() {
         // when
         Game game = new Game();
 
@@ -36,7 +36,34 @@ class LottoTest {
         assertThat(game.valueOf(6,false)).isEqualTo(FIRST);
         assertThat(game.valueOf(6,true)).isEqualTo(FIRST);
         assertThat(game.valueOf(5,  true)).isEqualTo(SECOND);
+        assertThat(game.valueOf(5,  false)).isEqualTo(THIRD);
+        assertThat(game.valueOf(4,  false)).isEqualTo(FORTH);
         assertThat(game.valueOf(2, false)).isEqualTo(null);
+    }
+
+    @DisplayName("로또 번호와 당첨 번호를 비교하여, 각 등수 당 당첨 개수를 구한다.")
+    @Test
+    void compareLotteries() {
+        // given
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));    // 1등
+        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 7));    // 2등
+        Lotto lotto3 = new Lotto(List.of(1, 2, 3, 4, 5, 8));    // 3등
+        Lotto lotto4 = new Lotto(List.of(1, 2, 3, 5, 6, 8));    // 3등
+        Lotto lotto5 = new Lotto(List.of(1, 2, 10, 14, 15, 18));
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        List<Lotto> lotteries = List.of(lotto1, lotto2, lotto3, lotto4, lotto5);
+
+        // when
+        Game game = new Game();
+        game.compareAll(lotteries, winningNumber,bonusNumber);
+
+        // then
+        assertThat(game.compareAll(lotteries, winningNumber,bonusNumber).get(Ranking.FIRST)).isEqualTo(1);
+        assertThat(game.compareAll(lotteries, winningNumber,bonusNumber).get(Ranking.SECOND)).isEqualTo(1);
+        assertThat(game.compareAll(lotteries, winningNumber,bonusNumber).get(Ranking.THIRD)).isEqualTo(2);
+        assertThat(game.compareAll(lotteries, winningNumber,bonusNumber).get(Ranking.FORTH)).isEqualTo(0);
+        assertThat(game.compareAll(lotteries, winningNumber,bonusNumber).get(Ranking.FIFTH)).isEqualTo(0);
     }
 
 
