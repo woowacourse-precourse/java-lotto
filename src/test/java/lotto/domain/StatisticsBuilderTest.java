7547f76c82b47f30d65640b567bf5109913de3d2
ryanha <ryanha@handong.ac.kr>
2022-11-15 00:41:29 +0900

    test(domain): 통계정보 구성기능 테스트 케이스 수정

--- src/test/java/lotto/domain/StatisticsBuilderTest.java
+++ src/test/java/lotto/domain/StatisticsBuilderTest.java
@@ -1,12 +1,13 @@
 package lotto.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import lotto.view.IOProcessor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class StatisticsBuilderTest {
     @Test
@@ -15,12 +16,14 @@ public class StatisticsBuilderTest {
         StatisticsBuilder builder = new StatisticsBuilder();
         List<Integer> winningNumber = new ArrayList<>(List.of(1, 2, 3, 4 ,5, 6));
         int bonusNumber = 10;
+
         List<Integer> winningOne = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
         List<Integer> winningTwo = new ArrayList<>(List.of(1, 2, 3, 4, 5, 10));
         List<Integer> winningThree = new ArrayList<>(List.of(1, 2, 3, 4, 5, 13));
         List<Integer> winningFour = new ArrayList<>(List.of(1, 2, 3, 4, 7, 13));
         List<Integer> winningFive = new ArrayList<>(List.of(1, 2, 3, 7, 9, 13));
         List<Integer> winningNone = new ArrayList<>(List.of(1, 2, 7, 8, 9, 10));
+
         List<Lotto> lottos = new ArrayList<>();
         lottos.add(new Lotto(winningOne));
         lottos.add(new Lotto(winningTwo));
@@ -30,7 +33,17 @@ public class StatisticsBuilderTest {
         lottos.add(new Lotto(winningNone));
 
         builder.build(winningNumber, bonusNumber, lottos);
-        assertThat(builder.getWinningInfo())
-                .isEqualTo(List.of(1, 1, 1, 1, 1, 1));
+
+        Map<String, Integer> result = builder.getWinningInfo();
+        Map<String, Integer> solution = new HashMap<>();
+        solution.put("WINNING_ONE", 1);
+        solution.put("WINNING_TWO", 1);
+        solution.put("WINNING_THREE", 1);
+        solution.put("WINNING_FOUR", 1);
+        solution.put("WINNING_FIVE", 1);
+
+        for(Map.Entry<String, Integer> entry : result.entrySet()) {
+            assertEquals(result.get(entry.getKey()), solution.get(entry.getKey()));
+        }
     }
 }
