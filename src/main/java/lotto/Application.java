a84406bb26b06fb8f7e05aeb29552feb48c2eb88
seonpilKim <ksp970306@gmail.com>
2022-11-14 17:07:23 +0900

    Feat: 구매 금액 입력 메소드 추가
    구매 안내 멘트 출력
    입력 받은 값이 숫자인지 검증
    금액이 1,000원 단위인지 검증
    금액 반환

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import static lotto.domain.Seller.*;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+	public static void main(String[] args) {
+		final Integer money = readPurchaseAmount();
+	}
+
+	private static Integer readPurchaseAmount() {
+		System.out.println(SELL_ANNOUNCEMENT);
+		try {
+			final Integer money = Integer.valueOf(Console.readLine());
+			if (isNotUnitsOf1000(money)) {
+				throw new IllegalArgumentException(ErrorMessage.isNotUnitsOf1000());
+			}
+			return money;
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(ErrorMessage.isInvalidNumber());
+		}
+	}
+
+	private static boolean isNotUnitsOf1000(Integer money) {
+		return money < LOTTO_PRICE || money % LOTTO_PRICE != 0;
+	}
+
 }
