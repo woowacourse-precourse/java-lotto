a8b5954fd0594953859669a9c9b8c33eb7b71b3b
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-14 19:32:37 +0900

    feat develop : Controller 당첨 번호 생성 구현

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Controller {
+    private View view;
     private List<Lotto> lottoNumbers;
     private List<Integer> winningNumbers;
 
@@ -13,5 +14,10 @@ public class Controller {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             lottoNumbers.add(new Lotto(numbers));
         }
+    }
+
+    public void makeWinningNumbers(){
+        winningNumbers = view.inputLottoNumber();
+        winningNumbers.add(view.inputBonusNumber());
     }
 }
