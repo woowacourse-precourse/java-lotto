e0fafdd23adec6be53e258f5bcf75596d0d892ce
YUSEONJUN <seonjun0906@gmail.com>
2022-11-15 00:26:56 +0900

    test : 보너스 포함 여부 값 성공 테스트

---
+++ src/test/java/lotto/LottoProgramTest.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.junit.jupiter.api.Test;
+
+public class LottoProgramTest {
+
+	LottoProgram lottoProgram = new LottoProgram();
+
+	@Test
+	public void checkBonus_테스트_성공() {
+		List<Integer> lottoNumbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+		lottoProgram.bonusNum = 6;
+		assertThat(lottoProgram.checkBonus(lottoNumbers)).isEqualTo(true);
+	}
+
+}
