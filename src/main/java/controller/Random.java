8719e52d0937059714ab83946b470a8a764bc63a
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-15 00:00:16 +0900

    refactor(Random) : 빈줄 삭제

--- src/main/java/controller/Random.java
+++ src/main/java/controller/Random.java
@@ -5,7 +5,6 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Random {
-
     public static List<Integer> getRandomNumber() {
         List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return randomNumbers;
