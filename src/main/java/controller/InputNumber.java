081667729d3a4e05a3b780a2b8a10274b10c5c83
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-15 01:10:44 +0900

    refactor(InputNumber) : add static to function

--- src/main/java/controller/InputNumber.java
+++ src/main/java/controller/InputNumber.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputNumber {
-    public List<Integer> parseWinningNumber(String userInput) {
+    public static List<Integer> parseWinningNumber(String userInput) {
         List<String> parsedString = new ArrayList<>(Arrays.asList(userInput.split("\\s*,\\s*")));
         List<Integer> winningNumber = new ArrayList<>();
         for (String parsed : parsedString) {
@@ -14,7 +14,7 @@ public class InputNumber {
         return winningNumber;
     }
 
-    public List<Integer> parseBonusNumber(String userInput){
+    public static List<Integer> parseBonusNumber(String userInput){
         List<Integer> bonusNumber = new ArrayList<>();
         bonusNumber.add(Integer.parseInt(userInput));
         return bonusNumber;
