e176440e1ab8a98f969bab85eb3d3c3be503927c
nn98 <jkllhgb@gmail.com>
2022-11-14 14:22:55 +0900

    Feat: 테스트 클래스 정의 및 기본값 초기화

--- src/test/java/lotto/ResultTest.java
+++ src/test/java/lotto/ResultTest.java
@@ -2,7 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 
-public class UserTest extends NsTest {
+public class ResultTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
     @Override
     public void runMain() {
--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -2,7 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 
-public class UserTest extends NsTest {
+public class WinningLottoTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
     @Override
     public void runMain() {
