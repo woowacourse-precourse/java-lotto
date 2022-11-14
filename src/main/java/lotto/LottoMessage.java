7e31c3fc938df9ed18547f20ab113b6ea34fba1e
code-tree <dltjdqls1200@naver.com>
2022-11-14 21:53:49 +0900

    refactor: 당첨 개수가 없을 경우 null 대신 0개가 뜨도록 수정

--- src/main/java/lotto/LottoMessage.java
+++ src/main/java/lotto/LottoMessage.java
@@ -1,8 +1,11 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import lotto.domain.Rank;
+import lotto.domain.WinningResult;
 
 import java.util.List;
+import java.util.Map;
 
 public class LottoMessage {
     public void printPurchaseMessage() {
@@ -10,9 +13,7 @@ public class LottoMessage {
     }
 
     public void printPurchasedLotteries(List<Lotto> lotteries) {
-        for (Lotto lotto : lotteries) {
-            System.out.println(lotto.toString());
-        }
+        lotteries.forEach(System.out::println);
     }
 
     public void printInputWinningNumber() {
@@ -21,5 +22,17 @@ public class LottoMessage {
 
     public void printInputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public void printWinningResult(WinningResult winningResult) {
+        Map<Rank, Integer> results = winningResult.getResults();
+
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + results.getOrDefault(Rank.FIFTH, 0) + "개");
+        System.out.println("4개 일치 (50,000원) - " + results.getOrDefault(Rank.FOURTH, 0) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + results.getOrDefault(Rank.THIRD, 0) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + results.getOrDefault(Rank.SECOND, 0) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + results.getOrDefault(Rank.FIRST, 0) + "개");
     }
 }
