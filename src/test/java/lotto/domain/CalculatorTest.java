ca40b3c77c85ec9a4122aeda8352d3617eebdf40
jaehyeon <jhjh0022@naver.com>
2022-11-15 01:23:08 +0900

    fix: 수익률 구하는 메서드 이름, 자료형 수정으로 인한 수정

--- src/test/java/lotto/domain/CalculatorTest.java
+++ src/test/java/lotto/domain/CalculatorTest.java
@@ -12,15 +12,16 @@ class CalculatorTest {
     final Calculator calculator = new Calculator();
 
     @Nested
-    class df {
+    class getyieldTest {
 
         @DisplayName("수익률 계산 -> 0 퍼센트일때")
         @Test
         void case1() {
             final List<Integer> totalReward = List.of(0);
             final int money = 1000;
+            final String answer = "0.0";
 
-            assertEquals(0, calculator.df(totalReward, money));
+            assertEquals(answer, calculator.getyield(totalReward, money));
         }
 
         @DisplayName("수익률 계산 -> 100퍼센트")
@@ -28,8 +29,9 @@ class CalculatorTest {
         void case2() {
             final List<Integer> totalReward = List.of(1000);
             final int money = 1000;
+            final String answer = "100.0";
 
-            assertEquals(100, calculator.df(totalReward, money));
+            assertEquals(answer, calculator.getyield(totalReward, money));
         }
     }
 
