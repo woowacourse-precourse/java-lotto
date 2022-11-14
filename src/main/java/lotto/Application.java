9ab16490ccc31f956b8a1a09027e992c98ffae52
seoeunbae <enyoupark@gmail.com>
2022-11-14 20:15:30 +0900

    feat : 출력 틀 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,19 +1,67 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static Game gameData;
+
+    static List<Lotto> lottos;
+    final static int LOTTO_PRICE = 1000;
+
+    final static int MIN = 1;
+
+    final static int MAX = 45;
+
+    final static int NUMBER_SIZE = 6;
     public static void main(String[] args) {
+        saveInput();
+        gameStart();
         // TODO: 프로그램 구현
     }
 
-    public void gameStart(){
+    public static void gameStart(){
         //TODO: 게임 시작 구현
+        int gameTurn = gameData.getLottoCount();
+        while(gameTurn > 0){
+
+
+            gameTurn--;
+        }
     }
 
-    public void saveInput(){
-        //TODO: 입력값 객체에 저장 구현
+    public static void saveInput(){
+        System.out.println("구입금액을 입력해 주세요.");
+        Integer money = Integer.valueOf(Console.readLine());
+        gameData.changeLottoCount(money / LOTTO_PRICE);
+        System.out.println(gameData.getLottoCount()+"개를 구매했습니다.");
+
+        publishLotto();
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] winningNumbers = Console.readLine().split(",");
+        List<Integer> winning = new ArrayList<>();
+        for(String number : winningNumbers){
+            winning.add(Integer.parseInt(number));
+        }
+
+        gameData.changeWinningNumbers(winning);
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        gameData.changeBonusNumber(Integer.parseInt(Console.readLine()));
     }
 
-    public void publishLotto(){
+    public static void publishLotto(){
+        for(int i=1; i <= gameData.getLottoCount() ; i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN, MAX, NUMBER_SIZE);
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+
+            System.out.println(numbers);
+        }
         //TODO: 로또 발행 구현
     }
 
--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,8 +1,12 @@
 package lotto;
 
+import java.util.List;
+
 public class Game {
     private int bonusNumber;
     private int lottoCount;
+
+    private List<Integer> winningNumbers;
 
 
     public int getBonusNumber(){
@@ -13,11 +17,19 @@ public class Game {
         return this.lottoCount;
     }
 
+    public List<Integer> getWinningNumbers(){
+        return this.winningNumbers;
+    }
+
     public void changeBonusNumber(int bonusNumber){
         this.bonusNumber = bonusNumber;
     }
 
     public void changeLottoCount(int lottoCount){
         this.lottoCount = lottoCount;
+    }
+
+    public void changeWinningNumbers(List<Integer> winningNumbers){
+        this.winningNumbers = winningNumbers;
     }
 }
