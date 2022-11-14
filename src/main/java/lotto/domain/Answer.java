b03d3f8e8b12f4ea5b726dffe630778ba704d5e4
eyeofsol <kingjihong@naver.com>
2022-11-15 05:44:56 +0900

    feat(Answer): 정답 로또 번호 입력 클래스 구현
    정답 로또 번호를 입력받아 유효성 검사와 ArrayList로 만드는 클래스를 구현했습니다.
    예외검사는 모두 4가지로 생성자와 함께 private 메서드로 만들었습니다.
    테스트는 생성자를 통해 진행하면 되겠습니다.
    
    1. 쉼표로 구분했을 때 길이가 6인지
    2. 숫자가 맞는지 (null도 예외처리)
    3. 모두 다른 숫자인지
    4. 1부터 45의 범위가 맞는지
    5. 문자열을 쉼표를 기준으로 어레이리스트로 만들어주는 기능

---
+++ src/main/java/lotto/domain/Answer.java
@@ -0,0 +1,88 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Answer {
+    private ArrayList<Integer> answer;
+    private int bonus;
+
+    public Answer(String answerInput) {
+        this.answer = createAnswer(answerInput);
+    }
+
+    private ArrayList<Integer> createAnswer(String answerInput) {
+        String[] strings = answerInput.split(",");
+        ArrayList<Integer> answer;
+
+        validateLength(strings);
+        for (String str : strings) {
+            validDigit(str);
+        }
+        answer = stringToList(strings);
+        validateInRange(answer);
+        validateNonSame(answer);
+
+        return answer;
+    }
+
+    private void validateLength(String[] strings) {
+        if (strings.length != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validDigit(String str) {
+        char c;
+
+        if (str == null) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(i);
+            if (!Character.isDigit(c)) {
+                throw (new IllegalArgumentException("[ERROR]오류"));
+            }
+        }
+    }
+
+    private ArrayList<Integer> stringToList(String[] strings) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for (String str : strings) {
+            answer.add(Integer.parseInt(str));
+        }
+
+        return answer;
+    }
+
+    private void validateInRange(ArrayList<Integer> answer) {
+        for (int number : answer) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void validateInRange(int bonus) {
+        if (bonus < 1 || bonus > 45) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNonSame(ArrayList<Integer> answer) {
+        for (int number : answer) {
+            if (answer.indexOf(number) != answer.lastIndexOf(number)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public ArrayList<Integer> getAnswer() {
+        return answer;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+}
