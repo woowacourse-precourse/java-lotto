075496ffe2285b3e55606f1613ef44fff03bf6a2
baksohyeon <bori12370@gmail.com>
2022-11-14 21:34:26 +0900

    feat: add input view
    참고해야할 점: MVC패턴 지켜서 쓸 것 
    
    1. controller 코드 있으면 안됨
    2. 데이터를 받을 때는 컨트롤러를 통해서 받을 것

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,44 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class InputView {
+    private static final String INPUT_ASK_FOR_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_PURCHASE_COMPLETED_MESSAGE = "%d개를 구매했습니다.";
+    private static final String INPUT_DRAW_LOTTERY_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_ASK_FOR_A_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+
+    public static int getAmountInput() {
+        System.out.println(INPUT_ASK_FOR_PURCHASE_MESSAGE);
+        int input = getNumberInput();
+        System.out.printf(INPUT_PURCHASE_COMPLETED_MESSAGE, input % 1000);
+        return input;
+    }
+
+    public static void printTickets(List<List<Integer>> tickets) {
+        System.out.println(INPUT_DRAW_LOTTERY_NUMBERS);
+        tickets.forEach(System.out::println);
+    }
+
+    public static String drawLotteryNumbers() {
+        System.out.println(INPUT_DRAW_LOTTERY_NUMBERS);
+        return Console.readLine();
+    }
+
+    public static int drawBonusNumber() {
+        System.out.println(INPUT_ASK_FOR_A_BONUS_NUMBER);
+        return getNumberInput();
+    }
+
+
+    private static int getNumberInput() {
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+        }
+    }
 }
