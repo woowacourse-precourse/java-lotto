bd4e437719cc0f814920423cb8c412b8e4dc4449
akskflwn <akskflwn@gmail.com>
2022-11-15 00:58:11 +0900

    refactor : TestCode 리펙토링 - LottoNumberGeneratorTest : DisplayName 추가 - LottoTest : 에러 검증 메서드 마다 hasMessageContaining 메서드 추가 - ResultTest : given,when,then으로 코드분리

--- src/test/java/lotto/domain/LottoNumberGeneratorTest.java
+++ src/test/java/lotto/domain/LottoNumberGeneratorTest.java
@@ -4,10 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import lotto.domain.generator.LottoNumberGenerator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoNumberGeneratorTest {
 
+    @DisplayName("랜덤으로 생성된 로또의 번호는 항상 45보다 작거나 1보다 크다.")
     @Test
     void 랜덤_숫자_생성_범위_검증_테스트() {
         List<Integer> numbers = LottoNumberGenerator.generate();
@@ -18,11 +20,11 @@ public class LottoNumberGeneratorTest {
         }
     }
 
+    @DisplayName("랜덤으로 생성된 로또의 번호 갯수는 항상 6이다.")
     @Test
     void 랜덤_숫자_생성_크기_검증_테스트(){
         List<Integer> numbers = LottoNumberGenerator.generate();
         assertThat(numbers.size()).isEqualTo(6);
     }
-
 
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -22,21 +22,24 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR] 로또 번호의 갯수는 6개로 이루어져야만 합니다.");
     }
 
     @DisplayName("로또 번호의 개수가 6개보다 작으면 예외가 발생한다.")
     @Test
     void createLottoByLesserSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR] 로또 번호의 갯수는 6개로 이루어져야만 합니다.");
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR] 로또의 번호는 중첩될수 없습니다.");
     }
 
     @DisplayName("로또 번호가 45보다 크거나 1보다 작으면 예외가 발생한다.")
@@ -44,7 +47,8 @@ class LottoTest {
     @ValueSource(ints = {-1, 0, 46, 99})
     void createLottoByGreaterThanMaxOrLesserThanMin(int input) {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, input)))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR] 로또 번호는 45보다 크거나 1보다 작을수 없습니다.");
     }
 
 }
