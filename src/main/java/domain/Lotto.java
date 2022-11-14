c7beba0d54c4a5dafb6232de7ebee94e51265d4f
juy4556 <kws05050@naver.com>
2022-11-15 01:23:20 +0900

    feat: 로또 객체 내 각 번호마다 맞는지 여부 확인 메소드 생성

---
+++ src/main/java/domain/Lotto.java
@@ -0,0 +1,38 @@
+package domain;
+
+import java.util.*;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<Integer> countMatch(Set<Integer> winningLottoNumbers, int bonusNumber) {
+        List<Integer> counts = new ArrayList<>();
+        int matchCount = 0;
+        int bonusIsMatched = 0;
+        for (int number : numbers) {
+            matchCount += checkIsMatched(number, winningLottoNumbers);
+            bonusIsMatched += checkBonusIsMatched(number, bonusNumber);
+        }
+        counts.add(matchCount);
+        counts.add(bonusIsMatched);
+        return counts;
+    }
+
+    private int checkIsMatched(int number, Set<Integer> winningLottoNumbers) {
+        if (winningLottoNumbers.contains(number)) {
+            return 1;
+        }
+        return 0;
+    }
+}
