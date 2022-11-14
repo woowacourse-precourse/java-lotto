0e3fd16229d9b258dbb0a2e5f3a2d46cb637e2f6
madlimeus <108660314+madlimeus@users.noreply.github.com>
2022-11-15 03:40:33 +0900

    add Function
    Set my Lotto number

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,16 @@
 package lotto;
 
 public class Application {
+    public static ArrayList<List<Integer>> GET_MY_LOTTO_NUMBER(int Number_Of_Lotto){
+        ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        for (int i = 0; i < Number_Of_Lotto; i++){
+            List<Integer> Tmp_Result = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            Collections.sort(Tmp_Result);
+            result.add(Tmp_Result);
+        }
+        return result;
+    }
     public static boolean CHECK_PAY_ERROR(String Input){
         boolean result = false;
         if (!Pattern.matches("^[0-9]+", Input)) {
@@ -22,8 +32,19 @@ public class Application {
         }
         return Integer.parseInt(result);
     }
+    public static ArrayList<List<Integer>> PRINT_MY_LOTTO_NUMBER(int Amount){
+        int Number_Of_Lotto = Amount / 1000;
+        ArrayList<List<Integer>> My_Lotto_Number = new ArrayList<List<Integer>>();
+        System.out.println(String.format("%d개를 구매했습니다.", Number_Of_Lotto));
+        My_Lotto_Number = GET_MY_LOTTO_NUMBER(Number_Of_Lotto);
+        for(int i = 0; i < Number_Of_Lotto; i++){
+            System.out.println(My_Lotto_Number.get(i).toString());
+        }
+        return My_Lotto_Number;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
             int Amount = PRINT_REQUEST_PAY();
+            ArrayList<List<Integer>> My_Lotto_Number = PRINT_MY_LOTTO_NUMBER(Amount);
     }
 }
