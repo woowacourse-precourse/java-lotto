3de306904959d2152a1fdd5894a7467d4aca6cfe
LeeJejune <leejejune0625@gmail.com>
2022-11-14 21:38:29 +0900

    feat: 로또 금액 검증 로직 추가

--- src/main/java/lotto/domain/LottoAmount.java
+++ src/main/java/lotto/domain/LottoAmount.java
@@ -7,6 +7,23 @@ public class LottoAmount {
     private final int amount;
 
     public LottoAmount(final int amount) {
+        validateAmount(amount);
         this.amount = amount;
+    }
+    private void validateAmount(int amount) {
+        validateMinAmount(amount);
+        validateDivisible(amount);
+    }
+
+    private void validateDivisible(int amount) {
+        if (amount % MIN_COST != 0) {
+            throw new IllegalArgumentException(String.format("[ERROR] 금액은 " + MIN_COST + " 이상이어야 합니다."));
+        }
+    }
+
+    private void validateMinAmount(int amount) {
+        if (amount < MIN_COST) {
+            throw new IllegalArgumentException(String.format("[ERROR] 금액은 " + MIN_COST + " 이상이어야 합니다."));
+        }
     }
 }
