0d1d5fce6a3a8fb95b3308c64606e0f95cd01878
Ridealist <cjsn92@gmail.com>
2022-11-15 04:35:53 +0900

    feat(Controller): 모델과 뷰 클래스들 사이를 조정하는 컨트롤러 클래스 기능 구현

---
+++ src/main/java/lotto/Controller.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import java.util.List;
+import lotto.model.BonusNumber;
+import lotto.model.LottoTickets;
+import lotto.model.Purchase;
+import lotto.model.Result;
+import lotto.model.WinningNumbers;
+import lotto.view.InputView;
+import lotto.view.output.BuyListView;
+import lotto.view.output.SummaryView;
+
+public class Controller {
+
+    public static LottoTickets createLottoTickets() {
+        int inputAmount = InputView.getPurchaseAmount();
+        Purchase purchase = new Purchase(inputAmount);
+        LottoTickets lottoTickets = new LottoTickets(purchase);
+        BuyListView.show(lottoTickets.getTickets(), purchase.getAmount());
+        return lottoTickets;
+    }
+
+    public static WinningNumbers createWinningLotto() {
+        List<Integer> inputWinningNumbers = InputView.createWinningNumbers();
+        return new WinningNumbers(inputWinningNumbers);
+    }
+
+    public static BonusNumber createBonusNumber(WinningNumbers winningNumbers) {
+        int inputBonusNumber = InputView.createBonusNumber();
+        return new BonusNumber(inputBonusNumber, winningNumbers);
+    }
+
+    public static void createSummary(LottoTickets lottoTickets, WinningNumbers winningNumbers,
+                                     BonusNumber bonusNumber) {
+        Result result = new Result(lottoTickets, winningNumbers, bonusNumber);
+        SummaryView.showResult(result);
+        SummaryView.showEarning(result, lottoTickets.getTickets().size());
+    }
+}
