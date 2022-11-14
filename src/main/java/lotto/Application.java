05d8dd51bd3dcf0f8363eb5d539347a03caf8813
Jin409 <sualng123@gmail.com>
2022-11-14 15:41:09 +0800

    feat(Application): StoreController 를 호출하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.controller.StoreController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StoreController storeController = new StoreController();
+
+        storeController.run();
     }
 }
