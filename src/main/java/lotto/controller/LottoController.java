661dd2611b1fd18562929bcc8510a7ba635cc3ea
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-15 00:37:53 +0900

    feat(LottoController): 당첨번호 view에서 입력받아서 LottoMachine에 전달하여 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,14 +1,21 @@
 package lotto.controller;
 
+import lotto.domain.LottoMachine;
 import lotto.domain.User;
 import lotto.view.InputView;
 
 public class LottoController {
 
     User user = new User();
+    private LottoMachine lottoMachine;
 
     public void buyLotto() {
         int money = Integer.parseInt(InputView.inputMoney());
         user = User.initUserLotto(money);
+    }
+
+    public void initWinningLotto() {
+        String bonusNumber = InputView.inputWinningNumber();
+        lottoMachine = LottoMachine.initLottoMachineNumber(bonusNumber).get();
     }
 }
