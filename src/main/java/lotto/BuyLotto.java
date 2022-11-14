f0396a35c2b0955b71fb3ae836de19cab24dd8bb
Cho Changhun <cvcvcx@naver.com>
2022-11-15 08:54:04 +0900

    feat: (BuyLottoImpl:getLottoNumber,addLottos,buyLotto) 랜덤한 숫자 리스트를 구하고, 그 숫자를 ArrayList<Lotto>로 반환하는 기능 구현

--- src/main/java/lotto/BuyLotto.java
+++ src/main/java/lotto/BuyLotto.java
@@ -1,9 +1,12 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface BuyLotto {
     public int getLottoCount();
     public void validate();
     public ArrayList<Lotto> buyLotto();
+
+    public List<Integer> getLottoNumber();
 }
--- src/main/java/lotto/BuyLottoImpl.java
+++ src/main/java/lotto/BuyLottoImpl.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class BuyLottoImpl implements BuyLotto{
 
@@ -14,7 +17,6 @@ public class BuyLottoImpl implements BuyLotto{
     @Override
     public int getLottoCount() {
         validate();
-
         return lottoPrice/money;
     }
 
@@ -27,7 +29,23 @@ public class BuyLottoImpl implements BuyLotto{
 
     @Override
     public ArrayList<Lotto> buyLotto() {
-        return null;
+        int lottoCount = getLottoCount();
+        ArrayList<Lotto> lottos = new ArrayList<>();
+        addLottos(lottoCount, lottos);
+        return lottos;
+    }
+
+    private void addLottos(int lottoCount, ArrayList<Lotto> lottos) {
+        for(int i = 0; i< lottoCount; i++){
+            Lotto lotto = new Lotto(getLottoNumber());
+            lottos.add(lotto);
+        }
+    }
+
+    @Override
+    public List<Integer> getLottoNumber() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+        return numbers;
     }
 
 }
