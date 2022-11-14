94e3880b2030b43eada3805f85b1bc37fdc9325b
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-15 01:26:27 +0900

    feat: 맞은 개수를 리턴하는 기능 구현
    사용자가 입력한 로또의 숫자와 임의의 로또의 숫자를 비교합니다.
    비교한 숫자를 기준으로 등수를 리턴합니다
    등수를 담은 리스트를 리턴합니다.

--- src/main/java/game/Calculate.java
+++ src/main/java/game/Calculate.java
@@ -1,26 +1,52 @@
 package game;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Calculate {
     public int grade(List<Integer> lotto, List<Integer> input, int bonus) {
         int cnt = 0;
 
-        for (int i = 0; i < input.size(); i ++) {
+        for (int i = 0; i < input.size(); i++) {
             if (lotto.contains(input.get(i))) cnt++;
-            if (cnt==5 && lotto.contains(bonus)) return 30_000_000;
+            if (cnt == 5 && lotto.contains(bonus)) return 2;
         }
 
-        if (cnt==6) return 2_000_000_000;
-        if (cnt==5) return 1_500_000;
-        if (cnt==4) return 50_000;
-        if (cnt==3) return 5_000;
+        if (cnt == 6) return 1;
+        if (cnt == 5) return 3;
+        if (cnt == 4) return 4;
+        if (cnt == 3) return 5;
         return 0;
     }
 
+    public List<Integer> correctLottoCount(List<List<Integer>> lottos, List<Integer> usersLotto, int bonusNum) {
+        int[] arr = new int[usersLotto.size()];
 
-    // TODO 수익률을 계산하는 기능 구현
-    public int EarningRate(int lottoCnt, List<Integer> lotto, List<Integer> usersLotto) {
-        return 0;
+        for (int i = 0; i < lottos.size(); i++) {
+            List<Integer> lotto = lottos.get(i);
+            int num = grade(lotto, usersLotto, bonusNum);
+            arr[num]++;
+        }
+
+        List<Integer> result = Arrays.stream(arr).boxed().collect(Collectors.toList());
+
+        return result;
     }
+
+        // TODO 수익률을 계산하는 기능 구현
+//    public String earningRate (List<List<Integer>> lottos, List<Integer> usersLotto, int bonusNum) {
+//        Calculate calculate = new Calculate();
+//        int money = lottos.size() * 1000;
+//        int result= 0;
+//
+//
+//        for (int i = 0; i < lottos.size(); i++) {
+//            int profit = calculate.checkCount(lottos.get(i), usersLotto, bonusNum);
+//            result += profit;
+//        }
+//
+//        double profitRate = (double)((result - money) / money * 100);
+//        return String.format("%.1f", profitRate);
+//    }
 }
--- src/main/java/game/PlayGame.java
+++ src/main/java/game/PlayGame.java
@@ -4,7 +4,6 @@ import lotto.MakeLotto;
 import user.BuyLotto;
 import user.UserInput;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -25,7 +24,7 @@ public class PlayGame {
         int lottoCnt = buyLotto.countHowMany(money);
         List<List<Integer>> lottos = makeLotto.publishedLottos(lottoCnt);
 
-        System.out.println(lottoCnt+COUNT_LOTTO.getStatus());
+        System.out.println(lottoCnt + COUNT_LOTTO.getStatus());
 
         System.out.println(INPUT_LOTTO_NUMBER.getStatus());
         List<Integer> usersLotto = userInput.userLotto(sc.next());
@@ -33,10 +32,6 @@ public class PlayGame {
         System.out.println(INPUT_BONUS_NUMBER.getStatus());
         int bonusNum = userInput.getBonusNumber(sc.nextInt());
 
-        // TODO 수익률을 계산하는 기능 구현
-    }
-
-    public void print() {
-
+        calculate.correctLottoCount(lottos, usersLotto, bonusNum);
     }
 }
