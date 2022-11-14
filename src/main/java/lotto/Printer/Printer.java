beb964ec53e95e0cbf3be10ad9d60a7c1bc50629
채희찬 <eqholic1125@gmail.com>
2022-11-14 23:01:54 +0900

    feat: 보너스 번호 입력 요청 기능 구현

--- src/main/java/lotto/Printer/Printer.java
+++ src/main/java/lotto/Printer/Printer.java
@@ -4,6 +4,7 @@ public class Printer {
     private static final String PURCHASE_AMOUNT_REQUEST_MESSAGE = "구매금액을 입력해 주세요.";
     private static final String PURCHASE_MESSAGE = "%개를 구매했습니다.";
     private static final String LOTTO_NUMBER_REQUEST_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_REQUEST_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public void requestPurchaseAmount(){
         System.out.println(PURCHASE_AMOUNT_REQUEST_MESSAGE);
@@ -17,6 +18,7 @@ public class Printer {
         System.out.println(LOTTO_NUMBER_REQUEST_MESSAGE);
     }
 
-
-
+    public void requestBonusNumber(){
+        System.out.println(BONUS_NUMBER_REQUEST_MESSAGE);
+    }
 }
