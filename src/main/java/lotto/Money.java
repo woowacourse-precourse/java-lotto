6fd57f33eaf7cc889bd8d6de5ffdea8146d8857c
BaeDongHyeon <sasd730@gmail.com>
2022-11-14 11:14:50 +0900

    refactor: NumberValidate 적용

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -8,19 +8,9 @@ public class Money {
     }
 
     private Long getPurchaseAmount(String money) {
-        validate(money);
+        NumberValidate.digitValidate(money);
+        NumberValidate.divideValidate(money);
         return Long.parseLong(money);
-    }
-
-    private void validate(String money) {
-        if (!money.matches("-?\\d+")) {
-            throw new IllegalArgumentException("[ERROR] 정수만 입력 가능합니다.");
-        }
-
-        int digitPurchaseAmount = Integer.parseInt(money);
-        if (digitPurchaseAmount % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입금액은 1000단위로 입력 할 수 있습니다.");
-        }
     }
 
     public Long getMoney() {
