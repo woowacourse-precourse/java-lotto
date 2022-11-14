1acc0abaa482eea1fbbde798478c785d50527c3a
glass <glass7312@gmail.com>
2022-11-14 18:48:03 +0900

    refactor:메세지 기능 분리,숫자외의 다른 입력시 예외 기능 추가 View클래스에 프린트메서드 정리 정규식을 이용하여 숫자외의 다른 것을 입력받았을 때 예외처리 기능 추가

--- src/main/java/Controller/GameController.java
+++ src/main/java/Controller/GameController.java
@@ -6,25 +6,29 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static Controller.mainController.get_stactiscs;
+import static Controller.mainController.print_gain;
 import static Service.View.*;
-import static domain.Game.getLottoCount;
-import static domain.Game.winning_numbers;
+import static domain.Game.*;
 
 public class GameController {
 
     public static void start() {
         read_buyingLottoAmount();
-        System.out.print(getLottoCount() + "개를 구매했습니다.");
+        print_LottoNumber();
         LottoService.lottoSpawner(getLottoCount());
         LottoService.printLottoNumbers();
         changeToInt(read_WinningNumbers());
         read_bonusNumber();
-        System.out.println("당첨 통계");
+        print_Message_Stastistics();
         mainController.setGrades();
         mainController.grade_count();
-        mainController.print_Stactistics();
+        print_Stactistics();
+        mainController.set_totalGain();
+        print_totalGain();
 
     }
+
     public static void changeToInt(String numbers) {
         String[] str = numbers.split(",");
         List<String> number = List.of(str);
@@ -33,13 +37,9 @@ public class GameController {
         winning_numbers = newforms;
     }
 
-    public static List<Integer> ascendingSort(List<Integer> numbers) {
-
-        Collections.sort(numbers);
-
-        return numbers;
-    }
-    public static List<Integer> getWinning_numbers(){
+    public static List<Integer> getWinning_numbers() {
         return winning_numbers;
     }
+
+
 }
--- src/main/java/Service/View.java
+++ src/main/java/Service/View.java
@@ -3,10 +3,23 @@ package Service;
 import camp.nextstep.edu.missionutils.Console;
 import domain.Game;
 
+import static Controller.mainController.*;
+import static domain.Game.getLottoCount;
+
 public class View {
-    private static final String MESSAGE_WINNINGNUMBERS_INPUT_FORMAT="당첨번호를 입력해주세요.";
-    public static final String MESSAGE_BUYINGAMOUNT_INPUT_FORMAT="구입 금액을 입력해주세요";
-    public static final String MESSAGE_BONUSNUMBER_INPUT_FORMAT="보너스 번호를 입력해주세요";
+    private static final String MESSAGE_WINNINGNUMBERS_INPUT_FORMAT = "당첨번호를 입력해주세요.";
+    public static final String MESSAGE_BUYINGAMOUNT_INPUT_FORMAT = "구입 금액을 입력해주세요";
+    public static final String MESSAGE_BONUSNUMBER_INPUT_FORMAT = "보너스 번호를 입력해주세요";
+    public static final String MESSAGE_ERROR_FORMAT="[ERROR]";
+    public static final String MESSAGE_THREEHIT_FORMAT="3개 일치 (5,000원) - " +fifth_count + "개";
+    public static final String MESSAGE_FOURHIT_FORMAT="4개 일치 (50,000원) - "+ +fourth_count + "개";
+    public static final String MESSAGE_FIVEHIT_FORMAT="5개 일치 (1,500,000원) - "+ third_count + "개";
+    public static final String MESSAGE_FIVE_AND_BONUHIT_FORMAT="5개 일치, 보너스 볼 일치 (30,000,000원) - "+ second_count + "개";
+    public static final String MESSAGE_SIX_FORMAT="6개 일치 (2,000,000,000원) - "+ first_count + "개";
+    public static final String MESSAGE_LOTTONUMBER_FORMAT=getLottoCount() + "개를 구매했습니다." +"\n";
+    public static final String MESSAGE_STACTISTICS_FORMAT="당첨 통계";
+    public static final String MESSAGE_TOTALGAIN_FORMAT="총 수익률은 " + get_stactiscs() +"%입니다.";
+    public static final boolean
 
     public static String read_WinningNumbers() {
         System.out.print(MESSAGE_WINNINGNUMBERS_INPUT_FORMAT);
@@ -16,13 +29,33 @@ public class View {
 
     public static void read_buyingLottoAmount() {
         System.out.print(MESSAGE_BUYINGAMOUNT_INPUT_FORMAT + "\n");
-        Game.paymentAmount = Integer.parseInt(Console.readLine());
+        String Amount = Console.readLine();
+        if (!Amount.matches("[0-9]*$")) {
+            System.out.print(MESSAGE_ERROR_FORMAT);
+            return;
+        }
+         Game.paymentAmount = Integer.parseInt(Amount);
         if (Game.paymentAmount % Game.LottoPrice != 0) throw new IllegalArgumentException();
     }
-
     public static void read_bonusNumber() {
         System.out.print(MESSAGE_BONUSNUMBER_INPUT_FORMAT + "\n");
         Game.bonusNumber = Integer.parseInt(Console.readLine());
+    }
+    public static void print_Stactistics() {
+        System.out.println(MESSAGE_THREEHIT_FORMAT);
+        System.out.println( MESSAGE_FOURHIT_FORMAT);
+        System.out.println(MESSAGE_FIVE_AND_BONUHIT_FORMAT);
+        System.out.println(MESSAGE_FIVE_AND_BONUHIT_FORMAT);
+        System.out.println(MESSAGE_SIX_FORMAT);
+    }
+    public static void print_LottoNumber(){
+        System.out.print(MESSAGE_LOTTONUMBER_FORMAT);
+    }
+    public static void print_Message_Stastistics(){
+        System.out.print(MESSAGE_STACTISTICS_FORMAT);
+    }
+    public static void print_totalGain(){
+        System.out.print(MESSAGE_TOTALGAIN_FORMAT);
     }
 
 }
