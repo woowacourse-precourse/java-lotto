447dc8dd9476cb17f40879e2a5f877ab8a993ed1
kimJaeHyeon <kmr2644@gmail.com>
2022-11-14 11:19:25 +0900

    feat: 로또당첨 결과 반환 메서드 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -3,8 +3,10 @@ package lotto.service;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lotto.domain.Lotto;
 import lotto.domain.LottoRepository;
+import lotto.type.Prize;
 
 public class LottoService {
     private final LottoRepository lottoRepository;
@@ -21,6 +23,24 @@ public class LottoService {
             numbers.add(lottoRepository.save(lotto));
         }
         return numbers;
+    }
+
+    private Double calcYield(List<Prize> prizes, int money) {
+        Long prizeMoney = prizes.stream()
+                .map(Prize::getPrizeMoney)
+                .reduce(Long::sum)
+                .orElse(1L);
+        return (double) money / prizeMoney * 100;
+
+    }
+
+    private List<Prize> confirmWinning(List<Integer> winningNumbers, Integer bonusNumber) {
+        List<Lotto> lottos = lottoRepository.findAll();
+        return lottos.stream()
+                .map(lotto -> lotto.compareWinningNumbers(winningNumbers, bonusNumber))
+                .filter(prize -> !Prize.NOTING.equals(prize))
+                .collect(Collectors.toList());
+
     }
 
 }
