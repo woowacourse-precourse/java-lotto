190c97a928fcd654c02f206744aa6f8a6269a5e3
footdev <ekclstkfka5@gmail.com>
2022-11-14 15:53:08 +0900

    feat: 입력에 따른 예외처리 기능 작성
    입력값에 대한 유효성 검사를 하는 기능 구현함.

--- src/main/java/lotto/domain/Validation.java
+++ src/main/java/lotto/domain/Validation.java
@@ -1,27 +1,47 @@
 package lotto.domain;
 
+import java.util.StringTokenizer;
+
 public class Validation {
     private static final String ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final int LOTTO_MONEY = 1000;
+    private static final int LOTTO_NUMBER = 6;
+    private static final int LOTTO_MIN = 1;
+    private static final int LOTTO_MAX = 45;
 
     public static boolean validateInteger(String input) {
-        return false;
+        if (!input.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+        return true;
     }
 
-    public static boolean isStartWithZero(int input) {
-        return false;
+    public static boolean isStartWithZero(String input) {
+        if (input.charAt(0) == '0') {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+        return true;
     }
 
     public static boolean validateAmount(int input) {
-        return false;
+        if (input % LOTTO_MONEY != 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+        return true;
     }
 
-    public static boolean validateNumber(String input) {
-        return false;
+    public static boolean validateNumbers(int input) {
+        if(input != LOTTO_NUMBER) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+        return true;
     }
 
     public static boolean validateRange(int number) {
-        return false;
+        if(number < LOTTO_MIN || number > LOTTO_MAX) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+        return true;
     }
 
 }
