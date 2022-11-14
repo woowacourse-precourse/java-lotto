c1d983d90dbddc04ba9d996fb49eaf84c594144e
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-14 23:11:42 +0900

    feat: 구매한 모든 로또의 결과를 판별하는 기능

--- src/main/java/lotto/LottoContainer.java
+++ src/main/java/lotto/LottoContainer.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoContainer {
     private final List<Lotto> lottos;
@@ -15,6 +17,27 @@ public class LottoContainer {
         for (Lotto lotto: lottos) {
             lotto.printNumbers();
         }
+    }
+
+    public Map<LottoResult, Integer> MatchAll(List<Integer> normalNumbers, int bonusNumber) {
+        Map<LottoResult, Integer> matchMap = getNothingMatchResult();
+        for(Lotto lotto: lottos) {
+            LottoResult lottoResult = lotto.getResult(normalNumbers, bonusNumber);
+            if (matchMap.containsKey(lottoResult)) {
+                matchMap.put(lottoResult, matchMap.get(lottoResult) + 1);
+            }
+        }
+
+        return matchMap;
+    }
+
+    public Map<LottoResult, Integer> getNothingMatchResult() {
+        Map<LottoResult, Integer> matchMap = new HashMap<>();
+        for(LottoResult lottoResult: LottoResult.values()) {
+            matchMap.put(lottoResult, 0);
+        }
+
+        return matchMap;
     }
 
 }
