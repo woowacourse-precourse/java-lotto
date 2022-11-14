a9d7fee037b898998bc97e49b9d0096b7c914661
dydckd <toofriend09@naver.com>
2022-11-14 15:55:07 +0900

    feat : 로또 당첨 번호 입력 기능 출가
    - 로또 당첨 번호를 입력받는 메서드 추가
      * ,로 구분되지 않는 6개의 번호가 아니면 예외 발생
      * 중복 숫자 있을시 예외 발생

--- src/main/java/lotto/Buyer.java
+++ src/main/java/lotto/Buyer.java
@@ -1,8 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Buyer {
-    final int purchaseAmount;
+    int purchaseAmount;
     int lottoAmount;
+
+    List<Integer> winningNum;
+
+    public Buyer(){
+
+    }
 
     public Buyer(int purchaseAmount) {
         amountRange(purchaseAmount);
@@ -20,8 +32,39 @@ public class Buyer {
         System.out.println(lottoAmount + "개를 구매하셨습니다.");
     }
 
-
-    public void buyMessage(){
+    public int getPurchaseAmount(){
+        return purchaseAmount;
+    }
+    public static void buyMessage(){
         System.out.println("구입금액을 입력해 주세요.");
     }
+
+    public static void winningMessage(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public List<Integer> getWinningNum() {
+        winningNum = new ArrayList<>();
+        String input = Console.readLine();
+        StringTokenizer st = new StringTokenizer(input, ",");
+        while (st.hasMoreTokens()) {
+            winningNum.add(Integer.parseInt(st.nextToken()));
+        }
+        validate(winningNum);
+        validateNum(winningNum);
+        return winningNum;
+    }
+    private void validate(List<Integer> winningNum) {
+        if (winningNum.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개 이고 , 로 구분합니다.");
+        }
+    }
+
+    // TODO: 추가 기능 구현
+    private void validateNum(List<Integer> winningNum) {
+        if(winningNum.size() != winningNum.stream().distinct().count()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않습니다.");
+        }
+    }
+
 }
