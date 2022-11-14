d309d6a81aad1fdce9289f53aa8741bd921b9029
inhwanK <inhan1009@naver.com>
2022-11-14 20:17:51 +0900

    feat(Application): 금액 입력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,42 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
+        int amount = getPurchaseAmount();
+        System.out.println(amount);
+
+        // ~개를 구매했습니다.
+        /*
+        [1, 2, 3, 4, 5, 6]
+         */
+
+        // 당첨 번호를 입력해 주세요.
+        // 1,2,3,4,5,6
+
+        // 보너스 번호를 입력해 주세요
+        // 7
+
+        // 당첨 통계
+        // ---
+        /*
+         ~개 일치 ()
+         */
+
+        // 총 수익률은 62.5%입니다.
+
         // TODO: 프로그램 구현
+    }
+
+    private static int getPurchaseAmount() {
+        printlnPurchaseNoticeMessage();
+        String input = Console.readLine();
+        // calcLottoEach(input);
+        return Integer.parseInt(input);
+    }
+
+    private static void printlnPurchaseNoticeMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
     }
 }
