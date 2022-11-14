2ec5b5a2c16bb06389e3bbec1cc2d94d4c4e37e3
baksohyeon <bori12370@gmail.com>
2022-11-15 01:05:04 +0900

    refactor: add getter, setter about tickets
    + 변경한 로직에 맞게 게터세터를 사용한 테스트 코드 수정

--- src/main/java/lotto/service/LottoMachine/LotteryTicketsIssuance.java
+++ src/main/java/lotto/service/LottoMachine/LotteryTicketsIssuance.java
@@ -1,36 +1,55 @@
-package lotto.service.executeLotto;
+package lotto.service.LottoMachine;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.service.validation.AmountValidation;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class LotteryTicketsIssuance {
+public class LotteryTicketsIssuance extends IllegalArgumentException{
     private final static int COUNT_LOTTERY_NUMBER_NUMBERS = 6;
     private final static int MIN_LOTTO_NUMBER = 1;
     private final static int MAX_LOTTO_NUMBER = 45;
     private int amount;
+    private List<List<Integer>> tickets;
 
     public LotteryTicketsIssuance(int amount) {
-        this.amount = amount;
+        try {
+            if (AmountValidation.isValidAmount(amount)) {
+                this.amount = amount;
+            }
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
     }
 
-    public List<List<Integer>> getLotteries() {
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setTickets() {
         List<List<Integer>> issuedLotteryTickets = new ArrayList<List<Integer>>();
-        while (amount != 0) {
+        int ticketsCount = amount / 1000;
+        for (int i = 0; i < ticketsCount; i++) {
             List<Integer> newLottery = issueLotteryNumbers();
             issuedLotteryTickets.add(newLottery);
-            amount = amount - 1000;
         }
-        return issuedLotteryTickets;
+        this.tickets = issuedLotteryTickets;
     }
+
+    public List<List<Integer>> getTickets() {
+        return tickets;
+    }
+
+
+
 
     // 랜덤 숫자 배열 1개 뽑음
     private List<Integer> issueLotteryNumbers() {
         List<Integer> LotteryNumbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, COUNT_LOTTERY_NUMBER_NUMBERS);
         Collections.sort(LotteryNumbers);
-        
+
         return LotteryNumbers;
     }
 }
--- src/test/java/lotto/service/LottoMachine/LotteryTicketsIssuanceTest.java
+++ src/test/java/lotto/service/LottoMachine/LotteryTicketsIssuanceTest.java
@@ -1,4 +1,4 @@
-package lotto.service.executeLotto;
+package lotto.service.LottoMachine;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,19 +12,22 @@ public class LotteryTicketsIssuanceTest {
     @DisplayName("입력한 금액만큼 복권을 발급하는지 테스트 1")
     @Test
     void issueTest1() {
-        LotteryTicketsIssuance issue = new LotteryTicketsIssuance(11000);
-        List<List<Integer>> numbers = issue.getLotteries();
+        int AMOUNT = 11000;
+        LotteryTicketsIssuance issue = new LotteryTicketsIssuance(AMOUNT);
+        issue.setTickets();
+
         int EXPECTED_RESULT = 11;
-        assertThat(numbers.size()).isEqualTo(EXPECTED_RESULT);
+        assertThat(issue.getTickets().size()).isEqualTo(EXPECTED_RESULT);
     }
 
 
     @DisplayName("입력한 금액만큼 복권을 발급하는지 테스트 2")
     @Test
     void issueTest2() {
-        LotteryTicketsIssuance issue = new LotteryTicketsIssuance(5000);
-        List<List<Integer>> numbers = issue.getLotteries();
+        int AMOUNT = 5000;
+        LotteryTicketsIssuance issue = new LotteryTicketsIssuance(AMOUNT);
+        issue.setTickets();
         int EXPECTED_RESULT = 5;
-        assertThat(numbers.size()).isEqualTo(EXPECTED_RESULT);
+        assertThat(issue.getTickets().size()).isEqualTo(EXPECTED_RESULT);
     }
 }
