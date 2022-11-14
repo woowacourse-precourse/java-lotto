d48344bdce3a79e79b81ff24b6c62029f839a7a0
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-14 23:21:16 +0900

    Move : 이동 util 패키지로 InputCheckerTest

--- src/test/java/lotto/util/InputCheckerTest.java
+++ src/test/java/lotto/util/InputCheckerTest.java
@@ -1,13 +1,12 @@
-package lotto.domain;
+package lotto.util;
 
-import lotto.util.InputChecker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-class InputCheckerTest{
+class InputCheckerTest {
     InputChecker inputChecker;
 
     @BeforeEach
