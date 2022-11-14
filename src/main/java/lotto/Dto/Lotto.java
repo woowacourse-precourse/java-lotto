e9108e784bc8ab46786df19a0648a111cb6b856a
namtndus <rnrmf0721@gmail.com>
2022-11-14 22:05:03 +0900

    feat(Lotto): 매개변수가 String 형태인 생성자 구현 및, vaildate(String) 오버라이드 구현
    당첨번호를 저장할 Lotto 생성자를 구현하고, 그때 들어오는 매개변수가, 유효한 값이지 확인하는 함수 구현

--- src/main/java/lotto/Dto/Lotto.java
+++ src/main/java/lotto/Dto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.Dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -10,6 +11,16 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public Lotto(String numbers){
+        vaildate(numbers);
+        List<Integer> winningNumbers = new ArrayList<>();
+        String[] winningNum = numbers.split(",\\s");
+        for(String number : winningNum){
+            winningNumbers.add(Integer.parseInt(number));
+        }
+        this.numbers = winningNumbers;
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호의 길이가 6이 아닙니다.");
@@ -17,8 +28,14 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
-    public String toString(){
+    public void vaildate(String numbers){
+        String regularExpression = "^\\d{1,2},\\s\\d{1,2},\\s\\d{1,2},\\s\\d{1,2},\\s\\d{1,2},\\s\\d{1,2}";
+        if(!numbers.matches(regularExpression)){
+            throw new IllegalArgumentException("[ERROR] 형식에 맞추어 입력하세요.");
+        }
+    }
 
+    public String toString(){
         return numbers.toString();
     }
 }
