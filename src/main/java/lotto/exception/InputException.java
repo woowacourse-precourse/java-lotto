466c2f8532622d4757b2aa6d05f41d5944811a2c
JUHA <khabh@naver.com>
2022-11-14 14:56:32 +0900

    feat(InputException): 사용자의 입력값이 Integer 범위를 벗어난 경우 예외 처리

--- src/main/java/lotto/exception/InputException.java
+++ src/main/java/lotto/exception/InputException.java
@@ -2,13 +2,21 @@ package lotto.exception;
 
 public class InputException {
     private final static String NUMBER_REGEX = "[0-9]+";
+    private final static int NUMBER_OUT_OF_RANGE = 1935365522;
 
     public static int validateIsNumber(String number) {
-        if (!number.matches(NUMBER_REGEX)) {
-            System.out.print("[ERROR] 숫자만 입력할 수 있습니다.");
+        try {
+            if (!number.matches(NUMBER_REGEX)) {
+                System.out.print("[ERROR] 숫자만 입력할 수 있습니다.");
+                throw new IllegalArgumentException();
+            }
+
+            return Integer.parseInt(number);
+        } catch (Exception exception) {
+            if (exception.hashCode() == NUMBER_OUT_OF_RANGE) {
+                System.out.println("[ERROR] 범위를 벗어난 숫자입니다.");
+            }
             throw new IllegalArgumentException();
         }
-
-        return Integer.parseInt(number);
     }
 }
