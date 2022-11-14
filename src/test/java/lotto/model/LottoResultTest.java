0e121e2d4068d0ffa815fc4ef1da31295192a598
Jimin <thswlals0219@gmail.com>
2022-11-14 22:28:22 +0900

    test(LottoResult): 당첨 통계 계산 기능을 테스트하는 코드 추가

--- src/test/java/lotto/model/LottoResultTest.java
+++ src/test/java/lotto/model/LottoResultTest.java
@@ -12,33 +12,50 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoResultTest {
-    private LottoResult lottoResult = new LottoResult();
     private Lotto winningLotto;
-    private List<Lotto> userLottoTickets;
+    private final static int bonusNumber = 10;
+
+    private WinningLotto winning;
+    private LottoResult lottoResult;
 
     @BeforeEach
     void setUp() {
         winningLotto = new Lotto(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6)));
-        userLottoTickets = new ArrayList<>();
-        userLottoTickets.add(new Lotto(new ArrayList<>(Arrays.asList(1, 3, 5, 14, 22, 45))));
-        userLottoTickets.add(new Lotto(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 10, 11))));
+        winning = new WinningLotto(winningLotto, bonusNumber);
+        lottoResult = new LottoResult(bonusNumber);
     }
 
     @DisplayName("당첨 로또와 발행된 각 로또의 일치하는 개수를 계산한다.")
     @Test
     void 일치하는_숫자_갯수_계산_테스트() {
-        Lotto userLotto = userLottoTickets.get(0);
-        WinningScore winningScore = lottoResult.compareLottoNumber(userLotto.getNumbers(),
+        Lotto userLotto = new Lotto(List.of(1, 3, 5, 14, 22, 45));
+        WinningScore winningScore = lottoResult.compareNumber(userLotto.getNumbers(),
                 winningLotto.getNumbers());
         assertThat(winningScore.name()).isEqualTo(WinningScore.THREE.name());
     }
 
     @DisplayName("발행된 로또의 당첨 결과를 구한다.")
     @Test
-    void 당첨_결과_계산_테스트() {
-        lottoResult.computeWinningScore(userLottoTickets, winningLotto);
-        Map<WinningScore, Integer> result = lottoResult.getWinningScore();
-        List<Integer> scoreCount = new ArrayList<>(Arrays.asList(1, 1, 0, 0, 0));
+    void 당첨_결과_계산_테스트_1() {
+        Lotto userLotto = new Lotto(List.of(1, 2, 3, 4, 5, 10));
+        lottoResult.computeWinningScore(userLotto, winningLotto);
+        Map<WinningScore, Integer> result = lottoResult.getWinningScoreResult();
+        List<Integer> scoreCount = new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0));
+        int i = 0;
+        for (WinningScore score : WinningScore.values()) {
+            assertThat(result.get(score)).isEqualTo(scoreCount.get(i));
+            i += 1;
+        }
+    }
+
+    @DisplayName("발행된 로또의 당첨 결과를 구한다.")
+    @Test
+    void 당첨_결과_계산_테스트_2() {
+        lottoResult.computeWinningScore(new Lotto(List.of(1, 2, 3, 4, 5, 10)), winningLotto);
+        lottoResult.computeWinningScore(new Lotto(List.of(1, 2, 3, 4, 11, 13)), winningLotto);
+
+        Map<WinningScore, Integer> result = lottoResult.getWinningScoreResult();
+        List<Integer> scoreCount = new ArrayList<>(Arrays.asList(0, 1, 0, 1, 0));
         int i = 0;
         for (WinningScore score : WinningScore.values()) {
             assertThat(result.get(score)).isEqualTo(scoreCount.get(i));
