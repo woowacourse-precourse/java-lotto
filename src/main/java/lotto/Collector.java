7d3c1d909c2d5fed06b79e4671fe28834dd62989
zcae1020 <zcae1020@naver.com>
2022-11-14 11:12:07 +0900

    feat(lotto): collector class 스켈레톤 코드 작성

---
+++ src/main/java/lotto/Collector.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import java.util.List;
+
+public class Collector {
+
+    public List<Integer> collectResults(List<Lotto> lottoList) {
+        return null;
+    }
+
+    private void printResults(List<Integer> results) {
+
+    }
+}
