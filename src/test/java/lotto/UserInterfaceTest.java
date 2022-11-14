095c7c9c9545255d6866f56a4ece4840e88b2f38
recicoco068 <gkapf838@gmail.com>
2022-11-14 12:39:42 +0900

    test(UserInterfaceTest.java) : 당첨 번호를 입력 받는 기능 테스트

--- src/test/java/lotto/UserInterfaceTest.java
+++ src/test/java/lotto/UserInterfaceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -28,5 +29,26 @@ class UserInterfaceTest {
         System.setIn(generateUserInput("123456789"));
         int money = userInterface.inputPaidMoney();
         assertThat(money).isEqualTo(123456789);
+    }
+
+    @DisplayName("형식에 맞지 않는 당첨 번호 입력시 에러 발생")
+    @Test
+    void 형식에_맞지_않는_당첨_번호_에러_발생() {
+        System.setIn(generateUserInput("1,2,,3,4,5,6"));
+        assertThatThrownBy(userInterface::inputWinningNumbers)
+                .isInstanceOf(IllegalArgumentException.class);
+
+        System.setIn(generateUserInput("1-2-3 4-5-6"));
+        assertThatThrownBy(userInterface::inputWinningNumbers)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("형식에 맞는 당첨 번호 입력")
+    @Test
+    void 형식에_맞는_당첨_번호() {
+        System.setIn(generateUserInput("1,2,3,4,5,6"));
+        List<Integer> answer = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> winning = userInterface.inputWinningNumbers();
+        assertThat(winning).isEqualTo(answer);
     }
 }
