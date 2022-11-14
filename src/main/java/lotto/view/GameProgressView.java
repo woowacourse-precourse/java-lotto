bee61bb067fca4df156a29bbebd2d29879a64bc7
jjmin9797 <jjmin9797@naver.com>
2022-11-14 15:26:17 +0900

    feat(GameProgressView.java): 구매한 복권의 갯수를 출력해주는 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/view/GameProgressView.java
+++ src/main/java/lotto/view/GameProgressView.java
@@ -2,8 +2,13 @@ package lotto.view;
 
 public class GameProgressView {
     public static final String PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String COUNT_MESSAGE = "개를 구매했습니다.";
 
     public static void showPurchaseMessage() {
         System.out.println(PURCHASE_MESSAGE);
+    }
+
+    public static void showCountMessage(int count){
+        System.out.println(count+COUNT_MESSAGE);
     }
 }
