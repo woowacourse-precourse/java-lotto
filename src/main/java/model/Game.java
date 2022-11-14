76a80bf6b30fb52dd757c03489953066ef8043b1
MinSungKim <timkim01@naver.com>
2022-11-14 16:33:31 +0900

    데이터 전달 객체 Game 클래스 생성

---
+++ src/main/java/model/Game.java
@@ -0,0 +1,33 @@
+package model;
+
+import lotto.Lotto;
+
+public class Game {
+    private int purchaseNumber;
+    private int bonusNumber;
+    private Lotto lotto;
+
+    public int getPurchaseNumber() {
+        return purchaseNumber;
+    }
+
+    public void setPurchaseNumber(int purchaseNumber) {
+        this.purchaseNumber = purchaseNumber;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Lotto getLotto() {
+        return lotto;
+    }
+
+    public void setLotto(Lotto lotto) {
+        this.lotto = lotto;
+    }
+}
