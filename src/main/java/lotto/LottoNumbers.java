dd0c072f52741f054da168100368cc8891f000c6
ssssudan <kankgm0324@gmail.com>
2022-11-14 14:10:09 +0900

    <refactor>: 랜덤 로또 번호를 중첨 리스트로 변경해서 저장

--- src/main/java/lotto/LottoNumbers.java
+++ src/main/java/lotto/LottoNumbers.java
@@ -7,15 +7,21 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoNumbers {
-    public List<Integer> lottoNums;
-
-    public LottoNumbers() {
+    public List<List<Integer>> lottoNums;
+    private int buyAmount;
+    public LottoNumbers(int buyAmount) {
+        this.buyAmount = buyAmount;
         lottoNums = new ArrayList<>();
         setLottoNumbers();
     }
 
     private void setLottoNumbers() {
-        lottoNums = Randoms.pickUniqueNumbersInRange(1, 45, 6); //중복 숫자는 리스트에 저장 하지 않음.
-        Collections.sort(lottoNums);
+        List<Integer> lotto = new ArrayList<>();
+        for (int i=0; i<buyAmount; i++) {
+            lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6); //중복 숫자는 리스트에 저장 하지 않음.
+            Collections.sort(lotto);
+
+            lottoNums.add(lotto);
+        }
     }
 }
--- src/main/java/lotto/Progress.java
+++ src/main/java/lotto/Progress.java
@@ -1,19 +1,21 @@
 package lotto;
 
+import java.util.List;
+
 public class Progress {
     public static void run() {
         BuyAmount buyAmount = new BuyAmount(UserInput.inputMoney());
         int buyLotto = buyAmount.buyLotto;  // 로또 구매 수량
-        for(int i=0; i<buyLotto; i++) {
-            LottoNumbers lottoNumbers = new LottoNumbers();
-            System.out.println(lottoNumbers.lottoNums);
+        System.out.println(buyLotto+"개를 구매했습니다.");
+
+        LottoNumbers lottoNumbers = new LottoNumbers(buyLotto);
+        for (List<Integer> lotto: lottoNumbers.lottoNums) {
+            System.out.println(lotto);
         }
 
         WinningNumberToList winningNumberToList = new WinningNumberToList();
         winningNumberToList.setWinNumbers(UserInput.inputWinNumber()); // 당첨 번호 입력 후 리스트 저장
 
         int bonusNum = UserInput.inputBonusNumber();
-
-        System.out.println(winningNumberToList.winNumbers);
     }
 }
