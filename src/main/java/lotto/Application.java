38bb7a9f697cae2dbfe26256590a8cee2d92c94a
Daeeun Ko <7273322@naver.com>
2022-11-15 08:11:00 +0900

    refactor: support other view flow using by polymorphism

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,13 @@
 package lotto;
 
 
-import lotto.view.DefaultView;
+import lotto.view.View;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         ApplicationContext context = ApplicationContext.getContext();
-        DefaultView view = context.getView();
+        View view = context.getView();
 
         view.render();
     }
