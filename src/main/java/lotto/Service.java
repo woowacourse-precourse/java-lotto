1c0648c089d4d6ad1339712e57e87bc94f8941cc
ingpyo <juninpyo72@gmail.com>
2022-11-14 10:40:25 +0900

    feat:입력한 금액의 따라 로또 구입

--- src/main/java/lotto/Service.java
+++ src/main/java/lotto/Service.java
@@ -6,18 +6,16 @@ import java.util.List;
 import java.util.Map;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Service {
-
 	LottoException exception = new LottoException();
 	NumberCount numberCount = new NumberCount();
 	BonusLotto bonusLotto = new BonusLotto();
 	Lotto lotto;
-
 	List<Integer> lottoNumber = new ArrayList<>();
 	List<List<Integer>> purchaseLotto = new ArrayList<>();
 	Map<Integer, Integer> prize = new HashMap<>();
-
 	int count;
 	int money;
 
@@ -25,6 +23,12 @@ public class Service {
 		String inputmoney = Console.readLine();
 		money = (Integer.parseInt(inputmoney));
 		count = (Integer.parseInt(inputmoney) / Constant.PRICE);
+	}
+
+	public void lottoIssued() {
+		for (int i = 0; i < count; i++) {
+			purchaseLotto.add(Randoms.pickUniqueNumbersInRange(1, 45, Constant.LOTTO_LENGTH));
+		}
 	}
 
 }
