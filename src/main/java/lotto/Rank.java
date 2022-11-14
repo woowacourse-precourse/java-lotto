60a36b763720160c16b40c55ee2b604bc4f8f093
KyungKyu Park <rudrb0505@gmail.com>
2022-11-14 11:53:20 +0900

    refactor(Rank): enum에 맞춘 개수를 의미하는 요소 추가

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,18 +1,18 @@
 package lotto;
 
-import java.util.Arrays;
-
 public enum Rank {
-	FIRST(2000000000),
-	SECOND(30000000),
-	THIRD(1500000),
-	FOURTH(50000),
-	FIFTH(5000),
-	NOTHING(0);
+	FIRST(6, 2000000000),
+	SECOND(5, 30000000),
+	THIRD(4, 1500000),
+	FOURTH(3, 50000),
+	FIFTH(2, 5000),
+	NOTHING(0, 0);
 
+	private final int matchCount;
 	private final int money;
 
-	Rank(int money) {
+	Rank(int matchCount, int money) {
+		this.matchCount = matchCount;
 		this.money = money;
 	}
 
@@ -30,6 +30,10 @@ public enum Rank {
 			return FIFTH;
 		}
 		return NOTHING;
+	}
+
+	public int getMatchCount() {
+		return matchCount;
 	}
 
 	public int getMoney() {
