95330445e9c420bec9e4faf3469a77490e9b390d
JunginPark4364 <wjddlsee@gmail.com>
2022-11-14 22:37:04 +0900

    feat: 로또 번호 정렬을 처리하는 위치 수정, 범위체크 기능 추가
    이전에는 로또 번호를 랜덤으로 생성하는 메서드에서
    정렬도 처리했지만 이를 분리하여 생성자에 넣었습니다.
    로또 번호 범위체크 하는 기능을 추가했습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,25 +8,40 @@ public class Lotto {
     private static final int BEGIN_RANGE = 1;
     private static final int END_RANGE = 45;
     private static final int LOTTO_COUNT = 6;
+    private static final String SIZE_EXCEPTION_MESSAGE = "로또 번호는 중복없는 6개의 값이어야 합니다.";
+    private static final String NUMBER_FORM_EXCEPTION_MESSAGE = "로또 번호는 1 ~ 45 사이 숫자여야 합니다.";
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
+        validateDuplicateAndSize(numbers);
+        validateRange(numbers);
+    }
+
+    private void validateDuplicateAndSize(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
 
-        if (uniqueNumbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (uniqueNumbers.size() != LOTTO_COUNT) {
+            throw new IllegalArgumentException(SIZE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < BEGIN_RANGE || number > END_RANGE) {
+                throw new IllegalArgumentException(NUMBER_FORM_EXCEPTION_MESSAGE);
+            }
         }
     }
 
     public static Lotto createLottoNumbers() {
         List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(BEGIN_RANGE, END_RANGE, LOTTO_COUNT));
-        Collections.sort(numbers);
         return new Lotto(numbers);
     }
 
@@ -47,6 +62,7 @@ public class Lotto {
         index = sb.lastIndexOf(",");
         sb.delete(index, sb.length());
         sb.append("]");
+        sb.append("\n");
 
         return sb.toString();
     }
