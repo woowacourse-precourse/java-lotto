7773796122384153bb2872192e558b754560f6c2
5uhwann <5uhwann27@gmail.com>
2022-11-14 10:05:30 +0900

    test(로또회사): 총 당첨금 반환 테스트 작성

--- src/test/java/lotto/domain/lotto/lottocompany/LottoCompanyImplTest.java
+++ src/test/java/lotto/domain/lotto/lottocompany/LottoCompanyImplTest.java
@@ -53,4 +53,30 @@ class LottoCompanyImplTest {
         assertThat(winResult.get(fifth)).isEqualTo(1);
 
     }
+
+    @Test
+    @DisplayName("로또 회사는 총 당첨 금액을 계산해서 반환한다.")
+    void getTotalPrizeTest() {
+        //given
+        LottoCompany lottoCompany = new LottoCompanyImpl(new LottoMachineImpl());
+
+        List<String> winningNumbers = List.of("2", "5", "8", "15", "20", "44");
+        int bonusNumber = 14;
+
+        Lotto lotto1 = new Lotto(List.of(2, 5, 8, 15, 20, 44));
+        Lotto lotto2 = new Lotto(List.of(2, 5, 8, 14, 15, 44));
+        Lotto lotto3 = new Lotto(List.of(2, 5, 8, 16, 20, 44));
+        Lotto lotto4 = new Lotto(List.of(2, 5, 8, 15, 24, 45));
+        Lotto lotto5 = new Lotto(List.of(2, 5, 8, 12, 27, 41));
+
+        List<Lotto> publishedNLotto = List.of(lotto1, lotto2, lotto3, lotto4, lotto5);
+
+        lottoCompany.makeWinResult(publishedNLotto, winningNumbers, bonusNumber);
+
+        //when
+        int totalPrize = lottoCompany.getTotalPrize();
+
+        //then
+        assertThat(totalPrize).isEqualTo(2031555000);
+    }
 }
