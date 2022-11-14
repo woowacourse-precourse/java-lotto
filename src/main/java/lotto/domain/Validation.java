4de7973de8836d461fcffa4ee22d906f49629599
footdev <ekclstkfka5@gmail.com>
2022-11-14 15:37:45 +0900

    feat: Validation 기능 명세 작성
    입력값에 대한 유효성 검사를 하는데 사용되는 메소드 명세를 작성함.

--- src/main/java/lotto/domain/Validation.java
+++ src/main/java/lotto/domain/Validation.java
@@ -1,4 +1,27 @@
 package lotto.domain;
 
 public class Validation {
+    private static final String ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final int LOTTO_MONEY = 1000;
+
+    public static boolean validateInteger(String input) {
+        return false;
+    }
+
+    public static boolean isStartWithZero(int input) {
+        return false;
+    }
+
+    public static boolean validateAmount(int input) {
+        return false;
+    }
+
+    public static boolean validateNumber(String input) {
+        return false;
+    }
+
+    public static boolean validateRange(int number) {
+        return false;
+    }
+
 }
