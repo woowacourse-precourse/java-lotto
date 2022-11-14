2f9cff0448ae10a65e5828e9ff60519efd0783ab
YE-EUN <musiclove529@naver.com>
2022-11-15 04:21:42 +0900

    test(WinningNumbersTest) : 테스트 코드 추가

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -23,6 +25,15 @@ class WinningNumbersTest {
         List<Integer> result = List.of(1,2,3,4,5,6);
 
         assertThat(numbers).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123456", "0,1,2,3,4,5", "1,1,1,1,1,1"})
+    void 당첨_번호_입력_예외_처리(String input){
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(() -> new WinningNumbers()).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
