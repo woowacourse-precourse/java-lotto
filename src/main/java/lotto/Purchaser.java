39fecc40b18671e0f6fe0e3609554a22fac5c1e8
youngin <bluest12@naver.com>
2022-11-14 16:43:30 +0900

    feat:Create new Class
    유효성 검사 클래스 생성

--- src/main/java/lotto/Purchaser.java
+++ src/main/java/lotto/Purchaser.java
@@ -8,18 +8,15 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Purchaser {
     private int purchaseSum;
+    private int purchaseCount;
     public String inputPurchaseAmount() {
         System.out.println("구입금액을 1000원 단위로 입력해 주세요.");
         return Console.readLine();
     }
-    public int changePurchaseAmount() {
+    public int changeInt() {
         purchaseSum = Integer.parseInt(inputPurchaseAmount());
         return purchaseSum;
     }
-
-
-
-    public
 
     public List<Integer> inputLottoNum() {
 
