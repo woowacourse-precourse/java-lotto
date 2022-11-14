fdfb54f54c58bbf1cd7f4d929aff3d4a6763b5b7
famo1245 <kandallee38@gmail.com>
2022-11-14 17:56:28 +0900

    1000으로 나눠지는지 확인하는 함수 작성

--- src/main/java/lotto/LottoVendingMachine.java
+++ src/main/java/lotto/LottoVendingMachine.java
@@ -8,8 +8,7 @@ public class LottoVendingMachine {
     private final int PRICE_OF_LOTTO = 1000;    // 로또 한 장의 가격
 
     public ArrayList<Lotto> publishLotto(int moneyInput) {
-        checkMoneyInput(moneyInput);
-        int numberOfLotto = moneyInput / PRICE_OF_LOTTO;
+        int numberOfLotto = canDivideBy1000(moneyInput);
         System.out.println(numberOfLotto + "개를 구매했습니다.");
         ArrayList<Lotto> publishedLotto = new ArrayList<>();
 
@@ -22,9 +21,11 @@ public class LottoVendingMachine {
         return publishedLotto;
     }
 
-    // 입력된 금액이 1000원 단위인지 확인
-    private void checkMoneyInput(int moneyInput) {
+    // 1000으로 나눠지는지 확인
+    private int canDivideBy1000(int moneyInput) {
         if (moneyInput % PRICE_OF_LOTTO != 0)
             throw new IllegalArgumentException("[ERROR] 1000원으로 나누어 떨어지는 돈을 넣어주세요");
+
+        return moneyInput / PRICE_OF_LOTTO;
     }
 }
