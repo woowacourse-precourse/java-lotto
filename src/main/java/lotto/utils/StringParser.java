8ed8383073a6acaf56f78472382dc6b1729a9f7c
saint6839 <saint6839@gmail.com>
2022-11-14 17:33:22 +0900

    feat(utils) : 입력값을 리스트로 만들어 반환하는 StringParser 메서드 구현
    - 숫자로 변환 가능한 문자열인지 검증하는 메서드 추가

--- src/main/java/lotto/utils/StringParser.java
+++ src/main/java/lotto/utils/StringParser.java
@@ -2,10 +2,21 @@ package lotto.utils;
 
 import lotto.domain.Lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class StringParser {
     private StringParser() {}
+
+    public static List<Integer> parseInputToIntegerList(String input) {
+        List<Integer> numbers = new ArrayList<>();
+        String[] inputSplits = input.split(",");
+        for (String inputSplit : inputSplits) {
+            validateParseStringToInt(inputSplit);
+            numbers.add(Integer.parseInt(inputSplit));
+        }
+        return numbers;
+    }
 
     public static String parseLottoToString(Lotto lotto) {
         StringBuilder sb = new StringBuilder();
@@ -20,5 +31,13 @@ public final class StringParser {
         }
         sb.append("]");
         return sb.toString();
+    }
+
+    private static void validateParseStringToInt(String inputSplit) {
+        try {
+            Integer.parseInt(inputSplit);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자를 입력해야 합니다.");
+        }
     }
 }
