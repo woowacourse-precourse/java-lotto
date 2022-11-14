20bdd1f514c4d844f91e7f05b3bd4a7806ebb065
sonsoowon <yuwoo1018@gmail.com>
2022-11-15 01:54:14 +0900

    refactor(JackpotBonus): 필드 선언 순서 및 메서드명 변경

--- src/main/java/lotto/domain/JackpotBonus.java
+++ src/main/java/lotto/domain/JackpotBonus.java
@@ -1,27 +1,33 @@
 package lotto.domain;
 
-import lotto.valid.LottoValidator;
-
 import java.util.List;
 
 public class JackpotBonus {
+    private static final String BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE = "[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.";
     private final List<Integer> jackpotNumbers;
     private final int bonusNumber;
-    private static final String BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE = "[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.";
 
     public JackpotBonus(List<Integer> jackpotNumbers, int bonusNumber) {
         this.jackpotNumbers = jackpotNumbers;
 
-        checkDuplicateBonus(jackpotNumbers, bonusNumber);
+        validateBonusNumber(jackpotNumbers, bonusNumber);
         this.bonusNumber = bonusNumber;
     }
+
+    private static void validateBonusNumber(List<Integer> jackpotNumbers, int bonusNumber) throws IllegalArgumentException {
+        if (jackpotNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+
     public Rank getRankOf(Lotto lotto) {
         List<Integer> lottoNumbers = lotto.getNumbers();
         int jackpotCnt = countJackpot(lottoNumbers);
         boolean hasBonus = hasBonus(lottoNumbers);
 
-        return Rank.of(jackpotCnt, hasBonus);
+        return Rank.getInstanceOf(jackpotCnt, hasBonus);
     }
+
     private int countJackpot(List<Integer> lottoNumbers) {
         return (int) lottoNumbers.stream()
                 .filter(jackpotNumbers::contains)
@@ -31,12 +37,4 @@ public class JackpotBonus {
     private boolean hasBonus(List<Integer> lottoNumbers) {
         return lottoNumbers.contains(bonusNumber);
     }
-
-    private static void checkDuplicateBonus(List<Integer> jackpotNumbers, int bonusNumber) {
-        if (jackpotNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE);
-        }
-    }
-
-
 }
