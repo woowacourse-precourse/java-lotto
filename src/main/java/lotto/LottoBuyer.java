9a9c976e0bc77d66c32d0ef4e0c8d3a1fc1e3eb3
김동규 <2016025196@hanyang.ac.kr>
2022-11-14 18:31:29 +0900

    LottoBuyer 로또 구매 출력코드 추가

--- src/main/java/lotto/LottoBuyer.java
+++ src/main/java/lotto/LottoBuyer.java
@@ -14,7 +14,7 @@ import static constant.LottoNumber.REQUIRE_LOTTO_NUMBER_COUNT;
 
 public class LottoBuyer {
     private int money;
-    private final List<Lotto> buyersLotto;
+    private List<Lotto> buyersLotto;
     public LottoBuyer(){
         buyersLotto = new ArrayList<>();
     }
@@ -31,25 +31,33 @@ public class LottoBuyer {
     }
     public void buyLotto(){
         int lottoCount = money/LOTTO_PRICE.getValue();
+        System.out.println(lottoCount+"개를 구매했습니다.");
         for(int i=0; i<lottoCount; ++i){
             List<Integer> newLottoNumber = makeRandomLottoNumber();
             buyersLotto.add(new Lotto(newLottoNumber));
         }
+        printLottos(buyersLotto);
+    }
+    private void printLottos(List<Lotto> lottos){
+        for(Lotto lotto : lottos){
+            System.out.println(lotto.getLottoNumber());
+        }
     }
     private List<Integer> makeRandomLottoNumber(){
-        List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER.getValue(),
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER.getValue(),
                 MAX_LOTTO_NUMBER.getValue(), REQUIRE_LOTTO_NUMBER_COUNT.getValue());
+        List<Integer> lottoNumber = new ArrayList<>(numbers);
         lottoNumber.sort(null);
         return lottoNumber;
     }
     private void isNotMinus(Integer number){
         if(number<0){
-            throw new IllegalArgumentException("[ERROR] : money의 값이 음수입니다.");
+            throw new IllegalArgumentException("[ERROR] money의 값이 음수입니다.");
         }
     }
     private void canDivMoneyByPrice(Integer money){
         if(money % LOTTO_PRICE.getValue() != 0){
-            throw new IllegalArgumentException("[ERROR] : 돈이 "+LOTTO_PRICE.getValue()+"로 나누어 떨어지지 않습니다.");
+            throw new IllegalArgumentException("[ERROR] 돈이 "+LOTTO_PRICE.getValue()+"로 나누어 떨어지지 않습니다.");
         }
     }
 
