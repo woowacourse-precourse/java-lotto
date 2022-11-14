4db15202e573987b66d6b0ae1f14a292fde42a6c
yujaemin <woawoa0513@gmail.com>
2022-11-14 22:59:27 +0900

    feat: place 필드 추가
    등 수를 의미하는 place 필드 추가

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -1,18 +1,24 @@
 package lotto.domain;
 
 public enum Ranking {
-    FIRST("6개 일치", "2,000,000,000원"),
-    SECOND("5개 일치, 보너스 볼 일치", "30,000,000원"),
-    THIRD("5개 일치", "1,500,000원"),
-    FOURTH("4개 일치", "50,000원"),
-    FIFTH("3개 일치", "5,000원");
+    FIRST(1,"6개 일치", "2,000,000,000원"),
+    SECOND(2, "5개 일치, 보너스 볼 일치", "30,000,000원"),
+    THIRD(3, "5개 일치", "1,500,000원"),
+    FOURTH(4, "4개 일치", "50,000원"),
+    FIFTH(5, "3개 일치", "5,000원");
 
+    private final int place;
     private final String condition;
     private final String reward;
 
-    Ranking(String condition, String reward) {
+    Ranking(int place, String condition, String reward) {
+        this.place = place;
         this.condition = condition;
         this.reward = reward;
+    }
+
+    public int getPlace() {
+        return place;
     }
 
     public String getCondition() {
