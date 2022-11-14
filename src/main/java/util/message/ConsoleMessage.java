f21c7b62b2ef73ecd2a959c70be2fbf2423a4473
gyuturn <rbtjrrbals@gmail.com>
2022-11-14 22:39:40 +0900

    fix($view): 요구사항에 맞게 콘솔 출력 수
    요구사항에 대한 출력값을 따르기 위함
    
    당첨통계\n--- -> \n당첨통계\n---
    당첨 번호를 입력해 주세요.->
    당첨 번호를 입력해 주세요.
    보너스 번호를 입력해 주세요.->
    보너스 번호를 입력해 주세요.

--- src/main/java/util/message/ConsoleMessage.java
+++ src/main/java/util/message/ConsoleMessage.java
@@ -2,10 +2,10 @@ package util.message;
 
 public class ConsoleMessage {
     public static final String MESSAGE_PURCHASE_LOTTO = "구입금액을 입력해 주세요.";
-    public static final String WIN_LOTTO_NUMBER="당첨 번호를 입력해 주세요.";
-    public static final String BONUS_NUMBER="보너스 번호를 입력해 주세요.";
+    public static final String WIN_LOTTO_NUMBER="\n당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER="\n보너스 번호를 입력해 주세요.";
 
-    public static final String WINNING_STATIC_MESSAGE = "당첨통계\n---";
+    public static final String WINNING_STATIC_MESSAGE = "\n당첨 통계\n---";
     public static final String WINNING_STATIC = "%d개 일치%s (%s) - %d개\n";
     public static final String BONUS_BALL = ", 보너스 볼 일치";
     public static final String TOTAL_YIELD = "총 수익률은 %s%%입니다.";
