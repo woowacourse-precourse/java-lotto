47184ce616453d5e21e67f328e4bb792ca775945
Young37 <younghk37@gmail.com>
2022-11-15 02:18:06 +0900

    feat(ui): add the feature of printing lottos and printing

--- src/main/java/lotto/ui/UserInterFace.java
+++ src/main/java/lotto/ui/UserInterFace.java
@@ -1,5 +1,10 @@
 package lotto.ui;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserInterFace {
@@ -11,5 +16,28 @@ public class UserInterFace {
         } catch (NumberFormatException error) {
             throw new IllegalArgumentException();
         }
+    }
+    
+    public void printLottos(List<List<Integer>> lottos) {
+        print(lottos.size() + "개를 구매했습니다.\n");
+
+        for (int index = 0; index < lottos.size(); index++) {
+            List<Integer> lotto = lottos.get(index);
+            print(lotto.toString() + "\n");
+        }
+        
+        print("\n");
+    }
+    
+    static void print(String text) {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        try {
+            bufferedWriter.write(text);
+            bufferedWriter.flush();
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+
     }
 }
