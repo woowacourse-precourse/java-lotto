2a640fb642c6c3a5871e3cf99e4e8a8aec021e68
kdkdhoho <hkim4410@naver.com>
2022-11-14 11:49:01 +0900

    refactor(User): 함수 위치 수정 및 rankResult 변수명 수정

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -12,7 +12,7 @@ public class User {
     private List<Lotto> lottos;
     private double money;
     private double winningAmount;
-    private Map<Number, Integer> rankResult = new HashMap<>();
+    private Map<Number, Integer> ranks = new HashMap<>();
     private double yield;
 
     public User() {
@@ -23,11 +23,11 @@ public class User {
     }
 
     private void initRank() {
-        rankResult.put(Number.FIVE, 0);
-        rankResult.put(Number.FOUR, 0);
-        rankResult.put(Number.THREE, 0);
-        rankResult.put(Number.TWO, 0);
-        rankResult.put(Number.ONE, 0);
+        ranks.put(Number.FIVE, 0);
+        ranks.put(Number.FOUR, 0);
+        ranks.put(Number.THREE, 0);
+        ranks.put(Number.TWO, 0);
+        ranks.put(Number.ONE, 0);
     }
 
     public User(List<Lotto> lottos) {
@@ -59,11 +59,11 @@ public class User {
     }
 
     public Map<Number, Integer> getRank() {
-        return rankResult;
+        return ranks;
     }
 
     public void setRank(Map<Number, Integer> rank) {
-        this.rankResult = rank;
+        this.ranks = rank;
     }
 
     public double getYield() {
@@ -74,38 +74,38 @@ public class User {
         this.yield = yield;
     }
 
-    public double calculateYield() {
-        double yield = this.winningAmount / this.money;
-        yield *= 1000;
-        yield = Math.round(yield);
-        yield /= 10;
-        return yield;
-    }
-
     public int getLottoCount() {
         return this.lottos.size();
     }
 
     public void updateRankAndWinningAmount(int rank) {
         if (rank == Number.FIVE.getValue()) {
-            rankResult.put(Number.FIVE, rankResult.get(Number.FIVE) + 1);
+            ranks.put(Number.FIVE, ranks.get(Number.FIVE) + 1);
             this.winningAmount += Number.FIFTH_WINNING_AMOUNT.getValue();
         }
         if (rank == Number.FOUR.getValue()) {
-            rankResult.put(Number.FOUR, rankResult.get(Number.FOUR) + 1);
+            ranks.put(Number.FOUR, ranks.get(Number.FOUR) + 1);
             this.winningAmount += Number.FOURTH_WINNING_AMOUNT.getValue();
         }
         if (rank == Number.THREE.getValue()) {
-            rankResult.put(Number.THREE, rankResult.get(Number.THREE) + 1);
+            ranks.put(Number.THREE, ranks.get(Number.THREE) + 1);
             this.winningAmount += Number.THIRD_WINNING_AMOUNT.getValue();
         }
         if (rank == Number.TWO.getValue()) {
-            rankResult.put(Number.TWO, rankResult.get(Number.TWO) + 1);
+            ranks.put(Number.TWO, ranks.get(Number.TWO) + 1);
             this.winningAmount += Number.SECOND_WINNING_AMOUNT.getValue();
         }
         if (rank == Number.ONE.getValue()) {
-            rankResult.put(Number.ONE, rankResult.get(Number.ONE) + 1);
+            ranks.put(Number.ONE, ranks.get(Number.ONE) + 1);
             this.winningAmount += Number.FIRST_WINNING_AMOUNT.getValue();
         }
+    }
+
+    public double calculateYield() {
+        double yield = this.winningAmount / this.money;
+        yield *= 1000;
+        yield = Math.round(yield);
+        yield /= 10;
+        return yield;
     }
 }
