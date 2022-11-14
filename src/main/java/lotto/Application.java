25f81980b1cfb0369e3f0eec396c50bf719358bc
impati <impati0716@icloud.com>
2022-11-14 22:11:26 +0900

    feat(Application) : 구매 금액 변환 적용

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        programStart();
+        Integer cost = Convert.costConvert(Console.readLine());
+
+    }
+    public static void programStart(){
+        System.out.print("구입금액을 입력해주세요.");
     }
 }
