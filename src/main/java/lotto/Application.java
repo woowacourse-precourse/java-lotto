85b7bd4a094d5a3b80b819ebca2e3a7224491cd4
neppiness <0414kjh@naver.com>
2022-11-14 11:32:09 +0900

    feat: re-arrange purchase and print lottos functions (src/main/java/lotto)
    Re-arrange methods of main method implement function 1 and 2, to be corresponded with function list

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Collections;
 import java.util.ArrayList;
 import java.util.EnumMap;
 
@@ -15,7 +16,9 @@ public class Application {
     static NumberType[] numberType = new NumberType[46];
     static public Map<Places, Integer> noOfLottoWinAt = new EnumMap<>(Places.class);
     static public Map<Places, Long> prizesAt = new EnumMap<>(Places.class);
+    static List<List<Integer>> purchasedLottos = new ArrayList<>();
     static int noOfLottos;
+
 
     public static void initializeNumberType() {
         for (int number = 1; number <= 45; number++)
@@ -34,6 +37,49 @@ public class Application {
         prizesAt.put(Places.SECOND, 30_000_000L);
         prizesAt.put(Places.FIRST, 2_000_000_000L);
         prizesAt.put(Places.NONE, 0L);
+    }
+
+    public static void guidePurchasePriceFormat() {
+        System.out.println("구매 금액(원 단위)을 1000의 배수로 입력해 주세요(최대 20억원)");
+    }
+
+    public static void isDividedBy1000(Long purchasePrice) {
+        if (purchasePrice % 1000 != 0)
+            throw new IllegalArgumentException("금액을 1000원 단위로 입력해주세요.");
+    }
+
+    public static void isLargerThan2billion(Long purchasePrice) {
+        if (purchasePrice > 2_000_000_000)
+            throw new IllegalArgumentException("1등 당첨금보다 더 큰 금액으로 복권을 구매할 수 없습니다.");
+    }
+
+    public static void getNoOfLotto() {
+        guidePurchasePriceFormat();
+        String purchasePriceInput = Console.readLine();
+
+        isVoidInput(purchasePriceInput);
+        isInputNumber(purchasePriceInput);
+
+        long purchasePrice = Long.parseLong(purchasePriceInput);
+        isDividedBy1000(purchasePrice);
+        isLargerThan2billion(purchasePrice);
+
+        noOfLottos = (int)(purchasePrice / 1000);
+    }
+
+    public static void printNoOfPurchasedLottos() { System.out.printf("\n%d개를 구매했습니다.\n", noOfLottos); }
+
+    public static void generateLottoNumbers() {
+        for (int no = 0; no < noOfLottos; no++) {
+            List<Integer> pickedNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(pickedNumbers);
+            purchasedLottos.add(pickedNumbers);
+        }
+    }
+
+    public static void printLottoNumbers() {
+        for(List<Integer> lotto: purchasedLottos)
+            System.out.println(lotto);
     }
 
     public static void guideWinningNumberFormat() { System.out.print("당첨 번호 6개를 쉼표로 구분해 입력해주세요: "); }
@@ -140,43 +186,6 @@ public class Application {
         numberType[bonusNumber.get(0)] = NumberType.BONUS;
     }
 
-    public static void guidePurchasePriceFormat() {
-        System.out.println("\n구매 금액(원 단위)을 1000의 배수로 입력해 주세요(최대 20억원)");
-    }
-
-    public static void isDividedBy1000(Long purchasePrice) {
-        if (purchasePrice % 1000 != 0)
-            throw new IllegalArgumentException("금액을 1000원 단위로 입력해주세요.");
-    }
-
-    public static void isLargerThan2billion(Long purchasePrice) {
-        if (purchasePrice > 2_000_000_000)
-            throw new IllegalArgumentException("1등 당첨금보다 더 큰 금액으로 복권을 구매할 수 없습니다.");
-    }
-
-    public static void getNoOfLotto() {
-        guidePurchasePriceFormat();
-        String purchasePriceInput = Console.readLine();
-
-        isVoidInput(purchasePriceInput);
-        isInputNumber(purchasePriceInput);
-
-        long purchasePrice = Long.parseLong(purchasePriceInput);
-        isDividedBy1000(purchasePrice);
-        isLargerThan2billion(purchasePrice);
-
-        noOfLottos = (int)(purchasePrice / 1000);
-    }
-
-    public static void printNoOfPurchasedLottos() { System.out.printf("\n%d개를 구매했습니다.\n", noOfLottos); }
-
-    public static void generateLottoNumbers() {
-        for (int no = 0; no < noOfLottos; no++) {
-            List<Integer> pickedNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            new Lotto(pickedNumbers);
-        }
-    }
-
     public static void printLottoResults() {
         System.out.println("\n당첨 내역");
         System.out.println("--------");
@@ -214,14 +223,15 @@ public class Application {
         initializeNoOfLottoWinAt();
         initializePrizesAtPlaces();
 
-        getWinningNumbers();
-        getBonusNumber();
-
         getNoOfLotto();
         printNoOfPurchasedLottos();
         generateLottoNumbers();
+        printLottoNumbers();
 
-        printLottoResults();
-        printEarningsRate();
+//        getWinningNumbers();
+//        getBonusNumber();
+//
+//        printLottoResults();
+//        printEarningsRate();
     }
 }
