134f108ce585327d042d95fabd2da38dad431cb5
This2sho <zx00018@naver.com>
2022-11-14 21:34:46 +0900

    feat(Salesman): 로또 구입 금액이 1000원보다 작은 경우 예외 발생 로직 구현

--- src/main/java/lotto/domain/Salesman.java
+++ src/main/java/lotto/domain/Salesman.java
@@ -5,6 +5,13 @@ public class Salesman {
 	private final static int LOTTO_PRICE = 1000;
 
 	public int sellLotto(int purchaseBudget) {
+		validateLittleBudget(purchaseBudget);
 		return purchaseBudget / LOTTO_PRICE;
+	}
+
+	private void validateLittleBudget(int purchaseBudget) {
+		if (purchaseBudget < LOTTO_PRICE) {
+			throw new IllegalArgumentException();
+		}
 	}
 }
