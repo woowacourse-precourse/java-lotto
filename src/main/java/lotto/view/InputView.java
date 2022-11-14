9f184a6a97160ab7438b3888248cef8373c0c39f
keon0711 <fnc6488@gmail.com>
2022-11-15 01:27:12 +0900

    feat(lotto): input winning numbers separated by comma

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,8 +10,10 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
     private static final String NOT_NUMBER_ERROR = "[ERROR] 숫자 이외의 값을 입력할 수 없습니다.";
+    private static final String COMMA_DELIMITER = ",";
 
     public static int readBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER);
@@ -21,11 +23,19 @@ public class InputView {
         return convertStringToInteger(input);
     }
 
+    public static List<Integer> readWinningNumbers() {
+        System.out.println(INPUT_WINNING_NUMBERS);
+        String input = Console.readLine();
+        System.out.println();
+
+        return convertStringToIntegerList(input);
+    }
+
     public static int readAmountOfMoney() {
         System.out.println(INPUT_MONEY);
         String input = Console.readLine();
         System.out.println();
-        
+
         return convertStringToInteger(input);
     }
 
@@ -35,5 +45,11 @@ public class InputView {
         } catch (Exception e) {
             throw new IllegalArgumentException(NOT_NUMBER_ERROR);
         }
+    }
+
+    private static List<Integer> convertStringToIntegerList(String input) {
+        return Arrays.stream(input.split(COMMA_DELIMITER))
+                .map(InputView::convertStringToInteger)
+                .collect(Collectors.toList());
     }
 }
