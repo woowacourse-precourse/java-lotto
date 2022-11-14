82bd42df9ec345d7745bf62d164d97dd4b050acf
impati <impati0716@icloud.com>
2022-11-14 22:24:21 +0900

    fix(InputValidation) : , 구분 예외 수정

--- src/main/java/lotto/InputValidation.java
+++ src/main/java/lotto/InputValidation.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 public class InputValidation {
     public static final String NUMBER_FORMAT_MESSAGE = "[ERROR] 숫자만 입력해주세요.";
+    public static final String COMMA_FORMAT_MESSAGE = "[ERROR] 당첨 번호는 ','로 구분해주세요.";
     public static boolean numberFormatExceptionValidation(String stringNumber){
         try{
             Integer.parseInt(stringNumber);
@@ -15,9 +16,11 @@ public class InputValidation {
         return true;
     }
     public static void winningNumberValidation(String winningNumber){
-        Arrays.stream(winningNumber.split(","))
+        int numberSize = Arrays.stream(winningNumber.split(","))
                 .filter(element -> numberFormatExceptionValidation(element))
-                .map(element -> Integer.parseInt(element));
+                .map(element -> Integer.parseInt(element))
+                .collect(Collectors.toList()).size();
+        if(numberSize != LottoValidation.LOTTO_COUNT) throw new IllegalArgumentException(NUMBER_FORMAT_MESSAGE);
     }
 }
 
