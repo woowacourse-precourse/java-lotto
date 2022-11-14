de446652bb0aceba4b292027b4a982e3137688ff
jjmin9797 <jjmin9797@naver.com>
2022-11-14 15:47:37 +0900

    feat(WinningStatsView.java): 당첨 내용 출력 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/view/WinningStatsView.java
+++ src/main/java/lotto/view/WinningStatsView.java
@@ -1,9 +1,16 @@
 package lotto.view;
 
+import lotto.domain.Prize;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class WinningStatsView {
 
     public static final String WINNING_STATS_MESSAGE = "당첨 통계";
     public static final String CONTOUR = "---";
+    public static final String WINNING_CONTOUR = " - ";
+    public static final String WINNING_UNIT = "개";
 
     public static void showWinningStatsMessage() {
         System.out.println(WINNING_STATS_MESSAGE);
@@ -11,5 +18,13 @@ public class WinningStatsView {
 
     public static void showContour() {
         System.out.println(CONTOUR);
+    }
+
+    public static void showWinning(ArrayList<Integer> numberOfWinningTypes) {
+        System.out.println(Prize.FIFTH_PLACE.getMessage() + WINNING_CONTOUR + numberOfWinningTypes.get(4) + WINNING_UNIT);
+        System.out.println(Prize.FOURTH_PLACE.getMessage() + WINNING_CONTOUR + numberOfWinningTypes.get(3) + WINNING_UNIT);
+        System.out.println(Prize.THIRD_PLACE.getMessage() + WINNING_CONTOUR + numberOfWinningTypes.get(2) + WINNING_UNIT);
+        System.out.println(Prize.SECOND_PLACE.getMessage() + WINNING_CONTOUR + numberOfWinningTypes.get(1) + WINNING_UNIT);
+        System.out.println(Prize.FIRST_PLACE.getMessage() + WINNING_CONTOUR + numberOfWinningTypes.get(0) + WINNING_UNIT);
     }
 }
