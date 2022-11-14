6332b7acf641cb1c864527d2ba4c6444d81e2e8a
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-15 08:45:53 +0900

    feat(view): view를 상속한 inputView 구현

---
+++ src/main/java/lotto/layer/InputView.java
@@ -0,0 +1,30 @@
+package lotto.layer;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView extends View{
+    private static final String REQUEST_MONEY = "구입금액을 입력해 주세요.";
+    private static final String REQUEST_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public String requestMoney() {
+        print(REQUEST_MONEY);
+        String input = Console.readLine();
+        printEmptyLine();
+        return input;
+    }
+
+    public String requestLotto() {
+        print(REQUEST_LOTTO_NUMBER);
+        String input = Console.readLine();
+        printEmptyLine();
+        return input;
+    }
+
+    public String requestBonusNumber() {
+        print(REQUEST_BONUS_NUMBER);
+        String input = Console.readLine();
+        printEmptyLine();
+        return input;
+    }
+}
