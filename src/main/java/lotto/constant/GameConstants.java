9409f7afb188a3feadab271572a4ba699d836f80
CAEL0 <kjongmin26@gmail.com>
2022-11-14 20:04:10 +0900

    refactor(GameConstants): 일부 상수명 중 NUMBER -> NUMBERS로 변경

--- src/main/java/lotto/constant/GameConstants.java
+++ src/main/java/lotto/constant/GameConstants.java
@@ -4,8 +4,8 @@ public class GameConstants {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
 
-    public static final int LOTTO_NUMBER_LENGTH = 6;
-    public static final int WINNING_NUMBER_LENGTH = 6;
+    public static final int LOTTO_NUMBERS_LENGTH = 6;
+    public static final int WINNING_NUMBERS_LENGTH = 6;
 
     public static final int LOTTO_PRICE = 1_000;
 
