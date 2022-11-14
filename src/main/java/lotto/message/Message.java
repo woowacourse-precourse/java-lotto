7ce7de4348d7fdaf8ea27c429925d6bbedf123c3
kmw2378 <rlarla677@gmail.com>
2022-11-14 23:59:02 +0900

    [feat] Message - 에러 상황이 아닌 상황에서 출력해야할 문구들을 모두 상수로 저장해놓은 Message 클래스 생성

---
+++ src/main/java/lotto/message/Message.java
@@ -0,0 +1,11 @@
+package lotto.message;
+
+public class Message {
+
+    public static final String INPUT_AMOUNT_MESSAGE = "구매 금액을 입력해 주세요.";
+    public static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
+    public static final String INPUT_NUM_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUM_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public static final String WINNING_STATISTICS_MESSAGE = "당첨 통계";
+}
