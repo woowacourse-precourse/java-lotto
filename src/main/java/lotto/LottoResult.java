e3b66f7beee49bf18f809805838fdd2e33d4f7d2
vcho1958 <gusckd57@gmail.com>
2022-11-15 06:29:59 +0900

    feat(LottoResult): 결과를 반환하기위한 LottoResult 추가

---
+++ src/main/java/lotto/LottoResult.java
@@ -0,0 +1,17 @@
+package lotto;
+
+public enum LottoResult {
+
+
+    FIRST(2000000000L),
+    SECOND(30000000L),
+    THIRD(1500000L),
+    FOURTH(50000L),
+    FIFTH(5000L),
+    NOTHING(0L);
+    private Long compensation;
+
+    LottoResult(Long compensation) {
+        this.compensation = compensation;
+    }
+}
