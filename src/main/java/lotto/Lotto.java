b806886038a8f42f7569b809b591cfd905d30d43
YUSEONJUN <seonjun0906@gmail.com>
2022-11-14 23:19:07 +0900

    feat : 로또 당첨 번호를 가져오는 함수

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,27 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
-    private final List<Integer> numbers;
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+	public Lotto(List<Integer> numbers) {
+		validate(numbers);
+		reduplicationLotto(numbers);
+		this.numbers = numbers;
+	}
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
+	private void validate(List<Integer> numbers) {
+		if (numbers.size() != 6) {
+			throw new IllegalArgumentException();
+		}
+	}
 
-    // TODO: 추가 기능 구현
+	// TODO: 추가 기능 구현
+
+	public List<Integer> getLotto() {
+		return numbers;
+	}
+
 }
