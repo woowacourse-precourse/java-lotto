873d3fb5d55a39358ee37efbbee877291d2d5225
namtndus <rnrmf0721@gmail.com>
2022-11-14 19:08:31 +0900

    feat(InputOutputController): outputLottoSize함수의 기능 구현
    lotto의 갯수를 출력하고, 컴퓨터가 만든 로또를 갯수만큼 출력

--- src/main/java/lotto/Controller/InputOutputController.java
+++ src/main/java/lotto/Controller/InputOutputController.java
@@ -1,22 +1,31 @@
 package lotto.Controller;
 
+import lotto.Dto.Lotto;
 import lotto.Dto.Money;
 import lotto.Services.CalculatedService;
+import lotto.Services.LogicService;
 import lotto.View.InputView;
 import lotto.View.OutputView;
+
+import java.util.List;
 
 public class InputOutputController {
 
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
     private final CalculatedService calculatedService = new CalculatedService();
+    private final LogicService logicService = new LogicService();
     private int lottoSize = 0;
+    private List<Lotto> lottos;
 
-    public void inputMoneyOutputLottoSize(){
+    public void inputMoney(){
         Money money = inputView.printLotoBuyMoney();
         lottoSize = calculatedService.lottoSize(money);
-        outputView.printLottoSize(lottoSize);
     }
 
-
+    public void outputLottoSize(){
+        outputView.printLottoSize(lottoSize);
+        lottos = logicService.makeLotto(lottoSize);
+        outputView.printLottoNumbers(lottos);
+    }
 }
