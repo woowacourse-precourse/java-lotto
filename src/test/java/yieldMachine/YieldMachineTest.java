1652ccfa912eafa5af404df62cf71d577aa4f032
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-14 21:27:12 +0900

    test: 로또 하나에 대한 당첨 계산 메서드 test 작성

--- src/test/java/yieldMachine/YieldMachineTest.java
+++ src/test/java/yieldMachine/YieldMachineTest.java
@@ -27,8 +27,8 @@ public class YieldMachineTest {
         Method findCountMethod
                 = YieldMachine.class.getDeclaredMethod("findCountOfSameNumberWithWinningNumber", BoughtLotto.class);
         findCountMethod.setAccessible(true);
-        List<Integer> isValidate = (List<Integer>) findCountMethod.invoke(yieldMachine, lotto);
-        assertThat(isValidate).isEqualTo(List.of(6, 0));
+        List<Integer> MatchingNumber = (List<Integer>) findCountMethod.invoke(yieldMachine, lotto);
+        assertThat(MatchingNumber).isEqualTo(List.of(6, 0));
     }
 
     @DisplayName("당첨번호 로또와 내가 구매한 로또 번호를 비교해 일치된 횟수를 반환한다. 2등")
@@ -40,8 +40,8 @@ public class YieldMachineTest {
         Method findCountMethod
                 = YieldMachine.class.getDeclaredMethod("findCountOfSameNumberWithWinningNumber", BoughtLotto.class);
         findCountMethod.setAccessible(true);
-        List<Integer> isValidate = (List<Integer>) findCountMethod.invoke(yieldMachine, lotto);
-        assertThat(isValidate).isEqualTo(List.of(5, 1));
+        List<Integer> MatchingNumber = (List<Integer>) findCountMethod.invoke(yieldMachine, lotto);
+        assertThat(MatchingNumber).isEqualTo(List.of(5, 1));
     }
 
     @DisplayName("당첨번호 로또와 내가 구매한 로또 번호를 비교해 일치된 횟수를 반환한다. 2개 일치")
@@ -53,7 +53,35 @@ public class YieldMachineTest {
         Method findCountMethod
                 = YieldMachine.class.getDeclaredMethod("findCountOfSameNumberWithWinningNumber", BoughtLotto.class);
         findCountMethod.setAccessible(true);
-        List<Integer> isValidate = (List<Integer>) findCountMethod.invoke(yieldMachine, lotto);
-        assertThat(isValidate).isEqualTo(List.of(2, 1));
+        List<Integer> MatchingNumber = (List<Integer>) findCountMethod.invoke(yieldMachine, lotto);
+        assertThat(MatchingNumber).isEqualTo(List.of(2, 1));
+    }
+
+    @DisplayName("로또 등수에 따른 리스트 값 확인 - 4등")
+    @Test
+    void calculateOneLottoReward_Fourth()
+            throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        YieldMachine yieldMachine = new YieldMachine(List.of(1, 2, 3, 4, 7, 8), 11);
+        BoughtLotto lotto = new BoughtLotto(List.of(1, 2, 3, 4, 5, 11));
+        Method calculateOneLottoRewardMethod
+                = YieldMachine.class.getDeclaredMethod("calculateOneLottoReward", BoughtLotto.class);
+        calculateOneLottoRewardMethod.setAccessible(true);
+        calculateOneLottoRewardMethod.invoke(yieldMachine, lotto);
+        List<Integer> countOfReward = yieldMachine.getCountOfReward();
+        assertThat(countOfReward).isEqualTo(List.of(0, 1, 0, 0, 0));
+    }
+
+    @DisplayName("로또 등수에 따른 리스트 값 확인 - 0등")
+    @Test
+    void calculateOneLottoReward_noPrize()
+            throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        YieldMachine yieldMachine = new YieldMachine(List.of(1, 2, 3, 4, 7, 8), 11);
+        BoughtLotto lotto = new BoughtLotto(List.of(1, 2, 9, 10, 11, 13));
+        Method calculateOneLottoRewardMethod
+                = YieldMachine.class.getDeclaredMethod("calculateOneLottoReward", BoughtLotto.class);
+        calculateOneLottoRewardMethod.setAccessible(true);
+        calculateOneLottoRewardMethod.invoke(yieldMachine, lotto);
+        List<Integer> countOfReward = yieldMachine.getCountOfReward();
+        assertThat(countOfReward).isEqualTo(List.of(0, 0, 0, 0, 0));
     }
 }
