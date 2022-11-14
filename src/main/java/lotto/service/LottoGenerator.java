0cdc777a0c374ccbd65d6d049f2ae6cf2c94b506
Gibeom Nam <rlqja0523@naver.com>
2022-11-14 23:18:10 +0900

    feat: 구매 개수만큼 로또 생성

--- src/main/java/lotto/service/LottoGenerator.java
+++ src/main/java/lotto/service/LottoGenerator.java
@@ -1,11 +1,24 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.entity.Lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
+
+    public static List<Lotto> generateMyLotto(int purchaseCount) {
+        List<Lotto> myLotto = new ArrayList<>();
+
+        for (int i = 0; i < purchaseCount; i++) {
+            List<Integer> numbers = generateNumbers();
+            Lotto lotto = new Lotto(numbers);
+            myLotto.add(lotto);
+        }
+        return myLotto;
+    }
 
     private static List<Integer> generateNumbers() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
