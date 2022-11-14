36c75230b44a90aa2cf555c0b316ab88359da2b6
bellljun3395 <195850@jnu.ac.kr>
2022-11-14 15:13:39 +0900

    refactor(Lotto): 이동 lotto/domain/lotto

---
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -0,0 +1,33 @@
+package lotto.domain.lotto;
+
+import lotto.domain.lotto.util.Validate;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Lotto {
+
+    private final String WARM = "[ERROR] ";
+    private final String WARM_LOTTONUMBERS_SIZE = WARM + "로또번호는 6개이어야 합니다.";
+    private final int LOTTO_SIZE = 6;
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(WARM_LOTTONUMBERS_SIZE);
+        }
+    }
+
+    // TODO: 추가 기능 구현
+
+}
