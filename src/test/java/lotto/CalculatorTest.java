c5f485a86efe25eea78f76efbacf90a43ec92ebc
swonny <wonny921@gmail.com>
2022-11-14 16:03:53 +0900

    test(CalculatorTest): 수익률 계산 테스트 추가

--- src/test/java/lotto/CalculatorTest.java
+++ src/test/java/lotto/CalculatorTest.java
@@ -1,2 +1,22 @@
-package lotto;public class CalculatorTest {
+package lotto;
+
+import enumCollections.RankNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.EnumMap;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CalculatorTest {
+    @DisplayName("수익률 계산")
+    @Test
+    void winSecondPrizeTest() {
+        EnumMap<RankNumber, Integer> resultStatistics = Checker.initializeResultStatistics();
+        resultStatistics.put(RankNumber.FIFTH, 1);
+        float expected = 62.5f;
+
+        assertThat(Calculator.getYield(8000, resultStatistics))
+                .isEqualTo(expected);
+    }
 }
