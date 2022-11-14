574546efffc732bff2f82ff4be05b78d8aa0be97
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-15 06:04:38 +0900

    feat : 로또 당첨 번호를 쉼표와 함께 입력한다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,6 +4,19 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_TYPE_IS_NOT_PROPER = "[ERROR] 잘못된 입력 형식입니다.";
+    public static String inputWinningLotto() {
+        String lotto = Console.readLine();
+        validateLottoType(lotto);
+        return lotto;
+    }
+
+    private static void validateLottoType(String lotto) {
+        try {
+            String.format("%d,%d,%d,%d,%d,%d", lotto.charAt(0), lotto.charAt(2), lotto.charAt(4), lotto.charAt(6), lotto.charAt(8), lotto.charAt(10));
+        } catch (RuntimeException e) {
+            throw new RuntimeException(INPUT_TYPE_IS_NOT_PROPER);
+        }
+    }
 
     public static Integer inputMoney() {
         String money = Console.readLine();
