322983ed737a90b574ce1f9c5b46300ac11aff4a
MinSungKim <timkim01@naver.com>
2022-11-14 15:49:03 +0900

    feat: 입력되는 당첨번호에 중복값이 있는경우 유효성검사 기능 구현

--- src/main/java/Utils/UserInput.java
+++ src/main/java/Utils/UserInput.java
@@ -2,6 +2,8 @@ package Utils;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashSet;
+import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -38,9 +40,30 @@ public class UserInput {
         Pattern pattern = Pattern.compile(lottoNumbersPattern);
         Matcher matcher = pattern.matcher(userInput);
 
-        if(!matcher.matches()){
+        if (!matcher.matches()) {
             throw new IllegalArgumentException("[ERROR] 형식에 맞게 입력해주세요. 번호는 쉼표(,)를 기준으로 구분합니다.");
         }
+    }
+
+    // 입력되는 당첨번호에 중복값이 있는 경우 유효성 검사
+    private void validateDuplicateInputLottoNumbers(String userInput) {
+        int lottoNumbersCount = getLottoNumbersSet(userInput).size();
+
+        if (lottoNumbersCount != 6) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 중복되지 않게 입력해주세요.");
+        }
+    }
+
+    // 당첨번호를 Set 으로 반환
+    private HashSet<String> getLottoNumbersSet(String userInput) {
+        StringTokenizer st = new StringTokenizer(userInput, ",");
+        HashSet<String> lottoNumbers = new HashSet<>();
+
+        while (st.hasMoreTokens()) {
+            lottoNumbers.add(st.nextToken());
+        }
+
+        return lottoNumbers;
     }
 
 }
