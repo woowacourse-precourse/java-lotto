54c3aef3e14552ccccb09e2415aaa6f32e4b307d
Cho SangHyun <98shcho@naver.com>
2022-11-14 21:39:28 +0900

    refactor(LottoGenerator): 로또번호 생성 방법 변경
    테스트 통과를 위해 new를 사용하는 방법으로 변경

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -2,14 +2,14 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
     public static List<Lotto> generateLottos(int lottoQuantity) {
         List<Lotto> lottos = new ArrayList<>();
         for (int index = 0; index < lottoQuantity; index++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             sortNumbers(numbers);
             Lotto lotto = new Lotto(numbers);
             lottos.add(lotto);
@@ -18,6 +18,6 @@ public class LottoGenerator {
     }
 
     private static void sortNumbers(List<Integer> numbers) {
-        numbers.sort(Comparator.naturalOrder());
+        Collections.sort(numbers);
     }
 }
