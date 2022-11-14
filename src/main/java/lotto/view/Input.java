9ee95b8f6615f8b6d5e78bbbf8efee8b1d25f2de
ohhonggi <5ghdrl@naver.com>
2022-11-15 04:26:45 +0900

    feat($Input): Implement inputPurchasingAmount function
    This function receives a lotto purchase amount from the user.
    
    Closes feat_1

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,6 +1,19 @@
 package lotto.view;
 
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Amounts;
+
+import static lotto.util.ValidUtil.validAmountUnit;
+import static lotto.util.ValidUtil.validInputAmounts;
+
 public class Input {
+
+    public static Amounts inputPurchasingAmount() {
+        System.out.println("구입 금액을 입력해 주세요");
+        String input = validInputAmounts(Console.readLine());
+        int amount = validAmountUnit(Integer.parseInt(input));
+        return new Amounts(amount);
+    }
 
 }
