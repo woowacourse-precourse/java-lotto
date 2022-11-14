64325323c429bb287891ca738d0ef0c9c3cc11fe
Lee-Jiseung <ljss98@naver.com>
2022-11-14 23:45:56 +0900

    feat: 당첨 번호 입력 검증

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -1,10 +1,13 @@
 package lotto.model;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.validation.InputValidation;
 
 public class WinningNumber {
+    private final InputValidation inputValidation = new InputValidation();
 
     public void inputWinningNumbers() {
         String numbers = Console.readLine();
+        inputValidation.validateWinningNumbers(numbers);
     }
 }
--- src/main/java/lotto/validation/InputValidation.java
+++ src/main/java/lotto/validation/InputValidation.java
@@ -1,10 +1,35 @@
 package lotto.validation;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class InputValidation {
 
     public void validatePrice(String price) {
         if (!Pattern.matches("^[1-9][0-9]*0{3}$", price)) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateWinningNumbers(String numbers) {
+        if (!Pattern.matches("^[0-9]{1,2},[0-9]{1,2},[0-9]{1,2},[0-9]{1,2},[0-9]{1,2},[0-9]{1,2}$", numbers)) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Integer> temp = new ArrayList<>();
+        List<Integer> winningNumbers = Arrays.stream(numbers.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        for (Integer winningNumber : winningNumbers) {
+            if (temp.contains(winningNumber)) {
+                throw new IllegalArgumentException();
+            }
+            if (winningNumber > 45) {
+                throw new IllegalArgumentException();
+            }
+            temp.add(winningNumber);
         }
     }
 }
