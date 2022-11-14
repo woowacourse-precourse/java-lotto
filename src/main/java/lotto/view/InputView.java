19422fb5780ca556e65c0da2fd377e538f0b45d2
baksohyeon <bori12370@gmail.com>
2022-11-15 01:08:49 +0900

    refactor: amend getAmountInput, printTickets method

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,20 +6,21 @@ import java.util.List;
 
 public class InputView {
     private static final String INPUT_ASK_FOR_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String INPUT_PURCHASE_COMPLETED_MESSAGE = "%d개를 구매했습니다.";
+    private static final String INPUT_PURCHASE_COMPLETED_MESSAGE = "%d개를 구매했습니다.\n";
     private static final String INPUT_DRAW_LOTTERY_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_ASK_FOR_A_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
 
     public static int getAmountInput() {
         System.out.println(INPUT_ASK_FOR_PURCHASE_MESSAGE);
-        int input = getNumberInput();
-        System.out.printf(INPUT_PURCHASE_COMPLETED_MESSAGE, input % 1000);
-        return input;
+        return getNumberInput();
+    }
+
+    public static void printTicketCounts(int input) {
+        System.out.printf(INPUT_PURCHASE_COMPLETED_MESSAGE, input / 1000);
     }
 
     public static void printTickets(List<List<Integer>> tickets) {
-        System.out.println(INPUT_DRAW_LOTTERY_NUMBERS);
         tickets.forEach(System.out::println);
     }
 
