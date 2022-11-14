60f323c9fabae045de66e245d74db214daab63fe
YoosunMoon <muzikue@naver.com>
2022-11-14 20:23:01 +0900

    feat: 구입 금액 입력받고, 유효한 값인지 확인

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static final int LOTTO_PRICE = 1000;
+    private static final String MONEY_TYPE_ERROR_MESSAGE = "[ERROR] 숫자가 아닌 값을 입력하였습니다.";
+    private static final String MONEY_UNIT_ERROR_MESSAGE = "[ERROR] 구입 금액은 1000원 단위입니다.";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int inputMoney = 0;
+        String userInput = Console.readLine();
+
+        try {
+            inputMoney = Integer.parseInt(userInput);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(MONEY_TYPE_ERROR_MESSAGE);
+        }
+
+        if (inputMoney % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(MONEY_UNIT_ERROR_MESSAGE);
+        }
     }
 }
