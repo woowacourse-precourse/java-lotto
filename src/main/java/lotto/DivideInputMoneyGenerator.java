fef2c2c9a0058045cdcfe1a74038e3a67ec0004e
jonghun <jonghuncu@gmail.com>
2022-11-14 21:58:58 +0900

    feat(lotto) : 입력값 예외처리 (구매금액, 입력숫자) 기능

--- src/main/java/lotto/DivideInputMoneyGenerator.java
+++ src/main/java/lotto/DivideInputMoneyGenerator.java
@@ -1,10 +1,10 @@
 package lotto;
 
 public class DivideInputMoneyGenerator {
-    public static boolean divideInputMoney(int inputMoney) {
+    public static boolean divideInputMoney(int inputMoney) throws IllegalArgumentException{
         int remainder = inputMoney % 1000;
         if(remainder != 0) {
-            return false;
+            throw new IllegalArgumentException("[ERROR] 값을 잘못 입력하셨습니다.");
         }
         return true;
     }
