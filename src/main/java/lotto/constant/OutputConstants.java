979925e4947eb80308cd9b4faf17c366c773e848
CAEL0 <kjongmin26@gmail.com>
2022-11-14 19:31:45 +0900

    refactor(OutputConstants): 당첨 번호 관련 이름 복수형으로 변경

--- src/main/java/lotto/constant/OutputConstants.java
+++ src/main/java/lotto/constant/OutputConstants.java
@@ -5,7 +5,7 @@ public enum OutputConstants {
     INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     PURCHASE_QUANTITY("%d개를 구매했습니다."),
 
-    INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
 
     STATISTICS("당첨 통계\n---"),
