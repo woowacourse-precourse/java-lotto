c520c0a5c4ed5542c516efe8683f7494d91e3732
ddingmin <ddingminyt@gmail.com>
2022-11-14 12:21:10 +0900

    feat(draw): enum 구현

--- src/main/java/lotto/ui/Constants.java
+++ src/main/java/lotto/ui/Constants.java
@@ -1,5 +1,7 @@
 package lotto.ui;
 
+import java.util.Arrays;
+
 public final class Constants {
     public static final int LOTTO_SIZE = 6;
     public static final int PRINT_SIZE = 5;
@@ -12,4 +14,24 @@ public final class Constants {
     public static final Integer[] winningMoney = {5000, 50000, 1500000,
             30000000, 2000000000};
 
+    public enum winningMoneys {
+        THREE(3, 5000), FOUR(4, 50000),
+        FIVE(5, 1500000), FIVE_BONUS(15, 30000000),
+        SIX(6, 2000000000);
+        private final int gotMoney;
+        private final int correctNumber;
+
+        winningMoneys(int gotMoney, int correctNumber) {
+            this.gotMoney = gotMoney;
+            this.correctNumber = correctNumber;
+        }
+
+        public winningMoneys getRank(int count) {
+            return Arrays.stream(values())
+                    .filter(statistic -> statistic.correctNumber == count)
+                    .findFirst()
+                    .orElseThrow();
+
+        }
+    }
 }
