3c470b0a6d3724708842b79caf60568f2e3cbbbb
qkrdmswl <eunji303@naver.com>
2022-11-14 17:15:54 +0900

    refactor : seperate feature of makeLotto to utils

--- src/main/java/utils/LottoGenerator.java
+++ src/main/java/utils/LottoGenerator.java
@@ -1,2 +1,12 @@
-package utils;public class LottoGenerator {
+package utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoGenerator {
+
+    public static List<Integer> makeLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
 }
