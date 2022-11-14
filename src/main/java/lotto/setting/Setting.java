3cff460a2ef976ddc2607e730fc4f38da1b6d9ea
cocomozila <joseyun94@naver.com>
2022-11-14 20:19:01 +0900

    feat(Setting): 로또 최소 숫자, 최대 숫자, 로또 사이즈 추가

--- src/main/java/lotto/setting/Setting.java
+++ src/main/java/lotto/setting/Setting.java
@@ -4,4 +4,7 @@ public class Setting {
 
     public static final int MIN_PURCHASE_AMOUNT = 1_000;
     public static final int PURCHASE_AMOUNT_UNIT = 1_000;
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
 }
