51de87c0a6956a2513d140d4dae726552a97a36d
이건회 <rg970604@naver.com>
2022-11-15 07:51:40 +0900

    feat(ErrorMessage.java) : 에러메세지 상수분리

---
+++ src/main/java/lotto/util/errorMessage/ErrorMessage.java
@@ -0,0 +1,14 @@
+package lotto.util.errorMessage;
+
+public class ErrorMessage {
+
+    public static final String LOTTOSIZE_ERROR = "로또 번호의 숫자는 여섯개 여야 합니다.";
+    public static final String LOTTORANGE_ERROR = "로또 번호는 1~45 이내여야 합니다.";
+    public static final String LOTTONUMBER_DUPLICATE_ERROR = "로또 번호는 중복될 수 없습니다.";
+    public static final String PAYMENT_UNIT_ERROR = "천 원 단위로만 구매가 가능합니다.";
+    public static final String MININUM_PAYMENT = "1000원 이상부터 지불 가능합니다.";
+    public static final String ALLOW_ONLY_NUMERIC = "숫자만 입력해야 합니다.";
+    public static final String ALLOW_ONLY_FIXED_DELIMITER = "지정된 구분자가 포함되지 않았습니다.";
+    public static final String DUPLICATE_BONUSNUM = "보너스 숫자가 중복됩니다";
+
+}
