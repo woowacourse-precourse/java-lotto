7450bfdf585d758936fa349b1f97f58667689763
seonpilKim <ksp970306@gmail.com>
2022-11-14 18:10:51 +0900

    Refactor: 전역 메소드로 변경

--- src/main/java/lotto/domain/Seller.java
+++ src/main/java/lotto/domain/Seller.java
@@ -9,13 +9,13 @@ public class Seller {
 	private static final int LOTTO_PRICE = 1_000;
 	private static final String PURCHASE_ANNOUNCEMENT = "%s개를 구매했습니다.";
 
-	public List<Lotto> sellLottoTickets(int money) {
+	public static List<Lotto> sellLottoTickets(int money) {
 		validate(money);
 		System.out.println(String.format(PURCHASE_ANNOUNCEMENT, money / LOTTO_PRICE));
 		return Lotto.newInstances(money / LOTTO_PRICE);
 	}
 
-	private void validate(Integer money) {
+	private static void validate(Integer money) {
 		if (money < LOTTO_PRICE || money % LOTTO_PRICE != 0) {
 			throw new IllegalArgumentException(ErrorMessage.isNotUnitsOf1000());
 		}
