730f40dece9846006df83039a0493aa3bd4b0419
beombu <sunil1369@bible.ac.kr>
2022-11-15 07:04:46 +0900

    Feat(Output) : 당첨 통계 메시지 출력 기능

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,12 +1,20 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
 
 public class Output {
     public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String COUNT_LOTTO_MESSAGE = "%d개 구매했습니다.\n";
     public static final String INPUT_PRIZE_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.\n";
     public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    public static final String WIN_STATISTICS_MESSAGE = "당첨 통계\n---";
+    public static final String FIRST_STATISTICS_MESSAGE = "6개 일치 (2,000,000,000원) - %d개\n";
+    public static final String SECOND_STATISTICS_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n";
+    public static final String THIRD_STATISTICS_MESSAGE = "5개 일치 (1,500,000원) - %d개\n";
+    public static final String FOURTH_STATISTICS_MESSAGE = "4개 일치 (50,000원) - %d개\n";
+    public static final String FIFTH_STATISTICS_MESSAGE = "3개 일치 (5,000원) - %d개\n";
+
 
     public static void printInputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
@@ -26,5 +34,15 @@ public class Output {
 
     public static void printInputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+    }
+
+    public static void printWinStatistics(Map<String, Integer> winStatistics) {
+        System.out.println(WIN_STATISTICS_MESSAGE);
+
+        System.out.printf(FIRST_STATISTICS_MESSAGE, winStatistics.get("1"));
+        System.out.printf(SECOND_STATISTICS_MESSAGE, winStatistics.get("2"));
+        System.out.printf(THIRD_STATISTICS_MESSAGE, winStatistics.get("3"));
+        System.out.printf(FOURTH_STATISTICS_MESSAGE, winStatistics.get("4"));
+        System.out.printf(FIFTH_STATISTICS_MESSAGE, winStatistics.get("5"));
     }
 }
