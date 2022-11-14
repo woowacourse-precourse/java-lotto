cd599222a7d752856199242d632933ac08090f27
saturnone1 <saturnone1@naver.com>
2022-11-14 09:11:27 +0900

    feat(Lotto): LottoEnum을 이용하여 if, else if문 제거

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,7 +5,6 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -18,9 +17,11 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("로또 번호의 길이는 6으로 맞추어야 합니다.");
-        } else if (numbers.size() != numbers.stream().distinct().count()) {
+        }
+        if (numbers.size() != numbers.stream().distinct().count()) {
             throw new IllegalArgumentException("중복되는 숫자가 존재합니다.");
-        } else if (numbers.stream().anyMatch(s -> s>45 || s<1)) {
+        }
+        if (numbers.stream().anyMatch(s -> s>45 || s<1)) {
             throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
@@ -30,60 +31,57 @@ public class Lotto {
         int bonusNumber = Integer.parseInt(Console.readLine());
         if (bonusNumber>45 || bonusNumber<1) {
             throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-        } else if (numbers.contains(bonusNumber)) {
+        }
+        if (numbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("보너스 번호는 입력한 로또 넘버에 포함되지 않는 숫자여야 합니다.");
         }
 
         return bonusNumber;
     }
 
-    public Map<Integer,Integer> rankCount(List<List<Integer>> userLottoNumbers, int bonus) {
-        Map<Integer,Integer> rankNumbers = new HashMap<>();
-        rankNumbers.put(3,0);
-        rankNumbers.put(4,0);
-        rankNumbers.put(5,0);
-        rankNumbers.put(50,0);
-        rankNumbers.put(6,0);
-
+    public Map<String,Integer> rankCount(List<List<Integer>> userLottoNumbers, int bonus) {
+        Map<String,Integer> rankNumbers = new HashMap<>(createInitialRankNumbers());
         for (List<Integer> lottoNumber : userLottoNumbers) {
-            int commonNumbers = Long.valueOf(numbers.stream().filter(lottoNumber::contains).count()).intValue();
-            if (commonNumbers == 5 && lottoNumber.contains(bonus)) {
-                rankNumbers.put(50, rankNumbers.get(50)+1);
-            } else {
-                rankNumbers.put(commonNumbers, rankNumbers.get(commonNumbers)+1);
-            }
+            int commonNumbers = setCommonNumber(
+                    Long.valueOf(numbers.stream().filter(lottoNumber::contains).count()).intValue(),
+                    bonus,
+                    lottoNumber);
 
+            if (commonNumbers > 2) {
+                LottoEnum lottoEnum = LottoEnum.valueOfNumber(commonNumbers);
+                rankNumbers.put(lottoEnum.name(), rankNumbers.get(lottoEnum.name())+1);
+            }
         }
         return rankNumbers;
     }
 
-    public int viewResult(Map<Integer,Integer> result) {
-        System.out.println("당첨 통계\n---");
-        int sumMoney = 0;
-        for (Map.Entry<Integer,Integer> entry : result.entrySet()) {
-            if (entry.getKey() == 3) {
-                System.out.print("3개 일치 (5,000원)");
-                sumMoney += 3000;
-            } else if (entry.getKey() == 4) {
-                System.out.print("4개 일치 (50,000원)");
-                sumMoney += 50000;
-            } else if (entry.getKey() == 5) {
-                System.out.print("5개 일치 (1,500,000원)");
-                sumMoney += 1500000;
-            } else if (entry.getKey() == 50) {
-                System.out.print("5개 일치, 보너스 볼 일치 (30,000,000원)");
-                sumMoney += 30000000;
-            } else if (entry.getKey() == 6) {
-                System.out.print("6개 일치 (2,000,000,000원)");
-                sumMoney += 2000000000;
-            }
-
-            System.out.printf(" - %d개\n",entry.getValue());
+    public int setCommonNumber(int commonNumber, int bonus, List<Integer> lottoNumber) {
+        if (commonNumber == 5 && lottoNumber.contains(bonus)) {
+            return commonNumber + 2;
         }
-        return sumMoney;
+        return commonNumber;
     }
 
-    public String yield(int amount, int sumMoney) {
-        return String.format("%.1f", (float) sumMoney/(amount*1000));
+    public Map<String, Integer> createInitialRankNumbers() {
+        Map<String,Integer> rankNumbers = new HashMap<>();
+        rankNumbers.put("THREE",0);
+        rankNumbers.put("FOUR",0);
+        rankNumbers.put("FIVE",0);
+        rankNumbers.put("FIVE_B",0);
+        rankNumbers.put("SIX",0);
+        return rankNumbers;
+    }
+
+    public long viewResult(Map<String,Integer> result) {
+        for (Map.Entry<String,Integer> entry : result.entrySet()) {
+            LottoEnum lottoEnum = LottoEnum.valueOf(entry.getKey());
+            lottoEnum.calculate(entry.getValue());
+            System.out.printf("%s - %d개\n", lottoEnum.getMoneyString(), entry.getValue());
+        }
+        return LottoEnum.getSumMoney();
+    }
+
+    public String yield(int amount, long sumMoney) {
+        return String.format("%.1f", (float) sumMoney/(amount*10));
     }
 }
