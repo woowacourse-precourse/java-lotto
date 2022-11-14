5512b6b382b6a74b51dff351b2a226ad56618714
jmjmjames <chungjm0711@gmail.com>
2022-11-14 19:08:48 +0900

    feat(Lotto): LOTTO 단위 상수 추가 및 LottoNumbersValidator validate 제거
    Randoms.pickUniqueNumbersInRange() 자체로 uniqueNumbers를 보장하므로 제거한다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -10,11 +10,11 @@ public class Lotto {
     public static final int LOTTO_NUMBERS_SIZE = 6;
     public static final int LOWER_BOUND_NUMBER = 1;
     public static final int UPPER_BOUND_NUMBER = 45;
+    public static final int LOTTO_UNIT = 1000;
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        LottoNumbersValidator.validate(numbers);
         this.numbers = numbers.stream()
                 .sorted()
                 .collect(Collectors.toList());
