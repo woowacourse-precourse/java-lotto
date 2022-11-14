644265cf3139eefd03d4fdd6b85e8fee11ac56bd
YE-EUN <musiclove529@naver.com>
2022-11-15 05:13:45 +0900

    test(WinningNumbersTest) : 테스트 코드 추가

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -15,14 +15,14 @@ class WinningNumbersTest {
 
     @Test
     void 당첨_번호_입력_확인(){
-        String input = "1,2,3,4,5,6";
+        String input = "1,2,3,4,5,45\n7";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
         WinningNumbers winningNumbers = new WinningNumbers();
         List<Integer> numbers = winningNumbers.getNumbers();
 
-        List<Integer> result = List.of(1,2,3,4,5,6);
+        List<Integer> result = List.of(1,2,3,4,5,45);
 
         assertThat(numbers).isEqualTo(result);
     }
@@ -30,6 +30,29 @@ class WinningNumbersTest {
     @ParameterizedTest
     @ValueSource(strings = {"123456", "0,1,2,3,4,5", "1,1,1,1,1,1"})
     void 당첨_번호_입력_예외_처리(String input){
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(() -> new WinningNumbers()).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스_번호_입력_확인(){
+        String input = "1,2,3,4,5,6\n10";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        WinningNumbers winningNumbers = new WinningNumbers();
+        int bonus = winningNumbers.getBonus();
+
+        int result = 10;
+
+        assertThat(bonus).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6\na","1,2,3,4,5,6\n48", "1,2,3,4,5,6\n1"})
+    void 보너스_번호_예외_처리(String input){
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
