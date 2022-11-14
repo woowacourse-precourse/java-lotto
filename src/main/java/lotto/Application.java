4b7a3d876b8be47de9cadbc41d7be608a1200b2d
YooByeongIk <quddlr96@naver.com>
2022-11-14 22:15:54 +0900

    feat(lotto): add methods to compare with winning lotto with purchase lotto

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Application {
@@ -13,7 +14,7 @@ public class Application {
         Lotto winningLotto = new Lotto(winningLottoNumbers);
         List<Lotto> lottoList = createLottoList(purchasePrice);
         int bonusNumber = getBonusNumber(Console.readLine(), winningLottoNumbers);
-
+        HashMap<String, Integer> history = createHistory(lottoList, winningLotto, bonusNumber);
     }
 
     public static int getPurchasePrice(String input) {
@@ -74,5 +75,48 @@ public class Application {
             lottoList.add(Lotto.createPurchaseLotto());
         }
         return lottoList;
+    }
+
+    public static HashMap<String, Integer> createHistory(List<Lotto> lottoList, Lotto winningLotto, int bonusNumber) {
+        HashMap<String, Integer> historyMap = initResultMap();
+        for (Lotto purchaseLotto : lottoList) {
+            int count = compareWinningLottoWithPurchaseLotto(winningLotto, purchaseLotto);
+            if (count == 5 && checkBonusNumber(purchaseLotto, bonusNumber)) {
+                historyMap.put("5+", historyMap.get("5+") + 1);
+                continue;
+            }
+            if (count >= 3) {
+                String key = Integer.toString(count);
+                historyMap.put(key, historyMap.get(key) + 1);
+            }
+        }
+        return historyMap;
+    }
+
+    public static HashMap<String, Integer> initResultMap() {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("3", 0);
+        hashMap.put("4", 0);
+        hashMap.put("5", 0);
+        hashMap.put("5+", 0);
+        hashMap.put("6", 0);
+        return hashMap;
+    }
+
+    public static int compareWinningLottoWithPurchaseLotto(Lotto winningLotto, Lotto purchaseLotto) {
+        List<Integer> winningLottoNumbers = winningLotto.getNumbers();
+        List<Integer> purchaseLottoNumbers = purchaseLotto.getNumbers();
+        int count = 0;
+        for (Integer number : purchaseLottoNumbers) {
+            if (winningLottoNumbers.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static boolean checkBonusNumber(Lotto purchaseLotto, int bonusNumber) {
+        List<Integer> purchaseLottoNumbers = purchaseLotto.getNumbers();
+        return purchaseLottoNumbers.contains(bonusNumber);
     }
 }
