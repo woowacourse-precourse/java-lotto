780e18ec26def06e49b93ab512c1b41a564a46c8
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-14 20:13:29 +0900

    feat develop : Controller 당첨 통계 계산구현

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -9,6 +9,8 @@ public class Controller {
     private List<Lotto> lottoNumbers;
     private List<Integer> winningNumbers;
 
+    private Rank rank;
+
     public void makeLottoNumbers(int number) {
         for (int i = 0; i < number; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
@@ -16,8 +18,29 @@ public class Controller {
         }
     }
 
-    public void makeWinningNumbers(){
+    public void makeWinningNumbers() {
         winningNumbers = view.inputLottoNumber();
         winningNumbers.add(view.inputBonusNumber());
+    }
+
+    public void checkLottoNumbers() {
+        for (Lotto L : lottoNumbers) {
+            compareTwoNumbers(L.getNumbers());
+        }
+    }
+
+    public void compareTwoNumbers(List<Integer> lottoNumbers) {
+        int count = 0;
+        for (int i = 0; i < winningNumbers.size(); i++) {
+            if (i == winningNumbers.size() - 1 && count == 5) {
+                Rank.valueOf("FIFTH_BONUS").increaseCount();
+            } else if (lottoNumbers.contains(winningNumbers.get(i))) {
+                count++;
+            }
+        }
+        Rank[] values = Rank.values();
+        for(int i = 0; i< values.length; i++){
+            values[i].increaseCount();
+        }
     }
 }
