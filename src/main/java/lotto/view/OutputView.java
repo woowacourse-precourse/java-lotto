f576e8e6603257304c2d9c03fd1df29b99ca6dbf
3juhwan <13selfesteem91@naver.com>
2022-11-14 14:43:10 +0900

    feat(lotto):시스템 출력 관련 클래스 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -30,11 +30,11 @@ public class OutputView {
     public static void printStatic(List<Integer> numbers, double number) {
         System.out.println("당첨 통계\n" +
                 "---\n" +
-                "3개 일치 (5,000원) - " + numbers.get(0) + "개\n" +
-                "4개 일치 (50,000원) - " + numbers.get(1) + "개\n" +
-                "5개 일치 (1,500,000원) - " + numbers.get(2) + "개\n" +
-                "5개 일치, 보너스 볼 일치 (30,000,000원) - " + numbers.get(3) + "개\n" +
-                "6개 일치 (2,000,000,000원) - " + numbers.get(4) + "개\n" +
+                "3개 일치 (5,000원) - " + numbers.get(1) + "개\n" +
+                "4개 일치 (50,000원) - " + numbers.get(2) + "개\n" +
+                "5개 일치 (1,500,000원) - " + numbers.get(3) + "개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - " + numbers.get(4) + "개\n" +
+                "6개 일치 (2,000,000,000원) - " + numbers.get(5) + "개\n" +
                 "총 수익률은 " + number + "%입니다.");
     }
 
