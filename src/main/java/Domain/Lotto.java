d2c254c43defa2990158f7cc6993e2a80166f770
ljh <dusk5588@naver.com>
2022-11-15 03:49:07 +0900

    fix(Lotto) : 로또 번호 중복으로 인한 예외처리 추가

--- src/main/java/Domain/Lotto.java
+++ src/main/java/Domain/Lotto.java
@@ -1,5 +1,6 @@
 package Domain;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -12,8 +13,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또의 길이가 6이 아닙니다");
         }
+        HashSet<Integer> hashSet = new HashSet<>();
+        for(int element : numbers)
+            hashSet.add(element);
+        if(hashSet.size() != 6)
+            throw new IllegalArgumentException("중복된 숫자로 이루어진 로또는 존재 불가능합니다.");
     }
 
     public List<Integer> getLottoNumbers(){
