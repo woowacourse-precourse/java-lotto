01abad4c7686006e129575badbf19033ea4342a5
jay <jaewhan232@naver.com>
2022-11-14 17:37:39 +0900

    Implement : 당첨 로또 번호 WinningLotto 클래스 구현.

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningLotto {
+    private static List<Integer> winningLottoNumber;
+    private static int winningBonusNumber;
+
+    private static WinningLotto winningLotto = new WinningLotto();
+
+    private WinningLotto(){}
+
+}
