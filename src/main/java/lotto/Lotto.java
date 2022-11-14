5324a7444074582f03bd1a93140dc333b6251b7e
devMuscle <hongsbien@naver.com>
2022-11-14 11:07:55 +0900

    feat(validate) : 로또 번호가 중복시 예외 발생

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,8 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    static final Integer cost = 1000;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,7 +14,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 6개가 넘는 숫자 입력");
+        }
+
+        Long nonDuplicatedElementCnt = numbers.stream().distinct().count();
+
+        if(numbers.size() != nonDuplicatedElementCnt.intValue()) {
+            throw new IllegalArgumentException("[ERROR] 중복되는 숫자 입력");
         }
     }
 
