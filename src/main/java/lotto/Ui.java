04e9792e51e9b05e53e951e5937186f0d5c4fef6
hyeonseog-won <smreo75@gmail.com>
2022-11-14 11:13:24 +0900

    feat: 발행한 로또 수량 및 번호 출력 기능 추가

--- src/main/java/lotto/Ui.java
+++ src/main/java/lotto/Ui.java
@@ -2,6 +2,10 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Ui {
     public void printMessage(String message) {
         System.out.println(message);
@@ -11,5 +15,15 @@ public class Ui {
         System.out.println(message);
         String input = Console.readLine();
         return input;
+    }
+
+    public void printLottos(List<Lotto> lottos) {
+        System.out.printf("%d개를 구매했습니다.%n", lottos.size());
+        for (Lotto lotto : lottos) {
+            List<Integer> numbers = lotto.getNumbers();
+            List<Integer> tempLotto = new ArrayList<>(lotto.getNumbers());
+            Collections.sort(tempLotto);
+            System.out.println(tempLotto);
+        }
     }
 }
