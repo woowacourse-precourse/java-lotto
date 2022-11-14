cdd8e48b5025ae7d5d6ab89cd6e1a1284b4feed3
JuneYub <ajsjdlwj0123@gmail.com>
2022-11-15 00:53:28 +0900

    <feat> 예외사항 발생 시 IllegalArgument 반응 수정

--- src/main/java/lotto/Cash.java
+++ src/main/java/lotto/Cash.java
@@ -8,7 +8,6 @@ public class Cash {
     int money = 0;
 
     public void Input(String inputCash) {
-        // String inputCash = Console.readLine();
         if(isNumeric(inputCash)) {
             money = Integer.parseInt(inputCash);
             validiate(money);
@@ -18,7 +17,6 @@ public class Cash {
 
     private void validiate(int money) {
         if(money % 1000 != 0) {
-            System.out.println("[ERROR] 잘못된 값을 입력하셨습니다");
             throw new IllegalArgumentException();
         }
     }
@@ -28,7 +26,6 @@ public class Cash {
             Double.parseDouble(s);
             return true;
         } catch (NumberFormatException e) {
-            System.out.println("[ERROR] 잘못된 값을 입력하셨습니다");
             throw new IllegalArgumentException();
         }
     }
