e5b820195da66005f8e298814cf922620b58ee4d
KimSangHyeok <tkdgur0906@naver.com>
2022-11-14 20:12:04 +0900

    Feat(lotto): Lotto를 구매하는 기능 Controller에 추가

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -1,2 +1,15 @@
-package lotto;public class Controller {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.Model.Purchase;
+
+public class Controller {
+    View view;
+    Purchase purchase;
+
+    void buyLotto(){
+        System.out.println(Constant.ASK_PRICE);
+        String money = Console.readLine();
+        purchase.setPrice(money);
+    }
 }
