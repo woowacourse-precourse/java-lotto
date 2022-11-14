05552840e90cbe4d789a057acc261815bdbdf282
choyeram <choyeram8@gmail.com>
2022-11-14 16:07:16 +0900

    refactor: 테스트 메서드 문장 형식 통일

--- src/test/java/lotto/domain/ValidatorTest.java
+++ src/test/java/lotto/domain/ValidatorTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class ValidatorTest {
     private final Validator validator = new Validator();
 
-    @DisplayName("로또 구매 금액이 숫자가 아니면 예외가 발생한다.")
+    @DisplayName("로또 구매 금액이 숫자가 아니면 예외 발생")
     @Test
     void inputAmountWithNonNumeric() {
         String input = "a";
@@ -19,7 +19,7 @@ public class ValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 구매 금액이 1,000원 단위가 아니면 예외가 발생한다.")
+    @DisplayName("로또 구매 금액이 1,000원 단위가 아니면 예외 발생")
     @Test
     void inputAmountWithNonThousandUnit() {
         String input = "1200";
@@ -27,7 +27,7 @@ public class ValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호가 1에서 45 사이의 숫자가 아니면 예외가 발생한다.")
+    @DisplayName("로또 번호가 1에서 45 사이의 숫자가 아니면 예외 발생")
     @Test
     void inputNumbersOutOfRange() {
         String input = "1,2,3,4,5,66";
@@ -35,7 +35,7 @@ public class ValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외 발생")
     @Test
     void inputNumbersOverSize() {
         String input = "1,2,3,4,5,6,7";
@@ -43,7 +43,7 @@ public class ValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호 입력 시 구분자가 쉼표가 아니면 예외가 발생한다.")
+    @DisplayName("로또 번호 입력 시 구분자가 쉼표가 아니면 예외 발생")
     @Test
     void inputNumbersWithWrongDelims() {
         String input = "1.2,3,4,5,6";
@@ -51,7 +51,7 @@ public class ValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외 발생")
     @Test
     void inputNumbersWithDuplicatedNumber() {
         String input = "1,1,2,3,4,5";
@@ -59,7 +59,7 @@ public class ValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("보너스 번호가 1에서 45 사이의 숫자가 아니면 예외가 발생한다.")
+    @DisplayName("보너스 번호가 1에서 45 사이의 숫자가 아니면 예외 발생")
     @Test
     void inputBonusOutOfRange() {
         String input = "46";
@@ -68,7 +68,7 @@ public class ValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("보너스 번호가 1개의 숫자가 아니면 예외가 발생한다.")
+    @DisplayName("보너스 번호가 1개의 숫자가 아니면 예외 발생")
     @Test
     void inputBonusOverSize() {
         String input = "7,8";
@@ -77,7 +77,7 @@ public class ValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 보너스 번호와 중복된 숫자가 있으면 예외가 발생한다.")
+    @DisplayName("로또 번호에 보너스 번호와 중복된 숫자가 있으면 예외 발생")
     @Test
     void inputBonusWithDuplicatedNumber() {
         String input = "1";
