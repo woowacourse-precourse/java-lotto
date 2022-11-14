117e315fdb3d1392f354bbb4bc43ca82215728a2
baekeunsun <eunsun5460@gmail.com>
2022-11-14 14:15:18 +0900

    feat(view/InputView): 당첨 번호를 입력받음
    - (InputView/InputWinNumber): 당첨번호를 입력받음

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,15 @@
 package lotto;
 
 import lotto.controller.Issue;
+import lotto.model.Lotto;
+import lotto.view.InputView;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         Issue user = new Issue();
+        List<Integer> numbers = InputView.InputWinNumber();
+        Lotto winLotto = new Lotto(numbers);
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,9 @@
 package lotto.view;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
@@ -7,6 +11,14 @@ public class InputView {
     public static int InputPrice() {
         System.out.println("구입금액을 입력해 주세요.");
         int input = Integer.valueOf(readLine());
+        return input;
+    }
+
+    public static List<Integer> InputWinNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<Integer> input = List.of(readLine().split(",")).stream()
+                .map(s -> Integer.parseInt(s))
+                .collect(Collectors.toList());
         return input;
     }
 }
