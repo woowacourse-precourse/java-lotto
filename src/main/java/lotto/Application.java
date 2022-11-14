102ef752b0f4124fda7ee9177d9f6c49766a885b
jiwon1217 <koagjw@gmail.com>
2022-11-15 03:09:41 +0900

    feat: 로또 게임 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import lotto.model.LottoCount;
+import lotto.model.LottoNumberGenerator;
+import lotto.model.Lottos;
+import lotto.model.Money;
+import lotto.model.Result;
+import lotto.model.WinningLotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Money money = new Money(InputView.inputMoney());
+        LottoCount lottoCount = new LottoCount(money);
+        OutputView.printLottoCount(lottoCount);
+
+        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+        Lottos lottos = lottoNumberGenerator.generateLottos(lottoCount);
+        OutputView.printPurchasedLottoNumbers(lottos);
+
+        String winningLottoNumbers = InputView.inputWinningLottoNumbers();
+        Integer bonusNumber = InputView.inputBonusNumber();
+        WinningLotto winningLotto = new WinningLotto(winningLottoNumbers, bonusNumber);
+
+        Result result = new Result(lottos, winningLotto);
+        OutputView.printResult(result);
+        OutputView.printYield(result, money);
     }
 }
