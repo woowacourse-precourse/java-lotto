e61a98ac285b56ed073624f0ce25e2bd0917ee91
egoing <dudqk9696@naver.com>
2022-11-14 19:50:01 +0900

    style : 변수명 대소문자 수정

--- src/main/java/controller/InputValidator.java
+++ src/main/java/controller/InputValidator.java
@@ -1,69 +1,69 @@
 package controller;
 
 import util.Constant;
+import util.Error;
 
-import java.util.HashSet;
 import java.util.List;
 
 public class InputValidator {
 
-    public void checkDigitPrice(String lottopuramount) {
-        for(int i=0; i<lottopuramount.length(); i++) {
-            if(!Character.isDigit(lottopuramount.charAt(i))) {
-                throw new IllegalArgumentException("[ERROR] 숫자를 입력하세요.");
+    public void checkDigitPrice(String lottoPurAmount) {
+        for(int i=0; i<lottoPurAmount.length(); i++) {
+            if(!Character.isDigit(lottoPurAmount.charAt(i))) {
+                throw new IllegalArgumentException(Error.NUMBER.getErrorMsg());
             }
         }
     }
 
-    public void checkDividePrice(Integer lottopuramount) {
-        if(lottopuramount % Constant.DIVIDEPRICE != 0) {
-            throw new IllegalArgumentException("[ERROR] 금액이 1000원 단위로 나누어 떨어지지 않습니다.");
+    public void checkDividePrice(Integer lottoPurAmount) {
+        if(lottoPurAmount % Constant.DIVIDEPRICE != 0) {
+            throw new IllegalArgumentException(Error.DIVIDE.getErrorMsg());
         }
     }
 
-    public void checkWinNumber(List<Integer> lottonumber) {
-        checkNumberQuantity(lottonumber);
-        checkLottoNumberRange(lottonumber);
+    public void checkWinNumber(List<Integer> lottoNumber) {
+        checkNumberQuantity(lottoNumber);
+        checkLottoNumberRange(lottoNumber);
     }
 
-    public void checkNumberQuantity(List<Integer> lottonumber) {
-        if(lottonumber.size() != Constant.LOTTONUMBERQUANTITY) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호가 6개가 아닙니다.");
+    public void checkNumberQuantity(List<Integer> lottoNumber) {
+        if(lottoNumber.size() != Constant.LOTTONUMBERQUANTITY) {
+            throw new IllegalArgumentException(Error.SIX.getErrorMsg());
         }
     }
 
-    public void checkDigitNumber(String lottonumber) {
-        for(int i=0; i<lottonumber.length(); i++) {
-            if(!Character.isDigit(lottonumber.charAt(i)) && lottonumber.charAt(i) != ',') {
-                throw new IllegalArgumentException("[ERROR] 잘못된 당첨 번호 입력입니다.");
+    public void checkDigitNumber(String lottoNumber) {
+        for(int i=0; i<lottoNumber.length(); i++) {
+            if(!Character.isDigit(lottoNumber.charAt(i)) && lottoNumber.charAt(i) != ',') {
+                throw new IllegalArgumentException(Error.WINNUMBER.getErrorMsg());
             }
         }
     }
 
-    public void checkLottoNumberRange(List<Integer> lottonumber) {
-        for(int checklottonum : lottonumber) {
-            if(!(checklottonum >= Constant.MINLOTTOVALUE && checklottonum <= Constant.MAXLOTTOVALUE)) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이의 숫자여야합니다.");
+    public void checkLottoNumberRange(List<Integer> lottoNumber) {
+        for(int checkNum : lottoNumber) {
+            if(!(checkNum >= Constant.MINLOTTOVALUE && checkNum <= Constant.MAXLOTTOVALUE)) {
+                throw new IllegalArgumentException(Error.RANGE.getErrorMsg());
             }
         }
     }
 
-    public void checkBonusNumber(String bonusNumer) {
-        isDigitBonusNumber(bonusNumer);
-        checkBonusNumberRange(bonusNumer);
+    public void checkBonusNumber(String bonusNumber) {
+        isDigitBonusNumber(bonusNumber);
+        checkBonusNumberRange(bonusNumber);
     }
 
     public void isDigitBonusNumber(String bonusNumber) {
         for(int i=0; i<bonusNumber.length(); i++) {
             if(!Character.isDigit(bonusNumber.charAt(i))) {
-                throw new IllegalArgumentException("[ERROR] 숫자를 입력하세요.");
+                throw new IllegalArgumentException(Error.NUMBER.getErrorMsg());
             }
         }
     }
 
     public void checkBonusNumberRange(String bonusNumber) {
-        if(Integer.parseInt(bonusNumber) > 45 || Integer.parseInt(bonusNumber) < 1) {
-            throw new IllegalArgumentException("[ERROR] 숫자를 입력하세요.");
+        if(Integer.parseInt(bonusNumber) > Constant.MAXLOTTOVALUE || Integer.parseInt(bonusNumber) < Constant.MINLOTTOVALUE) {
+            throw new IllegalArgumentException(Error.RANGE.getErrorMsg());
         }
     }
 }
