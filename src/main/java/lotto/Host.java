5dd71cd08f77bf0561384e3d8d5b1ce85514f31e
이한솔 <hansol8701@naver.com>
2022-11-14 16:32:49 +0900

    feat(Host): 로또 발행 기능 추가

--- src/main/java/lotto/Host.java
+++ src/main/java/lotto/Host.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Host {
@@ -7,13 +8,28 @@ public class Host {
     List<Integer> winNumbers;
     int bonusNumber;
 
+    List<List<Integer>> issuanceHistory;
+
+    public Host() {
+        issuanceHistory = new ArrayList<>();
+    }
+
     public void registerWinningInformation(List<Integer> winNumbers, int bonusNumber){
         this.winNumbers = winNumbers;
         this.bonusNumber = bonusNumber;
     }
 
     public Lotto issueLotto(){
-        return null;
+        NumberGenerator numberGenerator = new NumberGenerator();
+        List<Integer> numbers;
+        while (true){
+            numbers = numberGenerator.generateSixRandomNumbers();
+            if (!isIssued(numbers)){
+                issuanceHistory.add(numbers);
+                break;
+            }
+        }
+        return new Lotto(numbers);
     }
 
     private boolean isIssued(List<Integer> numbers){
