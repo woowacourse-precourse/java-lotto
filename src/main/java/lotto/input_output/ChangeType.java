fce1db932e94a88911fef0b7b4dff423356a3fed
leewoojin <zmfpdl64@naver.com>
2022-11-14 17:47:51 +0900

    Feat: ChangeType 객체 타입변환 메소드 작성
    1. String to Int
    2. String to String[]
    3. String to List<Integer>

--- src/main/java/lotto/input_output/ChangeType.java
+++ src/main/java/lotto/input_output/ChangeType.java
@@ -1,8 +1,21 @@
 package lotto.input_output;
 
-public class ChageInput {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ChangeType {
 
     public static String[] string_to_stringarray(String str){
+
         return str.split(",");
+    }
+
+    public static List<Integer> string_to_listinteger(String str){
+        return Arrays.stream(str.split(",")).mapToInt(Integer::valueOf).boxed().collect(Collectors.toList());
+    }
+
+    public static int string_to_int(String str){
+        return Integer.parseInt(str);
     }
 }
