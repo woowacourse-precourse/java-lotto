f94d3d61f2ebb991c1ecf116d41a6928ba48258a
leeyongjun <leeyongjun0310@naver.com>
2022-11-14 18:29:56 +0900

    test(lotto): 2개 맞았을 때 정상동작하는지 확인하는 테스트 코드 작성

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -48,7 +48,7 @@ class LottoTest {
     @Test
     void getPrizeMoney_정상동작_3등_당첨(){
         Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
-        int prizeMoney = lotto.getPrizeMoney(Arrays.asList(Arrays.asList(1,2,3,4,5,10)), 45);
+        int prizeMoney = lotto.getPrizeMoney(List.of(Arrays.asList(1, 2, 3, 4, 5, 10)), 45);
         assertThat(prizeMoney).isEqualTo(Rank.THIRD.getWinningsPrice());
     }
 
@@ -56,7 +56,7 @@ class LottoTest {
     @Test
     void getPrizeMoney_정상동작_4등_당첨(){
         Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
-        int prizeMoney = lotto.getPrizeMoney(Arrays.asList(Arrays.asList(1,2,3,4,9,10)), 45);
+        int prizeMoney = lotto.getPrizeMoney(List.of(Arrays.asList(1, 2, 3, 4, 9, 10)), 45);
         assertThat(prizeMoney).isEqualTo(Rank.FOURTH.getWinningsPrice());
     }
 
@@ -64,7 +64,15 @@ class LottoTest {
     @Test
     void getPrizeMoney_정상동작_5등_당첨(){
         Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
-        int prizeMoney = lotto.getPrizeMoney(Arrays.asList(Arrays.asList(1,2,3,8,9,10)), 45);
+        int prizeMoney = lotto.getPrizeMoney(List.of(Arrays.asList(1, 2, 3, 8, 9, 10)), 45);
         assertThat(prizeMoney).isEqualTo(Rank.FIFTH.getWinningsPrice());
+    }
+
+    @DisplayName("2개 맞았을때 정상동작 하는지 확인한다.")
+    @Test
+    void getPrizeMoney_정상동작_2개(){
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        int prizeMoney = lotto.getPrizeMoney(List.of(Arrays.asList(1, 2, 7, 10, 11, 12)), 45);
+        assertThat(prizeMoney).isEqualTo(0);
     }
 }
