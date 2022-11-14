1fc9d06c824d197ed887b2c69eb92262249355ab
famo1245 <kandallee38@gmail.com>
2022-11-14 16:36:06 +0900

    로또 번호 정렬 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,28 @@
 package lotto;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        // 로또 번호 정렬
+        this.numbers = new ArrayList<>(numbers);
+        this.numbers.sort(Integer::compareTo);
     }
 
     private void validate(List<Integer> numbers) {
+        // 번호가 6개인지 확인
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR]");
         }
+
+        // 중복 번호가 있는지 확인
+        Set<Integer> checkSet = new HashSet<>(numbers);
+
+        if(numbers.size() != checkSet.size())
+            throw new IllegalArgumentException();
     }
 
     public List<Integer> getNumbers() {
@@ -23,10 +31,10 @@ public class Lotto {
 
     @Override
     public String toString() {
-        Collections.sort(numbers);
         String value = "";
         for (Integer n : numbers)
             value += n + ", ";
+        value = value.substring(0, value.length()-2);
         return value;
     }
 }
