18adc2d3c914831da4825f716039b8cb1600791e
jin8073 <jin8073@gmail.com>
2022-11-14 20:42:30 +0900

    feat: 당첨 번호 입력 값이 쉼표로 구분된 숫자가 아닐 경우 예외 발생

--- src/main/java/lotto/calculation/Winning.java
+++ src/main/java/lotto/calculation/Winning.java
@@ -1,5 +1,7 @@
 package lotto.calculation;
 
+import lotto.exception.IllegalArgument;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +12,9 @@ public class Winning {
     public Winning(List<String> winningNumbers) {
         List<Integer> parsingWinningNumbers = new ArrayList<>();
         for (String winningNumber : winningNumbers) {
+            if (!IllegalArgument.isNumber(winningNumber)) {
+                throw new IllegalArgumentException("[ERROR] 1부터 45까지의 숫자만 입력 가능합니다.");
+            }
             parsingWinningNumbers.add(Integer.parseInt(winningNumber));
         }
         this.winningNumbers = parsingWinningNumbers;
