a2ce05fd5b1fc87236607bbdfc552c8ec663fb61
Beginner <dkstmdwn17@naver.com>
2022-11-14 19:34:04 +0900

    feat : 로또 구매 금액을 입력받는 메서드 구현
    로또 구매 금액을 입력받는 부분을 getMoney() 메서드를 통해 구현하였음.
    
    입력할 때에 유효성 검사를 통해 유효하지 않은 금액이 저장소에 저장되지 않도록 만들었다.
    
    1000원 이하일 경우, 1000원으로 나누어 떨어지지 않는 경우 IllegalArgumentException을 던진다.

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,4 +1,29 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
+    private static final String ERROR_MESSAGE = "[ERROR] ";
+
+    public int getMoney(){
+        try {
+            System.out.println("구입금액을 입력해 주세요.");
+            String unverifiedCharacters = Console.readLine();
+            int money = Integer.parseInt(unverifiedCharacters);
+            if(money < 1000) validationMoney();
+            if(money % 1000 != 0) validationDivideMoney();
+            return money;
+        }catch (IllegalArgumentException e){
+            System.out.println(ERROR_MESSAGE + e.getMessage());
+            return getMoney();
+        }
+    }
+
+    private void validationDivideMoney() {
+        throw new IllegalArgumentException("1000으로 나누어 떨어지는 금액을 입력해주세요.");
+    }
+
+    private void validationMoney() {
+        throw new IllegalArgumentException("1000원 이상의 금액을 입력해주세요.");
+    }
 }
