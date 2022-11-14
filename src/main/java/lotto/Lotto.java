8724c705c7caf8964aa97562ca2978ea6cfe8470
syahn <496300@naver.com>
2022-11-15 01:38:03 +0900

    feat: Lotto.java에 검증 기능, getter 구현
    - 숫자인지 검증하는 기능 외에 숫자 범위 검증, 숫자 중복 검증, 숫자 개수 검증을 하도록 함
    - lotto 숫자 리스트에 대해
    getter를 통해 접근가능하도록 함

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -11,10 +12,36 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+       validateNumberCounts(numbers);
+       validateDuplicate(numbers);
+       validateRange(numbers);
+    }
+
+    void validateNumberCounts(List<Integer> numbers){
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_COUNT_ERROR.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    void validateDuplicate(List<Integer> numbers){
+        for(int i : numbers){
+            if(this.numbers.contains(i)) {
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_DUPLICATE_ERROR.getMessage());
+            }
+            this.numbers.add(i);
+        }
+        Collections.sort(this.numbers);
+    }
+
+    void validateRange(List<Integer> numbers){
+        for(int i : numbers){
+            if(i <1 | i>45) {
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_RANGE_ERROR.getMessage());
+            }
+        }
+    }
+
+    public List<Integer> getLotto(){
+        return this.numbers;
+    }
 }
