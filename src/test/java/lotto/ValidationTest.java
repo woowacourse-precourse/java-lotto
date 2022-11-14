1e715f2567c00f9155d54084c3c0a88ec855bdfb
jeong-yeji <jeongyeji.mail@gmail.com>
2022-11-15 02:08:45 +0900

    test(Validation): 범위 밖 숫자 확인

--- src/test/java/lotto/ValidationTest.java
+++ src/test/java/lotto/ValidationTest.java
@@ -1,14 +1,28 @@
 package lotto;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class ValidationTest {
 
     Validation validation;
 
     @BeforeEach
-    void initialize() {
+    void setUp() {
         validation = new Validation();
     }
-    
+
+    @ParameterizedTest
+    @CsvSource({"0", "46", "-1"})
+    void 범위를_벗어난_숫자(int number) {
+        try {
+            validation.validateNumberRange(number);
+        } catch (IllegalArgumentException e) {
+            assertEquals(Errors.OUT_OF_RANGE.toString(), e.getMessage());
+        }
+    }
+
 }
