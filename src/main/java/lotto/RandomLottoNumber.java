7c39c6583436d6cae50e7c50686acdc4d12a7605
anso33 <hyeon131812@gmail.com>
2022-11-14 14:11:37 +0900

    [FIX](RandomLottoNumber): change random function that recommanded

--- src/main/java/lotto/RandomLottoNumber.java
+++ src/main/java/lotto/RandomLottoNumber.java
@@ -1,36 +1,20 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.TreeSet;
-
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 public class RandomLottoNumber {
-  TreeSet<Integer> lotto = new TreeSet<>();
+  List<Integer> numbers;
 
   public RandomLottoNumber() {
     setRandomNumber();
   }
 
   private void setRandomNumber() {
-
-    for (int i = 0; i < 6; i++) {
-      if (lotto.add(pickNumberInRange(1, 45))) {
-        i--;
-        continue;
-      }
-    }
+    numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
   }
 
-  public ArrayList<Integer> getRandomNumber() {
-    ArrayList<Integer> lotto_arr = new ArrayList<>();
-    Iterator<Integer> iter = lotto.iterator();
-
-    while (iter.hasNext()) {
-      lotto_arr.add(iter.next());
-    }
-
-    return lotto_arr;
+  public List<Integer> getRandomNumber() {
+    return numbers;
   }
 }
