7e2fdde99fabf47ebdc73d2e64eee4a90db24f40
yujaemin <woawoa0513@gmail.com>
2022-11-14 23:59:14 +0900

    test: 클래스명 변경
    기존의 LottoGame 클래스명을 LottoService로 변경하면서 함께 변경

--- src/test/java/lotto/controller/LottoGameTest.java
+++ src/test/java/lotto/controller/LottoGameTest.java
@@ -4,16 +4,17 @@ import static lotto.utils.Constants.LOTTO_PRICE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import lotto.domain.Lotto;
+import lotto.service.LottoService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-class LottoGameTest {
+class LottoServiceTest {
     @DisplayName("구매 금액 1,000원 당 한 장씩의 로또를 생성한다.")
     @Test
     void 구매금액에_맞는_개수의_로또_생성() {
-        List<Lotto> lottos = LottoGame.generateLottoByPurchaseAmount(LOTTO_PRICE * 7);
+        List<Lotto> lottos = LottoService.generateLottoByPurchaseAmount(LOTTO_PRICE * 7);
         int lottoCount = lottos.size();
 
         assertThat(lottoCount).isEqualTo(7);
