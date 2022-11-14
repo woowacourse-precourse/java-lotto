f3b92412bffc0f2daaba8d1f862f5f4d52f47031
wjh2012 <wjh2012@naver.com>
2022-11-15 00:15:50 +0900

    feat(LottoWinning): 당첨번호, 보너스번호를 저장한다 - builder 패턴

--- src/main/java/lotto/model/LottoWinning.java
+++ src/main/java/lotto/model/LottoWinning.java
@@ -4,14 +4,36 @@ import static lotto.config.Constants.LottoNumber.LOTTO_NUMBER_END_INCLUSIVE;
 import static lotto.config.Constants.LottoNumber.LOTTO_NUMBER_START_INCLUSIVE;
 
 public class LottoWinning {
-    private final Lotto winLotto;
-    private final int winBonus;
+    private Lotto winLotto;
+    private int winBonus;
 
-    public LottoWinning(Lotto lotto, int bonus) {
-        this.winLotto = lotto;
-        validateNumberRange(bonus);
-        validateDuplicated(bonus);
-        this.winBonus = bonus;
+    public static class Builder {
+        private Lotto winLotto;
+        private int winBonus;
+
+        public Builder winLotto(Lotto winLotto) {
+            this.winLotto = winLotto;
+            return this;
+        }
+
+        public Builder winBonus(int winBonus) {
+            this.winBonus = winBonus;
+            return this;
+        }
+
+        public LottoWinning build() {
+            return new LottoWinning(this);
+        }
+    }
+
+    protected LottoWinning() {
+    }
+
+    public LottoWinning(Builder builder) {
+        this.winLotto = builder.winLotto;
+        validateNumberRange(builder.winBonus);
+        validateDuplicated(builder.winBonus);
+        this.winBonus = builder.winBonus;
     }
 
     private void validateDuplicated(int bonus) {
@@ -21,7 +43,7 @@ public class LottoWinning {
     }
 
     private void validateNumberRange(int bonus) {
-        if (bonus >= LOTTO_NUMBER_START_INCLUSIVE && bonus <= LOTTO_NUMBER_END_INCLUSIVE) {
+        if (bonus < LOTTO_NUMBER_START_INCLUSIVE || bonus > LOTTO_NUMBER_END_INCLUSIVE) {
             throw new IllegalArgumentException();
         }
     }
