c5a52ab1a47e4a7c856166ffdf31c85142b069c7
juy4556 <kws05050@naver.com>
2022-11-15 00:36:59 +0900

    feat: 로또 게임 객체 내 로또 생성 메소드 생성

---
+++ src/main/java/domain/LottoGame.java
@@ -0,0 +1,34 @@
+package domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
+import static java.util.Arrays.asList;
+
+public class LottoGame {
+    private final static int WIN = 0;
+    private final static int SECOND = 0;
+    private final static int THIRD = 0;
+    private final static int FOURTH = 0;
+    private final static int FIFTH = 0;
+
+    private List<Lotto> lotteries;
+    private List<Integer> matchCounts;
+
+    public LottoGame() {
+        lotteries = new ArrayList<>();
+        matchCounts = new ArrayList<>(asList(0, 0, 0, 0, 0, 0, 0, 0)); // 0,1,2,3,4,5,6,bonus
+    }
+
+    public void createLotto(int buyCount) {
+        for (int i = 0; i < buyCount; i++) {
+            Lotto createdLotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            addLotto(createdLotto);
+        }
+    }
+
+    private void addLotto(Lotto lotto) {
+        lotteries.add(lotto);
+    }
+}
