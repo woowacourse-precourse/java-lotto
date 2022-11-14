4ea42b29c210c4f403f8074d43967ba9db26c973
YuHayeon <youha0930@gmail.com>
2022-11-14 19:24:30 +0900

    test(Lotto): 사용자 번호와 당첨 번호의 중복된 값 개수 테스트
    사용자 번호와 당첨 번호에서 중복된 값이 몇개인지 확인 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,12 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +24,15 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    void 중복된_값_개수_세기() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThat(lotto.countSameNumbers(List.of(1, 2, 3, 4, 5, 6))).isEqualTo(6);
+        assertThat(lotto.countSameNumbers(List.of(1, 2, 3, 4, 5, 9))).isEqualTo(5);
+        assertThat(lotto.countSameNumbers(List.of(1, 2, 3, 4, 8, 9))).isEqualTo(4);
+        assertThat(lotto.countSameNumbers(List.of(1, 2, 3, 7, 8, 9))).isEqualTo(3);
+        assertThat(lotto.countSameNumbers(List.of(1, 2, 10, 7, 8, 9))).isEqualTo(2);
+        assertThat(lotto.countSameNumbers(List.of(1, 45, 10, 7, 8, 9))).isEqualTo(1);
+    }
 }
