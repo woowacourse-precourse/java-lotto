8440e79fdcd942a3dc5871450d275d2f8dde9e73
marinesnow34 <m4rinesnow34@gmail.com>
2022-11-14 19:44:30 +0900

    feat: 출력 클래스 생성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,26 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+import java.util.HashMap;
+import lotto.model.Buyer;
+import lotto.model.LottoStatics;
+import lotto.model.Winning;
+
+public class OutputView {
+    private static final String MESSAGE_STATICS_START = "당첨 통계\n---\n";
+    private static final String MESSAGE_STATICS_INFO = "%s (%,d원) - %d개\n";
+    private static final String MESSAGE_YIELD = "총 수익률은 %.1f%%입니다.";
+
+
+    public static void printStatics(LottoStatics statics) {
+        HashMap<Winning, Integer> staticsInfo = statics.getStaticsInfo();
+        System.out.printf(MESSAGE_STATICS_START);
+        for (Winning value : Winning.values()) {
+            System.out.printf(MESSAGE_STATICS_INFO, value.getDetail(), value.getPrize(), staticsInfo.get(value));
+        }
+    }
+
+    public static void printYield(Buyer buyer, LottoStatics statics) {
+        float yield = (statics.getTotalPrize() / (float) buyer.getMoney()) * 100;
+        System.out.printf(MESSAGE_YIELD, yield);
+    }
 }
