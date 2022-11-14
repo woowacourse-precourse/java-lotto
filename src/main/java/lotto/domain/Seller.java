6b12594325b3abc2382d5abb33721d27a6d96dcc
seonpilKim <ksp970306@gmail.com>
2022-11-14 17:36:58 +0900

    Feat: 로또 티켓 판매 메소드 추가

--- src/main/java/lotto/domain/Seller.java
+++ src/main/java/lotto/domain/Seller.java
@@ -1,9 +1,24 @@
 package lotto.domain;
 
+import java.util.List;
+
+import lotto.ErrorMessage;
+
 public class Seller {
 
-	public static final int LOTTO_PRICE = 1_000;
-	public static final String SELL_ANNOUNCEMENT = "구입금액을 입력해 주세요.";
-	public static final String PURCHASE_ANNOUNCEMENT = "%s개를 구매했습니다.";
+	private static final int LOTTO_PRICE = 1_000;
+	private static final String PURCHASE_ANNOUNCEMENT = "%s개를 구매했습니다.";
+
+	private static boolean isNotUnitsOf1000(Integer money) {
+		return money < LOTTO_PRICE || money % LOTTO_PRICE != 0;
+	}
+
+	public List<Lotto> sellLottoTickets(int money) {
+		if (isNotUnitsOf1000(money)) {
+			throw new IllegalArgumentException(ErrorMessage.isNotUnitsOf1000());
+		}
+		System.out.println(String.format(PURCHASE_ANNOUNCEMENT, money / LOTTO_PRICE));
+		return Lotto.newInstances(money / LOTTO_PRICE);
+	}
 
 }
