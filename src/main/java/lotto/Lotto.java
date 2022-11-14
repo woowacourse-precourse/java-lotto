44cf677691e2d2170effb45c283022d530c0bf1c
eugene1204 <s1061001@naver.com>
2022-11-14 18:25:17 +0900

    refactor : BuyLotto, validate수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,12 +16,12 @@ public class Lotto extends IllegalArgumentException{
 
     private void validate(List<Integer> numbers) throws IllegalArgumentException {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호가 여섯 자리가 아닙니다.");
+            throw new IllegalArgumentException("당첨 번호가 여섯 자리가 아닙니다.");
         }
-        if(numbers.stream().distinct().count() != 6) throw new IllegalArgumentException("[ERROR] 중복된 숫자가 존재합니다.");
+        if(numbers.stream().distinct().count() != 6) throw new IllegalArgumentException("중복된 숫자가 존재합니다.");
         for (int number : numbers) {
             if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("[ERROR] 당첨 번호가 범위 밖의 숫자입니다.");
+                throw new IllegalArgumentException("당첨 번호가 범위 밖의 숫자입니다.");
             }
         }
     }
