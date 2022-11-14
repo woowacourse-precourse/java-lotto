5cd050b7f6d3934e3e63e2e482632e6f22b9d46f
yun <yshyeon102@gmail.com>
2022-11-14 10:55:59 +0900

    test(RandomNumberGenerator): 랜덤 넘버 생성 테스트 작성

---
+++ src/test/java/lotto/RandomNumberGeneratorTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import lotto.exception.Validator;
+import lotto.util.RandomNumberGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class RandomNumberGeneratorTest {
+
+    @Test
+    void 랜덤_넘버_생성시_로또_검증기를_통과() {
+        List<Integer> lottoNumbers = RandomNumberGenerator.makeRandomNumbers();
+        Validator.validateLottoNumberDuplicates(lottoNumbers);
+        Validator.validateLottoNumberRange(lottoNumbers);
+    }
+
+}
