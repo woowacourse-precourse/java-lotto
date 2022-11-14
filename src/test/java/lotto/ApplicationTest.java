2d1cd06e2f8d2ef5742bc981c80d6e1ab27532c1
Isu Shin <isu010614@naver.com>
2022-11-15 08:51:57 +0900

    feat: 어플리케이션 구동 기능 구현

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -52,6 +53,13 @@ class ApplicationTest extends NsTest {
             runException("1000j");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
+    }
+
+    @Test
+    void 예외_테스트2() {
+        assertThatThrownBy(() -> run("1000j"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] Only enter numbers.");
     }
 
     @Override
