2733c1e20e213c5ce2bfd14680ae2d85339f6860
ingpyo <juninpyo72@gmail.com>
2022-11-14 10:52:42 +0900

    feat:맞힌 개수 카운트

---
+++ src/main/java/lotto/NumberCount.java
@@ -0,0 +1,15 @@
+package lotto;
+
+public class NumberCount {
+	int guess;
+	int bonusGuess;
+
+	public void guessCount() {
+		guess++;
+	}
+
+	public void bonusGuessCount() {
+		bonusGuess++;
+	}
+
+}
