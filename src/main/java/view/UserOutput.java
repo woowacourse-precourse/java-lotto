cdce41b12d73ff819a34cbc071af92bfd63a9f45
eunseon <agseon9@gmail.com>
2022-11-15 03:18:28 +0900

    feat : 당첨 내역을 출력하는 기능
    당첨 내역을 출력하는 기능 추가

--- src/main/java/view/UserOutput.java
+++ src/main/java/view/UserOutput.java
@@ -1,9 +1,16 @@
 package view;
 
+import lotto.LottoCalculator;
+import lotto.LottoRank;
+import lotto.LottoResultBoard;
+import lotto.Money;
+
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class UserOutput {
     private static final String BUYING_LOTTO_MESSAGE = "개를 구매했습니다.";
+    private static final String WINNING_STATS_MESSAGE = "당첨 통계 :";
 
     public void printLottoCount(int lottoCount){
         System.out.println(lottoCount + BUYING_LOTTO_MESSAGE);
@@ -14,4 +21,35 @@ public class UserOutput {
             System.out.println(randomLottos.get(i));
         }
     }
+
+    public void printWinningStats() {
+        System.out.println(WINNING_STATS_MESSAGE);
+        System.out.println(setWinningStats());
+        printProfitMoney();
+    }
+
+    public StringBuffer setWinningStats(){
+        StringBuffer sb = new StringBuffer();
+        DecimalFormat decimalFormat = new DecimalFormat("###,###");
+
+        for (LottoRank rank : LottoRank.values()) {
+            if(rank != LottoRank.NOTHING && rank != LottoRank.SECOND){
+                sb.append(rank.getSameNumberCount() + "개 일치 (" + decimalFormat.format(rank.getLottoReward())+"원) - " + LottoResultBoard.getLottoResultBoard(rank) + "개");
+                sb.append("\n");
+            }
+            if(rank == LottoRank.SECOND){
+                sb.append(rank.getSameNumberCount() + "개 일치, 보너스 볼 일치 (" + decimalFormat.format(rank.getLottoReward()) +
+                        "원) - " + LottoResultBoard.getLottoResultBoard(rank) + "개");
+                sb.append("\n");
+            }
+        }
+        return sb;
+    }
+
+    public void printProfitMoney(){
+        System.out.println("총 수익률은 "
+                +LottoCalculator.calculateProfitRate(Money.getResultMoney(),LottoResultBoard.getProfitMoney())
+                +"%입니다.");
+    }
 }
+
