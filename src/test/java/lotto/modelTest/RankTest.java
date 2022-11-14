5b8209a14cdba70b716e811fe46ddafb8db2b670
gomljo <gomljo0307@naver.com>
2022-11-15 01:25:42 +0900

    test(RankTest.java): 로또 번호가 추첨 번호를 맞춘 상태에 맞는 등수를 반환하는 기능 검증

--- src/test/java/lotto/modelTest/RankTest.java
+++ src/test/java/lotto/modelTest/RankTest.java
@@ -16,25 +16,28 @@ public class RankTest {
         List<Integer> lottoState = List.of(5,1);
         assertThat(Rank.isSame(Rank.SECOND, lottoState)).isTrue();
     }
-
     @Test
     @DisplayName("로또 번호가 맞춘 추첨 번호의 상태를 보상표와 비교하여 다르다면 false를 반환한다.")
     public void checkSameStateBetweenLottoAndRank_wrong_case(){
         List<Integer> lottoState = List.of(5,1);
         assertThat(Rank.isSame(Rank.THIRD, lottoState)).isFalse();
     }
-
     @Test
     @DisplayName("로또 번호가 맞춘 추첨 번호의 상태를 보상표와 비교하여 해당하는 등수 객체를 반환한다.")
     public void findByMatchState(){
         List<Integer> lottoState = List.of(5,1);
         assertThat(Rank.findByMatches(lottoState)).isEqualTo(Rank.SECOND);
     }
-
     @Test
     @DisplayName("로또 번호가 맞춘 추첨 번호의 상태를 보상표와 비교하여 해당하는 등수 객체를 반환한다.")
     public void getRankNames(){
         List<String> rankNames = List.of("FIRST", "SECOND", "THIRD", "FOURTH", "FIFTH", "NOTHING");
         assertThat(Rank.getRankNames()).containsExactlyElementsOf(rankNames);
+    }
+    @Test
+    @DisplayName("로또 번호가 맞춘 추첨 번호의 상태를 보상표와 비교하여 해당하는 등수 이름을 반환한다.")
+    public void findRankNameByMatchState(){
+        List<Integer> lottoState = List.of(5,1);
+        assertThat(Rank.getRankNameByMatchState(lottoState)).isEqualTo("SECOND");
     }
 }
