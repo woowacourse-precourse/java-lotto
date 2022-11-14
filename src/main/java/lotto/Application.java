26f862a71cb52dbd3338c712cc4db97fbe6ef592
SeongukBaek <bsu6235@gmail.com>
2022-11-15 03:36:25 +0900

    feat: implement Application

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import lotto.domain.NumberBox;
+import lotto.domain.User;
+import lotto.util.Calculator;
+import lotto.util.Input;
+import lotto.util.Output;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Output.printInputMessage();
+        int money = Input.readMoney();
+
+        User user = new User(money);
+        user.printLottos();
+
+        Output.printInputWinnings();
+        List<Integer> winningNumbers = Input.readWinningNumbers();
+
+        Output.printInputBonus();
+        int bonusNumber = Input.readBonusNumber();
+
+        NumberBox numberBox = new NumberBox(winningNumbers, bonusNumber);
+
+        Output.printStatistics(Calculator.calculateResult(user.getLottos(), numberBox.getWinningNumbers(), numberBox.getBonusNumber()));
+
+        Output.printYield(Calculator.calculateYield(money));
     }
 }
