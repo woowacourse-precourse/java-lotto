9a4d45bbcea1a49b6d998338ebec820eb32805a3
jhon3242 <rldotnr2@gmail.com>
2022-11-14 11:15:51 +0900

    fix(Application) : 예외 시 "[ERROR] + 메시지" 출력하게 수정
    - 예외를 try-catch 문으로 잡아서 "[ERROR]"와 함께 출력하는 방식으로 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,7 +5,11 @@ import lotto.controller.LottoController;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        LottoController lottoController = new LottoController();
-        lottoController.start();
+        try {
+            LottoController lottoController = new LottoController();
+            lottoController.start();
+        } catch (Exception error) {
+            System.out.println("[ERROR] " + error);
+        }
     }
 }
