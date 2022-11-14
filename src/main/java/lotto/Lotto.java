2b71b003830b0bd8bf496614c4ba045fd9e6bcc3
Yeongseon Park <yseon406@gmail.com>
2022-11-15 05:12:22 +0900

    feat(lotto): 로또 번호 중복 확인 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,15 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        hasSameNumber(numbers);
         this.numbers = numbers;
     }
 
@@ -14,6 +17,14 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private void hasSameNumber(List<Integer> numbers) {
+        Set<Integer> sameNumber = new HashSet<>(numbers);
+        if (sameNumber.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+
     }
 
     public List<Integer> getNumbers() {
