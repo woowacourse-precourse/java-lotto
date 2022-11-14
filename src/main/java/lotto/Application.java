faafe2f74bd1cf6ed4166e531e65bdc7e20689c3
YUSEONJUN <seonjun0906@gmail.com>
2022-11-15 00:15:15 +0900

    feat : 로또 최종 main 함수

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,10 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
+
+        Controller controller = new Controller();
+        controller.run();
+
         // TODO: 프로그램 구현
     }
 }
