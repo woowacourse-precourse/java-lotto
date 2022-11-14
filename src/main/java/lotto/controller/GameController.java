628ece482a0e16bd68c070fca2d04539860b2db5
CAEL0 <kjongmin26@gmail.com>
2022-11-14 22:50:59 +0900

    feat(GameController): 실행 메서드 추가

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -2,7 +2,10 @@ package lotto.controller;
 
 import lotto.domain.Computer;
 import lotto.domain.GameResult;
+import lotto.domain.Lotto;
 import lotto.domain.Player;
+
+import java.util.List;
 
 public class GameController {
     private final Player player;
@@ -13,5 +16,15 @@ public class GameController {
         player = new Player();
         computer = new Computer();
         gameResult = new GameResult();
+    }
+
+    public void run() {
+        List<Lotto> lottos = player.getLottos();
+        List<Integer> winningNumbers = computer.getWinningNumbers();
+        int bonusNumber = computer.getBonusNumber();
+
+        gameResult.calculateTotalResult(lottos, winningNumbers, bonusNumber);
+        gameResult.calculateProfitRate(player.getLottos().size());
+        gameResult.printTotalResult();
     }
 }
