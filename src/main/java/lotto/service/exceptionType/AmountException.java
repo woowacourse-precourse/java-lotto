9a40534f45158bbd79e7bd26884daca45d034720
jjmin9797 <jjmin9797@naver.com>
2022-11-14 18:34:47 +0900

    feat(AmountException): 입력받은 금액이 1000원 단위인지 확인하는 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/service/exceptionType/AmountException.java
+++ src/main/java/lotto/service/exceptionType/AmountException.java
@@ -3,12 +3,19 @@ package lotto.service.exceptionType;
 public class AmountException {
     public static final String ERROR_MESSAGE = "[ERROR]";
     public static final String FAILED_TO_CONVERT_NUMBER_MESSAGE = " 숫자만 입력 가능합니다.";
-
+    public static final String INDIVISIBLE_EXCEPTION_MESSAGE = " 1000원 단위로만 입력 가능합니다.";
     public void checkIfAnAmountIsConvertibleToNumber(String purchaseAmount) {
         try {
             int money = Integer.parseInt(purchaseAmount);
         } catch (IllegalArgumentException e) {
             System.out.println(ERROR_MESSAGE + FAILED_TO_CONVERT_NUMBER_MESSAGE);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void checkIfAmountIsDivisibleByOneThousand(String purchaseAmount) {
+        if (Integer.parseInt(purchaseAmount)%1000 != 0) {
+            System.out.println(ERROR_MESSAGE+INDIVISIBLE_EXCEPTION_MESSAGE);
             throw new IllegalArgumentException();
         }
     }
