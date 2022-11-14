c73a521657ceec4381d66dd1818e4caeb1d9c746
danneh-0427 <jc14972@gmail.com>
2022-11-15 02:17:47 +0900

    feat(UserInput.java): 입력된 값이 1000원으로 나누어 떨어지는지 확인 하는 기능 및 예외 처리

--- src/main/java/lotto/UserInputNumber.java
+++ src/main/java/lotto/UserInputNumber.java
@@ -4,17 +4,24 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class UserInputNumber {
 
-    private final int userPurchaseAmount;
+    private final int purchaseInput;
 
-    public UserInputNumber(String input) {
-        validatePurchaseAmount(input);
+    public UserInputNumber(String purchaseInput) {
+        validatePurchaseAmount(purchaseInput);
+        validateThousandUnit(Integer.parseInt(purchaseInput));
     }
-    private void validatePurchaseAmount(String input) {
+    private void validatePurchaseAmount(String inputNumber) {
         try {
-            int p = Integer.parseint(input);
+            int p = Integer.parseInt(inputNumber);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException ("숫자를 입력해주세요.");
         }
     }
+    private void validateThousandUnit(Integer inputNumber) {
+        if (inputNumber % 1000 !=0) {
+            throw new IllegalArgumentException ("입력은 1000원 단위의 숫자만 가능합니다.");
+        }
+    }
+
 }
 
