15c5bf6f3b7bf8181891fdf29f706a448bd3758d
alivejuicy <kkddhh1998@gmail.com>
2022-11-15 02:12:12 +0900

    refactor : pickUniqueNumbersInRange() 함수로 랜덤 로또 숫자 생성 로직 수정

--- src/main/java/lotto/reward/RandomNumbers.java
+++ src/main/java/lotto/reward/RandomNumbers.java
@@ -1,29 +1,27 @@
 package lotto.reward;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomNumbers {
+import java.util.*;
+
+public class RandomNumbers implements Comparable<RandomNumbers> {
     private static final int START_NUMBER = 1;
     private static final int END_NUMBER = 45;
-    private static final int START_INDEX = 0;
     private static final int COUNT = 6;
+    private static final List<Lotto> lottoNumbers = new ArrayList<>();
 
-    public List<Integer> getRandomNumbers() {
-        return shuffleNumbers(setRandomNumbers());
+    public Lotto createRandomNumbers() {
+        Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, COUNT));
+        lottoNumbers.add(lotto);
+        return lotto;
     }
 
-    private List<Integer> shuffleNumbers(List<Integer> randomNumbers) {
-        Collections.shuffle(randomNumbers);
-        return randomNumbers.subList(START_INDEX, COUNT);
+    public List<Lotto> getLottoNumbers(){
+        return lottoNumbers;
     }
 
-    private List<Integer> setRandomNumbers() {
-        List<Integer> setRandomNum = new ArrayList<>();
-        for (int i = START_NUMBER; i < END_NUMBER; i++) {
-            setRandomNum.add(i);
-        }
-        return setRandomNum;
+    @Override
+    public int compareTo(RandomNumbers o) {
+        return 0;
     }
 }
