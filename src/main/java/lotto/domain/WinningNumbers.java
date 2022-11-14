3a43f1d15e2f04f515e01c739102e95df85290bd
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-15 06:52:50 +0900

    feat: 로또 번호를 맞췄을 때, 등수를 반환하는 기능 추가
    보너스 볼에 유무에 따라 2등을 반환하는 메소드 추가 구현

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,23 +1,39 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class WinningNumbers {
 
     private final String DUPLICATE_NUMBER_ERROR = "[ERROR] 당첨 번호와 보너스 번호는 중복될 수 없습니다.";
-    private final List<Integer> winningNubers;
+    public final List<Integer> winningNumbers;
     private final int bonusNumber;
 
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
-        this.winningNubers = winningNumbers;
+        this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
-    }
 
-    // 당첨 번호와 보너스 번호가 다른지 파악
+    }
 
     public void validateDuplicateNumber(List<Integer> winningNumbers, int bonusNumber) {
         if (winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR);
         }
     }
+
+    private int countHitNumber(List<Integer> lottoNumbers) {
+        return (int) lottoNumbers.stream()
+                .filter(lottoNumber -> winningNumbers.stream().anyMatch(Predicate.isEqual(lottoNumber)))
+                .count();
+    }
+
+    public LottoRank calculrateRank(List<Integer> lotto) {
+        int hitCount = countHitNumber(lotto);
+        boolean hasBonus = lotto.contains(bonusNumber);
+        return LottoRank.valueOf(hitCount, hasBonus);
+    }
+
+
+
 }
