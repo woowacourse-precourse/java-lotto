882cce0a4237bb862baa1633515c4f0658b420aa
JeongHunHui <deca3248@gmail.com>
2022-11-14 23:02:35 +0900

    refactor: 로또1개의 가격을 상수로 관리, 구매금액으로 구매개수를 구하는 기능을 메소드로 분리

--- src/main/java/lotto/GameManager.java
+++ src/main/java/lotto/GameManager.java
@@ -6,10 +6,15 @@ import static lotto.GameMessageManager.*;
 import static lotto.InputUtil.*;
 
 public class GameManager {
+  public static final int LOTTO_PRICE = 1000;
   public void lottoGameStart() {
     printInputBuyAmountMessage();
     int buyAmount = getIntegerInput();
-    int buyCount = buyAmount / 1000;
+    int buyCount = getBuyCountByBuyAmount(buyAmount);
     printBuyCount(buyCount);
+  }
+
+  private int getBuyCountByBuyAmount(int buyAmount){
+    return buyAmount / LOTTO_PRICE;
   }
 }
