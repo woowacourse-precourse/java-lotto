154fc1f8b7ebaeb6b66f30062b3037f21fbeb6a1
mng990 <ryoo0504@pusan.ac.kr>
2022-11-15 05:07:07 +0900

    feat(main/java/model): Prize.java 생성
    당첨 등수별 정보를 저장한 Enum Class

---
+++ src/main/java/model/Prize.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum Prize {
+    First(6,2000000000,"6개 일치 (2,000,000,000원) - 개"),
+    Second(5.5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - 개"),
+    Third(5, 1500000, "5개 일치 (1,500,000원) - 개"),
+    Fourth(4, 50000, "4개 일치 (50,000원) - 개"),
+    Fifth(3, 5000, "3개 일치 (5,000원) - 개");
+
+    private final double hit;
+    private final int money;
+    private final String message;
+
+    public double hit(){
+        return hit;
+    }
+    public int money(){
+        return money;
+    }
+    public String message(){
+        return message;
+    }
+
+    private static final Map<Double, Prize> BY_HIT =
+            Stream.of(values()).collect(Collectors.toMap(Prize::hit, Function.identity()));
+
+    public static Prize prizeOfHit(Double hit){
+        return BY_HIT.get(hit);
+    }
+
+    Prize(double hit, int money, String meessage) {
+        this.hit = hit;
+        this.money = money;
+        this.message = meessage;
+    }
+}
