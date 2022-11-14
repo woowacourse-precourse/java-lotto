4032ffe4ebee95a943ac4ece3d84de2309b60408
이한솔 <hansol8701@naver.com>
2022-11-14 12:32:44 +0900

    feat(InputValidator): 로또 번호 유효성 검사 메소드 구현

--- src/main/java/lotto/InputValidator.java
+++ src/main/java/lotto/InputValidator.java
@@ -8,6 +8,8 @@ public class InputValidator {
     }
 
     public void validateLottoNumber(int number) {
-        throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        if (number < 1 || number > 45){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 }
