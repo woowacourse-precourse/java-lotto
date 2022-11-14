4eec1e2c2ef9e514a17a23f29565ad59b308b803
lcw3176 <lcw3176@naver.com>
2022-11-14 20:23:58 +0900

    refactor: DigitUtil 을 통한 숫자 검증

--- src/main/java/lotto/filter/NumberFilter.java
+++ src/main/java/lotto/filter/NumberFilter.java
@@ -1,5 +1,7 @@
 package lotto.filter;
 
+import lotto.util.DigitUtil;
+
 public class NumberFilter implements Filter {
     @Override
     public void doFilter(String number) {
@@ -8,10 +10,8 @@ public class NumberFilter implements Filter {
             throw new IllegalArgumentException("빈 입력은 허용되지 않습니다.");
         }
 
-        for (char i : number.toCharArray()) {
-            if (!Character.isDigit(i)) {
-                throw new IllegalArgumentException("입력한 문자가 숫자가 아닙니다.");
-            }
+        if (!DigitUtil.isDigit(number)) {
+            throw new IllegalArgumentException("입력한 문자가 숫자가 아닙니다.");
         }
     }
 }
