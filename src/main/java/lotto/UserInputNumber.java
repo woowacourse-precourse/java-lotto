9f6c90d19a2f82e652e639745d01c904cb2858c9
danneh-0427 <jc14972@gmail.com>
2022-11-15 02:28:18 +0900

    feat(UserInputNumber.java): 입력값이 1000원 보다 큰지 검증하는 기능 추가

--- src/main/java/lotto/UserInputNumber.java
+++ src/main/java/lotto/UserInputNumber.java
@@ -9,6 +9,8 @@ public class UserInputNumber {
     public UserInputNumber(String purchaseInput) {
         validatePurchaseAmount(purchaseInput);
         validateThousandUnit(Integer.parseInt(purchaseInput));
+        validCorrectRange(Integer.parseInt(purchaseInput));
+        this.purchaseInput = Integer.parseInt(purchaseInput);
     }
     private void validatePurchaseAmount(String inputNumber) {
         try {
@@ -21,6 +23,15 @@ public class UserInputNumber {
         if (inputNumber % 1000 !=0) {
             throw new IllegalArgumentException ("입력은 1000원 단위의 숫자만 가능합니다.");
         }
+    }
+    private void validCorrectRange(Integer inputNumber){
+        if(inputNumber < 1000) {
+            throw new IllegalArgumentException ("최소 1000원 이상은 구매하셔야 합니다.");
+        }
+    }
+    private String getUserInput(){
+        System.out.println("구매하실 금액을 입력해주세요");
+        return Console.readLine();
     }
 
 }
