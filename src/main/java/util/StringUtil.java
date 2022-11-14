ca855f6d0c469dcd2a6edf917791f41efc7fec48
endlessmomo <soohyuk96@naver.com>
2022-11-14 22:34:48 +0900

    update : Util 기능 추가 문자열을 Int형으로 변경

--- src/main/java/util/StringUtil.java
+++ src/main/java/util/StringUtil.java
@@ -9,11 +9,19 @@ public class StringUtil {
     public static final String NO_BLANK = "";
     public static final String DELIMITER = ",";
 
-    public static String removeBlank(String data){
+    public static String removeBlank(String data) {
         return data.replace(BLANK, NO_BLANK);
     }
 
-    public static List <Integer> parseToNumbers(String data){
+    public static List <Integer> parseToList(String data) {
         return Arrays.stream(data.split(DELIMITER)).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    public static int parseToInt(String data) {
+        try {
+            return Integer.parseInt(data);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 숫자 이외의 입력이 불가능합니다.");
+        }
     }
 }
