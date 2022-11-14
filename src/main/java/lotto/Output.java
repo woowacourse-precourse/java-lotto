570a0126c476b45dbf3eed193f3e7e1abbd51fb0
lvalentine6 <ysr1227@naver.com>
2022-11-15 05:42:32 +0900

    feat : 결과 출력 기능
    당첨 통계, 수익률 출력 기능 구현

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Output {
     public static void printPurchaseAmount() {
-        System.out.println("구입 금액을 입력해 주세요.");
+        System.out.println("구입금액을 입력해 주세요.");
     }
 
     public static void printWinningNumber() {
@@ -25,4 +25,22 @@ public class Output {
         System.out.println("보너스 번호를 입력해 주세요");
     }
 
+    public static void printPreResult() {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+
+    public static void printResult() {
+        System.out.println("3개 일치 (5,000원) - " + Calculator.convertNull(Compare.compareLotto.get(3)) + "개");
+        System.out.println("4개 일치 (50,000원) - " + Calculator.convertNull(Compare.compareLotto.get(4)) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + Calculator.convertNull(Compare.compareLotto.get(5)) + "개");
+        System.out.println(
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - " + Calculator.convertNull(Compare.compareLotto.get(6)) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + Calculator.convertNull(Compare.compareLotto.get(7)) + "개");
+    }
+
+    public static void printRate(double rate) {
+        System.out.println("총 수익률은 " + rate + "%입니다.");
+    }
 }
