67f5e4a7a458e43955c4751dc469aecf7e311bbe
osy9757 <osy9757@gmail.com>
2022-11-15 05:23:55 +0900

    feat : 금액에 따른 로또 티켓수와 로또를 발행하는 함수 추가

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -3,10 +3,15 @@ package lotto;
 import lotto.print.PrintInputMessage;
 import lotto.print.PrintOutputMessage;
 import lotto.LottoAmount;
+import lotto.CreateLottoNum;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
     public void start() {
         LottoAmount purchaseAmount = inputLottoAmount();
+        List<Lotto> userLottoNumbers = createLottoNumbers(purchaseAmount);
     }
 
     private LottoAmount inputLottoAmount() {
@@ -18,7 +23,16 @@ public class Controller {
         }
     }
 
+    private int countLottoTickets(LottoAmount purchaseAmount)  {
+        int countTickets = purchaseAmount.countTickets();
+        PrintOutputMessage.printLottoAccount(countTickets);
+        return countTickets;
+    }
+    private List<Lotto> createLottoNumbers(LottoAmount purchaseAmount) {
+        int countTickets = countLottoTickets(purchaseAmount);
 
+        System.out.println(countTickets);
 
+    }
 
 }
