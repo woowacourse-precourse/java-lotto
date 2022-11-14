8b0a68773e5f2e0311f8f59db0245706497e3aba
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-14 22:06:18 +0900

    feat(ProfitRateCalculator): 로또 수익률을 구하는 기능 구현

---
+++ src/main/java/lotto/ProfitRateCalculator.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import java.util.List;
+
+public class ProfitRateCalculator {
+
+    public double calculate(List<Lotto> lottos, int investmentMoney, List<Integer> winningNumber, int bonusNumber) {
+        double totalWinningMoney = 0;
+        for (int i = 0; i < lottos.size(); i++) {
+            totalWinningMoney += lottos.get(i).matchRank(winningNumber, bonusNumber).getMoney();
+        }
+        double profitRate = ((double) investmentMoney / totalWinningMoney) * 100;
+        profitRate = Math.round(profitRate*100)/100.0;
+        return profitRate;
+    }
+}
