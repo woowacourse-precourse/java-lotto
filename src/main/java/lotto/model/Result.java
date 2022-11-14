aca40f3ccc22dd179019e6b3ecae0f4438aa2906
Ridealist <cjsn92@gmail.com>
2022-11-15 06:35:34 +0900

    refactor(Result): 상수 필드 추가, 초기값 설정 로직 별도 분리, 메소드 및 변수 이름 수정

--- src/main/java/lotto/model/Result.java
+++ src/main/java/lotto/model/Result.java
@@ -4,56 +4,65 @@ import java.util.HashMap;
 
 public class Result {
 
-    private HashMap<Prize, Integer> result = new HashMap<>();
+    private static final int DEFAULT_PRIZE_COUNT = 0;
+    private static final int SECOND_OR_THIRD_MATCHING_COUNT = 5;
+
+    private final HashMap<Prize, Integer> prizeCount = new HashMap<>();
 
     public Result(LottoTickets lottoTickets, WinningNumbers winningNumbers, BonusNumber bonusNumber) {
-        updateLotteryResult(lottoTickets, winningNumbers, bonusNumber);
+        setDefaultPrizeCount(prizeCount);
+        updateLottoTicketsResult(lottoTickets, winningNumbers, bonusNumber);
     }
 
-    public HashMap<Prize, Integer> getResult() {
-        return result;
+    public HashMap<Prize, Integer> getPrizeCount() {
+        return prizeCount;
     }
 
-    private void updateLotteryResult(LottoTickets lottoTickets, WinningNumbers winningNumbers,
-                                     BonusNumber bonusNumber) {
+    private void setDefaultPrizeCount(HashMap<Prize, Integer> prizeCount) {
+        for (Prize e : Prize.values()) {
+            prizeCount.put(e, DEFAULT_PRIZE_COUNT);
+        }
+    }
+
+    private void updateLottoTicketsResult(LottoTickets lottoTickets, WinningNumbers winningNumbers,
+                                          BonusNumber bonusNumber) {
         for (Lotto lotto : lottoTickets.getTickets()) {
             updateLottoResult(lotto, winningNumbers, bonusNumber);
         }
     }
 
     private void updateLottoResult(Lotto lotto, WinningNumbers winningNumbers, BonusNumber bonusNumber) {
-        int winningCnt = countMatchingNumbers(lotto, winningNumbers);
-        boolean isBonus = isBonusCorrect(lotto, bonusNumber);
-        filterSecondOrThird(winningCnt, isBonus);
+        int matchingCnt = countMatchingNumbers(lotto, winningNumbers);
+        boolean isBonus = isBonusNumberCorrect(lotto, bonusNumber);
+        filterSecondOrThirdPrize(matchingCnt, isBonus);
         for (Prize e : Prize.values()) {
-            result.put(e, result.getOrDefault(e, 0));
-            if (e.getWinningCount() == winningCnt && winningCnt != 5) {
-                result.put(e, result.get(e) + 1);
+            if (matchingCnt != SECOND_OR_THIRD_MATCHING_COUNT && e.getMatchingCount() == matchingCnt) {
+                prizeCount.put(e, prizeCount.get(e) + 1);
             }
         }
     }
 
-    private void filterSecondOrThird(int winningCnt, boolean isBonus) {
-        if (winningCnt == 5) {
+    private void filterSecondOrThirdPrize(int matchingCnt, boolean isBonus) {
+        if (matchingCnt == SECOND_OR_THIRD_MATCHING_COUNT) {
             if (isBonus) {
-                result.put(Prize.SECOND, result.getOrDefault(Prize.SECOND, 0) + 1);
+                prizeCount.put(Prize.SECOND, prizeCount.get(Prize.SECOND) + 1);
                 return;
             }
-            result.put(Prize.THIRD, result.getOrDefault(Prize.THIRD, 0) + 1);
+            prizeCount.put(Prize.THIRD, prizeCount.get(Prize.THIRD) + 1);
         }
     }
 
     private int countMatchingNumbers(Lotto lotto, WinningNumbers winningNumbers) {
-        int winningCount = 0;
+        int matchingCount = 0;
         for (int number : winningNumbers.getWinningNumbers()) {
             if (lotto.getNumbers().contains(number)) {
-                winningCount++;
+                matchingCount++;
             }
         }
-        return winningCount;
+        return matchingCount;
     }
 
-    private boolean isBonusCorrect(Lotto lotto, BonusNumber bonusNumber) {
+    private boolean isBonusNumberCorrect(Lotto lotto, BonusNumber bonusNumber) {
         return lotto.getNumbers().contains(bonusNumber.getBonusNumber());
     }
 }
