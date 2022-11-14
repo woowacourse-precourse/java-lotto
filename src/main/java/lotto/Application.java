49ac46912488eecc38e0a59234a51a7d9ffcc50b
hongsikcho <jjdd9758@naver.com>
2022-11-15 04:50:28 +0900

    refactor :9 예외_테스트 통과하도록 예외출력 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,10 @@ import lotto.controller.LottoController;
 public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
-        lottoController.startGame();
+        try {
+            lottoController.startGame();
+        }catch (IllegalArgumentException exception){
+            System.out.print(exception.getMessage());
+        }
     }
 }
