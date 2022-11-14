c13ed323b31d5dd3c9298127fb6c64828547357d
cocomozila <joseyun94@naver.com>
2022-11-14 22:37:08 +0900

    feat(WinningNumber): 당첨 번호 생성 기능

---
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import lotto.setting.Setting;
+
+public class WinningNumber {
+
+    private Lotto lotto;
+
+    // 당첨 번호 생성 기능
+    public void inputWinningNumber(String input) {
+        validateInput(input);
+    }
+
+    // 올바른 당첨 번호 입력값인지 검증하는 기능
+    private void validateInput(String input) {
+    }
+
+
+}
