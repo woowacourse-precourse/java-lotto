46a5f9d44991f176955c2fe6f2cd23851f30b04a
yummygyudon <bang2brew@gmail.com>
2022-11-15 07:30:08 +0900

    FEAT(domain): 로또 객체 값 비교 및 출력을 위한 toString() 오버라이딩
    - 출력 요구사항 형식에 맞추어 출력되도록 구성
    - Converter Test 내 `convertRandomNumbersToLotto`에 대한 테스트의 값 비교로도 사용

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,4 +17,21 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        for(int i = 0 ; i < numbers.size()-1 ; i++){
+            builder.append(numbers.get(i));
+            builder.append(", ");
+        }
+        builder.append(numbers.get(5));
+        builder.append("]");
+        return builder.toString();
+    }
+
+
+
 }
