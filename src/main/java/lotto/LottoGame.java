8cf462d2db513c963b54bf6655269d514df7d67b
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-15 03:13:34 +0900

    feat : 로또s 객체 생성

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -3,9 +3,11 @@ package lotto;
 public class LottoGame {
     private static final int MONEY_UNIT = 1000;
     private final int amount;
+    private final Lottos lottos;
 
     public LottoGame(Money money) {
         this.amount = getLottoAmount(money.getMoney());
+        this.lottos = new Lottos(amount);
     }
 
     private int getLottoAmount(int money) {
