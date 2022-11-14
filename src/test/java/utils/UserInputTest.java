a837667e26ef5874adec09a696be39cac7ab7344
MinSungKim <timkim01@naver.com>
2022-11-14 19:54:52 +0900

    test: 당첨번호 입력 형식이 올바른지 유효성검사 테스트코드 작성

--- src/test/java/utils/UserInputTest.java
+++ src/test/java/utils/UserInputTest.java
@@ -36,4 +36,17 @@ class UserInputTest {
         assertEquals("[ERROR] 1,000원 단위로만 입력해주세요.", exception.getMessage());
     }
 
+    @Test
+    void 당첨번호_입력_형식이_올바른지_유효성검사_테스트(){
+        String input = "1 2 3 4 5 6";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            UserInput.inputLottoNumber();
+        });
+        assertEquals("[ERROR] 형식에 맞게 입력해주세요. 번호는 쉼표(,)를 기준으로 구분합니다.", exception.getMessage());
+    }
+
 }
