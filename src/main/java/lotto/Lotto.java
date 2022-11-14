110840330d64a08135cf6e92508179617644e485
dvlpr0 <rladjswkd@naver.com>
2022-11-14 19:22:13 +0900

    feat: 금액에 맞게 로또를 발행하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,10 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 
 public class Lotto {
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
@@ -10,6 +12,9 @@ public class Lotto {
     private static final String INVALID_PURCHASE_AMOUNT_MESSAGE = ERROR_MESSAGE + " 구매 금액은 1,000원 단위여야 합니다.";
     private static final char INVALID_MOST_SIGNIFICANT_DIGIT = '0';
     private static final int SMALLEST_UNIT = 1000;
+    private static final int SMALLEST_LOTTERY_NUMBER = 1;
+    private static final int BIGGEST_LOTTERY_NUMBER = 45;
+    private static final int LOTTERY_NUMBER_COUNT = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -18,7 +23,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTERY_NUMBER_COUNT) {
             throw new IllegalArgumentException();
         }
     }
@@ -53,5 +58,16 @@ public class Lotto {
         purchaseAmount = readLine();
         validatePurchaseAmount(purchaseAmount);
         return (purchaseAmount);
+    }
+
+    public List<List<Integer>> pickLotteryNumbers(int ticketCount) {
+        List<List<Integer>> lotteryTickets = new ArrayList<>();
+
+        for (int i = 0; i < ticketCount; i++) {
+            lotteryTickets.add(
+                pickUniqueNumbersInRange(SMALLEST_LOTTERY_NUMBER, BIGGEST_LOTTERY_NUMBER, LOTTERY_NUMBER_COUNT)
+            );
+        }
+        return (lotteryTickets);
     }
 }
