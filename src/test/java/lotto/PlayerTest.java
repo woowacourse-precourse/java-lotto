2ccb16b9d673b17c59cb2743363facdcd961662e
chaeyeong222 <iky1505@naver.com>
2022-11-14 16:07:17 +0900

    test : 로또구입금액 사용자입력값 예외 테스트

---
+++ src/test/java/lotto/PlayerTest.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest extends NsTest {
+    
+    @Test
+    void 로또구입금액_1000단위_아닌_경우_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1250"))
+                    .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 로또구입금액_숫자_외_문자_입력_시_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1xx"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+
+
+}
