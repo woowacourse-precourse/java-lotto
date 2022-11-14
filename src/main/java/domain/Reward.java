1d7e721db13ce50b002fb9a211b90c49a3564915
bsh1603 <bsh1603@github.com>
2022-11-15 02:38:20 +0900

    Feat: 수익률 계산

--- src/main/java/domain/Reward.java
+++ src/main/java/domain/Reward.java
@@ -9,7 +9,11 @@ public class Reward {
         for(Rank rank : map.keySet()){
             sum += map.get(rank) * rank.getReward();
         }
-
         return sum;
+    }
+
+    public double percentage(Long sum, int money){
+        double percent = Math.round((sum / (double) money)*10) / 10.0;
+        return percent;
     }
 }
