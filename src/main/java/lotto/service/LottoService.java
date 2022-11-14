4ca5a0d42f82485cc26d2b5b8c4290a94f12578d
gnoesnooj <306yyy@naver.com>
2022-11-14 13:38:01 +0800

    feat(LottoService) : LottoService 당첨 계산 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -37,4 +37,16 @@ public class LottoService {
 
         return sb.toString();
     }
+
+    public String 당첨계산(List<Integer> winningNumbers, int bonusNumber) {
+        customer.checkJackpot(winningNumbers, bonusNumber);
+        int[] winnings = customer.getWinningList();
+        return "당첨 통계\n"
+                + "---\n"
+                + "3개 일치 (5,000원) - " + winnings[4] + "개\n"
+                + "4개 일치 (50,000원) - " + winnings[3] + "개\n"
+                + "5개 일치 (1,500,000원) - " + winnings[2] + "개\n"
+                + "5개 일치, 보너스 볼 일치 (30,000,000원) - " + winnings[1] + "개\n"
+                + "6개 일치 (2,000,000,000원) - " + winnings[0] + "개\n";
+    }
 }
