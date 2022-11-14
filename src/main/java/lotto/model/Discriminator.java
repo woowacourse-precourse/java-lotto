08739cda5d4b4709954dfe6f65eb46f839e13f03
gomljo <gomljo0307@naver.com>
2022-11-14 16:16:27 +0900

    feat(Discriminator.java): 로또와 추첨 번호를 비교하여 일치하는 갯수를 반환하는 기능 구현

--- src/main/java/lotto/model/Discriminator.java
+++ src/main/java/lotto/model/Discriminator.java
@@ -3,11 +3,23 @@ package lotto.model;
 import java.util.List;
 
 public class Discriminator {
+    private static final int ZERO = 0;
+    private static final int COUNT = 1;
     private final List<Integer> raffleNumbers;
     private final int bonusNumber;
 
     public Discriminator(RaffleNumber raffleNumber){
         this.raffleNumbers=raffleNumber.getRaffleNumbers();
         this.bonusNumber=raffleNumber.getBonusNumber();
+    }
+
+    public int guessLottoNumber(Lotto lotto) {
+        int numberOfMatches = ZERO;
+        for (int raffleNumber : raffleNumbers) {
+            if (lotto.has(raffleNumber)) {
+                numberOfMatches += COUNT;
+            }
+        }
+        return numberOfMatches;
     }
 }
