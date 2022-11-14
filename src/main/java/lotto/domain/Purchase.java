de6a669eedb004fa68b39fa2c9571369f1428f24
Yeongseon Park <yseon406@gmail.com>
2022-11-14 14:59:47 +0900

    feat(lotto): 사용자 금액 입력 받기 기능 구현

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -1,12 +1,21 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Purchase {
+    int money;
+
     public void inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
+        int userInput = Integer.parseInt(Console.readLine());
+        if (isValid(userInput)) {
+            this.money = userInput;
+        }
+
     }
 
-    public boolean isValid(int money) {
-        return money % 1000 == 0;
+    public boolean isValid(int userInput) {
+        return userInput % 1000 == 0;
     }
 
     public int canBuyLotto() {
