5301d3f51b170b63cbe27af86892b89f61bbdc2b
hhiyeon <hhyyeon0214@gmail.com>
2022-11-15 04:27:43 +0900

    feat: error-create error message enum

--- src/main/java/lotto/utils/Error.java
+++ src/main/java/lotto/utils/Error.java
@@ -1,4 +1,15 @@
 package lotto.utils;
 
 public enum Error {
+    INVALID_LOTTO_SIZE("로또 번호의 개수가 6개가 아닙니다.");
+
+    private final String errorMessage;
+
+    Error(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
 }
