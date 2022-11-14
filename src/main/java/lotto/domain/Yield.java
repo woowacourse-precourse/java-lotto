d5f2a671e6b62b044d8bf5c9f6f7c259d05e79ab
Eunkeee <eunkeeeee@gmail.com>
2022-11-14 20:35:04 +0900

    refcator(Yield): 메소드 분리 및 하드코딩 제거
    calculateTotalRevenue, calculateYield, Formatter#formatYield

--- src/main/java/lotto/domain/Yield.java
+++ src/main/java/lotto/domain/Yield.java
@@ -1,15 +1,27 @@
 package lotto.domain;
 
+import static lotto.util.Formatter.formatYield;
+
 public class Yield {
 
+    public static final int PERCENT_MULTIPLICATION = 100;
     private final String yield;
 
     public Yield(double totalPurchase) {
+        double totalRevenue = calculateTotalRevenue();
+        this.yield = formatYield(totalPurchase, totalRevenue);
+    }
+
+    private static double calculateTotalRevenue() {
         double totalRevenue = 0;
         for (Rank rank : Rank.values()) {
             totalRevenue += rank.getCount() * rank.getCashPrize();
         }
-        this.yield = String.format("%.1f", totalRevenue / totalPurchase * 100);
+        return totalRevenue;
+    }
+
+    public static double calculateYield(double totalPurchase, double totalRevenue) {
+        return totalRevenue / totalPurchase * PERCENT_MULTIPLICATION;
     }
 
     public String getYield() {
--- src/main/java/lotto/util/Formatter.java
+++ src/main/java/lotto/util/Formatter.java
@@ -1,5 +1,6 @@
 package lotto.util;
 
+import static lotto.domain.Yield.calculateYield;
 import static lotto.util.Constants.LOTTO_RANGE_MAX;
 import static lotto.util.Constants.LOTTO_RANGE_MIN;
 
@@ -8,8 +9,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Formatter {
+    public static final String PERCENT_FORMAT = "%.1f";
+    public static final String SPACE_REGEX = "\\s";
+    public static final String NO_SPACE = "";
+    public static final String COMMA_REGEX = ",";
+
     public static String removeSpace(String input) {
-        return input.replaceAll("\\s", "");
+        return input.replaceAll(SPACE_REGEX, NO_SPACE);
     }
 
     public static List<Integer> formatWinningNumbersInput(String input) {
@@ -17,7 +23,7 @@ public class Formatter {
     }
 
     private static List<String> separateStringByComma(String input) {
-        return Arrays.asList(input.split(","));
+        return Arrays.asList(input.split(COMMA_REGEX));
     }
 
     private static List<Integer> convertStringListToIntList(List<String> list) {
@@ -27,7 +33,11 @@ public class Formatter {
 
     public static void verifyRangeOfLottoNumber(Integer number) {
         if (number < LOTTO_RANGE_MIN || number > LOTTO_RANGE_MAX) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("1부터 45 사이의 숫자만 입력 가능합니다.");
         }
+    }
+
+    public static String formatYield(double totalPurchase, double totalRevenue) {
+        return String.format(PERCENT_FORMAT, calculateYield(totalPurchase, totalRevenue));
     }
 }
