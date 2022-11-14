dc762055cf295dd4ae834a9bce6478e0f85cbde3
bellljun3395 <195850@jnu.ac.kr>
2022-11-14 15:08:00 +0900

    feat(Validate): 구현 validateSize
    - lottoNumbers의 크기를 검증한다.

--- src/main/java/lotto/domain/lotto/util/Validate.java
+++ src/main/java/lotto/domain/lotto/util/Validate.java
@@ -1,10 +1,22 @@
 package lotto.domain.lotto.util;
 
+import lotto.domain.lotto.LottoNumber;
+
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Validate {
     private static final String WARM = "[ERROR] ";
     private static final int START_RANGE = 1;
     private static final int END_RANGE = 45;
     private static final String WARM_INPUT_NUMBER_RANGE = WARM + START_RANGE + "~" + END_RANGE + " 사이의 수를 입력해 주세요";
+
+    public static void validateSize(List<LottoNumber> lottoNumbers, String msg, Predicate<List<LottoNumber>> p) {
+        if (p.test(lottoNumbers)) {
+            System.out.println(msg);
+            throw new IllegalArgumentException(msg);
+        }
+    }
 
     public static void validateLottoNumberRange(int lottoNumber) {
         if (lottoNumber < START_RANGE || END_RANGE < lottoNumber) {
@@ -13,3 +25,4 @@ public class Validate {
         }
     }
 }
+
