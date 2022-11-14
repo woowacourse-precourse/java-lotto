e306c9150a56fd4a5b25f425d6b697b7e26c6a7b
jjmin9797 <jjmin9797@naver.com>
2022-11-14 18:30:30 +0900

    test(ExceptionServiceTest): 입력받은 금액이 숫자인지 확인하는 테스트
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/test/java/lotto/service/ExceptionServiceTest.java
+++ src/test/java/lotto/service/ExceptionServiceTest.java
@@ -1,4 +1,21 @@
+package lotto.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+@DisplayName("예외 처리 테스트")
 class ExceptionServiceTest {
-  
+
+    @Test
+    @DisplayName("입력받은 금액이 숫자인지 확인한다.")
+    void exceptionHandlingOfPurchaseAmountTest() {
+        ExceptionService exceptionService = new ExceptionService();
+        String amount = "팔천원";
+        assertThatThrownBy(() -> exceptionService.exceptionHandlingOfPurchaseAmount(amount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
