d33e9e3966aed770f8c79ff9edd15fd547f89681
chaos0103 <lyt1228@naver.com>
2022-11-14 14:35:57 +0900

    feat(Lotto): 구입 금액만큼 로또를 발행하는 기능 구현

--- src/main/java/lotto/domain/impl/IssueImpl.java
+++ src/main/java/lotto/domain/impl/IssueImpl.java
@@ -2,19 +2,31 @@ package lotto.domain.impl;
 
 import lotto.Lotto;
 import lotto.domain.Issue;
+import lotto.domain.NumberGenerator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static lotto.message.ExceptionMessage.AMOUNT_EXCEPTION;
 
 public class IssueImpl implements Issue {
 
+    private final NumberGenerator numberGenerator = new NumberGeneratorImpl();
+
     //예외
     //1. 금액이 1,000원으로 나누어 떨어지지 않는 경우
     @Override
     public List<Lotto> createLotto(int amount) {
         validate(amount);
-        return null;
+        int count = amount / 1000;
+
+        List<Lotto> result = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = numberGenerator.createRandomNumbers();
+            Lotto lotto = new Lotto(numbers);
+            result.add(lotto);
+        }
+        return result;
     }
 
     private void validate(int amount) {
