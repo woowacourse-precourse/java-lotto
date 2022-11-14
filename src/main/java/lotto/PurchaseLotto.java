ac7cf8aae63ae5c0c866c1fb17596dc50fb4ccaa
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-15 00:22:03 +0900

    refactor(PurchaseLotto) : 랜덤한 리스트 값생성 메소드 변경.
    정렬 기능 삭제.

--- src/main/java/lotto/PurchaseLotto.java
+++ src/main/java/lotto/PurchaseLotto.java
@@ -1,10 +1,9 @@
 package lotto;
-import java.util.Collections;
+
 import java.util.LinkedList;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 import ui.PurchasePrint;
-import constant.ConstantLottoProgram;
 import exceptioncase.PurchaseException;
 import camp.nextstep.edu.missionutils.Randoms;
 public class PurchaseLotto {
@@ -24,19 +23,19 @@ public class PurchaseLotto {
     public static List<Lotto> drawLotto(int lottocount){
         List<Lotto> answer = new LinkedList<>();
         for(int i=0 ;i<lottocount ; i++){
-            answer.add(new Lotto(makeRandomNumbers()));
+            answer.add(makeRandomNumbers());
         }
         return answer;
     }
-    private static List<Integer> makeRandomNumbers(){
-        List<Integer> numbers = new LinkedList<>();
-        while(numbers.size()<ConstantLottoProgram.LOTTO_LENGTH.getLength()){
-            int randomnumber = Randoms.pickNumberInRange(1,45);
-            if(numbers.contains(randomnumber))
-                continue;
-            numbers.add(randomnumber);
+    private static Lotto makeRandomNumbers(){
+        Lotto lotto;
+        while(true){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lotto = new Lotto(numbers);
+            if(lotto.overlapsFind(lotto.getNumbers())){
+                break;
+            }
         }
-        Collections.sort(numbers);
-        return numbers;
+        return lotto;
     }
 }
