11f95c0885c00d2fba3e8e3c158558bf1ed46287
Beginner <dkstmdwn17@naver.com>
2022-11-14 20:33:55 +0900

    feat : 통계를 위해 필요한 Enum을 작성하였다.
    accord : 당첨 번호와 비교해서 몇 번 맞췄는지
    
    prize : 로또 번호가 당첨 번호와 일치함에 따라 받을 상금을 의미한다.

--- src/main/java/lotto/domain/enumeration/Rating.java
+++ src/main/java/lotto/domain/enumeration/Rating.java
@@ -1,4 +1,26 @@
 package lotto.domain.enumeration;
 
 public enum Rating {
+    FIRST(6,2_000_000_000),
+    SECOND(5,30_000_000),
+    THIRD(5,1_500_000),
+    FOURTH(4,50_000),
+    FIFTH(3,5_000),
+    NONE(0,0);
+
+    private final int accord;
+    private final int prize;
+
+    Rating(int accord, int prize) {
+        this.accord = accord;
+        this.prize = prize;
+    }
+
+    public int getAccord() {
+        return accord;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
 }
