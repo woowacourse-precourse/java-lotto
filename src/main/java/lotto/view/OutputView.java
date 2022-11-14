6c8181a23fcde1afb868c9e5da7498d51907b206
footdev <ekclstkfka5@gmail.com>
2022-11-15 01:57:13 +0900

    feat: UI를 담당하는 기능 구현
    필요한 안내를 사용자에게 보여주는 기능을 구현함.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,50 @@
 package lotto.view;
 import java.lang.Enum;
+import java.sql.SQLOutput;
+
 public class OutputView {
+    private static final String START_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String NOTIFICATION_MESSAGE = "개를 구매했습니다.";
+    private static final String RECEIVE_MESSAGE = "를 입력해 주세요.";
+    private static final String WINNING_MESSAGE = "당첨 번호";
+    private static final String BONUS_MESSAGE = "보너스 번호";
+    private static final String RESULT_MESSAGE = "%d개 일치 (%s원) - %d개";
+    private static final String YIELD_MESSAGE = "총 수익률은 %f%입니다.";
+
+    public static void printBlank() {
+        System.out.println();
+    }
+
+    public static void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
+
+    public static void printNotificationMessage(int count) {
+        System.out.println(count + NOTIFICATION_MESSAGE);
+    }
+
+    public static void printLottoTickets(String lotto) {
+        System.out.println(lotto);
+    }
+
+    public static void printReceiveWinningMessage() {
+        System.out.println(WINNING_MESSAGE + RECEIVE_MESSAGE);
+    }
+
+    public static void printReceiveBonusMessage() {
+        System.out.println(BONUS_MESSAGE + RECEIVE_MESSAGE);
+    }
+
+    public static void printBeforeResultMessage() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+
+    public static void printResultMessage(int count, String reword, int winningNumber) {
+        System.out.printf(RESULT_MESSAGE, count, reword, winningNumber);
+    }
+
+    public static void printYieldMessage(float yield) {
+        System.out.printf(YIELD_MESSAGE, yield);
+    }
 }
