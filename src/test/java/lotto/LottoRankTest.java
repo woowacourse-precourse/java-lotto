f3e7d8eeb774cac9df2da9b3d2e3b157731ff192
anneu <b.imnuj@gmail.com>
2022-11-15 01:12:17 +0900

    refactor: 당첨 순위를 구하는 기능 테스트 케이스 추가

--- src/test/java/lotto/LottoRankTest.java
+++ src/test/java/lotto/LottoRankTest.java
@@ -1,19 +1,17 @@
 package lotto;
 
-import static lotto.LottoRank.RANK1;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoRankTest {
-    @DisplayName("당첨 번호와 6개가 일치할 경우 RANK1이다")
-    @Test
-    void findRank() {
-        LottoRank rank = LottoRank.findRank(6);
-        Assertions.assertThat(rank).isEqualTo(RANK1);
+    @DisplayName("당첨 순위를 구한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"0, NONE", "1, NONE", "2, NONE", "3, RANK5", "4, RANK4", "6, RANK1"})
+    void findRank(int matchingCount, LottoRank expected) {
+        LottoRank rank = LottoRank.findRank(matchingCount);
+        Assertions.assertThat(rank).isEqualTo(expected);
     }
 
     @DisplayName("로또 당첨 순위에 따른 당첨 금액 총합을 구한다.")
