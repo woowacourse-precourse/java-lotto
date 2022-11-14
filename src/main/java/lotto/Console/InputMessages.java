b5702218d98b9dff1dfe319a371d26ec7d5b3e00
jhsseonn <hosu0125@gmail.com>
2022-11-15 00:22:40 +0900

    feat: console readLine 기능 함수 구현

---
+++ src/main/java/lotto/Console/InputMessages.java
@@ -0,0 +1,9 @@
+package lotto.Console;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputMessages {
+    public String readLine() {
+        return Console.readLine();
+    }
+}
