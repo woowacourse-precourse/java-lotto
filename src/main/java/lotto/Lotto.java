662417fae32398da3544d3385101457a651b31f0
gyumin159 <gyumin159@naver.com>
2022-11-14 12:11:16 +0900

    feat : 당첨번호를 통해 로또가 몇 등인지 반환하는 메소드

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -20,4 +20,24 @@ public class Lotto {
     public void printLotto(){
         System.out.println(this.numbers);
     }
+
+    public int compareWithWinningNumbers(List<Integer> winningNumbers){
+        List<Integer> withoutBonusNumber = winningNumbers.subList(0,6);
+        int sameNumCount = 0;
+        for (Integer num :this.numbers){
+            if (withoutBonusNumber.contains(num)){
+                sameNumCount++;
+            }
+        }
+        return whichPlace(sameNumCount);
+    }
+
+    private int whichPlace(int sameNumCount){ // 같은 번호가 얼마나 맞았는지를 통해 몇 등인지 계산
+        int place = Math.abs(sameNumCount - 6);
+        if (place > 3) { // 5등 이하, 즉 같은 번호가 3개도 안되는 경우들
+            return 4;
+        }
+        return place;
+    }
+
 }
