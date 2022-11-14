2379c473cab9b66f69dd89e000a65c7c4bfcb462
gomljo <gomljo0307@naver.com>
2022-11-15 04:49:05 +0900

    feat(LottoGameController.java): 로또 게임 컨트롤러 생성

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,15 @@
+package lotto.controller;
+
+import lotto.model.*;
+import lotto.view.LottoGameView;
+
+public class LottoGameController {
+    LottoGameView lottoGameView = new LottoGameView();
+    Buyer buyer = new Buyer();
+    Lottery lottery = new Lottery();
+    RaffleNumber raffleNumber = new RaffleNumber();
+    Discriminator discriminator = new Discriminator();
+    Revenue revenue = new Revenue();
+    
+
+}
