78206f1838fac9c24e76e84490843e06740e12f6
DevJay1024 <devjay1024@gmail.com>
2022-11-14 17:55:28 +0900

    feat: 사용자 입력 예외처리 문구 관리 기능 작성

---
+++ src/main/java/lotto/InvalidInputMessage.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class InvalidInputMessage {
+
+    public static String ERR_USER_MONEY = "로또 구매금액이 유효하지 않습니다.";
+    public static String ERR_WINNING_NUMBERS = "로또 당첨번호 입력값이 유효하지 않습니다.";
+    public static String ERR_BONUS_NUMBER = "보너스 입력값이 유효하지 않습니다.";
+}
