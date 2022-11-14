44cafe687b4733ecd52c3e9db3e453646634b6c3
d0ri123 <lsh81550@gmail.com>
2022-11-14 10:17:58 +0900

    fix(lotto): 숫자 5개 일치한 상태에서 보너스 번호까지 일치한 케이스의 number 변경

--- src/main/java/lotto/WinningStatus.java
+++ src/main/java/lotto/WinningStatus.java
@@ -4,7 +4,7 @@ public enum WinningStatus {
     CASE1(3,5000),
     CASE2(4,50_000),
     CASE3(5,1_500_000),
-    CASE4(5,30_000_000),
+    CASE4(7,30_000_000),
     CASE5(6,2_000_000_000);
 
     private int matchNumber;
