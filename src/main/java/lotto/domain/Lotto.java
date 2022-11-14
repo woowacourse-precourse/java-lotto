d8489bc80e49d75e52c6ad36545619816a8e5511
LeeJejune <leejejune0625@gmail.com>
2022-11-14 21:32:48 +0900

    feat: 기본 로또 정보 설정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,13 @@
+public class Lotto {
     private static final String DUPLICATE_ERROR = "[ERROR] 로또 개수는 중복이 불가능합니다.";
     private static final int LOTTO_COUNT = 6;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     private static final String DELIMITER = ",";
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
