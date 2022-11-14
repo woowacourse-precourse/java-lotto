c1d6357b24cf38939bcbbadd6a6ac9248d0a4263
namtndus <rnrmf0721@gmail.com>
2022-11-14 11:09:45 +0900

    feat(CalculatedService): '금액을 저장할 money변수를 선언(변수)'를 구현

--- src/main/java/Services/CalculatedService.java
+++ src/main/java/Services/CalculatedService.java
@@ -4,11 +4,13 @@ import Dto.Money;
 
 public class CalculatedService {
 
-    public int lottoSize(Money size){
-        int number = size.getMoney();
-        if(number % 1000 != 0){
+    private int money;
+
+    public int lottoSize(Money money){
+        this.money = money.getMoney();
+        if(this.money % 1000 != 0){
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 돈을 입력하시오");
         }
-        return number / 1000;
+        return this.money / 1000;
     }
 }
