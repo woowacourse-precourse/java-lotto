3cfb4a122e1b4941b69f52ec09dd3f795c73a722
KimSunU <kgw6147@naver.com>
2022-11-14 16:51:37 +0900

    [feat]Lotto - validate 조건  구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,13 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    public static final int LOTTO_SIZE = 6;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -11,10 +15,32 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validateSize(numbers);
+        validateRangeOfNumber(numbers);
+        validateUnique(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers){
+        if(numbers.size() != LOTTO_SIZE){
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 개수가 올바르지 않습니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateUnique(List<Integer> numbers){
+        List<Integer> checkUnique = new ArrayList<>();
+        for(int number: numbers){
+            if(checkUnique.contains(number)){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복을 허용하지 않습니다.");
+            }
+            checkUnique.add(number);
+        }
+    }
+
+    private void validateRangeOfNumber(List<Integer> numbers){
+        for(int number : numbers){
+            if(number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 1-45입니다.");
+            }
+        }
+    }
 }
