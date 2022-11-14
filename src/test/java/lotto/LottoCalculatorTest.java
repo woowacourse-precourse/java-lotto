6d8a786b8d6db1e6c4c8a3399b1d3b0c0a677d61
Jeangyu <gjwlsrb1700@naver.com>
2022-11-15 02:47:09 +0900

    [FEAT: test] 구매 가능한 티켓 개수 확인 하는 테스트 구현

--- src/test/java/lotto/LottoCalculatorTest.java
+++ src/test/java/lotto/LottoCalculatorTest.java
@@ -1,10 +1,22 @@
 package lotto;
 
-import org.junit.jupiter.api.Test;
+import lotto.domain.LottoCalculator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoCalculatorTest {
 
+    LottoCalculator lottoCalculator;
+
     // 구매 가능한 티켓 개수를 구하는 메서드 체크
+    @ParameterizedTest
+    @ValueSource(ints = {8000, 16000, 25000, 3000})
+    void 구매_가능한_티켓_개수_확인(int budget){
+        lottoCalculator = new LottoCalculator(budget);
+        assertThat(lottoCalculator.getTicketSize()).isEqualTo((int)budget / 1000);
+    }
 
 
     // 이익을 구하는 메서드 체크
