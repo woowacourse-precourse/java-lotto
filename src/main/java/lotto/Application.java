6ba22b6ec8eeb84e0995fb48a209c83ecdb8c3aa
alpacaHyun <flaehdan@gmail.com>
2022-11-15 00:54:11 +0900

    type : feat ✨ 예외사항 처리
    body :
    
    footer :

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,25 +8,28 @@ public class Application {
     public static List<Integer> winningNumbers = new ArrayList<>();
 
     public static void main(String[] args) {
+        try {
+            // 구입금액 입력받고 개수 출력
+            LottoInput.inputNumber();
+            LottoInput.theNumberOfLotto();
 
-        // 구입금액 입력받고 개수 출력
-        LottoInput.inputNumber();
-        LottoInput.theNumberOfLotto();
+            //구입한 만큼 로또 생성
+            LottoGenerator.myLottoNumber();
 
-        //구입한 만큼 로또 생성
-        LottoGenerator.myLottoNumber();
+            //1등 번호 입력
+            Lotto winnerLotto = new Lotto(winningNumbers);
+            winnerLotto.inputLotto();
 
-        //1등 번호 입력
-        Lotto winnerLotto = new Lotto(winningNumbers);
-        winnerLotto.inputLotto(winningNumbers);
+            // 보너스 번호 입력
+            Lotto.bonusNum();
 
-        // 보너스 번호 입력
-        Lotto.bonusNum();
-
-        //당첨번호와 비교하기
-        CompareLotto.compareLotto();
-        CompareLotto.printCompareLotto();
-        CompareLotto.printRateOfReturn();
+            //당첨번호와 비교하기
+            CompareLotto.compareLotto(winningNumbers);
+            CompareLotto.printCompareLotto();
+            CompareLotto.printRateOfReturn();
+        }catch(IllegalArgumentException a){
+            System.out.println("[ERROR] ");
+        }
     }
 
 }
