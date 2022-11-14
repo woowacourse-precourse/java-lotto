401641e9dc9724ceaa2e31a5c0a3721d224c0d0c
jjmin9797 <jjmin9797@naver.com>
2022-11-14 18:38:55 +0900

    test(AmountExceptionTest): 입력받은 금액이 1000원으로 나누어 떨어지지 않는 경우 예외 발생 확인 테스트
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/test/java/lotto/service/ExceptionServiceTest.java
+++ src/test/java/lotto/service/ExceptionServiceTest.java
@@ -10,12 +10,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ExceptionServiceTest {
 
     @Test
-    @DisplayName("입력받은 금액이 숫자인지 확인한다.")
+    @DisplayName("입력받은 금액이 숫자가 아니면 예외가 발생한다.")
     void exceptionHandlingOfPurchaseAmountTest() {
         ExceptionService exceptionService = new ExceptionService();
         String amount = "팔천원";
         assertThatThrownBy(() -> exceptionService.exceptionHandlingOfPurchaseAmount(amount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("입력받은 금액이 1000원으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    void checkIfAmountIsDivisibleByOneThousandTest() {
+        ExceptionService exceptionService = new ExceptionService();
+        String amount = "8000";
+        assertThatThrownBy(() -> exceptionService.exceptionHandlingOfPurchaseAmount(amount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 
 }
