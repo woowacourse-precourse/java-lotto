fe8e8bf341362397b056302de69832149f9938f2
vcho1958 <gusckd57@gmail.com>
2022-11-15 04:29:31 +0900

    test(Money): 불필요 박싱 제거

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -11,21 +11,21 @@ class MoneyTest {
     @DisplayName("구매 금액이 음수일 때 예외를 반환합니다.")
     @Test
     void createMoneyWithNegative(){
-        Assertions.assertThatThrownBy(()->new Money(Long.valueOf(-1000)))
+        Assertions.assertThatThrownBy(()->new Money(-1000L))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("구매 금액이 1000으로 나누어 떨어지지 않을 때 예외를 반환합니다.")
     @Test
     void createMoneyWithNotDevidedNumberWithThousand(){
-        Assertions.assertThatThrownBy(()->new Money(Long.valueOf(1023)))
+        Assertions.assertThatThrownBy(()->new Money(1023L))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("생성 테스트")
     @Test
     void createMoney(){
-        Assertions.assertThat(new Money(Long.valueOf(2000)))
+        Assertions.assertThat(new Money(2000L))
                 .isInstanceOf(Money.class);
     }
 }
