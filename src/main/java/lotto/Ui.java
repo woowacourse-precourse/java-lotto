57b09e6d69ed88316dd08d5e3c0c49b8f1648782
hyeonseog-won <smreo75@gmail.com>
2022-11-14 23:07:47 +0900

    feat: 당첨 통계 print 기능 추가

--- src/main/java/lotto/Ui.java
+++ src/main/java/lotto/Ui.java
@@ -26,4 +26,18 @@ public class Ui {
             System.out.println(tempLotto);
         }
     }
+
+    public void printWinningStatistics() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.printf("3개 일치 (5,000원) - %d개%n");
+        System.out.printf("4개 일치 (50,000원) - %d개%n");
+        System.out.printf("5개 일치 (1,500,000원) - %d개%n");
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개%n");
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개%n");
+    }
+
+    public void printTotalRevenue() {
+        System.out.printf("총 수익률은 %f입니다.");
+    }
 }
