60ef1fede1b45bcd98f5590112f474ac23864ef4
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-14 13:55:05 +0900

    feat(Result): 모든 당첨 금액을 저장하는 기능 구현

--- src/main/java/lotto/model/Result.java
+++ src/main/java/lotto/model/Result.java
@@ -7,14 +7,14 @@ import java.util.List;
 public class Result {
     private int[] resultArray;
     private double rateOfReturn;
-    private int sum;
+    private int priceSum;
 
     public Result() {
         rateOfReturn=0;
         resultArray=new int[6];
         for(int i=0;i<6;i++)
             resultArray[i]=0;
-        sum=0;
+        priceSum=0;
     }
 
     public void calculatorResult(List<List<Integer>> randomNumbers, List<Integer> lottoNumber, int bonusNumber){
@@ -27,6 +27,7 @@ public class Result {
                         .filter(number->number==bonusNumber)
                         .count()))
                 );
+        calculatorSum();
     }
     private static int setResultArray(int number,int bonusNumber)
     {
@@ -47,5 +48,17 @@ public class Result {
 
     private void setResult(int index){
         resultArray[index]++;
+    }
+
+    private void calculatorSum(){
+        priceSum+= price.THREE.getPriceMoney()*resultArray[1];
+        priceSum+=price.FOUR.getPriceMoney()*resultArray[2];
+        priceSum+=price.FIVE.getPriceMoney()*resultArray[3];
+        priceSum+=price.FIVE_BONUS.getPriceMoney()*resultArray[4];
+        priceSum+=price.SIX.getPriceMoney()*resultArray[5];
+    }
+
+    public int getPriceSum() {
+        return priceSum;
     }
 }
