5f440f889662723db0f4450de0b43fa6b5473b3f
syahn <496300@naver.com>
2022-11-15 01:22:28 +0900

    feat: 로또 개수만큼 로또를 발행하는 기능 구현
    - 로또 개수만큼 pickUniqueNumbersInRange 이용해 random number를 추출
    - 리스트에 각 로또 번호
    리스트를 저장하여 로또 리스트를 생성함

---
+++ src/main/java/lotto/Issue.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Issue {
+    private final int lottoCount;
+    private final List<List<Integer>> lottoes = new ArrayList<>();
+
+    public Issue(int lottoCount){
+        this.lottoCount = lottoCount;
+        createLottoList();
+    }
+
+    void createLottoList(){
+        for(int i = 0; i<lottoCount; i++) {
+            List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            Collections.sort(numbers);
+            lottoes.add(numbers);
+        }
+    }
+
+    public List<List<Integer>> getLottoes(){
+        return this.lottoes;
+    }
+}
