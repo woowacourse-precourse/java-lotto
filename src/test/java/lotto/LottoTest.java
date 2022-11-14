d8a94ddf577dacc4ffad7bc6cbc8a16c4f70d613
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-14 09:06:18 +0900

    TEST:LottoTest.java/revise LottoByOverNum()

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
-    void createLottoByOverSize() {
+    void createLottoByOverNum() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
