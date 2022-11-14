c6670d999b664f9518cdd37d729686a2e7627828
ostar11 <cjw5877@gmail.com>
2022-11-15 02:30:04 +0900

    feat(Lottos): Lottos 클래스 생성
    Lotto 객체를 리스트로 보관

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,7 @@ package lotto;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.model.Lotto;
+import lotto.model.Lottos;
 import lotto.model.WinningNumbers;
 import lotto.view.LottoManager;
 
@@ -16,17 +17,18 @@ public class Application {
             final int lottoCount = lottoManager.inputPurchaseMoney();
             lottoManager.printLottoCount(lottoCount);
 
-            List<Lotto> lottos = new ArrayList<>(lottoCount);
-            for (int i = 0; i < lottoCount; i++) {
-                lottos.add(i, new Lotto(numberGenerator.createLottoNumbers()));
-            }
-            lottoManager.printLottoNumbers(lottos);
+//            List<Lotto> lottos = new ArrayList<>(lottoCount);
+//            for (int i = 0; i < lottoCount; i++) {
+//                lottos.add(i, new Lotto(numberGenerator.createLottoNumbers()));
+//            }
+            Lottos lottos = new Lottos(lottoCount);
+            lottoManager.printLottoNumbers(lottos.getLottos());
 
             List<Integer> winningNumbers = numberGenerator.createWinningNumbers(lottoManager.inputWinningNumbers());
             WinningNumbers w = new WinningNumbers(winningNumbers);
             final int bonusNumber = numberGenerator.createBonusNumbers(winningNumbers, lottoManager.inputBonusNumber());
 
-            for (Lotto lotto : lottos) {
+            for (Lotto lotto : lottos.getLottos()) {
                 int correctCount = lottoCalculator.compare(lotto.getNumbers(), w.getNumbers(), bonusNumber);
                 lottoCalculator.addRankCount(correctCount);
             }
