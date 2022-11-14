039816f2858e4c245a79e480415a887da9d9ffb0
metorg <metrog950@gmail.com>
2022-11-14 15:43:51 +0900

    feat: 중복 번호 예외처리 메서드 생성

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -1,10 +1,14 @@
 package lotto;
 
+import java.util.HashSet;
+
 public class Exception {
+
+    private HashSet<Integer> overlapNumber;
 
     //숫자가 아닌 문자 입력시 예외처리
     public boolean isNumeric(int money){
-
+        return true;
     }
 
     public boolean divThousandOfMoney(int money){
@@ -13,13 +17,32 @@ public class Exception {
     }
 
     public boolean lessThanThousand(int money){
-        if(money<1000)return true;
+        if(money>1000)return true;
         return false;
     }
 
-    //숫자가 45에 들어가는지
     public boolean lottoValidator(int number){
-        if(1<=number&&number<=45)return true;
+        if(1<=number&&number<=45) {
+            overlapNumber.add(number);
+            return true;
+        }
         return false;
     }
+
+    public boolean overlapCheck(String type){
+        if(type =="lotto"){
+            if(overlapNumber.size()==6)return true;
+        }
+
+        if(type=="userLotto"){
+            if(overlapNumber.size()==7)return true;
+        }
+
+        return false;
+    }
+
+    public void init(){
+        overlapNumber = new HashSet<>();
+    }
+
 }
