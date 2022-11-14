405ff3dfa57a41e456746e4fbcfcb7789581fc25
Jenn Kim <grdd0419@gmail.com>
2022-11-15 03:54:00 +0900

    feat: 하드 코딩을 방지하기 위해 로또 관련 설정값들을 저장하는 Preset 클래스 추가

---
+++ src/main/java/lotto/domain/Preset.java
@@ -0,0 +1,9 @@
+package lotto.domain;
+
+public class Preset {
+
+    private static final int LOTTO_LENGTH = 6;
+    private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_MIN_VALUE = 1;
+    private static final int LOTTO_MAX_VALUE = 45;
+}
