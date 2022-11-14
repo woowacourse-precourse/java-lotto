49a81cfe4c62820c93550bcc9899ac3dd4a9ef65
Hyunstone <hyun104467@naver.com>
2022-11-15 00:12:07 +0900

    feat(lotto): 메인메소드 구현(통계제외)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import lotto.domain.LottoGenerator;
 import lotto.domain.WinningNumber;
 
@@ -7,15 +8,32 @@ public class Application {
     public static void main(String[] args) {
         LottoGenerator lottoGenerator;
         WinningNumber winningNumber;
+        int generateCount = 0;
+        int[] result = new int[6];
 
         System.out.println("구입금액을 입력해주세요.");
         lottoGenerator = new LottoGenerator(Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine()));
+        generateCount = lottoGenerator.getGenerateCount();
+        Lotto[] lottoGroup = new Lotto[generateCount];
+        for (int i = 0; i < generateCount; i++) {
+            lottoGroup[i] = new Lotto(lottoGenerator.createLottoNumber());
+        }
+
+        System.out.println(generateCount + "개를 구매했습니다.");
+        for (Lotto lotto: lottoGroup) {
+            System.out.println(lotto);
+        }
 
         System.out.println("당첨 번호를 입력해 주세요.");
-        String winNumber = camp.nextstep.edu.missionutils.Console.readLine();
+        String winInputNumber = camp.nextstep.edu.missionutils.Console.readLine();
         System.out.println("보너스 번호를 입력해 주세요.");
-        String bonusNumber = camp.nextstep.edu.missionutils.Console.readLine();
+        String bonusInputNumber = camp.nextstep.edu.missionutils.Console.readLine();
         
-        winningNumber = new WinningNumber(winNumber, Integer.valueOf(bonusNumber));
+        winningNumber = new WinningNumber(winInputNumber, Integer.valueOf(bonusInputNumber));
+
+        for (int i = 0; i < generateCount; i++) {
+            int rank = lottoGroup[i].lottoChecker(winningNumber.getWinNumber(), winningNumber.getBonusNumber());
+            result[rank]++;
+        }
     }
 }
