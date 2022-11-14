f2f1c322a887f004b83dabc5ddbe2433d520097b
saewoo1 <wooozooo1123@gmail.com>
2022-11-15 08:49:35 +0900

    feat(Lotto): 로또 번호에 대한 유효성 검사 코드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import lotto.viewer.ErrorMessage;
+
 import java.util.*;
 
 public class Lotto {
@@ -15,10 +17,39 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        isValidSize(numbers);
+        isValidRange(numbers);
+        isValidOnlyNumber(numbers);
+        isValidSorted(numbers);
     }
     // TODO: 추가 기능 구현
 
+    public void isValidSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_SIX_NUMBERS.getMessage());
+        }
+    }
+
+    public void isValidRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(ErrorMessage.NOT_RANGE_MESSAGE.getMessage());
+            }
+        }
+    }
+
+    public void isValidOnlyNumber(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if (numbers.size() != set.size()) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_ONLY_WINNING_NUMBER.getMessage());
+        }
+    }
+
+    public void isValidSorted(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if (numbers.get(i) > numbers.get(i + 1)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 오름차순이어야 합니다.");
+            }
+        }
+    }
 }
