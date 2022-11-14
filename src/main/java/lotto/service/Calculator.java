efcf895b03c02aae023cbf7fc2144a43ba16c24b
Gibeom Nam <rlqja0523@naver.com>
2022-11-14 23:31:21 +0900

    feat: 총 당첨금액 계산

--- src/main/java/lotto/service/Calculator.java
+++ src/main/java/lotto/service/Calculator.java
@@ -1,8 +1,18 @@
 package lotto.service;
 
+import java.util.Map;
+
 public class Calculator {
 
     public static int getPurchaseCount(int purchaseAmount) {
         return purchaseAmount / 1000;
+    }
+
+    public static long sumWinningPrize(Map<Integer, Integer> prizes) {
+        long totalWinningPrize = 0L;
+        for (Map.Entry<Integer, Integer> prize : prizes.entrySet()) {
+            totalWinningPrize += (long) prize.getKey() * prize.getValue();
+        }
+        return totalWinningPrize;
     }
 }
