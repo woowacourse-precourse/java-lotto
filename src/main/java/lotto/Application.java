a63f863129e1ec285438ecfe8a38673cd2830a80
JJ <jeongjae1996@naver.com>
2022-11-15 02:16:18 +0900

    feat: Applicatin 클래스에서 전체적인 과정이 실행되도록 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,17 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        Money money = new Money();
+        try {
+            Money money = new Money();
+            System.out.println();
+            LottoNumber lottoNumber = new LottoNumber(money);
+            Lottos lottos = new Lottos(lottoNumber);
+            System.out.println();
+            WinningNumbers winningNumbers = new WinningNumbers();
+            Compute compute = new Compute(lottos, winningNumbers, money);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
