fc165e9cb5f2f8466a0f3125354f23065a71dba0
Konahyeon <koo@inha.edu>
2022-11-14 15:17:04 +0900

    feat: 당첨 번호 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Application {
     private static List<Lotto> lotto_list = new ArrayList<>(); //전체 로또 담을 리스트
@@ -35,9 +36,24 @@ public class Application {
         }
     }
 
+    public static List<Integer> winningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String numbers = Console.readLine();
+        List<Integer> winningNumbersList = new ArrayList<>(); //당첨 번호 담긴 리스트
+        StringTokenizer st = new StringTokenizer(numbers, ",");
+
+        while(st.hasMoreTokens()) {
+            int element = Integer.parseInt(st.nextToken());
+            winningNumbersList.add(element);
+        }
+
+        return winningNumbersList;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int lottoNumber = buyLotto();
         printLotto(lottoNumber);
+        Lotto winningLotto = new Lotto(winningNumber());
     }
 }
