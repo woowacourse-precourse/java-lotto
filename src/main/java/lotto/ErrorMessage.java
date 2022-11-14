66109d61cf9e41c1b72f5ab2ded2ab42bcd96af1
JerryK026 <kskyung0624@gmail.com>
2022-11-14 23:04:42 +0900

    feat(Lotto, Prize) : raffle()
    테스트를 만족하는 raffle()을 구현하고 등수를 나타내는 Prize를 구현한다

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -2,8 +2,9 @@ package lotto;
 
 public class ErrorMessage {
     // LOTTO
-    public static final String LOTTO_NUMBER_SHOULD_BE_SIX = "로또 번호 개수는 6개여야 합니다.";
-    public static final String LOTTO_DUPLICATED = "로또 번호는 중복되어선 안됩니다.";
+    public static final String LOTTO_NUMBER_SHOULD_BE_SIX = "[ERROR] 로또 번호 개수는 6개여야 합니다.";
+    public static final String LOTTO_DUPLICATED = "[ERROR] 로또 번호는 중복되어선 안됩니다.";
+    public static final String BONUS_DUPLICATED = "[ERROR] 입력된 보너스 번호는 로또 번호와 중복됩니다.";
 
     // Client
     public static final String MONEY_NOT_INTEGER = "[ERROR] 금액 입력은 정수여야 합니다.";
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,6 +12,53 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public Prize raffle(List<Integer> ticketNumbers, int ticketBonus, int bonus) {
+        validateBonusDuplicate(bonus);
+
+        int count = getContainCount(ticketNumbers);
+
+        return getPrize(count, ticketBonus, bonus);
+    }
+
+    private void validateBonusDuplicate(int bonus) {
+        if (numbers.contains(bonus)) {
+            throw new IllegalArgumentException(ErrorMessage.BONUS_DUPLICATED);
+        }
+    }
+
+    private Prize getPrize(int count, int ticketBonus, int bonus) {
+        switch (count) {
+            case 6:
+                return Prize.FIRST;
+            case 5:
+                return bonusPrize(ticketBonus, bonus);
+            case 4:
+                return Prize.FOURTH;
+            case 3:
+                return Prize.FIFTH;
+            default:
+                return Prize.NOTHING;
+        }
+    }
+
+    private Prize bonusPrize(int ticketBonus, int bonus) {
+        if (ticketBonus == bonus) {
+            return Prize.SECOND;
+        }
+        return Prize.THIRD;
+    }
+
+    private int getContainCount(List<Integer> ticketNumbers) {
+        int count = 0;
+        for (int number : ticketNumbers) {
+            if (numbers.contains(number)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_SHOULD_BE_SIX);
