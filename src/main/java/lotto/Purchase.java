63016af03fa684902ac088eec6a2f1853c729304
hdsung98 <hdsung98@gmail.com>
2022-11-14 16:01:42 +0900

    feat: create constructor and validity-checking methods in Class Purchase

--- src/main/java/lotto/Purchase.java
+++ src/main/java/lotto/Purchase.java
@@ -1,4 +1,18 @@
 package lotto;
 
 public class Purchase {
+    static Integer lottoSize;
+    public static final String ERROR_PRICE = "[ERROR] Price should be at least 1000.";
+
+    public Purchase(Integer price) {
+        checkSizeValidity(price);
+        lottoSize = price / 1000;
+    }
+
+    private void checkSizeValidity(Integer price) throws IllegalArgumentException {
+        if (price / 1000 <= 0) {
+            new IllegalArgumentException(ERROR_PRICE);
+        }
+    }
+
 }
