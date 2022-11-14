df2900c2a026612ff4e076d06506e47bd54331f6
ParangBird <seekerman@naver.com>
2022-11-14 22:19:33 +0900

    feat: 사용자 구입 금액 요청 메시지 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,20 @@
 package lotto.view;
 
 public class OutputView {
+    public final static String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    private void printlnMessage(String message) {
+
+        System.out.println(message);
+    }
+
+    private void printMessage(String message) {
+
+        System.out.print(message);
+    }
+
+    public void printPurchasePrice() {
+
+        printlnMessage(INPUT_PURCHASE_AMOUNT_MESSAGE);
+    }
 }
