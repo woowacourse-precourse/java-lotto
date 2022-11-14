0d83bbec9324c8841047af303a4342254c9a507b
kokodak <kokodakadokok@gmail.com>
2022-11-14 15:33:00 +0900

    test(winningStatistics): getWinningDetails 메서드에 대한 테스트 작성

---
+++ src/test/java/lotto/domain/WinningStatisticsTest.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningStatisticsTest {
+
+    @Test
+    @DisplayName("각 등수마다 몇 개씩 당첨되었는지 계산 -> 1등 1개, 2등 2개, 5등 2개, 꽝 1개")
+    void getWinningDetailsTest() {
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(List.of(40, 41, 42, 43, 44, 45));
+        Lotto lotto3 = new Lotto(List.of(1, 2, 3, 43, 44, 45));
+        Lotto lotto4 = new Lotto(List.of(40, 2, 3, 43, 44, 6));
+        Lotto lotto5 = new Lotto(List.of(40, 2, 3, 4, 5, 6));
+        Lotto lotto6 = new Lotto(List.of(40, 1, 2, 3, 4, 5));
+        Lottos lottos = new Lottos(List.of(lotto1, lotto2, lotto3, lotto4, lotto5, lotto6));
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 40);
+        Map<WinningRank, Integer> winningDetails = WinningStatistics.getWinningDetails(lottos, winningLotto);
+
+        assertThat(winningDetails.get(WinningRank.LAST_PLACE)).isEqualTo(1);
+        assertThat(winningDetails.get(WinningRank.FIFTH_PLACE)).isEqualTo(2);
+        assertThat(winningDetails.get(WinningRank.SECOND_PLACE)).isEqualTo(2);
+        assertThat(winningDetails.get(WinningRank.FIRST_PLACE)).isEqualTo(1);
+    }
+}
