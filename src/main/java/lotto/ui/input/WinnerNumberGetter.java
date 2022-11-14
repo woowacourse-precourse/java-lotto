e36f8e97c19cec354c59dbcc5c76a563efc34683
YSEO <1park4170@gmail.com>
2022-11-14 14:24:04 +0900

    refactor<ui: get winning number from user > : modify error handling to WinnerNumberGetter
    user can do wrong input, for example, not numbers, or numbers more or lesser than six, so add exception handler to this occassion
    
    before : no exception handling
    after :
     - if input is not number, catch NumberformatExeption and throw IllegalArgumentException to Controller
     - if input count is not match, throw IllegalArgumentException to Controller

--- src/main/java/lotto/ui/input/WinnerNumberGetter.java
+++ src/main/java/lotto/ui/input/WinnerNumberGetter.java
@@ -8,17 +8,28 @@ import java.util.Map;
 import java.util.function.IntConsumer;
 
 public class WinnerNumberGetter {
-    public static Map<Integer, Integer> getLotteryWinningNumbersInput() {
+    private static final IllegalArgumentException ILLEGAL_ARGUMENT_EXCEPTION = new IllegalArgumentException("[ERROR] 쉼표로 구분되는 6자리의 숫자를 입력해주세요");
+    private static final String WINNING_NUMBER_REQUEST = "당첨 번호를 입력해주세요.";
+    private static final String BONUS_NUMBER_REQUEST = "보너스 번호를 입력해주세요.";
+
+    public static Map<Integer, Integer> getLotteryWinningNumbersInput() throws IllegalArgumentException{
+        System.out.println(WINNING_NUMBER_REQUEST);
         Map<Integer, Integer> numbers = new HashMap<>();
-
-        Arrays.asList(Console.readLine().split(",")).stream()
-                .map(String::trim)
-                .mapToInt(Integer::parseInt).iterator().forEachRemaining((IntConsumer) i -> numbers.put(i, 1));
-
+        try {
+            Arrays.asList(Console.readLine().split(",")).stream()
+                    .map(String::trim)
+                    .mapToInt(Integer::parseInt).iterator().forEachRemaining((IntConsumer) i -> numbers.put(i, 1));
+            if (numbers.size() != 6) {
+                throw ILLEGAL_ARGUMENT_EXCEPTION;
+            }
+        } catch (NumberFormatException ne) {
+            throw ILLEGAL_ARGUMENT_EXCEPTION;
+        }
         return numbers;
     }
 
     public static int getBonusInput() {
+        System.out.println(BONUS_NUMBER_REQUEST);
         int bonus = Integer.parseInt(Console.readLine());
 
         return bonus;
