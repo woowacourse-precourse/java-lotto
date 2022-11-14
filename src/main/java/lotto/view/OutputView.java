ad42077de90507d2d092bd6eab71a2bcb1cfa0e2
Sangin <ksiisk99@gmail.com>
2022-11-14 22:15:27 +0900

    refactor: 출력할 때 String으로 변환할 필요 없음

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -11,7 +11,7 @@ public class OutputView {
     }
 
     public void printLottoQuantity(int lottoQuantity) {
-        System.out.println(String.valueOf(lottoQuantity) + "개를 구매했습니다.");
+        System.out.println(lottoQuantity + "개를 구매했습니다.");
     }
 
     public void printLotto(List<Integer> purchaserLotto) {
