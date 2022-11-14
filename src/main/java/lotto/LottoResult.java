c99305fa0d3e62c5f843e9bd945642fbaa426e80
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-15 04:04:27 +0900

    feat: implement function that counting results by prize

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,9 +1,6 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class LottoResult {
 
@@ -17,42 +14,13 @@ public class LottoResult {
         return matchCount;
     }
 
-
-
-
-
- /*   public static LinkedHashMap<Integer, Integer> compareResult() {
-        LinkedHashMap<String, Integer> resultList = new LinkedHashMap<>();
-
-        int ticketSheets = 8;
-        int firstPlace = 0;
-        int secondPlace = 0;
-        int thirdPlace = 0;
-        int fourthPlace = 0;
-        int fifthPlace = 0;
-
-        List<Integer> winningNumbers = LottoNumbers.getLotteryWinningNumbers();
-        List<List<Integer>> ticketNumbers = LottoTicket.paidLotteryNumberGenerator(ticketSheets);
-
-        for (int ticketIndex = 0; ticketIndex < ticketSheets; ticketIndex++) {
-            int matched = compareLotteryNumbers(winningNumbers, ticketNumbers.get(ticketIndex));
-            if (matched == 6) {
-                firstPlace++;
-            }
-            if (matched == 5) {
-                secondPlace++;
-                thirdPlace++;
-            }
-            if (matched == 4) {
-                fourthPlace++;
-            }
-            if (matched == 3) {
-                fifthPlace++;
-            }
+    public static LinkedHashMap<Integer, Integer> compareResult(List<Integer> matchCount, int tickets) {
+        LinkedHashMap<Integer, Integer> resultByRank = new LinkedHashMap<>();
+        for (int matchingNumbers = 3; matchingNumbers < 7; matchingNumbers++) {
+            resultByRank.put(matchingNumbers,Collections.frequency(matchCount, matchingNumbers));
         }
-
-
-    }*/
+        return resultByRank;
+    }
 
     public static int earningRatioCalculator(int totalMoney, int totalPrize) {
         int earnedRatio = totalPrize / totalMoney * 100;
