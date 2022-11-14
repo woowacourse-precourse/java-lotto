76c661c683b46151b8fe1eb99fb81094456e86eb
jihun-24k <gi020477@gmail.com>
2022-11-15 00:29:36 +0900

    Test: 구입 금액 성공 케이스 테스트

--- src/test/java/lotto/service/LottoGameServiceTest.java
+++ src/test/java/lotto/service/LottoGameServiceTest.java
@@ -1,5 +1,25 @@
 package lotto.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class LottoGameServiceTest {
 
+    private LottoGameService lottoGameService;
+
+    @BeforeEach
+    void init() {
+        this.lottoGameService = new LottoGameService();
+    }
+    @DisplayName("구입 금액에 맞춰 구매 가능한 로또 갯수를 반환한다.")
+    @Test
+    void buyLottoTickets() {
+        int expect = 8;
+        String purchaseAmount = "8000";
+        int actual = lottoGameService.buyLottoTickets(purchaseAmount);
+        assertThat(expect).isEqualTo(actual);
+    }
 }
