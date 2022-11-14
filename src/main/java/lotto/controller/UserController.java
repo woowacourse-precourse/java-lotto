f68c5fc6c8c302c0dac7ebcf40063a3d8ddbd7eb
jjmin9797 <jjmin9797@naver.com>
2022-11-14 16:24:48 +0900

    feat(UserController.java): 사용자한테 구매 금액을 입력받아 저장하는 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/controller/UserController.java
+++ src/main/java/lotto/controller/UserController.java
@@ -1,2 +1,19 @@
-package lotto.controller;public class UserController {
+package lotto.controller;
+
+import lotto.repository.UserRepository;
+import lotto.service.UserService;
+import lotto.view.GameProgressView;
+
+public class UserController {
+    private static UserRepository userRepository = new UserRepository();
+    private static UserService userService = new UserService();
+    private static GameProgressView gameProgressView = new GameProgressView();
+
+    public void savePurchaseAmount() {
+        gameProgressView.showPurchaseMessage();
+        String purchaseAmount = userService.enterPurchaseAmount();
+        //TODO : 예외 처리 필요
+        userRepository.setUserPurchaseMoney(Integer.parseInt(purchaseAmount));
+        userRepository.setUserNumberOfLotties(userService.findTheNumberOfLotteryTicketsPurchased(purchaseAmount));
+    }
 }
