71037a87fcd252b60f96e8a26e68ee2f18a55c54
chaeyeong222 <iky1505@naver.com>
2022-11-15 00:08:38 +0900

    feat : 결과 도출하는 이넘클래스 구현

---
+++ src/main/java/lotto/Result.java
@@ -0,0 +1,44 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public enum Result {
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    OUT(0,0);
+
+    private int reward;
+    private int winCount;
+
+    public int getReward() {
+        return reward;
+    }
+
+    public int getWinCount() {
+        return winCount;
+    }
+
+    Result(final int winCount, final int reward){
+        this.reward = reward;
+        this.winCount = winCount;
+    }
+
+    public static Result getResult(final int winCount, final boolean containBonusNumber){
+        if(winCount < FIFTH.winCount){
+            return OUT;
+        }
+        if(winCount == THIRD.winCount && !containBonusNumber){
+            return THIRD;
+        }
+
+        return Arrays.stream(values()).filter(rating -> rating.winCount == winCount).findAny()
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+
+
+}
