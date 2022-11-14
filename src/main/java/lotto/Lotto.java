1671dc0a9b8d6f0a30b2bb6111209138000aeb53
210_reverof <sheltonwon@gmail.com>
2022-11-14 12:06:06 +0900

    fix: add checking duplication

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,6 +16,10 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void print() {
