d544ef667ecfebfc19fd29f69314ae3515ee1e5d
sunghyuki <sunghyuk1609@gmail.com>
2022-11-15 00:21:02 +0900

    feat : 로또 자동 생성 기능 추가 - 1개의 로또를 발행할 때 중복되지 않는 6개의 숫자를 뽑는다. - 로또 번호의 숫자 범위는 1 ~ 45까지를 벗어날 경우 IllegalArgumentException 를 발생시키고, 에러 메시지를 출력한다. - 로또 번호의 개수는 6이다. 로또 번호의 개수가 6이 아닐 경우 IllegalArgumentException 를 발생시키고, 에러 메시지를 출력한다.

---
+++ src/main/java/lotto/AutoLottoGenerator.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class AutoLottoGenerator {
+    private static final int LOTTO_SIZE = 6;
+
+    public static Lotto generate() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(Lotto.MIN_VALUE, Lotto.MAX_VALUE, LOTTO_SIZE);
+        return new Lotto(randomNumbers);
+    }
+}
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,18 +3,35 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    private static final String LOTTO_NUMBER_RANGE_MISMATCH_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String LOTTO_SIZE_MISMATCH_MESSAGE = "[ERROR] 로또 한 장의 번호 개수는 6개입니다.";
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 45;
+    private static final int LOTTO_SIZE = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoNumbersRange(numbers);
+        validateLottoSize(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(LOTTO_SIZE_MISMATCH_MESSAGE);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateLottoNumbersRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (isNotInLottoNumberRange(number)) {
+                throw new IllegalArgumentException(LOTTO_NUMBER_RANGE_MISMATCH_MESSAGE);
+            }
+        }
+    }
+
+    private boolean isNotInLottoNumberRange(int number) {
+        return number < MIN_VALUE || number > MAX_VALUE;
+    }
 }
