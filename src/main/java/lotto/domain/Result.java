633af30e273b8dd275130e98bbd9f9ff26ebc8a4
sangeun-b <baek0007@algonquinlive.com>
2022-11-15 04:57:30 +0900

    refactor: 당첨 번호 비교 메소드 수정 - 모든 로또 번호를 한번에 비교해서 저장하는 방식을 - 각각 하나씩 비교하는 방식으로 변경

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -1,10 +1,8 @@
 package lotto.domain;
 
 import java.util.*;
-import java.util.stream.Stream;
 
 public class Result {
-    private int[][] result;
     private static Map<ResultType,Integer> allResult = new HashMap<>(){{
         put(ResultType.THREE,0);
         put(ResultType.FOUR,0);
@@ -12,25 +10,7 @@ public class Result {
         put(ResultType.FIVEPLUS,0);
         put(ResultType.SIX,0);
     }};
-
-//    public int[][] calculateLotto(List<Lotto> input,List<Integer> winning, String bonus){
-//        result = new int[input.size()][2];
-//        for(int i=0; i<input.size(); i++){
-//            compareLotto(input.get(i), winning,i);
-//            compareBonus(input.get(i),bonus,i);
-//        }
-//        return result;
-//    }
-
-    public Map<ResultType,Integer> initMap(){
-        allResult.put(ResultType.THREE,0);
-        allResult.put(ResultType.FOUR,0);
-        allResult.put(ResultType.FIVE,0);
-        allResult.put(ResultType.FIVEPLUS,0);
-        allResult.put(ResultType.SIX,0);
-
-        return allResult;
-    }
+    
 
     public Map<ResultType,Integer> calculateLotto(List<Lotto> input,List<Integer> winning, String bonus){
         int check = 0;
@@ -46,17 +26,10 @@ public class Result {
         ResultType resultType = makeTotalWinnings(check, checkBonus);
         if(allResult.containsKey(resultType))
             allResult.put(resultType,allResult.get(resultType)+1);
-//        if(!allResult.containsKey(resultType))
-//            allResult.put(resultType,1);
+
     }
 
-    private void compareLotto(Lotto lotto, List<Integer> winning, int i) {
-        for(int j = 0; j< lotto.getLotto().size(); j++){
-            if(winning.contains(lotto.getLotto().get(j))){
-                result[i][0]++;
-            }
-        }
-    }
+
 
     private int compareLotto(Lotto lotto, List<Integer> winning) {
         int check = 0;
@@ -75,11 +48,6 @@ public class Result {
         return false;
     }
 
-    private void compareBonus(Lotto lotto, String bonus, int i){
-        if(lotto.getLotto().contains(Integer.parseInt(bonus))){
-            result[i][1]++;
-        }
-    }
     public double calculateRate(double purchase, double win){
         return win/purchase;
     }
@@ -97,24 +65,6 @@ public class Result {
             return ResultType.THREE;
 
         return ResultType.NOWINNING;
-    }
-
-    public Map<ResultType,Integer> makeTotalResult(int[][] result){
-        Map<ResultType,Integer> totalResult = new HashMap<>();
-        for(int i=0; i<result.length; i++){
-            int cnt = 0;
-            if(result[i][0] == 6)
-                totalResult.put(ResultType.SIX,cnt++);
-            if(result[i][0] == 5 && result[i][1]==1)
-                totalResult.put(ResultType.FIVEPLUS,cnt++);
-            if(result[i][0] == 5)
-                totalResult.put(ResultType.FIVE,cnt++);
-            if(result[i][0] == 4)
-                totalResult.put(ResultType.FOUR,cnt++);
-            if(result[i][0] == 3)
-                totalResult.put(ResultType.THREE,cnt++);
-        }
-        return totalResult;
     }
 
     public int makePrize(Map<ResultType,Integer> result){
