2eacf7bbc41904ee519d6a00cfa9c17def883cff
YE-EUN <musiclove529@naver.com>
2022-11-15 08:43:38 +0900

    fix(Message) : 잘못나오는 메시지 수정

--- src/main/java/lotto/domain/Message.java
+++ src/main/java/lotto/domain/Message.java
@@ -5,7 +5,7 @@ public class Message {
     static final String LOTTO_GENERATE_SUCCESS = "개를 구매했습니다.";
     static final String WINNING_NUMBERS_REQUEST = "당첨 번호를 입력해 주세요.";
     static final String BONUS_NUMBER_REQUEST = "보너스 번호를 입력해 주세요.";
-    static final String STATISTICS_TITLE = "당첨 통계";
-    static final String START_OF_EARNING_RATE = "총 수익률은 ";
+    static final String STATISTICS_TITLE = "당첨 통계\n---\n";
+    static final String START_OF_EARNING_RATE = "총 수익률은";
     static final String END_OF_EARNING_RATE = "%입니다.";
 }
