df54418c1744c3939b30888807d547d74f79ce4b
madlimeus <108660314+madlimeus@users.noreply.github.com>
2022-11-15 03:42:49 +0900

    Update Application.java

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class Application {
     public static ArrayList<List<Integer>> GET_MY_LOTTO_NUMBER(int Number_Of_Lotto){
         ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
