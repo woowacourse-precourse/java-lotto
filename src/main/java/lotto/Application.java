aeff2a9c41fe913d00d3fca987ea2a17bea8dd8d
JaeHwanCho <makefuture@kakao.com>
2022-11-15 00:04:24 +0900

    feat(function): receive the winning number and bonus number
    Add new function:
    - receive the winning number
    - parsing input values
    - receive the bonus number

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,9 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -10,6 +13,11 @@ public class Application {
             return;
         }
         Integer price = Integer.valueOf(lotto);
+
+        List<Integer> nums = getNums();
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        Integer bonusNum = Integer.getInteger(Console.readLine());
     }
 
     public static String getLotto() {
@@ -28,5 +36,19 @@ public class Application {
 //            throw new IllegalArgumentException("[ERROR] 1000으로 나누어 떨어지지 않는 경우");
         }
         return price;
+    }
+
+    public static List<Integer> getNums() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningNums = Console.readLine();
+        List<Integer> nums = new ArrayList<>();
+        while (winningNums.contains(",")) {
+            String substring = winningNums.substring(0, winningNums.indexOf(","));
+            nums.add(Integer.valueOf(substring));
+            winningNums = winningNums.substring(winningNums.indexOf(",") + 1, winningNums.length());
+        }
+        nums.add(Integer.valueOf(winningNums));
+        Lotto lotto = new Lotto(nums);
+        return nums;
     }
 }
