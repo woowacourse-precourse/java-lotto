901a2654549896ef002a204b94a7a4a368425a48
backFox <jorealbusiness@gmail.com>
2022-11-14 19:09:41 +0900

    test(lottoApplication): LottoApplication에 대한 단위 테스트 코드 추가

---
+++ src/test/java/lotto/system/LottoApplicationTest.java
@@ -0,0 +1,33 @@
+package lotto.system;
+
+import lotto.mock.MockLottoApplication;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoApplicationTest {
+    @Nested
+    @DisplayName("주요 애플리케이션 구동을 담당하는 run 메소드 테스트")
+    class runTest {
+        @Test
+        @DisplayName("run 메소드 실행 중 사용자 입력으로 인한 예외가 발생하면 적절한 포맷의 에러 메시지를 출력한다.")
+        void givenLottoApplication_thenRunningControllerCausingException_thenPrintsFormattedMessage() {
+            //given
+            OutputStream out = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(out));
+            LottoApplication lottoApplication = new MockLottoApplication();
+
+            //when
+            lottoApplication.run();
+
+            //then
+            assertThat(out.toString()).isEqualTo("[ERROR] 테스트 예외입니다.\n");
+        }
+    }
+}
