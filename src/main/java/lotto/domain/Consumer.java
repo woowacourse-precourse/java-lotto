dc1f33362d23263fa405de09f4e003c0eb26c99d
JUHA <khabh@naver.com>
2022-11-15 07:12:05 +0900

    refactor(Consumer): 전체 로직 수정
    변수명 수정, 로또 생성 결과, 당첨 결과 출력 기능 추가

--- src/main/java/lotto/domain/Consumer.java
+++ src/main/java/lotto/domain/Consumer.java
@@ -7,39 +7,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Consumer {
-    private final static int PURCHASE_UNIT = 1000;
+    private final static int LOTTO_PRICE_UNIT = 1000;
     private List<Lotto> lotteries;
 
     public void purchase() {
-        OutputView outputView = new OutputView();
         int purchasePrice = new InputView().readPurchasePrice();
-
         validatePrice(purchasePrice);
-        lotteries = getLotteries(purchasePrice);
-        outputView.printLotteries(lotteries);
+        purchaseLotteries(purchasePrice);
+
+        getLotteriesResult();
+    }
+
+    private void getLotteriesResult() {
+        new LottoEstimator(WinningLotto.create()).estimate(lotteries);
     }
 
     private void validatePrice(int price) {
-        if (price < PURCHASE_UNIT || price % PURCHASE_UNIT != 0) {
-            System.out.println("[ERROR] 금액은 1,000원 단위로 입력해야 합니다. (최소 금액 1,000원)");
-            throw new IllegalArgumentException();
+        if (price < LOTTO_PRICE_UNIT || price % LOTTO_PRICE_UNIT != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 1,000원 단위로 입력해야 합니다. (최소 금액 1,000원)");
         }
     }
 
-    private List<Lotto> getLotteries(int purchasePrice) {
-        List<Lotto> lotteries = new ArrayList<>();
+    private void purchaseLotteries(int purchasePrice) {
+        lotteries = new ArrayList<>();
         LotteryWheel lotteryWheel = new LotteryWheel();
 
         while (purchasePrice > 0) {
             Lotto lotto = new Lotto(lotteryWheel.generateLotteryNumbers());
             lotteries.add(lotto);
-            purchasePrice -= PURCHASE_UNIT;
+            purchasePrice -= LOTTO_PRICE_UNIT;
         }
-
-        return lotteries;
-    }
-
-    public List<Lotto> lotteries() {
-        return lotteries;
+        new OutputView().printLotteries(lotteries);
     }
 }
