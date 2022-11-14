0e271a9b4ff8cc2a7364713f9ae9ebb234d1f3dd
sujin0607 <goweg0607@gmail.com>
2022-11-15 00:10:36 +0900

    feat: 사용자가 입력한 당첨 번호 확인 컨트롤러 추가

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -5,7 +5,9 @@ import lotto.console.OutputConsole;
 import lotto.domain.Lotto;
 import lotto.domain.LottoManager;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Controller {
     private InputConsole input = new InputConsole();
@@ -13,18 +15,33 @@ public class Controller {
     private LottoManager lottoManager = new LottoManager();
 
     private Lotto lotto;
-    public int findLottoCount(){
+
+    public int findLottoCount() {
         String inputMoney = input.Money();
         int lottoCount = lottoManager.countLotto(inputMoney);
         output.lottoCount(lottoCount);
         return lottoCount;
     }
 
-    public void makeRandomNum(int lottoCount){
+    public void makeRandomNum(int lottoCount) {
         List<List<Integer>> randomNumbers = lottoManager.makeRandomNumbers(lottoCount);
         output.randomNumbers(randomNumbers);
     }
 
+    public List<Integer> winingNumber() throws IllegalArgumentException{
+        String[] inputNumbers = input.Number().split(",");
+        List<Integer> winningLottoNumber = Arrays.asList(inputNumbers)
+                .stream()
+                .map(s -> Integer.parseInt(s))
+                .collect(Collectors.toList());
 
-    
+        lotto = new Lotto(winningLottoNumber);
+        return lotto.getNumbers();
+    }
+
+    public void bonusNumber(List<Integer> numbers) throws IllegalArgumentException{
+        int bonusNumber = Integer.parseInt(input.bonusNum());
+        lotto = new Lotto(numbers, bonusNumber);
+    }
+
 }
