5822c06b8ec797221e29ba231d1cba48df21822d
impati <impati0716@icloud.com>
2022-11-14 21:17:15 +0900

    test : 로또 번호와 당첨 로또 번호 보너스번호로 등수 여러개 테스트

--- src/test/java/lotto/LottoStatsTest.java
+++ src/test/java/lotto/LottoStatsTest.java
@@ -4,9 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -86,11 +84,55 @@ class LottoStatsTest {
 
     }
 
+
+    @Test
+    @DisplayName("여려개 로또 번호 테스트")
+    public void multiple() throws Exception{
+
+       List<Lotto> purchase = new ArrayList<>();
+       purchase.add(new Lotto(List.of(1,2,3,4,5,9)));
+       purchase.add(new Lotto(List.of(1,2,3,4,5,7)));
+       purchase.add(new Lotto(List.of(1,2,3,4,5,22)));
+       purchase.add(new Lotto(List.of(1,2,3,4,22,33)));
+       purchase.add(new Lotto(List.of(1,2,3,22,33,44)));
+       purchase.add(new Lotto(List.of(11,12,13,14,15,16)));
+
+        Lotto winning = new Lotto(List.of(1,2,3,4,5,9));
+        Integer bonus = 7;
+
+        Map<StatsType,Integer> lottoStats = new LinkedHashMap<>();
+        lottoStats.put(StatsType.five,0);
+        lottoStats.put(StatsType.four,0);
+        lottoStats.put(StatsType.third,0);
+        lottoStats.put(StatsType.second,0);
+        lottoStats.put(StatsType.first,0);
+
+        for(int i = 0;i<purchase.size();i++){
+            Lotto lotto = purchase.get(i);
+            Optional<StatsType> optionalStatType = compareLotto(lotto,winning,bonus);
+            if(optionalStatType.isPresent()) {
+                StatsType statType = optionalStatType.get();
+                lottoStats.put(statType, lottoStats.get(statType) + 1);
+            }
+        }
+
+        for(var element:lottoStats.keySet()){
+            Assertions.assertThat(lottoStats.get(element)).isEqualTo(1);
+        }
+
+    }
+
+
     private Optional<StatsType> compareLotto(Lotto lotto, Lotto winningNumber, Integer bonus){
         Long score = lotto.sameNumber(winningNumber,bonus);
         return Arrays.stream(StatsType.values())
                 .filter(type -> type.getScore() == score)
                 .findFirst();
     }
+
+
+
+
+
 
 }
