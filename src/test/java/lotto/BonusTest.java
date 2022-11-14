a52a252596ddbbf3d3e4497900875a96124522cf
bsh1603 <bsh1603@github.com>
2022-11-14 20:38:44 +0900

    Test: 보너스 번호 개수 검증

---
+++ src/test/java/lotto/BonusTest.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import domain.BonusNumber;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BonusTest {
+
+    private BonusNumber bonusNumber = new BonusNumber();
+
+    @Test
+    public void 보너스번호개수검증() throws Exception{
+        //given
+        String input = "1,2";
+
+        //when
+        try{
+            bonusNumber.numberCount(input);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        //then
+        assertThrows(IllegalArgumentException.class, () -> bonusNumber.numberCount(input));
+    }
+}
