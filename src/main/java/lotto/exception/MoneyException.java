8f218d9db54dd64569d21ac61c02265e37b2691d
jiwon1217 <koagjw@gmail.com>
2022-11-15 05:40:11 +0900

    refactor: 테스트가 실패하여 원래 코드로 수정

--- src/main/java/lotto/exception/MoneyException.java
+++ src/main/java/lotto/exception/MoneyException.java
@@ -1,15 +1,14 @@
 package lotto.exception;
 
-import java.util.regex.Pattern;
-
 public class MoneyException {
     public static final String INVALID_MONEY_TYPE = "[ERROR] 금액은 숫자여야 합니다.";
-    private static final Pattern INPUT_PATTERN = Pattern.compile("\\d");
     public static final String MONEY_MUST_NOT_NEGATIVE = "[ERROR] 금액은 음수일 수 없습니다.";
     public static final String MONEY_MUST_MULTIPLE_OF_THOUSAND = "[ERROR] 금액은 1,000원 단위로 나누어 떨어져야 합니다.";
 
     public static void isValidMoneyType(String money) {
-        if (!INPUT_PATTERN.matcher(money).matches()) {
+        try {
+            Integer.parseInt(money);
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_MONEY_TYPE);
         }
     }
