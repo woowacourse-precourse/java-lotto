dd4f636cb0227e7593fc09a817fb537f0ebe6bbe
greeng00se <thegreengoose7@gmail.com>
2022-11-14 23:32:42 +0900

    refactor(Customer): Optional 필드 제거

--- src/main/java/lotto/domain/Customer.java
+++ src/main/java/lotto/domain/Customer.java
@@ -1,19 +1,17 @@
 package lotto.domain;
 
-import java.util.Optional;
-
 public class Customer {
     private static final String INVALID_TICKET_MESSAGE = "로또를 아직 구매하지 않았습니다.";
 
     private Integer amount;
-    private Optional<LottoTicket> lottoTicket = Optional.empty();
+    private LottoTicket lottoTicket;
 
     public Customer(Integer amount) {
         this.amount = amount;
     }
 
     public void buyLottoTicketTo(LottoSeller lottoSeller) {
-        lottoTicket = Optional.of(lottoSeller.sell(amount));
+        lottoTicket = lottoSeller.sell(amount);
         spendAll();
     }
 
@@ -22,10 +20,16 @@ public class Customer {
     }
 
     public LottoResult check(LottoMachine lottoMachine) {
-        return lottoMachine.check(lottoTicket.orElseThrow(() -> new IllegalStateException(INVALID_TICKET_MESSAGE)));
+        if (lottoTicket == null) {
+            throw new IllegalStateException(INVALID_TICKET_MESSAGE);
+        }
+        return lottoMachine.check(lottoTicket);
     }
 
     public LottoTicket getLottoTicket() {
-        return lottoTicket.orElseThrow(() -> new IllegalStateException(INVALID_TICKET_MESSAGE));
+        if (lottoTicket == null) {
+            throw new IllegalStateException(INVALID_TICKET_MESSAGE);
+        }
+        return lottoTicket;
     }
 }
