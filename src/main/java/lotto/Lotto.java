4b31a1fcc3da4845b511f23c9a2e032ab7511392
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-11-14 13:19:44 +0900

    [ADD] 로또 - Lotto 상수 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,6 +5,17 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
 
+    private final static int LOTTERY_LENGTH = 6;
+    private final static int RANGE_MIN = 1;
+    private final static int RANGE_MAX = 45;
+    private final static int SECOND_THIRD_MATCH = 5;
+    private final static int FIFTH_MATCH = 3;
+
+    private final static int FIRST_PRIZE = 2000000000;
+    private final static int SECOND_PRIZE = 30000000;
+    private final static int THIRD_PRIZE = 1500000;
+    private final static int FOURTH_PRIZE = 50000;
+    private final static int FIFTH_PRIZE = 5000;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
