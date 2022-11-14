7afcc88c659c28363e2f753d0f65ee48a59aa472
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-15 01:27:29 +0900

    feat(InputNumber) : 당첨번호 6개가  아ㅣ니면 예외 발생

--- src/main/java/controller/InputNumber.java
+++ src/main/java/controller/InputNumber.java
@@ -5,18 +5,31 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputNumber {
+    private static final int WINNING_NUMBER_LENGTH = 6;
+    private static final int BONUS_NUMBER_LENGTH = 1;
+
     public static List<Integer> parseWinningNumber(String userInput) {
         List<String> parsedString = new ArrayList<>(Arrays.asList(userInput.split("\\s*,\\s*")));
         List<Integer> winningNumber = new ArrayList<>();
         for (String parsed : parsedString) {
             winningNumber.add(Integer.parseInt(parsed));
         }
-        return winningNumber;
+        if(isAppropriateLength(winningNumber)){
+            return winningNumber;
+        }
+        throw new IllegalArgumentException("[ERROR] 당첨번호는 6개여야 합니다.");
     }
 
-    public static List<Integer> parseBonusNumber(String userInput){
+    public static List<Integer> parseBonusNumber(String userInput) {
         List<Integer> bonusNumber = new ArrayList<>();
         bonusNumber.add(Integer.parseInt(userInput));
         return bonusNumber;
+    }
+
+    static boolean isAppropriateLength(List<Integer> winningNumber) throws IllegalArgumentException {
+        if (winningNumber.size() != WINNING_NUMBER_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 당첨번호는 6개여야 합니다.");
+        }
+        return true;
     }
 }
