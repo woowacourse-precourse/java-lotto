12c745dadd23d6e693a3754568ffa12b8c1eecd1
seoyoung <sy9723@korea.ac.kr>
2022-11-14 23:41:52 +0900

    test: 예외 처리 테스트 추가 ([보너스 번호] 보너스 번호가 당첨 번호와 중복되는 경우)

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -42,4 +42,11 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6)).validateBonusNumber("47"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("보너스 번호가 로또 번호와 중복이면 예외가 발생한다.")
+    @Test
+    void createBonusByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6)).validateBonusNumber("5"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
