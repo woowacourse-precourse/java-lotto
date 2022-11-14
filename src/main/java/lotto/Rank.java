aacf2955fa40c60b9addd363fd37d10e8413ad9a
hyungkya <hyungkya@uci.edu>
2022-11-15 02:35:05 +0900

    feat: 등수 별 당첨 금액 환산하기
    Create Enum class to convert winning price.

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Rank {
+    first(1, 2_000_000_000),
+    second(2, 30_000_000),
+    third(3, 1_500_000),
+    fourth(4, 50_000),
+    fifth(5, 5_000);
+
+    private static final Map<Integer, Rank> BY_PLACE = new HashMap<>();
+
+    static {
+        for (Rank r : values()) {
+            BY_PLACE.put(r.place, r);
+        }
+    }
+
+    private final int place;
+    private final int winningPrice;
+
+    Rank(int place, int winningPrice) {
+        this.place = place;
+        this.winningPrice = winningPrice;
+    }
+
+    public static Rank valueOfRank(int place) {
+        return BY_PLACE.get(place);
+    }
+}
