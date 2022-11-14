4761682076174749a26d620e51c39f099c83fde7
LAPTOP-4NC4E864\yongs <yongs170@naver.com>
2022-11-14 13:54:32 +0900

    feat(Validation) : numberRangeValidation 기능 구현
    로또 번호는 1부터 45까지

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -9,4 +9,9 @@ public class Validation {
    public static void numberCountValidation(List<Integer> numbers){
        if(numbers.size() != 6) throw new IllegalArgumentException("[ERROR] 로또 번호는 6개입니다.");
    }
+   public static void numberRangeValidation(List<Integer> numbers){
+       int numbersCount = numbers.stream()
+               .filter(element->element >= 1 && element <= 45)
+               .collect(Collectors.toList()).size();
+   }
 }
