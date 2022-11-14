b367bf0d8f2e022d3127f06917a5d6ba0bc4aeb2
김선동 <mok03127@gmail.com>
2022-11-14 21:33:56 +0900

    feat: 예외 처리 시에 관련 에러 메시지를 콘솔에 출력하는 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,10 @@ import controller.LottoGameController;
 public class Application {
     public static void main(String[] args) {
         LottoGameController lottoGameController = new LottoGameController();
-        lottoGameController.run();
+        try {
+            lottoGameController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
