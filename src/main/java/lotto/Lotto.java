635352433f24bb83e20f858ffcd3874dc02f03c7
Danden1 <goh1211@ajou.ac.kr>
2022-11-14 22:21:02 +0900

    fix : 요구사항 충족하도록 Lotto 변경 및 중복된 숫자 검증 추가
    Lotto class에 추가한 새로운 필드 제거.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,25 +1,37 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
     private final List<Integer> numbers;
-    private final Integer bonusNumber;
 
     public Lotto(List<Integer> numbers, int bonusNumber) {
-        validate(numbers, bonusNumber);
-        this.numbers = numbers;
-        this.bonusNumber = bonusNumber;
+        List<Integer> totalNumbers = new ArrayList<>(numbers);
+        totalNumbers.add(bonusNumber);
+        validate(totalNumbers);
+        this.numbers = totalNumbers;
     }
 
-    private void validate(List<Integer> numbers, int bonusNumber) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해주세요.");
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 7) {
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자와 한 개의 보너스 숫자를 입력해주세요.");
         }
-        else if(!validateRange(numbers) || bonusNumber < 1 || bonusNumber > 45){
-            throw new IllegalArgumentException("[ERROR]  로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        else if(!validateRange(numbers)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
+        else if(!validateDuplication(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.");
+        }
+    }
+
+    private boolean validateDuplication(List<Integer> numbers){
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() == numbers.size();
     }
 
     private boolean validateRange(List<Integer> numbers){
@@ -32,6 +44,6 @@ public class Lotto {
     }
 
     private LottoRanking caculateRanking(List<Integer> userNumbers){
-        return LottoRanking.caculateRanking(this.numbers, this.bonusNumber, userNumbers);
+        return LottoRanking.caculateRanking(this.numbers.subList(0,6), this.numbers.get(6), userNumbers);
     }
 }
