290e34c19c3aec9fe4255475990cef1d668417ae
iamsseaa <homingsea@gmail.com>
2022-11-14 15:39:19 +0900

    fix: 예외처리 접근제어자 변경

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -18,37 +18,37 @@ public class User {
         validateMoneyDivided1000Won(money);
     }
 
-    public void validateInputNothing(String input) {
+    private void validateInputNothing(String input) {
         if (input.equals("")) {
             throw new IllegalArgumentException(Error.INPUT_NOTHING.getMessage());
         }
     }
 
-    public void validateInputBlank(String input) {
+    private void validateInputBlank(String input) {
         if (input.equals(" ")) {
             throw new IllegalArgumentException(Error.INPUT_BLANK.getMessage());
         }
     }
 
-    public void validateInputWithBlank(String input) {
+    private void validateInputWithBlank(String input) {
         if (input.contains(" ")) {
             throw new IllegalArgumentException(Error.INPUT_WITH_BLACK.getMessage());
         }
     }
 
-    public void validateNumeric(String input) {
+    private void validateNumeric(String input) {
         if (!input.matches("[+-]?\\d*(\\.\\d+)?")) {
             throw new IllegalArgumentException(Error.INPUT_NOT_NUMERIC.getMessage());
         }
     }
 
-    public void validateMoreThan1000Won(String input) {
+    private void validateMoreThan1000Won(String input) {
         if (Integer.parseInt(input) < 1000) {
             throw new IllegalArgumentException(Error.LESS_THAN_1000WON.getMessage());
         }
     }
 
-    public void validateMoneyDivided1000Won(String input) {
+    private void validateMoneyDivided1000Won(String input) {
         if (Integer.parseInt(input) % 1000 != 0) {
             throw new IllegalArgumentException(Error.NOT_DIVIDED_1000WON.getMessage());
         }
