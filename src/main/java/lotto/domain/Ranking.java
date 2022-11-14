dfea1848998e3f6c4579eed513ea71b450b8d789
yujaemin <woawoa0513@gmail.com>
2022-11-14 23:29:33 +0900

    chore: 열거 순서 변경

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -1,11 +1,12 @@
 package lotto.domain;
 
 public enum Ranking {
-    FIRST(1, 6,"6개 일치", "2,000,000,000원"),
-    SECOND(2, 5, "5개 일치, 보너스 볼 일치", "30,000,000원"),
-    THIRD(3, 5, "5개 일치", "1,500,000원"),
+    FIFTH(5, 3, "3개 일치", "5,000원"),
     FOURTH(4, 4, "4개 일치", "50,000원"),
-    FIFTH(5, 3, "3개 일치", "5,000원");
+    THIRD(3, 5, "5개 일치", "1,500,000원"),
+    SECOND(2, 5, "5개 일치, 보너스 볼 일치", "30,000,000원"),
+    FIRST(1, 6,"6개 일치", "2,000,000,000원");
+
 
     private final int place;
     private final int matchingNumberCount;
