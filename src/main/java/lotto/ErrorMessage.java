75124b238351a7e923a4756c40db489c96b899ea
seonpilKim <ksp970306@gmail.com>
2022-11-14 18:07:47 +0900

    Feat: 유효하지 않은 로또 번호 개수 에러 메시지 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -7,9 +7,14 @@ public class ErrorMessage {
 	private static final String INVALID_LOTTO_NUMBER = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 	private static final String INVALID_NUMBER = "숫자가 아닙니다.";
 	private static final String NOT_UNITS_OF_1000 = "1,000원 단위의 금액이여야 합니다.";
+	private static final String INVALID_NUMBER_OF_LOTTO_NUMBERS = "로또 번호는 6개여야 합니다.";
 
 	public static String isNotUnitsOf1000() {
 		return generate(NOT_UNITS_OF_1000);
+	}
+
+	public static String isInvalidNumberOfLottoNumbers() {
+		return generate(INVALID_NUMBER_OF_LOTTO_NUMBERS);
 	}
 
 	public static String isInvalidNumber() {
