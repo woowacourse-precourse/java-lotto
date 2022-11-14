64956d4a245d5e1ac9088693192dc079f8079666
java-saeng <gusdn85554@naver.com>
2022-11-14 16:27:21 +0900

    feat : 명세에 따라 Lotto 번호는 오름차순으로 정렬

--- src/main/java/lotto/generator/RandomLottoGenerator.java
+++ src/main/java/lotto/generator/RandomLottoGenerator.java
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import lotto.model.Lotto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static lotto.global.LottoConstants.*;
 
@@ -12,7 +14,10 @@ public class RandomLottoGenerator {
 
     public static Lotto makeLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER,
-                                                                 MAX_LOTTO_NUMBER_COUNT);
+                                                                 MAX_LOTTO_NUMBER_COUNT)
+                                       .stream()
+                                       .sorted()
+                                       .collect(Collectors.toList());
 
         return new Lotto(numbers);
     }
