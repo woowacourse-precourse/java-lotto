e77b1e50abefb8d07e2e1c5f4cc10d5ba12ce1d6
apptie <appti@naver.com>
2022-11-14 19:51:29 +0900

    refactor(LottoExceptionMessageUtils): 예외 문구 수정

--- src/main/java/lotto/util/message/LottoExceptionMessageUtils.java
+++ src/main/java/lotto/util/message/LottoExceptionMessageUtils.java
@@ -7,7 +7,7 @@ public enum LottoExceptionMessageUtils {
     INVALID_NUMBER_SIZE("%s 유효한 숫자는 총 6개여야만 합니다. 유효 숫자 개수 : %d"),
     INVALID_PURCHASE_AMOUNT_UNIT("%s 구입 금액은 1000원 단위여야만 합니다. 입력 금액 : %s"),
     INVALID_SEPARATOR("%s 필요한 숫자 구분자(,)가 누락되었습니다. 입력 값 : %s"),
-    INVALID_NUMBER_LENGTH("%s 유효하지 않은 길이입니다. 입력 값 : %s"),
+    INVALID_NUMBER_LENGTH("%s 유효하지 않은 길이입니다. 입력 값 길이 : %s"),
     INVALID_RANKING("%s 존재하지 않는 등수입니다. 입력 값 : %s");
 
     private final String message;
