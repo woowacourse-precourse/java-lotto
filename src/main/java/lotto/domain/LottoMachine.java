fd1d7b61eeb936d1ed92a5ec4f2caccda19b321e
Sinryuji <siler2474@gmail.com>
2022-11-15 13:44:39 +0900

    feat(LottoMachine): 등수 총합 계산하는 기능 구현

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -17,6 +17,21 @@ public class LottoMachine {
         return lottos;
     }
 
+    public List<Integer> getResult(List<Lotto> lottos, List<Integer> numbers, int bonus) {
+        List<Integer> result = List.of(0, 0, 0, 0, 0);
+        calRank(result, lottos, numbers, bonus);
+        return result;
+    }
+
+    private void calRank (List<Integer> result, List<Lotto> lottos, List<Integer> numbers, int bonus) {
+        for (Lotto lotto : lottos) {
+            int matchBonus = lotto.getNumbers().contains(bonus) ? 1 : 0;
+            lotto.getNumbers().retainAll(numbers);
+            Rank rank = Rank.findRank(lotto.getNumbers().size(), matchBonus);
+            result.set(rank.getIndex(), result.get(rank.getIndex() + 1));
+        }
+    }
+
     private List<Integer> createNumbers() {
         return Randoms.pickUniqueNumbersInRange(Lotto.lottoMinNumber, Lotto.lottoMaxNumber, Lotto.lottoLength);
     }
--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,24 +3,34 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 0, 2_000_000_000),
-    SECOND(5, 1, 30_000_000),
-    THIRD(5, 0, 1_500_000),
-    FOURTH(4, 0, 50_000),
-    FIFTH(3, 0, 5_000)
+    FIRST(0, 6, 0, 2_000_000_000),
+    SECOND(1, 5, 1, 30_000_000),
+    THIRD(2, 5, 0, 1_500_000),
+    FOURTH(3, 4, 0, 50_000),
+    FIFTH(4,3, 0, 5_000)
     ;
 
-    public final int matchNumbers;
-    public final int matchBonus;
-    public final int winAmount;
+    private final int index;
+    private final int matchNumbers;
+    private final int matchBonus;
+    private final int winAmount;
 
-    Rank(int matchNumbers, int matchBonus, int winAmount) {
+    Rank(int index, int matchNumbers, int matchBonus, int winAmount) {
+        this.index = index;
         this.matchNumbers = matchNumbers;
         this.matchBonus = matchBonus;
         this.winAmount = winAmount;
     }
 
-    public Rank findRank(int numbersCount, int bonusCount) {
+    public int getWinAmount() {
+        return this.winAmount;
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+
+    public static Rank findRank(int numbersCount, int bonusCount) {
         return Arrays.stream(values())
                 .filter(rank -> rank.matchNumbers == numbersCount)
                 .filter(rank -> rank.matchBonus == bonusCount)
