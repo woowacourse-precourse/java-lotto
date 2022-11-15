b85c02e6e9a3ff72461ee51c3a944b5a1b5513ed
silver <silver36c@gmail.com>
2022-11-15 23:20:35 +0900

    fix: 당첨 번호 잘못된 값 입력 시 에러메시지 출력

--- src/main/java/lotto/Manager.java
+++ src/main/java/lotto/Manager.java
@@ -8,59 +8,64 @@ import java.util.Comparator;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.ui.ErrorMessage;
 import lotto.ui.ManagerMessage;
 
 public class Manager {
-	private static final String SPLIT = ",";
-	private List<Lotto> lottos;
-	private List<Integer> win;
+    private static final String SPLIT = ",";
+    private List<Lotto> lottos;
+    private List<Integer> win;
 
-	private Lotto makeLotto() {
-		List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-		return new Lotto(sort(numbers));
-	}
+    private Lotto makeLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(sort(numbers));
+    }
 
-	private List<Integer> sort(List<Integer> numbers) {
-		List<Integer> sorted = new ArrayList<Integer>(numbers);
-		sorted.sort(Comparator.naturalOrder());
-		return sorted;
-	}
+    private List<Integer> sort(List<Integer> numbers) {
+        List<Integer> sorted = new ArrayList<Integer>(numbers);
+        sorted.sort(Comparator.naturalOrder());
+        return sorted;
+    }
 
-	public void setLottos(int count) {
-		lottos.clear();
-		for (int i = 0; i < count; i++) {
-			lottos.add(makeLotto());
-		}
-	}
+    public void setLottos(int count) {
+        lottos.clear();
+        for (int i = 0; i < count; i++) {
+            lottos.add(makeLotto());
+        }
+    }
 
-	public void printLottos() {
-		for (Lotto lotto : lottos) {
-			System.out.println(lotto.getNumbers().toString());
-		}
-	}
+    public void printLottos() {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers().toString());
+        }
+    }
 
-	public void setWin() {
-		System.out.println(ManagerMessage.INSERT_WIN_NUMBERS.getMessage());
-		String input = readLine();
-		win = separateWinInput(input);
-	}
+    public void setWin() {
+        System.out.println(ManagerMessage.INSERT_WIN_NUMBERS.getMessage());
+        String input = readLine();
+        try {
+            win = separateWinInput(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ErrorMessage.IllegalArgumentException.getMessage());
+        }
+    }
 
-	private List<Integer> separateWinInput(String input) {
-		List<Integer> result = new ArrayList<Integer>();
-		List<String> splited = Arrays.asList(input.split(SPLIT));
-		if (splited.size() != 6) {
-			throw new IllegalArgumentException();
-		}
-		for(String s:splited) {
-			if (!Util.isInteger(s)) {
-				throw new IllegalArgumentException();
-			}
-			result.add(Integer.parseInt(s));
-		}
-		return result;
-	}
+    private List<Integer> separateWinInput(String input) {
+        List<Integer> result = new ArrayList<Integer>();
+        List<String> splited = Arrays.asList(input.split(SPLIT));
+        if (splited.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+        for (String s : splited) {
+            if (!Util.isInteger(s)) {
+                throw new IllegalArgumentException();
+            }
+            result.add(Integer.parseInt(s));
+        }
+        return result;
+    }
 
-	public List<Integer> getWin(){
-		return win;
-	}
+    public List<Integer> getWin() {
+        return win;
+    }
 }
