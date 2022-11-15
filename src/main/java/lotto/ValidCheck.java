93418b87baba1e2c32b4beeca489a9852f7bbf07
고동우 <shp05179@gmail.com>
2022-11-15 23:08:48 +0900

    style: 제약조건에 맞게 수정
    15줄 넘는 함수 수정
    하드코딩한 문자열 상수화

--- src/main/java/lotto/ValidCheck.java
+++ src/main/java/lotto/ValidCheck.java
@@ -5,6 +5,10 @@ import java.util.HashSet;
 import java.util.List;
 
 public class ValidCheck {
+    private static final String PATTERN_COMMA_CHECK = "(.+,+).+";
+    private static final String PATTERN_DIGIT = "\\d+";
+    private static final String PATTERN_THOUSAND_UNIT = "\\d+000";
+
     public static void validateLotto(List<Integer> numbers) {
         if (Collections.max(numbers) > 45 || Collections.min(numbers) < 1) {
             throw new IllegalArgumentException(Error.RANGE_ERROR.getText());
@@ -20,16 +24,14 @@ public class ValidCheck {
 
     public static void formatCheckLotto(String InputValue) {
         InputValue = InputValue.replaceAll(" ", "");
-        String pattern = "(.+,+).+";
-        if (!InputValue.matches(pattern)) {
+        if (!InputValue.matches(PATTERN_COMMA_CHECK)) {
             throw new IllegalArgumentException(Error.INPUT_FORMAT_ERROR.getText());
         }
-        List<String> numbers = List.of(InputValue.split(","));
-        if (numbers.size() != 6) {
+        if (List.of(InputValue.split(",")).size() != 6) {
             throw new IllegalArgumentException(Error.LENGTH_ERROR.getText());
         }
         try {
-            numbers.forEach((n) -> {
+            List.of(InputValue.split(",")).forEach((n) -> {
                 Integer.parseInt(n);
             });
         } catch (Exception e) {
@@ -39,8 +41,7 @@ public class ValidCheck {
 
     public static void formatCheckBonus(String InputValue) {
         InputValue = InputValue.replaceAll(" ", "");
-        String pattern = "\\d+";
-        if (!InputValue.matches(pattern)) {
+        if (!InputValue.matches(PATTERN_DIGIT)) {
             throw new IllegalArgumentException(Error.NONDIGIT_ERROR.getText());
         }
         if (Integer.parseInt(InputValue) < 1 || Integer.parseInt(InputValue) > 45) {
@@ -50,8 +51,7 @@ public class ValidCheck {
 
     public static void formatCheckPrice(String InputValue) {
         InputValue = InputValue.replaceAll(" ", "");
-        String pattern = "\\d+000";
-        if (!InputValue.matches(pattern)) {
+        if (!InputValue.matches(PATTERN_THOUSAND_UNIT)) {
             throw new IllegalArgumentException(Error.PURCHASE_UNIT_ERROR.getText());
         }
     }
