ef4b4355c5ea595e89733d83131821e422b72bef
Cho-El <syzzang21c@gmail.com>
2022-11-15 21:45:51 +0900

    refactor(InputView): 한 가지 기능만 담당할 수 있게 함수 분할
    1. SringTokenizer를 통한 Stirng 쪼개기
    2. 기능 분산

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,47 +1,55 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.model.InputValidator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class InputView {
     private static final String INPUT_MONEY_COMMENT = "구입금액을 입력해 주세요.";
-    private static final String INPUT_LOTTO_NUM_COMMENT = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_LOTTO_NUM_COMMENT = "보너스 번호를 입력해 주세요.";
+    private static final String INPUT_LOTTO_NUM_COMMENT = "\n당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_LOTTO_NUM_COMMENT = "\n보너스 번호를 입력해 주세요.";
 
-    private InputView(){
+    private static final String DELIMITER = ",";
+
+    private InputView() {
     }
 
     public static int inputMoney() {
         System.out.println(INPUT_MONEY_COMMENT);
-        return stringToInt(Console.readLine());
+        return readSingleNumber(Console.readLine());
     }
 
     public static List<Integer> inputLottoNum() {
         System.out.println(INPUT_LOTTO_NUM_COMMENT);
-        return stringToList(Console.readLine());
+        return readWinningNumbers(Console.readLine());
     }
 
     public static int inputBonusNum() {
         System.out.println(INPUT_BONUS_LOTTO_NUM_COMMENT);
-        return stringToInt(Console.readLine());
+        return readSingleNumber(Console.readLine());
     }
 
-    public static List<Integer> stringToList(String input) {
-        List<Integer> winNumbers = new ArrayList<>();
-        for (String num : input.split(",")) {
-            winNumbers.add(stringToInt(num));
+    private static int readSingleNumber(String input) {
+        return stringToInt(input);
+    }
+
+    private static List<Integer> readWinningNumbers(String input) {
+        InputValidator.validateWinningNumberDelimiter(input);
+        return transferToNumberList(new StringTokenizer(input, DELIMITER));
+    }
+
+    private static List<Integer> transferToNumberList(StringTokenizer tokenizer) {
+        List<Integer> numbers = new ArrayList<>();
+        while (tokenizer.hasMoreTokens()) {
+            numbers.add(stringToInt(tokenizer.nextToken()));
         }
-        return winNumbers;
+        return numbers;
     }
 
     public static int stringToInt(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch(NumberFormatException exception) {
-            throw new IllegalArgumentException();
-        }
+        return InputValidator.validateParsingInteger(input);
     }
-
 }
