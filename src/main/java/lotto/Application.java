b2802f095a2bf5375fe89f7d8db268a5ed291894
Hyosik Philip Joo <lux1937@gmail.com>
2022-11-15 20:52:30 +0900

    feat: get winning numbers from input

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -12,7 +16,16 @@ public class Application {
         return Integer.parseInt(payment) / 1000;
     }
 
+    public static List<Integer> getWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningNumbersInput = readLine();
+        return Arrays.stream(winningNumbersInput.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
         int payment = validatePayment();
+        List<Integer> winningNumbers = getWinningNumbers();
     }
 }
