a47b9ce4b96e075bc76ad835521015f889f885ef
younghyun-na <younghyunna@naver.com>
2022-11-15 22:53:11 +0900

    :recycle: refactor: 출력 메세지 상수화

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,26 +1,49 @@
 package lotto.view;
 
 import lotto.Ranking;
-
+import java.text.DecimalFormat;
 import java.util.Map;
+
 
 public class OutputView {
 
   public static final String WINNING_RESULT_MESSAGE = "당첨 통계";
   public static final String DOTTED_LINE = "---";
+  public static final String BUY_LOTTO_MESSAGE = "%d개를 구매했습니다.";
+  public static final String EQUAL_NUMBER_MESSAGE = "%d개 일치 (%s원) - %d개";
+  public static final String EQUAL_NUMBER_FIVE_HAVE_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
 
   public static void printLottoAmount(int number) {
-    System.out.println(number + "개를 구매했습니다.");
+    System.out.printf(BUY_LOTTO_MESSAGE, number);
+    System.out.println();
   }
 
   public static void printWinningStatistics(Map<Ranking, Integer> winningResult) {
     System.out.println(WINNING_RESULT_MESSAGE);
     System.out.println(DOTTED_LINE);
-    System.out.println("3개 일치 (5,000원) - " + winningResult.get(Ranking.FIFTH) + "개");
-    System.out.println("4개 일치 (50,000원) - " + winningResult.get(Ranking.FORTH) + "개");
-    System.out.println("5개 일치 (1,500,000원) - " + winningResult.get(Ranking.THIRD) + "개");
-    System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + winningResult.get(Ranking.SECOND) + "개");
-    System.out.println("6개 일치 (2,000,000,000원) - " + winningResult.get(Ranking.FIRST) + "개");
+
+    for (Map.Entry<Ranking, Integer> entry : winningResult.entrySet()) {
+
+      if (entry.getKey().getNumber() == 5 && entry.getKey().isHasBonus()) {
+        System.out.printf(EQUAL_NUMBER_FIVE_HAVE_BONUS_MESSAGE, entry.getKey().getNumber(), makePriceToString(entry.getKey().getMoney()), entry.getValue());
+        System.out.println();
+      }
+
+      else if (entry.getKey().getNumber() == 5) {
+        System.out.printf(EQUAL_NUMBER_MESSAGE, entry.getKey().getNumber(), makePriceToString(entry.getKey().getMoney()), entry.getValue());
+        System.out.println();
+      }
+
+      else {
+        System.out.printf(EQUAL_NUMBER_MESSAGE, entry.getKey().getNumber(), makePriceToString(entry.getKey().getMoney()), entry.getValue());
+        System.out.println();
+      }
+    }
+  }
+
+  public static String makePriceToString(int price) {
+    DecimalFormat decFormat = new DecimalFormat("###,###");
+    return decFormat.format(price);
   }
 
   public static void printTotalEarningsPercent(double earnings) {
