ccc149815f090a8c35bf00723723d5569a6d8721
GwansuShin <happysun7080@naver.com>
2022-11-15 11:37:34 +0900

    feat(Utilities): 리스트 변환 기능
    당첨 번호 문자열을 정수형 리스트로 변환하는 기능 추가

--- src/main/java/lotto/util/Utilities.java
+++ src/main/java/lotto/util/Utilities.java
@@ -1,5 +1,6 @@
 package lotto.util;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,6 +9,13 @@ public class Utilities {
         return numbers
                 .stream()
                 .sorted()
+                .collect(Collectors.toList());
+    }
+
+    public static List<Integer> convertToList(String numbers) {
+        return Arrays
+                .stream(numbers.split(","))
+                .map(Integer::parseInt)
                 .collect(Collectors.toList());
     }
 }
