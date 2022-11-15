48e66c458d19b131c81e2b8483e42c72d2fb68f2
hgo641 <hgo641@gmail.com>
2022-11-15 19:38:12 +0900

    feat: 이익률 지수 표현 제거 로직 변경

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,6 +1,6 @@
 package lotto.domain;
 
-import java.math.BigDecimal;
+import java.text.NumberFormat;
 
 public class Money {
     private static final int LOTTO_PRICE = 1000;
@@ -25,20 +25,20 @@ public class Money {
         try {
             Integer.parseInt(userMoney);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[Error] 로또 구입 금액은 숫자만 입력할 수 있습니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 숫자만 입력할 수 있습니다.");
         }
     }
 
     private static void validateSize(int amount) {
         if (amount < LOTTO_PRICE) {
-            throw new IllegalArgumentException(String.format("[Error] 로또 구입 최소 금액은 %d원입니다.", LOTTO_PRICE));
+            throw new IllegalArgumentException(String.format("[ERROR] 로또 구입 최소 금액은 %d원입니다.", LOTTO_PRICE));
         }
     }
 
     private static void validateDivisibleByLottoTicketPrice(int amount) {
         if (amount % LOTTO_PRICE != ZERO) {
             throw new IllegalArgumentException(String
-                    .format("[Error] 로또 구입 금액은 %s원 단위로 나누어 떨어져야합니다.", LOTTO_PRICE));
+                    .format("[ERROR] 로또 구입 금액은 %s원 단위로 나누어 떨어져야합니다.", LOTTO_PRICE));
         }
     }
 
@@ -49,7 +49,8 @@ public class Money {
     public String calculateProfitRate(int profit) {
         double profitRate = (double) profit / (double) amount * PROFIT_RATE_MULTIPLIER;
         double roundedProfitRate = Math.round(profitRate * NUMBER_DECIMAL_CALCULATION) / NUMBER_DECIMAL_CALCULATION;
-        BigDecimal profitRateRemovedExponential = BigDecimal.valueOf(Double.parseDouble(String.valueOf(roundedProfitRate)));
-        return profitRateRemovedExponential.toString();
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        numberFormat.setGroupingUsed(false);
+        return String.valueOf(numberFormat.format(roundedProfitRate));
     }
 }
