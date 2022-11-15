f1e89f34a0202c4daca6cf229d4c59a5bceeba17
Sinryuji <siler2474@gmail.com>
2022-11-15 14:53:53 +0900

    test(CalculatorTest): 결과 리스트 업데이트 테스트

--- src/test/java/lotto/CalculatorTest.java
+++ src/test/java/lotto/CalculatorTest.java
@@ -1,2 +1,28 @@
-package lotto;public class CalculatorTest {
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lotto.domain.Calculator;
+import lotto.domain.Lotto;
+
+public class CalculatorTest {
+    @DisplayName("결과 리스트가 잘 업데이트 되는지 확인")
+    @Test
+    void calRankTest() {
+        List<Integer> result = Arrays.asList(0, 0, 0, 0, 0);
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        lottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7)));
+        lottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 8)));
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int bonus = 7;
+        Calculator.calRank(result, lottos, numbers, bonus);
+        assertThat(result).isEqualTo(List.of(1, 1, 1, 0, 0));
+    }
 }
