8c6287e9e114ff13f66b6ce8ca18d1a451804db3
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-15 22:37:09 +0900

    feat: 예외처리 : checkLuckyLottoNumberExceptions() 메서드 추가

---
+++ src/main/java/lotto/exception/LuckyLottoNumberException.java
@@ -0,0 +1,21 @@
+package lotto.exception;
+
+import static lotto.domain.Constant.ERROR_MESSAGE;
+import static lotto.domain.Constant.MAX_LOTTO_NUMBER;
+import static lotto.domain.Constant.MIN_LOTTO_NUMBER;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lotto.domain.Constant;
+
+public class LuckyLottoNumberException {
+
+    public void checkLuckyLottoNumberExceptions(List<Integer> inputLuckyNumbers) {
+        checkDuplicatedLottoNumber(inputLuckyNumbers);
+        checkLottoNumberBetweenOneAndFortyFive(inputLuckyNumbers);
+    }
+
+
+
+}
