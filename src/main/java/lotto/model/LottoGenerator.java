5ac9de029a6dd54894dc76f3a24ada16c31c1a04
gomdol <oioifor@naver.com>
2022-11-15 22:42:45 +0900

    src(LottoGenerator) : 로또 번호 랜덤 출력 처리

---
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -0,0 +1,24 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGenerator {
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
+    private static final int MAX_LENGTH = 6;
+    public static List<List<Integer>> lottoGeterator(int count){
+        List<List<Integer>> lottos = new ArrayList<>();
+        for(int i =0; i<count; i++){
+
+            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_RANGE, MAX_RANGE, MAX_LENGTH);
+            Lotto lotto = new Lotto(lottoNumbers);
+            lottos.add(lotto.getNumbers());
+        }
+
+        return lottos;
+    }
+
+}
