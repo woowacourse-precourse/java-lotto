6a2034b41fb973f3d31d6b2e6942a1db62331f06
saehim <saeilee.vxv@gmail.com>
2022-11-15 16:33:29 +0900

    test(lotto) : 추첨 번호 입력 범위 유효 테스트

--- src/test/java/lotto/view/InputTest.java
+++ src/test/java/lotto/view/InputTest.java
@@ -1,14 +1,28 @@
 package lotto.view;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
 import java.util.Scanner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class InputTest {
+
+    static Scanner scanner;
+
+    @BeforeEach
+    void init() {
+        scanner = new Scanner(System.in);
+    }
 
     @Test
     void inputMoney() {
@@ -36,4 +50,19 @@ class InputTest {
                 .hasMessage(Messages.ERROR_NOT_DIVDE1000);
     }
 
+    @DisplayName("입력받은 당첨 번호가 범위에 맞는가?")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,49"})
+    void vaildNumsRange(String input) {
+        InputStream in = generateUserInput(input);
+        System.setIn(in);
+        scanner = new Scanner(System.in);
+        assertThatThrownBy(() -> Input.askWinNums())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Messages.ERROR_NOT_VAILDRANGE);
+    }
+
+    public static InputStream generateUserInput(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
 }
