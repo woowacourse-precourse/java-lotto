30180d3770ccc037de0dae81a7fdcc0f442cdf41
WonSubSub <boxer1995@naver.com>
2022-11-15 17:58:46 +0900

    feat: Lotto클래스에 입력 값에 따른 예외상황 판단 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,14 +8,31 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        sameNumber(numbers);
+        wrongNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(Print.lottoNumberError());
         }
     }
 
     // TODO: 추가 기능 구현
+    private void sameNumber(List<Integer> numbers){
+        for (int i = 0; i < numbers.size(); i++){
+            if (Collections.frequency(numbers, numbers.get(i)) > 1){
+                throw new IllegalArgumentException(Print.lottoNumberError());
+            }
+        }
+    }
+
+    private void wrongNumber(List<Integer> numbers){
+        for (int i = 0; i < numbers.size(); i++){
+            if (numbers.get(i) < 1 || numbers.get(i) > 45){
+                throw new IllegalArgumentException(Print.lottoNumberError());
+            }
+        }
+    }
 }
