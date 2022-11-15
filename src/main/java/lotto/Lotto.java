adcd773574d3c30c6eea0a356482d380261fe6f6
kwang <shinkwang@yonsei.ac.kr>
2022-11-15 20:21:22 +0900

    feat: Make validation feature for range of lottery numbers
    로또 번호의 범위는 1부터 45까지다. 따라서 숫자가 1부터 45의 범위를 넘어가는지 확인하는 기능을 만든다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,6 +15,7 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicate(numbers);
+        validateRange(numbers);
     }
 
     // TODO: 추가 기능 구현
@@ -28,6 +29,18 @@ public class Lotto {
         Set<Integer> duplicationSet = new HashSet<>(numbers);
         if (duplicationSet.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 숫자는 중복되어선 안됩니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            checkRangeOfNumeber(number);
+        }
+    }
+
+    private static void checkRangeOfNumeber(Integer number) {
+        if (number < 1 || 45 < number) {
+            throw new IllegalArgumentException("[ERROR] 로또 숫자의 범위는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 }
