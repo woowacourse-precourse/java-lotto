24dec1ede1fb2da831ced447b55b829fe82c9536
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-15 17:46:35 +0900

    test: 로또 번호가 정상일 시, 에러를 던지지 않는 케이스 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -28,5 +28,19 @@ class LottoTest {
     void createLottoNotRangeNumber() {
         assertThatThrownBy(()-> new Lotto(List.of(1,2,33,46,47,12)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("toString 사용 시 List 형태로 반환")
+    @Test
+    void toStringTest() {
+        assertThat(new Lotto(List.of(12,13,22,34,35,45)).toString())
+                .isEqualTo("[12, 13, 22, 34, 35, 45]");
+    }
+
+    @DisplayName("로또 번호 정상일 시, 에러를 던지지 않음")
+    @Test
+    void createValidLottoNumber() {
+        assertThatCode(() -> new Lotto(List.of(2,3,4,43,44,45)))
+                .doesNotThrowAnyException();
     }
 }
