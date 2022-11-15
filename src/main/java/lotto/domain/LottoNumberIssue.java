87193890e80162d0e2c489c7908df5980a8ff515
gunheeK <gunhee0516@gmail.com>
2022-11-15 10:38:06 +0900

    feat: 발행된 로또 번호 오름차순 정렬 기능 추가

--- src/main/java/lotto/domain/LottoNumberIssue.java
+++ src/main/java/lotto/domain/LottoNumberIssue.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class LottoNumberIssue {
@@ -14,7 +15,14 @@ public class LottoNumberIssue {
 
     public List<List<Integer>> lottoIssue() {
         List<List<Integer>> lottoNumber = createLottoNumber();
+        sortAscending(lottoNumber);
         return lottoNumber;
+    }
+
+    private void sortAscending(List<List<Integer>> lottoNumber) {
+        for(int i = 0; i < lottoNumber.size(); i++) {
+            lottoNumber.get(i).sort(Comparator.naturalOrder());
+        }
     }
 
     public List<List<Integer>> createLottoNumber() {
