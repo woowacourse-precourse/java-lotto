08a4c787622218a0b07ed09296aa6d0f72179f8d
mgOh <issac_oh@naver.com>
2022-11-15 16:01:05 +0900

    feat: 당첨 번호를 입력받는 기능 추가

--- src/main/java/lotto/input_processing/InputWinningNumbers.java
+++ src/main/java/lotto/input_processing/InputWinningNumbers.java
@@ -1,6 +1,7 @@
-package lotto;
+package lotto.input_processing;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.UserInputException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,10 +9,10 @@ import java.util.List;
 public class InputWinningNumbers {
     static UserInputException userInputException = new UserInputException();
 
-    InputWinningNumbers(){}
+    public InputWinningNumbers(){}
 
-    /** 당첨 번호 입력 및 예외처리 기능 */
-    static List<Integer> getWinningNumbers() {
+    /** 당첨 번호를 입력받는 기능 */
+    public static List<Integer> getWinningNumbers() {
         System.out.println("\n당첨 번호를 입력해 주세요.");
         String[] input = Console.readLine().split(",");
         if (!userInputException.checkWinningNumberSize(input.length))
@@ -24,18 +25,5 @@ public class InputWinningNumbers {
             result.add(Integer.valueOf(input[i]));
         }
         return result;
-    }
-
-    /** 보너스 번호 입력 및 예외처리 기능 */
-     static int getBonusNumbers(List<Integer> winningNumbers) {
-        System.out.println("\n보너스 번호를 입력해 주세요.");
-        String input = Console.readLine();
-        if (!userInputException.checkInputTypeAndRange(input))
-            throw new IllegalArgumentException("[ERROR] 보너스 번호 입력값이 잘못되었습니다.");
-        if(winningNumbers.contains(Integer.valueOf(input))){
-            throw new IllegalArgumentException("[ERROR] 당첨번호에 이미 존재하는 숫자입니다.");
-        }
-
-        return Integer.valueOf(input);
     }
 }
