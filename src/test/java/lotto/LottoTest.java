61ca9a344119343bbda948900c39ec638dc36cd4
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-15 20:13:42 +0900

    test: count test

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -44,5 +46,27 @@ class LottoTest {
     })
     void issueLotto(int number, int numberOfLottos) {
         assertThat(Lotto.issueLotto(numberOfLottos).size()).isEqualTo(number);
+    }
+
+    @ParameterizedTest
+    @DisplayName("당첨 결과 확인")
+    @CsvSource(value = {
+            "8,21,23,41,42,43:0",
+            "3,5,11,16,32,38 :2",
+            "7,11,16,35,36,44:0",
+            "1,8,11,31,41,42:1",
+            "13,14,16,38,42,45:0",
+            "7,11,30,40,42,43:0",
+            "2,13,22,32,38,45:1"
+    },delimiter = ':')
+    void count(String lotto, int count) {
+        Lotto user = new Lotto(
+                Stream.of(lotto.split(","))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList())
+        );
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThat(user.count(winningNumber)).isEqualTo(count);
     }
 }
