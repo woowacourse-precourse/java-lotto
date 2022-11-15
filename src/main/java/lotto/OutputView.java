ba14aab51e5315b6c264e6633e4f6473524acae8
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-15 17:39:32 +0900

    style(OutputView): 상수 문자열 값을 서식 형식의 문자열로 수정

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -4,12 +4,12 @@ import java.util.HashMap;
 import java.util.List;
 
 public class OutputView {
-    private static final String NUMBER_OF_LOTTOS_PURCHASED_MESSAGE = "개를 구매했습니다.";
+    private static final String NUMBER_OF_LOTTOS_PURCHASED_MESSAGE = "%d개를 구매했습니다.";
     private static final String START_WINNING_STATICS_MESSAGE = "당첨 통계\n---";
     private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %s%%입니다.";
 
     public static void printNumberOfLottosPurchased(int numberOfLottosPurchased) {
-        System.out.println(numberOfLottosPurchased + NUMBER_OF_LOTTOS_PURCHASED_MESSAGE);
+        System.out.printf(NUMBER_OF_LOTTOS_PURCHASED_MESSAGE, numberOfLottosPurchased);
     }
 
     public static void printLottosList(List<Lotto> lottos) {
