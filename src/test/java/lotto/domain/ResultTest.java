fb16abcce99b49a6e4a2a9a77b617473d8e6e5ac
SeongukBaek <bsu6235@gmail.com>
2022-11-15 12:10:26 +0900

    test: modify test

--- src/test/java/lotto/domain/ResultTest.java
+++ src/test/java/lotto/domain/ResultTest.java
@@ -6,18 +6,15 @@ import org.junit.jupiter.api.Test;
 
 public class ResultTest {
 
-    @DisplayName("주어진 점수에 따라 정상적인 Result가 반환")
+    @DisplayName("주어진 점수에 따른 정상적인 Result를 반환한다.")
     @Test
     void getResultTest_정상점수() {
         Assertions.assertThat(Result.computeScore(50)).isEqualTo(Result.FIVE);
     }
 
-    @DisplayName("정상적이지 않은 점수로 인해 IllegalArgumentException 발생")
+    @DisplayName("정상적이지 않은 점수인 경우 NOTHING을 반환한다.")
     @Test
     void getResultTest_비정상점수() {
-        Assertions.assertThatThrownBy(
-                        () -> Result.computeScore(90)
-                )
-                .isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThat(Result.computeScore(90)).isEqualTo(Result.NOTING);
     }
 }
