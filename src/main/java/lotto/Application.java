e34b98589210ae8a3ec763df9ac812074a47eda1
HASEUNGHEEE <ariul123@gmail.com>
2022-11-15 23:10:59 +0900

    feat: 로또 전체 프로그램 동작하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.ResultView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            LottoController lottoController = new LottoController();
+            lottoController.run();
+        } catch (IllegalArgumentException e) {
+            ResultView.printException(e);
+        }
     }
 }
