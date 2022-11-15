b180416616bc439220ea844f0945c1f040b408de
starwook <star.wookk@gmail.com>
2022-11-15 22:32:25 +0900

    feat(ErrorResource.java): Add static
    새로운 에러 출력을 위한 상수 추가

--- src/main/java/lotto/domain/ErrorResource.java
+++ src/main/java/lotto/domain/ErrorResource.java
@@ -3,4 +3,5 @@ package lotto.domain;
 public class ErrorResource {
     public static final String errorStart = "[ERROR]";
     public static final String numberCountNotFit = "숫자의 크기가 " + Resource.numberSize +"이 아닙니다";
+    public static final String numberDuplicate =" 중복되는 숫자가 있습니다";
 }
