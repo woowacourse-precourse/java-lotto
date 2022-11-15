6260fd0b86e1547736dc3d97fbf7799ed5b53c26
yw824 <yongwoo25@gmail.com>
2022-11-15 18:29:09 +0900

    test: 사용자 금액 입력 테스트 케이스 추가
    - 로또_수량_에러_테스트:
    	- 중간에 알파벳 있는 케이스
    	- 세자리 수 케이스
    	- 4자리 이상이지만 처음에 알파벳 있는 케이스
    	- 100원 단위가 있는 케이스
    - 로또_수량_정상_테스트:
    	- 12000 -> 12 반환

--- src/test/java/lotto/LogicTest.java
+++ src/test/java/lotto/LogicTest.java
@@ -1,4 +1,37 @@
 package lotto;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static lotto.ErrorCode.*;
+
 public class LogicTest {
+    Logic logic = new Logic();
+
+    @DisplayName("[ERROR] 로또 구입 금액은 1000원 단위로 입력되어야 합니다.")
+    @Test
+    void 로또_수량_에러_테스트() {
+        assertThatThrownBy(() -> logic.checkPieces("10000j"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> logic.checkPieces("100"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> logic.checkPieces("j000"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> logic.checkPieces("15500"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_수량_정상_테스트() {
+        assertThat(logic.checkPieces("12000")).isEqualTo(12);
+    }
+
 }
