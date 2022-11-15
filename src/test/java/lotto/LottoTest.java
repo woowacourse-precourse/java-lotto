a7695cca7d7b17f5597186c949de7bd3b4840f44
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-11-15 21:39:54 +0900

    test: rank설정 테스트 코드 추가
    Lotto class안의 Rank를 설정에 관련된 로직들을 테스트하는 코드 추가함

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +26,57 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("Rank를 반환한다.")
+    @Test
+    void getLottoRank() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        String input = "1,2,3,4,5,6\n7\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        WinningNumber winningNumbers = new WinningNumber();
+        lotto.setWinningNumber(winningNumbers);
+
+        //when
+        Rank rank = lotto.lottoRank();
+
+        //then
+        Assertions.assertThat(rank).isEqualTo(Rank.FIRST);
+
+
+    }
+
+    @Test
+    void matchWinningNumbers() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        String input = "1,2,3,4,5,6\n7\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        WinningNumber winningNumbers = new WinningNumber();
+        lotto.setWinningNumber(winningNumbers);
+
+        //when
+        int matchWinningNumberCount = lotto.matchWinningNumbers();
+
+        //then
+        Assertions.assertThat(matchWinningNumberCount).isEqualTo(6);
+    }
+
+    @Test
+    void matchBonusNumber() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        String input = "1,2,3,4,5,6\n7\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        WinningNumber winningNumbers = new WinningNumber();
+        lotto.setWinningNumber(winningNumbers);
+
+        //when
+        int matchBonusNumberCount = lotto.matchBonusNumber();
+
+        //then
+        Assertions.assertThat(matchBonusNumberCount).isEqualTo(0);
+    }
 }
