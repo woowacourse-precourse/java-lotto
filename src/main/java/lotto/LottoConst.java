585a6252bcfc332146e55152b588409fe81db257
limecats0331 <limecats0331@gmail.com>
2022-11-15 19:45:14 +0900

    fix(LottoConst): 상수 수정
    5개를 맞추고 보너스 숫자를 맞춘 경우가 생겨서
    매칭되는 길이를 +1 해줬음.

--- src/main/java/lotto/LottoConst.java
+++ src/main/java/lotto/LottoConst.java
@@ -5,6 +5,6 @@ public interface LottoConst {
     static final int END_RANGE = 45;
     static final int LOTTO_LENGTH = 6;
     static final int MIN_MATCH = 3;
-    static final int MAX_MATCH = 7;
+    static final int MAX_MATCH = 8;
     static final int PURCHASE_UNIT = 1000;
 }
