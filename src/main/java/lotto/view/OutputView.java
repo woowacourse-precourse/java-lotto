8d0d03170a630bd3f22b441ae9ca007c98582a48
gunheeK <gunhee0516@gmail.com>
2022-11-15 12:24:09 +0900

    feat: 발행된 로또 번호 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,18 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
     private static final String Lotto_Purchase_Quantity_Message = "개를 구매했습니다.";
 
     public static void OutputLottoPurchaseQuantity(int purchaseQuantity) {
         System.out.println();
         System.out.println(purchaseQuantity + Lotto_Purchase_Quantity_Message);
+    }
+
+    public static void lottoNumber(List<List<Integer>> lottoNumber) {
+        for (List<Integer> integers : lottoNumber) {
+            System.out.println(integers.toString());
+        }
     }
 }
