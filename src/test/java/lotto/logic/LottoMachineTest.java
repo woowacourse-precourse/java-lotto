6b476b1a7ce1a6e4fa54c522c6d9e3989120194f
DaWoon Lee <qkgk61@gmail.com>
2022-11-15 22:32:33 +0900

    Test : LottoMachine 클래스에서 하나의 로또를 만드는 메서드 generateLotto 테스트

---
+++ src/test/java/lotto/logic/LottoMachineTest.java
@@ -0,0 +1,35 @@
+package lotto.logic;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoInfo;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoMachineTest {
+
+    static LottoMachine lottoMachine;
+
+    @BeforeAll
+    static void init() {
+        lottoMachine = LottoMachine.getInstance();
+    }
+
+    @DisplayName("로또 번호가 오름차순으로 정렬된 하나의 로또를 만든다.")
+    @Test
+    void generateLotto() {
+        Lotto lotto = lottoMachine.generateLotto();
+
+        List<Integer> numbers = new ArrayList<>();
+        for (int size = LottoInfo.SIZE.value(), i = 0; i < size; i++) {
+            numbers.add(lotto.get(i));
+        }
+
+        assertThat(numbers).isSorted();
+    }
+
+}
