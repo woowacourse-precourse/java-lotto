4ba0f0e8d8b6fc916ac38d20cfd3959e7a460951
hyun0404woo <hyun0404woo@gmail.com>
2022-11-15 17:45:58 +0900

    feat: 로또 구매 시 발생할 수 있는 예외 사항 입력

--- src/main/java/lotto/exception/ExceptionType.java
+++ src/main/java/lotto/exception/ExceptionType.java
@@ -6,6 +6,8 @@ public enum ExceptionType {
 
     // LOTTO INPUT ERROR
     NOT_NUMBER(ERROR_PREFIX, "자연수를 입력해주세요."),
+    WRONG_UNIT_OF_MONEY_TO_PURCHASE(ERROR_PREFIX, "1000원 단위의 입력이 필요합니다."),
+    LACK_OF_MONEY_TO_PURCHASE(ERROR_PREFIX, "로또 구매를 위해서는 최소 1000원이 필요합니다."),
     NULL_INPUT(ERROR_PREFIX, "null 입력은 허용되지 않습니다."),
     WINNING_LOTTO_NUMBER_INPUT_PATTERN_NOT_MATCH(ERROR_PREFIX, "당첨 번호 입력 패턴이 일치하지 않습니다. Ex) 1,2,3,4,5,6"),
 
