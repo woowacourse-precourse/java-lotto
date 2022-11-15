300c94452ab9095085bd95d2e06f64245074af5e
jijongkwon <help3451@naver.com>
2022-11-15 14:30:58 +0900

    feat : 금액에 따른 티켓 구매 기능 추가

--- src/main/java/lotto/domain/PurchasingLottoList.java
+++ src/main/java/lotto/domain/PurchasingLottoList.java
@@ -5,10 +5,20 @@ import java.util.List;
 public class PurchasingLottoList {
     private NumberGenerator numberGenerator = new NumberGenerator();
     private List<List<Integer>> lottoList;
+    private int numberOfTicket;
 
-    public PurchasingLottoList(int ticketCount){
-        this.lottoList = numberGenerator.createRandomNumbers(ticketCount);
+    public PurchasingLottoList(){
+        this.lottoList = numberGenerator.createRandomNumbers(numberOfTicket);
+    }
+    public List<List<Integer>> getLottoList() {
+        return lottoList;
     }
 
+    public void moneyToTicket(int money){
+        if(money % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력하세요.");
+        }
 
+        this.numberOfTicket = money / 1000;
+    }
 }
