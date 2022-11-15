172da8e9108fbe27ff052b53e67b7784c687185d
hyeonseog-won <smreo75@gmail.com>
2022-11-15 22:08:43 +0900

    feat: Message 상수 클래스 생성

---
+++ src/main/java/lotto/MessageConstant.java
@@ -0,0 +1,13 @@
+package lotto;
+
+public class MessageConstant {
+    public static final String ERROR_MESSAGE = "[ERROR] ";
+    public static final String NOT_SIX_LOTTO_NUMBER_MESSAGE = "로또 숫자가 로또 숫자가 6개가 아닙니다.";
+    public static final String HAVE_DUPLICATE_NUMBER_MESSAGE = "중복된 번호가 있습니다.";
+    public static final String BONUS_NUMBER_DUPLICATE_MESSAGE = "당첨번호와 보너스 번호는 달라야 합니다.";
+    public static final String INPUT_LOTTO_MESSAGE = "숫자와 ,만 입력해 주세요.";
+    public static final String ONLY_NUMBER_MESSAGE = "숫자만 입력해 주세요.";
+    public static final String LOTTO_MINIMUM_PRICE_MESSAGE = "1,000원 이상을 입력해 주세요.";
+    public static final String LOTTO_PRICE_RULE_MESSAGE = "1,000원으로 나누어 떨어지는 금액만 입력해 주세요.";
+    public static final String LOTTO_NUMBER_RANGE_RULE_MESSAGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+}
