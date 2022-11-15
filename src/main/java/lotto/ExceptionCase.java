0e5c9a0b1de729c9382751e6928dd67ca52ca985
fnzl54 <rnjscksdud1@gmail.com>
2022-11-15 21:43:07 +0900

    Feat: create checkInputSameNumber method

--- src/main/java/lotto/ExceptionCase.java
+++ src/main/java/lotto/ExceptionCase.java
@@ -1,4 +1,14 @@
 package lotto;
 
+import java.util.*;
+
 public class ExceptionCase {
-}
+    private static boolean checkInputSameNumber(List<Integer> input) {
+        Set<Integer> inputRemoveSameNumber = new HashSet<>(input);
+
+        if (inputRemoveSameNumber.size() != 6) {
+            return false;
+        }
+
+        return true;
+    }
