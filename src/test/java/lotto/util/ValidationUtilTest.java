d729f0f3c41ead6db9c32a3396d48ca65cf9014a
jiwon <ljwon77@daum.net>
2022-11-15 10:56:28 +0900

    teat: add purchase amount divided by 1000
    구매 금액이 1000으로 나누어떨어지는지 검증하는 테스트 코드를 추가하였습니다.

--- src/test/java/lotto/util/ValidationUtilTest.java
+++ src/test/java/lotto/util/ValidationUtilTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import static lotto.constant.ErrorOutputMessage.PURCHASE_REMAINDER;
 import static lotto.constant.ErrorOutputMessage.PURCHASE_TYPE;
 import static org.assertj.core.api.Assertions.*;
 
@@ -36,4 +37,30 @@ class ValidationUtilTest {
                     .hasMessageContaining(PURCHASE_TYPE);
         }
     }
+
+    @Test
+    @DisplayName("구매 금액이 1000 단위로 나누어 떨어지는지 테스트")
+    void 구매_금액_나머지_테스트() throws Exception {
+        //given
+        ValidationUtil validationUtil = new ValidationUtil();
+        Method method = validationUtil.getClass().getDeclaredMethod("validateRemainder", String.class);
+        method.setAccessible(true);
+
+        //when
+        String correctAmount = "3000";
+        String wrongAmount = "1234";
+
+        //then
+        int purchaseAmount = (int) method.invoke(validationUtil, correctAmount);
+        assertThat(purchaseAmount).isEqualTo(3000);
+
+        try {
+            method.invoke(validationUtil, wrongAmount);
+        } catch (InvocationTargetException e) {
+            assertThat(e.getTargetException())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(PURCHASE_REMAINDER);
+        }
+    }
+
 }
