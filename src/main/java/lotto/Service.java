723af93539d860858bf25394e8a9d273f6c3bdfa
jootang2 <joohwan.song15b@gmail.com>
2022-11-15 10:25:37 +0900

    :sparkles: 예외 2. 숫자이지만 1000원 단위가 아니인 경우

--- src/main/java/lotto/Service.java
+++ src/main/java/lotto/Service.java
@@ -7,6 +7,9 @@ public class Service {
     public int getMoneyInput() {
         try{
             int input = Integer.parseInt(Console.readLine());
+            if (!(input % 1000 == 0)) {
+                throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
+            }
             return input;
         } catch (NumberFormatException e){
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
