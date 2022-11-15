9fc8c93fb07c8d16fae34d68da3a71a055e377fa
junyong <jymoon1121@gmail.com>
2022-11-15 18:41:59 +0900

    refactor: Application에서 실행(Executor 삭제)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,33 @@
 package lotto;
 
-import lotto.domain.Executor;
+
+import lotto.domain.LottoMachine;
+import lotto.domain.User;
+import lotto.model.Lotto;
+import lotto.view.Printer;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Executor executor = new Executor();
-        executor.startGame();
+        try {
+            Printer.inputBuyAmount();
+            LottoMachine machine = new LottoMachine();
+            machine.getInputMoney();
+            List<Lotto> lottos = machine.createLottoNumber();
+
+            User user = new User(lottos);
+            user.inputWinningNumber();
+            user.inputBonusNumber();
+
+            machine.printLottos();
+            machine.getResult(user.getWinningNumber(), user.getBonusNumber());
+
+            machine.printResult();
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
