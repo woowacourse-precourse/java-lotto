71a09874d01bfe246571366f5f2262dbcbf8a5bd
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-15 20:59:40 +0900

    test(LottoTest) : 당첨 번호 범위에 대한 예외 테스트 케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,12 +2,12 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    Game game = new Game();
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -24,4 +24,10 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 1~45 사이의 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfNumberRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
