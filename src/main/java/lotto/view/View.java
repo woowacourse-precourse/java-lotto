c3d15df30a694968b60125917283b784611035d4
kimJaeHyeon <kmr2644@gmail.com>
2022-11-15 17:14:47 +0900

    refactor: 로또번호 검증메서드 분리 및 정수검증메서드 구현

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -6,23 +6,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import lotto.type.Prize;
-import lotto.validator.LottoValidator;
 
 public class View {
 
     public long inputMoney() {
         System.out.println("구입금액을 입력해 주세요");
-        String inputString = Console.readLine();
-        LottoValidator.validateHasNumber(List.of(inputString));
-        long money = Long.parseLong(inputString);
-        validateMoney(money);
-        return money;
-    }
-
-    private void validateMoney(long money) {
-        if (money % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 입력금액은 1000원 단위여야 합니다.");
-        }
+        String input = Console.readLine();
+        validateHasNumber(List.of(input));
+        return Long.parseLong(input);
     }
 
     public void printBuyLotto(LottoDto lottoDto) {
@@ -33,36 +24,22 @@ public class View {
         }
     }
 
-    public LottoInputDto inputNumbers() {
-        List<Integer> winningNumbers = inputWinningNumbers();
-        int bonusNumber = inputBonusNumber();
-        if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 중복된 정수를 입력하면 안됩니다.");
-        }
-        return new LottoInputDto(winningNumbers, bonusNumber);
-    }
-
-    private List<Integer> inputWinningNumbers() {
+    public List<Integer> inputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요");
-        String numbers = Console.readLine();
-        List<String> inputStrings = Arrays.stream(numbers.split(","))
-                .sorted()
+        String input = Console.readLine();
+        List<String> winningNumbersInput = Arrays.stream(input.split(","))
                 .collect(Collectors.toList());
-        LottoValidator.validateHasNumber(inputStrings);
-
-        List<Integer> winningNumbers = inputStrings.stream()
+        validateHasNumber(winningNumbersInput);
+        return winningNumbersInput.stream()
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-        LottoValidator.validateWinningNumber(winningNumbers);
-
-        return winningNumbers;
     }
 
-    private int inputBonusNumber() {
+    public int inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요");
-        String inputString = Console.readLine();
-        LottoValidator.validateHasNumber(List.of(inputString));
-        return Integer.parseInt(inputString);
+        String input = Console.readLine();
+        validateHasNumber(List.of(input));
+        return Integer.parseInt(input);
     }
 
     public void printPrize(PrizeDto prizeDto) {
@@ -77,5 +54,12 @@ public class View {
         System.out.printf("총 수익률은 %.1f%%입니다.\n", prizeDto.getYield());
 
     }
-
+    private void validateHasNumber(List<String> list) {
+        String regex = "[0-9]+";
+        for (String number : list) {
+            if (!number.matches(regex)) {
+                throw new IllegalArgumentException("[ERROR] 정수만 입력해야 합니다.");
+            }
+        }
+    }
 }
