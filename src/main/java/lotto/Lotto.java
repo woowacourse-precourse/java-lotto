59c0f2c6a2c78a8a2db3aa0ff91a2ce07f98ed75
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-15 15:26:04 +0900

    feat: Add outOfRange function to check lotto number is in range

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,6 +17,9 @@ public class Lotto {
         if (!isDistinct(numbers)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 값이 없어야 합니다.");
         }
+        if (outOfRange(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 
     /**
@@ -27,5 +30,14 @@ public class Lotto {
     private static boolean isDistinct(List<Integer> numbers) {
         return numbers.stream().distinct()
             .count() == numbers.size();
+    }
+
+    /**
+     * Check if the Lotto number is out of range.
+     * @param numbers the list of integer to be checked.
+     * @return false if number is not in between 1 and 45.
+     */
+    private static boolean outOfRange(List<Integer> numbers) {
+        return numbers.stream().anyMatch(num -> num>=1 && num<=45);
     }
 }
