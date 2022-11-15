d8745a70d714a7582fda505db1158d1648e5ccc5
ddukddaki <rlfrkdms1@naver.com>
2022-11-15 15:47:19 +0900

    feat(LottoController): 로또 구입금을 받는 기능

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,23 +1,30 @@
 package lotto.controller;
 
+import lotto.domain.LottoPurchaseMoney;
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
 
+    private InputView inputView;
     private OutputView outputView;
 
-    public LottoController(OutputView outputView) {
+    public LottoController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
         this.outputView = outputView;
     }
 
     public void run() {
         try {
-
+            LottoPurchaseMoney lottoPurchaseMoney = getLottoPurchaseMoney();
         } catch (IllegalArgumentException e) {
             outputView.printErrorMessage(e.getMessage());
         }
     }
 
-
+    private LottoPurchaseMoney getLottoPurchaseMoney() {
+        int lottoPurchaseMoney = inputView.inputLottoPurchaseMoney();
+        return new LottoPurchaseMoney(lottoPurchaseMoney);
+    }
 
 }
