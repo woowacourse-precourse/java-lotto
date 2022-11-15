ba49ddefc6c5627ec01a9922692c1f6e5b4fbe2f
metorg <metrog950@gmail.com>
2022-11-15 21:43:25 +0900

    feat: 당첨결과 출력 메서드 생성

--- src/main/java/lotto/WinningResult.java
+++ src/main/java/lotto/WinningResult.java
@@ -9,14 +9,17 @@ public class WinningResult {
     private int third;
     private int fourth;
     private int fifth;
+    private View view;
 
-    public WinningResult() {
+    public WinningResult(View view) {
 
         this.first = 0;
         this.second = 0;
         this.third = 0;
         this.fourth = 0;
         this.fifth = 0;
+
+        this.view= view;
 
     }
 
@@ -26,11 +29,11 @@ public class WinningResult {
             int cnt=0;
             cnt+=lottos.get(i).rightNumberCheck(lotto);
             cnt+=lottos.get(i).rightBonusNumberCheck(bonus_number);
-            winnigRank(cnt);
+            winningRank(cnt);
         }
     }
 
-    public void winnigRank(int cnt){
+    public void winningRank(int cnt){
         if(cnt==6) first++;
         if(cnt==15)second++;
         if(cnt==5)third++;
@@ -38,7 +41,18 @@ public class WinningResult {
         if(cnt==3)fifth++;
     }
 
-    
+    public void showWinningResult(){
+        view.showMessage("---");
+        view.showMessage("3개 일치 (5,000원) - "+fifth+"개");
+        view.showMessage("4개 일치 (50,000원) - "+fourth+"개");
+        view.showMessage("5개 일치 (1,500,000원) - "+third+"개");
+        view.showMessage("5개 일치, 보너스 볼 일치 (30,000,000원) - "+second+"개");
+        view.showMessage("6개 일치 (2,000,000,000원) - "+first+"개");
+
+        //showYield();
+    }
+
+
 
 
 
