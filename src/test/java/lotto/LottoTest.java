619187a8f3eb1ac6d4280ba197c16bb5d8fc2c16
Longstick0 <789456jang@naver.com>
2022-11-15 12:06:05 +0900

    test: @ParameterizedTest 를 통해 여러 인자들을 받아서 한번에 처리할 수 있는 테스트 구현,  로또 번호가 범위 밖의 수를 입력 받으면 예외가 발생한다.(1 ~ 45)

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,6 +3,8 @@ package lotto;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -28,6 +30,14 @@ class LottoTest {
     @Test
     void createLottoByContainsDuplicateBonusNum() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6), 1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 범위 밖의 수를 입력 받으면 예외가 발생한다.(1 ~ 45)")
+    @ParameterizedTest
+    @CsvSource({"1, 2, 3, 4, 5, -1, 7", "1, 2, 3, 4, 5, 100, 7", "1, 2, 3, 4, 5, 6, -1" , "1, 2, 3, 4, 5, 6, 100"})
+    void createLottoByContainsOutArrangeNum(int num1, int num2, int num3, int num4, int num5, int num6, int bonusNum) {
+        assertThatThrownBy(() -> new Lotto(List.of(num1, num2, num3, num4, num5, num6), bonusNum))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
