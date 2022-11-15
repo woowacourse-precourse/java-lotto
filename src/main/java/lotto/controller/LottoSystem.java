3965160ca4a6cbae35616795735d4c565557833d
yunjeong <uu0078@naver.com>
2022-11-15 21:07:24 +0900

    feat: 리스트로 로또 발행

--- src/main/java/lotto/controller/LottoSystem.java
+++ src/main/java/lotto/controller/LottoSystem.java
@@ -1,9 +1,12 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.Lotto;
 import lotto.model.MoneyParser;
 import lotto.view.Terminal;
 import lotto.view.View;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoSystem {
@@ -15,9 +18,10 @@ public class LottoSystem {
         MoneyParser moneyParser = new MoneyParser();
         int lottoCount = moneyParser.parse(view.requestMoney()) / LOTTO_PRICE;
         view.printLottoCount(lottoCount);
+        List<Lotto> lottoList = new ArrayList<>();
         for(int i = 0; i < lottoCount; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoList.add(new Lotto(numbers));
         }
-
     }
 }
