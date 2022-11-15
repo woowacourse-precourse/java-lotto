15be46896418883ad7ba91d946006d22c87770e1
xxeol2 <jhj01177@naver.com>
2022-11-15 15:48:18 +0900

    feat: 당첨 내역을 출력한다

--- src/main/java/lotto/constant/Rank.java
+++ src/main/java/lotto/constant/Rank.java
@@ -2,35 +2,48 @@ package lotto.constant;
 
 import java.util.Arrays;
 import java.util.Optional;
+import lotto.WinningMoney;
 
 public enum Rank {
 
     FIRST(6, false, 2_000_000_000),
     SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
+    THIRD(5, false, 1_500_000) {
+        public String getMessage(int count) {
+            return String.format("%d개 일치 (%s), 보너스 볼 일치 - %d개", THIRD.winningBallMatchCount,
+                    THIRD.winningMoney.toString(),
+                    count);
+        }
+    },
     FOURTH(4, false, 50_000),
     FIFTH(3, false, 5_000),
     NONE(0, false, 0);
 
     private int winningBallMatchCount;
     private boolean isBonusBallMustMatch;
-    private int money;
+    private WinningMoney winningMoney;
 
-    Rank(int winningBallMatchCount, boolean isBonusBallMustMatch, int money) {
+    Rank(int winningBallMatchCount, boolean isBonusBallMustMatch, int winningMoney) {
         this.winningBallMatchCount = winningBallMatchCount;
         this.isBonusBallMustMatch = isBonusBallMustMatch;
-        this.money = money;
+        this.winningMoney = new WinningMoney(winningMoney);
     }
 
-    public int getMoney() {
-        return money;
+    public WinningMoney getWinningMoney() {
+        return winningMoney;
+    }
+
+    public String getMessage(int count) {
+        return String.format("%d개 일치 (%s) - %d개", this.winningBallMatchCount,
+                winningMoney.toString(),
+                count);
     }
 
     public static Rank getRank(int winningBallMatchCount, boolean isBonusBallMatch) {
         Optional<Rank> rank = Arrays.stream(values())
-                .filter(r -> r.winningBallMatchCount == winningBallMatchCount && verifyBonusBall(
-                        r.isBonusBallMustMatch, isBonusBallMatch))
-                .findAny();
+                .filter(r -> r.winningBallMatchCount == winningBallMatchCount
+                        && verifyBonusBall(r.isBonusBallMustMatch, isBonusBallMatch))
+                .findFirst();
         if (rank.isEmpty()) {
             return NONE;
         }
