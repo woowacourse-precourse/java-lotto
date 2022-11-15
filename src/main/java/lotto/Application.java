190cbbf1b760a17aaf58af55122ee643bf83ffcf
yechanissm <106381504+yechanissm@users.noreply.github.com>
2022-11-15 11:22:40 +0900

    Docs: 기능목록 수정 및 정리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static java.lang.Integer.parseInt;
+
 public class Application {
+    final static String PriceMessage = "구입금액을 입력해 주세요."
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+       int price = userInputPrice(PriceMessage);
+    }
+
+    private static int userInputPrice(String priceMessage) {
+        System.out.println(priceMessage);
+        try{
+            int price = parseInt(Console.readLine());
+            return price;
+        }
+        catch(Exception e){
+            throw new IllegalArgumentException(("[ERROR] 구입금액은 "))
+        }
     }
 }
