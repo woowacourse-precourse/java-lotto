affb648b273e9c0716889fb862219824f86e45d4
김선동 <mok03127@gmail.com>
2022-11-15 16:50:06 +0900

    feat: 로또 구입 금액 1000으로 나누어 떨어지지 않는 경우 예외 처리

--- src/main/java/lotto/LottoAmount.java
+++ src/main/java/lotto/LottoAmount.java
@@ -2,12 +2,15 @@ package lotto;
 
 public class LottoAmount {
     private static final int LOTTO_MINIMUM_AMOUNT = 1000;
+    private static final int ZERO = 0;
     private static final String LESS_THAN_THOUSAND_ERROR = "[ERROR] 최소 금액은 천원이며, 천 단위 숫자만 입력 가능합니다.";
-    
+    private static final String NOT_DIVISIBLE_BY_THOUSAND_ERROR = "[ERROR] 금액은 천원 단위로 입력해야 합니다.";
+
     private final int purchaseAmount;
 
     public LottoAmount(int purchaseAmount) {
         checkMinimumAmount(purchaseAmount);
+        checkLottoAmount(purchaseAmount);
         this.purchaseAmount = purchaseAmount;
     }
 
@@ -21,4 +24,9 @@ public class LottoAmount {
         }
     }
 
+    private void checkLottoAmount(int purchaseAmount) {
+        if (purchaseAmount % LOTTO_MINIMUM_AMOUNT != ZERO) {
+            throw new IllegalArgumentException(NOT_DIVISIBLE_BY_THOUSAND_ERROR);
+        }
+    }
 }
