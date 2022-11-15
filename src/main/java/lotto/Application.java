17e2c852045dd3cafb950607265d30a2015bec2e
Cyma-s <ssol0319@naver.com>
2022-11-15 12:01:48 +0900

    feat(Application): add game start logic

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.LottoGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            LottoGame lottoGame = new LottoGame();
+            lottoGame.startLottoGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
