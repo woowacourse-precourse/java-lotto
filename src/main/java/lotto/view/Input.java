df652dc42ee1539f1522799e08e082c4f894635e
core <jinjs6@gmail.com>
2022-11-15 10:23:16 +0900

    feat(lotto): 입력 금액에 대한 검증 기능 추가
    검증 목록
    - 맨 앞자리가 0이면 에러 발생
    - 1000원 단위의 금액이 아니면 에러 발생

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -7,4 +7,18 @@ public class Input {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
+
+    private static void validatePrice(String price) throws IllegalArgumentException {
+        for (int i = 0; i < price.length(); ++i) {
+            if (!('0' <= price.charAt(i) && price.charAt(i) <= '9') ||
+                    !(i == 0 && '1' <= price.charAt(i) && price.charAt(i) <= '9')) {
+                throw new IllegalArgumentException("[ERROR] 천원 단위의 금액을 입력하셔야 합니다.");
+            }
+        }
+        if (Integer.parseInt(price) % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 천원 단위의 금액을 입력하셔야 합니다.");
+        }
+    }
+
+
 }
