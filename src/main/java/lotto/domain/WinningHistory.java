e8f45cd1be241fafa1241789606f572b9d002b97
hossang <zkzk104@naver.com>
2022-11-15 17:49:23 +0900

    refactor: 사용자가 구매한 로또 번호와 당첨 번호를 비교하기

--- src/main/java/lotto/domain/WinningHistory.java
+++ src/main/java/lotto/domain/WinningHistory.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static lotto.domain.Error.*;
 import static lotto.domain.Number.*;
 import static lotto.domain.WinningMoney.*;
 
@@ -31,41 +32,60 @@ public class WinningHistory {
     public void compareLotto(Lotto winningNumber, int bonusNumber, List<Lotto> lottos) {
         validateUnique(bonusNumber, winningNumber);
         for (Lotto lotto : lottos) {
-            Set<Integer> set = lotto.getNumbers();
-            int count = (int) winningNumber.getNumbers().stream().filter(number -> set.contains(number)).count();
-            saveWinningHistory(count, bonusNumber, set);
+            Set<Integer> numbers = lotto.getNumbers();
+            int count = (int) winningNumber.getNumbers().stream().filter(number -> numbers.contains(number)).count();
+            saveWinningHistory(count, bonusNumber, numbers);
         }
     }
 
     private void validateUnique(int bonusNumber, Lotto winningNumber) {
         if (winningNumber.getNumbers().contains(bonusNumber)) {
-            throw new IllegalArgumentException(Error.UNIQUE_NUMBER.getMessage());
+            throw new IllegalArgumentException(UNIQUE_NUMBER.getMessage());
         }
     }
 
-    private void saveWinningHistory(int count, int bonusNumber, Set<Integer> set) {
-        if (count == THREE.getNumber()) {
-            winningHistory.put(FIVE_THOUSAND, winningHistory.get(FIVE_THOUSAND) + ONE.getNumber());
-            return;
-        }
-        if (count == FOUR.getNumber()) {
-            winningHistory.put(FIFTY_THOUSAND, winningHistory.get(FIFTY_THOUSAND) + ONE.getNumber());
-            return;
-        }
+    private void saveWinningHistory(int count, int bonusNumber, Set<Integer> numbers) {
+        saveFifthPlace(count);
+        saveFourthPlace(count);
+        saveThirdPlace(count, bonusNumber, numbers);
+        saveFirstPlace(count);
+    }
+
+    private void saveThirdPlace(int count, int bonusNumber, Set<Integer> numbers) {
         if (count == FIVE.getNumber()) {
-            if (set.contains(bonusNumber)) {
-                winningHistory.put(THIRTY_MILLION, winningHistory.get(THIRTY_MILLION) + ONE.getNumber());
+            if (saveSecondPlace(bonusNumber, numbers)) {
                 return;
             }
-            winningHistory.put(ONE_MILLION_AND_A_HALF_MILLION, winningHistory.get(ONE_MILLION_AND_A_HALF_MILLION) + ONE.getNumber());
-            return;
-        }
-        if (count == SIX.getNumber()) {
-            winningHistory.put(TWO_BILLION, winningHistory.get(TWO_BILLION) + ONE.getNumber());
-            return;
+            winningHistory.put(ONE_MILLION_AND_A_HALF_MILLION,
+                    winningHistory.get(ONE_MILLION_AND_A_HALF_MILLION) + ONE.getNumber());
         }
     }
 
+    private boolean saveSecondPlace(int bonusNumber, Set<Integer> numbers) {
+        if (numbers.contains(bonusNumber)) {
+            winningHistory.put(THIRTY_MILLION, winningHistory.get(THIRTY_MILLION) + ONE.getNumber());
+            return true;
+        }
+        return false;
+    }
+
+    private void saveFirstPlace(int count) {
+        if (count == SIX.getNumber()) {
+            winningHistory.put(TWO_BILLION, winningHistory.get(TWO_BILLION) + ONE.getNumber());
+        }
+    }
+
+    private void saveFourthPlace(int count) {
+        if (count == FOUR.getNumber()) {
+            winningHistory.put(FIFTY_THOUSAND, winningHistory.get(FIFTY_THOUSAND) + ONE.getNumber());
+        }
+    }
+
+    private void saveFifthPlace(int count) {
+        if (count == THREE.getNumber()) {
+            winningHistory.put(FIVE_THOUSAND, winningHistory.get(FIVE_THOUSAND) + ONE.getNumber());
+        }
+    }
 
     public void calculateYield(int money) {
         long sum = ZERO_L;
