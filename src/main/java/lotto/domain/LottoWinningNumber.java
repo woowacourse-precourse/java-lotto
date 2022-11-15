d9a9b01f45b73dfb183a261ebdb908c1dcf47766
kbm6609 <kbm6609@nvaer.com>
2022-11-15 19:03:22 +0900

    feat<lotto> : 입력 된 당첨 번호를 알맞는 포맷으로 바꿔주는 기능 및 기능 구현간 수정이 필요한 부분 수정

--- src/main/java/lotto/domain/LottoWinningNumber.java
+++ src/main/java/lotto/domain/LottoWinningNumber.java
@@ -1,9 +1,33 @@
 package lotto.domain;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoWinningNumber {
+
+
+    public List<Integer> winningNumberConversion(String inputWinningNumber) {
+        List<Integer> winningNumber = new ArrayList<>();
+        String[] separate = separateByComma(inputWinningNumber);
+
+        validSixNumber(separate);
+        validConsistNumber(separate);
+
+        for (int i = 0; i < separate.length; i++) {
+            winningNumber.add(Integer.parseInt(separate[i]));
+        }
+        validAllNumberRange(winningNumber);
+
+        return winningNumber;
+    }
+
+    public void validSixNumber(String[] inputNumbers) {
+        if (inputNumbers.length != Lotto.numberCount) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public void validConsistNumber(String[] inputNumbers) {
         try {
             for (int i = 0; i < inputNumbers.length; i++) {
@@ -14,21 +38,14 @@ public class LottoWinningNumber {
         }
     }
 
-    public void validSixNumber(String[] inputNumbers) {
-        if (inputNumbers.length != Lotto.numberCount) {
-            throw new IllegalArgumentException();
+    public void validAllNumberRange(List<Integer> inputNumbers) {
+        for (int inputNumber : inputNumbers) {
+            validOneNumberRange(inputNumber);
         }
     }
 
-    public void validAllNumberRange(List<Integer> winningNumbers, int bonusNumber) {
-        for (int winningNumber : winningNumbers) {
-            validOneNumberRange(winningNumber);
-        }
-        validOneNumberRange(bonusNumber);
-    }
-
-    public void validOneNumberRange(int number) {
-        if (number < Lotto.numberMinRage || number > Lotto.numberMaxRage) {
+    public void validOneNumberRange(int inputNumber) {
+        if (inputNumber < Lotto.numberMinRage || inputNumber > Lotto.numberMaxRage) {
             throw new IllegalArgumentException();
         }
     }
