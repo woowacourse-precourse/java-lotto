9779da9617cf46b61e5923fe661a15c61f35318a
kite707 <kite707707@gmail.com>
2022-11-15 17:31:33 +0900

    feat(lottoList): publicians만큼 로또 생성

---
+++ src/main/java/lotto/model/LottoList.java
@@ -0,0 +1,20 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoList {
+    private List<Lotto> lottoList = new ArrayList<>();
+
+    public LottoList(int publicians) {
+        System.out.println("\n" + publicians + "개를 구매했습니다.");
+        for (int i = 0; i < publicians; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            lottoList.add(lotto);
+        }
+    }
+
+
+}
