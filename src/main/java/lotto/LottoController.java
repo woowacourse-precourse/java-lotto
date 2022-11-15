cdfefa0e670d1b847041c9fda4c7fbf07db385e8
jxixeun <jxixeun@gmail.com>
2022-11-15 19:06:20 +0900

    feat: 구입 금액만큼 로또를 생성하고 출력하는 기능 작성

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +12,23 @@ public class LottoController {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         validate(input);
+        int money = Integer.parseInt(input);
+        int count = money / 1000;
+        System.out.println(count + "개를 구매했습니다.");
+        List<Lotto> lottos = makeLotto(count);
+        for (int i=0;i<count;i++){
+            System.out.println(lottos.get(i).getNumbers());
+
+        }
+    }
+
+    public List<Lotto> makeLotto(int count){
+        List <Lotto> lottos = new ArrayList<>();
+        for (int i=0;i<count;i++){
+            Lotto lotto = new Lotto(createRandomNumbers());
+            lottos.add(lotto);
+        }
+        return lottos;
     }
 
     public boolean isNumeric(String input) {
@@ -30,5 +48,9 @@ public class LottoController {
         if (money%1000 != 0) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public List<Integer> createRandomNumbers(){
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }
