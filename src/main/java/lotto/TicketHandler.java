918f5277b40ed1eee026c89b3555fd778797d2bc
DavinAn <blueapple1030@gmail.com>
2022-11-15 23:40:04 +0900

    feat : generateTickets, add method to create new lotto tickets with random numbers

--- src/main/java/lotto/TicketHandler.java
+++ src/main/java/lotto/TicketHandler.java
@@ -1,7 +1,8 @@
 package lotto;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
 
 public class TicketHandler {
     Map<Winnings, Integer> winningTickets = new LinkedHashMap<>();
@@ -12,5 +13,17 @@ public class TicketHandler {
         this.winningTickets.put(Winnings.ThirdPlace, 0);
         this.winningTickets.put(Winnings.SecondPlace, 0);
         this.winningTickets.put(Winnings.FirstPlace, 0);
+    }
+
+    public List<Lotto> generateTickets() {
+        int count = Application.getPurchaseAmount() / 1000;
+        System.out.println(count + "개를 구매했습니다.");
+        List<Lotto> lotto = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            Collections.sort(numbers);
+            lotto.add(new Lotto(numbers));
+        }
+        return lotto;
     }
 }
