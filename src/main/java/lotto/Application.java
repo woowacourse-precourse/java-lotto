51ce591a5257dc487afbdf43884404d62dea7c4d
pyj <les85611@hanyang.ac.kr>
2022-11-15 23:28:49 +0900

    필요한 list 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.*;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
 public class Application {
+
+    private static List<Lotto> lottos = new ArrayList<>();
+    private static List<Integer> userNums = new ArrayList<>();
+    private static int buyLot;
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
-}
+    }
+
+
+
