c9fc319bd71c094232986324b9be9415e92e1d03
LeeHaiLim <hailim1997@gmail.com>
2022-11-15 11:20:32 +0900

    feat(model): add method validate to winning lotto
    Added methods:
    -validate
    -is in range
    -is unique

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -12,6 +12,23 @@ public class WinningLotto {
         this.bonusNumber = bonusNumber;
     }
 
+    private void validate(Lotto lotto, int bonusNumber) {
+        isInRange(bonusNumber);
+        isUnique(lotto, bonusNumber);
+    }
+
+    private void isInRange(int bonusNumber) {
+        if (bonusNumber < Constants.NUMBER_RANGE_START || bonusNumber > Constants.NUMBER_RANGE_END) {
+            throw new IllegalArgumentException(BonusError.RANGE.message());
+        }
+    }
+
+    private void isUnique(Lotto lotto, int bonusNumber) {
+        if (lotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException(BonusError.UNIQUE.message());
+        }
+    }
+
     public int getBonusNumber() {
         return bonusNumber;
     }
