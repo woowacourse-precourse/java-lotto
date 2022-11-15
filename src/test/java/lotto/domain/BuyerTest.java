9982801e7dde9d0ef559e58ce308961fc45149e9
Yejin Shin <shinyejin0212@gmail.com>
2022-11-15 21:09:31 +0900

    refactor(BuyerTest): 주석 제거

---
+++ src/test/java/lotto/domain/BuyerTest.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BuyerTest {
+    @DisplayName("구입 금액이 1000원 미만일대 예외가 발생한다.")
+    @Test
+    void createBuyerMoneyLessThanTousand() {
+        assertThatThrownBy(() -> new Buyer(200, 2))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구입 금액이 1000으로 나누어떨어지지 않을 때 예외가 발생한다.")
+    @Test
+    void createBuyerMoneyMultipleThousand() {
+        assertThatThrownBy(() -> new Buyer(5050, 5))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
