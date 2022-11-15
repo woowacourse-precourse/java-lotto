5b4256e342ca9aae4da59b5ebf8d6dc860713eac
anneu <b.imnuj@gmail.com>
2022-11-15 14:10:50 +0900

    refactor: 에러 메세지 출력 시 공통으로 사용되는 상수를 ConstantUtil 클래스로 분리

--- src/main/java/lotto/model/Validator.java
+++ src/main/java/lotto/model/Validator.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import static lotto.utils.ConstantUtil.ERROR;
 import static lotto.utils.ConstantUtil.LOTTO_NUMBER_SIZE;
 import static lotto.utils.ConstantUtil.MAX_LOTTO_NUMBER;
 import static lotto.utils.ConstantUtil.MIN_LOTTO_NUMBER;
@@ -9,15 +10,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Validator {
-    private static final String PREFIX = "[ERROR] ";
-    private static final String SIZE_ERROR_MESSAGE = PREFIX + "로또 번호의 개수는 6개이어야 합니다.";
-    private static final String DUPLICATES_ERROR_MESSAGE = PREFIX + "로또 번호에 중복된 숫자가 있습니다.";
-    private static final String RANGE_ERROR_MESSAGE = PREFIX + "로또 번호에 1 ~ 45 범위를 벗어난 숫자가 있습니다.";
-    private static final String DIGIT_ERROR_MESSAGE = PREFIX + "숫자만 입력 가능합니다.";
-    private static final String DIGIT_COMMA_ERROR_MESSAGE = PREFIX + "숫자와 콤마(,)만 입력 가능합니다.";
-    private static final String MIN_MONEY_ERROR_MESSAGE = PREFIX + "구입금액은 1000원 이상부터 가능합니다.";
-    private static final String MONEY_UNIT_ERROR_MESSAGE = PREFIX + "구입금액은 1000원 단위만 가능합니다.";
-    private static final String BONUS_ERROR_MESSAGE = PREFIX + "보너스 번호는 당첨 번호와 중복될 수 없습니다.";
+    private static final String SIZE_ERROR_MESSAGE = ERROR + "로또 번호의 개수는 6개이어야 합니다.";
+    private static final String DUPLICATES_ERROR_MESSAGE = ERROR + "로또 번호에 중복된 숫자가 있습니다.";
+    private static final String RANGE_ERROR_MESSAGE = ERROR + "로또 번호에 1 ~ 45 범위를 벗어난 숫자가 있습니다.";
+    private static final String DIGIT_ERROR_MESSAGE = ERROR + "숫자만 입력 가능합니다.";
+    private static final String DIGIT_COMMA_ERROR_MESSAGE = ERROR + "숫자와 콤마(,)만 입력 가능합니다.";
+    private static final String MIN_MONEY_ERROR_MESSAGE = ERROR + "구입금액은 1000원 이상부터 가능합니다.";
+    private static final String MONEY_UNIT_ERROR_MESSAGE = ERROR + "구입금액은 1000원 단위만 가능합니다.";
+    private static final String BONUS_ERROR_MESSAGE = ERROR + "보너스 번호는 당첨 번호와 중복될 수 없습니다.";
 
     public void validateNumberSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBER_SIZE) {
--- src/main/java/lotto/utils/ConstantUtil.java
+++ src/main/java/lotto/utils/ConstantUtil.java
@@ -5,6 +5,7 @@ public final class ConstantUtil {
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
     public static final int MIN_LOTTO_PRICE = 1000;
+    public static final String ERROR = "[ERROR] ";
 
     private ConstantUtil() {
     }
