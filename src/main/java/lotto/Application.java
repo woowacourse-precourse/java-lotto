8dfcccc088f9d91ac92c6886c507cd72196511f8
liarreez <liarreez@gmail.com>
2022-11-15 18:26:13 +0900

    feat(lotto): 로또 당첨을 판단하고 당첨 내역과 수익률을 출력하는 기능 구현
    fix(lotto): 잘못된 당첨 번호와 보너스 번호 입력 시 에러 메세지를 출력하도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class Application {
     public static int lottoCount = 0;
+    public static List<List<Integer>> userLotto = new ArrayList<>();
     public static List<Integer> winningNumber = new ArrayList<>();
     public static int bonusNumber = 0;
 
@@ -17,6 +18,7 @@ public class Application {
         setWinningNumbers();
         Lotto lotto = new Lotto(winningNumber);
         setBonusNumber();
+        lotto.lottoCheck(winningNumber, bonusNumber, userLotto);
     }
 
     public static void lottoBuy() {
@@ -35,6 +37,7 @@ public class Application {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Collections.sort(numbers);
             System.out.println(numbers);
+            userLotto.add(numbers);
         }
     }
 
@@ -50,6 +53,9 @@ public class Application {
         System.out.println("보너스 번호를 입력해 주세요.");
         String inputNum = Console.readLine();
         int bonusNumberCheck = Integer.parseInt(inputNum);
+        if (bonusNumberCheck < 1 || bonusNumberCheck > 45){
+            throw new IllegalArgumentException("[ERROR]보너스 번호는 1에서 45사이의 숫자를 입력해 주세요.");
+        }
         if(winningNumber.contains(bonusNumberCheck)){
             throw new IllegalArgumentException("[ERROR]당첨 번호와 중복되지 않는 보너스 번호를 입력해 주세요.");
         }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers ;
+    private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -14,12 +14,86 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR]올바른 당첨금액을 입력해주세요.");
+            throw new IllegalArgumentException("[ERROR]당첨 숫자는 6가지로 입력해 주세요.");
         }
         Set<Integer> duplicateCheckSet = new HashSet<>(numbers);
-        if (duplicateCheckSet.size() != numbers.size()){
-            throw new IllegalArgumentException("[ERROR]올바른 당첨금액을 입력해주세요.");
+        if (duplicateCheckSet.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR]당첨 숫자는 중복 없이 입력해 주세요.");
+        }
+        for (int i = 0; i < numbers.size(); i++){
+            if (numbers.get(i) < 1 || numbers.get(i) > 45) {
+                throw new IllegalArgumentException("[ERROR]당첨 숫자는 1에서 45사이의 숫자를 입력해 주세요.");
+            }
         }
         System.out.println(numbers);
+    }
+
+    public void lottoCheck(List<Integer> numbers, int bonusNumber, List<List<Integer>> userLotto) {
+        int[] prizeCount = {0,0,0,0,0,0};
+
+        for (int i = 0; i < userLotto.size(); i++) {
+            int winCount = countWin(numbers, userLotto.get(i));
+            int bonusCount = countBonus(bonusNumber,userLotto.get(i));
+            prizeCount[lottoPrice(winCount,bonusCount)]++;
+        }
+        printPrize(prizeCount);
+        printRateOfReturn(prizeCount,userLotto);
+    }
+
+    public int countWin(List<Integer> numbers, List<Integer> userLotto) {
+        int winCount = 0;
+        for (int j = 0; j < numbers.size(); j++) {
+            if (numbers.contains(userLotto.get(j))) {
+                winCount++;
+            }
+        }
+        return winCount;
+    }
+
+    public int countBonus(int bonusNumber, List<Integer> userLotto) {
+        int bounsCount = 0;
+        for (int j = 0; j < numbers.size(); j++) {
+            if (bonusNumber == userLotto.get(j)) {
+                    bounsCount++;
+            }
+        }
+        return bounsCount;
+    }
+
+    public int lottoPrice(int winCount, int bonusCount) {
+        if (winCount == 3) {
+            return 4;
+        }
+        if (winCount == 4) {
+            return 3;
+        }
+        if (winCount == 5 && bonusCount == 0) {
+            return 2;
+        }
+        if (winCount == 5 && bonusCount == 1) {
+            return 1;
+        }
+        if (winCount == 6) {
+            return 0;
+        }
+        return 5;
+    }
+
+    public void printPrize(int[] prizeCount) {
+        System.out.println("3개 일치 (5,000원) - " + prizeCount[4]+"개");
+        System.out.println("4개 일치 (50,000원) - " + prizeCount[3]+"개");
+        System.out.println("5개 일치 (1,500,000원) - " + prizeCount[2]+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + prizeCount[1]+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + prizeCount[0]+"개");
+    }
+
+    public void printRateOfReturn(int[] prizeCount, List<List<Integer>> userLotto) {
+        double rateOfRevenue = 0;
+        double invest = userLotto.size()*1000;
+        double revenue = 5000*prizeCount[4] + 50000*prizeCount[3] + 1500000*prizeCount[2] + 30000000*prizeCount[1] + 2000000000*prizeCount[0];
+        rateOfRevenue = (revenue / invest) * 100;
+        String roundRateOfRevenue = String.format("%.2f",rateOfRevenue);
+        System.out.println("총 수익률은 "+ roundRateOfRevenue +"%입니다.");
+
     }
 }
