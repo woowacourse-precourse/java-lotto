a0d6701252ef6697aef2db3b8cec5611643fcb5f
namtndus <rnrmf0721@gmail.com>
2022-11-15 17:21:58 +0900

    feat(OutputView): enum Reword 클래스와 printWinnigResult함수 구현
    로또 결과를 불러올 상수들을 저장할 Reword를 구현하고, 로또의 결과를 출력하는 printWinnigResult함수를 구현

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -3,8 +3,18 @@ package lotto.View;
 import lotto.Dto.Lotto;
 
 import java.util.List;
+import java.util.Map;
 
-import static lotto.Dto.ConstDto.InputOutputConstDto.PRINT_LOTTO_SIZE_MESSAGE;
+import static lotto.Dto.ConstDto.InputOutputConstDto.*;
+
+enum Reword {
+    FIVE_WORD("3"),FOUR_WORD("4"),THREE_WORD("5"),TWO_WORD("5BonusNumber"),ONE_WORD("6");
+    public final String word;
+
+    Reword(String word) {
+        this.word = word;
+    }
+}
 
 public class OutputView {
 
@@ -17,5 +27,14 @@ public class OutputView {
             System.out.println(lotto.toString());
         }
         System.out.println();
+    }
+    public void printWinnigResult(Map<String,Integer> winningResult){
+        System.out.println(WINNING_STATS);
+        System.out.println("---");
+        System.out.println(THREE_MATCHES+winningResult.get(Reword.FIVE_WORD.word)+DEPENDENT_NOUN);
+        System.out.println(FOUR_MATCHES+winningResult.get(Reword.FOUR_WORD.word)+DEPENDENT_NOUN);
+        System.out.println(FIVE_MATCHES+winningResult.get(Reword.THREE_WORD.word)+DEPENDENT_NOUN);
+        System.out.println(FIVE_BONUS_NUMBER_MATCHES+winningResult.get(Reword.TWO_WORD.word)+DEPENDENT_NOUN);
+        System.out.println(SIX_MATCHES+winningResult.get(Reword.ONE_WORD.word)+DEPENDENT_NOUN);
     }
 }
