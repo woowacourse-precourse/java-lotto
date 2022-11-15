1778193de207ba5c2ac74e91a29a9a0554e46767
ddukddaki <rlfrkdms1@naver.com>
2022-11-15 16:31:22 +0900

    feat(Lotto): (예외 처리)번호가 6개가 아닌지 검증하는 기능

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,18 +9,18 @@ public class Lotto {
     private static final int NUMBER_MIN_RANGE = 1;
     private static final int NUMBER_MAX_RANGE = 45;
     private static final String ERR_UNLIKE_NUMBER = "[ERROR] 당첨번호는 중복되지 않아야합니다. ";
+    private static final String ERR_WRONG_LENGTH_OF_LOTTO = "[ERROR] 로또 번호는 6개여야 합니다. ";
     private static final String ERR_WRONG_NUMBER_RANGE = "[ERROR] 당첨 번호는 1~45 숫자여야 합니다. ";
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    public void validateLengthOfLotto(List<Integer> lottoNumber) {
+        if (lottoNumber.size() != LENGTH_OF_WINNING_NUMBER) {
+            throw new IllegalArgumentException(ERR_WRONG_LENGTH_OF_LOTTO);
         }
     }
 
