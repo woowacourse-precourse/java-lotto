83f31cf90eb50d41e41bd3c69dcf17c8bad183e5
KSH <tkdgns543@gmail.com>
2022-11-15 12:05:11 +0900

    test(domain): 일반, 보너스 번호 일치 여부에 따른 액수와 열거형 반환 확인

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import lotto.domain.Buyer;
+import lotto.domain.WinningNumbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -10,7 +11,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -93,6 +96,49 @@ class ApplicationTest extends NsTest {
                     buyer.countLottos(money);
                 });
             }
+        }
+    }
+
+    @Nested
+    @DisplayName("로또 발행 확인")
+    class ReceiveLottosTest {
+        Buyer buyer = new Buyer();
+
+        @ParameterizedTest
+        @CsvSource({"32000", "1000", "15000"})
+        void receiveLottosTest(long money) {
+            List<List<Integer>> bundleOfLottos = buyer.receiveLottos(money);
+
+            assertThat(bundleOfLottos.size()).isEqualTo(money/1000);
+
+            Set<Integer> comparer = new HashSet<>();
+            for (int lottoNumber = 1; lottoNumber <= 45; lottoNumber++) {
+                comparer.add(lottoNumber);
+            }
+            for (List<Integer> singleLotto : bundleOfLottos) {
+                Set<Integer> tmp = new HashSet<>(singleLotto);
+                tmp.retainAll(comparer);
+                assertThat(tmp.size()).isEqualTo(6);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("일치에 따른 액수 계산")
+    class WinningNumbersTest {
+
+        @ParameterizedTest
+        @CsvSource({"1,true,0","1,fasle,0", "2,false,0", "3,true,5000", "3,false,5000", "4,true,50000", "4,false,50000"
+                , "5,true,30000000", "5,false,1500000", "6,true,2000000000", "6,false,2000000000"})
+        void getMatchedRewardTest(int matches, boolean bonus, long answer) {
+            assertThat(WinningNumbers.getMatchedReward(matches, bonus)).isEqualTo(answer);
+        }
+
+        @ParameterizedTest
+        @CsvSource({"1,true,NOTHING","1,fasle,NOTHING", "2,false,NOTHING", "3,true,THREE", "3,false,THREE"
+                , "4,true,FOUR", "4,false,FOUR", "5,true,FIVE_BONUS", "5,false,FIVE", "6,true,SIX", "6,false,SIX"})
+        void getEnumTest(int matches, boolean bonus, String answer) {
+            assertThat(WinningNumbers.getEnum(matches, bonus).toString()).isEqualTo(answer);
         }
     }
 }
