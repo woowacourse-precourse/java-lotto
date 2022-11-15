a31453a76088ed4573a603f79db09cb8417d132e
kimsinhyun <ksh415622@naver.com>
2022-11-15 22:07:47 +0900

    feat: (Application) 전체 어플리케이션 흐름과 관련된 클래쓰
    1. 다른 클래쓰들를 실행시키고 어플리케이션을 제어 함.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,49 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
+    public static Purchaser purchaser;
+    public static Lotto lotto;
+
+    public static void main(String[] args) throws IllegalArgumentException {
+
         // TODO: 프로그램 구현
+        try {
+            // 로또 구매 및 구매한 로또 번호들에 파트
+            purchaser = new Purchaser();
+            purchaser.setCost();
+            purchaser.receiveLottoNumbers();
+            purchaser.printLottoNums();
+
+//            // 당첨 및 보너스 번호 입력
+            List<Integer> winningNumber = enterWinnigNumber();
+            lotto = new Lotto(winningNumber);
+            lotto.setBonusNumber();
+
+            Calculation.calculateWinningDetails(purchaser, lotto);
+            purchaser.totalEarns = Calculation.calculateTotalEarns(purchaser, lotto);
+
+            Outputs.printWinningDetails();
+            Outputs.printEarningRatio(purchaser);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
     }
+
+    public static List enterWinnigNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+
+        String[] winningNumbers = Console.readLine().split(",");
+        List<Integer> lottoNum = new ArrayList<>();
+        for(int i=0; i<winningNumbers.length; ++i){
+            lottoNum.add(Integer.parseInt(winningNumbers[i]));
+        }
+
+        System.out.println();
+        return lottoNum;
+    }
+
 }
