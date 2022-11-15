9e2aa53a29d679db0e39e93560599d5192fd2adf
kim1387 <kevink0@naver.com>
2022-11-15 16:29:20 +0900

    feat: 정답 로또 입력 받는 controller 구현

---
+++ src/main/java/lotto/view/LottoNumberController.java
@@ -0,0 +1,25 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.view.vlidation.InputCorrectLottoValidator;
+
+import java.util.List;
+
+import static lotto.utils.ConvertUtil.convertInputToIntegerList;
+import static lotto.view.vlidation.InputMoneyValidator.validateBlank;
+
+public class LottoNumberController {
+
+    private LottoNumberController() {
+    }
+
+    public static List<Integer> inputLottoNumbers() {
+        System.out.println(ConsoleMessage.INPUT_CORRECT_LOTTO_MONEY);
+        String input = Console.readLine();
+        validateBlank(input);
+        List<Integer> newLottoNumber = convertInputToIntegerList(input);
+        InputCorrectLottoValidator.validate(newLottoNumber);
+        return newLottoNumber;
+    }
+
+}
