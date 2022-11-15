acaaf86703bc078383e1dda7c9eab733c58830dc
CAEL0 <kjongmin26@gmail.com>
2022-11-15 21:28:46 +0900

    test(PlayerTest): 구입금액이 자연수가 아니면 예외 발생

---
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.Application;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static lotto.constant.ExceptionConstants.NOT_NATURAL_NUMBER_EXCEPTION;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayerTest extends NsTest {
+
+    @DisplayName("구입금액이 자연수가 아니면 예외가 발생한다.")
+    @Test
+    void ThrowExceptionIfPurchaseAmountIsNotNaturalNumber() {
+        assertSimpleTest(() -> {
+            runException("a");
+            assertThat(output()).contains(NOT_NATURAL_NUMBER_EXCEPTION.toString());
+        });
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
