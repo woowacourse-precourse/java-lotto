6e4618f474935ac3d2a031ed0ac6d11fb5713388
이지영 <wldudwldhd@naver.com>
2022-11-15 19:15:26 +0900

    feat:사용자입력숫자유효성검증코드ì¶추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,8 +14,27 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자여야 합니다.");
         }
+        if (!validateRange(numbers)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (!validateDuplicates(numbers)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+        }
+    }
+    private boolean validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validateDuplicates(List<Integer> numbers) {
+        Set<Integer> notDuplicated = new HashSet<>(numbers);
+        return notDuplicated.size() == 6;
     }
 
     // TODO: 추가 기능 구현
