81fee1d3c2f9c3c4e2cff1b9492f28d37c8c3afe
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-15 13:57:24 +0900

    feat(lotto) : 구매 금액을 숫자가 아닌 입력을 하는 경우 처리

--- src/main/java/lotto/console/BuyConsole.java
+++ src/main/java/lotto/console/BuyConsole.java
@@ -7,19 +7,20 @@ import lotto.domain.WinningTable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class BuyConsole {
     public static final String INPUT_PRICE = "구입금액을 입력해 주세요.";
     public static final String PURCHASE = "개를 구매했습니다.";
     public static final String INPUT_NUMBERS = "당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS = "보너스 번호를 입력해 주세요.";
-    public static final String STATISTICS = "당첨 통계 \n ---";
-    public static final String FIRST = "6개 일치 (2,000,000,000원) - ";
-    public static final String SECOND = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
-    public static final String THIRD = "5개 일치 (1,500,000원) - ";
-    public static final String FORTH = "4개 일치 (50,000원) - ";
-    public static final String FIFTH = "3개 일치 (5,000원) - ";
-    public static final String PROFIT = "총 수익률은 %.1f입니다.";
+    public static final String STATISTICS = "당첨 통계\n---";
+    public static final String FIRST = "6개 일치 (2,000,000,000원) - %d개\n";
+    public static final String SECOND = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n";
+    public static final String THIRD = "5개 일치 (1,500,000원) - %d개\n";
+    public static final String FORTH = "4개 일치 (50,000원) - %d개\n";
+    public static final String FIFTH = "3개 일치 (5,000원) - %d개\n";
+    public static final String PROFIT = "총 수익률은 %.1f%%입니다.";
 
     public void printInputMessage() {
         System.out.println(INPUT_PRICE);
@@ -27,6 +28,11 @@ public class BuyConsole {
 
     public int inputPrice() {
         String input = Console.readLine();
+        Pattern p = Pattern.compile("^[0-9]*$");
+        if (!p.matcher(input.trim()).matches()) {
+            System.out.println("[ERROR] 숫자만 입력 가능합니다. 다시 입력하세요.");
+            return inputPrice();
+        }
         return Integer.parseInt(input);
     }
 
@@ -61,11 +67,11 @@ public class BuyConsole {
 
     public void printStatistics(Map<WinningTable, Integer> map, double profitRate) {
         System.out.println(STATISTICS);
-        System.out.println(FIFTH + map.get(WinningTable.FIFTH));
-        System.out.println(FORTH + map.get(WinningTable.FORTH));
-        System.out.println(THIRD + map.get(WinningTable.THIRD));
-        System.out.println(SECOND + map.get(WinningTable.SECOND));
-        System.out.println(FIRST + map.get(WinningTable.FIRST));
+        System.out.printf(FIFTH, map.get(WinningTable.FIFTH));
+        System.out.printf(FORTH, map.get(WinningTable.FORTH));
+        System.out.printf(THIRD, map.get(WinningTable.THIRD));
+        System.out.printf(SECOND, map.get(WinningTable.SECOND));
+        System.out.printf(FIRST, map.get(WinningTable.FIRST));
         System.out.printf(PROFIT, profitRate);
     }
 }
