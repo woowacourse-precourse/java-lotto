8aac069d3d700068060d390a53562f982a496f87
coldbucks <met.hsw@gmail.com>
2022-11-15 19:09:05 +0900

    feat(Lotto): 구매금액 출력매소드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,18 @@
 package lotto;
 
 
+import lotto.domain.LottoSystem;
+
 public class Application {
 
 
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Application T = new Application();
+
+        LottoSystem lottoSystem = new LottoSystem();
+
+        lottoSystem.lottoAmount();
 
     }
 }
