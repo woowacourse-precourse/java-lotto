940a9e46cb1808f1b586f9cfa8594923e734d705
gnoesnooj <306yyy@naver.com>
2022-11-15 11:47:45 +0800

    refactor(LottoGenerator) : 로또 정렬 기능 구현
    - 로또 정렬 기능 구현

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -4,11 +4,13 @@ import static lotto.domain.LottoValidator.validateLottoNumbers;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoGenerator {
 
     public List<Integer> generateNumbers() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+                .stream().sorted().collect(Collectors.toList());
         validateLottoNumbers(numbers);
         return numbers;
     }
