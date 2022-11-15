6811293eb978086a3f5425c1b5656fefb69617da
ohhonggi <5ghdrl@naver.com>
2022-11-15 21:52:03 +0900

    chore($WinningNumber): Add class variable & constructor

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,5 +1,15 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class WinningNumber {
+    private final List<Integer> numbers;
+    private final int bonus;
+
+    public WinningNumber(List<Integer> numbers, int bonus) {
+        this.numbers = numbers;
+        this.bonus = bonus;
+    }
+
 
 }
