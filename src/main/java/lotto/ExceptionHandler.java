e50371a77b3ff8e13c5e4624fb26c3462f42fd26
youswim <youswim96@gmail.com>
2022-11-15 20:48:28 +0900

    feat(lotto): 당첨 번호 예외상황 메세지 추가

--- src/main/java/lotto/ExceptionHandler.java
+++ src/main/java/lotto/ExceptionHandler.java
@@ -6,5 +6,9 @@ public class ExceptionHandler {
     public final static String WRONG_WIN_NUMBERS = "당첨 번호는 숫자이고, 콤마로 구분된 6자리 입니다";
     public final static String WRONG_BONUS_NUMBERS = "보너스 번호는 숫자이고, 범위는 1~45입니다";
 
+    public final static String WRONG_NUMBERS_COUNT = "당첨 번호의 갯수는 6개여야만 합니다.";
+
+    public final static String DUPLICATED_WIN_NUMBERS = "당첨 번호의 중복은 불허합니다";
+
 
 }
