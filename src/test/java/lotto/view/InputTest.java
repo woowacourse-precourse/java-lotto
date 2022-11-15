32100bfc52afe7a4fa9ce8c03507e08e7b0d70ee
ohhonggi <5ghdrl@naver.com>
2022-11-15 20:34:53 +0900

    test($InputTest): Add inputWinningNumber test
    This test is when the Winning number is not
     an appropriate value.

--- src/test/java/lotto/view/InputTest.java
+++ src/test/java/lotto/view/InputTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputTest {
@@ -19,5 +21,23 @@ class InputTest {
 
         assertEquals(Input.inputPurchasingAmount().getMoney(),
                 new Amounts(Integer.parseInt(amount)).getMoney());
+    }
+
+    @Test
+    @DisplayName("조건에 맞지 않은 로또 구입 금액을 입력받은 case.")
+    void inputWinningNumber() {
+        String winningNumber = "1,2,3,4,6,6";       // 중복 숫자가 포함
+        byte[] buf = String.join("\n", winningNumber).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+
+        assertThatThrownBy(() -> Input.inputWinningNumber())
+                .isInstanceOf(IllegalArgumentException.class);
+
+        String winningNumber2 = "1,2,3,4,5";    // 6자리 수가 아닌 경우
+        buf = String.join("\n", winningNumber2).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+
+        assertThatThrownBy(() -> Input.inputWinningNumber())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
