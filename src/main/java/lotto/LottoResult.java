57169ddefa419a1d32c0165fc7770e968dccc4e2
KimSunU <kgw6147@naver.com>
2022-11-15 22:47:02 +0900

    [feat] 로또 결과 저장 클래스 구현

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,2 +1,31 @@
-package lotto;public class LottoResult {
+package lotto;
+
+import java.util.List;
+
+public class LottoResult {
+    private long totalPrize = 0;
+    private final int[] rankCount = new int[6];
+    private static String MORE_THAN_ONE = "로또를 하나 이상 사야 합니다.";
+
+    public void update(LottoRank lottoRank){
+        rankCount[lottoRank.getRank()]++;
+        totalPrize += lottoRank.getPrize();
+    }
+
+    public void updateLotto(List<LottoRank> ranks){
+        for(LottoRank rank : ranks){
+            update(rank);
+        }
+    }
+
+    public int getNumOfRank(LottoRank rank){
+        return rankCount[rank.getRank()];
+    }
+
+    public long getPrize(){
+        if(totalPrize==0 && rankCount[0]==0){
+            throw new IllegalArgumentException(MORE_THAN_ONE);
+        }
+        return totalPrize;
+    }
 }
