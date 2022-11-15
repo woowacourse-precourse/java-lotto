8e35b9942469452c1b2b3672867dcf5f3da37cde
LisaJeoung <jlisa9912234567890@gmail.com>
2022-11-15 09:22:30 +0900

    feat : 로또 금액 입력 받기
    getPurchaseAmount 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -17,4 +19,10 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public void getPurchaseAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
+        int price = Integer.parseInt(Console.readLine());
+        if (price % 1000 != 0)
+            throw new IllegalArgumentException("[ERROR] 구입금액은 1,000원 단위로 입력해야 합니다.");
+    }
 }
