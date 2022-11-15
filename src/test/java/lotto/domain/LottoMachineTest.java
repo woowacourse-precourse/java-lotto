51227cbec9b5a84af32bbcc5862a41e156652898
junyong <jymoon1121@gmail.com>
2022-11-15 18:08:02 +0900

    test: LottoMachineTest 추가

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,15 +1,48 @@
 package lotto.domain;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.ByteArrayInputStream;
 import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoMachineTest {
 
+    @ParameterizedTest
+    @ValueSource(strings = {"1100", "100", "3500", "12001", "50"})
+    void validateInputMoney(String input) throws Exception {
+        String data = input;
+        LottoMachine lottoMachine = new LottoMachine();
+        Method validateMoney = lottoMachine.getClass().getDeclaredMethod("validateMoney", String.class);
+        validateMoney.setAccessible(true);
+
+        Assertions.assertThatThrownBy(()->
+                        validateMoney.invoke(lottoMachine,data)).getRootCause()
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void createLottoNumber() {
+    }
+
+    @Test
+    void printLottos() {
+    }
+
+    @Test
+    void getResult() {
+    }
+
+    @Test
+    void calculateRevenue() {
+    }
+
+    @Test
+    void printResult() {
+    }
 }
