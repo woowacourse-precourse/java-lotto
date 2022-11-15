680b659f42c67e43a144c69c0ee746e0db43413f
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-11-15 12:43:05 +0900

    test(generateLottos): test feature

--- src/test/java/lotto/domain/MachineTest.java
+++ src/test/java/lotto/domain/MachineTest.java
@@ -1,9 +1,11 @@
 package lotto.domain;
 
+import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,6 +22,20 @@ class MachineTest {
 
         // Then
         assertThat(numberOfLottos).isEqualTo(new BigInteger("999999999123123123"));
+    }
+
+    @Test
+    @DisplayName("구매 가능한 로또의 개수만큼 로또를 생성한다.")
+    void generateLottos() {
+        // Given
+        BigInteger purchaseNumberOfLottos = new BigInteger("10000");
+
+        // When
+        List<Lotto> lottos = Machine.generateLottos(purchaseNumberOfLottos);
+        BigInteger countOfPurchasedLottos = new BigInteger(String.valueOf(lottos.size()));
+
+        // Then
+        assertThat(countOfPurchasedLottos).isEqualTo(purchaseNumberOfLottos);
     }
 
 }
