dca54b5345cc84df5a0ca1fc95692d8b94d6b439
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-15 11:22:36 +0900

    feat(application): 의존성 주입 및 게임 진행을 위한 application 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,35 @@
 package lotto;
 
+import lotto.layer.InputView;
+import lotto.layer.LottoController;
+import lotto.layer.LottoService;
+import lotto.layer.OutputView;
+import lotto.layer.Table;
+
 public class Application {
+
+    private static final String ERROR = "[ERROR]";
+    private static LottoController lottoController;
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        dependencyInjection();
+        try {
+            lottoController.generateLotto();
+            lottoController.getStatistics();
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR + " " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println(ERROR + " " + "알 수 없는 이유로 예외 발생");
+            e.printStackTrace();
+        }
+    }
+
+    private static void dependencyInjection() {
+        Table table = new Table();
+        LottoService lottoService = new LottoService(table);
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        lottoController = new LottoController(lottoService, inputView, outputView);
     }
 }
