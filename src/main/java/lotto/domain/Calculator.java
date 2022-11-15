62da5d4188ceb60ebd3f56a2759079322dd7c2e5
3juhwan <13selfesteem91@naver.com>
2022-11-15 12:51:11 +0900

    refactor(lotto): 상수 추가

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -6,24 +6,24 @@ import java.util.List;
 public class Calculator {
     private static final int PRICE_PER_LOTTO = 1000;
     private static final int PERCENT = 100;
-
-    private static List<Integer> digit = Arrays.asList(0, 5000, 50000, 1500000, 30000000, 2000000000);
-
-    private static long getWinningAmount(List<Integer> results) {
-        long winningAmount = 0;
-        for (int i = 0; i < digit.size(); i++) {
-            winningAmount += results.get(i) * digit.get(i);
-        }
-        return winningAmount;
-    }
+    private static final int ROUND_TO_ONE_DECIMAL = 10;
+    private static final double ROUND_TO_ONE_DOUBLE = 10.0;
 
     public static double getEarningRate(List<Integer> result) {
         try {
             double numberOfLotto = result.stream().mapToInt(Integer::intValue).sum();
-            return Math.round(getWinningAmount(result) * PERCENT * 10 / numberOfLotto / PRICE_PER_LOTTO) / 10.0;
+            return Math.round(getWinningAmount(result) * PERCENT * ROUND_TO_ONE_DECIMAL / numberOfLotto / PRICE_PER_LOTTO) / ROUND_TO_ONE_DOUBLE;
         } catch (ArrayIndexOutOfBoundsException ex) {
             throw new IllegalArgumentException("[ERROR] 잘못된 결과가 전달됐습니다. ");
         }
+    }
+
+    private static double getWinningAmount(List<Integer> results) {
+        double winningAmount = 0;
+        for (int place = 1; place <= 6; place++) {
+            winningAmount += results.get(place) * MoneyUnit.getUnit(place);
+        }
+        return winningAmount;
     }
 
 }
