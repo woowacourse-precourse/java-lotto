6365f805e8aafd3061740c90a015a1b08fb6dca7
sooyoungh <sooyoung.h8@gmail.com>
2022-11-15 19:03:59 +0900

    ✨ feat(OutputView): 구매한 티켓 갯수 출력하는 기능 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,26 @@
+package lotto.view;
+public class OutputView {
+
+    enum Message {
+        COUNT_LOTTO("개를 구매했습니다."),
+        TOTAL("당첨 통계"),
+        PROFIT_PRE("총 수익률은 "),
+        PROFIT_LAST("%입니다.");
+
+        private String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+    public static void printCountOfTicket(int count) {
+        System.out.println();
+        System.out.printf( "%d", count);
+        System.out.println(Message.COUNT_LOTTO.getMessage());
+    }
+}
