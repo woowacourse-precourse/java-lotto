5779eecd775dbcff3db43f39f5583e6271f45963
이지혁 <olzlgur@naver.com>
2022-11-15 16:23:43 +0900

    feat: 랜덤 숫자 반환 메서드 구현

---
+++ src/main/java/lotto/number/LottoNumber.java
@@ -0,0 +1,24 @@
+package lotto.number;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoNumber {
+    private static final int LENGTH = 6;
+
+    public List<Integer> generateWinNumber() {
+        List<Integer> winNumber = new ArrayList<Integer>();
+        while (winNumber.size() < LENGTH) {
+            Integer randomNumber = Randoms.pickNumberInRange(1, 45);
+            if (!winNumber.contains(randomNumber)) {
+                winNumber.add(randomNumber);
+            }
+        }
+        Collections.sort(winNumber);
+
+        return winNumber;
+    }
+}
