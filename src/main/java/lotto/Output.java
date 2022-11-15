bd8f858cd06548ef970c98395b2df0e19791128d
DavinAn <blueapple1030@gmail.com>
2022-11-15 23:46:41 +0900

    feat : printWinnings, prints out result of a lotto game

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Map;
+
 public class Output {
     private final TicketHandler ticketHandler;
 
@@ -11,5 +13,15 @@ public class Output {
         for (Lotto l : Application.getLotto()) {
             System.out.println(l.getNumbers());
         }
+    }
+
+    public void printWinnings() {
+        System.out.println("당첨 통계\n---");
+        int winnings = 0;
+        for (Map.Entry<Winnings, Integer> m : Application.getWinningTickets().entrySet()) {
+            System.out.println(m.getKey().getInfo() + m.getValue() + "개");
+            winnings += (m.getKey().getAmount() * m.getValue());
+        }
+        System.out.printf("총 수익률은 %.1f%%입니다.", ticketHandler.calcProfit(winnings));
     }
 }
