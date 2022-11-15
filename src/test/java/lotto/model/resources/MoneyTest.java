080e6eaa652f98f4d6b710cb5b3595a532f7f805
yback1223 <yoyob1223@gmail.com>
2022-11-15 22:36:26 +0900

    style: Clean import part
    Closes #44

--- src/test/java/lotto/model/resources/MoneyTest.java
+++ src/test/java/lotto/model/resources/MoneyTest.java
@@ -1,9 +1,6 @@
 package lotto.model.resources;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MoneyTest {
