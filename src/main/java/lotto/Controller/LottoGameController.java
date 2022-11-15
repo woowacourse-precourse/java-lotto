8048ca0e2dca11c570aa1cb94bf5aa251ea9c67a
jhsseonn <hosu0125@gmail.com>
2022-11-15 22:57:06 +0900

    feat: 변수 추가

---
+++ src/main/java/lotto/Controller/LottoGameController.java
@@ -0,0 +1,33 @@
+package lotto.Controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Console.InputMessages;
+import lotto.Console.OutputMessages;
+import lotto.Domain.Lotto;
+import lotto.Service.LottoGameService;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGameController {
+
+    Lotto lotto;
+    LottoGameService lottoGameService = new LottoGameService();
+    public List<List<Integer>> lottoList=new ArrayList<>();
+    public List<Integer> countEqualNumberList = new ArrayList<>();
+    public String inputCash;
+
+    public void LottoSystem(InputMessages inputMessages, OutputMessages outputMessages) {
+
+    }
+
+}
+
+enum rankType {
+    FIFTH,
+    FOURTH,
+    THIRD,
+    SECOND,
+    FIRST;
+}
