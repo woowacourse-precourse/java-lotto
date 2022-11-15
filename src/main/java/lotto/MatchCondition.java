384e11ec5427c3bff6c8c27d4195fb3c578cad78
meunrud <y3a0522@gmail.com>
2022-11-15 18:42:28 +0900

    :sparkles: 보너스 숫자가 겹치는지 확인하는 기능 구현

--- src/main/java/lotto/MatchCondition.java
+++ src/main/java/lotto/MatchCondition.java
@@ -8,6 +8,18 @@ import java.util.List;
 public class MatchCondition {
 
 
+    public boolean matchBonusNumber(int bonusNumber, List<Lotto> userLottosNumber){
+        int count = 0;
+        for(int i = 0; i<userLottosNumber.size(); i++){
+            List<Integer> sample = userLottosNumber.get(i).getNumbers();
+            if(sample.contains(bonusNumber)){
+                count ++;
+            }
+        }
+        return count != 0;
+    }
+
+
     public Double calculRateOfReturn(int money, List<Integer> placeResult){
         double profit = 0;
 
@@ -16,8 +28,6 @@ public class MatchCondition {
         }
         return Math.round(profit/money *100)*1000 / 1000000.0;
     }
-}
-
 }
 
 
