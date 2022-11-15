96f38d887f6767770d57893fb4762c3281fd2f31
yeonhee <ydol2@naver.com>
2022-11-15 13:47:12 +0900

    refactor(Validator) : 에러 메시지 psfs 형식으로 바꿈

--- src/main/java/lotto/common/exception/Validator.java
+++ src/main/java/lotto/common/exception/Validator.java
@@ -12,37 +12,44 @@ public class Validator {
 
     private static Pattern LOTTO_PATTERN = Pattern.compile("^([0-9],?){5}[0-9]$");
 
+    private static final String PRICE_MUST_INT = " 구입 금액은 정수로 입력해야 합니다.";
+    private static final String PRICE_MUST_BE_DIVIDED_BY_1000 = " 구입 금액은 1,000원 단위로 입력해야 합니다.";
+    private static final String LOTTO_MUST_MATCH_PATTERN = " 로또 숫자를 형식에 맞게 입력해야 합니다.";
+    private static final String LOTTO_MUST_NOT_DUPLICATION = " 로또 숫자를 형식에 맞게 입력해야 합니다.";
+    private static final String LOTTO_MUST_RANGE_IN_1_TO_45 = " 1 ~ 45 수를 입력해야 합니다.";
+
+
     public static int validateInt(String str){
         try {
             int stringToInt = Integer.parseInt(str);
             return stringToInt;
         }catch (Exception e){
-            throw new IllegalArgumentException(" 구입 금액은 정수로 입력해야 합니다.");
+            throw new IllegalArgumentException(PRICE_MUST_INT);
         }
     }
 
     public static void validatePrice(int price){
         if (price%LOTTO_MIN_PRICE!=0 || price==0)
-            throw new IllegalArgumentException(" 구입 금액은 1,000원 단위로 입력해야 합니다.");
+            throw new IllegalArgumentException(PRICE_MUST_BE_DIVIDED_BY_1000);
     }
 
     public static void validateLottoNumbers(String lottoNumbers) {
         if (!LOTTO_PATTERN.matcher(lottoNumbers).matches()) {
-            throw new IllegalArgumentException(" 로또 숫자를 형식에 맞게 입력해야 합니다.");
+            throw new IllegalArgumentException(LOTTO_MUST_MATCH_PATTERN);
         }
     }
 
     public static void validateDifferentLottoNumbers(List<Integer> lottoNumberList){
         Integer userNumberLen = Math.toIntExact(lottoNumberList.stream().distinct().count());
         if (userNumberLen!=LOTTO_LENGTH){
-            throw new IllegalArgumentException(" 중복없는 6가지 수로 입력해야 합니다.");
+            throw new IllegalArgumentException(LOTTO_MUST_NOT_DUPLICATION);
         }
     }
 
     public static void validateRangeLottoNumbers(List<Integer> lottoNumberList){
         for (Integer number : lottoNumberList) {
             if (number>LOTTO_MAX_VALUE || number < LOTTO_MIN_VALUE)
-                throw new IllegalArgumentException(" 1 ~ 45 수를 입력해야 합니다.");
+                throw new IllegalArgumentException(LOTTO_MUST_RANGE_IN_1_TO_45);
         }
     }
 
