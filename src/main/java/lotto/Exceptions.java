f6c1373fe7bbd28cddc7ba4b434a8364ea6e23e8
minje <alswp0910@naver.com>
2022-11-15 21:04:20 +0900

    fix: 기능에 필요없는 구문 제거
    이미 던지는 throw new를 사용하면서 함수에서 또 예외를 처리하는 throw의 중복 제거

--- src/main/java/lotto/Exceptions.java
+++ src/main/java/lotto/Exceptions.java
@@ -1,23 +1,24 @@
 package lotto;
 
 public class Exceptions {
-    public void inputPriceError() throws IllegalArgumentException {
+    public void inputPriceError() {
         throw new IllegalArgumentException("[ERROR] 숫자를 입력하지 않으셨습니다.");
     }
 
-    public void notDivisionPrice() throws IllegalArgumentException {
+    public void notDivisionPrice() {
         throw new IllegalArgumentException("[ERROR] 입력 받은 금액은 1,000원으로 나눌 수 없습니다.");
     }
 
-    public void notSplitNumbers() throws IllegalArgumentException {
+    public void notSplitNumbers() {
         throw new IllegalArgumentException("[ERROR] 쉼표를 제대로 입력하지 않으셨습니다.");
     }
 
-    public void notScopeNumbers() throws IllegalArgumentException {
+    public void notScopeNumbers() {
         throw new IllegalArgumentException("[ERROR] 1~45까지의 숫자가 아닙니다.");
     }
 
-    public void duplicationNumbers() throws IllegalArgumentException {
+    public void duplicationNumbers() {
         throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자 6개를 입력하지 않으셨습니다.");
     }
 }
+
