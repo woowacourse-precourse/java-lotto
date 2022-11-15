3651af30aea728ca7b845a3f768eccb8f9eda3ea
slbin-park <smpts00@naver.com>
2022-11-15 16:15:15 +0900

    refactor(OutputView) : 당첨 통계 출력 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,6 +9,8 @@ public class OutputView {
     private static final String PRINT_LOTTO_COUNT = "개를 구매했습니다.";
     private static final String FIRST_YIELD_MESSAGE = "총 수익률은 ";
     private static final String SECOND_YIELD_MESSAGE = "%입니다.";
+    private static final String STATICS_MESSAGE = "당첨 통계";
+    private static final String MIDDLE_BAR = "---";
     private static final int START_WINNING_LIST = 3;
     private static final int END_WINNING_LIST = 8;
 
@@ -24,6 +26,8 @@ public class OutputView {
     }
 
     public void printWinningList(List<Integer> winningList) {
+        System.out.println(STATICS_MESSAGE);
+        System.out.println(MIDDLE_BAR);
         for (int i = START_WINNING_LIST; i < END_WINNING_LIST; i++) {
             System.out.println(Rank.findMoney(i).getMessage(winningList.get(i)));
         }
