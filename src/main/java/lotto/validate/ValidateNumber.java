275ab5792aa386807edc72ad2f8bf4817e955c01
이지혁 <olzlgur@naver.com>
2022-11-15 16:20:09 +0900

    feat: 입력 타입 검증 메서드 구현

--- src/main/java/lotto/validate/ValidateNumber.java
+++ src/main/java/lotto/validate/ValidateNumber.java
@@ -6,16 +6,25 @@ public class ValidateNumber {
     private static final String NOT_1000_UNIT_MESSAGE = "[ERROR] 구매 금액은 1000원 단위여야 합니다.";
     private static final Integer STANDARD_MONEY = 1000;
 
-    private void isUnderStandardMoney(Integer purchaseMoney) {
-        if( purchaseMoney < 1000 ) {
+    private void isOverStandardMoney(Integer purchaseMoney) {
+        if (purchaseMoney < 1000) {
             throw new IllegalArgumentException(UNDER_1000_MESSAGE);
         }
         return;
     }
 
-    private void isNotUnitStandardMoney(Integer purchaseMoney){
+    private void isUnitStandardMoney(Integer purchaseMoney) {
         if (purchaseMoney % STANDARD_MONEY != 0) {
             throw new IllegalArgumentException(NOT_1000_UNIT_MESSAGE);
+        }
+    }
+
+    private void isNumeric(String purchaseMoney) {
+        try {
+            Double.parseDouble(purchaseMoney);
+            return;
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_NUMERIC_MESSAGE);
         }
     }
 }
