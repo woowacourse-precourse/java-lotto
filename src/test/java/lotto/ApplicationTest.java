65c448135c3071b4f71c7e0cbb62e4728489bcf4
young2866 <62212610+young2866@users.noreply.github.com>
2022-11-15 18:19:40 +0900

    test: 당첨 번호 범위 검증 테스트

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -96,6 +96,14 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 당첨번호_범위테스트() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("1000", "0,1,2,3,4,5"));
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
