24cf310f954f9831ab926b95df0142eeaa8a25d8
ESeokhwan <huan99@hanmail.net>
2022-11-15 21:25:34 +0900

    refactor: 여러 private method 추출

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -16,13 +16,19 @@ import java.util.List;
 
 public class LottoGameController {
     private final IoManager ioManager;
+
     private final ConsoleInputView inputView;
     private final ConsoleOutputView outputView;
+
+    private final LottoGenerator lottoGenerator;
+    private final LottoStatisticsGenerator statisticsGenerator;
 
     public LottoGameController() {
         ioManager = new IoManager();
         inputView = new ConsoleInputView();
         outputView = new ConsoleOutputView();
+        lottoGenerator = new LottoGenerator();
+        statisticsGenerator = new LottoStatisticsGenerator();
     }
 
     public void runGame() {
@@ -34,14 +40,22 @@ public class LottoGameController {
     }
 
     private void runGameWithNoExceptionControl () {
+        Payment payment = getAndProcessPaymentInput();
+        List<Lotto> lottos = purchaseLottos(payment);
+        WinningNumbers winningNumbers = getAndProcessWinningNumbersInput();
+        List<LottoResult> lottoResults = checkLottos(lottos, winningNumbers);
+        LottoStatistics lottoStatistics = statisticsGenerator.generate(lottoResults);
+
+        printLottoStatistics(payment, lottoStatistics);
+    }
+
+    private Payment getAndProcessPaymentInput() {
         outputView.println(ioManager.PUT_PAYMENT_INPUT_ALERT);
         int amount = inputView.readInteger();
+        return new Payment(amount);
+    }
 
-        Payment payment = new Payment(amount);
-
-
-        LottoGenerator lottoGenerator = new LottoGenerator();
-
+    private List<Lotto> purchaseLottos(Payment payment) {
         List<Lotto> lottos = new ArrayList<>();
         outputView.println(ioManager.makePaymentAlert(payment));
         for(int i = 0; i < payment.getLottoCount(); i++) {
@@ -49,7 +63,10 @@ public class LottoGameController {
             outputView.println(tmpLotto.getNumbers());
             lottos.add(tmpLotto);
         }
+        return lottos;
+    }
 
+    private WinningNumbers getAndProcessWinningNumbersInput() {
         outputView.println(ioManager.PUT_WINNING_NUMBERS_INPUT_ALERT);
         List<Integer> numbers = inputView.readIntegers();
         Lotto winningLotto = lottoGenerator.generate(numbers);
@@ -57,8 +74,10 @@ public class LottoGameController {
         outputView.println(ioManager.PUT_BONUS_NUMBER_INPUT_ALERT);
         int bonusNumber = inputView.readInteger();
 
-        WinningNumbers winningNumbers = new WinningNumbers(winningLotto, bonusNumber);
+        return new WinningNumbers(winningLotto, bonusNumber);
+    }
 
+    private List<LottoResult> checkLottos(List<Lotto> lottos, WinningNumbers winningNumbers) {
         List<LottoResult> lottoResults = new ArrayList<>();
         for(Lotto lotto : lottos) {
             int matchCount = winningNumbers.checkMatchCount(lotto);
@@ -67,13 +86,12 @@ public class LottoGameController {
 
             lottoResults.add(lottoResult);
         }
+        return lottoResults;
+    }
 
-        LottoStatisticsGenerator statisticsGenerator = new LottoStatisticsGenerator();
-        LottoStatistics lottoStatistics = statisticsGenerator.generate(lottoResults);
-
+    private void printLottoStatistics(Payment payment, LottoStatistics lottoStatistics) {
         outputView.println(ioManager.STATISTICS_LABEL);
         outputView.println(ioManager.DIVIDER);
-
         outputView.println(ioManager.makeLottoHistoryDescription(lottoStatistics));
         outputView.println(ioManager.makeReturnRateDescription(payment, lottoStatistics));
     }
