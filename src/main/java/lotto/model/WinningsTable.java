8e4ca59ed8af267919c1c460e7995d250dc07602
gomljo <gomljo0307@naver.com>
2022-11-15 13:43:41 +0900

    feat(WinnginsTable.java): 등수에 따른 맞춘 추첨 번호의 수를 반환하는 기능 구현

--- src/main/java/lotto/model/WinningsTable.java
+++ src/main/java/lotto/model/WinningsTable.java
@@ -5,36 +5,43 @@ import java.util.Collections;
 import java.util.List;
 
 public enum WinningsTable {
-    FIRST(List.of(6,0), "2,000,000,000"),
-    SECOND(List.of(5,1),"30,000,000"),
-    THIRD(List.of(5,0),"1,500,000"),
-    FOURTH(List.of(4,0),"50,000"),
-    FIFTH(List.of(3,0), "5,000"),
+    FIRST(List.of(6, 0), "2,000,000,000"),
+    SECOND(List.of(5, 1), "30,000,000"),
+    THIRD(List.of(5, 0), "1,500,000"),
+    FOURTH(List.of(4, 0), "50,000"),
+    FIFTH(List.of(3, 0), "5,000"),
     NOTHING(Collections.EMPTY_LIST, "0");
 
-    private static final int RAFFLE_MATCHES=0;
+    private static final int RAFFLE_MATCHES = 0;
 
-    private final List<Integer>  NUMBER_OF_MATCHES;
+    private final List<Integer> NUMBER_OF_MATCHES;
     private final String REVENUE;
 
-    WinningsTable(List<Integer> numberOfMatches, String revenue){
-        this.REVENUE=revenue;
-        this.NUMBER_OF_MATCHES=numberOfMatches;
+    WinningsTable(List<Integer> numberOfMatches, String revenue) {
+        this.REVENUE = revenue;
+        this.NUMBER_OF_MATCHES = numberOfMatches;
     }
-    public static boolean isSame(WinningsTable winningsTable, String rankName){
+
+    public static boolean isSame(WinningsTable winningsTable, String rankName) {
         return rankName.equals(winningsTable.name());
     }
 
-    public static WinningsTable findByMatches(String rankName){
+    public static WinningsTable findByMatches(String rankName) {
 
         return Arrays.stream(WinningsTable.values()).
                 filter(winningsTable -> WinningsTable.isSame(winningsTable, rankName)).
                 findAny().
                 orElse(NOTHING);
     }
-    public static String getWinningsByMatchState(String rankName){
+
+    public static String getWinningsByMatchState(String rankName) {
         WinningsTable winningsTable = WinningsTable.findByMatches(rankName);
         return winningsTable.REVENUE;
     }
-    
+
+    public static int getRaffleMatchesByMatchState(String rankName) {
+        WinningsTable winningsTable = WinningsTable.findByMatches(rankName);
+        return winningsTable.NUMBER_OF_MATCHES.get(RAFFLE_MATCHES);
+    }
+
 }
