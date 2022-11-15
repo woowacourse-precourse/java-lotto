c984dc0966e84ed8e2ec984032940b107c0dc1c0
CAEL0 <kjongmin26@gmail.com>
2022-11-15 22:53:22 +0900

    refactor(InputView): static import 대체

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,25 +1,24 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-
-import static lotto.constant.OutputConstants.*;
+import lotto.constant.OutputConstants;
 
 public class InputView {
 
     public static String readPurchaseAmount() {
-        System.out.println(INPUT_PURCHASE_AMOUNT);
+        System.out.println(OutputConstants.INPUT_PURCHASE_AMOUNT);
         return Console.readLine();
     }
 
     public static String readWinningNumbers() {
         System.out.println();
-        System.out.println(INPUT_WINNING_NUMBERS);
+        System.out.println(OutputConstants.INPUT_WINNING_NUMBERS);
         return Console.readLine();
     }
 
     public static String readBonusNumber() {
         System.out.println();
-        System.out.println(INPUT_BONUS_NUMBER);
+        System.out.println(OutputConstants.INPUT_BONUS_NUMBER);
         return Console.readLine();
     }
 }
