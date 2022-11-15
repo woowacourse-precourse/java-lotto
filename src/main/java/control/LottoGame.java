2aa82f82d38cf911307bef1778671ffa946c360a
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2022-11-15 12:00:36 +0900

    feat(LottoGame): 게임 진행 control을 담당하는 클래스 구현

---
+++ src/main/java/control/LottoGame.java
@@ -0,0 +1,31 @@
+package control;
+
+import view.InputView;
+import view.OutputView;
+import domain.LottoAnalysis;
+
+
+public class LottoGame {
+
+    private final LottoAnalysis lottoAnalysis;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoGame(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoAnalysis = new LottoAnalysis();
+    }
+
+    public void playLotto() {
+
+        int purchaseQuantity = inputView.purchaseAmount();
+        lottoAnalysis.makeLottoList(purchaseQuantity);
+        outputView.printAmount(purchaseQuantity);
+        outputView.printLotto(lottoAnalysis.lottoList);
+        lottoAnalysis.countWinning(purchaseQuantity,inputView.getWinningNumbers(),inputView.getBonusNumber());
+        outputView.printTotalResult(lottoAnalysis.getTotalReward(),purchaseQuantity);
+
+    }
+
+}
