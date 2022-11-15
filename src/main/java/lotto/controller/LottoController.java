f780704a13956382894d0f3535080375358d8790
Yongwoo Choi <cyw7515@naver.com>
2022-11-15 18:43:40 +0900

    fix (LottoController): 입력 에러 발생 시, 오류 메세지 출력 후 프로그램 종료 로직 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -14,12 +14,17 @@ public class LottoController {
     private static final String DELIMITER = ",";
     private Player player;
     private List<Integer> winningNumbers;
+    private boolean endFlag = false;
 
     public LottoController() {
     }
 
     public void run() {
         getPlayerMoney();
+
+        if (endFlag) {
+            return;
+        }
 
         buyingLottoUsingAllMoney();
 
@@ -30,6 +35,76 @@ public class LottoController {
         player.saveResult(prizeList);
 
         OutputView.printTotalResult(prizeList, player.getEarningRate());
+    }
+
+    private void getPlayerMoney() {
+        String moneyInput = InputView.getMoney();
+
+        try {
+            InputValidator.validateMoney(moneyInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            endFlag = true;
+            return;
+        }
+
+        int money = Integer.parseInt(moneyInput);
+
+        player = new Player(money);
+    }
+
+    private void buyingLottoUsingAllMoney() {
+        player.buyLotto();
+
+        List<Lotto> playerLotteries = player.getLotteries();
+
+        OutputView.printBuyingCount(playerLotteries);
+    }
+
+    private void getWinningNumbers() {
+        String numberInput = InputView.getWinningNumber();
+
+        try {
+            InputValidator.validateLotteryFormat(numberInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            endFlag = true;
+            return;
+        }
+
+        winningNumbers = parse(numberInput);
+
+        String bonusInput = InputView.getBonusNumber();
+
+        try {
+            InputValidator.validateLotteryNumber(bonusInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            endFlag = true;
+            return;
+        }
+
+        winningNumbers.add(Integer.parseInt(bonusInput));
+
+        try {
+            InputValidator.validateDuplicatedLotteryNumber(winningNumbers);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            endFlag = true;
+        }
+    }
+
+    private List<Integer> parse(String numberInput) {
+        List<Integer> result = new ArrayList<>();
+
+        String[] tokens = numberInput.split(DELIMITER);
+
+        for (String token : tokens) {
+            InputValidator.validateLotteryNumber(token);
+            result.add(Integer.parseInt(token));
+        }
+
+        return result;
     }
 
     private LinkedHashMap<LottoPrize, Long> checkLotteryResult() {
@@ -58,53 +133,6 @@ public class LottoController {
         }
 
         return prizeMap;
-    }
-
-    private void getWinningNumbers() {
-        String numberInput = InputView.getWinningNumber();
-
-        InputValidator.validateLotteryFormat(numberInput);
-
-        winningNumbers = parse(numberInput);
-
-        String bonusInput = InputView.getBonusNumber();
-
-        InputValidator.validateLotteryNumber(bonusInput);
-
-        winningNumbers.add(Integer.parseInt(bonusInput));
-
-        InputValidator.validateDuplicatedLotteryNumber(winningNumbers);
-    }
-
-    private void getPlayerMoney() {
-        String moneyInput = InputView.getMoney();
-
-        InputValidator.validateMoney(moneyInput);
-
-        int money = Integer.parseInt(moneyInput);
-
-        player = new Player(money);
-    }
-
-    private void buyingLottoUsingAllMoney() {
-        player.buyLotto();
-
-        List<Lotto> playerLotteries = player.getLotteries();
-
-        OutputView.printBuyingCount(playerLotteries);
-    }
-
-    private List<Integer> parse(String numberInput) {
-        List<Integer> result = new ArrayList<>();
-
-        String[] tokens = numberInput.split(DELIMITER);
-
-        for (String token : tokens) {
-            InputValidator.validateLotteryNumber(token);
-            result.add(Integer.parseInt(token));
-        }
-
-        return result;
     }
 
 }
