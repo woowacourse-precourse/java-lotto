462428153b71452b7d0861dafb4a4a667f22a18f
HAERYN <syt06119@naver.com>
2022-11-15 18:16:24 +0900

    feat: 보너스 번호 출력 기능

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -3,8 +3,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoManager {
-    public void lottoManager(List<Integer> numbers){
+    public void lottoManager(List<Integer> numbers, List<Integer> mynumbers){
         Collections.sort(numbers);
+        Collections.sort(mynumbers);
 
     }
 }
