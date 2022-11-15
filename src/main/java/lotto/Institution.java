d869d96ff34f7159e8f8a2f94628c6c30d9c765d
devMuscle <hongsbien@naver.com>
2022-11-15 12:51:35 +0900

    feat: 로또 추첨기관을 담당하는 Class 생성

---
+++ src/main/java/lotto/Institution.java
@@ -0,0 +1,60 @@
+package lotto;
+
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Institution {
+
+    private static Institution institution = new Institution();
+
+    private Institution() {};
+
+    public static Institution getInstance() {
+        return institution;
+    }
+
+    public Integer countPrizeAndLottoDuplicatedNumber(List<Integer> numbers, List<Integer> winningNumbers) {
+        List<Integer> nonDuplicatedNumbers = numbers.stream()
+                .filter(nums-> winningNumbers.stream().anyMatch(Predicate.isEqual(nums)))
+                .collect(Collectors.toList());
+
+        Integer count = nonDuplicatedNumbers.size();
+
+        return count;
+    }
+
+    public Boolean isBonusNumberIncluded(List<Integer> numbers, Integer bonusNumber) {
+        if(numbers.contains(bonusNumber)) {
+            return Boolean.TRUE;
+        }
+
+        return Boolean.FALSE;
+    }
+
+    public void updatePrizeWinnerCount(List<PrizeWinner> prizeWinners, Integer count, Boolean bonusNumberIncluded) {
+        if(count==6) {
+            prizeWinners.get(prizeWinners.indexOf(PrizeWinner.FIRSTPLACE)).addCount();
+        }else if(count==5 && bonusNumberIncluded==true) {
+            prizeWinners.get(prizeWinners.indexOf(PrizeWinner.SECONDPLACE)).addCount();
+        }else if(count==5 && bonusNumberIncluded==false) {
+            prizeWinners.get(prizeWinners.indexOf(PrizeWinner.THIRDPLACE)).addCount();
+        }else if(count==4) {
+            prizeWinners.get(prizeWinners.indexOf(PrizeWinner.FOURTHPLACE)).addCount();
+        }else if(count==3) {
+            prizeWinners.get(prizeWinners.indexOf(PrizeWinner.FIFTHPLACE)).addCount();
+        }
+    }
+
+    public void confirmWinner(List<PrizeWinner> prizeWinners, User user, Prize prize) {
+        List<Integer> winningNumbers = prize.getNumbers();
+
+        for(Lotto lotto : user.getLottos()) {
+            Integer count = countPrizeAndLottoDuplicatedNumber(lotto.getNumbers(), winningNumbers);
+            Boolean bonusNumberIncluded = isBonusNumberIncluded(lotto.getNumbers(), prize.getBonusNumber());
+
+            updatePrizeWinnerCount(prizeWinners, count, bonusNumberIncluded);
+        }
+    }
+
+}
