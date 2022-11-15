7bdca116baf9641fd290e2f00bd3699daba9d206
kite707 <kite707707@gmail.com>
2022-11-15 14:36:08 +0900

    feat(user): 구입금액 입력받고 1000으로 나눠떨어지지 않으면 예외처리

--- src/main/java/lotto/model/User.java
+++ src/main/java/lotto/model/User.java
@@ -1,4 +1,13 @@
 package lotto.model;
-
+import camp.nextstep.edu.missionutils.Console;
 public class User {
+    public void getBudget(){
+        System.out.println("구입금액을 입력해 주세요.");
+        int budget = Integer.parseInt(Console.readLine());
+        if(budget%1000!=0){
+            throw new IllegalArgumentException("구입금액은 1000원 단위입니다!");
+        }
+
+    }
+
 }
