a3ba1baa5714fec5e7435761580561381cbe740f
heejeong <hjx66@naver.com>
2022-11-15 20:54:51 +0900

    feat:  main문에서 Exception 처리, 예외발생 시 에러 문구 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,8 +4,12 @@ import lotto.domain.LottoSystem;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         LottoSystem lottoSystem = new LottoSystem();
-        lottoSystem.run();
+        try{
+            lottoSystem.run();
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
