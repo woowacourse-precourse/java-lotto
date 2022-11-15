616456c2c3c9a09e79c47ebffee56ed2dc92e085
ParkJoungWoo <jug78@naver.com>
2022-11-15 10:22:35 +0900

    fix(user/User.java) : User 클래스 수정
    User클래스 출력 책임 전가
    
    - User 클래스의 수익 출력 책임을 View로 옮겼습니다.
    - User 클래스의 로또 중복 확인 기능 추가

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,19 +1,22 @@
 package user;
 
 import lotto.Lotto;
-import ui.View;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
 
     private final int BASE_MONEY = 1000;
+    private final int ONE_HUNDRED_PERCENT = 100;
+    private final boolean HAVE = true;
     private List<Lotto> lottos;
     private int seedMoney;
     private int prizeMoney;
 
     public User() {
-        seedMoney = NumberComponent.makeMoney();
+        lottos = new ArrayList<>();
+        seedMoney = NumberValidator.makeMoney();
         prizeMoney = 0;
     }
 
@@ -21,29 +24,26 @@ public class User {
         return seedMoney / BASE_MONEY;
     }
 
-    public void setLottos(List<Lotto> lottos) {
-        this.lottos = lottos;
+    public void addLottos(Lotto lotto) {
+        if(this.hasLotto(lotto) != HAVE){
+            lottos.add(lotto);
+        }
     }
 
     public void setPrizeMoney(int prize) {
-        NumberComponent.checkMoney(prize);
+        NumberValidator.checkMoneyThousand(prize);
         prizeMoney = prize;
     }
 
     public String getRatio() {
         double prize = (double) prizeMoney;
         double seed = (double) seedMoney;
-        return String.format("%.1f", prize / seed * 100);
+        return String.format("%.1f", prize / seed * ONE_HUNDRED_PERCENT);
     }
 
-    public void printPurchaseResult() {
-        View.printPurchaseMount(this);
-        View.printLottoList(this);
+    public boolean hasLotto(Lotto lotto){
+        return lottos.contains(lotto);
     }
-    public void printRatio(){
-        View.printTotalPrize(this);
-    }
-
     public List<Lotto> getLottos() {
         return lottos;
     }
