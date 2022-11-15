30ec75c58943a521896012dcee627f1d420f12e8
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-15 21:30:14 +0900

    feat(Player): 구매 금액이 0인 경우 오류 처리

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -1,29 +1,37 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Player {
-    private final int payment;
+    private final long payment;
+    private final long lottoCount;
+    private final List<List<Integer>> lottoPapers;
+    final long WON_UNIT = 1000L;
 
-
-
-    private final int lottoCount;
-    final int WON_UNIT = 1000;
-
-    public Player(int money) {
+    public Player(long money) {
         validateWonUnit(money);
         this.payment = money;
         this.lottoCount = money / WON_UNIT;
+        this.lottoPapers = LottoStore.saleLotto(lottoCount);
     }
 
-    private void validateWonUnit(int money) {
-        final String WON_UNIT_ERROR_MESSAGE = "[ERROR] 구입 금액은 1,000원 단위만 가능합니다.";
+    private void validateWonUnit(long money) {
+        String WON_UNIT_ERROR_MESSAGE = "[ERROR] 구입 금액은 1,000원 단위만 가능합니다.";
 
-        if (money % WON_UNIT != 0) {
+        if (money % WON_UNIT != 0 || money == 0) {
             throw new IllegalArgumentException(WON_UNIT_ERROR_MESSAGE);
         }
     }
 
-    public int getLottoCount() {
+    public long getPayment() {
+        return payment;
+    }
+
+    public long getLottoCount() {
         return lottoCount;
     }
 
+    public List<List<Integer>> getLottoPapers() {
+        return lottoPapers;
+    }
 }
