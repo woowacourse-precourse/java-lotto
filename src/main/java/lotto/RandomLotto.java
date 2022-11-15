d68b81f4b5b166a7613ab6c300a6d2d31a9554dc
KimSunU <kgw6147@naver.com>
2022-11-15 22:44:17 +0900

    [feat] 랜던 로또 생성 구현

--- src/main/java/lotto/RandomLotto.java
+++ src/main/java/lotto/RandomLotto.java
@@ -1,2 +1,15 @@
-package lotto;public class RandomLotto {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RandomLotto {
+    private static int MIN_NUMBER = 1;
+    private static int MAX_NUMBER = 45;
+    private static int PICK_NUMBER = 6;
+
+    public static Lotto randomLotto(){
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, PICK_NUMBER);
+        return new Lotto(randomNumbers);
+    }
 }
