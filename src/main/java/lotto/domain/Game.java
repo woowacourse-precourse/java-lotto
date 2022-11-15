b81e6913e3d0e199a1a01f0b81f9b72cb5fb6b88
starwook <star.wookk@gmail.com>
2022-11-15 15:27:19 +0900

    feat(Game.java): Add CheckIfDivideWithThousand.java
    구입금액이 1000원 단위인지 확인하는 메소드 추가

--- src/main/java/lotto/domain/Game.java
+++ src/main/java/lotto/domain/Game.java
@@ -1,5 +1,15 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
+
+    public static final String notThousand =" 구입금액은 천원 단위여야합니다.";
+
+    public void checkIfDivideWithThousand(int purchase){
+        if(purchase%1000 !=0){
+            throw new IllegalArgumentException(ErrorResource.errorStart+notThousand);
+        }
+    }
 
 }
