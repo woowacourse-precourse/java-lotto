a692e15ba1095fc3a697857e63c70f34f3dac9b9
Haaae <101785628+Haaae@users.noreply.github.com>
2022-11-15 16:44:41 +0900

    refactor(Output): 당첨 통계 출력 기능 추가

--- src/main/java/lotto/UI/Output.java
+++ src/main/java/lotto/UI/Output.java
@@ -14,15 +14,21 @@ public class Output {
         System.out.println();
     }
 
-    public static void printResult(String resultPrintFormat) {
+    public static void printWinningStatistics(String resultPrintFormat, float earningRate) {
+        printResult(resultPrintFormat);
+        printEarningRate(earningRate);
+    }
+
+    private static void printResult(String resultPrintFormat) {
         System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---");
 
-        System.out.println(resultPrintFormat);
+        System.out.printf(resultPrintFormat);
     }
 
-    public static void printEarningRate(float earningRate) {
+    private static void printEarningRate(float earningRate) {
         System.out.printf("총 수익률은 %.1f%%입니다.\n", earningRate);
+        System.out.println();
     }
 }
