bd75f729d476f591b9aea817a64f5835d4b8a57c
inhyeok <ss112d86@gmail.com>
2022-11-15 10:48:04 +0900

    feat: 입력한 금액만큼 로또 생성 기능 구현

---
+++ src/main/java/lotto/domain/LottoService.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoService {
+
+  public List<Lotto> buy(int money) {
+    int lottoCount = payPrice(money);
+    return generateLotto(lottoCount);
+  }
+
+  private int payPrice(int money) {
+    if (money % 1000 != 0) {
+      System.out.println("[ERROR] 돈 단위가 맞지 않습니다.");
+      throw new IllegalArgumentException();
+    }
+    return money / 1000;
+  }
+
+  private List<Lotto> generateLotto(int count){
+    List<Lotto> lottos = new ArrayList<>();
+    for(int i = 0; i < count; i++){
+      List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+      lottos.add(new Lotto(numbers));
+    }
+    return lottos;
+  }
+}
---
+++ src/test/java/lotto/domain/LottoServiceTest.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LottoServiceTest {
+
+  private LottoService lottoService = new LottoService();
+
+  @Test
+  void 로또구매시_입력한금액이2030일경우_단위가맞지않아_실패() throws Exception {
+    int givenMoney = 2030;
+
+    assertThatThrownBy(() -> lottoService.buy(givenMoney))
+        .isInstanceOf(IllegalArgumentException.class);
+  }
+
+
+  @Test
+  void 정상적으로_구매시_로또_리스트가_리턴된다(){
+    int givenMoney = 2000;
+
+    List<Lotto> result = lottoService.buy(givenMoney);
+
+    assertThat(result.size()).isEqualTo(2);
+  }
+}
