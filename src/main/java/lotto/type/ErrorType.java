95834648be581bb6bc5a7900c7138d5e1afa061e
win9 <krrr8@naver.com>
2022-11-15 23:11:42 +0900

    Refactor(Type): 오류 문구 수정
    사용자가 쉼표가 아닌 다른 것으로 당첨번호를 구분하려고 할 시
    오류를 좀 더 명확히 표현하도록 한다.

--- src/main/java/lotto/type/ErrorType.java
+++ src/main/java/lotto/type/ErrorType.java
@@ -2,7 +2,7 @@ package lotto.type;
 
 public enum ErrorType {
     INVALID_PRICE_INPUT("[ERROR] 구입 금액은 1000원 으로 나누어 떨어져야 합니다."),
-    INVALID_NUMBER_DIGITS("[ERROR] 6자리의 숫자가 생성되어야 합니다."),
+    INVALID_NUMBER_DIGITS("[ERROR] 쉼표만으로 구분한 6자리 숫자를 입력해야 합니다."),
     INVALID_NUMBER_RANGE("[ERROR] 숫자는 1~45 사이의 숫자여야 합니다."),
     INVALID_DUPLICATED_NUMBER("[ERROR] 번호는 중복이될 수 없습니다"),
     INVALID_INPUT_TYPE("[ERROR] 숫자만 입력하여야 합니다.");
