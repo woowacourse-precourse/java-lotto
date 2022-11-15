08364d7b267cee22eaecc01f0a21d479690c4f84
yujeongkwon <yoojeongkwon@naver.com>
2022-11-15 22:53:40 +0900

    Implement(): 프로그램 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import lotto.controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+        try {
+            lottoController.buyLotto();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        lottoController.confirmPurchase();
+        lottoController.setLuckyNumber();
+        lottoController.checkWinningStatistics();
+
     }
+
 }
