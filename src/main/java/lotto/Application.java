7161b484a0df6e4d3b8331b363dd7807591f910f
limecats0331 <limecats0331@gmail.com>
2022-11-15 20:02:49 +0900

    feat(Application): 메인 함수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,14 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoUI lottoProgram = new LottoUI(new Manager());
+
+        lottoProgram.setPurchasePrice();
+        lottoProgram.publishLottos();
+
+        lottoProgram.getLottoNumber();
+        lottoProgram.setBonusNumber();
+
+        lottoProgram.getLottoResult();
     }
 }
