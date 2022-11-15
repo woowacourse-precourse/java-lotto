ecea569b0879a095d39312d89b7f61e7914c6875
sooyoungh <sooyoung.h8@gmail.com>
2022-11-15 18:28:46 +0900

    ✨ feat(LottoNumberFormatValidator): 입력 번호가 범위(1~45)내에 해당되는 지 검증하는 기능 구현

--- src/main/java/lotto/util/LottoNumberFormatValidator.java
+++ src/main/java/lotto/util/LottoNumberFormatValidator.java
@@ -1,6 +1,8 @@
 package lotto.util;
 
 public class LottoNumberFormatValidator {
+    public static int LOTTO_NUMBER_MIN = 1;
+    public static int LOTTO_NUMBER_MAX = 45;
 
     public static int validateInteger(String inputMoney) {
         try{
@@ -8,6 +10,12 @@ public class LottoNumberFormatValidator {
         }
         catch (NumberFormatException e){
             throw new IllegalArgumentException("입력값이 숫자 형식이어야 합니다.");
+        }
+    }
+
+    public static void validateLottoNumberFormat(Integer number) {
+        if ( number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX) {
+            throw new IllegalArgumentException("로또 번호는 "+ LOTTO_NUMBER_MIN +"이상 " + LOTTO_NUMBER_MAX + "이하 이어야 합니다.");
         }
     }
 }
