c53beb4584f973256f697fd9e11b10ed7df5f8e7
limecats0331 <limecats0331@gmail.com>
2022-11-15 10:28:43 +0900

    test(LottoIO): 입력 메서드 테스트
    테스트 케이스
    - 정상입력
    - 문자가 입력되었을 때
    - 실수가 입력되었을 때

---
+++ src/test/java/lotto/LottoIOTest.java
@@ -0,0 +1,47 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class LottoIOTest {
+
+    @DisplayName("올바른 입력이 들어왔을 때")
+    @Test
+    void rightInput() throws Exception {
+        //Given
+        System.setIn(new ByteArrayInputStream("1,2,3,4,5,6".getBytes()));
+
+        //When
+        List<Integer> lottoNumber = LottoIO.getLottoNumber();
+
+        //Then
+        assertThat(lottoNumber).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @DisplayName("문작 입력되었을 떄")
+    @Test
+    void stringInput() throws Exception {
+        //Given
+        System.setIn(new ByteArrayInputStream("1,a,3,4,5,6".getBytes()));
+
+        //When
+        assertThatThrownBy(() -> LottoIO.getLottoNumber())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("실수가 입력됬을때")
+    @Test
+    void floatInput() throws Exception {
+        //Given
+        System.setIn(new ByteArrayInputStream("1,1.1,3,4,5,6".getBytes()));
+
+        //When
+        assertThatThrownBy(() -> LottoIO.getLottoNumber())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
