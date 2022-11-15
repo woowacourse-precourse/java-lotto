f767a03703647d2191e6a8029d8637924494deb1
Ridealist <cjsn92@gmail.com>
2022-11-15 20:16:51 +0900

    fix(SummaryView): 수익률 세 자리 마다 콤마 구분 없는 오류 수정

--- src/main/java/lotto/view/output/SummaryView.java
+++ src/main/java/lotto/view/output/SummaryView.java
@@ -9,7 +9,7 @@ public class SummaryView {
     private static final String UNDER_BAR = "----%n";
     private static final String GENERIC_SUMMARY_MESSAGE = "%d개 일치 (%s원) - %d개" + "%n";
     private static final String SECOND_PRIZE_SUMMARY_MESSAGE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개" + "%n";
-    private static final String EARNING_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
+    private static final String EARNING_RATE_MESSAGE = "총 수익률은 %,.1f%%입니다.";
 
     public static void showResult(Result result) {
         System.out.printf(START_MESSAGE + UNDER_BAR);
@@ -34,7 +34,7 @@ public class SummaryView {
         for (Prize e : Prize.values()) {
             prizeMoney += (e.getReward() * result.getPrizeCount().get(e));
         }
-        float f = (prizeMoney / (amount * 1_000)) * 100;
-        System.out.printf(EARNING_RATE_MESSAGE, f);
+        float earningRate = (prizeMoney / (amount * 1_000)) * 100;
+        System.out.printf(EARNING_RATE_MESSAGE, earningRate);
     }
 }
