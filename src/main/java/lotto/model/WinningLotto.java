c4bbdfac6d2d3f4fe76bc06e47d398ab60b76800
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-15 11:13:32 +0900

    feat: 당첨 로또 번호를 담는 Model 객체 만들기

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,2 +1,22 @@
-package lotto.model;public class WinningLotto {
+package lotto.model;
+
+import java.util.List;
+
+public class WinningLotto {
+
+    private final List<Integer> winningLottoNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> winningLottoNumbers, int bonusNumber) {
+        this.winningLottoNumbers = winningLottoNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> getWinningLottoNumbers() {
+        return winningLottoNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
