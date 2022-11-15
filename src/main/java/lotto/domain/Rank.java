8e9006a593d71171ce8fbe4feb0491163a3abc47
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-15 09:43:40 +0900

    feat(Rank): add prize value to Rank

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,5 +1,19 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST_SIX_MATCHED, SECOND_FIVE_WITH_BONUS, THIRD_FIVE_MATCHED, FOURTH_FOUR_MATCHED, FIFTH_THREE_MATCHED, NONE
+    FIRST_SIX_MATCHED(2_000_000_000),
+    SECOND_FIVE_WITH_BONUS(30_000_000),
+    THIRD_FIVE_MATCHED(1_500_000),
+    FOURTH_FOUR_MATCHED(50_000),
+    FIFTH_THREE_MATCHED(5_000);
+
+    private final int prize;
+
+    Rank(int prize) {
+        this.prize = prize;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
 }
