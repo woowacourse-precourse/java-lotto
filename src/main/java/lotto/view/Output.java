71bb72480e3267030923ea1d3e92c549c083e39d
KyungKyu Park <rudrb0505@gmail.com>
2022-11-15 17:39:08 +0900

    refactor(Output): 출력 기능에 따라 메소드로 분리하여 정리

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -31,16 +31,25 @@ public class Output {
 				continue;
 			}
 			if (rank == Rank.SECOND) {
-				System.out.println(
-					rank.getMatchCount() + STATISTICS_WINNING_COUNT + STATISTICS_WINNING_BONUS + formatter.format(
-						rank.getMoney()) + STATISTICS_WINNING_MONEY
-						+ result.get(rank) + STATISTICS_WINNING_COUNT_MONEY);
+				printSecondStatistics(result, rank);
 				continue;
 			}
-			System.out.println(
-				rank.getMatchCount() + STATISTICS_WINNING_COUNT_WITH_BRACKET + formatter.format(rank.getMoney())
-					+ STATISTICS_WINNING_MONEY + result.get(rank) + STATISTICS_WINNING_COUNT_MONEY);
+			printStatistics(result, rank);
 		}
+	}
+
+	private static void printSecondStatistics (EnumMap<Rank, Integer> result, Rank rank) {
+		System.out.println(
+			rank.getMatchCount() + STATISTICS_WINNING_COUNT + STATISTICS_WINNING_BONUS
+				+ formatter.format(rank.getMoney()) + STATISTICS_WINNING_MONEY
+				+ result.get(rank) + STATISTICS_WINNING_COUNT_MONEY);
+	}
+
+	private static void printStatistics (EnumMap<Rank, Integer> result, Rank rank) {
+		System.out.println(
+			rank.getMatchCount() + STATISTICS_WINNING_COUNT_WITH_BRACKET
+				+ formatter.format(rank.getMoney()) + STATISTICS_WINNING_MONEY
+				+ result.get(rank) + STATISTICS_WINNING_COUNT_MONEY);
 	}
 
 	public static void printEarningRate(int paidMoney, Long earningMoney) {
