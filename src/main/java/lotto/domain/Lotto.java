64054df2c05de3b580304825b9ddf1894b82f515
seo <fungod19@gmail.com>
2022-11-15 23:05:18 +0900

    fix: 로또 유효성 검사

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,55 +1,32 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Console;
-
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import lotto.Utils;
 
 public class Lotto {
-    private List<Integer> numbers;
+    private final List<Integer> numbers;
 
-    public void playGame() {
-        System.out.println("당첨번를 입력해 주세요.");
-        String userInput = Console.readLine();
-        this.numbers = makeList(userInput);
-        validate(numbers);
-
-        System.out.println("보너스 번호를 입력해 주세요.");
-        Integer bonusInput = Integer.parseInt(Console.readLine());
-        numbers.add(bonusInput);
-        checkNum(numbers);
-
-    }
-
-    //사용자 입력받은거 리스트로 바꾸기
-    public List<Integer> makeList(String userInput) {
-        String[] arrayInput = userInput.split(",");
-        List<Integer> userNumbers = new ArrayList<>();
-        for (int i = 0; i < userInput.length(); i++) {
-            userNumbers.add(Integer.parseInt(arrayInput[i]));
-        }
-        return userNumbers;
-    }
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        Utils.validateLottoNumbers(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    public String toStringLotto() {
+        StringBuilder lotto = new StringBuilder("[");
+        for (int n : this.numbers) {
+            lotto.append(String.valueOf(n)).append(", ");
         }
-        if(numbers.size() != numbers.stream().distinct().count()){
-            throw new IllegalArgumentException();
-        }
-
+        lotto = new StringBuilder(lotto.substring(0, lotto.length() - 2) + "]");
+        return lotto.toString();
     }
 
-    private void checkNum(List<Integer> numbers){
-        // TODO: 중복
-        if(numbers.size() != numbers.stream().distinct().count()){
-            throw new IllegalArgumentException();
-        }
+    public int getMatchNumberCount(List<Integer> winning) {
+        return (int) numbers.stream().filter(winning::contains).count();
     }
 
+    public boolean haveBonusNumber(int bonusNumber) {
+        return numbers.contains(bonusNumber);
+    }
 }
