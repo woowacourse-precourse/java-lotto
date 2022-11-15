cf251a7e32e7bac6ccfb26654bbe59a29e71b6cc
bfly_notebook <kkyxw@soongsil.ac.kr>
2022-11-15 21:49:45 +0900

    refactor: revise the code in valueOf() to delete the reserved word, 'else if'
    - use the stream() method
    - make the branching when 'winningNumberCount' is 5, and consider whether 'bonusWinning' is true

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,6 +1,6 @@
 package lotto.domain;
 
-import lotto.validator.ErrorConstants;
+import java.util.Arrays;
 
 public enum Prize {
 	FIRST(6, 2_000_000_000),
@@ -23,21 +23,17 @@ public enum Prize {
 	}
 
 	public static Prize valueOf(int winningNumberCount, boolean bonusWinning) {
-		if (winningNumberCount == 6) {
-			return FIRST;
-		} else if (winningNumberCount == 5 && bonusWinning == true) {
-			return SECOND;
-		} else if (winningNumberCount == 5 && bonusWinning == false) {
-			return THIRD;
-		} else if (winningNumberCount == 4) {
-			return FOURTH;
-		} else if (winningNumberCount == 3) {
-			return FIFTH;
-		} else if (winningNumberCount < 3) {
-			return NONE;
+		if (winningNumberCount == 5) {
+			if (bonusWinning == true) {
+				return Prize.SECOND;
+			}
+
+			return Prize.THIRD;
 		}
 
-		throw new IllegalArgumentException(ErrorConstants.ERROR_WINNING_NUMBER_COUNT);
+		return Arrays.stream(Prize.values())
+				.filter(prize -> prize.winningNumberCount == winningNumberCount)
+				.findAny()
+				.orElse(NONE);
 	}
-
 }
