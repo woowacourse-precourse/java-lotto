7a8a587f7c63d2068237b411842aadefffca6821
고동우 <shp05179@gmail.com>
2022-11-15 17:41:30 +0900

    feat: 수익률계산- 일치하는 숫자 갯수 세기

--- src/main/java/lotto/Consumer.java
+++ src/main/java/lotto/Consumer.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Consumer {
@@ -21,4 +22,22 @@ public class Consumer {
         }
         LottoConsoleIo.printNoOfLotto(lottos);
     }
+    public static List<Integer> calcStat(Lotto winNumber, int bonus) {
+        List<Integer> stat = List.of(0,0,0,0,0,0);
+        HashSet winNumbers = new HashSet(winNumber.getNumbers());
+        for (Lotto lotto:lottos) {
+            HashSet<Integer> myNumbers = new HashSet(lotto.getNumbers());
+            myNumbers.retainAll(winNumbers);
+            if (myNumbers.size()<3){
+                continue;
+            }
+            if (myNumbers.size()==5 && winNumbers.contains(bonus)) {
+                stat.set(4, stat.get(4)+1);
+                continue;
+            }
+            stat.set(myNumbers.size()-3, stat.get(myNumbers.size()-3)+1);
+        }
+        return stat;
+    }
+
 }
