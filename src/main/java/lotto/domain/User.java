2fb4c5194a761c544887bab38d6e9d3d88a335f7
junyong <jymoon1121@gmail.com>
2022-11-15 17:07:56 +0900

    refactor: 당첨 번호와 보너스 번호 유효성 검증 에러 수정

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 public class User {
     private List<Lotto> lottos = new ArrayList<>();
-    private List<Integer> winningNumber;
+    private List<Integer> winningNumber = new ArrayList<>();
     private Integer bonusNumber;
 
     public User(List<Lotto> lottos) {
@@ -44,22 +44,25 @@ public class User {
         }
         for(int i = 0; i < numbers.size(); i++) {
             String num = numbers.get(i);
-            if(num.charAt(i) < 1 || num.charAt(i) > 45) {
+            if(num.matches("[^0-9]")) {
+                throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다. ");
+            }
+            int bonusNum = Integer.parseInt(num);
+            if(bonusNum < 1 || bonusNum > 45) {
                 throw new IllegalArgumentException("[ERROR] 1 ~ 45 숫자만 입력 가능합니다.");
             }
         }
     }
 
     private void validateBonusNum(String num) {
-        if(num.length() != 1) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 한개 입력해야 합니다. ");
-        }
-
         if(winningNumber.contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호와 보너스 번호가 중복되지 않아야 합니다.");
         }
-
-        if(num.charAt(0) < 1 || num.charAt(0) > 45) {
+        if(num.matches("[^0-9]]")) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다. ");
+        }
+        int bonusNum = Integer.parseInt(num);
+        if(bonusNum < 1 || bonusNum > 45) {
             throw new IllegalArgumentException("[ERROR] [ERROR] 1 ~ 45 숫자만 입력 가능합니다.");
         }
     }
