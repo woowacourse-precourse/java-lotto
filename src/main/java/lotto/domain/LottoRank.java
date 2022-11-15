81dcb2f5421b7241179bde9e5397c980c7386b5f
jmjmjames <chungjm0711@gmail.com>
2022-11-15 09:43:25 +0900

    refactor(LottoRank): 당첨 금액을 long 으로 변경 calclatePrizeMoney 구현

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -3,18 +3,30 @@ package lotto.domain;
 
 public enum LottoRank {
 
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    REST(0, 0);
+    FIRST(6, 2_000_000_000L),
+    SECOND(5, 30_000_000L),
+    THIRD(5, 1_500_000L),
+    FOURTH(4, 50_000L),
+    FIFTH(3, 5_000L),
+    REST(0, 0L);
 
     private final int matchCount;
-    private final int prizeMoney;
+    private final long prizeMoney;
 
-    LottoRank(int matchCount, int prizeMoney) {
+    LottoRank(int matchCount, long prizeMoney) {
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
+    }
+
+    public Integer getMatchCount() {
+        return matchCount;
+    }
+
+    public Long getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public Long calculatePrizeMoney(long count) {
+        return this.prizeMoney * count;
     }
 }
