8c0909f2fa89edb43b7bc29e13902b6c3009cebf
thdwoqor <celebarte01@gmail.com>
2022-11-15 11:55:31 +0900

    feat: `로또 최대 구매 개수 반환` 기능 작성
    금액을 입력하면 최대 구매 가능한 로또 개수를 반환하고 나누어떨어지지 않는 경우 에러를 발생시킨다.

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -10,6 +10,7 @@ public class LottoGenerator {
     private static final int START_Inclusive = 1;
     private static final int END_Inclusive = 45;
     private static final int COUNT = 6;
+    private static final int PRICE = 1000;
 
     public static List<Lotto> automatic(int count) {
         List<Lotto> lottos = new ArrayList<>();
@@ -17,5 +18,12 @@ public class LottoGenerator {
             lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(START_Inclusive, END_Inclusive, COUNT)));
         }
         return lottos;
+    }
+
+    public static int maximum(int money) {
+        if (money % PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000원으로 나누어 떨어지지 않습니다.");
+        }
+        return money / PRICE;
     }
 }
