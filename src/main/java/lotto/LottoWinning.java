817e19a10402c53bb071e8bac61165a3468b7bcc
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-15 17:39:35 +0900

    feat: 수익률 계산 함수 생성

--- src/main/java/lotto/LottoWinning.java
+++ src/main/java/lotto/LottoWinning.java
@@ -13,4 +13,13 @@ public class LottoWinning {
         if (numbers.contains(bonus))
             throw new IllegalArgumentException("보너스 번호와 당첨 번호는 일치할 수 없습니다.");
     }
+
+    public static double getBenefit(int[] count, Integer inputMoney) {
+        int[] money = {5000,50000,1500000,2000000000,30000000};
+        long benefit = 0;
+        for (int i = 0; i < 5; i++) {
+            benefit += money[i]*count[i];
+        }
+        return (double) (benefit * 100) / inputMoney;
+    }
 }
