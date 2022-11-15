596fd20b69485cf13f4dfa51ce812cea3a124ba5
endlessmomo <soohyuk96@naver.com>
2022-11-15 21:59:38 +0900

    etc : Application 주석 처리 (에러 찾기 위해)

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -6,13 +6,13 @@ import view.OutputView;
 
 public class LottoController {
     public void run() {
-        PurchaseMoney purchaseMoney = new PurchaseMoney(InputView.inputMoney());
-        /*LotteryMachine lotteryMachine = new LotteryMachine(purchaseMoney.getLottoCount());
+/*        PurchaseMoney purchaseMoney = new PurchaseMoney(InputView.inputMoney());
+        LotteryMachine lotteryMachine = new LotteryMachine(purchaseMoney.getLottoCount());
         OutputView.showLottos(lotteryMachine.getLottos());
         WinningNumber winningNumber = createWinningNumber();
         LottoResult lottoResult = new LottoResult(lotteryMachine, winningNumber);
         OutputView.showResult(lottoResult.getResult());
-        OutputView.showProfitPercent(purchaseMoney.getPurchaseMoney(), lottoResult)*/;
+        OutputView.showProfitPercent(purchaseMoney.getPurchaseMoney(), lottoResult);*/
     }
 
     private WinningNumber createWinningNumber() {
