590049301af95930a064a87aea4ea31ec3702ddf
wjh2012 <wjh2012@naver.com>
2022-11-15 19:02:29 +0900

    feat(Lotto): 에러 메시지 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.List;
 
 import static lotto.config.Constants.LottoNumber.*;
+import static lotto.exception.LottoException.*;
 
 public class Lotto {
 
@@ -19,19 +20,19 @@ public class Lotto {
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NUMBER_COUNT_MISMATCH.getErrorMessage());
         }
     }
 
     private void validateDuplicated(List<Integer> numbers) {
         if (!numbers.stream().allMatch(new HashSet<>()::add)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NUMBER_DUPLICATED.getErrorMessage());
         }
     }
 
     private void validateRange(List<Integer> numbers) {
         if (!numbers.stream().allMatch(n -> n >= LOTTO_NUMBER_START_INCLUSIVE && n <= LOTTO_NUMBER_END_INCLUSIVE)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NUMBER_OUT_OF_RANGE.getErrorMessage());
         }
     }
 
