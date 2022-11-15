9574ce961ccdd4820d6e14c714ccdbcbd4e8b095
doehy <dbehxks02@naver.com>
2022-11-15 20:20:02 +0900

    test : 당첨 번호 입력값이 6개가 아닐 경우 예외

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -1,11 +1,14 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import resources.Error;
+import controller.Validator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -29,6 +32,18 @@ public class InputTest extends NsTest {
             runException("9999.9");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
+    }
+
+    @DisplayName("당첨 번호 입력값이 6개가 아닐 경우 예외가 발생한다.")
+    @Test
+    void createWinningNumberSize() {
+        Validator validator = new Validator();
+        String[] winningNumber = {"1","2","3","4","5","6","7"};
+        assertThatThrownBy(() -> validator.isCorrectSize(winningNumber,Error.SIZE))
+                .isInstanceOf(IllegalArgumentException.class);
+        String[] winningNumbers = {"1","2","3","4","5"};
+        assertThatThrownBy(() -> validator.isCorrectSize(winningNumbers,Error.SIZE))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
