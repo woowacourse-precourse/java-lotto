eae269c206ee10b9113d38cc89a87f50d66d13fd
peageon <jinsoo2005@gmail.com>
2022-11-15 15:31:14 +0900

    feat($Lotto): added generateLotto function
    generateLotto function takes an integer number as input and generate that many lottos and return as a nested list

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
-import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,7 +16,21 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        Set<Integer> checkDupe = new HashSet<Integer>(numbers);
+        if (checkDupe.size() != numbers.size()) {
+            throw new IllegalArgumentException("Lotto numbers have to be unique");
+        }
     }
 
     // TODO: 추가 기능 구현
+    public static List<List<Integer>> generateLotto(Integer count) {
+        List<List<Integer>> lottos = new ArrayList<>();
+        for (int i=0; i < count; i++) {
+            List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
+
+    
 }
