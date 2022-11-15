46a6fa9e00ead2c4c8bd710c5cd106bd73fec694
This2sho <zx00018@naver.com>
2022-11-15 10:18:17 +0900

    style(Application): 개행 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,8 +3,9 @@ package lotto;
 import lotto.controller.RunController;
 
 public class Application {
-    public static void main(String[] args) {
-        RunController runController = new RunController();
-        runController.run();
-    }
+
+	public static void main(String[] args) {
+		RunController runController = new RunController();
+		runController.run();
+	}
 }
