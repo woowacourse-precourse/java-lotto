e3c62ceb6d23aa7517adaceb9d467b3fdddc437a
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-15 10:50:10 +0900

    refactor(LottoMatcher): remove static import

--- src/main/java/lotto/participant/LottoMatcher.java
+++ src/main/java/lotto/participant/LottoMatcher.java
@@ -8,8 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static lotto.constant.Rank.*;
-
 public class LottoMatcher {
 
     private Map<Rank, Integer> rankCounts;
@@ -43,15 +41,15 @@ public class LottoMatcher {
 
     private void saveResult(int numberOfMatchedLottoNumber, boolean bonus) {
         if (numberOfMatchedLottoNumber == 6) {
-            rankCounts.put(FIRST_SIX_MATCHED, rankCounts.getOrDefault(FIRST_SIX_MATCHED, 0) + 1);
+            rankCounts.put(Rank.FIRST_SIX_MATCHED, rankCounts.getOrDefault(Rank.FIRST_SIX_MATCHED, 0) + 1);
         } else if (numberOfMatchedLottoNumber == 5 && bonus) {
-            rankCounts.put(SECOND_FIVE_WITH_BONUS, rankCounts.getOrDefault(SECOND_FIVE_WITH_BONUS, 0) + 1);
+            rankCounts.put(Rank.SECOND_FIVE_WITH_BONUS, rankCounts.getOrDefault(Rank.SECOND_FIVE_WITH_BONUS, 0) + 1);
         } else if (numberOfMatchedLottoNumber == 5) {
-            rankCounts.put(THIRD_FIVE_MATCHED, rankCounts.getOrDefault(THIRD_FIVE_MATCHED, 0) + 1);
+            rankCounts.put(Rank.THIRD_FIVE_MATCHED, rankCounts.getOrDefault(Rank.THIRD_FIVE_MATCHED, 0) + 1);
         } else if (numberOfMatchedLottoNumber == 4) {
-            rankCounts.put(FOURTH_FOUR_MATCHED, rankCounts.getOrDefault(FOURTH_FOUR_MATCHED, 0) + 1);
+            rankCounts.put(Rank.FOURTH_FOUR_MATCHED, rankCounts.getOrDefault(Rank.FOURTH_FOUR_MATCHED, 0) + 1);
         } else if (numberOfMatchedLottoNumber == 3) {
-            rankCounts.put(FIFTH_THREE_MATCHED, rankCounts.getOrDefault(FIFTH_THREE_MATCHED, 0) + 1);
+            rankCounts.put(Rank.FIFTH_THREE_MATCHED, rankCounts.getOrDefault(Rank.FIFTH_THREE_MATCHED, 0) + 1);
         }
     }
 }
