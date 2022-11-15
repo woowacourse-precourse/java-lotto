1053177f6f03b09e26775d3af94782a01f99d37c
kimsinhyun <ksh415622@naver.com>
2022-11-15 14:13:02 +0900

    feat: (Error Class) 오류 열거체 추가
    1. ILLEGAL_LOTTO_LENGTH: 로또 번호가 6자리가 아닌 에러 추가
    2. LOTTO_NUM_DUPLICATE_ERROR: 로또 번호가 중복이 있는 에러 추가

--- src/main/java/lotto/Error.java
+++ src/main/java/lotto/Error.java
@@ -1,8 +1,10 @@
 package lotto;
 
 public enum Error {
-    PURCHASE_AMOUNT_ERROR   ("1,000 단위의 금액만 구입 가능합니다"),
-    NOT_NUMERIC             ("숫자만 입력 가능합니다");
+    PURCHASE_AMOUNT_ERROR       ("1,000 단위의 금액만 구입 가능합니다"),
+    NOT_NUMERIC                 ("숫자만 입력 가능합니다"),
+    ILLEGAL_LOTTO_LENGTH        ("로또 번호는 6자리여야 합니다"),
+    LOTTO_NUM_DUPLICATE_ERROR   ("로또 번호는 중복될 수 없습니다");
 
     private static final String ERROR_MESSAGE = "[ERROR]";
     private final String errorMessage;
