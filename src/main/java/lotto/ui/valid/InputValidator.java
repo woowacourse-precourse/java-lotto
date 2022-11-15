585b32b294b38995e915d7b966bcabdc2afef56d
sonsoowon <yuwoo1018@gmail.com>
2022-11-15 19:38:33 +0900

    fix(InputValidator): isParsableInt 메서드 수정
    - 정수 최대 범위를 벗어나는 입력을 검사하기 위해 수정
    - 문자 검사 정규식 필드 삭제

--- src/main/java/lotto/ui/valid/InputValidator.java
+++ src/main/java/lotto/ui/valid/InputValidator.java
@@ -9,11 +9,10 @@ import java.util.stream.Collectors;
 
 public enum InputValidator {
     INSTANCE;
-    private static final String MONEY_ERROR_MESSAGE = "[ERROR] 1000원 단위의 금액을 입력해야 합니다.";
+    private static final String MONEY_ERROR_MESSAGE = "[ERROR] 1000원 단위의 금액을 입력해야 합니다 (최대 금액 2,147,483,000원).";
     private static final String JACKPOT_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45사이의 중복되지 않은 숫자 6개여야 합니다.";
     private static final String BONUS_NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 보너스 번호는 1부터 45사이의 숫자여야 합니다.";
 
-    private static final String NON_DIGIT = "(.*)\\D(.*)";
     private static final String JACKPOT_INPUT_SEPARATOR = ",";
 
     public static int getValidMoney(String moneyInput) throws IllegalArgumentException {
@@ -51,7 +50,12 @@ public enum InputValidator {
     }
 
     private static boolean isParsableInt(String userInput) {
-        return (userInput.length() > 0) && !userInput.matches(NON_DIGIT);
+        try {
+            Integer.parseInt(userInput);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
     private static void validateAllParsable(String jackpotInput) throws IllegalArgumentException {
@@ -66,5 +70,4 @@ public enum InputValidator {
     private static boolean isInLottoRange(int lottoNumber) {
         return (lottoNumber >= Lotto.MIN_NUMBER) && (lottoNumber <= Lotto.MAX_NUMBER);
     }
-
 }
