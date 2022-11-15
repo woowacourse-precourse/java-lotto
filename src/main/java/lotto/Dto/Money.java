d4a90092d0f49d58ffe1620cd90d3543b52322e9
namtndus <rnrmf0721@gmail.com>
2022-11-15 22:22:47 +0900

    refactor(Money): validValue함수 수정
    예외처리를 잡고 프로그램이 정상적으로 종료되도록 설계

--- src/main/java/lotto/Dto/Money.java
+++ src/main/java/lotto/Dto/Money.java
@@ -1,21 +1,34 @@
 package lotto.Dto;
 
 public class Money {
-    private final int money;
+    private int money;
 
     public Money(String money){
-        validValue(money);
-        this.money = Integer.valueOf(money) ;
+        if(validValue(money)){
+            this.money = Integer.valueOf(money) ;
+        }
+
+
     }
 
     public int getMoney() {
         return money;
     }
 
-    private void validValue(String money){
+    private boolean validValue(String money) throws IllegalArgumentException{
+        IllegalArgumentException illegalArgumentException = new IllegalArgumentException("[ERROR] 숫자만을 입력하시오");
         String pattern = ".*[A-Za-zㄱ-ㅎ].*$";
-        if(money.matches(pattern)){
-            throw new IllegalArgumentException("[ERROR] 숫자만을 입력하시오");
+
+        try{
+            if(money.matches(pattern)){
+                System.out.print(illegalArgumentException.getMessage());
+                throw illegalArgumentException;
+            }
+        }catch (IllegalArgumentException e){
+            return false;
         }
+
+        return true;
+
     }
 }
