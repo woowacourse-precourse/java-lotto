678c78203494d959e63b44eee42f40c3ac5399eb
es <mascoeskim@gmail.com>
2022-11-15 17:33:30 +0900

    test(randomNumberTest): 난수생성 로또번호 범위 및 크기 테스트 구현

---
+++ src/test/java/lotto/lotto/RandomNumberTest.java
@@ -0,0 +1,33 @@
+package lotto.lotto;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class RandomNumberTest{
+
+
+	@DisplayName("랜덤으로 생성되는 로또의 수가 6개인지 확인")
+	@Test
+	void createRandomNumberSize() {
+		randomNumber randomNumber= new randomNumber();
+		assertThat(randomNumber.lotto.size()).isEqualTo(6);
+
+	}
+	@DisplayName("랜덤으로 생성되는 로또의 수 범위가가 1-45인지 확인")
+	@Test
+	void createRandomNumberRange() {
+		randomNumber randomNumber= new randomNumber();
+		for (int i=0; randomNumber.lotto.size()>i;i++){
+			assertThat(randomNumber.lotto.get(i)<45 &&
+				randomNumber.lotto.get(i)>1).isTrue();
+		}
+	}
+
+
+
+	// 아래에 추가 테스트 작성 가능
+}
