ca84852d7283c4c44bfac7efc0552c2685ca33cc
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-15 14:43:55 +0900

    feat: 로또 당첨 번호를 입력 받는다
    *Feat
    - 당첨 번호를 입력받는다
    - 쉼표를 기준으로 번호를 나누어 저장한다
    - 보너스 번호를 입력받는다
    
    *Exception
    - 로또 구입 금액이 1,000원으로 나누어 떨어지지 않는 경우
    - 로또 구입 금액이 숫자가 아닌 경우
    - 입력받은 당첨 번호에 쉼표와 숫자를 제외한 다른 문자가 있을 경우
    - 입력받은 당첨 번호 숫자가 1~45 사이의 값이 아닐 경우
    - 입력받은 당첨 번호에 중복된 값이 있을 경우
    - 당첨 번호가 6개가 아닐 경우
    - 보너스 번호가 1~45까지의 숫자가 아닐 경우

--- src/main/java/lotto/controller/ConsoleLottoReader.java
+++ src/main/java/lotto/controller/ConsoleLottoReader.java
@@ -1,31 +1,91 @@
+
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.info.Price;
 
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
+
+import static lotto.domain.info.Rule.*;
 
 public class ConsoleLottoReader implements LottoReader {
     @Override
     public int readAmount() {
-        int amount = Integer.parseInt(Console.readLine());
+        String stringAmount = Console.readLine();
+        validateIsNumber(stringAmount);
+
+        int amount = Integer.parseInt(stringAmount);
         validateAmount(amount);
+
         return amount;
     }
 
     @Override
-    public List<Integer> readLottoNumber() {
-        return null;
+    public Set<Integer> readLottoNumber() {
+        Set<Integer> lottoNumber = splitNumber(Console.readLine());
+
+        validateLottoNumber(lottoNumber);
+        validateCount(lottoNumber);
+        return lottoNumber;
     }
 
     @Override
     public int readBonus() {
-        return 0;
+        String stringBonus = Console.readLine();
+        validateIsNumber(stringBonus);
+
+        int bonus = Integer.parseInt(stringBonus);
+        validateBonus(bonus);
+
+        return bonus;
+    }
+
+    public Set<Integer> splitNumber(String number) {
+        Set<Integer> lottoNumber = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(number, ",");
+
+        while (st.hasMoreTokens()) {
+            String token = st.nextToken();
+            validateIsNumber(token);
+            lottoNumber.add(Integer.parseInt(token));
+        }
+        return lottoNumber;
     }
 
     private void validateAmount(int amount) {
         if (amount % Price.SINGLE_PRICE.getPrice() != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000 원 단위여야 합니다.");
+            String message = "[ERROR] 로또 구입 금액은 " + Price.SINGLE_PRICE.getPrice() + "원 단위여야 합니다.\"";
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    private void validateLottoNumber(Set<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < START_NUMBER.getNumber() || number > END_NUMBER.getNumber()) {
+                String message = "[ERROR] 로또 번호는" + START_NUMBER.getNumber() + "부터" + END_NUMBER.getNumber() + "사이의 숫자여야 합니다.";
+                throw new IllegalArgumentException(message);
+            }
+        }
+    }
+
+    private void validateBonus(int bonus) {
+        if (bonus < START_NUMBER.getNumber() || bonus > END_NUMBER.getNumber()) {
+            String message = "[ERROR] 보너스 번호는" + START_NUMBER.getNumber() + "부터" + END_NUMBER.getNumber() + "사이의 숫자여야 합니다.";
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    private void validateCount(Set<Integer> numbers) {
+        if (numbers.size() != NUMBER_COUNT.getNumber()) {
+            String message = "[ERROR] 로또 번호는 중복되지 않는 " + NUMBER_COUNT.getNumber() + "자리 숫자여야 합니다.";
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    private void validateIsNumber(String number) {
+        if (!Pattern.matches("^[0-9]*$", number)) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
         }
     }
 }
