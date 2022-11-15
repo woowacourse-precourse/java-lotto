7ee3bfdff4bb84e606506d51733c6105811077ac
canbeyour <wuzucy@gmail.com>
2022-11-15 15:52:15 +0900

    refactor: move function to package for reuse

--- src/main/java/lotto/Utils/commonUtil.java
+++ src/main/java/lotto/Utils/commonUtil.java
@@ -1,2 +1,27 @@
-package lotto.Utils;public class commonUtil {
+package lotto.Utils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+public class commonUtil {
+    public static int stringToNumber(String input) {
+        int number = 0;
+        try {
+            number = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+        return number;
+    }
+
+    public static List<Integer> stringToNumbers(String input) {
+        List<Integer> result = new ArrayList<>();
+        try {
+            result = Arrays.stream(input.split(","))
+                    .map(Integer::valueOf)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            new Error().outOfRange();
+        }
+        return result;
+    }
 }
