68d8eee40a3b4d0d71bc89e0520d15a8e675e5ef
hwi215 <buc06137@naver.com>
2022-11-15 14:50:10 +0900

    feat: [기능3] 1개의 로또 발행 시 중복되지 않는 6개의 숫자 뽑는 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,11 +1,28 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import static lotto.view.Input.input;
 
 public class Lotto {
     private final List<Integer> numbers;
+
+    public static List randomLottoNum(){
+        List<Integer> randomLotto = new ArrayList<>();
+
+        int num = 0;
+        while(num != 6){
+            int number = Randoms.pickNumberInRange(1, 45);
+            if(!randomLotto.contains(number)){
+                num++;
+                randomLotto.add(number);
+            }
+        }
+        return randomLotto;
+    }
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,7 +1,22 @@
 package lotto.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static lotto.Lotto.lottoNum;
+import static lotto.Lotto.randomLottoNum;
 
 public class LottoGameController {
-    int num = lottoNum();
+
+    public void LottoGame(){
+        int num = lottoNum();
+        List<Integer> lotto = new ArrayList<>();
+
+        for(int n = 0; n < num; n++){
+            lotto = randomLottoNum();
+            System.out.println(lotto.toString());
+
+        }
+
+    }
 }
