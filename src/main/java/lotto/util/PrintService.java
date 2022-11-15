1e6df8bc0a2c5c210de8222c5a2d9020fe63d31e
JeonghakLee <wjdgkr0527@naver.com>
2022-11-15 10:03:32 +0900

    feat: 구매한 로또를 출력하는 기능 구현

---
+++ src/main/java/lotto/util/PrintService.java
@@ -0,0 +1,16 @@
+package lotto.util;
+
+
+import java.util.List;
+import lotto.domain.Lotto;
+
+public class PrintService {
+
+    public static void printLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+}
