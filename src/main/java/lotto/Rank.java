e68efc524906c830cf20a0299ce01cc8694c6c5f
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-11-15 18:24:58 +0900

    feat: Rank에 출력 메세지 및 상금 추가
    Enum class를 통해 각 Rank에 맞는 출력 메세지와 상금을 도입했다.

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -2,5 +2,24 @@ package lotto;
 
 public enum Rank {
 
-    First, Second, Third, Fourth, Fifth, Miss
+    FIRST(2000000000, "6개 일치 (2,000,000,000원)"), SECOND(30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"), THIRD(1500000,
+            "5개 일치 (1,500,000원)"), FOURTH(50000,
+            "4개 일치 (50,000원)"), FIFTH(5000, "3개 일치 (5,000원)"), MISS(0,
+            "꽝");
+
+    private final int prize;
+    private final String matchCountMessage;
+
+    Rank(int prize, String matchCountMessage) {
+        this.prize = prize;
+        this.matchCountMessage = matchCountMessage;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getMatchCountMessage() {
+        return matchCountMessage;
+    }
 }
