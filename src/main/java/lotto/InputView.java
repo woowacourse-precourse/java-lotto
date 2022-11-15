e7e58a05f433510aaae3bd01e9aba2895112c424
noparamin <noparamin@naver.com>
2022-11-15 22:54:55 +0900

    feat: 입력 예외 검사 기능 구현

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.enums.EtcNumber;
 import lotto.enums.InputMessage;
 
 import java.util.List;
@@ -11,7 +12,9 @@ public class InputView {
 
     public int inputMoney() {
         System.out.println(InputMessage.MONEY_INPUT_MESSAGE.getMessage());
-        int purchasingAmount = Integer.parseInt(Console.readLine());
+        String inputMoney = Console.readLine();
+        validateInputMoney(inputMoney);
+        int purchasingAmount = Integer.parseInt(inputMoney);
         System.out.println();
 
         return purchasingAmount;
@@ -20,7 +23,9 @@ public class InputView {
     public List<Integer> inputWinningNumbers() {
         System.out.println(InputMessage.WINNING_NUMBERS_INPUT_MESSAGE.getMessage());
         String inputWinningNumbers = Console.readLine();
+        validateInputWinningLotto(inputWinningNumbers);
         List<Integer> winningNumbers = conversionUtility.convertStringWinningNumbersToList(inputWinningNumbers);
+        validateNumberRange(winningNumbers);
         System.out.println();
 
         return winningNumbers;
@@ -28,9 +33,83 @@ public class InputView {
 
     public int inputBonusNumber() {
         System.out.println(InputMessage.BONUS_NUMBER_INPUT_MESSAGE.getMessage());
-        int bonusNumber = Integer.parseInt(Console.readLine());
+        String inputBonusNumber = Console.readLine();
+        validateInputBonusNumber(inputBonusNumber);
+        int bonusNumber = Integer.parseInt(inputBonusNumber);
         System.out.println();
 
         return bonusNumber;
+    }
+
+    public void validateInputMoney(String inputMoney) {
+        if (isNumber(inputMoney) && isBiggerThanThousand(inputMoney) && isDivideByThousand(inputMoney)) {
+            return;
+        }
+        throw new IllegalArgumentException(InputMessage.ERROR_MESSAGE.getMessage());
+    }
+
+    public boolean isNumber(String input) {
+        for (int index = EtcNumber.ZERO.getNumber(); index < input.length(); index++) {
+            char number = input.charAt(index);
+            if (!Character.isDigit(number)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isBiggerThanThousand(String inputMoney) {
+        return Integer.parseInt(inputMoney) >= 1000;
+    }
+
+    public boolean isDivideByThousand(String inputMoney) {
+
+        if (Integer.parseInt(inputMoney) % 1000 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public void validateInputWinningLotto(String inputWinningNumbers) {
+        if (isNumberOrSeparator(inputWinningNumbers)) {
+            return;
+        }
+        throw new IllegalArgumentException(InputMessage.ERROR_MESSAGE.getMessage());
+    }
+
+    public boolean isNumberOrSeparator(String inputWinningNumbers) {
+        for (int index = EtcNumber.ZERO.getNumber(); index < inputWinningNumbers.length(); index++) {
+            char number = inputWinningNumbers.charAt(index);
+            if (!Character.isDigit(number) && number != ',') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void validateNumberRange(List<Integer> winningNumbers) {
+        for (int number : winningNumbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(InputMessage.ERROR_MESSAGE.getMessage());
+            }
+        }
+    }
+
+    void validateInputBonusNumber(String inputBonusNumber) {
+        if (isNumber(inputBonusNumber) && isRangeRight(inputBonusNumber)) {
+            return;
+        }
+        throw new IllegalArgumentException(InputMessage.ERROR_MESSAGE.getMessage());
+    }
+
+    public boolean isRangeRight(String inputBonusNumber) {
+        int number = Integer.parseInt(inputBonusNumber);
+        if (number >= 1 && number <= 45) {
+            return true;
+        }
+
+        return false;
     }
 }
