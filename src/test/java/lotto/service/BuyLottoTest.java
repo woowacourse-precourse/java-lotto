8c71e08f0ae19ead03e17cd255b26f6ef026bcfd
ywj9811 <ywj9811@naver.com>
2022-11-15 21:07:01 +0900

    testL BuyLottoTest 정렬 테스트 작성

--- src/test/java/lotto/service/BuyLottoTest.java
+++ src/test/java/lotto/service/BuyLottoTest.java
@@ -1,14 +1,19 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.Application;
 import lotto.exception.BuyMoneyException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class BuyLottoTest {
+public class BuyLottoTest extends NsTest {
     private BuyLotto buyLotto;
 
     @BeforeEach
@@ -29,5 +34,25 @@ public class BuyLottoTest {
         int quantity = 5;
         assertThat(buyLotto.getMyLottoNumbers(quantity).size())
                 .isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("정렬_테스트")
+    void sortTest() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("1000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "1개를 구매했습니다.",
+                            "[8, 21, 23, 41, 42, 43]"
+                    );
+                },
+                List.of(21, 8, 41, 23, 42, 43)
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
     }
 }
