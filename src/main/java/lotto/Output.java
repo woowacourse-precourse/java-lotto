2ecce956d7f0df04331290d8f1436c43928c6be0
DavinAn <blueapple1030@gmail.com>
2022-11-15 23:47:09 +0900

    docs : add javadocs to Output class

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -2,19 +2,33 @@ package lotto;
 
 import java.util.Map;
 
+/**
+ * Handles all console outputs.
+ *
+ * @author Davin An
+ * @version 1.0
+ */
 public class Output {
+
     private final TicketHandler ticketHandler;
 
     public Output() {
         ticketHandler = new TicketHandler();
     }
 
+
+    /**
+     * Prints out the lotto tickets the user has.
+     */
     public void printTickets() {
         for (Lotto l : Application.getLotto()) {
             System.out.println(l.getNumbers());
         }
     }
 
+    /**
+     * Prints out the winnings and the profit the user made.
+     */
     public void printWinnings() {
         System.out.println("당첨 통계\n---");
         int winnings = 0;
