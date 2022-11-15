1c21e3003f0f53eda06ff88206d68084a05dbc71
elizabethgim <yujingim@kookmin.ac.kr>
2022-11-15 11:04:57 +0900

    feat: 입력된 금액만큼 로또 구입

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,12 @@
 package lotto;
 
 public class Application {
+
+    public static int BuyLotto(int money){
+        int paper = money / 1000;
+        System.out.println(paper);
+        return paper;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
