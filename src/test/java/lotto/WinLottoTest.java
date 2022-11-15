aa24c177c49da85bf567d01708ddd8b2d48c5946
korjun1993 <corojoon93@naver.com>
2022-11-15 23:29:51 +0900

    feat(WinLotto) : 두 로또의 발행 번호 간 공통 번호를 알아내는 기능 테스트

--- src/test/java/lotto/WinLottoTest.java
+++ src/test/java/lotto/WinLottoTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class WinLottoTest {
@@ -13,5 +14,13 @@ public class WinLottoTest {
     void createWinLottoByDuplicateNumber() {
         Assertions.assertThatThrownBy(() -> new WinLotto(List.of(1, 2, 3, 4, 5, 6), 6))
                 .isInstanceOf(MyIllegalArgumentException.class);
+    }
+
+    @DisplayName("두 로또 번호의 교집합을 구하는 기능을 테스트한다.")
+    @Test
+    void compare() {
+        Lotto lotto = new Lotto(Arrays.asList(3, 2, 1, 5, 4, 6));
+        WinLotto winLotto = new WinLotto(Arrays.asList(1, 7, 8, 9, 10, 11), 12);
+        Assertions.assertThat(winLotto.compare(lotto)).isEqualTo(Arrays.asList(1));
     }
 }
