dde050251a6d3b091454cfc4b9bf6a1b8057c2d6
show2891 <show2891@gmail.com>
2022-11-15 23:20:36 +0900

    구입 금액 입력 받기_수정_형 변환 / 구매 개수 계산 및 출력_구현 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class Application {
 
-    static String getMoney() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+    static Integer getMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
         Scanner input = new Scanner(System.in);
         String userInput = input.nextLine();
-        return userInput;
+
+        Integer money = Integer.valueOf(userInput);
+        return money;
     }
     static String[] getWinNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
@@ -18,10 +20,20 @@ public class Application {
         return winNumbers;
     }
 
+    static Integer calculateLottoCount(Integer money) {
+
+        Integer lottoPrice = 1000;
+        Integer lottoCount = money / lottoPrice;
+
+        System.out.println(lottoCount + "개를 구매했습니다.");
+        return lottoCount;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String[] winNumbers = getWinNumbers();
-        String Money = getMoney();
+        Integer money = getMoney();
+        Integer lottoCount = calculateLottoCount(money);
 
+        String[] winNumbers = getWinNumbers();
     }
 }
