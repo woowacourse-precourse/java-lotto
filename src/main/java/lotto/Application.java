c7ab47eef68e99b04e56cc6ebab32a9c77406ac4
Qulip <ilkwon.ryu@gmail.com>
2022-11-15 23:30:47 +0900

    fix: Application에서 예외 처리 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,10 @@ import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController.runGame();
+        try {
+            LottoController.runGame();
+        } catch (IllegalStateException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
