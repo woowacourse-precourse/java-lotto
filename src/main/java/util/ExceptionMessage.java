5a5941d5e79a5bd4a83bd72d4974b543e3113f54
seheonCho <whtpgjs94@gmail.com>
2022-11-15 17:48:14 +0900

    refactor(ExceptionMessage): 인스턴스화가 필요 없어서 private 생성자 추가

--- src/main/java/util/ExceptionMessage.java
+++ src/main/java/util/ExceptionMessage.java
@@ -2,6 +2,8 @@ package util;
 
 public class ExceptionMessage {
 
+    private ExceptionMessage() {}
+
     private static final String PREFIX = "[ERROR] ";
     public static final String OUT_OF_SIZE = PREFIX + "6자리가 아닙니다.";
     public static final String DUPLICATE_NUMBER = PREFIX + "중복된 숫자가 있습니다.";
