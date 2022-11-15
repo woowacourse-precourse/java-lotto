0b1e75cef0c3b1fbd1852cfd395517282ddf84a1
lhyeonj120 <isk04387@gmail.com>
2022-11-15 18:43:55 +0900

    feat: 로또 구입 개수 계산 기능 구현

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 public class User {
+    private static int lottoCnt;
     private static final String regex = "^\\d+$";
 
     public User() {
@@ -8,6 +9,7 @@ public class User {
 
     public void buyLotto(String price) {
         validate(price);
+        lottoCnt = calculateLottoCnt(price);
     }
 
     private void validate(String price) {
@@ -17,5 +19,13 @@ public class User {
         if (Integer.parseInt(price) % 10 != 0) {
             throw new IllegalArgumentException();
         }
+    }
+    
+    private int calculateLottoCnt(String price) {
+        return Integer.parseInt(price) / 1000;
+    }
+
+    public int getLottoCnt() {
+        return lottoCnt;
     }
 }
