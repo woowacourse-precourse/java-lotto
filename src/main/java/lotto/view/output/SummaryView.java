a25bbeb473c774791e99a07b8f353c8fccbfcf94
Ridealist <cjsn92@gmail.com>
2022-11-15 19:42:40 +0900

    refactor(SummaryView): 출력문 String 값들 클래스 상수 선언

--- src/main/java/lotto/view/output/SummaryView.java
+++ src/main/java/lotto/view/output/SummaryView.java
@@ -5,36 +5,36 @@ import lotto.model.Prize;
 import lotto.model.Result;
 
 public class SummaryView {
-    // TODO 매개변수 너무 많음. 효율화 고민.
+    private static final String START_MESSAGE = "%n당첨 통계%n";
+    private static final String UNDER_BAR = "----%n";
+    private static final String GENERIC_SUMMARY_MESSAGE = "%d개 일치 (%s원) - %d개" + "%n";
+    private static final String SECOND_PRIZE_SUMMARY_MESSAGE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개" + "%n";
+    private static final String EARNING_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
+
     public static void showResult(Result result) {
-        System.out.printf("%n당첨 통계%n");
-        System.out.println("---");
+        System.out.printf(START_MESSAGE + UNDER_BAR);
         for (Prize e : Prize.values()) {
             if (e.equals(Prize.SECOND)) {
-                System.out.printf("%d개 일치, 보너스 볼 일치 (%s원) - %d개" + "%n", e.getMatchingCount(),
-                        intToCommaString(e.getReward()), result.getPrizeCount().get(e));
+                System.out.printf(SECOND_PRIZE_SUMMARY_MESSAGE, e.getMatchingCount(), intToCommaString(e.getReward()),
+                        result.getPrizeCount().get(e));
                 continue;
             }
-            System.out.printf("%d개 일치 (%s원) - %d개" + "%n", e.getMatchingCount(), intToCommaString(e.getReward()),
+            System.out.printf(GENERIC_SUMMARY_MESSAGE, e.getMatchingCount(), intToCommaString(e.getReward()),
                     result.getPrizeCount().get(e));
         }
     }
 
     private static String intToCommaString(long number) {
         DecimalFormat decFormat = new DecimalFormat("###,###");
-        String commaStr = decFormat.format(number);
-        return commaStr;
+        return decFormat.format(number);
     }
 
-    // TODO 매개변수 너무 많음. 효율화 고민 too.
     public static void showEarning(Result result, int amount) {
         float prizeMoney = 0;
         for (Prize e : Prize.values()) {
             prizeMoney += (e.getReward() * result.getPrizeCount().get(e));
-            System.out.println(prizeMoney);
         }
         float f = (prizeMoney / (amount * 1_000)) * 100;
-        System.out.println();
-        System.out.println("총 수익률은 " + String.format("%.1f", f) + "%입니다.");
+        System.out.printf(EARNING_RATE_MESSAGE, f);
     }
 }
