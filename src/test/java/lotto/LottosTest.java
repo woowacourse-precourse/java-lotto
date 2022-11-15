cc6907098b668e1d1da388823fb036c116ada5d8
Amyhds <dasomhan524@gmail.com>
2022-11-15 13:11:22 +0900

    test(lottostest): 변수명 수정

--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -5,16 +5,12 @@ import lotto.model.Lottos;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
-
 class LottosTest {
     @DisplayName("로또 리스트 생성 테스트")
     @Test
     void createLottos() {
-        int ticketAmount = 3;
-        Lottos testLottos = new Lottos(ticketAmount);
+        int testTicketAmount = 3;
+        Lottos testLottos = new Lottos(testTicketAmount);
         for (Lotto testLotto : testLottos.lottos) {
             testLotto.printLotto();
         }
