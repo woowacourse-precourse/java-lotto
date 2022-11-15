f16cc089f2da50f0f62667276097bff7f51d60e1
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-15 16:16:42 +0900

    fix(OutputView): 당첨이 있는 등수만 출력되는 에러 해결
    등수 맵에 등수에 따른 기본값 세팅 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,7 @@ import lotto.domain.lotto.Lotto;
 import lotto.domain.rank.LottoRank;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +13,7 @@ import java.util.stream.Collectors;
 public class OutputView {
 
     private static final DecimalFormat decFormat = new DecimalFormat("###,###");
+    private static final long DEFAULT_VALUE = 0L;
 
     public static void printLottoList(List<Lotto> lotties) {
         String printFormat = String.format(View.OUTPUT_LOTTO_ISSUE.message(), lotties.size());
@@ -31,6 +33,8 @@ public class OutputView {
         System.out.println(View.OUTPUT_STATISTICS.message());
         System.out.println(View.OUTPUT_DOTTED_LINE.message());
 
+        fillUnRankedToMap(winLottoInfo);
+
         winLottoInfo.keySet()
                 .stream()
                 .filter(rank -> !rank.equals(LottoRank.FAIL))
@@ -39,15 +43,23 @@ public class OutputView {
                 .forEach(rank -> printResultAccordingToBonus(rank, winLottoInfo.get(rank).intValue()));
     }
 
+    private static void fillUnRankedToMap(Map<LottoRank, Long> winLottoInfo) {
+        Arrays.stream(LottoRank.values())
+                .filter(rank -> !winLottoInfo.containsKey(rank))
+                .forEach(rank -> winLottoInfo.put(rank, DEFAULT_VALUE));
+    }
+
     private static void printResultAccordingToBonus(LottoRank rank, int totalCount) {
         String winMoney = decFormat.format(rank.winMoney());
-        String printFormat = String.format(View.OUTPUT_WIN_RESULT.message(), rank.matchCount(), winMoney, totalCount);
-
-        if (rank.isBonusNumber()) {
-            printFormat =  String.format(View.OUTPUT_BONUS_RESULT.message(), rank.matchCount(), winMoney, totalCount);
-        }
-
+        String printFormat = String.format(selectMessage(rank), rank.matchCount(), winMoney, totalCount);
         System.out.println(printFormat);
+    }
+
+    private static String selectMessage(LottoRank rank) {
+        if (rank.isBonusNumber()) {
+            return View.OUTPUT_BONUS_RESULT.message();
+        }
+        return View.OUTPUT_WIN_RESULT.message();
     }
 
     public static void printYieldResult(double lottoYield) {
