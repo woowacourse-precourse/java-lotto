d1e2fcd7b8799e9d1519fbec4df65914607d4681
kswhj <108874006+kswhj@users.noreply.github.com>
2022-11-15 17:00:01 +0900

    Create FifthPrize.java
    5등상금 총액 구하기

---
+++ src/test/java/lotto/calculate/FifthPrize.java
@@ -0,0 +1,16 @@
+package calculate;
+
+import java.util.List;
+
+public class FifthPrize {
+	public static int run(List<Integer> correctNums) {
+	int answer = 0;
+	for(int i : correctNums) {
+		if(i==3) {
+			answer++;
+		}
+	}
+	System.out.println("3개 일치 (5,000원) - "+ answer +"개");
+	return answer*5000;
+	}
+}
