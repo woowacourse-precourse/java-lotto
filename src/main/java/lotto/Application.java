dc27d1222f2ac2659a9ed027b1e55dd6d7065654
Jenn Kim <grdd0419@gmail.com>
2022-11-15 19:54:46 +0900

    refactor: 비활성화된 코드 정리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,17 +1,6 @@
 package lotto;
 
 import lotto.controller.LottoController;
-import lotto.domain.*;
-
-import java.text.DecimalFormat;
-import java.util.*;
-import java.util.stream.Stream;
-
-import static lotto.domain.Preset.LOTTO_LENGTH;
-import static lotto.domain.Preset.LOTTO_PRICE;
-import static lotto.domain.WinningType.getWinningType;
-import static lotto.view.InputView.*;
-import static lotto.view.OutputView.printPurchaseNum;
 
 public class Application {
     public static void main(String[] args) {
