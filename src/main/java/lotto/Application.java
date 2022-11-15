29bf851f33111732af06795c7d9e51d30651b687
seyeon ji <joeun9705@gmail.com>
2022-11-15 18:37:14 +0900

    refactor: apply fixed InputView class features

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,17 +1,23 @@
 package lotto;
 import lotto.Controller;
 public class Application {
+
     public static void main(String[] args) {
+        Integer money;
        InputView inputView = new InputView();
-        LottoNumberCreator lottoCreator = new LottoNumberCreator((inputView.getTicketMoney()));
+       try {
+           money = inputView.money(inputView.inputMoney());
+       }catch(Exception e) {
+           money = inputView.money(inputView.inputMoney());
+           System.out.println(e.getMessage());
+       }
+        LottoNumberCreator lottoCreator = new LottoNumberCreator(money);
         lottoCreator.showHowManyTickets();
        inputView.pressLottoNumbers();
        Lotto lotto = new Lotto(inputView.getLottoNumbers());
        inputView.pressBonusNumber();
        Controller controller = new Controller(lotto.getLottoNumbers(), inputView.getBonusNumber(), inputView.getTicketMoney());
        controller.showLottoWinningResult(lottoCreator.getNumbersContainer());
-
-
     }
 }
 
