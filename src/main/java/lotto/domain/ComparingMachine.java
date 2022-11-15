687d04b0d8ff0cb339bc24546fbca94cc8c2d751
jijongkwon <help3451@naver.com>
2022-11-15 17:53:49 +0900

    feat : 당첨번호 개수에 따른 금액확인 기능 추가

--- src/main/java/lotto/domain/ComparingMachine.java
+++ src/main/java/lotto/domain/ComparingMachine.java
@@ -1,11 +1,17 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ComparingMachine {
     private List<Integer> containNumberList = new ArrayList<>();
     private List<Integer> containBonusNumber = new ArrayList<>();
+    private Map<Integer,Integer> result = new HashMap<>();
+
+    int third = 0;
+    int fourth = 0;
+    int fifth = 0;
+    int fifth_bonus = 0;
+    int sixth = 0;
 
     public void countContainNumber(List<Integer> winningNumber, List<List<Integer>> lotto){
         for(int i = 0; i < lotto.size(); i++){
@@ -31,11 +37,31 @@ public class ComparingMachine {
         }
     }
 
+    public void countWinResult(){
+        for(int i = 0; i < containNumberList.size(); i++){
+            if(containNumberList.get(i) == 3){third++;}
+            if(containNumberList.get(i) == 4){fourth++;}
+            if(containNumberList.get(i) == 5 && containBonusNumber.get(i) == 0){fifth++;}
+            if(containNumberList.get(i) == 5 && containBonusNumber.get(i) == 1){fifth_bonus++;}
+            if(containNumberList.get(i) == 6){sixth++;}
+        }
+        result.put(5000,third);
+        result.put(50000,fourth);
+        result.put(1500000,fifth);
+        result.put(30000000,fifth_bonus);
+        result.put(2000000000,sixth);
+    }
+
+
     public List<Integer> getContainNumberList() {
         return containNumberList;
     }
 
     public List<Integer> getContainBonusNumber() {
         return containBonusNumber;
+    }
+
+    public Map<Integer, Integer> getResult() {
+        return result;
     }
 }
