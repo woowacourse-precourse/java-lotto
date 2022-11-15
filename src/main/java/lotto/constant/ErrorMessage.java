e17710868a0a530c9c3145332951ec4c701b277f
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-15 10:55:08 +0900

    feat(ErrorMessage): remove toString method

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -1,6 +1,7 @@
 package lotto.constant;
 
 public enum ErrorMessage {
+
     LENGTH_OF_LOTTO_NUMBERS_SHOULD_BE_SIX("로또 번호들은 6개의 숫자로 구성되어야 합니다."),
     LOTTO_NUMBERS_SHOULD_BE_UNIQUE("로또 번호들은 서로 다른 수여야 합니다."),
     PAYMENT_UNIT_SHOULD_BE_1000("구입 금액은 1,000원 단위의 수여야 합니다."),
@@ -9,7 +10,6 @@ public enum ErrorMessage {
     FORM_OF_BONUS_NUMBER_IS_WRONG("보너스 번호 형태가 잘못되었습니다."),
     BONUS_NUMBER_AND_WINNING_NUMBER_IS_DUPLICATED("보너스 번호가 당첨 번호와 중복되었습니다.");
 
-    private final String messageType = "[ERROR] ";
     private final String errorMessage;
 
     ErrorMessage(String errorMessage) {
@@ -17,11 +17,6 @@ public enum ErrorMessage {
     }
 
     public String getErrorMessage() {
-        return messageType + errorMessage;
-    }
-
-    @Override
-    public String toString() {
-        return messageType + errorMessage;
+        return "[ERROR] " + errorMessage;
     }
 }
