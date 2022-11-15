6cbf2b9d0587f54545d060e38587e2f45de7a9a4
canbeyour <wuzucy@gmail.com>
2022-11-15 16:59:02 +0900

    refactor: remove unused function and modify function to use static

--- src/main/java/lotto/Utils/Print.java
+++ src/main/java/lotto/Utils/Print.java
@@ -6,35 +6,22 @@ import lotto.domain.WinInfo;
 import java.util.*;
 
 public class Print {
-    public void askAmount() {
+    public static void askAmount() {
         System.out.println("구입 금액을 입력해 주세요.");
     }
-    public void askWinningNumber() {
+
+    public static void askWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
-    public void askBonusNumber() {
+
+    public static void askBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
-    public void purchaseCount(int count) {
-        System.out.printf("%d개를 구매했습니다.\n", count);
-    }
-    public void purchasedLottos(List<Lotto> lottos) {
+
+    public static void purchasedLottos(List<Lotto> lottos) {
         System.out.printf("%d개를 구매했습니다.\n", lottos.size());
         for (Lotto lotto : lottos) {
             lotto.print();
         }
-    }
-    public void winningStats(LinkedHashMap<String, Integer> winningStats) {
-        System.out.println("당첨 통계\n---");
-        for (Map.Entry<String, Integer> entry : winningStats.entrySet()) {
-            String key = entry.getKey();
-            System.out.printf("%d개 일치", WinInfo.valueOf(key).getMatch());
-            if (key.equals("SECOND"))
-                System.out.print(", 보너스 볼 일치");
-            System.out.printf(" (%s) - %d개\n" , WinInfo.valueOf(key).getWinnings(), winningStats.get(key));
-        }
-    }
-    public void returnRate(String returnRate) {
-        System.out.printf("총 수익률은 %s%%입니다.\n", returnRate);
     }
 }
