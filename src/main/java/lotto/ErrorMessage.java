8589bff947c4ce1d81ca8e43cc2cc1389c3cd668
This2sho <zx00018@naver.com>
2022-11-15 09:29:01 +0900

    refactor(ErrorMessage): 에러 메시지 변경

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -8,5 +8,5 @@ public class ErrorMessage {
 	public static final String LITTLE_BUDGET_ERROR_MESSAGE = "[ERROR] 로또 한장의 가격은 1,000원 입니다.";
 	public static final String INDIVISIBLE_ERROR_MESSAGE = "[ERROR] 구입금액을 1,000원 단위로 입력해주세요.";
 	public static final String DUPLICATE_WINNING_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 번호와 보너스 번호는 중복 될 수 없습니다.";
-	public static final String INVALID_WINNING_NUMBER_ERROR_MESSAGE = "[ERROR] 당첨 번호는 숫자와 ,로만 이루어져야합니다.";
+	public static final String INVALID_WINNING_NUMBER_ERROR_MESSAGE = "[ERROR] 당첨 번호는 ,로 구분된 6개의 숫자입니다.";
 }
