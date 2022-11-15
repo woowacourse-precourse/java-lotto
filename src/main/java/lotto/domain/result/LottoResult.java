5f3e46d574969a1e7f570ffa974d81b02eb34852
kimdoha <kkhhjj3393@gmail.com>
2022-11-15 23:51:41 +0900

    feat(Lotto): 로또 결과 반환 클래스 구현
    - 1. 추첨 기계가 생성한 자동 로또 번호 리스트를 모두 출력한다.
    - 2. 당첨 내역을 반환한다.
    - 3. 수익률을 반환한다.

--- src/main/java/lotto/domain/result/LottoResult.java
+++ src/main/java/lotto/domain/result/LottoResult.java
@@ -3,45 +3,59 @@ package lotto.domain.result;
 import static lotto.constant.ErrorMessage.ERROR_MESSAGE_OF_CONSTRUCTOR;
 
 import java.util.List;
+import java.util.Map;
+import lotto.constant.LottoRank;
 
 public class LottoResult {
 
-  public static final String LOTTO_STATUS_START = "[";
-  public static final String LOTTO_STATUS_END = "]";
-  public static final String LOTTO_NUMBER_DELIMETER = ", ";
   public static final String LINE_BREAK = "\n";
-  public static final String STRING_SPLIT_DELIMETER = "";
-
-  public static final String OUTPUT_MESSAGE_OF_LOTTERY_RESULT = "당첨 통계";
+  public static final String OUTPUT_MESSAGE_OF_LOTTO_RESULT = "당첨 통계";
   public static final String OUTPUT_MESSAGE_OF_DIVIDING_LINE = "---";
+  public static final String OUTPUT_MESSAGE_OF_LOTTO_RANKING = "%d개 일치";
+  public static final String COUNT = "개\n";
+  public static final String OUTPUT_MESSAGE_OF_EARNING_RATE = "총 수익률은 ";
+  public static final String OUTPUT_MESSAGE_OF_MAMOOREE = "%입니다.\n";
+
+
+  public static final String FIFTH_MESSAGE = "3개 일치 (5,000원) - ";
+  public static final String FOURTH_MESSAGE = "4개 일치 (50,000원) - ";
+  public static final String THIRD_MESSAGE = "5개 일치 (1,500,000원) - ";
+  public static final String SECOND_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+  public static final String FIRST_MESSAGE = "6개 일치 (2,000,000,000원) - ";
 
   private LottoResult() {
     throw new UnsupportedOperationException( ERROR_MESSAGE_OF_CONSTRUCTOR );
   }
 
-  private static String getRefinedStringList(String[] numbers) {
+  public static void makeStringResultOfEveryLottoList(List<List<Integer>> numbers) {
+    for ( List<Integer> lotto : numbers ) {
+      System.out.println(lotto);
+    }
+  }
+
+  public static String makeLottoStaticResult() {
     StringBuilder stringBuilder = new StringBuilder();
 
-    stringBuilder.append( LOTTO_STATUS_START );
-    stringBuilder.append( String.join( LOTTO_NUMBER_DELIMETER, numbers ) );
-    stringBuilder.append( LOTTO_STATUS_END );
-    stringBuilder.append( LINE_BREAK );
+    stringBuilder.append(OUTPUT_MESSAGE_OF_LOTTO_RESULT).append(LINE_BREAK);
+    stringBuilder.append(OUTPUT_MESSAGE_OF_DIVIDING_LINE).append(LINE_BREAK);
 
+    stringBuilder.append(OUTPUT_MESSAGE_OF_LOTTO_RANKING);
     return stringBuilder.toString();
   }
 
-  private static String convertString(List<Integer> numberList) {
-    return numberList.toString();
+  public static String getLottoStatisticInfo(Map<LottoRank, Integer> map) {
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append(FIFTH_MESSAGE).append(map.get(LottoRank.FIFTH)).append(COUNT);
+    stringBuilder.append(FOURTH_MESSAGE).append(map.get(LottoRank.FOURTH)).append(COUNT);
+    stringBuilder.append(THIRD_MESSAGE).append(map.get(LottoRank.THIRD)).append(COUNT);
+    stringBuilder.append(SECOND_MESSAGE).append(map.get(LottoRank.SECOND)).append(COUNT);
+    stringBuilder.append(FIRST_MESSAGE).append(map.get(LottoRank.FIRST)).append(COUNT);
+    return stringBuilder.toString();
   }
 
-  public static String makeStringResultOfEveryLottoList(List<List<Integer>> numbers) {
+  public static String getRevenuePercentageInfo(Double rate) {
     StringBuilder stringBuilder = new StringBuilder();
-
-    for ( List<Integer> lotto : numbers ) {
-      String[] numberList = convertString( lotto ).split( STRING_SPLIT_DELIMETER );
-      stringBuilder.append( getRefinedStringList( numberList ) );
-    }
-
+    stringBuilder.append(OUTPUT_MESSAGE_OF_EARNING_RATE).append( rate ).append( OUTPUT_MESSAGE_OF_MAMOOREE );
     return stringBuilder.toString();
   }
 }
