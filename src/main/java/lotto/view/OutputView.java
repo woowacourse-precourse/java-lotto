ccd88a356fdf74a3195baccf5f2a18a2871ae83e
LeeHaiLim <hailim1997@gmail.com>
2022-11-15 11:46:21 +0900

    feat(view): add method print statistics
    Added methods:
    -print statistics
    -print rank
    -print rate of return

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,6 +3,8 @@ package lotto.view;
 import java.util.List;
 import java.util.stream.Collectors;
 import lotto.model.Lotto;
+import lotto.model.Rank;
+import lotto.model.Result;
 
 public class OutputView {
     private static OutputView instance = new OutputView();
@@ -15,6 +17,27 @@ public class OutputView {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers().stream().sorted().collect(Collectors.toList()));
         }
+    }
+
+    public void printStatistics(Result result) {
+        System.out.println(OutputMessage.STATISTICS.message());
+        printRank(result);
+        printRateOfReturn(result);
+    }
+
+    private void printRank(Result result) {
+        for (Rank rank : Rank.values()) {
+            String format = OutputMessage.RESULT.message();
+            if (rank.bonus()) {
+                format = OutputMessage.RESULT_BONUS.message();
+            }
+
+            System.out.printf(format, rank.common(), rank.prise(), result.getRankResult(rank));
+        }
+    }
+
+    private void printRateOfReturn(Result result) {
+        System.out.printf(OutputMessage.RESULT_ROT.message(), result.getRateOfReturn());
     }
 
     public static OutputView getInstance() {
