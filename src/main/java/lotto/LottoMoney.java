def247930441f59409d0a74305d09f54343608e4
juno-junho <ssmm0205@naver.com>
2022-11-15 19:55:17 +0900

    feat(lotto): add constructor
    LottoMoney 클래스에 생성자 추가

--- src/main/java/lotto/LottoMoney.java
+++ src/main/java/lotto/LottoMoney.java
@@ -1,4 +1,16 @@
 package lotto;
 
 public class LottoMoney {
+
+    private static final int LOTTERY_PRICE_PER_ONE = 1_000;
+    private final int money;
+
+    private LottoMoney(int money) {
+        validate(money);
+        this.money = money;
+    }
+    public static LottoMoney from(int money) {
+        return new LottoMoney(money);
+    }
+
 }
