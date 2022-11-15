0f08ec24460205073ba61f0bf72e2eada97793fa
ddukddaki <rlfrkdms1@naver.com>
2022-11-15 16:28:40 +0900

    feat(Lotto): (예외 처리)번호가 1~45의 범위를 넘어서는기 검증하는 기능

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,11 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int NUMBER_MIN_RANGE = 1;
+    private static final int NUMBER_MAX_RANGE = 45;
+    private static final String ERR_WRONG_NUMBER_RANGE = "[ERROR] 당첨 번호는 1~45 숫자여야 합니다. ";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,5 +21,9 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void validateNumberInRange(int number) {
+        if (number < NUMBER_MIN_RANGE || NUMBER_MAX_RANGE < number) {
+            throw new IllegalArgumentException(ERR_WRONG_NUMBER_RANGE);
+        }
+    }
 }
