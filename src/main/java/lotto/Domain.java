393e9ec948d6ce1e5989ae8965c81efa79c96487
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-15 17:03:20 +0900

    feat: 로또 객체 생성 후 로또 번호를 랜덤으로 생성하는 기능 추가 createLottoTicket(): 1~45 사이의 서로다른 임의의 수를 6개 만큼 생성하여 배열에 저장

--- src/main/java/lotto/Domain.java
+++ src/main/java/lotto/Domain.java
@@ -10,6 +10,9 @@ public class Domain {
     private static final String INPUT_LOTTO_AMOUNT = "로또금액을 입력해 주세요.";
 
     private static final int TICKET_PRICE = 1000;
+    private static final int MAX_VALUE_OF_LOTTO_NUMBER = 45;
+    private static final int MIN_VALUE_OF_LOTTO_NUMBER = 1;
+    private static final int LOTTO_COUNT = 6;
 
     private static int lottoAmount;
     private static int ticketNumbers;
@@ -30,5 +33,11 @@ public class Domain {
     // TODO: TICKET_NUMBERS 변수를 초기화 한다.
     private static void initializationTicketNumbers(){
         ticketNumbers = lottoAmount / TICKET_PRICE;
+    }
+
+    // TODO: 로또 객체 생성 후 로또 번호를 랜덤으로 생성
+    private static void createLottoTicket(){
+        LottoList.add(new Lotto(Randoms.pickUniqueNumbersInRange(
+                MIN_VALUE_OF_LOTTO_NUMBER, MAX_VALUE_OF_LOTTO_NUMBER, LOTTO_COUNT)));
     }
 }
