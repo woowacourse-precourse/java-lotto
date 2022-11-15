efe93ca420bf19e798e8ffac84b3e084e282bfad
mihye126 <mihea12@gmail.com>
2022-11-15 22:13:52 +0900

    [feat] readLine에 대해서 validationTest 진행

--- src/main/java/lotto/util/InputUtility.java
+++ src/main/java/lotto/util/InputUtility.java
@@ -1,6 +1,9 @@
 package lotto.util;
 
 
+import static lotto.util.ValidationUtility.validationTest;
+
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -8,6 +11,12 @@ import java.util.stream.Stream;
 public class InputUtility {
 
     private InputUtility(){}
+
+    public static String readLine(int type) {
+        String line = Console.readLine();
+        validationTest(line, type);
+        return line;
+    }
 
     public static List<Integer> parseLotto(String input) {
         return Stream.of(input.split(","))
---
+++ src/main/java/lotto/util/ValidationUtility.java
@@ -0,0 +1,21 @@
+package lotto.util;
+
+
+public class ValidationUtility {
+    private ValidationUtility() {}
+
+    protected static void validationTest(String input, int type) {
+        if (type == Type.AMOUNT.getValue()) {
+
+        }
+
+        if (type == Type.LOTTO.getValue()) {
+
+        }
+        if (type == Type.BONUS.getValue()) {
+
+        }
+
+    }
+
+}
