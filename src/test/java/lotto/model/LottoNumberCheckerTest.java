372074964798932a8a3d4f5328fd58e73d6e5a92
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-15 12:53:20 +0900

    test<LottoNumberCheckerTest>: 손님 로또 당첨 결과 총 상금 테스트 코드 작성

---
+++ src/test/java/lotto/model/LottoNumberCheckerTest.java
@@ -0,0 +1,34 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoNumberCheckerTest {
+    Set<LottoNumber> makeLotto(int... numbers) {
+        return Arrays.stream(numbers)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toSet());
+    }
+
+    @DisplayName("3등과 5등 보너스 당첨")
+    @Test
+    void createMatchResult() {
+        Lotto winningLotto = new Lotto(makeLotto(1, 2, 3, 4, 5, 6));
+        LottoNumber bonusNumber = new LottoNumber(7);
+        List<Lotto> lottos = List.of(
+                new Lotto(makeLotto(1, 2, 3, 8, 9, 10)),
+                new Lotto(makeLotto(1, 2, 3, 4, 5, 7))
+        );
+        LottoNumberChecker lottoNumberChecker = new LottoNumberChecker(winningLotto, bonusNumber, lottos);
+
+        MatchResult matchingLottoResult = lottoNumberChecker.resultOfLotteries();
+        assertThat(matchingLottoResult.getTotalPrizeMoney())
+                .isEqualTo(MatchType.THREE.getPrizeMoney() + MatchType.FIVE_BONUS.getPrizeMoney());
+    }
+}
