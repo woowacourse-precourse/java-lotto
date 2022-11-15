42e44094b46a6f923d87ec2904e52e9f9564e2e9
3juhwan <13selfesteem91@naver.com>
2022-11-15 20:06:20 +0900

    test(lotto): 테스트 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -18,32 +18,29 @@ public class InputView {
             }
             return amount;
         } catch (NumberFormatException ex) {
-            System.out.println("[ERROR] 숫자를 입력해주세요. ");
+            System.out.println("[ERROR] 금액을 정확히 입력해주세요. ");
             throw new NoSuchElementException();
         }
     }
 
     public static List<Integer> getWinningNumber(String input) {
-        try {
-            List<Integer> winningNumber = Arrays.stream(input.split(","))
-                    .map(number -> Integer.valueOf(number))
-                    .collect(Collectors.toList());
-            for (Integer number : winningNumber) {
-                if (number < 1 || number > 45) {
-                    System.out.println("[ERROR] 1~45 사이의 값을 입력해주세요. ");
-                    throw new IllegalArgumentException();
-                }
-            }
-            return winningNumber;
-        } catch (NumberFormatException ex) {
-            System.out.println("[ERROR] 잘못된 값이 입력됐습니다. ");
+        List<Integer> winningNumber = splitStringByComma(input);
+
+        if (winningNumber.size() != 6) {
+            System.out.println("[ERROR] 숫자 6개만 입력해주세요. ");
             throw new IllegalArgumentException();
         }
+        for (Integer number : winningNumber) {
+            validateInRange(number);
+        }
+        return winningNumber;
     }
 
     public static Integer getBonusNumber(String input) {
         try {
-            return Integer.valueOf(input);
+            Integer inputNumber = Integer.valueOf(input);
+            validateInRange(inputNumber);
+            return inputNumber;
         } catch (NumberFormatException ex) {
             System.out.println("[ERROR] 숫자가 아닌 값이 입력됐습니다. ");
             throw new IllegalArgumentException();
@@ -60,5 +57,23 @@ public class InputView {
 
     public static Integer getBonusNumber() {
         return getBonusNumber(readLine());
+    }
+
+    private static List<Integer> splitStringByComma(String string) {
+        try {
+            return Arrays.stream(string.split(","))
+                    .map(number -> Integer.valueOf(number))
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException ex) {
+            System.out.println("[ERROR] 금액을 정확히 입력해주세요. ");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateInRange(Integer number) {
+        if (number < 1 || number > 45) {
+            System.out.println("[ERROR] 1~45 사이의 값을 입력해주세요. ");
+            throw new IllegalArgumentException();
+        }
     }
 }
