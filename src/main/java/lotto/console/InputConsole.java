7f98949d0683dfcce167766f04cb0131ad443c0b
sujin0607 <goweg0607@gmail.com>
2022-11-15 23:38:17 +0900

    refactor: 간격 조정

--- src/main/java/lotto/console/InputConsole.java
+++ src/main/java/lotto/console/InputConsole.java
@@ -7,17 +7,17 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputConsole {
 
-    public String Money(){
+    public String Money() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
-    public String Number(){
+    public String Number() {
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
-    public String bonusNum(){
+    public String bonusNum() {
         System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
