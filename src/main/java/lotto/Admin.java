b547d56cc5b0cb8ce40ce38c563d0ae7bfb0b764
Basak <chgy2131@naver.com>
2022-11-15 14:02:38 +0900

    feat(Admin): 당첨 번호와 보너스 번호를 각각입력받는 기능 추가
    inputWinningNumbers
    - 입력된 번호는 쉼표를 기준으로 구분한다.
    - 입력받은 String을 int로 변환한다.
    -
    당첨 번호 입력시 안내메세지가 표시된다.
    - 숫자와 쉼표만 입력으로 허용된다.
    
    inputBounusNumber
    - 보너스 번호를
    입력받는다.
    - 숫자만 입력으로 허용된다.

--- src/main/java/lotto/Admin.java
+++ src/main/java/lotto/Admin.java
@@ -1,32 +1,73 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Admin {
     public void inputGameNumbers() {
     }
 
-    private void inputWinningNumbers() {
+    // 당첨 번호를 입력 받는다.
+    private Lotto inputWinningNumbers() {
+        printInputWinningNumbers();
+        String input = Console.readLine();
+        validateWinningNumbersInput(input);
+        List<Integer> numbers = inputToNumbers(input);
+        return new Lotto(numbers);
     }
 
-    private void splitSeparator() {
+    private void validateWinningNumbersInput(String input) {
+        if (!input.matches("^[0-9|,]+$")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private List<Integer> inputToNumbers(String input) {
+        List<String> stringInput = splitSeparator(input);
+        return stringToIntegerList(stringInput);
+    }
+
+    private List<String> splitSeparator(String input) {
+        List<String> input2 = List.of(input.split(","));
+        return input2;
+    }
+
+    private List<Integer> stringToIntegerList(List<String> input) {
+        List<Integer> numbers = new ArrayList<Integer>();
+        for(String s : input) {
+            int number = Integer.parseInt(s);
+            numbers.add(number);
+        }
+        return numbers;
     }
 
     private void printInputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해주세요.");
     }
 
-    private void inputBonusNumber() {
+    // 보너스 번호를 입력받는다.
+    private int inputBonusNumber() {
+        printInputBonusNumber();
+        String input = Console.readLine();
+        validateBonusNumber(input);
+        return Integer.parseInt(input);
     }
 
     private void printInputBonusNumber() {
+        System.out.println("보너스 번호를 입력해주세요.");
     }
 
     private void validateGameNumbers() {
     }
 
-    private void validateWinningNumbers() {
-    }
-
-    private void validateBonusNumber() {
+    private void validateBonusNumber(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
+        if (!input.matches("^[0-9]+$")) {
+            throw new IllegalArgumentException();
+        }
     }
 }
