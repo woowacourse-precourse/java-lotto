8832aa3ae89cde9781651fafa9de006043464c63
taekwon-dev <taekwon_dev@naver.com>
2022-11-15 11:09:55 +0900

    feat: (Application) 로또 프로그램 시작 포인트

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.Judgement;
+import lotto.domain.Statistics;
+import lotto.view.print.MessagePrinter;
+import lotto.view.scanner.NumberScanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController(
+                new NumberScanner(),
+                new MessagePrinter(),
+                new Statistics(),
+                new Judgement()
+        );
+        lottoController.start();
     }
 }
