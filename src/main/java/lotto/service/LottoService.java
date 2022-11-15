e4833da252870ec13dbdb52b18bc2bf62cafce56
Jeeseob <jeeseob5761@gmail.com>
2022-11-15 16:13:09 +0900

    refactor: 로또 구매개수 반환 함수 수정

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -11,25 +11,29 @@ public class LottoService {
     public int toPurchaseAmount(String input) {
         validPurchaseAmount(input);
         int purchaseAmount = toInteger(input);
-        isDivideThousand(purchaseAmount);
-        return purchaseAmount / LottoConstant.LOTTO_PRICE.getValue();
+        return divideLottoPrice(purchaseAmount);
     }
 
     private void validPurchaseAmount(String input) {
-        isNumeric(input);
+        checkNumeric(input);
     }
 
     private int toInteger(String input) {
         return Integer.parseInt(input);
     }
+    
+    private int divideLottoPrice(int purchaseAmount) {
+        checkDivideLottoPrice(purchaseAmount);
+        return purchaseAmount / LottoConstant.LOTTO_PRICE.getValue();
+    }
 
-    private void isDivideThousand(int purchaseAmount) {
+    private void checkDivideLottoPrice(int purchaseAmount) {
         if (purchaseAmount % LottoConstant.LOTTO_PRICE.getValue() != 0) {
             throw new IllegalArgumentException(ArgumentExceptionMessage.INPUT_PURCHASE_AMOUNT_UNCONFORMABLE_VALUE.getMessage());   // 구매 금액이 1000원 단위가 아닌 경우
         }
     }
 
-    private void isNumeric(String input) {
+    private void checkNumeric(String input) {
         if (!input.matches("[1-9]+[0-9]*")) {
             throw new IllegalArgumentException(ArgumentExceptionMessage.INPUT_NOT_INTEGER.getMessage());   // 입력받은 값이 정수로 표현되지 않는 경우
         }
