4d9dd85de0370c4701e30f98e0da9d2fae7105cc
limecats0331 <limecats0331@gmail.com>
2022-11-15 09:13:09 +0900

    refactor(Manager): 숫자를 상수로 변경
    의미가 있는 숫자들을 상수로 변경

--- src/main/java/lotto/Manager.java
+++ src/main/java/lotto/Manager.java
@@ -7,11 +7,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Manager {
+    private static final int START_RANGE = 1;
+    private static final int END_RANGE = 45;
+    private static final int LOTTO_LENGTH = 6;
+    private static final int MIN_MATCH = 3;
+    private static final int MAX_MATCH = 7;
+    private static final int PURCHASE_UNIT = 1000;
 
     private Integer purchasePrice;
 
     public void getPurchasePrice(int purchasePrice) {
-        if (purchasePrice % 1000 != 0) {
+        if (purchasePrice % PURCHASE_UNIT != 0) {
             throw new IllegalArgumentException("[ERROR] ");
         }
 
@@ -20,23 +26,23 @@ public class Manager {
 
     public List<List<Integer>> publishPurchaseLottos() {
         List<List<Integer>> purchaseLottos = new ArrayList<>();
-        int publishCount = purchasePrice / 1000;
+        int publishCount = purchasePrice / PURCHASE_UNIT;
 
         for (int i = 0; i < publishCount; i++) {
-            purchaseLottos.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            purchaseLottos.add(Randoms.pickUniqueNumbersInRange(START_RANGE, END_RANGE, LOTTO_LENGTH));
         }
 
         return purchaseLottos;
     }
 
     public int[] findMatchCount(List<Integer> lotto, List<List<Integer>> purchaseLottos) {
-        int[] matchCounts = new int[7];
+        int[] matchCounts = new int[MAX_MATCH];
 
         purchaseLottos.stream()
                 .mapToInt(purchaseLotto -> (int) purchaseLotto.stream().filter(lotto::contains).count())
                 .forEach(matchCount -> matchCounts[matchCount] += 1);
 
-        return Arrays.copyOfRange(matchCounts, 3, 7);
+        return Arrays.copyOfRange(matchCounts, MIN_MATCH, MAX_MATCH);
     }
 
 }
