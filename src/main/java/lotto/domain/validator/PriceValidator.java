049ceac095da2b130928b5055f5347fc8de5d190
yummygyudon <bang2brew@gmail.com>
2022-11-15 19:20:58 +0900

    FIX(domain): 금액 입력값 유효성 검사 조건문 수정
    1. 수정 내용
      - parseInt 수행 및 해당 작업에 의한 NumberFormatException을 잡아내어
      IllegalArgumentException을 발생시키는 코드를
      조건식 형태로 변경하여 처리
    2. 수정 이유
      - 본래 발생하는 예외가 발생되지 않고 의도적인 타 예외를 발생시키는 것에 대한 효율성 고민

--- src/main/java/lotto/domain/validator/PriceValidator.java
+++ src/main/java/lotto/domain/validator/PriceValidator.java
@@ -3,27 +3,28 @@ package lotto.domain.validator;
 import lotto.util.ErrorMessages;
 
 public class PriceValidator {
-    private static final int REF_MONEY_AMOUNT = 1_000 ;
+    private static final int REF_MONEY_AMOUNT = 1_000;
 
-    public void validateMoneyInput(String inputValue){
-        try{
-            final int inputMoney = Integer.parseInt(inputValue.replace(",",""));
-            checkingPurchasable(inputMoney);
-            checkingDividable(inputMoney);
-        }catch(NumberFormatException e){
+    public void validateMoneyInput(String inputValue) {
+        if (!inputValue.matches("[0-9]+")) {
             throw new IllegalArgumentException(ErrorMessages.NOT_NUMBER_EXIST.getMessage());
         }
-
+        if (inputValue.contains(",")){
+            inputValue = inputValue.replace(",","");
+        }
+        long inputMoney = Long.parseLong(inputValue);
+        checkingPurchasable(inputMoney);
+        checkingDividable(inputMoney);
     }
 
-    public void checkingPurchasable(int inputMoney){
-        if (inputMoney < REF_MONEY_AMOUNT){
+    public void checkingPurchasable(long inputMoney) {
+        if (inputMoney < REF_MONEY_AMOUNT) {
             throw new IllegalArgumentException(ErrorMessages.BELOW_THE_PRICE_THRESHOLD.getMessage());
         }
     }
 
-    public void checkingDividable(int inputMoney){
-        if ((inputMoney % REF_MONEY_AMOUNT) != 0){
+    public void checkingDividable(long inputMoney) {
+        if ((inputMoney % REF_MONEY_AMOUNT) != 0) {
             throw new IllegalArgumentException(ErrorMessages.MONEY_UNDIVIDED.getMessage());
         }
     }
