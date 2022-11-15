63e0cc8770b0a5d3e747763b7e4937bad297d7cd
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-15 12:06:34 +0900

    test(CompareTest) : 당첨번호와 로또번호 일치하는 개수 확인

--- src/test/java/lotto/CompareTest.java
+++ src/test/java/lotto/CompareTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class CompareTest {
@@ -21,5 +22,11 @@ public class CompareTest {
                     Compare.getIntegratedWinningNumber(List.of(1,2,3,4,4,6), List.of(7));
                 })
                 .withMessage("[ERROR] 당첨번호와 보너스번호 내 중복이 있습니다.");
+    }
+
+    @DisplayName("당첨번호와 로또번호 일치하는 개수 확인")
+    @Test
+    void 번호일치_테스트() {
+        assertThat(Compare.compareWithWinningNumber(List.of(1,2,3,4,5,6),List.of(1,3,5,7,24,30))).isEqualTo(3);
     }
 }
