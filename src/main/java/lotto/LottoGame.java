e89ca7be1a704a5795046951f32c87f7015b50ba
Hiluxy <sunset.lhj@gmail.com>
2022-11-15 22:04:23 +0900

    feat 금액 입력 함수 구현

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,16 @@
+package lotto;
+
+public class LottoGame {
+
+    private final Message message = new Message();
+    private final ProcessInput processInput = new ProcessInput();
+    public void start() {
+        try {
+            //
+        } catch (IllegalArgumentException e) {
+            //
+        }
+    }
+
+
+}
