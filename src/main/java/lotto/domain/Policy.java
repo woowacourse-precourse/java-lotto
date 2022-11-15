eed8c84b52934c68270f0451bc8a94546a06244d
ryanha <ryanha@handong.ac.kr>
2022-11-15 22:37:49 +0900

    refactor(domain): Policy에 정의된 enums 중 불필요한 요소들 삭제

--- src/main/java/lotto/domain/Policy.java
+++ src/main/java/lotto/domain/Policy.java
@@ -4,28 +4,18 @@ public class Policy {
 
     public static final int PRICE = 1000; // 로또 한장당 가격
 
-    /*
-     * 당첨기준에 대한 정의
-     */
-    public enum WinningCriteria {
-        WINNING_ONE(1, 6, 0),
-        WINNING_TWO(2, 5, 1),
-        WINNING_THREE(3, 5, 0),
-        WINNING_FOUR(4, 4, 0),
-        WINNING_FIVE(5, 3, 0);
-
-        private final int rank; // 순위
+    public enum WinningCriteria { // 당첨기준
+        WINNING_FIVE(3, 0),
+        WINNING_FOUR(4, 0),
+        WINNING_THREE(5, 0),
+        WINNING_TWO(5, 1),
+        WINNING_ONE(6, 0);
         private final int matchingCount; // 당첨번호와 일치하는 수
         private final int bonusNumberCount; // 보너스 번호 일치여부 (-1: 의미없음, 0: 일치X, 1: 일치)
 
-        WinningCriteria(int rank, int matchingCount, int bonusNumberCount) {
-            this.rank = rank;
+        WinningCriteria(int matchingCount, int bonusNumberCount) {
             this.matchingCount = matchingCount;
             this.bonusNumberCount = bonusNumberCount;
-        }
-
-        public int getRank() {
-            return this.rank;
         }
 
         public int getMatchingCount() {
@@ -47,21 +37,16 @@ public class Policy {
         }
     }
 
-    /*
-     * 각 등수별 당첨금액에 대한 정의
-     */
-    public enum WinningAmount {
-        WINNING_ONE("WINNING_ONE", 2000000000),
-        WINNING_TWO("WINNING_TWO", 30000000),
-        WINNING_THREE("WINNING_THREE", 1500000),
-        WINNING_FOUR("WINNING_FOUR", 50000),
-        WINNING_FIVE("WINNING_FIVE", 5000);
+    public enum WinningAmount { // 각 등수별 당첨금액
+        WINNING_FIVE(5000),
+        WINNING_FOUR(50000),
+        WINNING_THREE(1500000),
+        WINNING_TWO(30000000),
+        WINNING_ONE(2000000000);
 
-        private final String gradeName;
         private final long amount;
 
-        WinningAmount(String gradeName, long amount) {
-            this.gradeName = gradeName;
+        WinningAmount(long amount) {
             this.amount = amount;
         }
 
