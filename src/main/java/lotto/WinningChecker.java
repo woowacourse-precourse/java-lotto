110db28688d1d43a38706e74274e4aa6d6a06563
Hyoungwoo <dlguddn3024@gmail.com>
2022-11-15 22:13:46 +0900

    Fix: WinningChecker 클래스에 당첨된 횟수를 세어주는 메소드 구현

--- src/main/java/lotto/WinningChecker.java
+++ src/main/java/lotto/WinningChecker.java
@@ -1,9 +1,13 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
 public class WinningChecker {
+
+    private final HashMap<String, Integer> winnigCount = new HashMap<>();
+
 
     public WinningChecker() {
 
@@ -23,30 +27,26 @@ public class WinningChecker {
         return count;
     }
 
-    private Optional<Winning> checkPrize(List<Integer> lotto, List<Integer> winnigNumber, int bonusNumber) {
+    private HashMap<String, Integer> checkWinningPaperCount(List<Integer> lotto, List<Integer> winnigNumber, int bonusNumber) {
         int checkWinning = checkDuplicatedNumber(lotto, winnigNumber, bonusNumber);
 
         if (checkWinning == 3) {
-            return Optional.of(Winning.FIFTH);
+            winnigCount.put("5등", winnigCount.getOrDefault("FIFTH", 0) + 1);
         }
         if (checkWinning == 4) {
-            return Winning.FOURTH;
+            winnigCount.put("4등", winnigCount.getOrDefault("FOURTH", 0) + 1);
         }
         if (checkWinning == 5) {
-            return Winning.THIRD;
+            winnigCount.put("3등", winnigCount.getOrDefault("THIRD", 0) + 1);
         }
         if (checkWinning == 6 && lotto.contains(bonusNumber)) {
-            return Winning.SECOND;
+            winnigCount.put("2등", winnigCount.getOrDefault("SECOND", 0) + 1);
         }
         if (checkWinning == 6 && !lotto.contains(bonusNumber)) {
-            return Winning.FIRST;
+            winnigCount.put("1등", winnigCount.getOrDefault("FIRST", 0) + 1);
         }
 
-        Optional<Winning> optionalWinning = !!!!!;
-        if (optionalWinning.isPresent()) {
-            Winning winning = optionalWinning.get();
-        }
-
-        return Optional.empty();
+        return winnigCount;
     }
+
 }
