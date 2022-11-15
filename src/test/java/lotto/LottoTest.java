0222acdceb3de92b05ac16232153461ccfd47a9f
kwang <shinkwang@yonsei.ac.kr>
2022-11-15 20:22:30 +0900

    test: Make test for validation about range of lottery numbers
    로또 번호의 범위에 대한 테스트를 만든다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,4 +24,20 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호 중 1개라도 1부터 45 사이의 범위를 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRangeNumber(){
+        //given
+        List<Integer> hasZeroList = List.of(0, 1 ,2, 3, 4, 5);
+        List<Integer> hasOutOfMinimumList = List.of(1, -5 ,2, 3, 4, 5);
+        List<Integer> hasOutOfMaximumList = List.of(1, 46 ,2, 3, 4, 5);
+
+        //when, then
+        assertThatThrownBy(() -> new Lotto(hasZeroList))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("[ERROR] 로또 숫자의 범위는 1부터 45 사이의 숫자여야 합니다.");
+        assertThatThrownBy(() -> new Lotto(hasOutOfMinimumList))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("[ERROR] 로또 숫자의 범위는 1부터 45 사이의 숫자여야 합니다.");
+        assertThatThrownBy(() -> new Lotto(hasOutOfMaximumList))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("[ERROR] 로또 숫자의 범위는 1부터 45 사이의 숫자여야 합니다.");
+    }
 }
