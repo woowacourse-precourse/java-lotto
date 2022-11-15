e80c5865470864e321ffc134023de46b1853a3b7
이건회 <rg970604@naver.com>
2022-11-15 18:44:14 +0900

    style(ProfitCalculatorTest.java) : 코드 컨벤션 리포맷팅

--- src/test/java/lotto/ProfitCalculatorTest.java
+++ src/test/java/lotto/ProfitCalculatorTest.java
@@ -19,10 +19,11 @@ public class ProfitCalculatorTest {
     @DisplayName("1000 지불 후 수익 5천 달성 시 수익률 500")
     @Test
     void profitrate_500_when_pay_1000() {
-        Lotto winningNum = new Lotto(List.of(1,2,3,8,9,10));
-        Lottos lottos = new Lottos(1,new FixedLottoGenerateStrategy());
-        Map<Prize,Integer> prizeBoard = LottoMachine.checkPrizes(winningNum,lottos,new Bonus(7,winningNum));
-        float profitRate = ProfitCalculator.calculateProfitRate(prizeBoard,new Money(1000));
+        Lotto winningNum = new Lotto(List.of(1, 2, 3, 8, 9, 10));
+        Lottos lottos = new Lottos(1, new FixedLottoGenerateStrategy());
+        Map<Prize, Integer> prizeBoard = LottoMachine.checkPrizes(winningNum, lottos,
+            new Bonus(7, winningNum));
+        float profitRate = ProfitCalculator.calculateProfitRate(prizeBoard, new Money(1000));
         Assertions.assertThat(profitRate).isEqualTo(500f);
     }
 
