377c2049b119ce3e452c8dfe743dff9dcf6aa7f1
jw0293 <k38836919@gmail.com>
2022-11-15 18:40:24 +0900

    feat : Print Result Message

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,2 +1,30 @@
-package lotto;public class LottoResult {
+package lotto;
+
+public class LottoResult {
+
+    private int totalProceeds;
+    private MatchCount matchCount;
+    private static final String INCOME_RESULT = "총 수익률은 ";
+    private static final String END_SENTENCE = "%입니다.";
+
+    LottoResult(){
+        totalProceeds = 0;
+    }
+
+    public void getLottoResult(int[] winningInformation, int userPrice){
+        for(int i=0;i<5;i++){
+            MatchCount matchInformation = matchCount.valueOfNumber(i);
+
+            String label = matchInformation.label();
+            totalProceeds += sumWinningCost(matchInformation, winningInformation[i]);
+
+            System.out.println(label + winningInformation[i] + "개");
+        }
+        double rate = ((double)totalProceeds / (double)userPrice) * 100;
+        System.out.println(INCOME_RESULT + String.format("%.1f", rate) + END_SENTENCE);
+    }
+
+    private int sumWinningCost(MatchCount information, int matchCnt){
+        return information.proceeds() * matchCnt;
+    }
 }
