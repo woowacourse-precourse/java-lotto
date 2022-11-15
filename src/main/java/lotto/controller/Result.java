ecc05de40a00ba43a8b68546abd5d3731647649b
saehim <saeilee.vxv@gmail.com>
2022-11-15 23:00:22 +0900

    feat(lotto) : 수익률 구하기 메서드 구현

--- src/main/java/lotto/controller/Result.java
+++ src/main/java/lotto/controller/Result.java
@@ -3,11 +3,10 @@ package lotto.controller;
 import lotto.domain.Lotto;
 import lotto.domain.Win;
 
-import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
-public class Compare {
+public class Result {
     public static void compare(Lotto win, Lotto lotto){
         for (int number : lotto.getNumbers()) {
             if (win.getNumbers().contains(number)) {
@@ -24,11 +23,20 @@ public class Compare {
     public static Map<Win, Integer> countWins(Lotto winLotto, int bonusNum, List<Lotto> lottos) {
         Win.initWins();
         for (Lotto lotto : lottos) {
-            Compare.compare(winLotto, lotto);
-            Compare.compareBonus(bonusNum, lotto);
+            Result.compare(winLotto, lotto);
+            Result.compareBonus(bonusNum, lotto);
             Win win = Win.getWin(lotto.getCorrectCount(), lotto.isBonus());
             win.plusWin(win);
         }
         return Win.getWins();
+    }
+
+    /** 수익률 */
+    public static double winningsRate(int input, Map<Win,Integer> wins) {
+        double sum = 0;
+        for (Win win : wins.keySet()) {
+            sum +=  win.getWinnings() * wins.get(win);
+        }
+        return sum/input;
     }
 }
