5b2c43de5495b79d1a9f247d60616ecc6e5956d4
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-15 22:41:43 +0900

    feat: checkLottoPriceDividedByOneThousand() 예외처리 메서드 추가

--- src/main/java/lotto/exception/UserException.java
+++ src/main/java/lotto/exception/UserException.java
@@ -11,6 +11,15 @@ public class UserException {
         checkLottoPriceDividedByOneThousand(userLottoPrice);
     }
 
+    public void checkLottoPriceDividedByOneThousand(String userLottoPrice) {
+        if ((Integer.parseInt(userLottoPrice)) % Constant.LOTTO_ONE_PRICE != 0
+                || (Integer.parseInt(userLottoPrice)) == 0 || Integer.parseInt(userLottoPrice) < 0) {
+            System.out.println(Constant.ERROR_MESSAGE + " 구매 단위는 1000으로 나누어 떨어져야 합니다. ");
+            throw new NoSuchElementException();
+        }
+    }
+
+
 
 
 }
