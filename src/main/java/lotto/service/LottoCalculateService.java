1fffacc7189efa9d8cc15ac79ebf35df51626fcb
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-15 22:44:00 +0900

    feat: getTotalPrice() 메서드 추가

--- src/main/java/lotto/service/LottoCalculateService.java
+++ src/main/java/lotto/service/LottoCalculateService.java
@@ -23,5 +23,40 @@ public class LottoCalculateService {
         outputView.printEarningRateOfLottoMessage(getEarningRate(user));
     }
 
+    public Long getTotalPrice() {
+        Long totalPrice = 0L;
+        for (Rank rank : rankMap.keySet()) {
+            if (rank == Rank.THIRD) {
+                int count = rankMap.get(rank);
+                totalPrice += (count) * 5000;
+                continue;
+            }
+
+            if (rank == Rank.FOURTH) {
+                int count = rankMap.get(rank);
+                totalPrice += (count) * 50000;
+                continue;
+            }
+
+            if (rank == Rank.FIVE) {
+                int count = rankMap.get(rank);
+                totalPrice += (count) * 1500000;
+                continue;
+            }
+
+            if (rank == Rank.FIVE_WITH_BONUS) {
+                int count = rankMap.get(rank);
+                totalPrice += (count) * 30000000;
+                continue;
+            }
+
+            if (rank == Rank.SIX) {
+                int count = rankMap.get(rank);
+                totalPrice += (count) * 2000000000;
+            }
+        }
+        return totalPrice;
+    }
+
 
 }
