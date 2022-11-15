c5fbac9d461c53220f4d2d1f1afcfb604577a024
kimJaeHyeon <kmr2644@gmail.com>
2022-11-15 17:09:11 +0900

    refactor: 로또번호에 대한 검증메서드를 LottoValidator 로 분리

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -2,11 +2,16 @@ package lotto.validator;
 
 import java.util.List;
 import lotto.constant.LottoConstant;
+import lotto.domain.Money;
 
 public class LottoValidator {
+    public static void validateLottoSize(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != LottoConstant.WINNING_NUMBER_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 당청번호의 갯수는 6개입니다.");
+        }
+    }
 
-    public static void validateWinningNumber(List<Integer> lottoNumbers) {
-        validateDuplicateNumber(lottoNumbers);
+    public static void validateNumberRange(List<Integer> lottoNumbers) {
         for (Integer integer : lottoNumbers) {
             if (integer > LottoConstant.MAX_LOTTO_NUMBER ||
                     integer < LottoConstant.LOW_LOTTO_NUMBER) {
@@ -14,7 +19,14 @@ public class LottoValidator {
             }
         }
     }
-    private static void validateDuplicateNumber(List<Integer> lottoNumbers) {
+
+    public static void validateDuplicateWinningAndBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 중복된 정수를 입력하면 안됩니다.");
+        }
+    }
+
+    public static void validateDuplicateNumber(List<Integer> lottoNumbers) {
         long count = lottoNumbers.stream()
                 .distinct()
                 .count();
@@ -22,12 +34,11 @@ public class LottoValidator {
             throw new IllegalArgumentException("[ERROR] 중복된 정수를 입력하면 안됩니다.");
         }
     }
-    public static void validateHasNumber(List<String> list) {
-        String regex = "[0-9]+";
-        for (String number : list) {
-            if (!number.matches(regex)) {
-                throw new IllegalArgumentException("[ERROR] 정수만 입력해야 합니다.");
-            }
+
+    public static void validateLottoMoney(Money money) {
+        if (!money.checkAmountUnit(LottoConstant.AMOUNT_UNIT)) {
+            throw new IllegalArgumentException("[ERROR] 입력금액은 1000원 단위여야 합니다.");
         }
     }
+
 }
