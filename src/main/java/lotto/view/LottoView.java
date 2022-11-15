00a528a1ff4505d76813c3691c90d28f4513c883
dev-green-flamingo <devgflamingo@gmail.com>
2022-11-15 20:58:46 +0900

    chore(output): 당첨 통계 출력 형식 변경
    당첨 통계 뒤에 '개'를 출력하는 부분이 빠져 이를 반영한다.

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -92,13 +92,13 @@ public class LottoView {
         double earningRate = controller.generateEarningsRate(lotteries, luckyLotto, luckyNumber);
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.printf("3개 일치 (5,000원) - %d\n", rankingToCount.get(RANKING.FIFTH));
-        System.out.printf("4개 일치 (50,000원) - %d\n", rankingToCount.get(RANKING.FORTH));
-        System.out.printf("5개 일치 (1,500,000원) - %d\n", rankingToCount.get(RANKING.THIRD));
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d\n",
+        System.out.printf("3개 일치 (5,000원) - %d개%n", rankingToCount.get(RANKING.FIFTH));
+        System.out.printf("4개 일치 (50,000원) - %d개%n", rankingToCount.get(RANKING.FORTH));
+        System.out.printf("5개 일치 (1,500,000원) - %d개%n", rankingToCount.get(RANKING.THIRD));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개%n",
             rankingToCount.get(RANKING.SECOND));
-        System.out.printf("6개 일치 (2,000,000,000원) - %d\n", rankingToCount.get(RANKING.FIRST));
-        System.out.printf("총 수익률은 %.1f%% 입니다.\n", earningRate);
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개%n", rankingToCount.get(RANKING.FIRST));
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", earningRate);
     }
 
     private Map<RANKING, Integer> createRankingToCount(final List<RANKING> rankings) {
