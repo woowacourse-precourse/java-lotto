cf585abc026d63f305dc3a9651948a1d1cb85f15
ESeokhwan <huan99@hanmail.net>
2022-11-15 11:11:00 +0900

    feat: 로또 게임의 전체적인 과정을 컨트롤하는 기능 추가

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,7 +1,68 @@
 package lotto.controller;
 
+import lotto.model.lotto.Lotto;
+import lotto.model.lotto.LottoGenerator;
+import lotto.model.payment.Payment;
+import lotto.model.statistics.LottoResult;
+import lotto.model.statistics.LottoStatistics;
+import lotto.model.statistics.LottoStatisticsGenerator;
+import lotto.model.winningnumbers.WinningNumbers;
+import lotto.view.ConsoleInputView;
+import lotto.view.ConsoleOutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGameController {
+    private final String PUT_PAYMENT_INPUT_ALERT = "구입금액을 입력해 주세요.";
+    private final String PUT_WINNING_NUMBERS_INPUT_ALERT = "당첨 번호를 입력해 주세요.";
+    private final String PUT_BONUS_NUMBER_INPUT_ALERT = "보너스 번호를 입력해 주세요.";
+    private final String STATISTICS_LABEL = "당첨 통계";
+    private final String DIVIDER = "---";
+
+    private final ConsoleInputView inputView;
+    private final ConsoleOutputView outputView;
+
+    public LottoGameController() {
+        inputView = new ConsoleInputView();
+        outputView = new ConsoleOutputView();
+    }
+
     public void runGame() {
-        // TODO 구현 필요
+        outputView.println(PUT_PAYMENT_INPUT_ALERT);
+        int amount = inputView.readInteger();
+        Payment payment = new Payment(amount);
+
+        LottoGenerator lottoGenerator = new LottoGenerator();
+
+        List<Lotto> lottos = new ArrayList<>();
+
+        outputView.println(payment.getLottoCount() + "개를 구매했습니다.");
+        for(int i = 0; i < payment.getLottoCount(); i++) {
+            Lotto tmpLotto = lottoGenerator.autoGenerate();
+            outputView.println(tmpLotto.getNumbers());
+            lottos.add(tmpLotto);
+        }
+
+        outputView.println(PUT_WINNING_NUMBERS_INPUT_ALERT);
+        List<Integer> numbers = inputView.readIntegers();
+
+        outputView.println(PUT_BONUS_NUMBER_INPUT_ALERT);
+        int bonusNumber = inputView.readInteger();
+
+        WinningNumbers winningNumbers = new WinningNumbers(numbers, bonusNumber);
+
+        List<LottoResult> lottoResults = new ArrayList<>();
+        for(Lotto lotto : lottos) {
+            lottoResults.add(winningNumbers.checkLotto(lotto));
+        }
+
+        LottoStatisticsGenerator lottoStatisticsGenerator = new LottoStatisticsGenerator(payment);
+        LottoStatistics lottoStatistics = lottoStatisticsGenerator.generate(lottoResults);
+
+        outputView.println(STATISTICS_LABEL);
+        outputView.println(DIVIDER);
+        outputView.println(lottoStatistics.getHistoryDescription());
+        outputView.println(lottoStatistics.getReturnRateDescription());
     }
 }
