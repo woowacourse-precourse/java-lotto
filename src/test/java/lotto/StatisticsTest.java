2cc0aaf21b045f0d98a11b0eaa02f2f0feae199f
Sangin <ksiisk99@gmail.com>
2022-11-15 21:17:41 +0900

    test: 수익률 계산

--- src/test/java/lotto/StatisticsTest.java
+++ src/test/java/lotto/StatisticsTest.java
@@ -52,4 +52,17 @@ public class StatisticsTest {
         assertEquals(5, actual.size());
     }
 
+    @DisplayName("구매자가 로또로 얻은 수익률을 계산한다.")
+    @Test
+    public void 수익률_계산() {
+        //given
+        statistics.countWinningLotto(lottos, winningLottoNumbers, bonusNumber);
+
+        //when
+        double rateReturn = statistics.computeRateReturn(2031555000);
+
+        //then
+        assertEquals(100, rateReturn);
+    }
+
 }
