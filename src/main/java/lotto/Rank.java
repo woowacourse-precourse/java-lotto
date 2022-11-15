bfe1c88df8aae4c54bb099560df0ca1fd600d6f7
신채원 <cw121@naver.com>
2022-11-15 18:58:10 +0900

    feat: enum 변수 추가

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -5,21 +5,23 @@ import java.util.List;
 
 public enum Rank {
 
-    LOSER(0, false, 0),
-    FIFTH(3, false, 5000),
-    FOURTH(4, false, 50000),
-    THIRD(5, false, 1500000),
-    SECOND(5, true, 30000000),
-    FIRST(6, false, 2000000000);
+    LOSER(0, false, 0, "0"),
+    FIFTH(3, false, 5000, "5,000"),
+    FOURTH(4, false, 50000, "50,000"),
+    THIRD(5, false, 1500000, "1,500,000"),
+    SECOND(5, true, 30000000, "30,000,000"),
+    FIRST(6, false, 2000000000, "2,000,000,000");
 
     private final int matchCount;
     private final boolean isBonus;
     private final int winMoney;
+    private final String stringWinMoney;
 
-    Rank(int matchCount, boolean isBonus, int winMoney) {
+    Rank(int matchCount, boolean isBonus, int winMoney, String stringWinMoney) {
         this.matchCount = matchCount;
         this.isBonus = isBonus;
         this.winMoney = winMoney;
+        this.stringWinMoney = stringWinMoney;
     }
 
     public static Rank of(int matchCount, boolean isBonus) {
@@ -49,5 +51,9 @@ public enum Rank {
 
     public int getMatchCount() {
         return this.matchCount;
+    }
+
+    public String getStringWinMoney() {
+        return this.stringWinMoney;
     }
 }
