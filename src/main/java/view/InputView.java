03b6ed810757c1d8c43f1979cb5cd8040ea1307e
doehy <dbehxks02@naver.com>
2022-11-15 15:39:11 +0900

    chore : 생략 됐던 [Error]문구 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,7 +2,7 @@ package view;
 
 public class InputView {
     public static final String PUSH_PURCHASING_MONEY = "구입금액을 입력해 주세요.";
-    public static final String SHOW_MONEY_ERROR = "로또 구입 금액을 바르게 입력해주시기 바랍니다.";
+    public static final String SHOW_MONEY_ERROR = "[ERROR] 로또 구입 금액을 바르게 입력해주시기 바랍니다.";
     public static final String PUSH_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     public static final String PUSH_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
