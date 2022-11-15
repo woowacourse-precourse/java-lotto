68fc8d4b31fbdf120fdb581ba602d4fd6eea47ec
dvlpr0 <rladjswkd@naver.com>
2022-11-15 14:17:04 +0900

    test: 보너스 번호 유효성 검사에 대한 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -20,5 +20,14 @@ class LottoTest {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 중복된 숫자거나, [1,45] 범위를 넘어선 숫자면 예외가 발생한다.")
+    @Test
+    void validateBonusNumberTest() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Throwable throwable = catchThrowable(() -> lotto.validateBonusNumber(7));
+
+        assertThat(throwable).isNull();
     }
 }
