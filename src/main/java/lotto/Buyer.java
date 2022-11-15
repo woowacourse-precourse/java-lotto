6385ed5eb95ee907da49a539d2e7bbcbd524fd30
LJW25 <dbjwl@g.skku.edu>
2022-11-15 16:22:29 +0900

    feat: 로또 발행
    - 구입한 금액 만큼 로또를 발행함

--- src/main/java/lotto/Buyer.java
+++ src/main/java/lotto/Buyer.java
@@ -1,27 +1,39 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Enums.ErrorMessage;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Buyer {
 
     private final int purchase;
     private int reward;
 
-    private ArrayList<Lotto> purchasedLotto;
-    private ArrayList<Integer> results;
+    private List<Lotto> purchasedLottos;
+    private List<Integer> results;
 
     public Buyer(int purchase){
         validate(purchase);
         this.purchase = purchase;
-        //TODO: 다른 인스턴스들도 초기화 예정
+        this.purchasedLottos = buyLottos(purchase/1000);
     }
 
     private void validate(int purchase){
         if(purchase%1000 != 0) {
             throw new IllegalArgumentException(ErrorMessage.PRICE_MOD.getMessage());
         }
+    }
+
+    private List<Lotto> buyLottos(int cnt){
+        List<Lotto> purchasedLottos = new ArrayList<>();
+
+        for(int i=0;i<cnt;i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            purchasedLottos.add(new Lotto(numbers));
+        }
+        return purchasedLottos;
     }
 
     public void getResult(){
