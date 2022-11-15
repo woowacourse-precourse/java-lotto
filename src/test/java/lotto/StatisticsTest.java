071ce63afdfa21f89dea3e5d80dfc62b18897d0c
Sangin <ksiisk99@gmail.com>
2022-11-15 17:38:33 +0900

    test: 공통된 초기값 분리

--- src/test/java/lotto/StatisticsTest.java
+++ src/test/java/lotto/StatisticsTest.java
@@ -2,7 +2,8 @@ package lotto;
 
 import lotto.domain.PrizeMoney;
 import lotto.domain.Statistics;
-import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,20 +13,28 @@ import java.util.Map.Entry;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StatisticsTest {
+    private List<List<Integer>> lottos;
+    private List<Integer> winningLottoNumbers;
+    private int bonusNumber;
+    private Statistics statistics;
 
-    @DisplayName("구매자의 6개의 로또가 각각 꽝~1등이 나오는지 테스트")
-    @Test
-    public void 구매자로또를_합격로또에_맞춰보기() {
-        //given
-        List<List<Integer>> lottos = List.of(List.of(1, 2, 3, 4, 5, 6),
+    @BeforeEach
+    public void 세팅() {
+        statistics = new Statistics();
+        lottos = List.of(List.of(1, 2, 3, 4, 5, 6),
                 List.of(1, 2, 3, 4, 5, 45),
                 List.of(1, 2, 3, 4, 5, 7),
                 List.of(1, 2, 3, 4, 7, 8),
                 List.of(1, 2, 3, 9, 45),
                 List.of(1, 2, 10, 11, 12, 45));
-        List<Integer> winningLottoNumbers = List.of(1, 2, 3, 4, 5, 6);
-        int bonusNumber = 45;
-        Statistics statistics = new Statistics();
+        winningLottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        bonusNumber = 45;
+    }
+
+    @DisplayName("구매자의 6개의 로또가 각각 꽝~1등이 나오는지 테스트")
+    @Test
+    public void 구매자로또를_합격로또에_맞춰보기() {
+        //given
         statistics.countWinningLotto(lottos, winningLottoNumbers, bonusNumber);
 
         //when
@@ -42,4 +51,5 @@ public class StatisticsTest {
         //then
         assertEquals(5, actual.size());
     }
+
 }
