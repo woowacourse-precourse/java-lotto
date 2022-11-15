550d5bc804d9e31aa14c3d4c64a0b4083043c56b
osy9757 <osy9757@gmail.com>
2022-11-15 14:09:59 +0900

    docs : 유저에게 로또 번호를 입력받는 함수 작성

--- src/main/java/lotto/print/PrintOutputMessage.java
+++ src/main/java/lotto/print/PrintOutputMessage.java
@@ -1,10 +1,13 @@
 package lotto.print;
 
 import java.text.DecimalFormat;
+import java.util.List;
+
+import lotto.Lotto;
 
 public class PrintOutputMessage {
 
-    private static final String PRINT_PURCHASE_AMOUNT = "%d개를 구매했습니다.";
+    private static final String PRINT_PURCHASE_AMOUNT = "\n%d개를 구매했습니다.\n";
 
     private static final String PRINT_WINNING_STATISTICS = "당첨 통계\n---";
 
@@ -18,6 +21,13 @@ public class PrintOutputMessage {
         System.out.printf(PRINT_PURCHASE_AMOUNT,lottoAccount);
     }
 
+    public static void printUserLottoNumbers(List<Lotto> userLottoNumbers) {
+        System.out.printf(PRINT_PURCHASE_AMOUNT,userLottoNumbers.size());
+        for(int i = 0 ; i < userLottoNumbers.size(); i++) {           //for문 작성법 수정
+            System.out.println(userLottoNumbers.get(i).getNumbers());
+        }
+        System.out.println();
+    }
     public static void printException(Exception exception) {
         System.out.println(exception.getMessage());
     }
