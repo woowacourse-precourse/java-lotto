56bd9822bffcbe8a47ba0de5fc3bd0af97cf78b0
Noah <shgustmd6865@naver.com>
2022-11-15 15:59:44 +0900

    Feat(Lotto) : 당첨 번호를 기준으로 당첨 여부 검사 기능 추가
    당첨 번호를 기준으로 당첨 여부 검사 기능 추가입니다.
    각 항목에 맞는 결과를반환하기 위해 ResultDto를 사용합니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import dto.ResultDto;
+
 import java.util.List;
 
 public class Lotto {
@@ -18,5 +20,31 @@ public class Lotto {
 
     public List<Integer> getNumbers() {
         return this.numbers;
+    }
+
+    public ResultDto getResult(List<Lotto> boughtLottos, Integer bonus) {
+        ResultDto result = new ResultDto();
+        for (Lotto boughtLotto : boughtLottos) {
+            int count = 0;
+            int bonusCount = 0;
+            for (Integer value : boughtLotto.getNumbers()) {
+                if (this.getNumbers().contains(value))
+                    count++;
+                if (value == bonus)
+                    bonusCount++;
+            }
+            if (count == 3)
+                result.setThreeCount(result.getThreeCount() + 1);
+            else if (count == 4)
+                result.setFourCount(result.getFourCount() + 1);
+            else if (count == 5 && bonusCount == 1)
+                result.setFiveBonusCount(result.getFiveBonusCount() + 1);
+            else if (count == 5)
+                result.setFiveCount(result.getFiveCount() + 1);
+            else if (count == 6)
+                result.setSixCount(result.getSixCount() + 1);
+        }
+
+        return result;
     }
 }
