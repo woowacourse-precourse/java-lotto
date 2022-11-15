0cbf311a86d9faf419a647849e231445ec7f3007
noparamin <noparamin@naver.com>
2022-11-15 21:26:29 +0900

    feat: 수익률 계산 기능 구현

---
+++ src/main/java/lotto/ProfitRate.java
@@ -0,0 +1,13 @@
+package lotto;
+
+public class ProfitRate {
+    double totalProfitRate;
+
+    public ProfitRate(double totalProfitRate) {
+        this.totalProfitRate = totalProfitRate;
+    }
+
+    public double getTotalProfitRate() {
+        return totalProfitRate;
+    }
+}
