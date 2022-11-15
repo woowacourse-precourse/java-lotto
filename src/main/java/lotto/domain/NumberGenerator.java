d71bae315f627d4a9a95fd1ab67bbd835f647114
lhyeonj120 <isk04387@gmail.com>
2022-11-15 19:01:53 +0900

    feat: 로또 당첨 번호 생성 기능 구현

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -1,17 +1,40 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberGenerator {
-    private final List<Integer> numbers;
+    private static List<Integer> numbers;
 
+    public NumberGenerator() {
+    }
+    
     public NumberGenerator(int start, int end, int count) {
         numbers = Randoms.pickUniqueNumbersInRange(start, end, count);
     }
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+    
+    public void setWinningNumber() {
+        String input = inputWinningNumber();
+        String[] inputNumber = splitStr(input);
+        numbers = new ArrayList<>();
+        for(int i = 0; i < inputNumber.length; i++) {
+            numbers.add(Integer.parseInt(inputNumber[i]));
+        }
+    }
+    
+    private String inputWinningNumber() {
+        return Console.readLine();
+    }
+    
+    private String[] splitStr(String str) {
+        String[] inputNumber = str.split(",");
+        return inputNumber;
     }
 }
