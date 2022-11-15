f40ba9e3c4f61498b315a4613249cc3ab1d622a3
CJ <choijun0627@naver.com>
2022-11-15 14:08:09 +0900

    <feat>(CalculatingLottoTest.java): 기능 요구 사항12번 테스트 코드 구현

--- src/test/java/lotto/CalculatingLottoTest.java
+++ src/test/java/lotto/CalculatingLottoTest.java
@@ -38,7 +38,17 @@ class CalculatingLottoTest {
         assertThat(calculatingLotto.getLottoRank(3, false)).isEqualTo(5);
         assertThat(calculatingLotto.getLottoRank(2, true)).isEqualTo(0);
         assertThat(calculatingLotto.getLottoRank(1, false)).isEqualTo(0);
+    }
 
+    @Test
+    void calculateLottoRankTest() {
+        assertThat(calculatingLotto.calculateLottoRank(List.of(1, 2, 3, 4, 5, 6), 7, List.of(1, 2, 3, 4, 5, 6))).isEqualTo(1);
+        assertThat(calculatingLotto.calculateLottoRank(List.of(1, 2, 3, 4, 5, 6), 7, List.of(1, 2, 3, 4, 5, 7))).isEqualTo(2);
+        assertThat(calculatingLotto.calculateLottoRank(List.of(1, 2, 3, 4, 5, 6), 7, List.of(1, 2, 3, 4, 5, 8))).isEqualTo(3);
+        assertThat(calculatingLotto.calculateLottoRank(List.of(1, 2, 3, 4, 5, 6), 7, List.of(1, 2, 3, 4, 8, 9))).isEqualTo(4);
+        assertThat(calculatingLotto.calculateLottoRank(List.of(1, 2, 3, 4, 5, 6), 7, List.of(1, 2, 3, 8, 9, 10))).isEqualTo(5);
+        assertThat(calculatingLotto.calculateLottoRank(List.of(1, 2, 3, 4, 5, 6), 7, List.of(1, 2, 8, 9, 10, 11 ))).isEqualTo(0);
+        assertThat(calculatingLotto.calculateLottoRank(List.of(1, 2, 3, 4, 5, 6), 7, List.of(8, 9, 10, 11, 12, 13 ))).isEqualTo(0);
 
     }
 }
