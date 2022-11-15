30fba0d42821a814eaffffdaba16dfd47cef80a4
hhiyeon <hhyyeon0214@gmail.com>
2022-11-15 12:43:10 +0900

    feat: response-print number of wins

--- src/main/java/lotto/view/ResponseMessage.java
+++ src/main/java/lotto/view/ResponseMessage.java
@@ -1,8 +1,15 @@
 package lotto.view;
 
+import lotto.domain.Rank;
+
 import java.util.Map;
 
 public class ResponseMessage {
+    private static final int SECOND_RANK_MATCH = 7;
+    private static final int START_KEY = 3;
+    private static final int END_KEY = 6;
+    private static final int SECOND_THIRD_RANK_MATCH = 5;
+    private static final int NOTHING_MATCH = 0;
     static StringBuilder responseMessage = new StringBuilder();
 
     public static void initStringBuilder() {
@@ -36,6 +43,15 @@ public class ResponseMessage {
     public static void printWinStatistics(Map<Integer, Integer> matches, String profitRate) {
         initStringBuilder();
         responseMessage.append("당첨 통계\n").append("---\n");
+
+        for (int key = START_KEY; key <= END_KEY; key++) {
+            responseMessage.append(Rank.findRankByMatch(key).toString())
+                    .append(matches.getOrDefault(key, NOTHING_MATCH)).append("개\n");
+            if (key == SECOND_THIRD_RANK_MATCH) {
+                responseMessage.append(Rank.findRankByMatch(SECOND_RANK_MATCH).toString())
+                        .append(matches.getOrDefault(SECOND_RANK_MATCH, NOTHING_MATCH)).append("개\n");
+            }
+        }
         responseMessage.append("총 수익률은 ").append(profitRate).append("%입니다.");
         System.out.println(responseMessage);
     }
