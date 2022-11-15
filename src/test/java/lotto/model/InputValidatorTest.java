72a415c74d987af370ec07b3ddc334e04b076355
LeeHaiLim <hailim1997@gmail.com>
2022-11-15 11:16:23 +0900

    test(model): add test methods of winning lotto validation
    Added test methods:
    -input string wrong length
    -input string not all number
    -input bonus not number

--- src/test/java/lotto/model/InputValidatorTest.java
+++ src/test/java/lotto/model/InputValidatorTest.java
@@ -26,4 +26,25 @@ class InputValidatorTest {
         assertThatThrownBy(() -> InputValidator.checkMoney("1234"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("당첨 번호 입력 시 6개가 아닌 숫자를 입력할 경우 예외가 발생한다.")
+    @Test
+    void inputStringWrongLength() {
+        assertThatThrownBy(() -> InputValidator.checkLottoNumbers("1,2,3,4,5,6,7"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호 입력 시 숫자가 아닌 문자를 입력할 경우 예외가 발생한다.")
+    @Test
+    void inputStringNotAllNumber() {
+        assertThatThrownBy(() -> InputValidator.checkLottoNumbers("1,2,3,4,5,z"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호 입력 시 숫자가 아닌 문자를 입력할 경우 예외가 발생한다.")
+    @Test
+    void inputBonusNotNumber() {
+        assertThatThrownBy(() -> InputValidator.checkBonusNumber("a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
