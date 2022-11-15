ee25b790eb6cf006005a375d3c444ed1108aa033
sooyoungh <sooyoung.h8@gmail.com>
2022-11-15 19:11:57 +0900

    ✨ feat(EventController): 구입한 티켓 갯수 계산하는 기능 구현

--- src/main/java/lotto/controller/EventController.java
+++ src/main/java/lotto/controller/EventController.java
@@ -5,6 +5,7 @@ import lotto.model.Lotto.BonusLotto;
 import lotto.model.Lotto.Lotto;
 import lotto.model.User;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class EventController {
 
@@ -16,5 +17,11 @@ public class EventController {
     private int inputMoneyForBuyTickets() {
         int money = InputView.inputMoney();
         return money;
+    }
+
+    private int calculateTicketCount(int money) {
+        int count = money / 1000;
+        OutputView.printCountOfTicket(count);
+        return count;
     }
 }
