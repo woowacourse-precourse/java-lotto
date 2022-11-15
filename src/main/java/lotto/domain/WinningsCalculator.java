a6872d65b230ff729f7b7955b2ff16e5475b2886
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-15 13:10:15 +0900

    feat(WinningsCalculator): 당첨 순위 계산

--- src/main/java/lotto/domain/WinningsCalculator.java
+++ src/main/java/lotto/domain/WinningsCalculator.java
@@ -11,11 +11,41 @@ public class WinningsCalculator {
         this.bonusNumber = number;
     }
 
+    public Map<String, Integer> countLottoRankings(List<List<Integer>> lottoPapers) {
+        Map<String, Integer> rankingCounts = new HashMap<>() {
+            {
+                put("FIRST_PLACE", 0);
+                put("SECOND_PLACE", 0);
+                put("THIRD_PLACE", 0);
+                put("FOURTH_PLACE", 0);
+                put("FIFTH_PLACE", 0);
+            }
+        };
+        for (List<Integer> lotto : lottoPapers) {
+            int winningCount = countMatchingWinningNumbers(lotto);
+            int bonusCount = countMatchingBonusNumbers(lotto);
+            addRankingCounts(rankingCounts, winningCount, bonusCount);
+        }
+        return rankingCounts;
+    }
+
+    public void addRankingCounts(Map<String, Integer> rankingCounts, int winningCount, int bonusCount) {
+        try {
+            WinningsCalculatorType rankingType = WinningsCalculatorType.selectRankingType(winningCount, bonusCount);
+            if (rankingType.name() == "NO_RANK") {
+                return;
+            }
+            rankingCounts.put(rankingType.name(), rankingCounts.get(rankingType.name()) + 1);
+        }
+        catch (Exception error) {
+            System.out.println(error.getMessage());
+        }
+    }
+
     public int countMatchingWinningNumbers(List<Integer> lotto) {
         int winningCount = 0;
         for (Integer number : winningNumbers) {
             if (isExistentNumber(lotto, number)) {
-                System.out.println("당첨" + number);
                 winningCount++;
             }
         }
