6198452de19a887d4915cfe3418aafb719c4cb49
ddukddaki <rlfrkdms1@naver.com>
2022-11-15 21:04:53 +0900

    refactor: 입력에 관한 모든 상수 inputConstant로 클래스 분리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,17 +1,16 @@
 package lotto.view;
 
+import static lotto.constants.ErrorConstant.ERR_INPUT_NOT_INTEGER;
+import static lotto.constants.InputConstant.INPUT_BONUS_NUMBER;
+import static lotto.constants.InputConstant.INPUT_LOTTO_PURCHASE_MONEY;
+import static lotto.constants.InputConstant.INPUT_WINNING_NUMBER;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static lotto.constants.ErrorConstant.ERR_INPUT_NOT_INTEGER;
-
 public class InputView {
-
-    private static final String INPUT_LOTTO_PURCHASE_MONEY = "구입금액을 입력해 주세요.";
-    private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public int inputLottoPurchaseMoney() {
         System.out.println(INPUT_LOTTO_PURCHASE_MONEY);
