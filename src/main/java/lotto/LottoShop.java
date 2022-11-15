8a5dd90b6457b44572ef4fbda19dc91fe0f181d8
01192mg <01192mg@gmail.com>
2022-11-15 09:24:14 +0900

    refactor(LottoShop): 로또 구매 코드 리팩토링
    0원이 아니면 구매하는 것보다 로또 금액보다 큰 경우 구매하는 게 의미가 명확하다고 생각돼서 변경함

--- src/main/java/lotto/LottoShop.java
+++ src/main/java/lotto/LottoShop.java
@@ -10,11 +10,10 @@ import java.util.List;
 
 public class LottoShop {
     private static final int LOTTO_PRICE = 1_000;
-    private static final int ZERO = 0;
 
     public static List<Lotto> purchase(Money money) {
         List<Lotto> lottos = new ArrayList<>();
-        while (money.getAmount() != ZERO) {
+        while (money.getAmount() >= LOTTO_PRICE) {
             lottos.add(issueLotto());
             money = money.decreaseAmount(LOTTO_PRICE);
         }
