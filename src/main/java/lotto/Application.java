66f8e74e18a2d6c42a1c9c3535bc5fb6cfa073c1
장한울 <swingjhw@naver.com>
2022-11-15 22:09:10 +0900

    feat(Computer) : 금액에 맞게 로또번호 받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,9 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.util.InputController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
 
@@ -14,7 +17,15 @@ public class Application {
         String strBuyAmount = Console.readLine();
         InputController inputController = new InputController();
         Integer number = inputController.getBuyAmount(strBuyAmount);
-        System.out.println(number);
+
+
+        List<List<Integer>> lottonumbers = new ArrayList<>();
+        while (lottonumbers.size() < number / 1000) {
+            Computer computer = new Computer();
+            List<Integer> lottoNumber = computer.getLottoNumber();
+            Lotto lotto = new Lotto(lottoNumber);
+            lottonumbers.add(lottoNumber);
+        }
 
 
 
--- src/main/java/lotto/Computer.java
+++ src/main/java/lotto/Computer.java
@@ -1,4 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
+
+    public List<Integer> getLottoNumber() {
+        List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return lottoNumber;
+    }
 }
