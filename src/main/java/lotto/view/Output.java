cba57beaec38794b6e6b0d0d24077d07f657e2e2
mikekang47 <gyeong0308@gmail.com>
2022-11-15 19:07:31 +0900

    Feat<Output>: 사용자 로또 번호 출력 메서드 추가

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,5 +1,10 @@
 package lotto.view;
 
+import lotto.domains.Lotto;
+import lotto.utils.LottoNumberConverter;
+
+import java.util.List;
+
 public class Output {
     private Output() {
     }
@@ -7,5 +12,12 @@ public class Output {
     public static void printNumberOfPurChase(int purchaseAmount) {
         System.out.println();
         System.out.println(purchaseAmount + "개를 구매했습니다.");
+    }
+
+    public static void printUserLottoNumbers(List<Lotto> lotteries) {
+        lotteries.stream()
+                .map(Lotto::getNumbers)
+                .map(LottoNumberConverter::convertToString)
+                .forEach(lottoNumberStr -> System.out.println("[" + lottoNumberStr + "]"));
     }
 }
