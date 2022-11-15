83840bbceeacfce46180321ac88985fc53b1e4d1
sypark <soun997@gmail.com>
2022-11-15 22:28:14 +0900

    feat(Buyer): budget 필드 추가, getter 메소드 추가

--- src/main/java/lotto/Model/Buyer.java
+++ src/main/java/lotto/Model/Buyer.java
@@ -4,12 +4,11 @@ package lotto.Model;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.View.Input;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Buyer {
     private static final int AMOUNT_PER_LOTTO = 1000;
+    private int budget;
     private int purchasedAmount;
     private List<Lotto> lottos = new ArrayList<>();
 
@@ -18,6 +17,7 @@ public class Buyer {
      */
     public void buyLotto(int budget){
         validateBudget(budget);
+        this.budget = budget;
         purchasedAmount = budget / AMOUNT_PER_LOTTO;
     }
 
@@ -36,8 +36,9 @@ public class Buyer {
      */
     public void pickLottoNumbers(){
         while (lottos.size() < purchasedAmount){
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            Collections.sort(numbers);
+            // 테스트케이스는 immutable한 형식으로 들어오므로 새로 ArrayList에 담은 후 정렬
+            List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            numbers.sort(Comparator.naturalOrder());
             if (validateLottoNumber(numbers, lottos)){
                 lottos.add(new Lotto(numbers));
             }
@@ -59,6 +60,7 @@ public class Buyer {
         return true;
     }
 
+    public int getBudget() { return budget; }
     public int getPurchasedAmount() { return purchasedAmount; }
     public List<Lotto> getLottos() { return lottos; }
 }
