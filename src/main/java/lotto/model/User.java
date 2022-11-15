aca9c2f14f7b8885a189bb8228e2f9d5003d8f60
kite707 <kite707707@gmail.com>
2022-11-15 14:38:29 +0900

    feat(user): 로또발행횟수 도출

--- src/main/java/lotto/model/User.java
+++ src/main/java/lotto/model/User.java
@@ -1,12 +1,14 @@
 package lotto.model;
 import camp.nextstep.edu.missionutils.Console;
 public class User {
+    private int publicans;
     public void getBudget(){
         System.out.println("구입금액을 입력해 주세요.");
         int budget = Integer.parseInt(Console.readLine());
         if(budget%1000!=0){
             throw new IllegalArgumentException("구입금액은 1000원 단위입니다!");
         }
+        this.publicans = budget / 1000;
 
     }
 
