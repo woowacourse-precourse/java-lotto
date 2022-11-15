d2d2b6b4499e9d5e80ec7932b3eed33111d0e845
Hyosik Philip Joo <lux1937@gmail.com>
2022-11-15 21:01:38 +0900

    feat: add print and illegalArgumentException to methods

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -12,6 +12,7 @@ public class Application {
         System.out.println("구입금액을 입력해 주세요.");
         String payment = readLine();
         if (Integer.parseInt(payment) % 1000 != 0) {
+            System.out.println("[ERROR] 유효한 구입금액이 아닙니다.");
             throw new IllegalArgumentException();
         }
         return Integer.parseInt(payment) / 1000;
@@ -20,9 +21,16 @@ public class Application {
     public static List<Integer> getWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String winningNumbersInput = readLine();
-        return Arrays.stream(winningNumbersInput.split(","))
+        List<Integer> winningNumbers = Arrays.stream(winningNumbersInput.split(","))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+        for (int number: winningNumbers) {
+            if (number < 1 || number > 45) {
+                System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                throw new IllegalArgumentException();
+            }
+        }
+        return winningNumbers;
     }
 
     public static void main(String[] args) {
