daf80554932649a7819befa9889722bbbdf5a566
juy4556 <kws05050@naver.com>
2022-11-15 21:44:24 +0900

    refactor: ApplicationTest에서 사용자 도메인 테스트로 테스트 이동

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -57,46 +57,6 @@ class ApplicationTest extends NsTest {
         });
     }
 
-    @Test
-    void 구매금액_천단위_입력_안할_때_예외테스트() {
-        String input = "1001";
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> exception.validateIsThousandUnit(input))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining("[ERROR]천 단위로 입력해주세요.")
-        );
-    }
-
-    @Test
-    void 구매금액_1000보다_적을_때_예외테스트() {
-        String input = "0";
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> exception.validateIsLessThanThousand(input))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining("[ERROR]구매금액은 1000 이상이어야 합니다.")
-        );
-    }
-
-    @Test
-    void 보너스번호_문자_입력될_때_예외처리_예외테스트() {
-        String input = "C";
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> exception.validateIsNumeric(input))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining("[ERROR]숫자만 입력해주세요.")
-        );
-    }
-
-    @Test
-    void 보너스번호_당첨번호와_중복되는지_예외테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() ->
-                        exception.validateIsDuplicatedWithWinningLotto(new Lotto(List.of(1,2,3,4,5,6)),1))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining("[ERROR]보너스번호가 우승 번호와 중복됩니다.")
-        );
-    }
-
     @Override
     public void runMain() {
         Application.main(new String[]{});
