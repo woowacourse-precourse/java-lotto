e51842e61747fbe82c0ecc1b389ffb36b2b38a1b
Sinryuji <siler2474@gmail.com>
2022-11-15 15:59:47 +0900

    feat(Application): 전체 로직 통합

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import java.util.List;
+import lotto.domain.Calculator;
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import lotto.domain.Rank;
+import lotto.ui.Input;
+import lotto.ui.Output;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            int amount = Input.inputAmount();
+            List<Lotto> lottos = LottoMachine.publishLotto(amount);
+            Output.outputBuyLotto(lottos);
+
+            List<Integer> numbers = Input.inputNumbers();
+            Lotto userLotto = new Lotto(numbers);
+            int bonus = Input.inputBonus(userLotto);
+
+            List<Integer> result = LottoMachine.getResult(lottos, numbers, bonus);
+            Output.outputStatistics(result, Calculator.calRevenue(result, amount));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
