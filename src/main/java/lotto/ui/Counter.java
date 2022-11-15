c84792ad25e08b5342087c6df9570d2c54c7b79d
Sinryuji <siler2474@gmail.com>
2022-11-15 09:36:47 +0900

    feat(Counter): 사용자로부터 당첨 번호를 입력 받는 기능 추가

--- src/main/java/lotto/ui/Counter.java
+++ src/main/java/lotto/ui/Counter.java
@@ -3,12 +3,22 @@ package lotto.ui;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Counter {
     private static final String inputAmountMessage = "구입 금액을 입력해 주세요.";
+    private static final String inputNumbersMessage = "당첨 번호를 입력해 주세요.";
 
     public int inputAmount() {
         System.out.println(inputAmountMessage);
-        return parseStrToInt(Console.readLine());
+        int amount = parseStrToInt(Console.readLine());
+        return amountValidate(amount);
+    }
+
+    public List<Integer> inputNumbers() {
+        System.out.println(inputNumbersMessage);
+        return parseStrToList(Console.readLine());
     }
 
     private int parseStrToInt(String input) {
@@ -19,12 +29,28 @@ public class Counter {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요");
         }
-        return amountValidate(result);
+        return result;
     }
 
-    private int amountValidate(int price) {
-        if (price % Lotto.lottoPrice != 0)
+    private int amountValidate(int amount) {
+        if (amount % Lotto.lottoPrice != 0)
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
-        return price;
+        return amount;
+    }
+
+    private List<Integer> numbersValidate(List<Integer> numbers) {
+        new Lotto(numbers);
+        return numbers;
+    }
+
+    private List<Integer> parseStrToList(String input) {
+        String[] numbers = input.split(",");
+        List<Integer> result = new ArrayList<>();
+
+        for (String number : numbers) {
+            result.add(parseStrToInt(number));
+        }
+
+        return numbersValidate(result);
     }
 }
