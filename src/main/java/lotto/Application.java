7efa76106e04f2eaae63f53fe51c013e0eb29c27
Lee-SungMin <saevers26@gmail.com>
2022-11-15 13:27:04 +0900

    refactor: 필요없는 코드 및 간격 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,15 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.domain.*;
+import lotto.domain.Lotto;
+import lotto.domain.Report;
+import lotto.domain.Winning;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     private static final String ERROR_MESSAGE = "[ERROR] ";
+
     public static void main(String[] args) {
         try {
             inputPrint();
@@ -17,8 +18,6 @@ public class Application {
             int lottoCount = Lotto.calculate(inputMoney);
             List<Lotto> allLotto = Lotto.createLotto(lottoCount);
             countPrint(lottoCount);
-
-            disableWarning();
 
             numbersPrint();
             String inputNumbers = Console.readLine();
@@ -35,26 +34,25 @@ public class Application {
         } catch (IllegalArgumentException e) {
             System.out.println(ERROR_MESSAGE + e.getMessage());
         }
-
     }
 
-    public static void inputPrint(){
+    public static void inputPrint() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public static void countPrint(int lottoCount){
+    public static void countPrint(int lottoCount) {
         System.out.println(lottoCount + "개를 구매했습니다.");
     }
 
-    public static void numbersPrint(){
+    public static void numbersPrint() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
-    public static void bonusPrint(){
+    public static void bonusPrint() {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
-    public static void reportPrint(Integer[] winningCount, double returnRate){
+    public static void reportPrint(Integer[] winningCount, double returnRate) {
         System.out.println("당첨 통계");
         System.out.println("---");
         System.out.println("3개 일치 (5,000원) - " + winningCount[1] + "개");
@@ -64,10 +62,4 @@ public class Application {
         System.out.println("6개 일치 (2,000,000,000원) - " + winningCount[5] + "개");
         System.out.println("총 수익률은 " + returnRate + "%입니다.");
     }
-
-    public static void disableWarning() {
-        System.err.close();
-        System.setErr(System.out);
-    }
-
 }
