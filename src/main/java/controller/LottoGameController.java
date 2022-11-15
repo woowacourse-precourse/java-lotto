1ccaf1f8fad5b3d573c0cb4a407a95ef3cc1eca3
JunHyeon <dlwnsgus07@naver.com>
2022-11-15 17:06:45 +0900

    refactor(메소드 분리): Controller의 메소드를 기능별로 분리

--- src/main/java/controller/LottoGameController.java
+++ src/main/java/controller/LottoGameController.java
@@ -11,27 +11,32 @@ import java.util.List;
 public class LottoGameController {
     public void gameStart() {
         try {
-            PurchaseMoney purchaseMoney = UserInputView.getPurchaseMoney();
-            List<Lotto> purchaseLottoList = new ArrayList<>();
-
-            for (int i = 0; i < purchaseMoney.getNumberLotteryPurchasesAvailable(); i++) {
-                purchaseLottoList.add(LottoNumberGenerator.makeLottoNumberGenerator());
-            }
-
-            UserOutputView.printPurchaseNumber(purchaseMoney);
-
-            for (Lotto lotto : purchaseLottoList) {
-                UserOutputView.printPurchaseLottoNumber(lotto);
-            }
-
-            Lotto winningLottoNumber = UserInputView.getWinningLottoNumber();
-            BonusNumber bonusLottoNumber = UserInputView.getBonusLottoNumber();
-            WinningLottoNumber winningLottoNumbers = new WinningLottoNumber(winningLottoNumber, bonusLottoNumber);
-            LottoResult lottoResult = new LottoResult(purchaseLottoList, winningLottoNumbers);
-            UserOutputView.printLottoResult(lottoResult);
+            List<Lotto> purchaseLottoList = getPurchaseLottoList();
+            getLottoWinningResult(purchaseLottoList);
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR]" + e.getMessage());
         }
 
+    }
+
+    private void getLottoWinningResult(List<Lotto> purchaseLottoList) {
+        Lotto winningLottoNumber = UserInputView.getWinningLottoNumber();
+        BonusNumber bonusLottoNumber = UserInputView.getBonusLottoNumber();
+        WinningLottoNumber winningLottoNumbers = new WinningLottoNumber(winningLottoNumber, bonusLottoNumber);
+        LottoResult lottoResult = new LottoResult(purchaseLottoList, winningLottoNumbers);
+        UserOutputView.printLottoResult(lottoResult);
+    }
+
+    private List<Lotto> getPurchaseLottoList() {
+        PurchaseMoney purchaseMoney = UserInputView.getPurchaseMoney();
+        List<Lotto> purchaseLottoList = new ArrayList<>();
+        for (int i = 0; i < purchaseMoney.getNumberLotteryPurchasesAvailable(); i++) {
+            purchaseLottoList.add(LottoNumberGenerator.makeLottoNumberGenerator());
+        }
+        UserOutputView.printPurchaseNumber(purchaseMoney);
+        for (Lotto lotto : purchaseLottoList) {
+            UserOutputView.printPurchaseLottoNumber(lotto);
+        }
+        return purchaseLottoList;
     }
 }
