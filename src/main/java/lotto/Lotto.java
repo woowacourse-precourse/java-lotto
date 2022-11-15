13839d13d1c05d6e07981ddcb2535db7f920d58a
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-15 20:59:06 +0900

    feat: 로또 번호 예외 처리 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,6 @@
 package lotto;
 
-import lotto.view.ErrorMessage;
+import lotto.console.ErrorMessage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,22 +11,28 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         repeat(numbers);
+        range(numbers);
         this.numbers = numbers;
     }
-
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(ErrorMessage.lottoIndex.getValue());
         }
     }
-
     private void repeat(List<Integer> numbers) {
         List<Integer> inputNumber = new ArrayList<>();
         for (int eachNumber : numbers) {
             if (inputNumber.contains(eachNumber)) {
-                throw new IllegalArgumentException(ErrorMessage.lottoRepeatation.getValue());
+                throw new IllegalArgumentException(ErrorMessage.lottoRepetition.getValue());
             } else {
                 inputNumber.add(eachNumber);
+            }
+        }
+    }
+    private void range(List<Integer> numbers) {
+        for (int eachNumber : numbers) {
+            if ( eachNumber < 1 || eachNumber > 45) {
+                throw new IllegalArgumentException(ErrorMessage.lottoRange.getValue());
             }
         }
     }
