97d3545e162beabe3a14b97f72dcb513280b10fa
dingding-21 <phj8221@naver.com>
2022-11-15 10:44:49 +0900

    test($InputTest): 로또 구입 금액 유효성 검증 테스트

---
+++ src/test/java/lotto/ui/InputTest.java
@@ -0,0 +1,25 @@
+package lotto.ui;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class InputTest {
+
+    @DisplayName("로또 구입 금액이 1000으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void validatePurchaseAmount() {
+        int isDivisible = 2000;
+        int isNotDivisible = 2001;
+
+        assertAll(
+                () -> assertThat(Input.validatePurchaseAmount(isDivisible)).isEqualTo(isDivisible),
+                () -> assertThatThrownBy(() -> Input.validatePurchaseAmount(isNotDivisible))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("[ERROR] 로또 구입 금액은 1,000원으로 나누어 떨어져야 합니다.")
+        );
+    }
+}
