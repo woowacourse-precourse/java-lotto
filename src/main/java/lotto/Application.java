c5e278dca6b0bafe11abde83057486db6d868430
SonMinGyu <littleking13@naver.com>
2022-11-15 14:47:27 +0900

    [FEAT] 로또 구입금액 입력받는 기능 구현
    - 로또를 몇개 살건지 구입금액 입력받는 기능을 구현함

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public static int getTheNumberOfLotto() {
+        int money = Integer.parseInt(Console.readLine());
+
+        int numberOfLotto = money / 1000;
+        
+        return numberOfLotto;
+    }
+
+
 }
