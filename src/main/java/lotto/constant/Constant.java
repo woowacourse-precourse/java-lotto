668e1bdde8de1aee35c4aa1e5ea718c5fae8491b
oyoungsun <plusmate@naver.com>
2022-11-15 17:02:33 +0900

    feat : add constant

--- src/main/java/lotto/constant/Constant.java
+++ src/main/java/lotto/constant/Constant.java
@@ -6,6 +6,12 @@ public final class Constant {
     public static final String ERROR_BY1000 = " 1000원 단위로 입력해야합니다.";
     public static final String ERROR_BONUS = " 보너스 번호는 당첨 번호와 겹치면 안됩니다.";
     public static final String ERROR_BOUNDARY = " 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-   // public static final String ERROR_
+
+    public static final String GETMONEY = "구입금액을 입력해 주세요.";
+    public static final String TICKET = "개를 구매했습니다.";
+    public static final String WINNING = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS = "보너스 번호를 입력해 주세요.";
+    public static final String RATIO = "당첨 통계\n---";
+
 
 }
