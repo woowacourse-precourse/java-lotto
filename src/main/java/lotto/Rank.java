3b35bc11b218cd97334388b8f704b4ce1889c47e
Park Sungchul <try615@naver.com>
2022-11-15 21:34:09 +0900

    refactor(Rank.java): 순위 ENUM 개선
    몇개의 숫자가 맞아야 하는지 정보 포함

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,18 +1,24 @@
 package lotto;
 
 public enum Rank {
-	RANK1(1, 2000000000),
-	RANK2(2, 30000000),
-	RANK3(3, 1500000),
-	RANK4(4, 50000),
-	RANK5(5, 5000);
+	RANK1(6, 1, 2000000000),
+	RANK2(105, 2, 30000000),
+	RANK3(5, 3, 1500000),
+	RANK4(4, 4, 50000),
+	RANK5(3, 5, 5000);
 
+	private final int match;
 	private final int ranking;
 	private final int price;
 
-	private Rank(int ranking, int price) {
+	private Rank(int match, int ranking, int price) {
+		this.match = match;
 		this.ranking = ranking;
 		this.price = price;
+	}
+
+	public int getMatch() {
+		return this.match;
 	}
 
 	public int getRanking() {
