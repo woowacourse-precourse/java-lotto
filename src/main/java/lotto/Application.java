9a41ea6d3acbc5db394c52dd4cc15ea8ffddc580
d0ri123 <lsh81550@gmail.com>
2022-11-15 20:20:42 +0900

    refactor: Application test 통과

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,18 +2,22 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        User user = new User();
-        user.enterPrice();
-        user.printLottoEA();
-        user.createLotto();
-        user.enterWinningNumbers();
+        try {
+            User user = new User();
+            user.enterPrice();
+            user.printLottoEA();
+            user.createLotto();
+            user.enterWinningNumbers();
 
-        Lotto lotto = new Lotto(user.getUserNumbers());
-        user.enterBonusNumber();
+            Lotto lotto = new Lotto(user.getUserNumbers());
+            user.enterBonusNumber();
 
-        LotteryMachine machine = new LotteryMachine(user, lotto);
-        machine.countWinningNumber();
-        machine.showStatistics();
-        machine.calculateRate();
+            LotteryMachine machine = new LotteryMachine(user, lotto);
+            machine.countWinningNumber();
+            machine.showStatistics();
+            machine.calculateRate();
+        } catch (Exception e){
+            System.out.println("[ERROR]" + e.getMessage());
+        }
     }
 }
