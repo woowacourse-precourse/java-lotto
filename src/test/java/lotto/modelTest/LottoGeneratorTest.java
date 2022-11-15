99df882184ba424bffc6edf06b7f822dd0d92013
gomljo <gomljo0307@naver.com>
2022-11-15 13:09:14 +0900

    test(LottoGenerator.java): 발행한 로또를 오름차순으로 정렬하는 기능 검증

--- src/test/java/lotto/modelTest/LottoGeneratorTest.java
+++ src/test/java/lotto/modelTest/LottoGeneratorTest.java
@@ -1,16 +1,14 @@
 package lotto.modelTest;
 
-import lotto.model.Buyer;
 import lotto.model.LottoGenerator;
+import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoGeneratorTest {
@@ -20,16 +18,26 @@ public class LottoGeneratorTest {
         int numberOfLottoNumbers=6;
         List<Integer> lotto = new ArrayList<>();
         LottoGenerator lottoGenerator = new LottoGenerator();
-        lotto=lottoGenerator.createLotto();
+        lotto= lottoGenerator.createLotto();
         assertThat(lotto.size()).isEqualTo(numberOfLottoNumbers);
     }
     @Test
     @DisplayName("구매한 로또의 수만큼 로또를 발행하는지 확인한다.")
     public void generateLottoOfBuyerTest(){
         int numberOfLotto=8;
-        List<List<Integer>> generatedLotto = new ArrayList<>();
+        List<Lotto> generatedLotto;
         LottoGenerator lottoGenerator = new LottoGenerator();
-        generatedLotto=lottoGenerator.generateLottoOfBuyer(numberOfLotto);
+        generatedLotto= lottoGenerator.generateLottoOfBuyer(numberOfLotto);
         assertThat(generatedLotto.size()).isEqualTo(numberOfLotto);
     }
+    @Test
+    @DisplayName("발행한 로또가 오름차순으로 정렬되는지 확인한다.")
+    public void checkLottoSorting(){
+        List<Integer> expectedLotto=List.of(1,2,3,4,5,6);
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        List<Integer> generatedLottoNumbers=List.of(2,6,3,4,1,5);
+        Stream<Integer> sortedLottoNumbers= lottoGenerator.sortLottoNumbers(new Lotto(generatedLottoNumbers));
+        assertThat(sortedLottoNumbers.toArray()).containsExactlyElementsOf(expectedLotto);
+    }
+    
 }
