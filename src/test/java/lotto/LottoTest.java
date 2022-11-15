5e4c7c562ffe63e79277028cc8b8a7ed748bb88a
eom-tae-in <eti0728@daum.net>
2022-11-15 20:00:56 +0900

    feat(LottoTest): Lotto클래스의 기능 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.domian.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +13,7 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+          .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
@@ -20,8 +21,21 @@ class LottoTest {
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+          .isInstanceOf(IllegalArgumentException.class);
+    }
+    // 여기부터 내가 작성
+    @DisplayName("로또 번호가 1~45사이의 숫자가 아닐경우 예외가 발생한다.")
+    @Test
+    void createLottoOutOfRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 50)))
+          .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("보너스 번호가 당첨번호와 일치하면 예외가 발생한다.")
+    @Test
+    void createBonusNumber() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThatThrownBy(() -> lotto.setBonusNumber(6))
+          .isInstanceOf(IllegalArgumentException.class);
+    }
 }
