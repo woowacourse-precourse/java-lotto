c8532e2a40d7af2e5b2ceaa6b42e984c67fc7f8d
eunseo <qnrqjrm03@naver.com>
2022-11-15 16:53:37 +0900

    test : 사용자가 입력한 금액에 대한 유효성 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -25,4 +26,28 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("금액에 맞게 로또를 발행해야 한다.")
+    @Test
+    void buyLottoTest1 (){
+        int money = 5000;
+        int number = Lotto.count(money);
+        Assertions.assertThat(number).isEqualTo(5);
+    }
+
+    @DisplayName("1000원 단위가 아니면 예외로 처리해야 한다.")
+    @Test
+    void buyLottoTest2 (){
+        int money = 5500;
+        assertThatThrownBy(() -> Lotto.count(money))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 1000원 단위로 입력해야 합니다.");
+    }
+    @DisplayName("금액을 잘못 입력하면 예외를 발생시켜야 한다.")
+    @Test
+    void buyLottoTest3 (){
+        int money = -5000;
+        assertThatThrownBy(() -> Lotto.count(money))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 값을 잘못 입력하였습니다.");
+    }
 }
