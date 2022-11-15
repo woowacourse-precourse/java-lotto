6f2639b66b1939413ec5c3cedb121704660a2548
ddukddaki <rlfrkdms1@naver.com>
2022-11-15 15:44:26 +0900

    feat(LottoPurchaseMoney): (예외 처리) 구입금이 1000원 단위인지 검증하는 기능

--- src/main/java/lotto/domain/LottoPurchaseMoney.java
+++ src/main/java/lotto/domain/LottoPurchaseMoney.java
@@ -4,6 +4,7 @@ public class LottoPurchaseMoney {
 
     private static final int A_LOTTO_PRICE = 1000;
     private static final String ERR_MONEY_MORE_THAN_A_LOTTO_PRICE = "[ERROR] 로또 구매 금액은 최소 1000원 이상이어야 합니다. ";
+    private static final String ERR_MONEY_UNFIT_A_LOTTO_PRICE = "[ERROR] 로또 구매 금액은 1000원 단위여야 합니다. ";
 
     private int lottoPurchaseMoney;
 
@@ -14,6 +15,12 @@ public class LottoPurchaseMoney {
     public void validateMoneyMoreThanLottoPrice(int lottoPurchaseMoney) {
         if (lottoPurchaseMoney < A_LOTTO_PRICE) {
             throw new IllegalArgumentException(ERR_MONEY_MORE_THAN_A_LOTTO_PRICE);
+        }
+    }
+
+    public void validateMoneyIsMultipleLottoPrice(int lottoPurchaseMoney) {
+        if (lottoPurchaseMoney % A_LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(ERR_MONEY_UNFIT_A_LOTTO_PRICE);
         }
     }
 }
