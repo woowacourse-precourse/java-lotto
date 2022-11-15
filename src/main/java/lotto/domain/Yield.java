275a69cd46883ddfb567b76a6387a1f4167424a5
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-15 20:22:22 +0900

    feat(LottoGame): 최종 수익률 계산

--- src/main/java/lotto/domain/Yield.java
+++ src/main/java/lotto/domain/Yield.java
@@ -1,4 +1,22 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Yield {
+    private final List<Grade> grades;
+    private final int totalInvestment;
+
+    public Yield(List<Grade> grades, int totalInvestment) {
+        this.grades = grades;
+        this.totalInvestment = totalInvestment;
+    }
+
+    public long getTotalWinningPrice() {
+        long totalWinningPrice = 0;
+        for(Grade grade: grades) {
+            totalWinningPrice += grade.getPrizeMoney();
+        }
+        return totalWinningPrice;
+    }
+
 }
