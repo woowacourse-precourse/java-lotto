4326156f61ac14fd6da80b36c941cc2e64dd590f
can019 <jys01012@gmail.com>
2022-11-15 21:21:27 +0900

    feat(app): connect all features

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,17 +1,62 @@
 package lotto;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Map;
+
 public class LottoController {
     private final LottoView lottoView;
+    private final LottoUtil lottoUtil;
+    private final LottoRandom lottoRandom;
 
-    private String tempInput;
-
-    public LottoController(){
+    public LottoController() {
         this.lottoView = new LottoView();
+        this.lottoRandom = new LottoRandom();
+        this.lottoUtil = new LottoUtil();
     }
 
-    public void run(){
-        this.lottoView.printPurchaseAmountQuestion();
-        this.tempInput = this.lottoView.getUserInput();
+    public void run() {
+        int purchaseAmount = this.getPurchaseAmount();
+        List<List<Integer>> lottoRandom = this.generateLottoRandom(purchaseAmount);
+        this.calculateStatics(lottoRandom, purchaseAmount);
+    }
 
+    private int getPurchaseAmount() {
+        this.lottoView.printPurchaseAmountQuestion();
+        String input = this.lottoView.getUserInput();
+
+        int parseIntInput = this.lottoUtil.StringToInt(input);
+
+        return parseIntInput;
+    }
+
+    private List<List<Integer>> generateLottoRandom(int purchaseAmount) {
+        int purchaseCount = this.lottoRandom.calculatePurchaseCount(purchaseAmount);
+        this.lottoView.printPurchaseUnitCount(purchaseCount);
+
+        List<List<Integer>> lottoRandom = this.lottoRandom.generateRandomLottoNumberList(purchaseCount);
+        this.lottoView.printNestedList(lottoRandom);
+
+        return lottoRandom;
+    }
+
+    private void calculateStatics(List<List<Integer>> userLottoList, int purchaseAmount) {
+        this.lottoView.printMatchLottoNumberQuestion();
+        String prizeNumberInput = this.lottoView.getUserInput();
+        List<Integer> prizeNumber = this.lottoUtil.StringListToIntList(
+                this.lottoUtil.tokenizeString(prizeNumberInput));
+
+        Lotto lotto = new Lotto(prizeNumber);
+
+        this.lottoView.printBonusNumberQuestion();
+        String bonusNumberInput = this.lottoView.getUserInput();
+        int bonusNumber = this.lottoUtil.StringToInt(bonusNumberInput);
+
+        Map<Lotto.MatchInfo, Integer> statistics = lotto.calculateLottoStatistics(userLottoList, bonusNumber);
+        BigInteger winningAmount = lotto.calculateWinningAmount(statistics);
+        BigDecimal ratio = lotto.calculateProfitRatio(winningAmount, purchaseAmount);
+
+        this.lottoView.printLottoStatics(statistics, ratio);
     }
 }
