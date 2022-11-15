26c68e483d7c0535c74b9b19029841d58cbeac37
ddukddaki <rlfrkdms1@naver.com>
2022-11-15 16:46:05 +0900

    feat(InputView): 로또 당첨 번호를 입력 받는 기능

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,15 +1,26 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
 
     private static final String ERR_INPUT_NOT_INTEGER = "[ERROR] 숫자를 입력 해야 합니다.";
     private static final String INPUT_LOTTO_PURCHASE_MONEY = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
 
     public int inputLottoPurchaseMoney() {
         System.out.println(INPUT_LOTTO_PURCHASE_MONEY);
         return convertStringToInt(Console.readLine());
+    }
+
+    public List<Integer> inputWinningNumber() {
+        System.out.println(INPUT_WINNING_NUMBER);
+        return Arrays.stream(Console.readLine().split(","))
+                .map(i -> convertStringToInt(i))
+                .collect(Collectors.toList());
     }
 
     public int convertStringToInt(String input) {
