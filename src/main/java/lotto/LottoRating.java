1434a44d355331761648e5eb4bbffddd27d4b47b
hyeonseog-won <smreo75@gmail.com>
2022-11-15 11:04:10 +0900

    feat: 로또 등급 보너스 번호 추가

--- src/main/java/lotto/LottoRating.java
+++ src/main/java/lotto/LottoRating.java
@@ -1,20 +1,21 @@
 package lotto;
 
 public enum LottoRating {
-    NOTHING(0, 0),
-    FIFTH(3, 5000),
-    FOURTH(4, 50000),
-    THIRD(5, 1500000),
-    SECOND(5, 30000000),
-    FIRST(6, 2000000000);
-
+    FIRST(6, 2000000000, false),
+    SECOND(5, 30000000, true),
+    THIRD(5, 1500000, false),
+    FOURTH(4, 50000, false),
+    FIFTH(3, 5000, false),
+    NOTHING(0, 0, false);
 
     private final int prizeOfMatch;
     private final int winningMoney;
+    private final boolean bonusNumber;
 
-    LottoRating(int prizeOfMatch, int winningMoney) {
+    LottoRating(int prizeOfMatch, int winningMoney, boolean bonusNumber) {
         this.prizeOfMatch = prizeOfMatch;
         this.winningMoney = winningMoney;
+        this.bonusNumber = bonusNumber;
     }
 
     public int getPrizeOfMatch() {
