5fea8d7e1a860ac26fc01ca1e9df607ed8dbcaa1
leesangjin <tkdwls0628@gmail.com>
2022-11-15 18:21:48 +0900

    feat(LottoCalculator): 당첨 등수 및 당첨 금액 계산 기능 구현

--- src/main/java/lotto/domain/LottoCalculator.java
+++ src/main/java/lotto/domain/LottoCalculator.java
@@ -6,7 +6,19 @@ import java.util.Map;
 
 public class LottoCalculator {
 
+    private long totalProfit = 0L;
     private Map<Rank, Integer> lottoResult = new HashMap<>();
+
+    public Map<Rank, Integer> calculateResult(List<Lotto> lottos, Lotto winningLotto, int bonusNumber) {
+        initLottoResult();
+        for (Lotto lotto : lottos) {
+            int matchCount = countMatchedNumber(lotto, winningLotto);
+            Rank rank = Rank.valueOf(lotto, bonusNumber, matchCount);
+            lottoResult.put(rank, lottoResult.get(rank) + 1);
+            totalProfit += rank.getMatchMoney();
+        }
+        return lottoResult;
+    }
 
     public void initLottoResult() {
         for (Rank rank : Rank.values()) {
