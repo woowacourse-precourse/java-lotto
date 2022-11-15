dad5a874d60164d6e7cf2fd7df29b8a2a0cff723
DESKTOP-TAPGE0L\COM <gwj0421@gmail.com>
2022-11-15 14:37:49 +0900

    feat: 로또 클래스 선언 및 예외사항 처리 기능 추가
    예외사항
    - 랜덤하게 뽑힌 로또 번호들이 정해진 개수만큼 없는 경우
    - 랜덤하게 뽑힌 로또 번호들이 서로 겹치는 것이 있는 경우

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,35 @@
 package lotto.domain;
 
+import lotto.Constant;
+
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        check_NotEqualSize(numbers);
+        check_Duplicate(numbers);
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private void check_NotEqualSize(List<Integer> numbers) {
+        if (numbers.size() != Constant.LOTTO_NUMBER_CNT.getNum()) {
+            throw new IllegalArgumentException("[ERROR] 랜덤한 로또의 번호의 크기가 정해진 개수만큼 존재하지 않습니다.");
+        }
+    }
+    private void check_Duplicate(List<Integer> numbers) {
+        HashSet set = new HashSet<>(numbers);
+        if (set.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 랜덤한 로또의 번호들이 서로 겹치는 것이 존재합니다.");
+        }
+    }
 }
