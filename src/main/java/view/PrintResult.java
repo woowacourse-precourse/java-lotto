b58e0198119bc8a40295e8398e80db2a261e8388
dydtmd6801 <dydtmd6801@naver.com>
2022-11-15 18:41:25 +0900

    feat(PrintResult): 당첨 기록 출력하는 기능
    enum 타입을 이용하여 당첨 내용과 당첨금을 출력하는 기능

--- src/main/java/view/PrintResult.java
+++ src/main/java/view/PrintResult.java
@@ -1,13 +1,48 @@
 package view;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class PrintResult {
+    private final String UNIT = "개";
+
     public void printBuyLotto(int buyLottoNumber){
         System.out.println(buyLottoNumber + "개를 구매했습니다.");
     }
 
     public void printLottoNumber(List<Integer> lottoNumber){
         System.out.println(lottoNumber);
+    }
+
+    enum NumberMatch {
+        THREE("3개 일치 (5,000원) - ",5000),
+        FORTH("4개 일치 (50,000원) - ", 50000),
+        FIFTH("5개 일치 (1,500,000원) - ", 1500000),
+        FIFTH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - ", 30000000),
+        SIXTH("6개 일치 (2,000,000,000원) - ", 2000000000);
+
+        private final String numberMatch;
+        private final int money;
+
+        private NumberMatch(String numberMatch, int money) {
+            this.numberMatch = numberMatch;
+            this.money = money;
+        }
+
+        public String getNumberMatch() {
+            return this.numberMatch;
+        }
+
+        public int getMoney(){
+            return this.money;
+        }
+    }
+
+    public void printMatchDetail(HashMap matchNumbers) {
+        List<Integer> order = List.of(3, 4, 5, 7, 6);
+        for (NumberMatch numberMatch : NumberMatch.values()) {
+            int idx = 0;
+            System.out.println(numberMatch.getNumberMatch() + matchNumbers.get(order.get(idx)) + UNIT);
+        }
     }
 }
