da1827c0f7145ce9f2a4e0a8dc97c1f2e9b6b2c6
CJiu01 <jiwoo020801@gmail.com>
2022-11-15 22:57:01 +0900

    [Feat] ResultView printWinnerStat 함수 리펙터링

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,6 +1,10 @@
 package lotto.view;
 
+import lotto.domain.Rank;
+
 import java.text.DecimalFormat;
+import java.util.Collections;
+import java.util.List;
 
 public class ResultView {
 
@@ -15,6 +19,12 @@ public class ResultView {
     public void printWinnerStat(List<Rank> ranks) {
         System.out.println("\n당첨 통계");
         System.out.println("---------");
+        System.out.println("3개 일치 (" + Rank.FIFTH.prize() + "원)-" + Collections.frequency(ranks, Rank.FIFTH) + "개");
+        System.out.println("4개 일치 (" + Rank.FOURTH.prize() + "원)-" + Collections.frequency(ranks, Rank.FOURTH) + "개");
+        System.out.println("5개 일치 (" + Rank.THIRD.prize() + "원)-" + Collections.frequency(ranks, Rank.THIRD) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치(" + Rank.SECOND.prize() + "원)-" + Collections.frequency(ranks, Rank.SECOND) + "개");
+        System.out.println("6개 일치 (" + Rank.FIRST.prize() + "원)-" + Collections.frequency(ranks, Rank.FIRST) + "개");
+
     }
 
     public void printEarningRatio(double rate) {
