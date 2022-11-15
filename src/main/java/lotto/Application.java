cc3ffd57b28e7db1f0734f19f45d8f243595a312
Juuuic <sn990908a@gmail.com>
2022-11-15 15:00:35 +0900

    feat: 당첨 번호 입력, 예외 처리 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -46,4 +46,39 @@ public class Application {
         }
         return lotto_list;
     }
+
+    //3.당첨 번호 입력
+    public static List<Integer> winLotto() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String win_lotto = Console.readLine();
+        exception_winLotto(win_lotto);
+        List<Integer> winLottoList = new ArrayList<>();
+        String[] numbers = win_lotto.split(",");
+        for(int i=0; i<numbers.length; i++){
+            winLottoList.add(Integer.parseInt(numbers[i]));
+        }
+        new Lotto(winLottoList);
+        return winLottoList;
+    }
+
+    //3.당첨 번호 입력 예외 처리
+    private static void exception_winLotto(String win_lotto) {
+        String[] winLottoList = win_lotto.split("");
+        int cnt = 0;
+        for (int i = 0; i < winLottoList.length; i++) {
+            if(winLottoList[i].equals(",")){
+                cnt++;
+            }
+        }
+        if(cnt!=5){
+            throw new IllegalArgumentException("[ERROR] 당첨 번호 6개를 입력해야 합니다.");
+        }
+        winLottoList = win_lotto.split(",");
+        for (int i = 0; i < winLottoList.length; i++) {
+            if(Integer.parseInt(winLottoList[i])>45 || Integer.parseInt(winLottoList[i])<1){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
 }
