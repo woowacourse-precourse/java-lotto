759ff3e6e7e877d99813d5da6d85f6a748389a0d
CAEL0 <kjongmin26@gmail.com>
2022-11-15 18:51:55 +0900

    test(GameResultTest): 6개가 일치하면 1등

---
+++ src/test/java/lotto/domain/GameResultTest.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameResultTest {
+    private GameResult gameResult;
+
+    @BeforeEach
+    void init() {
+        gameResult = new GameResult();
+    }
+
+    @DisplayName("6개가 일치하면 1등을 한다.")
+    @Test
+    void WinFirstPlaceIfSixNumbersAreMatched() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Lotto> lottos = new ArrayList<>(List.of(lotto));
+        List<Integer> winningNumber = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+
+        gameResult.calculateTotalResult(lottos, winningNumber, bonusNumber);
+        List<Integer> result = gameResult.getResult();
+
+        assertThat(result).isEqualTo(List.of(0, 1, 0, 0, 0, 0));
+    }
+}
