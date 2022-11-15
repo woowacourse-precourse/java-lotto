b688367d3f0c949a240ecb5cf9e7492b841b20a3
Kid-Chang <pch3310@gmail.com>
2022-11-15 09:26:25 +0900

    feat(CheckInput): 하나의 로또 내에 중복되는 숫자가 없는지, 6개로 이뤄진 것이 맞는 지 검증하는 메서드 구현

--- src/main/java/lotto/utils/CheckInput.java
+++ src/main/java/lotto/utils/CheckInput.java
@@ -1,7 +1,12 @@
 package lotto.utils;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class CheckInput {
     public static final String ERROR_INVALID_NUMBER_TYPE = "[ERROR] 숫자이외의 값이 포함되어 있습니다.";
+    public static final String ERROR_INVALID_LOTTO_LENGTH = "[ERROR] 입력되는 로또값은 6자리여야 합니다.";
     public static final String ERROR_INVALID_NUMBER_RANGE = "[ERROR] 입력은 0보다 커야합니다.";
     public static final String ERROR_INVALID_NUMBER_PRICE_TYPE = "[ERROR] 1,000 단위로만 입력할 수 있습니다.";
     public static final String ERROR_INVALID_LOTTO_RANGE = "[ERROR] 당첨번호는 1부터 45 사이의 숫자여야 합니다.";
@@ -9,7 +14,7 @@ public class CheckInput {
     public static final String ERROR_INVALID_SAME_LOTTO_BONUS = "[ERROR] 입력한 로또 번호와 보너스 번호가 중복됩니다.";
 
     public static void checkInputMoney(String input) {
-        if (!isNumeric(input))
+        if (isNumeric(input))
             throw new IllegalArgumentException(ERROR_INVALID_NUMBER_TYPE);
 
         if (!(Integer.parseInt(input) > 0))
@@ -19,8 +24,27 @@ public class CheckInput {
             throw new IllegalArgumentException(ERROR_INVALID_NUMBER_PRICE_TYPE);
     }
 
+    public static void checkInputLotto(String input) {
+        if (isNumeric(input))
+            throw new IllegalArgumentException(ERROR_INVALID_NUMBER_TYPE);
+        if (Integer.parseInt(input) < 0 || Integer.parseInt(input) > 45)
+            throw new IllegalArgumentException(ERROR_INVALID_LOTTO_RANGE);
+    }
+
+    public static void checkInputLottoLength(List<Integer> value) {
+        if (!(value.size() == 6))
+            throw new IllegalArgumentException(ERROR_INVALID_LOTTO_LENGTH);
+    }
+
+    public static void checkInputLottoDuplicate(List<Integer> value) {
+        Set<Integer> numSet = new HashSet<>(value);
+
+        if (numSet.size() != value.size())
+            throw new IllegalArgumentException(ERROR_INVALID_LOTTO_DUPLICATION);
+    }
+
     public static boolean isNumeric(String s) {
-        return s != null && s.matches("[-+]?\\d*\\.?\\d+");
+        return s == null || !s.matches("[-+]?\\d*\\.?\\d+");
     }
 }
 
