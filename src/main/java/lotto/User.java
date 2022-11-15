49232850bd7e9e80c8fe6483a7f1fdc4aec124a6
gmu1026 <gmu1026@gmail.com>
2022-11-15 17:57:58 +0900

    feat: 로또 구입 기능 구현 및 예외 처리

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,15 +1,20 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class User {
     private long amount;
-    private List<Lotto> lottos;
+    private long purchaseCount;
+    private List<Lotto> lottos = new ArrayList<>();
 
     public User(long amount) {
         validate(amount);
         this.amount = amount;
+        calculatePurchaseCount();
     }
 
     private void validate(long amount) {
@@ -18,12 +23,31 @@ public class User {
         }
     }
 
-    public void buyLotto() {
-        long purchaseCount = amount / 1000;
+    private void calculatePurchaseCount() {
+        this.purchaseCount = amount / 1000;
+    }
 
+    public void buyLotto() {
         for (int purchaseIndex = 0; purchaseIndex < purchaseCount; purchaseIndex++) {
             List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoNumbers.sort(Integer::compareTo);
             lottos.add(new Lotto(lottoNumbers));
         }
+    }
+
+    public void printLottos() {
+        System.out.println(purchaseCount + "개를 구매했습니다.");
+        System.out.println(lottosToString());
+    }
+
+    private String lottosToString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Lotto lotto:lottos) {
+            stringBuilder.append(lotto.toString())
+                    .append("\n");
+        }
+
+        return stringBuilder.toString();
     }
 }
