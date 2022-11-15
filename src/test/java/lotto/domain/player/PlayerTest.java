78b23185602316d0021b5f345f7938fd44f3e498
Yongwoo Choi <cyw7515@naver.com>
2022-11-15 20:03:49 +0900

    test (PlayerTest): 로또 결과 저장시 올바른 수익률이 반환되는지 확인하는 테스트 추가

--- src/test/java/lotto/domain/player/PlayerTest.java
+++ src/test/java/lotto/domain/player/PlayerTest.java
@@ -2,11 +2,15 @@ package lotto.domain.player;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashMap;
+import lotto.domain.lotto.LottoPrize;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
+    @DisplayName("로또 구매 테스트")
     @Test
-    void 로또_구매_테스트() {
+    void buyingLotto() {
         long initialMoney = 1000;
         long finalMoney = 0;
         Player player = new Player(initialMoney);
@@ -16,5 +20,20 @@ class PlayerTest {
         assertThat(player).isEqualTo(
                 new Player(finalMoney, player.getLotteries())
         );
+    }
+
+    @DisplayName("로또 결과를 저장하면 플레이어의 수익률이 저장된다.")
+    @Test
+    void saveResult() {
+        long initialMoney = 8000;
+        Player player = new Player(initialMoney);
+        HashMap<LottoPrize, Long> lottoResults = new HashMap<>();
+        lottoResults.put(LottoPrize.FIFTH, 1L);
+
+        player.buyLotto();
+        player.saveResult(lottoResults);
+
+        double earningRate = player.getEarningRate();
+        assertThat(earningRate).isEqualTo(62.5);
     }
 }
