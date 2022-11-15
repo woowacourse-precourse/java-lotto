b3f15d7dc07e2bc62f9ebf3564f2d89a4174fad4
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-11-15 19:07:15 +0900

    <feat>WinningPlayer : 당첨 결과 알려주기 기능 추가

--- src/main/java/lotto/WinningPayer.java
+++ src/main/java/lotto/WinningPayer.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 
+import static lotto.Place.*;
+
 enum Place {
     FIRST(7, 2000000000), SECOND(6, 30000000),
     THIRD(5, 1500000), FOURTH(4, 50000), FIFTH(3, 5000);
     int count;
     int winning;
 
-    Place (int count, int winning) {ㄹㄷ
+    Place (int count, int winning) {
         this.count = count;
         this.winning = winning;
     }
@@ -18,16 +20,21 @@ enum Place {
     int getCount() {
         return count;
     }
+
+    int getWinning() {
+        return winning;
+    }
 }
 
 public class WinningPayer {
     List<Integer> result = new ArrayList<Integer>();
-    int money;
+    float money;
+    float winningRate;
     EnumMap<Place,Integer> resultMap = new EnumMap<Place, Integer>(Place.class);
 
     WinningPayer(List<Integer> result, int money) {
         this.result = result;
-        this.money = money-money%1000;
+        this.money = (float) money;
     }
 
     void makeEnumMap() {
@@ -48,6 +55,26 @@ public class WinningPayer {
         for (int i : result) {
             countResult(i);
         }
+    }
+
+    void calculateWinningRate() {
+        float winning = 0;
+
+        for (Place p : Place.values()) {
+            winning += resultMap.get(p) * p.getWinning();
+        }
+        winningRate = winning / money * 100;
+    }
+
+    void informResult() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + resultMap.get(FIFTH) + "개 입니다.");
+        System.out.println("4개 일치 (50,000원) - " + resultMap.get(FOURTH) + "개 입니다.");
+        System.out.println("5개 일치 (1,500,000원) - " + resultMap.get(THIRD) + "개 입니다.");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + resultMap.get(SECOND) + "개 입니다.");
+        System.out.println("6개 일치 (2,000,000,000원) - " + resultMap.get(FIRST) + "개 입니다.");
+        System.out.println("총 수익률은 " + winningRate + "%입니다.");
     }
 }
 
