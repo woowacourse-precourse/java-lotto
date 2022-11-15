2c9981e0bbd204602ac2533a272ba35f675dff44
CJ <choijun0627@naver.com>
2022-11-15 09:28:49 +0900

    <feat>(InputTest.java): 기능 요구 사항 9번 테스트 코드 구현

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -6,15 +6,31 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.*;
 
 class InputTest {
-
+    Input input=new Input();
     @Test
     void checkMoneyTest() {
-        Input input=new Input();
-
         assertThatThrownBy(() -> input.checkMoney("1000j"))
                 .isInstanceOf(IllegalArgumentException.class);
 
         assertThatThrownBy(() -> input.checkMoney("12400"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void checkLottoTest() {
+        assertThatThrownBy(() -> input.checkLotto("33j"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> input.checkLotto("j"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> input.checkLotto("#"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> input.checkLotto("0"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> input.checkLotto("90"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
