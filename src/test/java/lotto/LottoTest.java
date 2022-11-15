5437ab0c1c087084d8bcd3b2ea112af1b3392fcb
Lee-SungMin <saevers26@gmail.com>
2022-11-15 17:52:32 +0900

    test: 로또 구입 금액 확인 예외 기능 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,5 +24,11 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 구입 금액의 단위가 1000원이 아니면 예외가 발생한다.")
+    @Test
+    void buyLottoNotUnit1000Won() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> Lotto.calculate("1200"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
