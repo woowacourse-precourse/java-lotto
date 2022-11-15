bca079fe63b114b3f1c5b0ba9b1322bba10ec46f
gnoesnooj <306yyy@naver.com>
2022-11-15 16:36:22 +0800

    style(LottoGenerator) : 컨벤션 수정

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -8,9 +8,15 @@ import java.util.stream.Collectors;
 
 public class LottoGenerator {
 
+    private final static int START = 1;
+    private final static int END = 45;
+    private final static int DIGIT = 6;
+
     public List<Integer> generateNumbers() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-                .stream().sorted().collect(Collectors.toList());
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START, END, DIGIT)
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
         validateLottoNumbers(numbers);
         return numbers;
     }
