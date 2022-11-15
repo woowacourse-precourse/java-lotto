7d7146130bc78d58aa1de4a9151b17c7017ec159
desfox <desfox99@naver.com>
2022-11-15 11:29:54 +0900

    refactor(BonusNumberInputValidation): 매개변수 이름 수정

--- src/main/java/lotto/validation/BonusNumberInputValidation.java
+++ src/main/java/lotto/validation/BonusNumberInputValidation.java
@@ -6,30 +6,30 @@ public class BonusNumberInputValidation {
     public static final int MIN_LOTTO_NUM = 1;
     public static final int MAX_LOTTO_NUM = 45;
 
-    public void validate(List<Integer> predictionNumbers, String userInputBonusNumber) {
-        validateInputIsInteger(userInputBonusNumber);
-        validateNoDuplicationWithPredictionNumbers(predictionNumbers, userInputBonusNumber);
-        validateNumberRange(userInputBonusNumber);
+    public void validate(List<Integer> predictionNumbers, String inputBonusNumber) {
+        validateInputIsInteger(inputBonusNumber);
+        validateNoDuplicationWithPredictionNumbers(predictionNumbers, inputBonusNumber);
+        validateNumberRange(inputBonusNumber);
     }
 
-    public void validateInputIsInteger(String userInputBonusNumber) {
+    public void validateInputIsInteger(String inputBonusNumber) {
         try {
-            Integer.parseInt(userInputBonusNumber);
+            Integer.parseInt(inputBonusNumber);
         } catch (Exception e) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
         }
     }
 
-    public void validateNoDuplicationWithPredictionNumbers(List<Integer> predictionNumbers, String userInputBonusNumber) {
-        int bonusNumber = Integer.parseInt(userInputBonusNumber);
+    public void validateNoDuplicationWithPredictionNumbers(List<Integer> predictionNumbers, String inputBonusNumber) {
+        int bonusNumber = Integer.parseInt(inputBonusNumber);
 
         if (predictionNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 예측 번호와 중복되는 번호를 입력할 수 없습니다.");
         }
     }
 
-    public void validateNumberRange(String userInputBonusNumber) {
-        int bonusNumber = Integer.parseInt(userInputBonusNumber);
+    public void validateNumberRange(String inputBonusNumber) {
+        int bonusNumber = Integer.parseInt(inputBonusNumber);
 
         if (bonusNumber < MIN_LOTTO_NUM || bonusNumber > MAX_LOTTO_NUM) {
             throw new IllegalArgumentException("[ERROR] 번호의 범위는 1이상 45이하여야 합니다.");
