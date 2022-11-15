d8bcd035db2814f5ff456e0fc8adcde8ffe6d69f
kimJaeHyeon <kmr2644@gmail.com>
2022-11-15 10:15:56 +0900

    refactor: compare 메서드 리팩토링

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import lotto.type.Prize;
 
 public class Lotto {
@@ -20,25 +19,18 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
-    // TODO: 추가 기능 구현
 
     public List<Integer> getNumbers() {
         return numbers;
     }
 
     public Prize compareWinningNumbers(List<Integer> winningNumbers, Integer bonusNumber) {
-        List<Integer> matchWinningNumbers = numbers.stream()
+        int matchNumberCount = (int) numbers.stream()
                 .filter(winningNumbers::contains)
-                .collect(Collectors.toList());
-        if (matchWinningNumbers.size() == 5) {
-            long count = numbers.stream()
-                    .filter(bonusNumber::equals)
-                    .count();
-            if (count == 1) {
-                return Prize.FIVE_AND_BONUS;
-            }
-            return Prize.FIVE;
+                .count();
+        if (matchNumberCount == 5 && numbers.contains(bonusNumber)) {
+            return Prize.FIVE_AND_BONUS;
         }
-        return Prize.of(matchWinningNumbers.size());
+        return Prize.of(matchNumberCount);
     }
 }
