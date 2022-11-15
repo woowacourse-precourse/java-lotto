f08618ae928a1f51190cfc0fa28a09a5124542f7
code-tree <dltjdqls1200@naver.com>
2022-11-15 11:15:59 +0900

    refactor: Rank가 등수에 따른 당첨 금액을 가지고 있도록 수정

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,24 +3,25 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6),
-    SECOND(5, true),
-    THIRD(5),
-    FOURTH(4),
-    FIFTH(3),
-    NONE(0);
+    FIRST(2_000_000_000L, 6),
+    SECOND(30_000_000L, 5, true),
+    THIRD(1_500_000L, 5),
+    FOURTH(50_000L, 4),
+    FIFTH(5_000L, 3),
+    NONE(0L, 0);
 
+    long winningAmount;
     int coincideCount;
     boolean coincideBonus;
 
-
-    Rank(int coincideCount, boolean coincideBonus) {
+    Rank(long winningAmount, int coincideCount, boolean coincideBonus) {
+        this.winningAmount = winningAmount;
         this.coincideCount = coincideCount;
         this.coincideBonus = coincideBonus;
     }
 
-    Rank(int coincideCount) {
-        this(coincideCount, false);
+    Rank(long winningAmount, int coincideCount) {
+        this(winningAmount, coincideCount, false);
     }
 
     public static Rank decide(long coincideCount, boolean coincideBonus) {
