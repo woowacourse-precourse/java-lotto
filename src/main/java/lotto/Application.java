585b95a45265f5ab201a75c3507947fa5911ba44
chesthyeon <niw927@naver.com>
2022-11-15 22:21:58 +0900

    예외전파를 위해 try catch를 사용. 로또 기능의 필요한 당첨 로또와 사용자 로또의 비교 기능, 수익률 계산, 사용자 로또 만들기 기능, 즉 컨트롤러가 해야하는 모든 기능을 작성함.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,37 +1,27 @@
 package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
-import java.io.*;
-import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) throws IllegalArgumentException{
-        System.out.println("구입금액을 입력해 주세요.");
-        String input = readLine();
-        purchaseAmountValidation(input);
-        User user = new User(Integer.parseInt(input));
-
-        purchaseLotto(user);
-        System.out.println(user.getLottoPurchaseCount() + "개를 구매했습니다.");
-        printUserLottos(user);
-
-        System.out.println("당첨 번호를 입력해 주세요.");
-        input = readLine();
-        lottoInputValidation(input);
-        Lotto winLotto = new Lotto(makeWinLottoNumber(input));
-
-
-        System.out.println("보너스 번호를 입력해 주세요.");
-        input = readLine();
-        bonusInputValidation(input);
-        int bonusNumber = Integer.parseInt(input);
-
-        Hashtable<Integer, Integer> counts = countsInitialization();
-        userLottoCheck(counts, user.getLottos(),winLotto,bonusNumber);
-        winningStatistics(counts);
-
-        calculateProfit(counts,user.getPurchaseAmount());
-
+        try {
+            Input input = new Input();
+            input.purchaseAmount();
+            User user = new User(Integer.parseInt(input.getInput()));
+            purchaseLotto(user);
+            System.out.println("\n"+user.getLottoPurchaseCount() + LottoPrint.boughtLotto.getDescription());
+            printUserLottos(user);
+            input.winningNumber();
+            Lotto winLotto = new Lotto(makeWinLottoNumber(input.getInput()));
+            input.bonusNumber(winLotto.getNumbers());
+            int bonusNumber = Integer.parseInt(input.getInput());
+            Hashtable<Integer, Integer> counts = countsInitialization();
+            userLottoCheck(counts, user.getLottos(),winLotto,bonusNumber);
+            System.out.println(LottoPrint.winningStatistics(counts));
+            calculateProfit(counts,user.getPurchaseAmount());
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
     public static Hashtable<Integer,Integer> countsInitialization(){
@@ -42,22 +32,12 @@ public class Application {
         return counts;
     }
 
-    public static void purchaseAmountValidation(String purchaseAmount) throws IllegalArgumentException{
-        if (!purchaseAmount.matches("[0-9]+")){
-            System.out.println("[ERROR]");
-            throw new IllegalArgumentException("[ERROR]");
-        }
-    }
-
-    public static void lottoInputValidation(String lottoInput) throws IllegalArgumentException{
-        if (!lottoInput.matches("[0-9,]+")){
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-    }
-
-    public static void bonusInputValidation(String bonusInput) throws IllegalArgumentException{
-        if (!bonusInput.matches("[0-45]")){
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    public static void winningLottoCheck(List<Integer> numbers){
+        for (int number: numbers
+        ) {
+            if (1> number || number > 45){
+                throw new IllegalArgumentException(ErrorPrint.lottoNumber.getErrorMessage());
+            }
         }
     }
 
@@ -75,7 +55,6 @@ public class Application {
         StringBuilder printLottos = new StringBuilder();
         List<Lotto> lottos = user.getLottos();
         Lotto lotto;
-        printLottos.append("\n");
         for (Lotto value : lottos) {
             lotto = value;
             printLottos.append(lotto.getNumbers().toString()).append("\n");
@@ -89,6 +68,7 @@ public class Application {
         while (stringTokenizer.hasMoreTokens()){
             lottoNumber.add(Integer.parseInt(stringTokenizer.nextToken()));
         }
+        winningLottoCheck(lottoNumber); // 만들어진 로또 번호가 1~45 사이의 값을 가지고 있는 검증
         return lottoNumber;
     }
 
@@ -117,21 +97,12 @@ public class Application {
         }
     }
 
-    public static void winningStatistics(Hashtable<Integer,Integer> counts){
-        String stringBuilder = "3개 일치 (5,000원) - " + counts.get(3) + "개\n" +
-                "4개 일치 (50,000원) - " + counts.get(4) + "개\n" +
-                "5개 일치 (1,500,000원) - " + counts.get(5) + "개\n" +
-                "5개 일치, 보너스 볼 일치 (30,000,000원) - " + counts.get(7) + "개\n" +
-                "6개 일치 (2,000,000,000원) - " + counts.get(6) + "개\n";
-        System.out.println(stringBuilder);
-    }
-
     public static void calculateProfit(Hashtable<Integer,Integer> counts, int purchaseAmount){
         double profit = counts.get(7) * 30000000;
-        profit += counts.get(3) * 5000;
-        profit += counts.get(4) * 50000;
-        profit += counts.get(5) * 150000;
-        profit += counts.get(6) * 2000000000;
-        System.out.println("총 수익률은 "+String.format("%.1f%%입니다.",profit/purchaseAmount*100));
+        List<Integer> winningAmount = new ArrayList<>(Arrays.asList(5000,50000,150000,2000000000));
+        for (int i = 3; i <= 6; i++){
+            profit += counts.get(i) * winningAmount.get(i - 3);
+        }
+        System.out.println(LottoPrint.profitReturn(profit/purchaseAmount*100));
     }
 }
