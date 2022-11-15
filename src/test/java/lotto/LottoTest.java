108f2296b9421aa88b6f8380879fa4077ea175ee
DavinAn <blueapple1030@gmail.com>
2022-11-16 00:33:27 +0900

    test : add exception test cases

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,9 +3,13 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -36,4 +40,37 @@ class LottoTest {
                 .hasMessageContaining(expected);
     }
 
+    @ParameterizedTest
+    @MethodSource("stringList")
+    void winningNumbersException(List<String> stringList, String expected) {
+        assertThatThrownBy(() -> exception.winningNumbersException(stringList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(expected);
+    }
+
+    static Stream<Arguments> stringList() {
+        return Stream.of(
+                Arguments.of(Arrays.asList("11", "37", "4"), "[ERROR] 04"),
+                Arguments.of(Arrays.asList("38", "21", "z", "8", "29", "10"), "[ERROR] 06"),
+                Arguments.of(Arrays.asList("18", "40", "19", "56", "0", "81"), "[ERROR] 05"),
+                Arguments.of(Arrays.asList("22", "23", "11", "27", "9", "9"), "[ERROR] 07")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("winningNum")
+    void bonusNumberException(List<Integer> winningNum, String bonusNum, String expected) {
+        assertThatThrownBy(() -> exception.bonusNumberException(winningNum, bonusNum))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(expected);
+    }
+
+    static Stream<Arguments> winningNum() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1,3,5,7,9), "", "[ERROR] 01"),
+                Arguments.of(Arrays.asList(1,3,5,7,9), "g", "[ERROR] 02"),
+                Arguments.of(Arrays.asList(1,3,5,7,9), "88", "[ERROR] 05"),
+                Arguments.of(Arrays.asList(1,3,5,7,9), "5", "[ERROR] 07")
+        );
+    }
 }
