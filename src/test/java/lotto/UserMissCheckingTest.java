113ddab05fe4c398044798d37ad13cd609d974cd
ijung-won <dlwnddnjs96@gmail.com>
2022-11-15 18:40:31 +0900

    test(UserMissCheckingTest): 잘못된 보너스 번호 입력 시 에러 테스트

--- src/test/java/lotto/UserMissCheckingTest.java
+++ src/test/java/lotto/UserMissCheckingTest.java
@@ -9,11 +9,18 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserMissCheckingTest {
 
-    private final List<Integer> wrongLotto = new ArrayList<>(List.of(1,3,3,6,12,35));
-    private final List<Integer> wrongLotto2 = new ArrayList<>(List.of(1,3,3,6,3,35));
-    private final List<Integer> wrongLotto3= new ArrayList<>(List.of(1,3,3,6,6,35));
+    private final List<Integer> lotto = new ArrayList<>(List.of(1, 3, 6, 12, 35, 44));
+    private final String input = "14";
+
+    private final List<Integer> wrongLotto = new ArrayList<>(List.of(1, 3, 3, 6, 12, 35));
+    private final List<Integer> wrongLotto2 = new ArrayList<>(List.of(1, 3, 3, 6, 3, 35));
+    private final List<Integer> wrongLotto3 = new ArrayList<>(List.of(1, 3, 3, 6, 6, 35));
+    private final String wrongInput = "1";
+    private final String wrongInput2 = "사십";
+    private final String wrongInput3 = "35";
+
     @Test
-    public void 숫자대신_문자입력_에러(){
+    public void 숫자대신_문자입력_에러() {
         assertThatThrownBy(() -> UserMissChecking.numberChecking("사십사"))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> UserMissChecking.numberChecking("test"))
@@ -23,30 +30,40 @@ class UserMissCheckingTest {
     }
 
     @Test
-    public void 로또금액_첫원단위_미입금(){
-        assertThatThrownBy(()->UserMissChecking.thousandChecking(4200))
+    public void 로또금액_첫원단위_미입금() {
+        assertThatThrownBy(() -> UserMissChecking.thousandChecking(4200))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(()->UserMissChecking.thousandChecking(12305))
+        assertThatThrownBy(() -> UserMissChecking.thousandChecking(12305))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(()->UserMissChecking.thousandChecking(123130))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    public void 지정된_로또_숫자범위_이외_숫자_삽입(){
-        assertThatThrownBy(()->UserMissChecking.numberLimitChecking(0))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(()->UserMissChecking.numberLimitChecking(47))
+        assertThatThrownBy(() -> UserMissChecking.thousandChecking(123130))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    public void 로또_당침번호_입력시_중복체크(){
-        assertThatThrownBy(()-> UserMissChecking.numRepeatCheck(wrongLotto))
+    public void 지정된_로또_숫자범위_이외_숫자_삽입() {
+        assertThatThrownBy(() -> UserMissChecking.numberLimitChecking(0))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(()-> UserMissChecking.numRepeatCheck(wrongLotto2))
+        assertThatThrownBy(() -> UserMissChecking.numberLimitChecking(47))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(()-> UserMissChecking.numRepeatCheck(wrongLotto3))
+    }
+
+    @Test
+    public void 로또_당침번호_입력시_중복체크() {
+        assertThatThrownBy(() -> UserMissChecking.numRepeatCheck(wrongLotto))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> UserMissChecking.numRepeatCheck(wrongLotto2))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> UserMissChecking.numRepeatCheck(wrongLotto3))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 보너스_숫자_넣기_테스트(){
+        assertThatThrownBy(()-> UserMissChecking.bonusNumberChecking(lotto, wrongInput))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()-> UserMissChecking.bonusNumberChecking(lotto, wrongInput2))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()-> UserMissChecking.bonusNumberChecking(lotto, wrongInput3))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
