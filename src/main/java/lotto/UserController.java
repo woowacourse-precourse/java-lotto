b408a22e8bdeffe514ad1078f4f1507d2135f5cd
mng990 <ryoo0504@pusan.ac.kr>
2022-11-15 16:16:39 +0900

    feat(main/java/lotto): UserController.java 생성
    + purchaseLottery: 로또 구입
    + howMuch: 로또 발행
    + convertLotteries: 로또 리스트를 출력 함수에 맞게 변형

---
+++ src/main/java/lotto/UserController.java
@@ -0,0 +1,42 @@
+package lotto;
+
+import static lotto.ExceptionHandler.parseInt;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Lotto;
+import model.User;
+import view.UserUI;
+
+public class UserController {
+    private User   model;
+    private UserUI view;
+
+    public void purchaseLottery(){
+        String purchase = view.getPurchaseMoney();
+        ExceptionHandler.validPurchaseForm(purchase);
+
+        howMuch(Integer.parseInt(purchase));
+    }
+
+    public void howMuch(int purchase){
+        int loop = purchase / 1000;
+        List<Lotto> lotteries = new ArrayList<>();
+
+        for(int i=0;i<loop;i++){
+            LottoController.makeLottery();
+            lotteries.add(LottoController.getLottery());
+        }
+
+        model = new User(lotteries);
+    }
+
+    public List<List<Integer>> convertLotteries(){
+        List<List<Integer>> lotteries = new ArrayList<>();
+        for(Lotto lottery : model.getLotteries()){
+            lotteries.add(lottery.numbers());
+        }
+
+        return lotteries;
+    }
+}
