9dd952bfd3457606f77139f2f30a9dc7752ae080
juno-junho <ssmm0205@naver.com>
2022-11-15 19:44:06 +0900

    feat(view): add printBoughtLotteryNumber method
    구매한 로또 개수를 출력하는 메서드 생성

--- src/main/java/view/Output.java
+++ src/main/java/view/Output.java
@@ -1,4 +1,20 @@
 package view;
 
+import lotto.Lotto;
+
 public class Output {
+    private static final String BUY_MESSAGE = "%d개를 구매했습니다.";
+    private static final String RESULT_START_MESSAGE = "당첨 통계\n---";
+    private static final String RESULT_RANK_MESSAGE = "%d개 일치 (%s원)- %d개";
+    private static final String RESULT_BOUNS_RANK_MESSAGE = "%d개 일치%s (%s원) - %d개";
+    private static final String BONUS_MESSAGE = ", 보너스 볼 일치";
+    private static final String EARNING_RATE_MESSAGE = "총 수익률은 %.2f%입니다.";
+
+    public static void printBoughtLotteryNumber(int count) {
+        System.out.printf(BUY_MESSAGE, count);
+    }
+
+    public static void printRandomLotteryNumber(Lotto lotto) {
+
+    }
 }
