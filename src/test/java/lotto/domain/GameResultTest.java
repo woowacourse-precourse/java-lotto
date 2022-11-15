38df2beba01d4b4c93dfc3892f2e7730a965e406
CAEL0 <kjongmin26@gmail.com>
2022-11-15 18:57:28 +0900

    test(GameResultTest): 2개가 일치하면 낙첨

--- src/test/java/lotto/domain/GameResultTest.java
+++ src/test/java/lotto/domain/GameResultTest.java
@@ -86,4 +86,18 @@ public class GameResultTest {
 
         assertThat(result).isEqualTo(List.of(0, 0, 0, 0, 0, 1));
     }
+
+    @DisplayName("2개가 일치하면 낙첨이다.")
+    @Test
+    void NotWinIfTwoNumbersAreMatched() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Lotto> lottos = new ArrayList<>(List.of(lotto));
+        List<Integer> winningNumber = new ArrayList<>(List.of(1, 2, 7, 8, 9, 10));
+        int bonusNumber = 11;
+
+        gameResult.calculateTotalResult(lottos, winningNumber, bonusNumber);
+        List<Integer> result = gameResult.getResult();
+
+        assertThat(result).isEqualTo(List.of(1, 0, 0, 0, 0, 0));
+    }
 }
