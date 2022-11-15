33eb9d8ce5f6b497183f1fd42a6eab6609da52e9
c4fiber <qudcjf153@gmail.com>
2022-11-15 22:14:23 +0900

    test(WalletTest): 구매한 로또가 없을때도 결과를 정상적으로 출력하는지 검증하는 테스트 추가

--- src/test/java/lotto/WalletTest.java
+++ src/test/java/lotto/WalletTest.java
@@ -1,11 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.Collections;
 
-class WalletTest {
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WalletTest extends NsTest {
     @DisplayName("0보다 적은 값을 설정하면 오류가 발생한다.")
     @Test
     void setMoneyLowerThanZero() {
@@ -13,5 +17,19 @@ class WalletTest {
             Wallet wallet = new Wallet();
             wallet.setMoney("-1");
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구매한 로또가 없으면 0개를 구매했다고 출력한다.")
+    @Test
+    void showMyLottosWithEmptyList() {
+        Wallet wallet = new Wallet();
+        wallet.buyLottos(Collections.emptyList());
+        wallet.showMyLottos();
+        assertThat(output()).contains("0개를 구매했습니다.");
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
     }
 }
