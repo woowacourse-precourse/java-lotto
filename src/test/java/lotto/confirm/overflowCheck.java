ec3e34e57794854b89cf82ea2fbc650f4939812c
kswhj <108874006+kswhj@users.noreply.github.com>
2022-11-15 17:19:45 +0900

    Delete overflowCheck.java

--- src/test/java/lotto/confirm/overflowCheck.java
+++
@@ -1,9 +0,0 @@
-package confirm;
-
-public class overflowCheck {
-	public static void run(int i) {
-		if(i>45 || i<1) {
-			 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-		}
-	}
-}
