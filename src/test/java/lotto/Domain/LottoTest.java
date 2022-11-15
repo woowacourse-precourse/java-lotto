5e8c949ce1e953b1df67395a309ba009a587f7e1
nakalic <nakalic28@gmail.com>
2022-11-15 16:53:25 +0900

    test(LottoTest): 로또 한 개 발행 테스트 코드 추가

--- src/test/java/lotto/Domain/LottoTest.java
+++ src/test/java/lotto/Domain/LottoTest.java
@@ -1,6 +1,5 @@
-package lotto;
+package lotto.Domain;
 
-import lotto.Domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -24,5 +23,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1~45 사이의 숫자가 아니라면 예외가 발생한다.")
+    @Test
+    void createLottoByNumberOutOfRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
