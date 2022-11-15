c055d59233d88199099f84430e7cee730b06406b
choijoohee213 <choijoohee@naver.com>
2022-11-15 23:57:19 +0900

    test: 로또 당첨번호 기능 테스트(예외 테스트) 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.service.ResultService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,10 +19,23 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1~45가 아니면 예외가 발생한다.")
+    @Test
+    void createLottoIsNotInRange() {
+        assertThatThrownBy(
+                () -> ResultService.getInstance().validateWinningNumbers(List.of("1", "2", "3", "4", "5", "46")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void createLottoIsNotDigit() {
+        assertThatThrownBy(
+                () -> ResultService.getInstance().validateWinningNumbers(List.of("1", "2", "3", "4", "5", "안녕")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
