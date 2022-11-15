c060fc6be39b8b6c7e73af36f974b3ef0ad29b1f
kite707 <kite707707@gmail.com>
2022-11-15 17:47:40 +0900

    feat(lotto): 로또 발행 시 중복 숫자 있으면 error발생

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
@@ -14,6 +16,29 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        for(int i = 0; i<numbers.size();i++){
+            isDuplicated(numbers,numbers.get(i),i);
+        }
+    }
+
+    public void isDuplicated(List<Integer> numbers, int number,int index) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if(i==index){
+                continue;
+            }
+            if (numbers.get(i) == number) {
+                throw new IllegalArgumentException("[ERROR] 서로 다른 숫자를 입력해주세요!");
+            }
+        }
+    }
+
+    public void printLotto(){
+        System.out.print('[');
+        for(int i =0;i<numbers.size()-1;i++){
+            System.out.print(numbers.get(i));
+            System.out.print(", ");
+        }
+        System.out.print(numbers.get(numbers.size()-1)+']');
     }
 
     // TODO: 추가 기능 구현
