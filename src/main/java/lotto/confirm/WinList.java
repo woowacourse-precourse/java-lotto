b7e9a9156d36f4801c909c443d8ef60858d84363
kswhj <108874006+kswhj@users.noreply.github.com>
2022-11-15 21:55:53 +0900

    Update WinList.java
    당첨 리스트 받기 추가

--- src/main/java/lotto/confirm/WinList.java
+++ src/main/java/lotto/confirm/WinList.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 public class WinList {
 	public static List<Integer> run(List<List<Integer>> playerPick, List<Integer> winNums) {
+		List<Integer> answer = Arrays.asList(0, 0, 0, 0, 0);
 		for (int i = 0; i < playerPick.size(); i++) {
 			int num = WinNumCheck.run(playerPick.get(i), winNums);
-			winNums.set(i, num);
+			answer = NumberInput.run(num, answer);
 		}
-		return winNums;
+		return answer;
 	}
 }
