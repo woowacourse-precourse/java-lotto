3a0a724b6f103bf53a371a79acc4666d792216e4
Somi Kim <rlathal009@gmail.com>
2022-11-15 17:17:36 +0900

    feat : 로또 당첨번호 입력받기 코드 추가

--- src/main/java/lotto/exception/LottoError.java
+++ src/main/java/lotto/exception/LottoError.java
@@ -4,7 +4,8 @@ public enum LottoError {
     NOT_NUMERIC("금액은 숫자여야 합니다."),
     WRONG_UNIT_OF_WON("금액은 1000원 단위여야 합니다."),
     INVALID_NUMBERS_LENGTH("6개의 숫자로 이루어져야 합니다."),
-    DISTINCT_NUMBERS("중복된 숫자가 존재합니다.");
+    DISTINCT_NUMBERS("중복된 숫자가 존재합니다."),
+    INVALID_NUMBER_RANGE("숫자는 1 ~ 45 사이의 범위여야 합니다.");
 
     private final String errorMessage;
 
