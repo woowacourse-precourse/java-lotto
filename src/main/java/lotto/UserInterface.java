8a99d8e3183256bb4a6065c6b10c6228d99d9cf3
kwondaehwi <kwondh8561@ajou.ac.kr>
2022-11-15 17:41:38 +0900

    feat: UserInterface 클래스 구현

---
+++ src/main/java/lotto/UserInterface.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserInterface {
+    Validation validation = new Validation();
+
+    public int getPrice() {
+    }
+
+    public List<Integer> getWinningNumbers() {
+    }
+
+    public int getBonusNumber() {
+
+    }
+}
