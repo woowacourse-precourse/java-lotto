77ede10a089cde3d309704130faab28c6feca122
ohhonggi <5ghdrl@naver.com>
2022-11-15 23:00:55 +0900

    feat($WinStatistics): Add calculateWinningAmount function

--- src/main/java/lotto/domain/WinStatistics.java
+++ src/main/java/lotto/domain/WinStatistics.java
@@ -1,14 +1,29 @@
 package lotto.domain;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.counting;
+
 public class WinStatistics {
-    private final Rank rank;
+    private final Map<Rank, Long> ranks;
     private final EarningRate earnRate;
 
-
-    public WinStatistics(Rank rank, EarningRate earnRate) {
-        this.rank = rank;
+    public WinStatistics(Map<Rank, Long> ranks, EarningRate earnRate) {
+        this.ranks = ranks;
         this.earnRate = earnRate;
     }
 
+    public WinStatistics calculateWinningAmount(Amounts amount, List<Rank> ranks) {
+        Map<Rank, Long> totalRank = ranks.stream().
+                collect(Collectors.groupingBy(rank -> rank, counting()));
+
+        int totalWinAmount = ranks.stream().
+                map(Rank::getReward).reduce(0, Integer::sum);
+
+        return new WinStatistics(totalRank,
+                EarningRate.calculateRateOfProfit(totalWinAmount, amount));
+    }
 
 }
