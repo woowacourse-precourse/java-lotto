ef9497d9f80137b4c1accd21d0166d544c6dde47
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-15 14:14:50 +0900

    refactor(LottoRank): bonus 필드 타입 boolean 으로 변경
    보너스의 매치 개수는 최대 1개로 정해져 있으니 보너스 넘버가 당첨됬는지 판단만 하도록 변경

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -4,29 +4,29 @@ import java.util.Arrays;
 
 public enum LottoRank {
 
-    FIRST( 6,  0, 2_000_000_000),
-    SECOND(5, 1, 30_000_000),
-    THIRD(5, 0, 1_500_000),
-    FOURTH(4, 0,  50_000),
-    FIFTH(3, 0,  5_000),
-    FAIL(0, 0, 0);
+    FIRST( 6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false,  50_000),
+    FIFTH(3, false,  5_000),
+    FAIL(0, false, 0);
 
     private final int matchCount;
 
-    private final int bonusCount;
+    private final boolean bonus;
 
     private final int winMoney;
 
-    LottoRank(int matchCount, int bonusCount, int winMoney) {
+    LottoRank(int matchCount, boolean bonus, int winMoney) {
         this.matchCount = matchCount;
-        this.bonusCount = bonusCount;
+        this.bonus = bonus;
         this.winMoney = winMoney;
     }
 
-    public static LottoRank of(int result, int bonus) {
+    public static LottoRank of(int result, boolean bonus) {
         return Arrays.stream(LottoRank.values())
                 .filter(lottoRank -> lottoRank.matchCount == result)
-                .filter(lottoRank -> lottoRank.bonusCount == bonus)
+                .filter(lottoRank -> !lottoRank.bonus || bonus)
                 .findAny()
                 .orElse(FAIL);
     }
@@ -36,7 +36,7 @@ public enum LottoRank {
     }
 
     public boolean isBonusNumber() {
-        return bonusCount == 1;
+        return bonus;
     }
 
     public int matchCount() {
