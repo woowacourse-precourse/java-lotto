311af0208fb42d159386caa111b287799f5cbd5b
LeeMyeongJin <dlaudwls1203@gmail.com>
2022-11-15 19:28:19 +0900

    feat: 예외 처리 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,11 +6,12 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Application {
 
-    static List<List<Integer>> lotteNumbers = new ArrayList<>();
+    static List<List<Integer>> lottoNumbersList = new ArrayList<>();
     public static void main(String[] args) {
 
         // TODO: 프로그램 구현
@@ -26,19 +27,30 @@ public class Application {
 
         System.out.println("보너스 번호를 입력해 주세요.");
         int BonusNum = BonusNumber();
+        if(LottoNumbers.contains(BonusNum))
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복지 않은 숫자여야 합니다.");
         System.out.println();
 
         System.out.println("당첨 통계");
         System.out.println("---");
         // Lotto lotto = new Lotto(LottoNumbers);
+        Lotto A = new Lotto(LottoNumbers);
+        A.Statistics(lottoNumbersList, BonusNum);
 
     }
 
-    private static int BonusNumber() {
+    public static int BonusNumber() {
         int Bonus = Integer.parseInt(Console.readLine());
-        if (Bonus < 0 || Bonus > 46){
+        if (Bonus < 1 || Bonus > 45){
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
+
+        String StrBonus = Integer.toString(Bonus);
+        if (StrBonus.replaceAll("[^0-9]","").length()!=StrBonus.length()) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값을 입력하였습니다.");
+        }
+
+
         return Bonus;
     }
 
@@ -48,6 +60,9 @@ public class Application {
         List<Integer> IntSplitNum = SplitNum.stream()
                                             .map(Integer::parseInt)
                                             .collect(Collectors.toList());
+        if (IntSplitNum.size() != 6){
+            throw new IllegalArgumentException("[ERROR] 당첨번호는 6개 여야합니다..");
+        }
         return IntSplitNum;
 
 
@@ -61,7 +76,7 @@ public class Application {
         for (int i = 0; i < LottoCount; i++) {
             List<Integer> List = RandomUniqueNumbers();
             Collections.sort(List);
-            lotteNumbers.add(List);
+            lottoNumbersList.add(List);
             System.out.println(List);
 
         }
@@ -69,14 +84,25 @@ public class Application {
 
     public static int purchaseInput() {
 
-        int purchase = Integer.parseInt(Console.readLine());
-        if (purchase < 1000) {
+        String purchase = Console.readLine();
+
+        String pattern = "^[0-9]*$";
+        boolean result = Pattern.matches(pattern, purchase);
+
+        if (!result){
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+        }
+
+        int Intpurchase = Integer.parseInt(purchase);
+
+        if (Intpurchase < 1000) {
             throw new IllegalArgumentException("[ERROR] 구매 금액이 1000보다 낮습니다.");
         }
-        if (purchase % 1000 != 0) {
+        if (Intpurchase % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 단위가 1000단위가 아닙니다.");
         }
-        return purchase / 1000;
+
+        return Intpurchase / 1000;
 
     }
 
