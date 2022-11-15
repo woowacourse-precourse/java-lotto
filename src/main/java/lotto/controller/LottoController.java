3f1e3c285c5d6ef42d3ab9dae060a94347bb27aa
hgo641 <hgo641@gmail.com>
2022-11-15 16:38:19 +0900

    refactor: 유저 로또 생성, 당첨 로또 생성, 당첨 통계 출력 기능 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,23 +1,45 @@
 package lotto.controller;
 
 import lotto.domain.*;
+import lotto.domain.strategy.InputLottoGenerateStrategy;
+import lotto.domain.strategy.RandomLottoGenerateStrategy;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+import lotto.view.printer.InputPrinter;
 
 import java.util.List;
 import java.util.Map;
 
 public class LottoController {
-    List<Lotto> userLottos;
+    Money userMoney;
+    List<Lotto> userMultipleLotto;
+    WinningLotto winningLotto;
 
     public void runLottoMachine() {
-        Money userMoney = Money.from(InputView.inputMoneyByUser());
-        userLottos = LottoGenerator.buyMultipleRandomLotto(userMoney);
-        OutputView.printUserLottos(userLottos);
-        Lotto winningNumber = LottoGenerator.getInputLotto(InputView.inputWinningNumbers());
+        userMoney = Money.from(InputView.inputMoneyByUser());
+        userMultipleLotto = createUserLottoGenerator().generateMultipleLotto(userMoney.getCountOfLotto());
+        OutputView.printUserLottos(userMultipleLotto);
+        winningLotto = createWinningLotto();
+        showUserWinningStatistics();
+    }
+
+    private LottoGenerator createUserLottoGenerator() {
+        return LottoGenerator.from(new RandomLottoGenerateStrategy());
+    }
+
+    private LottoGenerator createWinningLottoGenerator() {
+        return LottoGenerator.from(new InputLottoGenerateStrategy());
+    }
+
+    private WinningLotto createWinningLotto() {
+        InputPrinter.printInputWinningNumbersMessage();
+        Lotto winningNumber = createWinningLottoGenerator().generate();
         String bonusNumber = (InputView.inputBonusNumber());
-        WinningLotto winningLotto = WinningLotto.of(winningNumber, bonusNumber);
-        Map<Rank, Integer> userLottoScore = winningLotto.getUserLottoScore(userLottos);
+        return WinningLotto.of(winningNumber, bonusNumber);
+    }
+
+    private void showUserWinningStatistics() {
+        Map<Rank, Integer> userLottoScore = winningLotto.getUserLottoScore(userMultipleLotto);
         String profitRate = userMoney.calculateProfitRate(getTotalWinningPrize(userLottoScore));
         OutputView.printWinningStatistics(userLottoScore, profitRate);
     }
