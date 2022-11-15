fab30d107f67045aca11b1b43861e091f2d5d306
Sinryuji <siler2474@gmail.com>
2022-11-15 14:19:49 +0900

    feat(Output): 발행된 로또 출력하는 기능 구현

--- src/main/java/lotto/ui/Output.java
+++ src/main/java/lotto/ui/Output.java
@@ -1,2 +1,17 @@
-package lotto.ui;public class Output {
+package lotto.ui;
+
+import java.util.List;
+import lotto.domain.Lotto;
+
+public class Output {
+    private static final String buyLottoMessage = "개를 구매했습니다.";
+    private static final String statisticsMessage = "당첨통계\n---";
+
+    public static void outputBuyLotto(List<Lotto> lottos) {
+        System.out.println(lottos.size() + buyLottoMessage);
+
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers().toString());
+        }
+    }
 }
