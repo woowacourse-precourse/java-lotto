f0f438240b3a4d5875537051f5f971de61570420
GyuJeGal <lkjdee0329@gmail.com>
2022-11-15 17:12:45 +0900

    feat(OutputMessage): 출력하는 문자열 나열 출력할 문자열 상수타입으로 나열

---
+++ src/main/java/lotto/constant/OutputMessage.java
@@ -0,0 +1,10 @@
+package lotto.constant;
+
+public class OutputMessage {
+    public static final String PLEASE_WRITE_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String PRINT_PURCHASE_AMOUNT = "개를 구매했습니다.";
+    public static final String PLEASE_WRITE_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String PLEASE_WRITE_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public static final String WINNING_STATISTICS = "당첨 통계";
+    public static final String DIVISION = "---";
+}
