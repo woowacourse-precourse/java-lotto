95d6f86f5445b6a4400d21ff7c78ab6c414fd6c3
younghyun-na <younghyunna@naver.com>
2022-11-15 22:53:23 +0900

    :recycle: refactor: 에러 메세지 상수화

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -1,12 +1,16 @@
 package lotto;
 
 import java.util.HashSet;
-import java.util.stream.Collectors;
 
 public class WinningNumbers {
 
+  public static final String DUPLICATED_INPUT_WINNING_NUMBER_ERROR = "[ERROR] 당첨 번호에는 중복 숫자가 없어야 합니다.";
+  public static final String DUPLICATED_INPUT_BONUS_NUMBER_ERROR = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
+  public static final String OUT_OF_RANGE_INPUT_ERROR = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+
   private final Lotto winningNumber;
   private final int bonusNumber;
+
 
   public WinningNumbers(Lotto winningNumber, int bonusNumber) {
     validate(winningNumber, bonusNumber);
@@ -33,19 +37,19 @@ public class WinningNumbers {
 
   public void isDuplicatedWinningNum(Lotto winningNumber) {
     if (new HashSet<>(winningNumber.getNumbers()).size() != winningNumber.getNumbers().size()) {
-      throw new IllegalArgumentException("[ERROR] 당첨 번호에는 중복 숫자가 없어야 합니다.");
+      throw new IllegalArgumentException(DUPLICATED_INPUT_WINNING_NUMBER_ERROR);
     }
   }
 
   public void isDuplicatedBonusNum(Lotto winningNumber, int bonusNumber) {
     if (winningNumber.getNumbers().contains(bonusNumber)) {
-      throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+      throw new IllegalArgumentException(DUPLICATED_INPUT_BONUS_NUMBER_ERROR);
     }
   }
 
   public void isBetween(int number) {
     if (number < 1 || number > 45) {
-      throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 내에 있어야 합니다.");
+      throw new IllegalArgumentException(OUT_OF_RANGE_INPUT_ERROR);
     }
   }
 
