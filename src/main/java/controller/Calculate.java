501b558b2a96caafcec764d6df10bfb9c22f0826
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-15 15:23:20 +0900

    feat(Calculate) : add a function that calculate winning

--- src/main/java/controller/Calculate.java
+++ src/main/java/controller/Calculate.java
@@ -1,7 +1,11 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculate {
     private static final int PURCHASE_UNIT = 1000;
+    private static final String NO_WINNING = "NOTHING";
 
     public static int calculatePurchaseQuantity(int money) throws IllegalArgumentException {
         if (money % PURCHASE_UNIT == 0) {
@@ -13,5 +17,15 @@ public class Calculate {
     public static String calculateYield(int money, int winnings) {
         double yield = ((double) winnings / money) * 100;
         return String.format("%.1f", yield).concat("%");
+    }
+
+    public static String calculateWinning(List<Integer> winningCount) {
+        for (Winning winning : Winning.values()) {
+            List<List<Integer>> comparisonTarget = winning.getWinningCount();
+            if (comparisonTarget.contains(winningCount)) {
+                return winning.name();
+            }
+        }
+        return NO_WINNING;
     }
 }
