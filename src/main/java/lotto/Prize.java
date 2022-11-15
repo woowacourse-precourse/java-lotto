b303f7cfe6f71f30d2c3b1c074881c02ba843f1d
youswim <youswim96@gmail.com>
2022-11-15 15:40:36 +0900

    feat(lotto): Prize클래스에 NONE타입과 UNCHECKED타입 추가

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -5,7 +5,9 @@ public enum Prize {
     SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)"),
     THIRD("5개 일치 (1,500,000원)"),
     FOURTH("4개 일치 (50,000원)"),
-    FIFTH("3개 일치 (5,000원)");
+    FIFTH("3개 일치 (5,000원)"),
+    NONE("상금 없음"),
+    UNCHECKED("확인 전");
 
     private final String attribute;
     Prize(String attribute) {
