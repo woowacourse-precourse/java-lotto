d5b31f9e2c2cbb4709996c9c0a6a54e31906303e
LisaJeoung <jlisa9912234567890@gmail.com>
2022-11-15 09:39:58 +0900

    feat : 당첨번호, 보너스 번호 입력
    getLuckyNumber 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
@@ -14,7 +16,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 당첨 숫자는 6개 입니다.");
         }
     }
 
@@ -24,5 +26,18 @@ public class Lotto {
         int price = Integer.parseInt(Console.readLine());
         if (price % 1000 != 0)
             throw new IllegalArgumentException("[ERROR] 구입금액은 1,000원 단위로 입력해야 합니다.");
+    }
+    public void getLuckyNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] n = Console.readLine().split(",");
+        List<Integer> numbers = new ArrayList<>();
+        for (String s : n)
+            numbers.add(Integer.valueOf(s));
+        validate(numbers);
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonusNumber = Integer.parseInt(Console.readLine());
+        if (numbers.contains(bonusNumber))
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
     }
 }
