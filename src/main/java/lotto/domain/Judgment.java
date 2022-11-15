32b28386392603bb0f01398a240d3a6215f6f973
chaos0103 <lyt1228@naver.com>
2022-11-15 11:18:41 +0900

    style(Lotto): 변수명 변경

--- src/main/java/lotto/domain/Judgment.java
+++ src/main/java/lotto/domain/Judgment.java
@@ -1,12 +1,10 @@
 package lotto.domain;
 
-import lotto.Lotto;
-
 import java.util.List;
 
 public interface Judgment {
 
-    int correctCount(List<Integer> userNumbers, List<Integer> priceNumber);
+    int correctCount(List<Integer> userNumbers, List<Integer> priceNumbers);
 
     boolean hasBonusNumber(List<Integer> userNumbers, int bonusNumber);
 }
