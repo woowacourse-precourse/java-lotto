4e64459d925ba40d54af6d2a392a9e1cfa4b52e4
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-15 17:48:22 +0900

    style: 주석 저게

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -28,7 +28,6 @@ class LottoTest {
     @DisplayName("로또 번호의 범위가 1~45가 아니면 예외가 발생한다.")
     @Test
     void createLottoByOverLength() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 50)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
