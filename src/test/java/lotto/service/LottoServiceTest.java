ce0aae4cb04f10d43a5ce389908f1c8ebf832076
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-15 13:17:34 +0900

    test(LottoServiceTest): 로또 결과 2등 두개 테스트

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,5 +1,7 @@
 package lotto.service;
 
+import lotto.domain.Reward;
+import lotto.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +22,23 @@ public class LottoServiceTest {
         int matchCount = lottoService.matchLottoCount(winningNumbers, lottoNumbers);
 
         assertThat(matchCount).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("로또 번호 결과 저장 테스트 2등 두개")
+    void saveLottoResultTest() {
+
+        User user = lottoService.getUserForTest();
+
+        user.addLottoForTest(List.of(30, 24, 13, 4, 1, 6));
+        user.addLottoForTest(List.of(30, 5, 13, 2, 24, 6));
+
+        lottoService.saveWinningLotto("30,24,13,4,5,6");
+        lottoService.saveResultLotto();
+
+        Reward expectedReward = Reward.SECOND_REWARD;
+
+        assertThat(user.getLottiesResult().get(expectedReward)).isEqualTo(2);
     }
 
 }
