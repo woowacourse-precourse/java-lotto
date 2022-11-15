ad34eaa07406c550a1205571d26e2e823c456ff3
BoyCho <iijokv@naver.com>
2022-11-15 19:19:22 +0900

    test : 생성된 로또 번호 출력 기능 검증

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,8 +3,12 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,8 +28,22 @@ class LottoTest {
 
     @DisplayName("로또 번호가 오름차순으로 정렬되어 있지 않으면 예외가 발생한다.")
     @Test
-    void createLottoInAscendingOrder() {} {
-        assertThatThrownBy(() -> new Lotto(List.of(2,1,3,4,5,6)))
+    void createLottoInAscendingOrder() {
+        assertThatThrownBy(() -> new Lotto(List.of(2, 1, 3, 4, 5, 6)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("생성된 로또 번호 출력 메시지 검증")
+    @Test
+    void checkLottoNumberPrintMessage() {
+
+        String lottoNumberMessage = "[1, 2, 3, 4, 5, 6]";
+
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        new Lotto(List.of(1, 2, 3, 4, 5, 6)).getNumbers();
+
+        assertThat(out.toString().trim()).isEqualTo(lottoNumberMessage);
     }
 }
