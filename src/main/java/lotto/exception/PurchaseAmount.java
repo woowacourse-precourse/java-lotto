045e3a2db535c4b1e549a85d9c85a4c46a96246a
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-15 21:37:33 +0900

    Feat(Exception): Add PurchaseAmount ExceptionPurchaseAmount Method

--- src/main/java/lotto/exception/PurchaseAmount.java
+++ src/main/java/lotto/exception/PurchaseAmount.java
@@ -4,6 +4,12 @@ import lotto.constants.Error;
 import lotto.constants.Number;
 
 public class PurchaseAmount {
+    public static void exceptionPurchaseAmount(String purchaseAmount) {
+        exceptionNoNumber(purchaseAmount);
+        exceptinoLessThanPrice(purchaseAmount);
+        exceptionDividePrice(purchaseAmount);
+    }
+
     public static void exceptinoLessThanPrice(String purchaseAmount) {
         if (Integer.parseInt(purchaseAmount) < Number.PRICE.getNumber()) {
             throw new IllegalArgumentException(Error.LESS_THAN_LOTTO_PRICE.getMsg());
