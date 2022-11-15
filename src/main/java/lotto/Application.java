b0c760abb1ee6f77a238ec232be0545f541cf894
farmJun <junpreme1205@gmail.com>
2022-11-15 21:18:05 +0900

    feat(Application): 로또 구매 금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+
+import camp.nextstep.edu.missionutils.Console;
+import static lotto.GuideMessage.*;
+
 public class Application {
+    public static int getLottoPurchaseMoney() {
+        System.out.println(INPUT_REQUEST_MESSAGE.getMessage());
+        String userInput = Console.readLine();
+        int lottoPurchaseMoney = Integer.parseInt(userInput);
+        return lottoPurchaseMoney;
+
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int lottoPurchaseMoney = getLottoPurchaseMoney();
     }
 }
