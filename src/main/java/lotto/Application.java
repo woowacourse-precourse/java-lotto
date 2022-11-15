c1f84a328ab8ea72d9cf5260cf7ce95062c5aed5
Amenable-C <duswo0624@naver.com>
2022-11-15 12:05:48 +0900

    feat : 구입 금액에 문자가 있을 경우 예외처리 하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,12 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try{
+            LottoGame lottoGame = new LottoGame();
+            lottoGame.start();
+        }catch(IllegalArgumentException illegalArgumentException){
+            throw illegalArgumentException;
+        }
+
     }
 }
---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,46 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class LottoGame {
+    private static final int TICKET_PRICE = 1000;
+
+    public void start() {
+        int lottoAmount = getLottoAmount();
+    }
+
+    private int getLottoAmount(){
+        int payment = getPayment();
+
+        return (payment / TICKET_PRICE);
+    }
+
+    private int getPayment(){
+        System.out.println("구입 금액을 입력해 주세요.");
+        String payment = readLine();
+
+        if(!isValidPayment(payment)){
+            throw new IllegalArgumentException();
+        }
+
+        return Integer.parseInt(payment);
+    }
+
+    private boolean isValidPayment(String payment){
+        if(!isValidPaymentCharacter(payment)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isValidPaymentCharacter(String payment) {
+        try{
+            Integer.parseInt(payment);
+        } catch(IllegalArgumentException illegalArgumentException){
+            return false;
+        }
+
+        return true;
+    }
+}
