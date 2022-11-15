69de4585e1a6c7ef8b64c8277df98a1fc593a44e
YongGwan <32181771@dankook.ac.kr>
2022-11-15 22:56:25 +0900

    refactor : WinningHistory 클래스 변수명 및 초기화 방법 변경

--- src/main/java/winning/WinningHistory.java
+++ src/main/java/winning/WinningHistory.java
@@ -2,50 +2,38 @@ package winning;
 
 public class WinningHistory {
 
-    private int firstWinningCmt;
-    private int secondWinningCmt;
-    private int thirdWinningCmt;
-    private int forthWinningCmt;
-    private int fifthWinningCmt;
-
-    public WinningHistory() {
-        initializeWinningCmt();
-    }
+    private int firstWinningCount = 0;
+    private int secondWinningCount = 0;
+    private int thirdWinningCount = 0;
+    private int forthWinningCount = 0;
+    private int fifthWinningCount = 0;
 
     public void print() {
         System.out.println("\n당첨 통계\n---");
-        System.out.println("3개 일치 (5,000원) - " + fifthWinningCmt + "개");
-        System.out.println("4개 일치 (50,000원) - " + forthWinningCmt + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + thirdWinningCmt + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + secondWinningCmt + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + firstWinningCmt + "개");
+        System.out.println("3개 일치 (5,000원) - " + fifthWinningCount + "개");
+        System.out.println("4개 일치 (50,000원) - " + forthWinningCount + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + thirdWinningCount + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + secondWinningCount + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + firstWinningCount + "개");
     }
 
-    private void initializeWinningCmt() {
-        firstWinningCmt = 0;
-        secondWinningCmt = 0;
-        thirdWinningCmt = 0;
-        forthWinningCmt = 0;
-        fifthWinningCmt = 0;
+    public void addFirstWinningCount() {
+        firstWinningCount++;
     }
 
-    public void addFirstWinningCmt() {
-        firstWinningCmt++;
+    public void addSecondWinningCount() {
+        secondWinningCount++;
     }
 
-    public void addSecondWinningCmt() {
-        secondWinningCmt++;
+    public void addThirdWinningCount() {
+        thirdWinningCount++;
     }
 
-    public void addThirdWinningCmt() {
-        thirdWinningCmt++;
+    public void addForthWinningCount() {
+        forthWinningCount++;
     }
 
-    public void addForthWinningCmt() {
-        forthWinningCmt++;
-    }
-
-    public void addFifthWinningCmt() {
-        fifthWinningCmt++;
+    public void addFifthWinningCount() {
+        fifthWinningCount++;
     }
 }
