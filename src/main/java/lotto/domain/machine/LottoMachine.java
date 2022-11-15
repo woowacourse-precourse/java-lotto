4a8131440b946d46f5c036130789eacb56c2f00c
JuSeong1130 <jsmoon1130@naver.com>
2022-11-15 16:51:27 +0900

    feat(LottoMachine): 필드,생성자,getter 추가

--- src/main/java/lotto/domain/machine/LottoMachine.java
+++ src/main/java/lotto/domain/machine/LottoMachine.java
@@ -1,5 +1,22 @@
 package lotto.domain.machine;
 
+import lotto.domain.lotto.Lotto;
+
 public class LottoMachine {
 
+	private Lotto lotto;
+	private String bonusNumber;
+
+	public LottoMachine(String winLottoNumber, String bonusNumber) {
+		this.lotto = new Lotto(winLottoNumber,bonusNumber);
+		this.bonusNumber = bonusNumber;
+	}
+
+	public Lotto getLotto() {
+		return lotto;
+	}
+
+	public String getBonusNumber() {
+		return bonusNumber;
+	}
 }
