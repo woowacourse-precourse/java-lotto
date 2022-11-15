a99420f8bf35cff763e8fb8cfed477e3a1a8e256
kevstevie <jianna442@gmail.com>
2022-11-15 20:59:01 +0900

    refactor: Validation 클래스의 validate 적용

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,39 +1,14 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        Validation.validateNumbers(numbers);
         this.numbers = numbers;
     }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_SIX.printError());
-        }
-        validateLottoRange(numbers);
-        validateLottoDuplicate(numbers);
-    }
-
-    private void validateLottoRange(List<Integer> numbers) {
-        long count = numbers.stream().filter(s -> s > 45 || s < 1).count();
-        if (count != 0) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_RANGE.printError());
-        }
-    }
-
-    private void validateLottoDuplicate(List<Integer> numbers) {
-        long count = numbers.stream().distinct().count();
-        if (count != numbers.size()) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_DUPLICATE.printError());
-        }
-    }
-
     public void compareLotto(List<Integer> lotteries, int bonus) {
         long count = 0;
         boolean isBonus = numbers.contains(bonus);
@@ -43,21 +18,5 @@ public class Lotto {
             }
         }
         Calculator.calculateLottos(count, isBonus);
-    }
-
-    int validateBonus(String bonus) {
-        int bonusToNum;
-        try {
-            bonusToNum = Integer.parseInt(bonus.trim());
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_NUMBER.printError());
-        }
-        if (bonusToNum > 45 || bonusToNum < 1) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_RANGE.printError());
-        }
-        if (numbers.contains(bonusToNum)) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_DUPLICATE.printError());
-        }
-        return bonusToNum;
     }
 }
