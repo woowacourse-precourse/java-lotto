709517b5dd7a8be3a6f393308d4dbfd2e3c1486e
김선동 <mok03127@gmail.com>
2022-11-15 16:49:02 +0900

    feat: 로또 구입 금액 1000 미만인 경우 예외 처리

--- src/main/java/lotto/LottoAmount.java
+++ src/main/java/lotto/LottoAmount.java
@@ -2,11 +2,12 @@ package lotto;
 
 public class LottoAmount {
     private static final int LOTTO_MINIMUM_AMOUNT = 1000;
-
+    private static final String LESS_THAN_THOUSAND_ERROR = "[ERROR] 최소 금액은 천원이며, 천 단위 숫자만 입력 가능합니다.";
+    
     private final int purchaseAmount;
 
     public LottoAmount(int purchaseAmount) {
-
+        checkMinimumAmount(purchaseAmount);
         this.purchaseAmount = purchaseAmount;
     }
 
@@ -14,5 +15,10 @@ public class LottoAmount {
         return purchaseAmount / LOTTO_MINIMUM_AMOUNT;
     }
 
+    private void checkMinimumAmount(int purchaseAmount) {
+        if (purchaseAmount < LOTTO_MINIMUM_AMOUNT) {
+            throw new IllegalArgumentException(LESS_THAN_THOUSAND_ERROR);
+        }
+    }
 
 }
