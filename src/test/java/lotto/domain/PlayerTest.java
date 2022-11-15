6d9f30a3daed75abbf4b43af6b9c63a291357fb2
CAEL0 <kjongmin26@gmail.com>
2022-11-15 21:34:07 +0900

    test(PlayerTest): 구입금액이 로또 가격으로 니느아 떨어지지 않으면 예외 발생

--- src/test/java/lotto/domain/PlayerTest.java
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static lotto.constant.ExceptionConstants.LEADING_ZERO_EXCEPTION;
-import static lotto.constant.ExceptionConstants.NOT_NATURAL_NUMBER_EXCEPTION;
+import static lotto.constant.ExceptionConstants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayerTest extends NsTest {
@@ -27,6 +26,15 @@ public class PlayerTest extends NsTest {
         assertSimpleTest(() -> {
             runException("01000");
             assertThat(output()).contains(LEADING_ZERO_EXCEPTION.toString());
+        });
+    }
+
+    @DisplayName("구입금액이 로또 가격으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void ThrowExceptionIfPurchaseAmountIndivisible() {
+        assertSimpleTest(() -> {
+            runException("1500");
+            assertThat(output()).contains(PURCHASE_AMOUNT_INDIVISIBILITY_EXCEPTION.toString());
         });
     }
 
