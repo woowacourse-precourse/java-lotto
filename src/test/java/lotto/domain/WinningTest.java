2392fa5b20a03133f06520f6eff602b6e31b8113
hhiyeon <hhyyeon0214@gmail.com>
2022-11-15 15:50:51 +0900

    test: winning(test)-check winning number is not numeric

--- src/test/java/lotto/domain/WinningTest.java
+++ src/test/java/lotto/domain/WinningTest.java
@@ -1,4 +1,16 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class WinningTest {
+    @DisplayName("당첨 번호가 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void checkWinningNumberIsNotNumeric() {
+        assertThatThrownBy(() -> new Money("100d0"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
