f296dfe8ac0d7592eb55c3631e0212ff222eee4f
Park Sungchul <try615@naver.com>
2022-11-15 19:02:56 +0900

    feat(ConsoleOut.java): 구매 안내 문구 출력 기능 추가
    얼마 어치의 로또를 구매하겠냐는 문구 출력 기능

--- src/main/java/lotto/ConsoleOut.java
+++ src/main/java/lotto/ConsoleOut.java
@@ -2,4 +2,7 @@ package lotto;
 
 public class ConsoleOut {
 
+	private static void printInputMoney() {
+		System.out.println(Constants.PRINT_INPUT_MONEY);
+	}
 }
--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -1,5 +1,5 @@
 package lotto;
 
 public class Constants {
-
+	public static final String PRINT_INPUT_MONEY = "구입금액을 입력해 주세요.";
 }
