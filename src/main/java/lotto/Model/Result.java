5bc2efd3bd767b4acae8b861199d1ebd12e8a20c
이규승 <rpg8988@naver.com>
2022-11-15 22:36:18 +0900

    feat: 결과 계산 객체 생성 및 결과 생성

---
+++ src/main/java/lotto/Model/Result.java
@@ -0,0 +1,59 @@
+package lotto.Model;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Result {
+    private final Map<Rank, Long> lottoResult;
+
+    public Result(final User userLotto, final Winning winningLotto) {
+        this.lottoResult = createLottoResult(userLotto.compareLottoNumber(winningLotto));
+    }
+
+
+    public Map<Rank, Long> getLottoResult() {
+        return lottoResult;
+    }
+
+    private EnumMap<Rank, Long> initLottoResult() {
+        return Arrays.stream(Rank.values())
+                .filter(value -> value != Rank.Not_winning)
+                .collect(Collectors.toMap(value -> value, count -> 0L, (a, b) -> b,
+                        () -> new EnumMap<>(Rank.class)));
+    }
+
+    private Map<Rank, Long> createLottoResult(final List<Rank> lottoRanks) {
+        Map<Rank, Long> lottoResult = initLottoResult();
+        for (Rank lottoRank : lottoRanks) {
+            lottoResult.put(lottoRank, lottoResult.get(lottoRank) + 1);
+        }
+        return lottoResult;
+    }
+
+    public Double calculateYield(final Price pay) {
+        long reward = lottoResult.keySet().stream()
+                .mapToLong(lottoRank -> lottoRank.getReward() * lottoResult.get(lottoRank)).sum();
+        return pay.calearning(reward);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Result that = (Result) o;
+        return Objects.equals(lottoResult, that.lottoResult);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoResult);
+    }
+}
