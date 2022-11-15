7f6ce0e7f4b7b2009d1a80c970adecf2785c670c
HamSeungChan <head0618@naver.com>
2022-11-15 17:15:40 +0900

    style : 사용하지 않는 상수 제거

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,11 +3,6 @@ package lotto.view;
 import java.util.List;
 
 public class OutputView {
-    private static final int THREE_MATCHES = 3;
-    private static final int FOUR_MATCHES = 4;
-    private static final int FIVE_MATCHES = 5;
-    private static final int FIVE_AND_BONUS_MATCHES = 100;
-    private static final int SIX_MATCHES = 6;
     private static final int THREE_MATCH_INDEX = 0;
     private static final int FOUR_MATCH_INDEX = 1;
     private static final int FIVE_MATCH_INDEX = 2;
@@ -29,24 +24,24 @@ public class OutputView {
     }
 
     public static void printEnterWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println("\n당첨 번호를 입력해 주세요.");
     }
 
     public static void printEnterBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println("\n보너스 번호를 입력해 주세요.");
     }
 
     public static void printWinningTickets(List<Integer> ticketResult) {
-        System.out.println("당첨 통계\n---");
-        System.out.println(THREE_MATCHES + "개 일치 (5,0000원) - "
+        System.out.println("\n당첨 통계\n---");
+        System.out.println("3개 일치 (5,000원) - "
                 + ticketResult.get(THREE_MATCH_INDEX) + "개");
-        System.out.println(FOUR_MATCHES + "개 일치 (50,0000원) - "
+        System.out.println("4개 일치 (50,000원) - "
                 + ticketResult.get(FOUR_MATCH_INDEX) + "개");
-        System.out.println(FIVE_MATCHES + "개 일치 (1,500,0000원) - "
+        System.out.println("5개 일치 (1,500,000원) - "
                 + ticketResult.get(FIVE_MATCH_INDEX) + "개");
-        System.out.println(FIVE_MATCHES + "개, 보너스 볼 일치 (30,000,000원) - "
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "
                 + ticketResult.get(FIVE_AND_BONUS_MATCH_INDEX) + "개");
-        System.out.println(SIX_MATCHES + "개 일치 (2,000,000,000원) - "
+        System.out.println("6개 일치 (2,000,000,000원) - "
                 + ticketResult.get(SIX_MATCH_INDEX) + "개");
     }
 
