c055bc89de12095a5d1e905148aed7beecebe8c6
ysng_ysng <yeasung67@gmail.com>
2022-11-15 21:57:18 +0900

    feat(LotteryStoreUi): Implement the methods to print the result
    당첨결과를 출력하는 메서드를 모두 구현했습니다.

--- src/main/java/lotto/domain/lotterystore/LotteryStoreUi.java
+++ src/main/java/lotto/domain/lotterystore/LotteryStoreUi.java
@@ -2,87 +2,92 @@ package lotto.domain.lotterystore;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.controller.LotteryRequestController;
-import lotto.dto.lotteryseller.RandomNumbersSets;
-import lotto.dto.lotterystore.MoneyForPurchase;
-import lotto.dto.lotterystore.NumbersForWinningPrize;
+import lotto.domain.lotteryserver.LotteryServer;
+import lotto.dto.lotteryseller.LottoSetsDto;
+import lotto.dto.lotterystore.MoneyDto;
+import lotto.dto.lotterystore.WinningNumbersDto;
 
 import java.util.List;
 
 public class LotteryStoreUi {
-	private static final LotteryStoreUi storeUi = new LotteryStoreUi();
-	private static final ErrorChecker errorChecker = new ErrorChecker();
-	private final LotteryRequestController requestController;
 	private String winningPrizeNumbers;
+	private LottoSetsDto lottoSetsDto;
+	private String resultMessage;
 
-	private LotteryStoreUi() {
-		this.requestController = LotteryRequestController.getController();
-	}
-
-	public static LotteryStoreUi getStoreUi() {
-		return storeUi;
-	}
-
-	public void takeMoneyFromBuyer() {
+	public void takeMoneyFromBuyer(LotteryRequestController requestController) {
 		printPurchaseMessage();
 
 		String money = Console.readLine();
-		errorChecker.validateMoneyFromBuyer(money);
+		UIExceptions.validateMoneyFromBuyer(money);
 
-		requestToTransferMoney(money);
+		requestToTransferMoney(money, requestController);
+		System.out.println();
 	}
 
 	public void takeWinningLotteryNumbers() {
 		printMessageForWinningPrizeNumbers();
 
 		String winningPrizeNumbers = Console.readLine();
-		errorChecker.validateWinningNumbers(winningPrizeNumbers);
+		UIExceptions.validateWinningNumbers(winningPrizeNumbers);
 
 		this.winningPrizeNumbers = winningPrizeNumbers;
+		System.out.println();
 	}
 
-	public void takeBonusNumber() {
+	public void takeBonusNumber(LotteryRequestController requestController,
+		LotteryServer lotteryServer) {
 		printMessageForBonusNumber();
 
 		String bonusNumber = Console.readLine();
-		errorChecker.validateBonusNumbers(bonusNumber);
+		UIExceptions.validateBonusNumbers(bonusNumber, winningPrizeNumbers);
 
-		requestToTransferWinningPrizeNumbers(winningPrizeNumbers, bonusNumber);
+		requestToTransferWinningPrizeNumbers(winningPrizeNumbers, bonusNumber, requestController,
+			lotteryServer);
+		System.out.println();
 	}
 
-	public void printRandomNumberSets(RandomNumbersSets randomNumbersSets) {
-		List<List<Integer>> sets = randomNumbersSets.getRandomNumbersSets();
-		int size = randomNumbersSets.getRandomNumbersSetsSize();
-		printMessageForRandomNumberSets(size);
+	public void printPurchaseMessage() {
+		System.out.println(Messages.LOTTO_BUY_INFO);
+	}
+
+	public void receiveLottoSets(LottoSetsDto lottoSetsDto) {
+		this.lottoSetsDto = lottoSetsDto;
+	}
+
+	public void printMessageForLottoSets() {
+		List<List<Integer>> sets = lottoSetsDto.getLottoSets();
+		int size = lottoSetsDto.getLottoSetsSize();
+		System.out.println(size + Messages.BUYING_COMPLETE);
 		sets.forEach(System.out::println);
+		System.out.println();
 	}
 
-	private void printPurchaseMessage() {
-		System.out.println("구매금액을 입력해 주세요.");
+	public void receiveResultMessage(String resultMessage) {
+		this.resultMessage = resultMessage;
 	}
 
-	private void printMessageForWinningPrizeNumbers() {
-		System.out.println("당첨 번호를 입력해 주세요.");
+	public void printResultMessage() {
+		System.out.println(resultMessage);
 	}
 
-	private void printMessageForBonusNumber() {
-		System.out.println("보너스 번호를 입력해 주세요.");
-	}
-
-	private void printMessageForRandomNumberSets(int size) {
-		System.out.println(size + "개를 구매했습니다.");
-	}
-
-	private void requestToTransferMoney(String money) {
-		// FrontEnd에서 API를 호출해서 데이터 전송하는 것과 비슷하게(??) 구현
-		MoneyForPurchase moneyDto = new MoneyForPurchase(money);
-		requestController.receiveMoneyForPurchase(moneyDto);
+	private void requestToTransferMoney(String money, LotteryRequestController requestController) {
+		MoneyDto moneyDto = new MoneyDto(money);
+		requestController.receiveMoney(moneyDto);
 	}
 
 	private void requestToTransferWinningPrizeNumbers(String winningPrizeNumbers,
-		String bonusNumber) {
-		// FrontEnd에서 API를 호출해서 데이터 전송하는 것과 비슷하게(??) 구현
-		NumbersForWinningPrize winningNumbersDto =
-			new NumbersForWinningPrize(winningPrizeNumbers, bonusNumber);
-		requestController.receiveNumbersForWinningPrize(winningNumbersDto);
+		String bonusNumber, LotteryRequestController requestController,
+		LotteryServer lotteryServer) {
+		WinningNumbersDto winningNumbersDto =
+			new WinningNumbersDto(winningPrizeNumbers, bonusNumber);
+		requestController.receiveWinningNumbers(winningNumbersDto, lotteryServer);
+	}
+
+	private void printMessageForWinningPrizeNumbers() {
+		System.out.println(Messages.LOTTO_NUMBER_INPUT);
+	}
+
+	private void printMessageForBonusNumber() {
+		System.out.println(Messages.BONUS_NUMBER_INPUT);
 	}
 }
