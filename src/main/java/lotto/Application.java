903195097e427eede2ac3d833f3061a18cc16b70
yewonbahn <banyaewon98@gmail.com>
2022-11-15 20:31:57 +0900

    1000단위 나누기 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,8 +3,12 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
-        Lotto.game();
+        try{
+            Lotto.game();
+        }
+        catch (Exception e){
+            System.out.println("[ERROR]가 발생하였습니다.");
+        }
 
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -28,51 +28,59 @@ public class Lotto {
 
 
     public static void game(){
-        int cost=0;
+        String cost="";
 
         Output.printCost();
-        try{cost = Integer.parseInt(Console.readLine());}
-        catch (Exception e){
-            //컴파일 오류 발생
-            System.out.println("[ERROR] 금액이 올바르지 않습니다.");
-
+        cost = Console.readLine();
+        System.out.println(cost);
+        costcheck(String.valueOf(cost));
+        int cnt = Output.printOrder(Integer.parseInt(cost));
+        //로또 생성
+        List<List<Integer>> total = MakeLotto.totallotto(cnt);
+        //당청 번호 입력 받기
+        String lottonum;
+        int bonus;
+        Output.printNum();
+        lottonum = Console.readLine();
+        String[] lottolst = lottonum.split(",");
+        List<Integer> numbers = new ArrayList<>();
+        for(int i=0;i<lottolst.length;i++){
+            numbers.add(Integer.valueOf(lottolst[i]));
         }
-            int cnt = Output.printOrder(cost);
-            //로또 생성
-            List<List<Integer>> total = MakeLotto.totallotto(cnt);
-            //당청 번호 입력 받기
-            String lottonum;
-            int bonus;
-            Output.printNum();
-            lottonum = Console.readLine();
-            String[] lottolst = lottonum.split(",");
-            List<Integer> numbers = new ArrayList<>();
-            for(int i=0;i<lottolst.length;i++){
-                numbers.add(Integer.valueOf(lottolst[i]));
-            }
-            new Lotto(numbers);
+        new Lotto(numbers);
 
-            Output.printBonus();
-            //보너스 번호 입력받기
-            try {
-                bonus = Integer.parseInt(Console.readLine());
-            }catch (Exception e){
-                System.out.println("[ERROR] 숫자입력이 아닙니다.");
-                throw new IllegalArgumentException();
-            }
-            bonuscheck(bonus);
+        Output.printBonus();
+        //보너스 번호 입력받기
+        try {
+            bonus = Integer.parseInt(Console.readLine());
+        }catch (Exception e){
+            System.out.println("[ERROR] 숫자입력이 아닙니다.");
+            throw new IllegalArgumentException();
+        }
+        bonuscheck(bonus);
 
-            int[] result = CompareLotto.comparelotto(total,lottolst,bonus);
-            Output.printResult(result,cost);
+        int[] result = CompareLotto.comparelotto(total,lottolst,bonus);
+        Output.printResult(result, Integer.parseInt(cost));
 
         }
 
     public static void bonuscheck(int bonus) {
         if(bonus<=0 || bonus>=45){
-        System.out.println("[ERROR] 금액이 올바르지 않습니다.");
+        System.out.println("[ERROR] 0-45 숫자가 아닙니다.");
         throw new IllegalArgumentException();}
 
     }
+    public static void costcheck(String bonus) {
+        //INT형인지 확인
+        try {
+            Double.parseDouble(bonus);
+        }catch(Exception e) {
+            System.out.println("[ERROR] 금액이 숫자가 아닙니다.");
+            throw new IllegalArgumentException();
+        }
+        //1000원 단위로 나뉘는지 확인
+
+        }
 
 
     // TODO: 추가 기능 구현
