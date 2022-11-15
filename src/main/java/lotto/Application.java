68cb5279bd8366a1ffac3452cf0e5e709d8ca080
DavinAn <blueapple1030@gmail.com>
2022-11-15 23:48:58 +0900

    feat : add javadocs and control flow elements to Application class

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,16 +1,38 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+
+/**
+ * Driver class for Java-Lotto game.
+ *
+ * @author Davin An
+ * @version 2.0
+ */
+@SuppressWarnings("checkstyle:SummaryJavadoc")
 public class Application {
     private static int purchaseAmount = 0;
     private static List<Lotto> lotto = new ArrayList<>();
     private static List<Integer> winningNum = new ArrayList<>();
     private static Map<Winnings, Integer> winningTickets = new LinkedHashMap<>();
 
+    Input input;
+    Output output;
+    TicketHandler ticketHandler;
+
+
+    public Application() {
+        input = new Input();
+        output = new Output();
+        ticketHandler = new TicketHandler();
+    }
+
+
+    /**
+     * Game starts here. Catches all IllegalArgumentExceptions.
+     *
+     * @param args Console arguments
+     */
     public static void main(String[] args) {
         try {
             Application game = new Application();
@@ -18,11 +40,23 @@ public class Application {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getLocalizedMessage());
         }
+
     }
 
+
+    /**
+     * Control flow of overall game.
+     */
     public void playLotto() {
-
+        purchaseAmount = input.purchaseAmount();
+        lotto = ticketHandler.generateTickets();
+        output.printTickets();
+        winningNum = input.winningNumbers();
+        int bonusNum = input.bonusNumber();
+        ticketHandler.compareTickets(winningNum, bonusNum);
+        output.printWinnings();
     }
+
 
     // Getter methods
     public static int getPurchaseAmount() {
@@ -50,4 +84,5 @@ public class Application {
     public static void setWinningTickets(Map<Winnings, Integer> winningTickets) {
         Application.winningTickets = winningTickets;
     }
+
 }
