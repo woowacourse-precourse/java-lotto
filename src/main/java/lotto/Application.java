065691a96140283cf86c5bc9865402654ed325b4
yechanissm <106381504+yechanissm@users.noreply.github.com>
2022-11-15 11:38:58 +0900

    feat : 로또 생계 클래스 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,19 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 import static java.lang.Integer.parseInt;
 
 public class Application {
-    final static String PriceMessage = "구입금액을 입력해 주세요."
     public static void main(String[] args) {
-       int price = userInputPrice(PriceMessage);
-    }
 
-    private static int userInputPrice(String priceMessage) {
-        System.out.println(priceMessage);
-        try{
-            int price = parseInt(Console.readLine());
-            return price;
-        }
-        catch(Exception e){
-            throw new IllegalArgumentException(("[ERROR] 구입금액은 "))
-        }
     }
 }
---
+++ src/main/java/lotto/LottoMachine.java
@@ -0,0 +1,51 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
+public class LottoMachine {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+
+    private static final String RANGE_ERROR_MESSAGE = "[ERROR] 로또 범위에 맞지 않는 숫자값을 가지고 있습니다.";
+    private static final String SIZE_ERROR_MESSAGE = "[ERROR] 생성된 로또의 번호가 6개가 아닙니다.";
+    private static final String OVERLAP_ERROR_MESSAGE = "[ERROR] 로또에 중복된 번호가 있습니다.";
+
+    public List<Integer> createLotto(){
+        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(MIN_NUMBER,MAX_NUMBER,LOTTO_SIZE);
+        Collections.sort(lotto);
+
+        checkCorrectLotto(lotto);
+    }
+
+    private void checkCorrectLotto(List<Integer> lotto) {
+        rangeCheck(lotto);
+        sizeCheck(lotto);
+        overlapCheck(lotto);
+    }
+
+    private void rangeCheck(List<Integer> lotto){
+        for(int number : lotto){
+            if(number<1 || number>45){
+                throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
+            }
+        }
+    }
+    private void sizeCheck(List<Integer> lotto){
+        if(lotto.size() == LOTTO_SIZE){
+            return;
+        }
+        throw new IllegalArgumentException(SIZE_ERROR_MESSAGE)
+    }
+
+    private void overlapCheck(List<Integer> lotto){
+        Set<Integer> overlapLotto = new HashSet<>(lotto);
+        if(overlapLotto.size()==lotto.size()){
+            return;
+        }
+        throw new IllegalArgumentException(OVERLAP_ERROR_MESSAGE)
+    }
+
+}
