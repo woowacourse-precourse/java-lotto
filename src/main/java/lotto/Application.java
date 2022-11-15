15dd1b3eb1b0c379528aacf75ab65b893a1689d3
YusunPark <solarpy.kor@gmail.com>
2022-11-15 21:51:10 +0900

    feat(Application.java): add check function
    check for digit price

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,7 +2,6 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,12 +10,16 @@ public class Application {
     static void priceMessage() {
         System.out.println("구입금액을 입력해 주세요.");
     }
-
-    static boolean checkPrice(int n) {
-        if (n % 1000 == 0) {
-            return true;
-        } else {
-            return false;
+    static void checkPriceDigit(String numbers) {
+        for (int i = 0; i < numbers.length(); i++) {
+            if (!Character.isDigit(numbers.charAt(i))) {
+                throw new IllegalArgumentException("[ERROR] 구입금액은 숫자여야 합니다.");
+            }
+        }
+    }
+    static void checkPrice(int n) {
+        if (n % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입금액이 형식이 맞지 않습니다.");
         }
     }
 
@@ -24,9 +27,8 @@ public class Application {
         priceMessage();
         String price = Console.readLine();
         System.out.println();
-        if (!checkPrice(Integer.parseInt(price))) {
-            throw new IllegalArgumentException("[ERROR] 구입금액 형식이 맞지 않습니다.");
-        }
+        checkPriceDigit(price);
+        checkPrice(Integer.parseInt(price));
     }
 
     static void numberMessage() {
