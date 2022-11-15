fb588993f824e9b17f7281c0f02dbd28b57c01ce
hyunseo <goatsfish@gmail.com>
2022-11-15 12:16:16 +0900

    style : 로또 자동 생성 기능 정적 임포트 변경

--- src/main/java/lotto/util/LottoAutoGenerator.java
+++ src/main/java/lotto/util/LottoAutoGenerator.java
@@ -2,13 +2,14 @@ package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
-import lotto.domain.LottoSetting;
 
 import java.util.ArrayList;
+
+import static lotto.domain.LottoSetting.*;
 
 public class LottoAutoGenerator {
     public static Lotto generate() {
         return new Lotto(new ArrayList<>(Randoms.pickUniqueNumbersInRange(
-                LottoSetting.LOTTO_MIN_NUMBER.getSetting(), LottoSetting.LOTTO_MAX_NUMBER.getSetting(), LottoSetting.LOTTO_NUMBER_COUNT.getSetting())));
+                LOTTO_MIN_NUMBER.getSetting(), LOTTO_MAX_NUMBER.getSetting(), LOTTO_NUMBER_COUNT.getSetting())));
     }
 }
