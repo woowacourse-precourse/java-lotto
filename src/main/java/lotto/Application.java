b6717cad907dab5345eee037cbcfc41b1d172aa1
CJ <choijun0627@naver.com>
2022-11-15 20:42:20 +0900

    <style>(Application.java): 기능들 함수로 나눔
    main함수에 몰려 있는 기능들을 새로 함수 만들어서 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,10 +7,20 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Input input=new Input();
+        MakingLotto makingLotto=new MakingLotto();
 
         System.out.println("구입금액을 입력해 주세요.");
         int money= input.setMoney();
+        List<Lotto> allLotto=getLotto(money);
 
+        String lottoString=getLottString(input);
+        int bonus=getBonus(input);
+
+        Lotto winningLotto= makingLotto.getWinningLotto(lottoString);
+        showLotto(allLotto,bonus,winningLotto,money);
+    }
+
+    public static List<Lotto> getLotto(int money){
         MakingLotto makingLotto=new MakingLotto();
         int count=makingLotto.getLottoCount(money);
         List<Lotto> allLotto=makingLotto.getAllLotto(count);
@@ -18,15 +28,25 @@ public class Application {
         Output output=new Output();
         output.printLotto(allLotto);
 
+        return allLotto;
+    }
+
+    public static String getLottString(Input input){
         System.out.println("당첨 번호를 입력해 주세요.");
         String lottoString= input.setWinningLotto();
         System.out.println(lottoString);
+        return lottoString;
+    }
+
+    public static int getBonus(Input input){
         System.out.println("보너스 번호를 입력해 주세요.");
         int bonus= input.setBonusNumber();
         System.out.println(bonus);
+        return bonus;
+    }
 
-        Lotto winningLotto= makingLotto.getWinningLotto(lottoString);
-
+    public static void showLotto(List<Lotto> allLotto, int bonus, Lotto winningLotto, int money){
+        Output output=new Output();
         CalculatingLotto calculatingLotto=new CalculatingLotto();
 
         List<Integer>allRank= calculatingLotto.calculateAllLotto(winningLotto,bonus,allLotto);
