f71133b3fc569c346e1b26efbbdfc41f046bca15
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-15 21:22:13 +0900

    refactor: Rank가 bonus 판별 값을 가지도록 변경

--- src/main/java/lotto/domain/info/Rank.java
+++ src/main/java/lotto/domain/info/Rank.java
@@ -3,23 +3,33 @@ package lotto.domain.info;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000),
-    EXTERIOR(0, 0);
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    EXTERIOR(0,false, 0);
 
     private final int count;
+    private final boolean bonus;
     private final int price;
 
-    Rank(int count, int price) {
+    Rank(int count, boolean bonus, int price) {
         this.count = count;
+        this.bonus = bonus;
         this.price = price;
     }
 
     public int getPrice() {
         return price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public boolean isBonus() {
+        return bonus;
     }
 
     public static Rank getRank(int count, boolean bonus) {
