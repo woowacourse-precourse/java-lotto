9bc4b0c39f85ac1a49e85958cae56244a360db3e
youswim <youswim96@gmail.com>
2022-11-15 16:30:50 +0900

    refactor(lotto): enterPurchaseMoney 수정
    Purchase클래스의 enterPurchaseMoney()가 구입하는 로또의 갯수를 반환하도록 수정

--- src/main/java/lotto/Purchase.java
+++ src/main/java/lotto/Purchase.java
@@ -6,9 +6,12 @@ public class Purchase {
     static final int MIN_MONEY = 1_000;
     private int purchaseMoney;
 
-    public void enterPurchaseMoney() {
+    public int enterPurchaseMoney() {
         System.out.println("구입 금액을 입력해주세요.");
         this.purchaseMoney = validatePurchaseMoney(Console.readLine());
+        int lottoCount = purchaseMoney / MIN_MONEY;
+        System.out.println("\n"+lottoCount+"개를 구매했습니다");
+        return lottoCount;
     }
 
     public static Integer validatePurchaseMoney(String purchaseMoney) {
