39778e978446b1ddf708b7f517e070a7ba41557b
nomushroomplz <aaron97@knou.ac.kr>
2022-11-15 13:53:56 +0900

    feat(outputModule): 안내 메시지 출력 기능 구현

---
+++ src/main/java/lotto/outputModule.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class outputModule {
+    public static void getMessage() {
+        String messageInputPurchaseAmount = ("구매할 금액을 입력해 주세요.");
+        String messageInputLuckyNumbers = ("당첨 번호 6자리를 입력해 주세요.");
+        String messageInputBonusNumber = ("보너스 번호를 입력해 주세요.");
+    }
+}
