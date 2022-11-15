c1c307f78e1557bd3b5245c44f0e5738d744af40
LeeHaiLim <hailim1997@gmail.com>
2022-11-15 10:34:46 +0900

    feat(view): implement method print lotto numbers

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,20 @@
 package lotto.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.model.Lotto;
+
 public class OutputView {
     private static OutputView instance = new OutputView();
 
     private OutputView() {
+    }
+
+    public void printLottoNumbers(List<Lotto> lottos) {
+        System.out.printf((OutputMessage.PURCHASE.message()), lottos.size());
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers().stream().sorted().collect(Collectors.toList()));
+        }
     }
 
     public static OutputView getInstance() {
