37fa59bb77936bbeecb942461e4d53b401c39310
h0ngwon <w___on@naver.com>
2022-11-15 13:10:17 +0900

    refactor : refactor controller

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,14 @@
 package lotto;
 
 public class Application {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     public static void main(String[] args) {
         Controller controller = new Controller();
-        controller.start();
+        try {
+            controller.start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_MESSAGE);
+        }
     }
 }
--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -12,57 +12,85 @@ public class Controller {
     private static final String NOT_INSEPARABLE_ERROR = " 구입 금액이 1000원 단위가 아닙니다.";
 
     public void start() {
-        String amount, winningNumber, bonusNumber;
+        String amount;
         int ticketAmount;
-        List<Integer> winningNumbers;
-        Lotto lotto;
         LottoNumbers lottoNumbers = new LottoNumbers();
         List<Lotto> lottoTickets = new ArrayList<>();
 
-        amount = InputView.purchaseInputMessage();
+        amount = getAmount();
+        ticketAmount = getTicketAmount(amount);
 
-        isValidateNumber(amount);
-        isValidatePurchase(amount);
-
-        ticketAmount = stringToIntegerConvert(amount);
-        OutputView.lottoTicketAmountMessage(ticketAmount);
-
-        for (int i = 0; i < ticketAmount; i++) {
-            lotto = new Lotto(lottoNumbers.start());
-            lottoTickets.add(lotto);
-        }
-
-        for(int i = 0; i < ticketAmount; i++) {
-            Collections.sort(lottoTickets.get(i).getNumbers());
-        }
-
+        saveLotto(ticketAmount, lottoNumbers, lottoTickets);
+        sortLottoNumbers(ticketAmount, lottoTickets);
         OutputView.printLottoTickets(lottoTickets);
+        makeRankOfLotto(amount, lottoTickets);
+    }
+
+    private void makeRankOfLotto(String amount, List<Lotto> lottoTickets) {
+        String bonusNumber;
+        List<Integer> winningNumbers;
+        String winningNumber;
 
         winningNumber = InputView.winningNumberInputMessage();
         winningNumbers = Winning.makeWinningNumberList(winningNumber);
         bonusNumber = InputView.bonusNumberInputMessage();
+
         Winning.prize(lottoTickets, winningNumbers, bonusNumber, amount);
     }
 
-    private void isValidatePurchase(String userInput) {
-        int amount = Integer.parseInt(userInput);
+    private void sortLottoNumbers(int ticketAmount, List<Lotto> lottoTickets) {
+        for (int i = 0; i < ticketAmount; i++) {
+            Collections.sort(lottoTickets.get(i).getNumbers());
+        }
+    }
 
-        if (amount % 1000 != 0) {
+    private void saveLotto(int ticketAmount, LottoNumbers lottoNumbers, List<Lotto> lottoTickets) {
+        Lotto lotto;
+        for (int i = 0; i < ticketAmount; i++) {
+            lotto = new Lotto(lottoNumbers.start());
+            lottoTickets.add(lotto);
+        }
+    }
+
+    private int getTicketAmount(String amount) {
+        int ticketAmount;
+
+        ticketAmount = stringToIntegerConvert(amount);
+
+        isValidatePurchase(ticketAmount);
+        OutputView.lottoTicketAmountMessage(ticketAmount);
+
+        return ticketAmount;
+    }
+
+    private String getAmount() {
+        String amount;
+
+        amount = InputView.purchaseInputMessage();
+        isValidateNumber(amount);
+
+        return amount;
+    }
+
+    public void isValidatePurchase(int userInput) {
+        if (userInput % 1000 == 0) {
             throw new IllegalArgumentException(ERROR_MESSAGE + NOT_INSEPARABLE_ERROR);
         }
     }
 
-    private void isValidateNumber(String userInput) {
+    public void isValidateNumber(String userInput) {
         try {
             Integer.parseInt(userInput);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + NOT_NUMBER_ERROR);
+        } catch (NumberFormatException e) {
+            System.out.println(ERROR_MESSAGE + NOT_NUMBER_ERROR);
+            throw new IllegalArgumentException();
         }
     }
 
-    private int stringToIntegerConvert(String userInput) {
+    public int stringToIntegerConvert(String userInput) {
         int ticket = Integer.parseInt(userInput);
+        ticket /= 1000;
 
-        return ticket / 1000;
+        return ticket;
     }
 }
