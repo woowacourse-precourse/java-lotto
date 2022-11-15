b04f7658fca5d71e19fccc86a0f98faeffe45d2c
starwook <star.wookk@gmail.com>
2022-11-15 19:41:58 +0900

    refactor(Application.java): Refactor main method
    main에서 Game객체 생성후 게임시작

--- src/main/java/lotto/domain/Game.java
+++ src/main/java/lotto/domain/Game.java
@@ -8,25 +8,27 @@ import java.util.List;
 
 public class Game {
 
-    private static final String notThousand =" 구입금액은 천원 단위여야합니다.";
-    private static final String notNumber =" 구입금액은 숫자여야합니다.";
+    private static final String notThousand = " 구입금액은 천원 단위여야합니다.";
+    private static final String notNumber = " 구입금액은 숫자여야합니다.";
     private static final int unit = 1000;
 
-    public void gameStart(){
+    public void gameStart() {
         System.out.println(Resource.greetings);
-        showBoughtNumbers();
-
+        List<Lotto> lottoNumbers = showBoughtNumbers();
         WinningNumber winningNumber = makeWinningNumber();
+        showResult(lottoNumbers, winningNumber);
+        System.out.println(Resource.revenuePercentFront);
+        System.out.println(Resource.revenuePercentBack);
+    }
 
+    private void showResult(List<Lotto> lottoNumbers, WinningNumber winningNumber) {
+        JudgeMent judgeMent = new JudgeMent();
+        judgeMent.setCorrectResult(lottoNumbers, winningNumber);
+        System.out.println(judgeMent.getCorrectResult());
         System.out.println(Resource.resultOfWinning);
         System.out.println(Resource.countOfCorrectNumberFront);
         System.out.println(Resource.countOfCorrectNumberMiddle);
         System.out.println(Resource.countOfCorrectNumberBack);
-
-        System.out.println(Resource.revenuePercentFront);
-        System.out.println(Resource.revenuePercentBack);
-
-
     }
 
     private WinningNumber makeWinningNumber() {
@@ -34,37 +36,37 @@ public class Game {
         String winNumber = Console.readLine();
         System.out.println(Resource.inputOfBonusNumber);
         String bonusNumber = Console.readLine();
-        return new WinningNumber(winNumber,Integer.parseInt(bonusNumber));
+        return new WinningNumber(winNumber, Integer.parseInt(bonusNumber));
     }
 
-    private void showBoughtNumbers() {
+    private List<Lotto> showBoughtNumbers() {
         String purchase = Console.readLine();
         int lottoUnit = checkIfDivideWithThousand(purchase);
-        System.out.println(lottoUnit+Resource.countOfBuying);
-        List<Lotto> lottos = new ArrayList<>() ;
-        for(int i=0;i<lottoUnit;i++){
-            Lotto lotto = getLotto(lottos);
-            System.out.println(lotto);
+        System.out.println(lottoUnit + Resource.countOfBuying);
+        List<Lotto> lottoNumbers = new ArrayList<>();
+        for (int i = 0; i < lottoUnit; i++) {
+            Lotto lotto = getLotto(lottoNumbers);
+            System.out.println(lotto.getNumbers());
         }
+        return lottoNumbers;
     }
+
     private Lotto getLotto(List<Lotto> lottos) {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         Lotto lotto = new Lotto(numbers);
         lottos.add(lotto);
         return lotto;
     }
 
-
-    public int checkIfDivideWithThousand(String purchase){
-        try{
+    public int checkIfDivideWithThousand(String purchase) {
+        try {
             int purchaseBill = Integer.parseInt(purchase);
-            if(purchaseBill%unit !=0){
-                throw new IllegalArgumentException(ErrorResource.errorStart+notThousand);
+            if (purchaseBill % unit != 0) {
+                throw new IllegalArgumentException(ErrorResource.errorStart + notThousand);
             }
-            return purchaseBill/unit;
-        }
-        catch(NumberFormatException e){
-            throw new IllegalArgumentException(ErrorResource.errorStart+notNumber);
+            return purchaseBill / unit;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorResource.errorStart + notNumber);
         }
     }
 
