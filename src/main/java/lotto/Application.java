b857527d1a2240e56cd0b2f9e027ceb6fdc9dea0
heejeong <hjx66@naver.com>
2022-11-15 17:03:19 +0900

    feat: LottoSystem 클래스 생성, 메인 실행 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.domain.LottoSystem;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoSystem lottoSystem = new LottoSystem();
+        lottoSystem.run();
     }
 }
