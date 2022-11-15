e38addb1a7786a20f6140b5cc27d5a711fe90f6a
Amenable-C <duswo0624@naver.com>
2022-11-15 12:17:55 +0900

    feat : 지불한 금액에 해당하는만큼 로또를 발행하는 기능 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -2,11 +2,23 @@ package lotto;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import lotto.domain.Lotto;
+
 public class LottoGame {
     private static final int TICKET_PRICE = 1000;
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_MIN_NUMBER = 1;
 
     public void start() {
         int lottoAmount = getLottoAmount();
+        
+        List<Lotto> lottos;
+        lottos = createLottos(lottoAmount);
     }
 
     private int getLottoAmount(){
@@ -50,5 +62,23 @@ public class LottoGame {
 
     private boolean isValidPaymentUnit(String payment) {
         return Integer.parseInt(payment) % TICKET_PRICE == 0;
+    }
+
+    private List<Lotto> createLottos(int lottoAmount) {
+        System.out.println(lottoAmount +"개를 구매했습니다.");
+
+        List<Lotto> lottos = new ArrayList<>();
+
+        for(int idx = 0; idx < lottoAmount; idx++){
+            lottos.add(createLotto());
+        }
+        return lottos;
+    }
+
+    private Lotto createLotto() {
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBERS_SIZE);
+        Collections.sort(lottoNumbers);
+
+        return new Lotto(lottoNumbers);
     }
 }
