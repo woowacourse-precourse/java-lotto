5ea477336c29fe638dece890e74a4c11aecc796e
yummygyudon <bang2brew@gmail.com>
2022-11-15 11:43:17 +0900

    FEAT(ui): 출력 기능 클래스 구현
    - 클래스 명:Output
    - 활용 외부 클래스
      1. ProcessMessages
    - 메서드
      1. `printMessage`
        - System.out.print() 코드를 간소화 하기위한 메서드입니다.
      2. `printPlayerLotto`
        - 요구사항에 의거하여 출력형식에 맞춰 로또를 출력하는 메서드
        - Lotto 객체의 오버라이딩한 toString을 활용했습니다.
      3. `printStatistic`
        - `printRankQuantity`메서드를 통해 printf 포맷팅 출력을 통해 당첨된 등수의 갯수를 출력합니다.
        - ProcessMessages Enum클래스를 활용하여
        각 등수에 대한 통계치를 출력합니다.
      4. `printRankQuantity`
        - printf 포맷팅 출력을 통해 당첨된 등수의 갯수를 출력합니다.
      5. `printRate`
        - 수익률에 대해 출력합니다.
        - 둘째 자리에서 반올림하는 "%.1f" 포맷팅을 통해 출력합니다.

---
+++ src/main/java/lotto/ui/Output.java
@@ -0,0 +1,46 @@
+package lotto.ui;
+
+import java.util.List;
+import java.util.Map;
+import lotto.Lotto;
+import lotto.util.ProcessMessages;
+import lotto.util.Result;
+
+public class Output {
+
+    public void printMessage(String message) {
+        System.out.print(message);
+    }
+
+    public void printPlayerLotto(int lottoQuantity, List<Lotto> playerLotto) {
+        System.out.printf("%d개를 구매했습니다.\n", lottoQuantity);
+        for (Lotto lotto : playerLotto) {
+            System.out.println(lotto.toString());
+        }
+        printMessage(ProcessMessages.EMPTY_LINE.getMessage());
+    }
+
+    public void printStatistic(Map<Integer, Integer> resultBoard) {
+        printMessage(ProcessMessages.NOTICE_WORD.getMessage());
+        printMessage(ProcessMessages.CONTOUR.getMessage());
+        printMessage(Result.RANK_FIVE.getMessage());
+        printRankQuantity(resultBoard.get(5));
+        printMessage(Result.RANK_FOUR.getMessage());
+        printRankQuantity(resultBoard.get(4));
+        printMessage(Result.RANK_THREE.getMessage());
+        printRankQuantity(resultBoard.get(3));
+        printMessage(Result.RANK_TWO.getMessage());
+        printRankQuantity(resultBoard.get(2));
+        printMessage(Result.RANK_ONE.getMessage());
+        printRankQuantity(resultBoard.get(1));
+    }
+
+    private void printRankQuantity(int rankQuantity) {
+        System.out.printf("%d개\n", rankQuantity);
+    }
+
+
+    public void printRate(double rate) {
+        System.out.printf("총 수익률은 %.1f%%입니다.", rate);
+    }
+}
