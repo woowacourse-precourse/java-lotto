f83c49205d85106cc3a77eb2db3e00df72d40ca0
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-15 22:16:43 +0900

    Feat(Controller): Add LottoGameController Constants

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,5 +1,15 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.model.Lotto;
 public class LottoGameController {
+    static int purchaseAmount;
+    static int purchaseNumber;
+    static List<Lotto> lottos;
+    static List<Integer> winNumber;
+    static int bonusNumber;
+    static int[] winningStats;
+    static double rateOfReturn;
+
 
 }
