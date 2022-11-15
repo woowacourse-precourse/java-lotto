c569e0e6484db04bce4bbc56369a780f84f0efff
TeddyYeoni <po99027@naver.com>
2022-11-15 20:21:44 +0900

    feat(lotto) : 당첨 번호와 보너스 번호를 랜덤으로 뽑는 기능 구현

--- src/main/java/lotto/domain/GenerateNumbers.java
+++ src/main/java/lotto/domain/GenerateNumbers.java
@@ -1,16 +1,23 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class GenerateNumbers {
+	public static int bonusNumber;
+	public List<Integer> lottoNumbers;
+
+	public List<Integer> lotto() {
+		lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+		bonusNumber = Randoms.pickNumberInRange(1, 45);
+		while (lottoNumbers.contains(bonusNumber)) {
+			bonusNumber = Randoms.pickNumberInRange(1, 45);
+		}
+		lottoNumbers.add(bonusNumber);
+		Collections.sort(lottoNumbers);
+		return lottoNumbers;
+	}
 	
-	public static List<Integer> lotto() {
-
-		return null;
-	}
-
-	public static int bonus() {
-		return 0;
-	}
-
 }
