ab29b2f111048120331ad1d246cc27b2569b59cb
HangSub999 <gkdtjq999@gmail.com>
2022-11-15 18:28:28 +0900

    feat: 로또 enum 생성

---
+++ src/main/java/lotto/LottoRanking.java
@@ -0,0 +1,13 @@
+package lotto;
+
+public enum LottoRanking {
+    FIRST("6개 일치", 2000000000),
+    SECOND("5개 일치, 보너스 볼 일치", 300000000),
+    THIRD("5개 일치", 1500000),
+    FOURTH("4개 일치", 50000),
+    FIFTH("3개 일치", 5000),
+    NONE("낙첨", 0);
+
+    LottoRanking(String matches, int rankingMoney) {
+    }
+}
