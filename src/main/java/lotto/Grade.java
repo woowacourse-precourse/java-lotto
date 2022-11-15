cb0dd0a6cbaae44ab7f9077294097d140b67656f
ijung-won <dlwnddnjs96@gmail.com>
2022-11-15 13:22:55 +0900

    feat(Grade): enum 타입으로 맞춘 정답과 보너스 개수와 상금이 표기된 클래스 생성

--- src/main/java/lotto/Grade.java
+++ src/main/java/lotto/Grade.java
@@ -1,2 +1,28 @@
-package lotto;public enum Grade {
+package lotto;
+
+public enum Grade {
+    FIRST(2000000000, 6, 0),
+    SECOND(30000000, 5, 1),
+    THIRD(1500000, 5,0),
+    FOURTH(50000, 4, 0),
+    FIFTH(5000, 3,0);
+
+    private final int reward;
+    private final int correct;
+    private final int bonus;
+
+    Grade(int reward, int correct, int bonus){
+        this.reward = reward;
+        this.correct = correct;
+        this.bonus = bonus;
+    }
+
+    public static Grade getGrade(int correct, int bonus){
+        for (Grade grade: Grade.values()){
+            if (correct == grade.correct && bonus == grade.bonus) return grade;
+        }
+        return null;
+    }
+
+
 }
