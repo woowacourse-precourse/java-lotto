aa83eae2ce0c00e0d1460032c8b14d38f9269f66
yummygyudon <bang2brew@gmail.com>
2022-11-15 21:48:41 +0900

    TEST(domain): 유효성 검사 테스트 개선 [반복 테스트]
    - @ParameterizedTest 어노테이션
      : ValueSource로 등록된 테스트 케이스들을 해당 테스트 메서드의 매개변수로 넣어주는 방식으로 변환
    - @ValueSource 어노테이션
      : 반복 테스트에 쓰일 테스트 케이스들
    - @MethodSource 어노테이션
      : Arguments로 구성되어 있는 Stream을 반환하여 연관 값들끼리 테스트할 수 있도록 구현
    - @TestFactory 어노테이션
      : 단순 @Test를 사용했을 땐 반복 테스트에 대한 결과는 출력되었지만
       별도 예외 발생과 함꼐 타입을 좀 더 명시하라는 메시지가 발생하여 해당 어노테이션으로 교체하여 해결

--- src/test/java/lotto/domain/validator/LottoNumberValidatorTest.java
+++ src/test/java/lotto/domain/validator/LottoNumberValidatorTest.java
@@ -1,18 +1,21 @@
 package lotto.domain.validator;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Stream;
 import lotto.util.ErrorMessages;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("로또 번호 입력 유효성 검사 테스트")
 @TestMethodOrder(OrderAnnotation.class)
@@ -27,17 +30,19 @@ class LottoNumberValidatorTest {
 
     @Order(1)
     @DisplayName("1차:잘못된 구분자")
-    @Test
-    void separatorExceptionTest() {
-        String inputValue = "1, 2, 3, 4. 5. 6";
+    @ParameterizedTest(name = "{displayName} ({index}) : {0}")
+    @ValueSource(strings = {
+            "1, 2,  3&4,5.6",
+            "1, 2, 3, 4. 5. 6",
+            "1 /2 /3/ 4/ 5/ 6"
+    })
+    @TestFactory
+    void separatorExceptionTest(String inputValue) {
+        LottoNumberValidator validator = new LottoNumberValidator();
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> validator.checkingSeparator(inputValue))
                         .isInstanceOf(IllegalArgumentException.class)
         );
-        Exception exception = assertThrows(IllegalArgumentException.class,
-                () -> validator.checkingSeparator(inputValue));
-        assertThat(exception.getMessage()).isEqualTo(ErrorMessages.WRONG_SEPARATOR.getMessage());
-        // 피드백 문서 참고 - 새로운 테스트 방법(예외 메시지)
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> validator.checkingSeparator(inputValue))
                 .withMessage(ErrorMessages.WRONG_SEPARATOR.getMessage());
@@ -45,150 +50,135 @@ class LottoNumberValidatorTest {
 
     @Order(2)
     @DisplayName("2차:숫자가 아닌 값 입력")
-    @Test
-    void notNumberExceptionTest() {
-        String inputValue = "1, 2, 3, a, b, 5";
+    @ParameterizedTest(name = "{displayName} ({index}) : {0}")
+    @ValueSource(strings = {
+            "1, 2, 3, a, b, 5",
+            "a, b, c, d, e, f",
+            "1ab ,2 ,3, 4, 5, 6"
+    })
+    @TestFactory
+    void notNumberExceptionTest(String inputValue) {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> validator.checkingNotNumber(inputValue))
                         .isInstanceOf(IllegalArgumentException.class)
         );
-        Exception exception = assertThrows(IllegalArgumentException.class,
-                () -> validator.checkingNotNumber(inputValue));
-        assertThat(exception.getMessage()).isEqualTo(ErrorMessages.NOT_NUMBER_EXIST.getMessage());
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> validator.checkingNotNumber(inputValue))
+                .withMessage(ErrorMessages.NOT_NUMBER_EXIST.getMessage());
     }
 
     @Order(3)
     @DisplayName("3차:범위에서 벗어난 값 입력")
-    @Test
-    void outOfBoundExceptionTest() {
-        String inputValue1 = "-1, 2, 3, 4, 6, 5";
-        String inputValue2 = "1, 2, 3, 4, 5, 47";
-
+    @ParameterizedTest(name = "{displayName} ({index}) : {0}")
+    @ValueSource(strings = {
+            "-1, 2, 3, 4, 6, 5",
+            "1, 2, 3, 4, 5, 47"
+    })
+    @TestFactory
+    void outOfBoundExceptionTest(String inputValue) {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> validator.checkingRange(inputValue1))
+                assertThatThrownBy(() -> validator.checkingRange(inputValue))
                         .isInstanceOf(IllegalArgumentException.class)
         );
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> validator.checkingRange(inputValue2))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-        Exception exception1 = assertThrows(IllegalArgumentException.class, () -> validator.checkingRange(inputValue1));
-        Exception exception2 = assertThrows(IllegalArgumentException.class, () -> validator.checkingRange(inputValue2));
 
-        assertThat(exception1.getMessage()).isEqualTo(ErrorMessages.OUT_OF_BOUND.getMessage());
-        assertThat(exception2.getMessage()).isEqualTo(ErrorMessages.OUT_OF_BOUND.getMessage());
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> validator.checkingRange(inputValue))
+                .withMessage(ErrorMessages.OUT_OF_BOUND.getMessage());
     }
 
     @Order(4)
     @DisplayName("4차:6개보다 많거나 적게 숫자 입력")
-    @Test
-    void hasLessOrMoreNumber() {
-        String inputValueMore = "1, 2, 3, 4, 5, 6, 7";
-        String inputValueLess = "1, 2, 3, 4, 5";
+    @ParameterizedTest(name = "{displayName} ({index}) : {0}")
+    @ValueSource(strings = {
+            "1, 2, 3, 4, 5, 6, 7",
+            "1, 2, 3, 4, 5"
+    })
+    @TestFactory
+    void hasLessOrMoreNumber(String inputValue) {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> validator.checkingQuantity(inputValueMore))
+                assertThatThrownBy(() -> validator.checkingQuantity(inputValue))
                         .isInstanceOf(IllegalArgumentException.class)
         );
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> validator.checkingQuantity(inputValueLess))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-        Exception exceptionMore = assertThrows(IllegalArgumentException.class,
-                () -> validator.checkingQuantity(inputValueMore));
-        Exception exceptionLess = assertThrows(IllegalArgumentException.class,
-                () -> validator.checkingQuantity(inputValueLess));
-        assertThat(exceptionMore.getMessage()).isEqualTo(ErrorMessages.MORE_THAN_SIX.getMessage());
-        assertThat(exceptionLess.getMessage()).isEqualTo(ErrorMessages.LESS_THAN_SIX.getMessage());
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> validator.checkingQuantity(inputValue))
+                .withMessage(inputValue.equals("1, 2, 3, 4, 5, 6, 7")
+                        ? ErrorMessages.MORE_THAN_SIX.getMessage()
+                        : ErrorMessages.LESS_THAN_SIX.getMessage()
+                );
+
     }
 
     @Order(5)
     @DisplayName("5차:중복 값 입력")
-    @Test
-    void hasOverlapNumber() {
-        String inputValue = "1, 2, 2, 4, 7, 5";
+    @ParameterizedTest(name = "{displayName} ({index}) : {0}")
+    @ValueSource(strings = {
+            "1, 2, 2, 4, 7, 5"
+    })
+    @TestFactory
+    void hasOverlapNumber(String inputValue) {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> validator.checkingOverlap(inputValue))
                         .isInstanceOf(IllegalArgumentException.class)
         );
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> validator.checkingOverlap(inputValue));
-        assertThat(exception.getMessage()).isEqualTo(ErrorMessages.HAS_OVERLAP_NUMBER.getMessage());
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> validator.checkingOverlap(inputValue))
+                .withMessage(ErrorMessages.HAS_OVERLAP_NUMBER.getMessage());
     }
 
     @Order(6)
     @DisplayName("보너스 번호 입력 검증")
-    @Test
-    void notSingleNumber() {
-        String inputValue1 = "4   23";
-        String inputValue2 = "2, 5, 6";
-        String inputValue3 = "a";
-
+    @ParameterizedTest(name = "{displayName} ({index}) : {0}")
+    @ValueSource(strings = {
+            "4   23",
+            "2, 5, 6",
+            "1   .  3"
+    })
+    @TestFactory
+    void notSingleNumber(String inputValue) {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> validator.checkingQuantity(inputValue1))
+                assertThatThrownBy(() -> validator.checkingQuantity(inputValue))
                         .isInstanceOf(IllegalArgumentException.class)
         );
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> validator.checkingQuantity(inputValue2))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> validator.checkingQuantity(inputValue3))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-
-        Exception exceptionNotSingleNum = assertThrows(IllegalArgumentException.class,
-                () -> validator.checkingSingleNumber(inputValue1));
-        assertThat(exceptionNotSingleNum.getMessage()).isEqualTo(ErrorMessages.NOT_SINGLE_NUMBER.getMessage());
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> validator.checkingSingleNumber(inputValue))
+                .withMessage(ErrorMessages.NOT_SINGLE_NUMBER.getMessage());
     }
 
     @Order(7)
     @DisplayName("validateFomula 통합 테스트")
-    @Test
-    void fomulaNumberIntegrationTest() {
-        String inputValue1 = "1,2,3,4.5.6"; // 구분자 오류
-        String inputValue2 = "1,2,3,4,5,a"; // 숫자 여부
-        String inputValue3 = "1,2,3,4,5,47"; // 범위 준수 여부
-        String inputValue4 = "1,2,3,4,5,6,7"; // 숫자 갯수 준수 여부
-        String inputValue5 = "1,2,2,3,4,5"; // 중복 값 여부
-
-        Exception exception1 = assertThrows(IllegalArgumentException.class,
-                () -> validator.validateFomula(inputValue1));
-        assertThat(exception1.getMessage()).isEqualTo(ErrorMessages.WRONG_SEPARATOR.getMessage());
-
-        Exception exception2 = assertThrows(IllegalArgumentException.class,
-                () -> validator.validateFomula(inputValue2));
-        assertThat(exception2.getMessage()).isEqualTo(ErrorMessages.NOT_NUMBER_EXIST.getMessage());
-
-        Exception exception3 = assertThrows(IllegalArgumentException.class,
-                () -> validator.validateFomula(inputValue3));
-        assertThat(exception3.getMessage()).isEqualTo(ErrorMessages.OUT_OF_BOUND.getMessage());
-
-        Exception exception4 = assertThrows(IllegalArgumentException.class,
-                () -> validator.validateFomula(inputValue4));
-        assertThat(exception4.getMessage()).isEqualTo(ErrorMessages.MORE_THAN_SIX.getMessage());
-
-        Exception exception5 = assertThrows(IllegalArgumentException.class,
-                () -> validator.validateFomula(inputValue5));
-        assertThat(exception5.getMessage()).isEqualTo(ErrorMessages.HAS_OVERLAP_NUMBER.getMessage());
+    @ParameterizedTest(name = "{displayName} ({index}) : {0}")
+    @MethodSource("paramsForFomulaNumberIntegrationTest")
+    @TestFactory
+    void fomulaNumberIntegrationTest(String inputValue, String message) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> validator.validateFomula(inputValue))
+                .withMessage(message);
+    }
+    private static Stream<Arguments> paramsForFomulaNumberIntegrationTest(){
+        return Stream.of(
+                Arguments.of("1,2,3,4.5.6", ErrorMessages.WRONG_SEPARATOR.getMessage()),
+                Arguments.of("1,2,3,4,5,a", ErrorMessages.NOT_NUMBER_EXIST.getMessage()),
+                Arguments.of("1,2,3,4,5,47", ErrorMessages.OUT_OF_BOUND.getMessage()),
+                Arguments.of("1,2,3,4,5,6,7", ErrorMessages.MORE_THAN_SIX.getMessage()),
+                Arguments.of("1,2,2,3,4,5", ErrorMessages.HAS_OVERLAP_NUMBER.getMessage())
+        );
     }
 
     @Order(8)
     @DisplayName("validateBonus 통합 테스트")
-    @Test
-    void bonusNumberIntegrationTest() {
-        String inputValue1 = "4   23";
-        String inputValue2 = "a";
-        String inputValue3 = "47";
-        Exception exception1 = assertThrows(IllegalArgumentException.class,
-                () -> validator.validateBonus(inputValue1));
-        assertThat(exception1.getMessage()).isEqualTo(ErrorMessages.NOT_SINGLE_NUMBER.getMessage());
-
-        Exception exception2 = assertThrows(IllegalArgumentException.class,
-                () -> validator.validateBonus(inputValue2));
-        assertThat(exception2.getMessage()).isEqualTo(ErrorMessages.NOT_NUMBER_EXIST.getMessage());
-
-        Exception exception3 = assertThrows(IllegalArgumentException.class,
-                () -> validator.validateBonus(inputValue3));
-        assertThat(exception3.getMessage()).isEqualTo(ErrorMessages.OUT_OF_BOUND.getMessage());
-
+    @ParameterizedTest(name = "{displayName} ({index}) : {0}")
+    @MethodSource("paramsForbonusNumberIntegrationTest")
+    @TestFactory
+    void bonusNumberIntegrationTest(String inputValue, String message) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> validator.validateBonus(inputValue))
+                .withMessage(message);
+    }
+    private static Stream<Arguments> paramsForbonusNumberIntegrationTest(){
+        return Stream.of(
+                Arguments.of("4   23", ErrorMessages.NOT_SINGLE_NUMBER.getMessage()),
+                Arguments.of("a", ErrorMessages.NOT_NUMBER_EXIST.getMessage()),
+                Arguments.of("47", ErrorMessages.OUT_OF_BOUND.getMessage())
+                );
     }
 }
