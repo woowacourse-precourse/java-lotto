049c1cd11178f2abd532d5fedf9a7a4ae04e05df
yujaemin <woawoa0513@gmail.com>
2022-11-15 17:31:20 +0900

    feat: 도메인 로직 유효성 검사
    WinningNumber의 도메인 로직에서 유효성 검사를 실행한다.

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import static lotto.utils.Validation.hasDuplicatedNumber;
+import static lotto.utils.Validation.isNumbersInCorrectRange;
+import static lotto.utils.Validation.isNumberInCorrectRnage;
+
 import java.util.List;
 
 public class WinningNumber {
@@ -8,6 +12,16 @@ public class WinningNumber {
     private int bonusNumber;
 
     public WinningNumber(List<Integer> numbers, int bonusNumber) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또에는 총 6개의 번호가 존재해야 합니다.");
+        }
+        if (hasDuplicatedNumber(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복되는 번호가 존재합니다.");
+        }
+        if (!isNumbersInCorrectRange(numbers) || !isNumberInCorrectRnage(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+
         this.numbers = numbers;
         this.bonusNumber = bonusNumber;
     }
