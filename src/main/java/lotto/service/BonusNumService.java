0169b5aba0a2dc00ac404cc44dc5430769d1ce52
simba <shim9597@gmail.com>
2022-11-15 10:31:30 +0900

    refactor(lotto): 보너스 번호가 숫자가 아닐 경우 예외 발생시키는 기능 추가

--- src/main/java/lotto/service/BonusNumService.java
+++ src/main/java/lotto/service/BonusNumService.java
@@ -10,7 +10,8 @@ public class BonusNumService {
         System.out.println("보너스 번호를 입력해 주세요.");
 
         String inputString = Console.readLine();
-        int input = Integer.parseInt(inputString);
+
+        int input = toInt(inputString);
 
         validation(input, winningNum);
 
@@ -22,15 +23,26 @@ public class BonusNumService {
         rangeCheck(input);
     }
 
+    private static int toInt(String inputString) {
+        try {
+            return Integer.parseInt(inputString);
+        } catch (Exception e) {
+            System.out.println("숫자만 입력 가능합니다");
+            throw new IllegalArgumentException();
+        }
+    }
+
     private void rangeCheck(int input) throws IllegalArgumentException {
         if (input <= 0 || input > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45사이의 숫자여야 합니다.");
+            System.out.println("[ERROR] 보너스 번호는 1부터 45사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException();
         }
     }
 
     private static void duplicateWithWinningNum(int input, List<Integer> winningNum) {
         if (winningNum.contains(input)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호에 없는 숫자여야 합니다.");
+            System.out.println("[ERROR] 보너스 번호는 당첨 번호에 없는 숫자여야 합니다.");
+            throw new IllegalArgumentException();
         }
     }
 }
