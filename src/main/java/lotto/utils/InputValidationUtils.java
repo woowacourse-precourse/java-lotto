81584546e04e3dec8d646d6abaf14ce3031ee381
LEE-sh1673 <lsh901673@gmail.com>
2022-11-15 16:47:32 +0900

    feat(utils): 입력된 숫자가 1~45 사이의 숫자인지 검증하는 기능 추가

--- src/main/java/lotto/utils/InputValidationUtils.java
+++ src/main/java/lotto/utils/InputValidationUtils.java
@@ -5,6 +5,10 @@ public class InputValidationUtils {
 	private static final String ERR_MESSAGE_HEAD
 		= "[ERROR] ";
 
+	private static final int MAX_NUMBER = 45;
+
+	private static final int MIN_NUMBER = 1;
+
 	public static void validatePrice(final int price) {
 		if (!isDivisibleWithThousand(price)) {
 			throw new IllegalArgumentException(
@@ -15,5 +19,17 @@ public class InputValidationUtils {
 
 	private static boolean isDivisibleWithThousand(final int price) {
 		return price != 0 && price % 1000 == 0;
+	}
+
+	public static void validateNumber(final int number) {
+		if (isOutOfBound(number)) {
+			throw new IllegalArgumentException(
+				ERR_MESSAGE_HEAD + "로또 번호는 1부터 45 사이의 숫자여야 합니다."
+			);
+		}
+	}
+
+	private static boolean isOutOfBound(final int number) {
+		return number > MAX_NUMBER || number < MIN_NUMBER;
 	}
 }
--- src/test/java/lotto/utils/InputValidationUtilsTest.java
+++ src/test/java/lotto/utils/InputValidationUtilsTest.java
@@ -2,7 +2,6 @@ package lotto.utils;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,6 +14,16 @@ public class InputValidationUtilsTest {
 	void validatePrice_입력이_1000_단위로_나누어떨어지는_수인지_검증(final int money) {
 		Assertions.assertThatThrownBy(
 				() -> InputValidationUtils.validatePrice(money)
+			).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(ERROR_MESSAGE);
+	}
+
+	@DisplayName("입력된 숫자가 1~45 사이의 숫자인지 검증할 수 있다.")
+	@ParameterizedTest
+	@ValueSource(ints = {0, 46, -1})
+	void validateNumber_입력된_숫자가_1에서_45_사이의_숫자인지_검증(final int number) {
+		Assertions.assertThatThrownBy(
+				() -> InputValidationUtils.validateNumber(number)
 			).isInstanceOf(IllegalArgumentException.class)
 			.hasMessageContaining(ERROR_MESSAGE);
 	}
