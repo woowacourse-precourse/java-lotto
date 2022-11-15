a0f327e72c50f18b4031fa53cc1535b3195ac6b1
Sanmgmin <sangmins930@gmail.com>
2022-11-15 21:56:26 +0900

    feat(Constants): add SPLIT_MARK

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
     public static final int MAX_VALUE_OF_LOTTO = 45;
     public static final int MONEY_UNIT = 1000;
 
+    public static final String SPLIT_MARK = ",";
     public static final String ERROR_MESSAGE_FOR_SIZE = "[ERROR] 로또 번호는 " + NUM_OF_LOTTO + "자리여야 합니다.";
     public static final String ERROR_MESSAGE_FOR_RANGE_OF_NUMBER = "[ERROR] 로또 번호는 " + MIN_VALUE_OF_LOTTO + "부터 " + MAX_VALUE_OF_LOTTO + " 사이의 숫자여야 합니다.";
     public static final String ERROR_MESSAGE_FOR_DUPLICATION = "[ERROR] 로또 번호는 서로 중복되지 않는 숫자여야 합니다.";
