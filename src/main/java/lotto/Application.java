4293db53c77f3683a14a056438894851c59c65cd
tunkcalb <wjdgusdndi@gmail.com>
2022-11-15 16:22:39 +0900

    check input money

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,15 +6,24 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 public class Application {
     public static void main(String[] args) {
-
+        String inputmoney = InputMoney();
+        CheckUnit(inputmoney);
     }
 
-    public static int InputMoney(){
+    public static String InputMoney() {
         System.out.println("구매금액을 입력해 주세요.");
-        String Money = Console.readLine();
-
-    public static void CheckUnit(){
-
+        String inputmoney = Console.readLine();
+        return inputmoney;
+    }
+    public static void CheckUnit(String inputmoney){
+        try{
+            Integer money = Integer.valueOf(inputmoney);
+            if(money % 1000 !=0){
+                throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위입니다.");
+            }
+        } catch (Exception e){
+            throw new IllegalArgumentException("[ERROR] 유효한 숫자를 입력해주세요");
+        }
     }
 
     public static int CountLotto(int Money){
