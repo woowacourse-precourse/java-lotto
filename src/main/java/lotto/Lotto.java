e59eb300f68528737623b4775613ff064ae34d49
seongha <11pi885@gmail.com>
2022-11-15 16:08:35 +0900

    feat: 중복된 숫자 검사

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Lotto {
@@ -8,6 +9,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        checkDuplicate(numbers);
     }
 
     private void validate(List<Integer> numbers) {
@@ -17,4 +19,14 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void checkDuplicate(List<Integer> numbers) {
+        boolean[] visitNumbers = new boolean[46];
+        Arrays.fill(visitNumbers, false);
+        for (int number : numbers) {
+            if (visitNumbers[number]) {
+                throw new IllegalArgumentException();
+            }
+            visitNumbers[number] = true;
+        }
+    }
 }
