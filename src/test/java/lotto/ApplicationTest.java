2bf93957afe069333ce1d34426c58d9e4d7d2a49
hyun0404woo <hyun0404woo@gmail.com>
2022-11-15 20:34:16 +0900

    test: 시스템 정상 실행 여부 테스트
    - 5000원치 구매

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -13,7 +13,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest extends NsTest {
 
+    @DisplayName("전체_기능_테스트1 -> 8000원치 구매")
     @Test
+    void 전체_기능_테스트1() {
         assertRandomUniqueNumbersInRangeTest(
                 () -> {
                     run("8000", "1,2,3,4,5,6", "7");
@@ -46,7 +48,35 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @DisplayName("전체_기능_테스트2 -> 5000원치 구매")
     @Test
+    void 전체_기능_테스트2() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("5000", "1,8,11,41,42,44", "7");
+                    assertThat(output()).contains(
+                            "5개를 구매했습니다.",
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[13, 14, 16, 38, 42, 45]",
+                            "3개 일치 (5,000원) - 2개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 1개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 30200.0%입니다."
+                    );
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45)
+        );
+    }
+
     @DisplayName("로또_구매비용_지불_예외_테스트")
     @Test
     void 로또_구매비용_지불_예외_테스트() {
