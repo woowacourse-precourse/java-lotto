95ec4bfe8ca049e9b13a3eddca3c2cc8f8b8f197
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-15 21:32:42 +0900

    Feat(Exception): Add BonusNumber ExceptionRange Method

--- src/main/java/lotto/exception/BonusNumber.java
+++ src/main/java/lotto/exception/BonusNumber.java
@@ -1,5 +1,17 @@
 package lotto.exception;
 
+import lotto.constants.Error;
+import lotto.constants.Number;
+
 public class BonusNumber {
+    public static void exceptionRange(String bonusNumber) {
+        int rangeStart = Number.RANGE_START.getNumber();
+        int rangeEnd = Number.RANGE_END.getNumber();
+        int check = Integer.parseInt(bonusNumber);
+        if (!(rangeStart <= check && check <= rangeEnd)) {
+            throw new IllegalArgumentException(Error.WRONG_BONUS_NUM_RANGE.getMsg());
+        }
+    }
+
 
 }
