398b7077c3e54ab10e65b7da98f53b89718b12a7
beombu <sunil1369@bible.ac.kr>
2022-11-15 15:31:42 +0900

    Style : 변수명 및 띄어쓰기 수정

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,41 +1,41 @@
 package lotto.model;
 
 public enum Rank {
-    FIRST(6,2000000000),
-    SECOND(5,30000000),
-    THIRD(5,1500000),
-    FOURTH(4,50000),
-    FIFTH(3,5000),
-    NONE(0,0);
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    NONE(0, 0);
 
     public static final int RIGHT_BONUS = 1;
     public static final int WRONG_BONUS = 0;
 
-    private final int matchCount;
+    private final int matchNumber;
     private final int priceMoney;
 
     Rank(int matchNumber, int priceMoney) {
-        this.matchCount = matchNumber;
+        this.matchNumber = matchNumber;
         this.priceMoney = priceMoney;
     }
 
-    public static Rank findByMatchNumber(int number, int checkBonus){
-        if(number < FIFTH.matchCount){
+    public static Rank findByMatchNumber(int number, int checkBonus) {
+        if (number < FIFTH.matchNumber) {
             return NONE;
-        }else if(number + checkBonus == FIFTH.matchCount){
+        } else if (number + checkBonus == FIFTH.matchNumber) {
             return FIFTH;
-        }else if(number + checkBonus == FOURTH.matchCount){
+        } else if (number + checkBonus == FOURTH.matchNumber) {
             return FOURTH;
-        }else if(number == THIRD.matchCount && checkBonus == WRONG_BONUS){
+        } else if (number == THIRD.matchNumber && checkBonus == WRONG_BONUS) {
             return THIRD;
-        }else if(number == SECOND.matchCount && checkBonus == RIGHT_BONUS){
+        } else if (number == SECOND.matchNumber && checkBonus == RIGHT_BONUS) {
             return SECOND;
         }
         return null;
     }
 
-    public int getMatchCount() {
-        return matchCount;
+    public int getMatchNumber() {
+        return matchNumber;
     }
 
     public int getPriceMoney() {
