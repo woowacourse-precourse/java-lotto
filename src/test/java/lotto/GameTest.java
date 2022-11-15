5625978783f90dee6b9b4e73d9d9ca86a9ab75f6
twknds <twknds@naver.com>
2022-11-15 20:47:09 +0900

    test: 로또 구매 금액 범위, 값 예외처리 테스트 추가

--- src/test/java/lotto/GameTest.java
+++ src/test/java/lotto/GameTest.java
@@ -6,12 +6,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class GameTest {
+class GameTest{
     Game game;
     @BeforeEach
     void beforeEach(){
@@ -23,7 +22,7 @@ public class GameTest {
     }
     @Test
     @DisplayName("로또 게임 생성 테스트")
-    void generateLottoTest(){
+    void generateGameTest(){
         setInputValues("2000");
         game.inputLottoCount();
         game.generateLotto();
@@ -32,5 +31,57 @@ public class GameTest {
         setInputValues("7");
         game.generateBonusNumber();
     }
+    @Test
+    @DisplayName("로또 게임 예외 테스트 - 보너스 번호")
+    void bonusSizeOverExceptionTest(){
+        setInputValues("2000");
+        game.inputLottoCount();
+        game.generateLotto();
+        setInputValues("1,2,3,4,5,6");
+        game.generateAwardLotto();
+        setInputValues("7,8");
+        game.generateBonusNumber();
+        assertThatThrownBy(() -> game.generateBonusNumber())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("로또 게임 예외 테스트 - 정확한 숫자인지")
+    void numberUnCorrectExceptionTest(){
+        setInputValues("2000");
+        game.inputLottoCount();
+        game.generateLotto();
+        setInputValues("1,2,3,4,5,6");
+        game.generateAwardLotto();
+        setInputValues("46");
+        game.generateBonusNumber();
+        assertThatThrownBy(() -> game.generateBonusNumber())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("로또 게임 예외 테스트 - 중복 숫자 검사")
+    void mutualInputExceptionTest(){
+        setInputValues("2000");
+        game.inputLottoCount();
+        game.generateLotto();
+        setInputValues("1,2,3,4,5,6");
+        game.generateAwardLotto();
+        setInputValues("5");
 
+        assertThatThrownBy(() -> game.generateBonusNumber())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("로또 게임 예외 테스트 - 가격 범위")
+    void unCorrectMoneyRangeExceptionTest(){
+        setInputValues("100");
+        assertThatThrownBy(() -> game.inputLottoCount())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("로또 게임 예외 테스트 - 가격값")
+    void unCorrectMoneyInputExceptionTest(){
+        setInputValues("2000k");
+        assertThatThrownBy(() -> game.inputLottoCount())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
