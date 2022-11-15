e0bd8f49a71858e576bcbf0a0cd922c505a1e2e2
mng990 <ryoo0504@pusan.ac.kr>
2022-11-15 22:24:24 +0900

    feat(main/java/lotto/view): ExceptionHandlerUI.java 작성
    예외처리 입출력 클래스(view)

---
+++ src/main/java/lotto/view/ExceptionHandlerUI.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class ExceptionHandlerUI {
+    private final String ERROR_MESSAGE = "[ERROR] ";
+
+    public void printErrorMessage(String errorMessage){
+        System.out.println(ERROR_MESSAGE + errorMessage);
+    }
+}
