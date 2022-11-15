131401c508f25bab778a453bdf45b4b9cd0d0ca4
zcae1020 <zcae1020@naver.com>
2022-11-15 11:37:06 +0900

    test(lotto): 계산기의 parameter results값의 길이 테스트 추가

--- src/test/java/lotto/CalculatorTest.java
+++ src/test/java/lotto/CalculatorTest.java
@@ -5,16 +5,26 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
     Calculator calculator = new Calculator();
 
-    @DisplayName("계산기는 수익율을 구한다")
+    @DisplayName("계산기는 수익율을 구한다.")
     @Test
     void 수익률_구하기() {
         List<Integer> results = List.of(1,2,3,4,5);
         int money = 1_000_000_000;
         assertEquals(calculator.getProfitRate(results, money), 1012.4605);
+    }
+
+    @DisplayName("결과값은 5개의 원소로 이루어져야한다.")
+    @Test
+    void 결과값_5개원소() {
+        List<Integer> results = List.of(1,2,3,4);
+        int money = 1_000_000_000;
+        assertThatThrownBy(() -> calculator.getProfitRate(results, money))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
