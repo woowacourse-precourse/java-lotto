612818d4593e79f91177c4cdc4cf438783a143ba
YunDaeHyeok <bmong4mong0318@gmail.com>
2022-11-15 11:13:39 +0900

    feat: Add input console and interface (#4)
    Made input interface and input console to take input.

---
+++ src/main/java/lotto/io/InputConsole.java
@@ -0,0 +1,10 @@
+package lotto.io;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+public class InputConsole implements Input{
+
+    @Override
+    public String scan() {
+        return readLine();
+    }
+}
