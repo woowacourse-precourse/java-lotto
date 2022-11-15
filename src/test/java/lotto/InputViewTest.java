c455d157927c0f19a75a57bb8de38d85e78f770f
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-15 16:26:53 +0900

    test(InputViewTest): 구분 문자가 쉼표(,)가 아닌 경우 예외 처리 테스트 코드 작성

--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -1,9 +1,13 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import lotto.view.InputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputViewTest {
@@ -13,5 +17,13 @@ public class InputViewTest {
         assertThatThrownBy(() -> new InputView().validateNumeric("-10000"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 입력값은 숫자만 가능합니다.");
+    }
+
+    @DisplayName("당첨 번호에 숫자와 구분자(,) 외 다른 입력이 들어오면 예외가 발생한다.")
+    @Test
+    void getInputByUnValidSeparator() {
+        assertThatThrownBy(() -> new InputView().validateSeparator("1,#,4,6,7"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 숫자와 구분자(,)만 입력될 수 있습니다.");
     }
 }
