1020fe38e93c9f5a9492f1ae1f5660621d9c8ebe
noparamin <noparamin@naver.com>
2022-11-15 22:55:48 +0900

    feat: 에러 메시지 enum 추가

--- src/main/java/lotto/enums/InputMessage.java
+++ src/main/java/lotto/enums/InputMessage.java
@@ -1,7 +1,8 @@
 package lotto.enums;
 
 public enum InputMessage {
-    MONEY_INPUT_MESSAGE("구입금액을 입력해 주세요."), WINNING_NUMBERS_INPUT_MESSAGE("당첨 번호를 입력해 주세요."), BONUS_NUMBER_INPUT_MESSAGE("보너스 번호를 입력해 주세요.");
+    MONEY_INPUT_MESSAGE("구입금액을 입력해 주세요."), WINNING_NUMBERS_INPUT_MESSAGE("당첨 번호를 입력해 주세요."), BONUS_NUMBER_INPUT_MESSAGE("보너스 번호를 입력해 주세요."),
+    ERROR_MESSAGE("[ERROR] 잘못된 값을 입력했습니다. 프로그램을 종료합니다.");
 
     private String message;
 
