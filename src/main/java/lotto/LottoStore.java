0046cd7da5d3ded26cdd4df4382a518380282ad1
tenta <eunoia3802@gmail.com>
2022-11-15 21:51:45 +0900

    refactor: 로또 가격 상수값으로 변경

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -1,8 +1,10 @@
 package lotto;
 
 public class LottoStore {
+    private final int lottoPrice = 1000;
+
     public int calculatePurchasedLottoCount(int inputMoney) {
-        int lottoCount = inputMoney / 1000;
+        int lottoCount = inputMoney / lottoPrice;
         return lottoCount;
     }
 }
