5df15b8da3a5ce136ca90b6cb5fab987567f1188
LeeHaiLim <hailim1997@gmail.com>
2022-11-15 10:50:09 +0900

    feat(model): add parameter of detailed error message

--- src/main/java/lotto/model/InputValidator.java
+++ src/main/java/lotto/model/InputValidator.java
@@ -13,19 +13,19 @@ public class InputValidator {
         try {
             Integer.parseInt(number);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(InputError.NUMBER.message());
         }
     }
 
     private static void isPositive(String number) {
         if (Integer.parseInt(number) <= 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(InputError.MONEY_POSITIVE.message());
         }
     }
 
     private static void isMultiplePrice(String number) {
         if (Integer.parseInt(number) % Constants.LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(InputError.MONEY_MOD.message());
         }
     }
 }
