488b7b67fc45aa8f7ac6092e31adac8ca561821e
im-shung <seoyoung1113@gmail.com>
2022-11-16 01:01:27 +0900

    style: :art: 에러 코드 이름 정확성 향상시키기

--- src/main/java/lotto/exception/Error.java
+++ src/main/java/lotto/exception/Error.java
@@ -4,7 +4,7 @@ public enum Error {
     WRONG_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     OVER_SIZE("로또 번호의 개수는 6개여야만 합니다."),
     NOT_DUPLICATE("로또 번호는 중복되지 않아야 합니다."),
-    WRONG_AMOUNT("금액을 1,000단위로 입력해야 합니다."),
+    WRONG_LOTTO_AMOUNT("금액을 1,000단위로 입력해야 합니다."),
     NOT_INT("숫자를 입력해야 합니다.");
 
     private final String message;
