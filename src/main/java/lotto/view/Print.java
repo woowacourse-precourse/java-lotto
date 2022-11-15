266e270a86be24775bd4b4c299de5ca210bed23e
dooboocookie <bkh3030@gmail.com>
2022-11-15 17:13:17 +0900

    refactor: 로또의 출력 폼에 사용되는 문자열 상수로 분리

--- src/main/java/lotto/view/Print.java
+++ src/main/java/lotto/view/Print.java
@@ -16,6 +16,9 @@ public class Print {
     private static String MESSAGE_RANK_COUNTER_START = "당첨 통계\n---";
     private static String MESSAGE_TOTAL_YIELD = "총 수익률은 %s입니다.";
     private static String FORM_RANK_COUNTER = "%s - %d개\n";
+    private static String FORM_LIST_START = "[";
+    private static String FROM_LIST_END = "]";
+    private static String FORM_LIST_MIDDLE = ", ";
 
     public static void printInsertMoney() {
         System.out.println(MESSAGE_INSERT_MONEY);
@@ -30,13 +33,13 @@ public class Print {
 
     public static void printLotto(Lotto lotto) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("[");
+        stringBuilder.append(FORM_LIST_START);
         stringBuilder.append(lotto.findLottoNumber(0));
         for (int numberIndex = 1; numberIndex < Rules.LOTTO_SIZE; numberIndex++) {
-            stringBuilder.append(", ");
+            stringBuilder.append(FORM_LIST_MIDDLE);
             stringBuilder.append(lotto.findLottoNumber(numberIndex));
         }
-        stringBuilder.append("]");
+        stringBuilder.append(FROM_LIST_END);
         System.out.println(stringBuilder.toString());
     }
 
