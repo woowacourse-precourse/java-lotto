fd7626019682a3c8c5deb1620496b6045974c126
yejin130 <jinis2023@gmail.com>
2022-11-15 15:34:52 +0900

    refactor(Lotto): validate 메서드 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,26 +16,36 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        checkLength(numbers);
+        checkDuplicate(numbers);
+        checkRange(numbers);
+    }
+
+    private void checkLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(ERROR_LENGTH);
         }
+    }
 
+    private void checkDuplicate(List<Integer> numbers) {
         HashSet<Integer> numbersNotDuplicate = new HashSet<>();
         numbersNotDuplicate.addAll(numbers);
         if (numbersNotDuplicate.size() != numbers.size()) {
             throw new IllegalArgumentException(ERROR_DUPLICATE);
         }
+    }
 
+    private void checkRange(List<Integer> numbers) {
         if (Collections.min(numbers) < 1 || Collections.max(numbers) > 45) {
             throw new IllegalArgumentException(ERROR_RANGE);
         }
     }
 
-    private void sortNumbers(List<Integer> numbers){
+    private void sortNumbers(List<Integer> numbers) {
         Collections.sort(numbers);
     }
 
-    public List<Integer> getNumbers(){
+    public List<Integer> getNumbers() {
         return numbers;
     }
 
