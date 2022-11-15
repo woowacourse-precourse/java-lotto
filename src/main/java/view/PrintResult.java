05859d924def905ee04f6cb5e1301b38894c03c7
dydtmd6801 <dydtmd6801@naver.com>
2022-11-15 19:20:16 +0900

    fix(PrintResult): static 키워드 추가
    외부에서 참조하기 위해 static 키워드 추가

--- src/main/java/view/PrintResult.java
+++ src/main/java/view/PrintResult.java
@@ -6,17 +6,17 @@ import java.util.HashMap;
 import java.util.List;
 
 public class PrintResult {
-    private final String UNIT = "개";
+    private static final String UNIT = "개";
 
     public static void printBuyLotto(int buyLottoNumber){
         System.out.println(buyLottoNumber + "개를 구매했습니다.");
     }
 
-    public void printLottoNumber(List<Integer> lottoNumber){
+    public static void printLottoNumber(List<Integer> lottoNumber){
         System.out.println(lottoNumber);
     }
 
-    public void printMatchDetail(HashMap matchNumbers) {
+    public static void printMatchDetail(HashMap matchNumbers) {
         List<Integer> order = List.of(3, 4, 5, 7, 6);
         for (NumberMatch numberMatch : NumberMatch.values()) {
             int idx = 0;
@@ -24,7 +24,7 @@ public class PrintResult {
         }
     }
 
-    public void printRevenue(int totalMoney, int lottoMoney) {
+    public static void printRevenue(int totalMoney, int lottoMoney) {
         int revenue = totalMoney / lottoMoney * 100;
         System.out.println(revenue);
     }
