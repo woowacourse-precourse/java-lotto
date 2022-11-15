36f369f7c3301dd3410e9ed9160b325c4231479a
SeokHwan An <asok369258@naver.com>
2022-11-15 11:48:46 +0900

    refactor : 로또를 생성하는 기능 구현체 반환으로 수정

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -3,12 +3,13 @@ package lotto;
 import static util.Constant.*;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
 
     public List<Integer> createLotto() {
-        return Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUM, LOTTO_MAX_NUM, LOTTO_SIZE);
+        return new ArrayList<>(Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUM, LOTTO_MAX_NUM, LOTTO_SIZE));
     }
 
 }
