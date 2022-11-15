e39ea36d2392424a8a4368f71ecb9ca3c2703200
ckyeon <qsky8520@gmail.com>
2022-11-15 18:05:20 +0900

    feat(LottoShop): implement 구매 금액이 1,000원 단위인지 확인하는 기능

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -1,5 +1,14 @@
 package lotto.domain;
 
+import lotto.ui.ERROR;
+
 public class LottoShop {
 
+    private final int LOTTO_PRICE = 1000;
+
+    private void validate(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException(ERROR.IS_NOT_THOUSAND_UNIT.getMessage());
+        }
+    }
 }
