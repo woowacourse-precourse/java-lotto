bbd8967f3b8c33910c6e84d1b8d70f41b2948a64
KimSunU <kgw6147@naver.com>
2022-11-15 22:42:38 +0900

    [feat] output을 관리하는 클래스 구현

--- src/main/java/lotto/LottoOutput.java
+++ src/main/java/lotto/LottoOutput.java
@@ -1,2 +1,27 @@
-package lotto;public class LottoOutput {
+package lotto;
+
+import java.util.List;
+
+public class LottoOutput {
+    private static String BUY_LOTTERY_OUTPUT = "개를 구매했습니다.";
+    private static String STATISTIC_COMMENT = "당첨 통계\n---";
+    private static String RANK_INFORMATION_OUTPUT = "%s (%s원) - %d개";
+    private static String STATISTICS_OUTPUT = "총 수익률은 %.1f%%입니다.";
+
+    public static void printLottery(List<Lotto> lotteries){
+        System.out.println();
+        System.out.println(lotteries.size() + BUY_LOTTERY_OUTPUT);
+        for(Lotto lottery : lotteries){
+            System.out.println(lottery.getNumbers());
+        }
+        System.out.println();
+    }
+
+    public static void printResult(LottoResult lottoResult, int bonusNumber){
+        System.out.println(STATISTIC_COMMENT);
+        for(LottoRank rank: LottoRank.values()){
+            if(rank.getRank() ==0) continue;
+            System.out.println();
+        }
+    }
 }
