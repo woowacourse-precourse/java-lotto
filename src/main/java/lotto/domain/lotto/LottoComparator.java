ecb8efc3807550cddd6fabedd52195256414cf55
JuSeong1130 <jsmoon1130@naver.com>
2022-11-15 17:03:52 +0900

    feat(LottoComparator): 보너스번호 포함 여부 기능 구현

--- src/main/java/lotto/domain/lotto/LottoComparator.java
+++ src/main/java/lotto/domain/lotto/LottoComparator.java
@@ -1,5 +1,25 @@
 package lotto.domain.lotto;
 
+import java.util.List;
+import lotto.domain.machine.LottoMachine;
+
 public class LottoComparator {
+
+	LottoMachine lottoMachine;
+
+	public LottoComparator(LottoMachine lottoMachine) {
+		this.lottoMachine = lottoMachine;
+	}
+
+
+	public boolean hasBonusNumber(Lotto lotto) {
+		List<Integer> numbers = lotto.getNumbers();
+		Integer bonusNumber = Integer.parseInt(lottoMachine.getBonusNumber());
+		if (numbers.contains(bonusNumber)) {
+			return true;
+		}
+		return false;
+	}
+
 
 }
