0637d6f3875270bedef693598bc2e73563a76c32
thdwoqor <celebarte01@gmail.com>
2022-11-15 13:43:02 +0900

    test: 숫자 유효성 검증 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.util.Validate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,10 +19,37 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    void NULL값_체크() {
+        assertThatThrownBy(() -> Validate.isNull(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자_체크() {
+        assertThatThrownBy(() -> Validate.isNumber("123j"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자_리스트_체크() {
+        assertThatThrownBy(() -> Validate.isNumbers(List.of("1","2","j")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 길이_체크() {
+        assertThatThrownBy(() -> Validate.length(List.of(1,2,3,4),3))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 중복_체크() {
+        assertThatThrownBy(() -> Validate.duplication(List.of(1,2,3,4,4)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
