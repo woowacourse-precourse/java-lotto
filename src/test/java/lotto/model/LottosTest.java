ca22932c3b0235006adbe11f634ad00c0bb8dd15
cookienc <kim76744@naver.com>
2022-11-15 15:09:29 +0900

    test: Lottos 클래스 테스트 리팩토링

--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -2,6 +2,8 @@ package lotto.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 import java.util.Map;
@@ -12,15 +14,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @DisplayName("Lottos 클래스의 메서드를 테스트한다.")
 class LottosTest {
     @DisplayName("돈이 1000원으로 안 나눠 떨어지면 오류를 던진다.")
-    @Test
-    void ifHaveRestMoneyThrowException() {
-        assertThatThrownBy(() -> Lottos.purchaseLottos(12345))
+    @ParameterizedTest
+    @ValueSource(ints = {12345, 12001, 12100, 12010})
+    void ifHaveRestMoneyThrowException(int money) {
+        assertThatThrownBy(() -> Lottos.purchaseLottos(money))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("lottos의 compareResult를 실행하면 결과값 5개가 생성된다.")
+    @DisplayName("lottos의 compareResult를 실행하면 결과값 5개(1등 ~ 5등)가 생성된다.")
     @Test
-    void compareResult() {
+    void returnFiveWinningResultWhenCompareTenLotto() {
         //given
         Lottos lottos = Lottos.purchaseLottos(10_000);
         Result result = Result.of(List.of(1, 2, 3, 4, 5, 6), 7);
