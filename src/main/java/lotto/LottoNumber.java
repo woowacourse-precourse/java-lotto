956f37c33c84246e9c86249ffbca882e4f1c92c6
juno-junho <ssmm0205@naver.com>
2022-11-15 20:06:50 +0900

    feat(lotto): add constructor to LottoNumber
    LottoNumber 클래스에 생성자 추가

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -1,4 +1,13 @@
 package lotto;
 
 public class LottoNumber {
+
+    private static final int MIN_LOTTO_NUM = 1;
+    private static final int MAX_LOTTO_NUM = 45;
+    private final int number;
+
+    private LottoNumber(int number) {
+        validate(number);
+        this.number = number;
+    }
 }
