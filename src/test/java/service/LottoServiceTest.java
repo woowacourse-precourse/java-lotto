7a895130b408c11f9b2c2f7cacd68138238e3e7b
YooSuYeon <usuyn000@gmail.com>
2022-11-15 16:04:12 +0900

    test: 보너스 번호 입력 예외 테스트

--- src/test/java/service/LottoServiceTest.java
+++ src/test/java/service/LottoServiceTest.java
@@ -42,4 +42,23 @@ public class LottoServiceTest {
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> lottoService.saveWinningNumbers(numbers));
         assertThat(e.getMessage()).isEqualTo(Error.WINNING_NUMBERS_INCLUDE_SAME_NUMBER.getText());
     }
+
+    @DisplayName("보너스 번호 입력 형식 검사")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "0", "46", "50", "1-5"})
+    public void 보너스_번호_입력_형식_예외(String number) {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> lottoService.saveBonusNumber(number));
+        assertThat(e.getMessage()).isEqualTo(Error.BONUS_NUMBER_FORMAT.getText());
+    }
+
+    @DisplayName("보너스 번호 입력 중복 검사")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "10"})
+    public void 보너스_번호_입력_중복_예외(String number) {
+        String winningNumbers = "1,2,10,6,9,14";
+        lottoService.saveWinningNumbers(winningNumbers);
+        
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> lottoService.saveBonusNumber(number));
+        assertThat(e.getMessage()).isEqualTo(Error.BONUS_NUMBER_IN_WINNING_NUMBER.getText());
+    }
 }
