ccddaae85e3fbfdafbe250a2c22b3e80c6e0e000
JeonSangYong <tkddyd420@naver.com>
2022-11-15 13:00:21 +0900

    feat : 보너스 번호 입력받기

--- src/main/java/lotto/domain/Dealer.java
+++ src/main/java/lotto/domain/Dealer.java
@@ -12,14 +12,28 @@ public class Dealer {
     private Lotto lotto;
     private int bonusNumber;
 
+    public void start() {
+        inputAnswerNumbers();
+        inputBonusNumber();
+    }
+
     public void inputAnswerNumbers() {
         System.out.println(Message.INPUT_WIN_LOTTO_NUMBER);
         String numbers = Console.readLine();
         DealerValidator.validAnswerNumber(numbers);
-        List<Integer> lottoNumber = Arrays.stream(numbers.split(","))
+        List<Integer> lottoNumbers = Arrays.stream(numbers.split(","))
                 .map(Integer::parseInt)
                 .sorted()
                 .collect(Collectors.toList());
-        lotto = new Lotto(lottoNumber);
+        lotto = new Lotto(lottoNumbers);
+    }
+
+    public void inputBonusNumber() {
+        System.out.println(Message.INPUT_BONUS_LOTTO_NUMBER);
+        String number = Console.readLine();
+        // 숫자 입력이 아닐 때 예외처리
+        // 당첨 번호 숫자일 때 예외처리
+        // 1 ~ 45 범위의 숫자가 아닐 때 예외처리
+        bonusNumber = Integer.parseInt(number);
     }
 }
