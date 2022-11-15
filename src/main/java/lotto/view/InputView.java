2930cd06a7e8c307041aca4f25dbdf69e6931db6
baekeunsun <eunsun5460@gmail.com>
2022-11-15 14:20:50 +0900

    feat(view/InputView): 입력에 숫자가 아닌 문자가 들어왔을 경우 예외처리
    - (InputView/IsNumber): 입력에 숫자가 아닌 값이 들어왔을 경우 에러문구 출력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,13 +10,15 @@ public class InputView {
 
     public static int InputPrice() {
         System.out.println("구입금액을 입력해 주세요.");
-        int input = Integer.valueOf(readLine());
-        return input;
+        String input = readLine();
+        IsNumber(input);
+        return Integer.valueOf(input);
     }
 
     public static List<Integer> InputWinNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = readLine();
+        IsNumber(input);
         List<Integer> inputs = List.of(input.split(",")).stream()
                 .map(s -> Integer.parseInt(s))
                 .collect(Collectors.toList());
@@ -27,12 +29,21 @@ public class InputView {
         System.out.println("보너스 번호를 입력해 주세요.");
         String input = readLine();
         OnlyOne(input);
+        IsNumber(input);
         return Integer.valueOf(input);
     }
 
     private static void OnlyOne(String input) {
         if (input.contains(",") || input.contains(" ")) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호가 1개 이상입니다.");
+        }
+    }
+
+    private static void IsNumber(String input) {
+        for (char charinput : input.toCharArray()) {
+            if (Character.isDigit(charinput) == false) {
+                throw new IllegalArgumentException("[ERROR] 입력에 숫자가 아닌 값이 있습니다.");
+            }
         }
     }
 }
