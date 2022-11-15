ea316b29e44e720769d29d44d09b9d4b9a144b90
thdwoqor <celebarte01@gmail.com>
2022-11-15 18:10:44 +0900

    feat: 로또 게임 진행 컨트롤러 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,34 @@
+package lotto.controller;
+
+import lotto.Lotto;
+import lotto.Rank;
+import lotto.LottoGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoController {
+    public LottoController() {
+    }
+
+    public void play() {
+        try {
+            int money = InputView.money();
+            List<Lotto> ticket = LottoGenerator.automatic(LottoGenerator.maximum(money));
+            OutputView.ticket(ticket);
+
+            List<Rank> result = result(ticket, InputView.winningNumber(), InputView.bonusNumber());
+            OutputView.result(result, money);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private List<Rank> result(List<Lotto> lottosTicket, List<Integer> winningNumbers, int bonusNumber) {
+        return lottosTicket.stream()
+                .map(e -> e.result(winningNumbers, bonusNumber))
+                .collect(Collectors.toList());
+    }
+}
