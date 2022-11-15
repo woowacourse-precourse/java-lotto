67970bb4b12c293eee021dec2eff838d8fe0382f
qdtjj4 <76ylo1@gmail.com>
2022-11-15 16:38:55 +0900

    refactor: LottoTicket, LottoTickets 클래스 추가
    LottoTickets는 모든 발행한 로또의 집합이고 LottoTicket은 발행한 로또

---
+++ src/main/java/lotto/model/LottoTicket.java
@@ -0,0 +1,11 @@
+package lotto.model;
+
+import java.util.List;
+
+public class LottoTicket {
+    private final List<Integer> ticket;
+
+    public LottoTicket(List<Integer> lottoTicket) {
+        this.ticket = lottoTicket;
+    }
+}
---
+++ src/main/java/lotto/model/LottoTickets.java
@@ -0,0 +1,14 @@
+package lotto.model;
+
+import java.util.List;
+import java.util.Set;
+
+public class LottoTickets {
+    private final Set<List<Integer>> tickets;
+
+    public LottoTickets(Set<List<Integer>> lottoTickets) {
+        this.tickets = lottoTickets;
+    }
+
+
+}
