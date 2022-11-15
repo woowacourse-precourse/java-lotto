0f729bb8e8587b5c0f7fb5dc1b2acf3ebee31b7d
yeonhee <ydol2@naver.com>
2022-11-15 14:36:45 +0900

    refactor(LottoResult) : 결과와 상금을 함께 받아오도록 변경

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,5 +1,20 @@
 package lotto.domain;
 
 public enum LottoResult {
-    THREE, FOUR, FIVE, FIVE_BONUS, SIX, LOSING;
+    THREE(5_000),
+    FOUR(50_000),
+    FIVE(1_500_000),
+    FIVE_BONUS(30_000_000),
+    SIX(2_000_000_000),
+    LOSING(0);
+
+    private final int prize;
+
+    LottoResult(int prize){
+        this.prize = prize;
+    }
+
+    public int getPrize(){
+        return prize;
+    }
 }
