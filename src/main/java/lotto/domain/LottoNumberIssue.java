7adcc61a7ecd209e5db03b1367ac57aa3b8ad9d8
gunheeK <gunhee0516@gmail.com>
2022-11-15 10:35:32 +0900

    feat: 6개의 랜덤 로또 번호 발행 기능 추가

---
+++ src/main/java/lotto/domain/LottoNumberIssue.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoNumberIssue {
+    private final int purchaseQuantity;
+
+    public LottoNumberIssue(int purchaseQuantity) {
+        this.purchaseQuantity = purchaseQuantity;
+    }
+
+    public List<List<Integer>> lottoIssue() {
+        List<List<Integer>> lottoNumber = createLottoNumber();
+        return lottoNumber;
+    }
+
+    public List<List<Integer>> createLottoNumber() {
+        List<List<Integer>> lottoNumber = new ArrayList<>();
+        for (int i = 0; i < purchaseQuantity; i++) {
+            lottoNumber.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        }
+        return lottoNumber;
+    }
+}
