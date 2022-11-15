2c03aaf5ded66da087b3150dd6797e1cf3d7d77e
Sieun Park <psieun0606@gmail.com>
2022-11-15 19:01:54 +0900

    feat: 당첨 내역 출력 기능 추가

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -1,12 +1,16 @@
 package lotto.View;
 
 import lotto.Domain.LottoPublisher;
+import lotto.Domain.MatchWinning;
+import lotto.Domain.WinningPrice;
 
 import java.util.List;
 
 public class OutputView {
 
     LottoPublisher lottoPublisher;
+    WinningPrice winningPrice;
+    MatchWinning matchWinning;
 
     public void printTheNumberOfLotto(int theNumberOfLotto) {
         System.out.println(String.format("%d개를 구매했습니다.", theNumberOfLotto));
@@ -16,6 +20,16 @@ public class OutputView {
         for (List<Integer> column : userInputLottoNumberList) {
             System.out.println(column);
         }
+    }
+
+    public void printResultOfLotto() {
+        System.out.println("당첨통계");
+        System.out.println("---");
+        System.out.println(WinningPrice.FIFTH.getWinningMessage() + matchWinning.getFifthPrice() + "개");
+        System.out.println(WinningPrice.FORTH.getWinningMessage() + matchWinning.getForthPrice() + "개");
+        System.out.println(WinningPrice.THIRD.getWinningMessage() + matchWinning.getThirdPrice() + "개");
+        System.out.println(WinningPrice.SECOND.getWinningMessage() + matchWinning.getSecondPrice() + "개");
+        System.out.println(WinningPrice.FIRST.getWinningMessage() + matchWinning.getFirstPrice() + "개");
     }
 
     public void printEmptyLine() {
