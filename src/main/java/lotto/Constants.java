c5abfb65f902c0b87b11ce0793ff44f4b0466cac
Park Sungchul <try615@naver.com>
2022-11-15 19:27:46 +0900

    feat(Verify.java): 구매 금액 단위 검증 추가
    로또를 구입할 수 있는 단위(현재 1000원)인지 검증하는 메소드 추가.
    단위와 어긋날 시 에러 처리.

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -1,7 +1,11 @@
 package lotto;
 
 public class Constants {
+
+	public static final int MINIMUM_MONEY_UNIT = 1000;
+
 	public static final String PRINT_INPUT_MONEY = "구입금액을 입력해 주세요.";
 
 	public static final String ERROR_NOT_NUMBER_FORMAT = "[ERROR] 숫자를 입력해 주십시오.";
+	public static final String ERROR_NOT_MONEY_UNIT = "[ERROR] 구매 금액 단위를 맞춰 주십시오.";
 }
--- src/main/java/lotto/Verify.java
+++ src/main/java/lotto/Verify.java
@@ -2,4 +2,10 @@ package lotto;
 
 public class Verify {
 
+	public static void verifyThousandUnit(int money) {
+		if (money % Constants.MINIMUM_MONEY_UNIT != 0) {
+			throw new IllegalArgumentException(Constants.ERROR_NOT_MONEY_UNIT);
+		}
+	}
+
 }
