b820307af0fb943fc83fd3601239afeb9dbaccbb
Dongmin Kim <kimdongmin68@naver.com>
2022-11-15 17:29:36 +0900

    feat(lotto): Lotto 클래스의 테스트 통과를 위한 예외 처리 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,14 +5,18 @@ import java.util.Set;
 
 public class Lotto {
     private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
     private static final String ERROR_WRONG_LOTTO_SIZE = "[ERROR] 당첨 번호 6개를 입력해주세요.";
     private static final String ERROR_DUPLICATE_NUMBER = "[ERROR] 입력된 숫자가 중복입니다.";
+    private static final String ERROR_NO_LOTTO_NUMBER = "[ERROR] 정수 1 ~ 45를 입력해주세요.";
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         checkDuplicate(numbers);
+        checkLottoNumber(numbers);
         this.numbers = numbers;
     }
 
@@ -26,6 +30,14 @@ public class Lotto {
         Set<Integer> lottoNumbers = Set.copyOf(numbers);
         if (lottoNumbers.size() != LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException(ERROR_DUPLICATE_NUMBER);
+        }
+    }
+
+    private void checkLottoNumber(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < MIN_LOTTO_NUMBER || MAX_LOTTO_NUMBER < number) {
+                throw new IllegalArgumentException(ERROR_NO_LOTTO_NUMBER);
+            }
         }
     }
 
