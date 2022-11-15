0dcc1865c02c89d4733cfda6c5f1dce06c911846
KTH-96 <taehyun960@gmail.com>
2022-11-15 19:03:52 +0900

    refactor(LottoController): run 메소드 리펙토링

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -18,13 +18,27 @@ public class LottoController {
     }
 
     public void run() {
-        LottoPurchaseDto lottoPurchaseDto = lottoService.purchaseLotto(inputPaymentAmount());
-        OutputView.printLottoPurchaseNumber(lottoPurchaseDto.getLottos().size());
-        OutputView.printLottos(lottoPurchaseDto.getLottos());
+        LottoPurchaseDto lottoPurchaseDto = purchaseLottos();
+        WinnerStatistics winnerStatistics = getWinnerStatistics(lottoPurchaseDto);
+        getProfitRate(winnerStatistics);
+    }
+
+    private void getProfitRate(WinnerStatistics winnerStatistics) {
+        ProfitRate profitRate = lottoService.calculateProfitRate(winnerStatistics.getStatisticsStore());
+        OutputView.printProfitRate(profitRate.getProfitRate());
+    }
+
+    private WinnerStatistics getWinnerStatistics(LottoPurchaseDto lottoPurchaseDto) {
         WinnerStatistics winnerStatistics = lottoService.calculateWinnerStatistics(inputWinnerNumber(),
                 lottoPurchaseDto);
         OutputView.printWinnerStatistics(winnerStatistics.getStatisticsStore());
-        ProfitRate profitRate = lottoService.calculateProfitRate(winnerStatistics.getStatisticsStore());
-        OutputView.printProfitRate(profitRate.getProfitRate());
+        return winnerStatistics;
+    }
+
+    private LottoPurchaseDto purchaseLottos() {
+        LottoPurchaseDto lottoPurchaseDto = lottoService.purchaseLotto(inputPaymentAmount());
+        OutputView.printLottoPurchaseNumber(lottoPurchaseDto.getLottos().size());
+        OutputView.printLottos(lottoPurchaseDto.getLottos());
+        return lottoPurchaseDto;
     }
 }
