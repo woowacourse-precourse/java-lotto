9279837dff9b1f8f8a303104c8bbb7e5b28012ec
namtndus <rnrmf0721@gmail.com>
2022-11-15 15:18:12 +0900

    feat(LogicService): init함수와 checkLottoNumber함수 구현
    로또의 결과를 담을 winningResult를 초기화하는 함수 init와 로또에 당첨번호가 몇개 포한되어있는지 확인하는
    checkLottoNumber함수의 기능 구현

--- src/main/java/lotto/Services/LogicService.java
+++ src/main/java/lotto/Services/LogicService.java
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Dto.Lotto;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LogicService {
     public List<Lotto> makeLotto(int lottoSize) {
@@ -13,5 +15,43 @@ public class LogicService {
             lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
         }
         return lottos;
+    }
+
+    public Map<String,Integer> checkLottoNumber(List<Lotto> lottos, Lotto winningNumbers, int bonusNumber) {
+        Map<String,Integer> winningSize = init();
+        for(Lotto lotto : lottos){
+            // count가 5이고, bonusNumber도 포함되어있는 경우
+            int count = checkWinningNumber(lotto,winningNumbers.getNumbers());
+            if(count >=3){
+                if(count == 5 && lotto.contains(bonusNumber)){
+                    winningSize.put("5bonusNumber",winningSize.get("5bonusNumber")+1);
+                    continue;
+                }
+                winningSize.put(String.valueOf(count),winningSize.get(String.valueOf(count))+1);
+            }
+        }
+        return winningSize;
+    }
+
+    public int checkWinningNumber(Lotto lotto, List<Integer> winningNumbers){
+        int count = 0;
+        for(int winningNumber : winningNumbers){
+            if(lotto.contains(winningNumber)){
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    public Map<String,Integer> init(){
+        return new HashMap<>(){{
+            put(String.valueOf(3),0);
+            put(String.valueOf(4),0);
+            put(String.valueOf(5),0);
+            put("5bonusNumber",0);
+            put(String.valueOf(6),0);
+        }
+        };
     }
 }
