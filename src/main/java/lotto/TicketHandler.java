a8939714e07bc495cc2db6c888e9b1fda60d9898
DavinAn <blueapple1030@gmail.com>
2022-11-15 23:38:59 +0900

    feat : setup constructor and variables for TicketHandler

--- src/main/java/lotto/TicketHandler.java
+++ src/main/java/lotto/TicketHandler.java
@@ -1,4 +1,16 @@
 package lotto;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class TicketHandler {
+    Map<Winnings, Integer> winningTickets = new LinkedHashMap<>();
+
+    public TicketHandler() {
+        this.winningTickets.put(Winnings.FifthPlace, 0);
+        this.winningTickets.put(Winnings.FourthPlace, 0);
+        this.winningTickets.put(Winnings.ThirdPlace, 0);
+        this.winningTickets.put(Winnings.SecondPlace, 0);
+        this.winningTickets.put(Winnings.FirstPlace, 0);
+    }
 }
