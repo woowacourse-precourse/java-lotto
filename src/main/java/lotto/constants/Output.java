df9e24b08ec07ab3ffe50f126328f20be2b47b13
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-15 21:21:42 +0900

    Refactor(Constants): Refactor Output Enum Class

--- src/main/java/lotto/constants/Output.java
+++ src/main/java/lotto/constants/Output.java
@@ -1,16 +1,16 @@
 package lotto.constants;
 
 public enum Output {
-    PURCHASE_INPUT("구입금액을 입력해 주세요."),
-    PURCHASE("개를 구매했습니다."),
+    PURCHASE_AMOUNT_INPUT("구입금액을 입력해 주세요."),
+    PURCHASE_AMOUNT_OUTPUT("개를 구매했습니다."),
     WINNING_NUMBER_INPUT("당첨 번호를 입력해 주세요."),
     BONUS_NUMBER_INPUT("보너스 번호를 입력해 주세요."),
-    TOTAL_RETURN_START("총 수익률은 "),
-    TOTAL_RETURN_END("입니다.");
+    WINNING_Statistics("당첨 통계\n---"),
+    RATE_OF_RETURN("총 수익률은 %.1f%%입니다.");
 
     private final String msg;
 
-    private Output(String msg) {
+    Output(String msg) {
         this.msg = msg;
     }
 
