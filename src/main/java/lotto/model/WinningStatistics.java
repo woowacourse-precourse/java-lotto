a716a36131097a4ef9c1a2eb2f28b5bb41593993
Jeeseob <jeeseob5761@gmail.com>
2022-11-15 17:41:18 +0900

    feat: 로또 당첨 통계 객체 구현

---
+++ src/main/java/lotto/model/WinningStatistics.java
@@ -0,0 +1,81 @@
+package lotto.model;
+
+import lotto.util.LottoConstant;
+import lotto.util.PrizeMoneyMessage;
+
+import java.util.List;
+
+/**
+ * @Author : Jeeseob
+ * @CreateAt : 2022/11/12
+ */
+public class WinningStatistics {
+    private int firstGrade;
+    private int secondGrade;
+    private int thirdGrade;
+    private int fourthGrade;
+    private int fifthGrade;
+    private String rateOfReturn;   // 수익률
+
+    public WinningStatistics(Lotto winningLotto, List<Lotto> boughtLottos, int bonusNumber) {
+        init();
+        for (Lotto boughtLotto : boughtLottos) {
+            int count = 0;
+            count = getCount(winningLotto, boughtLotto, count);
+            addGrade(bonusNumber, boughtLotto, count);
+            calcRateOfReturn(boughtLottos.size() * 1000);
+        }
+    }
+
+    private void init() {
+        this.firstGrade = 0;
+        this.secondGrade = 0;
+        this.thirdGrade = 0;
+        this.fourthGrade = 0;
+        this.fifthGrade = 0;
+    }
+
+    private int getCount(Lotto winningLotto, Lotto boughtLotto, int count) {
+        for (int number : boughtLotto.getNumbers()) {
+            if (winningLotto.getNumbers().contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void addGrade(int bonusNumber, Lotto boughtLotto, int count) {
+        if (count == LottoConstant.MATCH_NUMBER_FIRST_GRADE.getValue()) {
+            firstGrade++;
+        } else if (count == LottoConstant.MATCH_NUMBER_THIRD_GRADE.getValue() && boughtLotto.getNumbers().contains(bonusNumber)) {
+            secondGrade++;
+        } else if (count == LottoConstant.MATCH_NUMBER_THIRD_GRADE.getValue()) {
+            thirdGrade++;
+        } else if (count == LottoConstant.MATCH_NUMBER_FOURTH_GRADE.getValue()) {
+            fourthGrade++;
+        } else if (count == LottoConstant.MATCH_NUMBER_FIFTH_GRADE.getValue()) {
+            fifthGrade++;
+        }
+    }
+
+    private void calcRateOfReturn(int boughtPrice) {
+        int returnMoney = fifthGrade * LottoConstant.LOTTO_PRIZE_MONEY_FIFTH_GRADE.getValue()
+                + fourthGrade * LottoConstant.LOTTO_PRIZE_MONEY_FOURTH_GRADE.getValue()
+                + thirdGrade * LottoConstant.LOTTO_PRIZE_MONEY_THIRD_GRADE.getValue()
+                + secondGrade * LottoConstant.LOTTO_PRIZE_MONEY_SECOND_GRADE.getValue()
+                + firstGrade * LottoConstant.LOTTO_PRIZE_MONEY_FIRST_GRADE.getValue();
+        this.rateOfReturn = String.format("%.1f", (double) returnMoney / boughtPrice * 100);
+    }
+
+    @Override
+    public String toString() {
+        return "당첨 통계\n" +
+                "---\n" +
+                PrizeMoneyMessage.FIFTH_GRADE.getMessage(fifthGrade) + "\n" +
+                PrizeMoneyMessage.FOURTH_GRADE.getMessage(fourthGrade) + "\n" +
+                PrizeMoneyMessage.THIRD_GRADE.getMessage(thirdGrade) + "\n" +
+                PrizeMoneyMessage.SECOND_GRADE.getMessage(secondGrade) + "\n" +
+                PrizeMoneyMessage.FIRST_GRADE.getMessage(firstGrade) + "\n" +
+                "총 수익률은 " + rateOfReturn +"%입니다.";
+    }
+}
--- src/main/java/lotto/view/LottoGame.java
+++ src/main/java/lotto/view/LottoGame.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import lotto.controller.LottoController;
 import lotto.model.Lotto;
+import lotto.model.WinningStatistics;
 import lotto.service.LottoService;
 import lotto.util.LottoRandom;
 
@@ -20,13 +21,14 @@ public class LottoGame {
     public void start() {
         System.out.println(INPUT_PURCHASE_AMOUNT);
         int purchaseAmount = lottoController.inputPurchaseAmount();     // 구입 금액 입력 받기
-        printLottos(createLottos(purchaseAmount));                      // 구매한 로또 출력
+        List<Lotto> boughtLottos = createLottos(purchaseAmount);                      // 구매한 로또 출력
+        printLottos(boughtLottos);
         System.out.println(purchaseAmount + NUMBER_OF_BOUGHT_LOTTO);    // 구매한 로또 개수 출력
         System.out.println(INPUT_PRIZE_NUMBER);
         Lotto winningLotto = lottoController.inputWinningNumbers();     // 당첨 번호 입력 받기
         System.out.println(INPUT_BONUS_NUMBER);
         int bonusNumber = lottoController.inputBonusNumber(winningLotto); // 보너스 번호 입력 받기
-        // TODO: 당첨 통계 출력
+        printWinningStatistics(winningLotto, boughtLottos, bonusNumber); // 당첨 통계 출력
     }
 
     private List<Lotto> createLottos(int purchaseAmount) {
@@ -35,5 +37,10 @@ public class LottoGame {
 
     private void printLottos(List<Lotto> lottos) {
         lottos.forEach(System.out::println);
+    }
+
+    private void printWinningStatistics(Lotto winningLotto, List<Lotto> boughtLottos, int bonusNumber) {
+        WinningStatistics winningStatistics = new WinningStatistics(winningLotto, boughtLottos, bonusNumber);
+        System.out.println(winningStatistics.toString());
     }
 }
