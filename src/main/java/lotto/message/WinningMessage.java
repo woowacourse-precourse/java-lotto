54acfc2c16646ff977ec71018cc46f454302978d
이원기 <log19951@gmail.com>
2022-11-15 22:58:06 +0900

    feat: 각종 메세지 클래스 구현
    - 에러메세지, 일반메세지, 결과메세지 구현

---
+++ src/main/java/lotto/message/WinningMessage.java
@@ -0,0 +1,11 @@
+package lotto.message;
+
+public class WinningMessage {
+
+    public static final String FIFTH_WINNING = "3개 일치 (5,000원) - ";
+    public static final String FORTH_WINNING = "4개 일치 (50,000원) - ";
+    public static final String THIRD_WINNING = "5개 일치 (1,500,000원) - ";
+    public static final String SECOND_WINNING = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    public static final String FIRST_WINNING = "6개 일치 (2,000,000,000원) - ";
+    public static final String COUNT = "개";
+}
