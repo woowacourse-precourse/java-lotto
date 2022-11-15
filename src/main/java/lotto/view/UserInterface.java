97626712c8f0cfc2d903fe410591f6447486dbb1
CHAYEONIL <jeay123@naver.com>
2022-11-15 16:24:02 +0900

    Reafactor - public 으로 전환, 로또 당첨 기능에 대한 코드 수정

--- src/main/java/lotto/view/UserInterface.java
+++ src/main/java/lotto/view/UserInterface.java
@@ -1,39 +1,38 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.Lotto;
+import lotto.utill.Lotto;
+import lotto.utill.YieldResult;
 
 import java.util.List;
 
 
 public class UserInterface {
-    String purchaseLotto() {
+    public static String purchaseLotto() {
         System.out.println("구입금액을 입력하세요.");
         return Console.readLine();
     }
 
-    void printLottos(List<Lotto> lottos) {
-        System.out.println(lottos.size() + "개를 구매하였습니다.");
+    public static void printLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
         lottos.forEach(System.out::println);
     }
 
-    String inputWinningNumber() {
+    public static String inputWinningNumber() {
         System.out.println("당첨 번호를 입력해주세요.");
         return Console.readLine();
     }
 
-    String inputWinningBonus() {
+    public static String inputWinningBonus() {
         System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
-
-    void printRank() {
+    public static void printWinningResult(YieldResult lottoResult, int cost) {
+        int reward = lottoResult.calcReward();
+        double yield = Math.round((float) reward / cost * 1000) / 10.0;
         System.out.println("당첨 통계");
         System.out.println("---");
+        System.out.println(lottoResult.getResult());
+        System.out.println("총 수익률은 "+ yield + "%입니다.");
     }
-
-    void printEarningRate() {
-        System.out.println("총 수익률은 %.3f 입니다.");
-    }
-
 }
