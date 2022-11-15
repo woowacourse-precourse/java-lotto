5135eb2f3fac696e30560099ef47dd79574ae9e2
inhwanK <inhan1009@naver.com>
2022-11-15 20:31:17 +0900

    refactor(Application): main 내부 로직 분리
    purchaseLottos 메서드 구현
    메서드 위치 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,33 +8,25 @@ import java.util.List;
 public class Application {
 
     private static final LottoStore lottoStore = new LottoStore();
+    private static List<Lotto> lottos = new ArrayList();
 
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해주세요.";
-    private static final String AMOUNT_OUTPUT_MESSAGE = "%d개를 구매했습니다.\n";
+    private static final String NUMBER_OF_LOTTOS_MESSAGE = "%d개를 구매했습니다.\n";
     private static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요";
     private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요";
     private static final String PROFIT_RATE_MESSAGE = "총 수익률은 %.2f%%입니다.\n";
-
 
     public static void main(String[] args) {
         try {
             Integer amount = getAmountInput(PURCHASE_AMOUNT_INPUT_MESSAGE);
 
-            Integer numOfLotto = lottoStore.calcLottoNumbers(amount);
-            // 출력
-            System.out.printf(AMOUNT_OUTPUT_MESSAGE, numOfLotto);
+            Integer numOfLotto = getNumberOfLotto(amount);
 
-            List<Lotto> lottos = lottoStore.issueLottos(numOfLotto);
-            // 출력
-            for (Lotto lotto : lottos) {
-                System.out.println(lotto);
-            }
+            List<Lotto> purchaseLottos = purchaseLottos(numOfLotto);
 
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
-
-        // TODO: 프로그램 구현
     }
 
 
@@ -57,12 +49,21 @@ public class Application {
         String amountInput = Console.readLine();
 
         Integer amount = validateAmountInput(amountInput);
-
+        System.out.println();
         return amount;
     }
 
-    private static void printNotifyMessage(String message) {
-        System.out.println(message);
+    private static Integer getNumberOfLotto(Integer amount) {
+        Integer numOfLotto = lottoStore.calcLottoNumbers(amount);
+        printNotifyMessage(NUMBER_OF_LOTTOS_MESSAGE, numOfLotto);
+
+        return numOfLotto;
+    }
+
+    private static List<Lotto> purchaseLottos(Integer numOfLotto) {
+        List<Lotto> lottos = lottoStore.issueLottos(numOfLotto);
+        printLottos(lottos);
+        return lottos;
     }
 
     private static Integer validateAmountInput(String amountInput) throws IllegalArgumentException {
@@ -75,6 +76,21 @@ public class Application {
 
             throw new IllegalArgumentException("[ERROR]");
         }
-
     }
+
+    private static void printNotifyMessage(String message) {
+        System.out.println(message);
+    }
+
+    private static void printNotifyMessage(String message, Integer numOfLotto) {
+        System.out.printf(message, numOfLotto);
+    }
+
+    private static void printLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
+
+
 }
