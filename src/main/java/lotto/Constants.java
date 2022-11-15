355ce8ab361e140c04630279a376df650b798e7e
taeyun-work <wmfajr1357@gmail.com>
2022-11-15 20:49:42 +0900

    fix: 입력받는 payment에 대한 정규식 예외 수정

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -26,7 +26,7 @@ public final class Constants {
         public static final String WINNING_NUMBERS = "^([1-9]{1},{1}|[1-3]{1}[0-9]{1},{1}|4{1}[0-5]{1},{1}){5}"
                 + "([1-9]{1}|[1-3]{1}[0-9]{1}|4{1}[0-5]{1})$";
         public static final String BONUS_NUMBER = "^[1-9]{1}|[1-3]{1}[0-9]{1}|4{1}[0-5]{1}$";
-        public static final String PAYMENT = "^[1-9]*000$";
+        public static final String PAYMENT = "^[1-9]{1}[0-9]*000$";
 
         private REGEX() {
         }
