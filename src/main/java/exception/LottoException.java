15725a6f355c3213a5d652948c8a836e16a6652e
seoeunbae <enyoupark@gmail.com>
2022-11-15 10:39:25 +0900

    feat : 예외 처리 필요 요소들 로직 구현 완료

---
+++ src/main/java/exception/LottoException.java
@@ -0,0 +1,41 @@
+package exception;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LottoException extends IllegalStateException {
+    LottoException(String message){
+        super(message);
+    }
+
+    public void IllegalNumberRangeException(List<Integer> numbers){
+        for(Integer number : numbers){
+            if(number < 1 || number > 45) throw new LottoException("[ERROR] 로또 숫자의 범위는 1에서 45사이만 유효합니다.");
+        }
+    }
+
+    public void IllegalNumberBlankException(List<Integer> numbers){
+        for(Integer number : numbers){
+            if(number == null) throw new LottoException("[ERROR] 로또 숫자는 널이 될 수 없습니다.");
+        }
+    }
+
+    public void IllegalNumberDuplicationException(List<Integer> numbers){
+        for(Integer number : numbers){
+            if(numbers.indexOf(number) != numbers.lastIndexOf(number)){
+                throw new LottoException("[ERROR] 로또 숫자는 중복될 수 없습니다.");
+            }
+        }
+    }
+
+    public void IllegalNumberSizeException(List<Integer> numbers){
+        if( numbers.size() != 6 ) throw new LottoException("[ERROR] 숫자의 개수를 6개민 유효합니다.");
+    }
+
+    public void IllegalMoneyException(int money){
+        if( money % 1000 != 0 ) throw new LottoException("[ERROR] 사용자 투자 금액은 1000으로 나누어떨어져야 유효합니다.");
+    }
+
+
+}
---
+++ src/main/java/lotto/Validator.java
@@ -0,0 +1,5 @@
+package lotto;
+
+//public class Validator extends IllegalStateException{
+//    void throwNumRangeException();
+//}
