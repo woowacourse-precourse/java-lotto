68e93ef6014eff49ff374e71f14c85e2689cb981
kswhj <108874006+kswhj@users.noreply.github.com>
2022-11-15 18:00:50 +0900

    Update WinList.java

--- src/main/java/lotto/confirm/WinList.java
+++ src/main/java/lotto/confirm/WinList.java
@@ -1,15 +1,13 @@
 package confirm;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class WinList {
 	public static List<Integer> run(List<List<Integer>> playerPick, List<Integer> winNums) {
-		List<Integer> answer = new ArrayList<>();
 		for (int i = 0; i < playerPick.size(); i++) {
 			int num = WinNumCheck.run(playerPick.get(i), winNums);
-			answer.set(i, num);
+			winNums.set(i, num);
 		}
-		return answer;
+		return winNums;
 	}
 }
