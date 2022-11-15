ab7eee59b1e09d2ff28ee76416f791038c807dc0
Ye Seok Hong <yshong1998@gmail.com>
2022-11-15 23:11:13 +0900

    feat : Game 클래스 완성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,17 +6,7 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         try {
-            LottoMachine lottoMachine = new LottoMachine();
-            StatisticManager statisticManager = new StatisticManager();
-            InputManager inputManager = new InputManager();
-            int playerMoney = inputManager.inputMoney();
-            int buyNumber = lottoMachine.calculateBuyNumber(playerMoney);
-            lottoMachine.buyLotto(buyNumber);
-            List<Integer> winningNumbers = statisticManager.stringListToIntegerList(inputManager.inputWinningNumbers());
-            int bonusNumber = statisticManager.stringToInt(inputManager.inputBonusNumber(winningNumbers));
-            statisticManager.calculateFinalRank(buyNumber,winningNumbers,bonusNumber);
-            statisticManager.calculateYield(playerMoney);
-            statisticManager.viewResult();
+            Game.playGame();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
