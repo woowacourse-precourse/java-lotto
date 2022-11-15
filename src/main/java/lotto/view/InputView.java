0dc7d36f005e751b9ad2e51e236da8a4704cc7cf
kimsh2948 <shkim2948@gmail.com>
2022-11-15 20:28:33 +0900

    refactor: 상수 이름 컨벤션에 맞게 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,22 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String inputMoneyMessage = "구입금액을 입력해 주세요.";
-    private static final String inputWinNumberMessage = "당첨 번호를 입력해 주세요.";
-    private static final String inputBonusNumberMessage = "보너스 번호를 입력해 주세요.";
+    private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WIN_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static String inputMoney() {
-        System.out.println(inputMoneyMessage);
+        System.out.println(INPUT_MONEY_MESSAGE);
         return Console.readLine();
     }
 
     public static String inputWinNumber() {
-        System.out.println(inputWinNumberMessage);
+        System.out.println(INPUT_WIN_NUMBER_MESSAGE);
         return Console.readLine();
     }
 
     public static String inputBonusNumber() {
-        System.out.println(inputBonusNumberMessage);
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,16 +8,16 @@ import java.util.Map;
 
 public class OutputView {
 
-    private static final String buyLottoCountMessage = "개를 구매했습니다.";
-    private static final String winStatisticsMessage = "당첨 통계";
-    private static final String threeSameNumberMessage = "3개 일치 (5,000원) - ";
-    private static final String fourSameNumberMessage = "4개 일치 (50,000원) - ";
-    private static final String fiveSameNumberMessage = "5개 일치 (1,500,000원) - ";
-    private static final String fiveAndBonusSameNumberMessage = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
-    private static final String sixSameNumberMessage = "6개 일치 (2,000,000,000원) - ";
+    private static final String BUY_LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
+    private static final String WIN_STATISTICS_MESSAGE = "당첨 통계";
+    private static final String THREE_SAME_NUMBER_MESSAGE = "3개 일치 (5,000원) - ";
+    private static final String FOUR_SAME_NUMBER_MESSAGE = "4개 일치 (50,000원) - ";
+    private static final String FIVE_SAME_NUMBER_MESSAGE = "5개 일치 (1,500,000원) - ";
+    private static final String FIVE_AND_BONUS_SAME_NUMBER_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    private static final String SIX_SAME_NUMBER_MESSAGE = "6개 일치 (2,000,000,000원) - ";
 
     public static void buyLottoCount(int count) {
-        System.out.println(count + buyLottoCountMessage);
+        System.out.println(count + BUY_LOTTO_COUNT_MESSAGE);
     }
 
     public static void showLottoTicket(LottoTicket lottoTicket) {
@@ -27,13 +27,13 @@ public class OutputView {
     }
 
     public static void winStatistics(Map<LottoRank, Integer> winStat) {
-        System.out.println(winStatisticsMessage);
+        System.out.println(WIN_STATISTICS_MESSAGE);
         System.out.println("---");
-        System.out.println(threeSameNumberMessage + winStat.get(LottoRank.FIFTH) + "개");
-        System.out.println(fourSameNumberMessage + winStat.get(LottoRank.FORTH) + "개");
-        System.out.println(fiveSameNumberMessage + winStat.get(LottoRank.THIRD) + "개");
-        System.out.println(fiveAndBonusSameNumberMessage + winStat.get(LottoRank.SECOND) + "개");
-        System.out.println(sixSameNumberMessage + winStat.get(LottoRank.FIRST) + "개");
+        System.out.println(THREE_SAME_NUMBER_MESSAGE + winStat.get(LottoRank.FIFTH) + "개");
+        System.out.println(FOUR_SAME_NUMBER_MESSAGE + winStat.get(LottoRank.FORTH) + "개");
+        System.out.println(FIVE_SAME_NUMBER_MESSAGE + winStat.get(LottoRank.THIRD) + "개");
+        System.out.println(FIVE_AND_BONUS_SAME_NUMBER_MESSAGE + winStat.get(LottoRank.SECOND) + "개");
+        System.out.println(SIX_SAME_NUMBER_MESSAGE + winStat.get(LottoRank.FIRST) + "개");
     }
 
     public static void showRating(double rating) {
