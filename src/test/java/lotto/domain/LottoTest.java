74842df045789abf725f75f53bfe434086c9a579
Haaae <101785628+Haaae@users.noreply.github.com>
2022-11-15 23:29:08 +0900

    feat(LottoTest): 발행한 복권 하나의 당첨 숫자 개수를 세는 기능 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -34,5 +36,15 @@ class LottoTest {
     void createLottoByUnderRangeNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호 숫자가 당첨 번호와 몇 개 같은지 센다.")
+    @Test
+    void countMatchingNumberTest() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winNumber = new Lotto(List.of(1, 2, 3, 7, 8, 9));
+        int expected = 3;
+        int answer = (int) lotto.countMatchingNumber(winNumber);
+        assertEquals(expected, answer);
     }
 }
