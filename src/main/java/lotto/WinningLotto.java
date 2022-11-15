9ee5aa7a036619b2d909c143fa594631a9fd6beb
Seung-Been Kim <mainlib990@gmail.com mainlib990s-MacBookPro.local>
2022-11-15 17:31:46 +0900

    feat(winningLotto): 사용자로부터 입력받은 번호를 생성하는 기능을 구현하세요...
    정규표현식을 이용하여 데이터 검증을 하게 설계하였지만,
    이는 오히려 더 복잡하게 만들었으며 어떤 버그가 존재할지 아직 모른다.
    
    나는 설계를 바꾸고 싶지만 이미 너무 많은 시간이 흘러버렸다.

---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,52 @@
+package lotto;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class WinningLotto {
+    private static final String AVAILABLE_NUMBERS = "(([1-9])|([1-3][0-9])|(4[0-5]))";
+    private static final String REGEX_LOTTO_TO_VALID
+            = "^"
+            + "(?<n1>" + AVAILABLE_NUMBERS + ")" + ","
+            + "(?!(\\k<n1>),)" + "(?<n2>" + AVAILABLE_NUMBERS + ")" + ","
+            + "(?!((\\k<n1>)|(\\k<n2>)),)" + "(?<n3>" + AVAILABLE_NUMBERS + ")" + ","
+            + "(?!((\\k<n1>)|(\\k<n2>)|(\\k<n3>)),)" + "(?<n4>" + AVAILABLE_NUMBERS + ")" + ","
+            + "(?!((\\k<n1>)|(\\k<n2>)|(\\k<n3>)|(\\k<n4>)),)" + "(?<n5>" + AVAILABLE_NUMBERS + ")" + ","
+            + "(?!((\\k<n1>)|(\\k<n2>)|(\\k<n3>)|(\\k<n4>)|(\\k<n5>)),)" + "(?<n6>" + AVAILABLE_NUMBERS + ")" + ","
+            + "(?!((\\k<n1>)|(\\k<n2>)|(\\k<n3>)|(\\k<n4>)|(\\k<n5>)|(\\k<n6>)))" + "(?<n7>" + AVAILABLE_NUMBERS + ")"
+            + "$";
+
+    private static final Pattern patternLotto = Pattern.compile(REGEX_LOTTO_TO_VALID);
+
+    private final List<Integer> primaryNumbers;
+    private final Integer bonusNumber;
+
+    public WinningLotto(String numbers) {
+        Matcher matcher = patternLotto.matcher(numbers);
+        if (!matcher.find()) {
+            Logger.error("올바르지 않은 값을 입력 하였습니다: " + numbers);
+            throw new IllegalArgumentException();
+        }
+        primaryNumbers = primaryNumbersOf(matcher);
+        bonusNumber = bonusNumberOf(matcher);
+    }
+
+    private List<Integer> primaryNumbersOf(Matcher matcher) {
+        return Stream.of(matcher.group("n1"),
+                        matcher.group("n2"),
+                        matcher.group("n3"),
+                        matcher.group("n4"),
+                        matcher.group("n5"),
+                        matcher.group("n6"))
+                .map(Integer::parseInt)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    private Integer bonusNumberOf(Matcher matcher) {
+        return Integer.parseInt(matcher.group("n7"));
+    }
+}
