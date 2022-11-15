bdaacab7c3f31632ecb0f8c428261872196dc3d3
Vector1331 <89854207+Vector1331@users.noreply.github.com>
2022-11-15 16:14:31 +0900

    [Feat] 로또 게임 시작 - player 구매 금액 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.view.View;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            lottoGameStart();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    static void lottoGameStart() {
+        View view = new View();
+        view.printSpentMoneyMenu();
+        String playerNum = Console.readLine();
     }
 }
---
+++ src/main/java/lotto/view/View.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class View {
+    private static final String howMuchLottoMessage = "구입금액을 입력해 주세요.";
+
+    public void printSpentMoneyMenu() {
+        print(howMuchLottoMessage);
+    }
+}
