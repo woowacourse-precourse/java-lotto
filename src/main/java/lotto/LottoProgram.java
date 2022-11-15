bed10602dccee58e7ddf0dcbddcfb259edee8d4a
Miseong Kim <miseongk@naver.com>
2022-11-15 18:01:42 +0900

    fix(lotto): change match count logic to make ranking and change methods name

--- src/main/java/lotto/LottoProgram.java
+++ src/main/java/lotto/LottoProgram.java
@@ -1,7 +1,7 @@
 package lotto;
 
 import lotto.domain.Lotto;
-import lotto.domain.Match;
+import lotto.domain.Rank;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -17,6 +17,7 @@ public class LottoProgram {
     private List<Lotto> lottos = new ArrayList<>();
     private Lotto winningLotto;
     private int bonusNumber;
+    private double rateOfReturn;
 
     public void progress() {
         buyLotto();
@@ -59,7 +60,7 @@ public class LottoProgram {
     private void makeWinningLotto(String numbers) {
         String[] lottoNumbers = numbers.split(",");
         List<Integer> winningNumbers = new ArrayList<>();
-        for (String s: lottoNumbers) {
+        for (String s : lottoNumbers) {
             winningNumbers.add(Integer.parseInt(s));
         }
         this.winningLotto = new Lotto(winningNumbers);
@@ -70,55 +71,54 @@ public class LottoProgram {
     }
 
     public void getResult() {
-        Map<String, Integer> matchCount = matchLotto();
+        Map<String, Integer> ranking = rank();
         OutputView.printStartResultMessage();
-        OutputView.printResult(matchCount);
+        OutputView.printResult(ranking);
+        OutputView.printRateOfReturn(this.rateOfReturn);
     }
 
-    public Map<String, Integer> matchLotto() {
-        Map<String, Integer> matchCount = makeDefaultCount(new HashMap<String, Integer>());
+    public Map<String, Integer> rank() {
+        Map<String, Integer> ranking = makeDefaultCount(new HashMap<>());
 
-        for (Lotto lotto: this.lottos) {
-            List<Integer> result = matchEachLotto(lotto);
-            int count = result.get(0);
-            int bonusCount = result.get(1);
+        for (Lotto lotto : this.lottos) {
+            int[] result = rankEachLotto(lotto);
+            int count = result[0];
+            int bonusCount = result[1];
 
-            matchCount = makeMatchCount(matchCount, count, bonusCount);
+            ranking = makeRanking(ranking, count, bonusCount);
+        }
+        //calculateRateOfReturn(ranking);
+
+        return ranking;
+    }
+
+    private Map<String, Integer> makeDefaultCount(Map<String, Integer> ranking) {
+        for (Rank rank : Rank.values()) {
+            ranking.put(rank.name(), 0);
         }
 
-        return matchCount;
+        return ranking;
     }
 
-    private Map<String, Integer> makeDefaultCount(Map<String, Integer> matchCount) {
-        for (Match match: Match.values()) {
-            if (match.getBonus()) {
-                matchCount.put(String.valueOf(match.getMatchCount()) + "+", 0);
-                continue;
+    private Map<String, Integer> makeRanking(Map<String, Integer> ranking, int count, int bonusCount) {
+        for (Rank rank: Rank.values()) {
+            if (count == 5 && bonusCount == 1 && rank.getMatchCount() == count) {
+                ranking.put(rank.name(), ranking.get(rank.name()) + 1);
+                break;
             }
-            matchCount.put(String.valueOf(match.getMatchCount()), 0);
+            if (count == rank.getMatchCount()) {
+                ranking.put(rank.name(), ranking.get(rank.name()) + 1);
+            }
         }
 
-        return matchCount;
+        return ranking;
     }
 
-    private Map<String, Integer> makeMatchCount(Map<String, Integer> matchCount, int count, int bonusCount) {
-        if (count == 5 && bonusCount == 1) {
-            matchCount.put("5+", matchCount.get("5+") + 1);
-
-            return matchCount;
-        }
-        if (count >= 3) {
-            matchCount.put(String.valueOf(count), matchCount.get(String.valueOf(count)) + 1);
-        }
-
-        return matchCount;
-    }
-
-    private List<Integer> matchEachLotto(Lotto lotto) {
+    private int[] rankEachLotto(Lotto lotto) {
         int count = 0;
         int bonusCount = 0;
 
-        for (Integer number: lotto.getNumbers()) {
+        for (Integer number : lotto.getNumbers()) {
             if (this.winningLotto.getNumbers().contains(number)) {
                 count += 1;
             }
@@ -127,6 +127,6 @@ public class LottoProgram {
             }
         }
 
-        return List.of(count, bonusCount);
+        return new int[]{count, bonusCount};
     }
 }
