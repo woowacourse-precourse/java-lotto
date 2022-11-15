829d4961fe04c126ceb8ce282adbf1a8c54ac110
kmw2378 <rlarla677@gmail.com>
2022-11-15 21:03:07 +0900

    [refactor] Application - 패키지 이동으로 인한 생성

---
+++ src/main/java/Application.java
@@ -0,0 +1,25 @@
+import controller.LottoController;
+import domain.lotto.LottoRepository;
+import service.LottoService;
+import controller.UserController;
+
+public class Application {
+    public static void main(String[] args) {
+
+        try {
+            UserController userController = new UserController();
+            userController.saveAmount();
+
+            LottoRepository lottoRepository = new LottoRepository();
+            LottoController lottoController = new LottoController(lottoRepository);
+            lottoController.createIssuanceLottoNumbers();
+
+            userController.saveNumber();
+
+            LottoService lottoService = new LottoService(lottoController);
+            lottoService.game();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
