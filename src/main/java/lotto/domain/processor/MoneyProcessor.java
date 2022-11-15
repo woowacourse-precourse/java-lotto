95bd56d2e5bb03e8c8ebc118563601d1e42ed717
5witchkr <hob4419@gmail.com>
2022-11-15 13:43:44 +0900

    docs(lotto): 가독성을 위한 javadoc 작성

--- src/main/java/lotto/domain/processor/MoneyProcessor.java
+++ src/main/java/lotto/domain/processor/MoneyProcessor.java
@@ -6,9 +6,26 @@ import java.util.List;
 
 public interface MoneyProcessor {
 
+    /**
+     * 마진을 계산해주는 로직
+     * @param beforeMoney - 원금
+     * @param afterMoney - 수익
+     * @return 마진률
+     */
     String calculateMargin(double beforeMoney, double afterMoney);
 
+    /**
+     * 현재 금액으로 몇개의 로또를 살 수 있는지 확인하는 로직
+     * @param money - 현재 금액
+     * @return 살 수 있는 로또의 수
+     */
     Integer calculateLottoCount(Integer money);
 
+    /**
+     * 당첨금을 계산하는 로직
+     * @param result - 당첨 카운트가 들어있는 리스트
+     * @param rankEnum - 당첨등수
+     * @return 리스트에 당첨등수가 몇개있는지 확인 후 해당 당첨등수의 금액을 리턴한다.
+     */
     Integer calculateWinning(List<Integer> result, RankEnum rankEnum);
 }
--- src/main/java/lotto/domain/processor/ValidatorProcessor.java
+++ src/main/java/lotto/domain/processor/ValidatorProcessor.java
@@ -7,11 +7,31 @@ import java.util.List;
 
 public interface ValidatorProcessor {
 
+    /**
+     * 입력금액과 현재 로또가격을 입력받아서 유효성을 검증한다.
+     * @param money - 입력금액
+     * @param priceEnum - 현재 로또가격이 담긴 enum
+     */
     void validateMoney(Integer money, PriceEnum priceEnum);
 
+    /**
+     * 입력값이 숫자인지 확인하고 반환한다.
+     * @param input - 입력 String
+     * @return - 입력값의 숫자
+     */
     List<Integer> validateAndParseLottoNumberInput(String input);
 
+    /**
+     * 로또 배열의 사이즈를 검증한다.
+     * @param size - 검증대상 사이즈
+     * @param lottoEnum - 기준 사이즈가 담긴 enum
+     */
     void validateLottoSize(Integer size, LottoEnum lottoEnum);
 
+    /**
+     * 로또 숫자의 범위가 유요한지 확인한다.
+     * @param number - 로또배열
+     * @param lottoEnum - 범위가 담긴 enum
+     */
     void validateLottoNumber(List<Integer> number, LottoEnum lottoEnum);
 }
