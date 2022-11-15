3c0db58e1b62040ef4aab00915faf4640b207cde
Park Sungchul <try615@naver.com>
2022-11-15 19:58:00 +0900

    refactor(LottoStore.java): 여러 장 로또 발행 리팩토링
    for문 만을 별개로 수행하는 메소드로 분리

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -1,20 +1,41 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class LottoStore {
 
 	private int moneyBuyLotto;
+	private List<Lotto> lottos;
 
 	public LottoStore() {
 		moneyBuyLotto = 0;
+		lottos = new ArrayList<>();
 	}
 
 	public void open() {
 		buy();
+		publish();
 	}
 
 	private void buy() {
 		ConsoleOut.printInputMoney();
 		moneyBuyLotto = Util.strToInt(ConsoleIn.readLine());
 		Verify.verifyThousandUnit(moneyBuyLotto);
+	}
+
+	private void publish() {
+		int numLotto = moneyBuyLotto / Constants.MINIMUM_MONEY_UNIT;
+		severalLottoPublish(numLotto);
+	}
+
+	private void severalLottoPublish(int numLotto) {
+		for (int num = 1; num <= numLotto; num++) {
+			Lotto lotto = new Lotto(
+				Util.pickUniqueNumbersInRange(Constants.LOTTO_UNDER_BOUNDARY, Constants.LOTTO_UPPER_BOUNDARY,
+					Constants.LOTTO_LENGTH));
+			lottos.add(lotto);
+		}
 	}
 }
