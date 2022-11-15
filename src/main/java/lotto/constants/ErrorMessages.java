e75ff208d4520d682471c2051cffc67ecb6ea6b9
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-15 16:20:52 +0900

    refactor(lotto) : 에러 메시지 수정

--- src/main/java/lotto/constants/ErrorMessages.java
+++ src/main/java/lotto/constants/ErrorMessages.java
@@ -6,5 +6,5 @@ public class ErrorMessages {
     public static final String DUPLICATE = "[ERROR] 로또 번호에 중복이 존재합니다.";
     public static final String SIZE = "[ERROR] 로또 번호의 개수가 6개가 아닙니다.";
     public static final String BONUS_DUPLICATE = "[ERROR] 보너스 번호가 중복입니다.";
-    public static final String INPUT_NUMBER_ERROR = "[ERROR] 숫자만 입력 가능합니다. 다시 입력하세요.";
+    public static final String INPUT_NUMBER_ERROR = "[ERROR] 숫자만 입력 가능합니다.";
 }
