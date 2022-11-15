a1f38785b3d70b0e9b01fd92b41ef72f4c0b67c5
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-15 23:32:38 +0900

    fix : 메서드 수정을 위해 삭제처리 하였다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -13,21 +13,11 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6 || duplicate(numbers) ) {
+        if (numbers.size() != 6 ) {
             throw new IllegalArgumentException("[ERROR] 로또 번호 오류");
         }
     }
 
-    private boolean duplicate(List<Integer> numbers){
-        boolean duplicate = false;
-        for(int i = 0; i < numbers.size(); i++){
-            for(int j = i; j< numbers.size(); j++){
-                if(numbers.get(i) == numbers.get(j)){
-                    duplicate = true;
-                }
-            }
-        }return duplicate;
-    }
 
     // TODO: 추가 기능 구현
 }
