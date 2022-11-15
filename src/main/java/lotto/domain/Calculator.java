2e1f211014f481fbe44aed79760183cd9c182000
Sinryuji <siler2474@gmail.com>
2022-11-15 15:58:10 +0900

    refactor(Calculator, Rank): 보너스 포함 여부 int -> boolean으로 변경

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -5,9 +5,8 @@ import java.util.List;
 public class Calculator {
     public static void calRank (List<Integer> result, List<Lotto> lottos, List<Integer> numbers, int bonus) {
         for (Lotto lotto : lottos) {
-            int matchBonus = lotto.getNumbers().contains(bonus) ? 1 : 0;
             lotto.getNumbers().retainAll(numbers);
-            Rank rank = Rank.findRank(lotto.getNumbers().size(), matchBonus);
+            Rank rank = Rank.findRank(lotto.getNumbers().size(), lotto.getNumbers().contains(bonus));
             if (rank != null)
                 result.set(rank.getIndex(), result.get(rank.getIndex()) + 1);
         }
--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,22 +3,22 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(0, 6, 0, 2_000_000_000, "6개 일치 (2,000,000,000원) - "),
-    SECOND(1, 5, 1, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    THIRD(2, 5, 0, 1_500_000, "5개 일치 (1,500,000원) - "),
-    FOURTH(3, 4, 0, 50_000, "4개 일치 (50,000원) - "),
-    FIFTH(4,3, 0, 5_000, "3개 일치 (5,000원) - ")
+    FIRST(0, 6, false, 2_000_000_000, "6개 일치 (2,000,000,000원) - "),
+    SECOND(1, 5, true, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(2, 5, false, 1_500_000, "5개 일치 (1,500,000원) - "),
+    FOURTH(3, 4, true, 50_000, "4개 일치 (50,000원) - "),
+    FIFTH(4,3, false, 5_000, "3개 일치 (5,000원) - ")
     ;
 
     private final int index;
     private final int matchNumbers;
-    private final int matchBonus;
+    private final boolean matchBonus;
     private final int winAmount;
     private final String message;
 
     public static final int length = 5;
 
-    Rank(int index, int matchNumbers, int matchBonus, int winAmount, String message) {
+    Rank(int index, int matchNumbers, boolean matchBonus, int winAmount, String message) {
         this.index = index;
         this.matchNumbers = matchNumbers;
         this.matchBonus = matchBonus;
@@ -45,10 +45,10 @@ public enum Rank {
         return this.message;
     }
 
-    public static Rank findRank(int numbersCount, int bonusCount) {
+    public static Rank findRank(int numbersCount, boolean bonusContain) {
         return Arrays.stream(values())
                 .filter(rank -> rank.matchNumbers == numbersCount)
-                .filter(rank -> rank.matchBonus == bonusCount)
+                .filter(rank -> rank.matchBonus == bonusContain)
                 .findAny()
                 .orElse(null);
     }
