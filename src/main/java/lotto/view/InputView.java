b51044bfbee36ef9445c9d987c5e145cb7501920
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-15 15:27:40 +0900

    feat(InputView): 구입 금액 입력 메시지 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,14 +4,19 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public double inputPayment() {
+        printPaymentMessage();
         String payment = Console.readLine();
         validateNumeric(payment);
         return Double.parseDouble(payment);
     }
 
-    private static void validateNumeric(String input) {
+    public static void validateNumeric(String input) {
         if (!input.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException("[ERROR] 입력값은 숫자만 가능합니다.");
         }
+    }
+
+    private void printPaymentMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
     }
 }
