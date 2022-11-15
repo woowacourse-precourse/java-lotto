71fb2a7fb321cf9da39e4aa3635ece94dbbad6b7
mihye126 <mihea12@gmail.com>
2022-11-15 23:02:31 +0900

    [feat] ApplicationTest 테스트 케이스 추가

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -53,6 +54,114 @@ class ApplicationTest extends NsTest {
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
+
+    @DisplayName("2등 보너스번호 일치 확인")
+    @Test
+    void secondPrize() {
+        assertRandomUniqueNumbersInRangeTest(
+            () -> {
+                run("7000", "1,3,11,21,31,41", "2");
+                assertThat(output()).contains(
+                    "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개"
+                );
+            },
+
+            List.of( 1,2,11,21,31,41),
+            List.of(2,3,12,22,32,42),
+            List.of(3,4,13,23,33,43),
+            List.of(8,40,41,43,44,45),
+            List.of(1,2,3,4,5,6),
+            List.of(1,2,3,4,5,6),
+            List.of(1,2,3,4,5,6)
+
+        );
+    }
+
+    @DisplayName("3등 보너스번호 불일치 확인")
+    @Test
+    void thirdPrize() {
+        assertRandomUniqueNumbersInRangeTest(
+            () -> {
+                run("7000", "1,3,11,21,31,41", "2");
+                assertThat(output()).contains(
+                    "5개 일치 (1,500,000원) - 1개"
+                );
+            },
+
+            List.of( 1,7,11,21,31,41),
+            List.of(2,3,12,22,32,42),
+            List.of(3,4,13,23,33,43),
+            List.of(8,40,41,43,44,45),
+            List.of(1,2,3,4,5,6),
+            List.of(1,2,3,4,5,6),
+            List.of(1,2,3,4,5,6)
+
+        );
+    }
+
+    @DisplayName("수익률확인")
+    @Test
+    void checkEarningRate() {
+        assertRandomUniqueNumbersInRangeTest(
+            () -> {
+                run("3000", "1,2,3,4,5,6", "7");
+                assertThat(output()).contains(
+                    "총 수익률은 67716666.7%입니다."
+                );
+            },
+
+            List.of( 1, 2, 3, 4, 5, 6),
+            List.of(1, 2, 3, 4, 5, 7),
+            List.of(1, 2, 3, 4, 5, 8)
+
+        );
+    }
+    @DisplayName("unsigned test-매우 큰 상금 테스트")
+    @Test
+    void hugePrizeTest() {
+        assertRandomUniqueNumbersInRangeTest(
+            () -> {
+                run("3000", "1,2,3,4,5,6", "7");
+                assertThat(output()).contains(
+                    "3개를 구매했습니다.",
+                    "[1, 2, 3, 4, 5, 6]",
+                    "[1, 2, 3, 4, 5, 6]",
+                    "[1, 2, 3, 4, 5, 7]",
+                    "3개 일치 (5,000원) - 0개",
+                    "4개 일치 (50,000원) - 0개",
+                    "5개 일치 (1,500,000원) - 0개",
+                    "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                    "6개 일치 (2,000,000,000원) - 2개",
+                    "총 수익률은 134333333.3%입니다."
+                );
+            },
+            List.of(1, 2, 3, 4, 5, 6),
+            List.of(1, 2, 3, 4, 5, 6),
+            List.of(1, 2, 3, 4, 5, 7)
+        );
+    }
+
+    @DisplayName("상금이 없는 경우 테스트")
+    @Test
+    void zeroPrizeTest() {
+        assertRandomUniqueNumbersInRangeTest(
+            () -> {
+                run("1000", "1,2,3,4,5,6", "7");
+                assertThat(output()).contains(
+                    "1개를 구매했습니다.",
+                    "[8, 9, 10, 14, 15, 16]",
+                    "3개 일치 (5,000원) - 0개",
+                    "4개 일치 (50,000원) - 0개",
+                    "5개 일치 (1,500,000원) - 0개",
+                    "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                    "6개 일치 (2,000,000,000원) - 0개",
+                    "총 수익률은 0.0%입니다."
+                );
+            },
+            List.of(8, 9, 10, 14, 15, 16)
+        );
+    }
+
 
     @Override
     public void runMain() {
