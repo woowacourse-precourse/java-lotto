647bd08b1c4eb03e7fefa75e22e49ab6380445cc
GyuJeGal <lkjdee0329@gmail.com>
2022-11-15 18:09:53 +0900

    feat(Lotto): 정수 리스트가 6개의 원소를 가지는지 확인하는 기능 구현 사이즈가 6이 아닐 경우 IllegalArgumentException 던짐

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import static lotto.constant.ErrorMessage.LOTTO_ELEMENTS_DUPLICATE_ERROR;
+import static lotto.constant.ErrorMessage.LOTTO_ELEMENTS_RANGE_ERROR;
+import static lotto.constant.ErrorMessage.LOTTO_ELEMENTS_SIZE_ERROR;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+
+    }
+
+    private void checkListSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(LOTTO_ELEMENTS_SIZE_ERROR);
+        }
+    }
+
+    public void printNumbers() {
+        System.out.println(this.numbers.toString());
+    }
+
+    public int getNumber(int index) {
+        return numbers.get(index);
+    }
+
+}
