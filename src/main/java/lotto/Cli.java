08d172aa6f090c5d8ec93ebf435b5a900a59c18f
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-15 17:07:29 +0900

    refactor: Change purchase lotto function to get purchase amount

--- src/main/java/lotto/Cli.java
+++ src/main/java/lotto/Cli.java
@@ -10,17 +10,14 @@ public class Cli {
 
   /**
    * Function that receives user input to purchase lotto.
-   * @return count of purchase.
+   * @return amount of purchase.
    */
-  public static int purchaseLotto() {
+  public static int getPurchaseAmount() {
     System.out.println("구입금액을 입력해 주세요.");
     String amount = readLine();
 
     validate(amount);
-    int purchaseCount = Integer.parseInt(amount) / 1000;
-
-    System.out.printf("%d개를 구입했습니다.", purchaseCount);
-    return purchaseCount;
+    return Integer.parseInt(amount);
   }
 
   private static void validate(String amount) {
