6b43c6f51587479d40fce9277e2f37aa179272b8
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-11-15 19:58:12 +0900

    fix: 중복된 번호 체크 함수 추가 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,12 +7,17 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
+        validate(numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+
+        boolean hasDuplicatedNum = checkDuplicate(numbers);
+        if (hasDuplicatedNum) {
             throw new IllegalArgumentException();
         }
     }
@@ -21,15 +26,31 @@ public class Lotto {
         return numbers;
     }
 
-    public int matchNumber(int pickedNumber){
+    public int matchNumber(int pickedNumber) {
+        int matchCount = 0;
+
         for (int j = 0; j < this.numbers.size(); j++) {
+
             if (this.numbers.get(j) == pickedNumber) {
-                return 1;
+                matchCount++;
             }
             if (this.numbers.get(j) > pickedNumber) {
-                return 0;
+                return matchCount;
             }
         }
-        return 0;
+        return matchCount;
+    }
+
+    public boolean checkDuplicate(List<Integer> numbers) {
+        for (int i = 0; i < 5; i++) {
+            int selectedNum = numbers.get(i);
+
+            int result = matchNumber(selectedNum);
+            if (result > 1){
+                return true;
+            }
+        }
+
+        return false;
     }
 }
