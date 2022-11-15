f9a17709f8f8c1af98949c18eeb90015afc5bb49
woogi <dnrlwo11@gmail.com>
2022-11-15 10:47:10 +0900

    refactor : import 수정 및 오탈자 수정

--- src/main/java/lotto/UserServiceImpl.java
+++ src/main/java/lotto/UserServiceImpl.java
@@ -1,27 +1,24 @@
 package lotto;
 import domain.Lotto;
-import utils.IllegalInputFormChecker;
+import utils.IllegalInputChecker;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class UserServiceImpl implements UserService{
-    private static int cntOfLotto = 0;
-    private static List<Integer> userNumList;
-    private static int bonus;
-    private IllegalInputFormChecker illegalInputFormChecker = new IllegalInputFormChecker();
+    private final IllegalInputChecker illegalInputChecker = new IllegalInputChecker();
 
     @Override
     public int getCntOfLotto(int userPayment) {
-        cntOfLotto = userPayment / 1000;
+        int cntOfLotto = userPayment / 1000;
         return cntOfLotto;
     }
 
     @Override
     public List<Integer> getUserNums(String userInput) {
         List<String> userInputList = Arrays.asList(userInput.split(","));
-        userNumList = userInputList.stream()
+        List<Integer> userNumList = userInputList.stream()
                 .map(string -> Integer.parseInt(string))
                 .collect(Collectors.toList());
         Lotto lotto = new Lotto(userNumList);
@@ -30,7 +27,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public int getUserBonus(String userBonus) {
-        bonus = Integer.parseInt(userBonus);
+        int bonus = Integer.parseInt(userBonus);
         return bonus;
     }
 }
--- src/main/java/view/PrintServiceImpl.java
+++ src/main/java/view/PrintServiceImpl.java
@@ -1,17 +1,17 @@
 package view;
 
 import domain.Statistics;
-import message.PrintMessage;
+import messages.InfoMessages;
 
 import java.util.List;
 import java.util.Map;
 
 public class PrintServiceImpl implements PrintService{
-    PrintMessage printMessage;
+    InfoMessages infoMessages;
 
     @Override
     public void printLotto(List<List<Integer>> lottoList) {
-        System.out.println(lottoList.size() + printMessage.PURCHASE_INFO);
+        System.out.println(lottoList.size() + infoMessages.PURCHASE_INFO);
         for (List<Integer> lotto : lottoList) {
             System.out.println(lotto);
         }
@@ -19,11 +19,11 @@ public class PrintServiceImpl implements PrintService{
 
     @Override
     public void printWinningResult(Map<Statistics, Integer> result, double yield) {
-        System.out.println(printMessage.STATISTIC_INFO);
+        System.out.println(infoMessages.STATISTIC_INFO);
         System.out.println("---");
         for (Statistics statistics : Statistics.values()) {
             System.out.println(statistics.getMessage() + result.get(statistics) + "개");
         }
-        System.out.println(printMessage.TOTAL_PRIZE + yield + "%입니다.");
+        System.out.println(infoMessages.TOTAL_PRIZE + yield + "%입니다.");
     }
 }
