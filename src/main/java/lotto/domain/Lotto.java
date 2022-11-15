014562e8da6fe71445eaaf4da4550dd939605da0
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-15 21:29:22 +0900

    feat: 로또 번호를 검증한다
    - 로또 번호가 중복될 경우 예외가 발생한다.
    - 정해진 범위 외의 번호가 있을 경우 예외가 발생한다.

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,48 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static lotto.domain.info.Rule.END_NUMBER;
+import static lotto.domain.info.Rule.START_NUMBER;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        validateDuplicate(numbers);
+        validateLottoNumber(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+    private void validateDuplicate(List<Integer> numbers) {
+        int size = numbers.size();
+        int distinctSize = new HashSet<>(numbers).size();
+        if (size > distinctSize) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateLottoNumber(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < START_NUMBER.getNumber() || number > END_NUMBER.getNumber()) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
+}
