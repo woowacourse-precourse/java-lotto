76845e285d3f44c5281eedf1c218cf0320e9aa99
heejeong <hjx66@naver.com>
2022-11-15 15:44:23 +0900

    feat(view): 1~5등 당첨 통계 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,6 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
-import lotto.domain.LottoMaker;
 import lotto.domain.Rank;
 
 import java.text.DecimalFormat;
@@ -10,12 +9,12 @@ import java.util.stream.Collectors;
 
 public class OutputView {
 
-    public static void printLottoTicktesCount(int ticketCount){
+    public static void printLottoTicktesCount(int ticketCount) {
         System.out.printf("%d개를 구매했습니다.\n", ticketCount);
     }
 
-    public static void printLottoTikects(List<Lotto> lottoTickets){
-        for(Lotto lotto : lottoTickets){
+    public static void printLottoTikects(List<Lotto> lottoTickets) {
+        for (Lotto lotto : lottoTickets) {
             List<Integer> lottoNumbers = lotto.getNumbers();
             List<Integer> sorted = lottoNumbers.stream()
                     .sorted()
@@ -24,24 +23,24 @@ public class OutputView {
         }
     }
 
-    public static void printWinningStatistics(Map<Rank, Integer> result){
+    public static void printWinningStatistics(Map<Rank, Integer> result) {
         System.out.println("당첨 통계\n---");
         DecimalFormat df = new DecimalFormat("###,###");
-        for (Rank rank:result.keySet()) {
+        List<Rank> sortedRanks = Arrays.stream(Rank.values())
+                .sorted(Comparator.reverseOrder())
+                .filter(rank -> rank != Rank.NORANK)
+                .collect(Collectors.toList());
+        for (Rank rank : sortedRanks) {
             String money = df.format(rank.getMoney());
-            System.out.printf("%d개 일치 (%s원) - %d개\n", rank.getCorrectNumCount(),money,result.get(rank));
+            System.out.printf("%d개 일치%s (%s원) - %d개\n", rank.getCorrectNumCount(), getBonusPrint(rank),money, result.get(rank));
         }
 
     }
-
-    public static void main(String[] args) {
-        Map<Rank,Integer> result = new TreeMap<>();
-        result.put(Rank.FIFTH,1);
-        result.put(Rank.FOURTH,0);
-        result.put(Rank.THIRD,0);
-        result.put(Rank.SECOND,0);
-        result.put(Rank.FIRST,0);
-        printWinningStatistics(result);
+    private static String getBonusPrint(Rank rank){
+        if(rank.equals(Rank.SECOND)){
+            return ", 보너스 볼 일치";
+        }
+        return "";
     }
 
 }
