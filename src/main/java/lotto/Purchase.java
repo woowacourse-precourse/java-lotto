c42873df33e530cfd2e3740036150036fefa9f44
Seoyoung <ryupark1006@gmail.com>
2022-11-15 11:58:15 +0900

    feat: 로또 구입 개수 계산 기능 구현

--- src/main/java/lotto/Purchase.java
+++ src/main/java/lotto/Purchase.java
@@ -4,10 +4,15 @@ import static lotto.Error.PRICE_UNIT_ERROR;
 
 public class Purchase {
 
-    public boolean checkPrice(int price) {
+    public static boolean checkPrice(int price) {
         if (price % 1000 != 0) {
             throw new IllegalArgumentException(PRICE_UNIT_ERROR.getMessage());
         }
         return true;
+    }
+
+    public static int getAmount(int price) {
+        int amount = price / 1000;
+        return amount;
     }
 }
