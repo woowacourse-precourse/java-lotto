90055178ea04efa7e31b2a3cf54d0878935bb1fd
liarreez <liarreez@gmail.com>
2022-11-15 15:40:01 +0900

    fix(lotto): 추가 문장 출력 구현, Scanner 대신에 Console.readLine을 사용 feat(lotto): 구입 금액에 해당하는 만큼 로또 발행하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,19 +1,34 @@
 package lotto;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Collections;
+import java.util.List;
 
 public class Application {
+    public static int lottoCount = 0;
+
     public static void main(String[] args) {
         lottoBuy();
+        lottoIssue();
     }
 
     public static void lottoBuy() {
-        Scanner scanner = new Scanner(System.in);
-        int buyLottoPrice = scanner.nextInt();
+        System.out.println("구입금액을 입력해 주세요.");
+        String inputLottoPrice = Console.readLine();
+        int buyLottoPrice = Integer.parseInt(inputLottoPrice);
         if(buyLottoPrice % 1000 != 0 ) {
             throw new IllegalArgumentException("[ERROR]구입 금액은 1,000원 단위로 입력해주세요.");
         }
-        int lottoCount = buyLottoPrice / 1000;
-        System.out.println(lottoCount+ "개를 구매했습니다.");
+        lottoCount = buyLottoPrice / 1000;
+        System.out.println("\n" + lottoCount+ "개를 구매했습니다.");
+    }
+
+    public static void lottoIssue() {
+        for(int i = 0 ; i < lottoCount ; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers);
+            System.out.println(numbers);
+        }
     }
 }
