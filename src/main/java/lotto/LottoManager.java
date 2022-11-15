45901b4aa49225c5baf66d6ef7226d5e263204a4
Kijung Park <kjpark4321@naver.com>
2022-11-15 23:16:56 +0900

    feat : 로또 결과 맵 구하는 메서드 구현

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -2,15 +2,37 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class LottoManager {
     public static final int MIN_NUM = 1;
     public static final int MAX_NUM = 45;
     public static final int LOTTO_COUNT = 6;
 
-    public List<Integer> makeRandomLottoNumbers(){
+    public List<Integer> makeRandomLottoNumbers() {
         return Randoms.pickUniqueNumbersInRange(MIN_NUM, MAX_NUM, LOTTO_COUNT);
+    }
+
+    public int compareSameNumberBetweenTwoLotto(Lotto left, Lotto right) {
+        Set<Integer> leftSet = left.getSetOfLottoNumbers();
+        Set<Integer> rightSet = right.getSetOfLottoNumbers();
+
+        leftSet.retainAll(rightSet);
+        return leftSet.size();
+    }
+
+    public Map<Integer, Integer> getScore(List<Lotto> myLottoList, Lotto answerLotto, int bonusNumber) {
+        Map<Integer, Integer> score = new HashMap<>();
+
+        for (Lotto myLotto : myLottoList) {
+            int eachScore = compareSameNumberBetweenTwoLotto(myLotto, answerLotto);
+            if (myLotto.getSetOfLottoNumbers().contains(bonusNumber)) {
+                eachScore++;
+            }
+
+            score.put(eachScore, score.getOrDefault(eachScore, 0) + 1);
+        }
+
+        return score;
     }
 }
