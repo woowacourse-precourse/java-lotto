a87713f9cfbaab26d484925d78988c3cc99a4212
dlrghks2090 <dlrghks2090@gmail.com>
2022-11-15 18:50:26 +0900

    feat: 로또 발행 기능

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,7 +1,11 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
 import lotto.domain.LottoCount;
+import lotto.domain.LottoMake;
 import lotto.view.Input;
+
+import java.util.List;
 
 public class LottoGame {
     String purchaseAmount;
@@ -9,8 +13,18 @@ public class LottoGame {
 
     public void start() {
         LottoCount lottoCount = new LottoCount(input.lottoPurchaseAmount());
+        List<Lotto> lottoPapers = buyLotto(lottoCount.getLottoCount());
+
         input.lottoWinningNumbers();
 
+
         input.lottoBonusNumber();
+    }
+
+    private List<Lotto> buyLotto(int count) {
+        LottoMake lottoMake = new LottoMake(count);
+        List<Lotto> lottoPapers = lottoMake.getLottoPapers();
+
+        return lottoPapers;
     }
 }
