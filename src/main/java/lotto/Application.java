7ae7e0b9ef3e01e6d24b9da2d95c16fc33c93cb9
dldmsdn99 <dldmsdn99@naver.com>
2022-11-15 21:39:44 +0900

    mian문 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,43 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Randoms;
 
-public class Application {
+import java.util.*;
+import java.util.List;
+
+import static lotto.UI.*;
+import static lotto.Winning.*;
+
+
+
+public class Application{
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<List<Integer>> number = new ArrayList<>();
+
+        System.out.println(Request_Input_Price);
+        Error.notNum(String.valueOf(price));
+        Error.Exception_Error(Integer.parseInt(Lot_EA));
+        UI.Lot_Count(Integer.parseInt(Lot_EA));
+
+        System.out.println(Lot_EA);
+
+        for(int i=0; i<EA; i++) {
+           List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+           number.add(numbers);
+           numbers.sort(Comparator.naturalOrder());
+           System.out.println(numbers);
+        }
+
+        System.out.println(Request_Winning_Number);
+        Winning_number(win_number);
+        System.out.println(Request_Bonus_Number);
+        Bonus_Number();
+        System.out.println(Lot_Result);
+        for(int j=0; j<number.size(); j++){
+            Result(win_numbers,number.get(j));
+        }
+        print_Bonus();
+        getRate(correctNumbers,Integer.parseInt(price));
     }
 }
+
+
