afa59966195e5bdc046ec84f5a8b724d203119d2
dooboocookie <bkh3030@gmail.com>
2022-11-15 14:46:54 +0900

    test: 로또를 추첨하여 등수 값 반환하는 테스트 1등, 2등 시나리오

--- src/test/java/lotto/JudgmentTest.java
+++ src/test/java/lotto/JudgmentTest.java
@@ -1,15 +1,73 @@
-package lotto.domain;
+package lotto;
 
+import lotto.domain.*;
+import lotto.utils.LottoGenerator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class JudgmentTest {
-    @DisplayName("당첨금액과 투입금액을 통한 수익률 계산 테스트")
-    @Test
-    void yieldTest() {
 
+    @DisplayName("로또 추첨하여 등수 값 테스트, 1등 번호 추첨 시나리오")
+    @Test
+    void drawLottoFirstClassTest() throws Exception{
+        User user = new User();
+        Integer[] numbers = {1,2,3,4,5,6};
+        Lotto lotto = new Lotto(new ArrayList<>(List.of(numbers)));
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(lotto);
+
+        Field userLottos = User.class.getDeclaredField("userLottos");
+        userLottos.setAccessible(true);
+        userLottos.set(user, lottos);
+
+        WinningNumbers winningNumbers = new WinningNumbers();
+        Integer[] winNumbers = {1,2,3,4,5,6};
+        winningNumbers.newWinningNumbers(new ArrayList<>(List.of(winNumbers)));
+        winningNumbers.newBonusNumber(7);
+
+        Judgment judgment = new Judgment(user, winningNumbers);
+
+        Method drawLotto = Judgment.class.getDeclaredMethod("drawLotto", int.class);
+        drawLotto.setAccessible(true);
+        Rank rank = (Rank) drawLotto.invoke(judgment, 0);
+
+        assertThat(rank).isEqualTo(Rank.FIRST_CLASS);
     }
+
+    @DisplayName("로또 추첨하여 등수 값 테스트, 2등 번호 추첨 시나리오")
+    @Test
+    void drawLottoSecondClassTest() throws Exception{
+        User user = new User();
+        Integer[] numbers = {1,2,3,4,5,7};
+        Lotto lotto = new Lotto(new ArrayList<>(List.of(numbers)));
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(lotto);
+
+        Field userLottos = User.class.getDeclaredField("userLottos");
+        userLottos.setAccessible(true);
+        userLottos.set(user, lottos);
+
+        WinningNumbers winningNumbers = new WinningNumbers();
+        Integer[] winNumbers = {1,2,3,4,5,6};
+        winningNumbers.newWinningNumbers(new ArrayList<>(List.of(winNumbers)));
+        winningNumbers.newBonusNumber(7);
+
+        Judgment judgment = new Judgment(user, winningNumbers);
+
+        Method drawLotto = Judgment.class.getDeclaredMethod("drawLotto", int.class);
+        drawLotto.setAccessible(true);
+        Rank rank = (Rank) drawLotto.invoke(judgment, 0);
+
+        assertThat(rank).isEqualTo(Rank.SECOND_CLASS);
+    }
+    
 
 }
