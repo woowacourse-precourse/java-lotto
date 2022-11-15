e248d4bf2eb279e356345df06bcac0d9d135c27a
osy9757 <osy9757@gmail.com>
2022-11-15 23:26:53 +0900

    refactor : 오류 경우의 수 추가

--- src/main/java/lotto/print/PrintInputMessage.java
+++ src/main/java/lotto/print/PrintInputMessage.java
@@ -12,6 +12,9 @@ public class PrintInputMessage {
     private static final String INPUT_WINNING_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_LOTTO_NUMBER = "보너스 번호를 입력해 주세요.";
     private static final String INPUT_WRONG_DATA_TYPE = "[ERROR] 입력한 값이 숫자가 아닙니다.";
+    private static final String PRINT_DUPLICATE_ERROR = "[ERROR] 겹치는 로또 숫자가 있습니다.";
+
+
 
 
     public static int getPurchaseAmount() {
@@ -31,11 +34,16 @@ public class PrintInputMessage {
         return  inputWinningNum.stream().map(Integer::valueOf).collect(Collectors.toList());
     }
 
-    public static int getBonusLottoNumber() {
+    public static int getBonusLottoNumber(List<Integer> getWinningLottoNum) {
+        System.out.println();
         System.out.println(INPUT_BONUS_LOTTO_NUMBER);
+        int getBonusNum = Integer.parseInt(Console.readLine());
+        if(getWinningLottoNum.contains(getBonusNum)) {
+            throw new IllegalArgumentException(PRINT_DUPLICATE_ERROR);
+        }
         try {
-            return Integer.parseInt(Console.readLine());
-        } catch (NumberFormatException ei) {
+            return getBonusNum;
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INPUT_WRONG_DATA_TYPE);
         }
     }
