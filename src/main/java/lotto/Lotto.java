22c650be2bcc6e5b695b05254b207e9dfef1313e
farmJun <junpreme1205@gmail.com>
2022-11-15 21:07:26 +0900

    feat(Lotto): 로또 번호 유효성(크기, 중복, 범위) 검사 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,17 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static lotto.ErrorMessage.*;
 
 public class Lotto {
+    private final int LOTTO_NUMBERS_SIZE = 6;
+    private final int NO_OUT_OF_RANGE_SIZE = 0;
+    private final int LOTTO_NUMBER_FIRST_RANGE = 1;
+    private final int LOTTO_NUMBER_LAST_RANGE = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -10,11 +19,33 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validate(List<Integer> numbers) throws IllegalArgumentException {
+        checkLottoNumbersOverSize(numbers);
+        checkLottoNumbersDuplicate(numbers);
+        checkLottoNumbersRange(numbers);
+    }
+
+    private void checkLottoNumbersOverSize(List<Integer> numbers) throws IllegalArgumentException {
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBERS_SIZE_MESSAGE.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void checkLottoNumbersDuplicate(List<Integer> numbers) throws IllegalArgumentException {
+        Set<Integer> duplicateNumbers = new HashSet<>(numbers);
+
+        if (numbers.size() != duplicateNumbers.size()) {
+            throw new IllegalArgumentException(DUPLICATED_NUMBERS_MESSAGE.getMessage());
+        }
+    }
+
+    private void checkLottoNumbersRange(List<Integer> numbers) {
+        List<Integer> outOfRange = numbers
+                .stream().filter(number -> number > LOTTO_NUMBER_LAST_RANGE | number < LOTTO_NUMBER_FIRST_RANGE)
+                .collect(Collectors.toList());
+
+        if (outOfRange.size() != NO_OUT_OF_RANGE_SIZE) {
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBERS_RANGE.getMessage());
+        }
+    }
 }
