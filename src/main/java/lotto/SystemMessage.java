648492a3fff44c67fdd242f58811af5d5ee6aa66
김선동 <mok03127@gmail.com>
2022-11-15 17:15:16 +0900

    feat: 컨트롤러에 필요한 시스템 메시지(구입 금액, 당첨 번호, 보너스 번호)

---
+++ src/main/java/lotto/SystemMessage.java
@@ -0,0 +1,27 @@
+package lotto;
+
+public class SystemMessage {
+    private static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static void moneyInput() {
+        System.out.println(MONEY_INPUT_MESSAGE);
+    }
+
+    public static void purchaseLotto(int lottoTickets, Lottos lottos) {
+        System.out.println();
+        System.out.println(lottoTickets + "개를 구매했습니다.");
+        for (Lotto lotto : lottos.getLottos()) {
+            System.out.println(lotto);
+        }
+    }
+
+    public static void winningNumber() {
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void bonusNumber() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}
