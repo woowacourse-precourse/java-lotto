13d6b82ec5d779aa9f08a894dcba15c3ffe3274d
CJiu01 <jiwoo020801@gmail.com>
2022-11-15 22:55:19 +0900

    [Feat] ResultView 함수 작성

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,5 +1,25 @@
 package lotto.view;
 
+import java.text.DecimalFormat;
+
 public class ResultView {
+
+    public void printTotalCount(int count) {
+        System.out.println(count + "개를 구매했습니다.");
+    }
+
+    public void printLottoTickets(String lottoTickets) {
+        System.out.println(lottoTickets);
+    }
+
+    public void printWinnerStat(List<Rank> ranks) {
+        System.out.println("\n당첨 통계");
+        System.out.println("---------");
+    }
+
+    public void printEarningRatio(double rate) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.print("총 수익률은 " + df.format(rate) + "입니다.");
+    }
 
 }
