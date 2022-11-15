0e3adda618aa9349c76bb245b8458f6c1cdbb323
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-15 18:17:21 +0900

    feat(LottoGame): 구입 금액만큼 발행하기

--- src/main/java/lotto/controller/Game.java
+++ src/main/java/lotto/controller/Game.java
@@ -1,12 +1,15 @@
 package lotto.controller;
 
-import lotto.domain.RandomNumbersGenerator;
+import lotto.domain.Lotto;
+import lotto.domain.LottoCashier;
 import lotto.view.InputView;
 import java.util.List;
 
 public class Game {
     public void startLottoGame() {
-
-
+        int money = InputView.inputLottoAmount();
+        LottoCashier lottoCashier = new LottoCashier();
+        int count = lottoCashier.getLottoCount(money);
+        List<Lotto> lottos = lottoCashier.createLottoList(count);
     }
 }
--- src/main/java/lotto/domain/LottoCashier.java
+++ src/main/java/lotto/domain/LottoCashier.java
@@ -1,10 +1,25 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoCashier {
     public int getLottoCount(int money) {
         if (money > 0 && money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 구입해야 합니다.");
             }
         return money / 1000;
+    }
+
+    public List<Lotto> createLottoList(int count) {
+        List<Lotto> purchasedLotto = new ArrayList<>();
+        RandomNumbersGenerator randomNumbers = new RandomNumbersGenerator();
+
+        for (int i = 0; i < count; i++) {
+            List<Integer> lottoNumbers = randomNumbers.createRandomNumbers();
+            Lotto lotto = new Lotto(lottoNumbers);
+            purchasedLotto.add(lotto);
+        }
+        return purchasedLotto;
     }
 }
