19030dbd3c86bd1c2b1d7f73f864ad6b439883fc
ddukddaki <rlfrkdms1@naver.com>
2022-11-15 16:50:56 +0900

    feat(LottoStore): 로또를 구매해 발행하는 기능

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -1,8 +1,25 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoStore {
 
     private static final int A_LOTTO_PRICE = 1000;
+    private static final int LENGTH_OF_WINNING_NUMBER = 6;
+    private static final int NUMBER_MAX_RANGE = 45;
+    private static final int NUMBER_MIN_RANGE = 1;
+
+    private List<Lotto> lottoTickets = new ArrayList<>();
+
+    public List<Lotto> buyLottoTickets(int lottoPurchaseMoney) {
+        int lottoAmount = getLottoAmount(lottoPurchaseMoney);
+        for (int i = 0; i < lottoAmount; i++) {
+            lottoTickets.add(new Lotto(Randoms.pickUniqueNumbersInRange(NUMBER_MIN_RANGE, NUMBER_MAX_RANGE, LENGTH_OF_WINNING_NUMBER)));
+        }
+        return lottoTickets;
+    }
 
     public int getLottoAmount(int lottoPurchaseMoney) {
         return lottoPurchaseMoney / A_LOTTO_PRICE;
