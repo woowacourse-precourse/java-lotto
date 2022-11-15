be037e669f4123f7daeaf1330fe5b15d707808ff
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-11-15 18:29:07 +0900

    feat: 로또 게임 실행
    UserLotto객체를 생성하여 게임을 실행한다. 발생하는 오류를 catch해서 오류 메세지를 출력한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //TODO: 프로그램 구현
+        try {
+            UserLotto userLotto = new UserLotto();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
 }
+
