d8ccd00e1b5008fcd373d64da25057cfe165dd8b
dvlpr0 <rladjswkd@naver.com>
2022-11-15 21:10:08 +0900

    fix: 출력 양식에 맞게 메시지 내용 수정

--- src/main/java/lotto/InformMessage.java
+++ src/main/java/lotto/InformMessage.java
@@ -2,15 +2,16 @@ package lotto;
 
 public enum InformMessage {
     PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
-    PURCHASE_COUNT("개를 구매했습니다."),
-    WINNING_NUMBER("당첨 번호를 입력해 주세요."),
-    RESULT("당첨 통계\n---\n" +
+    PURCHASE_COUNT("\n%d개를 구매했습니다.\n"),
+    WINNING_NUMBER("\n당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
+    RESULT("\n당첨 통계\n---\n" +
             "3개 일치 (5,000원) - %d개\n" +
             "4개 일치 (50,000원) - %d개\n" +
             "5개 일치 (1,500,000원) - %d개\n" +
             "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n" +
             "6개 일치 (2,000,000,000원) - %d개\n"),
-    EARNING_RATE("총 수익률은 .1f%입니다.\n");
+    EARNING_RATE("총 수익률은 %.1f%%입니다.\n");
 
     private final String MESSAGE;
 
