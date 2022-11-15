a09c45cb44f783f97006e250da2a983015deec56
hingjae <ljh23270@naver.com>
2022-11-15 19:04:17 +0900

    콘솔 입 출력

--- src/main/java/lotto/view/ConsoleOutput.java
+++ src/main/java/lotto/view/ConsoleOutput.java
@@ -1,2 +1,30 @@
-package lotto.view;public class ConsoleOutput {
+package lotto.view;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoList;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class ConsoleOutput {
+
+    public void printLottoList(int amount, LottoList lottoList) {
+        LottoAmount(amount);
+        System.out.println(lottoList.toString());
+    }
+
+    public void LottoAmount(int amount) {
+        System.out.println(amount + "개를 구매했습니다.");
+    }
+
+    public void printStatistics(String statistics) {
+        System.out.println("당첨 통계\n---");
+        printSameCountPhrase(statistics);
+    }
+    public void printSameCountPhrase(String rankStatistics) {
+        System.out.println(rankStatistics);
+    }
+    public void printProfitRatio(BigDecimal ratio) {
+        System.out.println("총 수익률은 " + ratio + "입니다.");
+    }
 }
