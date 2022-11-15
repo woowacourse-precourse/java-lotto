ec6c21dfcd36760fd2bd82efd173a79737cf4a6d
hingjae <ljh23270@naver.com>
2022-11-15 19:09:30 +0900

    당첨 통계 출력

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,2 +1,44 @@
-package lotto.domain;public enum LottoRank {
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+    FIRST(2_000_000_000, 6),
+    SECOND(30_000_000, 5),
+    THIRD(1_500_000, 5),
+    FOURTH(50_000, 4),
+    FIFTH(5_000, 3),
+    NONE(0, 0);
+
+    private final int money;
+    private final int matchedCount;
+    private final String matchedPhrase = "개 일치";
+    private final String matchedBonusPhrase = "보너스 볼 일치";
+
+    LottoRank(int money, int matchedCount) {
+        this.money = money;
+        this.matchedCount = matchedCount;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public static LottoRank checkRanking(int matchCount, boolean isBonus) {
+        if (matchCount == SECOND.matchedCount && isBonus) {
+            return SECOND;
+        }
+        return Arrays.stream(values())
+                .filter(r -> r.matchedCount == matchCount && r != SECOND)
+                .findFirst()
+                .orElse(LottoRank.NONE);
+    }
+
+    @Override
+    public String toString() {
+        if (this == SECOND) {
+            return matchedCount + matchedPhrase + "," + matchedBonusPhrase + " (" + money + "원)";
+        }
+        return matchedCount + matchedPhrase + " (" + money + "원)";
+    }
 }
--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,2 +1,42 @@
-package lotto.domain;public class LottoResult {
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class LottoResult {
+    private final HashMap<LottoRank, Integer> result = new HashMap<>();
+
+    public LottoResult(List<LottoRank> lottoRanks) {
+        initializationMap();
+        for (LottoRank lottoRank : lottoRanks) {
+            result.put(lottoRank, result.get(lottoRank) + 1);
+        }
+    }
+
+    public void initializationMap() {
+        for (LottoRank lottoRank : LottoRank.values()) {
+            result.put(lottoRank, 0);
+        }
+    }
+
+    public int totalPrize() {
+        int sum = 0;
+        for (LottoRank lottoRank : result.keySet()) {
+            sum += result.get(lottoRank) * lottoRank.getMoney();
+        }
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = LottoRank.values().length - 2; i >= 0; i--) {
+            sb.append(LottoRank.values()[i].toString());
+            sb.append("- ");
+            sb.append(result.get(LottoRank.values()[i]));
+            sb.append("개\n");
+        }
+        return sb.toString();
+    }
+
 }
