92d12c6d2c10ea8e920e412b504e700f87b39651
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-15 18:51:04 +0900

    feat: 보너스 번호 숫자 외 예외 처리

--- src/main/java/lotto/Bonus.java
+++ src/main/java/lotto/Bonus.java
@@ -10,13 +10,19 @@ public class Bonus {
 
     public Bonus(List<Integer> numbers, int bonusNumber) {
         range(bonusNumber);
-        //type(bonusNumber);
+        type(bonusNumber);
         //repeat(numbers, bonusNumber);
         this.bonusNumber = bonusNumber;
     }
     private void range(int bonus){
         if (String.valueOf(bonus).length() != 1) {
             throw new IllegalArgumentException(ErrorMessage.bonusLength.getValue());
+        }
+    }
+    private void type(int bonus) {
+        int eachChar = String.valueOf(bonus).charAt(0);
+        if (!Character.isDigit(eachChar)) {
+            throw new IllegalArgumentException(ErrorMessage.bonusType.getValue());
         }
     }
 }
