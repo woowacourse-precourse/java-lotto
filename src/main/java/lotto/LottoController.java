4b7d80e35f01a49d3eefc66114747c810733f9f3
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-15 14:42:38 +0900

    feat: 구매한 로또 출력 기능

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,10 +1,25 @@
 package lotto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LottoController {
     public void start() {
         Money userInputMoney = new Money();
         userInputMoney.init();
         LottoGame lottoGame = new LottoGame();
         lottoGame.buy(userInputMoney);
+        printLottos(lottoGame.getLottos());
+    }
+
+    public void printLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            String numbers = lotto.getNumbers().stream()
+                    .map(i -> Integer.toString(i))
+                    .collect(Collectors.joining(", "));
+            System.out.printf("[%s]" + System.lineSeparator(), numbers);
+        }
+        System.out.println();
     }
 }
