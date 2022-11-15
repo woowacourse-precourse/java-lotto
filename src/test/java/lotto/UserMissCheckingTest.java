e65a075d2f2857420d8dc1a8db4faef7afd56f46
ijung-won <dlwnddnjs96@gmail.com>
2022-11-15 18:29:42 +0900

    test(UserMissCheckingTest): 지정된 로또 숫자범위 1~45 이외 숫자 입력 시 오류 테스트 및 다르 테스트 여러 케이스 추가

--- src/test/java/lotto/UserMissCheckingTest.java
+++ src/test/java/lotto/UserMissCheckingTest.java
@@ -15,11 +15,27 @@ class UserMissCheckingTest {
     public void 숫자대신_문자입력_에러(){
         assertThatThrownBy(() -> UserMissChecking.numberChecking("사십사"))
                 .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> UserMissChecking.numberChecking("test"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> UserMissChecking.numberChecking(""))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void 로또금액_첫원단위_미입금(){
         assertThatThrownBy(()->UserMissChecking.thousandChecking(4200))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->UserMissChecking.thousandChecking(12305))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->UserMissChecking.thousandChecking(123130))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 지정된_로또_숫자범위_이외_숫자_삽입(){
+        assertThatThrownBy(()->UserMissChecking.numberLimitChecking(0))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->UserMissChecking.numberLimitChecking(47))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
