3a0b1e3aa5860ce768b122d47a54d8e833ad243a
June1010 <tjrwns1010@naver.com>
2022-11-15 23:43:40 +0900

    feat: 기존 로또 클래스에 정렬 메서드 및 검증 로직 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,35 @@
 package lotto;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        numbers = sortNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+        if (set.size() < numbers.size()) {
+            throw new IllegalArgumentException("로또 번호는 중복된 수로 구성될 수 없습니다.");
+        }
     }
 
-    // TODO: 추가 기능 구현
+    private List<Integer> sortNumber(List<Integer> numbers) {
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        return sortedNumbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
