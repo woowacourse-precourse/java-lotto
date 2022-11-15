b5ca75a02ccb5e3e5b304792d445c139f4d10227
yeonhee <ydol2@naver.com>
2022-11-15 14:40:35 +0900

    refactor(LottoPrize) : enum에서 로또 상금 받아 계산하도록 변경

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -9,11 +9,6 @@ import static lotto.domain.LottoResult.FIVE_BONUS;
 import static lotto.domain.LottoResult.SIX;
 
 public class LottoPrize {
-    private static final int GET_FIFTH_RANKING_PRIZE = 5000;
-    private static final int GET_FOURTH_RANKING_PRIZE = 50000;
-    private static final int GET_THIRD_RANKING_PRIZE = 1500000;
-    private static final int GET_SECOND_RANKING_PRIZE = 30000000;
-    private static final int GET_FIRST_RANKING_PRIZE = 2000000000;
 
     public float calculateEarning(int lottoPrice) {
         return ((float) addPrize() / (float) lottoPrice) * 100;
@@ -22,11 +17,11 @@ public class LottoPrize {
         Map<LottoResult,Integer>  lottoResultMap = LottoStatistics.getLottoResultMap();
         int total = 0;
         for ( LottoResult key : lottoResultMap.keySet() ) {
-            if (key == THREE) total+= GET_FIFTH_RANKING_PRIZE * lottoResultMap.get(key);
-            else if (key == FOUR) total += GET_FOURTH_RANKING_PRIZE * lottoResultMap.get(key);
-            else if (key == FIVE) total += GET_THIRD_RANKING_PRIZE * lottoResultMap.get(key);
-            else if (key == FIVE_BONUS) total += GET_SECOND_RANKING_PRIZE * lottoResultMap.get(key);
-            else if (key == SIX) total += GET_FIRST_RANKING_PRIZE * lottoResultMap.get(key);
+            if (key == THREE) total+= key.getPrize() * lottoResultMap.get(key);
+            else if (key == FOUR) total += key.getPrize() * lottoResultMap.get(key);
+            else if (key == FIVE) total += key.getPrize() * lottoResultMap.get(key);
+            else if (key == FIVE_BONUS) total += key.getPrize() * lottoResultMap.get(key);
+            else if (key == SIX) total += key.getPrize() * lottoResultMap.get(key);
         }
         return total;
     }
