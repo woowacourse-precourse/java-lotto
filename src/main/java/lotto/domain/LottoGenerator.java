535f23ba83484e02541fea06d0771cbb1e6f6b33
hgo641 <hgo641@gmail.com>
2022-11-15 14:31:01 +0900

    refactor: LottoNumberParser 기능 분리

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -3,8 +3,9 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+import static lotto.domain.LottoNumberParser.parseLottoNumbers;
 
 public class LottoGenerator {
     private static final int LOTTO_MIN_NUMBER = 1;
@@ -31,32 +32,5 @@ public class LottoGenerator {
     public static Lotto getInputLotto(String inputLottoNumbers) {
         List<Integer> lottoNumbers = parseLottoNumbers(inputLottoNumbers);
         return new Lotto(lottoNumbers);
-    }
-
-    private static List<Integer> parseLottoNumbers(String inputNumbers) {
-        List<Integer> lottoNumbers = new ArrayList<>();
-        Arrays.stream(inputNumbers.split(Lotto.NUMBER_SEPARATOR)).forEach(digit -> {
-            int numberConvert = covertDigitToNumber(digit);
-            validateNumberRange(numberConvert);
-            lottoNumbers.add(numberConvert);
-        });
-        return lottoNumbers;
-    }
-
-    private static int covertDigitToNumber(String digit) {
-        try {
-            Integer.parseInt(digit);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("당첨 번호는 숫자만 입력할 수 있습니다.");
-        }
-        return Integer.parseInt(digit);
-    }
-
-    private static void validateNumberRange(int number) {
-        if (number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER) {
-            throw new IllegalArgumentException(
-                    String.format("당첨 번호는 %d부터 %d까지의 수만 입력할 수 있습니다.", LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
-            );
-        }
     }
 }
---
+++ src/main/java/lotto/domain/LottoNumberParser.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LottoNumberParser {
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+
+    public static List<Integer> parseLottoNumbers(String inputNumbers) {
+        List<Integer> lottoNumbers = new ArrayList<>();
+        Arrays.stream(inputNumbers.split(Lotto.NUMBER_SEPARATOR)).forEach(digit -> {
+            int numberConvert = covertDigitToNumber(digit);
+            validateNumberRange(numberConvert);
+            lottoNumbers.add(numberConvert);
+        });
+        return lottoNumbers;
+    }
+
+    private static int covertDigitToNumber(String digit) {
+        try {
+            Integer.parseInt(digit);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("당첨 번호는 숫자만 입력할 수 있습니다.");
+        }
+        return Integer.parseInt(digit);
+    }
+
+    private static void validateNumberRange(int number) {
+        if (number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER) {
+            throw new IllegalArgumentException(
+                    String.format("당첨 번호는 %d부터 %d까지의 수만 입력할 수 있습니다.", LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
+            );
+        }
+    }
+}
