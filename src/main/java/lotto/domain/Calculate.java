e28644ea68cd0d26e15c4278573c0fe4a2d0e850
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-11-15 18:39:32 +0900

    feat(Calculate): 수익률 계산하는 기능 수정

--- src/main/java/lotto/domain/Calculate.java
+++ src/main/java/lotto/domain/Calculate.java
@@ -3,7 +3,8 @@ package lotto.domain;
 public class Calculate {
 
     public double calculateRateOfProfit(int buyAmount, long winnings) {
-        double division = (double) winnings / buyAmount * 100;
+        double division = (double) winnings / (buyAmount * 1000);
+        division *= 100;
         double round = Math.round(division * 10);
         double profit = round / 10;
         return profit;
