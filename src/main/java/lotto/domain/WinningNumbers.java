d3d9da61be4c504266959997984d70925ebd35d4
kkirby54 <sdk926@gmail.com>
2022-11-15 16:24:02 +0900

    feat: 당첨 번호 예외 처리
    - 앞 6개 번호는 로또 번호와 동일하게 예외 처리
    - 따라서 Lotto domain의 validate 메서드를 protected로 변경

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -9,6 +9,19 @@ public class WinningNumbers extends Lotto {
     public WinningNumbers(List<Integer> numbers, int bonus) {
         super(numbers);
         this.numbers = numbers;
+
+        validateBonus(bonus);
         this.bonus = bonus;
+    }
+
+    private void validateBonus(int bonus) {
+        if (this.numbers.contains(bonus)) {
+            System.out.println("[ERROR] 보너스 번호는 로또 번호와 중복이 없어야 합니다.");
+            throw new IllegalArgumentException();
+        }
+        if (bonus < 1 || bonus > 45) {
+            System.out.println("[ERROR] 보너스 번호는 1부터 45 사이의 자연수여야 합니다.");
+            throw new IllegalArgumentException();
+        }
     }
 }
