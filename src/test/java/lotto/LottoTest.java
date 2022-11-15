3ac07a281d49ac677a9a4bd6d1d91573dca70021
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-15 15:44:32 +0900

    test: issue lotto test

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,9 +2,13 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +27,22 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("발행 로또의 개수가 1보다 작으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {
+            -1
+    })
+    void issueLottoByLessThanOne(int number) {
+        assertThatThrownBy(() -> Lotto.issueLotto(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("발행 로또 개수 확인")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "8, 8"
+    })
+    void issueLotto(int number, int numberOfLottos) {
+        assertThat(Lotto.issueLotto(numberOfLottos).size()).isEqualTo(number);
+    }
 }
