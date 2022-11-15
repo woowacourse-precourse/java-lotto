90e9a9c908ae078ad3bedcb4f4dad521a06449bf
koo9b9h <90121024+koo9b9h@users.noreply.github.com>
2022-11-15 14:38:29 +0900

    feat: Add Application for lottostart method Execution

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+
+import lotto.Controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
-}
+        try {
+            LottoController lottoController = new LottoController();
+            lottoController.lottoStart();
+        } catch (IllegalArgumentException e){
+            System.out.println("시스템을 종료합니다.");
+        }//try-catch
+    }// main
+}// end class
