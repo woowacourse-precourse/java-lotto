84b68b8cc7e4c8a2d62d2562cf6044a8f14d0b56
Your Name <dbsdbxkrzz@naver.com>
2022-11-15 16:17:11 +0900

    feat: Lotto 클래스 변경
    유효성 검증은 모두 Validate클래스에 맏깁니다
    getter메서드를 추가합니다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,16 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
-
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        Validate.checkValidWinningNumbers(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+    public List<Integer> getNumbers(){
+        return this.numbers;
     }
-
-    // TODO: 추가 기능 구현
 }
