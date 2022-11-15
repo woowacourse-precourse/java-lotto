37d9a501b72c6e26b931f08204e0bded078943d3
jam961020 <jam961020@naver.com>
2022-11-15 21:56:21 +0900

    구매금액 입력 결과

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,19 +2,31 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Application {
     public static void Error(String ErrorType){
         throw new IllegalArgumentException("[ERROR] " + ErrorType);
     }
     public static Integer EnterAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
+
         Integer Amount;
         Amount = Integer.parseInt(Console.readLine());
         if(Amount%1000!=0){
-            Error("구매 금액은 1000의 배수여야 합니다.");
+            Error("구입금액은 1000의 배수여야 합니다.");
         }
         return Amount;
     }
-    public static void InPut(){
+    public static void PrintPurchaseResult(int Amount){
+        int NumberOfGame = Amount/1000;
+        System.out.println();
+        System.out.println(NumberOfGame + "개를 구매했습니다.");
+    }
+    public static List<Integer> setWinningNumber(){
+        
+    }
+    public static void UI(){
 
     }
     public static void main(String[] args) {
