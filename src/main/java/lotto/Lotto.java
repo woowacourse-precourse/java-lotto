99e75663e1d1f2526b86faabc4a56d888a27d2f8
박정현 <ros28081@naver.com>
2022-11-15 18:48:58 +0900

    feat: 보너스 여부 및 일치 갯수 출력 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,58 @@
 package lotto;
 
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import lotto.ErrorMessage;
+import lotto.SystemMessage;
+import lotto.Result;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) throws IllegalArgumentException {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_SIZE_ERROR.print());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) throws IllegalArgumentException {
+        for(int i=0; i<numbers.size(); i++) {
+            if (numbers.get(i) > 45 || numbers.get(i) < 1) {
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_RANGE_ERROR.print());
+            }
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) throws IllegalArgumentException {
+        for (int i = 0; i < numbers.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (numbers.get(i).equals(numbers.get(j))) {
+                    throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_IS_DUPLICATED.print());
+                }
+            }
+        }
+    }
+
+    public boolean isContainBonus(Integer bonus){
+        if(this.numbers.contains(bonus)) return true;
+        else return false;
+    }
+
+    public Integer getNumbersOfMatch(List<Integer> num){
+        int count=0;
+        for(Integer a : num){
+            if(this.numbers.contains(a)) count++;
+        }
+        return count;
+    }
+
 }
