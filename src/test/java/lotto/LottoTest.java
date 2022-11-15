76f07e98bd0dde5e6beea06b112aded47dca628d
olrlobt <317tmdgjs@naver.com>
2022-11-15 12:33:24 +0900

    test: 로또 번호가 범위를 넘어갈때, 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,5 +24,17 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 로또 범위를 넘어가는 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByOverRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 로또 범위를 넘어가는 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
