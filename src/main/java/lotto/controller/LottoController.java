63322ff31b0d5616ad888a94f9bfa34eca502297
leesangjin <tkdwls0628@gmail.com>
2022-11-15 18:49:15 +0900

    feat(LottoController): 로또 생성 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,19 +1,33 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
 import lotto.domain.User;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoController {
 
     private int moneyAmount;
+    private List<Lotto> lottos = new ArrayList<>();
 
     private User user = new User();
+    private LottoGenerator lottoGenerator = new LottoGenerator();
 
     public void buyLotto() {
         InputView.requestAmount();
         moneyAmount = user.inputOfMoneyAmount();
         int lottoAmount = moneyAmount / 1000;
         OutputView.printAmount(lottoAmount);
+    }
+
+    public void generateLottos(int lottoAmount) {
+        for (int i = 0; i < lottoAmount; i++) {
+            Lotto lotto = lottoGenerator.makeLotto();
+            lottos.add(lotto);
+        }
+        OutputView.printLottos(lottos);
     }
 }
