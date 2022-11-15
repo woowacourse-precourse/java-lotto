ce51eb5a5ca97a2c732ab5038927d9331d374bba
ashlovesliitea <skyrider1811@gmail.com>
2022-11-15 21:36:41 +0900

    feat: OutputView - 랭킹 메세지 생성 기능 구현

--- src/main/java/lotto/Ranking.java
+++ src/main/java/lotto/Ranking.java
@@ -3,11 +3,11 @@ package lotto;
 import java.util.Arrays;
 
 public enum Ranking {
-    FIRST(2000000000, 6, false),
-    SECOND(30000000, 5, true),
-    THIRD(1500000, 5, false),
-    FOURTH(50000, 4, false),
-    FIFTH(5000, 3, false),
+    FIRST(2_000_000_000, 6, false),
+    SECOND(30_000_000, 5, true),
+    THIRD(1_500_000, 5, false),
+    FOURTH(50_000, 4, false),
+    FIFTH(5_000, 3, false),
     FAIL(0, -1, false);
 
     private int reward;
