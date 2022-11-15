2573a154d0317b2851d0834fbbb3ad32b3940c08
eunseo <qnrqjrm03@naver.com>
2022-11-15 16:31:45 +0900

    feat : 로또 생성하기

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,5 +20,15 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static List<List<Integer>> makeLotto(int count) {
+        List<List<Integer>> lottos = new ArrayList<>();
+
+        for (int i = 0; i < count; i ++){
+            List<Integer> winLotto = Randoms.pickUniqueNumbersInRange(1,45,6);
+            winLotto = winLotto.stream().sorted().collect(Collectors.toList());
+            lottos.add(winLotto);
+        }
+
+        return lottos;
+    }
 }
