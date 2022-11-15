ac34b487f3ffca609303f8395258d1eae9509d7a
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-15 21:35:21 +0900

    style(LottoStore): lottoCount 자료형 변경

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -6,15 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoStore {
-    public static List<Lotto> saleLotto(int lottoCount) {
+    public static List<List<Integer>> saleLotto(long lottoCount) {
         final int MIN_NUMBER = 1;
         final int MAX_NUMBER = 45;
         final int NUMBER_SIZE = 6;
-        List<Lotto> lottoPapers = new ArrayList<Lotto>();
+        List<List<Integer>> lottoPapers = new ArrayList<>();
 
         for (int i = 0; i < lottoCount; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBER_SIZE);
-            lottoPapers.add(new Lotto(numbers));
+            lottoPapers.add(new Lotto(numbers).getNumbers());
         }
         return lottoPapers;
     }
