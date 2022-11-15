154a4a6cb1d1c2663226442afc6a9addbe82eff9
60jong <rudwhd515@gmail.com>
2022-11-15 18:31:02 +0900

    feat(LottoProgram) : 출력 메시지 상수로 추가

--- src/main/java/lotto/LottoProgram.java
+++ src/main/java/lotto/LottoProgram.java
@@ -10,7 +10,14 @@ public class LottoProgram {
     private final String ORDER_QUANTITY_ALERT_MESSAGE = "%d개를 구매했습니다.\n";
     private final String ASK_FOR_LOTTO_NUMBERS_MESSAGE = "당첨 번호를 입력해주세요.";
     private final String ASK_FOR_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해주세요.";
-    
+    private final String WINNING_STATS_TITLE_MESSAGE = "당첨 통계\n---";
+    private final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다.\n";
+    private final String _3_LOTTO_NUMBERS_MATCH_MESSAGE = "3개 일치 (5,000원) - %d개\n";
+    private final String _4_LOTTO_NUMBERS_MATCH_MESSAGE = "4개 일치 (50,000원) - %d개\n";
+    private final String _5_LOTTO_NUMBERS_MATCH_MESSAGE = "5개 일치 (1,500,000원) - %d개\n";
+    private final String _5_LOTTO_NUMBERS_AND_BONUS_NUMBER_MATCH_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n";
+    private final String _6_LOTTO_NUMBERS_MATCH_MESSAGE = "6개 일치 (2,000,000,000원) - %d개\n";
+
     private LottoController lottoController;
 
     public LottoProgram(LottoController lottoController) {
@@ -19,9 +26,12 @@ public class LottoProgram {
 
     // 로또 프로그램 실행
     public void run() {
+        LottoStats userLottoDetails = new LottoStats();
         List<Lotto> userLottos = createUserLottos();
         alertUserLottos(userLottos);
         WinningLotto winningLotto = createWinningLotto();
+
+        showWinningStats(winningLotto, userLottos);
     }
 
     private void alertUserLottos(List<Lotto> userLottos) {
@@ -69,4 +79,26 @@ public class LottoProgram {
         return Integer.parseInt(Console.readLine());
     }
 
+    private void showWinningStats(WinningLotto winningLotto, List<Lotto> userLottos) {
+        LottoStats lottoStats = lottoController.getLottoStats(winningLotto, userLottos);
+        alertLottoStats(lottoStats);
+        alertRateOfReturn(lottoStats.getRateOfReturn());
+    }
+
+    private void alertLottoStats(LottoStats lottoStats) {
+        System.out.println(WINNING_STATS_TITLE_MESSAGE);
+        System.out.printf(_3_LOTTO_NUMBERS_MATCH_MESSAGE, lottoStats.getCountOf3NumbersMatch());
+        System.out.printf(_4_LOTTO_NUMBERS_MATCH_MESSAGE, lottoStats.getCountOf4NumbersMatch());
+        System.out.printf(_5_LOTTO_NUMBERS_MATCH_MESSAGE, lottoStats.getCountOf5NumbersMatch());
+        System.out.printf(
+                _5_LOTTO_NUMBERS_AND_BONUS_NUMBER_MATCH_MESSAGE,
+                lottoStats.getCountOf5NumbersAndBonusNumberMatch());
+        System.out.printf(_6_LOTTO_NUMBERS_MATCH_MESSAGE, lottoStats.getCountOf6NumbersMatch());
+
+    }
+
+
+    private void alertRateOfReturn(Float rateOfReturn) {
+        System.out.printf(RATE_OF_RETURN_MESSAGE, rateOfReturn);
+    }
 }
