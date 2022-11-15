60f6fae04f0e88b5614bd58cbfb5865ff82196cd
hingjae <ljh23270@naver.com>
2022-11-15 11:35:40 +0900

    로또 번호 발급

--- src/main/java/lotto/domain/IssuedLotto.java
+++ src/main/java/lotto/domain/IssuedLotto.java
@@ -1,2 +1,15 @@
-package lotto.domain;public class IssuedLotto {
+package lotto.domain;
+
+import lotto.Lotto;
+
+public class IssuedLotto {
+    LottoList lottoList = new LottoList();
+
+    public IssuedLotto(int amount) {
+        for(int i = 0; i < amount; i++) {
+            Lotto lotto = new Lotto(LottoNumbers.generateNumber());
+            lottoList.addLottoList(lotto);
+        }
+        lottoList.printLottoList(amount);
+    }
 }
