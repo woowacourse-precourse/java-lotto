e1203764ce2ea8c41830c329389343f1fd4b9657
hyeongu <wancern@naver.com>
2022-11-15 23:32:18 +0900

    feat(ErrorMessage): 에러 메세지를 저장하는 클래스 생성

---
+++ src/main/java/lotto/ErrorMessage.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class ErrorMessage {
+    public static final String SIZE_ERROR = "[ERROR] 로또 번호의 개수가 6이 아닙니다.";
+    public static final String WRONG_INPUT_TYPE = "[ERROR] 입력 형식이 잘못되었습니다.";
+    public static final String WRONG_INPUT_MONEY = "[ERROR] 구입 금액은 1000의 배수여야 합니다.";
+    public static final String OUT_OF_RANGE = "[ERROR] 로또 번호의 범위는 1부터 45까지여야 합니다.";
+    public static final String NUMBER_DUPLICATE = "[ERROR] 중복된 숫자가 입력 되었습니다.";
+    public static final String BONUS_NUMBER_DUPLICATE = "[ERROR] 보너스 번호가 이미 로또 번호에 포함되어있습니다.";
+    public static final String CANNOT_FIND_RANK = "[ERROR] 해당하는 등수를 찾을 수 없습니다.";
+}
