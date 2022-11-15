d2a3e1d925d0bb90a44c6324fa4b6b1b63460deb
jijongkwon <help3451@naver.com>
2022-11-15 14:31:31 +0900

    test : 1000원 단위가 아닌 구매금액 입력시 예외 발생시키는 테스트 추가

---
+++ src/test/java/lotto/domain/PurchasingLottoListTest.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PurchasingLottoListTest {
+
+    @DisplayName("구입 금액이 1000원으로 나누어 떨어지지 않은면 예외가 발생한다.")
+    @Test
+    void moneyToTicket() {
+        assertThatThrownBy(() -> new PurchasingLottoList().moneyToTicket(13400))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
