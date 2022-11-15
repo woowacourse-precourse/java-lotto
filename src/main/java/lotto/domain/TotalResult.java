26fd8e1c90d7e49c13dfac591a04477e71be0c08
sonsoowon <yuwoo1018@gmail.com>
2022-11-15 16:02:14 +0900

    fix(TotalResult): 각 순위별 로또 개수(rankCounts)를 EnumMap 타입으로 저장하는 필드 추가
    - setRankCounts 메서드로 생성자 호출 시 필드 초기화
    - getYield 메서드의 매개변수 제거 및 반환 타입 double로 변경
    - 불필요한 필드 및 import 문 삭제

--- src/main/java/lotto/domain/TotalResult.java
+++ src/main/java/lotto/domain/TotalResult.java
@@ -1,67 +1,49 @@
 package lotto.domain;
 
-import java.text.DecimalFormat;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class TotalResult {
-    private static final String RANK_COUNT_FORMAT = " - %d개";
-    private static final int ZERO_COUNT = 0;
-    private static final DecimalFormat yieldFormat = new DecimalFormat("###,###.#");
+    private static final int DEFAULT_RANK_COUNT = 0;
     private final JackpotBonus jackpotBonus;
     private final List<Lotto> lotteries;
+    private final Map<Rank, Integer> rankCounts;
 
     public TotalResult(JackpotBonus jackpotBonus, List<Lotto> lotteries) {
         this.jackpotBonus = jackpotBonus;
         this.lotteries = lotteries;
+        this.rankCounts = setRankCounts(jackpotBonus, lotteries);
     }
 
-    public Map<Rank, Integer> getRankCounts() {
+    private static Map<Rank, Integer> setRankCounts(JackpotBonus jackpotBonus, List<Lotto> lotteries) {
         Map<Rank, Integer> rankCounts = new EnumMap<>(Rank.class);
 
         for (Lotto lotto : lotteries) {
             Rank rankOfLotto = jackpotBonus.getRankOf(lotto);
-            int currentCnt = rankCounts.getOrDefault(rankOfLotto, ZERO_COUNT);
+            int currentCnt = rankCounts.getOrDefault(rankOfLotto, DEFAULT_RANK_COUNT);
             rankCounts.put(rankOfLotto, currentCnt + 1);
         }
         return rankCounts;
     }
 
-    public List<String> getRankInfoWithCounts(Map<Rank, Integer> rankCounts) {
-        List<String> rankInfoWithCounts = new ArrayList<>();
-
-        List<Rank> reversedRanks = getReversedRanks();
-        for (Rank rank : reversedRanks) {
-            int count = rankCounts.getOrDefault(rank, ZERO_COUNT);
-            String line = rank.getInfo() + String.format(RANK_COUNT_FORMAT, count);
-            rankInfoWithCounts.add(line);
-        }
-        return rankInfoWithCounts;
+    public Map<Rank, Integer> getRankCounts() {
+        return rankCounts;
     }
 
-    public String getYield(Map<Rank, Integer> rankCounts) {
+    public double getYield() {
         if (lotteries.size() == 0) {
-            return yieldFormat.format(0);
+            return 0;
         }
 
         int sumOfRewards = Arrays.stream(Rank.values())
-                .mapToInt(rank -> rewardPerRank(rankCounts, rank))
+                .mapToInt(this::rewardPerRank)
                 .sum();
-        double yield = (double) sumOfRewards / (lotteries.size() * LottoIssuer.MONEY_UNIT) * 100;
 
-        return yieldFormat.format(yield);
+        return (double) sumOfRewards / (lotteries.size() * LottoIssuer.MONEY_UNIT) * 100;
     }
 
-    private static List<Rank> getReversedRanks() {
-        return Arrays.stream(Rank.values())
-                .filter(rank -> rank != Rank.NO_LUCK)
-                .sorted(Comparator.comparing(Rank::getRankNumber).reversed())
-                .collect(Collectors.toList());
-    }
-
-    private int rewardPerRank(Map<Rank, Integer> rankCounts, Rank rank) {
+    private int rewardPerRank(Rank rank) {
         int reward = rank.getReward();
-        int count = rankCounts.getOrDefault(rank, ZERO_COUNT);
+        int count = rankCounts.getOrDefault(rank, DEFAULT_RANK_COUNT);
 
         return reward * count;
     }
