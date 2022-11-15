1771ab32254ae36d5bc003085d73fc2d524d6eb3
조병우 <whquddn55@gmail.com>
2022-11-15 22:32:15 +0900

    refactor(Winning): WinningFormatter에 상수 도입

--- src/main/java/lotto/winning/WinningStatisticalResultKoreanFormatter.java
+++ src/main/java/lotto/winning/WinningStatisticalResultKoreanFormatter.java
@@ -5,13 +5,18 @@ import java.util.Arrays;
 import java.util.function.Predicate;
 
 public class WinningStatisticalResultKoreanFormatter implements WinningStatisticalFormatter {
+    public static final String HEADER = "당첨 통계\n---\n";
+    public static final String BONUS_BALL_SAME = ", 보너스 볼 일치";
+    public static final String WINNING_RESULT_FORMATTER = "%d개 일치%s(%s원) - %d개\n";
+    public static final String PROFIT_FORMATTER = "총 수익률은 %.1f%%입니다.";
+
     @Override
     public String format(WinningStatistical winningStatistical) {
         return getHeader() + getBody(winningStatistical) + getFooter(winningStatistical);
     }
 
     private String getHeader() {
-        return "당첨 통계\n" + "---\n";
+        return HEADER;
     }
 
     private String getBody(WinningStatistical winningStatistical) {
@@ -24,14 +29,14 @@ public class WinningStatisticalResultKoreanFormatter implements WinningStatistic
     private String formatWinningResult(WinningStatistical winningStatistical, WinningResult winningResult) {
         StringBuilder bonusMatchedMessage = new StringBuilder(" ");
         if (winningResult.getBonusMatched()) {
-            bonusMatchedMessage.insert(0, ", 보너스 볼 일치");
+            bonusMatchedMessage.insert(0, BONUS_BALL_SAME);
         }
-        return String.format("%d개 일치%s(%s원) - %d개\n", winningResult.getMatchedCount(), bonusMatchedMessage,
+        return String.format(WINNING_RESULT_FORMATTER, winningResult.getMatchedCount(), bonusMatchedMessage,
                 NumberFormat.getInstance().format(winningResult.getPrize()),
                 winningStatistical.getCountOfWinningResult(winningResult));
     }
 
     private String getFooter(WinningStatistical winningStatistical) {
-        return String.format("총 수익률은 %.1f%%입니다.", winningStatistical.calculateProfit());
+        return String.format(PROFIT_FORMATTER, winningStatistical.calculateProfit());
     }
 }
