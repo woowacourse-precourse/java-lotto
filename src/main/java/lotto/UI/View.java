aa8e1e37e9028b2e8b06212f34f949378ff179e9
Frog-Slayer <Frog-Slayer@github.com>
2022-11-15 14:14:29 +0900

    refactor(Text): WINNING_STATISTICS와 THREE_LINE을 합쳐 WINNING_STATISTICS_TITLE로 바꿈. feat(View): 추가 각 포맷에 맞는 출력 메서드 작성

--- src/main/java/lotto/UI/View.java
+++ src/main/java/lotto/UI/View.java
@@ -1,6 +1,32 @@
 package lotto.UI;
 
 public class View {
+    public void printBudgetInput(){
+        System.out.println(Text.BUDGET_INPUT.getMessage());
+    }
 
+    public void printButLotto(Integer lottoCount){
+        System.out.printf(Text.BUY_LOTTO.getMessage(), lottoCount);
+    }
+
+    public void printWinningNumberInput(){
+        System.out.println(Text.WINNING_NUMBER_INPUT.getMessage());
+    }
+
+    public void printBonusNumberInput(){
+        System.out.println(Text.BONUS_NUMBER_INPUT.getMessage());
+    }
+
+    public void printWinningStatisticsTitle(){
+        System.out.println(Text.WINNING_STATISTICS_TITLE.getMessage());
+    }
+
+    public void printWinningStatisticsDetail(Integer matchedNumberCount, String formattedAmount, Integer matchedLottoCount){
+        System.out.printf(Text.WINNING_STATISTICS_DETAIL.getMessage(), matchedNumberCount, formattedAmount, matchedLottoCount);
+    }
+
+    public void printProfitRate(Double profitRate){
+        System.out.printf(Text.PROFIT_RATE.getMessage(), profitRate);
+    }
 
 }
---
+++ src/test/java/lotto/UI/ViewTest.java
@@ -0,0 +1,35 @@
+package lotto.UI;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ViewTest {
+
+    @Test
+    void printBudgetInput() {
+    }
+
+    @Test
+    void printButLotto() {
+    }
+
+    @Test
+    void printWinningNumberInput() {
+    }
+
+    @Test
+    void printBonusNumberInput() {
+    }
+
+    @Test
+    void printWinningStatisticsTitle() {
+    }
+
+    @Test
+    void printWinningStatisticsDetail() {
+    }
+
+    @Test
+    void printProfitRate() {
+    }
+}
