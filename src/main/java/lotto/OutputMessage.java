b2866da8c258b0c9ab69a946b78cc8bc14b9d993
KimRiun <kimriun27@gmail.com>
2022-11-15 23:49:23 +0900

    feat: 출력 메세지 작성

--- src/main/java/lotto/OutputMessage.java
+++ src/main/java/lotto/OutputMessage.java
@@ -3,6 +3,11 @@ package lotto;
 public enum OutputMessage {
     PRICE("구입금액을 입력해 주세요."),
     AMOUNT("개를 구매했습니다."),
+    WIN("당첨 번호를 입력해 주세요."),
+    BONUS("보너스 번호를 입력해 주세요."),
+    STATISTICS("당첨 통계\n---");
+
+
 
     String message;
     private OutputMessage(String message) {
