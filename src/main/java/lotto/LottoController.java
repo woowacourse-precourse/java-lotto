cf0862f58d8c504f8685486b638e417000ffa8a3
ashlovesliitea <skyrider1811@gmail.com>
2022-11-15 22:14:49 +0900

    feat: Controller - 돈 입력하는 기능 구현

---
+++ src/main/java/lotto/LottoController.java
@@ -0,0 +1,49 @@
+package lotto;
+
+public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoFactory lottoFactory;
+
+    public LottoController(InputView inputView, OutputView outputView, LottoFactory lottoFactory) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoFactory = lottoFactory;
+    }
+
+    public void lottery(){
+        UserMoney money;
+        WinningNumbers winningNumbers;
+
+        try{
+            money=insertMoney();
+        }
+        catch (Exception e){
+            return;
+        }
+
+        try{
+            winningNumbers = pickWinningNumbers();
+        }
+        catch (Exception e){
+            return;
+        }
+
+        Ticket ticket = purchaseLottoTicket(money);
+        Result result = new Result(ticket.checkRankingCount(winningNumbers));
+        checkResult(money,result);
+
+    }
+
+
+
+
+
+    private UserMoney insertMoney(){
+        return new UserMoney(inputView.requestUserMoney());
+    }
+
+
+
+
+}
