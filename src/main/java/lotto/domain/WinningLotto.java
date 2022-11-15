a91eab63c1f03adfefcda9e50c44ba275a339384
ji-mango <susie000301@naver.com>
2022-11-15 21:12:03 +0900

    refactor: 메소드 분리, 숫자 하드코딩 상수로 변경

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -6,8 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WinningLotto {
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_END_NUMBER = 45;
+
     Lotto winningNumber = null;
     int bonusNumber;
+
+    public WinningLotto() {
+    }
+
+    public WinningLotto(Lotto winningNumber, int bonusNumber) {
+        this.winningNumber = winningNumber;
+        this.bonusNumber = bonusNumber;
+    }
 
     public Lotto getWinningNumber() {
         return winningNumber;
@@ -15,8 +26,15 @@ public class WinningLotto {
 
     public void receiveWinningNumber() {
         String input = Console.readLine();
-        String[] numbers = input.split(",");
+        String[] numbers = splitByComma(input);
+
         winningNumber = new Lotto(stringArrayToIntegerList(numbers));
+    }
+
+    public String[] splitByComma(String sentence) {
+        String[] numbers = sentence.split(",");
+
+        return numbers;
     }
 
     public void receiveBonusNumber() {
@@ -52,7 +70,7 @@ public class WinningLotto {
         return false;
     }
 
-    private List<Integer> stringArrayToIntegerList(String[] words) {
+    public List<Integer> stringArrayToIntegerList(String[] words) {
         List<Integer> result = new ArrayList<>();
 
         for (String w:words) {
@@ -65,28 +83,28 @@ public class WinningLotto {
     }
 
 
-    private void judgeIsDigitOfString(String sentence) {
+    public void judgeIsDigitOfString(String sentence) {
         for (int i = 0; i < sentence.length(); i++) {
             char l = sentence.charAt(i);
             judgeIsDigitOfChar(l);
         }
     }
 
-    private void judgeIsDigitOfChar(char word) {
+    public void judgeIsDigitOfChar(char word) {
         if (!Character.isDigit(word)) {
             throw new IllegalArgumentException("[ERROR] 입력은 자연수만 가능합니다.");
         }
     }
 
-    private void checkRangeOfNumber(String word) {
+    public void checkRangeOfNumber(String word) {
         int number = Integer.parseInt(word);
 
-        if (number < 1 || number > 45) {
+        if (number < LOTTO_START_NUMBER || number > LOTTO_END_NUMBER) {
             throw new IllegalArgumentException("[ERROR] 입력은 1~45사이의 자연수만 가능합니다.");
         }
     }
 
-    private void checkBonusInWinning() {
+    public void checkBonusInWinning() {
         if(winningNumber.getNumbers().contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 다른 번호를 입력해야 합니다.");
         }
