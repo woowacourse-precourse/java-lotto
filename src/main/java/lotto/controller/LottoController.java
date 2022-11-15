649aac8db0eabdc618bbf34ae3d006eb1a4b3723
core <jinjs6@gmail.com>
2022-11-15 11:41:06 +0900

    feat(lotto): 로또 금액 입력 검증 기능 내용 추가
    입렵 받은 금액의 문자열의 길이가 4이상인지 확인

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,9 +1,18 @@
 package lotto.controller;
 
+import lotto.view.Input;
+
 public class LottoController {
+    private static int price;
 
     //로또 프로그램 실행 메서드
     public static void execute() {
+        //금액 입력 받기
+        LottoController.getPrice();
+        //입력한 금액에 대한 로또 개수 구하기
+    }
 
+    public static void getPrice() {
+        price = Input.inputPrice();
     }
 }
--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -9,20 +9,24 @@ public class Input {
     }
 
     private static void validatePrice(final String price) throws IllegalArgumentException {
+        if (price.length() < 4) {
+            throw new IllegalArgumentException(ErrorMessage.GET_ERROR_FOR_PRICE.getMessage());
+        }
         for (int i = 0; i < price.length(); ++i) {
-            if (!('0' <= price.charAt(i) && price.charAt(i) <= '9') ||
-                    !(i == 0 && '1' <= price.charAt(i) && price.charAt(i) <= '9')) {
+            if (!('0' <= price.charAt(i) && price.charAt(i) <= '9')) {
                 throw new IllegalArgumentException(ErrorMessage.GET_ERROR_FOR_PRICE.getMessage());
             }
         }
-        if (Integer.parseInt(price) % 1000 != 0) {
+        if (price.charAt(0) == '0' || Integer.parseInt(price) % 1000 != 0) {
             throw new IllegalArgumentException(ErrorMessage.GET_ERROR_FOR_PRICE.getMessage());
         }
     }
 
-    private static int getIntPrice() {
+    public static int getIntPrice() {
         String price = Console.readLine();
         validatePrice(price);
         return Integer.parseInt(price);
     }
+
+
 }
