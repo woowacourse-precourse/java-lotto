878ddfba267e55e463c98fc00abf76dd504b734d
saehim <saeilee.vxv@gmail.com>
2022-11-15 23:01:05 +0900

    feat(lotto) : 메인 Application 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,14 @@
 package lotto;
 
+import lotto.controller.Result;
 import lotto.controller.Generator;
 import lotto.domain.Lotto;
+import lotto.domain.Win;
 import lotto.view.Input;
 import lotto.view.Messages;
 
 import java.util.List;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
@@ -18,5 +21,13 @@ public class Application {
 
         System.out.println(Messages.PRINT_LOTTOS(lottos));
 
+        Lotto winLotto = Input.askWinLotto();
+
+        int bonusNum = Input.askBonusNum(winLotto);
+
+        Map<Win, Integer> countWins = Result.countWins(winLotto,bonusNum,lottos);
+
+        double winningsRate = Result.winningsRate(Money, countWins);
+        System.out.println(Messages.PRINT_RESULT(countWins,winningsRate));
     }
 }
