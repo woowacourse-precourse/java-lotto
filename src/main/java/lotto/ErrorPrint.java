93bcca9cdedf9fa00feaa676ca90fd04d5109704
chesthyeon <niw927@naver.com>
2022-11-15 15:33:41 +0900

    ErrorPrint 보너스 넘버가 포함되어 있는지 확인 하는 메시지 추가

--- src/main/java/lotto/ErrorPrint.java
+++ src/main/java/lotto/ErrorPrint.java
@@ -7,8 +7,8 @@ public enum ErrorPrint {
     bonusNumber("[ERROR] 1~45 사이의 숫자로 이루어져 있어야 합니다."),
     lottoSize("[ERROR] 로또의 길이가 6이 아닙니다"),
     lottoOverlap("[ERROR] 중복된 숫자가 포함되어 있습니다."),
-    lottoNumber("[ERROR] 로또는 1~45의 숫자로 이루어져 있어야 합니다.");
-
+    lottoNumber("[ERROR] 로또는 1~45의 숫자로 이루어져 있어야 합니다."),
+    bonusNumberContain("[ERROR] 보너스 번호가 이미 당첨 로또에 포함되어 있습니다.");
     private final String errorMessage;
 
     ErrorPrint(String errorMessage){
