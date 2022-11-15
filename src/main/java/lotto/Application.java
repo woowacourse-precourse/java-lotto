90249728cc4302a1d90f4c957373d4114aac304a
HangSub999 <gkdtjq999@gmail.com>
2022-11-15 19:59:58 +0900

    feat: mian 배치

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoFactory lottoFactory = new LottoFactory();
+        LottoTicket lottoTicket = new LottoTicket();
+        InputOutput inputOutput = new InputOutput();
+
+        lottoFactory.setMoney(inputOutput.getMoneyInput());
+        lottoFactory.setLottoCount();
+
+        List<Lotto> buyerLottos = lottoTicket.createManyLotto(lottoFactory.getLottoCount());
+        inputOutput.printLottoCount(lottoFactory.getLottoCount());
+        inputOutput.printLotto(buyerLottos);
+        lottoFactory.setLottos(buyerLottos);
+
+        List<Integer> winnigLotto = inputOutput.getWinnigLotto();
+        int bonus = inputOutput.getBonusNumber(winnigLotto);
+
+        lottoTicket.setWinnigLotto(winnigLotto);
+        lottoTicket.setBonus(bonus);
+
+        Map<LottoRanking, Integer> lottoRanking = lottoTicket.compareLotto(lottoFactory.getLottos());
+        inputOutput.printWinningList(lottoRanking);
+
+        lottoFactory.setWinmoney(lottoRanking);
+        inputOutput.printWinningRate(lottoFactory.getWinMoney(), lottoFactory.getMoney());
+
     }
 }
