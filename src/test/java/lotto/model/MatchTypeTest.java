f090ddc051819f4790d2d3a3c89a5ca1047348f8
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-15 19:17:48 +0900

    test<MatchTypeTest>: 일치하는 숫자 개수가 3~6개가 아닐 경우 null 생성 테스트 케이스 작성

--- src/test/java/lotto/model/MatchTypeTest.java
+++ src/test/java/lotto/model/MatchTypeTest.java
@@ -15,4 +15,16 @@ class MatchTypeTest {
         assertThat(MatchType.fromNumberMatched(5.5)).isEqualTo(MatchType.FIVE_BONUS);
         assertThat(MatchType.fromNumberMatched(6.0)).isEqualTo(MatchType.SIX);
     }
+
+    @DisplayName("일치하는 숫자 개수가 3개 미만이면 null 이 생성되어야 한다.")
+    @Test
+    void createMatchTypeByNumberMatchedUnderThree() {
+        assertThat(MatchType.fromNumberMatched(2.0)).isEqualTo(null);
+    }
+
+    @DisplayName("일치하는 숫자 개수가 6개 초과면 null 이 생성되어야 한다.")
+    @Test
+    void createMatchTypeByNumberMatchedOverSix() {
+        assertThat(MatchType.fromNumberMatched(7.0)).isEqualTo(null);
+    }
 }
