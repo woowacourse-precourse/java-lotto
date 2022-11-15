6a3f9e08a84a0631a262e8ca7b70aeb984b1412e
mgOh <issac_oh@naver.com>
2022-11-15 18:10:29 +0900

    feat: 예외 테스트 통과를 위한 에러 전파 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -13,9 +13,14 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        ArrayList<Lotto> lottoList = inputPrice.createLottoNumbers();
+        ArrayList<Lotto> lottoList = getLottoList();
         List<Integer> winningNumbers = inputWinningNumbers.getWinningNumbers();
         int bonusNumber = inputWinningNumbers.getBonusNumbers(winningNumbers);
         checkWinningHistory.checkNumberMatching(lottoList, winningNumbers, bonusNumber);
+    }
+    private static ArrayList<Lotto> getLottoList() {
+        try {return inputPrice.createLottoNumbers();}
+        catch (Exception e) {System.out.println(e.getMessage());}
+        return null;
     }
 }
