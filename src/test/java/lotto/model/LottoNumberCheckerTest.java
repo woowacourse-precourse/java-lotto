d1943eda0645ce2fd28b92081888fb11a0ae6385
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-15 19:50:42 +0900

    test<LottoNumberCheckerTest>: 모든 등수 1개씩 당첨했을 때 당첨금과 당첨 개수 비교 테스트 작성

--- src/test/java/lotto/model/LottoNumberCheckerTest.java
+++ src/test/java/lotto/model/LottoNumberCheckerTest.java
@@ -16,7 +16,7 @@ class LottoNumberCheckerTest {
                 .collect(Collectors.toSet());
     }
 
-    @DisplayName("3등과 5등 보너스 당첨")
+    @DisplayName("3등과 5등 보너스 당첨했을 때 당첨금과 당첨 개수 비교")
     @Test
     void createMatchResult() {
         Lotto winningLotto = new Lotto(makeLotto(1, 2, 3, 4, 5, 6));
@@ -27,8 +27,39 @@ class LottoNumberCheckerTest {
         );
         LottoNumberChecker lottoNumberChecker = new LottoNumberChecker(winningLotto, bonusNumber, lotteries);
 
-        MatchResult matchingLottoResult = lottoNumberChecker.resultOfLotteries();
-        assertThat(matchingLottoResult.getTotalPrizeMoney())
+        MatchResult matchResult = lottoNumberChecker.resultOfLotteries();
+        assertThat(matchResult.getTotalPrizeMoney())
                 .isEqualTo(MatchType.THREE.getPrizeMoney() + MatchType.FIVE_BONUS.getPrizeMoney());
+        assertThat(matchResult.getResultByMatchType(MatchType.THREE)).isEqualTo(1);
+        assertThat(matchResult.getResultByMatchType(MatchType.FOUR)).isEqualTo(0);
+        assertThat(matchResult.getResultByMatchType(MatchType.FIVE)).isEqualTo(0);
+        assertThat(matchResult.getResultByMatchType(MatchType.FIVE_BONUS)).isEqualTo(1);
+        assertThat(matchResult.getResultByMatchType(MatchType.SIX)).isEqualTo(0);
+    }
+
+    @DisplayName("모든 등수 1개씩 당첨했을 때 당첨금과 당첨 개수 비교")
+    @Test
+    void createMatchResult2() {
+        Lotto winningLotto = new Lotto(makeLotto(1, 2, 3, 4, 5, 6));
+        LottoNumber bonusNumber = new LottoNumber(7);
+        List<Lotto> lotteries = List.of(
+                new Lotto(makeLotto(1, 2, 3, 7, 8, 9)),
+                new Lotto(makeLotto(1, 2, 3, 4, 8, 9)),
+                new Lotto(makeLotto(1, 2, 3, 4, 5, 8)),
+                new Lotto(makeLotto(1, 2, 3, 4, 5, 7)),
+                new Lotto(makeLotto(1, 2, 3, 4, 5, 6))
+        );
+        LottoNumberChecker lottoNumberChecker = new LottoNumberChecker(winningLotto, bonusNumber, lotteries);
+
+        MatchResult matchResult = lottoNumberChecker.resultOfLotteries();
+        assertThat(matchResult.getTotalPrizeMoney())
+                .isEqualTo(Arrays.stream(MatchType.values())
+                        .mapToInt(MatchType::getPrizeMoney)
+                        .sum());
+        assertThat(matchResult.getResultByMatchType(MatchType.THREE)).isEqualTo(1);
+        assertThat(matchResult.getResultByMatchType(MatchType.FOUR)).isEqualTo(1);
+        assertThat(matchResult.getResultByMatchType(MatchType.FIVE)).isEqualTo(1);
+        assertThat(matchResult.getResultByMatchType(MatchType.FIVE_BONUS)).isEqualTo(1);
+        assertThat(matchResult.getResultByMatchType(MatchType.SIX)).isEqualTo(1);
     }
 }
