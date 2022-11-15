e329915f8f1083b67214752160d879c6f7708cf9
Sieun Park <psieun0606@gmail.com>
2022-11-15 18:57:00 +0900

    feat: 등수 별 수익금과 횟수 저장하는 기능 추가

---
+++ src/main/java/lotto/Domain/MatchWinning.java
@@ -0,0 +1,35 @@
+package lotto.Domain;
+
+public class MatchWinning {
+
+    WinningPrice winningPrice;
+    private int profit;
+    private int firstPrice = 0;
+    private int secondPrice = 0;
+    private int thirdPrice = 0;
+    private int forthPrice = 0;
+    private int fifthPrice = 0;
+
+    public void matchThePrice(int theNumberOfWins, int winningBonus) {
+        if (theNumberOfWins == 6) {
+            profit += winningPrice.FIRST.getPrizeMoney();
+            firstPrice ++;
+        }
+        if (theNumberOfWins == 5 && winningBonus == 1) {
+            profit += winningPrice.SECOND.getPrizeMoney();
+            secondPrice++;
+        }
+        if (theNumberOfWins == 5 && winningBonus == 0) {
+            profit += winningPrice.THIRD.getPrizeMoney();
+            thirdPrice++;
+        }
+        if (theNumberOfWins == 4) {
+            profit += winningPrice.FORTH.getPrizeMoney();
+            forthPrice++;
+        }
+        if (theNumberOfWins == 3) {
+            profit += winningPrice.FIFTH.getPrizeMoney();
+            fifthPrice++;
+        }
+    }
+}
