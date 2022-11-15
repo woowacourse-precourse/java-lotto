e31e00514c9e635fc760c1d8b1ad0f127f0e734e
footdev <ekclstkfka5@gmail.com>
2022-11-15 21:44:59 +0900

    feat: 로또 게임 구현
    로또 게임을 구현함.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.controller.LottoGame;
+import lotto.domain.Result;
+import lotto.domain.User;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoGame game = new LottoGame(new User(), new Result());
+        game.run();
     }
 }
