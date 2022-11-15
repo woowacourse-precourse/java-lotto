ca361a0b9284df7aa9b8dcfab5e372c45959fbe2
masiljangajji <hjk172262@gmail.com>
2022-11-15 23:33:56 +0900

    feat: 당첨 번호의 숫자 범위 검사 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,20 +6,29 @@ import view.Message;
 public class Lotto {
     private final List<Integer> numbers;
     public Lotto(List<Integer> numbers) {
-        checkNumberRange(numbers);
+        checkNumberSize(numbers);
         checkNumberOverLap(numbers);
+        checkNumberRange(numbers);
         this.numbers = numbers;
     }
 
-    private void checkNumberRange(List<Integer> numbers)  {
+    private void checkNumberSize(List<Integer> numbers)  {
             if (numbers.size() != 6) {
-                throw new IllegalArgumentException(Message.ERRROR_NUMBER_RANGE.getMessage());
+                throw new IllegalArgumentException(Message.ERRROR_LOTTO_SIZE.getMessage());
             }
     }
 
     private void checkNumberOverLap(List<Integer>numbers){
         if(numbers.size() != numbers.stream().distinct().count()){
             throw new IllegalArgumentException(Message.ERROR_NUMBER_OVERLAP.getMessage());
+        }
+    }
+
+    private void checkNumberRange(List<Integer>numbers){
+        for(int i=0;i<numbers.size();i++){
+            if(!(numbers.get(i)>=1&&numbers.get(i)<=45)){
+                throw new IllegalArgumentException(Message.ERRROR_NUMBER_RANGE.getMessage());
+            }
         }
     }
 
