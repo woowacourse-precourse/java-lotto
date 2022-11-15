4bfafb76b30738cd12a36f8001fd7244c9f86995
KimJaeSung <behappy0916@naver.com>
2022-11-15 11:07:58 +0900

    feat: 아웃풋을 책임지는 OutPutView 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,30 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoTickets;
+import lotto.domain.Money;
+import lotto.domain.RankingResult;
+
 public class OutputView {
-    private static final String PRINT_LOTTO_COUNT = "%d개를 구매했습니다.";
+    private static final String PRINT_LOTTO_COUNT = "\n%d개를 구매했습니다.\n";
     public static void printPurchaseLotto(int lottoCount){
         System.out.printf(PRINT_LOTTO_COUNT,lottoCount);
+    }
+
+    public static void printResult(RankingResult rankingResult, Money money){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println(rankingResult.result()+"\n");
+        double profit = rankingResult.getProfit(money);
+
+        System.out.print("총 수익률은 " + profit + "%입니다.");
+    }
+
+    public static void printLottoPaper(LottoTickets lottoTickets){
+        List<List<Integer>> Tickets = lottoTickets.getLottoTicket();
+        for(int i=0; i<Tickets.size(); i++){
+            System.out.println(Tickets.get(i));
+        }
     }
 }
