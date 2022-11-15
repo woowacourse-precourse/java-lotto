d707decbfdc35fda8ecbc1f288d5f0e459efad57
kimdonghun <zhfptm12@naver.com>
2022-11-15 16:14:58 +0900

    feat : getter

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,9 +12,12 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 총 6개이어야 합니다.");
         }
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
     // TODO: 추가 기능 구현
 }
