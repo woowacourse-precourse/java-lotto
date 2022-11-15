3ebfd4f65fadf2f010d92d2e9ab31fc004e9fc73
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2022-11-15 11:33:08 +0900

    feat(InputView): 사용자로부터 입력을 받아오는 InputView 클래스 구현
    구매 금액을 입력받아오는 메소드 구현
    당첨 번호 입력받아오는 메소드 구현
    보너스 번호 입력받아오는 메소드 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,38 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+    private static final String DIVISION_CHAR = ",";
+    private static final Integer PURCHASE_UNIT = 1000;
+
+    public Integer purchaseAmount(){
+        System.out.println("구입 금액을 입력해 주세요.");
+        Integer input = Integer.parseInt(Console.readLine());
+        if (input % PURCHASE_UNIT > 0 ){
+            throw new IllegalArgumentException("1000원 단위로만 구매 가능합니다.");
+        }
+        return input/PURCHASE_UNIT;
+    }
+
+    public List<Integer> getWinningNumbers(){
+        System.out.println("당첨 번호를 입력해 주세요");
+        String input = Console.readLine();
+        return Arrays.stream(input.split(DIVISION_CHAR))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+    //예외 처리 필요합니다.
+
+    public int getBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요");
+        return Integer.parseInt(Console.readLine());
+    }
+    // 예외 처리 필요합니다.
+
+
+}
