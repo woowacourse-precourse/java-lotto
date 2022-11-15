381b9d4812ad27a1c530aa59617c103ebf84ca92
anso33 <hyeon131812@gmail.com>
2022-11-15 15:14:20 +0900

    [FEAT](constantValue): add constant int

--- src/main/java/constantValue/LottoRank.java
+++ src/main/java/constantValue/LottoRank.java
@@ -1,2 +1,8 @@
-package constantValue;public enum LottoRank {
+package constantValue;
+
+//unused
+public enum LottoRank {
+  NO_PRIZE_0, NO_PRIZE_1, NO_PRIZE_2, // 0 , 1, 2
+  FIFTH_PLACE, FOURTH_PLACE,          // 3, 4, 5
+  THIRD_PLACE, FIRST_PLACE, SECOND_PLACE // 6, 7
 }
--- src/main/java/constantValue/LottoUtilNumber.java
+++ src/main/java/constantValue/LottoUtilNumber.java
@@ -1,2 +1,7 @@
-package constantValue;public class LottoUtilNumber {
+package constantValue;
+
+public class LottoUtilNumber {
+  public static final int UNDER_RANGE = 1;
+  public static final int OVER_RANGE = 45;
+  public static final int LOTTO_SIZE = 6;
 }
