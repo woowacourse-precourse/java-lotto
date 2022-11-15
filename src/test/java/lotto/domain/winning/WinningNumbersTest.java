f99fafa4e1ad50f4601c87628562af0c8f1c7a51
wonjunYou <youwjune@gmail.com>
2022-11-15 20:46:19 +0900

    feat(WinningNumbers) : 당첨 번호 생성 기능 구현

--- src/test/java/lotto/domain/winning/WinningNumbersTest.java
+++ src/test/java/lotto/domain/winning/WinningNumbersTest.java
@@ -1,4 +1,30 @@
 package lotto.domain.winning;
 
+import lotto.domain.lotto.LottoNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class WinningNumbersTest {
+
+    @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createWinningNumbersByOverSize() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7), 10))
+                .withMessage("[ERROR] 당첨 번호의 총 개수가 6이 아닙니다.");
+    }
+
+    @DisplayName("당첨 번호의 개수가 6개 미만이면 예외가 발생한다.")
+    @Test
+    void createWinningNumbersByLessSize() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5), 10))
+                .withMessage("[ERROR] 당첨 번호의 총 개수가 6이 아닙니다.");
+    }
+
+
 }
