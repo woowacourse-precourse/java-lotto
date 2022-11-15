dcc5c5a92ff04f585c26ae9eaa6e70201ee2aa29
mihye126 <mihea12@gmail.com>
2022-11-15 22:40:39 +0900

    [feat] 생성자 팩토리 추가

--- src/main/java/lotto/controller/LottoBroadcast.java
+++ src/main/java/lotto/controller/LottoBroadcast.java
@@ -15,6 +15,11 @@ public class LottoBroadcast {
         drawWinning();
         drawBonus();
     }
+
+    public static LottoBroadcast onAir() {
+        return new LottoBroadcast();
+    }
+
     private void drawWinning() {
         this.winning = new Lotto(getWinningInput());
     }
