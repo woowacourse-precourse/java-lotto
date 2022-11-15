576a0705c1faec488509c9921e85533381346b9b
aud19 <aud19980518@gmail.com>
2022-11-15 20:04:25 +0900

    테스트 코드 작성

---
+++ src/test/java/lotto/BuyLottoTest.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import lotto.domain.vo.BuyLotto;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class BuyLottoTest {
+
+    @DisplayName("money 값에 들어가는지 테스트")
+    @Test
+    void createMoney() {
+        BuyLotto buyLotto = new BuyLotto("5000");
+        AssertionsForClassTypes.assertThat(buyLotto.getMoney()).isEqualTo(5000);
+    }
+
+    @DisplayName("1000원 단위의 돈이 아닐 경우 exception 발생 테스트")
+    @Test
+    void createNotMoneyUnit() {
+        assertThatThrownBy(() -> new BuyLotto("5500"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("문자가 들어갈 경우 exception 발생 테스트")
+    @Test
+    void createNotNumber() {
+        assertThatThrownBy(() -> new BuyLotto("5500k"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
