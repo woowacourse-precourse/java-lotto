eaea2f74ec8f6f7fbeaccbc64edc88a51f5bd299
sujin0607 <goweg0607@gmail.com>
2022-11-15 23:05:26 +0900

    refactor: 문자열과 숫자 상수로 수정

--- src/main/java/lotto/domain/LottoManager.java
+++ src/main/java/lotto/domain/LottoManager.java
@@ -8,6 +8,13 @@ import java.util.*;
  * 로또의 갯수와 랜덤수를 구하는 클래스
  */
 public class LottoManager {
+    private static final String ERROR = "[ERROR]";
+    private static final String ENTERNUMBER = "숫자를 입력해주세요.";
+    private static final String RESTNUMBER = "나머지가 있습니다.";
+    private static final String NOTENOUGHMONEY = "금액이 부족합니다.";
+    private static final int LOTTOSTARTNUMBER = 1;
+    private static final int LOTTOLASTNUMBER = 45;
+    private static final int LOTTOCOUNT = 6;
     public LottoManager() {
     }
 
@@ -20,21 +27,21 @@ public class LottoManager {
 
     public int isNumberic(String inputMoney) {
         if (inputMoney == null || inputMoney.matches("[-+]?\\d*\\.?\\d+") == false) {
-            System.out.println("[ERROR] 숫자로 입력해주세요");
-            throw new NoSuchElementException("[ERROR] 숫자로 입력해주세요");
+            System.out.println(ERROR + ENTERNUMBER);
+            throw new NoSuchElementException(ERROR + ENTERNUMBER);
         }
         return Integer.parseInt(inputMoney);
     }
 
     public void hasRestError(int money) {
         if (money % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 나누어 떨어지지 않습니다");
+            throw new IllegalArgumentException(ERROR + RESTNUMBER);
         }
     }
 
     public void MinimumAmountError(int money) {
         if (money < 1000) {
-            throw new IllegalArgumentException("[ERROR] 로또를 살 수 없는 금액입니다.");
+            throw new IllegalArgumentException(ERROR + NOTENOUGHMONEY);
         }
     }
 
@@ -53,7 +60,7 @@ public class LottoManager {
     }
 
     public List<Integer> makeRandomNumber() {
-        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(LOTTOSTARTNUMBER, LOTTOLASTNUMBER, LOTTOCOUNT));
         return numbers;
     }
 
