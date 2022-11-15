ce75dd5a1af8512d29ca0b1ea1c264dfba9744d9
Devtist <yt930310@snu.ac.kr>
2022-11-15 16:38:50 +0900

    refactor: 보너스 넘버를 사용한 등수를 결정하는 로직 변경

--- src/main/java/lotto/constant/WinningRating.java
+++ src/main/java/lotto/constant/WinningRating.java
@@ -1,19 +1,21 @@
 package lotto.constant;
 
 public enum WinningRating {
-	MISS(0,"0원"),
-	FIFTH(3, "5,000원"),
-	FOURTH(4, "50,000원"),
-	THIRD(5, "1,500,000원"),
-	SECOND(5, "30,000,000원"),
-	FIRST(6, "2,000,000,000원");
+	MISS(0,"0원", false),
+	FIFTH(3, "5,000원", false),
+	FOURTH(4, "50,000원", false),
+	THIRD(5, "1,500,000원", false),
+	SECOND(5, "30,000,000원", true),
+	FIRST(6, "2,000,000,000원", false);
 
 	private final int countOfMatchedNumber;
 	private final String prizeMoney;
+	private final boolean isUsingBonusNumber;
 
-	WinningRating(int countOfMatchedNumber, String prizeMoney) {
+	WinningRating(int countOfMatchedNumber, String prizeMoney, boolean isUsingBonusNumber) {
 		this.countOfMatchedNumber = countOfMatchedNumber;
 		this.prizeMoney = prizeMoney;
+		this.isUsingBonusNumber = isUsingBonusNumber;
 	}
 
 	public int getCountOfMatchedNumber() {
@@ -22,5 +24,8 @@ public enum WinningRating {
 
 	public String getPrizeMoney() {
 		return prizeMoney;
+	}
+	public boolean getIsUsingBonusNumber() {
+		return isUsingBonusNumber;
 	}
 }
--- src/main/java/lotto/domain/LottoComparator.java
+++ src/main/java/lotto/domain/LottoComparator.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -19,45 +18,24 @@ public class LottoComparator {
 	}
 
 	public void compare(Lotto lotto) {
-		List<Integer> winningNumbersWithBonusNumber = new ArrayList<>();
-
-		addBonusNumberToWinningNumbers(winningNumbersWithBonusNumber);
+		boolean hasBonusNumber = checkHavingBonusNumber(lotto);
 		int countOfMatchedNumber = calculateCountOfMatchedNumber(lotto.getNumbers());
-		rateWinning(lotto, winningNumbersWithBonusNumber, countOfMatchedNumber);
+		rateWinning(countOfMatchedNumber, hasBonusNumber);
 	}
 
-	private void addBonusNumberToWinningNumbers(List<Integer> winningNumbersWithBonusNumber) {
-		winningNumbersWithBonusNumber.addAll(winningNumbers.getWinningNumbers());
-		winningNumbersWithBonusNumber.add(bonusNumber.getBonusNumber());
+	private boolean checkHavingBonusNumber(Lotto lotto) {
+		return lotto.getNumbers().contains(bonusNumber.getBonusNumber());
 	}
 
 	private int calculateCountOfMatchedNumber(List<Integer> numbers) {
 		return (int)numbers.stream().filter(winningNumbers.getWinningNumbers()::contains).count();
 	}
 
-	private void rateWinning(Lotto lotto, List<Integer> winningNumbersWithBonusNumber, int countOfMatchedNumber) {
-		if (countOfMatchedNumber >= WinningRating.FIFTH.getCountOfMatchedNumber()
-			&& countOfMatchedNumber != WinningRating.SECOND.getCountOfMatchedNumber()) {
-			rateNotSecondAndThirdWinning(countOfMatchedNumber);
-		}
-		if (countOfMatchedNumber == WinningRating.SECOND.getCountOfMatchedNumber()) {
-			rateSecondOrThirdWinning(lotto, winningNumbersWithBonusNumber);
-		}
-	}
-
-	private void rateSecondOrThirdWinning(Lotto lotto, List<Integer> winningNumbersWithBonusNumber) {
-		winningRating = WinningRating.THIRD;
-		if (new HashSet<>(winningNumbersWithBonusNumber).containsAll(lotto.getNumbers())) {
-			winningRating = WinningRating.SECOND;
-		}
-	}
-
-	private void rateNotSecondAndThirdWinning(int countOfMatchedNumber) {
+	private void rateWinning(int countOfMatchedNumber, boolean hasBonusNumber) {
 		Arrays.stream(WinningRating.values())
-			.filter(rating -> rating != WinningRating.SECOND)
-			.collect(Collectors.toList())
 			.forEach(rating -> {
-				if (rating.getCountOfMatchedNumber() == countOfMatchedNumber)
+				if (rating.getCountOfMatchedNumber() == countOfMatchedNumber
+					&& rating.getIsUsingBonusNumber() == hasBonusNumber)
 					winningRating = rating;
 			});
 	}
