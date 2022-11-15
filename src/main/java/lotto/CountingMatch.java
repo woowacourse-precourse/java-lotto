df055a5a3c501ca2b0d4df710b19550005f0cea4
jw0293 <k38836919@gmail.com>
2022-11-15 18:38:17 +0900

    feat : Counting Same Number and Bonus

--- src/main/java/lotto/CountingMatch.java
+++ src/main/java/lotto/CountingMatch.java
@@ -1,2 +1,28 @@
-package lotto;public class CountingMatch {
+package lotto;
+
+public class CountingMatch {
+
+    private int sameCnt;
+    private boolean bonusSame;
+
+    CountingMatch(){
+        sameCnt = 0;
+        bonusSame = false;
+    }
+
+    public void addSameCnt(){
+        sameCnt++;
+    }
+
+    public void sameBonus(){
+        bonusSame = true;
+    }
+
+    public int getSameCnt(){
+        return sameCnt;
+    }
+
+    public boolean isBonusSame(){
+        return bonusSame;
+    }
 }
