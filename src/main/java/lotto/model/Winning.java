6b4d4511810b3aef24081b9cce02665a2c0ec757
UJ15 <dbwogml15@naver.com>
2022-11-15 12:53:34 +0900

    refactor : add values field, findByCountAndBonus method

--- src/main/java/lotto/model/Winning.java
+++ src/main/java/lotto/model/Winning.java
@@ -1,19 +1,32 @@
 package lotto.model;
 
-public enum Winning {
-    THREE(5_000),
-    FOUR(50_000),
-    FIVE(1_500_000),
-    FIVE_BONUS(30_000_000),
-    SIX(2_000_000_000);
+import java.util.Arrays;
+import java.util.Optional;
 
+public enum Winning {
+    THREE(3, false, 5_000),
+    FOUR(4, false, 50_000),
+    FIVE(5, false, 1_500_000),
+    FIVE_BONUS(5, true, 30_000_000),
+    SIX(6, false, 2_000_000_000);
+
+    private final int correctCount;
+    private final boolean bonus;
     private final long winningPrice;
 
-    Winning(long winningPrice) {
+    Winning(int correctCount, boolean bonus, long winningPrice) {
+        this.correctCount = correctCount;
+        this.bonus = bonus;
         this.winningPrice = winningPrice;
     }
 
     public long getWinningPrice() {
         return winningPrice;
+    }
+
+    public static Optional<Winning> findByCountAndBonus(int correctCount, boolean bonus) {
+        return Arrays.stream(Winning.values())
+                .filter(w -> w.correctCount == correctCount && w.bonus == bonus)
+                .findFirst();
     }
 }
