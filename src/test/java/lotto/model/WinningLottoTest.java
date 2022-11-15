e935d4dd5ed83b0175693ba5fc64c4cd6700e400
chanwonlee <clw8679@naver.com>
2022-11-15 21:08:57 +0900

    test: WinningLotto 테스트 추가

--- src/test/java/lotto/model/WinningLottoTest.java
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -14,4 +14,18 @@ class WinningLottoTest {
         assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6), new BonusNumber(6)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("당첨 번호들이 중복되면 예외생 발생한다.")
+    @Test
+    void createWinningNumbersByDuplicatedNumber() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 5), new BonusNumber(6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호가 1~45 사이의 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void createWinningNumbersByOverRange() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 46), new BonusNumber(6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
