61a40bac97d719c26c7036caf10dbdd3e1c1bdce
bradbrad97 <taehyun0121@naver.com>
2022-11-15 12:27:54 +0900

    fix(LottoPurchaseUI): 구매 개수 출력하도록 수정

--- src/main/java/lotto/view/LottoPurchaseUI.java
+++ src/main/java/lotto/view/LottoPurchaseUI.java
@@ -8,10 +8,13 @@ public class LottoPurchaseUI {
     public String takePurchaseAmount(){
         System.out.println("구입금액을 입력해 주세요.");
         String purchaseAmountInput = readLine();
+        System.out.println();
         return purchaseAmountInput;
     }
 
     public void showLottoNumbers(List<List<Integer>> userLottos){
+        System.out.println(String.format("%d개를 구매했습니다.", userLottos.size()));
         userLottos.forEach(System.out::println);
+        System.out.println();
     }
 }
