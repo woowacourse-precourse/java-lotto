3eb2a63e839f4c7c92e373b69c78c7595ea31c6e
dvlpr0 <rladjswkd@naver.com>
2022-11-15 21:07:29 +0900

    fix: 출력 양식에 맞게 출력할 메시지 변경 및 인덱스 범위 문제 해결

--- src/main/java/lotto/UserInterface.java
+++ src/main/java/lotto/UserInterface.java
@@ -12,9 +12,10 @@ public class UserInterface {
 
     public static void writeLotteryTickets(BoughtTicketsDTO boughtTickets) {
         List<Lotto> tickets = boughtTickets.getTickets();
+        String format = InformMessage.PURCHASE_COUNT
+                .toString();
 
-        System.out.println(tickets.size() + InformMessage.WINNING_NUMBER
-                .toString());
+        System.out.printf(format, tickets.size());
         for (Lotto ticket : tickets) {
             System.out.println(ticket.getSortedString());
         }
@@ -25,7 +26,7 @@ public class UserInterface {
         String format = InformMessage.RESULT
                 .toString();
 
-        System.out.printf(format, counts[0], counts[1], counts[2], counts[3], counts[4], counts[5]);
+        System.out.printf(format, counts[0], counts[1], counts[2], counts[3], counts[4]);
     }
 
     public static void writeEarningRate(double earningRate) {
