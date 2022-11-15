6ce24f33d495fab9247435367aa7f6582b4a28f4
yun <yshyeon102@gmail.com>
2022-11-15 09:17:20 +0900

    feat: LottoController 도입과 그에 따른 리팩토링

--- src/main/java/lotto/LottoApp.java
+++ src/main/java/lotto/LottoApp.java
@@ -1,39 +1,21 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Lottos;
 import lotto.domain.WinningLotto;
-import lotto.validator.LottoNumberValidator;
-import lotto.validator.MoneyValidator;
 
 import java.util.List;
 
 public class LottoApp {
     public LottoService lottoService = new LottoService();
+    public LottoController controller = new LottoController();
     public void run() {
         try {
-            LottoView.printBuyRequest();
-            String money = Console.readLine().trim();
-            MoneyValidator.validate(money);
-            int count = lottoService.calBuyingCount(Integer.parseInt(money));
-
-            LottoView.printBuyNumber(count);
-            Lottos lottos = lottoService.getLottos(count);
-            LottoView.printLottoNumbers(lottos);
-
-            LottoView.printWinningLottoRequest();
-            String lotto = Console.readLine().trim();
-            LottoNumberValidator.validate(lotto);
-            WinningLotto winningLotto = new WinningLotto(lotto);
-
-            LottoView.printBonusNumberRequest();
-            String bonusNumber = Console.readLine().trim();
-            winningLotto.setBonusNumber(bonusNumber);
-
-            List<Integer> result = lottoService.getWinningResult(winningLotto, lottos);
-            double rateOfReturn = lottoService.getRateOfReturn(Integer.parseInt(money), result);
-            LottoView.printWinningStatistics(result, rateOfReturn);
-
+            int money = controller.printAndRequestMoney();
+            int buyingCount = controller.calAndShowBuyingCount(money);
+            Lottos lottos = controller.getAndShowLotto(buyingCount);
+            WinningLotto winningLotto = controller.getWinningLotto();
+            List<Integer> result = controller.getAndShowLottoResult(winningLotto, lottos);
+            controller.calAndShowRateOfReturn(money, result);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
---
+++ src/main/java/lotto/LottoController.java
@@ -0,0 +1,60 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lottos;
+import lotto.domain.WinningLotto;
+import lotto.validator.LottoNumberValidator;
+import lotto.validator.MoneyValidator;
+
+import java.util.List;
+
+public class LottoController {
+    public LottoService lottoService = new LottoService();
+
+    public int printAndRequestMoney() {
+        LottoView.printBuyRequest();
+        String money = Console.readLine().trim();
+        MoneyValidator.validate(money);
+        return Integer.parseInt(money);
+    }
+
+    public int calAndShowBuyingCount(int money) {
+        int buyingCount = lottoService.calBuyingCount(money);
+        LottoView.printBuyNumber(buyingCount);
+        return buyingCount;
+    }
+
+    public Lottos getAndShowLotto(int count) {
+        Lottos lottos = lottoService.getLottos(count);
+        LottoView.printLottoNumbers(lottos);
+        return lottos;
+    }
+
+    public WinningLotto getWinningLotto() {
+        LottoView.printWinningLottoRequest();
+        String lotto = Console.readLine().trim();
+        LottoNumberValidator.validate(lotto);
+
+        WinningLotto winningLotto = new WinningLotto(lotto);
+        addBonusNumber(winningLotto);
+
+        return winningLotto;
+    }
+
+    public void addBonusNumber(WinningLotto winningLotto) {
+        LottoView.printBonusNumberRequest();
+        String bonusNumber = Console.readLine().trim();
+        winningLotto.setBonusNumber(bonusNumber);
+    }
+
+    public List<Integer> getAndShowLottoResult(WinningLotto winningLotto, Lottos lottos) {
+        List<Integer> result = lottoService.getWinningResult(winningLotto, lottos);
+        LottoView.printWinningStatistics(result);
+        return result;
+    }
+
+    public void calAndShowRateOfReturn(int money, List<Integer> result) {
+        double rateOfReturn = lottoService.getRateOfReturn(money, result);
+        LottoView.printRateOfReturn(rateOfReturn);
+    }
+}
