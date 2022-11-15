8f11047c986324d94975c358efdf23c3dace58c0
dlgosla <dlgoslaek1@gmail.com>
2022-11-15 23:08:16 +0900

    feat: 당첨번호 입력받기

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -3,12 +3,24 @@ package lotto;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static lotto.Util.toInt;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
 
     public int getMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         return toInt(readLine());
+    }
+
+    public List<Integer> getWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<Integer> winningNumbers = new ArrayList<>();
+
+        for (String s : readLine().split(",")) {
+            winningNumbers.add(toInt(s));
+        }
+        return winningNumbers;
     }
 
 
