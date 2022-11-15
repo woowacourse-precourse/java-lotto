ac746a3600c2c921e92e1274422530c4d3a31ad4
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-15 12:59:19 +0900

    refactor(messages): Rewrite error messages in more detail
    Feature refactored:
    - Error message for invalid answer(6 'unique' numbers)
    - Error message for invalid bonus('different number as the answer input')

--- src/main/java/lotto/messages.java
+++ src/main/java/lotto/messages.java
@@ -2,8 +2,8 @@ package lotto;
 
 public enum messages {
 
-    INVALID_ANSWER("[ERROR] 당첨번호는 쉼표(,)를 기준으로 6개의 숫자를 입력해 주세요."),
-    INVALID_BONUS("[ERROR] 보너스 번호는 1부터 45 사이의 숫자를 한 개만 입력해 주세요."),
+    INVALID_ANSWER("[ERROR] 당첨번호는 쉼표(,)를 기준으로 6개의 서로 다른 숫자를 입력해 주세요."),
+    INVALID_BONUS("[ERROR] 보너스 번호는 당첨번호와 중복되지 않는 1부터 45 사이의 숫자를 한 개만 입력해 주세요."),
     INVALID_FORMAT_MONEY("[ERROR] 로또 구입 금액은 숫자로 입력해 주세요."),
     INVALID_NUMBER_MONEY("[ERROR] 로또 구입 금액은 1,000원 단위로 입력해 주세요.");
 
