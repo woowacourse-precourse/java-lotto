0a4980fcd18c44db180afc3cb104455e8c59f792
kbm6609 <kbm6609@nvaer.com>
2022-11-15 22:09:47 +0900

    feat<lotto> : 구매자의 총 당첨 내역 계산 기능 구현

--- src/main/java/lotto/domain/Judgment.java
+++ src/main/java/lotto/domain/Judgment.java
@@ -3,6 +3,16 @@ package lotto.domain;
 import java.util.List;
 
 public class Judgment {
+    public int[] calculateWins(List<Lotto> myLottos, List<Integer> winningNumber) {
+        int[] wins = new int[Lotto.numberCount + 1];
+        for (int i = 0; i < myLottos.size(); i++) {
+            List<Integer> lottoNumber = myLottos.get(i).getNumbers();
+            int winCount = compareToNumber(lottoNumber, winningNumber);
+            wins[winCount]++;
+        }
+        return wins;
+    }
+
     public int compareToNumber(List<Integer> myNumber, List<Integer> winningNumber) {
         int winCount = 0;
         for (int i : myNumber) {
