5a37ce13c95d5b9fdc247c58ee4fd6f039f46633
JeonghakLee <wjdgkr0527@naver.com>
2022-11-15 09:30:48 +0900

    feat: 각 입력에 대한 검증 기능 구현

--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -1,6 +1,7 @@
 package lotto.service;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.domain.Lotto.LOTTO_PRICE;
 
 import java.util.Arrays;
 import java.util.List;
@@ -8,9 +9,22 @@ import java.util.stream.Collectors;
 
 public class InputService {
 
+    private static final String winningNumberFormat = "^[0-9]+(,[0-9]+){5}";
+
     public List<Integer> getWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = readLine();
+        validateWinningNumbersFormat(input);
+        return convertToIntegerList(input);
+    }
+
+    private void validateWinningNumbersFormat(String input) {
+        if (!input.matches(winningNumberFormat)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static List<Integer> convertToIntegerList(String input) {
         return Arrays.stream(input.split(","))
             .map(Integer::parseInt)
             .collect(Collectors.toList());
@@ -18,7 +32,16 @@ public class InputService {
 
     public Integer getPurchaseAmount() {
         System.out.println("구입 금액을 입력해 주세요.");
-        return getInteger();
+        int purchasedAmount = getInteger();
+        validatePurchaseAmount(purchasedAmount);
+        return purchasedAmount;
+    }
+
+    private void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(
+                String.format("로또는 %d원 단위로만 구매할 수 있습니다.", LOTTO_PRICE));
+        }
     }
 
     public Integer getBonusNumber() {
@@ -28,7 +51,10 @@ public class InputService {
 
     private static Integer getInteger() {
         String input = readLine();
-        return Integer.valueOf(input);
+        try {
+            return Integer.valueOf(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해 주세요");
+        }
     }
-
 }
