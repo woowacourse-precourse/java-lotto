c4ad5091f901b2067ca8b96a71c9311179694e0e
comet7001 <comet7001@gmail.com>
2022-11-15 19:20:32 +0900

    refactor(lotto): move validating methods from ConsoleUserInput class to Lotto class

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,18 @@
 package lotto;
 
+import utils.ConsoleUserInput;
+import utils.Validator;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        ConsoleUserInput in = new ConsoleUserInput();
         validate(numbers);
+        Validator.hasNoDuplication(numbers);
         this.numbers = numbers;
     }
 
@@ -15,6 +21,52 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
+    public static String validateMoneyForLottos(String moneyForLottos){
+        Validator.tryAndCatchIsValidInputForPayment(moneyForLottos);
+        Validator.tryAndCatchIsValidUnitForPayment(moneyForLottos);
+        Validator.tryAndCatchHasNoMoney(moneyForLottos);
+        return moneyForLottos;
+    }
 
-    // TODO: 추가 기능 구현
+    public static int ToIntegerMoneyForCalculation(String moneyForLottos){
+        return Integer.parseInt(moneyForLottos);
+    }
+
+    public static String validateWinningNumbers(String userInput){
+        userInput=userInput.trim();
+        userInput=userInput.replaceAll(" ","");
+        Validator.tryAndCatchIsValidLength(userInput, 18);
+        Validator.tryAndCatchIsValidFormatWithComma(userInput);
+        return userInput;
+    }
+
+    public static List<Integer> toIntegerNumbersWithoutComma(String userInput){
+        List<Integer> sixWinningNumbers = new ArrayList<>();
+        String[] numbersToStringsForValidation = userInput.split(",");
+        for(int index=0; index<numbersToStringsForValidation.length; index++) {
+            Validator.tryAndCatchIsInValidRange(Integer.parseInt(numbersToStringsForValidation[index]));
+        }
+        for(int index=0; index<numbersToStringsForValidation.length; index++) {
+            sixWinningNumbers.add(Integer.valueOf(numbersToStringsForValidation[index]));
+        }
+        Validator.tryAndCatchHasNoDuplication(sixWinningNumbers);
+        return sixWinningNumbers;
+    }
+
+    public static String validateBonusNumber(String bonusNumber, List<Integer> winningNumbers){
+        if(bonusNumber.length()>2){
+            Validator.tryAndCatchIsValidLength(bonusNumber,2);
+        }
+        Validator.tryAndCatchIsValidInput(bonusNumber);
+        Validator.tryAndCatchIsInValidRange(Integer.parseInt(bonusNumber));
+        int bonus = Integer.parseInt(bonusNumber);
+        Validator.tryAndCatchIsBonusDifferentWithWinningNumbers(winningNumbers, bonus);
+        return bonusNumber;
+    }
+
+    public static int getBonusNumber(String bonusNumber){
+        int bonus = Integer.parseInt(bonusNumber);
+        return bonus;
+    }
+
 }
--- src/main/java/utils/ConsoleUserInput.java
+++ src/main/java/utils/ConsoleUserInput.java
@@ -8,49 +8,25 @@ import java.util.List;
 public class ConsoleUserInput {
     // 입력부 제외 유효성 체크 완전히 빼서 lotto 내 메서드로 구현
 
+    public String inputMoneyForLottos(){
+        String moneyForLottos = Console.readLine();
+        return moneyForLottos;
+    }
+
     public String inputWinningNumbers(){
         String userInput = Console.readLine();
-        userInput=userInput.trim();
-        userInput=userInput.replaceAll(" ","");
-        validateWinningNumbers(userInput);
         return userInput;
     }
 
-    public void validateWinningNumbers(String userInput){
-        Validator.tryAndCatchIsValidLength(userInput, 18);
-        Validator.tryAndCatchIsValidFormatWithComma(userInput);
-    }
-
-    public List<Integer> toIntegerNumbersWithoutComma(String userInput){
-        List<Integer> sixWinningNumbers = new ArrayList<>();
-        String[] numbersToStringsForValidation = userInput.split(",");
-        for(int index=0; index<numbersToStringsForValidation.length; index++) {
-            Validator.tryAndCatchIsInValidRange(Integer.valueOf(numbersToStringsForValidation[index]));
-        }
-        for(int index=0; index<numbersToStringsForValidation.length; index++) {
-            sixWinningNumbers.add(Integer.valueOf(numbersToStringsForValidation[index]));
-        }
-        Validator.tryAndCatchHasNoDuplication(sixWinningNumbers);
-        return sixWinningNumbers;
-    }
-
-    public static int inputBonusNumber(List<Integer> winningNumbers){
+    public String inputBonusNumber(){
         String bonusNumber = Console.readLine();
-        if(bonusNumber.length()>2){
-            Validator.tryAndCatchIsValidLength(bonusNumber,2);
-        }
-        Validator.tryAndCatchIsValidInput(bonusNumber);
-        Validator.tryAndCatchIsInValidRange(Integer.parseInt(bonusNumber));
-        int bonus = Integer.parseInt(bonusNumber);
-        Validator.tryAndCatchIsBonusDifferentWithWinningNumbers(winningNumbers, bonus);
-        return bonus;
+        return bonusNumber;
     }
 
-    public static int inputMoneyForLottos(){
-        String moneyForLottos = Console.readLine();
-        Validator.tryAndCatchIsValidInputForPayment(moneyForLottos);
-        Validator.tryAndCatchIsValidUnitForPayment(moneyForLottos);
-        Validator.tryAndCatchHasNoMoney(moneyForLottos);
-        return Integer.parseInt(moneyForLottos);
-    }
+
+
+
+
+
+
 }
