36a5fec733f8c427b5edb9a23efd8d168541f9ee
Cho SangHyun <98shcho@naver.com>
2022-11-15 22:56:14 +0900

    fix(결과 출력하기): 수익률 출력 문구에 .추가

--- src/main/java/lotto/io/OutputMessages.java
+++ src/main/java/lotto/io/OutputMessages.java
@@ -7,7 +7,7 @@ public enum OutputMessages {
     LOTTO_PURCHASE_MESSAGE("%d개를 구매했습니다."),
     WINNING_STATISTICS_MESSAGE("당첨 통계"),
     DIVIDING_LINE("---"),
-    EARNING_RATE_MESSAGE("총 수익률은 %.1f%%입니다"),
+    EARNING_RATE_MESSAGE("총 수익률은 %.1f%%입니다."),
     FIFTH_STATISTICS_MESSAGE("3개 일치 (5,000원) - %d개"),
     FOURTH_STATISTICS_MESSAGE("4개 일치 (50,000원) - %d개"),
     THIRD_STATISTICS_MESSAGE("5개 일치 (1,500,000원) - %d개"),
