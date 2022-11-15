0100227845dd4020bb6d4aaca8587e8912b233c8
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-15 13:38:23 +0900

    refactor : 깨진 테스트 수정

--- src/test/java/lotto/ValidatiorTest.java
+++ src/test/java/lotto/ValidatiorTest.java
@@ -12,38 +12,53 @@ import org.junit.jupiter.params.provider.MethodSource;
 class ValidatiorTest {
     private static Collection<Arguments> param1() {
         return Arrays.asList(
-            Arguments.of("{1,2,3,4,5,6}은 정상이다", "1,2,3,4,5,6"),
-            Arguments.of("{1,2,3,9,8,6}은 정상이다", "1,2,3,9,8,6"),
-            Arguments.of("{1,2,3,9,8,45}은 정상이다", "1,2,3,9,8,45")
+            Arguments.of("{1,2,3,4,5,6}은 정상이다", "1,2,3,4,5,6", "7"),
+            Arguments.of("{1,2,3,9,8,6}은 정상이다", "1,2,3,9,8,6", "7"),
+            Arguments.of("{1,2,3,9,8,45}은 정상이다", "1,2,3,9,8,45", "7")
         );
     }
 
     @ParameterizedTest(name = "{index}: {0}")
     @MethodSource("param1")
-    @DisplayName("올바른 문자열로 isValidWinnings 테스트")
-    void test1(String description, String wins) {
+    @DisplayName("올바른 문자열로 isValidWinAndBonus 테스트")
+    void test1(String description, String wins, String bonus) {
         Validator validator = new Validator();
-        Assertions.assertDoesNotThrow(() -> validator.isValidWinnings(wins));
+        Assertions.assertDoesNotThrow(() -> validator.isValidWinAndBonus(wins, bonus));
     }
 
     private static Collection<Arguments> param2() {
         return Arrays.asList(
-            Arguments.of("{1,2,3,4,5,i}은 비정상이다", "1,2,3,4,5,i"),
-            Arguments.of("{1,2,k,9,8,6}은 비정상이다", "1,2,k,9,8,6"),
-            Arguments.of("{1,2,3,9,8}은 비정상이다", "1,2,3,9,8"),
-            Arguments.of("{1,2,3,9,8,99}은 비정상이다", "1,2,3,9,8,99"),
-            Arguments.of("{1,2,3,9,8,46}은 비정상이다", "1,2,3,9,8,46"),
-            Arguments.of("{1,2,3,9,8,0}은 비정상이다", "1,2,3,9,8,0"),
-            Arguments.of("{1,1,1,1,1,1}은 비정상이다", "1,1,1,1,1,1"),
-            Arguments.of("{1,2,3,4,5,5}은 비정상이다", "1,2,3,4,5,5")
+            Arguments.of("{1,2,3,4,5,i}은 비정상이다", "1,2,3,4,5,i", "7"),
+            Arguments.of("{1,2,k,9,8,6}은 비정상이다", "1,2,k,9,8,6", "7"),
+            Arguments.of("{1,2,3,9,8}은 비정상이다", "1,2,3,9,8", "7"),
+            Arguments.of("{1,2,3,9,8,99}은 비정상이다", "1,2,3,9,8,99", "7"),
+            Arguments.of("{1,2,3,9,8,46}은 비정상이다", "1,2,3,9,8,46", "7"),
+            Arguments.of("{1,2,3,9,8,0}은 비정상이다", "1,2,3,9,8,0", "7"),
+            Arguments.of("{1,1,1,1,1,1}은 비정상이다", "1,1,1,1,1,1", "7"),
+            Arguments.of("{1,2,3,4,5,5}은 비정상이다", "1,2,3,4,5,5", "7")
         );
     }
 
     @ParameterizedTest(name = "{index}: {0}")
     @MethodSource("param2")
-    @DisplayName("올바르지 않은 문자열로 isValidWinnings 테스트")
-    void test2(String description, String wins) {
+    @DisplayName("올바르지 않은 wins 문자열로 isValidWinAndBonus 테스트")
+    void test2(String description, String wins, String bonus) {
         Validator validator = new Validator();
-        Assertions.assertThrows(IllegalArgumentException.class, () -> validator.isValidWinnings(wins));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> validator.isValidWinAndBonus(wins, bonus));
+    }
+
+    private static Collection<Arguments> param3() {
+        return Arrays.asList(
+            Arguments.of("{1,2,3,4,5,i}은 비정상이다", "1,2,3,4,5,7", "7"),
+            Arguments.of("{1,2,3,4,5,i}은 비정상이다", "1,2,3,4,25,7", "25")
+        );
+    }
+
+    @ParameterizedTest(name = "{index}: {0}")
+    @MethodSource("param3")
+    @DisplayName("중복된 숫자가 존재하는 wins,bonus로 isValidWinnings 테스트")
+    void test3(String description, String wins, String bonus) {
+        Validator validator = new Validator();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> validator.isValidWinAndBonus(wins, bonus));
     }
 }
