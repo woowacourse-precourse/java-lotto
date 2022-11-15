8612690ff1da1b432aa4827eb535cb577cb4f2a5
sschan99 <seungchan1219@gmail.com>
2022-11-15 22:29:50 +0900

    fix(Lotto): modify not to have duplicate lotto numbers

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,7 @@ package lotto;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -14,7 +15,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (new HashSet<>(numbers).size() != 6) {
             throw new IllegalArgumentException();
         }
     }
