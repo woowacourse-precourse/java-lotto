6a7b3804e62d8c616de7ad3ad7e37029bdec979e
Kijung Park <kjpark4321@naver.com>
2022-11-15 23:12:52 +0900

    feat : 로또 중복숫자 갯수 비교 메서드 구현

---
+++ src/test/java/lotto/LottoManagerTest.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoManagerTest {
+    @Test
+    void retainAllTest(){
+        LottoManager lottoManager = new LottoManager();
+
+
+    }
+}
