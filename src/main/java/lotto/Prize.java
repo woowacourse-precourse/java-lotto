43c0280f592e787dc88821748af5b85f8d876dce
Sanmgmin <sangmins930@gmail.com>
2022-11-15 20:36:59 +0900

    refactor(Prize): delete not using variable, enum constant

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,21 +1,16 @@
 package lotto;
 
 public enum Prize {
-    NOTHING(0, false, 0),
-    FIFTH(3, false, 5_000),
-    FOURTH(4, false, 50_000),
-    THIRD(5, false, 1_500_000),
-    SECOND(5, true, 30_000_000),
-    JACKPOT(6, false, 2_000_000_000);
+    FIFTH(5_000),
+    FOURTH(50_000),
+    THIRD(1_500_000),
+    SECOND(30_000_000),
+    JACKPOT(2_000_000_000);
 
-    private final int numsOfMatchedNumber;
-    private final boolean containBonus;
     private final int prize;
     private int numsOfWinner;
 
-    Prize(int numsOfMatchedNumber, boolean containBonus, int prize) {
-        this.numsOfMatchedNumber = numsOfMatchedNumber;
-        this.containBonus = containBonus;
+    Prize(int prize) {
         this.prize = prize;
         this.numsOfWinner = 0;
     }
