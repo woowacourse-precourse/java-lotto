ff7812df0a213bfa92ee9ea2b4acf3d0759abd8f
jiwon <ljwon77@daum.net>
2022-11-15 18:42:42 +0900

    test: add integration test about winning numbers and bonus number
    당첨 번호 및 보너스 번호에 대한 통합 테스트 코드를 추가하였습니다.

--- src/test/java/lotto/LottoWinningNumberTest.java
+++ src/test/java/lotto/LottoWinningNumberTest.java
@@ -27,6 +27,25 @@ class LottoWinningNumberTest {
                 .isEqualTo(List.of(1, 2, 3, 4, 5, 6));
     }
 
+    @Test
+    @DisplayName("올바른 당첨, 보너스 번호가 들어왔을 때 해당 정보를 정상적으로 리턴해야 한다.")
+    void 당첨_번호_보너스_번호_통합_테스트() throws Exception {
+        //given
+        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber();
+
+        //when
+        command("1,2,3,4,5,6", "7");
+        LottoWinningNumber winningNumbersInfo = lottoWinningNumber.getWinningNumbersInfo();
+
+        //then
+        List<Integer> winningNums = List.of(1,2,3,4,5,6);
+        Integer bonusNum = 7;
+
+        assertThat(winningNumbersInfo).isNotNull();
+        assertThat(winningNumbersInfo.getWinningNums()).isEqualTo(winningNums);
+        assertThat(winningNumbersInfo.getBonusNum()).isEqualTo(bonusNum);
+    }
+
     private void command(final String... args) {
         final byte[] buf = String.join("\n", args).getBytes();
         System.setIn(new ByteArrayInputStream(buf));
