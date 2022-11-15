c2ae62284a0e3ace37287537204859711bc93db2
park-Kumin <rbals97@naver.com>
2022-11-15 23:25:06 +0900

    feat: 1. 사용자로부터 입출력을 위한 출력문 상수들과 입력 메소드를 가지고 있는 UserInputString enum 클래스 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
+
 }
