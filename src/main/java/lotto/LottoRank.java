5bb7da926ae6ee690522ae92b90203cf2b042d1f
KimSunU <kgw6147@naver.com>
2022-11-15 22:46:36 +0900

    [feat] 로또 순위 enum형식 구현

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -1,2 +1,32 @@
-package lotto;public enum LottoRank {
+package lotto;
+
+public enum LottoRank {
+    FIRST("6개 일치", 1,2000000000),
+    SECOND("5개 일치, 보너스 볼 일치", 2,30000000),
+    THIRD("5개 일치", 3,1500000),
+    FOURTH("4개 일치", 4,50000),
+    FIFTH("3개 일치", 5,5000),
+    ZERO("0개 일치", 0,0);
+
+    private final String countWriting;
+    private final int rank;
+    private final int prize;
+
+    LottoRank(String countWriting, int rank, int prize){
+        this.countWriting = countWriting;
+        this.rank = rank;
+        this.prize = prize;
+    }
+
+    public String getCountWriting() {
+        return countWriting;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
 }
