5ba19f6457b3ec322a82180e992df0a91d0ecd6a
Woogie95 <choisw950928@gmail.com>
2022-11-15 23:36:30 +0900

    refactor: 인풋 수정

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -2,28 +2,21 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Scanner;
-
 public class Input {
-
-    private final static Scanner scanner = new Scanner(System.in);
 
     public static String inputLottoPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-//        return Console.readLine(); 제출 시 바꾸기
-        return scanner.nextLine();
+        return Console.readLine();
     }
 
     public static String inputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
-//        return Console.readLine(); 제출 시 바꾸기
-        return scanner.nextLine();
+        return Console.readLine();
     }
 
     public static String inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-//        return Console.readLine(); 제출 시 바꾸기
-        return scanner.nextLine();
+        return Console.readLine();
     }
 
 }
