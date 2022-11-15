29b70403dbc4eeba8889fee63fe23ead2d34a1f5
Be-student <presentsong@naver.com>
2022-11-15 13:57:20 +0900

    chore: 안 쓰는 코드 제거

--- src/main/java/lotto/constant/LottoConstants.java
+++ src/main/java/lotto/constant/LottoConstants.java
@@ -21,12 +21,6 @@ public class LottoConstants {
      */
     public static final int LOTTO_LENGTH = 6;
 
-    /**
-     * 이 시스템에서 사용될 수 있는 최소 돈을 설정합니다
-     * 이 숫자보다 적은 돈이 입력되면 예외가 발생합니다
-     */
-    public static final int MINIMUM_MONEY = 1000;
-
     private LottoConstants() {
     }
 }
