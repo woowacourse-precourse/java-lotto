b77a1e1c99b69d5b17e1b0e0db3e386c34985bb4
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-11-15 20:12:08 +0900

    Refactor : 새로운 객체들로 기능 전부 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,8 +7,11 @@ import java.util.*;
 public class Application {
     public static void main(String[] args){
         LottoDriver lottoDriver = new LottoDriver();
-        lottoDriver.startLottoGame();
-        //startLottoGame();
+        try{
+            lottoDriver.startLottoGame();
+        }
+        catch (Exception e){
+        }
     }
 
 }
