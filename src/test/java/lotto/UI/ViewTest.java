6e68a397619af23c63fe64df70056fe3835d769a
Frog-Slayer <Frog-Slayer@github.com>
2022-11-15 14:34:30 +0900

    test(ViewTest): 총 수익률 출력이 제대로 이루어지는지 테스트

--- src/test/java/lotto/UI/ViewTest.java
+++ src/test/java/lotto/UI/ViewTest.java
@@ -1,35 +1,22 @@
 package lotto.UI;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class ViewTest {
 
+    @DisplayName(".1f 했을 때 반올림이 잘 되는지 테스트")
     @Test
-    void printBudgetInput() {
-    }
-
-    @Test
-    void printButLotto() {
-    }
-
-    @Test
-    void printWinningNumberInput() {
-    }
-
-    @Test
-    void printBonusNumberInput() {
-    }
-
-    @Test
-    void printWinningStatisticsTitle() {
-    }
-
-    @Test
-    void printWinningStatisticsDetail() {
-    }
-
-    @Test
-    void printProfitRate() {
+    public void printProfitRateTest() {
+        View view = new View();
+        view.printProfitRate(66.66666);
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        assertThat(out).toString().contains("66.7%");
     }
 }
