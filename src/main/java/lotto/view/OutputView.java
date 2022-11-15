e8c1809fb9688ee40409fe903d644bcca2c8ee84
wnsgml7267 <dlwns7267@naver.com>
2022-11-15 22:42:32 +0900

    feat(view/OutputView): 출력 기능 클래스 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,18 @@
+package lotto.view;
+
+public class OutputView {
+    private final String MESSAGE_TO_PRINT_COUNT = "개를 구매했습니다.";
+    private final String MESSAGE_TO_PRINT_RESULT = "\n당첨 통계\n---";
+    private final String MESSAGE_TO_PRINT_YIELD_A = "총 수익률은 ";
+    private final String MESSAGE_TO_PRINT_YIELD_B = "%입니다.";
+
+
+
+
+
+
+
+
+
+
+}
