1634e88650dce68010a883677cb554ac3d2f3100
BHC-Chicken <tij05078@gmail.com>
2022-11-15 11:23:39 +0900

    refactor : 수익률 자료형 변경
    정상적인 수익률 표기를 위해 int 에서 long으로 자료형을 변경한다.

--- src/main/java/lotto/compareLottoNumber/PrintResult.java
+++ src/main/java/lotto/compareLottoNumber/PrintResult.java
@@ -3,17 +3,17 @@ package lotto.compareLottoNumber;
 import lotto.constant.WinningAmount;
 
 public class PrintResult {
-    public float calcPrize(int[] result, int money) {
-        float total = 0;
+    public long calcPrize(int[] result, int money) {
+        long total = 0L;
 
         for (int index = 0; index < 5; index++) {
-            total += result[index] * WinningAmount.values()[index].getValue();
+            total += (long) result[index] * WinningAmount.values()[index].getValue();
         }
-        return (total / (money * 1_000)) * 100;
+        return (total / (money * 1000L)) * 100;
     }
 
     public void printResult(int[] result, int money) {
-        float profit = calcPrize(result, money);
+        long profit = calcPrize(result, money);
         String[] fewMatch = {"3개 일치 (", "4개 일치 (", "5개 일치 (", "5개 일치, 보너스 볼 일치 (", "6개 일치 ("};
 
         System.out.println("당첨 통계");
