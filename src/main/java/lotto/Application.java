e65235faa6c6d80429e9b4551030b7bbc3dbde40
LEE JIEUN <jieun3595@soongsil.ac.kr>
2022-11-15 15:58:32 +0900

    feat : 당첨번호와 보너스번호에 관한 함수 구현
    winningNums : 당첨번호를 입력받고 ,를 기준으로 split한 후에 리스트에 담는다. 이도 똑같이 정렬한다. 그다음에 리스트를 반환.
    여기서 당첨번호에 중복이 없는지 확인.(checkUniqueWinningNums) 중복이 있으면, 예외처리.
    checkLottoNum은 당첨번호와 보너스번호가 1부터 45인지 확인하는 함수.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,8 +11,11 @@ public class Application {
     private static final String ERROR_CHECKNUM = " 숫자가 아닌 수가 존재합니다.";
     private static final String ERROR_CHECKPRICE = " 1000원 단위로 입력해 주세요.";
     private static final String ERROR_CHECKLOTTONUM =  " 1부터 45사이의 수가 아니다.";
+    private static final String ERROR_UNIQUENUM = " 당첨번호에 중복이 발생합니다.";
     private static final String START_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String LOTTOSIZE_MESSAGE = "개를 구매했습니다.";
+    private static final String WINNINGNUM_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUSNUM_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static void main(String[] args) {
         Application main = new Application();
@@ -24,6 +27,14 @@ public class Application {
         System.out.println(lottoNum + LOTTOSIZE_MESSAGE);
         List<List<Integer>> myLottoArr = main.lottoArr(lottoNum);
 
+        System.out.println(WINNINGNUM_MESSAGE);
+        List numbers = main.winningNums(main.userInputNumber());
+        Lotto lotto = new Lotto(numbers);
+        System.out.println(numbers);
+
+        System.out.println(BONUSNUM_MESSAGE);
+        int bonusNum = main.checkLottoNum(main.checkNum(main.userInputNumber()));
+        System.out.println(bonusNum);
     }
     public String userInputNumber() {
         return Console.readLine();
@@ -55,6 +66,27 @@ public class Application {
         }
         return myLottoArr;
     }
+    public List<Integer> winningNums(String userInputNumber){
+        List<Integer> winningLottoNums = new ArrayList<>();
+        for (String splitInput : userInputNumber.split(",")) {
+            int stringToInt = checkLottoNum(checkNum(splitInput));
+            winningLottoNums.add(stringToInt);
+            winningLottoNums.sort(Comparator.naturalOrder());
+        }
+        checkUniqueWinningNums(winningLottoNums);
+        return winningLottoNums;
+    }
 
-
+    public void checkUniqueWinningNums(List winningLottoNums){
+        if(winningLottoNums.size() != winningLottoNums.stream().distinct().count()){
+            throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_UNIQUENUM);
+        }
+    }
+    
+    public int checkLottoNum(int lottoNum) {
+        if (0 >= lottoNum || lottoNum > 45) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + ERROR_CHECKLOTTONUM);
+        }
+        return lottoNum;
+    }
 }
