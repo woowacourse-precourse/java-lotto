4590fbcfa0170d105f99332996d7ec3d35b3c4c8
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-15 15:54:19 +0900

    feat: Lotto 클래스에 번호 포함 여부 메소드 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -28,7 +28,12 @@ public class Lotto {
         }
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
     }
 }
