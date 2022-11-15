7122546c202ea90eac2b446f01ac7ea91c70e5b0
Amyhds <dasomhan524@gmail.com>
2022-11-15 11:46:02 +0900

    refactor(profit): 추상화 레벨 맞춤

--- src/main/java/lotto/model/Profit.java
+++ src/main/java/lotto/model/Profit.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Profit {
-    public static final List<Integer> prizeMoney = new ArrayList<>();
-    public static double profitRate;
+    public List<Integer> prizeMoney = new ArrayList<>();
+    public double profitRate;
 
-    public Profit(int price, WinningResult winningResult) {
+    public Profit(int price, List<Integer> winningResult) {
         setPrize();
         calculateProfit(price, winningResult);
     }
@@ -18,10 +18,10 @@ public class Profit {
         prizeMoney.addAll(List.of(Prize.PRIZE_3.getPrize(), Prize.PRIZE_4.getPrize(), Prize.PRIZE_5.getPrize(), Prize.PRIZE_5BONUS.getPrize(), Prize.PRIZE_6.getPrize()));
     }
 
-    private void calculateProfit(int price, WinningResult winningResult) {
+    private void calculateProfit(int price, List<Integer> winningResult) {
         int totalProfit = 0;
         for (int index = 0; index < 5; index++) {
-            totalProfit += winningResult.winningResult.get(index) * prizeMoney.get(index);
+            totalProfit += winningResult.get(index) * prizeMoney.get(index);
         }
         profitRate = (double) totalProfit / price * 100;
     }
