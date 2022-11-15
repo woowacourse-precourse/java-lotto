25307c078d81515af96592b8dac22b6a2e5635d3
leesangjin <tkdwls0628@gmail.com>
2022-11-15 17:28:23 +0900

    style(Lotto): 로또 번호 변수명 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,11 +5,11 @@ import lotto.validator.LottoValidator;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<Integer> lottoNumbers;
 
-    public Lotto(List<Integer> numbers) {
-        LottoValidator.validateLottoNumbers(numbers);
-        this.numbers = numbers;
+    public Lotto(List<Integer> lottoNumbers) {
+        LottoValidator.validateLottoNumbers(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
     }
 
     // TODO: 추가 기능 구현
