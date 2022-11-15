77f794ceced1e12b60b614d57a115f23a3311587
dlrghks2090 <dlrghks2090@gmail.com>
2022-11-15 23:30:34 +0900

    feat: 로또 당첨 체크 기능

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,32 +1,39 @@
 package lotto.controller;
 
-import lotto.domain.Lotto;
-import lotto.domain.LottoCount;
-import lotto.domain.LottoMake;
-import lotto.domain.ProvidNumber;
+import lotto.domain.*;
 import lotto.view.Input;
+import lotto.view.Output;
 
 import java.util.List;
 
 public class LottoGame {
     String purchaseAmount;
     Input input = new Input();
+    Output output = new Output();
 
     public void start() {
-        LottoCount lottoCount = new LottoCount(input.lottoPurchaseAmount());
-        List<Lotto> lottoPapers = buyLotto(lottoCount.getLottoCount());
-        ProvidNumber providNumber = new ProvidNumber(input.lottoWinningNumbers(), input.lottoBonusNumber());
-
-        input.lottoWinningNumbers();
-
-
-        input.lottoBonusNumber();
+        try {
+            LottoCount lottoCount = new LottoCount(input.lottoPurchaseAmount());
+            List<Lotto> lottoPapers = buyLotto(lottoCount.getLottoCount());
+            ProvidNumber providNumber = new ProvidNumber(input.lottoWinningNumbers(), input.lottoBonusNumber());
+            calculateResult(lottoPapers, providNumber, lottoCount.getPurchaseAmount());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private List<Lotto> buyLotto(int count) {
         LottoMake lottoMake = new LottoMake(count);
         List<Lotto> lottoPapers = lottoMake.getLottoPapers();
 
+        output.printLottoPaperCount(count);
+        for (int i = 0; i < lottoPapers.size(); i++) {
+            output.printLottoPapers(lottoPapers.get(i).getNumbers());
+        }
         return lottoPapers;
+    }
+
+    private void calculateResult(List<Lotto> lottoPapers, ProvidNumber providNumber, int purchseAmount) {
+        RankCalculator rankCalculator = new RankCalculator(lottoPapers, providNumber);
     }
 }
--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
+import lotto.domain.Rank;
+
 import java.util.List;
+import java.util.Map;
 
 public class Output {
 
@@ -10,19 +13,20 @@ public class Output {
     private static final String WINNIG_AND_BONUS_BALL_MATCHING_MASSAGE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개\n";
     private static final String FINAL_PROFIT_MESSAGE = "총 수익률은 %.1f%%입니다.";
 
-    public static void printLottoPaperCount(int count) {
+    public void printLottoPaperCount(int count) {
         System.out.println(count + LOTTO_PAPER_COUNT_MASSAGE);
     }
 
-    public static void printLottoPapers(List<Integer> lottoNumbers) {
+    public void printLottoPapers(List<Integer> lottoNumbers) {
         System.out.println(lottoNumbers);
     }
 
-    public static void printResult(){
-
+    public void printResult(Map<Rank, Integer> result){
+        System.out.println(PAPER_COUNT_MASSAGE);
+        for
     }
 
-    public static void printProfit(double profit) {
+    public void printProfit(double profit) {
 
     }
 
