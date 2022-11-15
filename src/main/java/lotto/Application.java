015614bb6928b19a75000e1d34387886806dd7c0
tjwjdgus12 <01048733438a@gmail.com>
2022-11-15 17:38:52 +0900

    feat: 메인 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,50 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void run() throws IllegalArgumentException {
+        long money = inputMoney();
+        List<Lotto> lottos = LottoStore.getInstance().buy(money);
+        showLottos(lottos);
+
+        RankCounter rankCounter = new RankCounter();
+        rankCounter.count(lottos);
+
+        rankCounter.showRankCount();
+        showEarningRate(money, rankCounter.getTotalReword());
+    }
+
+    private static long inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        long money;
+        try {
+            money = Long.parseLong(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
+        }
+        return money;
+    }
+
+    private static void showLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.toString());
+        }
+    }
+
+    private static void showEarningRate(long spend, long earn) {
+        double earningRate = (double)earn / spend * 100;
+        System.out.println("총 수익률은 " + String.format("%.1f", earningRate) + "%입니다.");
     }
 }
