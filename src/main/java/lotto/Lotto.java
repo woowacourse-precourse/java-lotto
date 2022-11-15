13b7e40e73a1dd7cc310732f2f4fd4e0c52b2922
Mr.Kim <joajy98@gmail.com>
2022-11-15 20:18:49 +0900

    feat: update validate method to address other constraints

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import lotto.constant.Constant;
 
 public class Lotto {
     private final List<Integer> numbers;
     private final Integer LottoSize = 6;
+    private static Constant constant = new Constant();
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -12,11 +15,18 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        if(numbers.size() != LottoSize)
+            throw new IllegalArgumentException(constant.Print_CountNumbers());
+
+        for(int i : numbers)
+            if(i < 1 && i  > 45)
+                throw new IllegalArgumentException(constant.Print_OutOfBounds());
+
+        HashSet<Integer> Check_Duplicate_Num = new HashSet<Integer>(numbers);
+        if(LottoSize != Check_Duplicate_Num.size())
+            throw new IllegalArgumentException(constant.Print_Duplicate_Numbers());
     }
-    
+
     public void print_Numbers(){
         System.out.println(numbers);
     }
