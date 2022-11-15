c02099d0a52e7fff281a976a4ebb42acb6087097
wnsgml7267 <dlwns7267@naver.com>
2022-11-15 22:36:03 +0900

    feat(view/InputView): 입력 값 받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,7 +1,22 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
     private final String MESSAGE_TO_GET_MONEY = "구입금액을 입력해 주세요.";
     private final String MESSAGE_TO_GET_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.";
     private final String MESSAGE_TO_GET_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+
+    public int getMoney() {
+        System.out.println(MESSAGE_TO_GET_MONEY);
+        String input = Console.readLine().trim();
+
+        if (!isNumber(input)) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 돈을 넣어주세요.");
+        }
+
+        return Integer.parseInt(input);
+    }
 }
