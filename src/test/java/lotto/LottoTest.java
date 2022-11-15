144a4750e78817d92e09bec785cf50b778580c8d
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-15 15:26:41 +0900

    test: Add test case for out_of_bound function

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -23,5 +23,11 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 범위를 넘어서는 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfBound() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 }
