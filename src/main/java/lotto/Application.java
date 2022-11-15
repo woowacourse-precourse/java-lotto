b5bfeb57c91e8cb973035b47825352668608eb8c
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-11-15 16:27:57 +0900

    Application : 메인 메서드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+  static Lotto lotto; //로또번호
+  static LottoBonus lottoBonus; // 로또 보너스 번호
+  static UserLotto userLotto; //구매자에 관한 정보. 구입한 금액 및 랜덤 로또번호
+  static LottoResult lottoResult;
+
+  // 로또 진행
+  public static void main(String[] args) {
+    userBuyLotto();
+    createLotto();
+    compare();
+  }
 }
