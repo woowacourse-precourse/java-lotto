b44acfdd1b1600c7859b04fcaba1578a2e8799fc
고동우 <shp05179@gmail.com>
2022-11-15 17:01:53 +0900

    feat: 로또 구입 기능

---
+++ src/main/java/lotto/Consumer.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Consumer {
+    private static List<Lotto> lottos;
+
+    Consumer() {
+        lottos = new ArrayList<>();
+    }
+
+    public static void buyLotto() {
+        LottoConsoleIo.printRequestPurchase();
+        String inputValue = LottoConsoleIo.scanInputValue();
+        ValidCheck.formatCheckPrice(inputValue);
+        for (int i = 0; i < Integer.parseInt(inputValue); i++) {
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+    }
+
+}
