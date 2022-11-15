7a8ccae503ba99a6fba3de51267457d45af42f1c
jaeyeon kim <jakind@naver.com>
2022-11-15 12:10:56 +0900

    feat(Rank): 상수들의 matching 값을 얻어내는 메소드

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public enum Rank {
     THREE(3, 5_000),
     FOUR(4, 50_000),
@@ -7,8 +11,8 @@ public enum Rank {
     FIVE_AND_BONUS(5.5f, 30_000_000),
     ALL_MATCH(6, 2_000_000_000);
 
-    private float matching;
-    private long prize;
+    private final float matching;
+    private final long prize;
 
     Rank(float matching, long prize) {
         this.matching = matching;
@@ -21,5 +25,22 @@ public enum Rank {
 
     public long getPrize() {
         return this.prize;
+    }
+
+    /**
+     * 어떤 것을 수정해야할까?
+     *
+     * getMatching 이 아닌, 그냥 matching 번호를 넘겨주면?
+     * 그리고 values 를 했을 때 그것을 List 로 넘겨주어도 되겠다.
+     */
+
+
+    public static List<Float> getNumbersOfMatching() {
+        List<Float> numbersOfMatching = new ArrayList<>();
+
+        Arrays.stream(values())
+                .forEach(rank -> numbersOfMatching.add(rank.matching));
+
+        return numbersOfMatching;
     }
 }
