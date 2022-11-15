1983b0112a763511731bc39f40f3694ccc948f5b
kimdoha <kkhhjj3393@gmail.com>
2022-11-15 13:26:18 +0900

    feat(User): 유저 로또 구입 클래스 생성 및 기능 구현
    - 1. 로또 구입 전, 금액 입력 메세지 출력
    - 2. 로또 구입 금액을 입력 받는 기능 구현
    - 3. 구입 금액에 대한 검증 메소드 구현

---
+++ src/main/java/lotto/domain/user/User.java
@@ -0,0 +1,31 @@
+package lotto.domain.user;
+
+import lotto.constant.LottoPrice;
+import lotto.domain.utils.Input;
+
+public class User {
+  private Integer money;
+  public static final String INPUT_MESSAGE_OF_GETTING_USER_MONEY = "구입금액을 입력해 주세요.";
+  public static final String ERROR_MESSAGE_OF_USER_MONEY = "[ERROR] 구입금액은 천원 단위여야 합니다.";
+
+  public User() {
+    printMoneyInputMessage();
+    Integer money = Input.toInteger();
+    validateInputMoney(money);
+    this.money = money;
+  }
+
+  private void printMoneyInputMessage() {
+    System.out.println(INPUT_MESSAGE_OF_GETTING_USER_MONEY);
+  }
+
+  private void validateInputMoney(Integer money) {
+    if(money % getLottoPrice() != 0){
+      throw new IllegalArgumentException(ERROR_MESSAGE_OF_USER_MONEY);
+    }
+  }
+
+  private Integer getLottoPrice() {
+    return LottoPrice.LOTTO_PRICE.getPrice();
+  }
+}
