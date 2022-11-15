093634a2e98dcf8dfb8551101c74c9a288ef148b
doongu <must1080@naver.com>
2022-11-15 20:06:07 +0900

    feat(TEST): domain 단위의 test 클래스들 추가

---
+++ src/test/java/lotto/domain/LottoSystemTest.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoSystemTest {
+
+    @Test
+    void setUserLottoCount() {
+    }
+
+    @Test
+    void getUserLottoCount() {
+    }
+}
---
+++ src/test/java/lotto/domain/UserTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void setUserLottoNumber() {
+    }
+
+    @Test
+    void getUserPayAmount() {
+    }
+
+    @Test
+    void getUserLottoNumber() {
+    }
+}
