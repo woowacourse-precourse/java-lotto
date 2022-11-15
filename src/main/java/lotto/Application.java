db9e76be0987d43697910d305585861866518489
Basak <chgy2131@naver.com>
2022-11-15 17:26:59 +0900

    feat(Application): 동작 중 예외 발생시 이를 외부로 알리는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,26 +5,20 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // 클래스 선언
         Store store = new Store();
         Admin admin = new Admin(store);
         Buyer buyer = new Buyer(store);
 
-        // 로또 구매 입력
-        List<Lotto> lottos = new ArrayList<Lotto>();
-        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
-        lottos.add(new Lotto(List.of(7, 8, 9, 10, 11, 12)));
-        lottos.add(new Lotto(List.of(19, 17, 16, 15, 14, 13)));
-
-        // 당첨 번호 설정
-        admin.inputGameNumbers();
-
-        // 당첨 내역 확인
-        LottoResult lr = store.checkPrize(lottos);
-        System.out.println("1등 " + lr.countPrize(Prize.PRIZE1));
-        System.out.println("2등 " + lr.countPrize(Prize.PRIZE2));
-        System.out.println("3등 " + lr.countPrize(Prize.PRIZE3));
-        System.out.println("4등 " + lr.countPrize(Prize.PRIZE4));
-        System.out.println("5등 " + lr.countPrize(Prize.PRIZE5));
+        try {
+            // 로또 구매금액 입력 및 당첨 번호 입력
+            int seedMoney = buyer.inputSeedMoney();
+            buyer.buyLotto(seedMoney);
+            admin.inputGameNumbers();
+            // 당첨 내역 확인
+            LottoResult lottoResult = buyer.checkPrize(buyer.getMyLottos());
+            buyer.checkRate(seedMoney, (int) lottoResult.getTotalMoney());
+        } catch (IllegalArgumentException | NullPointerException e) {
+            System.out.println(e.getMessage()); // 에러 구문 전달
+        }
     }
 }
