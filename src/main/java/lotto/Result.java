c2347f326b4cc2f88aec44de582056e8069c8c8d
thdwoqor <celebarte01@gmail.com>
2022-11-15 15:17:25 +0900

    feat: 객체 비교를 위한 `equals` 메소드 오버라이딩

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Objects;
+
 public class Result {
     private final int match;
     private final boolean bonus;
@@ -15,5 +17,13 @@ public class Result {
 
     public boolean isBonus() {
         return bonus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Result compareResult = (Result) o;
+        return Objects.equals(match, compareResult.match) && Objects.equals(bonus, compareResult.bonus);
     }
 }
