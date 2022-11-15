2e4ac3d5258772729d1d09551d455fcc77cd6201
ijung-won <dlwnddnjs96@gmail.com>
2022-11-15 18:50:54 +0900

    test(UserTest): 로또 구매 시 금액에 따른 반환 개수 오류 테스트)

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 
 class UserTest {
 
+    private final User user = new User();
+
+    @Test
+    public void 로또구매시_개수_오류여부(){
+        assertThatThrownBy(() ->user.buyingLotto("234"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertEquals(14, user.buyingLotto("14000"));
+    }
 }
