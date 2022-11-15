4528407a4b6b180677289dd3d75d70d71522ff5e
dlrjs2360 <lhg3670@naver.com>
2022-11-15 17:46:22 +0900

    :sparkles: feat: Test 통과하도록 구조 수정
    구현내용
    - Lotto 도메인에 테스트 기능 구현
    - ApplicationTest exception 테스트 코드 수정

--- src/main/java/lotto/entity/Lotto.java
+++ src/main/java/lotto/entity/Lotto.java
@@ -1,6 +1,13 @@
 package lotto.entity;
 
+import lotto.Enum.LottoInfo;
+import lotto.utils.Validation;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,8 +18,12 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != numbers.size()) {
+            Validation.throwException("[ERROR] 중복된 번호는 입력할 수 없습니다.");
+        }
+        if (numbers.size() > LottoInfo.COUNT.getInfo()) {
+            Validation.throwException(LottoInfo.COUNT.getInfo()+"개의 숫자를 입력해 주세요.");
         }
     }
 
