d4b25ef81a55bfddbcab8de1976ab70beec10204
beombu <sunil1369@bible.ac.kr>
2022-11-15 18:22:14 +0900

    Feat(Input) : 천원 단위만 입력 가능하도록 하는 기능 추가

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -3,12 +3,18 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Input {
+    public static final String INPUT_MONEY_THOUSAND_ERROR = "[ERROR] 금액이 천원 단위가 아닙니다.";
+
     public static int inputMoney() {
-        return Integer.parseInt(Console.readLine());
+        String money = Console.readLine();
+        validateInputMoneyThousand(money);
+        return Integer.parseInt(money);
     }
 
     public static List<Integer> inputPrizeNumbers() {
@@ -21,5 +27,13 @@ public class Input {
 
     public static int inputBonusNumber() {
         return Integer.parseInt(Console.readLine());
+    }
+
+    private static void validateInputMoneyThousand(String money){
+        Pattern pattern = Pattern.compile("^[0-9]*[0]{3}$");
+        Matcher matcher = pattern.matcher(money);
+        if(!matcher.find()){
+            throw new IllegalArgumentException(INPUT_MONEY_THOUSAND_ERROR);
+        }
     }
 }
