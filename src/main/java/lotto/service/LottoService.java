578be7e8291bc97aad9241ef3b7789da8c531c71
Lemon Soju <yckk980320@naver.com>
2022-11-15 09:25:53 +0900

    feat(LottoService): function that count the number of matching numbers

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -10,6 +10,7 @@ import static lotto.view.InputView.*;
 import static lotto.view.OutputView.printLottoNumber;
 
 public class LottoService {
+    static final int LOTTO_SIZE = 6;
     public static void lottoService() {
         int count, bonus;
         Lotto inputLottoNumber;
@@ -34,5 +35,15 @@ public class LottoService {
             output.add(Integer.parseInt(e));
         }
         return output;
+    }
+
+    public static Integer countMatchingNumber(Lotto issuedLotto, Lotto inputLotto) {
+        int cnt = 0;
+        for (int i = 0; i < LOTTO_SIZE; i++) {
+            if (issuedLotto.getNumbers().contains(inputLotto.getNumbers().get(i))) {
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
