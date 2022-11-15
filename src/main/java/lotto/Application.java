d155249a2374c96bc9b9a18171daa4b122867b62
jjmin9797 <jjmin9797@naver.com>
2022-11-15 17:21:08 +0900

    refactor(Application): try/catch 위치 이동 Application.java -> LottoApplication.java 이동
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,12 +2,6 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        LottoApplication lottoApplication = new LottoApplication();
-        try {
-            lottoApplication.run();
-        }catch (IllegalArgumentException e){
-
-        }
-
+        LottoApplication.run();
     }
 }
--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -8,12 +8,14 @@ public class LottoApplication {
     private static UserController userController = new UserController();
 
     public static void run() {
-        userController.savePurchaseAmount();
-        userController.showPurchaseInfo();
-        userController.randomLotteryIssuanceAndShow();
-        userController.enterWinningNumbersAndSave();
-        userController.enterBonusNumberAndSave();
-        userController.showWinningStatisticsAndSave();
-        userController.showRateOfReturnAndSave();
+        try {
+            userController.savePurchaseAmount();
+            userController.showPurchaseInfo();
+            userController.randomLotteryIssuanceAndShow();
+            userController.enterWinningNumbersAndSave();
+            userController.enterBonusNumberAndSave();
+            userController.showWinningStatisticsAndSave();
+            userController.showRateOfReturnAndSave();
+        } catch (IllegalArgumentException e) {}
     }
 }
