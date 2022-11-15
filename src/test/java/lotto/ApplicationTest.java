e1e79e7f9a150c8451e92bcf50469ab43a1eba35
황민주 <minjoo@hwangminjooui-MacBookAir.local>
2022-11-15 13:39:30 +0900

    test(ApplicationTest): findLottoResult, findTotalLottoResult, calculateTotalPrizeAmount, calculateProfitPercentage 의 테스트 메소드 추가 (src/test/java/lotto/ApplicationTest.java)
    테스트 메소드 작성:
    -findLottoResult, findTotalLottoResult, calculateTotalPrizeAmount, calculateProfitPercentage를 테스트 하기 위해 추가함

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,13 +1,17 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import jdk.jshell.execution.Util;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -51,6 +55,72 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() -> {
             runException("1000j");
             assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void _1000_으로_나누어떨어지지_않는_경우() {
+        assertSimpleTest(() -> {
+            runException("100020");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 로또_리스트가_숫자가_아닌_경우() {
+        List <String> input = Arrays.asList("1","2","23","a","22", "11");
+        assertSimpleTest(() -> {
+            assertThat(Exception.checkWinningNumbersNumeric(input)).isEqualTo(false);
+        });
+    }
+
+    @Test
+    void test_findLottoResult() {
+        List <Integer> lotto = Arrays.asList(12, 15, 25, 26, 43, 44);
+        List <Integer> winningNumbers = Arrays.asList(12, 15, 24, 26, 43, 44);
+        int bonusNumber = 1;
+        int result = 2;
+        assertSimpleTest(() -> {
+            assertThat(Utility.findLottoResult(lotto, winningNumbers, bonusNumber)).isEqualTo(result);
+        });
+    }
+
+    @Test
+    void test_findTotalLottoResult() {
+        List <Lotto> purchaseLotto = new ArrayList<>();
+        List <Integer> lottoNumbers1 = Arrays.asList(12, 15, 25, 26, 43, 44);
+        List <Integer> lottoNumbers2 = Arrays.asList(12, 15, 23, 25, 43, 44);
+        List <Integer> lottoNumbers3 = Arrays.asList(12, 15, 23, 25, 43, 44);
+        Lotto lotto1 = new Lotto(lottoNumbers1);
+        Lotto lotto2 = new Lotto(lottoNumbers2);
+        Lotto lotto3 = new Lotto(lottoNumbers3);
+        purchaseLotto.add(lotto1);
+        purchaseLotto.add(lotto2);
+        purchaseLotto.add(lotto3);
+        List <Integer> winningNumbers = Arrays.asList(12, 15, 24, 26, 43, 44);
+        int bonusNumber = 25;
+        List <Integer> result = Arrays.asList(0,1,0,2,0,0);
+        assertSimpleTest(() -> {
+            assertThat(Utility.findTotalLottoResult(purchaseLotto, winningNumbers, bonusNumber)).isEqualTo(result);
+        });
+    }
+
+    @Test
+    void test_calculateTotalPrizeAmount() {
+        List <Integer> lottoResult = Arrays.asList(0, 3, 0, 2, 1, 4);
+        long result = 90105000;
+        assertSimpleTest(() -> {
+            assertThat(Utility.calculateTotalPrizeAmount(lottoResult)).isEqualTo(result);
+        });
+    }
+
+    @Test
+    void test_calculateProfitPercentage() {
+        long spentAmount = 10000;
+        long totalPrizeAmount = 90105000;
+        double result = 901050;
+        assertSimpleTest(() -> {
+            assertThat(Utility.calculateProfitPercentage(spentAmount, totalPrizeAmount)).isEqualTo(result);
         });
     }
 
