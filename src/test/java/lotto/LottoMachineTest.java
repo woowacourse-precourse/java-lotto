b46783833385ba4565837dd4a89c8cbe0a1c492d
Seungyong Choi <ppyooy336@naver.com>
2022-11-15 15:27:34 +0900

    test:  숫자 6개를 오름차순으로 정렬한다 테스트 코드 작성

---
+++ src/test/java/lotto/LottoMachineTest.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+
+class LottoMachineTest {
+
+    @Test
+    public void 로또_발행_오름차순_테스트(){
+        Lotto lotto = LottoMachine.createLotto();
+        List<Integer> numbers = lotto.getNumbers();
+
+        Assertions.assertThat(numbers.get(0))
+                .isLessThan(numbers.get(1));
+        Assertions.assertThat(numbers.get(4))
+                .isLessThan(numbers.get(5));
+    }
+}
