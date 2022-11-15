f0f030af16a03c979e87c9f85364c626b97e78fc
youswim <youswim96@gmail.com>
2022-11-15 16:13:46 +0900

    fix(lotto): 예외 이름 오타 수정

--- src/main/java/lotto/ExceptionHandler.java
+++ src/main/java/lotto/ExceptionHandler.java
@@ -2,8 +2,9 @@ package lotto;
 
 public class ExceptionHandler {
 
-    public final static String WORNG_PURCHASE_MONEY = "구입 금액은 숫자이고, 1,000원 단위입니다";
-    public final static String WORNG_WIN_NUMBERS = "당첨 숫자는 숫자이고, 콤마로 구분된 6자리 입니다";
+    public final static String WRONG_PURCHASE_MONEY = "구입 금액은 숫자이고, 1,000원 단위입니다";
+    public final static String WRONG_WIN_NUMBERS = "당첨 번호는 숫자이고, 콤마로 구분된 6자리 입니다";
+    public final static String WRONG_BONUS_NUMBERS = "보너스 번호는 숫자이고, 범위는 1~45입니다";
 
 
 }
