e626cbbed6d7ef2b461872e4248803cf2359811f
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-15 14:22:33 +0900

    feat(OutputView): 출력에 필요한 상수 String 필드 추가 정의

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,16 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.domain.Reward;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
+    private static final String LOTTO_STATISTICS_MESSAGE = "당첨 통계";
+    private static final String DIVISION_LINE = "---";
+    private static final String LOTTO_RESULT_MESSAGE = "- %d개";
 
     public static void printPurchasedLotto(int lottoCount) {
         System.out.println(lottoCount + PURCHASE_MESSAGE);
