d3e1de1d15f96726f3ac99727fbd9c5620b0415a
권태현 <kygkth2011@gmail.com>
2022-11-15 18:18:28 +0900

    fix(user): 금액 입력값 변환 후 무조건 Exception이 발생하던 오류 해결

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -15,8 +15,6 @@ public class User {
             return Integer.parseInt(userInput);
         } catch (Exception e) {
             System.out.println("[ERROR] 숫자가 아닌 값이 입력되었습니다.");
-            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값이 입력되었습니다.");
-        } finally {
             throw new NoSuchElementException("[ERROR] 숫자가 아닌 값이 입력되었습니다.");
         }
     }
