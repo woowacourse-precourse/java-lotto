51487167a0379eff60a6a1e64d2ff46b0b9dbb71
minju412 <ln8847@ajou.ac.kr>
2022-11-15 22:04:32 +0900

    test(ProfitTest): use CsvSource

--- src/test/java/lotto/domain/result/ProfitTest.java
+++ src/test/java/lotto/domain/result/ProfitTest.java
@@ -11,6 +11,8 @@ import lotto.domain.lottery.WinningLotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class ProfitTest {
 
@@ -21,34 +23,22 @@ class ProfitTest {
         fixedNumberGenerator = new FixedNumberGenerator();
     }
 
-    @Test
-    @DisplayName("총 수익을 계산합니다")
-    void calculateProfit() {
-        Cost cost = new Cost("8000");
-        WinningLotto winningLotto = new WinningLotto("1,2,3,5,10,12");
-        BonusNumber bonusNumber = new BonusNumber("8", winningLotto);
+    @ParameterizedTest
+    @CsvSource({
+            "'8000', '1,2,3,5,10,12', '8', 400_000",
+            "'200000', '1,2,3,4,5,6', '8', 400_000_000_000"
+    })
+    public void calculateProfit(String purchaseCost, String winningNumbers, String bonus, long expected) {
+        Cost cost = new Cost(purchaseCost);
+        WinningLotto winningLotto = new WinningLotto(winningNumbers);
+        BonusNumber bonusNumber = new BonusNumber(bonus, winningLotto);
         LottoGroup lottoGroup = new LottoGroup(cost.getPurchaseCount(), fixedNumberGenerator);
         HitResult result = new HitResult(lottoGroup, winningLotto, bonusNumber);
 
         Profit profit = new Profit(cost.getCost(), result);
 
         assertThat(profit.calculateProfit(result))
-                .isEqualTo(400_000L);
-    }
-
-    @Test
-    @DisplayName("총 수익을 계산합니다 - 큰 수의 경우")
-    void calculateProfit_large() {
-        Cost cost = new Cost("200000");
-        WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6");
-        BonusNumber bonusNumber = new BonusNumber("8", winningLotto);
-        LottoGroup lottoGroup = new LottoGroup(cost.getPurchaseCount(), fixedNumberGenerator);
-        HitResult result = new HitResult(lottoGroup, winningLotto, bonusNumber);
-
-        Profit profit = new Profit(cost.getCost(), result);
-
-        assertThat(profit.calculateProfit(result))
-                .isEqualTo(400_000_000_000L);
+                .isEqualTo(expected);
     }
 
     @Test
