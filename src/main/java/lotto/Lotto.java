e9bfb89cf6de1c2fb301b5561092f06cf769f54f
JUNBEOM_LEE <bom1215@naver.com>
2022-11-15 12:49:48 +0900

    feat(Size_validate method in Class Lotto) Add IllegalArgumentException function
    Add IllegalArgumentException function

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,38 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+
+
+        size_validate(numbers);
         this.numbers = numbers;
+
     }
 
-    private void validate(List<Integer> numbers) {
+    private void size_validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자 6개이어야 합니다.");
         }
+        Set<Integer> numbers_to_set = new HashSet<Integer>(numbers);
+        List<Integer> Deduplicated_Numbers =new ArrayList<Integer>(numbers_to_set);
+        if(Deduplicated_Numbers.size() !=6){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 서로 다른 숫자 6개이어야 합니다.");
+        }
+
     }
+
+
+
 
     // TODO: 추가 기능 구현
 }
