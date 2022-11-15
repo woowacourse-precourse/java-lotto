a48ec4ad1579ada91d27307b6f2303cef9410199
이병3 <101378867+SongJSeop@users.noreply.github.com>
2022-11-15 23:34:42 +0900

    예외처리 테스트 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -21,16 +21,18 @@ public class Application {
     static int HowManyLotto() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
-        if (!(IsNum(input))) throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
-        int input_money = Integer.parseInt(input);
-        int user_buy_num = input_money / 1000;
-
-        if (input_money % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 1000원 단위의 금액을 입력해 주세요.");
+        try {
+            int input_money = Integer.parseInt(input);
+            int user_buy_num = input_money / 1000;
+            if (input_money % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 1000원 단위의 금액을 입력해 주세요.");
+            }
+            System.out.println("\n" + user_buy_num + "개를 구매했습니다.");
+            return user_buy_num;
+        } catch (Exception e) {
+            System.out.println("[ERROR] 숫자를 입력해 주세요.");
+            throw new NoSuchElementException();
         }
-
-        System.out.println("\n" + user_buy_num + "개를 구매했습니다.");
-        return user_buy_num;
     }
 
     static boolean IsNum(String input) {
