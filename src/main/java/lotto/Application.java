94cc3b2a4c59820f6ff2b0882cf0110afc36138e
5uhwann <5uhwann27@gmail.com>
2022-11-15 14:57:18 +0900

    feat(기능연결): LottoSystemController 구현을 통한 각 기능 연결

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import lotto.controller.LottoSystemController;
+import lotto.domain.cashier.CashierImpl;
+import lotto.domain.lotto.lottocompany.LottoCompanyImpl;
+import lotto.domain.lotto.lottocompany.LottoMachineImpl;
+import lotto.view.input.InputImpl;
+import lotto.view.output.OutputImpl;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoSystemController lottoSystemController = new LottoSystemController(new InputImpl(), new OutputImpl(),
+                new CashierImpl(),
+                new LottoCompanyImpl(new LottoMachineImpl()));
+
+        lottoSystemController.runLottoSystem();
     }
 }
