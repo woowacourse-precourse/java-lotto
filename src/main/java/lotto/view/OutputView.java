ffc1d72030177aae44f1a19f39d90530d22cd678
Ukj0ng <xb253q@gmail.com>
2022-11-15 23:16:37 +0900

    feat(outPutView): 당첨 통계와 총 수익률 출력 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,13 +1,17 @@
 package lotto.view;
 
-import lotto.domain.Lotto;
-import lotto.domain.Lottos;
+import lotto.domain.*;
 
 import java.util.stream.Collectors;
 
 public class OutputView {
 
     private final static String COUNT_OF_LOTTO_MESSAGE = "%d개를 구매했습니다.\n";
+    private static final String STATISTIC_RESULT_MESSAGE = "당첨 통계\n---";
+    private static final String WINNING_MESSAGE = "%d개 일치 (%d원) - %d개\n";
+    private static final String SECOND_MESSAGE = "%d개 일치, 보너스 볼 일치 (%d원)- %d개\n";
+    private static final String PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.\n";
+    private static final int ZERO = 0;
 
     public static void printCountOfLotto(int LottoCount) {
         System.out.println();
@@ -24,5 +28,28 @@ public class OutputView {
     public static void printLotto(Lotto lotto){
         System.out.println(lotto.getLotto().toString());
     }
+
+    public static void printStatistics(Statistic statistic) {
+        System.out.println(STATISTIC_RESULT_MESSAGE);
+
+        statistic.getStatistics()
+                .entrySet()
+                .stream()
+                .filter(statistics -> statistics.getKey().getCount() != ZERO)
+                .forEach(statistics -> {
+                    if (statistics.getKey().hasBonusBall()) {
+                        System.out.printf(SECOND_MESSAGE, Rank.SECOND.getCount(), Rank.SECOND.getWinningPrice(), statistics.getValue());
+                        return;
+                    }
+                    System.out.printf(WINNING_MESSAGE, statistics.getKey().getCount(), statistics.getKey().getWinningPrice(), statistics.getValue());
+                });
+    }
+
+    public static void printProfitRate(Statistic statistic, Price price) {
+        double profitRate = statistic.getProfitRate(price);
+        System.out.printf(PROFIT_RATE_MESSAGE, profitRate);
+    }
+
+
 
 }
