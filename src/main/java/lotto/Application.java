b71e65d81e2eee46f7b09bb40278c5e8b2290438
ckyeon <qsky8520@gmail.com>
2022-11-15 22:30:48 +0900

    feat(Application): implement 당첨 번호를 입력받는 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.Lotto;
+import lotto.domain.LottoShop;
 import lotto.ui.Guide;
+import org.assertj.core.util.Lists;
 
 public class Application {
 
@@ -12,5 +19,14 @@ public class Application {
     private static int askPurchaseAmount() {
         Guide.ASK_PURCHASE_AMOUNT.print();
         return Integer.parseInt(Console.readLine());
+    }
+
+    private static List<Integer> askWinningNumbers() {
+        Guide.ASK_WINNING_NUMBER.print();
+
+        String[] winningNumbers = Console.readLine().split(",");
+        return Arrays.stream(winningNumbers)
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
     }
 }
