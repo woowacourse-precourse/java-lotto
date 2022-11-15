5603e900391d0beccbc29e818ba5d6e117817753
Jenn Kim <grdd0419@gmail.com>
2022-11-15 19:46:59 +0900

    refactor: WinningResult 생성자에서 중복되는 기능 분리

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -11,61 +11,53 @@ import static lotto.domain.WinningType.getWinningType;
 import static lotto.view.OutputView.printEachWinningResult;
 
 public class WinningResult {
-
     private final Map<WinningType, Integer> results;
 
-    public WinningResult() {
+    public WinningResult(WinningLotto winningLotto, Lottos purchasedLottos) {
         this.results = new EnumMap<>(WinningType.class);
+        updateResults(winningLotto, purchasedLottos);
     }
 
-    public WinningResult(WinningLotto winningLotto, Lottos purchasedLottos) {
-
-        this.results = new EnumMap<>(WinningType.class);
-
+    public void updateResults(WinningLotto winningLotto, Lottos purchasedLottos) {
         for (Lotto lotto : purchasedLottos.getLottos()) {
-
-            boolean withBonus = false;
             int equalNumber = winningLotto.countEqualNumber(lotto);
 
             if (equalNumber == SECOND_WINNING) {
-                WinningType type = getWinningType(FIVE_EQUALS, true);
+                findTypeAndIncreaseResult(FIVE_EQUALS, true);
                 continue;
             }
-            WinningType type = getWinningType(equalNumber, false);
-            this.increaseOneByKey(type);
+            findTypeAndIncreaseResult(equalNumber, false);
         }
     }
 
-    public void increaseOneByKey(WinningType type) {
+    public void findTypeAndIncreaseResult(int equalNumber, boolean withBonus) {
+        WinningType type = getWinningType(equalNumber, withBonus);
+        this.increaseOneByKey(type);
+    }
 
+    public void increaseOneByKey(WinningType type) {
         if (results.containsKey(type)) {
             results.put(type, results.get(type) + 1);
             return;
         }
-
         results.put(type, 1);
     }
 
     public double calculateEarning(int inputMoney) {
-
         double earning = 0;
 
         Iterator<WinningType> winningTypeIterator = getWinningTypeIterator();
-
         while (winningTypeIterator.hasNext()) {
 
             WinningType type = winningTypeIterator.next();
             earning += (double) type.getWinnings() * (double) results.get(type);
         }
-        earning /= inputMoney;
-
-        return earning;
+        return earning / inputMoney;
     }
 
     public void print() {
 
         Iterator<WinningType> winningTypeIterator = getWinningTypeIterator();
-
         while (winningTypeIterator.hasNext()) {
 
             WinningType type = winningTypeIterator.next();
