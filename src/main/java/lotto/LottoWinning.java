f9de4d1d82e2e345e8825dc80dd2e841c521aed3
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-15 17:48:28 +0900

    feat: 출력을 위한 일치 갯수 반환 함수 생성

--- src/main/java/lotto/LottoWinning.java
+++ src/main/java/lotto/LottoWinning.java
@@ -14,6 +14,28 @@ public class LottoWinning {
             throw new IllegalArgumentException("보너스 번호와 당첨 번호는 일치할 수 없습니다.");
     }
 
+    public static double getBenefit(int[] count, Integer inputMoney) {
+        int[] money = {5000,50000,1500000,2000000000,30000000};
+        long benefit = 0;
+        for (int i = 0; i < 5; i++) {
+            benefit += money[i]*count[i];
+        }
+        return (double) (benefit * 100) / inputMoney;
+    }
+
+    public static int[] compareEqaulsLotto(List<List<Integer>> lottosList) {
+        int[] count = new int[5];
+        for (int i = 0; i < lottosList.size(); i++) {
+            int[] tmp = getEqulasCount(lottosList.get(i));
+            if (tmp[0] == 5 && tmp[1] == 1)
+                count[4] += 1;
+            else if (tmp[0] >= 3) {
+                count[tmp[0]-3] += 1;
+            }
+        }
+        return count;
+    }
+
     public static int[] getEqulasCount(List<Integer> lottoList) {
         int[] count = new int[2];
         for (int i = 0; i < 6; i++) {
