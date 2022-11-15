3ffe5df90981530847f050e708e110a80515fc8b
bradbrad97 <taehyun0121@naver.com>
2022-11-15 16:01:36 +0900

    feat(LottoResult): 입력한 당첨 번호가 올바르지 않다면 에러 메시지 출력하는 기능 추가

--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -1,22 +1,57 @@
 package lotto.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class LottoResult {
-    private List<Integer> winningNumber;
+    private static final int WINNING_NUMBER_SIZE = 6;
+    private static final int MIN_WINNING_NUMBER = 1;
+    private static final int MAX_WINNING_NUMBER = 45;
+    private Set<Integer> winningNumbers;
 
     public LottoResult(String winningNumber){
-        this.winningNumber = splitter(winningNumber);
+        validate(winningNumber);
+        this.winningNumbers = splitter(winningNumber);
+        checkDuplication();
     }
 
-    public List<Integer> getWinningNumber(){
-        return winningNumber;
+    public Set<Integer> getWinningNumbers(){
+        return winningNumbers;
     }
 
-    private List<Integer> splitter(String winningNumber){
+    public void validate(String winningNumber){
+        validatePattern(winningNumber);
+        validateNumberRange(winningNumber);
+    }
+
+    private void validatePattern(String winningNumber){
+        String pattern = "(\\d*,){5}\\d";
+
+        if (!winningNumber.matches(pattern)){
+            throw new IllegalArgumentException("[ERROR] 당첨번호는 6개의 숫자를 쉼표로 구분지어 입력해야 합니다.");
+        }
+    }
+
+    private void validateNumberRange(String winningNumber){
+        String numbers[] = winningNumber.split(",");
+
+        for (String s : numbers) {
+            int number = Integer.parseInt(s);
+
+            if (number < MIN_WINNING_NUMBER || number > MAX_WINNING_NUMBER) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
+    private void checkDuplication(){
+        if (winningNumbers.size() != WINNING_NUMBER_SIZE){
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private Set<Integer> splitter(String winningNumber){
         String[] numberInputs = winningNumber.split(",");
-        List<Integer> numbers = new ArrayList<>();
+        Set<Integer> numbers = new HashSet<>();
 
         for (String numberInput: numberInputs){
             numbers.add(Integer.parseInt(numberInput));
