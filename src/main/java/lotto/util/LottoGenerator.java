2e55fc21152fd773bbb87dee9f604b337b5c7f4b
SeongukBaek <bsu6235@gmail.com>
2022-11-15 14:06:37 +0900

    refactor: modify sort logic

--- src/main/java/lotto/util/LottoGenerator.java
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -2,13 +2,12 @@ package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoGenerator {
     public static List<Integer> generate() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        numbers.sort(Comparator.naturalOrder());
-        return numbers;
+        return numbers.stream().sorted().collect(Collectors.toList());
     }
 }
