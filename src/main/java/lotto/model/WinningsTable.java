5ab6797ad112494c1dd5fd815f115319d105f2b2
gomljo <gomljo0307@naver.com>
2022-11-15 13:33:10 +0900

    feat(WinnginsTable.java): 당첨 조건 표 테이블 생성

---
+++ src/main/java/lotto/model/WinningsTable.java
@@ -0,0 +1,25 @@
+package lotto.model;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public enum WinningsTable {
+    FIRST(List.of(6,0), "2,000,000,000"),
+    SECOND(List.of(5,1),"30,000,000"),
+    THIRD(List.of(5,0),"1,500,000"),
+    FOURTH(List.of(4,0),"50,000"),
+    FIFTH(List.of(3,0), "5,000"),
+    NOTHING(Collections.EMPTY_LIST, "0");
+
+    private static final int RAFFLE_MATCHES=0;
+
+    private final List<Integer>  NUMBER_OF_MATCHES;
+    private final String REVENUE;
+
+    WinningsTable(List<Integer> numberOfMatches, String revenue){
+        this.REVENUE=revenue;
+        this.NUMBER_OF_MATCHES=numberOfMatches;
+    }
+    
+}
