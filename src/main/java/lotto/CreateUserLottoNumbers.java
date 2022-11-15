93e5e5ba410044a75d11d75a23156b8251075c62
meunrud <y3a0522@gmail.com>
2022-11-15 19:21:16 +0900

    :recycle: 필드 변수 이름 수정

--- src/main/java/lotto/CreateUserLottoNumbers.java
+++ src/main/java/lotto/CreateUserLottoNumbers.java
@@ -3,23 +3,21 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class CreateUserLottoNumbers {
-    List<Lotto> userLottosNumber;
+    List<Lotto> userBuyLottosGroup;
 
     public CreateUserLottoNumbers() {
-        userLottosNumber = new ArrayList<>();
+        userBuyLottosGroup = new ArrayList<>();
     }
 
     public List<Lotto> createUserLottoNumber(int amount) {
         for(int i = 0; i<amount; i++){
             List<Integer> sample = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-            Collections.sort(sample);
-            userLottosNumber.add(new Lotto(sample));
+            userBuyLottosGroup.add(new Lotto(sample));
         }
-        return userLottosNumber;
+        return userBuyLottosGroup;
     }
 
 }
