8cb70c3e21afc21843104ce118f9f723a094005e
dingding-21 <phj8221@naver.com>
2022-11-15 15:52:59 +0900

    style: Output.lottoCount함수 인자 타입 int로 변경

--- src/main/java/lotto/ui/Output.java
+++ src/main/java/lotto/ui/Output.java
@@ -7,7 +7,7 @@ public class Output {
         System.out.println(OutputMessage.INPUT_PURCHASE_AMOUNT);
     }
 
-    public static void lottoCount(Integer count) {
-        System.out.println(count.toString() + OutputMessage.LOTTO_COUNT);
+    public static void lottoCount(int count) {
+        System.out.println(String.valueOf(count) + OutputMessage.LOTTO_COUNT);
     }
 }
