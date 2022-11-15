9a7a35e6ee8068b8f13f771e9644c058d080137f
p <gustjr2332@naver.com>
2022-11-15 17:24:46 +0900

    feat(Output) : 각 등수별 당첨 내역 출력 기능 추가, 함수 static 제거, 하드코딩 수정

--- src/main/java/UI/Output.java
+++ src/main/java/UI/Output.java
@@ -1,35 +1,58 @@
 package UI;
 
+import lotto.Ranking;
+
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Output {
     public static final String INPUTPRICE = "구입금액을 입력해 주세요.";
     public static final String INPUTWINNINGNUMBERS = "당첨 번호를 입력해 주세요.";
     public static final String INPUTBONUSNUMBER = "보너스 번호를 입력해 주세요";
+    public static final String BUYINGLOTTO = "%d개를 구매했습니다.\n";
+    public static final String WINSTATISTICS = "당첨 통계\n---\n";
+    public static final String NUMBEROFMATCH = "%d개 일치";
+    public static final String MATCHBOUNSNUMBER = ", 보너스 볼 일치";
+    public static final String PRIZEMONEY_AND_NUMBEROFWIN = " (%s원) - %d개\n";
 
-    public static void inputPrice() {
+    public void inputPrice() {
         System.out.println(INPUTPRICE);
     }
 
-    public static void inputWinningNumber() {
+    public void inputWinningNumber() {
         System.out.println(INPUTWINNINGNUMBERS);
     }
 
-    public static void inputBounsNumber() {
+    public void inputBounsNumber() {
         System.out.println(INPUTBONUSNUMBER);
     }
 
-    //구매한 내역 출력
-    public static void printRandomLottoNumbers(List<List<Integer>> randomLottoNumbers) {
+    public void printRandomLottoNumbers(List<List<Integer>> randomLottoNumbers) {
         int numberOfLotto = randomLottoNumbers.size();
-        System.out.printf("%d 개를 구매했습니다\n", numberOfLotto);
+        System.out.printf(BUYINGLOTTO, numberOfLotto);
 
         for (List<Integer> number : randomLottoNumbers) {
             System.out.println(number);
         }
     }
 
+    public void printLottoRankingResult(Map<Ranking, Integer> rankingResults) {
+        System.out.print(WINSTATISTICS);
 
-    //당첨통계 출력
+        Set<Ranking> rankings = rankingResults.keySet();
+
+        for (Ranking ranking : rankings) {
+            int match = ranking.getMatch();
+            String money = ranking.getMoney();
+            Integer numberOfWin = rankingResults.get(ranking);
+
+            System.out.printf(NUMBEROFMATCH, match);
+            if (ranking.isBonusStatus()) {
+                System.out.print(MATCHBOUNSNUMBER);
+            }
+            System.out.printf(PRIZEMONEY_AND_NUMBEROFWIN, money, numberOfWin);
+        }
+    }
 
 }
