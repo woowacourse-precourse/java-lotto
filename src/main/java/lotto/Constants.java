033249562f6d3cac94f7f29488a9a3fb00592372
Park Sungchul <try615@naver.com>
2022-11-15 19:19:53 +0900

    feat(Util.java): 문자열에서 숫자 형변환 기능 추가
    String을 Int 타입으로 형변환하는 메소드 추가.
    만일 숫자가 아닐 시 에러 출력.

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -2,4 +2,6 @@ package lotto;
 
 public class Constants {
 	public static final String PRINT_INPUT_MONEY = "구입금액을 입력해 주세요.";
+
+	public static final String ERROR_NOT_NUMBER_FORMAT = "[ERROR] 숫자를 입력해 주십시오.";
 }
--- src/main/java/lotto/Util.java
+++ src/main/java/lotto/Util.java
@@ -2,4 +2,11 @@ package lotto;
 
 public class Util {
 
+	public static int strToInt(String word) {
+		try {
+			return Integer.parseInt(word);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(Constants.ERROR_NOT_NUMBER_FORMAT);
+		}
+	}
 }
