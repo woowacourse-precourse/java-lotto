7856a9114f1461c2d57899bc3987133a5b5eb634
GyuJeGal <lkjdee0329@gmail.com>
2022-11-15 16:41:01 +0900

    feat(InputPattern): 입력값 패턴 나열 상수 타입의 패턴 나열

---
+++ src/main/java/lotto/constant/InputPattern.java
@@ -0,0 +1,6 @@
+package lotto.constant;
+
+public class InputPattern {
+    private static final String INPUT_AMOUNT_PATTERN = "^[0-9]*$";
+    private static final String WINNING_NUMBER_PATTERN = "^(,|[0-9])*$";
+}
