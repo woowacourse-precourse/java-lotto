cf91b3441563e218509b465ac41187b52e2340f4
YIMHYEONGSUP <hs.yim@illb.co.kr>
2022-11-15 12:51:13 +0900

    feat - 구입 금액 클래스

---
+++ src/main/java/lotto/Account.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import java.util.regex.Pattern;
+
+public class Account {
+
+    private int account;
+
+    public Account (final String input) {
+        if(validAccount(input)){
+            this.account = 0;
+            IOUtils.errorMessage();
+            throw new IllegalArgumentException();
+        }
+        this.account = Integer.parseInt(input);
+    }
+
+    private static boolean validAccount(String input) {
+        // 부정형..
+        if(! Pattern.matches("[0-9]+" , input)){
+            IOUtils.errorMessage();
+            return true;
+        }
+        int account = Integer.parseInt(input);
+        if (account % 1000 == 0) {
+            return false;
+        }
+        IOUtils.errorMessage();
+        return true;
+    }
+
+    public int account() {
+        return account;
+    }
+}
