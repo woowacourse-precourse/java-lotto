548c04f8beeeaa4599dbd6beb8d5d040e7bb6165
juno-junho <ssmm0205@naver.com>
2022-11-15 22:55:48 +0900

    feat(lottos): add method createAutoLottos
    자동으로 난수 생성하는 메서드 추가

--- src/main/java/lotto/LottoFactory.java
+++ src/main/java/lotto/LottoFactory.java
@@ -1,8 +1,19 @@
 package lotto;
 
+import utils.NumberGenerator;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoFactory {
+
+    public static final List<Lotto> lottos = new ArrayList<>();
+
+    public static List<Lotto> createAutoLottos( int count) {
+        for (int i = 0; i < count; i++) {
+            lottos.add(Lotto.from(NumberGenerator.generateSixRandomNumber()));
+        }
+        return lottos;
+    }
 
 }
