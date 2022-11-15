ec560c80212605c10c826ad206e1fd05b18d1d71
kim1387 <kevink0@naver.com>
2022-11-15 17:24:17 +0900

    feat: 로또관련 출력 로직 추가

--- src/main/java/lotto/view/controller/LottoNumberView.java
+++ src/main/java/lotto/view/controller/LottoNumberView.java
@@ -6,6 +6,7 @@ import lotto.view.vlidation.InputCorrectLottoValidator;
 import java.util.List;
 
 import static lotto.utils.ConvertUtil.convertInputToIntegerList;
+import static lotto.view.controller.ConsoleMessage.*;
 import static lotto.view.vlidation.InputMoneyValidator.validateBlank;
 
 public class LottoNumberView {
@@ -14,7 +15,7 @@ public class LottoNumberView {
     }
 
     public static List<Integer> inputLottoNumbers() {
-        System.out.println(ConsoleMessage.INPUT_CORRECT_LOTTO_MONEY);
+        System.out.println(INPUT_CORRECT_LOTTO_MONEY);
         String input = Console.readLine();
         validateBlank(input);
         List<Integer> newLottoNumber = convertInputToIntegerList(input);
@@ -22,4 +23,11 @@ public class LottoNumberView {
         return newLottoNumber;
     }
 
+    public static void printTotalLottoCount(int lottoCount) {
+        System.out.println(String.valueOf(lottoCount) + NUMBER_COUNT_UNITS + PRINT_BUY);
+    }
+
+    public static void printLottoNumbers(List<Integer> lotto) {
+        System.out.println(lotto);
+    }
 }
