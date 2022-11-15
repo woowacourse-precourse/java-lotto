537861ae19c0f7fba26d360f7a1f9694b40858ec
song-wooseok <sws981202@gmail.com>
2022-11-15 18:22:04 +0900

    test: 로또 등수 내역 계산 테스트

---
+++ src/test/java/lotto/LottoRankTest.java
@@ -0,0 +1,51 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
+import lotto.domain.LottoStatistic;
+import lotto.domain.LottoWinNumbers;
+
+import lotto.domain.Lottos;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoRankTest {
+
+    @DisplayName("1등을 맞추었을 때 LottoRank FIRST 의 winCount 카운트가 1로 올라감")
+    @Test
+    void caseFirstPrize(){
+        LottoWinNumbers lottoWinNumbers = new LottoWinNumbers(List.of(1,2,3,4,5,6));
+        lottoWinNumbers.setBonus(7);
+
+        Lottos lottos = new Lottos();
+        lottos.addLotto(new Lotto(List.of(1,2,3,4,5,6)));
+
+        LottoStatistic result = new LottoStatistic(lottos, lottoWinNumbers);
+        result.checkLottosWin();
+
+        assertThat(LottoRank.FIRST.getWinCount()).isEqualTo(1);
+    }
+
+    @DisplayName("1등과 2등이 당첨되었을 경우 LottoRank FIRST 와 SECOND winCount 카운트가 1로 올라감")
+    @Test
+    void caseFirstPrizeSecondPrize(){
+        LottoWinNumbers lottoWinNumbers = new LottoWinNumbers(List.of(1,2,3,4,5,6));
+        lottoWinNumbers.setBonus(7);
+
+        Lottos lottos = new Lottos();
+        lottos.addLotto(new Lotto(List.of(1,2,3,4,5,6)));
+        lottos.addLotto(new Lotto(List.of(1,2,3,4,5,7)));
+
+        LottoStatistic result = new LottoStatistic(lottos, lottoWinNumbers);
+        result.checkLottosWin();
+
+        assertThat(LottoRank.FIRST.getWinCount()).isEqualTo(1);
+        assertThat(LottoRank.SECOND.getWinCount()).isEqualTo(1);
+    }
+
+}
