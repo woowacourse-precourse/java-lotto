1e9258d23c9f161c12c114a1d6625240e7856595
DDing77 <devseo2021@gmail.com>
2022-11-15 20:22:05 +0900

    feat(controller) : Lottery 클래스 작성 및 애플리케이션 로직 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import controller.Lottery;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Lottery lottery = new Lottery();
+        lottery.start();
     }
 }
