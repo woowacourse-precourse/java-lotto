b1e6f866c850475e73b3f2e221743e3564afaef9
hhiyeon <hhyyeon0214@gmail.com>
2022-11-15 15:30:02 +0900

    test: money(test)-check buy number is not numeric

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,4 +1,17 @@
 package lotto.domain;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class MoneyTest {
+    @DisplayName("구입 금액이 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void checkBuyMoneyIsNotNumeric() {
+        assertThatThrownBy(() -> new Money("100d0"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
