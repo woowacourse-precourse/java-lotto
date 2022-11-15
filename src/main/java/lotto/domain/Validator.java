da62913fa87138cc6f0f16233139199911d81409
kkimhaji <kkim.haji@gmail.com>
2022-11-15 23:25:46 +0900

    fix: check bonus number & IllegalArgumentsException
    콤마가 아닌 다른 문자로 구분됐을 때 ErrorMessage 출력하도록 변경

--- src/main/java/lotto/domain/Validator.java
+++ src/main/java/lotto/domain/Validator.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import lotto.enums.ErrorMessage;
 import lotto.enums.Range;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -20,19 +21,26 @@ public class Validator {
                 numbers.add(n);
             }
         }
+        validateLength(numbers);
         checkDuplicated(numbers);
         return numbers;
     }
 
-    private String[] winNumCommaValidate(String inputNum){
+    private String[] winNumCommaValidate(String inputNum) {
         String[] inputN = new String[6];
-        try{
-            inputN = inputNum.split(",");
-        }catch (Exception e){
+        boolean check = true;
+        for (int i = 0; i < Range.DIGIT.get(); i++) {
+            commaValidate(inputNum.charAt(i));
+        }
+        inputN = inputNum.split(",");
+        return inputN;
+    }
+
+    private boolean commaValidate(char c) {
+        if (!Character.isDigit(c) && c != ',') {
             throw new IllegalArgumentException(ErrorMessage.COMMA_ERROR.getErrorMessage());
         }
-
-        return inputN;
+        return true;
     }
 
     public int stringToInt(String s) {
@@ -40,12 +48,13 @@ public class Validator {
         return Integer.parseInt(s);
     }
 
-    private void validateNumber(String s) {
+    private boolean validateNumber(String s) {
         for (int i = 0; i < s.length(); i++) {
             if (!Character.isDigit(s.charAt(i))) {
                 throw new IllegalArgumentException(ErrorMessage.NUM_ERROR.getErrorMessage());
             }
         }
+        return true;
     }
 
     private boolean validateNumRange(int idxNum) {
@@ -76,13 +85,13 @@ public class Validator {
         }
     }
 
-    private void bonusDuplicate(int bonus, List<Integer> numbers){
-        if (numbers.contains(bonus)){
+    private void bonusDuplicate(int bonus, List<Integer> numbers) {
+        if (numbers.contains(bonus)) {
             throw new IllegalArgumentException(ErrorMessage.BONUS_DUPLICATE_ERROR.getErrorMessage());
         }
     }
 
-    public int bonusTotal(String stringBonus, List<Integer> numbers){
+    public int bonusTotal(String stringBonus, List<Integer> numbers) {
         int bonus = stringToInt(stringBonus);
         bonusDuplicate(bonus, numbers);
         validateNumRange(bonus);
