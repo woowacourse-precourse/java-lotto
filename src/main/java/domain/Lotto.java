bb6bce2c8994783def32fbfd63d632bd53cb75b9
thousandcows <milcamboo@gmail.com>
2022-11-15 21:20:21 +0900

    refactor: create validator class and move regarding functions
    Moved following functions
    - sizeValidator
    - checkDuplication
    - checkNumberRange
    - checkIfTheInputIsInteger
    - validatePayment

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -5,50 +5,28 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import Utils.Validator;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private final Validator validator;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        this.validator = new Validator();;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
 
-        checkDuplication(numbers);
+        validator.sizeValidator(numbers);
+
+        validator.checkDuplication(numbers);
 
         for (Integer number : numbers) {
-            checkNumberRange(number);
-        }
-    }
-
-    public static void validatePayment(String payment) {
-
-        checkIfTheInputIsInteger(payment);
-
-        if (Integer.parseInt(payment) % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 1000원 단위로 떨어지는 숫자를 입력해주세요.");
-        }
-
-    }
-
-    private void checkNumberRange(int n) {
-        if (n < 1 || n > 45) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-    }
-
-    private void checkDuplication(List<Integer> lottoNumbers) {
-        HashSet<Integer> hashSet = new HashSet<>(lottoNumbers);
-
-        if (hashSet.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 중복된 숫자는 입력할 수 없습니다.");
+            validator.checkNumberRange(number);
         }
     }
 
@@ -112,9 +90,9 @@ public class Lotto {
         return String.format("%.1f", (double) totalPrize / totalPayment * 100);
     }
 
-    public static int inputTotalPayment() {
+    public int inputTotalPayment() {
         String payment = Console.readLine();
-        validatePayment(payment);
+        validator.validatePayment(payment);
         return Integer.parseInt(payment);
     }
 
@@ -123,25 +101,19 @@ public class Lotto {
         return Integer.parseInt(bonus);
     }
 
-    public static List<Integer> inputLottoNumber(String input) {
+    public List<Integer> inputLottoNumber(String input) {
         StringTokenizer st = new StringTokenizer(input, ",");
         List<Integer> winningLottoNumber = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
             String currentDigit = st.nextToken();
 
-            checkIfTheInputIsInteger(currentDigit);
+            validator.checkIfTheInputIsInteger(currentDigit);
 
             winningLottoNumber.add(Integer.parseInt(currentDigit));
 
         }
 
         return winningLottoNumber;
-    }
-
-    public static void checkIfTheInputIsInteger(String digit) {
-        if (!digit.matches("^[0-9]*$")) {
-            throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
-        }
     }
 
     public static void showLottoPaymentReceipt(int totalPayment, List<List<Integer>> generatedLotto) {
