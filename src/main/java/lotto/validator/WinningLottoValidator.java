f189cc1f6e93f6c0c0ebdea9cabe0ab74bda038c
leesangjin <tkdwls0628@gmail.com>
2022-11-15 17:36:12 +0900

    feat(WinningLottoValidator): 6개의 당첨 번호 입력 검증 구현

--- src/main/java/lotto/validator/WinningLottoValidator.java
+++ src/main/java/lotto/validator/WinningLottoValidator.java
@@ -1,2 +1,18 @@
-package lotto.validator;public class WinningLottoValidator {
+package lotto.validator;
+
+import lotto.constant.Constant;
+import lotto.constant.ErrorMessage;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class WinningLottoValidator {
+    public static void validateWinningNumber(String userInput) {
+        try {
+            Arrays.stream(userInput.split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (RuntimeException e) {
+            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_WRONG_TYPE);
+        }
+    }
 }
