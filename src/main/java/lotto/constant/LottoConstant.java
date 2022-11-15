25548c616e793ea78d21b17ee66d4c08ecbc653b
kimJaeHyeon <kmr2644@gmail.com>
2022-11-15 16:58:36 +0900

    feat: 로또 금액단위 상수로 구현

--- src/main/java/lotto/constant/LottoConstant.java
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -4,4 +4,5 @@ public class LottoConstant {
     public static final int MAX_LOTTO_NUMBER = 45;
     public static final int LOW_LOTTO_NUMBER = 1;
     public static final int WINNING_NUMBER_SIZE = 6;
+    public static final int AMOUNT_UNIT = 1000;
 }
