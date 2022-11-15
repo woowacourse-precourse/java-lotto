a2b0d97dc7a40e3015d1358934d39fea747eedd1
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-15 22:51:03 +0900

    feat: InputView 클래스의 필요한 객체 생성

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.UserException;
+
+public class InputView {
+    private final UserException userException = new UserException();
+
+
+}
