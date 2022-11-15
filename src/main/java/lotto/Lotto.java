8ba968a2f49041095c0bce2fdab1bb7829799375
hyejuc <chjjudy@naver.com>
2022-11-15 23:23:53 +0900

    feat: Lotto 예외 점검 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,7 +12,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 총 6개여야 합니다.");
+        }
+        if (Validator.isDuplicated(numbers)){
+            throw new IllegalArgumentException("[ERROR] 번호는 중복되지 않는 6개의 숫자로 이루어져야 합니다.");
+        }
+        if (!Validator.isProperRange(numbers, 1,45)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
