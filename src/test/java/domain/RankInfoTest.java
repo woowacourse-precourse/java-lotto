b2b18c2ce2d9d1d06368226f871e45c28a3aa2d9
1eejisoo <jisu991012@gmail.com>
2022-11-15 14:49:56 +0900

    test: 랭크 테스트 추가

--- src/test/java/domain/RankInfoTest.java
+++ src/test/java/domain/RankInfoTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import domain.Money.Money;
 import domain.RankInfo.RankInfoController;
 import domain.Bonus.Bonus;
 import domain.Lotto.Lotto;
@@ -82,6 +83,110 @@ public class RankInfoTest extends NsTest {
                 "5개 일치 (1,500,000원) - 1개",
                 "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
                 "6개 일치 (2,000,000,000원) - 1개"
+        );
+    }
+
+    @Test
+    void 수익률_테스트1() {
+        Money money = new Money(5000);
+        RankInfo rankInfo = rankInfoController.getRankInfo(
+                rankInfoController.createRankInfo(),
+                new Lotto(List.of(1,2,3,4,5,6)),
+                new UserLotto(List.of(
+                        List.of(1,2,3,7,8,9), // 3개 일치
+                        List.of(11,12,13,14,15,17),
+                        List.of(11,12,13,14,15,22),
+                        List.of(11,12,13,43,25,22),
+                        List.of(11,12,13,43,25,22))),
+                new Bonus(7));
+        outputMessage.printStatistic(rankInfo);
+        outputMessage.printYield(rankInfoController.getYield(rankInfo, money));
+
+        assertThat(output()).contains(
+                "3개 일치 (5,000원) - 1개",
+                "4개 일치 (50,000원) - 0개",
+                "5개 일치 (1,500,000원) - 0개",
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                "6개 일치 (2,000,000,000원) - 0개",
+                "총 수익률은 100.0%입니다."
+        );
+    }
+
+    @Test
+    void 수익률_테스트2() {
+        Money money = new Money(5000);
+        RankInfo rankInfo = rankInfoController.getRankInfo(
+                rankInfoController.createRankInfo(),
+                new Lotto(List.of(1,2,3,4,5,6)),
+                new UserLotto(List.of(
+                        List.of(1,2,3,7,8,9), // 3개 일치
+                        List.of(1,2,3,7,8,9), // 3개 일치
+                        List.of(11,12,13,14,15,22),
+                        List.of(11,12,13,43,25,22),
+                        List.of(11,12,13,43,25,22))),
+                new Bonus(7));
+        outputMessage.printStatistic(rankInfo);
+        outputMessage.printYield(rankInfoController.getYield(rankInfo, money));
+
+        assertThat(output()).contains(
+                "3개 일치 (5,000원) - 2개",
+                "4개 일치 (50,000원) - 0개",
+                "5개 일치 (1,500,000원) - 0개",
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                "6개 일치 (2,000,000,000원) - 0개",
+                "총 수익률은 200.0%입니다."
+        );
+    }
+
+    @Test
+    void 수익률_테스트3() {
+        Money money = new Money(5000);
+        RankInfo rankInfo = rankInfoController.getRankInfo(
+                rankInfoController.createRankInfo(),
+                new Lotto(List.of(1,2,3,4,5,6)),
+                new UserLotto(List.of(
+                        List.of(1,2,3,4,5,9), // 5개 일치
+                        List.of(11,12,13,14,15,22),
+                        List.of(11,12,13,14,15,22),
+                        List.of(11,12,13,43,25,22),
+                        List.of(11,12,13,43,25,22))),
+                new Bonus(7));
+        outputMessage.printStatistic(rankInfo);
+        outputMessage.printYield(rankInfoController.getYield(rankInfo, money));
+
+        assertThat(output()).contains(
+                "3개 일치 (5,000원) - 0개",
+                "4개 일치 (50,000원) - 0개",
+                "5개 일치 (1,500,000원) - 1개",
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                "6개 일치 (2,000,000,000원) - 0개",
+                "총 수익률은 30,000.0%입니다."
+        );
+    }
+
+    @Test
+    void 수익률_테스트4() {
+        Money money = new Money(5000);
+        RankInfo rankInfo = rankInfoController.getRankInfo(
+                rankInfoController.createRankInfo(),
+                new Lotto(List.of(1,2,3,4,5,6)),
+                new UserLotto(List.of(
+                        List.of(1,2,3,4,15,22), // 4개 일치
+                        List.of(11,12,13,14,15,22),
+                        List.of(11,12,13,14,15,22),
+                        List.of(11,12,13,43,25,22),
+                        List.of(11,12,13,43,25,22))),
+                new Bonus(7));
+        outputMessage.printStatistic(rankInfo);
+        outputMessage.printYield(rankInfoController.getYield(rankInfo, money));
+
+        assertThat(output()).contains(
+                "3개 일치 (5,000원) - 0개",
+                "4개 일치 (50,000원) - 1개",
+                "5개 일치 (1,500,000원) - 0개",
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                "6개 일치 (2,000,000,000원) - 0개",
+                "총 수익률은 1,000.0%입니다."
         );
     }
     @Override
