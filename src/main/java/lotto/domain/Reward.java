2ef26ab6c92d58123c9e996d7d04da9dd310d3e1
starwook <star.wookk@gmail.com>
2022-11-15 18:35:16 +0900

    feat(Reward.java): Refactor Reward.java
    get,set메소드를 제거하라

--- src/main/java/lotto/domain/Reward.java
+++ src/main/java/lotto/domain/Reward.java
@@ -1,32 +1,20 @@
 package lotto.domain;
 
 public enum Reward {
-    THREE(3,"5,000원",false),
-    FOUR(4,"50,000원",false),
-    FIVE(5,"1,500,000원",false),
-    FIVE_BONUS(5,"30,000,000원",true),
-    SIX(6,"2,000,000,000원",true);
+    THREE(3,"5,000원"),
+    FOUR(4,"50,000원"),
+    FIVE(5,"1,500,000원"),
+    FIVE_BONUS(5,"30,000,000원"),
+    SIX(6,"2,000,000,000원"),
+    NOTHING(0,"0");
 
-    private int bonusNumber;
-    private boolean bonusCheck;
     private int number;
     private String bill;
 
-    Reward(int number, String bill,Boolean bonusCheck){
+    Reward(int number, String bill){
         this.number = number;
         this.bill = bill;
-        this.bonusCheck =bonusCheck;
     }
-
-
-    public void setBonusNumber(int number){
-        this.bonusNumber = number;
-    }
-
-    public int getBonusNumber(){
-        return bonusNumber;
-    }
-
 
 
 
