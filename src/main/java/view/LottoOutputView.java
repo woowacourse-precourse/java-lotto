85ff49dd4f848f4ef4e08104f4b08858a2e3238a
June <68291395+june-777@users.noreply.github.com>
2022-11-15 23:16:54 +0900

    refactor: 출력 기능 클래스 통합

--- src/main/java/view/LottoOutputView.java
+++ src/main/java/view/LottoOutputView.java
@@ -1,6 +1,12 @@
 package view;
 
-public class LottoTotalCountOutput {
+import model.Lotto;
+
+public class LottoOutputView {
+
+	public void showLotto(Lotto lotto) {
+		System.out.println(lotto.getNumbers());
+	}
 	
 	public void showLottoCount(long totalLottoCount) {
 		System.out.println("\n" + totalLottoCount + "개를 구매했습니다.");
