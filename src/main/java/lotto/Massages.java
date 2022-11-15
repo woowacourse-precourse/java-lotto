fdbd25080f69267a306bb44b88cfd3abaa8f3d94
yularete <keinikeiwo@gmail.com>
2022-11-15 15:55:36 +0900

    docs : 구현 기능 목록 수정

---
+++ src/main/java/lotto/Massages.java
@@ -0,0 +1,14 @@
+package lotto;
+
+public enum Massage {
+    INPUT_MONEY("구매금액을 입력해 주세요."),
+    PURCHASED_TICKET("개를 구입했습니다."),
+    INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUBER("보너스 번호를 입력해 주세요."),
+    WINNING_STATISTICS("당첨 통계\n---"),
+
+
+
+    ;
+
+}
