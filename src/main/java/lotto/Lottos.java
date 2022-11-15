2bd88805679774fd6b641e6146b5653c5543e4ab
신채원 <cw121@naver.com>
2022-11-15 17:59:45 +0900

    fix: new ArrayList<>로 감싸서 수정

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -11,13 +11,14 @@ public class Lottos {
     private static final String OUTPUT_PURCHASE_COUNT_MESSAGE = "개를 구매했습니다.";
 
     private static List<Integer> createSingleRandomLotto() {
-        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lotto = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         Collections.sort(lotto);
         return lotto;
 
     }
 
     public static List<List<Integer>> createRandomLottos(int purchase) {
+        System.out.println();
         System.out.println(purchase + OUTPUT_PURCHASE_COUNT_MESSAGE);
         List<List<Integer>> lottos = new ArrayList<>();
 
