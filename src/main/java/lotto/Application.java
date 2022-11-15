92b1bf5593f0f45c404904ba514b881309b0510a
dev-green-flamingo <devgflamingo@gmail.com>
2022-11-15 19:52:01 +0900

    feat(output): 로또 번호 출력
    랜덤으로 생성한 로또 번호를 출력한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,6 +8,7 @@ public class Application {
         try {
             LottoView view = new LottoView();
             view.addLotteries();
+            view.printLotteries();
         } catch (Exception excepted) {
             // view에서 에러메세지에 대한 출력 처리를 수행한다.
         }
--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -4,10 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import lotto.controller.LottoController;
 import lotto.model.Lotto;
+import lotto.model.LottoEnum;
 
 public class LottoView {
 
     private final LottoController controller = new LottoController();
+    private List<Lotto> lotteries;
 
     public void addLotteries() {
         System.out.println("구매금액을 입력해주세요.");
@@ -17,11 +19,32 @@ public class LottoView {
             throw new IllegalArgumentException();
         }
         try {
-            List<Lotto> lotteries = controller.createLotteries(Integer.parseInt(line) / 1000);
+            lotteries = controller.createLotteries(Integer.parseInt(line) / 1000);
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 최소1장 이상 구매가능합니다. ");
             throw new IllegalArgumentException();
         }
+    }
+
+    public void printLotteries() {
+        for (final Lotto lotto : lotteries) {
+            printLotto(lotto);
+        }
+    }
+
+    private void printLotto(Lotto lotto) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        for (final Integer number : lotto.getNumbers()) {
+            builder.append(number);
+            if (number.intValue() == lotto.getNumbers()
+                .get(LottoEnum.TOTAL_LOTTO_NUMBER.getValue() - 1)) {
+                continue;
+            }
+            builder.append(", ");
+        }
+        builder.append("]");
+        System.out.println(builder);
     }
 
 }
