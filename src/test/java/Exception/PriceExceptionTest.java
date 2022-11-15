0de25e58b0513f1b33235897c37974e866abbadb
p <gustjr2332@naver.com>
2022-11-15 20:10:03 +0900

    test(PriceExceptionTest) : DisplayName 추가

--- src/test/java/Exception/PriceExceptionTest.java
+++ src/test/java/Exception/PriceExceptionTest.java
@@ -1,16 +1,18 @@
 package Exception;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PriceExceptionTest {
+    @DisplayName("구입금액에 문자가 입력되면 예외가 발생한다.")
     @Test
     void priceContainCharacterException() {
         Assertions.assertThatThrownBy(()
                 -> PriceException.checkException("100k")).isInstanceOf(IllegalArgumentException.class);
 
     }
-
+    @DisplayName("구입금액이 1000원 단위로 입력 하지 않으면 예외가 발생한다.")
     @Test
     void priceNotUnitOf1000Exception() {
         Assertions.assertThatThrownBy(()
