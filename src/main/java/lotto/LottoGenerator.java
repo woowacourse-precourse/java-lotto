d3e15ed6866d32182428838974a0ed30ab3c484c
Miseong Kim <miseongk@naver.com>
2022-11-15 23:03:59 +0900

    fix(lotto): return arraylist

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
@@ -10,6 +11,6 @@ public class LottoGenerator {
     private static final int LOTTO_SIZE = 6;
 
     public static List<Integer> generateLotto() {
-        return Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, LOTTO_SIZE);
+        return new ArrayList<>(Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, LOTTO_SIZE));
     }
 }
