5f459ad21ef76eb7da835c0a4aa84bb98b105604
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-15 15:57:25 +0900

    fix: 당첨 번호와 보너스 번호 일치할 경우 오류가 발생하도록 수정

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,39 +1,34 @@
 package lotto.domain;
 
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class WinningNumbers {
 
-    private final String DUPLICATE_NUMBER_ERROR = "[ERROR] 당첨 번호와 보너스 번호는 중복될 수 없습니다.";
+    private static final String DUPLICATE_NUMBER_ERROR = "[ERROR] 당첨 번호와 보너스 번호는 중복될 수 없습니다.";
     public final List<Integer> winningNumbers;
     private final int bonusNumber;
 
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
-
+        validateDuplicateNumber();
     }
 
-    public void validateDuplicateNumber(List<Integer> winningNumbers, int bonusNumber) {
+    public void validateDuplicateNumber() {
         if (winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR);
         }
     }
 
-    private int countHitNumber(List<Integer> lottoNumbers) {
-        return (int) lottoNumbers.stream()
-                .filter(lottoNumber -> winningNumbers.stream().anyMatch(Predicate.isEqual(lottoNumber)))
+    private int countHitNumber(Lotto lottoNumbers) {
+        return (int) winningNumbers.stream()
+                .filter(lottoNumbers::contains)
                 .count();
     }
 
-    public LottoRank calculrateRank(List<Integer> lotto) {
+    public LottoRank calculateRank(Lotto lotto) {
         int hitCount = countHitNumber(lotto);
         boolean hasBonus = lotto.contains(bonusNumber);
         return LottoRank.valueOf(hitCount, hasBonus);
     }
-
-
-
 }
