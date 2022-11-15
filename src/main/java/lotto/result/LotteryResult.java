d477526d53be50b1bcaed7d8431a5b97d41a8222
juno-junho <ssmm0205@naver.com>
2022-11-15 22:50:59 +0900

    feat(lottos): create class LotteryResult
    결과를 담당하는 LotteryResult 클래스 생성

---
+++ src/main/java/lotto/result/LotteryResult.java
@@ -0,0 +1,23 @@
+package lotto.result;
+
+import lotto.Lotto;
+import lotto.WinningNumbers;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class LotteryResult {
+
+    private final LinkedHashMap<Rank, Integer> result = new LinkedHashMap<>();
+
+    private LotteryResult(List<Lotto> lottos, WinningNumbers winNumbers) {
+        validate(lottos);
+        for (Lotto lotto : lottos) {
+            Rank rank = Rank.of(lotto.countSameNum(winNumbers), lotto.contains(winNumbers.getBonus()));
+            add(rank);
+        }
+    }
+
+
+
+}
