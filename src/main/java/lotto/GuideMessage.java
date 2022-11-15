fc790b6cb172347028d593c5e8e045b756024eb7
farmJun <junpreme1205@gmail.com>
2022-11-15 20:54:33 +0900

    rename(GuideMessage):  nTH_WIN_MESSAGE to nTH_WIN_PLACE

--- src/main/java/lotto/GuideMessage.java
+++ src/main/java/lotto/GuideMessage.java
@@ -6,11 +6,11 @@ public enum GuideMessage {
     BONUS_NUMBER_REQUEST_MESSAGE ("\n보너스 번호를 입력해 주세요."),
     PURCHASE_LOTTO_TICKET_COUNT_MESSAGE ("\n%d개를 구매했습니다.\n"),
     WINNING_STATS_MESSAGE ("\n당첨 통계\n---"),
-    FIFTH_WIN_MESSAGE("3개 일치 (5,000원) - %d개\n"),
-    FOURTH_WIN_MESSAGE("4개 일치 (50,000원) - %d개\n"),
-    THIRD_WIN_MESSAGE("5개 일치 (1,500,000원) - %d개\n"),
-    SECOND_WIN_MESSAGE("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
-    FIRST_WIN_MESSAGE("6개 일치 (2,000,000,000원) - %d개\n");
+    FIFTH_PLACE_MESSAGE("3개 일치 (5,000원) - %d개\n"),
+    FOURTH_PLACE_MESSAGE("4개 일치 (50,000원) - %d개\n"),
+    THIRD_PLACE_MESSAGE("5개 일치 (1,500,000원) - %d개\n"),
+    SECOND_PLACE_MESSAGE("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
+    FIRST_PLACE_MESSAGE("6개 일치 (2,000,000,000원) - %d개\n");
 
     private String guideMessage;
 
