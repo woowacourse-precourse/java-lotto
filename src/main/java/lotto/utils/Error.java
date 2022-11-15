29c530e6291094e8da47dc8a560ecd3e827b5b24
chaeyeong222 <iky1505@naver.com>
2022-11-15 18:24:57 +0900

    refactor : 에러메세지 출력 이넘클래스로 구현

--- src/main/java/lotto/utils/Error.java
+++ src/main/java/lotto/utils/Error.java
@@ -1,7 +1,10 @@
-package lotto.model;
+package lotto.utils;
 
 public enum Error {
-    INVALID_NUMBER_COUNT("[ERROR] 숫자가 아닌 값을 포함하고 있습니다.");
+    NOT_NUMBER("[ERROR] 숫자가 아닌 값을 포함하고 있습니다."),
+    DUPLICATED_NUMBER("[ERROR] 중복된 숫자는 포함될 수 없습니다."),
+    INVALID_NUMBER_RANGE("[ERROR] 숫자는 1부터 45 사이의 숫자만 포함되어야 합니다."),
+    INPUT_MONEY_CHECK("[ERROR] 구입 금액은 1,000원 단위로 나누어 떨어져야 합니다.");
 
     Error(String message) {
         this.message = message;
