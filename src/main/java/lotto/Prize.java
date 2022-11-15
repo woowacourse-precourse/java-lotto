c100f5d1392befbfe73a6264b26897c3f5cc5e52
elizabethgim <yujingim@kookmin.ac.kr>
2022-11-15 22:40:36 +0900

    feat: 로또에 해당하는 순위 반환

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -3,6 +3,10 @@ package lotto;
 import java.util.ArrayList;
 
 public class Prize {
+    private final LottoRank rankResult;
+    public Prize(ArrayList<Integer> winningNumbers, Lotto lotto, int bounus) {
+        this.rankResult = resultPrize(winningNumbers, lotto, bounus);
+    }
     private static int checkForWinningNumber(ArrayList<Integer> winningNumbers, Lotto lotto){
         int same = 0;
         for(Integer winningNumber: winningNumbers){
@@ -20,9 +24,29 @@ public class Prize {
     private static boolean checkSecondPrize(int sameNumbers, boolean bounus){
         return sameNumbers == 5 && bounus;
     }
-    public static void resultPrize(ArrayList<Integer> winningNumbers, Lotto lotto, int bounus){
+
+    private static LottoRank rank(int sameNumbers, boolean sameBounus){
+        switch (sameNumbers) {
+            case 6:
+                return LottoRank.FIRST;
+            case 5:
+                if(sameBounus){
+                    return LottoRank.SECOND;
+                }
+                else {
+                    return LottoRank.THIRD;
+                }
+            case 4:
+                return LottoRank.FOURTH;
+            case 3:
+                return LottoRank.FIFTH;
+            default:
+                return LottoRank.NOTHING;
+        }
+    }
+    public static LottoRank resultPrize(ArrayList<Integer> winningNumbers, Lotto lotto, int bounus){
         int sameWinningNumbers = checkForWinningNumber(winningNumbers, lotto);
         boolean sameBounusNumber = checkForSameBounusNumber(lotto, bounus);
-
+        return rank(sameWinningNumbers, sameBounusNumber);
     }
 }
