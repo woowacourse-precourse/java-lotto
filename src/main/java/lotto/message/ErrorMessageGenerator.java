77fc7d433355a5966aabfec4df485b5e6d0e1a63
wonju-dev <wonjulee.dev@gmail.com>
2022-11-15 12:32:32 +0900

    refacotr(ErrorMessageGenerator): 상수 분리

--- src/main/java/lotto/message/ErrorMessageGenerator.java
+++ src/main/java/lotto/message/ErrorMessageGenerator.java
@@ -3,10 +3,13 @@ package lotto.message;
 import lotto.condition.Condition;
 
 public class ErrorMessageGenerator {
+
+    private static final String ERROR_MESSAGE_TEMPLATE = "[ERROR]";
+
     private ErrorMessageGenerator() {
     }
 
     public static String getErrorMessage(Condition condition) {
-        return "[ERROR]" + condition.getErrorMessage();
+        return ERROR_MESSAGE_TEMPLATE + condition.getErrorMessage();
     }
 }
