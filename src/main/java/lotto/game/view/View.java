704e791b9a63e1ee4926e9822ef93d5109bac2b2
GwansuShin <happysun7080@naver.com>
2022-11-15 11:34:12 +0900

    feat(View): 사용자 입력 및 콘솔 출력
    Controller에 의존하는 클래스
    사용자 입력 처리
    콘솔에 문자열 출력

---
+++ src/main/java/lotto/game/view/View.java
@@ -0,0 +1,74 @@
+package lotto.game.view;
+
+import lotto.util.Constant;
+
+import java.util.Map;
+
+public class View {
+    private static final String NEWLINE = "\n";
+    private static final String TRIPLE_DASH = "---";
+    private static final String RESULT_UNIT = "개";
+    private static final int DEFAULT_SCORE = 0;
+
+    public View() {
+    }
+
+    public String inputPurchaseAmount() {
+        Console.Out.printLine(Message.INPUT_PURCHASE_AMOUNT);
+        return Console.In.inputLine();
+    }
+
+    public void outputQuantity(int quantity) {
+        Console.Out.print(NEWLINE);
+        Console.Out.printLine(String.format("%d%s", quantity, Message.OUTPUT_LOTTO_NUMBERS.text()));
+    }
+
+    public void outputLottoNumbers(String lottoNumbers) {
+        Console.Out.printLine(lottoNumbers);
+    }
+
+    public String inputDrawNumbers() {
+        Console.Out.printLine(Message.INPUT_DRAW_NUMBERS);
+        return Console.In.inputLine();
+    }
+
+    public String inputBonusNumber() {
+        Console.Out.printLine(Message.INPUT_BONUS_NUMBER);
+        return Console.In.inputLine();
+    }
+
+    public void outputResult(Map<Integer, Integer> drawScore, Integer bonusScore) {
+        Console.Out.print(NEWLINE);
+        Console.Out.printLine(Message.OUTPUT_MATCH_TOTAL);
+        Console.Out.printLine(TRIPLE_DASH);
+        Console.Out.printLine(formatResult(
+                        Message.OUTPUT_THREE_MATCHES,
+                        drawScore.getOrDefault(Constant.THREE, DEFAULT_SCORE)));
+        Console.Out.printLine(formatResult(
+                Message.OUTPUT_FOUR_MATCHES,
+                drawScore.getOrDefault(Constant.FOUR, DEFAULT_SCORE)));
+        Console.Out.printLine(formatResult(
+                Message.OUTPUT_FIVE_MATCHES,
+                drawScore.getOrDefault(Constant.FIVE, DEFAULT_SCORE)));
+        Console.Out.printLine(formatResult(
+                Message.OUTPUT_BONUS_MATCHES,
+                bonusScore));
+        Console.Out.printLine(formatResult(
+                Message.OUTPUT_SIX_MATCHES,
+                drawScore.getOrDefault(Constant.SIX, DEFAULT_SCORE)));
+    }
+
+    private String formatResult(Message matchMessage, int score) {
+        return String.format("%s%d%s", matchMessage.text(), score, RESULT_UNIT);
+    }
+
+    public void outputEarningRate(double earningsRate) {
+        Console.Out.print(Message.OUTPUT_EARNINGS_RATE_PREFIX);
+        Console.Out.print(formatEarningsRate(earningsRate));
+        Console.Out.printLine(Message.OUTPUT_EARNINGS_RATE_POSTFIX);
+    }
+
+    private String formatEarningsRate(double earningsRate) {
+        return String.format("%,.1f%s", earningsRate * 100, "%");
+    }
+}
