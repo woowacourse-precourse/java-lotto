a6b8b631d0c0e7640ec695616650ab7047818a04
“rotomoo” <quddlf410@naver.com>
2022-11-15 19:55:50 +0900

    test(Player) : 구입 금액 1000단위 미입력 예외 발생 테스트 추가

--- src/test/java/lotto/domain/PlayerTest.java
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -1,29 +1,43 @@
 package lotto.domain;
 
+import lotto.exception.Check;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Player 테스트")
 class PlayerTest {
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player();
+    }
 
     @Test
     void compareLotto() {
     }
 
-    @DisplayName("로또 구매 테스트")
+    @DisplayName("true case : 로또 구매 테스트")
     @Test
     void buyLotto() {
-        Player player = new Player();
-
         List<Lotto> playerLottos = player.buyLotto(5000);
         int size = playerLottos.get(0).getNumbers().size();
 
         assertThat(playerLottos).hasSize(5);
         assertThat(size).isEqualTo(6);
+    }
+
+    @DisplayName("false case : 1000단위 미입력 로또 구매 테스트")
+    @Test
+    void buyLotto2() {
+        assertThatThrownBy(() -> player.buyLotto(5001))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
