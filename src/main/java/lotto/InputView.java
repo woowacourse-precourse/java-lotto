cbd92efbea68771b5749780273603e3814b71310
noparamin <noparamin@naver.com>
2022-11-15 21:35:47 +0900

    refactor: 하드코딩 부분 enum 적용하여 수정

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,34 +1,35 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.enums.InputMessage;
 
 import java.util.List;
 
 public class InputView {
-    private final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
-    private final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     private ConversionUtility conversionUtility = new ConversionUtility();
 
     public int inputMoney() {
-        System.out.println(MONEY_INPUT_MESSAGE);
+        System.out.println(InputMessage.MONEY_INPUT_MESSAGE.getMessage());
         int purchasingAmount = Integer.parseInt(Console.readLine());
+        System.out.println();
 
         return purchasingAmount;
     }
 
     public List<Integer> inputWinningNumbers() {
-        System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
+        System.out.println(InputMessage.WINNING_NUMBERS_INPUT_MESSAGE.getMessage());
         String inputWinningNumbers = Console.readLine();
         List<Integer> winningNumbers = conversionUtility.convertStringWinningNumbersToList(inputWinningNumbers);
+        System.out.println();
 
         return winningNumbers;
     }
 
     public int inputBonusNumber() {
-        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
+        System.out.println(InputMessage.BONUS_NUMBER_INPUT_MESSAGE.getMessage());
         int bonusNumber = Integer.parseInt(Console.readLine());
+        System.out.println();
 
         return bonusNumber;
     }
