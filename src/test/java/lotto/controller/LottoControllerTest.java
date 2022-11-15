350d279f03cadca876ab81e4c07b619e70620583
desfox <desfox99@naver.com>
2022-11-15 13:35:15 +0900

    test(LottoControllerTest): 구입한 로또 개수 계산하는 기능 테스트

---
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -0,0 +1,24 @@
+package lotto.controller;
+
+import lotto.validation.BonusNumberInputValidation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoControllerTest {
+    private LottoController lottoController;
+
+    @BeforeEach
+    public void initialize() {
+        lottoController = new LottoController();
+    }
+
+    @Test
+    public void computeAmountOfLottoTest() {
+        assertSimpleTest(() -> {
+            assertThat(lottoController.computeAmountOfLotto(8000)).isEqualTo(8);
+        });
+    }
+}
