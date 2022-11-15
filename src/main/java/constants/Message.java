990199f615c2827d706a33c128f1ec354ba06eb4
yejin130 <jinis2023@gmail.com>
2022-11-15 17:57:37 +0900

    fix: Message 수정

--- src/main/java/constants/Message.java
+++ src/main/java/constants/Message.java
@@ -3,9 +3,9 @@ package constants;
 public class Message {
     public static final String MESSAGE_INPUT_PAY_AMOUNT = "구입금액을 입력해 주세요.";
     public static final String MESSAGE_NUMBER_OF_TICKET_IS = "개를 구매했습니다.";
-    public static final String MESSAGE_INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
-    public static final String MESSAGE_INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    public static final String MESSAGE_RESULT_IS = "당첨 통계\n---";
+    public static final String MESSAGE_INPUT_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.";
+    public static final String MESSAGE_INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+    public static final String MESSAGE_RESULT_IS = "\n당첨 통계\n--- ";
     public static final String MESSAGE_RESULT_OF_RANK_1 = "6개 일치 (2,000,000,000원) - ";
     public static final String MESSAGE_RESULT_OF_RANK_2 = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
     public static final String MESSAGE_RESULT_OF_RANK_3 = "5개 일치 (1,500,000원) - ";
