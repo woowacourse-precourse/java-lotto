3c5e77ffbd47972206426c397591244969188819
이지영 <wldudwldhd@naver.com>
2022-11-15 20:15:05 +0900

    feat:ëapplication로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoVendor lottoVendor=new LottoVendor();
+        Buyer buyer=new Buyer();
+        int purchaseMoney=Integer.parseInt(Console.readLine());
+        buyer.purchase(lottoVendor,purchaseMoney);
+
+
     }
 }
