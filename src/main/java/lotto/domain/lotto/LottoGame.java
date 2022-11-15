2031afbf01d5dbe7b779ed4c8ade34467583031d
JuSeong1130 <jsmoon1130@naver.com>
2022-11-15 17:28:36 +0900

    refactor(LottoGame): 로또 구매,당첨번호 입력,당첨통계 출력 3가지로 메서드 분리

--- src/main/java/lotto/domain/lotto/LottoGame.java
+++ src/main/java/lotto/domain/lotto/LottoGame.java
@@ -2,42 +2,55 @@ package lotto.domain.lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import lotto.domain.machine.LottoMachine;
 import lotto.domain.person.Person;
+import lotto.domain.win.WinningStats;
 
 public class LottoGame {
 
 	Person person;
+	List<Lotto> lottos;
+
+	LottoMachine lottoMachine;
 
 	public void start() {
-		System.out.println("구입금액을 입력해 주세요.");
 		String money = askPurchaseAmount();
-		person = new Person(money);
-		List<Lotto> lottos = person.buyLotto();
-		System.out.println();
-		System.out.println(lottos.size() + "개를 구매했습니다");
-		for (Lotto lotto : lottos) {
-			System.out.println(lotto.getNumbers().toString());
-		}
-		System.out.println();
-		System.out.println("당첨 번호를 입력해 주세요.");
+		printPurchaseLotto(money);
 		String winLottoNumber = askWinLottoNumber();
-		System.out.println();
-		System.out.println("보너스 번호를 입력해 주세요.");
 		String bonusNumber = askBonusNumber();
-
+		System.out.println();
+		lottoMachine = new LottoMachine(winLottoNumber,bonusNumber);
+		WinningStats winningStats = new WinningStats(person,lottoMachine);
+		winningStats.printWinningStats();
 
 	}
 
 
 	public String askPurchaseAmount() {
+		System.out.println("구입금액을 입력해 주세요.");
 		return Console.readLine();
 	}
+	public void printPurchaseLotto(String money){
+		person = new Person(money);
+		lottos = person.buyLotto();
+		int lottoCount = lottos.size();
+		System.out.println();
+		System.out.println(lottoCount + "개를 구매했습니다");
+		for (Lotto lotto : lottos) {
+			System.out.println(lotto.getNumbers().toString());
+		}
+		System.out.println();
+	}
+
 
 	public String askWinLottoNumber() {
+		System.out.println("당첨 번호를 입력해 주세요.");
 		return Console.readLine();
 	}
 
 	public String askBonusNumber() {
+		System.out.println();
+		System.out.println("보너스 번호를 입력해 주세요.");
 		return Console.readLine();
 	}
 }
