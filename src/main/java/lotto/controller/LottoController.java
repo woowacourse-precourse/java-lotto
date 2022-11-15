8ab63b528e581f73321bdad16c748d7646b7e5f1
gunheeK <gunhee0516@gmail.com>
2022-11-15 10:32:09 +0900

    feat: 로또 발행 번호 컨트롤러 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,12 +1,23 @@
 package lotto.controller;
 
+import lotto.domain.LottoNumberIssue;
 import lotto.domain.LottoQuantityCalculation;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
+
 public class LottoController {
     public void start() {
         int purchaseQuantity = lottoPurchaseQuantity(InputView.inputLottoPurchaseAmount());
+        List<List<Integer>> lottoNumber = lottoIssue(purchaseQuantity);
+    }
+
+    private List<List<Integer>> lottoIssue(int purchaseQuantity) {
+        LottoNumberIssue lottoNumberIssue = new LottoNumberIssue(purchaseQuantity);
+        List<List<Integer>> lottoNumber = lottoNumberIssue.lottoIssue();
+        OutputView.lottoNumber(lottoNumber);
+        return lottoNumber;
     }
 
     private int lottoPurchaseQuantity(int purchaseAmount) {
