6163e3df77b40cab2c2342e12c37f99dc30d3b39
LeeJE20 <silver36c@gmail.com>
2022-11-15 16:30:13 +0900

    feat: Manager 당첨 번호 설정하기

--- src/main/java/lotto/Manager.java
+++ src/main/java/lotto/Manager.java
@@ -3,6 +3,7 @@ package lotto;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -10,10 +11,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 import lotto.ui.ManagerMessage;
 
 public class Manager {
+	private static final String SPLIT = ",";
 	private List<Lotto> lottos;
 	private List<Integer> win;
-	
-	
+
 	private Lotto makeLotto() {
 		List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
 		return new Lotto(sort(numbers));
@@ -33,19 +34,29 @@ public class Manager {
 	}
 
 	public void printLottos() {
-		for(Lotto lotto: lottos){
+		for (Lotto lotto : lottos) {
 			System.out.println(lotto.getNumbers().toString());
 		}
 	}
-	
+
 	public void setWin() {
 		System.out.println(ManagerMessage.INSERT_WIN_NUMBERS.getMessage());
 		String input = readLine();
 		win = separateWinInput(input);
 	}
-	
-	public List<Integer> separateWinInput(String input){
+
+	public List<Integer> separateWinInput(String input) {
 		List<Integer> result = new ArrayList<Integer>();
+		List<String> splited = Arrays.asList(input.split(SPLIT));
+		if (splited.size() != 6) {
+			throw new IllegalArgumentException();
+		}
+		for(String s:splited) {
+			if (!Util.isInteger(s)) {
+				throw new IllegalArgumentException();
+			}
+			result.add(Integer.parseInt(s));
+		}
 		return result;
 	}
 }
