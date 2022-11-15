34a88f811105cd2dab9ac5cfdbb4e7fa690e6f64
seheonCho <whtpgjs94@gmail.com>
2022-11-15 17:48:38 +0900

    refactor(LottoRule): 인스턴스화가 필요 없어서 private 생성자 추가

--- src/main/java/util/LottoRule.java
+++ src/main/java/util/LottoRule.java
@@ -2,6 +2,8 @@ package util;
 
 public class LottoRule {
 
+    private LottoRule() {}
+
     public static final int NUMBER_OF_SIZE = 6;
     public static final int START_INCLUSIVE = 1;
     public static final int END_INCLUSIVE = 45;
