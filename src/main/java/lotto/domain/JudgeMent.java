b437d278e2e02e162bd411be741becc60927a43d
starwook <star.wookk@gmail.com>
2022-11-15 16:51:30 +0900

    feat(JudgeMent.java): Add bonusCheck method
    맞춘 번호가 5개이고 추가로 보너스 넘버를 포함하였는지 확인하는 메소드를 추가하라

--- src/main/java/lotto/domain/JudgeMent.java
+++ src/main/java/lotto/domain/JudgeMent.java
@@ -1,4 +1,26 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class JudgeMent {
+    private static final int initNumber = 0;
+    private static final int correctNumberStandardWhenBonus = 5;
+
+    public int compareNumbers(List<Integer> numbers, List<Integer> lottoNumbers) {
+        int countCorrect = initNumber;
+        for (int i = 0; i < Resoure.numberSize; i++) {
+            int number = numbers.get(i);
+            if (lottoNumbers.contains(i)) {
+                countCorrect++;
+            }
+        }
+        return countCorrect;
+    }
+
+    public boolean bonusCheck(List<Integer> numbers, int correctCount, int bonusNumber) {
+        if (correctCount == 5 && numbers.contains(bonusNumber)){
+            return true;
+        }
+        return false;
+    }
 }
