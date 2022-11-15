025fa7863415307868d4e552f223c8b4bcb95fca
star-sil <kimse9811@naver.com>
2022-11-15 23:39:24 +0900

    refactor(test/lotto): remove duplicate test
    중복되는 테스트 코드를 제거했다.

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -8,14 +8,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputTest extends NsTest {
-    @DisplayName("구입금액을 숫자가 아닌 다른 값으로 입력시 예외가 발생한다.")
-    @Test
-    void 숫자가_아닌_값으로_구입금액입력_시_예외_발생() {
-        assertThatThrownBy(() -> runException("one hundred"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 숫자가 아닌 다른 값을 입력하였습니다.");
-    }
-
     @DisplayName("구입금액 1000원으로 나누어 떨어지지 않는 경우 예외가 발생한다.")
     @Test
     void 구입금액이_1000원으로_나누어_떨어지지_않는_경우_예외_발생() {
