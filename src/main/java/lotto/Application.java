87f3ea29d0ad05c0e7ad8f5b733deff0ed6a7c8c
YusunPark <solarpy.kor@gmail.com>
2022-11-15 21:41:09 +0900

    feat(Application.java): add price input feature

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,40 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
+
+    static void priceMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    static boolean checkPrice(int n) {
+        if (n % 1000 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static void inputPrice() {
+        priceMessage();
+        String price = Console.readLine();
+        System.out.println();
+        if (!checkPrice(Integer.parseInt(price))) {
+            throw new IllegalArgumentException("[ERROR] 구입금액 형식이 맞지 않습니다.");
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            inputPrice();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
