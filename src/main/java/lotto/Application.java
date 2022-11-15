321421e27afa0752234417ecda3e522b1b5cf48d
hoseon <hy770819@naver.com>
2022-11-15 18:25:49 +0900

    feat:로또 금구매 금액 입력 받는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Pattern;
+
 public class Application {
+
+    static final String exceptionHeader="[ERROR] ";
+    static final String inputMoneyExceptionNotInteger="숫자만 입력해주세요.\n";
+    static final String inputMoneyExceptionStr="로또 금액은 1000원 입니다. 1000원 단위로 입력해주세요.\n";
+
+    public static void getMoney(){
+        String pattern="^[0-9]*$";
+        String inputStr=Console.readLine();
+        if(!Pattern.matches(pattern,inputStr)){
+            throw new IllegalArgumentException(exceptionHeader+inputMoneyExceptionNotInteger);
+        }
+
+        int inputMoney = Integer.parseInt(inputStr);
+        if(inputMoney%1000!=0){
+            throw new IllegalArgumentException(exceptionHeader+inputMoneyExceptionStr);
+        }
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        getMoney();
+
     }
 }
--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -11,7 +11,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
-
+    @Test
+    void getMoneyTest(){
+        assertSimpleTest(() -> {
+            runException("1000j");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
     @Test
     void 기능_테스트() {
         assertRandomUniqueNumbersInRangeTest(
