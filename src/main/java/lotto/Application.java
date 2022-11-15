d19b0583b5a910432512e22f7e977ccd5ee28d29
KongDuHwan <kong_du@naver.com>
2022-11-15 21:08:56 +0900

    class 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import lotto.domain.Store;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Store store = new Store();
+
+        store.getSetMoney();
+        store.confirmMoney();
+        store.setAvailableLotto();
+        System.out.println(store.availableLotto + "개를 구매헸습니다.");
+        store.publishLotto();
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<Integer> winnernum = store.getWinnerNum();
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonus = store.getBonusNum();
+
+        Lotto lotto = new Lotto(winnernum,bonus);
+
     }
 }
