07e6ad02a289f63886e6c663951178d2ae4a762f
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-15 22:37:22 +0900

    feat(main\...\controller\WinResultManager) : implement correctCaseCollector method

--- src/main/java/lotto/controller/WinResultManager.java
+++ src/main/java/lotto/controller/WinResultManager.java
@@ -2,34 +2,42 @@ package lotto.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class WinResultManager {
 
     public static List<CorrectNumber> getCorrectedNumbersWithWin(List<List<Integer>> tickets,
-            List<Integer> answer, int bonus) {
-        List<CorrectNumber> resultCollection = new ArrayList<>();
+            List<Integer> winNumbers, int bonus) {
+        List<CorrectNumber> resultCollector = new ArrayList<>();
         for (List<Integer> ticket : tickets) {
             Set<Integer> matchedNumbers = ticket.stream()
-                    .filter(answer::contains)
+                    .filter(winNumbers::contains)
                     .collect(Collectors.toSet());
             if (matchedNumbers.size() == 3) {
-                resultCollection.add(CorrectNumber.THREE);
+                resultCollector.add(CorrectNumber.THREE);
             }
             if (matchedNumbers.size() == 4) {
-                resultCollection.add(CorrectNumber.FOUR);
+                resultCollector.add(CorrectNumber.FOUR);
             }
             if (matchedNumbers.size() == 5 && ticket.contains(bonus)) {
-                resultCollection.add(CorrectNumber.FIVE_BONUS);
+                resultCollector.add(CorrectNumber.FIVE_BONUS);
             }
             if (matchedNumbers.size() == 5 && !ticket.contains(bonus)) {
-                resultCollection.add(CorrectNumber.FIVE);
+                resultCollector.add(CorrectNumber.FIVE);
             }
             if (matchedNumbers.size() == 6) {
-                resultCollection.add(CorrectNumber.SIX);
+                resultCollector.add(CorrectNumber.SIX);
             }
         }
-        return resultCollection;
+        return resultCollector;
+    }
+
+    public static Map<CorrectNumber, Long> correctCaseCollector(List<CorrectNumber> resultCollection) {
+        Map<CorrectNumber, Long> resultMap = resultCollection.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        return resultMap;
     }
 }
