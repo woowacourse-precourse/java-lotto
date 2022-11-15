f3dd5f4f16ff3ccfcc29f5117749fd9c0d2f028a
kbm6609 <kbm6609@nvaer.com>
2022-11-15 17:43:55 +0900

    feat<lotto> : 당첨번호 유효성(숫자) 검증 기능 구현

--- src/main/java/lotto/domain/LottoWinningNumber.java
+++ src/main/java/lotto/domain/LottoWinningNumber.java
@@ -2,4 +2,13 @@ package lotto.domain;
 
 
 public class LottoWinningNumber {
+    public void validConsistNumber(String[] inputNumbers) {
+        try {
+            for (int i = 0; i < inputNumbers.length; i++) {
+                Integer.parseInt(inputNumbers[i]);
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
