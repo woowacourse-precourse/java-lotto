b8c2a989f392b1058f12b43af79d596f95e2af76
Life is a ninanino <tvxqinak@naver.com>
2022-11-15 17:26:24 +0900

    fix : 전체 변경사항 반영

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoGame;
+
+import java.util.List;
+import java.util.Map;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            int userMoney = InputConsole.buyLotto();
+            LottoGame lottoGame = new LottoGame(userMoney);
+
+            List<Lotto> lotto = lottoGame.payForLotto(userMoney);
+
+            List<Integer> winningNumber = InputConsole.inputNumber();
+
+            int bonusNumber = InputConsole.inputBonusNumber();
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -3,10 +3,11 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input{
-    
+
     private static final String SCANNER = Console.readLine();
 
-    public static String numberInput(){
+    public static String input(){
+
         return SCANNER;
     }
 }
