c88932cf77b38bebd578441174565e355496e1b9
CJ <choijun0627@naver.com>
2022-11-15 14:00:04 +0900

    <feat>(CalculatingLottoTest.java): 기능 요구 사항 14번 테스트 코드 작성

--- src/test/java/lotto/CalculatingLottoTest.java
+++ src/test/java/lotto/CalculatingLottoTest.java
@@ -2,15 +2,27 @@ package lotto;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatingLottoTest {
     CalculatingLotto calculatingLotto = new CalculatingLotto();
+
+
+
     @Test
     void findSameNumberTest() {
         assertThat(calculatingLotto.findSameNumber(6,6)).isEqualTo(1);
 
         assertThat(calculatingLotto.findSameNumber(5,6)).isEqualTo(0);
+    }
+
+    @Test
+    void checkBonusTest() {
+        assertThat(calculatingLotto.checkBonus(6, List.of(1, 2, 3, 4, 5, 6))).isEqualTo(true);
+
+        assertThat(calculatingLotto.checkBonus(6, List.of(1, 2, 3, 4, 5, 8))).isEqualTo(false);
     }
 }
