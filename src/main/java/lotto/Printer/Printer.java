c06c47edab9b2d65ab4ab1f8d68d59a060182958
채희찬 <eqholic1125@gmail.com>
2022-11-15 20:59:10 +0900

    fix: Printer 수익률 출력시 반올림 수정

--- src/main/java/lotto/Printer/Printer.java
+++ src/main/java/lotto/Printer/Printer.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Printer {
     private static final String PURCHASE_AMOUNT_REQUEST_MESSAGE = "구매금액을 입력해 주세요.";
-    private static final String PURCHASE_MESSAGE = "%개를 구매했습니다.";
+    private static final String PURCHASE_MESSAGE = "%d개를 구매했습니다.";
     private static final String LOTTO_NUMBER_REQUEST_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_REQUEST_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String WINNING_STATISTICS_MESSAGE = "당첨 통계";
@@ -14,7 +14,7 @@ public class Printer {
     private static final String THIRD_PLACE_MESSAGE = "5개 일치 (1,500,000원) - %d개";
     private static final String SECOND_PLACE_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
     private static final String FIRST_PLACE_MESSAGE = "6개 일치 (2,000,000,000원) - %d개";
-    private static final String EARNING_RATE_MESSAGE = "총 수익률은 %f%%입니다.";
+    private static final String EARNING_RATE_MESSAGE = "총 수익률은 %.2f%%입니다.";
 
     public void requestPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_REQUEST_MESSAGE);
