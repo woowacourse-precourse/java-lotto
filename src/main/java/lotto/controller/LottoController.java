03e1dd7d15de1f58fe13afb7dbec3ce894217210
hyee0715 <hyee0715@naver.com>
2022-11-15 17:47:20 +0900

    feat: 구입 금액에 해당하는 만큼 n개의 로또를 발행하는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,12 +2,14 @@ package lotto.controller;
 
 import static lotto.view.OutputView.printLottoPurchaseCount;
 
+import lotto.domain.lotto.TotalLottos;
 import lotto.domain.purchaseAmount.PurchaseAmount;
 
 public class LottoController {
     public void run() {
         long purchaseAmount = PurchaseAmount.getPurchaseAmount();
         long lottoPurchaseCount = takeLottoPurchaseCount(purchaseAmount);
+        TotalLottos totalLottos = takeTotalLottos(lottoPurchaseCount);
     }
 
     public static long takeLottoPurchaseCount(long purchaseAmount) {
@@ -15,5 +17,11 @@ public class LottoController {
         printLottoPurchaseCount(lottoPurchaseCount);
 
         return lottoPurchaseCount;
+    }
+
+    public static TotalLottos takeTotalLottos(long lottoPurchaseCount) {
+        TotalLottos totalLottos = new TotalLottos(lottoPurchaseCount);
+
+        return totalLottos;
     }
 }
