87a06a30cd750e3fa39158cdc7ed0a25c9e235a5
jijongkwon <help3451@naver.com>
2022-11-15 21:02:42 +0900

    feat : 상금금액 합 구하는 기능 추가

--- src/main/java/lotto/domain/RateCalculator.java
+++ src/main/java/lotto/domain/RateCalculator.java
@@ -1,7 +1,21 @@
 package lotto.domain;
 
+import java.util.Iterator;
+import java.util.Map;
+
 public class RateCalculator {
-    public double CalculateReturnOfRate(int purchaseMoney, int winningMoney){
+    private ComparingMachine comparingMachine = new ComparingMachine();
+    public int calculateWinningMoney(Map<Integer,Integer> winningMoney){
+        int sum = 0;
+        Iterator<Integer> keys = winningMoney.keySet().iterator();
+        while (keys.hasNext()){
+            int key = keys.next();
+            sum += key;
+        }
+        return sum;
+    }
+
+    public double calculateReturnOfRate(int purchaseMoney, int winningMoney){
         double rateMoney = ((double)winningMoney / (double) purchaseMoney) * 100;
         rateMoney = Math.round(rateMoney*100)/100.0;
         return rateMoney;
