1eb79492a55dd5128c53138a179de26206d2fa78
조병우 <whquddn55@gmail.com>
2022-11-15 22:06:46 +0900

    fix(Winning): WinningStatisticalResultKoreanFormater로 인한 통합테스트 실패 해결
    소수점 자리수, 보너스 번호 일치에 대한 게임 출력의 요구사항에 맞게 출력하지 못 하던 문제 해결함.

--- src/main/java/lotto/winning/WinningStatisticalResultKoreanFormatter.java
+++ src/main/java/lotto/winning/WinningStatisticalResultKoreanFormatter.java
@@ -1,6 +1,8 @@
 package lotto.winning;
 
+import java.text.NumberFormat;
 import java.util.Arrays;
+import java.util.function.Predicate;
 
 public class WinningStatisticalResultKoreanFormatter implements WinningStatisticalFormatter {
     @Override
@@ -14,17 +16,22 @@ public class WinningStatisticalResultKoreanFormatter implements WinningStatistic
 
     private String getBody(WinningStatistical winningStatistical) {
         StringBuilder stringBuilder = new StringBuilder();
-        Arrays.stream(WinningResult.values())
+        Arrays.stream(WinningResult.values()).filter(Predicate.not(WinningResult.NOTHING::equals))
                 .forEach(winningResult -> stringBuilder.append(formatWinningResult(winningStatistical, winningResult)));
         return stringBuilder.toString();
     }
 
     private String formatWinningResult(WinningStatistical winningStatistical, WinningResult winningResult) {
-        return String.format("%d개 일치 (%s원) - %d개\n", winningResult.getMatchedCount(), winningResult.getPrize(),
+        StringBuilder bonusMatchedMessage = new StringBuilder(" ");
+        if (winningResult.getBonusMatched()) {
+            bonusMatchedMessage.insert(0, ", 보너스 볼 일치");
+        }
+        return String.format("%d개 일치%s(%s원) - %d개\n", winningResult.getMatchedCount(), bonusMatchedMessage,
+                NumberFormat.getInstance().format(winningResult.getPrize()),
                 winningStatistical.getCountOfWinningResult(winningResult));
     }
 
     private String getFooter(WinningStatistical winningStatistical) {
-        return String.format("총 수익률은 %f%%입니다.", winningStatistical.calculateProfit());
+        return String.format("총 수익률은 %.1f%%입니다.", winningStatistical.calculateProfit());
     }
 }
