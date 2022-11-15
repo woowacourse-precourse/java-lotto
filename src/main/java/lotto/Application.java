3046a4a59cff6d5406a9561ed6e42d0b17a3d65d
parksey <parkseyeon99@naver.com>
2022-11-15 18:53:30 +0900

    feat(Application): 0원에 대한 예외처리 추가)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -47,6 +47,8 @@ public class Application {
         String amount = Console.readLine();
         InputException.emptyException(amount);
         InputException.outOfDigitException(amount);
+        InputException.userInputZeroException(amount);
+
         return amount;
     }
 
