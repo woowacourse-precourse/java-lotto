39c2751558126478d3150837004e93ac29841db9
hingjae <ljh23270@naver.com>
2022-11-15 14:05:07 +0900

    보너스 번호 입력 : 숫자 리스트와 중복 검사

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,2 +1,14 @@
-package lotto.domain;public class WinningLotto {
+package lotto.domain;
+
+public class WinningLotto {
+    private Lotto winningLottoNumber;
+    private int bonusNumber;
+
+    public WinningLotto(Lotto lotto, BonusNumber bonusNumber) {
+        this.winningLottoNumber = lotto;
+        if(winningLottoNumber.contain(bonusNumber.getNumber())) {
+            throw new IllegalArgumentException("[ERROR] 중복된 번호를 입력할 수 없습니다.");
+        }
+        this.bonusNumber = bonusNumber.getNumber();
+    }
 }
