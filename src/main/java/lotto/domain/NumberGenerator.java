586717e76c61847ff136beaf35ae97be2853c53e
jijongkwon <help3451@naver.com>
2022-11-15 14:03:19 +0900

    style : 파라미터 이름 변경

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -8,9 +8,9 @@ import java.util.List;
 public class NumberGenerator {
 
     // 구매 티켓만큼 로또번호 생성
-    public List<List<Integer>> createRandomNumbers(int ticketCount){
+    public List<List<Integer>> createRandomNumbers(int numberOfTicket){
         List<List<Integer>> lottoList =  new ArrayList<>();
-        for(int i = 0; i < ticketCount; i++){
+        for(int i = 0; i < numberOfTicket; i++){
             lottoList.add(Randoms.pickUniqueNumbersInRange(1,45,6));
         }
         return lottoList;
