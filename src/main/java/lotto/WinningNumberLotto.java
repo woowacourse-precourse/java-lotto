f31747f3619f438a76e394d72ebbfc4a7afddb04
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-15 14:21:19 +0900

    feat(checkInteger): WinningNumberLotto 클래스내  checkInteger 메서드 작성

--- src/main/java/lotto/WinningNumberLotto.java
+++ src/main/java/lotto/WinningNumberLotto.java
@@ -46,4 +46,13 @@ public class WinningNumberLotto extends Lotto {
     private void printBonusMessage() {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
+
+    private boolean checkInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (Exception exception) {
+            return false;
+        }
+        return true;
+    }
 }
