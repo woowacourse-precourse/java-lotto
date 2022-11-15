bda9b3b9f199cb333606badcf00286a833048106
p <gustjr2332@naver.com>
2022-11-15 19:04:13 +0900

    test(LottoNumberExceptionTest) : 당첨 번호와 보너스 번호 입력 예외 처리 테스트

--- src/test/java/Exception/LottoNumberExceptionTest.java
+++ src/test/java/Exception/LottoNumberExceptionTest.java
@@ -1,19 +1,44 @@
 package Exception;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class LottoNumberExceptionTest {
+    @DisplayName("당첨 번호에 문자가 입력되면 예외가 발생한다.")
     @Test
-    void inputLottoNumberByContainCharacter(){
-        Assertions.assertThatThrownBy(() -> LottoNumberException.checkException("a"))
+    void inputWinningNumberByContainCharacter() {
+        Assertions.assertThatThrownBy(() -> LottoNumberException.checkWinningNumberException("a"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("당첨 번호가 1에서 45사이 숫자가 아니면 예외가 발생한다.")
     @Test
-    void inputLottoNumberByOutOfRange(){
-        Assertions.assertThatThrownBy(() -> LottoNumberException.checkException("0"))
+    void inputWinningNumberByOutOfRange() {
+        Assertions.assertThatThrownBy(() -> LottoNumberException.checkWinningNumberException("0"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("보너스 번호에 문자가 입력되면 예외가 발생한다.")
+    @Test
+    void inputBonusNumberByContainCharacter() {
+        Assertions.assertThatThrownBy(() -> LottoNumberException.checkBonusNumberException("a", List.of(1, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 1에서 45사이 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void inputBonusNumberByOutOfRange() {
+        Assertions.assertThatThrownBy(() -> LottoNumberException.checkBonusNumberException("0", List.of(1, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 보너스 번호가 포함되어 있으면 예외가 발생한다.")
+    @Test
+    void winningNumberContainBonusNumber() {
+        Assertions.assertThatThrownBy(() -> LottoNumberException.checkBonusNumberException("1", List.of(1, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
