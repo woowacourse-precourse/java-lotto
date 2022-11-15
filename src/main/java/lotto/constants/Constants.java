9c50cb69f512da7aff3683fc1ea478ed061db8f1
we4sley <galaxy138742@gmail.com>
2022-11-15 23:39:42 +0900

    feat: 상수 생성

---
+++ src/main/java/lotto/constants/Constants.java
@@ -0,0 +1,13 @@
+package lotto.constants;
+
+public class Constants {
+
+    public static final int LOTTO_SIZE = 6;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_COST = 1000;
+    public static final int DEFAULT_VALUE = 0;
+    public static final int WIN_AMOUNT = 1;
+    public static final int PERCENT = 100;
+    public static final String COMMA = ",";
+}
