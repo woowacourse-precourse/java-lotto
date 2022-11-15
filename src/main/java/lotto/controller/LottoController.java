90d290a03ad88628672e69629925ebb11b75b945
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-15 21:27:56 +0900

    refactor: LottoController - 과한 메소드 추출 제거
    가독성 향상 목적

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -16,7 +16,7 @@ public class LottoController {
     public void run() {
         List<Lotto> lottoBundle = buyLotto();
         List<LottoResult> lottoResults = getLottoResults(lottoBundle);
-        printYield(lottoResults);
+        outputView.printYield(lottoService.calculateYield(lottoResults));
     }
 
     private List<Lotto> buyLotto() {
@@ -34,9 +34,4 @@ public class LottoController {
         outputView.printLottoResult(lottoResults);
         return lottoResults;
     }
-
-    private void printYield(List<LottoResult> lottoResults) {
-        outputView.printYield(lottoService.calculateYield(lottoResults));
-    }
-
 }
