1fd3b78655ee67d980c9a8cafc001510ef4c6097
“rotomoo” <quddlf410@naver.com>
2022-11-15 19:53:45 +0900

    feat(CustomException) : 구입 금액 1000단위 미입력 추가

--- src/main/java/lotto/exception/CustomException.java
+++ src/main/java/lotto/exception/CustomException.java
@@ -3,7 +3,8 @@ package lotto.exception;
 public enum CustomException {
     RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     VALIDATE("로또 번호를 6개 입력해주세요."),
-    DUPLICATE("중복된 로또 번호가 있습니다.");
+    DUPLICATE("중복된 로또 번호가 있습니다."),
+    HAVEREST("1000원 단위로 입력 해주세요.");
 
     private String message;
 
