2f24accafe86aa4ee64d0728d7578cabea9243ab
HAERYN <syt06119@naver.com>
2022-11-15 16:42:18 +0900

    feat: 로또 구입 금액 출력 구현

--- src/main/java/lotto/PurchaseLotto.java
+++ src/main/java/lotto/PurchaseLotto.java
@@ -1,7 +1,23 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
+// 1. 구입금액 입력
+// 2. 구입금액을 1000원 단위로 나누기
+// 3. 나누어 떨어지지 않는 경우 예외 처리
+// 4. 나누어 떨어지는 경우 구매갯수 처리
 public class PurchaseLotto {
-    public void buyLotto {
-        int i;
+    public List<Integer> buyLotto() {
+        System.out.println("구입금액을 입력해주세요");
+        List<Integer> myLotto = new ArrayList<>();
+        String moneyInput = Console.readLine();
+        int inputMoney = Integer.parseInt(moneyInput);
+        if (inputMoney % 1000 != 0)
+            throw new IllegalArgumentException("[ERROR] 로또 가격은 1000원 단위입니다!");
+        for (String lottoMoney : moneyInput.split("")) {
+            myLotto.add(Integer.parseInt(lottoMoney));
+        }
+        return myLotto;
     }
 }
