2dd71d65407fdaa8053ec29d0a74fc0d017be1ee
Umin <adorecamus@gmail.com>
2022-11-15 22:02:50 +0900

    feat(player): 당첨 번호, 보너스 번호 출력 기능\n refactor: 구입 결과 출력 기능 분리, 사용자 입력값을 숫자로 바꾸는 기능 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,9 +16,6 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 6개의 숫자여야 합니다.");
         }
-//        if (duplicate) {
-//            throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자여야 합니다.");
-//        }
         for(int number : numbers) {
             if (number < 1 || number > 45) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
--- src/main/java/lotto/Player.java
+++ src/main/java/lotto/Player.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -9,38 +11,67 @@ public class Player {
 
     private Set<Lotto> playerLotto;
 
-    public Set<Lotto> buyLotto(String money) {
+    public Set<Lotto> buyLotto() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String money = Console.readLine();
         int numberOfLotto = calculate(money);
-        System.out.println(numberOfLotto + "개를 구매했습니다.");
-
         for(int i=0; i< numberOfLotto; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             playerLotto.add(new Lotto(numbers));
         }
-
-        for(Lotto lotto : playerLotto) {
-            System.out.println(lotto);
-        }
-
+        buyResult(numberOfLotto);
         return playerLotto;
     }
 
     public int calculate(String money) throws IllegalArgumentException {
         int numberOfLotto = 0;
-        try {
-            numberOfLotto = Integer.parseInt(money) / 1000;
-        } catch (Exception e) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
-        }
+        numberOfLotto = changeToNumber(money);
         if (numberOfLotto % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
         }
         return numberOfLotto;
     }
 
-    public List<Integer> split(String input) {
+    public int changeToNumber(String input) throws IllegalArgumentException {
+        int number;
+        try {
+            number = Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
+        }
+        return number;
+    }
 
-        return null;
+    public void buyResult(int numberOfLotto) {
+        System.out.println(numberOfLotto + "개를 구매했습니다.");
+        for(Lotto lotto : playerLotto) {
+            System.out.println(lotto);
+        }
+    }
+
+    public List<Integer> pickNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        String[] numbers = input.split(",");
+        List<Integer> winningNumbers = new ArrayList<>();
+        for(String number : numbers) {
+            int eachNumber = changeToNumber(number);
+            if(!winningNumbers.contains(eachNumber)) {
+                winningNumbers.add(eachNumber);
+            }
+        }
+        new Lotto(winningNumbers);
+        return winningNumbers;
+    }
+
+    public int pickBonus() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        int bonusNumber = changeToNumber(input);
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        return bonusNumber;
     }
 
 }
