1e1921c34566db7db0545f58f42ba121fe69d767
chanwonlee <clw8679@naver.com>
2022-11-15 20:57:33 +0900

    test: PurchaseAmount 테스트 삭제후 Ticket 테스트로 병합

--- src/test/java/lotto/model/TicketTest.java
+++ src/test/java/lotto/model/TicketTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TicketTest {
     @DisplayName("구입 금액만큼 로또가 발행됐는지 테스트")
@@ -11,5 +12,19 @@ class TicketTest {
     void createTicket() {
         Ticket ticket = new Ticket(15000);
         assertThat(ticket.getLotteries().size()).isEqualTo(15);
+    }
+
+    @DisplayName("구입 금액이 1000원 이하이면 예외가 발생한다.")
+    @Test
+    void createPurchaseAmountByLess1000() {
+        assertThatThrownBy(() -> new Ticket(999))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구입 금액이 1000원 단위로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void createPurchaseAmountByNotDivide1000() {
+        assertThatThrownBy(() -> new Ticket(13500))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
