dcd013d193a875c774d02aba43861f7ee4af8e9d
wwwndus <simock22@gmail.com>
2022-11-15 21:16:12 +0900

    feat: 랜덤으로 생성한 번호 예외처리 - 길이, 수의 범위, 중복 확인

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import lotto.model.enumeration.Exception;
+
 import java.util.List;
 
 public class Lotto {
@@ -11,10 +13,32 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateLength(numbers);
+        validateRange(numbers);
+        validateDuplication(numbers);
+    }
+
+    private void validateLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number: numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(Exception.INVALID_LOTTO_NUMBER_RANGE.getExceptionMessage());
+            }
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            List<Integer> subNumbers = numbers.subList(i+1, numbers.size());
+            int number = numbers.get(i);
+            if (subNumbers.contains(number)) {
+                throw new IllegalArgumentException(Exception.INVALID_LOTTO_NUMBER_DUPLICATION.getExceptionMessage());
+            }
+        }
+    }
 }
