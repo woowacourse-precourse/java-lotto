056481b762a8a305e928c93e17b56427a0969c4c
youngin <bluest12@naver.com>
2022-11-15 17:58:38 +0900

    feat: Create method
    로또 생성 메서드 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Lotto {
@@ -17,8 +21,18 @@ public class Lotto {
     }
     // TODO: 추가 기능 구현
 
-    //구매횟수만큼 로또 출력
-    private void lottoGenerator() {
+    //구매 횟수만큼 로또 출력
+    private List lottoGenerator() {
+        Purchaser purchaser = new Purchaser();
+        System.out.printf("로또 %d개를 구매했습니다.", purchaser.dividePurchaseSum());
 
+        List<List> lottos = new ArrayList<>(purchaser.dividePurchaseSum());
+
+        for (int i = 0; i < lottos.size(); i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            numbers.sort(Comparator.naturalOrder());
+            lottos.add(i, numbers);
+        }
+        return lottos;
     }
 }
