c6c06396900067c332376c3c1661d6a9a7792f18
brotherstone97 <brotherstone97@gmail.com>
2022-11-15 14:34:10 +0900

    feat(LottoComparator): 발행된 로또 번호와 당첨 번호 비교

--- src/main/java/lotto/domain/LottoComparator.java
+++ src/main/java/lotto/domain/LottoComparator.java
@@ -1,5 +1,45 @@
 package lotto.domain;
 
-public class LottoComparator {
+import lotto.Lotto;
 
+import java.util.List;
+
+public class LottoComparator {
+    private List<Lotto> lottos;
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
+
+    public LottoComparator(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber) {
+        this.lottos = lottos;
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getNumberMatchesLottoNumber(int numberMatchesCount) {
+        return getNumberMatchesLottoNumber(numberMatchesCount, false);
+    }
+
+    public int getNumberMatchesLottoNumber(int numberMatchesCount, boolean includeBonus) {
+        int result = 0;
+
+        for (Lotto lotto : lottos) {
+            int matches = 0;
+            List<Integer> lottoNumbers = lotto.getNumbers();
+            for (Integer lottoNumber : lottoNumbers) {
+                if (winningNumbers.contains(lottoNumber)) {
+                    matches++;
+                }
+            }
+            if (includeBonus) {
+                if (numberMatchesCount == matches && lottoNumbers.contains(bonusNumber)) {
+                    result++;
+                }
+                continue;
+            }
+            if (numberMatchesCount == matches) {
+                result++;
+            }
+        }
+        return result;
+    }
 }
--- src/main/java/lotto/ui/LottoMachine.java
+++ src/main/java/lotto/ui/LottoMachine.java
@@ -6,11 +6,14 @@ import lotto.domain.LottoComparator;
 import lotto.domain.NumberGenerator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoMachine {
     InputScanner scanner = new InputScanner();
     List<Lotto> lottos = new ArrayList<>();
+    LottoComparator lottoComparator;
 
     public LottoMachine() {
     }
@@ -23,6 +26,8 @@ public class LottoMachine {
 
         Printer.requestBonusNumber();
         int bonusNumber = scanner.scanBonusNumber();
+        lottoComparator = new LottoComparator(lottos, winningNumbers, bonusNumber);
+        compareLottos();
     }
 
     private void purchaseLottos() {
@@ -33,5 +38,23 @@ public class LottoMachine {
             Lotto lotto = new Lotto(numberGenerator.createRandomNumbers());
             lottos.add(lotto);
         }
+    }
+    private void compareLottos(){
+        Map<String, Integer> lottoComparisonResults = new HashMap<>(Map.of("fifthPlace", 0,
+                "fourthPlace", 0,
+                "thirdPlace", 0,
+                "secondPlace", 0,
+                "firstPlace", 0));
+        lottoComparisonResults.put("fifthPlace", lottoComparator.getNumberMatchesLottoNumber(3));
+        lottoComparisonResults.put("fourthPlace", lottoComparator.getNumberMatchesLottoNumber(4));
+        lottoComparisonResults.put("thirdPlace", lottoComparator.getNumberMatchesLottoNumber(5));
+        lottoComparisonResults.put("secondPlace", lottoComparator.getNumberMatchesLottoNumber(5, true));
+        lottoComparisonResults.put("firstPlace", lottoComparator.getNumberMatchesLottoNumber(6));
+
+        System.out.printf("3개 일치 (5,000원) - %d개%n", lottoComparisonResults.get("fifthPlace"));
+        System.out.printf("4개 일치 (50,000원) - %d개%n", lottoComparisonResults.get("fourthPlace"));
+        System.out.printf("5개 일치 (1,500,000원) - %d개%n", lottoComparisonResults.get("thirdPlace"));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개%n", lottoComparisonResults.get("secondPlace"));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개%n", lottoComparisonResults.get("firstPlace"));
     }
 }
