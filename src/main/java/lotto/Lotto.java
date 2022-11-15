371b3338365e528446c7abe9b519e22b70f97d1e
SimSimEEE <rebe123@naver.com>
2022-11-15 19:00:17 +0900

    feat(Lotto.class): function to confirm lotto number is correct range

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,14 +4,23 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int MAXIMUM_NUMBER_OF_LOTTO = 45;
+    private static final int MINIMUM_NUMBER_OF_LOTTO = 1;
 
     public Lotto(List<Integer> numbers) {
+        numbers = DataInput.WinningNumber();
         validate(numbers);
         this.numbers = numbers;
     }
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+    private void validateRange(List<Integer> numbers) {
+        for(Integer number : numbers){
+            if(MAXIMUM_NUMBER_OF_LOTTO > number || MINIMUM_NUMBER_OF_LOTTO < number)
+                throw new IllegalArgumentException();
         }
     }
 }
