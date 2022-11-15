b41fc8768eb274bd7da23ebb1fbf00e91fd8442d
skydreamer21 <skydreamer210@gmail.com>
2022-11-15 18:41:20 +0900

    fix(ResultPhrase): 2등 출력 문구 올바르게 수정

--- src/main/java/lotto/view/constant/ResultPhrase.java
+++ src/main/java/lotto/view/constant/ResultPhrase.java
@@ -4,7 +4,7 @@ public enum ResultPhrase {
     TITLE("당첨 통계"),
     SEPARATE_LINE("---"),
     RANK("%d개 일치 (%s원) - %d개"),
-    SECOND_RANK("%d개 일치 (%s원), 보너스 볼 일치 - %d개"),
+    SECOND_RANK("%d개 일치, 보너스 볼 일치 (%s원) - %d개"),
     PROFIT_RATE("총 수익률은 %s%%입니다.");
 
     private final String phrase;
