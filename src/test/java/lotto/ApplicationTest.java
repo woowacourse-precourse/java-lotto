6695aff1aa5c07b01e2a50d1a0c13a7cf81eee3a
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-15 14:55:40 +0900

    feat(ApplicationTest): 테스트 추가

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -45,6 +45,43 @@ class ApplicationTest extends NsTest {
                 List.of(1, 3, 5, 14, 22, 45)
         );
     }
+    @Test
+    void bonus_test() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("10000", "5,16,25,26,34,35", "45");
+                    assertThat(output()).contains(
+                            "10개를 구매했습니다.",
+                            "[1, 19, 21, 28, 35, 38]",
+                            "[12, 13, 15, 27, 28, 33]",
+                            "[6, 10, 19, 23, 27, 30]",
+                            "[1, 18, 25, 32, 35, 45]",
+                            "[20, 23, 27, 32, 39, 41]",
+                            "[1, 7, 9, 24, 26, 42]",
+                            "[5, 11, 19, 23, 38, 39]",
+                            "[16, 19, 20, 28, 30, 35]",
+                            "[15, 20, 21, 25, 27, 36]",
+                            "[5, 16, 25, 26, 35, 45]",
+                            "3개 일치 (5,000원) - 0개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 300000.0%입니다."
+                    );
+                },
+                List.of(1, 19, 21, 28, 35, 38),
+                List.of(12, 13, 15, 27, 28, 33),
+                List.of(6, 10, 19, 23, 27, 30),
+                List.of(1, 18, 25, 32, 35, 45),
+                List.of(20, 23, 27, 32, 39, 41),
+                List.of(1, 7, 9, 24, 26, 42),
+                List.of(5, 11, 19, 23, 38, 39),
+                List.of(16, 19, 20, 28, 30, 35),
+                List.of(15, 20, 21, 25, 27, 36),
+                List.of(5, 16, 25, 26, 35, 45)
+        );
+    }
 
     @Test
     void noninteger_test() {
@@ -53,8 +90,6 @@ class ApplicationTest extends NsTest {
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
-
-
     @Override
     public void runMain() {
         Application.main(new String[]{});
