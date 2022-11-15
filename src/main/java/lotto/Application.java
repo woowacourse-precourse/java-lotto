b5ec00679a6f65cf82a23738e000a812fb0c5268
HikiJun97 <sgn04088@gmail.com>
2022-11-15 18:10:57 +0900

    feat: 문자열-정수 변환 메소드 구현
    올바르지 않은 입력이 들어왔을 경우 NumberFormatException을 catch 하고 다시 요구사항에 맞게 IllegalArgumentException을 throw.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import java.util.*;
+import java.text.NumberFormat;
+import java.lang.IllegalArgumentException;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    static int castInt(String str){
+        int num = 0;
+        try {
+            num = Integer.parseInt(str);
+        } catch (NumberFormatException ex){
+            throw new IllegalArgumentException(); //Error message 추후 수정
+        }
+        return num;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구매금액을 입력해주세요");
     }
 }
