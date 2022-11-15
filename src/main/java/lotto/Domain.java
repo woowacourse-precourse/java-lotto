625be1db9514846f7cdbc506d3b9e9dc454e84c6
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-15 17:01:49 +0900

    docs: Domain 클래스 인스턴스 변수 추가 lottoAmount: 로또 구매 금액 ticketNumbers: 로또 구매 개수

--- src/main/java/lotto/Domain.java
+++ src/main/java/lotto/Domain.java
@@ -3,8 +3,18 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Domain {
     private static final String INPUT_LOTTO_AMOUNT = "로또금액을 입력해 주세요.";
+
+    private static final int TICKET_PRICE = 1000;
+
+    private static int lottoAmount;
+    private static int ticketNumbers;
+
+    private static final List<Lotto> LottoList = new ArrayList<>();
 
     // TODO: 로또 금액 문장 출력 후 로또 금액을 입력 받는다.
     private static int inputLottoAmount(){
@@ -12,4 +22,13 @@ public class Domain {
         return Exception.isCorrectAmount(Console.readLine());
     }
 
+    // TODO: LOTTO_AMOUNT 변수를 초기화 한다.
+    private static void initializationLottoAmount(){
+        lottoAmount = inputLottoAmount();
+    }
+
+    // TODO: TICKET_NUMBERS 변수를 초기화 한다.
+    private static void initializationTicketNumbers(){
+        ticketNumbers = lottoAmount / TICKET_PRICE;
+    }
 }
