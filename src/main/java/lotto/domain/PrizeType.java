6f9b47c6b728213d186c4154c5a16d0b5dd73361
opuopuo <pureuml.dev@gmail.com>
2022-11-15 18:59:25 +0900

    style(prizeType): 줄바꿈 및 공백 수정

--- src/main/java/lotto/domain/PrizeType.java
+++ src/main/java/lotto/domain/PrizeType.java
@@ -7,10 +7,8 @@ public enum PrizeType {
     THREE(3, false, "3개 일치 (5,000원) - {0}개", (profit -> (profit * 5_000))),
     FOUR(4, false, "4개 일치 (50,000원) - {0}개", (profit -> (profit * 50_000))),
     FIVE(5, false, "5개 일치 (1,500,000원) - {0}개", (profit -> (profit * 1_500_000))),
-    FIVE_BONUS(5, true, "5개 일치, 보너스 볼 일치 (30,000,000원) - {0}개",
-            (profit -> (profit * 30_000_000))),
-    SIX(6, false, "6개 일치 (2,000,000,000원) - {0}개",
-            (profit -> (profit * 2_000_000_000)));
+    FIVE_BONUS(5, true, "5개 일치, 보너스 볼 일치 (30,000,000원) - {0}개", (profit -> (profit * 30_000_000))),
+    SIX(6, false, "6개 일치 (2,000,000,000원) - {0}개", (profit -> (profit * 2_000_000_000)));
 
     private final int type;
     private final boolean bonus;
@@ -25,7 +23,7 @@ public enum PrizeType {
     }
 
     public long calculateProfit(int count) {
-        return expression.apply((long)count);
+        return expression.apply((long) count);
     }
 
     public static PrizeType getPrizeType(int count, boolean bonus) {
