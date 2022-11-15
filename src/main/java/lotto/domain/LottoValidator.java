9ae1e07e960b34e3b6eb4a7707ed8d3bfacca7fa
jay <jaewhan232@naver.com>
2022-11-15 14:59:53 +0900

    Refactor : 기존 Lotto, WinningLotto 클래스에서 검수하던 로또 번호 유효성 검사 및 입력 메소드를 LottoInputFilter와 LottoValidator 클래스로 refactoring

---
+++ src/main/java/lotto/domain/LottoValidator.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class LottoValidator {
+
+    public static LottoValidator lottoValidator = new LottoValidator();
+
+    public static LottoValidator getInstance() {
+        return lottoValidator;
+    }
+
+    public static boolean isPositiveInteger(String numericString) {
+        if (numericString.chars().allMatch(Character::isDigit)) return true;
+        return false;
+    }
+
+    public static boolean isValidRange(int number) {
+        if (number < LottoConstant.STARTFROM.getValue()
+                || number > LottoConstant.ENDTO.getValue()) return false;
+        return true;
+    }
+
+    public static boolean isDuplicated(List<Integer> numbers) {
+        HashSet<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != numbers.size()) return true;
+        return false;
+    }
+
+    public static boolean isValidSize(List<Integer> numbers) {
+        if (numbers.size() != LottoConstant.SIZE.getValue()) return false;
+        return true;
+    }
+
+    public boolean isValidRangeNumbers(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (!isValidRange(number)) return false;
+        }
+        return true;
+    }
+}
