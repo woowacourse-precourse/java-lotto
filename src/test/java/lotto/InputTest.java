07da226f1c7f7639ef0e441ed82aa9439175184e
doehy <dbehxks02@naver.com>
2022-11-15 20:08:34 +0900

    test : 구입 금액이 1000원으로 나누어 떨어지지 않으면 예외

---
+++ src/test/java/lotto/InputTest.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputTest extends NsTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    @DisplayName("구입 금액이 1000원으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void examineDivideBy1000() {
+        assertSimpleTest(() -> {
+            runException("12345");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
