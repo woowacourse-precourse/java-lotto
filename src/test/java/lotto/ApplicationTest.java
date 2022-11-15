8e8e0ffd997a207285a9b3393c6a72024f1b8fa7
tlsqj <rnjstlsqja@inha.edu>
2022-11-15 23:11:40 +0900

    Test: Application.java 에 대한 테스트 구현

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -55,51 +55,7 @@ class ApplicationTest extends NsTest {
         });
     }
 
-    @Test
-    void 유저_로또_구매_성공(){
-        runTest(() -> {
-            Input.buyLotto();
-        }, "10000");
-    }
 
-
-    @Test
-    void 유저_로또_구매_실패(){
-        runTest(() -> {
-            assertThatThrownBy(() -> Input.buyLotto())
-                    .isInstanceOf(IllegalArgumentException.class);
-        }, "1000j");
-
-        runTest(() -> {
-            assertThatThrownBy(() -> Input.buyLotto())
-                    .isInstanceOf(IllegalArgumentException.class);
-        }, "1000J");
-
-        runTest(() -> {
-            assertThatThrownBy(() -> Input.buyLotto())
-                    .isInstanceOf(IllegalArgumentException.class);
-        }, "1100");
-    }
-
-    @Test
-    void 유저_당첨_번호_입력_성공(){
-        runTest(() -> {
-            Input.userInputLottoNumber();
-        }, "1,2,3,4,5,6");
-    }
-
-    @Test
-    void 유저_당첨_번호_입력_실패(){
-        runTest(() -> {
-            assertThatThrownBy(() -> Input.userInputLottoNumber())
-                    .isInstanceOf(IllegalArgumentException.class);
-        }, "1,2,3,3,4,5");
-
-        runTest(() -> {
-            assertThatThrownBy(() -> Input.userInputLottoNumber())
-                    .isInstanceOf(IllegalArgumentException.class);
-        }, "1,2,3,4,5,6,7");
-    }
 
     @Override
     public void runMain() {
