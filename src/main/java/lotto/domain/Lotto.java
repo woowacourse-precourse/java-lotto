6a74cd3ea34f73b90bfb248104419cee1e7ed37f
WonSubSub <boxer1995@naver.com>
2022-11-15 19:44:39 +0900

    feat: 보너스 번호가 1~45까지의 숫자면서 당첨번호에 있는지 판단하는 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -13,6 +13,12 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public Lotto(List<Integer> numbers, Integer bonus) {
+        sameBonusNumber(numbers, bonus);
+        wrongBonusNumber(numbers, bonus);
+        this.numbers = numbers;
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(Print.lottoNumberError());
@@ -20,19 +26,31 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
-    private void sameNumber(List<Integer> numbers){
-        for (int i = 0; i < numbers.size(); i++){
-            if (Collections.frequency(numbers, numbers.get(i)) > 1){
+    private void sameNumber(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if (Collections.frequency(numbers, numbers.get(i)) > 1) {
                 throw new IllegalArgumentException(Print.lottoNumberError());
             }
         }
     }
 
-    private void wrongNumber(List<Integer> numbers){
-        for (int i = 0; i < numbers.size(); i++){
-            if (numbers.get(i) < 1 || numbers.get(i) > 45){
+    private void wrongNumber(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) < 1 || numbers.get(i) > 45) {
                 throw new IllegalArgumentException(Print.lottoNumberError());
             }
+        }
+    }
+
+    private void sameBonusNumber(List<Integer> numbers, Integer bonus) {
+        if (Collections.frequency(numbers, bonus) == 1) {
+            throw new IllegalArgumentException(Print.bonusNumberError());
+        }
+    }
+
+    private void wrongBonusNumber(List<Integer> numbers, Integer bonus) {
+        if (bonus < 1 || bonus > 45) {
+            throw new IllegalArgumentException(Print.bonusNumberError());
         }
     }
 }
