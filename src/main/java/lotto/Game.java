d24694bc7f0eab9472f558facc95a5100076d594
JerryK026 <kskyung0624@gmail.com>
2022-11-15 09:49:10 +0900

    feat(Game) : 게임 수행
    게임을 수행할 수 있도록 Game을 구현한다

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,4 +1,27 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    private Client client;
+    private LottoAgency lottoAgency;
+
+    public Game() {
+        client = new Client(new ClientInputTerminal());
+    }
+    public void run() {
+        int amount = client.buy();
+        List<List<Integer>> tickets = new LottoAgency(amount, new NumberGeneratorRandom()).issue();
+        client.printLottoInfo(tickets);
+        List<Integer> answer = client.mark();
+        Integer bonus = client.bonusMark();
+
+        List<Prize> prizes = new ArrayList<>();
+        for (List<Integer> ticket : tickets) {
+            prizes.add(new Lotto(answer).raffle(ticket, bonus));
+        }
+        double profit = new Calculator(amount, prizes).profit();
+        client.printLottoResult(prizes, profit);
+    }
 }
