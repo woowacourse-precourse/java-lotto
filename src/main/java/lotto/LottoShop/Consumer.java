4eb2792767631e0a131af44b65b50e7fb3e98b96
skck0226 <skck0226@ajou.ac.kr>
2022-11-15 17:32:14 +0900

    [feat] 당첨 통계 출력

--- src/main/java/lotto/LottoShop/Consumer.java
+++ src/main/java/lotto/LottoShop/Consumer.java
@@ -55,6 +55,18 @@ public class Consumer {
                     profit += prize_money.get(prize);
                 }
         );
+        printPrizeStatistics(prize_count);
     }
 
+    private void printPrizeStatistics(HashMap<Integer, Integer> prize_count) {
+        System.out.println(
+                "당첨 통계\n" +
+                "---\n" +
+                "3개 일치 (5,000원) - " + prize_count.getOrDefault(FIFTH_GRADE_COUNT.getIntValue(), ZERO_COUNT.getIntValue()) + "개\n" +
+                "4개 일치 (50,000원) - " + prize_count.getOrDefault(FOURTH_GRADE_COUNT.getIntValue(), ZERO_COUNT.getIntValue()) + "개\n" +
+                "5개 일치 (1,500,000원) - " + prize_count.getOrDefault(THIRD_GRADE_COUNT.getIntValue(), ZERO_COUNT.getIntValue()) + "개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - " + prize_count.getOrDefault(SECOND_GRADE_COUNT.getIntValue(), ZERO_COUNT.getIntValue()) + "개\n" +
+                "6개 일치 (2,000,000,000원) - " + prize_count.getOrDefault(FIRST_GRADE_COUNT.getIntValue(), ZERO_COUNT.getIntValue()) + "개\n" +
+                "총 수익률은 " + String.format("%.1f", profit/(double)cost * HUNDRED.getIntValue()) + "%입니다.");
+    }
 }
