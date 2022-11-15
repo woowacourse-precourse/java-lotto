e83cdc6f1ed46e669c56ada48e431799fe67650e
이정우 <acg6138@naver.com>
2022-11-15 18:43:11 +0900

    refactor(InputView, OutputView): 출력문의 문자열 상수화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,18 +3,22 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMS_INPUT_MESSAGE = System.lineSeparator() + "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUM_INPUT_MESSAGE = System.lineSeparator() + "보너스 번호를 입력해 주세요.";
+
     public static String inputMoney() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(MONEY_INPUT_MESSAGE);
         return Console.readLine();
     }
 
     public static String inputWinningNums() {
-        System.out.println(System.lineSeparator() + "당첨 번호를 입력해 주세요.");
+        System.out.println(WINNING_NUMS_INPUT_MESSAGE);
         return Console.readLine();
     }
 
     public static String inputBonusNum() {
-        System.out.println(System.lineSeparator() + "보너스 번호를 입력해 주세요.");
+        System.out.println(BONUS_NUM_INPUT_MESSAGE);
         return Console.readLine();
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,9 +7,14 @@ import java.util.HashMap;
 import java.util.List;
 
 public class OutputView {
+    private static final String BUY_MESSAGE = System.lineSeparator() + "%d개를 구매했습니다." + System.lineSeparator();
+    private static final String START_RESULT_MESSAGE = System.lineSeparator() + "당첨 통계" + System.lineSeparator() + "---";
+    private static final String PRINT_RESULT_BY_RANK = "%d개 일치 %s - %d개" + System.lineSeparator();
+    private static final String PRINT_RESULT_OF_SECOND = "%d개 일치, 보너스 볼 일치 %s - %d개" + System.lineSeparator();
+    private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다.";
 
     public static void printLottos(int ticketCount, List<Lotto> lottos) {
-        System.out.println(System.lineSeparator() + ticketCount + "개를 구매했습니다.");
+        System.out.printf(BUY_MESSAGE, ticketCount);
         for (Lotto lotto : lottos) {
             System.out.println(lotto.get());
         }
@@ -17,19 +22,17 @@ public class OutputView {
 
     public static void printResult(HashMap<Rank, Integer> result) {
         List<Rank> printOrder = List.of(Rank.FIFTH, Rank.FOURTH, Rank.THIRD, Rank.SECOND, Rank.FIRST);
-        System.out.println(System.lineSeparator() + "당첨 통계" +  System.lineSeparator() + "---");
+        System.out.println(START_RESULT_MESSAGE);
         for (Rank rank : printOrder) {
             if (rank.equals(Rank.SECOND)) {
-                System.out.println(rank.getMatchCount() + "개 일치, 보너스 볼 일치 " + rank.getPrizeForPrint() +
-                        " - " + result.get(rank) + "개");
+                System.out.printf(PRINT_RESULT_OF_SECOND, rank.getMatchCount(), rank.getPrizeForPrint(), result.get(rank));
                 continue;
             }
-            System.out.println(rank.getMatchCount() + "개 일치 " + rank.getPrizeForPrint() +
-                    " - " + result.get(rank)  + "개");
+            System.out.printf(PRINT_RESULT_BY_RANK, rank.getMatchCount(), rank.getPrizeForPrint(), result.get(rank));
         }
     }
 
     public static void printRateOfReturn(double rateOfReturn) {
-        System.out.println("총 수익률은 " + rateOfReturn + "%입니다.");
+        System.out.printf(RATE_OF_RETURN_MESSAGE, rateOfReturn);
     }
 }
