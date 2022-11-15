179e142a3b6439d2034ef75eb0eb7ca8e68825ad
ckyeon <qsky8520@gmail.com>
2022-11-15 22:28:58 +0900

    feat(Application): implement 구매 금액을 입력받는 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.ui.Guide;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static int askPurchaseAmount() {
+        Guide.ASK_PURCHASE_AMOUNT.print();
+        return Integer.parseInt(Console.readLine());
     }
 }
