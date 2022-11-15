7e6c48ba739544ab79e648dcd7dd3e60eea31ad9
LeeHaiLim <hailim1997@gmail.com>
2022-11-15 11:23:23 +0900

    feat(lotto): add constant split regex

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final int NUMBER_RANGE_START = 1;
     public static final int NUMBER_RANGE_END = 45;
     public static final int LOTTO_PRICE = 1000;
+    public static final String SPLIT_REGEX = ",";
 }
