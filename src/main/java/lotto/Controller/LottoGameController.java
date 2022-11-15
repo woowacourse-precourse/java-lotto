2e1d38d49f5ea2ede68be3bb99883622943e6102
Sieun Park <psieun0606@gmail.com>
2022-11-15 16:29:22 +0900

    feat: 문자열을 정수 리스트로 변환하는 기능 추가

--- src/main/java/lotto/Controller/LottoGameController.java
+++ src/main/java/lotto/Controller/LottoGameController.java
@@ -1,15 +1,22 @@
 package lotto.Controller;
 
+import lotto.Domain.Lotto;
 import lotto.Domain.LottoPublisher;
 import lotto.Domain.PurchasingLotto;
 import lotto.View.InputView;
 import lotto.View.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoGameController {
 
     InputView inputView;
     OutputView outputView;
     PurchasingLotto purchasingLotto;
+
+    List<Lotto> userInputLottoNumberList = new ArrayList<>();
+    int bonusNumber;
 
     public void gameStart() {
         purchaseLotto();
@@ -21,5 +28,16 @@ public class LottoGameController {
         new PurchasingLotto(purchasing);
         outputView.printTheNumberOfLotto(purchasingLotto.getAmountOfLotto());
         outputView.printLottoNumber(purchasingLotto.getPurchase());
+    }
+
+    private List<Integer> translateStringToIntegerList(String text) {
+        List<Integer> integerList = new ArrayList<>();
+
+        for (int i = 0; i < text.length(); i++) {
+            int indexChar = text.charAt(i);
+            integerList.add(indexChar - '0');
+        }
+
+        return integerList;
     }
 }
