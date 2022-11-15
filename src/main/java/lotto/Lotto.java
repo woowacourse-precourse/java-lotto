c9b067e59e41653277316b648e23c328b0ab323c
yoonsj970407 <yoonsj970407@gmail.com>
2022-11-15 11:34:51 +0900

    Feat: 로또 당첨 번호 입력 후, 예외 처리 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,40 @@
 package lotto;
 
+import lotto.message.ErrorMessage;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumbersSize(numbers);
+        validateNumbersRange(numbers);
+        validateNumbersDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateNumbersSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(String.valueOf(ErrorMessage.Lotto_Numbers_Size));
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateNumbersRange(List<Integer> numbers) {
+        for(int i = 0; i < 6; i++) {
+            if(numbers.get(i) < 1 || numbers.get(i) > 45) {
+                throw new IllegalArgumentException(String.valueOf(ErrorMessage.Lotto_Numbers_Range));
+            }
+        }
+    }
+
+    private void validateNumbersDuplicate(List<Integer> numbers) {
+        if(!numbers.stream().distinct().equals(numbers)) {
+            throw new IllegalArgumentException(String.valueOf(ErrorMessage.Lotto_Numbers_Duplicate));
+        }
+    }
+
+    public List<Integer> lottoNumbers() {
+        return numbers;
+    }
 }
