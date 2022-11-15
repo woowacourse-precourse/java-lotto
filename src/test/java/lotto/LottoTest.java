db440b1a48e0c4cecd3e36858c9c78ecb9ac4d1c
star-sil <kimse9811@naver.com>
2022-11-15 19:56:04 +0900

    test(test/lotto): test lotto issue feature
    로또 번호 생성 기능 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,14 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoPrinter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,4 +28,24 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("중복되지 않은 번호 6개를 만든다.")
+    @Test
+    void 중복되지_않은_번호_6개를_생성_확인() {
+        List<Integer> numbers = LottoPrinter.makeUniqueNumbers();
+        HashSet<Integer> uniqueNumbers = new HashSet<>(numbers);
+
+        assertThat(numbers.size() == 6);
+        assertThat(numbers.size() == uniqueNumbers.size());
+    }
+
+    // 아래에 추가 테스트 작성 가능
+    @DisplayName("1 ~ 45 범위의 번호만을 생성한다.")
+    @Test
+    void 알맞은_범위의_번호_생성_확인() {
+        List<Integer> numbers = LottoPrinter.makeUniqueNumbers();
+
+        for (Integer number : numbers) {
+            assertThat(number > 0 && number < 46);
+        }
+    }
 }
