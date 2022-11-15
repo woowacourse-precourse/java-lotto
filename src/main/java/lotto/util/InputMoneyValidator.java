487b1c08eabc025cc4d025ddf16e3dbc82217bfa
sooyoungh <sooyoung.h8@gmail.com>
2022-11-15 18:45:15 +0900

    ✨ feat(InputMoneyValidator): 입력한 로또 구입 금액 형식 검증하는 기능 구현

--- src/main/java/lotto/util/InputMoneyValidator.java
+++ src/main/java/lotto/util/InputMoneyValidator.java
@@ -3,6 +3,13 @@ package lotto.util;
 public class InputMoneyValidator {
     private static final int MONEY_UNIT = 1000;
 
+    public static void validateInputMoney(String inputMoney) {
+        LottoNumberFormatValidator.validateInteger(inputMoney);
+        Integer money = Integer.parseInt(inputMoney);
+        validateMoneyUnit(money);
+        validateMoneyRange(money);
+    }
+
     public static void validateMoneyUnit(Integer money) {
         int remainder = money % MONEY_UNIT;
         if (remainder != 0) throw new IllegalArgumentException("입력 금액이 1000원 단위이어야 합니다.");
