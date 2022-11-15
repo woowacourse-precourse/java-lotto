a3c24f2a10d569e5a0b273c659cc662350898969
ghchoi <ghchoi0427@gmail.com>
2022-11-15 16:03:19 +0900

    fix: 상금 값 설정

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,5 +1,20 @@
 package lotto.domain;
 
 public enum LottoResult {
-    first, second, third, fourth, fifth, none
+    first(2000000000),
+    second(30000000),
+    third(1500000),
+    fourth(50000),
+    fifth(5000),
+    none(0);
+
+    private int prizeMoney;
+
+    LottoResult(int prizeMoney) {
+        this.prizeMoney = prizeMoney;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
 }
