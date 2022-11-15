db0f2b84dd70afa97aecdda8353cabda9ad1313a
chosunghyun18 <krismon@naver.com>
2022-11-15 17:42:08 +0900

    test(lotterymachine): add testcode about domain logic in the compare feature
    관련 기능 목록 : 로또 당첨 기계
    로또 당첨 번호를 비교하는 테스트 코드
    작은 기능의 함수 부터 통합 하여 결과를 합산하는 도메인 로직에 관련된
    테스트 코드입니다.

--- src/test/java/lotto/lotterymachine/service/LotteryMachineServiceTest.java
+++ src/test/java/lotto/lotterymachine/service/LotteryMachineServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.EnumMap;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import lotto.lotterymachine.repository.LotteryMachineRepository;
 import lotto.util.Constant;
@@ -12,14 +13,6 @@ import org.junit.jupiter.api.Test;
 
 class LotteryMachineServiceTest {
     private Map<Score, Integer> ScoreBoard;
-
-    @Test
-    void map_test() {
-        final Map<String, Integer> map = new HashMap<>();
-        map.put("aa",1);
-        map.put("aa",map.get("aa")+1);
-        System.out.println(map);
-    }
     @Test
     void score_board_total_check() {
 
@@ -39,7 +32,73 @@ class LotteryMachineServiceTest {
         assertThat(target).isEqualTo(result);
     }
 
+    @Test
+    void input_score_in_board_test() {
+        //when
+        LotteryMachineService lotteryMachineService = new LotteryMachineService();
+        ScoreBoard = lotteryMachineService.getScoreBoard();
+        for (int i = Constant.INIT_ZERO; i < Score.values().length; i++) {
+            ScoreBoard.put(Score.values()[i], Score.ZERO.ordinal());
+        }
+        lotteryMachineService.setScoreBoardForTest(ScoreBoard);
+        int target =1 ;
+        //then
+        lotteryMachineService.featScoreBoardPlusOne(3);
 
+        assertThat(target).isEqualTo(ScoreBoard.get(Score.THREE));
+    }
+    @Test
+    void input_score_if_board_bonus_ball_test() {
+
+        LotteryMachineService lotteryMachineService = new LotteryMachineService();
+        final List<Integer> userLottery = List.of(1, 2, 3, 4, 5, 6);
+        int testTarget = 1;
+        int result =lotteryMachineService.checkBonusNumber(userLottery,1,0);
+
+        assertThat(testTarget).isEqualTo(result);
+    }
+
+    @Test
+    void input_score_if_won_five_test() {
+        LotteryMachineService lotteryMachineService = new LotteryMachineService();
+        final List<Integer> userLottery = List.of(1, 2, 3, 4, 5, 6);
+        ScoreBoard = lotteryMachineService.getScoreBoard();
+        for (int i = Constant.INIT_ZERO; i < Score.values().length; i++) {
+            ScoreBoard.put(Score.values()[i], Score.ZERO.ordinal());
+        }
+
+        lotteryMachineService.setScoreBoardForTest(ScoreBoard);
+
+        int testTarget = 1;
+        lotteryMachineService.setScores(userLottery,11,5);
+        assertThat(testTarget).isEqualTo(ScoreBoard.get(Score.FIVE));
+    }
+    @Test
+    void input_score_if_won_five_Bonus_test() {
+        LotteryMachineService lotteryMachineService = new LotteryMachineService();
+        final List<Integer> userLottery = List.of(1, 2, 3, 4, 5, 6);
+        ScoreBoard = lotteryMachineService.getScoreBoard();
+        for (int i = Constant.INIT_ZERO; i < Score.values().length; i++) {
+            ScoreBoard.put(Score.values()[i], Score.ZERO.ordinal());
+        }
+        int testTarget = 1;
+        lotteryMachineService.setScores(userLottery,1,5);
+        assertThat(ScoreBoard.get(Score.FIVE_BONUS)).isEqualTo(testTarget);
+    }
+
+    @Test
+    void input_score_if_won_six_test() {
+        LotteryMachineService lotteryMachineService = new LotteryMachineService();
+        final List<Integer> userLottery = List.of(1, 2, 3, 4, 5, 6);
+        ScoreBoard = lotteryMachineService.getScoreBoard();
+        for (int i = Constant.INIT_ZERO; i < Score.values().length; i++) {
+            ScoreBoard.put(Score.values()[i], Score.ZERO.ordinal());
+        }
+
+        int testTarget = 1;
+        lotteryMachineService.setScores(userLottery,11,6);
+        assertThat(testTarget).isEqualTo(ScoreBoard.get(Score.SIX));
+    }
 
 
 }
