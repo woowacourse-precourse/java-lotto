9369a60ba1bcfbba370428b2b59aeb04a057ff3a
LeeHaiLim <hailim1997@gmail.com>
2022-11-15 09:44:31 +0900

    feat(view): implement class input view
    싱글톤 패턴으로 뷰 클래스 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+public class InputView {
+    private static InputView instance = new InputView();
+
+    private InputView(){}
+
+    public static InputView getInstance(){
+        return instance;
+    }
+}
