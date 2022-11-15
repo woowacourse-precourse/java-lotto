215da4b90fdc08a0c27a5d4abd2657e610455b3b
masiljangajji <hjk172262@gmail.com>
2022-11-15 19:06:11 +0900

    feat: 정수형 데이터에 유효성을 검사하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,19 +1,27 @@
 package lotto;
 
 import java.util.List;
+import view.Message;
 
 public class Lotto {
     private final List<Integer> numbers;
-
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        checkNumberRange(numbers);
+        checkNumberOverLap(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void checkNumberRange(List<Integer> numbers)  {
+            if (numbers.size() != 6) {
+                throw new IllegalArgumentException(Message.ERRROR_NUMBER_RANGE.getMessage());
+            }
+    }
+
+    private void checkNumberOverLap(List<Integer>numbers){
+        if(numbers.size() != numbers.stream().distinct().count()){
+            throw new IllegalArgumentException(Message.ERROR_NUMBER_OVERLAP.getMessage());
         }
     }
+
 
 }
