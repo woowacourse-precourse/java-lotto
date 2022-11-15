1bbe0e84e327d2f6945cd7bfc4f65cd9f4498a99
inhyeok <ss112d86@gmail.com>
2022-11-15 18:22:14 +0900

    feat: 결과 출력 기능 구현

--- src/main/java/lotto/console/ConsoleOutput.java
+++ src/main/java/lotto/console/ConsoleOutput.java
@@ -1,12 +1,17 @@
 package lotto.console;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lotto.domain.Lotto;
+import lotto.domain.LottoPrize;
 
 public class ConsoleOutput {
 
   private final StringBuilder stringBuilder = new StringBuilder();
+  private final String RESULT_MESSAGE = "당첨 통계\n---";
 
   public void printLotto(List<Lotto> lottos) {
     for (Lotto lotto : lottos) {
@@ -28,5 +33,42 @@ public class ConsoleOutput {
       stringBuilder.append(integers.get(i)).append(", ");
     }
     stringBuilder.append("\n");
+  }
+
+
+  public void printResult(List<LottoPrize> prizes, double profitRate) {
+    System.out.println(RESULT_MESSAGE);
+    Map<LottoPrize, Integer> eachPrizeCount = calculateEachPrizeCount(prizes);
+    getPrintedMessage(eachPrizeCount);
+    stringBuilder.append("총 수익률은 ").append(profitRate).append("%입니다.");
+    System.out.println(stringBuilder);
+    stringBuilder.setLength(0);
+  }
+
+  private void getPrintedMessage(Map<LottoPrize, Integer> eachPrizeCount) {
+    stringBuilder.append("3개 일치 (5,000원) - ").append(eachPrizeCount.get(LottoPrize.FIFTH))
+        .append("개\n");
+    stringBuilder.append("4개 일치 (50,000원) - ").append(eachPrizeCount.get(LottoPrize.FOURTH))
+        .append("개\n");
+    stringBuilder.append("5개 일치 (1,500,000원) - ").append(eachPrizeCount.get(LottoPrize.THIRD))
+        .append("개\n");
+    stringBuilder.append("5개 일치, 보너스 볼 일치 (30,000,000원) - ")
+        .append(eachPrizeCount.get(LottoPrize.SECOND)).append("개\n");
+    stringBuilder.append("6개 일치 (2,000,000,000원) - ").append(eachPrizeCount.get(LottoPrize.FIRST))
+        .append("개\n");
+  }
+
+
+  private Map<LottoPrize, Integer> calculateEachPrizeCount(List<LottoPrize> prizes) {
+    Map<LottoPrize, Integer> eachPrizeCount = new HashMap<>();
+    Arrays.stream(LottoPrize.values()).forEach(prize -> eachPrizeCount.put(prize, 0));
+
+    prizes.forEach(prize -> {
+      if (eachPrizeCount.containsKey(prize)) {
+        int value = eachPrizeCount.get(prize);
+        eachPrizeCount.put(prize, value + 1);
+      }
+    });
+    return eachPrizeCount;
   }
 }
--- src/test/java/lotto/console/ConsoleOutputTest.java
+++ src/test/java/lotto/console/ConsoleOutputTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.LottoPrize;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -40,6 +41,16 @@ class ConsoleOutputTest {
   }
 
 
+  @Test
+  void  당첨된결과와_수익률이_출력된다(){
+    List<LottoPrize> lottoPrizes = List.of(LottoPrize.FIFTH);
+    double givenRate = 62.5;
+
+    output.printResult(lottoPrizes, givenRate);
+
+    assertThat(outputStreamCaptor.toString()).contains("3개 일치 (5,000원) - 1개");
+    assertThat(outputStreamCaptor.toString()).contains("총 수익률은 62.5%입니다.");
+  }
   @AfterEach
   public void tearDown() {
     System.setOut(standardOut);
