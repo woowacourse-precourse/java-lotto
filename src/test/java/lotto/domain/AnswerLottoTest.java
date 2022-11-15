039d6724bb7ee4938a305f2e2a518e4c60ebb725
Yejin Shin <shinyejin0212@gmail.com>
2022-11-15 20:38:00 +0900

    test(AnswerLottoTest): add over limit number error test

---
+++ src/test/java/lotto/domain/AnswerLottoTest.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AnswerLottoTest {
+    @DisplayName("당첨 번호에 1 ~ 45 사이의 값이 아닌 숫자가 있을 경우 예외가 발생한다.")
+    @Test
+    void createAnswerLottoByOverLimitNumber(){
+        assertThatThrownBy(() -> new AnswerLotto(List.of(1, 2, 3, 4, 5, 55), 8))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
