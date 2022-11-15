025f2541ef7ec4e6b5dff8b439c5a01a48a3e246
kdkdhoho <hkim4410@naver.com>
2022-11-15 14:52:04 +0900

    refactor(InputUtil): 코드 간결화

--- src/main/java/lotto/util/InputUtil.java
+++ src/main/java/lotto/util/InputUtil.java
@@ -5,7 +5,6 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputUtil {
 
     public static String getUserInput() {
-        String input = readLine();
-        return input;
+        return readLine();
     }
 }
