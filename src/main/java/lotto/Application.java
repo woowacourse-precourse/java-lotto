ac15074cb573b2b159a0d8177dc78e561244df35
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-15 21:39:51 +0900

    refactor: Move logic of manipulation with lotto ticket to machine class

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,17 +1,11 @@
 package lotto;
 
-import static lotto.Cli.inputPurchaseAmount;
-import static lotto.Cli.print;
-import static lotto.Machine.createLotto;
-
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        int amount = inputPurchaseAmount();
-        int purchaseCount = amount / 1000;
-
-        List<Lotto> lottoList = createLotto(purchaseCount);
-        print(lottoList);
+        Machine lottoMachine = new Machine();
+        int ticketCount = lottoMachine.getMoney();
+        List<Lotto> ticketList = lottoMachine.sellTicket(ticketCount);
     }
 }
--- src/main/java/lotto/Machine.java
+++ src/main/java/lotto/Machine.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import static lotto.Cli.inputPurchaseAmount;
+import static lotto.Cli.print;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,21 +12,31 @@ import java.util.stream.Collectors;
 public class Machine {
 
   /**
-   * Function to create lotto.
-   * @param count The integer value of lotto count.
-   * @return The list of lotto.
+   * Get money to return ticket count.
+   * @return The count of ticket.
    */
-  public static List<Lotto> createLotto(Integer count) {
+  public Integer getMoney() {
+    int amount = inputPurchaseAmount();
+    return amount / 1000;
+  }
 
-    List<Lotto> lottoList = new ArrayList<>();
+
+  /**
+   * Function to sell lotto tickets.
+   * @param count The integer value of lotto count.
+   * @return The list of lotto tickets.
+   */
+  public List<Lotto> sellTicket(Integer count) {
+
+    List<Lotto> ticketList = new ArrayList<>();
     for (int i=0; i < count; i++) {
       List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
       List<Integer> sortedNumber = numbers.stream()
           .sorted().collect(Collectors.toList());
       Lotto lotto = new Lotto(sortedNumber);
-      lottoList.add(lotto);
+      ticketList.add(lotto);
     }
-
-    return lottoList;
+    print(ticketList);
+    return ticketList;
   }
 }
