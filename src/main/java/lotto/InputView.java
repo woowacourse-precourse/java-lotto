71686aacd16554e7961dd6015520b94cdbe3e4d2
ashlovesliitea <skyrider1811@gmail.com>
2022-11-15 20:42:28 +0900

    feat: InputView - 메세지 출력 기능 구현

---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,20 @@
+package lotto;
+
+public class InputView{
+
+    private void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public String requestUserMoney(){
+
+    }
+
+    public String requestWinningNumbers(){
+
+    }
+
+    public String requestBonusNumber(){
+
+    }
+}
