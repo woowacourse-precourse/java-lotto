38d6510939c2dfc6369c3f3edbbb89c1215d68e9
kite707 <kite707707@gmail.com>
2022-11-15 17:14:45 +0900

    docs(lotto): 로또 발행 횟수만큼 로또 번호 지정 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,17 @@
 package lotto.controller;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.model.Lotto;
 
 public class LottoController {
+    List<Lotto> lottoList = new ArrayList<>();
+    public void makeLottoList(int publicians) {
+        for (int i = 0; i < publicians; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            lottoList.add(lotto);
+        }
+    }
+
 }
