c7a03d361c4ad9bd120125bcb3a26d00788bda5c
farmJun <junpreme1205@gmail.com>
2022-11-15 21:23:32 +0900

    feat(Application): 로또 발행 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
-
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static lotto.GuideMessage.*;
 import static lotto.ErrorMessage.*;
@@ -10,6 +13,9 @@ public class Application {
     private static final int INVALID_PURCHASE_MONEY = -1;
     private static final int LOTTO_TICKET_MONEY = 1000;
     private static final int NEGATIVE_RANGE = 0;
+    private static final int LOTTO_NUMBER_FIRST_RANGE = 1;
+    private static final int LOTTO_NUMBER_LAST_RANGE = 45;
+    private static final int LOTTO_NUMBERS_SIZE = 6;
 
     public static int getLottoPurchaseMoney() {
         System.out.println(INPUT_REQUEST_MESSAGE.getMessage());
@@ -48,7 +54,23 @@ public class Application {
         }
     }
 
+    public static List<Lotto> makeLottery(int lottoPurchaseMoney) {
+
+        List<Lotto> lottery = new ArrayList<>();
+        int lottoTicketCount = lottoPurchaseMoney / LOTTO_TICKET_MONEY;
+
+        for (int iterator = 0; iterator < lottoTicketCount; iterator++) {
+            lottery.add(new Lotto(Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_FIRST_RANGE, LOTTO_NUMBER_LAST_RANGE, LOTTO_NUMBERS_SIZE)));
+        }
+
+        return lottery;
+    }
+
     public static void main(String[] args) {
         int lottoPurchaseMoney = getLottoPurchaseMoney();
+        if (lottoPurchaseMoney == INVALID_PURCHASE_MONEY) {
+            return;
+        }
+        List<Lotto> lottery = makeLottery(lottoPurchaseMoney);
     }
 }
