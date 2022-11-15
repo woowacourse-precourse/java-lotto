d4a0ab39f8f25a2608d65e28968ed67ae57b1531
junyong <jymoon1121@gmail.com>
2022-11-15 15:25:33 +0900

    feat: 당첨 결과 계산 함수 생성 구현

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -11,29 +11,56 @@ public class LottoMachine {
     private final int LOTTO_PRICE = 1000;
     private int paid;
     private ArrayList<Lotto> lottos = new ArrayList<>();
-    public LottoMachine() {
+    private int[] result;
 
+    public LottoMachine() {
     }
+
     public void getInputMoney() {
         String input = Console.readLine();
         int money = Integer.parseInt(input);
         validateMoney(money);
         this.paid = Integer.parseInt(input);
     }
+
     public void validateMoney(int money) {
-        if(money % LOTTO_PRICE != 0) {
+        if (money % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
         }
     }
+
     public List<Lotto> createLottoNumber() {
         int cnt = paid / LOTTO_PRICE;
-        for(int index = 0; index < cnt; index ++) {
+        for (int index = 0; index < cnt; index++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Lotto lotto = new Lotto(numbers);
             lottos.add(lotto);
         }
-        paid = 0;
         return lottos;
     }
 
+    public void printLottos() {
+        System.out.println(paid / LOTTO_PRICE + "개를 구매했습니다. ");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public  void getResult(List<Integer> winningNum, int bonusNum) {
+        result = new int [6];
+        for(Lotto lotto: lottos) {
+            int rank = 0;
+            List<Integer> numbers = lotto.getNumbers();
+            if(numbers.contains(bonusNum)) {
+                rank = -1;
+            }
+
+            numbers.retainAll(winningNum);
+            rank += 8-numbers.size();
+            if(numbers.size() == 6) {
+                rank = 1;
+            }
+            result[rank] += 1;
+        }
+    }
 }
