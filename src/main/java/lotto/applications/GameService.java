b122b725179739c259dc59dcc0175f4890d6e15b
mikekang47 <gyeong0308@gmail.com>
2022-11-15 19:36:54 +0900

    Feat<GameService>: 당첨 정보를 담은 Map을 반환하는 getProfitResult 메서드 추가

--- src/main/java/lotto/applications/GameService.java
+++ src/main/java/lotto/applications/GameService.java
@@ -1,14 +1,28 @@
 package lotto.applications;
 
 import lotto.domains.Lotto;
+import lotto.domains.Place;
 import lotto.utils.Calculator;
+import lotto.utils.LottoNumberConverter;
 import lotto.utils.LottoNumberMaker;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GameService {
     private static GameService INSTANCE;
+
+    private final Map<Place, Integer> prizeMap = new HashMap<>();
+    {
+        prizeMap.put(Place.FIRST, 0);
+        prizeMap.put(Place.SECOND, 0);
+        prizeMap.put(Place.THIRD, 0);
+        prizeMap.put(Place.FOURTH, 0);
+        prizeMap.put(Place.FIFTH, 0);
+        prizeMap.put(Place.ZERO, 0);
+    }
 
     private GameService() {
     }
@@ -32,5 +46,21 @@ public class GameService {
             lottoNumbers.add(lotto);
         }
         return lottoNumbers;
+    }
+
+    public Map<Place, Integer> getProfitResult(
+            final List<Lotto> userBoughtLotteries,
+            final Lotto winningLotto
+    ) {
+        final int bonusNumber = winningLotto.getBonusNumber();
+        List<List<Integer>> convertedUserLotteries = LottoNumberConverter
+                .convertTo2DimensionalList(userBoughtLotteries);
+
+        List<Integer> winningNumbers = winningLotto.getNumbers();
+        for (List<Integer> userLotto : convertedUserLotteries) {
+            Place place = Calculator.calculatePrize(userLotto, winningNumbers, bonusNumber);
+            prizeMap.put(place, prizeMap.get(place) + 1);
+        }
+        return prizeMap;
     }
 }
