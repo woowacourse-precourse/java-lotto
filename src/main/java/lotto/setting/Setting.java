8cb8bcdf6d3d9c1b03a83d2fa76d7e8acd0dbd61
cocomozila <joseyun94@naver.com>
2022-11-15 17:43:16 +0900

    feat(Setting): 당첨 결과 설정값 추가

--- src/main/java/lotto/setting/Setting.java
+++ src/main/java/lotto/setting/Setting.java
@@ -9,4 +9,6 @@ public class Setting {
     public static final int LOTTO_SIZE = 6;
     public static final int INPUT_WINNING_NUMBER_COMMA_COUNT = 5;
     public static final char SPLIT_CHARACTER = ',';
+    public static final int PRIZE_LIST_SIZE = 6;
+    public static final int SHOW_PRIZE_LIST_MIN_RANK = 1;
 }
