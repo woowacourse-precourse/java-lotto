44875faac0a2c8d1ceef0cee28e1ed83cd877b58
ostar11 <cjw5877@gmail.com>
2022-11-15 15:01:35 +0900

    chore(Judgement): 변수명 수정

--- src/main/java/lotto/Judgement.java
+++ src/main/java/lotto/Judgement.java
@@ -3,17 +3,17 @@ package lotto;
 import java.util.List;
 
 public class Judgement {
-    public int correctCount(List<Integer> lottoNumbers, List<Integer> winningNumbers) {
+    public int correctCount(List<Integer> lotto, List<Integer> winningNumber) {
         int count = 0;
-        for (Integer number : lottoNumbers) {
-            if (winningNumbers.contains(number)) {
+        for (Integer number : lotto) {
+            if (winningNumber.contains(number)) {
                 count++;
             }
         }
         return count;
     }
 
-    public boolean hasBonusNumbers(int bonusNumber, List<Integer> lottoNumbers) {
-        return lottoNumbers.contains(bonusNumber);
+    public boolean hasBonusNumbers(int bonusNumber, List<Integer> lotto) {
+        return lotto.contains(bonusNumber);
     }
 }
