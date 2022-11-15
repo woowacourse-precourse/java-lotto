40468e369386ea4f9b322f439d9dc2ac6bb9b0e2
noparamin <noparamin@naver.com>
2022-11-15 21:37:00 +0900

    refactor: 하드코딩 부분 enum 적용하여 수정

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.enums.EtcNumber;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,24 +9,32 @@ import java.util.List;
 public class LottoManager {
     CalculatorUtility calculatorUtility = new CalculatorUtility();
 
-    private List<Lotto> lottoTickets = new ArrayList<>();
-
-    public void generateLottoTickets(int purchasingAmount) {
+    public LottoTickets generateLottoTickets(int purchasingAmount) {
+        List<Lotto> lottos = new ArrayList<>();
         int tickets = calculatorUtility.convertMoneyToTickets(purchasingAmount);
-        for(int count = 0; count < tickets; count++) {
+
+        for (int count = EtcNumber.ZERO.getNumber(); count < tickets; count++) {
             List<Integer> numbers = generateLottoNumbers();
             Lotto lotto = new Lotto(numbers);
-            lottoTickets.add(lotto);
+            lottos.add(lotto);
         }
+        LottoTickets lottoTickets = new LottoTickets(lottos);
 
+        return lottoTickets;
     }
 
     public List<Integer> generateLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(EtcNumber.NUMBER_MIN.getNumber(), EtcNumber.NUMBER_MAX.getNumber(), EtcNumber.LOTTO_SIZE.getNumber());
     }
 
-    public List<Lotto> getLottoTickets() {
-        return lottoTickets;
+    public WinningLotto generateWinningLotto(List<Integer> winningNumbers) {
+        WinningLotto winningLotto = new WinningLotto(winningNumbers);
+        return winningLotto;
+    }
+
+    public BonusBall generateBonusBall(int bonusNumber) {
+        BonusBall bonusBall = new BonusBall(bonusNumber);
+        return bonusBall;
     }
 
 }
