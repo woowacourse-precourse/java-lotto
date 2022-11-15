721927db02e5ab0a31e273d6aa83e2e049d676c3
Haechan Lee <dlgockssla@gmail.com>
2022-11-15 20:46:06 +0900

    feat: 사용자가 잘못된 값 입력 시 에러 메시지 출력 후 종료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,11 @@ public class Application {
     public static void main(String[] args) {
 
         LottoController lottoController = new LottoController();
-        lottoController.run();
+        try {
+            lottoController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            // throw e;
+        }
     }
 }
