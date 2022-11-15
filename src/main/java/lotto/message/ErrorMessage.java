c70b0bc243102d42c2b6bb3b051560614245eb1c
Invidam <hansupark0208@gmail.com>
2022-11-15 11:07:54 +0900

    refactor(error-message): 중복 에러 메시지 추가

--- src/main/java/lotto/message/ErrorMessage.java
+++ src/main/java/lotto/message/ErrorMessage.java
@@ -1,7 +1,8 @@
 package lotto.message;
 
 public enum ErrorMessage {
-    OUT_OF_RANGE_MESSAGE("보너스 숫자의 범위가 1 ~ 45 이내가 아닙니다.");
+    OUT_OF_RANGE_MESSAGE("보너스 숫자의 범위가 1 ~ 45 이내가 아닙니다."),
+    DUPLICATE_MESSAGE("보너스 숫자와 중복이 되는 숫자가 존재합니다.");
 
     static final String PREFIX = "[ERROR]";
     final String message;
