3cb51b4f70afa57357020bf5e0497a1dad1119cb
wnsgml7267 <dlwns7267@naver.com>
2022-11-15 22:28:14 +0900

    feat(view/InputView): 입력 값 기능 클래스 생성

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class InputView {
+    private final String MESSAGE_TO_GET_MONEY = "구입금액을 입력해 주세요.";
+    private final String MESSAGE_TO_GET_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.";
+    private final String MESSAGE_TO_GET_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+}
