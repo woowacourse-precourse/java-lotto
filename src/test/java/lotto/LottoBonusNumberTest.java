6f5f5d74897eab4714409f5491a9ef3cd38810c0
KimJaeSung <behappy0916@naver.com>
2022-11-15 11:14:42 +0900

    test: bonusNumber에 관한 테스트 추가
    숫자 이외에 값 입력 시 예외 테스트
    숫자 범위 이외에 값 입력 시 예외 테스트

---
+++ src/test/java/lotto/LottoBonusNumberTest.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoBonusNumber;
+import org.junit.jupiter.api.Test;
+
+
+class LottoBonusNumberTest {
+    @Test
+    void 숫자_이외에_값_입력() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new LottoBonusNumber("a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void 숫자_범위_이외에_값_입력() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new LottoBonusNumber("46"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
