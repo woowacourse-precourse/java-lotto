eb34cbad3f1cf54782d38dccfddad34f92405bd1
0112leesy <0112leesy@naver.com>
2022-11-15 13:06:51 +0900

    feat(Game) : setWinningNumber 함수 추가
    주어진 입력 값을 당첨 로또로 설정하는 기능

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,16 +1,30 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
     static UI ui = new UI();
     static Validation validation = new Validation();
     static int purchase;
+    static Lotto winningNumber;
 
     static void setPurchase(String input) throws IllegalArgumentException {
         validation.checkNull(input);
         validation.checkNumeric(input);
         purchase = Integer.parseInt(input);
         validation.checkDividable(purchase);
+    }
+
+    static void setWinningNumber(String input) throws IllegalArgumentException {
+        validation.checkNull(input);
+        validation.checkWinningFormat(input);
+        List<Integer> number = new ArrayList<>();
+        for (String token : input.split(",")) {
+            number.add(Integer.parseInt(token));
+        }
+        winningNumber = new Lotto(number);
     }
 
 }
