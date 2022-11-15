110e6e47a050f4863bdf08546e22ec5008985082
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-15 20:51:05 +0900

    feat: 당첨 번호에 중복된 숫자 존재하면 예외 처리하는 메소드 구현

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,21 +1,31 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class WinningNumbers {
 
-    private static final String DUPLICATE_NUMBER_ERROR = "[ERROR] 당첨 번호와 보너스 번호는 중복될 수 없습니다.";
+    private static final String CONTAIN_BONUS_NUMBER_ERROR = "[ERROR] 당첨 번호와 보너스 번호는 중복될 수 없습니다.";
+    private static final String DUPLICATE_NUMBER_ERROR = "[ERROR] 당첨 번호에 중복된 숫자가 있습니다.";
     public final List<Integer> winningNumbers;
     private final int bonusNumber;
 
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+        validateContainBonusNumber();
         validateDuplicateNumber();
     }
 
-    private void validateDuplicateNumber() {
+    private void validateContainBonusNumber() {
         if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(CONTAIN_BONUS_NUMBER_ERROR);
+        }
+    }
+
+    private void validateDuplicateNumber() {
+        HashSet<Integer> numbers = new HashSet<>(winningNumbers);
+        if (numbers.size() != 6) {
             throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR);
         }
     }
