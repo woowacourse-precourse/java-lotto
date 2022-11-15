704cb4a364b375a1b13555d024deb01f4db35c27
neppiness <0414kjh@naver.com>
2022-11-15 13:17:15 +0900

    test: refactor GetBonusNumberMethodTest (src/main/java/lotto)
    Set display name to clarify cases

--- src/test/java/lotto/GetBonusNumberMethodTest.java
+++ src/test/java/lotto/GetBonusNumberMethodTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import lotto.Application.NumberType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -16,8 +17,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class GetBonusNumberMethodTest extends NsTest {
     ArrayList<Integer> winningNumbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
 
+    @DisplayName("보너스 번호는 numberType 변수에 NumberType.BONUS 값이 할당된다(1).")
     @Test
-    void getBonusNumber_메소드_테스트1() {
+    void getBonusNumberMethodTest1() {
         initializeNumberType();
         new Lotto(winningNumbers);
 
@@ -27,8 +29,9 @@ class GetBonusNumberMethodTest extends NsTest {
         });
     }
 
+    @DisplayName("보너스 번호는 numberType 변수에 NumberType.BONUS 값이 할당된다(2).")
     @Test
-    void getBonusNumber_메소드_테스트2() {
+    void getBonusNumberMethodTest2() {
         initializeNumberType();
         new Lotto(winningNumbers);
 
@@ -38,8 +41,9 @@ class GetBonusNumberMethodTest extends NsTest {
         });
     }
 
+    @DisplayName("보너스 번호엔 숫자 외 입력이 불가하다(1).")
     @Test
-    void getBonusNumber_메소드_예외_테스트1() {
+    void getBonusNumberMethodExceptionTest1() {
             assertThatThrownBy(() -> {
                 assertSimpleTest(() -> run("*"));
             })
@@ -47,8 +51,19 @@ class GetBonusNumberMethodTest extends NsTest {
                     .hasMessageContaining("[ERROR] 숫자 외 다른 문자는 입력이 불가합니다.");
     }
 
+    @DisplayName("보너스 번호엔 숫자 외 입력이 불가하다(2).")
     @Test
-    void getBonusNumber_메소드_예외_테스트2() {
+    void getBonusNumberMethodExceptionTest2() {
+        assertThatThrownBy(() -> {
+            assertSimpleTest(() -> run("neppiness"));
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 숫자 외 다른 문자는 입력이 불가합니다.");
+    }
+
+    @DisplayName("보너스 번호엔 9자리 이상 숫자가 입력이 불가하다.")
+    @Test
+    void getBonusNumberMethodExceptionTest3() {
         assertThatThrownBy(() -> {
             assertSimpleTest(() -> run("1234567890"));
         })
@@ -56,8 +71,9 @@ class GetBonusNumberMethodTest extends NsTest {
                 .hasMessageContaining("[ERROR] 번호는 1이상 45이하의 자연수로 구성해야 합니다.");
     }
 
+    @DisplayName("보너스 번호엔 45를 초과하는 자연수를 입력할 수 없다.")
     @Test
-    void getBonusNumber_메소드_예외_테스트3() {
+    void getBonusNumberMethodExceptionTest4() {
         assertThatThrownBy(() -> {
             assertSimpleTest(() -> run("46"));
         })
@@ -65,8 +81,9 @@ class GetBonusNumberMethodTest extends NsTest {
                 .hasMessageContaining("[ERROR] 번호는 1이상 45이하의 자연수로 구성해야 합니다.");
     }
 
+    @DisplayName("보너스 번호엔 0을 입력할 수 없다.")
     @Test
-    void getBonusNumber_메소드_예외_테스트4() {
+    void getBonusNumberMethodExceptionTest5() {
         assertThatThrownBy(() -> {
             assertSimpleTest(() -> run("0"));
         })
@@ -74,8 +91,9 @@ class GetBonusNumberMethodTest extends NsTest {
                 .hasMessageContaining("[ERROR] 번호는 1이상 45이하의 자연수로 구성해야 합니다.");
     }
 
+    @DisplayName("보너스 번호엔 당첨 번호와 동일한 번호를 입력할 수 없다.")
     @Test
-    void getBonusNumber_메소드_예외_테스트5() {
+    void getBonusNumberMethodExceptionTest6() {
         initializeNumberType();
         new Lotto(winningNumbers);
 
@@ -84,15 +102,6 @@ class GetBonusNumberMethodTest extends NsTest {
         })
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 당첨 번호가 아닌 다른 번호를 입력하십시오.");
-    }
-
-    @Test
-    void getBonusNumber_메소드_예외_테스트6() {
-        assertThatThrownBy(() -> {
-            assertSimpleTest(() -> run("neppiness"));
-        })
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 숫자 외 다른 문자는 입력이 불가합니다.");
     }
 
     @Override
