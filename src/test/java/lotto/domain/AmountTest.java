4ad5121a868ec178434f50229fb194e0322c2ce9
saint6839 <saint6839@gmail.com>
2022-11-15 11:33:48 +0900

    test(domain) : 입력 금액이 0원일 경우 예외를 던지는 테스트 작성

--- src/test/java/lotto/domain/AmountTest.java
+++ src/test/java/lotto/domain/AmountTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AmountTest {
-    @DisplayName("구매 금액이 1000으로 나누어 떨어지지 않으면 예외를 던지는지 테스트한다")
+    @DisplayName("구매 금액이 1000으로 나누어 떨어지지 않으면 예외를 던지는지 테스트한다.")
     @Test
     void validateCanDivideIntoOneThousand() {
         //given
@@ -19,7 +19,19 @@ class AmountTest {
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("구매금액을 로또 금액으로 나누었을때 로또 수량이 잘 반환되는지 테스트")
+    @DisplayName("구매 금액이 0원일 경우 예외를 던지는지 테스트한다.")
+    @Test
+    void validateAmountZero() {
+        //given
+        int value = 0;
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            new Amount(value);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구매금액을 로또 금액으로 나누었을때 로또 수량이 잘 반환되는지 테스트한다.")
     @Test
     void calculateLottoCount() {
         //given
