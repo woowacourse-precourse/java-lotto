c38be28307235748f100472ba36ad8e7adfee8d3
meunrud <y3a0522@gmail.com>
2022-11-15 18:45:52 +0900

    :art: 로또 당첨금 수정

--- src/main/java/lotto/MatchLotto.java
+++ src/main/java/lotto/MatchLotto.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public enum MatchLotto {
 
-    FIFTH(3,false,"3개 일치 (5,000원) - ",5000),
-    FOURTH(4,false,"4개 일치 (50,000원) - ",50000),
-    THIRD(5,false,"5개 일치 (1500,000원) - ",1500000),
-    SECOND(5,true,"5개 일치, 보너스 볼 일치 (30,000,000원) - ",30000000),
-    FIRST(6,false,"6개 일치 (2,000,000,000원) - ",2000000000);
+    FIFTH(4,false,"3개 일치 (5,000원) - ",5000),
+    FOURTH(3,false,"4개 일치 (50,000원) - ",50000),
+    THIRD(2,false,"5개 일치 (1,500,000원) - ",1500000),
+    SECOND(1,true,"5개 일치, 보너스 볼 일치 (30,000,000원) - ",30000000),
+    FIRST(0,false,"6개 일치 (2,000,000,000원) - ",2000000000);
 
     int matchNumber;
     boolean matchBonums;
