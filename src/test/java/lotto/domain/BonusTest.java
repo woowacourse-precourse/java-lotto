edec2e6d3ec08fb1b1b585af60dee6ed05615783
SeokHwan An <asok369258@naver.com>
2022-11-15 21:34:17 +0900

    test : 에러 메시지에 [ERROR] 포함되었는지 확인하는 절차 추가

--- src/test/java/lotto/domain/BonusTest.java
+++ src/test/java/lotto/domain/BonusTest.java
@@ -8,24 +8,29 @@ import org.junit.jupiter.api.Test;
 
 class BonusTest {
 
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     @DisplayName("보너스 번호가 45를 넘어가면 에러가 발생한다.")
     @Test
     void createBonusOver45() {
         assertThatThrownBy(() -> new Bonus(List.of(1, 2, 3, 4, 5, 6), 46))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @DisplayName("보너스 번호가 1보다 작으면 에러가 발생한다.")
     @Test
     void createBonusUnder1() {
         assertThatThrownBy(() -> new Bonus(List.of(1, 2, 3, 4, 5, 6), 0))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @DisplayName("보너스 번호가 당첨 번호에 포함되면 에러가 발생한다.")
     @Test
     void createBonusConsistedOfLotto() {
         assertThatThrownBy(() -> new Bonus(List.of(1, 2, 3, 4, 5, 6), 5))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_MESSAGE);
     }
 }
