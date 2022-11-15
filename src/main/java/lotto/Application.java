712418639b3e150fe89cdeda53a25425d0028a58
jay <jaewhan232@naver.com>
2022-11-15 10:27:00 +0900

    Implement : 필요한 객체들을 생성하여 controller로 주입 및 main 메소드에서 lottoController 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,29 @@
 package lotto;
 
+import lotto.domain.LottoMachine;
+import lotto.domain.Person;
+import lotto.domain.WinningLotto;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Application app = new Application();
+        LottoController lottoController = app.lottoController();
+    }
+
+    public LottoMachine lottoMachine() {
+        return new LottoMachine();
+    }
+
+    public Person person() {
+        return new Person();
+    }
+
+    public WinningLotto winningLotto() {
+        return WinningLotto.getInstance();
+    }
+
+    public LottoController lottoController() {
+        return new LottoController(lottoMachine(), person(), winningLotto());
     }
 }
