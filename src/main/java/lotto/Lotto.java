5adcb0707743ba30fe042925c034ec680e062045
YusunPark <solarpy.kor@gmail.com>
2022-11-15 23:32:29 +0900

    feat(Lotto.java): add enum variable Rank

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,12 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -20,7 +22,10 @@ public class Lotto {
 
     // TODO: 추가 기능 구현
     private static List<Integer> createLottoNum() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+        System.out.println(numbers);
+        return numbers;
     }
 
     static List<Lotto> createLottos(int cnt) {
@@ -30,5 +35,35 @@ public class Lotto {
             lottos.add(lotto);
         }
         return lottos;
+    }
+
+    public enum Rank {
+        FIRST(6, false, 2000000000),
+        SECOND(5, true, 30000000),
+        THIRD(5, false, 1500000),
+        FOURTH(4, false, 50000),
+        FIFTH(3, false, 5000);
+
+        private final int sameCount;
+        private final boolean isBonus;
+        private final int prize;
+
+        Rank(int sameCount, boolean isBonus, int prize) {
+            this.sameCount = sameCount;
+            this.isBonus = isBonus;
+            this.prize = prize;
+        }
+
+        public int getSameCount() {
+            return sameCount;
+        }
+
+        public boolean getIsBonus() {
+            return isBonus;
+        }
+
+        public long getPrize() {
+            return prize;
+        }
     }
 }
