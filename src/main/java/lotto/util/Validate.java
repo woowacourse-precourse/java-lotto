1e22a3001a32d339a81aab2e5572b34d0aa958e1
thdwoqor <celebarte01@gmail.com>
2022-11-15 13:38:49 +0900

    feat: 중복값 확인 메소드 추가

--- src/main/java/lotto/util/Validate.java
+++ src/main/java/lotto/util/Validate.java
@@ -1,10 +1,13 @@
 package lotto.util;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validate {
     public static final String ERROR_IS_NULL = "[ERROR] NULL 값은 허용하지 않습니다.";
     public static final String ERROR_INVALID_NUMBER = "[ERROR] 숫자이외의 값이 포함되어 있습니다.";
+    public static final String ERROR_DUPLICATE_VALUE = "[ERROR] 중복 값이 포함되어 있습니다.";
 
     public static void isNull(Object number) {
         if (number == null) {
@@ -20,5 +23,12 @@ public class Validate {
 
     public static void isNumbers(List<String> numbers) {
         numbers.stream().forEach(e -> isNumber(e));
+    }
+
+    public static void duplication(List<Integer> numbers) {
+        Set<Integer> sameNumber = new HashSet<>(numbers);
+        if (numbers.size() != sameNumber.size()) {
+            throw new IllegalArgumentException(ERROR_DUPLICATE_VALUE);
+        }
     }
 }
