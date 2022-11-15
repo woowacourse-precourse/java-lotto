a2d18793824f3bca27d341dfbf72ad300d2d2419
Your Name <dbsdbxkrzz@naver.com>
2022-11-15 15:38:22 +0900

    feat: Manager 클래스 showWinningStatus 메서드 추가
    구매한 로또번호들과 당첨 번호를 비교하여 맞힌 갯수에 맞는 등수를 winningInfo에 저장하여 출력하는 메서드를 작성합니다

--- src/main/java/lotto/Manager.java
+++ src/main/java/lotto/Manager.java
@@ -7,10 +7,12 @@ public class Manager {
 
 	public static LottoMachine lottoMachine;
 	public static Lotto lotto;
+	public static ArrayList<Integer> winningInfo;
 
 	public void init(){
 		showGeneratedLottos(buyLottos(userInput()));
 		inputWinningNumbers(userInput());
+		showWinningStatus(lotto.getNumbers(), inputBonusNumber(userInput()), lottoMachine.getLottos());
 	}
 
 	public static int buyLottos(String userInputMoney){
@@ -53,15 +55,44 @@ public class Manager {
 		return numbers;
 	}
 
-	public String inputBonusNumber(String bonusNumber) {
+	public int inputBonusNumber(String bonusNumber) {
 		printBonusNumberInput();
 		Validate.checkBonusNumber(bonusNumber, lotto.getNumbers());
-		return bonusNumber;
+		return Integer.parseInt(bonusNumber);
 	}
 
 	public static void printBonusNumberInput(){
 		System.out.println(Message.INPUT_BONUS_NUMBER.getMessage());
 	}
 
+	public void showWinningStatus(List<Integer> winningNumbers, int bonusNumber, List<List<Integer>> lottos){
+		winningInfo = new ArrayList<>(List.of(0, 0, 0, 0, 0, 0));
+		for (List<Integer> lotto : lottos){
+			checkResult(lotto, winningNumbers, bonusNumber);
+		}
+		Message.printWinningStatus(winningInfo);
+	}
 
+	public void checkResult(List<Integer> lotto, List<Integer> winningNumbers, int bonusNumber){
+		int count = 0;
+		int bonusCount = 0;
+		for (int number: lotto){
+			if (winningNumbers.contains(number)) count++;
+			if (number == bonusNumber) bonusCount++;
+		}
+		updateWinningInfo(checkRank(count, bonusCount));
+	}
+
+	public int checkRank(int count, int bonusCount){
+		if (count < 3) return 5;
+		if (count == 3) return 4;
+		if (count == 4) return 3;
+		if (count == 5 && bonusCount == 0) return 2;
+		if (count == 5 && bonusCount == 1) return 1;
+		return 0;
+	}
+
+	public void updateWinningInfo(int rank){
+		winningInfo.set(rank, winningInfo.get(rank) + 1);
+	}
 }
