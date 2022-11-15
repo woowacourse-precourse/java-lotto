8430279fc93ae7b54b69164e527202c22e7cbbca
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-15 22:47:20 +0900

    feat: UserInputNumbersService 클래스의 필요한 객체 생성

---
+++ src/main/java/lotto/service/UserInputNumbersService.java
@@ -0,0 +1,12 @@
+package lotto.service;
+
+import lotto.domain.User;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class UserInputNumbersService {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+
+}
