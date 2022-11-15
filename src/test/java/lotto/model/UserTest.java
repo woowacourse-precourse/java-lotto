738e6da1eccb3a8f7bcf72ce5166f541bda7ba56
kite707 <kite707707@gmail.com>
2022-11-15 16:37:14 +0900

    test(user): 숫자 6개 입력 시 예외처리 test code작성

--- src/test/java/lotto/model/UserTest.java
+++ src/test/java/lotto/model/UserTest.java
@@ -1,17 +1,43 @@
 package lotto.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.io.ByteArrayInputStream;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
+    void systemIn(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+
     User user = new User();
+
     @Test
-    void getNumbers() {
-        user.getNumbers();
+    void 금액입력시_올바른_횟수_출력() {
+        systemIn("1000");
+        user.getBudget();
+        assertThat(user.getPublicans()).isEqualTo(1);
     }
 
     @Test
-    void getPublicans() {
+    void 숫자6개_입력시_올바른_validation() {
+        systemIn("1,2,3,4");
+        assertThatThrownBy(() -> user.getNumbers()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 6개의 숫자를 입력해주세요!");
+
+        systemIn("1,2,3,4,k,7");
+        assertThatThrownBy(() -> user.getNumbers()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 숫자를 입력해주세요!");
+
+        systemIn("1,2,3,4,66,67");
+        assertThatThrownBy(() -> user.getNumbers()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 1~45사이의 숫자를 입력해주세요!");
+
+        systemIn("1,2,3,4,4,4");
+        assertThatThrownBy(() -> user.getNumbers()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 서로 다른 숫자를 입력해주세요!");
+
     }
+
 }
