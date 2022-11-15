fcac7a6b70ef28361f18a29e1aa547962edc4ccb
wnsgml7267 <dlwns7267@naver.com>
2022-11-15 22:55:32 +0900

    feat(domain/Lotto): 로또 클래스 생성

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private static final int LOTTO_NUMBER = 6;
+    private static final int LOTTO_MIN = 1;
+    private static final int LOTTO_MAX = 45;
+
+    private final List<Integer> numbers;
+
+
+
+
+}
