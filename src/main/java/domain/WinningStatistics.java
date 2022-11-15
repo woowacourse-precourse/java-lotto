4829cae1097eab728725fe9f294a10a39e9c8b28
doehy <dbehxks02@naver.com>
2022-11-15 11:49:16 +0900

    feat : 당첨 통계를 내기 위해 만든 비교 데이터

--- src/main/java/domain/WinningStatistics.java
+++ src/main/java/domain/WinningStatistics.java
@@ -1,15 +1,29 @@
 package domain;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class WinningStatistics {
     private final List<List<Integer>> randomNumbersContainer;
     private final List<Integer> winningNumber;
     private final int bonus;
+    private static final Map<Integer, Integer> comparisonTarget;
+    static {
+        comparisonTarget = new HashMap<>();
+        comparisonTarget.put(3000,0);
+        comparisonTarget.put(4000,0);
+        comparisonTarget.put(5000,0);
+        comparisonTarget.put(5100,0);
+        comparisonTarget.put(6000,0);
+    }
 
     public WinningStatistics(List<List<Integer>> randomNumbersContainer, List<Integer> winningNumber, int bonus) {
         this.randomNumbersContainer = randomNumbersContainer;
         this.winningNumber = winningNumber;
         this.bonus = bonus;
     }
+    
+
+
 }
