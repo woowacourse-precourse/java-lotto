5586df290c02a07e5158f11b78647f1f1445d75b
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-11-15 23:55:46 +0900

    fix: 기존 validate함수들 Validator 클래스로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -31,7 +31,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
         String input = readInput();
-        final int MONEY = validateMoneyInput(input);
+        final int MONEY = Validator.validateMoneyInput(input);
         if (MONEY == 0) {
             return;
         }
@@ -42,11 +42,11 @@ public class Application {
 
         System.out.println("당첨 번호를 입력해 주세요.");
         input = readInput();
-        List<Integer> pickedNumbers = validatePickedNumbers(input);
+        List<Integer> pickedNumbers = Validator.validatePickedNumbers(input);
 
         System.out.println("보너스 번호를 입력해 주세요.");
         input = readInput();
-        int bonusNum = validateBonusNum(input, pickedNumbers);
+        int bonusNum = Validator.validateBonusNum(input, pickedNumbers);
 
         LottoRegister lottoRegister = new LottoRegister();
         lottoRegister.calculateTotalGain(boughtLottos, pickedNumbers, List.of(bonusNum));
@@ -56,45 +56,6 @@ public class Application {
         System.out.println("%입니다.");
 
         return;
-    }
-
-    public static int validateBonusNum(String input, List pickedNumbers) {
-        int bonusNum = parseInt(input);
-
-        if (pickedNumbers.contains(bonusNum)) {
-            throw new IllegalArgumentException("[ERROR]");
-        }
-        if (bonusNum < 1 || bonusNum > 45) {
-            throw new IllegalArgumentException("[ERROR]");
-        }
-
-        return bonusNum;
-    }
-
-    public static List<Integer> validatePickedNumbers(String input) {
-        String[] numbers = input.split(",");
-        List<Integer> pickedNumbers = new ArrayList<Integer>();
-        if (numbers.length != 6) {
-            throw new IllegalArgumentException("[ERROR]");
-        }
-        pickedNumbers = checkExceptionCondition(numbers, pickedNumbers);
-
-        return pickedNumbers;
-    }
-
-    public static List<Integer> checkExceptionCondition(String[] numbers, List<Integer> pickedNumbers) {
-        for (int i = 0; i < numbers.length; i++) {
-            int number = parseInt(numbers[i]);
-
-            if (pickedNumbers.contains(number)) {
-                throw new IllegalArgumentException("[ERROR]");
-            }
-            if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("[ERROR]");
-            }
-            pickedNumbers.add(number);
-        }
-        return pickedNumbers;
     }
 
     public static void printLottoList(List lottos) {
@@ -149,27 +110,6 @@ public class Application {
         });
 
         return randomNumbers;
-    }
-
-    public static int validateMoneyInput(String input) {
-        int money = parseInt(input);
-        boolean isUnitOf1K = ((money % 1000) == 0);
-
-        if (isUnitOf1K == false) {
-            throw new IllegalArgumentException("[ERROR]");
-        }
-
-        return money;
-    }
-
-    public static int parseInt(String input) {
-        try {
-            int money = Integer.parseInt(input);
-            return money;
-        } catch (NumberFormatException e) {
-            System.out.println(new IllegalArgumentException("[ERROR]"));
-            return 0;
-        }
     }
 
     public static String readInput() {
