53588e6910ba0fba8b79fba75476a8c5edb1913a
jisoolee11 <jisu6414@likelion.org>
2022-11-15 22:21:58 +0900

    feat: 보너스 번호

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,12 +11,22 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        bonusNumber = BonusNumber();
         System.out.println(numbers);
+        System.out.println(bonusNumber);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public Integer BonusNumber() {
+        bonusNumber = Randoms.pickNumberInRange(1, 45);
+        while (numbers.contains(bonusNumber)) {
+            bonusNumber = Randoms.pickNumberInRange(1, 45);
+        }
+        return bonusNumber;
     }
 }
