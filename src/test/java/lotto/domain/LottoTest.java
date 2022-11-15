7b2569fe4b4a95d1332f3320fece2c3e515ad90a
kimJaeHyeon <kmr2644@gmail.com>
2022-11-15 20:01:09 +0900

    test: 불필요한 테스트 삭제

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -7,31 +7,15 @@ import static lotto.domain.Prize.NOTING;
 import static lotto.domain.Prize.SIX;
 import static lotto.domain.Prize.THREE;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoTest {
-//    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
-//    @Test
-//    void createLottoByOverSize() {
-//        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-//                .isInstanceOf(IllegalArgumentException.class);
-//    }
-
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 
     @DisplayName("당첨번호 일치갯수에 해당하는 Prize 가 생성되어야한다.")
     @ParameterizedTest(name = "{index} => {0} 당첨번호 {1} 보너스 번호 일때 {2} 생성")
