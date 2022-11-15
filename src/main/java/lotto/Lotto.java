8d03f9f066c3ceeb25c596797839228a2afebbea
rabbit0216 <rabbit1999k@gmail.com>
2022-11-15 14:24:04 +0900

    feat: 로또 정렬 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,12 +2,14 @@ package lotto;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        numbers.stream().sorted().collect(Collectors.toList());
         this.numbers = numbers;
     }
 
