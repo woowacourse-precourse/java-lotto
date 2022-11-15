f3b27c220b77e2f182bc6282643093a749e334ed
aran <aran0317@naver.com>
2022-11-15 18:41:43 +0900

    feat : InputMoney() 구현
    InputMoney(), InputException(), Error 처리 구현

---
+++ src/main/java/config/Error.java
@@ -0,0 +1,7 @@
+package config;
+
+public final class Error {
+    public static final String ERROR_MESSAGE = "[ERROR]";
+    public static final String ERROR_INTEGER = " 정수를 입력해야 합니다.";
+    public static final String ERROR_INPUT_MONEY = " 1000원으로 나누어져야합니다.";
+}
--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,35 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static config.Error.*;
+
 public class Application {
+
+    static String InputMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        return input;
+    }
+
+    static Integer InputMoneyException(String input) throws IllegalArgumentException{
+        int money;
+        try{
+            money = Integer.parseInt(input);
+            if(money % 1000 == 0 ){
+                return money % 1000;
+            }
+        }catch(Exception e){
+            System.out.println(ERROR_MESSAGE+ERROR_INTEGER);
+            throw new IllegalArgumentException(ERROR_MESSAGE+ERROR_INTEGER);
+        }
+        System.out.println(ERROR_MESSAGE+ERROR_INPUT_MONEY);
+        throw new IllegalArgumentException(ERROR_MESSAGE+ERROR_INPUT_MONEY);
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = InputMoney();
+        int chance = InputMoneyException(input);
+
     }
 }
