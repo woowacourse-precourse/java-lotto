6d956a53463840ed702569fd88397edf3c6ab619
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-15 23:38:22 +0900

    Test : add LottoNumber input Test

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -41,6 +41,21 @@ class LottoTest {
         assertThatThrownBy(() -> Exeption.inputMoneyExeption(test))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+    @DisplayName("로또 번호에 숫자가 아닌 문자가 들어갔을 때")
+    @Test
+    void inputString() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        String test = "12,3j,p,3,15,34";
+        assertThatThrownBy(() -> Exeption.inputWinningNumberExeptionDetail(Arrays.asList(test.split(","))))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("보너스 숫자가 중복되었을 때")
+    @Test
+    void inputBonusNumberOverlapped() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> Exeption.isNotoverlapped(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 3))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
 
 }
