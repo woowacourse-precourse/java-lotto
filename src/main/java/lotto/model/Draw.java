8594c859cdcb8c994c013a1ff0de8fa7835243fc
beombu <sunil1369@bible.ac.kr>
2022-11-15 16:08:13 +0900

    Refactor(Draw) : 추첨 기능 메소드 분리

--- src/main/java/lotto/model/Draw.java
+++ src/main/java/lotto/model/Draw.java
@@ -7,9 +7,9 @@ import java.util.Map;
 public class Draw {
     Map<Rank, Integer> result = new HashMap<>();
 
-    public Draw(){
-        for(Rank rank : Rank.values()){
-            result.put(rank,0);
+    public Draw() {
+        for (Rank rank : Rank.values()) {
+            result.put(rank, 0);
         }
     }
 
@@ -17,33 +17,41 @@ public class Draw {
         return result;
     }
 
-    public void drawLotto(List<Lotto> lottos, List<Integer> priceNumbers, int bonusNumber){
-        for(int i = 0; i < lottos.size(); i++){
-            int matchNumberCount = 0;
+    public void drawLotto(List<Lotto> lottos, List<Integer> priceNumbers, int bonusNumber) {
+        for (Lotto lotto : lottos) {
+            int matchNumberAmount = 0;
             int checkBonus = 0;
 
-            for(int j = 0; j < priceNumbers.size(); j++){
-                if(lottos.get(i).getNumbers().contains(j)){
-                    matchNumberCount++;
-                }
-            }
-
-            if(lottos.get(i).getNumbers().contains(bonusNumber)){
-                checkBonus = 1;
-            }
-
-            Rank rank = Rank.findByMatchNumber(matchNumberCount, checkBonus);
+            matchNumberAmount = getMatchNumberAmount(priceNumbers, lotto, matchNumberAmount);
+            checkBonus = getCheckBonus(bonusNumber, lotto, checkBonus);
+            Rank rank = Rank.findByMatchNumber(matchNumberAmount, checkBonus);
             result.put(rank, result.getOrDefault(rank, 0) + 1);
         }
     }
 
-    public float calculateRate(int money){
+    private int getMatchNumberAmount(List<Integer> priceNumbers, Lotto lotto, int matchNumberAmount) {
+        for (int num : priceNumbers) {
+            if (lotto.getNumbers().contains(num)) {
+                matchNumberAmount++;
+            }
+        }
+        return matchNumberAmount;
+    }
+
+    private int getCheckBonus(int bonusNumber, Lotto lotto, int checkBonus) {
+        if (lotto.getNumbers().contains(bonusNumber)) {
+            checkBonus = 1;
+        }
+        return checkBonus;
+    }
+
+    public float calculateRate(int money) {
         return calculateTotalProfit() / (float) money * 100L;
     }
 
-    public float calculateTotalProfit(){
+    public float calculateTotalProfit() {
         float sum = 0;
-        for(Map.Entry<Rank, Integer> entry : result.entrySet()){
+        for (Map.Entry<Rank, Integer> entry : result.entrySet()) {
             sum += Rank.valueOf(entry.getKey().toString()).getPriceMoney() * entry.getValue();
         }
         return sum;
