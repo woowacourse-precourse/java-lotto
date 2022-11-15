06b173060c97761ea782eb5b3bfb817fe73e05ef
kite707 <kite707707@gmail.com>
2022-11-15 18:12:44 +0900

    test(lottoList): lottoList 출력 testcode 작성

--- src/test/java/lotto/controller/LottoControllerTest.java
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -1,4 +1,14 @@
+package lotto.controller;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class LottoControllerTest {
-  
+
+    @Test
+    void printLottoList() {
+        LottoController lottoController = new LottoController(6);
+        lottoController.printLottoList();
+    }
 }
