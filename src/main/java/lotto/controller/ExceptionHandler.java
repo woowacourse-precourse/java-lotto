b20acaf82f153ab958febddb5fcab3bf9ac2c9f2
mng990 <ryoo0504@pusan.ac.kr>
2022-11-15 22:59:01 +0900

    refactor(main/java/lotto/controller): ExceptionHandler.java 에러 안내문 static을 상수로 전환

--- src/main/java/lotto/controller/ExceptionHandler.java
+++ src/main/java/lotto/controller/ExceptionHandler.java
@@ -6,16 +6,26 @@ import lotto.view.ExceptionHandlerUI;
 public class ExceptionHandler {
 
 
+    private static final String excMessageAboutRange = "1에서 45 사이의 수만 입력 가능합니다.";
+    private static final String excMessageAboutLenght = "6자리 수여야 합니다.";
+
+    private static final String excMessageAboutDupplication = "중복된 값은 입력할 수 없습니다.";
+    private static final String excMessageAboutWinningForm =  "쉼표로 구분된 6개의 수만 입력 가능합니다.";
+
+    private static final String excMessageAboutPurchaseForm = "1000원 단위로만 구입이 가능합니다.";
+
+    private static final String excMessageAboutInt = "정수 이외의 값은 입력할 수 없습니다.";
+
     private static ExceptionHandlerUI view = new ExceptionHandlerUI();
     public static void validNumberRange(int num){
         if(num < 1 || num > 45){
-            throw new IllegalArgumentException("1에서 45 사이의 수만 입력 가능합니다.");
+            throw new IllegalArgumentException(excMessageAboutRange);
         }
     }
 
     public static void validNumbersLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("6자리 수여야 합니다.");
+            throw new IllegalArgumentException(excMessageAboutLenght);
         }
     }
 
@@ -24,27 +34,27 @@ public class ExceptionHandler {
         validNumbersDupplication(numbers);
 
         if(numbers.contains(newNumber)){
-            throw new IllegalArgumentException("중복된 값은 입력할 수 없습니다.");
+            throw new IllegalArgumentException(excMessageAboutDupplication);
         } // 보너스 번호 중복 체크
     }
 
     public static void validNumbersDupplication(List<Integer> numbers){
         if(numbers.size() != numbers.stream().distinct().count()){
-            throw new IllegalArgumentException( "중복된 값은 입력할 수 없습니다.");
+            throw new IllegalArgumentException(excMessageAboutDupplication);
         } // 당첨 번호 중복 체크
     }
 
     public static void validWinningForm(String readline){
         String[] numbers = readline.split(",");
         if(numbers.length != 6){
-            throw new IllegalArgumentException( "쉼표로 구분된 6개의 수만 입력 가능합니다.");
+            throw new IllegalArgumentException(excMessageAboutWinningForm);
         }
     }
 
     public static void validPurchaseForm(String readline){
         int amount = excParseInt(readline);
         if(amount%1000 != 0){
-            throw new IllegalArgumentException("1000원 단위로만 구입이 가능합니다.");
+            throw new IllegalArgumentException(excMessageAboutPurchaseForm);
         }
     }
 
@@ -53,7 +63,7 @@ public class ExceptionHandler {
             return Integer.parseInt(input);
         }
         catch (NumberFormatException e) {
-            throw new IllegalArgumentException( "정수 이외의 값은 입력할 수 없습니다.");
+            throw new IllegalArgumentException( excMessageAboutInt);
         }
     }
 
