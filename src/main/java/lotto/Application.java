f24873cdbd09ac27cb0261b39c6ec14d74f35c2b
desfox <desfox99@naver.com>
2022-11-15 21:57:28 +0900

    refactor(Application): Exception 발생 시 던지지 않고 에러 문구 출력한 뒤 종료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,11 @@ import lotto.controller.LottoController;
 public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
-        lottoController.playLottoGame();
+
+        try {
+            lottoController.playLottoGame();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
