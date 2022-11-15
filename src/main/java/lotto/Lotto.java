382df1196dfe02df98675b4e6513a4329bce3da1
Haechan Lee <dlgockssla@gmail.com>
2022-11-15 11:25:33 +0900

    feat: validate 로또

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,8 +18,14 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 생성을 위해서는 6개의 숫자가 필요합니다.");
         }
-        // 범위 벗어날 때
-        // 중복된 숫자 있을 때.
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 1이상 45이하 입니다.");
+            }
+        }
+        if (new HashSet<>(numbers).size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또에 중복된 숫자가 존재합니다.");
+        }
     }
 
     // TODO: 추가 기능 구현
