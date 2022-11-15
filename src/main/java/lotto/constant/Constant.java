abd16f076645e162109b609406895743d455dd1e
leesangjin <tkdwls0628@gmail.com>
2022-11-15 19:19:26 +0900

    feat(Constant): 로또 금액 상수 추가

--- src/main/java/lotto/constant/Constant.java
+++ src/main/java/lotto/constant/Constant.java
@@ -4,4 +4,5 @@ public class Constant {
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
     public static final int LOTTO_SIZE = 6;
+    public static final int LOTTO_PRICE = 1000;
 }
