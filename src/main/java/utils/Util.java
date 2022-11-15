0a2137651ccfd690b55ed3947816ef9a64fc262b
juno-junho <ssmm0205@naver.com>
2022-11-15 22:58:45 +0900

    feat(utils): add method in Util
    Util 클래스에 수익률을 반환하는 메서드 생성

--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +17,15 @@ public class Util {
         return Arrays.stream(textNumbers.split(REGEX))
                 .map(Util::convertStringToInt)
                 .collect(Collectors.toList());
+    }
+
+    public static float getProfit(float nowMoney, float pastMoney) {
+        return nowMoney / pastMoney;
+    }
+
+    public static String convertIntegerToMoneyFormat(int money) {
+        DecimalFormat dc = new DecimalFormat("###,###,###,###");
+        return dc.format(money);
     }
 
 }
