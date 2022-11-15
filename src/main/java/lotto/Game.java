11c133197fa29afcbbfb957f2b84f38f2414e255
sxxgxxnz <tmddms2292@gmail.com>
2022-11-15 12:07:11 +0900

    refactor : 게임 순서 리팩토링

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -18,28 +18,41 @@ public class Game {
     public static int sixLuckyCount = 0;
     public static int amount = 0;
     public static int cashAmount = 0;
+    public static int bonus = 0;
+    public static double plusPercent = 0.0;
 
     public static void gameStart() {
+        inputPrice();
+        inputLuckyNumber();
+        inputBonusNumber();
+
+        compareLucky(luckyNumber,bonus);
+
+        printResult();
+    }
+    public static void printResult(){
+        Print.viewTotalLucky(threeLuckyCount,fourLuckyCount,fiveLuckyCount,bonusLuckyCount,sixLuckyCount);
+        amountPlus(threeLuckyCount,fourLuckyCount,fiveLuckyCount,bonusLuckyCount,sixLuckyCount);
+        Print.viewPlusPercent(plusPercent);
+    }
+    public static void inputBonusNumber(){
+        Print.viewInputBonusNubmer();
+        bonus = Lotto.getBonusNumber();
+    }
+    public static void inputLuckyNumber(){
+        Print.viewInputLuckyNumber();
+        luckyNumber = Lotto.getLuckyNumber();
+    }
+    public static void inputPrice(){
         Print.viewGameStart();
         String cash = Input.input();
         Validator.validateCash(cash);
         Lotto.myLotto(cash);
         cashAmount = Integer.parseInt(cash);
-        Print.viewInputLuckyNumber();
-        luckyNumber = Lotto.getLuckyNumber();
-        Print.viewInputBonusNubmer();
-        int bonus = Lotto.getBonusNumber();
-
-        compareLucky(luckyNumber,bonus);
-        //당첨 내역 출력
-        Print.viewTotalLucky(threeLuckyCount,fourLuckyCount,fiveLuckyCount,bonusLuckyCount,sixLuckyCount);
-        // 수익률 계산
-        amountPlus(threeLuckyCount,fourLuckyCount,fiveLuckyCount,bonusLuckyCount,sixLuckyCount);
     }
     public static void amountPlus(int threeLuckyCount,int fourLuckyCount,int fiveLuckyCount,int bonusLuckyCount,int sixLuckyCount){
         amount = threeLuckyCount * 5000 + fourLuckyCount * 50000 + fiveLuckyCount * 1500000 + bonusLuckyCount * 30000000 + sixLuckyCount * 2000000000;
-        double plusPercent = (double) amount/(double)cashAmount * 100.0;
-        Print.viewPlusPercent(plusPercent);
+        plusPercent = (double) amount/(double)cashAmount * 100.0;
     }
     public static void compareLucky(List<Integer> luckyNumber,int bonus){
         for(int i=0;i<autoNumber.size();i++){
