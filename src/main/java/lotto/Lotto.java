1dc166aa3a1844f270100f246bb891e9c4ac5017
sschan99 <seungchan1219@gmail.com>
2022-11-15 22:55:38 +0900

    refactor(Lotto): add exception message

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,7 +16,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (new HashSet<>(numbers).size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 중복되지 않는 6개의 숫자를 입력하시오.");
         }
     }
 
