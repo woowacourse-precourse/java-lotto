745f7a4852ac915307b833fa33f63eb2bfa37cde
kimdonghun <zhfptm12@naver.com>
2022-11-15 13:05:19 +0900

    feat : input view Strings

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,5 +7,11 @@ public class InputView {
     static public void purchaseLotto() {
         System.out.println(Strings.purchaseMessage);
     }
-
+    static public void winnigNumber() {
+        System.out.println(Strings.winningLottoMessage);
+    }
+    static public void bonusNumber() {
+        System.out.println(Strings.winnigBonusMessage);
+        
+    }
 }
