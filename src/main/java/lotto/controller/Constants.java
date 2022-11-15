04a69d5eb305eeeda529c2ba2aa9678b266a43f5
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-15 15:21:31 +0900

    feat(ExceptionHandler) : implement exception handler for win number input case

--- src/main/java/lotto/controller/Constants.java
+++ src/main/java/lotto/controller/Constants.java
@@ -3,6 +3,8 @@ package lotto.controller;
 public class Constants {
 
     public static final String INPUT_CORRECT_FORMAT_MONEY = "[ERROR] 1000원 단위의 알맞은 금액을 넣어주세요.";
+    public static final String NUMBER_RANGE_MUST_BETWEEN_1_45 = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String DUPLICATED_NUMBER_IS_NOT_ALLOWED = "[ERROR] 로또 번호는 중복되지 않아야 합니다.";
     public static final int MONEY_UNIT = 1000;
     public static final int PERCENTAGE = 100;
     public static final int LEAST_WIN_NUMBER = 3;
--- src/main/java/lotto/controller/ExceptionHandler.java
+++ src/main/java/lotto/controller/ExceptionHandler.java
@@ -2,13 +2,33 @@ package lotto.controller;
 
 import static lotto.controller.Constants.*;
 
+import java.util.List;
+
 public class ExceptionHandler {
+    private static final String pattern = "^[0-9]*$";
 
-    protected static void validateMoneyUnit(int money) {
+    public static void validateMoneyUnit(int money) {
 
-        String pattern = "^[0-9]*$";
         if (money % MONEY_UNIT != 0 || pattern.matches(Integer.toString(money))) {
             throw new IllegalArgumentException(INPUT_CORRECT_FORMAT_MONEY);
+        }
+    }
+
+    public static void validateWinNumber(List<Integer> winNumbers) {
+        String pattern = "^[0-9]*$";
+        for(int winNumberElement : winNumbers) {
+            if(pattern.matches(Integer.toString(winNumberElement))) {
+                throw new IllegalArgumentException(NUMBER_RANGE_MUST_BETWEEN_1_45);
+            }
+        }
+        if(winNumbers.stream().distinct().count() != winNumbers.size()){
+            throw new IllegalArgumentException(DUPLICATED_NUMBER_IS_NOT_ALLOWED);
+        }
+    }
+
+    public static void validateBonusNumber(int bonusNumber) {
+        if (pattern.matches(Integer.toString(bonusNumber))) {
+            throw new IllegalArgumentException(NUMBER_RANGE_MUST_BETWEEN_1_45);
         }
     }
 }
