258d967fa67c217c9c65cceb5f1cd2866d05e543
nakalic <nakalic28@gmail.com>
2022-11-15 17:06:19 +0900

    refactor(MoneyValidator): 출력문 수정

--- src/main/java/lotto/Validator/MoneyValidator.java
+++ src/main/java/lotto/Validator/MoneyValidator.java
@@ -13,21 +13,21 @@ public class MoneyValidator {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 자연수만 입력해주세요");
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
         }
     }
 
     public static void isMultiplesOf1000(String input) {
         int num = Integer.parseInt(input);
         if (num % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원으로 나누어떨어져야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000으로 나누어떨어져야 합니다.");
         }
     }
 
     public static void isOverThan1000(String input) {
         int num = Integer.parseInt(input);
         if (num < 1000) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원보다 커야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000보다 커야 합니다.");
         }
     }
 }
