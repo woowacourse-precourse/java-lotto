ff34844fc43faa928d5f6c1ffffa21f2b90a3340
juno-junho <ssmm0205@naver.com>
2022-11-15 20:41:17 +0900

    feat(result): add method in enum Rank
    Rank enum 내에 of, getWithoutDefault, isMatch 메서드 추가

--- src/main/java/lotto/result/Rank.java
+++ src/main/java/lotto/result/Rank.java
@@ -1,6 +1,11 @@
 package lotto.result;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Rank {
+    NONE("0", 0),
     FIFTH("5,000", 3),
     FOURTH("50,000", 4),
     THIRD("1,500,000", 5),
@@ -13,5 +18,25 @@ public enum Rank {
      Rank(String winningMoney, int matchCount) {
         this.winningMoney = winningMoney;
         this.matchCount = matchCount;
+    }
+
+    public static Rank of(int matchCount, boolean isBonus) {
+        if (matchCount == 5 && isBonus) {
+            return SECOND;
+        }
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank.isMatch(matchCount))
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    public static List<Rank> getWithoutDefault() {
+        return Arrays.stream(Rank.values())
+                .filter(rank -> !rank.equals(NONE))
+                .collect(Collectors.toList());
+    }
+
+    private boolean isMatch(int matchCount) {
+        return this.matchCount == matchCount;
     }
 }
