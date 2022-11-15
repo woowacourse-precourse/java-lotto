55331781be6c48534fc2b1e847e1bfc7ef440f52
HCH <clgusdl2@naver.com>
2022-11-15 21:04:18 +0900

    feat : 2등 3등 체크 함수 구현

--- src/main/java/lotto/CheckNumber.java
+++ src/main/java/lotto/CheckNumber.java
@@ -12,5 +12,13 @@ public class CheckNumber {
         if(lottoNum.contains(bonus)) return true;
         return false;
     }
+    static float winCheckNum(Lotto lotto,List<Integer> lottoNum,int bonus){
+        float result;
+        result = lotto.check_rank(lottoNum);
+        if(result == 5){
+            if(checkContainsBonus(lottoNum,bonus)) result = result + 0.5f;
+        }
+        return result;
+    }
 
 }
