05a0a679236393b07cdb1338598a2e6ca2242955
liarreez <liarreez@gmail.com>
2022-11-15 15:13:30 +0900

    feat(java): 로또 구입 금액을 입력 받고 구매 장수를 출력하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        lottoBuy();
+    }
+
+    public static void lottoBuy() {
+        Scanner scanner = new Scanner(System.in);
+        int buyLottoPrice = scanner.nextInt();
+        if(buyLottoPrice % 1000 != 0 ) {
+            throw new IllegalArgumentException("[ERROR]구입 금액은 1,000원 단위로 입력해주세요.");
+        }
+        int lottoCount = buyLottoPrice / 1000;
+        System.out.println(lottoCount+ "개를 구매했습니다.");
     }
 }
