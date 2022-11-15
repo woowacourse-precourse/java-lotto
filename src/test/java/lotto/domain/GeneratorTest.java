7f81db95039b9d3d76fc2dbe8d3fdedd720eac34
taeyun-work <wmfajr1357@gmail.com>
2022-11-15 15:33:27 +0900

    test: Generator.calculateRank test 작성

--- src/test/java/lotto/domain/GeneratorTest.java
+++ src/test/java/lotto/domain/GeneratorTest.java
@@ -8,11 +8,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import lotto.Constants.NUMBER;
+import lotto.Ranking;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import lotto.domain.Generator;
-import lotto.domain.Lotto;
 
 public class GeneratorTest {
     @DisplayName("로또 인스턴스를 인자로 받아 일치하는 당첨번호 및 보너스번호 개수를 HashMap에 담아 반환해준다")
@@ -25,7 +24,7 @@ public class GeneratorTest {
         Lotto.setWinningNumbers(winningNumbers);
         Lotto.setBonusNumber(7);
         Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        HashMap<String, Integer> winningSummary = (HashMap<String, Integer>)method.invoke(Generator.class, lotto);
+        HashMap<String, Integer> winningSummary = (HashMap<String, Integer>) method.invoke(Generator.class, lotto);
 
         HashMap<String, Integer> expectSummary = new HashMap<String, Integer>();
         expectSummary.put("winningCount", 6);
@@ -42,7 +41,7 @@ public class GeneratorTest {
         Lotto.setWinningNumbers(winningNumbers);
         Lotto.setBonusNumber(7);
         Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 7, 10));
-        HashMap<String, Integer> winningSummary = (HashMap<String, Integer>)method.invoke(Generator.class, lotto);
+        HashMap<String, Integer> winningSummary = (HashMap<String, Integer>) method.invoke(Generator.class, lotto);
 
         HashMap<String, Integer> expectSummary = new HashMap<String, Integer>();
         expectSummary.put("winningCount", 4);
@@ -54,6 +53,37 @@ public class GeneratorTest {
     void calculateProfitTest() {
         double result = Generator.calculateProfit(3000, 2_030_000_000);
         assertThat(result).isEqualTo(67666666.7);
+
+    }
+
+    @Test
+    void calculateRankTest() {
+        Buyer buyer = new Buyer();
+        List<Integer> winningNumbers = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto.setWinningNumbers(winningNumbers);
+        Lotto.setBonusNumber(7);
+
+        Lotto lotto1 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto lotto3 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        buyer.setPurchasedLotto(lotto1);
+        buyer.setPurchasedLotto(lotto2);
+        buyer.setPurchasedLotto(lotto3);
+
+        Generator.calculateRank(buyer);
+
+        HashMap<String, Integer> buyerWinningSummary = new HashMap<String, Integer>() {
+            {
+                put(Ranking.FIRST.getLabel(), 3);
+                put(Ranking.SECOND.getLabel(), NUMBER.ZERO);
+                put(Ranking.THIRD.getLabel(), NUMBER.ZERO);
+                put(Ranking.FOURTH.getLabel(), NUMBER.ZERO);
+                put(Ranking.FIFTH.getLabel(), NUMBER.ZERO);
+                put(Ranking.LOSE.getLabel(), NUMBER.ZERO);
+            }
+        };
+
+        assertThat(buyer.getWinningSummary()).isEqualTo(buyerWinningSummary);
 
     }
 }
