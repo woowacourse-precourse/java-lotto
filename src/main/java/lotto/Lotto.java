256d5249b9a198ed3c4916d1956649f122fa1d9d
gichan-song <thdrlcks97@gmail.com>
2022-11-15 20:49:13 +0900

    feat: Lotto.validate 유효한 숫자인지 확인
    사용자에게 입력받은 로또번호가 6자리인지
    중복된 숫자가 있는지
    1~45 사이의 숫자인지 확인하여 예외 발생

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,6 +14,15 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+        if(numbers.stream().distinct().collect(Collectors.toList()
+        ).size() != numbers.size()){
+            throw new IllegalArgumentException();
+        }
+        for(int i=0; i<6; i++){
+            if(numbers.get(i) < 1 || numbers.get(i) > 45){
+                throw new IllegalArgumentException();
+            }
         }
     }
 
