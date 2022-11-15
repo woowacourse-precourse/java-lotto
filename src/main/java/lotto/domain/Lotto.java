daeebede4244f329a634e9a4336d33886cc68285
yuntaeLee <leeyt1201@gmail.com>
2022-11-15 17:04:12 +0900

    feat: 통합 검증 메서드 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,14 +5,27 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static lotto.enumtype.LottoValidationMsg.*;
+
 public class Lotto {
     private static final int FIX_SIZE = 6;
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        validate(numbers);
         Collections.sort(numbers);
         this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (validateNumbersSize(numbers)) {
+            throw new IllegalArgumentException(String.format(OVER_LOTTO_FIX_SIZE_EXCEPTION.getMessage(), FIX_SIZE));
+        }
+
+        if (validateDuplicateNumbers(numbers)) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_EXCEPTION. getMessage());
+        }
     }
 
     private boolean validateNumbersSize(List<Integer> numbers) {
