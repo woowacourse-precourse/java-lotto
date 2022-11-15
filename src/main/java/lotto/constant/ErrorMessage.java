113d4619047446d0ed2a1fd8e1eac146cf1e685f
leesangjin <tkdwls0628@gmail.com>
2022-11-15 17:20:56 +0900

    feat(ErrorMessage): 로또 관련 에러 메세지 추가 정의

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -6,4 +6,9 @@ public class ErrorMessage {
     public static final String MONEY_AMOUNT_WRONG_TYPE = ERROR_MESSAGE + "구입금액은 정수로 입력해야 합니다.";
     public static final String MONEY_AMOUNT_CANNOT_BUY_LOTTO = ERROR_MESSAGE + "구입금액이 1000원 이상이여야 로또를 구매할 수 있습니다.";
     public static final String MONEY_AMOUNT_WRONG_VALUE = ERROR_MESSAGE + "구입금액은 1000원 단위여야 합니다.";
+
+    public static final String WRONG_SIZE_MSG = ERROR_MESSAGE + "로또는 6자리 수로 이루어져 있습니다.";
+    public static final String WRONG_VALUE_MSG = ERROR_MESSAGE + "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String DUPLICATE_MSG = ERROR_MESSAGE + "로또 번호는 중복되지 않는 수로 이루어져야 합니다.";
+
 }
