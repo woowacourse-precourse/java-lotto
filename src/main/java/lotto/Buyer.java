a64934a5da72b0e46f6b77ed3410d673517b77a6
dydckd <toofriend09@naver.com>
2022-11-15 16:38:17 +0900

    feat :로또 당첨 및 수익률 기능 추가 , refact
    - 로또 번호 비교해서 당첨 된거 확인후 출력 수익률 계산 해서 출력하는 기능 추가
    - 이전에 LottoMachine클래스와 buyer클래스를 나누어서 사용하려햇는데 인스턴스필드가 없음으로 나와서
      LottoMachine클래스에 내용을 buyer클래스로 이동시켜서 해결함.

--- src/main/java/lotto/Buyer.java
+++ src/main/java/lotto/Buyer.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,8 +10,20 @@ import java.util.StringTokenizer;
 public class Buyer {
     int purchaseAmount;
     int lottoAmount;
+    String inputWinningNum;
 
     List<Integer> winningNum;
+    int bonusNum;
+    final int firstBallNum = 1;
+    final int LASTBALLNUM = 45;
+    final int COUNT = 6;
+    List<List<Integer>> totalLotto;
+    public static int FIRST ;
+    public static int SECOND ;
+    public static int THIRD ;
+    public static int FOURTH ;
+    public static int FIFTH ;
+    double YIELD;
 
     public Buyer(){
 
@@ -25,8 +38,8 @@ public class Buyer {
             throw new IllegalArgumentException("[ERROR] 입력금액의 단위는 1000입니다.");
         }
     }
-    public int lottoAmount(int purchaseAmount){
-       return this.lottoAmount = purchaseAmount/1000;
+    public void lottoAmount(int purchaseAmount){
+        this.lottoAmount = purchaseAmount / 1000;
     }
     public void lottoAmountMessage(){
         System.out.println(lottoAmount + "개를 구매하셨습니다.");
@@ -42,17 +55,23 @@ public class Buyer {
     public static void winningMessage(){
         System.out.println("당첨 번호를 입력해 주세요.");
     }
-
-    public List<Integer> getWinningNum() {
+    public static void bonusMessage(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+    public String inputWinningNum(){
+        inputWinningNum = Console.readLine();
+        return inputWinningNum;
+    }
+    public List<Integer> getWinningNum(String inputWinningNum) {
         winningNum = new ArrayList<>();
-        String input = Console.readLine();
-        StringTokenizer st = new StringTokenizer(input, ",");
+        StringTokenizer st = new StringTokenizer(inputWinningNum, ",");
         while (st.hasMoreTokens()) {
             winningNum.add(Integer.parseInt(st.nextToken()));
         }
         validate(winningNum);
         validateNum(winningNum);
-        return winningNum;
+        validateWinningNumRange(winningNum);
+        return this.winningNum;
     }
     private void validate(List<Integer> winningNum) {
         if (winningNum.size() != 6) {
@@ -60,10 +79,96 @@ public class Buyer {
         }
     }
 
-    // TODO: 추가 기능 구현
     private void validateNum(List<Integer> winningNum) {
         if(winningNum.size() != winningNum.stream().distinct().count()){
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않습니다.");
+        }
+    }
+    private void validateWinningNumRange(List<Integer> winningNum){
+        for(int i = 0 ; i<winningNum.size() ; i++){
+            if(0> winningNum.get(i) && winningNum.get(i)>45){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45사이의 수입니다.");
+            }
+        }
+    }
+    public int inputBonusNum(){
+        bonusNum = Integer.parseInt(Console.readLine());
+        return this.bonusNum;
+    }
+    public int getBonusNum(int bonusNum){
+        validateRange(bonusNum);
+        if(winningNum.contains(bonusNum)) {
+            throw new IllegalArgumentException("[ERROR] 보너스번호와 로또 번호는 중복되지 않습니다.");
+        } return bonusNum;
+    }
+    private void validateRange(int bonusNum){
+        if(1>bonusNum && bonusNum>45){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45사이의 수입니다.");
+        }
+    }
+    public void checkWinNum (){
+        for(List<Integer> checkNum : totalLotto){
+
+            int win = countWin(checkNum);
+            int bonusWin = 0;
+
+            if(checkNum.contains(bonusNum)){
+                bonusWin++;
+            }
+            countWinningResult(win, bonusWin);
+        }
+    }
+    private int countWin(List<Integer> autoLotto) {
+        int win = 0;
+        for(int winningNumber : winningNum){
+            if(autoLotto.contains(winningNumber)){
+                win++;
+            }
+        }
+        return win;
+    }
+
+    private void countWinningResult(int win, int bonusWin) {
+        if(win==6) FIRST++;
+        if(win==5 && bonusWin==1) SECOND++;
+        if(win==5 && bonusWin==0) THIRD++;
+        if(win==4) FOURTH++;
+        if(win==3) FIFTH++;
+    }
+
+    public void printResult(){
+        System.out.println("통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - "+FIFTH+"개");
+        System.out.println("4개 일치 (50,000원) - "+FOURTH+"개");
+        System.out.println("5개 일치 (1,500,000원원) - "+THIRD+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+SECOND+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+FIRST+"개");
+        System.out.println("총 수익률은 " + String.format("%.1f",YIELD) + "%입니다.");
+
+    }
+    public  void yieldCalculate(int purchaseAmount){
+        long yield = 5000 * FIFTH + 50000 * FOURTH + 1500000 * THIRD + 30000000 * SECOND + 2000000000 * FIRST;
+        YIELD = yield / (double) purchaseAmount * 100;
+    }
+
+
+
+
+
+
+
+    public List<List<Integer>> makeLotto (int lottoAmount){
+        totalLotto = new ArrayList<>();
+        for(int i =0 ; i<lottoAmount ; i++){
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(firstBallNum, LASTBALLNUM, COUNT));
+            totalLotto.add(lotto.getNumbers());
+        }
+        return totalLotto;
+    }
+    public void outPutLottoNum (List<List<Integer>> totalLotto){
+        for(Object lottoNum : totalLotto){
+            System.out.println(lottoNum);
         }
     }
 
