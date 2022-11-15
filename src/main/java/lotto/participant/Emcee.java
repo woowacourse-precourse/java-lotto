3ae7bf9ef7ca11a21262523353806707422b044a
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-15 11:03:24 +0900

    refactor(Emcee): use enum instead of value

--- src/main/java/lotto/participant/Emcee.java
+++ src/main/java/lotto/participant/Emcee.java
@@ -1,5 +1,6 @@
 package lotto.participant;
 
+import lotto.constant.GuideMessage;
 import lotto.domain.Lotto;
 import lotto.constant.Rank;
 
@@ -12,30 +13,19 @@ import static lotto.constant.Rank.*;
 public class Emcee {
 
     public void guideGame() {
-        String startingGuide = "Lotto game 에 오신 것을 환영합니다.\n" +
-                "원하시는 만큼의 lotto 를 구매할 수 있으며 lotto 는 자동 발행됩니다.\n" +
-                "당첨 번호와 보너스 번호를 생성하면 발행된 lotto 들의 당첨 여부를 계산합니다.\n" +
-                "최종적으로 당첨 통계와 수익률을 알려드립니다.\n" +
-                "게임을 시작하겠습니다.";
-        System.out.println(startingGuide);
+        System.out.println(GuideMessage.STARTING_GUIDE);
     }
 
     public void guideAmountOfPurchaseMoney() {
-        String guide = "구입 금액을 입력해주세요.\n" +
-                "구입 금액은 1,000원 단위로 입력이 가능합니다.";
-        System.out.println(guide);
+        System.out.println(GuideMessage.PURCHASE_GUIDE);
     }
 
     public void guideWinningNumber() {
-        System.out.println("당첨 번호를 입력해 주세요.");
-        System.out.println("당첨 번호는 1과 45 사이의 서로 다른 6개의 숫자입니다.");
-        System.out.println("각 번호를 쉼표로 구분해서 입력해주세요.");
+        System.out.println(GuideMessage.WINNING_NUMBERS_GUIDE);
     }
 
     public void guideBonusNumber() {
-        System.out.println("\n보너스 번호를 입력해 주세요.");
-        System.out.println("보너스 번호는 당첨 번호와 중복되지 않는 1부터 45 사이의 숫자입니다.");
-        System.out.println("하나의 보너스 번호만 입력해 주세요.");
+        System.out.println(GuideMessage.BONUS_NUMBER_GUIDE);
     }
 
     public void showPublishedLottos(List<Lotto> lottos) {
