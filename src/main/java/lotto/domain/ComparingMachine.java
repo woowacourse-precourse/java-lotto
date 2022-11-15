7a9e3f883296250fa1602de0649c43213b6ba385
jijongkwon <help3451@naver.com>
2022-11-15 16:31:17 +0900

    feat : 보너스 숫자가 포함 여부 기능을 추가

--- src/main/java/lotto/domain/ComparingMachine.java
+++ src/main/java/lotto/domain/ComparingMachine.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class ComparingMachine {
     private List<Integer> containNumberList = new ArrayList<>();
+    private List<Integer> containBonusNumber = new ArrayList<>();
 
     public void countContainNumber(List<Integer> winningNumber, List<List<Integer>> lotto){
         for(int i = 0; i < lotto.size(); i++){
@@ -18,7 +19,23 @@ public class ComparingMachine {
         }
     }
 
+    public void countContainBonusNumber(int bonusNumber, List<List<Integer>> lotto){
+        for(List<Integer> lottoList : lotto){
+            if(lottoList.contains(bonusNumber)){
+                containBonusNumber.add(1);
+            }
+
+            if(!lottoList.contains(bonusNumber)){
+                containBonusNumber.add(0);
+            }
+        }
+    }
+
     public List<Integer> getContainNumberList() {
         return containNumberList;
+    }
+
+    public List<Integer> getContainBonusNumber() {
+        return containBonusNumber;
     }
 }
