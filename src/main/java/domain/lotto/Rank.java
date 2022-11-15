5913b6cdc181ef6d836b12e8eb77600906714a44
kmw2378 <rlarla677@gmail.com>
2022-11-15 21:29:51 +0900

    [refactor] Rank - 보너스 볼을 고려 하여 로직 수정

--- src/main/java/domain/lotto/Rank.java
+++ src/main/java/domain/lotto/Rank.java
@@ -3,33 +3,32 @@ package domain.lotto;
 import java.util.Arrays;
 
 public enum Rank {
-    NONE("없음", 0L, 0),
-    FIFTIETH("3개 일치", 5000L, 3),
-    FOURTH("4개 일치", 50000L, 4),
-    THIRD("5개 일치", 1500000L, 5),
-    SECOND("5개 일치, 보너스 볼 일치", 30000000L, 5),
-    FIRST("6개 일치", 2000000000L, 6);
+    NONE("없음", 0L, 0, false),
+    FIFTH("3개 일치", 5000L, 3, false),
+    FOURTH("4개 일치", 50000L, 4, false),
+    THIRD("5개 일치", 1500000L, 5, false),
+    SECOND("5개 일치, 보너스 볼 일치", 30000000L, 5, true),
+    FIRST("6개 일치", 2000000000L, 6, false);
 
     private final String output;
     private final Long reward;
     private final int condition;
     private int count;
+    private boolean isContainBonusNumber;
 
-    Rank(String output, Long reward, int condition) {
+    Rank(String output, Long reward, int condition, boolean isContainBonusNumber) {
         this.output = output;
         this.reward = reward;
         this.condition = condition;
         this.count = 0;
+        this.isContainBonusNumber = isContainBonusNumber;
     }
 
     public static Rank findRank(int NumberOfMatch, boolean isBonusNumberContain) {
 
-        if (NumberOfMatch == 5 && isBonusNumberContain) {
-            return SECOND;
-        }
-
         return Arrays.stream(Rank.values())
                 .filter(e -> e.condition == NumberOfMatch)
+                .filter(e -> e.isContainBonusNumber == isBonusNumberContain)
                 .findAny()
                 .orElse(NONE);
     }
