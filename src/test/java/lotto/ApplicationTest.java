6ac959462fe6832c64031674cad8853c37687404
JJONSOO <pjs970616@naver.com>
2022-11-15 14:34:04 +0900

    test : 로또 구입 금액을 천원단위로 입력했는가?

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -16,6 +18,7 @@ class ApplicationTest extends NsTest {
     void 기능_테스트() {
         assertRandomUniqueNumbersInRangeTest(
                 () -> {
+
                     run("8000", "1,2,3,4,5,6", "7");
                     assertThat(output()).contains(
                             "8개를 구매했습니다.",
@@ -54,8 +57,17 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 천원단위_테스트() {
+        assertSimpleTest(() -> {
+            runException("1010");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
     }
+
 }
