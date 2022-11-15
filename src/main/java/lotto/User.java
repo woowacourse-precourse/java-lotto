3b0b4ca10ab25b06298eccf6ca5f5a33a8788e00
Seungyong Choi <ppyooy336@naver.com>
2022-11-15 13:34:46 +0900

    feat: 입력받은 금액이 1,000단위로 나누어 떨어지지 않는다면 예외 처리한다.

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -15,7 +15,12 @@ public class User {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력할 수 있습니다.");
         }
+    }
 
+    public void isMoneyDividedWith1000(int money) {
+        if(money % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해야 합니다.");
+        }
     }
 
 
