e62acb745c67745dda61e3b0b35b1cc8c92b196d
JJONSOO <pjs970616@naver.com>
2022-11-15 14:27:37 +0900

    feat : create print calculateLotto result

--- src/main/java/lotto/Calculator.java
+++ src/main/java/lotto/Calculator.java
@@ -23,4 +23,20 @@ public class Calculator {
             }
         }
     }
+    public static void printLotto(List<Lotto> lottoBuckets, Lotto userLotto ,int bonus,int buyLottoCount){
+        int benefit = 0;
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for(int order=0;order<5;order++){
+            int key = scoreOrder.get(order);
+            if(key==-1){
+                System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+prizeBuckets.get(-1)+"개");
+            }
+            if(key>-1){
+                System.out.println(key+"개 일치 ("+moneyOrder.get(order)+"원) - "+prizeBuckets.get(key)+"개");
+            }
+            benefit+=prizeInfo.get(key)*prizeBuckets.get(key);
+        }
+        System.out.println("총 수익률은 "+String.format("%.1f", (double)((double)benefit/(double)(buyLottoCount*1000))*(double)100)+"%입니다.");
+    }
 }
