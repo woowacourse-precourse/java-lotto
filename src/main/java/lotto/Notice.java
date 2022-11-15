244abaab983c64a25433bca12b025ee131e87133
0112leesy <0112leesy@naver.com>
2022-11-15 18:01:59 +0900

    fix(Notice) : 안내 메시지 수정
    줄바꿈 문자 제거
    수익률 서식 수정

--- src/main/java/lotto/Notice.java
+++ src/main/java/lotto/Notice.java
@@ -2,19 +2,19 @@ package lotto;
 
 public enum Notice {
 
-    INPUT_PURCHASE("구입금액을 입력해 주세요.\n"),
-    LOTTO_COUNT("\n%d개를 구매했습니다.\n"),
-    LOTTO_NUMBER("%s\n"),
-    INPUT_WINNING_NUMBER("\n당첨 번호를 입력해 주세요.\n"),
-    INPUT_BONUS_NUMBER("\n보너스 번호를 입력해주세요.\n"),
+    INPUT_PURCHASE("구입금액을 입력해 주세요."),
+    LOTTO_COUNT("\n%d개를 구매했습니다."),
+    LOTTO_NUMBER("%s"),
+    INPUT_WINNING_NUMBER("\n당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("\n보너스 번호를 입력해주세요."),
     WINNING_STATUS("\n당첨 통계\n"
         + "---\n"
         + "3개 일치 (5,000원) - %d개\n"
         + "4개 일치 (50,000원) - %d개\n"
         + "5개 일치 (1,500,000원) - %d개\n"
         + "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"
-        + "6개 일치 (2,000,000,000원) - %d개\n"),
-    RATE_OF_RETURN("총 수익률은 %f%입니다.");
+        + "6개 일치 (2,000,000,000원) - %d개"),
+    RATE_OF_RETURN("총 수익률은 %.1f%%입니다.");
 
     private final String message;
 
