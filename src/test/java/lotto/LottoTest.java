8f3952fa2a77bb683c9c24c251b02afb75c74fc6
aran <aran0317@naver.com>
2022-11-15 18:42:15 +0900

    test : InputMoney() test 구현
    InputMoney(), InputException() 테스트 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class LottoTest {
+class LottoTest extends NsTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -23,5 +28,38 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("금액이 1000으로 나누어지지 않으면 예외 발생")
+    @Test
+    void 금액입력_예외_테스트1() {
+        assertSimpleTest(() -> {
+            runException("1030");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+    @DisplayName("금액이 정수가 아니면 예외 발생")
+    @Test
+    void 금액입력_예외_테스트2() {
+        assertSimpleTest(() -> {
+            runException("100aeb");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 금액입력_예외_테스트3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1234"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ERROR_MESSAGE)
+        );
+    }
+
+
+
 }
