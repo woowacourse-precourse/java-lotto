d47cb05fc98d6427011bfee0224ce9f212fd38b2
KimJaeSung <behappy0916@naver.com>
2022-11-15 11:08:29 +0900

    refactor: 초기에 테스트용으로 썼던 main문 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,19 +1,14 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.List;
-import lotto.domain.Judgement;
-import lotto.domain.NumberGenerator;
+import lotto.controller.LottoController;
+import lotto.domain.Money;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        NumberGenerator numberGenerator = new NumberGenerator();
-        Judgement judgement = new Judgement();
-//        List<Integer> numbers = new ArrayList<>();
-//        numbers = numberGenerator.createRandomNumbers();
-//        System.out.println(numbers);
-        int count = judgement.countCorrectNumbers(List.of(1,2,3,4,5,6),List.of(1,2,3,4,5,6));
-        System.out.println(count);
+        LottoController lottoController = new LottoController();
+        lottoController.start();
+
+
     }
 }
