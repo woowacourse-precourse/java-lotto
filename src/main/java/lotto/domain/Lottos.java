e23a85a1580b7c561d566d1c6f22452666ea96cc
Jenn Kim <grdd0419@gmail.com>
2022-11-15 13:31:11 +0900

    fix: unmodifiable list를 sort하지 않도록 수정

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lottos {
@@ -18,9 +19,13 @@ public class Lottos {
         List<Lotto> lottoTickets = new ArrayList<>();
 
         for (int i = 0; i < number; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> unmodifiableNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+            List<Integer> numbers = new ArrayList<>();
+            for(Integer elem : unmodifiableNumbers)
+                numbers.add(elem);
+            Collections.sort(numbers);
             Lotto l = new Lotto(numbers);
-            l.sortNumbers();
             lottoTickets.add(l);
         }
 
