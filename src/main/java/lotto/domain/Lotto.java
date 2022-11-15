5f795c85e3304c3cb64db5772c3bf8975c7b077b
saehim <saeilee.vxv@gmail.com>
2022-11-15 22:59:29 +0900

    feat(lotto) : getter, setter 설정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,6 +4,28 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private int correctCount;
+    private boolean bonus;
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public int getCorrectCount() {
+        return correctCount;
+    }
+
+    public void setCorrectCount(int correctCount) {
+        this.correctCount = correctCount;
+    }
+
+    public boolean isBonus() {
+        return bonus;
+    }
+
+    public void setBonus(boolean bonus) {
+        this.bonus = bonus;
+    }
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
