2b3ea2da2977adbd4a0531ef37c2350107008848
seonpilKim <ksp970306@gmail.com>
2022-11-15 19:37:07 +0900

    Feat: 상수 추가

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -4,6 +4,8 @@ public final class Constants {
 
 	public static final String COMMA = ",";
 	public static final String EMPTY = " ";
+	public static final String LINE_BREAK = "\n";
+	public static final String DIVISION_LINE = "---";
 
 	private Constants() {
 	}
