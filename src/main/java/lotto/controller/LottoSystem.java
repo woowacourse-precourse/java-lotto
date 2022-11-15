8a78b61949b5c9ffd9cc7e60a837541f1f948487
yunjeong <uu0078@naver.com>
2022-11-15 21:05:05 +0900

    feat: 구매한 개수만큼 중복되지 않는 1 ~ 45 사이 랜덤 숫자 6개 리스트 생성

--- src/main/java/lotto/controller/LottoSystem.java
+++ src/main/java/lotto/controller/LottoSystem.java
@@ -1,17 +1,23 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.model.MoneyParser;
 import lotto.view.Terminal;
 import lotto.view.View;
+import java.util.List;
 
 public class LottoSystem {
     public static final int LOTTO_PRICE = 1000;
 
     private final View view = new Terminal();
-    private final MoneyParser moneyParser = new MoneyParser();
 
     public void run() {
+        MoneyParser moneyParser = new MoneyParser();
         int lottoCount = moneyParser.parse(view.requestMoney()) / LOTTO_PRICE;
         view.printLottoCount(lottoCount);
+        for(int i = 0; i < lottoCount; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        }
+
     }
 }
