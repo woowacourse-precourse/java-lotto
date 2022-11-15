aa69a7275ea9ff20c6081b76fc21991e069fd286
farmJun <junpreme1205@gmail.com>
2022-11-15 21:20:13 +0900

    feat(Application): 로또 구매 금액 유효성 검사 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,15 +2,50 @@ package lotto;
 
 
 import camp.nextstep.edu.missionutils.Console;
+
 import static lotto.GuideMessage.*;
+import static lotto.ErrorMessage.*;
 
 public class Application {
+    private static final int INVALID_PURCHASE_MONEY = -1;
+    private static final int LOTTO_TICKET_MONEY = 1000;
+    private static final int NEGATIVE_RANGE = 0;
+
     public static int getLottoPurchaseMoney() {
         System.out.println(INPUT_REQUEST_MESSAGE.getMessage());
         String userInput = Console.readLine();
-        int lottoPurchaseMoney = Integer.parseInt(userInput);
-        return lottoPurchaseMoney;
+        boolean valid = validateUserInput(userInput);
+        if (valid) {
+            int lottoPurchaseMoney = Integer.parseInt(userInput);
+            return lottoPurchaseMoney;
+        }
+        return INVALID_PURCHASE_MONEY;
+    }
 
+    public static boolean validateUserInput(String userInput) throws IllegalArgumentException {
+        boolean valid = checkLottoPurchaseMoneyValid(userInput);
+        if (valid) {
+            int lottoPurchaseMoney = Integer.parseInt(userInput);
+            checkLottoPurchaseMoneyRange(lottoPurchaseMoney);
+        }
+        return valid;
+    }
+
+    public static boolean checkLottoPurchaseMoneyValid(String userInput) throws IllegalArgumentException {
+        try {
+            Integer.parseInt(userInput);
+            return true;
+        } catch (Exception e) {
+            IllegalArgumentException exception = new IllegalArgumentException();
+            System.out.println(NOT_NUMBER_INPUT_MESSAGE.getMessage());
+            return false;
+        }
+    }
+
+    public static void checkLottoPurchaseMoneyRange(int lottoPurchaseMoney) throws IllegalArgumentException {
+        if (lottoPurchaseMoney % LOTTO_TICKET_MONEY != 0 || lottoPurchaseMoney < NEGATIVE_RANGE) {
+            throw new IllegalArgumentException(INVALID_PURCHASE_MONEY_MESSAGE.getMessage());
+        }
     }
 
     public static void main(String[] args) {
