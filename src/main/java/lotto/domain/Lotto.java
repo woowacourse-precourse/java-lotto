8ced0a5a08c4a39ff33ebd6f4840a5e30349fd6e
ddukddaki <rlfrkdms1@naver.com>
2022-11-15 16:29:45 +0900

    feat(Lotto): (예외 처리)번호가 서로 중복되지 않는지 검증하는 기능

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,11 +1,14 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
 
+    private static final int LENGTH_OF_WINNING_NUMBER = 6;
     private static final int NUMBER_MIN_RANGE = 1;
     private static final int NUMBER_MAX_RANGE = 45;
+    private static final String ERR_UNLIKE_NUMBER = "[ERROR] 당첨번호는 중복되지 않아야합니다. ";
     private static final String ERR_WRONG_NUMBER_RANGE = "[ERROR] 당첨 번호는 1~45 숫자여야 합니다. ";
 
     private final List<Integer> numbers;
@@ -24,6 +27,13 @@ public class Lotto {
     public void validateNumberInRange(int number) {
         if (number < NUMBER_MIN_RANGE || NUMBER_MAX_RANGE < number) {
             throw new IllegalArgumentException(ERR_WRONG_NUMBER_RANGE);
+        }
+    }
+
+    public void validateOverlap(List<Integer> numbers) {
+        numbers = numbers.stream().distinct().collect(Collectors.toList());
+        if (numbers.size() != LENGTH_OF_WINNING_NUMBER) {
+            throw new IllegalArgumentException(ERR_UNLIKE_NUMBER);
         }
     }
 }
