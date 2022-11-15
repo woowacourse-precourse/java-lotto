3e2fb5ca476f225e684206cde2afc9aac2a6a006
Hiluxy <sunset.lhj@gmail.com>
2022-11-15 22:58:44 +0900

    feat 로또 번호 당첨 여부

--- src/main/java/lotto/ValidInput.java
+++ src/main/java/lotto/ValidInput.java
@@ -19,7 +19,7 @@ public class ValidInput {
     }
 
     public void validIsInputInRange(List<Integer> number) {
-        if (!((number < LottoEnum.END_NUM.getValue()) && (number > LottoEnum.START_NUM.getValue()))) {
+        if (number < LottoEnum.START_NUM.getValue() || number > LottoEnum.END_NUM.getValue()) {
             throw new IllegalArgumentException(
                 "[ERROR] 로또 번호가 " + LottoEnum.START_NUM.getValue() + "과 " +
                     LottoEnum.END_NUM.getValue() + "사이에 있지 않습니다!");
