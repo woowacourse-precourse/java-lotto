26f0dbfaf2d801c724514ad02143f5923105b1f9
tseop <77163842+tseop@users.noreply.github.com>
2022-11-15 17:45:09 +0900

    refactor(lotto): Exception class 코드 수정
    불필요한 코드 삭제

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -5,9 +5,6 @@ import lotto.ui.Message;
 
 public class LottoException extends IllegalArgumentException {
     public LottoException(String msg) {
-        if (msg.isBlank()) {
-            msg = Message.UNEXPECTED_ERROR;
-        }
-        throw new IllegalArgumentException(Message.ERROR_MESSAGE + msg);
+        super(Message.ERROR_MESSAGE + msg);
     }
 }
