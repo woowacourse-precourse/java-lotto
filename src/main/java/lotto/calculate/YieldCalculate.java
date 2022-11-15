81b0ef2cea9c3c55d08e36eb14e790a0121a38fe
kswhj <108874006+kswhj@users.noreply.github.com>
2022-11-15 18:05:13 +0900

    Update YieldCalculate.java
    출력문 수정

--- src/main/java/lotto/calculate/YieldCalculate.java
+++ src/main/java/lotto/calculate/YieldCalculate.java
@@ -3,6 +3,6 @@ package calculate;
 public class YieldCalculate {
 	public static void run(int num, int cost) {
 		double yield = num / cost;
-		System.out.printf("총 수익률은 %.1f%입니다.", yield);
+		System.out.println("총 수익률은 "+Math.round(yield)+"%입니다.");
 	}
 }
