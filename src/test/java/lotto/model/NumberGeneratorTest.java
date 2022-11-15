cb7e3f75e0516d3c50e99262b2e0f39107c5823b
송민진 <m_6595@naver.com>
2022-11-15 19:20:48 +0900

    test(lotto): check whether number generator works properly
    테스트코드 : 랜덤 로또 번호 생성 테스트코드 작성
    	- 생성된 결과가 리스트형인지 확인
    	- 생성된 결과의 리스트 원소가 정수형인지 확인

--- src/test/java/lotto/model/NumberGeneratorTest.java
+++ src/test/java/lotto/model/NumberGeneratorTest.java
@@ -1,4 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 class NumberGeneratorTest {
-  
+
+    private final NumberGenerator numberGenerator = new NumberGenerator();
+
+    @Test
+    @DisplayName("랜덤 로또 번호 생성 테스트")
+    void createRandomLottoNumbersTest() {
+        Assertions.assertThat(numberGenerator.createRandomLottoNumbers()).isInstanceOf(ArrayList.class);
+        Assertions.assertThat(numberGenerator.createRandomLottoNumbers().get(0)).isInstanceOf(Integer.class);
+    }
 }
