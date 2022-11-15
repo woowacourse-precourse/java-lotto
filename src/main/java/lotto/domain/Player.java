6e7aa48a0eed0018f5c3ec24ae2536c13459d77d
CAEL0 <kjongmin26@gmail.com>
2022-11-15 22:57:51 +0900

    refactor(Player): static import 대체

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -1,15 +1,14 @@
 package lotto.domain;
 
 import lotto.Lotto;
+import lotto.constant.ExceptionConstants;
+import lotto.constant.GameConstants;
 import lotto.util.Validator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static lotto.constant.ExceptionConstants.PURCHASE_AMOUNT_INDIVISIBILITY_EXCEPTION;
-import static lotto.constant.GameConstants.LOTTO_PRICE;
 
 public class Player {
     private int purchaseAmount;
@@ -31,15 +30,15 @@ public class Player {
     }
 
     private void validateDivisibility(int purchaseAmount) {
-        if (purchaseAmount % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException(PURCHASE_AMOUNT_INDIVISIBILITY_EXCEPTION.toString());
+        if (purchaseAmount % GameConstants.LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(ExceptionConstants.PURCHASE_AMOUNT_INDIVISIBILITY_EXCEPTION.toString());
         }
     }
 
     public void buyLotto() {
         while (this.purchaseAmount > 0) {
             lottos.add(Computer.issueLotto());
-            purchaseAmount -= LOTTO_PRICE;
+            purchaseAmount -= GameConstants.LOTTO_PRICE;
         }
     }
 
