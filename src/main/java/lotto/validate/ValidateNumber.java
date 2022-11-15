1ef1c55c160204dc08ec58ad58cd10e7b8b4a859
이지혁 <olzlgur@naver.com>
2022-11-15 17:32:14 +0900

    feat: 보너스 번호 검증 메서드 구현

--- src/main/java/lotto/validate/ValidateNumber.java
+++ src/main/java/lotto/validate/ValidateNumber.java
@@ -1,10 +1,18 @@
 package lotto.validate;
 
+import lotto.Lotto;
+
 public class ValidateNumber {
     private static final String NOT_NUMERIC_MESSAGE = "[ERROR] 구매 금액은 숫자여야 합니다.";
     private static final String UNDER_1000_MESSAGE = "[ERROR] 구매 금액은 1000원 이상이어야 합니다.";
     private static final String NOT_1000_UNIT_MESSAGE = "[ERROR] 구매 금액은 1000원 단위여야 합니다.";
+    private static final String NOT_IN_RANGE_MESSAGE = "[ERROR] 번호는 1 ~ 45 사이에 있어야 합니다.";
+    private static final String DUPLICATE_NUMBER_MESSAGE = "[EEROR] 중복된 번호가 있습니다.";
     private static final Integer STANDARD_MONEY = 1000;
+
+    private static final Integer RANGE_START = 1;
+
+    private static final Integer RANGE_END = 45;
 
     public static Integer validatePurchaseMoney(String purchaseMoney) {
         Integer money = Integer.valueOf(purchaseMoney);
@@ -35,5 +43,24 @@ public class ValidateNumber {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NOT_NUMERIC_MESSAGE);
         }
+    }
+
+    public static void validateBonusNumber(int bonusNumber, Lotto lotto) {
+        isInRange(bonusNumber);
+        isDuplicate(bonusNumber, lotto);
+    }
+
+    private static void isInRange(int bonusNumber) {
+        if (RANGE_START <= bonusNumber && RANGE_END >= bonusNumber) {
+            return;
+        }
+        throw new IllegalArgumentException(NOT_IN_RANGE_MESSAGE);
+    }
+
+    private static void isDuplicate(int bonusNumber, Lotto lotto) {
+        if (lotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_MESSAGE);
+        }
+
     }
 }
