eb8a722eb7e7b4b4920818677aa02d0717f08f8b
hingjae <ljh23270@naver.com>
2022-11-15 19:12:59 +0900

    done

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,21 +1,11 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.List;
+import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        User user = new User();
-        Computer computer = new Computer();
-        List<Integer> winningNumber = new ArrayList<>();
-
-        int money = user.inputMoneyMsg();
-        int amount = user.moneyToAmount(money);
-        user.setLottoList(computer.issuedLotto(amount));
-
-        winningNumber = computer.inputWinningNumber();
-        Lotto lotto = new Lotto(winningNumber);
-
+        LottoController lottoController = new LottoController();
+        lottoController.play();
     }
 }
