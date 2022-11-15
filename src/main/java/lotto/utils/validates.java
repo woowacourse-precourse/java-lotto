e91b94628891307a81d5e48a51060ebf7a857fa0
kimdonghun <zhfptm12@naver.com>
2022-11-15 15:03:49 +0900

    feat : validate 함수들

---
+++ src/main/java/lotto/utils/validates.java
@@ -0,0 +1,17 @@
+package lotto.utils;
+
+public class validates {
+    private boolean validatePurchaseMoney(int purchaseMoney) {
+        if(purchaseMoney % 1000 != 0)return false;
+        return true;
+    }
+
+    private boolean validateLottoCount() {
+        return true;
+    }
+
+    private boolean valadateLottoBound() {
+        return true;
+    }
+
+}
