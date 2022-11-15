2443bd714b6571d2d290267a71e5ac7b0b3b6bbd
chosunghyun18 <krismon@naver.com>
2022-11-15 20:25:27 +0900

    test(user) : remove the testcode
    remove user input
    testcode 필요없음

--- src/test/java/lotto/user/repository/UserRepositoryTest.java
+++ src/test/java/lotto/user/repository/UserRepositoryTest.java
@@ -6,18 +6,7 @@ import lotto.user.domain.User;
 class UserRepositoryTest {
 
     User user = new User();
-    @Test
-    void check_user_buy_lotto_case() {
 
-        User user = new User();
-
-        UserRepository.buyLotteryTickets(user);
-
-        int target = 36;
-        assertThat(user.getTicketAmount()).isEqualTo(target);
-        target = 36000;
-        assertThat(user.getPurchaseAmount()).isEqualTo(target);
-    }
     @Test
     void check_user_purchase_amount_of_tickets(){
         int ticketsAmount = 7;
