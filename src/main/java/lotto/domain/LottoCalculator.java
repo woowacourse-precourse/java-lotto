42fbbb9f19cfd9db823803a4cfc6742b227721ed
yummygyudon <bang2brew@gmail.com>
2022-11-15 12:51:19 +0900

    FEAT(domain): 로또 계산 기능 클래스 구현
    - 클래스 명 : LottoCalculator 클래스
    - 활용 클래스 : Result Enum
    - 메서드
      1. `getLottoQuantity`
        - 사용자의 구매 금액에 대해 로또 가격을 나누어 구매 로또 수를 구하는 메서드입니다.
      2. `getRate`
        - 사용 금액 대비 총 취득 상금의 수익률을 계산하는 메서드입니다.
        - Math패키지의 round 메서드를 통해 둘째자리에서 반올림합니다.
      3. `getEqualsCount`
        - 사용자 로또에서 winningNumbers와 비교하여 일치 갯수를 계산합니다.
        - 순수하게 처음부터 당첨 번호 6개가 모두 일치하면 7로, 5개가 일치하면서 보너스 번호가 포함되어 있으면 6으로 처리하여 반환한다.
      4. `getRank`
        - equalsCount의 값으로 RATING_TABLE에서 해당하는 등수를 반환합니다.
      5. `getRewardMoney`
        - 각 등수의 갯수마다 Result Enum의 상금과 곱하여 총 취득 상금액을 반환합니다.
    
    * 일치 갯수별 등수 반환을 위한 RATING_TABLE이라는 static final Map 선언
    * 불변할 상수 값인 로또 가격을 가진 static final 변수로 LOTTO_PRICE int 선언

---
+++ src/main/java/lotto/domain/LottoCalculator.java
@@ -0,0 +1,52 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.Lotto;
+import lotto.util.Result;
+
+public class LottoCalculator {
+    private static final int LOTTO_PRICE = 1_000;
+    private static final Map<Integer, Integer> RATING_TABLE = new HashMap<>() {{
+        put(3, 5);
+        put(4, 4);
+        put(5, 3);
+        put(6, 2);
+        put(7, 1);
+    }};
+
+    public int getLottoQuantity(long playerMoney) {
+        return (int) playerMoney / LOTTO_PRICE;
+    }
+
+    public double getRate(long rewardMoney, long playerMoney) {
+        double rate = (double) rewardMoney / playerMoney ;
+        return Math.round(rate * 1000) / 10.0  ;
+    }
+
+    public int getEqualsCount(List<Integer> winningNumbers, int bonusNumber, Lotto playerLotto) {
+        int equalsCount = playerLotto.getCompareResult(winningNumbers);
+        if (equalsCount == 6) {
+            equalsCount = 7;
+        }
+        if (equalsCount == 5 && playerLotto.isBonusContained(bonusNumber)) {
+            equalsCount = 6;
+        }
+        return equalsCount;
+    }
+
+    public int getRank(int equalsCount) {
+        return RATING_TABLE.get(equalsCount);
+    }
+
+    public long getRewardMoney(Map<Integer, Integer> resultBoard) {
+        long rewardMoney = 0L;
+        rewardMoney += Result.RANK_ONE.getReward() * resultBoard.get(1);
+        rewardMoney += Result.RANK_TWO.getReward() * resultBoard.get(2);
+        rewardMoney += Result.RANK_THREE.getReward() * resultBoard.get(3);
+        rewardMoney += Result.RANK_FOUR.getReward() * resultBoard.get(4);
+        rewardMoney += Result.RANK_FIVE.getReward() * resultBoard.get(5);
+        return rewardMoney;
+    }
+}
