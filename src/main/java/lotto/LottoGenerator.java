97a19fd1440c960f63c6f66f213bec0b795ae9db
youswim <youswim96@gmail.com>
2022-11-15 23:35:04 +0900

    fix(lotto): makeLottoNumber()메서드의 오류 수정
    무작위 숫자 생성을 pickUniqueNumberInRange()를 사용하도록 수정함.
    프로그래밍 요구사항을 지키기 위함.

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -8,13 +8,7 @@ import java.util.List;
 public class LottoGenerator {
 
     public static List<Integer> makeLottoNumber() {
-        List<Integer> lottoNumbers = new ArrayList<>();
-        while (lottoNumbers.size() < Lotto.WIN_NUMBER_SIZE) {
-            int random = Randoms.pickNumberInRange(Lotto.MIN_NUMBER, Lotto.MAX_NUMBER);
-            if (!lottoNumbers.contains(random)) {
-                lottoNumbers.add(random);
-            }
-        }
+        List<Integer> lottoNumbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         lottoNumbers.sort(Integer::compareTo);
         return lottoNumbers;
     }
