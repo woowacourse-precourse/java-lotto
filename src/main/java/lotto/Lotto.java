ebb906cec94f68da5db0915cefadbfeef0b3ddcb
kim <kajinori@gachon.ac.kr>
2022-11-15 23:11:08 +0900

    feat(Lotto): 로또 생성 및 정렬, 숫자 비교 기능, 보너스 볼 확인

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,50 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        validateDuplicate(numbers);
+        this.numbers = numbers.stream().sorted().collect(Collectors.toList());
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 숫자 6개를 입력해주세요.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        List<Integer> duplicateNumber = numbers.stream().distinct().collect(Collectors.toList());
+        if (duplicateNumber.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 같은 로또 번호는 존재할 수 없습니다.");
+        }
+    }
+
+    public int compare(Lotto compareNumbers) {
+        int count = 0;
+        List<Integer> winningNumbers = compareNumbers.getNumbers();
+
+        for (Integer number : numbers) {
+            if (winningNumbers.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean hasBonus(int bonus) {
+        if (numbers.contains(bonus)) {
+            return true;
+        }
+        return false;
+    }
 }
