362ebfd482b4302a96d5fe1d777c867a228354f5
Beginner <dkstmdwn17@naver.com>
2022-11-15 11:00:31 +0900

    refactor : 값이 하드코딩되어 있던 부분을 수정
    값이 하드코딩되어 있던 부분을
    static final 으로 분리하였다.

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -10,10 +10,21 @@ import java.util.stream.Collectors;
 
 public class Input {
     private static final String ERROR_MESSAGE = "[ERROR] ";
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String SCOPE_ERROR_MESSAGE = "범위 내에 입력해주세요.";
+    private static final String DUPLICATE_ERROR_MESSAGE = "당첨 번호와 중복됩니다. 다시 입력해주세요.";
+    private static final String IS_NUMBER_MESSAGE = "숫자만 입력해주세요.";
+    private static final String SCOPE_DUPLICATE_ERROR_MESSAGE = "범위 밖의 숫자 또는 중복된 숫자를 입력하셨습니다.";
+    private static final String DIVIDE_ERROR_MESSAGE = "1000으로 나누어 떨어지는 금액을 입력해주세요.";
+    private static final String SMALL_AMOUNT_ERROR_MESSAGE = "1000원 이상의 금액을 입력해주세요.";
+    private static final String SEPARATOR = ",";
+
 
     public int getMoney(){
         try {
-            System.out.println("구입금액을 입력해 주세요.");
+            System.out.println(PURCHASE_AMOUNT_MESSAGE);
             String unverifiedCharacters = Console.readLine();
             int money = Integer.parseInt(unverifiedCharacters);
             if(money < 1000) validationMoney();
@@ -26,11 +37,11 @@ public class Input {
     }
 
     public Lotto getWinningNumbers(){
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(WINNING_NUMBERS_MESSAGE);
 
         String winningNumbers = Console.readLine();
 
-        String[] numbers = winningNumbers.split(",");
+        String[] numbers = winningNumbers.split(SEPARATOR);
         try {
             List<String> winningNumbersCast =
                     Arrays.stream(numbers)
@@ -44,7 +55,7 @@ public class Input {
     }
 
     public int getBonusNumber(List<Integer> winningNumbers){
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(BONUS_NUMBER_MESSAGE);
         String bonusNumber = Console.readLine();
         try {
             validateBonusNumber(bonusNumber, winningNumbers);
@@ -59,10 +70,10 @@ public class Input {
         parseInt(bonusNumber);
         int bonusNum = Integer.parseInt(bonusNumber);
         if(!(bonusNum >=1 && bonusNum <=45)) {
-            throw new IllegalArgumentException("범위 내에 입력해주세요.");
+            throw new IllegalArgumentException(SCOPE_ERROR_MESSAGE);
         }
         if(winningNumbers.contains(bonusNum)) {
-            throw new IllegalArgumentException("당첨 번호와 중복됩니다. 다시 입력해주세요.");
+            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
         }
     }
 
@@ -70,7 +81,7 @@ public class Input {
         try {
             Integer.parseInt(number);
         }catch (Exception e){
-            throw new IllegalArgumentException("숫자만 입력해주세요.");
+            throw new IllegalArgumentException(IS_NUMBER_MESSAGE);
         }
     }
 
@@ -83,7 +94,7 @@ public class Input {
             isItInRange(winningNumbers);
             return new Lotto(winningNumbers);
         }catch (NumberFormatException e){
-            throw new IllegalArgumentException("숫자를 입력해주세요.");
+            throw new IllegalArgumentException(IS_NUMBER_MESSAGE);
         }
     }
 
@@ -93,7 +104,7 @@ public class Input {
                     filter(num -> num >= 1 && num <= 45).distinct()
                     .count();
             if(count != 6) {
-                throw new IllegalArgumentException("범위 밖의 숫자 또는 중복된 숫자를 입력하셨습니다.");
+                throw new IllegalArgumentException(SCOPE_DUPLICATE_ERROR_MESSAGE);
             }
         }catch (IllegalArgumentException e){
             throw new IllegalArgumentException(e.getMessage());
@@ -101,10 +112,10 @@ public class Input {
     }
 
     private void validationDivideMoney() {
-        throw new IllegalArgumentException("1000으로 나누어 떨어지는 금액을 입력해주세요.");
+        throw new IllegalArgumentException(DIVIDE_ERROR_MESSAGE);
     }
 
     private void validationMoney() {
-        throw new IllegalArgumentException("1000원 이상의 금액을 입력해주세요.");
+        throw new IllegalArgumentException(SMALL_AMOUNT_ERROR_MESSAGE);
     }
 }
