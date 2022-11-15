9c67f021546ebdc140a5e99319eeaa4dd74f5c91
KNH <nh2004@naver.com>
2022-11-15 09:12:22 +0900

    feat : 전체 기능 구현
    전체 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,52 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.controller.RankController;
+import lotto.controller.UserController;
+import lotto.domain.user.User;
+import lotto.view.RankingView;
+import lotto.view.Store;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoConfig lottoConfig = new LottoConfig();
+
+        Store store = lottoConfig.store();
+        RankingView rankingView = lottoConfig.rankingView();
+        UserController userController = lottoConfig.userController();
+        RankController rankController = lottoConfig.rankController();
+
+        startLottoGame(store, rankingView, userController, rankController);
+    }
+
+    private static void startLottoGame(Store store, RankingView rankingView,
+                                       UserController userController,
+                                       RankController rankController) {
+        try{
+            User user = buyLotto(store, userController);
+            drawLotto(rankingView, rankController, user);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // 로또 구매
+    private static User buyLotto(Store store, UserController userController) {
+        System.out.println(store.sellingLotto());
+        User user = new User(Console.readLine());
+
+        System.out.println(userController.buyLotto(user));
+        return user;
+    }
+
+    // 로또 추첨
+    private static void drawLotto(RankingView rankingView, RankController rankController, User user) {
+        System.out.println(rankingView.getWinningNumber());
+        String winningNumber = Console.readLine();
+
+        System.out.println(rankingView.getBonusNumber());
+        String bonus = Console.readLine();
+
+        System.out.println(rankController.statistics(user, winningNumber, bonus));
     }
 }
