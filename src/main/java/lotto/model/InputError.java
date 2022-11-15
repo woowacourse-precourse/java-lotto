eaf7c916b99b686adfc86ef612b7897bf3ee0c96
LeeHaiLim <hailim1997@gmail.com>
2022-11-15 19:20:01 +0900

    style(model): fix typo

--- src/main/java/lotto/model/InputError.java
+++ src/main/java/lotto/model/InputError.java
@@ -7,7 +7,7 @@ public enum InputError {
     NUMBER(PREFIX.message() + "숫자만 입력해야 합니다."),
     MONEY_POSITIVE(PREFIX.message() + "양수를 입력해야 합니다."),
     MONEY_MOD(PREFIX.message() + String.format("%d으로 나누어 떨어지는 숫자를 입력해야 합니다.", Constants.LOTTO_PRICE)),
-    LENGTH(PREFIX.message() + String.format("%d 자리의 숫자를 %s를 기준으로 구분하여 입력해야 합니다.",
+    LENGTH(PREFIX.message() + String.format("%d 개의 숫자를 %s를 기준으로 구분하여 입력해야 합니다.",
             Constants.NUMBER_LENGTH, Constants.SPLIT_REGEX));
     private final String message;
     InputError(String message) {
