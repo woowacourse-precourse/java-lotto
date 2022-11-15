25de4196ae8d87f18ed56eeada23df4397a43a26
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-15 20:00:20 +0900

    test(LottoGame): 당첨 번호, 보너스 번호 일치 여부에 따른 등수를 올바르게 설정하는 테스트

--- src/test/java/lotto/domain/GradeTest.java
+++ src/test/java/lotto/domain/GradeTest.java
@@ -1,4 +1,18 @@
 package lotto.domain;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class GradeTest {
+
+    @Test
+    @DisplayName("당첨 번호, 보너스 번호 일치 여부에 따른 등수를 올바르게 판별한다.")
+    void findingRank() {
+        assertThat(Grade.findingGrade(6,0)).isEqualTo(Grade.FIRST);
+        assertThat(Grade.findingGrade(5,1)).isEqualTo(Grade.SECOND);
+        assertThat(Grade.findingGrade(5,0)).isEqualTo(Grade.THIRD);
+        assertThat(Grade.findingGrade(4,0)).isEqualTo(Grade.FOURTH);
+        assertThat(Grade.findingGrade(3,0)).isEqualTo(Grade.FIFTH);
+        assertThat(Grade.findingGrade(0,0)).isNull();
+    }
 }
