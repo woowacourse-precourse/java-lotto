bec1bd12341a57dd7c14862272bbcf0925c8129c
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-15 15:55:12 +0900

    style(InputForm): add blank line

--- src/main/java/lotto/constant/InputForm.java
+++ src/main/java/lotto/constant/InputForm.java
@@ -1,6 +1,7 @@
 package lotto.constant;
 
 public enum InputForm {
+
     POSITIVE_INTEGER_FORM("[1-9][0-9]*"),
     SIX_POSITIVE_INTEGER_DIVIDED_BY_COMMA("[1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*");
 
