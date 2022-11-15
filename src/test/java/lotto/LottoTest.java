b21018be711a6855052a892cca3e416ea38dc92f
Konahyeon <koo@inha.edu>
2022-11-15 21:30:43 +0900

    test: 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,4 +24,10 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 범위(1~45)를 넘는 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 55)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
