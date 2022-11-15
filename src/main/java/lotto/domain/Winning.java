3121be61d73d193ee733913d9291e167b4c9e2ef
hhiyeon <hhyyeon0214@gmail.com>
2022-11-15 12:38:06 +0900

    feat: winning-check invalid bonus number to handler

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -11,16 +11,22 @@ public class Winning {
     private static final int MIN_LOTTO = 1;
     private static final int MAX_LOTTO = 45;
     private static final int WINNING_SIZE = 6;
+    private static final int BONUS_SIZE = 1;
 
-    private List<Integer> winningNumbers;
+    private final List<Integer> winningNumbers;
     private int bonusNumber;
 
-    public Winning(List<Integer> winningNumbers) {
-        this.winningNumbers = winningNumbers;
+    public Winning(String inputNumbers) {
+        isNotNumeric(inputNumbers);
+        this.winningNumbers = splitNumbers(inputNumbers);
+        validateWinningNumbers();
     }
 
-    public Winning(int bonusNumber) {
-        this.bonusNumber = bonusNumber;
+    public Winning(List<Integer> winningNumbers, String bonusNumber) {
+        isNotNumeric(bonusNumber);
+        this.winningNumbers = winningNumbers;
+        validateBonusNumbers(bonusNumber);
+        this.bonusNumber = Integer.parseInt(bonusNumber);
     }
 
     private void validateWinningNumbers() {
@@ -34,6 +40,22 @@ public class Winning {
 
         if (isInvalidNumberRange(winningNumbers)) {
             throw new ErrorException(Error.INVALID_LOTTO_RANGE.toString());
+        }
+    }
+
+    private void validateBonusNumbers(String bonusNumber) {
+        List<Integer> bonus = splitNumbers(bonusNumber);
+
+        if (isInvalidNumbersSize(bonus, BONUS_SIZE)) {
+            throw new ErrorException(Error.INVALID_BONUS_SIZE.toString());
+        }
+
+        if (isInvalidNumberRange(bonus)) {
+            throw new ErrorException(Error.INVALID_LOTTO_RANGE.toString());
+        }
+
+        if (isIncludeBonus(bonusNumber)) {
+            throw new ErrorException(Error.ALREADY_EXIST_WIN_NUMBER.toString());
         }
     }
 
