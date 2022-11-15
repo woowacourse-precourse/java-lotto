ed3c3e4e7f104a3674642f4cdbd6176f6b648929
Paul_Kim <mok03127@naver.com>
2022-11-15 13:20:41 +0900

    fix: 빌드 오류 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Lotto {
     private static final int MIN_NUMBER = 1;
@@ -53,5 +54,26 @@ public class Lotto {
         if (numbers.indexOf(number) != numbers.lastIndexOf(number)) {
             throw new IllegalArgumentException(LOTTO_HAS_DUPLICATED_NUMBER_ERROR_MESSAGE);
         }
+    }
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(numbers, lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
     }
 }
