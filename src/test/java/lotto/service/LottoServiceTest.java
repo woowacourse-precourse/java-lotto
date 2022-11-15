f46acba13966f8c67971ecd481e19d3baee9c8fd
yujaemin <woawoa0513@gmail.com>
2022-11-15 19:33:10 +0900

    test: LottoService 비즈니스 로직 테스트
    로또 생성 테스트

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,10 +1,9 @@
-package lotto.controller;
+package lotto.service;
 
 import static lotto.utils.Constants.LOTTO_PRICE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import lotto.domain.Lotto;
-import lotto.service.LottoService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
