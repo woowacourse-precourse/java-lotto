059758b0aad449b410071e3f78dbfb1e0d6405ef
jisoolee11 <jisu6414@likelion.org>
2022-11-15 22:32:18 +0900

    feat: 사용자에게 구입금액 입력받기

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,19 +1,23 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-    private Integer bonusNumber = 0;
+    private Integer bonusNumber;
+    private Integer userAmount;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
         bonusNumber = BonusNumber();
+        userAmount = UserAmount();
         System.out.println(numbers);
         System.out.println(bonusNumber);
+        System.out.println(userAmount);
     }
 
     private void validate(List<Integer> numbers) {
@@ -22,11 +26,17 @@ public class Lotto {
         }
     }
 
-    public Integer BonusNumber() {
+    private Integer BonusNumber() {
         bonusNumber = Randoms.pickNumberInRange(1, 45);
         while (numbers.contains(bonusNumber)) {
             bonusNumber = Randoms.pickNumberInRange(1, 45);
         }
         return bonusNumber;
+    }
+
+    private Integer UserAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        userAmount = Integer.parseInt(Console.readLine());
+        return userAmount;
     }
 }
