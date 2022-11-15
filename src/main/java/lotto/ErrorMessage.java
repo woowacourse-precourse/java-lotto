50b5ac4f2c08c01fe7cc85b68ed104a193e2e166
seonpilKim <ksp970306@gmail.com>
2022-11-15 19:37:43 +0900

    Feat: 로또 번호 중복 에러 메시지 반환 메소드 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -9,6 +9,11 @@ public class ErrorMessage {
 	private static final String INVALID_NUMBER = "숫자가 아닙니다.";
 	private static final String NOT_UNITS_OF_1000 = "1,000원 단위의 금액이여야 합니다.";
 	private static final String INVALID_NUMBER_OF_LOTTO_NUMBERS = "로또 번호는 6개여야 합니다.";
+	private static final String DUPLICATED_LOTTO_NUMBER = "로또 번호는 서로 다른 숫자여야 합니다.";
+
+	public static String isDuplicatedLottoNumber() {
+		return generate(DUPLICATED_LOTTO_NUMBER);
+	}
 
 	public static String isNotUnitsOf1000() {
 		return generate(NOT_UNITS_OF_1000);
@@ -26,7 +31,7 @@ public class ErrorMessage {
 		return generate(INVALID_LOTTO_NUMBER);
 	}
 
-	public static String generate(String message) {
+	private static String generate(String message) {
 		return PREFIX + EMPTY + message;
 	}
 
