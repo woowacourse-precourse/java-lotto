a6f3edb00ac6cfa8ef664f464633070c3468f6d1
Sinryuji <siler2474@gmail.com>
2022-11-15 14:13:29 +0900

    feat(Rank): Rank enum 메세지 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,23 +3,25 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(0, 6, 0, 2_000_000_000),
-    SECOND(1, 5, 1, 30_000_000),
-    THIRD(2, 5, 0, 1_500_000),
-    FOURTH(3, 4, 0, 50_000),
-    FIFTH(4,3, 0, 5_000)
+    FIRST(0, 6, 0, 2_000_000_000, "6개 일치 (2,000,000,000원) - "),
+    SECOND(1, 5, 1, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(2, 5, 0, 1_500_000, "5개 일치 (1,500,000원) - "),
+    FOURTH(3, 4, 0, 50_000, "4개 일치 (50,000원) - "),
+    FIFTH(4,3, 0, 5_000, "3개 일치 (5,000원) - ")
     ;
 
     private final int index;
     private final int matchNumbers;
     private final int matchBonus;
     private final int winAmount;
+    private final String message;
 
-    Rank(int index, int matchNumbers, int matchBonus, int winAmount) {
+    Rank(int index, int matchNumbers, int matchBonus, int winAmount, String message) {
         this.index = index;
         this.matchNumbers = matchNumbers;
         this.matchBonus = matchBonus;
         this.winAmount = winAmount;
+        this.message = message;
     }
 
     public int getWinAmount() {
@@ -35,6 +37,10 @@ public enum Rank {
                 .filter(rank -> rank.index == index)
                 .findAny()
                 .orElse(null);
+    }
+
+    public String getMessage() {
+        return this.message;
     }
 
     public static Rank findRank(int numbersCount, int bonusCount) {
