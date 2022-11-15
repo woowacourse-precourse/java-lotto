09f13204d8d602af551893d146857c219126d754
simba <shim9597@gmail.com>
2022-11-15 21:58:37 +0900

    refactor(lotto): 수익률 결과 오류로 인한 수정, 점수 출력 부분

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,10 @@
 package lotto.domain;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Lotto {
-
-    public static final int PRICE = 1000;
 
     private final List<Integer> numbers;
 
@@ -24,17 +21,15 @@ public class Lotto {
     private void validateDuplicate(List<Integer> numbers) throws IllegalArgumentException{
         Set<Integer> set = new HashSet<>(numbers);
         if (set.size() != numbers.size()) {
-            System.out.println("[ERROR] 로또 번호에는 중복된 숫자가 있으면 안됩니다.");
+            System.out.println(ExceptionMessage.DUPLICATE_NUM_LOTTO.getMessage());
             throw new IllegalArgumentException();
         }
-
     }
 
     private void validateSize(List<Integer> numbers) throws IllegalArgumentException{
-        if (numbers.size() != 6) {
-            System.out.println("로또 번호는 총 6개의 숫자여야 합니다.");
+        if (numbers.size() != Constants.LOTTO_LENGTH) {
+            System.out.println(ExceptionMessage.LENGTH.getMessage());
             throw new IllegalArgumentException();
         }
     }
-    // TODO: 추가 기능 구현
 }
--- src/main/java/lotto/view/ScorePrint.java
+++ src/main/java/lotto/view/ScorePrint.java
@@ -2,19 +2,10 @@ package lotto.view;
 
 import java.text.DecimalFormat;
 
+import static lotto.domain.Constants.SECOND_PRIZE;
 import static lotto.domain.Score.*;
 import static lotto.domain.Prize.*;
 
-
-/**
- * 당첨 통계
- * ---
- * 3개 일치 (5,000원) - 1개
- * 4개 일치 (50,000원) - 0개
- * 5개 일치 (1,500,000원) - 0개
- * 5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
- * 6개 일치 (2,000,000,000원) - 0개
- */
 public class ScorePrint {
 
     DecimalFormat formatter = new DecimalFormat("###,###.##");
@@ -25,7 +16,7 @@ public class ScorePrint {
         System.out.println("3개 일치 (" + formatter.format(FIFTH_PRIZE.getPrize()) + "원) - " + count.get(0) + "개");
         System.out.println("4개 일치 (" + formatter.format(FOURTH_PRIZE.getPrize()) + "원) - " + count.get(1) + "개");
         System.out.println("5개 일치 (" + formatter.format(THIRD_PRIZE.getPrize()) + "원) - " + count.get(2) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (" + formatter.format(SECOND_PRIZE.getPrize()) + "원) - " + bonusCount + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (" + formatter.format(SECOND_PRIZE) + "원) - " + bonusCount + "개");
         System.out.println("6개 일치 (" + formatter.format(FIRST_PRIZE.getPrize()) + "원) - " + count.get(3) + "개");
     }
 }
