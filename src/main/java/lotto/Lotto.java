c7d089a58b727b2766cb6cd78f54ac177568f9c7
Bumsoo <rhqjatn2398@naver.com>
2022-11-15 18:03:49 +0900

    feat(Lotto): 로또 번호 검증 기능에 조건을 추가
    중복이 있는 경우 에러 메시지를 추가.
    로또 번호가 1~45 범위 밖인 경우 에러 메시지를 출력하며 예외를 발생시킨다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,6 +15,19 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        Set<Integer> uniqueNumbers = Set.copyOf(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 있어선 안됩니다.");
+        }
+
+        long count = numbers.stream()
+                .filter(integer -> integer < 1 || 45 < integer)
+                .count();
+
+        if (count != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 
     public List<Integer> getNumbers() {
