5e3105f8b07b3f4f00330d667a2078e03a9ceca4
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-15 20:05:30 +0900

    test(LottoGame): 당첨 번호, 보너스 번호와 비교하여 등수를 올바르게 판별하는 테스트

--- src/test/java/lotto/domain/RefereeTest.java
+++ src/test/java/lotto/domain/RefereeTest.java
@@ -1,4 +1,20 @@
 package lotto.domain;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import java.util.List;
 
 public class RefereeTest {
+    @DisplayName("당첨 번호, 보너스 번호와 비교하여 등수를 올바르게 판별한다.")
+    @Test
+    void calculateGrades() {
+        List<Lotto> lottos = List.of(
+                new Lotto(List.of(1,2,3,4,6,8)),
+                new Lotto(List.of(1,2,3,4,5,8))
+        );
+        Lotto winningNumbers = new Lotto(List.of(1,2,3,4,8,12));
+        Referee referee = new Referee(lottos, winningNumbers, 6);
+        assertThat(referee.calculateGrades()).containsExactly(Grade.SECOND,Grade.THIRD);
+    }
 }
