180f49d09a49214f3721229cda46edadcaa36aa8
Seung-Been Kim <mainlib990@gmail.com mainlib990s-MacBookPro.local>
2022-11-15 17:51:54 +0900

    feat(lottoMachine): 입력한 금액만큼의 로또를 구입하는 기능을 구현하세요

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,10 +1,18 @@
 package lotto;
 
 import lotto.boundary.Console;
+import lotto.boundary.Randoms;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 public class LottoMachine extends AbstractLottoMachine {
+    private static final long BUY_UNIT = 1_000;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int NUM_DRAW = 6;
+
     @Override
     protected String askAmountWithUser() {
         System.out.println("구매금액을 입력해 주세요.");
@@ -13,7 +21,14 @@ public class LottoMachine extends AbstractLottoMachine {
 
     @Override
     protected List<Lotto> buyLotteries(long amount) {
-        return null;
+        if (0 != amount % BUY_UNIT) {
+            Logger.error("올바른 단위를 입력하세요: ^" + amount + "$");
+            throw new IllegalArgumentException();
+        }
+        return LongStream.range(0L, amount / BUY_UNIT)
+                .mapToObj(n -> Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, NUM_DRAW))
+                .map(Lotto::new)
+                .collect(Collectors.toList());
     }
 
     @Override
