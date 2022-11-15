3220b69b3d9be65a04976b99054377b8245e8d1d
davinc71998 <davinc71998@gmail.com>
2022-11-15 21:05:20 +0900

    Refactor: 변경 멤버변수 이름
    money는 가독성이 떨어지므로 shopMoney로 구매하는 돈 표현

--- src/main/java/lotto/LottoShop.java
+++ src/main/java/lotto/LottoShop.java
@@ -3,21 +3,27 @@ package lotto;
 public class LottoShop {
 
     private static final int LOTTO_PRICE = 1000;
-    private int money;
+    private int shopMoney;
 
-    public LottoShop(int money) {
-        validateMoney(money);
-        this.money = money;
+    public LottoShop(int shopMoney) {
+        validateMoney(shopMoney);
+        this.shopMoney = shopMoney;
     }
 
-    private void validateMoney(int money) {
-        if(money % LOTTO_PRICE != 0) {
+    private void validateMoney(int shopMoney) {
+        if(shopMoney % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException();
         }
     }
 
     public int LottoCountCalculate() {
-        int LottoCount = money / LOTTO_PRICE;
+        int LottoCount = shopMoney / LOTTO_PRICE;
+        System.out.println(LottoCount + "개를 구매했습니다.");
         return LottoCount;
     }
+
+    public int getShopMoney() {
+        return shopMoney;
+    }
+    
 }
