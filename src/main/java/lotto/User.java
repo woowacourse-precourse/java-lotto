36dde84f43a1176cbcf2829d74abda17362ebab0
gayeonP <pagaye@naver.com>
2022-11-15 15:05:18 +0900

    validation class 추가

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,4 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class User {
+    private int cash = 0;
+
+    public User() {
+
+    }
+
+    public void buyLotto() {
+        this.cash = Integer.valueOf(Console.readLine());
+    }
 }
