ed736a8af831454fea0841c0a96a25a5c144ae60
saehim <saeilee.vxv@gmail.com>
2022-11-15 18:24:27 +0900

    feat(lotto) : 보너스 번호 입력 메서드, 예외처리
    - 보너스 번호 입력 메서드
    - 보너스 번호 입력 메시지
    - 보너스 번호 숫자 확인 예외처리
    - 보너스 번호 범위 예외처리
    - 보너스 번호 추첨 번호 비교 예외처리

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,16 +1,18 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Input {
-    static Scanner scanner = new Scanner(System.in);
 
     /** 사용자에게 로또를 살 금액을 입력 요청하는 메서드 */
     public static int askMoney() throws NumberFormatException {
         System.out.println(Messages.ASK_MONEY);
-        return Integer.parseInt(scanner.nextLine());
+        return Integer.parseInt(readLine());
     }
 
     /** 복권 구매 */
@@ -21,14 +23,55 @@ public class Input {
         return lotto;
     }
 
+    /** 번호 입력 */
+    public static Lotto askWinLotto() {
+        System.out.println(Messages.ASK_WIN_NUMBER);
+        String input = readLine();
+        List<Integer> winNums = null;
+        try {
+            winNums = getWinNums(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(Messages.ERROR_NOT_NUMBER);
+        }
+
+        // 예외사항 체크
+        isVaildRange(winNums);
+        isSize6(winNums);
+        isDiffrent(winNums);
+
+        return new Lotto (winNums);
+    }
+
+    /** 보너스 번호 입력 */
+    public static int askBonusNum(Lotto winLotto) {
+        System.out.println(Messages.ASK_BONUS_NUMBER);
+        int number = getBonusNum(readLine());
+
+        isVaildRange(number);
+        isDiffrent(number, winLotto);
+
+        return number;
+    }
+
     private static List<Integer> getWinNums (String input) {
         List<Integer> winNums = new ArrayList<>();
         String[] strings = input.split(",");
 
         for (int i=0; i<strings.length; i++) {
-            winNums.add(Integer.parseInt(strings[i].trim()));
+            String string = strings[i].trim();
+            winNums.add(Integer.parseInt(string));
         }
         return winNums;
+    }
+
+    public static int getBonusNum (String input) {
+        int num=0;
+        try {
+            num = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(Messages.ERROR_NOT_NUMBER);
+        }
+        return num;
     }
 
     private static void vaildMoney(int Money) {
@@ -51,6 +94,12 @@ public class Input {
         }
     }
 
+    private static void isVaildRange(int num) {
+        if (num>45 || num<1) {
+            throw new IllegalArgumentException(Messages.ERROR_NOT_VAILDRANGE);
+        }
+    }
+
     private static void isSize6(List<Integer> nums) {
         if (nums.size()!=6) {
             throw new IllegalArgumentException(Messages.ERROR_NOT_SIZE6);
@@ -63,6 +112,12 @@ public class Input {
             if (compareNums.contains(nums.get(i))) {
                 throw new IllegalArgumentException(Messages.ERROR_NOT_DIFFRENT);
             }
+        }
+    }
+
+    private static void isDiffrent(int number, Lotto lotto) {
+        if(lotto.getNumbers().contains(number)) {
+            throw new IllegalArgumentException(Messages.ERROR_NOT_DIFFRENT_BONUS_NUMBER);
         }
     }
 }
--- src/main/java/lotto/view/Messages.java
+++ src/main/java/lotto/view/Messages.java
@@ -8,6 +8,7 @@ public class Messages {
     // Inputs
     public static final String ASK_MONEY = "구입금액을 입력해 주세요.";
     public static final String ASK_WIN_NUMBER = "당첨 번호를 입력해 주세요.";
+    public static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     // Results
     private static final String RESULT_BUY_LOTTO = "개를 구매했습니다.";
@@ -15,9 +16,11 @@ public class Messages {
     // ERROR
     public static final String  ERROR_NOT_DIVDE1000 = "[ERROR] 금액은 1,000원 단위로 입력할 수 있습니다.";
     public static final String  ERROR_NULL = "[ERROR] 0이 아닌 값을 입력하세요.";
-    public static final String  ERROR_NOT_SIZE6 = "[ERROR] 쉼표(,)로 구분하여 4개의 숫자를 입력하세요.";
+    public static final String  ERROR_NOT_SIZE6 = "[ERROR] 쉼표(,)로 구분하여 6개의 숫자를 입력하세요.";
     public static final String  ERROR_NOT_VAILDRANGE = "[ERROR] 각 숫자가 1~45 사이에 있어야 합니다.";
     public static final String  ERROR_NOT_DIFFRENT= "[ERROR] 각 숫자는 모두 달라야 합니다.";
+    public static final String  ERROR_NOT_NUMBER= "[ERROR] 숫자로 입력해 주세요.";
+    public static final String  ERROR_NOT_DIFFRENT_BONUS_NUMBER= "[ERROR] 보너스 숫자가 아미 당첨 번호에 포함되어 있습니다.";
 
     /** 로또 구입 개수 메시지 반환 메서드 */
     public static final String PRINT_BUY_LOTTO (int lotto) {
