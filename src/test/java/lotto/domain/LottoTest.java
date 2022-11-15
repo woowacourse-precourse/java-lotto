a5bac14daba9fe94e07b09f45aed719e9bfbfbff
CAEL0 <kjongmin26@gmail.com>
2022-11-15 13:26:45 +0900

    test(LottoTest): 로또를 5개로 생성하면 예외 발생

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoTest {
+    @DisplayName("로또 번호의 개수가 5개면 예외가 발생한다.")
+    @Test
+    void createLottoWithFiveNumbers() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
