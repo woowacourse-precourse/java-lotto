22ca19ac7dbca3497cd4a525dfcc225bd3e9c400
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-15 19:53:11 +0900

    feat(LottoGame): 맞춘 번호 갯수, 보너스 일치 여부에따른 등수 처리

--- src/main/java/lotto/controller/Game.java
+++ src/main/java/lotto/controller/Game.java
@@ -1,7 +1,9 @@
 package lotto.controller;
 
+import lotto.domain.Grade;
 import lotto.domain.Lotto;
 import lotto.domain.LottoCashier;
+import lotto.domain.Referee;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -20,6 +22,9 @@ public class Game {
 
         Lotto winningNumbers = setWinningNumbers();
         int bonusNumber = setBonusNumber(winningNumbers);
+
+        Referee referee = new Referee(lottos,winningNumbers,bonusNumber);
+        List<Grade> grades = referee.calculateGrades();
 
     }
 
