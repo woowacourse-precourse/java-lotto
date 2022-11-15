c20e21c157c1fad3e9c945fc7b7a0bc96d36ada7
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-11-15 23:18:57 +0900

    feat(WinningLotto): WinningLotto 클래스 추가

---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningLotto extends Lotto {
+
+    private final Integer bonusNumber;
+
+    public WinningLotto(List<Integer> numbers, Integer bonusNumber) {
+        super(numbers);
+        validate(numbers, bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(List<Integer> numbers, Integer bonusNumber) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+
+        validateBonusNumber(bonusNumber);
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateBonusNumber(Integer bonusNumber) {
+
+        if (bonusNumber <= 0) {
+            throw new IllegalArgumentException("0이하의 숫자는 당첨번호가 될 수 없습니다.");
+        }
+
+        if (bonusNumber > 45) {
+            throw new IllegalArgumentException("45보다 큰 숫자는 당첨번호가 될 수 없습니다.");
+        }
+    }
+}
