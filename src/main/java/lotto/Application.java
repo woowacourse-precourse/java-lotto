2a2c861d4d542ae9284f96e9eebfdf3f12edfcfb
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-15 20:36:46 +0900

    feat(LottoGame): 메인 메서드에 예외 사항 처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,6 +6,9 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Game game = new Game();
-        game.startLottoGame();
+        try{
+            game.startLottoGame();
+        }catch (IllegalArgumentException ex) {
+        }
     }
 }
