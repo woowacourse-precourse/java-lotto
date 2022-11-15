1a69da615cae75ac8c4769ebda9fd9d130f5b936
vcho1958 <gusckd57@gmail.com>
2022-11-15 09:22:22 +0900

    feat(LottoManager): 로또 번호와 당첨번호, 보너스 번호를 비교하여 ENUM을 기준으로 Map에 저장하는 기능

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,9 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LottoManager {
@@ -11,38 +11,60 @@ public class LottoManager {
     private Money benefit;
     private final LottoList lottos = new LottoList();
     private AnswerNumberList answerNumbers;
-
-    private Long getAmountOfMoney(){
+    private final Map<LottoResult, Long> lottoResultMap = generateLottoResultMap();
+    private Long getAmountOfMoney() {
         String amountOfMoney = Console.readLine();
         try {
             return Long.valueOf(amountOfMoney);
-        }catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
         }
     }
 
-    private void pickLottoNumbers(){
+    private void pickLottoNumbers() {
         Long amountOfLotto = input.getAmountOfLotto();
-        for(int i = 0; i < amountOfLotto; i++){
+        for (int i = 0; i < amountOfLotto; i++) {
             this.lottos.generateLotto();
         }
-        System.out.println(amountOfLotto+"개를 구매했습니다.");
+        System.out.println(amountOfLotto + "개를 구매했습니다.");
     }
-    public void buyLotto(){
+
+    public void buyLotto() {
         Long amountOfMoney = getAmountOfMoney();
         this.input = new Money(amountOfMoney);
         pickLottoNumbers();
     }
 
-    private List<Long> parseAnswerNumbers(String answerNumberString){
+    private List<Long> parseAnswerNumbers(String answerNumberString) {
         return Arrays.stream(answerNumberString.split(",")).map(Long::valueOf).collect(Collectors.toList());
     }
 
-    public void generateAnswerNumbers(){
+    public void generateAnswerNumbers() {
         String answerNumberString = Console.readLine();
         List<Long> answerLongNumbers = parseAnswerNumbers(answerNumberString);
         String bonusNumberString = Console.readLine();
         Long bonusNumber = Long.valueOf(bonusNumberString);
         this.answerNumbers = new AnswerNumberList(answerLongNumbers, bonusNumber);
+    }
+
+    private Map<LottoResult, Long> generateLottoResultMap(){
+        return new java.util.HashMap<>(Map.of(
+                LottoResult.FIRST, 0L,
+                LottoResult.SECOND, 0L,
+                LottoResult.THIRD, 0L,
+                LottoResult.FOURTH, 0L,
+                LottoResult.FIFTH, 0L
+        ));
+    }
+
+    private void checkWinningsInLottoList(){
+        List<Lotto> lottoList = lottos.getLottoList();
+        for(var lotto: lottoList){
+            LottoResult lottoResult = answerNumbers.checkLottoWinning(lotto);
+            lottoResultMap.replace(lottoResult, lottoResultMap.get(lottoResult)+1);
+        }
+    }
+    public void printStatistics() {
+        checkWinningsInLottoList();
     }
 }
