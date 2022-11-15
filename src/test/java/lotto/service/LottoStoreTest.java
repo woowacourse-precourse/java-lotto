1f9e19fe961b9a5fe7d6c5bb9488496c873ecf4f
BaeDongHyeon <sasd730@gmail.com>
2022-11-15 18:12:12 +0900

    test: LottoStore 테스트 케이스 추가
    - 올바른 당첨 번호 입력 테스트
    - 당첨 번호가 정수가 아니거나 범위 안에 없을 시 테스트
    - 당첨 번호가 6개가 아닐 시 테스트

--- src/test/java/lotto/service/LottoStoreTest.java
+++ src/test/java/lotto/service/LottoStoreTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class LottoStoreTest {
@@ -25,6 +27,27 @@ public class LottoStoreTest {
     @Test
     @DisplayName("8080원을 입력하면 예외처리 되어야 한다.")
     void createLottosNotDivide() {
-        assertThatThrownBy(() -> lottoStore.buyLottos(new Money("8080"))).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> lottoStore.buyLottos(new Money("8080")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("올바른 당첨 번호를 입력하면 리스트와 일치해야 한다.")
+    void inputWinningNumberSuccess() {
+        assertThat(lottoStore.confirmWinningNumbers("1,2,3,4,5,6").toString()).isEqualTo(Arrays.asList(1,2,3,4,5,6).toString());
+    }
+
+    @Test
+    @DisplayName("정수가 아니거나 1~45의 범위에 없으면 예외처리 해야한다.")
+    void inputWinningNumberNotInRange() {
+        assertThatThrownBy(() -> lottoStore.confirmWinningNumbers("a,2,3,4,5,46"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("6개가 아니면 예외처리 해야한다.")
+    void inputWinningNumberCountNotSix() {
+        assertThatThrownBy(() -> lottoStore.confirmWinningNumbers("1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
