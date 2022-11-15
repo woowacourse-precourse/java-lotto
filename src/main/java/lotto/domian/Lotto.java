0bbee99956d137c85e9091e77e57793b5f05ccb0
eom-tae-in <eti0728@daum.net>
2022-11-15 18:54:52 +0900

    feat(Lotto): winningNumber와 bonusNumber getter 생성

--- src/main/java/lotto/domian/Lotto.java
+++ src/main/java/lotto/domian/Lotto.java
@@ -16,5 +16,17 @@ public class Lotto {
         userInputWinningNumberService.checkThisWinningNumberIsValid(winningNumberArray);
         this.numbers = ArrayAndListTwoSidedConversion.stringArrayToIntegerList(winningNumberArray);
     }
-
+    public List<Integer> getWinningNumbers() {
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (int i = 0; i < NUMBER_OF_LOTTO_NUMBERS; i++) {
+            winningNumbers.add(numbers.get(i));
+        }
+        return winningNumbers;
+    }
+    public int getBonusNumber() {
+        return numbers.get(BONUS_NUMBER_INDEX);
+    }
+    public void setBonusNumber(int bonusNumber) {
+        this.numbers.add(bonusNumber);
+    }
 }
