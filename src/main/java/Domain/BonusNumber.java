f167df9b2593fed24d05c8d463807a1e5dadd61e
nahyeon cho <whskgus12@nate.com>
2022-11-15 21:41:54 +0900

    feat(domain): 보너스 번호가 주어진 범위를 벗어났을 때
    보너스 번호가 주어진 범위가 아니라면 예외 처리하는 기능입니다. 생성자를 사용하여 BonusNumber 클래스의 함수를 사용하기 전에 미리 예외 사항을 체크하도록 하였습니다.

--- src/main/java/Domain/BonusNumber.java
+++ src/main/java/Domain/BonusNumber.java
@@ -2,12 +2,26 @@ package Domain;
 
 public class BonusNumber {
 
+    String bonusNumber;
+
+    public BonusNumber(String bonusNumber){
+        validInputBonusNumber(bonusNumber);
+        outOfRangeBonusNumber(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
     private void validInputBonusNumber(String bonus) {
         try {
             Integer.parseInt(bonus);
         } catch (Exception e) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력하세요.");
         }
+    }
+
+    private void outOfRangeBonusNumber(String bonus){
+        int number = Integer.parseInt(bonus);
+        if (number < 1 || number > 45)
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
 
 }
