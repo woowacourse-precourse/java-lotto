470f6f446fd1d8ef9b9ec590ade00550575e1d09
lhyeonj120 <isk04387@gmail.com>
2022-11-15 21:15:29 +0900

    style: 공백 수정

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -16,7 +16,7 @@ public class NumberGenerator {
 
     public NumberGenerator() {
     }
-    
+
     public NumberGenerator(int start, int end, int count) {
         numbers = Randoms.pickUniqueNumbersInRange(start, end, count);
     }
@@ -24,61 +24,61 @@ public class NumberGenerator {
     public List<Integer> getNumbers() {
         return numbers;
     }
-    
+
     public void setWinningNumber() {
         String input = inputWinningNumber();
         validateWinningNumber(input);
         String[] inputNumber = splitStr(input);
         numbers = new ArrayList<>();
-        for(int i = 0; i < inputNumber.length; i++) {
+        for (int i = 0; i < inputNumber.length; i++) {
             numbers.add(Integer.parseInt(inputNumber[i]));
         }
     }
-    
+
     private String inputWinningNumber() {
         return Console.readLine();
     }
-    
+
     private void validateWinningNumber(String str) {
-        if(!str.matches(regexWinningNumber)) {
+        if (!str.matches(regexWinningNumber)) {
             throw new IllegalArgumentException();
         }
         String[] inputNumber = splitStr(str);
         Set<Integer> set = new HashSet<>();
-        for(int i = 0; i < inputNumber.length; i++) {
+        for (int i = 0; i < inputNumber.length; i++) {
             set.add(Integer.parseInt(inputNumber[i]));
         }
-        if(set.size() != 6) {
+        if (set.size() != 6) {
             throw new IllegalArgumentException();
         }
-        for(Integer i : set) {
-            if(i < 1 || i > 45) {
+        for (Integer i : set) {
+            if (i < 1 || i > 45) {
                 throw new IllegalArgumentException();
             }
         }
     }
-    
+
     private String[] splitStr(String str) {
         String[] inputNumber = str.split(",");
         return inputNumber;
     }
-    
+
     private void setBonusNumber() {
         String str = Console.readLine();
         validateBonusNumber(str);
         bonusNumber = Integer.parseInt(str);
     }
-    
+
     private void validateBonusNumber(String str) {
-        if(!str.matches(regexBonusNumber)) {
+        if (!str.matches(regexBonusNumber)) {
             throw new IllegalArgumentException();
         }
         int bonusNum = Integer.parseInt(str);
-        if(bonusNum < 1 || bonusNum > 45) {
+        if (bonusNum < 1 || bonusNum > 45) {
             throw new IllegalArgumentException();
         }
     }
-    
+
     public int getBonusNumber() {
         return bonusNumber;
     }
