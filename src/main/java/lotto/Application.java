ea4b3db7d21ff75c34f7d069d08c01a5770823b1
chaeyeong222 <iky1505@naver.com>
2022-11-15 18:26:13 +0900

    feat : 사용자 입력 구입금액 값 오류시 예외처리 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,7 @@
 package lotto;
 
-import lotto.domain.BonusNumber;
-import lotto.domain.Money;
-import lotto.domain.InputNumber;
+import lotto.model.*;
+import lotto.utils.Error;
 import lotto.view.FinalResult;
 import lotto.view.Print;
 
@@ -10,45 +9,45 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import static lotto.domain.Money.numberOfLottos;
-import static lotto.domain.InputNumber.*;
+import static lotto.model.Money.numberOfLottos;
+import static lotto.model.InputNumber.*;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        // TODO: 구입금액 입력
+        //TODO: 프로그램 구현
         Print.requestMoneyMessage();
-        Money.getMoney();
+        try{
+            Money.getMoney();
+        }catch (IllegalArgumentException e){
+            System.out.println("[ERROR] 숫자가 아닌 값을 포함하고 있습니다.");
+            return;
+        }
         Print.howmanyLottos(numberOfLottos);
 
         Compare.randomLottos = new HashMap<>();
         Compare.userList = new ArrayList<>();
 
-        // TODO: 구매 장수만큼 로또 번호 출력
         Lotto.getAllLottos();
 
-
-        //TODO: 사용자로부터 당첨번호 입력받기
         Print.requestWinNumbers();
+        //InputNumber.getWinNumbers();
 
         List<Integer> correctNumbers = InputNumber.getWinNumbers();
         for (int i = 0; i < correctNumbers.size(); i++) {
             Compare.userList.add(correctNumbers.get(i));
         }
-
         Print.winNumbers();
         Print.requestBonusNumber();
         InputNumber.getBonusNumber();
         InputNumber.bonusNumber = bonusNumber;
         Print.bonusNumber();
 
-        // 결과 비교
+        //TODO: 결과 비교
         Compare.numbers();
         BonusNumber.compare();
         Calculate.getResult();
 
+        //TODO: 최종출력
         FinalResult.printFinalResult();
-
-
     }
 }
--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -1,43 +1,30 @@
-package lotto.domain;
+package lotto.model;
 
 import camp.nextstep.edu.missionutils.Console;
-
-import static java.lang.Error.*;
+import lotto.utils.Error;
 
 public class Money {
     public static int money;
     public static int numberOfLottos;
 
-    public static int getMoney() throws IllegalStateException{
+    public static int getMoney() throws IllegalArgumentException{
         String playerInput = Console.readLine();
         playerInput = playerInput.trim();
-
         validateNumbers(playerInput);
-
         money = Integer.parseInt(playerInput);
         validate(money);
         numberOfLottos = money / 1000;
         return money;
     }
 
-    private static void validate(int money) throws IllegalStateException{
+    private static void validate(int money) throws IllegalArgumentException{
         if (money % 1000 != 0) {
             //throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 나누어 떨어져야 합니다.");
-            throw new IllegalArgumentException(Error.INVALID_NUMBER_COUNT.getMessage());
+            throw new IllegalArgumentException(Error.INPUT_MONEY_CHECK.getMessage());
         }
     }
 
-    /*
-        private static void validateNumbers(String playerInput){
-            try{
-                Integer.parseInt(playerInput);
-            }catch (Exception e){
-                throw new IllegalArgumentException(Error.INVALID_NUMBER_COUNT.getMessage());
-            }
-
-        }
-    */
-    private static boolean validateNumbers(String playerInput) throws IllegalStateException{
+    private static boolean validateNumbers(String playerInput) throws IllegalArgumentException{
         try {
             Integer.parseInt(playerInput);
             return true;
