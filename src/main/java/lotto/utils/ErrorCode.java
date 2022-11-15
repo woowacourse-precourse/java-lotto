2beac1e91f42f02477fe3634010729da8a7ef677
limjimin <dkdldsla0000@naver.com>
2022-11-15 23:35:37 +0900

    Feat: 플레이어 결과 저장 - 예외 코드 보완

--- src/main/java/lotto/utils/ErrorCode.java
+++ src/main/java/lotto/utils/ErrorCode.java
@@ -3,7 +3,8 @@ package lotto.utils;
 public enum ErrorCode {
     INVALID_SIZE("입력받은 수가 6자리 수가 아닙니다."),
     DUPLICATE_NUMBER("중복되는 숫자가 존재합니다."),
-    INVALID_RANGE("가능한 범위는 1 ~ 45 입니다.");
+    INVALID_RANGE("가능한 범위는 1 ~ 45 입니다."),
+    INVALID_PRICE("적절한 입력 가격이 아닙니다.");
 
     private final String message;
 
