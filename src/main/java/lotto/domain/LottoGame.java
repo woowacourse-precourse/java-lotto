cc84a57ab1d1a44c7a39e0477fb6683a922b00bb
yummygyudon <bang2brew@gmail.com>
2022-11-15 14:45:58 +0900

    FEAT(domain): Lotto 메인 게임 실행 기능 구현
    - 클래스 명 : LottoGame
    - 활용 메서드
      1. LottoConverter
      2. LottoGenerator
      3. LottoCalculator
      4. Input
      5. Output
    - 전체 기능 구현 클래스를 활용하여 메일 로직 구현

---
+++ src/main/java/lotto/domain/LottoGame.java
@@ -0,0 +1,61 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.Lotto;
+import lotto.ui.Input;
+import lotto.ui.Output;
+
+public class LottoGame {
+    private final LottoConverter numbersConverter;
+    private final LottoGenerator numbersGenerator;
+    private final LottoCalculator lottoCalculator;
+
+    private final Input input;
+    private final Output output;
+
+    public LottoGame() {
+        numbersConverter = new LottoConverter();
+        numbersGenerator = new LottoGenerator();
+        lottoCalculator = new LottoCalculator();
+        input = new Input();
+        output = new Output();
+    }
+
+    public void run() {
+        long playerMoney = numbersConverter.convertMoney(input.scanMoney());
+        int playerLottoQuantity = lottoCalculator.getLottoQuantity(playerMoney);
+
+        List<Lotto> playerLotto = numbersGenerator.getLottos(playerLottoQuantity);
+        output.printPlayerLotto(playerLottoQuantity, playerLotto);
+
+        List<Integer> winningLotto = numbersConverter.convertScanNumbersToWinningNumbers(input.scanNumbers());
+        int bonusNumber = numbersConverter.convertBonus(input.scanBonus());
+
+        Map<Integer, Integer> resultBoard = getStatistic(winningLotto, bonusNumber, playerLotto);
+        output.printStatistic(resultBoard);
+
+        long rewardMoney = lottoCalculator.getRewardMoney(resultBoard);
+        double rate = lottoCalculator.getRate(rewardMoney, playerMoney);
+        output.printRate(rate);
+    }
+
+    private Map<Integer, Integer> getStatistic(List<Integer> winningNumbers, int bonusNumber, List<Lotto> playerLotto) {
+        Map<Integer, Integer> resultMap = new HashMap<>() {{
+            put(1, 0);
+            put(2, 0);
+            put(3, 0);
+            put(4, 0);
+            put(5, 0);
+        }};
+        for (Lotto lotto : playerLotto) {
+            int equalsCount = lottoCalculator.getEqualsCount(winningNumbers, bonusNumber, lotto);
+            if (equalsCount >= 3) {
+                int rank = lottoCalculator.getRank(equalsCount);
+                resultMap.put(rank, resultMap.get(rank) + 1);
+            }
+        }
+        return resultMap;
+    }
+}
