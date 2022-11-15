a36a892875b3f0f64230d7b0d758df4eafed95c9
ddukddaki <rlfrkdms1@naver.com>
2022-11-15 17:05:29 +0900

    feat(LottoRank): 등수별로 조건(일치한 당첨 번호 개수, 보너스 번호 일치 여부, 당첨금, 내역)을 나열하는 기능

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+public enum LottoRank {
+    LOSE(0, false, 0, ""),
+    FIFTH(3, false, 5_000, "3개 일치 (5,000원)"),
+    FOURTH(4, false, 50_000, "4개 일치 (50,000원)"),
+    THIRD(5, false, 1_500_000, "5개 일치 (1,500,000원)"),
+    SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    FIRST(6, false, 2_000_000_000, "6개 일치 (2,000,000,000원)");
+
+    private final int winningNumberCount;
+    private final boolean hasBonusNumber;
+    private final int prizeMoney;
+    private final String description;
+
+    LottoRank(int winningNumberCount, boolean hasBonusNumber, int prizeMoney, String description) {
+        this.winningNumberCount = winningNumberCount;
+        this.hasBonusNumber = hasBonusNumber;
+        this.prizeMoney = prizeMoney;
+        this.description = description;
+    }
+}
