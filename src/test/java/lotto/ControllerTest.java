3a0e2157388a8f379ce111e19bcd223c7880c3a1
Frog-Slayer <Frog-Slayer@github.com>
2022-11-15 20:27:40 +0900

    test(Controller): 추가 bonus 입력 테스트

--- src/test/java/lotto/ControllerTest.java
+++ src/test/java/lotto/ControllerTest.java
@@ -3,14 +3,12 @@ package lotto;
 import lotto.UI.View;
 import lotto.domain.Game;
 import lotto.domain.User;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ControllerTest {
@@ -19,13 +17,14 @@ class ControllerTest {
     OutputStream outputStream = new ByteArrayOutputStream();
 
     @BeforeEach
+    @DisplayName("아래 테스트들은 모두 domain 패키지 클래스의 예외 처리까지 포함한다.")
     @Test
     void beforeEach() {
         controller = new Controller(new Game(), new User(), new View());
         System.setOut(new PrintStream(outputStream));
     }
 
-    @DisplayName("정수가 아닌 구매 금액을 입력했을 시 예외가 발생한다. ")
+    @DisplayName("정수가 아닌 구매 금액을 입력했을 시 예외가 발생한다.")
     @Test
     public void setBudget() {
         String input = "예외 발생";
@@ -43,19 +42,13 @@ class ControllerTest {
         assertThatThrownBy(() -> controller.setWinningNumbers()).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("입력이 정수가 아닐 경우 예외가 발생한다.")
     @Test
     void setBonus() {
+        String input = "예외 발생";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+        assertThatThrownBy(() -> controller.setBudget()).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void buyLotto() {
-    }
-
-    @Test
-    void computeStatistics() {
-    }
-
-    @Test
-    void showStatistics() {
-    }
 }
