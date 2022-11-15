7d909d18d6c2a1ca8e5b3dc55748b0bb1a8aee63
Giho <poqw741@gmail.com>
2022-11-15 21:28:43 +0900

    feat(lotto) : 당첨 개수 확인 및 오류 메시지 등 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static lotto.util.ErrorMessage.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,9 +14,43 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        Boolean[] isIncluded = new Boolean[46];
+        Arrays.fill(isIncluded, false);
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NUMBERS_COUNT_ERROR_MESSAGE.getMessage());
         }
+    }
+
+    private void checkNumberRange(List<Integer> numbers, Boolean[] isIncluded) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(NUMBER_RANGE_ERROR_MESSAGE.getMessage());
+            }
+            if (isIncluded[number]) {
+                throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR_MESSAGE.getMessage());
+            }
+            isIncluded[number] = true;
+        }
+    }
+
+    public List<Integer> getNumbers(){
+        return this.numbers;
+    }
+
+    public int checkWinning(List<Integer> winningNumbers, int bonusNumber) {
+        boolean[] isIncluded = new boolean[46];
+        winningNumbers.forEach(num -> isIncluded[num] = true);
+        int count = (int) this.numbers.stream().filter(num -> isIncluded[num]).count();
+        if (count < 3) {
+            return 0;
+        }
+        if (count == 6) {
+            return 1;
+        }
+        if (count == 5 && this.numbers.contains(bonusNumber)) {
+            return 2;
+        }
+        return 6 - count + 2;
     }
 
     // TODO: 추가 기능 구현
