2fc6e1148312fa6145729cd6c1b8f2953fdbb33f
JJ <jeongjae1996@naver.com>
2022-11-15 13:18:03 +0900

    feat: UI 로직을 분리하기 위해 출력 부분을 Output 클래스를 통해 기능 구현

---
+++ src/main/java/lotto/Output.java
@@ -0,0 +1,41 @@
+package lotto;
+
+public class Output {
+    public static final String BLANK_LINE = "";
+    public static final String PURCHASE_EVENT_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String AFTER_PURCHASE_EVENT_MESSAGE = "개를 구매했습니다.";
+    public static final String WIN_NUMBER_EVENT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_EVENT_MESSAGE = "보너스 번호를 입력해 주세요.";
+    public static final String WIN_RECORD_TITLE = "당첨 통계\n---";
+    public static final String PROFIT_EVENT_MESSAGE = "총 수익률은 $profit%입니다.";
+    public static final String REPLACE_PROFIT_FROM = "$profit";
+
+    public void blankLine() {
+        System.out.println(BLANK_LINE);
+    }
+
+    public void purchaseEventMessage() {
+        System.out.println(PURCHASE_EVENT_MESSAGE);
+    }
+
+    public void afterPurchaseEventMessage(int number) {
+        System.out.println(number + AFTER_PURCHASE_EVENT_MESSAGE);
+    }
+
+    public void winNumberEventMessage() {
+        System.out.println(WIN_NUMBER_EVENT_MESSAGE);
+    }
+
+    public void bonusNumberEventMessage() {
+        System.out.println(BONUS_NUMBER_EVENT_MESSAGE);
+    }
+
+    public void winRecordEventMessage() {
+        System.out.println(WIN_RECORD_TITLE);
+
+    }
+
+    public void profitEventMessage(float profit) {
+        System.out.println(PROFIT_EVENT_MESSAGE.replace(REPLACE_PROFIT_FROM, String.format("%.1f", profit)));
+    }
+}
