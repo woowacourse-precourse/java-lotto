a29b7bcea9200eadc86d07853a09bf017531ea46
starwook <star.wookk@gmail.com>
2022-11-15 16:42:34 +0900

    refactor(Game.java): Refactor CheckIfDivideWithThousand.java
    기존 메소드에서 숫자가 아닌 문자를 입력시 NumberFormatException 에러를 잡아
    IllegalArgumentException을 날리도록 변경하라
    최종적으로 1000으로 나누어 진다면 그 몫을 반환하라

--- src/main/java/lotto/domain/Game.java
+++ src/main/java/lotto/domain/Game.java
@@ -5,10 +5,19 @@ import camp.nextstep.edu.missionutils.Console;
 public class Game {
 
     public static final String notThousand =" 구입금액은 천원 단위여야합니다.";
+    public static final String notNumber =" 구입금액은 숫자여야합니다.";
+    public static final int unit = 1000;
 
-    public void checkIfDivideWithThousand(int purchase){
-        if(purchase%1000 !=0){
-            throw new IllegalArgumentException(ErrorResource.errorStart+notThousand);
+    public int checkIfDivideWithThousand(String purchase){
+        try{
+            int purchaseBill = Integer.parseInt(purchase);
+            if(purchaseBill%unit !=0){
+                throw new IllegalArgumentException(ErrorResource.errorStart+notThousand);
+            }
+            return purchaseBill/unit;
+        }
+        catch(NumberFormatException e){
+            throw new IllegalArgumentException(ErrorResource.errorStart+notNumber);
         }
     }
 
