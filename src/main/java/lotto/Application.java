465e16404dd650b0ae6eccbeb169f915750c8c96
seo <fungod19@gmail.com>
2022-11-15 20:03:11 +0900

    feat: 메인에서 번호 리스트 뽑기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
+import lotto.domain.LottoGenerator;
+
+import java.util.List;
 
 public class Application {
+    
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        List<List<Integer>> numList = null;
+        int count = lottoGenerator.count;
+
+        try{
+            for(int i =0 ; i< count; i++){
+                lottoGenerator.LottoNumbers();
+            }
+
+
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
     }
 }
