ba617c02843250924eed313f020985f23d734d02
jmjmjames <chungjm0711@gmail.com>
2022-11-15 11:58:00 +0900

    feat(LottoService): matchCount, bonusNumberHit을 통해 올바른 랭킹 값을 얻는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,5 +1,6 @@
 package lotto.service;
 
+import java.util.List;
 import lotto.Lotto;
 import lotto.domain.LottoRank;
 import lotto.domain.User;
@@ -7,13 +8,28 @@ import lotto.domain.WinningLotto;
 
 public class LottoService {
 
+    public static final Boolean BONUS_NUMBER_HIT = true;
+    public static final Boolean BONUS_NUMBER_MISS = false;
+
     public void calculateLottoRanks(User user, WinningLotto winningLotto) {
         for (Lotto purchasedLotto : user.getLottos()) {
-            calculateLottoRank(purchasedLotto, winningLotto);
+            LottoRank lottoRank = calculateLottoRank(purchasedLotto, winningLotto);
+            user.getLottoRanks().add(lottoRank);
         }
     }
 
     private LottoRank calculateLottoRank(Lotto purchasedLotto, WinningLotto winningLotto) {
-        return LottoRank.FIRST;
+        List<Integer> purchasedLottoNumbers = purchasedLotto.getNumbers();
+        List<Integer> winningLottoNumbers = winningLotto.getNumbers();
+        int matchCount = (int) purchasedLottoNumbers.stream()
+                .filter(winningLottoNumbers::contains)
+                .count();
+
+        Integer bonusNumber = winningLotto.getBonusNumber();
+        if (purchasedLottoNumbers.contains(bonusNumber)) {
+            return LottoRank.getLottoRank(matchCount, BONUS_NUMBER_HIT);
+        }
+        return LottoRank.getLottoRank(matchCount, BONUS_NUMBER_MISS);
     }
+
 }
