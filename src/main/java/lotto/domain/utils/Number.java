7c8d6a1bdef429ac12ed2280c40eb1d24c479bcb
kimdoha <kkhhjj3393@gmail.com>
2022-11-15 16:34:35 +0900

    refactor(Number): LottoNumber Enum 클래스에 따른 코드 개선

--- src/main/java/lotto/domain/utils/Number.java
+++ src/main/java/lotto/domain/utils/Number.java
@@ -1,12 +1,14 @@
 package lotto.domain.utils;
 
 
+import lotto.domain.lotto.Lotto;
+
 public class Number {
 
   public static final String ERROR_MESSAGE_OF_NUMBER_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 
   public static void validateRange(final Integer number) {
-    if(number < 1 || number > 45) {
+    if(number < Lotto.firstLottoNumber() || number > Lotto.lastLottoNumber()) {
       throw new IllegalArgumentException(ERROR_MESSAGE_OF_NUMBER_RANGE);
     }
   }
