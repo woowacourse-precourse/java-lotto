ff1f381241f6f714ecdd1769c4da2359e633345c
namtndus <rnrmf0721@gmail.com>
2022-11-15 19:12:29 +0900

    feat(CalculatedService): calculatedYield함수 구현
    로또의 당첨된 금액에 따른 수익률을 구하는 함수 기능 구현

--- src/main/java/lotto/Services/CalculatedService.java
+++ src/main/java/lotto/Services/CalculatedService.java
@@ -1,6 +1,9 @@
 package lotto.Services;
 
 import lotto.Dto.Money;
+import lotto.Dto.Reword;
+
+import java.util.Map;
 
 public class CalculatedService {
 
@@ -12,5 +15,15 @@ public class CalculatedService {
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 돈을 입력하시오");
         }
         return this.money / 1000;
+    }
+
+    public String calculatedYield(Map<String,Integer> lottoResult){
+        float total = lottoResult.get(Reword.FIVE_WORD.word) * Reword.FIVE_WORD.money
+                + lottoResult.get(Reword.FOUR_WORD.word) * Reword.FOUR_WORD.money
+                + lottoResult.get(Reword.THREE_WORD.word) * Reword.THREE_WORD.money
+                + lottoResult.get(Reword.TWO_WORD.word) * Reword.TWO_WORD.money
+                + lottoResult.get(Reword.ONE_WORD.word) * Reword.ONE_WORD.money;
+
+        return String.format("%.2f",total/money);
     }
 }
