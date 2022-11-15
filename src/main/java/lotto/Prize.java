f2454b49f5e3018c46a34d6a2b4a52bf9d534777
youswim <youswim96@gmail.com>
2022-11-15 15:27:48 +0900

    feat(lotto): 로또 당첨 등수를 위한 Prize클래스 생성
    로또 당첨 등수를 Prize클래스에 Enum클래스로 생성

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,5 +1,18 @@
 package lotto;
 
 public enum Prize {
+    FIRST("6개 일치 (2,000,000,000원)"),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD("5개 일치 (1,500,000원)"),
+    FOURTH("4개 일치 (50,000원)"),
+    FIFTH("3개 일치 (5,000원)");
 
+    private final String attribute;
+    Prize(String attribute) {
+        this.attribute = attribute;
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
 }
