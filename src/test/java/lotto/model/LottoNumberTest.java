f87e624d42a503362d0d2f938f96e7d4a6213354
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-15 19:33:11 +0900

    test<LottoNumber>: 문자열 1~45 범위가 아니라면 예외 발생 테스트 작성

--- src/test/java/lotto/model/LottoNumberTest.java
+++ src/test/java/lotto/model/LottoNumberTest.java
@@ -11,4 +11,13 @@ class LottoNumberTest {
         Assertions.assertThatThrownBy(() -> new LottoNumber(0)).isInstanceOf(IllegalArgumentException.class);
         Assertions.assertThatThrownBy(() -> new LottoNumber(46)).isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("문자열 1~45 범위가 아니라면 예외가 발생한다.")
+    @Test
+    void createBonusNumberByNumberRange() {
+        Assertions.assertThatThrownBy(() -> new LottoNumber("a")).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> new LottoNumber("")).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> new LottoNumber("0")).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> new LottoNumber("46")).isInstanceOf(IllegalArgumentException.class);
+    }
 }
