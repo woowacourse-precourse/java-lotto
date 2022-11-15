507bcac0c24613e53240592e50a14e7c0b316f30
hgleeee <94894328+hgleeee@users.noreply.github.com>
2022-11-15 18:24:03 +0900

    당첨 번호가 1부터 45 사이의 숫자여야 하는 제한 기능 추가

--- src/main/java/domain/WinningNumber.java
+++ src/main/java/domain/WinningNumber.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 public class WinningNumber {
 
+    private static final String COMMON_ERROR_MESSAGE = "[ERROR] 입력이 올바르지 않습니다.";
     private final Set<Integer> numbers;
     private final int bonusNumber;
 
@@ -26,9 +27,10 @@ public class WinningNumber {
         Set<Integer> set = new HashSet<>();
         String[] num = input.split(",");
         if (num.length != 6) {
-            throw new IllegalArgumentException("[ERROR] 입력이 올바르지 않습니다.");
+            throw new IllegalArgumentException(COMMON_ERROR_MESSAGE);
         }
         for (String s : num) {
+            validateNumberBetweenRange(s, 1, 45);
             set.add(Integer.valueOf(s));
         }
         if (set.size() != 6) {
@@ -37,6 +39,19 @@ public class WinningNumber {
         return set;
     }
 
+    private void validateNumberBetweenRange(String s, int min, int max) {
+        if (s.length() != 1 && s.length() != 2) {
+            throw new IllegalArgumentException(COMMON_ERROR_MESSAGE);
+        }
+        for (int i = 0; i < s.length(); ++i) {
+            if (!Character.isDigit(s.charAt(i))) {
+                throw new IllegalArgumentException("[ERROR] 각 당첨 번호는 숫자여야 합니다.");
+            }
+        }
+        if (Integer.valueOf(s) < min || Integer.valueOf(s) > max) {
+            throw new IllegalArgumentException("[ERROR] 각 당첨 번호는 " + min + "부터 " + max + " 사이의 숫자여야 합니다.");
+        }
+    }
 
     private void validateBonusNumber(int bonusNumber) {
         if (numbers.contains(bonusNumber)) {
