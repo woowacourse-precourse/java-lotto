8b344a310fa478c4c55439b1b32ad426cb6507fe
HangSub999 <gkdtjq999@gmail.com>
2022-11-15 19:36:55 +0900

    feat: 로또 등수가격 getter구현

--- src/main/java/lotto/LottoRanking.java
+++ src/main/java/lotto/LottoRanking.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 
 public enum LottoRanking {
     FIRST(6, "6개 일치", 2000000000),
-    SECOND(5, "5개 일치, 보너스 볼 일치", 300000000),
+    SECOND(5, "5개 일치, 보너스 볼 일치", 30000000),
     THIRD(5, "5개 일치", 1500000),
     FOURTH(4, "4개 일치", 50000),
     FIFTH(3, "3개 일치", 5000),
@@ -27,6 +27,10 @@ public enum LottoRanking {
                 return lottoRanking;
         }
         return LottoRanking.NONE;
+    }
+
+    public int getLottoRankingMoney() {
+        return lottoRankingMoney;
     }
 
     public String getDescription() {
