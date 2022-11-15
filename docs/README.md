358756da64d696555ffae3355db501a5b1889292
hanuel <ezzeezz@naver.com>
2022-11-15 11:25:06 +0900

    feat(lotto): 당첨 번호 입력 받는 기능 구현

--- docs/README.md
+++ docs/README.md
@@ -14,7 +14,7 @@
 - [x] 중복되지 않는 6개의 숫자 뽑기 - NumberGenerator
 
 - [ ] 당첨 번호 추첨 (중복되지 않는) - LottoHeadOffice
-    - [ ] 당첨 번호 입력 받기 (6개) - LottoHeadOffice#inputWinningNum
+    - [x] 당첨 번호 입력 받기 (6개) - LottoHeadOffice#inputWinningNum
     - [ ] 보너스 번호 입력 받기 (1개) - LottoHeadOffice#inputBonusNum
 
 - [ ] 당첨 금액 확인 - LottoResult
--- src/main/java/lotto/LottoHeadOffice.java
+++ src/main/java/lotto/LottoHeadOffice.java
@@ -1,15 +1,23 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoHeadOffice {
-    private List<Integer> winningNum;
+    private List<Integer> winningNum = new ArrayList<>();
     private int bonusNum;
 
     LottoHeadOffice() {
     }
 
     public void inputWinningNum() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String s = Console.readLine();
+        for (String digit : s.split(",")) {
+            this.winningNum.add(Integer.valueOf(digit));
+        }
     }
 
     public void inputBonusNum() {
