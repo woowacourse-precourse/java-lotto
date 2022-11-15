8048599d1774f3d4417d1ec32b6edb8787434f9c
hwanee__ <fkfkthth@naver.com>
2022-11-15 23:18:47 +0900

    feat: 예외 처리 메시지를 위한 try & catch

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,7 +7,12 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        LottoGame lottoGame = new LottoGame();
-        lottoGame.startLotto();
+        try{
+            LottoGame lottoGame = new LottoGame();
+            lottoGame.startLotto();
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
