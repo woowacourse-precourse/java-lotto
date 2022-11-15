3d5792f3679f3be859b904e3e37ed3903d4a9a1e
hdsung98 <hdsung98@gmail.com>
2022-11-15 23:24:17 +0900

    test: create tests of LottoFactory, LottoRank, WinningNumberTest

---
+++ src/test/java/lotto/domain/LottoFactoryTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoFactoryTest {
+    @Test
+    void 예외_테스트() {
+
+        assertThatThrownBy(()->LottoFactory.makeLottos(8500))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Invalid Argument");
+    }
+    @Test
+    void 정상_개수_생성_테스트(){
+        Lottos lottos = LottoFactory.makeLottos(6000);
+        assertThat(lottos.getSize())
+                .isEqualTo(6);
+    }
+}
