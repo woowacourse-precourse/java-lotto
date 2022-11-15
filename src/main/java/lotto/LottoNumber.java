2de11cd731881dc4b9266bf4ded37f91c534333d
gichan-song <thdrlcks97@gmail.com>
2022-11-15 22:58:32 +0900

    feat(LottoNumber): 당첨 번호 입력받기 기능
    LottoNumber 클래스 새로운 메서드 추가:
    생성자 - 6개의 숫자 쉼표로 구분하여 입력받기
    보너스 번호 입력받아 1~45 사이에 있는지 확인
    보너스 번호가 6개의 당첨번호에 포함되어있는지 확인

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class LottoNumber {
+
+  private List<Integer> Lottonumber;
+
+  LottoNumber(){
+    String[] numbers = Console.readLine().split(",");
+    for(int i=0; i<6; i++){
+      Lottonumber.add(Integer.parseInt(numbers[i]));
+    }
+    new Lotto(Lottonumber);
+    int bonusnumber = Integer.parseInt(Console.readLine());
+    if(bonusnumber < 1 || bonusnumber > 45){
+      throw new IllegalArgumentException();
+    }
+    if(Lottonumber.contains(bonusnumber)){
+      throw new IllegalArgumentException();
+    }
+  }
+}
