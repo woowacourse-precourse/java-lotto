78b9fc564de6546c8edbc179949414a0ea8e9465
mikekang47 <gyeong0308@gmail.com>
2022-11-15 18:09:54 +0900

    Feat<Validator>: 로또 번호 예외 처리 로직 추가

--- src/main/java/lotto/utils/Validator.java
+++ src/main/java/lotto/utils/Validator.java
@@ -1,9 +1,28 @@
 package lotto.utils;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Validator {
     public static void validateMoney(int money) {
         if(money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 반드시 1,000원 단위여야 합니다.");
+        }
+    }
+
+    public static void validateLottoNumber(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 갯수는 반드시 6개여야 합니다.");
+        }
+
+        if (numbers.get(0) < 1 || numbers.get(numbers.size() - 1) > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 반드시 1과 45 사이의 숫자여야합니다.");
+        }
+
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if(numberSet.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 }
