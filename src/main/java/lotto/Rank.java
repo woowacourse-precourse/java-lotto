d9c0c9ac7fcbb5c4be344578cf5a84b23fe70794
thdwoqor <celebarte01@gmail.com>
2022-11-15 17:43:32 +0900

    feat: 결과값 출력을 위해 `text` 추가

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -3,19 +3,21 @@ package lotto;
 import java.util.Arrays;
 
 public enum Rank {
-    FAIL(new Result(0, false), 0),
-    FIVE(new Result(3, false), 5_000),
-    FOUR(new Result(4, false), 50_000),
-    THREE(new Result(5, false), 1_500_000),
-    TWO(new Result(5, true), 30_000_000),
-    ONE(new Result(6, false), 2_000_000_000);
+    FAIL(new Result(0, false), 0,""),
+    FIVE(new Result(3, false), 5_000,"3개 일치 (5,000원) - "),
+    FOUR(new Result(4, false), 50_000, "4개 일치 (50,000원) - "),
+    THREE(new Result(5, false), 1_500_000,"5개 일치 (1,500,000원) - "),
+    TWO(new Result(5, true), 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    ONE(new Result(6, false), 2_000_000_000, "6개 일치 (2,000,000,000원) - ");
 
     private final Result result;
     private final int prize;
+    private final String text;
 
-    Rank(final Result result, final int prize) {
+    Rank(final Result result, final int prize,final String text) {
         this.result = result;
         this.prize = prize;
+        this.text = text;
     }
 
     public static Rank findByRank(final Result result) {
@@ -38,5 +40,9 @@ public enum Rank {
 
     public int getPrize() {
         return prize;
+    }
+
+    public String getText() {
+        return text;
     }
 }
