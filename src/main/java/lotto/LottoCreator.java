649f0a8499e33d22882f57e269b0f4cfc813505d
BoyCho <iijokv@naver.com>
2022-11-15 19:15:53 +0900

    fix : 로또 번호 오름차순 기능 오류 수정  - 함수 지원 가능 자료형으로 수정

--- src/main/java/lotto/LottoCreator.java
+++ src/main/java/lotto/LottoCreator.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,7 +11,7 @@ import constant.Message;
 public class LottoCreator {
 
     public List<Integer> createNumbers() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(Message.SMALLESTNUMBER, Message.LARGESTNUMBER, Message.LOTTOLENGTH);
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(Message.SMALLESTNUMBER, Message.LARGESTNUMBER, Message.LOTTOLENGTH));
         /*
             pickUniqueNumbersInRange(int a, int b, int c) : 한 리스트에 a ~ b 까지 숫자를 삽입한 후,
             그 리스트를 c 크기로 자른 후 Collections.Shuffle() 함수를 사용하여 무작위 구성의 형태로 반환하므로, 중복 없는 무작위 배열이 반환됨.
