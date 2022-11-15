a75366c32a7384ab0c9e9a584418e11f9f5dab5c
“rotomoo” <quddlf410@naver.com>
2022-11-15 20:03:12 +0900

    feat(PlusLotto) : 보너스 번호의 중복, 범위를 검사하는 기능 구현

--- src/main/java/lotto/domain/PlusLotto.java
+++ src/main/java/lotto/domain/PlusLotto.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import lotto.exception.Check;
+import lotto.exception.CustomException;
+
 import java.util.List;
 
 public class PlusLotto {
@@ -9,10 +12,18 @@ public class PlusLotto {
         this.plusNumber = plusNumber;
     }
 
-    private void duplicate(List<Integer> numbers) {
+    private void duplicate(List<Integer> numbers, int plusNumber) {
+        Check check = new Check();
+        if (!check.checkDuplicate(numbers, plusNumber)) {
+            throw new IllegalArgumentException(CustomException.DUPLICATE.getMessage());
+        }
     }
 
-    private void range(List<Integer> numbers) {
+    private void range(int plusNumber) {
+        Check check = new Check();
+        if (!check.checkRange(plusNumber)) {
+            throw new IllegalArgumentException(CustomException.RANGE.getMessage());
+        }
     }
 
     public int getPlusNumber() {
