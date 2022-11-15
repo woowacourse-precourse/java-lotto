b78087c13f86914479542f285c8ce32a61fa7340
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-11-15 21:37:40 +0900

    feat: 메소드 분리
    startGame()을 도입하여 생성자에 들어있던 기능을 단독 메소드로 옮김. 또한 로또 당첨번호를 입력 받는 메소드를 만들어 검증과 입력을 분리함

--- src/main/java/lotto/UserLotto.java
+++ src/main/java/lotto/UserLotto.java
@@ -6,29 +6,23 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 public class UserLotto {
 
     private int money;
     private int lottoCount;
-    private List<Lotto> lottos;
+    private List<Lotto> lottoBought;
 
     public UserLotto() {
-        validateMoneyInput();
-        setLottoCount();
 
-        buyLotto();
-        printLottoResult();
-        printProfit();
     }
 
     public void validateMoneyInput() {
         try {
-            money = Integer.parseInt(readLine());
+            money = Integer.parseInt(getMoneyInput());
         } catch (NumberFormatException e) {
-            throw new NoSuchElementException("[ERROR] 숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
         }
         if (money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
@@ -38,6 +32,10 @@ public class UserLotto {
         }
     }
 
+    public String getMoneyInput() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return readLine();
+    }
 
     public void setLottoCount() {
         this.lottoCount = money / 1000;
@@ -45,20 +43,20 @@ public class UserLotto {
 
     public void buyLotto() {
         WinningNumber winningNumber = new WinningNumber();
-        this.lottos = new ArrayList<>();
+        this.lottoBought = new ArrayList<>();
         System.out.println(lottoCount + "개를 구매했습니다.");
         for (int i = 0; i < lottoCount; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Lotto lotto = new Lotto(numbers);
             lotto.setWinningNumber(winningNumber);
             System.out.println(lotto.getNumbers());
-            lottos.add(lotto);
+            lottoBought.add(lotto);
         }
     }
 
 
     public Map<Rank, Long> getLottoRankCount() {
-        return lottos.stream()
+        return lottoBought.stream()
                 .map(Lotto::lottoRank)
                 .collect(Collectors.groupingBy(rank -> rank, Collectors.counting()));
     }
@@ -86,5 +84,13 @@ public class UserLotto {
         System.out.println("총 수익률은 " + String.format("%.1f", profit * 100) + "%" + "입니다.");
     }
 
+    public void startGame() {
+        validateMoneyInput();
+        setLottoCount();
+
+        buyLotto();
+        printLottoResult();
+        printProfit();
+    }
 
 }
