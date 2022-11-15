0780fa6dc49d1de4e066c42baf6c705d24236843
Sinryuji <siler2474@gmail.com>
2022-11-15 14:31:39 +0900

    feat(Output): 통계 및 수익률 출력 기능 구현

--- src/main/java/lotto/ui/Output.java
+++ src/main/java/lotto/ui/Output.java
@@ -2,10 +2,12 @@ package lotto.ui;
 
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.Rank;
 
 public class Output {
     private static final String buyLottoMessage = "개를 구매했습니다.";
     private static final String statisticsMessage = "당첨통계\n---";
+    private static final String revenueMessage = "총 수익률은 ";
 
     public static void outputBuyLotto(List<Lotto> lottos) {
         System.out.println(lottos.size() + buyLottoMessage);
@@ -15,4 +17,11 @@ public class Output {
         }
     }
 
+    public static void outputStatistics(List<Integer> result, float revenue) {
+        for (int i = 0; i < Rank.length; i++) {
+            Rank rank = Rank.getRank(i);
+            System.out.println(rank.getMessage() + result.get(rank.getIndex()) + "개");
+        }
+        System.out.println(revenueMessage + revenue + "입니다.");
+    }
 }
