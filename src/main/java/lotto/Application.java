5da05a9125e6e864e703d3ecf2bd735673707ee1
lms0806 <godmlzkf1@naver.com>
2022-11-15 12:13:12 +0900

    refactor:함수 생성 및 변수명 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,22 +9,32 @@ public class Application {
     public static void main(String[] args) {
     	User user = new User();
     	
-    	int n = user.input_money();
+    	int user_input_number = user.input_money();
     	
-    	if(n == 0) {
+    	if(check(user_input_number)) {
     		return;
     	}
     	
-		List<Lotto> arr = BuyLotto.print(n);
-    	
+		List<Lotto> arr = BuyLotto.print(user_input_number);
     	List<Integer> win_number = user.winning_number();
     	
-    	if(win_number.size() == 0) {
+    	if(check(win_number.size())) {
     		return;
     	}
     	
     	int bonus_number = user.winning_number_bonus();
     	
-		Result.print(arr, win_number, bonus_number, n);
+    	if(check(bonus_number)) {
+    		return;
+    	}
+    	
+		Result.print(arr, win_number, bonus_number, user_input_number);
+    }
+    
+    public static boolean check(int check_num) {
+    	if(check_num == 0) {
+    		return true;
+    	}
+    	return false;
     }
 }
