8cd875a55d64731f4e12d4a63fb8b0256593d61f
mikekang47 <gyeong0308@gmail.com>
2022-11-15 19:53:58 +0900

    Feat<GameController>: 사용자 당첨 정보를 출력하는 로직 추가

--- src/main/java/lotto/controllers/GameController.java
+++ src/main/java/lotto/controllers/GameController.java
@@ -49,5 +49,6 @@ public class GameController {
     private void closing(final int money, final List<Lotto> lotteries, final Lotto winningLotto) {
         Map<Place, Integer> prizeMap = gameService.getRewardMap(lotteries, winningLotto);
         double result = gameService.getProfitResult(money, prizeMap);
+        Output.printUserHitInformation(prizeMap);
     }
 }
