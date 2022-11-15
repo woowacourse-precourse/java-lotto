ee13fc3063fa593e5de37f1087884eed5fda39f9
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-11-15 16:52:19 +0900

    feat(Prize) : create start message

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,2 +1,9 @@
-package lotto;public class Prize {
+package lotto;
+
+public class Prize {
+    public static final String enterMoney = "구매금액을 입력해 주세요.";
+
+    public Prize() {
+        System.out.println(enterMoney);
+    }
 }
