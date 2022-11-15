4543aa50fa6a00e0d286355542649b92aa6e8a09
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-15 23:09:29 +0900

    feat : 메인에서 Lotto 프로세스 기능 구현
    입력부터 결과까지 순서대로 구현과 예외 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InOutPut bring = new InOutPut();
+        ProcessData change = new ProcessData();
+        CoreProcess core = new CoreProcess();
+        int moneyChance;
+        int bonus;
+        List<Integer> lotto;
+
+        try {
+            moneyChance = change.moneyChance(change.money(bring.InputNumber(bring.start)));
+            System.out.println(moneyChance);
+            bring.confirm(moneyChance);
+            System.out.println(core.lottoPack(moneyChance));
+            lotto = change.pickedNumbers(bring.InputNumber(bring.input));
+            new Lotto(lotto);
+            bonus = change.money(bring.InputNumber(bring.inputBonus));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        new ResultLotto(moneyChance, lotto, bonus);
+
     }
+
 }
