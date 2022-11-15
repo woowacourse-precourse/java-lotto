96746be1711bbbdb8f1d0c37c768ad0f0dc3b4f7
gnoesnooj <306yyy@naver.com>
2022-11-15 11:49:17 +0800

    feat(LottoConstant) : 매직넘버 상수화위한 클래스 구현

---
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -0,0 +1,12 @@
+package lotto.constant;
+
+public class LottoConstant {
+
+    public static final int AMOUNT_LIMIT = 1000;
+
+    public static final int DIGIT = 6;
+
+    private LottoConstant(){
+
+    }
+}
