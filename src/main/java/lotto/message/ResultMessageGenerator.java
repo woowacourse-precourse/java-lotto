2daa13dc070ce04fcb624c1ed95fc4140a14e231
wonju-dev <wonjulee.dev@gmail.com>
2022-11-15 14:18:16 +0900

    refactor(ResultMessageGenerator): 상수 분리

--- src/main/java/lotto/message/ResultMessageGenerator.java
+++ src/main/java/lotto/message/ResultMessageGenerator.java
@@ -3,30 +3,31 @@ package lotto.message;
 import lotto.Hit;
 
 public class ResultMessageGenerator {
-    private static final String PURCHASE_COUNT_MESSAGE_TEMPLATE = "개를 구매했습니다.";
+    private static final String PURCHASE_COUNT_MESSAGE_TEMPLATE = "%s개를 구매했습니다.";
+    private static final String NET_EARNING_RATE_MESSAGE_TEMPLATE = "총 수익률은 %s%%입니다.";
+    private static final String COUNT_MATCH_MESSAGE_TEMPLATE = "%s개 일치%s (%s원) - %s개";
+    private static final String BONUS_BALL_MATCH_MESSAGE_TEMPLATE = ", 보너스 볼 일치";
+    private static final String BLANK = "";
+    private static final String ROUND = "%.1f";
 
     public static String getPurchaseConfirmMessage(Integer count) {
-        return count + PURCHASE_COUNT_MESSAGE_TEMPLATE;
+        return String.format(PURCHASE_COUNT_MESSAGE_TEMPLATE, count);
     }
 
     public static String getEarningRateMessage(Float earningRate) {
-        return "총 수익률은 " + String.format("%.1f", earningRate) + "%입니다.";
+        return String.format(NET_EARNING_RATE_MESSAGE_TEMPLATE, String.format(ROUND, earningRate));
     }
 
     public static String getLottoResultMessage(Hit hit, Integer count) {
-        String message = "";
-        int hitCount = hit.getHitCount();
-
-        message += hitCount + "개 일치";
-
         if (hit.requireBonus()) {
-            message += ", 보너스 볼 일치";
+            return String.format(COUNT_MATCH_MESSAGE_TEMPLATE,
+                    hit.getHitCount(),
+                    BONUS_BALL_MATCH_MESSAGE_TEMPLATE,
+                    hit.getPrize(),
+                    count
+            );
         }
 
-        message += " (" + hit.getPrize() + "원)";
-
-        message += " - " + count + "개";
-
-        return message;
+        return String.format(COUNT_MATCH_MESSAGE_TEMPLATE, hit.getHitCount(), BLANK, hit.getPrize(), count);
     }
 }
