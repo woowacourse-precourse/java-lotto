d1286f21d6e7ec222618bb2fa0e93f806d7fd4b5
이정우 <acg6138@naver.com>
2022-11-15 15:35:41 +0900

    feat(OutputView): 구입한 로또와 당첨 결과를 출력하는 OutputView 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,35 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class OutputView {
+
+    public static void printLottos(int ticketCount, List<Lotto> lottos) {
+        System.out.println(System.lineSeparator() + ticketCount + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.get());
+        }
+    }
+
+    public static void printResult(HashMap<Rank, Integer> result) {
+        List<Rank> printOrder = List.of(Rank.FIFTH, Rank.FOURTH, Rank.THIRD, Rank.SECOND, Rank.FIRST);
+        System.out.println(System.lineSeparator() + "당첨 통계" +  System.lineSeparator() + "---");
+        for (Rank rank : printOrder) {
+            if (rank.equals(Rank.SECOND)) {
+                System.out.println(rank.getMatchCount() + "개 일치, 보너스 볼 일치 " + rank.getPrizeForPrint() +
+                        " - " + result.get(rank) + "개");
+                continue;
+            }
+            System.out.println(rank.getMatchCount() + "개 일치 " + rank.getMatchCount() +
+                    " - " + result.get(rank)  + "개");
+        }
+    }
+
+    public static void printRateOfReturn(float rateOfReturn) {
+        System.out.println("총 수익률은 " + rateOfReturn + "%입니다.");
+    }
 }
