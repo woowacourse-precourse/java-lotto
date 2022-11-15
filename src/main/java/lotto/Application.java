92b142c3d957622b6fd0220d0692891606c7c4d4
khj <kim37171@cuk.edu>
2022-11-15 22:56:01 +0900

    fix: 예외처리 메시지 출력 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,10 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Lottery lottery = new Lottery();
-        lottery.start();
+        try{
+            lottery.start();
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
