ff8f86263342ea4e942cf2d4d6ccb926df38d4da
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-15 22:58:07 +0900

    feat : 당첨번호가 중복되면 예외처리 되는 기능 추가
    입력시 중복 숫자 입력하면 예외 처리 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,10 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,9 +13,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != 6 || duplicate(numbers) ) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호 오류");
         }
+    }
+
+    private boolean duplicate(List<Integer> numbers){
+        boolean duplicate = false;
+        for(int i = 0; i < numbers.size(); i++){
+            for(int j = i; j< numbers.size(); j++){
+                if(numbers.get(i) == numbers.get(j)){
+                    duplicate = true;
+                }
+            }
+        }return duplicate;
     }
 
     // TODO: 추가 기능 구현
