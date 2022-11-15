db1ee6f7a5c1ad489ceb76050aa05b12b9bdba76
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-15 21:12:55 +0900

    feat: Add input_bunus_number function

--- src/main/java/lotto/Cli.java
+++ src/main/java/lotto/Cli.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static lotto.Validation.validateAnswer;
+import static lotto.Validation.validateBonus;
 import static lotto.Validation.validatePurchase;
 
 import java.util.Arrays;
@@ -36,6 +37,17 @@ public class Cli {
     return Arrays.stream(inputArray)
         .map(Integer::parseInt)
         .collect(Collectors.toList());
+  }
+
+  /**
+   * Function that receives bonus numbers.
+   * @return The number of bonus.
+   */
+  public static Integer inputBonusNumber() {
+    System.out.println("보너스 번호를 입력해 주세요.");
+    String number = readLine();
+    validateBonus(number);
+    return Integer.parseInt(number);
   }
 
   public static void print(List<Lotto> lottoList) {
