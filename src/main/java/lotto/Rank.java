6d62c0ef09ffe4004de95a54a3a6bafcbe941005
YooByeongIk <quddlr96@naver.com>
2022-11-15 19:48:46 +0900

    feat(rank): add rank class for handling result values

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,26 @@
+package lotto;
+
+public enum Rank {
+    FIFTH("3개 일치 ",5000),
+    FOURTH("4개 일치 ",50000),
+    THIRD("5개 일치 ",1500000),
+    SECOND("5개 일치, 보너스 볼 일치 ", 30000000),
+    FIRST("6개 일치 ",2000000000);
+
+    private String message;
+    private int price;
+
+    Rank(String message, int price) {
+        this.message = message;
+        this.price = price;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+}
