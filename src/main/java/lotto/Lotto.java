bcd2be1fbd104ace6c496a9d4b885b8c9decd464
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-15 15:22:25 +0900

    Feat(Lotto): add func printMatch Main

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -46,4 +46,18 @@ public class Lotto {
         }
         return 0;
     }
+    public void printMatch(List<Map<Integer, List<Integer>>> result, int bonus, int price) {
+        int three = matchItemCount(result, 0, 3);
+        int four = matchItemCount(result, 0, 4);
+        int five = matchItemCount(result, 0, 5);
+        int fiveWithBonus = matchItemCount(result, bonus, 5);
+        int six = matchItemCount(result, 0, 6);
+        System.out.println("당첨 통계\n---");
+        System.out.println("3개 일치 (5,000원) - " + three + "개");
+        System.out.println("4개 일치 (50,000원) - " + four + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + five + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + fiveWithBonus + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + six + "개");
+        printBenefit(three, four, five, fiveWithBonus, six, price);
+    }
 }
