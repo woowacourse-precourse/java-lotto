ba433ca9770f32678734365488dcdf955336ef67
lhyeonj120 <isk04387@gmail.com>
2022-11-15 18:44:33 +0900

    test: 로또 구입 개수 테스트

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,5 +23,14 @@ class UserTest {
         User user = new User();
         assertThatThrownBy(() -> user.buyLotto("lotto"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @DisplayName("로또 구입 개수 테스트")
+    @Test
+    void userLottoCntExceptionTest() {
+        User user = new User();
+        user.buyLotto("10000");
+        String result = Integer.toString(user.getLottoCnt());
+        assertThat(result.equals("10"));
     }
 }
