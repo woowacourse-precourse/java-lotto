c8d96d50fd0db919db52e07fa3c3daffa57c3927
Kijung Park <kjpark4321@naver.com>
2022-11-15 22:42:58 +0900

    feat : 로또 당첨번호 입력 받는 메서드 구현

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -2,7 +2,13 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+
+    private List<Integer> answerNumberList;
+
     public void start(){
         System.out.println("구입금액을 입력해 주세요.");
         int inputCost = Integer.parseInt(Console.readLine());
@@ -10,5 +16,26 @@ public class Game {
         if (inputCost%1000 != 0){
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력하세요");
         }
+
+        int lottoCount = getCountOfLottoByCost(inputCost);
+        System.out.println(lottoCount + "개를 구매했습니다.");
+
+        answerNumberList = getAnswerNumberList();
+    }
+
+    private List<Integer> getAnswerNumberList(){
+        String numberLine = Console.readLine();
+
+        List<Integer> numberList = new ArrayList<>();
+        String[] numberSplit = numberLine.split(",");
+        for(String number : numberSplit){
+            numberList.add(Integer.parseInt(number));
+        }
+
+        return numberList;
+    }
+
+    private int getCountOfLottoByCost(int cost){
+        return cost / 1000;
     }
 }
