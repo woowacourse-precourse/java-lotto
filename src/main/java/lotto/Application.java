e061d4536ae2cb410f6b1ada0e7f89cc60079e7b
chopinballadeno4 <qo98333@naver.com>
2022-11-15 14:59:59 +0900

    feat: Catch Exception, print exception

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,10 @@ import lotto.controller.AppController;
 public class Application {
     public static void main(String[] args) {
         AppController appController = new AppController();
-        appController.run();
+        try {
+            appController.run();
+        } catch(IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
