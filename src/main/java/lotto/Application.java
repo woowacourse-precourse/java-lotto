adca128b319b8e8bc1f34101a8eb0ad73481b1a1
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-11-15 10:16:45 +0900

    feat($main): 발행한 로또 번호 출력 기능 구현
    1. 로또 생성하는 함수는 통일성을 위해 Lotto.java에 구현
    2. Application.java에 구현한 관련 함수는, 출력 이후 해당 로또 번호들을 리턴하는 형식으로 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,16 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static lotto.Code.ErrorCode.*;
 import static lotto.Code.PrintCode.*;
 
 public class Application {
+
     public static void main(String[] args) {
 
         System.out.println(GET_MONEY);
@@ -14,6 +19,7 @@ public class Application {
 
         int lottoCount = payedMoney/1000;
         System.out.println("\n" + lottoCount + PRINT_LOTTO_COUNT);
+        List<List<Integer>> candidateLotto = printSeveralLotto(lottoCount);
 
     }
 
@@ -24,6 +30,15 @@ public class Application {
     public static void checkMoneyCanDivideBy1000(int money){
         if (money/1000 != 0)
             throw new IllegalArgumentException(String.valueOf(NO_DIVIDE_BY_1000));
+    }
+    public static List<List<Integer>> printSeveralLotto(int lottoCount){
+        List<List<Integer>> candidateLotto = new ArrayList<>();
+        for (int count = 0; count < lottoCount; count++){
+            List<Integer> tmpLotto = lotto.Lotto.createLotto();
+            candidateLotto.add(tmpLotto);
+            System.out.println(tmpLotto);
+        }
+        return candidateLotto;
     }
 
 
--- src/main/java/lotto/Code/PrintCode.java
+++ src/main/java/lotto/Code/PrintCode.java
@@ -2,7 +2,8 @@ package lotto.Code;
 
 public enum PrintCode {
 
-    GET_MONEY("구입금액을 입력해 주세요.");
+    GET_MONEY("구입금액을 입력해 주세요."),
+    PRINT_LOTTO_COUNT("개를 구매했습니다.");
 
     private String message;
 
