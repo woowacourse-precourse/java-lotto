c315259a983e1855891bf5a615c4886cc77750da
qdtjj4 <76ylo1@gmail.com>
2022-11-15 18:29:43 +0900

    refactor: Output 클래스의 출력 기능 private -> public 변경 및 수정
    변수와 메서드에 static 추가

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -6,30 +6,30 @@ import java.util.Map;
 import java.util.Set;
 
 public class Output {
-    private final String QUANTITY_OF_LOTTO = "개를 구매했습니다.";
-    private final String WINNING_STATISTICS = "당첨 통계";
-    private final String PERFORATION = "---";
-    private final String THREE_CORRECT = "3개 일치 (5,000원) - ";
-    private final String FOUR_CORRECT = "4개 일치 (50,000원) - ";
-    private final String FIVE_CORRECT = "5개 일치 (1,500,000원) - ";
-    private final String FIVE_AND_BONUS_CORRECT = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
-    private final String SIX_CORRECT = "6개 일치 (2,000,000,000원) - ";
-    private final String TOTAL_PROFIT = "총 수익률은 .%1f%입니다.";
+    private static final String QUANTITY_OF_LOTTO = "개를 구매했습니다.";
+    private static final String WINNING_STATISTICS = "당첨 통계";
+    private static final String PERFORATION = "---";
+    private static final String THREE_CORRECT = "3개 일치 (5,000원) - ";
+    private static final String FOUR_CORRECT = "4개 일치 (50,000원) - ";
+    private static final String FIVE_CORRECT = "5개 일치 (1,500,000원) - ";
+    private static final String FIVE_AND_BONUS_CORRECT = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    private static final String SIX_CORRECT = "6개 일치 (2,000,000,000원) - ";
+    private static final String TOTAL_PROFIT = "총 수익률은 .%1f%입니다.";
 
     private Output() {
     }
 
-    private void printQuantityOfLotto(int quantity) {
+    public static void printQuantityOfLotto(int quantity) {
         System.out.println(quantity + QUANTITY_OF_LOTTO);
     }
 
-    private void printLottoTickets(Set<List<Integer>> lottoTickets) {
+    public static void printLottoTickets(Set<List<Integer>> lottoTickets) {
         for (List<Integer> lottoTicket : lottoTickets) {
             System.out.println(formLottoTicket(lottoTicket));
         }
     }
 
-    private String formLottoTicket(List<Integer> lottoTicket) {
+    private static String formLottoTicket(List<Integer> lottoTicket) {
         List<String> ticket = new ArrayList<>();
         for (Integer eachNumber : lottoTicket) {
             ticket.add(String.valueOf(eachNumber));
@@ -40,7 +40,7 @@ public class Output {
         return openBracket + printFormat + closeBracket;
     }
 
-    private void printTotalRank(Map<Integer, Integer> totalRank) {
+    public static void printTotalRank(Map<Integer, Integer> totalRank) {
         System.out.println(WINNING_STATISTICS);
         System.out.println(PERFORATION);
         System.out.println(THREE_CORRECT + totalRank.get(5) + "개");
@@ -50,11 +50,11 @@ public class Output {
         System.out.println(SIX_CORRECT + totalRank.get(1) + "개");
     }
 
-    private void printRateOfReturn(double rateOfReturn) {
+    public static void printRateOfReturn(double rateOfReturn) {
         System.out.println(String.format(TOTAL_PROFIT, rateOfReturn));
     }
 
-    private long calculateSumOfPrize(Map<Integer, Integer> totalRank) {
+    private static long calculateSumOfPrize(Map<Integer, Integer> totalRank) {
         long sumOfPrize = 0;
         sumOfPrize += totalRank.get(1) * 2000000000L;
         sumOfPrize += totalRank.get(2) * 30000000L;
@@ -64,7 +64,7 @@ public class Output {
         return sumOfPrize;
     }
 
-    private double calculateRateOfReturn(long sumOfPrize, int purchaseAmount) {
+    private static double calculateRateOfReturn(long sumOfPrize, int purchaseAmount) {
         double rateOfReturn = (double) sumOfPrize / (double) purchaseAmount;
         return rateOfReturn;
     }
