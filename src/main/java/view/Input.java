ecce3399d81c5c62e6066b18e218e16990f657ef
masiljangajji <hjk172262@gmail.com>
2022-11-15 15:48:43 +0900

    style: 메소드 이름수정

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -11,14 +11,17 @@ public class Input {
     public static final int MINIMUM = 1;
     public static final int MAXIMUM = 45;
     public static final int LOTTO_SIZE = 6;
+
     public static String inputNumber() {
-        String outputNumber=Console.readLine();
+        String outputNumber = Console.readLine();
         return outputNumber;
     }
 
-    public static List getRandomNumber() {
+    public static List makeRandomNumber() {
         List<Integer> randomNumbers;
         randomNumbers = Randoms.pickUniqueNumbersInRange(MINIMUM, MAXIMUM, LOTTO_SIZE);
         return randomNumbers;
     }
+
+
 }
