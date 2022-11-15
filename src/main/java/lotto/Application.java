000094981b8e6c2cc46e14b91e30e873b6bc8809
YongNyeo <won05121@naver.com>
2022-11-15 22:58:12 +0900

    feat: 로또 구매 금액 입력 받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BuyLotto buyLotto = new BuyLotto(InputBuyNum());
+
+        //구매금액 입력
+        public static String InputBuyNum() {
+            System.out.println("구매금액을 입력해 주세요");
+            String buyNum = readLine();
+            return buyNum;
+        }
     }
 }
