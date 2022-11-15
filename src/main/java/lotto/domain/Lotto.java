9d1c2c69f5a5d29b5b98e8956cdc48370517756d
dlrghks2090 <dlrghks2090@gmail.com>
2022-11-15 20:03:00 +0900

    refactor: ErrorMessege 메세지 상수 이름 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,28 +9,30 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("");
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_SIZE.getErrorMessage());
         }
     }
 
     private void validateRange(List<Integer> numbers) {
         for (int i = 0; i < numbers.size(); i++) {
             if (numbers.get(i) < 1 || numbers.get(i) > 45) {
-                throw new IllegalArgumentException("");
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_RANGE.getErrorMessage());
             }
         }
     }
 
-    private void validateUnduplicate(List<Integer> numbers) {
+    private void validateDuplicate(List<Integer> numbers) {
         Set<Integer> unduplicatedNumbers = new HashSet<>(numbers);
 
         if (unduplicatedNumbers.size() != numbers.size()) {
-            throw new IllegalArgumentException("");
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_DUPLICATE.getErrorMessage());
         }
     }
 
