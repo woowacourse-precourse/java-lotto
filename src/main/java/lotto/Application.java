68921927843f10d70db43452c1dc6f966307d42e
peageon <jinsoo2005@gmail.com>
2022-11-15 16:13:23 +0900

    feat($application): 당첨번호 입력받기
    당첨번호 입력 받아서 Lotto 객체 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,9 +2,22 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    public static List<Integer> inputConversion(String input) {
+        String[] nums = input.split(",");
+        List<Integer> numbers = new ArrayList<>();
+        for (String num:nums) {
+            try {
+                numbers.add(Integer.parseInt(num));
+            } catch (Exception e){
+                throw new IllegalArgumentException("[Error] 로또 숫자는 Integer 이어야 합니다.");
+            }
+        }
+        return numbers;
+    }
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
         Integer amount;
@@ -13,9 +26,14 @@ public class Application {
         } catch (Exception e) {
             throw new IllegalArgumentException("구매 금액은 숫자여야 합니다.");
         }
+
         Lotto.checkBuyAmount(amount);
         amount = amount/1000;
         List<List<Integer>> lottos = Lotto.generateLotto(amount);
 
+        //로또 당첨 번호 입력 받기
+        System.out.println("당첨 번호를 입력해 주세요.");
+        Lotto firstPlace = new Lotto(inputConversion(Console.readLine()));
+        
     }
 }
