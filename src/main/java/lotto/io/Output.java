3352cc14bc18d9917bb19586d9d9ef4971f801f3
UJ15 <dbwogml15@naver.com>
2022-11-15 10:40:01 +0900

    feat : create print result method

--- src/main/java/lotto/io/Output.java
+++ src/main/java/lotto/io/Output.java
@@ -2,6 +2,7 @@ package lotto.io;
 
 import java.util.List;
 import lotto.model.Lotto;
+import lotto.model.WinningStatus;
 
 public interface Output {
     void printPurchasePrompt();
@@ -11,4 +12,8 @@ public interface Output {
     void printBonusNumberPrompt();
 
     void printLottos(int count, List<Lotto> lottos);
+
+    void printResult(WinningStatus winningStatus);
+
+    void printError(Exception e);
 }
