c39858f3cf120fbf864ff27b54c49d86645bb683
jxixeun <jxixeun@gmail.com>
2022-11-15 18:06:03 +0900

    test: 구입 금액 입력 테스트 작성

---
+++ src/test/java/lotto/LottoControllerTest.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoControllerTest {
+    @Test
+    void 숫자가_아닌_입력() {
+        assertThatThrownBy(() -> new LottoController().validate("abc"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 천원단위가_아닌_입력() {
+        assertThatThrownBy(() -> new LottoController().validate("1999"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
