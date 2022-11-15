9d48be2741b5d89efab3c55c2081804157492402
kbm6609 <kbm6609@nvaer.com>
2022-11-15 22:14:36 +0900

    feat<lotto> : 총 당첨 금액 계산 기능 구현

--- src/main/java/lotto/domain/Judgment.java
+++ src/main/java/lotto/domain/Judgment.java
@@ -1,8 +1,23 @@
 package lotto.domain;
 
+import lotto.domain.enums.WinningAmount;
+
 import java.util.List;
 
 public class Judgment {
+    private static final int PRINT_START_INDEX = 3;
+
+    public int calculatePrizeMoney(int[] wins) {
+        WinningAmount[] values = WinningAmount.values();
+        int totalMoney = 0;
+
+        for (int i = 0; i < values.length; i++) {
+            totalMoney += values[i].getPrizeMoney() * wins[i + PRINT_START_INDEX];
+        }
+
+        return totalMoney;
+    }
+
     public int[] calculateWins(List<Lotto> myLottos, List<Integer> winningNumber) {
         int[] wins = new int[Lotto.numberCount + 1];
         for (int i = 0; i < myLottos.size(); i++) {
