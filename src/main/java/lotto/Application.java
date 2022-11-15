79b4c8fb951e7ccf4b6317deff6f24d833d59142
mikekang47 <gyeong0308@gmail.com>
2022-11-15 19:58:38 +0900

    Feat<Application>: 게임 실행 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controllers.GameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = GameController.getInstance();
+        try {
+            gameController.startGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
