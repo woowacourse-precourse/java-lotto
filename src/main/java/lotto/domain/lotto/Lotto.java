a9e2e71bc2b67cf132e024fb7e127a01003c6b47
JuSeong1130 <jsmoon1130@naver.com>
2022-11-15 16:30:37 +0900

    feat(Lotto): lotto 숫자와 관련된 유효성 기능 추가
    6개의 길이, 서로 다른 숫자,숫자인지, 1~45의 범위,보너스번호 포함여부 유효성 검사

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -1,24 +1,92 @@
 package lotto.domain.lotto;
 
+import static lotto.util.constant.Constant.LOTTO_END_NUMBER;
+import static lotto.util.constant.Constant.LOTTO_LENGTH;
+import static lotto.util.constant.Constant.LOTTO_START_NUMBER;
+
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Lotto {
-    private final List<Integer> numbers;
 
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+	private static final String DIVIDE = ",";
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+	private static final String NOT_THE_RIGHT_LOTTO_LENGTH = "[ERROR] 입력한 개수가 올바르지 않습니다.";
+	private static final String NOT_THE_RIGHT_NUMBER = "[ERROR] 숫자가 아닌 값이 입력 됬습니다";
+	private static final String NOT_THE_RIGHT_RANGE = "[ERROR] 올바른 범위의 숫자가 입력되지 않았습니다";
+	private static final String NOT_THE_ANOTHER_NUMBER = "[ERROR] 1~45의 서로다른 숫자가 입력되지 않았습니다";
+	private final List<Integer> numbers;
+
+	public List<Integer> getNumbers() {
+		return numbers;
+	}
+
+	public Lotto(List<Integer> numbers) {
+		this.numbers = numbers;
+		validate();
+	}
+
+	public Lotto(String winLottoNumber,String bonusNumber) {
+		isNumber(winLottoNumber);
+		this.numbers = convertStringToList(winLottoNumber);
+		isAlreadyIncludeNumber(bonusNumber);
+		validate();
+	}
+
+	private void validate() {
+		isLengthRight();
+		isInRangeNumber();
+		isAnotherNumber();
+	}
+
+	public void isNumber(String number) {
+		number = number.replace(",", "");
+		try {
+			Long.parseLong(number);
+		} catch (Exception e) {
+			throw new IllegalArgumentException(NOT_THE_RIGHT_NUMBER);
+		}
+	}
+
+	public void isLengthRight() {
+		if (numbers.size() != LOTTO_LENGTH) {
+			throw new IllegalArgumentException(NOT_THE_RIGHT_LOTTO_LENGTH);
+		}
+	}
+
+	public void isInRangeNumber() {
+		for (Integer num : numbers) {
+			if (num < LOTTO_START_NUMBER || num > LOTTO_END_NUMBER) {
+				throw new IllegalArgumentException(NOT_THE_RIGHT_RANGE);
+			}
+		}
+	}
+
+	public void isAnotherNumber() {
+		int removeDisticntSize  = (int) numbers.stream()
+				.distinct()
+                .count();
+        int size = numbers.size();
+		if (removeDisticntSize != size) {
+			throw new IllegalArgumentException(NOT_THE_ANOTHER_NUMBER);
+		}
+	}
+
+	public List<Integer> convertStringToList(String number) {
+		String[] arr = number.split(DIVIDE);
+		return Stream.of(arr)
+				.map(Integer::parseInt)
+				.collect(Collectors.toList());
+	}
+
+	public void isAlreadyIncludeNumber(String number) {
+		Integer num = Integer.parseInt(number);
+		if (numbers.contains(num)){
+            throw new IllegalArgumentException(NOT_THE_ANOTHER_NUMBER);
         }
-    }
+	}
 
-    // TODO: 추가 기능 구현
+
 }
