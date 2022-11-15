a000401efcbb1e39b6b03d518c24a7c1b7e9e2d3
Sieun Park <psieun0606@gmail.com>
2022-11-15 12:00:21 +0900

    refactor: 구입 금액이 1000원으로 나누어 떨어지지 않을 때 예외 처리 기능 메서드 이동

--- src/main/java/lotto/Domain/PurchasingLotto.java
+++ src/main/java/lotto/Domain/PurchasingLotto.java
@@ -7,12 +7,12 @@ public class PurchasingLotto {
     private final String ERROR_DEVISION_EXCEPTION = "[ERROR] 구입 금액이 1000원으로 나누어 떨어지지 않습니다.";
 
     public PurchasingLotto(int purchase) {
-        devisionValid(purchase);
         this.amountOfLotto = calculateNumberOfLotto(purchase);
         this.purchase = purchase;
     }
 
     public int calculateNumberOfLotto(int purchasingAmount) {
+        devisionValid(purchase);
         int numberOfLotto = purchasingAmount / 1000;
         return numberOfLotto;
     }
