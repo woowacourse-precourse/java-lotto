e7b100b6c7bb394067c66d8d219a1388f9847c03
jeomxon <jeomxon@gmail.com>
2022-11-15 18:36:05 +0900

    fix: 예외 타입 변경 및 불필요한 출력문 삭제

--- src/main/java/lotto/domain/purchase/Purchase.java
+++ src/main/java/lotto/domain/purchase/Purchase.java
@@ -1,7 +1,5 @@
 package lotto.domain.purchase;
 
-import java.util.NoSuchElementException;
-
 public class Purchase {
     private static final String NOT_INTEGER_ERROR = "[ERROR] 정수 값이 입력되어야 합니다.";
     private static final String PURCHASE_AMOUNT_ERROR = "[ERROR] 구입 금액은 1,000원 단위여야 합니다.";
@@ -30,14 +28,12 @@ public class Purchase {
         String regex = "^[0-9]+$";
 
         if (!input.matches(regex)) {
-            System.out.println(NOT_INTEGER_ERROR);
-            throw new NoSuchElementException(NOT_INTEGER_ERROR);
+            throw new IllegalArgumentException(NOT_INTEGER_ERROR);
         }
     }
 
     private void validateMoneyUnit(int purchaseAmount) {
         if (purchaseAmount % 1000 != 0) {
-            System.out.println(PURCHASE_AMOUNT_ERROR);
             throw new IllegalArgumentException(PURCHASE_AMOUNT_ERROR);
         }
     }
