6fb50ce970fe879d4882d5875c493559c0f52c6e
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-15 20:58:09 +0900

    refactor: 예외처리 오류 수정

--- src/main/java/lotto/Bonus.java
+++ src/main/java/lotto/Bonus.java
@@ -1,25 +1,25 @@
 package lotto;
 
-import lotto.view.ErrorMessage;
+import lotto.console.ErrorMessage;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Bonus {
     private final int bonusNumber;
 
-    public Bonus(List<Integer> numbers, int bonusNumber) {
-        range(bonusNumber);
-        type(bonusNumber);
-        repeat(numbers, bonusNumber);
-        this.bonusNumber = bonusNumber;
+    public Bonus(List<Integer> numbers, String bonus) {
+        type(bonus);
+        int bonusNum = Integer.parseInt(bonus);
+        range(bonusNum);
+        repeat(numbers, bonusNum);
+        this.bonusNumber = bonusNum;
     }
     private void range(int bonus){
-        if (String.valueOf(bonus).length() != 1) {
+        if (bonus < 1 || bonus > 45) {
             throw new IllegalArgumentException(ErrorMessage.bonusLength.getValue());
         }
     }
-    private void type(int bonus) {
+    private void type(String bonus) {
         int eachChar = String.valueOf(bonus).charAt(0);
         if (!Character.isDigit(eachChar)) {
             throw new IllegalArgumentException(ErrorMessage.bonusType.getValue());
