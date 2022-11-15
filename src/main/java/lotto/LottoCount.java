155c7c1a70d5382e0d6786064cc54272c398c20b
유지웅 <poujis0@gmail.com>
2022-11-15 14:43:00 +0900

    test - 로또 갯수 출력 테스트 및 마이너스 갯수 Valid 테스트

--- src/main/java/lotto/LottoCount.java
+++ src/main/java/lotto/LottoCount.java
@@ -1,13 +1,21 @@
 package lotto;
 
 public class LottoCount {
+    private static final String VALID_LOTTO_MINUS_MESSAGE = "로또 갯수가 마이너스 입니다.";
     private int lottoCount;
 
     public LottoCount(int lottoCount) {
+        validLottoMinusCount(lottoCount);
         this.lottoCount = lottoCount;
     }
 
     public int getLottoCount() {
         return lottoCount;
+    }
+
+    private void validLottoMinusCount(int lottoCount) {
+        if (lottoCount < 0){
+            throw new IllegalArgumentException(VALID_LOTTO_MINUS_MESSAGE);
+        }
     }
 }
--- src/test/java/lotto/LottoCountTest.java
+++ src/test/java/lotto/LottoCountTest.java
@@ -1,9 +1,15 @@
 package lotto;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import view.OutputView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class LottoCountTest {
     @ParameterizedTest
@@ -12,5 +18,23 @@ public class LottoCountTest {
     void 로또_갯수_Count_테스트(String input){
         LottoMoney lottoMoney = new LottoMoney(input);
         Assertions.assertThat(lottoMoney.changeToLottoCount()).isEqualTo(Integer.parseInt(input)/LottoMoney.LOTTO_PRICE);
+    }
+
+    @ParameterizedTest
+    @DisplayName(("로또 갯수를 출력이 올바른지 확인하는 테스트"))
+    @ValueSource(ints = {1,14,58,52,4,8})
+    void 로또_갯수_콘솔_출력_테스트(int count){
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        OutputView.printLottoCount(new LottoCount(count));
+        LottoCount lottoCount = new LottoCount(count);
+        Assertions.assertThat(String.format("%d개를 구매했습니다",count)).isEqualTo(byteArrayOutputStream.toString());
+    }
+    @ParameterizedTest
+    @DisplayName("로또 갯수가 마이너스이면 에러를 띄우는 테스트")
+    @ValueSource(ints = {-1,-2,-3,-4,-5,-6,-7,-8})
+    void 로또_갯수가_마이너스(int count){
+        Assertions.assertThatThrownBy(()->new LottoCount(count)).isInstanceOf(IllegalArgumentException.class);
     }
 }
