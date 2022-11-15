237f3c3910718f23a3f05e25801b4e8e7c04e6d9
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-15 21:42:22 +0900

    Feat(Exception): Add WinNumber ExceptionRange Method

--- src/main/java/lotto/exception/WinNumber.java
+++ src/main/java/lotto/exception/WinNumber.java
@@ -1,5 +1,19 @@
 package lotto.exception;
 
+import lotto.constants.Error;
+import lotto.constants.Number;
+
 public class WinNumber {
+    public static void exceptionRange(String input) {
+        String[] winNumber = input.split(",");
+        int rangeStart = Number.RANGE_START.getNumber();
+        int rangeEnd = Number.RANGE_END.getNumber();
+        for (int i = 0; i < winNumber.length; i++) {
+            if (!(rangeStart <= Integer.parseInt(winNumber[i]) && Integer.parseInt(winNumber[i]) <= rangeEnd)) {
+                throw new IllegalArgumentException(Error.WRONG_LOTTO_RANGE.getMsg());
+            }
+        }
+    }
+
 
 }
