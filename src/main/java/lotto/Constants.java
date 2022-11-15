e49c8058c571770c55dceede3044f75ba80cce35
Sanmgmin <sangmins930@gmail.com>
2022-11-15 20:14:50 +0900

    refactor(Constants): change text to variable

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -10,6 +10,6 @@ public class Constants {
     public static final String ERROR_MESSAGE_FOR_RANGE_OF_NUMBER = "[ERROR] 로또 번호는 " + MIN_VALUE_OF_LOTTO + "부터 " + MAX_VALUE_OF_LOTTO + " 사이의 숫자여야 합니다.";
     public static final String ERROR_MESSAGE_FOR_DUPLICATION = "[ERROR] 로또 번호는 서로 중복되지 않는 숫자여야 합니다.";
     public static final String ERROR_MESSAGE_FOR_DUPLICATION_WITH_WINNING_NUMBER = "[ERROR] 보너스 번호는 로또 번호와 중복되지 않은 숫자여야 합니다.";
-    public static final String ERROR_MESSAGE_FOR_RANGE_OF_BONUS_NUMBER = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
-    public static final String ERROR_MESSAGE_FOR_SEED_MONEY = "[ERROR] 구입금액은 1000원 단위여야 합니다.";
+    public static final String ERROR_MESSAGE_FOR_RANGE_OF_BONUS_NUMBER = "[ERROR] 보너스 번호는 " + MIN_VALUE_OF_LOTTO + "부터 " + MAX_VALUE_OF_LOTTO + " 사이의 숫자여야 합니다.";
+    public static final String ERROR_MESSAGE_FOR_SEED_MONEY = "[ERROR] 구입금액은 " + MONEY_UNIT + "원 단위여야 합니다.";
 }
