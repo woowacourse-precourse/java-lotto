336fdc8c178d245253515fe9f7d40910e2fcad49
ysng_ysng <yeasung67@gmail.com>
2022-11-15 21:59:14 +0900

    feat(Application): Implement the exception
    발생한 예외들에 대해 오류메시지를 출력하는 부분을 구현했습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
 public class Application {
-    public static void main(String[] args) {
-
-    }
+	public static void main(String[] args) {
+		try {
+			LottoPlay.run();
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
