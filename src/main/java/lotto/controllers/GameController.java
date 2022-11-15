8e0d21e361b642b3fcacdc061d6aff5c8a9807b4
mikekang47 <gyeong0308@gmail.com>
2022-11-15 17:41:48 +0900

    Feat<GameController>: 서비스 의존성 주입

--- src/main/java/lotto/controllers/GameController.java
+++ src/main/java/lotto/controllers/GameController.java
@@ -1,14 +1,18 @@
 package lotto.controllers;
 
+import lotto.applications.GameService;
 import lotto.view.Input;
 
 public class GameController {
     private static GameController INSTANCE;
-    private GameController() {}
+    private final GameService gameService;
+    private GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
 
     public static GameController getInstance() {
         if (INSTANCE == null) {
-            INSTANCE = new GameController();
+            INSTANCE = new GameController(GameService.getInstance());
         }
         return INSTANCE;
     }
