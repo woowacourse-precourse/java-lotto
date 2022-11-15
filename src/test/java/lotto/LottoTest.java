f89f3a09454a27c00c1a6cfb6572d786fa8e85b6
0112leesy <0112leesy@naver.com>
2022-11-15 19:51:36 +0900

    test(LottoTest) : 로또 번호 확인 테스트 코드 작성
    hasNumber 함수와 sortedNumbersToString 함수 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,18 +1,21 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 class LottoTest {
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
@@ -20,8 +23,25 @@ class LottoTest {
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 없는 숫자를 찾으면 False를 반환한다.")
+    @Test
+    void checkNumberNotInLotto() {
+        assertFalse(new Lotto(List.of(1, 2, 3, 4, 5, 6)).hasNumber(7));
+    }
+
+    @DisplayName("로또 번호에 없는 숫자를 찾으면 True를 반환한다.")
+    @Test
+    void checkNumberInLotto() {
+        assertTrue(new Lotto(List.of(1, 2, 3, 4, 5, 6)).hasNumber(1));
+    }
+
+    @DisplayName("같은 번호를 가지는 로또는 오름차순으로 정렬된 번호 문자열이 같다.")
+    @Test
+    void checkEqualNumberLotto() {
+        assertThat(new Lotto(List.of(1, 3, 5, 2, 4, 6)).sortedNumbersToString()).isEqualTo(
+            new Lotto(List.of(6, 5, 4, 1, 2, 3)).sortedNumbersToString());
+    }
 }
