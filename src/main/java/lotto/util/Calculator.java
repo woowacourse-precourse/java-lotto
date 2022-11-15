145b21ce56c56691170ef9a2683ffa8539cd5139
SeongukBaek <bsu6235@gmail.com>
2022-11-15 13:01:28 +0900

    refactor: extract feature to new method

--- src/main/java/lotto/util/Calculator.java
+++ src/main/java/lotto/util/Calculator.java
@@ -7,19 +7,27 @@ import java.util.*;
 
 public class Calculator {
     private static Map<Result, Integer> resultCounts;
-    private static int earn;
 
     public static Map<Result, Integer> calculateResult(List<Lotto> lottos, Set<Integer> winningNumbers, int bonusNumber) {
         initializeMap();
-        earn = 0;
 
         for (Lotto lotto : lottos) {
             Result computedResult = lotto.computeResult(winningNumbers, bonusNumber);
             resultCounts.computeIfPresent(computedResult, (result, count) -> count + 1);
-            earn += stringToInt(computedResult.getMoney());
         }
 
         return resultCounts;
+    }
+
+    public static int calculateEarn(List<Lotto> lottos, Set<Integer> winningNumbers, int bonusNumber) {
+        int earn = 0;
+
+        for (Lotto lotto : lottos) {
+            Result computedResult = lotto.computeResult(winningNumbers, bonusNumber);
+            earn += stringToInt(computedResult.getMoney());
+        }
+
+        return earn;
     }
 
     private static void initializeMap() {
@@ -36,7 +44,7 @@ public class Calculator {
         return Integer.parseInt(money.replace(",", ""));
     }
 
-    public static double calculateYield(int money) {
+    public static double calculateYield(int money, int earn) {
         if (money == 0 || earn == 0) {
             return 0;
         }
