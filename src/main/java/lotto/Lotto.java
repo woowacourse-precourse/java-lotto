a1100d940e6a530c19a563da14d31bfb34c8a58d
KimSunU <kgw6147@naver.com>
2022-11-15 22:41:00 +0900

    [feat] Lotto 클래스 조건 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static String ERROR_NUMBERS_SIZE = "로또 번호의 개수가 올바르지 않습니다.";
+    private static String ERROR_NUMBERS_UNIQUE = "로또 번호는 중복을 허용하지 않습니다.";
+    private static String ERROR_NUMBERS_RANGE = "로또 번호의 범위가 올바르지 않습니다.";
     public static final int LOTTO_SIZE = 6;
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
@@ -12,6 +15,10 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers(){
+        return numbers;
     }
 
     private void validate(List<Integer> numbers) {
@@ -22,7 +29,7 @@ public class Lotto {
 
     private void validateSize(List<Integer> numbers){
         if(numbers.size() != LOTTO_SIZE){
-            throw new IllegalArgumentException("[ERROR] 로또 번호의 개수가 올바르지 않습니다.");
+            throw new IllegalArgumentException(ERROR_NUMBERS_SIZE);
         }
     }
 
@@ -30,7 +37,7 @@ public class Lotto {
         List<Integer> checkUnique = new ArrayList<>();
         for(int number: numbers){
             if(checkUnique.contains(number)){
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복을 허용하지 않습니다.");
+                throw new IllegalArgumentException(ERROR_NUMBERS_UNIQUE);
             }
             checkUnique.add(number);
         }
@@ -39,7 +46,7 @@ public class Lotto {
     private void validateRangeOfNumber(List<Integer> numbers){
         for(int number : numbers){
             if(number < MIN_NUMBER || number > MAX_NUMBER) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 1-45입니다.");
+                throw new IllegalArgumentException(ERROR_NUMBERS_RANGE);
             }
         }
     }
