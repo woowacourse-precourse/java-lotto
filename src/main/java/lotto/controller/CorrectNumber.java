9588b7949d5ea806520eb31ba8d62b2966479610
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-15 11:57:46 +0900

    refactor(main\...\controller\CorrectNumber) : add information of cashprize in constants field of CorrectNumber enum class

--- src/main/java/lotto/controller/CorrectNumber.java
+++ src/main/java/lotto/controller/CorrectNumber.java
@@ -1,17 +1,19 @@
 package lotto.controller;
 
 public enum CorrectNumber {
-    THREE(3, "3개 일치 (5,000원) - "),
-    FOUR(4, "4개 일치 (50,000원) - "),
-    FIVE(5, "5개 일치 (1,500,000원) - "),
-    FIVE_BONUS(5, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    SIX(6, "6개 일치 (2,000,000,000원) - ");
+    THREE(3, 5000, "3개 일치 (5,000원) - "),
+    FOUR(4, 50_000, "4개 일치 (50,000원) - "),
+    FIVE(5, 1_500_000, "5개 일치 (1,500,000원) - "),
+    FIVE_BONUS(5, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    SIX(6, 2_000_000_000, "6개 일치 (2,000,000,000원) - ");
 
     private final int correctNumber;
-    private final String cashPrize;
+    private final int cashPrize;
+    private final String prizeInformationMent;
 
-    CorrectNumber(int correctNumber, String cashPrize) {
+    CorrectNumber(int correctNumber, int cashPrize, String prizeInformationMent) {
         this.correctNumber = correctNumber;
         this.cashPrize = cashPrize;
+        this.prizeInformationMent = prizeInformationMent;
     }
 }
