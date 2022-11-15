fc12e05693ff19c0b02a90941616d46b9abad0dc
ycopp9 <aciees95@gmail.com>
2022-11-15 14:57:25 +0900

    feat(LottoEvaluator): 로또결과를 통해 총 당첨금을 반환하는 메서드 작성
    각 등수 별로 몇 장 있는 지를 통해 총 당첨금을 반환하는 메서드 구현함.

--- src/main/java/lotto/domain/LottoEvaluator.java
+++ src/main/java/lotto/domain/LottoEvaluator.java
@@ -4,6 +4,11 @@ import java.util.Map;
 
 public class LottoEvaluator {
     public int evaluate(Map<WinningResult, Integer> lottoResults) {
-        return 0;
+        int money = 0;
+        for (WinningResult result : WinningResult.values()) {
+            int winningTicket = lottoResults.getOrDefault(result, 0);
+            money += winningTicket * result.getPrize();
+        }
+        return money;
     }
 }
