f3680f033442bb1a95eedda02590a0a114aaee4a
hyeongu <wancern@naver.com>
2022-11-15 22:57:08 +0900

    feat(Game): 초기 클래스 구조 생성

---
+++ src/main/java/lotto/Game.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import java.util.List;
+
+public class Game {
+    private static List<Lotto> randomLottos;
+    private static Lotto winningLotto;
+    private static int bonusNumber;
+
+    public void gameStart(){
+
+    }
+}
