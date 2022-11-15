650733b9db18f4cdb469cd0e24fe3938c03d600c
kevstevie <jianna442@gmail.com>
2022-11-15 20:57:25 +0900

    feat: 입력 당첨 번호 validate 메서드 추가

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -1,9 +1,12 @@
 package lotto;
 
+import java.util.List;
+
 public class Validation {
     
     private static final int MAX_NUMBER = 45;
     private static final int MIN_NUMBER = 1;
+    private static final int NUMBERS_SIZE = 6;
     private static final long ZERO = 0L;
     private static final long MIN_UNIT = 1000L;
 
@@ -33,5 +36,31 @@ public class Validation {
     public static void validateMoney(String str) {
         validateNumber(str);
         validateUnit(str);
+    }
+
+    public static void validateNumbers(List<Integer> numbers) {
+        validateNumbersSize(numbers);
+        validateNumbersRange(numbers);
+    }
+
+    private static void validateNumbersSize(List<Integer> numbers) {
+        if (numbers.size() != NUMBERS_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_SIX.printError());
+        }
+    }
+
+    private static void validateNumbersRange(List<Integer> numbers) {
+        long count = numbers.stream().filter(s -> s >= 1 && s <= 45)
+                .count();
+        if (count != NUMBERS_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_RANGE.printError());
+        }
+    }
+
+    private static void validateNumbersDuplicate(List<Integer> numbers) {
+        long count = numbers.stream().distinct().count();
+        if (count != NUMBERS_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_DUPLICATE.printError());
+        }
     }
 }
