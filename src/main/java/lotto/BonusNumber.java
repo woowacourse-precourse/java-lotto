ad05501c6be68638ea7410d4d673199804129c07
Seoyoung <ryupark1006@gmail.com>
2022-11-15 16:26:28 +0900

    feat: 보너스 번호 중복 확인 기능 구현

--- src/main/java/lotto/BonusNumber.java
+++ src/main/java/lotto/BonusNumber.java
@@ -1,18 +1,30 @@
 package lotto;
 
+import java.util.List;
+
+import static lotto.Error.BONUS_NUMBER_DUPLICATED_ERROR;
 import static lotto.Error.BONUS_NUMBER_RANGE_ERROR;
 
 public class BonusNumber {
     private final int bonusNumber;
 
-    public BonusNumber(int bonusNumber) {
+    public BonusNumber(int bonusNumber, List<Integer> winNumbers) {
         checkRange(bonusNumber);
+        checkDuplicated(bonusNumber, winNumbers);
         this.bonusNumber = bonusNumber;
     }
 
     private void checkRange(int bonusNumber) {
         if (bonusNumber < 1 || bonusNumber > 45) {
             throw new IllegalArgumentException(BONUS_NUMBER_RANGE_ERROR.getMessage());
+        }
+    }
+
+    private void checkDuplicated(int bonusNumber, List<Integer> winNumbers) {
+        for (Integer winNumber : winNumbers) {
+            if (winNumber.equals(bonusNumber)) {
+                throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATED_ERROR.getMessage());
+            }
         }
     }
 }
--- src/main/java/lotto/Error.java
+++ src/main/java/lotto/Error.java
@@ -9,7 +9,8 @@ public enum Error {
     WIN_NUMBER_SIZE_ERROR("[ERROR] 당첨 번호는 6개의 숫자여야 합니다."),
     WIN_NUMBER_RANGE_ERROR("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다."),
     WIN_NUMBER_DUPLICATED_ERROR("[ERROR] 당첨 번호는 중복되지 않는 숫자여야 합니다."),
-    BONUS_NUMBER_RANGE_ERROR("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+    BONUS_NUMBER_RANGE_ERROR("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다."),
+    BONUS_NUMBER_DUPLICATED_ERROR("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않는 숫자여야 합니다.");
 
     private final String message;
 
