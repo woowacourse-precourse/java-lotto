92cf779ceb779b767f946eec0667ba59401c9f04
Seoyoung <ryupark1006@gmail.com>
2022-11-16 00:21:51 +0900

    refactor: Result enum 리팩토링

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -2,29 +2,23 @@ package lotto;
 
 public enum Result {
 
-    FIRST(6, false, "6개 일치 (2,000,000,000원) - "),
-    SECOND(5, true, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    THIRD(5, false, "5개 일치 (1,500,000원) - "),
-    FOURTH(4, false, "4개 일치 (50,000원) - "),
-    FIFTH(3, false, "3개 일치 (5,000원) - "),
-    NO_REWARD(0, false, "");
+    FIFTH(3, "3개 일치 (5,000원) - "),
+    FOURTH(4, "4개 일치 (50,000원) - "),
+    THIRD(5, "5개 일치 (1,500,000원) - "),
+    SECOND(5, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    FIRST(6, "6개 일치 (2,000,000,000원) - "),
+    NO_REWARD(0, "");
 
     private final int equalCount;
-    private final boolean isBonus;
     private final String message;
 
-    Result(int equalCount, boolean isBonus, String message) {
+    Result(int equalCount, String message) {
         this.equalCount = equalCount;
-        this.isBonus = isBonus;
         this.message = message;
     }
 
     public int getEqualCount() {
         return equalCount;
-    }
-
-    public boolean getIsBonus() {
-        return isBonus;
     }
 
     public String getMessage() {
