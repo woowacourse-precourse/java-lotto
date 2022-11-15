a153e9f04349088cd4adc6d914d21dd8dea15e2f
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-15 20:14:00 +0900

    feat : 로또 구입 금액 입력이 정수를 벗어날 때와 문자열로 0과 1000단위가 붙여져 주어졌을 때 예외 기능 구현

--- src/main/java/lotto/handler/InputMoneyHandler.java
+++ src/main/java/lotto/handler/InputMoneyHandler.java
@@ -20,17 +20,25 @@ public class InputMoneyHandler {
 
     public void checkIsNumber(String inputMoney) {
         boolean checkNumeric = inputMoney.matches("[+-]?\\d*(\\.\\d+)?");
+        String[] checkInputMoney = inputMoney.split("");
 
         if(!checkNumeric){
             throw new IllegalArgumentException(output.ERROR_ORDER+" 입력 금액은 숫자여야 합니다.");
         }
+
+        if(checkInputMoney.length > 1 && inputMoney.charAt(0) == '0'){
+            throw new IllegalArgumentException(output.ERROR_ORDER+" 입력 금액의 맨 앞에 0이 들어갈 수 없습니다.");
+        }
     }
 
     public void checkPositiveNumber(String inputMoney) {
-        int money = Integer.parseInt(inputMoney);
+        long money = Long.parseLong(inputMoney);
 
         if(money <= 0) {
             throw new IllegalArgumentException(output.ERROR_ORDER+" 입력 금액은 0원 보다 크게 입력해야 합니다.");
+        }
+        if(money > Integer.MAX_VALUE){
+            throw new IllegalArgumentException(output.ERROR_ORDER+" 입력 금액이 입력해야 할 범위를 벗어났습니다.");
         }
     }
 
