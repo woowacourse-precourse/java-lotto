6b4d5360b03dc11fa978a9562a29dd514a109e68
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-15 14:08:25 +0900

    style: 자바 컨벤션 규칙 준수하기 위한 수정

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -33,7 +33,6 @@ public class Lotto {
                 throw new IllegalArgumentException("[ERROR] 발행되는 로또 번호는 중복되지 않아야 합니다.");
             }
         }
-
     }
 
     public List<Integer> getNumbers() {
