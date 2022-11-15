50eadd970cbe802f5fefc38f78968b189b9fe4bc
오승윤 <>
2022-11-15 22:58:25 +0900

    feat (InputViewImple) : 당첨 번호 입력 형식 예외 처리 추가
    SIZE_ERROR_MESSAGE의 안내문 일부를 형식 예외 안내문으로 이동

--- src/main/java/lotto/domain/messages/ErrorMessages.java
+++ src/main/java/lotto/domain/messages/ErrorMessages.java
@@ -1,10 +1,10 @@
 package lotto.domain.messages;
 
 public class ErrorMessages {
-    public static final String SIZE_ERROR_MESSAGE = "로또 번호는 6개 입니다 , 를 구분자로 입력해 주십시오";
+    public static final String SIZE_ERROR_MESSAGE = "로또 번호는 6개 입니다";
     public static final String DUPLICATE_ERROR_MESSAGE = "로또 번호는 서로 중복될 수 없습니다";
     public static final String RANGE_ERROR_MESSAGE = "로또 번호는 1~45사이의 수로 구성되어야 합니다";
     public static final String DUPLICATE_BONUS_ERROR_MESSAGE = "보너스 번호는 당첨 번호와 중복될 수 없습니다";
     public static final String EXPENSES_RANGE_ERROR_MESSAGE = "금액은 1000이상 INTEGER_MAX_VALUE 이하의 1000의 배수여야 합니다";
-
+    public static final String WINNIG_NUMBERS_INPUT_FORMAT_ERROR_MESSAGE = "당첨 번호는 ,를 구분자로 숫자인 문자를 입력해야 합니다";
 }
--- src/main/java/lotto/view/inputView/InputViewImple.java
+++ src/main/java/lotto/view/inputView/InputViewImple.java
@@ -6,6 +6,7 @@ import lotto.view.util.InputUtil;
 import java.util.List;
 
 import static lotto.domain.messages.ErrorMessages.EXPENSES_RANGE_ERROR_MESSAGE;
+import static lotto.domain.messages.ErrorMessages.WINNIG_NUMBERS_INPUT_FORMAT_ERROR_MESSAGE;
 
 public class InputViewImple implements InputView {
 
@@ -21,8 +22,13 @@ public class InputViewImple implements InputView {
 
     @Override
     public List<Integer> inputWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
-        return InputUtil.convertStringSplitToList(Console.readLine());
+        try {
+            System.out.println("당첨 번호를 입력해 주세요.");
+            return InputUtil.convertStringSplitToList(Console.readLine());
+        }catch (Exception e){
+            throw new IllegalArgumentException(WINNIG_NUMBERS_INPUT_FORMAT_ERROR_MESSAGE);
+        }
+
     }
 
     @Override
