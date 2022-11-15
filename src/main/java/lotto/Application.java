c4fe8300e9f0e8f139a540376df15fa1718560fa
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-15 18:26:02 +0900

    feat: 메인에서 구입금액 입력받는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.*;
+import lotto.view.InputMessage;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = new Input();
+        Generator generator = new Generator();
+        Judgement judgement = new Judgement();
+        Calculator calculator = new Calculator();
+        Output output = new Output();
+
+        int pay = askPay();
+        generator.createLottoByPay(pay); //로또n개 생성
+        output.outputLotteries(pay, generator.lotteries); //로또 목록 출력
+    }
+    public static int askPay() {
+        Input input = new Input();
+        String payment = input.payForLotto();
+        Pay pay = new Pay(payment);
+        int payNum = Integer.parseInt(payment) / 1000;
+        return payNum;
     }
 }
