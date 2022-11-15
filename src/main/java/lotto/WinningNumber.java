4760c634da65297348a4a55496fbd32f1e0294f0
bando99 <bando991225@gmail.com>
2022-11-15 16:37:15 +0900

    FEAT : 예외처리 기능 추가

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -6,8 +6,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static lotto.Exceptions.isSameBonusNum;
+import static lotto.Exceptions.isSameNumber;
+
 public class WinningNumber {
-    private List<Integer> numbers = new ArrayList<>();
+    private ArrayList<Integer> numbers = new ArrayList<>();
     private int bonusNumber;
     public WinningNumber(){
     }
@@ -17,10 +20,26 @@ public class WinningNumber {
         splitNumber(winningString);
 
         System.out.println("보너스 번호를 입력하세요");
-        String bonus = Console.readLine();
-        bonusNumber = Integer.parseInt(bonus);
 
-        printNumber();
+        inputBonus();
+
+        validation();
+        //printNumber();
+    }
+    private void validation(){
+        isSameNumber(numbers);
+        isSameBonusNum(numbers,bonusNumber);
+    }
+
+
+    private void inputBonus(){
+        try{
+            String bonus = Console.readLine();
+            bonusNumber = Integer.parseInt(bonus);
+        }
+        catch (NumberFormatException e){
+            System.out.println( "[ERROR] 숫자만 입력해 주세요" + e.getMessage());
+        }
     }
     private void splitNumber(String number){
         String[] stringNumber = number.split(",");
