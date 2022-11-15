3a3285478edc3522f9580ed68da5225fbccea515
ye1ne <yewon7036@naver.com>
2022-11-15 16:48:20 +0900

    feature : make random method

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,5 +8,19 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        List<Integer> numbers = makeRandomNumbers();
+
+    }
+
+    private static List<Integer> makeRandomNumbers() {
+        List<Integer> numberList = new ArrayList<>();
+        while (numberList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 45);
+            if (!numberList.contains(randomNumber)) {
+                numberList.add(randomNumber);
+            }
+        }
+        return numberList;
     }
 }
