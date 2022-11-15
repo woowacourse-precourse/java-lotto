398502e7a788a9d7a543df7648a7649db92524d2
gomljo <gomljo0307@naver.com>
2022-11-15 14:05:38 +0900

    test(WinningsTableTest): 주어진 등수이름과 당첨 테이블과 비교하여 원하는 정보를 찾는 기능 검증

---
+++ src/test/java/lotto/modelTest/WinningsTableTest.java
@@ -0,0 +1,21 @@
+package lotto.modelTest;
+
+import lotto.model.RaffleNumber;
+import lotto.model.WinningsTable;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinningsTableTest {
+    @Test
+    @DisplayName("주어진 등수이름과 당첨 테이블과 비교하여 원하는 정보를 찾는지 확인한다.")
+    public void checkWinsInformationByMatches(){
+        WinningsTable winningsTable = WinningsTable.findByMatches("FIRST");
+        assertThat(winningsTable.name()).isEqualTo("FIRST");
+    }
+}
