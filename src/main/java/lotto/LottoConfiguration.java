855b55709190d2db3eabfe9eb855e4547c4e4710
korjun1993 <corojoon93@naver.com>
2022-11-15 20:09:46 +0900

    feat(lotto) : LottoConfiguration 도메인 추가
    로또에 대한 정보 (로또 구매 금액, 로또 번호 범위, 로또 번호 갯수 등) 을 갖는 설정 파일이다.

---
+++ src/main/java/lotto/LottoConfiguration.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class LottoConfiguration {
+    public static final int START_NUMBER = 1;
+    public static final int END_NUMBER = 45;
+    public static final int SIZE = 6;
+    public static final int PRICE = 1000;
+}
