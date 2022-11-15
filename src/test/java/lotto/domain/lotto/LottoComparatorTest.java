34d9087e4f6567656853bedbaada1768f3d20cbf
JuSeong1130 <jsmoon1130@naver.com>
2022-11-15 17:08:10 +0900

    test(LottoComparator): 로또 번호 숫자 일치 개수 테스트

--- src/test/java/lotto/domain/lotto/LottoComparatorTest.java
+++ src/test/java/lotto/domain/lotto/LottoComparatorTest.java
@@ -25,10 +25,36 @@ class LottoComparatorTest {
 		assertTrue(lottoComparator.hasBonusNumber(lotto));
 	}
 
-	@DisplayName("보보너스 번호 포함 False")
+	@DisplayName(" 번호 포함 False")
 	@Test
 	void hasNumberFalse(){
 		Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
 		assertFalse(lottoComparator.hasBonusNumber(lotto));
 	}
+
+	@DisplayName("로또 숫자 6개 일치 테스트")
+	@Test
+	void 로또번호_6개_일치(){
+		Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+		assertEquals(6,lottoComparator.getMatchCount(lotto));
+	}
+	@DisplayName("로또 숫자 5개 일치 테스트")
+	@Test
+	void 로또번호_5개_일치(){
+		Lotto lotto = new Lotto(List.of(1,2,3,4,5,7));
+		assertEquals(5,lottoComparator.getMatchCount(lotto));
+	}
+	@DisplayName("로또 숫자 4개 일치 테스트")
+	@Test
+	void 로또번호_4개_일치(){
+		Lotto lotto = new Lotto(List.of(1,2,3,4,7,8));
+		assertEquals(4,lottoComparator.getMatchCount(lotto));
+	}
+	@DisplayName("로또 숫자 3개 일치 테스트")
+	@Test
+	void 로또번호_3개_일치(){
+		Lotto lotto = new Lotto(List.of(1,2,3,7,8,9));
+		assertEquals(3,lottoComparator.getMatchCount(lotto));
+	}
+
 }
