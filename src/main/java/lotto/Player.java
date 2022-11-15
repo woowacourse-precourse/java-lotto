fae08c2f97bcbc4b137aff3c77d38b15f763166c
delaylee <delaylee219@gmail.com>
2022-11-15 15:25:08 +0900

    feat: 당첨 번호 입력 받기 기능 추가

--- src/main/java/lotto/Player.java
+++ src/main/java/lotto/Player.java
@@ -2,9 +2,11 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
 
-    private String pay;
     private int lottoCount;
 
     public int getLottoCount() {
@@ -13,12 +15,13 @@ public class Player {
 
     public void buyLotto() {
         System.out.println("구입금액을 입력해 주세요.");
-        pay = Console.readLine();
+        String pay = Console.readLine();
         payValidate(pay);
         lottoCount = Integer.parseInt(pay) / 1000;
+        printCount();
     }
 
-    private void payValidate(String input) {
+    public void payValidate(String input) {
         Integer price;
         try {
             price = Integer.parseInt(input);
@@ -30,5 +33,38 @@ public class Player {
         if (price % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위만 입력 가능합니다");
         }
+    }
+
+    private void printCount() {
+        System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+
+    public void getLottoNum() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String numbers = Console.readLine();
+        List<Integer> integers = stringToList(numbers);
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        //보너스 입력 구현
+    }
+
+    public List<Integer> stringToList(String numbers) {
+        String[] split = numbers.split(",");
+        List<Integer> integers = new ArrayList<>();
+
+        for (String s : split) {
+            try {
+                integers.add(Integer.parseInt(s));
+            }
+            catch (NumberFormatException ex) {
+                throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다");
+            }
+        }
+
+        if (integers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해야합니다");
+        }
+
+        return integers;
     }
 }
