9e7f4ea2206209513cb43cebf371fcfea35fa3fb
nakalic <nakalic28@gmail.com>
2022-11-15 17:27:35 +0900

    test(LottoValidatorTest): 보너스 번호를 검증하는 테스트 코드 추가

---
+++ src/test/java/lotto/Validator/LottoValidatorTest.java
@@ -0,0 +1,43 @@
+package lotto.Validator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import lotto.Domain.Lotto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoValidatorTest {
+
+    private LottoValidator lottoValidator;
+
+    @BeforeEach
+    void setUp() {
+        lottoValidator = new LottoValidator();
+    }
+    @DisplayName("로또 번호가 1~45사이의 숫자라면 true를 반환한다")
+    @Test
+    void checkNumber() {
+        assertThat(lottoValidator.checkLottoRange(45)).isTrue();
+    }
+
+
+    @DisplayName("보너스 번호가 당첨 번호에 포함되어 있으면 예외가 발생한다.")
+    @Test
+    void createNotNaturalNumber1() {
+        assertThatThrownBy(() -> {
+            lottoValidator.checkBonusContain(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 6);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 1~45 사이의 숫자가 아니라면 예외가 발생한다.")
+    @Test
+    void createNotNaturalNumber2() {
+        assertThatThrownBy(() -> {
+            lottoValidator.checkBonusRange(46);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
