36b609feab86816f23f306456a197bc2ca4ad1b6
hhiyeon <hhyyeon0214@gmail.com>
2022-11-15 15:40:38 +0900

    test: rank(test)-find rank by match number

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -1,4 +1,17 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RankTest {
+    @DisplayName("일치하는 번호 개수로 Rank 찾기 성공")
+    @Test
+    void findRankByMatchNumber() {
+        Rank rank = Rank.findRankByMatch(3);
+        Rank result = Rank.FIFTH_RANK;
+
+        assertThat(result).isEqualTo(rank);
+    }
 }
