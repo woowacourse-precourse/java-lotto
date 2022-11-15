2d0bab8146f5ab623165530d3987f76abd1050f1
jasonoh22 <jasonoh22@naver.com>
2022-11-15 20:21:25 +0900

    FEAT[java-lotto]: 중복 숫자 있는지 확인 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,17 +1,25 @@
 package lotto;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        sameNumValidate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void sameNumValidate(List<Integer> numbers){
+        Set<Integer> numSet = new HashSet<>(numbers);
+        if(numSet.size() != numbers.size()){
             throw new IllegalArgumentException();
         }
     }
