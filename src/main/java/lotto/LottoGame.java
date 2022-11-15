0868e8d5fddca65e0473f646089061e4b1bdf8c7
masiljangajji <hjk172262@gmail.com>
2022-11-15 23:29:34 +0900

    refactor: LottoGame Class 변수명,메소드명 정리

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -3,67 +3,69 @@ package lotto;
 import view.Input;
 import model.Data;
 import view.MessagePrint;
-import java.util.ArrayList;
+
 import java.util.Collections;
-import java.util.List;
 
-public class LottoGame{
-
+public class LottoGame {
     public void playLottoGame() {
 
-        Data data = new Data();
-        MessagePrint message = new MessagePrint();
+        try {
+            Exception exception = new Exception();
+            Data data = new Data();
+            MessagePrint message = new MessagePrint();
 
-        message.printInputAmount();
-        data.purchaseAmount= Input.inputNumber();
-        data.getNumberOfLotto();
+            message.printInputAmount();
 
-        message.printLottoCount(data.numberOfLotto);
+            data.purchaseAmount = Input.inputNumber();
+            exception.checkPurchaseAmount(data.purchaseAmount);
 
-        data.initializeUserLotto();
+            data.makeNumberOfLotto();
+            message.printLottoCount(data.numberOfLotto);
 
-        for(int i=0;i<data.numberOfLotto;i++){
-            data.userLotto[i].addAll(Input.getRandomNumber());
-            Collections.sort(data.userLotto[i]);
-            new Lotto(data.userLotto[i]);
+            data.initializeUserLotto();
+
+            for (int i = 0; i < data.numberOfLotto; i++) {
+                data.userLotto[i].addAll(Input.makeRandomNumber());
+                Collections.sort(data.userLotto[i]);
+            }
+
+            message.printUserLottoNumber(data.userLotto, data.numberOfLotto);
+
+
+            message.printInputWinningNumber();
+
+
+            data.winningNumber = Input.inputNumber();
+            data.winningNumberSplit = data.winningNumber.split(",");
+            for (int i = 0; i < data.winningNumberSplit.length; i++) {
+                exception.winningNumberRange(data.winningNumberSplit[i]);
+            }
+            data.makeWinningLottoNumber();
+
+            new Lotto(data.winningLottoNumber);
+
+
+            message.printInputBonusNumber();
+            data.BonusNumber = Input.inputNumber();
+            exception.checkBonusNumber(data.BonusNumber);
+            exception.checkBonusNumberOverLap(data.BonusNumber, data.winningLottoNumber);
+            message.printStatistics();
+
+            data.initializeWinningStatistics();
+
+            for (int i = 0; i < data.userLotto.length; i++) {
+                data.makeWinningStatistics(data.userLotto[i], data.checkLottoNumber(data.userLotto[i]));
+            }
+
+            message.printWinningStatistics(data.winningStatistics);
+
+            data.makeYield();
+
+            message.printYield(data.yield);
+        } catch (IllegalArgumentException e) {
+            System.out.print(e.getMessage());
+            return;
         }
-
-        message.printUserLotto(data.userLotto, data.numberOfLotto);
-
-
-        message.printInputWinningNumber();
-
-
-        data.winningNumber=Input.inputNumber();
-
-
-        String[] winningNumberSplit = data.winningNumber.split(",");
-        for (int i = 0; i < winningNumberSplit.length; i++) {
-            data.winningLottoNumber.add(Integer.valueOf(winningNumberSplit[i]));
-        }
-
-        new Lotto(data.winningLottoNumber);
-
-
-        message.printBonusNumber();
-        data.BonusNumber=Input.inputNumber();
-
-        message.printSTATISTICS();
-
-        data.initializeMap();
-
-        for (int i = 0; i < data.userLotto.length; i++) {
-            int checkLotto = data.checkLottoNumber(data.userLotto[i]);
-            data.map(data.userLotto[i], checkLotto);
-        }
-
-        message.printCheckLotto(data.Win);
-
-        data.getYield();
-
-
-        message.printYield(data.yield);
-
     }
 
 }
