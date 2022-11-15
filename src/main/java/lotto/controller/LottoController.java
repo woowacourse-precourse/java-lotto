bbd10b587a79239100e9b192d5b6fa46861e2a72
kite707 <kite707707@gmail.com>
2022-11-15 18:12:05 +0900

    feat(LottoList): LottoList 출력 기능

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,15 +3,22 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.model.Lotto;
+import lotto.model.LottoList;
 
 public class LottoController {
-    List<Lotto> lottoList = new ArrayList<>();
-    public void makeLottoList(int publicians) {
+    private List<Lotto> lottoList = new ArrayList<>();
+
+    public LottoController(int publicians) {
+        System.out.println("\n" + publicians + "개를 구매했습니다.");
         for (int i = 0; i < publicians; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Lotto lotto = new Lotto(numbers);
             lottoList.add(lotto);
         }
     }
-
+    public void printLottoList(){
+        for(int i=0;i<lottoList.size();i++){
+            lottoList.get(i).printLotto();
+        }
+    }
 }
