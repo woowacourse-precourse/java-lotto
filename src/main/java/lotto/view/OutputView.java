ac84f4e811a4fd5e5f80e1ffac00516f9971dd87
desfox <desfox99@naver.com>
2022-11-15 16:27:36 +0900

    feat(OutputView): 당첨 내역 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,8 @@ package lotto.view;
 
 import java.util.List;
 
+import static lotto.domain.Rank.*;
+
 public class OutputView {
     public void printPurchasePriceMessage() {
         System.out.println("구입금액을 입력해 주세요.");
@@ -22,4 +24,15 @@ public class OutputView {
     public void printAmountOfLottoMessage(int amountOfLotto) {
         System.out.println(amountOfLotto + "개를 구매했습니다.");
     }
+
+    public void printWinningLog(int[] winningLog) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (" + FIFTH.getPrize() + "원) -  " + winningLog[FIFTH.getWinningLogIdx()] + "개");
+        System.out.println("4개 일치 (" + FOURTH.getPrize() + "원) -  " + winningLog[FOURTH.getWinningLogIdx()] + "개");
+        System.out.println("5개 일치 (" + THIRD.getPrize() + "원) -  " + winningLog[THIRD.getWinningLogIdx()] + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (" + SECOND.getPrize() + "원) -  " + winningLog[SECOND.getWinningLogIdx()] + "개");
+        System.out.println("6개 일치 (" + FIRST.getPrize() + "원) -  " + winningLog[FIRST.getWinningLogIdx()] + "개");
+    }
+
 }
