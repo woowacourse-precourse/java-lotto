6f4c6af47ebe787210c27f43c7f7385387993280
hantae <hantae96@naver.com>
2022-11-15 14:25:28 +0900

    Feat : 사용자에게 로또 구입금액을 입력받고 1000단위인지 확인 하는 코드 추가

---
+++ src/main/java/lotto/Constants.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public final class Constants {
+    public static final int DIV_MONEY = 1000;
+
+    //errorCode
+    public static final String ERROR_DIV = "[ERROR]1000단위로 나눠지는 숫자를 입력하세요";
+}
