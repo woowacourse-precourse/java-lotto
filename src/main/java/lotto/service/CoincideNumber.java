5f4dd29e270d500dbc8c0a51a32b2d914445340c
yoonjuho <dbswn990@naver.com>
2022-11-15 10:55:33 +0900

    refactor(CoincideNumber) : rank 값 변경

--- src/main/java/lotto/service/CoincideNumber.java
+++ src/main/java/lotto/service/CoincideNumber.java
@@ -6,10 +6,10 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum CoincideNumber {
-    THREE_COINCIDE("3개 일치 (5,000원) - ",5,0,5000),
-    FOUR_COINCIDE("4개 일치 (50,000원) - ",4,0,50000),
-    FIVE_COINCIDE("5개 일치 (1,500,000원) - ",3,0,1500000),
-    FIVE_BONUS_COINCIDE("5개 일치, 보너스 볼 일치 (30,000,000원) - ",2,0,30000000),
+    THREE_COINCIDE("3개 일치 (5,000원) - ",4,0,5000),
+    FOUR_COINCIDE("4개 일치 (50,000원) - ",3,0,50000),
+    FIVE_COINCIDE("5개 일치 (1,500,000원) - ",2,0,1500000),
+    FIVE_BONUS_COINCIDE("5개 일치, 보너스 볼 일치 (30,000,000원) - ",5,0,30000000),
     SIX_COINCIDE("6개 일치 (2,000,000,000원) - ",1,0,2000000000);
     private String phrase;
     private int rank;
@@ -39,7 +39,7 @@ public enum CoincideNumber {
         BY_RANK.get(rank).plusCount();
     }
     public int calculateRevenue(){
-        return this.count * this.money;
+        return (this.count * this.money);
     }
 
 }
