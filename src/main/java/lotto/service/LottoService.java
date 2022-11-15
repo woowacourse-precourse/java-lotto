3e03fb19c6ec55630d9f7a0f2baf3de66d5e0c47
hhiyeon <hhyyeon0214@gmail.com>
2022-11-15 12:52:19 +0900

    feat: service-add total profit for winning prize

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,6 +1,7 @@
 package lotto.service;
 
 import lotto.domain.Lotto;
+import lotto.domain.Rank;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -50,5 +51,13 @@ public class LottoService {
     private String getLottoRate() {
         DecimalFormat RateFormat = new DecimalFormat("###,###.0");
         return RateFormat.format((totalProfit / buyMoney) * DECIMAL_PLACE);
+    }
+
+    private void addProfit(int match, boolean flag) {
+        if (match == Rank.THIRD_RANK.getMatch() && flag) {
+            totalProfit += Rank.SECOND_RANK.getPrize();
+            return;
+        }
+        totalProfit += Rank.findRankByMatch(match).getPrize();
     }
 }
