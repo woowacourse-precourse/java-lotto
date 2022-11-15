b531c29fef1ae1f040773b75804d2d0d97a33128
cutthebutter <42573282+cutthebutter@users.noreply.github.com>
2022-11-15 11:41:29 +0900

    number>BonusNumber

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
