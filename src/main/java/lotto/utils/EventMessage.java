883f612589f4a98a09af44b7e82517350881e2c1
hongjinwon <herenever@naver.com>
2022-11-15 17:09:00 +0900

    refactor(utils): EventMessage
    EventMessage 문구 추가

--- src/main/java/lotto/utils/EventMessage.java
+++ src/main/java/lotto/utils/EventMessage.java
@@ -3,9 +3,12 @@ package lotto.utils;
 public enum EventMessage {
 
     PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
+    ISSUED_AMOUNT("%s개를 구매했습니다."),
     WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
     BONUS_NUMBERS("보너스 번호를 입력해 주세요."),
-    STATISTICS("당첨 통계");
+    STATISTICS_HEADER("당첨 통계\n---"),
+    STATISTICS_BODY("%d개 일치 (%s원) - %s개"),
+    STATISTICS_FOOTER("총 수익률은 %s%입니다.");
 
     public final String message;
     EventMessage(String message){
