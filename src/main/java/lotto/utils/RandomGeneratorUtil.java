6bfd0afce8be341a83600cbca1e9b7ef8440d591
kim1387 <kevink0@naver.com>
2022-11-15 17:24:38 +0900

    fix: 로또번호 생성후 정렬하도록 로직 변경

--- src/main/java/lotto/utils/RandomGeneratorUtil.java
+++ src/main/java/lotto/utils/RandomGeneratorUtil.java
@@ -2,6 +2,7 @@ package lotto.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Collections;
 import java.util.List;
 
 import static lotto.domain.LottoConfig.*;
@@ -12,7 +13,9 @@ public class RandomGeneratorUtil {
     }
 
     public static List<Integer> generateDistinctSixRandomNumberInRangeOneToFortyFive() {
-        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER.getNumber(), MAX_NUMBER.getNumber(), MAX_LENGTH.getNumber());
+        List<Integer> integers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER.getNumber(), MAX_NUMBER.getNumber(), MAX_LENGTH.getNumber());
+        Collections.sort(integers);
+        return integers;
     }
 
 }
