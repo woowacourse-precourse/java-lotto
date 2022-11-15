ece9ce949a4e7586dc9ba567bc1d07a4baf21b85
hyeongu <wancern@naver.com>
2022-11-15 23:06:38 +0900

    feat(Game): 입력 금액만큼 로또를 생성하는 기능 추가

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,8 +1,11 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static lotto.Validation.moneyValidate;
 
@@ -15,6 +18,9 @@ public class Game {
         // 구매 금액 입력 및 타당성 검증
         String money = inputString("구입금액을 입력해 주세요.");
         int convertMoney = moneyValidate(money);
+
+        // 금액만큼 랜덤으로 번호 생성
+        randomLottos = makeLottosList(money);
     }
 
     private String inputString(String message){
@@ -24,4 +30,25 @@ public class Game {
         return input;
     }
 
+    private List<Lotto> makeLottosList(String money){
+        int times = Integer.parseInt(money) / 1000;
+        List<Lotto> randomLottos = new ArrayList<>();
+
+        while(times-- > 0){
+            Lotto newLotto = makeLotto();
+            randomLottos.add(newLotto);
+        }
+
+        return randomLottos;
+    }
+
+    private Lotto makeLotto(){
+        Lotto newLotto = new Lotto(makeNumbers());
+        return newLotto;
+    }
+
+    private List<Integer> makeNumbers(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers.stream().sorted().collect(Collectors.toList());
+    }
 }
