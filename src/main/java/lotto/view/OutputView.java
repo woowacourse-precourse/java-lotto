8ccac83699b8233d87c626b24bc76b863b92dbf5
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-15 15:10:48 +0900

    chore(OutputView): LOTTO_RESULT_MESSAGE 공백 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,7 +10,7 @@ public class OutputView {
     private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
     private static final String LOTTO_STATISTICS_MESSAGE = "당첨 통계";
     private static final String DIVISION_LINE = "---";
-    private static final String LOTTO_RESULT_MESSAGE = "- %d개";
+    private static final String LOTTO_RESULT_MESSAGE = " - %d개";
 
     public static void printPurchasedLotto(int lottoCount) {
         System.out.println(lottoCount + PURCHASE_MESSAGE);
@@ -20,5 +20,17 @@ public class OutputView {
         for (Lotto lotto : lotties) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public static void printLottoStatistics(Map<Reward, Integer> lottiesResult) {
+        System.out.println(LOTTO_STATISTICS_MESSAGE);
+        System.out.println(DIVISION_LINE);
+
+        for (Map.Entry<Reward, Integer> eachReward : lottiesResult.entrySet()) {
+            Reward reward = eachReward.getKey();
+            System.out.println(reward.getMessage() +
+                    String.format(LOTTO_RESULT_MESSAGE, eachReward.getValue()));
+        }
+
     }
 }
