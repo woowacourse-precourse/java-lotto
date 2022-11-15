40dc24a5238654507d77329310f078f1bf9cea86
신채원 <cw121@naver.com>
2022-11-15 19:03:46 +0900

    feat: 실행함수 구현 및 기능 구현 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,12 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try{
+            LottoController lottoController = new LottoController();
+            lottoController.run();
+        }catch (IllegalArgumentException exception) {
+            exception.printStackTrace();
+        }
+
     }
 }
---
+++ src/main/java/lotto/LottoController.java
@@ -0,0 +1,58 @@
+package lotto;
+
+import java.util.List;
+
+import static lotto.InputView.inputBonusNumber;
+import static lotto.InputView.inputPurchase;
+import static lotto.InputView.inputWinLotto;
+import static lotto.Lottos.createRandomLottos;
+
+public class LottoController {
+
+    public void run() {
+        int purchase = getPurchase();
+
+        List<List<Integer>> randomLottos = getRandomLottos(purchase);
+        ResultView.printRandomLottos(randomLottos,purchase / 1000);
+
+        List<Integer> WinLotto = getWinLotto();
+        int bonus = getBonusNumber();
+
+        printResult(WinLotto, randomLottos, bonus, purchase);
+    }
+
+    private int getPurchase() {
+        int inputPurchase = inputPurchase();
+        Purchase purchase = new Purchase(inputPurchase);
+        return purchase.getPurchase();
+    }
+
+    private List<List<Integer>> getRandomLottos(int purchase) {
+        return createRandomLottos(purchase / 1000);
+    }
+
+    private List<Integer> getWinLotto() {
+        List<Integer> inputWinLotto = inputWinLotto();
+
+        Lotto lotto = new Lotto(inputWinLotto);
+        return lotto.getLotto();
+    }
+
+    private int getBonusNumber() {
+        int inputBonusNumber = inputBonusNumber();
+        BonusNumber bonusNumber = new BonusNumber(inputBonusNumber);
+        return bonusNumber.getBonusNumber();
+    }
+
+    private void printResult(List<Integer> WinLotto, List<List<Integer>> randomLottos, int bonus, int purchase) {
+        Lotto lotto = new Lotto(WinLotto);
+        RankResult rankResult = new RankResult();
+
+        rankResult.initEnumMap();
+        rankResult.addRankMapCount(lotto.getLotto(), randomLottos, bonus);
+
+        ResultView.printResult(rankResult);
+        ResultView.printProfit(rankResult, purchase);
+    }
+
+}
