83f9ca86ba36dc6e81620f05e87f2a9951b109ae
yujindonut <hyujin0825@gmail.com>
2022-11-15 20:56:44 +0900

    feat: 로또 번호 생성 기능
    로또 번호를 생성하는 기능

---
+++ src/main/java/lotto/NumberGenerator.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberGenerator {
+  public List<List<Integer>> createNumbers(int count) {
+    List<List<Integer>> number = new ArrayList<>();
+    for (int i = 0; i < count; i++) {
+      List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+      number.add(numbers);
+    }
+    return number;
+  }
+}
