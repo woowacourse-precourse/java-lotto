c0c0fefc5798915eb4fcecb46217e42ff65bfc1c
lqsml <dvlprsml@gmail.com>
2022-11-15 22:46:19 +0900

    feat: 상수 선언

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.*;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+	
+	static final String PAY_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+	static final String PAY_CHECK_MESSAGE = "개를 구매했습니다.";
+	static final String WIN_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+	static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+	static final String ERROR_MESSAGE = "[ERROR]";
+	
+	static final int MIN_NUMBER = 1;
+	static final int MAX_NUMBER = 45;
+	static final int LOTTO_PRICE = 1000;
+	static final long[] PRIZE_BY_RANK = {2000000000, 30000000, 1500000, 50000, 5000};
+    
+	public static void main(String[] args) {
+	
+        
     }
+    
 }
