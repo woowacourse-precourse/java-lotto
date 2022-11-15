64cf0bc48ba827f0547a95e893205e676df931c7
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-15 10:40:55 +0900

    refactor(ErrorMessage): divide message type

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -1,14 +1,15 @@
 package lotto.constant;
 
 public enum ErrorMessage {
-    LENGTH_OF_LOTTO_NUMBERS_SHOULD_BE_SIX("[ERROR] 로또 번호들은 6개의 숫자로 구성되어야 합니다."),
-    LOTTO_NUMBERS_SHOULD_BE_UNIQUE("[ERROR] 로또 번호들은 서로 다른 수여야 합니다."),
-    PAYMENT_UNIT_SHOULD_BE_1000("[ERROR] 구입 금액은 1,000원 단위의 수여야 합니다."),
-    FORM_OF_WINNING_NUMBERS_IS_WRONG("[ERROR] 당첨 번호들의 형태가 잘못되었습니다."),
-    VALUES_OF_WINNING_NUMBERS_ARE_WRONG("[ERROR] 당첨 번호들의 값들이 잘못되었습니다."),
-    FORM_OF_BONUS_NUMBER_IS_WRONG("[ERROR] 보너스 번호 형태가 잘못되었습니다."),
-    BONUS_NUMBER_AND_WINNING_NUMBER_IS_DUPLICATED("[ERROR] 보너스 번호가 당첨 번호와 중복되었습니다.");
+    LENGTH_OF_LOTTO_NUMBERS_SHOULD_BE_SIX("로또 번호들은 6개의 숫자로 구성되어야 합니다."),
+    LOTTO_NUMBERS_SHOULD_BE_UNIQUE("로또 번호들은 서로 다른 수여야 합니다."),
+    PAYMENT_UNIT_SHOULD_BE_1000("구입 금액은 1,000원 단위의 수여야 합니다."),
+    FORM_OF_WINNING_NUMBERS_IS_WRONG("당첨 번호들의 형태가 잘못되었습니다."),
+    VALUES_OF_WINNING_NUMBERS_ARE_WRONG("당첨 번호들의 값들이 잘못되었습니다."),
+    FORM_OF_BONUS_NUMBER_IS_WRONG("보너스 번호 형태가 잘못되었습니다."),
+    BONUS_NUMBER_AND_WINNING_NUMBER_IS_DUPLICATED("보너스 번호가 당첨 번호와 중복되었습니다.");
 
+    private final String messageType = "[ERROR] ";
     private final String errorMessage;
 
     ErrorMessage(String errorMessage) {
@@ -16,11 +17,11 @@ public enum ErrorMessage {
     }
 
     public String getErrorMessage() {
-        return errorMessage;
+        return messageType + errorMessage;
     }
 
     @Override
     public String toString() {
-        return getErrorMessage();
+        return messageType + errorMessage;
     }
 }
