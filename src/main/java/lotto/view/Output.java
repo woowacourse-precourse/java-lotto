1b94a5a2c5ae9ca07835be8871b203810d2f2d18
mikekang47 <gyeong0308@gmail.com>
2022-11-15 18:01:35 +0900

    Feat<Output>: 사용자 구매 갯수 출력

---
+++ src/main/java/lotto/view/Output.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+public class Output {
+    private Output() {
+    }
+
+    public static void printNumberOfPurChase(int purchaseAmount) {
+        System.out.println();
+        System.out.println(purchaseAmount + "개를 구매했습니다.");
+    }
+}
