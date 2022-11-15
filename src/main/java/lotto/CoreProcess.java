129331a1f0a813c98dc3df4cb0c2ffc7192e75d1
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-15 09:14:20 +0900

    feat : Lotto 구현 주요 기능 클래스의 번호 비교 확인기능과 보너스 번호 확인 기능
    비교 기능의 변수를 정리 하고, 번호를 불린형으로 확인한후, 2등을 출력하도록 메서드를 구현하였다.

--- src/main/java/lotto/CoreProcess.java
+++ src/main/java/lotto/CoreProcess.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -9,17 +10,49 @@ public class CoreProcess {
 
     ProcessData bring = new ProcessData();
 
+
     public List<Integer> setLottoNumber() {
         List<Integer> LottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(LottoNumber);
         return LottoNumber;
     }
 
     public int compareNumber(List<Integer> Lotto, List<Integer> pickedNumber) {
         List<Integer> overlapList = Lotto.stream()
-                .filter(old -> pickedNumber.stream()
-                        .anyMatch(Predicate.isEqual(old)))
+                .filter(num -> pickedNumber.stream()
+                        .anyMatch(Predicate.isEqual(num)))
                 .collect(Collectors.toList());
         return overlapList.size();
+    }
+
+    public boolean checkBonus(List<Integer> Lotto, int bonus) {
+        boolean check = false;
+        for (int i = 0; i < Lotto.size(); i++) {
+            if (Lotto.get(i) == bonus) {
+                check = true;
+            }
+        }
+        return check;
+    }
+
+    public winPrize checkSecond(boolean bonus) {
+        if (bonus) {
+            return winPrize.SECOND;
+        }
+        return null;
+    }
+
+
+    public winPrize setResult(int compareNumber, boolean bonus) {
+        if (compareNumber == winPrize.SECOND.count) {
+            return checkSecond(bonus);
+        }
+        for (winPrize value : winPrize.values()) {
+            if (value.count == compareNumber) {
+                return value;
+            }
+        }
+        return null;
     }
 
 }
