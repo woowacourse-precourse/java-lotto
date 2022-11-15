aaa72c64df2f373b9d005000d0d200e6b843a596
lynne921 <dkfls921@gmail.com>
2022-11-15 18:47:31 +0900

    fead(LottoTicket): 생성된 로또 번호를 오름차순으로 정렬하는 기능 추가
    - 생성된 로또 번호를 오름차순으로 정렬하는 기능 추가
    - 이를 검증하는 테스트 코드 추가

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -2,18 +2,23 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoTicket {
-    private List<Integer> lotto;
+    private Integer[] lotto;
 
     public List<Integer> makeLotto() {
-        lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6).toArray(new Integer[0]);
         printLotto();
-        return lotto;
+        return new ArrayList<>(Arrays.asList(lotto));
     }
 
     private void printLotto() {
-        System.out.println(lotto);
+        Arrays.sort(lotto);
+        System.out.println(Arrays.toString(lotto));
     }
 }
--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -22,4 +22,16 @@ public class LottoTicketTest {
                 List.of(1, 3, 6, 7, 8, 9)
         );
     }
+
+    @DisplayName("구매한 로또 번호를 오름차순으로 출력한다.")
+    @Test
+    void createAndPrintLottoTicket() {
+        List<Integer> random = List.of(1, 7, 8, 9, 15, 42);
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    assertThat(lottoTicket.makeLotto()).isEqualTo(random);
+                },
+                List.of(42, 8, 1, 7, 15, 9)
+        );
+    }
 }
