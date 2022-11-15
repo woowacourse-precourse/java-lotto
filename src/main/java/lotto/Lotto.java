779730f297e15fafc6f555c7dbf388d8fd51d9f7
sungjun kim <dd8873@naver.com>
2022-11-15 20:27:32 +0900

    feat : 정적팩토리메소드 추가, 로또 번호에 보너스 번호가 있는지 체크하는 기능, 구입한 로또 번호와 당첨 번호 비교 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,51 @@
 package lotto;
 
-import java.util.Collections;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static lotto.LottoFactory.LOTTO_COUNT;
+import static lotto.Results.DEFAULT;
+import static lotto.Results.INCREASED_VALUE;
 
 public class Lotto {
 
     public static final int LOTTO_NUMBER_LENGTH_ZERO = 0;
+    private static final String NUMBER_SPLIT_CRITERIA = ",";
 
     private final List<Integer> numbers;
 
+    public static Lotto from(String winningNumber) {
+        try {
+            return new Lotto(Arrays.stream(winningNumber.split(NUMBER_SPLIT_CRITERIA))
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .collect(Collectors.toList()));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호중 숫자가 아닌 값이 있습니다.");
+        }
+    }
+
     public Lotto(List<Integer> numbers) {
         this.numbers = validateLottoNumber(numbers);
+    }
+
+    public boolean isBonusNumber(LottoBonusNumber lottoBonusNumber) {
+        return lottoBonusNumber.isBonusNumber(this.numbers);
+    }
+
+    public int repeatLottoNumberCheck(Lotto lottoNumber) {
+        int lottoNumberMatchCount = DEFAULT;
+        for (int i : lottoNumber.numbers) {
+            lottoNumberMatchCount = compareLottoNumber(i, lottoNumberMatchCount);
+        }
+        return lottoNumberMatchCount;
+    }
+
+    private int compareLottoNumber(int i, int lottoNumberMatchCount) {
+        if (this.numbers.contains(i)) {
+            return lottoNumberMatchCount + INCREASED_VALUE;
+        }
+        return lottoNumberMatchCount;
     }
 
     private List<Integer> validateLottoNumber(List<Integer> numbers) {
