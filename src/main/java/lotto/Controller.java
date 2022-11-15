dce4cab075e3f113e084f504458409295659659d
chj <bluedog129@gmail.com>
2022-11-15 23:17:37 +0900

    feat(Controller) : 당첨내역 출력, 예외처리 컨트롤러 구성 출력

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -1,6 +1,8 @@
 package lotto;
 
 
+import java.util.List;
+
 public class Controller {
     private final Model model = new Model();
     private final View view = new View();
@@ -16,5 +18,28 @@ public class Controller {
     public void purchaseLotto() {
         this.model.generateLotto();
         this.view.printPurchase(this.model.getLottoList());
+    }
+
+    public void inputWinningNumber() {
+        List<String> input = view.inputWinNumber();
+        this.exceptionController.validateWinNumIsInt(input);
+        this.model.setWinningLotto(input);
+    }
+
+    public void inputBonusNumber() {
+        String input = this.view.inputBonus();
+        this.exceptionController.validateBonus(input);
+        this.model.setBonus(Integer.parseInt(input));
+    }
+
+    public void printStatistics() {
+        this.model.compare();
+        this.view.printStatistics(this.model.getSameNumbers());
+    }
+
+    public void printReturnRate() {
+        this.model.calculateReturn();
+        double returnRate = this.model.getReturnRate();
+        this.view.printReturnRate(returnRate);
     }
 }
