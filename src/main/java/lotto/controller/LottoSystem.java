1f705b9f59138e0baf5b171b3c8dd54d401aa6c7
yunjeong <uu0078@naver.com>
2022-11-15 21:27:58 +0900

    feat: 발행한 로또 번호 예외 체크

--- src/main/java/lotto/controller/LottoSystem.java
+++ src/main/java/lotto/controller/LottoSystem.java
@@ -11,6 +11,9 @@ import java.util.List;
 
 public class LottoSystem {
     public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_MIN = 1;
+    public static final int LOTTO_MAX = 45;
+    public static final int LOTTO_NUM_COUNT = 6;
 
     private final View view = new Terminal();
 
@@ -18,10 +21,10 @@ public class LottoSystem {
         MoneyParser moneyParser = new MoneyParser();
         int lottoCount = moneyParser.parse(view.requestMoney()) / LOTTO_PRICE;
         view.printLottoCount(lottoCount);
-        List<Lotto> lottoList = new ArrayList<>();
+        List<Lotto> boughtLottos = new ArrayList<>();
         for(int i = 0; i < lottoCount; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            lottoList.add(new Lotto(numbers));
+            boughtLottos.add(new Lotto(numbers));
         }
     }
 }
