15868404c5dfb747973da7b8db7d18262cfc8bcb
zeunala <zeunala@naver.com>
2022-11-15 14:12:29 +0900

    feat: 구입한 모든 로또의 등수확인 기능 추가

--- src/main/java/lotto/utils/LottoCompare.java
+++ src/main/java/lotto/utils/LottoCompare.java
@@ -2,7 +2,10 @@ package lotto.utils;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoAnswer;
+import lotto.domain.LottoOrder;
 import lotto.domain.LottoRank;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class LottoCompare {
     /**
@@ -24,5 +27,28 @@ public class LottoCompare {
         }
 
         return null;
+    }
+
+    /**
+     * 구입한 모든 로또와 당첨/보너스 번호를 비교해 각 등수별 당첨 횟수 반환
+     *
+     * @param lottoOrder  구입한 로또들
+     * @param lottoAnswer 당첨/보너스 번호
+     * @return 각 등수별 당첨 횟수
+     */
+    public static Map<LottoRank, Integer> compareAllTickets(LottoOrder lottoOrder, LottoAnswer lottoAnswer) {
+        Map<LottoRank, Integer> totalRank = new LinkedHashMap<>();
+
+        for (LottoRank lottoRank : LottoRank.values()) {
+            totalRank.put(lottoRank, 0);
+        }
+        for (Lotto lotto : lottoOrder.getLottoTickets()) {
+            LottoRank lottoRank = compareOneTicket(lotto, lottoAnswer);
+            if (lottoRank != null) {
+                totalRank.put(lottoRank, totalRank.get(lottoRank) + 1);
+            }
+        }
+
+        return totalRank;
     }
 }
