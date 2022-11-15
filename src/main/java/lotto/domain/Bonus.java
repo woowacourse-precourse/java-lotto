1921828c4b053131085e19d24ac70e881d7fa701
kmularise <kmula222@gmail.com>
2022-11-15 10:18:10 +0900

    feat(lotto): make function of saving bonus number

---
+++ src/main/java/lotto/domain/Bonus.java
@@ -0,0 +1,41 @@
+package lotto.domain;
+
+import lotto.domain.Lotto;
+
+public class Bonus {
+    private final int number;
+
+    public Bonus(int number, Lotto lotto) {
+        validate(number, lotto);
+        this.number = number;
+    }
+
+    private void validate(int number, Lotto lotto) {
+        if (!isValidRange(number)) {
+            throw new IllegalArgumentException();
+        }
+        if (hasSameNumber(number, lotto)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private Boolean isValidRange(int number) {
+        if (number < 1 || number > 45) {
+            return false;
+        }
+        return true;
+    }
+
+    private Boolean hasSameNumber(int number, Lotto lotto) {
+        for (int comparedNumber : lotto.getNumbers()) {
+            if (number == comparedNumber) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
