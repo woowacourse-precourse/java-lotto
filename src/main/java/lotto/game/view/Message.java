05158e7ba95132f9f8d5d1d1898fde779f02198b
GwansuShin <happysun7080@naver.com>
2022-11-15 11:39:34 +0900

    refactor(Message,Constant): 메시지 열거형, 상수명 개선
    프로젝트 내 일관성을 위해 Message, Constant 수정
    수익률 출력 메시지 추가

--- src/main/java/lotto/game/view/Message.java
+++ src/main/java/lotto/game/view/Message.java
@@ -8,17 +8,20 @@ public enum Message {
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
 
     OUTPUT_LOTTO_NUMBERS("개를 구매했습니다."),
-    OUTPUT_WIN_STATS("당첨 통계"),
+    OUTPUT_MATCH_TOTAL("당첨 통계"),
     OUTPUT_THREE_MATCHES("3개 일치 ("
-            + String.format("%,d", Constant.THREE_MATCHES) + "원)"),
+            + String.format("%,d", Constant.THREE_MATCHES_PRIZE) + "원) - "),
     OUTPUT_FOUR_MATCHES("4개 일치 ("
-            + String.format("%,d", Constant.FOUR_MATCHES) + "원)"),
+            + String.format("%,d", Constant.FOUR_MATCHES_PRIZE) + "원) - "),
     OUTPUT_FIVE_MATCHES("5개 일치 ("
-            + String.format("%,d", Constant.FIVE_MATCHES) + "원)"),
-    OUTPUT_FIVE_WITH_BONUS_MATCHES("5개 일치, 보너스 볼 일치 ("
-            + String.format("%,d", Constant.FIVE_WITH_BONUS) + "원)"),
+            + String.format("%,d", Constant.FIVE_MATCHES_PRIZE) + "원) - "),
+    OUTPUT_BONUS_MATCHES("5개 일치, 보너스 볼 일치 ("
+            + String.format("%,d", Constant.BONUS_MATCHES_PRIZE) + "원) - "),
     OUTPUT_SIX_MATCHES("6개 일치 ("
-            + String.format("%,d", Constant.SIX_MATCHES) + "원)");
+            + String.format("%,d", Constant.SIX_MATCHES_PRIZE) + "원) - "),
+    OUTPUT_EARNINGS_RATE_PREFIX("총 수익률은 "),
+    OUTPUT_EARNINGS_RATE_POSTFIX("입니다");
+
 
     private final String text;
 
--- src/main/java/lotto/util/Constant.java
+++ src/main/java/lotto/util/Constant.java
@@ -1,9 +1,14 @@
 package lotto.util;
 
 public interface Constant {
-    int THREE_MATCHES = 5_000;
-    int FOUR_MATCHES = 50_000;
-    int FIVE_MATCHES = 1_500_000;
-    int FIVE_WITH_BONUS = 30_000_000;
-    int SIX_MATCHES = 2_000_000_000;
+    int THREE_MATCHES_PRIZE = 5_000;
+    int FOUR_MATCHES_PRIZE = 50_000;
+    int FIVE_MATCHES_PRIZE = 1_500_000;
+    int BONUS_MATCHES_PRIZE = 30_000_000;
+    int SIX_MATCHES_PRIZE = 2_000_000_000;
+
+    int THREE = 3;
+    int FOUR = 4;
+    int FIVE = 5;
+    int SIX = 6;
 }
