afa725c76e1554c2ef816e8272670bfc9e2431dc
chj <bluedog129@gmail.com>
2022-11-15 23:23:10 +0900

    feat(View) : 당첨번호, 보너스번호, 당첨통계, 수익률 출력

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class View {
@@ -18,5 +19,27 @@ public class View {
             System.out.println(lotto);
         }
     }
+    public List<String> inputWinNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return Arrays.asList(input.split(","));
+    }
 
+    public String inputBonus() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public void printStatistics(List<Integer> sameNumbers) {
+        System.out.print("당첨 통계\n---\n");
+        for(PrizeNumber prizeNumber : PrizeNumber.values()) {
+            System.out.print(
+                    prizeNumber.prizeString() + " - "
+                            + sameNumbers.get(prizeNumber.ordinal()) + "개\n"
+            );
+        }
+    }
+    public void printReturnRate(double returnRate) {
+        System.out.println("총 수익률은 " + returnRate + "%입니다.");
+    }
 }
