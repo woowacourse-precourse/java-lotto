b81b95a21aef8933de8c54c6b5237c417c5a9702
kimsinhyun <ksh415622@naver.com>
2022-11-15 20:06:40 +0900

    fix: (Error Enum) 몇 가지 오류 수정 및 추가
    1. 오류명 수정
    2. 오류 추가: 보너스 숫자가 1~45가 아닐 경우의 오류

--- src/main/java/lotto/Error.java
+++ src/main/java/lotto/Error.java
@@ -1,10 +1,11 @@
 package lotto;
 
 public enum Error {
-    PURCHASE_AMOUNT_ERROR       ("1,000 단위의 금액만 구입 가능합니다"),
-    NOT_NUMERIC                 ("숫자만 입력 가능합니다"),
-    ILLEGAL_LOTTO_LENGTH        ("로또 번호는 6자리여야 합니다"),
-    LOTTO_NUM_DUPLICATE_ERROR   ("로또 번호는 중복될 수 없습니다");
+    COST_ERROR                  ("1,000 단위의 금액만 구입 가능합니다"),
+    NOT_NUMERIC_ERROR           ("숫자만 입력 가능합니다"),
+    LOTTO_LENGTH_ERROR          ("로또 번호는 6자리여야 합니다"),
+    LOTTO_NUM_DUPLICATE_ERROR   ("로또 번호는 중복될 수 없습니다"),
+    RANGE_ERROR                 ("로또 번호는 1~45사이만 가능합니다.");
 
     private static final String ERROR_MESSAGE = "[ERROR]";
     private final String errorMessage;
