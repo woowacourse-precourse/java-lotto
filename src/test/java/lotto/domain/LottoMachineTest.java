b423683d017ea192d14744f7d81bd96c0b3e18b4
junyong <jymoon1121@gmail.com>
2022-11-15 18:19:07 +0900

    test: 랜덤 로또 생성 테스트케이스 추가

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -14,35 +15,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class LottoMachineTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"1100", "100", "3500", "12001", "50"})
+    @ValueSource(strings = {"1100", "100", "3500", "12001", "50", "abcd", "12db", " ", "9999", "0"})
+    @DisplayName("구입금액을 잘못 입력하면 오류 발생")
     void validateInputMoney(String input) throws Exception {
-        String data = input;
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
         LottoMachine lottoMachine = new LottoMachine();
-        Method validateMoney = lottoMachine.getClass().getDeclaredMethod("validateMoney", String.class);
-        validateMoney.setAccessible(true);
 
         Assertions.assertThatThrownBy(()->
-                        validateMoney.invoke(lottoMachine,data)).getRootCause()
+                        lottoMachine.getInputMoney())
                 .isInstanceOf(IllegalArgumentException.class);
     }
+    
+    @ParameterizedTest
+    @CsvSource({"1000,1", "2000,2", "3000,3", "5000,5"})
+    void createLottoNumber(String input, String size) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        LottoMachine lottoMachine = new LottoMachine();
 
-    @Test
-    void createLottoNumber() {
-    }
+        lottoMachine.getInputMoney();
+        Assertions.assertThat(lottoMachine.createLottoNumber().size()).isEqualTo(Integer.valueOf(size));
 
-    @Test
-    void printLottos() {
-    }
 
-    @Test
-    void getResult() {
-    }
-
-    @Test
-    void calculateRevenue() {
-    }
-
-    @Test
-    void printResult() {
     }
 }
