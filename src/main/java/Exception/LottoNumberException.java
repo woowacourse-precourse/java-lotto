38bfe52af5dad90fe7e85d5d02ba6d47120e9149
p <gustjr2332@naver.com>
2022-11-15 20:23:32 +0900

    refator(LottoNumberException) : 하드코딩 수정

--- src/main/java/Exception/LottoNumberException.java
+++ src/main/java/Exception/LottoNumberException.java
@@ -7,21 +7,24 @@ public class LottoNumberException {
     private static final String NOTNUMBER_ERRORMESSAGE = "[ERROR] 숫자만 입력해주세요.";
     private static final String OUTOFRANGE_ERRORMESSAGE = "[ERROR] 1~45사이의 숫자만 입력해주세요.";
     private static final String WINNINGNUMBERCONTAINBONUSNUMBER_ERROMESSAGE = "[ERROR] 당첨번호에 보너스 번호가 포함되어 있습니다.";
+    private static final String NUMERICAL_REGULAREXPRESSION = "^[0-9]*$";
+    private static final int MAXIMUM_LOTTONUMBER = 45;
+    private static final int MINIMUM_LOTTONUMBER = 1;
 
     public static void checkWinningNumberException(String number) {
-        if (!Pattern.matches("^[0-9]*$", String.valueOf(number))) {
+        if (!Pattern.matches(NUMERICAL_REGULAREXPRESSION, String.valueOf(number))) {
             throw new IllegalArgumentException(NOTNUMBER_ERRORMESSAGE);
         }
-        if (Integer.valueOf(number) < 1 || Integer.valueOf(number) > 45) {
+        if (Integer.valueOf(number) < MINIMUM_LOTTONUMBER || Integer.valueOf(number) > MAXIMUM_LOTTONUMBER) {
             throw new IllegalArgumentException(OUTOFRANGE_ERRORMESSAGE);
         }
     }
 
     public static void checkBonusNumberException(String number, List<Integer> WinningNumbers) {
-        if (!Pattern.matches("^[0-9]*$", String.valueOf(number))) {
+        if (!Pattern.matches(NUMERICAL_REGULAREXPRESSION, String.valueOf(number))) {
             throw new IllegalArgumentException(NOTNUMBER_ERRORMESSAGE);
         }
-        if (Integer.valueOf(number) < 1 || Integer.valueOf(number) > 45) {
+        if (Integer.valueOf(number) < MINIMUM_LOTTONUMBER || Integer.valueOf(number) > MAXIMUM_LOTTONUMBER) {
             throw new IllegalArgumentException(OUTOFRANGE_ERRORMESSAGE);
         }
         if (WinningNumbers.contains(Integer.valueOf(number))) {
