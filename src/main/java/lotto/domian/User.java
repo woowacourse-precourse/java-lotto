f7ab56d0cc69914159cbcf57d91c3c86efc370af
eom-tae-in <eti0728@daum.net>
2022-11-15 18:58:25 +0900

    feat(User): iv에 대한 setter, getter 생성

--- src/main/java/lotto/domian/User.java
+++ src/main/java/lotto/domian/User.java
@@ -12,12 +12,40 @@ public class User {
     private final long userLottoQuantity;
     private double userYield;
     GenerateLottoNumbers generateLottoNumbers = new GenerateLottoNumbers();
-
     public User(long price) {
         this.userLottoBuyingPrice = price;
         this.userLottoNumbers = new ArrayList<>();
         this.userWinningPrice = INITIAL_NUMBER;
         this.userLottoQuantity = price / LOTTO_PRICE;
         this.userYield = INITIAL_NUMBER;
+    }
+
+    public void setUserLottoNumbers(List<UserSingleLottoNumber> lottoNumberList) {
+        this.userLottoNumbers = lottoNumberList;
+    }
+    public void createLottoNumbers(long lottoQuantity) {
+        List<UserSingleLottoNumber> lottoNumbers = generateLottoNumbers.generateLottoNumber(lottoQuantity);
+        setUserLottoNumbers(lottoNumbers);
+    }
+    public List<UserSingleLottoNumber> getUserLottoNumbers() {
+        return userLottoNumbers;
+    }
+    public long getUserLottoBuyingPrice() {
+        return userLottoBuyingPrice;
+    }
+    public double getUserWinningPrice() {
+        return userWinningPrice;
+    }
+    public long getUserLottoQuantity() {
+        return userLottoQuantity;
+    }
+    public void addUserWinningPrice(int price) {
+        userWinningPrice += price;
+    }
+    public void setUserYield(double yield) {
+        this.userYield = yield;
+    }
+    public double getUserYield() {
+        return userYield;
     }
 }
