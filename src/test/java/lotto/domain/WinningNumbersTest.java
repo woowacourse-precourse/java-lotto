766bd93608c69727ea533e2ff62d0faa9605d7ab
ddukddaki <rlfrkdms1@naver.com>
2022-11-15 17:45:18 +0900

    test(WinningNumbersTest): 당첨 번호와 로또의 번호를 비교해 개수를 셀 수 있다

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -1,8 +1,10 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import java.util.List;
@@ -25,5 +27,17 @@ class WinningNumbersTest {
     void 보너스_번호_당첨번호에_있는_수로_생성(int bonusNumber) {
         assertThatThrownBy(() -> new WinningNumbers(winningNumber, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호와 로또의 번호를 비교해 개수를 셀 수 있다. ")
+    @Test
+    void 당첨_번호와_일치하는_수가_3개_일경우() {
+        //given
+        WinningNumbers winningNumbers = new WinningNumbers(winningNumber, 10);
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 34, 35, 36));
+        //when
+        int countMatchNumber = winningNumbers.countMatchWinningNumber(lotto);
+        //then
+        assertThat(countMatchNumber).isEqualTo(3);
     }
 }
