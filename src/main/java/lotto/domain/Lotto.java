071714a4ed4e4c4ec8cf4bd81d476087bcda7c16
Heesu Jeong <huissppea@gmail.com>
2022-11-15 21:44:28 +0900

    feat: 로또 오름차순 정렬

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -8,8 +9,9 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-
-        this.numbers = numbers;
+        List<Integer> lottoNumbers = new ArrayList<>(numbers);
+        Collections.sort(lottoNumbers);
+        this.numbers = lottoNumbers;
     }
 
     private void validate(List<Integer> numbers) {
