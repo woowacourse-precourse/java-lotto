55063aadedf6646e801d2a42c80081ba8e555a6c
Fizioo0102 <a27313797@gmail.com>
2022-11-15 22:36:28 +0900

    feat: 상수 선언
    - 메시지 출력을 위한 상수선언.

---
+++ src/main/java/lotto/RewardMessage.java
@@ -0,0 +1,14 @@
+package lotto;
+
+public class RewardMessage {
+
+    private static final String FIRST_MESSAGE ="6개 일치 (2,000,000,000원) - ";
+    private static final String SECOND_MESSAGE ="5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    private static final String THIRD_MESSAGE ="5개 일치 (1,500,000원) - ";
+    private static final String FOURTH_MESSAGE ="4개 일치 (50,000원) - ";
+    private static final String FIVTH_MESSAGE ="3개 일치 (5,000원) - ";
+    private static final String AMOUNT ="개";
+    private static final String ALL_YIELD_MESSAGE_FRONT = "총 수익률은 ";
+    private static final String ALL_YIELD_MESSAGE_BACK = "%입니다.";
+
+}
