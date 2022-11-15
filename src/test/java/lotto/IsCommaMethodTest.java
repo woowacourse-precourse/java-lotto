df965fc1dbff8aeb2a476f6ed487acb8a4f20e6f
neppiness <0414kjh@naver.com>
2022-11-15 13:33:43 +0900

    test: refactor IsCommaMethodTest (src/main/java/lotto)
    - Set display name to clarify cases
    - Rename functions

--- src/test/java/lotto/IsCommaMethodTest.java
+++ src/test/java/lotto/IsCommaMethodTest.java
@@ -1,19 +1,22 @@
 package lotto;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static lotto.Application.isComma;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class IsCommaMethodTest {
+    @DisplayName("쉼표의 경우 true를 반환한다.")
     @Test
-    void isComma_메소드_정상_입력_테스트() {
+    void isCommaMethodTest() {
         char character = ',';
         assertThat(isComma(character)).isEqualTo(true);
     }
 
+    @DisplayName("쉼표를 제외한 모든 경우 false를 반환한다.")
     @Test
-    void isComma_메소드_예외_테스트() {
+    void isCommaMethodExceptionTest() {
         for(char asciicode = 0; asciicode < 256; asciicode++) {
             if(asciicode == ',') continue;
             assertThat(isComma(asciicode)).isEqualTo(false);
