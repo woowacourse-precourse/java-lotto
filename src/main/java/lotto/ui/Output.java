2989cdfc603376c2d69baf4a826656f08b790bed
dingding-21 <phj8221@naver.com>
2022-11-15 11:53:20 +0900

    feat($Output): 로또 구입 금액 입력 메시지 출력하는 기능 구현

---
+++ src/main/java/lotto/ui/Output.java
@@ -0,0 +1,9 @@
+package lotto.ui;
+
+import lotto.utils.OutputMessage;
+
+public class Output {
+    public static void inputPurchaseAmountMessage() {
+        System.out.println(OutputMessage.INPUT_PURCHASE_AMOUNT);
+    }
+}
