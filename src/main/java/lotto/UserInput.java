07ed619254d8e256fc2ea589c90cdda8c1aa122b
Fizioo0102 <a27313797@gmail.com>
2022-11-15 21:57:50 +0900

    feat: 금액 입력받기 구현
    - money() 함수를 통하여 금액을 사용자에게 입력받는다
    - validate를 통하여 입력받은 금액을 검증한다

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class UserInput {
 
     private static final int LOTTO_MAX_NUMBER = 45;
@@ -7,5 +9,20 @@ public class UserInput {
     private static final String ERROR_NUMBER_MESSAGE = " 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final String MONEY_AMOUNT_MESSAGE = " 로또는 1000원 단위로만 구매할 수 있습니다.";
 
+    static String money(){
+        return Console.readLine();
+    }
+
+    static void validateMoney(String coin){
+        if(!coin.matches("[0-9]+")){
+            throw new IllegalArgumentException(ERROR_NUMBER_MESSAGE);
+        }
+        if(Integer.parseInt(coin)<1000){
+            throw new IllegalArgumentException(MONEY_AMOUNT_MESSAGE);
+        }
+        if(Integer.parseInt(coin) % 1000 != 0){
+            throw new IllegalArgumentException(MONEY_AMOUNT_MESSAGE);
+        }
+    }
 
 }
