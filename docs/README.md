a3f116af72a551f0892e0137996f10940bf90d56
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-11-15 11:09:20 +0900

    feat(NumberGenerator): 오름차순으로 정렬하는 기능 구현

--- docs/README.md
+++ docs/README.md
@@ -3,7 +3,7 @@
 ## 📄 기능 목록
 - [x] 로또 구입 금액을 입력 받는다. - InputOutput#getLottoBuyAmount()
 - [x] 1부터 45까지의 서로 다른 수 6개를 생성한다. - NumberGenerator#createRandomNumbers()
-- [ ] 로또 번호는 오름차순으로 정렬한다. - NumberGenerator#sortAscendingOrder()
+- [x] 로또 번호는 오름차순으로 정렬한다. - NumberGenerator#sortAscendingOrder()
 - [ ] 로또 당첨 번호 6개를 입력 받는다. - InputOutput#getLottoNumbers()
 - [ ] 로또 보너스 번호 1개를 입력 받는다. - InputOutput#getLottoBonusNumber()
 - [ ] 당첨번호와 로또번호를 비교할 수 있다. - compare()
--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -1,14 +1,20 @@
 package lotto.domain;
 
+import java.util.Collections;
+import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberGenerator {
     public List<Integer> createRandomNumbers(){
         List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1,45,6);
         // ErrorHandler#createLottoNumberError(randomNumbers);
+        randomNumbers=sortAscendingOrder(randomNumbers);
         return randomNumbers;
     }
 
-
+    public List<Integer> sortAscendingOrder(List<Integer> object){
+        Collections.sort(object);
+        return object;
+    }
 
 }
