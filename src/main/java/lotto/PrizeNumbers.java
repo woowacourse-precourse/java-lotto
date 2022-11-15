fc29be1f26cb4ac8e5f5a86f62ef9e55ebdbc397
chaos0103 <lyt1228@naver.com>
2022-11-15 15:32:26 +0900

    refactor(Lotto): 당첨 번호와 보너스 번호를 포함한 클래스 분리

---
+++ src/main/java/lotto/PrizeNumbers.java
@@ -0,0 +1,59 @@
+package lotto;
+
+import java.util.List;
+
+import static lotto.Constants.*;
+import static lotto.message.ExceptionMessage.*;
+
+public class PrizeNumbers {
+
+    private final List<Integer> numbers;
+    private final int bonusNumber;
+
+    public PrizeNumbers(List<Integer> numbers, int bonusNumber) {
+        validate(numbers);
+        duplicate(numbers);
+        numbersRangeValidate(numbers);
+        bonusNumberRangeValidate(bonusNumber);
+        this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBERS_COUNT) {
+            throw new IllegalArgumentException(SIZE_EXCEPTION);
+        }
+    }
+
+    private void duplicate(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(DUPLICATE_EXCEPTION);
+        }
+    }
+
+    private void numbersRangeValidate(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (isOutOfRange(number)) {
+                throw new IllegalArgumentException(OUT_OF_RANGE_EXCEPTION);
+            }
+        }
+    }
+
+    private void bonusNumberRangeValidate(int bonusNumber) {
+        if (isOutOfRange(bonusNumber)) {
+            throw new IllegalArgumentException(OUT_OF_RANGE_EXCEPTION);
+        }
+    }
+
+    private boolean isOutOfRange(int number) {
+        return !(NUMBER_MIN_RANGE < number && number < NUMBER_MAX_RANGE);
+    }
+}
--- src/main/java/lotto/message/ExceptionMessage.java
+++ src/main/java/lotto/message/ExceptionMessage.java
@@ -8,5 +8,6 @@ public final class ExceptionMessage {
     public static final String TYPE_EXCEPTION = "숫자만 입력이 가능합니다.";
     public static final String SIZE_EXCEPTION = "번호는 반드시 6개여야 합니다.";
     public static final String DUPLICATE_EXCEPTION = "중복된 번호가 존재합니다.";
+    public static final String OUT_OF_RANGE_EXCEPTION = "로또 번호의 숫자 범위는 1~45까지이다.";
 
 }
