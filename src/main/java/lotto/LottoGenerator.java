01601f0f802844742e1263b7072488fe01fc123f
xxeol2 <jhj01177@naver.com>
2022-11-15 16:46:32 +0900

    feat:  로또 번호는 오름차순으로 정렬하여 출력한다

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -5,14 +5,23 @@ import static lotto.constant.Rules.NUMBER_LENGTH;
 import static lotto.constant.Rules.START_INCLUSIVE;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoGenerator {
 
-    public static Lotto generate() {
+    public static List<Lotto> generateLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(generate());
+        }
+        return lottos;
+    }
+
+    private static Lotto generate() {
         List<LottoNumber> lottoNumbers = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE,
-                        END_INCLUSIVE, NUMBER_LENGTH).stream().map(LottoNumber::new)
+                        END_INCLUSIVE, NUMBER_LENGTH).stream().sorted().map(LottoNumber::new)
                 .collect(Collectors.toList());
         return new Lotto(lottoNumbers);
     }
