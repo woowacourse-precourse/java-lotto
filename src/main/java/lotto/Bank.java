195e17bd04d7436185b47172dd2fcb2d1938792c
korjun1993 <corojoon93@naver.com>
2022-11-16 01:58:46 +0900

    feat(Bank) : 당첨 결과를 계산하는 기능 추가

--- src/main/java/lotto/Bank.java
+++ src/main/java/lotto/Bank.java
@@ -2,36 +2,62 @@ package lotto;
 
 import lotto.exception.MyIllegalArgumentException;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import static lotto.exception.ErrorCode.BONUS_NUMBER_ERROR;
 
 public class Bank {
-    private static final Map<Integer, String> score;
-    static {
-        score = new HashMap<>();
-        score.put(0, null);
-        score.put(1, null);
-        score.put(2, null);
-        score.put(3, "3개 일치 (5,000원)");
-        score.put(4, "4개 일치 (50,000원)");
-        score.put(5, "5개 일치 (1,500,000원)");
-        score.put(6, "6개 일치 (2,000,000,000원)");
-        score.put(7, "5개 일치, 보너스 볼 일치 (30,000,000원)");
-    }
+    private final static int[] MONEY = {0, 2000000000, 30000000, 1500000, 50000, 5000};
+    private final static int WIN_SIZE = 6;
     private final Lotto winLotto;
     private final int bonusNumber;
+    private int payment[];
 
     public Bank(Lotto winLotto, int bonusNumber) {
         validate(winLotto, bonusNumber);
         this.winLotto = winLotto;
         this.bonusNumber = bonusNumber;
+        this.payment = new int[WIN_SIZE];
     }
 
     private void validate(Lotto winLotto, int bonusNumber) {
         if (winLotto.contains(bonusNumber)) {
             throw new MyIllegalArgumentException(BONUS_NUMBER_ERROR);
         }
+    }
+
+    private int payMoney(Lotto userLotto) {
+        int commonNumberCount = userLotto.getCommonNumbers(winLotto).size();
+        if (commonNumberCount == WIN_SIZE) { // 1등
+            payment[WIN_SIZE + 1 - commonNumberCount]++;
+            return MONEY[WIN_SIZE + 1 - commonNumberCount];
+        }
+        if (userLotto.contains(bonusNumber) && commonNumberCount == WIN_SIZE - 1) { // 2등
+            payment[WIN_SIZE + 1 - commonNumberCount]++;
+            return MONEY[WIN_SIZE + 1 - commonNumberCount];
+        }
+        if (commonNumberCount >= 3) { // 3~5등
+            payment[WIN_SIZE + 2 - commonNumberCount]++;
+            return MONEY[WIN_SIZE + 2 - commonNumberCount];
+        }
+        return 0;
+    }
+
+    public long getTotalMoney(List<Lotto> lottos) {
+        long totalMoney = 0;
+        for (Lotto user : lottos) {
+            totalMoney += payMoney(user);
+        }
+        return totalMoney;
+    }
+
+    public void printPayResult() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + payment[5] + "개");
+        System.out.println("4개 일치 (50,000원) - " + payment[4] + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + payment[3] + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + payment[2] + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + payment[1] + "개");
     }
 }
