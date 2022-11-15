f41fc7d9c567a91cc40dc2609b7713719db115ed
devMuscle <hongsbien@naver.com>
2022-11-15 12:58:09 +0900

    feat(addPrizeWinnerCount): PrizeWinner의 addCount method test 생성

---
+++ src/test/java/lotto/PrizeWinnerTest.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PrizeWinnerTest {
+
+    @DisplayName("PrizeWinner의 count를 증가시킨다")
+    @Test
+    void addPrizeWinnerCount() {
+        List<PrizeWinner> p = Arrays.asList(PrizeWinner.values());
+
+        p.get(p.indexOf(PrizeWinner.FIRSTPLACE)).addCount();
+        Integer expected = 1;
+
+        Assertions.assertEquals(p.get(p.indexOf(PrizeWinner.FIRSTPLACE)).getCount(), expected);
+
+    }
+}
