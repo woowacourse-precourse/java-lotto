f3423779ea8ba5979255335572e2c3509317f1b2
Sanmgmin <sangmins930@gmail.com>
2022-11-15 19:00:08 +0900

    feat(MyLotto, Constants): implement buyLotto
    변수 myLotto -> myPocket으로 변경
    변수 MONEY_UNIT(금액 단위) 생성
    살 수 있는 로또 개수만큼 로또 구매하는 method 구현

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
     public static final int NUM_OF_LOTTO = 6;
     public static final int MIN_VALUE_OF_LOTTO = 1;
     public static final int MAX_VALUE_OF_LOTTO = 45;
+    public static final int MONEY_UNIT = 1000;
 
     public static final String ERROR_MESSAGE_FOR_SIZE = "[ERROR] 로또 번호는 " + NUM_OF_LOTTO + "자리여야 합니다.";
     public static final String ERROR_MESSAGE_FOR_RANGE_OF_NUMBER = "[ERROR] 로또 번호는 " + MIN_VALUE_OF_LOTTO + "부터 " + MAX_VALUE_OF_LOTTO + " 사이의 숫자여야 합니다.";
--- src/main/java/lotto/MyLotto.java
+++ src/main/java/lotto/MyLotto.java
@@ -1,22 +1,35 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static lotto.Constants.ERROR_MESSAGE_FOR_SEED_MONEY;
+import static lotto.Constants.MONEY_UNIT;
 
 public class MyLotto {
     private int money;
     private int countNumsOfLotto;
-    private List<List<Integer>> myLottoNumbers;
+    private List<List<Integer>> myPocket;
 
     MyLotto(int money) {
         validateMoney(money);
         this.money = money;
-        this.countNumsOfLotto = money / 1000;
+        this.countNumsOfLotto = money / MONEY_UNIT;
+        this.myPocket = buyLotto(countNumsOfLotto);
     }
+
     private void validateMoney(int money) {
         if (money % 1000 != 0) {
             throw new IllegalArgumentException(ERROR_MESSAGE_FOR_SEED_MONEY);
         }
+    }
+
+    private List<List<Integer>> buyLotto(int count) {
+        List<List<Integer>> boughtLotto = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> Lotto = LottoMachine.generateLotto();
+            boughtLotto.add(Lotto);
+        }
+        return boughtLotto;
     }
 }
