3af2857402a76264227a3914fa40fbfcf9d43b32
canbeyour <wuzucy@gmail.com>
2022-11-15 17:03:33 +0900

    refactor: modify to reduce process

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,34 +1,28 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-import lotto.UI.*;
+import lotto.Utils.Print;
 import lotto.domain.*;
 
 import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
-        Print print = new Print();
 
-        print.askAmount();
-        int count = new Purchase(Console.readLine()).getCount();
-        print.purchaseCount(count);
+        Print.askAmount();
 
-        List<List<Integer>> publishedLottos = new Publish(count).getLottos();
-        print.purchasedLottos(publishedLottos);
+        LottoStore store = new LottoStore();
+        store.inputPurchase();
 
-        print.askWinningNumber();
-        String numbersInput = Console.readLine();
+        List<Lotto> lottos = store.buyLotto();
+        Print.purchasedLottos(lottos);
 
-        print.askBonusNumber();
-        String bonusNumberInput = Console.readLine();
+        LottoCompany company = new LottoCompany();
 
-        WinningLotto winningLotto = new WinningLotto(numbersInput, bonusNumberInput);
-        List<Integer> winningNumbers = winningLotto.getNumbers();
-        int bonusNumber = winningLotto.getBonusNumber();
+        for (Lotto lotto : lottos) {
+            company.processLotto(lotto);
+        }
 
-        LinkedHashMap<String, Integer> winningStats = new WinningStats(winningNumbers, bonusNumber, publishedLottos).getStats();
-        print.winningStats(winningStats);
-        print.returnRate(new CalculateReturn(count, winningStats).getReturnRate());
+        WinningStats stats = new WinningStats(store.getPurchase(), lottos);
+        stats.print();
     }
 }
