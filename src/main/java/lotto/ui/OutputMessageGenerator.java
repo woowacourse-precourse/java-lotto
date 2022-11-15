a6f635c08929cd9037362a99ba130e56f792519d
sonsoowon <yuwoo1018@gmail.com>
2022-11-15 14:35:22 +0900

    feat(OutputMessageGenerator): 각 로또 번호를 오름차순 정렬하여 문자열 리스트로 반환
    Lotto의 toString 메서드는 이후 Lotto 객체의 정보를 반환하도록 수정 예정

--- src/main/java/lotto/ui/OutputMessageGenerator.java
+++ src/main/java/lotto/ui/OutputMessageGenerator.java
@@ -1,5 +1,11 @@
 package lotto.ui;
 
+import lotto.domain.Lotto;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum OutputMessageGenerator {
     INSTANCE;
 
@@ -7,5 +13,17 @@ public enum OutputMessageGenerator {
 
     public static String getPurchaseLottoMessage(int lottoCount) {
         return String.format(PURCHASE_LOTTO_MESSAGE_FORMAT, lottoCount);
+    }
+
+    public static List<String> getAllLottoNumbersByAscendingOrder(List<Lotto> lotteries) {
+        return lotteries.stream()
+                .map(OutputMessageGenerator::getLottoNumbersByAscendingOrder)
+                .collect(Collectors.toList());
+    }
+    private static String getLottoNumbersByAscendingOrder(Lotto lotto) {
+        return lotto.getNumbers().stream()
+                .sorted()
+                .collect(Collectors.toList())
+                .toString();
     }
 }
--- src/test/java/lotto/ui/OutputMessageGeneratorTest.java
+++ src/test/java/lotto/ui/OutputMessageGeneratorTest.java
@@ -1,7 +1,10 @@
 package lotto.ui;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static lotto.ui.OutputMessageGenerator.*;
@@ -12,5 +15,13 @@ public class OutputMessageGeneratorTest {
     void getPurchaseLottoMessageTest() {
         assertThat(getPurchaseLottoMessage(4))
                 .isEqualTo("4개를 구매했습니다.");
+    }
+
+    @DisplayName("오름차순으로 정렬한 로또 번호를 문자열로 반환한다.")
+    @Test
+    void getAllLottoNumbersByAscendingOrderTest() {
+        List<Lotto> lotteries = List.of(new Lotto(List.of(1, 4, 5, 6, 2, 3)));
+        assertThat(getAllLottoNumbersByAscendingOrder(lotteries))
+                .isEqualTo(List.of("[1, 2, 3, 4, 5, 6]"));
     }
 }
