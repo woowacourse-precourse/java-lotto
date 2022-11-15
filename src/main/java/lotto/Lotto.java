85a7838adf93aa515c0d941195b8472534ed184b
HyowonSin <tlsgydnjs@naver.com>
2022-11-15 23:42:38 +0900

    feat($Main, $Lotto): setting the execution environment
    Setting the Execution Environment:
    $Main
    - run method
    
    $Lotto
    - modify private method to public static
    - input money String to Integer

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -27,32 +27,35 @@ public class Lotto {
     }
 
     // 입력 받기 (구입 금액, 당첨 번호, 보너스 번호)
-    private int inputMoney() {
+    public static int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        int money = Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
+        String money = camp.nextstep.edu.missionutils.Console.readLine();
         validateMoney(money);
-        return money;
+        return Integer.parseInt(money);
     }
 
-    private void validateMoney(int money) {
-        if (money < 1000) {
+    private static void validateMoney(String money) {
+        if (money == null || money.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액을 입력해 주세요.");
+        } else if (!money.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+        } else if (Integer.parseInt(money) < 1000) {
             throw new IllegalArgumentException("[ERROR] 로또 1장의 가격은 1,000원입니다.");
-        } else if (money % 1000 != 0) {
+        } else if (Integer.parseInt(money) % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또는 1,000원 단위로 구입 가능합니다.");
         }
     }
 
-    private List<Integer> inputWinningNumbers() {
+    public static Lotto inputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         List<Integer> winingNumbers = Arrays.stream(camp.nextstep.edu.missionutils.Console.readLine().split(","))
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-        validate(winingNumbers);
-        return winingNumbers;
+        return new Lotto(winingNumbers);
     }
 
-    private int inputBonusNumber() {
+    public static int inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         return Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
     }
