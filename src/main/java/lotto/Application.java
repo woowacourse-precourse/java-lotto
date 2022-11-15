5e7f8a67d36c79c4e2e23b5f364822e783a7d0a7
skydreamer21 <skydreamer210@gmail.com>
2022-11-15 18:36:19 +0900

    feat(Application): main 함수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import lotto.controller.LotteryController;
+import lotto.controller.UserController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            UserController.buyLotteryTicket();
+            LotteryController.draw();
+            UserController.checkLotteryResult();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
