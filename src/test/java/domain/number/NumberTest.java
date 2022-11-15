8520e86b25b00f7dcb4b42567364455eb3deab94
kmw2378 <rlarla677@gmail.com>
2022-11-15 20:46:40 +0900

    [test] NumberTest - 요구 사항에 맞게 숫자 간격 수정

---
+++ src/test/java/domain/number/NumberTest.java
@@ -0,0 +1,27 @@
+package domain.number;
+
+import domain.user.Number;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class NumberTest {
+
+    Number number = new Number("1,2,3,4,5,6");
+
+    @Test
+    void 입력_받은_문자열을_리스트로_변환_테스트() {
+        String input = "1,2,3,4,5";
+        int[] intArray = { 1, 2, 3, 4, 5};
+        List<Integer> result = number.stringToList(input);
+
+        assertThat(result.size()).isEqualTo(intArray.length);
+
+        for (int i = 0; i < intArray.length; i++) {
+            assertThat(result.get(i)).isEqualTo(intArray[i]);
+        }
+    }
+}
