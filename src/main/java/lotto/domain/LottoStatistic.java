fffc8fe07e353d5fea99e35bb0e5f1e847eeb501
tdmdgh <tdmdgh@gmail.com>
2022-11-15 15:52:12 +0800

    test(lotto): LottoStaticTest 추가

--- src/main/java/lotto/domain/LottoStatistic.java
+++ src/main/java/lotto/domain/LottoStatistic.java
@@ -36,7 +36,7 @@ public class LottoStatistic {
         return print;
     }
 
-    private String summarizeRank(Rank rank) {
+    public String summarizeRank(Rank rank) {
         DecimalFormat df = new DecimalFormat("###,###");
         int count = rank.getCounts();
         String price = df.format(rank.getPrice());
@@ -78,7 +78,7 @@ public class LottoStatistic {
         }
     }
 
-    private int compareWin_lotto(Lotto lotto) {
+    public int compareWin_lotto(Lotto lotto) {
         int count = 0;
         for (int user_num : lotto.getNumbers()) {
             if (win_lotto.getNumbers().contains(user_num))
