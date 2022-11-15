db436b4c291c464624c4b1a67a04f40cc575c8f5
hhiyeon <hhyyeon0214@gmail.com>
2022-11-15 15:53:40 +0900

    test: winning(test)-include duplicate number to winning numbers

--- src/test/java/lotto/domain/WinningTest.java
+++ src/test/java/lotto/domain/WinningTest.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -10,7 +12,14 @@ public class WinningTest {
     @DisplayName("당첨 번호가 숫자가 아니면 예외가 발생한다.")
     @Test
     void checkWinningNumberIsNotNumeric() {
-        assertThatThrownBy(() -> new Money("100d0"))
+        assertThatThrownBy(() -> new Winning("100d0"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void checkWinningNumberDuplicated() {
+        assertThatThrownBy(() -> new Winning("1,2,3,4,5,5"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
