018b4cd93ddcc706d51b545aa061ebd2e77aedfb
LeeHaiLim <hailim1997@gmail.com>
2022-11-15 09:48:32 +0900

    feat(lotto): implement class constants
    Added constants:
    - number length
    - number range start
    - number range end
    - lotto price

---
+++ src/main/java/lotto/Constants.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class Constants {
+    public static final int NUMBER_LENGTH = 6;
+    public static final int NUMBER_RANGE_START = 1;
+    public static final int NUMBER_RANGE_END = 45;
+    public static final int LOTTO_PRICE = 1000;
+}
