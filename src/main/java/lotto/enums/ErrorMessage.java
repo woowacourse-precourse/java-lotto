5f5fd9feddf208bbe45d8164bb8dc57fb02bf1af
kkimhaji <kkim.haji@gmail.com>
2022-11-15 16:56:01 +0900

    feat: add error message
    금액 단위에 대한 에러 메시지 추가

--- src/main/java/lotto/enums/ErrorMessage.java
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -3,9 +3,10 @@ package lotto.enums;
 public enum ErrorMessage {
     NUM_ERROR("[ERROR] 숫자만 입력해주세요."),
     LEN_ERROR("[ERROR] 6자리 숫자를 입력해주세요."),
-    RANGE_ERROR("[ERROR] 1부터 45 사이의 숫자를 입력해주세요."),
-    DUPLICATE_ERROR("[ERROR] 중복 없이 입력해주세요.")
-    ;
+    RANGE_ERROR("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATE_ERROR("[ERROR] 중복 없이 입력해주세요."),
+    PRICE_ERROR("[ERROR] 금액은 1000원 단위로 입력해주세요.");
+
 
     private final String errorMessage;
 
