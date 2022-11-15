16327447c7494c0bc9e681097c3bbfe94d8fa16b
LEE JIEUN <jieun3595@soongsil.ac.kr>
2022-11-15 15:38:39 +0900

    feat : 구입한 개수만큼 리스트를 출력해주는 함수 구현
    중첩리스트를 이용하여 중복이 되지 않게 1부터 45까지 6개를 뽑은 후에 정렬하고, Array에 넣고 이걸 sout해서 총 리스트를 출력하고, 나의 로또 Array를 반환한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,18 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     private static final String ERROR_MESSAGE = "[ERROR]";
     private static final String ERROR_CHECKNUM = " 숫자가 아닌 수가 존재합니다.";
-    private static final String ERROR_CHECKPRICE = "1000원 단위로 입력해 주세요.";
+    private static final String ERROR_CHECKPRICE = " 1000원 단위로 입력해 주세요.";
+    private static final String ERROR_CHECKLOTTONUM =  " 1부터 45사이의 수가 아니다.";
     private static final String START_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String LOTTOSIZE_MESSAGE = "개를 구매했습니다.";
 
     public static void main(String[] args) {
         Application main = new Application();
@@ -14,7 +20,10 @@ public class Application {
         System.out.println(START_MESSAGE);
         int price = main.checkNum(main.userInputNumber());
         int lottoNum = main.checkPrice(price);
-        System.out.println(lottoNum);
+
+        System.out.println(lottoNum + LOTTOSIZE_MESSAGE);
+        List<List<Integer>> myLottoArr = main.lottoArr(lottoNum);
+
     }
     public String userInputNumber() {
         return Console.readLine();
@@ -34,4 +43,18 @@ public class Application {
         }
         return price / 1000;
     }
+
+    public List<List<Integer>> lottoArr(int lottoNum) {
+        List<Integer> myLotto;
+        List<List<Integer>> myLottoArr = new ArrayList<>();
+        for (int i = 0; i < lottoNum; i++) {
+            myLotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            myLotto.sort(Comparator.naturalOrder());
+            myLottoArr.add(myLotto);
+            System.out.println(myLottoArr.get(i));
+        }
+        return myLottoArr;
+    }
+
+
 }
