38d8321cccbe8d18995aea03bd677c5c94668180
young2866 <62212610+young2866@users.noreply.github.com>
2022-11-15 18:16:58 +0900

    test: 당첨 번호 중복 검증 테스트

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -80,6 +80,14 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 당첨번호_중복테스트() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("1000", "1,2,3,4,5,5"));
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
