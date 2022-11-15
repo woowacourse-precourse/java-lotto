bdff964e5eecadf685644d7aca172b14e8f3ae84
Mr.Kim <joajy98@gmail.com>
2022-11-15 20:24:48 +0900

    feat: add function to make winning numbers

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,38 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+import lotto.constant.Constant;
+
 public class Application {
+    private static Constant constant = new Constant();
+    private static String Answer_Numbers = "당첨 번호를 입력해 주세요.";
+    private static String Regex = ",";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+    
+    public static Lotto MakeAnswer() {
+        List<String> winning = new ArrayList<>();
+        List<Integer> WinningNumberSet = new ArrayList<>();
+
+        System.out.println(Answer_Numbers);
+        String Winning_Numbers;
+
+        if((Winning_Numbers = Console.readLine()) != null){
+            String[] split_number = Winning_Numbers.split(Regex);
+            for(String s: split_number)
+                winning.add(s);
+        }
+
+        if(winning.stream().distinct().count() != winning.size())
+            throw new IllegalArgumentException(constant.Print_Duplicate_Numbers());
+        System.out.println(winning);
+
+        for(String number : winning)
+            WinningNumberSet.add(Integer.parseInt(number));
+        return new Lotto(WinningNumberSet);
     }
 }
