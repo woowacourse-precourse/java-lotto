c259068e54607414e87ced8bc6b585ee11a78249
sooyoungh <sooyoung.h8@gmail.com>
2022-11-15 17:46:03 +0900

    ✨ feat(Prize): 점수별 수익 및 출력 메세지 매핑한 Enum 클래스 구현

---
+++ src/main/java/lotto/model/Prize.java
@@ -0,0 +1,21 @@
+package lotto.model;
+
+import java.util.function.Function;
+
+public enum Prize {
+    THREE_STRIKE(3.0, "3개 일치 (5,000원)", resultCount -> resultCount * 5000),
+    FOUR_STRIKE(4.0, "4개 일치 (50,000원)", resultCount -> resultCount * 50000),
+    FIVE_STRIKE(5.0, "5개 일치 (1,500,000원)", resultCount -> resultCount * 1500000),
+    FIVE_STRIKE_WITH_BONUS(5.5, "5개 일치, 보너스 볼 일치 (30,000,000원)", resultCount -> resultCount * 30000000),
+    SIX_STRIKE(6.0, "6개 일치 (2,000,000,000원)", resultCount -> resultCount * 2000000000);
+
+    private Double prize;
+    private String message;
+    private Function<Integer, Integer> expression;
+
+    Prize(Double prize, String message, Function<Integer, Integer> expression) {
+        this.prize = prize;
+        this.message = message;
+        this.expression = expression;
+    }
+}
