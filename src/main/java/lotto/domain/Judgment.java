97f4b42afab92639e4eca274d83b8a6684968d22
kbm6609 <kbm6609@nvaer.com>
2022-11-15 21:16:56 +0900

    feat<lotto> : 당첨번호, 로또 번호 비교 기능 구현

--- src/main/java/lotto/domain/Judgment.java
+++ src/main/java/lotto/domain/Judgment.java
@@ -1,5 +1,15 @@
 package lotto.domain;
 
-public class Judgment {
+import java.util.List;
 
+public class Judgment {
+    public int compareToNumber(List<Integer> myNumber, List<Integer> winningNumber) {
+        int winCount = 0;
+        for (int i : myNumber) {
+            if (winningNumber.contains(i)) {
+                winCount++;
+            }
+        }
+        return winCount;
+    }
 }
