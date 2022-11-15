7abf0bbea18c918b2aa683f2fec53a919634280c
“rotomoo” <quddlf410@naver.com>
2022-11-15 19:17:43 +0900

    feat(Check) : 로또 입력 예외 공통 기능 구현

--- src/main/java/lotto/exception/Check.java
+++ src/main/java/lotto/exception/Check.java
@@ -1,22 +1,29 @@
 package lotto.exception;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Check {
 
     public boolean checkDuplicate(List<Integer> numbers) {
-        return true;
+        HashSet<Integer> duplicateSet = new HashSet<>(numbers);
+        return duplicateSet.size() == 6;
     }
 
     public boolean checkDuplicate(List<Integer> numbers, int number) {
-        return true;
+        HashSet<Integer> duplicateSet = new HashSet<>(numbers);
+        duplicateSet.add(number);
+        return duplicateSet.size() == 7;
     }
 
     public boolean checkRange(List<Integer> numbers) {
-        return true;
+        int max = Collections.max(numbers);
+        int min = Collections.min(numbers);
+        return (max <= 45 && min >= 1);
     }
 
-    public boolean checkRange(List<Integer> numbers, int number) {
-        return true;
+    public boolean checkRange(int number) {
+        return (number <= 45 && number >= 1);
     }
 }
