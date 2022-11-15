4185e4d4dd027512a535cc873d53c377a72c80f9
dingding-21 <phj8221@naver.com>
2022-11-15 10:45:05 +0900

    feat($Input): 로또 구입 금액 입력 구현

--- src/main/java/lotto/ui/Input.java
+++ src/main/java/lotto/ui/Input.java
@@ -1,14 +1,23 @@
 package lotto.ui;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.utils.Constant;
 
 public class Input {
     private static final Integer REMAINDER = 0;
+
+    public static Integer getPurchaseAmount() {
+        return validatePurchaseAmount(toInts(Console.readLine()));
+    }
 
     public static Integer validatePurchaseAmount(int amount) {
         if (!(amount % Constant.PURCHASE_AMOUNT_UNIT == REMAINDER)) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원으로 나누어 떨어져야 합니다.");
         }
         return amount;
+    }
+
+    private static Integer toInts(String number) {
+        return Integer.parseInt(number);
     }
 }
