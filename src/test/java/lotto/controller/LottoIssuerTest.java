c723d29aba51ddd99d295cbe04442b485295f946
sonsoowon <yuwoo1018@gmail.com>
2022-11-15 18:15:50 +0900

    test(LottoIssuerTest): 코드 정렬

--- src/test/java/lotto/controller/LottoIssuerTest.java
+++ src/test/java/lotto/controller/LottoIssuerTest.java
@@ -9,14 +9,14 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
-
 public class LottoIssuerTest {
     @DisplayName("1부터 45사이의 중복되지 않는 숫자 6개를 골라 로또를 생성한 경우 예외가 발생하지 않는다")
     @Test
-    void issueCorrectLotto(){
+    void issueCorrectLotto() {
         assertThatNoException()
                 .isThrownBy(() -> LottoIssuer.issue());
     }
+
     @DisplayName("로또 구매 수량만큼의 로또를 발행한다")
     @Test
     void issueLottoByCorrectQuantity() {
