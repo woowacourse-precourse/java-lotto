042d875ff30d0cf2bbc10fb8d758b1717bab6d26
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-15 22:32:10 +0900

    feat: User에 대한 필요한 변수 및 상수 추가

---
+++ src/main/java/lotto/domain/User.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import static lotto.util.LottoNumbersGenerator.lottoGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.util.LottoNumbersGenerator;
+
+
+public class User {
+    private List<UserLotto> userLottos = new ArrayList<>();
+
+    private int price;
+    private int quantity;
+    private int earnTotalMoney;
+
+
+
+
+
+
+}
