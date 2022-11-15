86601e79e192f3c81fbc225ed2e541982f6eb150
이규승 <rpg8988@naver.com>
2022-11-15 22:17:14 +0900

    feat: Bonus 숫자 생성 및 유효성 확인 로직

---
+++ src/main/java/lotto/Model/Bonus.java
@@ -0,0 +1,48 @@
+package lotto.Model;
+
+import static lotto.Validator.checkRange;
+import static lotto.Validator.checkSizeNumber;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Bonus {
+
+    private final Integer bonus;
+
+    public Bonus(final Integer bonus) {
+        this.bonus = bonus;
+    }
+
+    public Bonus(final Lotto firstPlace, final String bonus) {
+        validate(bonus);
+        firstPlace.duplicateCheckBonusNumber(bonus);
+        this.bonus = Integer.parseInt(bonus);
+    }
+
+    private static void validate(final String bonus) {
+        checkSizeNumber(bonus);
+        checkRange(bonus);
+    }
+
+    public Boolean calMatch(final List<Integer> numbers) {
+        return numbers.contains(this.bonus);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Bonus bonus1 = (Bonus) o;
+        return Objects.equals(bonus, bonus1.bonus);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bonus);
+    }
+}
