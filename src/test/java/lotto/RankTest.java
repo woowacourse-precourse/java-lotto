3999ba86d835abb1e40b2a09035d8ea5ea7738dd
xxeol2 <jhj01177@naver.com>
2022-11-15 20:28:42 +0900

    test: 사용자의 로또와 당첨 로또를 비교하여 당첨 내역을 계산한다

--- src/test/java/lotto/RankTest.java
+++ src/test/java/lotto/RankTest.java
@@ -7,13 +7,16 @@ import static lotto.rank.Rank.NONE;
 import static lotto.rank.Rank.SECOND;
 import static lotto.rank.Rank.THIRD;
 import static lotto.rank.Rank.getRank;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lotto.lotto.Lotto;
 import lotto.lotto.LottoNumber;
+import lotto.lotto.WinningLotto;
 import lotto.rank.Rank;
-import org.assertj.core.api.Assertions;
+import lotto.rank.RankChecker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -28,9 +31,9 @@ public class RankTest {
         Rank rank1 = getRank(0, true);
         Rank rank2 = getRank(1, true);
         Rank rank3 = getRank(2, false);
-        Assertions.assertThat(rank1).isEqualTo(NONE);
-        Assertions.assertThat(rank2).isEqualTo(NONE);
-        Assertions.assertThat(rank3).isEqualTo(NONE);
+        assertThat(rank1).isEqualTo(NONE);
+        assertThat(rank2).isEqualTo(NONE);
+        assertThat(rank3).isEqualTo(NONE);
     }
 
     @DisplayName("3개가 일치하면 5등이다.")
@@ -38,8 +41,8 @@ public class RankTest {
     void threeMatchThenFifthRank() {
         Rank rank1 = getRank(3, true);
         Rank rank2 = getRank(3, false);
-        Assertions.assertThat(rank1).isEqualTo(FIFTH);
-        Assertions.assertThat(rank2).isEqualTo(FIFTH);
+        assertThat(rank1).isEqualTo(FIFTH);
+        assertThat(rank2).isEqualTo(FIFTH);
     }
 
     @DisplayName("4개가 일치하면 4등이다.")
@@ -47,31 +50,49 @@ public class RankTest {
     void FourMatchThenFourthRank() {
         Rank rank1 = getRank(4, true);
         Rank rank2 = getRank(4, false);
-        Assertions.assertThat(rank1).isEqualTo(FOURTH);
-        Assertions.assertThat(rank2).isEqualTo(FOURTH);
+        assertThat(rank1).isEqualTo(FOURTH);
+        assertThat(rank2).isEqualTo(FOURTH);
     }
 
     @DisplayName("5개가 일치하고 보너스볼이 일치하지 않으면 3등이다.")
     @Test
     void fiveMatchAndBonusNotMatchThenThirdRank() {
         Rank rank = getRank(5, false);
-        Assertions.assertThat(rank).isEqualTo(THIRD);
+        assertThat(rank).isEqualTo(THIRD);
     }
 
     @DisplayName("5개가 일치하고 보너스볼이 일치하면 2등이다.")
     @Test
     void fiveMatchAndBonusMatchThenSecondRank() {
         Rank rank = getRank(5, true);
-        Assertions.assertThat(rank).isEqualTo(SECOND);
+        assertThat(rank).isEqualTo(SECOND);
     }
 
     @DisplayName("6개가 일치하면 1등이다.")
     @Test
     void sixMatchThenFirstRank() {
         Rank rank = getRank(6, false);
-        Assertions.assertThat(rank).isEqualTo(FIRST);
+        assertThat(rank).isEqualTo(FIRST);
     }
 
+    @DisplayName("사용자의 로또와 당첨 로또를 비교하여 당첨 내역을 계산한다")
+    @Test
+    void getRanksByComparingLottoAndWinningLotto() {
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(generateLottoByIntegerList(List.of(1, 2, 3, 4, 5, 6)));
+        lottos.add(generateLottoByIntegerList(List.of(1, 2, 3, 4, 5, 7)));
+        lottos.add(generateLottoByIntegerList(List.of(1, 2, 3, 4, 5, 8)));
+        lottos.add(generateLottoByIntegerList(List.of(1, 2, 3, 4, 7, 8)));
+        lottos.add(generateLottoByIntegerList(List.of(1, 2, 3, 7, 8, 9)));
+        lottos.add(generateLottoByIntegerList(List.of(1, 2, 7, 8, 9, 10)));
+        lottos.add(generateLottoByIntegerList(List.of(7, 8, 9, 10, 11, 12)));
+        List<LottoNumber> winningNumber = convertIntegerListToLottoNumberList(
+                List.of(1, 2, 3, 4, 5, 6));
+        LottoNumber bonusNumber = new LottoNumber(7);
+        WinningLotto winningLotto = new WinningLotto(winningNumber, bonusNumber);
+        assertThat(RankChecker.checkRank(lottos, winningLotto)).isEqualTo(
+                List.of(FIRST, SECOND, THIRD, FOURTH, FIFTH, NONE, NONE));
+    }
 
     private Lotto generateLottoByIntegerList(List<Integer> numbers) {
         return new Lotto(convertIntegerListToLottoNumberList(numbers));
