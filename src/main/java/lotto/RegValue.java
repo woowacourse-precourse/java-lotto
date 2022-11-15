8c8f85309501159cb4cc0eb9ea199d2c76622e85
MIN-04 <kimji_0424@naver.com>
2022-11-15 15:40:46 +0900

    refactor(RegValue.java): modify 변수에서 상수로
    Modified 변수에서 상수로 선언:
    - add final

--- src/main/java/lotto/RegValue.java
+++ src/main/java/lotto/RegValue.java
@@ -4,7 +4,7 @@ public enum RegValue {
   NUMBER_REG("^[0-9]+$"),
   LOTTO_NUMBER_REG("^[1-9]{1}$|^[1-3]{1}[0-9]{1}$|^4{1}[0-5]{1}$");
 
-  private String reg;
+  private final String reg;
 
   RegValue(String reg) {
     this.reg = reg;
