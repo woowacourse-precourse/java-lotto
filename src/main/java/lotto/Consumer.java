a29a9a573614d75666053eff2ea570fd9b115f8e
고동우 <shp05179@gmail.com>
2022-11-15 18:46:48 +0900

    style: 띄어쓰기 수정

--- src/main/java/lotto/Consumer.java
+++ src/main/java/lotto/Consumer.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Consumer {
 
-    private static final List<Integer> STATISTICS_INIT = List.of(0,0,0,0,0,0);
+    private static final List<Integer> STATISTICS_INIT = List.of(0, 0, 0, 0, 0, 0);
     private static final int LOTTO_PRICE = 1_000;
     private static final int PROFIT_IND = 5;
 
@@ -27,32 +27,33 @@ public class Consumer {
         }
         LottoConsoleIo.printNoOfLotto(lottos);
     }
+
     public static List<Integer> calcStat(Lotto winNumber, int bonus) {
         List<Integer> stat = new ArrayList<>(STATISTICS_INIT);
         HashSet winNumbers = new HashSet(winNumber.getNumbers());
-        for (Lotto lotto:lottos) {
+        for (Lotto lotto : lottos) {
             HashSet<Integer> myNumbers = new HashSet(lotto.getNumbers());
             myNumbers.retainAll(winNumbers);
-            if (Prize.sizeToInd(myNumbers.size())<Prize.THREE.getIndex()){
+            if (Prize.sizeToInd(myNumbers.size()) < Prize.THREE.getIndex()) {
                 continue;
             }
-            if (Prize.sizeToInd(myNumbers.size())==Prize.FIVE.getIndex()
+            if (Prize.sizeToInd(myNumbers.size()) == Prize.FIVE.getIndex()
                     && winNumbers.contains(bonus)) {
-                stat.set(Prize.FIVE.getIndex(), stat.get(Prize.FIVE.getIndex())+1);
+                stat.set(Prize.FIVE.getIndex(), stat.get(Prize.FIVE.getIndex()) + 1);
                 continue;
             }
-            stat.set(Prize.sizeToInd(myNumbers.size()), stat.get(Prize.sizeToInd(myNumbers.size())+1));
+            stat.set(Prize.sizeToInd(myNumbers.size()), stat.get(Prize.sizeToInd(myNumbers.size()) + 1));
         }
         return stat;
     }
 
-    public static List<Integer> calcProfit(List<Integer> stat){
-        float moneySpent = lottos.size()*LOTTO_PRICE;
+    public static List<Integer> calcProfit(List<Integer> stat) {
+        float moneySpent = lottos.size() * LOTTO_PRICE;
         float moneyEarned = 0;
-        for (Prize p:Prize.getList()) {
-            moneyEarned = moneyEarned + p.getReward()*stat.get(p.getIndex());
+        for (Prize p : Prize.getList()) {
+            moneyEarned = moneyEarned + p.getReward() * stat.get(p.getIndex());
         }
-        stat.set(PROFIT_IND, Math.round(moneyEarned/moneySpent*100)/100);
+        stat.set(PROFIT_IND, Math.round(moneyEarned / moneySpent * 100) / 100);
         return stat;
     }
 
