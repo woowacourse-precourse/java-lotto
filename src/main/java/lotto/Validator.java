dd06e4feaa55a80f0d9c754a1ba7722c103ab4f0
Hyoungwoo <dlguddn3024@gmail.com>
2022-11-15 18:33:32 +0900

    Validator 클래스에 입력받은 금액의 유효성 평가 기능을 추가

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -15,6 +15,11 @@ public class Validator {
         validateDuplicate(numbers, bonusNumber);
     }
 
+    public void validatePayedMoney(int money) throws IllegalArgumentException {
+        validateMoneySize(money);
+        validateMoneyUnit(money);
+    }
+
     private void validateSize(final List<Integer> numbers) throws IllegalArgumentException {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개를 입력해야 합니다.");
@@ -35,15 +40,27 @@ public class Validator {
         }
     }
 
-    private void validateRange(final int bonusNumber) {
+    private void validateRange(final int bonusNumber) throws IllegalArgumentException {
         if (bonusNumber > 45 || bonusNumber < 1) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45사이의 숫자여야 합니다.");
         }
     }
 
-    private void validateDuplicate(final List<Integer> numbers, final int bonusNumber) {
+    private void validateDuplicate(final List<Integer> numbers, final int bonusNumber) throws IllegalArgumentException {
         if (numbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨번호와 다른 숫자여야 합니다.");
+        }
+    }
+
+    private void validateMoneySize(final int money) throws IllegalArgumentException {
+        if (money < 1000) {
+            throw new IllegalArgumentException("[ERROR] 금액은 최소 1000원이여야 합니다.");
+        }
+    }
+
+    private void validateMoneyUnit(final int money) throws IllegalArgumentException {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 천원 단위로 나누어떨어져야 합니다.");
         }
     }
 }
