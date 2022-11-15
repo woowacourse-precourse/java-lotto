31d08ef205732fcc3207ee007753470142e1f294
Fizioo0102 <a27313797@gmail.com>
2022-11-15 21:55:34 +0900

    feat: 상수 선언
    - private static final를 활용하여 상수를 선언하였다

---
+++ src/main/java/lotto/UserInput.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class UserInput {
+
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final String ERROR_NUMBER_MESSAGE = " 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String MONEY_AMOUNT_MESSAGE = " 로또는 1000원 단위로만 구매할 수 있습니다.";
+
+
+}
