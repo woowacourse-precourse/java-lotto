bd54f85adcfc202b8ac1edfdaf08ae626abbf411
임원정 <oddish0513@gmail.com>
2022-11-15 21:23:22 +0900

    fix(lotto): 개행 수정

--- src/main/java/lotto/domain/view/Message.java
+++ src/main/java/lotto/domain/view/Message.java
@@ -2,7 +2,7 @@ package lotto.domain.view;
 
 public enum Message {
 	START("구입금액을 입력해 주세요.\n"),
-	BUY("개를 구매했습니다.\n"),
+	BUY("개를 구매했습니다."),
 	WINNING_NUMBER("\n당첨 번호를 입력해 주세요.\n"),
 	BONUS_NUMBER("\n보너스 번호를 입력해 주세요.\n"),
 	RESULT_TITLE("\n당첨 통계\n---\n"),
