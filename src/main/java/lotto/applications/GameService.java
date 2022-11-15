d5f8f73c41e12bc635a80eb60c7ca8505bac7baf
mikekang47 <gyeong0308@gmail.com>
2022-11-15 18:12:48 +0900

    Feat<GameService>: 로또 번호를 반환하는 로직 추가

--- src/main/java/lotto/applications/GameService.java
+++ src/main/java/lotto/applications/GameService.java
@@ -1,6 +1,11 @@
 package lotto.applications;
 
+import lotto.domains.Lotto;
 import lotto.utils.Calculator;
+import lotto.utils.LottoNumberMaker;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameService {
     private static GameService INSTANCE;
@@ -17,5 +22,15 @@ public class GameService {
 
     public int getNumberOfLotto(final int money) {
         return Calculator.calculateNumberOfLotto(money);
+    }
+
+    public List<Lotto> createLotteries(final int numberOfLotto) {
+        List<Lotto> lottoNumbers = new ArrayList<>();
+
+        for (int i = 0; i < numberOfLotto; i++) {
+            Lotto lotto = LottoNumberMaker.createLottoNumbers();
+            lottoNumbers.add(lotto);
+        }
+        return lottoNumbers;
     }
 }
