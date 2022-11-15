9ae6bd66b847fd2a960c6bb128a844baf0eeb596
sujin0607 <goweg0607@gmail.com>
2022-11-15 22:36:43 +0900

    refactor: 메서드명 수정

--- src/main/java/lotto/domain/LottoManager.java
+++ src/main/java/lotto/domain/LottoManager.java
@@ -5,51 +5,58 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 public class LottoManager {
+    public LottoManager() {
+    }
 
-    public int countLotto(String inputMoney){
-        int money = stringToInteger(inputMoney);
+    public int countLotto(String inputMoney) {
+        int money = isNumberic(inputMoney);
         hasRestError(money);
         MinimumAmountError(money);
         return money / 1000;
     }
-    public int stringToInteger(String money) throws IllegalArgumentException{
-        return Integer.parseInt(money);
+
+    public int isNumberic(String inputMoney){
+        if(inputMoney == null || inputMoney.matches("[-+]?\\d*\\.?\\d+") == false){
+            System.out.println("[ERROR] 숫자로 입력해주세요");
+            throw new NoSuchElementException("[ERROR] 숫자로 입력해주세요");
+        }
+        return Integer.parseInt(inputMoney);
     }
 
-    public void hasRestError(int money){
-        if(money%1000 != 0){
-            throw new IllegalArgumentException();
+    public void hasRestError(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 나누어 떨어지지 않습니다");
         }
     }
 
-    public void MinimumAmountError(int money){
-        if(money < 1000){
-            throw new IllegalArgumentException();
+    public void MinimumAmountError(int money) {
+        if (money < 1000) {
+            throw new IllegalArgumentException("[ERROR] 로또를 살 수 없는 금액입니다.");
         }
     }
 
-    public List<List<Integer>> makeRandomNumbers(int lottoCount){
+    public List<List<Integer>> makeRandomNumbers(int lottoCount) {
         List<List<Integer>> numbers = new ArrayList<>();
         int cnt = 0;
-        while(cnt < lottoCount){
-            List<Integer> randomNumbers = randomNumbers();
-            if(isNotDuplication(randomNumbers)){
+        while (cnt < lottoCount) {
+            List<Integer> randomNumbers = makeRandomNumber();
+            if (isNotDuplication(randomNumbers)) {
                 Collections.sort(randomNumbers);
                 numbers.add(randomNumbers);
-                cnt ++;
+                cnt++;
             }
         }
-
         return numbers;
     }
 
-    public List<Integer> randomNumbers(){
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    public List<Integer> makeRandomNumber() {
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        return numbers;
     }
 
-    public boolean isNotDuplication(List<Integer> numbers){
+    public boolean isNotDuplication(List<Integer> numbers) {
         Set<Integer> numberSet = new HashSet<>(numbers);
-        if(numbers.size() != numberSet.size()){
+        if (numbers.size() != numberSet.size()) {
             return false;
         }
         return true;
