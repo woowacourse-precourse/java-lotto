6ec4b5c6215aca59e38234dc5404f2cc2b78c112
nomushroomplz <aaron97@knou.ac.kr>
2022-11-15 23:25:20 +0900

    feat(OutputModule): 메시지 출력 기능

--- src/main/java/lotto/view/OutputModule.java
+++ src/main/java/lotto/view/OutputModule.java
@@ -1,4 +1,15 @@
 package lotto.view;
-
+import static lotto.view.PrintMessage.*;
 public class OutputModule {
+    public static void printInputPurchasePrice() {
+        System.out.println(Guide_InputPurchasePrice.getMessage());
+    }
+
+    public static void printInputLuckyNumbers() {
+        System.out.println(Guide_InputLuckyNumbers.getMessage());
+    }
+
+    public static void printInputBonusNumber() {
+        System.out.println(Guide_InputBonusNumber.getMessage());
+    }
 }
