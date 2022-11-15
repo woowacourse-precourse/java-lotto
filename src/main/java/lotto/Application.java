ba2728f9fd4d5a4fdccbfc7caf1c44567bbdc5c3
yewon <yewon.ux@gmail.com>
2022-11-15 23:35:15 +0900

    feat: get user input

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String INPUT_AMOUNT_MONEY = "구입 금액을 입력해주세요.";
+        String INPUT_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
+        String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+        System.out.println(INPUT_AMOUNT_MONEY);
+        String anmountMoney = Console.readline();
+        System.out.println(INPUT_LOTTO_NUMBER);
+        String lottoNumber = Console.readline();
+        System.out.println(INPUT_BONUS_NUMBER);
+        String lottoNumber = Console.readline();
     }
 }
