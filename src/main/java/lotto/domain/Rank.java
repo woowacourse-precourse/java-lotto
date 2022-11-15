2880a4a914013ba79c93b0d2742741531de82af7
ohhonggi <5ghdrl@naver.com>
2022-11-15 22:36:20 +0900

    refactor($Rank): Modify Rank structure & Add calculateRank function
    Modified structure:
    - boolean -> BiPredicate
    
    Added function list
    - calculateRank

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,20 +1,29 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.function.BiPredicate;
+
 public enum Rank {
-    FIRST(2000000000, 6, false),
-    SECOND_BONUS(30000000, 5, true),
-    SECOND(1500000, 5, false),
-    FOURTH(50000, 4, false),
-    FIFTH(5000, 3, false),
-    SIX(0, 0, false);
+    FIRST(2000000000, 6, (count, bonus) -> count == 6),
+    SECOND_BONUS(30000000, 5, (count, bonus) -> count == 5 && bonus),
+    SECOND(1500000, 5, (count, bonus) -> count == 5),
+    FOURTH(50000, 4, (count, bonus) -> count == 4),
+    FIFTH(5000, 3, (count, bonus) -> count == 3),
+    SIX(0, 0, (count, bonus) -> count < 3);
 
     private final int reward;
     private final int count;
-    private final boolean bonus;
+    private final BiPredicate<Integer, Boolean> compare;
 
-    Rank(int reward, final int count, boolean bonus) {
+    Rank(int reward, final int count, BiPredicate<Integer, Boolean> compare) {
         this.reward = reward;
         this.count = count;
-        this.bonus = bonus;
+        this.compare = compare;
+    }
+
+    public Rank calculateRank(int count, boolean bonus) {
+        return Arrays.stream(Rank.values()).
+                filter(rank -> rank.compare.test(count, bonus))
+                .findAny().get();
     }
 }
