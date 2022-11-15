8215856a8844beb8b89579cb9ff728e08ed11303
chordpli <chordplaylist@gmail.com>
2022-11-15 22:18:10 +0900

    refactor: 에러메세지 enum 사용

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,8 @@ package lotto;
 
 import java.util.List;
 
+import static Exception.Error.*;
+
 public class Lotto {
     private final List<Integer> numbers;
 
@@ -14,7 +16,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호가 부족합니다.");
+            throw new IllegalArgumentException(ERROR_LACK_NUMBER.getMessage());
         }
     }
     
@@ -26,15 +28,14 @@ public class Lotto {
     // 로또 넘버가 중복될 때 예외
     public void validateOverlap(List<Integer> numbers) {
         if (numbers.size() != numbers.stream().distinct().count()) {
-            System.out.println("[ERROR] 로또 번호가 중복되었습니다.");
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_OVERLAP_NUMBER.getMessage());
         }
     }
     // 로또 넘버가 1~45 범위에 해당되지 않을 때 예외.
     public void validateRangeOver(List<Integer> numbers) {
         for (Integer number : numbers) {
             if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                throw new IllegalArgumentException(ERROR_RangeOver.getMessage());
             }
         }
     }
