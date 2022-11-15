5c73f93f84b0115a2167b8e620bf65257e2e4c35
thousandcows <milcamboo@gmail.com>
2022-11-15 21:38:07 +0900

    feat: create InputValues class and move regarding functions from Lotto.java
    Moved following functions
    - inputTotalPayment
    - inputBonusNumber
    - inputLottoNumber

---
+++ src/main/java/UserInterface/InputValues.java
@@ -0,0 +1,43 @@
+package UserInterface;
+
+import Utils.Validator;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class InputValues {
+
+    private final Validator validator;
+
+    public InputValues(Validator validator) {
+        this.validator = validator;
+    }
+
+    public int inputTotalPayment() {
+        String payment = Console.readLine();
+        validator.validatePayment(payment);
+        return Integer.parseInt(payment);
+    }
+
+    public int inputBonusNumber() {
+        String bonus = Console.readLine();
+        return Integer.parseInt(bonus);
+    }
+
+    public List<Integer> inputLottoNumber(String input) {
+        StringTokenizer st = new StringTokenizer(input, ",");
+        List<Integer> winningLottoNumber = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            String currentDigit = st.nextToken();
+
+            validator.checkIfTheInputIsInteger(currentDigit);
+
+            winningLottoNumber.add(Integer.parseInt(currentDigit));
+
+        }
+
+        return winningLottoNumber;
+    }
+}
