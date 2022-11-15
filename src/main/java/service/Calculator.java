4ad5133a8f6b68dffe824e22f3511580178e6e1e
DDing77 <devseo2021@gmail.com>
2022-11-15 20:15:59 +0900

    feat(service) : 수익률 계산 메소드

--- src/main/java/service/Calculator.java
+++ src/main/java/service/Calculator.java
@@ -1,7 +1,10 @@
 package service;
 
 import common.regulation.NumberRule;
+import domain.Rank;
 import exception.InputException;
+
+import java.util.List;
 
 public class Calculator {
 
@@ -12,5 +15,17 @@ public class Calculator {
         int oneLottoPrice = NumberRule.LOTTO_ONE_PRICE.getValue();
 
         return total / oneLottoPrice;
+    }
+
+    public static double getYield(List<Rank> ranks, int size) {
+        double yield;
+        int sumTotal = 0;
+
+        for (Rank rank : ranks) {
+            sumTotal += rank.getTotalReward();
+        }
+
+        yield = sumTotal / (double) (size * NumberRule.LOTTO_ONE_PRICE.getValue());
+        return (Math.round(yield * 10000) / 100.0);
     }
 }
