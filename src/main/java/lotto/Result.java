bc8ceb38dbb25ddd847ef458a3e8a13b2b422000
박정현 <ros28081@naver.com>
2022-11-15 17:45:17 +0900

    feat: Result enum 작성 및 결과 출력

---
+++ src/main/java/lotto/Result.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import java.util.Arrays;
+
+public enum Result {
+    DEFAULT(0, false, "상금 없음", 0),
+    FIFTH(3, false, "3개 일치 (5,000원) - ", 5000),
+    FOURTH(4, false, "4개 일치 (50,000원) - ", 50000),
+    THIRD(5, false, "5개 일치 (1,500,000원) - ", 1500000),
+    SECOND(5, true, "5개 일치, 보너스 볼 일치 (30,000,000원) - ", 30000000),
+    FIRST(6, false, "6개 일치 (2,000,000,000원) - ", 2000000000);
+
+    private final int correctCount;
+    private final boolean isBonus;
+    private final String print;
+    private final int price;
+
+    Result(int correctCount, boolean isBonus, String print, int price){
+        this.correctCount = correctCount;
+        this.isBonus = isBonus;
+        this.print = print;
+        this.price = price;
+    }
+
+    public String print(){
+        return print;
+    }
+
+    public static Result getResult(int correctCount, boolean isBonus){
+        return Arrays.stream(Result.values())
+                .filter(result -> (result.isBonus==isBonus && result.correctCount==correctCount))
+                .findAny()
+                .orElse(DEFAULT);
+    }
+
+}
