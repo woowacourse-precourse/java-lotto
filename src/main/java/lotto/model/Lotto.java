ee441db969d4235b6aec27d6c9b1f8d64c2dca14
Jeeseob <jeeseob5761@gmail.com>
2022-11-15 17:02:53 +0900

    feat: 로또 객체 valid 체크 함수에 범위, 중복 여부 체크 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,11 @@
 package lotto.model;
 
+import lotto.util.ArgumentExceptionMessage;
+import lotto.util.LottoConstant;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,13 +16,37 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        checkLength(numbers);
+        checkRange(numbers);
+        checkOverlap(numbers);
+    }
+
+    private void checkLength(List<Integer> numbers) {
+        if (numbers.size() != LottoConstant.LOTTO_LENGTH.getValue()) {
+            throw new IllegalArgumentException(ArgumentExceptionMessage.INPUT_LOTTO_UNCONFORMABLE_LENGTH.getMessage());
+        }
+    }
+
+    private void checkRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(integer -> integer < LottoConstant.LOTTO_RANGE_MIN.getValue() || integer > LottoConstant.LOTTO_RANGE_MAX.getValue())) {
+            throw new IllegalArgumentException(ArgumentExceptionMessage.INPUT_LOTTO_UNCONFORMABLE_NUMBER_RANGE.getMessage());
+        }
+    }
+
+    private void checkOverlap(List<Integer> numbers) {
+        Set<Integer> numbersOverlap = new HashSet<>(numbers);
+        if (numbersOverlap.size() != LottoConstant.LOTTO_LENGTH.getValue()) {
+            throw new IllegalArgumentException(ArgumentExceptionMessage.INPUT_LOTTO_NUMBER_OVERLAP.getMessage());
         }
     }
 
     // TODO: 추가 기능 구현
     public List<Integer> getNumbers() {
         return this.numbers;
+    }
+
+    @Override
+    public String toString() {
+        return this.numbers.toString();
     }
 }
