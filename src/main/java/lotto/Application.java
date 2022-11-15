e3ef70e3082b4ea09e69d380daf07c845f8a1c6e
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-11-15 18:54:56 +0900

    docs(README): 기능 목록 추가 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,11 @@
 package lotto;
 
-import java.util.List;
+import lotto.domain.LottoProgram;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        lotto.domain.NumberGenerator numberGenerator=new lotto.domain.NumberGenerator();
-        List<Integer> result = numberGenerator.createRandomNumbers();
-        System.out.println(result);
+        LottoProgram lottoProgram = new LottoProgram();
     }
 }
--- src/main/java/lotto/domain/LottoProgram.java
+++ src/main/java/lotto/domain/LottoProgram.java
@@ -1,2 +1,60 @@
-package lotto.domain;public class LottoProgram {
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.commons.InformationLines;
+import lotto.commons.Lotto;
+import lotto.commons.WinLotto;
+
+public class LottoProgram {
+
+    InputOutput inputOutput = new InputOutput();
+    NumberGenerator numberGenerator = new NumberGenerator();
+    Referee referee = new Referee();
+    Calculate calculate = new Calculate();
+
+    List<Lotto> lottos = new ArrayList<>();
+    WinLotto winLotto;
+
+
+    public LottoProgram() {
+        int buyAmount = buyLotto();
+        getWinLotto();
+        winStats(buyAmount);
+    }
+
+    private int buyLotto() {
+        System.out.println(InformationLines.BUY_AMOUNT_LINE.getLine());
+        int buyAmount = inputOutput.getLottoBuyAmount();
+        inputOutput.printLottoBuyAmount(buyAmount);
+
+        for (int index = 0; index < buyAmount; index++) {
+            lottos.add(new Lotto(numberGenerator.createRandomNumbers()));
+            System.out.println(lottos.get(index).getNumbers());
+        }
+        return buyAmount;
+    }
+
+    private void getWinLotto() {
+        System.out.println(InformationLines.WIN_NUMBER_LINE.getLine());
+        List<Integer> winNumbers = inputOutput.getLottoNumbers();
+
+        System.out.println(InformationLines.BONUS_NUMBER_LINE.getLine());
+        int bonusNumber = inputOutput.getLottoBonusNumbers();
+
+        winLotto = new WinLotto(winNumbers, bonusNumber);
+    }
+
+    private void winStats(int buyAmount) {
+        for (Lotto myLotto : lottos) {
+            String compareResult = referee.compare(myLotto.getNumbers(),
+                winLotto.getNumbers(), winLotto.getBonus());
+            winLotto.updateWinHistory(compareResult);
+        }
+        double profit = calculate.calculateRateOfProfit(buyAmount, winLotto.getWinning());
+
+        System.out.println(InformationLines.WIN_STATS_LINE.getLine());
+        inputOutput.printWinHistory(winLotto.getWinHistory());
+        inputOutput.printRateOfProfit(profit);
+    }
 }
