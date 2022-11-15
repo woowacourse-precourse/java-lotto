0daa22df1550287782df2f43d9d629457af28363
anneu <b.imnuj@gmail.com>
2022-11-15 14:46:18 +0900

    refactor: 보너스 생성 관련 검증 기능을 Bonus 클래스로 이동

--- src/main/java/lotto/model/Bonus.java
+++ src/main/java/lotto/model/Bonus.java
@@ -1,8 +1,11 @@
 package lotto.model;
 
+import static lotto.utils.ConstantUtil.ERROR;
+
 import java.util.List;
 
 public class Bonus {
+    private static final String BONUS_ERROR_MESSAGE = ERROR + "보너스 번호는 당첨 번호와 중복될 수 없습니다.";
 
     private final int bonusNumber;
 
@@ -16,8 +19,13 @@ public class Bonus {
     }
 
     private void validate(int bonusNumber, Lotto winningLotto) {
-        Validator validator = new Validator();
-        validator.validateNumberRange(bonusNumber);
-        validator.validateBonusNumber(bonusNumber, winningLotto);
+        validateNumberRange(bonusNumber);
+        validateBonusNumber(bonusNumber, winningLotto);
+    }
+
+    private void validateBonusNumber(int bonusNumber, Lotto winningLotto) {
+        if (winningLotto.isExistNumber(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_ERROR_MESSAGE);
+        }
     }
 }
