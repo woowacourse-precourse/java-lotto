914579a0bac04e249b766cbf60e70bbe2fc91f49
Cyma-s <ssol0319@naver.com>
2022-11-15 18:28:15 +0900

    test(InputErrorTest): add test to verify format of bonus number

--- src/test/java/lotto/InputErrorTest.java
+++ src/test/java/lotto/InputErrorTest.java
@@ -27,8 +27,16 @@ public class InputErrorTest extends NsTest {
     @Test
     void 당첨번호_숫자가_아닌_문자열_테스트() {
         assertSimpleTest(() -> {
-            runException("2000", "21,42,6,15,27f");
+            runException("2000", "21,42,6,15,27f,8");
             assertThat(output()).contains(InputErrorMessage.INVALID_LOTTO_NUMBERS.getErrorMessage());
+        });
+    }
+
+    @Test
+    void 보너스번호_형식_테스트() {
+        assertSimpleTest(() -> {
+            runException("2000", "21,42,6,15,27,9", "E");
+            assertThat(output()).contains(InputErrorMessage.BONUS_NUMBER_IS_NOT_NUMBER.getErrorMessage());
         });
     }
 
