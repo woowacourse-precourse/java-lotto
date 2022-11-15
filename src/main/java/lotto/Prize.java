43cd1ca9ea61194a2faa47a5ffadcd7d9816cb4a
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-11-15 16:56:25 +0900

    feat(Prize) : create to enter an amount
    create the 'money' with console that to enter an amount
    change money from string to number

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,9 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Prize {
     public static final String enterMoney = "구매금액을 입력해 주세요.";
 
     public Prize() {
+        Integer money = getMoneyToBuyLotto();
+        System.out.println(money);
+    }
+
+    public Integer getMoneyToBuyLotto(){
         System.out.println(enterMoney);
+        String money = Console.readLine();
+        return Integer.valueOf(money);
     }
 }
