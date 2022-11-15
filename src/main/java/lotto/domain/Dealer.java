c6519ec62ddf4a0ebc1737f3c7989b95236f97fb
JeonSangYong <tkddyd420@naver.com>
2022-11-15 13:04:56 +0900

    feat : 보너스 번호 예외처리

--- src/main/java/lotto/domain/Dealer.java
+++ src/main/java/lotto/domain/Dealer.java
@@ -31,9 +31,9 @@ public class Dealer {
     public void inputBonusNumber() {
         System.out.println(Message.INPUT_BONUS_LOTTO_NUMBER);
         String number = Console.readLine();
-        // 숫자 입력이 아닐 때 예외처리
-        // 당첨 번호 숫자일 때 예외처리
-        // 1 ~ 45 범위의 숫자가 아닐 때 예외처리
+        DealerValidator.validNumber(number);
         bonusNumber = Integer.parseInt(number);
+        DealerValidator.validLottoContainsNumber(lotto.getNumbers(), bonusNumber);
+        DealerValidator.validBetween1And45(bonusNumber);
     }
 }
--- src/main/java/lotto/validate/DealerValidator.java
+++ src/main/java/lotto/validate/DealerValidator.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static lotto.LottoGenerator.*;
+import static lotto.log.CustomLogger.*;
 
 public class DealerValidator extends Validator {
 
@@ -19,7 +20,14 @@ public class DealerValidator extends Validator {
                 .collect(Collectors.toList());
 
         if (results.size() != LOTTO_NUMBER_COUNT) {
-            CustomLogger.LOG.severe("쉼표를 기준으로 중복되지 않는 6개의 로또 번호를 입력하지 않았습니다.");
+            LOG.severe("쉼표를 기준으로 중복되지 않는 6개의 로또 번호를 입력하지 않았습니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validLottoContainsNumber(List<Integer> lotto, int number) {
+        if (lotto.contains(number)) {
+            LOG.severe("당첨 번호와 중복되었습니다.");
             throw new IllegalArgumentException();
         }
     }
