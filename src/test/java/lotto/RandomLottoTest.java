e60dbb5e30edd5889782613787f06096bc97eef9
delaylee <delaylee219@gmail.com>
2022-11-15 15:07:21 +0900

    test: 로또 생성 개수 확인 테스트 추가

---
+++ src/test/java/lotto/RandomLottoTest.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomLottoTest {
+
+    @DisplayName("count 개수 만큼 로또가 생성되는지 확인")
+    @Test
+    public void lottoCountTest() {
+        RandomLotto randomLotto = new RandomLotto();
+        List<List<Integer>> randomLottos = randomLotto.getRandomLottos(10);
+
+        assertThat(randomLottos.size()).isEqualTo(10);
+    }
+}
