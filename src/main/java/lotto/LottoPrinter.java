867c9b602f07113aebe0f605021422c3e3419392
BaeDongHyeon <sasd730@gmail.com>
2022-11-15 10:26:06 +0900

    feat: 로또 결과 출력 메소드 생성
    - lottoResultPrint 메소드로 일치하는 개수별 통계 출력
    - revenueResultPrint 메소드로 수익률 출력

--- src/main/java/lotto/LottoPrinter.java
+++ src/main/java/lotto/LottoPrinter.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
+import java.util.Map;
 
 public class LottoPrinter {
     public static String inputer(String message) {
@@ -18,5 +19,19 @@ public class LottoPrinter {
             System.out.println(lotto);
         }
         System.out.println();
+    }
+
+    public static void lottoResultPrint(Map<LottoGrade, Integer> lottoResults) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.printf("3개 일치 (5,000원) - %d개\n", lottoResults.get(LottoGrade.FIVE));
+        System.out.printf("4개 일치 (50,000원) - %d개\n", lottoResults.get(LottoGrade.FOUR));
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", lottoResults.get(LottoGrade.THREE));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", lottoResults.get(LottoGrade.TWO));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", lottoResults.get(LottoGrade.ONE));
+    }
+
+    public static void revenueResultPrint(double revenue) {
+        System.out.printf("총 수익률은 %.1f%s입니다.", revenue, "%");
     }
 }
