fc4ce6e278ff5dba70d0876dea3af91d948d739e
이지영 <wldudwldhd@naver.com>
2022-11-15 21:15:59 +0900

    fix:testcase예외 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,11 @@
 package lotto;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.Comparator;
 
 public class Lotto {
     private final List<Integer> numbers;
--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -7,13 +7,7 @@ import java.util.List;
 //로또 번호 발행 기계
 public class LottoMachine {
     public Lotto autoGenerate(){
-        List<Integer> numbers=new ArrayList<>();
-        while(numbers.size()<6){
-            int randomNumber = Randoms.pickNumberInRange(1, 45);
-            if (!numbers.contains(randomNumber)) {
-                numbers.add(randomNumber);
-            }
-        }
+        List<Integer> numbers=new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45,6));
         return new Lotto(numbers);
     }
     public Lotto manualGenerate(List<Integer> numbers){
