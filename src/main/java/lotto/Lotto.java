e9b21c76934a52adbeb8f60b7278222dfd6e25c3
jw0293 <k38836919@gmail.com>
2022-11-15 18:38:49 +0900

    feat : Check valid Lotto Numbers

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,43 @@
 package lotto;
 
-import java.util.List;
+import java.util.*;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        numbers.sort(Comparator.naturalOrder());
         this.numbers = numbers;
+        showLotto();
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        Set<Integer> numSet = new HashSet<>(numbers);
+        if(numSet.size() != numbers.size()){
+            throw new IllegalArgumentException();
+        }
     }
 
     // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+
+    private void showLotto() {
+        System.out.print("[");
+        for (int i = 0; i < numbers.size(); i++) {
+            if (i == numbers.size() - 1) {
+                System.out.print(numbers.get(i));
+                continue;
+            }
+            System.out.print(numbers.get(i) + ", ");
+        }
+        System.out.println("]");
+    }
 }
