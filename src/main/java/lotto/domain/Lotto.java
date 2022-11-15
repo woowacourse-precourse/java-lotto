299a30745711bdfced63247fb0847d065155a1a3
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-15 18:49:40 +0900

    feat(LottoGame): 랜덤으로 만들어진 로또 번호 갯수가 6개인지 확인

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,29 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+    private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateRange(numbers);
+    }
+
+    public void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (int a : numbers) {
+            if (a < 1 || a > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
 }
