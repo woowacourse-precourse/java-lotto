2db97c7a08e1929947923e7ddcbf1321106ada51
tunkcalb <wjdgusdndi@gmail.com>
2022-11-15 22:52:54 +0900

    modity Lotto error

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,20 +17,23 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리 입니다.");
         }
-    }
 
-    public void DuplicateNumber(List<Integer> numbers) {
         Set<Integer> number = new HashSet<>(numbers);
         if (number.size() != numbers.size()) {
-            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 존재합니다.");
+            System.out.println("[ERROR] 중복된 숫자가 존재합니다.");
+            throw new IllegalArgumentException();
         }
+
+        for(Integer num : numbers){
+            if(num < 1 || num > 45){
+                throw new IllegalArgumentException("[ERROR] 1에서 45 사이의 숫자를 입력하세요.");
+            }
+        }
+
     }
 
     public void NumberRange(List<Integer> numbers) {
-        int RangeCount = (int) numbers.stream().filter(number -> number < 1 && 45 < number).count();
-        if (RangeCount != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
+
     }
     public List<Integer> GetNumbers(){
         return new ArrayList<>(this.numbers);
