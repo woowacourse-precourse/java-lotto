257277634fdb9d626c33e4c7888d041be0b5c11a
DavinAn <blueapple1030@gmail.com>
2022-11-15 23:30:02 +0900

    feat : winningNumbers(), reads in user's winning lotto numbers

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -2,11 +2,24 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Input {
 
     public int purchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine().trim();
         return Integer.parseInt(input);
+    }
+
+    public List<Integer> winningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] input = Console.readLine().trim().split(",");
+        List<String> stringList = Arrays.asList(input);
+        return stringList.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
