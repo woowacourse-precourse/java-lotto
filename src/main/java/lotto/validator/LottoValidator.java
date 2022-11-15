8f59f43eb505478eb5028a91edbf20c9e13b53f9
leesangjin <tkdwls0628@gmail.com>
2022-11-15 17:26:34 +0900

    refactor(LottoValidator): 접근 제어자 변경

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -2,7 +2,6 @@ package lotto.validator;
 
 import lotto.constant.Constant;
 import lotto.constant.ErrorMessage;
-
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -15,20 +14,20 @@ public class LottoValidator {
         validateDuplicates(lottoNumbers);
     }
 
-    public static void validateLottoSize(List<Integer> lottoNumbers) {
+    private static void validateLottoSize(List<Integer> lottoNumbers) {
         if (lottoNumbers.size() != Constant.LOTTO_SIZE) {
             throw new IllegalArgumentException(ErrorMessage.WRONG_SIZE_MSG);
         }
     }
 
-    public static void validateLottoValues(List<Integer> lottoNumbers) {
+    private static void validateLottoValues(List<Integer> lottoNumbers) {
         if (lottoNumbers.stream().anyMatch(number ->
                 number < Constant.MIN_LOTTO_NUMBER || number > Constant.MAX_LOTTO_NUMBER)) {
             throw new IllegalArgumentException(ErrorMessage.WRONG_VALUE_MSG);
         }
     }
 
-    public static void validateDuplicates(List<Integer> lottoNumbers) {
+    private static void validateDuplicates(List<Integer> lottoNumbers) {
         Set<Integer> nonDuplicateNumbers = new HashSet<>(lottoNumbers);
         if (nonDuplicateNumbers.size() != Constant.LOTTO_SIZE) {
             throw new IllegalArgumentException(ErrorMessage.DUPLICATE_MSG);
