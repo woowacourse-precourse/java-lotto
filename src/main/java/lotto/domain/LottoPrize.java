2667be45f9db021e376e9f9c80d050623b39a5a4
yeonhee <ydol2@naver.com>
2022-11-15 14:46:21 +0900

    refactor(Lottoprize) : 총 상금 계산하는 함수 stream() 이용하여 변경

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -2,27 +2,19 @@ package lotto.domain;
 
 import java.util.Map;
 
-import static lotto.domain.LottoResult.FOUR;
-import static lotto.domain.LottoResult.THREE;
-import static lotto.domain.LottoResult.FIVE;
-import static lotto.domain.LottoResult.FIVE_BONUS;
-import static lotto.domain.LottoResult.SIX;
-
 public class LottoPrize {
 
     public float calculateEarning(int lottoPrice) {
         return ((float) addPrize() / (float) lottoPrice) * 100;
     }
+    
     private int addPrize(){
         Map<LottoResult,Integer>  lottoResultMap = LottoStatistics.getLottoResultMap();
-        int total = 0;
-        for ( LottoResult key : lottoResultMap.keySet() ) {
-            if (key == THREE) total+= key.getPrize() * lottoResultMap.get(key);
-            else if (key == FOUR) total += key.getPrize() * lottoResultMap.get(key);
-            else if (key == FIVE) total += key.getPrize() * lottoResultMap.get(key);
-            else if (key == FIVE_BONUS) total += key.getPrize() * lottoResultMap.get(key);
-            else if (key == SIX) total += key.getPrize() * lottoResultMap.get(key);
-        }
+        int total = lottoResultMap.keySet()
+                .stream()
+                .map(key -> key.getPrize() * lottoResultMap.get(key))
+                .mapToInt(prize -> (int) prize)
+                .reduce(0,Integer::sum);
         return total;
     }
 }
