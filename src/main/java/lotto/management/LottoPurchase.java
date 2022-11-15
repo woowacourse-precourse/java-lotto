1b39a8a19fe70bb8066360c40e5b61cf0f235501
HamSeungChan <head0618@naver.com>
2022-11-15 16:26:04 +0900

    refactor : 매개 변수 타입 변경 및 예외 확인 기능 추가 구현

--- src/main/java/lotto/management/LottoPurchase.java
+++ src/main/java/lotto/management/LottoPurchase.java
@@ -2,21 +2,30 @@ package lotto.management;
 
 public class LottoPurchase {
 
-    static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_PRICE = 1000;
+    private static final String NUMBER_REGEX  = "^[0-9]*$";
 
     final int LOTTO_TICKETS;
 
-    public LottoPurchase(int purchaseMoney) {
-        valid(purchaseMoney);
+    public LottoPurchase(String purchaseMoney) {
+        isNumber(purchaseMoney);
+        validDivide(purchaseMoney);
         this.LOTTO_TICKETS = changeMoneyToLottoTickets(purchaseMoney);
     }
 
-    private int changeMoneyToLottoTickets(int purchaseAmount) {
-        return purchaseAmount / LOTTO_PRICE;
+    private int changeMoneyToLottoTickets(String purchaseAmount) {
+        return Integer.parseInt(purchaseAmount) / LOTTO_PRICE;
     }
 
-    private void valid(int purchaseMoney) {
-        if (purchaseMoney % LOTTO_PRICE > 0) {
+    private void isNumber(String purchaseMoney){
+        if (!purchaseMoney.matches(NUMBER_REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 숫자로 입력받아야 합니다.");
+        }
+    }
+
+    private void validDivide(String purchaseMoney) {
+        int amount = Integer.parseInt(purchaseMoney);
+        if (amount % LOTTO_PRICE > 0) {
             throw new IllegalArgumentException("[ERROR] 구매 금액이 1000원 단위여야 합니다.");
         }
     }
