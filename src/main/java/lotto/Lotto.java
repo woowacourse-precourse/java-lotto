1a8e1a18028b56b04e2253d5bb5feec8f72a92a2
thousandcows <milcamboo@gmail.com>
2022-11-15 11:33:09 +0900

    fix: fix bug in calculating lotto score

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -28,7 +28,7 @@ public class Lotto {
         }
     }
 
-    public void validatePayment(String payment) {
+    public static void validatePayment(String payment) {
 
         checkIfTheInputIsInteger(payment);
 
@@ -52,7 +52,7 @@ public class Lotto {
         }
     }
 
-    public List<List<Integer>> generateLottoNumber(int theNumberOfLotto) {
+    public static List<List<Integer>> generateLottoNumber(int theNumberOfLotto) {
         List<List<Integer>> generatedLottoNumber = new ArrayList<>();
         for (int i = 0; i < theNumberOfLotto; i++) {
             generatedLottoNumber.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
@@ -71,7 +71,10 @@ public class Lotto {
         int matchCount = 0;
 
         for (int i = 0; i < 6; i++) {
-            matchCount += checkNumberMatch(numbers.get(i), lotto.get(i));
+            if (numbers.contains(lotto.get(i))) {
+                matchCount += 1;
+            }
+//            matchCount += checkNumberMatch(numbers.get(i), lotto.get(i));
         }
 
         if (matchCount < 3) {
@@ -90,7 +93,6 @@ public class Lotto {
 
         for (List<Integer> lotto : generatedLottoNumber) {
             int index = getIndividualLottoScore(lotto, bonus) - 3;
-
             if (index < 0) {
                 continue;
             }
@@ -107,19 +109,21 @@ public class Lotto {
         for (int i = 0; i < 5; i++) {
             totalPrize += prizeValues.get(i) * result.get(i);
         }
-
-        return String.format("%.1f", (double) totalPrize / totalPayment);
+        return String.format("%.1f", (double) totalPrize / totalPayment * 100);
     }
 
-    public String inputTotalPayment() {
-        return Console.readLine();
+    public static int inputTotalPayment() {
+        String payment = Console.readLine();
+        validatePayment(payment);
+        return Integer.parseInt(payment);
     }
 
-    public String inputBonusNumber() {
-        return Console.readLine();
+    public static int inputBonusNumber() {
+        String bonus = Console.readLine();
+        return Integer.parseInt(bonus);
     }
 
-    public List<Integer> inputLottoNumber(String input) {
+    public static List<Integer> inputLottoNumber(String input) {
         StringTokenizer st = new StringTokenizer(input, ",");
         List<Integer> winningLottoNumber = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
@@ -134,13 +138,13 @@ public class Lotto {
         return winningLottoNumber;
     }
 
-    public void checkIfTheInputIsInteger(String digit) {
+    public static void checkIfTheInputIsInteger(String digit) {
         if (!digit.matches("^[0-9]*$")) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
         }
     }
 
-    public void showLottoPaymentReceipt(int totalPayment, List<List<Integer>> generatedLotto) {
+    public static void showLottoPaymentReceipt(int totalPayment, List<List<Integer>> generatedLotto) {
         String receipt = "";
 
         receipt += String.format("%d개를 구매했습니다.\n", totalPayment);
@@ -156,8 +160,8 @@ public class Lotto {
         report += String.format("3개 일치 (5,000원) - %d개\n", result.get(0));
         report += String.format("4개 일치 (50,000원) - %d개\n", result.get(1));
         report += String.format("5개 일치 (1,500,000원) - %d개\n", result.get(2));
-        report += String.format("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", result.get(3));
-        report += String.format("6개 일치 (2,000,000,000원) - %d개\n", result.get(4));
+        report += String.format("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", result.get(4));
+        report += String.format("6개 일치 (2,000,000,000원) - %d개\n", result.get(3));
         report += "총 수익률은 "+ yield + "%입니다.";
         System.out.println(report);
     }
