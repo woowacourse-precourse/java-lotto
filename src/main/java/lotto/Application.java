15e3e1b733f2d45386001e3091eaab617c1fb5ba
eyeofsol <kingjihong@naver.com>
2022-11-15 21:34:24 +0900

    feat(Application): Application 작성
    메인 메서드를 작성해서 동작시켰습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.*;
+import lotto.ui.Input;
+import lotto.ui.InputMessages;
+import lotto.ui.Output;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = new Input();
+        Output output = new Output();
+
+        Money money = input.buy();
+        Purchase purchase = new Purchase(money.getMoney());
+        output.printPurchase(purchase);
+
+        Answer answer = input.answer();
+        input.bonus(answer);
+        Pick pick = new Pick(answer.getAnswer(), answer.getBonus());
+        pick.drawLottoes(purchase.getLottoes());
+
+        output.printPick(pick);
+        output.printRateOfReturn(pick.getRateOfReturn(money.getMoney()));
     }
+
+
 }
