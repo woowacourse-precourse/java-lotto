bd49f4adeb87eb37a88cdef4405a2690a59145d8
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-15 18:04:55 +0900

    feat(LottoGame): 로또 랜덤 번호 발행하기

--- src/main/java/lotto/controller/Game.java
+++ src/main/java/lotto/controller/Game.java
@@ -1,10 +1,13 @@
 package lotto.controller;
 
+import lotto.domain.RandomNumbersGenerator;
 import lotto.view.InputView;
+import java.util.List;
 
 public class Game {
     public void startLottoGame() {
         int money = InputView.inputLottoAmount();
-
+        RandomNumbersGenerator randomNumbers = new RandomNumbersGenerator();
+        List<Integer> lottoNumbers = randomNumbers.createRandomNumbers();
     }
 }
