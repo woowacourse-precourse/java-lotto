773420f10361ff1519f6e12de53cac7da2a1551c
ChaCha3088 <cha3088@gmail.com>
2022-11-15 20:59:28 +0900

    fix(/src/main/java/Lotto.java): fix constant final 키워드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
-    private List<Integer> numbers;
+    private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         HashSet<Integer> playerNumbers = Model.MakeListToSet(numbers);
