673712526f60ad95b1edeb8f127f5920d9cb017f
ijung-won <dlwnddnjs96@gmail.com>
2022-11-15 20:25:12 +0900

    test(ApplicationTest): 새로 구현한 내용에 맡게 refactoring

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -74,7 +74,7 @@ public class Lotto {
         System.out.println("당첨 통계");
         System.out.println("---");
         System.out.println("3개 일치 (5,000원) - " + Collections.frequency(gradeList, Grade.FIFTH) +"개");
-        System.out.println("4개 일치 (50,000원) -" + Collections.frequency(gradeList, Grade.FOURTH) +"개");
+        System.out.println("4개 일치 (50,000원) - " + Collections.frequency(gradeList, Grade.FOURTH) +"개");
         System.out.println("5개 일치 (1,500,000원) - " + Collections.frequency(gradeList, Grade.THIRD) +"개");
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + Collections.frequency(gradeList, Grade.SECOND) +"개");
         System.out.println("6개 일치 (2,000,000,000원) - " + Collections.frequency(gradeList, Grade.FIRST) +"개");
