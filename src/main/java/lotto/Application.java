7a34c020a74c65b99624494ab9f9bf4c84907ab2
YooSeHyeok <tpgurtpdms@gmail.com>
2022-11-15 19:47:57 +0900

    메인

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,11 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            LottoSystem lottosystem = LottoSystem.start();
+            lottosystem.result();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
