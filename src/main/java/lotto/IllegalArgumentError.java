e14f3317a7f3423c3abf8297aad329fb1bac7b58
dlgosla <dlgoslaek1@gmail.com>
2022-11-15 23:09:56 +0900

    chore: 필요없는 메소드 제거

--- src/main/java/lotto/IllegalArgumentError.java
+++ src/main/java/lotto/IllegalArgumentError.java
@@ -2,14 +2,6 @@ package lotto;
 
 public class IllegalArgumentError extends IllegalArgumentException {
 
-    public IllegalArgumentError() {
-    }
-
-    public IllegalArgumentError(String message, Throwable cause) {
-
-        super("[ERROR] " + message, cause);
-    }
-
     public IllegalArgumentError(String message) {
         super("[ERROR] " + message);
     }
