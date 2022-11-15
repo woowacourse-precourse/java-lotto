d5fbe29e2a9222579365c8e54d340c861a256e04
SeongbinPark <binni0515@gmail.com>
2022-11-15 20:49:47 +0900

    feat(Rank): add Rank enum with countOfMatchingNumbers and winningMoney

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,18 @@
+package lotto;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    LOSE(0, 0);
+
+    private final int countOfMatchingNumbers;
+    private final int winningMoney;
+
+    Rank(int countOfMatchingNumbers, int winningMoney) {
+        this.countOfMatchingNumbers = countOfMatchingNumbers;
+        this.winningMoney = winningMoney;
+    }
+}
