e29eff81664bb0692acdf176beacc4ce1f575277
sonsoowon <yuwoo1018@gmail.com>
2022-11-15 18:19:36 +0900

    test(InputValidatorTest): 불필요한 import문 삭제

--- src/test/java/lotto/ui/valid/InputValidatorTest.java
+++ src/test/java/lotto/ui/valid/InputValidatorTest.java
@@ -1,6 +1,5 @@
 package lotto.ui.valid;
 
-import lotto.ui.valid.InputValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
