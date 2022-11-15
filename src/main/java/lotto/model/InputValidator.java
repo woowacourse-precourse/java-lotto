042f94d44a427892ac39cd69384bcbcb80211063
LeeHaiLim <hailim1997@gmail.com>
2022-11-15 09:52:14 +0900

    feat(model): implement method check money
    Added validation methods
    -check money
    -is number
    -is positive
    -is multiple price

--- src/main/java/lotto/model/InputValidator.java
+++ src/main/java/lotto/model/InputValidator.java
@@ -1,4 +1,31 @@
 package lotto.model;
 
+import lotto.Constants;
+
 public class InputValidator {
+    public static void checkMoney(String money) {
+        isNumber(money);
+        isPositive(money);
+        isMultiplePrice(money);
+    }
+
+    private static void isNumber(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void isPositive(String number) {
+        if (Integer.parseInt(number) <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void isMultiplePrice(String number) {
+        if (Integer.parseInt(number) % Constants.LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
