7db7b7919013c80c8fce9f83f9e0bd8a6d001b79
coldbucks <met.hsw@gmail.com>
2022-11-15 18:58:32 +0900

    restart(lotto): 다시시작

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,22 +1,13 @@
 package lotto;
 
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import lotto.domain.LottoSystem;
-
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class Application {
 
 
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Application T = new Application();
 
     }
 }
--- src/test/java/lotto/domain/LottoSystemTest.java
+++
@@ -1,9 +0,0 @@
-package lotto.domain;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class LottoSystemTest {
-
-}
