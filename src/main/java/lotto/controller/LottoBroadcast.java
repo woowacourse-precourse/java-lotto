4204273b416bedba0b390713d8575a6ae4500548
mihye126 <mihea12@gmail.com>
2022-11-15 22:38:59 +0900

    [feat] draw winning number

--- src/main/java/lotto/controller/LottoBroadcast.java
+++ src/main/java/lotto/controller/LottoBroadcast.java
@@ -12,8 +12,18 @@ public class LottoBroadcast {
     private int bonus;
 
     private LottoBroadcast() {
-
+        drawWinning();
     }
+    private void drawWinning() {
+        this.winning = new Lotto(getWinningInput());
+    }
+
+    private List<Integer> getWinningInput() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = InputUtility.readLine(Type.LOTTO.getValue());
+        return InputUtility.parseLotto(input);
+    }
+
 
     public Lotto getWinning() {
         return winning;
