8bae7a008c19e71ce547d18d2b5c52072f67d09c
chj <bluedog129@gmail.com>
2022-11-15 21:49:22 +0900

    feat(Model) : 보너스 로또 번호 입력 받기 set

--- src/main/java/lotto/Model.java
+++ src/main/java/lotto/Model.java
@@ -9,6 +9,7 @@ public class Model {
     private final List<Lotto> lottoList;
     private int userMoney;
     private Lotto winningLotto;
+    private int bonus;
 
     public Model() {
         this.lottoList = new ArrayList<>();
@@ -25,5 +26,32 @@ public class Model {
             this.lottoList.add(new Lotto(numbers));
         }
     }
+    public void setWinningLotto(List<String> stringList) {
+        List<Integer> winNumbers = new ArrayList<>();
+        for (String item : stringList) {
+            winNumbers.add(Integer.parseInt(item));
+        }
+        this.winningLotto = new Lotto(winNumbers);
+    }
 
+    public void setBonus(int bonus) {
+        ExceptionController.validateDuplicateBonusWithWin(this.winningLotto, bonus);
+        this.bonus = bonus;
+    }
+
+    private int compareWithIndex(int index) {
+        int num = 0;
+        int bonus = 0;
+
+        List<Integer> temp = this.lottoList.get(index).getNumbers();
+        for (int i = 0; i < 6; i++) {
+            if (temp.contains(this.winningLotto.getNumbers().get(i))) {
+                num += 1;
+            }
+        }
+        if (temp.contains(this.bonus)) {
+            bonus += 1;
+        }
+        return num + bonus;
+    }
 }
