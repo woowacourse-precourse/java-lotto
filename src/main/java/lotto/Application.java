9c3d7f1a12fc26c3e4b08e61c58896cc9aa36bb0
CJiu01 <jiwoo020801@gmail.com>
2022-11-15 22:53:41 +0900

    [Feat] Application main 함수 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            LottoController controller = new LottoController(new InputView(), new ResultView());
+            controller.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
