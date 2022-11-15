a55a754bd6e69e72e1f2f346f266d3482b8b5e5b
coldbucks <met.hsw@gmail.com>
2022-11-15 19:52:44 +0900

    feat(Lotto): [ERROR] 로또 중복 숫자 예외발생 처리하기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,6 @@
 package lotto;
 
 
-import lotto.domain.LottoSystem;
-
 public class Application {
 
 
@@ -10,10 +8,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        LottoSystem lottoSystem = new LottoSystem();
-
-        lottoSystem.lottoAmount();
-
+        
     }
 }
 
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -10,14 +10,23 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-               this.numbers = numbers;
+        duplicateNumber(numbers);
+        this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
-            }
         }
-        // TODO: 추가 기능 구현
+    }
+
+    private boolean duplicateNumber(List<Integer> numbers) {
+        for(Integer correctNumber : numbers){
+            if (numbers.contains(correctNumber)) {
+                throw new IllegalArgumentException();
+                }
+            }
+        return true;
+        }
     }
 
