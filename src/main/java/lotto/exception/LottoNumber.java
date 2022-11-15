a9d2f0f53c9ecd59ae5a625466feb13f2be9b7d4
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-15 21:39:25 +0900

    Feat(Exception): Add LottoNumber ExceptionDuplication Method

--- src/main/java/lotto/exception/LottoNumber.java
+++ src/main/java/lotto/exception/LottoNumber.java
@@ -1,5 +1,19 @@
 package lotto.exception;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lotto.constants.Error;
+import lotto.constants.Number;
+
 public class LottoNumber {
+    public static void exceptionDuplication(List<Integer> lotto) {
+        Set<Integer> lottoSet = new HashSet<>(lotto);
+
+        if (lottoSet.size() != lotto.size()) {
+            throw new IllegalArgumentException(Error.DUPLICATE_LOTTOS_NUM.getMsg());
+        }
+    }
+
 
 }
