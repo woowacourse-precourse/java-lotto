e378667623e529d513257ae35afebf6bd1d5a8b4
ParkJoungWoo <jug78@naver.com>
2022-11-15 10:17:34 +0900

    refactor(user/NumberValidator.java) : NumberValidator 리팩토링
    NumberComponent를 NumberValidator로 이름을 수정했습니다.
    에러 메세지 클래스 변경점을 적용했습니다.
    
    - checkMoneyPositive를 삭제했습니다.

--- src/main/java/user/NumberValidator.java
+++ src/main/java/user/NumberValidator.java
@@ -2,14 +2,14 @@ package user;
 
 import camp.nextstep.edu.missionutils.Console;
 import ui.View;
-import ui.Constants;
+import ui.ErrorConstant;
 
-public class NumberComponent {
+public class NumberValidator {
 
     public static int makeMoney() {
         View.promptSeedMoney();
         int money = inputToInteger();
-        checkMoney(money);
+        checkMoneyThousand(money);
         return money;
     }
 
@@ -30,7 +30,7 @@ public class NumberComponent {
 
     public static void checkInputNull(String input) {
         if (input == null) {
-            throw new IllegalArgumentException(Constants.NOT_NULL_ERROR);
+            throw new IllegalArgumentException(ErrorConstant.NOT_NULL_ERROR);
         }
     }
 
@@ -38,37 +38,26 @@ public class NumberComponent {
     public static void checkComponent(String input) {
         for (int i = 0; i < input.length(); i++) {
             if (input.charAt(i) < '0' || input.charAt(i) > '9') {
-                throw new IllegalArgumentException(Constants.NOT_NUMBER_COMPONENT);
+                throw new IllegalArgumentException(ErrorConstant.NOT_NUMBER_COMPONENT);
             }
         }
     }
 
     public static void checkFirstNumberZero(String input) {
         if (input.charAt(0) == '0') {
-            throw new IllegalArgumentException(Constants.NOT_FIRST_NUMBER_ZERO);
-        }
-    }
-
-    public static void checkMoney(int money) {
-//        checkMoneyPositive(money);
-        checkMoneyThousand(money);
-    }
-
-    public static void checkMoneyPositive(int money) {
-        if (money < 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorConstant.NOT_FIRST_NUMBER_ZERO);
         }
     }
 
     public static void checkMoneyThousand(int money) {
         if (money % 1000 != 0) {
-            throw new IllegalArgumentException(Constants.MONEY_NOT_DIVIDE_1000);
+            throw new IllegalArgumentException(ErrorConstant.MONEY_NOT_DIVIDE_1000);
         }
     }
 
     public static void checkLottoNumber(int lottoNumber) {
         if (lottoNumber < 1 || lottoNumber > 45) {
-            throw new IllegalArgumentException(Constants.LOTTO_NOT_NUMBER_RANGE);
+            throw new IllegalArgumentException(ErrorConstant.LOTTO_NOT_NUMBER_RANGE);
         }
     }
 }
