2e057828a820ad2639c2938d82cc16431c90edf1
GwansuShin <happysun7080@naver.com>
2022-11-15 23:06:35 +0900

    test(Lotto): 로또 번호 및 보너스 번호 예외 처리
    요구 사항에 따른 예외 테스트케이스 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,6 +1,5 @@
-package lotto;
+package lotto.domain;
 
-import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +19,20 @@ class LottoTest {
     @Test
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호에 이미 존재하면 예외가 발생한다.")
+    @Test
+    void generateBonusByDuplicatedDrawNumber() {
+        assertThatThrownBy(() -> Lotto.generate(Lotto.generate("1,2,3,4,5,6"), "1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 1이상 45이하 범위에 속하지 않으면 예외가 발생한다.")
+    @Test
+    void generateBonusByOverRange() {
+        assertThatThrownBy(() -> Lotto.generate(Lotto.generate("1,2,3,4,5,6"), "46"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
