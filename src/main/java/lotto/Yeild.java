e19d84cca7ec54dec3e54d32e562af12ca0275f3
syahn <496300@naver.com>
2022-11-15 10:51:12 +0900

    refactor: 수익률 계산 코드 수정

--- src/main/java/lotto/Yeild.java
+++ src/main/java/lotto/Yeild.java
@@ -11,13 +11,14 @@ public class Yeild {
     public Yeild(int money, List<Integer> prizeCounts) {
         this.money = money;
         this.prizeCounts = prizeCounts;
+        this.prizeMoney = 0;
     }
 
     void createStatistic() {
         for (int i = 0; i < prizeCounts.size(); i++) {
             prizeMoney += Prize.values()[i].getMoney() * prizeCounts.get(i);
         }
-        yeild = (Math.round(((double) prizeMoney / money) * 100 * 100));
+        yeild = Math.round(((double) prizeMoney / money)* 100*100)/100.0;
     }
 
     public double getYeild() {
