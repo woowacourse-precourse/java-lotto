0821c7778fabefdfb433f4ee9ef542962d41ad24
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-15 22:53:23 +0900

    feat: printBuyingLottoQuantityMessage() 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,6 +9,11 @@ public class OutputView {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
+    public void printBuyingLottoQuantityMessage(User user) {
+        System.out.println();
+        System.out.println((user.getPrice() / 1000) + "개를 구매했습니다.");
+    }
+
 
 
 
