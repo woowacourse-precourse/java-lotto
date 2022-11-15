b8dd172fa3fd5002a69d90e766d1b5bd2865c25c
namtndus <rnrmf0721@gmail.com>
2022-11-15 22:43:24 +0900

    feat: 프로그램 구현을 위한 함수 호출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.Controller.InputOutputController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputOutputController inputOutputController = new InputOutputController();
+        inputOutputController.inputMoney();
+        inputOutputController.outputLottoSize();
+        inputOutputController.inputWinningNumbers();
+        inputOutputController.inputBonusNumber();
+        inputOutputController.checkLottoNumbers();
+        inputOutputController.calculatedYield();
     }
 }
