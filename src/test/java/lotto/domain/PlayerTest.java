04194658b0a38f98db63b3436ce0124a09a65845
CAEL0 <kjongmin26@gmail.com>
2022-11-15 21:30:13 +0900

    test(PlayerTest): 구입금액이 0으로 시작하면 예외 발생

--- src/test/java/lotto/domain/PlayerTest.java
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static lotto.constant.ExceptionConstants.LEADING_ZERO_EXCEPTION;
 import static lotto.constant.ExceptionConstants.NOT_NATURAL_NUMBER_EXCEPTION;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,6 +18,15 @@ public class PlayerTest extends NsTest {
         assertSimpleTest(() -> {
             runException("a");
             assertThat(output()).contains(NOT_NATURAL_NUMBER_EXCEPTION.toString());
+        });
+    }
+
+    @DisplayName("구입금액이 0으로 시작하면 예외가 발생한다.")
+    @Test
+    void ThrowExceptionIfPurchaseAmountWithLeadingZero() {
+        assertSimpleTest(() -> {
+            runException("01000");
+            assertThat(output()).contains(LEADING_ZERO_EXCEPTION.toString());
         });
     }
 
