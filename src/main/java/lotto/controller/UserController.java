ffd833b078435729473bebaf8f094c612e25678e
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-15 22:02:48 +0900

    Feat(Controller): Add UserController getPurchaseAmount function

--- src/main/java/lotto/controller/UserController.java
+++ src/main/java/lotto/controller/UserController.java
@@ -1,5 +1,21 @@
 package lotto.controller;
 
-public class UserController {
+import lotto.exception.PurchaseAmount;
+import lotto.view.Input;
+import lotto.view.Output;
 
+public class UserController {
+    public static int getPurchaseAmount() {
+        Output.printStartMsg();
+        String input = Input.inputPurchaseAmount();
+        PurchaseAmount.exceptionPurchaseAmount(input);
+        int purchaseAmount = toStringPurchaseAmount(input);
+
+        return purchaseAmount;
+    }
+
+    public static int toStringPurchaseAmount(String input) {
+        int purchaseAmount = Integer.valueOf(input);
+        return purchaseAmount;
+    }
 }
