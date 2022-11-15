94ac89c62944ed2e00dfc609eac91aa3860ae7e7
이승훈 <qwqw8019@naver.com>
2022-11-15 22:04:40 +0900

    feat: 당첨 내역을 출력하는 기능 구현
    printWinnings()
    checkWinnings()
    computeWinningIndex()
    generateWinningCount()
    printYield() 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,32 +9,31 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
         int money = inputMoney();
-        Lotto lotto = inputLottoNumber();
-        int bonus = inputBonusNumber(lotto);
+        Lotto winningLotto = inputLottoNumber();
+        int bonus = inputBonusNumber(winningLotto);
+        List<Integer> winningInfo;
 
         List<Lotto> issuedLotto = issueLotto(money);
         printIssuedLotto(issuedLotto);
+
+        winningInfo = checkWinnings(issuedLotto, winningLotto, bonus);
+        printWinnings(winningInfo);
+        printYield(winningInfo, money);
+
     }
 
 
     public static int inputMoney() {
         String inputMoneyString = Console.readLine();
-        int money;
-        boolean inputMoneyError = false;
+        int money = 0;
 
         try {
             money = checkInput(inputMoneyString);
         } catch (Exception e) {
-            inputMoneyError = true;
-            throw new IllegalArgumentException("[ERROR] 잘못된 입력이 들어왔습니다.");
-//            System.out.println("[ERROR] 잘못된 입력이 들어왔습니다.");
-        } finally {
-            if (inputMoneyError) {
-                String errorMessage = "[ERROR] 잘못된 입력이 들어왔습니다.";
-                System.out.println(errorMessage);
-            }
+
+            System.out.println("[ERROR] 잘못된 입력이 들어왔습니다.");
         }
 
         return money;
@@ -113,7 +112,6 @@ public class Application {
 
         for (int i = 0; i < numberOfLotto; i++) {
             List<Integer> newLottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            newLottoNumber.sort(Comparator.naturalOrder());
             Lotto newLotto = new Lotto(newLottoNumber);
             issuedLotto.add(newLotto);
         }
@@ -124,6 +122,77 @@ public class Application {
         for (Lotto lotto : lottoList) {
             lotto.printLotto();
         }
+    }
+
+    public static void printWinnings(List<Integer> winningInfo) {
+        System.out.println("3개 일치 (5,000원) - " + winningInfo.get(0) + "개");
+        System.out.println("4개 일치 (50,000원) - " + winningInfo.get(1) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + winningInfo.get(2) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + winningInfo.get(3) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + winningInfo.get(4) + "개");
+
+    }
+
+    public static List<Integer> checkWinnings(List<Lotto> issuedLotto, Lotto lotto, int bonus) {
+        List<Integer> winningInfo = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            winningInfo.add(0);
+        }
+        for (Lotto currentLotto : issuedLotto) {
+            int winningIndex;
+            winningIndex = computeWinningIndex(currentLotto, lotto, bonus);
+            if (winningIndex != -1) {
+                int currentCount = winningInfo.get(winningIndex);
+                winningInfo.set(winningIndex, currentCount + 1);
+            }
+        }
+
+
+        return winningInfo;
+    }
+
+    private static int computeWinningIndex(Lotto currentLotto, Lotto lotto, int bonus) {
+
+        int winningCount = 0;
+        boolean bonusFlag = false;
+        for (int lottoNumber : currentLotto.getNumbers()) {
+            if (lotto.getNumbers().contains(lottoNumber)) {
+                winningCount++;
+            }
+            if (lottoNumber == bonus) {
+                bonusFlag = true;
+            }
+        }
+        return generateWinningCount(winningCount, bonusFlag);
+    }
+
+    public static int generateWinningCount(int winningCount, boolean bonusFlag) {
+        if (winningCount == 3) {
+            return 0;
+        }
+        if (winningCount == 4) {
+            return 1;
+        }
+        if (winningCount == 5 && !bonusFlag) {
+            return 2;
+        }
+        if (winningCount == 5) {
+            return 3;
+        }
+        if (winningCount == 6) {
+            return 4;
+        }
+        return -1;
+    }
+
+    public static void printYield(List<Integer> winningInfo, int money) {
+        float profit;
+        profit = winningInfo.get(0)*5000;
+        profit += winningInfo.get(1)*50000;
+        profit += winningInfo.get(2)*1500000;
+        profit += winningInfo.get(3)*30000000;
+        profit += winningInfo.get(4)*2000000000;
+        System.out.println("총 수익률은 "+ (profit/money)*100 + "%입니다.");
     }
 
 }
