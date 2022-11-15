16593dfc07c68bcbdccdc7f79d393751962c00b8
dvlpr0 <rladjswkd@naver.com>
2022-11-15 19:30:35 +0900

    fix(BoughtTicketsDTO): tickets 변수가 초기화 되지 않던 문제 수정

--- src/main/java/lotto/BoughtTicketsDTO.java
+++ src/main/java/lotto/BoughtTicketsDTO.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BoughtTicketsDTO   {
@@ -7,6 +8,7 @@ public class BoughtTicketsDTO   {
     private List<Lotto> tickets;
 
     public BoughtTicketsDTO(List<Lotto> boughtTickets) {
+        tickets = new ArrayList<>();
         tickets.addAll(boughtTickets);
     }
 
