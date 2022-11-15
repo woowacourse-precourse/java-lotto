0ec48261c704642c6c3632c92745914e84efbb19
kimdonghun <zhfptm12@naver.com>
2022-11-15 15:08:31 +0900

    feat : 로또 번호 리스트생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,6 +4,7 @@ import lotto.model.Money;
 import lotto.view.*;
 import lotto.Lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoController {
@@ -11,6 +12,7 @@ public class LottoController {
     private InputView inputView;
     private OutputView outputView;
     private Lotto lotto;
+    private List<Lotto> lottoList;
     // 인스턴스 접근과, 임포트 접근 후 함수 호출 방식의 차이점은?
     public LottoController() {
     }
@@ -18,7 +20,7 @@ public class LottoController {
         money = new Money();
         inputView = new InputView();
         outputView = new OutputView();
-
+        lottoList = new ArrayList<>();
     }
     public void run() {
         init();
@@ -26,7 +28,7 @@ public class LottoController {
 //        InputView.purchaseLotto();
         int purchaseMoney = Integer.valueOf(camp.nextstep.edu.missionutils.Console.readLine());
         saveMoney(purchaseMoney);
-        outputView.lottoNumber(money.getMoney(), lotto);
+        outputView.lottoNumber(calculateLottoCount(money.getMoney()), lottoList);
 
 
 
@@ -34,8 +36,15 @@ public class LottoController {
     public void saveMoney(int purchaseMoney) {
         money.setMoney(purchaseMoney);
     }
-    public List<Integer> makeLottoNumbers() {
+    public int calculateLottoCount(int purchaseMoney) {
+        return purchaseMoney % 1000;
+    }
+    public List<Integer> makeLottoNumbers(List<Lotto> lottoList, int purchaseMoney) {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        int count = calculateLottoCount(purchaseMoney);
+        for (int i = 0; i < count; i++) {
+            lottoList.add(new Lotto(numbers));
+        }
         return numbers;
     }
 
