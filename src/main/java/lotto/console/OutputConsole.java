fa29993a0889f01539dcd958e9138c800a4e42f8
sujin0607 <goweg0607@gmail.com>
2022-11-15 19:01:21 +0900

    feat: 당첨 내역 출력 메서드 추가

--- src/main/java/lotto/console/OutputConsole.java
+++ src/main/java/lotto/console/OutputConsole.java
@@ -1,18 +1,29 @@
 package lotto.console;
 
+import lotto.domain.Rank;
+
 import java.util.List;
 
 /***
  * 출력 클래스
  */
 public class OutputConsole {
-    public void lottoCount(int lottoCount){
-        System.out.printf("%d개를 구매했습니다.\n",lottoCount);
+    public void lottoCount(int lottoCount) {
+        System.out.printf("%d개를 구매했습니다.\n", lottoCount);
     }
 
-    public void randomNumbers(List<List<Integer>> numbers){
+    public void randomNumbers(List<List<Integer>> numbers) {
         for (List<Integer> number : numbers) {
             System.out.println(number);
+        }
+    }
+
+    public void lottoRank(List<Integer> lottoResult) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (int i = 0; i < lottoResult.size(); i++) {
+            Rank rank = Rank.valueOf(i);
+            System.out.printf("%s - %d개\n", rank.getMessage(), lottoResult.get(i));
         }
     }
 
