d0e4520e9e00f855217a1dc3222b123921aba347
LisaJeoung <jlisa9912234567890@gmail.com>
2022-11-15 19:25:57 +0900

    refactor : 매게변수 및 반환값 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,28 +23,32 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
-    public void getPurchaseAmount() {
+    public static int getPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         int price = Integer.parseInt(Console.readLine());
         if (price % 1000 != 0)
             throw new IllegalArgumentException("[ERROR] 구입금액은 1,000원 단위로 입력해야 합니다.");
+        return price/1000;
     }
 
-    public void getLuckyNumber() {
+    public static List<Integer> getLuckyNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String[] n = Console.readLine().split(",");
         List<Integer> numbers = new ArrayList<>();
         for (String s : n)
             numbers.add(Integer.valueOf(s));
-        validate(numbers);
+        return numbers;
+    }
 
+    public int getBonusNumber(){
         System.out.println("보너스 번호를 입력해 주세요.");
         int bonusNumber = Integer.parseInt(Console.readLine());
         if (numbers.contains(bonusNumber))
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        return bonusNumber;
     }
 
-    public List<Integer>[] createNumbers(int count) {
+    public static List<Integer>[] createNumbers(int count) {
         List<Integer>[] allNumbers = new List[count];
         for (int i = 0; i < count; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
@@ -53,17 +58,17 @@ public class Lotto {
         return allNumbers;
     }
 
-    public void printNumbers(List<Integer>[] allNumbers) {
+    public static void printNumbers(List<Integer>[] allNumbers) {
         System.out.println(allNumbers.length + "개를 구매했습니다.");
         for (List allNumber : allNumbers)
             System.out.println(allNumber);
     }
 
-    public void getResult(List<Integer>[] customerNumbers, List<Integer> LuckyNumbers, int bonusNumber) {
+    public void getResult(List<Integer>[] customerNumbers, int bonusNumber) {
         for (List<Integer> num : customerNumbers) {
             int matchCount = 0, bonus = 0;
             for (int n : num) {
-                if (LuckyNumbers.contains(n))
+                if (numbers.contains(n))
                     matchCount++;
                 if (bonusNumber == n)
                     bonus = 1;
