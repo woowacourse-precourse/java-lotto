df43fc81e3b8c4e274527380484e541dd738420f
고동우 <shp05179@gmail.com>
2022-11-15 14:07:23 +0900

    feat: 당첨 통계 출력기능
    일치 갯수 별로 해당되는 숫자와 수익률을 담은 리스트를 받아서 순서대로 출력한다.

--- src/main/java/lotto/LottoConsoleIo.java
+++ src/main/java/lotto/LottoConsoleIo.java
@@ -22,4 +22,14 @@ public class LottoConsoleIo {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
+    static void printResultStatistics(List result) {
+        System.out.println("당첨 통계\n---");
+        System.out.println("3개 일치 (5,000원) - "+result.get(0)+"개\n" +
+                            "4개 일치 (50,000원) - "+result.get(1)+"개\n" +
+                            "5개 일치 (1,500,000원) - "+result.get(2)+"개\n" +
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - "+result.get(3)+"개\n" +
+                            "6개 일치 (2,000,000,000원) - "+result.get(4)+"개\n" +
+                            "총 수익률은 "+result.get(5)+"%입니다.");
+    }
+
 }
