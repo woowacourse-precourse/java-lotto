5c5187bbd6ab4df0b24c7f2063f5324ba04eef83
limecats0331 <limecats0331@gmail.com>
2022-11-15 10:16:03 +0900

    feat(LottoIO): 사용자한테 숫자 입력받는 메소드 추가

---
+++ src/main/java/lotto/LottoIO.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoIO {
+    static public List<Integer> getLottoNumber() {
+        String[] input = Console.readLine().trim().split(",");
+
+        return Arrays.stream(input)
+                .peek(eachNumber -> validateIsNumber(eachNumber))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    private static void validateIsNumber(String each) {
+        try {
+            Integer.parseInt(each);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("[ERROR] 입력이 숫자가 아닙니다.");
+        }
+    }
+}
