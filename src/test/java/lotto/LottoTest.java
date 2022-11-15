aaf2a2f1fa378d4bca797937bc44a3aedb97148b
namtndus <rnrmf0721@gmail.com>
2022-11-15 18:02:41 +0900

    test(LottoTest): checkWinningNumberResult함수 구현
    로또 번호에 당첨 번호가 몇개 포함되어있는지와 각각의 로또의 등수 갯수를 리턴해주는 함수를 테스트하는 함수 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import lotto.Dto.Lotto;
 import lotto.Dto.Money;
 import lotto.Services.CalculatedService;
@@ -8,9 +9,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class LottoTest {
+class LottoTest extends NsTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -46,5 +49,44 @@ class LottoTest {
     void creatWinningNumbers() {
         assertThatThrownBy(() -> new Lotto("1, 2, 3,4,5,6"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨된 로또의 갯수가 반환하는 함수를 테스트")
+    @Test
+    void checkWinningNumberResult() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("8000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "8개를 구매했습니다.",
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[1, 3, 5, 14, 22, 45]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[13, 14, 16, 38, 42, 45]",
+                            "[1, 2, 3, 4, 5, 7]",
+                            "[2, 13, 22, 32, 38, 45]",
+                            "[1, 2, 3, 4, 5, 7]",
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 2개",
+                            "6개 일치 (2,000,000,000원) - 0개"
+                    );
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(1, 3, 5, 14, 22, 45),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(1, 2, 3, 4, 5, 7),
+                List.of(2, 13, 22, 32, 38, 45),
+                List.of(1, 2, 3, 4, 5, 7)
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
     }
 }
