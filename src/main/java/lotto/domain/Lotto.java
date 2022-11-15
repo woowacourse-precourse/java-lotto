2358661dea2d0e6bb4894c9ceb872facb539fd52
LEE-sh1673 <lsh901673@gmail.com>
2022-11-15 14:39:36 +0900

    feat(lotto): 로또 번호에 대한 중복 검사 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,17 +1,28 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+
+        if (nonDuplicateNumbers.size() != numbers.size()) {
             throw new IllegalArgumentException();
         }
     }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -2,54 +2,65 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.List;
-
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
-@DisplayNameGeneration(
-	DisplayNameGenerator.ReplaceUnderscores.class
-)
-public class LottoTest {
+import java.util.List;
 
-	static Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+import lotto.domain.Lotto;
 
-	@DisplayName("당첨번호와 로또번호를 비교하여 몇 개가 일치하는지 알 수 있다.")
-	@Test
-	void getMatchingNumbers_당첨번호와_로또번호_일치하는_개수_반환() {
-		assertThat(
-			lotto.getMatchingNumbers(List.of(1, 10, 11, 12, 13, 14))
-		).isEqualTo(1);
-		assertThat(
-			lotto.getMatchingNumbers(List.of(1, 10, 6, 5, 3, 14))
-		).isEqualTo(4);
-		assertThat(
-			lotto.getMatchingNumbers(List.of(11, 12, 13, 14, 15, 16))
-		).isEqualTo(0);
-	}
-	
-	@DisplayName("보너스와 로또번호를 비교하여 보너스번호가 포함되어 있는지 알 수 있다.")
-	@Test
-	void containsBonus_보너스번호가_로또번호에_포함되어_있는지_알수있다() {
-		assertThat(lotto.containsBonus(10)).isFalse();
-		assertThat(lotto.containsBonus(1)).isTrue();
-	}
+class LottoTest {
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
-	@DisplayName("로또번호와 당첨번호 및 보너스 번호와 비교하여 당첨 내역을 알 수 있다.")
-	@Test
-	void compareTo_당첨번호_와_보너스번호와_비교하여_당첨내역을_구할수있다() {
-		WinningRule rule = WinningRule.of(
-			lotto.compareTo(List.of(1, 2, 3, 4, 5, 6), 7)
-		);
-		assertThat(rule.getStatus()).isEqualTo("6개 일치");
-		assertThat(rule.getPrice()).isEqualTo(2_000_000_000);
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
-		rule = WinningRule.of(
-			lotto.compareTo(List.of(1, 3, 5, 14, 22, 45), 7)
-		);
-		assertThat(rule.getStatus()).isEqualTo("3개 일치");
-		assertThat(rule.getPrice()).isEqualTo(5_000);
-	}
+    // 아래에 추가 테스트 작성 가능
+    static Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+    @DisplayName("당첨번호와 로또번호를 비교하여 몇 개가 일치하는지 알 수 있다.")
+    @Test
+    void getMatchingNumbers_당첨번호와_로또번호_일치하는_개수_반환() {
+        assertThat(
+            lotto.getMatchingNumbers(List.of(1, 10, 11, 12, 13, 14))
+        ).isEqualTo(1);
+        assertThat(
+            lotto.getMatchingNumbers(List.of(1, 10, 6, 5, 3, 14))
+        ).isEqualTo(4);
+        assertThat(
+            lotto.getMatchingNumbers(List.of(11, 12, 13, 14, 15, 16))
+        ).isEqualTo(0);
+    }
+
+    @DisplayName("보너스와 로또번호를 비교하여 보너스번호가 포함되어 있는지 알 수 있다.")
+    @Test
+    void containsBonus_보너스번호가_로또번호에_포함되어_있는지_알수있다() {
+        assertThat(lotto.containsBonus(10)).isFalse();
+        assertThat(lotto.containsBonus(1)).isTrue();
+    }
+
+    @DisplayName("로또번호와 당첨번호 및 보너스 번호와 비교하여 당첨 내역을 알 수 있다.")
+    @Test
+    void compareTo_당첨번호_와_보너스번호와_비교하여_당첨내역을_구할수있다() {
+        WinningRule rule = WinningRule.of(
+            lotto.compareTo(List.of(1, 2, 3, 4, 5, 6), 7)
+        );
+        assertThat(rule.getStatus()).isEqualTo("6개 일치");
+        assertThat(rule.getPrice()).isEqualTo(2_000_000_000);
+
+        rule = WinningRule.of(
+            lotto.compareTo(List.of(1, 3, 5, 14, 22, 45), 7)
+        );
+        assertThat(rule.getStatus()).isEqualTo("3개 일치");
+        assertThat(rule.getPrice()).isEqualTo(5_000);
+    }
 }
