6235473f248fef8e743f17c1a2004d70366f497d
jijongkwon <help3451@naver.com>
2022-11-15 17:54:23 +0900

    test : 당첨번호 개수에 따른 금액확인 테스트 추가

--- src/test/java/lotto/domain/ComparingMachineTest.java
+++ src/test/java/lotto/domain/ComparingMachineTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
@@ -26,5 +27,19 @@ class ComparingMachineTest {
         comparingMachine.countContainBonusNumber(7,
                 List.of(List.of(1,2,3,4,5,6),List.of(1,5,6,7,8,9)));
         assertThat(comparingMachine.getContainBonusNumber()).isEqualTo(List.of(0,1));
+    }
+
+
+    @DisplayName("당첨번호 개수에 따른 금액을 확인한다.")
+    @Test
+    void countWinResult() {
+        ComparingMachine comparingMachine = new ComparingMachine();
+        comparingMachine.countContainNumber(List.of(1,2,3,4,5,6),
+                List.of(List.of(1,2,3,6,7,5), List.of(12,13,14,15,16,17)));
+        comparingMachine.countContainBonusNumber(7,
+                List.of(List.of(1,2,3,6,7,5), List.of(12,13,14,15,16,17)));
+        comparingMachine.countWinResult();
+        assertThat(comparingMachine.getResult()).isEqualTo(Map.of(
+                5000,0,50000,0,1500000,0,30000000,1,2000000000,0));
     }
 }
