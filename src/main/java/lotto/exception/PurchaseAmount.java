f0c77ee19b71289ee27575e4f04a3128339ca960
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-15 21:36:26 +0900

    Feat(Exception): Add PurchaseAmount ExceptionLessThanPrice Method

--- src/main/java/lotto/exception/PurchaseAmount.java
+++ src/main/java/lotto/exception/PurchaseAmount.java
@@ -1,5 +1,14 @@
 package lotto.exception;
 
+import lotto.constants.Error;
+import lotto.constants.Number;
+
 public class PurchaseAmount {
+    public static void exceptinoLessThanPrice(String purchaseAmount) {
+        if (Integer.parseInt(purchaseAmount) < Number.PRICE.getNumber()) {
+            throw new IllegalArgumentException(Error.LESS_THAN_LOTTO_PRICE.getMsg());
+        }
+    }
+
 
 }
