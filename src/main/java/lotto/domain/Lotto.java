f475bf930aea82bc33a2733ca2993205b6d57192
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-15 22:27:15 +0900

    feat(domain) : Lotto 클래스의 변수 및 객체 생성

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.exception.LuckyLottoNumberException;
+
+public class Lotto {
+    private List<Integer> numbers;
+    private int bonusNumber;
+
+    private final LuckyLottoNumberException luckyLottoNumberException = new LuckyLottoNumberException();
+
+
+
+    // TODO: 추가 기능 구현
+}
