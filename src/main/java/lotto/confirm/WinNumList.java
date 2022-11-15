3542e9e619b70b5c7eaf072db0846e6d60214e88
kswhj <108874006+kswhj@users.noreply.github.com>
2022-11-15 17:51:57 +0900

    Update WinNumList.java
    리스트 수정

--- src/main/java/lotto/confirm/WinNumList.java
+++ src/main/java/lotto/confirm/WinNumList.java
@@ -7,7 +7,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class WinNumList {
 	public static List<Integer> run() {
-		List<Integer> answer = Arrays.asList(0, 0, 0, 0, 0, 0, 0);
+		List<Integer> answer = Arrays.asList(0, 0, 0, 0, 0, 0);
 		String[] numList = Console.readLine().split(",");
 		for (int i = 0; i < 6; i++) {
 			answer.set(i, Integer.parseInt(numList[i]));
