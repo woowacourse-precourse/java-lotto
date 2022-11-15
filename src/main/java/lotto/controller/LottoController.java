c4d30cd8f8cf100ecb671a89e962432434102977
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-15 15:55:08 +0900

    style: 메소드 오탈자 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,15 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
 import lotto.domain.LottoRank;
 import lotto.domain.WinningNumbers;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import lotto.view.InputView;
-import lotto.view.OutputView;
 
 public class LottoController {
     private static final int PERCENTAGE_CONVERTER = 100;
@@ -22,6 +22,14 @@ public class LottoController {
 
     private Map<LottoRank, Integer> lottoResult = new HashMap<>();
 
+    public static double calculateLottoEarningRate(int earningMoney, int purchaseMoney) {
+        return (double) earningMoney / purchaseMoney * PERCENTAGE_CONVERTER;
+    }
+
+    private int generateTickets(int inputMoney) {
+        return inputMoney / LOTTO_ONE_GAME_PRICE;
+    }
+
     public void run() {
         int inputLottoMoney = InputView.inputLottoPurchaseMoney();
         int lottoTickets = generateTickets(inputLottoMoney);
@@ -30,23 +38,12 @@ public class LottoController {
 
         WinningNumbers winningNumbers = new WinningNumbers(winningNumber, bonusNumber);
         OutputView.printLottoPurchaseCompleteMessage(lottoTickets);
-        calculrateLotto(lottoTickets,winningNumbers);
-        int lottoEarningMoney = calculrateLottoEarningMoney();
-        double lottoEarningRate = calculrateLottoEarningRate(lottoEarningMoney, inputLottoMoney);
+        calculateLotto(lottoTickets, winningNumbers);
+        int lottoEarningMoney = calculateLottoEarningMoney();
+        double lottoEarningRate = calculateLottoEarningRate(lottoEarningMoney, inputLottoMoney);
         OutputView.printLottoEarningRate(lottoEarningRate);
     }
 
-    private int generateTickets(int inputMoney) {
-        return inputMoney / LOTTO_ONE_GAME_PRICE;
-    }
-
-    private void calculrateLotto(int lottoTickets, WinningNumbers winningNumbers) {
-        initializeLottoResultMap();
-        for (int i = 0; i < lottoTickets; i++) {
-            List<Integer> lotto = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_SIZE);
-            calculrateResult(winningNumbers, lotto);
-        }
-    }
 
     private void initializeLottoResultMap() {
         for (LottoRank lottoRank : LottoRank.values()) {
@@ -54,13 +51,22 @@ public class LottoController {
         }
     }
 
-    private void calculrateResult(WinningNumbers winningNumbers, List<Integer> lotto) {
-        LottoRank lottoRank = winningNumbers.calculrateRank(lotto);
+    private void calculateLotto(int lottoTickets, WinningNumbers winningNumbers) {
+        initializeLottoResultMap();
+        for (int i = 0; i < lottoTickets; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_SIZE);
+            Lotto lotto = new Lotto(numbers);
+            calculateResult(winningNumbers, lotto);
+        }
+    }
+
+    private void calculateResult(WinningNumbers winningNumbers, Lotto lotto) {
+        LottoRank lottoRank = winningNumbers.calculateRank(lotto);
         lottoResult.put(lottoRank, lottoResult.getOrDefault(lottoRank, 0) + 1);
         OutputView.printLottoNumber(lotto);
     }
 
-    private int calculrateLottoEarningMoney() {
+    private int calculateLottoEarningMoney() {
         int earningMoney = DEFAULT_EARNING_MONEY;
         for (LottoRank lottoRank : lottoResult.keySet()) {
             if (lottoResult.get(lottoRank) >= 1) {
@@ -69,10 +75,6 @@ public class LottoController {
         }
         OutputView.printLottoResult(lottoResult);
         return earningMoney;
-    }
-
-    public static double calculrateLottoEarningRate(int earningMoney, int purchaseMoney) {
-        return (double) earningMoney / purchaseMoney * PERCENTAGE_CONVERTER;
     }
 
 }
