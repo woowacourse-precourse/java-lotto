610da445b9e19d2f2c30eda0450da2e5426fe3d3
김선동 <mok03127@gmail.com>
2022-11-15 17:14:41 +0900

    feat: 당첨 번호와 보너스 번호 등록하는 기능 컨트롤러에 추가

--- src/main/java/lotto/LottoGameController.java
+++ src/main/java/lotto/LottoGameController.java
@@ -1,11 +1,14 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoGameController {
     private final LottoGenerator lottoGenerator = new LottoGenerator();
 
     public void run() {
         LottoAmount amount = inputMoney();
         Lottos lottos = purchaseLotto(amount.makeLottoTickets());
+        WinningLotto winningLotto = makeWinningLotto();
 
     }
 
@@ -18,5 +21,13 @@ public class LottoGameController {
         Lottos lottos = lottoGenerator.makeLottos(lottoTickets);
         SystemMessage.purchaseLotto(lottoTickets, lottos);
         return lottos;
+    }
+
+    private WinningLotto makeWinningLotto() {
+        SystemMessage.winningNumber();
+        List<Integer> winningNumber = Input.winningNumber();
+        SystemMessage.bonusNumber();
+        int bonus = Input.getNumber();
+        return lottoGenerator.makeLotto(winningNumber, bonus);
     }
 }
