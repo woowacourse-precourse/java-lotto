efe8a1952c478f9562ee8754b16c8d39023f6bc2
권민정 <22alswjd@naver.com>
2022-11-15 17:02:40 +0900

    feat: 로또 중복 번호 발행 시 예외를 발생시킴

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,12 +1,15 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicatedNumbers(numbers);
         this.numbers = numbers;
     }
 
@@ -17,6 +20,12 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicatedNumbers(List<Integer> numbers) {
+        Set<Integer> nonDuplicatedNumbers = new HashSet<>(numbers);
+        if (nonDuplicatedNumbers.size() != 6) {
+            throw new IllegalArgumentException("중복된 번호가 발행되었습니다.");
+        }
+    }
     public List<Integer> getLottoNumbers() {
         return numbers;
     }
