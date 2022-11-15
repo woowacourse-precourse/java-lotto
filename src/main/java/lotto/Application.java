cb1f88b823224da49d0407783820c15b198aefc6
HamSeungChan <head0618@naver.com>
2022-11-15 16:22:56 +0900

    feat : 어플리케이션 실행 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,29 +1,10 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
+import lotto.management.LottoManagementController;
 
 public class Application {
-
-  static final int LOTTO_AMOUNT = 1000;
-
   public static void main(String[] args) {
-    // TODO: 프로그램 구현
-  }
-
-  private static int enterPurchaseAmount() {
-    System.out.println("구매금액을 입력해 주세요.");
-    String purchaseAmount = Console.readLine();
-    return Integer.parseInt(purchaseAmount);
-  }
-
-  private static int getNumberOfBuyLotto(int purchaseAmount) {
-    valid(purchaseAmount);
-    return purchaseAmount / LOTTO_AMOUNT;
-  }
-
-  private static void valid(int purchaseAmount){
-    if(purchaseAmount%LOTTO_AMOUNT>0){
-      throw new IllegalArgumentException("[ERROR] 구매 금액이 1000원 단위여야 합니다.");
-    }
+    LottoManagementController m = new LottoManagementController();
+    m.run();
   }
 }
