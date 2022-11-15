213754ce859a45057fbe47aafe1d9aa3b164402a
1eejisoo <jisu991012@gmail.com>
2022-11-15 17:54:11 +0900

    refactor: 통계 작업 기능 분리 & 메서드명, 순서  수정

--- src/main/java/domain/RankInfo/RankInfoService.java
+++ src/main/java/domain/RankInfo/RankInfoService.java
@@ -8,20 +8,49 @@ import domain.UserLotto.UserLotto;
 import java.util.List;
 
 public class RankInfoService {
-    private final static int CHECK_COUNT = 5;
+    private final static int CHECK_POINT = 5;
     private final static Double PERCENTAGE = 100.0;
 
     public RankInfo createRankInfo() {
         return new RankInfo();
     }
-
     public RankInfo putRankInfo(RankInfo rankInfo, Lotto lotto, UserLotto userLotto, Bonus bonus) {
+        List<Integer> userLottoNumbers;
         for (int index = 0; index < userLotto.getUserLottoSize(); index++) {
-            compare(rankInfo, lotto, userLotto.getOneOfUserLotto(index), bonus);
+            userLottoNumbers =  userLotto.getOneOfUserLotto(index);
+            checkCondition(calcMatchPoint(lotto,userLottoNumbers), rankInfo, userLottoNumbers, bonus);
         }
         return rankInfo;
     }
+    public void checkCondition(int matchPoint, RankInfo rankInfo, List<Integer> userLottoNumbers, Bonus bonus) {
+        if (matchPoint != CHECK_POINT)
+            updateMatchPoint(matchPoint, rankInfo, userLottoNumbers, bonus);
+        if (matchPoint == CHECK_POINT)
+            updateMatchPointWithCondition(matchPoint, rankInfo, userLottoNumbers, bonus);
+    }
+    public int  calcMatchPoint(Lotto lotto, List<Integer> userLottoNumbers) {
+        int matchPoint = 0;
 
+        for (int index = 0; index < lotto.getLottoSize(); index++) {
+            if (userLottoNumbers.contains(lotto.getLottoNumber(index))) matchPoint += 1;
+        }
+        return matchPoint;
+    }
+    public void updateMatchPoint(int matchPoint, RankInfo rankInfo, List<Integer> userLottoNumbers, Bonus bonus) {
+        for (Rank rank : Rank.values()) {
+            if (rank.getMatchCount() == matchPoint && !isSecond(matchPoint, userLottoNumbers, bonus))
+                rankInfo.countUp(rank);
+        }
+    }
+    public void updateMatchPointWithCondition(int matchPoint, RankInfo rankInfo, List<Integer> userLottoNumbers, Bonus bonus) {
+        if (isSecond(matchPoint, userLottoNumbers, bonus))
+            rankInfo.countUp(Rank.SECOND);
+        if (!isSecond(matchPoint, userLottoNumbers, bonus))
+            rankInfo.countUp(Rank.THIRD);
+    }
+    public boolean isSecond(int matchPoint, List<Integer> userLottoNumbers, Bonus bonus) {
+        return (matchPoint == CHECK_POINT && userLottoNumbers.contains(bonus.getBonusNumber()));
+    }
     public double calcYield(RankInfo rankInfo, Money money) {
         double profit = 0.0;
 
@@ -30,35 +59,5 @@ public class RankInfoService {
                 profit += rank.getReward() * rankInfo.getMatchNumber(rank);
         }
         return profit / money.getMoney() * PERCENTAGE;
-    }
-
-    public void compare(RankInfo rankInfo, Lotto lotto, List<Integer> userLottoNumbers, Bonus bonus) {
-        int count = 0;
-
-        for (int index = 0; index < lotto.getLottoSize(); index++) {
-            if (userLottoNumbers.contains(lotto.getLottoNumber(index))) count += 1;
-        }
-        if (count != CHECK_COUNT)
-            updateMatchNumber(rankInfo, userLottoNumbers, bonus, count);
-        if (count == CHECK_COUNT)
-            updateMatchNumberWithCondition(rankInfo, userLottoNumbers, bonus, count);
-    }
-
-    public void updateMatchNumber(RankInfo rankInfo, List<Integer> userLottoNumbers, Bonus bonus, int count) {
-        for (Rank rank : Rank.values()) {
-            if (rank.getMatchCount() == count && !isSecond(count, userLottoNumbers, bonus))
-                rankInfo.countUp(rank);
-        }
-    }
-
-    public void updateMatchNumberWithCondition(RankInfo rankInfo, List<Integer> userLottoNumbers, Bonus bonus, int count) {
-        if (isSecond(count, userLottoNumbers, bonus))
-            rankInfo.countUp(Rank.SECOND);
-        if (!isSecond(count, userLottoNumbers, bonus))
-            rankInfo.countUp(Rank.THIRD);
-    }
-
-    public boolean isSecond(int count, List<Integer> userLottoNumbers, Bonus bonus) {
-        return (count == CHECK_COUNT && userLottoNumbers.contains(bonus.getBonusNumber()));
     }
 }
