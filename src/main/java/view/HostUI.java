5f39a98afc733069fb3f726468f0c0f7da7a814f
mng990 <ryoo0504@pusan.ac.kr>
2022-11-15 14:46:15 +0900

    feat(main/java/view): HostUI.java 생성
    + getWinningNumber: 당첨 번호를 입력받는 함수
    + getBonusNumber: 보너스 번호를 입력받는 함수

---
+++ src/main/java/view/HostUI.java
@@ -0,0 +1,19 @@
+package view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class HostUI {
+
+    private final String winningMessage = "당첨 번호를 입력해 주세요.";
+    private final String bonusMessage = "보너스 번호를 입력해 주세요.";
+
+    public String getWinningNumber(){
+        System.out.println(winningMessage);
+        return readLine();
+    }
+
+    public String getBonusNumber(){
+        System.out.println(bonusMessage);
+        return readLine();
+    }
+}
