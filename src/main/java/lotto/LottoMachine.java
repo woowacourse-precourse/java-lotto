06ffb1ec7a6d69284f59015a403d830c1edf8411
Seungyong Choi <ppyooy336@naver.com>
2022-11-15 15:31:25 +0900

    feat: 로또 구입 금액에 맞는 로또를 발행한다

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -2,10 +2,19 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
 
+    public static List<Lotto> buyLotto(int money) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < money / 1000; i++) {
+            lottos.add(createLotto());
+        }
+
+        return lottos;
+    }
     public static Lotto createLotto(){
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return new Lotto(numbers);
--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -2,9 +2,11 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class User {
     private int money;
-
+    private List<Lotto> lottos;
     public User() {
     }
 
@@ -16,12 +18,18 @@ public class User {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력할 수 있습니다.");
         }
         isMoneyDividedWith1000(money);
+
+        buyLotto();
     }
 
     public void isMoneyDividedWith1000(int money) {
         if(money % 1000 != 0){
             throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해야 합니다.");
         }
+    }
+
+    private void buyLotto(){
+        this.lottos = LottoMachine.buyLotto(money);
     }
 
 
