0343dda351c4ea7ce97bb5e5f71089731ae79ba9
kingseungwooya <ksw_0505@nate.com>
2022-11-15 18:56:04 +0900

    test(lottotest): add lottotest

--- src/test/java/lotto/domain/model/LottoTest.java
+++ src/test/java/lotto/domain/model/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto.domain.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -11,7 +12,9 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또의 번호는 총 6개여야 합니다.");
+        ;
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
@@ -19,8 +22,25 @@ class LottoTest {
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 번호는 중복될 수 없습니다.");
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또번호가 범위에 맞지 않는 경우")
+    @Test
+    void CHECK_LOTTO_RANGE() {
+
+        assertAll(
+                () -> assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 99)))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+                () -> assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 0, 45)))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+                () -> assertThatThrownBy(() -> new Lotto(List.of(-1, 2, 3, 4, 0, 45)))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+        );
+    }
+
 }
