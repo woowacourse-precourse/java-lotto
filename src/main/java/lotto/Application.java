6e4334f2c2e1d54903645c838d2b08a35f0b526f
seongha <11pi885@gmail.com>
2022-11-15 16:22:54 +0900

    feat: 로또 번호들 발행하기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -28,12 +30,25 @@ public class Application {
         return Integer.parseInt(priceString);
     }
 
+    public static List<Lotto> buyLottoList(int price) {
+        int count = price / 1000;
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoList.add(new Lotto(numbers));
+        }
+        return lottoList;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int price;
+        List<Lotto> lottoList;
 
         try {
             price = readLottoPrice();
+            lottoList = buyLottoList(price);
+
         } catch (Exception e) {
             System.out.println("[ERROR] 입력이 올바르지 않습니다.");
             return;
