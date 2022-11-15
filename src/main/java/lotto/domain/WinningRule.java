eeb70b1957079a0a30c8703e426b4a657d487767
LEE-sh1673 <lsh901673@gmail.com>
2022-11-15 13:30:40 +0900

    feat(lotto): 기능 완성

--- src/main/java/lotto/domain/WinningRule.java
+++ src/main/java/lotto/domain/WinningRule.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 import java.util.function.Predicate;
 
 public enum WinningRule {
-	ALL_MATCH("6개 일치",
-		(result) -> result.getMatches() == 6, 2_000_000_000),
-	BONUS_MATCH("5개 일치, 보너스 볼 일치",
-		(result) -> result.getMatches() == 5 && result.containsBonus(), 30_000_000),
-	FIVE_MATCH("5개 일치",
-		(result) -> result.getMatches() == 5, 1_500_000),
-	FOUR_MATCH("4개 일치",
-		(result) -> result.getMatches() == 4, 50_000),
 	THREE_MATCH("3개 일치",
 		(result) -> result.getMatches() == 3, 5_000),
+	FOUR_MATCH("4개 일치",
+		(result) -> result.getMatches() == 4, 50_000),
+	FIVE_MATCH("5개 일치",
+		(result) -> result.getMatches() == 5, 1_500_000),
+	BONUS_MATCH("5개 일치, 보너스 볼 일치",
+		(result) -> result.getMatches() == 5 && result.containsBonus(), 30_000_000),
+	ALL_MATCH("6개 일치",
+		(result) -> result.getMatches() == 6, 2_000_000_000),
 	NONE_MATCH("0개 일치",
 		(result) -> result.getMatches() < 3, 0);
 
