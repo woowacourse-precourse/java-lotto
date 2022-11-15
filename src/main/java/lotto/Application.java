c37002c76ef04d5bfe149f6fcfec7207713304ec
pasly0920 <inwoo920621@gmail.com>
2022-11-15 20:15:00 +0900

    feat: 로또 종합 Controller application 반영

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.controller.Controller;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            Controller controller = new Controller();
+            controller.startLotto();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
