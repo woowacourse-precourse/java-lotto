d71003656f4928e05bbae655633dd9cc51fcf383
dvlpr0 <rladjswkd@naver.com>
2022-11-15 21:11:58 +0900

    feat: main 함수 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,19 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            Buyer buyer = new Buyer(UserInterface.readUserInput(InformMessage.PURCHASE_AMOUNT));
+            Lotto correct;
+            int bonus;
+
+            UserInterface.writeLotteryTickets(buyer.getBoughtTicketDTO());
+            correct = new Lotto(UserInterface.readUserInput(InformMessage.WINNING_NUMBER));
+            bonus = correct.parseBonusNumber(UserInterface.readUserInput(InformMessage.BONUS_NUMBER));
+            buyer.checkBoughtTickets(correct, bonus);
+            UserInterface.writeResult(buyer.getWinningCountDTO());
+            UserInterface.writeEarningRate(buyer.calculateEarningRate());
+        } catch (IllegalArgumentException ex) {
+            UserInterface.writeExceptionMessage(ex);
+        }
     }
 }
