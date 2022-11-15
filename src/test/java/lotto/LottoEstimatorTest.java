428eb728788e3ec99d9eb1f6d99f92508696a22b
JUHA <khabh@naver.com>
2022-11-15 13:07:05 +0900

    test(LottoEstimatorTest): 수정 사항에 따라 테스트 코드 재작성

--- src/test/java/lotto/LottoEstimatorTest.java
+++ src/test/java/lotto/LottoEstimatorTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,27 +22,23 @@ public class LottoEstimatorTest {
 
         List<Lotto> purchasedLotteries = new ArrayList<>(List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), new Lotto(List.of(1, 2, 3, 4, 5, 7))));
         lottoStatics = new LottoEstimator(WinningLotto.create()).estimate(purchasedLotteries);
-
     }
 
     @Test
     void 로또_등수_매기기() {
-        HashMap<LottoRank, Integer> expected = new HashMap<>();
-        for (LottoRank lottoRank : LottoRank.values()) {
-            expected.put(lottoRank, 0);
-        }
-        expected.put(LottoRank.FIRST_PLACE, 1);
-        expected.put(LottoRank.SECOND_PLACE, 1);
+        String result = lottoStatics.getRankStaticsResult();
 
-        HashMap<LottoRank, Integer> result = lottoStatics.rankStatics();
-
-        assertThat(result).isEqualTo(expected);
+        assertThat(result).contains("3개 일치 (5,000원) - 0개",
+                "4개 일치 (50,000원) - 0개",
+                "5개 일치 (1,500,000원) - 0개",
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                "6개 일치 (2,000,000,000원) - 1개");
     }
 
     @Test
     void 수익률_구하기() {
-        float result = lottoStatics.profitRate();
+        String result = lottoStatics.getProfitRateResult();
 
-        assertThat(result).isEqualTo(1.015E8f);
+        assertThat(result).contains("총 수익률은 101500000.0%입니다.");
     }
 }
