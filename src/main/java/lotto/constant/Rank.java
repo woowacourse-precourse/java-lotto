be08b724dba6f8da5ccfdbc80c036ce658174579
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-15 11:22:37 +0900

    feat(Rank): add message to Rank

--- src/main/java/lotto/constant/Rank.java
+++ src/main/java/lotto/constant/Rank.java
@@ -1,19 +1,26 @@
 package lotto.constant;
 
 public enum Rank {
-    FIRST_SIX_MATCHED(2_000_000_000),
-    SECOND_FIVE_WITH_BONUS(30_000_000),
-    THIRD_FIVE_MATCHED(1_500_000),
-    FOURTH_FOUR_MATCHED(50_000),
-    FIFTH_THREE_MATCHED(5_000);
+    FIRST_SIX_MATCHED(2_000_000_000, "6개 일치 (2,000,000,000원)"),
+    SECOND_FIVE_WITH_BONUS(30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD_FIVE_MATCHED(1_500_000, "5개 일치 (1,500,000원)"),
+    FOURTH_FOUR_MATCHED(50_000, "4개 일치 (50,000원)"),
+    FIFTH_THREE_MATCHED(5_000, "3개 일치 (5,000원)");
 
     private final int prize;
+    private final String message;
 
-    Rank(int prize) {
+    Rank(int prize, String message) {
         this.prize = prize;
+        this.message = message;
     }
 
     public int getPrize() {
         return prize;
+    }
+
+    @Override
+    public String toString() {
+        return message;
     }
 }
