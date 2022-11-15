753dc1fb5e5832f11ef8b465b3c3e1507f06eee3
“rotomoo” <quddlf410@naver.com>
2022-11-15 19:32:57 +0900

    feat(Lotto) : 로또 번호의 중복, 범위를 검사하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import lotto.exception.Check;
+import lotto.exception.CustomException;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -8,20 +11,30 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicate(numbers);
+        range(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(CustomException.VALIDATE.getMessage());
         }
     }
 
     // TODO: 추가 기능 구현
     private void duplicate(List<Integer> numbers) {
+        Check check = new Check();
+        if (!check.checkDuplicate(numbers)) {
+            throw new IllegalArgumentException(CustomException.DUPLICATE.getMessage());
+        }
     }
 
     private void range(List<Integer> numbers) {
+        Check check = new Check();
+        if (!check.checkRange(numbers)) {
+            throw new IllegalArgumentException(CustomException.RANGE.getMessage());
+        }
     }
 
     public List<Integer> getNumbers() {
