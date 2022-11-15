3e812447c8cef107bb18872874963162012f133f
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-15 23:26:55 +0900

    fix<WinningLotto>: 로또 숫자가 int 범위 초과일 경우 예외 처리를 하기 위해 LottoNumber에 String으로 전달하여 검증 위임

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -4,23 +4,17 @@ import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import lotto.util.LottoExceptionMessage;
 
 public class WinningLotto {
     private final Set<LottoNumber> numbers;
 
     public WinningLotto(String lotto) {
-        try {
-            numbers = toLottoNumbers(lotto);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(LottoExceptionMessage.LOTTO_NUMBER_RANGE.getMessage());
-        }
+        numbers = toLottoNumbers(lotto);
     }
 
     public Set<LottoNumber> toLottoNumbers(String lotto) {
         return Stream.of(lotto.split(","))
-                .mapToInt(Integer::parseInt)
-                .mapToObj(LottoNumber::new)
+                .map(LottoNumber::new)
                 .collect(Collectors.toCollection(TreeSet::new));
     }
 
