1262f3e3d713653af1f1e96bc4870a3a4e08f87a
ykmxxi <ykmxxi97@gmail.com>
2022-11-15 12:13:35 +0900

    refactor: 불필요한 값 제거

--- src/main/java/lotto/domain/enums/LottoRanking.java
+++ src/main/java/lotto/domain/enums/LottoRanking.java
@@ -1,28 +1,16 @@
 package lotto.domain.enums;
 
 public enum LottoRanking {
-    FIRST(6, false, 2000000000L),
-    SECOND(5, true, 30000000L),
-    THIRD(5, false, 1500000L),
-    FOURTH(4, false, 50000L),
-    FIFTH(3, false, 5000L);
+    FIRST(2000000000L),
+    SECOND(30000000L),
+    THIRD(1500000L),
+    FOURTH(50000L),
+    FIFTH(5000L);
 
-    private final int count;
-    private final boolean bonus;
     private final long winnings;
 
-    LottoRanking(int count, boolean bonus, long winnings) {
-        this.count = count;
-        this.bonus = bonus;
+    LottoRanking(long winnings) {
         this.winnings = winnings;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public boolean hasBonus() {
-        return bonus;
     }
 
     public Long getWinnings() {
