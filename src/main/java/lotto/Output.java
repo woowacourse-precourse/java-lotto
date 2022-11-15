74ead6de2cda7722aa11a2f7a4220a23fdf7a551
CJ <choijun0627@naver.com>
2022-11-15 15:37:33 +0900

    <feat>(Output.java): 기능 요구 사항 17번 구현
    기능 요구 사항 17번 당첨 내역 출력하는 함수 구현
    
    당첨 내역 출력에 필요한 당첨금 상수로 선언

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -3,6 +3,11 @@ package lotto;
 import java.util.List;
 
 public class Output {
+    private static final int FIRSTPRICE=2000000000;
+    private static final int SECONDPRICE=30000000;
+    private static final int THIRDPRICE=1500000;
+    private static final int FOURTHPRICE=50000;
+    private static final int FIFTHPRICE=5000;
 
     public void printLotto(List<List<Integer>> allLotto ){
         int lottoCount=allLotto.size();
@@ -12,8 +17,19 @@ public class Output {
         }
     }
 
-    public void printWinningRate(List<Integer> winningCount){
+    public int printWinningRate(List<Integer> winningCount){
+        System.out.println("당첨 통계\n---");
+        System.out.println("3개 일치 ("+FIFTHPRICE+"원) - "+winningCount.get(4)+"개");
+        System.out.println("4개 일치 ("+FOURTHPRICE+"원) - "+winningCount.get(3)+"개");
+        System.out.println("5개 일치 ("+THIRDPRICE+"원) - "+winningCount.get(2)+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 ("+SECONDPRICE+"원) - "+winningCount.get(1)+"개");
+        System.out.println("6개 일치 ("+FIRSTPRICE+"원) - "+winningCount.get(0)+"개");
 
+        int earnedMoney=FIRSTPRICE*winningCount.get(0)+SECONDPRICE*winningCount.get(1)+
+                THIRDPRICE*winningCount.get(2)+FOURTHPRICE*winningCount.get(3)+
+                FIFTHPRICE*winningCount.get(4);
+
+        return earnedMoney;
     }
 
     public void printProfitRate(int earnedMoney, int payedMoney){
