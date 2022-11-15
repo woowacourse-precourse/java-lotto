cb47d3466cd3559505622eb442f5c329acf05045
hwi215 <buc06137@naver.com>
2022-11-15 15:03:36 +0900

    feat: [기능4] 당첨번호 입력받는 기능

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -5,18 +5,23 @@ import java.util.List;
 
 import static lotto.Lotto.lottoNum;
 import static lotto.Lotto.randomLottoNum;
+import static lotto.view.Input.prizeNum;
 
 public class LottoGameController {
 
     public void LottoGame(){
         int num = lottoNum();
         List<Integer> lotto = new ArrayList<>();
+        ArrayList<Integer> prize = new ArrayList<>();
 
         for(int n = 0; n < num; n++){
             lotto = randomLottoNum();
             System.out.println(lotto.toString());
 
         }
+
+        prize = prizeNum(); // 당첨번호 + 보너스 리스트
+        System.out.println("prize: " + prize.toString());
 
     }
 }
--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,4 +1,6 @@
 package lotto.view;
+import java.util.ArrayList;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Input {
     public static int input(){
@@ -7,5 +9,25 @@ public class Input {
         int amount = Integer.valueOf(input);
 
         return amount;
+    }
+
+    public static ArrayList prizeNum(){
+        ArrayList<Integer> prizeAll = new ArrayList<>();
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String prizeNum = readLine();
+        String[] prizeNums = prizeNum.split(",");
+        for(int num = 0; num < prizeNums.length; num++){
+            int target = Integer.valueOf(prizeNums[num]);
+            prizeAll.add(target);
+        }
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bo = readLine();
+        int bonus = Integer.valueOf(bo);
+        prizeAll.add(bonus);
+
+        return prizeAll;
+
     }
 }
