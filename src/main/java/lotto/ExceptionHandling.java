63460dfe6c9374bb5ee76b35ff25cef451cc7386
abbd2 <ajkety00@gmail.com>
2022-11-15 19:08:03 +0900

    Style(ExceptionHandling): 코드 변경 없음

--- src/main/java/lotto/ExceptionHandling.java
+++ src/main/java/lotto/ExceptionHandling.java
@@ -11,33 +11,33 @@ public class ExceptionHandling {
     private static final String NUMBER_REGEX = "^[0-9]*$";
     private static final String LOTTO_NUMBER_REGEX = "^[1-9]{1}$|^[1-3]{1}[0-9]{1}$|^4{1}[0-5]{1}$";
 
-    public void notDivide(long amount){
-        if(amount % 1000 != 0 || amount <= 0){
+    public void notDivide(long amount) {
+        if (amount % 1000 != 0 || amount <= 0) {
             throw new IllegalArgumentException(ShowErrorMessage.showNotDivide());
         }
     }
 
-    public void isNumeric(String value){
-        if(!Pattern.matches(NUMBER_REGEX, value) || value.equals("")){ // 숫자가 아니라면
+    public void isNumeric(String value) {
+        if (!Pattern.matches(NUMBER_REGEX, value) || value.equals("")) { // 숫자가 아니라면
             throw new IllegalArgumentException(ShowErrorMessage.showOutRangeLottoNumber());
         }
     }
 
-    public void outRangeLottoNumber(String value){
-        if(!Pattern.matches(LOTTO_NUMBER_REGEX, value) || value.equals("")){ // 로또 번호의 범위가 아니라면
+    public void outRangeLottoNumber(String value) {
+        if (!Pattern.matches(LOTTO_NUMBER_REGEX, value) || value.equals("")) { // 로또 번호의 범위가 아니라면
             throw new IllegalArgumentException(ShowErrorMessage.showOutRangeLottoNumber());
         }
     }
 
-    public void removeDuplicateNumber(List<Integer> winNumber){
+    public void removeDuplicateNumber(List<Integer> winNumber) {
         Set<Integer> winNumberSet = new HashSet<>(winNumber);
-        if(winNumberSet.size() != winNumber.size()){
+        if (winNumberSet.size() != winNumber.size()) {
             throw new IllegalArgumentException(ShowErrorMessage.showDuplicateLottoNumber());
         }
     }
 
-    public void containsBonusNumber(List<Integer> winNumbers, int bonusNumber){
-        if(winNumbers.contains(bonusNumber)){
+    public void containsBonusNumber(List<Integer> winNumbers, int bonusNumber) {
+        if (winNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(ShowErrorMessage.showContainsBonusNumber());
         }
     }
