eef359e05f03dc9b677c5ba4907347ad773564c8
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-15 23:42:09 +0900

    refactor: refactor exception case message

--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -6,13 +6,17 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 public class LottoTicket {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    private static final String NUMBERS_ONLY = " 숫자만 입력해 주세요.";
+    private static final String NO_CHANGE = " 잔돈이 발생합니다.";
+    private static final String TOO_LITTLE_MONEY = " 로또의 최소 가격은 1000원 입니다.";
     public static int getLotteryTicketPrice() {
-        System.out.println("구입금액을 입력해 주세요.\n");
+        System.out.println("구입금액을 입력해 주세요.");
         String cash = Console.readLine();
         if (!cash.matches("[0-9]+")){
-            String message = "[ERROR] 숫자만 입력해 주세요.\n";
-            System.out.printf("%s", message);
-            throw new IllegalArgumentException(message);
+            System.out.println(ERROR_MESSAGE+NUMBERS_ONLY);
+            throw new IllegalArgumentException(ERROR_MESSAGE+NUMBERS_ONLY);
         }
         int price = Integer.parseInt(cash);
         return price;
@@ -22,10 +26,10 @@ public class LottoTicket {
         int ticketPrice = 1000;
         int ticketCount;
         if (totalCash % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 잔돈이 발생합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE+NO_CHANGE);
         }
         if (totalCash < 1000) {
-            throw new IllegalArgumentException("[ERROR] 로또의 최소 가격은 1000원 입니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE+TOO_LITTLE_MONEY);
         }
         ticketCount = totalCash / ticketPrice;
         System.out.printf("%d개를 구매했습니다.\n", ticketCount);
