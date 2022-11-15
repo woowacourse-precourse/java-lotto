419ff8d4f10b14ef2e4ba9309580451ae019b8c0
limzzum <chfhddl1234@naver.com>
2022-11-15 23:40:16 +0900

    refactor : 게임 실행 함수 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,18 +8,23 @@ public class Application {
     public static void main(String[] args) {
         LottoMachine machine = new LottoMachine();
         User user = new User();
+
         try {
-            machine.getInputMoney();
-            machine.createLottoNumber();
-
-            user.inputWinningNumber();
-            user.inputBonusNumber();
-
-            machine.printLottos();
-            machine.getResult(user.getWinningNumber(), user.getBonusNumber());
-            machine.printResult();
+            lottoGame(machine, user);
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
+    }
+
+    private static void lottoGame(LottoMachine machine, User user) {
+        machine.getInputMoney();
+        machine.createLottoNumber();
+
+        user.inputWinningNumber();
+        user.inputBonusNumber();
+
+        machine.printLottos();
+        machine.getResult(user.getWinningNumber(), user.getBonusNumber());
+        machine.printResult();
     }
 }
