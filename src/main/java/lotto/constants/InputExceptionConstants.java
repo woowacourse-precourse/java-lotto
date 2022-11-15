47e3a50bb256ebb03997c2d2ad2a74339fb93f66
qkdk <86948395+qkdk@users.noreply.github.com>
2022-11-15 16:32:19 +0900

    style: 사용되지 않는 필드 제거

--- src/main/java/lotto/constants/InputExceptionConstants.java
+++ src/main/java/lotto/constants/InputExceptionConstants.java
@@ -5,8 +5,7 @@ public enum InputExceptionConstants {
     INPUT_NONE_NUMERIC("수가 아닌 입력이 있습니다."),
     INPUT_BLANK("빈 문자열이 입력되었습니다."),
     INPUT_ZERO("0원은 입력받을 수 없습니다."),
-    INPUT_REST_EXIST("입력금액과 로또 금액이 나누어 떨어지지 않습니다."),
-    ;
+    INPUT_REST_EXIST("입력금액과 로또 금액이 나누어 떨어지지 않습니다.");
 
     private final String value;
 
