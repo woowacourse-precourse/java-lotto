ab04589517bf8ab7ea70f5824d1e6097a9f94031
yejin <kwonyj1022@naver.com>
2022-11-15 21:18:41 +0900

    fix: enum Win 클래스 구성 변경

--- src/main/java/lotto/domain/result/domain/Win.java
+++ src/main/java/lotto/domain/result/domain/Win.java
@@ -1,25 +1,22 @@
 package lotto.domain.result.domain;
 
 public enum Win {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000);
+    FIRST(2_000_000_000, "6개 일치 (2,000,000,000원) - "),
+    SECOND(30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(1_500_000, "5개 일치 (1,500,000원) - "),
+    FOURTH(50_000, "4개 일치 (50,000원) - "),
+    FIFTH(5_000, "3개 일치 (5,000원) - ");
 
-    private final int sameNumberCnt;
     private final int prize;
+    private final String winning;
 
-    Win(int sameNumberCnt, int prize) {
-        this.sameNumberCnt = sameNumberCnt;
+    Win(int prize, String winning) {
         this.prize = prize;
-    }
-
-    public int sameNumberCnt() {
-        return sameNumberCnt;
+        this.winning = winning;
     }
 
     public int prize() {
         return prize;
     }
+    public String winning() { return winning; }
 }
