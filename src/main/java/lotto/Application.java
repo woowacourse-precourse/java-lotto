109f73d41fe9b5bb3aa3922e937a18248529d9dd
ijung-won <dlwnddnjs96@gmail.com>
2022-11-15 19:34:45 +0900

    feat(User): 구매 개수 출력, console 포멧 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -20,7 +20,7 @@ public class Application {
         System.out.println();
         user.printCount();
         user.printBoughtLotto();
-
+        System.out.println();
 
         System.out.println("당첨 번호를 입력해 주세요.");
         String inputLottoNum = Console.readLine();
