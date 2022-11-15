44eead370e450ef68980ce5175afbb29037b8f0d
hyunseo <goatsfish@gmail.com>
2022-11-15 11:58:38 +0900

    feat : 다수 로또 당첨 수익률 계산 기능 추가

---
+++ src/main/java/lotto/domain/LottoResults.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import static lotto.domain.LottoSetting.LOTTO_PRICE;
+
+public class LottoResults {
+    private final Map<LottoWin, Integer> lottoResults;
+
+    public LottoResults(Map<LottoWin, Integer> lottoResults) {
+        this.lottoResults = new EnumMap<>(lottoResults);
+    }
+
+    public Map<LottoWin, Integer> getLottoResults() {
+        return new EnumMap<>(lottoResults);
+    }
+
+    public double totalRate() {
+        double totalCost = totalCost();
+        if (totalCost == 0.0) {
+            return totalCost;
+        }
+        return totalPrize() / totalCost * 100;
+    }
+
+    private long totalCost() {
+        return (long) lottoResults.values()
+                .stream()
+                .reduce(0, Integer::sum) * (long) LOTTO_PRICE.getSetting();
+    }
+
+    private long totalPrize() {
+        return lottoResults.entrySet()
+                .stream()
+                .mapToLong(lottoWin -> (long) lottoWin.getKey().getPrize() * lottoWin.getValue())
+                .sum();
+    }
+}
