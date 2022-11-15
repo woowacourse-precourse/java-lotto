06064a9687ee56b4651d79862755b43b9277b425
Yongwoo Choi <cyw7515@naver.com>
2022-11-15 18:25:01 +0900

    fix (LottoController): immutable list를 정렬할 수 없어 발생하는 에러 수정

--- src/main/java/lotto/domain/lotto/LottoGenerator.java
+++ src/main/java/lotto/domain/lotto/LottoGenerator.java
@@ -1,13 +1,17 @@
 package lotto.domain.lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
     public static Lotto generate() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        Collections.sort(numbers);
-        return new Lotto(numbers);
+
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+
+        return new Lotto(sortedNumbers);
     }
 }
