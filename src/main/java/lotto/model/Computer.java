46a0332a2b5f58dd5d56522614eb24b2bc225c37
Lee-Jiseung <ljss98@naver.com>
2022-11-15 20:08:49 +0900

    fix: List 구현 방식 변경

--- src/main/java/lotto/model/Computer.java
+++ src/main/java/lotto/model/Computer.java
@@ -2,12 +2,13 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
 
     public Lotto buyLotto() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         return new Lotto(numbers);
     }
 }
