4016cc36ce026579571f587e3d8eaa9d9f647666
Lemon Soju <yckk980320@naver.com>
2022-11-15 13:54:51 +0900

    feat(LottoService): function to count the number of statistics
    Select Statistic based on the number of matching Lotto numbers and bonus numbers,
    and add 1 to the value of StatisticAndCount if the conditions are met.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,11 +1,14 @@
 package lotto.service;
 
 import lotto.domain.Lotto;
+import lotto.domain.Statistic;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static lotto.domain.Lotto.issueLotto;
+import static lotto.domain.Statistic.findStatistic;
 import static lotto.view.InputView.*;
 import static lotto.view.OutputView.printLottoNumber;
 
@@ -35,6 +38,17 @@ public class LottoService {
             output.add(Integer.parseInt(e));
         }
         return output;
+    }
+
+    // 일치하는 로또 번호 개수와 보너스 번호를 기준으로 Statistic을 선정하고 조건을 만족하면 StatisticAndCount의 value에 1을 추가한다.
+    private static void countStatistic(Lotto inputLotto, int bonus, List<Lotto> issuedLotteries,
+                                       HashMap<Statistic, Integer> countStatistic, int index) {
+        int matchingNumber = countMatchingNumber(issuedLotteries.get(index), inputLotto);
+        boolean bonusFlag = compareBonus(issuedLotteries.get(index), bonus);
+        Statistic statistic = findStatistic(matchingNumber, bonusFlag);
+        if (statistic != null) {
+            countStatistic.replace(statistic, countStatistic.get(statistic) + 1);
+        }
     }
 
     public static Integer countMatchingNumber(Lotto issuedLotto, Lotto inputLotto) {
