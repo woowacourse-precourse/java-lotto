3f9bb313fc2acc8995ff0efee03be10d755effca
dingding-21 <phj8221@naver.com>
2022-11-15 15:09:50 +0900

    feat($Game): 로또 수량만큼 로또 발행하는 기능 구현

--- src/main/java/lotto/domain/Game.java
+++ src/main/java/lotto/domain/Game.java
@@ -1,19 +1,36 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.ui.Input;
 import lotto.utils.Constant;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
     public Game() {
+        createLottoWithCount(getLottoCount(Input.getPurchaseAmount()));
     }
 
     public Integer getLottoCount(int amount) {
         return amount / Constant.PURCHASE_AMOUNT_UNIT;
     }
 
-    private List<Integer> getRandomLottoNumber() {
+    public Lotto createLotto() {
+        return new Lotto(getRandomLottoNumber());
+    }
+
+    public List<Lotto> createLottoWithCount(int count) {
+        List<Lotto> lotto = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            lotto.add(createLotto());
+        }
+
+        return lotto;
+    }
+
+    public List<Integer> getRandomLottoNumber() {
         return Randoms.pickUniqueNumbersInRange(Constant.MIN_LOTTO_NUMBER, Constant.MAX_LOTTO_NUMBER, Constant.LOTTO_NUMBER_COUNT);
     }
 }
