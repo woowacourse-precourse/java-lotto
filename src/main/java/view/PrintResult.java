658ebf385d4c4ccff500171abc97a2fd59a57434
dydtmd6801 <dydtmd6801@naver.com>
2022-11-15 18:04:37 +0900

    feat(PrintResult): 로또 번호 출력 기능
    구매자가 구매한 로또 번호를 출력해주는 기능

--- src/main/java/view/PrintResult.java
+++ src/main/java/view/PrintResult.java
@@ -1,7 +1,13 @@
 package view;
 
+import java.util.List;
+
 public class PrintResult {
     public void printBuyLotto(int buyLottoNumber){
         System.out.println(buyLottoNumber + "개를 구매했습니다.");
+    }
+
+    public void printLottoNumber(List<Integer> lottoNumber){
+        System.out.println(lottoNumber);
     }
 }
