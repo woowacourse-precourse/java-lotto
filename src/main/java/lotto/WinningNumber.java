acdaa08d2aedcb642bf3403721a5cd1c9c5e0680
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-11-15 18:21:38 +0900

    fix: Exception을 상위 메소드로 올리게 수정
    Exception을 catch하지 않고 상위 메소드로 올리게 수정

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -11,61 +11,60 @@ public class WinningNumber {
     private static List<Integer> winningNumbers;
     private static int bonusNumber;
 
-    public WinningNumber() {
-        try{
-            setWinningNumbers();
-            validateWinningNumber(winningNumbers);
-            setBonusNumber();
-            validateBonusNumber(winningNumbers, bonusNumber);
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-        }
+    public WinningNumber() throws IllegalArgumentException {
+
+        setWinningNumbers();
+        validateWinningNumber(winningNumbers);
+        setBonusNumber();
+        validateBonusNumber(winningNumbers, bonusNumber);
+
     }
 
-    private void validateWinningNumber(List<Integer> winningNumbers) {
-        if (winningNumbers.size() != 5) {
-            throw new IllegalArgumentException("[Error] 5개의 당첨 번호를 입력해주세요.");
+    private void validateWinningNumber(List<Integer> winningNumbers) throws IllegalArgumentException {
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 6개의 당첨 번호를 입력해주세요.");
         }
         if (winningNumbers.stream().anyMatch(number -> number < 1 || number > 45)) {
-            throw new IllegalArgumentException("[Error] 당첨 번호는 1~45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1~45 사이의 숫자여야 합니다.");
         }
-        if (winningNumbers.stream().distinct().count() != 5) {
-            throw new IllegalArgumentException("[Error] 당첨 번호는 중복될 수 없습니다.");
+        if (winningNumbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
         }
 
     }
 
-    private static void validateBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
+    private static void validateBonusNumber(List<Integer> winningNumbers, int bonusNumber)
+            throws IllegalArgumentException {
         if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[Error] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
         }
         if (bonusNumber < 1 || bonusNumber > 45) {
-            throw new IllegalArgumentException("[Error] 보너스 번호는 1~45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이의 숫자여야 합니다.");
         }
     }
 
     private void setWinningNumbers() throws IllegalArgumentException {
-        System.out.println("당첨 번호를 입력해 주세요. (1~45)중 5개를 골라주세요. 쉼표로 구분됩니다");
+        System.out.println("당첨 번호를 입력해 주세요. (1~45)중 6개를 골라주세요. 쉼표로 구분됩니다");
         String winningNumberUserInput = readLine();
-        try{
+        try {
             List<Integer> winningNumbers = Stream.of(winningNumberUserInput.split(","))
                     .map(String::trim)
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
             this.winningNumbers = winningNumbers;
-        }catch (Exception e){
-            throw new IllegalArgumentException("[Error] 당첨 번호는 숫자만 입력 가능합니다.");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자만 입력 가능합니다.");
         }
     }
 
     private void setBonusNumber() throws IllegalArgumentException {
         System.out.println("보너스 번호를 입력해 주세요. (1~45)중 1개를 골라주세요.");
         String bonusNumberUserInput = readLine();
-        try{
+        try {
             int bonusNumber = Integer.parseInt(bonusNumberUserInput);
             this.bonusNumber = bonusNumber;
-        }catch (Exception e){
-            throw new IllegalArgumentException("[Error] 보너스 번호는 숫자만 입력 가능합니다.");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자만 입력 가능합니다.");
         }
     }
 
