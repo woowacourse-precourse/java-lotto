e758c386a3f6322eb71b1e9a4bc46eda2f47a167
wonseokjee <wsji9404@gmail.com>
2022-11-15 14:20:41 +0900

    Refactor(Application): 수정 Apllication class

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -12,25 +12,12 @@ public class Application {
         /*8개를 구매했습니다.*/
         User user = new User(money);
         user.textTicketcount();
-        List<List<Integer>> allTicket = playLotto.allTicket(money);
-
-        /*당첨번호입력*/
-        createLotto.textWinningNumber();
-        createLotto.winningNumber(createLotto.inputWinningNumber());
-
-        /*보너스 번호 입력*/
-        createLotto.textBounsNumber();
-        createLotto.bonusNumber(createLotto.inputBonusNumber());
+        List<List<Integer>> allTicket = playLotto.allTicket(user.countTicket());
 
         /*당첨통계*/
-        List<Integer> correctNumber = playLotto.allTicketCheckCorrectNumber(allTicket, createLotto.inputWinningNumber(), createLotto.inputBonusNumber());
-        playLotto.print5th(correctNumber);
-        playLotto.print4th(correctNumber);
-        playLotto.print3rd(correctNumber);
-        playLotto.print2nd(correctNumber);
-        playLotto.print1st(correctNumber);
+        List<Integer> correctNumber = playLotto.allTicketCheckCorrectNumber(allTicket, inputNumber.inputWinningNumber(), inputNumber.inputBonusNumber());
+        playLotto.printRanking(correctNumber);
 
-        double yield = playLotto.yield(money, correctNumber);
-        playLotto.printYield(yield);
+        playLotto.yield(money, correctNumber);
     }
 }
