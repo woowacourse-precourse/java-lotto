71416a6bc6e990aa8d58e282fbf72f8d71247911
kimdonghun <zhfptm12@naver.com>
2022-11-15 13:00:54 +0900

    refactor : modify winning, bonus number string

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,11 @@
+package lotto.view;
+import lotto.strings.Strings;
+public class InputView {
+    public InputView() {
+    }
+
+    static public void purchaseLotto() {
+        System.out.println(Strings.purchaseMessage);
+    }
+
+}
