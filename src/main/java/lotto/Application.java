47dd4a6184e99960fdcd6eb2c49826020b163c5c
Junmin <junmin1623@naver.com>
2022-11-15 19:34:41 +0900

    feat(Lotto,app):lotto 에 있던 구입금액에 따른 갯수 설정 app을 옮김

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 // git check
 public class Application {
+
+    public static int Lotto_cnt(String my_money){
+        return Integer.parseInt(my_money)/1000;
+    }
+    public static List<Integer> rnd_Lotto_number_maker(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String money,ans,bonus;
+        int L_cnt;
+        System.out.println("구입금액을 입력해 주세요.");
+        L_cnt = Lotto_cnt(Console.readLine());
+        System.out.printf("%d개를 구입했습니다.",L_cnt);
+
+
+
+
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,13 +4,10 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-    private int lotto_cnt;
+
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
-    }
-    public void lotto_publish(String money){
-        this.lotto_cnt = (Integer.parseInt(money))/1000;
     }
 
     private void validate(List<Integer> numbers) {
