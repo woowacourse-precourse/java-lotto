ba170c0b5996134429246e14b3ffa8ee7b2c2f88
TeddyYeoni <po99027@naver.com>
2022-11-15 22:23:30 +0900

    refactor(lotto) : 구입 개수에 따른 당첨 번호 출력 기능 구현

--- src/main/java/lotto/domain/GenerateNumbers.java
+++ src/main/java/lotto/domain/GenerateNumbers.java
@@ -3,11 +3,13 @@ package lotto.domain;
 import java.util.Collections;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class GenerateNumbers {
-	public static int bonusNumber;
+	public static final int LOTTO_PRICE = 1000;
 	public List<Integer> lottoNumbers;
+	public static int bonusNumber;
 
 	public List<Integer> lotto() {
 		lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
@@ -20,4 +22,17 @@ public class GenerateNumbers {
 		return lottoNumbers;
 	}
 	
+	public String winningNubmer() {
+		System.out.println("구입 금액을 입력해 주세요.");
+		String inputAmount = Console.readLine();
+		int purchaseAmount = Integer.parseInt(inputAmount);
+		int lotteryCount = purchaseAmount / LOTTO_PRICE;
+		
+		String winningNumbers = "";
+		for (int repeat = 0; repeat < lotteryCount; repeat++) {
+			winningNumbers += lotto() + "\n";
+		}
+		winningNumbers = lotteryCount + "개를 구매했습니다.\n" + winningNumbers;
+		return winningNumbers;
+	}
 }
