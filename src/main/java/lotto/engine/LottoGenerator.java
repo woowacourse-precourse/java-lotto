2d6ca4873096dc2936ad5f43b0c46ca290312e95
UJ15 <dbwogml15@naver.com>
2022-11-15 13:17:00 +0900

    refactor : move ErrorMessage, get message from error

--- src/main/java/lotto/engine/LottoGenerator.java
+++ src/main/java/lotto/engine/LottoGenerator.java
@@ -3,16 +3,13 @@ package lotto.engine;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import lotto.error.Error;
 import lotto.model.Lotto;
 
 public class LottoGenerator {
     private static final int MINIMUM_LOTTO_COUNT = 1;
     private static final int AMOUNT_UNIT = 1_000;
     private static final int AMOUNT_LIMIT = 100_000;
-    private static final String EMPTY_INPUT_ERROR_MESSAGE = "금액을 입력하세요";
-    private static final String NOT_DIGIT_ERROR_MESSAGE = "금액은 숫자로 입력하세요.";
-    private static final String DIVIDE_UNIT_ERROR_MESSAGE = "금액은 1,000원 단위로 입력하세요.";
-    private static final String AMOUNT_RANGE_ERROR_MESSAGE = "금액은 100,000원 이하 1000원 이상으로 입력하세요.";
 
     private final NumberGenerator numberGenerator;
 
@@ -32,7 +29,7 @@ public class LottoGenerator {
 
     public int parseToLottoCount(String purchasePrice) {
         if (purchasePrice == null || purchasePrice.isEmpty()) {
-            throw new IllegalArgumentException(EMPTY_INPUT_ERROR_MESSAGE);
+            throw new IllegalArgumentException(Error.PRICE_EMPTY_INPUT_ERROR.getMessage());
         }
         businessValidate(purchasePrice);
 
@@ -41,18 +38,18 @@ public class LottoGenerator {
 
     private void businessValidate(String purchasePrice) {
         if (!purchasePrice.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException(NOT_DIGIT_ERROR_MESSAGE);
+            throw new IllegalArgumentException(Error.NOT_DIGIT_ERROR.getMessage());
         }
 
         int limitNumberLength = (int) Math.log10(AMOUNT_LIMIT) + 1;
         if (purchasePrice.length() > limitNumberLength
                 || Integer.parseInt(purchasePrice) > AMOUNT_LIMIT
                 || Integer.parseInt(purchasePrice) < AMOUNT_UNIT) {
-            throw new IllegalArgumentException(AMOUNT_RANGE_ERROR_MESSAGE);
+            throw new IllegalArgumentException(Error.AMOUNT_RANGE_ERROR.getMessage());
         }
 
         if (Integer.parseInt(purchasePrice) % AMOUNT_UNIT != 0) {
-            throw new IllegalArgumentException(DIVIDE_UNIT_ERROR_MESSAGE);
+            throw new IllegalArgumentException(Error.DIVIDE_UNIT_ERROR.getMessage());
         }
     }
 }
--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -8,13 +8,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import lotto.error.Error;
 
 public class WinningNumber {
     private static final String NUMBER_INPUT_REGEX = "^(\\d{1,2},){5}(\\d{1,2}$)";
     private static final String BONUS_INPUT_REGEX = "^(\\d{1,2})$";
-    private static final String EMPTY_INPUT_ERROR_MESSAGE = "번호를 입력하세요.";
-    private static final String INPUT_FORMAT_ERROR_MESSAGE = "당첨 숫자는 1~45 숫자를 '0,0,0,0,0,0' 형식으로 입력해주세요.";
-    private static final String BONUS_FORMAT_ERROR_MESSAGE = "보너스 숫자는 1~45 숫자를 당첨 숫자와 중복되지 않게 입력해주세요";
 
     private final List<Integer> numbers;
     private final int bonusNumber;
@@ -45,7 +43,7 @@ public class WinningNumber {
                 || userInputBonusNumber.isEmpty()
                 || userInputNumber.isEmpty()
         ) {
-            throw new IllegalArgumentException(EMPTY_INPUT_ERROR_MESSAGE);
+            throw new IllegalArgumentException(Error.WINNING_EMPTY_INPUT_ERROR.getMessage());
         }
 
         List<Integer> numbers = parseToNumbers(userInputNumber);
@@ -56,7 +54,7 @@ public class WinningNumber {
 
     private static List<Integer> parseToNumbers(String userInputNumber) {
         if (!userInputNumber.matches(NUMBER_INPUT_REGEX)) {
-            throw new IllegalArgumentException(INPUT_FORMAT_ERROR_MESSAGE);
+            throw new IllegalArgumentException(Error.INPUT_FORMAT_ERROR.getMessage());
         }
 
         List<Integer> numbers = Arrays.stream(userInputNumber.split(","))
@@ -65,7 +63,7 @@ public class WinningNumber {
                 .collect(Collectors.toList());
 
         if (numbers.size() < COUNT) {
-            throw new IllegalArgumentException(INPUT_FORMAT_ERROR_MESSAGE);
+            throw new IllegalArgumentException(Error.INPUT_FORMAT_ERROR.getMessage());
         }
 
         return numbers;
@@ -73,13 +71,13 @@ public class WinningNumber {
 
     private static int parseToBonusNumber(String userInputBonusNumber, List<Integer> numbers) {
         if (!userInputBonusNumber.matches(BONUS_INPUT_REGEX)) {
-            throw new IllegalArgumentException(BONUS_FORMAT_ERROR_MESSAGE);
+            throw new IllegalArgumentException(Error.BONUS_FORMAT_ERROR.getMessage());
         }
 
         int bonusNumber = Integer.parseInt(userInputBonusNumber);
 
         if (bonusNumber > END_RANGE_NUMBER || bonusNumber < START_RANGE_NUMBER || numbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException(BONUS_FORMAT_ERROR_MESSAGE);
+            throw new IllegalArgumentException(Error.BONUS_FORMAT_ERROR.getMessage());
         }
 
         return bonusNumber;
