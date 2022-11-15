103ecc892b98f3a24bb52c2320ee8fd1630a4635
June <68291395+june-777@users.noreply.github.com>
2022-11-15 22:49:10 +0900

    feat: LottoMachine에서 사용하는 상수 클래스 생성

---
+++ src/main/java/model/LottoMachineConstants.java
@@ -0,0 +1,15 @@
+package model;
+
+public class LottoMachineConstants {
+
+	public static final int NONE = -1;
+	public static final int FIRST = 0;
+	public static final int SECOND = 1;
+	public static final int THIRD = 2;
+	public static final int FOURTH = 3;
+	public static final int FIFTH = 4;
+
+	private LottoMachineConstants() {
+
+	}
+}
