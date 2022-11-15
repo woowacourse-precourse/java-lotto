738b37ea300461da49abfb6b5935f8ef58399500
korjun1993 <corojoon93@naver.com>
2022-11-15 22:06:19 +0900

    feat(lotto) : 당첨 번호를 입력받는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,24 +1,26 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.exception.ErrorCode;
 import lotto.exception.MyIllegalArgumentException;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static lotto.exception.ErrorCode.MONEY_ERROR;
+import static lotto.exception.ErrorCode.WIN_NUMBER_ERROR;
 
 public class Application {
     public static void main(String[] args) {
-        List<Lotto> lottos;
-        Receipt receipt;
-        int money;
         try {
-            money = askMoney();
-            receipt = new Receipt(money);
-            lottos = receipt.buy();
+            int money = askMoney();
+            Receipt receipt = new Receipt(money);
+            List<Lotto> lottos = receipt.buy();
             print(receipt, lottos);
+            List<Integer> winNumbers = askWinNumbers();
+            Lotto winLotto = new Lotto(winNumbers);
         } catch (MyIllegalArgumentException e) {
             e.printErrorMessage();
-            return;
         }
     }
 
@@ -37,6 +39,15 @@ public class Application {
             return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException ignored) {
         }
-        throw new MyIllegalArgumentException(ErrorCode.MONEY_ERROR);
+        throw new MyIllegalArgumentException(MONEY_ERROR);
+    }
+
+    public static List<Integer> askWinNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        try {
+            return Stream.of(Console.readLine().split(",")).map(Integer::parseInt).collect(Collectors.toList());
+        } catch (Exception ignored) {
+        }
+        throw new MyIllegalArgumentException(WIN_NUMBER_ERROR);
     }
 }
