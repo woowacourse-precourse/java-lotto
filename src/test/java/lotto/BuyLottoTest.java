39a77a259278843a51ec799e69b6f723d42a53f0
eugene1204 <s1061001@naver.com>
2022-11-15 18:02:21 +0900

    feat : BuyLottoTest 테스트코드 작성, buyLotto

--- src/test/java/lotto/BuyLottoTest.java
+++ src/test/java/lotto/BuyLottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BuyLottoTest {
     @DisplayName("금액이 1000원 미만이면 예외가 발생한다.")
@@ -22,5 +23,11 @@ public class BuyLottoTest {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() ->  new BuyLotto("1500"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("금액을 입력하면 로또구매개수를 출력한다.")
+    @Test
+    void buyLotto() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertEquals(new BuyLotto("1000").buyLotto(),1);
     }
 }
