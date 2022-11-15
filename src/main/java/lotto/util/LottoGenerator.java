ed44b1393a395039c4e2204008b508edf607a71e
SeongukBaek <bsu6235@gmail.com>
2022-11-15 16:44:56 +0900

    style: add newline

--- src/main/java/lotto/util/LottoGenerator.java
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -9,7 +9,13 @@ public class LottoGenerator {
     private LottoGenerator() {}
 
     public static List<Integer> generate() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(Validator.START_NUMBER, Validator.END_NUMBER, Validator.LOTTO_SIZE);
-        return numbers.stream().sorted().collect(Collectors.toList());
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                Validator.START_NUMBER,
+                Validator.END_NUMBER,
+                Validator.LOTTO_SIZE
+        );
+        return numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
