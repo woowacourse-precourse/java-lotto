4ecaa7fb33ed0096012811997d84dd51d2ff881a
cpot5620 <=cpot5620@naver.com>
2022-11-15 16:37:36 +0900

    style(WinningType): add new line for readability

--- src/main/java/lotto/resource/WinningType.java
+++ src/main/java/lotto/resource/WinningType.java
@@ -2,12 +2,16 @@ package lotto.resource;
 
 
 public enum WinningType {
-    THREE("3개 일치 (5,000원) - %d개", 3, 5000),
-    FOUR("4개 일치 (50,000원) - %d개", 4, 50000),
-    FIVE("5개 일치 (1,500,000원) - %d개", 5, 1500000),
+    THREE("3개 일치 (5,000원) - %d개",
+            3, 5000),
+    FOUR("4개 일치 (50,000원) - %d개",
+            4, 50000),
+    FIVE("5개 일치 (1,500,000원) - %d개",
+            5, 1500000),
     FIVE_AND_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개",
             6, 30000000),
-    SIX("6개 일치 (2,000,000,000원) - %d개", 7, 2000000000);
+    SIX("6개 일치 (2,000,000,000원) - %d개",
+            7, 2000000000);
 
     private final String message;
     private final int numberOfMatching;
