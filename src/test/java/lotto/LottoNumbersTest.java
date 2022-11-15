269187144fdd087a58f237371b04c9e3ad5b3d8e
sangeun-b <baek0007@algonquinlive.com>
2022-11-15 09:48:54 +0900

    test: 로또 번호 생성 테스트 추가

--- src/test/java/lotto/LottoNumbersTest.java
+++ src/test/java/lotto/LottoNumbersTest.java
@@ -1,14 +1,18 @@
 package lotto;
 
+import lotto.domain.LottoNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LottoNumbersTest {
+    private final LottoNumbers lottoNumbers = new LottoNumbers();
 
     @DisplayName("구입 금액에 맞춰서 로또 생성한다.")
     @Test
     void createLottoNumbersByAmount() {
-
+         assertThat(lottoNumbers.printLotto(7).size()).isEqualTo(7);
     }
 
 }
