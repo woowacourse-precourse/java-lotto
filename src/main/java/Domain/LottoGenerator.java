957478f883d09f5751080eac028c507ee9e95e18
nahyeon cho <whskgus12@nate.com>
2022-11-15 20:41:21 +0900

    feat(domain): 로또 구입 단위가 1000원이 아닐 때
    로또 구입 단위가 1000원이 아닐 때 예외 처리하는 기능입니다. 생성자를 사용하여 LottoGenerator 함수를 사용하기 전에 미리 예외 사항을 체크하도록 하였습니다.

--- src/main/java/Domain/LottoGenerator.java
+++ src/main/java/Domain/LottoGenerator.java
@@ -7,6 +7,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
+    private String price;
+
+    public LottoGenerator(String price) {
+        haveOnlyNumbers(price);
+        divideByThousand(price);
+        this.price = price;
+    }
 
     public List<Integer> createLottoNumbers() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
@@ -19,6 +26,12 @@ public class LottoGenerator {
         final String numberRegex = "[0-9]+";
         if (!price.matches(numberRegex)) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자만 입력하세요");
+        }
+    }
+
+    private void divideByThousand(String price) {
+        if (Integer.parseInt(price) % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액을 1000원 단위로 입력하세요.");
         }
     }
 
