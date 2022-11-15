14286ee05791cb4f9b50459608e9496e25f1d252
ohhonggi <5ghdrl@naver.com>
2022-11-15 20:28:03 +0900

    feat($Input): Add inputWinningNumber function
    This function receives a Winning number from the user.
    
    Closes feat_5

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -4,16 +4,31 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Amounts;
 
-import static lotto.util.ValidUtil.validAmountUnit;
-import static lotto.util.ValidUtil.validInputAmounts;
+import java.util.LinkedList;
+import java.util.List;
+
+import static lotto.util.ValidUtil.*;
 
 public class Input {
 
     public static Amounts inputPurchasingAmount() {
-        System.out.println("구입 금액을 입력해 주세요");
-        String input = validInputAmounts(Console.readLine());
+        System.out.println("구입 금액을 입력해 주세요.");
+        String input = validNumber(Console.readLine());
         int amount = validAmountUnit(Integer.parseInt(input));
         return new Amounts(amount);
+    }
+
+    public static List<Integer> inputWinningNumber() {
+        System.out.println("당첨 번호를 입력해주세요.");
+        String[] numbers = validAllWinningNumber(Console.readLine());
+
+        List<Integer> winningAndBonusNumber = new LinkedList<>();
+        for (String number : numbers) {
+            winningAndBonusNumber.add(Integer.parseInt(number));
+        }
+
+        validateDuplication(winningAndBonusNumber);
+        return winningAndBonusNumber;
     }
 
 }
