edd96b2a5a78876ff222a79008f33597e94e2a3d
kimsh2948 <shkim2948@gmail.com>
2022-11-15 20:39:32 +0900

    refactor: 당첨 통계 출력 메세지 enum으로 적용

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,21 +1,23 @@
 package lotto.domain;
 
 public enum LottoRank {
-    FIFTH(3, 0, 5_000),
-    FORTH(4, 0, 50_000),
-    THIRD(5, 0, 1_500_000),
-    SECOND(5, 1, 30_000_000),
-    FIRST(6, 0, 2_000_000_000),
-    MISS(0, 0, 0);
+    FIFTH(3, 0, 5_000, "3개 일치 (5,000원) - "),
+    FORTH(4, 0, 50_000, "4개 일치 (50,000원) - "),
+    THIRD(5, 0, 1_500_000, "5개 일치 (1,500,000원) - "),
+    SECOND(5, 1, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    FIRST(6, 0, 2_000_000_000, "6개 일치 (2,000,000,000원) - "),
+    MISS(0, 0, 0, "낙첨");
 
     private final int count;
     private final int bonusCount;
     private final long rewardMoney;
+    private final String message;
 
-    LottoRank(int count, int bonusCount, long rewardMoney) {
+    LottoRank(int count, int bonusCount, long rewardMoney, String message) {
         this.count = count;
         this.bonusCount = bonusCount;
         this.rewardMoney = rewardMoney;
+        this.message = message;
     }
 
     public static LottoRank getLottoRank(final int matchCount, final int bonusCount) {
@@ -33,5 +35,9 @@ public enum LottoRank {
 
     public long getRewardMoney() {
         return rewardMoney;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,11 +10,6 @@ public class OutputView {
 
     private static final String BUY_LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
     private static final String WIN_STATISTICS_MESSAGE = "당첨 통계";
-    private static final String THREE_SAME_NUMBER_MESSAGE = "3개 일치 (5,000원) - ";
-    private static final String FOUR_SAME_NUMBER_MESSAGE = "4개 일치 (50,000원) - ";
-    private static final String FIVE_SAME_NUMBER_MESSAGE = "5개 일치 (1,500,000원) - ";
-    private static final String FIVE_AND_BONUS_SAME_NUMBER_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
-    private static final String SIX_SAME_NUMBER_MESSAGE = "6개 일치 (2,000,000,000원) - ";
 
     public static void buyLottoCount(int count) {
         System.out.println(count + BUY_LOTTO_COUNT_MESSAGE);
@@ -29,11 +24,11 @@ public class OutputView {
     public static void winStatistics(Map<LottoRank, Integer> winStat) {
         System.out.println(WIN_STATISTICS_MESSAGE);
         System.out.println("---");
-        System.out.println(THREE_SAME_NUMBER_MESSAGE + winStat.get(LottoRank.FIFTH) + "개");
-        System.out.println(FOUR_SAME_NUMBER_MESSAGE + winStat.get(LottoRank.FORTH) + "개");
-        System.out.println(FIVE_SAME_NUMBER_MESSAGE + winStat.get(LottoRank.THIRD) + "개");
-        System.out.println(FIVE_AND_BONUS_SAME_NUMBER_MESSAGE + winStat.get(LottoRank.SECOND) + "개");
-        System.out.println(SIX_SAME_NUMBER_MESSAGE + winStat.get(LottoRank.FIRST) + "개");
+        System.out.println(LottoRank.FIFTH.getMessage() + winStat.get(LottoRank.FIFTH) + "개");
+        System.out.println(LottoRank.FORTH.getMessage() + winStat.get(LottoRank.FORTH) + "개");
+        System.out.println(LottoRank.THIRD.getMessage() + winStat.get(LottoRank.THIRD) + "개");
+        System.out.println(LottoRank.SECOND.getMessage() + winStat.get(LottoRank.SECOND) + "개");
+        System.out.println(LottoRank.FIRST.getMessage() + winStat.get(LottoRank.FIRST) + "개");
     }
 
     public static void showRating(double rating) {
