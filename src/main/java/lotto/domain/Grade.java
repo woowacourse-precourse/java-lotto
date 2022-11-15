5255d66923b513a8a4531d20e6df0d41342ce2b3
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-15 19:43:40 +0900

    feat(LottoGame): Grade에 일치한 갯수별 금액 지정

--- src/main/java/lotto/domain/Grade.java
+++ src/main/java/lotto/domain/Grade.java
@@ -1,4 +1,22 @@
 package lotto.domain;
 
-public class Grade {
+public enum Grade {
+
+    FIFTH("3",3, 5_000),
+    FOURTH("4",4, 50_000),
+    THIRD("5",5, 1_500_000),
+    SECOND("5+B",5, 30_000_000),
+    FIRST("6",6, 2_000_000_000);
+
+    private final int matchCount;
+    private final String key;
+    private final int prizeMoney;
+
+
+    Grade(String key, int matchCount, int prizeMoney) {
+        this.key = key;
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+    }
+
 }
