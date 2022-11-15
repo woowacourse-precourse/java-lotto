7a6ecbf05a487c8434445e47002129347c9843b3
p <gustjr2332@naver.com>
2022-11-15 19:02:14 +0900

    feat(LottoNumberException) : 보너스 번호가 당첨 번호에 이미 있을 경우 예외처리 추가, 당첨 번호와 보너스 번호 예외처리 분리

--- src/main/java/Exception/LottoNumberException.java
+++ src/main/java/Exception/LottoNumberException.java
@@ -1,18 +1,31 @@
 package Exception;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class LottoNumberException {
     private static final String NOTNUMBER_ERRORMESSAGE = "[ERROR] 숫자만 입력해주세요.";
     private static final String OUTOFRANGE_ERRORMESSAGE = "[ERROR] 1~45사이의 숫자만 입력해주세요.";
+    private static final String WINNINGNUMBERCONTAINBONUSNUMBER_ERROMESSAGE = "[ERROR] 당첨번호에 보너스 번호가 포함되어 있습니다.";
 
-    public static void checkException(String number) {
-
-        if (!Pattern.matches("^[1-9]*$", String.valueOf(number))) {
+    public static void checkWinningNumberException(String number) {
+        if (!Pattern.matches("^[0-9]*$", String.valueOf(number))) {
             throw new IllegalArgumentException(NOTNUMBER_ERRORMESSAGE);
         }
         if (Integer.valueOf(number) < 1 || Integer.valueOf(number) > 45) {
             throw new IllegalArgumentException(OUTOFRANGE_ERRORMESSAGE);
+        }
+    }
+
+    public static void checkBonusNumberException(String number, List<Integer> WinningNumbers) {
+        if (!Pattern.matches("^[0-9]*$", String.valueOf(number))) {
+            throw new IllegalArgumentException(NOTNUMBER_ERRORMESSAGE);
+        }
+        if (Integer.valueOf(number) < 1 || Integer.valueOf(number) > 45) {
+            throw new IllegalArgumentException(OUTOFRANGE_ERRORMESSAGE);
+        }
+        if (WinningNumbers.contains(Integer.valueOf(number))) {
+            throw new IllegalArgumentException(WINNINGNUMBERCONTAINBONUSNUMBER_ERROMESSAGE);
         }
     }
 }
