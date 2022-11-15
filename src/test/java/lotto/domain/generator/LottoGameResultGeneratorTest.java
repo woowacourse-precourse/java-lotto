94fa85f1254cde615453ea97528ce751e2bd6abd
Cho SangHyun <98shcho@naver.com>
2022-11-15 19:10:02 +0900

    test(결과 생성하기): 겹치는 수에 따른 상금 구하기 기능 테스트 추가

--- src/test/java/lotto/domain/generator/LottoGameResultGeneratorTest.java
+++ src/test/java/lotto/domain/generator/LottoGameResultGeneratorTest.java
@@ -9,21 +9,22 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static lotto.domain.Rank.FIRST;
+import static lotto.domain.Rank.SECOND;
+import static lotto.domain.Rank.THIRD;
+import static lotto.domain.Rank.FOURTH;
+import static lotto.domain.Rank.FIFTH;
 
 public class LottoGameResultGeneratorTest {
-    LottoGameResultGenerator lottoGameResultGenerator;
+    LottoGameResultGenerator lottoGameResultGenerator = new LottoGameResultGenerator();
 
     @Nested
-    class WinningCountTest {
-        @BeforeEach
-        void initGenerator() {
-            lottoGameResultGenerator = new LottoGameResultGenerator();
-        }
+    class WinningCountCalculatingTest {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
         @DisplayName("당첨번호가 하나도 안 겹치는 경우")
         @Test
         void case1() {
-            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
             WinningNumbers winningNumbers = new WinningNumbers(List.of(7, 8, 9, 10, 11, 12));
 
             int winningCount = lottoGameResultGenerator.calculateWinningCount(lotto, winningNumbers);
@@ -34,7 +35,6 @@ public class LottoGameResultGeneratorTest {
         @DisplayName("당첨번호가 하나만 겹치는 경우")
         @Test
         void case2() {
-            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
             WinningNumbers winningNumbers = new WinningNumbers(List.of(6, 7, 8, 9, 10, 11));
 
             int winningCount = lottoGameResultGenerator.calculateWinningCount(lotto, winningNumbers);
@@ -44,7 +44,6 @@ public class LottoGameResultGeneratorTest {
         @DisplayName("당첨번호가 두 개 겹치는 경우")
         @Test
         void case3() {
-            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
             WinningNumbers winningNumbers = new WinningNumbers(List.of(5, 6, 7, 8, 9, 10));
 
             int winningCount = lottoGameResultGenerator.calculateWinningCount(lotto, winningNumbers);
@@ -55,7 +54,6 @@ public class LottoGameResultGeneratorTest {
         @DisplayName("당첨번호가 세 개 겹치는 경우")
         @Test
         void case4() {
-            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
             WinningNumbers winningNumbers = new WinningNumbers(List.of(4, 5, 6, 7, 8, 9));
 
             int winningCount = lottoGameResultGenerator.calculateWinningCount(lotto, winningNumbers);
@@ -66,7 +64,6 @@ public class LottoGameResultGeneratorTest {
         @DisplayName("당첨번호가 네 개 겹치는 경우")
         @Test
         void case5() {
-            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
             WinningNumbers winningNumbers = new WinningNumbers(List.of(3, 4, 5, 6, 7, 8));
 
             int winningCount = lottoGameResultGenerator.calculateWinningCount(lotto, winningNumbers);
@@ -77,7 +74,6 @@ public class LottoGameResultGeneratorTest {
         @DisplayName("당첨번호가 다섯 개 겹치는 경우")
         @Test
         void case6() {
-            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
             WinningNumbers winningNumbers = new WinningNumbers(List.of(2, 3, 4, 5, 6, 7));
 
             int winningCount = lottoGameResultGenerator.calculateWinningCount(lotto, winningNumbers);
@@ -88,12 +84,82 @@ public class LottoGameResultGeneratorTest {
         @DisplayName("당첨번호가 여섯 개 겹치는 경우")
         @Test
         void case7() {
-            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
             WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6));
 
             int winningCount = lottoGameResultGenerator.calculateWinningCount(lotto, winningNumbers);
 
             assertThat(winningCount).isEqualTo(6);
+        }
+    }
+
+    @Nested
+    class PrizeMoneyGettingTest {
+        WinningNumbers winningNumbers;
+
+        @BeforeEach
+        void initWinningNumbers() {
+            winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6));
+            winningNumbers.registerBonusNumber(7);
+        }
+
+        @DisplayName("6개가 일치할 때 1등상금을 가져오는지 테스트")
+        @Test
+        void case1() {
+             Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+             int winningCount = 6;
+
+             int prizeMoney = lottoGameResultGenerator.
+                     getPrizeMoneyByWinningCount(lotto, winningNumbers, winningCount);
+
+             assertThat(prizeMoney).isEqualTo(FIRST.prizeMoney());
+        }
+
+        @DisplayName("5개가 일치하고 보너스번호가 일치할 때 2등상금을 가져오는지 테스트")
+        @Test
+        void case2() {
+            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+            int winningCount = 5;
+
+            int prizeMoney = lottoGameResultGenerator.
+                    getPrizeMoneyByWinningCount(lotto, winningNumbers, winningCount);
+
+            assertThat(prizeMoney).isEqualTo(SECOND.prizeMoney());
+        }
+
+        @DisplayName("5개가 일치하지만 보너스번호가 불일치할 때 3등상금을 가져오는지 테스트")
+        @Test
+        void case3() {
+            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
+            int winningCount = 5;
+
+            int prizeMoney = lottoGameResultGenerator.
+                    getPrizeMoneyByWinningCount(lotto, winningNumbers, winningCount);
+
+            assertThat(prizeMoney).isEqualTo(THIRD.prizeMoney());
+        }
+
+        @DisplayName("4개가 일치할 때 4등상금을 가져오는지 테스트")
+        @Test
+        void case4() {
+            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 7, 8));
+            int winningCount = 4;
+
+            int prizeMoney = lottoGameResultGenerator.
+                    getPrizeMoneyByWinningCount(lotto, winningNumbers, winningCount);
+
+            assertThat(prizeMoney).isEqualTo(FOURTH.prizeMoney());
+        }
+
+        @DisplayName("3개가 일치할 때 5등상금을 가져오는지 테스트")
+        @Test
+        void case5() {
+            Lotto lotto = new Lotto(List.of(1, 2, 3, 7, 8, 9));
+            int winningCount = 3;
+
+            int prizeMoney = lottoGameResultGenerator.
+                    getPrizeMoneyByWinningCount(lotto, winningNumbers, winningCount);
+
+            assertThat(prizeMoney).isEqualTo(FIFTH.prizeMoney());
         }
     }
 }
