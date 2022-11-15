2acbb023e899158a6be11b71a7a32f067001da92
GilHun <suleemon1@naver.com>
2022-11-15 22:59:19 +0900

    feat(Application): 에러발생시 에러내용 출력하면서 프로그램 종료하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import lotto.domain.Buyer;
+import lotto.domain.ResultPresenter;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try{
+            Buyer buyer = new Buyer();
+            buyer.buy();
+
+            ResultPresenter myResult = new ResultPresenter();
+            myResult.input();
+
+            buyer.checkResult(myResult);
+            buyer.printLottoResult();
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return;
+        }
+
     }
 }
