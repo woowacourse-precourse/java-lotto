e09281acfd69cc2a385a5a55d9887bb3c76b9926
sunghyuki <sunghyuk1609@gmail.com>
2022-11-15 15:59:55 +0900

    test : Lotto 생성 기능 테스트 추가 - 로또 번호에 중복이 존재하지 않는지 확인하는 테스트를 위해 validateLottoIsUnique 메서드 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,14 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Lotto {
-    private static final String LOTTO_NUMBER_RANGE_MISMATCH_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-    private static final String LOTTO_SIZE_MISMATCH_MESSAGE = "[ERROR] 로또 한 장의 번호 개수는 6개입니다.";
-    private static final String LOTTO_IS_NOT_UNIQUE_MESSAGE ="[ERROR] 로또 번호에 중복이 존재할 수 없습니다.";
+    public static final String LOTTO_NUMBER_RANGE_MISMATCH_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String LOTTO_SIZE_MISMATCH_MESSAGE = "[ERROR] 로또 한 장의 번호 개수는 6개입니다.";
+    public static final String LOTTO_IS_NOT_UNIQUE_MESSAGE ="[ERROR] 로또 번호에 중복이 존재할 수 없습니다.";
     public static final int MIN_VALUE = 1;
     public static final int MAX_VALUE = 45;
     private static final int LOTTO_SIZE = 6;
@@ -26,10 +27,12 @@ public class Lotto {
     }
 
     private void validateLottoIsUnique(List<Integer> numbers) {
-        Set<Integer> lottoNumberSet = new HashSet<>(numbers);
-
-        if (lottoNumberSet.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException(LOTTO_IS_NOT_UNIQUE_MESSAGE);
+        List<Integer> lottoNumbers = new ArrayList<>();
+        for (int number : numbers) {
+            if (lottoNumbers.contains(number)) {
+                throw new IllegalArgumentException(LOTTO_IS_NOT_UNIQUE_MESSAGE);
+            }
+            lottoNumbers.add(number);
         }
     }
 
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -12,16 +12,31 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Lotto.LOTTO_SIZE_MISMATCH_MESSAGE);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Lotto.LOTTO_IS_NOT_UNIQUE_MESSAGE);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 45보다 크면 예외가 발생한다.")
+    @Test
+    void createLottoByOverLottoNumberRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Lotto.LOTTO_NUMBER_RANGE_MISMATCH_MESSAGE);
+    }
+
+    @DisplayName("로또 번호가 1보다 작으면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderLottoNumberRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Lotto.LOTTO_NUMBER_RANGE_MISMATCH_MESSAGE);
+    }
 }
