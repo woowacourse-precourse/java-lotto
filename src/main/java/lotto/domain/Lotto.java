7ecc0b3cdd71fe47c5c1aad2b43354e5e30c8b20
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-15 15:07:08 +0900

    feat: 중복된 로또 번호 입력 시 오류 발생 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,34 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
+
+    private static final String LOTTO_INVALID_SIZE_ERROR = "[ERROR] 로또 번호가 6자리가 아닙니다.";
+    private static final String LOTTO_DUPLICATED_SIZE_ERROR = "[ERROR] 로또 번호가 중복되었습니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
+        validate();
+        validateDuplicate();
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate() {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LOTTO_INVALID_SIZE_ERROR);
         }
+    }
+
+    private void validateDuplicate() {
+        HashSet<Integer> lotto = new HashSet<>(numbers);
+        if (lotto.size() != 6) {
+            throw new IllegalArgumentException(LOTTO_DUPLICATED_SIZE_ERROR);
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 }
