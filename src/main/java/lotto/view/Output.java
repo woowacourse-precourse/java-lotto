6e865f4cda36ab9e97b986016fd412f7fd5423fa
mikekang47 <gyeong0308@gmail.com>
2022-11-15 19:53:36 +0900

    Feat<Output>: 사용자 당첨 정보를 출력하는 메서드 추가

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,9 +1,13 @@
 package lotto.view;
 
 import lotto.domains.Lotto;
+import lotto.domains.Place;
 import lotto.utils.LottoNumberConverter;
 
 import java.util.List;
+import java.util.Map;
+
+import static lotto.domains.Place.*;
 
 public class Output {
     private Output() {
@@ -19,5 +23,16 @@ public class Output {
                 .map(Lotto::getNumbers)
                 .map(LottoNumberConverter::convertToString)
                 .forEach(lottoNumberStr -> System.out.println("[" + lottoNumberStr + "]"));
+    }
+
+    public static void printUserHitInformation(Map<Place, Integer> prizeMap) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + prizeMap.get(FIFTH) + "개");
+        System.out.println("4개 일치 (50,000원) - " + prizeMap.get(FOURTH) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + prizeMap.get(THIRD) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + prizeMap.get(SECOND) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + prizeMap.get(FIRST) + "개");
     }
 }
