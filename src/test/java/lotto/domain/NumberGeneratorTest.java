7c723416e7f485aa5dacf559d6ca59a9f3084025
Yejin Shin <shinyejin0212@gmail.com>
2022-11-15 21:35:23 +0900

    test(NumberGeneratorTest): 랜덤 생성 로또 개수 테스트

--- src/test/java/lotto/domain/NumberGeneratorTest.java
+++ src/test/java/lotto/domain/NumberGeneratorTest.java
@@ -1,2 +1,22 @@
-package lotto.domain;public class NumberGeneratorTest {
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import lotto.view.InputView;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NumberGeneratorTest {
+    @DisplayName("구입 금액에 따른 개수만큼 로또 번호를 랜덤 생성한다.")
+    @Test
+    void createRandomLottoBasedOnPurchasePrice(){
+        Integer money = 8000;
+        Buyer buyer = new Buyer(money);
+        NumberGenerator lottogenerator = new NumberGenerator();
+        Integer NumberOfLotto = buyer.getNumOfLotto();
+        LottoGroup lottoGroup = new LottoGroup(lottogenerator.generateLotto(NumberOfLotto));
+        assertThat(lottoGroup.getLottoGroup().size()).isEqualTo(8);
+    }
 }
