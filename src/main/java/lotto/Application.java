ba3f400fae2e6059186084076eb495b649fd891c
show2891 <show2891@gmail.com>
2022-11-15 23:11:33 +0900

    구입 금액 입력 받기_구현 완료 / 당첨번호 입력 받기_구현 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import java.util.Scanner;
+
 public class Application {
+
+    static String getMoney() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        Scanner input = new Scanner(System.in);
+        String userInput = input.nextLine();
+        return userInput;
+    }
+    static String[] getWinNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        Scanner input = new Scanner(System.in);
+        String userInput = input.nextLine();
+        String[] winNumbers = userInput.split(",");
+        return winNumbers;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String[] winNumbers = getWinNumbers();
+        String Money = getMoney();
+
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Lotto {
