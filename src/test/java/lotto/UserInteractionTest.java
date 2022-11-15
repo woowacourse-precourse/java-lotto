527c8e0e0a32ec6289b6ef988fbf6a8cfc9477eb
poter0501 <poter0501@gmail.com>
2022-11-15 20:00:18 +0900

    test(UserInteraction): 테스트 코드에서 검사하는 예외를 변경
    테스트 코드에서 assertThatThrownBy 가 잡아내는 예외를
    IllegalArgumentException 에서 NoSuchElementException로 변경

--- src/test/java/lotto/UserInteractionTest.java
+++ src/test/java/lotto/UserInteractionTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,14 +23,14 @@ class UserInteractionTest {
     @Test
     void tryParseInt_exception_test_1() {
         assertThatThrownBy(() -> testUserInteraction.tryParseInt("800 0"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(NoSuchElementException.class);
     }
 
     @DisplayName("(tryParseInt) 입력 받은 문자열을 정수형으로 변환 시도2")
     @Test
     void tryParseInt_exception_test_2() {
         assertThatThrownBy(() -> testUserInteraction.tryParseInt("1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(NoSuchElementException.class);
     }
 
     @DisplayName("(tryParseInt) 의도한 입력이 들어온 경우, 정수형으로 변환이 이루어지는지 확인")
@@ -44,7 +45,7 @@ class UserInteractionTest {
     @Test
     void tryParseIntegers_exception_test() {
         assertThatThrownBy(() -> testUserInteraction.tryParseInt("1,a,3"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(NoSuchElementException.class);
     }
 
     @DisplayName("(tryParseIntegers) 의도한 입력이 들어온 경우, 정수형으로 변환이 이루어지는지 확인")
@@ -59,20 +60,20 @@ class UserInteractionTest {
     @Test
     void checkNumbers_exception_test_1() {
         assertThatThrownBy(() -> testUserInteraction.checkNumbers(List.of(1, 1, 1, 4, 5, 6)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(NoSuchElementException.class);
     }
 
     @DisplayName("(checkNumbers) 1~45 사이를 벗어나는 숫자를 입력으로 받은 경우 에러 확인")
     @Test
     void checkNumbers_exception_test_2() {
         assertThatThrownBy(() -> testUserInteraction.checkNumbers(List.of(-1, 1, 1, 4, 5, 46)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(NoSuchElementException.class);
     }
 
     @DisplayName("(checkBonusNumber) 당첨 번호와 보너스 번호가 중복되는 경우 에러 확인")
     @Test
     void checkBonusNumber_exception_test() {
         assertThatThrownBy(() -> testUserInteraction.checkBonusNumber(List.of(1, 2, 3, 4, 5, 6), 2))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
