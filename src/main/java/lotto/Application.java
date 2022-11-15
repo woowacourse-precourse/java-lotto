af075354d8fa2b43b0ae3e101a9ee290df364f18
minje <alswp0910@naver.com>
2022-11-15 11:47:27 +0900

    feat: 로또 기능 구현 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,40 @@
 package lotto;
 
+import lotto.domain.Computer;
+import lotto.domain.Lucky;
+import lotto.domain.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // TODO: 프로그램 구현
+        game();
+    }
+
+    public static void game() {
+        User user = new User();
+        int price = user.inputPrice();
+        int count = user.countLotto(price);
+        List<List<Integer>> userLotto = new ArrayList<>(user.showLotto(count));
+        System.out.println();
+
+        Computer com = new Computer();
+        String str = com.inputNumbers();
+        String[] arr = new String[6];
+        arr = com.splitNumbers(str);
+        List<Integer> list = new ArrayList<>(com.checkNumbers(arr));
+        com.checkDuplication(list);
+        System.out.println();
+
+        int bns = com.bonusNumber();
+        System.out.println();
+        Lucky lucky = new Lucky();
+        List<Integer> res = new ArrayList<>(lucky.compareNumbers(userLotto, list, bns));
+        HashMap<Integer, Integer> map = new HashMap<>(lucky.compareResult(res));
+        lucky.showResult(map);
+        lucky.showProfit(lucky.profitResult(map, price));
     }
 }
