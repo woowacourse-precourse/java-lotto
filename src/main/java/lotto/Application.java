b838886a2e6a24ab8a9699de3cf7c2626a0f7324
MJ Choi <1621139@gmail.com>
2022-11-15 20:40:37 +0900

    feature controller

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoTicket;
+import lotto.domain.LottoShop;
+import lotto.domain.WinningLotto;
+import lotto.dto.RoundResult;
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int money = InputView.receivePurchaseMoney();
+        LottoTicket lotteryTicket = LottoShop.buyLottos(money);
+        ResultView.printLottoNumbers(lotteryTicket.exportLottoNumbers());
+
+        Lotto winningNumbers = new Lotto(InputView.getListOfInteger());
+        int bonusNumber = InputView.receiveBonusNumber();
+        WinningLotto winningLottery = new WinningLotto(winningNumbers, bonusNumber);
+
+        RoundResult roundResult = lotteryTicket.getResult(winningLottery);
+        ResultView.printResult(roundResult);
     }
 }
