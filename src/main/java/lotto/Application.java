038190ae5b44956b92449785ae7f4ff514faa7d8
hyejuc <chjjudy@naver.com>
2022-11-15 23:24:15 +0900

    feat: 게임 실행 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import static lotto.ConsoleManager.playGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        playGame();
     }
 }
