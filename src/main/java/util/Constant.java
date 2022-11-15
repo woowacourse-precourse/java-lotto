6a0db1e954e810acb9dd6365368c018b22f65800
SeokHwan An <asok369258@naver.com>
2022-11-15 09:29:36 +0900

    feat : 당첨 결과를 출력하는 기능 추가

--- src/main/java/util/Constant.java
+++ src/main/java/util/Constant.java
@@ -13,6 +13,8 @@ public class Constant {
     public static final String LOTTO_PURCHASE_REGEX = "^[0-9]*$";
     public static final String ROUND_SECOND_DECIMAL_PLACE = "%.1f";
     public static final String PERCENT = "100.0";
+    public static final String DECIMAL_FORMAT = "###,###";
+    public static final String RANK_SECOND = "SECOND";
 
     public static final String ERROR_MSG_SAME_NUMBER = "[ERROR] 서로 다른 숫자로 구성되어 있지 않습니다.";
     public static final String ERROR_MSG_OTHER_NUMBER_BETWEEN_1_AND_45 = "[ERROR] 로또 번호가 1과 45사이의 수로 구성되어 있지 않습니다.";
@@ -25,4 +27,7 @@ public class Constant {
     public static final String ERROR_MSG_PURCHASE_MONEY_CONSIST_OF_NUM = "[ERROR] 로또 구입 금액은 숫자여야 합니다.";
 
     public static final String PURCHASE_LOTTO_TICKETS_MSG = "%n%d개를 구매했습니다.%n";
+    public static final String WINNING_STATS_MSG = "\n당첨 통계\n---";
+    public static final String WINNING_STATS_RANK_SECOND_MSG = "%d개 일치, 보너스 볼 일치 (%s원) - %d개%n";
+    public static final String WINNING_STATS_OTHER_RANK_MSG = "%d개 일치 (%s원) - %d개%n";
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,7 +2,10 @@ package view;
 
 import static util.Constant.*;
 
+import java.text.DecimalFormat;
 import lotto.Lottos;
+import lotto.Rank;
+import lotto.Result;
 
 public class OutputView {
 
@@ -10,5 +13,20 @@ public class OutputView {
     public static void printPerchasedLottoNumber(int money, Lottos lottos) {
         System.out.printf(PURCHASE_LOTTO_TICKETS_MSG, money / LOTTO_PURCHASE_COST);
         lottos.getLottos().forEach(lotto -> System.out.println(lotto.getNumbers().toString()));
+    }
+
+    // 당첨 결과를 출력하는 기능
+    public static void printWinningStats(Result results) {
+        System.out.println(WINNING_STATS_MSG);
+        DecimalFormat formatter = new DecimalFormat(DECIMAL_FORMAT);
+        for (Rank rank : results.getResult().keySet()) {
+            if (rank.name().equals(RANK_SECOND)) {
+                System.out.printf(WINNING_STATS_RANK_SECOND_MSG, rank.getCorrectCount(),
+                    formatter.format(rank.getPrize()), results.getResult().get(rank));
+                continue;
+            }
+            System.out.printf(WINNING_STATS_OTHER_RANK_MSG, rank.getCorrectCount(),
+                formatter.format(rank.getPrize()), results.getResult().get(rank));
+        }
     }
 }
