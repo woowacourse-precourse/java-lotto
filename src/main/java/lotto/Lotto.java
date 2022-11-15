3e6ca138ed0e611e2f5aa766b1b30303c90a1916
dlgosla <dlgoslaek1@gmail.com>
2022-11-15 23:17:40 +0900

    feat: 로또 번호들이 int가 아닌 로또 번호 객체를 가지도록 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,19 +1,27 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+
+    private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+        this.numbers = toLottoNumbers(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    public List<LottoNumber> getNumbers() {
+        return numbers;
+    }
+
+    public List<LottoNumber> toLottoNumbers(List<Integer> numbers) {
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        for (int number : numbers) {
+            lottoNumbers.add(new LottoNumber(number));
         }
+        return lottoNumbers;
     }
 
     // TODO: 추가 기능 구현
