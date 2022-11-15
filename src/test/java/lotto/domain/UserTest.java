f66f871ef3b9575812bc5361f89db925b55ef0ef
SeongukBaek <bsu6235@gmail.com>
2022-11-15 12:11:06 +0900

    style: unify DisplayName

--- src/test/java/lotto/domain/UserTest.java
+++ src/test/java/lotto/domain/UserTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UserTest {
-    @DisplayName("구입 금액이 1000원 단위가 아니라면 예외를 반환한다.")
+    @DisplayName("구입 금액이 1000원 단위가 아닌 경우 예외가 발생한다.")
     @Test
     void validateTest_1000원단위가_아닌경우() {
         Assertions.assertThatThrownBy(
