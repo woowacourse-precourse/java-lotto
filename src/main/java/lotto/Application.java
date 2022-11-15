e1341ae2e89c3f288c5690718c8e89a178cf2549
hyunho12 <hyundho12@gmail.com>
2022-11-15 23:14:39 +0900

    feat: 애플리케이션 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Input input = new Input();
+        LottoTicket lottoTicket = new LottoTicket();
+        LottoSeller lottoSeller = new LottoSeller();
+
+        int ticketMoney = input.inputPurchaseAmount();
+        lottoTicket.buyTicket(lottoSeller, ticketMoney);
+
+        List<Integer> winningNumber = input.inputWinningNumbers();
+        Lotto winningNumbers = new Lotto(winningNumber);
+        int bonusNumber = input.inputBonusNumber();
+        LottoValidate lottoValidate = new LottoValidate(winningNumbers, bonusNumber);
+
+        LottoRankCheck lottoRankCheck = new LottoRankCheck();
+
     }
 }
