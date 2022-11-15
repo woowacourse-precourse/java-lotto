20bac9607d5a7a51675d2fc143027c9c17c91af6
JeonSangYong <tkddyd420@naver.com>
2022-11-15 13:57:19 +0900

    refactor : 보너스 번호 일치 여부 추가

--- src/main/java/lotto/util/Rank.java
+++ src/main/java/lotto/util/Rank.java
@@ -3,32 +3,32 @@ package lotto.util;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    EMPTY(0, 0);
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    EMPTY(0, false, 0);
 
     private final int matchCount;
+    private final boolean matchBonus;
     private final int price;
 
-    Rank(int matchCount, int price) {
+    Rank(int matchCount, boolean matchBonus, int price) {
         this.matchCount = matchCount;
+        this.matchBonus = matchBonus;
         this.price = price;
     }
 
-    public int getMatchCount() {
-        return matchCount;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public static Rank fromCount(long count) {
+    public static Rank fromCount(long count, boolean matchBonus) {
         return Arrays.stream(Rank.values())
-                .filter(rank -> rank.matchCount == count)
+                .filter(rank -> {
+                    if (count == 5) {
+                        return rank.matchCount == count && rank.matchBonus == matchBonus;
+                    }
+
+                    return rank.matchCount == count;
+                })
                 .findFirst()
                 .orElseGet(Rank::makeEmpty);
     }
