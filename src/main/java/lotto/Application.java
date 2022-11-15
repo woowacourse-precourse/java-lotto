fbe20b10616435722392966c3c190a36ba4e693a
hdsung98 <hdsung98@gmail.com>
2022-11-15 18:20:08 +0900

    feat: create buyLottos method in Application
    로또 구매 및 그 결과 표시를 담당하는 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,23 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import domain.LottoFactory;
+import domain.Lottos;
+import dto.LottosDto;
+import view.InputUtil;
+import view.OutputUtil;
 
 import java.util.ArrayList;
 
 public class Application {
+    public static void buyLottos(){
+        OutputUtil.noticePurchase();
+        Integer purchase = InputUtil.inputNumber();
+        Lottos lottos = LottoFactory.makeLottos(purchase);
+        LottosDto lottosDto = lottos.sendDto();
+        OutputUtil.showPurchaseResult(lottosDto);
+    }
     public static void main(String[] args) {
-
+        buyLottos();
     }
 }
