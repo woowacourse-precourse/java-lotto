ad5d204c1d00d6d31dd1cf5a5558a9065bfb5e99
elizabethgim <yujingim@kookmin.ac.kr>
2022-11-15 11:05:29 +0900

    feat: 입력된 금액이 1000원보다 작은지 확인

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,13 @@
 package lotto;
 
 public class Application {
-
+    private static void CheckSmallMoney(int money){
+        if(money % 1000 != 0){
+            throw new IllegalArgumentException("로또는 1000원 단위로 구매가 가능합니다.");
+        }
+    }
     public static int BuyLotto(int money){
+        CheckSmallMoney(money);
         int paper = money / 1000;
         System.out.println(paper);
         return paper;
