bc1d81220ebeb44fab2f267407e4ea0fa22068b2
korjun1993 <corojoon93@naver.com>
2022-11-15 23:29:44 +0900

    feat(WinLotto) : 두 로또의 발행 번호 간 공통 번호를 알아내는 기능 추가

--- src/main/java/lotto/WinLotto.java
+++ src/main/java/lotto/WinLotto.java
@@ -2,6 +2,7 @@ package lotto;
 
 import lotto.exception.MyIllegalArgumentException;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import static lotto.exception.ErrorCode.BONUS_NUMBER_ERROR;
@@ -20,5 +21,15 @@ public class WinLotto {
         if (winNumbers.contains(bonusNumber)) {
             throw new MyIllegalArgumentException(BONUS_NUMBER_ERROR);
         }
+    }
+
+    public List<Integer> compare(Lotto userLotto) {
+        List<Integer> common = new LinkedList<>();
+        for (int number : userLotto.getNumbers()) {
+            if (lotto.getNumbers().contains(number)) {
+                common.add(number);
+            }
+        }
+        return common;
     }
 }
