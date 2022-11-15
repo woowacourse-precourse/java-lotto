b41d83cfc33ca1be1952868a2a21be3593495cc1
DaWoon Lee <qkgk61@gmail.com>
2022-11-15 23:14:58 +0900

    Test : PaymentValidator 클래스에서 지불한 금액과 로또의 가격 사이의 관계를 검증하는 메서드 validatePayment 테스트

--- src/test/java/lotto/validator/PaymentValidatorTest.java
+++ src/test/java/lotto/validator/PaymentValidatorTest.java
@@ -2,6 +2,7 @@ package lotto.validator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import lotto.domain.LottoInfo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +13,22 @@ public class PaymentValidatorTest {
     void validateNumberFormat() {
         String payment = "ass12";
         assertThatThrownBy(() -> PaymentValidator.validateNumberFormat(payment))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("지불한 금액이 로또를 구매하기에 충분하지 않은 경우 예외 발생")
+    @Test
+    void validatePayment_LowerPrice() {
+        long payment = 0;
+        assertThatThrownBy(() -> PaymentValidator.validatePayment(payment, LottoInfo.PRICE.value()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("지불한 금액으로 로또를 구매하고 거스름돈이 있는 경우 예외 발생")
+    @Test
+    void validatePayment_Indivisible() {
+        long payment = LottoInfo.PRICE.value() + 1;
+        assertThatThrownBy(() -> PaymentValidator.validatePayment(payment, LottoInfo.PRICE.value()))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
