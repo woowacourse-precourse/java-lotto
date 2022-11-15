f6e04c32ddaa194b5acd1d5635dbdd6f2b763efc
tjwjdgus12 <01048733438a@gmail.com>
2022-11-15 13:25:08 +0900

    fix: Lotto 생성시 stream을 통해 정렬하도록 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,15 +1,16 @@
 package lotto;
 
-import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        numbers.sort(Comparator.naturalOrder());
-        this.numbers = numbers;
+        this.numbers = numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     private void validate(List<Integer> numbers) {
