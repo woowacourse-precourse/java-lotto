2512ddd21f529e13649f2d378bad8848b1fa723f
twoosky <lhjae3@naver.com>
2022-11-15 21:06:29 +0900

    feat(view): 로또 결과 출력 메소드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -23,4 +23,31 @@ public class OutputView {
         System.out.println(OUTPUT_LOTTO_DONE);
         System.out.println(OUTPUT_LINE);
     }
+
+    public static void printLottoResult(LottoResults results) {
+        Arrays.stream(LottoResult.values())
+            .filter(result -> result.getMatchCount() != 0)
+            .forEach(result -> {
+                int count = getLottoMatchCount(results, result);
+                printResult(result, count);
+            });
+    }
+
+    private static int getLottoMatchCount(LottoResults results, LottoResult result) {
+        return (int) results.getResults().stream()
+                .filter(result::equals)
+                .count();
+    }
+
+    private static void printResult(LottoResult result, int count) {
+        int lottoMatchCount = result.getMatchCount();
+        int lottoAmount = result.getAmount();
+
+        if (result.isMatchBonusNumber()) {
+            System.out.printf(OUTPUT_BONUS_MATCH_RESULT, lottoMatchCount, lottoAmount, count);
+        }
+        if (!result.isMatchBonusNumber()) {
+            System.out.printf(OUTPUT_MATCH_RESULT, result.getMatchCount(), result.getAmount(), count);
+        }
+    }
 }
