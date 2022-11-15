12e4b620f3399966cf7b3b78823e8df93a389bca
3juhwan <13selfesteem91@naver.com>
2022-11-15 22:14:39 +0900

    refactor(lotto): 미사용 함수 삭제

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -5,18 +5,12 @@ import java.util.List;
 
 public class Player {
     private List<Lotto> lottoInHand = new ArrayList<>();
-    private Integer numberOfLotto = 0;
 
     public void buyLotto(Lotto lotto) {
         lottoInHand.add(lotto);
-        numberOfLotto++;
     }
 
     public List<Lotto> getLottoInHand() {
         return lottoInHand;
-    }
-
-    public Integer getNumberOfLotto() {
-        return numberOfLotto;
     }
 }
