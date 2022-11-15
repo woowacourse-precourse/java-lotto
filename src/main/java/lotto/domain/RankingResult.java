39b76a1238fbe8ff802aa750000fb13f5fa482e5
KimJaeSung <behappy0916@naver.com>
2022-11-15 11:02:49 +0900

    feat: enum을 활용해 수익률을 계산하는 기능과 enum을 관리하는 기능 구현

---
+++ src/main/java/lotto/domain/RankingResult.java
@@ -0,0 +1,63 @@
+package lotto.domain;
+
+import java.text.DecimalFormat;
+import lotto.domain.Ranking;
+import java.util.EnumMap;
+import java.util.Map;
+
+public class RankingResult {
+    private Map<Ranking, Integer> rankings;
+    DecimalFormat decFormat = new DecimalFormat("###,###");
+
+    public RankingResult() {
+        rankings = new EnumMap<>(Ranking.class);
+        for (Ranking ranking : Ranking.values()) {
+            rankings.put(ranking, 0);
+        }
+    }
+
+    public void update(Ranking ranking) {
+        rankings.put(ranking, rankings.get(ranking) + 1);
+
+    }
+
+    public Map<Ranking, Integer> getRankings() {
+        return rankings;
+    }
+
+    public double getProfit(Money money) {
+        double profit = profitMoney();
+        return profitRates(profit, money.getMoney());
+    }
+
+    public String result() {
+
+        String outputString = "";
+        for (Ranking ranking : Ranking.winningResults()) {
+            if (ranking == Ranking.SECOND) {
+                outputString +=
+                        ranking.getMatchCount() + "개 일치, 보너스 볼 일치 (" + decFormat.format(ranking.getPrize()) + "원) - "
+                                + rankings.get(ranking) + "개\n";
+                continue;
+            }
+            outputString +=
+                    ranking.getMatchCount() + "개 일치 (" + decFormat.format(ranking.getPrize()) + "원) - " + rankings.get(
+                            ranking) + "개\n";
+        }
+
+        return outputString;
+    }
+
+    private double profitRates(double profit, int value) {
+        return (profit / value) * 100;
+    }
+
+
+    private double profitMoney() {
+        double profit = 0;
+        for (Ranking ranking : Ranking.winningResults()) {
+            profit += ranking.getPrize() * rankings.get(ranking);
+        }
+        return profit;
+    }
+}
