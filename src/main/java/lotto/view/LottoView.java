38adc1d2f4bc9efe9b051c5bd3885d812fb25caa
simba <shim9597@gmail.com>
2022-11-15 11:25:06 +0900

    refactor(lotto): 로또 번호 오름 차순 출력 기능 추가

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -2,6 +2,8 @@ package lotto.view;
 
 import lotto.domain.Lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoView {
@@ -9,7 +11,12 @@ public class LottoView {
     public void printAllLotto(List<Lotto> wallet) {
         for (Lotto lotto : wallet) {
             List<Integer> numbers = lotto.getNumbers();
-            System.out.println(numbers);
+
+            List<Integer> tmp = new ArrayList<>(numbers);
+
+            Collections.sort(tmp);
+
+            System.out.println(tmp);
         }
     }
 }
