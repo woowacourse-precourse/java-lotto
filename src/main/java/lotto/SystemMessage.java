1e4caa81a3d2c986f5e2915aa5cca3848ab37b02
김선동 <mok03127@gmail.com>
2022-11-15 17:29:17 +0900

    feat: 당첨 통계 출력하는 기능

--- src/main/java/lotto/SystemMessage.java
+++ src/main/java/lotto/SystemMessage.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.text.NumberFormat;
+import java.util.Map;
 
 public class SystemMessage {
     private static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
@@ -31,4 +32,31 @@ public class SystemMessage {
         return NumberFormat.getInstance().format(number);
     }
 
+    public static void winningResult(Map<Ranking, Integer> winningResult) {
+        System.out.println();
+        System.out.println("당첨 통계\n---");
+        for (Ranking lottoOperator : winningResult.keySet()) {
+            if (lottoOperator == Ranking.NOTHING) {
+                continue;
+            }
+            if (lottoOperator == Ranking.SECOND) {
+                printBonusResult(winningResult, lottoOperator);
+                continue;
+            }
+            printCommonResult(winningResult, lottoOperator);
+        }
+    }
+
+    private static void printBonusResult(Map<Ranking, Integer> winningResult, Ranking rank) {
+        System.out.println(
+                rank.getCount() + "개 일치, 보너스 볼 일치 (" +
+                        putComma(rank.getPrize()) + "원) - " +
+                        winningResult.get(rank) + "개");
+    }
+
+    private static void printCommonResult(Map<Ranking, Integer> winningResult, Ranking rank) {
+        System.out.println(rank.getCount() + "개 일치 (" +
+                        putComma(rank.getPrize()) + "원) - " +
+                        winningResult.get(rank) + "개");
+    }
 }
