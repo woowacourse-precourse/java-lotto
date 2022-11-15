ff001ba43e543b82cf3fc749cfe7b4e67dafd3b3
KNH <nh2004@naver.com>
2022-11-15 10:20:49 +0900

    fix(ErrorMsg) : 로또 번호의 범위를 벗어났을 경우 Error 문구 수정

--- src/main/java/lotto/error/ErrorMsg.java
+++ src/main/java/lotto/error/ErrorMsg.java
@@ -6,7 +6,7 @@ public enum ErrorMsg {
     WRONG_BUYING_UNIT("[ERROR] 로또의 구입 금액은 "+ LottoInfo.PRICE+"원 단위입니다."),
     WRONG_LOTTO_SIZE("[ERROR] 로또의 길이가 6이 아닙니다."),
     DUPLICATE_LOTTO_NUMBER("[ERROR] 로또에 중복된 숫자가 존재합니다."),
-    WRONG_LOTTO_NUMBER("[ERROR] 로또 범위를 벗어나는 숫자입니다. (범위 : " + LottoInfo.MIN +"~"+LottoInfo.MAX+")"),
+    WRONG_LOTTO_NUMBER("[ERROR] 로또 번호는 "+LottoInfo.MIN+"부터 "+LottoInfo.MAX+" 사이의 숫자여야 합니다."),
     INPUT_ERROR("[ERROR] 잘못된 입력입니다."),
     DUPLICATE_BONUS_NUMBER("[ERROR] 로또 번호와 보너스 번호가 중복됩니다.");
 
