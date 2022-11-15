23a8139b02e538c6b54a19e42f9dd6f0cb84cc55
mikekang47 <gyeong0308@gmail.com>
2022-11-15 19:35:31 +0900

    Feat<Calculator>: 맞은 갯수에 따른 등수 리턴로직 추가

--- src/main/java/lotto/utils/Calculator.java
+++ src/main/java/lotto/utils/Calculator.java
@@ -1,11 +1,40 @@
 package lotto.utils;
 
+import lotto.domains.Place;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Calculator {
     private final static int MONEY_CELL = 1000;
 
-    private Calculator() {}
+    private Calculator() {
+    }
 
     public static int calculateNumberOfLotto(int money) {
         return money / MONEY_CELL;
+    }
+
+    public static Place calculatePrize(
+            List<Integer> userLottoNumbers,
+            List<Integer> winningNumbers,
+            int bonusNumber
+    ) {
+        boolean bonus = userLottoNumbers.contains(bonusNumber);
+        int intersection = getIntersection(winningNumbers, userLottoNumbers);
+
+        if (intersection == Place.THIRD.getCorrection()) {
+            intersection += 1;
+        }
+
+        return Place.getPlace(intersection, bonus);
+    }
+
+    private static int getIntersection(List<Integer> userNumbers, List<Integer> winningNumber) {
+        return winningNumber.stream()
+                .distinct()
+                .filter(userNumbers::contains)
+                .collect(Collectors.toSet())
+                .size();
     }
 }
