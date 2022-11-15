ebe5a7811ab6720d5e2cda21329f578797a6dae4
KimRiun <kimriun27@gmail.com>
2022-11-15 23:49:13 +0900

    refactor: text 를 price로 변수명 변경

--- src/main/java/lotto/Price.java
+++ src/main/java/lotto/Price.java
@@ -1,40 +1,33 @@
 package lotto;
 
 public class Price {
-    private String text;
-    private int price = 0;
+    private final int price;
 
-    public Price() {
-        text = inputPrice();
-        if(!validate(text)) {
+    public Price(String price) {
+        if(!validate(price)) {
             System.out.println(ErrorMessage.PRICE.get());
             throw new IllegalArgumentException();
         }
-        price = Integer.parseInt(text);
+        this.price = Integer.parseInt(price);
     }
 
     public int get() {
         return price;
     }
 
-    private String inputPrice() {
-        KeyboardInput k = new KeyboardInput();
-        return k.read();
-    }
-
-    private boolean validate(String text){
-        if (!isNumber(text))
+    private boolean validate(String price){
+        if (!isNumber(price))
             return false;
-        if (!isPositiveNumber(text))
+        if (!isPositiveNumber(price))
             return false;
-        if (!isDividedBy1000(text))
+        if (!isDividedBy1000(price))
             return false;
         return true;
     }
 
-    private boolean isNumber(String text) {
+    private boolean isNumber(String price) {
         try {
-            Integer.parseInt(text);
+            Integer.parseInt(price);
             return true;
         }
         catch (NumberFormatException e) {
@@ -42,16 +35,14 @@ public class Price {
         }
     }
 
-    private boolean isPositiveNumber(String text) {
-        int price = Integer.parseInt(text);
-        if (price > 0)
+    private boolean isPositiveNumber(String price) {
+        if (Integer.parseInt(price) > 0)
             return true;
         return false;
     }
 
-    private boolean isDividedBy1000(String text) {
-        int price = Integer.parseInt(text);
-        if ((price % 1000) == 0)
+    private boolean isDividedBy1000(String price) {
+        if ((Integer.parseInt(price) % 1000) == 0)
             return true;
         return false;
     }
