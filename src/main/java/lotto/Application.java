cdb8ac611bd37a25281de12893de89f5dca73013
SonMinGyu <littleking13@naver.com>
2022-11-15 22:48:51 +0900

    [FEAT] 수익률 계산, 출력 기능 구현
    - 수익률 계산, 출력 기능을 구현함

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -31,15 +31,17 @@ public class Application {
         for (Lotto l : lottos) {
             l.checkWinLotto(result);
         }
-        
+
         List<Integer> lottoResult = result.get("myLottoPriceList");
         List<Integer> prizeResult = result.get("prize");
 
-        printResult(lottoResult, prizeResult);
+        printResult(lottoResult);
+        printYield(prizeResult, numberOfLotto);
 
     }
 
     public static int getTheNumberOfLotto() {
+        System.out.println("구입금액을 입력해 주세요.");
         String money = Console.readLine();
         moneyValidate(money);
         int numberOfLotto = Integer.parseInt(money) / 1000;
@@ -81,12 +83,16 @@ public class Application {
     }
 
     public static void printLottos(List<Lotto> lottos) {
+        System.out.println();
+        System.out.println(String.format("%d개를 구매했습니다.", lottos.size()));
         for (Lotto lotto : lottos) {
             lotto.printLotto();
         }
     }
 
     public static List<Integer> getMyLottoNumbers() {
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
         String[] lotto = Console.readLine().split(",");
 
         myLottoValidate(lotto);
@@ -101,6 +107,8 @@ public class Application {
     }
 
     public static List<Integer> getMyBonusNumbers(List<Integer> myLotto) {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
         String bonus = Console.readLine();
 
         bonusNumberValidate(bonus, myLotto);
@@ -203,20 +211,31 @@ public class Application {
         return false;
     }
 
-    public static void printResult(List<Integer> lottoResult, List<Integer> prizeResult) {
+    public static void printResult(List<Integer> lottoResult) {
+        System.out.println();
         System.out.println(String.format(
                 "당첨 통계" + "\n"
+                        + "---" + "\n"
                         + "3개 일치 (5,000원) - %d" + "\n"
                         + "4개 일치 (50,000원) - %d" + "\n"
                         + "5개 일치 (1,500,000원) - %d" + "\n"
                         + "5개 일치, 보너스 볼 일치 (30,000,000원) - %d" + "\n"
-                        + "6개 일치 (2,000,000,000원) - %d" + "\n"
+                        + "6개 일치 (2,000,000,000원) - %d"
                 , lottoResult.get(0),
                 lottoResult.get(1),
                 lottoResult.get(2),
                 lottoResult.get(4),
                 lottoResult.get(3))
         );
+    }
+
+    public static void printYield(List<Integer> prizeResult, int numberOfLotto) {
+        double prize = (double) prizeResult.get(0);
+        int totalUseMoney = numberOfLotto * 1000;
+
+        double yield = (prize / totalUseMoney);
+        double yieldRate = yield * 100;
+        System.out.println(String.format("총 수익률은 %.1f", yieldRate) + "%입니다.");
     }
 
 
