e871b5c2b71d492a8cc14dd0f976ec786e94f73f
masiljangajji <hjk172262@gmail.com>
2022-11-15 21:09:07 +0900

    test: 당첨 번호의 숫자 범위 예외 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -23,5 +23,12 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호의 숫자 범위를 벗어나면 예외가 발생한다")
+    @Test
+    void createLottoByDuplicatedNumber2(){
+        assertThatThrownBy(() -> new Lotto(List.of(46, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     // 아래에 추가 테스트 작성 가능
 }
