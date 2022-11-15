dcc1785f09f28c1aaee997a7387051830376a464
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-15 14:39:55 +0900

    feat(test\...\model) : create model package and make lotto ticket storage test class

---
+++ src/test/java/lotto/model/LottoTicketsStorageTest.java
@@ -0,0 +1,18 @@
+package lotto.model;
+
+import lotto.controller.TicketMachine;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoTicketsStorageTest {
+
+    @DisplayName("로또 티켓 발행 저장 여부 확인")
+    @Test
+    void lottoTicketsStorageTest() {
+        LottoTicketsStorage lottoTicketsStorage = new LottoTicketsStorage();
+        lottoTicketsStorage.setLottoTickets(TicketMachine.issueTickets(5));
+        assertThat(lottoTicketsStorage.getLottoTickets().size()).isEqualTo(5);
+    }
+}
