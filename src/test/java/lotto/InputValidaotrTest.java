5ea35463e17d1b6ecf588b51801385dab3ece7bc
meunrud <y3a0522@gmail.com>
2022-11-15 10:34:02 +0900

    :sparkles: 입력값에 문자가 있으면 오류 발생하는지 확인

---
+++ src/test/java/lotto/InputValidaotrTest.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputValidaotrTest {
+    InputValidator inputValidator;
+    String moneyinput;
+
+    @BeforeEach
+    void setUp(){
+        moneyinput = "8000원";
+        inputValidator = new InputValidator(moneyinput);
+    }
+
+
+    @DisplayName("입력값에 문자가 있으면 오류 발생")
+    @Test
+    void checkOnlyNumber(){
+        //given
+
+        //when
+        assertThatThrownBy(() -> inputValidator.checkOnlyNumber(moneyinput))
+                .isInstanceOf(IllegalArgumentException.class);
+
+
+    }
+}
