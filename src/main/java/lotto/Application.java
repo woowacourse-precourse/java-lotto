f01f112926bf8b7342936b6c3befc0373eb16fc6
w0ntae <bitcampcoding@gmail.com>
2022-11-15 21:40:51 +0900

    Feat(Application):Modified main
    Add methods to main function to start the lotto

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import java.util.List;
+
+import static lotto.Lotto.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int money = read();
+        numOfTickets(money);
+        List<List> tickets = purchased();
+        printTickets(tickets);
+        List<String> winningIn = winInput();
+        List<Integer> winningNum = validateWin(winningIn);
+        loop(winningNum, tickets);
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,13 +11,7 @@ public class Lotto {
     
     private static int bonusNum;
     
-    /*
-    1등: 6개 번호 일치 / 2,000,000,000원
-    2등: 5개 번호 + 보너스 번호 일치 / 30,000,000원
-    3등: 5개 번호 일치 / 1,500,000원
-    4등: 4개 번호 일치 / 50,000원
-    5등: 3개 번호 일치 / 5,000원
-    */
+    private static int tickets = 0;
     
     enum winnings {
         FIRST,
@@ -39,51 +33,66 @@ public class Lotto {
         }
     }
     
-    public Integer read() throws IllegalArgumentException{
+    public static Integer read() throws IllegalArgumentException{
+        System.out.println("구입금액을 입력해 주세요.");
         try{
             int purchaseAmount = Integer.valueOf(Console.readLine().strip());
             return purchaseAmount;
         }
         catch(Exception e){
+            System.out.println("[ERROR]");
             throw e;
         }
     }
     
-    public Integer numOfTickets(int money){
-        if (money > 1000){
-            return money / 1000;
+    public static void numOfTickets(int money){
+        if(money % 1000 > 0){
+            tickets = 0;
         }
-        return 0;
+        if (money > 1000){
+            tickets = money / 1000;
+        }
     }
     
-    public List<String> winInput(){
+    public static void printTickets(List<List> tickets){
+        System.out.println( tickets + "개를 구매했습니다.");
+        for (List l : tickets){
+            System.out.println(l);
+        }
+    }
+    
+    public static List<String> winInput(){
         List<String> results = new ArrayList<>();
+        System.out.println("당첨 번호를 입력해 주세요.");
         String winningInput = Console.readLine().strip();
+        System.out.println("보너스 번호를 입력해 주세요.");
         String bonus = Console.readLine().strip();
         results.add(winningInput);
         results.add(bonus);
         return results;
     }
     
-    public List<Integer> validateWin(List<String> input) throws IllegalArgumentException{
+    public static List<Integer> validateWin(List<String> input) throws IllegalArgumentException{
         IllegalArgumentException exception = new IllegalArgumentException();
         String[] temp = input.get(0).split(",");
         List<Integer> result = new ArrayList<>();
         for(int n = 0; n < 6; n++){
             int tempNum = val(input, n);
             if (result.contains(tempNum)){
+                System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
                 throw exception;
             }
             result.add(tempNum);
         }
         bonusNum = val(input, 7);
         if(result.contains(bonusNum)){
+            System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             throw exception;
         }
         return result;
     }
     
-    public int val(List<String> in, int num) throws IllegalArgumentException{
+    public static int val(List<String> in, int num) throws IllegalArgumentException{
         try{
             int temp = Integer.valueOf(in.get(num));
             if (temp < 45 && temp > 0){
@@ -91,25 +100,26 @@ public class Lotto {
             }
         }
         catch(Exception e){
+            System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             throw e;
         }
         return 0;
     }
     
-    public List<Integer> gen(){
+    public static List<Integer> gen(){
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
     
-    public List<List> purchased(int  n){
+    public static List<List> purchased(){
         List<List> result = new ArrayList<>();
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < tickets; i++){
             List<Integer> gen = gen();
             result.add(gen);
         }
         return result;
     }
     
-    public void loop(List<Integer> win, List<List> purchased){
+    public static void loop(List<Integer> win, List<List> purchased){
         int first = 0, second = 0 , third = 0, fourth = 0, fifth = 0;
         for(List l : purchased){
             boolean bonus = false;
@@ -134,15 +144,44 @@ public class Lotto {
                 fifth++;
             }
         }
+        System.out.println("당첨 통계\n" + "---");
         System.out.println("3개 일치 (5,000원) - "+ fifth + "개");
-        System.out.println("4개 일치 (50,000원) - " + fourth +"0개");
-        System.out.println("5개 일치 (1,500,000원) - " + third +"0개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + second +"0개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + first +"0개");
+        System.out.println("4개 일치 (50,000원) - " + fourth +"개");
+        System.out.println("5개 일치 (1,500,000원) - " + third +"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + second +"개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + first +"개");
         
+        float result = calculate(tickets, first,second,third,fourth,fifth);
+        float temp = Math.round(result * 100) / 100;
+    
+        System.out.println("총 수익률은 " + temp +"%입니다.");
     }
     
-    public int compare(List<Integer> win, List<Object> ticket){
+    public static float calculate(int spent, int first, int second, int third, int fourth, int fifth){
+        int won = 0;
+        
+        won += (first * 2000000000);
+        won += (second * 30000000);
+        won += (third * 1500000);
+        won += (fourth * 50000);
+        won += (fifth * 5000);
+    /*
+    1등: 6개 번호 일치 / 2,000,000,000원
+    2등: 5개 번호 + 보너스 번호 일치 / 30,000,000원
+    3등: 5개 번호 일치 / 1,500,000원
+    4등: 4개 번호 일치 / 50,000원
+    5등: 3개 번호 일치 / 5,000원
+    */
+        
+        if(won == 0){
+            return 0;
+        }
+        float s = (spent * 1000), w = won;
+        float result = w / s;
+        return result;
+    }
+    
+    public static int compare(List<Integer> win, List<Object> ticket){
         int result = 0;
         for(Object o : ticket){
             if(win.contains(o)){
@@ -152,7 +191,7 @@ public class Lotto {
         return  result;
     }
     
-    public winnings winAmount(int count, boolean bonus){
+    public static winnings winAmount(int count, boolean bonus){
         if (count == 6){
             return winnings.FIRST;
         }
