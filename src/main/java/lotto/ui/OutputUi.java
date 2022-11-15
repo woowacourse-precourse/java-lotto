42f0b12247c3ac76ba6dabaa1d5bee318fd954ce
Your Name <you@example.com>
2022-11-15 18:34:15 +0900

    refactor(ui) : 출력 로직 전체 띄어쓰기 수정

--- src/main/java/lotto/ui/OutputUi.java
+++ src/main/java/lotto/ui/OutputUi.java
@@ -24,12 +24,14 @@ public class OutputUi {
      * 로또 수량과 로또 번호 출력 하기
      */
     public void printLottoAmountAndNumber(List<Lotto> lotteries) {
-        System.out.println(lotteries.size() + "개를 구매했습니다.");
+        System.out.println("\n" + lotteries.size() + "개를 구매했습니다.");
         for (Lotto lottery : lotteries) {
-            lottery.getNumbers()
+            List<Integer> lotteryNumbers = lottery.getNumbers()
                     .stream()
                     .sorted(Comparator.naturalOrder())
                     .collect(Collectors.toList());
+
+            System.out.println(lotteryNumbers);
         }
     }
 
@@ -37,14 +39,14 @@ public class OutputUi {
      * 로또 당첨 번호 입력해 달라는 문구 출력
      */
     public void printInputLottoWinNumber() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println("\n당첨 번호를 입력해 주세요.");
     }
 
     /**
      * 로또 보너스 번호 입력해 달라는 문구 출력
      */
     public void printInputLottoBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println("\n보너스 번호를 입력해 주세요.");
     }
 
     /**
