4cfb05dfe68fbf033e32386d689a178eaea14f26
JunHyeon <dlwnsgus07@naver.com>
2022-11-15 22:19:27 +0900

    fix: 사용자가 1이하의 숫자를 입력할 때 예외 사항 추가

--- src/main/java/lotto/PurchaseMoney.java
+++ src/main/java/lotto/PurchaseMoney.java
@@ -2,6 +2,7 @@ package lotto;
 
 public class PurchaseMoney {
     static final String ERROR_MESSAGE_FOR_PURCHASE_PRICE = "구매하시는 가격은 1000의 배수여야 합니다.";
+    static final String ERROR_MESSAGE_FOR_PURCHASE_PRICE_UNDER_ONE = "구매하시는 가격은 양수여야 합니다.";
 
     private final Integer purchaseMoney;
 
@@ -13,6 +14,9 @@ public class PurchaseMoney {
     private void validation(Integer purchaseMoney) {
         if (purchaseMoney % 1000 != 0) {
             throw new IllegalArgumentException(ERROR_MESSAGE_FOR_PURCHASE_PRICE);
+        }
+        if(purchaseMoney < 1){
+            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_PURCHASE_PRICE_UNDER_ONE);
         }
     }
 
