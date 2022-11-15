8f41e23d180ec2a81b367356c067f3b01da62a93
kevstevie <jianna442@gmail.com>
2022-11-15 21:11:22 +0900

    refactor: 상수명 변경

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -4,7 +4,7 @@ public enum ErrorMessage {
 
     PREFIX("[ERROR] "),
     INPUT_NUMBER("숫자를 입력해야 합니다."),
-    INPUT_1000("1000원 단위로 입력해야 합니다."),
+    INPUT_PROPER_UNIT("1000원 단위로 입력해야 합니다."),
     INPUT_SIX("숫자를 여섯개 입력해야 합니다."),
     INPUT_DUPLICATE("중복되지 않은 숫자를 입력해야 합니다."),
     INPUT_RANGE("1부터 45사이의 숫자를 입력해야 합니다.");
