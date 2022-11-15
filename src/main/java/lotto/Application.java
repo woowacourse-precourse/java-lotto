cd05c03418fe34e14009732e6095ae8c7c5fc4ae
jaeyeon kim <jakind@naver.com>
2022-11-15 10:42:50 +0900

    refactor: 애플리케이션 내에서 발생하는 모든 IllegalException 을 Catch 하여 출력하는 구문 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,10 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        new Store().entry();
+        try {
+            new Store().entry();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
