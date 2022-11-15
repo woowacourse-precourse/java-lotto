e81f64838ffb60ae9476b6099ee4db4ff95e07bd
hyeonseog-won <smreo75@gmail.com>
2022-11-15 22:08:50 +0900

    refactor: 하드코딩된 숫자, 문자 상수로 변경

--- src/main/java/lotto/Validate.java
+++ src/main/java/lotto/Validate.java
@@ -4,29 +4,25 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static lotto.MessageConstant.*;
+
 public class Validate {
+    private static final int LOTTO_SIZE = 6;
+    private static final int MINIMUM_LOTTO_PRICE = 1000;
+    private static final int ZERO = 0;
+    private static final int MINIMUM_LOTTO_NUMBER = 1;
+    private static final int MAXIMUM_LOTTO_NUMBER = 45;
     private static final String INPUT_LOTTO_PATTERN = "^([1-9]\\d?,){5}[1-9]\\d?$";
     private static final String ONLY_NUMBER_PATTERN = "^\\d*$";
-    private static final String ERROR_MESSAGE = "[ERROR] ";
-    private static final String NOT_SIX_LOTTO_NUMBER_MESSAGE = "로또 숫자가 로또 숫자가 6개가 아닙니다.";
-    private static final String HAVE_DUPLICATE_NUMBER_MESSAGE = "중복된 번호가 있습니다.";
-    private static final String BONUS_NUMBER_DUPLICATE_MESSAGE = "당첨번호와 보너스 번호는 달라야 합니다.";
-    private static final String INPUT_LOTTO_MESSAGE = "숫자와 ,만 입력해 주세요.";
-    private static final String ONLY_NUMBER_MESSAGE = "숫자만 입력해 주세요.";
-    private static final String LOTTO_MINIMUM_PRICE_MESSAGE = "1,000원 이상을 입력해 주세요.";
-    private static final String LOTTO_PRICE_RULE_MESSAGE = "1,000원으로 나누어 떨어지는 금액만 입력해 주세요.";
-    private static final String LOTTO_NUMBER_RANGE_RULE_MESSAGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-
-
     public static void lottoSize(List<Integer> lottoNumbers) {
-        if (lottoNumbers.size() != 6) {
+        if (lottoNumbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(ERROR_MESSAGE + NOT_SIX_LOTTO_NUMBER_MESSAGE);
         }
     }
 
     public static void lottoDuplicate(List<Integer> lottoNumbers) {
         Set<Integer> nonDuplicateNumbers = new HashSet<>(lottoNumbers);
-        if (nonDuplicateNumbers.size() != 6) {
+        if (nonDuplicateNumbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(ERROR_MESSAGE + HAVE_DUPLICATE_NUMBER_MESSAGE);
         }
     }
@@ -57,17 +53,17 @@ public class Validate {
     }
 
     public static void canBuyLotto(int money) {
-        if (money < 1000) {
+        if (money < MINIMUM_LOTTO_PRICE) {
             throw new IllegalArgumentException(ERROR_MESSAGE + LOTTO_MINIMUM_PRICE_MESSAGE);
         }
 
-        if (money % 1000 != 0) {
+        if (money % MINIMUM_LOTTO_PRICE != ZERO) {
             throw new IllegalArgumentException(ERROR_MESSAGE + LOTTO_PRICE_RULE_MESSAGE);
         }
     }
 
     public static void inRangeLottoNumber(Integer lottoNumber) {
-        if (lottoNumber >= 1 && lottoNumber <= 45) {
+        if (lottoNumber >= MINIMUM_LOTTO_NUMBER && lottoNumber <= MAXIMUM_LOTTO_NUMBER) {
             return;
         }
         throw new IllegalArgumentException(ERROR_MESSAGE + LOTTO_NUMBER_RANGE_RULE_MESSAGE);
