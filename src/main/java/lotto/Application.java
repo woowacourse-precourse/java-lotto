693482480447d546b1c0a0d10cbc21bc3624bc56
seyeon ji <joeun9705@gmail.com>
2022-11-15 17:32:12 +0900

    fix: connect features

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
-
+import lotto.Controller;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+       InputView inputView = new InputView();
+        LottoNumberCreator lottoCreator = new LottoNumberCreator((inputView.getTicketMoney()));
+        lottoCreator.showHowManyTickets();
+       inputView.pressLottoNumbers();
+       Lotto lotto = new Lotto(inputView.getLottoNumbers());
+       inputView.pressBonusNumber();
+       Controller controller = new Controller(lotto.getLottoNumbers(), inputView.getBonusNumber(), inputView.getTicketMoney());
+       controller.showLottoWinningResult(lottoCreator.getNumbersContainer());
+
+
     }
 }
+
