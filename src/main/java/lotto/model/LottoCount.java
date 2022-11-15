02147ccdd069a58d7e0591ef4971228c51831904
jiwon1217 <koagjw@gmail.com>
2022-11-15 21:28:54 +0900

    refactor: 변수 이름 변경

--- src/main/java/lotto/model/LottoCount.java
+++ src/main/java/lotto/model/LottoCount.java
@@ -1,11 +1,11 @@
 package lotto.model;
 
 public class LottoCount {
-    private static final String UNIT = "1000";
+    private static final String DEFAULT_UNIT = "1000";
     private final int lottoCount;
 
     public LottoCount(Money money) {
-        lottoCount = money.calculateLottoCount(new Money(UNIT));
+        lottoCount = money.calculateLottoCount(new Money(DEFAULT_UNIT));
     }
 
     public int getLottoCount() {
