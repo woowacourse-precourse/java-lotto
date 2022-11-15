d02aeddb27d84b885ea72aef546489bf7e1d2040
kmularise <kmula222@gmail.com>
2022-11-15 14:02:59 +0900

    feat(lotto): make function of generating lotto tickets

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static final String START_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
     public static void main(String[] args) {
         play();
         // TODO: 프로그램 구현
@@ -11,5 +14,9 @@ public class Application {
         System.out.println(START_MESSAGE);
         Parser parser = new Parser();
         int count = parser.getCount();
+        System.out.println(count + PURCHASE_MESSAGE);
+        Generator generator = new Generator(count);
+        List<List<Integer>> tickets = generator.getTickets();
+        tickets.stream().forEach(System.out::println);
     }
 }
