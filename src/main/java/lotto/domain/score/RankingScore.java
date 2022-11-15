1bec8e6ad21978385d513daf2bd0ff6ab03f1674
jaehong <println@kakao.com>
2022-11-15 14:36:56 +0900

    refactor (RankingScore) :등수에 맞는 점수 목록 변경

--- src/main/java/lotto/domain/score/RankingScore.java
+++ src/main/java/lotto/domain/score/RankingScore.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public enum RankingScore {
     NOTING(List.of(new Score(0, 0))),
-    FIFTH_SCORE(List.of(new Score(2, 1), new Score(3, 0))),
-    FOURTH_SCORE(List.of(new Score(3, 1), new Score(4, 0))),
-    THIRD_SCORE(List.of(new Score(4, 1), new Score(5, 0))),
+    FIFTH_SCORE(List.of( new Score(3, 1), new Score(3,0 ))),
+    FOURTH_SCORE(List.of(new Score(4, 1), new Score(4, 0))),
+    THIRD_SCORE(List.of(new Score(5, 0))),
     SECOND_SCORE(List.of(new Score(5, 1))),
     FIRST_SCORE(List.of(new Score(6, 0)));
 
