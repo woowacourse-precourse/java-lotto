957f11bc0b250c8a6dd89d0dbf26012bbaae797c
BHC-Chicken <tij05078@gmail.com>
2022-11-15 09:47:59 +0900

    feat : 중복 검증
    입력 받은 값들 사이에 중복이 있는지 검증한다.

--- src/main/java/lotto/Validate.java
+++ src/main/java/lotto/Validate.java
@@ -6,4 +6,8 @@ public class Validate {
     public boolean wrongNumber(int number) {
         return (number <= 0 || number >= 46);
     }
+
+    public boolean duplicateNumber(int number, int[] numbers) {
+        return Arrays.stream(numbers).anyMatch(n -> n == number);
+    }
 }
