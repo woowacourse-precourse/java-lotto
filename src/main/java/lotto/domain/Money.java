832719c097844a0693eb646d54b7dae70cbfa370
yuntaeLee <leeyt1201@gmail.com>
2022-11-15 17:23:40 +0900

    feat: 돈범위 검증 메서드 및 예외 메세지 구현

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,10 +1,18 @@
 package lotto.domain;
 
+import static lotto.enumtype.LottoValidationMsg.*;
+
 public class Money {
     private final int money;
 
     public Money(int money) {
         this.money = money;
+    }
+
+    private void validateMoney(int money) {
+        if (money <= 0) {
+            throw new IllegalArgumentException(NOT_INPUT_NEGATIVE_MONEY_EXCEPTION.getMessage());
+        }
     }
 
     public int getMoney() {
--- src/main/java/lotto/enumtype/LottoValidationMsg.java
+++ src/main/java/lotto/enumtype/LottoValidationMsg.java
@@ -7,7 +7,8 @@ public enum LottoValidationMsg {
     OVER_LOTTO_FIX_SIZE_EXCEPTION("입력 가능한 로또 번호의 개수는 %d개 입니다."),
     LOTTO_NUMBER_RANGE_EXCEPTION("로또 번호는 %d부터 %d 사이의 숫자여야 합니다."),
     DUPLICATE_NUMBER_EXCEPTION("로또 번호는 중복될 수 없습니다."),
-    DUPLICATE_BONUS_NUMBER_EXCEPTION("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    DUPLICATE_BONUS_NUMBER_EXCEPTION("보너스 번호는 당첨 번호와 중복될 수 없습니다."),
+    NOT_INPUT_NEGATIVE_MONEY_EXCEPTION("양수만 입력 가능 합니다.");
 
     String message;
 
