c726061c1bf56a9671ebecaac18e03742439c021
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-15 19:20:24 +0900

    Feat($Lotto) 예외처리 형식 변경
     기존 NoSuchElementException -> IllegalArgumentException

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,6 @@
 package lotto;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 public class Lotto {
@@ -16,43 +15,30 @@ public class Lotto {
         // 6자리가 아니면 예외처리
         if (numbers.size() != 6) {
             System.out.println("[ERROR] 로또 번호는 6자리여야 합니다.");
-            throw new NoSuchElementException("[ERROR] 로또 번호는 6자리여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리여야 합니다.");
         }
         // 중복된 값이 있으면 예외처리
         if (numbers.stream().distinct().collect(Collectors.toList()).size() != 6) {
             System.out.println("[ERROR] 로또 번호에 중복된 값이 존재합니다.");
-            throw new NoSuchElementException("[ERROR] 로또 번호에 중복된 값이 존재합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 값이 존재합니다.");
         }
     }
 
     // 로또 당첨을 체크하는 메소드
+    // TODO 15줄로 줄이기
     public int winningCheck(List<Integer> winningNumberList) {
         int winningCount = 0;
-
-        int thisIndex = 0;
-        int winningIndex = 0;
-
-        while (thisIndex < numbers.size() && winningIndex < winningNumberList.size()) {
-            int thisNumber = numbers.get(thisIndex);
-            int winningNumber = winningNumberList.get(winningIndex);
-
-            if (thisNumber < winningNumber) {
-                thisIndex++;
+        for (int thisIndex = 0, winningIndex = 0; thisIndex < numbers.size() && winningIndex < winningNumberList.size(); thisIndex++, winningIndex++) {
+            if (numbers.get(thisIndex) < winningNumberList.get(winningIndex)) {
+                winningIndex--;
                 continue;
             }
-
-            if (winningNumber < thisNumber) {
-                winningIndex++;
+            if (winningNumberList.get(winningIndex) < numbers.get(thisIndex)) {
+                thisIndex--;
                 continue;
             }
-
-            if (winningNumber == thisNumber) {
-                winningCount++;
-                winningIndex++;
-                thisIndex++;
-            }
+            winningCount++;
         }
-
         return winningCount;
     }
 
