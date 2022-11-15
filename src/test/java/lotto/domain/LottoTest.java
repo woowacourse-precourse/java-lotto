414dff0562d5faa7feeb66a5ad44aeae8f9bee97
yun8565 <yunyun8565@naver.com>
2022-11-15 17:22:20 +0900

    test: 로또 번호 정렬 및 비교 기능 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -35,4 +35,27 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR + WRONG_RANGE_MESSAGE);
     }
+
+    @DisplayName("로또 번호 정렬이 잘 되는지 확인한다.")
+    @Test
+    void checkLottoWellSorted() {
+        String numbersBeforeSortInString = "13,1,42,9,11,23";
+        List<Integer> numbersBeforeSort = List.of(42,13,9,1,11,23);
+        List<Integer> numbersAfterSort = List.of(1,9,11,13,23,42);
+
+        Lotto testLotto1 = new Lotto(numbersBeforeSort);
+        Lotto testLotto2 = new Lotto(numbersBeforeSortInString);
+
+        assertThat(testLotto1.getNumbers()).isEqualTo(numbersAfterSort);
+        assertThat(testLotto2.getNumbers()).isEqualTo(numbersAfterSort);
+    }
+
+    @DisplayName("로또 번호 비교 기능이 잘 동작하는지 확인한다.")
+    @Test
+    void checkLottoWellCompared() {
+        Lotto testLotto = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto lottoToCompare = new Lotto(List.of(1,2,3,11,12,13));
+
+        assertThat(testLotto.countMatchingNumbers(lottoToCompare)).isEqualTo(3);
+    }
 }
