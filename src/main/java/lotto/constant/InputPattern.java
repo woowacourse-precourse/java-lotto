d4103a5b2d37cbbcc98c3227a542e93202258eaf
GyuJeGal <lkjdee0329@gmail.com>
2022-11-15 16:46:55 +0900

    refactor(InputPattern): 변수 이름 수정 INPUT_AMOUNT_PATTERN에서 PURCHASE_AMOUNT_PATTERN으로 수정

--- src/main/java/lotto/constant/InputPattern.java
+++ src/main/java/lotto/constant/InputPattern.java
@@ -1,6 +1,6 @@
 package lotto.constant;
 
 public class InputPattern {
-    public static final String INPUT_AMOUNT_PATTERN = "^[0-9]*$";
+    public static final String PURCHASE_AMOUNT_PATTERN = "^[0-9]*$";
     public static final String WINNING_NUMBER_PATTERN = "^(,|[0-9])*$";
 }
