625ebef4675bc09f6b949dcd7e6388d935b3514d
hhiyeon <hhyyeon0214@gmail.com>
2022-11-15 12:46:41 +0900

    feat: service-add lotto random numbers to list lotteries

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,7 +1,23 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoService {
+    private List<Lotto> lotteries = new ArrayList<>();
+
     public LottoService() {
 
+    }
+
+    public void addLotteries(int buyNumber) {
+        while (buyNumber != 0) {
+            List<Integer> randomNumbers = Lotto.createRandomNumbers();
+            Lotto lotto = new Lotto(randomNumbers);
+            lotteries.add(lotto);
+            buyNumber--;
+        }
     }
 }
