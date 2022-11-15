8c29867116ac92ab4a584584f57171a07ba34f8e
Frog-Slayer <Frog-Slayer@github.com>
2022-11-15 23:41:40 +0900

    fix : 예외처리 문제 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,16 +6,21 @@ import lotto.domain.User;
 
 public class Application {
     public static void main(String[] args) {
-        Game game = new Game();
-        User user = new User();
-        View view = new View();
-        Controller controller = new Controller(game, user, view);
-        controller.setBudget();
-        controller.buyLotto();
-        controller.showLottos();
-        controller.setWinningNumbers();
-        controller.setBonus();
-        controller.computeStatistics();
-        controller.showStatistics();
+        try {
+            Game game = new Game();
+            User user = new User();
+            View view = new View();
+            Controller controller = new Controller(game, user, view);
+            controller.setBudget();
+            controller.buyLotto();
+            controller.showLottos();
+            controller.setWinningNumbers();
+            controller.setBonus();
+            controller.computeStatistics();
+            controller.showStatistics();
+        }
+        catch (IllegalArgumentException exception){
+            System.out.println("[ERROR]");
+        }
     }
 }
