8669eeaf08dae213fa8d868de19a3d0780febe4f
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-15 22:58:21 +0900

    refactor(LottoGame): 금액을 입력 받을 때의 예외 상황 조건 수정 (&& -> ||)

--- src/main/java/lotto/domain/LottoCashier.java
+++ src/main/java/lotto/domain/LottoCashier.java
@@ -7,7 +7,7 @@ public class LottoCashier {
     private static final String VALIDATE_UNIT_MASSAGE = "[ERROR] 1000원 단위로 구입해야 합니다.";
 
     public int getLottoCount(int money) {
-        if (money > 0 && money % 1000 != 0) {
+        if (money > 0 || money % 1000 != 0) {
             throw new IllegalArgumentException(VALIDATE_UNIT_MASSAGE);
             }
         return money / 1000;
