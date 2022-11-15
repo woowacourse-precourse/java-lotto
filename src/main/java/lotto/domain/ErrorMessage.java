2fe99fb6628e819dc7667dd6bf050e981a2470e1
parkmuhyeun <pjhg410@gmail.com>
2022-11-15 20:39:02 +0900

    refactor: ErrorMessage 매직넘버 변환

--- src/main/java/lotto/domain/ErrorMessage.java
+++ src/main/java/lotto/domain/ErrorMessage.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import static lotto.domain.LottoStatus.*;
+
 public enum ErrorMessage {
     HEADER {
         @Override
@@ -10,14 +12,14 @@ public enum ErrorMessage {
     LOTTO_INCORRECT_SIZE {
         @Override
         public String toString() {
-            return HEADER + "로또 번호 개수는 " + LottoStatus.SIZE.getValue() + "개여야 합니다.";
+            return HEADER + "로또 번호 개수는 " + SIZE.getValue() + "개여야 합니다.";
         }
     },
     LOTTO_OUT_OF_RANGE {
         @Override
         public String toString() {
-            return HEADER + "로또 번호는 " + LottoStatus.START.getValue() +
-                    "부터 " + LottoStatus.END.getValue() + " 사이의 숫자여야 합니다.";
+            return HEADER + "로또 번호는 " + START.getValue() +
+                    "부터 " + END.getValue() + " 사이의 숫자여야 합니다.";
         }
     },
     LOTTO_DUPLICATION {
@@ -29,13 +31,13 @@ public enum ErrorMessage {
     INCORRECT_PURCHASING_AMOUNT{
         @Override
         public String toString() {
-            return HEADER + "1000원 단위의 숫자여야 합니다.";
+            return HEADER + "" + PRICE.getValue() + "원 단위의 숫자여야 합니다.";
         }
     },
     INCORRECT_LUCKY_NUMBER_SIZE{
         @Override
         public String toString() {
-            return HEADER + "당첨 번호는 ,로 구분하여 6개의 번호(1~45)를 입력하여야 합니다.";
+            return HEADER + "당첨 번호는 ,로 구분하여 " + SIZE.getValue() + "개의 번호(" + START.getValue() + "~" + END.getValue() + ")를 입력하여야 합니다.";
         }
     },
     INCORRECT_LUCKY_NUMBER_DIGIT{
@@ -47,7 +49,7 @@ public enum ErrorMessage {
     INCORRECT_LUCKY_NUMBER_RANGE{
         @Override
         public String toString() {
-            return HEADER + "당첨 번호는 1부터 45사이의 숫자여야 합니다.";
+            return HEADER + "당첨 번호는 " + START.getValue() + "부터 " + END.getValue() + "사이의 숫자여야 합니다.";
         }
     },
     LUCKY_NUMBER_DUPLICATION{
@@ -59,7 +61,7 @@ public enum ErrorMessage {
     INCORRECT_BONUS_NUMBER_SIZE{
         @Override
         public String toString() {
-            return HEADER + "보너스 번호는 1~45 사이의 번호여야 합니다.";
+            return HEADER + "보너스 번호는 "+ START.getValue() + "~" + END.getValue() +" 사이의 번호여야 합니다.";
         }
     },
     INCORRECT_BONUS_NUMBER_DIGIT{
@@ -71,7 +73,7 @@ public enum ErrorMessage {
     INCORRECT_BONUS_NUMBER_RANGE{
         @Override
         public String toString() {
-            return HEADER + "보너스 번호는 1부터 45사이의 숫자여야 합니다.";
+            return HEADER + "보너스 번호는 " + START.getValue() + "부터 " + END.getValue() + "사이의 숫자여야 합니다.";
         }
     },
     BONUS_NUMBER_DUPLICATION{
