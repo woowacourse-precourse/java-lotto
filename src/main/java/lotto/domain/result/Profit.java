c327c35578da1cffed74179ae22ab81c1aa53d74
hyee0715 <hyee0715@naver.com>
2022-11-15 18:35:57 +0900

    feat: 수익률을 계산하는 기능 구현

---
+++ src/main/java/lotto/domain/result/Profit.java
@@ -0,0 +1,27 @@
+package lotto.domain.result;
+
+import java.util.Map;
+
+public class Profit {
+    private static final int PERCENTAGE = 100;
+
+    public static double calculateProfit(Map<LottoRank, Integer> lottoResult, long purchaseAmount) {
+        long income = calculateIncome(lottoResult);
+
+        double profit = (double)income / purchaseAmount * PERCENTAGE;
+
+        return profit;
+    }
+
+    public static long calculateIncome(Map<LottoRank, Integer> lottoResult) {
+        long income = 0;
+
+        for (LottoRank lottoRank : lottoResult.keySet()) {
+            long money = lottoRank.getPrizeMoney() * lottoResult.get(lottoRank);
+
+            income += money;
+        }
+
+        return income;
+    }
+}
