2822654346853b78cd0619bf947da21fa1fa3f96
fnzl54 <rnjscksdud1@gmail.com>
2022-11-15 20:37:20 +0900

    Feat: declaration constant in LottoMachine class

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,4 +1,9 @@
 package lotto;
 
 public class LottoMachine {
+
+    private final int buyLineMoney = 1000;
+    private final int minLottoNumber = 1;
+    private final int maxLottoNumber = 45;
+    private final int oneLineLottoNumber = 6;
 }
