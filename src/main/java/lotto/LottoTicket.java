cff20aa3fb6c353958fc8c857d067874f8afacfb
HangSub999 <gkdtjq999@gmail.com>
2022-11-15 19:11:09 +0900

    feat: 로또 비교출력 구현

--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -1,11 +1,18 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoTicket {
+
+    private List<Integer> winnigLotto;
+    private int bonusNum;
+
+    LottoTicket() {
+        winnigLotto = null;
+        bonusNum = 0;
+    }
 
     public List<Lotto> createManyLotto(int lottoCount) {
         List<Lotto> lottos = new ArrayList<>();
@@ -20,8 +27,41 @@ public class LottoTicket {
     private Lotto createLotto() {
         Lotto lotto;
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
         lotto = new Lotto(numbers);
         return lotto;
+    }
+
+    public void setWinnigLotto(List<Integer> winnigLotto) {
+        this.winnigLotto = winnigLotto;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonusNum = bonus;
+    }
+
+    public Map<LottoRanking, Integer> compareLotto(List<Lotto> lottos) {
+
+        Map<LottoRanking, Integer> winningList = new EnumMap<>(LottoRanking.class);
+
+        for (Lotto lotto : lottos) {
+            LottoRanking lottoRanking = lotto.compare(winnigLotto, bonusNum);
+
+            putInMapIfEmpty(winningList, lottoRanking);
+
+            increasePrizeCount(winningList, lottoRanking);
+        }
+        return winningList;
+    }
+
+    private void increasePrizeCount(Map<LottoRanking, Integer> winningList, LottoRanking lottoRanking) {
+        int prizeCount = winningList.get(lottoRanking);
+        winningList.put(lottoRanking, prizeCount + 1);
+    }
+
+    private void putInMapIfEmpty(Map<LottoRanking, Integer> winningList, LottoRanking lottoRanking) {
+        if (winningList.get(lottoRanking) == null)
+            winningList.put(lottoRanking, 0);
     }
 
 }
