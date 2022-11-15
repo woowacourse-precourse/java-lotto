770fbea04d35fc62a11e19b08b9d66fc13d7f4da
Giho <poqw741@gmail.com>
2022-11-15 21:18:37 +0900

    feat(lotto) : 입력 받는 메서드 생성

--- src/main/java/lotto/domain/WinningNumberInput.java
+++ src/main/java/lotto/domain/WinningNumberInput.java
@@ -1,2 +1,63 @@
-package lotto.domain;public class WinningNumberInput {
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.Lotto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static lotto.util.ErrorMessage.*;
+
+public class WinningNumberInput {
+    private static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public Lotto winningNumbers;
+
+
+    public static Lotto winningNumberInput(){
+        System.out.println(WINNING_NUMBER_INPUT_MESSAGE);
+        Lotto winningNumbers = new Lotto(convertNumbers(Console.readLine()));
+        System.out.println();
+        return winningNumbers;
+    }
+
+    public static List<Integer> convertNumbers(String numbers) {
+        try {
+            return Arrays.stream(numbers.split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(TYPE_ERROR_MESSAGE.getMessage());
+        }
+    }
+
+    public int BonusNumberInput() {
+        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
+        int bonusNumber = convertNumbers(Console.readLine()).get(0);
+        checkBonusNumber(bonusNumber);
+
+        return bonusNumber;
+    }
+
+    private void checkBonusNumber(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException(NUMBER_RANGE_ERROR_MESSAGE.getMessage());
+        }
+
+        for (int number : winningNumbers.getNumbers()) {
+            if (number == bonusNumber) {
+                throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATION_ERROR_MESSAGE.getMessage());
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
