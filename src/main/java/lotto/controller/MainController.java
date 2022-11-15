f2079c39f4b00d355e441b62a81e4584d495e21d
Heosejin98 <sejin3319@naver.com>
2022-11-15 12:09:27 +0900

    Refactor : 코드 컨벤션 수정

--- src/main/java/lotto/controller/MainController.java
+++ src/main/java/lotto/controller/MainController.java
@@ -12,16 +12,13 @@ public class MainController {
 	private final OutputView outputView = OutputView.getInstance();
 
 	public void run() {
-
 		try {
 			int purchaseAmount = inputView.createPurchaseAmount();
 			Wallet wallet = new LottoMachine(purchaseAmount).buyLotto();
 			LottoCompany lottoCompany = new LottoCompany(inputView.winningNumber(), inputView.bonusNumber());
 			PrizeChecker prizeChecker;
-
 			outputView.LottoInformation(wallet.getMyLotto().size(), wallet.getMyLotto());
 			prizeChecker = new PrizeChecker(lottoCompany, wallet.getMyLotto());
-
 			outputView.lottoResult(prizeChecker.getPrizeResult());
 			outputView.yield(prizeChecker.calculationYield(wallet.getReceiptLotto()));
 		} catch (IllegalArgumentException e) {
