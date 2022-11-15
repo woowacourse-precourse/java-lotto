fb896bc4115bc4438e7bc40a3d23563dbc0de67b
CJiu01 <jiwoo020801@gmail.com>
2022-11-15 23:00:02 +0900

    [Feat] LottoResult 생성자 추가

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoResult {
+    private final List<lotto.domain.Rank> ranks;
+
+    private final int investMoney;
+
+    public LottoResult(List<lotto.domain.Rank> ranks, int investMoney) {
+        this.ranks = ranks;
+        this.investMoney = investMoney;
+    }
+
+    public double earningRate() {
+        int earning = 0;
+        for (lotto.domain.Rank rank : ranks) {
+            earning += rank.prize();
+        }
+        return (double) earning / investMoney;
+    }
+
+    public List<lotto.domain.Rank> getRanks() {
+        return ranks;
+    }
+    
+}
