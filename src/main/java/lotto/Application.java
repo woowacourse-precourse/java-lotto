066fb19b3abb5982ae197ebbdfb2f597a3d4c720
abbd2 <ajkety00@gmail.com>
2022-11-15 16:43:57 +0900

    Feat(Application): Lotto의 생성자 매개 값 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        Lotto lotto = new Lotto(List.of(0, 1, 2, 3, 4, 5));
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         lotto.startLotto();
     }
 }
