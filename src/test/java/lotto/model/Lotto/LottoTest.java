8c4f58147ba1fa913b6bc7eecc91678b974a25f4
sooyoungh <sooyoung.h8@gmail.com>
2022-11-15 17:39:34 +0900

    ✅ test(LottoTest): 로또 맞춘 갯수 판단하는 기능 테스트 작성

--- src/test/java/lotto/model/Lotto/LottoTest.java
+++ src/test/java/lotto/model/Lotto/LottoTest.java
@@ -1,8 +1,10 @@
-package lotto;
+package lotto.model.Lotto;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -23,5 +25,16 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("한 티켓에서 로또 당첨 갯수 세기.")
+    @Test
+    void countWinLottoOfTickets () {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Integer strikeZero = lotto.countWinLotto(Arrays.asList(11, 12, 13, 14, 15, 16));
+        Integer strikeThree = lotto.countWinLotto(Arrays.asList(1, 2, 3, 13, 14, 15));
+        Integer strikeAll = lotto.countWinLotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        Assertions.assertEquals(strikeZero, 0);
+        Assertions.assertEquals(strikeThree, 3);
+        Assertions.assertEquals(strikeAll, 6);
+    }
 }
