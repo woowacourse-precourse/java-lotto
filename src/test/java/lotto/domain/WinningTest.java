988a106baedf2f4b381cd206c33e27a13d8da3ba
hhiyeon <hhyyeon0214@gmail.com>
2022-11-15 15:58:23 +0900

    test: winning(test)-check lotto number over than 45

--- src/test/java/lotto/domain/WinningTest.java
+++ src/test/java/lotto/domain/WinningTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class WinningTest {
@@ -26,14 +25,21 @@ public class WinningTest {
     @DisplayName("당첨 번호와 보너스 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void checkWinningAndBonusNumberDuplicated() {
-        assertThatThrownBy(() -> new Winning(List.of(1,2,3,4,5,6), "5"))
+        assertThatThrownBy(() -> new Winning(List.of(1, 2, 3, 4, 5, 6), "5"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호가 1보다 작으면 예외가 발생한다.")
     @Test
     void checkNumberLessOne() {
-        assertThatThrownBy(() -> new Winning(List.of(0,2,3,4,5,6), "5"))
+        assertThatThrownBy(() -> new Winning(List.of(0, 2, 3, 4, 5, 6), "5"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 45보다 크면 예외가 발생한다.")
+    @Test
+    void checkNumberOverFortyFive() {
+        assertThatThrownBy(() -> new Winning(List.of(46, 2, 3, 4, 5, 6), "5"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
