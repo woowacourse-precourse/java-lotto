ba573731590a3d52bff45abb91789b12288174f5
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-11-15 18:33:46 +0900

    feat(lotto): 구매 개수, 생성된 로또 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,13 +1,45 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.Lotto;
+import lotto.domain.LottoGenerator;
+import lotto.domain.NumberOfLottos;
+
 public class OutputView {
 
-    private static final String NUMBER_OF_LOTTOS_PHRASE = "%d개를 구매했습니다.";
+    private static final String NUMBER_OF_LOTTOS_PHRASE = "%d개를 구매했습니다.\n";
+    private static final String FRONT_SQUARE_BRACKETS = "[";
+    private static final String BACK_SQUARE_BRACKETS = "]\n";
+    private static final String DELIMITER = ", %d";
 
     public OutputView() {
     }
 
-    public static void printNumberOfLottos(int numberOfLottos) {
-        System.out.printf(NUMBER_OF_LOTTOS_PHRASE, numberOfLottos);
+    public static void printLottos(NumberOfLottos numberOfLottos, LottoGenerator lottoGenerator) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int priceAmount = numberOfLottos.getPrice();
+
+        String lottoAmountPhrase = String.format(NUMBER_OF_LOTTOS_PHRASE,
+                numberOfLottos.calculateNumberOfLottos(priceAmount));
+        stringBuilder.append(lottoAmountPhrase);
+
+        List<Lotto> lottos = lottoGenerator.getLottos();
+
+        for (Lotto lotto : lottos) {
+            gererateLottosPhrase(lotto, stringBuilder);
+        }
+        System.out.println(stringBuilder);
+    }
+
+    private static void gererateLottosPhrase(Lotto lotto, StringBuilder stringBuilder) {
+        List<Integer> numbers = lotto.getNumbers();
+        stringBuilder.append(FRONT_SQUARE_BRACKETS);
+        stringBuilder.append(numbers.get(0));
+
+        for (int i = 1; i < numbers.size(); i++) {
+            String generatedLotto = String.format(DELIMITER, numbers.get(i));
+            stringBuilder.append(generatedLotto);
+        }
+        stringBuilder.append(BACK_SQUARE_BRACKETS);
     }
 }
