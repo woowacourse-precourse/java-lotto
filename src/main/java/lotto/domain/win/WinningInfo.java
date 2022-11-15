9b52c6ce7ec2f1d7acff843deb4125de59454686
JuSeong1130 <jsmoon1130@naver.com>
2022-11-15 17:24:38 +0900

    feat(WinningInfo): Winning Enum객체에 대한 값을 담고 있기 위한 클래스 구현

---
+++ src/main/java/lotto/domain/win/WinningInfo.java
@@ -0,0 +1,23 @@
+package lotto.domain.win;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class WinningInfo {
+
+	private Map<Winning,Integer> winnings;
+
+	WinningInfo() {
+		winnings = new EnumMap<>(Winning.class);
+		for(Winning winning : Winning.values()){
+			winnings.put(winning,0);
+		}
+	}
+	public void update(final Winning winning) {
+		winnings.put(winning, winnings.get(winning) + 1);
+	}
+
+	public Map<Winning, Integer> getWinnings() {
+		return winnings;
+	}
+}
