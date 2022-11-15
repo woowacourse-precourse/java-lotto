4a621aa06a17ced2295f97efa38afaf9886d8074
kswhj <108874006+kswhj@users.noreply.github.com>
2022-11-15 17:30:10 +0900

    Create ShowNums.java
    출력을 위한 객체 생성

---
+++ src/main/java/lotto/player/ShowNums.java
@@ -0,0 +1,13 @@
+package player;
+
+import java.util.List;
+
+public class ShowNums {
+	public static void run(List<Integer> arr) {
+		int[] arr2 = new int[6];
+		for (int i = 0; i < 6; i++) {
+			arr2[i] = arr.get(i);
+		}
+		System.out.printf("[%d, %d, %d, %d, %d, %d]", arr2[0], arr2[1], arr2[2], arr2[3], arr2[4], arr2[5]);
+	}
+}
