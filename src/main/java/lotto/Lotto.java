9f5a2ced31b137aaf265b4c00fdc822f54c23519
lsw725 <725lsw@hanmail.net>
2022-11-15 09:28:55 +0900

    feat(Lotto): 두 개의 로또를 비교하여 같은 원소를 갖고 있는지 세는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Lotto {
@@ -11,10 +12,61 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        raiseErrorIfNotMatchNumberSize(numbers, 6);
+        raiseErrorIfDuplicatedElement(numbers);
+    }
+
+    private void raiseErrorIfNotMatchNumberSize(List<Integer> numbers, Integer mustBe) {
+        if (numbers.size() != mustBe) {
+            throw new IllegalArgumentException("[ERROR] 길이가 " + mustBe.toString() + "이 아닙니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void raiseErrorIfDuplicatedElement(List<Integer> numbers) {
+        if (hasDuplicatedElement(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 존재합니다.");
+        }
+    }
+
+    private boolean hasDuplicatedElement(List<Integer> numbers) {
+        int size = numbers.size();
+        for (int i = 0; i < size; ++i) {
+            if (numbers.subList(i+1, size).contains(numbers.get(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Integer compare(Lotto other) {
+        try {
+            return compareTwoNumbers(numbers, other.getNumbers());
+        } catch (IndexOutOfBoundsException e) {
+            logError(e);
+        }
+        return 0;
+    }
+
+    private Integer compareTwoNumbers(List<Integer> target, List<Integer> source) {
+        return countEqualElement(target, source);
+    }
+
+    private Integer countEqualElement(List<Integer> target, List<Integer> source) {
+        int size = source.size();
+        Integer count = 0;
+        for (int i : source) {
+            if (target.contains(i)) {
+                ++count;
+            }
+        }
+        return count;
+    }
+
+    private void logError(Exception exception) {
+        System.out.println(exception.getMessage());
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -18,10 +19,87 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("같은 번호의 로또 두 개를 비교하여 같은 원소가 6이 아니라면 예외가 발생한다.")
+    @Test
+    void compareLottosAllEquals() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        int count = lotto.compare(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+
+        assertThat(count).isEqualTo(6);
+    }
+
+    @DisplayName("1개의 원소만 다른 로또 두 개를 비교하여 같은 원소가 5이 아니라면 예외가 발생한다.")
+    @Test
+    void compareLottosOneDiff() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        int count = lotto.compare(new Lotto(List.of(1, 2, 3, 4, 5, 7)));
+
+        assertThat(count).isEqualTo(5);
+    }
+
+    @DisplayName("2개의 원소만 다른 로또 두 개를 비교하여 같은 원소가 4이 아니라면 예외가 발생한다.")
+    @Test
+    void compareLottosTwoDiff() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        int count = lotto.compare(new Lotto(List.of(1, 2, 3, 4, 7, 8)));
+
+        assertThat(count).isEqualTo(4);
+    }
+
+    @DisplayName("3개의 원소만 다른 로또 두 개를 비교하여 같은 원소가 3이 아니라면 예외가 발생한다.")
+    @Test
+    void compareLottosThreeDiff() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        int count = lotto.compare(new Lotto(List.of(1, 2, 3, 7, 8, 9)));
+
+        assertThat(count).isEqualTo(3);
+    }
+
+    @DisplayName("4개의 원소만 다른 로또 두 개를 비교하여 같은 원소가 2이 아니라면 예외가 발생한다.")
+    @Test
+    void compareLottosFourDiff() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        int count = lotto.compare(new Lotto(List.of(1, 2, 7, 8, 9, 10)));
+
+        assertThat(count).isEqualTo(2);
+    }
+
+    @DisplayName("5개의 원소만 다른 로또 두 개를 비교하여 같은 원소가 1이 아니라면 예외가 발생한다.")
+    @Test
+    void compareLottosFiveDiff() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        int count = lotto.compare(new Lotto(List.of(1, 7, 8, 9, 10, 11)));
+
+        assertThat(count).isEqualTo(1);
+    }
+
+    @DisplayName("모든 원소가 다른 로또 두 개를 비교하여 같은 원소가 0이 아니라면 예외가 발생한다.")
+    @Test
+    void compareLottosAllDiff() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        int count = lotto.compare(new Lotto(List.of(7, 8, 9, 10, 11, 12)));
+
+        assertThat(count).isEqualTo(0);
+    }
+
+    @DisplayName("모든 원소가 같은 정렬되지 않은 로또 두 개를 비교하여 같은 원소가 6이 아니라면 예외가 발생한다.")
+    @Test
+    void compareLottosRandomPos() {
+        Lotto lotto = new Lotto(List.of(6, 4, 2, 3, 1, 5));
+
+        int count = lotto.compare(new Lotto(List.of(4, 3, 1, 6, 5, 2)));
+
+        assertThat(count).isEqualTo(6);
+    }
 }
