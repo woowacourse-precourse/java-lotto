b09b375e89f52b4f97a25e74a663316355179508
0112leesy <0112leesy@naver.com>
2022-11-15 15:08:11 +0900

    fix(Rank) : Rank enum 필드값 추가
    등수 별 일치하는 번호의 개수 및 상금 필드 추가
    5개의 번호와 보너스 번호가 일치하는 5등은 7개의 번호가 일치한다고 가정

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -2,20 +2,32 @@ package lotto;
 
 public enum Rank {
 
-    FIRST("1등"),
-    SECOND("2등"),
-    THIRD("3등"),
-    FOURTH("4등"),
-    FIFTH("5등");
+    FIRST("1등", 6, 2000000000),
+    SECOND("2등", 7, 30000000),
+    THIRD("3등", 5, 1500000),
+    FOURTH("4등", 4, 50000),
+    FIFTH("5등", 3, 5000);
 
     private final String message;
+    private final int wins;
+    private final int price;
 
-    Rank(String message) {
+    Rank(String message, int wins, int price) {
         this.message = message;
+        this.wins = wins;
+        this.price = price;
     }
 
-    public String message() {
+    public String getMessage() {
         return message;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public int getPrice() {
+        return price;
     }
 
 }
