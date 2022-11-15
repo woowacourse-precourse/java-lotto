a050f2b5ce434b92b7498b4fbab741ed39cd72c9
sujin0607 <goweg0607@gmail.com>
2022-11-15 19:00:41 +0900

    refactor: message값 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -4,39 +4,46 @@ package lotto.domain;
 import java.util.List;
 
 public enum Rank {
-    THREE(3,5000),
-    FOUR(4,50_000),
-    FIVE(5,1_500_000),
-    FIVEBONUS(5,3_000_000),
-    SIZ(6,2_000_000_000);
+    THREE(3, 5000, "3개 일치 (5,000원)", 0),
+    FOUR(4, 50_000, "4개 일치 (50,000원)", 1),
+    FIVE(5, 1_500_000, "5개 일치 (1,500,000원)", 2),
+    FIVEBONUS(5, 3_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원)", 3),
+    SIZ(6, 2_000_000_000, "6개 일치 (2,000,000,000원)", 4);
 
     private final int rightNumber;
     private final int prize;
+    private final String message;
+    private final int index;
 
-    Rank(int rightNumber, int prize) {
+    Rank(int rightNumber, int prize, String message, int index) {
         this.rightNumber = rightNumber;
         this.prize = prize;
+        this.message = message;
+        this.index = index;
     }
 
     public int getRightNumber() {
         return rightNumber;
     }
 
-    public int getPrice() {
+    public int getPrize() {
         return prize;
     }
 
-    public Rank valueOf(int rightNumber, boolean bonusBall) {
-        if (rightNumber == Rank.FIVEBONUS.rightNumber || bonusBall == true) {
-            return Rank.FIVEBONUS;
-        }
+    public int getIndex() {
+        return index;
+    }
 
+    public String getMessage() {
+        return message;
+    }
+
+    public static Rank valueOf(int index) {
         for (Rank rank : Rank.values()) {
-            if (rank.rightNumber == rightNumber) {
+            if (rank.index == index) {
                 return rank;
             }
         }
-
         return null;
     }
 }
