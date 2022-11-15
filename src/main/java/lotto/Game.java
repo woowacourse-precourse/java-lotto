b54c705a7f99c5e992e512990654e7278558537b
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-15 12:00:20 +0900

    feat: issue lottos

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,9 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     public void playGame() {
         new Print().inputMoney();
         int numberOfLottos = Input.inputPurchaseMoney();
         new Output().outputNumberOfLotto(numberOfLottos);
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < numberOfLottos; i++) {
+            List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(lotto));
+        }
     }
 }
