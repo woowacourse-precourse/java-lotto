6d26e144ad8db900dccc421659e7dd60cadb27cd
hyun0404woo <hyun0404woo@gmail.com>
2022-11-15 17:59:17 +0900

    feat: 사용자의 구매 금액을 입력받아 로또를 구매하는 메서드
    - 사용자가 구매한 로또를 관리하는 LottoUser 객체 반환

--- src/main/java/lotto/service/LottoObjectManager.java
+++ src/main/java/lotto/service/LottoObjectManager.java
@@ -1,6 +1,10 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.LottoUser;
 import lotto.util.converter.LottoInputConverter;
+
+import static lotto.util.ConstValue.LottoConst.*;
 
 public class LottoObjectManager {
 
@@ -8,5 +12,20 @@ public class LottoObjectManager {
 
     public LottoObjectManager() {
         inputConverter = new LottoInputConverter();
+    }
+
+    public LottoUser createLottoUser() {
+        System.out.println(USER_LOTTO_PURCHASE_INPUT_MESSAGE);
+        final String input = Console.readLine();
+        final int purchaseCost = inputConverter.userLottoPurchaseCostToInt(input);
+
+        final LottoUser lottoUser = new LottoUser(purchaseCost);
+        showPurchaseLottoMessage(purchaseCost);
+
+        return lottoUser;
+    }
+
+    private void showPurchaseLottoMessage(final int purchaseCost) {
+        System.out.println(purchaseCost / LOTTO_BASIC_COST + USER_LOTTO_PURCHASE_TAIL_MESSAGE);
     }
 }
