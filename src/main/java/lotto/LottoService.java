8f5ecb1e38900a9e0b4a9c3b92a019f4a23307f7
jxixeun <jxixeun@gmail.com>
2022-11-15 20:00:54 +0900

    feat: 사용자 입력과 로또 번호 비교 검사 기능 작성

---
+++ src/main/java/lotto/LottoService.java
@@ -0,0 +1,20 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoService {
+
+    public int matchNumbers(List<Integer> lotto, List<Integer> userLotto){
+        int result = 0;
+        for (int i=0;i<6;i++){
+            if (lotto.get(i) == userLotto.get(i)){
+                result += 1;
+            }
+        }
+        return result;
+    }
+
+    public boolean matchBonus(List<Integer> lotto, int bonusNumber) {
+        return lotto.contains(bonusNumber);
+    }
+}
