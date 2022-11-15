5043362899295658251aa1b5b41fd43f7889c1b6
pasly0920 <inwoo920621@gmail.com>
2022-11-15 20:34:31 +0900

    fix: 15줄 제한 준수 lotto 결과 출력 부분 간소화

--- src/main/java/lotto/view/LottoResultRespondent.java
+++ src/main/java/lotto/view/LottoResultRespondent.java
@@ -24,22 +24,19 @@ public class LottoResultRespondent {
     }
 
     private Integer printLottoWinningResult(List<LottoGrade> lottoWinningResult) {
-        Integer lottoWinningProfitAmount = 0;
         Integer gradeFifthFrequency = Collections.frequency(lottoWinningResult, LottoGrade.FIFTH);
         System.out.println("3개 일치 (5,000원) - " + gradeFifthFrequency + "개");
-        lottoWinningProfitAmount += gradeFifthFrequency * 5000;
         Integer gradeFourthFrequency = Collections.frequency(lottoWinningResult, LottoGrade.FOURTH);
         System.out.println("4개 일치 (50,000원) - " + gradeFourthFrequency + "개");
-        lottoWinningProfitAmount += gradeFourthFrequency * 50000;
         Integer gradeThirdFrequency = Collections.frequency(lottoWinningResult, LottoGrade.THIRD);
         System.out.println("5개 일치 (1,500,000원) - " + gradeThirdFrequency + "개");
-        lottoWinningProfitAmount += gradeThirdFrequency * 1500000;
         Integer gradeSecondFrequency = Collections.frequency(lottoWinningResult, LottoGrade.SECOND);
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + gradeSecondFrequency + "개");
-        lottoWinningProfitAmount += gradeSecondFrequency * 30000000;
         Integer gradeFirstFrequency = Collections.frequency(lottoWinningResult, LottoGrade.FIRST);
         System.out.println("6개 일치 (2,000,000,000원) - " + gradeFirstFrequency + "개");
-        lottoWinningProfitAmount += gradeFirstFrequency * 2000000000;
+        Integer lottoWinningProfitAmount = gradeFifthFrequency * 5000 + gradeFourthFrequency * 50000
+                + gradeThirdFrequency * 1500000 + gradeSecondFrequency * 30000000
+                + gradeFirstFrequency * 2000000000;
         return lottoWinningProfitAmount;
     }
 
