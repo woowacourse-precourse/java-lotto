5bfa34718c35b183c011a3b3950ae46410d8ceb7
Seoyoung <ryupark1006@gmail.com>
2022-11-15 12:53:07 +0900

    feat: 로또 숫자 생성 기능 구현

---
+++ src/main/java/lotto/LottoCreator.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoCreator {
+
+    public static List<Integer> createNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
+}
