3422bd5c8da2d436ae395d1339de87377b79ce62
DaWoon Lee <qkgk61@gmail.com>
2022-11-15 22:17:51 +0900

    Refactor : 하나의 테스트 메서드에서 하나의 경우만 테스트 하도록 분리

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -7,10 +7,15 @@ import org.junit.jupiter.api.Test;
 
 public class RankTest {
 
-    @DisplayName("당첨 번호와 같은 번호의 수, 보너스 번호와 같은 번호의 수에 대응되는 등수를 확인한다.")
+    @DisplayName("당첨 번호와 같은 번호의 수, 보너스 번호와 같은 번호의 수에 대응되는 등수는 존재한다.")
     @Test
-    void getRank() {
+    void getRank_Present() {
         assertThat(Rank.getRank(5, 1)).isPresent();
+    }
+
+    @DisplayName("당첨 번호와 같지 않은 번호의 수, 보너스 번호와 같지 않은 번호의 수에 대응되는 등수는 존재하지 않는다.")
+    @Test
+    void getRank_Empty() {
         assertThat(Rank.getRank(7, 0)).isEmpty();
     }
 
