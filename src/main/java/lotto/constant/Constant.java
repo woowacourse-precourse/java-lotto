df38d83bc0ddf0e3d3d1bf129ebbf10192964572
leesangjin <tkdwls0628@gmail.com>
2022-11-15 16:34:38 +0900

    feat(Constant): 로또와 관련된 상수 정의

---
+++ src/main/java/lotto/constant/Constant.java
@@ -0,0 +1,7 @@
+package lotto.constant;
+
+public class Constant {
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
+}
