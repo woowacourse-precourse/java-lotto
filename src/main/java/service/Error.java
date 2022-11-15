34f4d454bd467cc713a79cd4a2ea9eef97e74ca0
YooSuYeon <usuyn000@gmail.com>
2022-11-15 15:34:04 +0900

    feat: 당첨 번호 안 중복 검사

--- src/main/java/service/Error.java
+++ src/main/java/service/Error.java
@@ -3,7 +3,8 @@ package service;
 public enum Error {
     PURCHASE_AMOUNT_FORMAT("[ERROR] 구입 금액은 숫자만 입력해야 합니다."),
     PURCHASE_AMOUNT_VALUE("[ERROR] 구입 금액은 1,000원 단위만 입력해야 합니다."),
-    WINNING_NUMBERS_FORMAT("[ERROR] 1~45 사이 번호 6개를 콤마와 공백 없이 입력해야 합니다. (e.g. 1,2,3,4,5,6)");
+    WINNING_NUMBERS_FORMAT("[ERROR] 1부터 45 사이의 번호 6개를 콤마와 공백 없이 입력해야 합니다. (e.g. 1,2,3,4,5,6)"),
+    WINNING_NUMBERS_INCLUDE_SAME_NUMBER("[ERROR] 6개의 번호 중 중복되는 번호가 존재합니다.");
 
     private final String errorText;
 
--- src/main/java/service/LottoService.java
+++ src/main/java/service/LottoService.java
@@ -4,9 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 import domain.Lotto;
 import repository.LottoRepository;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class LottoService {
     private final LottoRepository lottoRepository;
@@ -37,8 +39,16 @@ public class LottoService {
 
     }
 
-    private void validateWinningNumbers(String numbers) {
+    private List<Integer> validateWinningNumbers(String numbers) {
         checkWinningNumbersFormat(numbers);
+
+        List<Integer> winningNumbers = Arrays.stream(numbers.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        checkIncludeSameNumber(winningNumbers);
+
+        return winningNumbers;
     }
 
     private void checkWinningNumbersFormat(String numbers) {
@@ -46,6 +56,16 @@ public class LottoService {
 
         if (!Pattern.matches(winningNumbers.getRegex(), numbers)) {
             throw new IllegalArgumentException(Error.WINNING_NUMBERS_FORMAT.getText());
+        }
+    }
+
+    private void checkIncludeSameNumber(List<Integer> winningNumbers) {
+        int uniqueNumberCount = (int) winningNumbers.stream()
+                .distinct()
+                .count();
+
+        if (uniqueNumberCount != Rule.PER_LOTTO.getCount()) {
+            throw new IllegalArgumentException(Error.WINNING_NUMBERS_INCLUDE_SAME_NUMBER.getText());
         }
     }
 }
