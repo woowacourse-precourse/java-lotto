f2c18b0debda5b000a3942a140d97912ebcf9d4c
hojunking96 <junsong96@naver.com>
2022-11-15 11:01:25 +0900

    feats(java-lotto) : 출력 문구 상수로 저장

---
+++ src/main/java/lotto/data/OutputConstants.java
@@ -0,0 +1,12 @@
+package lotto.data;
+
+public class OutputConstants {
+    public final static String boughtOutput = "개를 구매했습니다.";
+    public final static String inputWinNumOutput = "당첨 번호를 입력해 주세요.";
+    public final static String inputBonusNumOutput = "보너스 번호를 입력해 주세요.";
+    public final static String winResultOutput = "당첨 통계";
+    public final static String linebreakOutput = "---";
+    public final static String totalRevenueOutput1 = "총 수익률은 ";
+    public final static String totalRevenueOutput2 = "%입니다.";
+
+}
