ef402bca802ace4c7040979dff1e95ad57ed2d63
kswhj <108874006+kswhj@users.noreply.github.com>
2022-11-15 11:33:34 +0900

    Update Player.java
    로또 구입 단계 기능 구현

--- src/main/java/lotto/player/Player.java
+++ src/main/java/lotto/player/Player.java
@@ -1,1 +1,16 @@
+package player;
 
+import java.util.List;
+
+public class Player {
+	public static void start() {
+		try {
+			int coin = Coin.CoinIn();
+			CoinErrorCheck.CEC(coin);
+			int chance = Chance.run(coin);
+			List<List<Integer>> playerPick = PlayerChoose.run(chance);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+	}
+}
