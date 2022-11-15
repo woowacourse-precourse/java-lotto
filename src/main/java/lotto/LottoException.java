302873fb9034470f5015670c72a8f849518aea91
tenta <eunoia3802@gmail.com>
2022-11-15 23:56:40 +0900

    refactor: 에러 문자열 변수 상수화

--- src/main/java/lotto/LottoException.java
+++ src/main/java/lotto/LottoException.java
@@ -4,6 +4,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoException {
+    private final String INPUT_MONEY_UNIT_ERROR = "[ERROR]: 금액은 1000원 단위로 입력해야 합니다.";
+    private final String DUPLICATE_ERROR = "[ERROR]: 중복된 숫자는 입력할 수 없습니다.";
+    private final String NUMBER_RANGE_ERROR = "[ERROR]: 담청 번호는 총 6자리를 입력해야 합니다.";
+    private final String INPUT_BONUS_NUMBER_ERROR = "[ERROR]: 보너스 번호의 입력 범위는 1~45까지 입니다.";
 
     public void inputMoneyException(int inputMoney) {
         if (!isNumber(inputMoney)) {
@@ -13,7 +17,7 @@ public class LottoException {
 
     public void inputMoneyUnitException(int inputMoney, int lottoPrice) {
         if (inputMoney % lottoPrice != 0) {
-            System.out.println("[ERROR]: 금액은 1000원 단위로 입력해야 합니다.");
+            System.out.println(INPUT_MONEY_UNIT_ERROR);
             throw new IllegalArgumentException();
         }
     }
@@ -30,11 +34,11 @@ public class LottoException {
         for (String number : winningNumbers) {
             int count = Collections.frequency(List.of(winningNumbers), number);
             if (count > 1) {
-                System.out.println("[ERROR]: 중복된 숫자는 입력할 수 없습니다.");
+                System.out.println(DUPLICATE_ERROR);
                 throw new IllegalArgumentException();
             }
             if (winningNumbers.length != 6) {
-                System.out.println("[ERROR]: 담청 번호는 총 6자리를 입력해야 합니다.");
+                System.out.println(NUMBER_RANGE_ERROR);
                 throw new IllegalArgumentException();
             }
         }
@@ -61,7 +65,7 @@ public class LottoException {
 
     public void bonusNumberException(int bonusNumber) {
         if (bonusNumber < 1 || bonusNumber > 45) {
-            System.out.println("[ERROR]: 보너스 번호의 입력 범위는 1~45까지 입니다.");
+            System.out.println(INPUT_BONUS_NUMBER_ERROR);
             throw new IllegalArgumentException();
         }
     }
