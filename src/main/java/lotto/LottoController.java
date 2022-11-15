5f86720ee20038a9d53c6241bc5ff92c03f80391
inwoo <korean5078@gmail.com>
2022-11-15 20:43:11 +0900

    perf : 수익률 반올림 코드 구현

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -166,5 +166,6 @@ public class LottoController {
             profit += prizes[i].getCount() * prices[i];
         }
         profit = profit / (NumberOfLotto * 1000) * 100;
+        profit = Math.round(profit * 10)/10.0;
     }
 }
