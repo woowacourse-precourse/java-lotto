fa28fe6b548ad32e459595f15b7d0ca6b294e332
chaos0103 <lyt1228@naver.com>
2022-11-15 11:10:57 +0900

    refactor(Lotto): 매서드 추출 및 람다식 변경

--- src/main/java/lotto/domain/impl/IssueImpl.java
+++ src/main/java/lotto/domain/impl/IssueImpl.java
@@ -4,8 +4,9 @@ import lotto.Lotto;
 import lotto.domain.Issue;
 import lotto.domain.NumberGenerator;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static lotto.Constants.*;
 import static lotto.message.ExceptionMessage.AMOUNT_EXCEPTION;
@@ -14,25 +15,28 @@ public class IssueImpl implements Issue {
 
     private final NumberGenerator numberGenerator = new NumberGeneratorImpl();
 
-    //예외
-    //1. 금액이 1,000원으로 나누어 떨어지지 않는 경우
     @Override
     public List<Lotto> createLotto(int amount) {
-        validate(amount);
-        int count = amount / LOTTO_PRICE;
+        int lottoIssueCount = getLottoIssueCount(amount);
 
-        List<Lotto> result = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            List<Integer> numbers = numberGenerator.createRandomNumbers();
-            Lotto lotto = new Lotto(numbers);
-            result.add(lotto);
-        }
-        return result;
+        return IntStream.range(0, lottoIssueCount)
+                .mapToObj(i -> numberGenerator.createRandomNumbers())
+                .map(Lotto::new)
+                .collect(Collectors.toList());
+    }
+
+    private int getLottoIssueCount(int amount) {
+        validate(amount);
+        return amount / LOTTO_PRICE;
     }
 
     private void validate(int amount) {
-        if (amount % LOTTO_PRICE != 0) {
+        if (!isMultipleOfLottoPrice(amount)) {
             throw new IllegalArgumentException(AMOUNT_EXCEPTION);
         }
+    }
+
+    private boolean isMultipleOfLottoPrice(int amount) {
+        return amount % LOTTO_PRICE == 0;
     }
 }
