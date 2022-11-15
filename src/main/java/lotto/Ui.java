83612782959c7c3658481709f1501854974a91a7
bfinecpa1@naver.com <bfinecpa1@naver.com>
2022-11-15 18:37:15 +0900

    feat: 몇 개의 로또를 살 수 있는지 구하는 기능
    구입 금액을 통해 몇개의 로또를 살 수 있는 구하는 기능

--- src/main/java/lotto/Ui.java
+++ src/main/java/lotto/Ui.java
@@ -8,12 +8,22 @@ public class Ui {
     public void startLotto(){
 
         String purchaseAmount = inputPurchaseAmount();
+
+        int lottoCount = getLottoCount(purchaseAmount);
+
+
     }
 
     private String inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
          return readLine();
     }
+
+    private int getLottoCount(String input){
+        int purchaseAmount = Integer.parseInt(input);
+        return purchaseAmount / Money.THOUSAND.getAmount();
+    }
+
 
 
 }
