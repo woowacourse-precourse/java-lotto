04e18ba31861b85090f8c53db44889e90c8a438b
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-15 13:33:40 +0900

    feat : 새로운 에러 메세지 생성

--- src/main/java/lotto/common/Error.java
+++ src/main/java/lotto/common/Error.java
@@ -5,7 +5,8 @@ public enum Error {
     THOUSAND("[ERROR] 천원단위여야 합니다."),
     MINUS("[ERROR] 최소 천원이어야 합니다."),
     FORMAT("[ERROR] 6글자여야 합니다."),
-    SIZE("[ERROR] 1에서 45사이여야 합니다.");
+    SIZE("[ERROR] 1에서 45사이여야 합니다."),
+    DUPLICATE("[ERROR] 중복된 숫자가 있으면 안됩니다.");
     private String msg;
 
     Error(String msg) {
