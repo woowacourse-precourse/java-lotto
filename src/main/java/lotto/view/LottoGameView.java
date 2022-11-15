8ec336abe553c0e6b470757d87cdaec27f257ee2
gomljo <gomljo0307@naver.com>
2022-11-15 14:19:22 +0900

    feat(LottoGameView): skeleton 코드 작성

--- src/main/java/lotto/view/LottoGameView.java
+++ src/main/java/lotto/view/LottoGameView.java
@@ -2,19 +2,56 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.model.Buyer;
+import lotto.model.Lotto;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class LottoGameView{
+
+
     private String purchaseAmount;
     private String raffleNumbers;
     private String bonusNumber;
+
     public void askPurchaseAmount(){
-        purchaseAmount = Console.readLine();
+
     }
 
     public void askRaffleNumbers(){
-        raffleNumbers= Console.readLine();
+
     }
+
     public void askBonusNumber(){
-        bonusNumber=Console.readLine();
+
+    }
+
+    public void noticeNumberOfTickets(int numberOfTickets){
+
+    }
+
+    public void noticeRateOfRevenue(float rateOfRevenue){
+
+    }
+
+    public void noticeLotteries(String lottoNumbers){
+
+    }
+    public void noticeReport(String winReport){
+
+    }
+
+    public String getPurchaseAmount(){
+        return purchaseAmount;
+    }
+
+    public String getRaffleNumbers(){
+        return raffleNumbers;
+    }
+
+    public String getBonusNumber(){
+        return bonusNumber;
     }
 }
