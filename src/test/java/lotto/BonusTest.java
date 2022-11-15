bf8ce3f107cd527bdfa4630839ba047761b35ba9
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-15 22:34:09 +0900

    Test: Add BonusByNotNumber Test Function

--- src/test/java/lotto/BonusTest.java
+++ src/test/java/lotto/BonusTest.java
@@ -1,5 +1,21 @@
 package lotto;
 
-public class BonusTest {
-    
+import lotto.exception.BonusNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BonusTest {
+    @DisplayName("보너스 번호가 숫자가 아닐 경우 오류가 발생한다.")
+    @Test
+    void createBonusByNotNumber() {
+        assertThatThrownBy(() ->
+                BonusNumber.exceptionBonusNumber("a",List.of(1, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
