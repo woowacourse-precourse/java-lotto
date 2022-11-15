edf8e9d54d78a607e7ac9e0472631f6c781ec7a4
leesangjin <tkdwls0628@gmail.com>
2022-11-15 17:00:13 +0900

    feat(User): 검증 기능 추가

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,11 +1,14 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.validator.MoneyAmountValidator;
 
 public class User {
     public int inputOfAmount(){
         String userInput = Console.readLine();
+        MoneyAmountValidator.validateInputType(userInput);
         int moneyAmount = Integer.parseInt(userInput);
+        MoneyAmountValidator.validateMoneyAmount(moneyAmount);
         return moneyAmount;
     }
 }
