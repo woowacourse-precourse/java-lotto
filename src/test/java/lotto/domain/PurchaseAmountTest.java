3781807764fa4505b25cced3a3e460c91d5692b3
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-15 20:45:57 +0900

    refactor: 사용하지 않는 import 문 제거

--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -1,7 +1,5 @@
 package lotto.domain;
 
-import lotto.domain.ExceptionMessage;
-import lotto.domain.PurchaseAmount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
--- src/test/java/lotto/domain/PurchaseLottosTest.java
+++ src/test/java/lotto/domain/PurchaseLottosTest.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import lotto.domain.PurchaseLottos;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
