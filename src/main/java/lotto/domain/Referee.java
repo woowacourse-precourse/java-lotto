e2335d4e3f9aa142e5121d49ae34a7d589cebd68
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-15 19:32:59 +0900

    feat(LottoGame): 구매한 로또 번호들과 당첨 번호를 비교

--- src/main/java/lotto/domain/Referee.java
+++ src/main/java/lotto/domain/Referee.java
@@ -1,4 +1,26 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Referee {
+    private final List<Lotto> lottos;
+    private final Lotto winningNumbers;
+    private final int bonusNumber;
+
+    public Referee(List<Lotto> lottos, Lotto winningNumbers, int bonusNumber) {
+        this.lottos = lottos;
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getWinningNumberMatchCount(Lotto lotto) {
+        int winNumberMatchCount = 0;
+        for (Integer winningNumber : winningNumbers.getNumbers()) {
+            if (lotto.getNumbers().contains(winningNumber)) {
+                winNumberMatchCount++;
+            }
+        }
+        return winNumberMatchCount;
+    }
 }
