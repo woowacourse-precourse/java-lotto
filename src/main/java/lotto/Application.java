e2b6a1112aadc1059e2e179f73ec68d4c1904a8d
LisaJeoung <jlisa9912234567890@gmail.com>
2022-11-15 19:25:14 +0900

    feat : main 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int numberOfLotto = Lotto.getPurchaseAmount();
+        List<Integer>[] myNums = Lotto.createNumbers(numberOfLotto);
+        Lotto.printNumbers(myNums);
+        List<Integer> luckyNumber = Lotto.getLuckyNumber();
+        Lotto lotto = new Lotto(luckyNumber);
+        int bonusNumber = lotto.getBonusNumber();
+
+        lotto.getResult(myNums, bonusNumber);
+        lotto.printResult(bonusNumber);
+
     }
 }
