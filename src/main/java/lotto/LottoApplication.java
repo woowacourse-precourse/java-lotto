6bd995a8841283655404d33153703a83071fb2c3
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2022-11-15 13:29:27 +0900

    refactor(LottoApplication): runApplication 메서드 분리
    - 해당 메서드가 전반적인 일을 전부 하는 것 같아 크게 두가지 과정으로 분리 가능하므로 분리함
    - buyLotto(), showRewardResult() 에서 공유해야하는 변수들은 field 값으로 설정함
    - 리팩토링 후 테스트 통과 확인

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -9,13 +9,15 @@ import lotto.reward.RewardCoordinator;
 import lotto.setting.LottoApplicationSetting;
 import lotto.winningnumber.WinningNumber;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class LottoApplication {
 
     private final Input input;
     private final Output output;
     private final LottoApplicationSetting setting;
+
+    private Integer purchasePrice;
+    private List<Lotto> lottos;
 
     LottoApplication(Input input, Output output, LottoApplicationSetting setting) {
         this.input = input;
@@ -32,20 +34,28 @@ public class LottoApplication {
     }
 
     private void runApplication() {
-        Integer purchasePrice = inputInteger(InputRequestMessage.PURCHASE_AMOUNT);
+        buyLotto();
+        showRewardResult();
+    }
+
+    private void buyLotto() {
+        purchasePrice = inputInteger(InputRequestMessage.PURCHASE_AMOUNT);
 
         LottoShop lottoShop = setting.createLottoShop();
-        List<Lotto> lottos = lottoShop.buyLotto(purchasePrice);
+        lottos = lottoShop.buyLotto(purchasePrice);
+
         String lottosInfo = lottoShop.getLottoInfo(lottos);
         output.print(lottosInfo);
+    }
 
+    private void showRewardResult() {
         List<Integer> winningNumbers = inputIntegerList(InputRequestMessage.WINNING_NUMBER);
         List<Integer> bonusNumbers = inputIntegerList(InputRequestMessage.BONUS_NUMBER);
 
         WinningNumber winningNumber = setting.createWinningNumber(winningNumbers, bonusNumbers);
         RewardCoordinator rewardCoordinator = setting.createRewardCoordinator(winningNumber, purchasePrice);
-        String result = rewardCoordinator.getRewardResult(lottos);
-        output.print(result);
+        String rewardResult = rewardCoordinator.getRewardResult(lottos);
+        output.print(rewardResult);
     }
 
     private Integer inputInteger(InputRequestMessage message) {
