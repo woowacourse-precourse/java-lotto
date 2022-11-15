0131945122c807c7bcbc031515b985efe8f53f2c
Seungyong Choi <ppyooy336@naver.com>
2022-11-15 15:14:45 +0900

    test: 입력받은 금액이 1,000단위로 나누어 떨어지지 않는다면 예외 처리한다 테스트 코드 작성

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -7,7 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
-
-
+    @Test
+    public void 단위가_1000원이_아닌경우(){
+        int money = 1100;
+        User user = new User();
+        Assertions.assertThatIllegalArgumentException().isThrownBy(()
+                -> user.isMoneyDividedWith1000(money));
+    }
 
 }
