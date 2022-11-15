7f3d6e52fd943ee392e4788114e17c9c20f57eec
KimRiun <kimriun27@gmail.com>
2022-11-15 18:52:34 +0900

    test: 사용하지 않는 메소드 삭제

--- src/test/java/lotto/PriceTest.java
+++ src/test/java/lotto/PriceTest.java
@@ -1,12 +1,11 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class PriceTest extends NsTest {
+public class PriceTest {
     @Test
     void 구입금액이_정상적인_경우_테스트() {
         Price price = new Price("8000");
@@ -29,10 +28,5 @@ public class PriceTest extends NsTest {
     void 구입금액이_1000으로_나누어_떨어지지_않는_경우_테스트() {
         assertThatThrownBy(() -> new Price("8100"))
                 .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Override
-    public void runMain() {
-        return;
     }
 }
