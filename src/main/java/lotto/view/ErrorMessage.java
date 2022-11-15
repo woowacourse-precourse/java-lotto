21e1526c8e618ef4bad9cbef264a2a4b81d2af47
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-15 18:55:01 +0900

    feat: 보너스 예외처리 문장 추가

--- src/main/java/lotto/view/ErrorMessage.java
+++ src/main/java/lotto/view/ErrorMessage.java
@@ -4,7 +4,10 @@ public enum ErrorMessage {
     numberType("[ERROR] 올바른 형식의 숫자를 입력해 주세요."),
     numberMultiple("[ERROR] 로또는 1000원 단위로만 구매하실 수 있습니다."),
     lottoIndex("[ERROR] 6개의 숫자를 입력해 주세요."),
-    lottoRepeatation("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    lottoRepetition("[ERROR] 로또 번호는 중복되지 않아야 합니다."),
+    bonusLength("[ERROR] 한자리 숫자를 입력해 주세요."),
+    bonusType("[ERROR] 숫자를 입력해 주세요."),
+    bonusRepetition("[ERROR] 로또 번호와 중복되지 않아야 합니다.");
     private final String value;
 
     ErrorMessage(String value) {
