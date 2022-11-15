a67362292411139f668a84a508514e9cc7b66d5b
JeonghakLee <wjdgkr0527@naver.com>
2022-11-15 14:43:01 +0900

    test: InputServiceTest 작성

--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 
 public class InputService {
 
-    private static final String winningNumberFormat = "^[0-9]+(,[0-9]+){5}";
+    private static final String WINNING_NUMBER_INPUT_FORMAT = "^[0-9]+(,[0-9]+){5}";
 
     public List<Integer> getWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
@@ -18,9 +18,9 @@ public class InputService {
         return convertToIntegerList(input);
     }
 
-    private void validateWinningNumbersFormat(String input) {
-        if (!input.matches(winningNumberFormat)) {
-            throw new IllegalArgumentException();
+    void validateWinningNumbersFormat(String input) {
+        if (!input.matches(WINNING_NUMBER_INPUT_FORMAT)) {
+            throw new IllegalArgumentException("당첨 번호의 입력 형식에 맞지 않습니다. 입력형식 예시: 1,2,3,4,5,6");
         }
     }
 
@@ -32,12 +32,13 @@ public class InputService {
 
     public Integer getPurchaseAmount() {
         System.out.println("구입 금액을 입력해 주세요.");
-        int purchasedAmount = getInteger();
+        String input = readLine();
+        int purchasedAmount = getInteger(input);
         validatePurchaseAmount(purchasedAmount);
         return purchasedAmount;
     }
 
-    private void validatePurchaseAmount(int purchaseAmount) {
+    void validatePurchaseAmount(int purchaseAmount) {
         if (purchaseAmount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(
                 String.format("로또는 %d원 단위로만 구매할 수 있습니다.", LOTTO_PRICE));
@@ -46,11 +47,11 @@ public class InputService {
 
     public Integer getBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        return getInteger();
+        String input = readLine();
+        return getInteger(input);
     }
 
-    private static Integer getInteger() {
-        String input = readLine();
+    static Integer getInteger(String input) {
         try {
             return Integer.valueOf(input);
         } catch (NumberFormatException e) {
