8d45e376ecc780fe87a9d518f2d8f3ae21518c41
miffyKing <whqudghk28@gmail.com>
2022-11-15 21:54:42 +0900

    feat(Application): Add exception handling session that catches thrown exception from all over the project.(#41)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,21 +1,11 @@
 package lotto;
 
-import io.Input;
-import io.InputKeyboard;
-import io.Output;
-import io.OutputScreen;
-import io.Sentence;
+import lotto.Model.Game;
 
 public class Application {
+
     public static void main(String[] args) {
-
-        Input input = new InputKeyboard();
-        Output output = new OutputScreen();
-
-        //output.printOutput(Sentence.INPUTMONEY);
-        Game game = new Game(input, output);
+        Game game = new Game();
         game.run();
-
-        // TODO: 프로그램 구현
     }
 }
