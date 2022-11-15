8271d91ec46dd0b58f8e85926a84f0baa3c56630
sungjun kim <dd8873@naver.com>
2022-11-15 20:37:24 +0900

    feat : 출력 기능 완료

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -1,9 +1,14 @@
 package lotto;
 
+import java.text.DecimalFormat;
+
 public class Output {
 
-    public static void printLottoCount(int lottoCount) {
-        System.out.println(lottoCount + "개를 구매했습니다.");
+    private static final int MIN_MATCH_LOTTO_NUMBER_COUNT = 0;
+
+    public static void printLottoCount(UserAmount userAmount) {
+        System.out.println(userAmount.calculateLottoCount() + "개를 구매했습니다.");
+        System.out.println();
     }
 
     public static void printLottoNumber(LottoNumbers lottoNumbers) {
@@ -13,8 +18,28 @@ public class Output {
     }
 
     public static void printRankingMsg() {
+        System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---------");
+    }
+
+    public static void printRanking(Ranking ranking, Results totalLottoRankingCount) {
+        DecimalFormat decimalFormat = new DecimalFormat("###,###");
+        if (ranking.getMatchCount() > MIN_MATCH_LOTTO_NUMBER_COUNT) {
+            System.out.println(ranking.getMatchCount() + "개 일치 (" +
+                    decimalFormat.format(ranking.getPrizeMoney()) + "원) - " +
+                    totalLottoRankingCount.getLottoRankingCount().get(ranking) + "개");
+        }
+        if (Ranking.SECOND == ranking) {
+            System.out.println(ranking.getMatchCount() + "개 일치, 보너스 볼 일치 (" +
+                    decimalFormat.format(ranking.getPrizeMoney()) + "원) - " +
+                    totalLottoRankingCount.getLottoRankingCount().get(ranking) + "개");
+        }
+    }
+
+    public static void printProfitsRate(int totalAmount, UserAmount userAmount) {
+        System.out.println("총 수익률은 " + String.format("%.1f",
+                (((double) totalAmount / (double) userAmount.getAmount())) * 100) + "%입니다.");
     }
 
 }
