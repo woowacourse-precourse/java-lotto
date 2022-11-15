24876f54bfdc8f5beea1acf140c7f0ca25fea8ab
3juhwan <13selfesteem91@naver.com>
2022-11-15 16:40:38 +0900

    feat(lotto): 단위 문자열 추가

--- src/main/java/lotto/domain/MoneyUnit.java
+++ src/main/java/lotto/domain/MoneyUnit.java
@@ -1,21 +1,27 @@
 package lotto.domain;
 
 public enum MoneyUnit {
-    FIRST_PRIZE_MONEY(2_000_000_000),
-    SECOND_PRIZE_MONEY(30_000_000),
-    THIRD_PRIZE_MONEY(1_500_000),
-    FOURTH_PRIZE_MONEY(50_000),
-    FIFTH_PRIZE_MONEY(5_000),
-    NO_PRIZE_MONEY(0)
+    FIRST_PRIZE_MONEY(2_000_000_000, "2,000,000,000"),
+    SECOND_PRIZE_MONEY(30_000_000, "30,000,000"),
+    THIRD_PRIZE_MONEY(1_500_000, "1,500,000"),
+    FOURTH_PRIZE_MONEY(50_000, "50,000"),
+    FIFTH_PRIZE_MONEY(5_000, "5,000"),
+    NO_PRIZE_MONEY(0, "0");
     ;
 
     private final Integer unit;
+    private final String unitString;
 
-    MoneyUnit(Integer unit) {
+    MoneyUnit(Integer unit, String unitString) {
         this.unit = unit;
+        this.unitString = unitString;
     }
 
     public static Integer getUnit(int place) {
         return MoneyUnit.values()[place-1].unit;
+    }
+
+    public static String getUnitString(int place) {
+        return MoneyUnit.values()[place-1].unitString;
     }
 }
