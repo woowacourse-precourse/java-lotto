b82eeb445c693964ee8e5d28cbae67707ff56f15
kimsh2948 <shkim2948@gmail.com>
2022-11-15 19:01:17 +0900

    test: 수익률 구하는 기능에 대한 테스트 구현

--- src/test/java/lotto/domain/WinningTest.java
+++ src/test/java/lotto/domain/WinningTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -16,9 +17,9 @@ class WinningTest {
 
     @BeforeEach
     void setUp() {
-        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 26));
         Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 30));
-        Lotto lotto3 = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        Lotto lotto3 = new Lotto(List.of(1, 2, 3, 4, 35, 8));
         Lotto lotto4 = new Lotto(List.of(1, 2, 3, 42, 15, 40));
         Lotto lotto5 = new Lotto(List.of(1, 20, 31, 41, 42, 43));
         Lotto lotto6 = new Lotto(List.of(11, 20, 30, 40, 43, 44));
@@ -38,15 +39,22 @@ class WinningTest {
         winning.checkWinningResult();
     }
 
+    @DisplayName("각 등수에 맞게 결과가 생성되었는지 테스트.")
     @Test
     void resultMatchRank() {
         Map<LottoRank, Integer> result = winning.getLottoResult();
 
         assertThat(result.get(LottoRank.FIFTH)).isEqualTo(1);
-        assertThat(result.get(LottoRank.FORTH)).isEqualTo(0);
-        assertThat(result.get(LottoRank.THIRD)).isEqualTo(1);
-        assertThat(result.get(LottoRank.SECOND)).isEqualTo(1);
-        assertThat(result.get(LottoRank.FIRST)).isEqualTo(1);
+        assertThat(result.get(LottoRank.FORTH)).isEqualTo(1);
+        assertThat(result.get(LottoRank.THIRD)).isEqualTo(2);
+        assertThat(result.get(LottoRank.SECOND)).isEqualTo(0);
+        assertThat(result.get(LottoRank.FIRST)).isEqualTo(0);
         assertThat(result.get(LottoRank.MISS)).isEqualTo(2);
+    }
+
+    @DisplayName("수익률 소수점 둘째자리에서 반올림한 값 테스트.")
+    @Test
+    void totalRate() {
+        assertThat(winning.getTotalRate()).isEqualTo(50916.7);
     }
 }
