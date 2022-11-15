cac087f8beaf162fdc89a7644355c59585cd2341
younghyun-na <younghyunna@naver.com>
2022-11-15 15:48:01 +0900

    :recycle: refactor: 로또 번호 중복 시 예외 발생 기능 리팩토링

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -14,10 +15,17 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if (hasDuplicatedNumber(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호에는 중복 숫자가 없어야 합니다.");
+        }
     }
 
     // TODO: 추가 기능 구현
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    public boolean hasDuplicatedNumber(List<Integer> numbers) {
+        return new HashSet<>(numbers).size() != numbers.size();
     }
 }
