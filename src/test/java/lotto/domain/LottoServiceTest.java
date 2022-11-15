0caedcdbd9a072bb338d99ecc102010a33370c0f
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-15 23:34:29 +0900

    구매 로또 정보와 당첨 로또 정보를 통해 당첨 순위 확인하는 통합 테스트

--- src/test/java/lotto/domain/LottoServiceTest.java
+++ src/test/java/lotto/domain/LottoServiceTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import lotto.model.Lotto;
+import lotto.model.WinningLotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -28,12 +29,41 @@ class LottoServiceTest {
     }
 
     @ParameterizedTest
+    @DisplayName("구매 로또 정보와 당첨 로또 정보를 통해 당첨 순위 확인하는 통합 테스트")
+    @CsvSource(value = {"1,2,3,4,5,7,6,2", "1,2,3,4,5,6,7,1", "1,2,3,4,5,8,9,3", "1,2,3,4,8,9,10,4", "1,2,3,9,10,11,12,5"})
+    void checkLottoNumbersTest(int number1, int number2, int number3, int number4, int number5, int number6, int bonusNumber, int expectedNumber) {
+
+        Lotto buyingLotto = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto winningLottoNumber = new Lotto(List.of(number1, number2, number3, number4, number5, number6));
+        WinningLotto winningLotto = new WinningLotto(winningLottoNumber, bonusNumber);
+        WinningRank expectedRank;
+
+        WinningRank winningRank = lottoService.checkLottoNumbers(buyingLotto, winningLotto);
+
+        if (expectedNumber == 1) {
+            expectedRank = WinningRank.FIRST;
+        } else if (expectedNumber == 2) {
+            expectedRank = WinningRank.SECOND;
+        } else if (expectedNumber == 3) {
+            expectedRank = WinningRank.THIRD;
+        } else if (expectedNumber == 4) {
+            expectedRank = WinningRank.FOURTH;
+        } else if (expectedNumber == 5) {
+            expectedRank = WinningRank.FIFTH;
+        } else {
+            expectedRank = WinningRank.NOTHING;
+        }
+
+        assertThat(winningRank).isEqualTo(expectedRank);
+    }
+
+    @ParameterizedTest
     @DisplayName("당첨 순위 결과가 제대로 확인되는지 검사하는 단위 테스트")
     @CsvSource(value = {"6, true, 1", "5, true, 2" ,"5, false, 3", "4, true, 4","3, true, 5" })
     void checkWinningRank(int matchCount, boolean isBonusMatch, int expectedNumber) {
 
         WinningRank result = lottoService.checkWinningRank(matchCount, isBonusMatch);
-        
+
         WinningRank expectedRank;
 
         if (expectedNumber == 1) {
