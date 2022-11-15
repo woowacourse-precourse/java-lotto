28fb05162bdb07a8f2a11db05508c5b3adb114e9
endlessmomo <soohyuk96@naver.com>
2022-11-15 21:35:07 +0900

    fix : 원인 찾기

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,6 +4,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         LottoController lottoController = new LottoController();
+        
         try{
             lottoController.run();
         } catch(Exception e){
