470d4c4c3c20c267a34bc271ee08e472f57a554c
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-11-15 18:53:49 +0900

    feat(lotto): Controller 로또 생성 로직 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.domain.LottoGenerator;
 import lotto.domain.NumberOfLottos;
 import lotto.view.InputView;
 
@@ -8,6 +9,7 @@ public class LottoController {
     public void gameStart() {
         try {
             NumberOfLottos numberOfLottos = getPriceAmount();
+            LottoGenerator lottoGenerator = getLottos(numberOfLottos);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
@@ -15,5 +17,12 @@ public class LottoController {
 
     private NumberOfLottos getPriceAmount() {
         return new NumberOfLottos(InputView.getPriceAmount());
+    }
+
+    private LottoGenerator getLottos(NumberOfLottos numberOfLottos) {
+        int price = numberOfLottos.getPrice();
+        int amount = numberOfLottos.calculateNumberOfLottos(price);
+
+        return new LottoGenerator(amount);
     }
 }
