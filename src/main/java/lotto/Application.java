2bf62d64dc7034850e331cdcde4ba30d038a9c10
Umin <adorecamus@gmail.com>
2022-11-15 21:56:02 +0900

    refactor: 사용자 입력을 연관 메서드에 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -12,16 +12,11 @@ public class Application {
     public static void main(String[] args) {
         try {
             Player player = new Player();
-            System.out.println("구입금액을 입력해 주세요.");
-            String money = Console.readLine();
-            Set<Lotto> playerLotto = player.buyLotto(money);
+            Set<Lotto> playerLotto = player.buyLotto();
 
-            System.out.println("당첨 번호를 입력해 주세요.");
-            String numbers = Console.readLine();
-            List<Integer> winningNumber = player.split(numbers);
+            List<Integer> winningNumbers = player.pickNumbers();
+            int bonusNumber = player.pickBonus();
 
-            System.out.println("보너스 번호를 입력해 주세요.");
-            String bonusNumber = Console.readLine();
 
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
