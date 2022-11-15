595b8ebf26ac07bbefc10d7b36f22b84739989c6
lee2963 <sodel2963@naver.com>
2022-11-15 15:31:50 +0900

    Frat: 에러 발생 try catch 문 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,18 +3,22 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
 
-        Buyer buyer = new Buyer();
-        int buyMoney = buyer.validateBuyLottoMoney(buyer.inputBuyLottoMoney());
-        int cnt = buyer.calculateLotteryNum(buyMoney);
-        buyer.setLottos(cnt);
-        buyer.printBuyLotto();
+        try {
+            Buyer buyer = new Buyer();
+            int buyMoney = buyer.validateBuyLottoMoney(buyer.inputBuyLottoMoney());
+            int cnt = buyer.calculateLotteryNum(buyMoney);
+            buyer.setLottos(cnt);
+            buyer.printBuyLotto();
 
-        LotteryMachine lotteryMachine = new LotteryMachine();
-        lotteryMachine.makeWinningLottoNum(lotteryMachine.inputWiningLottoNum());
-        lotteryMachine.makeBonusNum(lotteryMachine.getLotto().getNumbers(), lotteryMachine.inputBonusNum());
+            LotteryMachine lotteryMachine = new LotteryMachine();
+            lotteryMachine.makeWinningLottoNum(lotteryMachine.inputWiningLottoNum());
+            lotteryMachine.makeBonusNum(lotteryMachine.getLotto().getNumbers(), lotteryMachine.inputBonusNum());
 
-        int[] lottoResult = lotteryMachine.calculateLottoResult(buyer);
-        lotteryMachine.printResult(lottoResult);
-        lotteryMachine.printRateOfReturn(lotteryMachine.calculateRateOfReturn(lottoResult, buyMoney));
+            int[] lottoResult = lotteryMachine.calculateLottoResult(buyer);
+            lotteryMachine.printResult(lottoResult);
+            lotteryMachine.printRateOfReturn(lotteryMachine.calculateRateOfReturn(lottoResult, buyMoney));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
