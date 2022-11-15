44338e70b0ee10dbd46683fe8e0bc73fa988e45a
ChanghyunKim <variety82p@gamil.com>
2022-11-15 21:45:48 +0900

    feat(Messages) : 결과 출력 메세지 추가

--- src/main/java/lotto/utils/Messages.java
+++ src/main/java/lotto/utils/Messages.java
@@ -5,6 +5,8 @@ public class Messages {
     public static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
     public static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
     public static final String LOTTO_RESULT_MESSAGE ="당첨 통계\n___";
-    public static final String ERROR_MESSAGE = "[ERROR] ";
+    public static final String ERROR_MESSAGE = "[ERROR]";
+    public static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다.";
+    public static final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
 }
 
