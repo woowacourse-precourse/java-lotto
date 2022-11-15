842e269003d034d3c0094c1604c40f19546ec74c
Cho Changhun <cvcvcx@naver.com>
2022-11-15 09:05:05 +0900

    test: (BuyLottoImplTest) 제시한 금액이 로또 금액으로 나누어지지 않을 때 오류를 반환하는지 검증

--- src/test/java/lotto/BuyLottoImplTest.java
+++ src/test/java/lotto/BuyLottoImplTest.java
@@ -3,19 +3,21 @@ package lotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuyLottoImplTest {
 
     BuyLotto buyLotto = new BuyLottoImpl(1500,15000);
-
+    BuyLotto buyLottoNoDivide = new BuyLottoImpl(1200,15000);
     @Test
     void getLottoCount() {
-        Assertions.assertThat(buyLotto.getLottoCount()).isEqualTo(10);
+        assertThat(buyLotto.getLottoCount()).isEqualTo(10);
     }
 
     @Test
     void validate() {
+        assertThatThrownBy(()->buyLottoNoDivide.validate()).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
