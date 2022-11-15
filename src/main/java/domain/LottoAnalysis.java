1711e95d2bdb3cc1a83fd145c75a662111bbd5ac
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2022-11-15 11:49:57 +0900

    feat(LottoAnalysis): 구매한 로또의 당첨 정보를 분석하는 클래스 구현
    로또 리스트 생성자 메소드 구현
    총 당첨 개수를 조회하는 메소드 구현
    총 당참 상금을 조회하는 메소드 구현

---
+++ src/main/java/domain/LottoAnalysis.java
@@ -0,0 +1,35 @@
+package domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoAnalysis{
+
+    public final List<Lotto> lottoList;
+
+    public LottoAnalysis() {
+        this.lottoList = new ArrayList<>();
+    }
+
+    public void makeLottoList(int amount) {
+        for (int i = 0; i < amount; i++) {
+            lottoList.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+    }
+
+    public void countWinning(int amount,List<Integer> winningNum, int bonusNum) {
+        for (int i = 0; i < amount ; i++) {
+            lottoList.get(i).getRank(winningNum, bonusNum);
+        }
+    }
+
+    public double getTotalReward(){
+        double totalReward = 0 ;
+        for (LottoRank lottoRank : LottoRank.values()){
+            totalReward += lottoRank.getReward()*lottoRank.count;
+        }
+        return totalReward;
+    }
+
+}
