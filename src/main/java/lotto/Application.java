ec09fa2d5e9a19e50b37823b8b82d47e19f03b1f
SonMinGyu <littleking13@naver.com>
2022-11-15 16:05:38 +0900

    [FEAT] 잘못된 구입 금액 입력 예외처리 기능 구현
    - 잘못된 구입 금액이 입력되었을 때 예외처리하는 기능을 구현함
    - [ERROR] 구매 금액은 1000원 단위로 입력해야 합니다. 출력하고 IllegalArgumentException() 발생시킴

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,15 +4,44 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        int numberOfLotto = getTheNumberOfLotto();
+
     }
 
     public static int getTheNumberOfLotto() {
-        int money = Integer.parseInt(Console.readLine());
+        String money = Console.readLine();
+        validate(money);
+        int numberOfLotto = Integer.parseInt(money) / 1000;
 
-        int numberOfLotto = money / 1000;
-        
         return numberOfLotto;
     }
 
+    public static void validate(String money) {
+        if (validateMoneyType(money) || validateMoneyRange(money)) {
+            System.out.println("[ERROR] 구매 금액은 1000원 단위로 입력해야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static boolean validateMoneyType(String money) {
+        // 오류가 있으면 true
+        for (char c : money.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean validateMoneyRange(String money) {
+        // 오류가 있으면 true
+        if(Integer.parseInt(money) % 1000 != 0) {
+            return true;
+        }
+
+        return false;
+    }
 
 }
