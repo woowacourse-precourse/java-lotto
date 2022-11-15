b966b407f1faecbf365925b0af4925864afcc2e3
beombu <sunil1369@bible.ac.kr>
2022-11-15 19:35:58 +0900

    Test : 기능 테스트 오류 해결

--- src/main/java/lotto/model/LottoNumberCreator.java
+++ src/main/java/lotto/model/LottoNumberCreator.java
@@ -2,13 +2,14 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoNumberCreator {
 
     public static List<Integer> createRandomNumbers() {
-        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lottoNumbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         sortLottoNumbers(lottoNumbers);
         return lottoNumbers;
     }
--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -9,7 +9,7 @@ import static lotto.model.Rank.*;
 
 public class Output {
     public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
-    public static final String COUNT_LOTTO_MESSAGE = "%d개 구매했습니다.\n";
+    public static final String COUNT_LOTTO_MESSAGE = "%d개를 구매했습니다.\n";
     public static final String INPUT_PRIZE_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.\n";
     public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     public static final String WIN_STATISTICS_MESSAGE = "당첨 통계\n---";
