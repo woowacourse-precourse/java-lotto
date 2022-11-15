ac3193e02c2293f4aa03e2afa54ffab7eec7fa10
kimdonghun <zhfptm12@naver.com>
2022-11-15 12:54:13 +0900

    feat : 문구들 상수화

---
+++ src/main/java/lotto/strings/Strings.java
@@ -0,0 +1,11 @@
+package lotto.strings;
+
+public class Strings {
+    public final static String purchaseMessage = "구입금액을 입력해 주세요.";
+    public final static String purchaseCheckMessage = "개를 구매했습니다.";
+    public final static String lottoMessage = "당첨 번호를 입력해 주세요.";
+    public final static String bonusMessage = "보너스 번호를 입력해 주세요.";
+    public final static String statisticsMessage = "당첨 통계";
+
+
+}
