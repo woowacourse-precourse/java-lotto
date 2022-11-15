f32d6306b5130831f5b916f66ef10a022bde117d
JJONSOO <pjs970616@naver.com>
2022-11-15 14:27:04 +0900

    feat : create calculateLotto Method

--- src/main/java/lotto/Calculator.java
+++ src/main/java/lotto/Calculator.java
@@ -9,5 +9,18 @@ public class Calculator {
     private static final List<Integer> scoreOrder=List.of(3,4,5,-1,6);;
     private static final List<String> moneyOrder=List.of("5,000","50,000","1,500,000","","2,000,000,000");;
 
-
+    public static void calculateLotto(List<Lotto> lottoBuckets, Lotto userLotto ,int bonus){
+        for(Lotto lotto : lottoBuckets){
+            int corretNum = userLotto.compare(lotto);
+            if((corretNum>2 && corretNum <5) || corretNum==6){
+                prizeBuckets.put(corretNum,prizeBuckets.get(corretNum)+1);
+            }
+            if(corretNum==5&&!lotto.containBonus(bonus)){
+                prizeBuckets.put(corretNum,prizeBuckets.get(corretNum)+1);
+            }
+            if(corretNum==5&&lotto.containBonus(bonus)) {
+                prizeBuckets.put(-1,prizeBuckets.get(corretNum)+1);
+            }
+        }
+    }
 }
