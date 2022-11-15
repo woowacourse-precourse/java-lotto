6f01e1443f396b872ba9767a4cb3c0e87a41b554
canbeyour <wuzucy@gmail.com>
2022-11-15 16:20:49 +0900

    refactor: modify function to use static

--- src/main/java/lotto/Utils/Error.java
+++ src/main/java/lotto/Utils/Error.java
@@ -1,15 +1,15 @@
 package lotto.Utils;
 
 public class Error {
-    public void amountInput() {
+    public static void amountInput() {
         throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해 주세요.");
     }
 
-    public void duplicated() {
+    public static void duplicated() {
         throw new IllegalArgumentException("[ERROR] 중복되지 않은 숫자를 입력해 주세요");
     }
 
-    public void outOfRange() {
+    public static void outOfRange() {
         throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
 }
