eef04d110676802b6718e65af5524814d1bff5cd
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-15 16:26:10 +0900

    feat: Add validate function to cli

--- src/main/java/lotto/Cli.java
+++ src/main/java/lotto/Cli.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.Validation.isNumeric;
 
 /** Cli class to interact with user. */
 public class Cli {
@@ -13,9 +14,16 @@ public class Cli {
     System.out.println("구입금액을 입력해 주세요.");
     String amount = readLine();
 
+    validate(amount);
     int purchaseCount = Integer.parseInt(amount) / 1000;
 
     System.out.printf("%d개를 구입했습니다.", purchaseCount);
     return purchaseCount;
+  }
+
+  private static void validate(String amount) {
+    if (!isNumeric(amount)) {
+      throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자 자료형이어야 합니다.");
+    }
   }
 }
