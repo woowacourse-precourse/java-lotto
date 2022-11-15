5afa1e42c6a7e90bcd171488a69dc6308fb170d6
devMuscle <hongsbien@naver.com>
2022-11-15 14:26:50 +0900

    refactor(inputByNotDivideBy1000): 1000으로 나누어지지 않는 입력 예외 테스트 수정

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -1,34 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class UserTest {
-    private List<Lotto> lottos;
-    private Lotto lotto;
+class UserTest extends NsTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
 
-    @BeforeEach
-    void setUp() {
-        lotto = new Lotto(List.of(1,2,3,4,5,6));
-        lottos.add(lotto);
-    }
-
-    @DisplayName("숫자가 아닌 것을 입력하면 예외가 발생한다.")
-    @Test
-    void inputByNotNumber() {
-        assertThatThrownBy(() -> new User("ABC", lottos))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("숫자가 아닌 것을 입력하면 예외가 발생한다.")
+    @DisplayName("1000으로 나누엊지 않는 숫자면 예외가 발생한다.")
     @Test
     void inputByNotDividedBy1000() {
-        assertThatThrownBy(() -> new User("15001", lottos))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertSimpleTest(() -> {
+            runException("10001");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
     }
 }
