2ecdf44f2528cbbb89c3a6ad95d57442c5f870dd
EUNJEONGMUN <munej26@gmail.com>
2022-11-15 19:57:32 +0900

    test: 당첨번호 등수 반환테스트 메서드 명 변경 및 정렬

--- src/test/java/lotto/domain/MatchCountTest.java
+++ src/test/java/lotto/domain/MatchCountTest.java
@@ -16,20 +16,23 @@ class MatchCountTest {
     @DisplayName("당첨번호 등수 반환 테스트")
     @ParameterizedTest(name = "{index}: {5}")
     @MethodSource("isSameResultTest")
-    void 테스트(int winningCount1, int bonusCount1, int winningCount2, int bonusCount2, boolean expected, String message) {
-        MatchCount matchCount1 = new MatchCount(winningCount1, bonusCount1);
-        MatchCount matchCount2 = new MatchCount(winningCount2, bonusCount2);
+    void sameResultTest(int winning1, int bonus1, int winning2, int bonus2, boolean expected, String message) {
+        MatchCount matchCount1 = new MatchCount(winning1, bonus1);
+        MatchCount matchCount2 = new MatchCount(winning2, bonus2);
 
         assertThat(matchCount1.isSameResult(matchCount2)).isEqualTo(expected);
     }
 
     private static Stream<Arguments> isSameResultTest() {
         return Stream.of(
-                Arguments.of(3, 0, 3, 1, true, "번호 3개일치와 번호3개일치+보너스1개일치의 등수는 같아야 한다."),
-                Arguments.of(4, 0, 4, 1, true, "번호 4개일치와 번호4개일치+보너스1개일치의 등수는 같아야 한다."),
-                Arguments.of(5, 0, 5, 1, false, "번호 5개일치와 번호5개일치+보너스1개일치의 등수는 달라야 한다."),
-                Arguments.of(5, 1, 6, 0, false, "번호 5개일치+보너스1개일치와 당첨번호6개일치의 등수는 달라야 한다.")
-
+                Arguments.of(3, 0, 3, 1, true,
+                        "번호 3개일치와 번호3개일치+보너스1개일치의 등수는 같아야 한다."),
+                Arguments.of(4, 0, 4, 1, true,
+                        "번호 4개일치와 번호4개일치+보너스1개일치의 등수는 같아야 한다."),
+                Arguments.of(5, 0, 5, 1, false,
+                        "번호 5개일치와 번호5개일치+보너스1개일치의 등수는 달라야 한다."),
+                Arguments.of(5, 1, 6, 0, false,
+                        "번호 5개일치+보너스1개일치와 당첨번호6개일치의 등수는 달라야 한다.")
         );
     }
 
