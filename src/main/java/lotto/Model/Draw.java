aca53f20a17c56ca5c72a380b84d0528a8c3d03d
syahn <496300@naver.com>
2022-11-15 17:05:40 +0900

    refactor: enum의 increaseCount 메소드 통해  값 증가시키도록 변경
    - 기존의 코드는 count 리스트의 인덱스를 통해 접근하여 값을 증가시켜 저장
    - 변경 후, enum을 통해 관리가 편해짐

--- src/main/java/lotto/Model/Draw.java
+++ src/main/java/lotto/Model/Draw.java
@@ -1,5 +1,7 @@
 package lotto.Model;
 
+import lotto.Model.enums.Prize;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
@@ -8,7 +10,6 @@ public class Draw {
     private final List<Integer> winning;
     private final int bonus;
     private final List<List<Integer>> lottoes;
-    private List<Integer> prizeCounts = Arrays.asList(0, 0, 0, 0, 0);
 
     public Draw(List<Integer> winning, int bonus, List<List<Integer>> lottoes) {
         this.winning = winning;
@@ -24,23 +25,9 @@ public class Draw {
             int bonusCount = 0;
             if (lotto.contains(bonus)) bonusCount = 1;
 
-            int prizeCount = countPrize(count, bonusCount);
-
-            if (prizeCount != 5)
-                prizeCounts.set(prizeCount, prizeCounts.get(prizeCount) + 1);
+            if(Prize.findNameByValue(count, bonusCount) != null){
+                Prize.findNameByValue(count, bonusCount).increaseCount();
+            }
         }
-    }
-
-    int countPrize(int count, int bonusCount) {
-        if (count == 6) return 4;
-        if (count == 5 && bonusCount == 1) return 3;
-        if (count == 5) return 2;
-        if (count == 4) return 1;
-        if (count == 3) return 0;
-        return 5;
-    }
-
-    public List<Integer> getPrizeCounts() {
-        return this.prizeCounts;
     }
 }
