0b4d983f114a1fe41fd5fb184186ab26086eebc8
skck0226 <skck0226@ajou.ac.kr>
2022-11-15 17:23:45 +0900

    [test] 당첨비교 테스트 수정

--- src/test/java/lotto/PrizeTest.java
+++ src/test/java/lotto/PrizeTest.java
@@ -3,25 +3,38 @@ package lotto;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PrizeTest extends NsTest {
 
     @Test
     void 당첨비교_테스트() {
-        assertSimpleTest(
+        assertRandomUniqueNumbersInRangeTest(
                 () -> {
                     run("5000", "1,2,3,4,5,6", "7");
                     assertThat(output()).contains(
+                            "5개를 구매했습니다.",
+                            "[1, 2, 3, 15, 26, 34]",
+                            "[1, 2, 3, 4, 11, 15]",
+                            "[1, 2, 3, 4, 5, 9]",
+                            "[1, 2, 3, 4, 5, 7]",
+                            "[1, 2, 3, 4, 5, 6]",
                             "3개 일치 (5,000원) - 1개",
                             "4개 일치 (50,000원) - 1개",
                             "5개 일치 (1,500,000원) - 1개",
                             "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
                             "6개 일치 (2,000,000,000원) - 1개",
-                            "총 수익률은 40631100%입니다."
+                            "총 수익률은 40631100.0%입니다."
                     );
-                }
+                },
+                List.of(1, 2, 3, 15, 34, 26),
+                List.of(1, 2, 3, 4, 11, 15),
+                List.of(1, 2, 3, 4, 5, 9),
+                List.of(1, 2, 3, 4, 5, 7),
+                List.of(1, 2, 3, 4, 5, 6)
         );
     }
 
