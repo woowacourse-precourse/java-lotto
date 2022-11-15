16b94bf2f090f5f5c4525f17c4af8b14236b5648
김태용 <amico741@naver.com>
2022-11-15 14:00:02 +0900

    fix: 구입 금액에 아무것도 입력하지 않았을 때 버그 수정

--- src/main/java/lotto/Message.java
+++ src/main/java/lotto/Message.java
@@ -1,14 +1,15 @@
 package lotto;
 
 public enum Message {
-
     WELCOME_MESSAGE("구입 금액을 입력해 주세요."),
+
+    REQUIRE_INPUT("[ERROR] 구입 금액을 입력해야 합니다."),
     COUNT_MESSAGE("개를 구매했습니다."),
     INPUT_WINNING_MESSAGE("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_MESSAGE("보너스 번호를 입력해주세요."),
     WINNING_STATIC_MESSAGE("당첨통계\n---"),
-    WINNING_INPUT_MESSAGE("정확히 숫자를 입력하고 `,`로 구분 지을 수 있습니다."),
-    BONUS_INPUT_MESSAGE("보너스 번호는 숫자만 입력할 수 있습니다."),
+    WINNING_INPUT_MESSAGE("[ERROR] 정확히 숫자를 입력하고 `,`로 구분 지을 수 있습니다."),
+    BONUS_INPUT_MESSAGE("[ERROR] 보너스 번호는 숫자만 입력할 수 있습니다."),
     NOT_MOD_ZERO_MESSAGE("[ERROR] 1000으로 나누어지는 금액이 아닙니다."),
     REJECT_ZERO("[ERROR] 구입 금액 맨 앞에 0을 입력할 수 없습니다."),
     REJECT_NOT_NUMBERS("[ERROR] 구입 금액에 숫자를 제외한 다른 입력이 올 수 없습니다."),
--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -5,11 +5,13 @@ import static lotto.Message.*;
 import java.util.regex.Pattern;
 
 public class Money {
-    public static final int STANDARD_WON = 1000;
+    private static final int STANDARD_WON = 1000;
+    private static final int NULL = 0;
     private final int money;
     private final int lottoCount;
 
     public Money(String input) throws IllegalArgumentException {
+        validateNotNull(input);
         validateConsistOfNumbers(input);
         validateMoneyZero(input);
         validateMoney(input);
@@ -17,21 +19,27 @@ public class Money {
         this.lottoCount = Math.floorDiv(this.money, STANDARD_WON);
     }
 
-    public void validateMoney(String input) {
+    private void validateNotNull(String input) {
+        if(input.length() == NULL) {
+            throw new IllegalArgumentException(REQUIRE_INPUT.getValue());
+        }
+    }
+
+    private void validateMoney(String input) {
         int money = Integer.parseInt(input);
         if (Math.floorMod(money, STANDARD_WON) != 0) {
             throw new IllegalArgumentException(NOT_MOD_ZERO_MESSAGE.getValue());
         }
     }
 
-    public void validateConsistOfNumbers(String input) throws IllegalArgumentException {
+    private void validateConsistOfNumbers(String input) throws IllegalArgumentException {
         boolean result = Pattern.matches("^[0-9]*$", input);
         if (!result) {
             throw new IllegalArgumentException(REJECT_NOT_NUMBERS.getValue());
         }
     }
 
-    public void validateMoneyZero(String input) throws IllegalArgumentException {
+    private void validateMoneyZero(String input) throws IllegalArgumentException {
         if (String.valueOf(input.charAt(0)).equals("0")) {
             throw new IllegalArgumentException(Message.REJECT_ZERO.getValue());
         }
