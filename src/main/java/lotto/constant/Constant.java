b250737485cb157f447a8b5d852322f8236d1541
eom-tae-in <eti0728@daum.net>
2022-11-15 19:57:13 +0900

    feat(Constant): 상수 선언

---
+++ src/main/java/lotto/constant/Constant.java
@@ -0,0 +1,19 @@
+package lotto.constant;
+
+public class Constant {
+
+    public static final String ERROR_MESSAGE = "[ERROR] ";
+    public static final int INITIAL_NUMBER = 0;
+    public static final int INCREASE_COUNT_NUMBER = 1;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int NUMBER_OF_ZERO_IN_THOUSAND = 3;
+    public static final int LOWEST_RANK_MATCHED_LOTTO_NUMBERS = 3;
+    public static final int FOUR_DIGIT_NUMBER = 4;
+    public static final int CORRECT_FIVE_NUMBERS = 5;
+    public static final int BONUS_NUMBER_INDEX = 6;
+    public static final int NUMBER_OF_LOTTO_NUMBERS = 6;
+    public static final int CORRECT_FIVE_AND_BONUS_NUMBER = 7;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int HUNDRED = 100;
+    public static final int LOTTO_PRICE = 1000;
+}
