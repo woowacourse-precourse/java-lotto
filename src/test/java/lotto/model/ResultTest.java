da23371bc956e7c4e0366df3c670077fa536b4b7
LeeHaiLim <hailim1997@gmail.com>
2022-11-15 20:05:50 +0900

    test(model): add test method rate of return test

--- src/test/java/lotto/model/ResultTest.java
+++ src/test/java/lotto/model/ResultTest.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -53,5 +54,22 @@ public class ResultTest {
         Result result = new Result(winningLotto, List.of(lotto), 1000);
 
         assertThat(result.getRankResult(Rank.FIFTH)).isEqualTo(1);
+    }
+
+    @DisplayName("수익률을 정확히 계산한다.")
+    @Test
+    void rateOfReturnTest() {
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(List.of(8, 21, 23, 41, 42, 43)));
+        lottos.add(new Lotto(List.of(3, 5, 11, 16, 32, 38)));
+        lottos.add(new Lotto(List.of(7, 11, 16, 35, 36, 44)));
+        lottos.add(new Lotto(List.of(1, 8, 11, 31, 41, 42)));
+        lottos.add(new Lotto(List.of(13, 14, 16, 38, 42, 45)));
+        lottos.add(new Lotto(List.of(7, 11, 30, 40, 42, 43)));
+        lottos.add(new Lotto(List.of(2, 13, 22, 32, 38, 45)));
+        lottos.add(new Lotto(List.of(1, 3, 5, 14, 22, 45)));
+
+        Result result = new Result(winningLotto, lottos, 8000);
+        assertThat(result.getRateOfReturn()).isEqualTo(62.5f);
     }
 }
