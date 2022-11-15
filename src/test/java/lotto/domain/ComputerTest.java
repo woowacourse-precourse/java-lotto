b5a32c8f8d997728019e4c66e876ee2ab1183958
CAEL0 <kjongmin26@gmail.com>
2022-11-15 15:46:51 +0900

    test(ComputerTest): 당첨 번호가 0으로 시작하면 예외 발생

--- src/test/java/lotto/domain/ComputerTest.java
+++ src/test/java/lotto/domain/ComputerTest.java
@@ -9,10 +9,19 @@ import static org.assertj.core.api.Assertions.*;
 
 public class ComputerTest {
 
-    @DisplayName("당첨 번호가 자연수가 아니면 예외가 발생한다.")
+    @DisplayName("당첨 번호를 콤마로 구분했을 때 자연수가 아니면 예외가 발생한다.")
     @Test
     void createComputerWithNotNaturalNumber() {
         String input = "1,2,3,4,5,a";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        assertThatThrownBy(Computer::new)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호가 0으로 시작하면 예외가 발생한다.")
+    @Test
+    void createComputerWithLeadingZero() {
+        String input = "1,2,3,4,5,06";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         assertThatThrownBy(Computer::new)
                 .isInstanceOf(IllegalArgumentException.class);
