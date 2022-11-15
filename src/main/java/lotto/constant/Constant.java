f8127eadfdb260e8274f6a3334d4cbce63396e70
oyoungsun <plusmate@naver.com>
2022-11-15 15:25:08 +0900

    feat : constant 언어 추가

--- src/main/java/lotto/constant/Constant.java
+++ src/main/java/lotto/constant/Constant.java
@@ -2,4 +2,10 @@ package lotto.constant;
 
 public final class Constant {
     public static final String ERROR_MESSAGE = "[ERROR]";
+    public static final String ERROR_INTEGER = " 정수를 입력해야 합니다.";
+    public static final String ERROR_BY1000 = " 1000원 단위로 입력해야합니다.";
+    public static final String ERROR_BONUS = " 보너스 번호는 당첨 번호와 겹치면 안됩니다.";
+    public static final String ERROR_BOUNDARY = " 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+   // public static final String ERROR_
+
 }
