ef9c947fcd74894eb9b43a917185fb961366f040
dingding-21 <phj8221@naver.com>
2022-11-15 11:58:46 +0900

    feat($Output): 로또 수량 출력하는 기능 구현

--- src/main/java/lotto/ui/Output.java
+++ src/main/java/lotto/ui/Output.java
@@ -6,4 +6,8 @@ public class Output {
     public static void inputPurchaseAmountMessage() {
         System.out.println(OutputMessage.INPUT_PURCHASE_AMOUNT);
     }
+
+    public static void lottoCount(Integer count) {
+        System.out.println(count.toString() + OutputMessage.LOTTO_COUNT);
+    }
 }
--- src/main/java/lotto/utils/OutputMessage.java
+++ src/main/java/lotto/utils/OutputMessage.java
@@ -2,4 +2,5 @@ package lotto.utils;
 
 public class OutputMessage {
     public static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String LOTTO_COUNT = "개를 구매했습니다.";
 }
