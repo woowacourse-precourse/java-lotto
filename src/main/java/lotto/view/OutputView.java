ffb535e652888bf448a29e96cc42d4e3e96b61ba
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-15 22:55:12 +0900

    feat: printTotalResultMessage() 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -30,6 +30,17 @@ public class OutputView {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
+    public void printTotalResultMessage(Map<Rank, Integer> countPrize) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + countPrize.getOrDefault(Rank.THIRD, 0) + "개");
+        System.out.println("4개 일치 (50,000원) - " + countPrize.getOrDefault(Rank.FOURTH, 0) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + countPrize.getOrDefault(Rank.FIVE, 0) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + countPrize.getOrDefault(Rank.FIVE_WITH_BONUS, 0) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + countPrize.getOrDefault(Rank.SIX, 0) + "개");
+    }
+
 
 
 
