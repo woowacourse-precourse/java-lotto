438a5d2936d83a8a8e418ff9b846848bcb448fbf
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-15 13:24:14 +0900

    test(test/OperationTest): 테스트 케이스 추가
    - buyAmount(): 로또 구입 금액에 맞추어 로또를 구매하는지 테스트
    - generateNumbersDuplication(): 로또 번호를 생성할 때 번호끼리 중복되지 않는지 테스트

--- src/test/java/lotto/OperationTest.java
+++ src/test/java/lotto/OperationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,9 +19,25 @@ public class OperationTest {
         lottos.add(new Lotto(List.of(1, 2, 3, 4, 8, 9)));
         lottos.add(new Lotto(List.of(1, 2, 3, 8, 9, 10)));
         Match match = new Match(lottos, winning, bonus);
-        int earning=Operation.calculateEarning(match);
-        double earningRate=Operation.calculateEarningRate(2000,earning);
+        int earning = Operation.calculateEarning(match);
+        double earningRate = Operation.calculateEarningRate(2000, earning);
         System.out.println(earningRate);
-        assertThat(earningRate).isEqualTo(2750);
+        assertThat(earningRate).isEqualTo(2750.0);
+    }
+
+    @DisplayName("로또 구입 금액에 맞는 로또 구입 갯수를 계산한다.")
+    @Test
+    void buyAmount() {
+        int purchaseAmount = 14000;
+        int buyAmount = Operation.buyAmount(purchaseAmount);
+        assertThat(buyAmount).isEqualTo(14);
+    }
+
+    @DisplayName("로또 번호를 생성할 때 중복되는 숫자가 없는지 확인한다.")
+    @Test
+    void generateNumbersDuplication(){
+        List<Integer> numbers = Operation.generateNumbers();
+        HashSet<Integer> checkDuplication=new HashSet<>(numbers);
+        assertThat (checkDuplication.size()).isEqualTo(6);
     }
 }
