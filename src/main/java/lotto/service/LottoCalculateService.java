7cc0ab3516ee53f6db3bf82a9641280e29c9acee
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-15 22:42:58 +0900

    feat: LottoCalculateService 클래스의 필요한 객체 생성

---
+++ src/main/java/lotto/service/LottoCalculateService.java
@@ -0,0 +1,16 @@
+package lotto.service;
+
+import java.util.HashMap;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
+import lotto.domain.User;
+import lotto.domain.UserLotto;
+import lotto.view.OutputView;
+
+public class LottoCalculateService {
+    private static HashMap<Rank, Integer> rankMap = new HashMap<>();
+    private static OutputView outputView = new OutputView();
+
+
+}
