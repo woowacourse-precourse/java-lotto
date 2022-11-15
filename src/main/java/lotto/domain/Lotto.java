a478def617a8a2b78aaef020d555d5707f1f5e79
ohhonggi <5ghdrl@naver.com>
2022-11-15 18:56:36 +0900

    perf($Lotto): Move Lotto function
    The two lotto functions are moved to the ValidUtil class.
    
    modified function name:
    - validate -> validateNumberUnit
    
    lotto functions
    - validateNumberUnit
    - validateDuplication

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,39 +1,23 @@
 package lotto.domain;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static lotto.constants.ErrorCode.INVALID_NUMBER_OF_DIGITS;
-import static lotto.constants.ErrorCode.INVALID_NUMERIC_CONFIGURATION;
+import static lotto.util.ValidUtil.validateDuplication;
+import static lotto.util.ValidUtil.validateNumberUnit;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumberUnit(numbers);
         validateDuplication(numbers);
         Collections.sort(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException(INVALID_NUMBER_OF_DIGITS);
-        }
-    }
-
 
     // TODO: 추가 기능 구현
-    private void validateDuplication(List<Integer> numbers) {
-        for (int i = 0; i<numbers.size(); i++) {
-            List<Integer> subList = numbers.subList(i+1, numbers.size());
-            if (subList.contains(numbers.get(i))) {
-                throw new IllegalArgumentException(INVALID_NUMERIC_CONFIGURATION);
-            }
-        }
-    }
-
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder("[");
--- src/main/java/lotto/util/ValidUtil.java
+++ src/main/java/lotto/util/ValidUtil.java
@@ -1,18 +1,35 @@
 package lotto.util;
 
 
+import java.util.List;
+
 import static lotto.constants.AmountCode.AMOUNTS_UNIT;
-import static lotto.constants.ErrorCode.NOT_NUMBER;
-import static lotto.constants.ErrorCode.UNSUITABLE_UNIT;
+import static lotto.constants.ErrorCode.*;
 
 public class ValidUtil {
 
-    public static String validInputAmounts(String input) {
+    public static void validateNumberUnit(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(INVALID_NUMBER_OF_DIGITS);
+        }
+    }
+
+    public static String validNumber(String input) {
         if (!input.matches("^[0-9]+$")) {
             throw new IllegalArgumentException(NOT_NUMBER);
         }
         return input;
     }
+
+    public static void validateDuplication(List<Integer> numbers) {
+        for (int i = 0; i<numbers.size(); i++) {
+            List<Integer> subList = numbers.subList(i+1, numbers.size());
+            if (subList.contains(numbers.get(i))) {
+                throw new IllegalArgumentException(INVALID_NUMERIC_CONFIGURATION);
+            }
+        }
+    }
+
 
     public static int validAmountUnit(int amount) {
         if (amount % AMOUNTS_UNIT != 0) {
