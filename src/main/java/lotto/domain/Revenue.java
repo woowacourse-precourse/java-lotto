9ecf3dc0dc2b0018031fcda4325cf5fc33c159af
Hongsugyeong <olivia258@naver.com>
2022-11-15 14:19:56 +0900

    (Refactor) : reward 값을 초기화해주는 생성자 생성

--- src/main/java/lotto/domain/Revenue.java
+++ src/main/java/lotto/domain/Revenue.java
@@ -3,13 +3,14 @@ package lotto.domain;
 import java.util.List;
 
 public class Revenue {
+   private static int reward = 0;
 
     private static long oneRevenue(WinDetail winDetail){
         return winDetail.reward();
     }
 
     public static long revenue(List<WinDetail> result){
-        int reward=0;
+        new Revenue();
         for(WinDetail win : result) {
             reward += oneRevenue(win);
         }
@@ -20,5 +21,9 @@ public class Revenue {
         double rate = (double)reward / Double.parseDouble(money)* 100;
         rate = Math.round(rate*10.0)/10.0;
         return rate;
+    }
+
+    public Revenue(){
+        this.reward = 0;
     }
 }
