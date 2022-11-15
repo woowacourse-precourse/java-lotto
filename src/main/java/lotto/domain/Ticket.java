917a39fdbb7ccef8f1d7c558d301cd0d5a49d91c
ycopp9 <aciees95@gmail.com>
2022-11-15 16:15:40 +0900

    feat(Ticket): numberFormatCheck 구현
    유저의 입력값이 정수형으로 변환 불가능 한 경우 예외 발생하는 메서드
    구현함.

--- src/main/java/lotto/domain/Ticket.java
+++ src/main/java/lotto/domain/Ticket.java
@@ -1,15 +1,27 @@
 package lotto.domain;
 
+import lotto.constants.LottoConstants;
+
 public class Ticket {
     private int count;
 
     Ticket(String userInput) {
         validate(userInput);
-        count = Integer.parseInt(userInput) / 1_000;
+        count = Integer.parseInt(userInput) / LottoConstants.TICKET_PRICE.getValue();
     }
 
     private void validate(String userInput) {
+        int money = numberFormatCheck(userInput);
+    }
 
+    private int numberFormatCheck(String userInput) {
+        int value;
+        try {
+            value = Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        return value;
     }
 
     public int getCount() {
