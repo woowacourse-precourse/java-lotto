dcb9e9782ff4a4f9f4711e4afc84619556c22e25
minju412 <ln8847@ajou.ac.kr>
2022-11-15 20:24:59 +0900

    test(RankTest): use CsvSource

--- src/test/java/lotto/domain/result/RankTest.java
+++ src/test/java/lotto/domain/result/RankTest.java
@@ -2,57 +2,23 @@ package lotto.domain.result;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class RankTest {
 
-    @Test
-    @DisplayName("알맞은 Rank 를 가져옵니다 - 3개 매칭")
-    void getRank_3hits() {
-        assertThat(Rank.getRank(3, false))
-                .isEqualTo(Rank.THREE_MATCHES);
-    }
-
-    @Test
-    @DisplayName("알맞은 Rank 를 가져옵니다 - 4개 매칭")
-    void getRank_4hits() {
-        assertThat(Rank.getRank(4, false))
-                .isEqualTo(Rank.FOUR_MATCHES);
-    }
-
-    @Test
-    @DisplayName("알맞은 Rank 를 가져옵니다 - 5개 매칭 & 보너스 매칭")
-    void getRank_5hits_with_bonus() {
-        assertThat(Rank.getRank(5, true))
-                .isEqualTo(Rank.FIVE_MATCHES_WITH_BONUS);
-    }
-
-    @Test
-    @DisplayName("알맞은 Rank 를 가져옵니다 - 5개 매칭")
-    void getRank_5hits() {
-        assertThat(Rank.getRank(5, false))
-                .isEqualTo(Rank.FIVE_MATCHES_WITHOUT_BONUS);
-    }
-
-    @Test
-    @DisplayName("알맞은 Rank 를 가져옵니다 - 6개 매칭")
-    void getRank_6hits() {
-        assertThat(Rank.getRank(6, false))
-                .isEqualTo(Rank.SIX_MATCHES);
-    }
-
-    @Test
-    @DisplayName("알맞은 Rank 를 가져옵니다 - 2개 이하 매칭")
-    void getRank_none() {
-        assertThat(Rank.getRank(2, false))
-                .isEqualTo(Rank.NONE);
-    }
-
-    @Test
-    @DisplayName("유효하지 않은 타입을 요청합니다")
-    void getRank_invalid_type() {
-        assertThat(Rank.getRank(10, false))
-                .isEqualTo(Rank.NONE);
+    @ParameterizedTest
+    @CsvSource({
+            "3, false, 'THREE_MATCHES'",
+            "4, false, 'FOUR_MATCHES'",
+            "5, true, 'FIVE_MATCHES_WITH_BONUS'",
+            "5, false, 'FIVE_MATCHES_WITHOUT_BONUS'",
+            "6, false, 'SIX_MATCHES'",
+            "2, false, 'NONE'",
+            "10, false, 'NONE'"
+    })
+    public void calculateMatchResults(int hitCount, boolean hasBonusNumber, String rank) {
+        assertThat(Rank.getRank(hitCount, hasBonusNumber).toString())
+                .isEqualTo(rank);
     }
 }
