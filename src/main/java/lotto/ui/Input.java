c7a5a5260b84fa19432930c4685c139f80a9f3f4
dingding-21 <phj8221@naver.com>
2022-11-15 17:28:52 +0900

    feat($Input): 당첨 번호가 유효한 값인지 검증하는 기능 구현

--- src/main/java/lotto/ui/Input.java
+++ src/main/java/lotto/ui/Input.java
@@ -3,6 +3,10 @@ package lotto.ui;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.utils.Constant;
 import lotto.utils.ErrorMessage;
+import lotto.utils.Validation;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Input {
     private static final Integer REMAINDER = 0;
@@ -20,5 +24,19 @@ public class Input {
 
     private static Integer toInts(String number) {
         return Integer.parseInt(number);
+    }
+
+    public static void validateWiningNumber(String winingNumber) {
+        String[] splits = winingNumber.split(",");
+        List<Integer> numbers = new ArrayList<>();
+
+        Validation.checkSplitLength(splits.length);
+
+        for (String split : splits) {
+            numbers.add(Integer.parseInt(split));
+        }
+
+        Validation.checkNumberRange(numbers);
+        Validation.checkDuplicatedNumber(numbers);
     }
 }
--- src/main/java/lotto/utils/Constant.java
+++ src/main/java/lotto/utils/Constant.java
@@ -5,5 +5,7 @@ public class Constant {
     public static final Integer MAX_LOTTO_NUMBER = 45;
     public static final Integer MIN_LOTTO_NUMBER = 1;
     public static final Integer LOTTO_NUMBER_COUNT = 6;
+    public static final Integer LOTTO_NUMBER_DUPLICATED_LIMIT = 1;
+
 
 }
