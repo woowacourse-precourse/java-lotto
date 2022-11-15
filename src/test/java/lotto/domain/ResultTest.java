6231ef3e367617d5b133ef15a49e02d80c5ab67a
박승민 <tmddudals369@naver.com>
2022-11-15 14:08:33 +0900

    refactor: 중복 코드 제거

--- src/test/java/lotto/domain/ResultTest.java
+++ src/test/java/lotto/domain/ResultTest.java
@@ -1,13 +1,19 @@
 package lotto.domain;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 import lotto.constants.Rank;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ResultTest {
 
@@ -18,73 +24,32 @@ class ResultTest {
         result = new Result();
     }
 
-    @Test
-    @DisplayName("당첨 번호와 로또 번호가 모두 일치할 경우 1등")
-    void testName() {
-        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
-        int bonusNumber = 7;
-        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+    @ParameterizedTest
+    @DisplayName("당첨 번호와 보너스 번호를 로또 번호와 비교해 당첨 결과를 구한다")
+    @MethodSource("generateData")
+    void getResultCompareWinningNumberAndBonsNumberWithLottoNumbers(List<Integer> winningNumbers,
+                                                                    List<Integer> lottoNumbers, int bonusNumber,
+                                                                    Rank rank, int expected) {
+        List<Lotto> lottos = List.of(new Lotto(lottoNumbers));
 
-        result.matchLotto(winningNumber, lottos, bonusNumber);
+        result.matchLotto(winningNumbers, lottos, bonusNumber);
         Map<Rank, Integer> rankInfo = result.getRankInfo();
-        assertThat(rankInfo).contains(entry(Rank.FIRST, 1), entry(Rank.SECOND, 0), entry(Rank.THIRD, 0),
-                entry(Rank.FOURTH, 0), entry(Rank.FIFTH, 0));
+        assertThat(rankInfo).contains(entry(rank, expected));
+
+        rankInfo.remove(rank);
+        for (Rank notMatchedRank : rankInfo.keySet()) {
+            assertThat(rankInfo.get(notMatchedRank)).isEqualTo(0);
+        }
     }
 
-    @Test
-    @DisplayName("당첨 번호와 로또 번호가 5개 그리고 보너스 번호가 일치할 경우 2등")
-    void testName2() {
-        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
-        int bonusNumber = 7;
-        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 7)));
-
-        result.matchLotto(winningNumber, lottos, bonusNumber);
-        Map<Rank, Integer> rankInfo = result.getRankInfo();
-
-        assertThat(rankInfo).contains(entry(Rank.FIRST, 0), entry(Rank.SECOND, 1), entry(Rank.THIRD, 0),
-                entry(Rank.FOURTH, 0), entry(Rank.FIFTH, 0));
-    }
-
-    @Test
-    @DisplayName("당첨 번호와 로또 번호가 5개가 일치할 경우 3등")
-    void testName3() {
-        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
-        int bonusNumber = 7;
-        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 8)));
-
-        result.matchLotto(winningNumber, lottos, bonusNumber);
-        Map<Rank, Integer> rankInfo = result.getRankInfo();
-
-        assertThat(rankInfo).contains(entry(Rank.FIRST, 0), entry(Rank.SECOND, 0), entry(Rank.THIRD, 1),
-                entry(Rank.FOURTH, 0), entry(Rank.FIFTH, 0));
-    }
-
-    @Test
-    @DisplayName("당첨 번호와 로또 번호가 4개가 일치할 경우 4등")
-    void testName4() {
-        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
-        int bonusNumber = 7;
-        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 9, 10)));
-
-        result.matchLotto(winningNumber, lottos, bonusNumber);
-        Map<Rank, Integer> rankInfo = result.getRankInfo();
-
-        assertThat(rankInfo).contains(entry(Rank.FIRST, 0), entry(Rank.SECOND, 0), entry(Rank.THIRD, 0),
-                entry(Rank.FOURTH, 1), entry(Rank.FIFTH, 0));
-    }
-
-    @Test
-    @DisplayName("당첨 번호와 로또 번호가 3개가 일치할 경우 5등")
-    void testName5() {
-        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
-        int bonusNumber = 7;
-        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 11, 15, 16)));
-
-        result.matchLotto(winningNumber, lottos, bonusNumber);
-        Map<Rank, Integer> rankInfo = result.getRankInfo();
-
-        assertThat(rankInfo).contains(entry(Rank.FIRST, 0), entry(Rank.SECOND, 0), entry(Rank.THIRD, 0),
-                entry(Rank.FOURTH, 0), entry(Rank.FIFTH, 1));
+    static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6), Arrays.asList(1, 2, 3, 4, 5, 6), 7, Rank.FIRST, 1),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6), Arrays.asList(1, 2, 3, 4, 5, 7), 7, Rank.SECOND, 1),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6), Arrays.asList(1, 2, 3, 4, 5, 8), 7, Rank.THIRD, 1),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6), Arrays.asList(1, 2, 3, 4, 9, 10), 7, Rank.FOURTH, 1),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6), Arrays.asList(1, 2, 3, 14, 19, 10), 7, Rank.FIFTH, 1)
+        );
     }
 
     @Test
