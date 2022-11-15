87fd8243d55d694a7c819b1bbf66628e22720842
kim1387 <kevink0@naver.com>
2022-11-15 21:58:52 +0900

    feat: view와 핵심로직을 분리한다.

--- src/main/java/lotto/utils/ScoreUtil.java
+++ src/main/java/lotto/utils/ScoreUtil.java
@@ -2,8 +2,12 @@ package lotto.utils;
 
 import lotto.domain.ScoreInfo;
 
+import java.util.List;
+
 import static lotto.domain.ScoreInfo.ELSE;
 import static lotto.domain.ScoreInfo.FIVE;
+import static lotto.view.controller.ConsoleMessage.PRINT_BACK_TOTAL_RETURN_RATE;
+import static lotto.view.controller.ConsoleMessage.PRINT_FRONT_TOTAL_RETURN_RATE;
 
 public class ScoreUtil {
 
@@ -26,5 +30,14 @@ public class ScoreUtil {
         }
         return scoreInfo;
     }
-
+    public static String calRateOfReturnMessage(List<Integer> scoreBoard) {
+        Float totalReturn = 0F;
+        Integer totalBuyMoney = scoreBoard.stream().mapToInt(Integer::intValue).sum() * 1000;
+        for (ScoreInfo scoreInfo : ScoreInfo.values()) {
+            totalReturn += (scoreInfo.getWinnerMoney() * scoreBoard.get(scoreInfo.getRank()));
+        }
+        return PRINT_FRONT_TOTAL_RETURN_RATE.getMessage()
+                + String.format("%.1f", (totalReturn / totalBuyMoney) * 100)
+                + PRINT_BACK_TOTAL_RETURN_RATE.getMessage();
+    }
 }
--- src/main/java/lotto/view/controller/MoneyView.java
+++ src/main/java/lotto/view/controller/MoneyView.java
@@ -1,11 +1,11 @@
 package lotto.view.controller;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.domain.ScoreInfo;
 
 import java.util.List;
 
-import static lotto.view.controller.ConsoleMessage.*;
+import static lotto.utils.ScoreUtil.calRateOfReturnMessage;
+import static lotto.view.controller.ConsoleMessage.INPUT_BUY_MONEY;
 import static lotto.view.vlidation.InputMoneyValidator.validate;
 
 public class MoneyView {
@@ -20,14 +20,8 @@ public class MoneyView {
     }
 
     public static void printRateOfReturn(List<Integer> scoreBoard) {
-        Float totalReturn = 0F;
-        Integer totalBuyMoney = scoreBoard.stream().mapToInt(Integer::intValue).sum() * 1000;
-        for (ScoreInfo scoreInfo : ScoreInfo.values()) {
-            totalReturn += (scoreInfo.getWinnerMoney() * scoreBoard.get(scoreInfo.getRank()));
-        }
-        System.out.println(PRINT_FRONT_TOTAL_RETURN_RATE.getMessage()
-                + String.format("%.1f", (totalReturn / totalBuyMoney) * 100)
-                + PRINT_BACK_TOTAL_RETURN_RATE.getMessage());
+        String rateOfReturnMessage = calRateOfReturnMessage(scoreBoard);
+        System.out.println(rateOfReturnMessage);
 
     }
 }
