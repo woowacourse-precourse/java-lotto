eb1bb4ff161fa1da3aa9621f88d5d740973ab677
MinjeongKong <kong1301@ajou.ac.kr>
2022-11-15 22:58:46 +0900

    feat: 에러 문구 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -16,7 +16,7 @@ public class LottoController {
             lottoService.draw();
             lottoService.printResult();
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 잘못된 값이 입력 되었습니다.");
+            System.out.println("[ERROR] " + e.getMessage());
         }
 
     }
--- src/main/java/lotto/domain/Valid.java
+++ src/main/java/lotto/domain/Valid.java
@@ -10,32 +10,32 @@ public class Valid {
 
     public void isCorrectSize(List<Integer> numbers) throws IllegalArgumentException {
         if (numbers.size() != lottoSize) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("번호는 " + lottoSize + "개 여야 합니다.");
         }
     }
 
     public void isCorrectRange(List<Integer> numbers) throws IllegalArgumentException {
         if (!numbers.stream()
                 .allMatch(number -> (number >= startLottoNum && number <= endLottoNum))) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(startLottoNum + "부터 " + endLottoNum + " 사이의 숫자여야 합니다.");
         }
     }
 
     public void isNoDuplicate(List<Integer> numbers) throws IllegalArgumentException {
         if (numbers.size() != numbers.stream().distinct().count()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("중복되는 숫자가 없어야 합니다.");
         }
     }
 
     public void isNumeric(String amount) throws IllegalArgumentException {
         if (!amount.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
         }
     }
 
     public void isCorrectPrice(String amount) throws IllegalArgumentException {
         if (Integer.parseInt(amount) % lottoPrice != 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(lottoPrice + "원 단위로 입력해주세요.");
         }
     }
 }
