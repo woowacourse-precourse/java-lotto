43dca6241c9cde907530519812115bfe1dad988a
wodnjs7800 <wodnjs7800@naver.com>
2022-11-15 21:44:46 +0900

    로또 당첨번호

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,11 +11,11 @@ public class Lotto {
     }
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[Error] 당첨 번호를 잘못 입력 하셨습니다.");
+            throw new IllegalArgumentException("[ERROR] 당첨 번호를 잘못 입력 하셨습니다.");
         }
         for(int n:numbers){
             if(n<1 || n>45 || Collections.frequency(numbers,n)!=1){
-                throw new IllegalArgumentException("[Error] 당첨 번호를 잘못 입력 하셨습니다.");
+                throw new IllegalArgumentException("[ERROR] 당첨 번호를 잘못 입력 하셨습니다.");
             }
         }
     }
