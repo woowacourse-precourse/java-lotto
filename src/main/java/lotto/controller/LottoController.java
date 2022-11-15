48f0b51779c7c2fbe87cdfd3bf0439d18b494d70
CJiu01 <jiwoo020801@gmail.com>
2022-11-15 22:52:22 +0900

    [Feat] LottoController 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,14 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
 public class LottoController {
+    private InputView inputView;
+    private ResultView resultView;
+
+    public LottoController(InputView inputView, ResultView resultView) {
+        this.inputView = inputView;
+        this.resultView = resultView;
+    }
 }
