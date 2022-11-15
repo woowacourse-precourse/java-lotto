a552de9a32331f89b0486fdb375c340fb506ec66
SeongukBaek <bsu6235@gmail.com>
2022-11-15 12:01:43 +0900

    test: add new tests for class Lotto

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -18,15 +18,28 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호의 개수가 6개보다 작다면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("당첨 번호에 따른 계산 결과가 정상적이라면 그 결과를 반환한다.")
+    @DisplayName("로또 번호에 범위가 맞지 않는 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByNotInRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 60)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호와 당첨 번호에 대한 계산 후 결과를 반환한다.")
     @Test
     void computeResult_정상적인경우() {
         Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
@@ -34,5 +47,15 @@ class LottoTest {
         int bonusNumber = 7;
 
         Assertions.assertThat(lotto.computeResult(winningNumbers, bonusNumber)).isEqualTo(Result.SIX);
+    }
+
+    @DisplayName("로또 번호와 당첨 번호의 일치 개수가 3개 미만인 경우 결과는 NOTHING을 반환한다.")
+    @Test
+    void computeResult_NOTHING() {
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        Set<Integer> winningNumbers = new HashSet<>(Set.of(7,8,9,1,2,10));
+        int bonusNumber = 29;
+
+        Assertions.assertThat(lotto.computeResult(winningNumbers, bonusNumber)).isEqualTo(Result.NOTING);
     }
 }
