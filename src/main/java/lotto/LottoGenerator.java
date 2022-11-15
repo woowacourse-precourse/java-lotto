48268a27b795fe7dceb4c8f46d41c5714a04c6c5
youswim <youswim96@gmail.com>
2022-11-15 17:18:08 +0900

    fix(lotto): 로또 번호 생성이 7개까지 되는 오류 수정

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -9,7 +9,7 @@ public class LottoGenerator {
 
     public static List<Integer> makeLottoNumber() {
         List<Integer> lottoNumbers = new ArrayList<>();
-        while (lottoNumbers.size() <= Lotto.WIN_NUMBER_SIZE) {
+        while (lottoNumbers.size() < Lotto.WIN_NUMBER_SIZE) {
             int random = Randoms.pickNumberInRange(Lotto.MIN_NUMBER, Lotto.MAX_NUMBER);
             if (!lottoNumbers.contains(random)) {
                 lottoNumbers.add(random);
