e20b003197c98f3fdcfc5eb48253e64f9df27141
thdwoqor <celebarte01@gmail.com>
2022-11-15 13:37:47 +0900

    feat: 숫자인지 확인 메소드 추가

--- src/main/java/lotto/util/Validate.java
+++ src/main/java/lotto/util/Validate.java
@@ -1,11 +1,24 @@
 package lotto.util;
 
+import java.util.List;
+
 public class Validate {
     public static final String ERROR_IS_NULL = "[ERROR] NULL 값은 허용하지 않습니다.";
+    public static final String ERROR_INVALID_NUMBER = "[ERROR] 숫자이외의 값이 포함되어 있습니다.";
 
-    public static void isNull(Object number){
+    public static void isNull(Object number) {
         if (number == null) {
             throw new IllegalArgumentException(ERROR_IS_NULL);
         }
+    }
+
+    public static void isNumber(String number) {
+        if (!number.matches("[-+]?\\d*\\.?\\d+")) {
+            throw new IllegalArgumentException(ERROR_INVALID_NUMBER);
+        }
+    }
+
+    public static void isNumbers(List<String> numbers) {
+        numbers.stream().forEach(e -> isNumber(e));
     }
 }
