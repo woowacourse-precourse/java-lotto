ab78be6180aeea056a9a1b747aab6c0534ae03e5
sonsoowon <yuwoo1018@gmail.com>
2022-11-15 18:17:35 +0900

    test(JackpotBonusTest): 불필요한 import문 삭제

--- src/test/java/lotto/model/JackpotBonusTest.java
+++ src/test/java/lotto/model/JackpotBonusTest.java
@@ -1,13 +1,11 @@
 package lotto.model;
 
-import lotto.model.JackpotBonus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class JackpotBonusTest {
     private static final JackpotBonus jackpotBonus = new JackpotBonus(List.of(1, 2, 3, 4, 5, 6), 7);
