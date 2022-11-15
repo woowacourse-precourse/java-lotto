6fbd5ec40e1a25636aa784adb08df42f800986c2
jay <jaewhan232@naver.com>
2022-11-15 15:13:58 +0900

    Fix : 오타 수정

--- src/main/java/lotto/domain/LottoInputFilter.java
+++ src/main/java/lotto/domain/LottoInputFilter.java
@@ -3,7 +3,7 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
--import java.util.List;
+import java.util.List;
 
 public class LottoInputFilter {
 
