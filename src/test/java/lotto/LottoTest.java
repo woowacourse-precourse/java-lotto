329762d7118fb1dedfa7857cd1a3b790536c06a2
ddogga <hayeonlee60327@gmail.com>
2022-11-15 15:57:35 +0900

    LottoTest에 당첨 번호 개수를 구하는 테스트 케이스 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -26,8 +27,19 @@ class LottoTest {
     // 아래에 추가 테스트 작성 가능
 
     //당첨 개수 구하기 테스트
+    @DisplayName("당첨된 로또 번호 개수가 예상개수와 일치하지 않으면 예외가 발생한다.")
+    @Test
+    void checkCountWinningNumbers() {
+        List<Integer> winningNums = List.of(1,23,11,19,36,13);
+
+        Lotto lotto = new Lotto(List.of(6,13,19,23,33,39));
+
+        int result = lotto.countMatchedNumbers(winningNums);
+        assertThat(result).isEqualTo(3);
+    }
 
     //당첨 번호 개수가 5개 일때, 보너스 점수 일치 여부 테스트
+
 
     //등수 구하는 메서드 테스트
 }
