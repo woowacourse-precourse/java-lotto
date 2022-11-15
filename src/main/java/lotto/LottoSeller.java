30d208b4bcdc486d650f69c6e8139843fd607539
SeongbinPark <binni0515@gmail.com>
2022-11-15 17:11:14 +0900

    refactor(LottoSeller): extract methods - getCountOfLottos - printMessage - generateLottos

--- src/main/java/lotto/LottoSeller.java
+++ src/main/java/lotto/LottoSeller.java
@@ -6,11 +6,25 @@ public class LottoSeller {
     private static final String SUFFIX = "개를 구매했습니다.";
 
     public List<Lotto> sellLottos(int purchasePrice) {
-        int countOfLottos = purchasePrice / Lotto.PRICE;
+        int countOfLottos = getCountOfLottos(purchasePrice);
 
+        printMessage(countOfLottos);
+
+        List<Lotto> lottos = generateLottos(countOfLottos);
+
+        return lottos;
+    }
+
+    private int getCountOfLottos(int purchasePrice) {
+        return purchasePrice / Lotto.PRICE;
+    }
+
+    private void printMessage(int countOfLottos) {
         System.out.println();
         System.out.println(countOfLottos + SUFFIX);
+    }
 
+    private List<Lotto> generateLottos(int countOfLottos) {
         LottoGenerator lottoGenerator = new LottoGenerator();
         List<Lotto> lottos = lottoGenerator.generateLottos(countOfLottos);
 
