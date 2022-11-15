88fe8369990449b3c6489c834f206df753240959
mgOh <issac_oh@naver.com>
2022-11-15 16:47:41 +0900

    fix: 하드코딩 제거

--- src/main/java/lotto/input_processing/InputPrice.java
+++ src/main/java/lotto/input_processing/InputPrice.java
@@ -9,7 +9,10 @@ import java.util.*;
 
 public class InputPrice {
     static UserInputException userInputException = new UserInputException();
-
+    final static int minimumUnitOfAmount = 1000;
+    final static int lottoNumLength = 6;
+    final static int numSizeStart = 1;
+    final static int numSizeEnd = 45;
     public InputPrice(){}
 
     /** 구입금액 입력받기, 로또 구매 개수 계산 및 예외처리 후 로또 개수 반환 */
@@ -32,15 +35,15 @@ public class InputPrice {
             throw new IllegalArgumentException("[ERROR]");
         }
         // 구매한 로또 개수를 출력한다.
-        System.out.println(purchasingAmount / 1000 + "개를 구매했습니다.");
-        return purchasingAmount / 1000;
+        System.out.println(purchasingAmount / minimumUnitOfAmount + "개를 구매했습니다.");
+        return purchasingAmount / minimumUnitOfAmount;
     }
     /** 각 6개의 서로 다른 로또 번호 랜덤 생성 기능 */
     public static ArrayList<Lotto> createLottoNumbers(){
         int countOfLotto = buyingLotto();
         ArrayList<Lotto> lottoList = new ArrayList<Lotto>();
         for (int i=0;i<countOfLotto;i++){
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(numSizeStart, numSizeEnd, lottoNumLength);
             Integer[] arr = numbers.toArray(new Integer[numbers.size()]);
             Arrays.sort(arr); // ApplicationTest 에서 List.of를 사용해 직접 List 를 정렬하면 오류가 발생한다.
             List<Integer> sortedList = Arrays.asList(arr); // List<Integer> 형태로 반환하기 위해 다시 형변환
