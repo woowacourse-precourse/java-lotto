c3b1a15e7e21af003b9dce47bd926893b38d6401
ashlovesliitea <skyrider1811@gmail.com>
2022-11-15 20:43:36 +0900

    feat: InputView - 구입 금액 입력 UI 구현

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView{
 
     private void printMessage(String message) {
@@ -7,7 +9,8 @@ public class InputView{
     }
 
     public String requestUserMoney(){
-
+        printMessage("구입 금액을 입력해주세요.");
+        return readLine();
     }
 
     public String requestWinningNumbers(){
