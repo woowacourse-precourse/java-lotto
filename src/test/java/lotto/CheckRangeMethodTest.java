ece9b5e2d82aa1d27d067028ca89cc8edc7cba21
neppiness <0414kjh@naver.com>
2022-11-15 10:36:22 +0900

    test: refactor CheckRangeMethodTest (src/main/java/lotto)
    Add ERROR_MESSAGE variable

--- src/test/java/lotto/CheckRangeMethodTest.java
+++ src/test/java/lotto/CheckRangeMethodTest.java
@@ -6,10 +6,13 @@ import static lotto.Application.checkRange;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CheckRangeMethodTest {
-    private static final String ERROR_MESSAGE = "[ERROR] 당첨 번호는 1이상 45이하의 자연수로 구성해야 합니다.";
+    private static final String ERROR_MESSAGE = "[ERROR] 번호는 1이상 45이하의 자연수로 구성해야 합니다.";
 
     @Test
-    void checkRange_메소드_정상_입력_테스트1() { for (int number = 1; number <= 45; number++) checkRange(number); }
+    void checkRange_메소드_정상_입력_테스트1() {
+        for (int number = 1; number <= 45; number++)
+            checkRange(number);
+    }
 
     @Test
     void checkRange_메소드_예외_테스트1() {
