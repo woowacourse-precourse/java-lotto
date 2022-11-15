e8bfd65780a25e4281b4ae5cc6177c32880aa33d
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-15 11:18:13 +0900

    fix(MatchingForm): fix error # 10
    Found error # 10.
    Error no.10 is about matching form.
    Should check if number is positive integer.
    Cause of error:
        - + means 1 or more in regular expression.
        - positive number can be single digit.
    Fixed:
        - Change + to *
        - * means 0 or more.
    
    Closes # 10

--- src/main/java/lotto/constant/MatchingForm.java
+++ src/main/java/lotto/constant/MatchingForm.java
@@ -1,7 +1,7 @@
 package lotto.constant;
 
 public enum MatchingForm {
-    POSITIVE_INTEGER_FORM("[1-9][0-9]+"),
+    POSITIVE_INTEGER_FORM("[1-9][0-9]*"),
     SIX_POSITIVE_INTEGER_DIVIDED_BY_COMMA("[1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*");
 
     private final String form;
