68840e20eae503863e4040deef331b3c319b4f12
jam961020 <jam961020@naver.com>
2022-11-15 21:50:08 +0900

    Error출력 함수 및 구입 금액 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static void Error(String ErrorType){
+        throw new IllegalArgumentException("[ERROR] " + ErrorType);
+    }
+    public static Integer EnterAmount(){
+        Integer Amount;
+        Amount = Integer.parseInt(Console.readLine());
+        if(Amount%1000!=0){
+            Error("구매 금액은 1000의 배수여야 합니다.");
+        }
+        return Amount;
+    }
+    public static void InPut(){
+
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
