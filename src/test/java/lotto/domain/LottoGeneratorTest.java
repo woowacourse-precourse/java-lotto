2e3c4072adea65087e6859f6d7f177cf27eb9fcd
wormjoo <wormjoo@naver.com>
2022-11-15 20:03:28 +0900

    test: LottoGenerator 클래스에 대한 단위 테스트 구현

---
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoGeneratorTest {
+    @DisplayName("생성된 로또 번호가 오름차순으로 정렬되었는지 테스트")
+    @Test
+    void createLottoWithSortedRandomNumbers() {
+        List<Integer> lottoNumbers = LottoGenerator.createLottoNumbers();
+        List<Integer> sortedNumbers = lottoNumbers.stream().sorted()
+                .collect(Collectors.toList());
+        assertThat(lottoNumbers).isEqualTo(sortedNumbers);
+    }
+}
