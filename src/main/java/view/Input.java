7f99181481b097b81e06e8774a379df9f13e27a8
juno-junho <ssmm0205@naver.com>
2022-11-15 23:07:40 +0900

    feat(view): add exception
    숫자가 아닌 것을 입력했을때 예외처리

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -4,12 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 import utils.Util;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Input {
     private static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String LOTTO_NUMBER_INPUT_MESSAGE = "\n당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT_MESSAGE = "\n보너스 번호를 입력해 주세요.";
-
+    private static final Pattern PATTERN = Pattern.compile("^[\\d]+, [\\d]+, [\\d]+, [\\d]+, [\\d]+, [\\d]+$");
 
     public static int inputMoney() {
         System.out.println(MONEY_INPUT_MESSAGE);
@@ -33,7 +34,14 @@ public class Input {
 
     private static int inputNumber() {
         String textNumber = Console.readLine();
+        validateLottoNums(textNumber);
         return Util.convertStringToInt(textNumber);
+    }
+
+    private static void validateLottoNums(String lottoNumbers) {
+        if (!PATTERN.matcher(lottoNumbers).matches()) {
+            throw new IllegalArgumentException("[ERROR] 입력값이 잘못되었습니다.");
+        }
     }
 
 }
