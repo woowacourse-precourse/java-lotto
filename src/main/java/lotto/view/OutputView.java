e9f8b307618bcce8e103b9b5649368bded3d7a8a
ddukddaki <rlfrkdms1@naver.com>
2022-11-15 17:02:43 +0900

    feat(outputView): 발행한 로또 수량 및 번호를 출력하는 기능 (오름차순 정렬)

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,23 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OutputView {
+
+    private static final String OUTPUT_AMOUNT_OF_LOTTO = "개를 구매했습니다.";
 
     public void printErrorMessage(String errorMessage) {
         System.out.println(errorMessage);
+    }
+
+    public void outputLottoTickets(List<Lotto> lottoTickets) {
+        System.out.println(lottoTickets.size() + OUTPUT_AMOUNT_OF_LOTTO);
+        for (Lotto lottoTicket : lottoTickets) {
+            System.out.println(lottoTicket.getNumbers().stream().sorted().collect(Collectors.toList()));
+        }
     }
 
 }
