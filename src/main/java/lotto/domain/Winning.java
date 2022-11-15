1135846cafd17690d33c72f28d89d0babde9f1b7
yoonsj970407 <yoonsj970407@gmail.com>
2022-11-15 12:20:27 +0900

    Feat: 당첨 결과 계산하는 기능 구현

---
+++ src/main/java/lotto/domain/Winning.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Winning {
+    List<Integer> lottoNumbers;
+    int bonus, rank;
+
+    public Winning(List<Integer> lottoNumbers, int bonus) {
+        this.lottoNumbers = lottoNumbers;
+        this.bonus = bonus;
+    }
+
+    public int checkWinning(List<Integer> userNumbers) {
+        if(lottoNumbers.containsAll(userNumbers)) {
+            return rank = 1; // 6개 일치
+        } else if(lottoNumbers.contains(userNumbers)) {
+            rank = compareNumbers(userNumbers);
+
+            if(rank > 2) {
+                return rank;
+            }
+        }
+
+        return 0;
+    }
+
+    public int compareNumbers(List<Integer> userNumbers) {
+        int count = 0;
+
+        for(int i = 0; i < 6; i++) {
+            if(lottoNumbers.contains(userNumbers.get(i))) {
+                count++;
+            }
+        }
+
+        if(count == 5 && userNumbers.contains(bonus)) {
+            return count = 2;
+        }
+
+        return count;
+    }
+}
