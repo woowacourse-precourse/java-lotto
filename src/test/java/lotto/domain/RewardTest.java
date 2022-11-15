f54b323be81ae9f834598da726c67daf4ae44bd0
starwook <star.wookk@gmail.com>
2022-11-15 14:01:19 +0900

    test(RewardTest.java): Add RewardTest.java
    RewardTest.java 생성

---
+++ src/test/java/lotto/domain/RewardTest.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RewardTest {
+
+    @Test
+    void values() {
+        boolean bonusNumber =true;
+        int number =5;
+        Reward reward;
+
+    }
+
+}
