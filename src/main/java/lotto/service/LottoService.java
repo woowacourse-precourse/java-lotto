e8e36c5c83783ae97df66b54864134b771f48216
hhiyeon <hhyyeon0214@gmail.com>
2022-11-15 12:55:08 +0900

    feat: service-check bonus number

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -5,13 +5,20 @@ import lotto.domain.Rank;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 public class LottoService {
+    private static final int SECOND_RANK_MATCH = 7;
+    private static final int SECOND_THIRD_RANK_MATCH = 5;
+    private static final int NOTHING_MATCH = 0;
+    private static final int DEFAULT_MATCH = 1;
     private static final double DECIMAL_PLACE = 100.0f;
     private List<Lotto> lotteries = new ArrayList<>();
     private List<Integer> winnings = new ArrayList<>();
+    private Map<Integer, Integer> matches = new HashMap<>();
     private int bonus;
     private double buyMoney;
     private String profitRate;
@@ -19,6 +26,16 @@ public class LottoService {
 
     public LottoService() {
 
+    }
+
+    private boolean checkBonusMatch(int win, List<Integer> lottoNumbers) {
+        if (win == SECOND_THIRD_RANK_MATCH && checkBonusNumber(lottoNumbers)) {
+            matches.put(SECOND_RANK_MATCH,
+                    matches.getOrDefault(SECOND_RANK_MATCH, NOTHING_MATCH) + DEFAULT_MATCH);
+            return true;
+        }
+        matches.put(win, matches.getOrDefault(win, NOTHING_MATCH) + DEFAULT_MATCH);
+        return false;
     }
 
     public void addLotteries(int buyNumber) {
