d396f983603b24e11f004e0a5bae558b2d2a16ab
hyeonseog-won <smreo75@gmail.com>
2022-11-15 21:08:35 +0900

    feat: 로또 시작 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,12 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
+        Game game = new Game(new Ui(), new LottoTicket());
+        try {
+            game.start();
+        }catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
 
     }
 }
--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -22,6 +22,6 @@ public class Game {
 
         WinningResult winningResult = new WinningResult(winningLotto, lottos);
         ui.printWinningStatistics(winningResult);
-        ui.printTotalProfit(winningResult, purchaseAmount);
+        ui.printTotalProfit(winningResult,purchaseAmount);
     }
 }
