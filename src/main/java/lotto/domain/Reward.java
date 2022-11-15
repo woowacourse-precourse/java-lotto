0335ed5d69bdfce934e7bb356ff45f33adb70b7e
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-15 10:31:09 +0900

    refactor(Reward): 코드 공백, final 한정자 추가

--- src/main/java/lotto/domain/Reward.java
+++ src/main/java/lotto/domain/Reward.java
@@ -1,14 +1,14 @@
 package lotto.domain;
 
 public enum Reward {
-    FIRST_REWARD("6개 일치 (2,000,000,000원)",2000000000),
-    SECOND_REWARD("5개 일치, 보너스 볼 일치 (30,000,000원)",30000000),
-    THIRD_REWARD("5개 일치 (1,500,000원)",1500000),
-    FOURTH_REWARD("4개 일치 (50,000원)",50000),
-    FIFTH_REWARD("3개 일치 (5,000원)",5000);
+    FIRST_REWARD("6개 일치 (2,000,000,000원)", 2000000000),
+    SECOND_REWARD("5개 일치, 보너스 볼 일치 (30,000,000원)", 30000000),
+    THIRD_REWARD("5개 일치 (1,500,000원)", 1500000),
+    FOURTH_REWARD("4개 일치 (50,000원)", 50000),
+    FIFTH_REWARD("3개 일치 (5,000원)", 5000);
 
-    private String message;
-    private int reward;
+    private final String message;
+    private final int reward;
 
     Reward(String message, int reward) {
         this.message = message;
