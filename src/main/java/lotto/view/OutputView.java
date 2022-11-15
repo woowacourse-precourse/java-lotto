c8f2d2f9c251d805479e13320da6d0f1a5ceb149
eom-tae-in <eti0728@daum.net>
2022-11-15 19:50:42 +0900

    refactor(OutputView): printBuyingPriceQuestion() -> 구입 금액을 물어보는 문구를 출력해주는 메서드

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import java.util.Map;
+import lotto.domian.User;
+import lotto.domian.WinningCriteria.WinningCriteriaInfo;
+import static lotto.constant.Constant.*;
+
+
+public class OutputView {
+    public void printBuyingPriceQuestion() {
+        System.out.println("구입 금액을 입력해 주세요.");
+    }
+}
