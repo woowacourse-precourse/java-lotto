12e4cba1b260cb5010f18b32d4fb2007342421ea
고동우 <shp05179@gmail.com>
2022-11-15 13:57:09 +0900

    feat: 구매한 로또 출력 기능
    Lotto를 엘리먼트로 갖는 리스트를 입력으로 받아서
    로또 구매갯수는 size()메소드로,
    로또의 번호는 Lotto클래스의 getNumbers메소드로 적절히 출력한다

--- src/main/java/lotto/LottoConsoleIo.java
+++ src/main/java/lotto/LottoConsoleIo.java
@@ -1,8 +1,17 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoConsoleIo {
     void printRequestPurchase() {
         System.out.println("구입금액을 입력해주세요.");
+    }
+
+    static void printNoOfLotto(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto:lottos){
+            System.out.println(lotto.getNumbers());
+        }
     }
 
 }
