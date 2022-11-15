65e835a6eafb0b77ddc9df172ff328bca6e52566
p <gustjr2332@naver.com>
2022-11-15 20:11:43 +0900

    refactor(CreateRandomLotto) : 하드코딩 수정

--- src/main/java/Service/CreateRandomLotto.java
+++ src/main/java/Service/CreateRandomLotto.java
@@ -4,15 +4,15 @@ package Service;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CreateRandomLotto {
+    private static final int UNITOFPURCHASEAMOUNT = 1000;
 
     public static List<List<Integer>> createRandomLottoNumber(int purchaseAmount) {
-        int numberOfLotto = purchaseAmount / 1000;
+        int numberOfLotto = purchaseAmount / UNITOFPURCHASEAMOUNT;
         List<List<Integer>> randomLottoNumbers = new ArrayList<>();
 
         for (int count = 0; count < numberOfLotto; count++) {
