3876bf1bad5b146a2001c082b90a82ec9c7df63b
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-15 16:49:28 +0900

    test: 로또 번호 범위 테스트
    1~45 범위 이외의 번호를 입력할 경우

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,12 +7,14 @@ public class Lotto {
 
     private static final String LOTTO_INVALID_SIZE_ERROR = "[ERROR] 로또 번호가 6자리가 아닙니다.";
     private static final String LOTTO_DUPLICATED_SIZE_ERROR = "[ERROR] 로또 번호가 중복되었습니다.";
+    private static final String LOTTO_INVALID_RANGE_ERROR = "[ERROR] 로또 번호의 범위를 잘못 입력하였습니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers;
         validate();
         validateDuplicate();
+        validateRange();
     }
 
     private void validate() {
@@ -25,6 +27,14 @@ public class Lotto {
         HashSet<Integer> lotto = new HashSet<>(numbers);
         if (lotto.size() != 6) {
             throw new IllegalArgumentException(LOTTO_DUPLICATED_SIZE_ERROR);
+        }
+    }
+
+    private void validateRange() {
+        for (int number: numbers) {
+            if (!(number > 0 && number < 46)) {
+                throw new IllegalArgumentException(LOTTO_INVALID_RANGE_ERROR);
+            }
         }
     }
 
