c85b53b3e6e7d4fdaa3e0773da72584522809e3f
devMuscle <hongsbien@naver.com>
2022-11-15 15:19:10 +0900

    refactor: 생성자 오버로딩

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Prize extends Lotto{
-    private final Integer bonusNumber;
+    private Integer bonusNumber;
 
     public Prize(List<Integer> numbers, Integer bonusNumber) {
         super(numbers);
         validateBonusNumber(numbers, bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    public Prize(List<Integer> numbers) {
+        super(numbers);
     }
 
     private void validateBonusNumber(List<Integer> numbers, Integer bonusNumber) {
@@ -20,5 +24,10 @@ public class Prize extends Lotto{
 
     public Integer getBonusNumber() {
         return bonusNumber;
+    }
+
+    public void setBonusNumber(Integer bonusNumber) {
+        validateBonusNumber(this.getNumbers(), bonusNumber);
+        this.bonusNumber = bonusNumber;
     }
 }
