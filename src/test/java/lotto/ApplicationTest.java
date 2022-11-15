b5e335ef1bec31e15dc2362bd52a5d3d8011ac53
김태용 <amico741@naver.com>
2022-11-15 13:23:21 +0900

    refactor: 당첨번호티켓 테스트 클래스 분리

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -86,34 +86,6 @@ class ApplicationTest extends NsTest {
                 .isThrownBy(() -> new Money(input));
     }
 
-    @Test
-    @DisplayName("당첨 번호의 숫자 범위가 1 ~ 45가 벗어나면 IllegalArgumentException 발생한다.")
-    void 당첨번호범위_벗어나면_예외발생() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new WinningTicket(Arrays.asList(1, 2, 3, 4, 5, 46), 7));
-    }
-
-    @Test
-    @DisplayName("당첨 번호의 숫자 리스트 길이가 6이 아니면 IllegalArgumentException 발생한다.")
-    void 당첨번호리스트_길이가_6을_벗어나면_예외발생() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new WinningTicket(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 8));
-    }
-
-    @Test
-    @DisplayName("당첨번호 리스트에 중복이 존재하면 IllegalArgumentException 발생한다.")
-    void 당첨번호리스트_중복있으면_예외발생() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new WinningTicket(Arrays.asList(1, 2, 3, 4, 6, 6), 8));
-    }
-
-    @Test
-    @DisplayName("보너스 번호가 당첨번호 리스트에 존재하면 IllegalArgumentException 발생한다.")
-    void 보너스번호_중복이면_예외발생() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new WinningTicket(Arrays.asList(1, 2, 3, 4, 5, 6), 6));
-    }
-
     @Override
     public void runMain() {
         Application.main(new String[]{});
