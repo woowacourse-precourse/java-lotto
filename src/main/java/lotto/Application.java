22e8a3a11c595423390664dd9dfff45bac9a619a
ch0213 <cndghks0213@gmail.com>
2022-11-15 16:41:05 +0900

    feat: 로또 게임 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,32 @@
 package lotto;
 
+import lotto.domain.Lottos;
+import lotto.domain.LottosGenerator;
+import lotto.domain.Money;
+import lotto.domain.RandomNumbersGenerationStrategy;
+import lotto.domain.Result;
+import lotto.domain.WinningNumber;
+
+import static lotto.ui.InputView.*;
+import static lotto.ui.OutputView.*;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            LottosGenerator generator = new LottosGenerator(new RandomNumbersGenerationStrategy());
+            Money money = new Money(money());
+            WinningNumber winningNumber = new WinningNumber(winningNumber(), bonusNumber());
+
+            Lottos lottos = generator.lottos(money.count());
+            printLottos(lottos);
+
+            Result result = new Result(lottos.compare(winningNumber), money);
+            printResult(result);
+        } catch (IllegalArgumentException e) {
+            printErrorMessage(e);
+        } catch (Exception e) {
+            printUnexpectedErrorMessage();
+        }
     }
 }
