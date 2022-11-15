2c8d1f44369efdeea2e145e8b36907706c7b0def
dingding-21 <phj8221@naver.com>
2022-11-15 15:08:12 +0900

    feat($Game): 1부터 45까지 중복되지 않는 6개의 숫자 생성하는 기능 구현

--- src/main/java/lotto/domain/Game.java
+++ src/main/java/lotto/domain/Game.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.utils.Constant;
+
+import java.util.List;
 
 public class Game {
     public Game() {
@@ -8,5 +11,9 @@ public class Game {
 
     public Integer getLottoCount(int amount) {
         return amount / Constant.PURCHASE_AMOUNT_UNIT;
+    }
+
+    private List<Integer> getRandomLottoNumber() {
+        return Randoms.pickUniqueNumbersInRange(Constant.MIN_LOTTO_NUMBER, Constant.MAX_LOTTO_NUMBER, Constant.LOTTO_NUMBER_COUNT);
     }
 }
--- src/main/java/lotto/utils/Constant.java
+++ src/main/java/lotto/utils/Constant.java
@@ -2,4 +2,8 @@ package lotto.utils;
 
 public class Constant {
     public static final Integer PURCHASE_AMOUNT_UNIT = 1000;
+    public static final Integer MAX_LOTTO_NUMBER = 45;
+    public static final Integer MIN_LOTTO_NUMBER = 1;
+    public static final Integer LOTTO_NUMBER_COUNT = 6;
+
 }
