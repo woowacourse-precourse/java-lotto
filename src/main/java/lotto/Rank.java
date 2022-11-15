9d01c77fd4727f1a9927ae1c7c0aa91133618103
gichan-song <thdrlcks97@gmail.com>
2022-11-15 23:42:53 +0900

    feat(Rank) : 당첨 등수 enum 추가
    로또 게임의 당첨 등수 정의

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,34 @@
+package lotto;
+
+public enum Rank {
+
+  FIRST("1등", 6, 2000000000),
+  SECOND("2등", 7, 30000000),
+  THIRD("3등", 5, 1500000),
+  FOURTH("4등", 4, 50000),
+  FIFTH("5등", 3, 5000),
+  UNRANKED("낙첨", 0, 0);
+
+  private final String message;
+  private final int wins;
+  private final int price;
+
+  Rank(String message, int wins, int price) {
+    this.message = message;
+    this.wins = wins;
+    this.price = price;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public int getWins() {
+    return wins;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+}
