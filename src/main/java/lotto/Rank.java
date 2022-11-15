1d2c679aeed74219b5de74369799261644fce893
thdwoqor <celebarte01@gmail.com>
2022-11-15 17:44:57 +0900

    refactor: 코드 정렬

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -3,10 +3,10 @@ package lotto;
 import java.util.Arrays;
 
 public enum Rank {
-    FAIL(new Result(0, false), 0,""),
-    FIVE(new Result(3, false), 5_000,"3개 일치 (5,000원) - "),
+    FAIL(new Result(0, false), 0, ""),
+    FIVE(new Result(3, false), 5_000, "3개 일치 (5,000원) - "),
     FOUR(new Result(4, false), 50_000, "4개 일치 (50,000원) - "),
-    THREE(new Result(5, false), 1_500_000,"5개 일치 (1,500,000원) - "),
+    THREE(new Result(5, false), 1_500_000, "5개 일치 (1,500,000원) - "),
     TWO(new Result(5, true), 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
     ONE(new Result(6, false), 2_000_000_000, "6개 일치 (2,000,000,000원) - ");
 
@@ -14,7 +14,7 @@ public enum Rank {
     private final int prize;
     private final String text;
 
-    Rank(final Result result, final int prize,final String text) {
+    Rank(final Result result, final int prize, final String text) {
         this.result = result;
         this.prize = prize;
         this.text = text;
