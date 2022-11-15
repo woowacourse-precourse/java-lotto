cc4c1d38da664673e7da6af8a0ed6590a9c2ec0d
dydckd <toofriend09@naver.com>
2022-11-15 10:16:04 +0900

    test : 구입 금액 입력 테스트 , 로또 당첨 번호 테스트 추가
    - 구입금액이 1000단위로 입력이 안될시 예외발생 테스트
    - 로또 당첨 번호 입력시 중복 이거나 6자리가 넘거나 1~45범위의 수가 아닐시에 예외발생 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +24,38 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또를 사는 돈이 1000의 배수가 아니면 예외 발생")
+    @Test
+    void inputWrongNum(){
+        assertThatThrownBy(() -> new Buyer(15500))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @DisplayName("로또 당첨 번호 입력시 6개가 넘어가면 예외발생")
+    @Test
+
+    void WinningNumRange(){
+        Buyer buyer = new Buyer();
+        assertThatThrownBy(() -> buyer.getWinningNum("1,2,3,4,5,6,7"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 당첨 번호 입력시 중복이 있으면 예외발생")
+    @Test
+
+    void DuplicateWinningNum(){
+        Buyer buyer = new Buyer();
+        assertThatThrownBy(() -> buyer.getWinningNum("1,1,3,4,5,6,7"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 당첨 번호 입력시 1~45범위안의 수가 아니면 예외발생")
+    @Test
+
+    void WinningNumRangeOut(){
+        Buyer buyer = new Buyer();
+        assertThatThrownBy(() -> buyer.getWinningNum("0,2,3,4,5,6,46"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
