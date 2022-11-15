ff8928169700bd0732572729e78fe34a1e1ee933
이건회 <rg970604@naver.com>
2022-11-15 18:41:17 +0900

    style(LottoPiecesTest.java) : 미사용 import 삭제

--- src/test/java/lotto/LottoPiecesTest.java
+++ src/test/java/lotto/LottoPiecesTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.*;
 
 import lotto.domain.LottoPieces;
 import lotto.domain.Money;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
