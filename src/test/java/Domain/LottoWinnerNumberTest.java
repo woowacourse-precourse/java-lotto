02ee0340f6baf8b4e222842d17e1ecf850338911
nahyeon cho <whskgus12@nate.com>
2022-11-15 23:00:08 +0900

    test(domain): 당첨 번호가 숫자가 아닐 때
    당첨 번호에 숫자와 콤마 이외의 문자가 있을 때 예외가 발생하는지 확인합니다.
    테스트 대상은 LottoWinnerNumber 클래스의 validInputWinNumber 함수입니다.
    테스트 결과 정상적으로 예외가 발생하는 것을 확인했습니다.

--- src/test/java/Domain/LottoWinnerNumberTest.java
+++ src/test/java/Domain/LottoWinnerNumberTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoWinnerNumberTest {
@@ -24,6 +25,13 @@ class LottoWinnerNumberTest {
 
         //then
         assertEquals(convertLottoNumber,expectLottoNumber);
+    }
+
+    @Test
+    @DisplayName("당첨 번호에 숫자와 콤마 이외의 문자가 있다면 예외가 발생한다.")
+    void validInputWinNumber() {
+        assertThatThrownBy(() -> new LottoWinnerNumber("2,3,4,8,9;10;"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
