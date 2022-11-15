57e47a9cc93b8b8f264afc048cc37811e9f0eb41
이정우 <acg6138@naver.com>
2022-11-15 15:32:17 +0900

    refactor(Rank): 출력을 위한 당첨 금액을 속성으로 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,18 +3,20 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    LOSE(0, 0),
-    FIFTH(5000, 3),
-    FOURTH(50000, 4),
-    THIRD(1500000, 5),
-    SECOND(30000000, 5),
-    FIRST(2000000000, 6);
+    LOSE(0, "0원", 0),
+    FIFTH(5000, "(5,000원)", 3),
+    FOURTH(50000, "(50,000원)", 4),
+    THIRD(1500000, "(1,500,000원)", 5),
+    SECOND(30000000, "(30,000,000원)", 5),
+    FIRST(2000000000, "(2,000,000,000원)", 6);
 
     private final int prize;
+    private final String prizeForPrint;
     private final int matchCount;
 
-    Rank(int prize, int matchCount) {
+    Rank(int prize, String prizeForPrint, int matchCount) {
         this.prize = prize;
+        this.prizeForPrint = prizeForPrint;
         this.matchCount = matchCount;
     }
 
@@ -30,5 +32,13 @@ public enum Rank {
 
     public int getPrize() {
         return prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public String getPrizeForPrint() {
+        return getPrizeForPrint();
     }
 }
