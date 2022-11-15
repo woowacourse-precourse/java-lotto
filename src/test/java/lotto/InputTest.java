3293746ca7b8b450bb54955b71a564819e65f65f
star-sil <kimse9811@naver.com>
2022-11-15 23:58:29 +0900

    refactor(test/lotto): update test code
    사용자 입력 오류가 발생할때 에러 발생이 아닌 알맞은 에러 메시지를 출력하는지 확인

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -5,15 +5,18 @@ import lotto.view.InputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputTest extends NsTest {
     @DisplayName("구입금액 1000원으로 나누어 떨어지지 않는 경우 예외가 발생한다.")
     @Test
     void 구입금액이_1000원으로_나누어_떨어지지_않는_경우_예외_발생() {
-        assertThatThrownBy(() -> runException("1234"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 유효하지 않은 금액 단위입니다.");
+        assertSimpleTest(() -> {
+            runException("1234");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 금액 단위입니다.");
+        });
     }
 
     @DisplayName("당첨번호를 6개 입력하지 않으면 예외 발생")
