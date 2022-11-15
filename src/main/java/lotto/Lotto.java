abea3997288238d2a2f0f7e4a51041f0d2b08477
김선동 <mok03127@gmail.com>
2022-11-15 16:35:27 +0900

    feat: 로또 번호는 1부터 45 사이의 수 그렇지 않은 경우 예외 처리
    이후 정답 로또에도 상속해서 쓸 기능이므로 protected로 선언

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,9 +6,13 @@ import java.util.List;
 import java.util.Objects;
 
 public class Lotto {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private static final int LOTTO_SIZE = 6;
     private static final String LOTTO_SIZE_ERROR = "[ERROR] 중복되지 않는 숫자 6개를 입력해 주세요.";
     private static final String LOTTO_HAS_DUPLICATED_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복될 수 없습니다.";
+    private static final String LOTTO_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -25,7 +29,7 @@ public class Lotto {
         checkLottoSize(numbers);
         for (Integer number : numbers) {
             checkDuplicates(numbers, number);
-
+            checkLottoRange(number);
         }
     }
 
@@ -38,6 +42,12 @@ public class Lotto {
     private static void checkDuplicates(List<Integer> numbers, Integer number) {
         if (numbers.indexOf(number) != numbers.lastIndexOf(number)) {
             throw new IllegalArgumentException(LOTTO_HAS_DUPLICATED_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
+    protected void checkLottoRange(Integer number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException(LOTTO_RANGE_ERROR_MESSAGE);
         }
     }
 
