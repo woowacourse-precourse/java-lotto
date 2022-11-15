521bc9fd9fa13e8da14ce5fa4f406efa50fd5ae2
MuHyeonSon <smuhyeon84@gmail.com>
2022-11-15 10:26:35 +0900

    feat(lotto): 받은 금액에 대한 로또 구매 개수를 계산하는 기능

---
+++ src/main/java/lotto/Calculation.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class Calculation {
+    public Integer calculateNumberOfLottoes(Integer enteredMoney) {
+        int numberOfLottoes = enteredMoney /1000;
+        return numberOfLottoes;
+    }
+}
