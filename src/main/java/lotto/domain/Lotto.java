35208baed0b25087ff19fb9446648a57c6b05ce9
CJiu01 <jiwoo020801@gmail.com>
2022-11-15 22:49:00 +0900

    [Feat] Lotto 객체 유효성 검사 코드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,10 +3,12 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+    private static final int LOTTERY_NUMBER_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+
         this.numbers = numbers;
     }
 
@@ -14,7 +16,22 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+        for (int i = 0; i < numbers.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (numbers.get(i).equals(numbers.get(j))) {  // 중복 검사
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
     }
 
-    // TODO: 추가 기능 구현
+    public boolean contains(Integer number) {
+        return numbers.contains(number);
+    }
+
+    public String toString() {
+        return numbers.toString();
+    }
+
 }
