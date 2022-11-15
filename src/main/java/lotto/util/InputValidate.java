fd90c00717b8395dddadb9e6c1ee84f07d9dd05c
charlesuu <tiger0192004@gmail.com>
2022-11-15 20:42:22 +0900

    fix: 무의미한 당첨 번호 검증 메서드 삭제

--- src/main/java/lotto/util/InputValidate.java
+++ src/main/java/lotto/util/InputValidate.java
@@ -4,13 +4,12 @@ import java.util.Collections;
 import java.util.HashSet;
 
 public class InputValidate {
+    private static final String ERROR_MESSAGE_NOT_INT = "[ERROR] 숫자만 입력 가능합니다.";
     private static final String ERROR_MESSAGE_NOT_PROPER_AMOUNT = "[ERROR] 구입 금액은 1000원 단위로 입력할 수 있습니다.";
     private static final String ERROR_MESSAGE_NOT_PROPER_WINNING_NUMBER_COUNT = "[ERROR] 6개의 숫자를 입력해야 합니다.";
-    private static final String ERROR_MESSAGE_STRING_NOT_INCLUDE_FIVE_COMMA = "[ERROR] 5개의 쉼표를 포함해야 합니다.";
     private static final String ERROR_MESSAGE_NOT_INT_BETWEEN_COMMA = "[ERROR] 각 쉼표 사이에 숫자만 입력 가능합니다.";
-    private static final String ERROR_MESSAGE_NOT_PROPER_NUMBER_RANGE = "[ERROR] 1~45 사이의 수를 입력해야 합니다.";
     private static final String ERROR_MESSAGE_NUMBER_DUPLICATE = "[ERROR] 각 숫자는 중복될 수 없습니다.";
-    private static final String ERROR_MESSAGE_NOT_INT = "[ERROR] 숫자만 입력 가능합니다.";
+    private static final String ERROR_MESSAGE_NOT_PROPER_NUMBER_RANGE = "[ERROR] 1~45 사이의 수를 입력해야 합니다.";
 
     public InputValidate() {
     }
@@ -21,21 +20,10 @@ public class InputValidate {
     }
 
     public void validateInputWinningNumber(String input) {
-        validateStringContainFiveComma(input);
         validateElementCount(input);
         validateOnlyNumbers(input);
         validateNotDuplicate(input);
         validateNumberRange(input);
-    }
-
-    private void validateStringContainFiveComma(String input) {
-        final int expectCommaCount = 5;
-
-        int commaCount = input.length() - input.replace(String.valueOf(','), "").length();
-
-        if (expectCommaCount != commaCount) {
-            throw new IllegalArgumentException(ERROR_MESSAGE_STRING_NOT_INCLUDE_FIVE_COMMA);
-        }
     }
 
     private void validateElementCount(String input) {
