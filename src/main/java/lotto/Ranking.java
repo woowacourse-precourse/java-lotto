044e1b43219f807c5b574298132e1101121f713c
Woogie95 <choisw950928@gmail.com>
2022-11-15 19:42:59 +0900

    refactor: enum 수정

--- src/main/java/lotto/Ranking.java
+++ src/main/java/lotto/Ranking.java
@@ -1,20 +1,40 @@
 package lotto;
 
+import java.util.Arrays;
+
 public enum Ranking {
-    NONE(0, 0),
-    FIFTH(3, 5_000),
-    FOURTH(4, 50_000),
-    THIRD(5, 1_500_000),
-    SECOND(5, 30_000_000),
-    FIRST(6, 2_000_000_000);
+    NONE(0, 0, false),
+    FIFTH(5_000, 3, false),
+    FOURTH(50_000, 4, false),
+    THIRD(1_500_000, 5, false),
+    SECOND(30_000_000, 5, true),
+    FIRST(2_000_000_000, 6, false);
 
-    private final int numberCount;
     private final int prizeMoney;
+    private final int numberCount;
+    private final boolean isBonusNumber;
 
-    Ranking(int numberCount, int prizeMoney) {
-        this.numberCount = numberCount;
+    public static Ranking findOfRanking(int numberCount, boolean isBonusNumber) {
+        return Arrays.stream(Ranking.values())
+                .filter(ranking -> checkNumberCountBonus(ranking, numberCount, isBonusNumber))
+                .findAny()
+                .orElse(NONE);
+    }
+
+    private static boolean checkNumberCountBonus(Ranking ranking, int numberCount, boolean isBonusNumber) {
+        if (ranking.getNumberCount() != numberCount) {
+            return false;
+        }
+        if (SECOND.getNumberCount() == numberCount) {
+            return ranking.isBonusNumber() == isBonusNumber;
+        }
+        return true;
+    }
+
+    Ranking(int prizeMoney, int numberCount, boolean isBonusNumber) {
         this.prizeMoney = prizeMoney;
-
+        this.numberCount = numberCount;
+        this.isBonusNumber = isBonusNumber;
     }
 
     public int getNumberCount() {
@@ -23,6 +43,10 @@ public enum Ranking {
 
     public int getPrizeMoney() {
         return prizeMoney;
+    }
+
+    public boolean isBonusNumber() {
+        return isBonusNumber;
     }
 
 }
