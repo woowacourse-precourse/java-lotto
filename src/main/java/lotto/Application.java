b9b4f5bc6ab03c92c327800aa4ecfc0b71e37f40
dlgosla <dlgoslaek1@gmail.com>
2022-11-15 23:40:04 +0900

    feat: 컨트롤러 시작

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
 public class Application {
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            new Controller().start();
+        } catch (IllegalArgumentError e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
