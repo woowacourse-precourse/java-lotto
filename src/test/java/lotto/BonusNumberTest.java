cfd025e87e071c5c7ad384c803a46670a8facc44
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-11-15 19:21:01 +0900

    test<BonusNumberTest>: 리팩토링에 의한 테스트 코드 내 클래스명 변경

--- src/test/java/lotto/BonusNumberTest.java
+++ src/test/java/lotto/BonusNumberTest.java
@@ -12,14 +12,14 @@ public class BonusNumberTest {
     @DisplayName("로또 번호와 보너스 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createBonusNumberByDuplicatedNumber() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6), 6))
+        assertThatThrownBy(() -> new BonusNumber(List.of(1, 2, 3, 4, 5, 6), 6))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("보너스 번호에 1~45 이외의 숫자가 있으면 예외가 발생한다.")
     @Test
     void createBonusNumberNotMeetRequirement() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6), 46))
+        assertThatThrownBy(() -> new BonusNumber(List.of(1, 2, 3, 4, 5, 6), 46))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
