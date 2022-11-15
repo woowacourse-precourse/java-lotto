a860012dc87c3b76cf7a507fcef287e853c70ea1
sschan99 <seungchan1219@gmail.com>
2022-11-15 11:35:40 +0900

    feat(LotterySeller): receive money and issue lotto

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,8 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LotterySeller lotterySeller = new LotterySeller();
+        lotterySeller.sellLotto();
+        System.out.println(lotterySeller.soldLotto);
     }
 }
---
+++ src/main/java/lotto/LotterySeller.java
@@ -0,0 +1,40 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LotterySeller {
+    int numberOfLottoIssued = 0;
+    List<Lotto> soldLotto = new ArrayList<>();
+
+    public void sellLotto() {
+        receiveMoney();
+        issueLotto();
+        printLotto();
+    }
+
+    public void receiveMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String userInput = Console.readLine();
+        System.out.println();
+
+        int money = Integer.parseInt(userInput);
+        if (money % 1000 != 0) {
+            System.out.println("거스름돈: " + money % 1000 + "원");
+        }
+        numberOfLottoIssued += money / 1000;
+    }
+
+    public void issueLotto() {
+        while (soldLotto.size() != numberOfLottoIssued) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            soldLotto.add(lotto);
+        }
+    }
+
+    public void printLotto() {
+
+    }
+}
