51d41ed0e75069261e6961a75fbb9d74e51aa712
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-15 10:09:20 +0900

    style: 당첨 결과 초기화 메소드 이름 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -15,11 +15,12 @@ public class LottoController {
 
     private final int LOTTO_ONE_GAME_PRICE = 1000;
     private static final int PERCENTAGE_CONVERTER = 100;
+    private final int DEFAULT_EARNING_MONEY = 0;
     private Map<LottoRank, Integer> lottoResult = new HashMap<>();
 
     public void run() {
         int inputLottoMoney = inputLottoPurchaseMoney();
-        int lottoTickets = createLottoTickets(inputLottoMoney);
+        int lottoTickets = generateLottoTickets(inputLottoMoney);
         List<Integer> winningNumber = inputWinningNumber();
         int bonusNumber = inputBonusNumber();
 
@@ -31,12 +32,12 @@ public class LottoController {
         printLottoEarningRate(lottoEarningRate);
     }
 
-    private int createLottoTickets(int inputMoney) {
+    private int generateLottoTickets(int inputMoney) {
         return inputMoney / LOTTO_ONE_GAME_PRICE;
     }
 
     private void calculrateLotto(int lottoTickets, WinningNumbers winningNumbers) {
-        lottoResultMap();
+        initializeLottoResultMap();
         for (int i = 0; i < lottoTickets; i++) {
             List<Integer> lotto = LottoTicket.createLottoOneGameNumber();
             LottoRank lottoRank = winningNumbers.calculrateRank(lotto);
@@ -44,7 +45,7 @@ public class LottoController {
         }
     }
 
-    private void lottoResultMap() {
+    private void initializeLottoResultMap() {
         for (LottoRank lottoRank : LottoRank.values()) {
             lottoResult.put(lottoRank, 0);
         }
@@ -57,7 +58,7 @@ public class LottoController {
     }
 
     private int calculrateLottoEarningMoney() {
-        int earningMoney = 0;
+        int earningMoney = DEFAULT_EARNING_MONEY;
         for (LottoRank lottoRank : lottoResult.keySet()) {
             if (lottoResult.get(lottoRank) >= 1) {
                 earningMoney += lottoResult.get(lottoRank) * lottoRank.getRewardMoney();
