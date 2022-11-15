bffb8b1629ff714102cdd57467bb5f92b6485a66
mikekang47 <gyeong0308@gmail.com>
2022-11-15 19:18:20 +0900

    Feat<Lotto>: 보너스 번호 추가 및 생성자 추가

--- src/main/java/lotto/domains/Lotto.java
+++ src/main/java/lotto/domains/Lotto.java
@@ -7,6 +7,15 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
 
+    private int bonusNumber;
+
+    public Lotto(List<Integer> numbers, int bonusNumber) {
+        Validator.validateLottoNumber(numbers);
+        Validator.validateBonusNumber(numbers, bonusNumber);
+        this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
+    }
+
     public Lotto(List<Integer> numbers) {
         Validator.validateLottoNumber(numbers);
         this.numbers = numbers;
@@ -14,5 +23,9 @@ public class Lotto {
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
     }
 }
