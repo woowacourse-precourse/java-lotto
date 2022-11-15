d87f240062cb324862c06780505982e714f0041b
CJ <choijun0627@naver.com>
2022-11-15 13:41:05 +0900

    <feat>(CalculatingLotto.java): 기능 요구 사항 12번 구현
    기능 요구 사항 12번 당첨 번호와 발행 로또 번호 비교하는 함수 구현
    
    함수 구현 및 필요한 상수 선언

--- src/main/java/lotto/CalculatingLotto.java
+++ src/main/java/lotto/CalculatingLotto.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class CalculatingLotto {
     private static final int LOTTONUMCOUNT=6;
+    private static final int YES=1;
+    private static final int NO=0;
 
     public int calculateLottoRank(List<Integer> winningLotto, int bonus, List<Integer> oneLotto){
         int sameNumCount=0;
@@ -16,6 +18,13 @@ public class CalculatingLotto {
 
         int rank=getLottoRank(sameNumCount,bonusCount);
         return rank;
+    }
+
+    public int findSameNumber(int winningLottoNumber, int oneLottoNumber){
+        if(winningLottoNumber==oneLottoNumber){
+            return YES;
+        }
+        return NO;
     }
 
     public List<Integer> calculateAllLotto(List<Integer> winningLotto, int bonus, List<List<Integer>> allLotto){
