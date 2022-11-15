8739a089ad3dd42b65cdf9a14b73884beb0175e0
ashlovesliitea <skyrider1811@gmail.com>
2022-11-15 19:13:43 +0900

    feat: 사용자 구입금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+
+    public int userInput(String s) {
+        int number=0;
+        try{
+            number=Integer.parseInt(s);
+            if(number<1000 || number%1000!=0){
+                throw new IllegalArgumentException();
+            }
+            return number/1000;
+        }
+        catch(NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
     }
 }
