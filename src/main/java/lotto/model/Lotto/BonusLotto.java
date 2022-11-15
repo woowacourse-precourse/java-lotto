0f5921c325ab515fb97c3df2bf11afafcd0fe35c
sooyoungh <sooyoung.h8@gmail.com>
2022-11-15 17:41:54 +0900

    ✨ feat(BonusLotto): 보너스 로또 맞췄는지 판단하는 기능 구현

--- src/main/java/lotto/model/Lotto/BonusLotto.java
+++ src/main/java/lotto/model/Lotto/BonusLotto.java
@@ -1,10 +1,16 @@
 package lotto.model.Lotto;
 
+import java.util.List;
+
 public class BonusLotto {
 
     private final Integer number;
 
     public BonusLotto (Integer number) {
         this.number = number;
+    }
+
+    public Boolean isBonusLotto(List<Integer> userTicket) {
+        return userTicket.contains(number);
     }
 }
