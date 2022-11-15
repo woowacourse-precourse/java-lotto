3c0994136c296cd6a28d4d5415da3cb1fd69f56d
Seoyoung <ryupark1006@gmail.com>
2022-11-16 01:45:50 +0900

    feat: 수익률 출력 기능 구현

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class OutputView {
 
     public static void printLottos(List<Lotto> lottos, int amount) {
-        System.out.println(Integer.toString(amount) + "개를 구매했습니다.");
+        System.out.println(amount + "개를 구매했습니다.");
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
         }
@@ -18,5 +18,9 @@ public class OutputView {
         for (Result result : Result.values()) {
             System.out.println(result.getMessage() + lottoResult.get(result) + "개");
         }
+    }
+
+    public static void printEarning(double earning) {
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", earning);
     }
 }
