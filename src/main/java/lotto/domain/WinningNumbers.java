3e346308df580ffe168f0f1788e94589ce77d9a3
KimJaeSung <behappy0916@naver.com>
2022-11-15 11:01:19 +0900

    feat: 랭킹을 업데이트하는 기능 구현
    자동으로 뽑은 로또와
    입력받은 로또를 비교해서
    Ranking enumMap을 업데이트함.

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,32 +1,57 @@
 package lotto.domain;
 
-import lotto.domain.LottoNumber;
-import lotto.domain.LottoTickets;
-import java.util.ArrayList;
+import lotto.domain.RankingResult;
+
 import java.util.List;
 
 public class WinningNumbers {
-    private final LottoNumber lottoNumber;
+    private static final String ERROR_ALREADY_EXIST = "[ERROR]이미 존재하는 수 입니다.";
+    private final Lotto lotto;
     private final LottoTickets lottoTickets;
-    public static List<Integer> Resultlotto;
+    private final int bonusNumber;
+    public static List<List<Integer>> resultlotto;
+    public static List<Integer> userLottoNumbers;
 
-    public WinningNumbers(LottoNumber lottoNumber, LottoTickets lottoTickets) {
-        this.lottoNumber = lottoNumber;
-        this.lottoTickets = lottoTickets;
-    }
-
-    public void abc(){
-        Resultlotto = new ArrayList<>();
-
-    }
-
-    public int countCorrectNumbers(List<LottoNumbers> ComputerLottoNumbers, List<Integer> userLottoNumbers){
-        int count = 0;
-        for(int i=0; i<ComputerLottoNumbers.size(); i++){
-            if(userLottoNumbers.contains(ComputerLottoNumbers.get(i))){
-                count ++;
-            }
+    public WinningNumbers(Lotto lotto, LottoTickets lottoTickets, int bonusNumber) {
+        try {
+            validate(lotto,bonusNumber);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
         }
-        return count;
+        this.bonusNumber = bonusNumber;
+        this.lotto = lotto;
+        this.lottoTickets = lottoTickets;
+
+    }
+
+    public RankingResult createRanking() {
+        resultlotto = lottoTickets.getLottoTicket();
+        userLottoNumbers = lotto.getLottoNumbers();
+        RankingResult rankingResult = new RankingResult();
+        boolean bonus;
+        for (int i = 0; i < resultlotto.size(); i++) {
+            int count = 0;
+            bonus = containBonusNumber(resultlotto.get(i), bonusNumber);
+            for (int j = 0; j < userLottoNumbers.size(); j++) {
+                if (resultlotto.get(i).contains(userLottoNumbers.get(j))) {
+                    count++;
+                }
+            }
+            rankingResult.update(Ranking.getRanking(bonus, count));
+        }
+        return rankingResult;
+    }
+
+    public void validate(Lotto lotto,int bonusNumber) {
+        if (lotto.getLottoNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException(ERROR_ALREADY_EXIST);
+        }
+    }
+
+    public boolean containBonusNumber(List<Integer> resultlotto, int bonusNumber) {
+        if (!resultlotto.contains(bonusNumber)) {
+            return false;
+        }
+        return true;
     }
 }
