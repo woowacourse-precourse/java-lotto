70cea30d340dd2dea551be02d53fad1db1309f79
starwook <star.wookk@gmail.com>
2022-11-15 18:45:53 +0900

    feat(Reward.java): Add checkReward method
    맞춘 갯수에 따라 Reward Enum의 Number와 매치에 그에 맞는 reward를 반환하라

--- src/main/java/lotto/domain/JudgeMent.java
+++ src/main/java/lotto/domain/JudgeMent.java
@@ -1,15 +1,19 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class JudgeMent {
     private static final int initNumber = 0;
     private static final int correctNumberStandardWhenBonus = 5;
+    private static final int minCorrectNumber =3;
+    private Map<Reward,Integer> correctResult= new HashMap<>();
 
-    public int compareNumbers(List<Integer> numbers, List<Integer> lottoNumbers) {
+    public int compareNumbers(List<Integer> winningNumbers, List<Integer> lottoNumbers) {
         int countCorrect = initNumber;
         for (int i = 0; i < Resource.numberSize; i++) {
-            int number = numbers.get(i);
+            int number = winningNumbers.get(i);
             if (lottoNumbers.contains(i)) {
                 countCorrect++;
             }
@@ -18,9 +22,26 @@ public class JudgeMent {
     }
 
     public boolean bonusCheck(List<Integer> numbers, int correctCount, int bonusNumber) {
-        if (correctCount == 5 && numbers.contains(bonusNumber)){
+        if (correctCount == correctNumberStandardWhenBonus && numbers.contains(bonusNumber)){
             return true;
         }
         return false;
+    }
+
+
+    private Reward checkReward(List<Integer> numbers, WinningNumber winningNumber, int correctCount) {
+        if(minCorrectNumber == Reward.THREE.getNumber()){
+            return Reward.THREE;
+        }
+        if(minCorrectNumber==Reward.FOUR.getNumber()){
+            return Reward.FOUR;
+        }
+        if(minCorrectNumber==Reward.FIVE.getNumber()){
+            if (bonusCheck(numbers, correctCount, winningNumber.getBonusNumber())) {
+                return Reward.FIVE_BONUS;
+            }
+            return Reward.FIVE;
+        }
+        return Reward.SIX;
     }
 }
