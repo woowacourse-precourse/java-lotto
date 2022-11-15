e346997138a8c6f2d85affe68abbd60a83403b95
Jeeseob <jeeseob5761@gmail.com>
2022-11-15 17:47:25 +0900

    feat: 게임 실행부 및 에러 메세지 출력 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.view.LottoGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            LottoGame lottoGame = new LottoGame();
+            lottoGame.start();  // 프로그램 실행
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage()); // 예외 발생시, 에러 메세지 출력
+        }
     }
 }
