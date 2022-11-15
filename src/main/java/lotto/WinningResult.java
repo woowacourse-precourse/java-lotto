a1c94a0f94c2059a58fdd298ce77004715fec223
hyeonseog-won <smreo75@gmail.com>
2022-11-15 17:09:42 +0900

    feat: 당첨 등수와 개수 map에 저장하는 기능 추가

--- src/main/java/lotto/WinningResult.java
+++ src/main/java/lotto/WinningResult.java
@@ -1,10 +1,36 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class WinningResult {
-    public List<LottoRating> checkAllLotto(WinningLotto winningLotto, List<Lotto> lottos) {
+    private Map<LottoRating, Integer> totalResult;
+
+    public WinningResult(WinningLotto winningNumber, List<Lotto> lottos) {
+        this.totalResult = conversionLotto(winningNumber, lottos);
+    }
+
+    public Map<LottoRating, Integer> conversionLotto(WinningLotto winningNumber, List<Lotto> lottos) {
+        Map<LottoRating, Integer> totalResult = new EnumMap<>(LottoRating.class);
+        totalResult.put(LottoRating.FIFTH, 0);
+        totalResult.put(LottoRating.FOURTH, 0);
+        totalResult.put(LottoRating.THIRD, 0);
+        totalResult.put(LottoRating.SECOND, 0);
+        totalResult.put(LottoRating.FIRST, 0);
+
+        List<LottoRating> lottoResult = checkAllLotto(winningNumber, lottos);
+        for (LottoRating lottoGrade : lottoResult) {
+            if (lottoGrade != LottoRating.NOTHING) {
+                totalResult.put(lottoGrade, totalResult.get(lottoGrade) + 1);
+            }
+        }
+
+        return totalResult;
+    }
+
+    private List<LottoRating> checkAllLotto(WinningLotto winningLotto, List<Lotto> lottos) {
         List<LottoRating> winningResult = new ArrayList<>();
         for (Lotto lotto : lottos) {
             LottoRating lottoRating = check(winningLotto, lotto);
@@ -16,7 +42,7 @@ public class WinningResult {
         return winningResult;
     }
 
-    public LottoRating check(WinningLotto winningLotto, Lotto lotto) {
+    private LottoRating check(WinningLotto winningLotto, Lotto lotto) {
         boolean bonusNumber = lotto.getNumbers().contains(winningLotto.getBonusNumber());
         int prizeOfMatch = 0;
 
@@ -27,6 +53,4 @@ public class WinningResult {
         }
         return LottoRating.find(prizeOfMatch, bonusNumber);
     }
-
-
 }
