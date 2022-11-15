857f1299fc91c222c65460db3ea6f5c1e1621deb
YooByeongIk <quddlr96@naver.com>
2022-11-15 20:23:46 +0900

    test(calculator): add test code for validating createHistory method

--- src/test/java/lotto/CalculatorTest.java
+++ src/test/java/lotto/CalculatorTest.java
@@ -30,4 +30,20 @@ class CalculatorTest {
         lottoList.add(new Lotto(List.of(1, 2, 3, 10, 11, 12)));
         calculator = new Calculator(winningLotto, lottoList, bonusNumber);
     }
+
+
+    @DisplayName("당첨 기록 생성")
+    @Test
+    public void createHistoryTest() {
+        HashMap<Rank, Integer> history = calculator.getHistory();
+
+        //history의 size 검사
+        Assertions.assertThat(history.size()).isEqualTo(5);
+
+        //결과값 검증
+        Set<Rank> keys = history.keySet();
+        for (Rank rank : keys) {
+            Assertions.assertThat(history.get(rank)).isEqualTo(1);
+        }
+    }
 }
