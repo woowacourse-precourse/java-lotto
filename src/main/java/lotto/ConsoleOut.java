d746cdc12d06cf7173623f3b02d9babe0c263476
Park Sungchul <try615@naver.com>
2022-11-15 21:02:07 +0900

    refactor(ConsoleOut.java): 개행 출력 메소드 개선
    빈 줄을 출력하여 콘솔에 개행을 해주는 기능을 메소드로 분리

--- src/main/java/lotto/ConsoleOut.java
+++ src/main/java/lotto/ConsoleOut.java
@@ -4,15 +4,23 @@ import java.util.List;
 
 public class ConsoleOut {
 
+	public static void printEmptyLine() {
+		System.out.println();
+	}
+
 	public static void printInputMoney() {
 		System.out.println(Constants.PRINT_INPUT_MONEY);
 	}
 
 	public static void printLottos(List<Lotto> lottos) {
-		System.out.println();
+		printEmptyLine();
 		System.out.println(lottos.size() + Constants.PRINT_LOTTO_HISTORY);
 		for (Lotto lotto : lottos) {
 			System.out.println(lotto);
 		}
+	}
+
+	public static void printInputGoal() {
+		printEmptyLine();
 	}
 }
