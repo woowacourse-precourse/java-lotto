3a301a942be64d8f7a8f0af362acf559363c7d33
starwook <star.wookk@gmail.com>
2022-11-15 21:47:27 +0900

    refactor(Game.java): Refactor gameStart Method
    gameStart() 메소드에 구매금액이 필요하기에 Console.readLine()을 gameStart() 메소드로 옮겨서
    구매금액을 받는다

--- src/main/java/lotto/domain/Game.java
+++ src/main/java/lotto/domain/Game.java
@@ -3,33 +3,48 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Game {
 
     private static final String notThousand = " 구입금액은 천원 단위여야합니다.";
     private static final String notNumber = " 구입금액은 숫자여야합니다.";
     private static final int unit = 1000;
-
     public void gameStart() {
         System.out.println(Resource.greetings);
-        List<Lotto> lottoNumbers = showBoughtNumbers();
+        String purchase = Console.readLine();
+        List<Lotto> lottoNumbers = showBoughtNumbers(purchase);
         WinningNumber winningNumber = makeWinningNumber();
-        showResult(lottoNumbers, winningNumber);
+        getResult(lottoNumbers, winningNumber);
         System.out.println(Resource.revenuePercentFront);
         System.out.println(Resource.revenuePercentBack);
     }
 
-    private void showResult(List<Lotto> lottoNumbers, WinningNumber winningNumber) {
+    private void getResult(List<Lotto> lottoNumbers, WinningNumber winningNumber) {
         JudgeMent judgeMent = new JudgeMent();
         judgeMent.setCorrectResult(lottoNumbers, winningNumber);
-        System.out.println(judgeMent.getCorrectResult());
         System.out.println(Resource.resultOfWinning);
-        System.out.println(Resource.countOfCorrectNumberFront);
-        System.out.println(Resource.countOfCorrectNumberMiddle);
-        System.out.println(Resource.countOfCorrectNumberBack);
+        Map<Reward,Integer> result = judgeMent.getCorrectResult();
+        Iterator<Reward> keys = result.keySet().iterator();
+        while(keys.hasNext()){
+            Reward key = keys.next();
+            showResult(result,key);
+        }
     }
+    private void showResult(Map<Reward, Integer> result, Reward key) {
+        if(key.getNumber()==0){
+            return;
+        }
+        System.out.print(key.getNumber()+Resource.countOfCorrectNumberFront);
+        if(key.getBIll() == Reward.FIVE_BONUS.getBIll()){
+            System.out.print(Resource.bonusCorrect);
+        }
+        System.out.print(Resource.afterBonus);
+        System.out.print(key.getBIll()+Resource.countOfCorrectNumberMiddle);
+        System.out.println(result.get(key)+Resource.countOfCorrectNumberBack);
+    }
+
+//    public void getRevenuePercent()
 
     private WinningNumber makeWinningNumber() {
         System.out.println(Resource.inputOfWinningNumber);
@@ -39,8 +54,8 @@ public class Game {
         return new WinningNumber(winNumber, Integer.parseInt(bonusNumber));
     }
 
-    private List<Lotto> showBoughtNumbers() {
-        String purchase = Console.readLine();
+    private List<Lotto> showBoughtNumbers(String purchase) {
+
         int lottoUnit = checkIfDivideWithThousand(purchase);
         System.out.println(lottoUnit + Resource.countOfBuying);
         List<Lotto> lottoNumbers = new ArrayList<>();
