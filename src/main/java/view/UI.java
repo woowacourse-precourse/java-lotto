2aeef036e82c640ce77d198852ae86d6e8353896
chordpli <chordplaylist@gmail.com>
2022-11-15 22:18:59 +0900

    refactor: UI 분리
    1. 입/출력 메서드 분리

---
+++ src/main/java/view/UI.java
@@ -0,0 +1,115 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.Lotto;
+import lotto.Buyer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static Exception.Error.ERROR_AMOUNT_IS_NOT_RIGHT;
+import static Exception.Error.ERROR_NOT_NUMBER;
+import static lotto.Winning.*;
+
+public class UI {
+
+    // 금액 입력
+    public int inputCoin() {
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (IllegalStateException | NumberFormatException e ) {
+            System.out.println(e.getMessage());
+            System.out.println(ERROR_NOT_NUMBER.getMessage());
+            throw new NoSuchElementException();
+        }
+    }
+
+    // 발행 로또 출력
+    public int lottoCount(int purchaseAmount) {
+        int lottoCount = 0;
+        // 1,000원 단위로 나눈다.
+        if (purchaseAmount % 1000 == 0) {
+            return purchaseAmount / 1000;
+        }
+
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException(ERROR_AMOUNT_IS_NOT_RIGHT.getMessage());
+        }
+        return lottoCount;
+    }
+
+    // 로또 번호 출력
+    public void printLottoInfo(int lottoCount, List<Buyer> userLottoList) {
+        System.out.println(lottoCount + "개를 구매했습니다.");
+        // 갯수만큼 로또 초기화
+        inputUserLottoNumber(lottoCount, userLottoList);
+        // 로또 번호 출력
+        printLottoNumbers(lottoCount, userLottoList);
+    }
+
+    public void printLottoNumbers(int lottoCount, List<Buyer> userLottos) {
+        for (int i = 0; i < lottoCount; i++) {
+            System.out.println(userLottos.get(i).getUserNumber());
+        }
+    }
+
+    public void inputUserLottoNumber(int lottoCount, List<Buyer> userLottoList) {
+        for (int i = 0; i < lottoCount; i++) {
+            userLottoList.add(new Buyer());
+        }
+    }
+
+    public List<Integer> getLottoNums() {
+        String[] lottoNumString = Console.readLine().split(",");
+        return convertInteger(lottoNumString);
+    }
+
+    public int getBonusNumber() {
+        int bonusNumber = Integer.parseInt(Console.readLine());
+        return bonusNumber;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        List<Integer> lottoNums = getLottoNums();
+        Lotto lottoNumber = new Lotto(lottoNums);
+        return lottoNums;
+    }
+
+    public List<Integer> convertInteger(String[] lottoNumString) {
+        List<Integer> lottoNums = new ArrayList<>();
+        for (String s : lottoNumString) {
+            lottoNums.add(Integer.parseInt(s));
+        }
+        lottoNums.sort(Integer::compareTo);
+        return lottoNums;
+    }
+
+    public void printWinningStats(int money) {
+        System.out.println("당첨 통계\n---");
+        printMatch();
+        printYield(money);
+    }
+
+    public void printMatch() {
+        System.out.println(FifthPlace.getDESCRIPTION() + " " + FifthPlace.getSRING_PRICE() + " - " + FifthPlace.getCount() + "개");
+        System.out.println(FourthPlace.getDESCRIPTION() + " " + FourthPlace.getSRING_PRICE() + " - " + FourthPlace.getCount() + "개");
+        System.out.println(ThirdPlace.getDESCRIPTION() + " " + ThirdPlace.getSRING_PRICE() + " - " + ThirdPlace.getCount() + "개");
+        System.out.println(SecondPlace.getDESCRIPTION() + " " + SecondPlace.getSRING_PRICE() + " - " + SecondPlace.getCount() + "개");
+        System.out.println(FirstPlace.getDESCRIPTION() + " " + FirstPlace.getSRING_PRICE() + " - " + FirstPlace.getCount() + "개");
+    }
+
+
+    public void printYield(int money) {
+        int earnings = getPrizeMoney();
+        System.out.printf("총 수익률은 %.1f%%입니다.", (double) earnings / money * 100);
+    }
+
+    public int getPrizeMoney() {
+        return (FifthPlace.getCount() * FifthPlace.getPRICE())
+                + (FourthPlace.getCount() * FourthPlace.getPRICE())
+                + (ThirdPlace.getCount() * ThirdPlace.getPRICE())
+                + (SecondPlace.getCount() * SecondPlace.getPRICE())
+                + (FirstPlace.getCount() * FirstPlace.getPRICE());
+    }
+}
