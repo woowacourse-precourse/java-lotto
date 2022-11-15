fdc5e8491485eb744c509c1f0b209c8a19cb8aa0
BoyCho <iijokv@naver.com>
2022-11-15 15:40:44 +0900

    feat : 로또 번호 생성 기능 추가  - 중복 없는 1 ~ 45 사이 숫자 6개를 담는 Integer List 생성  - 오름차순 정렬 및 반환

---
+++ src/main/java/lotto/LottoCreator.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import java.util.Collections;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import constant.Message;
+
+public class LottoCreator {
+
+    public List<Integer> createNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(Message.SMALLESTNUMBER, Message.LARGESTNUMBER, Message.LOTTOLENGTH);
+        /*
+            pickUniqueNumbersInRange(int a, int b, int c) : 한 리스트에 a ~ b 까지 숫자를 삽입한 후,
+            그 리스트를 c 크기로 자른 후 Collections.Shuffle() 함수를 사용하여 무작위 구성의 형태로 반환하므로, 중복 없는 무작위 배열이 반환됨.
+        */
+        Collections.sort(numbers);
+        return numbers;
+    }
+}
