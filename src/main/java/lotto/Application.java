ef32996f0a17c10d5052ecf653a3e2f2b66568e9
bomin1996 <bomin199628@gmail.com>
2022-11-15 14:21:24 +0900

    <feat> : 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,113 +7,192 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    private static final int LOTTO_NUM_SIZE = 6;
+
+    private static int buyLottoCount() {
         System.out.println("구입금액을 입력해 주세요.");
-        Integer numOfLotto = isUserPaymentValid(userPayment());
-        System.out.println(numOfLotto+"개를 구매했습니다.");
-        List<List<Integer>> userLottoTotal = userLottoNumber(numOfLotto);
+        String input = Console.readLine();
+        if(!checkNumber(input)) {
+            System.out.println("[ERROR]");
+            throw new NoSuchElementException();
+        }
+        int money = Integer.parseInt(input);
+        if(money%1000 != 0) {
+            System.out.println("[ERROR]");
+            throw new IllegalArgumentException();
+        }
+        int lottoCount = money/1000;
+        return lottoCount;
+    }
+    private static boolean checkNumber(String input){
+        char check;
+        if(input.equals("")) {
+            return false;
+        }
+        for(int i = 0; i < input.length(); i++){
+            check = input.charAt(i);
+            if(check < 48 || check > 58) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private static List<Lotto> myLotto(int lottoCount) {
+        List<Lotto> myLotto = new ArrayList<>();
+        for(int i = 0; i < lottoCount; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, LOTTO_NUM_SIZE);
+//            Collections.sort(numbers);
+            sortLotto(numbers);
+            Lotto lotto = new Lotto(numbers);
+            myLotto.add(lotto);
+        }
+        return myLotto;
+    }
+    private static List<Integer> sortLotto(List<Integer> numbers) {
+        for(int i = 0; i < numbers.size() - 1; i++) {
+            for(int j = i + 1; j < numbers.size(); j++) {
+                numbers = changeNumbers(numbers, i ,j);
+            }
+        }
+        return numbers;
+    }
+    private static List<Integer> changeNumbers(List<Integer> numbers, int i, int j) {
+        if(numbers.get(i) > numbers.get(j)) {
+            int temp = numbers.get(i);
+            numbers.set(i, numbers.get(j));
+            numbers.set(j, temp);
+        }
+        return numbers;
+    }
+
+    private static void printMyLotto(List<Lotto> myLotto) {
+        System.out.println(myLotto.size() + "개를 구매했습니다.");
+        for(int i = 0; i < myLotto.size(); i++) {
+            System.out.println(myLotto.get(i).getNumbers());
+        }
+    }
+    private static Lotto winningLotto() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        List<Integer> prize = prizeNumber(Console.readLine());
+        String numbers = Console.readLine();
+        String[] tempWinningLotto = numbers.split(",");
+        winningNumbersException(tempWinningLotto);
+        List<Integer> winningLotto = new ArrayList<>();
+        for(int i = 0; i < LOTTO_NUM_SIZE; i++) {
+            winningLotto.add(Integer.parseInt(tempWinningLotto[i]));
+        }
+        Lotto lotto = new Lotto(winningLotto);
+        return lotto;
+    }
+    private static void winningNumbersException(String[] tempWinningLottoNumbers) {
+        if(tempWinningLottoNumbers.length != LOTTO_NUM_SIZE) {
+            System.out.println("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException();
+        }
+        for(int i = 0; i < LOTTO_NUM_SIZE; i++) {
+            if(Integer.parseInt(tempWinningLottoNumbers[i]) < 1 || 45 < Integer.parseInt(tempWinningLottoNumbers[i])) {
+                numberInputException();
+            }
+        }
+    }
+    private static int bonusLottoNumber(Lotto winningLotto) {
         System.out.println("보너스 번호를 입력해 주세요.");
-        Integer bonus = bonusNumber(Console.readLine());
+        int bonusNumber = Integer.parseInt(Console.readLine());
+        if(bonusNumber < 1 || 45 < bonusNumber) {
+            numberInputException();
+        }
+        if(winningLotto.getNumbers().contains(bonusNumber)) {
+            System.out.println("[ERROR]");
+            throw new IllegalArgumentException();
+        }
+        return bonusNumber;
+    }
+    private static void numberInputException() {
+        System.out.println("[ERROR]");
+        throw new IllegalArgumentException();
+    }
+    private static List<Double> myWinningCount(List<Lotto> myLottoNumbers, Lotto winningLottoNumbers, int bonusNumber) {
+        List<Double> winningCount = new ArrayList<>();
+        for(int i = 0; i < myLottoNumbers.size(); i++) {
+            double count = oneOfMyWinningCount(myLottoNumbers.get(i), winningLottoNumbers);
+            if(count == 5 && myLottoNumbers.get(i).getNumbers().contains(bonusNumber)) {
+                count += 0.5;
+            }
+            winningCount.add(count);
+        }
+        return winningCount;
+    }
+    private static double oneOfMyWinningCount(Lotto oneOfMyLottoNumbers, Lotto winningLottoNumbers) {
+        double count = 0.0;
+        for(int i = 0; i < LOTTO_NUM_SIZE; i++) {
+            if(oneOfMyLottoNumbers.getNumbers().contains(winningLottoNumbers.getNumbers().get(i))) {
+                count++;
+            }
+        }
+        return count;
+    }
+    private static List<String> printWinningMent() {
+        List<String> printWinningCount = new ArrayList<>();
+        printWinningCount.add("3개 일치 (5,000원) - ");
+        printWinningCount.add("4개 일치 (50,000원) - ");
+        printWinningCount.add("5개 일치 (1,500,000원) - ");
+        printWinningCount.add("5개 일치, 보너스 볼 일치 (30,000,000원) - ");
+        printWinningCount.add("6개 일치 (2,000,000,000원) - ");
+        return printWinningCount;
+    }
+    private static List<Double> winningCountNumber() {
+        List<Double> winningCountNumbers = new ArrayList<>();
+        winningCountNumbers.add(3.0);
+        winningCountNumbers.add(4.0);
+        winningCountNumbers.add(5.0);
+        winningCountNumbers.add(5.5);
+        winningCountNumbers.add(6.0);
+        return winningCountNumbers;
+    }
+    private static List<Integer> winningLottoMoney() {
+        List<Integer> winningMoney = new ArrayList<>();
+        winningMoney.add(5000);
+        winningMoney.add(50000);
+        winningMoney.add(1500000);
+        winningMoney.add(30000000);
+        winningMoney.add(2000000000);
+        return winningMoney;
+    }
+    private static List<Integer> printWinningResult(List<Double> winningCount) {
+        List<Integer> winningResult = new ArrayList<>();
+        List<String> printWinningCount = printWinningMent();
+        List<Double> winningCountNumbers = winningCountNumber();
+        for(int i = 0; i < printWinningCount.size(); i++) {
+            winningResult.add(Collections.frequency(winningCount, winningCountNumbers.get(i)));
+            System.out.println(printWinningCount.get(i) + winningResult.get(i) + "개");
+        }
+        return winningResult;
+    }
+    private static String moneyResultPercent(int lottoCount, List<Integer> winningResult) {
+        List<Integer> winningMoney = winningLottoMoney();
+        int myMoneyResult = 0;
+        for(int i = 0; i < winningResult.size(); i++) {
+            myMoneyResult += winningMoney.get(i)*winningResult.get(i);
+        }
+        String myMoneyResultPercent = String.format("%.1f", (double)myMoneyResult/(lottoCount*1000)*100);
+        return myMoneyResultPercent;
+    }
+    private static void lottoProgram() {
+        int myLottoCount = buyLottoCount();
+        List<Lotto> myLotto = myLotto(myLottoCount);
+        printMyLotto(myLotto);
+        Lotto winningLotto = winningLotto();
+        int bonusLottoNumber = bonusLottoNumber(winningLotto);
         System.out.println("당첨 통계");
         System.out.println("---");
-        Map<Collect, Integer> result = lotteryResult(userLottoTotal, prize, bonus);
-        lottoResultOutput(result);
-        lottoProfit(numOfLotto, result);
-    }
-    public static String userPayment () {
-        return Console.readLine();
-    }
-    public static Integer isUserPaymentValid (String userPayment) {
-        try {Integer.parseInt(userPayment);
-        }catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR]");
-        }
-        Integer numberOfLotto = Integer.parseInt(userPayment);
-        try {
-            if (numberOfLotto%1000 != 0) {
-                throw new IllegalArgumentException();
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR]");
-        }
-        return numberOfLotto/1000;
-    }
-    public static List<List<Integer>> userLottoNumber (Integer numOfLotto) {
-        List<List<Integer>> userLotto = new ArrayList<>();
-        for ( int i = 0 ; i < numOfLotto ; i++) {
-            List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            Lotto lottoNumberResult = new Lotto(lottoNumber);
-            List<Integer> lottoNum = new ArrayList<>(lottoNumberResult.getNumbers());
-            Collections.sort(lottoNum);
-            System.out.println(lottoNum);
-            userLotto.add(lottoNumberResult.getNumbers());
-        }
-        return userLotto;
-    }
-    public static List<Integer> prizeNumber (String prizeInput) {
-        List<String> list = Arrays.asList(prizeInput.split(","));
-        List<Integer> prizeNumberResult = list.stream()
-                .map(s -> Integer.parseInt(s))
-                .collect(Collectors.toList());
-
-        return prizeNumberResult;
-    }
-    public static Integer bonusNumber (String bonusInput) {
-        return Integer.parseInt(bonusInput);
-    }
-    public static Map<Collect, Integer> lotteryResult (List<List<Integer>> userLotto, List<Integer> prize, Integer bonus) {
-        Map<Collect, Integer> result = new HashMap<>();
-        result.put(Collect.FIRST, 0);
-        result.put(Collect.SECOND, 0);
-        result.put(Collect.THIRD, 0);
-        result.put(Collect.FOURTH, 0);
-        result.put(Collect.FIFTH, 0);
-        for (List<Integer> lotto : userLotto) {
-            Collect resultCollect = Collect.find(lotteryComparison(lotto, prize, bonus));
-            if (resultCollect != null) {
-                result.put(resultCollect, result.get(resultCollect)+1);
-            }
-        }
-
-        return result;
+        List<Double> myWinningCount = myWinningCount(myLotto, winningLotto, bonusLottoNumber);
+        List<Integer> winningResult = printWinningResult(myWinningCount);
+        String myMoneyResultPercent = moneyResultPercent(myLottoCount, winningResult);
+        System.out.println("총 수익률은 " + myMoneyResultPercent + "%입니다.");
     }
 
-    public static Float lotteryComparison(List<Integer> userLotto, List<Integer> prize, Integer bonus) {
-        Integer sameLottoCount = 0;
-        for ( int i = 0 ; i < userLotto.size() ; i++) {
-            if (prize.contains(userLotto.get(i))) {
-                sameLottoCount++;
-            }
-        }
-        if (sameLottoCount.equals(5)) {
-            Float result = sameLottoCount + isBonus(userLotto, bonus);
-            return result;
-        }
-        return sameLottoCount.floatValue();
-    }
-
-    public static Float isBonus(List<Integer> userLotto, Integer bonus) {
-        if (userLotto.contains(bonus)) {
-            return 0.5f;
-        }
-        return 0.0f;
-    }
-
-    public static void lottoResultOutput(Map<Collect, Integer> lotteryResult) {
-        for (Collect rk : Collect.values()) {
-            System.out.println(rk.getDescription() + " - " + lotteryResult.get(rk)+"개");
-        }
-    }
-    public static void lottoProfit(Integer numOfLotto, Map<Collect, Integer> lotteryResult) {
-        Integer totalPrize = 0;
-
-        for (Collect rk : Collect.values()) {
-            totalPrize += lotteryResult.get(rk)*rk.getPrize();
-        }
-        Float result = (totalPrize.floatValue() / (numOfLotto*1000))*100;
-        System.out.println("총 수익률은 " + String.format("%.1f", result) + "%입니다.");
+    public static void main(String[] args) {
+        // TODO: 프로그램 구현
+        lottoProgram();
     }
 }
