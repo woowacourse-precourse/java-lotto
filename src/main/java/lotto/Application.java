e97c60a06c6f55140a9aa7a379c4436a31ad94e4
pyj <les85611@hanyang.ac.kr>
2022-11-15 23:29:20 +0900

    입력 받을 금액에 대해 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -12,6 +12,19 @@ public class Application {
     private static List<Integer> userNums = new ArrayList<>();
     private static int buyLot;
 
+    // 구입 금액
+    public static void buy() {
+        System.out.println("구입 금액: ");
+        String input_pay = Console.readLine();
+
+        try {
+            buyLot = Integer.parseInt(input_pay);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[Error] 숫자만 입력!");
+        }
+    }
+
+
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
