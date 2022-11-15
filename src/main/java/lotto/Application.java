7ca6ad69f9d52e2f4edf79d80f70fbf1979b3e12
Grooomit <tmd5972@gmail.com>
2022-11-15 23:05:10 +0900

    [Feat] 로또 미션 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,31 +6,27 @@ public class Application {
     public static void main(String[] args) {
 
         Service service = new Service();
+        Output output = new Output();
 
-        // TODO: 프로그램 구현
-        System.out.println(Situation.INPUT_MONEY.message);
-        int purchaseAmount = service.getPurchaseAmount();
+        try {
+            System.out.println(Message.INPUT_MONEY.getMessage());
+            int purchaseAmount = service.getPurchaseAmount();
 
-        System.out.println(purchaseAmount + Situation.PURCHASE.message);
-        List<List<Integer>> lottoNumbers = service.LottoList(purchaseAmount);
+            System.out.println(purchaseAmount + Message.PURCHASE.getMessage());
+            List<List<Integer>> lottoNumbers = service.LottoList(purchaseAmount);
 
-        System.out.println(Situation.WIN_NUMBER.message);
-        List<Integer> winningNumber = service.getWinningNumber();
+            System.out.println(Message.WIN_NUMBER.getMessage());
+            List<Integer> winningNumber = service.getWinningNumber();
 
+            System.out.println(Message.BONUS_INPUT.getMessage());
+            int bonusNumber = service.getBonusInput();
 
-    }
-
-    public enum Situation{
-        INPUT_MONEY("구입금액을 입력해 주세요."),
-        PURCHASE("개를 구입했습니다."),
-        WIN_NUMBER("당첨 번호를 입력해 주세요."),
-        BONUS_INPUT("보너스 번호를 입력해 주세요."),
-        WIN_STATISTICS("당첨 통계");
-        private final String message;
-        Situation(String message) {
-            this.message = message;
+            Integer[] winStatistics = service.calculatorWinner(lottoNumbers, winningNumber, bonusNumber);
+            output.WinnerStatistics(winStatistics, purchaseAmount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
-    }
 
+    }
 
 }
