a30ae097e2d775a751dfa96f2fc2678a94f374f5
nahyeon cho <whskgus12@nate.com>
2022-11-15 23:25:52 +0900

    feat(Controller): 로또 게임 컨트롤러 생성
    로또 게임의 View와 Domain을 연결하는 컨트롤러입니다. startLottoGame을 통해 게임을 시작할 수 있습니다.

---
+++ src/main/java/lotto/Controller/LottoController.java
@@ -0,0 +1,61 @@
+package lotto.Controller;
+
+import lotto.Domain.BonusNumber;
+import lotto.Domain.LottoCompare;
+import lotto.Domain.LottoGenerator;
+import lotto.Domain.LottoWinnerNumber;
+import lotto.Lotto;
+import lotto.View.InputView;
+import lotto.View.OutputView;
+
+import java.util.List;
+
+public class LottoController {
+    List<List<Integer>> purchaseUserLotto;
+    List<Integer> winnerLotto;
+    String userMoney;
+
+    public void startLottoGame() {
+        userMoney = InputView.inputMoney();
+        purchaseLotto(userMoney);
+    }
+
+    public void purchaseLotto(String userMoney) {
+        LottoGenerator lottoGenerator = new LottoGenerator(userMoney);
+        purchaseUserLotto = lottoGenerator.issueLotto();
+        OutputView.printUserLotto(purchaseUserLotto);
+        inputWinnerLotto();
+    }
+
+    public void inputWinnerLotto() {
+        String winnerLottoNumber = InputView.inputWinnerLotto();
+        LottoWinnerNumber lottoWinnerNumber = new LottoWinnerNumber(winnerLottoNumber);
+        winnerLotto = lottoWinnerNumber.convertWinnerNumber(winnerLottoNumber);
+        new Lotto(winnerLotto);
+        inputBonusLotto();
+    }
+
+    public void inputBonusLotto() {
+        String bonus = InputView.inputBonusNumber();
+        BonusNumber bonusNumber = new BonusNumber(bonus);
+        bonusNumber.duplicateInputBonusNumber(winnerLotto);
+        compareWinnerNumber(bonus);
+    }
+
+    public void compareWinnerNumber(String bonusNumber) {
+        LottoCompare lottoCompare = new LottoCompare();
+        List<List<Integer>> compareResultLotto = lottoCompare.compareResultLotto(purchaseUserLotto, winnerLotto, bonusNumber);
+        lottoCompare.resultLottoCount(compareResultLotto);
+        getLottoWinningMessage();
+    }
+
+    public void getLottoWinningMessage() {
+        OutputView.printUserResultLottoMessage();
+        getUserProfit();
+    }
+
+    public void getUserProfit() {
+        double userProfit = LottoCompare.resultProfit(Integer.parseInt(userMoney));
+        OutputView.printUserProfit(userProfit);
+    }
+}
