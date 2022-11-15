63f3fd95acb95206e31a5c29b10d7485ed3d7654
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-15 20:56:58 +0900

    refactor(ALL) : 기능 실행을 위한 코드 정리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> winGames;
+        Game game = new Game();
+        try {
+            game.buy();
+            game.setWinningNumbers();
+            Lotto lotto = new Lotto(game.getWinningNumbers());
+            lotto.validateBonus(game.getBonus());
+            winGames = game.compareNumbers(lotto.returnNumbers());
+            game.printResult(winGames, game.getMoney());
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
