b6ca1e2e480ff1c0e6cdbb2dc94b543403bec60e
zeunala <zeunala@naver.com>
2022-11-15 12:36:25 +0900

    feat: 로또 한 세트의 등수 확인 기능 추가

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+public enum LottoRank {
+    RANK_5(3, false, 5000, "3개 일치 (5,000원)"),
+    RANK_4(4, false, 50000, "4개 일치 (50,000원)"),
+    RANK_3(5, false, 1500000, "5개 일치 (1,500,000원)"),
+    RANK_2(5, true, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    RANK_1(6, false, 2000000000, "6개 일치 (2,000,000,000원)");
+
+    public final int CORRECT_COUNT_CONDITION; // 해당 등수에서 필요한 당첨 번호 개수
+    public final boolean IS_BONUS_NEEDED; // 보너스 번호도 맞아야 하는지
+    public final long PRIZE_MONEY; // 해당 등수에서의 상금
+    public final String PRIZE_DESCRIPTION; // 해당 등수에 대한 서술
+
+    LottoRank(int correctCountCondition, boolean isBonusNeeded, long prizeMoney, String prizeDescription) {
+        this.CORRECT_COUNT_CONDITION = correctCountCondition;
+        this.IS_BONUS_NEEDED = isBonusNeeded;
+        this.PRIZE_MONEY = prizeMoney;
+        this.PRIZE_DESCRIPTION = prizeDescription;
+    }
+}
---
+++ src/main/java/lotto/utils/LottoCompare.java
@@ -0,0 +1,30 @@
+package lotto.utils;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoAnswer;
+import lotto.domain.LottoRank;
+
+public class LottoCompare {
+    /**
+     * 구입한 로또 하나와 당첨/보너스 번호를 비교해 등수 반환
+     *
+     * @param lotto       구입한 로또 하나
+     * @param lottoAnswer 당첨/보너스 번호
+     * @return 당첨 등수 반환 (없을 경우 null 반환)
+     */
+    public static LottoRank compareOneTicket(Lotto lotto, LottoAnswer lottoAnswer) {
+        int correctCount = lottoAnswer.getCorrectCount(lotto);
+        boolean containBonus = lottoAnswer.isContainBonus(lotto);
+
+        for (LottoRank lottoRank : LottoRank.values()) {
+            if (lottoRank.IS_BONUS_NEEDED && !containBonus) {
+                continue;
+            }
+            if (correctCount == lottoRank.CORRECT_COUNT_CONDITION) {
+                return lottoRank;
+            }
+        }
+
+        return null;
+    }
+}
