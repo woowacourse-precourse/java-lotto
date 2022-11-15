405c7f41ae884ebb4d7846a4c8aca16b7afc63df
ckyeon <qsky8520@gmail.com>
2022-11-15 21:42:49 +0900

    feat(Lotto): implement 잘못된 로또 번호가 있는지 확인하는 기능

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
+import java.util.regex.Pattern;
 import lotto.ERROR;
 
 public class Lotto {
@@ -28,6 +29,10 @@ public class Lotto {
         if (hasDuplicateNumber(numbers)) {
             throw new IllegalArgumentException(ERROR.HAS_DUPLICATE_NUMBER.getMessage());
         }
+
+        if (hasInvalidInput(numbers)) {
+            throw new IllegalArgumentException(ERROR.IS_INVALID_INPUT.getMessage());
+        }
     }
 
     private boolean isCorrectNumber(List<Integer> numbers) {
@@ -37,6 +42,15 @@ public class Lotto {
     private boolean hasDuplicateNumber(List<Integer> numbers) {
         HashSet<Integer> set = new HashSet<>(numbers);
         return set.size() < numbers.size();
+    }
+
+    private boolean hasInvalidInput(List<Integer> numbers) {
+        Pattern isInvalidInputPattern = Pattern.compile("[^1-45]");
+        return numbers.stream()
+            .anyMatch(number -> isInvalidInputPattern
+                .matcher(number.toString())
+                .find()
+            );
     }
 
     private void sortAsc(List<Integer> numbers) {
