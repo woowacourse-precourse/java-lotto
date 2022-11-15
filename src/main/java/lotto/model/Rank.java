3494fec835638300c41715d1c1a41c5ce745658b
LeeHaiLim <hailim1997@gmail.com>
2022-11-15 19:21:25 +0900

    feat(model): add static method find rank

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,12 +1,14 @@
 package lotto.model;
 
+import java.util.Arrays;
+
 public enum Rank {
-    FIFTH(3, false, 5_000),
-    FOURTH(4, false, 50_000),
-    THIRD(5, false, 1_500_000),
+    FIRST(6, false, 2_000_000_000),
     SECOND(5, true, 30_000_000),
-    FIRST(6, false, 2_000_000_000)
-    ;
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    EMPTY(0, false, 0);
     private final int common;
     private final boolean bonus;
     private final int prise;
@@ -15,6 +17,16 @@ public enum Rank {
         this.common = common;
         this.bonus = bonus;
         this.prise = prise;
+    }
+
+    public static Rank findRank(int common, boolean bonus) {
+        if (common == SECOND.common() && bonus) {
+            return SECOND;
+        }
+        return Arrays.stream(Rank.values())
+                .filter(rank -> !rank.bonus() && rank.common() == common)
+                .findAny()
+                .orElse(EMPTY);
     }
 
     public int common() {
