fc2c68761f2908469069080accc3ce94ddb2be64
jijongkwon <help3451@naver.com>
2022-11-15 22:58:52 +0900

    fix : 에러 메시지 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -14,20 +14,20 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 크기가 초과되었습니다.");
         }
     }
 
     private void checkDuplication(List<Integer> numbers){
         if(numbers.size() != numbers.stream().distinct().count()){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 중복된 수가 입력되었습니다.");
         }
     }
 
     private void checkOverRange(List<Integer> numbers){
         for(int numbersValue : numbers){
             if(numbersValue > 45 || numbersValue < 1){
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
         }
     }
