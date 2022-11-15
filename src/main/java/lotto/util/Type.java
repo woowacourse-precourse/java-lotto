ff52aea3a910d7086e72676a6fdd79ef7c42d3d6
mihye126 <mihea12@gmail.com>
2022-11-15 22:34:01 +0900

    [style] 정렬

--- src/main/java/lotto/util/Type.java
+++ src/main/java/lotto/util/Type.java
@@ -2,13 +2,13 @@ package lotto.util;
 
 public enum Type {
     LOTTO(1),
-    BONUS (2),
-    AMOUNT (3);
+    BONUS(2),
+    AMOUNT(3);
 
     private final int value;
 
     Type(int value) {
-        this.value=value;
+        this.value = value;
     }
 
     public int getValue() {
