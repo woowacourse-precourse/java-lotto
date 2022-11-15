8869545e08c26dc57da53266e7740fc44b66a0c0
WhalesBob <ndw8200@naver.com>
2022-11-15 19:02:30 +0900

    chore(WinningPrizeTest) : add comments, DisplayName for easy to look

--- src/test/java/lotto/controller/WinningPrizeTest.java
+++ src/test/java/lotto/controller/WinningPrizeTest.java
@@ -20,13 +20,14 @@ class WinningPrizeTest {
         Lotto lottoPapers;
         int bonusNumber;
 
-        @DisplayName("2등 테스팅을 위하여, 로또한장의 번호는 (1,2,3,4,5,6) 으로 고정한다.")
+        // 2등 테스팅을 위하여, 로또한장의 번호는 (1,2,3,4,5,6) 으로 고정한다.
         @BeforeEach
         void beforeSettingOfFindRanking(){
             lottoNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
             lottoPapers = new Lotto(lottoNumbers);
         }
 
+        @DisplayName("6개 당첨, bonus 번호 포함.(1등)")
         @Test
         void findRanking_case1_1(){
             int amountOfCorrectNumbers = 6;
@@ -35,6 +36,7 @@ class WinningPrizeTest {
             assertThat(WinningPrize.findRanking(amountOfCorrectNumbers,lottoPapers,bonusNumber)).isEqualTo(output);
         }
 
+        @DisplayName("6개 당첨, bonus 번호 미포함.(1등)")
         @Test
         void findRanking_case1_2(){
             int amountOfCorrectNumbers = 6;
@@ -43,6 +45,7 @@ class WinningPrizeTest {
             assertThat(WinningPrize.findRanking(amountOfCorrectNumbers,lottoPapers,bonusNumber)).isEqualTo(output);
         }
 
+        @DisplayName("5개 당첨, bonus 번호 포함.(2등)")
         @Test
         void findRanking_case2(){
             int amountOfCorrectNumbers = 5;
@@ -51,6 +54,7 @@ class WinningPrizeTest {
             assertThat(WinningPrize.findRanking(amountOfCorrectNumbers,lottoPapers,bonusNumber)).isEqualTo(output);
         }
 
+        @DisplayName("5개 당첨, bonus 번호 미포함.(3등)")
         @Test
         void findRanking_case3(){
             int amountOfCorrectNumbers = 5;
@@ -59,6 +63,7 @@ class WinningPrizeTest {
             assertThat(WinningPrize.findRanking(amountOfCorrectNumbers,lottoPapers,bonusNumber)).isEqualTo(output);
         }
 
+        @DisplayName("4개 당첨, bonus 번호 포함.(4등)")
         @Test
         void findRanking_case4_1(){
             int amountOfCorrectNumbers = 4;
@@ -66,6 +71,8 @@ class WinningPrizeTest {
             int output = 4;
             assertThat(WinningPrize.findRanking(amountOfCorrectNumbers,lottoPapers,bonusNumber)).isEqualTo(output);
         }
+
+        @DisplayName("4개 당첨, bonus 번호 미포함.(4등)")
         @Test
         void findRanking_case4_2(){
             int amountOfCorrectNumbers = 4;
@@ -74,6 +81,7 @@ class WinningPrizeTest {
             assertThat(WinningPrize.findRanking(amountOfCorrectNumbers,lottoPapers,bonusNumber)).isEqualTo(output);
         }
 
+        @DisplayName("3개 당첨, bonus 번호 포함.(5등)")
         @Test
         void findRanking_case5_1(){
             int amountOfCorrectNumbers = 3;
@@ -81,6 +89,8 @@ class WinningPrizeTest {
             int output = 5;
             assertThat(WinningPrize.findRanking(amountOfCorrectNumbers,lottoPapers,bonusNumber)).isEqualTo(output);
         }
+
+        @DisplayName("3개 당첨, bonus 번호 미포함.(5등)")
         @Test
         void findRanking_case5_2(){
             int amountOfCorrectNumbers = 3;
@@ -89,6 +99,7 @@ class WinningPrizeTest {
             assertThat(WinningPrize.findRanking(amountOfCorrectNumbers,lottoPapers,bonusNumber)).isEqualTo(output);
         }
 
+        @DisplayName("2개 당첨, bonus 번호 포함.(탈락)")
         @Test
         void findRanking_case6_1(){
             int amountOfCorrectNumbers = 2;
@@ -96,6 +107,8 @@ class WinningPrizeTest {
             int output = -1;
             assertThat(WinningPrize.findRanking(amountOfCorrectNumbers,lottoPapers,bonusNumber)).isEqualTo(output);
         }
+
+        @DisplayName("2개 당첨, bonus 번호 미포함.(탈락)")
         @Test
         void findRanking_case6_2(){
             int amountOfCorrectNumbers = 2;
@@ -104,6 +117,7 @@ class WinningPrizeTest {
             assertThat(WinningPrize.findRanking(amountOfCorrectNumbers,lottoPapers,bonusNumber)).isEqualTo(output);
         }
 
+        @DisplayName("1개 당첨, bonus 번호 포함.(탈락)")
         @Test
         void findRanking_case6_3(){
             int amountOfCorrectNumbers = 1;
@@ -111,6 +125,8 @@ class WinningPrizeTest {
             int output = -1;
             assertThat(WinningPrize.findRanking(amountOfCorrectNumbers,lottoPapers,bonusNumber)).isEqualTo(output);
         }
+
+        @DisplayName("1개 당첨, bonus 번호 미포함.(탈락)")
         @Test
         void findRanking_case6_4(){
             int amountOfCorrectNumbers = 1;
@@ -119,6 +135,7 @@ class WinningPrizeTest {
             assertThat(WinningPrize.findRanking(amountOfCorrectNumbers,lottoPapers,bonusNumber)).isEqualTo(output);
         }
 
+        @DisplayName("0개 당첨, bonus 번호 포함.(탈락)")
         @Test
         void findRanking_case6_5(){
             int amountOfCorrectNumbers = 0;
@@ -126,6 +143,8 @@ class WinningPrizeTest {
             int output = -1;
             assertThat(WinningPrize.findRanking(amountOfCorrectNumbers,lottoPapers,bonusNumber)).isEqualTo(output);
         }
+
+        @DisplayName("0개 당첨, bonus 번호 미포함.(탈락)")
         @Test
         void findRanking_case6_6(){
             int amountOfCorrectNumbers = 0;
@@ -138,6 +157,7 @@ class WinningPrizeTest {
     @Nested
     class FindEnumByRankTest {
 
+        @DisplayName("1등 랭킹 넣었을 때, FIRST WinningPrize Enum 나오기.")
         @Test
         void findEnumByRank_case1(){
             int input = 1;
@@ -145,6 +165,7 @@ class WinningPrizeTest {
             assertThat(WinningPrize.findEnumByRank(input)).isEqualTo(output);
         }
 
+        @DisplayName("2등 랭킹 넣었을 때, SECOND WinningPrize Enum 나오기.")
         @Test
         void findEnumByRank_case2(){
             int input = 2;
@@ -152,6 +173,7 @@ class WinningPrizeTest {
             assertThat(WinningPrize.findEnumByRank(input)).isEqualTo(output);
         }
 
+        @DisplayName("3등 랭킹 넣었을 때, THIRD WinningPrize Enum 나오기.")
         @Test
         void findEnumByRank_case3(){
             int input = 3;
@@ -159,6 +181,7 @@ class WinningPrizeTest {
             assertThat(WinningPrize.findEnumByRank(input)).isEqualTo(output);
         }
 
+        @DisplayName("4등 랭킹 넣었을 때, FOURTH WinningPrize Enum 나오기.")
         @Test
         void findEnumByRank_case4(){
             int input = 4;
@@ -166,6 +189,7 @@ class WinningPrizeTest {
             assertThat(WinningPrize.findEnumByRank(input)).isEqualTo(output);
         }
 
+        @DisplayName("5등 랭킹 넣었을 때, FIFTH WinningPrize Enum 나오기.")
         @Test
         void findEnumByRank_case5(){
             int input = 5;
