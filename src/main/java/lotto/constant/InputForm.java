16a4269cef165adc49f0c220d684efd0b0501a0d
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-15 13:12:24 +0900

    refactor(MatchingForm): rename to InputForm

--- src/main/java/lotto/constant/InputForm.java
+++ src/main/java/lotto/constant/InputForm.java
@@ -1,12 +1,12 @@
 package lotto.constant;
 
-public enum MatchingForm {
+public enum InputForm {
     POSITIVE_INTEGER_FORM("[1-9][0-9]*"),
     SIX_POSITIVE_INTEGER_DIVIDED_BY_COMMA("[1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*");
 
     private final String form;
 
-    MatchingForm(String form) {
+    InputForm(String form) {
         this.form = form;
     }
 
