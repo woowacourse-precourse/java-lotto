09a7ce3f733a95c4194584967f964cf42fbcb4b4
Frog-Slayer <Frog-Slayer@github.com>
2022-11-15 15:04:10 +0900

    refactor(View): 단순히 출력만 하는 메서드들은 출력할 텍스트를 인자로 갖는 메서드 하나로 병합

---
+++ src/main/java/lotto/Controller.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.UI.View;
+import lotto.domain.Game;
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
+import lotto.domain.User;
+
+public class Controller {
+    private Game game;
+    private User user;
+    private View view;
+
+    public Controller(Game game, User user, View view){
+        this.game = game;
+        this.user = user;
+        this.view = view;
+    }
+
+
+
+
+
+
+
+
+
+
+}
--- src/main/java/lotto/UI/View.java
+++ src/main/java/lotto/UI/View.java
@@ -1,24 +1,12 @@
 package lotto.UI;
 
 public class View {
-    public void printBudgetInput(){
-        System.out.println(Text.BUDGET_INPUT.getMessage());
+    public void printMessage(Text text){
+        System.out.println(text.getMessage());
     }
 
     public void printButLotto(Integer lottoCount){
         System.out.printf(Text.BUY_LOTTO.getMessage(), lottoCount);
-    }
-
-    public void printWinningNumberInput(){
-        System.out.println(Text.WINNING_NUMBER_INPUT.getMessage());
-    }
-
-    public void printBonusNumberInput(){
-        System.out.println(Text.BONUS_NUMBER_INPUT.getMessage());
-    }
-
-    public void printWinningStatisticsTitle(){
-        System.out.println(Text.WINNING_STATISTICS_TITLE.getMessage());
     }
 
     public void printWinningStatisticsDetail(Integer matchedNumberCount, String formattedAmount, Integer matchedLottoCount){
