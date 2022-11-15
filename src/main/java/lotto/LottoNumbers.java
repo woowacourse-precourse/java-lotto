e6a2defe1206562456c7057a94d213f443f4d485
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-15 15:34:20 +0900

    feat: implement exception for non number input

--- src/main/java/lotto/LottoNumbers.java
+++ src/main/java/lotto/LottoNumbers.java
@@ -6,26 +6,28 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import lotto.Lotto.*;
+
 public class LottoNumbers {
 
     public static List<Integer> getLotteryWinningNumbers() {
-
         System.out.println("당첨 번호를 입력해주세요.\n");
         String[] lottoNumberList = Console.readLine().split(",");
         List<String> LottoNumberListInString = Arrays.asList(lottoNumberList);
+        for (int numberIndex = 0; numberIndex < 6; numberIndex++) {
+            if (LottoNumberListInString.get(numberIndex).charAt(0) < 48 || LottoNumberListInString.get(numberIndex).charAt(0) > 57) {
+                throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.");
+            }
+        }
         List<Integer> LottoNumberInInteger = new ArrayList<>();
-
-        for(String stringIndex : LottoNumberListInString){
+        for (String stringIndex : LottoNumberListInString) {
             LottoNumberInInteger.add(Integer.parseInt(stringIndex));
         }
-
-        System.out.println(LottoNumberListInString);
-        System.out.println(LottoNumberInInteger);
         return LottoNumberInInteger;
     }
 
-    public static int getLotteryBonusNumber(){
+    public static int getLotteryBonusNumber() {
 
         System.out.println("보너스 번호를 입력해 주세요.");
         int bonusNumber = Integer.parseInt(Console.readLine());
