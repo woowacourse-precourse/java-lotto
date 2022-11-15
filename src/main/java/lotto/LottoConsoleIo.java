20a05d51d000eac66c0a2f0066fa1e5b8ee5df4d
고동우 <shp05179@gmail.com>
2022-11-15 23:37:30 +0900

    fix: sort를 위해 List를 ArrayList로 변경
    기존 : Collections.sort(List형태)
    변경 후 : Collections.sort(ArrayList형태)

--- src/main/java/lotto/LottoConsoleIo.java
+++ src/main/java/lotto/LottoConsoleIo.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,7 +14,7 @@ public class LottoConsoleIo {
     static void printNoOfLotto(List<Lotto> lottos) {
         System.out.println("\n"+lottos.size() + "개를 구매했습니다.");
         for (Lotto lotto : lottos) {
-            List<Integer> numbers = lotto.getNumbers();
+            List<Integer> numbers = new ArrayList<>(lotto.getNumbers());
             Collections.sort(numbers);
             System.out.println(numbers);
         }
