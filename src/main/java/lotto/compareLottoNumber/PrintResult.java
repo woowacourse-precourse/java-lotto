1bf81b7c2bd465ffc4f98ce381bd4db3bbb5b526
BHC-Chicken <tij05078@gmail.com>
2022-11-15 13:34:11 +0900

    refactor : 소수점 표기
    요구사항 충족을 위해 소수 두번째 자리에서 반올림하여 표기 하도록 변경했다.

--- src/main/java/lotto/compareLottoNumber/PrintResult.java
+++ src/main/java/lotto/compareLottoNumber/PrintResult.java
@@ -1,19 +1,22 @@
 package lotto.compareLottoNumber;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import lotto.constant.WinningAmount;
 
 public class PrintResult {
-    public long calcPrize(int[] result, int money) {
-        long total = 0L;
+    public String calcPrize(int[] result, int money) {
+        long total = 0;
 
         for (int index = 0; index < 5; index++) {
             total += (long) result[index] * WinningAmount.values()[index].getValue();
         }
-        return (total / (money * 1000L)) * 100;
+        return String.valueOf(Math.round((total / (money * 1000.0) * 100 * 10)) / 10.0);
     }
 
     public void printResult(int[] result, int money) {
-        long profit = calcPrize(result, money);
+        DecimalFormat decimalFormat = new DecimalFormat("###,###.0");
+        BigDecimal profit = new BigDecimal(calcPrize(result, money));
         String[] fewMatch = {"3개 일치 (", "4개 일치 (", "5개 일치 (", "5개 일치, 보너스 볼 일치 (", "6개 일치 ("};
 
         System.out.println("당첨 통계");
@@ -23,9 +26,7 @@ public class PrintResult {
             System.out.println(fewMatch[index] + WinningAmount.values()[index].getValueString()
                     + "원) - " + result[index] + "개");
         }
-
-        System.out.println("총 수익률은 " + profit + "%입니다.");
-
+        System.out.println("총 수익률은 " + decimalFormat.format(profit) + "%입니다.");
     }
 
 }
