63cbd7248394dc0f2c57fbc77e7a8e4ac6ad554b
oyoungsun <plusmate@naver.com>
2022-11-15 17:25:55 +0900

    feat : make InputUtil

--- src/main/java/lotto/util/InputUtil.java
+++ src/main/java/lotto/util/InputUtil.java
@@ -1,2 +1,15 @@
-package lotto.util;public class InputUtil {
+package lotto.util;
+
+import static lotto.constant.Constant.ERROR_INTEGER;
+import static lotto.constant.Constant.ERROR_MESSAGE;
+
+public class InputUtil {
+    public static int stringToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_MESSAGE + ERROR_INTEGER);
+        }
+        return -1;
+    }
 }
