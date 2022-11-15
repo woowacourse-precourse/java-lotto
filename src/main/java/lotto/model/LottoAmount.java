2d422112451c14a93a61b8b3e9f6b3d8a263a05e
qdtjj4 <76ylo1@gmail.com>
2022-11-15 17:08:10 +0900

    refactor: LottoQuantity -> LottoAmount 클래스로 변경
    로또 수량보다 구입 금액을 모델링하는게 적합해서 변경함

--- src/main/java/lotto/model/LottoAmount.java
+++ src/main/java/lotto/model/LottoAmount.java
@@ -1,27 +1,27 @@
 package lotto.model;
 
-public class LottoQuantity {
-    private final int quantity;
+public class LottoAmount {
+    private final int amount;
 
-    public LottoQuantity(int purchaseAmount) {
+    public LottoAmount(int purchaseAmount) {
         validateEnoughMoney(purchaseAmount);
         validateChangeIsZero(purchaseAmount);
-        this.quantity = quantityOfLotto(purchaseAmount);
+        this.amount = purchaseAmount;
     }
 
-    static void validateEnoughMoney(int purchaseAmount) {
+    public int quantityOfLotto(int purchaseAmount) {
+        return purchaseAmount / 1000;
+    }
+
+    private void validateEnoughMoney(int purchaseAmount) {
         if (purchaseAmount < 1000) {
             throw new IllegalArgumentException("[ERROR] 구입금액이 부족합니다.");
         }
     }
 
-    static void validateChangeIsZero(int purchaseAmount) {
+    private void validateChangeIsZero(int purchaseAmount) {
         if (purchaseAmount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 남는 금액이 존재합니다.");
         }
-    }
-
-    static int quantityOfLotto(int purchaseAmount) {
-        return purchaseAmount / 1000;
     }
 }
