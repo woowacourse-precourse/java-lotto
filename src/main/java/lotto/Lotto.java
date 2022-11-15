7020672aa5377573d0c570ccc91ccb56667a3e76
DavinAn <blueapple1030@gmail.com>
2022-11-15 23:41:29 +0900

    feat : handle exceptions in Lotto class

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,13 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        for (int i = 0; i < 6; i++) {
+            if ((numbers.get(i) < 1) || (numbers.get(i) > 45)) {
+                throw new IllegalArgumentException("[ERROR] 05 숫자가 범위가 올바르지 않습니다.");
+            } else if (numbers.subList(i + 1, 6).contains(numbers.get(i))) {
+                throw new IllegalArgumentException("[ERROR] 07 중복되는 숫자가 입력되었습니다.");
+            }
+        }
     }
 
 
