196833784f2f549639e66b47ca801f66efea21ff
bradbrad97 <taehyun0121@naver.com>
2022-11-15 18:54:14 +0900

    "feat(LottoResult): 로또 숫자와 당첨 숫자 비교 기능 추가"

---
+++ src/main/java/lotto/model/LottoResult.java
@@ -0,0 +1,48 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class LottoResult {
+    enum Rank{
+        FIRST(6, 2000000000),
+        SECOND(5, 30000000),
+        THIRD(5,1500000),
+        FOURTH(4,50000),
+        FIFTH(3,5000);
+
+        final private int matches;
+        final private int price;
+
+        private Rank(int matches, int price){
+            this.matches = matches;
+            this.price = price;
+        }
+
+        public int getMatches() {
+            return matches;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+    }
+
+    private List<Integer> scoreBoard = new ArrayList<>();
+    private int winnings;
+
+    private int compareLottoWinningNumber(List<Integer> lotto, Set<Integer> winningNumbers){
+        int matches = 0;
+
+        for (int number : lotto){
+            if (winningNumbers.contains(number)){
+                matches++;
+            }
+        }
+
+        return matches;
+    }
+
+
+}
