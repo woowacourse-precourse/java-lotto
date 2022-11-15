64b690b59bd55628fc6fd46ff131d99f38b997c1
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-15 11:13:54 +0900

    feat: 당첨 번호 입력받는 기능 최종 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,17 +2,26 @@ package lotto;
 
 import lotto.domain.LottoService;
 import lotto.model.BuyingLottoList;
+import lotto.model.WinningLotto;
 import lotto.view.LottoBuyView;
+import lotto.view.WinningNumberView;
+
+import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         LottoBuyView lottoServiceView = new LottoBuyView();
+        WinningNumberView winningNumberView = new WinningNumberView();
         LottoService lottoService = new LottoService();
 
         int numberOfLotto = lottoServiceView.buyLotto();
         BuyingLottoList buyingLottoList = new BuyingLottoList(lottoService.generateLottoList(numberOfLotto));
+        List<Integer> winningNumberList = winningNumberView.inputWinningNumber();
+        int bonusNumber = winningNumberView.inputBonusNumber();
+        WinningLotto winningLotto = new WinningLotto(winningNumberList, bonusNumber);
+
 
     }
 }
