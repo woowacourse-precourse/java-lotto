c17e2e21c72e75b6c9eb067ae5063a2f469017f0
p <gustjr2332@naver.com>
2022-11-15 20:20:22 +0900

    refactor(PriceException) : 하드코딩 수정

--- src/main/java/Exception/PriceException.java
+++ src/main/java/Exception/PriceException.java
@@ -5,12 +5,14 @@ import java.util.regex.Pattern;
 public class PriceException {
     private static final String NOTNUMBER_ERRORMESSAGE = "[ERROR] 숫자만 입력해주세요.";
     private static final String PRICEUNIT_ERRORMESSAGE = "[ERROR] 구입 금액은 1000원 단위로 입력해주세요.";
+    private static final String NUMERICAL_REGULAREXPRESSION = "^[0-9]*$";
+    private static final int UNITOFPURCHASEAMOUNT = 1000;
 
     public static void checkException(String price) {
-        if (!Pattern.matches("^[0-9]*$", price)) {
+        if (!Pattern.matches(NUMERICAL_REGULAREXPRESSION, price)) {
             throw new IllegalArgumentException(NOTNUMBER_ERRORMESSAGE);
         }
-        if (Integer.valueOf(price) % 1000 != 0) {
+        if (Integer.valueOf(price) % UNITOFPURCHASEAMOUNT != 0) {
             throw new IllegalArgumentException(PRICEUNIT_ERRORMESSAGE);
         }
     }
