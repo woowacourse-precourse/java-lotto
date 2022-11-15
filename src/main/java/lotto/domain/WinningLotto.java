0cbc3d250bc175b0cd1ec58ba31ef61634f3ba7a
hgo641 <hgo641@gmail.com>
2022-11-15 13:51:07 +0900

    refactor: isBonus 함수를 거치지않고 바로 보너스 여부를 구하게 변경

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -13,44 +13,37 @@ public class WinningLotto {
         this.bonusNumber = bonusNumber;
     }
 
-    public static WinningLotto of(Lotto winningLottoNumbers, String bonusNumber){
+    public static WinningLotto of(Lotto winningLottoNumbers, String bonusNumber) {
         int convertedBonusNumber = convertBonusNumber(bonusNumber);
         validateBonusNumberDuplicate(winningLottoNumbers, convertedBonusNumber);
         return new WinningLotto(winningLottoNumbers, convertedBonusNumber);
     }
 
-    private static int convertBonusNumber(String bonusNumber){
-        try{
+    private static int convertBonusNumber(String bonusNumber) {
+        try {
             Integer.parseInt(bonusNumber);
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("보너스 번호는 숫자만 입력할 수 있습니다.");
         }
         return Integer.parseInt(bonusNumber);
     }
 
-    public static void validateBonusNumberDuplicate(Lotto winningLottoNumbers, int bonusNumber){
-        if(winningLottoNumbers.isContain(bonusNumber)){
+    public static void validateBonusNumberDuplicate(Lotto winningLottoNumbers, int bonusNumber) {
+        if (winningLottoNumbers.isContain(bonusNumber)) {
             throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
         }
     }
 
-    public Map<Rank, Integer> getUserLottoScore(List<Lotto> userLottos){
+    public Map<Rank, Integer> getUserLottoScore(List<Lotto> userLottos) {
         Map<Rank, Integer> userLottoScore = new HashMap<>();
-        for(Rank rank : Rank.values()){
+        for (Rank rank : Rank.values()) {
             userLottoScore.put(rank, 0);
         }
         userLottos.forEach(userLotto -> {
-            Rank rank = Rank.of(winningLottoNumbers.getMatchedCount(userLotto), isBonus(userLotto));
+            Rank rank = Rank.of(winningLottoNumbers.getMatchedCount(userLotto), userLotto.isContain(bonusNumber));
             userLottoScore.put(rank, userLottoScore.get(rank) + 1);
         });
-        userLottos.remove(Rank.NOTHING);
+        userLottoScore.remove(Rank.NOTHING);
         return userLottoScore;
-    }
-
-    private boolean isBonus(Lotto userLotto){
-        if(userLotto.isContain(bonusNumber)){
-            return true;
-        }
-        return false;
     }
 }
