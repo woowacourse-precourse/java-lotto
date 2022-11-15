1012533e61848a9e68eb75f735847d6eb7ea2a27
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-11-15 22:13:22 +0900

    Fix: 추가 로또 중복 검증 메서드
    1,2,3,4,5,5 이런 입력에 대해 예외 발생시키기

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,21 +1,30 @@
 package lotto;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private static final int LOTTO_NUMBER_AMOUNT = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBER_AMOUNT) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 잘못된 개수를 입력하였습니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != LOTTO_NUMBER_AMOUNT) {
+            throw new IllegalArgumentException("[ERROR] 중복된 로또번호를 입력하였습니다.");
         }
     }
 
