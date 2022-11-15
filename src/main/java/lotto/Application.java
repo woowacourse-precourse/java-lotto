1e0ae6d07e5d558b2fbb25bb58909d6a531e6842
jsc1014 <wotlr5209@gmail.com>
2022-11-15 20:00:58 +0900

    feat: main 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> numbers = new ArrayList<Integer>();
+        List<List<Integer>> userLottoNumbers = new ArrayList<>();
+        Map<Integer,Integer> winStatistics = new HashMap<>();
+        int buyMoney = UserInterface.inputMoney();
+        UserInterface.printNumberOfLotto(userLottoNumbers,buyMoney);
+        UserInterface.inputWinNumbers(numbers);
+        int bonusNumber = UserInterface.inputBonusNumber(numbers);
+        BuyLotto.howManyMatchNumbers(userLottoNumbers,numbers,bonusNumber,winStatistics);
+        UserInterface.printWinStatics(winStatistics);
+        UserInterface.printYield(buyMoney,winStatistics);
     }
+
+
 }
