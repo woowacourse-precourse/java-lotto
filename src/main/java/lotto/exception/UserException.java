d8472bd99cf1e0a63c6664b63767cc93a1533324
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-15 22:41:58 +0900

    feat: checkUserLottoPriceAboutOnlyNumbers() 예외처리 메서드 추가

--- src/main/java/lotto/exception/UserException.java
+++ src/main/java/lotto/exception/UserException.java
@@ -19,7 +19,14 @@ public class UserException {
         }
     }
 
-
+    public static void checkUserLottoPriceAboutOnlyNumbers(String userLottoPrice) {
+        for (int i = 0; i < userLottoPrice.length(); i++) {
+            if (!Character.isDigit(userLottoPrice.charAt(i))) {
+                System.out.println(Constant.ERROR_MESSAGE + "숫자만 입력이 가능합니다. ");
+                throw new NoSuchElementException();
+            }
+        }
+    }
 
 
 }
