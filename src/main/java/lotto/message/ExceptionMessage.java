dd99d98341e98faccbf4695fe9281ec453120fc8
phdljr <phdljr@naver.com>
2022-11-15 15:31:13 +0900

    feat: ExceptionMessage/LottoConfig 설정 추가

--- src/main/java/lotto/message/ExceptionMessage.java
+++ src/main/java/lotto/message/ExceptionMessage.java
@@ -3,6 +3,7 @@ package lotto.message;
 import lotto.config.LottoConfig;
 
 public class ExceptionMessage {
+    public static final String ERROR_FORMAT = "[ERROR] 숫자와 콤마로 이뤄진 문자열만 입력가능합니다.";
     public static final String ERROR_PRICE = "[ERROR] 로또는 천원 단위로만 구매할 수 있습니다.";
     public static final String ERROR_BUY_PRICE = "[ERROR] 로또는 1000원 이상 21억 이하의 금액으로만 구매할 수 있습니다.";
     public static final String ERROR_DUPLICATED = "[ERROR] 로또 번호는 서로 중복되지 않아야 합니다.";
