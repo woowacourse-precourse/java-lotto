1d89256d18b9bb2fab3cc7ed64893f7af4911b38
Hiluxy <sunset.lhj@gmail.com>
2022-11-15 23:02:32 +0900

    feat 수익률 출력 구현

--- src/main/java/lotto/Message.java
+++ src/main/java/lotto/Message.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.text.NumberFormat;
+import java.util.EnumMap;
 import java.util.List;
 import lotto.model.Lotto;
 
@@ -26,5 +28,23 @@ public class Message {
 
     public void printMsg(String msg) {
         System.out.println(msg);
+    }
+    public void printResult(EnumMap<ResultEnum, Integer> result, Double roi) {
+        NumberFormat rewardFormat = NumberFormat.getNumberInstance();
+        printMsg("\n" + "당첨 통계" + "\n" + "---");
+        for (ResultEnum resultEnum : result.keySet()) {
+            Integer hitAmount = resultEnum.getHitAmount();
+            Integer prize = resultEnum.getRewardAmount();
+            Integer amount = result.get(resultEnum);
+
+            if (resultEnum == ResultEnum.HIT_5_AND_BONUS) {
+                printMsg(hitAmount + "개 일치, 보너스 볼 일치 (" + rewardFormat.format(prize) + "원)" + " - "
+                    + amount + "개");
+                continue;
+            }
+            printMsg(
+                hitAmount + "개 일치 (" + rewardFormat.format(prize) + "원)" + " - " + amount + "개");
+        }
+        printMsg("총 수익률은 " + roi + "%입니다.");
     }
 }
