21202b110e9f209aa69faa0be227307dea061c38
kwang <shinkwang@yonsei.ac.kr>
2022-11-15 20:05:28 +0900

    feat: Make validation for duplication of lottery number
    로또 번호는 중복되어서는 안되기 때문에 중복된 번호가 있는지 확인하는 기능을 만든다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,12 +14,20 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateSize(numbers);
+        validateDuplicate(numbers);
     }
 
     // TODO: 추가 기능 구현
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호의 개수는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> duplicationSet = new HashSet<>(numbers);
+        if (duplicationSet.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 숫자는 중복되어선 안됩니다.");
         }
     }
 }
