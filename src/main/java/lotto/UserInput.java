9ca31222af333ac164d168359845d1648fd8fcae
kkirby54 <sdk926@gmail.com>
2022-11-15 16:40:34 +0900

    feat: 당첨 번호 입력 받기
    - getWinningNumbers()는 입력받은 String을 List<Integer>로 파싱하는 역할을 담당한다.
    - 범위 및 중복에 대한 예외는 당첨 번호 domain에서 담당한다.

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -1,6 +1,11 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.WinningNumbers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserInput {
     public static Long getPurchaseMoney() {
@@ -21,5 +26,30 @@ public class UserInput {
             System.out.println("[ERROR] 구입 금액은 1000의 배수여야 합니다.");
             throw new IllegalArgumentException();
         }
+    }
+
+    public static WinningNumbers getWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        List<Integer> numbers = convertToList(input);
+
+        int bonus = -1; // to be implemented
+
+        return new WinningNumbers(numbers, bonus);
+    }
+
+    private static List<Integer> convertToList(String input) {
+        return Arrays.stream(input.split(","))
+                .filter(UserInput::validateNumber)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean validateNumber(String input) {
+        if (!input.trim().matches("^[0-9]+$")) {
+            System.out.println("[ERROR] 로또 번호는 숫자여야 합니다.");
+            throw new IllegalArgumentException();
+        }
+        return true;
     }
 }
