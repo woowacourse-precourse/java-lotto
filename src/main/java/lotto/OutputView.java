1b0334f0b347932f586eac5857deea88b738da86
Seoyoung <ryupark1006@gmail.com>
2022-11-15 23:52:07 +0900

    feat: 당첨 내역 출력 기능 구현

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
@@ -8,6 +9,14 @@ public class OutputView {
         System.out.println(Integer.toString(amount) + "개를 구매했습니다.");
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public static void printResult(Map<Result, Integer> lottoResult) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (Result result : Result.values()) {
+            System.out.println(result.getMessage() + lottoResult.get(result) + "개");
         }
     }
 }
