7f8a4bebc63741309682c28b4022201d3bd0f5ac
fnzl54 <rnjscksdud1@gmail.com>
2022-11-15 20:39:13 +0900

    Feat: create buyLottoLineNumber in LottoMachine class

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -6,4 +6,11 @@ public class LottoMachine {
     private final int minLottoNumber = 1;
     private final int maxLottoNumber = 45;
     private final int oneLineLottoNumber = 6;
+
+    public int buyLottoLineNumber(String inputMoney) {
+
+        int money = Integer.parseInt(inputMoney);
+
+        return money / buyLineMoney;
+    }
 }
