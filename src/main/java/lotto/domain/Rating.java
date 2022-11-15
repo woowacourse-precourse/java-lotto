c8e9d58c15f406c5e7646b366e2d5023f149fe88
footdev <ekclstkfka5@gmail.com>
2022-11-15 19:07:42 +0900

    refactor: 출력에 나타내는 상수 추가
    출력의 형식에 맞게 내보내기 위해 Rating Enum 클래스에 필드 추가함.

--- src/main/java/lotto/domain/Rating.java
+++ src/main/java/lotto/domain/Rating.java
@@ -3,19 +3,21 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rating {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000),
-    NONE(0, 0);
+    FIRST(6, 2000000000, "2,000,000,000"),
+    SECOND(5, 30000000, "30,000,000"),
+    THIRD(5, 1500000, "1,500,000"),
+    FOURTH(4, 50000, "50,000"),
+    FIFTH(3, 5000, "5,000"),
+    NONE(0, 0, "0");
 
     private final int count;
     private final int reward;
+    private final String wrappingReward;
 
-    Rating(final int count, final int reward) {
+    Rating(final int count, final int reward, final String wrappingReward) {
         this.count = count;
         this.reward = reward;
+        this.wrappingReward = wrappingReward;
     }
 
     public int getCount() {
@@ -25,6 +27,8 @@ public enum Rating {
     public int getReward() {
         return reward;
     }
+
+    public String getWrappingReward() {return wrappingReward; }
 
     public static Rating getRating(final int count, final boolean bonusCount) {
         if(count == THIRD.count && !bonusCount) {
