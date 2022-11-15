3fdda37fbf32ec288b66fb8a0cfe77091e643ccb
GwansuShin <happysun7080@naver.com>
2022-11-15 17:03:20 +0900

    refactor(Controller): run 메소드 내부에 정의되었던 게임 로직을 모듈화
    "구매", "당첨번호 입력", "계산 및 결과 출력"으로 모듈화

--- src/main/java/lotto/game/Controller.java
+++ src/main/java/lotto/game/Controller.java
@@ -1,51 +1,47 @@
 package lotto.game;
 
 import lotto.game.view.View;
-import lotto.user.Lotto;
-
-import java.util.List;
-import java.util.Map;
 
 public class Controller {
     private final View view;
     private final Service service;
 
     public Controller() {
-        this.view = new View();
-        this.service = new Service();
+        view = new View();
+        service = new Service();
     }
 
     public void run() {
         try {
-            // 1. 입력 - 로또 구입 금액
-            String purchaseAmount = view.inputPurchaseAmount();
-
-            // 2. 로또 번호 발행
-            int quantity = service.generateQuantity(purchaseAmount);
-            List<Lotto> lottos = service.generateLottos(quantity);
-
-            // 3. 출력 - 발행한 로또 수량
-            view.outputQuantity(quantity);
-
-            // 4. 출력 - 발행한 로또 번호
-            lottos.forEach(
-                    lotto -> view.outputLottoNumbers(lotto.toString()));
-
-            // 5. 입력 - 당첨 번호
-            Lotto draw = service.generateDraw(view.inputDrawNumbers());
-
-            // 6. 입력 - 보너스 번호
-            int bonus = service.generateBonus(view.inputBonusNumber());
-
-            // 7. 당첨 계산
-            Map<Integer, Integer> drawScore = service.calculateDrawScore(lottos, draw);
-            int bonusScore = service.calculateBonusScore(drawScore, lottos, draw, bonus);
-            double earningsRate = service.calculateEarningsRate(drawScore, bonusScore, quantity);
-
-            // 8. 당첨 통계 출력
-            view.outputResult(drawScore, bonusScore);
-            view.outputEarningRate(earningsRate);
+            purchase();
+            generateDrawAndBonus();
+            calculate();
         } catch (IllegalArgumentException ignored) {
         }
+    }
+
+    private void purchase() {
+        view.outputQuantity(
+                service.generateQuantity(
+                        view.inputPurchaseAmount()));
+        service.generateLottos()
+                .forEach(lotto ->
+                        view.outputLottoNumbers(lotto.toString()));
+    }
+
+    private void generateDrawAndBonus() {
+        service.generateDraw(
+                view.inputDrawNumbers());
+        service.generateBonus(
+                view.inputBonusNumber());
+    }
+
+    private void calculate() {
+        view.outputResult(
+                service.calculateDrawScore(),
+                service.calculateBonusScore()
+        );
+        view.outputEarningRate(
+                service.calculateEarningsRate());
     }
 }
