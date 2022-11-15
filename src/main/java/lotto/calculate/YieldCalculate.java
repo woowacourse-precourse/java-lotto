8780fe89e4c8cecf8c50209ca3af9a10b465fc8b
kswhj <108874006+kswhj@users.noreply.github.com>
2022-11-15 17:17:54 +0900

    Create YieldCalculate.java
    수익률 계산

---
+++ src/main/java/lotto/calculate/YieldCalculate.java
@@ -0,0 +1,8 @@
+package calculate;
+
+public class YieldCalculate {
+	public static void run(int num, int cost) {
+		double yield = num / cost;
+		System.out.printf("총 수익률은 %.1f%입니다.", yield);
+	}
+}
