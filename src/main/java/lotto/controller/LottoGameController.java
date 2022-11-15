0d1d4c915dab1307bb96301b13ca6a6ae84368b8
gomljo <gomljo0307@naver.com>
2022-11-15 14:17:04 +0900

    feat(LottoGameController): skeleton 코드 작성

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -5,11 +5,52 @@ import lotto.view.LottoGameView;
 
 public class LottoGameController {
     LottoGameView lottoGameView = new LottoGameView();
-    Buyer buyer = new Buyer();
-    Lottery lottery = new Lottery();
+    Buyer buyer=new Buyer();
+    LottoGenerator lottoGenerator = new LottoGenerator();
     RaffleNumber raffleNumber = new RaffleNumber();
     Discriminator discriminator = new Discriminator();
     Revenue revenue = new Revenue();
-    
+
+    public void calculateTickets(){
+
+    }
+
+    public void noticeNumberOfTickets(){
+
+    }
+    public void createLotteries(){
+
+    }
+    public void noticeLotteries(){
+
+    }
+    public void checkLotteryWin(){
+
+    }
+    public void figureOutWinStatics(){
+
+    }
+    public void noticeWinReport(){
+
+    }
+    public void calculateRateOfRevenue(){
+
+    }
+    public void noticeRateOfRevenue(){
+
+    }
+    public void start(){
+
+        calculateTickets();
+        noticeNumberOfTickets();
+        createLotteries();
+        noticeLotteries();
+        checkLotteryWin();
+        figureOutWinStatics();
+        noticeWinReport();
+        calculateRateOfRevenue();
+        noticeRateOfRevenue();
+
+    }
 
 }
