bd20e70516484faf4061d7cbfbe53618cbdd34c7
jaeyeon kim <jakind@naver.com>
2022-11-15 12:10:32 +0900

    test(RankTest): 상수들의 matching 값을 얻어내는 메소드 테스트

---
+++ src/test/java/lotto/RankTest.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RankTest {
+
+    @DisplayName("Rank numberOfMatching 메소드 확인")
+    @Test
+    void numberOfMatchingMethodOfRankValidate() {
+        // given
+        List<Float> numberOfMatching = Rank.getNumbersOfMatching();
+        List<Float> compare = List.of(3f, 4f, 5f, 5.5f, 6f);
+
+        // then
+        assertThat(numberOfMatching.containsAll(compare)).isTrue();
+    }
+}
