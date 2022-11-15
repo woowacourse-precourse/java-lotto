37bb0b05ac190c90b3ff031119f177673fb6fa50
danneh-0427 <jc14972@gmail.com>
2022-11-15 22:21:32 +0900

    docs(README.md): 사용자가 번호를 입력할 때 검증해야할 부분들 추가 feat(Lotto.java): 사용자가 최대 6개의 숫자를 옳바르게 입력했는지 검증하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,17 +4,27 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_LENGTH = 6;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+    public List<Integer> getUserLottoNumber() {
+        return numbers;
     }
 
-    // TODO: 추가 기능 구현
+   private void validate(List<Integer> numbers) {
+        validateUserInput(numbers);
+    }
+
+    private void validateUserInput(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_LENGTH) {
+            throw new IllegalArgumentException("총 6개의 숫자를 입력하셔야 합니다.")
+        }
+    }
 }
+
