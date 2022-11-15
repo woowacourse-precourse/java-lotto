293a2c2d540c39b2ecc8ae86249e647566895f83
desfox <desfox99@naver.com>
2022-11-15 14:07:07 +0900

    feat(LottoController): 구입한 로또 개수만큼 로또 객체 생성하는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,12 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoController {
     public static final int PRICE_OF_LOTTO = 1000;
@@ -9,13 +14,26 @@ public class LottoController {
     private InputView inputView = new InputView();
     private OutputView outputView = new OutputView();
 
+    private List<Lotto> lottos = new ArrayList<>();
+
     public void playLottoGame() {
         int purchasePrice = inputView.userInputPurchasePrice();
         int amountOfLotto = computeAmountOfLotto(purchasePrice);
         outputView.printAmountOfLottoMessage(amountOfLotto);
+
+        generateLotto(amountOfLotto);
     }
 
     public int computeAmountOfLotto(int purchasePrice) {
         return purchasePrice / PRICE_OF_LOTTO;
+    }
+
+    public void generateLotto(int amountOfLotto) {
+        LottoMachine lottoMachine = new LottoMachine();
+
+        for (int repeatCnt = 0; repeatCnt < amountOfLotto; repeatCnt++) {
+            Lotto lotto = lottoMachine.generateLotto();
+            lottos.add(lotto);
+        }
     }
 }
