2fb445f0aeb0ae49c6c91f83a1f610b1cd88740d
Haechan Lee <dlgockssla@gmail.com>
2022-11-15 15:18:30 +0900

    fix(view): lotto instance를 join하는 버그 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,17 +8,19 @@ import lotto.Lotto;
 public class OutputView {
 
     public static void printBuyHistory(List<Lotto> lottos) {
+        // TODO: Parser 로 옮기기
         String start = "[";
         String end = "]";
         String delimiter = ", ";
 
         int amount = lottos.size();
         System.out.println(String.format("%d개를 구매했습니다.", amount));
-
-        String lottoStr = lottos.stream()
-                .map(String::valueOf)
-                .collect(Collectors.joining(delimiter));
-        System.out.println(start + lottoStr + end);
+        for (Lotto lotto : lottos) {
+            String lottoStr = lotto.getNumbers().stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(delimiter));
+            System.out.println(start + lottoStr + end);
+        }
     }
 
 }
