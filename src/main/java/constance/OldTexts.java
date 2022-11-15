30e049aa8d27a1d55dc21ea5cacd3944f2dd388b
nn98 <jkllhgb@gmail.com>
2022-11-15 17:33:01 +0900

    Refactor: Constance Texts to enum

--- src/main/java/constance/OldTexts.java
+++ src/main/java/constance/OldTexts.java
@@ -1,6 +1,7 @@
 package constance;
 
-public class Texts {
+public class OldTexts {
+
     public static final String LOTTO_USER_INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     public static final String LOTTO_USER_PURCHASE_RESULT = "%d개를 구매했습니다.\n%s";
     public static final String LOTTO_USER_INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
