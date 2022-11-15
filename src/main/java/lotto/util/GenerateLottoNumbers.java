4dee6d7f1a25c11d0574f79b43494bab83b74c5d
eom-tae-in <eti0728@daum.net>
2022-11-15 19:34:03 +0900

    feat(GenerateLottoNumbers): generateLottoNumber() -> 랜덤 로또 번호 6개를 생성해주는 메서드

--- src/main/java/lotto/util/GenerateLottoNumbers.java
+++ src/main/java/lotto/util/GenerateLottoNumbers.java
@@ -1,13 +1,24 @@
 package lotto.util;
 
+import static lotto.constant.Constant.*;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import lotto.domian.UserSingleLottoNumber;
 
 public class GenerateLottoNumbers {
     public List<Integer> listSort(List<Integer> unsortedList) {
         List<Integer> sortedList = new ArrayList<>(unsortedList);
         Collections.sort(sortedList);
         return sortedList;
+    }
+    public List<UserSingleLottoNumber> generateLottoNumber(long lottoQuantity) {
+        List<UserSingleLottoNumber> userAllLottoNumbers = new ArrayList<>();
+        for (int i = 0; i < lottoQuantity; i++) {
+            List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, NUMBER_OF_LOTTO_NUMBERS);
+            userAllLottoNumbers.add(new UserSingleLottoNumber(listSort(lottoNumber)));
+        }
+        return userAllLottoNumbers;
     }
 }
