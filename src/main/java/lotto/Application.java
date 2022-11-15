3a9621736abd4c9ad52f5e02dd31f3f7195f2410
Jaewoong Kim <kimje1109@gmail.com>
2022-11-15 14:53:38 +0900

    refactor: main 메소드 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,28 +1,38 @@
 package lotto;
 
 public class Application {
-
-
     public static void main(String[] args) {
         Notice notice = new Notice();
         User user = new User();
 
+        purchase(user, notice);
+        Lottery lottery = new Lottery(user.getPurchaseNumber());
+        notice.lottery(lottery);
+        winningNumber(user, notice);
+        bonusNumber(user, notice);
+        winningResult(user, notice, lottery);
+    }
+
+    public static void purchase(User user, Notice notice) {
         notice.purchasePrice();
         user.setPurchasePrice();
-
         notice.purchaseNumber(user.getPurchaseNumber());
-        Lottery lottery = new Lottery(user.getPurchaseNumber());
+    }
 
-        notice.lottery(lottery);
-
+    public static void winningNumber(User user, Notice notice) {
         notice.winningNumber();
         user.setWinningNumbers();
+    }
 
+    public static void bonusNumber(User user, Notice notice) {
         notice.bonusNumber();
         user.setBonusNumber();
+    }
 
+    public static void winningResult(User user, Notice notice, Lottery lottery) {
         lottery.calcGrade(user);
         notice.winningAnalysis(lottery.getGradeCount());
+        notice.profitRatio(user.getProfitRatio(lottery.getWinningPrice()));
     }
 
 }
