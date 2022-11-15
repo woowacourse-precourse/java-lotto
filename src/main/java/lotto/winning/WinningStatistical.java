e0620c2ff04819d07004f24f94276f9e622eac29
조병우 <whquddn55@gmail.com>
2022-11-15 22:33:28 +0900

    refactor(Winning): WinningStatistical에 상수 도입

--- src/main/java/lotto/winning/WinningStatistical.java
+++ src/main/java/lotto/winning/WinningStatistical.java
@@ -5,22 +5,24 @@ import java.util.Map;
 import lotto.LottoGame.LottoGame;
 
 public class WinningStatistical {
+    public static final int PERCENT = 100;
+    public static final int DEFAULT_WINNING_COUNT = 0;
     private final Map<WinningResult, Integer> winningResultCount = new HashMap<>();
     private Integer boughtPrice = 0;
     private Long winningPrice = 0L;
 
     public void addWinningResult(WinningResult winningResult) {
-        Integer prevWinningResultCount = winningResultCount.getOrDefault(winningResult, 0);
+        Integer prevWinningResultCount = winningResultCount.getOrDefault(winningResult, DEFAULT_WINNING_COUNT);
         winningResultCount.put(winningResult, prevWinningResultCount + 1);
         winningPrice += winningResult.getPrize();
         boughtPrice += LottoGame.PRICE_UNIT;
     }
 
     public Integer getCountOfWinningResult(WinningResult winningResult) {
-        return winningResultCount.getOrDefault(winningResult, 0);
+        return winningResultCount.getOrDefault(winningResult, DEFAULT_WINNING_COUNT);
     }
 
     public Double calculateProfit() {
-        return (winningPrice.doubleValue() / boughtPrice) * 100;
+        return (winningPrice.doubleValue() / boughtPrice) * PERCENT;
     }
 }
