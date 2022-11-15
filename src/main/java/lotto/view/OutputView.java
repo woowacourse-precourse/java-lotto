323c8f9a81a4d61a692d7a811a0e0e1f90dcc174
This2sho <zx00018@naver.com>
2022-11-15 09:10:28 +0900

    feat(OutputView): 에러 메시지 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -59,4 +59,8 @@ public class OutputView {
 	public void printNewLine() {
 		System.out.println();
 	}
+
+	public void printErrorMessage(String errorMessage) {
+		System.out.println(errorMessage);
+	}
 }
