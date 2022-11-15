2a52d4292981f4ab6594af1a6ae27dce192bdf1a
qdtjj4 <76ylo1@gmail.com>
2022-11-15 17:16:02 +0900

    refactor: 입력 기능 private -> public 및 static 추가

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -7,23 +7,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Input {
-    private final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    private final String AMOUNT_ERROR = "[ERROR] 구입금액은 양의 정수여야 합니다.";
-    private final String INPUT_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
-    private final String LOTTO_ERROR = "[ERROR] 로또 번호는 정수여야 합니다.";
-    private final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    private final String BONUS_ERROR = "[ERROR] 보너스 번호는 정수여야 합니다.";
+    private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String AMOUNT_ERROR = "[ERROR] 구입금액은 양의 정수여야 합니다.";
+    private static final String INPUT_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String LOTTO_ERROR = "[ERROR] 로또 번호는 정수여야 합니다.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String BONUS_ERROR = "[ERROR] 보너스 번호는 정수여야 합니다.";
 
     private Input() {
     }
 
-    private int inputPurchaseAmount() {
+    public static int inputPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT);
         String purchaseAmount = Console.readLine();
         return convertToNumber(purchaseAmount);
     }
 
-    private int convertToNumber(String purchaseAmount) {
+    private static int convertToNumber(String purchaseAmount) {
         try {
             return Integer.parseInt(purchaseAmount);
         } catch (NumberFormatException e) {
@@ -31,18 +31,18 @@ public class Input {
         }
     }
 
-    private Lotto inputLottoNumbers(String numbers) {
+    public static Lotto inputLottoNumbers(String numbers) {
         System.out.println(INPUT_LOTTO_NUMBERS);
         numbers = Console.readLine();
         return replaceSeparator(numbers);
     }
 
-    private Lotto replaceSeparator(String numbers) {
+    private static Lotto replaceSeparator(String numbers) {
         String[] eachNumbers = numbers.split(",");
         return collectLottoNumbers(eachNumbers);
     }
 
-    private Lotto collectLottoNumbers(String[] eachNumbers) {
+    private static Lotto collectLottoNumbers(String[] eachNumbers) {
         List<Integer> lottoNumbers = new ArrayList<>();
         for (String eachNumber : eachNumbers) {
             lottoNumbers.add(convertEachNumbers(eachNumber));
@@ -51,7 +51,7 @@ public class Input {
         return lotto;
     }
 
-    private int convertEachNumbers(String eachNumber) {
+    private static int convertEachNumbers(String eachNumber) {
         try {
             return Integer.parseInt(eachNumber);
         } catch (NumberFormatException e) {
@@ -59,13 +59,13 @@ public class Input {
         }
     }
 
-    private int inputBonusNumber(String number) {
+    public static int inputBonusNumber(String number) {
         System.out.println(INPUT_BONUS_NUMBER);
         number = Console.readLine();
         return convertBonusNumber(number);
     }
 
-    private int convertBonusNumber(String bonusNumber) {
+    private static int convertBonusNumber(String bonusNumber) {
         try {
             return Integer.parseInt(bonusNumber);
         } catch (NumberFormatException e) {
