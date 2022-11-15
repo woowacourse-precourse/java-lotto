a38b18839912dcaaae1c5a108e647e09f6fe1828
raycho <raycho521@yonsei.ac.kr>
2022-11-15 17:45:13 +0900

    feat(lotto): add getYield method to calculate yield
    2022.11.15 by Ray

--- src/main/java/lotto/Statistics.java
+++ src/main/java/lotto/Statistics.java
@@ -1,10 +1,20 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Statistics {
+    private static final int first = 2_000_000_000;
+    private static final int second = 30_000_000;
+    private static final int third = 1_500_000;
+    private static final int fourth = 50_000;
+    private static final int fifth = 5_000;
+
     private final Lotto winning;
     private final int bonus;
+    private static final List<Integer> reward = new ArrayList<>(Arrays.asList(first, second, third, fourth, fifth));
+
 
     public Statistics(Lotto winning, int bonus) {
         this.winning = winning;
@@ -40,5 +50,14 @@ public class Statistics {
         if (lotto.getNumbers().contains(bonus))
             return true;
         return false;
+    }
+
+
+    public static double getYield(int cost, List<Integer> winningCount) {
+        int totalReward = 0;
+        for (int i = 0; i < winningCount.size(); i++) {
+            totalReward += winningCount.get(i) * reward.get(i);
+        }
+        return (double) totalReward / (double) cost * 100;
     }
 }
