e7ce73b8d4c58a8f6b6950d94b2e4a29cc0b8ce0
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-15 17:21:30 +0900

    feat(InputView): 당첨 번호 6개 + 보너스 번호 1개 입력 받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -16,10 +16,19 @@ public class InputView {
     }
 
     public List<Integer> inputWinningNumbers() {
-        printWinningNumberMessage();
+        printWinningNumbersMessage();
         String winningNumbers = Console.readLine();
         validateSeparator(winningNumbers);
         return changeStringToList(winningNumbers);
+    }
+
+    public int inputBonusNumber() {
+        printBonusNumberMessage();
+        String bonusNumber = Console.readLine();
+        validateNumeric(bonusNumber);
+        int number = Integer.parseInt(bonusNumber);
+        validateNumberRange(number);
+        return number;
     }
 
     public void validateNumeric(String input) {
@@ -32,6 +41,14 @@ public class InputView {
         final String REGEX = "^\\d,\\d,\\d,\\d,\\d,\\d$";
         if (!Pattern.matches(REGEX, input)) {
             throw new IllegalArgumentException("[ERROR] 6개의 숫자를 쉼표(,)로 구분하여 입력하세요.");
+        }
+    }
+
+    public void validateNumberRange(int number) {
+        final int MIN = 1;
+        final int MAX = 45;
+        if (number < MIN || number > MAX) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1이상 45이하의 숫자만 가능합니다.");
         }
     }
 
@@ -49,7 +66,11 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    private void printWinningNumberMessage() {
+    private void printWinningNumbersMessage() {
         System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    private void printBonusNumberMessage() {
+        System.out.println("보너스 번호를 입력해 주세요.");
     }
 }
