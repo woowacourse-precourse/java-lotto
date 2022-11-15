328b1c5aa4f88ac41eda49c86a03f03078a947ab
anneu <b.imnuj@gmail.com>
2022-11-15 16:04:33 +0900

    feat: 보너스 객체를 비교하는 equals, hashcode 기능 오버라이딩

--- src/main/java/lotto/model/Bonus.java
+++ src/main/java/lotto/model/Bonus.java
@@ -3,6 +3,7 @@ package lotto.model;
 import static lotto.utils.ConstantUtil.ERROR;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Bonus {
     private static final String BONUS_ERROR_MESSAGE = ERROR + "보너스 번호는 당첨 번호와 중복될 수 없습니다.";
@@ -26,5 +27,22 @@ public class Bonus {
         if (winningLotto.isExistNumber(bonusNumber)) {
             throw new IllegalArgumentException(BONUS_ERROR_MESSAGE);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Bonus bonus = (Bonus) o;
+        return bonusNumber == bonus.bonusNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bonusNumber);
     }
 }
