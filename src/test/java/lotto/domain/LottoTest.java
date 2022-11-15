a2e165cb8a7017cecbeaeb4338ee4df585867bba
juy4556 <kws05050@naver.com>
2022-11-15 21:40:42 +0900

    feat: 로또 오름차순 정렬된 상태인지 확인 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,12 +1,14 @@
 package lotto.domain;
 
-import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -31,4 +33,13 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 오름차순 정렬된 상태인지 확인한다.")
+    @Test
+    void createNumbersWithAscending() {
+        Lotto lotto = new Lotto(List.of(4, 3, 1, 5, 6, 2));
+        List<Integer> numbers;
+        List<Integer> expect = new ArrayList<>(List.of(1,2,3,4,5,6));
+        numbers = lotto.sortAscendingLotto();
+        assertEquals(numbers, expect);
+    }
 }
