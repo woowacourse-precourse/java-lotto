b23326b94c72b47e676528f3847647c660ca8449
오승윤 <>
2022-11-15 23:29:45 +0900

    refactor (LottoNumber) : 매직 넘버 상수화

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -3,6 +3,9 @@ package lotto.domain;
 import static lotto.domain.messages.ErrorMessages.RANGE_ERROR_MESSAGE;
 
 public class LottoNumber {
+
+    private static final Integer MAX_LOTTO_NUMBER = 45;
+    private static final Integer MIN_LOTTO_NUMBER = 1;
     private Integer number;
 
     private LottoNumber(int number) {
@@ -19,7 +22,7 @@ public class LottoNumber {
     }
 
     private void validateTo45From1(int number) {
-        if (number < 1 || number > 45) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
         }
     }
