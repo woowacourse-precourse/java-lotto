5ce1404c60ecd06d49560e29467b21b489bedaa5
p <gustjr2332@naver.com>
2022-11-15 15:52:05 +0900

    refactor(LottoRankingResults) : 하드코딩 수정

--- src/main/java/lotto/LottoRankingResults.java
+++ src/main/java/lotto/LottoRankingResults.java
@@ -6,19 +6,19 @@ import java.util.Map;
 public class LottoRankingResults {
 
     private Map<Ranking, Integer> rankingResults = new LinkedHashMap<>();
+    private final int INITIAL_NUMBEROFWINNING = 0;
 
     public LottoRankingResults() {
         for (Ranking ranking : Ranking.values())
-            rankingResults.put(ranking, 0);
+            rankingResults.put(ranking, INITIAL_NUMBEROFWINNING);
     }
 
     public void recordTheRanking(Ranking rank) {
-        Integer count = rankingResults.get(rank) + 1;
-        rankingResults.replace(rank, count++);
+        Integer numberOfWinning = rankingResults.get(rank);
+        rankingResults.replace(rank, ++numberOfWinning);
     }
 
     public Map<Ranking, Integer> getRankingResults() {
         return rankingResults;
     }
-
 }
