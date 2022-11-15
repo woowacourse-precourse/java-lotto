fd6fdc669d6ac42e6dc02af2d2095db3d8a6cd82
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-15 21:53:46 +0900

    Feat(View): Add Print Game Message Methods

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,5 +1,19 @@
 package lotto.view;
 
 public class Output {
+    public static void printStartMsg() {
+        System.out.println(lotto.constants.Output.PURCHASE_AMOUNT_INPUT.getMsg());
+    }
 
+    public static void printPurchaseMsg(int number) {
+        System.out.println(number + lotto.constants.Output.PURCHASE_AMOUNT_OUTPUT.getMsg());
+    }
+
+    public static void printWinningMsg() {
+        System.out.println(lotto.constants.Output.WINNING_NUMBER_INPUT.getMsg());
+    }
+
+    public static void printBonusMsg() {
+        System.out.println(lotto.constants.Output.BONUS_NUMBER_INPUT.getMsg());
+    }
 }
