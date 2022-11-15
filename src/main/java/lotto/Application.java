8ab6075d40c2c2f341e3f5c8f82d54e79ac1d2fa
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-15 17:22:03 +0900

    feat: Add create lotto function to application

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,35 @@
 package lotto;
 
+import static lotto.Cli.getPurchaseAmount;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int amount = getPurchaseAmount();
+        int purchaseCount = amount / 1000;
+        List<Lotto> lottoList = createLotto(purchaseCount);
+    }
+
+    /**
+     * Function to create lotto.
+     * @param count The integer value of lotto count.
+     * @return The list of lotto.
+     */
+    private static List<Lotto> createLotto(Integer count) {
+
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i=0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> sortedNumber = numbers.stream()
+                .sorted().collect(Collectors.toList());
+            Lotto lotto = new Lotto(sortedNumber);
+            lottoList.add(lotto);
+        }
+
+        return lottoList;
     }
 }
