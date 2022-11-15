f80e6474192063762e20e9d4d920c7e48bc3ac79
dener8 <8dener@gmail.com>
2022-11-15 19:41:16 +0900

    feat(NumbersMaker): 에러 던져주는 코드 추가
    OUT_OF_SIZE_ERROR
    DUPLICATE_ERROR
    위의 두 경우 throw

--- src/main/java/lotto/NumbersMaker.java
+++ src/main/java/lotto/NumbersMaker.java
@@ -2,17 +2,30 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import org.assertj.core.util.Sets;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+
+import static resource.ErrorMessage.DUPLICATE_ERROR;
+import static resource.ErrorMessage.OUT_OF_SIZE_ERROR;
 
 public class NumbersMaker {
     public List<Integer> makeWinningNumbers(String numbersWithComma) {
         List<Integer> winningNumbers = Arrays.stream(numbersWithComma.split(","))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException(OUT_OF_SIZE_ERROR);
+        }
+
+        Set<Integer> set = Set.copyOf(winningNumbers);
+        if (set.size() != 6) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR);
+        }
         return winningNumbers;
     }
 
@@ -24,7 +37,7 @@ public class NumbersMaker {
     public List<List<Integer>> makePurchaserNumbers(int purchaseAmount) {
         List<List<Integer>> purchaserNumbers = new ArrayList<>();
         List<Integer> oneGameNumbers;
-        int gameCycle = purchaseAmount / 10;
+        int gameCycle = purchaseAmount / 1000;
 
         for (int i = 0; i < gameCycle; i++) {
             oneGameNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
@@ -32,6 +45,5 @@ public class NumbersMaker {
         }
         return purchaserNumbers;
     }
-
 
 }
