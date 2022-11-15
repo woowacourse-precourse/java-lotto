954e537d1c6488c79710d6e3f3092ffe4ad9362b
이건회 <rg970604@naver.com>
2022-11-15 18:43:56 +0900

    style(LottosTest.java) : 코드 컨벤션 리포맷팅

--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -12,7 +12,7 @@ public class LottosTest {
     @DisplayName("7장 생성시 Lottos의 리스트 사이즈는 7이 된다")
     @Test
     void generate7piecesLottos() {
-        Lottos lottos = new Lottos(7,new RandomLottoGenerateStrategy());
+        Lottos lottos = new Lottos(7, new RandomLottoGenerateStrategy());
         assertThat(lottos.getLottos().size()).isEqualTo(7);
     }
 }
