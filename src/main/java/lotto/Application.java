b0b4d032fa445c76c0c8697a94a7d745248ddebd
hyunseo <goatsfish@gmail.com>
2022-11-15 12:02:11 +0900

    feat : 로또 게임 애플리케이션 메인 실행 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            new LottoController().process();
+        } catch (IllegalArgumentException e) {
+            System.out.print(e.getMessage());
+        }
     }
 }
