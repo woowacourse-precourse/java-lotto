d1f607f71255de61a293f050634297b8e6fb0de0
desfox <desfox99@naver.com>
2022-11-15 10:46:34 +0900

    feat(BonusNumberInputValidation): 보너스 번호가 예측 번호와 중복 없는지 검증하는 로직 구현

--- src/main/java/lotto/validation/BonusNumberInputValidation.java
+++ src/main/java/lotto/validation/BonusNumberInputValidation.java
@@ -1,11 +1,21 @@
 package lotto.validation;
 
+import java.util.List;
+
 public class BonusNumberInputValidation {
     public void validateInputIsInteger(String userInputBonusNumber) {
         try {
             Integer.parseInt(userInputBonusNumber);
         } catch (Exception e) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+        }
+    }
+
+    public void validateNoDuplicationWithPredictionNumbers(List<Integer> predictionNumbers, String userInputBonusNumber) {
+        int bonusNumber = Integer.parseInt(userInputBonusNumber);
+
+        if (predictionNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 예측 번호와 중복되는 번호를 입력할 수 없습니다.");
         }
     }
 }
