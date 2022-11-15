592a0817bc2bcff20e2e7a4fcdeb56bf6b40f77d
leesangjin <tkdwls0628@gmail.com>
2022-11-15 17:17:09 +0900

    feat(LottoGenerator): 로또 정렬 기능 구현

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Lotto;
 import lotto.constant.Constant;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoGenerator {
     public Lotto makeLotto() {
@@ -11,6 +12,10 @@ public class LottoGenerator {
                 Constant.MIN_LOTTO_NUMBER,
                 Constant.MAX_LOTTO_NUMBER,
                 Constant.LOTTO_SIZE);
-        return new Lotto(lottoNumbers);
+        return new Lotto(sortLottoNumbers(lottoNumbers));
+    }
+
+    private List<Integer> sortLottoNumbers(List<Integer> lottoNumbers) {
+        return lottoNumbers.stream().sorted().collect(Collectors.toList());
     }
 }
