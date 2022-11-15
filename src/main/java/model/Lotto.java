0c61cdc21e404cbef268e0a4b5387950e8690a09
June <68291395+june-777@users.noreply.github.com>
2022-11-15 14:27:24 +0900

    refactor: 로또 유효성 검사 기능을 난수생성 클래스에서 Lotto 클래스로 이동
    Lotto 클래스를 일급 컬렉션으로 사용하기 위함
    유효성 검증의 책임은 일급 컬렉션에서 수행하는 것이 옳다고 판단

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -1,6 +1,8 @@
 package model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
 	private final List<Integer> numbers;
@@ -10,9 +12,34 @@ public class Lotto {
 		this.numbers = numbers;
 	}
 
-	private void validate(List<Integer> numbers) {
-		if (numbers.size() != 6) {
-			throw new IllegalArgumentException();
+	private void validate(List<Integer> sixLengthRandomUniqueNumbers) {
+		validateRange(sixLengthRandomUniqueNumbers);
+		validateSize(sixLengthRandomUniqueNumbers);
+		validateDuplicate(sixLengthRandomUniqueNumbers);
+	}
+
+	private void validateRange(List<Integer> randomNumbers) {
+		for (int randomNumber : randomNumbers) {
+			if (randomNumber < 1) {
+				throw new IllegalArgumentException("[ERROR] 로또 번호에 최소값(1) 보다 작은 값이 있습니다.");
+			}
+			if (randomNumber > 45) {
+				throw new IllegalArgumentException("[ERROR] 로또 번호에 최대값(45) 보다 큰 값이 있습니다.");
+			}
+		}
+	}
+
+	private void validateSize(List<Integer> randomNumbers) {
+		if (randomNumbers.size() != 6) {
+			throw new IllegalArgumentException("[ERROR] 로또 번호 개수가 6개가 아닙니다.");
+		}
+
+	}
+
+	private void validateDuplicate(List<Integer> randomNumbers) {
+		Set<Integer> uniqueNumbers = new HashSet<>(randomNumbers);
+		if (uniqueNumbers.size() != randomNumbers.size()) {
+			throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 값이 있습니다.");
 		}
 	}
 
