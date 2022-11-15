d39dc942f9967979627588e4ad3d88b51ced41ff
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2022-11-15 12:03:03 +0900

    feat(Application): 최종실행 코드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import control.LottoGame;
+import view.InputView;
+import view.OutputView;
+
 public class Application {
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        new LottoGame(inputView,outputView).playLotto();
     }
 }
