c8498a88491a1201796e1102b592ba18a7e58608
kjg1514 <kjg1514@naver.com>
2022-11-15 18:04:24 +0900

    feat : 로또 당첨 번호 맞추기

---
+++ src/main/java/lotto/domain/Compare.java
@@ -0,0 +1,54 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static lotto.domain.Rank.*;
+
+public class Compare {
+    private static final Rank[] RANKS = {FIFTH, FOURTH, THIRD, SECOND, FIRST};
+    private static int first;
+    private static int second;
+    private static int third;
+    private static int fourth;
+    private static int fifth;
+    public int compareNumbers(List<Integer> lottoGroups, List<Integer> lottoNum) {
+        int cnt = 0;
+        for (int i = 0; i < lottoNum.size(); i++) {
+            if (lottoGroups.contains(lottoNum.get(i))) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    public Rank compare_Numbers_Group(Map<Integer, List> lottoGroups, List<Integer> lottoNum, int bonusNum) {
+
+        for (int i = 0; i < lottoGroups.size(); i++) {
+            int cnt = compareNumbers(lottoGroups.get(i), lottoNum);
+            if (cnt == 6) {
+                first++;
+                return FIRST;
+            }
+            if (cnt == 5 && lottoGroups.get(i).contains(bonusNum)) {
+                second++;
+                return SECOND;
+            }
+            if (cnt == 5) {
+                third++;
+                return THIRD;
+            }
+            if (cnt == 4) {
+                fourth++;
+                return FOURTH;
+            }
+            if (cnt == 3) {
+                fifth++;
+                return FIFTH;
+            }
+        }
+        return NOTHING;
+    }
+
+}
