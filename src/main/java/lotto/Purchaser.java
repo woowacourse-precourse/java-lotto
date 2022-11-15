a59179d2a56035cc6c037e23effe268a9965eee1
kimsinhyun <ksh415622@naver.com>
2022-11-15 13:34:29 +0900

    feat: (Purchase Class) 구입한 금액 만큼의 로또 번호 생성
    1. calculateLottoNum()를 통해 구입한 로또 개수 받기
    2. pickRandomNumbers()룰 통해 구입한 개수만큼의 6개 무작위 숫자로
       이뤄진 로또 번호 생성

--- src/main/java/lotto/Purchaser.java
+++ src/main/java/lotto/Purchaser.java
@@ -1,28 +1,31 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 
 public class Purchaser {
 
     private static final Long LOTTO_PRICE = 1_000L;
 
-    private static Long purchaseAmount;
+    private static long purchaseAmount;
+    private static long lottoNum;
+    protected static List[] numbers;
 
-    public static Long setPurchaseAmount(){
-//        금액을 정확히 입력했는지 확인
+    protected static long setPurchaseAmount(){
         String inputMoney = Console.readLine();
-        System.out.println("inputMoney: " + inputMoney);
+        // 금액을 정확히 입력했는지 확인
         if (validPurchaseAmount(inputMoney)){
             purchaseAmount = Long.parseLong(inputMoney);
         }
         return purchaseAmount;
     }
 
-    public static boolean validPurchaseAmount(String inputMoney) throws IllegalArgumentException{
+    protected static boolean validPurchaseAmount(String inputMoney) throws IllegalArgumentException{
 //        숫자로 입력했는지, 아니면 Exception
         try{
-            Long temp = Long.parseLong(inputMoney);
+            long temp = Long.parseLong(inputMoney);
         }
         catch(Exception e){
             Error.NOT_NUMERIC.wrongInput();
@@ -32,5 +35,21 @@ public class Purchaser {
             Error.PURCHASE_AMOUNT_ERROR.wrongInput();
         }
         return true;
+    }
+
+    protected static void calculateLottoNum(){
+        lottoNum = purchaseAmount / LOTTO_PRICE;
+    }
+
+    protected static void pickRandomNumbers(){
+        numbers = new List [(int)lottoNum];
+
+        for(int i = 0; i < (int)lottoNum ; ++i){
+            numbers[i] = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        }
+    }
+
+    public Long getLottoNum(){
+        return this.lottoNum;
     }
 }
