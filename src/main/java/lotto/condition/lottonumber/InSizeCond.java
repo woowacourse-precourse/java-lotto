15f92b362940ba55cf547f39103fa2681a479516
wonju-dev <wonjulee.dev@gmail.com>
2022-11-15 15:04:36 +0900

    feat(InSizeCond): 입력된 숫자 개수가 6개인지 확인하는 로직 구현

---
+++ src/main/java/lotto/condition/lottonumber/InSizeCond.java
@@ -0,0 +1,29 @@
+package lotto.condition.lottonumber;
+
+import lotto.Util;
+import lotto.condition.Condition;
+
+import static lotto.domain.lottomachine.LottoMachine.LOTTO_NUMBER_SIZE;
+
+public class InSizeCond implements Condition {
+
+    private static final Condition instance = new InSizeCond();
+    private static final String ERROR_MESSAGE = "6개의 숫자로 구성되어야 합니다";
+
+    private InSizeCond() {
+    }
+
+    public static Condition getInstance() {
+        return instance;
+    }
+
+    @Override
+    public Boolean isSatisfied(String input) {
+        return Util.getParsedNumbers(input).size() <= LOTTO_NUMBER_SIZE;
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return ERROR_MESSAGE;
+    }
+}
