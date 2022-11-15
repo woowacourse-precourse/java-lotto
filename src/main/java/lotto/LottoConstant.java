91723d524a3f64a3e6e8be314b3e928d5274870c
Il kwon <alexryu1105@naver.com>
2022-11-15 09:25:17 +0900

    fix: 오류 메세지 오탈자 수정

--- src/main/java/lotto/LottoConstant.java
+++ src/main/java/lotto/LottoConstant.java
@@ -7,7 +7,7 @@ public class LottoConstant {
     public static final String INPUT_IS_NOT_NUMBER_ERROR_MESSAGE = "[ERROR] 입력값이 올바른 숫자가 아닙니다.";
     public static final String INPUT_IS_TOO_LOW_TO_BUY_LOTTO_ERROR_MESSAGE = "[ERROR] 로또를 구매하기에 너무 작은 금액입니다.";
     public static final String INPUT_IS_NOT_FALL_APART_ERROR_MESSAGE = "[ERROR] 입력 금액이 1000으로 나누어 떨어지지 않습니다.";
-    public static final String INPUT_IS_NOT_MATCH_REGEX_ERROR_MESSAGE = "[ERROR] 입력 로도 번호가 올바르지 않습니다.";
+    public static final String INPUT_IS_NOT_MATCH_REGEX_ERROR_MESSAGE = "[ERROR] 입력 로또 번호가 올바르지 않습니다.";
 
     private LottoConstant() {
     }
