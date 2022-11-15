40a415507f64a48a5e2518a9289b7dd0ab383fac
5uhwann <5uhwann27@gmail.com>
2022-11-15 15:51:24 +0900

    refactor(출력): 에러메시지 관리 클래스 구현

---
+++ src/main/java/lotto/utils/message/ErrorMessage.java
@@ -0,0 +1,10 @@
+package lotto.utils.message;
+
+public class ErrorMessage {
+
+    public static String NOT_OVER_MINIMUM_PAYMENT_AMOUNT = "[ERROR] 최소 로또 구매 금액은 1000원입니다.";
+    public static String UNSATISFIED_PAYMENT_AMOUNT = "[ERROR] 로또 구매 금액은 1000원 단위의 금액이어야 합니다.";
+    public static String UNSATISFIED_WINNING_NUMBER = "[ERROR] 당첨 번호는 1~45의 서로다른 6개 숫자를 입력해야합니다.";
+    public static String UNSATISFIED_LOTTO_NUMBER = "[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다.";
+
+}
