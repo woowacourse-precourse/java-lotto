0cae7de3850d9202a0e190b2d63e19fd01248391
Hiluxy <sunset.lhj@gmail.com>
2022-11-15 22:32:37 +0900

    refactor 당첨 번호 중복 검증

--- src/main/java/lotto/ValidInput.java
+++ src/main/java/lotto/ValidInput.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.Objects;
 
 public class ValidInput {
 
@@ -18,11 +19,21 @@ public class ValidInput {
 
     public void validIsInputInRange(List<Integer> number) {
         if (!((number < LottoEnum.END_NUM.getValue()) && (number > LottoEnum.START_NUM.getValue()))) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호가 " + LottoEnum.START_NUM.getValue() + "과 " +
-                LottoEnum.END_NUM.getValue() + "사이에 있지 않습니다!");
+            throw new IllegalArgumentException(
+                "[ERROR] 로또 번호가 " + LottoEnum.START_NUM.getValue() + "과 " +
+                    LottoEnum.END_NUM.getValue() + "사이에 있지 않습니다!");
+        }
     }
 
     public void validInputHasDuplicatedNumber(List<Integer> numbers) {
-
+        for (int count1 = 0; count1 < numbers.size(); count1++) {
+            for (int count2 = count1 + 1; count2 < numbers.size(); count2++) {
+                if (Objects.equals(numbers.get(count1), numbers.get(count2))) {
+                    throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 존재합니다!");
+                }
+            }
+        }
     }
+
 }
+
