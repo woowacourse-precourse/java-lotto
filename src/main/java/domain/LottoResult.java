a2d9b31b20dda60eaf2ce09bfd0ce14670b24939
doehy <dbehxks02@naver.com>
2022-11-15 12:22:19 +0900

    feat : 당첨 결과를 알려주는 문구 계산

--- src/main/java/domain/LottoResult.java
+++ src/main/java/domain/LottoResult.java
@@ -8,6 +8,7 @@ public class LottoResult {
     private final List<Integer> lottoStatistics;
     private final int money;
     private int winningMoney = 0;
+    private String winningOutput = "";
     private static final Map<Integer, Integer> winningAmount;
     static {
         winningAmount = new HashMap<>();
@@ -16,6 +17,15 @@ public class LottoResult {
         winningAmount.put(2, 1500000);
         winningAmount.put(3, 30000000);
         winningAmount.put(4, 2000000000);
+    }
+    private static final Map<Integer,String> resultView;
+    static {
+        resultView = new HashMap<>();
+        resultView.put(0, "3개 일치 (5,000원) - ");
+        resultView.put(1, "4개 일치 (50,000원) - ");
+        resultView.put(2, "5개 일치 (1,500,000원) - ");
+        resultView.put(3, "5개 일치, 보너스 볼 일치 (30,000,000원) - ");
+        resultView.put(4, "6개 일치 (2,000,000,000원) - ");
     }
 
     public LottoResult(List<Integer> lottoStatistics, int money) {
@@ -27,7 +37,7 @@ public class LottoResult {
         for (int i = 0; i < 5; i++) {
             int number = lottoStatistics.get(i);
             addAmount(winningAmount.get(i),number);
-
+            addOutput(resultView.get(i),number);
         }
 
     }
@@ -35,5 +45,10 @@ public class LottoResult {
     private void addAmount(int amount, int count) {
         int sum = amount * count;
         winningMoney += sum;
+    }
+
+    private void addOutput(String phrase, int count) {
+        String addingPhrase = phrase + changeType(count) + "개\n";
+        winningOutput += addingPhrase;
     }
 }
