630596312a883fb74f2f36479af96f71501e38dd
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-16 00:00:18 +0900

    test(LottoStoreTest): 주석 제거

--- src/test/java/lotto/LottoStoreTest.java
+++ src/test/java/lotto/LottoStoreTest.java
@@ -4,8 +4,6 @@ import lotto.domain.LottoStore;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoStoreTest {
