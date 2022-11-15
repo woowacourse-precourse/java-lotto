cfe2968cc2bf5a838762c8fc9ee5759a952504a3
aossuper7 <aossuper7@naver.com>
2022-11-15 13:34:55 +0900

    refactor: enum Result에 {} 제거, 변수에 final 적용

--- src/main/java/lotto/Constant/Result.java
+++ src/main/java/lotto/Constant/Result.java
@@ -5,29 +5,19 @@ import java.util.function.IntToLongFunction;
 
 public enum Result {
     THREE("3", value -> value * 5_000L,
-        count -> {
-            System.out.println("3개 일치 (5,000원) - " + count + "개");
-        }),
+        count -> System.out.println("3개 일치 (5,000원) - " + count + "개")),
     FOUR("4", value -> value * 50_000L,
-        count -> {
-            System.out.println("4개 일치 (50,000원) - " + count + "개");
-        }),
+        count -> System.out.println("4개 일치 (50,000원) - " + count + "개")),
     FIVE("5", value -> value * 1_500_000L,
-        count -> {
-            System.out.println("5개 일치 (1,500,000원) - " + count + "개");
-        }),
+        count -> System.out.println("5개 일치 (1,500,000원) - " + count + "개")),
     FIVE_B("5B", value -> value * 30_000_000L,
-        count -> {
-            System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + count + "개");
-        }),
+        count -> System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + count + "개")),
     SIX("6", value -> value * 2_000_000_000L,
-        count -> {
-            System.out.println("6개 일치 (2,000,000,000원) - " + count + "개");
-        });
+        count -> System.out.println("6개 일치 (2,000,000,000원) - " + count + "개"));
 
-    private String matchNumber;
-    private IntToLongFunction countingMoney;
-    private IntConsumer print;
+    private final String matchNumber;
+    private final IntToLongFunction countingMoney;
+    private final IntConsumer print;
 
     Result(String matchNumber, IntToLongFunction countingMoney, IntConsumer print) {
         this.matchNumber = matchNumber;
