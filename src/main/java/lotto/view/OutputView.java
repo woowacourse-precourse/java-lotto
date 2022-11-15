7b667dbe8e99d2a267549a784541cee67cc422e7
Jenn Kim <grdd0419@gmail.com>
2022-11-15 09:51:43 +0900

    refactor: 출력 문구 오타 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,8 +6,8 @@ public class OutputView {
 
     private static final String READ_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String READ_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String READ_BONUS_NUMBER_MESSAGE = "구입금액을 입력해 주세요.";
-    
+    private static final String READ_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
     private static final String PRINT_PURCHASE_NUM_MESSAGE = "개를 구매했습니다.";
 
     public static String getReadInputMessage() {
@@ -15,11 +15,11 @@ public class OutputView {
     }
 
     public static String getReadWinningNumbersMessage() {
-        return READ_WINNING_NUMBERS_MESSAGE;
+        return LINE_FEED + READ_WINNING_NUMBERS_MESSAGE;
     }
 
     public static String getReadBonusNumberMessage() {
-        return READ_BONUS_NUMBER_MESSAGE;
+        return LINE_FEED + READ_BONUS_NUMBER_MESSAGE;
     }
 
     public static void printPurchaseNum(int number) {
