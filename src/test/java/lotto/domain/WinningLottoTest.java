ee2638fbf5cea098bfaf5b53f3383ebb57f25c40
taekwon-dev <taekwon_dev@naver.com>
2022-11-15 10:50:01 +0900

    fix: (WinningLottoTest) 로또 생성 시 불변 타입 리스트로 생성
    - 테스트 일관성 고려

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -4,7 +4,7 @@ import lotto.domain.exception.IllegalArgumentException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -12,7 +12,7 @@ public class WinningLottoTest {
     @DisplayName("당첨 로또 번호 리스트가 보너스 번호를 포함하고 있는 경우 예외가 발생한다.")
     @Test
     void createWinningLottoByWinningLottoContainsBonusNumber() {
-        assertThatThrownBy(() -> new WinningLotto(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)), new Bonus(1)))
+        assertThatThrownBy(() -> new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), new Bonus(1)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
