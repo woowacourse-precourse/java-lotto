6a4c0cc5ce79a43d78e8080b2f64dfc6b4a92376
Woogie95 <choisw950928@gmail.com>
2022-11-15 23:08:17 +0900

    refactor: 비교하는 로직 수정, 정적 팩토리 메소드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,22 +1,51 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static lotto.Money.BLANK;
 import static lotto.LottoFactory.LOTTO_MAX_NUMBER;
 import static lotto.LottoFactory.LOTTO_MIN_NUMBER;
-import static lotto.Money.BLANK;
 import static lotto.WinningNumber.REPEAT_INITIAL_VALUE;
 
 public class Lotto {
 
+    private final static String SEPARATOR = ",";
     private static final int LOTTO_DEFAULT_LENGTH = 6;
 
     private final List<Integer> numbers;
+
+    public static Lotto from(String winningNumber) {
+        try {
+            return new Lotto(Arrays.stream(winningNumber.split(SEPARATOR))
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .collect(Collectors.toList()));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 입력한 당첨 번호가 숫자가 아닙니다.");
+        }
+    }
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
+
+    public int calculateMatchCount(Lotto lottoNumbers) {
+        return lottoNumbers.compareLottoNumbers(this.numbers);
+    }
+
+    private int compareLottoNumbers(List<Integer> numbers) {
+        return (int) numbers.stream()
+                .filter(this.numbers::contains)
+                .count();
+    }
+
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
+
 
     private void validate(List<Integer> numbers) {
         for (int i = REPEAT_INITIAL_VALUE; i < numbers.size(); i++) {
@@ -33,7 +62,7 @@ public class Lotto {
     }
 
     private void validateLottoNumberRange(int initialValue, List<Integer> numbers) {
-        if (numbers.get(initialValue) < LOTTO_MIN_NUMBER || numbers.get(initialValue) < LOTTO_MAX_NUMBER) {
+        if (numbers.get(initialValue) < LOTTO_MIN_NUMBER || numbers.get(initialValue) > LOTTO_MAX_NUMBER) {
             throw new IllegalArgumentException("[ERROR] 로또 숫자 범위를 벗어났습니다.");
         }
     }
