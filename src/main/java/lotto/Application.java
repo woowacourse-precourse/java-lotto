60802f9369857c3c5ef8645162d3c6b276aa978b
d0ri123 <lsh81550@gmail.com>
2022-11-15 20:06:09 +0900

    fix: 충돌 해결

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.Controller.LotteryShop;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LotteryShop shop = new LotteryShop();
+        shop.open();
     }
 }
--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -1,4 +1,55 @@
 package lotto;
 
+import lotto.Model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class UserTest {
+    private User user;
+
+    @BeforeEach
+    void setUp(){
+        user = new User();
+    }
+
+    @DisplayName("구매금액이 1000단위가 아니라면 예외가 발생한다.")
+    @Test
+    void isValidPrice(){
+        assertThatThrownBy(() -> user.isValidPrice(1500))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구매금액을 숫자 아닌 다른 문자로 입력하면 예외가 발생한다.")
+    @Test
+    void isNumericPrice(){
+        assertThatThrownBy(() -> user.isNumeric("1000j"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨번호가 1~46 범위 밖의 숫자라면 예외가 발생한다.")
+    @Test
+    void isOutOfRange(){
+        assertThatThrownBy(() -> user.isOutOfRange("1,2,0,3,47,8"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨번호를 6자리로 입력하지 않는다면 예외가 발생한다.")
+    @Test
+    void isSixDigit(){
+        assertThatThrownBy(()->user.isSixDigit("1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨번호를 중복해서 입력한다면 예외가 발생한다.")
+    @Test
+    void isDuplicate(){
+        assertThatThrownBy(()->user.isDuplicate("1,2,3,4,5,5"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
