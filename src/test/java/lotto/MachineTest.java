238dcc3e5d3f9c52c0b2526efa126e1d2afeb9ca
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-15 21:55:32 +0900

    test: Add test case for machine class

---
+++ src/test/java/lotto/MachineTest.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MachineTest {
+  static Machine lottoMachine;
+
+  @BeforeAll
+  static void beforeAll() {
+    lottoMachine = new Machine();
+  }
+
+  @DisplayName("티켓 판매 테스트")
+  @Test
+  void sellTicketTest() {
+    int ticketCount = 5;
+    List<Lotto> ticketList = lottoMachine.sellTicket(ticketCount);
+    assertThat(ticketList.size()).isEqualTo(ticketCount);
+  }
+
+}
