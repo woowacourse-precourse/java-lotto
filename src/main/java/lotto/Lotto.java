664dc5bf260921b6dc8c622c1f5fb018b28dca74
nahyeon cho <whskgus12@nate.com>
2022-11-15 22:52:02 +0900

    feat(lotto): 당첨 번호가 주어진 범위를 벗어났을 때
    당첨 번호가 주어진 범위를 벗어난 경우 예외 처리 하는 기능입니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,6 +8,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validLengthWinningNumber(numbers);
         duplicateWinningNumber(numbers);
+        outOfRangeWinningNumber(numbers);
         this.numbers = numbers;
     }
 
@@ -19,7 +20,14 @@ public class Lotto {
 
     // TODO: 추가 기능 구현
     private void duplicateWinningNumber(List<Integer> winningLottoNumber) {
-        if(winningLottoNumber.size()!=winningLottoNumber.stream().distinct().count())
+        if (winningLottoNumber.size() != winningLottoNumber.stream().distinct().count())
             throw new IllegalArgumentException("[ERROR] 당첨 번호에 중복이 있으면 안됩니다.");
+    }
+
+    private void outOfRangeWinningNumber(List<Integer> winningLottoNumber) {
+        for (Integer number : winningLottoNumber) {
+            if (number < 1 || number > 45)
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 }
