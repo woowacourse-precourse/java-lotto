1553a12dd5391c394150683975a6ccad215e2048
이병3 <101378867+SongJSeop@users.noreply.github.com>
2022-11-15 20:37:30 +0900

    Lotto Test 예외처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,7 +13,8 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        Set<Integer> unique_num = new HashSet<>(numbers);
+        if (numbers.size() != 6 || unique_num.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
