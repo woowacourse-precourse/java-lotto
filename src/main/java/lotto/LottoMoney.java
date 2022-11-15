304d583678f081a6bb9c8d78656ac0247c6490c4
juno-junho <ssmm0205@naver.com>
2022-11-15 20:01:31 +0900

    feat(lotto): override equals and hashcode method
    equals와 hashcode 메서드 override

--- src/main/java/lotto/LottoMoney.java
+++ src/main/java/lotto/LottoMoney.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Objects;
+
 public class LottoMoney {
 
     private static final int LOTTERY_PRICE_PER_ONE = 1_000;
@@ -27,5 +29,20 @@ public class LottoMoney {
 
     public int getMoney() {
         return money;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof LottoMoney))
+            return false;
+        LottoMoney that = (LottoMoney) o;
+        return getMoney() == that.getMoney();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getMoney());
     }
 }
