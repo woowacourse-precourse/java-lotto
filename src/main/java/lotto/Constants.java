e128c7bb2e80fa2ef85675677d187473bb8c636e
Sanmgmin <sangmins930@gmail.com>
2022-11-15 15:49:18 +0900

    feat(Constants): create Constants class
    하드코딩을 피하기 위한 로또 최소값, 최대값, 개수 상수 할당

---
+++ src/main/java/lotto/Constants.java
@@ -0,0 +1,7 @@
+package lotto;
+
+public class Constants {
+    public static final int NUM_OF_LOTTO = 6;
+    public static final int MIN_VALUE_OF_LOTTO = 1;
+    public static final int MAX_VALUE_OF_LOTTO = 45;
+}
