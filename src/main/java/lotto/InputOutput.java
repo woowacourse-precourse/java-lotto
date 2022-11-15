61fecdc91aac29b6227ec801bb27533a3144ef1f
HangSub999 <gkdtjq999@gmail.com>
2022-11-15 18:58:57 +0900

    feat: 로또 1등번호,보너스번호 입력 구현

--- src/main/java/lotto/InputOutput.java
+++ src/main/java/lotto/InputOutput.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -37,5 +38,46 @@ public class InputOutput {
     public void printLotto(List<Lotto> buyerLotto) {
         Stream<Lotto> stream = buyerLotto.stream();
         stream.forEach(Lotto::printLotto);
+    }
+
+    public List<Integer> getWinnigLotto() {
+        String winnigLottoString = Console.readLine();
+
+        String[] split = winnigLottoString.split(",");
+
+        return arrayStringToIntList(split);
+    }
+
+    private List<Integer> arrayStringToIntList(String[] split) {
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String singleNumString : split) {
+            int num = stringToIntAndCheckError(winningNumbers, singleNumString);
+            winningNumbers.add(num);
+        }
+        return winningNumbers;
+    }
+
+    private int stringToIntAndCheckError(List<Integer> winningNumbers, String string) {
+        int num = validateStringToInt(string);
+
+        checkNumInRange(num);
+
+        checkDuplication(winningNumbers, num);
+        return num;
+    }
+
+    private void checkNumInRange(int num) {
+        if (num < 1 || num > 45)
+            throw new IllegalArgumentException("[ERROR] 1~45의 범위에 있는 숫자를 입력해주세요.");
+    }
+
+    private void checkDuplication(List<Integer> winningNumbers, int num) {
+        if (winningNumbers.contains(num))
+            throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자를 입력해주세요.");
+    }
+
+    public int getBonusNumber(List<Integer> winningNumber) {
+        String bonusNumString = Console.readLine();
+        return stringToIntAndCheckError(winningNumber, bonusNumString);
     }
 }
