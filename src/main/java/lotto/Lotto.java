a41cb862f89e2ae70a1757ec88a2d13ccf1bc988
eunseo <qnrqjrm03@naver.com>
2022-11-15 16:42:29 +0900

    feat : 로또 생성시 유효성 검증

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,17 +6,37 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static lotto.Message.*;
+
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkDuplicatedNumber(numbers);
+        checkRangeLottoNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MORE_SIX_LOTTO.getMessage());
+        }
+    }
+
+    private void checkDuplicatedNumber(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(ERROR_DUPLICATED_LOTTO.getMessage());
+        }
+    }
+
+    public void checkRangeLottoNumber(List<Integer> numbers) {
+        final int start = 1;
+        final int end = 45;
+        for (int number : numbers) {
+            if (!(number >= start || number <= end)) {
+                throw new IllegalArgumentException(BETWEEN_NUMBER.getMessage());
+            }
         }
     }
 
