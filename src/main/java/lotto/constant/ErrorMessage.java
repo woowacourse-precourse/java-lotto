3b0dadb2e2f279dcb74339ba270ed08e20960fa3
GyuJeGal <lkjdee0329@gmail.com>
2022-11-15 16:37:32 +0900

    feat(ErrorMessage): 에러 메세지 나열 상수 타입의 에러 메세지 나열

---
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -0,0 +1,10 @@
+package lotto.constant;
+
+public class ErrorMessage {
+    public static final String PURCHASE_AMOUNT_TYPE_ERROR = "[ERROR] 구입 금액은 숫자만 입력 가능합니다.";
+    public static final String PURCHASE_AMOUNT_RANGE_ERROR = "[ERROR] 구입 금액은 1000원 이상입니다.";
+    public static final String PURCHASE_AMOUNT_UNIT_ERROR = "[ERROR] 구입 금액은 1000원 단위입니다.";
+    public static final String WINNING_NUMBER_TYPE_ERROR = "[ERROR] 당첨 번호는 숫자와 콤마(,)로 이루어져야 합니다.";
+    public static final String WINNING_NUMBER_RANGE_ERROR = "[ERROR] 당첨 번호는 1 ~ 45 사이의 양수입니다.";
+    public static final String WINNING_NUMBER_SIZE_ERROR = "[ERROR] 당첨 번호는 6개를 입력해야 합니다.";
+}
