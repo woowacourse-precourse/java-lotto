42f7186209a46e1b8e8a609af9f5522e046d9fc2
seonpilKim <ksp970306@gmail.com>
2022-11-15 19:38:35 +0900

    Feat: Reader 클래스 추가 및 입력 역할 분리

---
+++ src/main/java/lotto/domain/Reader.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import static lotto.Constants.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import lotto.ErrorMessage;
+
+public class Reader {
+
+	public static Integer readPurchaseAmount() {
+		try {
+			return Integer.valueOf(Console.readLine());
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(ErrorMessage.isInvalidNumber());
+		}
+	}
+
+	public static List<Integer> readWinningNumbers() {
+		try {
+			return Arrays.stream(Console.readLine()
+							.split(COMMA))
+					.map(Integer::valueOf)
+					.collect(Collectors.toList());
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(ErrorMessage.isInvalidNumber());
+		}
+	}
+
+	public static Integer readBonusNumber() {
+		try {
+			return Integer.valueOf(Console.readLine());
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(ErrorMessage.isInvalidNumber());
+		}
+	}
+
+}
