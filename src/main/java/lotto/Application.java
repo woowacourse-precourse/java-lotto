8b34a5a447d7dec3fb5f3749f35070642b6cf2ee
minje <alswp0910@naver.com>
2022-11-15 21:47:19 +0900

    fix: 예외 테스트 오류 수정
    예외를 전파해서 assert가 인식할 수 있게 try-catch문을 사용하도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,7 @@ package lotto;
 import lotto.domain.Computer;
 import lotto.domain.Lucky;
 import lotto.domain.User;
+import lotto.Exceptions;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,22 +16,27 @@ public class Application {
     }
 
     public static void game() {
-        User user = new User();
-        int price = user.inputPrice();
-        int count = user.countLotto(price);
-        List<List<Integer>> userLotto = new ArrayList<>(user.showLotto(count));
+        try {
+            User user = new User();
+            int price = user.inputPrice();
+            int count = user.countLotto(price);
+            List<List<Integer>> userLotto = new ArrayList<>(user.showLotto(count));
 
-        Computer com = new Computer();
-        String str = com.inputNumbers();
-        String [] arr = com.splitNumbers(str);
-        List<Integer> list = new ArrayList<>(com.checkNumbers(arr));
-        com.checkDuplication(list);
-        int bns = com.bonusNumber();
+            Computer com = new Computer();
+            String str = com.inputNumbers();
+            String[] arr = com.splitNumbers(str);
+            List<Integer> list = new ArrayList<>(com.checkNumbers(arr));
+            com.checkDuplication(list);
+            int bns = com.bonusNumber();
 
-        Lucky lucky = new Lucky();
-        List<Integer> res = new ArrayList<>(lucky.compareNumbers(userLotto, list, bns));
-        HashMap<Integer, Integer> map = new HashMap<>(lucky.compareResult(res));
-        lucky.showResult(map);
-        lucky.showProfit(lucky.profitResult(map, price));
+            Lucky lucky = new Lucky();
+            List<Integer> res = new ArrayList<>(lucky.compareNumbers(userLotto, list, bns));
+            HashMap<Integer, Integer> map = new HashMap<>(lucky.compareResult(res));
+            lucky.showResult(map);
+            lucky.showProfit(lucky.profitResult(map, price));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
