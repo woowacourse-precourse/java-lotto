e3f9f576bcc70e76eb7ec37d1cbe1c0d91d5cae7
leesangjin <tkdwls0628@gmail.com>
2022-11-15 17:56:51 +0900

    style(User): 금액 입력 메소드 이름 변경

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class User {
-    public int inputOfAmount(){
+    public int inputOfMoneyAmount(){
         String userInput = Console.readLine();
         MoneyAmountValidator.validateInputType(userInput);
         int moneyAmount = Integer.parseInt(userInput);
