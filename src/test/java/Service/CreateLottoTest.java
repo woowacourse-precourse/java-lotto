610438fe36b7f583b5a265b9d7e86b14650695ac
p <gustjr2332@naver.com>
2022-11-15 20:10:48 +0900

    test(CreateLottoTest) : DisplayName 추가

--- src/test/java/Service/CreateLottoTest.java
+++ src/test/java/Service/CreateLottoTest.java
@@ -1,11 +1,14 @@
 package Service;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 class CreateLottoTest {
+
+    @DisplayName("8000원 입력, 로또 8장 발행")
     @Test
     void checkRandomLottoSize(){
         List<List<Integer>> randomLottoNumbers = CreateRandomLotto.createRandomLottoNumber(8000);
