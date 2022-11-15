561d1c02f00908a4bf2045f0b5868198fc496780
jsc1014 <wotlr5209@gmail.com>
2022-11-15 19:45:07 +0900

    feat: 각 등수가 몇번 당첨됐는지 출력

--- src/main/java/lotto/UserInterface.java
+++ src/main/java/lotto/UserInterface.java
@@ -70,4 +70,16 @@ public class UserInterface {
 
         return bonusNumber;
     }
+
+    private static String messageWinStatstic(int index,int numberOfLotto){
+        List<String> eachMessageWinStatistic = List.of
+                ("3개 일치 (5,000원) - " + numberOfLotto + "개",
+                        "4개 일치 (50,000원) - " + numberOfLotto + "개",
+                        "5개 일치 (1,500,000원) - " + numberOfLotto + "개",
+                        "5개 일치, 보너스 볼 일치 (30,000,000원) - " + numberOfLotto + "개",
+                        "6개 일치 (2,000,000,000원) - " + numberOfLotto + "개");
+
+        return eachMessageWinStatistic.get(index);
+    }
+
 }
