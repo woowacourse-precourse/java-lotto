344ae4ea253fd8a94e5fdd77e2a69163e648e67d
baekeunsun <eunsun5460@gmail.com>
2022-11-15 19:45:12 +0900

    test(controller/Yield): 수익률 계산 테스트 코드

--- src/main/java/lotto/controller/Yield.java
+++ src/main/java/lotto/controller/Yield.java
@@ -11,12 +11,12 @@ public class Yield {
         OutputView.OutputYield(yield);
     }
 
-    public int SumWinLotto(int fifth, int fourth, int third, int second, int first) {
+    public static int SumWinLotto(int fifth, int fourth, int third, int second, int first) {
         int totalRevenue = (fifth*5000) + (fourth*50000) + (third*1500000) + (second*30000000) + (first*2000000000);
         return totalRevenue;
     }
 
-    private double YieldLotto(int totalRevenue, int userPrice) {
+    public static double YieldLotto(int totalRevenue, int userPrice) {
         return Math.round((totalRevenue / (double) userPrice * 100) * 10) / 10.0;
     }
 }
---
+++ src/test/java/lotto/controller/YieldTest.java
@@ -0,0 +1,23 @@
+package lotto.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class YieldTest {
+
+    @DisplayName("로또로 번 돈 계산")
+    @Test
+    public void SumMoneyTest() {
+        assertThat((Yield.SumWinLotto(1,0,0,0,0) == 5000));
+    }
+
+    @DisplayName("수익률 계산")
+    @Test
+    public void YieldTest() {
+        assertThat((Yield.YieldLotto(3000,9000) == 3.33));
+    }
+
+}
