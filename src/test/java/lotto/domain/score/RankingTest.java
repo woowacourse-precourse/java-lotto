632a56812fe30479c290ad4eb999175091eec677
JaehongDev <println@kakao.com>
2022-11-15 20:29:16 +0900

    refactor (RankingTest) : 점수를 등수로 변환하는 테스코드 추가

--- src/test/java/lotto/domain/score/RankingTest.java
+++ src/test/java/lotto/domain/score/RankingTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class RankingTest {
@@ -30,5 +32,23 @@ class RankingTest {
         var actual = stringBuilder.toString()
                 .trim();
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 점수에따라_스코어를_반환합니다() {
+        assertThat(Ranking.generateRankByScore(new Score(6, 0))).isEqualTo(Ranking.FIRST_WINNING);
+        assertThat(Ranking.generateRankByScore(new Score(5, 1))).isEqualTo(Ranking.SECOND_WINNING);
+        assertThat(Ranking.generateRankByScore(new Score(5, 0))).isEqualTo(Ranking.THIRD_WINNING);
+        assertThat(Ranking.generateRankByScore(new Score(4, 1))).isEqualTo(Ranking.FOURTH_WINNING);
+        assertThat(Ranking.generateRankByScore(new Score(4, 0))).isEqualTo(Ranking.FOURTH_WINNING);
+        assertThat(Ranking.generateRankByScore(new Score(3, 1))).isEqualTo(Ranking.FIFTH_WINNING);
+        assertThat(Ranking.generateRankByScore(new Score(3, 0))).isEqualTo(Ranking.FIFTH_WINNING);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:0", "0:1", "1:0", "1:1", "2:0", "2:1"}, delimiterString = ":")
+    void 해당점수는_낙첨입니다(final Integer normal, final Integer bonus) {
+        assertThat(Ranking.generateRankByScore(new Score(normal, bonus))).isEqualTo(Ranking.NOT_WINNING);
+
     }
 }
