60e9be7df8210b62b0009efdfdb36802f654a4cc
KimRiun <kimriun27@gmail.com>
2022-11-15 23:46:55 +0900

    feat: 에러 메세지 작성

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -1,7 +1,8 @@
 package lotto;
 
 public enum ErrorMessage {
-    PRICE("[ERROR] 1000으로 나누어 떨어지는 양의 정수로 입력하세요.");
+    PRICE("[ERROR] 1000으로 나누어 떨어지는 양의 정수로 입력하세요."),
+    LOTTO("[ERROR] 1 ~ 45 범위의 서로 다른 6개의 숫자를 입력하세요. (ex) 13, 14, 16, 38, 42, 45");
 
     String message;
     private ErrorMessage(String message) {
