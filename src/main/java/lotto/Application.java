68eb203a01491a0f090f4d872626799dee2cc2ca
Minji Kim <minji1289@naver.com>
2022-11-15 23:14:29 +0900

    사용자 입력 받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        String money = Console.readLine(); //로또 구입 금액 입력
+
+        List<Integer> numbers=new ArrayList<>(); //로또 당첨 번호 입력받기
+
+        for (int i=0; i<5; i++){
+            int input = Integer.parseInt(Console.readLine());
+            numbers.add(input);
+        }
+
+        int bonus = Integer.parseInt(Console.readLine()); //보너스 번호 입력받기
+
+
         // TODO: 프로그램 구현
     }
 }
