b59055198d34cc5673016cffff90545ef755896e
ckyeon <qsky8520@gmail.com>
2022-11-15 21:02:28 +0900

    refactor(LottoShopView): make methods static

--- src/main/java/lotto/ui/LottoShopView.java
+++ src/main/java/lotto/ui/LottoShopView.java
@@ -1,15 +1,16 @@
 package lotto.ui;
 
 import java.util.List;
+import lotto.domain.Lotto;
 
 public class LottoShopView {
 
-    public void printLottoQuantity() {
+    public static void printLottoQuantity(List<Lotto> lottos) {
         int lottoQuantity = lottos.size();
         System.out.println(lottoQuantity + "개를 구매했습니다.");
     }
 
-    public void printLottos() {
+    public static void printLottos(List<Lotto> lottos) {
         lottos.forEach(lotto -> {
             List<Integer> lottoNumbers = lotto.getNumbers();
             System.out.println(lottoNumbers);
