8f108c7c29abee94d7065556405aad4240af8d6a
baeseohyeon <formnarago@naver.com>
2022-11-15 13:53:33 +0900

    feat: 로또를 진행하는 컨트롤러 코드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import static lotto.util.NumberParser.stringArrayToIntegerList;
+import static lotto.util.NumberParser.stringToInteger;
+
+import lotto.model.Lotto;
+import lotto.model.LottoMachine;
+import lotto.view.View;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            View view = new View();
+            view.printInputMoneyMenu();
+            LottoMachine lottoMachine = new LottoMachine(stringToInteger(view.readLine()));
+            view.printLottoCount(lottoMachine.getPurchasedLottoCount());
+            view.print(lottoMachine.lottosToString());
+            view.printInputWinningNumbersMenu();
+            Lotto winningLotto = new Lotto(stringArrayToIntegerList(view.readLine().split(",")));
+            view.printInputBonusNumberMenu();
+            int bonusNumber = stringToInteger(view.readLine());
+            winningLotto.validateBonusNumber(bonusNumber);
+            lottoMachine.checkLottosRank(winningLotto, bonusNumber);
+            view.printRankStatistic(lottoMachine.rankStatisticsToString());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
