9968822a7745c366be0cc724bdc0b5c54a8d6072
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-15 14:36:51 +0900

    refactor(main/Print): 결과 출력 format 바꿈

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -1,36 +1,39 @@
 package lotto;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
+import static lotto.Instance.*;
+
 public class Print {
-    static void getPurchaseAmount(){
+    static void getPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    static void userLottos(List<Lotto> lottos, int purchaseAmount){
+    static void userLottos(List<Lotto> lottos, int purchaseAmount) {
         int buyAmount = Operation.buyAmount(purchaseAmount);
         System.out.printf("%d개를 구매했습니다.\n", buyAmount);
-        for (Lotto lotto: lottos) {
+        for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
         }
     }
 
-    static void getWinningNumbers(){
+    static void getWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
-    static void getBonusNumber(){
+    static void getBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
-    static void result(double earningRate, Match match){
+    static void result(double earningRate, Match match) {
+        DecimalFormat formatter = new DecimalFormat("###,###");
         System.out.println("당첨 통계\n---");
-        System.out.printf("3개 일치 (5,000원) - %d개\n", match.match3);
-        System.out.printf("4개 일치 (50,000원) - %d개\n", match.match4);
-        System.out.printf("5개 일치 (1,500,000원) - %d개\n", match.match5);
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", match.match5Bonus);
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", match.match6);
+        System.out.printf("3개 일치 (" + formatter.format(AMOUNT_THREE) + "원) - %d개\n", match.MATCH_THREE);
+        System.out.printf("4개 일치 (" + formatter.format(AMOUNT_FOUR) + "원) - %d개\n", match.MATCH_FOUR);
+        System.out.printf("5개 일치 (" + formatter.format(AMOUNT_FIVE) + "원) - %d개\n", match.MATCH_FIVE);
+        System.out.printf("5개 일치, 보너스 볼 일치 (" + formatter.format(AMOUNT_FIVE_BONUS) + "원) - %d개\n", match.MATCH_FIVE_BONUS);
+        System.out.printf("6개 일치 (" + formatter.format(AMOUNT_SIX) + "원) - %d개\n", match.MATCH_SIX);
         System.out.printf("총 수익률은 %.1f%%입니다.\n", earningRate);
     }
-
 }
