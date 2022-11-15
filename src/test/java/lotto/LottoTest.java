c7f48f5eca0b5102db4985084e81f50c8518e8c4
swjeong98 <tree5678@konkuk.ac.kr>
2022-11-15 11:33:26 +0900

    test(Lotto) : 로또 1개를 구성하는 번호 6개 출력이 제대로 되는지 확인하는 테스트 구성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
-class LottoTest {
+class LottoTest extends NsTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -23,5 +24,17 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호가 정해진 형식으로 출력되는지 확인한다.(생성될때 이미 오름차순 정렬을 해주어서, 정렬은 보장된 상태)")
+    @Test
+    void printLottoByAscendingOrder(){
+        Lotto lotto = new Lotto(List.of(2,9,16,22,25,41));
+        lotto.Print();
+        assertThat(output()).contains("[2, 9, 16, 22, 25, 41]");
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
     // 아래에 추가 테스트 작성 가능
 }
