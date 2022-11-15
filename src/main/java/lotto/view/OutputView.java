60d8d8523e79b7fc9097d6cdf963ce17f9c78ee2
eom-tae-in <eti0728@daum.net>
2022-11-15 19:52:51 +0900

    feat(OutputView): printUserLottoQuantity() -> 유저가 구매한 로또의 수를 출력해주는 메서드

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,4 +10,8 @@ public class OutputView {
     public void printBuyingPriceQuestion() {
         System.out.println("구입 금액을 입력해 주세요.");
     }
+    public void printUserLottoQuantity(long lottoQuantity) {
+        System.out.println();
+        System.out.println(lottoQuantity + "개를 구매했습니다.");
+    }
 }
