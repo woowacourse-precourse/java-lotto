15580eeb5225f8e983616618988a3c8c6ee4e974
jijongkwon <help3451@naver.com>
2022-11-15 18:17:22 +0900

    test : 메서드 파라미터 추가

--- src/test/java/lotto/domain/PurchasingLottoListTest.java
+++ src/test/java/lotto/domain/PurchasingLottoListTest.java
@@ -10,7 +10,7 @@ class PurchasingLottoListTest {
     @DisplayName("구입 금액이 1000원으로 나누어 떨어지지 않은면 예외가 발생한다.")
     @Test
     void moneyToTicket() {
-        assertThatThrownBy(() -> new PurchasingLottoList().moneyToTicket(13400))
+        assertThatThrownBy(() -> new PurchasingLottoList(13400).moneyToTicket(13400))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
