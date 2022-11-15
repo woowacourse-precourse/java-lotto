6a2ce79570134485b9fa0f164853c39dd1914fe8
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-15 19:57:32 +0900

    type:refactor 10~45까지의 로또 번호를 입력했을 때 에러 메시지를 출력하지 않도록 정규표현식 수정

--- src/main/java/lotto/controller/Error.java
+++ src/main/java/lotto/controller/Error.java
@@ -12,7 +12,7 @@ public class Error {
     }
 
     static void isValidInput(String input) {
-        if (!input.matches("[0-9],[0-9],[0-9],[0-9],[0-9],[0-9]")) {
+        if (!input.matches("[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+")) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자를 쉼표만으로 구분하여 입력해야 합니다.");
         }
     }
