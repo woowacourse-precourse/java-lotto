6ab46a75e0510d1d303039c765aa1dad8cbc6806
이지혁 <olzlgur@naver.com>
2022-11-15 23:34:40 +0900

    feat: 결과 출력 메서드 구현

--- src/main/java/lotto/printer/Printer.java
+++ src/main/java/lotto/printer/Printer.java
@@ -1,6 +1,17 @@
 package lotto.printer;
 
+import java.util.HashMap;
+
 public class Printer {
+
+    private static final HashMap<Integer, String> prize = new HashMap<>() {{
+        put(3, "5,000");
+        put(4, "50,000");
+        put(5, "1,500,000");
+        put(6, "2,000,000,000");
+        put(7, "30,000,000");
+    }};
+
     private static final int FIVE_MATCH_BALL = 5;
     private static final String PURCHASE_REQUEST_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String PURCHASE_MESSAGE = "%d개를 구매했습니다.";
@@ -10,8 +21,8 @@ public class Printer {
     private static final String DIVIDER_LINE = "---";
     private static final String MATCH_NUMBER_RESULT = "%d개 일치";
     private static final String MATCH_BONUS_BALL_MESSAGE = ", 보너스 볼 일치";
-    private static final String MATCH_RESULT = " (%d원) - %d개\n";
-    private static final String EARNING_RATE_MESSAGE = "총 수익률은 %.1f입니다.";
+    private static final String MATCH_RESULT = " - %d개\n";
+    private static final String EARNING_RATE_MESSAGE = "총 수익률은 %.1f"+"%%"+"입니다.";
 
     public static void requestPurchase() {
         System.out.println(PURCHASE_REQUEST_MESSAGE);
@@ -19,6 +30,7 @@ public class Printer {
 
     public static void printNumberOfTickets(int numberOfTickets) {
         System.out.printf(PURCHASE_MESSAGE, numberOfTickets);
+        System.out.println();
     }
 
     public static void requestWinningNumber() {
@@ -29,5 +41,17 @@ public class Printer {
         System.out.println(BONUS_NUMBER_REQUEST_MESSAGE);
     }
 
+    public static void printResultMessage() {
+        System.out.println(WINNING_STATISTICS_MESSAGE);
+        System.out.println(DIVIDER_LINE);
+    }
 
+    public static void printResult(String message, Integer count) {
+        System.out.print(message);
+        System.out.printf(MATCH_RESULT, count);
+    }
+
+    public static void printEarningRate(Float money) {
+        System.out.printf(EARNING_RATE_MESSAGE, money);
+    }
 }
