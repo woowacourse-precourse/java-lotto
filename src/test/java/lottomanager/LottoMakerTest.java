d531ab449ac09aafac0c88298963513095f58cf8
CJY-kor <vivo_reprise@naver.com>
2022-11-15 22:48:25 +0900

    feat : comparebonus 기능을 test하다.

--- src/test/java/lottomanager/LottoMakerTest.java
+++ src/test/java/lottomanager/LottoMakerTest.java
@@ -12,12 +12,20 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class LottoMakerTest {
 
     private final LottoMaker lottoMaker = new LottoMaker();
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @DisplayName("로또 번호중 몇개가 같은 지 반환한다.")
     @Test
     void checkCompareLotto() {
         List<Integer> winningLotto = List.of(1, 2, 3, 4, 5, 6);
         List<Integer> payedLotto = List.of(1, 2, 3, 4, 5, 7);
         assertEquals(lottoMaker.compareLotto(winningLotto,payedLotto),5);
+    }
+
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void checkCompareBonus() {
+        List<Integer> winningLotto = List.of(1, 2, 3, 4, 5, 6);
+        int bonus = 5;
+        assertEquals(lottoMaker.compareBonus(bonus, winningLotto),true);
     }
 
 
