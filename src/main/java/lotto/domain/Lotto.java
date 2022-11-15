d20e6cca56d19c24676cbdf034ff04a89b1e3e8d
Miseong Kim <miseongk@naver.com>
2022-11-15 19:23:44 +0900

    refactor(lotto): remove validation method

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,28 +3,14 @@ package lotto.domain;
 import lotto.view.OutputView;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        Set<Integer> check = new HashSet<>(numbers);
-
-        if (numbers.size() != check.size()) {
-            throw new IllegalArgumentException();
-        }
-
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
     }
 
     public void printLottoNumbers() {
