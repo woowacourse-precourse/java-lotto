d85979903692132fe37bd89ab8ceebb9834e4909
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-11-15 16:27:05 +0900

    feat: 에러 메세지 처리를 위한 enum 클래스 추가

--- src/main/java/lotto/ErrorCode.java
+++ src/main/java/lotto/ErrorCode.java
@@ -3,7 +3,8 @@ package lotto;
 public enum ErrorCode {
     INPUT_NOT_NUMBER("입력값이 숫자가 아닙니다."),
     NOT_UNIQUE_NUMBERS("중복된 숫자가 존재합니다."),
-    INVALID_SIZE("로또는 번호는 6자리여야 합니다.")
+    INVALID_SIZE("로또는 번호는 6자리여야 합니다."),
+    OUT_OF_RANGE("로또 번호는 1에서 45 사이의 숫자여야 합니다.")
     ;
 
     private String message;
