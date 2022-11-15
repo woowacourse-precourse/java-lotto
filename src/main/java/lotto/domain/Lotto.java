bd07656a93562cd021913f271c2987d364293174
hongsikcho <jjdd9758@naver.com>
2022-11-15 15:30:43 +0900

    fix :5 Lotto생성 시 예외메세지 없는 버그 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.status.ExceptionStatus;
 import lotto.status.NumberStatus;
 
 import java.util.HashSet;
@@ -12,12 +13,21 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         checkDuplicate(numbers);
+        checkBoundary(numbers);
         this.numbers = numbers;
+    }
+
+    private void checkBoundary(List<Integer> numbers) {
+        for(Integer number : numbers){
+            if(number > NumberStatus.MAX_LOTTO_NUMBER.getNumber() || number < NumberStatus.MIN_LOTTO_NUMBER.getNumber()){
+               throw new IllegalArgumentException(ExceptionStatus.NOT_BOUNDARY_NUMBER.getText());
+            }
+        }
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != NumberStatus.TOTAL_NUMBER_OF_LOTTO.getNumber()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ExceptionStatus.NOT_CORRECT_SHAPE.getText());
         }
     }
 
@@ -27,7 +37,7 @@ public class Lotto {
             checkNumber.add(number);
         }
         if (checkNumber.size() != numbers.size()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ExceptionStatus.IS_DUPLICATED.getText());
         }
     }
 
