59185de4db58bc4a1aecaf30d66facaa27ae0914
hhiyeon <hhyyeon0214@gmail.com>
2022-11-15 12:30:15 +0900

    feat: winning-split string number to integer list

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -3,7 +3,9 @@ package lotto.domain;
 import lotto.utils.Error;
 import lotto.utils.ErrorException;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Winning {
     private List<Integer> winningNumbers;
@@ -21,5 +23,11 @@ public class Winning {
         if (!inputNumbers.replace(",", "").chars().allMatch(Character::isDigit)) {
             throw new ErrorException(Error.IS_NOT_NUMERIC.toString());
         }
+    }
+
+    private List<Integer> splitNumbers(String inputNumbers) {
+        return Arrays.stream(inputNumbers.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
