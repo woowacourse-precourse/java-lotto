26fa76b320351be5f9b8badc359445ecd910c1f2
lacram <chemin9898@gmail.com>
2022-11-15 21:02:40 +0900

    feat: 당첨 결과를 계산하는 기능 추가

--- src/main/java/lotto/LottoMatch.java
+++ src/main/java/lotto/LottoMatch.java
@@ -1,15 +1,22 @@
 package lotto;
 
 public enum LottoMatch {
-    THREE("3개 일치", 5000), FOUR("4개 일치", 50_000),
-    FIVE("5개 일치", 1_500_000), FIVE_BONUS("5개 일치, 보너스 볼 일치", 30_000_000), SIX("6개 일치", 2_000_000_000);
+    THREE(0, "3개 일치", 5000), FOUR(1, "4개 일치", 50_000),
+    FIVE(2,"5개 일치", 1_500_000), FIVE_BONUS(3,"5개 일치, 보너스 볼 일치", 30_000_000),
+    SIX(4,"6개 일치", 2_000_000_000);
 
+    private int idx;
     private String name;
-    private long value;
+    private int value;
 
-    LottoMatch(String name, int value) {
+    LottoMatch(int idx, String name, int value) {
+        this.idx = idx;
         this.name = name;
         this.value = value;
+    }
+
+    public int getIdx() {
+        return idx;
     }
 
     public String getName() {
