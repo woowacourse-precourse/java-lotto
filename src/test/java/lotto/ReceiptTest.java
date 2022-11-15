eae0c1ae2756bbc883b9af0892a3cd3295c1b51c
korjun1993 <corojoon93@naver.com>
2022-11-15 21:34:10 +0900

    test(ReceiptTest) : Receipt 생성자 변경으로 인한 테스트 코드 수정

--- src/test/java/lotto/ReceiptTest.java
+++ src/test/java/lotto/ReceiptTest.java
@@ -12,28 +12,28 @@ public class ReceiptTest {
     @DisplayName("금액이 음수로 주어졌을 때 Receipt 생성 기능 테스트")
     @Test
     void createReceiptByNegative() {
-        Assertions.assertThatThrownBy(() -> new Receipt("-1000"))
+        Assertions.assertThatThrownBy(() -> new Receipt(-1000))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("금액이 0으로 주어졌을 때 Receipt 생성 기능 테스트")
     @Test
     void createReceiptByZero() {
-        Assertions.assertThatThrownBy(() -> new Receipt("-1000"))
+        Assertions.assertThatThrownBy(() -> new Receipt(-1000))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("금액이 1000으로 나누어지지 않을 때 Receipt 생성 기능 테스트")
     @Test
     void createReceiptByNotDividedValue() {
-        Assertions.assertThatThrownBy(() -> new Receipt("-1000"))
+        Assertions.assertThatThrownBy(() -> new Receipt(-1000))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 구매 기능 테스트")
     @Test
     void buyTest() {
-        Receipt receipt = new Receipt("2000");
+        Receipt receipt = new Receipt(2000);
         List<Lotto> lottos = receipt.buy();
         Assertions.assertThat(lottos.size()).isEqualTo(2);
     }
@@ -44,7 +44,7 @@ public class ReceiptTest {
         ByteArrayOutputStream output = new ByteArrayOutputStream();
         System.setOut(new PrintStream(output));
 
-        Receipt receipt = new Receipt("2000");
+        Receipt receipt = new Receipt(2000);
         receipt.printReceipt();
 
         Assertions.assertThat(output.toString()).isEqualTo("2개를 구매했습니다.\n");
