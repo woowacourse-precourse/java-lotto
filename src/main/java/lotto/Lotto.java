77565babea48c5f27abf146c445146a78bb89222
KJE <12181501@inha.edu>
2022-11-15 11:58:53 +0900

    feat: Lotto-보너스번호 입력 및 예외처리 함수 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,10 +6,15 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static int bonusNum;
+    private static List<Integer> winNumbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        for(int i = 0; i < 5;i++){
+            this.winNumbers.add(0);
+        }
     }
 
     private void validate(List<Integer> numbers) {
@@ -24,5 +29,13 @@ public class Lotto {
                 throw new IllegalArgumentException();
             }
         }
+    }
+
+    public void getBonus(int bonus){
+        if(Collections.frequency(numbers, bonus) != 0){
+            System.out.println("[ERROR] 보너스번호를 중복되지 않게 입력해주세요.");
+            throw new IllegalArgumentException();
+        }
+        this.bonusNum = bonus;
     }
 }
