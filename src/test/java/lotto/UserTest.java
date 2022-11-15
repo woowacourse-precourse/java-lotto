f7c057eec4595e30abd362519cd3c17b0d7a7f6c
Somi Kim <rlathal009@gmail.com>
2022-11-15 16:58:03 +0900

    test : 유저 금액 입력 받기 테스트

---
+++ src/test/java/lotto/UserTest.java
@@ -0,0 +1,43 @@
+package lotto;
+
+import lotto.domain.User;
+import lotto.exception.LottoError;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+
+    @Test
+    public void 유저_금액_입력_받기() throws Exception {
+        //given
+        User user = new User();
+
+        //when
+        user.inputPurchaseAmount("4000");
+
+        //then
+        assertThat(user.getPurchaseAmount()).isEqualTo(4000);
+    }
+
+    @Test
+    public void 잘못된_금액_입력() throws Exception {
+        //given
+        User user = new User();
+
+        //when
+        String wrongUnit = "4300";
+        String notNumeric = " 12345 ";
+
+        //then
+        assertThrows(IllegalArgumentException.class,
+                () -> user.inputPurchaseAmount(wrongUnit))
+                .getMessage()
+                .equals(LottoError.WRONG_UNIT_OF_WON.getErrorMessage());
+        assertThrows(IllegalArgumentException.class,
+                () -> user.inputPurchaseAmount(notNumeric))
+                .getMessage()
+                .equals(LottoError.NOT_NUMERIC.getErrorMessage());
+    }
+}
