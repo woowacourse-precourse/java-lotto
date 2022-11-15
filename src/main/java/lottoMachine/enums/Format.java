d84f33c563e749b354da276699fc801365161df9
jeongwle <dldl4568@naver.com>
2022-11-15 18:52:14 +0900

    chore: 소수점 출력 포맷 살짝 수정

--- src/main/java/lottoMachine/enums/Format.java
+++ src/main/java/lottoMachine/enums/Format.java
@@ -4,7 +4,7 @@ public enum Format {
     NUMBER_FORMAT("^[0-9]+$"),
     WINNING_NUMBER_FORMAT("\\d+,\\d+,\\d+,\\d+,\\d+,\\d+"),
     DECIMAL_FORMAT("%.1f"),
-    DECIMAL_PRINT_FORMAT("#,###.0");
+    DECIMAL_PRINT_FORMAT("#,##0.0");
 
     private final String format;
 
