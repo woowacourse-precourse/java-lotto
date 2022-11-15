fbcce5c8f27d9b7cd53a036cbdb5086b8a5df2fc
kwondaehwi <kwondh8561@ajou.ac.kr>
2022-11-15 18:46:08 +0900

    feat: LottoService 클래스 printLottos() 구현 printLottos(): List<lotto> lottos의 lotto 리스트를 모두 출력

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -3,6 +3,8 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -12,10 +14,17 @@ public class LottoService {
 
     public void purchaseLottos() {
         this.lottos = new ArrayList<>();
+        System.out.println("구입금액을 입력해 주세요.");
         int numberOfLottos = userInterface.getPrice() / 1000;
+        System.out.println(numberOfLottos + "개를 구매했습니다.");
         IntStream.range(0,numberOfLottos).forEach(i -> {
-            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6));
+            List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+            randomNumbers.sort(Comparator.naturalOrder());
+            Lotto lotto = new Lotto(randomNumbers);
             this.lottos.add(lotto);
         });
+    }
+    public void printLottos() {
+        lottos.forEach(Lotto::printLotto);
     }
 }
