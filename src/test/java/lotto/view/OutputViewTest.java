03891f2c7ffb6208535f1f0c32071623c07a3b53
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-11-15 13:01:45 +0900

    test(printLottos): test feature

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import lotto.model.Lotto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -8,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -79,5 +82,36 @@ class OutputViewTest {
         String resultMessage = numberOfLottos + message;
         assertEquals(resultMessage, outContent.toString());
     }
-    
+
+    @Test
+    @DisplayName("구매한 lotto들을 출력")
+    void printLottos() {
+        // Given
+        List<Lotto> lottos = List.of(
+                new Lotto(List.of(8, 21, 23, 41, 42, 43)),
+                new Lotto(List.of(3, 5, 11, 16, 32, 38)),
+                new Lotto(List.of(7, 11, 16, 35, 36, 44)),
+                new Lotto(List.of(1, 8, 11, 31, 41, 42)),
+                new Lotto(List.of(13, 14, 16, 38, 42, 45)),
+                new Lotto(List.of(7, 11, 30, 40, 42, 43)),
+                new Lotto(List.of(2, 13, 22, 32, 38, 45)),
+                new Lotto(List.of(1, 3, 5, 14, 22, 45))
+        );
+
+        // When
+        outputView.printLottos(lottos);
+
+        // Then
+        String message =
+                "[8, 21, 23, 41, 42, 43]\n"
+                + "[3, 5, 11, 16, 32, 38]\n"
+                + "[7, 11, 16, 35, 36, 44]\n"
+                + "[1, 8, 11, 31, 41, 42]\n"
+                + "[13, 14, 16, 38, 42, 45]\n"
+                + "[7, 11, 30, 40, 42, 43]\n"
+                + "[2, 13, 22, 32, 38, 45]\n"
+                + "[1, 3, 5, 14, 22, 45]\n";
+        assertEquals(message, outContent.toString());
+    }
+
 }
