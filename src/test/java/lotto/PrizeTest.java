b81156aa0356fe4a75ec1ccf443eb7dbd0448b89
Ilchul Kim <huj0159@gmail.com>
2022-11-15 19:36:59 +0900

    test: throw exception when correctCount over 6

--- src/test/java/lotto/PrizeTest.java
+++ src/test/java/lotto/PrizeTest.java
@@ -1,4 +1,17 @@
 package lotto;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class PrizeTest {
+    @DisplayName("맞춘 번호가 6을 초과할시 예외가 발생한다.")
+    @Test
+    void correctNumberOverSixInvokeException() {
+        assertThatThrownBy(() -> Prize.makingResult(7,false).getCorrectCount())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
