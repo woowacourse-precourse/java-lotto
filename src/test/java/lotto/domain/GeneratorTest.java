58c033da00fc18bbaf044442fbd723c743ceaa50
taeyun-work <wmfajr1357@gmail.com>
2022-11-15 14:54:36 +0900

    test: judgeWinningCount 메소드 테스트 수정

--- src/test/java/lotto/domain/GeneratorTest.java
+++ src/test/java/lotto/domain/GeneratorTest.java
@@ -15,25 +15,22 @@ import lotto.domain.Generator;
 import lotto.domain.Lotto;
 
 public class GeneratorTest {
-    @InjectMocks
-    private Generator target;
-
     @DisplayName("로또 인스턴스를 인자로 받아 일치하는 당첨번호 및 보너스번호 개수를 HashMap에 담아 반환해준다")
     @Test
-    void judgeWinningCountValidation() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        Method method = target.getClass().getDeclaredMethod("judgeWinningCount", Lotto.class);
+    void judgeWinningCountTest() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Method method = Generator.class.getDeclaredMethod("judgeWinningCount", Lotto.class);
         method.setAccessible(true);
-
 
         List<Integer> winningNumbers = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6));
         Lotto.setWinningNumbers(winningNumbers);
         Lotto.setBonusNumber(7);
         Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        HashMap<String, Integer> winningSummary = (HashMap<String, Integer>)method.invoke(target, lotto);
+        HashMap<String, Integer> winningSummary = (HashMap<String, Integer>)method.invoke(Generator.class, lotto);
 
         HashMap<String, Integer> expectSummary = new HashMap<String, Integer>();
-        expectSummary.put("winningCount", 1);
-        expectSummary.put("bonusCount", 1);
+        expectSummary.put("winningCount", 6);
+        expectSummary.put("bonusCount", 0);
         assertThat(winningSummary).isEqualTo(expectSummary);
     }
+
 }
