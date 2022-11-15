a601354f0f562db70a870d8b7e41b931ec1adef5
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-15 16:20:35 +0900

    refactor(ErrorMessage): 보너스 번호 에러 메세지 수정

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -6,7 +6,7 @@ public enum ErrorMessage {
     INVALID_LOTTO_SIZE_ERROR("[ERROR] 로또 번호는 6개 입니다."),
     INVALID_LOTTO_FORM_ERROR("[ERROR] 콤마로 구분하여 6개의 숫자를 입력해 주십시오."),
     INVALID_LOTTO_NUMBER_ERROR("[ERROR] 로또 번호에는 중복이 있으면 안됩니다."),
-    INVALID_BONUS_NUMBER_ERROR("[ERROR] 당첨 번호와 중복되는 보너스 번호입니다.");
+    INVALID_BONUS_NUMBER_ERROR("[ERROR] 당첨 번호와 중복되지 않는 1부터 45 사이의 숫자여야 합니다.");
 
     private String message;
 
