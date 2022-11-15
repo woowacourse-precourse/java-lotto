9952254abe3bf9b27c299d84d7a11745daef5a73
Minji Kim <minji1289@naver.com>
2022-11-15 23:15:24 +0900

    예외 처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,9 +10,26 @@ public class Application {
 
         List<Integer> numbers=new ArrayList<>(); //로또 당첨 번호 입력받기
 
-        for (int i=0; i<5; i++){
-            int input = Integer.parseInt(Console.readLine());
-            numbers.add(input);
+        try{
+            for (int i=0; i<5; i++){
+                int input = Integer.parseInt(Console.readLine());
+                if(numbers.contains(input)){ //중복 제거하기
+                    throw new IllegalArgumentException();
+                }
+                if (input<1 | input>45){ //숫자 범위 체크
+                    throw new IllegalArgumentException();
+                }
+                numbers.add(input);
+            }
+
+            if (numbers.size() != 6) { //사용자 입력(당첨 번호) 사이즈 체크
+                throw new IllegalArgumentException();
+            }
+
+        }
+
+        catch(IllegalArgumentException e){
+            System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
 
         int bonus = Integer.parseInt(Console.readLine()); //보너스 번호 입력받기
