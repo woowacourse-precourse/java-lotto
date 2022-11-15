f4681773de6228b2aee7f86a62bfb13b0ceee709
jijongkwon <help3451@naver.com>
2022-11-15 16:31:46 +0900

    test : 보너스 숫자 일치여부를 확인하는 테스트 추가

--- src/test/java/lotto/domain/ComparingMachineTest.java
+++ src/test/java/lotto/domain/ComparingMachineTest.java
@@ -18,4 +18,13 @@ class ComparingMachineTest {
                 List.of(List.of(1,2,3,6,7,8), List.of(12,13,14,15,16,17)));
         assertThat(comparingMachine.getContainNumberList()).isEqualTo(List.of(4,0));
     }
+
+    @DisplayName("보너스 숫자 일치여부를 확인한다.")
+    @Test
+    void countContainBonusNumber() {
+        ComparingMachine comparingMachine = new ComparingMachine();
+        comparingMachine.countContainBonusNumber(7,
+                List.of(List.of(1,2,3,4,5,6),List.of(1,5,6,7,8,9)));
+        assertThat(comparingMachine.getContainBonusNumber()).isEqualTo(List.of(0,1));
+    }
 }
