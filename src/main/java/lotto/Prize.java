1c7f07e95058ef4092eab26ad669bb39073ff701
youswim <youswim96@gmail.com>
2022-11-15 20:45:59 +0900

    refactor(lotto): Prize클래스에 value변수 추가
    value에 등수에 따른 상금을 기록하기 위해 추가함

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,20 +1,26 @@
 package lotto;
 
 public enum Prize {
-    FIRST("6개 일치 (2,000,000,000원)"),
-    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    THIRD("5개 일치 (1,500,000원)"),
-    FOURTH("4개 일치 (50,000원)"),
-    FIFTH("3개 일치 (5,000원)"),
-    NONE("상금 없음"),
-    UNCHECKED("확인 전");
+    FIRST("6개 일치 (2,000,000,000원)", 2_000_000_000),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000),
+    THIRD("5개 일치 (1,500,000원)", 1_500_000),
+    FOURTH("4개 일치 (50,000원)", 50_000),
+    FIFTH("3개 일치 (5,000원)", 5_000),
+    NONE("상금 없음", 0),
+    UNCHECKED("확인 전", -1);
 
     private final String attribute;
-    Prize(String attribute) {
+    private final int value;
+    Prize(String attribute, int value) {
         this.attribute = attribute;
+        this.value = value;
     }
 
     public String getAttribute() {
         return attribute;
+    }
+
+    public int getValue() {
+        return value;
     }
 }
