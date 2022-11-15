e88fb215538e171d55352c63127e0b101e055299
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-11-15 18:19:53 +0900

    refactor(lotto): 상수명 통일, 당첨번호 입력시 공백 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,12 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import lotto.Lotto;
 
 public class InputView {
 
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
     private static final String INPUT_PRICE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String INPUT_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBERS = "보너스 번호를 입력해 주세요.";
@@ -39,7 +38,7 @@ public class InputView {
 
     private static List<Integer> convertStringtoList(String numbers) {
         try {
-            return Arrays.stream(numbers.split(",")).map(Integer::valueOf)
+            return Arrays.stream(numbers.replaceAll(" ", "").split(",")).map(Integer::valueOf)
                     .collect(Collectors.toList());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(DATA_TYPE_ERR_MESSAGE);
@@ -54,7 +53,7 @@ public class InputView {
     }
 
     private static void validateRange(int bonus) {
-        if (!(bonus >= MIN_NUMBER && bonus <= MAX_NUMBER)) {
+        if (!(bonus >= LOTTO_NUMBER_MIN && bonus <= LOTTO_NUMBER_MAX)) {
             throw new IllegalArgumentException(LOTTO_RANGE_ERR_MESSAGE);
         }
     }
