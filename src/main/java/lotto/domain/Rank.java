0a9af9367330cbbe48db09cb8412f036d1c8f4a5
josolha <josolha@nate.com>
2022-11-15 15:29:00 +0900

    style(Rank) : 우테코 스타일 적용

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -8,7 +8,6 @@ public enum Rank {
     SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
     FIRST(6, false, 2_000_000_000, "6개 일치 (2,000,000,000원) - ");
 
-
     private final int RANK_NUMBER;
     private final boolean BONUS_NUMBER;
     private final int PROFIT;
