3346495bfee2efa664e08ed8070732c6fd433ba3
yeop0740 <ssucgoo@gmail.com>
2022-11-15 18:20:18 +0900

    fix(Rank): 열거형에 다양한 변수 추가

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,15 +1,38 @@
 package lotto;
 
 public enum Rank {
-    FIRST(0), SECOND(1), THIRD(2), FOURTH(3), FIFTH(4), NONE(5);
+    FIRST(0, 6, 2000000000, "6개 일치 (2,000,000,000원) - "),
+    SECOND(1, 7, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(2, 5, 1500000, "5개 일치 (1,500,000원) - "),
+    FOURTH(3, 4, 50000, "4개 일치 (50,000원) - "),
+    FIFTH(4, 3, 5000, "3개 일치 (5,000원) - "),
+    NONE(5, 0, 0, "");
 
     private final int value;
+    private final int count;
+    private final int price;
+    private final String printing;
 
-    Rank(int value) {
+    Rank(int value, int count, int price, String printing) {
         this.value = value;
+        this.count = count;
+        this.price = price;
+        this.printing = printing;
     }
 
     public int getValue() {
         return value;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getPrinting() {
+        return printing;
     }
 }
