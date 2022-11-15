70d390215a2afec54bd4cce53882c8d6ebf5d905
yw824 <yongwoo25@gmail.com>
2022-11-15 10:54:09 +0900

    feat: Lotto 클래스 구현
    - validate(List numbers):
    	- 입력된 로또 리스트 크기 6 확인 - 아니면 Exception 발생
    	- 중복 확인 - 없거나 중복 있으면 Exception 발생
    - throwInputExceptions(condition, exceptionMessage):
    	- condition이라는 주어진 boolean 값에 따라
    	- 만족하지 못하면 Exception 발생시키는 함수
    - getNumbers():
    	- 생성자를 통해 입력받은 값을 검증 후
    	- 객체에 저장시키는 과정 완료 후
    	- 해당 Lotto 객체 내의 리스트 반환하는 함수
    	- Lotto 테스트를 위함

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,25 +1,43 @@
 package lotto;
 
 import java.util.List;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-
         this.numbers = validate(numbers);
     }
 
+    // validate 구현  입력되는 로또 형태의 유효성을 검사하는 함수
+    // 유효하면 그대로 전달한 리스트 반환
+    // 유효하지 않으면 에러 발생
     private List<Integer> validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        //입력된 로또 리스트 크기 확인
+        throwInputExceptions(numbers.size() == 6, "로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.");
+
+        // 중복 확인
+        int frequency; // 확인할 빈도 수
+        int comp; // 중복 확인하기 위해 각 순회마다 받을 배열
+        Iterator<Integer> iter = numbers.iterator();
+        while(iter.hasNext()) {
+            comp = iter.next();
+            frequency = Collections.frequency(numbers, comp);
+            throwInputExceptions(frequency == 1, "로또 번호에 중복된 숫자가 있으면 예외가 발생한다.");
         }
         return numbers;
     }
 
-    public List<Integer> getNumbers() {
-        return this.numbers;
+    private void throwInputExceptions(boolean condition, String exceptionMessage) {
+        if(condition) {
+            return;
+        }
+        throw new IllegalArgumentException(exceptionMessage);
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
