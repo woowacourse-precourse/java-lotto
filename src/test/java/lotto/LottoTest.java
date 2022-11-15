ea6e078e68c9b5b87c2e24dbc660699e8efa032b
glass <glass7312@gmail.com>
2022-11-15 14:53:52 +0900

    test:,를 기준으로 문자열을 나누는 기능 테스트,각 테스트 코드에 DisplayName 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -32,38 +32,51 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-
+    @DisplayName(",를 기준으로 문자열을 나눈다.")
     @Test
-    void overLapTest(){
-        List<Integer> test = Randoms.pickUniqueNumbersInRange(1,45,6);
+    void splitTest() {
+        String str = "1,2,3";
+        String[] t_str = str.split(",");
+        assertThat(t_str).containsExactly("1", "2", "3");
+    }
+
+    @DisplayName("중복값이 있는지 테스트")
+    @Test
+    void overLapTest() {
+        List<Integer> test = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         assertThat(test.size()).isEqualTo(test.stream().distinct().count());
     }
-    @Test
-    void hitCountTest(){
-        Lotto lotto=new Lotto(List.of(11,12,13,42,37,10));
 
-        assertThat(lotto.get_hitCount(List.of(1,31,42,37,11,12))).isEqualTo(4);
+    @DisplayName("로또 번호맞은 갯수 판별")
+    @Test
+    void hitCountTest() {
+        Lotto lotto = new Lotto(List.of(11, 12, 13, 42, 37, 10));
+
+        assertThat(lotto.get_hitCount(List.of(1, 31, 42, 37, 11, 12))).isEqualTo(4);
     }
 
+    @DisplayName("로또의 당첨등수가 출력되는지 테스트")
     @Test
-    void getGradeTest(){
+    void getGradeTest() {
 
-        assertThat(getGrade(3,false)).isEqualTo(Grade.Lottogrades.fifth);
+        assertThat(getGrade(3, false)).isEqualTo(Grade.Lottogrades.fifth);
         AbstractComparableAssert<?, Grade.Lottogrades> equalTo = assertThat(getGrade(5, true)).isEqualTo(Grade.Lottogrades.second);
 
 
     }
+
     @Test
-    void controllerPrintMessageTest(){
+    void controllerPrintMessageTest() {
         lottoSpawner(8);
 
         List<Integer> winning_numbers = List.of(1, 31, 25, 36, 42, 11);
         setGrades();
         //assertThat().isEqualTo();
     }
-    @Test
-    void getLottoCountTest(){
 
+    @Test
+    void getLottoCountTest() {
+        assertThatThrownBy(() ->).isInstanceOf(IllegalArgumentException.class);
     }
 
     // 아래에 추가 테스트 작성 가능
