728617531bc7796412617eb0bac3d2c215cd751e
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-15 13:37:42 +0900

    test(LottoTest): test exception situation on creating Lotto instance
    Test created(IllegalArgumentException should be thrown):
    - Numbers out of range(1-45)
    - Less than 6 numbers generated
    - Duplicates of numbers out of range(1-45)
    - Empty list generated

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,4 +24,33 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 1-45 범위를 벗어나는 수가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 65)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 개수가 6개보다 적으면 예외가 발생한다.")
+    @Test
+    void createLottoWithUnderSixNumbers() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("1-45 범위를 벗어나는 수가 중복된 경우에도 예외가 발생한다.")
+    @Test
+    void createLottoDuplicateOutOfRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 65, 65)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("비어있는 경우에도 예외가 발생한다.")
+    @Test
+    void createLottoByNull() {
+        assertThatThrownBy(() -> new Lotto(List.of()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
