fcf4387ce7c03239b9702a6067303bc420fb3d2b
CMHyuk <alsgur9710@naver.com>
2022-11-15 21:35:11 +0900

    style(InputView): 줄바꿈 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 public class InputView {
+
     private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
     private static final String PURCHASE_LOTTO = "개를 구매했습니다.";
     private static final String INPUT_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
