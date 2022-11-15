ed7372872dd1c9d3464442dab540a0358a9a4014
hanuel <ezzeezz@naver.com>
2022-11-15 22:08:02 +0900

    feat(lotto): 당첨 숫자 입력에 대한 예외처리 구현

--- src/main/java/lotto/LottoHeadOffice.java
+++ src/main/java/lotto/LottoHeadOffice.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LottoHeadOffice {
@@ -18,10 +19,39 @@ public class LottoHeadOffice {
     private void inputWinningNum() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String s = Console.readLine();
-        // validate 검사 (입력형식, 중복되지 않는지)
+
+        validateWinningNum(s);
+
         for (String digit : s.split(",")) {
             this.winningNum.add(Integer.valueOf(digit));
         }
+    }
+
+    private void validateWinningNum(String s) {
+        String[] split = s.split(",");
+        for (String num : split) {
+            // 숫자가 아닌 문자가 입력되었을 때
+            char[] arrCh = num.toCharArray();
+            for (char ch : arrCh) {
+                if (!Character.isDigit(ch))
+                    throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자로 이루어져야 합니다.");
+            }
+            // 숫자 범위가 1~45가 아닐 때
+            int winningNum = Integer.parseInt(num);
+            if (winningNum < 1 || 45 < winningNum)
+                throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 1~45입니다.");
+        }
+
+        // 중복된 숫자가 없는지
+        List<String> toList = Arrays.asList(split);
+        if (toList.size() != toList.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않은 수들로 이루어져야 합니다.");
+        }
+
+        // 6개의 숫자로 이루어졌는지
+        if (toList.size() != 6)
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자로 이루어져야 합니다.");
+
     }
 
     private void inputBonusNum() {
