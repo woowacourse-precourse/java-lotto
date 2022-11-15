a19be34887318310657fe1cd2fe00bf0cbdfc227
sschan99 <seungchan1219@gmail.com>
2022-11-15 13:19:31 +0900

    refactor(DrawMachine): change methods to use instance variables

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,12 @@
 package lotto;
 
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
         LotterySeller lotterySeller = new LotterySeller();
         lotterySeller.sellLotto();
 
         DrawMachine drawMachine = new DrawMachine();
-        List<Integer> numbers = drawMachine.drawNumbers();
-        int bonusNumber = drawMachine.drawBonusNumber();
+        drawMachine.drawNumbers();
+        drawMachine.drawBonusNumber();
     }
 }
--- src/main/java/lotto/DrawMachine.java
+++ src/main/java/lotto/DrawMachine.java
@@ -2,27 +2,27 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class DrawMachine {
-    public List<Integer> drawNumbers() {
+    List<Integer> numbers;
+    int bonusNumber;
+
+    public void drawNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String userInput = Console.readLine();
         System.out.println();
 
-        List<Integer> numbers = new ArrayList<>();
         for (String number : userInput.split(",")) {
             numbers.add(Integer.parseInt(number));
         }
-        return numbers;
     }
 
-    public int drawBonusNumber() {
+    public void drawBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String userInput = Console.readLine();
         System.out.println();
 
-        return Integer.parseInt(userInput);
+        bonusNumber = Integer.parseInt(userInput);
     }
 }
