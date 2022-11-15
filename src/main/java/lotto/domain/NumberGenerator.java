09dc95db3c06a2578a3227d710670590db90a50b
lhyeonj120 <isk04387@gmail.com>
2022-11-15 18:00:40 +0900

    feat: 로또 번호 생성 기능 구현

---
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class NumberGenerator {
+    private final List<Integer> numbers;
+    
+    public NumberGenerator(int start, int end, int count) {
+        numbers = Randoms.pickUniqueNumbersInRange(start, end, count);
+    }
+    
+    public List<Integer> getNumbers(){
+        return numbers;
+    }
+}
