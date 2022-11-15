bfc5c1344e0dbe14f1056ed5072e8c844f0baf99
This2sho <zx00018@naver.com>
2022-11-15 10:17:29 +0900

    style(WinningNumberTest): 사용되지 않는 import 제거

--- src/test/java/lotto/domain/WinningNumberTest.java
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.List;
 import java.util.stream.Stream;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
