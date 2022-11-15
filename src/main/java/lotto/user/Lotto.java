4089a6a4cb6ed2c3b90e668c56d8e43905e1275f
GwansuShin <happysun7080@naver.com>
2022-11-15 14:49:26 +0900

    refactor(Lotto): 검증 클래스를 내부로 이동
    LottoValidator를 Lotto의 내부 클래스로 정의

--- src/main/java/lotto/user/Lotto.java
+++ src/main/java/lotto/user/Lotto.java
@@ -58,4 +58,33 @@ public class Lotto {
     public String toString() {
         return numbers.toString();
     }
+
+    static class LottoValidator extends Validator {
+        private static final int START_INCLUSIVE = 1;
+        private static final int END_INCLUSIVE = 45;
+        private static final int NUMBERS_SIZE = 6;
+
+        public static void validateNumbersSize(List<Integer> list) {
+            if (list.size() != NUMBERS_SIZE) {
+                ErrorHandler.throwException(ErrorMessage.LOTTO_SIZE_EXCEEDED);
+            }
+        }
+
+        public static void validateNumbersNotRepeated(List<Integer> numbers) {
+            if (numbers
+                    .stream()
+                    .distinct()
+                    .count() != numbers.size()) {
+                ErrorHandler.throwException(ErrorMessage.LOTTO_NUMBERS_REPEATED);
+            }
+        }
+
+        public static void validateNumbersInRange(List<Integer> numbers) {
+            if (numbers
+                    .stream()
+                    .anyMatch(number -> (number < START_INCLUSIVE || number > END_INCLUSIVE))) {
+                ErrorHandler.throwException(ErrorMessage.LOTTO_NUMBERS_NOT_IN_RANGE);
+            }
+        }
+    }
 }
