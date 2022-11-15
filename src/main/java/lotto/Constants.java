e2fcc18a12bfa098c21141bdd1036e1b26a113a1
Sanmgmin <sangmins930@gmail.com>
2022-11-15 22:09:56 +0900

    refactor(Constants): change ERROR String

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -13,6 +13,6 @@ public class Constants {
     public static final String ERROR_MESSAGE_FOR_DUPLICATION_WITH_WINNING_NUMBER = "[ERROR] 보너스 번호는 로또 번호와 중복되지 않은 숫자여야 합니다.";
     public static final String ERROR_MESSAGE_FOR_RANGE_OF_BONUS_NUMBER = "[ERROR] 보너스 번호는 " + MIN_VALUE_OF_LOTTO + "부터 " + MAX_VALUE_OF_LOTTO + " 사이의 숫자여야 합니다.";
     public static final String ERROR_MESSAGE_FOR_SEED_MONEY = "[ERROR] 구입금액은 " + MONEY_UNIT + "원 단위여야 합니다.";
-    public static final String ERROR_MESSAGE_FOR_IS_NUMERIC = "[ERROR] 구입금액은 숫자여야 합니다.";
+    public static final String ERROR_MESSAGE_IS_NOT_NUMERIC = "[ERROR] 입력이 숫자가 아닙니다.";
     public static final String ERROR_MESSAGE_FOR_PROPER_MONEY = "[ERROR] 구입금액은 최소 1000원 입니다.";
 }
