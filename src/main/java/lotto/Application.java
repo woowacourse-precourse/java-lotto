a5f41b9e8157e95aa9c2de7c1f93a8a2cf1ad730
coldbucks <met.hsw@gmail.com>
2022-11-15 20:10:48 +0900

    feat(Lotto): 로또 구매수량 갯수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,13 +5,11 @@ import lotto.domain.LottoSystem;
 
 public class Application {
 
-
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         LottoSystem lottoSystem = new LottoSystem();
-        lottoSystem.purchaseAmount();
-
+//        lottoSystem.purchaseAmount();
+        lottoSystem.purchaseLottoAmount();
 
     }
 }
--- src/main/java/lotto/domain/LottoSystem.java
+++ src/main/java/lotto/domain/LottoSystem.java
@@ -11,4 +11,9 @@ public class LottoSystem {
         String lottoMoney = Console.readLine();
         return Integer.parseInt(lottoMoney);
     }
+    //구매로또수량
+    public void  purchaseLottoAmount() {
+        int lottoAmount = purchaseAmount() / TICKET_PRICE;
+        System.out.println(lottoAmount + "개를 구매했습니다.");
+    }
 }
