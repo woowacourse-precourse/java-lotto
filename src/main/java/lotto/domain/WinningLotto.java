1dc77392037f09e5ff03e0d087f5a86e289ed6ea
wlsh44 <k87913j@gmail.com>
2022-11-15 20:07:03 +0900

    fix: 보너스 번호 범위 검증 기능 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -2,7 +2,10 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.domain.LottoGenerator.MAXIMUM_LOTTO_NUMBER;
+import static lotto.domain.LottoGenerator.MINIMUM_LOTTO_NUMBER;
 import static lotto.support.ErrorMessage.DUPLICATED_NUMBER_ERROR;
+import static lotto.support.ErrorMessage.OUT_OF_RANGE_NUMBER_ERROR;
 
 public class WinningLotto {
 
@@ -10,14 +13,29 @@ public class WinningLotto {
     private final int bonusNumber;
 
     public WinningLotto(Lotto lotto, int bonusNumber) {
-        validateBonusNumberDuplicated(lotto.getNumbers(), bonusNumber);
+        validateBonusNumber(lotto.getNumbers(), bonusNumber);
         this.bonusNumber = bonusNumber;
         this.winningLotto = lotto;
+    }
+
+    private void validateBonusNumber(List<Integer> numbers, int bonusNumber) {
+        validateNumberRange(bonusNumber);
+        validateBonusNumberDuplicated(numbers, bonusNumber);
+    }
+
+    public static WinningLotto from(Lotto lotto, int bonusNumber) {
+        return new WinningLotto(lotto, bonusNumber);
     }
 
     private void validateBonusNumberDuplicated(List<Integer> numbers, int bonusNumber) {
         if (numbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(DUPLICATED_NUMBER_ERROR);
+        }
+    }
+
+    private void validateNumberRange(Integer number) {
+        if (number < MINIMUM_LOTTO_NUMBER || MAXIMUM_LOTTO_NUMBER < number) {
+            throw new IllegalArgumentException(OUT_OF_RANGE_NUMBER_ERROR);
         }
     }
 
@@ -34,9 +52,5 @@ public class WinningLotto {
 
     public boolean isMatchedBonusNumber(List<Integer> lottoNumbers) {
         return lottoNumbers.contains(bonusNumber);
-    }
-
-    public static WinningLotto from(Lotto lotto, int bonusNumber) {
-        return new WinningLotto(lotto, bonusNumber);
     }
 }
