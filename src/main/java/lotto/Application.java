52f71f7e6d381424687d893f28001d4d53d9711e
hong <hong@hongui-MacBookAir.local>
2022-11-15 17:23:35 +0900

    feat: 구매한 총 로또 당첨내역 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -22,4 +23,49 @@ public class Application {
         return all_lottery;
     }
 
+    public enum LOTTERY_MATCHES{
+        NONE,
+        SIX,
+        FIVE_BONUS,
+        FIVE,
+        FOUR,
+        THREE
+    }
+    public static void printHowManyMatches(List<Lotto> all_lottery, Lotto win_lottery, int bonus) {
+        HashMap<Integer,Integer> lottery_ranks = lotteryRanks(all_lottery,win_lottery, bonus);
+        lottery_ranks.forEach((key,value)->{
+            if(key == LOTTERY_MATCHES.THREE.ordinal())
+                System.out.println("3개 일치 (5,000원) - "+ value + "개");
+            if(key == LOTTERY_MATCHES.FOUR.ordinal())
+                System.out.println("4개 일치 (50,000원) - "+ value + "개");
+            if(key == LOTTERY_MATCHES.FIVE.ordinal())
+                System.out.println("5개 일치 (1,500,000원) - "+ value + "개");
+            if(key == LOTTERY_MATCHES.FIVE_BONUS.ordinal())
+                System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+ value + "개");
+            if(key == LOTTERY_MATCHES.SIX.ordinal())
+                System.out.println("6개 일치 (2,000,000,000원) - "+ value + "개");
+        });
+    }
+
+    public static HashMap<Integer,Integer> lotteryRanks(List<Lotto> all_lottery, Lotto win_lottery, int bonus){
+        HashMap<Integer,Integer> lottery_ranks = initLotteryRanks();
+
+        for (Lotto lotto : all_lottery
+        ) {
+            int rank = lotto.rank(win_lottery, bonus);
+            if(rank >0)
+                lottery_ranks.put(rank,lottery_ranks.get(rank) + 1);
+        }
+        return lottery_ranks;
+    }
+
+    public static HashMap<Integer,Integer> initLotteryRanks(){
+        return new HashMap<>() {{
+            put(1,0);
+            put(2,0);
+            put(3,0);
+            put(4,0);
+            put(5,0);
+        }};
+    }
 }
--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static lotto.Application.printHowManyMatches;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest extends NsTest {
@@ -31,6 +32,31 @@ class ApplicationTest extends NsTest {
 
         int result = 2;
         assertThat(result).isEqualTo(input_nums.rank(win_nums, bonus));
+    }
+
+    @Test
+    void  당첨내역_출력_테스트(){
+        List<Lotto> all_lottery = new ArrayList<>();
+        all_lottery.add(new Lotto(List.of(8, 21, 23, 41, 42, 43)));
+        all_lottery.add(new Lotto(List.of(7, 11, 16, 35, 36, 44)));
+        all_lottery.add(new Lotto(List.of(1, 8, 11, 31, 41, 42)));
+        all_lottery.add(new Lotto(List.of(13, 14, 16, 38, 42, 45)));
+        all_lottery.add(new Lotto(List.of(7, 11, 30, 40, 42, 43)));
+        all_lottery.add(new Lotto(List.of(2, 13, 22, 32, 38, 45)));
+        all_lottery.add(new Lotto(List.of(1, 3, 5, 14, 22, 45)));
+
+        Lotto win_nums = new Lotto(List.of(1,2,3,4,5,6));
+        int bonus = 7;
+
+        printHowManyMatches(all_lottery,win_nums,bonus);
+
+        assertThat(output()).contains(
+                "3개 일치 (5,000원) - 1개",
+                "4개 일치 (50,000원) - 0개",
+                "5개 일치 (1,500,000원) - 0개",
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                "6개 일치 (2,000,000,000원) - 0개"
+        );
     }
     @Test
     void 기능_테스트() {
