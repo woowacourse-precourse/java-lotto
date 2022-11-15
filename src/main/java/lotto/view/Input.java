710306c73d07f8f76555a7804636496b4cd67d36
mikekang47 <gyeong0308@gmail.com>
2022-11-15 19:12:21 +0900

    Feat<Input>: 당첨 번호 및 보너스 번호 입력 로직 추가

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,7 +1,10 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.utils.LottoNumberConverter;
 import lotto.utils.Validator;
+
+import java.util.List;
 
 public class Input {
     private static final String NUMERIC_ERROR_MSG = "[ERROR] 반드시 숫자여야 합니다.";
@@ -14,6 +17,27 @@ public class Input {
             int money = Integer.parseInt(Console.readLine());
             Validator.validateMoney(money);
             return money;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMERIC_ERROR_MSG);
+        }
+    }
+
+    public static List<Integer> inputWinningNumber() {
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요");
+        try {
+            String bonusNumber = Console.readLine();
+            return LottoNumberConverter.convertWinningNumberToList(bonusNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMERIC_ERROR_MSG);
+        }
+    }
+
+    public static Integer inputBonusNumber() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+        try {
+            return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NUMERIC_ERROR_MSG);
         }
