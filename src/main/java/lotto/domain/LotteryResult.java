0eca4f2ed7b446b853f2566db5be0f2176e20614
skydreamer21 <skydreamer210@gmail.com>
2022-11-15 12:25:24 +0900

    feat(LotteryResult): 유저의 로또 당첨 결과 저장 클래스 구현
    [세부사항]
    - 필드
    1. result : Rank 별 개수를 저장
    2. totalReward : 총 상금
    3. size : 저장된 결과의 개수

--- src/main/java/lotto/domain/LotteryResult.java
+++ src/main/java/lotto/domain/LotteryResult.java
@@ -1,4 +1,49 @@
 package lotto.domain;
 
+import lotto.constant.LotteryRank;
+import lotto.constant.Status;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LotteryResult {
+    private static final int UNIT = 1;
+
+    private Map<LotteryRank, Integer> result;
+    private long totalReward;
+    private int size;
+
+    public LotteryResult() {
+        result = new HashMap<>();
+        initResult();
+        totalReward = 0;
+        size = 0;
+    }
+
+    private void initResult() {
+        for (LotteryRank rank : LotteryRank.values()) {
+            if (rank == LotteryRank.BEFORE_DRAW) {
+                continue;
+            }
+            result.put(rank, Status.EMPTY.getStatus());
+        }
+    }
+
+    public void add (LotteryRank rank) {
+        result.put(rank, result.get(rank) + UNIT);
+        totalReward += rank.getReward();
+        size++;
+    }
+
+    public int findByRank (LotteryRank rank) {
+        return result.get(rank);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public long getTotalReward() {
+        return totalReward;
+    }
 }
