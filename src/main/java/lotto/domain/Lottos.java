eb53c6b8a6a02cffa58d6ba53a6ba947295f825c
song-wooseok <sws981202@gmail.com>
2022-11-15 21:16:10 +0900

    refactor: lottos 인스턴스 변수 final 키워드 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -3,7 +3,7 @@ package lotto.domain;
 import java.util.ArrayList;
 
 public class Lottos {
-    private ArrayList<Lotto> lottos;
+    private final ArrayList<Lotto> lottos;
 
     public Lottos(){
         lottos = new ArrayList<>();
