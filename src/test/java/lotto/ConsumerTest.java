11d73c67cffd067816cd8773dc59fb278ec14762
고동우 <shp05179@gmail.com>
2022-11-15 20:48:46 +0900

    test: calcProfit 테스트 케이스 작성
    stat list가 주어졌을때 결과값을 계산

--- src/test/java/lotto/ConsumerTest.java
+++ src/test/java/lotto/ConsumerTest.java
@@ -2,21 +2,37 @@ package lotto;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ConsumerTest {
+    @Test
+    void testBuyLotto() {
+    }
 
     @Test
-    void buyLotto() {
+    void putWinLotto() {
+    }
+
+    @Test
+    void putBonusNumber() {
     }
 
     @Test
     void calcStat() {
-        Lotto winLotto = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto winLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         Consumer tester = new Consumer(winLotto);
-        assertThat(tester.calcStat(new Lotto(List.of(1,2,3,4,5,6)),7))
-                .isEqualTo(List.of(0,0,0,1,0,0));
+        assertThat(tester.calcStat(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7))
+                .isEqualTo(List.of(0, 0, 0, 1, 0, 0));
+    }
+
+    @Test
+    void calcProfit() {
+        Consumer consumer = new Consumer(new Lotto(List.of(1,2,3,4,5,6)));
+        List<Integer> stat = new ArrayList<>(List.of(1,0,0,0,0,0));
+        assertThat(consumer.calcProfit(stat))
+                .isEqualTo(List.of(1, 0, 0, 0, 0, 5000));
     }
 }
