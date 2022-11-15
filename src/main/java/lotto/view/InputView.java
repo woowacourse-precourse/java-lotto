21d19cbe9e3627844f061b36dd3d00837e90ba39
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-15 16:26:18 +0900

    feat(InputView): 구분 문자가 쉼표(,)가 아닌 경우 예외 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,8 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Pattern;
+
 public class InputView {
     public double inputPayment() {
         printPaymentMessage();
@@ -10,13 +12,30 @@ public class InputView {
         return Double.parseDouble(payment);
     }
 
-    public static void validateNumeric(String input) {
+    public void inputWinningNumbers() {
+        printWinningNumberMessage();
+        String winningNumbers = Console.readLine();
+        validateSeparator(winningNumbers);
+    }
+
+    public void validateNumeric(String input) {
         if (!input.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException("[ERROR] 입력값은 숫자만 가능합니다.");
         }
     }
 
+    public void validateSeparator(String input) {
+        final String REGEX = "^[0-9,]*$";
+        if (!Pattern.matches(REGEX, input)) {
+            throw new IllegalArgumentException("[ERROR] 숫자와 구분자(,)만 입력될 수 있습니다.");
+        }
+    }
+
     private void printPaymentMessage() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    private void printWinningNumberMessage() {
+        System.out.println("당첨 번호를 입력해 주세요.");
     }
 }
