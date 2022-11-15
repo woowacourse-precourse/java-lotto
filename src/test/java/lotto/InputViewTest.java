6fcee2cd0d6ac521490c594e58941d604c2c88d0
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-15 17:03:08 +0900

    test(InputViewTest): 입력 문자열 숫자 리스트로 변환하기 테스트 코드 작성

--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -22,8 +22,17 @@ public class InputViewTest {
     @DisplayName("당첨 번호에 숫자와 구분자(,) 외 다른 입력이 들어오면 예외가 발생한다.")
     @Test
     void getInputByUnValidSeparator() {
-        assertThatThrownBy(() -> new InputView().validateSeparator("1,#,4,6,7"))
+        assertThatThrownBy(() -> new InputView().validateSeparator("1,4,6,7,8"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 숫자와 구분자(,)만 입력될 수 있습니다.");
+                .hasMessageContaining("[ERROR] 6개의 숫자를 쉼표(,)로 구분하여 입력하세요.");
+    }
+
+    @DisplayName("입력 문자열 숫자 리스트로 변환하기")
+    @Test
+    void changeStringToListTest() {
+        String input = "1,4,5,2,7";
+        List<Integer> result = List.of(1, 4, 5, 2, 7);
+        assertThat(new InputView().changeStringToList(input))
+                .isEqualTo(result);
     }
 }
