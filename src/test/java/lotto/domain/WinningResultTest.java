907440fad752806fb33f6fd2917da5b37939d9e3
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-15 22:39:48 +0900

    test(lotto) : WinningResultTest 테스트 구현

--- src/test/java/lotto/domain/WinningResultTest.java
+++ src/test/java/lotto/domain/WinningResultTest.java
@@ -1,44 +1,62 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class WinningResultTest {
 
-    @Test
-    void getMap() {
+    List<Lotto> lottos;
+    WinningResult winningResult;
+
+    @BeforeEach
+    void setUp() {
         Lotto lotto1 = mock(Lotto.class);
         Lotto lotto2 = mock(Lotto.class);
         Lotto lotto3 = mock(Lotto.class);
 
-        when(lotto1.getNumbers())
-                .thenReturn(List.of(1, 2, 3, 4, 5, 6));
-        when(lotto2.getNumbers())
-                .thenReturn(List.of(1, 2, 3, 4, 5, 7));
-        when(lotto3.getNumbers())
-                .thenReturn(List.of(1, 2, 3, 4, 7, 8));
+        when(lotto1.getNumbers()).thenReturn(List.of(1, 2, 3, 4, 5, 6));
+        when(lotto2.getNumbers()).thenReturn(List.of(1, 2, 3, 4, 5, 7));
+        when(lotto3.getNumbers()).thenReturn(List.of(1, 2, 3, 4, 7, 8));
 
-        List<Lotto> lottos = List.of(lotto1, lotto2, lotto3);
+        lottos = List.of(lotto1, lotto2, lotto3);
 
         WinningLotto winningLotto = mock(WinningLotto.class);
-        when(winningLotto.getNumbers())
-                .thenReturn(List.of(1, 2, 3, 4, 5, 6));
-        when(winningLotto.getBonusNumber())
-                .thenReturn(7);
+        when(winningLotto.getNumbers()).thenReturn(List.of(1, 2, 3, 4, 5, 6));
+        when(winningLotto.getBonusNumber()).thenReturn(7);
 
-        WinningResult wr = new WinningResult(lottos, winningLotto);
-        Map<WinningTable, Integer> map = wr.getMap();
-        for (WinningTable wt : map.keySet()) {
-            System.out.println(wt + ": " + map.get(wt));
-        }
+        winningResult = new WinningResult(lottos, winningLotto);
     }
 
+    @DisplayName("당첨 결과 맵<등수, 개수> 생성")
     @Test
-    void getList() {
+    void getMap() {
+        Map<WinningTable, Integer> map = winningResult.getMap();
+        assertThat(map.get(WinningTable.FIRST)).isEqualTo(1);
+        assertThat(map.get(WinningTable.SECOND)).isEqualTo(1);
+        assertThat(map.get(WinningTable.THIRD)).isEqualTo(0);
+        assertThat(map.get(WinningTable.FORTH)).isEqualTo(1);
+        assertThat(map.get(WinningTable.FIFTH)).isEqualTo(0);
+        assertThat(map.get(WinningTable.LOSE)).isEqualTo(0);
+    }
+
+    @DisplayName("당첨 결과 가격 리스트 생성")
+    @Test
+    void getWinningPrices() {
+        List<Integer> winningPrices = winningResult.getWinningPrices();
+
+        assertThat(winningPrices.contains(WinningTable.FIRST.getPrice())).isTrue();
+        assertThat(winningPrices.contains(WinningTable.SECOND.getPrice())).isTrue();
+        assertThat(winningPrices.contains(WinningTable.THIRD.getPrice())).isFalse();
+        assertThat(winningPrices.contains(WinningTable.FORTH.getPrice())).isTrue();
+        assertThat(winningPrices.contains(WinningTable.FIFTH.getPrice())).isFalse();
+        assertThat(winningPrices.contains(WinningTable.LOSE.getPrice())).isFalse();
     }
 }
