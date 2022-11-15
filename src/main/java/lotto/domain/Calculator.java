cc57c9a5152fd3838fb8ea00620efb6b4f2c9be7
Sinryuji <siler2474@gmail.com>
2022-11-15 15:46:53 +0900

    fix(Calculator): 결과 리스트 업데이트 시 널 가드 추가

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -8,7 +8,8 @@ public class Calculator {
             int matchBonus = lotto.getNumbers().contains(bonus) ? 1 : 0;
             lotto.getNumbers().retainAll(numbers);
             Rank rank = Rank.findRank(lotto.getNumbers().size(), matchBonus);
-            result.set(rank.getIndex(), result.get(rank.getIndex()) + 1);
+            if (rank != null)
+                result.set(rank.getIndex(), result.get(rank.getIndex()) + 1);
         }
     }
 
