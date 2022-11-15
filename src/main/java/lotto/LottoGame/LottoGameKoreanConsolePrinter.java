7aff9e19ee0a591d5de6d3edc8fa8f959db4184d
조병우 <whquddn55@gmail.com>
2022-11-15 22:35:52 +0900

    refactor(LottoGame): LottoGameeKoreanConsolePrinter에 상수 도입

--- src/main/java/lotto/LottoGame/LottoGameKoreanConsolePrinter.java
+++ src/main/java/lotto/LottoGame/LottoGameKoreanConsolePrinter.java
@@ -6,6 +6,11 @@ import lotto.winning.WinningStatistical;
 import lotto.winning.WinningStatisticalFormatter;
 
 public class LottoGameKoreanConsolePrinter implements LottoGamePrinter {
+    public static final String REQUEST_BUYING_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String LOTTO_COUNT_MESSAGE_FORMATTER = "%d개를 구매했습니다.\n";
+
+    public static final String REQUEST_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String REQUEST_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private final WinningStatisticalFormatter winningStatisticalFormatter;
 
     public LottoGameKoreanConsolePrinter(WinningStatisticalFormatter winningStatisticalFormatter) {
@@ -14,23 +19,23 @@ public class LottoGameKoreanConsolePrinter implements LottoGamePrinter {
 
     @Override
     public void printRequestBuyingPrice() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(REQUEST_BUYING_PRICE_MESSAGE);
     }
 
     @Override
     public void printBoughtLottos(List<Lotto> lottos) {
-        System.out.printf("%d개를 구매했습니다.\n", lottos.size());
+        System.out.printf(LOTTO_COUNT_MESSAGE_FORMATTER, lottos.size());
         lottos.forEach(System.out::println);
     }
 
     @Override
     public void printRequestWinningNumber() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(REQUEST_WINNING_NUMBER_MESSAGE);
     }
 
     @Override
     public void printRequestBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(REQUEST_BONUS_NUMBER_MESSAGE);
     }
 
     @Override
