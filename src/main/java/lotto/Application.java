ebfefe1e345cdef5c58b300ca09e8b0cc0da51f3
comet7001 <comet7001@gmail.com>
2022-11-15 13:34:02 +0900

    debug(application): add try/catch to fulfill a condition in ApplicationTest class

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import statements.ErrorStatements;
 import utils.ConsoleUserInput;
 import utils.ConsoleUserOutput;
+import utils.Validator;
 
 import java.util.List;
 
@@ -10,30 +12,47 @@ public class Application {
     public static ConsoleUserOutput out = new ConsoleUserOutput();
     public static void main(String[] args) {
         int theNumberOfTickets;
+        int bonusNumber;
+        float principal=0;
         List<Integer> winningNumbers;
         List<List<Integer>> lottoNumbersOnTickets;
+        LottoCounter lottoCounter = new LottoCounter();
 
-        out.askHowMuchWouldYouPayForLottos();
-        theNumberOfTickets=LottoTicketing.countTheNumberOfTickets(in.inputMoneyForLottos());
-        out.insertBlankLine();
+        try {
+            out.askHowMuchWouldYouPayForLottos();
+            theNumberOfTickets = LottoTicketing.countTheNumberOfTickets(in.inputMoneyForLottos());
+            principal = theNumberOfTickets * 1000;
+            out.insertBlankLine();
 
-        out.tellNumberOfLottosUserBought(theNumberOfTickets);
-        lottoNumbersOnTickets = LottoTicketing.giveLottoTickets(theNumberOfTickets);
-        for(int lottoSheet=0; lottoSheet<theNumberOfTickets; lottoSheet++) {
-            System.out.println(lottoNumbersOnTickets.get(lottoSheet));
+            out.tellNumberOfLottosUserBought(theNumberOfTickets);
+            lottoNumbersOnTickets = LottoTicketing.giveLottoTickets(theNumberOfTickets);
+            for (int lottoSheet = 0; lottoSheet < theNumberOfTickets; lottoSheet++) {
+                System.out.println(lottoNumbersOnTickets.get(lottoSheet));
+            }
+            out.insertBlankLine();
+
+            out.askWinningNumbers();
+            winningNumbers = in.toIntegerNumbersWithoutComma(in.inputWinningNumbers());
+            out.insertBlankLine();
+
+            out.askBonusNumber();
+            bonusNumber = in.inputBonusNumber(winningNumbers);
+            out.insertBlankLine();
+
+        for(int lottosheet=0; lottosheet<theNumberOfTickets; lottosheet++) {
+            lottoCounter.countHowManyNumbersMatched(lottoNumbersOnTickets.get(lottosheet), winningNumbers,bonusNumber);
         }
-        out.insertBlankLine();
 
-        out.askWinningNumbers();
-        winningNumbers=in.toIntegerNumbersWithoutComma(in.inputWinningNumbers());
-        System.out.println(winningNumbers+"these are winning numbers");
-        out.insertBlankLine();
-
-        out.askBonusNumber();
-        in.inputBonusNumber();
-        out.insertBlankLine();
-
-        //out.showWinningRate();
-
+        out.showResultForm();
+        out.showTheNumberOfThreeMatchedTickets(lottoCounter.threeMatchedTickets());
+        out.showTheNumberOfFourMatchedTickets(lottoCounter.fourMatchedTickets());
+        out.showTheNumberOfFiveMatchedTickets(lottoCounter.fiveMatchedTickets());
+        out.showTheNumberOfFiveAndBonusMatchedTickets(lottoCounter.fiveAndBonusMatchedTickets());
+        out.showTheNumberOfSixMatchedTickets(lottoCounter.sixMatchedTickets());
+        out.showLottoYield(principal, lottoCounter.addAllPrizeMoney());
+        }catch (IllegalArgumentException exit){
+            System.out.println(ErrorStatements.SHOUT_INVALID_FORMAT_ERROR.alarmed());
+            System.out.println(ErrorStatements.EXIT_PROGRAM.alarmed());
+        }
     }
 }
