e94d56dddcb902c2ffa06fc53f3de7eeb3a875af
hayeon <qorgkdus12@naver.com>
2022-11-15 21:46:38 +0900

    feat(exception): add test case

--- src/main/java/lotto/ui/Lotto.java
+++ src/main/java/lotto/ui/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.ui;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -8,12 +10,13 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         rangeValid(numbers);
+        blockDuplication(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate(List<Integer> numbers)throws IllegalArgumentException {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또의 수는 6개까지만 허용한다.");
         }
     }
 
@@ -21,8 +24,18 @@ public class Lotto {
     private void rangeValid(List<Integer> numbers){
         for (int i=0;i<6;i++){
             if (1>numbers.get(i) || numbers.get(i)>45){
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("1~45 사이의 수를 입력해주세요!");
             }
         }
     }
+
+    private void blockDuplication(List<Integer> numbers){
+        // Set 으로 변환
+        Set<Integer> numSet = new HashSet<>(numbers);
+        if(numSet.size()!= numbers.size()){
+            throw new IllegalArgumentException("중복된 요소가 있습니다!");
+        }
+    }
+
+
 }
