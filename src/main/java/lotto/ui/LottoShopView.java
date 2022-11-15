3d8f8b34399b82de6bbd395f54d708b86cd916da
ckyeon <qsky8520@gmail.com>
2022-11-15 20:19:17 +0900

    feat(LottoShopView): add LottoShopView class

---
+++ src/main/java/lotto/ui/LottoShopView.java
@@ -0,0 +1,14 @@
+package lotto.ui;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.Lotto;
+
+public class LottoShopView {
+
+    private final List<Lotto> lottos;
+
+    public LottoShopView(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+}
