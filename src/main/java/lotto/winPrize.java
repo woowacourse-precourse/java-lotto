cac2698463c79d24a732286e51deb4664098bc07
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-15 22:18:09 +0900

    feat : 당첨 등수 분류 기능 클래스와 Enum 활용하기위해 메서드 기능 추가
    Enum을 활용해 등수를 분류하고 메서드를 생성했다.

--- src/main/java/lotto/winPrize.java
+++ src/main/java/lotto/winPrize.java
@@ -1,5 +1,12 @@
 package lotto;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
 public enum winPrize {
+    MISS(0, 0),
     FIFTH(3, 5000),
     FOURTH(4, 50000),
     THIRD(5, 1500000),
@@ -13,4 +20,31 @@ public enum winPrize {
         this.count = count;
         this.prize = prize;
     }
+}
+
+class winPrizeResult{
+
+    private Map<winPrize, Integer> winPrizeResult;
+    public winPrizeResult(){
+        winPrizeResult = new EnumMap<winPrize, Integer>(winPrize.class);
+        for(winPrize value : winPrize.values()){
+            winPrizeResult.put(value, 0);
+        }
+    }
+
+    public void winPrizeCount(winPrize value){
+        winPrizeResult.put(value, winPrizeResult.get(value)+1);
+    }
+
+    public int getCount(winPrize value){
+        return winPrizeResult.get(value);
+    }
+
+    public List<Integer> resultCount() {
+        List<Integer> valuelist = new ArrayList<>();
+        for (winPrize value : winPrize.values()) {
+            valuelist.add(getCount(value));
+        }return valuelist;
+    }
+
 }
