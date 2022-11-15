8c2925c7beec5da1fc04843905e4975e9781913a
leesangjin <tkdwls0628@gmail.com>
2022-11-15 17:32:00 +0900

    feat(User): 당첨 로또 번호 입력 구현

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -3,6 +3,10 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.validator.MoneyAmountValidator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class User {
     public int inputOfAmount(){
         String userInput = Console.readLine();
@@ -10,5 +14,13 @@ public class User {
         int moneyAmount = Integer.parseInt(userInput);
         MoneyAmountValidator.validateMoneyAmount(moneyAmount);
         return moneyAmount;
+    }
+
+    public List<Integer> inputOfWinningNumber() {
+        String userInput = Console.readLine();
+        System.out.println();
+        return Arrays.stream(userInput.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
