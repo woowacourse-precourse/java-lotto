d41cdc2420a00d02ddf6b2438c00400f6697d881
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-15 22:53:00 +0900

    feat: printAskBuyingLottoPriceMessage() 메서드 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+import java.util.Map;
+import lotto.domain.Rank;
+import lotto.domain.User;
+
+public class OutputView {
+    public void printAskBuyingLottoPriceMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+
+
+
+}
