d77cabd548dbbeedeca003841c61c9d0697a22e7
CJiu01 <jiwoo020801@gmail.com>
2022-11-15 23:08:52 +0900

    [Feat] LottoController execute 메소드 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,9 @@
 package lotto.controller;
 
+import lotto.domain.LottoMachine;
+import lotto.domain.LottoPurchaseMoney;
+import lotto.domain.LottoResult;
+import lotto.domain.WinningNumbers;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
@@ -11,4 +15,20 @@ public class LottoController {
         this.inputView = inputView;
         this.resultView = resultView;
     }
+
+    public void execute() {
+        LottoPurchaseMoney lottoPurchaseMoney = new LottoPurchaseMoney(inputView.getMoney());
+        LottoMachine lottoMachine = new LottoMachine(lottoPurchaseMoney);
+
+
+        resultView.printTotalCount(lottoMachine.ticketsCount());
+        resultView.printLottoTickets(lottoMachine.ticketsToString());
+
+        WinningNumbers winningNumbers = new WinningNumbers(inputView.getWinner(), inputView.getBonusNumber());
+        LottoResult lottoResult = lottoMachine.confirm(winningNumbers);
+
+        resultView.printWinnerStat(lottoResult.getRanks());
+        resultView.printEarningRatio(lottoResult.earningRate());
+    }
+
 }
