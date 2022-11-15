f816086075c7ad71d0af7aff193e9009d8a297af
yujaemin <woawoa0513@gmail.com>
2022-11-15 14:23:10 +0900

    feat: 도메인 로직 유효성 검사 함수 추가
    도메인 로직에서 생성할 떄, 중복 번호가 있는지 검사한다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import static lotto.utils.Validation.hasDuplicatedNumber;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -14,7 +16,10 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또에는 총 6개의 번호가 존재해야 합니다.");
+        }
+        if (hasDuplicatedNumber(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복되는 번호가 존재합니다.");
         }
     }
 
