4074b46346d73fa3b4800ba5dcc34d3a8aa18efb
Amyhds <dasomhan524@gmail.com>
2022-11-15 12:09:26 +0900

    test(lottostest): 로또 리스트 생성 테스트 추가

--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -1,9 +1,22 @@
 package lotto;
 
+import lotto.model.Lotto;
 import lotto.model.Lottos;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 class LottosTest {
+    @DisplayName("로또 리스트 생성 테스트")
+    @Test
+    void createLottos() {
+        int ticketAmount = 3;
+        Lottos testLottos = new Lottos(ticketAmount);
+        for (Lotto testLotto : testLottos.lottos) {
+            testLotto.printLotto();
+        }
+    }
 }
