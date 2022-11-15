e4b672e4e1384d7660f4a5dd3335d214f7f958be
Egenieee <hhgg0925@naver.com>
2022-11-15 20:53:12 +0900

    fix: Application 에 try catch block 추가하여 예외를 받아서 종료시키도록 수정함

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,10 @@ import controller.Controller;
 public class Application {
     public static void main(String[] args) {
         Controller controller = new Controller();
-        controller.run();
+        try {
+            controller.run();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
