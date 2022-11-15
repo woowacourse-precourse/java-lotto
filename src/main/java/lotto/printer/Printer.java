a6328aaee6d77bd79f8fd63194dc769d9c7edb53
이지혁 <olzlgur@naver.com>
2022-11-15 17:01:58 +0900

    refactor: 구매 티켓 출력 메서드명 수정

--- src/main/java/lotto/printer/Printer.java
+++ src/main/java/lotto/printer/Printer.java
@@ -13,19 +13,19 @@ public class Printer {
     private static final String MATCH_RESULT = " (%d원) - %d개\n";
     private static final String EARNING_RATE_MESSAGE = "총 수익률은 %.1f입니다.";
 
-    public void requestPurchase() {
+    public static void requestPurchase() {
         System.out.println(PURCHASE_REQUEST_MESSAGE);
     }
 
-    private void printNumberOfTickets(int numberOfTickets) {
+    public static void printNumberOfTickets(int numberOfTickets) {
         System.out.printf(PURCHASE_MESSAGE, numberOfTickets);
     }
 
-    public void requestWinningNumber() {
+    public static void requestWinningNumber() {
         System.out.println(WINNING_NUMBER_REQUEST_MESSAGE);
     }
 
-    public void requestBonusNumber() {
+    public static void requestBonusNumber() {
         System.out.println(BONUS_NUMBER_REQUEST_MESSAGE);
     }
 
