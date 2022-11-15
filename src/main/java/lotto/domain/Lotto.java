e34948c4b8486bfbee4c0e911acc2ebf5779c80e
core <jinjs6@gmail.com>
2022-11-15 17:16:09 +0900

    feat(lotto): 각각의 당첨 번호 유효성 검증 기능 추가
    검증 내용
    - 각각의 당첨 번호의 범위 검증
    - 각각의 당첨 번호 중복 여부 검증
    Lotto 클래스에 구현하였습니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,45 @@
 package lotto.domain;
 
+import lotto.view.ErrorMessage;
+
 import java.util.List;
 
 public class Lotto {
+
+    private static final int LOTTO_SIZE = 6;
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoSize(numbers);
+        validateLottoNumberDuplication(numbers);
+        validateLottoNumberRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateLottoSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateLottoNumberDuplication(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage
+                    .GET_ERROR_FOR_INPUT_WINNING_NUMBER_SIZE
+                    .getMessage());
+        }
+    }
+
+    private void validateLottoNumberRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (!(START_NUMBER <= number && number <= END_NUMBER)) {
+                throw new IllegalArgumentException(ErrorMessage
+                        .GET_ERROR_FOR_INPUT_WINNING_NUMBER_COUNT
+                        .getMessage());
+            }
         }
     }
 
--- src/main/java/lotto/view/ErrorMessage.java
+++ src/main/java/lotto/view/ErrorMessage.java
@@ -2,7 +2,9 @@ package lotto.view;
 
 public enum ErrorMessage {
     GET_ERROR_FOR_PRICE("[ERROR] 천원 단위의 금액을 입력하셔야 합니다."),
-    GET_ERROR_FOR_INPUT_WINNING_NUMBER("[ERROR] 당첨 번호는 콤마(,) 구분으로 입력하셔야 합니다.");
+    GET_ERROR_FOR_INPUT_WINNING_NUMBER("[ERROR] 당첨 번호는 콤마(,) 구분으로 입력하셔야 합니다."),
+    GET_ERROR_FOR_INPUT_WINNING_NUMBER_SIZE("[ERROR] 당첨 번호는 6개를 입력하셔야 합니다."),
+    GET_ERROR_FOR_INPUT_WINNING_NUMBER_COUNT("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
 
     private final String message;
 
