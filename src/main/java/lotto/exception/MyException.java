8bb116af009ee474fc57c141413fe0db4175aeee
KTH-96 <taehyun960@gmail.com>
2022-11-15 18:09:56 +0900

    refactor(MyException): 에러 메세지 출력 하나로 통일

--- src/main/java/lotto/exception/MyException.java
+++ src/main/java/lotto/exception/MyException.java
@@ -3,7 +3,6 @@ package lotto.exception;
 public class MyException extends IllegalArgumentException{
 
     public MyException(ErrorMessage errorMessage) {
-        System.out.println(errorMessage.getMessage());
         throw new IllegalArgumentException(errorMessage.getMessage());
     }
 }
