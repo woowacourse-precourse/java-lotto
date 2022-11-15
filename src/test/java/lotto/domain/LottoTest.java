726f0d0ec2e1a3b4564f6f93263f2cf92c79dfc1
nswon <swnam0729@gmail.com>
2022-11-15 15:39:20 +0900

    test: 로또용지가 생성되는지 확인하는 테스트케이스 작성

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoTest {
+    @DisplayName("Lotto용지가 생성되는지 확인")
+    @Test
+    void when_createLotto_Expect_numbers() {
+        List<Integer> numbers = List.of(1,32,35,29,10,8);
+        Lotto lotto = new Lotto(numbers);
+        assertThat(String.valueOf(lotto.getNumbers())).contains("[1, 32, 35, 29, 10, 8]");
+    }
+}
