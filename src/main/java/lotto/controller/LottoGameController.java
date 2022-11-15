9b6e94364ba13d5421d015c3d1f454c284114c7f
ryanha <ryanha@handong.ac.kr>
2022-11-15 23:31:17 +0900

    refactor(controller): 문자열 하드코딩 요소 제거

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -12,6 +12,22 @@ public class LottoGameController {
     private static final StatisticsBuilder builder = new StatisticsBuilder();
     private static List<Integer> winningNumber;
     private static int bonusNumber;
+    private static final String delimiter = "\n";
+
+    private enum Messages {
+        REQUEST_AMOUNT("구입금액을 입력해 주세요."),
+        REQUEST_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+        REQUEST_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+        private final String message;
+
+        Messages(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return this.message;
+        }
+    }
 
     public static void run() {
         purchaseLotto(); // 구매금액을 입력받아 로또를 구매함
@@ -21,16 +37,16 @@ public class LottoGameController {
     }
 
     public static void purchaseLotto() {
-        String userInput = IOProcessor.getUserInput("구입금액을 입력해 주세요.", "\n");
+        String userInput = IOProcessor.getUserInput(Messages.REQUEST_AMOUNT.getMessage(), delimiter);
         Validator.amountIsValid(userInput, 1000);
         store.purchaseLotto(Integer.parseInt(userInput)); // 로또를 구매함
         IOProcessor.printLottoPurchaseInfo(store.getPurchasedLotto());
     }
 
     public static void getWinningAndBonusNumber() {
-        String userInput = IOProcessor.getUserInput("당첨 번호를 입력해 주세요.", "\n");
+        String userInput = IOProcessor.getUserInput(Messages.REQUEST_WINNING_NUMBER.getMessage(), delimiter);
         winningNumber = Validator.winningNumberIsValid(userInput);
-        userInput = IOProcessor.getUserInput("보너스 번호를 입력해 주세요.", "\n");
+        userInput = IOProcessor.getUserInput(Messages.REQUEST_BONUS_NUMBER.getMessage(), delimiter);
         bonusNumber = Validator.bonusNumberIsValid(userInput, winningNumber);
     }
 
