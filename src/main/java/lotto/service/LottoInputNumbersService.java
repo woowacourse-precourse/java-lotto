0475bfd2ad35e758a4be67656cc27d3d911adb09
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-15 22:45:20 +0900

    feat: LottoInputNumbersService의 필요한 객체 생성

---
+++ src/main/java/lotto/service/LottoInputNumbersService.java
@@ -0,0 +1,14 @@
+package lotto.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoInputNumbersService {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+
+}
