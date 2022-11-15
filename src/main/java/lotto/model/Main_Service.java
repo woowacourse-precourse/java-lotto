ffb64f7ff0ca383c8fc8c43c574908d09ec34bfc
DESKTOP-TAPGE0L\COM <gwj0421@gmail.com>
2022-11-15 14:32:02 +0900

    feat: 랜덤한 로또 번호와 당첨 번호의 교집합을 구하고 크기를 구함
    make_Intersection = 두 인자의 교집합 구함
    make_Difference = 두 인자의 차집합 구함
    count_MatchingNumbers = 구한 교집합의 크기를 구함
    이렇게 구한 교집합의 크기로 대략적인 당첨 요소를 알 수 있음

--- src/main/java/lotto/model/Main_Service.java
+++ src/main/java/lotto/model/Main_Service.java
@@ -2,6 +2,9 @@ package lotto.model;
 
 import lotto.Constant;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main_Service {
     public Constant constant;
 
@@ -9,4 +12,20 @@ public class Main_Service {
         return money / constant.LOTTO_PRICE.getNum();
     }
 
+    private List<Integer> make_Intersection(List<Integer> eachLottoNumbers, List<Integer> winNumbers) {
+        List<Integer> intersection = new ArrayList<>(eachLottoNumbers);
+        intersection.retainAll(winNumbers);
+        return intersection;
+    }
+
+    private List<Integer> make_Difference(List<Integer> eachLottoNumbers, List<Integer> winNumbers) {
+        List<Integer> difference = new ArrayList<>(eachLottoNumbers);
+        difference.removeAll(winNumbers);
+        return difference;
+    }
+
+    public int count_MatchingNumbers(List<Integer> eachLottoNumbers, List<Integer> winNumbers) {
+        List<Integer> intersection = make_Intersection(eachLottoNumbers, winNumbers);
+        return intersection.size();
+    }
 }
