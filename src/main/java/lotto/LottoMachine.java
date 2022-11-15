ed4173b6ceb1b240227f56e28ee330a37b9f000f
Sanmgmin <sangmins930@gmail.com>
2022-11-15 17:23:59 +0900

    feat(LottoMachine): implement sortListInAscending and complete generateLotto

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -2,16 +2,22 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Comparator;
 import java.util.List;
 
 import static lotto.Constants.*;
 
 public class LottoMachine {
+    private static void sortListInAscending(List<Integer> numbers) {
+        numbers.sort(Comparator.naturalOrder());
+    }
+
     public static List<Integer> generateLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
                 MIN_VALUE_OF_LOTTO,
                 MAX_VALUE_OF_LOTTO,
                 NUM_OF_LOTTO);
+        LottoMachine.sortListInAscending(numbers);
         return numbers;
     }
 }
