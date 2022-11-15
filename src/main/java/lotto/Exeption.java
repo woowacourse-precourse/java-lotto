3d0205a41c4105177b93180379f51bedcac74452
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-15 23:37:51 +0900

    Feat<Exeption> : add LottoNumber input Exeption methods

--- src/main/java/lotto/Exeption.java
+++ src/main/java/lotto/Exeption.java
@@ -1,8 +1,70 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 public class Exeption {
+    // String to List<Integer>
+    public static List<Integer> stringToList(String input) {
+        List<String> splitInput = Arrays.asList(input.split(","));
+        // String to Integer // 중복 제거
+        return splitInput.stream()
+                .map(Integer::parseInt)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    // input 예외처리
+    public static void inputWinningNumberExeption(String input, boolean isFirst) {
+        List<String> splitInput = Arrays.asList(input.split(","));
+        inputWinningNumberExeptionDetail(splitInput);
+        StringToInteger(splitInput, isFirst);
+    }
+
+    //List<String> to List<Integer>
+    public static void StringToInteger(List<String> input, boolean isFirst) {
+        int length = input.size();
+        List<Integer> numbers = input.stream()
+                .map(Integer::parseInt)
+                .distinct()
+                .collect(Collectors.toList());
+        wrongNumberSize(numbers, length, isFirst);
+
+    }
+
+    public static void wrongNumberSize(List<Integer> numbers, int length, boolean isFirst) {
+        if (length != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복입력 하였습니다.");
+        }
+        // 첫 입력
+        if (isFirst) {
+            if (numbers.size() != 6) {
+                throw new IllegalArgumentException("[ERROR] 숫자 개수를 잘못입력하셨습니다.");
+            }
+        }
+        // 보너스 입력
+        if (!isFirst) {
+            if (numbers.size() != 1) {
+                throw new IllegalArgumentException("[ERROR] 숫자 개수를 잘못입력하셨습니다.");
+            }
+        }
+
+    }
+
+    //로또 번호에 숫자가 아닌 문자가 들어갔을 때
+    public static void inputWinningNumberExeptionDetail(List<String> splitInput) {
+        splitInput.stream().forEach((num) -> {
+            if (!num.matches("[+-]?\\d*(\\.\\d+)?")) {
+                throw new IllegalArgumentException("[ERROR] 숫자가 아닙니다.");
+            }
+            int number = Integer.parseInt(num);
+            if (number < 1 && number > 45) {
+                throw new IllegalArgumentException("[ERROR] 숫자는 1에서 45사이입니다.");
+            }
+        });
+    }
     public static void inputMoneyExeption(String money) {
         if (!money.matches("[+-]?\\d*(\\.\\d+)?")) {
             System.out.println("[ERROR]");
