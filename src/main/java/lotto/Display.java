d13e2451b8284c48f25fed77cd91e7d7121463cb
fnzl54 <rnjscksdud1@gmail.com>
2022-11-15 20:46:09 +0900

    Feat: create displayLottoPaper

--- src/main/java/lotto/Display.java
+++ src/main/java/lotto/Display.java
@@ -1,4 +1,14 @@
 package lotto;
 
+import java.util.List;
+
 public class Display {
+    static void displayLottoPaper (List<List<Integer>> LottoPaper){
+
+        System.out.println(LottoPaper.size() + "개를 구매했습니다.");
+
+        for (int i = 0; i < LottoPaper.size(); i++) {
+            System.out.println(LottoPaper.get(i));
+        }
+    }
 }
