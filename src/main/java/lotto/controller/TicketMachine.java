d1de198a6abecfab76733a82769edd4d866ec4a7
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-15 14:27:15 +0900

    feat(main\...\controller) : implement ticketMachine class

--- src/main/java/lotto/controller/TicketMachine.java
+++ src/main/java/lotto/controller/TicketMachine.java
@@ -1,7 +1,16 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TicketMachine {
-    public static List<List<Integer>> issueTickets(int )
+
+    public static List<List<Integer>> issueTickets(int ticketNumber) {
+        List<List<Integer>> tickets = new ArrayList<>();
+        while (tickets.size() < ticketNumber) {
+            tickets.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        }
+        return tickets;
+    }
 }
