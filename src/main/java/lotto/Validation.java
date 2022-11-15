8c30b14149137b0bd54e7f12a954b9d9cd05cba1
kwondaehwi <kwondh8561@ajou.ac.kr>
2022-11-15 17:24:15 +0900

    feat: Validation 클래스 생성

---
+++ src/main/java/lotto/Validation.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+public class Validation {
+
+    private static final int LOTTO_SIZE = 6;
+
+    public void isValidNumber(int number) {
+
+    }
+
+    public void isValidPrice(int price) {
+
+    }
+
+    public void isValidWinningNumbers(List<Integer> numbers) {
+
+    }
+
+    public void isValidSize(List<Integer> numbers) {
+
+    }
+
+    public void isValidWinningNumbersInput(String input) {
+
+    }
+
+    public void hasDuplicateNumber(List<Integer> numbers) {
+
+    }
+}
