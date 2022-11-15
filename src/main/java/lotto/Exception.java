9ffaed089eacff564d3ed5677a7f00c4d852ca63
masiljangajji <hjk172262@gmail.com>
2022-11-15 20:54:30 +0900

    feat: 보너스 숫자의 사이즈 검사 기능 추가

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -4,27 +4,38 @@ import view.Message;
 
 public class Exception {
 
+    public void checkPurchaseAmount(String purchaseAmount) {
+        checkPurchaseAmountRange(purchaseAmount);
+        checkNoRemainder(purchaseAmount);
+    }
+
     public void checkPurchaseAmountRange(String purchaseAmount) {
-        for (int i = 0; i < purchaseAmount.length(); i++) {
+        if(purchaseAmount.charAt(0)=='0'){
+            throw new IllegalArgumentException(Message.ERROR_START_ZERO.getMessage());
+        }
+        for (int i = 1; i < purchaseAmount.length(); i++) {
             if (!(purchaseAmount.charAt(i) >= '0' && purchaseAmount.charAt(i) <= '9')) {
                 throw new IllegalArgumentException(Message.ERRROR_NUMBER_RANGE.getMessage());
             }
         }
     }
 
-    public void checkZeroRemainder(String purchaseAmount) {
+    public void checkNoRemainder(String purchaseAmount) {
         int Amount = Integer.parseInt(purchaseAmount);
         if (Amount % 1000 != 0) {
             throw new IllegalArgumentException(Message.ERROR_NOT_ZERO_REMAINDER.getMessage());
         }
     }
 
-    public void checkBonusNumber(String bonusNumber){
-        if(bonusNumber.length()!=1){
+
+
+    public void checkBonusNumberSize(String bonusNumber) {
+        if (!(bonusNumber.length() >= 1 && bonusNumber.length() <= 2)) {
             throw new IllegalArgumentException(Message.ERROR_BONUS_SIZE.getMessage());
         }
-        if(!(bonusNumber.charAt(0)>='0'&&bonusNumber.charAt(0)<='9')){
-            throw new IllegalArgumentException(Message.ERRROR_NUMBER_RANGE.getMessage());
-        }
     }
+
+
+
+
 }
