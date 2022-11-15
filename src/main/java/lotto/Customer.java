05b147ba3cc0fc3fe7a9719f25986768487a3f03
LeeJE20 <silver36c@gmail.com>
2022-11-15 11:07:55 +0900

    style: Customer 자동정렬 적용

--- src/main/java/lotto/Customer.java
+++ src/main/java/lotto/Customer.java
@@ -6,29 +6,28 @@ import lotto.ui.Ready;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Customer {
-    private static final int UNIT = 1000;
-    private int amount;
+	private static final int UNIT = 1000;
+	private int amount;
 
-    public void inputAmount() {
-        System.out.println(Ready.INSERT_AMOUNT.getValue());
-        String amount = readLine();
-        try {
-        	validateAmount(amount);
-        	this.amount = Integer.parseInt(amount);
-        }
-        catch (IllegalArgumentException e) {
-        	System.out.println(ErrorMessage.IllegalArgumentException.getMessage());
-        }
-    }
+	public void inputAmount() {
+		System.out.println(Ready.INSERT_AMOUNT.getValue());
+		String amount = readLine();
+		try {
+			validateAmount(amount);
+			this.amount = Integer.parseInt(amount);
+		} catch (IllegalArgumentException e) {
+			System.out.println(ErrorMessage.IllegalArgumentException.getMessage());
+		}
+	}
 
-    public int getAmount() {
-        return amount;
-    }
+	public int getAmount() {
+		return amount;
+	}
 
-    private boolean validateAmount(String amount) {
-        if (!Util.isInteger(amount) || Integer.parseInt(amount) % UNIT != 0)
-            throw (new IllegalArgumentException());
-        return true;
-    }
+	private boolean validateAmount(String amount) {
+		if (!Util.isInteger(amount) || Integer.parseInt(amount) % UNIT != 0)
+			throw (new IllegalArgumentException());
+		return true;
+	}
 
 }
