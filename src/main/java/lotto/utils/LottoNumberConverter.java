f5d499286c1cc11d625dbbcd996c949deba4edcf
mikekang47 <gyeong0308@gmail.com>
2022-11-15 19:13:04 +0900

    Feat<LottoNumberConverter>: 문자열 로또 번호를 잘라 리스트로 반환하는 로직 추기

--- src/main/java/lotto/utils/LottoNumberConverter.java
+++ src/main/java/lotto/utils/LottoNumberConverter.java
@@ -1,11 +1,26 @@
 package lotto.utils;
 
+import lotto.domains.Lotto;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoNumberConverter {
+    private static final String NUMERIC_ERROR_MSG = "[ERROR] 반드시 쉼표를 기준으로 구분되어야 합니다.";
+
     public static String convertToString(List<Integer> numbers) {
         return numbers.stream().map(Object::toString)
                 .collect(Collectors.joining(", "));
+    }
+
+    public static List<Integer> convertWinningNumberToList(String bonusNumber) {
+        try {
+            return Arrays.stream(bonusNumber.split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMERIC_ERROR_MSG);
+        }
     }
 }
