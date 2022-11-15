05704661dd716cb40fcc915b4efb618dfc548724
Park Sungchul <try615@naver.com>
2022-11-15 20:18:27 +0900

    feat(ConsoleIn.java): 당첨 번호 입력 기능 추가
    당첨 번호 6개를 입력받는 기능 추가

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -9,7 +9,9 @@ public class Constants {
 
 	public static final String PRINT_INPUT_MONEY = "구입금액을 입력해 주세요.";
 	public static final String PRINT_LOTTO_HISTORY = "개를 구매했습니다.";
+	public static final String DELIMITER = ",";
 
 	public static final String ERROR_NOT_NUMBER_FORMAT = "[ERROR] 숫자를 입력해 주십시오.";
 	public static final String ERROR_NOT_MONEY_UNIT = "[ERROR] 구매 금액 단위를 맞춰 주십시오.";
+
 }
