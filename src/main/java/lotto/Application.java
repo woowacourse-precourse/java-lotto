bc7a75d807ab5f856a13d09e6b6ddb9da565b8b8
fnzl54 <rnjscksdud1@gmail.com>
2022-11-15 20:55:50 +0900

    Feat: LottoMachie Object in main method

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -8,5 +9,9 @@ public class Application {
 
         System.out.println("구입금액을 입력해 주세요.");
         String inputMoney = readLine();
+
+        LottoMachine game = new LottoMachine();
+        List<List<Integer>> LottoPaper = game.makeBuyLottoPaper(inputMoney);
+        Display.displayLottoPaper(LottoPaper);
     }
 }
