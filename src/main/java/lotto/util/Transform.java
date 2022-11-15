8183538e1eea16c2669f4a9a2a09ee34e32f250b
parkjungyoon <tbr06057@naver.com>
2022-11-15 15:15:59 +0900

    refactor(Util): 문자열 구분 문자 주변 공백 제거 후 split하도록 코드 리팩터링

--- src/main/java/lotto/util/Transform.java
+++ src/main/java/lotto/util/Transform.java
@@ -1,11 +1,14 @@
 package lotto.util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Transform {
     public List<String> splitLottoNumbers(String input) {
-        String[] splitedInput = input.split(",");
+        String[] splitedInput = Arrays.stream(input.split(","))
+                .map(String::trim)
+                .toArray(String[]::new);
         List<String> numbers = new ArrayList<>(List.of(splitedInput));
         return numbers;
     }
