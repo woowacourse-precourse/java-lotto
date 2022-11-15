54d8d12520befa03cc34b4f7692d8207b7fb9d0e
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-15 15:26:05 +0900

    test(InputViewTest): 문자, 공백이 포함될 경우 예외 처리 메서드 이름 변경

--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -10,7 +10,7 @@ public class InputViewTest {
     @DisplayName("입력값이 숫자가 아니면 예외가 발생한다.")
     @Test
     void getInputByNonNumeric() {
-        assertThatThrownBy(() -> new InputView().validate("-10000"))
+        assertThatThrownBy(() -> new InputView().validateNumeric("-10000"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 입력값은 숫자만 가능합니다.");
     }
