abf3f9e0183d4bb0cb8f80a666afbe7c333cb802
Bumsoo <rhqjatn2398@naver.com>
2022-11-15 15:48:07 +0900

    test(LottoResultCalculatorTest): 당첨 금액을 구하는 기능의 테스트 코드 작성

--- src/test/java/lotto/LottoResultCalculatorTest.java
+++ src/test/java/lotto/LottoResultCalculatorTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -22,7 +23,7 @@ class LottoResultCalculatorTest {
         lottoResultCalculator.setBonusNum(7);
     }
 
-    private static EnumMap<Rank, Integer> expectedConstructor(List<Integer> values) {
+    private static EnumMap<Rank, Integer> ranksConstructor(List<Integer> values) {
         return new EnumMap<>(Map.ofEntries(
                 Map.entry(Rank.FIRST_PRIZE, values.get(0)),
                 Map.entry(Rank.SECOND_PRIZE, values.get(1)),
@@ -35,14 +36,14 @@ class LottoResultCalculatorTest {
 
     static Stream<Arguments> parameterProvider() {
         List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), new Lotto(List.of(1, 2, 3, 4, 7, 8)));
-        EnumMap<Rank, Integer> expected = expectedConstructor(List.of(1, 0, 0, 1, 0, 0));
+        EnumMap<Rank, Integer> expected = ranksConstructor(List.of(1, 0, 0, 1, 0, 0));
 
         List<Lotto> lottos1 = List.of(new Lotto(List.of(8, 9, 10, 11, 12, 13)),
                 new Lotto(List.of(1, 2, 3, 21, 22, 23)));
-        EnumMap<Rank, Integer> expected1 = expectedConstructor(List.of(0, 0, 0, 0, 1, 1));
+        EnumMap<Rank, Integer> expected1 = ranksConstructor(List.of(0, 0, 0, 0, 1, 1));
 
         List<Lotto> lottos2 = List.of(new Lotto(List.of(1, 9, 10, 11, 12, 13)), new Lotto(List.of(1, 2, 3, 4, 5, 7)));
-        EnumMap<Rank, Integer> expected2 = expectedConstructor(List.of(0, 1, 0, 0, 0, 1));
+        EnumMap<Rank, Integer> expected2 = ranksConstructor(List.of(0, 1, 0, 0, 0, 1));
 
         return Stream.of(
                 arguments(lottos, expected),
@@ -55,5 +56,21 @@ class LottoResultCalculatorTest {
     @MethodSource("parameterProvider")
     void calRankAll(List<Lotto> lottos, EnumMap<Rank, Integer> expected) {
         assertThat(lottoResultCalculator.calRankAll(lottos)).isEqualTo(expected);
+    }
+
+    @Test
+    void 당첨_금액_구하기() {
+        EnumMap<Rank, Integer> ranks = ranksConstructor(List.of(2, 1, 5, 7, 3, 1));
+        long expected = 2_000_000_000L * 2 + 30_000_000L * 1 + 1_500_000 * 5 + 50_000 * 7 + 5_000 * 3 + 0 * 1;
+
+        assertThat(lottoResultCalculator.calWinningAmount(ranks)).isEqualTo(expected);
+    }
+
+    @Test
+    void 당첨_금액_구하기1() {
+        EnumMap<Rank, Integer> ranks = ranksConstructor(List.of(0, 0, 0, 0, 0, 50));
+        long expected = 0;
+
+        assertThat(lottoResultCalculator.calWinningAmount(ranks)).isEqualTo(expected);
     }
 }
