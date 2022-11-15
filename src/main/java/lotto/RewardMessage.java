8c6c7bbd5a9b388d8f6f0fb71446fff7515756b7
Fizioo0102 <a27313797@gmail.com>
2022-11-15 22:43:16 +0900

    feat: 등수 카운트 구현
    - enumMap에 저장한 정보를 iterator로 꺼내와 해당 등수가 몇번 당첨되었는지 알려주는 함수 구현.

--- src/main/java/lotto/RewardMessage.java
+++ src/main/java/lotto/RewardMessage.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.Iterator;
+import java.util.Map;
+
 public class RewardMessage {
 
     private static final String FIRST_MESSAGE ="6개 일치 (2,000,000,000원) - ";
@@ -11,4 +14,30 @@ public class RewardMessage {
     private static final String ALL_YIELD_MESSAGE_FRONT = "총 수익률은 ";
     private static final String ALL_YIELD_MESSAGE_BACK = "%입니다.";
 
+    static void prizeMessage(Map<Rank,Integer> ranking){
+        Iterator<Rank> iterator = ranking.keySet().iterator();
+        while (iterator.hasNext()) {
+            Rank key = iterator.next();
+            int value = ranking.get(key);
+            matchPrizeMessage(key,value);
+        }
+    }
+
+    static void matchPrizeMessage(Rank key, int value) {
+        if(key.equals(Rank.FIVE)){
+            System.out.println(FIVTH_MESSAGE+value+AMOUNT);
+        }
+        if(key.equals(Rank.FOUR)){
+            System.out.println(FOURTH_MESSAGE+value+AMOUNT);
+        }
+        if(key.equals(Rank.THIRD)){
+            System.out.println(THIRD_MESSAGE+value+AMOUNT);
+        }
+        if(key.equals(Rank.SECOND)){
+            System.out.println(SECOND_MESSAGE+value+AMOUNT);
+        }
+        if(key.equals(Rank.FIRST)){
+            System.out.println(FIRST_MESSAGE+value+AMOUNT);
+        }
+    }
 }
