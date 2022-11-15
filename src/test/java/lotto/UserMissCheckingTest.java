01857772e58a47a6ff2f4a44eede1af001c96267
ijung-won <dlwnddnjs96@gmail.com>
2022-11-15 18:35:54 +0900

    test(UserMissCheckingTest): 로또번호 입력 시 중복 번호 기입 에러 테스트

--- src/test/java/lotto/UserMissCheckingTest.java
+++ src/test/java/lotto/UserMissCheckingTest.java
@@ -9,8 +9,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserMissCheckingTest {
 
-
-
+    private final List<Integer> wrongLotto = new ArrayList<>(List.of(1,3,3,6,12,35));
+    private final List<Integer> wrongLotto2 = new ArrayList<>(List.of(1,3,3,6,3,35));
+    private final List<Integer> wrongLotto3= new ArrayList<>(List.of(1,3,3,6,6,35));
     @Test
     public void 숫자대신_문자입력_에러(){
         assertThatThrownBy(() -> UserMissChecking.numberChecking("사십사"))
@@ -36,6 +37,16 @@ class UserMissCheckingTest {
         assertThatThrownBy(()->UserMissChecking.numberLimitChecking(0))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(()->UserMissChecking.numberLimitChecking(47))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 로또_당침번호_입력시_중복체크(){
+        assertThatThrownBy(()-> UserMissChecking.numRepeatCheck(wrongLotto))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()-> UserMissChecking.numRepeatCheck(wrongLotto2))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()-> UserMissChecking.numRepeatCheck(wrongLotto3))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
