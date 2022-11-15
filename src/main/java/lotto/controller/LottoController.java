0d68cb0ca7d8dd83a83c088872527eed8057d2d7
desfox <desfox99@naver.com>
2022-11-15 16:38:04 +0900

    refactor(LottoController): 당첨 횟수 증가시키는 로직 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -97,15 +97,20 @@ public class LottoController {
             int predictionMatchCnt = matchCnt.get(IDX_FOR_PREDICTION_MATCH_CNT);
             int bonusMatchCnt = matchCnt.get(IDX_FOR_BONUS_MATCH_CNT);
 
-            if (predictionMatchCnt == FIFTH.getPredictionMatchCnt()) winningLog[FIFTH.getWinningLogIdx()]++;
-
-            if (predictionMatchCnt == FOURTH.getPredictionMatchCnt()) winningLog[FOURTH.getWinningLogIdx()]++;
-
-            if (predictionMatchCnt == THIRD.getPredictionMatchCnt()) winningLog[THIRD.getWinningLogIdx()]++;
-
-            if (predictionMatchCnt == SECOND.getPredictionMatchCnt() && bonusMatchCnt == SECOND.getBonusMatchCnt()) winningLog[SECOND.getWinningLogIdx()]++;
-
-            if (predictionMatchCnt == FIRST.getPredictionMatchCnt()) winningLog[FIRST.getWinningLogIdx()]++;
+            increaseWinningLogForRank(predictionMatchCnt, bonusMatchCnt);
         }
+    }
+
+    private void increaseWinningLogForRank(int predictionMatchCnt, int bonusMatchCnt) {
+        if (predictionMatchCnt == FIFTH.getPredictionMatchCnt()) winningLog[FIFTH.getWinningLogIdx()]++;
+
+        if (predictionMatchCnt == FOURTH.getPredictionMatchCnt()) winningLog[FOURTH.getWinningLogIdx()]++;
+
+        if (predictionMatchCnt == THIRD.getPredictionMatchCnt()) winningLog[THIRD.getWinningLogIdx()]++;
+
+        if (predictionMatchCnt == SECOND.getPredictionMatchCnt())
+            if (bonusMatchCnt == SECOND.getBonusMatchCnt()) winningLog[SECOND.getWinningLogIdx()]++;
+
+        if (predictionMatchCnt == FIRST.getPredictionMatchCnt()) winningLog[FIRST.getWinningLogIdx()]++;
     }
 }
