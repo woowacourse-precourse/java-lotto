eadd87ab79eb7798378c6a41d778b33387df7f70
Eunkeee <eunkeeeee@gmail.com>
2022-11-15 21:52:06 +0900

    refactor(Player): sorting 코드 개선

--- src/main/java/lotto/model/Player.java
+++ src/main/java/lotto/model/Player.java
@@ -6,8 +6,8 @@ import static lotto.util.Constants.NUMBER_COUNTS;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Player {
 
@@ -24,16 +24,11 @@ public class Player {
     }
 
     private static List<Integer> createPlayerNumbers() {
-        return sortInAscendingOrders(createSixRandomUniqueNumbers());
+        return createSixRandomUniqueNumbers().stream().sorted().collect(Collectors.toList());
     }
 
     private static List<Integer> createSixRandomUniqueNumbers() {
         return new ArrayList<>(Randoms.pickUniqueNumbersInRange(LOTTO_RANGE_MIN, LOTTO_RANGE_MAX, NUMBER_COUNTS));
-    }
-
-    private static List<Integer> sortInAscendingOrders(List<Integer> playerNumbers) {
-        Collections.sort(playerNumbers);
-        return playerNumbers;
     }
 
     public List<List<Integer>> getAllPlayerNumbers() {
