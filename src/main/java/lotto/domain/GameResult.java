7b8f52c07d991c9c828fa531330ad8d72f1c8dca
dingding-21 <phj8221@naver.com>
2022-11-15 20:32:36 +0900

    feat($GameResult): 당첨 기준에 따른 로또 등수를 구하는 기능 구현

--- src/main/java/lotto/domain/GameResult.java
+++ src/main/java/lotto/domain/GameResult.java
@@ -15,7 +15,21 @@ public class GameResult {
         return count;
     }
 
-    private static boolean isContained(List<Integer> lotto, int number) {
+    public static boolean isContained(List<Integer> lotto, int number) {
         return lotto.contains(number);
+    }
+
+    public static Integer getGameRank(List<Integer> randomLotto, List<Integer> inputLotto, int bonusNumber) {
+        int count = getMatchingCount(randomLotto, inputLotto);
+
+        if (count == 6) {
+            return 1;
+        } else if (count == 5) {
+            if (isContained(randomLotto, bonusNumber)) return 2;
+            return 3;
+        } else if (count == 4) {
+            return 4;
+        }
+        return 5;
     }
 }
