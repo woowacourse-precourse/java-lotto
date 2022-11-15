440c5efd2370563ddef50c18daf6b386517a6556
seonpilKim <ksp970306@gmail.com>
2022-11-15 19:43:42 +0900

    Feat: 당첨 통계 출력 단계 구현 및 역할 분리 리팩토링

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,72 +1,40 @@
 package lotto;
 
-import static lotto.Constants.*;
-
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.LotteryMachine;
 import lotto.domain.Lotto;
+import lotto.domain.Reader;
 import lotto.domain.Seller;
+import lotto.domain.Writer;
 
 public class Application {
 
-	private static final String INPUT_PURCHASE_AMOUNT_ANNOUNCEMENT = "구입금액을 입력해 주세요.";
-	private static final String INPUT_WINNING_NUMBERS_ANNOUNCEMENT = "당첨 번호를 입력해 주세요.";
-	private static final String INPUT_BONUS_NUMBER_ANNOUNCEMENT = "보너스 번호를 입력해 주세요.";
-
 	public static void main(String[] args) {
-		final Integer money = readPurchaseAmount();
-		final List<Lotto> tickets = Seller.sellLottoTickets(money);
-		writeLottoNumbers(tickets);
-		final LotteryMachine machine = drawAndGetMachine();
+		try {
+			final List<Lotto> tickets = buyLottoTickets();
+			final LotteryMachine machine = drawAndGetMachine();
+			Writer.writeWinningStats(tickets, machine);
+		} catch (IllegalArgumentException e) {
+			Writer.writeExceptionMessage(e);
+		}
 	}
 
 	private static LotteryMachine drawAndGetMachine() {
-		final List<Integer> numbers = readWinningNumbers();
+		Writer.writeInputWinningNumbers();
+		final List<Integer> numbers = Reader.readWinningNumbers();
 		final LotteryMachine machine = LotteryMachine.of(numbers);
-		final Integer bonusNumber = readBonusNumber();
+		Writer.writeInputBonusNumber();
+		final Integer bonusNumber = Reader.readBonusNumber();
 		machine.setBonusNumber(bonusNumber);
 		return machine;
 	}
 
-	private static Integer readBonusNumber() {
-		System.out.println(INPUT_BONUS_NUMBER_ANNOUNCEMENT);
-		try {
-			return Integer.valueOf(Console.readLine());
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException(ErrorMessage.isInvalidNumber());
-		}
-	}
-
-	private static List<Integer> readWinningNumbers() {
-		System.out.println(INPUT_WINNING_NUMBERS_ANNOUNCEMENT);
-		try {
-			return Arrays.stream(Console.readLine()
-					.split(COMMA))
-				.map(Integer::valueOf)
-				.collect(Collectors.toList());
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException(ErrorMessage.isInvalidNumber());
-		}
-	}
-
-	private static void writeLottoNumbers(List<Lotto> tickets) {
-		tickets.stream()
-			.map(Lotto::getNumbers)
-			.map(List::toString)
-			.forEach(System.out::println);
-	}
-
-	private static Integer readPurchaseAmount() {
-		System.out.println(INPUT_PURCHASE_AMOUNT_ANNOUNCEMENT);
-		try {
-			return Integer.valueOf(Console.readLine());
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException(ErrorMessage.isInvalidNumber());
-		}
+	private static List<Lotto> buyLottoTickets() {
+		Writer.writeInputPurchaseAnnouncement();
+		final List<Lotto> tickets = Seller.sellLottoTickets(Reader.readPurchaseAmount());
+		Writer.writePurchasedLottoTickets(tickets);
+		return tickets;
 	}
 
 }
