f90b55902ff8c214f4354b97430f3e45b14da42e
kimsinhyun <ksh415622@naver.com>
2022-11-15 22:01:03 +0900

    fix: (Lotto Class) 로또 클래쓰 수정
    1. 해당 클래쓰 안에 있던 유효성 검증 클래쓰들을 모두 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,60 +1,47 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
-    public final int LOTTO_LENGTH = 6;
     private final List<Integer> numbers;
 
-    public HashSet<Integer> checkDuplicate;
-    public int bonusNumber;
+    public static int bonusNumber;
+    private static HashSet checkDuplicate = new HashSet();
 
     public Lotto(List<Integer> numbers) {
-        validateLength(numbers);
-        validateDuplicate(numbers);
+        Validations.checkLength(numbers);
+        Validations.checkDuplication(numbers);
 
-        this.numbers = numbers;
+        // 로또 번호를 부여하기 전 정렬.
+        ArrayList sortedNumbers = new ArrayList(numbers);
+        Collections.sort(sortedNumbers);
+        this.numbers = sortedNumbers;
     }
 
-    public void setBonusNumber(String bonusNumberInput){
-        int bonusNumber;
+    public void setBonusNumber(){
+        String temp = Console.readLine();
 
-        validateBonusNumberIsNumeric(bonusNumberInput);
-        bonusNumber = Integer.parseInt(bonusNumberInput);
+        Validations.checkBonusNumberIsNumeric(temp);
+        bonusNumber = Integer.parseInt(temp);
 
-        validateBonusNumberDuplicate(bonusNumber);
-        this.bonusNumber = bonusNumber;
+        Validations.checkBonusNumberDuplicate(bonusNumber, numbers);
+        Validations.checkNumberRange(bonusNumber);
     }
 
-    private void validateLength(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_LENGTH) {
-            Error.ILLEGAL_LOTTO_LENGTH.wrongInput();
-        }
+    // 테스트용 오버로딩 함수
+    public void setBonusNumber(String temp ){
+        Validations.checkBonusNumberIsNumeric(temp);
+        bonusNumber = Integer.parseInt(temp);
+
+        Validations.checkBonusNumberDuplicate(bonusNumber, numbers);
+        Validations.checkNumberRange(bonusNumber);
     }
 
-    private void validateDuplicate(List<Integer> numbers) {
-        checkDuplicate = new HashSet<Integer>();
-        for (int n: numbers) {
-            if (!checkDuplicate.add(n)){
-                Error.LOTTO_NUM_DUPLICATE_ERROR.wrongInput();
-            }
-        }
-    }
-
-    private void validateBonusNumberIsNumeric(String bonusNumberInput){
-        try{
-            int temp = Integer.parseInt(bonusNumberInput);
-        }
-        catch(IllegalArgumentException e){
-            Error.NOT_NUMERIC.wrongInput();
-        }
-    }
-
-    private void validateBonusNumberDuplicate(int bonusNumber){
-        if(!checkDuplicate.add(bonusNumber)){
-            Error.LOTTO_NUM_DUPLICATE_ERROR.wrongInput();
-        }
+    public List getNumbers(){
+        return this.numbers;
     }
 }
