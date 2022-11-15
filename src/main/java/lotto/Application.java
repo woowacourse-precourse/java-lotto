2fef4f83a3164d25cee73ffd29b0661a821e2849
Cho Changhun <cvcvcx@naver.com>
2022-11-15 12:27:01 +0900

    test: 예외 결과값 반환 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,20 +11,21 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("구입금액을 입력해 주세요");
         String moneyString = Console.readLine();
-        int money = getMoney(moneyString);
+        checkMoneyInt(moneyString);
+        int money = Integer.parseInt(moneyString);
         BuyLotto buyLotto = new BuyLottoImpl(LOTTO_PRICE,money);
         GoalLotto goalLotto = new GoalLottoImpl();
         Result result = new ResultImpl(buyLotto,goalLotto,money);
         result.printResult();
     }
 
-    private static int getMoney(String moneyString) {
-        int money = 0;
+    private static void checkMoneyInt(String moneyString) {
+
         try {
-            money = Integer.parseInt(moneyString);
-        }catch (IllegalArgumentException e){
-            throw new IllegalArgumentException("[ERROR] 입력된 금액이 올바르지 않습니다!");
+            Integer.parseInt(moneyString);
+        }catch (NumberFormatException e){
+           throw new IllegalArgumentException("[ERROR] 입력된 금액이 올바르지 않습니다!");
         }
-        return money;
+
     }
 }
