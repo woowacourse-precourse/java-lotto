0b4c4d340c778387927ebd7e457517bb180314e8
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-15 22:35:09 +0900

    feat: UserLotto 생성자 추가

--- src/main/java/lotto/domain/UserLotto.java
+++ src/main/java/lotto/domain/UserLotto.java
@@ -5,5 +5,11 @@ import java.util.List;
 public class UserLotto {
     private List<Integer> lottoNumbers;
 
+    public UserLotto(List<Integer> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+
+
+
 
 }
