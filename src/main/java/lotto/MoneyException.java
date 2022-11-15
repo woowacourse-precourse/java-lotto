2d9c03c6272abde5326e95f0648888c563cb083d
yon11b <70383140+yon11b@users.noreply.github.com>
2022-11-15 18:55:00 +0900

    remove: move to ui directory

--- src/main/java/lotto/MoneyException.java
+++
@@ -1,9 +0,0 @@
-package lotto;
-
-public class MoneyException {
-    public MoneyException(int money) {
-        if (money % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 금액은 1000원으로 나누어 떨어져야 합니다.");
-        }
-    }
-}
