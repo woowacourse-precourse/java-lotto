eaf0cc6a5e3594a8acdd5c6ab25c293764ef388a
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-15 21:33:50 +0900

    refactor(test\...\controller) : refactor earning rate caculator test

--- src/main/java/lotto/controller/EarningRateCalculator.java
+++ src/main/java/lotto/controller/EarningRateCalculator.java
@@ -3,14 +3,17 @@ package lotto.controller;
 import static lotto.controller.Constants.PERCENTAGE;
 
 import java.util.Arrays;
+import lotto.Lotto;
+import lotto.model.LottoStorage;
 
 public class EarningRateCalculator {
 
-    public static float calculateEarningRate(String inputMomey) {
+    public static double calculateEarningRate() {
+        int inputMoney = Integer.parseInt(LottoStorage.getInputMoney());
         int prizeSum = 0;
-        for(CorrectNumber correctNumber : CorrectNumber.values()) {
-
+        for(CorrectNumber correctNumber : LottoStorage.getResultCollectionStorage()) {
+            prizeSum += correctNumber.getCashPrize();
         }
-        return (float) (prizeSum / Integer.parseInt(inputMomey))*PERCENTAGE;
+        return ((double)prizeSum / inputMoney) * PERCENTAGE;
     }
 }
--- src/test/java/lotto/controller/EarningRateCalculatorTest.java
+++ src/test/java/lotto/controller/EarningRateCalculatorTest.java
@@ -1,23 +1,29 @@
 package lotto.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import lotto.model.LottoStorage;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
+import static lotto.controller.Constants.PERCENTAGE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EarningRateCalculatorTest {
 
-    @Disabled
     @DisplayName("수익률 계산")
-    @ParameterizedTest
-    @CsvSource(value = {"THREE, 62.5", "SIX, 25_000_000", "LESS_THAN_THREE, 0"})
-    void calculateEarningRate(String constants, float expectedRate) {
+    @Test
+    void calculateEarningRate() {
         int inputMoney = 8_000;
-
-        assertThat(EarningRateCalculator.calculateEarningRate(LottoStorage.getInputMoney()))
-                .isEqualTo(expectedRate);
+        int prizeSum = 0;
+        double expected = 62.5;
+        List<CorrectNumber> resultCollection = new ArrayList<>();
+        resultCollection.add(CorrectNumber.THREE);
+        for(CorrectNumber correctNumber : resultCollection) {
+            prizeSum += correctNumber.getCashPrize();
+        }
+        assertThat(((double)prizeSum / inputMoney) * PERCENTAGE)
+                .isEqualTo(expected);
     }
 }
