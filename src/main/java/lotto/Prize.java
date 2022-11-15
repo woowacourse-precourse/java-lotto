4797a43d4bfeba49a0d38bda2ccb86de95808e41
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-11-15 17:39:32 +0900

    feat(Prize) : calculate the number of lotto
    print out the number of lotto according to the purchase amount
    
    BREAKING CHANGE: change method 'getMoneyToBuyLotto' to 'purchaseLotto'

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -3,16 +3,20 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Prize {
-    public static final String enterMoney = "구매금액을 입력해 주세요.";
+    static final String ENTER_MONEY = "구매금액을 입력해 주세요.";
+    static final String PURCHASE_LOTTO = "%d개를 구매했습니다.";
+    static final int LOTTO_COUNT = 1000;
 
     public Prize() {
-        Integer money = getMoneyToBuyLotto();
-        System.out.println(money);
+        Integer lottoCount = purchaseLotto();
+        System.out.printf(PURCHASE_LOTTO, lottoCount);
     }
 
-    public Integer getMoneyToBuyLotto(){
-        System.out.println(enterMoney);
-        String money = Console.readLine();
-        return Integer.valueOf(money);
+    public Integer purchaseLotto(){
+        System.out.println(ENTER_MONEY);
+        String inputMoney = Console.readLine();
+        Integer money = Integer.valueOf(inputMoney);
+        int lottoCount = money / LOTTO_COUNT;
+        return lottoCount;
     }
 }
