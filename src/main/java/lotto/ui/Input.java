e1a6a8752c8adc30e17e10fa85c87033207e1ea6
yoonjuho <dbswn990@naver.com>
2022-11-15 22:50:44 +0900

    refactor(Input) : 입력메서드 통일

--- src/main/java/lotto/ui/Input.java
+++ src/main/java/lotto/ui/Input.java
@@ -5,18 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
     private String input;
 
-    public int money(){
-        input = Console.readLine();
-        return Integer.parseInt(input);
-    }
-
-    public String winningNum(){
+    public String number(){
         input = Console.readLine();
         return input;
-    }
-
-    public int bonusNum(){
-        input = Console.readLine();
-        return Integer.parseInt(input);
     }
 }
