35c1e206044861f850c091e4aa76cefae9166d27
endlessmomo <soohyuk96@naver.com>
2022-11-15 21:56:17 +0900

    etc : LotteryMachine 제외 주석처리 (에러 찾기 위해)

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -5,15 +5,15 @@ import view.InputView;
 import view.OutputView;
 
 public class LottoController {
-   /* public void run() {
+    public void run() {
         PurchaseMoney purchaseMoney = new PurchaseMoney(InputView.inputMoney());
-        LotteryMachine lotteryMachine = new LotteryMachine(purchaseMoney.getLottoCount());
+        /*LotteryMachine lotteryMachine = new LotteryMachine(purchaseMoney.getLottoCount());
         OutputView.showLottos(lotteryMachine.getLottos());
         WinningNumber winningNumber = createWinningNumber();
         LottoResult lottoResult = new LottoResult(lotteryMachine, winningNumber);
         OutputView.showResult(lottoResult.getResult());
-        OutputView.showProfitPercent(purchaseMoney.getPurchaseMoney(), lottoResult);
-    }*/
+        OutputView.showProfitPercent(purchaseMoney.getPurchaseMoney(), lottoResult)*/;
+    }
 
     private WinningNumber createWinningNumber() {
         Lotto winningNumbers = new Lotto(InputView.inputWinningNumber());
