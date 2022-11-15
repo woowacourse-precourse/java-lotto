15460275de1f5a93f1b7f2cd0d6001b7d49b15b9
ye1ne <yewon7036@naver.com>
2022-11-15 16:54:31 +0900

    feature : make buy Lotto mthod

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -9,8 +10,22 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        List<Integer> numbers = makeRandomNumbers();
+        Lotto[] lottos = new Lotto[numberOfLotto()];
+        
+        for (int i = 0; i < lottos.length; i++) {
+            lottos[i] = new Lotto(makeRandomNumbers());
+        }
 
+
+    }
+
+    private static int numberOfLotto() {
+        String inputStringMoney = Console.readLine();
+        int money = Integer.parseInt(inputStringMoney);
+        if(money < 0 || money%1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+        return money/1000;
     }
 
     private static List<Integer> makeRandomNumbers() {
