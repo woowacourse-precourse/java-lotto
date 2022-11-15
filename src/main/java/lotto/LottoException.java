375b6b34fbb195f224c5615f9d115d2d3c8b606b
tenta <eunoia3802@gmail.com>
2022-11-15 21:40:48 +0900

    add: 당첨 번호 입력 예외 메서드 내 에러문 추가

--- src/main/java/lotto/LottoException.java
+++ src/main/java/lotto/LottoException.java
@@ -23,10 +23,11 @@ public class LottoException {
         for (String number : winningNumbers) {
             int count = Collections.frequency(List.of(winningNumbers), number);
             if (count > 1) {
+                System.out.println("[ERROR]: 중복된 숫자는 입력할 수 없습니다.");
                 throw new IllegalArgumentException();
             }
             if (winningNumbers.length != 6) {
-                System.out.println("[ERROR]: 당청 번호는 총 6자리를 입력하셔야 합니다.");
+                System.out.println("[ERROR]: 당청 번호는 총 6자리를 입력해야 합니다.");
                 throw new IllegalArgumentException();
             }
         }
