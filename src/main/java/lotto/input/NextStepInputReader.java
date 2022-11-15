ce71fb00f6974d5d0716db9812583122d253ed9f
yjlee0235 <yjlee0235@naver.com>
2022-11-15 19:10:15 +0900

    fix(input): 입력시 공백문자 제거하는 부분 추가

--- src/main/java/lotto/input/NextStepInputReader.java
+++ src/main/java/lotto/input/NextStepInputReader.java
@@ -5,6 +5,6 @@ import camp.nextstep.edu.missionutils.Console;
 public class NextStepInputReader implements InputReader {
     @Override
     public String readLine() {
-        return Console.readLine();
+        return Console.readLine().trim();
     }
 }
