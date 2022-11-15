b94968a531d86aefdefacd7b469bbbc50eb582a2
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-11-15 16:34:15 +0900

    Lotto : 당첨번호를 입력 및 객체화

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,50 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        // 로또 번호 검증
+        validation(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private void validation(List<Integer> numbers) {
+        countValidate(numbers);
+        duplicateValidation(numbers);
+        integerCheckAndRangeValidate(numbers);
+    }
+
+    private void countValidate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            System.out.println("[ERROR] 여섯자리가 아닙니다.");
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void duplicateValidation(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != numbers.size()) {
+            System.out.println("[ERROR] 중복되는 수가 포함되어 있습니다");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void integerCheckAndRangeValidate(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                System.out.println("[ERROR] 1 ~ 45 사이의 숫자만 입력 가능합니다");
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
