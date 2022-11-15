a562d727be8d360d5ee2f05aeefc551098a584b2
meunrud <y3a0522@gmail.com>
2022-11-15 11:22:40 +0900

    :sparkles: 생성된 로또 번호의 개수 확인

--- src/test/java/lotto/CreateUserLottoNumbersTest.java
+++ src/test/java/lotto/CreateUserLottoNumbersTest.java
@@ -4,28 +4,30 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CreateUserLottoNumbersTest {
     CreateUserLottoNumbers createUserLottoNumbers;
+    List<Lotto> lottos;
 
     @BeforeEach
     void setUp(){
         createUserLottoNumbers = new CreateUserLottoNumbers();
+        lottos = new ArrayList<>();
     }
 
-    @DisplayName("로또 구매 개수대로 로또가 생성되는지 확인")
+    @DisplayName("생성된 로또 번호의 개수 확인")
     @Test
-    void randomLottoNumbers(){
+    void createUserLottoNumber(){
         //given
-        int amount = 4;
+
 
         //when
-        List<List<Integer>> actual = createUserLottoNumbers.randomLottoNumbers(amount);
-
+        List<Lotto> actual = createUserLottoNumbers.createUserLottoNumber();
         //then
-        assertThat(actual.size()).isEqualTo(4);
+        assertThat(actual.get(0).getNumbers().size()).isEqualTo(6);
     }
 }
