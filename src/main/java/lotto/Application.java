cd087a59adf073170d6f0aab27b024f7e31efe91
sypark <soun997@gmail.com>
2022-11-15 23:31:47 +0900

    fix(Application): Exception 발생하면 로그를 출력하고 종료하도록 try-catch로 실행부를 감싸줌

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,11 @@ import lotto.Controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        new LottoController().run();
+        LottoController lottoController = new LottoController();
+        try {
+            lottoController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 에러가 발생하여 종료합니다.");
+        }
     }
 }
