5d4961fd2036c38770ec17b54c60b793f46b5450
Chohye <cindykim2010@gmail.com>
2022-11-16 16:59:18 +0900

    feat: 당첨 결과 및 통계 출력 기능 추가

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -9,4 +9,18 @@ public class Print {
             System.out.println(ticket);
         }
     }
+
+    public static void printResult(int[] rankResults, double yield) {
+        System.out.println("\n당첨 통계\n---");
+        printRankResult(Rank.FIFTH.ballCount(), Rank.FIFTH.prizeMoney(), rankResults[4]);
+        printRankResult(Rank.FOURTH.ballCount(), Rank.FOURTH.prizeMoney(), rankResults[3]);
+        printRankResult(Rank.THIRD.ballCount(), Rank.THIRD.prizeMoney(), rankResults[2]);
+        printRankResult(Rank.SECOND.ballCount(), Rank.SECOND.prizeMoney(), rankResults[1]);
+        printRankResult(Rank.FIRST.ballCount(), Rank.FIRST.prizeMoney(), rankResults[0]);
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", yield);
+    }
+
+    private static void printRankResult(String ballCount, int prizeMoney, int rankResult) {
+        System.out.printf("%s (%,d원) - %d개\n", ballCount, prizeMoney, rankResult);
+    }
 }
