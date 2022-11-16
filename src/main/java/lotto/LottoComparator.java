c5bcbadeb852cde4e4d4695f8c0e155cfb74b430
Kayoung <rkdkdudjd@gmail.com>
2022-11-16 16:00:14 +0900

    refactor(LottoComparator): 숫자 비교 메서드 추출

--- src/main/java/lotto/LottoComparator.java
+++ src/main/java/lotto/LottoComparator.java
@@ -1,18 +1,21 @@
 package lotto;
 
-import java.util.Map;
-
 public class LottoComparator {
     public static ComparisonToWinningNumberResult compare(Lotto generatedLotto, WinningNumber winningNumber) {
-        int matchCount = (int) winningNumber.mainNumbers.getNumbers().stream()
-                .filter(number -> generatedLotto.getNumbers().contains(number))
-                .count();
+        int matchCount = matchNumbers(generatedLotto, winningNumber);
         boolean isBonusMatched = generatedLotto.getNumbers().contains(winningNumber.bonusNumber);
+
         return new ComparisonToWinningNumberResult(matchCount, isBonusMatched);
     }
 
+    private static int matchNumbers(Lotto generatedLotto, WinningNumber winningNumber) {
+        return (int) winningNumber.mainNumbers.getNumbers().stream()
+                .filter(number -> generatedLotto.getNumbers().contains(number))
+                .count();
+    }
+
     public static LottoPrize compareAndCountMatchLotto(Lotto lotto,
-                                                 WinningNumber winningNumber) {
+                                                       WinningNumber winningNumber) {
         ComparisonToWinningNumberResult result = LottoComparator.compare(lotto, winningNumber);
         LottoPrize prizeOfResult = LottoPrize.of(result);
         return prizeOfResult;
