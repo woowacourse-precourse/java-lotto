18b2941081a884a0557c51bcf7092aff36d1db07
Kayoung <rkdkdudjd@gmail.com>
2022-11-16 19:14:33 +0900

    fix(WinningNumber): 보너스 번호의 범위가 유효하지 않은 경우 예외처리

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class WinningNumber {
     private static final String DUPLICATE_WITH_MAIN_NUMBER = "보너스 번호가 당첨 번호와 중복됩니다.";
+    private static final String BONUS_NUMBER_OUT_OF_RANGE = "보너스 번호의 범위가 유효하지 않습니다.";
     public final Lotto mainNumbers;
     int bonusNumber;
 
@@ -14,6 +15,9 @@ public class WinningNumber {
     }
 
     private void validate(List<Integer> numbers, int bonusNumber) {
+        if (bonusNumber < Lotto.MIN_NUMBER || bonusNumber > Lotto.MAX_NUMBER) {
+            throw new IllegalArgumentException(BONUS_NUMBER_OUT_OF_RANGE);
+        }
         if (numbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(DUPLICATE_WITH_MAIN_NUMBER);
         }
