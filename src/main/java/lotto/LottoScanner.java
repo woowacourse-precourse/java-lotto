1dca552b451adfb997e997aa37c9676c4b80947e
Kayoung <rkdkdudjd@gmail.com>
2022-11-16 18:24:04 +0900

    revert(LottoScanner): 1000원 미만의 금액을 입력할 경우 예외처리

--- src/main/java/lotto/LottoScanner.java
+++ src/main/java/lotto/LottoScanner.java
@@ -11,19 +11,31 @@ public class LottoScanner {
     private static final String READ_BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
     private static final String DELIMITER = ",";
     public static final String REQUIRE_NUMERIC_VALUE = "숫자를 입력해주세요.";
-    public static final String SHOULD_BE_DIVIDED_BY_1000 = "구입금액은 1000으로 나누어 떨어져야합니다.";
+    public static final String SHOULD_BE_DIVIDED_INTO_1000 = "구입금액은 1000으로 나누어 떨어져야합니다.";
+    public static final String LESS_THAN_MIN_PURCHASE_AMOUNT = "구입금액은 1000원 이상이어야합니다.";
     public static final String DO_NOT_INCLUDE_NUMERIC_VALUE = "숫자가 아닌 문자가 있습니다.";
 
     public static int readPurchaseAmount() {
         System.out.println(READ_MONEY_PROMPT);
         int purchaseAmount = readLineAsInteger();
-        validateDividedBy1_000(purchaseAmount);
+        validate(purchaseAmount);
         return purchaseAmount;
     }
 
-    private static void validateDividedBy1_000(int purchaseAmount) {
+    private static void validate(int purchaseAmount) {
+        validateNotLessThan1_000(purchaseAmount);
+        validateDivideInto1_000(purchaseAmount);
+    }
+
+    private static void validateDivideInto1_000(int purchaseAmount) {
         if ((purchaseAmount % 1000) != 0) {
-            throw new IllegalArgumentException(SHOULD_BE_DIVIDED_BY_1000);
+            throw new IllegalArgumentException(SHOULD_BE_DIVIDED_INTO_1000);
+        }
+    }
+
+    private static void validateNotLessThan1_000(int purchaseAmount) {
+        if (purchaseAmount < 1000) {
+            throw new IllegalArgumentException(LESS_THAN_MIN_PURCHASE_AMOUNT);
         }
     }
 
@@ -45,14 +57,16 @@ public class LottoScanner {
 
     private static List<Integer> readSixNumbers() {
         System.out.println(READ_WINNING_NUMBER_PROMPT);
-
         String[] numbersSplitByDelimiter = Console.readLine().split(DELIMITER);
 
-        List<Integer> parsedNumbers = Arrays.stream(numbersSplitByDelimiter)
+        List<Integer> parsedNumbers = parseNumberOrThrow(numbersSplitByDelimiter);
+        return parsedNumbers;
+    }
+
+    private static List<Integer> parseNumberOrThrow(String[] numbersSplitByDelimiter) {
+        return Arrays.stream(numbersSplitByDelimiter)
                 .mapToInt(LottoScanner::parseIntOrThrowException)
                 .boxed().collect(Collectors.toList());
-
-        return parsedNumbers;
     }
 
     private static Integer parseIntOrThrowException(String number) {
--- src/test/java/lotto/LottoScannerTest.java
+++ src/test/java/lotto/LottoScannerTest.java
@@ -7,18 +7,32 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoScannerTest {
+
     @Nested
     @DisplayName("구입금액 입력 테스트")
     class ReadMoneyTest extends NsTest {
+        @DisplayName("1000원 미만의 금액을 입력하면 예외를 던져야한다.")
+        @ParameterizedTest
+        @ValueSource(strings = {"-1000", "0", "200"})
+        public void throwExceptionWhenPurchaseAmountIsLessThan1_000(String purchaseAmount) {
+            assertSimpleTest(() -> {
+                assertThatThrownBy(() ->
+                        run(purchaseAmount)).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(LottoScanner.LESS_THAN_MIN_PURCHASE_AMOUNT);
+            });
+        }
+
         @DisplayName("1000원으로 나누어떨어지지 않으면 예외를 던져야한다.")
         @Test
         public void throwExceptionWhen1000DoesNotDivideMoney() {
             assertSimpleTest(() -> {
                 assertThatThrownBy(() ->
                         run("25200")).isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining(LottoScanner.SHOULD_BE_DIVIDED_BY_1000);
+                        .hasMessageContaining(LottoScanner.SHOULD_BE_DIVIDED_INTO_1000);
             });
         }
 
@@ -44,7 +58,7 @@ public class LottoScannerTest {
         @DisplayName("번호가 6개가 아닌 경우 예외가 발생한다.")
         @Test
         public void throwExceptionWhenLackOfNumber() {
-            assertThatThrownBy(()-> {
+            assertThatThrownBy(() -> {
                 run("1,2,3,4,5", "7");
             }).isInstanceOf(IllegalArgumentException.class);
         }
@@ -52,11 +66,12 @@ public class LottoScannerTest {
         @DisplayName("숫자와 구분자 외의 다른 문자가 들어오면 예외가 발생한다.")
         @Test
         public void throwExceptionWhenOtherCharacterExists() {
-            assertThatThrownBy(()-> {
+            assertThatThrownBy(() -> {
                 run("12.23,4,2,5,6");
             }).isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining(LottoScanner.DO_NOT_INCLUDE_NUMERIC_VALUE);
         }
+
         @Override
         protected void runMain() {
             LottoScanner.readWinningNumber();
