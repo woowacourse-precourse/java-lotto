cb90fdedbed251306080f17e2f7def10ebf18d1b
penrose15 <seongju99@naver.com>
2022-11-16 13:07:47 +0900

    refactor : try ~ catch in main

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,10 +5,18 @@ import lotto.domain.Money;
 import lotto.domain.Numbers;
 import lotto.service.Service;
 
+import java.util.NoSuchElementException;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Controller controller = new Controller();
-        controller.play();
+
+        try {
+            Controller controller = new Controller();
+            controller.play();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
--- src/main/java/lotto/view/validation/MoneyValidation.java
+++ src/main/java/lotto/view/validation/MoneyValidation.java
@@ -5,6 +5,8 @@ import lotto.exception.ExceptionPhrase;
 import java.util.NoSuchElementException;
 import java.util.logging.Logger;
 
+import static lotto.exception.ExceptionPhrase.EXCEPTION_INVALID_INPUT;
+
 public class MoneyValidation implements Validation{
 
     private final String regex = "[0-9]";
@@ -12,13 +14,13 @@ public class MoneyValidation implements Validation{
     private static final Logger logger = Logger.getGlobal();
 
     @Override
-    public void validateInputIsInteger(String input) {
+    public void validateInputIsInteger(String input) throws IllegalArgumentException{
 
         input = input.replaceAll(regex,"");
 
-        if(input.length() != 0) {
-            System.out.println(ExceptionPhrase.EXCEPTION_INVALID_INPUT.getMessage());
-            throw new NoSuchElementException();
+        if(!input.matches("[1-9]+[0-9]*")) {
+//            System.out.println(EXCEPTION_INVALID_INPUT.getMessage());
+            throw new IllegalArgumentException(EXCEPTION_INVALID_INPUT.getMessage());
         }
     }
 }
