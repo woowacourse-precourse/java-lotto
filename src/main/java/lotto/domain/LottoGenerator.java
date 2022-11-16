d2a5de411330cf249d88ac62fdadba9f51c74bb9
LEE-sh1673 <lsh901673@gmail.com>
2022-11-16 11:08:22 +0900

    refactor(lotto): 로또 생성기의 로또를 생성하는 부분을 스트림 방식으로 대체

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -5,22 +5,22 @@ import static lotto.domain.LottoAttributes.MIN_LOTTO_NUMBER;
 import static lotto.domain.LottoAttributes.MAX_LOTTO_NUMBER;
 import static lotto.domain.LottoAttributes.MAX_LOTTO_NUMBERS_SIZE;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottoGenerator {
 
     public static List<Lotto> publish(final int numberOfTickets) {
-        List<Lotto> tickets = new ArrayList<>();
-
-        for (int i = 0; i < numberOfTickets; i++) {
-            tickets.add(new Lotto(pickUniqueNumbersInRange(MIN_LOTTO_NUMBER,
-                MAX_LOTTO_NUMBER, MAX_LOTTO_NUMBERS_SIZE)));
-        }
-        return tickets.stream()
+        return Stream.generate(() -> new Lotto(getNonDuplicateNumbers()))
+            .limit(numberOfTickets)
             .sorted(Comparator.comparingInt(Lotto::getSumOfNumbers))
             .collect(Collectors.toList());
+    }
+
+    private static List<Integer> getNonDuplicateNumbers() {
+        return pickUniqueNumbersInRange(MIN_LOTTO_NUMBER,
+            MAX_LOTTO_NUMBER, MAX_LOTTO_NUMBERS_SIZE);
     }
 }
