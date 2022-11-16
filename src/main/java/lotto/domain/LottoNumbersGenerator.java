c62fa36e268e980dbfc91fd589b8006576b509f3
charlesuu <tiger0192004@gmail.com>
2022-11-16 19:37:07 +0900

    fix: 로또 번호 오름차순 정렬 관련 문제 수정(최종 제출 이후)
    제공 된 Random API에서 반환된 List를 Colloections.sort() 메서드에 넣으면 테스트에 실패하는 문제가 존재했음.
    새로운 ArrayList객체를 만들어서 값을 복사한 뒤 정렬하는 방식으로 수정.

--- src/main/java/lotto/domain/LottoNumbersGenerator.java
+++ src/main/java/lotto/domain/LottoNumbersGenerator.java
@@ -2,17 +2,25 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoNumbersGenerator {
     public List<Integer> getLottoNumbers() {
-        List<Integer> numbers = generateLottoNumbers();
+        List<Integer> numbers = new ArrayList<>();
+        numbers.addAll(generateLottoNumbers());
+        sortLottoNumbers(numbers);
         return numbers;
     }
 
     private List<Integer> generateLottoNumbers() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
+
+    private List<Integer> sortLottoNumbers(List<Integer> numbers) {
+        Collections.sort(numbers);
         return numbers;
     }
 }
