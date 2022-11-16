352e0ec15998116f09c9e86eb07054de22cf2f81
Chohye <cindykim2010@gmail.com>
2022-11-16 13:50:30 +0900

    refactor: 구매 내역 출력 기능 수정

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -3,20 +3,11 @@ package lotto;
 import java.util.List;
 
 public class Print {
-    public static void printTicketAmount(int ticketAmount) {
+    public static void printPurchase(int ticketAmount, List<List<Integer>> ticketList) {
         System.out.println(ticketAmount + "개를 구매했습니다.");
-    }
-
-    public static void printTicketList(List<List<Integer>> ticketList) {
-        for (int i = 0; i < ticketList.size(); i++) {
-            List<Integer> ticket = ticketList.get(i);
-            System.out.print("[");
-            System.out.print(ticket.get(0));
-            for (int j = 1; j < ticket.size(); j++) {
-                System.out.print(", ");
-                System.out.print(ticket.get(j));
-            }
-            System.out.println("]");
+        for (List<Integer> ticket : ticketList) {
+            System.out.println(ticket);
         }
+        System.out.println("");
     }
 }
