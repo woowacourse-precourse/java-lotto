89e38d9eeece71a016bb8b34cb09d2417b5bedfd
Chohye <cindykim2010@gmail.com>
2022-11-16 16:53:31 +0900

    simplify: simplify printPurchase()

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -4,10 +4,9 @@ import java.util.List;
 
 public class Print {
     public static void printPurchase(int ticketAmount, List<List<Integer>> ticketList) {
-        System.out.println(ticketAmount + "개를 구매했습니다.");
+        System.out.println("\n" + ticketAmount + "개를 구매했습니다.");
         for (List<Integer> ticket : ticketList) {
             System.out.println(ticket);
         }
-        System.out.println("");
     }
 }
