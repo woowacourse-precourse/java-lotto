1975ea3c47549b553f332225ea8b9660ea874040
jihwan <sgh002400@naver.com>
2022-11-18 15:14:24 +0900

    refactor(LottoTest): LottoTest에 hasMessageContaining으로 구체적으로 검증

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import static lotto.common.exception.ErrorMessage.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
@@ -14,27 +15,31 @@ class LottoTest {
 	@Test
 	void createLottoByOverSize() {
 		assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-			.isInstanceOf(IllegalArgumentException.class);
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(NOT_VALID_SIZE);
 	}
 
 	@DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
 	@Test
 	void createLottoByDuplicatedNumber() {
 		assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-			.isInstanceOf(IllegalArgumentException.class);
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(NOT_VALID_LOTTO_DUPLICATE);
 	}
 
 	@DisplayName("로또 번호의 개수가 부족하면 예외가 발생한다.")
 	@Test
 	void createLottoByLessSize() {
 		assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
-			.isInstanceOf(IllegalArgumentException.class);
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(NOT_VALID_SIZE);
 	}
 
 	@DisplayName("로또 번호 범위에 벗어나면 예외가 발생한다.")
 	@Test
 	void createLottoByOverRange() {
 		assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 66)))
-			.isInstanceOf(IllegalArgumentException.class);
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(NOT_VALID_NUMBER);
 	}
 }
