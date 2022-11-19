3990f07001039bc275a4cf51aead21114e373c7e
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-19 12:02:17 +0900

    refactor: enum타입 Ranking에 상수값들 추가

--- src/main/java/lotto/Ranking.java
+++ src/main/java/lotto/Ranking.java
@@ -1,5 +1,18 @@
 package lotto;
 
 public enum Ranking {
-    NOTHING, THREE_MATCH, FOUR_MATCH, FIVE_MATCH, FIVE_MATCH_WITH_BONUS, SIX_MATCH
+    NOTHING(0, 0L),
+    THREE_MATCH(3, 5_000),
+    FOUR_MATCH(4, 50_000),
+    FIVE_MATCH(5, 1_500_000),
+    FIVE_MATCH_WITH_BONUS(5, 30_000_000),
+    SIX_MATCH(6, 2_000_000_000);
+
+    private int countOfMatch;
+    private long winningMoney;
+
+    private Ranking(int countOfMatch, long winningMoney){
+        this.countOfMatch = countOfMatch;
+        this.winningMoney = winningMoney;
+    }
 }
