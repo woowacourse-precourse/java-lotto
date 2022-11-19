799a245e7a685425b187ee0ca6ee702fb2d12449
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-19 22:01:54 +0900

    refactor(lotto) : 바뀐 객체 구조에 따라 테스트 코드 변경

--- src/test/java/lotto/domain/WinningTest.java
+++ src/test/java/lotto/domain/WinningTest.java
@@ -2,8 +2,8 @@ package lotto.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import java.util.List;
 
@@ -11,110 +11,39 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@DisplayName("1등, 2등, 4등 당첨")
 class WinningTest {
+    List<Lotto> lottos;
+    List<Integer> winningNumbers;
+    int bonus;
+    Winning winning;
 
-    @DisplayName("등수를 올바르게 계산하는지")
-    @Nested
-    class CalMyScore {
-        Lotto lotto;
-        List<Integer> winningNumbers;
-        int bonus;
-        Winning winning;
+    @BeforeEach
+    void setUp() {
+        Lotto lotto1 = mock(Lotto.class);
+        Lotto lotto2 = mock(Lotto.class);
+        Lotto lotto3 = mock(Lotto.class);
+        winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        bonus = 7;
+        when(lotto1.matchCount(winningNumbers)).thenReturn(6);
+        when(lotto2.matchCount(winningNumbers)).thenReturn(5);
+        when(lotto2.containsBonus(bonus)).thenReturn(true);
+        when(lotto3.matchCount(winningNumbers)).thenReturn(4);
+        lottos = List.of(lotto1, lotto2, lotto3);
+        winning = new Winning(winningNumbers, bonus);
+    }
+    @ParameterizedTest(name = "{0}은 당첨 정보에 포함")
+    @EnumSource(value = WinningTable.class, names = {"FIRST", "SECOND", "FORTH"})
+    void getWinningInfo(WinningTable winningTable) {
+        List<WinningTable> winningInfo = winning.getWinningInfo(lottos);
+        assertThat(winningInfo.contains(winningTable)).isTrue();
+    }
 
-        @BeforeEach
-        void setUp() {
-            lotto = mock(Lotto.class);
-        }
-
-        @DisplayName("맞춘 번호 : 6개, 1등 당첨")
-        @Test
-        void case1() {
-            winningNumbers = List.of(1,2,3,4,5,6);
-            bonus = 7;
-            when(lotto.matchCount(winningNumbers))
-                    .thenReturn(6);
-            winning = new Winning(winningNumbers, bonus);
-            assertThat(winning.calMyScore(lotto)).isEqualTo(WinningTable.FIRST);
-        }
-
-        @DisplayName("맞춘 번호 : 5개, 보너스 2등 당첨")
-        @Test
-        void case2() {
-            winningNumbers = List.of(1,2,3,4,5,6);
-            bonus = 7;
-            when(lotto.matchCount(winningNumbers))
-                    .thenReturn(5);
-            when(lotto.containsBonus(bonus))
-                    .thenReturn(true);
-            winning = new Winning(winningNumbers, bonus);
-            assertThat(winning.calMyScore(lotto)).isEqualTo(WinningTable.SECOND);
-        }
-
-        @DisplayName("맞춘 번호 : 5개, 3등 당첨")
-        @Test
-        void case3() {
-            winningNumbers = List.of(1,2,3,4,5,6);
-            bonus = 7;
-            when(lotto.matchCount(winningNumbers))
-                    .thenReturn(5);
-            winning = new Winning(winningNumbers, bonus);
-            assertThat(winning.calMyScore(lotto)).isEqualTo(WinningTable.THIRD);
-        }
-
-        @DisplayName("맞춘 번호 : 4개, 4등 당첨")
-        @Test
-        void case4() {
-            winningNumbers = List.of(1,2,3,4,5,6);
-            bonus = 7;
-            when(lotto.matchCount(winningNumbers))
-                    .thenReturn(4);
-            winning = new Winning(winningNumbers, bonus);
-            assertThat(winning.calMyScore(lotto)).isEqualTo(WinningTable.FORTH);
-        }
-
-        @DisplayName("맞춘 번호 : 3개, 5등 당첨")
-        @Test
-        void case5() {
-            winningNumbers = List.of(1,2,3,4,5,6);
-            bonus = 7;
-            when(lotto.matchCount(winningNumbers))
-                    .thenReturn(3);
-            winning = new Winning(winningNumbers, bonus);
-            assertThat(winning.calMyScore(lotto)).isEqualTo(WinningTable.FIFTH);
-        }
-
-        @DisplayName("맞춘 번호 : 2개, 꽝")
-        @Test
-        void case6() {
-            winningNumbers = List.of(1,2,3,4,5,6);
-            bonus = 7;
-            when(lotto.matchCount(winningNumbers))
-                    .thenReturn(2);
-            winning = new Winning(winningNumbers, bonus);
-            assertThat(winning.calMyScore(lotto)).isEqualTo(WinningTable.LOSE);
-        }
-
-        @DisplayName("맞춘 번호 : 1개, 꽝")
-        @Test
-        void case7() {
-            winningNumbers = List.of(1,2,3,4,5,6);
-            bonus = 7;
-            when(lotto.matchCount(winningNumbers))
-                    .thenReturn(1);
-            winning = new Winning(winningNumbers, bonus);
-            assertThat(winning.calMyScore(lotto)).isEqualTo(WinningTable.LOSE);
-        }
-
-        @DisplayName("맞춘 번호 : 0개, 꽝")
-        @Test
-        void case8() {
-            winningNumbers = List.of(1,2,3,4,5,6);
-            bonus = 7;
-            when(lotto.matchCount(winningNumbers))
-                    .thenReturn(0);
-            winning = new Winning(winningNumbers, bonus);
-            assertThat(winning.calMyScore(lotto)).isEqualTo(WinningTable.LOSE);
-        }
+    @ParameterizedTest(name = "{0}은 당첨 정보에 포함되지 않음")
+    @EnumSource(value = WinningTable.class, names = {"THIRD", "FIFTH", "LOSE"})
+    void getWinningInfo2(WinningTable winningTable) {
+        List<WinningTable> winningInfo = winning.getWinningInfo(lottos);
+        assertThat(winningInfo.contains(winningTable)).isFalse();
     }
 
 
