65c240259ef715722b02b27dac7af6264ddecc1c
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-19 17:13:28 +0900

    refactor(lotto) : 테스트 코드 중복 제거
    - ParameterizedTest 사용

--- src/test/java/lotto/domain/BuyLottoTest.java
+++ src/test/java/lotto/domain/BuyLottoTest.java
@@ -2,64 +2,35 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class BuyLottoTest {
-
     BuyLotto buyLotto;
 
     @DisplayName("금액에 맞는 개수의 로또를 구입했는지 체크")
     @Nested
     class CountFromPrice {
-        @DisplayName("8000원은 8개")
-        @Test
-        void case1() {
-            buyLotto = new BuyLotto(8000);
-            assertThat(buyLotto.getCount()).isEqualTo(8);
-        }
-
-        @DisplayName("4000원은 4개")
-        @Test
-        void case2() {
-            buyLotto = new BuyLotto(4000);
-            assertThat(buyLotto.getCount()).isEqualTo(4);
-        }
-
-        @DisplayName("0원은 0개")
-        @Test
-        void case3() {
-            buyLotto = new BuyLotto(0);
-            assertThat(buyLotto.getCount()).isEqualTo(0);
+        @ParameterizedTest(name = "{0}원은 {1}개")
+        @CsvSource(value = {"8000, 8", "4000, 4", "0, 0"})
+        void countTest(int price, int count) {
+            buyLotto = new BuyLotto(price);
+            assertThat(buyLotto.getCount()).isEqualTo(count);
         }
     }
 
     @DisplayName("예외 : 금액이 1,000원 단위가 아닐 때")
     @Nested
     class Exceptions {
-        @DisplayName("1230원")
-        @Test
-        void case1() {
-            assertThatThrownBy(() -> new BuyLotto(1230))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
-
-        @DisplayName("7452원")
-        @Test
-        void case2() {
-            assertThatThrownBy(() -> new BuyLotto(7452))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
-
-        @DisplayName("15001원")
-        @Test
-        void case3() {
-            assertThatThrownBy(() -> new BuyLotto(15001))
+        @ParameterizedTest(name = "{0}원은 1,000원 단위가 아니다.")
+        @ValueSource(ints = {1230, 7452, 15001})
+        void exceptionCases(int price) {
+            assertThatThrownBy(() -> new BuyLotto(price))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
-
-
 }
