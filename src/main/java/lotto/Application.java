3010fe887e91f5e7065e0ba73e0cb2baa9151f19
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-11-22 15:23:09 +0900

    refactor(lotto) : 피드백 반영하여 메인 함수 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.controller.LottoController;
 import lotto.domain.Lotto;
 import lotto.domain.User;
 import lotto.domain.Winning;
@@ -11,29 +12,7 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        LottoView lottoView = new LottoView();
-        User user = new User();
-
-        int number = lottoView.lottoPurchase();
-        List<Lotto> list = user.randomLotto(number);
-
-        lottoView.lottoPublish(list);
-
-        System.out.println();
-
-        String win = lottoView.winNum();
-        Winning winning = new Winning();
-        Lotto lotto = new Lotto(winning.winning(win));
-
-        System.out.println();
-        lottoView.bonusNum(lotto, user);
-
-        winning.score(list, lotto.getNumbers(), user.getBonusNum());
-
-        double rate = winning.rate(number * 1000);
-
-        System.out.println();
-
-        lottoView.winView(winning, rate);
+        LottoController lottoController = new LottoController();
+        lottoController.run();
     }
 }
---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,46 @@
+package lotto.controller;
+
+import lotto.domain.Lotto;
+import lotto.domain.User;
+import lotto.domain.Winning;
+import lotto.view.LottoView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoController {
+
+    private LottoView lottoView = new LottoView();
+    private User user = new User();
+    private List<Lotto> userLotto = new ArrayList<>();
+    private int money = 0;
+    private Winning winning = new Winning();
+    private Lotto lotto;
+
+    public void run() {
+        lottoBuy();
+        winningNumber();
+        printScore();
+    }
+
+    public void lottoBuy() {
+        money = lottoView.lottoPurchase();
+        userLotto = user.randomLotto(money);
+        lottoView.lottoPublish(userLotto);
+        System.out.println();
+    }
+
+    public void winningNumber() {
+        String win = lottoView.winNum();
+        lotto = new Lotto(winning.winning(win));
+        System.out.println();
+        lottoView.bonusNum(lotto, user);
+    }
+
+    public void printScore() {
+        winning.score(userLotto, lotto.getNumbers(), user.getBonusNum());
+        double rate = winning.rate(money * 1000);
+        System.out.println();
+        lottoView.winView(winning, rate);
+    }
+}
