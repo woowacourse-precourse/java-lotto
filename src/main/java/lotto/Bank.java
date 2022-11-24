7f5d3c87ae4070d28d842efb8fa50254bfee7607
korjun1993 <corojoon93@naver.com>
2022-11-24 17:36:12 +0900

    fix(Bank) : 결과 출력 기능 버그수정

--- src/main/java/lotto/Bank.java
+++ src/main/java/lotto/Bank.java
@@ -7,8 +7,7 @@ import java.util.List;
 import static lotto.exception.ErrorCode.BONUS_NUMBER_ERROR;
 
 public class Bank {
-    private final static int[] MONEY = {0, 2000000000, 30000000, 1500000, 50000, 5000};
-    private final static int WIN_SIZE = 6;
+    private final static int WIN_SIZE = Rank.values().length;
     private final Lotto winLotto;
     private final int bonusNumber;
     private final int[] payment;
@@ -45,10 +44,10 @@ public class Bank {
     public void printPayResult() {
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - " + payment[5] + "개");
-        System.out.println("4개 일치 (50,000원) - " + payment[4] + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + payment[3] + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + payment[2] + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + payment[1] + "개");
+        System.out.println("3개 일치 (5,000원) - " + payment[Rank.FIFTH.ordinal()] + "개");
+        System.out.println("4개 일치 (50,000원) - " + payment[Rank.FOURTH.ordinal()] + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + payment[Rank.THIRD.ordinal()] + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + payment[Rank.SECOND.ordinal()] + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + payment[Rank.FIRST.ordinal()] + "개");
     }
 }
