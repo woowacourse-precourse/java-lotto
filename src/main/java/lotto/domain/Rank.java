d09b1dfd75f158509accab5c067cc0fbcbd0e386
ho2n <jungho2n@gmail.com>
2022-11-25 17:39:56 +0900

    refactor(Rank): Enum Rank의 bonus 인스턴스 변수 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,18 +1,22 @@
 package lotto.domain;
 
+import java.util.concurrent.locks.AbstractOwnableSynchronizer;
+
 public enum Rank {
-    SIX_NUMBERS_MATCHING(PrintRank.FIRST, 2_000_000_000),
-    FIVE_NUMBERS_AND_BONUS_NUMBER_MATCHING(PrintRank.BONUS, 30_000_000),
-    ONLY_FIVE_NUMBERS_MATCHING(PrintRank.THIRD, 1_500_000),
-    FOUR_NUMBERS_MATCHING(PrintRank.FOURTH, 50_000),
-    THREE_NUMBERS_MATCHING(PrintRank.FIFTH, 5_000);
+    SIX_NUMBERS_MATCHING(PrintRank.FIRST, 2_000_000_000, 0),
+    FIVE_NUMBERS_AND_BONUS_NUMBER_MATCHING(PrintRank.BONUS, 30_000_000, 1),
+    ONLY_FIVE_NUMBERS_MATCHING(PrintRank.THIRD, 1_500_000, 0),
+    FOUR_NUMBERS_MATCHING(PrintRank.FOURTH, 50_000, 0),
+    THREE_NUMBERS_MATCHING(PrintRank.FIFTH, 5_000, 0);
 
     private final PrintRank print;
     private int winningMoney;
+    private int bonus;
 
-    Rank(PrintRank print, int winningMoney) {
+    Rank(PrintRank print, int winningMoney, int bonus) {
         this.print = print;
         this.winningMoney = winningMoney;
+        this.bonus = bonus;
     }
 
     public void printRank() {
