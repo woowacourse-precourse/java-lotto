e21a0437d4be552d4948d2d2ac928ccbaf8b5b22
apptie <appti@naver.com>
2022-11-28 11:04:24 +0900

    feat(ExceptionMessageUtil):  예외 메세지 관련 유틸리티 기능 추가

---
+++ src/main/java/lotto/utils/consts/LottoConst.java
@@ -0,0 +1,14 @@
+package lotto.utils.consts;
+
+public final class LottoConst {
+
+    public static final int MIN_NUMBER_INT_VALUE = 1;
+    public static final int MAX_NUMBER_INT_VALUE = 45;
+    public static final int NUMBER_SIZE = 6;
+    public static final long PURCHASE_AMOUNT = 1000L;
+
+    public static final String SEPARATOR = ",";
+
+    private LottoConst() {
+    }
+}
---
+++ src/main/java/lotto/utils/message/ExceptionMessageUtil.java
@@ -0,0 +1,37 @@
+package lotto.utils.message;
+
+import lotto.utils.consts.LottoConst;
+
+public enum ExceptionMessageUtil {
+    WRONG_NUMBER_FORMAT("입력한 값을 숫자로 치환할 수 없습니다."),
+
+    WRONG_NUMBER_RANGE("로또 번호는 %d부터 %d 사이의 숫자여야 합니다.",
+            LottoConst.MIN_NUMBER_INT_VALUE,
+            LottoConst.MAX_NUMBER_INT_VALUE),
+
+    WRONG_NUMBER_SIZE("유효한 숫자는 총 %d개여야만 합니다.",
+            LottoConst.NUMBER_SIZE),
+
+    WRONG_PURCHASE_AMOUNT_UNIT("구입 금액은 %d원 단위여야만 합니다.",
+            LottoConst.PURCHASE_AMOUNT),
+
+    WRONG_PURCHASE_AMOUNT_VALUE("구입 금액은 %d원 이상이어야만 합니다",
+            LottoConst.PURCHASE_AMOUNT),
+
+    WRONG_SEPARATOR("필요한 숫자 구분자 %s가 누락되었습니다.",
+            LottoConst.SEPARATOR),
+
+    WRONG_NUMBER_LENGTH("유효하지 않은 길이입니다."),
+
+    WRONG_RANKING("존재하지 않는 등수입니다.");
+
+    private final String message;
+
+    ExceptionMessageUtil(String baseMessage, Object... replaces) {
+        this.message = String.format(baseMessage, replaces);
+    }
+
+    public String findFullMessage() {
+        return this.message;
+    }
+}
