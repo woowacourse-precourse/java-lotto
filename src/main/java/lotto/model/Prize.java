17f816cc01f11003790deab23ce9c5aefeb8929d
sooyoungh <sooyoung.h8@gmail.com>
2022-11-29 15:12:03 +0900

    ♻ refactor(Prize): Enum의 계산 기능 수정
    연산식 자체를 상수화하는 대신 숫자만 상수화하고 연산 기능은 메소드화

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -8,24 +8,24 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum Prize {
-    THREE_STRIKE(3.0, "3개 일치 (5,000원)", resultCount -> resultCount * 5000),
-    FOUR_STRIKE(4.0, "4개 일치 (50,000원)", resultCount -> resultCount * 50000),
-    FIVE_STRIKE(5.0, "5개 일치 (1,500,000원)", resultCount -> resultCount * 1500000),
-    FIVE_STRIKE_WITH_BONUS(5.5, "5개 일치, 보너스 볼 일치 (30,000,000원)", resultCount -> resultCount * 30000000),
-    SIX_STRIKE(6.0, "6개 일치 (2,000,000,000원)", resultCount -> resultCount * 2000000000);
+    THREE_STRIKE(3.0, "3개 일치 (5,000원)",  5_000),
+    FOUR_STRIKE(4.0, "4개 일치 (50,000원)",  50_000),
+    FIVE_STRIKE(5.0, "5개 일치 (1,500,000원)", 1_500_000),
+    FIVE_STRIKE_WITH_BONUS(5.5, "5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000),
+    SIX_STRIKE(6.0, "6개 일치 (2,000,000,000원)",  2_000_000_000);
 
     private Double prize;
     private String message;
-    private Function<Integer, Integer> expression;
+    private Integer money;
 
-    Prize(Double prize, String message, Function<Integer, Integer> expression) {
+    Prize(Double prize, String message, Integer money) {
         this.prize = prize;
         this.message = message;
-        this.expression = expression;
+        this.money = money;
     }
 
     public Integer calculate(int count) {
-        return expression.apply(count);
+        return money * count;
     }
 
     public static Prize getPrizeByScore(Double score) {
