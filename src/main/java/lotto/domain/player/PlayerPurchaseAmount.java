7899c717014dfb819f3d41f3e57527b2df792b44
apptie <appti@naver.com>
2022-11-29 20:40:19 +0900

    feat(PlayerPurchaseAmount): 플레이어가 입력한 로또 구매 금액을 관리하는 기능 추가

---
+++ src/main/java/lotto/domain/player/PlayerPurchaseAmount.java
@@ -0,0 +1,62 @@
+package lotto.domain.player;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import lotto.utils.consts.LottoConst;
+import lotto.utils.message.ExceptionMessageUtil;
+
+public class PlayerPurchaseAmount {
+
+    private static final BigDecimal PRICE_OF_ONE = new BigDecimal(LottoConst.PURCHASE_AMOUNT);
+    private static final BigDecimal PERCENT_UNIT = new BigDecimal("100");
+    private static final int PERCENT_SCALE = 1;
+
+    private final BigDecimal playerPurchaseAmount;
+
+    public PlayerPurchaseAmount(String amountInput) {
+        this.playerPurchaseAmount = calculatePlayerPurchaseAmount(amountInput);
+    }
+
+    private BigDecimal calculatePlayerPurchaseAmount(String amountInput) {
+        BigDecimal playerPurchaseAmount = mapToNumber(amountInput);
+
+        validatePlayerInputAmount(playerPurchaseAmount);
+        return playerPurchaseAmount;
+    }
+
+    private void validatePlayerInputAmount(BigDecimal playerPurchaseAmount) {
+        validateMinAmount(playerPurchaseAmount);
+        validateAmountUnit(playerPurchaseAmount);
+    }
+
+    private BigDecimal mapToNumber(String amountInput) {
+        try {
+            return new BigDecimal(amountInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionMessageUtil.WRONG_NUMBER_FORMAT.findFullMessage(), e);
+        }
+    }
+
+    private void validateMinAmount(BigDecimal playerPurchaseAmount) {
+        if (playerPurchaseAmount.compareTo(PRICE_OF_ONE) < 0) {
+            throw new IllegalArgumentException(ExceptionMessageUtil.WRONG_PURCHASE_AMOUNT_VALUE.findFullMessage());
+        }
+    }
+
+    private void validateAmountUnit(BigDecimal playerPurchaseAmount) {
+        if (playerPurchaseAmount.remainder(PRICE_OF_ONE).compareTo(BigDecimal.ZERO) > 0) {
+            throw new IllegalArgumentException(ExceptionMessageUtil.WRONG_PURCHASE_AMOUNT_UNIT.findFullMessage());
+        }
+    }
+
+    public BigInteger calculatePurchaseLottoAmount() {
+        return playerPurchaseAmount.divide(PRICE_OF_ONE, RoundingMode.FLOOR).toBigInteger();
+    }
+
+    public BigDecimal calculateRevenuePercent(BigDecimal totalReward) {
+        return totalReward
+                .multiply(PERCENT_UNIT)
+                .divide(playerPurchaseAmount, PERCENT_SCALE, RoundingMode.HALF_UP);
+    }
+}
