047f8b80095ec5f2b35d0c17bfdaf1a30867b38b
ycopp9 <aciees95@gmail.com>
2022-11-29 15:18:23 +0900

    refactor(LottoShop): NumberGenerator class 삭제 및 통합

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -1,20 +1,26 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.constants.LottoConstants;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoShop {
-    private NumberGenerator generator;
-
-    public LottoShop() {
-        generator = new NumberGenerator();
-    }
 
     public List<Lotto> purchase(Ticket ticket) {
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < ticket.getCount(); i++) {
-            lottos.add(new Lotto(generator.createRandomNumbers()));
+            lottos.add(new Lotto(createRandomNumbers()));
         }
         return lottos;
+    }
+
+    public List<Integer> createRandomNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                LottoConstants.START_NUMBER,
+                LottoConstants.END_NUMBER,
+                LottoConstants.LENGTH);
+        return numbers;
     }
 }
