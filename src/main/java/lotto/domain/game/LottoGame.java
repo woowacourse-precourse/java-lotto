28f05f980399320c7ad307508cf72882bd99f92e
apptie <appti@naver.com>
2022-11-30 16:34:08 +0900

    feat(LottoGame): 로또 게임을 진행하는 기능 추가

---
+++ src/main/java/lotto/domain/game/LottoGame.java
@@ -0,0 +1,34 @@
+package lotto.domain.game;
+
+import java.math.BigDecimal;
+import java.util.List;
+import lotto.domain.number.Lotto;
+import lotto.domain.number.LottoNumber;
+import lotto.domain.player.Player;
+import lotto.utils.number.LottoNumbersGenerator;
+
+public class LottoGame {
+
+    private final Player player;
+    private Lotto winningLotto;
+
+    public LottoGame(String amountInput, LottoNumbersGenerator generator) {
+        this.player = new Player(amountInput, generator);
+    }
+
+    public void createWinningLotto(String winningNumbers) {
+        winningLotto = new Lotto(winningNumbers);
+    }
+
+    public LottoResult calculateLottoResult(LottoNumber bonusNumber) {
+        return new LottoResult(player, winningLotto, bonusNumber);
+    }
+
+    public BigDecimal calculateRevenuePercent(BigDecimal totalReward) {
+        return player.calculateRevenuePercent(totalReward);
+    }
+
+    public List<String> getPlayerPurchaseLottos() {
+        return player.getPurchaseLottos();
+    }
+}
