5e02895593dba64e5d4c9d6fed39ca3781ae881b
Eunkeee <eunkeeeee@gmail.com>
2022-12-02 16:25:01 +0900

    test(LottoStore): 정상 구매 테스트

---
+++ src/test/java/lotto/model/LottoStoreTest.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class LottoStoreTest {
+
+    private LottoStore lottoStore;
+
+    @BeforeEach
+    void setUp() {
+        lottoStore = LottoStore.buyTicketsByBudget(8_000);
+    }
+
+    @Nested
+    class validInput {
+
+        @Test
+        void 정상_구매() {
+            Assertions.assertEquals(8, lottoStore.getTicketNumber());
+        }
+    }
+
+}
