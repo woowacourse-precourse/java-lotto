d47fc651ceec3ec1bc4d30ac87d90d9476c947ab
HaODay <ycis5013@naver.com>
2022-12-03 16:53:06 +0900

    feat(InputView): 보너스 번호를 입력 받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,27 +7,36 @@ import java.util.List;
 
 public class InputView {
 
-    public static int readPurchaseAmount(){
+    public static int readPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        String purchaseAmount=Console.readLine();
-        try{
+        String purchaseAmount = Console.readLine();
+        try {
             return Integer.parseInt(purchaseAmount);
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자를 입력해야 합니다.");
         }
     }
 
-    public static List<Integer> readWinningLotto(){
+    public static List<Integer> readWinningLotto() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        String[] input=Console.readLine().split(",");
-        List<Integer> winningLotto=new ArrayList<>();
+        String[] input = Console.readLine().split(",");
+        List<Integer> winningLotto = new ArrayList<>();
         try {
             for (String s : input) {
                 winningLotto.add(Integer.parseInt(s));
             }
-        } catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자를 입력해야 합니다.");
         }
         return winningLotto;
+    }
+
+    public static int readBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+        }
     }
 }
