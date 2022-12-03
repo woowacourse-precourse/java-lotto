20c88e347279cfec1e6f11a11498297f83741690
apptie <appti@naver.com>
2022-12-03 22:17:08 +0900

    test(LottoTestArgument): 잘못된 테스트 매개변수 수정

--- src/test/java/lotto/domain/number/arguments/LottoTestArgument.java
+++ src/test/java/lotto/domain/number/arguments/LottoTestArgument.java
@@ -11,11 +11,11 @@ public final class LottoTestArgument {
 
     static Stream<Arguments> lottoRankingArgument() {
         return Stream.of(
-                Arguments.of("11,12,13,14,15,16", 11, LottoRanking.NOTHING),
-                Arguments.of("1,2,3,11,12,13", 12, LottoRanking.FIFTH),
+                Arguments.of("11,12,13,14,15,16", 21, LottoRanking.NOTHING),
+                Arguments.of("1,2,3,11,12,13", 15, LottoRanking.FIFTH),
                 Arguments.of("1,2,3,4,11,12", 13, LottoRanking.FOURTH),
                 Arguments.of("1,2,3,4,5,12", 14, LottoRanking.THIRD),
-                Arguments.of("1,2,3,4,5,12", 12, LottoRanking.SECOND),
+                Arguments.of("1,2,3,4,5,12", 6, LottoRanking.SECOND),
                 Arguments.of("1,2,3,4,5,6", 16, LottoRanking.FIRST)
         );
     }
