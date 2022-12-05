88ee495de0f8bd20bed2183853501796a8c07f4a
HaODay <ycis5013@naver.com>
2022-12-05 19:39:49 +0900

    fix(InputView): 중복 검사하는 코드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -18,21 +18,26 @@ public class InputView {
     }
 
     public static List<Integer> readWinningLotto() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println("\n당첨 번호를 입력해 주세요.");
         String[] input = Console.readLine().split(",");
         List<Integer> winningLotto = new ArrayList<>();
-        try {
-            for (String s : input) {
-                winningLotto.add(Integer.parseInt(s));
+        int number;
+        for (String s : input) {
+            try {
+                number = Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("숫자를 입력해야 합니다.");
             }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+            if(winningLotto.contains(number)){
+                throw new IllegalArgumentException("중복된 숫자를 입력할 수 없습니다.");
+            }
+            winningLotto.add(number);
         }
         return winningLotto;
     }
 
     public static int readBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println("\n보너스 번호를 입력해 주세요.");
         try {
             return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
