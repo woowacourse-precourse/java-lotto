51e1aeb894067795a0a05bf194d02a8d45ee6a9e
HaODay <ycis5013@naver.com>
2022-12-05 19:34:39 +0900

    feat(OutputView): 당첨내역 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,37 @@
 package lotto.view;
 
+import lotto.domain.Prize;
+
+import java.text.DecimalFormat;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     public static void printMyLotto(List<List<Integer>> myLotto) {
-        System.out.println("/n"+myLotto.size()+"개를 구매했습니다.");
+        System.out.println();
+        System.out.println(myLotto.size() + "개를 구매했습니다.");
         for (List<Integer> lotto : myLotto) {
             System.out.println(lotto);
         }
     }
+
+    public static void printResult(EnumMap<Prize, Integer> result) {
+        System.out.println("\n당첨 통계\n---");
+        for (Map.Entry<Prize, Integer> e : result.entrySet()) {
+            DecimalFormat decFormat = new DecimalFormat("###,###");
+            String str = decFormat.format(e.getKey().money);
+            if (e.getKey().sameNumbers == 7) {
+                System.out.println("5개 일치, 보너스 볼 일치 (" + str + "원) - " + e.getValue() + "개");
+            }
+            if (e.getKey().sameNumbers != 7) {
+                System.out.println(e.getKey().sameNumbers + "개 일치 (" + str + "원) - " + e.getValue() + "개");
+            }
+        }
+    }
+
+    public static void printRate(float rate) {
+        System.out.printf("총 수익률은 %.1f%%입니다.", rate);
+    }
+
 }
