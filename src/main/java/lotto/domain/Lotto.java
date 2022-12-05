a01b92dbab340e6409f3b15c1f1ea31533fe25a9
HaODay <ycis5013@naver.com>
2022-12-05 19:35:41 +0900

    chore: 작은 수정사항 반영

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -14,12 +14,9 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("6개의 숫자를 입력해야 합니다.");
         }
-        for(int i=0;i<numbers.size();i++){
-            if(numbers.get(i)<1||numbers.get(i)>45){
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) < 1 || numbers.get(i) > 45) {
                 throw new IllegalArgumentException("로또 번호의 숫자 범위는 1~45까지입니다.");
-            }
-            if(numbers.contains(numbers.get(i))){
-                throw new IllegalArgumentException("중복된 숫자를 입력할 수 없습니다.");
             }
         }
     }
