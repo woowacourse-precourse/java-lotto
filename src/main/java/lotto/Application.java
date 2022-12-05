6dc7133c608b677d3c36945b934578bb6e58fe98
HaODay <ycis5013@naver.com>
2022-12-06 06:27:26 +0900

    chore: 포매팅

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,8 +6,8 @@ public class Application {
     public static void main(String[] args) {
         try {
             GameController.run();
-        } catch(IllegalArgumentException e){
-            System.out.println("[ERROR] "+e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
         }
     }
 }
