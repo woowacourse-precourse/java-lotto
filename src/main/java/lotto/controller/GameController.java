62c59cda451886a74ecf111d431bfc6515cc894e
HaODay <ycis5013@naver.com>
2022-12-05 20:39:31 +0900

    refactor(GameController): 메서드 분리

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,6 +1,13 @@
 package lotto.controller;
 
-import lotto.domain.*;
+import lotto.domain.BonusNumber;
+import lotto.domain.Calculator;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
+import lotto.domain.PurchaseAmount;
+
+import lotto.util.Prize;
+
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -9,16 +16,33 @@ import java.util.List;
 
 public class GameController {
 
+    static PurchaseAmount purchaseAmount;
+    static List<List<Integer>> myLotto;
+    static Lotto winningLotto;
+    static BonusNumber bonusNumber;
+
     public static void run() throws IllegalArgumentException{
-        PurchaseAmount purchaseAmount = new PurchaseAmount(InputView.readPurchaseAmount());
-        List<List<Integer>> myLotto = LottoGenerator.generateLotto(purchaseAmount);
-        OutputView.printMyLotto(myLotto);
-        Lotto winningLotto = new Lotto(InputView.readWinningLotto());
-        BonusNumber bonusNumber = new BonusNumber(InputView.readBonusNumber(), winningLotto);
-        Calculator calculator = new Calculator(myLotto, winningLotto, bonusNumber);
-        EnumMap<Prize, Integer> result = calculator.saveResult();
-        float rate = calculator.caculateRate(purchaseAmount);
+        issueLotto();
+        setWinningLotto();
+        getResult();
+    }
+
+    private static void getResult() {
+        Calculator.setCalculator(myLotto, winningLotto, bonusNumber);
+        EnumMap<Prize, Integer> result = Calculator.saveResult();
         OutputView.printResult(result);
+        float rate = Calculator.calculateRate(purchaseAmount);
         OutputView.printRate(rate);
+    }
+
+    private static void setWinningLotto() throws IllegalArgumentException{
+        winningLotto = new Lotto(InputView.readWinningLotto());
+        BonusNumber.validate(InputView.readBonusNumber(), winningLotto);
+    }
+
+    public static void issueLotto() throws IllegalArgumentException{
+        PurchaseAmount.validate(InputView.readPurchaseAmount());
+        myLotto = LottoGenerator.generateLotto(purchaseAmount);
+        OutputView.printMyLotto(myLotto);
     }
 }
