e9c1d35a8f984d9329d8c44b638f3b9284410546
hingjae <ljh23270@naver.com>
2022-12-06 12:57:56 +0900

    구입 금액 입력 예외처리

--- src/test/java/lotto/inputview/InputTest.java
+++ src/test/java/lotto/inputview/InputTest.java
@@ -1,4 +1,24 @@
+package lotto.inputview;
+
+import lotto.Exception.Exception;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class InputTest {
-  
+    @Test
+    @DisplayName("구입 금액 입력 시 예외처리 (숫자 X)")
+    void 구입금액() {
+        Assertions.assertThatThrownBy(() -> Input.isValid("1000a")).isInstanceOf(IllegalArgumentException.class);
+        assertThrows(IllegalArgumentException.class, () -> Input.isValid("abcd"));
+    }
+
+    @Test
+    @DisplayName("구입 금액 입력 시 예외처리 (천원 단위 X)")
+    void 구입금액2() {
+        assertThrows(IllegalArgumentException.class, () -> Input.isValid("1100"));
+        assertThrows(IllegalArgumentException.class, () -> Input.isValid("11500"));
+    }
 }
