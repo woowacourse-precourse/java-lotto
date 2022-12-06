faf53943c750241de3f285d05abc6c31c4bf6d29
hingjae <ljh23270@naver.com>
2022-12-06 19:33:24 +0900

    당첨 번호 입력

--- src/main/java/lotto/inputview/InputWinningLotto.java
+++ src/main/java/lotto/inputview/InputWinningLotto.java
@@ -1,2 +1,11 @@
-package lotto.inputview;public class InputWinningLotto {
+package lotto.inputview;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputWinningLotto {
+    public String input() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String numbers = Console.readLine();
+        return numbers;
+    }
 }
