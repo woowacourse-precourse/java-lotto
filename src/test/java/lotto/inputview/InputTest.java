32dbcafebcc71d4284fef6afef044967e609de6c
hingjae <ljh23270@naver.com>
2022-12-06 19:37:15 +0900

    구입 금액 입력 시 예외 테스트

--- src/test/java/lotto/inputview/InputTest.java
+++ src/test/java/lotto/inputview/InputTest.java
@@ -1,6 +1,5 @@
 package lotto.inputview;
 
-import lotto.Exception.Exception;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,17 +7,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputTest {
+    InputMoney input = new InputMoney();
     @Test
     @DisplayName("구입 금액 입력 시 예외처리 (숫자 X)")
     void 구입금액() {
-        Assertions.assertThatThrownBy(() -> Input.isValid("1000a")).isInstanceOf(IllegalArgumentException.class);
-        assertThrows(IllegalArgumentException.class, () -> Input.isValid("abcd"));
+        Assertions.assertThatThrownBy(() -> input.isValid("1000a")).isInstanceOf(IllegalArgumentException.class);
+        assertThrows(IllegalArgumentException.class, () -> input.isValid("abcd"));
     }
 
     @Test
     @DisplayName("구입 금액 입력 시 예외처리 (천원 단위 X)")
     void 구입금액2() {
-        assertThrows(IllegalArgumentException.class, () -> Input.isValid("1100"));
-        assertThrows(IllegalArgumentException.class, () -> Input.isValid("11500"));
+        assertThrows(IllegalArgumentException.class, () -> input.isValid("1100"));
+        assertThrows(IllegalArgumentException.class, () -> input.isValid("11500"));
     }
 }
