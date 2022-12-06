c2db6a83d6f7f4913e0c92538fe1557bbc3158e7
hingjae <ljh23270@naver.com>
2022-12-06 19:35:50 +0900

    입력한 money를 amount로 반환

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -1,4 +1,16 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MoneyTest {
-  
+    @Test
+    @DisplayName("입력한 money를 amount로 변환")
+    void moneyToAmount() {
+        Money money = new Money("5000");
+        Assertions.assertThat(money.moneyToAmount()).isEqualTo(5);
+    }
 }
