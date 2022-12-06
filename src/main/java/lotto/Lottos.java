9978608d839397a1791f599f8864b707d08af7ed
hingjae <ljh23270@naver.com>
2022-12-06 19:19:33 +0900

    랜덤 로또 번호 생성 : 발급받은 로또번호 리스트

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -1,2 +1,22 @@
-package lotto;public class Lottos {
+package lotto;
+
+import lotto.lottonumber.LottoNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    List<Lotto> lottos = new ArrayList<>();
+
+    public void issuedLottos(int amount) {
+        for (int i = 0; i < amount; i++) {
+            List lottonumbers = LottoNumber.generate();
+            Lotto lotto = new Lotto(lottonumbers);
+            lottos.add(lotto);
+        }
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
 }
