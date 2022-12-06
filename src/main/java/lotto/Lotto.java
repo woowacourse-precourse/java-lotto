ad771b3a05b1468cf71ec135354f44c0924396e4
hingjae <ljh23270@naver.com>
2022-12-06 19:16:51 +0900

    당첨 번호 입력 예외처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,19 +1,45 @@
 package lotto;
 
+import lotto.Exception.Exception;
+
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        isValid(numbers);
+        sortNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+    private void isValid(List<Integer> numbers) {
+        validateSize(numbers);
+        isNotDup(numbers);
+        isValidRange(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        Exception.checkSize(numbers);
+    }
+
+    private void isNotDup(List<Integer> numbers) {
+        Exception.checkDup(numbers);
+    }
+
+    private void isValidRange(List<Integer> numbers) {
+        Exception.checkRange(numbers);
+    }
+
+    private void sortNumbers(List<Integer> numbers) {
+        Collections.sort(numbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
     // TODO: 추가 기능 구현
