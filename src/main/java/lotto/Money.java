446c67cb598cc694c7c5318513cfcedfc6b19dfc
hingjae <ljh23270@naver.com>
2022-12-06 19:20:57 +0900

    구입 금액 입력 : 돈에 맞는 수량 반환

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -1,2 +1,24 @@
-package lotto;public class Money {
+package lotto;
+
+public class Money {
+    private int won;
+
+    public Money(String won) {
+        this.won = Integer.parseInt(won);
+    }
+
+    public int moneyToAmount() {
+        int amount;
+        amount = this.won / 1000;
+        return amount;
+
+    }
+
+    public int getMoney() {
+        return won;
+    }
+
+    public void setMoney(int won) {
+        this.won = won;
+    }
 }
