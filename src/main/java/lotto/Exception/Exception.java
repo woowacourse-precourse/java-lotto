83e74916afad5b40dd98af4cfef7697549db6926
hingjae <ljh23270@naver.com>
2022-12-06 12:50:01 +0900

    구입 금액 입력 예외처리

--- src/main/java/lotto/Exception/Exception.java
+++ src/main/java/lotto/Exception/Exception.java
@@ -1,2 +1,18 @@
-package lotto.Exception;public class Exception {
+package lotto.Exception;
+
+public class Exception {
+    public static void isNum(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    public static void isThousands(String input) {
+        int money = Integer.parseInt(input);
+        if(money % 1000 != 0) {
+            throw new IllegalArgumentException("1000원 단위로 입력할 수 있습니다.");
+        }
+    }
 }
