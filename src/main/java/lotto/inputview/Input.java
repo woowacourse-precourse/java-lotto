02acf848164d86ade8fb790a0409a286575890dd
hingjae <ljh23270@naver.com>
2022-12-06 12:49:14 +0900

    구입 금액 입력 : 1000원 단위 숫자

--- src/main/java/lotto/inputview/Input.java
+++ src/main/java/lotto/inputview/Input.java
@@ -1,2 +1,19 @@
-package lotto.inputview;public class Input {
+package lotto.inputview;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.Exception.Exception;
+
+public class Input {
+    public static String inputMoney() {
+        String money;
+        System.out.println("구입금액을 입력해 주세요.");
+        money = Console.readLine();
+        isValid(money);
+        return money;
+    }
+
+    public static void isValid(String input) {
+        Exception.isNum(input);
+        Exception.isThousands(input);
+    }
 }
