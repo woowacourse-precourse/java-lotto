2f008c0731869c8d33c635d7166e3f8498f31712
hingjae <ljh23270@naver.com>
2022-12-06 19:27:09 +0900

    랜덤 로또 번호 생성

--- src/main/java/lotto/lottonumber/LottoNumber.java
+++ src/main/java/lotto/lottonumber/LottoNumber.java
@@ -1,2 +1,12 @@
-package lotto.lottonumber;public class LottoNumber {
+package lotto.lottonumber;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoNumber {
+    public static List generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
 }
