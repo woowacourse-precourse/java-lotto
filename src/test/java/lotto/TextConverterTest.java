17b0a0cdf0220fd7166b4c417211a4d558a28db5
hingjae <ljh23270@naver.com>
2022-12-06 19:36:29 +0900

    당청 번호 입력 받은 후 타입 변환 테스트

--- src/test/java/lotto/TextConverterTest.java
+++ src/test/java/lotto/TextConverterTest.java
@@ -1,4 +1,19 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TextConverterTest {
-  
+    @Test
+    @DisplayName("콤마로 문자열을 분할 후 리스트로 변환")
+    void comma() {
+        String[] strings = TextConverter.splitComma("1,2,3,4,5");
+        List list = Arrays.asList(strings);
+        System.out.println("list = " + list);
+    }
 }
