88ebd911b3af6c173cb4adc7f3b3685890672621
ym2629 <dydals2629@naver.com>
2022-12-07 13:51:57 +0900

    refactor: ouput my lotto

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,14 @@
 package lotto.view;
 
+import java.util.List;
+
+import static lotto.util.message.OutputMessage.OUTPUT_MY_LOTTO;
+
 public class OutputView {
+    public static void myLotto(List<List<Integer>> myLotto) {
+        System.out.printf(OUTPUT_MY_LOTTO.get(), myLotto.size());
+        for (List<Integer> lotto : myLotto) {
+            System.out.println(lotto);
+        }
+    }
 }
