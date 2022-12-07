4dfea281294d51c3fe4047503c066b58520e7d5d
ym2629 <dydals2629@naver.com>
2022-12-07 13:12:03 +0900

    refactor: get winning number

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,7 +3,11 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.InputValidator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static lotto.util.message.OutputMessage.INPUT_PURCHASE_AMOUNT;
+import static lotto.util.message.OutputMessage.INPUT_WINNING_LOTTO;
 
 public class InputView {
     public static int purchaseMoney(){
@@ -12,7 +16,22 @@ public class InputView {
         return numberCheck(purchaseMoney);
     }
 
+    public static List<Integer> winningLotto() {
+        System.out.println(INPUT_WINNING_LOTTO.get());
+        String[] input = Console.readLine().split(",");
+        List<Integer> winningLotto = listNumbersCheck(input);
+        return winningLotto;
+    }
     public static int numberCheck(String number){
         return InputValidator.validate(number);
+    }
+
+    public static List<Integer> listNumbersCheck(String[] arr){
+        List<Integer> list = new ArrayList<>();
+        for (String s : arr) {
+            int number =  numberCheck(s);
+            list.add(number);
+        }
+        return list;
     }
 }
