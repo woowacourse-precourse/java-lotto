dee29e5ce344604f45215f6a59ee3bc86938c263
hingjae <ljh23270@naver.com>
2022-12-08 11:05:32 +0900

    보너스 번호 입력

--- src/main/java/lotto/inputview/InputBonusNumber.java
+++ src/main/java/lotto/inputview/InputBonusNumber.java
@@ -1,2 +1,11 @@
-package lotto.inputview;public class InputBonusNumber {
+package lotto.inputview;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputBonusNumber {
+    public String input() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusNumber = Console.readLine();
+        return bonusNumber;
+    }
 }
