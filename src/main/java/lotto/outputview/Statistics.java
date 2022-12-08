2fcad155bdcb50366d25f5e68880ec767439291b
hingjae <ljh23270@naver.com>
2022-12-08 11:08:33 +0900

    출력 : 당첨내역, 수익률

--- src/main/java/lotto/outputview/Statistics.java
+++ src/main/java/lotto/outputview/Statistics.java
@@ -1,2 +1,15 @@
-package lotto.outputview;public class Statistics {
+package lotto.outputview;
+
+import lotto.lotto.LottoRank;
+
+public class Statistics {
+    public static void printWin() {
+        System.out.println("당첨 통계\n--");
+        for(LottoRank lottoRank : LottoRank.values()) {
+            System.out.println(lottoRank);
+        }
+    }
+    public static void printRevenue(double result) {
+        System.out.println("총 수익률은 " + String.format("%.1f", result) +"%입니다.");
+    }
 }
