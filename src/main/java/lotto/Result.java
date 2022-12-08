e47205a54eef54eb738ece774e201f566730393f
hingjae <ljh23270@naver.com>
2022-12-08 11:03:12 +0900

    당첨 번호와 비교, 통계, 수익률 계산

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -1,2 +1,62 @@
-package lotto;public class Result {
+package lotto;
+
+import lotto.lotto.Lotto;
+import lotto.lotto.LottoRank;
+import lotto.lotto.Lottos;
+import lotto.lotto.WinningLotto;
+
+import java.util.List;
+
+public class Result {
+    public void compare(Lottos lottos, WinningLotto winningLotto) {
+        for (Lotto lotto : lottos.getLottos()) {
+            int result = checkCount(lotto, winningLotto);
+            whatRank(result, lotto, winningLotto);
+        }
+    }
+
+    private int checkCount(Lotto lotto, WinningLotto winningLotto) {
+        int count = 0;
+        List winningLottoNumbers = winningLotto.getLotto().getNumbers();
+        for (int number : lotto.getNumbers()) {
+            if (winningLottoNumbers.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void whatRank(int count, Lotto lotto, WinningLotto winningLotto) {
+        for (LottoRank rank : LottoRank.values()) {
+            if (rank.getMatchedCount() == 5 && count == 5) {
+                checkBonus(lotto, winningLotto);
+                return;
+            }
+            if (count == rank.getMatchedCount()) {
+                rank.plus();
+            }
+        }
+    }
+
+    private void checkBonus(Lotto lotto, WinningLotto winningLotto) {
+        if (lotto.getNumbers().contains(winningLotto.getBonusNumber())) {
+            LottoRank.SECOND.plus();
+            return;
+        }
+        LottoRank.THIRD.plus();
+    }
+
+    public double revenue(int money) {
+        int total = 0;
+        for (LottoRank lottoRank : LottoRank.values()) {
+            int benefit = lottoRank.getResult() * lottoRank.getMoney();
+            total += benefit;
+        }
+        return calculateRevenue(total, money);
+    }
+
+    private double calculateRevenue(int total, int money) {
+        double result = ((double)total / money) * 100;
+        return result;
+    }
 }
