5bb653538554350d72400c42c55308600354e644
hingjae <ljh23270@naver.com>
2022-12-08 11:10:02 +0900

    타입 변환 테스트

--- src/test/java/lotto/TextConverterTest.java
+++ src/test/java/lotto/TextConverterTest.java
@@ -1,12 +1,11 @@
 package lotto;
 
+import lotto.inputtool.TextConverter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class TextConverterTest {
     @Test
