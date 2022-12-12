dad19a43e2ef7c530b1bdd920bc6b7bfd1b15d31
hong-sile <gurwns9325@naver.com>
2022-12-12 19:54:51 +0900

    feat: 당첨번호를 입력받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,18 +1,33 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final String VALUE_IS_NON_NUMERIC_MESSAGE = "입력값은 정수여야 합니다.";
+    private static final String SEPARATOR = ",";
 
     public static int inputPurchaseAmount() {
-        return inputInteger();
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        return convertInteger(input);
     }
 
-    private static int inputInteger() {
+    public static List<Integer> inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해주세요.");
         String input = Console.readLine();
+        String[] splitNumbers = input.split(SEPARATOR);
+        List<Integer> winningNumbers = Arrays.stream(splitNumbers)
+                .map(InputView::convertInteger)
+                .collect(Collectors.toList());
+        return winningNumbers;
+    }
+
+    private static int convertInteger(String number) {
         try {
-            return Integer.parseInt(input);
+            return Integer.parseInt(number);
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException(VALUE_IS_NON_NUMERIC_MESSAGE);
         }
