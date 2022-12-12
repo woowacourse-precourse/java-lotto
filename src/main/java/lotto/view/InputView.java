f2fac55429fda3148f468a02e45fa3dd18b32c9c
hong-sile <gurwns9325@naver.com>
2022-12-12 19:58:45 +0900

    feat: 보너스 번호를 입력받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,22 +7,31 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final String VALUE_IS_NON_NUMERIC_MESSAGE = "입력값은 정수여야 합니다.";
+    private static final String READ_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String READ_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해주세요.";
+    private static final String READ_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String SEPARATOR = ",";
 
-    public static int inputPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
+    public static int readPurchaseAmount() {
+        System.out.println(READ_PURCHASE_AMOUNT_MESSAGE);
         String input = Console.readLine();
         return convertInteger(input);
     }
 
-    public static List<Integer> inputWinningNumbers() {
-        System.out.println("당첨 번호를 입력해주세요.");
+    public static List<Integer> readWinningNumbers() {
+        System.out.println(READ_WINNING_NUMBERS_MESSAGE);
         String input = Console.readLine();
         String[] splitNumbers = input.split(SEPARATOR);
         List<Integer> winningNumbers = Arrays.stream(splitNumbers)
                 .map(InputView::convertInteger)
                 .collect(Collectors.toList());
         return winningNumbers;
+    }
+
+    public static int readBonusNumber() {
+        System.out.println(READ_BONUS_NUMBER_MESSAGE);
+        String input = Console.readLine();
+        return convertInteger(input);
     }
 
     private static int convertInteger(String number) {
