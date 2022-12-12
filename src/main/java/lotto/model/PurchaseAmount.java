e633014cb8e24d7cb169dca4826541c98142899b
hong-sile <gurwns9325@naver.com>
2022-12-12 20:36:59 +0900

    refactor: LottoPurchaseAmount에서 PurchaseAmount로 클래스 이름 변경

--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -1,11 +1,11 @@
 package lotto.model;
 
-public class LottoPurchaseAmount {
+public class PurchaseAmount {
     public static final int LOTTO_PRICE = 1_000;
     private static final String INVALID_AMOUNT_MESSAGE = "구매 금액은 1000원으로 나눠 떨어져야 합니다.";
     private final int amount;
 
-    public LottoPurchaseAmount(int amount) {
+    public PurchaseAmount(int amount) {
         validate(amount);
         this.amount = amount;
     }
