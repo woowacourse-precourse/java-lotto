b1dea831eb4f49d39517189411332bcaaebe8234
hong-sile <gurwns9325@naver.com>
2022-12-12 19:28:43 +0900

    feat: 로또의 유효성 검사 추가
    1. 로또의 사이즈가 6인지에 대한 검사
    2. 로또번호 중에 중복이 있는지에 대한 검사

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,18 +1,37 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private static final String INVALID_LOTTO_SIZE_MESSAGE = "로또는 6개의 숫자로 구성됩니다.";
+    private static final String LOTTO_HAS_DUPLICATE_MESSAGE = "로또는 중복되지 않은 숫자로 구성됩니다.";
+    private static final int LOTTO_SIZE = 6;
+    private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = numbers.stream()
+                .map(LottoNumber::valueOf)
+                .collect(Collectors.toList());
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        checkSizeValidate(numbers);
+        checkListDuplicate(numbers);
+    }
+
+    private static void checkSizeValidate(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(INVALID_LOTTO_SIZE_MESSAGE);
+        }
+    }
+
+    private static void checkListDuplicate(List<Integer> numbers) {
+        HashSet<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+        if (numbers.size() != nonDuplicateNumbers.size()) {
+            throw new IllegalArgumentException(LOTTO_HAS_DUPLICATE_MESSAGE);
         }
     }
 
