db3504f76e46eaa8b9a09d28de0a4d95235b9c97
hong-sile <gurwns9325@naver.com>
2022-12-12 19:36:46 +0900

    feat: 로또 구입금액을 표현하는 LottoPurchaseAmount라는 클래스 추가
    로또 구입 금액이 1,000원으로 나누어 떨어지지 않는 경우 예외 처리

---
+++ src/main/java/lotto/model/LottoPurchaseAmount.java
@@ -0,0 +1,18 @@
+package lotto.model;
+
+public class LottoPurchaseAmount {
+    public static final int LOTTO_PRICE = 1_000;
+    private static final String INVALID_AMOUNT_MESSAGE = "구매 금액은 1000원으로 나눠 떨어져야 합니다.";
+    private final int amount;
+
+    public LottoPurchaseAmount(int amount) {
+        validate(amount);
+        this.amount = amount;
+    }
+
+    private void validate(int amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_MESSAGE);
+        }
+    }
+}
