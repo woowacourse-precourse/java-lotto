ad8d030d5695f9ad43e11cb0412d621f5e77adaa
hong-sile <gurwns9325@naver.com>
2022-12-14 13:12:13 +0900

    feat: 번호 일치개수와 보너스 일치 여부를 입력받으면, LottoPrize를 반환하는 메소드추가

--- src/main/java/lotto/model/LottoPrize.java
+++ src/main/java/lotto/model/LottoPrize.java
@@ -1,12 +1,14 @@
 package lotto.model;
 
+import java.util.Arrays;
+
 public enum LottoPrize {
     _1ST_PRIZE(6, 2_000_000_000),
-    _2nd_PRIZE(5, 30_000_000),
-    _3rd_PRIZE(5, 1_500_000),
-    _4th_PRIZE(4, 50_000),
-    _5th_PRIZE(3, 5_000),
-    nothing(0, 0);
+    _2ND_PRIZE(5, 30_000_000),
+    _3RD_PRIZE(5, 1_500_000),
+    _4TH_PRIZE(4, 50_000),
+    _5TH_PRIZE(3, 5_000),
+    _NOTHING(0, 0);
 
     private final int matchCount;
     private final int prize;
@@ -14,5 +16,24 @@ public enum LottoPrize {
     LottoPrize(int matchCount, int prize) {
         this.matchCount = matchCount;
         this.prize = prize;
+    }
+
+    public LottoPrize of(int matchCount, boolean bonusMatch) {
+        LottoPrize lottoPrize = getLottoPrize(matchCount);
+        return check2ndOr3rd(lottoPrize, bonusMatch);
+    }
+
+    private LottoPrize check2ndOr3rd(LottoPrize lottoPrize, boolean bonusMatch) {
+        if (lottoPrize == _2ND_PRIZE && !bonusMatch) {
+            return _3RD_PRIZE;
+        }
+        return lottoPrize;
+    }
+
+    private LottoPrize getLottoPrize(int matchCount) {
+        return Arrays.stream(LottoPrize.values())
+                .filter(prize -> prize.matchCount == matchCount)
+                .findFirst()
+                .orElse(_NOTHING);
     }
 }
