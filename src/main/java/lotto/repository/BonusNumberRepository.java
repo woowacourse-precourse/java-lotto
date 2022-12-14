9bfa66f5d4d5f5b02683222828b802522cfe382d
HiiWee <wpdlzhf159@gmail.com>
2022-12-14 20:21:41 +0900

    feat: 보너스 번호 저장소 생성

---
+++ src/main/java/lotto/repository/BonusNumberRepository.java
@@ -0,0 +1,28 @@
+package lotto.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.BonusNumber;
+
+public class BonusNumberRepository {
+    private static final BonusNumberRepository instance = new BonusNumberRepository();
+    private static final List<BonusNumber> store = new ArrayList<>();
+    public static final int BONUS_NUMBER_INDEX = 0;
+
+    private BonusNumberRepository() {
+    }
+
+    public static BonusNumberRepository getInstance() {
+        return instance;
+    }
+
+    public BonusNumber save(final BonusNumber bonusNumber) {
+        store.add(bonusNumber);
+        return find();
+    }
+
+    private BonusNumber find() {
+        return store.get(BONUS_NUMBER_INDEX);
+    }
+
+}
