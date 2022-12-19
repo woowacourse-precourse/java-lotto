90e84019c9504158d3be8ef01ae247fe450aeb28
fakerdeft <whakswp123@naver.com>
2022-12-20 00:08:45 +0900

    feat: 구매 개수 출력문 추가

--- src/main/java/Views/OutputView.java
+++ src/main/java/Views/OutputView.java
@@ -1,9 +1,17 @@
 package Views;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static Views.InputView.INPUT_VIEW;
+
 public class OutputView {
     public static final OutputView OUTPUT_VIEW = new OutputView();
 
     public void PrintPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void PrintCountPurchasingLotto() {
+        System.out.println(INPUT_VIEW.InputCash(Console.readLine()) + "개를 구매했습니다.");
     }
 }
