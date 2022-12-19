06b83cd30c1bc40c9270b4db0de7318aec8d1eee
fakerdeft <whakswp123@naver.com>
2022-12-19 23:56:36 +0900

    feat: 구매 금액 명령문 추가

--- src/main/java/Views/OutputView.java
+++ src/main/java/Views/OutputView.java
@@ -1,4 +1,9 @@
 package Views;
 
 public class OutputView {
+    public static final OutputView OUTPUT_VIEW = new OutputView();
+
+    public void PrintPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
 }
