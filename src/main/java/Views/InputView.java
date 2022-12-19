762ea7acd65b33722a7582ac125b39d27addb118
fakerdeft <whakswp123@naver.com>
2022-12-19 23:56:07 +0900

    feat: 구매 금액 입력 추가

--- src/main/java/Views/InputView.java
+++ src/main/java/Views/InputView.java
@@ -1,4 +1,12 @@
 package Views;
 
+import Utils.Exceptions;
+
 public class InputView {
+    public static final InputView INPUT_VIEW = new InputView();
+
+    public int InputCash(String readLine) {
+        Exceptions.ExcludeUnderThousand(readLine);
+        return Integer.parseInt(readLine) / 1000;
+    }
 }
