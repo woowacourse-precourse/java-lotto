b57492e166998e37352cec6e33d94e921d6975c1
fakerdeft <whakswp123@naver.com>
2022-12-22 00:30:52 +0900

    feat: 당첨 번호 입력문 추가

--- src/main/java/Views/OutputView.java
+++ src/main/java/Views/OutputView.java
@@ -3,11 +3,15 @@ package Views;
 public class OutputView {
     public static final OutputView OUTPUT_VIEW = new OutputView();
 
-    public void PrintPurchaseAmount() {
+    public void PrintInputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
     public void PrintCountPurchasingLotto(int readLine) {
         System.out.println(readLine + "개를 구매했습니다.");
+    }
+
+    public void PrintInputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
     }
 }
