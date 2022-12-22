e6a1d14ca235e01db4f33b4397a8b3327026b342
fakerdeft <whakswp123@naver.com>
2022-12-23 01:07:07 +0900

    feat: 당첨 번호와 보너스 번호 비교하는 기능 추가

--- src/main/java/Controller/LottoController.java
+++ src/main/java/Controller/LottoController.java
@@ -1,4 +1,24 @@
 package Controller;
 
+import Utils.Exceptions;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static Views.InputView.INPUT_VIEW;
+import static Views.OutputView.OUTPUT_VIEW;
+
 public class LottoController {
+    public static final LottoController LOTTO_CONTROLLER = new LottoController();
+
+    public List<List<String>> CompareWinningNumbersWithBonusNumbers() {
+        List<List<String>> winningNumbers = new ArrayList<>();
+        OUTPUT_VIEW.PrintInputWinningNumbers();
+        winningNumbers.add(INPUT_VIEW.InputWinningNumbers(Console.readLine()));
+        OUTPUT_VIEW.PrintInputBonusNumber();
+        winningNumbers.add(INPUT_VIEW.InputBonusNumber(Console.readLine()));
+        Exceptions.ValidateNumbersDuplication(winningNumbers);
+        return winningNumbers;
+    }
 }
