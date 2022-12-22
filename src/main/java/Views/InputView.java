e2e99cb816ac320f9b1a830769ec3d7a62a632c8
fakerdeft <whakswp123@naver.com>
2022-12-23 01:05:30 +0900

    feat: 당첨 번호/보너스 번호 입력 기능 추가

--- src/main/java/Views/InputView.java
+++ src/main/java/Views/InputView.java
@@ -2,11 +2,28 @@ package Views;
 
 import Utils.Exceptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
     public static final InputView INPUT_VIEW = new InputView();
 
     public int InputCash(String readLine) {
         Exceptions.ExcludeUnderThousand(readLine);
         return Integer.parseInt(readLine) / 1000;
+    }
+
+    public List<String> InputWinningNumbers(String readLine) {
+        List<String> winningNumbers;
+        winningNumbers = List.of(readLine.split(","));
+        Exceptions.ExcludeNoLottoNumbers(winningNumbers);
+        return winningNumbers;
+    }
+
+    public List<String> InputBonusNumber(String readLine) {
+        List<String> bonusNumber = new ArrayList<>();
+        Exceptions.ExcludeNoBonusNumber(readLine);
+        bonusNumber.add(readLine);
+        return bonusNumber;
     }
 }
