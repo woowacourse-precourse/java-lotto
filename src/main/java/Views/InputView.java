875f25bd1390c211206d8599d337902ad0279994
fakerdeft <whakswp123@naver.com>
2022-12-24 10:20:04 +0900

    feat: 참조하는 객체명 수정

--- src/main/java/Views/InputView.java
+++ src/main/java/Views/InputView.java
@@ -1,28 +1,28 @@
 package Views;
 
-import Utils.Exceptions;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import static Utils.Exceptions.EXCEPTIONS;
 
 public class InputView {
     public static final InputView INPUT_VIEW = new InputView();
 
     public int InputCash(String readLine) {
-        Exceptions.ExcludeUnderThousand(readLine);
+        EXCEPTIONS.ExcludeUnderThousand(readLine);
         return Integer.parseInt(readLine) / 1000;
     }
 
     public List<String> InputWinningNumbers(String readLine) {
         List<String> winningNumbers;
         winningNumbers = List.of(readLine.split(","));
-        Exceptions.ExcludeNoLottoNumbers(winningNumbers);
+        EXCEPTIONS.ExcludeNoLottoNumbers(winningNumbers);
         return winningNumbers;
     }
 
     public List<String> InputBonusNumber(String readLine) {
         List<String> bonusNumber = new ArrayList<>();
-        Exceptions.ExcludeNoBonusNumber(readLine);
+        EXCEPTIONS.ExcludeNoBonusNumber(readLine);
         bonusNumber.add(readLine);
         return bonusNumber;
     }
